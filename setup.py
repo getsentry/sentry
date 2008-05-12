8e3e088f155a27d1a466588761130c127a8b28f5
David Cramer <dcramer@gmail.com>
2008-05-12 16:28:33 +0000

    added distutils support

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='djangodblog',
+      version='1.0',
+      description='Django DB Error Logging',
+      author='David Cramer',
+      author_email='dcramer@gmail.com',
+      url='http://code.curse.com/p/django-db-log/',
+      packages=['djangodblog'],
+     )
