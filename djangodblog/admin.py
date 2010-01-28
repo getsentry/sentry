dd32336d3860a004358335c45d82cd25a1a77f9e
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-01-28 16:07:11 -0600

    Some admin cleanup

--- djangodblog/admin.py
+++ djangodblog/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from models import ErrorBatch, Error
 
 class ErrorBatchAdmin(admin.ModelAdmin):
-    list_display    = ('logger', 'class_name', 'status', 'message', 'last_seen', 'times_seen', 'url', 'server_name')
+    list_display    = ('server_name', 'logger', 'class_name', 'status', 'message', 'last_seen', 'times_seen', 'url')
+    list_display_links = ('message',)
     list_filter     = ('logger', 'class_name', 'server_name', 'status')
-    ordering        = ('status', '-last_seen')
+    ordering        = ('-last_seen')
     actions         = ['resolve_errorbatch']
 
     def resolve_errorbatch(self, request, queryset):
