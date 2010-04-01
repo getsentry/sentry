d4366392eabcde0b555d1d51367070235ba9a2d0
David Cramer <dcramer@David-Cramers-MacBook.local>
2010-03-31 20:05:57 -0500

    Removed unused imports

--- djangodblog/models.py
+++ djangodblog/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
-from django.template.defaultfilters import urlizetrunc
 
 try:
     from idmapper.models import SharedMemoryModel as Model
