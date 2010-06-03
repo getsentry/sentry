93472b0555de02b930b7cbacc088fe4a1305bc85
David Cramer <dcramer@gmail.com>
2010-06-03 16:16:28 -0700

    Missing migrations from last commit

---
+++ djangodblog/migrations/0002_update_indexes.py
@@ -0,0 +1,71 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+    
+    def forwards(self, orm):
+        
+        # Adding index on 'Error', fields ['class_name']
+        db.create_index('djangodblog_error', ['class_name'])
+
+        # Adding index on 'Error', fields ['datetime']
+        db.create_index('djangodblog_error', ['datetime'])
+
+        # Adding index on 'ErrorBatch', fields ['first_seen']
+        db.create_index('djangodblog_errorbatch', ['first_seen'])
+
+        # Adding index on 'ErrorBatch', fields ['last_seen']
+        db.create_index('djangodblog_errorbatch', ['last_seen'])
+    
+    
+    def backwards(self, orm):
+        
+        # Removing index on 'Error', fields ['class_name']
+        db.delete_index('djangodblog_error', ['class_name'])
+
+        # Removing index on 'Error', fields ['datetime']
+        db.delete_index('djangodblog_error', ['datetime'])
+
+        # Removing index on 'ErrorBatch', fields ['first_seen']
+        db.delete_index('djangodblog_errorbatch', ['first_seen'])
+
+        # Removing index on 'ErrorBatch', fields ['last_seen']
+        db.delete_index('djangodblog_errorbatch', ['last_seen'])
+    
+    
+    models = {
+        'djangodblog.error': {
+            'Meta': {'object_name': 'Error'},
+            'class_name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'data': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'datetime': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'default': '40', 'db_index': 'True', 'blank': 'True'}),
+            'logger': ('django.db.models.fields.CharField', [], {'default': "'root'", 'max_length': '64', 'db_index': 'True', 'blank': 'True'}),
+            'message': ('django.db.models.fields.TextField', [], {}),
+            'server_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'db_index': 'True'}),
+            'traceback': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        },
+        'djangodblog.errorbatch': {
+            'Meta': {'unique_together': "(('logger', 'server_name', 'checksum'),)", 'object_name': 'ErrorBatch'},
+            'checksum': ('django.db.models.fields.CharField', [], {'max_length': '32', 'db_index': 'True'}),
+            'class_name': ('django.db.models.fields.CharField', [], {'db_index': 'True', 'max_length': '128', 'null': 'True', 'blank': 'True'}),
+            'first_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'last_seen': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now', 'db_index': 'True'}),
+            'level': ('django.db.models.fields.PositiveIntegerField', [], {'default': '40', 'db_index': 'True', 'blank': 'True'}),
+            'logger': ('django.db.models.fields.CharField', [], {'default': "'root'", 'max_length': '64', 'db_index': 'True', 'blank': 'True'}),
+            'message': ('django.db.models.fields.TextField', [], {}),
+            'server_name': ('django.db.models.fields.CharField', [], {'max_length': '128', 'db_index': 'True'}),
+            'status': ('django.db.models.fields.PositiveIntegerField', [], {'default': '0', 'db_column': "'is_resolved'"}),
+            'times_seen': ('django.db.models.fields.PositiveIntegerField', [], {'default': '1'}),
+            'traceback': ('django.db.models.fields.TextField', [], {'null': 'True', 'blank': 'True'}),
+            'url': ('django.db.models.fields.URLField', [], {'max_length': '200', 'null': 'True', 'blank': 'True'})
+        }
+    }
+    
+    complete_apps = ['djangodblog']
