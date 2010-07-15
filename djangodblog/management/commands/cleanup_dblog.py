25fa3dc857d0517d8868699e29d3281a7bd46c98
David Cramer <dcramer@gmail.com>
2010-07-15 11:49:20 -0700

    Add --logger option to cleanup-dblog

--- djangodblog/management/commands/cleanup_dblog.py
+++ djangodblog/management/commands/cleanup_dblog.py
@@ -8,7 +8,8 @@ import datetime
 
 class Command(BaseCommand):
     option_list = BaseCommand.option_list + (
-        make_option('--days', action='store',  dest='cutoff'),
+        make_option('--days', action='store', dest='days'),
+        make_option('--logger', action='store', dest='logger')
     )
     
     help = 'Cleans up old entries in the log.'
@@ -16,5 +17,9 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         ts = datetime.datetime.now() - datetime.timedelta(days=options.get('days', 30))
         
-        ErrorBatch.objects.filter(last_seen__lte=ts).delete()
-        Error.objects.filter(datetime__lte=ts).delete()
+        base_kwargs = {}
+        if options.get('logger'):
+            base_kwargs['logger'] = options['logger']
+        
+        ErrorBatch.objects.filter(last_seen__lte=ts, **base_kwargs).delete()
+        Error.objects.filter(datetime__lte=ts, **base_kwargs).delete()
