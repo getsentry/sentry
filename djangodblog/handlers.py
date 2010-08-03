f14bc66672e8c9fc48806198ab8c00b353a70e10
David Cramer <dcramer@gmail.com>
2010-08-03 14:45:04 -0700

    Prevent import issues with pulling handler into settings files

--- djangodblog/handlers.py
+++ djangodblog/handlers.py
@@ -1,7 +1,7 @@
-from djangodblog.models import Error
-
 import logging
 
 class DBLogHandler(logging.Handler):
     def emit(self, record):
+        from djangodblog.models import Error
+
         Error.objects.create_from_record(record)
