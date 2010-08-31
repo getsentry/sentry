e915082cdc0c1560b7f9c8c9abe10d3a07197a02
David Cramer <dcramer@gmail.com>
2010-08-30 21:44:11 -0700

    Fix for no messages

--- dblog/templatetags/dblog_helpers.py
+++ dblog/templatetags/dblog_helpers.py
@@ -2,24 +2,25 @@ from django import template
 register = template.Library()
 
 def with_priority(result_list, key='score'):
-    if isinstance(result_list[0], dict):
-        _get = lambda x, k: x[k]
-    else:
-        _get = lambda x, k: getattr(x, k)
-
-    min_, max_ = min([_get(r, key) for r in result_list]), max([_get(r, key) for r in result_list])
-    mid = (max_ - min_) / 4
-    for result in result_list:
-        val = _get(result, key)
-        if val > max_ - mid:
-            priority = 'veryhigh'
-        elif val > max_ - mid * 2:
-            priority = 'high'
-        elif val > max_ - mid * 3:
-            priority = 'medium'
-        elif val > max_ - mid * 4:
-            priority = 'low'
+    if result_list:
+        if isinstance(result_list[0], dict):
+            _get = lambda x, k: x[k]
         else:
-            priority = 'verylow'
-        yield result, priority
+            _get = lambda x, k: getattr(x, k)
+
+        min_, max_ = min([_get(r, key) for r in result_list]), max([_get(r, key) for r in result_list])
+        mid = (max_ - min_) / 4
+        for result in result_list:
+            val = _get(result, key)
+            if val > max_ - mid:
+                priority = 'veryhigh'
+            elif val > max_ - mid * 2:
+                priority = 'high'
+            elif val > max_ - mid * 3:
+                priority = 'medium'
+            elif val > max_ - mid * 4:
+                priority = 'low'
+            else:
+                priority = 'verylow'
+            yield result, priority
 with_priority = register.filter(with_priority)
