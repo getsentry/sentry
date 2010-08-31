74bb13606a203c708735179ec49202b22be4daca
David Cramer <dcramer@gmail.com>
2010-08-30 22:31:45 -0700

    Update readme and example urls

--- example_project/urls.py
+++ example_project/urls.py
@@ -1,19 +1,8 @@
 from django.conf.urls.defaults import *
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Example:
-    # (r'^example_project/', include('example_project.foo.urls')),
-
-    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
-    # to INSTALLED_APPS to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # (r'^admin/', include(admin.site.urls)),
-    
+    (r'^admin/', include(admin.site.urls)),
     (r'^', include('dblog.urls')),
 )
