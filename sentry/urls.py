1942cb5f17d55bc4711460c5fb1427fc03f5f6d4
DZ <david@nodnod.net>
2010-08-31 14:42:20 -0700

    add static serving for media

--- sentry/urls.py
+++ sentry/urls.py
@@ -1,3 +1,5 @@
+import os
+
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.utils.hashcompat import md5_constructor
@@ -7,9 +9,13 @@ import views
 
 hashed_secret = md5_constructor(settings.SECRET_KEY).hexdigest()
 
+SENTRY_ROOT = os.path.dirname(__file__) 
+
 urlpatterns = patterns('',
     url(r'^feeds/%s/messages.xml$' % hashed_secret, MessageFeed(), name='sentry-feed-messages'),
     url(r'^feeds/%s/summaries.xml$' % hashed_secret, SummaryFeed(), name='sentry-feed-summaries'),
     url(r'^group/(\d+)$', views.group, name='sentry-group'),
     url(r'^$', views.index, name='sentry'),
+    (r'^_media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': os.path.join(SENTRY_ROOT, 'media')}),
 )
