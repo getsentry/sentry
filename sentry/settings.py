bcdbd692dc491c4616ddd427648c6788d22eb13f
David Cramer <dcramer@gmail.com>
2010-08-31 14:20:36 -0700

    Update sentry settings to use SENTRY prefix

--- sentry/settings.py
+++ sentry/settings.py
@@ -1,12 +1,12 @@
 from django.conf import settings
 
-CATCH_404_ERRORS = getattr(settings, 'DBLOG_CATCH_404_ERRORS', False)
+CATCH_404_ERRORS = getattr(settings, 'SENTRY_CATCH_404_ERRORS', False)
 
-ENHANCED_TRACEBACKS = getattr(settings, 'DBLOG_ENHANCED_TRACEBACKS', True)
+ENHANCED_TRACEBACKS = getattr(settings, 'SENTRY_ENHANCED_TRACEBACKS', True)
 
-DATABASE_USING = getattr(settings, 'DBLOG_DATABASE_USING', None)
+DATABASE_USING = getattr(settings, 'SENTRY_DATABASE_USING', None)
 
-USE_LOGGING = getattr(settings, 'DBLOG_USE_LOGGING', False)
+USE_LOGGING = getattr(settings, 'SENTRY_USE_LOGGING', False)
 
-THRASHING_TIMEOUT = getattr(settings, 'DBLOG_THRASHING_TIMEOUT', 60)
-THRASHING_LIMIT = getattr(settings, 'DBLOG_THRASHING_LIMIT', 10)
+THRASHING_TIMEOUT = getattr(settings, 'SENTRY_THRASHING_TIMEOUT', 60)
+THRASHING_LIMIT = getattr(settings, 'SENTRY_THRASHING_LIMIT', 10)
