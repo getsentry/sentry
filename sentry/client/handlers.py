700fa0144c5276d8e31c01a243340f6cbac07e8f
David Cramer <dcramer@gmail.com>
2010-09-23 12:02:46 -0700

    Add a safety net for recursive logging

--- sentry/client/handlers.py
+++ sentry/client/handlers.py
@@ -1,7 +1,13 @@
 import logging
+import sys
 
 class SentryHandler(logging.Handler):
     def emit(self, record):
         from sentry.client.models import get_client
+
+        # Avoid typical config issues by overriding loggers behavior
+        if record.name == 'sentry.errors':
+            print >> sys.stderr, record.message
+            return
 
         get_client().create_from_record(record)
