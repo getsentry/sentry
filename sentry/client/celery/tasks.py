9b27a5b7473b7d2715767570e4a704a053f6a06c
David Cramer <dcramer@gmail.com>
2010-09-24 15:38:38 -0700

    Don't access GroupedMessage directly, instead fall back to default client so Celery task can use multi-db or REMOTE_URL

--- sentry/client/celery/tasks.py
+++ sentry/client/celery/tasks.py
@@ -1,7 +1,7 @@
 from celery.decorators import task
+from sentry.client.base import SentryClient
 from sentry.client.celery import settings
 
 @task(routing_key=settings.CELERY_ROUTING_KEY)
 def send(data):
-    from sentry.models import GroupedMessage
-    return GroupedMessage.objects.from_kwargs(**data)
+    return SentryClient().send(**data)
