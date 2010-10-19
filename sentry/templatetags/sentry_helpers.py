9b0e9874ecd984dc6e9ccc80fc05ebeca2ea0907
David Cramer <dcramer@gmail.com>
2010-10-19 10:22:39 -0700

    Tweak behavior for SQLite in chart_data

--- sentry/templatetags/sentry_helpers.py
+++ sentry/templatetags/sentry_helpers.py
@@ -62,7 +62,7 @@ def chart_data(group, max_days=90):
                       .order_by('grouper'))
 
     min_date = chart_qs[0][0]
-    if min_date < datetime.datetime.now() - datetime.timedelta(days=1):
+    if min_date and min_date < datetime.datetime.now() - datetime.timedelta(days=1):
         stop_hours = (datetime.datetime.now() - min_date).days * 24
         start_hours = (datetime.datetime.now() - chart_qs[-1][0]).days * 24
     else:
