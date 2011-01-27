87faf833c79451ef67801500995a45c927fd1ba9
David Cramer <dcramer@gmail.com>
2011-01-26 21:23:25 -0800

    Disable sphinxtogithub for ReadTheDOcs

--- docs/conf.py
+++ docs/conf.py
@@ -25,7 +25,8 @@ import sys, os
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinxtogithub']
+#extensions = ['sphinxtogithub']
+extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
