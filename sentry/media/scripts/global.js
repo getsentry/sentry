aa9867886b019f3e237fde4705b29d437a1712ec
Trey Hunner <treyhunner@gmail.com>
2011-02-22 11:55:47 -0800

    Added jQuery AJAX hook to ensure CSRF token is sent with all AJAX POST requests

--- sentry/media/scripts/global.js
+++ sentry/media/scripts/global.js
@@ -169,4 +169,28 @@ $(document).ready(function(){
             });
        }
     });
+
+    // Ensure that the CSRF token is sent with AJAX POSTs sent by jQuery
+    // Taken from the documentation: http://docs.djangoproject.com/en/dev/ref/contrib/csrf/
+    $('html').ajaxSend(function(event, xhr, settings) {
+        function getCookie(name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != '') {
+                var cookies = document.cookie.split(';');
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = jQuery.trim(cookies[i]);
+                    // Does this cookie string begin with the name we want?
+                    if (cookie.substring(0, name.length + 1) == (name + '=')) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
+                }
+            }
+            return cookieValue;
+        }
+        if (!(/^http:.*/.test(settings.url) || /^https:.*/.test(settings.url))) {
+            // Only send the token to relative URLs i.e. locally.
+            xhr.setRequestHeader("X-CSRFToken", getCookie('csrftoken'));
+        }
+    });
 });
