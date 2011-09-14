300c98a1079d6ade9dcaf750bb8d38c8bdd643ad
David Cramer <dcramer@gmail.com>
2011-09-14 15:21:09 -0700

    Gracefully handle cProfile error with loader

--- sentry/utils/stacks.py
+++ sentry/utils/stacks.py
@@ -21,7 +21,24 @@ def get_lines_from_file(filename, lineno, context_lines, loader=None, module_nam
     """
     source = None
     if loader is not None and hasattr(loader, "get_source"):
-        source = loader.get_source(module_name)
+        try:
+            source = loader.get_source(module_name)
+        except ImportError:
+            # Traceback (most recent call last):
+            #   File "/Users/dcramer/Development/django-sentry/sentry/client/handlers.py", line 31, in emit
+            #     get_client().create_from_record(record, request=request)
+            #   File "/Users/dcramer/Development/django-sentry/sentry/client/base.py", line 325, in create_from_record
+            #     data['__sentry__']['frames'] = varmap(shorten, get_stack_info(stack))
+            #   File "/Users/dcramer/Development/django-sentry/sentry/utils/stacks.py", line 112, in get_stack_info
+            #     pre_context_lineno, pre_context, context_line, post_context = get_lines_from_file(filename, lineno, 7, loader, module_name)
+            #   File "/Users/dcramer/Development/django-sentry/sentry/utils/stacks.py", line 24, in get_lines_from_file
+            #     source = loader.get_source(module_name)
+            #   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/pkgutil.py", line 287, in get_source
+            #     fullname = self._fix_name(fullname)
+            #   File "/System/Library/Frameworks/Python.framework/Versions/2.7/lib/python2.7/pkgutil.py", line 262, in _fix_name
+            #     "module %s" % (self.fullname, fullname))
+            # ImportError: Loader for module cProfile cannot handle module __main__
+            source = None
         if source is not None:
             source = source.splitlines()
     if source is None:
