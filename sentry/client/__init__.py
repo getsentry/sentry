139d9140d05a4a8a672b27299a7d4fd10eb2412b
David Cramer <dcramer@gmail.com>
2011-10-11 15:02:18 -0700

    Add deprecation into sentry.client init

--- sentry/client/__init__.py
+++ sentry/client/__init__.py
@@ -5,3 +5,5 @@ sentry.client
 :copyright: (c) 2010 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
+
+warnings.warn('sentry.client will be removed in version 1.14.0. You should switch to raven.client.django', DeprecationWarning)
