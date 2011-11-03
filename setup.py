fa2289fc2200850e1eda09cce1262b368013df26
David Cramer <dcramer@gmail.com>
2011-11-03 12:48:48 -0700

    Updated the message details page to match group details

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ if sys.version_info[:2] < (2, 5):
 
 setup(
     name='django-sentry',
-    version='1.13.3',
+    version='1.13.4',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/django-sentry',
