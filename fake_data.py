e7b0016c9ac6ba5659435eb128380486e67ee5da
David Cramer <dcramer@gmail.com>
2011-12-26 20:34:22 -0800

    More data for data faker

--- fake_data.py
+++ fake_data.py
@@ -11,19 +11,20 @@ def funcs():
     exceptions = itertools.cycle([SyntaxError, ValueError, TypeError, NameError, IndexError])
     messages = itertools.cycle(['hello world', 'beep boop beep', 'herp derp', 'shiny test data'])
     queries = itertools.cycle(['SELECT * FROM table', 'SELECT COUNT(1) FROM foo', 'INSERT INTO foo (id) VALUES (1)', 'DELETE FROM foo'])
+    loggers = itertools.cycle(['root', 'foo', 'foo.bar'])
 
     def query(client):
         duration = random.randint(0, 10000) / 1000.0
-        return client.capture('Query', query=queries.next(), engine=engine.next(), time_spent=duration)
+        return client.capture('Query', query=queries.next(), engine=engine.next(), time_spent=duration, site='sql', logger=loggers.next())
 
     def exception(client):
         try:
             raise exceptions.next()(messages.next())
         except:
-            return client.capture('Exception')
+            return client.capture('Exception', site='web', logger=loggers.next())
 
     def message(client):
-        return client.capture('Message', message=messages.next())
+        return client.capture('Message', message=messages.next(), site='web', logger=loggers.next())
 
     return [query, exception, message]
 
