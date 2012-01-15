07f67e1c62f3e55e7fb8237ac6ee97ab887e7e3a
David Cramer <dcramer@gmail.com>
2012-01-15 14:16:26 -0800

    Missing helpers import

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ install_requires = [
 
 setup(
     name='sentry',
-    version='2.0.0-RC2',
+    version='2.0.0-RC3',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry',
