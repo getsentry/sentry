cdadde923d5f10e3e569433e5dfc1dc0f76d29a4
David Cramer <dcramer@gmail.com>
2012-01-31 14:59:49 -0800

    Correctly handle delete_ghosts

--- sentry/commands/upgrade.py
+++ sentry/commands/upgrade.py
@@ -14,11 +14,11 @@ from sentry.commands.utils import options, opt
     opt('--interactive', default=False, action='store_true'),
     opt('--delete-ghost-migrations', default=True, dest='delete_ghosts'),
 )
-def upgrade(interactive=True):
+def upgrade(interactive=True, delete_ghosts=True):
     call_command('syncdb', database='default', interactive=interactive)
 
     if 'south' in settings.INSTALLED_APPS:
-        call_command('migrate', database='default', interactive=interactive)
+        call_command('migrate', database='default', interactive=interactive, delete_ghosts=delete_ghosts)
 
     register_views()
 
