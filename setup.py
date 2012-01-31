aba61baebe8b15b123707ec90c06563ccbadb48d
David Cramer <dcramer@gmail.com>
2012-01-31 01:37:38 -0800

    More cleanup on various error pages and search

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ install_requires = [
 
 setup(
     name='sentry',
-    version='2.2.0',
+    version='2.2.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry',
