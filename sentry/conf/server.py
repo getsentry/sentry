60259bf7c974db219159960c7754c59fda1e0afb
bkonkle <brandon@konkle.us>
2012-02-06 09:09:33 -0700

    Add the Access-Control-Allow-Origin header

--- sentry/conf/server.py
+++ sentry/conf/server.py
@@ -83,6 +83,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     # 'django.contrib.messages.middleware.MessageMiddleware',
+    'sentry.middleware.AccessControlMiddleware',
 )
 
 ROOT_URLCONF = 'sentry.conf.urls'
--- sentry/middleware.py
+++ sentry/middleware.py
@@ -1,3 +1,20 @@
+from sentry.conf import settings
+
+
 class SentryMiddleware(object):
     # DEPRECATED
     pass
+
+
+class AccessControlMiddleware(object):
+    """
+    This middleware provides the Access-Control-Allow-Origin header to enable
+    cross-site HTTP requests. You can find more information about this header
+    here: https://developer.mozilla.org/En/HTTP_access_control#Simple_requests
+    """
+
+    def process_response(self, request, response):
+        origin = getattr(settings, 'ALLOW_ORIGIN', None)
+        if origin:
+            response['Access-Control-Allow-Origin'] = origin
+        return response
