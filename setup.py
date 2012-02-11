1c2ddef017845f6147a773c1def8e12b62c60462
David Cramer <dcramer@gmail.com>
2012-02-10 19:47:33 -0800

    Version bump to 2.8.1

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ install_requires = [
 
 setup(
     name='sentry',
-    version='2.8.0',
+    version='2.8.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry',
