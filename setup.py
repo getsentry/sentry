f334d6edde18c8148666761204f6ca378ababb68
David Cramer <dcramer@gmail.com>
2012-02-23 02:55:18 -0800

    Version bump to 3.1.2

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ install_requires = [
 
 setup(
     name='sentry',
-    version='3.1.1',
+    version='3.1.2',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry',
