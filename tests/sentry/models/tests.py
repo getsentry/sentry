99ab74c71d978419fd4096a961f9ecfa1d820621
David Cramer <dcramer@gmail.com>
2012-03-21 21:16:18 -0700

    Correct invite token tests

--- tests/sentry/models/tests.py
+++ tests/sentry/models/tests.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from __future__ import absolute_import
 
 
@@ -55,12 +57,17 @@ class PendingProjectMemberTest(TestCase):
     fixtures = ['tests/fixtures/views.json']
 
     def test_token_generation(self):
-        member = PendingProjectMember(project_id=1, email='foo@example.com')
+        member = PendingProjectMember(id=1, project_id=1, email='foo@example.com')
         with self.Settings(SENTRY_KEY='a'):
-            self.assertEquals(member.token, 'a')
+            self.assertEquals(member.token, 'f3f2aa3e57f4b936dfd4f42c38db003e')
+
+    def test_token_generation_unicode_key(self):
+        member = PendingProjectMember(id=1, project_id=1, email='foo@example.com')
+        with self.Settings(SENTRY_KE="\xfc]C\x8a\xd2\x93\x04\x00\x81\xeak\x94\x02H\x1d\xcc&P'q\x12\xa2\xc0\xf2v\x7f\xbb*lX"):
+            self.assertEquals(member.token, '92ad5905c20f683096759a4c4987e823')
 
     def test_send_invite_email(self):
-        member = PendingProjectMember(project_id=1, email='foo@example.com')
+        member = PendingProjectMember(id=1, project_id=1, email='foo@example.com')
         with self.Settings(SENTRY_URL_PREFIX='http://example.com'):
             member.send_invite_email()
 
