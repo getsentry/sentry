1561e3ec978c0a901ae9b01450d6b0b58dd87546
David Cramer <dcramer@gmail.com>
2012-03-25 20:50:36 -0700

    3.7.0

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ install_requires = [
 
 setup(
     name='sentry',
-    version='3.6.4',
+    version='3.7.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry',
