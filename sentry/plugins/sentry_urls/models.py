cb915bf599969332c8cb76b9ebe1c50ed0be6115
David Cramer <dcramer@gmail.com>
2012-04-06 16:12:19 -0700

    Plugins are expected to provide a models.py, which is where things should get initialized

---
+++ sentry/plugins/sentry_urls/models.py
@@ -0,0 +1,51 @@
+"""
+sentry.plugins.sentry_urls.models
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2012 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+import sentry
+
+from django.db.models import Sum
+from django.utils.translation import ugettext_lazy as _
+
+from sentry.plugins import Plugin, register
+
+
+class UrlsPlugin(Plugin):
+    """
+    Adds additional support for showing information about urls including:
+
+    * A panel which shows all urls a message was seen on.
+    * A sidebar module which shows the urls most actively seen on.
+    """
+    slug = 'urls'
+    title = _('URLs')
+    version = sentry.VERSION
+    author = "Sentry Team"
+    author_url = "https://github.com/dcramer/sentry"
+
+    def get_unique_urls(self, group):
+        return group.messagefiltervalue_set.filter(key='url')\
+                    .values_list('value')\
+                    .annotate(times_seen=Sum('times_seen'))\
+                    .values_list('value', 'times_seen', 'first_seen', 'last_seen')\
+                    .order_by('-times_seen')
+
+    def panels(self, request, group, panel_list, **kwargs):
+        panel_list.append((self.get_title(), self.get_url(group)))
+        return panel_list
+
+    def view(self, request, group, **kwargs):
+        return self.render('sentry/plugins/sentry_urls/index.html', {
+            'group': group,
+            'unique_urls': self.get_unique_urls(group),
+        })
+
+    def widget(self, request, group, **kwargs):
+        return self.render('sentry/plugins/sentry_urls/widget.html', {
+            'group': group,
+            'unique_urls': list(self.get_unique_urls(group)[:10]),
+        })
+register(UrlsPlugin)
