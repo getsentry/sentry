baaa113759214b7035948396a541a6d4d2261e32
David Cramer <dcramer@gmail.com>
2012-04-16 15:45:07 -0700

    Correctly jsonify variables everywhere

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ install_requires = [
 
 setup(
     name='sentry',
-    version='4.0.8',
+    version='4.0.9',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry',
