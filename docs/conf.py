b29580306ea42583b0834848e6d59b3bd83c3ffa
David Cramer <dcramer@gmail.com>
2012-05-12 10:40:16 -0500

    Ensure django is configured for doc imports

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,9 @@
 
 import sys, os
 
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'sentry.conf.server'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
