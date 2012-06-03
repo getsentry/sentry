3bfe70e5bd36089392bea9f06706d426f9ba7e81
David Cramer <dcramer@gmail.com>
2012-06-03 15:46:24 -0700

    Correct default link to be 24 hours

--- setup.py
+++ setup.py
@@ -72,7 +72,7 @@ dependency_links = [
 
 setup(
     name='sentry',
-    version='4.5.4',
+    version='4.5.4.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry',
