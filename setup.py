b6659a55d2bf54e214096079c60d7eb9815d3d6a
David Cramer <dcramer@gmail.com>
2012-06-17 12:42:11 -0700

    4.7.5

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ dependency_links = [
 
 setup(
     name='sentry',
-    version='4.7.4',
+    version='4.7.5',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://github.com/dcramer/sentry',
