fcd9c364c88d9d41d6e373aa9054e7621a900b7a
David Cramer <dcramer@gmail.com>
2012-07-12 18:54:21 +0200

    Add Team to admin

--- sentry/admin.py
+++ sentry/admin.py
@@ -1,9 +1,15 @@
 from django.contrib import admin
-from sentry.models import Project
+from sentry.models import Project, Team
 
 
 class ProjectAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner', 'public', 'date_added', 'status')
+    list_display = ('name', 'owner', 'slug', 'public', 'date_added', 'status')
     list_filter = ('public', 'status')
 
 admin.site.register(Project, ProjectAdmin)
+
+
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'slug')
+
+admin.site.register(Team, TeamAdmin)
