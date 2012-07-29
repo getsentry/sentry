8c11b2db7f09844aa860bfe7f1c3ff23c0d30f94
David Cramer <dcramer@gmail.com>
2012-07-29 15:00:35 -0700

    Remove bad delete_unique call as it was already applied in migration 0015

--- sentry/migrations/0062_correct_del_index_sentry_groupedmessage_logger__view__checksum.py
+++ sentry/migrations/0062_correct_del_index_sentry_groupedmessage_logger__view__checksum.py
@@ -8,18 +8,9 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
-        # Removing unique constraint on 'GroupedMessage', fields ['logger', 'view', 'checksum']
-        # FIXES 0015
-        try:
-            db.delete_unique('sentry_groupedmessage', ['logger', 'view', 'checksum'])
-        except:
-            # TODO: we should probaby remove this since it seems to only affect previous installs that
-            # hadnt applied migrations, and MySQL doesnt like try/excepts (aka South)
-            pass
+        pass
 
     def backwards(self, orm):
-        # Adding unique constraint on 'GroupedMessage', fields ['checksum', 'logger', 'view']
-        #FIXES 0015
-        db.create_unique('sentry_groupedmessage', ['checksum', 'logger', 'view'])
+        pass
 
     complete_apps = ['sentry']
