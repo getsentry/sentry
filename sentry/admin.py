26c85b6c34d71d8372e7d5b5b3ae81d136620bb5
David Cramer <dcramer@gmail.com>
2012-08-14 13:54:17 -0700

    Change owner to be a raw_id_field

--- sentry/admin.py
+++ sentry/admin.py
@@ -6,6 +6,7 @@ class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'slug', 'public', 'date_added', 'status')
     list_filter = ('public', 'status')
     search_fields = ('name', 'owner__username', 'owner__email', 'slug')
+    raw_id_fields = ('owner',)
 
 admin.site.register(Project, ProjectAdmin)
 
@@ -13,5 +14,6 @@ admin.site.register(Project, ProjectAdmin)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'slug')
     search_fields = ('name', 'owner__username', 'owner__email', 'slug')
+    raw_id_fields = ('owner',)
 
 admin.site.register(Team, TeamAdmin)
