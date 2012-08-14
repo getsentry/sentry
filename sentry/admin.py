0ea9098252b228637cae43921556470c4b466b27
David Cramer <dcramer@gmail.com>
2012-08-14 13:52:51 -0700

    Add searching to Project and Team admins

--- sentry/admin.py
+++ sentry/admin.py
@@ -5,11 +5,13 @@ from sentry.models import Project, Team
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'slug', 'public', 'date_added', 'status')
     list_filter = ('public', 'status')
+    search_fields = ('name', 'owner__username', 'owner__email', 'slug')
 
 admin.site.register(Project, ProjectAdmin)
 
 
 class TeamAdmin(admin.ModelAdmin):
     list_display = ('name', 'owner', 'slug')
+    search_fields = ('name', 'owner__username', 'owner__email', 'slug')
 
 admin.site.register(Team, TeamAdmin)
