27f9300068bebc394b8922d3d6ad2224a2e2e712
David Cramer <dcramer@gmail.com>
2012-09-12 21:43:22 -0700

    Scrap terrible idea of a custom gunicorn worker and use awesome new Logan magic to fix gevent support (fixes GH-400)

--- src/sentry/services/http.py
+++ src/sentry/services/http.py
@@ -6,54 +6,8 @@ sentry.services.http
 :license: BSD, see LICENSE for more details.
 """
 
-import sys
-import gunicorn
-
-from gunicorn import arbiter
-from gunicorn.app import djangoapp
-import gunicorn.util
-
+from django.core.management import call_command
 from sentry.services.base import Service
-
-
-def _setup_app(app):
-    djangoapp.make_default_env(app.cfg)
-    djwsgi = gunicorn.util.import_module("gunicorn.app.django_wsgi")
-    djwsgi.make_wsgi_application()
-
-
-class SentryApplication(djangoapp.DjangoApplication):
-
-    def __init__(self, options):
-        self.usage = None
-        self.cfg = None
-        self.config_file = options.get("config") or ""
-        self.options = options
-        self.callable = None
-        self.project_path = None
-
-        self.do_load_config()
-
-        for k, v in self.options.items():
-            if k.lower() in self.cfg.settings and v is not None:
-                self.cfg.set(k.lower(), v)
-
-    def init(self, parser, opts, args):
-        pass
-
-    def do_load_config(self):
-        original_args = sys.argv
-        sys.argv = sys.argv[0:1]
-        try:
-            return super(SentryApplication, self).do_load_config()
-        finally:
-            sys.argv = original_args
-
-    def load(self):
-        # application should be imported at first to setup env
-        from sentry.wsgi import application
-        _setup_app(self)
-        return application
 
 
 class SentryHTTPServer(Service):
@@ -86,12 +40,7 @@ class SentryHTTPServer(Service):
         if workers:
             options['workers'] = workers
 
-        self.app = SentryApplication(options)
+        self.options = options
 
     def run(self):
-        try:
-            arbiter.Arbiter(self.app).run()
-        except RuntimeError, e:
-            sys.stderr.write("\nError: %s\n\n" % e)
-            sys.stderr.flush()
-            sys.exit(1)
+        call_command('run_gunicorn', **self.options)
