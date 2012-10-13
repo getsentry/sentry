2abb315ace9b3d9c86975f400a4e68ec0423125d
David Cramer <dcramer@gmail.com>
2012-10-13 11:35:14 -0700

    5.0.15

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ install_requires = [
 
 setup(
     name='sentry',
-    version='5.0.14',
+    version='5.0.15',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://www.getsentry.com',
