6cee54da300d9be3c5b3babe68180c7b9765be8b
David Cramer <dcramer@gmail.com>
2012-10-30 22:16:48 -0700

    Add @fixture helper

---
+++ src/sentry/testutils.py
@@ -0,0 +1,9 @@
+"""
+sentry.testutils
+~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2012 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from sentry.utils import cached_property as fixture
