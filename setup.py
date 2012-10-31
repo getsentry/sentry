a87093bb0de6cb3657d6ee8cdcd1f4b059b0dc30
David Cramer <dcramer@gmail.com>
2012-10-30 23:08:01 -0700

    Bump Django min version to 1.4.2

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ install_requires = [
     'django-celery>=2.5.5',
     'celery>=2.5.3',
     'django-crispy-forms>=1.1.4',
-    'Django>=1.4.1,<=1.5',
+    'Django>=1.4.2,<=1.5',
     'django-indexer>=0.3.0',
     'django-paging>=0.2.4',
     'django-picklefield>=0.2.0',
