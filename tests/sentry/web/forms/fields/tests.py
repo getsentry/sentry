b3d97f93994ae2f1d829f1b3739e20f03f002029
David Cramer <dcramer@gmail.com>
2012-12-01 13:47:56 -0800

    Add origins validation logic to EditProjectForm (fixes GH-665)

---
+++ tests/sentry/web/forms/fields/tests.py
@@ -0,0 +1,39 @@
+from django import forms
+from sentry.web.forms.fields import OriginsField
+from sentry.testutils import TestCase, fixture
+
+
+class OriginsFieldTest(TestCase):
+    @fixture
+    def field(self):
+        return OriginsField()
+
+    def test_supports_wildcards(self):
+        value = '*'
+        result = self.field.clean(value)
+        self.assertEquals(result, ['*'])
+
+    def test_supports_wildcard_domains(self):
+        value = '*.example.com'
+        result = self.field.clean(value)
+        self.assertEquals(result, ['*.example.com'])
+
+    def test_supports_base_domains(self):
+        value = 'example.com'
+        result = self.field.clean(value)
+        self.assertEquals(result, ['example.com'])
+
+    def test_supports_full_base_uri(self):
+        value = 'http://example.com:80'
+        result = self.field.clean(value)
+        self.assertEquals(result, ['http://example.com:80'])
+
+    def test_doesnt_support_domain_with_port(self):
+        value = 'example.com:80'
+        with self.assertRaises(forms.ValidationError):
+            self.field.clean(value)
+
+    def test_doesnt_support_wildcard_domain_with_port(self):
+        value = '*.example.com:80'
+        with self.assertRaises(forms.ValidationError):
+            self.field.clean(value)
