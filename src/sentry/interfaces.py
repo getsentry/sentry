86a5f96428e7b4ffc564a489a3eb1793436fa12e
David Cramer <dcramer@gmail.com>
2012-12-05 01:45:38 -0800

    Simplified grouping logic for messages

--- src/sentry/interfaces.py
+++ src/sentry/interfaces.py
@@ -417,10 +417,7 @@ class Exception(Interface):
         }
 
     def get_hash(self):
-        output = filter(bool, [self.module, self.type])
-        if not output:
-            output = [self.value]
-        return output
+        return filter(bool, [self.type, self.value])
 
     def to_html(self, event):
         last_frame = None
