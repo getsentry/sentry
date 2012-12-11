724d14b7d0754d50bea0666e0c536f60f3599b80
David Cramer <dcramer@gmail.com>
2012-12-10 18:34:25 -0800

    Add slugify util

--- src/sentry/static/sentry/scripts/utils.js
+++ src/sentry/static/sentry/scripts/utils.js
@@ -102,6 +102,24 @@
                 }
             }
             return time;
+        },
+
+        slugify: function(str) {
+            str = str.replace(/^\s+|\s+$/g, ''); // trim
+            str = str.toLowerCase();
+          
+            // remove accents, swap ñ for n, etc
+            var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
+            var to   = "aaaaeeeeiiiioooouuuunc------";
+            for (var i=0, l=from.length ; i<l ; i++) {
+                str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+            }
+
+            str = str.replace(/[^a-z0-9\s\-]/g, '') // remove invalid chars
+                .replace(/\s+/g, '-') // collapse whitespace and replace by -
+                .replace(/-+/g, '-'); // collapse dashes
+
+            return str;
         }
 
     };
