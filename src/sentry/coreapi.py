d751ccd828fdaded1868574ea22c758d68d71aa2
David Cramer <dcramer@gmail.com>
2012-12-15 13:56:07 -0800

    Correct project_from_api_key_and_id behavior when there is no key user (fixes GH-622)

--- src/sentry/coreapi.py
+++ src/sentry/coreapi.py
@@ -149,9 +149,9 @@ def project_from_api_key_and_id(api_key, project_id):
 
         tm.project = project
 
-    result = plugins.first('has_perm', tm.user, 'create_event', project)
-    if result is False:
-        raise APIForbidden('Creation of this event was blocked')
+        result = plugins.first('has_perm', tm.user, 'create_event', project)
+        if result is False:
+            raise APIForbidden('Creation of this event was blocked')
 
     return project
 
