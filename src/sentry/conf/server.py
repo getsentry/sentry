aa8d4022f38b8f86482210071308741b9cdaff97
David Cramer <dcramer@gmail.com>
2012-12-19 22:45:35 -0800

    Improve default logging config

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -287,14 +287,18 @@ LOGGING = {
     'loggers': {
         '()': {
             'handlers': ['console', 'sentry'],
-            'propagate': True,
         },
         'root': {
             'handlers': ['console', 'sentry'],
         },
-        'sentry.errors': {
+        'sentry': {
             'level': 'ERROR',
             'handlers': ['console', 'sentry'],
+            'propagate': False,
+        },
+        'sentry.errors': {
+            'level': 'ERROR',
+            'handlers': ['console'],
             'propagate': False,
         },
         'django.request': {
