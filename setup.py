04ea0b89320499a7cccffa79db01ec4e22d6899b
David Cramer <dcramer@gmail.com>
2012-12-27 14:04:15 -0800

    Build css :( (5.1.1.1)

--- setup.py
+++ setup.py
@@ -73,7 +73,7 @@ install_requires = [
 
 setup(
     name='sentry',
-    version='5.1.1',
+    version='5.1.1.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://www.getsentry.com',
