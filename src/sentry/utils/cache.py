09b4b4141c1aa6ca293f39e6fe245d7c5cf37fb1
David Cramer <dcramer@gmail.com>
2013-01-14 17:21:39 -0800

    Missing header

--- src/sentry/utils/cache.py
+++ src/sentry/utils/cache.py
@@ -1,3 +1,10 @@
+"""
+sentry.utils.cache
+~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2012 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
 import logging
 import time
 
