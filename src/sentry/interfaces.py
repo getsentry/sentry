34798d5a984f575ae1b7fec60fac9f04a45ef9c2
David Cramer <dcramer@gmail.com>
2013-01-15 15:02:38 -0800

    Move Http information below Template stacktrace

--- src/sentry/interfaces.py
+++ src/sentry/interfaces.py
@@ -513,7 +513,7 @@ class Http(Interface):
     >>>  }
     """
 
-    display_score = 10000
+    display_score = 1000
     score = 800
 
     # methods as defined by http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html + PATCH
