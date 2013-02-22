9e5ffb8ef5ad553afdfe6493c2234a88e81a90bb
David Cramer <dcramer@gmail.com>
2013-02-22 05:27:33 -0800

    Move Query and Message interface tests

--- tests/sentry/interfaces/tests.py
+++ tests/sentry/interfaces/tests.py
@@ -5,7 +5,7 @@ from __future__ import absolute_import
 import mock
 import pickle
 
-from sentry.interfaces import Interface, Message, Query, get_context
+from sentry.interfaces import Interface, get_context
 from sentry.models import Event
 from sentry.testutils import TestCase, fixture
 
@@ -47,69 +47,6 @@ class InterfaceTest(InterfaceBase):
 
     def test_validate_default(self):
         self.interface.validate()
-
-
-class MessageTest(InterfaceBase):
-    @fixture
-    def interface(self):
-        return Message(message='Hello there %s!', params=('world',))
-
-    def test_serialize_behavior(self):
-        assert self.interface.serialize() == {
-            'message': self.interface.message,
-            'params': self.interface.params,
-        }
-
-    def test_get_hash_uses_message(self):
-        assert self.interface.get_hash() == [self.interface.message]
-
-    def test_get_search_context_with_params_as_list(self):
-        interface = self.interface
-        interface.params = ['world']
-        assert interface.get_search_context(self.event) == {
-            'text': [interface.message] + list(interface.params)
-        }
-
-    def test_get_search_context_with_params_as_tuple(self):
-        assert self.interface.get_search_context(self.event) == {
-            'text': [self.interface.message] + list(self.interface.params)
-        }
-
-    def test_get_search_context_with_params_as_dict(self):
-        interface = self.interface
-        interface.params = {'who': 'world'}
-        interface.message = 'Hello there %(who)s!'
-        assert self.interface.get_search_context(self.event) == {
-            'text': [interface.message] + interface.params.values()
-        }
-
-    def test_get_search_context_with_unsupported_params(self):
-        interface = self.interface
-        interface.params = object()
-        interface.message = 'Hello there %(who)s!'
-        assert self.interface.get_search_context(self.event) == {
-            'text': [interface.message],
-        }
-
-
-class QueryTest(InterfaceBase):
-    @fixture
-    def interface(self):
-        return Query(query='SELECT 1', engine='psycopg2')
-
-    def test_serialize_behavior(self):
-        assert self.interface.serialize() == {
-            'query': self.interface.query,
-            'engine': self.interface.engine,
-        }
-
-    def test_get_hash_uses_query(self):
-        assert self.interface.get_hash() == [self.interface.query]
-
-    def test_get_search_context(self):
-        assert self.interface.get_search_context(self.event) == {
-            'text': [self.interface.query],
-        }
 
 
 class GetContextTest(TestCase):
