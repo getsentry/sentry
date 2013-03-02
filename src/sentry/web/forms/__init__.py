642e0994f7dbbc1d3d7ee73b33c3c4d891d5c470
Damian Zaremba <damian@damianzaremba.co.uk>
2013-03-02 18:49:42 +0000

    Allow promoting a user to admin via the manage users page.
    Sometimes (such as if you're using ldap for authentication),
    the primary admin of the install won't be that of the initial
    superuser.
    
    Using the initial superuser, you should be able to promote any
    other user to be an admin - via the is_staff flag.
    
    This patch allows this promotion, by extending the ChangeUserForm,
    currently used to administratively modify users.

--- src/sentry/web/forms/__init__.py
+++ src/sentry/web/forms/__init__.py
@@ -44,8 +44,11 @@ class NewUserForm(BaseUserForm):
 
 
 class ChangeUserForm(BaseUserForm):
+    is_staff = forms.BooleanField(required=False, label=_('Admin'),
+        help_text=_("Designates whether this user can perform administrative functions."))
+
     class Meta:
-        fields = ('first_name', 'username', 'email', 'is_active')
+        fields = ('first_name', 'username', 'email', 'is_active', 'is_staff')
         model = User
 
 
