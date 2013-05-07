a10036d567521731be12068c6ec2568ba013c6c2
David Cramer <dcramer@gmail.com>
2013-05-07 08:15:31 -0700

    Correct path

--- src/sentry/admin.py
+++ src/sentry/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.models.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin
 from sentry.models import Project, Team, User
 
 
