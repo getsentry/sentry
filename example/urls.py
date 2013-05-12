260a785bed5f99a4e599182b0135bed878af792f
David Cramer <dcramer@gmail.com>
2013-05-12 14:37:43 -0700

    Remove unmaintained example app (fixes GH-884)

--- example/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import patterns, include, url
-
-
-urlpatterns = patterns('',
-    (r'^sentry/', include('sentry.web.urls')),
-    (r'^captureMessage/', 'views.captureMessage'),
-    (r'^captureException/', 'views.captureException'),
-    (r'^loggingError/', 'views.loggingError'),
-)
