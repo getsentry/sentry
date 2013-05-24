e8bcc47dee4aaeeb6674388696ad443038743383
David Cramer <dcramer@gmail.com>
2013-05-24 02:37:12 -0700

    Break everything but define proper rollups

--- src/sentry/tsdb/utils.py
+++ src/sentry/tsdb/utils.py
@@ -9,16 +9,24 @@ sentry.tsdb.utils
 from datetime import timedelta
 
 
-class Rollup(object):
-    SECONDS = 0
-    MINUTES = 1
-    HOURS = 2
-    DAYS = 3
-    WEEKS = 4
-    MONTHS = 5
-    YEARS = 6
-    ALL_TIME = 7
+ONE_MINUTE = 60
+ONE_HOUR = ONE_MINUTE * 60
+ONE_DAY = ONE_HOUR * 24
+ONE_MONTH = ONE_DAY * 30
 
+
+ROLLUPS = (
+    # time in seconds, samples to keep
+    (10, 24),  # 4 minutes at 10 seconds
+    (ONE_MINUTE, 30),  # 30 minutes at 1 minute
+    (ONE_MINUTE * 10, 20),  # 2 hours at 10 minutes
+    (ONE_HOUR, 24),  # 1 day at 1 hour
+    (ONE_DAY, 30),  # 1 month at 1 day
+    (ONE_MONTH, 24),  # 2 years at 1 month
+)
+
+
+class Rollup(object):
     @classmethod
     def get_choices(cls):
         if not hasattr(cls, '__choice_cache'):
