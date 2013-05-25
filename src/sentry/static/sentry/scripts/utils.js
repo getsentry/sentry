0ae73f4c5aee147d6e6dc098af5c390ccb563f1c
David Cramer <dcramer@gmail.com>
2013-05-25 16:26:10 +0800

    i have no idea what im doing

--- src/sentry/static/sentry/scripts/utils.js
+++ src/sentry/static/sentry/scripts/utils.js
@@ -274,7 +274,7 @@
 
     $(function(){
         // Change all select boxes to select2 elements.
-        $('.body select, .toolbar select, .project-controls select').each(function(){
+        $('.body select').each(function(){
             var $this = $(this),
                 options = {
                     width: 'element',
