c33be200f3a5cae9e390e7381161b718a53cf6c4
David Cramer <dcramer@gmail.com>
2013-05-27 12:37:16 +0800

    Various tweaks and fixes for rollups

--- src/sentry/tsdb/utils.py
+++ src/sentry/tsdb/utils.py
@@ -10,17 +10,14 @@ sentry.tsdb.utils
 ONE_MINUTE = 60
 ONE_HOUR = ONE_MINUTE * 60
 ONE_DAY = ONE_HOUR * 24
-ONE_MONTH = ONE_DAY * 30
 
 
 ROLLUPS = (
     # time in seconds, samples to keep
-    (10, 24),  # 4 minutes at 10 seconds
-    (ONE_MINUTE, 30),  # 30 minutes at 1 minute
-    (ONE_MINUTE * 10, 20),  # 2 hours at 10 minutes
-    (ONE_HOUR, 24),  # 1 day at 1 hour
-    (ONE_DAY, 30),  # 1 month at 1 day
-    (ONE_MONTH, 24),  # 2 years at 1 month
+    (10, 30),  # 5 minutes at 10 seconds
+    (ONE_MINUTE, 120),  # 2 hours at 1 minute
+    (ONE_HOUR, 48),  # 2 days at 1 hour
+    (ONE_DAY, 365),  # 1 year at 1 day
 )
 
 
@@ -34,4 +31,6 @@ class Rollup(object):
         i.e. if the rollup is minutes, the resulting timestamp would have
         the seconds and microseconds rounded down.
         """
-        return int(int(timestamp.strftime('%s')) / seconds)
+        epoch = int(timestamp.strftime('%s'))
+        print epoch, epoch % seconds
+        return epoch - (epoch % seconds)
--- tests/sentry/tsdb/utils/tests.py
+++ tests/sentry/tsdb/utils/tests.py
@@ -1,6 +1,6 @@
 import pytz
 
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from sentry.testutils import TestCase
 from sentry.tsdb.utils import Rollup
@@ -12,4 +12,10 @@ timestamp = datetime(2013, 5, 18, 15, 13, 58, 132928, tzinfo=pytz.UTC)
 class NormalizeToEpochTest(TestCase):
     def test_simple(self):
         result = normalize_to_epoch(timestamp, 60)
-        assert result == 22814833
+        assert result == 1368889980
+        result = normalize_to_epoch(timestamp + timedelta(seconds=20), 60)
+        assert result == 1368890040
+        result = normalize_to_epoch(timestamp + timedelta(seconds=30), 60)
+        assert result == 1368890040
+        result = normalize_to_epoch(timestamp + timedelta(seconds=70), 60)
+        assert result == 1368890100
