a56b3f6478d357b02b1b70ee30774c2f9d98cc19
David Cramer <dcramer@gmail.com>
2013-07-04 13:48:54 +0200

    Some bare bones rest framework code

---
+++ src/sentry/web/restapi/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, url, include
+from rest_framework import routers
+
+router = routers.DefaultRouter()
+# router.register(r'users', views.UserViewSet)
+# router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browseable API.
+urlpatterns = patterns('',
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+)
