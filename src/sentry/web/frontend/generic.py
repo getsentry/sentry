7a124030553a04ef693c7917f3d75fd9343d2bd0
David Cramer <dcramer@gmail.com>
2013-07-15 20:09:32 -0700

    Remove unused static code

--- src/sentry/web/frontend/generic.py
+++ src/sentry/web/frontend/generic.py
@@ -6,9 +6,7 @@ sentry.web.frontend.generic
 :license: BSD, see LICENSE for more details.
 """
 from django.http import HttpResponseRedirect
-from django.contrib.staticfiles import finders
 from django.core.urlresolvers import reverse
-from django.utils.datastructures import SortedDict
 from django.utils.translation import ugettext as _
 
 from sentry.models import Team
@@ -17,14 +15,6 @@ from sentry.plugins import plugins
 from sentry.plugins.base import Response
 from sentry.web.decorators import login_required
 from sentry.web.helpers import render_to_response
-
-
-def find_static_files(ignore_patterns=()):
-    found_files = SortedDict()
-    for finder in finders.get_finders():
-        for path, storage in finder.list(ignore_patterns):
-            found_files[path] = storage.path(path)
-    return found_files
 
 
 @login_required
