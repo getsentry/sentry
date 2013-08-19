1b857290acfffcbc103ccdd4ab7b028c8f67ebe3
David Cramer <dcramer@gmail.com>
2013-08-18 22:25:01 -0700

    Shortcut tags added via TagPlugin that are beyond the max value length

--- src/sentry/plugins/bases/tag.py
+++ src/sentry/plugins/bases/tag.py
@@ -5,6 +5,7 @@ sentry.plugins.bases.tag
 :copyright: (c) 2010-2013 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
+from sentry.constants import MAX_TAG_VALUE_LENGTH
 from sentry.models import Group
 from sentry.plugins import Plugin
 
@@ -24,7 +25,11 @@ class TagPlugin(Plugin):
         raise NotImplementedError
 
     def get_tags(self, event, **kwargs):
-        return [(self.tag, v) for v in self.get_tag_values(event)]
+        return [
+            (self.tag, v)
+            for v in self.get_tag_values(event)
+            if len(v) <= MAX_TAG_VALUE_LENGTH
+        ]
 
     def post_process(self, group, event, is_new, is_sample, **kwargs):
         # legacy compatibility for older plugins
