b20622248d822178fc0ea2d6bf7d609db12d5ee1
David Cramer <dcramer@gmail.com>
2013-08-26 16:47:10 -0700

    6.1.2

--- setup.py
+++ setup.py
@@ -116,7 +116,7 @@ class PyTest(TestCommand):
 
 setup(
     name='sentry',
-    version='6.1.1',
+    version='6.1.2',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='http://www.getsentry.com',
