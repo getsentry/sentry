63ca54e320b77501a304aa0520e133e1194e1324
David Cramer <dcramer@gmail.com>
2013-10-14 18:09:22 +0100

    Break delete_project into smaller tasks

--- src/sentry/tasks/deletion.py
+++ src/sentry/tasks/deletion.py
@@ -11,13 +11,31 @@ from celery.task import task
 
 @task(name='sentry.tasks.deletion.delete_project', queue='cleanup')
 def delete_project(object_id, **kwargs):
-    from sentry.models import Project
+    from sentry.models import (
+        Project, TagKey, TagValue, GroupTagKey, GroupTag, GroupCountByMinute,
+        ProjectCountByMinute, Activity, EventMapping, Event, Group
+    )
 
     try:
         p = Project.objects.get(id=object_id)
     except Project.DoesNotExist:
         return
 
+    logger = delete_project.get_logger()
+
+    # This handles cascades properly
+    # TODO: this doesn't clean up the index
+    for model in (
+            TagKey, TagValue, GroupTagKey, GroupTag, GroupCountByMinute,
+            ProjectCountByMinute, Activity, EventMapping, Event, Group):
+        logger.info('Removing %r objects where project=%s', model, p.id)
+        has_results = False
+        for obj in model.objects.filter(project=p)[:1000]:
+            obj.delete()
+            has_results = True
+
+        if has_results:
+            delete_project.delay(object_id=object_id)
     p.delete()
 
 
