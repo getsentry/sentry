915036430edc3a1ac772e2df320c25d226d90bc2
David Cramer <dcramer@gmail.com>
2013-10-14 14:44:25 +0100

    Move resolve endpoint into group details PUT method

--- src/sentry/web/restapi/urls.py
+++ src/sentry/web/restapi/urls.py
@@ -1,19 +1,15 @@
 from django.conf.urls import patterns, url
 
-from .endpoints.event_index import EventListView
-from .endpoints.event_details import EventDetailsView
-from .endpoints.event_resolve import ResolveEventView
+from .endpoints.group_index import GroupListView
+from .endpoints.group_details import GroupDetailsView
 
 urlpatterns = patterns(
     '',
-    url(r'^(?P<team_slug>[^\/]+)/(?P<project_id>[^\/]+)/events/$',
-        EventListView.as_view(),
+    url(r'^(?P<team_slug>[^\/]+)/(?P<project_id>[^\/]+)/groups/$',
+        GroupListView.as_view(),
         name='sentry-api-1-event-list'),
-    url(r'^(?P<team_slug>[^\/]+)/(?P<project_id>[^\/]+)/events/(?P<group_id>\d+)/$',
-        EventDetailsView.as_view(),
+    url(r'^(?P<team_slug>[^\/]+)/(?P<project_id>[^\/]+)/groups/(?P<group_id>\d+)/$',
+        GroupDetailsView.as_view(),
         name='sentry-api-1-event-details'),
-    url(r'^(?P<team_slug>[^\/]+)/(?P<project_id>[^\/]+)/events/(?P<group_id>\d+)/resolve/$',
-        ResolveEventView.as_view(),
-        name='sentry-api-1-resolve-event'),
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 )
