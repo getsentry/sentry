11f112277366cdaf97b6d1506af6af67f046ac10
David Cramer <dcramer@gmail.com>
2013-10-26 17:03:10 -0700

    Changelog for 6.4.0 and 6.3.0

--- setup.py
+++ setup.py
@@ -119,7 +119,7 @@ class PyTest(TestCommand):
 
 setup(
     name='sentry',
-    version='6.4.0-DEV',
+    version='6.4.0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='https://www.getsentry.com',
