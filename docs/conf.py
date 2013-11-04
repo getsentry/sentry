924fff48b05733d9ece476a8222876918779c753
David Cramer <dcramer@gmail.com>
2013-11-03 19:00:58 -0800

    Use standard copyright message

--- docs/conf.py
+++ docs/conf.py
@@ -55,7 +55,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Sentry'
-copyright = u'2011, David Cramer'
+copyright = u'2010-2013, the Sentry Team'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
