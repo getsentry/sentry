1e646fc5e327a38be99617021fa91459043a0be8
David Cramer <dcramer@gmail.com>
2013-12-02 20:14:49 -0800

    Add contenttypes to initial migration

--- src/sentry/migrations/0001_initial.py
+++ src/sentry/migrations/0001_initial.py
@@ -171,6 +171,13 @@ class Migration(SchemaMigration):
             'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
             'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '128'})
         },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
     }
 
-    complete_apps = ['sentry']
+    complete_apps = ['sentry', 'auth', 'contenttypes']
