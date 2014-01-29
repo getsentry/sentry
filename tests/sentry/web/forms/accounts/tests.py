a8ef5053b39e66793c2dce778716544134dacf75
David Cramer <dcramer@gmail.com>
2014-01-28 23:44:19 -0800

    Allow make the user aware of usernames when they matter

--- tests/sentry/web/forms/accounts/tests.py
+++ tests/sentry/web/forms/accounts/tests.py
@@ -14,3 +14,13 @@ class AccountSettingsFormTest(TestCase):
         user.set_password('foo')
         form = AccountSettingsForm(user=user)
         self.assertIn('old_password', form.fields)
+
+    def test_removes_username_if_matches_email(self):
+        user = User(username='foo@example.com', email='foo@example.com')
+        form = AccountSettingsForm(user=user)
+        assert 'username' not in form.fields
+
+    def test_requires_username_if_not_matches_email(self):
+        user = User(username='bar@example.com', email='foo@example.com')
+        form = AccountSettingsForm(user=user)
+        assert 'username' in form.fields
