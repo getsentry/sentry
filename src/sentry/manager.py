46156b6d717b59a47fa2c2aa6711b17f3876ece0
David Cramer <dcramer@gmail.com>
2014-02-04 12:09:59 -0500

    Correct log level behavior to properly handle int values

--- src/sentry/manager.py
+++ src/sentry/manager.py
@@ -212,11 +212,9 @@ class GroupManager(BaseManager, ChartMixin):
     def normalize_event_data(self, data):
         # TODO(dcramer): store http.env.REMOTE_ADDR as user.ip
         # First we pull out our top-level (non-data attr) kwargs
-        if (
-            not data.get('level') or
-            not isinstance(data['level'], basestring)
-            or data['level'] not in LOG_LEVELS
-        ):
+        if not isinstance(data.get('level'), (basestring, int)):
+            data['level'] = logging.ERROR
+        elif data['level'] not in LOG_LEVELS:
             data['level'] = logging.ERROR
 
         if not data.get('logger'):
