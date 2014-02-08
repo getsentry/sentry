c6af30a8583d46a5b14a994127d536d5c8683f5e
Matt Robenolt <matt@ydekproductions.com>
2014-02-07 20:06:51 -0800

    raven.js 1.1.10

--- tests/sentry/web/helpers/tests.py
+++ tests/sentry/web/helpers/tests.py
@@ -80,4 +80,4 @@ class GetRavenJsUrl(TestCase):
             self.assertEquals(get_raven_js_url(), url)
 
     def test_with_default_raven_js_url(self):
-        self.assertEquals(get_raven_js_url(), 'cdn.ravenjs.com/1.1.9/jquery,native/raven.min.js')
+        self.assertEquals(get_raven_js_url(), 'cdn.ravenjs.com/1.1.10/jquery,native/raven.min.js')
