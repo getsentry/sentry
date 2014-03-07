3ba4a9286682d05cf48ac7d5fe23b38e52737cf0
David Cramer <dcramer@gmail.com>
2014-03-07 11:57:45 -0800

    docstring for import_submodules

--- src/sentry/utils/imports.py
+++ src/sentry/utils/imports.py
@@ -38,6 +38,11 @@ def import_string(path):
 
 
 def import_submodules(context, root_module, path):
+    """
+    Import all submodules and register them in the ``context`` namespace.
+
+    >>> import_submodules(globals(), __name__, __path__)
+    """
     for loader, module_name, is_pkg in pkgutil.walk_packages(path):
         module = loader.find_module(module_name).load_module(module_name)
         for k, v in vars(module).iteritems():
