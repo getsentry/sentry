585d30af1b1233bd7dce1c2899a3cae975c61c08
Matt Robenolt <matt@ydekproductions.com>
2014-03-18 10:22:02 -0700

    All users are probably not logged in

--- src/sentry/middleware/sudo.py
+++ src/sentry/middleware/sudo.py
@@ -13,7 +13,8 @@ from sentry.constants import EMPTY_PASSWORD_VALUES
 class SudoMiddleware(BaseSudoMiddleware):
     def has_sudo_privileges(self, request):
         # Users without a password are assumed to always have sudo powers
-        if request.user.password in EMPTY_PASSWORD_VALUES:
+        user = request.user
+        if user.is_authenticated() and user.password in EMPTY_PASSWORD_VALUES:
             return True
 
         return super(SudoMiddleware, self).has_sudo_privileges(request)
