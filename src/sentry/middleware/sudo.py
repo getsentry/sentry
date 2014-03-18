e1e7b82079e12c059035378ca99a9cbaa68f096d
Matt Robenolt <matt@ydekproductions.com>
2014-03-18 02:22:02 -0700

    Neaten

--- src/sentry/middleware/sudo.py
+++ src/sentry/middleware/sudo.py
@@ -14,16 +14,18 @@ class SudoMiddleware(object):
         request.is_sudo = lambda: has_sudo_privileges(request)
 
     def process_response(self, request, response):
-        if request._sentry_sudo is None:
+        is_sudo = request._sentry_sudo
+
+        if is_sudo is None:
             return response
 
         # We have explicitly had sudo revoked, so clean up cookie
-        if request._sentry_sudo is False and SUDO_COOKIE_NAME in request.COOKIES:
+        if is_sudo is False and SUDO_COOKIE_NAME in request.COOKIES:
             response.delete_cookie(SUDO_COOKIE_NAME)
             return response
 
         # Sudo mode has been granted, and we have a token to send back to the user agent
-        if request._sentry_sudo is True and hasattr(request, '_sentry_sudo_token'):
+        if is_sudo is True and hasattr(request, '_sentry_sudo_token'):
             token = request._sentry_sudo_token
             max_age = request._sentry_sudo_max_age
             response.set_cookie(
