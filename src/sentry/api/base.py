076114027404c3e7d675677898db3a811d901a86
David Cramer <dcramer@gmail.com>
2014-05-02 12:27:51 -0300

    Abstract stats endpoint into base class

--- src/sentry/api/base.py
+++ src/sentry/api/base.py
@@ -1,14 +1,22 @@
+from datetime import datetime, timedelta
+from pytz import utc
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.parsers import JSONParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from urllib2 import quote
 
+from sentry.tsdb.base import ROLLUPS
+
 from .authentication import KeyAuthentication
 from .paginator import Paginator
 
 
 LINK_HEADER = '<{uri}&cursor={cursor}>; rel="{name}"'
+
+ONE_MINUTE = 60
+ONE_HOUR = ONE_MINUTE * 60
+ONE_DAY = ONE_HOUR * 24
 
 
 class Endpoint(APIView):
@@ -60,3 +68,42 @@ class Endpoint(APIView):
             headers['Link'] = ', '.join(link_values)
 
         return Response(results, headers=headers)
+
+
+class BaseStatsEndpoint(Endpoint):
+    def _parse_args(self, request):
+        resolution = request.GET.get('resolution')
+        if resolution:
+            resolution = self._parse_resolution(resolution)
+
+            assert any(r for r in ROLLUPS if r[0] == resolution)
+
+        end = request.GET.get('until')
+        if end:
+            end = datetime.fromtimestamp(float(end)).replace(tzinfo=utc)
+        else:
+            end = datetime.utcnow().replace(tzinfo=utc)
+
+        start = request.GET.get('since')
+        if start:
+            start = datetime.fromtimestamp(float(start)).replace(tzinfo=utc)
+        else:
+            start = end - timedelta(days=1, seconds=-1)
+
+        return {
+            'start': start,
+            'end': end,
+            'rollup': resolution,
+        }
+
+    def _parse_resolution(self, value):
+        if value.endswith('h'):
+            return int(value[:-1]) * ONE_HOUR
+        elif value.endswith('d'):
+            return int(value[:-1]) * ONE_DAY
+        elif value.endswith('m'):
+            return int(value[:-1]) * ONE_MINUTE
+        elif value.endswith('s'):
+            return int(value[:-1])
+        else:
+            raise ValueError(value)
