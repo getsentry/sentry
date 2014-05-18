f9fe902bfe14317f205ca903952c02784ffc96d7
David Cramer <dcramer@gmail.com>
2014-05-18 12:40:39 -0700

    Lint

--- tests/sentry/plugins/useragents/tests.py
+++ tests/sentry/plugins/useragents/tests.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-import mock
 from ua_parser.user_agent_parser import Parse
 
-from sentry.plugins.bases.issue import IssuePlugin
 from sentry.testutils import TestCase
 from sentry.plugins.sentry_useragents.models import (
-    BrowserPlugin, DevicePlugin, OsPlugin )
+    BrowserPlugin, DevicePlugin, OsPlugin
+)
+
 
 class UserAgentPlugins(TestCase):
     data = [
@@ -18,11 +18,9 @@ class UserAgentPlugins(TestCase):
         },
     ]
 
-
     def test_plugins(self):
         for row in self.data:
             ua = Parse(row['user_agent'])
             assert BrowserPlugin().get_tag_from_ua(ua) == row['browser_plugin_output']
             assert DevicePlugin().get_tag_from_ua(ua) == row['device_plugin_output']
             assert OsPlugin().get_tag_from_ua(ua) == row['os_plugin_output']
-
