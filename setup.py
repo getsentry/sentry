9ec98fc29f971764866f8611600eeeed515bd406
Alex Gaynor <alex.gaynor@gmail.com>
2014-05-17 19:07:07 -0700

    Fix remaining py3k syntax errors

--- setup.py
+++ setup.py
@@ -91,6 +91,7 @@ install_requires = [
     'raven>=4.0.2',
     'redis>=2.7.0,<2.9.0',
     'simplejson>=3.1.0,<3.4.0',
+    'six',
     'setproctitle>=1.1.7,<1.2.0',
     'South==0.8.2',
     'toronado>=0.0.4,<0.1.0',
