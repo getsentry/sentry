118d26bd3142a8c4873956771471ca09379cb351
Alex Gaynor <alex.gaynor@gmail.com>
2014-05-17 18:59:44 -0700

    Too many lines

--- src/sentry/management/commands/start.py
+++ src/sentry/management/commands/start.py
@@ -14,7 +14,6 @@ from django.core.management import call_command
 from django.core.management.base import BaseCommand, CommandError
 
 
-
 class Command(BaseCommand):
     args = '<service>'
     help = 'Starts the specified service'
