344a4817db9a92091072974055b686692df950e0
David Cramer <dcramer@gmail.com>
2014-05-21 10:38:10 -0700

    Basic tests for date/new sorts

--- tests/sentry/search/django/tests.py
+++ tests/sentry/search/django/tests.py
@@ -2,6 +2,7 @@
 
 from __future__ import absolute_import
 
+from datetime import datetime
 from exam import fixture
 
 from sentry.search.django.backend import DjangoSearchBackend
@@ -19,11 +20,15 @@ class DjangoSearchTest(TestCase):
             project=project,
             checksum='a' * 40,
             message='foo',
+            last_seen=datetime(2013, 8, 13, 3, 8, 24, 880386),
+            first_seen=datetime(2013, 7, 13, 3, 8, 24, 880386),
         )
         group2 = self.create_group(
             project=project,
             checksum='b' * 40,
             message='bar',
+            last_seen=datetime(2013, 7, 14, 3, 8, 24, 880386),
+            first_seen=datetime(2013, 7, 14, 3, 8, 24, 880386),
         )
 
         results = self.backend.query(project, query='foo')
@@ -33,3 +38,13 @@ class DjangoSearchTest(TestCase):
         results = self.backend.query(project, query='bar')
         assert len(results) == 1
         assert results[0] == group2
+
+        results = self.backend.query(project, sort_by='date')
+        assert len(results) == 2
+        assert results[0] == group1
+        assert results[1] == group2
+
+        results = self.backend.query(project, sort_by='new')
+        assert len(results) == 2
+        assert results[0] == group2
+        assert results[1] == group1
