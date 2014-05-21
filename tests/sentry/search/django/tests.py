a233a62365e7d6280926b07f68bb6c999a585f11
David Cramer <dcramer@gmail.com>
2014-05-21 10:39:40 -0700

    Basic test for search status filter

--- tests/sentry/search/django/tests.py
+++ tests/sentry/search/django/tests.py
@@ -5,6 +5,7 @@ from __future__ import absolute_import
 from datetime import datetime
 from exam import fixture
 
+from sentry.constants import STATUS_RESOLVED, STATUS_UNRESOLVED
 from sentry.search.django.backend import DjangoSearchBackend
 from sentry.testutils import TestCase
 
@@ -20,6 +21,7 @@ class DjangoSearchTest(TestCase):
             project=project,
             checksum='a' * 40,
             message='foo',
+            status=STATUS_UNRESOLVED,
             last_seen=datetime(2013, 8, 13, 3, 8, 24, 880386),
             first_seen=datetime(2013, 7, 13, 3, 8, 24, 880386),
         )
@@ -27,6 +29,7 @@ class DjangoSearchTest(TestCase):
             project=project,
             checksum='b' * 40,
             message='bar',
+            status=STATUS_RESOLVED,
             last_seen=datetime(2013, 7, 14, 3, 8, 24, 880386),
             first_seen=datetime(2013, 7, 14, 3, 8, 24, 880386),
         )
@@ -48,3 +51,11 @@ class DjangoSearchTest(TestCase):
         assert len(results) == 2
         assert results[0] == group2
         assert results[1] == group1
+
+        results = self.backend.query(project, status=STATUS_UNRESOLVED)
+        assert len(results) == 1
+        assert results[0] == group1
+
+        results = self.backend.query(project, status=STATUS_RESOLVED)
+        assert len(results) == 1
+        assert results[0] == group2
