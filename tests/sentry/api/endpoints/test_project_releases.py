0d231161f3466da5e554a3b23beaf65702c7b70b
David Cramer <dcramer@gmail.com>
2014-05-25 20:26:20 -0700

    Add project release list endpoint

---
+++ tests/sentry/api/endpoints/test_project_releases.py
@@ -0,0 +1,38 @@
+from datetime import datetime
+from django.core.urlresolvers import reverse
+
+from sentry.models import Release
+from sentry.testutils import APITestCase
+
+
+class ProjectReleasesTest(APITestCase):
+    def test_simple(self):
+        self.login_as(user=self.user)
+
+        project1 = self.create_project(owner=self.user, name='foo')
+        project2 = self.create_project(owner=self.user, name='bar')
+
+        release1 = Release.objects.create(
+            project=project1,
+            version='1',
+            date_added=datetime(2013, 8, 13, 3, 8, 24, 880386),
+        )
+        release2 = Release.objects.create(
+            project=project1,
+            version='2',
+            date_added=datetime(2013, 8, 14, 3, 8, 24, 880386),
+        )
+        Release.objects.create(
+            project=project2,
+            version='1',
+        )
+
+        url = reverse('sentry-api-0-project-releases', kwargs={
+            'project_id': project1.id,
+        })
+        response = self.client.get(url, format='json')
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 2
+        assert response.data[0]['id'] == str(release2.id)
+        assert response.data[1]['id'] == str(release1.id)
