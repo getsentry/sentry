4ae41c5e7b379db09d828690615e33f255c2c6b1
David Cramer <dcramer@gmail.com>
2014-05-25 20:26:20 -0700

    Add project release list endpoint

---
+++ src/sentry/api/serializers/models/release.py
@@ -0,0 +1,13 @@
+from sentry.api.serializers import Serializer, register
+from sentry.models import Release
+
+
+@register(Release)
+class ReleaseSerializer(Serializer):
+    def serialize(self, obj, user):
+        d = {
+            'id': str(obj.id),
+            'version': obj.version,
+            'dateCreated': obj.date_added,
+        }
+        return d
