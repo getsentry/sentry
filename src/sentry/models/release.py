c6063e8e0cb084f82ff3a02a23d470d9e5ad555f
David Cramer <dcramer@gmail.com>
2014-05-25 20:25:55 -0700

    Add Release model

---
+++ src/sentry/models/release.py
@@ -0,0 +1,29 @@
+"""
+sentry.models.release
+~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from django.db import models
+from django.utils import timezone
+
+from sentry.db.models import Model, sane_repr
+
+
+class Release(Model):
+    """
+    A release is generally created when a new version is pushed into a
+    production state.
+    """
+    project = models.ForeignKey('sentry.Project')
+    version = models.CharField(max_length=64)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        app_label = 'sentry'
+        db_table = 'sentry_release'
+        unique_together = (('project', 'version'),)
+
+    __repr__ = sane_repr('project_id', 'version')
