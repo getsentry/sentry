eecc11d31077f76d4d1adc2afca99abbb99a0980
David Cramer <dcramer@gmail.com>
2014-06-01 16:10:46 -0700

    Improve API key management
    - Add label to keys
    - Add edit page

--- src/sentry/web/frontend/projects/keys.py
+++ src/sentry/web/frontend/projects/keys.py
@@ -16,10 +16,11 @@ from django.utils.translation import ugettext_lazy as _
 from sentry.constants import MEMBER_OWNER
 from sentry.models import ProjectKey
 from sentry.permissions import (
-    can_remove_project_key, can_add_project_key
+    can_remove_project_key, can_add_project_key, can_edit_project_key
 )
 from sentry.plugins import plugins
 from sentry.web.decorators import has_access
+from sentry.web.forms.projectkeys import EditProjectKeyForm
 from sentry.web.helpers import render_to_response
 
 
@@ -62,6 +63,39 @@ def new_project_key(request, team, project):
     )
 
     return HttpResponseRedirect(reverse('sentry-manage-project-keys', args=[project.team.slug, project.slug]))
+
+
+@has_access(MEMBER_OWNER)
+@csrf_protect
+def edit_project_key(request, team, project, key_id):
+    if not can_edit_project_key(request.user, project):
+        return HttpResponseRedirect(reverse('sentry-manage-project-keys', args=[project.team.slug, project.slug]))
+
+    try:
+        key = ProjectKey.objects.get(
+            id=key_id,
+            project=project,
+        )
+    except ProjectKey.DoesNotExist():
+        return HttpResponseRedirect(reverse('sentry-manage-project-keys', args=[project.team.slug, project.slug]))
+
+    form = EditProjectKeyForm(request.POST or None, instance=key)
+    if form.is_valid():
+        form.save()
+
+        messages.add_message(
+            request, messages.SUCCESS, _('Changes to your API key were saved.'))
+        return HttpResponseRedirect(reverse('sentry-manage-project-keys', args=[project.team.slug, project.slug]))
+
+    context = {
+        'team': team,
+        'project': project,
+        'page': 'keys',
+        'key': key,
+        'form': form,
+    }
+
+    return render_to_response('sentry/projects/edit_key.html', context, request)
 
 
 @require_http_methods(['POST'])
