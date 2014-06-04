9f629ffe49f45e87224737f31cec27e04d442b1d
David Cramer <dcramer@gmail.com>
2014-06-04 16:26:17 -0700

    Initial cache abstraction

--- src/sentry/app.py
+++ src/sentry/app.py
@@ -24,6 +24,7 @@ def get_instance(path, options):
 
 
 buffer = get_instance(settings.SENTRY_BUFFER, settings.SENTRY_BUFFER_OPTIONS)
+cache = get_instance(settings.SENTRY_CACHE, settings.SENTRY_CACHE_OPTIONS)
 quotas = get_instance(settings.SENTRY_QUOTAS, settings.SENTRY_QUOTA_OPTIONS)
 nodestore = get_instance(
     settings.SENTRY_NODESTORE, settings.SENTRY_NODESTORE_OPTIONS)
