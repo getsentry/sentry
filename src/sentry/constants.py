f1906ad42a2125e372e08481c93ac246ca4e17b5
David Cramer <dcramer@gmail.com>
2014-06-18 06:48:07 +0800

    Add service-based notification actions

--- src/sentry/constants.py
+++ src/sentry/constants.py
@@ -182,6 +182,7 @@ TAG_LABELS = {
 # TODO(dcramer): once this is more flushed out we want this to be extendable
 SENTRY_RULES = (
     'sentry.rules.actions.notify_event.NotifyEventAction',
+    'sentry.rules.actions.notify_event_service.NotifyEventServiceAction',
     'sentry.rules.conditions.every_event.EveryEventCondition',
     'sentry.rules.conditions.first_seen_event.FirstSeenEventCondition',
     'sentry.rules.conditions.regression_event.RegressionEventCondition',
