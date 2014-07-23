1e092d15b4d6eccfbc99bd014c2fb78f560c838b
David Cramer <dcramer@gmail.com>
2014-07-23 15:03:39 +0200

    Expand API docs and move to /help/api/

---
+++ src/sentry/api/help_urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import patterns, url
+
+from .views.help_index import ApiHelpIndexView
+
+
+urlpatterns = patterns(
+    '',
+
+    url(r'^$', ApiHelpIndexView.as_view(),
+        name='sentry-api-0'),
+)
