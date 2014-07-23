2f442fb74b986ede05fda40b45ff6d3215e7e9f8
David Cramer <dcramer@gmail.com>
2014-07-23 13:45:48 +0200

    Mock help index
    cherry-pick from 35aec738bab99ce4031609aea5c29ddd40b9cef0

--- src/sentry/constants.py
+++ src/sentry/constants.py
@@ -147,7 +147,7 @@ MAX_CULPRIT_LENGTH = 200
 # which we don't want to worry about conflicts on.
 RESERVED_ORGANIZATION_SLUGS = (
     'admin', 'manage', 'login', 'account', 'register', 'api',
-    'organizations', 'teams', 'projects',
+    'organizations', 'teams', 'projects', 'help',
 )
 
 RESERVED_TEAM_SLUGS = RESERVED_ORGANIZATION_SLUGS
