6e9dc240aa59ba2bc5beb04dedd8f5101fd961df
David Cramer <dcramer@gmail.com>
2014-08-10 12:32:02 -0700

    Remove unused function (get_internal_project)
    Conflicts:
    	src/sentry/web/helpers.py
    
    cherry-pick 75e922cc80446e4bbc4f3253eec18096afb198c5

--- src/sentry/web/helpers.py
+++ src/sentry/web/helpers.py
@@ -17,7 +17,7 @@ from django.utils.safestring import mark_safe
 
 from sentry import options
 from sentry.constants import EVENTS_PER_PAGE, STATUS_HIDDEN
-from sentry.models import Project, Team, ProjectOption, ProjectKey
+from sentry.models import Project, Team, ProjectOption
 
 logger = logging.getLogger('sentry.errors')
 
@@ -64,22 +64,6 @@ def get_login_url(reset=False):
         if _LOGIN_URL is None:
             _LOGIN_URL = reverse('sentry-login')
     return _LOGIN_URL
-
-
-def get_internal_project():
-    try:
-        project = Project.objects.get(id=settings.SENTRY_PROJECT)
-    except Project.DoesNotExist:
-        return {}
-    try:
-        projectkey = ProjectKey.objects.filter(project=project).order_by('-user')[0]
-    except IndexError:
-        return {}
-
-    return {
-        'id': project.id,
-        'dsn': projectkey.get_dsn(public=True)
-    }
 
 
 def get_default_context(request, existing_context=None, team=None):
