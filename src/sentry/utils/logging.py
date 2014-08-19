e2a23f0661d0aeab3c7e20cde4c03d3c6bad7ab6
Matt Robenolt <m@robenolt.com>
2014-08-18 22:03:33 -0700

    lol

--- src/sentry/utils/logging.py
+++ src/sentry/utils/logging.py
@@ -1,8 +1,8 @@
 """
 sentry.utils.logging
-~~~~~~~~~~~~~~~~~
+~~~~~~~~~~~~~~~~~~~~
 
-:copyright: (c) 2010-2013 by the Sentry Team, see AUTHORS for more details.
+:copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
 from __future__ import absolute_import, print_function
