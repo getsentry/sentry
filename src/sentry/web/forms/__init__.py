getsentry/sentry

commit c5f224ae1bbba2ce97fde8f481a11989e04ed215

Implement managed users feature
A managed user is authenticated externally to Sentry, for example
via basic auth by the web server. Such users should not be allowed
to change their username or password from within Sentry.

A new configuration setting, SENTRY_MANAGED_USER_FIELDS, can be
used to further prevent a managed user from changing their name
or email address.

src/sentry/web/forms/__init__.py
@@ -10,7 +10,7 @@ from django.utils.translation import ugettext_lazy as _
 
 from sentry.constants import HTTP_METHODS
 from sentry.models import User, Activity
-from sentry.web.forms.fields import RadioFieldRenderer
+from sentry.web.forms.fields import RadioFieldRenderer, ReadOnlyTextField
 
 
 class ReplayForm(forms.Form):
@@ -54,6 +54,16 @@ class ChangeUserForm(BaseUserForm):
         fields = ('first_name', 'username', 'email', 'is_active', 'is_staff',
                   'is_superuser')
         model = User
+
+    def __init__(self, *args, **kwargs):
+        super(ChangeUserForm, self).__init__(*args, **kwargs)
+        self.user = kwargs['instance']
+        if self.user.is_managed:
+            self.fields['username'] = ReadOnlyTextField(label="Username (managed)")
+
+    def clean_username(self):
+        if self.user.is_managed:
+            return self.user.username
 
 
 class RemoveUserForm(forms.Form):

src/sentry/admin.py
@@ -61,7 +61,8 @@ class UserAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
+            'is_managed')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
@@ -74,7 +75,7 @@ class UserAdmin(admin.ModelAdmin):
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
     list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
-    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_managed')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
     inlines = (TeamMemberInline,)

src/sentry/conf/server.py
@@ -693,6 +693,11 @@ SENTRY_DISALLOWED_IPS = (
     '255.255.255.255/32',
 )
 
+# Fields which managed users cannot change via Sentry UI. Username and password
+# cannot be changed by managed users. Optionally include 'email' and
+# 'first_name' in SENTRY_MANAGED_USER_FIELDS.
+SENTRY_MANAGED_USER_FIELDS = ('email',)
+
 # Configure celery
 import djcelery
 djcelery.setup_loader()

src/sentry/models/user.py
@@ -34,6 +34,12 @@ class User(Model, AbstractBaseUser):
         _('superuser status'), default=False,
         help_text=_('Designates that this user has all permissions without '
                     'explicitly assigning them.'))
+    is_managed = models.BooleanField(
+        _('managed'), default=False,
+        help_text=_('Designates whether this user should be treated as '
+                    'managed. Select this to disallow the user from '
+                    'modifying their account (username, password, etc).'))
+
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     objects = UserManager(cache_fields=['pk'])

src/sentry/templates/sentry/account/settings.html
@@ -28,14 +28,18 @@
                 <a href="http://gravatar.com/">Change your avatar at Gravatar.com</a>
             </div>
         </fieldset>
+        {% if form.new_password %}
         <hr>
         <p>{% trans "You may also optionally change your password." %}</p>
         {% with form.new_password as field %}
             {{ field|as_crispy_field }}
         {% endwith %}
+        {% endif %}
 
+        {% if not form.is_readonly %}
         <fieldset class="form-actions">
             <button type="submit" class="btn btn-primary">{% trans "Save Changes" %}</button>
         </fieldset>
+        {% endif %}
     </form>
 {% endblock %}

src/sentry/web/forms/accounts.py
@@ -20,6 +20,7 @@ from six.moves import range
 from sentry.constants import LANGUAGES
 from sentry.models import UserOption, User
 from sentry.utils.auth import find_users
+from sentry.web.forms.fields import ReadOnlyTextField
 
 
 # at runtime we decide whether we should support recaptcha
@@ -244,12 +245,38 @@ class AccountSettingsForm(forms.Form):
         self.user = user
         super(AccountSettingsForm, self).__init__(*args, **kwargs)
 
+        if self.user.is_managed:
+            # username and password always managed, email and
+            # first_name optionally managed
+            for field in ('email', 'first_name', 'username'):
+                if field == 'username' or field in settings.SENTRY_MANAGED_USER_FIELDS:
+                    self.fields[field] = ReadOnlyTextField(label=self.fields[field].label)
+            # don't show password field at all
+            del self.fields['new_password']
+
         # don't show username field if its the same as their email address
         if self.user.email == self.user.username:
             del self.fields['username']
 
+    def is_readonly(self):
+        if self.user.is_managed:
+            return set(('email', 'first_name')) == set(settings.SENTRY_MANAGED_USER_FIELDS)
+        return False
+
+    def _clean_managed_field(self, field):
+        if self.user.is_managed and (field == 'username' or
+                field in settings.SENTRY_MANAGED_USER_FIELDS):
+            return getattr(self.user, field)
+        return self.cleaned_data[field]
+
+    def clean_email(self):
+        return self._clean_managed_field('email')
+
+    def clean_first_name(self):
+        return self._clean_managed_field('first_name')
+
     def clean_username(self):
-        value = self.cleaned_data['username']
+        value = self._clean_managed_field('username')
         if User.objects.filter(username__iexact=value).exclude(id=self.user.id).exists():
             raise forms.ValidationError(_("That username is already in use."))
         return value

src/sentry/web/forms/fields.py
@@ -1,14 +1,16 @@
 """
-sentry.web.forms.teams
-~~~~~~~~~~~~~~~~~~~~~~
+sentry.web.forms.fields
+~~~~~~~~~~~~~~~~~~~~~~~
 
 :copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
 from django.core.validators import URLValidator
-from django.forms.widgets import RadioFieldRenderer, TextInput, Textarea
-from django.forms import CharField, IntegerField, ValidationError
+from django.forms.widgets import RadioFieldRenderer, TextInput, Textarea, Widget
+from django.forms.util import flatatt
+from django.forms import Field, CharField, IntegerField, ValidationError
 from django.utils.encoding import force_unicode
+from django.utils.html import format_html
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
@@ -105,6 +107,27 @@ class OriginsField(CharField):
             return False
 
         return True
+
+
+class ReadOnlyTextWidget(Widget):
+    def render(self, name, value, attrs):
+        final_attrs = self.build_attrs(attrs)
+        if not value:
+            value = mark_safe("<em>%s</em>" % _("Not set"))
+        return format_html("<div{0}>{1}</div>", flatatt(final_attrs), value)
+
+
+class ReadOnlyTextField(Field):
+    widget = ReadOnlyTextWidget
+
+    def __init__(self, *args, **kwargs):
+        kwargs.setdefault("required", False)
+        super(ReadOnlyTextField, self).__init__(*args, **kwargs)
+
+    def bound_data(self, data, initial):
+        # Always return initial because the widget doesn't
+        # render an input field.
+        return initial
 
 
 def get_team_label(team):

commit cb246c5cab05cb5ca33f197f4905e0bced06e115

Fix issue #1269
Fix for regression introduced by c5f224ae1b (Implement managed users
feature, 2014-08-18): not allowing a (non-managed) user to be edited
from admin/users.

src/sentry/web/forms/__init__.py
@@ -64,6 +64,7 @@ class ChangeUserForm(BaseUserForm):
     def clean_username(self):
         if self.user.is_managed:
             return self.user.username
+        return self.cleaned_data['username']
 
 
 class RemoveUserForm(forms.Form):