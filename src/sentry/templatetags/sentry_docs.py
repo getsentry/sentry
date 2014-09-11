ab1b0e1305b4f8ab90fe8828896e5aa166730357
David Cramer <dcramer@gmail.com>
2014-09-11 02:42:50 -0700

    Move help page links into sidebar

--- src/sentry/templatetags/sentry_docs.py
+++ src/sentry/templatetags/sentry_docs.py
@@ -1,6 +1,9 @@
+from __future__ import absolute_import, print_function
+
 from django import template
 
 from sentry.api.base import DocSection
+from sentry.models import HelpPage
 
 register = template.Library()
 
@@ -11,6 +14,10 @@ def render_doc_sidebar(context):
         ({'id': v.name.lower(), 'name': v.value} for v in DocSection),
         key=lambda x: x['name'])
 
+    pages = list(HelpPage.objects.filter(is_visible=True))
+    pages.sort(key=lambda x: (-x.priority, x.title))
+
     return {
         'api_sections': api_sections,
+        'page_list': pages,
     }
