0e9885c30e99b5f5ec5900b96a5c6e78801c1c91
David Cramer <dcramer@gmail.com>
2014-09-11 02:14:34 -0700

    Correct is_managed reference

--- src/sentry/admin.py
+++ src/sentry/admin.py
@@ -62,8 +62,7 @@ class UserAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {'fields': ('username', 'password')}),
         (_('Personal info'), {'fields': ('first_name', 'last_name', 'email')}),
-        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser',
-            'is_managed')}),
+        (_('Permissions'), {'fields': ('is_active', 'is_staff', 'is_superuser')}),
         (_('Important dates'), {'fields': ('last_login', 'date_joined')}),
     )
     add_fieldsets = (
