2da06d16485d4c8f21e2a9418322ad9c2b440246
David Cramer <dcramer@gmail.com>
2014-09-11 19:03:26 -0700

    Cleanup to group usage
    - Add GroupStatus
    - Refactor delete/update endpoints
    
    cherry-pick 2cc85a5bceb03b70db0044f41cbeef23dd1d7fc4

---
+++ src/sentry/api/fields/user.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import, print_function
+
+from rest_framework import serializers
+
+from sentry.models import User
+from sentry.utils.auth import find_users
+
+
+class UserField(serializers.WritableField):
+    def to_native(self, obj):
+        return obj.username
+
+    def from_native(self, data):
+        if not data:
+            return None
+
+        if data.isdigit():
+            try:
+                return User.objects.get(id=data)
+            except User.DoesNotExist:
+                pass
+
+        try:
+            return find_users(data)[0]
+        except IndexError:
+            raise serializers.ValidationError('Unable to find user')
--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -10,7 +10,6 @@ from .endpoints.group_details import GroupDetailsEndpoint
 from .endpoints.group_resolve import GroupResolveEndpoint
 from .endpoints.group_bookmark import GroupBookmarkEndpoint
 from .endpoints.group_markseen import GroupMarkSeenEndpoint
-from .endpoints.group_delete import GroupDeleteEndpoint
 from .endpoints.group_events import GroupEventsEndpoint
 from .endpoints.group_events_latest import GroupEventsLatestEndpoint
 from .endpoints.group_notes import GroupNotesEndpoint
@@ -110,9 +109,6 @@ urlpatterns = patterns(
     url(r'^groups/(?P<group_id>\d+)/markseen/$',
         GroupMarkSeenEndpoint.as_view(),
         name='sentry-api-0-group-markseen'),
-    url(r'^groups/(?P<group_id>\d+)/delete/$',
-        GroupDeleteEndpoint.as_view(),
-        name='sentry-api-0-group-delete'),
     url(r'^groups/(?P<group_id>\d+)/events/$',
         GroupEventsEndpoint.as_view(),
         name='sentry-api-0-group-events'),
