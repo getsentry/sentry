723726fba36f634f4ec8665659dcdcbe20ccaa52
David Cramer <dcramer@gmail.com>
2014-09-16 01:49:17 -0700

    Add status and assignment queries

--- src/sentry/search/utils.py
+++ src/sentry/search/utils.py
@@ -1,17 +1,36 @@
 from __future__ import absolute_import, division, print_function
 
+from sentry.constants import STATUS_CHOICES
+from sentry.utils.auth import find_users
 
-def parse_query(query):
+
+def parse_query(query, user):
     # TODO(dcramer): make this better
     tokens = query.split(' ')
 
     results = {'tags': {}, 'query': []}
     for token in tokens:
-        if ':' in token:
-            key, value = token.split(':', 1)
-            results['tags'][key] = value
-        else:
+        if ':' not in token:
             results['query'].append(token)
+            continue
+
+        key, value = token.split(':', 1)
+
+        if key == 'is':
+            try:
+                results['status'] = STATUS_CHOICES[value]
+            except KeyError:
+                pass
+        elif key == 'assigned':
+            if value == 'me':
+                results['assigned_to'] = user
+            else:
+                try:
+                    results['assigned_to'] = find_users(value)[0]
+                except IndexError:
+                    pass
+        else:
+            results['tags'][key] = value
 
     results['query'] = ' '.join(results['query'])
 
