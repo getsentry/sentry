f00e39bfb26400feb969b6350d06b04b1300e8b8
David Cramer <dcramer@gmail.com>
2014-09-15 19:51:47 -0700

    Add bulk update/delete endpoints for project groups

--- src/sentry/api/endpoints/project_group_index.py
+++ src/sentry/api/endpoints/project_group_index.py
@@ -1,14 +1,26 @@
 from datetime import timedelta
+from django.db.models import Q
 from django.utils import timezone
+from rest_framework import serializers
 from rest_framework.response import Response
 
 from sentry.app import search
 from sentry.api.base import Endpoint
 from sentry.api.permissions import assert_perm
 from sentry.api.serializers import serialize
-from sentry.constants import DEFAULT_SORT_OPTION, STATUS_CHOICES
-from sentry.models import TagKey, Project
+from sentry.constants import (
+    DEFAULT_SORT_OPTION, STATUS_CHOICES, STATUS_RESOLVED
+)
+from sentry.db.models.query import create_or_update
+from sentry.models import Activity, Group, GroupBookmark, Project, TagKey
 from sentry.utils.dates import parse_date
+
+
+class GroupSerializer(serializers.Serializer):
+    status = serializers.ChoiceField(choices=zip(
+        STATUS_CHOICES.keys(), STATUS_CHOICES.keys()
+    ))
+    isBookmarked = serializers.BooleanField()
 
 
 class ProjectGroupIndexEndpoint(Endpoint):
@@ -84,3 +96,129 @@ class ProjectGroupIndexEndpoint(Endpoint):
         results = search.query(**query_kwargs)
 
         return Response(serialize(list(results), request.user))
+
+    def put(self, request, project_id):
+        """
+        Bulk mutate various attributes on groups.
+
+        - For non-status updates, only queries by 'id' are accepted.
+        - For status updates, the 'id' parameter may be omitted for a batch
+        "update all" query.
+
+        PUT ?id=1&id=2&id=3
+          status=resolved&
+          isBookmarked=1
+
+        Attributes:
+
+        - status=[resolved|unresolved|muted]
+        - isBookmarked=[1|0]
+
+        If any ids are out of scope this operation will succeed without any data
+        mutation
+        """
+        project = Project.objects.get_from_cache(
+            id=project_id,
+        )
+
+        assert_perm(project, request.user, request.auth)
+
+        group_ids = request.GET.getlist('id')
+        if group_ids:
+            group_list = Group.objects.filter(project=project, id__in=group_ids)
+            # filter down group ids to only valid matches
+            group_ids = [g.id for g in group_list]
+
+            if not group_ids:
+                return Response(status=204)
+
+        serializer = GroupSerializer(data=request.DATA, partial=True)
+        if not serializer.is_valid():
+            return Response(status=400)
+
+        result = serializer.object
+
+        # validate that we've passed a selector for non-status bulk operations
+        if not group_ids and result.get('isBookmarked') is not None:
+            return Response(status=400)
+
+        if group_ids:
+            filters = Q(id__in=group_ids)
+        else:
+            filters = Q(project=project)
+
+        if result.get('status') == 'resolved':
+            now = timezone.now()
+
+            happened = Group.objects.filter(filters).exclude(
+                status=STATUS_RESOLVED,
+            ).update(
+                status=STATUS_RESOLVED,
+                resolved_at=now,
+            )
+
+            if group_ids and happened:
+                for group in group_list:
+                    create_or_update(
+                        Activity,
+                        project=group.project,
+                        group=group,
+                        type=Activity.SET_RESOLVED,
+                        user=request.user,
+                    )
+        elif result.get('status'):
+            new_status = STATUS_CHOICES[result['status']]
+
+            Group.objects.filter(filters).exclude(
+                status=new_status,
+            ).update(
+                status=new_status,
+            )
+
+        if result.get('isBookmarked'):
+            for group in group_list:
+                GroupBookmark.objects.get_or_create(
+                    project=group.project,
+                    group=group,
+                    user=request.user,
+                )
+        elif result.get('isBookmarked') is False:
+            GroupBookmark.objects.filter(
+                group__in=group_ids,
+                user=request.user,
+            ).delete()
+
+        return Response(status=204)
+
+    def delete(self, request, project_id):
+        """
+        Permanently remove the given groups.
+
+        Only queries by 'id' are accepted.
+
+        DELETE ?id=1&id=2&id=3
+
+        If any ids are out of scope this operation will succeed without any data
+        mutation
+        """
+        project = Project.objects.get_from_cache(
+            id=project_id,
+        )
+
+        assert_perm(project, request.user, request.auth)
+
+        group_ids = request.GET.getlist('id')
+        if group_ids:
+            group_list = Group.objects.filter(project=project, id__in=group_ids)
+            # filter down group ids to only valid matches
+            group_ids = [g.id for g in group_list]
+        else:
+            # missing any kind of filter
+            return Response(status=400)
+
+        if not group_ids:
+            return Response(status=204)
+
+        group_list.delete()
+
+        return Response(status=204)
