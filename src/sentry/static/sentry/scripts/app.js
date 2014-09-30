9dd12a5aeaed90f82dfb41bcb6a084d42ab52435
David Cramer <dcramer@gmail.com>
2014-09-29 19:33:09 -0700

    Load tags on group details async

--- src/sentry/static/sentry/scripts/app.js
+++ src/sentry/static/sentry/scripts/app.js
@@ -320,6 +320,35 @@
                 $this.find('textarea').addClass('disabled');
             });
 
+            $('.tag-widget').each(function(){
+                var $widget = $(this);
+                $.ajax({
+                    url: $widget.data('url'),
+                    success: function(data) {
+                        var total = data.total,
+                            eTagName = encodeURIComponent(data.name);
+
+                        $widget.find('.loading').remove();
+                        $.each(data.values, function(_, item){
+                            var tagValue = item[0],
+                                timesSeen = item[1],
+                                percent = parseInt(timesSeen / total * 100, 10),
+                                url = app.config.urlPrefix + '/' + app.config.teamId + '/' + app.config.projectId + '/';
+
+                            $('<li>' +
+                                '<div class="progressbar">' +
+                                    '<div style="width:' + percent + '%">' + timesSeen + '</div>' +
+                                    '<a href="' + url + '?' + eTagName + '=' + encodeURIComponent(tagValue) + '">' +
+                                        tagValue +
+                                        '<span>' + percent + '%</span>' +
+                                    '</a>' +
+                                '</div>' +
+                            '</li>').appendTo($widget);
+                        });
+                    }
+                });
+            });
+
             var $event_nav = $('#event_nav');
             if ($event_nav.length > 0) {
                 var $window = $(window);
