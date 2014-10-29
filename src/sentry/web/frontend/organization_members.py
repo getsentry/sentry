90c0931eb30c53d887108ccaf0da7773e4a7ae30
David Cramer <dcramer@gmail.com>
2014-10-28 20:33:42 -0700

    Show team counts with organization members

--- src/sentry/web/frontend/organization_members.py
+++ src/sentry/web/frontend/organization_members.py
@@ -1,17 +1,35 @@
 from __future__ import absolute_import
 
+from collections import defaultdict
+
 from sentry.models import OrganizationMember, OrganizationMemberType
 from sentry.web.frontend.base import OrganizationView
+
+OrganizationMemberTeams = OrganizationMember.teams.through
 
 
 class OrganizationMembersView(OrganizationView):
     required_access = OrganizationMemberType.ADMIN
 
     def get(self, request, organization):
+        queryset = OrganizationMemberTeams.objects.filter(
+            organizationmember__organization=organization,
+        ).select_related('team')
+
+        team_map = defaultdict(list)
+        for omt in queryset:
+            team_map[omt.organizationmember_id].append(omt.team)
+
+        queryset = OrganizationMember.objects.filter(
+            organization=organization,
+        ).select_related('user')
+
+        member_list = []
+        for om in queryset:
+            member_list.append((om, team_map[om.id]))
+
         context = {
-            'member_list': OrganizationMember.objects.filter(
-                organization=organization
-            ).select_related('user'),
+            'member_list': member_list,
         }
 
         return self.respond('sentry/organization-members.html', context)
