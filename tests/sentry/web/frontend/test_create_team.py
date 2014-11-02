53ac6c04d7166d74bd56e331ae06a2f97f5d28f9
David Cramer <dcramer@gmail.com>
2014-11-01 22:28:37 -0700

    Migrate create team tests/template

---
+++ tests/sentry/web/frontend/test_create_team.py
@@ -0,0 +1,34 @@
+from __future__ import absolute_import
+
+from django.core.urlresolvers import reverse
+
+from sentry.models import Team
+from sentry.testutils import TestCase
+
+
+class CreateTeamTest(TestCase):
+    def test_renders_with_context(self):
+        organization = self.create_organization()
+        path = reverse('sentry-new-team', args=[organization.id])
+        self.login_as(self.user)
+        resp = self.client.get(path)
+        assert resp.status_code == 200
+        self.assertTemplateUsed(resp, 'sentry/create-team.html')
+        assert resp.context['organization'] == organization
+        assert resp.context['form']
+
+    def test_valid_params(self):
+        organization = self.create_organization()
+        path = reverse('sentry-new-team', args=[organization.id])
+        self.login_as(self.user)
+        resp = self.client.post(path, {
+            'name': 'bar',
+        })
+        assert resp.status_code == 302
+
+        team = Team.objects.get(organization=organization, name='bar')
+
+        assert team.name == 'bar'
+
+        redirect_uri = reverse('sentry-new-project', args=[team.slug])
+        assert resp['Location'] == 'http://testserver' + redirect_uri
