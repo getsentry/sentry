d529d131b2f4ac5cfcee424381361512887aab5f
David Cramer <dcramer@gmail.com>
2014-11-02 10:13:56 -0800

    Correct test

--- tests/sentry/api/endpoints/test_user_details.py
+++ tests/sentry/api/endpoints/test_user_details.py
@@ -7,10 +7,6 @@ from sentry.testutils import APITestCase
 class UserDetailsTest(APITestCase):
     def test_simple(self):
         user = self.create_user(email='a@example.com')
-        team_1 = self.create_team(owner=user, name='a')
-        project_1 = self.create_project(team=team_1)
-        team_2 = self.create_team(owner=user, name='b')
-        project_2 = self.create_project(team=team_2)
 
         self.login_as(user=user)
 
@@ -21,14 +17,6 @@ class UserDetailsTest(APITestCase):
 
         assert response.status_code == 200, response.content
         assert response.data['id'] == str(user.id)
-        assert len(response.data['teams']) == 2
-        response.data['teams'].sort(key=lambda x: x['name'])
-        assert response.data['teams'][0]['id'] == str(team_1.id)
-        assert response.data['teams'][1]['id'] == str(team_2.id)
-        assert len(response.data['teams'][0]['projects']) == 1
-        assert response.data['teams'][0]['projects'][0]['id'] == str(project_1.id)
-        assert len(response.data['teams'][1]['projects']) == 1
-        assert response.data['teams'][1]['projects'][0]['id'] == str(project_2.id)
 
 
 class UserUpdateTest(APITestCase):
