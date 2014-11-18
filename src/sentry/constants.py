831c3b3a07ad5e2904afd4dcb2dff2a48a6f2c60
Steven Klass <sklass@pointcircle.com>
2014-11-18 12:13:38 -0700

    Added a level rule for matching event rules.

--- src/sentry/constants.py
+++ src/sentry/constants.py
@@ -193,6 +193,7 @@ SENTRY_RULES = (
     'sentry.rules.conditions.regression_event.RegressionEventCondition',
     'sentry.rules.conditions.tagged_event.TaggedEventCondition',
     'sentry.rules.conditions.event_frequency.EventFrequencyCondition',
+    'sentry.rules.conditions.level.LevelCondition',
 )
 
 # methods as defined by http://www.w3.org/Protocols/rfc2616/rfc2616-sec9.html + PATCH
