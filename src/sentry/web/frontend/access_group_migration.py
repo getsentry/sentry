10d5f11313eacf684c449c2d19c15dfe4f70c3c1
David Cramer <dcramer@gmail.com>
2014-11-30 11:52:14 -0800

    First working version of migration wizard

--- src/sentry/web/frontend/access_group_migration.py
+++ src/sentry/web/frontend/access_group_migration.py
@@ -2,17 +2,61 @@ from __future__ import absolute_import
 
 from collections import defaultdict
 
-from sentry.models import AccessGroup, OrganizationMemberType
+from sentry.models import (
+    AccessGroup, OrganizationMember, OrganizationMemberType, Team
+)
 from sentry.web.frontend.base import OrganizationView
 
 
 class AccessGroupMigrationView(OrganizationView):
     required_access = OrganizationMemberType.ADMIN
 
-    def get(self, request, organization):
+    def process_posted_member(self, request, organization, member):
+        global_access = request.POST.get('user[%s][global_access]' % member.user_id)
+        teams = request.POST.getlist('user[%s][team]' % member.user_id)
+        remove = request.POST.get('user[%s][remove]' % member.user_id)
+        access_type = request.POST.get('user[%s][type]' % member.user_id)
+
+        if not access_type:
+            return
+
+        if remove != '1':
+            if access_type == 'member':
+                access_type = OrganizationMemberType.MEMBER
+            elif access_type == 'admin':
+                access_type = OrganizationMemberType.ADMIN
+            else:
+                return
+
+            global_access = global_access == '1'
+
+            om = OrganizationMember.objects.create(
+                organization=organization,
+                has_global_access=global_access,
+                type=access_type,
+                user=member.user,
+            )
+
+            if not global_access:
+                for team in teams:
+                    om.teams.add(Team.objects.get_from_cache(slug=team))
+
+        member.delete()
+
+    def handle(self, request, organization):
         member_list = list(AccessGroup.members.through.objects.filter(
             accessgroup__team__organization=organization,
         ).select_related('user', 'accessgroup', 'accessgroup__team'))
+
+        if request.method == 'POST':
+            for member in member_list:
+                self.process_posted_member(request, organization, member)
+
+            for ag in AccessGroup.objects.filter(team__organization=organization):
+                if not ag.members.exists():
+                    ag.delete()
+
+            return self.redirect(request.path)
 
         group_list = set(m.accessgroup for m in member_list)
 
