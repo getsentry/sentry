41048de111396ac3d25c834265a0938126983fa5
David Cramer <dcramer@gmail.com>
2014-12-01 10:09:41 -0800

    Add test for organization settings

---
+++ tests/sentry/web/frontend/test_organization_settings.py
@@ -0,0 +1,43 @@
+from __future__ import absolute_import
+
+from django.core.urlresolvers import reverse
+
+from sentry.models import Organization
+from sentry.testutils import TestCase
+
+
+class OrganizationSettingsTest(TestCase):
+    def test_renders_with_context(self):
+        organization = self.create_organization(name='foo', owner=self.user)
+        team = self.create_team(organization=organization)
+        project = self.create_project(team=team)
+
+        path = reverse('sentry-organization-settings', args=[organization.id])
+
+        self.login_as(self.user)
+
+        resp = self.client.get(path)
+
+        assert resp.status_code == 200
+
+        self.assertTemplateUsed(resp, 'sentry/organization-settings.html')
+
+        assert resp.context['organization'] == organization
+        assert resp.context['form']
+
+    def test_saves(self):
+        organization = self.create_organization(name='foo', owner=self.user)
+        team = self.create_team(organization=organization)
+        project = self.create_project(team=team)
+
+        path = reverse('sentry-organization-settings', args=[organization.id])
+
+        self.login_as(self.user)
+
+        resp = self.client.post(path, {'name': 'bar'})
+
+        assert resp.status_code == 302
+
+        organization = Organization.objects.get(id=organization.id)
+
+        assert organization.name == 'bar'
