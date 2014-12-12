362b9262301e03c6a325381ed5b9cc0e8d05dacd
David Cramer <dcramer@gmail.com>
2014-12-11 18:42:03 -0800

    Add project breakdown to usage

--- src/sentry/api/endpoints/organization_stats.py
+++ src/sentry/api/endpoints/organization_stats.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from sentry.app import tsdb
 from sentry.api.base import BaseStatsEndpoint
 from sentry.api.permissions import assert_perm
-from sentry.models import Organization
+from sentry.models import Organization, Project, Team
 
 
 class OrganizationStatsEndpoint(BaseStatsEndpoint):
@@ -14,18 +14,49 @@ class OrganizationStatsEndpoint(BaseStatsEndpoint):
 
         assert_perm(organization, request.user, request.auth)
 
+        group = request.GET.get('group')
+        if not group:
+            keys = [organization.id]
+        elif group == 'project':
+            team_list = Team.objects.get_for_user(
+                organization=organization,
+                user=request.user,
+            )
+
+            project_list = []
+            for team in team_list:
+                project_list.extend(Project.objects.get_for_user(
+                    team=team,
+                    user=request.user,
+                ))
+            keys = [p.id for p in project_list]
+        else:
+            raise ValueError('Invalid group: %s' % group)
+
+        if not keys:
+            return Response([])
+
         stat = request.GET.get('stat', 'received')
         if stat == 'received':
-            stat_model = tsdb.models.organization_total_received
+            if group == 'project':
+                stat_model = tsdb.models.project_total_received
+            else:
+                stat_model = tsdb.models.organization_total_received
         elif stat == 'rejected':
-            stat_model = tsdb.models.organization_total_rejected
+            if group == 'project':
+                stat_model = tsdb.models.project_total_rejected
+            else:
+                stat_model = tsdb.models.organization_total_rejected
         else:
             raise ValueError('Invalid stat: %s' % stat)
 
         data = tsdb.get_range(
             model=stat_model,
-            keys=[organization.id],
+            keys=keys,
             **self._parse_args(request)
-        )[organization.id]
+        )
+
+        if not group:
+            data = data[organization.id]
 
         return Response(data)
