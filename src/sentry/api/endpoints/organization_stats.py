9444a4e1f59bff943505b75db77e9f7dd0977c81
David Cramer <dcramer@gmail.com>
2014-12-11 17:33:45 -0800

    Switch organization stats to aggregate model

--- src/sentry/api/endpoints/organization_stats.py
+++ src/sentry/api/endpoints/organization_stats.py
@@ -1,12 +1,11 @@
 from __future__ import absolute_import
 
 from rest_framework.response import Response
-from six.moves import range
 
 from sentry.app import tsdb
 from sentry.api.base import BaseStatsEndpoint
 from sentry.api.permissions import assert_perm
-from sentry.models import Organization, Project, Team
+from sentry.models import Organization
 
 
 class OrganizationStatsEndpoint(BaseStatsEndpoint):
@@ -17,36 +16,16 @@ class OrganizationStatsEndpoint(BaseStatsEndpoint):
 
         stat = request.GET.get('stat', 'received')
         if stat == 'received':
-            stat_model = tsdb.models.project_total_received
+            stat_model = tsdb.models.organization_total_received
         elif stat == 'rejected':
-            stat_model = tsdb.models.project_total_rejected
+            stat_model = tsdb.models.organization_total_rejected
         else:
             raise ValueError('Invalid stat: %s' % stat)
 
-        team_list = Team.objects.get_for_user(
-            organization=organization,
-            user=request.user,
-        )
-
-        project_list = []
-        for team in team_list:
-            project_list.extend(Project.objects.get_for_user(
-                team=team,
-                user=request.user,
-            ))
-
-        if not project_list:
-            return Response([])
-
         data = tsdb.get_range(
             model=stat_model,
-            keys=[p.id for p in project_list],
+            keys=[organization.id],
             **self._parse_args(request)
-        ).values()
+        )[organization.id]
 
-        summarized = []
-        for n in range(len(data[0])):
-            total = sum(d[n][1] for d in data)
-            summarized.append((data[0][n][0], total))
-
-        return Response(summarized)
+        return Response(data)
--- tests/sentry/api/endpoints/test_organization_stats.py
+++ tests/sentry/api/endpoints/test_organization_stats.py
@@ -9,15 +9,8 @@ class OrganizationStatsTest(APITestCase):
         self.login_as(user=self.user)
 
         org = self.create_organization(owner=self.user, name='baz')
-        team = self.create_team(organization=org, name='foo')
-        project_1 = self.create_project(team=team, name='a')
-        project_2 = self.create_project(team=team, name='b')
-        team_2 = self.create_team(organization=org, name='bar')
-        project_3 = self.create_project(team=team_2, name='c')
 
-        tsdb.incr(tsdb.models.project_total_received, project_1.id, count=3)
-        tsdb.incr(tsdb.models.project_total_received, project_2.id, count=5)
-        tsdb.incr(tsdb.models.project_total_received, project_3.id, count=10)
+        tsdb.incr(tsdb.models.organization_total_received, org.id, count=3)
 
         url = reverse('sentry-api-0-organization-stats', kwargs={
             'organization_id': org.id,
@@ -25,7 +18,7 @@ class OrganizationStatsTest(APITestCase):
         response = self.client.get(url, format='json')
 
         assert response.status_code == 200, response.content
-        assert response.data[-1][1] == 18, response.data
+        assert response.data[-1][1] == 3, response.data
         for point in response.data[:-1]:
             assert point[1] == 0
         assert len(response.data) == 24
