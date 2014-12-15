81c18642bf3f5f763c99e9e102cfbdde190577d7
David Cramer <dcramer@gmail.com>
2014-12-15 00:01:52 -0800

    Remove (broken/unused) sparklines

--- src/sentry/static/sentry/scripts/app.js
+++ src/sentry/static/sentry/scripts/app.js
@@ -190,65 +190,6 @@
 
     });
 
-    app.SelectTeamPage = BasePage.extend({
-
-        initialize: function(){
-            BasePage.prototype.initialize.apply(this, arguments);
-
-            this.refreshSparklines();
-            $(window).on('resize', this.refreshSparklines);
-        },
-
-        refreshSparklines: function(){
-            $('.chart').each(function(n, el){
-                var $el = $(el);
-                $.ajax({
-                    url: $el.attr('data-api-url'),
-                    type: 'get',
-                    dataType: 'json',
-                    data: {
-                        since: new Date().getTime() / 1000 - 3600 * 24,
-                        resolution: '1h'
-                    },
-                    success: _.bind(function(data){
-                        for (var i = 0; i < data.length; i++) {
-                            // set timestamp to be in millis
-                            data[i][0] = data[i][0] * 1000;
-                        }
-
-                        $.plot($el, [{
-                                data: data,
-                                color: '#ebeff3',
-                                shadowSize: 0,
-                                lines: {
-                                    lineWidth: 2,
-                                    show: true,
-                                    fill: true,
-                                    color: '#f6f8fa'
-                            }
-                            }], {
-                                yaxis: {
-                                    min: 0
-                                },
-                                grid: {
-                                    show: false
-                                },
-                                hoverable: false,
-                                legend: {
-                                    noColumns: 5
-                                },
-                                lines: {
-                                    show: false
-                                }
-                            }
-                        );
-                    }, this)
-                });
-            });
-        }
-
-    });
-
     app.GroupDetailsPage = BasePage.extend({
 
         initialize: function(data){
