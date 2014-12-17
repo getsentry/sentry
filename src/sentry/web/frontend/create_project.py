c14b7a9b04ae1f2fe67c6f6badcfd10268813f83
David Cramer <dcramer@gmail.com>
2014-12-17 11:44:48 -0800

    Fix/test create project permissions

--- src/sentry/web/frontend/create_project.py
+++ src/sentry/web/frontend/create_project.py
@@ -1,11 +1,17 @@
 from __future__ import absolute_import
 
 from django import forms
+from django.contrib import messages
 from django.core.urlresolvers import reverse
 
-from sentry.models import OrganizationMemberType, Project, Team
+from sentry.models import (
+    OrganizationMember, OrganizationMemberType, Project, Team
+)
 from sentry.web.forms.add_project import AddProjectForm
 from sentry.web.frontend.base import OrganizationView
+
+
+ERR_NO_TEAMS = 'You cannot create a new project because there are no teams to assign it to.'
 
 
 class AddProjectWithTeamForm(AddProjectForm):
@@ -42,21 +48,35 @@ class CreateProjectView(OrganizationView):
     # TODO(dcramer): I'm 95% certain the access is incorrect here as it would
     # be probably validating against global org access, and all we care about is
     # team admin
-    required_access = OrganizationMemberType.ADMIN
+    def get_form(self, request, organization, team_list):
+        return AddProjectWithTeamForm(request.user, team_list, request.POST or None, initial={
+            'team': request.GET.get('team'),
+        })
 
-    def get_form(self, request, organization):
+    def has_permission(self, request, organization):
+        if organization is None:
+            return False
+        if request.user.is_superuser:
+            return True
+        # we special case permissions here as a team admin can create projects
+        # but they are restricted to only creating projects on teams where they
+        # are an admin
+        return OrganizationMember.objects.filter(
+            user=request.user,
+            type__lte=OrganizationMemberType.ADMIN,
+        )
+
+    def handle(self, request, organization):
         team_list = Team.objects.get_for_user(
             organization=organization,
             user=request.user,
             access=OrganizationMemberType.ADMIN,
         )
+        if not team_list:
+            messages.error(request, ERR_NO_TEAMS)
+            return self.redirect(reverse('sentry-organization-home', args=[organization.slug]))
 
-        return AddProjectWithTeamForm(request.user, team_list, request.POST or None, initial={
-            'team': request.GET.get('team'),
-        })
-
-    def handle(self, request, organization):
-        form = self.get_form(request, organization)
+        form = self.get_form(request, organization, team_list)
         if form.is_valid():
             project = form.save(request.user, request.META['REMOTE_ADDR'])
 
