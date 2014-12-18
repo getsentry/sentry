3d4b784ba5e30b06774be5d3bd39cf0221c28336
David Cramer <dcramer@gmail.com>
2014-12-18 10:47:59 -0800

    Correct test

--- tests/sentry/web/frontend/test_project_settings.py
+++ tests/sentry/web/frontend/test_project_settings.py
@@ -74,6 +74,7 @@ class ProjectSettingsTest(TestCase):
             'name': 'bar',
             'slug': self.project.slug,
             'platform': 'python',
+            'team': self.team.id,
         })
         assert resp.status_code == 302
         self.assertEquals(resp['Location'], 'http://testserver' + self.path)
