c023c6a63ecaa9e068c433544528040edc737942
David Cramer <dcramer@gmail.com>
2014-12-18 13:21:50 -0800

    Add dsn to platform index

--- src/sentry/web/frontend/help_platform_index.py
+++ src/sentry/web/frontend/help_platform_index.py
@@ -1,19 +1,8 @@
 from __future__ import absolute_import, print_function
 
-from sentry.web.helpers import render_to_response
-from sentry.web.frontend.base import BaseView
+from sentry.web.frontend.help_platform_base import HelpPlatformBaseView
 
 
-class HelpPlatformIndexView(BaseView):
-    auth_required = False
-
-    def get(self, request):
-        try:
-            pid = int(request.GET.get('pid', 0))
-        except (TypeError, ValueError):
-            pid = None
-
-        if pid:
-            request.session['pid'] = pid
-
-        return render_to_response('sentry/help/platform_index.html', {}, request)
+class HelpPlatformIndexView(HelpPlatformBaseView):
+    def get(self, request, project_list, selected_project):
+        return self.respond('sentry/help/platform_index.html')
