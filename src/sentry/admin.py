e8dcc1ccd32fb8ec115432fb72ba7222e3f7b65a
David Cramer <dcramer@gmail.com>
2014-12-19 10:25:02 -0800

    Correct search fields on AuditLogEntry

--- src/sentry/admin.py
+++ src/sentry/admin.py
@@ -254,7 +254,7 @@ admin.site.register(User, UserAdmin)
 class AuditLogEntryAdmin(admin.ModelAdmin):
     list_display = ('event', 'organization')
     list_filter = ('event',)
-    search_fields = ('actor', 'organization')
+    search_fields = ('actor__email', 'organization__name', 'organization__slug')
     raw_id_fields = ('organization', 'actor', 'target_user')
 
 admin.site.register(AuditLogEntry, AuditLogEntryAdmin)
