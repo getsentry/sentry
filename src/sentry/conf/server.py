fca4c53508005a20f76d0a88fd1603e9cbdbcedb
David Cramer <dcramer@gmail.com>
2014-12-24 12:33:11 -0800

    Revert to pickle serializer (required for social auth)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -214,7 +214,8 @@ AUTHENTICATION_BACKENDS = (
 SOCIAL_AUTH_USER_MODEL = AUTH_USER_MODEL = 'sentry.User'
 
 SESSION_ENGINE = "django.contrib.sessions.backends.signed_cookies"
-SESSION_COOKIE_NAME = "sentrysid2"
+SESSION_COOKIE_NAME = "sentrysid"
+SESSION_SERIALIZER = "django.contrib.sessions.serializers.PickleSerializer"
 
 TWITTER_CONSUMER_KEY = ''
 TWITTER_CONSUMER_SECRET = ''
