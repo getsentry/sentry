0024f5f0b0ecc7577dee08794a7c699a42e8a1b0
David Cramer <dcramer@gmail.com>
2014-12-23 19:38:55 -0800

    Correct NotifyEventService action

--- tests/sentry/rules/actions/test_notify_event_service.py
+++ tests/sentry/rules/actions/test_notify_event_service.py
@@ -7,8 +7,8 @@ from sentry.rules.actions.notify_event_service import NotifyEventServiceAction
 class NotifyEventServiceActionTest(RuleTestCase):
     rule_cls = NotifyEventServiceAction
 
-    @patch('sentry.plugins.sentry_mail.models.MailPlugin.notify_users')
-    def test_applies_correctly(self, mail_notify_users):
+    @patch('sentry.plugins.sentry_mail.models.MailPlugin.notify')
+    def test_applies_correctly(self, mail_notify):
         event = self.get_event()
 
         rule = self.get_rule({
@@ -16,4 +16,4 @@ class NotifyEventServiceActionTest(RuleTestCase):
         })
         rule.after(event=event, state=self.get_state())
 
-        mail_notify_users.assert_called_once_with(group=event.group, event=event)
+        mail_notify.assert_called_once()
