713c727cf824ca28cb37def2c2faa6d786334472
David Cramer <dcramer@gmail.com>
2014-12-29 13:15:42 -0800

    More react basics

--- src/sentry/static/sentry/app-react/main.jsx
+++ src/sentry/static/sentry/app-react/main.jsx
@@ -1,18 +1,44 @@
 /*** @jsx React.DOM */
 var React = require("react");
+var Router = require("react-router");
+var $ = require("jquery");
 
-var CommentBox = React.createClass({
+var Route = Router.Route;
+var NotFoundRoute = Router.NotFoundRoute;
+var DefaultRoute = Router.DefaultRoute;
+var Link = Router.Link;
+var RouteHandler = Router.RouteHandler;
+
+var App = React.createClass({
   render: function() {
     return (
-      <div className="commentBox">
-        Hello, world! I am a CommentBox.
+      <div>
+        Hello World!
+        <RouteHandler/>
       </div>
     );
   }
 });
 
+var Example = React.createClass({
+  render: function() {
+    return (
+      <div>
+        Test
+      </div>
+    );
+  }
+});
 
-React.renderComponent(
-  <CommentBox />,
-  document.getElementById('content')
+var routes = (
+  <Route handler={App}>
+    <Route name="example" handler={Example}/>
+    <DefaultRoute handler={Example}/>
+  </Route>
 );
+
+$(function(){
+  Router.run(routes, function (Handler) {
+    React.render(<Handler/>, document.body);
+  });
+});
