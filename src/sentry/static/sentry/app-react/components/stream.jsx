61447bee199ef6fc13b15b1f8374d0769ad58689
David Cramer <dcramer@gmail.com>
2014-12-29 23:41:25 -0800

    Move search bar into react components

--- src/sentry/static/sentry/app-react/components/stream.jsx
+++ src/sentry/static/sentry/app-react/components/stream.jsx
@@ -1,8 +1,120 @@
 /*** @jsx React.DOM */
 var React = require("react");
 
+var utils = require("../utils");
+
 var Count = require("./count");
 var TimeSince = require("./timeSince");
+
+var SearchDropdown = React.createClass({
+  componentDidMount: function(){
+    $('.filter-nav .search-input').focus(function(){
+      $('.search-dropdown').show();
+    }).blur(function(){
+      $('.search-dropdown').hide();
+    });
+  },
+
+  render: function() {
+    return (
+      <div className="search-dropdown" style={{display:"none"}}>
+        <ul className="search-helper search-autocomplete-list">
+          <li className="search-autocomplete-item">
+            <span className="icon icon-tag"></span>
+            <h4>Tag - <span className="search-description">key/value pair associated to an event</span></h4>
+            <p className="search-example">browser:"Chrome 34"</p>
+          </li>
+          <li className="search-autocomplete-item">
+            <span className="icon icon-toggle"></span>
+            <h4>Status - <span className="search-description">State of an event</span></h4>
+            <p className="search-example">is:resolved, unresolved, muted</p>
+          </li>
+          <li className="search-autocomplete-item">
+            <span className="icon icon-user"></span>
+            <h4>Assigned - <span className="search-description">team member assigned to an event</span></h4>
+            <p className="search-example">assigned:[me|user@example.com]</p>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+});
+
+var SearchBar = React.createClass({
+  render: function() {
+    return (
+      <div className="search">
+        <form className="form-horizontal" action="." method="GET">
+          <div>
+            <input type="text" className="search-input form-control"
+                   placeholder="Search for events, users, tags, and everything else."
+                   name="query" />
+            <span className="icon-search"></span>
+          </div>
+          <SearchDropdown />
+        </form>
+      </div>
+    );
+  }
+});
+
+var FilterSelectLink = React.createClass({
+  render: function() {
+    var className = this.props.extraClass;
+    className += ' btn btn-default';
+
+    if (this.props.isActive) {
+      className += ' btn-active';
+    }
+
+    var queryString = '?' + this.props.query;
+
+    return (
+      <a href={queryString}
+          className={className}>{this.props.label}</a>
+    );
+  }
+});
+
+var FilterSelect = React.createClass({
+  render: function() {
+    var params = utils.getQueryParams();
+    var activeButton;
+    if (params.bookmarks) {
+      activeButton = 'bookmarks';
+    } else if (params.assigned) {
+      activeButton = 'assigned';
+    } else {
+      activeButton = 'all';
+    }
+
+    return (
+      <div className="filter-nav" ng-controller="ProjectStreamControlsCtrl">
+        <div className="row">
+          <div className="col-sm-4 primary-filters">
+            <div className="btn-group btn-group-justified">
+              <FilterSelectLink label="All Events"
+                                query=""
+                                isActive={activeButton === 'all'}
+                                extraClass="btn-all-events" />
+              <FilterSelectLink label="Bookmarks"
+                                query="bookmarks=1"
+                                isActive={activeButton === 'bookmarks'}
+                                extraClass="btn-middle btn-bookmarks" />
+              <FilterSelectLink label="Assigned"
+                                query="assigned=1"
+                                isActive={activeButton === 'assigned'}
+                                extraClass="btn-assigned" />
+            </div>
+          </div>
+          <div className="col-sm-8">
+            <SearchBar />
+          </div>
+        </div>
+      </div>
+    );
+  }
+});
 
 var Actions = React.createClass({
   render: function() {
@@ -14,10 +126,10 @@ var Actions = React.createClass({
           </div>
           <div className="btn-group">
             <a href="javascript:void(0)" className="btn btn-default btn-sm action action-resolve">
-                <i aria-hidden="true" className="icon-checkmark"></i>
+              <i aria-hidden="true" className="icon-checkmark"></i>
             </a>
             <a href="javascript:void(0)" className="btn btn-default btn-sm action action-bookmark">
-                <span className="icon icon-bookmark"></span>
+              <span className="icon icon-bookmark"></span>
             </a>
             <a className="btn btn-default btn-sm hidden-xs action action-more dropdown-toggle" data-toggle="dropdown">
               <span className="icon-ellipsis"></span>
@@ -165,6 +277,7 @@ var Stream = React.createClass({
   render: function() {
     return (
       <div>
+        <FilterSelect />
         <div className="group-header-container" data-spy="affix" data-offset-top="134">
           <div className="container">
             <div className="group-header">
