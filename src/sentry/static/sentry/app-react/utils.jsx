04eba070739fdb7da1bb8f8be4eaafe5e6bace9b
David Cramer <dcramer@gmail.com>
2014-12-29 23:41:25 -0800

    Move search bar into react components

---
+++ src/sentry/static/sentry/app-react/utils.jsx
@@ -0,0 +1,26 @@
+var getQueryParams = function() {
+  var vars = {},
+      href = window.location.href,
+      hashes, hash;
+
+  if (href.indexOf('?') == -1)
+    return vars;
+
+  hashes = href.slice(
+    href.indexOf('?') + 1,
+    (href.indexOf('#') != -1 ? href.indexOf('#') : href.length)
+  ).split('&');
+  for (var i = 0, chunk; (chunk = hashes[i]); i++) {
+    hash = chunk.split('=');
+    if (!hash[0] && !hash[1])
+      return;
+
+    vars[decodeURIComponent(hash[0])] = (hash[1] ? decodeURIComponent(hash[1]).replace(/\+/, ' ') : '');
+  };
+
+  return vars;
+};
+
+module.exports = {
+  getQueryParams: getQueryParams
+};
