e141a37034fb9046ba931738e5b04d74928090d9
David Cramer <dcramer@gmail.com>
2014-12-30 23:44:18 -0800

    Initial work on action modals

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ function vendorFile(name) {
 module.exports = {
   entry: {
     "app": file("app-react/main.jsx"),
-    "vendor": ["react", "react-router", "jquery", "moment"]
+    "vendor": ["react", "react-bootstrap", "jquery", "moment"]
   },
   module: {
     loaders: [
