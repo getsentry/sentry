caa9ea3bcdd58d6d9073444fc599098e440e0d85
David Cramer <dcramer@gmail.com>
2015-01-02 10:27:50 -0800

    Add Plugin2.get_actions

--- src/sentry/templatetags/sentry_plugins.py
+++ src/sentry/templatetags/sentry_plugins.py
@@ -20,13 +20,17 @@ def get_actions(group, request):
     project = group.project
 
     action_list = []
-    for plugin in plugins.for_project(project):
+    for plugin in plugins.for_project(project, version=1):
         results = safe_execute(plugin.actions, request, group, action_list)
 
         if not results:
             continue
 
         action_list = results
+
+    for plugin in plugins.for_project(project, version=2):
+        for action in (safe_execute(plugin.get_actions, request, group) or ()):
+            action_list.append(action)
 
     return [(a[0], a[1], request.path == a[1]) for a in action_list]
 
