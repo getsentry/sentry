b416340cc1fb0318d56326b928b4e9d3d9e23e3e
David Cramer <dcramer@gmail.com>
2015-01-01 16:49:40 -0800

    Bump version to 7.1.0.dev0

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ class BuildStatic(Command):
 
 setup(
     name='sentry',
-    version='7.0.0',
+    version='7.1.0.dev0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='https://www.getsentry.com',
