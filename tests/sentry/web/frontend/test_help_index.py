f175c2bde0000dfe2c3fc2658e86bccbe7df1a88
David Cramer <dcramer@gmail.com>
2015-01-12 14:31:47 -0600

    Improve help index test

--- tests/sentry/web/frontend/test_help_index.py
+++ tests/sentry/web/frontend/test_help_index.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 from django.core.urlresolvers import reverse
 
 from sentry.testutils import TestCase
@@ -9,3 +11,4 @@ class HelpIndexTest(TestCase):
 
         resp = self.client.get(path)
         assert resp.status_code == 200
+        self.assertTemplateUsed(resp, 'sentry/help/index.html')
