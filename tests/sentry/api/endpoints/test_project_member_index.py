9f80e8339ed4c5658f8874ea9b6c9f4512cb5f9a
David Cramer <dcramer@gmail.com>
2015-01-15 15:39:30 -0800

    Correct project member endpoint (no access group support)

--- tests/sentry/api/endpoints/test_project_member_index.py
+++ tests/sentry/api/endpoints/test_project_member_index.py
@@ -1,5 +1,7 @@
+from __future__ import absolute_import
+
 from django.core.urlresolvers import reverse
-from sentry.models import AccessGroup
+
 from sentry.testutils import APITestCase
 
 
@@ -8,12 +10,12 @@ class ProjectMemberIndexTest(APITestCase):
         user_1 = self.create_user('foo@localhost', username='foo')
         user_2 = self.create_user('bar@localhost', username='bar')
         user_3 = self.create_user('baz@localhost', username='baz')
-        team = self.create_team(slug='baz', owner=user_1)
+        org = self.create_organization(owner=user_1)
+        team = self.create_team(organization=org, slug='baz')
         project_1 = self.create_project(team=team, slug='foo')
         project_2 = self.create_project(team=team, slug='bar')
-        ag = AccessGroup.objects.create(team=team, name='foo')
-        ag.projects.add(project_1)
-        ag.members.add(user_2)
+        org.member_set.create(user=user_2, has_global_access=True)
+        org.member_set.create(user=user_3, has_global_access=False)
 
         self.login_as(user=user_1)
 
@@ -23,5 +25,5 @@ class ProjectMemberIndexTest(APITestCase):
         response = self.client.get(url)
         assert response.status_code == 200
         assert len(response.data) == 2
-        assert response.data[0]['email'] == 'bar@localhost'
-        assert response.data[1]['email'] == 'foo@localhost'
+        assert response.data[0]['email'] == user_2.email
+        assert response.data[1]['email'] == user_1.email
