e94e6922041b7aad62123c351059a6b8ccfbb8aa
David Cramer <dcramer@gmail.com>
2015-01-15 11:38:38 -0800

    Correct cursor test

--- tests/sentry/utils/test_cursors.py
+++ tests/sentry/utils/test_cursors.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import math
+
 from mock import Mock
 
 from sentry.utils.cursors import build_cursor, Cursor
@@ -20,23 +22,28 @@ def test_build_cursor():
 
     results = [event1, event2, event3]
 
-    cursor = build_cursor(results, key='id', limit=1)
+    cursor_kwargs = {
+        'key': lambda x: math.floor(x.id),
+        'limit': 1,
+    }
+
+    cursor = build_cursor(results, **cursor_kwargs)
     assert isinstance(cursor.next, Cursor)
     assert cursor.next
     assert isinstance(cursor.prev, Cursor)
     assert not cursor.prev
     assert list(cursor) == [event1]
 
-    cursor = build_cursor(results, key='id', limit=1, cursor=cursor.next)
+    cursor = build_cursor(results[1:], cursor=cursor.next, **cursor_kwargs)
     assert isinstance(cursor.next, Cursor)
     assert cursor.next
     assert isinstance(cursor.prev, Cursor)
     assert cursor.prev
     assert list(cursor) == [event2]
 
-    cursor = build_cursor(results, key='id', limit=1, cursor=cursor.next)
+    cursor = build_cursor(results[2:], cursor=cursor.next, **cursor_kwargs)
     assert isinstance(cursor.next, Cursor)
-    assert cursor.next
+    assert not cursor.next
     assert isinstance(cursor.prev, Cursor)
     assert cursor.prev
     assert list(cursor) == [event3]
