3f16b6f9cec50c4fed258455a6bc655b8b9526a9
David Cramer <dcramer@gmail.com>
2015-01-20 17:59:10 -0800

    Improve assignee action abstraction

--- src/sentry/static/sentry/app/actions/aggregateListActions.jsx
+++ src/sentry/static/sentry/app/actions/aggregateListActions.jsx
@@ -2,8 +2,34 @@
 
 var Reflux = require("reflux");
 
-var AggregateListActions = Reflux.createActions([
-  "setAssignedTo"
-]);
+var AggregateListActions = Reflux.createActions({
+  "assignTo": {
+    children: ["completed", "failed"]
+  }
+});
+
+
+AggregateListActions.assignTo.listen(function(itemId, userEmail){
+  if (!itemId) {
+    this.failed(itemId, userEmail);
+    return false;
+  }
+
+  $.ajax({
+    url: '/api/0/groups/' + itemId + '/',
+    method: 'PUT',
+    data: JSON.stringify({
+      assignedTo: userEmail
+    }),
+    contentType: 'application/json',
+    success: function(data){
+      this.completed(itemId, userEmail, data);
+    }.bind(this),
+    error: function(){
+      this.failed(itemId, userEmail);
+    }.bind(this)
+  });
+});
+
 
 module.exports = AggregateListActions;
