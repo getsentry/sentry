60efd96af6c747e218bb48f2cb49960f3a541d84
David Cramer <dcramer@gmail.com>
2015-01-21 16:24:26 -0800

    7.1.4

--- setup.py
+++ setup.py
@@ -147,7 +147,7 @@ class BuildStatic(Command):
 
 setup(
     name='sentry',
-    version='7.1.3',
+    version='7.1.4',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='https://www.getsentry.com',
