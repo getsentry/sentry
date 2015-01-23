272731c1b63d7ecffa98dc3a6fbb50f329681e4c
David Cramer <dcramer@gmail.com>
2015-01-23 13:23:51 -0800

    Add activity outline to overview

--- src/sentry/static/sentry/app/views/aggregate/activity.jsx
+++ src/sentry/static/sentry/app/views/aggregate/activity.jsx
@@ -1,23 +1,21 @@
 /*** @jsx React.DOM */
 var React = require("react");
 
-var FlotChart = require("../../components/flotChart");
 var PropTypes = require("../../proptypes");
 
-var AggregateChart = React.createClass({
+var AggregateActivity = React.createClass({
   propTypes: {
     aggregate: PropTypes.Aggregate.isRequired
   },
 
   render: function() {
-    var points = [{x: 1421722207, y: 50}, {x: 1421722267, y: 150}];
-
     return (
       <div className="box">
+        <div className="box-header">
+            <h3>Activity</h3>
+        </div>
         <div className="box-content with-padding">
-          <FlotChart
-              points={points}
-              className="chart" />
+
         </div>
       </div>
     );
@@ -25,4 +23,5 @@ var AggregateChart = React.createClass({
 
 });
 
-module.exports = AggregateChart;
+module.exports = AggregateActivity;
+
