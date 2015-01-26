c97aa3d789ad281c757c3f08521e8d37dd799906
David Cramer <dcramer@gmail.com>
2015-01-26 15:20:47 -0800

    Improve test extensions
    - Remove warnings-as-errors from extension
    - Move middleware into testutils

--- conftest.py
+++ conftest.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
-import sys
-import os.path
-
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
-
 pytest_plugins = [
     'sentry.utils.pytest'
 ]
+
+
+def pytest_configure(config):
+    import warnings
+    warnings.filterwarnings('error', '', Warning, r'(sentry|raven)')
