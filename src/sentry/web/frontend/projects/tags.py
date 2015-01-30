aa03797b2b798a3d1fb897bc87aa0e15ce2f1f0c
David Cramer <dcramer@gmail.com>
2015-01-29 17:08:42 -0800

    Support removing tag keys via project settings

--- src/sentry/web/frontend/projects/tags.py
+++ src/sentry/web/frontend/projects/tags.py
@@ -13,23 +13,31 @@ from django.http import HttpResponseRedirect
 from django.utils.translation import ugettext_lazy as _
 
 from sentry.constants import MEMBER_ADMIN
-from sentry.models import TagKey
+from sentry.models import ProjectOption, TagKey, TagKeyStatus
 from sentry.web.decorators import has_access
-from sentry.web.forms.projects import ProjectTagsForm
 from sentry.web.helpers import render_to_response
 
 
 @has_access(MEMBER_ADMIN)
 def manage_project_tags(request, organization, project):
-    tag_list = TagKey.objects.all_keys(project)
+    tag_list = TagKey.objects.filter(
+        project=project,
+        status=TagKeyStatus.VISIBLE,
+    )
 
-    if tag_list:
-        form = ProjectTagsForm(project, tag_list, request.POST or None)
-    else:
-        form = None
+    enabled_filters = ProjectOption.objects.get_value(
+        project, 'tags', [t.key for t in tag_list])
+    enabled_annotations = ProjectOption.objects.get_value(
+        project, 'annotations', ['sentry:user'])
 
-    if form and form.is_valid():
-        form.save()
+    if request.POST:
+        filters = request.POST.getlist('filters')
+        ProjectOption.objects.set_value(
+            project, 'tags', filters)
+
+        annotations = request.POST.getlist('annotations')
+        ProjectOption.objects.set_value(
+            project, 'annotations', annotations)
 
         messages.add_message(
             request, messages.SUCCESS,
@@ -40,9 +48,10 @@ def manage_project_tags(request, organization, project):
     context = {
         'organization': organization,
         'team': project.team,
-        'tag_list': tag_list,
-        'page': 'tags',
         'project': project,
-        'form': form,
+        'tag_list': tag_list,
+        'enabled_filters': enabled_filters,
+        'enabled_annotations': enabled_annotations,
+        'page': 'tags',
     }
     return render_to_response('sentry/projects/manage_tags.html', context, request)
