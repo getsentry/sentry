6a104471d7e85dcb22ff094d970fb0eb9429074a
ckj <chriskjennings@gmail.com>
2015-02-04 12:01:24 -0500

    Style activity

--- src/sentry/static/sentry/app/views/aggregate/activity.jsx
+++ src/sentry/static/sentry/app/views/aggregate/activity.jsx
@@ -10,19 +10,21 @@ var AggregateActivity = React.createClass({
 
   render: function() {
     return (
-      <div>
+      <div className="activity">
         <h5>Timeline</h5>
         <div className="activity-field">
-          <input />
+          <textarea className="form-control" placeholder="Type a note and press enter" />
         </div>
         <ul className="activity">
           <li className="activity-item">
             <img className="avatar" src="" />
+            <time>just now</time>
             <h6><a href="#">David Cramer</a></h6>
             <p>This seems fixed in riak-2.2.0. That is, it will likely still error somehow, but I think they addressed the BadStatusLine stuff.</p>
           </li>
           <li className="activity-item">
             <img className="avatar" src="" />
+            <time>2h ago</time>
             <h6><a href="#">Sentry</a></h6>
             <p>Heads up, we just saw this event for the first time.</p>
           </li>
