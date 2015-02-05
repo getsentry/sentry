1ca85675084aad3d4a627d2b448af4417bdbf28b
David Cramer <dcramer@gmail.com>
2015-02-05 08:01:41 -0800

    Basic loading of tag data

--- src/sentry/static/sentry/app/views/aggregateTags.jsx
+++ src/sentry/static/sentry/app/views/aggregateTags.jsx
@@ -3,6 +3,7 @@
 var React = require("react");
 var Router = require("react-router");
 
+var api = require("../api");
 var PropTypes = require("../proptypes");
 
 var AggregateTags = React.createClass({
@@ -12,10 +13,53 @@ var AggregateTags = React.createClass({
     aggregate: PropTypes.Aggregate.isRequired
   },
 
+  getInitialState() {
+    return {
+      tagList: null,
+      loading: true
+    };
+  },
+
+  componentWillMount() {
+    this.fetchData();
+  },
+
+  fetchData() {
+    var params = this.getParams();
+
+    this.setState({loading: true});
+
+    api.request('/groups/' + params.aggregateId + '/tags/', {
+      success: (data) => {
+        this.setState({tagList: data});
+      },
+      error: () => {
+        // TODO(dcramer):
+      },
+      complete: () => {
+        this.setState({loading: false});
+      }
+    });
+  },
+
+
   render() {
+    var children = [];
+
+    if (this.state.tagList) {
+      children = this.state.tagList.map((tag) => {
+        return (
+          <li>{tag.name}</li>
+        );
+      });
+    }
+
     return (
       <div>
-      Tags
+        Tags
+        <ul>
+          {children}
+        </ul>
       </div>
     );
   }
