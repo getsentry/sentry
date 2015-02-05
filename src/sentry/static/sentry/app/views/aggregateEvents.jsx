004fe00f48ba855e4f5a819592206c8137d867d6
David Cramer <dcramer@gmail.com>
2015-02-05 10:39:20 -0800

    Add basic data to events

--- src/sentry/static/sentry/app/views/aggregateEvents.jsx
+++ src/sentry/static/sentry/app/views/aggregateEvents.jsx
@@ -3,6 +3,7 @@
 var React = require("react");
 var Router = require("react-router");
 
+var api = require("../api");
 var PropTypes = require("../proptypes");
 
 var AggregateEvents = React.createClass({
@@ -12,10 +13,55 @@ var AggregateEvents = React.createClass({
     aggregate: PropTypes.Aggregate.isRequired
   },
 
+  getInitialState() {
+    return {
+      eventList: null,
+      loading: true
+    };
+  },
+
+  componentWillMount() {
+    this.fetchData();
+  },
+
+  fetchData() {
+    var params = this.getParams();
+
+    this.setState({loading: true});
+
+    api.request('/groups/' + params.aggregateId + '/events/', {
+      success: (data) => {
+        this.setState({eventList: data});
+      },
+      error: () => {
+        // TODO(dcramer):
+      },
+      complete: () => {
+        this.setState({loading: false});
+      }
+    });
+  },
+
+
   render() {
+    var children = [];
+
+    if (this.state.eventList) {
+      children = this.state.eventList.map((event, eventIdx) => {
+        return (
+          <tr key={eventIdx}>
+            <td>{event.message}</td>
+          </tr>
+        );
+      });
+    }
+
     return (
       <div>
-      Events
+        Events
+        <table>
+          {children}
+        </table>
       </div>
     );
   }
