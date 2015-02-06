0f3441288082fbea78b62c5e4df918f8925468f7
David Cramer <dcramer@gmail.com>
2015-02-05 18:24:56 -0800

    Some basic for team dashboard

--- src/sentry/static/sentry/app/views/teamDashboard.jsx
+++ src/sentry/static/sentry/app/views/teamDashboard.jsx
@@ -5,16 +5,47 @@ var Router = require("react-router");
 
 var TeamState = require("../mixins/teamState");
 
+var EventList = require("./teamDashboard/eventList");
+
 var TeamDashboard = React.createClass({
   mixins: [
     Router.State,
     TeamState
   ],
 
+  getTrendingEventsEndpoint() {
+    var params = this.getParams();
+    return "/teams/" + params.orgId + "/" + params.teamId + "/groups/trending/";
+  },
+
+  getNewEventsEndpoint() {
+    var params = this.getParams();
+    return "/teams/" + params.orgId + "/" + params.teamId + "/groups/new/";
+  },
+
   render() {
-    var team = this.getTeam();
     return (
-      <div>{team.name}</div>
+      <div>
+        <div className="box">
+          <div className="box-header">
+            <h3>Last 7 days</h3>
+          </div>
+          <div className="box-content with-padding">
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <EventList
+                title="Trends"
+                endpoint={this.getTrendingEventsEndpoint()} />
+          </div>
+          <div className="col-md-6">
+            <EventList
+                title="New Events"
+                endpoint={this.getNewEventsEndpoint()} />
+          </div>
+        </div>
+      </div>
     );
   }
 });
