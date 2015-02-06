8b21bba13e7f6284f1c740b6b531786891db8caa
ckj <chriskjennings@gmail.com>
2015-02-06 17:20:30 -0500

    Clean up tags. Replace progressbar with tag-bar.

--- src/sentry/static/sentry/app/views/aggregateTags.jsx
+++ src/sentry/static/sentry/app/views/aggregateTags.jsx
@@ -69,33 +69,38 @@ var AggregateTags = React.createClass({
         var valueChildren = tag.topValues.map((tagValue, tagValueIdx) => {
           return (
             <li key={tagValueIdx}>
-              <a href="">
-                {tagValue.value}
-                <span><Count value={tagValue.count} /></span>
+              <a className="tag-bar" href="">
+                <span className="tag-bar-background" style={{width: '10%'}}></span>
+                <span className="tag-bar-label">{tagValue.value}</span>
+                <span className="tag-bar-count"><Count value={tagValue.count} /></span>
               </a>
             </li>
           );
         });
 
         return (
-          <div className="span6" key={tagIdx}>
-            <div className="page-header">
-              <span className="pull-right">
-                <a href="">More Details</a>
-              </span>
-              <h4>{tag.name} <small><Count value={tag.totalValues} /></small></h4>
-            </div>
+          <div className="col-md-6" key={tagIdx}>
+            <div className="box">
+              <div className="box-content with-padding">
+                <div className="page-header">
+                  <span className="pull-right">
+                    <a href="">More Details</a>
+                  </span>
+                  <h5>{tag.name} <small><Count value={tag.totalValues} /></small></h5>
+                </div>
 
-            <ul>
-              {valueChildren}
-            </ul>
+                <ul className="list-unstyled">
+                  {valueChildren}
+                </ul>
+              </div>
+            </div>
           </div>
         );
       });
     }
 
     return (
-      <div>
+      <div className="row">
         {children}
       </div>
     );
