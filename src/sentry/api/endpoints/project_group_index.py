9df382961f2570aa98144aaa738d291c2ba6981a
David Cramer <dcramer@gmail.com>
2015-02-06 13:38:27 -0800

    Remove print

--- src/sentry/api/endpoints/project_group_index.py
+++ src/sentry/api/endpoints/project_group_index.py
@@ -121,7 +121,6 @@ class ProjectGroupIndexEndpoint(Endpoint):
             query_kwargs['cursor'] = Cursor.from_string(cursor)
 
         query = request.GET.get('query', 'is:unresolved')
-        print (query)
         if query is not None:
             query_kwargs.update(parse_query(query, request.user))
 
