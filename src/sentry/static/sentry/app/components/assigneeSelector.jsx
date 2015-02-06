9d84d071fb3a8e514e302bc044695b61b11d4eb8
ckj <chriskjennings@gmail.com>
2015-02-06 17:38:19 -0500

    Style clear assignee

--- src/sentry/static/sentry/app/components/assigneeSelector.jsx
+++ src/sentry/static/sentry/app/components/assigneeSelector.jsx
@@ -6,7 +6,6 @@ var Reflux = require("reflux");
 var api = require("../api");
 var AggregateListStore = require("../stores/aggregateListStore");
 var DropdownLink = require("./dropdownLink");
-var Gravatar = require("./gravatar");
 var MenuItem = require("./menuItem");
 var PropTypes = require("../proptypes");
 
@@ -18,66 +17,35 @@ var AssigneeSelector = React.createClass({
     memberList: React.PropTypes.instanceOf(Array).isRequired,
   },
 
-  getInitialState() {
-    return {
-      filterQuery: ''
-    };
-  },
-
-  assignTo(member) {
+  assignTo: function(member) {
     api.assignTo({id: this.props.aggregate.id, email: member.email});
-    this.setState({filterQuery: ''});
   },
 
-  clearAssignTo() {
+  clearAssignTo: function() {
     api.assignTo({id: this.props.aggregate.id, email: ''});
-    this.setState({filterQuery: ''});
   },
 
-  onChangeFilter() {
-    this.setState({
-      filterQuery: event.target.value
-    });
-  },
-
-  onDropdownOpen() {
-    this.refs.filter.getDOMNode().focus();
-  },
-
-  onDropdownClose() {
-    this.setState({
-      filterQuery: ''
-    });
-  },
-
-  render() {
+  render: function() {
     var agg = this.props.aggregate;
+
     var loading = AggregateListStore.hasStatus(agg.id, 'assignTo');
 
     var className = "assignee-selector anchor-right";
     if (!agg.assignedTo) {
       className += " unassigned";
     }
-    var filterQuery = this.state.filterQuery;
-    var memberNodes = this.props.memberList.filter((item) => {
-      if (item.email.indexOf(filterQuery) !== -1) {
-        return true;
-      }
-      if (item.name && item.name.indexOf(filterQuery) !== -1) {
-        return true;
-      }
-      return false;
-    }).map((item) => {
-      return (
+
+    var memberNodes = [];
+    this.props.memberList.forEach(function(item){
+      memberNodes.push(
         <MenuItem key={item.id}
                   disabled={!loading}
                   onSelect={this.assignTo.bind(this, item)} >
-          <Gravatar email={item.email} className="avatar"
-                    size={16} />
+          <img src={item.avatarUrl} className="avatar" />
           {item.name || item.email}
         </MenuItem>
       );
-    });
+    }.bind(this));
 
     return (
       <div className={className}>
@@ -86,23 +54,19 @@ var AssigneeSelector = React.createClass({
         :
           <DropdownLink
             className="assignee-selector-toggle"
-            onOpen={this.onDropdownOpen}
-            onClose={this.onDropdownClose}
             title={agg.assignedTo ?
               <img src={agg.assignedTo.avatarUrl} className="avatar" />
               :
               <span className="icon-user" />
             }>
             <MenuItem noAnchor={true} key="filter">
-              <input type="text" className="form-control input-sm"
-                     ref="filter" placeholder="Filter people"
-                     onChange={this.onChangeFilter} />
+              <input type="text" className="form-control input-sm" placeholder="Filter people" />
             </MenuItem>
             {agg.assignedTo ?
-              <MenuItem key="clear"
+              <MenuItem className="clear-assignee" key="clear"
                         disabled={!loading}
                         onSelect={this.clearAssignTo}>
-                Clear Assignee
+                <span className="icon-close"/> Clear Assignee
               </MenuItem>
             : ''}
             {memberNodes}
