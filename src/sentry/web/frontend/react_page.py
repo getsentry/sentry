295bce014a41c1468647c547f43532e4f4e66722
David Cramer <dcramer@gmail.com>
2015-02-11 12:09:17 -0800

    Switch to extract_lazy_object

--- src/sentry/web/frontend/react_page.py
+++ src/sentry/web/frontend/react_page.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from sentry.web.frontend.base import OrganizationView
+from sentry.utils.functional import extract_lazy_object
 
 
 # TODO(dcramer): once we implement basic auth hooks in React we can make this
@@ -9,6 +10,6 @@ class ReactPageView(OrganizationView):
     def handle(self, request, **kwargs):
         if request.user.is_authenticated():
             # remove lazy eval
-            request.user = request.user._wrapped
+            request.user = extract_lazy_object(request.user)
 
         return self.respond('sentry/bases/react.html')
