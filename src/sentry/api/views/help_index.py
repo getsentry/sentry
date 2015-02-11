673062a991e25243ef9e2f554e9ef905ece9b3a7
David Cramer <dcramer@gmail.com>
2015-02-11 12:21:15 -0800

    Add URL prefix to api overview

--- src/sentry/api/views/help_index.py
+++ src/sentry/api/views/help_index.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+from django.conf import settings
+
 from sentry.api.views.help_base import ApiHelpBase
 from sentry.web.helpers import render_to_response
 
@@ -11,7 +13,8 @@ class ApiHelpIndexView(ApiHelpBase):
         prefix = '/api/0/'
 
         context = {
-            'section_list': self.get_sections(prefix)
+            'section_list': self.get_sections(prefix),
+            'SENTRY_URL_PREFIX': settings.SENTRY_URL_PREFIX,
         }
 
         return render_to_response('sentry/help/api_index.html', context, request)
