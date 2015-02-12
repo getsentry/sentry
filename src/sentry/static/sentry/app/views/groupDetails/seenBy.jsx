9d027219e3a3e54baeda3ea0d49188d5bae8caf9
David Cramer <dcramer@gmail.com>
2015-02-12 11:42:02 -0800

    Use real data for seen by

--- src/sentry/static/sentry/app/views/groupDetails/seenBy.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/seenBy.jsx
@@ -2,12 +2,32 @@
 
 var React = require("react");
 
+var Gravatar = require("../../components/gravatar");
+var GroupState = require("../../mixins/groupState");
+
 var GroupSeenBy = React.createClass({
+  mixins: [GroupState],
+
   render() {
+    var group = this.getGroup();
+
+    var seenByNodes = group.seenBy.map((user, userIdx) => {
+      return (
+        <li key={userIdx}>
+          <Gravatar size={24} email={user.email} />
+        </li>
+      );
+    });
+
+    if (!seenByNodes) {
+      return <div />;
+    }
+
     return (
       <div className="seen-by">
         <ul>
           <li>Seen by:</li>
+          {seenByNodes}
         </ul>
       </div>
     );
