cd11dce7ed3acba690176fbc3e72786756ee77bf
David Cramer <dcramer@gmail.com>
2015-02-12 18:10:50 -0800

    Remove use of react-bootstrap on BarChart

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,8 @@ module.exports = {
   entry: {
     "app": "app",
     "vendor": [
+      "bootstrap/js/dropdown",
+      "bootstrap/js/tooltip",
       "crypto-js/md5",
       "jquery",
       "moment",
