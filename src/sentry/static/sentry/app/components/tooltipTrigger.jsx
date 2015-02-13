6a1eee1e1f8f700dbe09f987dc8ccd80ebe6e9e4
David Cramer <dcramer@gmail.com>
2015-02-12 18:21:07 -0800

    Add default viewport selector/padding

--- src/sentry/static/sentry/app/components/tooltipTrigger.jsx
+++ src/sentry/static/sentry/app/components/tooltipTrigger.jsx
@@ -7,31 +7,42 @@ require("bootstrap/js/tooltip");
 var TooltipTrigger = React.createClass({
   propTypes: {
     title: React.PropTypes.node.isRequired,
-    placement: React.PropTypes.string
+    placement: React.PropTypes.string,
+    viewport: React.PropTypes.shape({
+      selector: React.PropTypes.string,
+      padding: React.PropTypes.number
+    })
   },
 
   getDefaultProps() {
     return {
-      placement: 'left'
+      placement: "left",
+      viewport: {
+        selector: "body .app",
+        padding: 5
+      }
     };
   },
 
   componentDidMount() {
     // These can be configured via options; this is just a demo
     $(this.getDOMNode()).tooltip({
+      html: true,
       placement: this.props.placement,
       title: React.renderToString(this.props.title),
-      html: true
+      viewport: this.props.viewport
     });
   },
 
   componentWillUnmount() {
     var node = $(this.getDOMNode());
-    node.tooltip('destroy');
-    node.unbind('show.bs.tooltip', 'shown.bs.tooltip', 'hide.bs.tooltip', 'hidden.bs.tooltip');
+    node.tooltip("destroy");
+    node.unbind("show.bs.tooltip", "shown.bs.tooltip", "hide.bs.tooltip", "hidden.bs.tooltip");
   },
 
   render() {
     return this.props.children;
   }
 });
+
+module.exports = TooltipTrigger;
