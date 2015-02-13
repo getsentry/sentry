09858f5687ce575f75cf0e011334c232830064ef
David Cramer <dcramer@gmail.com>
2015-02-12 16:25:08 -0800

    Move stats bar into contained component

---
+++ src/sentry/static/sentry/app/views/teamDashboard/statsBar.jsx
@@ -0,0 +1,30 @@
+/*** @jsx React.DOM */
+
+var React = require("react");
+
+var TeamStatsBar = React.createClass({
+  render() {
+    return (
+      <div className="row team-stats">
+        <div className="col-md-3 stat-column">
+          <span className="count">323</span>
+          <span className="count-label">events seen</span>
+        </div>
+        <div className="col-md-3 stat-column">
+          <span className="count">137</span>
+          <span className="count-label">new events</span>
+        </div>
+        <div className="col-md-3 stat-column">
+          <span className="count">16</span>
+          <span className="count-label">releases</span>
+        </div>
+        <div className="col-md-3 stat-column align-right bad">
+          <span className="count">20%</span>
+          <span className="count-label">more than last week</span>
+        </div>
+      </div>
+    );
+  }
+});
+
+module.exports = TeamStatsBar;
