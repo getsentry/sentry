a8dfb7e066f9d319b73294e05cd4b1cac32a3206
David Cramer <dcramer@gmail.com>
2015-02-13 11:49:04 -0800

    Remove access group creation endpoint

--- tests/sentry/api/endpoints/test_team_access_group_index.py
+++ tests/sentry/api/endpoints/test_team_access_group_index.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 from django.core.urlresolvers import reverse
-from sentry.constants import MEMBER_USER
 from sentry.models import AccessGroup
 from sentry.testutils import APITestCase
 
@@ -23,23 +22,3 @@ class TeamAccessGroupIndexTest(APITestCase):
         assert len(response.data) == 2
         assert response.data[0]['id'] == str(group_1.id)
         assert response.data[1]['id'] == str(group_2.id)
-
-
-class TeamAccessGroupCreateTest(APITestCase):
-    def test_simple(self):
-        self.login_as(user=self.user)
-        team = self.create_team(slug='baz')
-        url = reverse('sentry-api-0-team-access-group-index', kwargs={
-            'organization_slug': team.organization.slug,
-            'team_slug': team.slug,
-        })
-        resp = self.client.post(url, data={
-            'name': 'hello world',
-            'type': 'user',
-        })
-        assert resp.status_code == 201, resp.content
-        access_group = AccessGroup.objects.get(id=resp.data['id'])
-        assert access_group.name == 'hello world'
-        assert access_group.type == MEMBER_USER
-        assert access_group.managed is False
-        assert access_group.team == team
