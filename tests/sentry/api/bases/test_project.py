21bbc6cef5f41e935b22f2366290ba4960d44dd3
David Cramer <dcramer@gmail.com>
2015-02-18 09:57:22 -0800

    Basic tests for project permissions

---
+++ tests/sentry/api/bases/test_project.py
@@ -0,0 +1,88 @@
+from __future__ import absolute_import
+
+from mock import Mock
+
+from sentry.api.bases.project import ProjectPermission
+from sentry.models import ApiKey, OrganizationMemberType, ProjectKey
+from sentry.testutils import TestCase
+
+
+class ProjectPermissionBase(TestCase):
+    def setUp(self):
+        self.org = self.create_organization()
+        self.team = self.create_team(organization=self.org)
+        self.project = self.create_project(organization=self.org)
+        super(ProjectPermissionBase, self).setUp()
+
+    def has_object_perm(self, auth, user, obj, method='GET'):
+        perm = ProjectPermission()
+        request = Mock()
+        request.auth = auth
+        request.user = user
+        request.method = method
+        return perm.has_object_permission(request, None, obj)
+
+
+class ProjectPermissionTest(ProjectPermissionBase):
+    def test_regular_user(self):
+        user = self.create_user()
+        assert not self.has_object_perm(None, user, self.project)
+
+    def test_superuser(self):
+        user = self.create_user(is_superuser=True)
+        assert self.has_object_perm(None, user, self.project)
+
+    def test_org_member_without_team_access(self):
+        user = self.create_user()
+        om = self.create_member(
+            user=user,
+            organization=self.org,
+            type=OrganizationMemberType.MEMBER,
+            has_global_access=False,
+        )
+        assert not self.has_object_perm(None, user, self.project)
+
+    def test_org_member_with_global_access(self):
+        user = self.create_user()
+        om = self.create_member(
+            user=user,
+            organization=self.org,
+            type=OrganizationMemberType.MEMBER,
+            has_global_access=True,
+        )
+        assert self.has_object_perm(None, user, self.project)
+
+    def test_org_member_with_team_access(self):
+        user = self.create_user()
+        om = self.create_member(
+            user=user,
+            organization=self.org,
+            type=OrganizationMemberType.MEMBER,
+            has_global_access=False,
+        )
+        om.teams.add(self.team)
+        assert self.has_object_perm(None, user, self.project)
+
+    def test_project_key_with_project_access(self):
+        key = ProjectKey.objects.create(
+            project=self.project,
+        )
+        assert self.has_object_perm(key, None, self.project)
+
+    def test_project_key_without_project_access(self):
+        key = ProjectKey.objects.create(
+            project=self.create_project(organization=self.org),
+        )
+        assert not self.has_object_perm(key, None, self.project)
+
+    def test_api_key_with_org_access(self):
+        key = ApiKey.objects.create(
+            organization=self.org,
+        )
+        assert self.has_object_perm(key, None, self.project)
+
+    def test_api_key_without_org_access(self):
+        key = ApiKey.objects.create(
+            organization=self.create_organization(),
+        )
+        assert not self.has_object_perm(key, None, self.project)
