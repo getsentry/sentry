247ef82a69c2b83926043f92e795d5fa904d990f
David Cramer <dcramer@gmail.com>
2015-02-18 15:50:42 -0800

    Remove redirect in favor of internal query

--- src/sentry/api/endpoints/group_events_latest.py
+++ src/sentry/api/endpoints/group_events_latest.py
@@ -1,8 +1,6 @@
 from __future__ import absolute_import
 
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-
+from sentry.api import client
 from sentry.api.bases.group import GroupEndpoint
 
 
@@ -10,6 +8,4 @@ class GroupEventsLatestEndpoint(GroupEndpoint):
     def get(self, request, group):
         event = group.get_latest_event()
 
-        return HttpResponseRedirect(reverse('sentry-api-0-event-details', kwargs={
-            'event_id': event.id,
-        }))
+        return client.get('/events/{}/'.format(event.id), request.user, request.auth)
