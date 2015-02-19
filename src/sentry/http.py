1b52565fafb7490a53e7848393c317fc224fbc37
David Cramer <dcramer@gmail.com>
2015-02-19 15:23:36 -0800

    Expand accepted args of safe_urlopen

--- src/sentry/http.py
+++ src/sentry/http.py
@@ -57,20 +57,41 @@ def build_session():
     return session
 
 
-def safe_urlopen(url, params=None, data=None, headers=None, allow_redirects=False,
-                 timeout=30):
+def safe_urlopen(url, method=None, params=None, data=None, json=None,
+                 headers=None, allow_redirects=False, timeout=30):
     """
     A slightly safer version of ``urlib2.urlopen`` which prevents redirection
     and ensures the URL isn't attempting to hit a blacklisted IP range.
     """
 
     session = build_session()
+
+    kwargs = {}
+
+    if json:
+        kwargs['json'] = json
+        if not headers:
+            headers = {}
+        headers.setdefault('Content-Type', 'application/json')
+
     if data:
-        method = session.post
-    else:
-        method = session.get
-    return method(url, headers=headers, params=params, data=data, stream=True,
-                  allow_redirects=allow_redirects, timeout=timeout)
+        kwargs['data'] = data
+
+    if params:
+        kwargs['params'] = params
+
+    if headers:
+        kwargs['headers'] = headers
+
+    if method is None:
+        method = 'POST' if (data or json) else 'GET'
+
+    return getattr(session, method.lower())(
+        url,
+        allow_redirects=allow_redirects,
+        timeout=timeout,
+        **kwargs
+    )
 
 
 def safe_urlread(response):
