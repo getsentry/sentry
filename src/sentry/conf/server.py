17be420953de02ca76ad114a47d33649417a6ea3
David Cramer <dcramer@gmail.com>
2015-02-19 17:31:31 -0800

    Update gunicorn usage and improve how defaults are set

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -581,10 +581,7 @@ SENTRY_MAX_SAMPLE_TIME = 10000
 # Web Service
 SENTRY_WEB_HOST = 'localhost'
 SENTRY_WEB_PORT = 9000
-SENTRY_WEB_OPTIONS = {
-    'workers': 3,
-    'limit_request_line': 0,  # required for raven-js
-}
+SENTRY_WEB_OPTIONS = {}
 
 # UDP Service
 SENTRY_UDP_HOST = 'localhost'
