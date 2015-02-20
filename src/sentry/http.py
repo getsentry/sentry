00294d3c16662d77b969be02d79e990f0445812d
David Cramer <dcramer@gmail.com>
2015-02-20 11:06:36 -0800

    Dont verify SSL on source fetching

--- src/sentry/http.py
+++ src/sentry/http.py
@@ -59,7 +59,8 @@ def build_session():
 
 
 def safe_urlopen(url, method=None, params=None, data=None, json=None,
-                 headers=None, allow_redirects=False, timeout=30):
+                 headers=None, allow_redirects=False, timeout=30,
+                 verify_ssl=True):
     """
     A slightly safer version of ``urlib2.urlopen`` which prevents redirection
     and ensures the URL isn't attempting to hit a blacklisted IP range.
@@ -91,6 +92,7 @@ def safe_urlopen(url, method=None, params=None, data=None, json=None,
         url,
         allow_redirects=allow_redirects,
         timeout=timeout,
+        verify=verify_ssl,
         **kwargs
     )
 
