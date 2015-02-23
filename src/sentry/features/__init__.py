bf9120e199cda380fc35e1f0586af9555582bbcc
David Cramer <dcramer@gmail.com>
2015-02-23 13:45:34 -0800

    Add feature abstraction
    This adds a new abstraction for determining if a feature is enabled. This will slowly expand to encompass a number of things, and will provide a second layer hook to permissions.
    
    For example, getsentry.com might want to say "this organization cannot create teams". That's not so much permission related (and the permission system isn't great). This also makes it much simpler to provide a layer that would abstract this into something like Gargoyle.

---
+++ src/sentry/features/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from .base import *  # NOQA
+from .manager import *  # NOQA
+
+
+default_manager = FeatureManager()
+default_manager.add('auth:register')
+default_manager.add('social-auth:register')
+
+# expose public api
+add = default_manager.add
+get = default_manager.get
+has = default_manager.has
