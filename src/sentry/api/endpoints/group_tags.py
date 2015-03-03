68437e5c4f32242256f173df4fe8c5a9b59c81bb
David Cramer <dcramer@gmail.com>
2015-03-03 14:38:44 -0800

    Add tag key details endpoint

--- src/sentry/api/endpoints/group_tags.py
+++ src/sentry/api/endpoints/group_tags.py
@@ -1,42 +1,14 @@
 from __future__ import absolute_import
 
 from datetime import timedelta
-from django.db import connections
-from django.db.models import Sum
 from django.utils import timezone
 from rest_framework.response import Response
 
 from sentry.api.bases.group import GroupEndpoint
 from sentry.models import GroupTagValue, GroupTagKey, TagKey
-from sentry.utils import db
 
 
 class GroupTagsEndpoint(GroupEndpoint):
-    def _get_value_count(self, group_id, key):
-        if db.is_postgres():
-            # This doesnt guarantee percentage is accurate, but it does ensure
-            # that the query has a maximum cost
-            cursor = connections['default'].cursor()
-            cursor.execute("""
-                SELECT SUM(t)
-                FROM (
-                    SELECT times_seen as t
-                    FROM sentry_messagefiltervalue
-                    WHERE group_id = %s
-                    AND key = %s
-                    AND last_seen > NOW() - INTERVAL '7 days'
-                    LIMIT 10000
-                ) as a
-            """, [group_id, key])
-            return cursor.fetchone()[0] or 0
-
-        cutoff = timezone.now() - timedelta(days=7)
-        return GroupTagValue.objects.filter(
-            group=group_id,
-            key=key,
-            last_seen__gte=cutoff,
-        ).aggregate(t=Sum('times_seen'))['t']
-
     def _get_top_values(self, group_id, key, num=5):
         cutoff = timezone.now() - timedelta(days=7)
         return GroupTagValue.objects.filter(
@@ -56,11 +28,11 @@ class GroupTagsEndpoint(GroupEndpoint):
         # O(N) db access
         data = []
         for tag_key in tag_keys:
-            total_values = self._get_value_count(group.id, tag_key.key)
+            total_values = GroupTagValue.get_value_count(group.id, tag_key.key)
             top_values = self._get_top_values(group.id, tag_key.key)
 
             data.append({
-                'id': tag_key.id,
+                'id': str(tag_key.id),
                 'key': tag_key.key,
                 'name': tag_key.get_label(),
                 'uniqueValues': tag_key.values_seen,
