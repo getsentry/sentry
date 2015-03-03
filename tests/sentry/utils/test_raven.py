e1eafd9c25ae89b7e1ccaf114b781a8ffd3bc00d
David Cramer <dcramer@gmail.com>
2015-03-03 12:37:03 -0800

    Correct inverted logic in internal client

---
+++ tests/sentry/utils/test_raven.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, print_function
+
+from mock import Mock, patch
+from raven.contrib.django.models import client
+
+from sentry.models import Event
+from sentry.testutils import TestCase
+from sentry.utils.raven import SentryInternalClient
+
+
+class SentryInternalClientTest(TestCase):
+    @patch.object(SentryInternalClient, 'is_enabled', Mock(return_value=True))
+    def test_simple(self):
+        assert client.__class__ is SentryInternalClient
+
+        with self.settings(CELERY_ALWAYS_EAGER=True):
+            client.captureMessage('internal client test')
+
+        event = Event.objects.get()
+        assert event.message == 'internal client test'
