4bd188770909bb95614eaf6b063bb2fd810a4211
David Cramer <dcramer@gmail.com>
2015-03-02 16:44:30 -0800

    Remove livereload

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,6 @@ var gulp = require("gulp"),
     gp_clean = require("gulp-clean"),
     gp_concat = require("gulp-concat"),
     gp_less = require("gulp-less"),
-    gp_livereload = require("gulp-livereload"),
     gp_rename = require("gulp-rename"),
     gp_uglify = require("gulp-uglify"),
     gp_util = require("gulp-util"),
@@ -103,26 +102,18 @@ gulp.task("watch:css:wall", function(){
 
 gulp.task("watch:css", ["watch:css:sentry", "watch:css:wall"]);
 
-gulp.task("watch:templates", function(){
-  return gp_watch(path.join(__dirname, 'src/sentry/templates/**'), function(){
-    gp_livereload.changed('.');
-  });
-});
-
 // TODO(dcramer): this is causing issues, use webpack --watch for now
 gulp.task("watch:webpack", function(callback){
   var config = require('./webpack.config.js');
   webpack(config).watch(200, function(err, stats) {
     if(err) throw new gutil.PluginError("webpack", err);
     gp_util.log("[webpack]", stats.toString(webpackStatsOptions));
-    gp_livereload.changed('.');
   });
   callback();
 });
 
 gulp.task("watch", function(){
-  gp_livereload.listen();
-  return gulp.start(["watch:css", "watch:webpack", "watch:templates"]);
+  return gulp.start(["watch:css", "watch:webpack"]);
 });
 
 gulp.task("default", ["dist"]);
