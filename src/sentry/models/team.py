b747258bbbaa8566812a8df1ad4a7f2ea18eb2d1
David Cramer <dcramer@gmail.com>
2015-03-03 11:22:42 -0800

    Correct AuthIdentity queries to include user

--- src/sentry/models/team.py
+++ src/sentry/models/team.py
@@ -196,6 +196,7 @@ class Team(Model):
         try:
             auth_identity = AuthIdentity.objects.get(
                 auth_provider__organization=self.organization_id,
+                user=member.user_id,
             )
         except AuthIdentity.DoesNotExist:
             return True
