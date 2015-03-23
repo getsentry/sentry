cf8bd373cd9d83e2300ce9e5fb6bb381c1960a9c
David Cramer <dcramer@gmail.com>
2015-03-23 14:38:30 -0700

    Handle no latest event

--- src/sentry/api/endpoints/group_events_latest.py
+++ src/sentry/api/endpoints/group_events_latest.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+from rest_framework.response import Response
+
 from sentry.api import client
 from sentry.api.bases.group import GroupEndpoint
 
@@ -7,5 +9,7 @@ from sentry.api.bases.group import GroupEndpoint
 class GroupEventsLatestEndpoint(GroupEndpoint):
     def get(self, request, group):
         event = group.get_latest_event()
+        if not event:
+            return Response({'detail': 'No events found'}, status=404)
 
         return client.get('/events/{}/'.format(event.id), request.user, request.auth)
