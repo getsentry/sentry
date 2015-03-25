19edc33ec85bc2285fbdaccac438bac8b4b14833
David Cramer <dcramer@gmail.com>
2015-03-25 16:12:18 -0700

    Initial working pass at stream manager

---
+++ src/sentry/static/sentry/app/utils/parseLinkHeader.jsx
@@ -0,0 +1,23 @@
+/*** @jsx React.DOM */
+
+module.exports = function(header) {
+  if (header === null) {
+    return {};
+  }
+
+  var header_vals = header.split(','),
+      links = {};
+
+  header_vals.forEach((val) => {
+    var match = /<([^>]+)>; rel="([^"]+)"(?:; results="([^"]+)")?(?:; cursor="([^"]+)")?/g.exec(val);
+    var hasResults = (match[3] === 'true' ? true : (match[3] === 'false' ? false : null));
+
+    links[match[2]] = {
+      href: match[1],
+      results: hasResults,
+      cursor: match[4]
+    };
+  });
+
+  return links;
+};
