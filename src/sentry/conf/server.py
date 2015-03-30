a10fb9ee179edc3930a5bfe1700301f13fcd3862
David Cramer <dcramer@gmail.com>
2015-03-30 13:29:04 -0700

    Explicitly only support UTC (refs GH-1472)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -82,13 +82,7 @@ if 'DATABASE_URL' in os.environ:
 
 EMAIL_SUBJECT_PREFIX = '[Sentry] '
 
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
+# This should always be UTC.
 TIME_ZONE = 'UTC'
 
 # Language code for this installation. All choices can be found here:
