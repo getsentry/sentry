a748fe7ef15e981e1f24f864ee46383d593d97f6
David Cramer <dcramer@gmail.com>
2015-04-07 13:19:01 -0700

    Add basic tests for organization login

---
+++ tests/sentry/web/frontend/test_organization_auth_login.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+
+from django.core.urlresolvers import reverse
+
+from sentry.models import AuthProvider
+from sentry.testutils import AuthProviderTestCase
+
+
+class OrganizationAuthSettingsTest(AuthProviderTestCase):
+    def test_renders_basic_login_form(self):
+        organization = self.create_organization(name='foo', owner=self.user)
+        team = self.create_team(organization=organization)
+        project = self.create_project(team=team)
+
+        path = reverse('sentry-auth-organization', args=[organization.slug])
+
+        self.login_as(self.user)
+
+        resp = self.client.get(path)
+
+        assert resp.status_code == 200
+
+        self.assertTemplateUsed(resp, 'sentry/organization-login.html')
+
+        assert resp.context['form']
+        assert 'provider_key' not in resp.context
+        assert resp.context['CAN_REGISTER']
+
+    def test_renders_auth_provider(self):
+        organization = self.create_organization(name='foo', owner=self.user)
+        team = self.create_team(organization=organization)
+        project = self.create_project(team=team)
+        auth_provider = AuthProvider.objects.create(
+            organization=organization,
+            provider='dummy',
+        )
+
+        path = reverse('sentry-auth-organization', args=[organization.slug])
+
+        self.login_as(self.user)
+
+        resp = self.client.get(path)
+
+        self.assertTemplateUsed(resp, 'sentry/organization-login.html')
+
+        assert 'form' not in resp.context
+        assert resp.context['provider_key'] == 'dummy'
+        assert not resp.context['CAN_REGISTER']
