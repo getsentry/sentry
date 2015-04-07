2bad18d9290b113e820701b2e3e54296135c3477
David Cramer <dcramer@gmail.com>
2015-04-06 22:36:23 -0700

    Remove second chart

--- src/sentry/static/sentry/app/views/groupDetails/chart.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/chart.jsx
@@ -10,24 +10,15 @@ var GroupChart = React.createClass({
 
   render: function() {
     var group = this.getGroup();
-    var stats_24h = group.stats['24h'];
-    var points_24h = stats_24h.map((point) => {
+    var stats = group.stats['24h'];
+    var points = stats.map((point) => {
       return {x: point[0], y: point[1]};
     });
-
-    var stats_30d = group.stats['30d'];
-    var points_30d = stats_30d.map((point) => {
-      return {x: point[0], y: point[1]};
-    });
-
 
     return (
       <div className="bar-chart group-chart">
-        <h6>Last 24 hours</h6>
-        <BarChart points={points_24h} className="sparkline" />
-
-        <h6>Last 30 Days</h6>
-        <BarChart points={points_30d} className="sparkline small" />
+        <h6>Last 24 Hours</h6>
+        <BarChart points={points} className="sparkline" />
       </div>
     );
   }
