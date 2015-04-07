de06547ead44327b0d7eb7817c36ae01173a0be7
David Cramer <dcramer@gmail.com>
2015-04-06 21:51:13 -0700

    Functional SSO redirection

--- src/sentry/web/frontend/auth_login.py
+++ src/sentry/web/frontend/auth_login.py
@@ -1,19 +1,54 @@
 from __future__ import absolute_import, print_function
 
+from django.contrib import messages
 from django.contrib.auth import login
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from django.utils.translation import ugettext_lazy as _
 
 from sentry import features
+from sentry.models import AuthProvider, Organization
 from sentry.web.forms.accounts import AuthenticationForm
 from sentry.web.frontend.base import BaseView
 from sentry.utils.auth import get_login_redirect
+
+ERR_NO_SSO = _('The organization does not exist or does not have Single Sign-On enabled.')
 
 
 class AuthLoginView(BaseView):
     auth_required = False
 
+    def get_auth_provider(self, organization_slug):
+        try:
+            organization = Organization.objects.get(
+                slug=organization_slug
+            )
+        except Organization.DoesNotExist:
+            return None
+
+        try:
+            auth_provider = AuthProvider.objects.get(
+                organization=organization
+            )
+        except AuthProvider.DoesNotExist:
+            return None
+
+        return auth_provider
+
     def handle(self, request):
         if request.user.is_authenticated():
             return self.redirect(get_login_redirect(request))
+
+        if request.POST.get('op') == 'sso' and request.POST.get('organization'):
+            auth_provider = self.get_auth_provider(request.POST['organization'])
+            if auth_provider:
+                next_uri = reverse('sentry-auth-organization',
+                                   args=[request.POST['organization']])
+            else:
+                next_uri = request.path
+                messages.add_message(request, messages.ERROR, ERR_NO_SSO)
+
+            return HttpResponseRedirect(next_uri)
 
         form = AuthenticationForm(request, request.POST or None,
                                   captcha=bool(request.session.get('needs_captcha')))
