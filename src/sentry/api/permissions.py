de2820b17af22e64ac00b1eba5d2832742d4ba59
David Cramer <dcramer@gmail.com>
2015-04-16 21:42:20 -0700

    Add internal stats endpoint

--- src/sentry/api/permissions.py
+++ src/sentry/api/permissions.py
@@ -52,3 +52,10 @@ class ScopedPermission(permissions.BasePermission):
 
     def is_project_key(self, request):
         return isinstance(request.auth, ProjectKey)
+
+
+class SuperuserPermission(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.user.is_superuser:
+            return True
+        return False
