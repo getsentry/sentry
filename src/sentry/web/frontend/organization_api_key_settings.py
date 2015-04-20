638b076ab0841cd7dbede7bbb0928ca68b1e8673
David Cramer <dcramer@gmail.com>
2015-04-20 13:55:39 -0700

    Basics for api creation/settings

--- src/sentry/web/frontend/organization_api_key_settings.py
+++ src/sentry/web/frontend/organization_api_key_settings.py
@@ -1,7 +1,15 @@
 from __future__ import absolute_import
 
+from django import forms
+
 from sentry.models import ApiKey, OrganizationMemberType
 from sentry.web.frontend.base import OrganizationView
+
+
+class ApiKeyForm(forms.ModelForm):
+    class Meta:
+        model = ApiKey
+        fields = ('label', 'scopes', 'allowed_origins')
 
 
 class OrganizationApiKeySettingsView(OrganizationView):
@@ -10,8 +18,11 @@ class OrganizationApiKeySettingsView(OrganizationView):
     def handle(self, request, organization, key_id):
         key = ApiKey.objects.get(organization=organization, id=key_id)
 
+        form = ApiKeyForm(request.POST or None, instance=key)
+
         context = {
             'key': key,
+            'form': form,
         }
 
         return self.respond('sentry/organization-api-key-settings.html', context)
