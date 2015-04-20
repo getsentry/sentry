28ca98b62ed8ae84e0fc8392cb969ebf34253b06
David Cramer <dcramer@gmail.com>
2015-04-20 14:20:41 -0700

    Improve origins ux

--- src/sentry/web/frontend/organization_api_key_settings.py
+++ src/sentry/web/frontend/organization_api_key_settings.py
@@ -1,12 +1,17 @@
 from __future__ import absolute_import
 
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 from sentry.models import ApiKey, OrganizationMemberType
+from sentry.web.forms.fields import OriginsField
 from sentry.web.frontend.base import OrganizationView
 
 
 class ApiKeyForm(forms.ModelForm):
+    allowed_origins = OriginsField(label=_('Allowed Domains'), required=False,
+        help_text=_('Separate multiple entries with a newline.'))
+
     class Meta:
         model = ApiKey
         fields = ('label', 'scopes', 'allowed_origins')
