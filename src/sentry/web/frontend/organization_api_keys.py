c947026058ef57ee5927ddcb85c6674193c8be5f
David Cramer <dcramer@gmail.com>
2015-04-20 13:55:39 -0700

    Basics for api creation/settings

--- src/sentry/web/frontend/organization_api_keys.py
+++ src/sentry/web/frontend/organization_api_keys.py
@@ -1,5 +1,8 @@
 from __future__ import absolute_import
 
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+
 from sentry.models import ApiKey, OrganizationMemberType
 from sentry.web.frontend.base import OrganizationView
 
@@ -8,6 +11,13 @@ class OrganizationApiKeysView(OrganizationView):
     required_access = OrganizationMemberType.ADMIN
 
     def handle(self, request, organization):
+        if request.POST.get('op') == 'newkey':
+            key = ApiKey.objects.create(organization=organization)
+            redirect_uri = reverse('sentry-organization-api-key-settings', args=[
+                organization.slug, key.id,
+            ])
+            return HttpResponseRedirect(redirect_uri)
+
         key_list = sorted(ApiKey.objects.filter(
             organization=organization,
         ), key=lambda x: x.label)
