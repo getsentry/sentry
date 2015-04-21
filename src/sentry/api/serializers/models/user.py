63a1a37f597d6b0313136caf011d2d4f50c17329
David Cramer <dcramer@gmail.com>
2015-04-20 23:18:54 -0700

    Use display name instead of full name

--- src/sentry/api/serializers/models/user.py
+++ src/sentry/api/serializers/models/user.py
@@ -10,7 +10,7 @@ class UserSerializer(Serializer):
     def serialize(self, obj, attrs, user):
         d = {
             'id': str(obj.id),
-            'name': obj.get_full_name(),
+            'name': obj.get_display_name(),
             'email': obj.email,
             'avatarUrl': get_gravatar_url(obj.email, size=32),
         }
