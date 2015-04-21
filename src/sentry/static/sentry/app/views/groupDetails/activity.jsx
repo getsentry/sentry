c2e1ba5532f5ad4e7ae694432cdf5bdc99d65d4f
David Cramer <dcramer@gmail.com>
2015-04-20 23:18:59 -0700

    Improve rendering logic

--- src/sentry/static/sentry/app/views/groupDetails/activity.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/activity.jsx
@@ -7,19 +7,33 @@ var PropTypes = require("../../proptypes");
 var TimeSince = require("../../components/timeSince");
 var utils = require("../../utils");
 
+var formatActivity = function(item) {
+  var data = item.data;
 
-var ACTIVITY_ACTION_STRINGS = {
-    note: "left a note",
-    set_resolved: "marked this event as resolved",
-    set_unresolved: "marked this event as unresolved",
-    set_muted: "marked this event as muted",
-    set_public: "made this event public",
-    set_private: "made this event private",
-    set_regression: "marked this event as a regression",
-    create_issue: "created an issue on {provider:s} titled <a href=\"{location:s}\">{title:s}</a>",
-    first_seen: "first saw this event",
-    assigned: "assigned this event to {user:s}",
-    unassigned: "unassigned this event"
+  switch(item.type) {
+    case "note":
+      return "left a note";
+    case "set_resolved":
+      return "marked this event as resolved";
+    case "set_unresolved":
+      return "marked this event as unresolved";
+    case "set_muted":
+      return "marked this event as muted";
+    case "set_public":
+      return "made this event public";
+    case "set_private":
+      return "made this event private";
+    case "set_regression":
+      return "marked this event as a regression";
+    case "create_issue":
+      return `created an issue on ${data.provider} titled <a href="${data.location}">${data.title}</a>`;
+    case "first_seen":
+      return "first saw this event";
+    case "assigned":
+      return `assigned this event to ${data.user}`;
+    case "unassigned":
+      return "unassigned this event";
+  }
 };
 
 var GroupActivity = React.createClass({
@@ -37,7 +51,7 @@ var GroupActivity = React.createClass({
         item.user.name :
         'Sentry');
 
-      var label = ACTIVITY_ACTION_STRINGS[item.type];
+      var label = formatActivity(item);
 
       return (
         <li className="activity-item" key={itemIdx}>
