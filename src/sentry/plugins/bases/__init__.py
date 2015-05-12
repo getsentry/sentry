bb14bf0dfc11a3da66b60e7201f14e585470b4b9
David Cramer <dcramer@gmail.com>
2015-05-11 19:06:07 -0700

    Add separate Issue Tracking configuration pane

--- src/sentry/plugins/bases/__init__.py
+++ src/sentry/plugins/bases/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from .issue import IssuePlugin  # NOQA
+from .issue import IssueTrackingPlugin  # NOQA
 from .notify import NotificationPlugin  # NOQA
 from .releasetracking import ReleaseTrackingPlugin  # NOQA
 from .tag import TagPlugin  # NOQA
--- tests/sentry/plugins/bases/issue/tests.py
+++ tests/sentry/plugins/bases/issue/tests.py
@@ -7,7 +7,7 @@ import mock
 from social_auth.models import UserSocialAuth
 
 from sentry.models import User
-from sentry.plugins.bases.issue import IssuePlugin
+from sentry.plugins import IssueTrackingPlugin
 from sentry.testutils import TestCase
 
 
@@ -20,18 +20,18 @@ class GetAuthForUserTest(TestCase):
 
     def test_requires_auth_provider(self):
         user = self._get_mock_user()
-        p = IssuePlugin()
+        p = IssueTrackingPlugin()
         self.assertRaises(AssertionError, p.get_auth_for_user, user)
 
     def test_returns_none_on_missing_identity(self):
         user = self._get_mock_user()
-        p = IssuePlugin()
+        p = IssueTrackingPlugin()
         p.auth_provider = 'test'
         self.assertEquals(p.get_auth_for_user(user), None)
 
     def test_returns_identity(self):
         user = User.objects.create(username='test', email='test@example.com')
         auth = UserSocialAuth.objects.create(provider='test', user=user)
-        p = IssuePlugin()
+        p = IssueTrackingPlugin()
         p.auth_provider = 'test'
         self.assertEquals(p.get_auth_for_user(user), auth)
