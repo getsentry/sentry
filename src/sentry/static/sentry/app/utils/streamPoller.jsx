ba5a583ca9ec9b5680b315fe0800893c7e063024
David Cramer <dcramer@gmail.com>
2015-05-29 12:47:57 +0200

    Reduce polling rate when data slows down

--- src/sentry/static/sentry/app/utils/streamPoller.jsx
+++ src/sentry/static/sentry/app/utils/streamPoller.jsx
@@ -8,14 +8,24 @@ class StreamPoller {
     this.options = options;
     this._timeoutId = null;
     this._active = true;
-    this._delay = 3000;
+    this._baseDelay = 3000;
+    this._maxDelay = 60000;
+    this._reqsWithoutData = 0;
     this._pollingEndpoint = options.endpoint;
+  }
+
+  getDelay() {
+    var delay = this._baseDelay * (this._reqsWithoutData + 1);
+    if (delay > this._maxDelay) {
+      delay = this._maxDelay;
+    }
+    return delay;
   }
 
   enable(){
     this._active = true;
     if (!this._timeoutId) {
-      this._timeoutId = window.setTimeout(this.poll.bind(this), this._delay);
+      this._timeoutId = window.setTimeout(this.poll.bind(this), this.getDelay());
     }
   }
 
@@ -37,7 +47,12 @@ class StreamPoller {
 
         // if theres no data, nothing changes
         if (!data.length) {
+          this._reqsWithoutData += 1;
           return;
+        }
+
+        if (this._reqsWithoutData > 0) {
+          this._reqsWithoutData -= 1;
         }
 
         var links = parseLinkHeader(jqXHR.getResponseHeader('Link'));
@@ -48,7 +63,7 @@ class StreamPoller {
       },
       complete: () => {
         if (this._active) {
-          this._timeoutId = window.setTimeout(this.poll.bind(this), this._delay);
+          this._timeoutId = window.setTimeout(this.poll.bind(this), this.getDelay());
         }
       }
     });
