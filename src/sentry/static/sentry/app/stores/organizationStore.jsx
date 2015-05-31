c7796dfa099dd33b00b28aae3cc207c45a3c6a38
David Cramer <dcramer@gmail.com>
2015-05-31 14:35:50 +0200

    Move organizationList into organizationStore

---
+++ src/sentry/static/sentry/app/stores/organizationStore.jsx
@@ -0,0 +1,28 @@
+/** @jsx React.DOM */
+
+var Reflux = require("reflux");
+
+var OrganizationStore = Reflux.createStore({
+  init() {
+    this.items = [];
+  },
+
+  get(slug) {
+    for (var i = 0; i < this.items.length; i++) {
+      if (this.items[i].slug === slug) {
+        return this.items[i];
+      }
+    }
+  },
+
+  getAll() {
+    return this.items;
+  },
+
+  load(items) {
+    this.items = items;
+    this.trigger(items);
+  }
+});
+
+module.exports = OrganizationStore;
