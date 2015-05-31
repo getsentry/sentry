64833b517a00be9977e856442a70d011980ae504
ckj <chriskjennings@gmail.com>
2015-05-30 20:01:26 -0400

    Mock up new org selector

--- src/sentry/static/sentry/app/components/organizationHeader.jsx
+++ src/sentry/static/sentry/app/components/organizationHeader.jsx
@@ -13,7 +13,19 @@ var OrganizationHeader = React.createClass({
     return (
       <div>
         <header>
-          <div className="container">Sup</div>
+          <div className="container">
+            <a href="/"><span className="icon-sentry-logo"></span></a>
+
+            <div className="dropdown org-selector">
+              <a className="dropdown-toggle" href="#">Sentry <span className="icon-arrow-down"></span></a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Default</a></li>
+                <li><a href="#">Sentry</a></li>
+                <li className="divider"></li>
+                <li><a href="#">New Organization</a></li>
+              </ul>
+            </div>
+          </div>
         </header>
         <div className="sub-header">
           <div className="container">
