aedaf5915fec2844bc30a529510f7e1962b44b2e
ckj <chriskjennings@gmail.com>
2015-05-30 20:21:34 -0400

    Update project header

--- src/sentry/static/sentry/app/components/projectHeader.jsx
+++ src/sentry/static/sentry/app/components/projectHeader.jsx
@@ -33,7 +33,21 @@ var ProjectHeader = React.createClass({
       <div>
         <header>
           <div className="container">
-            <a href="#">Sentry</a>
+            <div className="pull-right">
+              <div className="dropdown user-nav">
+                <a className="dropdown-toggle" href="#">Dcramer <span className="icon-arrow-down"></span></a>
+              </div>
+            </div>
+            <a href="/"><span className="icon-sentry-logo"></span></a>
+            <div className="dropdown org-selector">
+              <a className="dropdown-toggle" href="#">Sentry <span className="icon-arrow-down"></span></a>
+              <ul className="dropdown-menu">
+                <li><a href="#">Default</a></li>
+                <li><a href="#">Sentry</a></li>
+                <li className="divider"></li>
+                <li><a href="#">New Organization</a></li>
+              </ul>
+            </div>
           </div>
         </header>
         <div className="sub-header">
