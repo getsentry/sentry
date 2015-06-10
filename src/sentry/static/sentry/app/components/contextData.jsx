72a7940f1517026b0d290a94140f86882db25f41
Armin Ronacher <armin.ronacher@active-4.com>
2015-06-10 16:22:25 +0200

    Add toggle functionality for data

--- src/sentry/static/sentry/app/components/contextData.jsx
+++ src/sentry/static/sentry/app/components/contextData.jsx
@@ -1,71 +1,7 @@
 /*** @jsx React.DOM */
 
 var React = require('react');
-
-
-function renderValue(value) {
-  function walk(value) {
-    var i = 0, children = [];
-    if (value === null) {
-      return <span className="val-null">None</span>;
-    } else if (value === true || value === false) {
-      return <span className="val-bool">{value ? 'True' : 'False'}</span>;
-    } else if (typeof value === 'string' || value instanceof String) {
-      // consider auto quoting?
-      return <span className="val-string">{value}</span>;
-    } else if (typeof value === 'number' || value instanceof Number) {
-      return <span className="val-number">{value}</span>;
-    } else if (value instanceof Array) {
-      for (i = 0; i < value.length; i++) {
-        children.push(
-          <span className="val-array-item" key={i}>
-            {walk(value[i])}
-            {i < value.length - 1 ? <span className="val-array-sep">{', '}</span> : null}
-          </span>
-        );
-      }
-      return (
-        <span className="val-array">
-          <span className="val-array-marker">{'['}</span>
-          <span className="val-array-items">{children}</span>
-          <span className="val-array-marker">{']'}</span>
-        </span>
-      );
-    } else {
-      var keys = Object.keys(value);
-      for (i = 0; i < keys.length; i++) {
-        var key = keys[i];
-        children.push(
-          <span className="val-dict-pair" key={key}>
-            <span className="val-dict-key">
-              <span className="val-string">{key}</span>
-            </span>
-            <span className="val-dict-col">{': '}</span>
-            <span className="val-dict-value">
-              {walk(value[key])}
-              {i < keys.length - 1 ? <span className="val-dict-sep">{', '}</span> : null}
-            </span>
-          </span>
-        );
-      }
-      return (
-        <span className="val-dict">
-          <span className="val-dict-marker">{'{'}</span>
-          <span className="val-dict-items">{children}</span>
-          <span className="val-dict-marker">{'}'}</span>
-        </span>
-      );
-    }
-  }
-  return walk(value);
-}
-
-function renderKeyPosValue(value) {
-  if (typeof value === 'string' || value instanceof String) {
-    return <span className="val-string">{value}</span>;
-  }
-  return renderValue(value);
-}
+var jQuery = require('jquery');
 
 
 var ContextData = React.createClass({
@@ -79,12 +15,96 @@ var ContextData = React.createClass({
     };
   },
 
+  renderValue(value) {
+    function toggle(event) {
+      jQuery(event.target).parent().toggleClass('val-toggle-open');
+      event.preventDefault();
+    }
+
+    function makeToggle(collapsed, children) {
+      var className = "val-toggle";
+      if (!collapsed) {
+        className += " val-toggle-open";
+      }
+      return (
+        <span className={className}>
+          <a href="#" className="val-toggle-link" onClick={toggle}></a>
+          {children}
+        </span>
+      );
+    }
+
+    function walk(value, depth) {
+      var i = 0, children = [];
+      // XXX: where do we hide?
+      var collapsed = depth > 1;
+      if (value === null) {
+        return <span className="val-null">None</span>;
+      } else if (value === true || value === false) {
+        return <span className="val-bool">{value ? 'True' : 'False'}</span>;
+      } else if (typeof value === 'string' || value instanceof String) {
+        // consider auto quoting?
+        return <span className="val-string">{value}</span>;
+      } else if (typeof value === 'number' || value instanceof Number) {
+        return <span className="val-number">{value}</span>;
+      } else if (value instanceof Array) {
+        for (i = 0; i < value.length; i++) {
+          children.push(
+            <span className="val-array-item" key={i}>
+              {walk(value[i], depth + 1)}
+              {i < value.length - 1 ? <span className="val-array-sep">{', '}</span> : null}
+            </span>
+          );
+        }
+        return (
+          <span className="val-array">
+            <span className="val-array-marker">{'['}</span>
+            {makeToggle(collapsed, <span className="val-array-items">{children}</span>)}
+            <span className="val-array-marker">{']'}</span>
+          </span>
+        );
+      } else {
+        var keys = Object.keys(value);
+        for (i = 0; i < keys.length; i++) {
+          var key = keys[i];
+          children.push(
+            <span className="val-dict-pair" key={key}>
+              <span className="val-dict-key">
+                <span className="val-string">{key}</span>
+              </span>
+              <span className="val-dict-col">{': '}</span>
+              <span className="val-dict-value">
+                {walk(value[key], depth + 1)}
+                {i < keys.length - 1 ? <span className="val-dict-sep">{', '}</span> : null}
+              </span>
+            </span>
+          );
+        }
+        return (
+          <span className="val-dict">
+            <span className="val-dict-marker">{'{'}</span>
+            {makeToggle(collapsed, <span className="val-dict-items">{children}</span>)}
+            <span className="val-dict-marker">{'}'}</span>
+          </span>
+        );
+      }
+    }
+    return walk(value, 0);
+  },
+
+  renderKeyPosValue(value) {
+    if (typeof value === 'string' || value instanceof String) {
+      return <span className="val-string">{value}</span>;
+    }
+    return this.renderValue(value);
+  },
+
   render() {
     var {data, className, ...other} = this.props;
     other.className = 'val ' + (className || '');
 
     return (
-      <pre {...other}>{renderValue(data)}</pre>
+      <pre {...other}>{this.renderValue(data)}</pre>
     );
   }
 });
