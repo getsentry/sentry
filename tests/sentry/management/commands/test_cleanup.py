8fb1809b5e57b177592919346d5fa0c35b2cbacb
David Cramer <dcramer@gmail.com>
2015-06-24 11:50:44 -0700

    Optimize cleanup task to perform bulk deletes where possible
    This pulls in code from getsentry to do more efficient bulk deletes given a Postgres backend on various models.

--- tests/sentry/management/commands/test_cleanup.py
+++ tests/sentry/management/commands/test_cleanup.py
@@ -2,8 +2,9 @@
 
 from __future__ import absolute_import
 
+from django.core.management import call_command
+
 from sentry.models import Event, Group, GroupTagValue, TagValue, TagKey
-from sentry.tasks.cleanup import cleanup
 from sentry.testutils import TestCase
 
 ALL_MODELS = (Event, Group, GroupTagValue, TagValue, TagKey)
@@ -13,7 +14,7 @@ class SentryCleanupTest(TestCase):
     fixtures = ['tests/fixtures/cleanup.json']
 
     def test_simple(self):
-        cleanup(days=1)
+        call_command('cleanup', days=1)
 
         for model in ALL_MODELS:
             assert model.objects.count() == 0
@@ -23,12 +24,12 @@ class SentryCleanupTest(TestCase):
         for model in ALL_MODELS:
             orig_counts[model] = model.objects.count()
 
-        cleanup(days=1, project=2)
+        call_command('cleanup', days=1, project=2)
 
         for model in ALL_MODELS:
             assert model.objects.count() == orig_counts[model]
 
-        cleanup(days=1, project=1)
+        call_command('cleanup', days=1, project=1)
 
         for model in ALL_MODELS:
             assert model.objects.count() == 0
