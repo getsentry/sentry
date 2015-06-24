497e95d462a33d7aa22f85b43fbe0adeed64e4f6
David Cramer <dcramer@gmail.com>
2015-06-24 12:04:04 -0700

    Add doc for latest event endpoint

--- src/sentry/api/endpoints/group_events_latest.py
+++ src/sentry/api/endpoints/group_events_latest.py
@@ -1,11 +1,22 @@
 from __future__ import absolute_import
 
 from sentry.api import client
+from sentry.api.base import DocSection
 from sentry.api.bases.group import GroupEndpoint
 
 
 class GroupEventsLatestEndpoint(GroupEndpoint):
+    doc_section = DocSection.EVENTS
+
     def get(self, request, group):
+        """
+        Retrieve the latest event
+
+        Return details on the latest event for this group.
+
+            {method} {path}
+
+        """
         event = group.get_latest_event()
 
         return client.get('/events/{}/'.format(event.id), request.user, request.auth)
