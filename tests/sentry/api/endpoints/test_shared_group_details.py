f3b285a3798da53a9a0e661719f08cc39ec9f8b3
David Cramer <dcramer@gmail.com>
2015-06-30 14:03:01 -0700

    Specialized endpoint for shared groups

---
+++ tests/sentry/api/endpoints/test_shared_group_details.py
@@ -0,0 +1,20 @@
+from __future__ import absolute_import, print_function
+
+from django.core.urlresolvers import reverse
+
+from sentry.testutils import APITestCase
+
+
+class SharedGroupDetailsTest(APITestCase):
+    def test_simple(self):
+        self.login_as(user=self.user)
+
+        group = self.create_group()
+
+        url = reverse('sentry-api-0-shared-group-details', kwargs={
+            'share_id': group.get_share_id(),
+        })
+        response = self.client.get(url, format='json')
+
+        assert response.status_code == 200, response.content
+        assert response.data['id'] == str(group.id)
