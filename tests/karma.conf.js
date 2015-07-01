f638f5bc23356c55e042a4ea5a0b3d263dc85735
Chris Montrois <cmontrois@quickleft.com>
2015-07-01 14:36:09 -0600

    Fix broken JS tests
    General changes around getting the test harness working for JS.
    - Moved js installation into Makefile
    - Moved some npm dependencies into devDependencies
    - Removed broken, legacy tests
    - Updated Karma config for webpack
    - Added webpack test loader

--- tests/karma.conf.js
+++ tests/karma.conf.js
@@ -1,51 +1,39 @@
 // Karma configuration
 // Generated on Sat Jul 26 2014 13:49:45 GMT+0200 (CEST)
 var path = require('path');
-
 var appPrefix = path.join(__dirname, "../src/sentry/static/sentry/app");
-
-console.log(appPrefix);
 
 module.exports = function(config) {
   config.set({
 
     // base path that will be used to resolve all patterns (eg. files, exclude)
-    basePath: '..',
-
+    basePath: '../',
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
     frameworks: ['mocha', 'chai', 'sinon'],
 
-
     // list of files / patterns to load in the browser
     files: [
-      'src/sentry/static/sentry/dist/vendor.js',
-      'src/sentry/static/sentry/dist/app.js',
-      'tests/js/**/*Spec.js'
-    ],
-
-
-    // list of files to exclude
-    exclude: [
+      'tests/js/vendor/**/*.js',
+      'tests/tests.webpack.js'
     ],
 
     webpack: {
       cache: true,
-        module: {
-        resolve: {
-          alias: {
-            "app": appPrefix
-          },
-          modulesDirectories: ["node_modules"],
-          extensions: ["", ".jsx", ".js", ".json"]
+      resolve: {
+        alias: {
+          "app": appPrefix
         },
+        modulesDirectories: ["node_modules"],
+        extensions: ["", ".jsx", ".js", ".json"]
+      },
+      module: {
         loaders: [
           {
-            test: /\.jsx$/,
-            loader: "jsx-loader?insertPragma=React.DOM&harmony",
-            include: path.join(__dirname, appPrefix),
-            exclude: /vendor/
+            exclude: /(vendor|node_modules)/,
+            test: /\.jsx?$/,
+            loader: "babel-loader"
           }
         ]
       },
@@ -53,15 +41,7 @@ module.exports = function(config) {
     },
 
     webpackMiddleware: {
-        // webpack-dev-middleware configuration
-        // i. e.
-        noInfo: true
-    },
-
-    webpackServer: {
-      stats: {
-        colors: true
-      }
+      noInfo: true
     },
 
     plugins: [
@@ -76,36 +56,31 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      'tests/**/*.js': ['webpack', 'sourcemap']
+      'tests/tests.webpack.js': ['webpack', 'sourcemap']
     },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
-
+    reporters: ['dots'],
 
     // web server port
     port: 9876,
 
-
     // enable / disable colors in the output (reporters and logs)
     colors: true,
-
 
     // level of logging
     // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
 
-
     // enable / disable watching file and executing tests whenever any file changes
     autoWatch: true,
 
-
     // start these browsers
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    // browsers: ['PhantomJS'],
     browsers: ['PhantomJS'],
-
 
     // Continuous Integration mode
     // if true, Karma captures browsers, runs the tests and exits
