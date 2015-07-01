d3ab80d467cc930e6044b779a76ad5c7476ca49a
ckj <chriskjennings@gmail.com>
2015-07-01 12:36:16 -0400

    Stub out saved searches

--- src/sentry/static/sentry/app/views/stream/filters.jsx
+++ src/sentry/static/sentry/app/views/stream/filters.jsx
@@ -25,10 +25,10 @@ var SearchDropdown = React.createClass({
     return (
       <div className="search-dropdown" style={style}>
         <ul className="nav nav-tabs nav-tabs-xs">
-          <li className="active"><a href="#">Search Options</a></li>
-          <li><a href="#">Saved Searches</a></li>
+          <li><a href="#">Search Options</a></li>
+          <li className="active"><a href="#">Saved Searches</a></li>
         </ul>
-        <ul className="search-helper search-autocomplete-list">
+        <ul className="search-helper search-autocomplete-list hidden">
           <li className="search-autocomplete-item">
             <span className="icon icon-tag"></span>
             <h4>Tag - <span className="search-description">key/value pair associated to an event</span></h4>
@@ -43,6 +43,29 @@ var SearchDropdown = React.createClass({
             <span className="icon icon-user"></span>
             <h4>Assigned - <span className="search-description">team member assigned to an event</span></h4>
             <p className="search-example">assigned:[me|user@example.com]</p>
+          </li>
+        </ul>
+        <ul className="search-saved-searches search-autocomplete-list">
+          <li className="search-autocomplete-item">
+            <a className="pull-right remove-saved-search">
+              <span className="icon icon-trash"></span>
+            </a>
+            <span className="icon icon-search"></span>
+            <h4>os:"Mac OS X 10.8", browser:"Chrome 31"</h4>
+          </li>
+          <li className="search-autocomplete-item">
+            <a className="pull-right remove-saved-search">
+              <span className="icon icon-trash"></span>
+            </a>
+            <span className="icon icon-search"></span>
+            <h4>status:unresolved, assigned:me</h4>
+          </li>
+          <li className="search-autocomplete-item">
+            <a className="pull-right remove-saved-search">
+              <span className="icon icon-trash"></span>
+            </a>
+            <span className="icon icon-search"></span>
+            <h4>server:"web-1", server:"web-2", server:"web-3"</h4>
           </li>
         </ul>
       </div>
