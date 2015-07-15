a10545105d53ad3c765d7e6daa61371b3a3612fd
David Cramer <dcramer@gmail.com>
2015-07-14 18:00:49 -0700

    Expand seen info

---
+++ src/sentry/static/sentry/app/components/version.jsx
@@ -0,0 +1,28 @@
+var React = require("react");
+var Router = require("react-router");
+
+var Version = React.createClass({
+  contextTypes: {
+    router: React.PropTypes.func
+  },
+
+  render() {
+    var version = this.props.version;
+    var shortVersion = version.length === 40 ? version.substr(0, 12) : version;
+    var params = this.context.router.getCurrentParams();
+
+    return (
+      <Router.Link
+          to="releaseDetails"
+          params={{
+            orgId: params.orgId,
+            projectId: params.projectId,
+            version: version,
+          }}>
+        <span title={version}>{shortVersion}</span>
+      </Router.Link>
+    );
+  }
+});
+
+module.exports = Version;
