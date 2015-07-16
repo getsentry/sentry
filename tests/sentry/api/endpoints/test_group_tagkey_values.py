e9dc79ef7fecfa2bb70f5dbfe81de916a79606a0
David Cramer <dcramer@gmail.com>
2015-07-16 01:01:32 -0700

    Fix tests

--- tests/sentry/api/endpoints/test_group_tagkey_values.py
+++ tests/sentry/api/endpoints/test_group_tagkey_values.py
@@ -2,22 +2,34 @@ from __future__ import absolute_import
 
 from django.core.urlresolvers import reverse
 
-from sentry.models import GroupTagValue, TagKey
+from sentry.models import GroupTagValue, TagKey, TagValue
 from sentry.testutils import APITestCase
 
 
 class GroupTagKeyValuesTest(APITestCase):
     def test_simple(self):
+        key, value = 'foo', 'bar'
+
         project = self.create_project()
         group = self.create_group(project=project)
-        tagkey = TagKey.objects.create(project=project, key='foo')
-        grouptagvalue = GroupTagValue.objects.create(group=group, key='foo', value='bar')
+        TagKey.objects.create(project=project, key=key)
+        TagValue.objects.create(
+            project=project,
+            key=key,
+            value=value,
+        )
+        GroupTagValue.objects.create(
+            project=project,
+            group=group,
+            key=key,
+            value=value,
+        )
 
         self.login_as(user=self.user)
 
         url = reverse('sentry-api-0-group-tagkey-values', kwargs={
             'group_id': group.id,
-            'key': tagkey.key,
+            'key': key,
         })
 
         response = self.client.get(url)
