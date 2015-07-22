60ad47bd103e903ed18bd714a15c60f7f45f1e13
David Cramer <dcramer@gmail.com>
2015-07-22 17:25:19 +0200

    Abstract status checks into extensible system

--- src/sentry/templatetags/sentry_status.py
+++ src/sentry/templatetags/sentry_status.py
@@ -1,22 +1,15 @@
 from __future__ import absolute_import, print_function
 
 from django import template
-from time import time
 
-from sentry import options
+from sentry import status_checks
 
 register = template.Library()
-
-ERR_WORKERS_LONG_PING = "Background workers haven't checked in recently. This can mean an issue with your configuration or a serious backlog in tasks."
 
 
 @register.inclusion_tag('sentry/partial/system-status.html', takes_context=True)
 def show_system_status(context):
-    problems = []
-
-    last_ping = options.get('sentry:last_worker_ping') or 0
-    if last_ping < time() - 300:
-        problems.append(ERR_WORKERS_LONG_PING)
+    problems = status_checks.check_all()
 
     return {
         'problems': problems,
