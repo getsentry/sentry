0d91f95e21a7b54462524ea78011061145d3e6a6
David Cramer <dcramer@gmail.com>
2015-07-27 15:34:32 -0700

    Store action match

--- src/sentry/receivers/rules.py
+++ src/sentry/receivers/rules.py
@@ -13,6 +13,7 @@ def create_default_rules(instance, created=True, RuleModel=Rule, **kwargs):
         project=instance,
         label='Send a notification for new events',
         data={
+            'match': 'all',
             'conditions': [
                 {'id': 'sentry.rules.conditions.first_seen_event.FirstSeenEventCondition'},
             ],
@@ -26,6 +27,7 @@ def create_default_rules(instance, created=True, RuleModel=Rule, **kwargs):
         project=instance,
         label='Send a notification for regressions',
         data={
+            'match': 'all',
             'conditions': [
                 {'id': 'sentry.rules.conditions.regression_event.RegressionEventCondition'},
             ],
