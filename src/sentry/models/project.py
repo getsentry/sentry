df559a9fc8067010021391f67b4ebdc5c0d10aaa
David Cramer <dcramer@gmail.com>
2015-08-10 00:52:38 -0700

    Dont duplicate name in emails

--- src/sentry/models/project.py
+++ src/sentry/models/project.py
@@ -232,3 +232,8 @@ class Project(Model):
             'public': self.public,
             'platform': self.platform,
         }
+
+    def get_full_name(self):
+        if self.team.name not in self.name:
+            return '%s %s' % (self.team.name, self.name)
+        return self.name
