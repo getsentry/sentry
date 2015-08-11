7ea5992a7e3b793d65863b7033573b8293b14b82
David Cramer <dcramer@gmail.com>
2015-08-11 00:59:59 -0700

    Add firstEvent and lastEvent to release serializer

--- src/sentry/api/serializers/models/release.py
+++ src/sentry/api/serializers/models/release.py
@@ -1,11 +1,27 @@
 from __future__ import absolute_import
 
 from sentry.api.serializers import Serializer, register
-from sentry.models import Release
+from sentry.models import Release, TagValue
 
 
 @register(Release)
 class ReleaseSerializer(Serializer):
+    def get_attrs(self, item_list, user):
+        tags = {
+            tk.value: tk
+            for tk in TagValue.objects.filter(
+                project=item_list[0].project,
+                key='sentry:release',
+                value__in=[o.version for o in item_list],
+            )
+        }
+        result = {}
+        for item in item_list:
+            result[item] = {
+                'tag': tags.get(item.version),
+            }
+        return result
+
     def serialize(self, obj, attrs, user):
         d = {
             'version': obj.version,
@@ -18,4 +34,14 @@ class ReleaseSerializer(Serializer):
             'data': obj.data,
             'newGroups': obj.new_groups,
         }
+        if attrs['tag']:
+            d.update({
+                'lastEvent': attrs['tag'].last_seen,
+                'firstEvent': attrs['tag'].first_seen,
+            })
+        else:
+            d.update({
+                'lastEvent': None,
+                'firstEvent': None,
+            })
         return d
