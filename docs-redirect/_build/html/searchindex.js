3b8f4e212e0ab4294b0f1af6089d5168a8f71ab1
David Cramer <dcramer@gmail.com>
2015-08-12 13:53:39 -0700

    Revert "Add docs-redirect project (for RTD.org)"
    This reverts commit 918ca281a0e7d3f4d87833e4bebe7faa01d8eabf.

--- docs-redirect/_build/html/searchindex.js
+++
@@ -1,1 +0,0 @@
-Search.setIndex({envversion:46,filenames:["index"],objects:{},objnames:{},objtypes:{},terms:{content:0,index:0,modul:0,page:0,search:0},titles:["Welcome to Sentry&#8217;s documentation!"],titleterms:{document:0,indic:0,sentri:0,tabl:0,welcom:0}})
