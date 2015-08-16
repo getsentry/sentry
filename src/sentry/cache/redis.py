f3b322c181ba2ebc0c96d035b413843aad724c11
Armin Ronacher <armin.ronacher@active-4.com>
2015-08-16 22:39:48 +0200

    Switch from nydus to rb in most places.

--- src/sentry/cache/redis.py
+++ src/sentry/cache/redis.py
@@ -9,7 +9,7 @@ sentry.cache.redis
 from __future__ import absolute_import
 
 from django.conf import settings
-from nydus.db import create_cluster
+from rb import Cluster
 from threading import local
 
 from sentry.utils import json
@@ -26,24 +26,21 @@ class RedisCache(local):
         options.setdefault('hosts', {
             0: {},
         })
-        options.setdefault('router', 'nydus.db.routers.keyvalue.PartitionRouter')
-        self.conn = create_cluster({
-            'engine': 'nydus.db.backends.redis.Redis',
-            'router': options['router'],
-            'hosts': options['hosts'],
-        })
+        self.cluster = Cluster(options['hosts'])
+        self.client = self.cluster.get_routing_client()
 
     def set(self, key, value, timeout):
-        with self.conn.map() as conn:
-            conn.set(key, json.dumps(value))
-            if timeout:
-                conn.expire(key, timeout)
+        v = json.dumps(value)
+        if timeout:
+            self.client.setex(key, int(timeout), v)
+        else:
+            self.client.set(key, v)
 
     def delete(self, key):
-        self.conn.delete(key)
+        self.client.delete(key)
 
     def get(self, key):
-        result = self.conn.get(key)
+        result = self.client.get(key)
         if result is not None:
             result = json.loads(result)
         return result
