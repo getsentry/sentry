283e57dc05ea9b25d26a085c6e93fde83acee60c
ckj <chriskjennings@gmail.com>
2015-08-17 19:39:59 -0400

    Try alert-style message for release links to the stream

--- src/sentry/static/sentry/app/views/releaseNewEvents.jsx
+++ src/sentry/static/sentry/app/views/releaseNewEvents.jsx
@@ -11,9 +11,14 @@ var ReleaseNewEvents = React.createClass({
 
   render() {
     return (
-      <GroupList
-        query={'first-release:"' + this.context.release.version + '"'}
-        canSelectGroups={false} bulkActions={false} />
+      <div>
+      <div className="alert alert-block">
+          <a href="#"><span className="icon icon-open"></span> View new events in the stream</a>
+        </div>
+        <GroupList
+          query={'first-release:"' + this.context.release.version + '"'}
+          canSelectGroups={false} bulkActions={false} />
+      </div>
     );
   }
 });
