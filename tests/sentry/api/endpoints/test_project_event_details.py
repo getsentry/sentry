48df201044689f9c0552a2b78c60368ae1cdcf51
David Cramer <dcramer@gmail.com>
2015-08-18 16:07:09 -0700

    Clean up event details endpoints
    - Remove internal endpoint
    - Switch event IDs from internal IDs
    - Remove invalid perm check

--- tests/sentry/api/endpoints/test_project_event_details.py
+++ tests/sentry/api/endpoints/test_project_event_details.py
@@ -1,10 +1,12 @@
+from __future__ import absolute_import
+
 from datetime import datetime
 from django.core.urlresolvers import reverse
 
 from sentry.testutils import APITestCase
 
 
-class EventDetailsTest(APITestCase):
+class ProjectEventDetailsTest(APITestCase):
     def test_simple(self):
         self.login_as(user=self.user)
 
@@ -25,13 +27,15 @@ class EventDetailsTest(APITestCase):
             datetime=datetime(2013, 8, 13, 3, 8, 26),
         )
 
-        url = reverse('sentry-api-0-event-details', kwargs={
-            'event_id': cur_event.id,
+        url = reverse('sentry-api-0-project-event-details', kwargs={
+            'event_id': cur_event.event_id,
+            'project_slug': cur_event.project.slug,
+            'organization_slug': cur_event.project.organization.slug,
         })
         response = self.client.get(url, format='json')
 
         assert response.status_code == 200, response.content
         assert response.data['id'] == str(cur_event.id)
-        assert response.data['nextEventID'] == str(next_event.id)
-        assert response.data['previousEventID'] == str(prev_event.id)
+        assert response.data['nextEventID'] == str(next_event.event_id)
+        assert response.data['previousEventID'] == str(prev_event.event_id)
         assert response.data['groupID'] == group.id
