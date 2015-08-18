cca489573f0c92a85327a31a90d75c9a88374e5c
David Cramer <dcramer@gmail.com>
2015-08-18 12:04:22 -0700

    Better handling of invalid length attributes (release, event_id)
    Conflicts:
    	src/sentry/models/eventerror.py

--- src/sentry/models/eventerror.py
+++ src/sentry/models/eventerror.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import
 class EventError(object):
     INVALID_DATA = 'invalid_data'
     INVALID_ATTRIBUTE = 'invalid_attribute'
+    VALUE_TOO_LONG = 'value_too_long'
     UNKNOWN_ERROR = 'unknown_error'
     SECURITY_VIOLATION = 'security_violation'
 
@@ -17,6 +18,7 @@ class EventError(object):
     _titles = {
         INVALID_DATA: 'Discarded invalid data',
         INVALID_ATTRIBUTE: 'Discarded invalid attribute',
+        VALUE_TOO_LONG: 'Discarded value due to exceeding maximum length',
         UNKNOWN_ERROR: 'Unknown error',
         SECURITY_VIOLATION: 'Security violation',
         JS_GENERIC_FETCH_ERROR: 'Unable to fetch URL',
