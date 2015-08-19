663a8746eece24ae624b075a6cd08a1e1a079173
Ben Vinegar <ben@benv.ca>
2015-08-19 09:48:51 -0700

    Make raw exception output just plaintext (refs #1771)

--- tests/js/spec/components/interfaces/rawStacktraceContent.spec.jsx
+++ tests/js/spec/components/interfaces/rawStacktraceContent.spec.jsx
@@ -3,17 +3,14 @@ var React = require("react/addons");
 var Cookies = require("js-cookie");
 
 var api = require("app/api");
-var RawStacktraceContent = require("app/components/interfaces/rawStacktraceContent");
+import {getJavaFrame} from "app/components/interfaces/rawStacktraceContent";
 
 var TestUtils = React.addons.TestUtils;
 
 describe("RawStacktraceContent", function() {
   describe("getJavaFrame()", function() {
     it('should render java frames', function () {
-      var data = { frames: [] };
-      var elem = TestUtils.renderIntoDocument(<RawStacktraceContent data={data} />);
-
-      expect(elem.getJavaFrame({
+      expect(getJavaFrame({
         module: 'org.mortbay.thread.QueuedThreadPool$PoolThread',
         function: 'run',
         filename: 'QueuedThreadPool.java',
@@ -21,14 +18,14 @@ describe("RawStacktraceContent", function() {
       })).to.eql('    at org.mortbay.thread.QueuedThreadPool$PoolThread.run(QueuedThreadPool.java:582)');
 
       // without line number
-      expect(elem.getJavaFrame({
+      expect(getJavaFrame({
         module: 'org.mortbay.thread.QueuedThreadPool$PoolThread',
         function: 'run',
         filename: 'QueuedThreadPool.java'
       })).to.eql('    at org.mortbay.thread.QueuedThreadPool$PoolThread.run(QueuedThreadPool.java)');
 
       // without line number and filename
-      expect(elem.getJavaFrame({
+      expect(getJavaFrame({
         module: 'org.mortbay.thread.QueuedThreadPool$PoolThread',
         function: 'run'
       })).to.eql('    at org.mortbay.thread.QueuedThreadPool$PoolThread.run');
