b2d77139c86d365f4c8e3fc4a0d8451efd44689f
Ben Vinegar <ben@benv.ca>
2015-08-19 16:30:04 -0700

    Add tooltips to "last seen" avatars (fixes #1769)

--- src/sentry/static/sentry/app/views/groupDetails/seenBy.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/seenBy.jsx
@@ -2,15 +2,38 @@ import React from "react";
 import Gravatar from "../../components/gravatar";
 import GroupState from "../../mixins/groupState";
 
+import {userDisplayName} from "../../utils/formatters";
+
 var GroupSeenBy = React.createClass({
   mixins: [GroupState],
+
+  componentDidMount() {
+    this.attachTooltips();
+  },
+
+  componentWillUnmount() {
+    this.removeTooltips();
+    $(this.getDOMNode()).unbind();
+  },
+
+  attachTooltips() {
+    $(this.getDOMNode()).tooltip({
+      html: true,
+      selector: ".tip"
+    });
+  },
+
+  removeTooltips() {
+    $(this.getDOMNode()).tooltip("destroy");
+  },
 
   render() {
     var group = this.getGroup();
 
     var seenByNodes = group.seenBy.map((user, userIdx) => {
+      var displayName = userDisplayName(user);
       return (
-        <li key={userIdx}>
+        <li key={userIdx} className="tip" data-title={displayName}>
           <Gravatar size={52} email={user.email} />
         </li>
       );
