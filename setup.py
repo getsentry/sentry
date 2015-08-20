ce0ec79b14bbb6e091806ddc1f3508c73cabbf9b
David Cramer <dcramer@gmail.com>
2015-08-20 12:11:57 -0700

    Unify version as 8.0.0

--- setup.py
+++ setup.py
@@ -199,7 +199,7 @@ class SmartInstall(install):
 
 setup(
     name='sentry',
-    version='7.8.0.dev0',
+    version='8.0.0.dev0',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='https://www.getsentry.com',
