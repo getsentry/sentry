d62c42dea56de3b28f1fc78a1b59223850becacb
Armin Ronacher <armin.ronacher@active-4.com>
2015-08-23 18:20:26 +0200

    Fixed up a bad docstring for apis

--- src/sentry/api/endpoints/project_event_details.py
+++ src/sentry/api/endpoints/project_event_details.py
@@ -14,11 +14,9 @@ class ProjectEventDetailsEndpoint(ProjectEndpoint):
     def get(self, request, project, event_id):
         """
         Retrieve an event for a project
+        ```````````````````````````````
 
         Return details on an individual event.
-
-            {method} {path}
-
         """
         event = Event.objects.get(
             event_id=event_id,
