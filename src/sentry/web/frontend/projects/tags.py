a533cb4c8d88017afcdd3a3710ede940566f1101
David Cramer <dcramer@gmail.com>
2015-08-25 16:24:22 -0700

    Remove tag options (fixes GH-1836)

--- src/sentry/web/frontend/projects/tags.py
+++ src/sentry/web/frontend/projects/tags.py
@@ -7,13 +7,8 @@ sentry.web.frontend.projects
 """
 from __future__ import absolute_import
 
-from django.contrib import messages
-from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-from django.utils.translation import ugettext_lazy as _
-
 from sentry.constants import MEMBER_ADMIN
-from sentry.models import ProjectOption, TagKey, TagKeyStatus
+from sentry.models import TagKey, TagKeyStatus
 from sentry.web.decorators import has_access
 from sentry.web.helpers import render_to_response
 
@@ -25,33 +20,11 @@ def manage_project_tags(request, organization, project):
         status=TagKeyStatus.VISIBLE,
     )
 
-    enabled_filters = ProjectOption.objects.get_value(
-        project, 'tags', [t.key for t in tag_list])
-    enabled_annotations = ProjectOption.objects.get_value(
-        project, 'annotations', ['sentry:user'])
-
-    if request.POST:
-        filters = request.POST.getlist('filters')
-        ProjectOption.objects.set_value(
-            project, 'tags', filters)
-
-        annotations = request.POST.getlist('annotations')
-        ProjectOption.objects.set_value(
-            project, 'annotations', annotations)
-
-        messages.add_message(
-            request, messages.SUCCESS,
-            _('Your settings were saved successfully.'))
-
-        return HttpResponseRedirect(reverse('sentry-manage-project-tags', args=[project.organization.slug, project.slug]))
-
     context = {
         'organization': organization,
         'team': project.team,
         'project': project,
         'tag_list': tag_list,
-        'enabled_filters': enabled_filters,
-        'enabled_annotations': enabled_annotations,
         'page': 'tags',
     }
     return render_to_response('sentry/projects/manage_tags.html', context, request)
