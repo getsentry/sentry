cea8d3c036c505f77f23ec199df924bfcca9cf24
Armin Ronacher <armin.ronacher@active-4.com>
2015-08-28 19:31:52 +0200

    Bypass sudo flag for api requests.

--- src/sentry/api/decorators.py
+++ src/sentry/api/decorators.py
@@ -5,11 +5,20 @@ import json
 from django.http import HttpResponse
 from functools import wraps
 
+from sentry.models import ApiKey
+
+
+def is_considered_sudo(request):
+    return request.is_sudo() or \
+        isinstance(request.auth, ApiKey)
+
 
 def sudo_required(func):
     @wraps(func)
     def wrapped(self, request, *args, **kwargs):
-        if not request.is_sudo():
+        # If we are already authenticated through an API key we do not
+        # care about the sudo flag.
+        if not is_considered_sudo(request):
             # TODO(dcramer): support some kind of auth flow to allow this
             # externally
             data = {
