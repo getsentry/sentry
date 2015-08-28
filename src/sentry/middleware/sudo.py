33e478c231fd7a84fa3455af8cbb8b7dd3fd8020
Armin Ronacher <armin.ronacher@active-4.com>
2015-08-28 19:31:23 +0200

    Reverted a broken commit.

--- src/sentry/middleware/sudo.py
+++ src/sentry/middleware/sudo.py
@@ -14,10 +14,6 @@ from sentry.constants import EMPTY_PASSWORD_VALUES
 
 class SudoMiddleware(BaseSudoMiddleware):
     def has_sudo_privileges(self, request):
-        print request.auth
-        if request.auth:
-            return True
-
         # Users without a password are assumed to always have sudo powers
         user = request.user
         if user.is_authenticated() and user.password in EMPTY_PASSWORD_VALUES:
