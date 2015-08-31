8d502e5734ed2eff4b567a137499fd0c7ae53f70
David Cramer <dcramer@gmail.com>
2015-08-31 10:20:27 -0700

    Allow javascript scraping to be disabled

--- src/sentry/lang/javascript/plugin.py
+++ src/sentry/lang/javascript/plugin.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, print_function
 
 from django.conf import settings
 
+from sentry.models import Project
 from sentry.plugins import Plugin2
 
 from .processor import SourceProcessor
@@ -11,8 +12,14 @@ def preprocess_event(data):
     if data.get('platform') != 'javascript':
         return
 
+    project = Project.objects.get_from_cache(
+        id=data['project'],
+    )
+    if not bool(project.get_option('sentry:scrape_javascript', True)):
+        return
+
     processor = SourceProcessor()
-    return processor.process(data)
+    return processor.process(project, data)
 
 
 class JavascriptPlugin(Plugin2):
