65a447fb33615e0578dd67a7ca62e2ed5ee8e013
David Cramer <dcramer@gmail.com>
2015-09-01 10:40:42 -0700

    Load React config on all pages
    - Fixes lack of urlPrefix with project selector (fixes GH-1869)

--- src/sentry/web/frontend/react_page.py
+++ src/sentry/web/frontend/react_page.py
@@ -1,29 +1,11 @@
 from __future__ import absolute_import
 
-from sentry import features
 from sentry.web.frontend.base import BaseView, OrganizationView
-from sentry.utils import json
-from sentry.utils.functional import extract_lazy_object
-from django.utils.safestring import mark_safe
 
 
 class ReactMixin(object):
     def handle_react(self, request):
-        if request.user.is_authenticated():
-            # remove lazy eval
-            request.user = extract_lazy_object(request.user)
-
-        enabled_features = []
-        if features.has('organizations:create', actor=request.user):
-            enabled_features.append('organizations:create')
-        if features.has('auth:register', actor=request.user):
-            enabled_features.append('auth:register')
-
-        context = {
-            'features': mark_safe(json.dumps(enabled_features)),
-        }
-
-        return self.respond('sentry/bases/react.html', context)
+        return self.respond('sentry/bases/react.html')
 
 
 # TODO(dcramer): once we implement basic auth hooks in React we can make this
