fb702369c2faa6dc5202a8e14ec64718e46a05a2
Matt Robenolt <matt@ydekproductions.com>
2015-09-08 12:36:35 -0700

    Wrap curl url with quotes

--- tests/js/spec/components/events/interfaces/utils.spec.jsx
+++ tests/js/spec/components/events/interfaces/utils.spec.jsx
@@ -42,7 +42,7 @@ describe("components/interfaces/utils", function() {
         ' -H "Referer: http://example.com" \\\n' +
         ' -H "User-Agent: Mozilla/5.0 (Windows NT 6.2; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1500.72 Safari/537.36" \\\n' +
         ' --data "{\\"hello\\": \\"world\\"}" \\\n' +
-        ' http://example.com/foo?foo=bar'
+        ' "http://example.com/foo?foo=bar"'
       );
 
       // --compressed (because Accept-Encoding: gzip)
@@ -75,7 +75,7 @@ describe("components/interfaces/utils", function() {
         ' -H "Referer: http://example.com" \\\n' +
         ' -H "Accept-Encoding: gzip" \\\n' +
         ' --data "{\\"hello\\": \\"world\\"}" \\\n' +
-        ' http://example.com/foo?foo=bar'
+        ' "http://example.com/foo?foo=bar"'
       );
     });
   });
