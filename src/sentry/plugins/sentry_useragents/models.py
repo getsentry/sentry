1e940d07ef900f8935640753e482c9e1101258a8
David Cramer <dcramer@gmail.com>
2015-09-09 13:47:42 -0500

    Revert "Update user agents support for headers as lists (fixes GH-1922)"
    This reverts commit b48c85826e910d782506b23243d52b7c54bb327a.

--- src/sentry/plugins/sentry_useragents/models.py
+++ src/sentry/plugins/sentry_useragents/models.py
@@ -27,18 +27,15 @@ class UserAgentPlugin(TagPlugin):
             return []
         if not http.headers:
             return []
-
-        output = []
-        for key, value in http.headers:
-            if key != 'User-Agent':
-                continue
-            ua = Parse(value)
-            if not ua:
-                continue
-            result = self.get_tag_from_ua(ua)
-            if result:
-                output.append(result)
-        return result
+        if 'User-Agent' not in http.headers:
+            return []
+        ua = Parse(http.headers['User-Agent'])
+        if not ua:
+            return []
+        result = self.get_tag_from_ua(ua)
+        if not result:
+            return []
+        return [result]
 
 
 class BrowserPlugin(UserAgentPlugin):
