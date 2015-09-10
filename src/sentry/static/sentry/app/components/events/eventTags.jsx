a0db1ac765db99f20d572dc324d1e7c18efb00a7
Ben Vinegar <ben@benv.ca>
2015-09-09 17:54:00 -0700

    Sort tags on group details (refs #1899)

--- src/sentry/static/sentry/app/components/events/eventTags.jsx
+++ src/sentry/static/sentry/app/components/events/eventTags.jsx
@@ -1,6 +1,9 @@
 import React from "react";
-import PropTypes from "../../proptypes";
 import Router from "react-router";
+import _ from "underscore";
+
+import PropTypes from "../../proptypes";
+
 import EventDataSection from "./eventDataSection";
 import {isUrl} from "../../utils";
 
@@ -15,44 +18,41 @@ var EventTags = React.createClass({
   },
 
   render() {
-    var params = this.context.router.getCurrentParams(),
-      children = [],
-      value;
+    var params = this.context.router.getCurrentParams();
 
-    var {group, event} = this.props;
-
-    for (var key in this.props.event.tags) {
-      value = this.props.event.tags[key];
-
-      children.push(
-        <li key={key}>
-          {key} = <Router.Link
-            to="stream"
-            params={params}
-            query={{query: key + ':' + '"' + value + '"'}}>
-            {value}
-          </Router.Link>
-          {isUrl(value) &&
-            <a href={value} className="external">
-              <em className="icon-open" />
-            </a>
-          }
-        </li>
-      );
-    }
-
-    if (children.length === 0) {
+    let tags = this.props.event.tags;
+    if (_.isEmpty(tags))
       return null;
-    }
+
+    let sortedTags = _.chain(tags)
+      .map((val, key) => [key, val])
+      .sortBy(([key,]) => key)
+      .value();
 
     return (
       <EventDataSection
-          group={group}
-          event={event}
+          group={this.props.group}
+          event={this.props.event}
           title="Tags"
           type="tags">
         <ul className="mini-tag-list">
-          {children}
+          {sortedTags.map(([key, value]) => {
+            return (
+              <li key={key}>
+                {key} = <Router.Link
+                  to="stream"
+                  params={params}
+                  query={{query: `${key}:"${value}"`}}>
+                  {value}
+                </Router.Link>
+                {isUrl(value) &&
+                  <a href={value} className="external">
+                    <em className="icon-open" />
+                  </a>
+                }
+              </li>
+            );
+          })}
         </ul>
       </EventDataSection>
     );
