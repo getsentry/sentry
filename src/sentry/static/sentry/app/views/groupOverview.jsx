e49756dc15ba6577521cc162c616c25006303650
David Cramer <dcramer@gmail.com>
2015-09-13 11:04:42 -0700

    Render aggregate sidebar on event details (refs GH-1947)

--- src/sentry/static/sentry/app/views/groupOverview.jsx
+++ src/sentry/static/sentry/app/views/groupOverview.jsx
@@ -1,17 +1,15 @@
 import React from "react";
 import Router from "react-router";
-import api from "../../api";
+import api from "../api";
 
-import RouteMixin from "../../mixins/routeMixin";
-import GroupState from "../../mixins/groupState";
-import ApiMixin from "../../mixins/apiMixin";
+import RouteMixin from "../mixins/routeMixin";
+import GroupState from "../mixins/groupState";
+import ApiMixin from "../mixins/apiMixin";
 
-import GroupChart from "../groupDetails/chart";
-import GroupEventEntries from "../../components/events/eventEntries";
-import LoadingError from "../../components/loadingError";
-import LoadingIndicator from "../../components/loadingIndicator";
-import SeenInfo from "./seenInfo";
-import TagDistributionMeter from "./tagDistributionMeter";
+import EventEntries from "../components/events/eventEntries";
+import GroupSidebar from "../components/group/sidebar";
+import LoadingError from "../components/loadingError";
+import LoadingIndicator from "../components/loadingIndicator";
 
 var GroupOverview = React.createClass({
   contextTypes: {
@@ -80,53 +78,9 @@ var GroupOverview = React.createClass({
     var orgId = this.getOrganization().slug;
     var projectId = this.getProject().slug;
 
-    var tagList = [];
-    for (var key in group.tags) {
-      tagList.push([group.tags[key].name, key]);
-    }
-    tagList.sort();
-
     return (
       <div>
         <div className="row group-overview">
-          <div className="col-md-3 group-stats-column">
-            <div className="group-stats">
-              <GroupChart statsPeriod="24h" group={group}
-                          title="Last 24 Hours"
-                          firstSeen={group.firstSeen}
-                          lastSeen={group.lastSeen} />
-              <GroupChart statsPeriod="30d" group={group}
-                          title="Last 30 Days"
-                          className="bar-chart-small"
-                          firstSeen={group.firstSeen}
-                          lastSeen={group.lastSeen} />
-
-              <h6 className="first-seen"><span>First seen</span></h6>
-              <SeenInfo
-                  orgId={orgId}
-                  projectId={projectId}
-                  date={group.firstSeen}
-                  release={group.firstRelease} />
-
-              <h6 className="last-seen"><span>Last seen</span></h6>
-              <SeenInfo
-                  orgId={orgId}
-                  projectId={projectId}
-                  date={group.lastSeen}
-                  release={group.lastRelease} />
-
-              <h6><span>Tags</span></h6>
-              {tagList.map((data) => {
-                return (
-                  <TagDistributionMeter
-                    key={data[0]}
-                    group={group}
-                    name={data[0]}
-                    tag={data[1]} />
-                );
-              })}
-            </div>
-          </div>
           <div className="col-md-9">
             {this.state.loading ?
               <LoadingIndicator />
@@ -147,11 +101,12 @@ var GroupOverview = React.createClass({
                     </div>
                     This summary is based on the most recent event in this aggregate.
                 </div>
-                <GroupEventEntries
-                    group={group}
-                    event={evt} />
+                <EventEntries group={group} event={evt} />
               </div>
             )}
+          </div>
+          <div className="col-md-3">
+            <GroupSidebar group={group} />
           </div>
         </div>
       </div>
