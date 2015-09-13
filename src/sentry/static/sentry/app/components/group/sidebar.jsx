c0ca3c653913a6a02f6f6c4e273013562b813c1c
David Cramer <dcramer@gmail.com>
2015-09-13 11:04:42 -0700

    Render aggregate sidebar on event details (refs GH-1947)

---
+++ src/sentry/static/sentry/app/components/group/sidebar.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import GroupChart from "./chart";
+import GroupState from "../../mixins/groupState";
+import SeenInfo from "./seenInfo";
+import TagDistributionMeter from "./tagDistributionMeter";
+import PropTypes from "../../proptypes";
+
+var GroupSidebar = React.createClass({
+  mixins: [GroupState],
+
+  propTypes: {
+    event: PropTypes.Event.isRequired
+  },
+
+  render(){
+    var orgId = this.getOrganization().slug;
+    var projectId = this.getProject().slug;
+    var group = this.getGroup();
+    var tagList = [];
+    for (var key in group.tags) {
+      tagList.push([group.tags[key].name, key]);
+    }
+    tagList.sort();
+
+    return (
+      <div className="group-stats">
+        <GroupChart statsPeriod="24h" group={group}
+                    title="Last 24 Hours"
+                    firstSeen={group.firstSeen}
+                    lastSeen={group.lastSeen} />
+        <GroupChart statsPeriod="30d" group={group}
+                    title="Last 30 Days"
+                    className="bar-chart-small"
+                    firstSeen={group.firstSeen}
+                    lastSeen={group.lastSeen} />
+
+        <h6 className="first-seen"><span>First seen</span></h6>
+        <SeenInfo
+            orgId={orgId}
+            projectId={projectId}
+            date={group.firstSeen}
+            release={group.firstRelease} />
+
+        <h6 className="last-seen"><span>Last seen</span></h6>
+        <SeenInfo
+            orgId={orgId}
+            projectId={projectId}
+            date={group.lastSeen}
+            release={group.lastRelease} />
+
+        <h6><span>Tags</span></h6>
+        {tagList.map((data) => {
+          return (
+            <TagDistributionMeter
+              key={data[0]}
+              group={group}
+              name={data[0]}
+              tag={data[1]} />
+          );
+        })}
+      </div>
+    );
+  }
+});
+
+export default GroupSidebar;
--- src/sentry/static/sentry/app/views/groupDetails/event.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/event.jsx
@@ -1,13 +1,8 @@
 import React from "react";
-import DateTime from "../../components/dateTime";
-import FileSize from "../../components/fileSize";
-import GroupEventEntries from "../../components/events/eventEntries";
+import EventEntries from "../../components/events/eventEntries";
 import GroupState from "../../mixins/groupState";
+import GroupSidebar from "../../components/group/sidebar";
 import PropTypes from "../../proptypes";
-import TimeSince from "../../components/timeSince";
-
-import UserWidget from "./userWidget";
-import ReleaseWidget from "./releaseWidget";
 
 var GroupEvent = React.createClass({
   mixins: [GroupState],
@@ -23,30 +18,10 @@ var GroupEvent = React.createClass({
     return (
       <div className="row event">
         <div className="col-md-9">
-          <GroupEventEntries
-              group={group}
-              event={evt} />
+          <EventEntries group={group} event={evt} />
         </div>
         <div className="col-md-3">
-          <div className="event-stats group-stats">
-            <h6><span>Meta</span></h6>
-            <dl>
-              <dt>ID:</dt>
-              <dd className="truncate">{evt.eventID}</dd>
-              <dt>When:</dt>
-              <dd><TimeSince date={evt.dateCreated} /></dd>
-              <dt>Date:</dt>
-              <dd><DateTime date={evt.dateCreated} /></dd>
-              <dt>Size:</dt>
-              <dd><FileSize bytes={evt.size} /></dd>
-            </dl>
-            {evt.user &&
-              <UserWidget data={evt.user} />
-            }
-            {evt.release &&
-              <ReleaseWidget data={evt.release} />
-            }
-          </div>
+          <GroupSidebar group={group} />
         </div>
       </div>
     );
