14b55f9b5fd8a0ca70b929f76106483b46a6cac5
David Cramer <dcramer@gmail.com>
2015-09-15 11:07:39 -0700

    Fix bad queries

--- src/sentry/utils/apidocs.py
+++ src/sentry/utils/apidocs.py
@@ -242,7 +242,7 @@ class MockUtils(object):
         from sentry.models import User
         user, _ = User.objects.get_or_create(
             username=mail,
-            defaults={
+            default={
                 'email': mail,
             }
         )
@@ -259,7 +259,7 @@ class MockUtils(object):
         dummy_member, _ = OrganizationMember.objects.create(
             user=owner,
             organization=org,
-            defaults={
+            default={
                 'has_global_access': False,
             }
         )
@@ -288,7 +288,7 @@ class MockUtils(object):
         from sentry.models import Team
         return Team.objects.get_or_create(
             name=name,
-            defaults={
+            default={
                 'organization': org,
             },
         )[0]
@@ -298,7 +298,7 @@ class MockUtils(object):
         return Project.objects.get_or_create(
             team=team,
             name=name,
-            defaults={
+            default={
                 'organization': org,
             }
         )[0]
