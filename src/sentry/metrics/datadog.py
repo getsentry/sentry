e1f9a0580c7765012080ae62b3dbb616c6bc108a
David Cramer <dcramer@gmail.com>
2015-09-15 11:20:19 -0700

    Add default host tag to Datadog

--- src/sentry/metrics/datadog.py
+++ src/sentry/metrics/datadog.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 __all__ = ['DatadogMetricsBackend']
 
 from datadog import initialize, ThreadStats
+from datadog.util.hostname import get_hostname
 
 from .base import MetricsBackend
 
@@ -10,6 +11,11 @@ from .base import MetricsBackend
 class DatadogMetricsBackend(MetricsBackend):
     def __init__(self, prefix=None, **kwargs):
         # TODO(dcramer): it'd be nice if the initialize call wasn't a global
+        self.tags = kwargs.pop('tags', None)
+        if 'host' in kwargs:
+            self.host = kwargs.pop('host')
+        else:
+            self.host = get_hostname()
         initialize(**kwargs)
         self._stats = ThreadStats()
         self._stats.start()
@@ -19,7 +25,12 @@ class DatadogMetricsBackend(MetricsBackend):
         self._stats.stop()
 
     def incr(self, key, amount=1, sample_rate=1):
-        self._stats.increment(self._get_key(key), amount, sample_rate=sample_rate)
+        self._stats.increment(self._get_key(key), amount,
+                              sample_rate=sample_rate,
+                              tags=self.tags,
+                              host=self.host)
 
     def timing(self, key, value, sample_rate=1):
-        self._stats.timing(self._get_key(key), value, sample_rate=sample_rate)
+        self._stats.timing(self._get_key(key), value, sample_rate=sample_rate,
+                           tags=self.tags,
+                           host=self.host)
