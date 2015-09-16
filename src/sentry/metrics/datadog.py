1042de941732322703a9346c58537977ff2c903a
David Cramer <dcramer@gmail.com>
2015-09-15 21:50:32 -0700

    Tags are a list of a strings for Datadog

--- src/sentry/metrics/datadog.py
+++ src/sentry/metrics/datadog.py
@@ -36,6 +36,7 @@ class DatadogMetricsBackend(MetricsBackend):
             tags.update(self.tags)
         if instance:
             tags['instance'] = instance
+        tags = ['{}:{}'.format(*i) for i in tags.items()]
         self.stats.increment(self._get_key(key), amount,
                              sample_rate=sample_rate,
                              tags=tags,
@@ -48,6 +49,7 @@ class DatadogMetricsBackend(MetricsBackend):
             tags.update(self.tags)
         if instance:
             tags['instance'] = instance
+        tags = ['{}:{}'.format(*i) for i in tags.items()]
         self.stats.timing(self._get_key(key), value, sample_rate=sample_rate,
                           tags=tags,
                           host=self.host)
