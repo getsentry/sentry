ca74185d51759ad6d1fab21b61cdb5d30773418a
Ted Kaemming <ted@kaemming.com>
2015-09-21 15:20:31 -0700

    Move backoff bits around.

--- src/sentry/timelines/base.py
+++ src/sentry/timelines/base.py
@@ -1,2 +1,15 @@
 class Backend(object):
     pass
+
+
+class Backoff(object):
+    def __init__(self, function, maximum_steps=2):
+        self.function = function
+        self.maximum_steps = maximum_steps
+
+    def __call__(self, step):
+        return self.function(min(step, self.maximum_steps))
+
+    @property
+    def maximum(self):
+        return self(self.maximum_steps)
