78ae760ab29c19e523ff1b8291bea2da48492e24
Matt Robenolt <matt@ydekproductions.com>
2015-09-22 13:25:48 -0700

    Add versioning to plugin configurations

--- src/sentry/plugins/base/v1.py
+++ src/sentry/plugins/base/v1.py
@@ -14,11 +14,14 @@ import logging
 from django.core.urlresolvers import reverse
 from django.http import HttpResponseRedirect
 from threading import local
+from hashlib import md5
 
 from sentry.auth import access
 from sentry.plugins.base.response import Response
 from sentry.plugins.base.view import PluggableViewMixin
-from sentry.plugins.base.configuration import default_plugin_config
+from sentry.plugins.base.configuration import (
+    default_plugin_config, default_plugin_options,
+)
 
 
 class PluginMount(type):
@@ -166,6 +169,25 @@ class IPlugin(local, PluggableViewMixin):
         if not self.conf_key:
             return self.get_conf_title().lower().replace(' ', '_')
         return self.conf_key
+
+    def get_conf_form(self, project=None):
+        if project is not None:
+            return self.project_conf_form
+        return self.site_conf_form
+
+    def get_conf_template(self, project=None):
+        if project is not None:
+            return self.project_conf_template
+        return self.site_conf_template
+
+    def get_conf_options(self, project=None):
+        return default_plugin_options(self, project)
+
+    def get_conf_version(self, project):
+        options = self.get_conf_options(project)
+        return md5(
+            '&'.join(sorted('%s=%s' % o for o in options.iteritems()))
+        ).hexdigest()[:3]
 
     def get_conf_title(self):
         """
