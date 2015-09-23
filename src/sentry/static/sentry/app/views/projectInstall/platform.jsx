3c1129ef03e6d4498b30a59421f14e6e215a488c
David Cramer <dcramer@gmail.com>
2015-09-22 17:44:07 -0700

    Some markup tweaks

--- src/sentry/static/sentry/app/views/projectInstall/platform.jsx
+++ src/sentry/static/sentry/app/views/projectInstall/platform.jsx
@@ -70,23 +70,47 @@ var ProjectInstallPlatform = React.createClass({
     let {platform} = params;
 
     return (
-      <div>
-        <h1>Configure {this.static.platforms[platform].display}</h1>
+      <div className="row">
+        <div className="col-md-2">
+          <h6 className="nav-header">Python</h6>
+          <ul className="nav nav-stacked">
+            <li className="active"><a href="#">Generic</a></li>
+            <li><a href="#">Bottle</a></li>
+            <li><a href="#">Django</a></li>
+          </ul>
+          <h6 className="nav-header">JavaScript</h6>
+          <ul className="nav nav-stacked">
+            <li><a href="#">Generic</a></li>
+            <li><a href="#">Angular</a></li>
+            <li><a href="#">Ember</a></li>
+            <li><a href="#">React</a></li>
+          </ul>
+        </div>
+        <div className="col-md-10">
+          <div className="box">
+            <div className="box-content with-padding">
 
-        {this.state.loading
-          ? <LoadingIndicator/>
-          : (
-            <div>
-              <p>
-                This is a quick getting started guide. For in-depth instructions on integrating Sentry with {this.state.name}, view <a href={this.state.link}>our complete documentation</a>.
-              </p>
-              <div dangerouslySetInnerHTML={{__html: this.state.html }}/>
-              <Link to="stream" params={params} className="btn btn-primary">Continue</Link>
-              <Link to="projectInstall" params={params} className="btn btn-default">Back to all platforms</Link>
+              <div className="pull-right">
+                <a href={this.state.link} className="btn btn-default">Full Documentation</a>
+              </div>
+
+              <h1>Configure {this.static.platforms[platform].display}</h1>
+
+              {this.state.loading
+                ? <LoadingIndicator/>
+                : (
+                  <div>
+                    <p>
+                      This is a quick getting started guide. For in-depth instructions on integrating Sentry with {this.state.name}, view <a href={this.state.link}>our complete documentation</a>.
+                    </p>
+                    <div dangerouslySetInnerHTML={{__html: this.state.html}}/>
+                    <Link to="stream" params={params} className="btn btn-primary">Continue</Link>
+                  </div>
+                )
+              }
             </div>
-          )
-        }
-
+          </div>
+        </div>
       </div>
     );
   }
