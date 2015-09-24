bcdd0add888380643df02b3251170cb35f0b8684
Matt Robenolt <matt@ydekproductions.com>
2015-09-24 13:06:27 -0700

    Add backend feature for blacklisting IPs per-project

--- tests/sentry/web/api/tests.py
+++ tests/sentry/web/api/tests.py
@@ -67,6 +67,15 @@ class StoreViewTest(TestCase):
         self.assertIn('Access-Control-Allow-Origin', resp)
         self.assertEquals(resp['Access-Control-Allow-Origin'], 'http://foo.com')
 
+    @mock.patch('sentry.web.api.is_valid_ip', mock.Mock(return_value=False))
+    def test_request_with_backlisted_ip(self):
+        resp = self.client.get(self.path)
+        assert resp.status_code == 403, resp.content
+        resp = self.client.post(self.path)
+        assert resp.status_code == 403, resp.content
+        resp = self.client.options(self.path)
+        assert resp.status_code == 403, resp.content
+
     @mock.patch('sentry.coreapi.ClientApiHelper.insert_data_to_database')
     def test_scrubs_ip_address(self, mock_insert_data_to_database):
         self.project.update_option('sentry:scrub_ip_address', True)
