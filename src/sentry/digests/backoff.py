77266f9376d0e93e802ee7bbcd927b549dbb42cb
Ted Kaemming <ted@kaemming.com>
2015-09-24 10:44:58 -0700

    Enable absolute imports for CI.

--- src/sentry/digests/backoff.py
+++ src/sentry/digests/backoff.py
@@ -1,3 +1,6 @@
+from __future__ import absolute_import
+
+
 class BackoffStrategy(object):
     def __call__(self, iteration):
         raise NotImplementedError
--- tests/sentry/digests/test_redis.py
+++ tests/sentry/digests/test_redis.py
@@ -1,3 +1,5 @@
+from __future__ import absolute_import
+
 import functools
 import itertools
 import mock
