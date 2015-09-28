f387d9917e1bc8d13a7b9271db618a3fa98bbd73
David Cramer <dcramer@gmail.com>
2015-09-28 12:14:36 -0700

    Add EventUser storage and capabilities
    This adds a new model for storing users seen within events (which
    were previously only stored as tags). It also updates the search
    engine to allow queries such as 'user.email:foo@example.com' and
    will look these up from EventUser first to try and map them up with
    the appropriate tag.
    
    Refs GH-2033

--- src/sentry/api/serializers/models/tagvalue.py
+++ src/sentry/api/serializers/models/tagvalue.py
@@ -1,11 +1,50 @@
 from __future__ import absolute_import
 
+import operator
+
+from django.db.models import Q
+
 from sentry.api.serializers import Serializer, register
-from sentry.models import TagValue
+from sentry.models import EventUser, TagValue
+
+
+def parse_user_tag(value):
+    lookup, value = value.split(':', 1)
+    if lookup == 'id':
+        lookup = 'ident'
+    return {lookup: value}
 
 
 @register(TagValue)
 class TagValueSerializer(Serializer):
+    def get_attrs(self, item_list, user):
+        user_lookups = [
+            Q(**parse_user_tag(i.value))
+            for i in item_list
+            if i.key == 'sentry:user'
+        ]
+
+        tag_labels = {}
+        if user_lookups:
+            tag_labels.update({
+                ('sentry:user', euser.tag_value): euser.get_label()
+                for euser in EventUser.objects.filter(
+                    reduce(operator.or_, user_lookups),
+                    project=item_list[0].project,
+                )
+            })
+
+        result = {}
+        for item in item_list:
+            try:
+                label = tag_labels[(item.key, item.value)]
+            except KeyError:
+                label = item.get_label()
+            result[item] = {
+                'name': label,
+            }
+        return result
+
     def serialize(self, obj, attrs, user):
         if obj.key.startswith('sentry:'):
             key = obj.key.split('sentry:', 1)[-1]
@@ -14,6 +53,7 @@ class TagValueSerializer(Serializer):
 
         return {
             'key': key,
+            'name': attrs['name'],
             'value': obj.value,
             'count': obj.times_seen,
             'lastSeen': obj.last_seen,
