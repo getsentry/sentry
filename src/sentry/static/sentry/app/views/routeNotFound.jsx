8d7199eb041dbfa6c93f26b4fa8866fe9477a647
Matt Robenolt <matt@ydekproductions.com>
2015-09-28 15:54:21 -0700

    append trailing slashes in react router
    fixes GH-2028

--- src/sentry/static/sentry/app/views/routeNotFound.jsx
+++ src/sentry/static/sentry/app/views/routeNotFound.jsx
@@ -4,6 +4,19 @@ import Footer from "../components/footer";
 import Header from "../components/header";
 
 var RouteNotFound = React.createClass({
+  statics: {
+    // Try and append a trailing slash to the route when we "404".
+    //
+    // Reference:
+    //   https://github.com/rackt/react-router/blob/0.13.x/examples/auth-flow/app.js#L46-L50
+    willTransitionTo(transition) {
+      let path = transition.path;
+      if (path.charAt(path.length-1) !== '/') {
+        transition.redirect(path + '/');
+      }
+    }
+  },
+
   getTitle() {
     return 'Page Not Found';
   },
