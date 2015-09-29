b212b1386666bcf9e1ca97f9ced3a273d9e6c3b7
David Cramer <dcramer@gmail.com>
2015-09-29 15:17:31 -0700

    Improve link flow to support unauthenticated pre-existing users

--- src/sentry/web/frontend/auth_link_identity.py
+++ src/sentry/web/frontend/auth_link_identity.py
@@ -1,37 +1,33 @@
 from __future__ import absolute_import, print_function
 
 from django.contrib import messages
+from django.contrib.auth import login
 from django.core.urlresolvers import reverse
 from django.utils.translation import ugettext_lazy as _
 
 from sentry.auth.helper import AuthHelper
 from sentry.models import AuthProvider, Organization, OrganizationMember
+from sentry.web.forms.accounts import AuthenticationForm
 from sentry.web.frontend.base import BaseView
 
 ERR_LINK_INVALID = _('Either you are not a member of the given organization or it does not exist.')
 
 
 class AuthLinkIdentityView(BaseView):
-    # TODO(dcramer): ideally we could show a login form here if they were auth'd
-    # as an invalid account
+    auth_required = False
+
+    def get_login_form(self, request):
+        return AuthenticationForm(
+            request, request.POST or None,
+            captcha=bool(request.session.get('needs_captcha')),
+        )
+
     def handle(self, request, organization_slug):
         try:
             organization = Organization.objects.get(
                 slug=organization_slug
             )
         except Organization.DoesNotExist:
-            messages.add_message(
-                request, messages.ERROR,
-                ERR_LINK_INVALID,
-            )
-            return self.redirect(reverse('sentry'))
-
-        try:
-            om = OrganizationMember.objects.get(
-                organization=organization,
-                user=request.user,
-            )
-        except OrganizationMember.DoesNotExist:
             messages.add_message(
                 request, messages.ERROR,
                 ERR_LINK_INVALID,
@@ -49,6 +45,52 @@ class AuthLinkIdentityView(BaseView):
             )
             return self.redirect(reverse('sentry-organization-home',
                                          args=[organization.slug]))
+        request.session.set_test_cookie()
+
+        if request.user.is_authenticated():
+            return self.handle_authed(request, organization, auth_provider)
+        return self.handle_anon(request, organization, auth_provider)
+
+    def handle_anon(self, request, organization, auth_provider):
+        """
+        Allow the user to authenticate and then link there account.
+
+        This functions differently than the standard login flow as we *always*
+        allow them to try a username/password, whereas the default flow would
+        force the standard organization login.
+        """
+        form = self.get_login_form(request)
+
+        if form.is_valid():
+            login(request, form.get_user())
+
+            request.session.pop('needs_captcha', None)
+
+            return self.handle_authed(request, organization, auth_provider)
+
+        elif request.POST and not request.session.get('needs_captcha'):
+            request.session['needs_captcha'] = 1
+            form = self.get_login_form(request)
+            form.errors.pop('captcha', None)
+
+        context = {
+            'form': form,
+            'organization': organization,
+        }
+        return self.respond('sentry/auth-link-login.html', context)
+
+    def handle_authed(self, request, organization, auth_provider):
+        try:
+            om = OrganizationMember.objects.get(
+                organization=organization,
+                user=request.user,
+            )
+        except OrganizationMember.DoesNotExist:
+            messages.add_message(
+                request, messages.ERROR,
+                ERR_LINK_INVALID,
+            )
+            return self.redirect(reverse('sentry'))
 
         if request.method == 'POST':
             helper = AuthHelper(
