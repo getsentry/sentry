0c56d2f3a4a8bf20824d930053a1b4e1ed7c563e
Ted Kaemming <ted@kaemming.com>
2015-09-29 14:47:15 -0700

    Scaffolding for preview email.

--- src/sentry/digests/__init__.py
+++ src/sentry/digests/__init__.py
@@ -2,7 +2,13 @@ from __future__ import absolute_import
 
 from collections import namedtuple
 
+from sentry.utils.dates import to_datetime
 
-Record = namedtuple('Record', 'key value timestamp')
+
+class Record(namedtuple('Record', 'key value timestamp')):
+    @property
+    def datetime(self):
+        return to_datetime(self.timestamp)
+
 
 ScheduleEntry = namedtuple('ScheduleEntry', 'key timestamp')
