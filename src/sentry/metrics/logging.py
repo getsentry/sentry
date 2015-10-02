4c14b0c8a8d5a4f62185b08c1c0e5f34a9707367
Ted Kaemming <ted@kaemming.com>
2015-10-02 11:53:59 -0700

    Add `result` tag to `jobs.duration` metric.

--- src/sentry/metrics/logging.py
+++ src/sentry/metrics/logging.py
@@ -12,11 +12,11 @@ class LoggingBackend(MetricsBackend):
     def incr(self, key, instance=None, tags=None, amount=1, sample_rate=1):
         logger.debug('%r: %+g', key, amount, extra={
             'instance': instance,
-            'tags': tags or [],
+            'tags': tags or {},
         })
 
     def timing(self, key, value, instance=None, tags=None, sample_rate=1):
         logger.debug('%r: %g ms', key, value, extra={
             'instance': instance,
-            'tags': tags or [],
+            'tags': tags or {},
         })
