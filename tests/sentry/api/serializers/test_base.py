7198a70795c3e68bd37eee44d479ebd94f4f4d31
Matt Robenolt <matt@ydekproductions.com>
2015-10-06 16:55:23 -0700

    backfill some more test cases

--- tests/sentry/api/serializers/test_base.py
+++ tests/sentry/api/serializers/test_base.py
@@ -2,8 +2,17 @@
 
 from __future__ import absolute_import
 
-from sentry.api.serializers import serialize
+from sentry.api.serializers import serialize, Serializer
 from sentry.testutils import TestCase
+
+
+class Foo(object):
+    pass
+
+
+class FooSerializer(Serializer):
+    def serialize(self, *args, **kwargs):
+        return 'lol'
 
 
 class BaseSerializerTest(TestCase):
@@ -15,8 +24,13 @@ class BaseSerializerTest(TestCase):
         # We don't want to assert on the value, just that it serialized
         assert isinstance(serialize(user), dict)
 
-        f = type('lol', (), {})()
-        assert serialize(f) is f, 'should return the object when unknown'
+        # explicitly passed serializer
+        foo_serializer = FooSerializer()
+        assert serialize(user, serializer=foo_serializer) == 'lol'
+
+        foo = Foo()
+        assert serialize(foo) is foo, 'should return the object when unknown'
+        assert serialize(foo, serializer=foo_serializer) == 'lol'
 
         rv = serialize([user])
         assert isinstance(rv, list)
