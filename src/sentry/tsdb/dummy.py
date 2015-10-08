8ebec493b086525d23bbe4110c9d277c9b9b8301
Ted Kaemming <ted@kaemming.com>
2015-10-08 12:00:35 -0700

    Add support for DummyTSDB backend.

--- src/sentry/tsdb/dummy.py
+++ src/sentry/tsdb/dummy.py
@@ -19,3 +19,12 @@ class DummyTSDB(BaseTSDB):
 
     def get_range(self, model, keys, start, end, rollup=None):
         return dict((k, []) for k in keys)
+
+    def record(self, model, key, values, timestamp=None):
+        pass
+
+    def get_distinct_counts_series(self, model, keys, start, end=None, rollup=None):
+        return {k: [] for k in keys}
+
+    def get_distinct_counts_totals(self, model, keys, start, end=None, rollup=None):
+        return {k: 0 for k in keys}
