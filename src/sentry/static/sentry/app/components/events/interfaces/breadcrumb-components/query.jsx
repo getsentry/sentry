59460c48c253218abef5a165beaf80795a302711
Armin Ronacher <armin.ronacher@active-4.com>
2015-10-09 22:08:14 +0200

    Render parameters in queries

--- src/sentry/static/sentry/app/components/events/interfaces/breadcrumb-components/query.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/breadcrumb-components/query.jsx
@@ -9,9 +9,21 @@ var QueryCrumbComponent = React.createClass({
 
   render() {
     var data = this.props.data;
+    var placeholderIdx = 0;
+    var queryElements = [];
+
+    data.query.split(/(%s)/).forEach((item) => {
+      queryElements.push(
+        item === '%s'
+          ? <span className="param">{
+              data.params ? data.params[placeholderIdx++] : item}</span>
+          : <span className="literal">{item}</span>
+      );
+    });
+
     return (
       <p>
-        <strong>Query:</strong> <code>{data.query}</code>
+        <strong>Query:</strong> <code>{queryElements}</code>
         <Classifier value={data.classifier} title="%s query" prefix="query" />
       </p>
     );
