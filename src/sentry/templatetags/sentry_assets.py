31bae84a6cbf2458b2d2d357224c9098385d30e8
David Cramer <dcramer@gmail.com>
2015-10-09 17:04:06 -0700

    Support STATIC_URL correctly

--- src/sentry/templatetags/sentry_assets.py
+++ src/sentry/templatetags/sentry_assets.py
@@ -1,8 +1,8 @@
 from __future__ import absolute_import
 
 from django.conf import settings
-from django.core.urlresolvers import reverse
 from django.template import Library
+from urlparse import urljoin
 
 register = Library()
 
@@ -16,8 +16,7 @@ def asset_url(module, path):
       {% asset_url 'sentry' 'dist/sentry.css' %}
       =>  "/_static/74d127b78dc7daf2c51f/sentry/dist/sentry.css"
     """
-    return reverse('sentry-media', kwargs={
-        'version': settings.ASSET_VERSION,
-        'module': module,
-        'path': path,
-    })
+    return urljoin(
+        settings.STATIC_URL.format(version=settings.ASSET_VERSION),
+        '{}/{}'.format(module, path)
+    )
