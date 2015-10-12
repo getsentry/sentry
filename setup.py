98ec72caf110dc18aaed349f6f8a4c1bbf8c6a5c
David Cramer <dcramer@gmail.com>
2015-10-11 22:04:02 -0700

    7.7.1

--- setup.py
+++ setup.py
@@ -192,7 +192,7 @@ class SmartInstall(install):
 
 setup(
     name='sentry',
-    version='7.7.0',
+    version='7.7.1',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='https://www.getsentry.com',
