613ecfa161347d2b11db802bba9e043f1b66b34c
Matt Robenolt <matt@ydekproductions.com>
2015-10-14 13:12:08 -0700

    Improvements to CSP reporting
    * Reject some unusable reports
    * Better validation on fields in report
    * Improve grouping logic
    * More human friendly message

--- tests/sentry/coreapi/tests.py
+++ tests/sentry/coreapi/tests.py
@@ -342,8 +342,8 @@ class CspApiHelperTest(BaseAPITest):
         report = {
             "document-uri": "http://45.55.25.245:8123/csp",
             "referrer": "http://example.com",
-            "violated-directive": "child-src https://45.55.25.245:8123/",
-            "effective-directive": "frame-src",
+            "violated-directive": "img-src https://45.55.25.245:8123/",
+            "effective-directive": "img-src",
             "original-policy": "default-src  https://45.55.25.245:8123/; child-src  https://45.55.25.245:8123/; connect-src  https://45.55.25.245:8123/; font-src  https://45.55.25.245:8123/; img-src  https://45.55.25.245:8123/; media-src  https://45.55.25.245:8123/; object-src  https://45.55.25.245:8123/; script-src  https://45.55.25.245:8123/; style-src  https://45.55.25.245:8123/; form-action  https://45.55.25.245:8123/; frame-ancestors 'none'; plugin-types 'none'; report-uri http://45.55.25.245:8123/csp-report?os=OS%20X&device=&browser_version=43.0&browser=chrome&os_version=Lion",
             "blocked-uri": "http://google.com",
             "status-code": 200
