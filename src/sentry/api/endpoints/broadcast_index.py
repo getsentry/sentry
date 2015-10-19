3b96c005797df1496ba1f159b7fae5e505cdd79e
David Cramer <dcramer@gmail.com>
2015-10-18 19:42:06 -0700

    Add the ability to mark broadcasts as seen

--- src/sentry/api/endpoints/broadcast_index.py
+++ src/sentry/api/endpoints/broadcast_index.py
@@ -1,11 +1,17 @@
 from __future__ import absolute_import
 
+from django.db import IntegrityError, transaction
+from rest_framework import serializers
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from sentry.api.base import Endpoint
 from sentry.api.serializers import serialize
-from sentry.models import Broadcast
+from sentry.models import Broadcast, BroadcastSeen
+
+
+class BroadcastSerializer(serializers.Serializer):
+    hasSeen = serializers.BooleanField()
 
 
 class BroadcastIndexEndpoint(Endpoint):
@@ -17,3 +23,28 @@ class BroadcastIndexEndpoint(Endpoint):
         ))
 
         return Response(serialize(broadcasts, request.user))
+
+    def put(self, request):
+        serializer = BroadcastSerializer(data=request.DATA, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        result = serializer.object
+
+        # limit scope of query
+        queryset = Broadcast.objects.filter(
+            is_active=True,
+        )[:100]
+
+        if result.get('hasSeen'):
+            for broadcast in queryset:
+                try:
+                    with transaction.atomic():
+                        BroadcastSeen.objects.create(
+                            broadcast=broadcast,
+                            user=request.user,
+                        )
+                except IntegrityError:
+                    pass
+
+        return Response(result)
