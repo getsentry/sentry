b50bee2c2d1e9aa4e1e18ebf7d663032095c0c87
Matt Robenolt <matt@ydekproductions.com>
2015-10-19 13:13:14 -0700

    From webpack, always set frame.in_app explicitly.
    The client will always send a value for in_app, but in the case of
    webpack, we want to clobber it entirely. We cannot reasonable calculate
    this from the client when sourcemaps are involved.

--- src/sentry/lang/javascript/processor.py
+++ src/sentry/lang/javascript/processor.py
@@ -561,14 +561,13 @@ class SourceProcessor(object):
                     else:
                         filename = filename.split('webpack:///', 1)[-1]
 
-                    if frame.in_app is None:
-                        # As noted above, '~/' means they're coming from node_modules,
-                        # so these are not app dependencies
-                        if filename.startswith('~/'):
-                            frame.in_app = False
-                        # And conversely, local dependencies start with './'
-                        elif filename.startswith('./'):
-                            frame.in_app = True
+                    # As noted above, '~/' means they're coming from node_modules,
+                    # so these are not app dependencies
+                    if filename.startswith('~/'):
+                        frame.in_app = False
+                    # And conversely, local dependencies start with './'
+                    elif filename.startswith('./'):
+                        frame.in_app = True
 
                     # We want to explicitly generate a webpack module name
                     frame.module = generate_module(filename)
