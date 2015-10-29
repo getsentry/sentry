d54496031ca5288d1cbe0bcf08ae4254639e8e8b
Ben Vinegar <ben@benv.ca>
2015-10-29 16:11:37 -0700

    Fix search by event id from stream (fixes #2248)

--- src/sentry/static/sentry/app/views/stream.jsx
+++ src/sentry/static/sentry/app/views/stream.jsx
@@ -207,7 +207,7 @@ const Stream = React.createClass({
           let params = this.props.params;
           let groupId = data[0].id;
 
-          return void this.history.pushState(null, `/${params.orgId}/${params.projectId}/${groupId}/`);
+          return void this.history.pushState(null, `/${params.orgId}/${params.projectId}/group/${groupId}/`);
         }
 
         this._streamManager.push(data);
