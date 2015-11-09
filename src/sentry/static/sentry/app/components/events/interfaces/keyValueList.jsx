0436fd4cff3e082252b45d2996bdbecca5d6e5f5
ckj <chriskjennings@gmail.com>
2015-11-09 12:55:05 -0800

    Move key to tr

--- src/sentry/static/sentry/app/components/events/interfaces/keyValueList.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/keyValueList.jsx
@@ -22,16 +22,16 @@ const KeyValueList = React.createClass({
         {data.map(([key, value]) => {
           if (this.props.isContextData) {
             return [
-              <tr>
-                <td key={'dt-' + key}>{key}</td>
-                <td key={'dd-' + key}><ContextData data={value}/></td>
+              <tr key={key}>
+                <td>{key}</td>
+                <td><ContextData data={value}/></td>
               </tr>
             ];
           } else {
             return [
-              <tr>
-                <td key={'dt-' + key}>{key}</td>
-                <td key={'dd-' + key}><pre>{'' + value || ' '}</pre></td>
+              <tr key={key}>
+                <td>{key}</td>
+                <td><pre>{'' + value || ' '}</pre></td>
               </tr>
             ];
           }
