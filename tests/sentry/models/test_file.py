0cd4ec6c324dd7ea21e234736c071addc5ecec51
David Cramer <dcramer@gmail.com>
2015-11-10 13:26:55 -0800

    Improve file conversion behavior + tests

--- tests/sentry/models/test_file.py
+++ tests/sentry/models/test_file.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from django.conf import settings
 from django.core.files.base import ContentFile
 
-from sentry.models import FileBlob
+from sentry.models import File, FileBlob
 from sentry.testutils import TestCase
 
 
@@ -17,4 +17,42 @@ class FileBlobTest(TestCase):
         assert my_file1.storage == settings.SENTRY_FILESTORE
 
         my_file2 = FileBlob.from_file(fileobj)
-        assert my_file1 == my_file2
+        # deep check
+        assert my_file1.id == my_file2.id
+        assert my_file1.checksum == my_file2.checksum
+        assert my_file1.path == my_file2.path
+        assert my_file1.storage == my_file2.storage
+        assert my_file1.storage_options == my_file2.storage_options
+
+
+class FileTest(TestCase):
+    def test_blob_conversion(self):
+        file1 = File.objects.create(
+            path='foo/bar',
+            name='baz.js',
+            type='default',
+            size=100,
+            checksum='a' * 40,
+            storage=settings.SENTRY_FILESTORE,
+        )
+        file1.ensure_blob()
+
+        assert file1.blob
+        assert file1.blob.path == 'foo/bar'
+        assert file1.blob.size == 100
+        assert file1.blob.checksum == 'a' * 40
+
+        file2 = File.objects.create(
+            path='foo/bar',
+            name='baz.js',
+            type='default',
+            size=100,
+            checksum='a' * 40,
+            storage=settings.SENTRY_FILESTORE,
+        )
+        file2.ensure_blob()
+
+        assert file2.blob == file1.blob
+        assert file2.blob.path == 'foo/bar'
+        assert file2.blob.size == 100
+        assert file2.blob.checksum == 'a' * 40
