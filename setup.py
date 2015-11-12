7d365457de3e218e12a9aeae5796b268831f3feb
Matt Robenolt <matt@ydekproductions.com>
2015-11-12 07:58:37 -0800

    7.7.4

--- setup.py
+++ setup.py
@@ -192,7 +192,7 @@ class SmartInstall(install):
 
 setup(
     name='sentry',
-    version='7.7.3',
+    version='7.7.4',
     author='David Cramer',
     author_email='dcramer@gmail.com',
     url='https://www.getsentry.com',
