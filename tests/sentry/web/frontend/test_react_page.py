e77c59797b0557f8ec750a023347bd864c80b445
David Cramer <dcramer@gmail.com>
2015-11-20 13:50:23 -0800

    Expand tests for org perms

--- tests/sentry/web/frontend/test_react_page.py
+++ tests/sentry/web/frontend/test_react_page.py
@@ -6,7 +6,7 @@ from sentry.testutils import TestCase
 
 
 class ReactPageViewTest(TestCase):
-    def test_renders_with_context(self):
+    def test_superuser_can_load(self):
         org = self.create_organization(owner=self.user)
         path = reverse('sentry-organization-home', args=[org.slug])
 
@@ -19,12 +19,13 @@ class ReactPageViewTest(TestCase):
         assert resp.context['request']
 
     def test_redirects_user_to_auth_without_membership(self):
-        org = self.create_organization(owner=self.user)
-        user = self.create_user('foo@example.com')
+        owner = self.create_user('bar@example.com')
+        org = self.create_organization(owner=owner)
+        non_member = self.create_user('foo@example.com')
 
         path = reverse('sentry-organization-home', args=[org.slug])
 
-        self.login_as(user)
+        self.login_as(non_member)
 
         resp = self.client.get(path)
 
@@ -42,3 +43,14 @@ class ReactPageViewTest(TestCase):
         assert resp['Location'] != 'http://testserver{}'.format(
             reverse('sentry-auth-organization', args=[org.slug]),
         )
+
+        # ensure we dont redirect with valid membership
+        path = reverse('sentry-organization-home', args=[org.slug])
+
+        self.login_as(owner)
+
+        resp = self.client.get(path)
+
+        assert resp.status_code == 200
+        self.assertTemplateUsed(resp, 'sentry/bases/react.html')
+        assert resp.context['request']
