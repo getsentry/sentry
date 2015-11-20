70139cd7a7a3d71616943db80b01d05b5208310a
David Cramer <dcramer@gmail.com>
2015-11-20 11:41:16 -0800

    Add date_joined to user display

--- src/sentry/admin.py
+++ src/sentry/admin.py
@@ -180,7 +180,7 @@ class UserAdmin(admin.ModelAdmin):
     form = UserChangeForm
     add_form = UserCreationForm
     change_password_form = AdminPasswordChangeForm
-    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff')
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
     list_filter = ('is_staff', 'is_superuser', 'is_active', 'is_managed')
     search_fields = ('username', 'first_name', 'last_name', 'email')
     ordering = ('username',)
