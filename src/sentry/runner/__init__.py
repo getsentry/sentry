ee49a2eb4af1b2332068f799ec35d500615170fb
Matt Robenolt <matt@ydekproductions.com>
2015-11-30 02:15:09 -0800

    Refactor `sentry` cli tooling to use Click
    * rm -rf logan
    * Support for loading config.yml files
    * Replace all of the builtin Django commands by Click commands
    * Hide django subcommands under `sentry django`
    * `sentry init` now should be pointed at a directory

---
+++ src/sentry/runner/__init__.py
@@ -0,0 +1,120 @@
+"""
+sentry.runner
+~~~~~~~~~~~~~
+
+:copyright: (c) 2015 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+from __future__ import absolute_import, print_function
+
+import os
+import click
+
+
+@click.group()
+@click.option(
+    '--config',
+    default='~/.sentry',
+    envvar='SENTRY_CONF',
+    help='Path to configuration files.',
+    metavar='PATH')
+@click.version_option()
+@click.pass_context
+def cli(ctx, config):
+    "<3"
+    ctx.obj['config'] = config
+
+
+# TODO(mattrobenolt): Autodiscover commands?
+from .commands.init import init
+cli.add_command(init)
+
+from .commands.start import start
+cli.add_command(start)
+
+from .commands.help import help
+cli.add_command(help)
+
+from .commands.repair import repair
+cli.add_command(repair)
+
+from .commands.django import django
+cli.add_command(django)
+
+from .commands.cleanup import cleanup
+cli.add_command(cleanup)
+
+from .commands.upgrade import upgrade
+cli.add_command(upgrade)
+
+from .commands.backup import import_, export
+cli.add_command(import_)
+cli.add_command(export)
+
+from .commands.createuser import createuser
+cli.add_command(createuser)
+
+
+def make_django_command(name, django_command=None, help=None):
+    "A wrapper to convert a Django subcommand a Click command"
+    if django_command is None:
+        django_command = name
+
+    @click.command(
+        name=name,
+        help=help,
+        add_help_option=False,
+        context_settings=dict(
+            ignore_unknown_options=True,
+        ))
+    @click.argument('management_args', nargs=-1, type=click.UNPROCESSED)
+    @click.pass_context
+    def inner(ctx, management_args):
+        ctx.params['management_args'] = (django_command,) + management_args
+        ctx.forward(django)
+
+    return inner
+
+
+map(cli.add_command, (
+    make_django_command('devserver', 'runserver', help='Start a light Web server for development.'),
+    make_django_command('shell', help='Run a Python interactive interpreter.'),
+    make_django_command('celery'),
+))
+
+
+def configure():
+    """
+    Kick things off and configure all the things.
+
+    A guess is made as to whether the entrypoint is coming from Click
+    or from another invocation of `configure()`. If Click, we're able
+    to pass along the Click context object.
+    """
+    from .settings import discover_configs, configure
+    try:
+        ctx = click.get_current_context()
+    except RuntimeError:
+        ctx = None
+    _, py, yaml = discover_configs(ctx)
+    configure(ctx, py, yaml)
+
+
+def get_prog():
+    """
+    Extract the proper program executable.
+
+    In the case of `python -m sentry`, we want to detect this and
+    make sure we return something useful rather than __main__.py
+    """
+    import sys
+    try:
+        if os.path.basename(sys.argv[0]) in ('__main__.py', '-c'):
+            return '%s -m sentry' % sys.executable
+    except (AttributeError, TypeError, IndexError):
+        pass
+    return 'sentry'
+
+
+def main():
+    cli(prog_name=get_prog(), obj={}, max_content_width=100)
---
+++ src/sentry/runner/settings.py
@@ -0,0 +1,110 @@
+"""
+sentry.runner.settings
+~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2015 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+from __future__ import absolute_import, print_function
+
+import os
+import click
+
+
+DEFAULT_SETTINGS_MODULE = 'sentry.conf.server'
+DEFAULT_SETTINGS_CONF = 'config.yml'
+DEFAULT_SETTINGS_OVERRIDE = 'sentry.conf.py'
+CONFIG_TEMPLATE = """# https://docs.getsentry.com/
+
+system.debug: false
+system.secret-key: %(default_key)r
+"""
+
+
+def generate_settings():
+    """
+    This command is run when ``default_path`` doesn't exist, or ``init`` is
+    run and returns a string representing the default data to put into their
+    settings file.
+    """
+    from base64 import b64encode
+    output = CONFIG_TEMPLATE % dict(
+        default_key=b64encode(os.urandom(40)),
+    )
+    return output
+
+
+def discover_configs(ctx=None):
+    """
+    Discover the locations of three configuration components:
+     * Config directory (~/.sentry)
+     * Optional python config file (~/.sentry/sentry.conf.py)
+     * Optional yaml config (~/.sentry/config.yml)
+    """
+    if ctx:
+        config = ctx.obj['config']
+    else:
+        try:
+            config = os.environ['SENTRY_CONF']
+        except KeyError:
+            config = '~/.sentry'
+
+    config = os.path.expanduser(config)
+
+    # This is the old, now deprecated code path where SENTRY_CONF is pointed directly
+    # to a python file
+    if config.endswith(('.py', '.conf')) or os.path.isfile(config):
+        return (
+            os.path.dirname(config),
+            config,
+            None,
+        )
+
+    return (
+        config,
+        os.path.join(config, DEFAULT_SETTINGS_OVERRIDE),
+        os.path.join(config, DEFAULT_SETTINGS_CONF),
+    )
+
+
+def configure(ctx, py, yaml):
+    """
+    Given the two different config files, set up the environment.
+
+    NOTE: Will only execute once, so it's safe to call multiple times.
+    """
+    global __installed
+    if __installed:
+        return
+
+    from .importer import install
+
+    if yaml is None:
+        # `yaml` will be None when SENTRY_CONF is pointed
+        # directly to a file, in which case, this file must exist
+        if not os.path.exists(py):
+            if ctx:
+                raise click.ClickException("Configuration file does not exist. Use '%s init' to initialize the file." % ctx.command_path)
+            raise ValueError("Configuration file does not exist at '%s'" % click.format_filename(py))
+    elif not os.path.exists(yaml):
+        if ctx:
+            raise click.ClickException("Configuration file does not exist. Use '%s init' to initialize the file." % ctx.command_path)
+        raise ValueError("Configuration file does not exist at '%s'" % click.format_filename(yaml))
+
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'sentry_config'
+    install('sentry_config', py, DEFAULT_SETTINGS_MODULE)
+
+    # TODO(mattrobenolt): clean up this and use the callbacks from install
+    from django.conf import settings
+    from .initializer import initialize_app, on_configure
+    initialize_app({
+        'config_path': py,
+        'settings': settings,
+        'options': yaml,
+    }, skip_backend_validation=True)  # TODO(mattrobenolt): Bring back env var
+    on_configure({'settings': settings})
+
+    __installed = True
+
+
+__installed = False
