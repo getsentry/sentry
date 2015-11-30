dc379d54cbcc3569b9aa7f9fb8b3c4e0e9e5bbe2
Armin Ronacher <armin.ronacher@active-4.com>
2015-11-30 01:01:34 +0100

    Automatically mark catalogs good if they have more than 80% translations.

--- src/sentry/static/sentry/app/translations.jsx
+++ src/sentry/static/sentry/app/translations.jsx
@@ -1,9 +1,16 @@
+const catalogs = (function() {
+  var info = require('../../../locale/catalogs.json');
+  return info.supported_catalogs;
+})();
+
 const translations = (function() {
   var ctx = require.context('../../../locale/', true, /\.po$/);
   var rv = {};
   ctx.keys().forEach((translation) => {
     var langCode = translation.match(/([a-zA-Z_]+)/)[1];
-    rv[langCode] = ctx(translation);
+    if (catalogs.indexOf(langCode) >= 0) {
+      rv[langCode] = ctx(translation);
+    }
   });
   return rv;
 })();
