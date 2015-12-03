342da37f14c6ac991bd3f7c904bd5b7f1196493a
Ted Kaemming <ted@kaemming.com>
2015-12-03 15:56:05 -0800

    Fix digests `DummyBackend` method signatures.
    This makes them consistent with the base backend API.
    
    RIP @tkaemming, stung by dynamic typing.

--- src/sentry/digests/backends/dummy.py
+++ src/sentry/digests/backends/dummy.py
@@ -6,11 +6,11 @@ from sentry.digests.backends.base import Backend
 
 
 class DummyBackend(Backend):
-    def add(self, key, record):
+    def add(self, key, record, increment_delay=None, maximum_delay=None):
         pass
 
     @contextmanager
-    def digest(self, key):
+    def digest(self, key, minimum_delay=None):
         yield []
 
     def schedule(self, deadline):
