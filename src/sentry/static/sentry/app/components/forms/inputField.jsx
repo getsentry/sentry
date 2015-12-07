99568723d73b0e83520b4683026d79a93a2255fe
David Cramer <dcramer@gmail.com>
2015-12-07 14:06:55 -0800

    Improve UX behind disabled inputs

--- src/sentry/static/sentry/app/components/forms/inputField.jsx
+++ src/sentry/static/sentry/app/components/forms/inputField.jsx
@@ -1,10 +1,32 @@
+import jQuery from 'jquery';
 import React from 'react';
+import ReactDOM from 'react-dom';
 import FormField from './formField';
 
 export default class InputField extends FormField {
   constructor(props) {
     super(props);
     this.state.value = props.defaultValue || '';
+  }
+
+  // XXX(dcramer): this comes from TooltipMixin
+  componentDidMount() {
+    this.attachTooltips();
+  }
+
+  componentWillUnmount() {
+    this.removeTooltips();
+    jQuery(ReactDOM.findDOMNode(this)).unbind();
+  }
+
+  attachTooltips() {
+    jQuery('.tip', ReactDOM.findDOMNode(this))
+      .tooltip();
+  }
+
+  removeTooltips() {
+    jQuery('.tip', ReactDOM.findDOMNode(this))
+      .tooltip('destroy');
   }
 
   onChange(e) {
@@ -20,6 +42,11 @@ export default class InputField extends FormField {
       <div className="control-group">
         <div className="controls">
           <label>{this.props.label}</label>
+          {this.props.disabled && this.props.disabledReason &&
+            <span className="disabled-indicator tip" title={this.props.disabledReason}>
+              <span className="icon-question" />
+            </span>
+          }
           <input type={this.getType()}
                  className="form-control"
                  placeholder={this.props.placeholder}
