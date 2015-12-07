a1e917a6cb6e70be7b26aa9d831e355c012e9588
David Cramer <dcramer@gmail.com>
2015-12-06 19:57:54 -0800

    Some initial framing for org dashboard

--- src/sentry/api/serializers/models/activity.py
+++ src/sentry/api/serializers/models/activity.py
@@ -36,18 +36,27 @@ class OrganizationActivitySerializer(ActivitySerializer):
             item_list, user,
         )
 
+        group_list = list(set([i.group for i in item_list if i.group]))
+        groups = dict(zip(
+            [g.id for g in group_list],
+            serialize(group_list, user)
+        ))
+
+        project_list = list(set([i.project for i in item_list]))
         projects = dict(zip(
-            item_list,
-            serialize([i.project for i in item_list], user)
+            [p.id for p in project_list],
+            serialize(project_list, user)
         ))
 
         for item in item_list:
-            attrs[item]['project'] = projects[item]
+            attrs[item]['issue'] = groups[item.group_id] if item.group_id else None
+            attrs[item]['project'] = projects[item.project_id]
         return attrs
 
     def serialize(self, obj, attrs, user):
         context = super(OrganizationActivitySerializer, self).serialize(
             obj, attrs, user,
         )
+        context['issue'] = attrs['issue']
         context['project'] = attrs['project']
         return context
