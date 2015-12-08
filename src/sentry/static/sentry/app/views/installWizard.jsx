37ec4984acfa035430e11a4aa7e50169b0331298
Matt Robenolt <matt@ydekproductions.com>
2015-12-07 17:07:09 -0800

    Make sure to alwyas call defaultValue

--- src/sentry/static/sentry/app/views/installWizard.jsx
+++ src/sentry/static/sentry/app/views/installWizard.jsx
@@ -28,7 +28,7 @@ const InstallWizardSettings = React.createClass({
     for (let key of missingOptions) {
       let option = options[key];
       if (!option.value) {
-        option.value = getOption(key).defaultValue;
+        option.value = getOption(key).defaultValue();
       }
       fields.push(getOptionField(key, this.onFieldChange.bind(this, key), option.value, option.field));
       // options is used for submitting to the server, and we dont submit values
