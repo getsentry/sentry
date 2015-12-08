14b2b83e0ef1323d46c7546a0f5ffa74f92e2c31
Ben Vinegar <ben@benv.ca>
2015-12-07 16:49:04 -0800

    Fix onUpdateError not handling undefined itemIds

--- tests/js/spec/stores/groupStore.spec.jsx
+++ tests/js/spec/stores/groupStore.spec.jsx
@@ -31,36 +31,43 @@ describe('GroupStore', function () {
     });
   });
 
-  describe('onUpdate()', function () {
-    it('should treat undefined itemIds argument as \'all\'', function () {
+  describe('update methods', function () {
+    beforeEach(function () {
       GroupStore.items = [
         {id: 1},
         {id: 2},
         {id: 3},
       ];
-
-      this.sandbox.stub(GroupStore, 'trigger');
-      GroupStore.onUpdate(1337, undefined, 'somedata');
-
-
-      expect(GroupStore.trigger.calledOnce).to.be.ok;
-      expect(GroupStore.trigger.firstCall.args[0]).to.eql(new Set([1,2,3]));
     });
-  });
 
-  describe('onUpdateSuccess()', function () {
-    it('should treat undefined itemIds argument as \'all\'', function () {
-      GroupStore.items = [
-        {id: 1},
-        {id: 2},
-        {id: 3},
-      ];
+    describe('onUpdate()', function () {
+      it('should treat undefined itemIds argument as \'all\'', function () {
+        this.sandbox.stub(GroupStore, 'trigger');
+        GroupStore.onUpdate(1337, undefined, 'somedata');
 
-      this.sandbox.stub(GroupStore, 'trigger');
-      GroupStore.onUpdateSuccess(1337, undefined, 'somedata');
+        expect(GroupStore.trigger.calledOnce).to.be.ok;
+        expect(GroupStore.trigger.firstCall.args[0]).to.eql(new Set([1,2,3]));
+      });
+    });
 
-      expect(GroupStore.trigger.calledOnce).to.be.ok;
-      expect(GroupStore.trigger.firstCall.args[0]).to.eql(new Set([1,2,3]));
+    describe('onUpdateSuccess()', function () {
+      it('should treat undefined itemIds argument as \'all\'', function () {
+        this.sandbox.stub(GroupStore, 'trigger');
+        GroupStore.onUpdateSuccess(1337, undefined, 'somedata');
+
+        expect(GroupStore.trigger.calledOnce).to.be.ok;
+        expect(GroupStore.trigger.firstCall.args[0]).to.eql(new Set([1,2,3]));
+      });
+    });
+
+    describe('onUpdateError()', function () {
+      it('should treat undefined itemIds argument as \'all\'', function () {
+        this.sandbox.stub(GroupStore, 'trigger');
+        GroupStore.onUpdateError(1337, undefined, 'something failed', false);
+
+        expect(GroupStore.trigger.calledOnce).to.be.ok;
+        expect(GroupStore.trigger.firstCall.args[0]).to.eql(new Set([1,2,3]));
+      });
     });
   });
 });
