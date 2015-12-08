3dfe299893a5c4259b2f6abd3f9e2e458a32ef44
Matt Robenolt <matt@ydekproductions.com>
2015-12-07 19:58:53 -0800

    Make sure all React pages have a CSRF token in session
    Fixes GH-2419

--- src/sentry/web/frontend/react_page.py
+++ src/sentry/web/frontend/react_page.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from django.core.context_processors import csrf
 from django.http import HttpResponse
 from django.template import loader, Context
 
@@ -9,6 +10,8 @@ from sentry.web.frontend.base import BaseView, OrganizationView
 class ReactMixin(object):
     def handle_react(self, request):
         context = Context({'request': request})
+        context.update(csrf(request))
+
         template = loader.render_to_string('sentry/bases/react.html', context)
 
         response = HttpResponse(template)
