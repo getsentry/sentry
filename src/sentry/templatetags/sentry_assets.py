5032d80ac91d9c25d3476517674d1b4ed9f817b2
Matt Robenolt <matt@ydekproductions.com>
2015-12-09 16:00:12 -0800

    Don't generate absolute asset urls by default
    Introduces a new template tag `absolute_asset_url` to give a fully
    resolved url.
    
    Fixes GH-2436

--- src/sentry/templatetags/sentry_assets.py
+++ src/sentry/templatetags/sentry_assets.py
@@ -8,13 +8,16 @@ from sentry.utils.http import absolute_uri
 register = Library()
 
 
+register.simple_tag(get_asset_url, name='asset_url')
+
+
 @register.simple_tag
-def asset_url(module, path):
+def absolute_asset_url(module, path):
     """
-    Returns a versioned asset URL (located within Sentry's static files).
+    Returns a versioned absolute asset URL (located within Sentry's static files).
 
     Example:
-      {% asset_url 'sentry' 'dist/sentry.css' %}
+      {% absolute_asset_url 'sentry' 'dist/sentry.css' %}
       =>  "http://sentry.example.com/_static/74d127b78dc7daf2c51f/sentry/dist/sentry.css"
     """
     return absolute_uri(get_asset_url(module, path))
