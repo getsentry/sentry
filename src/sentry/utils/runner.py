1b6579376007a74673d9e27fb1a8c54c2c78cf53
Matt Robenolt <matt@ydekproductions.com>
2015-12-11 11:10:19 -0800

    Add DeprecationWarning to `sentry.utils.runner` module

--- src/sentry/utils/runner.py
+++ src/sentry/utils/runner.py
@@ -10,3 +10,7 @@ from __future__ import absolute_import, print_function
 
 # Backwards compatibility
 from sentry.runner import configure, main  # NOQA
+
+import warnings
+warnings.warn("'sentry.utils.runner' has moved to 'sentry.runner'",
+              DeprecationWarning)
