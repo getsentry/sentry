ed18be31f51e35ca02d788929e7b09c86b60a058
Matt Robenolt <matt@ydekproductions.com>
2015-12-15 16:12:29 -0800

    Add a better type system for Options

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -9,16 +9,17 @@ from __future__ import absolute_import, print_function
 
 
 from sentry.options import register, FLAG_NOSTORE, FLAG_REQUIRED, FLAG_PRIORITIZE_DISK
+from sentry.options.types import Dict
 
 
 register('cache.backend', flags=FLAG_NOSTORE)
-register('cache.options', default={}, flags=FLAG_NOSTORE)
+register('cache.options', type=Dict, flags=FLAG_NOSTORE)
 register('system.admin-email', flags=FLAG_REQUIRED)
-register('system.databases', default={}, flags=FLAG_NOSTORE)
+register('system.databases', type=Dict, flags=FLAG_NOSTORE)
 register('system.debug', default=False, flags=FLAG_NOSTORE)
-register('system.rate-limit', default=0, type=int, flags=FLAG_PRIORITIZE_DISK)
+register('system.rate-limit', default=0, flags=FLAG_PRIORITIZE_DISK)
 register('system.secret-key', flags=FLAG_NOSTORE)
-register('redis.options', default={}, flags=FLAG_NOSTORE)
+register('redis.options', type=Dict, flags=FLAG_NOSTORE)
 
 # Absolute URL to the sentry root directory. Should not include a trailing slash.
 register('system.url-prefix', ttl=60, grace=3600, flags=FLAG_REQUIRED | FLAG_PRIORITIZE_DISK)
