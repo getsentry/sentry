59140aff3719ef965ed7f2d7397197317a2af0a3
David Cramer <dcramer@gmail.com>
2015-12-16 11:51:56 -0800

    Correct pytest-timeout req

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ tests_require = [
     'cqlsh',
     'datadog',
     'pytest-cov>=1.8.0,<1.9.0',
-    'pytest-timeout>=1.0.0,<1.1.0',
+    'pytest-timeout>=0.5.0,<0.6.0',
     'pytest-xdist>=1.11.0,<1.12.0',
     'python-coveralls',
     'responses',
