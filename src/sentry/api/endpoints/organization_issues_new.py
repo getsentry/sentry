14d166098f8772398bb0a4c3031094c537cb35dc
David Cramer <dcramer@gmail.com>
2015-12-17 12:41:28 -0800

    Add new organization issues endpoint

---
+++ src/sentry/api/endpoints/organization_issues_new.py
@@ -0,0 +1,19 @@
+from __future__ import absolute_import
+
+from datetime import timedelta
+from django.utils import timezone
+
+from sentry.api.bases import OrganizationIssuesEndpoint
+from sentry.models import Group, GroupStatus
+
+
+class OrganizationIssuesNewEndpoint(OrganizationIssuesEndpoint):
+    def get_queryset(self, request, organization, member, project_list):
+        cutoff = timezone.now() - timedelta(days=3)
+
+        return Group.objects.filter(
+            status=GroupStatus.UNRESOLVED,
+            active_at__gte=cutoff,
+        ).extra(
+            select={'sort_by': 'sentry_groupedmessage.first_seen'},
+        ).select_related('project').order_by('-sort_by')
