427b456f18c4040127f7a05805089f372e920df0
David Cramer <dcramer@gmail.com>
2016-01-04 15:42:35 -0800

    Expand on RouteError

--- src/sentry/static/sentry/app/views/routeError.jsx
+++ src/sentry/static/sentry/app/views/routeError.jsx
@@ -3,7 +3,6 @@ import Raven from 'raven-js';
 import React from 'react';
 
 import ConfigStore from '../stores/configStore';
-import {t} from '../locale';
 
 
 function loadScript(url) {
@@ -44,8 +43,24 @@ const RouteError = React.createClass({
     // TODO(dcramer): show additional resource links
     return (
       <div className="alert alert-block alert-error">
-        <span className="icon-exclamation" style={{marginRight: 10}}/>
-        {t('There was an unexpected error rendering this page.')}
+
+        <div style={{fontSize: 24, marginBottom: 10}}>
+          <span className="icon-exclamation" style={{fontSize: 20, marginRight: 10}} />
+          <span>Oops! Something went wrong</span>
+        </div>
+        <p>It looks like you've hit an issue in our client appliation. Don't worry
+          though! We use Sentry to monitor Sentry and it's likely we're already
+          looking into this!</p>
+        <p>If you're daring, you may want to try the following:</p>
+        <ul>
+          {window && window.adblockSuspected &&
+            <li>We detected something AdBlock-like. Try disabling it, as it's known to cause issues.</li>
+          }
+          <li>Give it a few seconds and <a onClick={() => {
+              window.location.href = window.location.href;
+            }}>reload the page</a>.</li>
+          <li>If all else fails, <a href="http://github.com/getsentry/sentry/issues">create an issue</a> with more details.</li>
+        </ul>
       </div>
     );
   },
