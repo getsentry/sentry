efa18745d2b002178de74c2577c3f87dc1ed2b3b
Matt Robenolt <matt@ydekproductions.com>
2016-01-04 20:09:14 -0800

    Fix tests to not use compiled assets

--- tests/sentry/web/frontend/generic/test_static_media.py
+++ tests/sentry/web/frontend/generic/test_static_media.py
@@ -5,7 +5,7 @@ from sentry.testutils import TestCase
 
 class StaticMediaTest(TestCase):
     def test_basic(self):
-        url = '/_static/sentry/dist/app.js'
+        url = '/_static/sentry/app/index.js'
         response = self.client.get(url)
         assert response.status_code == 200, response
         assert 'Cache-Control' not in response
@@ -13,22 +13,22 @@ class StaticMediaTest(TestCase):
         assert response['Access-Control-Allow-Origin'] == '*'
 
     def test_versioned(self):
-        url = '/_static/1234567890/sentry/dist/app.js'
+        url = '/_static/1234567890/sentry/app/index.js'
         response = self.client.get(url)
         assert response.status_code == 200, response
         assert 'Cache-Control' in response
         assert 'Vary' not in response
         assert response['Access-Control-Allow-Origin'] == '*'
 
-        url = '/_static/a43db3b08ddd4918972f80739f15344b/sentry/dist/app.js'
+        url = '/_static/a43db3b08ddd4918972f80739f15344b/sentry/app/index.js'
         response = self.client.get(url)
         assert response.status_code == 200, response
         assert 'Cache-Control' in response
         assert 'Vary' not in response
         assert response['Access-Control-Allow-Origin'] == '*'
 
-    def test_css(self):
-        url = '/_static/sentry/dist/sentry.css'
+    def test_no_cors(self):
+        url = '/_static/sentry/images/favicon.ico'
         response = self.client.get(url)
         assert response.status_code == 200, response
         assert 'Cache-Control' not in response
