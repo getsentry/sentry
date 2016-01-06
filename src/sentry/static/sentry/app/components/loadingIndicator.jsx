8e55b3abee4195b319c77c77dbed2d6e4603203d
Ben Vinegar <ben@benv.ca>
2016-01-06 14:26:04 -0800

    Use "toast" success/err msgs for stream/issue actions (fixes #2470)

--- src/sentry/static/sentry/app/components/loadingIndicator.jsx
+++ src/sentry/static/sentry/app/components/loadingIndicator.jsx
@@ -1,34 +1,21 @@
 import classNames from 'classnames';
 import React from 'react';
 
-const LoadingIndicator = React.createClass({
-  propTypes: {
-    global: React.PropTypes.bool,
-    mini:  React.PropTypes.bool,
-    triangle: React.PropTypes.bool
-  },
+function LoadingIndicator(props) {
+  let {mini, triangle} = props;
+  let classes = {
+    loading: true,
+    mini,
+    triangle
+  };
 
-  shouldComponentUpdate() {
-    return false;
-  },
-
-  render() {
-    let className = classNames({
-      'loading': true,
-      'mini': this.props.mini,
-      'global': this.props.global,
-      'triangle': this.props.triangle,
-    });
-
-    return (
-      <div className={classNames(this.props.className, className)}>
-        <div className="loading-mask"></div>
-        <div className="loading-indicator"></div>
-        <div className="loading-message">{this.props.children}</div>
-      </div>
-    );
-  }
-});
+  return (
+    <div className={classNames(props.className, classes)}>
+      <div className="loading-indicator"></div>
+      <div className="loading-message">{props.children}</div>
+    </div>
+  );
+}
 
 export default LoadingIndicator;
 
