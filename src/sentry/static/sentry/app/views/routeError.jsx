15f31b4da15d609718c74e42d538586df7c87806
Ben Vinegar <ben@benv.ca>
2016-01-13 14:30:55 -0800

    Use Raven.showReportDialog

--- src/sentry/static/sentry/app/views/routeError.jsx
+++ src/sentry/static/sentry/app/views/routeError.jsx
@@ -1,42 +1,17 @@
-import jQuery from 'jquery';
+import $ from 'jquery';
 import Raven from 'raven-js';
 import React from 'react';
-
-import ConfigStore from '../stores/configStore';
-
-
-function loadScript(url) {
-  let script = document.createElement('script');
-  script.type = 'text/javascript';
-  script.async = true;
-  script.src = url;
-  (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(script);
-}
 
 const RouteError = React.createClass({
   componentWillMount() {
     // TODO(dcramer): show something in addition to embed (that contains it?)
     // TODO(dcramer): capture better context
     Raven.captureException(this.props.error);
-    this.setState({
-      errorId: Raven.lastEventId(),
-    }, () => {
-      loadScript(this.getCrashReportUrl());
-    });
+    Raven.showReportDialog();
   },
 
   componentWillUnmount() {
-    // TODO(dcramer): kill crash dialog
-  },
-
-  getCrashReportUrl() {
-    // TODO(dcramer): needs urlPrefix
-    let query = jQuery.param({
-      eventId: this.state.errorId,
-      // TODO(dcramer): dsn should come from raven-js
-      dsn: ConfigStore.get('dsn'),
-    });
-    return `/api/embed/error-page/?${query}`;
+    $('.sentry-error-embed-wrapper').remove();
   },
 
   render() {
