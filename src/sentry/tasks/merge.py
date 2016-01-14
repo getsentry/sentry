d0e433179391aa7ae5ec3b086f26414397eba432
David Cramer <dcramer@gmail.com>
2016-01-14 13:45:10 -0800

    Correct update clause for merges

--- src/sentry/tasks/merge.py
+++ src/sentry/tasks/merge.py
@@ -149,16 +149,22 @@ def merge_objects(models, group, new_group, limit=1000,
         if logger is not None:
             logger.info('Merging %r objects where %r into %r', model, group,
                         new_group)
-        if 'group' in model._meta.get_all_field_names():
+        has_group = 'group' in model._meta.get_all_field_names()
+        if has_group:
             queryset = model.objects.filter(group=group)
         else:
             queryset = model.objects.filter(group_id=group.id)
         for obj in queryset[:limit]:
             try:
                 with transaction.atomic(using=router.db_for_write(model)):
-                    model.objects.filter(
-                        id=obj.id
-                    ).update(group_id=new_group.id)
+                    if has_group:
+                        model.objects.filter(
+                            id=obj.id
+                        ).update(group=new_group)
+                    else:
+                        model.objects.filter(
+                            id=obj.id
+                        ).update(group_id=new_group.id)
             except IntegrityError:
                 delete = True
             else:
