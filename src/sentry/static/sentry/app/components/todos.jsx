8d69896c8eb10123e2b8b4ef97b5fb583f159dd2
ckj <chriskjennings@gmail.com>
2016-01-22 16:23:28 -0800

    Stub out TodoItem

--- src/sentry/static/sentry/app/components/todos.jsx
+++ src/sentry/static/sentry/app/components/todos.jsx
@@ -21,49 +21,55 @@ const Todos = React.createClass({
         <div className="onboarding-wrapper">
           <h3>Remaining Todos</h3>
           <ul className="list-unstyled">
-            <li className="checked">
-              <div className="ob-checkbox">
-                <span className="icon-checkmark"/>
-              </div>
-              <h4>Send your first event</h4>
-              <p>
-                View our <a href="#">installation instructions</a>
-              </p>
-            </li>
-            <li>
-              <div className="ob-checkbox"></div>
-              <h4>Invite team members</h4>
-              <p>
-                Learn about <a href="#">how access works</a> on Sentry
-              </p>
-            </li>
-            <li>
-              <div className="ob-checkbox"></div>
-              <h4>Teach Sentry about your project</h4>
-              <p>
-                Track users, releases, and other rich context &middot; <a href="#">Learn More</a>
-              </p>
-            </li>
-            <li>
-              <div className="ob-checkbox"></div>
-              <h4>Add an issue tracker</h4>
-              <p>
-                Link Sentry Issues in Jira, GitHub, Trello, and others &middot; <a href="#">Learn More</a>
-              </p>
-              <a href="#" className="skip-btn btn btn-default">Skip</a>
-            </li>
-            <li>
-              <div className="ob-checkbox"></div>
-              <h4>Setup notification services</h4>
-              <p>
-                Be notified of Issues via Slack, HipChat, and More &middot; <a href="#">Learn More</a>
-              </p>
-              <a href="#" className="skip-btn btn btn-default">Skip</a>
-            </li>
+            <TodoItem completed={true} />
+            <TodoItem skippable={true} />
+            <TodoItem />
           </ul>
         </div>
     );
   }
 });
 
-export default Todos
+const TodoItem = React.createClass({
+  propTypes: {
+    completed: React.PropTypes.bool,
+    skippable: React.PropTypes.bool
+  },
+
+  getDefaultProps: function() {
+    return {
+      completed: false,
+      skippable: false
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      showTodos: false
+    };
+  },
+
+  render: function() {
+
+    let classNames = '';
+
+    if (this.props.completed) {
+      classNames += ' checked';
+    }
+
+    return (
+      <li className={classNames}>
+        <div className="ob-checkbox">
+          { this.props.completed ? <span className="icon-checkmark" /> : null }
+        </div>
+        <h4>Setup notification services</h4>
+        <p>
+          Be notified of Issues via Slack, HipChat, and More &middot; <a href="#">Learn More</a>
+        </p>
+        { this.props.skippable ? <a className="skip-btn btn btn-default">Skip</a> : null }
+      </li>
+    );
+  }
+});
+
+export default Todos;
