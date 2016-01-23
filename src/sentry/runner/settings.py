68e60dd63a3ed4faf56f813c57f13baa960b490a
Matt Robenolt <matt@ydekproductions.com>
2016-01-22 17:17:44 -0800

    Update docs

--- src/sentry/runner/settings.py
+++ src/sentry/runner/settings.py
@@ -165,7 +165,7 @@ SENTRY_FILESTORE_OPTIONS = {
 # SESSION_COOKIE_SECURE = True
 # CSRF_COOKIE_SECURE = True
 
-# If you're not hosting at the root of your web server, and not using uWSGI,
+# If you're not hosting at the root of your web server,
 # you need to uncomment and set it to the path where Sentry is hosted.
 # FORCE_SCRIPT_NAME = '/sentry'
 
