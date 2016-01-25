13f0624dbcf919ec870a8f1abc4c8766e9be71f1
David Cramer <dcramer@gmail.com>
2016-01-25 11:55:05 -0800

    Handle non-JSON error responses

--- src/sentry/static/sentry/app/utils/logging.jsx
+++ src/sentry/static/sentry/app/utils/logging.jsx
@@ -7,7 +7,10 @@ export function logException(ex, context) {
 }
 
 export function logAjaxError(error, context) {
-  let message = `HTTP ${error.status}: ${error.responseJSON.detail || error.responseJSON.toString()}`;
+  let errorString = (error.responseJSON ?
+    error.responseJSON.detail || error.responseJSON.toString() :
+    error.responseText.substr(0, 255));
+  let message = `HTTP ${error.status}: ${errorString}`;
   Raven.captureMessage(message, {
     extra: context
   });
