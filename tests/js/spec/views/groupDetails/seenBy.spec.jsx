98531712f8ad8a28eb3f2f0d97e4b75981952ac6
Ben Vinegar <ben@benv.ca>
2016-02-02 18:22:33 -0800

    Use more enzyme testing utils (shallow rendering, context stubbing)

--- tests/js/spec/views/groupDetails/seenBy.spec.jsx
+++ tests/js/spec/views/groupDetails/seenBy.spec.jsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import GroupSeenBy from 'app/views/groupDetails/seenBy';
 import ConfigStore from 'app/stores/configStore';
-import Gravatar from 'app/components/gravatar';
-
-
-import stubContext from '../../../helpers/stubContext';
-import stubReactComponent from '../../../helpers/stubReactComponent';
 
 describe('OrganizationTeams', function() {
 
@@ -15,7 +10,6 @@ describe('OrganizationTeams', function() {
     this.sandbox = sinon.sandbox.create();
 
     this.sandbox.stub(ConfigStore, 'get').returns([]);
-    stubReactComponent(this.sandbox, [Gravatar]);
   });
 
   afterEach(function() {
@@ -24,30 +18,31 @@ describe('OrganizationTeams', function() {
 
   describe('render()', function() {
     it('should return null if seenBy is falsy', function () {
-      let ContextStubbedGroupSeenBy = stubContext(GroupSeenBy, {
-        group: {id: '1337'},
-        project: {},
-        team: {}
+      let wrapper = shallow(<GroupSeenBy/>, {
+        context: {
+          group: {id: '1337'},
+          project: {id: '2448'},
+          team: {id: '3559'}
+        }
       });
-
-      let wrapper = mount(<ContextStubbedGroupSeenBy/>);
       expect(wrapper.children()).to.have.length(0);
     });
 
     it('should return a list of each user that saw', function () {
-      let ContextStubbedGroupSeenBy = stubContext(GroupSeenBy, {
-        group: {
-          id: '1337',
-          seenBy: [
-            {id: 1, email: 'jane@example.com'},
-            {id: 2, email: 'john@example.com'}
-          ]
-        },
-        project: {},
-        team: {}
+      let wrapper = shallow(<GroupSeenBy/>, {
+        context: {
+          group: {
+            id: '1337',
+            seenBy: [
+              {id: 1, email: 'jane@example.com'},
+              {id: 2, email: 'john@example.com'}
+            ]
+          },
+          project: {id: '2448'},
+          team: {id: '3559'}
+        }
       });
 
-      let wrapper = mount(<ContextStubbedGroupSeenBy/>);
       expect(wrapper.find('li')).to.have.length(3); // +1 for "icon-eye"
     });
   });
