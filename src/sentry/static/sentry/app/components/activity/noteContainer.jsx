d71887c3cf97690976e17ae3d8d20bec09817d02
Ben Vinegar <ben@benv.ca>
2016-02-02 16:54:30 -0800

    Fix comment HTTP updates canceled via race condition (fixes #2603)

--- src/sentry/static/sentry/app/components/activity/noteContainer.jsx
+++ src/sentry/static/sentry/app/components/activity/noteContainer.jsx
@@ -1,17 +1,9 @@
 import React from 'react';
-import ApiMixin from '../../mixins/apiMixin';
-import IndicatorStore from '../../stores/indicatorStore';
-import GroupStore from '../../stores/groupStore';
 
 import Note from './note';
 import NoteInput from './noteInput';
-import {t} from '../../locale';
 
 const NoteContainer = React.createClass({
-  mixins: [
-    ApiMixin
-  ],
-
   getInitialState() {
     return {
       editing: false
@@ -27,27 +19,7 @@ const NoteContainer = React.createClass({
   },
 
   onDelete() {
-    let {group, item} = this.props;
-    // Optimistically remove from UI
-    let index = GroupStore.removeActivity(group.id, item.id);
-    if (index === -1) {
-        // I dunno, the id wasn't found in the GroupStore
-        return;
-    }
-
-    let loadingIndicator = IndicatorStore.add(t('Removing comment..'));
-
-    this.api.request('/issues/' + group.id + '/comments/' + item.id + '/' , {
-      method: 'DELETE',
-      error: (error) => {
-        // TODO(mattrobenolt): Show an actual error that this failed,
-        // but just bring it back in place for now
-        GroupStore.addActivity(group.id, item, index);
-      },
-      complete: () => {
-        IndicatorStore.remove(loadingIndicator);
-      }
-    });
+    this.props.onDelete(this.props.item);
   },
 
   render() {
