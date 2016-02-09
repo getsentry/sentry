7e7af4e163846a9bde713c01daf65c900f153cb6
Armin Ronacher <armin.ronacher@active-4.com>
2016-02-08 21:27:55 -0800

    Properly enter and exit the driver

--- src/sentry/lang/native/symbolizer.py
+++ src/sentry/lang/native/symbolizer.py
@@ -4,10 +4,8 @@ from symsynd.report import ReportSymbolizer
 from sentry.lang.native.dsymcache import dsymcache
 
 
-driver = Driver()
-
-
 def make_symbolizer(project, binary_images):
+    driver = Driver()
     dsym_path, loaded = dsymcache.fetch_dsyms(
         [x['uuid'] for x in binary_images])
     return ReportSymbolizer(driver, dsym_path, binary_images)
