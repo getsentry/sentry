69d33e77c49d9b8d0360426c1df977cec0bdfa83
Ted Kaemming <ted@kaemming.com>
2016-02-09 16:27:16 -0800

    Add frequency table (Count-Min Sketch), without usage.

--- src/sentry/tsdb/dummy.py
+++ src/sentry/tsdb/dummy.py
@@ -28,3 +28,29 @@ class DummyTSDB(BaseTSDB):
 
     def get_distinct_counts_totals(self, model, keys, start, end=None, rollup=None):
         return {k: 0 for k in keys}
+
+    def record_frequency_multi(self, requests, timestamp=None):
+        pass
+
+    def get_most_frequent(self, model, keys, start, end=None, rollup=None, limit=None):
+        return {key: [] for key in keys}
+
+    def get_frequency_series(self, model, items, start, end=None, rollup=None):
+        rollup, series = self.get_optimal_rollup_series(start, end, rollup)
+
+        results = {}
+        for key, members in items.items():
+            result = results[key] = []
+            for timestamp in series:
+                result.append((
+                    timestamp,
+                    {k: 0.0 for k in members},
+                ))
+
+        return results
+
+    def get_frequency_totals(self, model, items, start, end=None, rollup=None):
+        results = {}
+        for key, members in items.items():
+            results[key] = {member: 0.0 for member in members}
+        return results
