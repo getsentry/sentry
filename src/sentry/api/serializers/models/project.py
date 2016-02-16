1716f5844d7c7d07733d16a9be79f75a80ec6512
David Cramer <dcramer@gmail.com>
2016-02-16 11:08:40 -0800

    Add project bookmark status to API responses

--- src/sentry/api/serializers/models/project.py
+++ src/sentry/api/serializers/models/project.py
@@ -1,11 +1,27 @@
 from __future__ import absolute_import
 
 from sentry.api.serializers import Serializer, register
-from sentry.models import Project
+from sentry.models import Project, ProjectBookmark
 
 
 @register(Project)
 class ProjectSerializer(Serializer):
+    def get_attrs(self, item_list, user):
+        if user.is_authenticated() and item_list:
+            bookmarks = set(ProjectBookmark.objects.filter(
+                user=user,
+                project__in=item_list,
+            ).values_list('project_id', flat=True))
+        else:
+            bookmarks = set()
+
+        result = {}
+        for item in item_list:
+            result[item] = {
+                'is_bookmarked': item.id in bookmarks,
+            }
+        return result
+
     def serialize(self, obj, attrs, user):
         from sentry import features
 
@@ -22,6 +38,7 @@ class ProjectSerializer(Serializer):
             'slug': obj.slug,
             'name': obj.name,
             'isPublic': obj.public,
+            'isBookmarked': attrs['is_bookmarked'],
             'dateCreated': obj.date_added,
             'firstEvent': obj.first_event,
             'features': feature_list,
