47fb7a82676ac064e7057b68618331e4d81de309
David Cramer <dcramer@gmail.com>
2016-02-22 19:44:29 -0800

    Various improvements to errorHandler
    - Dont fail mounting error handler if Raven fails
    - Capture componentWillMount

--- src/sentry/static/sentry/app/utils/errorHandler.jsx
+++ src/sentry/static/sentry/app/utils/errorHandler.jsx
@@ -3,12 +3,19 @@ import RouteError from '../views/routeError';
 
 export default function errorHandler(Component) {
   const originalRender = Component.prototype.render;
-
-  Component.prototype.render = function tryRender() {
+  Component.prototype.render = function() {
     try {
       return originalRender.apply(this, arguments);
     } catch (err) {
-      console.error(err);
+      return <RouteError error={err} component={this} />;
+    }
+  };
+
+  const originalComponentWillMount = Component.prototype.componentWillMount;
+  Component.prototype.componentWillMount = function() {
+    try {
+      return originalComponentWillMount.apply(this, arguments);
+    } catch (err) {
       return <RouteError error={err} component={this} />;
     }
   };
