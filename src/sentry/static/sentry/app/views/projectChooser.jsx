0e1d503190b97563327986b84b2655550ac0c876
Eric Feng <erichfeng@gmail.com>
2016-02-22 19:56:44 -0800

    fixing linting errors

--- src/sentry/static/sentry/app/views/projectChooser.jsx
+++ src/sentry/static/sentry/app/views/projectChooser.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import {History} from 'react-router';
-import {Link} from 'react-router';
 
-import AllTeamsList from './organizationTeams/allTeamsList';
-import ExpandedTeamList from './organizationTeams/expandedTeamList';
 import OrganizationState from '../mixins/organizationState';
 import TodoList from '../components/todos';
 
@@ -26,42 +23,42 @@ const ProjectChooser = React.createClass({
 
     // Expect onboarding=1 and task=<task id> parameters and task.feature_location == 'project'
     // TODO throw up report dialog if not true
-    let task = TodoList.TASKS.filter((task) => task.task == this.props.location.query.task)[0];
+    let task = TodoList.TASKS.filter((t) => t.task == this.props.location.query.task)[0];
     if (task.feature_location != 'project') {
-      throw new Error("User arrived on project chooser without a valid task id");
+      throw new Error('User arrived on project chooser without a valid task id.');
     }
-    let teamProjectList = []
+    let teamProjectList = [];
 
     org.teams.map((team) => {
-      let projectList = []
+      let projectList = [];
       team.projects.map((project) => {
-        let next_url = "/" + org.slug+ "/" + project.slug + "/" + task.location;
+        let next_url = '/' + org.slug+ '/' + project.slug + '/' + task.location;
         projectList.push(
           <tr key={project.id}><td><h5>
             <a href={next_url}>{project.name}</a>
           </h5></td></tr>);
-      })
+      });
 
       teamProjectList.push(
         <div key={team.id}>
-          <div className="box-header" key={team.id}>
+          <div className='box-header' key={team.id}>
             <h3>{team.name}</h3>
           </div>
-          <div className="box-content">
-            <table className="table project-list">
+          <div className='box-content'>
+            <table className='table project-list'>
               <tbody>
                 {projectList}
               </tbody>
             </table>
           </div>
         </div>);
-    })
+    });
 
     return (
-      <div className="container">
+      <div className='container'>
         <h3>Choose a project</h3>
-        <div className="team-list">
-          <div className="box">
+        <div className='team-list'>
+          <div className='box'>
             {teamProjectList}
           </div>
         </div>
