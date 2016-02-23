ab6b7b78202353dd605f7327cc5f5714bdc1aa90
David Cramer <dcramer@gmail.com>
2016-02-22 19:50:21 -0800

    That was a lie

--- src/sentry/static/sentry/app/utils/errorHandler.jsx
+++ src/sentry/static/sentry/app/utils/errorHandler.jsx
@@ -10,15 +10,5 @@ export default function errorHandler(Component) {
       return <RouteError error={err} component={this} />;
     }
   };
-
-  const originalComponentWillMount = Component.prototype.componentWillMount;
-  Component.prototype.componentWillMount = function() {
-    try {
-      return originalComponentWillMount.apply(this, arguments);
-    } catch (err) {
-      return <RouteError error={err} component={this} />;
-    }
-  };
-
   return Component;
 }
