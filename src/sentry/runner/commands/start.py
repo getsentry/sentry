getsentry/sentry

commit 12697cd16d0b80f78bc4ca89c679322f7894f4c7

Remove default value from --workers in click
Fixes GH-2624

src/sentry/runner/commands/start.py
@@ -18,7 +18,7 @@ SERVICES = {
 
 @click.command()
 @click.option('--bind', '-b', default=None, help='Bind address.', metavar='ADDRESS')
-@click.option('--workers', '-w', default=3, show_default=True, help='The number of worker processes for handling requests.')
+@click.option('--workers', '-w', help='The number of worker processes for handling requests.')
 @click.option('--upgrade', default=False, is_flag=True, help='Upgrade before starting.')
 @click.option('--noinput', default=False, is_flag=True, help='Do not prompt the user for input of any kind.')
 @click.argument('service', default='http', type=click.Choice(sorted(SERVICES.keys())))

commit 6d8349b16c708fa198a444c41ea23407131ad174

`--workers` should be explicitly an integer
Fixed regression from 12697cd16d0b80f78bc4ca89c679322f7894f4c7

src/sentry/runner/commands/start.py
@@ -18,7 +18,7 @@ SERVICES = {
 
 @click.command()
 @click.option('--bind', '-b', default=None, help='Bind address.', metavar='ADDRESS')
-@click.option('--workers', '-w', help='The number of worker processes for handling requests.')
+@click.option('--workers', '-w', default=0, help='The number of worker processes for handling requests.')
 @click.option('--upgrade', default=False, is_flag=True, help='Upgrade before starting.')
 @click.option('--noinput', default=False, is_flag=True, help='Do not prompt the user for input of any kind.')
 @click.argument('service', default='http', type=click.Choice(sorted(SERVICES.keys())))