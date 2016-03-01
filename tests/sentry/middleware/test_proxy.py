55abd32fc1bd88a0a5c5810ada0a47dae2c76dec
Matt Robenolt <matt@ydekproductions.com>
2016-03-01 11:01:29 -0800

    Adding test file for b4033e04229409786670d4139423f0c963d3e032
    I apparently forgot to commit this long ago. It's been sitting on my
    computer.

---
+++ tests/sentry/middleware/test_proxy.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+from exam import fixture
+from django.http import HttpResponse, StreamingHttpResponse
+
+from sentry.testutils import TestCase
+from sentry.middleware.proxy import ContentLengthHeaderMiddleware
+
+
+class ContentLengthHeaderMiddlewareTest(TestCase):
+    middleware = fixture(ContentLengthHeaderMiddleware)
+
+    def test_simple(self):
+        response = self.middleware.process_response(None, HttpResponse('lol'))
+        assert response['Content-Length'] == '3'
+        assert 'Transfer-Encoding' not in response
+
+    def test_streaming(self):
+        response = self.middleware.process_response(None, StreamingHttpResponse())
+        assert response['Transfer-Encoding'] == 'chunked'
+        assert 'Content-Length' not in response
