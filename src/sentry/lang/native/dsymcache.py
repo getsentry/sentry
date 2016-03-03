0caab339695ad91d81735fc110d8335b788747fd
Armin Ronacher <armin.ronacher@active-4.com>
2016-03-03 20:27:48 +0100

    Bump modified times every day for dsyms

--- src/sentry/lang/native/dsymcache.py
+++ src/sentry/lang/native/dsymcache.py
@@ -1,8 +1,13 @@
 import os
+import time
+import errno
 import shutil
 
 from django.conf import settings
 from sentry.models import find_dsym_file
+
+
+ONE_DAY = 60 * 60 * 24
 
 
 class DSymCache(object):
@@ -13,6 +18,9 @@ class DSymCache(object):
     def get_project_path(self, project):
         return os.path.join(settings.DSYM_CACHE_PATH, str(project.id))
 
+    def get_global_path(self):
+        return os.path.join(settings.DSYM_CACHE_PATH, 'global')
+
     def fetch_dsyms(self, project, uuids):
         base = self.get_project_path(project)
         loaded = []
@@ -22,16 +30,34 @@ class DSymCache(object):
                 loaded.append(dsym)
         return base, loaded
 
+    def try_bump_timestamp(self, path, old_stat):
+        now = int(time.time())
+        if old_stat.st_ctime < now - ONE_DAY:
+            os.utime(path, (now, now))
+        return path
+
     def fetch_dsym(self, project, image_uuid):
         image_uuid = image_uuid.lower()
-        base = self.get_project_path(project)
-        dsym = os.path.join(base, image_uuid)
-        if os.path.isfile(dsym):
-            return dsym
+        for path in self.get_project_path(project), self.get_global_path():
+            base = self.get_project_path(project)
+            dsym = os.path.join(base, image_uuid)
+            try:
+                stat = os.stat(dsym)
+            except OSError as e:
+                if e.errno != errno.ENOENT:
+                    raise
+            else:
+                return self.try_bump_timestamp(dsym, stat)
 
         dsf = find_dsym_file(project, image_uuid)
         if dsf is None:
             return None
+
+        if dsf.is_global:
+            base = self.get_global_path()
+        else:
+            base = self.get_project_path(project)
+        dsym = os.path.join(base, image_uuid)
 
         try:
             os.makedirs(base)
