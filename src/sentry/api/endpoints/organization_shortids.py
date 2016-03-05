df2b4ff8cee2854ee1d6dc423cbeec45953285ad
Armin Ronacher <armin.ronacher@active-4.com>
2016-03-05 20:27:39 +0100

    Updated project settings and api for callsign changes

--- src/sentry/api/endpoints/organization_shortids.py
+++ src/sentry/api/endpoints/organization_shortids.py
@@ -2,7 +2,11 @@ from __future__ import absolute_import
 
 from rest_framework.response import Response
 
+from django.db import transaction
+
 from sentry.api.bases.organization import OrganizationEndpoint
+from sentry.models import Project
+from sentry.utils.strings import validate_callsign
 
 
 class ShortIdsUpdateEndpoint(OrganizationEndpoint):
@@ -16,20 +20,54 @@ class ShortIdsUpdateEndpoint(OrganizationEndpoint):
 
         :pparam string organization_slug: the slug of the organization the
                                           short ID should be looked up in.
-        :param projects: a dictionary of project IDs to their intended
-                         short IDs.
+        :param callsigns: a dictionary of project IDs to their intended
+                          callsigns.
         :auth: required
         """
-        projects = dict((str(p.id), p) for p in organization.project_set.all())
+        callsigns = request.DATA.get('callsigns', {})
+        for project_id, callsign in callsigns.iteritems():
+            callsign = validate_callsign(callsign)
+            if callsign is None:
+                return Response({'detail': 'invalid callsign "%s"' % callsign},
+                                status=400)
+            callsigns[project_id] = callsign
+
+        if len(callsigns) != len(callsigns.values()):
+            return Response({'detail': 'Duplicate callsigns'}, status=400)
+
+        project_q = organization.project_set.filter(
+            pk__in=[int(x) for x in callsigns if x.isdigit()]
+        )
+
         rv = {}
 
-        for project_id, callsign in request.DATA.get('projects', {}).iteritems():
-            project = projects.get(project_id)
-            if project is None:
-                continue
-            project.callsign = callsign
-            project.update_option('sentry:reviewed-callsign', True)
-            rv[project.id] = callsign
+        with transaction.atomic():
+            projects = {}
+
+            # Clear out all call-signs first so that we can move them
+            # around through the uniqueness
+            for project in project_q:
+                projects[str(project.id)] = project
+                project.callsign = None
+                project.save()
+
+            # Set new ones
+            for project_id, callsign in callsigns.iteritems():
+                project = projects.get(project_id)
+                if project is None:
+                    continue
+                other = Project.objects.filter(
+                    callsign=callsign,
+                    organization=organization
+                ).exclude(id=project.id).first()
+                if other is not None:
+                    if len(callsigns) != len(callsigns.values()):
+                        return Response({'detail': 'Duplicate callsign %s'
+                                         % callsign}, status=400)
+                project.callsign = callsign
+                project.update_option('sentry:reviewed-callsign', True)
+                project.save()
+                rv[project_id] = callsign
 
         return Response({
             'updated_short_ids': rv
