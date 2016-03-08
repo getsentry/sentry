b89de6fb489011e28f31f3a46bc65f0f18bc31f5
Armin Ronacher <armin.ronacher@active-4.com>
2016-03-08 12:03:41 +0100

    Only load symbols for symbolication referenced by the crashed thread

--- src/sentry/lang/native/symbolizer.py
+++ src/sentry/lang/native/symbolizer.py
@@ -8,11 +8,29 @@ except ImportError:
 from sentry.lang.native.dsymcache import dsymcache
 
 
-def make_symbolizer(project, binary_images):
+def make_symbolizer(project, binary_images, threads=None):
+    """Creates a symbolizer for the given project and binary images.  If a
+    list of threads is referenced (from an apple crash report) then only
+    images needed by those frames are loaded.
+    """
     if not have_symsynd:
         raise RuntimeError('symsynd is unavailable.  Install sentry with '
                            'the dsym feature flag.')
     driver = Driver()
-    dsym_paths, loaded = dsymcache.fetch_dsyms(project,
-        [x['uuid'] for x in binary_images])
+
+    if threads is None:
+        to_load = [x['uuid'] for x in binary_images]
+    else:
+        image_map = {}
+        for image in binary_images:
+            image_map[image['image_addr']] = image['uuid']
+        to_load = set()
+        for thread in threads:
+            for frame in thread['backtrace']['contents']:
+                img_uuid = image_map.get(frame['object_addr'])
+                if img_uuid is not None:
+                    to_load.add(img_uuid)
+        to_load = list(to_load)
+
+    dsym_paths, loaded = dsymcache.fetch_dsyms(project, to_load)
     return ReportSymbolizer(driver, dsym_paths, binary_images)
