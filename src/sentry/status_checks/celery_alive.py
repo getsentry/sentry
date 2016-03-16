c873fa541f58e2c8be35d0854da7d5aa3491267a
Ted Kaemming <ted@kaemming.com>
2016-03-15 18:03:16 -0700

    Add link to queue graphs for `CeleryAliveCheck` result.

--- src/sentry/status_checks/celery_alive.py
+++ src/sentry/status_checks/celery_alive.py
@@ -2,9 +2,12 @@ from __future__ import absolute_import
 
 from time import time
 
-from sentry import options
+from django.core.urlresolvers import reverse
 
-from .base import StatusCheck, Problem
+from sentry import options
+from sentry.utils.http import absolute_uri
+
+from .base import Problem, StatusCheck
 
 
 class CeleryAliveCheck(StatusCheck):
@@ -13,5 +16,8 @@ class CeleryAliveCheck(StatusCheck):
         if last_ping >= time() - 300:
             return []
         return [
-            Problem("Background workers haven't checked in recently. This can mean an issue with your configuration or a serious backlog in tasks."),
+            Problem(
+                "Background workers haven't checked in recently. This can mean an issue with your configuration or a serious backlog in tasks.",
+                url=absolute_uri(reverse('sentry-admin-queue')),
+            ),
         ]
