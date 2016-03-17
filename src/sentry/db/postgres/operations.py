6cfda99bc1f8bcc8ba56d85ff8f64c611912581e
David Cramer <dcramer@gmail.com>
2016-03-16 18:47:26 -0700

    Remove HOST() usage for IP addresses (fixes GH-2854)

---
+++ src/sentry/db/postgres/operations.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+from django.db.backends.postgresql_psycopg2.base import DatabaseOperations
+
+
+class DatabaseOperations(DatabaseOperations):
+    # Remove HOST() lookups for GenericIPAddressField
+    def field_cast_sql(self, db_type, internal_type):
+        return '%s'
