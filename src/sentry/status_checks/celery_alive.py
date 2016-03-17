b66c6ddb0f300fa4f2f2e5cb69a2b603e19b31a1
Matt Robenolt <matt@ydekproductions.com>
2016-03-17 16:15:28 -0700

    Detect if the worker issue is actually because of backlog or not

--- src/sentry/status_checks/celery_alive.py
+++ src/sentry/status_checks/celery_alive.py
@@ -15,9 +15,22 @@ class CeleryAliveCheck(StatusCheck):
         last_ping = options.get('sentry:last_worker_ping') or 0
         if last_ping >= time() - 300:
             return []
+
+        backlogged, size = None, 0
+        from sentry.monitoring.queues import backend
+        if backend is not None:
+            size = backend.get_size('default')
+            backlogged = size > 0
+
+        message = "Background workers haven't checked in recently. "
+        if backlogged:
+            message += "It seems that you have a backlog of %d tasks. Either your workers aren't running or you need more capacity." % size
+        else:
+            message += "This is likely an issue with your configuration or the workers aren't running."
+
         return [
             Problem(
-                "Background workers haven't checked in recently. This can mean an issue with your configuration or a serious backlog in tasks.",
+                message,
                 url=absolute_uri(reverse('sentry-admin-queue')),
             ),
         ]
