4d981cbfadaf341871f9c122c7dc297763cb8ee1
Ted Kaemming <ted@kaemming.com>
2016-03-22 15:52:59 -0700

    Add `GroupRedirect` model.

---
+++ src/sentry/models/groupredirect.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+from sentry.db.models import BoundedBigIntegerField, Model
+
+
+class GroupRedirect(Model):
+    """
+    Maintains a reference from a group that has been merged (and subsequently
+    deleted) to the group that superceded it.
+    """
+    __core__ = False
+
+    group_id = BoundedBigIntegerField(db_index=True)
+    previous_group_id = BoundedBigIntegerField(unique=True)
+
+    class Meta:
+        db_table = 'sentry_groupredirect'
+        app_label = 'sentry'
