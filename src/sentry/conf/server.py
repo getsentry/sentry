b4ac1460084b2063ad578f6437e9f50bcf2c0381
Ben Vinegar <ben@benv.ca>
2016-03-25 14:10:02 -0700

    Use devtool: cheap-module-eval-source-map in development ...
    * It's becoming the default in webpack 2.0 (currently in beta)
    * It fixes Chrome's source map caching issue (since source maps are evaled as part of app.js)
    * It rebuilds faster according to webpack docs: https://webpack.github.io/docs/configuration.html#devtool

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -929,7 +929,7 @@ SENTRY_API_RESPONSE_DELAY = 0
 # XXX(dcramer): this doesn't work outside of a source distribution as the
 # webpack.config.js is not part of Sentry's datafiles
 SENTRY_WATCHERS = (
-    ('webpack', [os.path.join(NODE_MODULES_ROOT, '.bin', 'webpack'), '-d', '--watch',
+    ('webpack', [os.path.join(NODE_MODULES_ROOT, '.bin', 'webpack'), '--output-pathinfo', '--watch',
      "--config={}".format(os.path.normpath(os.path.join(PROJECT_ROOT, os.pardir, os.pardir, "webpack.config.js")))]),
 )
 
