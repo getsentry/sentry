0647098100aa0469a7b0494d249b28c78b41ec66
Jess MacQueen <jess@getsentry.com>
2016-04-04 17:40:06 -0700

    show 404 view when no platform/integration

--- src/sentry/static/sentry/app/views/routeNotFound.jsx
+++ src/sentry/static/sentry/app/views/routeNotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import DocumentTitle from 'react-document-title';
 import Footer from '../components/footer';
 import Header from '../components/header';
-import {t} from '../locale';
+import NotFound from '../components/errors/notFound';
 
 const RouteNotFound = React.createClass({
   getTitle() {
@@ -18,21 +18,7 @@ const RouteNotFound = React.createClass({
           <div className="container">
             <div className="content">
               <section className="body">
-                <div className="alert alert-block alert-error">
-                  <div style={{fontSize: 24, marginBottom: 10}}>
-                    <span className="icon-exclamation" style={{fontSize: 20, marginRight: 10}} />
-                    <span>{t('Page Not Found')}</span>
-                  </div>
-
-                  <p>The page you are looking for was not found.</p>
-                  <p>You may wish to try the following:</p>
-                  <ul>
-                      <li>If you entered the address manually, double check the path. Did you forget a trailing slash?</li>
-                      <li>If you followed a link here, try hitting back and reloading the page. It's possible the resource was moved out from under you.</li>
-                      <li>If all else fails, <a href="http://github.com/getsentry/sentry/issues">create an issue</a> with more details.</li>
-                  </ul>
-                  <p>Not sure what to do? <a href="/">Return to the dashboard</a></p>
-                </div>
+                <NotFound />
               </section>
             </div>
           </div>
