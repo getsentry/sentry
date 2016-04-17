3898b7c97466d0b55bea269ea19f27e7963ebf80
Jess MacQueen <jess@getsentry.com>
2016-04-17 09:29:26 -0700

    add tests for html version of letter avatar

--- tests/sentry/utils/test_letter_avatar.py
+++ tests/sentry/utils/test_letter_avatar.py
@@ -6,6 +6,7 @@ def test_letter_avatar():
     letter_avatar = get_letter_avatar('Jane Doe', 'janedoe@example.com')
     assert 'JD' in letter_avatar
     assert '#E56AA6' in letter_avatar
+    assert 'svg' in letter_avatar
 
     # Test email as display name and id as identifier
     letter_avatar = get_letter_avatar('johnsmith@example.com', 2)
@@ -14,5 +15,21 @@ def test_letter_avatar():
 
     # Test no display name and ip address as identifier
     letter_avatar = get_letter_avatar(None, '127.0.0.1')
+    assert '?' in letter_avatar
+    assert '#E35141' in letter_avatar
+
+    # Test name as display name and email as identifier for html
+    letter_avatar = get_letter_avatar('Jane Doe', 'janedoe@example.com', use_svg=False)
+    assert 'JD' in letter_avatar
+    assert '#E56AA6' in letter_avatar
+    assert 'span' in letter_avatar
+
+    # Test email as display name and id as identifier for html
+    letter_avatar = get_letter_avatar('johnsmith@example.com', 2, use_svg=False)
+    assert 'J' in letter_avatar
+    assert '#6FBA57' in letter_avatar
+
+    # Test no display name and ip address as identifier for html
+    letter_avatar = get_letter_avatar(None, '127.0.0.1', use_svg=False)
     assert '?' in letter_avatar
     assert '#E35141' in letter_avatar
