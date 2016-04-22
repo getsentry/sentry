23efe2bd6e6ae40aac6f2e576cc1c979139cee8d
David Cramer <dcramer@gmail.com>
2016-04-22 10:52:13 -0700

    Remove File.blob usage
    - Backfill any missing FileBlobIndex entries
    - Remove references to File.blob
    
    /cc @getsentry/team

--- src/sentry/lang/javascript/processor.py
+++ src/sentry/lang/javascript/processor.py
@@ -200,7 +200,7 @@ def fetch_release_file(filename, release):
             releasefile = ReleaseFile.objects.filter(
                 release=release,
                 ident=ident,
-            ).select_related('file', 'file__blob').get()
+            ).select_related('file').get()
         except ReleaseFile.DoesNotExist:
             logger.debug('Release artifact %r not found in database (release_id=%s)',
                          filename, release.id)
