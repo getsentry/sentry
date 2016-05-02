cbc93bffb3076fd683401e6aded13c806231f2aa
Armin Ronacher <armin.ronacher@active-4.com>
2016-05-03 00:31:49 +0200

    Fixed JS linter failure

--- src/sentry/static/sentry/app/components/events/interfaces/breadcrumbComponents/summaryLine.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/breadcrumbComponents/summaryLine.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Duration from '../../../duration';
-
 
 const SummaryLine = React.createClass({
   propTypes: {
@@ -9,7 +7,6 @@ const SummaryLine = React.createClass({
   },
 
   render() {
-    let {crumb} = this.props;
     // this is where we can later also show other interesting
     // information (maybe duration?)
     return (
