7824feaec5f28ed1c28ba5bfb8d4e5aa5a3c9ebb
Armin Ronacher <armin.ronacher@active-4.com>
2016-05-02 21:14:51 +0200

    Refactored UI code to support crumb types

--- src/sentry/static/sentry/app/components/events/interfaces/breadcrumbComponents/crumb.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/breadcrumbComponents/crumb.jsx
@@ -2,28 +2,17 @@ import React from 'react';
 import moment from 'moment';
 import _ from 'underscore';
 
-import Category from './category';
-import Duration from '../../../duration';
+import HttpRenderer from './httpRenderer';
+import DefaultRenderer from './defaultRenderer';
+
+const CUSTOM_RENDERERS = {
+  http: HttpRenderer,
+};
+
 
 const CrumbComponent = React.createClass({
   propTypes: {
     crumb: React.PropTypes.object.isRequired,
-  },
-
-  renderSummary() {
-    let {crumb} = this.props;
-
-    return (
-      <div className="summary">
-        {crumb.message && <pre><code>{crumb.message}</code></pre>}
-        {crumb.category && <Category value={crumb.category}/>}
-        {crumb.duration &&
-          <span className="crumb-timing">
-            [<Duration seconds={crumb.duration}/>]
-          </span>
-        }
-      </div>
-    );
   },
 
   getClassName() {
@@ -35,74 +24,24 @@ const CrumbComponent = React.createClass({
     return rv;
   },
 
-  getTitle() {
-    // TODO(mitsuhiko): refactor to support different types better
-    let crumb = this.props.crumb;
-    if (crumb.type === 'default') {
-      return null;
-    }
-    return crumb.type.split(/[_-\s]+/g).map((word) => {
-      return word.substr(0, 1).toUpperCase() + word.substr(1);
-    }).join(' ');
-  },
-
-  getLevel() {
-    // TODO(mitsuhiko): refactor to support different types better
-    let crumb = this.props.crumb;
-    if ((crumb.type !== 'default' && crumb.level === 'info') ||
-        crumb.type === 'error') {
-      return null;
-    }
-    return (
-      <span className="crumb-level">{crumb.level}</span>
-    );
-  },
-
-  renderData() {
+  renderType() {
     let {crumb} = this.props;
-
-    if (!crumb.data) {
-      return null;
-    }
-
-    let body = _.chain(crumb.data)
-      .map((val, key) => [val, key])
-      .map(([val, key]) => {
-        return (
-          <tr key={key}>
-            <td className="key">{key}</td>
-            <td className="value"><pre>{val + ''}</pre></td>
-          </tr>
-        );
-      })
-      .value();
-
+    let Renderer = CUSTOM_RENDERERS[crumb.type] || DefaultRenderer;
     return (
-      <tbody>{body}</tbody>
+      <Renderer crumb={crumb} />
     );
   },
 
   render() {
-    let crumb = this.props.crumb;
-
     return (
       <li className={this.getClassName()}>
         <span className="icon-container">
           <span className="icon"/>
         </span>
-        <span className="dt">{moment(crumb.timestamp).format('HH:mm:ss')}</span>
-        <table className="table key-value">
-          <thead>
-            <tr>
-              <td className="key">
-                {this.getTitle()}
-                {this.getLevel()}
-              </td>
-              <td className="value">{this.renderSummary()}</td>
-            </tr>
-          </thead>
-          {this.renderData()}
-        </table>
+        <span className="dt">
+          {moment(this.props.crumb.timestamp).format('HH:mm:ss')}
+        </span>
+        {this.renderType()}
       </li>
     );
   }
