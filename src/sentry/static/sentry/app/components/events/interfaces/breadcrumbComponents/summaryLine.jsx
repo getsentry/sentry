e3af8aeb9dcf0e08c0add0cb8dcb2ddbcc5c61ae
Armin Ronacher <armin.ronacher@active-4.com>
2016-05-03 15:15:08 +0200

    Add support for collapsable summaries

--- src/sentry/static/sentry/app/components/events/interfaces/breadcrumbComponents/summaryLine.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/breadcrumbComponents/summaryLine.jsx
@@ -1,16 +1,50 @@
 import React from 'react';
 
 
+function isOverflowing(el) {
+  return el.clientHeight < el.scrollHeight;
+}
+
 const SummaryLine = React.createClass({
   propTypes: {
     crumb: React.PropTypes.object.isRequired
   },
 
+  getInitialState() {
+    return {
+      expanded: false,
+      hasOverflow: false
+    }
+  },
+
+  makeSummariesGreatAgain(ref) {
+    let hasOverflow = isOverflowing(ref);
+    if (hasOverflow !== this.state.hasOverflow) {
+      this.setState({
+        hasOverflow: hasOverflow
+      });
+    }
+  },
+
+  onToggle() {
+    this.setState({
+      expanded: !this.state.expanded
+    });
+  },
+
   render() {
-    // this is where we can later also show other interesting
-    // information (maybe duration?)
+    let className = 'summary';
+    if (this.state.hasOverflow) {
+      className += ' can-expand';
+    }
+    if (this.state.expanded) {
+      className += ' expanded';
+    }
     return (
-      <div className="summary">
+      <div
+        className={className}
+        onClick={this.onToggle}
+        ref={this.makeSummariesGreatAgain}>
         {this.props.children}
       </div>
     );
