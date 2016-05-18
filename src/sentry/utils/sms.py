c058b716bdd4cf146f6657fff1bdc96d01a989c5
Armin Ronacher <armin.ronacher@active-4.com>
2016-05-18 22:53:03 +0200

    Use hyphens for twilio settings

--- src/sentry/utils/sms.py
+++ src/sentry/utils/sms.py
@@ -11,11 +11,11 @@ logger = logging.getLogger(__name__)
 
 
 def sms_available():
-    return bool(options.get('sms.twilio_account'))
+    return bool(options.get('sms.twilio-account'))
 
 
 def send_sms(body, to, from_=None):
-    account = options.get('sms.twilio_account')
+    account = options.get('sms.twilio-account')
     if not account:
         raise RuntimeError('SMS backend is not configured.')
     if account[:2] != 'AC':
@@ -23,9 +23,9 @@ def send_sms(body, to, from_=None):
     url = 'https://api.twilio.com/2010-04-01/Accounts/%s/Messages.json' % \
         urllib.quote(account)
     rv = requests.post(url, auth=(account,
-                             options.get('sms.twilio_token')), data={
+                             options.get('sms.twilio-token')), data={
         'To': to,
-        'From': options.get('sms.twilio_number'),
+        'From': options.get('sms.twilio-number'),
         'Body': body,
     })
     if not rv.ok:
