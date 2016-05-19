ccb4a2cef1be854a0d41af1b33236e0bf2d71e7c
Armin Ronacher <armin.ronacher@active-4.com>
2016-05-19 16:02:31 +0200

    Validate system prefix in signed links

--- tests/sentry/utils/test_linksign.py
+++ tests/sentry/utils/test_linksign.py
@@ -2,26 +2,34 @@
 
 from __future__ import absolute_import
 
+from django.test.client import RequestFactory
+
 from sentry.testutils import TestCase
 from sentry.utils import linksign
-from django.test.client import RequestFactory
 
 
 class LinkSignTestCase(TestCase):
 
     def test_link_signing(self):
+        rf = RequestFactory()
+
         url = linksign.generate_signed_link(self.user, 'sentry')
         assert url.startswith('http://')
 
-        req = RequestFactory().get('/' + url.split('/', 3)[-1])
+        req = rf.get('/' + url.split('/', 3)[-1])
         signed_user = linksign.process_signature(req)
         assert signed_user
         assert signed_user.id == self.user.id
 
-        req = RequestFactory().get('/what' + url.split('/', 3)[-1])
+        req = rf.get('/what' + url.split('/', 3)[-1])
         signed_user = linksign.process_signature(req)
         assert signed_user is None
 
-        req = RequestFactory().get('/' + url.split('/', 3)[-1] + 'garbage')
+        req = rf.get('/' + url.split('/', 3)[-1] + 'garbage')
+        signed_user = linksign.process_signature(req)
+        assert signed_user is None
+
+        rf.defaults['SERVER_NAME'] = 'something-else'
+        req = rf.get('/' + url.split('/', 3)[-1])
         signed_user = linksign.process_signature(req)
         assert signed_user is None
