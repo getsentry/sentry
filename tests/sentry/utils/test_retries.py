62404c73a47c117fc711c64006134d2e35abc85b
Ted Kaemming <ted@kaemming.com>
2016-05-23 14:30:50 -0700

    Allow overriding the clock used by `TimedRetryPolicy`.
    This is intended to allow isolating the mocked clock used by
    `TimedRetryPolicyTestCase` to avoid failures when these tests are ran in
    environments that are also accessing clock methods like `time.time`.

--- tests/sentry/utils/test_retries.py
+++ tests/sentry/utils/test_retries.py
@@ -1,7 +1,7 @@
 import mock
 
-from sentry.utils.retries import TimedRetryPolicy, RetryException
 from sentry.testutils import TestCase
+from sentry.utils.retries import TimedRetryPolicy, RetryException
 
 
 class TimedRetryPolicyTestCase(TestCase):
@@ -10,21 +10,27 @@ class TimedRetryPolicyTestCase(TestCase):
         callable = mock.MagicMock(side_effect=[bomb, mock.sentinel.OK])
 
         retry = TimedRetryPolicy(30, delay=lambda i: 10)
-        with mock.patch('time.sleep'), mock.patch('time.time', side_effect=[0, 15]):
-            assert retry(callable) is mock.sentinel.OK
-            assert callable.call_count == 2
+        retry.clock = mock.Mock()
+        retry.clock.sleep = mock.MagicMock()
+        retry.clock.time = mock.MagicMock(side_effect=[0, 15])
+
+        assert retry(callable) is mock.sentinel.OK
+        assert callable.call_count == 2
 
     def test_policy_failure(self):
         bomb = Exception('Boom!')
         callable = mock.MagicMock(side_effect=bomb)
 
         retry = TimedRetryPolicy(30, delay=lambda i: 10)
-        with mock.patch('time.sleep'), mock.patch('time.time', side_effect=[0, 15, 25]):
-            try:
-                retry(callable)
-            except RetryException as exception:
-                assert exception.exception is bomb
-            else:
-                self.fail('Expected {!r}!'.format(RetryException))
+        retry.clock = mock.Mock()
+        retry.clock.sleep = mock.MagicMock()
+        retry.clock.time = mock.MagicMock(side_effect=[0, 15, 25])
 
-            assert callable.call_count == 2
+        try:
+            retry(callable)
+        except RetryException as exception:
+            assert exception.exception is bomb
+        else:
+            self.fail('Expected {!r}!'.format(RetryException))
+
+        assert callable.call_count == 2
