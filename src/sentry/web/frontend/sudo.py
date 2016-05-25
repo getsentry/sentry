447fea03022b5c0a5dace4abd099d4115360e8d0
Matt Robenolt <matt@ydekproductions.com>
2016-05-25 07:02:15 -0700

    Fix sudo flow for password (#3319)

--- src/sentry/web/frontend/sudo.py
+++ src/sentry/web/frontend/sudo.py
@@ -10,17 +10,23 @@ class SudoView(BaseSudoView):
     template_name = 'sentry/account/sudo.html'
 
     def handle_sudo(self, request, redirect_to, context):
+        if BaseSudoView.handle_sudo(self, request, redirect_to, context):
+            return True
+
         interface = Authenticator.objects.get_interface(request.user, 'u2f')
 
         if interface.is_available and interface.is_enrolled:
             challenge = interface.activate(request).challenge
             if request.method == 'POST':
-                if 'challenge' in request.POST:
-                    challenge = json.loads(request.POST['challenge'])
-                if 'response' in request.POST:
-                    response = json.loads(request.POST['response'])
-                    if interface.validate_response(request, challenge, response):
-                        return True
+                if 'challenge' in request.POST and 'response' in request.POST:
+                    try:
+                        challenge = json.loads(request.POST['challenge'])
+                        response = json.loads(request.POST['response'])
+                    except ValueError:
+                        pass
+                    else:
+                        if interface.validate_response(request, challenge, response):
+                            return True
             context['u2f_challenge'] = challenge
 
-        return BaseSudoView.handle_sudo(self, request, redirect_to, context)
+        return False
