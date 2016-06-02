68473371c8c7ad5b1c0af969ac420ebbc8546f87
Matt Robenolt <matt@ydekproductions.com>
2016-06-02 11:34:58 -0700

    Correct static media behavior around directories
    Fixes GH-3386

--- src/sentry/web/frontend/generic.py
+++ src/sentry/web/frontend/generic.py
@@ -29,9 +29,9 @@ def resolve(path):
     normalized_path = posixpath.normpath(unquote(path)).lstrip('/')
     absolute_path = finders.find(normalized_path)
     if not absolute_path:
-        if path.endswith('/') or path == '':
-            raise Http404("Directory indexes are not allowed here.")
         raise Http404("'%s' could not be found" % path)
+    if path[-1] == '/' or os.path.isdir(absolute_path):
+        raise Http404('Directory indexes are not allowed here.')
     return os.path.split(absolute_path)
 
 
@@ -48,10 +48,10 @@ def static_media(request, **kwargs):
 
     try:
         document_root, path = resolve(path)
-    except Http404:
+    except Http404 as e:
         # Return back a simpler plain-text 404 response, more suitable
         # for static files, rather than our full blown HTML.
-        return HttpResponseNotFound('File not found.\n', content_type='text/plain')
+        return HttpResponseNotFound(e.message + '\n', content_type='text/plain')
 
     if 'gzip' in request.META.get('HTTP_ACCEPT_ENCODING', '') and not path.endswith('.gz') and not settings.DEBUG:
         paths = (path + '.gz', path)
