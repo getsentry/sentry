e4118ee02c1060591b80781855e3084293f9852b
Armin Ronacher <armin.ronacher@active-4.com>
2016-06-03 22:13:35 +0200

    Fixed linksign using incorrect bases

--- src/sentry/utils/linksign.py
+++ src/sentry/utils/linksign.py
@@ -4,7 +4,7 @@ from django.core.urlresolvers import reverse
 from sentry import options
 from sentry.models import User
 from sentry.utils.http import absolute_uri
-from sentry.utils.numbers import base36_encode, base32_decode
+from sentry.utils.numbers import base36_encode, base36_decode
 
 
 def get_signer():
@@ -26,7 +26,8 @@ def generate_signed_link(user, viewname, args=None, kwargs=None):
         user_id = user
 
     path = reverse(viewname, args=args, kwargs=kwargs)
-    item = '%s|%s|%s' % (options.get('system.url-prefix'), path, user_id)
+    item = '%s|%s|%s' % (options.get('system.url-prefix'), path,
+                         base36_encode(user_id))
     signature = ':'.join(get_signer().sign(item).rsplit(':', 2)[1:])
 
     return '%s?_=%s:%s' % (
@@ -56,6 +57,6 @@ def process_signature(request, max_age=60 * 60 * 24 * 10):
         return None
 
     try:
-        return User.objects.get(pk=base32_decode(user_id))
+        return User.objects.get(pk=base36_decode(user_id))
     except (ValueError, User.DoesNotExist):
         return None
