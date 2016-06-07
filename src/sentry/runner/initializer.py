dfb26fb60fc05077232ae3037468fa2a23324cec
James Cunningham <jtcunning@getsentry.com>
2016-06-07 10:54:20 -0700

    Strip the timestamp.

--- src/sentry/runner/initializer.py
+++ src/sentry/runner/initializer.py
@@ -159,12 +159,10 @@ def configure_structlog():
         processors=[
             structlog.stdlib.add_log_level,
             structlog.stdlib.PositionalArgumentsFormatter(),
-            structlog.processors.TimeStamper(fmt="iso"),
             structlog.processors.StackInfoRenderer(),
             structlog.processors.format_exc_info,
             structlog.processors.ExceptionPrettyPrinter(),
             structlog.processors.KeyValueRenderer(key_order=[
-                'timestamp',
                 'name',
                 'level',
                 'event',
