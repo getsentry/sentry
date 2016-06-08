bf2c4d254f720a1e00aaaccac2343a8a9ef4ff3f
David Cramer <dcramer@gmail.com>
2016-06-08 14:39:26 -0700

    Add sorting on tag value endpoint

--- src/sentry/api/endpoints/group_tagkey_values.py
+++ src/sentry/api/endpoints/group_tagkey_values.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 from sentry.api.base import DocSection
 from sentry.api.bases.group import GroupEndpoint
 from sentry.api.exceptions import ResourceDoesNotExist
+from sentry.api.paginator import DateTimePaginator, OffsetPaginator, Paginator
 from sentry.api.serializers import serialize
 from sentry.models import GroupTagValue, TagKey, TagKeyStatus, Group
 from sentry.utils.apidocs import scenario
@@ -53,9 +54,24 @@ class GroupTagKeyValuesEndpoint(GroupEndpoint):
             key=lookup_key,
         )
 
+        sort = request.GET.get('sort')
+        if sort == 'date':
+            order_by = '-last_seen'
+            paginator_cls = DateTimePaginator
+        elif sort == 'age':
+            order_by = '-first_seen'
+            paginator_cls = DateTimePaginator
+        elif sort == 'freq':
+            order_by = '-times_seen'
+            paginator_cls = OffsetPaginator
+        else:
+            order_by = '-id'
+            paginator_cls = Paginator
+
         return self.paginate(
             request=request,
             queryset=queryset,
-            order_by='-id',
+            order_by=order_by,
+            paginator_cls=paginator_cls,
             on_results=lambda x: serialize(x, request.user),
         )
