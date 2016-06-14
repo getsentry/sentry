d10c24c6820b0b2953274e89dc95f429567a3a56
Matt Robenolt <matt@ydekproductions.com>
2016-06-14 15:31:19 -0700

    Always set security headers

---
+++ src/sentry/middleware/security.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import
+
+
+class SecurityHeadersMiddleware(object):
+    """
+    Ensure that we have proper security headers set
+    """
+    def process_response(self, request, response):
+        # NOTE: there is no `response.setdefault()`
+        if 'X-Frame-Options' not in response:
+            response['X-Frame-Options'] = 'deny'
+        if 'X-Content-Type-Options' not in response:
+            response['X-Content-Type-Options'] = 'nosniff'
+        if 'X-XSS-Protection' not in response:
+            response['X-XSS-Protection'] = '1; mode=block'
+        return response
