f4f7356d9442179399b5c851df9416859ca883ad
Matt Robenolt <matt@ydekproductions.com>
2016-06-17 11:30:54 -0700

    Make sure Rules labels are unicode
    This causes formatting issues since the values we pass in are unicode,
    so if they're outside ascii, it blows up.
    
    Fixes SENTRY-188

--- src/sentry/rules/conditions/event_attribute.py
+++ src/sentry/rules/conditions/event_attribute.py
@@ -92,7 +92,7 @@ class EventAttributeCondition(EventCondition):
     # TODO(dcramer): add support for stacktrace.vars.[name]
 
     form_cls = EventAttributeForm
-    label = 'An events {attribute} value {match} {value}'
+    label = u'An events {attribute} value {match} {value}'
 
     def _get_attribute_values(self, event, attr):
         # TODO(dcramer): we should validate attributes (when we can) before
