076b23210ec0597a99961fc6aa6f3edf05ab8500
David Cramer <dcramer@gmail.com>
2016-06-21 12:43:45 -0700

    Improve acceptance tests
    - Abstract login helper to re-use existing fixtures
    - Add basic rendering of stream states
    - Add local screenshot capture
    - Remove captcha on auth tests
    
    /cc @getsentry/ui @getsentry/infrastructure

--- tests/acceptance/test_auth.py
+++ tests/acceptance/test_auth.py
@@ -4,23 +4,32 @@ from sentry.testutils import AcceptanceTestCase
 
 
 class AuthTest(AcceptanceTestCase):
+    def enter_auth(self, username, password):
+        # disable captcha as it makes these tests flakey (and requires waiting
+        # on external resources)
+        with self.settings(RECAPTCHA_PUBLIC_KEY=None):
+            self.browser.get(self.route('/auth/login/'))
+            self.browser.find_element_by_id('id_username').send_keys(username)
+            self.browser.find_element_by_id('id_password').send_keys(password)
+            self.browser.find_element_by_xpath("//button[contains(text(), 'Login')]").click()
+
     def test_auth_page(self):
         self.browser.get(self.live_server_url)
-        self.percy.snapshot(name='login')
+        self.snapshot(name='login')
 
     def test_auth_failures(self):
-        self.login('', '')
-        self.percy.snapshot(name='login fields required')
+        self.enter_auth('', '')
+        self.snapshot(name='login fields required')
 
-        self.login('bad-username', 'bad-username')
-        self.percy.snapshot(name='login fields invalid')
+        self.enter_auth('bad-username', 'bad-username')
+        self.snapshot(name='login fields invalid')
 
-    def test_new_organization(self):
+    def test_auth_success(self):
         email = 'dummy@example.com'
         password = 'dummy'
         user = self.create_user(email=email)
         user.set_password(password)
         user.save()
 
-        self.login(email, password)
-        self.percy.snapshot()
+        self.enter_auth(email, password)
+        self.snapshot(name='login success')
