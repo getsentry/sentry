15ef4142c23741459627ee68ae9732389bb0dfef
James Cunningham <cunninghamjt09@gmail.com>
2016-06-21 13:35:06 -0700

    Log record.args no matter what structure it is. (#3549)
    * Log record.args no matter what structure it is.
    
    * Just assign args blindly.

--- src/sentry/logging/handlers.py
+++ src/sentry/logging/handlers.py
@@ -19,9 +19,8 @@ class StructLogHandler(logging.StreamHandler):
         }
         if record.exc_info:
             kwargs['exc_info'] = record.exc_info
-
-        if isinstance(record.args, dict):
-            kwargs.update(record.args)
+        if record.args:
+            kwargs['args'] = record.args
 
         # HACK(JTCunning): Calling structlog.log instead of the corresponding level
         # methods steps on the toes of django client loggers and their testing components.
