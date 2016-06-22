88f64e94847ad1e3d8bb07a1ef27be8658877e30
Matt Robenolt <matt@ydekproductions.com>
2016-06-22 13:44:25 -0700

    Pass positional_args correctly from stdlib log handler to structlog (#3564)

--- src/sentry/logging/handlers.py
+++ src/sentry/logging/handlers.py
@@ -11,8 +11,6 @@ import logging
 from structlog import get_logger
 from structlog.processors import _json_fallback_handler
 
-logger = get_logger()
-
 _default_encoder = JSONEncoder(
     separators=(',', ':'),
     ignore_nan=True,
@@ -32,19 +30,29 @@ class JSONRenderer(object):
 
 
 class StructLogHandler(logging.StreamHandler):
-    def emit(self, record):
+    def emit(self, record, logger=get_logger()):
         kwargs = {
             'name': record.name,
         }
         if record.exc_info:
             kwargs['exc_info'] = record.exc_info
+
         if record.args:
-            kwargs['positional_args'] = record.args
+            # record.args inside of LogRecord.__init__ gets unrolled
+            # if it's the shape `({},)`, a single item dictionary.
+            # so we need to check for this, and re-wrap it because
+            # down the line of structlog, it's expected to be this
+            # original shape.
+            if isinstance(record.args, (tuple, list)):
+                kwargs['positional_args'] = record.args
+            else:
+                kwargs['positional_args'] = (record.args,)
 
         # HACK(JTCunning): Calling structlog.log instead of the corresponding level
         # methods steps on the toes of django client loggers and their testing components.
-        log = getattr(logger, logging.getLevelName(record.levelno).lower(), None)
-        if log:
-            log(record.msg, **kwargs)
-        else:
+        try:
+            log = getattr(logger, logging.getLevelName(record.levelno).lower())
+        except AttributeError:
             super(StructLogHandler, self).emit(record)
+        else:
+            log(record.msg, **kwargs)
