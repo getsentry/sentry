0dce54016709a20fdf827fdf841d0652e2555e59
David Cramer <dcramer@gmail.com>
2016-06-22 18:14:54 -0700

    Merge contexts together

--- src/sentry/static/sentry/app/components/events/contexts.jsx
+++ src/sentry/static/sentry/app/components/events/contexts.jsx
@@ -1,15 +1,46 @@
 import React from 'react';
 
 import GroupEventDataSection from './eventDataSection';
-import {toTitleCase} from '../../utils';
+import {objectIsEmpty, toTitleCase} from '../../utils';
+import {t} from '../../locale';
 
 const CONTEXT_TYPES = {
   'default': require('./contexts/default'),
   'device': require('./contexts/device'),
   'os': require('./contexts/os'),
   'runtime': require('./contexts/runtime'),
+  'user': require('./contexts/user'),
 };
 
+const ContextChunk = React.createClass({
+  propTypes: {
+    type: React.PropTypes.string.isRequired,
+    alias: React.PropTypes.string.isRequired,
+    value: React.PropTypes.object.isRequired,
+  },
+
+  render() {
+    let {type, alias, value} = this.props;
+    let title = value.title || toTitleCase(alias);
+    let Component = CONTEXT_TYPES[type] || CONTEXT_TYPES.default;
+
+    return (
+      <div className="context-box">
+        <div className="context-header">
+          <h5>
+            <strong>{title}</strong>
+            {value.title &&
+              <small> ({name})</small>
+            }
+          </h5>
+        </div>
+        <div className="context-content">
+          <Component alias={alias} data={value} />
+        </div>
+      </div>
+    );
+  },
+});
 
 const ContextsInterface = React.createClass({
   propTypes: {
@@ -20,35 +51,40 @@ const ContextsInterface = React.createClass({
   render() {
     let group = this.props.group;
     let evt = this.props.event;
-    let rv = [];
-    for (let key in evt.contexts) {
-      let value = evt.contexts[key];
-      let Component = CONTEXT_TYPES[value.type] || CONTEXT_TYPES.default;
-
-      let title = (
-        <div>
-          <h3>
-            <strong>{value.title || toTitleCase(key)}</strong>
-            <small> ({key})</small>
-          </h3>
-        </div>
-      );
-
-      rv.push((
-        <GroupEventDataSection
-            className="context-box"
-            group={group}
-            event={evt}
-            key={key}
-            type={`${key}-context`}
-            wrapTitle={false}
-            title={title}>
-          <Component alias={key} data={value} />
-        </GroupEventDataSection>
-
+    let children = [];
+    if (!objectIsEmpty(evt.user)) {
+      children.push((
+        <ContextChunk
+          type="user"
+          alias="user"
+          value={evt.user}
+          key="user" />
       ));
     }
-    return <div>{rv}</div>;
+
+    let value = null;
+    for (let key in evt.contexts) {
+      value = evt.contexts[key];
+      children.push((
+        <ContextChunk
+          type={value.type}
+          alias={key}
+          value={value}
+          key={key} />
+      ));
+    }
+
+    return (
+      <GroupEventDataSection
+          className="context-section"
+          group={group}
+          event={evt}
+          key="context"
+          type="contexts"
+          title={t('Context')}>
+        {children}
+      </GroupEventDataSection>
+    );
   },
 });
 
