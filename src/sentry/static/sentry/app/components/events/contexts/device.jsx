1b33b7e4e5a115ad7a2b8f7c9474cd2c67a48a16
Armin Ronacher <armin.ronacher@active-4.com>
2016-06-30 10:52:36 +0300

    Allow some known values on the context to be skipped

--- src/sentry/static/sentry/app/components/events/contexts/device.jsx
+++ src/sentry/static/sentry/app/components/events/contexts/device.jsx
@@ -16,13 +16,13 @@ const DeviceContextType = React.createClass({
       <ContextBlock
         data={data}
         knownData={[
-          ['Name', name],
+          ['?Name', name],
           ['Family', family],
           ['Model', model + (model_id ? ` (${model_id})` : '')],
           ['Architecture', arch],
-          ['Battery Level', defined(battery_level)
+          ['?Battery Level', defined(battery_level)
             ? `${battery_level}%` : null],
-          ['Orientation', orientation],
+          ['?Orientation', orientation],
         ]}
         alias={this.props.alias} />
     );
