dac8a2b8894ccc3e9aadca14d4738cd64e05cb48
Jess MacQueen <jess@getsentry.com>
2016-07-01 18:04:43 -0700

    wip plugins to react
    move github configuration to react form
    
    fix main plugin config page, add error handling to react component
    
    handle plugin not being configured for a project
    
    make external issues tab handle multiple plugins
    
    resolve todos in select2 form field component
    
    add success/error messages for plugin configuration component
    
    improve external issues plugin ui
    
    move route definitions to plugin
    
    move issue plugin forms to modal
    
    add external issues to the sidebar
    
    uncomment error that isn't happening anymore
    
    remove unused methods, add read only fields
    
    move error messages to modal, add basic validation
    
    handle github api errors
    
    move config form into separate component
    
    add assignee back to create form
    
    add helptext and required to react forms

--- src/sentry/plugins/bases/__init__.py
+++ src/sentry/plugins/bases/__init__.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from .issue import IssueTrackingPlugin  # NOQA
+from .issue2 import IssueTrackingPlugin2  # NOQA
 from .notify import NotificationPlugin  # NOQA
 from .releasetracking import ReleaseTrackingPlugin  # NOQA
 from .tag import TagPlugin  # NOQA
--- src/sentry/web/frontend/project_plugin_configure.py
+++ src/sentry/web/frontend/project_plugin_configure.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 
-from sentry.plugins import plugins
+from sentry.plugins import plugins, IssueTrackingPlugin2
 from sentry.web.frontend.base import ProjectView
 
 
@@ -19,16 +19,28 @@ class ProjectPluginConfigureView(ProjectView):
         if not plugin.can_configure_for_project(project):
             return self.redirect(reverse('sentry-manage-project', args=[project.organization.slug, project.slug]))
 
-        view = plugin.configure(request=request, project=project)
-        if isinstance(view, HttpResponse):
-            return view
+        issue_v2_plugin = None
+        is_enabled = plugin.is_enabled(project)
+        if isinstance(plugin, IssueTrackingPlugin2):
+            view = None
+            issue_v2_plugin = {
+                'title': plugin.get_title(),
+                'slug': plugin.slug,
+                'can_disable': plugin.can_disable,
+                'is_enabled': is_enabled
+            }
+        else:
+            view = plugin.configure(request=request, project=project)
+            if isinstance(view, HttpResponse):
+                return view
 
         context = {
             'page': 'plugin',
             'title': plugin.get_title(),
             'view': view,
             'plugin': plugin,
-            'plugin_is_enabled': plugin.is_enabled(project),
+            'plugin_is_enabled': is_enabled,
+            'issue_v2_plugin': issue_v2_plugin
         }
 
         return self.respond('sentry/projects/plugins/configure.html', context)
