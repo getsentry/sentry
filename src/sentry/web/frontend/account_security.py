5bb9e50ab826049e14e5ec6ec3205f923d420f1f
Matt Robenolt <matt@ydekproductions.com>
2016-07-06 20:31:16 -0700

    Pass along 2fa status to security settings tab (#3661)
    This was missing, causing the page to always say 2fa was disabled

--- src/sentry/web/frontend/account_security.py
+++ src/sentry/web/frontend/account_security.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from sentry.models import Authenticator
 from sentry.web.frontend.base import BaseView
 
 
@@ -7,4 +8,5 @@ class AccountSecurityView(BaseView):
     def handle(self, request):
         return self.respond('sentry/account/security.html', {
             'page': 'security',
+            'has_2fa': Authenticator.objects.user_has_2fa(request.user),
         })
