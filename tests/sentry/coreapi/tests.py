e6f18e889f78de3c5fc194320c972548996ecefe
Matt Robenolt <matt@ydekproductions.com>
2016-07-11 11:19:23 -0700

    Allow passing sentry_release querystring to CSP reports (#3665)
    This allows passing along release information to CSP reports which will
    help reduce noise by being able to leverage "resolved in next release"
    functionality.

--- tests/sentry/coreapi/tests.py
+++ tests/sentry/coreapi/tests.py
@@ -480,11 +480,16 @@ class CspApiHelperTest(BaseAPITest):
             "effective-directive": "img-src",
             "original-policy": "default-src  https://45.55.25.245:8123/; child-src  https://45.55.25.245:8123/; connect-src  https://45.55.25.245:8123/; font-src  https://45.55.25.245:8123/; img-src  https://45.55.25.245:8123/; media-src  https://45.55.25.245:8123/; object-src  https://45.55.25.245:8123/; script-src  https://45.55.25.245:8123/; style-src  https://45.55.25.245:8123/; form-action  https://45.55.25.245:8123/; frame-ancestors 'none'; plugin-types 'none'; report-uri http://45.55.25.245:8123/csp-report?os=OS%20X&device=&browser_version=43.0&browser=chrome&os_version=Lion",
             "blocked-uri": "http://google.com",
-            "status-code": 200
+            "status-code": 200,
+            "_meta": {
+                "release": "abc123",
+            }
         }
         result = self.helper.validate_data(self.project, report)
         assert result['logger'] == 'csp'
         assert result['project'] == self.project.id
+        assert result['release'] == 'abc123'
+        assert result['errors'] == []
         assert 'message' in result
         assert 'culprit' in result
         assert 'tags' in result
