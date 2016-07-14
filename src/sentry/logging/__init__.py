f32adc85e0e36e0b89655a354b6fcec247145ea2
James Cunningham <cunninghamjt09@gmail.com>
2016-07-14 07:06:00 -1000

    Add a small utility to wrap structlog.logger.bind() (#3717)

--- src/sentry/logging/__init__.py
+++ src/sentry/logging/__init__.py
@@ -7,7 +7,17 @@ sentry.logging
 
 from __future__ import absolute_import
 
+from structlog import get_logger
+
 
 class LoggingFormat(object):
     HUMAN = 'human'
     MACHINE = 'machine'
+
+
+def bind(name, **kwargs):
+    """
+    Syntactic sugar for binding arbitrary kv pairs to a given logger instantiated from
+    logging.getLogger instead of structlog.get_logger.
+    """
+    return get_logger(name=name).bind(**kwargs)
