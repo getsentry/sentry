8ed6c8d66edb8047d898c84e3fcb00211347019b
Ted Kaemming <ted@kaemming.com>
2016-07-14 14:53:49 -0700

    Make event datetimes in new event and digest previews deterministic.
    I forgot this as part of GH-3719 (a6a1f32efee783ad1ad00f8e663389fbea0853d8).

--- src/sentry/web/frontend/debug/mail.py
+++ src/sentry/web/frontend/debug/mail.py
@@ -220,11 +220,9 @@ def new_event(request):
         organization=org,
     )
 
+    random = get_random(request)
     group = next(
-        make_group_generator(
-            get_random(request),
-            project,
-        ),
+        make_group_generator(random, project),
     )
 
     event = Event(
@@ -233,6 +231,12 @@ def new_event(request):
         group=group,
         message=group.message,
         data=load_data(platform),
+        datetime=to_datetime(
+            random.randint(
+                to_timestamp(group.first_seen),
+                to_timestamp(group.last_seen),
+            ),
+        ),
     )
 
     rule = Rule(label="An example rule")
@@ -268,8 +272,6 @@ def new_event(request):
 @login_required
 def digest(request):
     random = get_random(request)
-
-    now = datetime.utcnow().replace(tzinfo=pytz.utc)
 
     # TODO: Refactor all of these into something more manageable.
     org = Organization(
@@ -326,7 +328,12 @@ def digest(request):
                 group=group,
                 message=group.message,
                 data=load_data('python'),
-                datetime=now - offset,
+                datetime=to_datetime(
+                    random.randint(
+                        to_timestamp(group.first_seen),
+                        to_timestamp(group.last_seen),
+                    ),
+                )
             )
 
             records.append(
