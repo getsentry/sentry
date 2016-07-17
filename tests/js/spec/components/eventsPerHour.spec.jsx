7fdcc5fd717b6691686504b1381711f52e541545
Ben Vinegar <benvinegar@users.noreply.github.com>
2016-07-17 13:30:52 -0700

    Upgrade to React 15.2.1 [try 2] (#3689) (#3734)

--- tests/js/spec/components/eventsPerHour.spec.jsx
+++ tests/js/spec/components/eventsPerHour.spec.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import TestUtils from 'react-addons-test-utils';
+import {shallow} from 'enzyme';
 import EventsPerHour from 'app/components/events/eventsPerHour';
-
 
 describe('EventsPerHour', function() {
   const data = {
@@ -9,9 +8,10 @@ describe('EventsPerHour', function() {
     blacklisted: [[1461099600, 0], [1461103200, 0]],
     rejected: [[1461099600, 2867], [1461103200, 2742]]
   };
-  describe('formatData()', function() {
-    sinon.stub(EventsPerHour.prototype.__reactAutoBindMap, 'fetchData').returns(null);
-    let eventsPerHour = TestUtils.renderIntoDocument(<EventsPerHour />);
+
+  it('should work', function () {
+    sinon.stub(EventsPerHour.prototype, 'fetchData');
+    let eventsPerHour = shallow(<EventsPerHour/>).instance();
     expect(eventsPerHour.formatData(data)).to.deep.equal([{x: 1461099600, y: [28867, 2867, 0]},
                                                           {x: 1461103200, y: [34048, 2742, 0]}]);
   });
