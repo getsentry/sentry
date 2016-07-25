f474ae290d44611526ec167faae162d6c0b707f3
Eric Feng <eric@getsentry.com>
2016-07-25 14:30:16 -0700

    Email Signals
    Adding email_modified signals. Also changing old email_verified signals (will modify this in getsentry to accept new arguments)
    
    /cc @getsentry/team

--- src/sentry/signals.py
+++ src/sentry/signals.py
@@ -39,4 +39,5 @@ member_joined = BetterSignal(providing_args=["member"])
 issue_tracker_used = BetterSignal(providing_args=["plugin", "project", "user"])
 plugin_enabled = BetterSignal(providing_args=["plugin", "project", "user"])
 
-email_verified = BetterSignal(providing_args=["email"])
+email_modified = BetterSignal(providing_args=["useremail"])
+email_verified = BetterSignal(providing_args=["useremail"])
