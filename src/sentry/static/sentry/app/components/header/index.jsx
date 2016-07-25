5faba6275be24d1239d9e9f8d18d131c00e5b7dc
ckj <chriskjennings@gmail.com>
2016-07-25 13:12:34 -0700

    Move sidebarPanel into its own component file

--- src/sentry/static/sentry/app/components/header/index.jsx
+++ src/sentry/static/sentry/app/components/header/index.jsx
@@ -11,6 +11,7 @@ import StatusPage from './statuspage';
 import UserNav from './userNav';
 import requiredAdminActions from '../requiredAdminActions';
 import OrganizationSelector from './organizationSelector';
+import SidebarPanel from '../sidebarPanel';
 import TodoList from '../todos';
 import {t} from '../../locale';
 
@@ -57,40 +58,6 @@ function getFirstRequiredAdminAction(org) {
   }
   return null;
 }
-
-
-const SidebarPanel = React.createClass({
-  propTypes: {
-    title: React.PropTypes.string,
-    items: React.PropTypes.array,
-    onHidePanel: React.PropTypes.func
-  },
-
-  render() {
-    return (
-      <div className="sidebar-panel">
-        <div className="sidebar-panel-header">
-          <a className="close pull-right" onClick={this.props.hidePanel}><span className="icon-x" /></a>
-          <h2>{this.props.title}</h2>
-        </div>
-        <div className="sidebar-panel-items">
-          <SidebarPanelItem />
-        </div>
-      </div>
-    );
-  }
-});
-
-const SidebarPanelItem = React.createClass({
-  render() {
-    return (
-      <div className="sidebar-panel-item">
-        <h3>Type Error <span className="culprit">poll(../../sentry/scripts/views.js)</span></h3>
-        <div className="message">Object [object Object] has no method 'updateFrom'</div>
-      </div>
-    );
-  }
-});
 
 const Header = React.createClass({
   propTypes: {
