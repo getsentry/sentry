c36ac1b93e5adc8c3346e6d608932ae613f831b4
ckj <chriskjennings@gmail.com>
2016-08-01 17:30:37 -0700

    Stub out proof of concept

--- webpack.config.js
+++ webpack.config.js
@@ -123,6 +123,13 @@ var config = {
       {
         test: /\.(woff|woff2|ttf|eot|svg|png|gif|ico|jpg)($|\?)/,
         loader: 'file-loader?name=' + '[name].[ext]'
+      },
+      {
+          test: /\.css$/,
+          loaders: [
+              'style?sourceMap',
+              'css?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]'
+          ]
       }
     ],
     noParse: [
@@ -144,7 +151,7 @@ var config = {
       'root.jQuery': 'jquery',
       Raven: 'raven-js'
     }),
-    new ExtractTextPlugin('[name].css'),
+    new ExtractTextPlugin('[name].css', { allChunks: true }),
     new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/), // ignore moment.js locale files
     new webpack.DefinePlugin({
       'process.env': {
