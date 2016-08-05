af1145365d8a7239ecc96696394714f15a667f86
Matt Robenolt <matt@ydekproductions.com>
2016-08-04 17:15:08 -0700

    Remove my social-auth patch and just apply it now to our vendored version

--- src/social_auth/utils.py
+++ src/social_auth/utils.py
@@ -42,42 +42,6 @@ def sanitize_log_data(secret, data=None, leave_characters=LEAVE_CHARS):
     return replace_secret
 
 
-def sanitize_redirect(host, redirect_to):
-    """
-    Given the hostname and an untrusted URL to redirect to,
-    this method tests it to make sure it isn't garbage/harmful
-    and returns it, else returns None, similar as how's it done
-    on django.contrib.auth.views.
-
-    >>> print sanitize_redirect('myapp.com', None)
-    None
-    >>> print sanitize_redirect('myapp.com', '')
-    None
-    >>> print sanitize_redirect('myapp.com', {})
-    None
-    >>> print sanitize_redirect('myapp.com', 'http://notmyapp.com/path/')
-    None
-    >>> print sanitize_redirect('myapp.com', 'http://myapp.com/path/')
-    http://myapp.com/path/
-    >>> print sanitize_redirect('myapp.com', '/path/')
-    /path/
-    """
-    # Quick sanity check.
-    if not redirect_to:
-        return None
-
-    # Heavier security check, don't allow redirection to a different host.
-    try:
-        netloc = urlparse(redirect_to)[1]
-    except TypeError:  # not valid redirect_to value
-        return None
-
-    if netloc and netloc != host:
-        return None
-
-    return redirect_to
-
-
 def group_backend_by_type(items, key=lambda x: x):
     """Group items by backend type."""
 
