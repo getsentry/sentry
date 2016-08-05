5a08dee5ac91f5c650b44b19235838ca91845601
Matt Robenolt <matt@ydekproductions.com>
2016-08-04 17:15:08 -0700

    Remove my social-auth patch and just apply it now to our vendored version

--- src/sentry/web/urls.py
+++ src/sentry/web/urls.py
@@ -276,7 +276,7 @@ urlpatterns += patterns(
 
     url(r'^account/remove/$', RemoveAccountView.as_view(),
         name='sentry-remove-account'),
-    url(r'^account/settings/social/', include('sentry.social_auth.urls')),
+    url(r'^account/settings/social/', include('social_auth.urls')),
 
     # Admin
     url(r'^manage/queue/$', AdminQueueView.as_view(),
