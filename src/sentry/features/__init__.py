9f6e00f13b61980098327c5eed6b68667d760e98
ted kaemming <t.kaemming+github@gmail.com>
2016-08-09 11:02:05 -0700

    Basic report implementation. (#3836)
    This is a work in progress, and isn't configured as part of the Celery schedule accordingly.

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -13,10 +13,11 @@ default_manager.add('organizations:sso', OrganizationFeature)  # NOQA
 default_manager.add('organizations:onboarding', OrganizationFeature)  # NOQA
 default_manager.add('organizations:callsigns', OrganizationFeature)  # NOQA
 default_manager.add('organizations:new-tracebacks', OrganizationFeature)  # NOQA
+default_manager.add('organizations:reports:prepare', OrganizationFeature)  # NOQA
+default_manager.add('organizations:reports:deliver', OrganizationFeature)  # NOQA
 default_manager.add('projects:global-events', ProjectFeature)  # NOQA
 default_manager.add('projects:quotas', ProjectFeature)  # NOQA
 default_manager.add('projects:plugins', ProjectPluginFeature)  # NOQA
-
 
 # expose public api
 add = default_manager.add
