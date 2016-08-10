1870d9f801c0865047d045378347871bc8456a21
Jess MacQueen <jess@getsentry.com>
2016-08-09 18:38:29 -0700

    fix a few ui issues with react plugins

--- src/sentry/static/sentry/app/components/forms/form.jsx
+++ src/sentry/static/sentry/app/components/forms/form.jsx
@@ -6,13 +6,15 @@ const Form = React.createClass({
   propTypes: {
     onSubmit: React.PropTypes.func.isRequired,
     submitDisabled: React.PropTypes.bool,
-    submitLabel: React.PropTypes.string.isRequired
+    submitLabel: React.PropTypes.string.isRequired,
+    footerClass: React.PropTypes.string
   },
 
   getDefaultProps() {
     return {
       submitLabel: t('Save Changes'),
       submitDisabled: false,
+      footerClass: 'form-actions'
     };
   },
 
@@ -25,7 +27,7 @@ const Form = React.createClass({
     return (
       <form onSubmit={this.onSubmit}>
         {this.props.children}
-        <div className="form-actions" style={{marginTop: 25}}>
+        <div className={this.props.footerClass} style={{marginTop: 25}}>
           <button className="btn btn-primary"
                   disabled={this.props.submitDisabled}
                   type="submit">{this.props.submitLabel}</button>
