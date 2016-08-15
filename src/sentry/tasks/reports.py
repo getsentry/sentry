dc908e6fa554d3809191229f633d42d8d7daa703
ted kaemming <t.kaemming+github@gmail.com>
2016-08-15 12:49:01 -0700

    Prevent scheduling report delivery to organization members without associated user accounts. (#3940)

--- src/sentry/tasks/reports.py
+++ src/sentry/tasks/reports.py
@@ -386,7 +386,14 @@ def prepare_organization_report(timestamp, duration, organization_id):
 
     backend.prepare(timestamp, duration, organization)
 
-    for user_id in organization.member_set.values_list('user_id', flat=True):
+    # If an OrganizationMember row doesn't have an associated user, this is
+    # actually a pending invitation, so no report should be delivered.
+    member_set = organization.member_set.filter(
+        user_id__isnull=False,
+        user__is_active=True,
+    )
+
+    for user_id in member_set.values_list('user_id', flat=True):
         deliver_organization_user_report.delay(
             timestamp,
             duration,
