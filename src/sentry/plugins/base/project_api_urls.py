b98bfad36e8b56c22c82b353b4330e0f396728b7
Matt Robenolt <matt@ydekproductions.com>
2016-09-02 01:07:04 -0700

    Protect ourselves against broken plugin urls

--- src/sentry/plugins/base/project_api_urls.py
+++ src/sentry/plugins/base/project_api_urls.py
@@ -1,17 +1,44 @@
 from __future__ import absolute_import
 
+import logging
 import re
 
+from django.core.urlresolvers import RegexURLResolver, RegexURLPattern
 from django.conf.urls import patterns, include, url
 
 from sentry.plugins import plugins
 
+logger = logging.getLogger('sentry.plugins')
 
-urlpatterns = patterns('')
 
-for _plugin in plugins.all():
-    _plugin_project_urls = _plugin.get_project_urls()
-    if _plugin_project_urls:
-        urlpatterns.append(
-            url(r'^%s/' % re.escape(_plugin.slug), include(_plugin_project_urls))
-        )
+def is_valid_urls(urls):
+    if not urls:
+        return False
+
+    for u in urls:
+        if not isinstance(u, (RegexURLResolver, RegexURLPattern)):
+            return False
+
+    return True
+
+
+def load_plugin_urls(plugins):
+    urlpatterns = patterns('')
+
+    for _plugin in plugins:
+        try:
+            _plugin_project_urls = _plugin.get_project_urls()
+            assert is_valid_urls(_plugin_project_urls)
+        except Exception:
+            logger.exception('routes.failed', extra={
+                'plugin': type(_plugin).__name__,
+            })
+        else:
+            urlpatterns.append(
+                url(r'^%s/' % re.escape(_plugin.slug), include(_plugin_project_urls))
+            )
+
+    return urlpatterns
+
+
+urlpatterns = load_plugin_urls(plugins.all())
