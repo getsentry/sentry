d014b95bb639448247de7e017cdd18d14fabc5ed
David Cramer <dcramer@gmail.com>
2016-09-01 17:10:37 -0700

    Improve error handling

--- src/sentry/plugins/sentry_webhooks/plugin.py
+++ src/sentry/plugins/sentry_webhooks/plugin.py
@@ -8,6 +8,7 @@ from django import forms
 from django.conf import settings
 from django.utils.translation import ugettext_lazy as _
 
+from sentry.exceptions import PluginError
 from sentry.plugins.bases import notify
 from sentry.http import is_valid_url, safe_urlopen
 from sentry.utils.safe import safe_execute
@@ -20,9 +21,9 @@ def validate_urls(value, **kwargs):
         if not url:
             continue
         if not url.startswith(('http://', 'https://')):
-            raise forms.ValidationError('Not a valid URL.')
+            raise PluginError('Not a valid URL.')
         if not is_valid_url(url):
-            raise forms.ValidationError('Not a valid URL.')
+            raise PluginError('Not a valid URL.')
         output.append(url)
     return '\n'.join(output)
 
