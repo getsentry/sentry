5335db91ceb2568c3ba592fb904524eb3eb377ac
David Cramer <dcramer@gmail.com>
2016-09-01 23:23:26 -0700

    Optimize path for ReleaseEnvironment updates

--- tests/sentry/models/test_releaseenvironment.py
+++ tests/sentry/models/test_releaseenvironment.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from datetime import timedelta
 from django.utils import timezone
 
 from sentry.models import Environment, Release, ReleaseEnvironment
@@ -9,7 +10,7 @@ from sentry.testutils import TestCase
 class GetOrCreateTest(TestCase):
     def test_simple(self):
         project = self.create_project()
-        now = timezone.now()
+        datetime = timezone.now()
 
         release = Release.objects.create(
             project=project,
@@ -23,9 +24,34 @@ class GetOrCreateTest(TestCase):
             project=project,
             release=release,
             environment=env,
-            datetime=now,
+            datetime=datetime,
         )
 
         assert relenv.project_id == project.id
         assert relenv.release_id == release.id
         assert relenv.environment_id == env.id
+
+        datetime_new = datetime + timedelta(days=1)
+
+        relenv = ReleaseEnvironment.get_or_create(
+            project=project,
+            release=release,
+            environment=env,
+            datetime=datetime_new,
+        )
+
+        assert relenv.first_seen == datetime
+        assert relenv.last_seen == datetime_new
+
+        datetime_new2 = datetime_new + timedelta(seconds=1)
+
+        # this should not update immediately as the window is too close
+        relenv = ReleaseEnvironment.get_or_create(
+            project=project,
+            release=release,
+            environment=env,
+            datetime=datetime_new2,
+        )
+
+        assert relenv.first_seen == datetime
+        assert relenv.last_seen == datetime_new
