33952f8d95581426424d6ae4d5e27d476b9eb697
David Cramer <dcramer@gmail.com>
2016-09-15 09:27:58 -0700

    Improve react form abstractions (#4139)
    - Add GenericField which renders standard field config abstraction
    - Replace CheckboxField with BooleanField
    - Add email field
    - Add range field
    - Add number field
    - Move form config abstraction into utility

--- src/sentry/plugins/config.py
+++ src/sentry/plugins/config.py
@@ -2,9 +2,7 @@ from __future__ import absolute_import
 
 __all__ = ['PluginConfigMixin']
 
-import six
-
-from django import forms
+from sentry.utils.forms import form_to_config
 
 from .validators import DEFAULT_VALIDATORS
 
@@ -12,28 +10,6 @@ from .validators import DEFAULT_VALIDATORS
 class PluginConfigMixin(object):
     asset_key = None
     assets = []
-
-    def field_to_config(self, name, field):
-        config = {
-            'name': name,
-            'label': field.label or name.replace('_', ' ').title(),
-            'placeholder': field.widget.attrs.get('placeholder'),
-            'help': field.help_text,
-            'required': field.required,
-        }
-        if isinstance(field, forms.URLField):
-            config['type'] = 'url'
-        elif isinstance(field, forms.CharField):
-            if isinstance(field.widget, forms.PasswordInput):
-                config['type'] = 'secret'
-            elif isinstance(field.widget, forms.Textarea):
-                config['type'] = 'textarea'
-            else:
-                config['type'] = 'text'
-        elif isinstance(field, forms.ChoiceField):
-            config['type'] = 'select'
-            config['choices'] = field.choices
-        return config
 
     def get_assets(self):
         return self.assets
@@ -52,12 +28,7 @@ class PluginConfigMixin(object):
         if not form:
             return []
 
-        config = []
-        for name, field in six.iteritems(form.base_fields):
-            row = self.field_to_config(name, field)
-            row['default'] = field.initial
-            config.append(row)
-        return config
+        return form_to_config(form)
 
     def validate_config_field(self, project, name, value, actor=None):
         """
