12c22bbd38fe8b3a2d2953f3734e37bbd4411201
David Cramer <dcramer@gmail.com>
2016-09-19 12:52:59 -0700

    Make SS scrubbing less greedy

--- src/sentry/utils/data_scrubber.py
+++ src/sentry/utils/data_scrubber.py
@@ -52,7 +52,7 @@ class SensitiveDataFilter(object):
         # various private/public keys
         r'-----BEGIN[A-Z ]+(PRIVATE|PUBLIC) KEY-----.+-----END[A-Z ]+(PRIVATE|PUBLIC) KEY-----',
         # social security numbers (US)
-        r'^\b\d{3}-?\d{2}-?\d{4}\b',
+        r'^\b(?!(000|666|9))\d{3}-(?!00)\d{2}-(?!0000)\d{4}\b',
     ]), re.DOTALL)
     URL_PASSWORD_RE = re.compile(r'\b((?:[a-z0-9]+:)?//[^:]+:)([^@]+)@')
 
