938c0c8cd2ada043aa9e6408b3435927f98fb7c5
maxbittker <maxbittker@gmail.com>
2016-09-23 14:58:39 -0700

    rough ui version

--- src/sentry/static/sentry/app/components/events/interfaces/stacktraceContent.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/stacktraceContent.jsx
@@ -66,9 +66,16 @@ const StacktraceContent = React.createClass({
 
     let expandFirstFrame = this.props.expandFirstFrame;
     let frames = [];
+    let nRepeats = 0;
     data.frames.forEach((frame, frameIdx) => {
       let nextFrame = data.frames[frameIdx + 1];
-      if (this.frameIsVisible(frame, nextFrame)) {
+      let repeatedFrame = nextFrame && frame.lineNo == nextFrame.lineNo && frame.function == nextFrame.function;
+      if(repeatedFrame){
+        nRepeats++;
+      }
+      debugger
+      if (this.frameIsVisible(frame, nextFrame)
+          && !repeatedFrame ){
         frames.push(
           <Frame
             key={frameIdx}
@@ -77,8 +84,12 @@ const StacktraceContent = React.createClass({
             emptySourceNotation={lastFrameIdx === frameIdx && frameIdx === 0}
             isOnlyFrame={this.props.data.frames.length === 1}
             nextFrameInApp={nextFrame && nextFrame.inApp}
-            platform={this.props.platform} />
+            platform={this.props.platform}
+            timesRepeated={nRepeats}/>
         );
+      }
+      if(!repeatedFrame){
+        nRepeats = 0;
       }
       if (frameIdx === firstFrameOmitted) {
         frames.push(this.renderOmittedFrames(
