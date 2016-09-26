b1634589aeba565b63280888b3f1ee8698cd353e
Armin Ronacher <armin.ronacher@active-4.com>
2016-09-26 17:51:47 +0200

    Temporarily undo the address normalization because of limitations

--- src/sentry/interfaces/stacktrace.py
+++ src/sentry/interfaces/stacktrace.py
@@ -56,7 +56,10 @@ def to_hex_addr(addr):
         return '0x%x' % addr
     elif isinstance(addr, six.string_types):
         if addr[:2] == '0x':
-            addr = int(addr[2:], 16)
+            # XXX: More correct would be this but we currently can't do
+            # that yet.
+            # addr = int(addr[2:], 16)
+            return addr
         return '0x%x' % int(addr)
     raise ValueError('Unsupported address format %r' % (addr,))
 
