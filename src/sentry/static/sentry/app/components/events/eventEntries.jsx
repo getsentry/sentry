b2e1d981cb80a90c2d0810ab5d9ca00003497d19
ckj <chriskjennings@gmail.com>
2016-09-28 16:00:53 -0700

    Polish event-level alerts

--- src/sentry/static/sentry/app/components/events/eventEntries.jsx
+++ src/sentry/static/sentry/app/components/events/eventEntries.jsx
@@ -117,10 +117,11 @@ const EventEntries = React.createClass({
         }
         {!utils.objectIsEmpty(evt.sdk) && evt.sdk.upstream.isNewer &&
           <div className="alert-block alert-info box">
+            <span className="icon-exclamation"/>
             {t('This event was reported with an old version of the %s SDK.', evt.platform)}
             {evt.sdk.upstream.url &&
               <a href={evt.sdk.upstream.url}
-                 style={{marginLeft: 10}}>{t('Learn More')}</a>
+                 className="btn btn-sm btn-default">{t('Learn More')}</a>
             }
           </div>
         }
