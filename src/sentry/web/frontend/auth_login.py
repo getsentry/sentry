209396e9ae9cf658a5c8d741a67f789ab6d06c71
David Cramer <dcramer@gmail.com>
2016-10-03 11:28:52 -0700

    Improve accept invite flow
    - Show a message if you're an existing member (and allow logout)
    - Add next param to login/register urls

--- src/sentry/web/frontend/auth_login.py
+++ src/sentry/web/frontend/auth_login.py
@@ -125,11 +125,7 @@ class AuthLoginView(BaseView):
     @never_cache
     @transaction.atomic
     def handle(self, request):
-        redirect_next = request.GET.get('next', None)
-        if redirect_next:
-            # TODO: enforce max redirect_next limit? (potential for abuse?)
-            request.session['_next'] = redirect_next
-
+        auth.initiate_login(request, request.GET.get('next', None))
         if request.user.is_authenticated():
             return self.redirect_to_org(request)
 
