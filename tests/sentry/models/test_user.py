6a6a3350454f20b2bf10bb837086aab6a94a18ed
David Cramer <dcramer@gmail.com>
2016-10-04 14:34:30 -0700

    [merge] add UserEmail to merged objects

--- tests/sentry/models/test_user.py
+++ tests/sentry/models/test_user.py
@@ -1,16 +1,38 @@
 from __future__ import absolute_import
 
-from sentry.models import OrganizationMember
+from sentry.models import OrganizationMember, UserEmail
 from sentry.testutils import TestCase
 
 
 class UserMergeToTest(TestCase):
     def test_simple(self):
         from_user = self.create_user('foo@example.com')
-
+        from_email = UserEmail.objects.create(
+            user=from_user,
+            email='foo@example.com',
+            is_verified=True
+        )
         to_user = self.create_user('bar@example.com')
-
+        to_email = UserEmail.objects.create(
+            user=from_user,
+            email='foo@example.com',
+            is_verified=True,
+        )
         from_user.merge_to(to_user)
+
+        assert UserEmail.objects.filter(
+            id=to_email.id,
+            user=to_user,
+            email=to_email.email,
+            is_verified=True,
+        ).exists()
+
+        assert UserEmail.objects.filter(
+            id=from_email.id,
+            user=to_user,
+            email=from_email.email,
+            is_verified=True,
+        ).exists()
 
     def test_duplicate_memberships(self):
         from_user = self.create_user('foo@example.com')
