8b7f6cfc9eaff7ca6289e7fc5c3e1c028e85b3d8
David Cramer <dcramer@gmail.com>
2016-10-05 12:59:20 -0700

    [emails] fix re-setting of verification hash

---
+++ tests/sentry/models/test_useremail.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+from sentry.models import UserEmail
+from sentry.testutils import TestCase
+
+
+class UserEmailTest(TestCase):
+    def test_hash_does_not_reset(self):
+        user = self.create_user('foo@example.com')
+        email = UserEmail.objects.get_or_create(user=user)[0]
+        email2 = UserEmail.objects.get(id=email.id)
+
+        assert email.validation_hash
+        assert email.validation_hash == email2.validation_hash
