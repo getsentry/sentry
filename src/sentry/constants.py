e436b66d2a48979b48136f317457e0575138d9a0
David Cramer <dcramer@gmail.com>
2016-10-06 23:23:06 -0700

    [sudo] Fix behavior with unusable passwords (#4299)

--- src/sentry/constants.py
+++ src/sentry/constants.py
@@ -58,10 +58,6 @@ STATUS_CHOICES = {
     'muted': STATUS_MUTED,
 }
 
-# A list of values which represent an unset or empty password on
-# a User instance.
-EMPTY_PASSWORD_VALUES = ('!', '', '$')
-
 # Normalize counts to the 15 minute marker. This value MUST be less than 60. A
 # value of 0 would store counts for every minute, and is the lowest level of
 # accuracy provided.
