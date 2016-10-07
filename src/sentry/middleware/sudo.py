e36a91cac63663c3add04c7fa9d780ba9834e191
David Cramer <dcramer@gmail.com>
2016-10-06 23:23:06 -0700

    [sudo] Fix behavior with unusable passwords (#4299)

--- src/sentry/middleware/sudo.py
+++ src/sentry/middleware/sudo.py
@@ -9,14 +9,12 @@ from __future__ import absolute_import
 
 from sudo.middleware import SudoMiddleware as BaseSudoMiddleware
 
-from sentry.constants import EMPTY_PASSWORD_VALUES
-
 
 class SudoMiddleware(BaseSudoMiddleware):
     def has_sudo_privileges(self, request):
         # Users without a password are assumed to always have sudo powers
         user = request.user
-        if user.is_authenticated() and user.password in EMPTY_PASSWORD_VALUES:
+        if user.is_authenticated() and not user.has_usable_password():
             return True
 
         return super(SudoMiddleware, self).has_sudo_privileges(request)
