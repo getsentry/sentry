46c80d676dee26674fea4f0e94fe27b65727c031
David Cramer <dcramer@gmail.com>
2016-10-10 10:30:30 -0700

    Initial support for commit storage (#4209)
    - Add Repository model
    - Add Commit model
    - Add CommitAuthor model
    - Add ReleaseCommit model
    - Add ReleaseHook.set_commits API

---
+++ src/sentry/models/commit.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import, print_function
+
+from django.db import models
+from django.utils import timezone
+
+from sentry.db.models import (
+    BoundedPositiveIntegerField, FlexibleForeignKey, Model, sane_repr
+)
+
+
+class Commit(Model):
+    __core__ = False
+
+    organization_id = BoundedPositiveIntegerField(db_index=True)
+    repository_id = BoundedPositiveIntegerField()
+    key = models.CharField(max_length=64)
+    date_added = models.DateTimeField(default=timezone.now)
+    # all commit metadata must be optional, as it may not be available
+    # when the initial commit object is referenced (and thus created)
+    author = FlexibleForeignKey('sentry.CommitAuthor', null=True)
+    message = models.TextField(null=True)
+
+    class Meta:
+        app_label = 'sentry'
+        db_table = 'sentry_commit'
+        unique_together = (('repository_id', 'key'),)
+
+    __repr__ = sane_repr('organization_id', 'repository_id', 'key')
