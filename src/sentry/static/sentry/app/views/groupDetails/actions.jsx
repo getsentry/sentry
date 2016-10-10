c5d2fbbe7a3ddf65dff4875f53c5482440812070
Katie Lundsgaard <katie@getsentry.com>
2016-10-10 16:26:22 -0700

    Add translation tag
    /cc @getsentry/team

--- src/sentry/static/sentry/app/views/groupDetails/actions.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/actions.jsx
@@ -100,6 +100,7 @@ const GroupActions = React.createClass({
 
     let hasRelease = defined(group.lastRelease);
     let releaseTrackingUrl = '/' + this.getOrganization().slug + '/' + this.getProject().slug + '/settings/release-tracking/';
+    let linkTrackerText = t('Link Issue Tracker');
 
     return (
       <div className="group-actions">
@@ -230,7 +231,7 @@ const GroupActions = React.createClass({
         {!group.pluginIssues.length &&
           <a href={`/${this.getOrganization().slug}/${this.getProject().slug}/settings/issue-tracking/`}
              className={'btn btn-default btn-sm btn-config-issue-tracking'}>
-            Link Issue Tracker
+            {linkTrackerText}
           </a>
         }
       </div>
