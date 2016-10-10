22265edb2fa29b3c681436c805dd21c15ea71a90
David Cramer <dcramer@gmail.com>
2016-10-10 15:50:22 -0700

    [vcs] expand commits apis
    - add release commits list endpoint
    - support creating commits via attribute to webhook (and thus release details)
    - make reposting of release details acceptable
    - dont set date_released by default

--- tests/sentry/api/endpoints/test_release_details.py
+++ tests/sentry/api/endpoints/test_release_details.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from django.core.urlresolvers import reverse
 
-from sentry.models import File, Release, ReleaseFile
+from sentry.models import File, Release, ReleaseCommit, ReleaseFile
 from sentry.testutils import APITestCase
 
 
@@ -49,6 +49,35 @@ class UpdateReleaseDetailsTest(APITestCase):
 
         release = Release.objects.get(id=release.id)
         assert release.ref == 'master'
+
+    def test_commits(self):
+        self.login_as(user=self.user)
+
+        project = self.create_project(name='foo')
+
+        release = Release.objects.create(
+            project=project,
+            version='1',
+        )
+
+        url = reverse('sentry-api-0-release-details', kwargs={
+            'organization_slug': project.organization.slug,
+            'project_slug': project.slug,
+            'version': release.version,
+        })
+        response = self.client.put(url, data={
+            'commits': [
+                {'id': 'a' * 40},
+                {'id': 'b' * 40},
+            ]
+        })
+
+        assert response.status_code == 200, (response.status_code, response.content)
+
+        rc_list = list(ReleaseCommit.objects.filter(
+            release=release,
+        ).select_related('commit', 'commit__author').order_by('order'))
+        assert len(rc_list) == 2
 
 
 class ReleaseDeleteTest(APITestCase):
