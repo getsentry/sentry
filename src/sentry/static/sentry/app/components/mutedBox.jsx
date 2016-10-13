26089cb76672f2a149a6c782c7ba5ece9ec1ab5f
ckj <chriskjennings@gmail.com>
2016-10-12 17:17:46 -0700

    Clean up issue status bar

--- src/sentry/static/sentry/app/components/mutedBox.jsx
+++ src/sentry/static/sentry/app/components/mutedBox.jsx
@@ -14,16 +14,19 @@ const MutedBox = React.createClass({
   render() {
     let statusDetails = this.props.statusDetails;
     return (
-      <div className="alert alert-info alert-block">
-        {statusDetails.ignoreUntil ?
-          <span>{t(
-            'This issue has been ignored until %s',
-            <strong><DateTime date={statusDetails.ignoreUntil} /></strong>
-          )} &mdash; </span>
-        :
-          <span>{t('This issue has been ignored')} &mdash; </span>
-        }
-        {t('You will not be notified of any changes and it will not show up by default in feeds.')}
+      <div className="box">
+        <span className="icon icon-soundoff" />
+        <p>
+          {statusDetails.ignoreUntil ?
+            <span>{t(
+              'This issue has been ignored until %s',
+              <strong><DateTime date={statusDetails.ignoreUntil} /></strong>
+            )} &mdash; </span>
+          :
+            <span>{t('This issue has been ignored')} &mdash; </span>
+          }
+          {t('You will not be notified of any changes and it will not show up by default in feeds.')}
+        </p>
       </div>
     );
   }
