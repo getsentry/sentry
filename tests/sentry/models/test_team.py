202b8158af96aeea924d020a76c945c0d56ef3ab
David Cramer <dcramer@gmail.com>
2016-10-17 12:51:33 -0700

    [admin] Add Team.transfer_to helper
    This takes the change organization logic currently present in /admin/ and moves it into a utility function. It also adds handling of cases where a duplicate team already exists in the transfer org.

--- tests/sentry/models/test_team.py
+++ tests/sentry/models/test_team.py
@@ -2,7 +2,9 @@
 
 from __future__ import absolute_import
 
-from sentry.models import OrganizationMember, OrganizationMemberTeam
+from sentry.models import (
+    OrganizationMember, OrganizationMemberTeam, Project, Team
+)
 from sentry.testutils import TestCase
 
 
@@ -59,3 +61,73 @@ class TeamTest(TestCase):
         )
 
         assert member not in team.member_set.all()
+
+
+class TransferTest(TestCase):
+    def test_simple(self):
+        user = self.create_user()
+        org = self.create_organization(name='foo', owner=user)
+        org2 = self.create_organization(name='bar', owner=None)
+        team = self.create_team(organization=org)
+        project = self.create_project(team=team)
+        user2 = self.create_user('foo@example.com')
+        self.create_member(
+            user=user2,
+            organization=org,
+            role='admin',
+            teams=[team],
+        )
+        self.create_member(
+            user=user2,
+            organization=org2,
+            role='member',
+            teams=[],
+        )
+        team.transfer_to(org2)
+
+        assert team.organization == org2
+        team = Team.objects.get(id=team.id)
+        assert team.organization == org2
+
+        project = Project.objects.get(id=project.id)
+        assert project.organization == org2
+
+        # owner does not exist on new org, so should not be transferred
+        assert not OrganizationMember.objects.filter(
+            user=user,
+            organization=org2,
+        ).exists()
+
+        # existing member should now have access
+        member = OrganizationMember.objects.get(
+            user=user2,
+            organization=org2,
+        )
+        assert list(member.teams.all()) == [team]
+        # role should not automatically upgrade
+        assert member.role == 'member'
+
+        # old member row should still exist
+        assert OrganizationMember.objects.filter(
+            user=user2,
+            organization=org,
+        ).exists()
+
+        # no references to old org for this team should exist
+        assert not OrganizationMemberTeam.objects.filter(
+            organizationmember__organization=org,
+            team=team,
+        ).exists()
+
+    def test_existing_team(self):
+        org = self.create_organization(name='foo')
+        org2 = self.create_organization(name='bar')
+        team = self.create_team(name='foo', organization=org)
+        team2 = self.create_team(name='foo', organization=org2)
+        project = self.create_project(team=team)
+        team.transfer_to(org2)
+
+        project = Project.objects.get(id=project.id)
+        assert project.team == team2
+
+        assert not Team.objects.filter(id=team.id).exists()
