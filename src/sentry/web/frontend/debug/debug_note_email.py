1fc543d847160c5e829dc2192e37f452d6c201e4
ted kaemming <ted@kaemming.com>
2016-10-17 14:45:49 -0700

    Fix vertical alignment of avatar column in note email. (#4377)
    Also makes debug message view for note email random text.

--- src/sentry/web/frontend/debug/debug_note_email.py
+++ src/sentry/web/frontend/debug/debug_note_email.py
@@ -2,15 +2,22 @@ from __future__ import absolute_import
 
 from sentry.models import Activity
 
-from .mail import ActivityMailDebugView
+from .mail import ActivityMailDebugView, get_random, make_message
 
 
 class DebugNoteEmailView(ActivityMailDebugView):
     def get_activity(self, request, event):
+        random = get_random(request)
         return {
             'type': Activity.NOTE,
             'user': request.user,
             'data': {
-                'text': 'This is an example note!',
+                'text': make_message(
+                    random,
+                    max(
+                        2,
+                        int(random.weibullvariate(12, 0.4)),
+                    ),
+                )
             },
         }
