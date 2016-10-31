7e99aba02ecfb1cfb92b9ddc7d60a134a58487dd
Jess MacQueen <jess@getsentry.com>
2016-10-31 11:23:57 -0700

    override refresh_token in asana backend

--- src/social_auth/backends/asana.py
+++ src/social_auth/backends/asana.py
@@ -73,6 +73,12 @@ class AsanaAuth(BaseOAuth2):
         return self.do_auth(response['access_token'], response=response,
                             *args, **kwargs)
 
+    @classmethod
+    def refresh_token(cls, token):
+        params = cls.refresh_token_params(token)
+        response = requests.post(cls.ACCESS_TOKEN_URL, data=params,
+                                 headers=cls.auth_headers())
+        return response.json()
 
 # Backend definition
 BACKENDS = {
