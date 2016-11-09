9a63e2e3b77a5418e3bf748114215d671d7bb81f
Jess MacQueen <macqueen@users.noreply.github.com>
2016-11-09 13:30:53 -0800

    add editing/not editing state to PasswordField component (#4492)
    * make secret fields indicate more obviously that a saved value exists
    
    * style tweaks
    
    * remove inline styles
    
    * changes
    
    * make sure has_saved_value is a bool
    
    * consistently show 20 characters

--- src/sentry/api/serializers/models/plugin.py
+++ src/sentry/api/serializers/models/plugin.py
@@ -61,5 +61,8 @@ def serialize_field(project, plugin, field):
     }
     if field.get('type') != 'secret':
         data['value'] = plugin.get_option(field['name'], project)
+    else:
+        data['has_saved_value'] = bool(field.get('has_saved_value', False))
+        data['prefix'] = field.get('prefix', '')
 
     return data
