bc029796873fe8450212e0927a758ddd3093cf62
David Cramer <dcramer@gmail.com>
2016-11-22 16:48:10 -0800

    [tags] Improve handling of values (#4589)
    - Add export to csv action in UI
    - Remove uniqueValues reference (not issue specific)
    - Augment data when user tag is selected
    - Add email action for users

--- src/sentry/api/serializers/models/tagvalue.py
+++ src/sentry/api/serializers/models/tagvalue.py
@@ -1,41 +1,28 @@
 from __future__ import absolute_import
 
-import operator
 import six
 
-from django.db.models import Q
-from six.moves import reduce
-
-from sentry.api.serializers import Serializer, register
+from sentry.api.serializers import Serializer, register, serialize
 from sentry.models import EventUser, TagKey, TagValue
-
-
-def parse_user_tag(value):
-    lookup, value = value.split(':', 1)
-    if lookup == 'id':
-        lookup = 'ident'
-    elif lookup == 'ip':
-        lookup = 'ip_address'
-    return {lookup: value}
 
 
 @register(TagValue)
 class TagValueSerializer(Serializer):
     def get_attrs(self, item_list, user):
-        user_lookups = [
-            Q(**parse_user_tag(i.value))
+        user_tags = [
+            i.value
             for i in item_list
             if i.key == 'sentry:user'
         ]
 
         tag_labels = {}
-        if user_lookups:
+        if user_tags:
             tag_labels.update({
-                ('sentry:user', euser.tag_value): euser.get_label()
-                for euser in EventUser.objects.filter(
-                    reduce(operator.or_, user_lookups),
-                    project=item_list[0].project,
-                )
+                ('sentry:user', k): v.get_label()
+                for k, v in six.iteritems(EventUser.for_tags(
+                    project_id=item_list[0].project_id,
+                    values=user_tags,
+                ))
             })
 
         result = {}
@@ -67,3 +54,33 @@ class EnvironmentTagValueSerializer(Serializer):
             'id': six.text_type(obj.id),
             'name': obj.value,
         }
+
+
+class UserTagValueSerializer(Serializer):
+    def get_attrs(self, item_list, user):
+        users = EventUser.for_tags(
+            project_id=item_list[0].project_id,
+            values=[t.value for t in item_list],
+        )
+
+        result = {}
+        for item in item_list:
+            result[item] = {
+                'user': users.get(item.value),
+            }
+        return result
+
+    def serialize(self, obj, attrs, user):
+        if not attrs['user']:
+            result = {
+                'id': None,
+            }
+        else:
+            result = serialize(attrs['user'], user)
+        result.update({
+            'value': obj.value,
+            'count': obj.times_seen,
+            'lastSeen': obj.last_seen,
+            'firstSeen': obj.first_seen,
+        })
+        return result
