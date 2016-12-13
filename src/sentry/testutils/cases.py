762966c8f22c63aae3ab439b1d1179163c1eb3cb
Matt Robenolt <matt@ydekproductions.com>
2016-12-13 10:33:25 -0800

    Revert "[tests] install pytest-responses (#4661)"
    This reverts commit ee6b3d8bb6a188d628f50c3593aadf69bd0a1550.

--- src/sentry/testutils/cases.py
+++ src/sentry/testutils/cases.py
@@ -448,7 +448,6 @@ class CliTestCase(TestCase):
 
 
 @pytest.mark.usefixtures('browser')
-@pytest.mark.withoutresponses
 class AcceptanceTestCase(TransactionTestCase):
     def save_session(self):
         self.session.save()
