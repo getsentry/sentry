4245ee3e313e2c4b496f0dc28fa2e1c21c13678b
David Cramer <dcramer@gmail.com>
2016-12-14 13:05:29 -0800

    [privacy] hide sdk.clientIP from non superusers (#4674)

--- src/sentry/static/sentry/app/components/events/sdk.jsx
+++ src/sentry/static/sentry/app/components/events/sdk.jsx
@@ -31,10 +31,12 @@ const EventSdk = React.createClass({
               <td className="key">Version</td>
               <td className="value"><pre>{data.version}</pre></td>
             </tr>
-            <tr key="clientIP">
-              <td className="key">Client IP</td>
-              <td className="value"><pre>{data.clientIP || 'n/a'}</pre></td>
-            </tr>
+            {data.clientIP &&
+              <tr key="clientIP">
+                <td className="key">Client IP</td>
+                <td className="value"><pre>{data.clientIP}</pre></td>
+              </tr>
+            }
           </tbody>
         </table>
       </GroupEventDataSection>
