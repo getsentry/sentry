e7124a9a59ee7c9d99a80f18a9d6fb03001bf21c
David Cramer <dcramer@gmail.com>
2017-01-09 14:59:31 -0800

    [quotas] configurable rate limits (#4732)
    * [quotas] configurable rate limits
    
    - add per-hour account (org) limits
    - add configurable windows to quota implementations
    - remove deprecated project quotas
    
    * remove useless check
    
    * invalid comment
    
    * fix 1hr window

--- src/sentry/static/sentry/app/components/forms/numberField.jsx
+++ src/sentry/static/sentry/app/components/forms/numberField.jsx
@@ -4,4 +4,11 @@ export default class NumberField extends InputField {
   getType() {
     return 'number';
   }
+
+  getAttributes() {
+    return {
+        min: this.props.min || undefined,
+        max: this.props.max || undefined,
+    };
+  }
 }
