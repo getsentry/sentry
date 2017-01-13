f51a1feab41655dd347b45fd229402b0cb9ef7d8
Mitchell Grice <mitch@gricey.net>
2017-01-13 12:14:54 +1000

    Change localhost filter to also look at the url
    If the URL is available and is something that is obviously local to the user, it should be filtered out by this filter.
    Discussion is at #4729
    
    I haven't tested this code, getting a local sentry development going seems to be a huge endeavour. Might try tonight after work and see how I go. In the mean time I'd appreciate some code review or if someone who already is set up to test it could give it a run.

--- src/sentry/filters/localhost.py
+++ src/sentry/filters/localhost.py
@@ -1,20 +1,37 @@
 from __future__ import absolute_import
 
+import re
+
 from .base import Filter
 
 LOCAL_IPS = frozenset(['127.0.0.1', '::1'])
+LOCAL_DOMAINS = frozenset(['127.0.0.1', 'localhost'])
+DOMAIN_FROM_URL = re.compile(r'.*?\:\/\/?([^\/\?#:]+).*')
 
 
 class LocalhostFilter(Filter):
     id = 'localhost'
     name = 'Filter out errors coming from localhost'
-    description = 'This applies to to both IPv4 (``127.0.0.1``) and IPv6 (``::1``) addresses.'
+    description = 'This applies to to both IPv4 (``127.0.0.1``) and IPv6 (``::1``) addresses as well as ``127.0.0.1`` and ``localhost`` domains.'
 
     def get_ip_address(self, data):
         try:
             return data['sentry.interfaces.User']['ip_address']
         except KeyError:
             return ''
+    
+    def get_url(self, data):
+        try:
+            http = data['sentry.interfaces.Http']
+            if http:
+                return ['url']
+            return ''
+        except KeyError:
+            return ''
+    
+    def get_domain(self, data):
+        matchObj = DOMAIN_FROM_URL.match(self.get_url(data))
+        return matchObj.group(1) or ''
 
     def test(self, data):
-        return self.get_ip_address(data) in LOCAL_IPS
+        return self.get_ip_address(data) in LOCAL_IPS or self.get_domain(data) in LOCAL_DOMAINS
