5e844090db63f61b7447a15a5fcae22551943310
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-01-17 12:32:40 +0100

    Add memory and Storage info for Apple Devices (#4772)
    * Format memory and storage for ios devices
    
    * Add CHANGES entry

--- src/sentry/static/sentry/app/components/fileSize.jsx
+++ src/sentry/static/sentry/app/components/fileSize.jsx
@@ -1,29 +1,14 @@
 import React from 'react';
+import {formatBytes} from '../utils';
 
 const FileSize = React.createClass({
   propTypes: {
     bytes: React.PropTypes.number.isRequired
   },
 
-  units: ['KB','MB','GB','TB','PB','EB','ZB','YB'],
-
-  formatBytes: function(bytes) {
-      let thresh = 1024;
-      if (bytes < thresh) {
-        return bytes + ' B';
-      }
-
-      let u = -1;
-      do {
-        bytes /= thresh;
-        ++u;
-      } while (bytes >= thresh);
-      return bytes.toFixed(1) + ' ' + this.units[u];
-  },
-
   render: function() {
     return (
-      <span>{this.formatBytes(this.props.bytes)}</span>
+      <span>{formatBytes(this.props.bytes)}</span>
     );
   }
 });
