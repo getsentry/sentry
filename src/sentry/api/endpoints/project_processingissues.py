a5a3aefce8cbe9d2d09812e91a8bb89305ddc0ad
Armin Ronacher <armin.ronacher@active-4.com>
2017-01-17 11:02:07 +0100

    Remove initial reprocessing storage support

--- src/sentry/api/endpoints/project_processingissues.py
+++ src/sentry/api/endpoints/project_processingissues.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 
 from sentry.api.bases.project import ProjectEndpoint
 from sentry.api.serializers import serialize
-from sentry.models import ProcessingIssue, ProcessingIssueGroup
 
 
 class ProjectProcessingIssuesEndpoint(ProjectEndpoint):
@@ -12,27 +11,14 @@ class ProjectProcessingIssuesEndpoint(ProjectEndpoint):
         """
         List a project's processing issues.
         """
-        num_issues = ProcessingIssue.objects.filter(
-            project=project
-        ).count()
-        num_groups = ProcessingIssueGroup.objects.filter(
-            issue__project=project
-        ).values('group').distinct().count()
-        num_releases = ProcessingIssueGroup.objects.filter(
-            issue__project=project
-        ).values('release').distinct().count()
-
         data = {
-            'hasIssues': num_issues > 0 and num_groups > 0,
-            'affectedIssues': num_issues,
-            'affectedGroups': num_groups,
-            'affectedReleases': num_releases,
+            'hasIssues': False,
+            'affectedIssues': 0,
+            'affectedGroups': 0,
+            'affectedReleases': 0,
         }
 
         if request.GET.get('detailed') == '1':
-            q = ProcessingIssue.objects.with_num_groups().filter(
-                project=project
-            ).order_by('type')
-            data['issues'] = [serialize(x, request.user) for x in q]
+            data['issues'] = []
 
         return Response(serialize(data, request.user))
