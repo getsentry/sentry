0e277c3b34e8788e4b49f16b0d985f114b4d01e4
David Cramer <dcramer@gmail.com>
2017-01-19 11:27:32 -0800

    [releases] move release-emails behind project feature (#4790)

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -16,7 +16,7 @@ default_manager.add('organizations:repos', OrganizationFeature)  # NOQA
 default_manager.add('organizations:release-commits', OrganizationFeature)  # NOQA
 default_manager.add('projects:global-events', ProjectFeature)  # NOQA
 default_manager.add('projects:plugins', ProjectPluginFeature)  # NOQA
-default_manager.add('workflow:release-emails')
+default_manager.add('workflow:release-emails', ProjectFeature)  # NOQA
 default_manager.add('projects:sample-events', ProjectFeature)  # NOQA
 
 # expose public api
