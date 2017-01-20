e79858250e253a0ab8e40a6b26bb80177e9ed80d
Katie Lundsgaard <katie@getsentry.com>
2017-01-20 10:46:18 -0800

    Add tooltip to commit authors

--- src/sentry/static/sentry/app/components/releaseStats.jsx
+++ src/sentry/static/sentry/app/components/releaseStats.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import Avatar from './avatar';
+import TooltipMixin from '../mixins/tooltip';
 
 const ReleaseStats = React.createClass({
   propTypes: {
     release: React.PropTypes.object,
   },
+
+  mixins: [
+    TooltipMixin({
+      selector: '.tip'
+    }),
+  ],
 
   render() {
     let release = this.props.release;
@@ -12,7 +19,12 @@ const ReleaseStats = React.createClass({
       <div className="release-info">
         <div><b>{release.commitCount} commits by {release.authors.length} authors</b></div>
         {release.authors.map(author => {
-          return <Avatar user={author}/>;
+          return (
+            <span className="assignee-selector tip"
+                 title={author.name + ' ' + author.email }>
+              <Avatar user={author}/>
+            </span>
+          );
         })}
       </div>
     );
