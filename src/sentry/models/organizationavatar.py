11a1280b8baeadd0ee343ba99840a338ab278c78
David Cramer <dcramer@gmail.com>
2017-01-26 11:30:31 -0800

    Add organization avatars (#4818)
    - Add OrganizationAvatar model
    - Refactor shared avatar components
    - Add avatar/avatarType to organization details endpoint

--- src/sentry/models/organizationavatar.py
+++ src/sentry/models/organizationavatar.py
@@ -2,33 +2,30 @@ from __future__ import absolute_import
 
 from django.db import models
 
-from sentry.db.models import BaseManager, FlexibleForeignKey
+from sentry.db.models import FlexibleForeignKey
 
 from . import AvatarBase
 
 
-class UserAvatar(AvatarBase):
+class OrganizationAvatar(AvatarBase):
     """
-    A UserAvatar associates a User with their avatar photo File
+    An OrganizationAvatar associates an Organization with their avatar photo File
     and contains their preferences for avatar type.
     """
 
     AVATAR_TYPES = (
         (0, 'letter_avatar'),
         (1, 'upload'),
-        (2, 'gravatar'),
     )
 
     FILE_TYPE = 'avatar.file'
 
-    user = FlexibleForeignKey('sentry.User', unique=True, related_name='avatar')
+    organization = FlexibleForeignKey('sentry.Organization', unique=True, related_name='avatar')
     avatar_type = models.PositiveSmallIntegerField(default=0, choices=AVATAR_TYPES)
-
-    objects = BaseManager(cache_fields=['user'])
 
     class Meta:
         app_label = 'sentry'
-        db_table = 'sentry_useravatar'
+        db_table = 'sentry_organizationavatar'
 
     def get_cache_key(self, size):
-        return 'avatar:%s:%s' % (self.user_id, size)
+        return 'org_avatar:%s:%s' % (self.organization_id, size)
