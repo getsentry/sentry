f6bd38259ac3ab4ccbcfea5e3a3aab4493c57ff3
Jess MacQueen <jessmacqueen@gmail.com>
2017-01-26 13:04:51 -0800

    have new_groups count only apply on a project level for less confusion (#4794)
    * have new_groups count only apply on a project level for less confusion
    
    * update tests
    
    * remove data migration from transaction
    
    * fix migrations and release serializer
    
    * wip - get buffer incrementing nulls for postgres
    
    * fix a few of the tests that relied on ScoreClause
    
    * set null new_groups in release project migration to 0
    
    * revert buffer changes
    
    * update changes
    
    * fix buffer test for mysql
    
    * regenerate migration
    
    * regenerate migration, add optimizations

--- src/sentry/api/serializers/models/release.py
+++ src/sentry/api/serializers/models/release.py
@@ -2,13 +2,15 @@ from __future__ import absolute_import
 
 import six
 
+from django.db.models import Sum
+
 from sentry.api.serializers import Serializer, register, serialize
 from sentry.models import Release, ReleaseProject, TagValue
 
 
 @register(Release)
 class ReleaseSerializer(Serializer):
-    def get_attrs(self, item_list, user):
+    def get_attrs(self, item_list, user, *args, **kwargs):
         tags = {
             tk.value: tk
             for tk in TagValue.objects.filter(
@@ -24,15 +26,31 @@ class ReleaseSerializer(Serializer):
             for d in serialize(set(i.owner for i in item_list if i.owner_id), user)
         }
 
+        if kwargs.get('project'):
+            group_counts_by_release = dict(ReleaseProject.objects.filter(
+                project=kwargs.get('project'),
+                release__in=item_list
+            ).values_list('release_id', 'new_groups'))
+        else:
+            # assume it should be a sum across release
+            # if no particular project specified
+            group_counts_by_release = dict(
+                ReleaseProject.objects.filter(release__in=item_list, new_groups__isnull=False)
+                                      .values('release_id')
+                                      .annotate(new_groups=Sum('new_groups'))
+                                      .values_list('release_id', 'new_groups')
+            )
+
         result = {}
         for item in item_list:
             result[item] = {
                 'tag': tags.get(item.version),
                 'owner': owners[six.text_type(item.owner_id)] if item.owner_id else None,
+                'new_groups': group_counts_by_release.get(item.id) or 0
             }
         return result
 
-    def serialize(self, obj, attrs, user):
+    def serialize(self, obj, attrs, user, *args, **kwargs):
         d = {
             'version': obj.version,
             'shortVersion': obj.short_version,
@@ -42,7 +60,7 @@ class ReleaseSerializer(Serializer):
             'dateReleased': obj.date_released,
             'dateCreated': obj.date_added,
             'data': obj.data,
-            'newGroups': obj.new_groups,
+            'newGroups': attrs['new_groups'],
             'owner': attrs['owner'],
         }
         if attrs['tag']:
