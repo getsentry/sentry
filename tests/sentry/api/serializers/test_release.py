ae9051ffcfbf635f60ca6ca6bbf7fad996106f58
Katie Lundsgaard <katie@getsentry.com>
2017-01-27 11:06:44 -0800

    Release stats (#4816)

--- tests/sentry/api/serializers/test_release.py
+++ tests/sentry/api/serializers/test_release.py
@@ -6,7 +6,8 @@ from django.utils import timezone
 from uuid import uuid4
 
 from sentry.api.serializers import serialize
-from sentry.models import Release, ReleaseProject, TagValue
+from sentry.models import (Commit, CommitAuthor,
+    Release, ReleaseCommit, ReleaseProject, TagValue, User, UserEmail,)
 from sentry.testutils import TestCase
 
 
@@ -37,6 +38,25 @@ class ReleaseSerializerTest(TestCase):
             last_seen=timezone.now(),
             times_seen=5,
         )
+        commit_author = CommitAuthor.objects.create(
+            name='stebe',
+            email='stebe@sentry.io',
+            organization_id=project.organization_id,
+        )
+        commit = Commit.objects.create(
+            organization_id=project.organization_id,
+            repository_id=1,
+            key='abc',
+            author=commit_author,
+            message='waddap',
+        )
+        ReleaseCommit.objects.create(
+            organization_id=project.organization_id,
+            project_id=project.id,
+            release=release,
+            commit=commit,
+            order=1,
+        )
 
         result = serialize(release, user)
         assert result['version'] == release.version
@@ -45,6 +65,8 @@ class ReleaseSerializerTest(TestCase):
         assert result['newGroups'] == 2
         assert result['firstEvent']
         assert result['lastEvent']
+        assert result['commitCount'] == 1
+        assert result['authors'] == [{'name': 'stebe', 'email': 'stebe@sentry.io'}]
 
         result = serialize(release, user, project=project)
         # should be groups from one project
@@ -63,8 +85,103 @@ class ReleaseSerializerTest(TestCase):
             version=uuid4().hex,
         )
         release.add_project(project)
+        commit_author = CommitAuthor.objects.create(
+            name='stebe',
+            email='stebe@sentry.io',
+            organization_id=project.organization_id,
+        )
+        commit = Commit.objects.create(
+            organization_id=project.organization_id,
+            repository_id=1,
+            key='abc',
+            author=commit_author,
+            message='waddap',
+        )
+        ReleaseCommit.objects.create(
+            organization_id=project.organization_id,
+            project_id=project.id,
+            release=release,
+            commit=commit,
+            order=1,
+        )
 
         result = serialize(release, user)
         assert result['version'] == release.version
         assert not result['firstEvent']
         assert not result['lastEvent']
+
+    def test_get_user_from_email(self):
+        user = User.objects.create(email='stebe@sentry.io')
+        UserEmail.get_primary_email(user=user)
+        project = self.create_project()
+        self.create_member(user=user, organization=project.organization)
+        release = Release.objects.create(
+            organization_id=project.organization_id,
+            version=uuid4().hex,
+            new_groups=1,
+        )
+        release.add_project(project)
+        commit_author = CommitAuthor.objects.create(
+            name='stebe',
+            email='stebe@sentry.io',
+            organization_id=project.organization_id,
+        )
+        commit = Commit.objects.create(
+            organization_id=project.organization_id,
+            repository_id=1,
+            key='abc',
+            author=commit_author,
+            message='waddap',
+        )
+        ReleaseCommit.objects.create(
+            organization_id=project.organization_id,
+            project_id=project.id,
+            release=release,
+            commit=commit,
+            order=1,
+        )
+
+        result = serialize(release, user)
+        result_author = result['authors'][0]
+        assert int(result_author['id']) == user.id
+        assert result_author['email'] == user.email
+        assert result_author['username'] == user.username
+
+    def test_get_single_user_from_email(self):
+        user = User.objects.create(email='stebe@sentry.io')
+        otheruser = User.objects.create(email='adifferentstebe@sentry.io')
+        UserEmail.objects.create(user=otheruser, email='stebe@sentry.io')
+        project = self.create_project()
+        self.create_member(user=user, organization=project.organization)
+        release = Release.objects.create(
+            organization_id=project.organization_id,
+            version=uuid4().hex,
+            new_groups=1,
+        )
+        release.add_project(project)
+        commit_author = CommitAuthor.objects.create(
+            name='stebe',
+            email='stebe@sentry.io',
+            organization_id=project.organization_id,
+        )
+        commit = Commit.objects.create(
+            organization_id=project.organization_id,
+            repository_id=1,
+            key='abc',
+            author=commit_author,
+            message='waddap',
+        )
+        ReleaseCommit.objects.create(
+            organization_id=project.organization_id,
+            project_id=project.id,
+            release=release,
+            commit=commit,
+            order=1,
+        )
+
+        result = serialize(release, user)
+        assert len(result['authors']) == 1
+        result_author = result['authors'][0]
+        assert int(result_author['id']) == user.id
+        assert result_author['email'] == user.email
+        assert result_author['username'] == user.username
