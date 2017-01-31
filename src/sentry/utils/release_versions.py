40ccee7342ca821d06a732ecaa4b796dce053d4a
Jess MacQueen <jess@getsentry.com>
2017-01-30 16:09:29 -0800

    add merge and update helper functions, write migration

--- src/sentry/utils/release_versions.py
+++ src/sentry/utils/release_versions.py
@@ -6,6 +6,8 @@ from __future__ import absolute_import
 
 import re
 
+from django.db import IntegrityError, transaction
+
 
 def is_full_sha(version):
     # sha1 or md5
@@ -44,3 +46,64 @@ def is_short_sha_and_date(version):
 def is_word_and_date(version):
     # release-2016-01-01
     return bool(re.match(r'([a-z]*)-(\d{4})-(\d{2})-(\d{2})', version))
+
+
+def merge(to_release, from_releases, sentry_models):
+    # WARNING: this script does not attempt to merge ReleaseFiles, so
+    # any release in the `from_releases` arg should not have ReleaseFiles
+    # The following models reference release:
+    # ReleaseCommit.release
+    # ReleaseEnvironment.release_id
+    # ReleaseProject.release
+    # GroupRelease.release_id
+    # GroupResolution.release
+    # Group.first_release
+    # ReleaseFile.release -- NOT handled
+
+    model_list = (
+        sentry_models.ReleaseCommit,
+        sentry_models.ReleaseEnvironment,
+        sentry_models.ReleaseProject,
+        sentry_models.GroupRelease,
+        sentry_models.GroupResolution
+    )
+    for release in from_releases:
+        for model in model_list:
+            if hasattr(model, 'release'):
+                update_kwargs = {'release': to_release}
+            else:
+                update_kwargs = {'release_id': to_release.id}
+            try:
+                with transaction.atomic():
+                    model.objects.filter(
+                        release_id=release.id
+                    ).update(**update_kwargs)
+            except IntegrityError:
+                for item in model.objects.filter(release_id=release.id):
+                    try:
+                        with transaction.atomic():
+                            model.objects.filter(
+                                id=item.id
+                            ).update(**update_kwargs)
+                    except IntegrityError:
+                        item.delete()
+
+        sentry_models.Group.objects.filter(
+            first_release=release
+        ).update(first_release=to_release)
+
+        release.delete()
+
+
+def update_version(release, sentry_models):
+    old_version = release.version
+    project_slug = release.projects.values_list('slug', flat=True)[0]
+    new_version = '%s-%s' % (project_slug, old_version)
+    sentry_models.Release.objects.filter(
+        id=release.id
+    ).update(version=new_version)
+    sentry_models.TagValue.objects.filter(
+        project__in=release.projects.all(),
+        key='sentry:release',
+        value=old_version
+    ).update(value=new_version)
