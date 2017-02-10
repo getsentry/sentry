a45bb19bbe1b2353bf790267f45ca0e3446e1a70
Jess MacQueen <jessmacqueen@gmail.com>
2017-02-10 12:58:38 -0800

    get rid of unneeded locks in release creation (#4894)

--- src/sentry/receivers/releases.py
+++ src/sentry/receivers/releases.py
@@ -5,12 +5,10 @@ import re
 from django.db import IntegrityError, transaction
 from django.db.models.signals import post_save
 
-from sentry.app import locks
 from sentry.models import (
     Activity, Commit, Group, GroupCommitResolution, Release, TagValue
 )
 from sentry.tasks.clear_expired_resolutions import clear_expired_resolutions
-from sentry.utils.retries import TimedRetryPolicy
 
 _fixes_re = re.compile(r'\bFixes\s+([A-Za-z0-9_-]+-[A-Z0-9]+)\b', re.I)
 
@@ -22,35 +20,23 @@ def ensure_release_exists(instance, created, **kwargs):
     if instance.data and instance.data.get('release_id'):
         return
 
-    affected = Release.objects.filter(
-        organization_id=instance.project.organization_id,
-        version=instance.value,
-        projects=instance.project
-    ).update(date_added=instance.first_seen)
-    if not affected:
-        release = Release.objects.filter(
+    try:
+        with transaction.atomic():
+            release = Release.objects.create(
+                organization_id=instance.project.organization_id,
+                version=instance.value,
+                date_added=instance.first_seen,
+            )
+    except IntegrityError:
+        release = Release.objects.get(
             organization_id=instance.project.organization_id,
-            version=instance.value
-        ).first()
-        if release:
-            release.update(date_added=instance.first_seen)
-        else:
-            lock_key = Release.get_lock_key(instance.project.organization_id, instance.value)
-            lock = locks.get(lock_key, duration=5)
-            with TimedRetryPolicy(10)(lock.acquire):
-                try:
-                    release = Release.objects.get(
-                        organization_id=instance.project.organization_id,
-                        version=instance.value,
-                    )
-                except Release.DoesNotExist:
-                    release = Release.objects.create(
-                        organization_id=instance.project.organization_id,
-                        version=instance.value,
-                        date_added=instance.first_seen,
-                    )
-                    instance.update(data={'release_id': release.id})
-        release.add_project(instance.project)
+            version=instance.value,
+        )
+        release.update(date_added=instance.first_seen)
+    else:
+        instance.update(data={'release_id': release.id})
+
+    release.add_project(instance.project)
 
 
 def resolve_group_resolutions(instance, created, **kwargs):
