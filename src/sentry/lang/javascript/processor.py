fe97b74a28822e0d8fe41911d50201c1c3d76e04
Armin Ronacher <armin.ronacher@active-4.com>
2017-02-15 11:12:25 +0100

    Fixed a bad signature on two methods

--- src/sentry/lang/javascript/processor.py
+++ src/sentry/lang/javascript/processor.py
@@ -647,7 +647,7 @@ class JavaScriptStacktraceProcessor(StacktraceProcessor):
             platform == 'javascript'
         )
 
-    def process_frame(self, processable_frame, processing_task):
+    def process_frame(self, processable_frame):
         frame = processable_frame.frame
         last_token = None
         token = None
