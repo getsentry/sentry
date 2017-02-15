810a02b9a8c5e45f264e2b03b05fb7bff45b786f
Jess MacQueen <jess@getsentry.com>
2017-02-15 14:09:43 -0800

    add organization_id and many-to-many project relation to Environment

--- src/sentry/models/environment.py
+++ src/sentry/models/environment.py
@@ -7,19 +7,33 @@ sentry.models.release
 """
 from __future__ import absolute_import, print_function
 
-from django.db import models
+from django.db import IntegrityError, models, transaction
 from django.utils import timezone
 
 from sentry.db.models import (
-    BoundedPositiveIntegerField, Model, sane_repr
+    BoundedPositiveIntegerField, FlexibleForeignKey, Model, sane_repr
 )
 from sentry.utils.cache import cache
 from sentry.utils.hashlib import md5_text
 
 
+class EnvironmentProject(Model):
+    __core__ = False
+
+    project = FlexibleForeignKey('sentry.Project')
+    environment = FlexibleForeignKey('sentry.Environment')
+
+    class Meta:
+        app_label = 'sentry'
+        db_table = 'sentry_environmentproject'
+        unique_together = (('project', 'environment'),)
+
+
 class Environment(Model):
     __core__ = False
 
+    organization_id = BoundedPositiveIntegerField(null=True)
+    projects = models.ManyToManyField('sentry.Project', through=EnvironmentProject)
     project_id = BoundedPositiveIntegerField()
     name = models.CharField(max_length=64)
     date_added = models.DateTimeField(default=timezone.now)
@@ -47,6 +61,18 @@ class Environment(Model):
                 project_id=project.id,
                 name=name,
             )[0]
+            if env.organization_id is None:
+                cls.objects.filter(id=env.id).update(
+                    organization_id=project.organization_id
+                )
+            env.add_project(project)
             cache.set(cache_key, env, 3600)
 
         return env
+
+    def add_project(self, project):
+        try:
+            with transaction.atomic():
+                EnvironmentProject.objects.create(project=project, environment=self)
+        except IntegrityError:
+            pass
