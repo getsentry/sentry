3445015b3b352d2d71b066ef3e91717f5fc2f8d7
Jess MacQueen <jess@getsentry.com>
2017-02-16 11:32:29 -0800

    fix project.has_releases migration

--- src/sentry/south_migrations/0298_backfill_project_has_releases.py
+++ src/sentry/south_migrations/0298_backfill_project_has_releases.py
@@ -16,7 +16,7 @@ class Migration(DataMigration):
         db.commit_transaction()
         queryset = Project.objects.all()
         for item in RangeQuerySetWrapperWithProgressBar(queryset):
-            if item.flags | 1:
+            if item.flags == item.flags | 1:
                 continue
             if not ReleaseProject.objects.filter(project=item).exists():
                 continue
