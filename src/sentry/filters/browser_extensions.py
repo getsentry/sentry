b2752eb6ec4090e2dd515431409a8d831b02b784
maxbittker <maxbittker@gmail.com>
2017-02-21 16:54:52 -0800

    add kapersky lab to browser extension filters

--- src/sentry/filters/browser_extensions.py
+++ src/sentry/filters/browser_extensions.py
@@ -31,6 +31,8 @@ EXTENSION_EXC_VALUES = re.compile('|'.join((re.escape(x) for x in (
     # Google Search app (iOS)
     # See: https://github.com/getsentry/raven-js/issues/756
     'null is not an object (evaluating \'elt.parentNode\')'
+    # Kaspersky Protection browser extension
+    'in KasperskyLab'
 ))), re.I)
 
 EXTENSION_EXC_SOURCES = re.compile('|'.join((
