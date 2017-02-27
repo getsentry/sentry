8793209c81cff2d5c17a93d4b7ba95157c233da2
ckj <chriskjennings@gmail.com>
2017-02-27 14:16:53 -0800

    Convert files changed to list-group

--- src/sentry/static/sentry/app/components/fileChange.jsx
+++ src/sentry/static/sentry/app/components/fileChange.jsx
@@ -4,8 +4,6 @@ import Avatar from '../components/avatar';
 import TooltipMixin from '../mixins/tooltip';
 import ApiMixin from '../mixins/apiMixin';
 
-import {t} from '../locale';
-
 const FileChange = React.createClass({
   propTypes: {
     filename: React.PropTypes.string.isRequired,
@@ -31,29 +29,31 @@ const FileChange = React.createClass({
     authors = Array.from(authors);
     types = Array.from(types);
     return (
-      <li className="row">
-        <div className="col-sm-3">{filename}</div>
-        <div className="col-sm-3 avatar-grid">
-        {authors.map(author => {
-            return (
-              <span className="avatar-grid-item tip"
-                   title={author.name + ' ' + author.email}>
-                <Avatar user={author}/>
-              </span>);
-        })}
-        </div>
-        <div className="col-sm-3">
-        {types.map(type => {
-          if (type ===  'A') {
-            return (<span key={type}>{t('Added')} </span>);
-          }
-          else if (type === 'D') {
-            return (<span key={type}>{t('Deleted')} </span>);
-          }
-          else if (type === 'M') {
-            return (<span key={type}>{t('Modified')} </span>);
-          }
-        })}
+      <li className="list-group-item">
+        <div className="row">
+          <div className="col-sm-9">{filename}</div>
+          <div className="col-sm-3 avatar-grid">
+          {authors.map(author => {
+              return (
+                <span className="avatar-grid-item m-b-0 tip"
+                     title={author.name + ' ' + author.email}>
+                  <Avatar user={author}/>
+                </span>);
+          })}
+          </div>
+          {/* <div className="col-sm-3">
+          {types.map(type => {
+            if (type ===  'A') {
+              return (<span key={type}>{t('Added')} </span>);
+            }
+            else if (type === 'D') {
+              return (<span key={type}>{t('Deleted')} </span>);
+            }
+            else if (type === 'M') {
+              return (<span key={type}>{t('Modified')} </span>);
+            }
+          })}
+          </div> */}
         </div>
       </li>
     );
