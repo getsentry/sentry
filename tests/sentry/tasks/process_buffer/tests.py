991a8ebf271554fa73219752b6b97f16a4b3d556
David Cramer <dcramer@gmail.com>
2017-03-06 09:46:44 -0800

    very basic test for process_buffer

--- tests/sentry/tasks/process_buffer/tests.py
+++ tests/sentry/tasks/process_buffer/tests.py
@@ -16,3 +16,10 @@ class ProcessIncrTest(TestCase):
         filters = {'pk': 1}
         process_incr(model=model, columns=columns, filters=filters)
         process.assert_called_once_with(model=model, columns=columns, filters=filters)
+
+
+class ProcessBufferTest(TestCase):
+    @mock.patch('sentry.buffer.backend.process_pending')
+    def test_nothing(self, process_pending):
+        # this effectively just says "does the code run"
+        process_pending.assert_called_once_with()
