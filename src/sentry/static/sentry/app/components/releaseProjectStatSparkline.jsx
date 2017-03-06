b8a50c1150b2e06b9424feb48cbfe63fee5bc53d
Katie Lundsgaard <katie@getsentry.com>
2017-03-06 14:12:13 -0800

    Remove static data and add number of new issues to ReleaseProjectStatSparkline

--- src/sentry/static/sentry/app/components/releaseProjectStatSparkline.jsx
+++ src/sentry/static/sentry/app/components/releaseProjectStatSparkline.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import {Link} from 'react-router';
+
 import LoadingIndicator from '../components/loadingIndicator';
 import LoadingError from '../components/loadingError';
 
@@ -9,7 +11,8 @@ import ApiMixin from '../mixins/apiMixin';
 const ReleaseProjectStatSparkline = React.createClass({
   propTypes: {
     orgId: React.PropTypes.string,
-    projectId: React.PropTypes.string,
+    project: React.PropTypes.object,
+    version: React.PropTypes.string,
   },
 
   mixins: [ApiMixin],
@@ -19,11 +22,13 @@ const ReleaseProjectStatSparkline = React.createClass({
       loading: true,
       error: false,
       stats: [],
+      issues: 0,
     };
   },
 
   componentDidMount() {
-    let {orgId, projectId} = this.props;
+    let {orgId} = this.props;
+    let projectId = this.props.project.slug;
     let path = `/projects/${orgId}/${projectId}/stats/`;
     this.api.request(path, {
       method: 'GET',
@@ -41,9 +46,25 @@ const ReleaseProjectStatSparkline = React.createClass({
         });
       }
     });
+    let issuesPath = `/projects/${orgId}/${projectId}/issues/`;
+    this.api.request(issuesPath, {
+      method: 'GET',
+      data: {'query': 'first-release:"' + this.props.version + '"'},
+      success: (data, _, jqXHR) => {
+        this.setState({
+          issues: data.length,
+        });
+      },
+      error: () => {
+        this.setState({
+          error: true,
+        });
+      }
+    });
   },
 
   render() {
+    let {orgId, project} = this.props;
     let values = this.state.stats.map(tuple => tuple[1]);
     if (this.state.loading)
       return <LoadingIndicator/>;
@@ -51,11 +72,19 @@ const ReleaseProjectStatSparkline = React.createClass({
     if (this.state.error)
       return <LoadingError/>;
     return (
-      <div className="sparkline">
-        <Sparklines data={values} width={100} height={32}>
-          <SparklinesLine style={{stroke: '#8f85d4', fill: 'none', strokeWidth: 3}}/>
-        </Sparklines>
-      </div>
+      <li>
+        <div className="sparkline pull-right" style={{width: 96}}>
+          <Sparklines data={values} width={100} height={32}>
+            <SparklinesLine style={{stroke: '#8f85d4', fill: 'none', strokeWidth: 3}}/>
+          </Sparklines>
+        </div>
+        <Link to={`/${orgId}/${project.slug}/`}>
+          <h6 className="m-b-0">
+            {project.name}
+          </h6>
+          <p className="m-b-0">{this.state.issues} New Issues</p>
+        </Link>
+      </li>
     );
   }
 });
