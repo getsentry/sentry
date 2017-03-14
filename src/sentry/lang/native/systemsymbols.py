f61e9db0c9792552ad52e82bf8f58639b92839bc
Armin Ronacher <armin.ronacher@active-4.com>
2017-03-14 01:13:18 +0100

    Stop using safe_urlopen in symbolserver code

--- src/sentry/lang/native/systemsymbols.py
+++ src/sentry/lang/native/systemsymbols.py
@@ -6,7 +6,7 @@ from django.conf import settings
 
 from requests.exceptions import RequestException
 
-from sentry.http import safe_urlopen
+from sentry.http import Session
 from sentry.lang.native.utils import sdk_info_to_sdk_id
 
 
@@ -24,6 +24,8 @@ def lookup_system_symbols(symbols, sdk_info=None, cpu_name=None):
     if not settings.SYMBOL_SERVER_ENABLED:
         return
 
+    url = '%s/lookup' % settings.SYMBOL_SERVER_URL.rstrip('/')
+    sess = Session()
     symbol_query = {
         'sdk_id': sdk_info_to_sdk_id(sdk_info),
         'cpu_name': cpu_name,
@@ -31,15 +33,16 @@ def lookup_system_symbols(symbols, sdk_info=None, cpu_name=None):
     }
 
     attempts = 0
-    while 1:
-        try:
-            rv = safe_urlopen('%s/lookup' % settings.SYMBOL_SERVER_URL.rstrip('/'),
-                              method='POST', json=symbol_query)
-            rv.raise_for_status()
-            return rv.json()['symbols']
-        except (IOError, RequestException):
-            attempts += 1
-            if attempts > MAX_ATTEMPTS:
-                logger.error('Failed to contact system symbol server',
-                             exc_info=True)
-                return
+
+    with sess:
+        while 1:
+            try:
+                rv = sess.post(url, json=symbol_query)
+                rv.raise_for_status()
+                return rv.json()['symbols']
+            except (IOError, RequestException):
+                attempts += 1
+                if attempts > MAX_ATTEMPTS:
+                    logger.error('Failed to contact system symbol server',
+                                 exc_info=True)
+                    return
