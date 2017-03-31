436da598f443d3f0db649bdf73706b1614eff1fb
Jess MacQueen <jessmacqueen@gmail.com>
2017-03-31 16:53:41 -0700

    use camel case in release head commit serializers (#5179)

--- src/sentry/api/serializers/rest_framework/release_head_commit.py
+++ src/sentry/api/serializers/rest_framework/release_head_commit.py
@@ -4,6 +4,6 @@ from rest_framework import serializers
 
 
 class ReleaseHeadCommitSerializer(serializers.Serializer):
-    current_id = serializers.CharField(max_length=64)
+    currentId = serializers.CharField(max_length=64)
     repository = serializers.CharField(max_length=64)
-    previous_id = serializers.CharField(max_length=64, required=False)
+    previousId = serializers.CharField(max_length=64, required=False)
