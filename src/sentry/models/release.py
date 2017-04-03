b15cc08ce81e825de718c5fd23ef0042810b2913
Jess MacQueen <jess@getsentry.com>
2017-04-03 15:46:36 -0700

    pass repo model

--- src/sentry/models/release.py
+++ src/sentry/models/release.py
@@ -279,7 +279,7 @@ class Release(Model):
                 provider = provider_cls(id=repo.provider)
                 try:
                     repo_commits = provider.compare_commits(
-                        repo.config['name'], start_sha, end_sha, actor=user
+                        repo, start_sha, end_sha, actor=user
                     )
                 except NotImplementedError:
                     pass
--- src/sentry/plugins/providers/dummy/repository.py
+++ src/sentry/plugins/providers/dummy/repository.py
@@ -24,11 +24,11 @@ class DummyRepositoryProvider(RepositoryProvider):
     def compare_commits(self, repo, start_sha, end_sha, actor=None):
         return [{
             'id': '62de626b7c7cfb8e77efb4273b1a3df4123e6216',
-            'repository': repo,
+            'repository': repo.name,
         }, {
             'id': '58de626b7c7cfb8e77efb4273b1a3df4123e6345',
-            'repository': repo,
+            'repository': repo.name,
         }, {
             'id': end_sha,
-            'repository': repo,
+            'repository': repo.name,
         }]
