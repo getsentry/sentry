9b570480fe6fed439e46fa444e6ae655b38ef70b
Brett Hoerner <brett@bretthoerner.com>
2017-04-04 13:29:34 -0500

    Lookup new SDK versions regardless of case.

--- src/sentry/interfaces/sdk.py
+++ src/sentry/interfaces/sdk.py
@@ -18,12 +18,11 @@ def get_with_prefix(d, k, default=None, delimiter=":"):
     """
 
     prefix = k.split(delimiter, 1)[0]
-    if k in d:
-        return d[k]
-    elif prefix in d:
-        return d[prefix]
-    else:
-        return default
+    for key in [k, k.lower(), prefix, prefix.lower()]:
+        if key in d:
+            return d[key]
+
+    return default
 
 
 class Sdk(Interface):
