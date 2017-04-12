ab3ac8311afdce30835e5436ee158814fe66d605
David Cramer <dcramer@gmail.com>
2017-04-12 13:20:08 -0700

    improve user list

--- src/sentry/static/sentry/app/components/eventUserList.jsx
+++ src/sentry/static/sentry/app/components/eventUserList.jsx
@@ -16,33 +16,21 @@ export default React.createClass({
     let {data, orgId, projectId} = this.props;
     return (
       <div className="panel panel-default">
-        <div className="panel-heading panel-heading-bold">
-          <div className="row">
-            <div className="col-sm-4">
-              Commit
-            </div>
-            <div className="col-sm-4">
-              Location
-            </div>
-            <div className="col-sm-4">
-              Last seen
-            </div>
-          </div>
-        </div>
+
         <div className="list-group">
           {data.map((user) => {
             return (
               <div className="list-group-item" key={user.id}>
                 <div className="row">
-                  <div className="list-group-avatar col-sm-4">
+                  <div className="list-group-avatar col-sm-6">
                     <Avatar user={user} size={36} />
                     <h5><EventUserModalLink user={user} orgId={orgId} projectId={projectId} /></h5>
                     <p className="text-muted">First seen <TimeSince date={user.dateCreated} /></p>
                   </div>
-                  <div className="col-sm-4">
+                  <div className="col-sm-3">
                     {location && <Location location={user.lastLocation} />}
                   </div>
-                  <div className="col-sm-4">
+                  <div className="col-sm-3">
                     <h5><TimeSince date={user.lastIssue.lastSeen} /></h5>
                     <p className="text-muted">{user.lastIssue.project.name}</p>
                   </div>
