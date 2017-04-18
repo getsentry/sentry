1082090913555e857e49caeaeca8530eb3832a2a
Jess MacQueen <jess@getsentry.com>
2017-04-18 15:54:43 -0700

    add external_id column to CommitAuthor

--- src/sentry/models/commitauthor.py
+++ src/sentry/models/commitauthor.py
@@ -11,6 +11,7 @@ class CommitAuthor(Model):
     organization_id = BoundedPositiveIntegerField(db_index=True)
     name = models.CharField(max_length=128, null=True)
     email = models.EmailField()
+    external_id = models.CharField(max_length=164, null=True)
 
     class Meta:
         app_label = 'sentry'
