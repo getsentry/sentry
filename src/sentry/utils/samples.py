0a3a7e306466de32508700883d58232dcce7dd41
David Cramer <dcramer@gmail.com>
2017-04-22 10:46:56 -0700

    [mocks] general improvements
    - randomize commits
    - add ip/name to users
    - add deploy activity item
    - expand users in events

--- src/sentry/utils/samples.py
+++ src/sentry/utils/samples.py
@@ -8,12 +8,15 @@ sentry.utils.samples
 from __future__ import absolute_import
 
 import os.path
+import random
 from datetime import datetime, timedelta
+
+import six
 
 from sentry.constants import DATA_ROOT
 from sentry.event_manager import EventManager
+from sentry.interfaces.user import User as UserInterface
 from sentry.utils import json
-
 
 epoch = datetime.utcfromtimestamp(0)
 
@@ -21,6 +24,57 @@ epoch = datetime.utcfromtimestamp(0)
 def milliseconds_ago(now, milliseconds):
     ago = (now - timedelta(milliseconds=milliseconds))
     return (ago - epoch).total_seconds()
+
+
+def random_ip():
+    not_valid = [10, 127, 169, 172, 192]
+
+    first = random.randrange(1, 256)
+    while first in not_valid:
+        first = random.randrange(1, 256)
+
+    return '.'.join((
+        six.text_type(first),
+        six.text_type(random.randrange(1, 256)),
+        six.text_type(random.randrange(1, 256)),
+        six.text_type(random.randrange(1, 256))
+    ))
+
+
+def random_username():
+    return random.choice([
+        'jess', 'david', 'chris', 'eric', 'katie', 'ben', 'armin', 'saloni',
+        'max', 'meredith', 'matt', 'sentry',
+    ])
+
+
+def name_for_username(username):
+    return {
+        'ben': 'Ben Vinegar',
+        'chris': 'Chris Jennings',
+        'david': 'David Cramer',
+        'matt': 'Matt Robenolt',
+        'jess': 'Jess MacQueen',
+        'katie': 'Katie Lundsgaard',
+        'saloni': 'Saloni Dudziak',
+        'max': 'Max Bittker',
+        'meredith': 'Meredith Heller',
+        'eric': 'Eric Feng',
+        'armin': 'Armin Ronacher',
+    }.get(username, username.replace('_', ' ').title())
+
+
+def generate_user(username=None, email=None, ip_address=None, id=None):
+    if username is None and email is None:
+        username = random_username()
+        email = '{}@example.com'.format(username)
+    return UserInterface.to_python({
+        'id': id,
+        'username': username,
+        'email': email,
+        'ip_address': ip_address or random_ip(),
+        'name': name_for_username(username),
+    }).to_json()
 
 
 def load_data(platform, default=None, timestamp=None, sample_name=None):
@@ -56,11 +110,12 @@ def load_data(platform, default=None, timestamp=None, sample_name=None):
 
     data['platform'] = platform
     data['message'] = 'This is an example %s exception' % (sample_name,)
-    data['sentry.interfaces.User'] = {
-        "username": "getsentry",
-        "id": "1671",
-        "email": "foo@example.com"
-    }
+    data['sentry.interfaces.User'] = generate_user(
+        ip_address='127.0.0.1',
+        username='sentry',
+        id=1,
+        email='sentry@example.com',
+    )
     data['extra'] = {
         'session': {
             'foo': 'bar',
