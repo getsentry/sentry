d301a8dcd1b90fa157a61a3ba63e925075609be6
Ted Kaemming <ted@kaemming.com>
2017-04-26 14:04:20 -0700

    Add `state` field to `GroupHash`.

--- src/sentry/models/grouphash.py
+++ src/sentry/models/grouphash.py
@@ -8,17 +8,29 @@ sentry.models.grouphash
 from __future__ import absolute_import
 
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-from sentry.db.models import FlexibleForeignKey, Model
+from sentry.db.models import BoundedPositiveIntegerField, FlexibleForeignKey, Model
 from sentry.utils import redis
 
 
 class GroupHash(Model):
     __core__ = False
 
+    class State:
+        ACTIVE = 0
+        MIGRATION_IN_PROGRESS = 1
+
     project = FlexibleForeignKey('sentry.Project', null=True)
     hash = models.CharField(max_length=32)
     group = FlexibleForeignKey('sentry.Group', null=True)
+    state = BoundedPositiveIntegerField(
+        choices=(
+            (State.ACTIVE, _('Active')),
+            (State.MIGRATION_IN_PROGRESS, _('Migration in Progress')),
+        ),
+        default=State.ACTIVE,
+    )
 
     class Meta:
         app_label = 'sentry'
