4e86a3925eb73bac25fa137be3bf1e3c411cc36c
Jess MacQueen <macqueen@users.noreply.github.com>
2017-05-02 17:50:36 -0700

    don't try to pull commit from db in fetch_commits (#5334)

--- src/sentry/tasks/commits.py
+++ src/sentry/tasks/commits.py
@@ -37,15 +37,6 @@ def fetch_commits(release_id, user_id, refs, prev_release_id=None, **kwargs):
             continue
 
         try:
-            commit = Commit.objects.get(
-                organization_id=release.organization_id,
-                repository_id=repo.id,
-                key=ref['commit'],
-            )
-        except Commit.DoesNotExist:
-            continue
-
-        try:
             provider_cls = bindings.get('repository.provider').get(repo.provider)
         except KeyError:
             continue
@@ -66,7 +57,7 @@ def fetch_commits(release_id, user_id, refs, prev_release_id=None, **kwargs):
         else:
             continue
 
-        end_sha = commit.key
+        end_sha = ref['commit']
         provider = provider_cls(id=repo.provider)
         try:
             repo_commits = provider.compare_commits(
