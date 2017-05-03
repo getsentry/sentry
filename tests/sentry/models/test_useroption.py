50f27565a8d505c6799e646f497dfda80497eb05
maxbittker <maxbittker@gmail.com>
2017-05-02 18:22:15 -0700

    add test for this behavior

---
+++ tests/sentry/models/test_useroption.py
@@ -0,0 +1,31 @@
+from __future__ import absolute_import
+
+from sentry.models import UserOption, UserOptionValue
+from sentry.testutils import TestCase
+
+
+class UserOptionManagerTest(TestCase):
+    def test_simple(self):
+        self.user = self.create_user('foo@example.com')
+        self.org = self.create_organization(owner=None)
+        self.team = self.create_team(organization=self.org)
+        self.create_member(user=self.user, organization=self.org, teams=[self.team])
+
+        UserOption.objects.set_value(
+            user=self.user,
+            organization=self.org,
+            key='deploy-emails',
+            value=UserOptionValue.all_deploys,
+        )
+
+        UserOption.objects.unset_value(
+            user=self.user,
+            organization=self.org,
+            key='deploy-emails',
+        )
+
+        assert UserOption.objects.get_value(
+            user=self.user,
+            key='deploy-emails',
+            organization=self.org,
+        ) == UserOption.UserOption.committed_deploys_only
