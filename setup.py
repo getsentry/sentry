bf1fcb26524b770d825886e7d986de50fac504ec
Armin Ronacher <armin.ronacher@active-4.com>
2017-05-05 12:15:10 +0200

    Bump symsynd to 3.0.0

--- setup.py
+++ setup.py
@@ -138,7 +138,7 @@ install_requires = [
     'statsd>=3.1.0,<3.2.0',
     'structlog==16.1.0',
     'South==1.0.1',
-    'symsynd>=2.5.0,<3.0.0',
+    'symsynd>=3.0.0,<4.0.0',
     'toronado>=0.0.11,<0.1.0',
     'ua-parser>=0.6.1,<0.8.0',
     'urllib3>=1.14,<1.17',
