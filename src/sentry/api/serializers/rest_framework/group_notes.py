a3eec592fb87c43766208d387efb65264c600cd5
Meredith <meredith@getsentry.com>
2017-05-10 10:44:51 -0700

    fix indentation and grammar

--- src/sentry/api/serializers/rest_framework/group_notes.py
+++ src/sentry/api/serializers/rest_framework/group_notes.py
@@ -17,6 +17,6 @@ class NoteSerializer(serializers.Serializer):
             member_ids = set(project.member_set.filter(user_id__in=mentions).values_list('user_id', flat=True))
             invalid_user_ids = [m for m in mentions if int(m) not in member_ids]
             if invalid_user_ids:
-                    raise serializers.ValidationError('Cannot mention to non-team member')
+                raise serializers.ValidationError('Cannot mention a non-team member')
 
         return attrs
