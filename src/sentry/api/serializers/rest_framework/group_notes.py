1c9f795a6f633689c656fd7da1d24d32b74a4f47
Meredith <meredith@getsentry.com>
2017-05-10 13:46:24 -0700

    simplify noteSerializer

--- src/sentry/api/serializers/rest_framework/group_notes.py
+++ src/sentry/api/serializers/rest_framework/group_notes.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 from rest_framework import serializers
-from sentry.models import Project
 
 from .list import ListField
 
@@ -13,7 +12,7 @@ class NoteSerializer(serializers.Serializer):
     def validate_mentions(self, attrs, source):
         if source in attrs and 'group' in self.context:
             mentions = attrs[source]
-            project = Project.objects.get(id=self.context['group'].project_id)
+            project = self.context['group'].project
             member_ids = set(project.member_set.filter(user_id__in=mentions).values_list('user_id', flat=True))
             invalid_user_ids = [m for m in mentions if int(m) not in member_ids]
             if invalid_user_ids:
