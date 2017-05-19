8c0accab145a6c9b2c9534835956b5fe4523a0c4
David Cramer <dcramer@gmail.com>
2017-05-19 10:07:53 -0700

    add organization to hook

--- src/sentry/static/sentry/app/views/projectKeyDetails.jsx
+++ src/sentry/static/sentry/app/views/projectKeyDetails.jsx
@@ -129,6 +129,7 @@ const KeyStats = React.createClass({
 
 const KeySettings = React.createClass({
   propTypes: {
+    organization: React.PropTypes.object.isRequired,
     project: React.PropTypes.object.isRequired,
     access: React.PropTypes.object.isRequired,
     data: React.PropTypes.object.isRequired,
@@ -255,7 +256,7 @@ const KeySettings = React.createClass({
     let isSaving = this.state.state === FormState.SAVING;
     let {errors, formData} = this.state;
     let hasChanges = !underscore.isEqual(this.props.initialData, formData);
-    let {access, data, rateLimitsEnabled, project} = this.props;
+    let {access, data, rateLimitsEnabled, organization, project} = this.props;
     return (
       <form onSubmit={this.onSubmit} className="form-stacked">
         {this.state.state === FormState.ERROR &&
@@ -302,7 +303,7 @@ const KeySettings = React.createClass({
             {!rateLimitsEnabled
               ? this.state.hooksDisabled
                   .map(hook => {
-                    return hook(project, data);
+                    return hook(organization, project, data);
                   })
                   .find(() => true)
               : <div className="form-group">
@@ -519,6 +520,7 @@ export default React.createClass({
           <KeyStats params={params} />
 
           <KeySettings
+            organization={this.getOrganization()}
             project={this.getProject()}
             access={this.getAccess()}
             params={params}
