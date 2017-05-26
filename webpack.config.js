747a2f413a9505970f95ae2a7fca0d918938134b
Ben Vinegar <ben.vinegar@gmail.com>
2017-05-26 11:28:02 -0700

    Revert "Upgrade Webpack, loaders + enable cacheDirectory for babel-loader (#5433)" (#5458)
    This reverts commit 14fa645d591b61e8b560814b1f7fcb8e70d4c568.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 /*eslint-env node*/
 /*eslint no-var:0*/
 var path = require('path'),
-  fs = require('fs'),
-  webpack = require('webpack'),
-  ExtractTextPlugin = require('extract-text-webpack-plugin');
+    fs = require('fs'),
+    webpack = require('webpack'),
+    ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-var staticPrefix = 'src/sentry/static/sentry', distPath = staticPrefix + '/dist';
+var staticPrefix = 'src/sentry/static/sentry',
+    distPath = staticPrefix + '/dist';
 
 // this is set by setup.py sdist
 if (process.env.SENTRY_STATIC_DIST_PATH) {
-  distPath = process.env.SENTRY_STATIC_DIST_PATH;
+    distPath = process.env.SENTRY_STATIC_DIST_PATH;
 }
 
 var IS_PRODUCTION = process.env.NODE_ENV === 'production';
 
-var babelConfig = JSON.parse(fs.readFileSync(path.join(__dirname, '.babelrc')));
-babelConfig.cacheDirectory = true;
+var babelConfig = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '.babelrc'))
+);
 
 // only extract po files if we need to
 if (process.env.SENTRY_EXTRACT_TRANSLATIONS === '1') {
@@ -29,14 +31,14 @@ if (process.env.SENTRY_EXTRACT_TRANSLATIONS === '1') {
       t: ['msgid'],
       tn: ['msgid', 'msgid_plural', 'count'],
       tct: ['msgid']
-    }
+    },
   });
 }
 
 var entry = {
   // js
-  app: 'app',
-  vendor: [
+  'app': 'app',
+  'vendor': [
     'babel-polyfill',
     'bootstrap/js/dropdown',
     'bootstrap/js/tab',
@@ -67,7 +69,7 @@ var entry = {
   // css
   // NOTE: this will also create an empty 'sentry.js' file
   // TODO: figure out how to not generate this
-  sentry: 'less/sentry.less'
+  'sentry': 'less/sentry.less'
 };
 
 // dynamically iterate over locale files and add to `entry` config
@@ -75,8 +77,9 @@ var localeCatalogPath = path.join(__dirname, 'src', 'sentry', 'locale', 'catalog
 var localeCatalog = JSON.parse(fs.readFileSync(localeCatalogPath, 'utf8'));
 var localeEntries = [];
 
-localeCatalog.supported_locales.forEach(function(locale) {
-  if (locale === 'en') return;
+localeCatalog.supported_locales.forEach(function (locale) {
+  if (locale === 'en')
+    return;
 
   // Django locale names are "zh_CN", moment's are "zh-cn"
   var normalizedLocale = locale.toLowerCase().replace('_', '-');
@@ -86,6 +89,7 @@ localeCatalog.supported_locales.forEach(function(locale) {
   ];
   localeEntries.push('locale/' + normalizedLocale);
 });
+
 
 var config = {
   entry: entry,
@@ -115,7 +119,7 @@ var config = {
         test: /\.less$/,
         include: path.join(__dirname, staticPrefix),
         loader: ExtractTextPlugin.extract({
-          fallbackLoader: 'style-loader?sourceMap=false',
+          fallbackLoader: 'style-loader',
           loader: 'css-loader' + (IS_PRODUCTION ? '?minimize=true' : '') + '!less-loader'
         })
       },
@@ -129,7 +133,7 @@ var config = {
       /dist\/jquery\.js/,
       /jed\/jed\.js/,
       /marked\/lib\/marked\.js/
-    ]
+    ],
   },
   plugins: [
     new webpack.optimize.CommonsChunkPlugin({
@@ -162,7 +166,7 @@ var config = {
   ],
   resolve: {
     alias: {
-      flot: path.join(__dirname, staticPrefix, 'vendor', 'jquery-flot'),
+      'flot': path.join(__dirname, staticPrefix, 'vendor', 'jquery-flot'),
       'flot-tooltip': path.join(__dirname, staticPrefix, 'vendor', 'jquery-flot-tooltip'),
       'sentry-locale': path.join(__dirname, 'src', 'sentry', 'locale')
     },
@@ -170,41 +174,37 @@ var config = {
     extensions: ['*', '.jsx', '.js', '.json']
   },
   output: {
-    path: path.join(__dirname, distPath),
+    path: distPath,
     filename: '[name].js',
     libraryTarget: 'var',
     library: 'exports',
-    sourceMapFilename: '[name].js.map'
+    sourceMapFilename: '[name].js.map',
   },
-  devtool: IS_PRODUCTION ? '#source-map' : '#cheap-source-map'
+  devtool: IS_PRODUCTION ?
+    '#source-map' :
+    '#cheap-source-map'
 };
 
 if (IS_PRODUCTION) {
   // This compression-webpack-plugin generates pre-compressed files
   // ending in .gz, to be picked up and served by our internal static media
   // server as well as nginx when paired with the gzip_static module.
-  config.plugins.push(
-    new (require('compression-webpack-plugin'))({
-      algorithm: function(buffer, options, callback) {
-        require('zlib').gzip(buffer, callback);
-      },
-      regExp: /\.(js|map|css|svg|html|txt|ico|eot|ttf)$/
-    })
-  );
+  config.plugins.push(new (require('compression-webpack-plugin'))({
+    algorithm: function(buffer, options, callback) {
+      require('zlib').gzip(buffer, callback);
+    },
+    regExp: /\.(js|map|css|svg|html|txt|ico|eot|ttf)$/,
+  }));
 
   // Disable annoying UglifyJS warnings that pollute Travis log output
   // NOTE: This breaks -p in webpack 2. Must call webpack w/ NODE_ENV=production for minification.
-  config.plugins.push(
-    new webpack.optimize.UglifyJsPlugin({
-      compress: {
-        warnings: false
-      },
-      // https://github.com/webpack/webpack/blob/951a7603d279c93c936e4b8b801a355dc3e26292/bin/convert-argv.js#L442
-      sourceMap: config.devtool &&
-        (config.devtool.indexOf('sourcemap') >= 0 ||
-          config.devtool.indexOf('source-map') >= 0)
-    })
-  );
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    },
+    // https://github.com/webpack/webpack/blob/951a7603d279c93c936e4b8b801a355dc3e26292/bin/convert-argv.js#L442
+    sourceMap: config.devtool && (config.devtool.indexOf('sourcemap') >= 0 || config.devtool.indexOf('source-map') >= 0)
+  }));
 }
 
 module.exports = config;
