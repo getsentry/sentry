698a70afa025ee0bd778ba24b71c40c41a96d568
Eric Feng <erichfeng@gmail.com>
2017-05-30 17:56:47 -0700

    adding receivers and tables for feature adoption

---
+++ src/sentry/models/featureadoption.py
@@ -0,0 +1,143 @@
+from __future__ import absolute_import
+
+from django.core.cache import cache
+from django.db import models
+from django.utils import timezone
+from jsonfield import JSONField
+
+from sentry.db.models import (
+    BaseManager,
+    FlexibleForeignKey,
+    Model,
+    sane_repr
+)
+
+
+class Feature(object):
+    """
+    prerequesites: list of feature_id's that are requirements or tuples (which are ORs)
+    """
+    def __init__(self, slug, name, location, prerequisite=None):
+        """
+        Args:
+            slug(str): unique
+            name(str): Human readable
+            type(str): what kind of feature: language, framework, code, webui, admin
+            prerequesite(list): list of feature slugs or tuples. Tuples indicate OR relationships.
+        """
+        self.slug = slug
+        self.name = name
+        self.location = location
+        self.prerequisite = prerequisite
+
+    def __repr__(self):
+        return self.name
+
+features = [
+    # Languages
+    Feature("python", "Python", "language"),
+    Feature("javascript", "JavaScript", "language"),
+    Feature("node", "Node.js", "language"),
+    Feature("ruby", "Ruby", "language"),
+    Feature("java", "Java", "language"),
+    Feature("cocoa", "Cocoa", "language"),
+    Feature("objc", "Objective-C", "language"),
+    Feature("php", "PHP", "language"),
+    Feature("go", "Go", "language"),
+    Feature("csharp", "C#", "language"),
+    Feature("perl", "Perl", "language"),
+    Feature("elixir", "Elixir", "language"),
+    Feature("cfml", "CFML", "language"),
+    Feature("groovy", "Groovy", "language"),
+    Feature("csp", "CSP Reports", "language"),
+
+    # Frameworks
+    Feature("flask", "Flask", "framework", prerequisite=["python"]),
+    Feature("django", "Django", "framework", prerequisite=["python"]),
+
+    # Configuration
+    Feature("first_event", "First Event", "code", prerequisite=["first_project"]),
+    Feature("release_tracking", "Release Tracking", "code", prerequisite=["first_event"]),
+    Feature("environment_tracking", "Environment Tracking", "code", prerequisite=["first_event"]),
+    Feature("user_tracking", "User Tracking", "code", prerequisite=["first_event"]),
+    Feature("custom_tags", "Custom Tags", "code", prerequisite=["first_event"]),
+    Feature("source_maps", "Source Maps", "code", prerequisite=["first_event", "javascript"]),
+    Feature("user_feedback", "User Feedback", "code", prerequisite=["user_tracking"]),
+    Feature("api", "API", "code", prerequisite=["first_event"]),
+    Feature("breadcrumbs", "Breadcrumbs", "code", prerequisite=["first_event", ("python", "javascript", "node", "php")]),
+    # Feature("resolve_in_commit", "Resolve in Commit", "code", prerequisite=["first_event", "releases"]),
+
+    # Web UI
+    Feature("first_project", "First Project", "web"),
+    Feature("invite_team", "Invite Team", "web", prerequisite=["first_project"]),
+    Feature("assignment", "Assign Issue", "web", prerequisite=["invite_team", "first_event"]),
+    Feature("resolved_in_release", "Resolve in Next Release", "web", prerequisite=["release_tracking"]),
+    # Feature("snooze", "Snooze Issue", "web", prerequisite=["first_event"]),
+    # Feature("merge", "Merge Issues", "web", prerequisite=["first_event"]),
+    Feature("advanced_search", "Advanced Search", "web", prerequisite=["first_event"]),
+    Feature("saved_search", "Saved Search", "web", prerequisite=["advanced_search"]),
+    Feature("inbound_filters", "Inbound Filters", "web", prerequisite=["first_event"]),
+    Feature("alert_rules", "Alert Rules", "web", prerequisite=["first_event"]),
+    Feature("issue_tracker_integration", "Issue Tracker Integration", "web", prerequisite=["first_project"]),
+    Feature("notification_integration", "Notification Integration", "web", prerequisite=["first_project"]),
+    # Feature("releases", "Releases", "web", prerequisite=["first_project"]),
+
+    # Admin UI
+    Feature("sso", "SSO", "admin", prerequisite=["invite_team"]),
+    Feature("data_scrubbers", "Data Scrubbers", "admin", prerequisite=["first_event"]),
+
+    # Future features
+    # Feature("Health Metrics", "code"),
+    # Feature("Customer Support Integration", "web", prerequisite=["User Tracking"]),
+    # Feature("ETL Integration", "web", prerequesite=["First Event"]),
+    # Feature("2-Factor Auth", "web", prerequisite=["Invite Team"]),
+]
+
+# No feature slug should be duplicate
+assert len(features) == len(set([f.slug for f in features]))
+
+
+class FeatureAdoptionManager(BaseManager):
+    def record(self, organization_id, feature_slug, **kwargs):
+        cache_key = 'featureadoption:%s:%s' % (
+            organization_id,
+            feature_slug,
+        )
+        if cache.get(cache_key) is None:
+            row, created = self.create_or_update(
+                organization_id=organization_id,
+                feature_slug=feature_slug,
+                values={
+                    'date_modified': timezone.now(),
+                    'complete': True,
+                },
+                defaults={
+                    'applicable': True,  # Only on the first time should override CS
+                },
+            )
+            # Store marker to prevent running all the time
+            cache.set(cache_key, 1, 3600)
+            return created
+
+        return False
+
+
+class FeatureAdoption(Model):
+    __core__ = False
+
+    organization = FlexibleForeignKey('sentry.Organization')
+    feature_slug = models.SlugField()
+    date_completed = models.DateTimeField(default=timezone.now)
+    date_modified = models.DateTimeField(null=True)
+    complete = models.BooleanField(default=False)
+    applicable = models.BooleanField(default=True)  # Is this feature applicable to this team?
+    data = JSONField()
+
+    objects = FeatureAdoptionManager()
+
+    __repr__ = sane_repr('organization_id', 'feature_slug', 'complete', 'applicable')
+
+    class Meta:
+        app_label = 'sentry'
+        db_table = 'sentry_featureadoption'
+        unique_together = (('organization', 'feature_slug'),)
---
+++ src/sentry/receivers/features.py
@@ -0,0 +1,214 @@
+from __future__ import absolute_import
+
+from sentry.interfaces.base import get_interface
+from sentry.models import FeatureAdoption, features
+from sentry.plugins import IssueTrackingPlugin, IssueTrackingPlugin2
+from sentry.plugins.bases.notify import NotificationPlugin
+from sentry.receivers.rules import DEFAULT_RULE_LABEL, DEFAULT_RULE_DATA
+from sentry.signals import (
+    alert_rule_created,
+    event_processed,
+    first_event_received,
+    project_created,
+    member_joined,
+    plugin_enabled,
+    user_feedback_received,
+    api_called,
+    issue_assigned,
+    issue_resolved_in_release,
+    advanced_search,
+    save_search_created,
+    inbound_filter_toggled,
+    sso_enabled,
+    data_scrubber_enabled,
+)
+from sentry.utils.javascript import has_sourcemap
+
+
+# First Event
+@first_event_received.connect(weak=False)
+def record_first_event(project, group, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="first_event",
+        complete=True)
+
+
+@event_processed.connect(weak=False)
+def record_event_processed(project, group, event, **kwargs):
+    # Platform
+    platform_feature_map = {feature.name: feature.slug for feature in features if feature.location == 'language'}
+    if platform_feature_map.get(group.platform):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug=platform_feature_map.get(group.platform),
+            complete=True)
+    elif event.data.get(get_interface('csp')):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="csp",
+            complete=True)
+
+    # First Event
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="first_event",
+        complete=True)
+
+    # Release Tracking
+    if event.get_tag('sentry:release'):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="release_tracking",
+            complete=True)
+
+    # Environment Tracking
+    if event.get_tag('environment'):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="environment_tracking",
+            complete=True)
+
+    # User Tracking
+    if event.data.get('sentry.interfaces.User'):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="user_tracking",
+            complete=True)
+
+    # Custom Tags
+    default_tags = set(['sentry_version', 'environment', 'level', 'logger',
+        'browser', 'browser.name', 'device', 'os', 'os.name', 'device', 'device.name',
+        'app.device', 'url', 'server_name', 'react'])
+    if set(tag[0] for tag in event.tags) - default_tags:
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="custom_tags",
+            complete=True)
+
+    # Sourcemaps
+    if has_sourcemap(event):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="source_maps",
+            complete=True)
+
+    # Breadcrumbs
+    if event.data.get(get_interface('breadcrumbs')):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="breadcrumbs",
+            complete=True)
+
+
+@user_feedback_received.connect(weak=False)
+def record_user_feedback(project, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="user_feedback",
+        complete=True)
+
+
+@api_called.connect(weak=False)
+def record_api_called(project, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="api",
+        complete=True)
+
+
+@project_created.connect(weak=False)
+def record_project_created(project, user, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="first_project",
+        complete=True)
+
+
+@member_joined.connect(weak=False)
+def record_member_joined(member, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=member.organization_id,
+        feature_slug="invite_team",
+        complete=True)
+
+
+@issue_assigned.connect(weak=False)
+def record_issue_assigned(project, group, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="assignment",
+        complete=True)
+
+
+@issue_resolved_in_release.connect(weak=False)
+def record_issue_resolved_in_release(project, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="resolved_in_release",
+        complete=True)
+
+
+@advanced_search.connect(weak=False)
+def record_advanced_search(project, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="advanced_search",
+        complete=True)
+
+
+@save_search_created.connect(weak=False)
+def record_save_search_created(project, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="saved_search",
+        complete=True)
+
+
+@inbound_filter_toggled.connect(weak=False)
+def record_inbound_filter_toggled(project, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="inbound_filters",
+        complete=True)
+
+
+@alert_rule_created.connect(weak=False)
+def record_alert_rule_created(project, rule, **kwargs):
+    if rule.label == DEFAULT_RULE_LABEL or rule.data == DEFAULT_RULE_DATA:
+        return
+
+    FeatureAdoption.objects.record(
+        organization_id=project.organization_id,
+        feature_slug="alert_rules",
+        complete=True)
+
+
+@plugin_enabled.connect(weak=False)
+def record_plugin_enabled(plugin, project, user, **kwargs):
+    if isinstance(plugin, IssueTrackingPlugin) or isinstance(plugin, IssueTrackingPlugin2):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="issue_tracker_integration",
+            complete=True)
+    elif isinstance(plugin, NotificationPlugin):
+        FeatureAdoption.objects.record(
+            organization_id=project.organization_id,
+            feature_slug="notification_integration",
+            complete=True)
+
+
+@sso_enabled.connect(weak=False)
+def record_sso_enabled(organization, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=organization.id,
+        feature_slug="sso",
+        complete=True)
+
+
+@data_scrubber_enabled.connect(weak=False)
+def record_data_scrubber_enabled(organization, **kwargs):
+    FeatureAdoption.objects.record(
+        organization_id=organization.id,
+        feature_slug="data_scrubbers",
+        complete=True)
