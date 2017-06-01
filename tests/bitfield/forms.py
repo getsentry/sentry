6171898c2e557b8c028473c3e0ad7224214b419e
Ben Vinegar <ben.vinegar@gmail.com>
2017-06-01 10:57:10 -0700

    Can now run/debug python tests in vscode (#5490)

--- tests/bitfield/forms.py
+++ tests/bitfield/forms.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from django import forms
 
-from bitfield.tests.models import BitFieldTestModel
+from .models import BitFieldTestModel
 
 
 class BitFieldTestModelForm(forms.ModelForm):
