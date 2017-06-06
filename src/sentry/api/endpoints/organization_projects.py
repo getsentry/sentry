0b0a27dd938e728be929cbc76884aeea1d4c0ecf
David Cramer <dcramer@gmail.com>
2017-06-06 15:37:00 -0700

    api: pagination for org members and projects

--- src/sentry/api/endpoints/organization_projects.py
+++ src/sentry/api/endpoints/organization_projects.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import
 
-import six
-
 from rest_framework.response import Response
 
 from sentry.api.base import DocSection
 from sentry.api.bases.organization import OrganizationEndpoint
+from sentry.api.paginator import OffsetPaginator
 from sentry.api.serializers import serialize
+from sentry.api.serializers.models.project import ProjectWithTeamSerializer
 from sentry.models import Project, Team
 from sentry.utils.apidocs import scenario, attach_scenarios
 
@@ -38,33 +38,30 @@ class OrganizationProjectsEndpoint(OrganizationEndpoint):
             # TODO: remove this, no longer supported probably
             if hasattr(request.auth, 'project'):
                 team_list = [request.auth.project.team]
-                project_list = [request.auth.project]
+                queryset = queryset = Project.objects.filter(
+                    id=request.auth.project.id,
+                ).select_related('team')
             elif request.auth.organization is not None:
                 org = request.auth.organization
                 team_list = list(Team.objects.filter(
                     organization=org,
                 ))
-                project_list = list(Project.objects.filter(
+                queryset = Project.objects.filter(
                     team__in=team_list,
-                ).order_by('name'))
+                ).select_related('team')
             else:
                 return Response({'detail': 'Current access does not point to '
                                  'organization.'}, status=400)
         else:
             team_list = list(request.access.teams)
-            project_list = list(Project.objects.filter(
+            queryset = Project.objects.filter(
                 team__in=team_list,
-            ).order_by('name'))
+            ).select_related('team')
 
-        team_map = {
-            d['id']: d
-            for d in serialize(team_list, request.user)
-        }
-
-        context = []
-        for project, pdata in zip(project_list, serialize(project_list, request.user)):
-            assert six.text_type(project.id) == pdata['id']
-            pdata['team'] = team_map[six.text_type(project.team_id)]
-            context.append(pdata)
-
-        return Response(context)
+        return self.paginate(
+            request=request,
+            queryset=queryset,
+            order_by='slug',
+            on_results=lambda x: serialize(x, request.user, ProjectWithTeamSerializer()),
+            paginator_cls=OffsetPaginator,
+        )
