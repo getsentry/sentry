18009e756b16dcd25d879a90b114961c8c53dc6d
David Cramer <dcramer@gmail.com>
2017-06-08 11:10:05 -0700

    debugger: pin sqlparse to compatible version

--- setup.py
+++ setup.py
@@ -138,6 +138,7 @@ install_requires = [
     'statsd>=3.1.0,<3.2.0',
     'structlog==16.1.0',
     'South==1.0.1',
+    'sqlparse>=0.1.16,<0.2.0',
     'symsynd>=3.0.0,<4.0.0',
     'toronado>=0.0.11,<0.1.0',
     'ua-parser>=0.6.1,<0.8.0',
