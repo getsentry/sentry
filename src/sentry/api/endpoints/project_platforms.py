c35442ec36ff770bdf0e6c5c0c04a4d4ab1a79be
denamwangi <dena@getsentry.com>
2017-06-16 15:49:19 -0700

    Remove post from project platform endpoint

--- src/sentry/api/endpoints/project_platforms.py
+++ src/sentry/api/endpoints/project_platforms.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
-from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework import serializers
 from sentry.api.bases.project import ProjectEndpoint
@@ -19,21 +18,3 @@ class ProjectPlatformsEndpoint(ProjectEndpoint):
             project_id=project.id
         ).values('project_id', 'platform', 'date_added', 'last_seen', 'date_chosen')
         return Response(serialize(list(queryset), request.user))
-
-    def post(self, request, project):
-        now = timezone.now()
-        serializer = ProjectPlatformSerializer(data=request.DATA, context={'project': project})
-        if not serializer.is_valid():
-            return Response(serializer.errors, status=400)
-        data = dict(serializer.object)
-        values = {
-            'date_chosen': now,
-            'date_added': None,
-            'last_seen': None
-        }
-        project_platform = ProjectPlatform.objects.create_or_update(
-            project_id=data['project_id'],
-            platform=data['platform'],
-            values=values
-        )
-        return Response(serialize(project_platform, request.user), status=201)
