0edbc8a2209f310e181eda897001daf679ee1b89
denamwangi <dena@getsentry.com>
2017-06-20 11:42:42 -0700

    Remove put from teamindex endpoint and remove associated test

--- tests/sentry/api/endpoints/test_team_project_index.py
+++ tests/sentry/api/endpoints/test_team_project_index.py
@@ -51,28 +51,3 @@ class TeamProjectCreateTest(APITestCase):
             'slug': 'foobar',
         })
         assert resp.status_code == 409, resp.content
-
-
-class TeamProjectUpdateTest(APITestCase):
-    def test_simple(self):
-        self.login_as(user=self.user)
-        team = self.create_team(slug='baz')
-        project_1 = self.create_project(team=team, slug='fiz')
-
-        url = reverse('sentry-api-0-team-project-index', kwargs={
-            'organization_slug': team.organization.slug,
-            'team_slug': team.slug,
-        })
-        resp = self.client.put(url, data={
-            'slug': project_1.slug,
-            'platform': 'c'
-        })
-        assert resp.status_code == 200, resp.content
-        project = Project.objects.get(id=resp.data['id'])
-        assert project.platform == 'c'
-
-        resp = self.client.put(url, data={
-            'slug': 'not_real_slug',
-            'platform': 'c'
-        })
-        assert resp.status_code == 404, resp.content
