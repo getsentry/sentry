3a5ef35a7845d93c69104f852b0b83df45fe5e40
Jess MacQueen <jess@getsentry.com>
2017-06-19 19:41:34 -0700

    add models and basic mark as discarded endpoint

---
+++ src/sentry/models/grouptombstone.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+
+import logging
+
+from django.db import models
+
+from sentry.constants import LOG_LEVELS, MAX_CULPRIT_LENGTH
+from sentry.db.models import BoundedPositiveIntegerField, FlexibleForeignKey, Model
+
+
+class GroupTombstone(Model):
+    __core__ = False
+
+    project = FlexibleForeignKey('sentry.Project', null=True)
+    level = BoundedPositiveIntegerField(
+        choices=LOG_LEVELS.items(), default=logging.ERROR, blank=True,
+        db_index=True)
+    message = models.TextField()
+    culprit = models.CharField(
+        max_length=MAX_CULPRIT_LENGTH, blank=True, null=True,
+    )
+    type = models.TextField()
+
+    class Meta:
+        app_label = 'sentry'
+        db_table = 'sentry_grouptombstone'
