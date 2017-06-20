24645045a0a266796803d7d5b9c93172c8050552
Armin Ronacher <armin.ronacher@active-4.com>
2017-06-20 20:38:10 +0200

    Added a missing test for dsym listing

--- tests/sentry/api/endpoints/test_dsym_files.py
+++ tests/sentry/api/endpoints/test_dsym_files.py
@@ -109,3 +109,71 @@ class DSymFilesUploadTest(APITestCase):
         assert vdf.build == '1'
         assert vdf.dsym_app.app_id == 'com.example.myapp'
         assert vdf.dsym_file.uuid == PROGUARD_UUID
+
+    def test_list_dsyms(self):
+        project = self.create_project(name='foo')
+
+        url = reverse('sentry-api-0-dsym-files', kwargs={
+            'organization_slug': project.organization.slug,
+            'project_slug': project.slug,
+        })
+
+        self.login_as(user=self.user)
+
+        out = BytesIO()
+        f = zipfile.ZipFile(out, 'w')
+        f.writestr('proguard/%s.txt' % PROGUARD_UUID, PROGUARD_SOURCE)
+        f.close()
+
+        response = self.client.post(url, {
+            'file': SimpleUploadedFile('symbols.zip', out.getvalue(),
+                                       content_type='application/zip'),
+        }, format='multipart')
+
+        assert response.status_code == 201, response.content
+        assert len(response.data) == 1
+
+        url = reverse('sentry-api-0-associate-dsym-files', kwargs={
+            'organization_slug': project.organization.slug,
+            'project_slug': project.slug,
+        })
+
+        response = self.client.post(url, {
+            'checksums': ['e6d3c5185dac63eddfdc1a5edfffa32d46103b44'],
+            'platform': 'android',
+            'name': 'MyApp',
+            'appId': 'com.example.myapp',
+            'version': '1.0',
+            'build': '1',
+        }, format='json')
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 1
+        assert response.data['associatedDsymFiles'][0]['uuid'] == PROGUARD_UUID
+
+        url = reverse('sentry-api-0-dsym-files', kwargs={
+            'organization_slug': project.organization.slug,
+            'project_slug': project.slug,
+        })
+
+        response = self.client.get(url)
+
+        assert response.status_code == 200, response.content
+
+        app, = response.data['apps']
+        assert app['appId'] == 'com.example.myapp'
+        assert app['iconUrl'] is None
+        assert app['name'] == 'MyApp'
+        assert app['platform'] == 'android'
+
+        dsym, = response.data['debugSymbols']
+        assert dsym['build'] == '1'
+        assert dsym['version'] == '1.0'
+        assert dsym['dsym']['cpuName'] == 'any'
+        assert dsym['dsym']['headers'] == {'Content-Type': 'text/x-proguard+plain'}
+        assert dsym['dsym']['objectName'] == 'proguard-mapping'
+        assert dsym['dsym']['sha1'] == 'e6d3c5185dac63eddfdc1a5edfffa32d46103b44'
+        assert dsym['dsym']['symbolType'] == 'proguard'
+        assert dsym['dsym']['uuid'] == '6dc7fdb0-d2fb-4c8e-9d6b-bb1aa98929b1'
+
+        assert response.data['unreferencedDebugSymbols'] == []
