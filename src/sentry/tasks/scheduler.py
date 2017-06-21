e6681c38542470339f6c5c95be0c35c82932b39e
Eric Feng <erichfeng@gmail.com>
2017-06-21 15:43:01 -0700

    using TimedRetryPolicy instead of manually creating locks

--- src/sentry/tasks/scheduler.py
+++ src/sentry/tasks/scheduler.py
@@ -5,12 +5,10 @@ import logging
 
 from django.utils import timezone
 
+from sentry.app import locks
 from sentry.models import ScheduledJob
 from sentry.tasks.base import instrumented_task
-from sentry.utils.locking import UnableToAcquireLock
-from sentry.utils import redis
-from sentry.utils.locking.backends.redis import RedisLockBackend
-from sentry.utils.locking.manager import LockManager
+from sentry.utils.retries import TimedRetryPolicy
 
 logger = logging.getLogger('sentry')
 
@@ -19,22 +17,17 @@ logger = logging.getLogger('sentry')
 def enqueue_scheduled_jobs(**kwargs):
     from sentry.celery import app
 
-    lock_key = 'scheduler:process'
-    try:
-        locks = LockManager(RedisLockBackend(redis.clusters.get('default')))
-        lock = locks.get(key=lock_key, duration=60)
-        with lock.acquire():
-            queryset = list(ScheduledJob.objects.filter(
-                date_scheduled__lte=timezone.now(),
-            )[:100])
+    lock = locks.get('scheduler:process', duration=60)
+    with TimedRetryPolicy(5)(lock.acquire):
+        queryset = list(ScheduledJob.objects.filter(
+            date_scheduled__lte=timezone.now(),
+        )[:100])
 
-            for job in queryset:
-                logger.info('Sending scheduled job %s with payload %r',
-                            job.name, job.payload)
-                app.send_task(job.name, kwargs=job.payload)
+        for job in queryset:
+            logger.info('Sending scheduled job %s with payload %r',
+                        job.name, job.payload)
+            app.send_task(job.name, kwargs=job.payload)
 
-            ScheduledJob.objects.filter(
-                id__in=[o.id for o in queryset],
-            ).delete()
-    except UnableToAcquireLock:
-        logger.error('Failed to get scheduler lock', exc_info=True)
+        ScheduledJob.objects.filter(
+            id__in=[o.id for o in queryset],
+        ).delete()
