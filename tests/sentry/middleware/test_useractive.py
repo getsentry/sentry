45e26f51f7ffdbe1d052963d88a87699d0c8bb80
denamwangi <dena@getsentry.com>
2017-06-26 12:47:32 -0700

    Add migration file for change to user model

--- tests/sentry/middleware/test_useractive.py
+++ tests/sentry/middleware/test_useractive.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 from datetime import timedelta
 from django.utils import timezone
 from django.test import RequestFactory
-from sentry.utils.auth import login
 from exam import fixture
 
 from sentry.middleware.user import UserActiveMiddleware
@@ -22,10 +21,10 @@ class UserActiveMiddlewareTest(TestCase):
         req.user = user
 
         resp = self.middleware.process_view(req, self.view, [], {})
-        assert resp is None      
+        assert resp is None
         assert timezone.now() - user.last_active < timedelta(minutes=1)
 
         user.last_active = None
         resp = self.middleware.process_view(req, self.view, [], {})
-        assert resp is None      
+        assert resp is None
         assert timezone.now() - user.last_active < timedelta(minutes=1)
