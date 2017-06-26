6648166e2151061b7b7989a2e84dca3a91ca95bd
Eric Feng <erichfeng@gmail.com>
2017-06-26 11:02:31 -0700

    adding ted and jtcunning's comments

--- src/sentry/tasks/scheduler.py
+++ src/sentry/tasks/scheduler.py
@@ -1,4 +1,3 @@
-
 from __future__ import absolute_import, division
 
 import logging
@@ -8,26 +7,24 @@ from django.utils import timezone
 from sentry.app import locks
 from sentry.models import ScheduledJob
 from sentry.tasks.base import instrumented_task
-from sentry.utils.retries import TimedRetryPolicy
 
-logger = logging.getLogger('sentry')
+logger = logging.getLogger('sentry.scheduler')
 
 
 @instrumented_task(name='sentry.tasks.enqueue_scheduled_jobs')
 def enqueue_scheduled_jobs(**kwargs):
     from sentry.celery import app
 
-    lock = locks.get('scheduler:process', duration=60)
-    with TimedRetryPolicy(5)(lock.acquire):
-        queryset = list(ScheduledJob.objects.filter(
+    with locks.get('scheduler.process', duration=60).acquire():
+        queryset = ScheduledJob.objects.filter(
             date_scheduled__lte=timezone.now(),
-        )[:100])
+        )
+        job_count = queryset.count()
+        if job_count > 100:
+            logger.info('More than 100 ScheduledJob\'s: %d jobs found.' % job_count)
 
-        for job in queryset:
-            logger.info('Sending scheduled job %s with payload %r',
+        for job in queryset.all()[:100]:
+            logger.debug('Sending scheduled job %s with payload %r',
                         job.name, job.payload)
             app.send_task(job.name, kwargs=job.payload)
-
-        ScheduledJob.objects.filter(
-            id__in=[o.id for o in queryset],
-        ).delete()
+            job.delete()
