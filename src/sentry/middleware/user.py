f26d3e73e65c7b60cd0770e84ef75f6a24ae8792
denamwangi <dena@getsentry.com>
2017-06-26 12:55:28 -0700

    Adjust frequency of last active update

--- src/sentry/middleware/user.py
+++ src/sentry/middleware/user.py
@@ -16,7 +16,7 @@ class UserActiveMiddleware(object):
             return
 
         now = timezone.now()
-        freq = timedelta(minutes=1)
+        freq = timedelta(minutes=60)
         last_active = request.user.last_active
 
         if last_active and freq > (now - last_active):
