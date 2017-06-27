e72d6c523443dd4905be54504d67e5d46d4655df
Katie Lundsgaard <katie@getsentry.com>
2017-06-27 10:34:11 -0700

    Update scope and permissions of `GroupTombstoneEndpoint`

--- tests/sentry/api/endpoints/test_group_tombstone.py
+++ tests/sentry/api/endpoints/test_group_tombstone.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import, print_function
 
+from django.core.urlresolvers import reverse
+
 from sentry.models import GroupHash, GroupTombstone
 from sentry.testutils import APITestCase
 
@@ -7,16 +9,22 @@ from sentry.testutils import APITestCase
 class GroupTombstoneTest(APITestCase):
 
     def test_simple(self):
-        # import pdb; pdb.set_trace()
-        self.login_as(user=self.user)
-        org = self.create_organization(
+        self.user = self.create_user('foo@example.com')
+        self.org = self.create_organization(
             owner=self.user,
+            name='Rowdy Tiger'
         )
-        project = self.create_project(
-            organization=org,
-            name='CoolProj',
+        self.team = self.create_team(
+            organization=self.org,
+            name='Mariachi Band'
         )
-        group = self.create_group(project=project)
+        self.project = self.create_project(
+            organization=self.org,
+            team=self.team,
+            name='Bengal',
+        )
+        self.login_as(self.user)
+        group = self.create_group(project=self.project)
         tombstone = GroupTombstone.objects.create(
             project_id=group.project_id,
             level=group.level,
@@ -31,9 +39,14 @@ class GroupTombstoneTest(APITestCase):
             group_tombstone=tombstone
         )
 
-        path = '/api/0/projects/{}/{}/tombstone/'.format(org.id, project.id)
-        response = self.client.get(path)
+        path = reverse('sentry-api-0-group-tombstone',
+            kwargs={
+                'organization_slug': self.org.slug,
+                'project_slug': self.project.slug,
+            }
+        )
 
+        response = self.client.get(path)
         assert response.status_code == 200, response
         assert response.data[0]['message'] == group.message
-        assert response.data[0]['project']['name'] == project.name
+        assert response.data[0]['project']['name'] == self.project.name
