abb600a81a136df66a21fc1fc2c92f4f890ac85a
David Cramer <dcramer@gmail.com>
2017-07-06 13:38:36 -0700

    basic docstrings for tenant helpers

--- src/sentry/utils/tenants.py
+++ src/sentry/utils/tenants.py
@@ -11,6 +11,9 @@ class Tenant(object):
     def __init__(self, user_id=None):
         self.user_id = user_id
 
+    def __repr__(self):
+        return '<{} user_id={}>'.format(type(self).__name__, self.user_id)
+
     @classmethod
     def from_user(cls, user):
         if not user:
@@ -49,38 +52,76 @@ class Tenant(object):
 
 
 class UnrestrictedTenant(object):
+    """
+    An omnipotent tenant which has no restrictions on queries. This i
+    useful in async process or other always-actor-less code paths.
+    """
     organization_ids = ALL
     team_ids = ALL
     project_ids = ALL
+
+    def __repr__(self):
+        return '<{}>'.format(type(self).__name__)
 
 
 UnrestrictedTenant = UnrestrictedTenant()
 
 
 def get_current_user():
+    """
+    Retrieve the user from the current request
+
+    >>> get_current_user()
+    <User id=1>
+    """
     if not env.request:
         return None
     return env.request.user
 
 
-def get_tenant_from_request():
+def load_tenant_from_request():
+    """
+    Load the tenant for the current request.
+
+    >>> load_tenant_from_request()
+    <Tenant user_id=1L>
+    """
     user = get_current_user()
     return Tenant.from_user(user)
 
 
 def get_current_tenant():
+    """
+    Retrieve the current tenant, from the active request
+    if available.
+
+    >>> get_current_tenant()
+    <UnrestrictedTenant>
+    """
     rv = getattr(env, 'tenant', None)
     if rv is None:
-        rv = get_tenant_from_request()
+        rv = load_tenant_from_request()
         env.tenant = rv
     return rv
 
 
 def set_current_tenant(tenant):
+    """
+    Set the current tenant.
+
+    >>> set_current_tenant(UnrestrictedTenant)
+    """
     env.tenant = tenant
 
 
 class TenantContext(object):
+    """
+    Bind a tenant within a given context.
+
+    >>> with TenantContext(UnrestrictedTenant):
+    >>>   # I'm free!!!!!
+    """
+
     def __init__(self, tenant):
         self.tenant = tenant
 
