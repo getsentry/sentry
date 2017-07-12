4573d26085405da8e29914b58134ab1719b7af00
Eric Feng <ehfeng@users.noreply.github.com>
2017-07-12 15:33:43 -0700

    Remove IntegrityError exception logging (#5710)
    * Fixes #5709
    
    * removing unused variable

--- src/sentry/models/featureadoption.py
+++ src/sentry/models/featureadoption.py
@@ -175,10 +175,12 @@ class FeatureAdoptionManager(BaseManager):
                 self.bulk_create(features)
                 return True
 
-        except IntegrityError as e:
+        except IntegrityError:
             # This can occur if redis somehow loses the set of complete features and
             # we attempt to insert duplicate (org_id, feature_id) rows
-            logger.exception(e)
+            # This also will happen if we get parallel processes running `bulk_record` and
+            # `get_all_cache` returns in the second process before the first process
+            # can `bulk_set_cache`.
             return False
         finally:
             return self.bulk_set_cache(organization_id, *incomplete_feature_ids)
