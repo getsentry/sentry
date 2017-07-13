caffa655a5211f397a91ed64c5c5bd3c7fd2b596
Katie Lundsgaard <katie@getsentry.com>
2017-07-13 15:08:59 -0700

    Remove project from serializer and check for project on `GroupTombstoneDetailsEndpoint` before deleting

--- tests/sentry/api/endpoints/test_group_tombstone_details.py
+++ tests/sentry/api/endpoints/test_group_tombstone_details.py
@@ -51,3 +51,56 @@ class GroupTombstoneDetailsTest(APITestCase):
 
         assert response.status_code == 204, response
         assert not GroupHash.objects.filter(group_tombstone=tombstone).exists()
+
+    def test_dont_delete_from_other_proj(self):
+        self.user = self.create_user('foo@example.com')
+        self.org = self.create_organization(
+            owner=self.user,
+            name='Rowdy Tiger'
+        )
+        self.team = self.create_team(
+            organization=self.org,
+            name='Mariachi Band'
+        )
+        self.project = self.create_project(
+            organization=self.org,
+            team=self.team,
+            name='Bengal',
+        )
+
+        self.other_project = self.create_project(
+            organization=self.org,
+            team=self.team,
+            name='Snake',
+        )
+
+        self.login_as(user=self.user)
+
+        group = self.create_group()
+        tombstone = GroupTombstone.objects.create(
+            project_id=group.project_id,
+            level=group.level,
+            message=group.message,
+            culprit=group.culprit,
+            data=group.data,
+            previous_group_id=group.id,
+        )
+        GroupHash.objects.create(
+            project=group.project,
+            hash='x' * 32,
+            group=group,
+            group_tombstone=tombstone
+        )
+        assert GroupHash.objects.filter(group_tombstone=tombstone).exists()
+        path = reverse('sentry-api-0-group-tombstone-details',
+                       kwargs={
+                           'organization_slug': self.org.slug,
+                           'project_slug': self.other_project.slug,
+                           'tombstone_id': tombstone.id,
+                       }
+                       )
+        response = self.client.delete(path)
+
+        assert response.status_code == 204, response
+        assert GroupHash.objects.filter(group_tombstone=tombstone).exists()
+        assert GroupTombstone.objects.filter(project_id=self.project.id, id=tombstone.id).exists()
