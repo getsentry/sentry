3ee1868ef16bad71deb8112a1d2c4d2fbfe572df
Billy Vong <billyvg@gmail.com>
2017-07-14 13:31:15 -0700

    Add stories for Detailed Error

--- docs-ui/index.js
+++ docs-ui/index.js
@@ -2,3 +2,9 @@
 import 'jquery';
 import 'bootstrap/js/tooltip';
 import './less/sentry.less';
+
+import ConfigStore from '../src/sentry/static/sentry/app/stores/configStore';
+
+ConfigStore.loadInitialData({
+  gravatarBaseUrl: 'https://secure.gravatar.com'
+});
