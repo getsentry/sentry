37ee476ce4ea538a95c2e0ebcd2ba6f6f1c1bf9c
maxbittker <maxbittker@gmail.com>
2017-07-25 13:48:40 -0700

    Revert "remove lodash from bundle"
    This reverts commit a9d890f4d8d3a338d22b49f2ead9b8c888277700.

--- src/sentry/static/sentry/app/index.js
+++ src/sentry/static/sentry/app/index.js
@@ -44,6 +44,7 @@ import ReactDOM from 'react-dom';
 import {renderToStaticMarkup} from 'react-dom/server';
 import Reflux from 'reflux';
 import * as Router from 'react-router';
+import lodash from 'lodash';
 import ReactBootstrapModal from 'react-bootstrap/lib/Modal';
 
 import * as api from './api';
@@ -66,6 +67,7 @@ export default {
   },
   Reflux: Reflux,
   Router: Router,
+  underscore: lodash,
 
   Sentry: {
     api: api,
