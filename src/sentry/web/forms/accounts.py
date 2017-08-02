3674ee02a04f52d719b4bfccb8d0c0ee015e48f7
Max Bittker <maxbittker@gmail.com>
2017-08-02 15:53:47 -0700

    gmt -> utc (#5802)

--- src/sentry/web/forms/accounts.py
+++ src/sentry/web/forms/accounts.py
@@ -33,7 +33,7 @@ def _get_timezone_choices():
     for tz in pytz.common_timezones:
         now = datetime.now(pytz.timezone(tz))
         offset = now.strftime('%z')
-        results.append((int(offset), tz, '(GMT%s) %s' % (offset, tz)))
+        results.append((int(offset), tz, '(UTC%s) %s' % (offset, tz)))
     results.sort()
 
     for i in range(len(results)):
