c265dde3243e1144dcdfd657b85cf7615eeb1a83
Katie Lundsgaard <katie@getsentry.com>
2017-08-11 15:56:31 -0700

    Add raw toggle for additional data section

--- src/sentry/static/sentry/app/components/events/extraData.jsx
+++ src/sentry/static/sentry/app/components/events/extraData.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import PropTypes from '../../proptypes';
 import {objectToArray} from '../../utils';
+import ContextData from '../contextData';
 import EventDataSection from './eventDataSection';
 import KeyValueList from './interfaces/keyValueList';
 import {t} from '../../locale';
@@ -12,21 +13,66 @@ const EventExtraData = React.createClass({
     event: PropTypes.Event.isRequired
   },
 
+  getInitialState() {
+    return {
+      raw: false
+    };
+  },
+
   shouldComponentUpdate(nextProps, nextState) {
-    return this.props.event.id !== nextProps.event.id;
+    return this.props.event.id !== nextProps.event.id || this.state.raw !== nextState.raw;
+  },
+
+  toggleRaw(shouldBeRaw) {
+    this.setState({
+      raw: !this.state.raw
+    });
+  },
+
+  renderRaw() {
+    let extraRawData = JSON.stringify(this.props.event.context);
+    // extraRawData = [extraRawData];
+    return (
+      <table className="table key-value">
+        <tbody>
+          <tr>
+            <td className="value">
+              <ContextData data={extraRawData} />
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    );
+  },
+
+  renderPretty() {
+    let extraDataArray = objectToArray(this.props.event.context);
+    return <KeyValueList data={extraDataArray} isContextData={true} />;
   },
 
   render() {
-    let extraDataArray = objectToArray(this.props.event.context);
-
     return (
-      <EventDataSection
-        group={this.props.group}
-        event={this.props.event}
-        type="extra"
-        title={t('Additional Data')}>
-        <KeyValueList data={extraDataArray} isContextData={true} />
-      </EventDataSection>
+      <div>
+        <EventDataSection
+          group={this.props.group}
+          event={this.props.event}
+          type="extra"
+          title={t('Additional Data')}>
+          <div className="btn-group">
+            <a
+              className={(!this.state.raw ? 'active' : '') + ' btn btn-default btn-sm'}
+              onClick={this.toggleRaw}>
+              {t('Formatted')}
+            </a>
+            <a
+              className={(this.state.raw ? 'active' : '') + ' btn btn-default btn-sm'}
+              onClick={this.toggleRaw}>
+              {t('Raw')}
+            </a>
+          </div>
+          {this.state.raw ? this.renderRaw() : this.renderPretty()}
+        </EventDataSection>
+      </div>
     );
   }
 });
