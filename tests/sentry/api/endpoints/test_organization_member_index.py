211d44f2b6236f11e62e5672681fcddec1117b6c
Eric Feng <ehfeng@users.noreply.github.com>
2017-08-11 13:08:27 -0700

    adding email query to org member api (#5859)
    * adding email query to org member api
    
    * katie comments
    
    * added tests for UserEmail search

--- tests/sentry/api/endpoints/test_organization_member_index.py
+++ tests/sentry/api/endpoints/test_organization_member_index.py
@@ -6,25 +6,41 @@ from sentry.testutils import APITestCase
 
 
 class OrganizationMemberListTest(APITestCase):
-    def test_simple(self):
-        user_1 = self.create_user('foo@localhost', username='foo')
-        user_2 = self.create_user('bar@localhost', username='bar')
+    def setUp(self):
+        self.user_1 = self.create_user('foo@localhost', username='foo')
+        self.user_2 = self.create_user('bar@localhost', username='bar')
         self.create_user('baz@localhost', username='baz')
 
-        org = self.create_organization(owner=user_1)
-        org.member_set.create(user=user_2)
+        self.org = self.create_organization(owner=self.user_1)
+        self.org.member_set.create(user=self.user_2)
 
-        self.login_as(user=user_1)
+        self.login_as(user=self.user_1)
 
-        url = reverse(
+        self.url = reverse(
             'sentry-api-0-organization-member-index', kwargs={
-                'organization_slug': org.slug,
+                'organization_slug': self.org.slug,
             }
         )
 
-        response = self.client.get(url)
+    def test_simple(self):
+        response = self.client.get(self.url)
 
         assert response.status_code == 200
         assert len(response.data) == 2
-        assert response.data[0]['email'] == user_2.email
-        assert response.data[1]['email'] == user_1.email
+        assert response.data[0]['email'] == self.user_2.email
+        assert response.data[1]['email'] == self.user_1.email
+
+    def test_email_query(self):
+        response = self.client.get(self.url + "?query=email:foo@localhost")
+
+        assert response.status_code == 200
+        assert len(response.data) == 1
+        assert response.data[0]['email'] == self.user_1.email
+
+    def test_user_email_email_query(self):
+        self.create_useremail(self.user_1, 'baz@localhost')
+        response = self.client.get(self.url + "?query=email:baz@localhost")
+
+        assert response.status_code == 200
+        assert len(response.data) == 1
+        assert response.data[0]['email'] == self.user_1.email
