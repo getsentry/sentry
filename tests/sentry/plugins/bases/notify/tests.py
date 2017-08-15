89742db7537ddcad9d4bcc41216dd85de3073f8c
Eric Feng <ehfeng@users.noreply.github.com>
2017-08-15 11:53:35 -0700

    add plugin slug to urls (#5881)
    * add plugin slug to urls
    
    * matt comments
    
    * adding test
    
    * adding more cases
    
    * adding case where slug is blank

---
+++ tests/sentry/plugins/bases/notify/tests.py
@@ -0,0 +1,24 @@
+from __future__ import absolute_import
+
+from sentry.plugins import NotificationPlugin
+from sentry.testutils import TestCase
+
+
+class NotifyPlugin(TestCase):
+    def test_add_notification_referrer_param(self):
+        n = NotificationPlugin()
+        n.slug = 'slack'
+        url = 'https://sentry.io/'
+        assert n.add_notification_referrer_param(url) == url + '?referrer=' + n.slug
+
+        url = 'https://sentry.io/?referrer=notslack'
+        assert n.add_notification_referrer_param(url) == 'https://sentry.io/?referrer=slack'
+
+        url = 'https://sentry.io/?utm_source=google'
+        assert n.add_notification_referrer_param(
+            url
+        ) == 'https://sentry.io/?referrer=slack&utm_source=google'
+
+        n.slug = ''
+        url = 'https://sentry.io/'
+        assert n.add_notification_referrer_param(url) == 'https://sentry.io/'
