b20ce21d116b5a7a224bb4ed18c46cf091c1f786
Dena Mwangi <dena@getsentry.com>
2017-08-16 16:34:00 -0500

    Add schedule_jobs function (#5870)
    * Add function to make it easier to schedule jobs

--- tests/sentry/tasks/test_enqueue_scheduled_jobs.py
+++ tests/sentry/tasks/test_enqueue_scheduled_jobs.py
@@ -2,9 +2,13 @@ from __future__ import absolute_import, print_function
 
 from datetime import timedelta
 from django.utils import timezone
+from django.core.exceptions import ValidationError
 from mock import patch
 
+import pytest
+
 from sentry.models import ScheduledJob
+from sentry.models.scheduledjob import schedule_jobs
 from sentry.testutils import TestCase
 from sentry.tasks.scheduler import enqueue_scheduled_jobs
 
@@ -41,3 +45,38 @@ class EnqueueScheduledJobsTest(TestCase):
             'sentry.tasks.enqueue_scheduled_jobs',
             kwargs={'foo': 'bar'},
         )
+
+    def test_schedule_job(self):
+        job = [
+            (
+                {
+                    'foo': 'baz'
+                }, 'sentry.tasks.enqueue_scheduled_jobs', timezone.now() + timedelta(days=1)
+            ), (
+                {
+                    'foo': 'baz'
+                }, 'sentry.tasks.enqueue_scheduled_jobs_followup',
+                timezone.now() + timedelta(days=1)
+            )
+        ]
+        schedule_jobs(job)
+        assert set(
+            ScheduledJob.objects.filter(
+                payload={'foo': 'baz'},
+            ).values_list('name', flat=True)
+        ) == set(
+            ['sentry.tasks.enqueue_scheduled_jobs', 'sentry.tasks.enqueue_scheduled_jobs_followup']
+        )
+
+    def test_schedule_job_order(self):
+        with pytest.raises(
+            ValidationError, message="ValidationError raised. Check order of inputs"
+        ):
+            job = [
+                (
+                    'sentry.tasks.enqueue_scheduled_jobs', {
+                        'foo': 'baz'
+                    }, timezone.now() + timedelta(days=1)
+                )
+            ]
+            schedule_jobs(job)
