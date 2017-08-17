f4161bdfbdf8f588422142aa8f95ac66d5b3f629
Billy Vong <billyvg@gmail.com>
2017-08-16 18:34:38 -0700

    Add flow layout, add stories for split/spread layout

--- src/sentry/static/sentry/app/components/spreadLayout.jsx
+++ src/sentry/static/sentry/app/components/spreadLayout.jsx
@@ -4,8 +4,12 @@ import classNames from 'classnames';
 // Flexbox container whose children will have `justify-content: space-between`
 //
 // Intended for children.length == 2
-const SpreadLayout = ({children, className, ...props}) => {
-  const cx = classNames(className, 'spread-layout', {});
+// "responsive" will change flex-direction to be column on small widths
+const SpreadLayout = ({children, className, responsive, ...props}) => {
+  const cx = classNames('spread-layout', className, {
+    'allow-responsive': responsive
+  });
+
   return (
     <div {...props} className={cx}>
       {children}
@@ -16,7 +20,7 @@ const SpreadLayout = ({children, className, ...props}) => {
 SpreadLayout.propTypes = {
   children: PropTypes.node,
   style: PropTypes.object,
-  className: PropTypes.string
+  responsive: PropTypes.bool
 };
 
 export default SpreadLayout;
