e6081d94ace7bd4cf4e5c95ca6ee8498fadef8c4
Matt Robenolt <matt@ydekproductions.com>
2017-08-22 10:59:18 -0700

    db: add citext support

---
+++ src/sentry/db/models/fields/citext.py
@@ -0,0 +1,49 @@
+"""
+sentry.db.models.fields.citext
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+:copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
+:license: BSD, see LICENSE for more details.
+"""
+
+from __future__ import absolute_import, print_function
+
+import six
+
+from django.conf import settings
+from django.db import models
+
+__all__ = ('CITextField', 'CICharField', 'CIEmailField')
+
+
+class CIText(object):
+    def get_db_type(self, connection):
+        engine = connection.settings_dict['ENGINE']
+        if 'postgres' in engine:
+            return 'citext'
+        return super(CIText, self).db_type(connection)
+
+
+class CITextField(CIText, models.TextField):
+    pass
+
+
+class CICharField(CIText, models.CharField):
+    pass
+
+
+class CIEmailField(CIText, models.EmailField):
+    pass
+
+
+if hasattr(models, 'SubfieldBase'):
+    CITextField = six.add_metaclass(models.SubfieldBase)(CITextField)
+    CICharField = six.add_metaclass(models.SubfieldBase)(CICharField)
+    CIEmailField = six.add_metaclass(models.SubfieldBase)(CIEmailField)
+
+if 'south' in settings.INSTALLED_APPS:
+    from south.modelsinspector import add_introspection_rules
+
+    add_introspection_rules([], ["^sentry\.db\.models\.fields\.citext\.CITextField"])
+    add_introspection_rules([], ["^sentry\.db\.models\.fields\.citext\.CICharField"])
+    add_introspection_rules([], ["^sentry\.db\.models\.fields\.citext\.CIEmailField"])
