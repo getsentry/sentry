ce32cf931d6bc6630127aea8fbcbf194612380c9
Jess MacQueen <jess@getsentry.com>
2017-08-23 17:29:19 -0700

    clean up discarded group list UI

--- src/sentry/static/sentry/app/components/groupTombstones.jsx
+++ src/sentry/static/sentry/app/components/groupTombstones.jsx
@@ -1,60 +1,71 @@
 import React from 'react';
 
 import AlertActions from '../actions/alertActions';
-
+import Avatar from '../components/avatar';
+import EventOrGroupTitle from '../components/eventOrGroupTitle';
 import LoadingError from '../components/loadingError';
 import LinkWithConfirmation from '../components/linkWithConfirmation';
+import TooltipMixin from '../mixins/tooltip';
 
 import ApiMixin from '../mixins/apiMixin';
 
-import {t, tct} from '../locale';
+import {t} from '../locale';
 
 const GroupTombstoneRow = React.createClass({
   propTypes: {
     data: React.PropTypes.object.isRequired,
-    undiscard: React.PropTypes.func.isRequired
+    undiscard: React.PropTypes.func.isRequired,
+    orgId: React.PropTypes.string.isRequired,
+    projectId: React.PropTypes.string.isRequired
   },
+
+  mixins: [
+    TooltipMixin({
+      selector: '.tip'
+    })
+  ],
+
   render() {
-    let {data, undiscard} = this.props;
+    let {data, undiscard} = this.props, actor = data.actor;
+
     return (
       <li className={`group row level-${data.level} type-${data.type}`}>
-        <div className="col-md-7 col-xs-8 event-details">
-          <div>
+        <div className="col-md-10 event-details issue">
+          <div className="event-issue-header">
             <h3 className="truncate">
               <span className="error-level truncate" title={data.level}>
                 {data.level}
               </span>
-              <span>
-                <span style={{marginRight: 10}}>{data.type}</span>
-                <em>{data.culprit}</em><br />
-              </span>
+              <EventOrGroupTitle data={data} />
             </h3>
             <div className="event-message truncate">
               <span className="message">{data.message}</span>
             </div>
-            <div className="event-extra">
-
-              <LinkWithConfirmation
-                title={t('Undiscard')}
-                className="btn btn-warning btn-xs"
-                message={t(
-                  'Undiscarding this group means that ' +
-                    'incoming events that match this will no longer be discarded. ' +
-                    'New incoming events will count toward your event quota ' +
-                    'and will display on your issues dashboard. ' +
-                    'Are you sure you wish to continue?'
-                )}
-                onConfirm={() => {
-                  undiscard(data.id);
-                }}>
-                <span>{t('Undiscard')}</span>
-              </LinkWithConfirmation>
-              {data.actor &&
-                <span style={{marginLeft: 10}} className="event-message actor">
-                  {tct('discarded by [actor]', {actor: data.actor.name})}
-                </span>}
-            </div>
           </div>
+        </div>
+        <div className="col-md-1 event-actor">
+          {actor &&
+            <span className="tip" title={t('Discarded by %s', actor.name || actor.email)}>
+              <Avatar user={data.actor} />
+            </span>}
+        </div>
+        <div className="col-md-1 event-undiscard">
+          <span className="tip" title={t('Undiscard')}>
+            <LinkWithConfirmation
+              className="group-remove btn btn-default btn-sm"
+              message={t(
+                'Undiscarding this group means that ' +
+                  'incoming events that match this will no longer be discarded. ' +
+                  'New incoming events will count toward your event quota ' +
+                  'and will display on your issues dashboard. ' +
+                  'Are you sure you wish to continue?'
+              )}
+              onConfirm={() => {
+                undiscard(data.id);
+              }}>
+              <span className="icon-trash undiscard" />
+            </LinkWithConfirmation>
+          </span>
         </div>
       </li>
     );
@@ -100,11 +111,11 @@ const GroupTombstones = React.createClass({
   render() {
     if (this.props.tombstoneError) return <LoadingError />;
 
-    let {tombstones} = this.props;
+    let {tombstones, orgId, projectId} = this.props;
     return (
       <div>
         <div className="row" style={{paddingTop: 10}}>
-          <div className="col-sm-8">
+          <div className="col-md-12 discarded-groups">
             <h5>{t('Discarded Groups')}</h5>
             {tombstones.length
               ? <ul className="group-list">
@@ -113,6 +124,8 @@ const GroupTombstones = React.createClass({
                       <GroupTombstoneRow
                         key={data.id}
                         data={data}
+                        orgId={orgId}
+                        projectId={projectId}
                         undiscard={this.undiscard}
                       />
                     );
