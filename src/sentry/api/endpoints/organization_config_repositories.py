56c0d5e763836f4067f6cfa437210a388030a42f
Max Bittker <maxbittker@gmail.com>
2017-08-25 12:37:22 -0700

    remove bitbucket feature flag (#5873)
    * remove bitbucket feature flag
    
    * changelog

--- src/sentry/api/endpoints/organization_config_repositories.py
+++ src/sentry/api/endpoints/organization_config_repositories.py
@@ -4,7 +4,6 @@ from rest_framework.response import Response
 
 from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.plugins import bindings
-from sentry import features
 
 
 class OrganizationConfigRepositoriesEndpoint(OrganizationEndpoint):
@@ -14,16 +13,13 @@ class OrganizationConfigRepositoriesEndpoint(OrganizationEndpoint):
         providers = []
         for provider_id in provider_bindings:
             provider = provider_bindings.get(provider_id)(id=provider_id)
-            if provider_id != 'bitbucket' or features.has(
-                'organizations:bitbucket-repos', organization
-            ):
-                providers.append(
-                    {
-                        'id': provider_id,
-                        'name': provider.name,
-                        'config': provider.get_config(),
-                    }
-                )
+            providers.append(
+                {
+                    'id': provider_id,
+                    'name': provider.name,
+                    'config': provider.get_config(),
+                }
+            )
 
         return Response({
             'providers': providers,
