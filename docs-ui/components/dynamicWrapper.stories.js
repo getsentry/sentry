94c0a5a4316d11f31dbdd6b20258841927b0c157
Billy Vong <billyvg@gmail.com>
2017-08-25 09:39:47 -0700

    ui(component): <DynamicWrapper> for acceptance tests

---
+++ docs-ui/components/dynamicWrapper.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info';
+// import {action} from '@storybook/addon-actions';
+
+import DynamicWrapper from 'sentry-ui/dynamicWrapper';
+
+storiesOf('DynamicWrapper', module).add(
+  'default',
+  withInfo(
+    'Use this to wrap dynamic content (i.e. dates) for acceptance/snapshot tests. Currently checks for existance of PERCY_TOKEN env var'
+  )(() => <DynamicWrapper fixed="Fixed Content" value={new Date()} />)
+);
