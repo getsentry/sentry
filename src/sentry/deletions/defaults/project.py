846c73f4d1b7b189fd3a704e468ca49d4efccf9f
Brett Hoerner <brett@bretthoerner.com>
2017-09-01 15:47:36 -0500

    fix: Clean up DSym files when a Project is deleted.
    Fixes GH-5265

--- src/sentry/deletions/defaults/project.py
+++ src/sentry/deletions/defaults/project.py
@@ -42,7 +42,7 @@ class ProjectDeletionTask(ModelDeletionTask):
         # in bulk
         # Release needs to handle deletes after Group is cleaned up as the foreign
         # key is protected
-        model_list = (models.Group, models.ReleaseProject)
+        model_list = (models.Group, models.ReleaseProject, models.ProjectDSymFile)
         relations.extend(
             [ModelRelation(m, {'project_id': instance.id}, ModelDeletionTask) for m in model_list]
         )
