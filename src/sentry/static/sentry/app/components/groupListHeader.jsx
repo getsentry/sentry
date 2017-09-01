376ec1ca5269b5f717640b7f5d2c668c377cbbee
Evan Purkhiser <evanpurkhiser@gmail.com>
2017-09-01 15:57:10 -0700

    fix: Remove unneeded .nav-header from groupListHeader
    This class wasn't adding anything that wasn't already set aside from a
    bottom-margin that was breaking an items that flex below it.

--- src/sentry/static/sentry/app/components/groupListHeader.jsx
+++ src/sentry/static/sentry/app/components/groupListHeader.jsx
@@ -8,10 +8,10 @@ const GroupListHeader = React.createClass({
     return (
       <div className="group-header">
         <Toolbar className="stream-actions row">
-          <ToolbarHeader className="stream-actions-left col-md-7 col-sm-8 col-xs-8 nav-header">
+          <ToolbarHeader className="stream-actions-left col-md-7 col-sm-8 col-xs-8">
             {t('Event')}
           </ToolbarHeader>
-          <ToolbarHeader className="hidden-sm hidden-xs stream-actions-graph col-md-2 col-md-offset-1 align-right nav-header">
+          <ToolbarHeader className="hidden-sm hidden-xs stream-actions-graph col-md-2 col-md-offset-1 align-right">
             {t('Last 24 hours')}
           </ToolbarHeader>
           <ToolbarHeader className="stream-actions-count align-right col-md-1 col-sm-2 col-xs-2">
