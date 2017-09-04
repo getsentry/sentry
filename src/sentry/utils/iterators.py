66de040d63935901640f29683fa500ef2171c9f8
cclauss <cclauss@bluewin.ch>
2017-09-04 19:58:05 +0200

    Update iterators.py

--- src/sentry/utils/iterators.py
+++ src/sentry/utils/iterators.py
@@ -19,7 +19,7 @@ def shingle(n, iterator):
     return itertools.izip(
         *map(
             lambda i_and_iterator: advance(i_and_iterator[0], i_and_iterator[1]),
-            enumerate(itertools.tee(i_and_iterator[1], n)),
+            enumerate(itertools.tee(iterator, n)),
         )
     )
 
