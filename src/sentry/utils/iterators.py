0fd2d75e57b85b6f39e1ce19b6c57ae9725405c4
cclauss <cclauss@bluewin.ch>
2017-09-04 17:46:02 +0200

    Python 3 lambda cannot take explicit tuple param
    See #6030

--- src/sentry/utils/iterators.py
+++ src/sentry/utils/iterators.py
@@ -18,7 +18,7 @@ def shingle(n, iterator):
     """
     return itertools.izip(
         *map(
-            lambda (i_and_iterator): advance(i_and_iterator[0], i_and_iterator[1]),
+            lambda i_and_iterator: advance(i_and_iterator[0], i_and_iterator[1]),
             enumerate(itertools.tee(i_and_iterator[1], n)),
         )
     )
