8be4466375e673b87b84046c0bf4464d345752e5
Eric Feng <ehfeng@users.noreply.github.com>
2017-09-08 11:23:17 -0700

    Adding global email model (#5913)
    Adding Email model
    Adding post_save triggers on UserEmail to fill in Email
    Adding on_delete signal
    adding CIText to testing setup

--- src/sentry/db/models/fields/citext.py
+++ src/sentry/db/models/fields/citext.py
@@ -11,13 +11,15 @@ from __future__ import absolute_import, print_function
 import six
 
 from django.conf import settings
-from django.db import models
+from django.db import connections, models
+from django.db.models.signals import pre_syncdb
+
 
 __all__ = ('CITextField', 'CICharField', 'CIEmailField')
 
 
 class CIText(object):
-    def get_db_type(self, connection):
+    def db_type(self, connection):
         engine = connection.settings_dict['ENGINE']
         if 'postgres' in engine:
             return 'citext'
@@ -47,3 +49,19 @@ if 'south' in settings.INSTALLED_APPS:
     add_introspection_rules([], ["^sentry\.db\.models\.fields\.citext\.CITextField"])
     add_introspection_rules([], ["^sentry\.db\.models\.fields\.citext\.CICharField"])
     add_introspection_rules([], ["^sentry\.db\.models\.fields\.citext\.CIEmailField"])
+
+
+def create_citext_extension(db, **kwargs):
+    from sentry.utils.db import is_postgres
+
+    # We always need the citext extension installed for Postgres,
+    # and for tests, it's not always guaranteed that we will have
+    # run full migrations which installed it.
+    if is_postgres(db):
+        cursor = connections[db].cursor()
+        try:
+            cursor.execute('CREATE EXTENSION IF NOT EXISTS citext')
+        except Exception:
+            pass
+
+pre_syncdb.connect(create_citext_extension)
