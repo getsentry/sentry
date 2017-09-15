99a0f328759d439e8146ce12b5b7afb6a6143dc1
David Cramer <dcramer@gmail.com>
2017-09-15 13:08:43 -0700

    fix(plugins): Various fixes for repository providers
    This fixes some behavior as noticed in VSTS:
    
    - Repository config is no longer hardcoded to 'name'
    - Internal errors are handled much better
    - Propgation of changes is more correct

--- tests/js/setup.js
+++ tests/js/setup.js
@@ -20,8 +20,13 @@ window.TestStubs = {
     isActive: sinon.spy(),
     createHref: sinon.spy()
   }),
+  location: () => ({
+    query: {},
+    pathame: '/mock-pathname/'
+  }),
   Team: (...params) => {
     return {
+      id: '1',
       slug: 'team-slug',
       name: 'Team Name',
       ...params
@@ -29,6 +34,7 @@ window.TestStubs = {
   },
   Project: (...params) => {
     return {
+      id: '2',
       slug: 'project-slug',
       name: 'Project Name',
       subjectTemplate: '[$project] ${tag:level}: $title',
@@ -39,8 +45,50 @@ window.TestStubs = {
   },
   Organization: (...params) => {
     return {
+      id: '3',
       slug: 'org-slug',
       name: 'Organization Name',
+      access: [
+        'org:read',
+        'org:write',
+        'org:admin',
+        'project:read',
+        'project:write',
+        'project:admin',
+        'team:read',
+        'team:write',
+        'team:admin'
+      ],
+      features: [],
+      onboardingTasks: [],
+      teams: [],
+      ...params
+    };
+  },
+  Repository: (...params) => {
+    return {
+      id: '4',
+      name: 'repo-name',
+      provider: 'github',
+      url: 'https://github.com/example/repo-name',
+      status: 'visible',
+      ...params
+    };
+  },
+  GitHubRepositoryProvider: (...params) => {
+    return {
+      id: 'github',
+      name: 'GitHub',
+      config: [
+        {
+          name: 'name',
+          label: 'Repository Name',
+          type: 'text',
+          placeholder: 'e.g. getsentry/sentry',
+          help: 'Enter your repository name, including the owner.',
+          required: true
+        }
+      ],
       ...params
     };
   }
