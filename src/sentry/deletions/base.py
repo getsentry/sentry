51412c53bb8399d0fa9ab684b7e8c2eb08aeff67
Brett Hoerner <brett@bretthoerner.com>
2017-09-18 15:38:42 -0500

    fix: Skip some double deletions during cleanup (#6143)

--- src/sentry/deletions/base.py
+++ src/sentry/deletions/base.py
@@ -29,15 +29,17 @@ class BaseDeletionTask(object):
 
     DEFAULT_CHUNK_SIZE = 100
 
-    def __init__(self, manager, transaction_id=None, actor_id=None, chunk_size=DEFAULT_CHUNK_SIZE):
+    def __init__(self, manager, skip_models=None, transaction_id=None,
+                 actor_id=None, chunk_size=DEFAULT_CHUNK_SIZE):
         self.manager = manager
+        self.skip_models = set(skip_models) if skip_models else None
         self.transaction_id = transaction_id
         self.actor_id = actor_id
         self.chunk_size = chunk_size
 
     def __repr__(self):
-        return '<%s: transaction_id=%s actor_id=%s>' % (
-            type(self), self.transaction_id, self.actor_id,
+        return '<%s: skip_models=%s transaction_id=%s actor_id=%s>' % (
+            type(self), self.skip_models, self.transaction_id, self.actor_id,
         )
 
     def chunk(self):
@@ -58,6 +60,13 @@ class BaseDeletionTask(object):
             # ModelRelation(Model, {'parent_id__in': [i.id for id in instance_list]})
         ]
 
+    def filter_relations(self, child_relations):
+        if not self.skip_models or not child_relations:
+            return child_relations
+
+        return list(filter(lambda rel: rel.params.get('model')
+                           not in self.skip_models, child_relations))
+
     def delete_bulk(self, instance_list):
         """
         Delete a batch of objects bound to this task.
@@ -68,6 +77,7 @@ class BaseDeletionTask(object):
         self.mark_deletion_in_progress(instance_list)
 
         child_relations = self.get_child_relations_bulk(instance_list)
+        child_relations = self.filter_relations(child_relations)
         if child_relations:
             has_more = self.delete_children(child_relations)
             if has_more:
@@ -75,6 +85,7 @@ class BaseDeletionTask(object):
 
         for instance in instance_list:
             child_relations = self.get_child_relations(instance)
+            child_relations = self.filter_relations(child_relations)
             if child_relations:
                 has_more = self.delete_children(child_relations)
                 if has_more:
