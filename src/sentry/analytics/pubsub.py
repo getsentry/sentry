0b610cadf0f1a57297f7631ff64188701625c72e
Matt Robenolt <matt@ydekproductions.com>
2017-09-19 13:38:35 -0700

    Match default BatchSettings
    These defaults seem plenty reasonable.

--- src/sentry/analytics/pubsub.py
+++ src/sentry/analytics/pubsub.py
@@ -9,10 +9,12 @@ from .base import Analytics
 
 
 class PubSubAnalytics(Analytics):
-    def __init__(self, project, topic, batch_max_bytes=1024 * 100, batch_max_latency=1):
+    def __init__(self, project, topic, batch_max_bytes=1024 * 1024 *
+                 5, batch_max_latency=0.05, batch_max_messages=1000):
         settings = pubsub_v1.types.BatchSettings(
             max_bytes=batch_max_bytes,
             max_latency=batch_max_latency,
+            max_messages=batch_max_messages,
         )
         self.publisher = pubsub_v1.PublisherClient(settings)
         self.topic = self.publisher.topic_path(project, topic)
