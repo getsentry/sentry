7bc9ccbf780fa23629536ea4522981f6f0338ace
Daniel Griesser <daniel.griesser.86@gmail.com>
2017-09-26 15:43:59 +0200

    fix(api): Allow signed links to pass through SSO (#6050)
    * fix(api): Allow signed links to pass through SSO
    
    Fixes GH-6044
    
    * fix(api): Add tests for skipping sso with signed link
    
    * ref(api): Refactor user_signed_link check for readablity

--- src/sentry/api/bases/organization.py
+++ src/sentry/api/bases/organization.py
@@ -45,8 +45,19 @@ class OrganizationPermission(ScopedPermission):
 
         else:
             request.access = access.from_request(request, organization)
-            # session auth needs to confirm various permissions
-            if request.user.is_authenticated() and self.needs_sso(request, organization):
+
+            if auth.is_user_signed_request(request):
+                # if the user comes from a signed request
+                # we let him pass if sso is enabled
+                logger.info(
+                    'access.signed-sso-passthrough',
+                    extra={
+                        'organization_id': organization.id,
+                        'user_id': request.user.id,
+                    }
+                )
+            elif request.user.is_authenticated() and self.needs_sso(request, organization):
+                # session auth needs to confirm various permissions
                 logger.info(
                     'access.must-sso',
                     extra={
