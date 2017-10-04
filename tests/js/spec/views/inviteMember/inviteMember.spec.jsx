1b6fd1ff91caf5a5fdf930117a7640b2dcbad3df
maxbittker <maxbittker@gmail.com>
2017-10-03 17:05:12 -0700

    update snaps

--- tests/js/spec/views/inviteMember/inviteMember.spec.jsx
+++ tests/js/spec/views/inviteMember/inviteMember.spec.jsx
@@ -3,17 +3,19 @@ import {shallow, mount} from 'enzyme';
 
 import InviteMember from 'app/views/inviteMember/inviteMember';
 import {Client} from 'app/api';
+import ConfigStore from 'app/stores/configStore';
 
 jest.mock('app/api');
 
 describe('CreateProject', function() {
   beforeEach(function() {
     this.sandbox = sinon.sandbox.create();
-    this.stubbedApiRequest = this.sandbox.stub(Client.prototype, 'request');
+    this.sandbox.stub(ConfigStore, 'get').returns({id: 1});
   });
 
   afterEach(function() {
     this.sandbox.restore();
+    Client.clearMockResponses();
   });
 
   describe('render()', function() {
@@ -23,56 +25,81 @@ describe('CreateProject', function() {
       }
     };
 
-    it('should render', function() {
-      let props = {
-        ...baseProps
-      };
-
-      let wrapper = shallow(<InviteMember {...props} />, {
-        context: {
-          organization: {
-            id: '1',
-            slug: 'testOrg',
-            teams: [{slug: 'test', id: '1', name: 'test', hasAccess: false}]
-          },
-          location: {query: {}}
+    const baseContext = {
+      context: {
+        organization: {
+          id: '1',
+          slug: 'testOrg',
+          teams: [{slug: 'test', id: '1', name: 'test', hasAccess: false}]
         },
-        childContextTypes: {
-          organization: React.PropTypes.object,
-          location: React.PropTypes.object
-        }
-      });
+        router: TestStubs.router(),
+        location: {query: {}}
+      },
+      childContextTypes: {
+        organization: React.PropTypes.object,
+        location: React.PropTypes.object,
+        router: React.PropTypes.object
+      }
+    };
+
+    it('should render', function() {
+      let wrapper = shallow(<InviteMember {...baseProps} />, baseContext);
       expect(wrapper).toMatchSnapshot();
     });
 
-    it('should redirect on zero teams', function() {
+    it('should redirect when no roles available', function() {
       Client.addMockResponse({
-        url: '/organizations/test/members/',
+        url: '/organizations/testOrg/members/1/',
         body: {
-          entries: 'test'
+          role_list: [
+            {
+              role: {
+                id: 1,
+                name: 'member',
+                desc: 'a normal member'
+              },
+              allowed: false
+            }
+          ]
         }
       });
 
-      let props = {
-        ...baseProps
-      };
+      let handleSubmitStub = this.sandbox.stub(
+        InviteMember.prototype,
+        'redirectToMemberPage'
+      );
 
-      let wrapper = mount(<InviteMember {...props} />, {
-        context: {
-          organization: {
-            id: '1',
-            slug: 'testOrg',
-            teams: [{slug: 'test', id: '1', name: 'test', hasAccess: false}]
-          },
-          router: TestStubs.router(),
-          location: {query: {}}
-        },
-        childContextTypes: {
-          organization: React.PropTypes.object,
-          location: React.PropTypes.object,
-          router: React.PropTypes.object
+      // 👺 ⚠️ this is a hack to defeat the method auto binding so we can fully stub the method. It would not be neccessary with es6 class components and it relies on react internals so it's fragile - maxbittker
+      const index =
+        InviteMember.prototype.__reactAutoBindPairs.indexOf('redirectToMemberPage') + 1;
+      InviteMember.prototype.__reactAutoBindPairs[index] = handleSubmitStub;
+
+      let wrapper = mount(<InviteMember {...baseProps} />, baseContext);
+
+      expect(handleSubmitStub.callCount).toEqual(1);
+      expect(wrapper.state('loading')).toBe(false);
+    });
+
+    it('should render roles when available and allowed', function() {
+      Client.addMockResponse({
+        url: '/organizations/testOrg/members/1/',
+        body: {
+          role_list: [
+            {
+              role: {
+                id: 1,
+                name: 'member',
+                desc: 'a normal member'
+              },
+              allowed: true
+            }
+          ]
         }
       });
+
+      let wrapper = mount(<InviteMember {...baseProps} />, baseContext);
+
+      expect(wrapper.state('loading')).toBe(false);
       expect(wrapper).toMatchSnapshot();
     });
   });
