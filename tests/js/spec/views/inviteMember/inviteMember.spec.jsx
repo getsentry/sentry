f1c6be2d15e32fbc23cf1d088385334aa6b1cc4a
maxbittker <maxbittker@gmail.com>
2017-10-04 18:02:00 -0700

    component tests

--- tests/js/spec/views/inviteMember/inviteMember.spec.jsx
+++ tests/js/spec/views/inviteMember/inviteMember.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {shallow, mount} from 'enzyme';
-
+import _ from 'lodash';
 import InviteMember from 'app/views/inviteMember/inviteMember';
 import {Client} from 'app/api';
 import ConfigStore from 'app/stores/configStore';
@@ -11,11 +11,11 @@ describe('CreateProject', function() {
   beforeEach(function() {
     this.sandbox = sinon.sandbox.create();
     this.sandbox.stub(ConfigStore, 'get').returns({id: 1});
+    Client.clearMockResponses();
   });
 
   afterEach(function() {
     this.sandbox.restore();
-    Client.clearMockResponses();
   });
 
   describe('render()', function() {
@@ -30,7 +30,10 @@ describe('CreateProject', function() {
         organization: {
           id: '1',
           slug: 'testOrg',
-          teams: [{slug: 'test', id: '1', name: 'test', hasAccess: false}]
+          teams: [
+            {slug: 'bar', id: '1', name: 'bar', hasAccess: true},
+            {slug: 'foo', id: '2', name: 'foo', hasAccess: false}
+          ]
         },
         router: TestStubs.router(),
         location: {query: {}}
@@ -44,6 +47,16 @@ describe('CreateProject', function() {
 
     it('should render', function() {
       let wrapper = shallow(<InviteMember {...baseProps} />, baseContext);
+      expect(wrapper).toMatchSnapshot();
+    });
+
+    it('should render no team select when there is only one option', function() {
+      let context = _.cloneDeep(baseContext);
+
+      context.context.organization.teams = context.context.organization.teams.slice(0, 1);
+
+      let wrapper = shallow(<InviteMember {...baseProps} />, context);
+
       expect(wrapper).toMatchSnapshot();
     });
 
@@ -68,7 +81,6 @@ describe('CreateProject', function() {
         InviteMember.prototype,
         'redirectToMemberPage'
       );
-
       // 👺 ⚠️ this is a hack to defeat the method auto binding so we can fully stub the method. It would not be neccessary with es6 class components and it relies on react internals so it's fragile - maxbittker
       const index =
         InviteMember.prototype.__reactAutoBindPairs.indexOf('redirectToMemberPage') + 1;
@@ -80,27 +92,60 @@ describe('CreateProject', function() {
       expect(wrapper.state('loading')).toBe(false);
     });
 
-    it('should render roles when available and allowed', function() {
+    it('should render roles when available and allowed, and handle submitting', function(
+      done
+    ) {
       Client.addMockResponse({
         url: '/organizations/testOrg/members/1/',
         body: {
           role_list: [
             {
-              role: {
-                id: 1,
-                name: 'member',
-                desc: 'a normal member'
-              },
+              role: {id: 1, name: 'member', desc: 'a normal member'},
+              allowed: true
+            },
+            {
+              role: {id: 2, name: 'bar', desc: 'another role'},
               allowed: true
             }
           ]
         }
       });
 
-      let wrapper = mount(<InviteMember {...baseProps} />, baseContext);
+      Client.addMockResponse({
+        url: '/organizations/testOrg/members/',
+        method: 'POST',
+        statusCode: 200,
+        body: {}
+      });
+
+      let wrapper;
+
+      // 👺 ⚠️ this is a hack to defeat the method auto binding so we can fully stub the method. It would not be neccessary with es6 class components and it relies on react internals so it's fragile - maxbittker
+      const index =
+        InviteMember.prototype.__reactAutoBindPairs.indexOf('redirectToMemberPage') + 1;
+      InviteMember.prototype.__reactAutoBindPairs[index] = () => {
+        expect(wrapper.state('loading')).toBe(true);
+        done();
+      };
+
+      wrapper = mount(<InviteMember {...baseProps} />, baseContext);
 
       expect(wrapper.state('loading')).toBe(false);
+
+      let node = wrapper.find('.radio').first();
+      node.props().onClick();
+
+      node = wrapper.find('.team-choices > div').first();
+      node.props().onClick({preventDefault: () => {}});
+
       expect(wrapper).toMatchSnapshot();
+
+      node = wrapper.find('.submit-new-team').first();
+      node.props().onClick({preventDefault: () => {}});
+      expect(wrapper.state('loading')).toBe(false);
+
+      wrapper.setState({email: 'test@email.com'});
+      node.props().onClick({preventDefault: () => {}});
     });
   });
 });
