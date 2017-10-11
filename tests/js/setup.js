97a2d361089ff829b8d2575d855d182ce60d727a
Billy Vong <billy@sentry.io>
2017-10-10 17:11:48 -0700

    feat(react): Add react views for Org Auth Provider

--- tests/js/setup.js
+++ tests/js/setup.js
@@ -43,6 +43,25 @@ window.TestStubs = {
     };
   },
 
+  AuthProviders: () => {
+    return [['dummy', 'Dummy']];
+  },
+
+  AuthProvider: () => {
+    return {
+      auth_provider: {
+        id: '1',
+        provider: 'dummy'
+      },
+      require_link: true,
+      default_role: 'member',
+      login_url: 'http://loginUrl',
+      provider_name: 'dummy',
+      pending_links_count: 0,
+      content: ''
+    };
+  },
+
   Team: (...params) => {
     return {
       id: '1',
