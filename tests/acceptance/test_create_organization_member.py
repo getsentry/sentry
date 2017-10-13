0855b18381bc83576c1d177e2642fd5151c8a60d
maxbittker <maxbittker@gmail.com>
2017-10-13 14:58:31 -0700

    test

--- tests/acceptance/test_create_organization_member.py
+++ tests/acceptance/test_create_organization_member.py
@@ -39,6 +39,6 @@ class CreateOrganizationMemberTest(AcceptanceTestCase):
         self.browser.snapshot(name='invite organization member')
         self.browser.click('.submit-new-team')
 
+        self.browser.wait_until('.alert-success')
         self.browser.wait_until_not('.loading')
         assert self.browser.element_exists('.member-list')
-        assert self.browser.element_exists('.alert-success')
