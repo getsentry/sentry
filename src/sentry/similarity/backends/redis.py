972c698dcde842eee4efea998c30eb474a6a2f9a
Ted Kaemming <ted@kaemming.com>
2017-10-16 18:10:16 -0700

    don't read this

--- src/sentry/similarity/backends/redis.py
+++ src/sentry/similarity/backends/redis.py
@@ -1,23 +1,52 @@
 from __future__ import absolute_import
 
+import functools
 import itertools
+import struct
 import time
+from collections import Counter, defaultdict
 
 from sentry.similarity.backends.abstract import AbstractIndexBackend
 from sentry.utils.iterators import chunked
+from sentry.utils.functional import apply_values
 from sentry.utils.redis import load_script, with_future_responses
 
 
 index = load_script('similarity/index.lua')
 
 
-def band(n, value):
+def banded(n, value):
     assert len(value) % n == 0
     return list(chunked(value, len(value) / n))
 
 
 def flatten(value):
     return list(itertools.chain.from_iterable(value))
+
+
+def avg(values):
+    return sum(values) / float(len(values))
+
+
+def scale_to_total(values):
+    total = float(sum(values))
+    return map(lambda value: value / total, values)
+
+
+def get_manhattan_distance(this, that):
+    return sum(abs(this.get(key, 0) - that.get(key, 0)) for key in set(this).union(that))
+
+
+def get_similarity(this, that):
+    rescale = functools.partial(map, functools.partial(apply_values, scale_to_total))
+    this = rescale(this)
+    that = rescale(that)
+    return avg(
+        map(
+            lambda (this, that): 1 - (get_manhattan_distance(this, that) / 2),
+            zip(this, that)
+        )
+    )
 
 
 class RedisMinHashIndexBackend(AbstractIndexBackend):
@@ -31,30 +60,141 @@ class RedisMinHashIndexBackend(AbstractIndexBackend):
         self.retention = retention
         self.candidate_set_limit = candidate_set_limit
 
-    def __get_active_time_series(self, timestamp, function):
-        raise NotImplementedError
+        self.__frequency_key_structs = (
+            struct.Struct('>B'),
+            struct.Struct('>' + ('H' * int(self.signature_builder.columns / self.bands))),
+        )
 
-    def __get_active_time_series_range(self, timestamp, function):
-        raise NotImplementedError
+    def __pack_frequency_key(self, band, bucket):
+        band_struct, bucket_struct = self.__frequency_key_structs
+        return ''.join([
+            band_struct.pack(band),
+            bucket_struct.pack(*bucket),
+        ])
 
-    def __get_membership_key(self, index, interval, band, bucket):
-        raise NotImplementedError
+    def __unpack_frequency_key(self, key):
+        band_struct, bucket_struct = self.__frequency_key_structs
+        band, = band_struct.unpack_from(key)
+        bucket = bucket_struct.unpack_from(key, band_struct.size)
+        return band, bucket
 
-    def __get_frequency_key(self, index, item):
-        raise NotImplementedError
+    def __convert_frequency_response(self, response):
+        result = [{} for _ in range(self.bands)]
+        for key, value in response.result().items():
+            band, bucket = self.__unpack_frequency_key(key)
+            result[band][bucket] = int(value)
+        return result
 
-    def __search(self, scope):
+    def __search(self, scope, parameters, timestamp, limit):
+        time_series = range(
+            int(timestamp / self.interval),
+            int(timestamp / self.interval) + self.retention + 1,
+        )
+
+        def fetch_candidates(pipeline, (index, frequencies, threshold)):
+            bands = [[] for _ in range(self.bands)]
+            for band, buckets in enumerate(frequencies):
+                for bucket in buckets.keys():
+                    for time_index in time_series:
+                        bands[band].append(
+                            pipeline.smembers(
+                                '{namespace}:{{{scope}}}:{index}:m:{time}:{coordinates}'.format(
+                                    namespace=self.namespace,
+                                    scope=scope,
+                                    index=index,
+                                    time=time_index,
+                                    coordinates=self.__pack_frequency_key(band, bucket),
+                                )
+                            )
+                        )
+            return bands
+
+        def collect_candidates((index, frequencies, threshold), results):
+            candidates = Counter()
+            for band, responses in enumerate(results):
+                candidates.update(
+                    reduce(
+                        lambda candidates, response: candidates | set(response.result()),
+                        responses,
+                        set(),
+                    )
+                )
+            return candidates.most_common(limit)
+
+        def combine_candidates(candidates):
+            results = defaultdict(list)
+            for i, candidates in enumerate(candidates):
+                for key, hits in candidates:
+                    results[key].append(hits)
+
+            return [key for key, hits in sorted(
+                results.items(),
+                key=lambda (key, hits): (
+                    sum(hits) / len(hits),
+                    len(hits),
+                    key,
+                ),
+            )[:limit]]
+
+        def fetch_frequencies(pipeline, parameters, keys):
+            results = []
+            for key in keys:
+                results.append([
+                    pipeline.hgetall(
+                        '{namespace}:{{{scope}}}:{index}:f:{key}'.format(
+                            namespace=self.namespace,
+                            scope=scope,
+                            index=index,
+                            key=key,
+                        )
+                    ) for (index, frequencies, threshold) in parameters
+                ])
+            return results
+
+        def calculate_similarity(parameters, frequencies):
+            return map(
+                lambda ((index, source_frequencies, threshold), candidate_frequencies): get_similarity(
+                    source_frequencies,
+                    candidate_frequencies,
+                ),
+                zip(
+                    parameters,
+                    map(self.__convert_frequency_response, frequencies),
+                ),
+            )
+
         with with_future_responses(self.cluster.pipeline()) as pipeline:
-            # fetch candidates for all indices
+            results = map(
+                functools.partial(fetch_candidates, pipeline),
+                parameters,
+            )
             pipeline.execute()
 
-        # sort and truncate candidates based on limit
+        candidates = combine_candidates(
+            map(
+                collect_candidates,
+                parameters,
+                results,
+            ),
+        )
 
         with with_future_responses(self.cluster.pipeline()) as pipeline:
-            # fetch candidate data for all indices in parameters
+            frequencies = fetch_frequencies(pipeline, parameters, candidates)
             pipeline.execute()
 
-        # sort similar items
+        return sorted(
+            zip(
+                candidates,
+                map(
+                    functools.partial(calculate_similarity, parameters),
+                    frequencies,
+                ),
+            ),
+            key=lambda (key, frequencies): (
+                sum(frequencies) * -1,
+                key,
+            ),
+        )[:limit]
 
     def classify(self, scope, items, limit=None, timestamp=None):
         if timestamp is None:
@@ -68,13 +208,29 @@ class RedisMinHashIndexBackend(AbstractIndexBackend):
         if timestamp is None:
             timestamp = int(time.time())
 
-        raise NotImplementedError
-
         with with_future_responses(self.cluster.pipeline()) as pipeline:
-            # fetch data
+            results = [
+                pipeline.hgetall(
+                    '{namespace}:{{{scope}}}:{index}:f:{key}'.format(
+                        namespace=self.namespace,
+                        scope=scope,
+                        index=index,
+                        key=key,
+                    )
+                ) for (index, threshold) in items
+            ]
             pipeline.execute()
 
-        return self.__search(scope)
+        return self.__search(
+            scope,
+            [
+                (index, self.__convert_frequency_response(result), threshold)
+                for (index, threshold), result in
+                zip(items, results)
+            ],
+            timestamp,
+            limit,
+        )
 
     def record(self, scope, key, items, timestamp=None):
         if not items:
@@ -83,10 +239,42 @@ class RedisMinHashIndexBackend(AbstractIndexBackend):
         if timestamp is None:
             timestamp = int(time.time())
 
-        raise NotImplementedError
-
         with with_future_responses(self.cluster.pipeline()) as pipeline:
-            # write data
+            for index, features in items:
+                signature = list(self.signature_builder(features))
+                key_prefix = '{namespace}:{{{scope}}}:{index}'.format(
+                    namespace=self.namespace,
+                    scope=scope,
+                    index=index,
+                )
+                for band, bucket in enumerate(banded(self.bands, signature)):
+                    frequency_key = '{}:f:{}'.format(key_prefix, key)
+                    pipeline.hincrby(
+                        frequency_key,
+                        self.__pack_frequency_key(band, bucket),
+                        1,
+                    )
+                    pipeline.expireat(
+                        frequency_key,
+                        timestamp + (self.retention * self.interval),
+                    )
+
+                    time_series = range(
+                        int(timestamp / self.interval),
+                        int(timestamp / self.interval) + self.retention + 1,
+                    )
+                    for time_index in time_series:
+                        membership_set_key = '{}:m:{}:{}'.format(
+                            key_prefix,
+                            time_index,
+                            self.__pack_frequency_key(band, bucket),
+                        )
+                        pipeline.sadd(membership_set_key, key)
+                        pipeline.expireat(
+                            membership_set_key,
+                            time_index * self.interval + (self.retention * self.interval),
+                        )
+
             pipeline.execute()
 
     def merge(self, scope, destination, items, timestamp=None):
@@ -139,7 +327,7 @@ class RedisScriptMinHashIndexBackend(AbstractIndexBackend):
             return [0] * self.bands
 
         arguments = []
-        for bucket in band(self.bands, self.signature_builder(features)):
+        for bucket in banded(self.bands, self.signature_builder(features)):
             arguments.extend([1, ','.join(map('{}'.format, bucket)), 1])
         return arguments
 
