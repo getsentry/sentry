e552868b717d5ab89daba239502af826d8ca1c3a
Armin Ronacher <armin.ronacher@active-4.com>
2017-10-18 17:14:21 +0200

    tests(javascript): Expanded the test for source caches

--- tests/sentry/lang/javascript/test_cache.py
+++ tests/sentry/lang/javascript/test_cache.py
@@ -21,3 +21,20 @@ class BasicCacheTest(TestCase):
         cache.alias(url + 'x', url)
         assert url + 'x' in cache
         assert cache.get(url + 'x')[0] == u'foo'
+
+    def test_encoding_fallback(self):
+        cache = SourceCache()
+
+        url = 'http://example.com/foo.js'
+
+        # fall back to utf-8
+        cache.add(url, b'foobar', encoding='utf-32')
+        assert cache.get(url)[0] == u'foobar'
+
+    def test_encoding_support(self):
+        cache = SourceCache()
+        url = 'http://example.com/foo.js'
+
+        # fall back to utf-8
+        cache.add(url, 'foobar'.encode('utf-32'), encoding='utf-32')
+        assert cache.get(url)[0] == u'foobar'
