3a3c25697883c415d0e780c335e619e4ce450227
denamwangi <dena@getsentry.com>
2017-10-23 18:18:15 -0700

    Add extra split for emails with periods between names

--- src/sentry/models/user.py
+++ src/sentry/models/user.py
@@ -139,7 +139,7 @@ class User(BaseModel, AbstractBaseUser):
         return self.username
 
     def get_salutation_name(self):
-        name = self.name or self.username.split('@', 1)[0]
+        name = self.name or self.username.split('@', 1)[0].split('.', 1)[0]
         first_name = name.split(' ', 1)[0]
         return first_name.capitalize()
 
