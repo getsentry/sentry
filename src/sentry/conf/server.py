5d2ea014c9fafdff7def9e99da3ffd1dbb5dea4c
maxbittker <maxbittker@gmail.com>
2017-10-24 14:13:32 -0700

    defalt-flag

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -725,7 +725,7 @@ SENTRY_FEATURES = {
     'organizations:sso-rippling': False,
     'organizations:group-unmerge': False,
     'organizations:integrations-v3': False,
-    'organizations:invite-member': True,
+    'organizations:invite-members': True,
     'projects:global-events': False,
     'projects:plugins': True,
     'projects:dsym': False,
