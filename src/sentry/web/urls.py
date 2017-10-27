17a0ca7993b1e4a865000b2d8904b1ff11e88dc7
alex-hofsteede <alex@hofsteede.com>
2017-10-27 09:13:07 -0600

    feat(schemas): Add HTTP endpoint to get API schema
    Return the current schema for the /api/store endpoint at
    /api/store/schema

--- src/sentry/web/urls.py
+++ src/sentry/web/urls.py
@@ -120,6 +120,7 @@ urlpatterns += patterns(
         api.crossdomain_xml,
         name='sentry-api-crossdomain-xml'
     ),
+    url(r'^api/store/schema$', api.StoreSchemaView.as_view(), name='sentry-api-store-schema'),
 
     # The static version is either a 10 digit timestamp, a sha1, or md5 hash
     url(
