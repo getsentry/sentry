a719ffd6d8d08d2a1ed0308884ff9b5bd8afba24
cclauss <cclauss@bluewin.ch>
2017-11-17 00:06:07 +0100

    Fix lambda expressions for Python 3 (#6569)

--- src/sentry/rules/conditions/event_frequency.py
+++ src/sentry/rules/conditions/event_frequency.py
@@ -29,7 +29,7 @@ class EventFrequencyForm(forms.Form):
         choices=[
             (key, label)
             for key, (label, duration
-                      ) in sorted(intervals.items(), key=lambda (key, (label, duration)): duration)
+                      ) in sorted(intervals.items(), key=lambda key____label__duration: key____label__duration[1][1])
         ]
     )
     value = forms.IntegerField(
