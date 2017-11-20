ff1b18d1d1410824b589b99b54e55dc119ec5edd
Eric Feng <erichfeng@gmail.com>
2017-11-20 13:37:46 -0800

    fixing import and max's suggestion

--- src/sentry/static/sentry/app/views/onboarding/configure/index.jsx
+++ src/sentry/static/sentry/app/views/onboarding/configure/index.jsx
@@ -7,7 +7,7 @@ import ApiMixin from '../../../mixins/apiMixin';
 import ProjectContext from '../../projects/projectContext';
 import ProjectDocsContext from '../../projectInstall/docsContext';
 import ProjectInstallPlatform from '../../projectInstall/platform';
-import HookStore from '../../../hookStore';
+import HookStore from '../../../stores/hookStore';
 
 const Configure = React.createClass({
   mixins: [ApiMixin],
@@ -84,9 +84,7 @@ const Configure = React.createClass({
 
   submit() {
     this.redirectUrl();
-    HookStore.get('analytics:onboarding-complete').forEach(cb => {
-      cb();
-    })
+    HookStore.get('analytics:onboarding-complete').forEach(cb => cb());
   },
 
   redirectToNeutralDocs() {
