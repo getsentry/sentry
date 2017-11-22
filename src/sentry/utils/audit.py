ef6710f9855212900ce5718f6cbc0ead438500c5
Lauryn Brown <lauryndbrown@gmail.com>
2017-11-21 17:41:47 -0800

    Added Project Deletions to create audit entry.

--- src/sentry/utils/audit.py
+++ src/sentry/utils/audit.py
@@ -1,8 +1,14 @@
 from __future__ import absolute_import
-from sentry.models import ApiKey, AuditLogEntry, AuditLogEntryEvent, DeletedOrganization
+from sentry.models import ApiKey, AuditLogEntry, AuditLogEntryEvent, DeletedOrganization, DeletedProject, Project
 
 
 def create_audit_entry(request, transaction_id=None, logger=None, **kwargs):
+
+    def complete_delete_entry(delete_log, entry):
+        delete_log.actor_label = entry.actor_label
+        delete_log.actor_id = entry.actor_id
+        delete_log.actor_key = entry.actor_key
+        delete_log.ip_address = entry.ip_address
 
     user = request.user if request.user.is_authenticated() else None
     api_key = request.auth if hasattr(request, 'auth') \
@@ -23,10 +29,30 @@ def create_audit_entry(request, transaction_id=None, logger=None, **kwargs):
         delete_log.slug = entry.organization.slug
         delete_log.date_created = entry.organization.date_added
 
-        delete_log.actor_label = entry.actor_label
-        delete_log.actor_id = entry.actor_id
-        delete_log.actor_key = entry.actor_key
-        delete_log.ip_address = entry.ip_address
+        complete_delete_entry(delete_log, entry)
+
+        delete_log.save()
+
+    elif entry.event == AuditLogEntryEvent.PROJECT_REMOVE:
+        # import pdb
+        # pdb.set_trace()
+        delete_log = DeletedProject()
+
+        project = Project.objects.get(id=entry.target_object)
+        delete_log.name = project.name
+        delete_log.slug = project.slug
+        delete_log.date_created = project.date_added
+
+        delete_log.organization_id = entry.organization.id
+        delete_log.organization_name = entry.organization.name
+        delete_log.organization_slug = entry.organization.slug
+
+        team = project.team
+        delete_log.team_id = team.id
+        delete_log.team_name = team.name
+        delete_log.team_slug = team.slug
+
+        complete_delete_entry(delete_log, entry)
 
         delete_log.save()
 
