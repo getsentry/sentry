6e32e67570796bdf4072c704c3c7aa6270681e6f
alex-hofsteede <alex@hofsteede.com>
2017-11-29 13:21:46 +1300

    fix: More idempotency issues with event serialization
    1. truncatechars had an integer underflow issue which caused strings to
    be truncated using negative indexes, making them longer than they should
    be. eg. `truncatechars("ABCDEFG", 1) => 'ABCDE...'`
    
    2. Dictionaries were trimmed in arbitrary order, making the result
    change between runs.

--- tests/sentry/utils/test_strings.py
+++ tests/sentry/utils/test_strings.py
@@ -5,7 +5,7 @@ import functools
 
 from sentry.utils.strings import (
     is_valid_dot_atom, iter_callsign_choices, soft_break, soft_hyphenate,
-    tokens_from_name, codec_lookup
+    tokens_from_name, codec_lookup, truncatechars
 )
 
 ZWSP = u'\u200b'  # zero width space
@@ -97,3 +97,21 @@ def test_is_valid_dot_atom():
     assert not is_valid_dot_atom('.foo.bar')
     assert not is_valid_dot_atom('foo.bar.')
     assert not is_valid_dot_atom('foo.\x00')
+
+
+def test_truncatechars():
+    assert truncatechars("12345", 6) == "12345"
+    assert truncatechars("12345", 5) == "12345"
+    assert truncatechars("12345", 4) == "1..."
+    assert truncatechars("12345", 3) == "..."
+    assert truncatechars("12345", 2) == "..."
+    assert truncatechars("12345", 1) == "..."
+    assert truncatechars("12345", 0) == "..."
+
+    assert truncatechars("12345", 6, ellipsis=u"\u2026") == u"12345"
+    assert truncatechars("12345", 5, ellipsis=u"\u2026") == u"12345"
+    assert truncatechars("12345", 4, ellipsis=u"\u2026") == u"123\u2026"
+    assert truncatechars("12345", 3, ellipsis=u"\u2026") == u"12\u2026"
+    assert truncatechars("12345", 2, ellipsis=u"\u2026") == u"1\u2026"
+    assert truncatechars("12345", 1, ellipsis=u"\u2026") == u"\u2026"
+    assert truncatechars("12345", 0, ellipsis=u"\u2026") == u"\u2026"
