52b0a811acef36fb1f9b2e3a5273b6c0317f08cb
Ted Kaemming <ted@kaemming.com>
2017-11-29 08:54:37 -0800

    it dangerous

--- src/sentry/south_migrations/0367_remove_old_grouphash_last_processed_event_data.py
+++ src/sentry/south_migrations/0367_remove_old_grouphash_last_processed_event_data.py
@@ -12,7 +12,7 @@ class Migration(DataMigration):
 
     # Flag to indicate if this migration is too risky
     # to run online and needs to be coordinated for offline
-    is_dangerous = False
+    is_dangerous = True
 
     def forwards(self, orm):
         from sentry.utils import redis
