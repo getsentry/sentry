86e7d79329faf0bd085d9a352c26fb4604314717
Lauryn Brown <laurynbrown@Lauryns-MacBook-Pro.local>
2017-11-30 15:54:33 -0800

    Improved docstring formatting.

--- src/sentry/models/deletedteam.py
+++ src/sentry/models/deletedteam.py
@@ -8,12 +8,12 @@ from sentry.models.deletedentry import DeletedEntry
 
 class DeletedTeam(DeletedEntry):
     """
-    This model tracks an intent to delete.
-    If an org is marked pending_delete through the UI, a deletedteam is created to log this deletion.
+    This model tracks an intent to delete. If an org is marked pending_delete
+    through the UI, a deletedteam is created to log this deletion.
 
-    This model does not account for aborted or failed deletions
-    and is currently unable to log deletions that occur implicity
-    (i.e. when the sole parent object is deleted, the child is also marked for deletion as well).
+    This model does not account for aborted or failed deletions and is currently
+    unable to log deletions that occur implicity (i.e. when the sole parent object
+    is deleted, the child is also marked for deletion as well).
     """
     name = models.CharField(max_length=64, null=True)
     slug = models.CharField(max_length=50, null=True)
