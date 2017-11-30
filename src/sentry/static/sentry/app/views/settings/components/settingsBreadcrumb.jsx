983c525b6ca7c2948f1690b254dd0682fca026cf
Billy Vong <billy@sentry.io>
2017-11-30 14:11:36 -0800

    refactor settingsbreadcrumb dropdowns to use DropdownMenu + hover

--- src/sentry/static/sentry/app/views/settings/components/settingsBreadcrumb.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsBreadcrumb.jsx
@@ -9,7 +9,45 @@ import Link from '../../../components/link';
 import SentryTypes from '../../../proptypes';
 import recreateRoute from '../../../utils/recreateRoute';
 
-import IconChevronRight from '../../../icons/icon-chevron-right';
+import Crumb from './crumb.styled';
+import SettingsBreadcrumbDropdown from './settingsBreadcrumbDropdown';
+import SettingsBreadcrumbDivider from './settingsBreadcrumbDivider';
+
+const Breadcrumbs = withTheme(
+  styled.div`
+    display: flex;
+    align-items: center;
+  `
+);
+
+const MenuItem = styled(Link)`
+  display: block;
+  padding: 15px;
+  border-bottom: 1px solid ${p => p.theme.borderLight};
+
+  &:last-child {
+    border: none;
+  }
+
+  &:hover {
+    background: ${p => p.theme.offWhite};
+  }
+
+  ${p =>
+    p.active
+      ? `
+      font-weight: bold;
+    background: ${p.theme.offWhite};
+  `
+      : ''};
+`;
+
+const StyledLink = styled(Link)`
+  color: ${p => p.theme.gray3};
+  &:hover {
+    color: ${p => p.theme.gray5};
+  }
+`;
 
 const withOrganizations = WrappedComponent =>
   React.createClass({
@@ -21,7 +59,7 @@ const withOrganizations = WrappedComponent =>
     },
   });
 
-const ProjectCrumb = ({team, organization, params, routes, route, isLast}) => {
+const ProjectCrumb = ({team, organization, params, routes, route, ...props}) => {
   if (!organization) return null;
 
   let {teams} = organization;
@@ -29,27 +67,20 @@ const ProjectCrumb = ({team, organization, params, routes, route, isLast}) => {
   let hasMenu = projects && projects.length > 1;
 
   return (
-    <Crumb hasMenu={hasMenu}>
-      <div style={{display: 'inline'}}>{route.name} </div>
-      {!isLast && (
-        <Divider hasMenu={hasMenu}>
-          <IconChevronRight size="15" />
-        </Divider>
-      )}
-      <Menu className="menu">
-        {projects.map(project => (
-          <MenuItem
-            to={recreateRoute(route, {
-              routes,
-              params: {...params, projectId: project.slug},
-            })}
-            key={project.slug}
-          >
-            {project.name}
-          </MenuItem>
-        ))}
-      </Menu>
-    </Crumb>
+    <SettingsBreadcrumbDropdown hasMenu={hasMenu} route={route} {...props}>
+      {projects.map(project => (
+        <MenuItem
+          to={recreateRoute(route, {
+            routes,
+            params: {...params, projectId: project.slug},
+          })}
+          active={project.slug === params.projectId}
+          key={project.slug}
+        >
+          {project.name}
+        </MenuItem>
+      ))}
+    </SettingsBreadcrumbDropdown>
   );
 };
 ProjectCrumb.displayName = 'ProjectCrumb';
@@ -62,18 +93,12 @@ ProjectCrumb.propTypes = {
 };
 
 const MENUS = {
-  Organization: withOrganizations(({organizations, params, routes, route, isLast}) => {
-    let hasMenu = organizations.length > 1;
+  Organization: withOrganizations(
+    ({organizations, params, routes, route, isLast, ...props}) => {
+      let hasMenu = organizations.length > 1;
 
-    return (
-      <Crumb hasMenu={hasMenu}>
-        <div style={{display: 'inline'}}>{route.name} </div>
-        {!isLast && (
-          <Divider hasMenu={hasMenu}>
-            <IconChevronRight size="15" />
-          </Divider>
-        )}
-        <Menu className="menu">
+      return (
+        <SettingsBreadcrumbDropdown hasMenu={hasMenu} route={route} {...props}>
           {organizations.map(organization => (
             <MenuItem
               to={recreateRoute(route, {
@@ -85,20 +110,13 @@ const MENUS = {
               {organization.name}
             </MenuItem>
           ))}
-        </Menu>
-      </Crumb>
-    );
-  }),
+        </SettingsBreadcrumbDropdown>
+      );
+    }
+  ),
 
   Project: ProjectCrumb,
 };
-
-const StyledLink = styled(Link)`
-  color: ${p => p.theme.gray3};
-  &:hover {
-    color: ${p => p.theme.gray5};
-  }
-`;
 
 class SettingsBreadcrumb extends React.Component {
   static propTypes = {
@@ -128,11 +146,7 @@ class SettingsBreadcrumb extends React.Component {
                   <StyledLink to={recreateRoute(route, {routes, params})}>
                     {route.name}{' '}
                   </StyledLink>
-                  {!isLast && (
-                    <Divider>
-                      <IconChevronRight size="15" />
-                    </Divider>
-                  )}
+                  <SettingsBreadcrumbDivider isLast={isLast} />
                 </Crumb>
               );
 
@@ -152,75 +166,5 @@ class SettingsBreadcrumb extends React.Component {
     );
   }
 }
-
-const Breadcrumbs = withTheme(
-  styled.div`
-    display: flex;
-    align-items: center;
-  `
-);
-
-const Crumb = styled('div')`
-  display: block;
-  position: relative;
-  font-size: 18px;
-  color: ${p => p.theme.gray3};
-  margin-right: 10px;
-  cursor: pointer;
-  > span {
-    transition: 0.1s all ease;
-  }
-
-  &:hover {
-    color: ${p => p.theme.gray5};
-    ${p =>
-      p.hasMenu
-        ? `> span {
-      transform: rotate(90deg);
-      top: 0;
-    }`
-        : ''} > .menu {
-      opacity: 1;
-      visibility: visible;
-    }
-  }
-`;
-
-const Divider = styled.span`
-  display: inline-block;
-  margin-left: 6px;
-  color: ${p => p.theme.gray1};
-  position: relative;
-  top: -1px;
-`;
-
-const Menu = styled.div`
-  font-size: 16px;
-  opacity: 0;
-  visibility: hidden;
-  position: absolute;
-  top: 140%;
-  width: 200px;
-  background: #fff;
-  border: 1px solid ${p => p.theme.borderDark};
-  box-shadow: ${p => p.theme.dropShadowHeavy};
-  transition: 0.1s all ease;
-  border-radius: ${p => p.theme.radius};
-  overflow: hidden;
-`;
-
-const MenuItem = styled(Link)`
-  display: block;
-  padding: 15px;
-  border-bottom: 1px solid ${p => p.theme.borderLight};
-
-  &:last-child {
-    border: none;
-  }
-
-  &:hover {
-    background: ${p => p.theme.offWhite};
-  }
-`;
 
 export default SettingsBreadcrumb;
---
+++ src/sentry/static/sentry/app/views/settings/components/settingsBreadcrumbDropdown.jsx
@@ -0,0 +1,124 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'react-emotion';
+
+import DropdownMenu from '../../../components/dropdownMenu';
+import SettingsBreadcrumbDivider from './settingsBreadcrumbDivider';
+import Crumb from './crumb.styled';
+
+const Menu = styled.div`
+  font-size: 16px;
+  position: absolute;
+  opacity: 0;
+  visibility: hidden;
+  top: 140%;
+  width: 200px;
+  background: #fff;
+  border: 1px solid ${p => p.theme.borderDark};
+  box-shadow: ${p => p.theme.dropShadowHeavy};
+  transition: 0.1s all ease;
+  border-radius: ${p => p.theme.radius};
+  overflow: hidden;
+
+  ${p =>
+    p.isOpen
+      ? `opacity: 1;
+  visibility: visible;`
+      : ''};
+`;
+
+class SettingsBreadcrumbDropdown extends React.Component {
+  static propTypes = {
+    hasMenu: PropTypes.bool,
+    route: PropTypes.object,
+    isLast: PropTypes.bool,
+    enterDelay: PropTypes.number,
+  };
+
+  static defaultProps = {
+    enterDelay: 200,
+  };
+
+  constructor(...args) {
+    super(...args);
+
+    this.entering = false;
+    this.leaving = false;
+    this.state = {isOpen: false};
+  }
+
+  // Adds a delay when mouse hovers on actor (in this case the breadcrumb)
+  handleMouseEnterActor = () => {
+    if (this.leaving) {
+      clearTimeout(this.leaving);
+    }
+
+    this.entering = setTimeout(
+      () => this.setState({isOpen: true}),
+      this.props.enterDelay
+    );
+  };
+
+  // handles mouseEnter event on actor and menu, should clear the leaving timeout and keep menu open
+  handleMouseEnter = () => {
+    if (this.leaving) {
+      clearTimeout(this.leaving);
+    }
+
+    this.setState({isOpen: true});
+  };
+
+  // handles mouseLeave event on actor and menu, adds a timeout before updating state to account for
+  // mouseLeave into
+  handleMouseLeave = e => {
+    if (this.entering) {
+      clearTimeout(this.entering);
+    }
+
+    this.leaving = setTimeout(() => this.setState({isOpen: false}), 200);
+  };
+
+  // Close immediately when clicked outside
+  handleClose = () => {
+    this.setState({isOpen: false});
+  };
+
+  render() {
+    let {children, hasMenu, route, isLast} = this.props;
+    return (
+      <DropdownMenu isOpen={this.state.isOpen} onClickOutside={this.handleClose}>
+        {({isOpen, getRootProps, getActorProps, getMenuProps}) => {
+          return (
+            <Crumb {...getRootProps({hasMenu})}>
+              <div
+                {...getActorProps({
+                  onMouseEnter: this.handleMouseEnterActor,
+                  onMouseLeave: this.handleMouseLeave,
+                  style: {display: 'inline'},
+                })}
+              >
+                {route.name}{' '}
+              </div>
+              <SettingsBreadcrumbDivider isHover={hasMenu && isOpen} isLast={isLast} />
+              {hasMenu && (
+                <Menu
+                  {...getMenuProps({
+                    isOpen,
+                    isStyled: true,
+                    onMouseEnter: this.handleMouseEnter,
+                    onMouseLeave: this.handleMouseLeave,
+                    className: 'menu',
+                  })}
+                >
+                  {children}
+                </Menu>
+              )}
+            </Crumb>
+          );
+        }}
+      </DropdownMenu>
+    );
+  }
+}
+
+export default SettingsBreadcrumbDropdown;
