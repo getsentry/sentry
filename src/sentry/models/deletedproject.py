6621ff5e93e0bb5987aa676b671b9252237d8e1b
Lauryn Brown <laurynbrown@Lauryns-MacBook-Pro.local>
2017-11-30 11:45:06 -0800

    Added explanatory comments. Needs Review

--- src/sentry/models/deletedproject.py
+++ src/sentry/models/deletedproject.py
@@ -7,6 +7,14 @@ from sentry.models.deletedentry import DeletedEntry
 
 
 class DeletedProject(DeletedEntry):
+    """
+    This model tracks an intent to delete.
+    If an org is marked pending_delete through the UI, a deletedproject is created to log this deletion.
+
+    This model does not account for aborted or failed deletions
+    and is currently unable to log deletions that occur implicity
+    (i.e. when the sole parent object is deleted, the child is also marked for deletion as well).
+    """
     slug = models.SlugField(null=True)
     name = models.CharField(max_length=200, null=True)
 
