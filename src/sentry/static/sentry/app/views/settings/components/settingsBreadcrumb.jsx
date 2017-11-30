2b298def4962cf8d7c5b5d6bde0178e2606c205e
ckj <chriskjennings@gmail.com>
2017-11-29 17:04:14 -0800

    Stub out non-functioning breadcrumb dropdowns

--- src/sentry/static/sentry/app/views/settings/components/settingsBreadcrumb.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsBreadcrumb.jsx
@@ -29,12 +29,18 @@ class SettingsBreadcrumb extends React.Component {
 
           return (
             <span key={`${route.name}:${route.path}`}>
-              <Crumb to={recreateRoute(route, {routes, params})}>{route.name}</Crumb>
-              {!isLast && (
-                <Divider>
-                  <IconChevronRight size="15" />
-                </Divider>
-              )}
+              <Crumb to={recreateRoute(route, {routes, params})}>
+                {route.name}{' '}
+                {!isLast && (
+                  <Divider>
+                    <IconChevronRight size="15" />
+                  </Divider>
+                )}
+                <Menu className="menu">
+                  <MenuItem>Error, Inc.</MenuItem>
+                  <MenuItem>RIP Industries</MenuItem>
+                </Menu>
+              </Crumb>
             </span>
           );
         })}
@@ -51,21 +57,65 @@ const Breadcrumbs = withTheme(
 );
 
 const Crumb = styled(Link)`
+  display: block;
+  position: relative;
   font-size: 18px;
   color: ${p => p.theme.gray3};
-  margin-right: 8px;
+  margin-right: 10px;
   cursor: pointer;
+  > span {
+    transition: 0.1s all ease;
+  }
 
   &:hover {
     color: ${p => p.theme.gray5};
+    > span {
+      transform: rotate(90deg);
+      top: 0;
+    }
+
+    > .menu {
+      opacity: 1;
+      visibility: visible;
+    }
   }
 `;
 
 const Divider = styled.span`
-  margin-right: 8px;
+  display: inline-block;
+  margin-left: 6px;
   color: ${p => p.theme.gray1};
   position: relative;
   top: -1px;
+`;
+
+const Menu = styled.div`
+  font-size: 16px;
+  opacity: 0;
+  visibility: hidden;
+  position: absolute;
+  top: 140%;
+  width: 200px;
+  background: #fff;
+  border: 1px solid ${p => p.theme.borderDark};
+  box-shadow: ${p => p.theme.dropShadowHeavy};
+  transition: 0.1s all ease;
+  border-radius: ${p => p.theme.radius};
+  overflow: hidden;
+`;
+
+const MenuItem = styled(Link)`
+  display: block;
+  padding: 15px;
+  border-bottom: 1px solid ${p => p.theme.borderLight};
+
+  &:last-child {
+    border: none;
+  }
+
+  &:hover {
+    background: ${p => p.theme.offWhite};
+  }
 `;
 
 export default SettingsBreadcrumb;
