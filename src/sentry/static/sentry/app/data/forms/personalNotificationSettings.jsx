251a20ffce5f71b268285ebb0e196408227a1d78
chrissy <fivefourths@gmail.com>
2017-12-01 14:01:37 -0800

    add all form inputs that don't require a new form component type

--- src/sentry/static/sentry/app/data/forms/personalNotificationSettings.jsx
+++ src/sentry/static/sentry/app/data/forms/personalNotificationSettings.jsx
@@ -5,7 +5,6 @@ const forms = [
       {
         name: 'projectAlerts',
         type: 'boolean',
-        required: true,
         label: 'Send Me Project Alerts',
         help: 'Alerts are defined in [Project] » Project Settings » Alerts » Rules.',
       }
@@ -13,13 +12,31 @@ const forms = [
   },
 
   {
-    title: 'Workflow Notifications',
+    title: 'Weekly Reports',
     fields: [
       {
         name: 'weeklyReports',
         type: 'boolean',
         label: 'Send Me Weekly Reports',
         help: "Reports contain a summary of what's happened within your organization.",
+      }
+    ]
+  },
+
+  {
+    title: 'My Activity',
+    fields: [
+      {
+        name: 'personalActivity',
+        type: 'boolean',
+        label: 'Notify Me About My Own Activity',
+        help: "Enable this to recieve notifications about your own actions on Sentry.",
+      },
+      {
+        name: 'claimUnassignedIssues',
+        type: 'boolean',
+        label: 'Claim Unassigned Issues I\'ve Resolved',
+        help: "You'll recieve notifications about any changes that happen afterwords.",
       }
     ]
   }
