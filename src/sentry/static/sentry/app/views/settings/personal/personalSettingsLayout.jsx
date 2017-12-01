e9889eb5a4e2227e500123944ccf66858b2ce636
chrissy <fivefourths@gmail.com>
2017-11-30 17:01:54 -0800

    add notifications header label

--- src/sentry/static/sentry/app/views/settings/personal/personalSettingsLayout.jsx
+++ src/sentry/static/sentry/app/views/settings/personal/personalSettingsLayout.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
 import SettingsLayout from '../settingsLayout';
+import SettingsPageHeader from '../components/settingsPageHeader';
 
 class PersonalSettingsLayout extends React.Component {
   render() {
 
     return (
-      <SettingsLayout
-        {...this.props}
-      >
+      <SettingsLayout {...this.props}>
         {this.props.children}
+        <div>
+          <SettingsPageHeader label="Notifications" />
+        </div>
+
       </SettingsLayout>
     );
   }
