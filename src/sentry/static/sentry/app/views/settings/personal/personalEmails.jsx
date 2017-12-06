8d9d4d135451ab7ef59b2c02b9cb153fd6f807ed
chrissy <fivefourths@gmail.com>
2017-12-06 11:19:04 -0800

    use pure es6 classes rather than react createClass whenever possible

--- src/sentry/static/sentry/app/views/settings/personal/personalEmails.jsx
+++ src/sentry/static/sentry/app/views/settings/personal/personalEmails.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import SettingsPageHeader from '../components/settingsPageHeader';
 
-const PersonalEmails = React.createClass({
-  render() {
-    return (
-      <div>
-        <SettingsPageHeader label="Emails" />
-      </div>
-    );
-  }
-});
+const PersonalEmails = () => {
+  return (
+    <div>
+      <SettingsPageHeader label="Emails" />
+    </div>
+  );
+};
 
 export default PersonalEmails;
--- src/sentry/static/sentry/app/views/settings/personal/personalSettingsNavigation.jsx
+++ src/sentry/static/sentry/app/views/settings/personal/personalSettingsNavigation.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 import SettingsNavigation from '../components/settingsNavigation';
 import navigationConfiguration from './navigationConfiguration';
 
-const PersonalSettingsNavigation = React.createClass({
-  render() {
-    return (
-      <SettingsNavigation navigationObjects={navigationConfiguration}/>
-    );
-  }
-});
+const PersonalSettingsNavigation = () => {
+  return (
+    <SettingsNavigation navigationObjects={navigationConfiguration}/>
+  );
+};
 
 export default PersonalSettingsNavigation;
