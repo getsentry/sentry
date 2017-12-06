7f8dca64f9a4302148c41ed19dbcc3b0f65e48ff
Chris Clark <christopherchristopherclark@gmail.com>
2017-12-06 13:52:53 -0800

    Personal Settings Setup (#6689)
    Setup a new page for personal settings, with a sidebar and react routes for email and notifications

---
+++ src/sentry/static/sentry/app/views/settings/personal/personalSettingsLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import SettingsLayout from '../settingsLayout';
+import PersonalSettingsNavigation from './personalSettingsNavigation';
+
+class PersonalSettingsLayout extends React.Component {
+  render() {
+    return (
+      <SettingsLayout
+        {...this.props}
+        renderNavigation={() => <PersonalSettingsNavigation {...this.props} />}
+      >
+        {this.props.children}
+      </SettingsLayout>
+    );
+  }
+}
+
+export default PersonalSettingsLayout;
