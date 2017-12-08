87b1fe05597118e84400a0d7ba8635aa6ea436db
Billy Vong <billy@sentry.io>
2017-12-08 11:21:57 -0800

    sudo endpoint accepts u2f

--- src/sentry/api/endpoints/sudo.py
+++ src/sentry/api/endpoints/sudo.py
@@ -1,16 +1,42 @@
 from __future__ import absolute_import
 
+import json
+
 from django.contrib import auth
 from rest_framework.response import Response
 from sudo.utils import grant_sudo_privileges
 
 from sentry.api.base import Endpoint
+from sentry.models import Authenticator
 
 
 class SudoEndpoint(Endpoint):
     permission_classes = ()
 
     def post(self, request):
-        if auth.authenticate(username=request.user.email, password=request.DATA.get('password')):
+        authenticated = False
+
+        if 'challenge' in request.DATA and 'response' in request.DATA:
+            try:
+                interface = Authenticator.objects.get_interface(request.user, 'u2f')
+                if not interface.is_enrolled:
+                    raise LookupError()
+
+                challenge = json.loads(request.DATA['challenge'])
+                response = json.loads(request.DATA['response'])
+                authenticated = interface.validate_response(request, challenge, response)
+            except ValueError:
+                pass
+            except LookupError:
+                pass
+
+        else:
+            authenticated = auth.authenticate(
+                username=request.user.email,
+                password=request.DATA.get('password'))
+
+        if authenticated:
             grant_sudo_privileges(request._request)
             return Response(status=200)
+
+        return Response({'allowFail': True}, content_type="application/json", status=401)
