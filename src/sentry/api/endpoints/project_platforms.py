e53296abf1382f0b9e35e25d33c8262b3ecb800b
Lauryn Brown <lauryndbrown@gmail.com>
2017-12-07 16:18:17 -0800

    altered project platform's get function and test to get instead of filter as well as throwing an error when needed.

--- src/sentry/api/endpoints/project_platforms.py
+++ src/sentry/api/endpoints/project_platforms.py
@@ -2,17 +2,24 @@ from __future__ import absolute_import
 
 from rest_framework.response import Response
 from sentry.api.bases.project import ProjectEndpoint
-from sentry.api.serializers import serialize, register, Serializer
 from sentry.models import ProjectPlatform
+from rest_framework import serializers, status
+from sentry.api.serializers import serialize
+from django.core.exceptions import ObjectDoesNotExist
 
 
-@register(ProjectPlatform)
-class ProjectPlatformSerializer(Serializer):
-    def serialize(self, obj, attrs, user):
-        return {'platform': obj.platform, 'dateCreated': obj.date_added}
+class ProjectPlatformSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectPlatform
+        fields = ('platform',)
 
 
 class ProjectPlatformsEndpoint(ProjectEndpoint):
     def get(self, request, project):
-        queryset = ProjectPlatform.objects.filter(project_id=project.id)
-        return Response(serialize(list(queryset), request.user))
+        try:
+            project_platform = ProjectPlatform.objects.get(project_id=project.id)
+
+        except ObjectDoesNotExist:
+            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+
+        return Response(serialize(project_platform, request.user), status=status.HTTP_200_OK)
