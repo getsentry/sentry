585c84934c439269135137df54ed1ab2c1699740
Lauryn Brown <lauryndbrown@gmail.com>
2017-12-12 11:27:05 -0800

    Removed block that threw an error if there were no project_platforms.

--- src/sentry/api/endpoints/project_platforms.py
+++ src/sentry/api/endpoints/project_platforms.py
@@ -10,9 +10,5 @@ from sentry.api.serializers import serialize
 class ProjectPlatformsEndpoint(ProjectEndpoint):
     def get(self, request, project):
 
-        project_platforms = ProjectPlatform.objects.filter(project_id=project.id)
-
-        if len(project_platforms) < 1:
-            return Response({}, status=status.HTTP_400_BAD_REQUEST)
-
-        return Response(serialize(list(project_platforms), request.user), status=status.HTTP_200_OK)
+        project_platforms = list(ProjectPlatform.objects.filter(project_id=project.id))
+        return Response(serialize(project_platforms, request.user), status=status.HTTP_200_OK)
