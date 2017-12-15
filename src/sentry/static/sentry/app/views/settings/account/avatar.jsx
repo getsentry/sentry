013d09de3a570ae28b4df168315d8acaa790cc23
Lyn Nagara <lyn.nagara@gmail.com>
2017-12-14 17:28:10 -0800

    feat: Migrate personal settings avatar page

--- src/sentry/static/sentry/app/views/settings/account/avatar.jsx
+++ src/sentry/static/sentry/app/views/settings/account/avatar.jsx
@@ -1,13 +1,13 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import AlertActions from '../actions/alertActions';
-import ApiMixin from '../mixins/apiMixin';
-import AvatarCropper from '../components/avatarCropper';
-import AvatarRadio from '../components/avatarRadio';
-import LoadingError from '../components/loadingError';
-import LoadingIndicator from '../components/loadingIndicator';
-import {t} from '../locale';
+import AlertActions from '../../../actions/alertActions';
+import ApiMixin from '../../../mixins/apiMixin';
+import AvatarCropper from '../../../components/avatarCropper';
+import AvatarRadio from '../../../components/avatarRadio';
+import LoadingError from '../../../components/loadingError';
+import LoadingIndicator from '../../../components/loadingIndicator';
+import {t} from '../../../locale';
 
 const AvatarSettings = React.createClass({
   propTypes: {
@@ -101,7 +101,7 @@ const AvatarSettings = React.createClass({
     );
 
     return (
-      <div>
+      <div style={{lineHeight: '1.5em'}}>
         <form>
           <AvatarRadio user={this.state.user} updateUser={this.updateUserState} />
 
