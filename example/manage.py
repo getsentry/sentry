0db380f11a94e03467497c20d7f31c26ebb5cd7e
seavpor <34526863+seavpor@users.noreply.github.com>
2017-12-15 08:39:31 +0700

    Update manage.py

--- example/manage.py
+++ example/manage.py
@@ -5,10 +5,7 @@ try:
     imp.find_module('settings') # Assumed to be in the same directory.
 except ImportError:
     import sys
-    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
-    sys.exit(1)
-
-import settings
+    sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It
 
 
 import sys
