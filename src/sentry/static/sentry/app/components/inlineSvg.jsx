02aa98b2d3c91d2b687facd9759be98e4beb83d0
Chris Clark <christopherchristopherclark@gmail.com>
2017-12-14 16:45:44 -0800

    build(svg): add svg-sprite-loader

---
+++ src/sentry/static/sentry/app/components/inlineSvg.jsx
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const InlineSvg = ({src, size, width, height}) => {
+  const {id, viewBox} = require(`../icons/${src}.svg`).default;
+
+  return (
+    <svg
+      viewBox={viewBox}
+      width={width || size || '1em'}
+      height={height || size || '1em'}
+    >
+      <use href={`#${id}`} xlinkHref={`#${id}`} />
+    </svg>
+  );
+};
+
+InlineSvg.propTypes = {
+  src: PropTypes.string.isRequired,
+  size: PropTypes.string,
+  width: PropTypes.string,
+  height: PropTypes.string,
+};
+
+export default InlineSvg;
