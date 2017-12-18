b77dc8d924e3b9a3804d82f09eba94d69bbed9c7
Jess MacQueen <jess@getsentry.com>
2017-12-18 13:23:32 -0800

    test(projects): Add tests for ProjectManager.get_for_user

--- tests/sentry/manager/tests.py
+++ tests/sentry/manager/tests.py
@@ -3,7 +3,7 @@
 from __future__ import absolute_import
 
 from sentry import tagstore
-from sentry.models import Group, Team, User
+from sentry.models import Group, Project, Team, User
 from sentry.testutils import TestCase
 
 
@@ -114,3 +114,61 @@ class TeamManagerTest(TestCase):
             with_projects=True,
         )
         assert result == [(team, [project2, project])]
+
+
+class ProjectManagerTest(TestCase):
+    def test_simple(self):
+        user = User.objects.create(username='foo')
+        org = self.create_organization()
+        team = self.create_team(organization=org, name='Test')
+        project = self.create_project(team=team, name='foo')
+        project2 = self.create_project(team=team, name='baz')
+
+        result = Project.objects.get_for_user(
+            team=team,
+            user=user,
+            _skip_team_check=True,
+        )
+        assert result == [project2, project]
+
+        result = Project.objects.get_for_user(
+            team=team,
+            user=user,
+            _skip_team_check=False,
+        )
+        assert result == []
+
+        self.create_member(organization=org, user=user, teams=[team])
+
+        # check again after creating member
+        result = Project.objects.get_for_user(
+            team=team,
+            user=user,
+            _skip_team_check=True,
+        )
+        assert result == [project2, project]
+
+        result = Project.objects.get_for_user(
+            team=team,
+            user=user,
+            _skip_team_check=False,
+        )
+        assert result == [project2, project]
+
+        # test with scope user doesn't have
+        result = Project.objects.get_for_user(
+            team=team,
+            user=user,
+            _skip_team_check=False,
+            scope='project:write',
+        )
+        assert result == []
+
+        # check with scope they do have
+        result = Project.objects.get_for_user(
+            team=team,
+            user=user,
+            _skip_team_check=False,
+            scope='project:read',
+        )
+        assert result == [project2, project]
