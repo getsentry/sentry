23b06a9e8d91815ad7765dec2c5a4f84d145e370
Billy Vong <billy@sentry.io>
2017-12-18 14:39:52 -0800

    remove pluginnav item component

--- src/sentry/static/sentry/app/views/projectSettings/pluginNavigation.jsx
+++ src/sentry/static/sentry/app/views/projectSettings/pluginNavigation.jsx
@@ -1,24 +1,7 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import Reflux from 'reflux';
 
 import PluginsStore from '../../stores/pluginsStore';
-
-class PluginNavigationItem extends React.PureComponent {
-  static propTypes = {
-    urlRoot: PropTypes.string,
-    id: PropTypes.string,
-    name: PropTypes.string,
-  };
-  render() {
-    let {urlRoot, id, name} = this.props;
-    return (
-      <li>
-        <a href={`${urlRoot}/plugins/${id}/`}>{name}</a>
-      </li>
-    );
-  }
-}
 
 const PluginNavigation = React.createClass({
   mixins: [Reflux.connect(PluginsStore, 'store')],
@@ -34,8 +17,10 @@ const PluginNavigation = React.createClass({
 
     return (
       <div>
-        {enabledPlugins.map(plugin => (
-          <PluginNavigationItem key={plugin.id} urlRoot={urlRoot} {...plugin} />
+        {enabledPlugins.map(({id, name}) => (
+          <li key={id}>
+            <a href={`${urlRoot}/plugins/${id}/`}>{name}</a>
+          </li>
         ))}
       </div>
     );
