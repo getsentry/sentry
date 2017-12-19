8d967d36b1f697b24b8bd29137cb27a6ca8168f7
chrissy <fivefourths@gmail.com>
2017-12-18 18:16:46 -0800

    remove accidentally added line from routes.jsx

--- src/sentry/static/sentry/app/routes.jsx
+++ src/sentry/static/sentry/app/routes.jsx
@@ -400,7 +400,6 @@ function routes() {
             component={errorHandler(OrganizationContext)}
           >
             <Route component={errorHandler(OrganizationSettingsLayout)}>
-              {hooksOrgRoutes}
               {orgSettingsRoutes}
             </Route>
 
