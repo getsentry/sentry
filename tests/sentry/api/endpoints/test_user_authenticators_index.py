b063780a3746d906d7010e8ba5375438efc404f0
Billy Vong <billy@sentry.io>
2017-12-19 12:44:37 -0800

    add tests for authenticators_index

--- tests/sentry/api/endpoints/test_user_authenticators_index.py
+++ tests/sentry/api/endpoints/test_user_authenticators_index.py
@@ -1,6 +1,7 @@
 from __future__ import absolute_import
 
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 from sentry.models import Authenticator
 from sentry.testutils import APITestCase
@@ -12,6 +13,17 @@ class AuthenticatorIndex(APITestCase):
         Authenticator.objects.create(
             type=3,  # u2f
             user=user,
+            config={
+                'devices': [{
+                    'binding': {
+                        'publicKey': u'aowekroawker',
+                        'keyHandle': u'aowkeroakewrokaweokrwoer',
+                        'appId': u'https://dev.getsentry.net:8000/auth/2fa/u2fappid.json'
+                    },
+                    'name': u'Amused Beetle',
+                    'ts': 1512505334
+                }]
+            }
         )
 
         self.login_as(user=user, superuser=True)
@@ -19,7 +31,13 @@ class AuthenticatorIndex(APITestCase):
         url = reverse(
             'sentry-api-0-authenticator-index',
         )
-        resp = self.client.get(url, format='json')
 
-        assert resp.status_code == 200, (resp.status_code, resp.content)
-        assert len(resp.content) == 1
+        new_options = settings.SENTRY_OPTIONS
+        new_options['system.url-prefix'] = 'https://testserver'
+        with self.settings(SENTRY_OPTIONS=new_options):
+            resp = self.client.get(url, format='json')
+
+            assert resp.status_code == 200, (resp.status_code, resp.content)
+            assert resp.data
+            assert resp.data[0]['challenge']
+            assert resp.data[0]['id'] == 'u2f'
