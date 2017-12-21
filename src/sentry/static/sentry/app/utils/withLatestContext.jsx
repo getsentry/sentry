971070ef10790a905ae10512c0d28843c90fc6c3
Lyn Nagara <lyn.nagara@gmail.com>
2017-12-21 10:06:16 -0800

    fix: Fix withLatestContext return value
    Returns undefined if no organization instead of 0

--- src/sentry/static/sentry/app/utils/withLatestContext.jsx
+++ src/sentry/static/sentry/app/utils/withLatestContext.jsx
@@ -21,7 +21,8 @@ const withLatestContext = WrappedComponent =>
         // expect consistent data structure because OrganizationsStore has a list
         // of orgs but not full org details
         let latestOrganization =
-          organization || (organizations && organizations.length && organizations[0]);
+          organization ||
+          (organizations && organizations.length ? organizations[0] : null);
 
         return (
           <WrappedComponent
