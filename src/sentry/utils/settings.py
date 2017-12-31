466cfcc14a583816773e3002d62f039ef209b00f
Arik Fraimovich <arik@arikfr.com>
2017-12-31 11:17:17 +0200

    Copy paste error? Fix module name in module comment.

--- src/sentry/utils/settings.py
+++ src/sentry/utils/settings.py
@@ -1,6 +1,6 @@
 """
-sentry.utils.imports
-~~~~~~~~~~~~~~~~~~~~
+sentry.utils.settings
+~~~~~~~~~~~~~~~~~~~~~
 
 :copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
