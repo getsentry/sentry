da2ffb1593a39401c9b4784cc6e49e19c023bfd4
Billy Vong <billy@sentry.io>
2018-01-05 13:37:36 -0800

    simplify condition

--- src/sentry/static/sentry/app/utils/emotion-setup.js
+++ src/sentry/static/sentry/app/utils/emotion-setup.js
@@ -1,9 +1,9 @@
 /* global process */
-import {sheet} from 'emotion';
 
 // NEEDS to be true for production because of performance
 // But travis builds with NODE_ENV = production, so turn off speed when
 // IS_PERCY is true (i.e. we are in TRAVIS and PERCY_TOKEN is set)
-
-let isSpeedy = process.env.IS_PERCY ? false : process.env.NODE_ENV !== 'development';
-sheet.speedy(isSpeedy);
+if (process.env.IS_PERCY) {
+  const sheet = require('emotion').sheet;
+  sheet.speedy(false);
+}
