6b14be3bc679ee11d02bd6bf3e74b7750708fe05
Billy Vong <billy@sentry.io>
2018-01-05 11:48:21 -0800

    feat(ui): Add prop to formfield for non-inline

--- docs-ui/components/form.stories.js
+++ docs-ui/components/form.stories.js
@@ -11,7 +11,9 @@ import {
   BooleanField,
 } from 'sentry-ui/forms';
 import RadioField from 'settings-ui/forms/radioField';
+import RadioGroup from 'settings-ui/forms/radioGroup';
 import Form from 'settings-ui/forms/form';
+import FormField from 'settings-ui/forms/formField';
 
 class UndoButton extends React.Component {
   static contextTypes = {
@@ -101,6 +103,27 @@ storiesOf('Forms/Fields', module)
             ['choice_three', 'Choice Three'],
           ]}
         />
+      </Form>
+    ))
+  )
+  .add(
+    'Non-inline field',
+    withInfo('Radio field')(() => (
+      <Form>
+        <FormField name="radio" label="Radio Field" inline={false}>
+          {({value, label, onChange}) => (
+            <RadioGroup
+              onChange={onChange}
+              label={label}
+              value={value}
+              choices={[
+                ['choice_one', 'Choice One'],
+                ['choice_two', 'Choice Two'],
+                ['choice_three', 'Choice Three'],
+              ]}
+            />
+          )}
+        </FormField>
       </Form>
     ))
   );
