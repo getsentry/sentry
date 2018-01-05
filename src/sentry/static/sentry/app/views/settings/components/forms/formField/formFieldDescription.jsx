a8757885161641642cf18f2b9de77a8b90280dcd
Billy Vong <billyvg@users.noreply.github.com>
2018-01-05 14:17:07 -0800

    feat(ui): Add prop to formfield for non-inline (#6894)

--- src/sentry/static/sentry/app/views/settings/components/forms/formField/formFieldDescription.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/formField/formFieldDescription.jsx
@@ -1,10 +1,16 @@
-import styled from 'react-emotion';
 import {Box} from 'grid-emotion';
+import React from 'react';
+import styled from 'react-emotion';
 
-const FormFieldDescription = styled(Box)`
+const FormFieldDescription = styled(({inline, ...props}) => <Box {...props} />)`
+  ${p =>
+    p.inline
+      ? `
   width: 50%;
   padding-right: 10px;
   flex-shrink: 0;
+  `
+      : 'margin-bottom: 10px;'};
 `;
 
 export default FormFieldDescription;
