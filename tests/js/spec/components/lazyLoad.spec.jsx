e767fdcc661889c5f0349825f8ffc31b2b4b5daf
Billy Vong <billyvg@users.noreply.github.com>
2018-01-09 11:21:31 -0800

    feat(ui): Change `<LazyLoad>` and code split Account Notifications (#6872)
    * feat(ui): Change `<LazyLoad>` and code split Account Notifications
    
    `<LazyLoad>`:
    - Adds tests + storybook
    - shows error message on errors
    - accepts react-router `route` object with `getComponent` property that
    is a function that returns a promise of a React.Component (Component
    that will be dynamically imported)
    - Change to code split `<AccountNotifications>`

---
+++ tests/js/spec/components/lazyLoad.spec.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import {shallow, mount} from 'enzyme';
+import LazyLoad from 'app/components/lazyLoad';
+
+jest.mock('raven-js');
+
+describe('LazyLoad', function() {
+  it('renders with a loading indicator when promise is not resolved yet', function() {
+    let promise = new Promise((resolve, reject) => {});
+    let getComponent = () => promise;
+    let wrapper = shallow(<LazyLoad component={getComponent} />);
+
+    // Should be loading
+    expect(wrapper.find('LoadingIndicator').length).toBe(1);
+  });
+
+  it('renders when given a promise of a "button" component', async function() {
+    let res;
+    let promise = new Promise((resolve, reject) => {
+      res = resolve;
+    });
+    let getComponent = () => promise;
+    let wrapper = mount(<LazyLoad component={getComponent} />);
+
+    // Should be loading
+    expect(wrapper.find('LoadingIndicator').length).toBe(1);
+
+    // resolve with button
+    let ResolvedComponent = 'button';
+    res(ResolvedComponent);
+
+    await promise;
+    wrapper.update();
+    expect(wrapper.state('Component')).toEqual('button');
+    expect(wrapper.find('button').length).toBe(1);
+    expect(wrapper.find('LoadingIndicator').length).toBe(0);
+  });
+
+  it('renders with error message when promise is rejected', async function() {
+    let reject;
+    let promise = new Promise((resolve, rej) => {
+      reject = rej;
+    });
+    let getComponent = () => promise;
+    let wrapper;
+
+    try {
+      wrapper = mount(<LazyLoad component={getComponent} />);
+
+      reject(new Error('Could not load component'));
+      await promise;
+    } catch (err) {
+      // ignore
+    }
+
+    wrapper.update();
+    expect(wrapper.find('LoadingIndicator').length).toBe(0);
+    expect(wrapper.find('LoadingError').length).toBe(1);
+  });
+});
