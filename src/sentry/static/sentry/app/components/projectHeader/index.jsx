d43962bee8f6d8f1f8d74c37700380c6bd985832
Lyn Nagara <lyn.nagara@gmail.com>
2018-01-09 13:01:52 -0800

    feat(environments): Add environments toggle if feature flag is enabled

--- src/sentry/static/sentry/app/components/projectHeader/index.jsx
+++ src/sentry/static/sentry/app/components/projectHeader/index.jsx
@@ -5,22 +5,43 @@ import classNames from 'classnames';
 
 import ProjectSelector from './projectSelector';
 import BookmarkToggle from '../projects/bookmarkToggle';
+import DropdownLink from '../dropdownLink';
+import MenuItem from '../menuItem';
 
 import {t} from '../../locale';
+
+import {
+  setActiveEnvironment,
+  clearActiveEnvironment,
+} from '../../actionCreators/environments';
 
 class ProjectHeader extends React.Component {
   static propTypes = {
     project: PropTypes.object.isRequired,
     organization: PropTypes.object.isRequired,
+    environments: PropTypes.array.isRequired,
     activeSection: PropTypes.string,
+    activeEnvironment: PropTypes.object,
+  };
+
+  static defaultProps = {
+    environments: [],
   };
 
   render() {
+    let {project, environments, activeEnvironment} = this.props;
     let navSection = this.props.activeSection;
-    let project = this.props.project;
     let org = this.props.organization;
     let features = new Set(project.features);
     let access = new Set(org.access);
+    let allEnvironmentsLabel = t('All environments');
+
+    // TODO: remove when feature is released
+    let hasEnvironmentsFeature = new Set(org.features).has('environments');
+
+    let activeEnvironmentTitle = activeEnvironment
+      ? activeEnvironment.name
+      : allEnvironmentsLabel;
 
     let projectIconClass = classNames('project-select-bookmark icon icon-star-solid', {
       active: project.isBookmarked,
@@ -28,40 +49,65 @@ class ProjectHeader extends React.Component {
 
     return (
       <div className="sub-header flex flex-container flex-vertically-centered">
-        <div className="p-t-1">
-          <div className="project-select-wrapper">
-            <ProjectSelector organization={org} projectId={project.slug} />
-            <BookmarkToggle orgId={org.slug} project={project}>
-              <a className={projectIconClass} />
-            </BookmarkToggle>
-          </div>
+        <div className="project-header p-t-1">
+          <div className="project-header-main">
+            <div className="project-select-wrapper">
+              <ProjectSelector organization={org} projectId={project.slug} />
+              <BookmarkToggle orgId={org.slug} project={project}>
+                <a className={projectIconClass} />
+              </BookmarkToggle>
+            </div>
 
-          <ul className="nav nav-tabs">
-            <li className={navSection == 'stream' ? 'active' : ''}>
-              <Link to={`/${org.slug}/${project.slug}/`}>{t('Issues')}</Link>
-            </li>
-            {features.has('global-events') && (
-              <li className={navSection == 'events' ? 'active' : ''}>
-                <Link to={`/${org.slug}/${project.slug}/events/`}>{t('Events')}</Link>
+            <ul className="nav nav-tabs">
+              <li className={navSection == 'stream' ? 'active' : ''}>
+                <Link to={`/${org.slug}/${project.slug}/`}>{t('Issues')}</Link>
               </li>
-            )}
-            <li className={navSection == 'dashboard' ? 'active' : ''}>
-              <Link to={`/${org.slug}/${project.slug}/dashboard/`}>{t('Overview')}</Link>
-            </li>
-            <li className={navSection == 'user-feedback' ? 'active' : ''}>
-              <Link to={`/${org.slug}/${project.slug}/user-feedback/`}>
-                {t('User Feedback')}
-              </Link>
-            </li>
-            <li className={navSection == 'releases' ? 'active' : ''}>
-              <Link to={`/${org.slug}/${project.slug}/releases/`}>{t('Releases')}</Link>
-            </li>
-            {access.has('project:write') && (
-              <li className={navSection == 'settings' ? 'active' : ''}>
-                <Link to={`/${org.slug}/${project.slug}/settings/`}>{t('Settings')}</Link>
+              {features.has('global-events') && (
+                <li className={navSection == 'events' ? 'active' : ''}>
+                  <Link to={`/${org.slug}/${project.slug}/events/`}>{t('Events')}</Link>
+                </li>
+              )}
+              <li className={navSection == 'dashboard' ? 'active' : ''}>
+                <Link to={`/${org.slug}/${project.slug}/dashboard/`}>
+                  {t('Overview')}
+                </Link>
               </li>
-            )}
-          </ul>
+              <li className={navSection == 'user-feedback' ? 'active' : ''}>
+                <Link to={`/${org.slug}/${project.slug}/user-feedback/`}>
+                  {t('User Feedback')}
+                </Link>
+              </li>
+              <li className={navSection == 'releases' ? 'active' : ''}>
+                <Link to={`/${org.slug}/${project.slug}/releases/`}>{t('Releases')}</Link>
+              </li>
+              {access.has('project:write') && (
+                <li className={navSection == 'settings' ? 'active' : ''}>
+                  <Link to={`/${org.slug}/${project.slug}/settings/`}>
+                    {t('Settings')}
+                  </Link>
+                </li>
+              )}
+            </ul>
+          </div>
+          {hasEnvironmentsFeature && (
+            <div className="project-header-toggle">
+              <label>Environment</label>
+              <DropdownLink
+                anchorRight={true}
+                title={activeEnvironmentTitle}
+                className="environment-selector-toggle"
+              >
+                <MenuItem onClick={clearActiveEnvironment}>
+                  {allEnvironmentsLabel}
+                </MenuItem>
+                {environments.map(env => (
+                  <MenuItem key={env.id} onClick={() => setActiveEnvironment(env)}>
+                    {env.name}
+                  </MenuItem>
+                ))}
+              </DropdownLink>
+            </div>
+          )}
         </div>
       </div>
     );
