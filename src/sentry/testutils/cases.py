47fa829336d349f21ff4115089d888e46775314f
Ted Kaemming <ted@kaemming.com>
2018-01-09 17:50:46 -0800

    ref(rules): Remove `is_sample` as an argument to `RuleProcessor`

--- src/sentry/testutils/cases.py
+++ src/sentry/testutils/cases.py
@@ -342,7 +342,6 @@ class RuleTestCase(TestCase):
     def get_state(self, **kwargs):
         kwargs.setdefault('is_new', True)
         kwargs.setdefault('is_regression', True)
-        kwargs.setdefault('is_sample', True)
         return EventState(**kwargs)
 
     def assertPasses(self, rule, event=None, **kwargs):
