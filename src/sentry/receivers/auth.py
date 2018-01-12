fd50e2db427d1e624b7d6407b35eee46e0ade043
Lauryn Brown <lauryndbrown@gmail.com>
2018-01-12 12:53:14 -0800

    Added functions to catch when a change is made to the user/useremail. need to verify this is specific enough.

--- src/sentry/receivers/auth.py
+++ src/sentry/receivers/auth.py
@@ -6,7 +6,6 @@ import six
 from django.contrib.auth.models import update_last_login
 from django.contrib.auth.signals import user_logged_in
 from django.db.utils import DatabaseError
-
 from sentry.models import UserOption, LostPasswordHash
 
 
--- src/sentry/receivers/email.py
+++ src/sentry/receivers/email.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from django.db import IntegrityError, transaction
 from django.db.models.signals import post_delete, post_save
 
-from sentry.models import Email, UserEmail
+from sentry.models import Email, UserEmail, User, LostPasswordHash
 
 
 def create_email(instance, created, **kwargs):
@@ -22,5 +22,21 @@ def delete_email(instance, **kwargs):
     Email.objects.filter(email=instance.email).delete()
 
 
+def remove_lost_password_hashes_by_user(instance, **kwargs):
+    LostPasswordHash.objects.filter(user=instance).delete()
+
+
+def remove_lost_password_hashes_by_useremail(instance, **kwargs):
+    LostPasswordHash.objects.filter(user=instance.user).delete()
+
 post_save.connect(create_email, sender=UserEmail, dispatch_uid="create_email", weak=False)
 post_delete.connect(delete_email, sender=UserEmail, dispatch_uid="delete_email", weak=False)
+
+post_save.connect(remove_lost_password_hashes_by_user,
+                  sender=User,
+                  dispatch_uid='remove_lost_password_hashes_by_user',
+                  weak=False)
+post_save.connect(remove_lost_password_hashes_by_useremail,
+                  sender=UserEmail,
+                  dispatch_uid='remove_lost_password_hashes_by_useremail',
+                  weak=False)
