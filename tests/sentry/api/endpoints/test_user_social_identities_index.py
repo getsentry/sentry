bc82446b909f5ad0fe11686d3c9eba9d86520cb1
Billy Vong <billy@sentry.io>
2018-01-11 17:27:49 -0800

    fix test

--- tests/sentry/api/endpoints/test_user_social_identities_index.py
+++ tests/sentry/api/endpoints/test_user_social_identities_index.py
@@ -10,7 +10,9 @@ class UserSocialIdentitiesIndexTest(APITestCase):
     def test_simple(self):
         UserSocialAuth.create_social_auth(self.user, '1234', 'github')
         self.login_as(self.user)
-        url = reverse('sentry-api-0-user-social-identities-index')
+        url = reverse('sentry-api-0-user-social-identities-index', kwargs={
+            'user_id': self.user.id,
+        })
         response = self.client.get(url)
         assert response.status_code == 200, response.content
         assert len(response.data) == 1
