89f5cde693ccd67e918671bc96218b85a8054429
Lyn Nagara <lyn.nagara@gmail.com>
2018-01-11 17:06:29 -0800

    ref(environments): Move default environment definition to store

--- src/sentry/static/sentry/app/stores/environmentStore.jsx
+++ src/sentry/static/sentry/app/stores/environmentStore.jsx
@@ -1,4 +1,16 @@
 import Reflux from 'reflux';
+import {toTitleCase} from '../utils';
+
+const PRODUCTION_ENV_NAMES = new Set([
+  'production',
+  'prod',
+  'release',
+  'master',
+  'trunk',
+]);
+
+const DEFAULT_ENV_NAME = '(Default Environment)';
+const DEFAULT_ROUTING_NAME = 'none';
 
 const EnvironmentStore = Reflux.createStore({
   init() {
@@ -6,7 +18,16 @@ const EnvironmentStore = Reflux.createStore({
   },
 
   loadInitialData(items) {
-    this.items = items;
+    this.items = items.map(item => ({
+      id: item.id,
+      name: item.name,
+      get displayName() {
+        return toTitleCase(item.name) || DEFAULT_ENV_NAME;
+      },
+      get urlRoutingName() {
+        return item.name || DEFAULT_ROUTING_NAME;
+      },
+    }));
     this.trigger(this.items, 'initial');
   },
 
@@ -22,6 +43,15 @@ const EnvironmentStore = Reflux.createStore({
 
   getAll() {
     return this.items;
+  },
+
+  // Default environment is either the first based on the set of common names
+  // or the first in the environment list if none match
+  getDefault() {
+    let allEnvs = this.items;
+    let prodEnvs = allEnvs.filter(e => PRODUCTION_ENV_NAMES.has(e.name));
+
+    return (prodEnvs.length && prodEnvs[0]) || (allEnvs.length && allEnvs[0]) || null;
   },
 });
 
