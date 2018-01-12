42e502792ea987adb386e51da427e6b2ffdef7a5
chrissy <fivefourths@gmail.com>
2018-01-11 16:06:01 -0800

    styles

--- src/sentry/static/sentry/app/views/settings/components/crossSectionLinkButton.jsx
+++ src/sentry/static/sentry/app/views/settings/components/crossSectionLinkButton.jsx
@@ -2,10 +2,33 @@ import styled from 'react-emotion';
 import PropTypes from 'prop-types';
 import React from 'react';
 
-const CrossSectionLinkButton = styled(({href, children}) => (
-  <a href={href}>{children}</a>
+import InlineSvg from '../../../components/inlineSvg';
+
+const CrossSectionLinkButtonText = styled(({children, ...props}) => (
+  <div {...props}>{children}</div>
 ))`
-  color: blue;
+  flex-grow: 1;
+  margin-left: 0.75em;
+`;
+
+const CrossSectionLinkButton = styled(({href, children, ...props}) => (
+  <a href={href} {...props}>
+    <InlineSvg src="icon-mail" width="1.5em" height="1em" />
+    <CrossSectionLinkButtonText>{children}</CrossSectionLinkButtonText>
+    <InlineSvg src="icon-chevron-right" size="1em" />
+  </a>
+))`
+  display: flex;
+  background-color: ${t => t.theme.yellowLightest};
+  color: ${t => t.theme.gray5};
+  border: 1px dashed ${t => t.theme.borderDark};
+  padding: 1em;
+  border-radius: 0.25em;
+  transition: 0.2s border-color;
+
+  &:hover {
+    border-color: ${t => t.theme.blueLight};
+  }
 `;
 
 CrossSectionLinkButton.propTypes = {
