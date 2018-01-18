c90950cd2eb7071b5733127182296f970560997b
Alex Hofsteede <alex@hofsteede.com>
2018-01-18 15:42:06 +1300

    fix: sort ANY tag searches last. (#6972)
    * fix: sort ANY tag searches last.
    
    The existing code did not appear to do what it was supposed to, which is
    to put {'tag': ANY} filters at the end of the list.
    
    Also add test.

--- src/sentry/tagstore/v2/backend.py
+++ src/sentry/tagstore/v2/backend.py
@@ -643,14 +643,14 @@ class V2TagStorage(TagStorage):
             _key__key='sentry:user',
         ).order_by('-last_seen')[:limit])
 
-    def get_group_ids_for_search_filter(self, project_id, environment_id, tags):
+    def get_group_ids_for_search_filter(self, project_id, environment_id, tags, limit=1000):
         from sentry.search.base import ANY, EMPTY
         # Django doesnt support union, so we limit results and try to find
         # reasonable matches
 
         # ANY matches should come last since they're the least specific and
         # will provide the largest range of matches
-        tag_lookups = sorted(six.iteritems(tags), key=lambda x: x != ANY)
+        tag_lookups = sorted(six.iteritems(tags), key=lambda (k, v): v == ANY)
 
         # get initial matches to start the filter
         matches = None
@@ -682,7 +682,7 @@ class V2TagStorage(TagStorage):
                 # restrict matches to only the most recently seen issues
                 base_qs = base_qs.order_by('-last_seen')
 
-            matches = list(base_qs.values_list('group_id', flat=True)[:1000])
+            matches = list(base_qs.values_list('group_id', flat=True)[:limit])
 
             if not matches:
                 return None
