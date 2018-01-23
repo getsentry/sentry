a990059a29cfcf96b5d8b6c61b5e8d555fc15112
chrissy <fivefourths@gmail.com>
2018-01-22 17:34:10 -0800

    hover state and other styles

--- src/sentry/static/sentry/app/views/settings/components/forms/submitOnReturnButton.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/submitOnReturnButton.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
+import styled from 'react-emotion';
 import InlineSvg from '../../../../components/inlineSvg';
 
-const SubmitOnReturnButton = props => {
+const submitOnReturnButtonStyles = p => `
+  background: transparent;
+  box-shadow: none;
+  border: 1px solid transparent;
+  border-radius: ${p.theme.borderRadius};
+  transition: 0.2s all;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 1.4em;
+  width: 1.4em;
+
+  &:hover {
+    cursor: pointer;
+    background: #fff;
+    box-shadow: ${p.theme.dropShadowLight};
+    border: 1px solid ${p.theme.borderLight};
+  }
+`;
+
+const SubmitOnReturnButton = styled(props => {
   return (
     <div {...props}>
-      <InlineSvg size="12px" src="icon-return-key" />
+      <InlineSvg size="0.75em" src="icon-return-key" />
     </div>
   );
-};
+})`
+  ${submitOnReturnButtonStyles};
+`;
 
 export default SubmitOnReturnButton;
