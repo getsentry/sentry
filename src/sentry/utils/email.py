fab24b36193b416b5a42111c9238a26a8bd1d2b3
Brett Hoerner <brett@bretthoerner.com>
2018-01-23 15:18:28 -0600

    Handle feedback.

--- src/sentry/utils/email.py
+++ src/sentry/utils/email.py
@@ -9,7 +9,6 @@ from __future__ import absolute_import
 
 import logging
 import os
-import re
 import six
 import subprocess
 import tempfile
@@ -44,8 +43,7 @@ MAX_RECIPIENTS = 5
 
 # The fake TLD used to construct email addresses when one is required,
 # for example by automatically generated SSO accounts.
-FAKE_EMAIL_DOMAIN = 'sentry-fake'
-FAKE_EMAIL_RE = re.compile("\.{}$".format(FAKE_EMAIL_DOMAIN))
+FAKE_EMAIL_TLD = 'sentry-fake'
 
 logger = logging.getLogger('sentry.mail')
 
@@ -155,18 +153,18 @@ def get_from_email_domain():
 
 def create_fake_email(unique_id, namespace):
     """
-    Generate a fake email of the form: {unique_id}@{namespace}.{FAKE_EMAIL_DOMAIN}
+    Generate a fake email of the form: {unique_id}@{namespace}.{FAKE_EMAIL_TLD}
 
     For example: c74e5b75-e037-4e75-ad27-1a0d21a6b203@cloudfoundry.sentry-fake
     """
-    return "{}@{}.{}".format(unique_id, namespace, FAKE_EMAIL_DOMAIN)
+    return u"{}@{}.{}".format(unique_id, namespace, FAKE_EMAIL_TLD)
 
 
 def is_fake_email(email):
     """
     Returns True if the provided email matches the fake email pattern.
     """
-    return bool(FAKE_EMAIL_RE.search(email))
+    return bool(email.endswith(".{}".format(FAKE_EMAIL_TLD)))
 
 
 def get_email_addresses(user_ids, project=None):
