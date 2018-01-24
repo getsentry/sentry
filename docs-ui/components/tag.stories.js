91d0bc503f8da10b28a821d6d5c17db50f73f89f
chrissy <fivefourths@gmail.com>
2018-01-24 10:36:57 -0800

    improve language for storybook

--- docs-ui/components/tag.stories.js
+++ docs-ui/components/tag.stories.js
@@ -7,17 +7,19 @@ import Tag from 'settings-ui/tag';
 storiesOf('Tags', module)
   .add(
     'default',
-    withInfo('If you pass no type, it will be gray')(() => <Tag>Development</Tag>)
-  )
-  .add(
-    'warning',
-    withInfo('Use this to signal that something is maybe not so great')(() => (
-      <Tag priority="warning">Development</Tag>
+    withInfo('A basic tag-like thing. If you pass no type, it will be gray')(() => (
+      <Tag>Development</Tag>
     ))
   )
   .add(
+    'warning',
+    withInfo(
+      'A warning tag-like thing. Use this to signal that something is maybe not so great'
+    )(() => <Tag priority="warning">Development</Tag>)
+  )
+  .add(
     'success',
-    withInfo('Use this to signal something good')(() => (
+    withInfo('A happy tag-like thing. Use this to signal something good')(() => (
       <Tag priority="success">Development</Tag>
     ))
   );
