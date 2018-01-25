a40bc94ecd08c280c9ab518854b31f04a3bbba08
Lyn Nagara <lyn.nagara@gmail.com>
2018-01-24 16:31:02 -0800

    test: Delete duplicate test

--- tests/js/spec/components/confirm.spec.jsx
+++ tests/js/spec/components/confirm.spec.jsx
@@ -64,30 +64,4 @@ describe('Confirm', function() {
     expect(wrapper.find('Modal').prop('show')).toBe(false);
     expect(mock).toHaveBeenCalled();
   });
-
-  it('clicks Confirm in modal and calls `onConfirm` callback', function() {
-    let mock = jest.fn();
-    let wrapper = shallow(
-      <Confirm message="Are you sure?" onConfirm={mock}>
-        <button>Confirm?</button>
-      </Confirm>
-    );
-
-    expect(mock).not.toHaveBeenCalled();
-
-    wrapper.find('button').simulate('click');
-    wrapper.update();
-
-    // Click "Confirm" button, should be last button
-    let confirm = wrapper
-      .find('Modal')
-      .find('Button')
-      .last();
-
-    confirm.simulate('click');
-    confirm.simulate('click');
-
-    expect(wrapper.find('Modal').prop('show')).toBe(false);
-    expect(mock.mock.calls).toHaveLength(1);
-  });
 });
