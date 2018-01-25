aae2d2ce49bf5552a089073944f51207a13ce817
Ted Kaemming <ted@kaemming.com>
2018-01-25 15:40:25 -0800

    Add filtering to read API

--- tests/sentry/api/endpoints/test_project_environments.py
+++ tests/sentry/api/endpoints/test_project_environments.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from django.core.urlresolvers import reverse
 
-from sentry.models import Environment
+from sentry.models import Environment, EnvironmentProject
 from sentry.testutils import APITestCase
 
 
@@ -38,3 +38,55 @@ class ProjectEnvironmentsTest(APITestCase):
         assert len(response.data) == 2
         assert response.data[0]['name'] == 'production'
         assert response.data[1]['name'] == 'staging'
+
+    def test_visibility_filtering(self):
+        project = self.create_project()
+
+        env1 = Environment.objects.create(
+            project_id=project.id,
+            organization_id=project.organization_id,
+            name='production',
+        )
+
+        EnvironmentProject.objects.create(
+            project=project,
+            environment=env1,
+            is_hidden=False,
+        )
+
+        env2 = Environment.objects.create(
+            project_id=project.id,
+            organization_id=project.organization_id,
+            name='staging',
+        )
+
+        EnvironmentProject.objects.create(
+            project=project,
+            environment=env2,
+            is_hidden=True,
+        )
+
+        self.login_as(user=self.user)
+
+        url = reverse(
+            'sentry-api-0-project-environments',
+            kwargs={
+                'organization_slug': project.organization.slug,
+                'project_slug': project.slug,
+            }
+        )
+        response = self.client.get(url, format='json')
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 1
+        assert response.data[0]['name'] == 'production'
+
+        response = self.client.get(url + '?visibility=all', format='json')
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 2
+        assert response.data[0]['name'] == 'production'
+        assert response.data[1]['name'] == 'staging'
+
+        response = self.client.get(url + '?visibility=hidden', format='json')
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 1
+        assert response.data[0]['name'] == 'staging'
