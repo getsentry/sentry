4ca2c498578cac6262904b41a8969c01beff8af3
Eric Feng <erichfeng@gmail.com>
2018-01-24 17:30:38 -0800

    refactoring components

--- src/sentry/static/sentry/app/components/dugoutHandle.jsx
+++ src/sentry/static/sentry/app/components/dugoutHandle.jsx
@@ -1,8 +1,14 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 import createReactClass from 'create-react-class';
 import classNames from 'classnames';
 
 const DugoutHandle = createReactClass({
+  propTypes: {
+    open: PropTypes.bool,
+    message: PropTypes.string,
+  },
+
   getInitialState(props) {
     return {
       isOpen: false,
