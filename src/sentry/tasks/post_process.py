a9d0f271aab0bfcb6e48f4b6db46f763ede5a3b6
Matt Robenolt <matt@ydekproductions.com>
2018-01-24 16:35:41 -0800

    fix: Using cache.set wrong
    Fixes SENTRY-5SQ

--- src/sentry/tasks/post_process.py
+++ src/sentry/tasks/post_process.py
@@ -31,7 +31,7 @@ def _get_service_hooks(project_id):
     if result is None:
         result = [(h.id, h.events) for h in
                   ServiceHook.objects.filter(project_id=project_id)]
-        cache.set(result, 60)
+        cache.set(cache_key, result, 60)
     return result
 
 
