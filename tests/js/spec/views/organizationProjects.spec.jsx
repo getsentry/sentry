d23e0de086ab121e277812bf51d0ae5ccb155080
maxbittker <maxbittker@gmail.com>
2018-01-30 13:59:36 -0800

    touch up test

--- tests/js/spec/views/organizationProjects.spec.jsx
+++ tests/js/spec/views/organizationProjects.spec.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import {mount} from 'enzyme';
 
 import {Client} from 'app/api';
@@ -7,60 +6,46 @@ import ProjectsStore from 'app/stores/projectsStore';
 import OrganizationProjectsViewContainer from 'app/views/settings/organization/projects/organizationProjectsView';
 
 describe('OrganizationProjectsView', function() {
-  let sandbox;
   let org;
+  let getMock;
+  let putMock;
   beforeEach(function() {
-    sandbox = sinon.sandbox.create();
     let project = TestStubs.Project();
     ProjectsStore.loadInitialData([project]);
     org = TestStubs.Organization();
 
-    Client.addMockResponse({
+    getMock = Client.addMockResponse({
       url: '/organizations/org-slug/stats/',
       body: [[[], 1]],
     });
-    Client.addMockResponse({
+
+    putMock = Client.addMockResponse({
       method: 'PUT',
       url: '/projects/org-slug/project-slug/',
     });
   });
 
   afterEach(function() {
-    sandbox.restore();
+    Client.clearMockResponses();
   });
 
   describe('render()', function() {
     it('Should render the projects in the store', function() {
       let wrapper = mount(
         <OrganizationProjectsViewContainer params={{orgId: org.slug}} />,
-        {
-          context: {
-            organization: org,
-            location: TestStubs.location(),
-            router: TestStubs.router(),
-          },
-          childContextTypes: {
-            organization: PropTypes.object,
-            location: PropTypes.object,
-            router: PropTypes.object,
-          },
-        }
+        TestStubs.routerOrganizationContext()
       );
       expect(wrapper).toMatchSnapshot();
 
       expect(wrapper.find('.project-name').text()).toBe('Project Name');
 
-      expect(
-        Client.findMockResponse('/projects/org-slug/project-slug/', {method: 'PUT'})[0]
-          .callCount
-      ).toBe(0);
+      expect(getMock).toHaveBeenCalledTimes(1);
+
+      expect(putMock).toHaveBeenCalledTimes(0);
 
       wrapper.find('.icon-star-outline').simulate('click');
       expect(wrapper.find('.icon-star-solid')).toBeTruthy();
-      expect(
-        Client.findMockResponse('/projects/org-slug/project-slug/', {method: 'PUT'})[0]
-          .callCount
-      ).toBe(1);
+      expect(putMock).toHaveBeenCalledTimes(1);
     });
   });
 });
