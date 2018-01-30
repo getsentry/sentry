54d1678553cc25068b51bdd6fa9d136e5224e9c1
Ted Kaemming <ted@kaemming.com>
2018-01-30 12:52:11 -0800

    Add to get_or_create.

--- src/sentry/models/groupenvironment.py
+++ src/sentry/models/groupenvironment.py
@@ -21,13 +21,14 @@ class GroupEnvironment(Model):
     __repr__ = sane_repr('group_id', 'environment_id')
 
     @classmethod
-    def get_or_create(cls, group_id, environment_id):
+    def get_or_create(cls, group_id, environment_id, defaults=None):
         cache_key = 'groupenv:1:{}:{}'.format(group_id, environment_id)
         instance = cache.get(cache_key)
         if instance is None:
             instance, created = cls.objects.get_or_create(
                 group_id=group_id,
                 environment_id=environment_id,
+                defaults=defaults,
             )
             cache.set(cache_key, instance, 3600)
         else:
