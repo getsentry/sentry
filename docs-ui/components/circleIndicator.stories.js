9798875617f4a1da0eb82d508d12390139ec06db
Billy Vong <billyvg@users.noreply.github.com>
2018-01-31 11:48:18 -0800

    feat(ui): Add `CircleIndicator` component (#7119)

---
+++ docs-ui/components/circleIndicator.stories.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info';
+import {number, boolean} from '@storybook/addon-knobs';
+
+import CircleIndicator from 'sentry-ui/circleIndicator';
+
+storiesOf('CircleIndicator', module).add(
+  'default',
+  withInfo('Description')(() => {
+    let enabled = boolean('Enabled', true);
+    return (
+      <React.Fragment>
+        <CircleIndicator
+          style={{marginRight: 12}}
+          size={number('Size', 14)}
+          enabled={enabled}
+        />
+        <CircleIndicator size={number('Size', 14)} enabled={!enabled} />
+      </React.Fragment>
+    );
+  })
+);
