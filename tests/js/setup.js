f613aa4455a72acba58fc47d77882aed2ec249f4
Billy Vong <billyvg@users.noreply.github.com>
2018-01-31 08:53:48 -0800

    feat(ui): Change "Debug Symbols" to use `Panel` (#6989)

--- tests/js/setup.js
+++ tests/js/setup.js
@@ -45,10 +45,12 @@ window.TestStubs = {
     context: {
       location: TestStubs.location(),
       router: TestStubs.router(),
+      organization: TestStubs.Organization(),
     },
     childContextTypes: {
       router: PropTypes.object,
       location: PropTypes.object,
+      organization: PropTypes.object,
     },
   }),
 
@@ -118,6 +120,42 @@ window.TestStubs = {
       },
     ];
   },
+
+  DebugSymbols: params => ({
+    debugSymbols: [
+      {
+        dateAdded: '2018-01-31T07:16:26.072Z',
+        dsym: {
+          headers: {'Content-Type': 'text/x-proguard+plain'},
+          sha1: 'e6d3c5185dac63eddfdc1a5edfffa32d46103b44',
+          uuid: '6dc7fdb0-d2fb-4c8e-9d6b-bb1aa98929b1',
+          objectName: 'proguard-mapping',
+          dateCreated: '2018-01-31T07:16:26.010Z',
+          cpuName: 'any',
+          id: '1',
+          symbolType: 'proguard',
+          size: 212,
+        },
+        dsymAppId: 1,
+        version: '1.0',
+        build: '1',
+        id: '1',
+      },
+    ],
+    unreferencedDebugSymbols: [],
+    apps: [
+      {
+        lastSync: '2018-01-31T07:16:26.070Z',
+        name: 'MyApp',
+        iconUrl: null,
+        platforms: '',
+        platform: 'android',
+        appId: 'com.example.myapp',
+        id: '1',
+      },
+    ],
+    ...params,
+  }),
 
   GitHubRepositoryProvider: params => {
     return {
