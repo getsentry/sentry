9b0cb1902fdc9aa225c8653c715cc732b2fe9048
Jess MacQueen <jessmacqueen@gmail.com>
2018-02-01 11:31:32 -0800

    feat(projects): Add ability to add team to project from project teams page

--- src/sentry/static/sentry/app/views/settings/project/projectTeams.jsx
+++ src/sentry/static/sentry/app/views/settings/project/projectTeams.jsx
@@ -8,8 +8,10 @@ import ApiMixin from '../../../mixins/apiMixin';
 import AsyncView from '../../asyncView';
 import Button from '../../../components/buttons/button';
 import Confirm from '../../../components/confirm';
+import DropdownLink from '../../../components/dropdownLink';
 import EmptyMessage from '../components/emptyMessage';
 import IndicatorStore from '../../../stores/indicatorStore';
+import MenuItem from '../../../components/menuItem';
 import Panel from '../components/panel';
 import PanelBody from '../components/panelBody';
 import PanelHeader from '../components/panelHeader';
@@ -85,14 +87,48 @@ const TeamRow = createReactClass({
 class ProjectTeams extends AsyncView {
   getEndpoints() {
     let {orgId, projectId} = this.props.params;
-    return [['teams', `/projects/${orgId}/${projectId}/teams/`]];
+    return [
+      ['projectTeams', `/projects/${orgId}/${projectId}/teams/`],
+      ['allTeams', `/organizations/${orgId}/teams/`],
+    ];
   }
 
   handleRemovedTeam(removedTeam) {
-    this.setState({
-      teams: this.state.teams.filter(team => {
-        return team.slug !== removedTeam.slug;
-      }),
+    this.setState(prevState => {
+      return {
+        projectTeams: this.state.projectTeams.filter(team => {
+          return team.slug !== removedTeam.slug;
+        }),
+      };
+    });
+  }
+
+  handleAddedTeam(team) {
+    this.setState(prevState => {
+      return {
+        projectTeams: this.state.projectTeams.concat([team]),
+      };
+    });
+  }
+
+  handleAdd(team) {
+    if (this.state.loading) return;
+
+    let loadingIndicator = IndicatorStore.add(t('Saving changes..'));
+    let {orgId, projectId} = this.props.params;
+    this.api.request(`/projects/${orgId}/${projectId}/teams/${team.slug}/`, {
+      method: 'POST',
+      success: (d, _, jqXHR) => {
+        this.handleAddedTeam(team);
+        IndicatorStore.remove(loadingIndicator);
+      },
+      error: () => {
+        this.setState({
+          error: true,
+          loading: false,
+        });
+        IndicatorStore.remove(loadingIndicator);
+      },
     });
   }
 
@@ -109,7 +145,7 @@ class ProjectTeams extends AsyncView {
     return [
       <PanelHeader key={'header'}>{t('Team')}</PanelHeader>,
       <PanelBody key={'body'}>
-        {this.state.teams.map(team => {
+        {this.state.projectTeams.map(team => {
           return (
             <TeamRow
               access={access}
@@ -128,12 +164,40 @@ class ProjectTeams extends AsyncView {
   renderBody() {
     let body;
 
-    if (this.state.teams.length > 0) body = this.renderResults();
+    if (this.state.projectTeams.length > 0) body = this.renderResults();
     else body = this.renderEmpty();
+
+    let projectTeams = new Set(this.state.projectTeams.map(team => team.slug));
+    let teamsToAdd = this.state.allTeams.filter(team => {
+      return team.hasAccess && !projectTeams.has(team.slug);
+    });
 
     return (
       <div>
-        <SettingsPageHeader title={t('Teams')} />
+        <SettingsPageHeader
+          title={t('Teams')}
+          action={
+            <DropdownLink
+              anchorRight
+              className="btn btn-primary btn-sm"
+              title={t('Add Team')}
+            >
+              {teamsToAdd.length ? (
+                teamsToAdd.map(team => {
+                  return (
+                    <MenuItem noAnchor={true} key={team.slug}>
+                      <a onClick={this.handleAdd.bind(this, team)}>{team.name}</a>
+                    </MenuItem>
+                  );
+                })
+              ) : (
+                <MenuItem noAnchor={true} key={'empty'}>
+                  <a>{t('No available teams')}</a>
+                </MenuItem>
+              )}
+            </DropdownLink>
+          }
+        />
         <Panel>{body}</Panel>
       </div>
     );
