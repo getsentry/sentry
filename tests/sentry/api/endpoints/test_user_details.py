c731ed899b59ee1328d49f32ac9ab5210779c39c
Billy Vong <billyvg@users.noreply.github.com>
2018-02-02 10:32:17 -0800

    feat(api): Rework user details/appearance endpoints (#7046)
    * Change sudo endpoint to respond with 204
    * remove updating password from user details endpoint
    * move modifying user appearance options into user details endpoint
    * change stacktraceOrder to be an int

--- tests/sentry/api/endpoints/test_user_details.py
+++ tests/sentry/api/endpoints/test_user_details.py
@@ -39,6 +39,10 @@ class UserDetailsTest(APITestCase):
 
         assert resp.status_code == 200, resp.content
         assert resp.data['id'] == six.text_type(user.id)
+        assert resp.data['options']['timezone'] == 'UTC'
+        assert resp.data['options']['language'] == 'en'
+        assert resp.data['options']['stacktraceOrder'] == -1
+        assert not resp.data['options']['clock24Hours']
 
     def test_superuser(self):
         user = self.create_user(email='a@example.com')
@@ -60,48 +64,56 @@ class UserDetailsTest(APITestCase):
 
 
 class UserUpdateTest(APITestCase):
-    def test_simple(self):
-        user = self.create_user(email='a@example.com')
-
-        self.login_as(user=user)
-
-        url = reverse(
+    def setUp(self):
+        self.user = self.create_user(email='a@example.com', is_managed=False, name='example name')
+        self.login_as(user=self.user)
+        self.url = reverse(
             'sentry-api-0-user-details', kwargs={
                 'user_id': 'me',
             }
         )
 
+    def test_simple(self):
         resp = self.client.put(
-            url,
+            self.url,
             data={
                 'name': 'hello world',
-                'username': 'b@example.com',
                 'options': {
-                    'seenReleaseBroadcast': True
+                    'timezone': 'UTC',
+                    'stacktraceOrder': '2',
+                    'language': 'fr',
+                    'clock24Hours': True,
+                    'extra': True,
+                    'seenReleaseBroadcast': True,
                 }
             }
         )
         assert resp.status_code == 200, resp.content
-        assert resp.data['id'] == six.text_type(user.id)
+        assert resp.data['id'] == six.text_type(self.user.id)
 
-        user = User.objects.get(id=user.id)
+        user = User.objects.get(id=self.user.id)
         assert user.name == 'hello world'
-        assert user.email == 'b@example.com'
-        assert user.username == user.email
+        # note: email should not change, removed support for email changing from this endpoint
+        assert user.email == 'a@example.com'
+        assert user.username == 'a@example.com'
         assert UserOption.objects.get_value(
             user=user,
             key='seen_release_broadcast',
         ) is True
+        assert UserOption.objects.get_value(user=self.user, key='timezone') == 'UTC'
+        assert UserOption.objects.get_value(user=self.user, key='stacktrace_order') == '2'
+        assert UserOption.objects.get_value(user=self.user, key='language') == 'fr'
+        assert UserOption.objects.get_value(user=self.user, key='clock_24_hours')
+        assert not UserOption.objects.get_value(user=self.user, key='extra')
 
     def test_superuser(self):
-        user = self.create_user(email='a@example.com')
+        # superuser should be able to change self.user's name
         superuser = self.create_user(email='b@example.com', is_superuser=True)
-
         self.login_as(user=superuser, superuser=True)
 
         url = reverse(
             'sentry-api-0-user-details', kwargs={
-                'user_id': user.id,
+                'user_id': self.user.id,
             }
         )
 
@@ -110,15 +122,67 @@ class UserUpdateTest(APITestCase):
             data={
                 'name': 'hello world',
                 'email': 'c@example.com',
-                'username': 'foo',
                 'isActive': 'false',
             }
         )
         assert resp.status_code == 200, resp.content
-        assert resp.data['id'] == six.text_type(user.id)
+        assert resp.data['id'] == six.text_type(self.user.id)
+
+        user = User.objects.get(id=self.user.id)
+        assert user.name == 'hello world'
+        # note: email should not change, removed support for email changing from this endpoint
+        assert user.email == 'a@example.com'
+        assert user.username == 'a@example.com'
+        assert not user.is_active
+
+    def test_managed_fields(self):
+        assert self.user.name == 'example name'
+        with self.settings(SENTRY_MANAGED_USER_FIELDS=('name', )):
+            resp = self.client.put(
+                self.url,
+                data={
+                    'name': 'new name',
+                }
+            )
+            assert resp.status_code == 200
+
+            # name remains unchanged
+            user = User.objects.get(id=self.user.id)
+            assert user
+
+    def test_change_username_when_different(self):
+        # if email != username and we change username, only username should change
+        user = self.create_user(email="c@example.com", username="diff@example.com")
+        self.login_as(user=user, superuser=False)
+
+        resp = self.client.put(
+            self.url,
+            data={
+                'username': 'new@example.com',
+            }
+        )
+        assert resp.status_code == 200, resp.content
 
         user = User.objects.get(id=user.id)
-        assert user.name == 'hello world'
+
         assert user.email == 'c@example.com'
-        assert user.username == 'foo'
-        assert not user.is_active
+        assert user.username == 'new@example.com'
+
+    def test_change_username_when_same(self):
+        # if email == username and we change username,
+        # keep email in sync
+        user = self.create_user(email="c@example.com", username="c@example.com")
+        self.login_as(user=user)
+
+        resp = self.client.put(
+            self.url,
+            data={
+                'username': 'new@example.com',
+            }
+        )
+        assert resp.status_code == 200, resp.content
+
+        user = User.objects.get(id=user.id)
+
+        assert user.email == 'new@example.com'
+        assert user.username == 'new@example.com'
