f6b73fbbb6ac31c67c3f23a082642157d59ef71f
Billy Vong <billy@sentry.io>
2018-02-02 14:10:51 -0800

    add interactive story

--- docs-ui/components/indicators.stories.js
+++ docs-ui/components/indicators.stories.js
@@ -3,30 +3,70 @@ import {storiesOf} from '@storybook/react';
 import {withInfo} from '@storybook/addon-info';
 import {select} from '@storybook/addon-knobs';
 
-import {Indicators} from 'sentry-ui/indicators';
+import IndicatorContainer, {Indicators} from 'sentry-ui/indicators';
+import IndicatorStore from 'application-root/stores/indicatorStore';
+import Button from 'sentry-ui/buttons/button';
 
 const stories = storiesOf('Toast Indicators', module);
-stories.add(
-  'static',
-  withInfo('Toast Indicators')(() => {
-    let type = select(
-      'Type',
-      {success: 'success', error: 'error', loading: 'loading'},
-      'success'
-    );
+stories
+  .add(
+    'static',
+    withInfo('Toast Indicators')(() => {
+      let type = select(
+        'Type',
+        {success: 'success', error: 'error', loading: 'loading'},
+        'success'
+      );
 
-    return (
-      <div style={{backgroundColor: 'white', padding: 12}}>
-        <Indicators
-          items={[
-            {
-              id: '',
-              type,
-              message: 'Indicator message',
-            },
-          ]}
-        />
-      </div>
-    );
-  })
-);
+      return (
+        <div style={{backgroundColor: 'white', padding: 12}}>
+          <Indicators
+            items={[
+              {
+                id: '',
+                type,
+                message: 'Indicator message',
+              },
+            ]}
+          />
+        </div>
+      );
+    })
+  )
+  .add(
+    'interactive',
+    withInfo('Toast Indicators')(() => {
+      let success;
+      let error;
+
+      return (
+        <div style={{backgroundColor: 'white', padding: 12}}>
+          <Button
+            onClick={() => {
+              if (success) {
+                IndicatorStore.remove(success);
+                success = null;
+              } else {
+                success = IndicatorStore.addSuccess('Success');
+              }
+            }}
+          >
+            Toggle Success
+          </Button>
+          <Button
+            onClick={() => {
+              if (error) {
+                IndicatorStore.remove(error);
+                error = null;
+              } else {
+                error = IndicatorStore.addError('Error');
+              }
+            }}
+          >
+            Toggle Error
+          </Button>
+          <IndicatorContainer />
+        </div>
+      );
+    })
+  );
