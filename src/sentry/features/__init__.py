f966e09e421e6aaf120381ef94a8ab8af30c99e2
Jess MacQueen <jessmacqueen@gmail.com>
2018-02-02 14:01:55 -0800

    ref(features): Add a catchall internal feature flag

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -19,6 +19,7 @@ default_manager.add('organizations:invite-members', OrganizationFeature)  # NOQA
 default_manager.add('organizations:new-settings', OrganizationFeature)  # NOQA
 default_manager.add('organizations:integrations-v3', OrganizationFeature)  # NOQA
 default_manager.add('organizations:require-2fa', OrganizationFeature)  # NOQA
+default_manager.add('organizations:internal-catchall', OrganizationFeature)  # NOQA
 default_manager.add('projects:similarity-view', ProjectFeature)  # NOQA
 default_manager.add('projects:global-events', ProjectFeature)  # NOQA
 default_manager.add('projects:plugins', ProjectPluginFeature)  # NOQA
