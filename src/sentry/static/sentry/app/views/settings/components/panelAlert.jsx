e53c7287ee0df089089fe5f209c7e1114f77aa0a
ckj <chriskjennings@gmail.com>
2018-02-08 16:25:28 -0800

    More straightforward math

--- src/sentry/static/sentry/app/views/settings/components/panelAlert.jsx
+++ src/sentry/static/sentry/app/views/settings/components/panelAlert.jsx
@@ -3,7 +3,7 @@ import styled from 'react-emotion';
 import Alert from '../../../components/alert';
 
 const PanelAlert = styled(Alert)`
-  margin: ${p => p.theme.grid * -2 - 1}px ${p => p.theme.grid * -2 - 1}px
+  margin: -${p => p.theme.grid * 2 + 1}px -${p => p.theme.grid * 2 + 1}px
     ${p => p.theme.grid * 3}px;
   border-radius: 0;
 `;
