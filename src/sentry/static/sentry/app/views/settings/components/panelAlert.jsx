69d7748ff6e3354a6c740d89282708dd82c5a981
ckj <chriskjennings@gmail.com>
2018-02-08 16:37:41 -0800

    Sure

--- src/sentry/static/sentry/app/views/settings/components/panelAlert.jsx
+++ src/sentry/static/sentry/app/views/settings/components/panelAlert.jsx
@@ -3,8 +3,8 @@ import styled from 'react-emotion';
 import Alert from '../../../components/alert';
 
 const PanelAlert = styled(Alert)`
-  margin: -${p => p.theme.grid * 2 + 1}px -${p => p.theme.grid * 2 + 1}px
-    ${p => p.theme.grid * 3}px;
+  margin: ${p =>
+    `-${p.theme.grid * 2 + 1}px -${p.theme.grid * 2 + 1}px ${p.theme.grid * 3}px`};
   border-radius: 0;
 `;
 
