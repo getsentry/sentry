f4ec2ae9afb1cf95db435bae4be4737557afdb18
ted kaemming <ted@kaemming.com>
2018-02-12 14:20:25 -0800

    fix: Correct casing of environment `isHidden` in API (#7181)

--- src/sentry/api/endpoints/project_environments.py
+++ src/sentry/api/endpoints/project_environments.py
@@ -4,22 +4,21 @@ from rest_framework.response import Response
 
 from sentry.api.bases.project import ProjectEndpoint
 from sentry.api.serializers import serialize
-from sentry.models import Environment
+from sentry.models import EnvironmentProject
 
 
 environment_visibility_filter_options = {
     'all': lambda queryset: queryset,
-    'hidden': lambda queryset: queryset.filter(environmentproject__is_hidden=True),
-    'visible': lambda queryset: queryset.exclude(environmentproject__is_hidden=True),
+    'hidden': lambda queryset: queryset.filter(is_hidden=True),
+    'visible': lambda queryset: queryset.exclude(is_hidden=True),
 }
 
 
 class ProjectEnvironmentsEndpoint(ProjectEndpoint):
     def get(self, request, project):
-        queryset = Environment.objects.filter(
-            projects=project,
-            organization_id=project.organization_id,
-        ).order_by('name')
+        queryset = EnvironmentProject.objects.filter(
+            project=project,
+        ).select_related('environment').order_by('environment__name')
 
         visibility = request.GET.get('visibility', 'visible')
         if visibility not in environment_visibility_filter_options:
