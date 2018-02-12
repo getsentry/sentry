1c1274033b0274057f89dc9bfa5c48e93e07c872
ReScO <remon@dead-pixel.nl>
2018-02-12 15:27:04 +0100

    OAuth2 Provider work
    Fixed OAuth2Provider
    - Removed fixed config
    - Added replacement methods
    
    Fixed SlackIdentityProvider to use new methods
    
    Added GitLabIdentityProvider

---
+++ src/sentry/identity/gitlab/provider.py
@@ -0,0 +1,48 @@
+from __future__ import absolute_import
+
+from sentry import options
+from sentry.options.manager import FLAG_PRIORITIZE_DISK
+from sentry.identity.oauth2 import OAuth2Provider
+
+options.register('gitlab.client-id', flags=FLAG_PRIORITIZE_DISK)
+options.register('gitlab.client-secret', flags=FLAG_PRIORITIZE_DISK)
+options.register('gitlab.url', flags=FLAG_PRIORITIZE_DISK)
+
+
+class GitLabIdentityProvider(OAuth2Provider):
+    key = 'gitlab'
+    name = 'Gitlab'
+
+    oauth_scopes = (
+        'read_user',
+        'api',
+    )
+
+    def get_oauth_client_id(self):
+        return options.get('gitlab.client-id')
+
+    def get_oauth_client_secret(self):
+        return options.get('gitlab.client-secret')
+
+    def get_oauth_access_token_url(self):
+        return '{}/oauth/token'.format(options.get('gitlab.url'))
+
+    def get_oauth_authorize_url(self):
+        return '{}/oauth/authorize'.format(options.get('gitlab.url'))
+
+    def build_identity(self, data):
+        data = data['data']
+
+        return {
+            'type': 'slack',
+            'id': data['user']['id'],
+            'email': data['user']['email'],
+            'scopes': sorted(data['scope'].split(',')),
+            'data': self.get_oauth_data(data),
+        }
+
+    def get_pipeline(self):
+        pass
+
+    def refresh_identity(self, auth_identity):
+        pass
