d42457c64c1be41a1055253f644daf4809b1d629
Lauryn Brown <lauryndbrown@gmail.com>
2018-02-13 13:43:40 -0800

    feat(api): Add environment filter to UserReport Endpoints (#7195)
    * Added environment to UserReport in the event manager pipeline.
    
    * Fixed typo with environment
    
    * Added tests and changes to the endpoint so that userreports can be filteredby environments
    
    * Fixed some of the tests. Need more inforation on no environment vs all environments.
    
    * All tests pass will need feedback.
    
    * Added project_id to event query
    
    * making sure event manager tests are updated
    
    * fixed spacing.
    
    * Added filtering by environment to group user reports. made a test case so I could reuse test code.
    
    * Added queryset filter chaining and changed empty queryset to UserRerport.objects.none()
    
    * Removed unnecessary select_related.
    
    * Removed error catching around event.get_environment. Altered previous tests in CreateProjectReportTest to reflect that change. Changed my tests to ensure that filtering by environments happens when there is more than one group.

--- tests/sentry/api/endpoints/test_project_user_reports.py
+++ tests/sentry/api/endpoints/test_project_user_reports.py
@@ -1,9 +1,11 @@
 from __future__ import absolute_import
 
 import six
+from exam import fixture
 
-from sentry.testutils import APITestCase
-from sentry.models import EventUser, GroupStatus, UserReport
+from sentry.testutils import APITestCase, UserReportEnvironmentTestCase
+from sentry.models import EventUser, Environment, GroupStatus, UserReport
+from sentry.event_manager import EventManager
 
 
 class ProjectUserReportListTest(APITestCase):
@@ -87,12 +89,23 @@ class ProjectUserReportListTest(APITestCase):
 
 
 class CreateProjectUserReportTest(APITestCase):
+    def make_environment(self, project, name='production'):
+        environment = Environment.objects.create(
+            project_id=project.id,
+            organization_id=project.organization_id,
+            name=name,
+        )
+        environment.add_project(project)
+        return environment
+
     def test_simple(self):
         self.login_as(user=self.user)
 
         project = self.create_project()
         group = self.create_group(project=project)
-        event = self.create_event(group=group)
+        environment = self.make_environment(project)
+        event = self.create_event(group=group, tags={
+            'environment': environment.name})
 
         url = '/api/0/projects/{}/{}/user-feedback/'.format(
             project.organization.slug,
@@ -125,7 +138,10 @@ class CreateProjectUserReportTest(APITestCase):
 
         project = self.create_project()
         group = self.create_group(project=project)
-        event = self.create_event(group=group)
+        environment = self.make_environment(project)
+        event = self.create_event(group=group, tags={
+            'environment': environment.name})
+
         UserReport.objects.create(
             group=group,
             project=project,
@@ -166,9 +182,11 @@ class CreateProjectUserReportTest(APITestCase):
 
         project = self.create_project()
         group = self.create_group(project=project)
+        environment = self.make_environment(project)
         event = self.create_event(
             group=group, tags={
                 'sentry:user': 'email:foo@example.com',
+                'environment': environment.name,
             }
         )
         euser = EventUser.objects.create(
@@ -214,3 +232,121 @@ class CreateProjectUserReportTest(APITestCase):
 
         euser = EventUser.objects.get(id=euser.id)
         assert euser.name == 'Foo Bar'
+
+
+class ProjectUserReportByEnvironmentsTest(UserReportEnvironmentTestCase):
+
+    def setUp(self):
+        super(ProjectUserReportByEnvironmentsTest, self).setUp()
+        group_2 = self.create_group()
+        env1_events = self.create_events_for_environment(group_2, self.env1, 5)
+        env2_events = self.create_events_for_environment(group_2, self.env2, 5)
+
+        self.env1_userreports += self.create_user_report_for_events(
+            self.project, group_2, env1_events, self.env1)
+        self.env2_userreports += self.create_user_report_for_events(
+            self.project, group_2, env2_events, self.env2)
+
+        self.env1_events += env1_events
+        self.env2_events += env2_events
+
+    @fixture
+    def path(self):
+        return '/api/0/projects/{}/{}/user-feedback/'.format(
+            self.project.organization.slug,
+            self.project.slug,
+        )
+
+    def test_environment_gets_user_report(self):
+        event_id = 'a' * 32
+        manager = EventManager(
+            self.make_event(
+                environment=self.env1.name,
+                event_id=event_id,
+                group=self.group))
+        manager.normalize()
+        manager.save(self.project.id)
+
+        self.login_as(user=self.user)
+        response = self.client.post(
+            self.path,
+            data={
+                'event_id': event_id,
+                'email': 'foo@example.com',
+                'name': 'Foo Bar',
+                'comments': 'It broke!',
+            }
+        )
+
+        assert response.status_code == 200, response.content
+        assert UserReport.objects.get(event_id=event_id).environment == self.env1
+
+    def test_user_report_gets_environment(self):
+        event_id = 'a' * 32
+        self.login_as(user=self.user)
+        response = self.client.post(
+            self.path,
+            data={
+                'event_id': event_id,
+                'email': 'foo@example.com',
+                'name': 'Foo Bar',
+                'comments': 'It broke!',
+            }
+        )
+
+        manager = EventManager(
+            self.make_event(
+                environment=self.env1.name,
+                event_id=event_id,
+                group=self.group))
+        manager.normalize()
+        manager.save(self.project.id)
+        assert response.status_code == 200, response.content
+        assert UserReport.objects.get(event_id=event_id).environment == self.env1
+
+    def test_specified_enviroment(self):
+        self.login_as(user=self.user)
+
+        response = self.client.get(self.path + '?environment=' + self.env1.name)
+        assert response.status_code == 200, response.content
+        assert len(response.data) == len(self.env1_events)
+        self.assert_same_userreports(response.data, self.env1_userreports)
+
+        response = self.client.get(self.path + '?environment=' + self.env2.name)
+        assert response.status_code == 200, response.content
+        assert len(response.data) == len(self.env2_events)
+        self.assert_same_userreports(response.data, self.env2_userreports)
+
+    def test_no_environment_does_not_exists(self):
+        self.login_as(user=self.user)
+        response = self.client.get(self.path + '?environment=')
+        assert response.status_code == 200
+        assert response.data == []
+
+    def test_no_environment(self):
+        self.login_as(user=self.user)
+
+        empty_env = self.create_environment(self.project, u'')
+        empty_env_events = self.create_events_for_environment(self.group, empty_env, 5)
+        userreports = self.create_user_report_for_events(
+            self.project, self.group, empty_env_events, empty_env)
+        response = self.client.get(self.path + '?environment=')
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == len(userreports)
+        self.assert_same_userreports(response.data, userreports)
+
+    def test_all_environments(self):
+        self.login_as(user=self.user)
+        response = self.client.get(self.path)
+        userreports = self.env1_userreports + self.env2_userreports
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == len(userreports)
+        self.assert_same_userreports(response.data, userreports)
+
+    def test_invalid_environment(self):
+        self.login_as(user=self.user)
+        response = self.client.get(self.path + '?environment=invalid_env')
+        assert response.status_code == 200
+        assert response.data == []
