96dfaf1c239a1ee5c3b5b65e7199b26736aeb8fe
ckj <chriskjennings@gmail.com>
2018-02-14 15:40:28 -0800

    Make form fields not look disabled

--- src/sentry/static/sentry/app/views/settings/components/forms/formField/formFieldWrapper.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/formField/formFieldWrapper.jsx
@@ -2,9 +2,6 @@ import React from 'react';
 import styled, {css} from 'react-emotion';
 import {Flex} from 'grid-emotion';
 
-import SettingsInputField from '../styled/input';
-import SettingsTextAreaField from '../styled/textarea';
-
 const inlineStyle = p =>
   p.inline
     ? css`
@@ -26,13 +23,6 @@ const FormFieldWrapper = styled(({highlighted, inline, ...props}) => <Flex {...p
   padding: 0.9em 0 0.9em 1.3em;
   border-bottom: 1px solid ${p => p.theme.borderLight};
   transition: background 0.15s;
-
-  &:hover,
-  &:focus {
-    ${SettingsInputField}, ${SettingsTextAreaField} {
-      border: 1px solid ${p => p.theme.borderDark};
-    }
-  }
 
   ${inlineStyle} ${highlightedStyle} &:last-child {
     border-bottom: none;
