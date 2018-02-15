28e38f795e4d27942c4f01656c5415179e73c880
Billy Vong <billy@sentry.io>
2018-02-15 12:48:11 -0800

    fix team settings when updating slug

--- tests/js/spec/views/teamSettings.spec.jsx
+++ tests/js/spec/views/teamSettings.spec.jsx
@@ -1,8 +1,9 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount, shallow} from 'enzyme';
 
 import TeamSettings from 'app/views/settings/team/teamSettings.old';
+import NewTeamSettings from 'app/views/settings/team/teamSettings';
 
 const childContextTypes = {
   organization: PropTypes.object,
@@ -10,6 +11,7 @@ const childContextTypes = {
   location: PropTypes.object,
 };
 
+// #NEW-SETTINGS
 describe('TeamSettings', function() {
   describe('render()', function() {
     it('renders', function() {
@@ -32,5 +34,71 @@ describe('TeamSettings', function() {
       wrapper.update();
       expect(wrapper).toMatchSnapshot();
     });
+  });
+});
+
+describe('NewTeamSettings', function() {
+  beforeEach(function() {
+    MockApiClient.clearMockResponses();
+    sinon.stub(window.location, 'assign');
+  });
+
+  afterEach(function() {
+    window.location.assign.restore();
+  });
+
+  it('can change name and slug', function(done) {
+    let team = TestStubs.Team();
+    let putMock = MockApiClient.addMockResponse({
+      url: `/teams/org/${team.slug}/`,
+      method: 'PUT',
+    });
+
+    let wrapper = mount(
+      <NewTeamSettings
+        routes={[]}
+        params={{orgId: 'org', teamId: team.slug}}
+        team={team}
+        onTeamChange={() => {}}
+      />,
+      TestStubs.routerContext()
+    );
+
+    wrapper
+      .find('input[name="name"]')
+      .simulate('change', {target: {value: 'New Name'}})
+      .simulate('blur');
+
+    expect(putMock).toHaveBeenCalledWith(
+      `/teams/org/${team.slug}/`,
+      expect.objectContaining({
+        data: {
+          name: 'New Name',
+        },
+      })
+    );
+
+    wrapper
+      .find('input[name="slug"]')
+      .simulate('change', {target: {value: 'new-slug'}})
+      .simulate('blur');
+
+    expect(putMock).toHaveBeenCalledWith(
+      `/teams/org/${team.slug}/`,
+      expect.objectContaining({
+        data: {
+          slug: 'new-slug',
+        },
+      })
+    );
+
+    setTimeout(() => {
+      expect(
+        window.location.assign.calledWith(
+          '/settings/organization/org/teams/new-slug/settings/'
+        )
+      ).toBe(true);
+      done();
+    }, 1);
   });
 });
