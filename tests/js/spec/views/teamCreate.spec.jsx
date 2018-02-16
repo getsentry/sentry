7ab13902c02cf4242d374e48246d73f0e9f971e3
Ben Vinegar <ben.vinegar@gmail.com>
2018-02-16 09:39:34 -0800

    fix(ui): After team creation, redirect to team settings (#7264)
    * After team creation, redirect to team settings
    * Redirect to root team route
    * Add tests to satisfy codecov
    * Use window.location.assign

--- tests/js/spec/views/teamCreate.spec.jsx
+++ tests/js/spec/views/teamCreate.spec.jsx
@@ -19,4 +19,54 @@ describe('TeamCreate', function() {
       expect(wrapper).toMatchSnapshot();
     });
   });
+
+  describe('onSubmitSuccess()', function() {
+    let wrapper;
+    let locationAssignMock;
+
+    beforeEach(function() {
+      wrapper = shallow(
+        <TeamCreate
+          params={{
+            orgId: 'org',
+          }}
+        />,
+        {
+          context: {
+            router: TestStubs.router(),
+            organization: {
+              id: '1337',
+            },
+          },
+        }
+      );
+      locationAssignMock = jest.fn();
+      window.location.assign = locationAssignMock;
+      wrapper.instance().redirect = locationAssignMock;
+    });
+
+    it('redirects to legacy team settings', function() {
+      wrapper.instance().onSubmitSuccess({
+        slug: 'new-team',
+      });
+      expect(locationAssignMock).toBeCalledWith(
+        '/organizations/org/projects/new/?team=new-team'
+      );
+    });
+
+    it('redirects to new team settings', function() {
+      wrapper.setContext({
+        organization: {
+          id: '1337',
+          features: ['internal-catchall'],
+        },
+      });
+      wrapper.instance().onSubmitSuccess({
+        slug: 'new-team',
+      });
+      expect(locationAssignMock).toBeCalledWith(
+        '/settings/organization/org/teams/new-team/'
+      );
+    });
+  });
 });
