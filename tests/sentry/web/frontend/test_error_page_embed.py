5f1e23e13954f67cbba03b44d1cd05d7dd7e4ce1
Lauryn Brown <lauryndbrown@gmail.com>
2018-02-16 13:31:09 -0800

    fix(api): Error page adds environment (#7268)
    * Added the code for the error_page_embed to update env. Need to add tests.
    
    * Added tests to error page to make sure environment is added.
    
    * Reverted changes made to cases.

--- tests/sentry/web/frontend/test_error_page_embed.py
+++ tests/sentry/web/frontend/test_error_page_embed.py
@@ -3,9 +3,11 @@ from __future__ import absolute_import
 from django.core.urlresolvers import reverse
 from six.moves.urllib.parse import quote
 from uuid import uuid4
+import logging
 
-from sentry.models import UserReport
+from sentry.models import Environment, UserReport
 from sentry.testutils import TestCase
+from sentry.event_manager import EventManager
 
 
 class ErrorPageEmbedTest(TestCase):
@@ -91,3 +93,74 @@ class ErrorPageEmbedTest(TestCase):
             HTTP_REFERER='http://example.com'
         )
         assert resp.status_code == 400
+
+
+class ErrorPageEmbedEnvironmentTest(TestCase):
+
+    urls = 'sentry.conf.urls'
+
+    def setUp(self):
+        self.project = self.create_project()
+        self.project.update_option('sentry:origins', ['example.com'])
+        self.key = self.create_project_key(self.project)
+        self.event_id = uuid4().hex
+        self.path = '%s?eventId=%s&dsn=%s' % (
+            reverse('sentry-error-page-embed'), quote(self.event_id), quote(self.key.dsn_public),
+        )
+        self.environment = Environment.objects.create(
+            project_id=self.project.id,
+            organization_id=self.project.organization_id,
+            name='production',
+        )
+        self.environment.add_project(self.project)
+
+    def make_event(self, **kwargs):
+        result = {
+            'event_id': 'a' * 32,
+            'message': 'foo',
+            'timestamp': 1403007314.570599,
+            'level': logging.ERROR,
+            'logger': 'default',
+            'tags': [],
+        }
+        result.update(kwargs)
+        manager = EventManager(result)
+        manager.normalize()
+        manager.save(self.project.id)
+
+    def test_environment_gets_user_report(self):
+        self.make_event(
+            environment=self.environment.name,
+            event_id=self.event_id,
+            group=self.group,
+        )
+        self.login_as(user=self.user)
+        response = self.client.post(
+            self.path, {
+                'name': 'Jane Doe',
+                'email': 'jane@example.com',
+                'comments': 'This is an example!',
+            },
+            HTTP_REFERER='http://example.com'
+        )
+
+        assert response.status_code == 200, response.content
+        assert UserReport.objects.get(event_id=self.event_id).environment == self.environment
+
+    def test_user_report_gets_environment(self):
+        self.login_as(user=self.user)
+        response = self.client.post(
+            self.path, {
+                'name': 'Jane Doe',
+                'email': 'jane@example.com',
+                'comments': 'This is an example!',
+            },
+            HTTP_REFERER='http://example.com'
+        )
+        self.make_event(
+            environment=self.environment.name,
+            event_id=self.event_id,
+            group=self.group,
+        )
+        assert response.status_code == 200, response.content
+        assert UserReport.objects.get(event_id=self.event_id).environment == self.environment
