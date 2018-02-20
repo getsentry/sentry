780126a1b5e9514d62f1f9bb801d1423fbfb4a1f
Billy Vong <billy@sentry.io>
2018-02-19 18:03:51 -0800

    fix some proptypes

--- src/sentry/static/sentry/app/components/autoSelectText.jsx
+++ src/sentry/static/sentry/app/components/autoSelectText.jsx
@@ -6,14 +6,14 @@ import {selectText} from '../utils/selectText';
 class AutoSelectText extends React.Component {
   static propTypes = {
     /**
-     * Can be a `string` for a simple auto select div container.
+     * Can be a `node` for a simple auto select div container.
      * When children is a render function, it is passed 2 functions:
      * - `doMount` - should be applied on parent element's `ref`
      *   (or `innerRef` for styled components) whose children is the
      *   text to be copied
      * - `doSelect` - selects text
      */
-    children: PropTypes.oneOfType([PropTypes.func, PropTypes.string]),
+    children: PropTypes.oneOfType([PropTypes.func, PropTypes.node]),
   };
 
   selectText = () => {
