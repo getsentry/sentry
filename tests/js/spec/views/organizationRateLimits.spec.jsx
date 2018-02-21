5008321b75850cedb9f0142f9f1b8c3ee8979182
Billy Vong <billyvg@users.noreply.github.com>
2018-02-21 14:00:56 -0800

    feat(ui): Update Org Stats (#7307)

---
+++ tests/js/spec/views/organizationRateLimits.spec.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import {mount} from 'enzyme';
+
+import OrganizationStats from 'app/views/organizationStats';
+
+describe('OrganizationStats', function() {
+  beforeEach(function() {
+    MockApiClient.clearMockResponses();
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/projects/',
+      method: 'GET',
+      body: [TestStubs.Project()],
+    });
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/stats/',
+      method: 'GET',
+      body: [],
+    });
+  });
+
+  it('renders', function(done) {
+    let wrapper = mount(
+      <OrganizationStats params={{orgId: 'org-slug'}} location={TestStubs.location()} />,
+      TestStubs.routerContext()
+    );
+
+    setTimeout(() => {
+      wrapper.update();
+      expect(wrapper).toMatchSnapshot();
+      done();
+    }, 1);
+  });
+});
