56f0b7d7073ede6d653ced29d3272891942f1eaa
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-02-20 16:19:58 -0800

    Cleanup global integrations list style

--- src/sentry/static/sentry/app/views/projectPlugins/organizationIntegrations.jsx
+++ src/sentry/static/sentry/app/views/projectPlugins/organizationIntegrations.jsx
@@ -1,14 +1,27 @@
-import {Flex, Box} from 'grid-emotion';
+import {Box} from 'grid-emotion';
 import React from 'react';
+import styled from 'react-emotion';
 
 import {t} from '../../locale';
 import AsyncComponent from '../../components/asyncComponent';
+import Link from '../../components/link';
 import Panel from '../settings/components/panel';
 import PanelBody from '../settings/components/panelBody';
 import PanelHeader from '../settings/components/panelHeader';
-import Row from '../settings/components/row';
 import PluginIcon from '../../plugins/components/pluginIcon';
-import Link from '../../components/link';
+import Row from '../settings/components/row';
+import theme from '../../utils/theme';
+
+const ProviderName = styled.div`
+  font-size: 16px;
+  font-weight: bold;
+  margin-bottom: 3px;
+`;
+
+const TeamName = styled.div`
+  color: ${p => p.theme.gray2};
+  font-size: 14px;
+`;
 
 export default class OrganizationIntegrations extends AsyncComponent {
   // TODO: proptypes
@@ -23,8 +36,6 @@ export default class OrganizationIntegrations extends AsyncComponent {
     // TODO: This is for sure being passsed in to many things
     let {orgId, projectId} = this.props.params;
 
-    console.log(this.props.params);
-
     return (
       <Panel>
         <PanelHeader disablePadding={true}>
@@ -36,15 +47,19 @@ export default class OrganizationIntegrations extends AsyncComponent {
           {this.state.config.providers.map(provider => {
             return (
               <Row key={provider.key}>
-                <Box pr={1}>
-                  <PluginIcon size={24} pluginId={provider.key} />
+                <Box>
+                  <PluginIcon size={32} pluginId={provider.key} />
                 </Box>
-                <Box py={1 / 2} flex={1}>
-                  <Link
-                    to={`/settings/organization/${orgId}/project/${projectId}/integrations/${provider.key}/`}
-                  >
-                    {provider.name}
-                  </Link>
+                <Box px={2} flex={1}>
+                  <ProviderName>
+                    <Link
+                      to={`/settings/organization/${orgId}/project/${projectId}/integrations/${provider.key}/`}
+                      css={{color: theme.gray5}}
+                    >
+                      {provider.name}
+                    </Link>
+                  </ProviderName>
+                  <TeamName>{provider.metadata.author}</TeamName>
                 </Box>
               </Row>
             );
