32f200d3187571ad198c2b2674df47a4a485bbbe
Chris Clark <christopherchristopherclark@gmail.com>
2018-02-22 18:30:24 -0800

    feat(ui): dropdown button component (#7308)
    * basic structure for storybook component dev
    
    * copy pasta autocomplete
    
    * naive onblur solution, click shield solution probably necessary :((((
    
    * fix bug using pointer-events
    
    * working solution using _.throttle, might consider nesting DropdownMenus
    
    * couple very light positioning styles
    
    * allow component injection into item list
    
    * add chevron
    
    * button styles
    
    * border radius for dropdown
    
    * whoops
    
    * fix some typos in test data
    
    * use styled components for highlighted state
    
    * style input
    
    * gooder padding
    
    * hover states
    
    * allow literally whatever to be put in as a group label
    
    * restructure the code so that groups are completely removed if no items are present
    
    * correct indexing for elements with labels mixed in
    
    * toString function should not return content because it can be html
    
    * close on select
    
    * onSelect and items params
    
    * onSelect can be optional
    
    * add indeces at the end so that filtering doesn't effect arrow key stuff
    
    * add label html example to storybook
    
    * re-add onBlur prop which accidentally got removed
    
    * fix styles
    
    * fix some style hover problems
    
    * remove border radius on menu when opengs
    
    * move dropdownAutoComplete into it's own component
    
    * allow dropdownAutoComplete to manage state and have whatever control it
    
    * allows communication of openNess to children
    
    * pass onBlur event directly to the autocomplete component using the getInputProps object
    
    * seperate dropdown button and dropdown autocomplete inside of storybook
    
    * dropdownButton is a pure function
    
    * autocomplete onblur handles the settimeout stuff so we don't need to worry about it
    
    * remove default option for now
    
    * couple small changes to methods
    
    * don't need to check and see if the menu is open, we can just open it now
    
    * refactor filtering and data transformation to be shorter and easier to read
    
    * revert some stuff that we don't need in this pull
    
    * improved storybook docs
    
    * improved storybook docs
    
    * dropdownAutoComplete tests
    
    * remove accidentally added .jsx from import
    
    * use more common import for input styles (why we have the other i'm not sure)
    
    * keep items grouped for longer (but not too long)
    
    * keep items grouped for longer (but not too long)
    
    * use value as key not index
    
    * more robust propTypes
    
    * a little dash of formatting and one small bugfix for ungrouped inputs

---
+++ src/sentry/static/sentry/app/components/dropdownButton.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import styled from 'react-emotion';
+import Button from './buttons/button';
+import InlineSvg from './inlineSvg';
+
+const DropdownButton = ({isOpen, children}) => {
+  return (
+    <StyledButton isOpen={isOpen}>
+      <StyledChevronDown />
+      {children}
+    </StyledButton>
+  );
+};
+
+DropdownButton.propTypes = {
+  isOpen: PropTypes.bool,
+};
+
+const StyledChevronDown = styled(props => (
+  <InlineSvg src="icon-chevron-down" {...props} />
+))`
+  margin-right: 0.5em;
+`;
+
+const StyledButton = styled(props => <Button {...props} />)`
+  border-bottom-right-radius: ${p => (p.isOpen ? 0 : p.theme.borderRadius)};
+  border-bottom-left-radius: ${p => (p.isOpen ? 0 : p.theme.borderRadius)};
+  position: relative;
+  z-index; 1;
+  box-shadow: none;
+
+  &, &:hover { border-bottom-color: ${p =>
+    p.isOpen ? 'transparent' : p.theme.borderDark};}
+`;
+
+export default DropdownButton;
