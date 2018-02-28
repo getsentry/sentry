3ea2116b2e8f3d8b12cd518a4ebc8ace8058d9b0
Matt Robenolt <matt@ydekproductions.com>
2018-02-27 17:13:28 -0800

    feat(ownership): Add grammar parser

--- src/sentry/models/projectownership.py
+++ src/sentry/models/projectownership.py
@@ -4,8 +4,10 @@ from jsonfield import JSONField
 
 from django.db import models
 from django.utils import timezone
+
 from sentry.db.models import Model, sane_repr
 from sentry.db.models.fields import FlexibleForeignKey
+from sentry.ownership.grammar import dump_schema, parse_rules
 
 
 class ProjectOwnership(Model):
@@ -24,3 +26,10 @@ class ProjectOwnership(Model):
         db_table = 'sentry_projectownership'
 
     __repr__ = sane_repr('project_id', 'is_active')
+
+    def save(self, *args, **kwargs):
+        if self.raw is None:
+            self.schema = None
+        else:
+            self.schema = dump_schema(parse_rules(self.raw))
+        return super(ProjectOwnership, self).save(*args, **kwargs)
