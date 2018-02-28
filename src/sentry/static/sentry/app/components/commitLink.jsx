0d1d7d86d04e3759f539c49b9eab31409b776042
David Cramer <dcramer@gmail.com>
2018-02-28 15:47:43 -0800

    ref(committers): Redesign suspected commits
    This utilizes the existing commit styling used in releases and expands the list to include all suspects.

--- src/sentry/static/sentry/app/components/commitLink.jsx
+++ src/sentry/static/sentry/app/components/commitLink.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
-import InlineSvg from '../../components/inlineSvg';
+import InlineSvg from './inlineSvg';
 
 class CommitLink extends React.Component {
   static propTypes = {
