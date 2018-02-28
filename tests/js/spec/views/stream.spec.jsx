e5c80c5688a032a09d3ffc09383e39010f1f7946
Lyn Nagara <lyn.nagara@gmail.com>
2018-02-28 13:33:26 -0800

    test(stream): Add stream snapshot (#7406)

--- tests/js/spec/views/stream.spec.jsx
+++ tests/js/spec/views/stream.spec.jsx
@@ -186,6 +186,7 @@ describe('Stream', function() {
         loading: false,
         dataLoading: false,
       });
+      expect(wrapper).toMatchSnapshot();
       expect(wrapper.find('.group-list').length).toBeTruthy();
     });
 
