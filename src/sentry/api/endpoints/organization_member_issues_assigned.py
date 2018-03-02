4718d1bae4d3d7077abf24ec22c2611bec561966
maxbittker <maxbittker@gmail.com>
2018-03-01 20:01:46 -0800

    inital query

--- src/sentry/api/endpoints/organization_member_issues_assigned.py
+++ src/sentry/api/endpoints/organization_member_issues_assigned.py
@@ -2,13 +2,16 @@ from __future__ import absolute_import
 
 from sentry.api.bases import OrganizationIssuesEndpoint
 from sentry.models import Group
+from django.db.models import Q
 
 
 class OrganizationMemberIssuesAssignedEndpoint(OrganizationIssuesEndpoint):
     def get_queryset(self, request, organization, member, project_list):
         return Group.objects.filter(
-            assignee_set__user=member.user,
-            assignee_set__project__in=project_list,
+            Q(assignee_set__user=member.user,
+                assignee_set__project__in=project_list) |
+            Q(assignee_set__team__in=member.get_teams(),
+              assignee_set__project__in=project_list)
         ).extra(
             select={'sort_by': 'sentry_groupasignee.date_added'},
         ).order_by('-sort_by')
