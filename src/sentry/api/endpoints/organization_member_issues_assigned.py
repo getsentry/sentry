70f46d8d6b6ba9a97f474c1f0d23803a088d57ea
maxbittker <maxbittker@gmail.com>
2018-03-05 15:00:58 -0800

    test

--- src/sentry/api/endpoints/organization_member_issues_assigned.py
+++ src/sentry/api/endpoints/organization_member_issues_assigned.py
@@ -8,10 +8,8 @@ from django.db.models import Q
 class OrganizationMemberIssuesAssignedEndpoint(OrganizationIssuesEndpoint):
     def get_queryset(self, request, organization, member, project_list):
         return Group.objects.filter(
-            Q(assignee_set__user=member.user,
-                assignee_set__project__in=project_list) |
-            Q(assignee_set__team__in=member.get_teams(),
-              assignee_set__project__in=project_list)
+            Q(assignee_set__user=member.user, assignee_set__project__in=project_list) |
+            Q(assignee_set__team__in=member.get_teams())
         ).extra(
             select={'sort_by': 'sentry_groupasignee.date_added'},
         ).order_by('-sort_by')
