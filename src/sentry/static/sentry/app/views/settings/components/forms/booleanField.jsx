58d14279c2e59091c1ceefd98bbd6e4d0429387b
Billy Vong <billy@sentry.io>
2018-03-07 06:13:17 +0900

    add `confirm` prop to BooleanField to open a Confirm modal

--- src/sentry/static/sentry/app/views/settings/components/forms/booleanField.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/booleanField.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import InputField from './inputField';
 import Switch from '../../../../components/switch';
+import Confirm from '../../../../components/confirm';
 
 export default class BooleanField extends InputField {
+  static propTypes = {
+    ...InputField.propTypes,
+    confirm: PropTypes.node,
+  };
   coerceValue(value) {
     return value ? true : false;
   }
 
-  onChange = (value, onChange, onBlur, e) => {
+  handleChange = (value, onChange, onBlur, e) => {
     // We need to toggle current value because Switch is not an input
     let newValue = this.coerceValue(!value);
     onChange(newValue, e);
@@ -16,17 +22,35 @@ export default class BooleanField extends InputField {
   };
 
   render() {
+    let {confirm, ...fieldProps} = this.props;
+
     return (
       <InputField
-        {...this.props}
+        {...fieldProps}
         field={({onChange, onBlur, value, disabled, ...props}) => (
-          <Switch
-            size="lg"
-            {...props}
-            isActive={!!value}
-            isDisabled={disabled}
-            toggle={this.onChange.bind(this, value, onChange, onBlur)}
-          />
+          <Confirm
+            message={confirm}
+            onConfirm={this.handleChange.bind(this, value, onChange, onBlur, {})}
+          >
+            {({open}) => (
+              <Switch
+                size="lg"
+                {...props}
+                isActive={!!value}
+                isDisabled={disabled}
+                toggle={e => {
+                  // If we have a `confirm` prop and enabling switch
+                  // Then show confirm dialog, otherwise propagate change as normal
+                  if (!!confirm && !value) {
+                    open();
+                    return;
+                  }
+
+                  this.handleChange(value, onChange, onBlur, e);
+                }}
+              />
+            )}
+          </Confirm>
         )}
       />
     );
