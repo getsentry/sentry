c69fb2639c18e401999307901768e6f0a326fd11
Brett Hoerner <brett@bretthoerner.com>
2018-03-07 09:33:35 -0600

    fix: Filter merge queries by project/project_id when possible
    Fixes SENTRY-61R

--- src/sentry/tasks/merge.py
+++ src/sentry/tasks/merge.py
@@ -301,18 +301,31 @@ def merge_objects(models, group, new_group, limit=1000, logger=None, transaction
     has_more = False
     for model in models:
         all_fields = model._meta.get_all_field_names()
+
+        # not all models have a 'project' or 'project_id' field, but we make a best effort
+        # to filter on one if it is available
+        has_project = 'project' in all_fields
+        has_project_id = 'project_id' in all_fields
+        if has_project:
+            project_qs = model.objects.filter(project=group.project)
+        elif has_project_id:
+            project_qs = model.objects.filter(project_id=group.project_id)
+        else:
+            project_qs = model.objects.all()
+
         has_group = 'group' in all_fields
         if has_group:
-            queryset = model.objects.filter(group=group)
+            queryset = project_qs.filter(group=group)
         else:
-            queryset = model.objects.filter(group_id=group.id)
+            queryset = project_qs.filter(group_id=group.id)
+
         for obj in queryset[:limit]:
             try:
                 with transaction.atomic(using=router.db_for_write(model)):
                     if has_group:
-                        model.objects.filter(id=obj.id).update(group=new_group)
+                        project_qs.filter(id=obj.id).update(group=new_group)
                     else:
-                        model.objects.filter(id=obj.id).update(group_id=new_group.id)
+                        project_qs.filter(id=obj.id).update(group_id=new_group.id)
             except IntegrityError:
                 delete = True
             else:
