41e35b3df66aecee0c0ee37a2ce9c2e21e7c49f1
Billy Vong <billyvg@users.noreply.github.com>
2018-03-09 14:06:43 -0800

    ref(ui): Cleanup project alerts/settings form header/footers (#7518)

--- src/sentry/static/sentry/app/data/forms/projectAlerts.jsx
+++ src/sentry/static/sentry/app/data/forms/projectAlerts.jsx
@@ -8,53 +8,38 @@ const formatMinutes = value => {
   return tn('%d minute', '%d minutes', value);
 };
 
-const formGroups = [
-  {
-    // Form "section"/"panel"
-    title: t('Email Settings'),
-    fields: [
-      {
-        name: 'subjectTemplate',
-        type: 'string',
+export const fields = {
+  subjectTemplate: {
+    name: 'subjectTemplate',
+    type: 'string',
 
-        // additional data/props that is related to rendering of form field rather than data
-        label: t('Subject Template'),
-        placeholder: 'e.g. [${tag:environment}]',
-        help: t(
-          'The email subject to use (excluding the prefix) for individual alerts. Usable variables include: $project, $title, $shortID, and ${tag:key}, such as ${tag:environment} or ${tag:release}.'
-        ),
-      },
-    ],
+    // additional data/props that is related to rendering of form field rather than data
+    label: t('Subject Template'),
+    placeholder: 'e.g. [${tag:environment}]',
+    help: t(
+      'The email subject to use (excluding the prefix) for individual alerts. Usable variables include: $project, $title, $shortID, and ${tag:key}, such as ${tag:environment} or ${tag:release}.'
+    ),
   },
-
-  {
-    // Form "section"/"panel"
-    title: t('Digests'),
-    fields: [
-      {
-        name: 'digestsMinDelay',
-        type: 'range',
-        min: 60,
-        max: 3600,
-        step: 60,
-        defaultValue: 300,
-        label: t('Minimum delivery interval'),
-        help: t('Notifications will be delivered at most this often.'),
-        formatLabel: formatMinutes,
-      },
-      {
-        name: 'digestsMaxDelay',
-        type: 'range',
-        min: 60,
-        max: 3600,
-        step: 60,
-        defaultValue: 300,
-        label: t('Maximum delivery interval'),
-        help: t('Notifications will be delivered at least this often.'),
-        formatLabel: formatMinutes,
-      },
-    ],
+  digestsMinDelay: {
+    name: 'digestsMinDelay',
+    type: 'range',
+    min: 60,
+    max: 3600,
+    step: 60,
+    defaultValue: 300,
+    label: t('Minimum delivery interval'),
+    help: t('Notifications will be delivered at most this often.'),
+    formatLabel: formatMinutes,
   },
-];
-
-export default formGroups;
+  digestsMaxDelay: {
+    name: 'digestsMaxDelay',
+    type: 'range',
+    min: 60,
+    max: 3600,
+    step: 60,
+    defaultValue: 300,
+    label: t('Maximum delivery interval'),
+    help: t('Notifications will be delivered at least this often.'),
+    formatLabel: formatMinutes,
+  },
+};
