738ab727662c87c36ad761413a3a7775c29e50ba
Max Bittker <maxbittker@gmail.com>
2018-03-09 11:24:20 -0800

    feat(owners): Display code owners for events (#7540)
    * display owners for event

--- src/sentry/static/sentry/app/components/actorAvatar.jsx
+++ src/sentry/static/sentry/app/components/actorAvatar.jsx
@@ -20,11 +20,11 @@ class ActorAvatar extends React.Component {
     let {actor, ...props} = this.props;
     if (actor.type == 'user') {
       let user = MemberListStore.getById(actor.id);
-      return <Avatar user={user} {...props} hasTooltip />;
+      return <Avatar user={user} hasTooltip {...props} />;
     }
     if (actor.type == 'team') {
       let team = TeamStore.getById(actor.id);
-      return <TeamAvatar team={team} {...props} hasTooltip />;
+      return <TeamAvatar team={team} hasTooltip {...props} />;
     }
 
     Raven.captureException('Unknown avatar type', {
