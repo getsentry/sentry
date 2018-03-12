bb4361bd0cc8d90d969b0e2fe4058d02c7c13604
Billy Vong <billyvg@users.noreply.github.com>
2018-03-12 15:07:52 -0700

    feat(ui): Add Teams breadcrumb in settings (#7566)
    * rename settingsBreadcrumb
    * feat(ui): Add `<TextLink>` component
    * fix unused arg in teamstore, fix fetchDetails

--- src/sentry/static/sentry/app/views/settings/team/teamProjects.jsx
+++ src/sentry/static/sentry/app/views/settings/team/teamProjects.jsx
@@ -81,7 +81,7 @@ const TeamProjects = createReactClass({
         } else {
           team.projects = team.projects.filter(({id}) => id != project.id);
         }
-        TeamActions.updateSuccess(0, teamId, team);
+        TeamActions.updateSuccess(teamId, team);
         addSuccessMessage(
           action === 'add'
             ? t('Successfully added project to team.')
