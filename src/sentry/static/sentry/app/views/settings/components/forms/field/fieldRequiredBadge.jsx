27d34784527130f2ce0e57f8dd6c88cf7e6b16e7
Chris Jennings <chriskjennings@gmail.com>
2018-03-14 16:56:37 -0700

    ref(settings-ui): red required badge (#7622)

--- src/sentry/static/sentry/app/views/settings/components/forms/field/fieldRequiredBadge.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/field/fieldRequiredBadge.jsx
@@ -2,7 +2,8 @@ import styled from 'react-emotion';
 
 const FieldRequiredBadge = styled.div`
   display: inline-block;
-  background: ${p => p.theme.gray2};
+  background: ${p => p.theme.redLight};
+  opacity: 0.6;
   width: 5px;
   height: 5px;
   border-radius: 5px;
