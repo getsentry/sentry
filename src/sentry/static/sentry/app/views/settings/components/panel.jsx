81acb488aef0e3f007180179b5085a7621ee94d2
Billy Vong <billyvg@users.noreply.github.com>
2018-03-15 08:02:06 -0700

    feat(ui): Refresh Project Plugins UI (#7628)
    * give `Switch` a default size

--- src/sentry/static/sentry/app/views/settings/components/panel.jsx
+++ src/sentry/static/sentry/app/views/settings/components/panel.jsx
@@ -1,6 +1,11 @@
+import PropTypes from 'prop-types';
+import React from 'react';
 import styled from 'react-emotion';
 
-const Panel = styled.div`
+import PanelBody from './panelBody';
+import PanelHeader from './panelHeader';
+
+const StyledPanel = styled.div`
   background: #fff;
   border-radius: ${p => p.theme.borderRadius};
   border: 1px solid ${p => p.theme.borderDark};
@@ -8,5 +13,32 @@ const Panel = styled.div`
   margin-bottom: ${p => p.theme.grid * 4}px;
   position: relative;
 `;
+
+class Panel extends React.Component {
+  static propTypes = {
+    /**
+     * When `title` and `body` are defined, use as children to `<PanelHeader>` and `<PanelBody>` respectively
+     */
+    title: PropTypes.node,
+    body: PropTypes.node,
+  };
+
+  render() {
+    let {title, body} = this.props;
+    let hasHeaderAndBody = !!title && !!body;
+
+    if (hasHeaderAndBody) {
+      return (
+        <StyledPanel>
+          <PanelHeader>{title}</PanelHeader>
+
+          <PanelBody>{body}</PanelBody>
+        </StyledPanel>
+      );
+    }
+
+    return <StyledPanel {...this.props} />;
+  }
+}
 
 export default Panel;
