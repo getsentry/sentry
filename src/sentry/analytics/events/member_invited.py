9c279fc60e3bd4a597858b5acac915b4ef4aeeff
Adhiraj Somani <adhiraj@sentry.io>
2018-03-15 19:24:22 -0700

    newlines

--- src/sentry/analytics/events/member_invited.py
+++ src/sentry/analytics/events/member_invited.py
@@ -7,8 +7,10 @@ class MemberInvitedEvent(analytics.Event):
     type = 'member.invited'
 
     attributes = (
-        analytics.Attribute('inviter_user_id'), analytics.Attribute(
-            'invited_member_id'), analytics.Attribute('organization_id'), analytics.Attribute('referrer', required=False)
+        analytics.Attribute('inviter_user_id'),
+        analytics.Attribute('invited_member_id'),
+        analytics.Attribute('organization_id'),
+        analytics.Attribute('referrer', required=False),
     )
 
 
