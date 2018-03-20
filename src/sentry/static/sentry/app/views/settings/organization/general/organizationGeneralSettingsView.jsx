03dd603640fe3ae75b9ebce9572499cef2cda0ad
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-03-20 14:47:52 -0700

    fix(settings): Use browserHistory.replace for org slug (#7724)

--- src/sentry/static/sentry/app/views/settings/organization/general/organizationGeneralSettingsView.jsx
+++ src/sentry/static/sentry/app/views/settings/organization/general/organizationGeneralSettingsView.jsx
@@ -102,7 +102,7 @@ const OrganizationGeneralSettingsView = createReactClass({
   handleSave(prevData, data) {
     if (data.slug && data.slug !== prevData.slug) {
       changeOrganizationSlug(prevData, data);
-      browserHistory.push(`/settings/organization/${data.slug}/settings/`);
+      browserHistory.replace(`/settings/organization/${data.slug}/settings/`);
     } else {
       // TODO(dcramer): this should propagate
       this.setState({data});
