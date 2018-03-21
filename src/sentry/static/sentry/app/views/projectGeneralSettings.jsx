8d58e199b46aa0b6884d49ba05fa0b1d8356456d
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-03-21 12:05:16 -0700

    Ref: Prettier transfer project dialog (#7718)

--- src/sentry/static/sentry/app/views/projectGeneralSettings.jsx
+++ src/sentry/static/sentry/app/views/projectGeneralSettings.jsx
@@ -170,11 +170,10 @@ export default class ProjectGeneralSettings extends AsyncView {
                   </TextBlock>
                   <TextBlock>
                     {t(
-                      'Please enter the owner of the organization you would like to transfer this project to.'
+                      'Please enter the organization owner you would like to transfer this project to.'
                     )}
                   </TextBlock>
                   <Panel>
-                    <PanelHeader>{t('Transfer to')}</PanelHeader>
                     <Form
                       hideFooter
                       onFieldChange={this.handleTransferFieldChange}
