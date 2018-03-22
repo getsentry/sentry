781f10a345852109baea85ae7d79c601d9d48c9f
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-03-22 16:52:23 -0700

    fix(integrations): Correct integrations configure button link

--- src/sentry/static/sentry/app/views/projectPlugins/organizationIntegrations.jsx
+++ src/sentry/static/sentry/app/views/projectPlugins/organizationIntegrations.jsx
@@ -58,7 +58,7 @@ export default class OrganizationIntegrations extends AsyncComponent {
         <Box>
           <Button
             size="small"
-            to={`/settings/organization/${orgId}/project/${projectId}/integrations/${provider.key}/`}
+            to={`/settings/${orgId}/project/${projectId}/integrations/${provider.key}/`}
           >
             {t('Configure')}
           </Button>
