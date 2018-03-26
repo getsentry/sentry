aedb141c32565bb5a17ddcddfcf114f456cc6132
alex-hofsteede <alex@hofsteede.com>
2018-03-26 14:05:20 -0700

    ref(tagstore) create a keys_and_top_values method
    Create a tagstore method that returns top keys and their top values as
    plain python objects. This should make it possible to use a different
    non-django backend to return this data.

--- tests/sentry/api/endpoints/test_group_tags.py
+++ tests/sentry/api/endpoints/test_group_tags.py
@@ -7,25 +7,21 @@ from sentry.testutils import APITestCase
 class GroupTagsTest(APITestCase):
     def test_simple(self):
         this_group = self.create_group()
-        this_group.data['tags'] = (['foo', 'bar'], ['biz', 'baz'])
+        this_group.data['tags'] = (['foo', ['bar', 'quux']], ['biz', 'baz'], [
+                                   'sentry:release', 'releaseme'])
+
         this_group.save()
 
         other_group = self.create_group()
-        other_group.data['tags'] = (['abc', 'xyz'], )
+        other_group.data['tags'] = (['abc', 'xyz'],)
         other_group.save()
 
         for group in (this_group, other_group):
-            for key, value in group.data['tags']:
+            for key, values in group.data['tags']:
                 tagstore.create_tag_key(
                     project_id=group.project_id,
                     environment_id=None,
                     key=key,
-                )
-                tagstore.create_tag_value(
-                    project_id=group.project_id,
-                    environment_id=None,
-                    key=key,
-                    value=value,
                 )
                 tagstore.create_group_tag_key(
                     project_id=group.project_id,
@@ -33,17 +29,37 @@ class GroupTagsTest(APITestCase):
                     environment_id=None,
                     key=key,
                 )
-                tagstore.create_group_tag_value(
-                    project_id=group.project_id,
-                    group_id=group.id,
-                    environment_id=None,
-                    key=key,
-                    value=value,
-                )
+
+                if not isinstance(values, list):
+                    values = [values]
+                for value in values:
+                    tagstore.create_tag_value(
+                        project_id=group.project_id,
+                        environment_id=None,
+                        key=key,
+                        value=value,
+                    )
+                    tagstore.create_group_tag_value(
+                        project_id=group.project_id,
+                        group_id=group.id,
+                        environment_id=None,
+                        key=key,
+                        value=value,
+                    )
 
         self.login_as(user=self.user)
 
         url = '/api/0/issues/{}/tags/'.format(this_group.id)
         response = self.client.get(url, format='json')
         assert response.status_code == 200, response.content
-        assert len(response.data) == 2
+        assert len(response.data) == 3
+
+        data = sorted(response.data, key=lambda r: r['key'])
+        assert data[0]['key'] == 'biz'
+        assert len(data[0]['topValues']) == 1
+
+        assert data[1]['key'] == 'foo'
+        assert len(data[1]['topValues']) == 2
+
+        assert data[2]['key'] == 'release'  # Formatted from sentry:release
+        assert len(data[2]['topValues']) == 1
