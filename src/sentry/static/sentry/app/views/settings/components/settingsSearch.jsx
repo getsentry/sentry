54433320dcd5e346393cda921eccd20886b22938
Billy Vong <billy@sentry.io>
2018-03-28 10:39:41 -0700

    use generic label/description to display default results

--- src/sentry/static/sentry/app/views/settings/components/settingsSearch.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsSearch.jsx
@@ -43,8 +43,17 @@ class SearchResult extends React.Component {
     ]),
   };
 
+  render() {
+    return (
+      <Flex justify="space-between" align="center">
+        <Content>{this.renderContent()}</Content>
+        {this.renderResultType()}
+      </Flex>
+    );
+  }
+
   renderContent() {
-    let {sourceType, resultType, title, description, model, params} = this.props;
+    let {sourceType, resultType, field, title, description, model, params} = this.props;
 
     let isSettings = resultType === 'settings';
 
@@ -112,15 +121,6 @@ class SearchResult extends React.Component {
     }
 
     return <ResultTypeIcon src="icon-location" />;
-  }
-
-  render() {
-    return (
-      <Flex justify="space-between" align="center">
-        <Content>{this.renderContent()}</Content>
-        {this.renderResultType()}
-      </Flex>
-    );
   }
 }
 
