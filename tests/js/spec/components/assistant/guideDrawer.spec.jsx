50cc297003ffe23ab78cb6fdd93962573f0aae37
Eric Feng <erichfeng@gmail.com>
2018-03-29 15:01:54 -0700

    adding interpolate to tests

--- tests/js/spec/components/assistant/guideDrawer.spec.jsx
+++ tests/js/spec/components/assistant/guideDrawer.spec.jsx
@@ -10,7 +10,7 @@ describe('GuideDrawer', function() {
     page: 'issue',
     required_targets: ['target 1'],
     steps: [
-      {message: 'Message 1', target: 'target 1', title: '1. Title 1'},
+      {message: 'Message 1 ${org_slug}', target: 'target 1', title: '1. Title 1'},
       {message: 'Message 2', target: 'target 2', title: '2. Title 2'},
     ],
   };
@@ -18,8 +18,15 @@ describe('GuideDrawer', function() {
   it('gets dismissed', function() {
     let mock = jest.fn();
     let mock2 = jest.fn();
+    let slug = 'test';
     let wrapper = shallow(
-      <GuideDrawer guide={data} step={1} onFinish={mock} onDismiss={mock2} />
+      <GuideDrawer
+        guide={data}
+        step={1}
+        onFinish={mock}
+        onDismiss={mock2}
+        orgSlug={slug}
+      />
     );
     expect(wrapper).toMatchSnapshot();
     wrapper
@@ -32,8 +39,15 @@ describe('GuideDrawer', function() {
   it('renders next step', function() {
     let mock = jest.fn();
     let mock2 = jest.fn();
+    let slug = 'test';
     let wrapper = shallow(
-      <GuideDrawer guide={data} step={2} onFinish={mock} onDismiss={mock2} />
+      <GuideDrawer
+        guide={data}
+        step={2}
+        onFinish={mock}
+        onDismiss={mock2}
+        orgSlug={slug}
+      />
     );
     expect(wrapper).toMatchSnapshot();
 
