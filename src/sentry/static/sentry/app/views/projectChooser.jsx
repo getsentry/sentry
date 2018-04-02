15c018ec6265ce447d5a4eba6b133d6b414c2054
Dena Mwangi <dena@getsentry.com>
2018-04-02 09:16:59 -0700

    fix: Broken project chooser (#7824)

--- src/sentry/static/sentry/app/views/projectChooser.jsx
+++ src/sentry/static/sentry/app/views/projectChooser.jsx
@@ -2,17 +2,33 @@ import React from 'react';
 import createReactClass from 'create-react-class';
 import {browserHistory} from 'react-router';
 import $ from 'jquery';
-import {t} from '../locale';
+import {Box} from 'grid-emotion';
+import styled from 'react-emotion';
 
+import {t} from '../locale';
+import Link from '../components/link';
 import OrganizationState from '../mixins/organizationState';
+import {sortProjects} from '../utils';
+import theme from '../utils/theme';
 import TodoList from '../components/onboardingWizard/todos';
+import Panel from './settings/components/panel';
+import PanelBody from './settings/components/panelBody';
+import PanelHeader from './settings/components/panelHeader';
+import PanelItem from './settings/components/panelItem';
+import SettingsPageHeader from './settings/components/settingsPageHeader';
+import ProjectLabel from '../components/projectLabel';
+import SentryTypes from '../proptypes';
 
 const ProjectChooser = createReactClass({
   displayName: 'ProjectChooser',
+
+  propTypes: {
+    organization: SentryTypes.Organization,
+  },
+
   mixins: [OrganizationState],
 
   componentWillMount() {
-    $(document.body).addClass('narrow');
     this.redirectNoMultipleProjects();
   },
 
@@ -40,7 +56,6 @@ const ProjectChooser = createReactClass({
     let task = TodoList.TASKS.filter(
       task_inst => task_inst.task == this.props.location.query.task
     )[0];
-    let features = new Set(org.features);
 
     // Expect onboarding=1 and task=<task id> parameters and task.featureLocation == 'project'
     // TODO throw up report dialog if not true
@@ -48,32 +63,35 @@ const ProjectChooser = createReactClass({
       throw new Error('User arrived on project chooser without a valid task id.');
     }
     return (
-      <div className="container">
-        <h3>{t('Choose a project')}</h3>
-        <div className="box">
-          <div className="box-content">
-            <table className="table">
-              <tbody>
-                {org.projects.map(project => {
-                  return (
-                    <tr key={project.id}>
-                      <td>
-                        <h4>
-                          <a href={`/${org.slug}/${project.slug}/${task.location}`}>
-                            {features.has('new-teams') ? project.slug : project.name}
-                          </a>
-                        </h4>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
+      <div className="container" css={{'padding-left': '90px', 'padding-top': '30px'}}>
+        <SettingsPageHeader title="Projects" />
+        <Panel>
+          <PanelHeader hasButtons>{t('Projects')}</PanelHeader>
+          <PanelBody css={{width: '100%'}}>
+            {sortProjects(org.projects).map((project, i) => (
+              <PanelItem p={0} key={project.id} align="center">
+                <Box p={2} flex="1">
+                  <Link
+                    to={`/${org.slug}/${project.slug}/${task.location}`}
+                    css={{color: theme.gray3}}
+                  >
+                    <StyledProjectLabel
+                      project={project}
+                      organization={this.props.organization}
+                    />
+                  </Link>
+                </Box>
+              </PanelItem>
+            ))}
+          </PanelBody>
+        </Panel>
       </div>
     );
   },
 });
+
+const StyledProjectLabel = styled(ProjectLabel)`
+  color: ${p => p.theme.blue};
+`;
 
 export default ProjectChooser;
