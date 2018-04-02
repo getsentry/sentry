633cea3664d68947cd0d2cf1a33a03c383f21fa6
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-04-02 11:58:33 -0700

    fix(integrations): Correct Admin permissions

--- src/sentry/web/frontend/integration_setup.py
+++ src/sentry/web/frontend/integration_setup.py
@@ -3,15 +3,17 @@ from __future__ import absolute_import, print_function
 import logging
 
 from sentry.integrations.pipeline import IntegrationPipeline
-from sentry.web.frontend.base import BaseView
+from sentry.web.frontend.base import OrganizationView
 
 logger = logging.getLogger('sentry.integrations')
 
 
-class IntegrationSetupView(BaseView):
+class IntegrationSetupView(OrganizationView):
+    required_scope = 'org:integrations'
+
     csrf_protect = False
 
-    def handle(self, request, provider_id):
+    def handle(self, request, organization, provider_id):
         pipeline = IntegrationPipeline.get_for_request(request=request)
         if not pipeline:
             logging.error('integration.setup-error')
