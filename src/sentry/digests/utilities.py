70b884b89b3a6432f00036e5169dcd23e8f3dd4c
Lauryn Brown <lauryndbrown@gmail.com>
2018-04-02 14:26:42 -0700

    Added method to digest utilities to create a mapping between events and rules.

--- src/sentry/digests/utilities.py
+++ src/sentry/digests/utilities.py
@@ -23,3 +23,16 @@ def get_digest_metadata(digest):
                     end = record.datetime
 
     return start, end, counts
+
+
+def get_digest_event_rules(digest):
+    """
+    Returns a dictionary consisting of {event:rules}
+    from a digest object.
+    """
+    event_rules = {}
+    for groups in six.itervalues(digest):
+        for group in groups:
+            value = groups[group][0].value
+            event_rules[value.event] = value.rules
+    return event_rules
