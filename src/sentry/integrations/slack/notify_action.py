8ca75a2c1f6c4bbf6b821de0b5b6cb71c507da53
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-04-03 14:33:34 -0700

    ref(alerts): Wording on slack / all services notifications

--- src/sentry/integrations/slack/notify_action.py
+++ src/sentry/integrations/slack/notify_action.py
@@ -64,7 +64,7 @@ class SlackNotifyServiceForm(forms.Form):
 
 class SlackNotifyServiceAction(EventAction):
     form_cls = SlackNotifyServiceForm
-    label = u'Send a notification to the {workspace} Slack workspace to {channel} and include tags {tags}'
+    label = u'Send a notification to the {workspace} Slack workspace to {channel} and show tags {tags} in notification'
 
     def __init__(self, *args, **kwargs):
         super(SlackNotifyServiceAction, self).__init__(*args, **kwargs)
