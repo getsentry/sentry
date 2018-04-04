bbabace934b82e8bd6c4147ca6db5e31086d0eda
HazA <daniel.griesser.86@gmail.com>
2018-04-04 13:17:23 +0900

    ref: Remove .all()

--- src/sentry/web/frontend/setup_wizard.py
+++ src/sentry/web/frontend/setup_wizard.py
@@ -32,7 +32,7 @@ class SetupWizardView(BaseView):
             member_set__role__in=[x.id for x in roles.with_scope('org:read')],
             member_set__user=request.user,
             status=OrganizationStatus.VISIBLE,
-        ).all().order_by('-date_added')[:50]
+        ).order_by('-date_added')[:50]
 
         filled_projects = []
 
