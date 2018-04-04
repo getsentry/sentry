43d9b8dca738f5195bdb11848a24d1058d15191f
Lauryn Brown <lauryndbrown@gmail.com>
2018-04-03 18:38:31 -0700

    saving place.

--- src/sentry/digests/utilities.py
+++ src/sentry/digests/utilities.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 import six
 
-from collections import Counter
+from collections import Counter, OrderedDict
 
 
 # TODO(tkaemming): This should probably just be part of `build_digest`.
@@ -23,3 +23,24 @@ def get_digest_metadata(digest):
                     end = record.datetime
 
     return start, end, counts
+
+
+def get_personalized_digests(project, digest, user_ids):
+    pass
+
+
+def build_custom_digest(original_digest, user_id, events_by_users):
+    user_digest = OrderedDict()
+    for rule, rule_groups in six.iteritems(original_digest):
+        user_rule_groups = OrderedDict()
+        for group, group_records in six.iteritems(rule_groups):
+            user_group_records = [
+                record for record in group_records
+                if user_id in events_by_users[record.value.event]
+            ]
+            if user_group_records:
+                user_rule_groups[group] = user_group_records
+
+        if user_rule_groups:
+            user_digest[rule] = user_rule_groups
+    return user_digest
