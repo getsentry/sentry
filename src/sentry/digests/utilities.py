c97a471cb42329db6409a28b3c0dd07cbc783895
Lauryn Brown <lauryndbrown@gmail.com>
2018-04-06 14:26:06 -0700

    Added additional methods for testing the larger changes.:

--- src/sentry/digests/utilities.py
+++ src/sentry/digests/utilities.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 import six
 
 from collections import Counter, OrderedDict
-from sentry.models import ProjectOwnership, Team, User
+from sentry.models import OrganizationMemberTeam, ProjectOwnership, Team, User
 
 
 # TODO(tkaemming): This should probably just be part of `build_digest`.
@@ -34,12 +34,20 @@ def get_personalized_digests(project_id, digest, user_ids):
     if ProjectOwnership.objects.filter(project_id=project_id).exists():
         events = get_events_from_digest(digest)
         events_by_actor = build_events_by_actor(project_id, events)
-        events_by_users = convert_actors_to_user_set(events_by_actor)
+        events_by_user = convert_actors_to_user_set(events_by_actor, user_ids)
         for user_id in user_ids:
-            yield user_id, build_custom_digest(digest, user_id, events_by_users)
+            yield user_id, build_custom_digest(digest, events_by_user[user_id])
     else:
         for user_id in user_ids:
             yield user_id, digest
+
+
+def sort_records(records):
+    """
+    Sorts records for fetch_state method
+    fetch_state is expecting these records to be ordered from newest to oldest
+    """
+    return sorted(records, key=lambda r: r.value.event.datetime, reverse=True)
 
 
 def get_events_from_digest(digest):
@@ -50,7 +58,7 @@ def get_events_from_digest(digest):
     return set(events)
 
 
-def build_custom_digest(original_digest, user_id, events_by_users):
+def build_custom_digest(original_digest, events):
     """
     build_custom_digest(original_digest: Digest, user_id: Int, events_by_users: Map[User_Id:Set(Events)]) -> Digest
     """
@@ -60,11 +68,10 @@ def build_custom_digest(original_digest, user_id, events_by_users):
         for group, group_records in six.iteritems(rule_groups):
             user_group_records = [
                 record for record in group_records
-                if user_id in events_by_users[record.value.event]
+                if record.value.event in events
             ]
             if user_group_records:
-                user_rule_groups[group] = user_group_records
-
+                user_rule_groups[group] = sort_records(user_group_records)
         if user_rule_groups:
             user_digest[rule] = user_rule_groups
     return user_digest
@@ -87,26 +94,55 @@ def build_events_by_actor(project_id, events):
     return events_by_actor
 
 
-def convert_actors_to_user_set(events_by_actor):
+def convert_actors_to_user_set(events_by_actor, user_ids):
     """
-    convert_actors_to_user_set(events_by_actor: Map[Actor:Set(Events)]) -> Map[User_Id:Set(Events)]
+    convert_actors_to_user_set(events_by_actor: Map[Actor:Set(Events)], user_ids: List(Int)) -> Map[User_Id:Set(Events)]
     """
     user_by_events = {}
+    team_actors = [actor for actor in six.iterkeys(events_by_actor) if actor.type == Team]
+    teams_to_user_ids = team_actors_to_user_ids(team_actors, user_ids)
     for actor, events in six.iteritems(events_by_actor):
         if actor.type == Team:
-            user_ids = team_to_user_ids(actor.id)
-            for user_id in user_ids:
-                if user_id in user_by_events:
-                    user_by_events[user_id].update(events)
-                else:
-                    user_by_events[user_id] = events
+            try:
+                user_ids = teams_to_user_ids[actor.id]
+            except KeyError:
+                pass  # TODO(LB) Not certain what to do if a team has no members
+            else:
+                for user_id in user_ids:
+                    if user_id in user_by_events:
+                        user_by_events[user_id].update(events)
+                    else:
+                        user_by_events[user_id] = events
         else:
             user_by_events[actor.id] = events
     return user_by_events
 
 
+def team_actors_to_user_ids(team_actors, user_ids):
+    """
+    team_actors_to_user_ids(team_actors: List(Actors), user_ids: List(Int)) -> Map[TeamActor: Set(User_ids)]
+    """
+    team_ids = [actor.id for actor in team_actors]
+    members = OrganizationMemberTeam.objects.filter(
+        team_id__in=team_ids,
+        is_active=True,
+        organizationmember__user_id__in=user_ids,
+    ).select_related('organizationmember')
+
+    team_members = {}
+    for member in members:
+        user_id = member.organizationmember.user_id
+        if member.team_id in team_members:
+            team_members[member.team_id].add(user_id)
+        else:
+            team_members[member.team_id] = set([user_id])
+
+    return team_members
+
+
 def team_to_user_ids(team_id):
     """
+    Defunct first attempt but O(n) Queries. Switching to team_actors_to_user_id
     team_to_user_ids(team_id:Int) -> List(User_ids)
     """
     return User.objects.filter(
--- tests/sentry/digests/test_utilities.py
+++ tests/sentry/digests/test_utilities.py
@@ -1,14 +1,16 @@
 from __future__ import absolute_import
 
+
 from sentry.api.fields.actor import Actor
 from sentry.digests.notifications import build_digest, event_to_record
 from sentry.digests.utilities import (
-    # get_personalized_digests,
-    get_events_from_digest,
-    # build_custom_digest,
     build_events_by_actor,
     convert_actors_to_user_set,
+    get_events_from_digest,
+    get_personalized_digests,
+    sort_records,
     team_to_user_ids,
+    team_actors_to_user_ids,
 )
 from sentry.models import ProjectOwnership, Team, User
 from sentry.ownership.grammar import Rule, Owner, Matcher, dump_schema
@@ -72,6 +74,27 @@ class UtilitiesTestCase(TestCase):
             users[0].id, users[1].id, users[2].id, users[3].id]
         assert sorted(team_to_user_ids(team2.id)) == [users[3].id, users[4].id, users[5].id]
 
+    # team without users not checked
+    def test_team_actors_to_user_ids(self):
+        team1 = self.create_team()
+        team2 = self.create_team()
+        users = [self.create_user() for i in range(0, 6)]
+
+        self.create_member(user=users[0], organization=self.organization, teams=[team1])
+        self.create_member(user=users[1], organization=self.organization, teams=[team1])
+        self.create_member(user=users[2], organization=self.organization, teams=[team1])
+        self.create_member(user=users[3], organization=self.organization, teams=[team1, team2])
+        self.create_member(user=users[4], organization=self.organization, teams=[team2, self.team])
+        self.create_member(user=users[5], organization=self.organization, teams=[team2])
+
+        team_actors = [Actor(team1.id, Team), Actor(team2.id, Team)]
+        user_ids = [user.id for user in users]
+
+        assert team_actors_to_user_ids(team_actors, user_ids) == {
+            team1.id: set([users[0].id, users[1].id, users[2].id, users[3].id]),
+            team2.id: set([users[3].id, users[4].id, users[5].id]),
+        }
+
     def test_convert_actors_to_user_set(self):
         user1 = self.create_user()
         user2 = self.create_user()
@@ -102,7 +125,7 @@ class UtilitiesTestCase(TestCase):
         events_by_actor = {
             Actor(team1.id, Team): team1_events,
             Actor(team2.id, Team): team2_events,
-            Actor(user1.id, User): team1_events.union(team2_events),
+            Actor(user3.id, User): team1_events.union(team2_events),
             Actor(user4.id, User): user4_events,
         }
         user_by_events = {
@@ -111,7 +134,7 @@ class UtilitiesTestCase(TestCase):
             user3.id: team1_events.union(team2_events),
             user4.id: user4_events,
         }
-        assert convert_actors_to_user_set(events_by_actor) == user_by_events
+        assert convert_actors_to_user_set(events_by_actor, user_by_events.keys()) == user_by_events
 
     def test_build_events_by_actor(self):
         user1 = self.create_user()
@@ -176,3 +199,108 @@ class UtilitiesTestCase(TestCase):
             Actor(user4.id, User): user4_events,
         }
         assert build_events_by_actor(self.project.id, events) == events_by_actor
+
+    def test_get_personalized_digests(self):
+        user1 = self.create_user()
+        user2 = self.create_user()
+        user3 = self.create_user()
+        user4 = self.create_user()
+
+        team1 = self.create_team()
+        team2 = self.create_team()
+        team3 = self.create_team()
+
+        self.project = self.create_project(teams=[team1, team2, team3])
+
+        self.create_member(user=user1, organization=self.organization, teams=[team1])
+        self.create_member(user=user2, organization=self.organization, teams=[team2])
+        self.create_member(user=user3, organization=self.organization, teams=[team1, team2])
+        self.create_member(user=user4, organization=self.organization, teams=[team3])
+
+        team1_events = [
+            self.create_event(
+                group=self.create_group(
+                    project=self.project),
+                data=self.create_event_data('hello.py')),
+            self.create_event(
+                group=self.create_group(
+                    project=self.project),
+                data=self.create_event_data('goodbye.py')),
+            self.create_event(
+                group=self.create_group(
+                    project=self.project),
+                data=self.create_event_data('hola.py')),
+            self.create_event(
+                group=self.create_group(
+                    project=self.project),
+                data=self.create_event_data('adios.py')),
+        ]
+        team2_events = [
+            self.create_event(
+                group=self.create_group(
+                    project=self.project),
+                data=self.create_event_data('old.cbl')),
+            self.create_event(
+                group=self.create_group(
+                    project=self.project),
+                data=self.create_event_data('retro.cbl')),
+            self.create_event(
+                group=self.create_group(
+                    project=self.project),
+                data=self.create_event_data('cool.cbl')),
+            self.create_event(
+                group=self.create_group(
+                    project=self.project),
+                data=self.create_event_data('gem.cbl')),
+        ]
+        user4_events = [
+            self.create_event(
+                group=self.create_group(
+                    project=self.project), data=self.create_event_data(
+                    'foo.bar', 'helloworld.org')),
+            self.create_event(
+                group=self.create_group(
+                    project=self.project), data=self.create_event_data(
+                    'bar.foo', 'helloworld.org')),
+        ]
+
+        team1_matcher = Matcher('path', '*.py')
+        team2_matcher = Matcher('path', '*.cbl')
+        user4_matcher = Matcher('url', '*.org')
+
+        ProjectOwnership.objects.create(
+            project_id=self.project.id,
+            schema=dump_schema([
+                Rule(team1_matcher, [
+                    Owner('team', team1.slug),
+                    Owner('user', user3.email),
+                ]),
+                Rule(team2_matcher, [
+                    Owner('team', team2.slug),
+                ]),
+                Rule(user4_matcher, [
+                    Owner('user', user4.email),
+                ]),
+            ]),
+            fallthrough=True,
+        )
+
+        rule = self.project.rule_set.all()[0]
+        records = [event_to_record(event, (rule, ))
+                   for event in team1_events + team2_events + user4_events]
+        digest = build_digest(self.project, sort_records(records))
+
+        expected_result = {
+            user1.id: set(team1_events),
+            user2.id: set(team2_events),
+            user3.id: set(team1_events + team2_events),
+            user4.id: set(user4_events),
+        }
+        user_ids = expected_result.keys()
+        result_user_ids = []
+        for user_id, user_digest in get_personalized_digests(self.project.id, digest, user_ids):
+            assert user_id in expected_result
+            assert expected_result[user_id] == get_events_from_digest(user_digest)
+            result_user_ids.append(user_id)
+
+        assert sorted(user_ids) == sorted(result_user_ids)
