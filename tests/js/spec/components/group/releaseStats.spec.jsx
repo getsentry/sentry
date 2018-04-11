59bf771af570377da791ab44e996ad6379672b91
Lyn Nagara <lyn.nagara@gmail.com>
2018-04-11 11:35:27 -0700

    fix: Event toolbar and charts show time in user's local timezone
    Fixes #7630

--- tests/js/spec/components/group/releaseStats.spec.jsx
+++ tests/js/spec/components/group/releaseStats.spec.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import {mount} from 'enzyme';
 
+import ConfigStore from 'app/stores/configStore';
 import GroupReleaseStats from 'app/components/group/releaseStats';
 import EnvironmentStore from 'app/stores/environmentStore';
 
 describe('GroupReleaseStats', function() {
   let component;
   beforeEach(function() {
+    // Set timezone for snapshot
+    ConfigStore.loadInitialData({
+      user: {
+        options: {
+          timezone: 'America/Los_Angeles',
+        },
+      },
+    });
+
     component = mount(
       <GroupReleaseStats group={TestStubs.Group()} location={TestStubs.location()} />,
       {
