60dbd12d8e25416b6d364d4d009d13a072f2bf09
David Cramer <dcramer@gmail.com>
2018-04-16 09:50:15 -0700

    ref(ui): Expose ErrorBoundary

--- src/sentry/static/sentry/app/index.js
+++ src/sentry/static/sentry/app/index.js
@@ -128,6 +128,7 @@ export default {
     DateTime: require('./components/dateTime').default,
     DropdownLink: require('./components/dropdownLink').default,
     DynamicWrapper: require('./components/dynamicWrapper').default,
+    ErrorBoundary: require('./components/errorBoundary').default,
     Form: require('./components/forms/form').default,
     FormState: require('./components/forms/index').FormState,
     GuideAnchor: require('./components/assistant/guideAnchor').default,
