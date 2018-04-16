e6088e1f2ce6e3801bd2f63f4668a3151991b84c
Lyn Nagara <lyn.nagara@gmail.com>
2018-04-16 11:03:49 -0700

    feat: Add multi select component for owner dropdown(#8043)
    We'll deprecate select2 in favor of react-select

--- src/sentry/static/sentry/app/utils.jsx
+++ src/sentry/static/sentry/app/utils.jsx
@@ -224,8 +224,6 @@ export function sortProjects(projects) {
 export const buildUserId = id => `user:${id}`;
 export const buildTeamId = id => `team:${id}`;
 
-export const actorEquality = (a, b) => a.type === b.type && a.id === b.id;
-
 // re-export under utils
 export {parseLinkHeader, deviceNameMapper, Collection, PendingChangeQueue, CursorPoller};
 
