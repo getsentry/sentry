195b326dcc8f5aa542967916e299eea0478ba032
MeredithAnya <meredith.a.heller@gmail.com>
2018-04-16 17:05:53 -0700

    fix(project-search): Add typeahead search to org projects (#8048)
    * fix(project-search): Add typeahead search to org projects
    
    * update snapshot
    
    * add more tests
    
    * remove getProjects state dependency

--- tests/js/spec/views/organizationProjects.spec.jsx
+++ tests/js/spec/views/organizationProjects.spec.jsx
@@ -4,6 +4,8 @@ import {mount} from 'enzyme';
 import {Client} from 'app/api';
 import OrganizationProjectsViewContainer from 'app/views/settings/organization/projects/organizationProjectsView';
 
+jest.mock('lodash/debounce', () => jest.fn(fn => fn));
+
 describe('OrganizationProjectsView', function() {
   let org;
   let project;
@@ -54,6 +56,34 @@ describe('OrganizationProjectsView', function() {
       wrapper.find('.icon-star-outline').simulate('click');
       expect(wrapper.find('.icon-star-solid')).toBeTruthy();
       expect(projectsPutMock).toHaveBeenCalledTimes(1);
+    });
+
+    it('should search organization projects', async function() {
+      let searchMock = MockApiClient.addMockResponse({
+        url: `/organizations/${org.slug}/projects/?query=${project.slug}`,
+        body: [],
+      });
+      let routerOrganizationContext = TestStubs.routerOrganizationContext();
+      let wrapper = mount(
+        <OrganizationProjectsViewContainer location={{}} params={{orgId: org.slug}} />,
+        routerOrganizationContext
+      );
+
+      expect(searchMock).not.toHaveBeenCalled();
+
+      wrapper.find('Input').simulate('change', {target: {value: `${project.slug}`}});
+
+      expect(wrapper.state('searchQuery')).toBe(`${project.slug}`);
+      expect(searchMock).toHaveBeenCalled();
+      expect(searchMock).toHaveBeenCalledWith(
+        `/organizations/${org.slug}/projects/?query=${project.slug}`,
+        expect.objectContaining({
+          method: 'GET',
+        })
+      );
+
+      wrapper.find('PanelHeader form').simulate('submit');
+      expect(routerOrganizationContext.context.router.push.calledOnce).toBe(true);
     });
   });
 });
