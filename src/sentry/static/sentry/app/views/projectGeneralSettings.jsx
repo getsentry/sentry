c466ba60e2081b18e5af2c758d5b15f43fa63074
David Cramer <dcramer@gmail.com>
2018-04-18 13:30:50 -0700

    fix(settings): Correct subject template/prefix configuration
    - subjectPrefix was being referenced as subjectTemplate
    - fixed docstrings on subjectPrefix and subjectTemplate

--- src/sentry/static/sentry/app/views/projectGeneralSettings.jsx
+++ src/sentry/static/sentry/app/views/projectGeneralSettings.jsx
@@ -255,7 +255,7 @@ class ProjectGeneralSettings extends AsyncView {
           <JsonForm
             {...jsonFormProps}
             title={t('Email')}
-            fields={[fields.subjectTemplate]}
+            fields={[fields.subjectPrefix]}
           />
 
           <JsonForm
