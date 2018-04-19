6c7399e3cd545cc49dd18fa71fd1ba7f1203b5af
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-04-18 17:49:55 -0700

    fix(integration): Prepare for backfill of missing extenral_id

--- src/sentry/integrations/pipeline.py
+++ src/sentry/integrations/pipeline.py
@@ -4,6 +4,7 @@ __all__ = ['IntegrationPipeline']
 
 from django.http import HttpResponse
 from django.utils import timezone
+from django.db.models import Q
 
 from sentry.api.serializers import serialize
 from sentry.models import Identity, IdentityProvider, IdentityStatus, Integration
@@ -56,11 +57,26 @@ class IntegrationPipeline(Pipeline):
         identity = data.get('user_identity')
 
         if identity:
-            # Create identity provider for this integration if nessicary
-            idp, created = IdentityProvider.objects.get_or_create(
-                organization=self.organization,
-                type=identity['type'],
-            )
+            try:
+                idp = IdentityProvider.objects.get(
+                    Q(external_id=data['external_id']) | Q(external_id=None),
+                    organization=self.organization,
+                    type=identity['type'],
+                )
+            except IdentityProvider.DoesNotExist:
+                idp = IdentityProvider.objects.create(
+                    external_id=data['external_id'],
+                    organization=self.organization,
+                    type=identity['type'],
+                )
+
+            # TODO(epurkhiser): Once external_id is backfilled we can get away with this
+            # # Create identity provider for this integration if necessary
+            # idp, created = IdentityProvider.objects.get_or_create(
+            #     external_id=data['external_id'],
+            #     organization=self.organization,
+            #     type=identity['type'],
+            # )
 
             identity, created = Identity.objects.get_or_create(
                 idp=idp,
