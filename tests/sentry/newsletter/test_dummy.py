a351334df544dd1b4ee16f2845188f17a4f3be79
Mike Clarke <mike.k.clarke@gmail.com>
2018-04-20 13:24:18 -0700

    Re-create newsletter between tests

--- tests/sentry/newsletter/test_dummy.py
+++ tests/sentry/newsletter/test_dummy.py
@@ -3,17 +3,18 @@ from __future__ import absolute_import
 from sentry.newsletter.dummy import DummyNewsletter
 from sentry.testutils import TestCase
 
-newsletter = DummyNewsletter()
-
 
 class DummyNewsletterTest(TestCase):
 
+    def setUp(self):
+        self.newsletter = DummyNewsletter()
+
     def test_defaults(self):
-        assert newsletter.DEFAULT_LISTS == newsletter.get_default_list_ids()
-        assert newsletter.DEFAULT_LIST_ID == newsletter.get_default_list_id()
+        assert self.newsletter.DEFAULT_LISTS == self.newsletter.get_default_list_ids()
+        assert self.newsletter.DEFAULT_LIST_ID == self.newsletter.get_default_list_id()
 
     def assert_subscriptions(self, user, count):
-        subscriptions = newsletter.get_subscriptions(user)
+        subscriptions = self.newsletter.get_subscriptions(user)
         assert subscriptions.get('subscriptions') is not None
         assert len(subscriptions['subscriptions']) == count
 
@@ -21,12 +22,12 @@ class DummyNewsletterTest(TestCase):
         user = self.create_user('subscriber@example.com')
 
         self.assert_subscriptions(user, 0)
-        newsletter.create_or_update_subscription(user)
+        self.newsletter.create_or_update_subscription(user)
         self.assert_subscriptions(user, 1)
 
     def test_update_subscriptions(self):
         user = self.create_user('subscriber@example.com')
 
         self.assert_subscriptions(user, 0)
-        newsletter.create_or_update_subscriptions(user)
+        self.newsletter.create_or_update_subscriptions(user)
         self.assert_subscriptions(user, 1)
