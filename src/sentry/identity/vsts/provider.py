8984a672568437b93e1e7e81d6ec60008ab5a6ef
Lauryn Brown <lauryndbrown@gmail.com>
2018-04-20 13:30:33 -0700

    Got the access code.

--- src/sentry/identity/vsts/provider.py
+++ src/sentry/identity/vsts/provider.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 
 from sentry import options
 from sentry.options.manager import FLAG_PRIORITIZE_DISK
-from sentry.identity.oauth2 import OAuth2Provider
+from sentry.identity.oauth2 import OAuth2Provider, OAuth2LoginView, OAuth2CallbackView
 
 options.register('vsts.client-id', flags=FLAG_PRIORITIZE_DISK)
 options.register('vsts.client-secret', flags=FLAG_PRIORITIZE_DISK)
@@ -27,3 +27,55 @@ class VSTSIdentityProvider(OAuth2Provider):
 
     def get_oauth_client_secret(self):
         return options.get('vsts.client-secret')
+
+    def get_pipeline_views(self):
+        return [
+            OAuth2LoginView(
+                authorize_url=self.oauth_authorize_url,
+                client_id=self.get_oauth_client_id(),
+                scope=' '.join(self.get_oauth_scopes()),
+            ),
+            VSTSOAuth2CallbackView(
+                access_token_url=self.oauth_access_token_url,
+                client_id=self.get_oauth_client_id(),
+                client_secret=self.get_oauth_client_secret(),
+            ),
+        ]
+
+
+class VSTSOAuth2CallbackView(OAuth2CallbackView):
+
+    def __init__(self, access_token_url=None, client_id=None, client_secret=None, *args, **kwargs):
+        super(
+            VSTSOAuth2CallbackView,
+            self).__init__(
+            access_token_url,
+            client_id,
+            client_secret,
+            *
+            args,
+            **kwargs)
+
+    def exchange_token(self, request, pipeline, code):
+        from sentry.http import safe_urlopen, safe_urlread
+        from sentry.utils.http import absolute_uri
+        from six.moves.urllib.parse import parse_qsl
+        from sentry.utils import json
+        req = safe_urlopen(
+            url=self.access_token_url,
+            headers={
+                'Content-Type': 'application/x-www-form-urlencoded',
+                'Content-Length': '1322',
+            },
+            data={
+                'client_assertion_type': 'urn:ietf:params:oauth:client-assertion-type:jwt-bearer',
+                'client_assertion': self.client_secret,
+                'grant_type': 'urn:ietf:params:oauth:grant-type:jwt-bearer',
+                'assertion': code,
+                'redirect_uri': absolute_uri(pipeline.redirect_url()),
+            },
+        )
+        body = safe_urlread(req)
+        if req.headers['Content-Type'].startswith('application/x-www-form-urlencoded'):
+            return dict(parse_qsl(body))
+        return json.loads(body)
--- src/sentry/integrations/vsts/integration.py
+++ src/sentry/integrations/vsts/integration.py
@@ -35,19 +35,45 @@ class VSTSIntegration(Integration):
     def get_pipeline_views(self):
         identity_pipeline_config = {
             'oauth_scopes': self.identity_oauth_scopes,
-            'redirect_url': absolute_uri('/extensions/vsts/setup/'),  # where does this go?
+            'redirect_url': absolute_uri('/extensions/vsts/setup/'),
         }
 
         identity_pipeline_view = NestedPipelineView(
             bind_key='identity',
             provider_key='vsts',
-            pipeline_cls=IdentityProviderPipeline,  # how do we get to the VSTS provider?
+            pipeline_cls=IdentityProviderPipeline,
             config=identity_pipeline_config,
         )
 
         return [identity_pipeline_view]
 
+    def get_config(self):
+        return [{
+            'name': 'name',
+            'label': 'Name',
+            'type': 'text',
+            'required': True,
+        }]
+
     def build_integration(self, state):
+        data = state['identity']['data']
+
+        scopes = sorted(self.identity_oauth_scopes)
+        team_data = self.get_team_info(data['access_token'])
+
         return {
-            'external_id': state['name'],
+            'name': data['team_name'],
+            'external_id': state['team_id'],
+            'metadata': {
+                'access_token': data['access_token'],
+                'scopes': scopes,
+                'icon': team_data['icon'],
+                'domain_name': team_data['domain'],
+            },
+            'user_identity': {
+                'type': 'vsts',
+                'external_id': data['installer_user_id'],
+                'scopes': [],
+                'data': {},
+            },
         }
