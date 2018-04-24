bd4228de45eb8c8acafdb1ab995a0cfb60935914
Mike Clarke <mike.k.clarke@gmail.com>
2018-04-24 15:12:40 -0700

    fix(newsletter): Remove update_subscription call when adding an email (#8173)

--- src/sentry/web/frontend/accounts.py
+++ src/sentry/web/frontend/accounts.py
@@ -577,12 +577,6 @@ def show_emails(request):
                 new_email.set_hash()
                 new_email.save()
                 user.send_confirm_email_singular(new_email)
-                # Update newsletter subscription and mark as unverified
-                newsletter.update_subscription(
-                    user=user,
-                    create=True,
-                    verified=False,
-                )
 
                 logger.info(
                     'user.email.add',
