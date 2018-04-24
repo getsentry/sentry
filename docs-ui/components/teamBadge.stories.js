579669d9807b4553b114f535d98dd000faf9e6a6
Billy Vong <billyvg@users.noreply.github.com>
2018-04-24 09:21:54 -0700

    feat(ui): Add `<TeamBadge>` component (#8141)

---
+++ docs-ui/components/teamBadge.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import {storiesOf} from '@storybook/react';
+import {withInfo} from '@storybook/addon-info';
+// import {action} from '@storybook/addon-actions';
+
+import TeamBadge from 'sentry-ui/teamBadge';
+
+storiesOf('TeamBadge', module).add(
+  'default',
+  withInfo('Standard way to display a team')(() => (
+    <TeamBadge team={{slug: 'team-slug'}} />
+  ))
+);
---
+++ src/sentry/static/sentry/app/components/teamBadge.jsx
@@ -0,0 +1,44 @@
+import {Flex} from 'grid-emotion';
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'react-emotion';
+
+import Avatar from './avatar';
+import space from '../styles/space';
+import SentryTypes from '../proptypes';
+
+class TeamBadge extends React.Component {
+  static propTypes = {
+    team: SentryTypes.Team.isRequired,
+    avatarSize: PropTypes.number,
+    hideAvatar: PropTypes.bool,
+    className: PropTypes.string,
+  };
+
+  static defaultProps = {
+    avatarSize: 24,
+    hideAvatar: false,
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let {className, team, hideAvatar, avatarSize} = this.props;
+
+    return (
+      <Flex align="center" className={className}>
+        {!hideAvatar && <StyledAvatar team={team} size={avatarSize} />}
+        <span>#{team.slug}</span>
+      </Flex>
+    );
+  }
+}
+
+export default TeamBadge;
+
+const StyledAvatar = styled(Avatar)`
+  margin-right: ${space(1)};
+  flex-shrink: 0;
+`;
