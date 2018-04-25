0f85e3d31ab54d2b17c878cc19f999ece34bfa56
Chris Jennings <chriskjennings@gmail.com>
2018-04-25 14:29:28 -0700

    feature(ui): introduce Text component (#8174)
    * feature(ui): introduce text component

---
+++ src/sentry/static/sentry/app/components/text.jsx
@@ -0,0 +1,62 @@
+import styled from 'react-emotion';
+
+const Text = styled.div`
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6,
+  p,
+  ul,
+  ol,
+  table,
+  dl,
+  blockquote,
+  form,
+  pre {
+    margin-bottom: 20px;
+
+    &:last-child {
+      margin-bottom: 0;
+    }
+  }
+
+  h1,
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    line-height: 1.2;
+  }
+
+  p,
+  ul,
+  ol,
+  blockquote {
+    line-height: 1.5;
+  }
+
+  h1 {
+    font-size: 2em;
+  }
+
+  h2 {
+    font-size: 1.75em;
+  }
+
+  h3 {
+    font-size: 1.5em;
+  }
+
+  h4 {
+    font-size: 1.25em;
+  }
+
+  h5 {
+    font-size: 1em;
+  }
+`;
+
+export default Text;
