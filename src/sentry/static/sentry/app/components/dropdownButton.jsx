53a169d8a93f13576965dc21ce0232837bcb4ee4
chrissy <fivefourths@gmail.com>
2018-04-25 11:51:07 -0700

    add one scope level to dropdownButton styles to prevent override when compiled css order changes in production

--- src/sentry/static/sentry/app/components/dropdownButton.jsx
+++ src/sentry/static/sentry/app/components/dropdownButton.jsx
@@ -24,15 +24,17 @@ const StyledChevronDown = styled(props => (
 `;
 
 const StyledButton = styled(({isOpen, ...props}) => <Button {...props} />)`
-  border-bottom-right-radius: ${p => (p.isOpen ? 0 : p.theme.borderRadius)};
-  border-bottom-left-radius: ${p => (p.isOpen ? 0 : p.theme.borderRadius)};
-  position: relative;
-  z-index: 2;
-  box-shadow: none;
+  &.button {
+    border-bottom-right-radius: ${p => (p.isOpen ? 0 : p.theme.borderRadius)};
+    border-bottom-left-radius: ${p => (p.isOpen ? 0 : p.theme.borderRadius)};
+    position: relative;
+    z-index: 2;
+    box-shadow: none;
 
-  &,
-  &:hover {
-    border-bottom-color: ${p => (p.isOpen ? 'transparent' : p.theme.borderDark)};
+    &,
+    &:hover {
+      border-bottom-color: ${p => (p.isOpen ? 'transparent' : p.theme.borderDark)};
+    }
   }
 `;
 
