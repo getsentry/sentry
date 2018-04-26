93896ebd87ffc24b643013e2da2afee4dad0a7d6
Lyn Nagara <lyn.nagara@gmail.com>
2018-04-26 12:02:52 -0700

    feat(dashboard): Add ability to star project (#8205)

--- src/sentry/static/sentry/app/views/organizationDashboard/projectCard.jsx
+++ src/sentry/static/sentry/app/views/organizationDashboard/projectCard.jsx
@@ -1,13 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 import {withRouter} from 'react-router';
+import {Flex} from 'grid-emotion';
 
 import SentryTypes from '../../proptypes';
 import Link from '../../components/link';
 
+import {Client} from '../../api';
+
+import {update} from '../../actionCreators/projects';
+
 class ProjectCard extends React.Component {
   static propTypes = {
     project: SentryTypes.Project.isRequired,
+    params: PropTypes.object,
+  };
+
+  toggleProjectBookmark = () => {
+    const {project, params} = this.props;
+
+    update(new Client(), {
+      orgId: params.orgId,
+      projectId: project.slug,
+      data: {
+        isBookmarked: !project.isBookmarked,
+      },
+    });
   };
 
   render() {
@@ -15,9 +34,16 @@ class ProjectCard extends React.Component {
 
     return (
       <StyledProjectCard>
-        <ProjectCardHeader>
-          <Link to={`/${params.orgId}/${project.slug}/`}>{project.slug}</Link>
-        </ProjectCardHeader>
+        <Flex justify="space-between" p={2} align="center">
+          <Link to={`/${params.orgId}/${project.slug}/`}>
+            <strong>{project.slug}</strong>
+          </Link>
+          <Star
+            active={project.isBookmarked}
+            className="project-select-bookmark icon icon-star-solid"
+            onClick={this.toggleProjectBookmark}
+          />
+        </Flex>
       </StyledProjectCard>
     );
   }
@@ -30,6 +56,13 @@ const StyledProjectCard = styled.div`
   box-shadow: ${p => p.theme.dropShadowLight};
 `;
 
-const ProjectCardHeader = styled.div``;
+const Star = styled.a`
+  color: ${p => (p.active ? p.theme.yellowOrange : '#afa3bb')};
+  &:hover {
+    color: ${p => p.theme.yellowOrange};
+    opacity: 0.6;
+  }
+`;
 
+export {ProjectCard};
 export default withRouter(ProjectCard);
