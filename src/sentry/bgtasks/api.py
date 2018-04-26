221631158c1773fd675f6c68b5d0a66601a7f8a6
Michael Schier <schiermike@gmail.com>
2018-04-26 10:58:18 +0200

    fix loop logic in BgTask.run
    The threads that get spawned by spawn_bgtasks(role) currently immediately exit as the BgTask.running flag never gets set to True. Also, there's an issue with the value of next_run after the first callback execution.

--- src/sentry/bgtasks/api.py
+++ src/sentry/bgtasks/api.py
@@ -38,17 +38,18 @@ class BgTask(object):
     def run(self):
         if self.running:
             return
+        self.running = True
 
         next_run = time.time() + self.interval * random.random()
         while self.running:
-            started = time.time()
-            if next_run >= started:
+            now = time.time()
+            if now >= next_run:
                 try:
                     self.callback()
                 except Exception:
                     logging.error('bgtask.failed', exc_info=True,
                                   extra=dict(task_name=self.name))
-                next_run = started + self.interval
+                next_run = now + self.interval
             time.sleep(1.0)
 
     def reconfigure(self, cfg):
