80bb9e038ed4b8082ba7e0375cf69d948c33aefa
David Cramer <dcramer@gmail.com>
2018-04-26 09:44:54 -0700

    feat(api): Support security interfaces fully
    - Expose ExpectCT, ExpectStaple, and HPKP in UI
    - Correct hashing on ExpectCT, ExpectStaple, and HPKP to include type
    - Add support for ExpectCT and HPKP samples
    - Add bin/mock-event helper

--- src/sentry/static/sentry/app/components/eventOrGroupHeader.jsx
+++ src/sentry/static/sentry/app/components/eventOrGroupHeader.jsx
@@ -20,7 +20,14 @@ class EventOrGroupHeader extends React.Component {
     data: PropTypes.shape({
       id: PropTypes.string,
       level: PropTypes.string,
-      type: PropTypes.oneOf(['error', 'csp', 'default']).isRequired,
+      type: PropTypes.oneOf([
+        'error',
+        'csp',
+        'hpkp',
+        'expectct',
+        'expectstaple',
+        'default',
+      ]).isRequired,
       title: PropTypes.string,
       metadata: Metadata,
       groupID: PropTypes.string,
@@ -45,6 +52,10 @@ class EventOrGroupHeader extends React.Component {
         return metadata.value;
       case 'csp':
         return metadata.message;
+      case 'expectct':
+      case 'expectstaple':
+      case 'hpkp':
+        return '';
       default:
         return culprit || '';
     }
