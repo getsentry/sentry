ee61fe5795724368e598395cbb36bee4426e8ed3
Lyn Nagara <lyn.nagara@gmail.com>
2018-04-30 10:49:06 -0700

    feat(dashboard): Add platform list

--- src/sentry/static/sentry/app/views/organizationDashboard/projectCard.jsx
+++ src/sentry/static/sentry/app/views/organizationDashboard/projectCard.jsx
@@ -12,6 +12,76 @@ import {Client} from 'app/api';
 import {update} from 'app/actionCreators/projects';
 import overflowEllipsis from 'app/styles/overflowEllipsis';
 
+class PlatformList extends React.Component {
+  static propTypes = {
+    platforms: PropTypes.arrayOf(PropTypes.string),
+  };
+
+  getIcon(platform) {
+    return (
+      <StyledPlatformIconWrapper key={platform} className={platform}>
+        <StyledPlatformIcon className={`platformicon platformicon-${platform}`} />
+      </StyledPlatformIconWrapper>
+    );
+  }
+
+  getIcons(platforms) {
+    return (
+      <Flex direction="row-reverse" m={18}>
+        {platforms.map(this.getIcon)}
+      </Flex>
+    );
+  }
+  render() {
+    const {platforms} = this.props;
+
+    if (!platforms.length)
+      return (
+        <NoPlatforms align="center" p={2}>
+          No platforms yet
+        </NoPlatforms>
+      );
+
+    return (
+      <Flex align="center">
+        <div className="org-dashboard-platform-list">{this.getIcons(platforms)}</div>
+        <PlatformText>
+          {platforms
+            .slice()
+            .reverse()
+            .join(', ')}
+        </PlatformText>
+      </Flex>
+    );
+  }
+}
+
+const StyledPlatformIconWrapper = styled.span`
+  display: block;
+  margin-right: -14px;
+  margin-left: -2px;
+`;
+
+const StyledPlatformIcon = styled.span`
+  display: block;
+  color: white;
+  height: 34px;
+  width: 34px;
+  font-size: 22px;
+  border-radius: 4px;
+  border: 2px solid white;
+  padding: 4px;
+`;
+
+const PlatformText = styled.div`
+  color: ${p => p.theme.gray2};
+  font-size: 13px;
+`;
+
+const NoPlatforms = styled(Flex)`
+  height: 70px;
+`;
+
 class ProjectCard extends React.Component {
   static propTypes = {
     project: SentryTypes.Project.isRequired,
@@ -45,6 +115,7 @@ class ProjectCard extends React.Component {
             onClick={this.toggleProjectBookmark}
           />
         </Flex>
+        <PlatformList platforms={project.platforms} />
       </StyledProjectCard>
     );
   }
--- tests/js/spec/views/organizationDashboard/projectCard.spec.jsx
+++ tests/js/spec/views/organizationDashboard/projectCard.spec.jsx
@@ -7,14 +7,17 @@ describe('ProjectCard', function() {
   let wrapper, projectMock;
   beforeEach(function() {
     wrapper = mount(
-      <ProjectCard project={TestStubs.Project()} params={{orgId: 'org-slug'}} />,
+      <ProjectCard
+        project={TestStubs.Project({platforms: ['javascript']})}
+        params={{orgId: 'org-slug'}}
+      />,
       TestStubs.routerContext()
     );
 
     projectMock = MockApiClient.addMockResponse({
       url: '/projects/org-slug/project-slug/',
       method: 'PUT',
-      data: TestStubs.Project({isBookmarked: false}),
+      data: TestStubs.Project({isBookmarked: false, platforms: ['javascript']}),
     });
   });
 
