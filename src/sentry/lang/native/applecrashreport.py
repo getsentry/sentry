be5e91a9906032d441ec8c3f569ddd143c94a266
Jan Michael Auer <account@jauer.org>
2018-04-30 21:04:10 +0200

    fix(issues): Fix unsymbolicated raw view (#8248)

--- src/sentry/lang/native/applecrashreport.py
+++ src/sentry/lang/native/applecrashreport.py
@@ -173,7 +173,10 @@ class AppleCrashReport(object):
         slide_value = parse_addr(debug_image['image_vmaddr'])
         image_addr = parse_addr(debug_image['image_addr']) + slide_value
         return '%s - %s %s %s  <%s> %s' % (
-            hex(image_addr), hex(image_addr + debug_image['image_size'] - 1),
-            image_name(debug_image['name']), self.context['device']['arch'],
-            debug_image['uuid'].replace('-', '').lower(), debug_image['name']
+            hex(image_addr),
+            hex(image_addr + debug_image['image_size'] - 1),
+            image_name(debug_image['name']),
+            self.context['device']['arch'],
+            (debug_image.get('id') or debug_image.get('uuid')).replace('-', '').lower(),
+            debug_image['name']
         )
