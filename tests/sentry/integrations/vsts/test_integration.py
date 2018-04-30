654a7fb964b4faa5c00ebb473a18c04ac87afe1c
Lauryn Brown <lauryndbrown@gmail.com>
2018-04-30 11:48:47 -0700

    Added tests to integration.

--- tests/sentry/integrations/vsts/test_integration.py
+++ tests/sentry/integrations/vsts/test_integration.py
@@ -12,6 +12,8 @@ from sentry.testutils import IntegrationTestCase
 
 class VSTSIntegrationTest(IntegrationTestCase):
     provider = VSTSIntegration
+    instance = 'example.visualstudio.com'
+    default_project = 'MyFirstProject'
 
     @responses.activate
     def test_path(self):
@@ -28,31 +30,21 @@ class VSTSIntegrationTest(IntegrationTestCase):
         assert params['response_type'] == ['code']
         assert params['client_id'] == ['vsts-client-id']
 
-        # once we've asserted on it, switch to a singular values to make life
-        # easier
+    def test_oath(self):
+        resp = self.client.get(self.path)
+        assert resp.status_code == 302
+        redirect = urlparse(resp['Location'])
+        params = parse_qs(redirect.query)
         authorize_params = {k: v[0] for k, v in six.iteritems(params)}
 
         responses.add(
-            responses.POST, 'https://slack.com/api/oauth.token',
+            responses.GET, 'https://app.vssps.visualstudio.com/oauth2/token',
             json={
-                'ok': True,
-                'user_id': 'UXXXXXXX1',
-                'access_token': 'xoxp-xxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx',
-                'team_id': 'TXXXXXXX1',
-                'team_name': 'Example',
-                'installer_user_id': 'UXXXXXXX1',
-            }
-        )
-
-        responses.add(
-            responses.GET, 'https://slack.com/api/team.info',
-            json={
-                'ok': True,
-                'team': {
-                    'domain': 'test-slack-workspace',
-                    'icon': {'image_132': 'http://example.com/ws_icon.jpg'},
-                },
-            }
+                'access_token': 'xxxxxxxxx',
+                'token_type': 'jwt-bearer',
+                'expires_in': '3599',
+                'refresh_token': 'zzzzzzzzzz',
+            },
         )
 
         resp = self.client.get('{}?{}'.format(
@@ -67,9 +59,9 @@ class VSTSIntegrationTest(IntegrationTestCase):
         req_params = parse_qs(mock_request.body)
         assert req_params['grant_type'] == ['authorization_code']
         assert req_params['code'] == ['oauth-code']
-        assert req_params['redirect_uri'] == ['http://testserver/extensions/slack/setup/']
-        assert req_params['client_id'] == ['slack-client-id']
-        assert req_params['client_secret'] == ['slack-client-secret']
+        assert req_params['redirect_uri'] == ['http://testserver/extensions/vsts/setup/']
+        assert req_params['client_id'] == ['vsts-client-id']
+        assert req_params['client_secret'] == ['vsts-client-secret']
 
         assert resp.status_code == 200
         self.assertDialogSuccess(resp)
@@ -99,3 +91,50 @@ class VSTSIntegrationTest(IntegrationTestCase):
             external_id='UXXXXXXX1',
         )
         assert identity.status == IdentityStatus.VALID
+
+    @responses.activate
+    def test_build_integration(self):
+        first_project_id = 'xxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxx'
+        responses.add(
+            responses.GET,
+            'https://{}/DefaultCollection/_apis/projects'.format(self.instance),
+            json={
+                'value': [
+                    {
+                        'id': first_project_id,
+                        'name': self.default_project,
+                        'url': 'https://myfirstproject.visualstudio.com/DefaultCollection/_apis/projects/xxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxx',
+                        'description': 'My First Project!',
+                    },
+                    {
+                        'id': 'xxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxz',
+                        'name': 'MySecondProject',
+                        'url': 'https://mysecondproject.visualstudio.com/DefaultCollection/_apis/projects/xxxxxxx-xxxx-xxxx-xxxxxxxxxxxxxxxz',
+                        'description': 'Not My First Project!',
+                    }
+                ],
+                'count': 2,
+            },
+        )
+
+        integration = VSTSIntegration()
+        state = {
+            'identity': {
+                'data': {'access_token': 'xxxxxxxxxxxxxxx', },
+                'instance': self.instance,
+                'default_project': self.default_project,
+            }
+        }
+        integration_dict = integration.build_integration(state)
+        assert integration_dict['name'] == self.default_project
+        assert integration_dict['external_id'] == first_project_id
+        assert integration_dict['metadata']['access_token'] == state['identity']['data']['access_token']
+        assert set(integration_dict['metadata']['scopes']) == set(integration.identity_oauth_scopes)
+        assert integration_dict['metadata']['domain_name'] == self.instance
+
+        assert integration_dict['user_identity'] == {
+            'type': 'vsts',
+            'external_id': self.instance,
+            'scopes': [],
+            'data': {},
+        }
