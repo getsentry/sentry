4f7ffc719aea149c20bd92d45c2fb1cbc16af70c
Lauryn Brown <lauryndbrown@gmail.com>
2018-04-30 18:13:50 -0700

    Added test specifically for vsts oauth

--- tests/sentry/integrations/vsts/test_provider.py
+++ tests/sentry/integrations/vsts/test_provider.py
@@ -1,8 +1,10 @@
 
 from __future__ import absolute_import
 from mock import Mock
-from sentry.identity.vsts.provider import ConfigView
+import responses
+from sentry.identity.vsts.provider import ConfigView, VSTSOAuth2CallbackView
 from sentry.testutils import TestCase
+from six.moves.urllib.parse import parse_qs
 
 
 class TestConfigView(TestCase):
@@ -41,4 +43,43 @@ class TestConfigView(TestCase):
 
 
 class TestVSTSOAuthCallbackView(TestCase):
-    pass
+    @responses.activate
+    def test_exchange_token(self):
+        def redirect_url():
+            return 'https://app.vssps.visualstudio.com/oauth2/authorize'
+
+        view = VSTSOAuth2CallbackView(
+            access_token_url='https://app.vssps.visualstudio.com/oauth2/token',
+            client_id='vsts-client-id',
+            client_secret='vsts-client-secret',
+        )
+        request = Mock()
+        pipeline = Mock()
+
+        pipeline.redirect_url = redirect_url
+
+        responses.add(
+            responses.POST, 'https://app.vssps.visualstudio.com/oauth2/token',
+            json={
+                'access_token': 'xxxxxxxxx',
+                'token_type': 'jwt-bearer',
+                'expires_in': '3599',
+                'refresh_token': 'zzzzzzzzzz',
+            },
+        )
+
+        result = view.exchange_token(request, pipeline, 'oauth-code')
+        mock_request = responses.calls[0].request
+        req_params = parse_qs(mock_request.body)
+
+        assert req_params['grant_type'] == ['urn:ietf:params:oauth:grant-type:jwt-bearer']
+        assert req_params['assertion'] == ['oauth-code']
+        assert req_params['redirect_uri'] == ['https://app.vssps.visualstudio.com/oauth2/authorize']
+        assert req_params['client_assertion_type'] == [
+            'urn:ietf:params:oauth:client-assertion-type:jwt-bearer']
+        assert req_params['client_assertion'] == ['vsts-client-secret']
+
+        assert result['access_token'] == 'xxxxxxxxx'
+        assert result['token_type'] == 'jwt-bearer'
+        assert result['expires_in'] == '3599'
+        assert result['refresh_token'] == 'zzzzzzzzzz'
