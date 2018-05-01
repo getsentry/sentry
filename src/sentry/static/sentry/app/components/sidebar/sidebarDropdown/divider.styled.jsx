864c5891d96a45ffece430844979e30aabca1c66
Billy Vong <billy@sentry.io>
2018-05-01 15:10:56 -0700

    remove text-indent

--- src/sentry/static/sentry/app/components/sidebar/sidebarDropdown/divider.styled.jsx
+++ src/sentry/static/sentry/app/components/sidebar/sidebarDropdown/divider.styled.jsx
@@ -2,7 +2,6 @@ import styled from 'react-emotion';
 
 const Divider = styled('div')`
   height: 0;
-  text-indent: -9999em;
   border-bottom: 1px solid ${p => p.theme.borderLight};
   margin: 5px 0;
 `;
