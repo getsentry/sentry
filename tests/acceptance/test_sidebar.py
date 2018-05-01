066a18e5e9e483bea23b326694154b765ba67740
Billy Vong <billy@sentry.io>
2018-04-30 22:20:11 -0700

    fix acceptance test

--- tests/acceptance/test_sidebar.py
+++ tests/acceptance/test_sidebar.py
@@ -12,17 +12,18 @@ class SidebarTest(AcceptanceTestCase):
         self.create_organization(name="Bar Bar Bar Bar Bar Bar Bar", owner=self.user)
         self.path = '/'
 
-    def test_simple(self):
-        self.browser.get(self.path)
-        self.browser.wait_until_not('.loading')
-        self.browser.click('[data-test-id="sidebar-dropdown"]')
-        self.browser.move_to('[data-test-id="sidebar-switch-org"]')
-        self.browser.wait_until('[data-test-id="sidebar-switch-org-menu"]')
-        self.browser.snapshot('sidebar - switch org expanded')
-        self.browser.click('[data-test-id="sidebar-collapse"]')
-        self.browser.snapshot('sidebar - collapsed')
-        self.browser.click('[data-test-id="sidebar-broadcasts"]')
-        self.browser.wait_until('[data-test-id="sidebar-broadcasts-panel"]')
-        self.browser.snapshot('sidebar - broadcasts panel')
-        self.browser.click('.organization-home')
-        self.browser.wait_until_not('[data-test-id="sidebar-broadcasts-panel"]')
+    def test_new_sidebar(self):
+        with self.feature('organizations:dashboard'):
+            self.browser.get(self.path)
+            self.browser.wait_until_not('.loading')
+            self.browser.click('[data-test-id="sidebar-dropdown"]')
+            self.browser.move_to('[data-test-id="sidebar-switch-org"]')
+            self.browser.wait_until('[data-test-id="sidebar-switch-org-menu"]')
+            self.browser.snapshot('sidebar - switch org expanded')
+            self.browser.click('[data-test-id="sidebar-collapse"]')
+            self.browser.snapshot('sidebar - collapsed')
+            self.browser.click('[data-test-id="sidebar-broadcasts"]')
+            self.browser.wait_until('[data-test-id="sidebar-broadcasts-panel"]')
+            self.browser.snapshot('sidebar - broadcasts panel')
+            self.browser.click('.organization-home')
+            self.browser.wait_until_not('[data-test-id="sidebar-broadcasts-panel"]')
