dc00e9126b18d45748f09899ed08aea30d3a0cb9
Lyn Nagara <lyn.nagara@gmail.com>
2018-04-30 17:10:14 -0700

    fixes

---
+++ src/sentry/static/sentry/app/views/organizationDashboard/platformList.jsx
@@ -0,0 +1,74 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'react-emotion';
+import {Flex} from 'grid-emotion';
+
+export default class PlatformList extends React.Component {
+  static propTypes = {
+    platforms: PropTypes.arrayOf(PropTypes.string),
+  };
+
+  getIcon(platform) {
+    return (
+      <StyledPlatformIconWrapper key={platform} className={platform}>
+        <StyledPlatformIcon className={`platformicon platformicon-${platform}`} />
+      </StyledPlatformIconWrapper>
+    );
+  }
+
+  getIcons(platforms) {
+    return (
+      <Flex direction="row-reverse" p={2}>
+        {platforms.map(this.getIcon)}
+      </Flex>
+    );
+  }
+  render() {
+    const {platforms} = this.props;
+
+    if (!platforms.length)
+      return (
+        <NoPlatforms align="center" p={2}>
+          No platforms yet
+        </NoPlatforms>
+      );
+
+    return (
+      <Flex align="center">
+        <div className="org-dashboard-platform-list">{this.getIcons(platforms)}</div>
+        <PlatformText>
+          {platforms
+            .slice()
+            .reverse()
+            .join(', ')}
+        </PlatformText>
+      </Flex>
+    );
+  }
+}
+
+const StyledPlatformIconWrapper = styled.span`
+  display: block;
+  margin-right: -14px;
+  margin-left: -2px;
+`;
+
+const StyledPlatformIcon = styled.span`
+  display: block;
+  color: white;
+  height: 34px;
+  width: 34px;
+  font-size: 22px;
+  border-radius: 4px;
+  border: 2px solid white;
+  padding: 4px;
+`;
+
+const PlatformText = styled.div`
+  color: ${p => p.theme.gray2};
+  font-size: 13px;
+`;
+
+const NoPlatforms = styled(Flex)`
+  height: 70px;
+`;
--- src/sentry/static/sentry/app/views/organizationDashboard/projectCard.jsx
+++ src/sentry/static/sentry/app/views/organizationDashboard/projectCard.jsx
@@ -6,81 +6,10 @@ import {Flex} from 'grid-emotion';
 
 import SentryTypes from 'app/proptypes';
 import Link from 'app/components/link';
-
 import {Client} from 'app/api';
-
 import {update} from 'app/actionCreators/projects';
 import overflowEllipsis from 'app/styles/overflowEllipsis';
-
-class PlatformList extends React.Component {
-  static propTypes = {
-    platforms: PropTypes.arrayOf(PropTypes.string),
-  };
-
-  getIcon(platform) {
-    return (
-      <StyledPlatformIconWrapper key={platform} className={platform}>
-        <StyledPlatformIcon className={`platformicon platformicon-${platform}`} />
-      </StyledPlatformIconWrapper>
-    );
-  }
-
-  getIcons(platforms) {
-    return (
-      <Flex direction="row-reverse" m={18}>
-        {platforms.map(this.getIcon)}
-      </Flex>
-    );
-  }
-  render() {
-    const {platforms} = this.props;
-
-    if (!platforms.length)
-      return (
-        <NoPlatforms align="center" p={2}>
-          No platforms yet
-        </NoPlatforms>
-      );
-
-    return (
-      <Flex align="center">
-        <div className="org-dashboard-platform-list">{this.getIcons(platforms)}</div>
-        <PlatformText>
-          {platforms
-            .slice()
-            .reverse()
-            .join(', ')}
-        </PlatformText>
-      </Flex>
-    );
-  }
-}
-
-const StyledPlatformIconWrapper = styled.span`
-  display: block;
-  margin-right: -14px;
-  margin-left: -2px;
-`;
-
-const StyledPlatformIcon = styled.span`
-  display: block;
-  color: white;
-  height: 34px;
-  width: 34px;
-  font-size: 22px;
-  border-radius: 4px;
-  border: 2px solid white;
-  padding: 4px;
-`;
-
-const PlatformText = styled.div`
-  color: ${p => p.theme.gray2};
-  font-size: 13px;
-`;
-
-const NoPlatforms = styled(Flex)`
-  height: 70px;
-`;
+import PlatformList from 'app/views/organizationDashboard/platformList';
 
 class ProjectCard extends React.Component {
   static propTypes = {
