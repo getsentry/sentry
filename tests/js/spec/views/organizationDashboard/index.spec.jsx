85402da9c86c6b142766252edf1f4606c4454c5a
Lyn Nagara <lyn.nagara@gmail.com>
2018-05-01 11:28:59 -0700

    add test

--- tests/js/spec/views/organizationDashboard/index.spec.jsx
+++ tests/js/spec/views/organizationDashboard/index.spec.jsx
@@ -1,29 +1,59 @@
 import React from 'react';
-import {mount} from 'enzyme';
+import {shallow} from 'enzyme';
 
 import {Dashboard} from 'app/views/organizationDashboard';
 
 describe('OrganizationDashboard', function() {
-  beforeEach(function() {
-    MockApiClient.addMockResponse({
-      url: '/organizations/org-slug/projects/?statsPeriod=24h',
-      data: [],
-    });
-  });
-
   afterEach(function() {
     MockApiClient.clearMockResponses();
   });
 
-  it('renders empty state', function() {
-    const teams = [TestStubs.Team()];
-    const projects = [];
+  describe('empty state', function() {
+    beforeEach(function() {
+      MockApiClient.addMockResponse({
+        url: '/organizations/org-slug/projects/?statsPeriod=24h',
+        body: [],
+      });
+    });
 
-    const wrapper = mount(
-      <Dashboard teams={teams} projects={projects} params={{orgId: 'org-slug'}} />,
-      TestStubs.routerContext()
-    );
-    const emptyState = wrapper.find('EmptyState');
-    expect(emptyState).toHaveLength(1);
+    it('renders', function() {
+      const teams = [TestStubs.Team()];
+      const projects = [];
+
+      const wrapper = shallow(
+        <Dashboard teams={teams} projects={projects} params={{orgId: 'org-slug'}} />,
+        TestStubs.routerContext()
+      );
+      const emptyState = wrapper.find('EmptyState');
+      expect(emptyState).toHaveLength(1);
+    });
+  });
+
+  describe('with projects', function() {
+    beforeEach(function() {
+      MockApiClient.addMockResponse({
+        url: '/organizations/org-slug/projects/?statsPeriod=24h',
+        body: [
+          TestStubs.Project({
+            teams: [TestStubs.Team()],
+            stats: [[1517281200, 2], [1517310000, 1]],
+          }),
+        ],
+      });
+    });
+
+    it('renders project cards', function() {
+      const teams = [TestStubs.Team()];
+      const projects = [TestStubs.Project({teams})];
+
+      const wrapper = shallow(
+        <Dashboard teams={teams} projects={projects} params={{orgId: 'org-slug'}} />,
+        TestStubs.routerContext()
+      );
+      const emptyState = wrapper.find('EmptyState');
+      const projectCard = wrapper.find('ProjectCardWrapper');
+      expect(emptyState).toHaveLength(0);
+      expect(projectCard).toHaveLength(1);
+    });
   });
 });
