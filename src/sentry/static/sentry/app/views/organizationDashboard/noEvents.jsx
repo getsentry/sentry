41a59921345dacc6fc9f466f48785666aad33905
Lyn Nagara <lyn.nagara@gmail.com>
2018-05-02 13:40:30 -0700

    feat(dashboard): New project card empty state design (#8285)

--- src/sentry/static/sentry/app/views/organizationDashboard/noEvents.jsx
+++ src/sentry/static/sentry/app/views/organizationDashboard/noEvents.jsx
@@ -1,31 +1,16 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 import {Flex} from 'grid-emotion';
-
-import Button from 'app/components/buttons/button';
 
 import {t} from 'app/locale';
 
 export default class NoEvents extends React.Component {
-  static propTypes = {
-    projectId: PropTypes.string,
-    orgId: PropTypes.string,
-    platformId: PropTypes.string,
-  };
   render() {
-    const {orgId, projectId, platformId} = this.props;
-    const link = `/${orgId}/${projectId}/getting-started/${platformId
-      ? platformId + '/'
-      : ''}`;
-
     return (
       <Container>
-        <Background align="center" justify="center">
-          <Button size="small" to={link}>
-            {t('Configure project')}
-          </Button>
-        </Background>
+        <EmptyText align="center" justify="center">
+          {t('No activity yet.')}
+        </EmptyText>
       </Container>
     );
   }
@@ -38,9 +23,10 @@ const Container = styled.div`
   right: 0;
 `;
 
-const Background = styled(Flex)`
+const EmptyText = styled(Flex)`
   background-color: rgba(175, 163, 187, 0.2);
   margin-left: 4px;
   margin-right: 4px;
   height: 80px;
+  color: ${p => p.theme.gray2};
 `;
