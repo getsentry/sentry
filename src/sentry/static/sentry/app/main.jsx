3c609b130010b586feefcd160800200011b74d41
Billy Vong <billy@sentry.io>
2018-05-03 11:19:12 -0700

    fix(ui): Fix sidebar being too jumpy on load

--- src/sentry/static/sentry/app/main.jsx
+++ src/sentry/static/sentry/app/main.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import {Router, browserHistory} from 'react-router';
+
 import routes from 'app/routes';
+import {loadSidebarState} from 'app/actionCreators/sidebar';
 
 export default class Main extends React.Component {
+  componentDidMount() {
+    loadSidebarState();
+  }
+
   render() {
     return <Router history={browserHistory}>{routes()}</Router>;
   }
