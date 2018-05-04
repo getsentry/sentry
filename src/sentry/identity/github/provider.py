d7a738d9c95143dd1f34d480d335c63e72189c18
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-05-03 17:16:04 -0700

    Correctly retrieve the users github identity

--- src/sentry/identity/github/provider.py
+++ src/sentry/identity/github/provider.py
@@ -1,7 +1,21 @@
 from __future__ import absolute_import
 
+from sentry import http
 from sentry import options
 from sentry.identity.oauth2 import OAuth2Provider
+
+
+def get_user_info(access_token):
+    session = http.build_session()
+    resp = session.get(
+        'https://api.github.com/user',
+        params={'access_token': access_token},
+        headers={'Accept': 'application/vnd.github.machine-man-preview+json'}
+    )
+    resp.raise_for_status()
+    resp = resp.json()
+
+    return resp
 
 
 class GitHubIdentityProvider(OAuth2Provider):
@@ -21,11 +35,12 @@ class GitHubIdentityProvider(OAuth2Provider):
 
     def build_identity(self, data):
         data = data['data']
+        user = get_user_info(data['access_token'])
 
         return {
             'type': 'github',
-            'id': data['user']['id'],
-            'email': data['user']['email'],
-            'scopes': sorted(data['scope'].split(',')),
+            'id': user['id'],
+            'email': user['email'],
+            'scopes': [],  # GitHub apps do not have user scopes
             'data': self.get_oauth_data(data),
         }
