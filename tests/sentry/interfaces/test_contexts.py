21aa4f6255b6be2eb784fb54a42d37d2dafa8428
Bruno Garcia <github@brunogarcia.com>
2018-05-07 23:27:45 +0200

    feat(contexts): runtime normalization (#8314)
    * feat(contexts): Runtime info sent via raw_description is parsed when no Name and Version is set
    * feat: Runtime version takes only the numbers - icons for .NET, Mono and Electron.
    * feat: more precise runtime information

--- tests/sentry/interfaces/test_contexts.py
+++ tests/sentry/interfaces/test_contexts.py
@@ -71,6 +71,27 @@ class ContextsTest(TestCase):
             }
         }
 
+    def test_runtime_normalization(self):
+        ctx = Contexts.to_python({
+            'runtime': {
+                'raw_description': '.NET Framework 4.0.30319.42000',
+                'build': '461808',
+            }
+        })
+        assert sorted(ctx.iter_tags()) == [
+            ('runtime', '.NET Framework 4.7.2'),
+            ('runtime.name', '.NET Framework')
+        ]
+        assert ctx.to_json() == {
+            'runtime': {
+                'type': 'runtime',
+                'raw_description': '.NET Framework 4.0.30319.42000',
+                'build': '461808',
+                'name': '.NET Framework',
+                'version': '4.7.2'
+            }
+        }
+
     def test_device(self):
         ctx = Contexts.to_python(
             {
