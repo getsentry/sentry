1e03139d50d7f784fd374a11da56c0c0d1c0ae11
denamwangi <dena@getsentry.com>
2018-05-09 12:56:19 -0700

    update css- might be janky

--- src/sentry/static/sentry/app/views/planout/installReact.jsx
+++ src/sentry/static/sentry/app/views/planout/installReact.jsx
@@ -23,7 +23,7 @@ const InstallReactTest = createReactClass({
 
     return (
       <div>
-        <h2> Installation </h2>
+        <h3> Installation </h3>
         <p>
           {' '}
           Start by adding the <span className="pre"> raven.js </span> script tag to your
@@ -31,42 +31,42 @@ const InstallReactTest = createReactClass({
           bundle.{' '}
         </p>
         <pre>
-          <span style={{color: '#111111'}}>{'<'}</span>
-          <span style={{color: '#2eb0f7'}}>script </span>
-          <span style={{color: '#a47ac6'}}>src</span>
-          <span style={{color: '#111111'}}>=</span>
-          <span style={{color: '#e8535a'}}>
+          <span>{'<'}</span>
+          <span className="script">script </span>
+          <span className="attribute">src</span>
+          <span>=</span>
+          <span className="value-text">
             "https://cdn.ravenjs.com/3.24.0/raven.min.js"
           </span>
           <br />
-          <span style={{color: '#a47ac6'}}> crossorigin</span>
-          <span style={{color: '#111111'}}>=</span>
-          <span style={{color: '#e8535a'}}>{'anonymous'}</span>
-          <span style={{color: '#111111'}}>{'>'}</span>
-          <span style={{color: '#111111'}}>{'</'}</span>
-          <span style={{color: '#2eb0f7'}}>script</span>
-          <span style={{color: '#111111'}}>{'>'}</span>
+          <span className="attribute"> crossorigin</span>
+          <span>=</span>
+          <span className="value-text">{'anonymous'}</span>
+          <span>{'>'}</span>
+          <span>{'</'}</span>
+          <span className="script">script</span>
+          <span>{'>'}</span>
         </pre>
 
-        <h2> Basic Configuration </h2>
+        <h3> Basic Configuration </h3>
         <p>{t('Next configure Raven.js to use your Sentry DSN: ')}</p>
 
         <pre>
-          <span style={{color: '#111111'}}>{'Raven.config('}</span>
-          <span style={{color: '#e8535a'}}>{"'"}</span>
-          <span style={{color: '#e8535a'}}>{dsn}</span>
-          <span style={{color: '#111111'}}>{"', {"}</span>
+          <span>{'Raven.config('}</span>
+          <span className="value-text">{"'"}</span>
+          <span className="value-text">{dsn}</span>
+          <span>{"', {"}</span>
           <br />
 
-          <span style={{color: '#111111'}}>{'   release: '}</span>
-          <span style={{color: '#e8535a'}}>{'test-1-2-3'}</span>
-          <span style={{color: '#111111'}}>{','}</span>
+          <span>{'   release: '}</span>
+          <span className="value-text">{'test-1-2-3'}</span>
+          <span>{','}</span>
           <br />
-          <span style={{color: '#111111'}}>{'   environment: '}</span>
-          <span style={{color: '#e8535a'}}>{'development'}</span>
-          <span style={{color: '#111111'}}>{','}</span>
+          <span>{'   environment: '}</span>
+          <span className="value-text">{'development'}</span>
+          <span>{','}</span>
           <br />
-          <span style={{color: '#111111'}}>{'}).install()'}</span>
+          <span>{'}).install()'}</span>
         </pre>
         <p>Congrats! Raven is now ready to capture any uncaught exceptions</p>
       </div>
