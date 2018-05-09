0b6c621db4b377fff386e7be2b60505cc81e29e8
Jess MacQueen <jessmacqueen@gmail.com>
2018-05-09 14:13:16 -0700

    feat(integrations): Add endpoint to return config for linking external issues to a group

--- tests/sentry/api/endpoints/test_group_integration_details.py
+++ tests/sentry/api/endpoints/test_group_integration_details.py
@@ -1,11 +1,44 @@
 from __future__ import absolute_import
 
+import six
+
 from sentry.models import ExternalIssue, GroupLink, Integration
 from sentry.testutils import APITestCase
 
 
 class GroupIntegrationDetailsTest(APITestCase):
-    def test_simple(self):
+    def test_simple_get(self):
+        self.login_as(user=self.user)
+        org = self.organization
+        group = self.create_group()
+        integration = Integration.objects.create(
+            provider='example',
+            name='Example',
+        )
+        integration.add_organization(org.id)
+
+        path = '/api/0/issues/{}/integrations/{}/?action=link'.format(group.id, integration.id)
+
+        response = self.client.get(path)
+
+        assert response.data == {
+            'id': six.text_type(integration.id),
+            'name': integration.name,
+            'icon': integration.metadata.get('icon'),
+            'domain_name': integration.metadata.get('domain_name'),
+            'provider': {
+                'key': integration.get_provider().key,
+                'name': integration.get_provider().name,
+            },
+            'linkIssueConfig': [{
+                'default': '',
+                'type': 'string',
+                'name': 'externalIssue',
+                'label': 'Issue',
+            }]
+        }
+
+    def test_simple_put(self):
         self.login_as(user=self.user)
         org = self.organization
         group = self.create_group()
