09b874e675fd641495d18fe82f9e7dde0d8f4d1c
HazA <daniel.griesser.86@gmail.com>
2018-05-09 10:46:16 +0200

    ref: Remove attrs from exception

--- src/sentry/relay/queries/base.py
+++ src/sentry/relay/queries/base.py
@@ -1,14 +1,8 @@
 from __future__ import absolute_import
 
-from rest_framework import status
-
 
 class InvalidQuery(Exception):
-    # TODO(hazat): dunno about this attrs
-    message = 'Unsupported query type'
-    code = 1001
-    response = {'error': message, 'code': code}
-    status_code = status.HTTP_400_BAD_REQUEST
+    pass
 
 
 class BaseQuery(object):
