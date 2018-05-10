5125ffe6bb24feaad7f701bac7b4db6e5cbe5ba3
Jess MacQueen <jessmacqueen@gmail.com>
2018-05-09 18:06:28 -0700

    feat(integrations): Add JIRA issue search endpoint

--- src/sentry/integrations/jira/client.py
+++ src/sentry/integrations/jira/client.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 import datetime
 import jwt
+import re
 from six.moves.urllib.parse import urlparse
 
 from sentry.http import build_session
@@ -15,6 +16,7 @@ JIRA_KEY = '%s.jira' % (urlparse(absolute_uri()).hostname, )
 class JiraApiClient(object):
     COMMENT_URL = '/rest/api/2/issue/%s/comment'
     ISSUE_URL = '/rest/api/2/issue/%s'
+    SEARCH_URL = '/rest/api/2/search/'
 
     def __init__(self, base_url, shared_secret):
         self.base_url = base_url
@@ -47,6 +49,14 @@ class JiraApiClient(object):
 
     def get_issue(self, issue_id):
         return self.request('GET', self.ISSUE_URL % (issue_id,))
+
+    def search_issues(self, query):
+        # check if it looks like an issue id
+        if re.search(r'^[A-Za-z]+-\d+$', query):
+            jql = 'id="%s"' % query.replace('"', '\\"')
+        else:
+            jql = 'text ~ "%s"' % query.replace('"', '\\"')
+        return self.request('GET', self.SEARCH_URL, params={'jql': jql})
 
     def create_comment(self, issue_key, comment):
         return self.request('POST', self.COMMENT_URL % issue_key, data={'body': comment})
