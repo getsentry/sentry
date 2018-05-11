4b397b542e4166995240c59a685995920ef9a52d
maheskett <meg.heskett@gmail.com>
2018-05-10 22:57:33 -0700

    Add recovery code copy, download buttons

--- src/sentry/static/sentry/app/views/settings/account/accountSecurity/components/recoveryCodes.jsx
+++ src/sentry/static/sentry/app/views/settings/account/accountSecurity/components/recoveryCodes.jsx
@@ -5,6 +5,7 @@ import styled, {css} from 'react-emotion';
 
 import {t} from 'app/locale';
 import Button from 'app/components/buttons/button';
+import Clipboard from 'app/components/clipboard';
 import Confirm from 'app/components/confirm';
 import EmptyMessage from 'app/views/settings/components/emptyMessage';
 import InlineSvg from 'app/components/inlineSvg';
@@ -34,36 +35,70 @@ class RecoveryCodes extends React.Component {
 
     if (!isEnrolled || !codes) return null;
 
+    let formattedCodes = codes.join('\n');
+
+    let printCodes = () => {
+      let iframe = window.frames.printable;
+      iframe.document.write(codes.join('<br>'));
+      iframe.print();
+      iframe.document.close();
+    };
+
     return (
       <Panel css={{marginTop: 30}}>
         <PanelHeader>
           <Flex align="center">
-            <Box flex="1">{t('Unused Codes')}</Box>
+            <Box ml={2}>{t('Unused Codes')}</Box>
+            <Box ml={3}>
+              <Clipboard hideUnsupported value={formattedCodes}>
+                <Button size="small">
+                  <InlineSvg src="icon-copy" />
+                </Button>
+              </Clipboard>
+            </Box>
+            <Box ml={1}>
+              <Button size="small" onClick={printCodes}>
+                <InlineSvg src="icon-print" />
+              </Button>
+            </Box>
+            <Box ml={1}>
+              <Button
+                size="small"
+                download="sentry-recovery-codes.txt"
+                href={`data:text/plain;charset=utf-8,${formattedCodes}`}
+              >
+                <InlineSvg src="icon-download" />
+              </Button>
+            </Box>
+          </Flex>
+          <Box ml={3}>
             <Confirm
               onConfirm={this.props.onRegenerateBackupCodes}
               message={t(
                 'Are you sure you want to regenerate recovery codes? Your old codes will no longer work.'
               )}
             >
-              <Button priority="danger" size="small">
+              <Button priority="danger" size="small" className="align-right">
                 {t('Regenerate Codes')}
               </Button>
             </Confirm>
-          </Flex>
+          </Box>
         </PanelHeader>
         <PanelBody>
           <PanelWarning>
             <InlineSvg css={{fontSize: '2em'}} src="icon-warning-sm" />
             <Flex align="center" ml={2} flex="1">
-              {t(`Make sure to keep a copy of these codes to recover your account if you lose
-              your authenticator.`)}
+              {t(
+                'Make sure to save a copy of your recovery codes and store them in a safe place.'
+              )}
             </Flex>
           </PanelWarning>
-          {!!codes.length && codes.map(code => <Code key={code}>{code}</Code>)}
+          <Box>{!!codes.length && codes.map(code => <Code key={code}>{code}</Code>)}</Box>
           {!codes.length && (
             <EmptyMessage>{t('You have no more recovery codes to use')}</EmptyMessage>
           )}
         </PanelBody>
+        <iframe name="printable" css={{display: 'none'}} />
       </Panel>
     );
   }
