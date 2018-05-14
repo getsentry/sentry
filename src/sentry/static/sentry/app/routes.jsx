74b5035180c5a35b9f852bb8ae1754bc4140d892
Jan Michael Auer <account@jauer.org>
2018-05-14 15:18:06 +0200

    feat: Implement relay basics (#8162)
    This adds the initial support for relays. So far only explicitly whitelisted relays are permitted in production.

--- src/sentry/static/sentry/app/routes.jsx
+++ src/sentry/static/sentry/app/routes.jsx
@@ -11,6 +11,7 @@ import AdminOverview from 'app/views/adminOverview';
 import AdminProjects from 'app/views/adminProjects';
 import AdminQueue from 'app/views/adminQueue';
 import AdminQuotas from 'app/views/adminQuotas';
+import AdminRelays from 'app/views/adminRelays';
 import AdminSettings from 'app/views/adminSettings';
 import AdminUsers from 'app/views/adminUsers';
 import ApiApplicationDetails from 'app/views/apiApplicationDetails';
@@ -620,6 +621,7 @@ function routes() {
       <Route path="/manage/" component={errorHandler(AdminLayout)}>
         <IndexRoute component={errorHandler(AdminOverview)} />
         <Route path="buffer/" component={errorHandler(AdminBuffer)} />
+        <Route path="relays/" component={errorHandler(AdminRelays)} />
         <Route path="organizations/" component={errorHandler(AdminOrganizations)} />
         <Route path="projects/" component={errorHandler(AdminProjects)} />
         <Route path="queue/" component={errorHandler(AdminQueue)} />
