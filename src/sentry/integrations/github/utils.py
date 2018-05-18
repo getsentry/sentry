9169ed157f96297ae6dd971fb4bf7eeff1544b27
Meredith <meredith@getsentry.com>
2018-05-18 14:05:14 -0700

    refactor get_jwt

--- src/sentry/integrations/github/utils.py
+++ src/sentry/integrations/github/utils.py
@@ -9,6 +9,10 @@ from sentry import options
 
 
 def get_jwt(github_id=None, github_private_key=None):
+    if github_id is None:
+        github_id = options.get('github-app.id')
+    if github_private_key is None:
+        github_private_key = options.get('github-app.private-key')
     exp = datetime.datetime.utcnow() + datetime.timedelta(minutes=10)
     exp = calendar.timegm(exp.timetuple())
     # Generate the JWT
@@ -18,8 +22,7 @@ def get_jwt(github_id=None, github_private_key=None):
         # JWT expiration time (10 minute maximum)
         'exp': exp,
         # Integration's GitHub identifier
-        'iss': github_id or options.get('github-app.id'),
+        'iss': github_id,
     }
 
-    return jwt.encode(payload, github_private_key or options.get(
-        'github-app.private-key'), algorithm='RS256')
+    return jwt.encode(payload, github_private_key, algorithm='RS256')
