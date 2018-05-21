cf6a160d8867da19ef6451bdf211c53a5b7a58d9
Ted Kaemming <ted@kaemming.com>
2018-05-21 16:15:27 -0700

    more oops :sweat_smile:

--- src/sentry/utils/snuba.py
+++ src/sentry/utils/snuba.py
@@ -203,7 +203,7 @@ def get_related_project_ids(column, ids):
     mappings = {
         'environment': (Environment, 'id', 'project_id'),
         'issue': (Group, 'id', 'project_id'),
-        'sentry:release': (ReleaseProject, 'release_id', 'project_id'),
+        'tags[sentry:release]': (ReleaseProject, 'release_id', 'project_id'),
     }
     if ids:
         if column == "project_id":
