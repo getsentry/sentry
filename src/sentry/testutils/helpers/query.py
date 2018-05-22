8b9a77ace129304d472a28288c44edbd333fddeb
HazA <daniel.griesser.86@gmail.com>
2018-05-22 11:44:32 +0200

    fix: mysql/postgres query parsing

--- src/sentry/testutils/helpers/query.py
+++ src/sentry/testutils/helpers/query.py
@@ -16,14 +16,19 @@ def parse_queries(captured_queries):
     real_queries = {}
 
     for query in captured_queries:
+        raw_sql = query['sql']
         match = re.search(r"QUERY = (.+) - PARAMS", query['sql'])
-        if match:
-            parsed = sqlparse.parse(ast.literal_eval(match.group(1)))
-            for token in parsed[0].tokens:
-                if token.ttype is DML:
-                    if token.value.upper() in write_ops:
-                        if real_queries.get(parsed[0].get_name()) is None:
-                            real_queries[parsed[0].get_name()] = 0
-                        real_queries[parsed[0].get_name()] += 1
+        if match:  # this is a sqlite query
+            raw_sql = ast.literal_eval(match.group(1))
+        parsed = sqlparse.parse(raw_sql)
+        for token in parsed[0].tokens:
+            if token.ttype is DML:
+                if token.value.upper() in write_ops:
+                    table_name = parsed[0].get_real_name()
+                    if parsed[0].get_real_name() == "*":  # DELETE * FROM ...
+                        table_name = parsed[0].get_name()
+                    if real_queries.get(table_name) is None:
+                        real_queries[table_name] = 0
+                    real_queries[table_name] += 1
 
     return real_queries
