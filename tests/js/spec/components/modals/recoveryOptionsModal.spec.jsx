e6788cf490a75065421468aa59108c0dfef491ad
Megan Heskett <meg.heskett@gmail.com>
2018-05-22 15:34:27 -0700

    feat(ui): Add "Recovery Options" Modal (#8411)
    * Add recovery code copy, download buttons
    
    * Update recovery code description
    
    * Add RecoveryOptionsModal
    
    * Add small fixes
    
    * Change import to absolute path
    
    * Update messaging
    
    * Test redirects
    
    * Add PanelAlert

---
+++ tests/js/spec/components/modals/recoveryOptionsModal.spec.jsx
@@ -0,0 +1,61 @@
+import {Modal} from 'react-bootstrap';
+import React from 'react';
+
+import {mount} from 'enzyme';
+import RecoveryOptionsModal from 'app/components/modals/recoveryOptionsModal';
+
+describe('RecoveryOptionsModal', function() {
+  let closeModal = jest.fn();
+  let onClose = jest.fn();
+  let wrapper;
+
+  beforeEach(function() {
+    MockApiClient.clearMockResponses();
+    MockApiClient.addMockResponse({
+      url: '/users/me/authenticators/',
+      method: 'GET',
+      body: Object.values(TestStubs.Authenticators()).map(x => x()),
+    });
+    wrapper = mount(
+      <RecoveryOptionsModal
+        Body={Modal.Body}
+        Header={Modal.Header}
+        authenticatorName="Authenticator App"
+        closeModal={closeModal}
+        onClose={onClose}
+      />,
+      TestStubs.routerContext()
+    );
+  });
+
+  afterEach(function() {});
+
+  it('can redirect to recovery codes if user skips backup phone setup', async function() {
+    let getRecoveryCodes = 'RecoveryOptionsModal Button[name="getCodes"]';
+    expect(wrapper.find(getRecoveryCodes)).toHaveLength(0);
+
+    // skip backup phone setup
+    wrapper.find('RecoveryOptionsModal Button[name="skipStep"]').simulate('click');
+    expect(wrapper.find(getRecoveryCodes)).toHaveLength(1);
+
+    let mockId = 16;
+    expect(
+      wrapper.find('RecoveryOptionsModal Button[name="getCodes"]').prop('to')
+    ).toMatch(`/settings/account/security/${mockId}/`);
+
+    wrapper.find(getRecoveryCodes).simulate('click');
+    expect(closeModal).toHaveBeenCalled();
+  });
+
+  it('can redirect to backup phone setup', async function() {
+    let backupPhone = 'RecoveryOptionsModal Button[name="addPhone"]';
+
+    expect(wrapper.find(backupPhone)).toHaveLength(1);
+    expect(wrapper.find(backupPhone).prop('to')).toMatch(
+      '/settings/account/security/sms/enroll/'
+    );
+
+    wrapper.find(backupPhone).simulate('click');
+    expect(closeModal).toHaveBeenCalled();
+  });
+});
