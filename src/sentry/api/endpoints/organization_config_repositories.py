efcac0a82c364ffe1df09453efaada9910eab0c8
MeredithAnya <meredith.a.heller@gmail.com>
2018-05-23 14:38:44 -0700

    feat(integrations): Add IntegrationRepositoryProvider and Bindings (#8526)
    * feat(integrations): Add IntegrationRepositoryProvider and bindings
    
    * internal-catchall
    
    * fix naming typo
    
    * handle no gh-apps integrations for repos
    
    * use self.model

--- src/sentry/api/endpoints/organization_config_repositories.py
+++ src/sentry/api/endpoints/organization_config_repositories.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from rest_framework.response import Response
 
+from sentry import features
 from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.plugins import bindings
 
@@ -10,6 +11,7 @@ class OrganizationConfigRepositoriesEndpoint(OrganizationEndpoint):
 
     def get(self, request, organization):
         provider_bindings = bindings.get('repository.provider')
+        integrations_provider_bindings = bindings.get('integration-repository.provider')
 
         providers = []
         for provider_id in provider_bindings:
@@ -21,6 +23,17 @@ class OrganizationConfigRepositoriesEndpoint(OrganizationEndpoint):
                         'id': provider_id,
                         'name': provider.name,
                         'config': provider.get_config(),
+                    }
+                )
+
+        if features.has('organizations:internal-catchall', organization, actor=request.user):
+            for provider_id in integrations_provider_bindings:
+                provider = integrations_provider_bindings.get(provider_id)(id=provider_id)
+                providers.append(
+                    {
+                        'id': provider_id,
+                        'name': provider.name,
+                        'config': provider.get_config(organization),
                     }
                 )
 
