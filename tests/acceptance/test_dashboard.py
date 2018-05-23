ae73c6e61ea3a57a69377c9dfe86da6071d49b0f
Lyn Nagara <lyn.nagara@gmail.com>
2018-05-23 10:36:47 -0700

    update acceptance test

--- tests/acceptance/test_dashboard.py
+++ tests/acceptance/test_dashboard.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import
 from django.utils import timezone
 
 from sentry.testutils import AcceptanceTestCase
-from sentry.models import GroupAssignee
+from sentry.models import GroupAssignee, Release, Environment, Deploy, ReleaseProjectEnvironment
 from sentry.utils.samples import create_sample_event
 from datetime import datetime
 
@@ -28,6 +28,31 @@ class DashboardTest(AcceptanceTestCase):
             role='owner',
             teams=[self.team],
         )
+
+        release = Release.objects.create(
+            organization_id=self.org.id,
+            version='1',
+        )
+
+        environment = Environment.objects.create(
+            organization_id=self.org.id,
+            name='production',
+        )
+
+        deploy = Deploy.objects.create(
+            environment_id=environment.id,
+            organization_id=self.org.id,
+            release=release,
+            date_finished='2018-05-23'
+        )
+
+        ReleaseProjectEnvironment.objects.create(
+            project_id=self.project.id,
+            release_id=release.id,
+            environment_id=environment.id,
+            last_deploy_id=deploy.id
+        )
+
         self.login_as(self.user)
         self.path = '/{}/'.format(self.org.slug)
 
