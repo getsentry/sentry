ff7abbf0f670ffed982b17810169a5ec956cc0da
maxbittker <maxbittker@gmail.com>
2018-05-25 14:31:29 -0700

    refactor webhooks and adapt pull request webhook handler

--- src/sentry/integrations/github/webhook.py
+++ src/sentry/integrations/github/webhook.py
@@ -8,7 +8,7 @@ import logging
 import six
 
 from django.db import IntegrityError, transaction
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse
 from django.utils.crypto import constant_time_compare
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
@@ -17,7 +17,7 @@ from django.utils import timezone
 from simplejson import JSONDecodeError
 from sentry import options
 from sentry.models import (
-    Commit, CommitAuthor, CommitFileChange, Integration,
+    Commit, CommitAuthor, CommitFileChange, Integration, PullRequest,
     Repository, User
 )
 from sentry.utils import json
@@ -38,42 +38,61 @@ def get_external_id(username):
 
 
 class Webhook(object):
-    def __call__(self, event, organization=None):
+    def _handle(self, event, organization, repo):
         raise NotImplementedError
+
+    def __call__(self, event, organization=None):
+        integration = Integration.objects.get(
+            external_id=event['installation']['id'],
+            provider='github',
+        )
+        # TODO(maxbittker) select org directly to make this pattern scale to many
+        # orgs on one integration
+        organizations = list(integration.organizations.all())
+
+        for org in organizations:
+
+            try:
+                repo = Repository.objects.get(
+                    organization_id=org.id,
+                    provider='integrations:github',
+                    external_id=six.text_type(event['repository']['id']),
+                )
+            except Repository.DoesNotExist:
+                continue
+
+            # We need to track GitHub's "full_name" which is the repository slug.
+            # This is needed to access the API since `external_id` isn't sufficient.
+            if repo.config.get('name') != event['repository']['full_name']:
+                repo.config['name'] = event['repository']['full_name']
+                repo.save()
+
+            self._handle(event, org, repo)
 
 
 class InstallationEventWebhook(Webhook):
     # https://developer.github.com/v3/activity/events/types/#installationevent
-    def __call__(self, event, organization=None):
+    def _handle(self, event, organization, repo):
+        # TODO(maxbittker) these might need different behavior for missing
+        # repository models in __call__
         pass
 
 
 class InstallationRepositoryEventWebhook(Webhook):
     # https://developer.github.com/v3/activity/events/types/#installationrepositoriesevent
-    def __call__(self, event, organization=None):
+    def _handle(self, event, organization, repo):
+        # TODO(maxbittker) these might need different behavior for missing
+        # repository models in __call__
         pass
 
 
 class PushEventWebhook(Webhook):
-    def _handle(self, event, organization, is_apps):
+    # https://developer.github.com/v3/activity/events/types/#pushevent
+
+    def _handle(self, event, organization, repo):
         authors = {}
         client = GitHubAppsClient(options.get('github-app.id'))
         gh_username_cache = {}
-
-        try:
-            repo = Repository.objects.get(
-                organization_id=organization.id,
-                provider='integrations:github',
-                external_id=six.text_type(event['repository']['id']),
-            )
-        except Repository.DoesNotExist:
-            raise Http404()
-
-        # We need to track GitHub's "full_name" which is the repository slug.
-        # This is needed to access the API since `external_id` isn't sufficient.
-        if repo.config.get('name') != event['repository']['full_name']:
-            repo.config['name'] = event['repository']['full_name']
-            repo.save()
 
         for commit in event['commits']:
             if not commit['distinct']:
@@ -211,29 +230,78 @@ class PushEventWebhook(Webhook):
             except IntegrityError:
                 pass
 
-    # https://developer.github.com/v3/activity/events/types/#pushevent
-    def __call__(self, event, organization=None):
-        is_apps = 'installation' in event
-        if organization is None:
-            if 'installation' not in event:
-                return
-            integration = Integration.objects.get(
-                external_id=event['installation']['id'],
-                provider='github',
-            )
-            organizations = list(integration.organizations.all())
-        else:
-            organizations = [organization]
 
-        for org in organizations:
-            self._handle(event, org, is_apps)
+class PullRequestEventWebhook(Webhook):
+    # https://developer.github.com/v3/activity/events/types/#pullrequestevent
+
+    def _handle(self, event, organization, repo):
+        pull_request = event['pull_request']
+        number = pull_request['number']
+        title = pull_request['title']
+        body = pull_request['body']
+        user = pull_request['user']
+        # The value of the merge_commit_sha attribute changes depending on the state of the pull request. Before a pull request is merged, the merge_commit_sha attribute holds the SHA of the test merge commit. After a pull request is merged, the attribute changes depending on how the pull request was merged:
+        # - If the pull request was merged as a merge commit, the attribute represents the SHA of the merge commit.
+        # - If the pull request was merged via a squash, the attribute represents the SHA of the squashed commit on the base branch.
+        # - If the pull request was rebased, the attribute represents the commit that the base branch was updated to.
+        # https://developer.github.com/v3/pulls/#get-a-single-pull-request
+        merge_commit_sha = pull_request['merge_commit_sha'] if pull_request['merged'] else None
+
+        author_email = u'{}@localhost'.format(user['login'][:65])
+        try:
+            commit_author = CommitAuthor.objects.get(
+                external_id=get_external_id(user['login']),
+                organization_id=organization.id,
+            )
+            author_email = commit_author.email
+        except CommitAuthor.DoesNotExist:
+            try:
+                user_model = User.objects.filter(
+                    social_auth__provider='github',
+                    social_auth__uid=user['id'],
+                    org_memberships=organization,
+                )[0]
+            except IndexError:
+                pass
+            else:
+                author_email = user_model.email
+
+        try:
+            author = CommitAuthor.objects.get(
+                organization_id=organization.id,
+                external_id=get_external_id(user['login']),
+            )
+        except CommitAuthor.DoesNotExist:
+            try:
+                author = CommitAuthor.objects.get(
+                    organization_id=organization.id,
+                    email=author_email,
+                )
+            except CommitAuthor.DoesNotExist:
+                author = CommitAuthor.objects.create(
+                    organization_id=organization.id,
+                    email=author_email,
+                    external_id=get_external_id(user['login']),
+                    name=user['login'][:128]
+                )
+
+        try:
+            PullRequest.objects.create_or_update(
+                repository_id=repo.id,
+                key=number,
+                values={
+                    'organization_id': organization.id,
+                    'title': title,
+                    'author': author,
+                    'message': body,
+                    'merge_commit_sha': merge_commit_sha,
+                },
+            )
+        except IntegrityError:
+            pass
 
 
 class GithubWebhookBase(View):
-    _handlers = {
-        'push': PushEventWebhook,
-    }
-
     # https://developer.github.com/webhooks/
     def get_handler(self, event_type):
         return self._handlers.get(event_type)
@@ -326,6 +394,7 @@ class GithubWebhookBase(View):
 class GithubIntegrationsWebhookEndpoint(GithubWebhookBase):
     _handlers = {
         'push': PushEventWebhook,
+        'pull_request': PullRequestEventWebhook,
         'installation': InstallationEventWebhook,
         'installation_repositories': InstallationRepositoryEventWebhook,
     }
