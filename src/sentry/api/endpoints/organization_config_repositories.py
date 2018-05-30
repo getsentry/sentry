920f4e7f7a8b98dbb9c8051710a82c8f9ac5a104
Meredith <meredith@getsentry.com>
2018-05-30 14:04:38 -0700

    feat(integrations): Add feature flag for GitHub Apps

--- src/sentry/api/endpoints/organization_config_repositories.py
+++ src/sentry/api/endpoints/organization_config_repositories.py
@@ -13,11 +13,16 @@ class OrganizationConfigRepositoriesEndpoint(OrganizationEndpoint):
         provider_bindings = bindings.get('repository.provider')
         integrations_provider_bindings = bindings.get('integration-repository.provider')
 
+        has_github_apps = features.has('organizations:github-apps',
+                                       organization,
+                                       actor=request.user)
+        early_adopters = ['integrations:github'] if has_github_apps else []
+
         providers = []
         for provider_id in provider_bindings:
             provider = provider_bindings.get(provider_id)(id=provider_id)
             # TODO(jess): figure out better way to exclude this
-            if provider_id != 'github_apps':
+            if 'integrations:%s' % provider_id not in early_adopters and provider_id != 'github_apps':
                 providers.append(
                     {
                         'id': provider_id,
@@ -26,9 +31,9 @@ class OrganizationConfigRepositoriesEndpoint(OrganizationEndpoint):
                     }
                 )
 
-        if features.has('organizations:internal-catchall', organization, actor=request.user):
-            for provider_id in integrations_provider_bindings:
-                provider = integrations_provider_bindings.get(provider_id)(id=provider_id)
+        for provider_id in integrations_provider_bindings:
+            provider = integrations_provider_bindings.get(provider_id)(id=provider_id)
+            if provider_id in early_adopters:
                 providers.append(
                     {
                         'id': provider_id,
