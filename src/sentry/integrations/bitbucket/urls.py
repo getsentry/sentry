371d59e55feb2ba52db371abc31af0019067d3d3
Lauryn Brown <lauryndbrown@gmail.com>
2018-05-29 17:25:59 -0700

    feat(integrations): Copy and pasted webhook files. (#8544)
    * Copy and pasted webhook files.
    
    * Got tests working.
    
    * Made sure the commits were created when a push event occured.
    
    * Fixed it so that it uses integrationrepoprovider.

--- src/sentry/integrations/bitbucket/urls.py
+++ src/sentry/integrations/bitbucket/urls.py
@@ -4,9 +4,11 @@ from django.conf.urls import patterns, url
 
 from .descriptor import BitbucketDescriptorEndpoint
 from .installed import BitbucketInstalledEndpoint
-
+from .webhook import BitbucketWebhookEndpoint
 urlpatterns = patterns(
     '',
     url(r'^descriptor/$', BitbucketDescriptorEndpoint.as_view()),
     url(r'^installed/$', BitbucketInstalledEndpoint.as_view()),
+    url(r'^organizations/(?P<organization_id>[^\/]+)/webhook/$',
+        BitbucketWebhookEndpoint.as_view()),
 )
