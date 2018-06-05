85925cda2692b7826fe04f5f120006fae7eb30f9
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-06-05 16:33:20 -0700

    fix time formatting

--- src/sentry/integrations/github/client.py
+++ src/sentry/integrations/github/client.py
@@ -93,6 +93,9 @@ class GitHubClientMixin(ApiClient):
         token = self.integration.metadata['access_token']
         expires_at = self.integration.metadata['expires_at']
 
+        if expires_at is not None:
+            expires_at = datetime.strptime(expires_at, '%Y-%m-%dT%H:%M:%S')
+
         if not token or expires_at < datetime.utcnow():
             res = self.create_token()
             token = res['token']
@@ -103,7 +106,7 @@ class GitHubClientMixin(ApiClient):
 
             self.integration.metadata.update({
                 'access_token': token,
-                'expires_at': expires_at,
+                'expires_at': expires_at.isoformat(),
             })
             self.integration.save()
 
