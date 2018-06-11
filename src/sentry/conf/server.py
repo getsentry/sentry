53730c5ba88b659870d0a05a462869b83d61f8af
0x6a6f7368 <joshua.r.li.98@gmail.com>
2018-06-11 11:38:39 -0700

    feat(filestore): Add GCS backend (#8708)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -875,6 +875,7 @@ SENTRY_EMAIL_BACKEND_ALIASES = {
 SENTRY_FILESTORE_ALIASES = {
     'filesystem': 'django.core.files.storage.FileSystemStorage',
     's3': 'sentry.filestore.s3.S3Boto3Storage',
+    'gcs': 'sentry.filestore.gcs.GoogleCloudStorage',
 }
 
 SENTRY_ANALYTICS_ALIASES = {
