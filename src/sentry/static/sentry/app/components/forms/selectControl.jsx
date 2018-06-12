6768454021a0d9cbbd1ce028bb5ad529a7b40b9d
Billy Vong <billy@sentry.io>
2018-06-12 16:29:43 -0700

    move to utils

--- src/sentry/static/sentry/app/components/forms/selectControl.jsx
+++ src/sentry/static/sentry/app/components/forms/selectControl.jsx
@@ -1,20 +1,9 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 import ReactSelect, {Async} from 'react-select';
 import styled from 'react-emotion';
 
-// Converts arg from a `select2` choices array to a `react-select` `options` array
-const convertFromSelect2Choices = choices => {
-  if (!Array.isArray(choices)) return null;
-
-  // Accepts an array of strings or an array of tuples
-  return choices.map(
-    choice =>
-      Array.isArray(choice)
-        ? {value: choice[0], label: choice[1]}
-        : {value: choice, label: choice}
-  );
-};
+import convertFromSelect2Choices from 'app/utils/convertFromSelect2Choices';
 
 export default class SelectControl extends React.Component {
   static propTypes = {
