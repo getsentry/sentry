ea4098d57463abf52c69c5225f9581071cd03f5b
MeredithAnya <meredith.a.heller@gmail.com>
2018-06-12 17:27:51 -0700

    feat(integrations): Repo panel for add/delete repos (#8677)
    * feat(integrations): Repo panel for add/delete repos
    
    * cleanup unneeded things
    
    * only render integration's repos
    
    * add integration repo endpoint
    
    * add tests
    
    * remove unused import
    
    * doesn't need Fragment
    
    * add RepositoryMixin
    
    * clean up/edit tests
    
    * fix linting error

---
+++ src/sentry/api/endpoints/organization_integration_repos.py
@@ -0,0 +1,28 @@
+from __future__ import absolute_import
+
+from django.http import Http404
+
+from sentry.api.bases.organization import (
+    OrganizationEndpoint, OrganizationIntegrationsPermission
+)
+from sentry.integrations.repositories import RepositoryMixin
+from sentry.models import Integration
+
+
+class OrganizationIntegrationReposEndpoint(OrganizationEndpoint):
+    permission_classes = (OrganizationIntegrationsPermission, )
+
+    def get(self, request, organization, integration_id):
+
+        try:
+            integration = Integration.objects.get(id=integration_id, organizations=organization)
+        except Integration.DoesNotExist:
+            raise Http404
+
+        install = integration.get_installation()
+        if isinstance(install, RepositoryMixin):
+            repositories = install.get_repositories()
+            context = {'repos': repositories}
+            return self.respond(context)
+
+        return self.respond({'detail': 'Repositories not supported'}, status=400)
