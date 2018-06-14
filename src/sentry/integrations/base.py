3372546c859b1819e15cf0a4f8ccd07ac7ed404a
Lauryn Brown <lauryndbrown@gmail.com>
2018-06-14 13:49:19 -0700

    feat(integration): Added OrganizationIntegraiton and ProjectIntegration to Integration base (#8737)
    * Added project_integration to base
    
    * Added the org integration as well. I'll have a merge conflict anyway..
    
    * Added tests to base.py

--- src/sentry/integrations/base.py
+++ src/sentry/integrations/base.py
@@ -16,7 +16,7 @@ from .exceptions import (
     ApiHostError, ApiError, ApiUnauthorized, IntegrationError, UnsupportedResponseType
 )
 from .constants import ERR_UNAUTHORIZED, ERR_INTERNAL, ERR_UNSUPPORTED_RESPONSE_TYPE
-from sentry.models import Identity, OrganizationIntegration
+from sentry.models import Identity, OrganizationIntegration, ProjectIntegration
 
 IntegrationMetadata = namedtuple('IntegrationMetadata', [
     'description',  # A markdown description of the integration
@@ -81,14 +81,14 @@ class IntegrationProvider(PipelineProvider):
     features = frozenset()
 
     @classmethod
-    def get_installation(cls, model, organization_id=None, **kwargs):
+    def get_installation(cls, model, organization_id=None, project_id=None, **kwargs):
         if cls.integration_cls is None:
             raise NotImplementedError
 
         if cls.needs_default_identity is True and organization_id is None:
             raise NotImplementedError('%s requires an organization_id' % cls.name)
 
-        return cls.integration_cls(model, organization_id, **kwargs)
+        return cls.integration_cls(model, organization_id, project_id, **kwargs)
 
     def get_logger(self):
         return logging.getLogger('sentry.integration.%s' % (self.key, ))
@@ -158,9 +158,23 @@ class Integration(object):
 
     logger = logging.getLogger('sentry.integrations')
 
-    def __init__(self, model, organization_id=None):
+    def __init__(self, model, organization_id=None, project_id=None):
         self.model = model
-        self.organization_id = organization_id
+        if organization_id is not None:
+            self.org_integration = OrganizationIntegration.objects.get(
+                organization_id=organization_id,
+                integration_id=model.id,
+            )
+        else:
+            self.org_integration = None
+
+        if project_id is not None:
+            self.project_integration = ProjectIntegration.objects.get(
+                project_id=project_id,
+                integration_id=model.id,
+            )
+        else:
+            self.project_integration = None
 
     def get_organization_config(self):
         """
@@ -187,15 +201,10 @@ class Integration(object):
         """
         For Integrations that rely solely on user auth for authentication
         """
-        if self.organization_id is None:
-            raise NotImplementedError
+        if self.org_integration is None:
+            raise NotImplementedError('%s requires an organization_id' % self.name)
 
-        org_integration = OrganizationIntegration.objects.get(
-            organization_id=self.organization_id,
-            integration_id=self.model.id,
-        )
-        identity = Identity.objects.get(id=org_integration.default_auth_id)
-
+        identity = Identity.objects.get(id=self.org_integration.default_auth_id)
         return identity
 
     def error_message_from_json(self, data):
