782f16bc13b563c266a91786b074f54d917712f1
Kelly Carino <Kelly Carino>
2018-06-13 18:01:52 -0700

    add another check

--- src/sentry/static/sentry/app/components/events/eventEntries.jsx
+++ src/sentry/static/sentry/app/components/events/eventEntries.jsx
@@ -81,7 +81,7 @@ const EventEntries = createReactClass({
   componentDidMount() {
     let {event} = this.props;
 
-    if (!event.errors.length > 0) return;
+    if (!event.errors || !event.errors.length > 0) return;
     let errors = event.errors;
     let errorTypes = errors.map((errorEntries) => errorEntries.type);
 
