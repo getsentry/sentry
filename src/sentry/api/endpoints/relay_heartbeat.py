420fda14a6f7f902901e7f8250c02bb5f899a00e
Jan Michael Auer <account@jauer.org>
2018-06-15 12:55:40 +0200

    fix(relay): Update last_seen on every heartbeat (#8742)
    * fix(relay): Update last_seen on every heartbeat
    
    * feat(relay): Debounce relay last_seen updates

--- src/sentry/api/endpoints/relay_heartbeat.py
+++ src/sentry/api/endpoints/relay_heartbeat.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import
 
 from rest_framework.response import Response
 
+from datetime import timedelta
+from django.utils import timezone
+
 from sentry.api.base import Endpoint
 from sentry.relay import change_set, query
 from sentry.api.permissions import RelayPermission
@@ -13,6 +16,10 @@ class RelayHeartbeatEndpoint(Endpoint):
     permission_classes = (RelayPermission, )
 
     def post(self, request):
+        now = timezone.now()
+        if now >= request.relay.last_seen + timedelta(minutes=1):
+            request.relay.update(last_seen=now)
+
         changesets = request.relay_request_data.get('changesets')
         if changesets:
             change_set.execute_changesets(request.relay, changesets)
