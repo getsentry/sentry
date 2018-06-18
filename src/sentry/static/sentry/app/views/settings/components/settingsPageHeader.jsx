270af0ac9a7b7d18f0f173ce8321c2187d9e515e
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-06-18 14:14:28 -0700

    feat(ui): Add titleNode prop for SettingsPageHeading

--- src/sentry/static/sentry/app/views/settings/components/settingsPageHeader.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsPageHeader.jsx
@@ -4,20 +4,33 @@ import styled from 'react-emotion';
 import {Flex, Box} from 'grid-emotion';
 import space from 'app/styles/space';
 
+const Heading = styled(Flex)`
+  margin: ${space(3)} 0;
+  flex: 1;
+`;
+
+const Title = styled(Heading)`
+  font-size: 20px;
+  font-weight: bold;
+`;
+
 class SettingsPageHeading extends React.Component {
   static propTypes = {
     icon: PropTypes.node,
     title: PropTypes.node,
     action: PropTypes.node,
     tabs: PropTypes.node,
+    noStyles: PropTypes.bool,
   };
 
   render() {
+    const StyledTitle = this.props.noStyles ? Heading : Title;
+
     return (
       <Wrapper tabs={this.props.tabs}>
         <Flex align="center">
           {this.props.icon && <Box pr={1}>{this.props.icon}</Box>}
-          {this.props.title && <Title>{this.props.title}</Title>}
+          {this.props.title && <StyledTitle>{this.props.title}</StyledTitle>}
           {this.props.action && <div>{this.props.action}</div>}
         </Flex>
 
@@ -31,13 +44,6 @@ const Wrapper = styled.div`
   font-size: 14px;
   box-shadow: inset 0 -1px 0 ${p => p.theme.borderLight};
   margin: -${space(3)} 0 ${space(3)};
-`;
-
-const Title = styled.div`
-  font-size: 20px;
-  font-weight: bold;
-  margin: ${space(3)} 0;
-  flex: 1;
 `;
 
 export default SettingsPageHeading;
