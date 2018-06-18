6b845800a4cf22d982b5aac0756a2f099e9deb27
Lyn Nagara <lyn.nagara@gmail.com>
2018-06-18 13:06:00 -0700

    fixes

--- src/sentry/api/endpoints/organization_discover.py
+++ src/sentry/api/endpoints/organization_discover.py
@@ -8,14 +8,12 @@ from sentry.models import Project, ProjectStatus
 
 from sentry.utils import snuba
 
-from django.core.exceptions import ValidationError
-
 
 class OrganizationDiscoverEndpoint(OrganizationEndpoint):
     permission_classes = (OrganizationDiscoverPermission, )
 
-    def do_query(self, start, end, selected_columns=[], conditions=[], having=[], filters={},
-                 aggregations=[], orderby='-last_seen', limit=None, groupby=[], rollup=None):
+    def do_query(self, start, end, selected_columns=None, conditions=None, having=None, filters=None,
+                 aggregations=None, orderby=None, limit=None, groupby=None, rollup=None):
 
         snuba_results = snuba.raw_query(
             start=start,
@@ -41,30 +39,30 @@ class OrganizationDiscoverEndpoint(OrganizationEndpoint):
             'project_id': data['projects']
         } if 'projects' in data else None
 
-        selected_columns = data['fields'] if 'fields' in data else None
+        selected_columns = data.get('fields')
 
-        orderby = data['orderby'] if 'orderby' in data else None
+        orderby = data.get('orderby', '-last_seen')
 
-        conditions = data['conditions'] if 'conditions' in data else None
+        conditions = data.get('conditions')
 
-        limit = data['limit'] if 'limit' in data else 1000
+        limit = data.get('limit', 1000)
 
-        aggregations = data['aggregations'] if 'aggregations' in data else None
+        aggregations = data.get('aggregations')
 
-        groupby = data['groupby'] if 'groupby' in data else None
+        groupby = data.get('groupby')
 
-        rollup = data['rollup'] if 'rollup' in data else None
+        rollup = data.get('rollup')
 
-        org_projects = [org.id for org in list(Project.objects.filter(
+        org_projects = [project.id for project in list(Project.objects.filter(
             organization=organization,
             status=ProjectStatus.VISIBLE,
         ))]
 
         if (not isinstance(limit, int) or limit < 0 or limit > 1000):
-            raise ValidationError('Invalid limit parameter')
+            return Response({'detail': 'Invalid limit parameter'}, status=400)
 
         if any(project_id not in org_projects for project_id in data['projects']):
-            raise ValidationError('Invalid projects')
+            return Response({'detail': 'Invalid projects'}, status=400)
 
         fmt = '%Y-%m-%dT%H:%M:%S'
         start = datetime.strptime(data['start'], fmt)
