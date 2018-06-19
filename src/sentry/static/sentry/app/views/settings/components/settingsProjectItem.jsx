29441d6bef36531d057aaedc58628cbe20ddcc96
Billy Vong <billy@sentry.io>
2018-06-18 18:27:20 -0700

    fix(settings): Better settings permissions
    * Links to Project Settings are only enabled for `project:write` perms
    * Links to Team details (w/ Members/Projects/Settings) enabled for *everyone*
      * Team Settings enabled only for `team:write`
    * Members Details enabled only for `org:write`

--- src/sentry/static/sentry/app/views/settings/components/settingsProjectItem.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsProjectItem.jsx
@@ -1,8 +1,6 @@
 import styled from 'react-emotion';
 import React from 'react';
-
 import createReactClass from 'create-react-class';
-import theme from 'app/utils/theme';
 
 import {update} from 'app/actionCreators/projects';
 import ApiMixin from 'app/mixins/apiMixin';
@@ -17,10 +15,6 @@ const InlineButton = styled('button')`
   border: none;
   background-color: inherit;
   padding: 0;
-`;
-
-const StyledProjectLabel = styled(ProjectLabel)`
-  color: ${p => p.theme.blue};
 `;
 
 const ProjectItem = createReactClass({
@@ -68,6 +62,7 @@ const ProjectItem = createReactClass({
     let {project, organization} = this.props;
     let org = organization;
     let {isBookmarked} = this.state;
+    let LinkOrSpan = organization.access.includes('project:write') ? Link : 'span';
 
     return (
       <div key={project.id} className={isBookmarked ? 'isBookmarked' : null}>
@@ -80,9 +75,9 @@ const ProjectItem = createReactClass({
             )}
           </InlineButton>
         </Tooltip>
-        <Link to={`/settings/${org.slug}/${project.slug}/`} css={{color: theme.gray3}}>
-          <StyledProjectLabel project={project} organization={this.props.organization} />
-        </Link>
+        <LinkOrSpan to={`/settings/${org.slug}/${project.slug}/`}>
+          <ProjectLabel project={project} organization={this.props.organization} />
+        </LinkOrSpan>
       </div>
     );
   },
