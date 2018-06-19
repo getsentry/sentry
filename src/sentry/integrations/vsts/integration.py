75f6e5edccc80af971eb84934799f241ff85c9a0
Lauryn Brown <lauryndbrown@gmail.com>
2018-06-19 12:24:05 -0700

    ref(integrations): Moved Vsts default_project to the config form (#8698)
    * Added get_organization_config.
    
    * Added default project to vsts org config settings and fixed org config as well
    
    * Reverted changes to org config ui.
    
    * Moved project config to config interface and removed the django form during config.
    
    * changed tests to work.
    
    * Added ugettext instead of ugettext_lazy. Populated the default_project with the id.
    
    * Added try-catch block to project client thing. need to know what to do about it.
    
    * Adding tests found an error I'm going to follow up on.
    
    * Added tests
    
    * Added try-catch to default_project.

--- src/sentry/integrations/vsts/integration.py
+++ src/sentry/integrations/vsts/integration.py
@@ -1,13 +1,11 @@
 from __future__ import absolute_import
-from sentry import http
 from time import time
-from django import forms
 
-from django.utils.translation import ugettext_lazy as _
-from sentry.web.helpers import render_to_response
+from django.utils.translation import ugettext as _
 from sentry.integrations import Integration, IntegrationProvider, IntegrationMetadata
+from sentry.integrations.exceptions import ApiError
 from .client import VstsApiClient
-from sentry.pipeline import NestedPipelineView, PipelineView
+from sentry.pipeline import NestedPipelineView
 from sentry.identity.pipeline import IdentityProviderPipeline
 from sentry.identity.vsts import VSTSIdentityProvider
 from sentry.utils.http import absolute_uri
@@ -27,48 +25,6 @@ metadata = IntegrationMetadata(
 )
 
 
-class ProjectConfigView(PipelineView):
-    def dispatch(self, request, pipeline):
-        if 'project' in request.POST:
-            project_id = request.POST.get('project')
-            projects = pipeline.fetch_state(key='projects')
-            project = self.get_project_from_id(project_id, projects)
-            if project is not None:
-                pipeline.bind_state('project', project)
-                return pipeline.next_step()
-
-        identity_data = pipeline.fetch_state(key='identity')
-        instance = identity_data['instance']
-        access_token = identity_data['data']['access_token']
-        projects = get_projects(instance, access_token)['value']
-        pipeline.bind_state('projects', projects)
-        project_form = ProjectForm(projects)
-
-        return render_to_response(
-            template='sentry/integrations/vsts-config.html',
-            context={
-                'form': project_form,
-            },
-            request=request,
-        )
-
-    def get_project_from_id(self, project_id, projects):
-        for project in projects:
-            if project['id'] == project_id:
-                return project
-        return None
-
-
-class ProjectForm(forms.Form):
-    def __init__(self, projects, *args, **kwargs):
-        super(ProjectForm, self).__init__(*args, **kwargs)
-        self.fields['project'] = forms.ChoiceField(
-            choices=[(project['id'], project['name']) for project in projects],
-            label='Project',
-            help_text='Enter the Visual Studio Team Services project name that you wish to use as a default for new work items'
-        )
-
-
 class VstsIntegration(Integration):
     def __init__(self, *args, **kwargs):
         super(VstsIntegration, self).__init__(*args, **kwargs)
@@ -83,6 +39,50 @@ class VstsIntegration(Integration):
             raise ValueError('Identity missing access token')
         return VstsApiClient(access_token)
 
+    def get_project_config(self):
+        client = self.get_client()
+        disabled = False
+        try:
+            projects = client.get_projects(self.model.metadata['domain_name'])
+        except ApiError:
+            # TODO(LB): Disable for now. Need to decide what to do with this in the future
+            # should a message be shown to the user?
+            # Should we try refreshing the token? For VSTS that often clears up the problem
+            project_choices = []
+            disabled = True
+        else:
+            project_choices = [(project['id'], project['name']) for project in projects['value']]
+
+        try:
+            # TODO(LB): Will not work in the UI until the serliazer sends a `project_id` to get_installation()
+            # serializers and UI are being refactored and it's not worth trying to fix
+            # the old system. Revisit
+            default_project = self.project_integration.config.get('default_project')
+        except Exception:
+            default_project = None
+
+        initial_project = ('', '')
+        if default_project is not None:
+            for project_id, project_name in project_choices:
+                if default_project == project_id:
+                    initial_project = (project_id, project_name)
+                    break
+
+        return [
+            {
+                'name': 'default_project',
+                'type': 'choice',
+                'allowEmpty': True,
+                'disabled': disabled,
+                'required': True,
+                'choices': project_choices,
+                'initial': initial_project,
+                'label': _('Default Project Name'),
+                'placeholder': _('MyProject'),
+                'help': _('Enter the Visual Studio Team Services project name that you wish to use as a default for new work items'),
+            },
+        ]
+
 
 class VstsIntegrationProvider(IntegrationProvider):
     key = 'vsts'
@@ -92,6 +92,7 @@ class VstsIntegrationProvider(IntegrationProvider):
     api_version = '4.1'
     needs_default_identity = True
     integration_cls = VstsIntegration
+    can_add_project = True
 
     setup_dialog_config = {
         'width': 600,
@@ -112,30 +113,28 @@ class VstsIntegrationProvider(IntegrationProvider):
 
         return [
             identity_pipeline_view,
-            ProjectConfigView(),
         ]
 
     def build_integration(self, state):
         data = state['identity']['data']
         account = state['identity']['account']
         instance = state['identity']['instance']
-        project = state['project']
 
         scopes = sorted(VSTSIdentityProvider.oauth_scopes)
         return {
-            'name': project['name'],
-            'external_id': project['id'],
+            'name': account['AccountName'],
+            'external_id': account['AccountId'],
             'metadata': {
                 'domain_name': instance,
                 'scopes': scopes,
-                # icon doesn't appear to be possible
             },
+            # TODO(LB): Change this to a Microsoft account as opposed to a VSTS workspace
             'user_identity': {
                 'type': 'vsts',
                 'external_id': account['AccountId'],
                 'scopes': [],
                 'data': self.get_oauth_data(data),
-            }
+            },
         }
 
     def get_oauth_data(self, payload):
@@ -157,17 +156,3 @@ class VstsIntegrationProvider(IntegrationProvider):
             VstsRepositoryProvider,
             id='integrations:vsts',
         )
-
-
-def get_projects(instance, access_token):
-    session = http.build_session()
-    url = 'https://%s/DefaultCollection/_apis/projects' % instance
-    response = session.get(
-        url,
-        headers={
-            'Content-Type': 'application/json',
-            'Authorization': 'Bearer %s' % access_token,
-        }
-    )
-    response.raise_for_status()
-    return response.json()
