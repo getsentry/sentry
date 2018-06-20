0ad0bf838665178b2377c150f431a974c2df6769
Ayesha Omarali <ayesha.omarali@gmail.com>
2018-06-20 13:33:25 -0700

    feat(ui): Project Chooser Dropdown Platformicons (#8618)
    Platformicons display max 3 platforms used per project in dropdown/project chooser.
    Fixes APP-280 (https://getsentry.atlassian.net/browse/APP-280)

---
+++ src/sentry/static/sentry/app/components/platformList.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'react-emotion';
+
+import Platformicon from 'app/components/platformicon';
+import space from '../styles/space';
+
+const MAX_PLATFORMS = 3;
+const ICON_SIZE = 16;
+
+class PlatformList extends React.Component {
+  static propTypes = {
+    platforms: PropTypes.array,
+  };
+
+  static defaultProps = {
+    platforms: [],
+  };
+
+  getIcon(platform) {
+    return (
+      <StyledPlatformIcon key={platform} platform={platform} size={ICON_SIZE + 'px'} />
+    );
+  }
+
+  getIcons(platforms) {
+    return platforms
+      .slice()
+      .reverse()
+      .map(this.getIcon);
+  }
+
+  render() {
+    const {platforms} = this.props;
+    const platformsPreview = platforms.slice(0, MAX_PLATFORMS);
+    return (
+      <PlatformIcons>
+        {platforms.length > 0 ? (
+          this.getIcons(platformsPreview)
+        ) : (
+          <StyledPlatformIcon size={ICON_SIZE + 'px'} platform="default" />
+        )}
+      </PlatformIcons>
+    );
+  }
+}
+
+const PlatformIcons = styled('div')`
+  margin-right: ${space(0.5)};
+  display: flex;
+  flex-direction: row;
+  justify-content: flex-end;
+  width: 24px;
+`;
+
+const StyledPlatformIcon = styled(Platformicon)`
+  border-radius: 3px;
+  box-shadow: 0 0 0 1px #fff;
+  &:not(:first-child) {
+    margin-left: ${ICON_SIZE * -1 + 4}px;
+  }
+`;
+
+export default PlatformList;
