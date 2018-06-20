6fea75a65431769e48ce1652c18ab521bc67a28f
Billy Vong <billyvg@users.noreply.github.com>
2018-06-20 09:48:50 -0700

    feat(ui): Change <Select2TextField> to use react-select (#8731)

--- tests/js/spec/components/forms/genericField.spec.jsx
+++ tests/js/spec/components/forms/genericField.spec.jsx
@@ -4,36 +4,34 @@ import {shallow} from 'enzyme';
 import {GenericField, FormState} from 'app/components/forms';
 
 describe('GenericField', function() {
-  describe('render()', function() {
-    it('renders text as TextInput', function() {
-      let wrapper = shallow(
-        <GenericField
-          formState={FormState.READY}
-          config={{
-            name: 'field-name',
-            label: 'field label',
-            type: 'text',
-          }}
-        />
-      );
-      expect(wrapper).toMatchSnapshot();
-      expect(wrapper.name()).toEqual('TextField');
-    });
+  it('renders text as TextInput', function() {
+    let wrapper = shallow(
+      <GenericField
+        formState={FormState.READY}
+        config={{
+          name: 'field-name',
+          label: 'field label',
+          type: 'text',
+        }}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.name()).toEqual('TextField');
+  });
 
-    it('renders text with choices as Select2TextInput', function() {
-      let wrapper = shallow(
-        <GenericField
-          formState={FormState.READY}
-          config={{
-            name: 'field-name',
-            label: 'field label',
-            type: 'text',
-            choices: [],
-          }}
-        />
-      );
-      expect(wrapper).toMatchSnapshot();
-      expect(wrapper.name()).toEqual('Select2TextField');
-    });
+  it('renders text with choices as SelectCreatableField', function() {
+    let wrapper = shallow(
+      <GenericField
+        formState={FormState.READY}
+        config={{
+          name: 'field-name',
+          label: 'field label',
+          type: 'text',
+          choices: [],
+        }}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+    expect(wrapper.name()).toEqual('SelectCreatableField');
   });
 });
