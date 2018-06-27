447dd5d5391124e39c43b74e8fdf9708d1a91681
Lyn Nagara <lyn.nagara@gmail.com>
2018-06-27 14:56:30 -0700

    feat(discover): Add basic aggregation support (#8832)
    Adds a basic implementation of how aggregations could work in the UI.
    
    Currently it supports topK, uniq and count aggregations
    
    Also changes the behavior of the API endpoint to treat 'fields' as a 'select' if no aggregation is provided, or if there are aggregations for this to mean 'groupby'

--- src/sentry/static/sentry/app/views/organizationDiscover/queryBuilder.jsx
+++ src/sentry/static/sentry/app/views/organizationDiscover/queryBuilder.jsx
@@ -1,5 +1,7 @@
 import moment from 'moment-timezone';
 
+import {COLUMNS} from './data';
+
 const DATE_TIME_FORMAT = 'YYYY-MM-DDTHH:mm:ss';
 
 const DEFAULTS = {
@@ -36,20 +38,61 @@ export default function createQueryBuilder(initial = {}, projectList) {
   }
 
   function getExternal() {
-    if (!query.projects.length) {
-      return {...query, projects: projectList.map(project => parseInt(project.id, 10))};
-    } else {
-      return query;
+    // Default to all projects if none is selected
+    const projects = query.projects.length
+      ? query.projects
+      : projectList.map(project => parseInt(project.id, 10));
+
+    // Default to all fields if there are none selected, and no aggregation or groupby is specified
+    const useDefaultFields =
+      !query.fields.length && !query.aggregations.length && !query.groupby;
+
+    const fields = useDefaultFields ? COLUMNS.map(({name}) => name) : query.fields;
+
+    // Remove orderby property if it is not set
+    if (!query.orderby) {
+      delete query.orderby;
     }
+
+    return {
+      ...query,
+      projects,
+      fields,
+    };
   }
 
   function updateField(field, value) {
     query[field] = value;
+
+    // If an aggregation is added, we need to remove the orderby parameter if it's not in the selected fields
+    if (field === 'aggregations' && value.length > 0) {
+      query.orderby = null;
+      query.limit = null;
+    }
+  }
+
+  function getFieldOptions() {
+    return COLUMNS.map(({name}) => ({
+      value: name,
+      label: name,
+    }));
+  }
+
+  function getOrderByOptions() {
+    return COLUMNS.reduce((acc, {name}) => {
+      return [
+        ...acc,
+        {value: name, label: `${name} asc`},
+        {value: `-${name}`, label: `${name} desc`},
+      ];
+    }, []);
   }
 
   return {
     getInternal,
     getExternal,
     updateField,
+    getFieldOptions,
+    getOrderByOptions,
   };
 }
