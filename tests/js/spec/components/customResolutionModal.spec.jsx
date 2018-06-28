e415fd142755f703028b78401b1aacfe4729920c
Billy Vong <billyvg@users.noreply.github.com>
2018-06-28 15:55:25 -0700

    fix(ui): Fix CustomResolutionModal + add tests (#8882)
    * add tests for asana

--- tests/js/spec/components/customResolutionModal.spec.jsx
+++ tests/js/spec/components/customResolutionModal.spec.jsx
@@ -1,20 +1,56 @@
 import React from 'react';
-import {shallow} from 'enzyme';
+import {mount} from 'enzyme';
 
 import CustomResolutionModal from 'app/components/customResolutionModal';
 
 describe('CustomResolutionModal', function() {
-  describe('render()', function() {
-    it('renders correctly', function() {
-      let wrapper = shallow(
-        <CustomResolutionModal
-          orgId="org"
-          projectId="project"
-          onCanceled={() => false}
-          onSelected={() => false}
-        />
-      );
-      expect(wrapper).toMatchSnapshot();
+  let releasesMock;
+  beforeEach(function() {
+    releasesMock = MockApiClient.addMockResponse({
+      url: '/projects/org-slug/project-slug/releases/',
+      body: [TestStubs.Release()],
+    });
+  });
+
+  it('can select a version', async function() {
+    let onSelected = jest.fn();
+    let wrapper = mount(
+      <CustomResolutionModal
+        orgId="org-slug"
+        projectId="project-slug"
+        onCanceled={() => false}
+        onSelected={onSelected}
+        show
+      />,
+      TestStubs.routerContext()
+    );
+
+    expect(releasesMock).toHaveBeenCalled();
+
+    await tick();
+    wrapper.update();
+
+    expect(wrapper.find('Select').prop('options')).toEqual([
+      expect.objectContaining({
+        value: '92eccef279d966b2319f0802fa4b22b430a5f72b',
+        label: expect.anything(),
+      }),
+    ]);
+
+    wrapper.find('input[id="version"]').simulate('change', {target: {value: '9'}});
+
+    await tick();
+    wrapper.update();
+
+    wrapper.find('input[id="version"]').simulate('keyDown', {keyCode: 13});
+
+    expect(wrapper.find('SelectControl').prop('value')).toEqual({
+      value: '92eccef279d966b2319f0802fa4b22b430a5f72b',
+      label: expect.anything(),
+    });
+    wrapper.find('form').simulate('submit');
+    expect(onSelected).toHaveBeenCalledWith({
+      inRelease: '92eccef279d966b2319f0802fa4b22b430a5f72b',
     });
   });
 });
