f76668d1fcd31f46e0461ab4a9d16bc09939dfd2
Matt Robenolt <matt@ydekproductions.com>
2018-06-29 10:40:41 -0700

    fix: Force an invalid gravatar to 404 and not render blank (#8865)

--- src/sentry/static/sentry/app/components/avatar/gravatar.jsx
+++ src/sentry/static/sentry/app/components/avatar/gravatar.jsx
@@ -50,7 +50,10 @@ class Gravatar extends React.Component {
 
     let query = {
       s: remoteSize || undefined,
-      d: placeholder || 'blank',
+      // If gravatar is not found we need the request to return an error,
+      // otherwise error handler will not trigger and avatar will not have a display a LetterAvatar backup.
+      d: placeholder || '404', 
+     
     };
 
     url += '?' + qs.stringify(query);
