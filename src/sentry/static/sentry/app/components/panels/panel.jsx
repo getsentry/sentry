0e20fa9f4ce95ff6e1b3e593006ccf344ee663b9
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-07-02 15:19:19 -0700

    space

--- src/sentry/static/sentry/app/components/panels/panel.jsx
+++ src/sentry/static/sentry/app/components/panels/panel.jsx
@@ -14,7 +14,6 @@ const Panel = styled(({title, body, ...props}) => {
   ) : (
     <div {...props}>
       <PanelHeader>{title}</PanelHeader>
-
       <PanelBody>{body}</PanelBody>
     </div>
   );
