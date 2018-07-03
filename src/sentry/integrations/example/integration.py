cca8d7499ae0200d64f9df3348c7d22334749f86
Lauryn Brown <lauryndbrown@gmail.com>
2018-07-03 14:27:51 -0700

    Added self to example integration method.:

--- src/sentry/integrations/example/integration.py
+++ src/sentry/integrations/example/integration.py
@@ -65,8 +65,7 @@ class ExampleIntegration(Integration, IssueSyncMixin):
     def sync_assignee_outbound(self, external_issue, user, assign=True, **kwargs):
         pass
 
-    def sync_status_outbound(external_issue, is_resolved, project_id):
-        # TODO(lb): should this have self?
+    def sync_status_outbound(self, external_issue, is_resolved, project_id):
         pass
 
 
