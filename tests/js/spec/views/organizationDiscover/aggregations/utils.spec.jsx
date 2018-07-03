9f8c8808e81eb8b92bfbdc2646aadd0785fb48a7
Lyn Nagara <lyn.nagara@gmail.com>
2018-07-02 17:52:19 -0700

    feat(discover): Prevent invalid queries
    Try to prevent invalid queries by removing any invalid conditions and aggregations before fetching query
    
    This will most likely be an empty condition or aggregation as they are also being validated on input

--- tests/js/spec/views/organizationDiscover/aggregations/utils.spec.jsx
+++ tests/js/spec/views/organizationDiscover/aggregations/utils.spec.jsx
@@ -1,7 +1,10 @@
 import {
   getInternal,
   getExternal,
+  isValidAggregation,
 } from 'app/views/organizationDiscover/aggregations/utils';
+
+import {COLUMNS} from 'app/views/organizationDiscover/data';
 
 const aggregationList = [
   {
@@ -34,6 +37,40 @@ describe('Aggregations', function() {
       aggregationList.forEach(({internal, external}) => {
         expect(getInternal(external)).toEqual(internal);
       });
+    });
+  });
+
+  describe('isValidAggregation()', function() {
+    it('validates count', function() {
+      expect(isValidAggregation(['count()', null, 'count'], COLUMNS)).toEqual(true);
+      expect(isValidAggregation(['count', null, 'count'], COLUMNS)).toEqual(false);
+      expect(isValidAggregation(['count()', 'email', 'count'], COLUMNS)).toEqual(false);
+    });
+
+    it('validates topK', function() {
+      expect(isValidAggregation(['topK(5)', 'email', 'topK_5_email'], COLUMNS)).toEqual(
+        true
+      );
+      expect(isValidAggregation(['topK()', 'email', 'topK_5_email'], COLUMNS)).toEqual(
+        false
+      );
+      expect(isValidAggregation(['topK(5)', null, 'topK_5_email'], COLUMNS)).toEqual(
+        false
+      );
+    });
+
+    it('validates uniq', function() {
+      expect(isValidAggregation(['uniq', 'email', 'uniq_email'], COLUMNS)).toEqual(true);
+
+      expect(isValidAggregation(['uniq', 'mail', 'uniq_mail'], COLUMNS)).toEqual(false);
+    });
+
+    it('validates avg', function() {
+      expect(
+        isValidAggregation(['avg', 'device_battery_level', 'avg_email'], COLUMNS)
+      ).toEqual(true);
+
+      expect(isValidAggregation(['avg', 'email', 'avg_email'], COLUMNS)).toEqual(false);
     });
   });
 });
