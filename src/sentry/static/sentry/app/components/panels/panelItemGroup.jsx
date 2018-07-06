e2c9218ea1416981318feaa20fb83eb1ec1ef9a2
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-07-06 13:12:02 -0700

    feat(ui): Add panelItemGroup

---
+++ src/sentry/static/sentry/app/components/panels/panelItemGroup.jsx
@@ -0,0 +1,20 @@
+import styled from 'react-emotion';
+
+import PanelItem from 'app/components/panels/panelItem';
+
+const PanelItemGroup = styled.div`
+  border-left: 4px solid ${p => p.theme.borderLight};
+  border-bottom: 1px solid ${p => p.theme.borderLight};
+  background: ${p => p.theme.offWhite};
+
+  /* stylelint-disable-next-line no-duplicate-selectors */
+  ${PanelItem} {
+    border-bottom-color: ${p => p.theme.borderLighter};
+
+    &:last-child {
+      border-bottom: none;
+    }
+  }
+`;
+
+export default PanelItemGroup;
