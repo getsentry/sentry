fd0060592b22983f4f814cce457e943dd0b5ff7c
0x6a6f7368 <joshua.r.li.98@gmail.com>
2018-07-06 10:34:29 -0700

    files: queue deletes (#8951)
    round 2.

---
+++ src/sentry/tasks/files.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+from sentry.tasks.base import instrumented_task
+from sentry.tasks.deletion import MAX_RETRIES
+
+
+@instrumented_task(
+    name='sentry.tasks.files.delete_file',
+    queue='files.delete',
+    default_retry_delay=60 * 5,
+    max_retries=MAX_RETRIES
+)
+def delete_file(path, checksum, **kwargs):
+    from sentry.models.file import get_storage, FileBlob
+    from sentry.app import locks
+    from sentry.utils.retries import TimedRetryPolicy
+
+    lock = locks.get('fileblob:upload:{}'.format(checksum), duration=60 * 10)
+    with TimedRetryPolicy(60)(lock.acquire):
+        if not FileBlob.objects.filter(checksum=checksum).exists():
+            get_storage().delete(path)
