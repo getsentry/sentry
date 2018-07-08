81a9eb29d1957ecc108a8480520c169fb80881ff
Lauryn Brown <lauryndbrown@gmail.com>
2018-07-08 02:00:09 -0700

    Changed unrelated files to match master

--- src/sentry/tasks/integrations.py
+++ src/sentry/tasks/integrations.py
@@ -49,9 +49,7 @@ def sync_assignee_outbound(external_issue_id, user_id, assign, **kwargs):
         user = None
     else:
         user = User.objects.get(id=user_id)
-    integration.get_installation(
-        organization_id=external_issue.organization_id).sync_assignee_outbound(
-        external_issue, user, assign=assign)
+    integration.get_installation().sync_assignee_outbound(external_issue, user, assign=assign)
 
 
 @instrumented_task(
