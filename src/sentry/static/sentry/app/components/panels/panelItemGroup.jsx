3d8de03be0a0e6a6f68c21875c39454ffce71765
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-07-09 13:43:13 -0700

    inverse style dep

--- src/sentry/static/sentry/app/components/panels/panelItemGroup.jsx
+++ src/sentry/static/sentry/app/components/panels/panelItemGroup.jsx
@@ -1,20 +1,9 @@
 import styled from 'react-emotion';
 
-import PanelItem from './panelItem';
-
 const PanelItemGroup = styled('div')`
   border-left: 4px solid ${p => p.theme.borderLight};
   border-bottom: 1px solid ${p => p.theme.borderLight};
   background: ${p => p.theme.offWhite};
-
-  /* stylelint-disable-next-line no-duplicate-selectors */
-  ${PanelItem} {
-    border-bottom-color: ${p => p.theme.borderLight};
-
-    &:last-child {
-      border-bottom: none;
-    }
-  }
 `;
 
 export default PanelItemGroup;
