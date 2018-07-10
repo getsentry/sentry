21581bdaaa263bd2857f6c5f39f93fd29ef37ac1
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-07-10 14:26:09 -0700

    remove dupe integration provider test fixture

--- tests/js/setup.js
+++ tests/js/setup.js
@@ -556,34 +556,6 @@ window.TestStubs = {
     };
   },
 
-  JiraIntegrationProvider: params => {
-    return {
-      key: 'jira',
-      name: 'Jira',
-      canAdd: false,
-      canAddProject: true,
-      config: [],
-      features: [],
-      metadata: {
-        description: '*markdown* formatted Jira _description_',
-        author: 'Rick',
-        noun: 'Instance',
-        issue_url: 'http://example.com/jira_integration_issue_url',
-        source_url: 'http://example.com/jira_integration_source_url',
-        aspects: {
-          externalInstall: [
-            {
-              url: 'http://jira.com',
-              buttonText: 'Visit Jira',
-              noticeText: 'You must visit jira to install the integration',
-            },
-          ],
-        },
-      },
-      ...params,
-    };
-  },
-
   GitHubIntegration: params => {
     return {
       domainName: 'gtithub.com/test-integration',
