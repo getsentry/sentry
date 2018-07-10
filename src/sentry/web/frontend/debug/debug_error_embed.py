12121c0d8e647333a202e6b9628401e47442da03
David Cramer <dcramer@gmail.com>
2018-07-10 13:53:21 -0700

    feat: Overhaul localization for user feedback
    - Remove unused tags in user feedback localization (discards existing translations)
    - Support ?lang=[code] via locale middleware to force language on a single page load
    - Correct debug page for embed QA
    - Support customization of most strings

--- src/sentry/web/frontend/debug/debug_error_embed.py
+++ src/sentry/web/frontend/debug/debug_error_embed.py
@@ -1,7 +1,10 @@
 from __future__ import absolute_import
 
+import six
+
 from django.conf import settings
 from django.views.generic import View
+from six.moves.urllib.parse import urlencode
 
 from sentry.models import ProjectKey
 from sentry.web.helpers import render_to_response
@@ -16,6 +19,8 @@ class DebugErrorPageEmbedView(View):
     def get(self, request):
         context = {
             'dsn': self._get_project_key().dsn_public,
+            'event_id': '342a3d7f690a49f8bd7c4cf0e61a9ded',
+            'options': urlencode({k: v for k, v in six.iteritems(request.GET)}),
         }
 
         return render_to_response('sentry/debug/error-page-embed.html', context, request)
