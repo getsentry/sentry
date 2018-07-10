4ad53e907274b025da66203e12c136df7a6c8ad5
Helmut Januschka <helmut@januschka.com>
2018-07-10 11:40:10 +0200

    only on textfields

--- src/sentry/static/sentry/app/utils/selectText.jsx
+++ src/sentry/static/sentry/app/utils/selectText.jsx
@@ -1,3 +1,15 @@
 export function selectText(node) {
-  node.select();
+  if(node.type && node.type === 'text') {
+    node.select();
+  } else if (document.selection) {
+    let range = document.body.createTextRange();
+    range.moveToElementText(node);
+    range.select();
+  } else if (window.getSelection) {
+    let range = document.createRange();
+    range.selectNode(node);
+    let selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+  }
 }
