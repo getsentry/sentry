7f30a4ac6c4e34b861441e380666dddd02071953
Lauryn Brown <lauryndbrown@gmail.com>
2018-07-11 15:40:11 -0700

    Added new model rough draft

---
+++ src/sentry/models/externalintegrationproject.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+from django.db import models
+from django.utils import timezone
+
+from sentry.db.models import (
+    BoundedPositiveIntegerField, Model
+)
+
+
+class ExternalProjectIntegration(Model):
+    __core__ = False
+
+    organization_integration = BoundedPositiveIntegerField(db_index=True)
+    date_added = models.DateTimeField(default=timezone.now)
+    name = models.CharField(max_length=128)
+    external_id = models.CharField(max_length=64)
+
+    class Meta:
+        app_label = 'sentry'
+        db_table = 'sentry_externalprojectintegration'
