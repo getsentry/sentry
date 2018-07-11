c282ddf8243e50ae0bbe406757b764f1c012599d
Lauryn Brown <lauryndbrown@gmail.com>
2018-07-11 15:47:54 -0700

    Adde3d a unqiue together.

--- src/sentry/models/externalintegrationproject.py
+++ src/sentry/models/externalintegrationproject.py
@@ -11,7 +11,7 @@ from sentry.db.models import (
 class ExternalProjectIntegration(Model):
     __core__ = False
 
-    organization_integration = BoundedPositiveIntegerField(db_index=True)
+    organization_integration_id = BoundedPositiveIntegerField(db_index=True)
     date_added = models.DateTimeField(default=timezone.now)
     name = models.CharField(max_length=128)
     external_id = models.CharField(max_length=64)
@@ -19,3 +19,4 @@ class ExternalProjectIntegration(Model):
     class Meta:
         app_label = 'sentry'
         db_table = 'sentry_externalprojectintegration'
+        unique_together = (('organization_integration_id', 'external_id'),)
