585d60788f3fac83a04108ba6def0c22e1b6211f
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-07-16 14:50:08 -0700

    fix(ui): Remove border on last PanelItemGroup (#9033)

--- src/sentry/static/sentry/app/components/panels/panelItemGroup.jsx
+++ src/sentry/static/sentry/app/components/panels/panelItemGroup.jsx
@@ -4,6 +4,10 @@ const PanelItemGroup = styled('div')`
   border-left: 4px solid ${p => p.theme.borderLight};
   border-bottom: 1px solid ${p => p.theme.borderLight};
   background: ${p => p.theme.offWhite};
+
+  &:last-child {
+    border-bottom: 0;
+  }
 `;
 
 export default PanelItemGroup;
