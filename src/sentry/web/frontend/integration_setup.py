584f40377205f46a1b94949953e6608e579f9218
Adhiraj Somani <adhiraj@sentry.io>
2018-07-16 12:13:16 -0700

    Make the change to /extensions/vsts/setup
    This is the actual vsts callback. the previous view isn't, but I'm going to keep that change as is because it is the Github callback which can also benefit from being re-used.

--- src/sentry/web/frontend/integration_setup.py
+++ src/sentry/web/frontend/integration_setup.py
@@ -2,6 +2,10 @@ from __future__ import absolute_import, print_function
 
 import logging
 
+from django.contrib import messages
+from django.utils.translation import ugettext_lazy as _
+
+from sentry.identity.pipeline import IdentityProviderPipeline
 from sentry.integrations.pipeline import IntegrationPipeline
 from sentry.web.frontend.base import BaseView
 
@@ -9,14 +13,17 @@ logger = logging.getLogger('sentry.integrations')
 
 
 class IntegrationSetupView(BaseView):
-    required_scope = 'org:integrations'
+    auth_required = False
 
     csrf_protect = False
 
     def handle(self, request, provider_id):
         pipeline = IntegrationPipeline.get_for_request(request=request)
         if not pipeline:
-            logging.error('integration.setup-error')
+            pipeline = IdentityProviderPipeline.get_for_request(request=request)
+
+        if pipeline is None or not pipeline.is_valid():
+            messages.add_message(request, messages.ERROR, _("Invalid request."))
             return self.redirect('/')
 
         return pipeline.current_step()
