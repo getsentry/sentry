cfddf61afd1985f68b15f7cabc5cc09df68a44f5
denamwangi <dena@getsentry.com>
2018-07-19 09:56:14 -0700

    make api take experiment name in explicitly

--- src/sentry/static/sentry/app/utils/log_exposure.jsx
+++ src/sentry/static/sentry/app/utils/log_exposure.jsx
@@ -1,8 +1,11 @@
 import HookStore from 'app/stores/hookStore';
 
 /**
- * @param {Object} data Experiment data to be recorded: {experiment_name, unit_name, unit_id, params}
+ * @param {String} name name of the experiment
+ * @param {Object} data Experiment data to be recorded: {unit_name, unit_id, params}
+ * 					note that params need to string
  */
-export default function log_exposure(data) {
-  HookStore.get('analytics:log-experiment').forEach(cb => cb(data));
+export default function log_exposure(name, data) {
+	data.experiment_name = name;
+	HookStore.get('analytics:log-exposure').forEach(cb => cb(data));
 }
