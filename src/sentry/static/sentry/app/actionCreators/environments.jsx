9dbda5176c5c6bec506a8a7eb4b804e9355ede44
Billy Vong <billy@sentry.io>
2018-07-20 11:22:03 -0700

    feat(ui): Add actionCreator to fetch envs for a project
    This will be used to fetch environments for multiple projects in Health.

--- src/sentry/static/sentry/app/actionCreators/environments.jsx
+++ src/sentry/static/sentry/app/actionCreators/environments.jsx
@@ -1,5 +1,15 @@
+import {Client} from 'app/api';
 import EnvironmentActions from 'app/actions/environmentActions';
 import EnvironmentStore from 'app/stores/environmentStore';
+
+export function fetchEnvironmentsForProject(orgId, projectId, hidden) {
+  let api = new Client();
+  return api.requestPromise(`/projects/${orgId}/${projectId}/environments/`, {
+    query: {
+      visibility: hidden ? 'hidden' : 'visible',
+    },
+  });
+}
 
 export function setActiveEnvironment(environment) {
   EnvironmentActions.setActive(environment);
