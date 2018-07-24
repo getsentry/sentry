aa4b26adf977ece8fd3e8a0469c241dd5fcfe54c
Billy Vong <billyvg@users.noreply.github.com>
2018-07-24 14:33:17 -0700

    feat(health): Add request action creator (#9128)

---
+++ src/sentry/static/sentry/app/actionCreators/health.jsx
@@ -0,0 +1,36 @@
+const BASE_URL = org => `/organizations/${org.slug}/health/`;
+
+/**
+ * Make requests to `health` endpoint
+ *
+ * @param {Object} api API client instance
+ * @param {Object} options Request parameters
+ * @param {Object} options.organization Organization object
+ * @param {Number[]} options.projects List of proejct ids
+ * @param {String} options.tag The "tag" to query for
+ * @param {Boolean} options.timeseries Should we group results by time period
+ * @param {String[]} options.environments List of environments to query for
+ * @param {String} options.period Time period to query for, in the format: <integer><units> where units are "d" or "h"
+ * @param {Boolean} options.includePrevious Should request also return reqsults for previous period?
+ * @param {Number} options.topk Include topk results
+ */
+export const doHealthRequest = (
+  api,
+  {organization, projects, tag, environments, period, timeseries, includePrevious, topk}
+) => {
+  if (!api) return Promise.reject(new Error('API client not available'));
+
+  const path = timeseries ? 'graph/' : 'top/';
+  const query = {
+    tag,
+    includePrevious,
+    statsPeriod: period,
+    project: projects,
+    environment: environments,
+    ...(topk ? {topk} : {}),
+  };
+
+  return api.requestPromise(`${BASE_URL(organization)}${path}`, {
+    query,
+  });
+};
