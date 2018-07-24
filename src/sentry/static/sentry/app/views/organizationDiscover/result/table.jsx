ec73485aa1d34ea793ad106d13911d0a80992aff
Lyn Nagara <lyn.nagara@gmail.com>
2018-07-24 12:36:28 -0700

    feat(discover): Add grid styles and autosizing
    Autosizes grid to window width, and sets max height of 15 rows. Add
    table styling.

--- src/sentry/static/sentry/app/views/organizationDiscover/result/table.jsx
+++ src/sentry/static/sentry/app/views/organizationDiscover/result/table.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
-import {MultiGrid} from 'react-virtualized';
+import {MultiGrid, AutoSizer} from 'react-virtualized';
 import PropTypes from 'prop-types';
 import styled from 'react-emotion';
 import {Box} from 'grid-emotion';
 
+import theme from 'app/utils/theme';
 import AutoSelectText from 'app/components/autoSelectText';
 
 import {getDisplayValue} from './utils';
@@ -18,11 +19,10 @@ export default class Result extends React.Component {
 
   cellRenderer = ({key, rowIndex, columnIndex, style}) => {
     const {meta, data} = this.props.result;
-
     const colName = meta[columnIndex].name;
 
     return (
-      <Cell key={key} style={style}>
+      <Cell key={key} style={style} isOddRow={rowIndex % 2 === 1}>
         {rowIndex === 0 ? (
           <strong>{colName}</strong>
         ) : (
@@ -35,17 +35,27 @@ export default class Result extends React.Component {
   renderTable() {
     const {meta, data} = this.props.result;
 
+    const maxVisibleResults = Math.min(data.length, 15);
+
     return (
-      <MultiGrid
-        width={800}
-        height={300}
-        rowCount={data.length + 1} // Add 1 for header row
-        columnCount={meta.length}
-        fixedRowCount={1}
-        rowHeight={30}
-        columnWidth={100}
-        cellRenderer={this.cellRenderer}
-      />
+      <GridContainer visibleRows={maxVisibleResults + 1}>
+        <AutoSizer>
+          {({width, height}) => (
+            <MultiGrid
+              width={width}
+              height={height}
+              rowCount={data.length + 1} // Add 1 for header row
+              columnCount={meta.length}
+              fixedRowCount={1}
+              rowHeight={30}
+              columnWidth={120}
+              cellRenderer={this.cellRenderer}
+              style={{border: `1px solid ${theme.borderLight}`}}
+              styleTopRightGrid={{borderBottom: `2px solid ${theme.borderDark}`}}
+            />
+          )}
+        </AutoSizer>
+      </GridContainer>
     );
   }
 
@@ -67,10 +77,19 @@ export default class Result extends React.Component {
   }
 }
 
+const GridContainer = styled('div')`
+  height: ${p => p.visibleRows * 30}px;
+
+  .ReactVirtualized__Grid {
+    outline: none;
+  }
+`;
+
 const Cell = styled('div')`
-  overflow: scroll;
+  ${p => p.isOddRow && `background-color: ${p.theme.borderLighter};`} overflow: scroll;
   font-size: 14px;
-  padding: 1px;
+  line-height: 30px;
+  padding: 0 4px;
 `;
 
 const Summary = styled(Box)`
