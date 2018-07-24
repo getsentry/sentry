28a6bcc7bb46f2c3673ca50d4ac8a18acfa22633
Ted Kaemming <ted@kaemming.com>
2018-07-23 17:37:39 -0700

    explain join

--- src/sentry/eventstream/kafka/utils.py
+++ src/sentry/eventstream/kafka/utils.py
@@ -2,6 +2,17 @@ from __future__ import absolute_import
 
 
 def join(consumers, timeout=0.0, throttle=0.1):
+    """
+    Creates an iterator that can be used to consume from multiple independent
+    Kafka consumers.
+
+    - The ``timeout`` will be used as the poll interval for each message,
+      allowing for rapid consumption when consumers are receving messages at a
+      high rate.
+    - The ``throttle`` value will be added to the ``timeout`` value if none of
+      the consumers returned a message on the last iteration through the loop
+      (avoiding busy waiting.)
+    """
     i = 0
     while True:
         for consumer in consumers:
