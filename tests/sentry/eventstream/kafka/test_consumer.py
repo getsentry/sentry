88e664d3028fbedf403382df1436bc5357c1f60b
Ted Kaemming <ted@kaemming.com>
2018-07-25 11:47:39 -0700

    add stubs

---
+++ tests/sentry/eventstream/kafka/test_consumer.py
@@ -0,0 +1,21 @@
+from __future__ import absolute_import
+
+
+def test_consumer_start_from_partition_start():
+    raise NotImplementedError
+
+
+def test_consumer_start_from_committed_offset():
+    raise NotImplementedError
+
+
+def test_consumer_rebalance_from_partition_start():
+    raise NotImplementedError
+
+
+def test_consumer_rebalance_from_committed_offset():
+    raise NotImplementedError
+
+
+def test_consumer_rebalance_from_uncommitted_offset():
+    raise NotImplementedError
