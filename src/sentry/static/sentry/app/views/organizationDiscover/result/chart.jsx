fb38e71817ca6087c58306b05545816a62002225
Lyn Nagara <lyn.nagara@gmail.com>
2018-07-25 15:47:18 -0700

    feat(discover): Improve chart label text and add comment

--- src/sentry/static/sentry/app/views/organizationDiscover/result/chart.jsx
+++ src/sentry/static/sentry/app/views/organizationDiscover/result/chart.jsx
@@ -9,13 +9,29 @@ export default class Result extends React.Component {
     aggregations: PropTypes.array,
   };
 
+  // Converts a value to a string for the chart label. This could
+  // potentially cause incorrect grouping, e.g. if the value null and string
+  // 'null' are both present in the same series they will be merged into 1 value
+  getLabel(value) {
+    if (typeof value === 'object') {
+      try {
+        value = JSON.stringify(value);
+      } catch (err) {
+        // eslint-disable-next-line no-console
+        console.error(err);
+      }
+    }
+
+    return value;
+  }
+
   getDataForChart(queryData, groupbyFields) {
     const {aggregations} = this.props;
     const aggregate = aggregations[0][2];
 
     const output = {};
     queryData.forEach(data => {
-      const key = groupbyFields.map(field => data[field]).join(',');
+      const key = groupbyFields.map(field => this.getLabel(data[field])).join(',');
       if (key in output) {
         output[key][aggregate].push(data[aggregate]);
       } else {
