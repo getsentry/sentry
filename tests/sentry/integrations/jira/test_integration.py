adbd554234bc4aa0705de118a37317bee617ebff
Jess MacQueen <macqueen@users.noreply.github.com>
2018-07-25 09:41:14 -0700

    feat(jira): Add project status mapping to org integration config (#9164)

--- tests/sentry/integrations/jira/test_integration.py
+++ tests/sentry/integrations/jira/test_integration.py
@@ -501,3 +501,116 @@ class JiraIntegrationTest(APITestCase):
                 # test resolve -- 31 is "done" transition id
                 installation.sync_status_outbound(external_issue, True, project.id)
                 mock_transition_issue.assert_called_with('SEN-5', '31')
+
+    def test_update_organization_config(self):
+        org = self.organization
+        self.login_as(self.user)
+
+        integration = Integration.objects.create(
+            provider='jira',
+            name='Example Jira',
+        )
+        integration.add_organization(org.id)
+
+        installation = integration.get_installation(org.id)
+
+        data = {
+            'sync_comments': True,
+            'sync_forward_assignment': True,
+            'sync_reverse_assignment': True,
+            'sync_status_reverse': True,
+            'sync_status_forward': {
+                10100: {
+                    'on_resolve': '4',
+                    'on_unresolve': '3',
+                },
+            },
+        }
+
+        installation.update_organization_config(data)
+
+        org_integration = OrganizationIntegration.objects.get(
+            organization_id=org.id,
+            integration_id=integration.id,
+        )
+
+        org_integration.config == {
+            'sync_comments': True,
+            'sync_forward_assignment': True,
+            'sync_reverse_assignment': True,
+            'sync_status_reverse': True,
+            'sync_status_forward': True,
+        }
+
+        assert IntegrationExternalProject.objects.filter(
+            organization_integration_id=org_integration.id,
+            resolved_status='4',
+            unresolved_status='3',
+        ).exists()
+
+        # test update existing
+        data = {
+            'sync_comments': True,
+            'sync_forward_assignment': True,
+            'sync_reverse_assignment': True,
+            'sync_status_reverse': True,
+            'sync_status_forward': {
+                10100: {
+                    'on_resolve': '4',
+                    'on_unresolve': '5',
+                },
+            },
+        }
+
+        installation.update_organization_config(data)
+
+        org_integration = OrganizationIntegration.objects.get(
+            organization_id=org.id,
+            integration_id=integration.id,
+        )
+
+        org_integration.config == {
+            'sync_comments': True,
+            'sync_forward_assignment': True,
+            'sync_reverse_assignment': True,
+            'sync_status_reverse': True,
+            'sync_status_forward': True,
+        }
+
+        assert IntegrationExternalProject.objects.filter(
+            organization_integration_id=org_integration.id,
+            resolved_status='4',
+            unresolved_status='5',
+        ).exists()
+
+        assert IntegrationExternalProject.objects.filter(
+            organization_integration_id=org_integration.id,
+        ).count() == 1
+
+        # test disable forward
+        data = {
+            'sync_comments': True,
+            'sync_forward_assignment': True,
+            'sync_reverse_assignment': True,
+            'sync_status_reverse': True,
+            'sync_status_forward': {},
+        }
+
+        installation.update_organization_config(data)
+
+        org_integration = OrganizationIntegration.objects.get(
+            organization_id=org.id,
+            integration_id=integration.id,
+        )
+
+        org_integration.config == {
+            'sync_comments': True,
+            'sync_forward_assignment': True,
+            'sync_reverse_assignment': True,
+            'sync_status_reverse': True,
+            'sync_status_forward': False,
+        }
+
+        assert IntegrationExternalProject.objects.filter(
+            organization_integration_id=org_integration.id,
+        ).count() == 0
