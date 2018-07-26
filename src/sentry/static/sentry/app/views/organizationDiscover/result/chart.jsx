b07998cd494bcecc891ee2482e4f5af7f2be7ddc
Lyn Nagara <lyn.nagara@gmail.com>
2018-07-25 17:33:14 -0700

    fix(discover): Chart component gets the last run query (#9195)
    Chart component needs the last run query that corresponds to data, not
    the current querybuilder state

--- src/sentry/static/sentry/app/views/organizationDiscover/result/chart.jsx
+++ src/sentry/static/sentry/app/views/organizationDiscover/result/chart.jsx
@@ -4,10 +4,15 @@ import PropTypes from 'prop-types';
 export default class Result extends React.Component {
   static propTypes = {
     data: PropTypes.object.isRequired,
+    query: PropTypes.object.isRequired,
   };
 
   render() {
+    const {aggregations, fields} = this.props.query;
     // TODO: implement charts
-    return `data for charts: ${JSON.stringify(this.props.data)}`;
+    return (
+      `data for charts: ${JSON.stringify(this.props.data)} ` +
+      `chart query: ${fields} ${aggregations}`
+    );
   }
 }
