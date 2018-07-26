ce3deebcf0670a95290873bddd27f4bf916ddcb8
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-07-26 14:10:16 -0700

    feat(forms): Support autosize on textareas

--- src/sentry/static/sentry/app/views/settings/components/forms/controls/textarea.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/controls/textarea.jsx
@@ -1,8 +1,26 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+import TextareaAutosize from 'react-autosize-textarea';
 import styled from 'react-emotion';
 
 import {inputStyles} from 'app/styles/input';
 
-const TextArea = styled.textarea`
+const TextArea = styled(
+  p =>
+    p.autosize ? (
+      <TextareaAutosize rows={p.rows ? p.rows : 2} async={true} {...p} />
+    ) : (
+      <textarea {...p} />
+    )
+)`
   ${inputStyles};
 `;
+
+TextArea.propTypes = {
+  /**
+   * Enable autosizing of the textarea.
+   */
+  autosize: PropTypes.bool,
+};
+
 export default TextArea;
