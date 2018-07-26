185e02fc229d3c0ca85fe65a096707ef34ed042b
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-07-26 12:09:24 -0700

    fix(forms): Use space in fieldHelp

--- src/sentry/static/sentry/app/views/settings/components/forms/field/fieldHelp.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/field/fieldHelp.jsx
@@ -1,9 +1,11 @@
 import styled from 'react-emotion';
 
+import space from 'app/styles/space';
+
 const FieldHelp = styled.div`
   color: ${p => p.theme.gray2};
   font-size: 14px;
-  margin-top: 8px;
+  margin-top: ${space(1)};
   line-height: 1.4;
 `;
 
