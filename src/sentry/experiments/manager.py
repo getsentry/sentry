37ae8458d6bad91e8f889711465c85b2cf470455
denamwangi <dena@getsentry.com>
2018-07-26 13:57:35 -0700

    generalize a bit to accomodate other params and log_exposure

--- src/sentry/experiments/manager.py
+++ src/sentry/experiments/manager.py
@@ -6,12 +6,15 @@ class ExperimentManager(object):
     def __init__(self):
         self._experiments = {}
 
-    def add(self, experiments):
-        for experiment in experiments:
-            self._experiments[experiment.__name__] = experiment
+    def add(self, experiment, param, log_exposure=True):
+        self._experiments[experiment.__name__] = {
+            'experiment': experiment, 'param': param, 'log_exposure': log_exposure}
 
     def all(self, org, actor):
         assignments = {}
         for k, v in six.iteritems(self._experiments):
-            assignments[k] = v(org=org, actor=actor).get_variant('exposed', log_exposure=False)
+            cls = v['experiment']
+            assignments[k] = cls(
+                org=org, actor=actor).get_variant(
+                v['param'], log_exposure=v['log_exposure'])
             return assignments
