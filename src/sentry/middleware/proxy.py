227437886f3855123b1c66d53fcf4e4c8fd2693f
Jan Michael Auer <jan.auer@sentry.io>
2018-07-27 12:24:14 +0200

    ref(proxy): Decompress lazily

--- src/sentry/middleware/proxy.py
+++ src/sentry/middleware/proxy.py
@@ -14,6 +14,7 @@ except ImportError:
 from django.conf import settings
 
 logger = logging.getLogger(__name__)
+Z_CHUNK = 1024 * 8
 
 
 if has_uwsgi:
@@ -34,6 +35,74 @@ if has_uwsgi:
                 self._internal_buffer = self._internal_buffer[n:]
 
             return n
+
+
+class ZDecoder(io.RawIOBase):
+    """
+    Base class for HTTP content decoders based on zlib
+    See: https://github.com/eBay/wextracto/blob/9c789b1c98d95a1e87dbedfd1541a8688d128f5c/wex/http_decoder.py
+    """
+
+    def __init__(self, fp, z=None):
+        self.fp = fp
+        self.z = z
+        self.flushed = None
+
+    def readable(self):
+        return True
+
+    def readinto(self, buf):
+        if self.z is None:
+            self.z = zlib.decompressobj()
+            retry = True
+        else:
+            retry = False
+
+        n = 0
+        max_length = len(buf)
+
+        while max_length > 0:
+            if self.flushed is None:
+                chunk = self.fp.read(Z_CHUNK)
+                compressed = (self.z.unconsumed_tail + chunk)
+                try:
+                    decompressed = self.z.decompress(compressed, max_length)
+                except zlib.error:
+                    if not retry:
+                        raise
+                    self.z = zlib.decompressobj(-zlib.MAX_WBITS)
+                    retry = False
+                    decompressed = self.z.decompress(compressed, max_length)
+
+                if not chunk:
+                    self.flushed = self.z.flush()
+            else:
+                if not self.flushed:
+                    return n
+
+                decompressed = self.flushed[:max_length]
+                self.flushed = self.flushed[max_length:]
+
+            buf[n:n + len(decompressed)] = decompressed
+            n += len(decompressed)
+            max_length = len(buf) - n
+
+        return n
+
+
+class DeflateDecoder(ZDecoder):
+    """
+    Decoding for "content-encoding: deflate"
+    """
+
+
+class GzipDecoder(ZDecoder):
+    """
+    Decoding for "content-encoding: gzip"
+    """
+
+    def __init__(self, fp):
+        ZDecoder.__init__(self, fp, zlib.decompressobj(16 + zlib.MAX_WBITS))
 
 
 class SetRemoteAddrFromForwardedFor(object):
@@ -77,29 +146,22 @@ class ChunkedMiddleware(object):
             request.META['CONTENT_LENGTH'] = '4294967295'  # 0xffffffff
 
 
-COMPRESSED_ENCODINGS = ('gzip', 'deflate')
-
-
-def decompress(data, encoding):
-    if encoding == 'gzip':
-        return zlib.decompress(data, zlib.MAX_WBITS | 16)
-    if encoding == 'deflate':
-        return zlib.decompress(data, -zlib.MAX_WBITS)
-
-
 class DecompressBodyMiddleware(object):
     def process_request(self, request):
+        decode = False
         encoding = request.META.get('HTTP_CONTENT_ENCODING', '').lower()
-        if encoding in COMPRESSED_ENCODINGS:
-            data = request._stream.read()
-            try:
-                uncompressed = decompress(data, encoding)
-                request._stream = six.StringIO(uncompressed)
-                request.META['CONTENT_LENGTH'] = len(uncompressed)
-                request.META['HTTP_CONTENT_ENCODING'] = 'identity'
-            except zlib.error:
-                logger.debug('Failed to decompress body payload', exc_info=True)
-                request._stream = six.StringIO(data)
+
+        if encoding == 'gzip':
+            request._stream = GzipDecoder(request._stream)
+            decode = True
+
+        if encoding == 'deflate':
+            request._stream = DeflateDecoder(request._stream)
+            decode = True
+
+        if decode:
+            request.META['CONTENT_LENGTH'] = '4294967295'  # 0xffffffff
+            del request.META['HTTP_CONTENT_ENCODING']
 
 
 class ContentLengthHeaderMiddleware(object):
