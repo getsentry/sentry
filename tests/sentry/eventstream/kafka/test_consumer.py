fc4ca7fbf97336603d86212950bd3c28115a97ae
Ted Kaemming <ted@kaemming.com>
2018-07-26 17:06:17 -0700

    initial balanced tests

--- tests/sentry/eventstream/kafka/test_consumer.py
+++ tests/sentry/eventstream/kafka/test_consumer.py
@@ -227,7 +227,120 @@ def test_consumer_start_from_committed_offset():
 
 
 def test_consumer_rebalance_from_partition_start():
-    raise NotImplementedError
+    consumer_group = 'consumer-{}'.format(uuid.uuid1().hex)
+    synchronize_commit_group = 'consumer-{}'.format(uuid.uuid1().hex)
+
+    messages_delivered = defaultdict(list)
+
+    def record_message_delivered(error, message):
+        assert error is None
+        messages_delivered[message.topic()].append(message)
+
+    producer = Producer({
+        'bootstrap.servers': 'localhost:9092',
+        'on_delivery': record_message_delivered,
+    })
+
+    with create_topic(partitions=2) as topic, create_topic() as commit_log_topic:
+
+        # Produce some messages into the topic.
+        for i in range(4):
+            producer.produce(topic, '{}'.format(i).encode('utf8'), partition=i % 2)
+
+        assert producer.flush(5) == 0, 'producer did not successfully flush queue'
+
+        consumer_a = SynchronizedConsumer(
+            bootstrap_servers='localhost:9092',
+            consumer_group=consumer_group,
+            commit_log_topic=commit_log_topic,
+            synchronize_commit_group=synchronize_commit_group,
+        )
+
+        assignments_received = defaultdict(list)
+
+        def on_assign(consumer, assignment):
+            assignments_received[consumer].append(assignment)
+
+        consumer_a.subscribe([topic], on_assign=on_assign)
+
+        # Wait until the first consumer has received its assignments.
+        for i in xrange(10):  # this takes a while
+            assert consumer_a.poll(1) is None
+            if assignments_received[consumer_a]:
+                break
+
+        assert len(assignments_received[consumer_a]
+                   ) == 1, 'expected to receive partition assignment'
+        assert set((i.topic, i.partition)
+                   for i in assignments_received[consumer_a][0]) == set([(topic, 0), (topic, 1)])
+
+        assignments_received[consumer_a].pop()
+
+        consumer_b = SynchronizedConsumer(
+            bootstrap_servers='localhost:9092',
+            consumer_group=consumer_group,
+            commit_log_topic=commit_log_topic,
+            synchronize_commit_group=synchronize_commit_group,
+        )
+
+        consumer_b.subscribe([topic], on_assign=on_assign)
+
+        assignments = {}
+
+        # Wait until *both* consumers have received updated assignments.
+        for consumer in [consumer_a, consumer_b]:
+            for i in xrange(10):  # this takes a while
+                assert consumer.poll(1) is None
+                if assignments_received[consumer]:
+                    break
+
+            assert len(assignments_received[consumer]
+                       ) == 1, 'expected to receive partition assignment'
+            assert len(assignments_received[consumer][0]
+                       ) == 1, 'expected to have a single partition assignment'
+
+            i = assignments_received[consumer][0][0]
+            assignments[(i.topic, i.partition)] = consumer
+
+        assert set(assignments.keys()) == set([(topic, 0), (topic, 1)])
+
+        for expected_message in messages_delivered[topic]:
+            consumer = assignments[(expected_message.topic(), expected_message.partition())]
+
+            # Make sure that there are no messages ready to consume.
+            assert consumer.poll(1) is None
+
+            # Move the committed offset forward for our synchronizing group.
+            producer.produce(
+                commit_log_topic,
+                key='{}:{}:{}'.format(
+                    expected_message.topic(),
+                    expected_message.partition(),
+                    synchronize_commit_group,
+                ).encode('utf8'),
+                value='{}'.format(
+                    expected_message.offset() + 1,
+                ).encode('utf8'),
+            )
+
+            assert producer.flush(5) == 0, 'producer did not successfully flush queue'
+
+            # We should have received a single message.
+            # TODO: Can we also assert that the position is unpaused?)
+            for i in xrange(5):
+                received_message = consumer.poll(1)
+                if received_message is not None:
+                    break
+
+            assert received_message is not None, 'no message received'
+
+            assert received_message.topic() == expected_message.topic()
+            assert received_message.partition() == expected_message.partition()
+            assert received_message.offset() == expected_message.offset()
+
+            # We should not be able to continue reading into the topic.
+            # TODO: Can we assert that the position is paused?
+            assert consumer.poll(1) is None
 
 
 def test_consumer_rebalance_from_committed_offset():
