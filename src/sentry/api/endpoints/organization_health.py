fda1fad99d236478abd31bab23a6258817754ab7
Matt Robenolt <matt@ydekproductions.com>
2018-07-27 16:04:01 -0700

    Allow time period unit to be optional and assume seconds

--- src/sentry/api/endpoints/organization_health.py
+++ src/sentry/api/endpoints/organization_health.py
@@ -27,11 +27,13 @@ def parse_stats_period(period):
     Convert a value such as 1h into a
     proper timedelta.
     """
-    m = re.match('^(\d+)([hdms])$', period)
+    m = re.match('^(\d+)([hdms]?)$', period)
     if not m:
         return None
     value, unit = m.groups()
     value = int(value)
+    if not unit:
+        unit = 's'
     return timedelta(**{
         {'h': 'hours', 'd': 'days', 'm': 'minutes', 's': 'seconds'}[unit]: value,
     })
