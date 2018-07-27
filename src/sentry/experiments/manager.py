592f6d363cef57c8aff6edec017568b7c391e7fd
denamwangi <dena@getsentry.com>
2018-07-26 18:34:42 -0700

    respond to comments

--- src/sentry/experiments/manager.py
+++ src/sentry/experiments/manager.py
@@ -3,18 +3,27 @@ import six
 
 
 class ExperimentManager(object):
+    """
+    Allows loading of experiment assignments (done in getsentry) on the frontend by
+    including them in the serialized org details via the org serializer which is in sentry.
+    """
+
     def __init__(self):
         self._experiments = {}
 
-    def add(self, experiment, param, log_exposure=True):
+    def add(self, experiment, param):
+        """
+        >>> ExperimentManager.add(ExperimentClass, param='name_of_param')
+        """
         self._experiments[experiment.__name__] = {
-            'experiment': experiment, 'param': param, 'log_exposure': log_exposure}
+            'experiment': experiment, 'param': param}
 
     def all(self, org, actor):
+        """Returns an object with all the experiment assignments for the org, given this particular actor."""
         assignments = {}
         for k, v in six.iteritems(self._experiments):
             cls = v['experiment']
             assignments[k] = cls(
                 org=org, actor=actor).get_variant(
-                v['param'], log_exposure=v['log_exposure'])
-            return assignments
+                v['param'], log_exposure=False)
+        return assignments
