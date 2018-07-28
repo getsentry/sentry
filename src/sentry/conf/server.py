c96abc7716d8f753f019e4d35a22414c6c348e0f
Matt Robenolt <matt@ydekproductions.com>
2018-07-27 17:23:46 -0700

    Revert "feat(minidump): Support gzipped crashpad requests (#9208)"
    This reverts commit 99afeb0d261e5ada0c65f65691434dade2e0dfd5.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -211,7 +211,6 @@ TEMPLATE_LOADERS = (
 
 MIDDLEWARE_CLASSES = (
     'sentry.middleware.proxy.ChunkedMiddleware',
-    'sentry.middleware.proxy.DecompressBodyMiddleware',
     'sentry.middleware.proxy.ContentLengthHeaderMiddleware',
     'sentry.middleware.security.SecurityHeadersMiddleware',
     'sentry.middleware.maintenance.ServicesUnavailableMiddleware',
