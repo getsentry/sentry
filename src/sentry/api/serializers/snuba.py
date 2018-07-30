8a6a732eff28061d450325c3357c463e6bba74b8
Matt Robenolt <matt@ydekproductions.com>
2018-07-30 16:24:13 -0700

    Fix off by one in zerofill code
    Right now, when querying for say, 7 days, with a 1 day rollup, we get
    back 8 buckets instead of the expected 7.
    
    TODO: figure out why it returns 8 and how to make it not do that.

--- src/sentry/api/serializers/snuba.py
+++ src/sentry/api/serializers/snuba.py
@@ -88,7 +88,7 @@ def value_from_row(row, tagkey):
 
 def zerofill(data, start, end, rollup):
     rv = []
-    start = ((int(to_timestamp(start)) / rollup) * rollup) + rollup
+    start = ((int(to_timestamp(start)) / rollup) * rollup) - rollup
     end = ((int(to_timestamp(end)) / rollup) * rollup) + rollup
     i = 0
     for key in xrange(start, end, rollup):
