f1544190f92dc85c68d3352a9dea183cbafbb5d8
Jess MacQueen <jessmacqueen@gmail.com>
2018-07-30 18:38:57 -0700

    fix(integrations): Return an error when vsts repo fetching fails

--- src/sentry/api/endpoints/organization_integration_repos.py
+++ src/sentry/api/endpoints/organization_integration_repos.py
@@ -6,6 +6,7 @@ from sentry.constants import ObjectStatus
 from sentry.api.bases.organization import (
     OrganizationEndpoint, OrganizationIntegrationsPermission
 )
+from sentry.integrations.exceptions import IntegrationError
 from sentry.integrations.repositories import RepositoryMixin
 from sentry.models import Integration
 
@@ -26,7 +27,11 @@ class OrganizationIntegrationReposEndpoint(OrganizationEndpoint):
 
         install = integration.get_installation(organization.id)
         if isinstance(install, RepositoryMixin):
-            repositories = install.get_repositories()
+            try:
+                repositories = install.get_repositories()
+            except IntegrationError as e:
+                return self.respond({'detail': e.message}, status=400)
+
             context = {'repos': repositories}
             return self.respond(context)
 
