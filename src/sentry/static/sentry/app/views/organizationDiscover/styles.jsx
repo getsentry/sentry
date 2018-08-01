725eabfc73dbe8abc7bfd107dc3177057908308e
Lyn Nagara <lyn.nagara@gmail.com>
2018-08-01 13:33:00 -0700

    feat(discover): Add loading indicator
    Displays a loading indicator and disables button when a query is in
    flight

--- src/sentry/static/sentry/app/views/organizationDiscover/styles.jsx
+++ src/sentry/static/sentry/app/views/organizationDiscover/styles.jsx
@@ -1,5 +1,6 @@
 import styled from 'react-emotion';
 import {Flex} from 'grid-emotion';
+import {keyframes} from 'emotion';
 
 import space from 'app/styles/space';
 
@@ -23,4 +24,25 @@ export const AddText = styled.span`
   font-size: 13px;
   line-height: 16px;
   color: ${p => p.theme.gray1};
+`;
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
+
+export const ButtonSpinner = styled.div`
+  animation: ${spin} 0.4s linear infinite;
+  width: 14px;
+  height: 14px;
+  border-radius: 14px;
+  border-top: 2px solid ${p => p.theme.borderLight};
+  border-right: 2px solid ${p => p.theme.borderLight};
+  border-bottom: 2px solid ${p => p.theme.borderLight};
+  border-left: 2px solid ${p => p.theme.purple};
+  margin-left: 4px;
 `;
