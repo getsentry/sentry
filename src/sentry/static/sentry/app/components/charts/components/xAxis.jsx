15e5da8c12da3a0be76041a82baae0df374be6f3
Billy Vong <billyvg@users.noreply.github.com>
2018-08-03 10:52:33 -0700

    feat(charts): Add "100% Stacked Percentage Column Chart" (#9247)

--- src/sentry/static/sentry/app/components/charts/components/xAxis.jsx
+++ src/sentry/static/sentry/app/components/charts/components/xAxis.jsx
@@ -6,15 +6,18 @@ export default function XAxis(props = {}) {
     axisLine: {
       lineStyle: {
         color: theme.gray1,
+        ...(props.axisLine || {}),
       },
     },
     axisTick: {
       lineStyle: {
         color: theme.gray1,
       },
+      ...(props.axisTick || {}),
     },
     axisLabel: {
       margin: 12,
+      ...(props.axisLabel || {}),
     },
     ...props,
   };
