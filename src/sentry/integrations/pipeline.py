3a31ffba9f037de4472dbeced2db4c1f3c6ea975
Jess MacQueen <jessmacqueen@gmail.com>
2018-08-03 14:34:25 -0700

    fix(integrations): Make sure status is updated in ensure_integration

--- src/sentry/integrations/pipeline.py
+++ src/sentry/integrations/pipeline.py
@@ -33,6 +33,7 @@ def ensure_integration(key, data):
     defaults = {
         'metadata': data.get('metadata', {}),
         'name': data.get('name', data['external_id']),
+        'status': ObjectStatus.VISIBLE,
     }
     integration, created = Integration.objects.get_or_create(
         provider=key,
