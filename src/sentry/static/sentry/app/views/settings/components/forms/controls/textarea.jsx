a9cf38a5f92809b38eb28405d9812da11b06faab
Billy Vong <billyvg@users.noreply.github.com>
2018-08-03 15:27:55 -0700

    tests(js): Fix TextArea react warnings (#9314)
    There's probably a more elegant way but I can't read our travis logs so *shrug*

--- src/sentry/static/sentry/app/views/settings/components/forms/controls/textarea.jsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/controls/textarea.jsx
@@ -5,22 +5,55 @@ import styled from 'react-emotion';
 
 import {inputStyles} from 'app/styles/input';
 
-const TextArea = styled(
-  p =>
-    p.autosize ? (
-      <TextareaAutosize rows={p.rows ? p.rows : 2} async={true} {...p} />
-    ) : (
-      <textarea {...p} />
-    )
-)`
+class TextareaOrAutosize extends React.Component {
+  static propTypes = {
+    /**
+     * Enable autosizing of the textarea.
+     */
+    autosize: PropTypes.bool,
+
+    /**
+     * Number of rows to start with if autosize
+     */
+    rows: PropTypes.number,
+
+    innerRef: PropTypes.func,
+  };
+
+  render() {
+    let {
+      autosize,
+      rows,
+      innerRef,
+      highlighted, // eslint-disable-line
+      field, // eslint-disable-line
+      multiline, // eslint-disable-line
+      getValue, // eslint-disable-line
+      setValue, // eslint-disable-line
+      error, // eslint-disable-line
+      initialData, // eslint-disable-line
+      getData, // eslint-disable-line
+      extraHelp, // eslint-disable-line
+      ...props
+    } = this.props;
+
+    if (autosize) {
+      return (
+        <TextareaAutosize
+          rows={rows ? rows : 2}
+          async={true}
+          innerRef={innerRef}
+          {...props}
+        />
+      );
+    }
+
+    return <textarea {...props} ref={innerRef} />;
+  }
+}
+
+const TextArea = styled(TextareaOrAutosize)`
   ${inputStyles};
 `;
-
-TextArea.propTypes = {
-  /**
-   * Enable autosizing of the textarea.
-   */
-  autosize: PropTypes.bool,
-};
 
 export default TextArea;
