getsentry/sentry

commit c0181dd63c5f17c2a582665ce661a04dd08ab765

feat(discover): Add NOT LIKE support for conditions (#9272)

src/sentry/static/sentry/app/views/organizationDiscover/conditions/utils.jsx
@@ -63,19 +63,17 @@ export function getExternal(internal, columns) {
   if (specialConditions.has(remaining)) {
     external[1] = remaining;
   } else {
-    const operatorMatch = remaining.match(/^([^\s]+).*$/);
-    const operatorValue = operatorMatch ? operatorMatch[1] : null;
-
-    if (new Set(CONDITION_OPERATORS).has(operatorValue)) {
-      external[1] = operatorValue;
-    }
+    CONDITION_OPERATORS.forEach(operator => {
+      if (remaining.startsWith(operator)) {
+        external[1] = operator;
+      }
+    });
   }
 
   // Validate value and convert to correct type
   if (external[0] && external[1] && !specialConditions.has(external[1])) {
-    const valuesMatch = internal.match(/^[a-zA-Z0-9_\.:-\[\]]+ [^\s]+ (.*)$/);
+    external[2] = internal.replace(`${external[0]} ${external[1]} `, '');
 
-    external[2] = valuesMatch ? valuesMatch[1] : null;
     const type = columns.find(({name}) => name === colValue).type;
 
     if (type === 'number') {

tests/js/spec/views/organizationDiscover/conditions/utils.spec.jsx
@@ -27,6 +27,10 @@ const conditionList = [
     internal: 'retention_days >= 0',
     external: ['retention_days', '>=', 0],
   },
+  {
+    internal: 'message NOT LIKE something%',
+    external: ['message', 'NOT LIKE', 'something%'],
+  },
 ];
 
 describe('Conditions', function() {

src/sentry/static/sentry/app/views/organizationDiscover/conditions/condition.jsx
@@ -60,7 +60,7 @@ export default class Condition extends React.Component {
     const column = this.props.columns.find(({name}) => name === colName);
     const colType = column ? column.type : 'string';
     const numberOnlyOperators = new Set(['>', '<', '>=', '<=']);
-    const stringOnlyOperators = new Set(['LIKE']);
+    const stringOnlyOperators = new Set(['LIKE', 'NOT LIKE']);
 
     return CONDITION_OPERATORS.filter(operator => {
       if (colType === 'number') {

src/sentry/static/sentry/app/views/organizationDiscover/data.jsx
@@ -79,6 +79,7 @@ const CONDITION_OPERATORS = [
   'IS NULL',
   'IS NOT NULL',
   'LIKE',
+  'NOT LIKE',
 ];
 
 const TOPK_COUNTS = [5, 10, 20, 50, 100];

tests/js/spec/views/organizationDiscover/conditions/condition.spec.jsx
@@ -41,7 +41,7 @@ describe('Condition', function() {
 
     it('renders operator options for string column', function() {
       const options = wrapper.instance().filterOptions([], 'col1');
-      expect(options).toHaveLength(5);
+      expect(options).toHaveLength(6);
       expect(options[0]).toEqual({value: 'col1 =', label: 'col1 ='});
     });
 

commit b7eb8bd11618cac73a34c652654a43225b8f13dc

fix(discover): Fix converting to external condition format
Fixes a bug introduced in #9272 which incorrectly converted an internal condition with the format 'message LIKE' to the external format ['message', 'LIKE', 'message LIKE'] instead of ['message', 'LIKE', null].

src/sentry/static/sentry/app/views/organizationDiscover/conditions/utils.jsx
@@ -72,7 +72,11 @@ export function getExternal(internal, columns) {
 
   // Validate value and convert to correct type
   if (external[0] && external[1] && !specialConditions.has(external[1])) {
-    external[2] = internal.replace(`${external[0]} ${external[1]} `, '');
+    const strStart = `${external[0]} ${external[1]} `;
+
+    if (internal.startsWith(strStart)) {
+      external[2] = internal.replace(strStart, '');
+    }
 
     const type = columns.find(({name}) => name === colValue).type;
 

tests/js/spec/views/organizationDiscover/conditions/utils.spec.jsx
@@ -31,6 +31,10 @@ const conditionList = [
     internal: 'message NOT LIKE something%',
     external: ['message', 'NOT LIKE', 'something%'],
   },
+  {
+    internal: 'message LIKE',
+    external: ['message', 'LIKE', null],
+  },
 ];
 
 describe('Conditions', function() {