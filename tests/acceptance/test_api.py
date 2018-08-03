c0c0acd9286c5c1baeae92c3cdeadfc484c1bc16
Billy Vong <billy@sentry.io>
2018-08-03 10:56:42 -0700

    tests(percy): Fix API Application snapshotting toast

--- tests/acceptance/test_api.py
+++ tests/acceptance/test_api.py
@@ -42,4 +42,5 @@ class ApiApplicationTest(AcceptanceTestCase):
 
         self.browser.click('[href="/settings/account/api/applications/"]')
         self.browser.wait_until_not('.loading')
+        self.browser.wait_until_not('.ref-toast')
         self.browser.snapshot('api applications - single application')
