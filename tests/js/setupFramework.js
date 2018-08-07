54db179e81dcbfb7d89a18eee798d4d074acd2b1
Billy Vong <billy@sentry.io>
2018-08-06 17:57:50 -0700

    wip hackweek

--- tests/js/setupFramework.js
+++ tests/js/setupFramework.js
@@ -1,137 +1,101 @@
 /* global process */
-// jest snapshot serializer for emotion
-import PercyClient from 'percy-client';
-import ReactDOM from 'react-dom';
-import Environment from 'percy-client/dist/environment';
-import slugify from 'slugify';
-import sprite from 'svg-sprite-loader/runtime/sprite.build';
+import slugify from '@sindresorhus/slugify';
 import puppeteer from 'puppeteer';
-const {toMatchImageSnapshot} = require('jest-image-snapshot');
+
+import React from 'react';
+import {mount} from 'enzyme';
+import App from 'app/views/app';
+import OrganizationDetails from 'app/views/organizationDetails';
+import OrganizationRoot from 'app/views/organizationRoot';
+
+// import ReactDOM from 'react-dom';
+// import sprite from 'svg-sprite-loader/runtime/sprite.build';
+// import {toMatchImageSnapshot} from 'jest-image-snapshot';
+// expect.extend({toMatchImageSnapshot});
 
 process.on('unhandledRejection', (reason, promise) => {
   // eslint-disable-next-line no-console
   console.error(reason);
 });
 
-function createPercyClient() {
-  const token = process.env.PERCY_TOKEN;
-  const apiUrl = process.env.PERCY_API;
-  const clientInfo = `percy-jest 0.1.0`;
-  return new PercyClient({token, apiUrl, clientInfo});
-}
-function parseMissingResources(response) {
-  return (
-    (response.body.data &&
-      response.body.data.relationships &&
-      response.body.data.relationships['missing-resources'] &&
-      response.body.data.relationships['missing-resources'].data) ||
-    []
-  );
-}
-function uploadMissingResources(percyClient, buildId, response, shaToResource) {
-  const missingResources = parseMissingResources(response);
-  console.log('missing resources', missingResources);
-  const promises = [];
-  if (missingResources.length > 0) {
-    for (const missingResource of missingResources) {
-      promises.push(
-        percyClient
-          .uploadResource(buildId, shaToResource[missingResource.id].content)
-          .then(() => {})
-          // eslint-disable-next-line no-console
-          .catch(err => console.log('[percy jest] uploadMissingResources', err))
-      );
-    }
-  }
-  return Promise.all(promises);
-}
-const percy = createPercyClient();
-const environment = new Environment(process.env);
+MockApiClient.addMockResponse({
+  url: '/organizations/',
+  body: [TestStubs.Organization()],
+});
+MockApiClient.addMockResponse({
+  url: '/organizations/org-slug/',
+  body: TestStubs.Organization(),
+});
+MockApiClient.addMockResponse({
+  url: '/internal/health/',
+  body: {},
+});
+const client = puppeteer.launch();
 
-async function testSnapshot({currentTestName, source}) {
-  const build = await percy.createBuild(environment.repo, {resources: []});
-  const buildId = build.body.data.id;
-  // console.log(build);
-  // console.log(percy);
-  // console.log(this.currentTestName);
-  try {
-    const rootResource = percy.makeResource({
-      resourceUrl: '/',
-      content: source,
-      isRoot: true,
-      mimetype: 'text/html',
-    });
-
-    const snapshot = await percy.createSnapshot(buildId, [rootResource], {
-      name: slugify(currentTestName),
-      // content:
-    });
-    console.log(snapshot.body.data);
-    const snapshotId = snapshot.body.data.id;
-    const shaToResource = {};
-    shaToResource[rootResource.sha] = rootResource;
-    await uploadMissingResources(percy, buildId, snapshot, shaToResource);
-    percy.finalizeSnapshot(snapshotId);
-  } catch (err) {
-    console.log(err.message);
-  }
-  let resp = await percy.finalizeBuild(buildId);
-  console.log(resp.body);
-  // console.log(received.getDOMNode()); console.log(document.documentElement.outerHTML);
-}
 expect.extend({
-  toSnapshot: function(received, argument) {
-    ReactDOM.render(received, document.body);
+  async toSnapshot(received, argument) {
+    // received = enzyme wrapper
+    // ReactDOM.render(received, document.body);
     // console.log(sprite.stringify());
     const cloned = document.documentElement.cloneNode(true);
-    console.log('cloned', cloned);
+    // console.log('cloned', cloned);
     const body = cloned.getElementsByTagName('body').item(0);
     body.innerHTML = received.html();
-    puppeteer.launch().then(async browser => {
-      const page = await browser.newPage();
-      page.setViewport({width: 1200, height: 600, deviceScaleFactor: 4});
-      await page.setContent(cloned.outerHTML);
-      const image = await page.screenshot({
-        path: `${this.currentTestName}.png`,
-        fullPage: true,
+    await client
+      .then(async browser => {
+        const page = await browser.newPage();
+        page.setViewport({width: 1200, height: 600, deviceScaleFactor: 4});
+        await page.setContent(cloned.outerHTML);
+        const fs = require('fs');
+        const css = fs
+          .readFileSync('./static/dist/sentry.css', 'utf8')
+          .replace(/[\r\n]+/g, '');
+        page.addStyleTag({
+          content: css,
+        });
+        console.log(this);
+        await page.screenshot({
+          path: `./.artifacts/jest/${slugify(this.currentTestName)}.png`,
+          fullPage: true,
+        });
+        // expect(image).toMatchImageSnapshot();
+        page.close();
+      })
+      .catch(err => {
+        console.error(err);
       });
-      expect(image).toMatchImageSnapshot();
-      await browser.close();
-    });
-    console.log(cloned.outerHTML);
+    // console.log(cloned.outerHTML);
     return {
       message: () => 'expected to save snapshot',
       pass: true,
     };
   },
-  toPercy: function(received, argument) {
-    // ReactDOM.render(received, document.body);
-    // console.log(sprite.stringify());
-    const cloned = document.documentElement.cloneNode(true);
-    const body = cloned.getElementsByTagName('body').item(0);
-    body.innerHTML = received.html();
-    puppeteer.launch().then(async browser => {
-      const page = await browser.newPage();
-      page.setViewport({width: 1200, height: 600, deviceScaleFactor: 4});
-      await page.setContent(cloned.outerHTML);
-      const image = await page.screenshot({
-        path: `${this.currentTestName}.png`,
-        fullPage: true,
-      });
-      expect(image).toMatchImageSnapshot();
-      await browser.close();
+  async toSnapshotWithShell(received, argument) {
+    MockApiClient.addMockResponse({
+      url: '/organizations/',
+      body: [TestStubs.Organization()],
     });
-    console.log(cloned.outerHTML);
-    // testSnapshot({
-    //   currentTestName: this.currentTestName,
-    //   source: cloned.outerHTML,
-    // });
-    // console.log(received.getDOMNode()); console.log(document.documentElement.outerHTML);
-    return {
-      message: () => 'expected to save to Percy',
-      pass: true,
-    };
+    MockApiClient.addMockResponse({
+      url: '/broadcasts/',
+      body: [TestStubs.Organization()],
+    });
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/',
+      body: TestStubs.Organization(),
+    });
+    MockApiClient.addMockResponse({
+      url: '/internal/health/',
+      body: {},
+    });
+    let wrapper = mount(
+      <App>
+        <OrganizationDetails params={{orgId: 'org-slug'}}>
+          <OrganizationRoot params={{orgId: 'org-slug'}}>{received}</OrganizationRoot>
+        </OrganizationDetails>, TestStubs.routerContext()
+        {received}
+      </App>,
+      TestStubs.routerContext()
+    );
+    return this.toSnapshot(wrapper, argument);
   },
 });
-
-expect.extend({toMatchImageSnapshot});
--- tests/js/spec/components/alertLink.spec.jsx
+++ tests/js/spec/components/alertLink.spec.jsx
@@ -1,7 +1,3 @@
-import theme from '../../../../src/sentry/static/sentry/app/utils/theme';
-
-import {ThemeProvider} from 'emotion-theming';
-
 import React from 'react';
 import {mount, shallow} from 'enzyme';
 import AlertLink from 'app/components/alertLink';
@@ -9,24 +5,22 @@ import AlertLink from 'app/components/alertLink';
 describe('AlertLink', function() {
   it('renders', function() {
     let wrapper = shallow(
-      <ThemeProvider theme={theme}>
-        <AlertLink to="/settings/accounts/notifications">
-          This is an external link button
-        </AlertLink>
-      </ThemeProvider>
+      <AlertLink to="/settings/accounts/notifications">
+        This is an external link button
+      </AlertLink>,
+      TestStubs.routerContext()
     );
     expect(wrapper).toMatchSnapshot();
   });
 
   it('renders with icon', function() {
     let wrapper = mount(
-      <ThemeProvider theme={theme}>
-        <AlertLink to="/settings/accounts/notifications" icon="icon-mail">
-          This is an external link button
-        </AlertLink>
-      </ThemeProvider>
+      <AlertLink to="/settings/accounts/notifications" icon="icon-mail">
+        This is an external link button
+      </AlertLink>,
+      TestStubs.routerContext()
     );
-    expect(wrapper).toPercy();
+    expect(wrapper).toSnapshot();
     expect(wrapper).toMatchSnapshot();
   });
 });
