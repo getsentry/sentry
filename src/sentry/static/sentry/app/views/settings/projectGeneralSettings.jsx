39b2651977a304fa255e61622f57b0f194ad1fc8
Jan Michael Auer <account@jauer.org>
2018-08-15 01:50:39 +0200

    feat(minidump): Optionally add minidumps as event attachments (#9269)
    * feat(settings): Add options to store minidumps
    
    * feat(minidump): Save minidumps as event attachments
    
    * feat(ui): Only show the crash report setting with feature
    
    * test: Update settings snapshots
    
    * ref: Do not explicitly set the size of event attachments
    
    * feat(cache): Add a raw flag to the cache
    
    * ref(minidump): Store attachments in cache first
    
    * ref(settings): Use features array instead of set
    
    * ref(attachments): Create a custom cache for attachments
    
    * ref(attachments): Added an indirection for proper cache configuration
    
    * feat(attachments): Compress attachments in the cache
    
    * ref: Use proper Redis client
    
    * fix: Do not fallback to non-clustered Redis

--- src/sentry/static/sentry/app/views/settings/projectGeneralSettings.jsx
+++ src/sentry/static/sentry/app/views/settings/projectGeneralSettings.jsx
@@ -268,12 +268,14 @@ class ProjectGeneralSettings extends AsyncView {
           <JsonForm
             {...jsonFormProps}
             title={t('Data Privacy')}
+            features={organization.features}
             fields={[
               fields.dataScrubber,
               fields.dataScrubberDefaults,
               fields.scrubIPAddresses,
               fields.sensitiveFields,
               fields.safeFields,
+              fields.storeCrashReports,
             ]}
           />
 
