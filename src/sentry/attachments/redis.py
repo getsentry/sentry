61e8c95baa0e4cd5a8889304481e886e659c0bba
Anton Ovchinnikov <anton@tonyo.info>
2018-08-14 18:06:44 +0300

    ref: Use proper Redis client

--- src/sentry/attachments/redis.py
+++ src/sentry/attachments/redis.py
@@ -8,11 +8,28 @@ sentry.attachments.redis
 
 from __future__ import absolute_import
 
+import logging
+
+from django.conf import settings
+
 from sentry.cache.redis import RedisCache
+from sentry.utils import redis
 
 from .base import BaseAttachmentCache
+
+logger = logging.getLogger(__name__)
 
 
 class RedisAttachmentCache(BaseAttachmentCache):
     def __init__(self, **options):
-        super(RedisAttachmentCache, self).__init__(RedisCache(**options), **options)
+        cluster_id = getattr(
+            settings,
+            'SENTRY_ATTACHMENTS_REDIS_CLUSTER',
+            'rc-short',
+        )
+        try:
+            cache = redis.redis_clusters.get(cluster_id)
+        except KeyError:
+            cache = RedisCache(**options)
+            logger.info('No redis cluster provided for attachments, using {!r}.'.format(cache))
+        super(RedisAttachmentCache, self).__init__(cache, **options)
