d363fcd74e560222fe2b3b262cb8847b4667fbeb
Billy Vong <billyvg@users.noreply.github.com>
2018-08-15 10:34:04 -0700

    feat(health): Add `interval` to health request action creator (#9364)
    * Also adds another test for requests w/o `tag`

--- src/sentry/static/sentry/app/actionCreators/health.jsx
+++ src/sentry/static/sentry/app/actionCreators/health.jsx
@@ -11,12 +11,23 @@ const BASE_URL = org => `/organizations/${org.slug}/health/`;
  * @param {Boolean} options.timeseries Should we group results by time period
  * @param {String[]} options.environments List of environments to query for
  * @param {String} options.period Time period to query for, in the format: <integer><units> where units are "d" or "h"
+ * @param {String} options.interval Time interval to group results in, in the format: <integer><units> where units are "d", "h", "m", "s"
  * @param {Boolean} options.includePrevious Should request also return reqsults for previous period?
  * @param {Number} options.topk Include topk results
  */
 export const doHealthRequest = (
   api,
-  {organization, projects, tag, environments, period, timeseries, includePrevious, topk}
+  {
+    organization,
+    projects,
+    tag,
+    environments,
+    period,
+    interval,
+    timeseries,
+    includePrevious,
+    topk,
+  }
 ) => {
   if (!api) return Promise.reject(new Error('API client not available'));
 
@@ -24,6 +35,7 @@ export const doHealthRequest = (
   const query = {
     tag,
     includePrevious,
+    interval,
     statsPeriod: period,
     project: projects,
     environment: environments,
