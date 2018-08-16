25061d219465770c9176069afd8f1ccecaf20a06
Lauryn Brown <lauryndbrown@gmail.com>
2018-08-16 16:17:08 -0700

    feat(vsts): VSTS Autocomplete link issue (#9398)
    * fixed tests
    
    * saving place.
    
    * Added Mer's recommendations.
    
    * Creating method from the client.
    
    * Added search to link issue.
    
    * Removed changes to tests
    
    * Removed Comment
    
    * removed repeat code
    
    * Instantiated Response.
    
    * Added 400 at the end of endpoint
    
    * Account name rather than instance

--- src/sentry/integrations/vsts/client.py
+++ src/sentry/integrations/vsts/client.py
@@ -27,6 +27,7 @@ class VstsApiPath(object):
     subscriptions = u'https://{account_name}/_apis/hooks/subscriptions'
     work_items = u'https://{account_name}/DefaultCollection/_apis/wit/workitems/{id}'
     work_items_create = u'https://{account_name}/{project}/_apis/wit/workitems/${type}'
+    work_item_search = u'https://{account_name}.almsearch.visualstudio.com/_apis/search/workitemsearchresults'
     work_item_states = u'https://{account_name}/{project}/_apis/wit/workitemtypes/{type}/states'
     users = u'https://{account_name}.vssps.visualstudio.com/_apis/graph/users'
 
@@ -264,4 +265,24 @@ class VstsApiClient(ApiClient, OAuth2RefreshMixin):
                 account_name=instance,
                 subscription_id=subscription_id,
             )
+        )
+
+    def search_issues(self, account_name, query=None):
+        return self.post(
+            VstsApiPath.work_item_search.format(
+                account_name=account_name,
+            ),
+            data={
+                'searchText': query,
+                '$top': 1000,
+                'filters': {
+                    'System.WorkItemType': [
+                        'Bug',
+                        'User Story',
+                        'Feature',
+                        'Task'
+                    ],
+                }
+            },
+            api_preview=True,
         )
--- src/sentry/integrations/vsts/urls.py
+++ src/sentry/integrations/vsts/urls.py
@@ -1,8 +1,13 @@
 from __future__ import absolute_import, print_function
 
 from django.conf.urls import patterns, url
+from .search import VstsSearchEndpoint
 from .webhooks import WorkItemWebhook
 urlpatterns = patterns(
     '',
     url(r'^issue-updated/$', WorkItemWebhook.as_view(), name='sentry-extensions-vsts-issue-updated'),
+    url(r'^search/(?P<organization_slug>[^\/]+)/(?P<integration_id>\d+)/$',
+        VstsSearchEndpoint.as_view(),
+        name='sentry-extensions-vsts-search'
+        ),
 )
