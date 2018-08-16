a6c8c36a9d661db96e4322ee041b5649e60f7e0f
denamwangi <dena@getsentry.com>
2018-08-16 14:13:36 -0700

    diff button being clicked actually

--- tests/acceptance/test_onboarding.py
+++ tests/acceptance/test_onboarding.py
@@ -43,8 +43,8 @@ class OrganizationOnboardingTest(AcceptanceTestCase):
         assert project.platform == 'javascript-angular'
 
         self.browser.snapshot(name='onboarding-configure-project')
-        self.browser.click('.btn-primary')
+        self.browser.click('button[aria-label="All done"]')
         self.browser.wait_until_not('.loading')
 
         assert self.browser.element_exists('.robot')
-        assert self.browser.element_exists('button[aria-label="All done"]')
+        assert self.browser.element_exists('.btn-primary')
