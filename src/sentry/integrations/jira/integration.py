3e31882dcfb330bbcdc2c047cc3a1de4db35988d
Jess MacQueen <jessmacqueen@gmail.com>
2018-08-17 12:10:36 -0700

    fix(jira): Hide fields that we want people to set in jira

--- src/sentry/integrations/jira/integration.py
+++ src/sentry/integrations/jira/integration.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import
 
 import logging
+import six
+
 from six.moves.urllib.parse import quote_plus
 
 from django.core.urlresolvers import reverse
@@ -394,13 +396,19 @@ class JiraIntegration(Integration, IssueSyncMixin):
 
         # TODO(jess): are we going to allow ignored fields?
         # ignored_fields = (self.get_option('ignored_fields', group.project) or '').split(',')
-        ignored_fields = set()
+
+        # hide sprint, epic link, parent and linked issues fields because they don't work
+        # since sprint and epic link are "custom" we need to search fo rthem by name
+        ignored_fields = set(
+            k for k, v in six.iteritems(issue_type_meta['fields']) if v['name'] in {'Sprint', 'Epic Link'}
+        )
+        ignored_fields.update(['parent', 'issuelinks'])
 
         # apply ordering to fields based on some known built-in Jira fields.
         # otherwise weird ordering occurs.
         anti_gravity = {"priority": -150, "fixVersions": -125, "components": -100, "security": -50}
 
-        dynamic_fields = issue_type_meta.get('fields').keys()
+        dynamic_fields = issue_type_meta['fields'].keys()
         dynamic_fields.sort(key=lambda f: anti_gravity.get(f) or 0)
         # build up some dynamic fields based on required shit.
         for field in dynamic_fields:
