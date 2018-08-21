212d382a9bc12dfbdf59af77bfae1f64dff2a7ca
Evan Purkhiser <evanpurkhiser@gmail.com>
2018-08-21 15:41:19 -0500

    feedback

--- src/sentry/static/sentry/app/components/commitLink.jsx
+++ src/sentry/static/sentry/app/components/commitLink.jsx
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'react-emotion';
 
 import {t} from 'app/locale';
+import {getShortVersion} from 'app/utils';
 import Button from 'app/components/buttons/button';
+import ExternalLink from 'app/components/externalLink';
 import InlineSvg from 'app/components/inlineSvg';
 
 // TODO(epurkhiser, jess): This should be moved into plugins.
@@ -10,17 +13,17 @@ const SUPPORTED_PROVIDERS = [
   {
     icon: 'icon-github',
     providerIds: ['github', 'integrations:github', 'integrations:github_enterprise'],
-    commitUrl: v => `${v.baseUrl}/commit/${v.commitId}`,
+    commitUrl: ({baseUrl, commitId}) => `${baseUrl}/commit/${commitId}`,
   },
   {
     icon: 'icon-bitbucket',
     providerIds: ['bitbucket', 'integrations:bitbucket'],
-    commitUrl: v => `${v.baseUrl}/commits/${v.commitId}`,
+    commitUrl: ({baseUrl, commitId}) => `${baseUrl}/commits/${commitId}`,
   },
   {
     icon: 'icon-vsts',
     providerIds: ['visualstudio', 'integrations:vsts'],
-    commitUrl: v => `${v.baseUrl}/commit/${v.commitId}`,
+    commitUrl: ({baseUrl, commitId}) => `${baseUrl}/commit/${commitId}`,
   },
 ];
 
@@ -29,7 +32,7 @@ function CommitLink({inline, commitId, repository}) {
     return <span>{t('Unknown Commit')}</span>;
   }
 
-  const shortId = commitId.slice(0, 7);
+  const shortId = getShortVersion(commitId);
 
   const providerData = SUPPORTED_PROVIDERS.find(provider =>
     provider.providerIds.includes(repository.provider.id)
@@ -49,14 +52,10 @@ function CommitLink({inline, commitId, repository}) {
       {shortId}
     </Button>
   ) : (
-    <a className="inline-commit" href={commitUrl} target="_blank">
-      <InlineSvg
-        src={providerData.icon}
-        style={{verticalAlign: 'text-top', marginTop: '2px'}}
-        size="14px"
-      />
+    <ExternalLink className="inline-commit" href={commitUrl}>
+      <CommitIcon src={providerData.icon} />
       {' ' + shortId}
-    </a>
+    </ExternalLink>
   );
 }
 
@@ -65,5 +64,12 @@ CommitLink.propTypes = {
   repository: PropTypes.object,
   inline: PropTypes.bool,
 };
+
+const CommitIcon = styled(p => (
+  <InlineSvg size="14px" src={p.src} className={p.className} />
+))`
+  vertical-align: text-top;
+  margin-top: 2px;
+`;
 
 export default CommitLink;
