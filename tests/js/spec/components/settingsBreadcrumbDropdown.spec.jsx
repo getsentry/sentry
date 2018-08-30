351f7dcb106a0d1d33e9700c7ee2551252dd0d33
Billy Vong <billy@sentry.io>
2018-08-30 11:56:07 -0700

    feat(ui): Allow use of react-virtualized in dropdown menus
    This will help in things like the Project selector for orgs with a large number of projects.

--- tests/js/spec/components/settingsBreadcrumbDropdown.spec.jsx
+++ tests/js/spec/components/settingsBreadcrumbDropdown.spec.jsx
@@ -3,6 +3,15 @@ import {mount} from 'enzyme';
 
 import BreadcrumbDropdown from 'app/views/settings/components/settingsBreadcrumb/breadcrumbDropdown';
 
+// Need to mock AutoSizer:/
+//
+const ActualReactVirtualized = require.requireActual('react-virtualized');
+jest.mock('react-virtualized', () => ({
+  ...ActualReactVirtualized,
+  AutoSizer: ({children}) => {
+    return children({width: 100, height: 100});
+  },
+}));
 jest.useFakeTimers();
 
 const CLOSE_DELAY = 0;
