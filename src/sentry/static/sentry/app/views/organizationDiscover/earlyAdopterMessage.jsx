2e437139fcd7c8590930a7233cf1c34c6711cae7
Ayesha Omarali <ayesha.omarali@gmail.com>
2018-09-04 14:13:35 -0700

    feat(ui): Moved Alpha Release Feedback to Bottom of Screen (#9581)

--- src/sentry/static/sentry/app/views/organizationDiscover/earlyAdopterMessage.jsx
+++ src/sentry/static/sentry/app/views/organizationDiscover/earlyAdopterMessage.jsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from 'react-emotion';
+import {Flex} from 'grid-emotion';
 
 import Alert from 'app/components/alert';
 import Link from 'app/components/link';
-import {tct} from 'app/locale';
+import {t} from 'app/locale';
 
 export default class EarlyAdopterMessage extends React.Component {
   render() {
     return (
-      <StyledAlert type="info" icon="icon-circle-exclamation">
-        {tct(
-          'This is an alpha release and may change in the future. Please email any feedback to [email:feedback-discover@sentry.io]. Thanks for being an early adopter!',
-          {
-            email: <Link href="mailto:feedback-discover@sentry.io" />,
-          }
-        )}
+      <StyledAlert type="info" icon="icon-labs">
+        <Flex justify="space-between">
+          {t('Sentry Discover is alpha software. Thanks for being an early adopter.')}
+          <Link href="mailto:feedback-discover@sentry.io">
+            {t('feedback-discover@sentry.io')}
+          </Link>
+        </Flex>
       </StyledAlert>
     );
   }
@@ -23,5 +24,4 @@ export default class EarlyAdopterMessage extends React.Component {
 const StyledAlert = styled(Alert)`
   padding: ${p => p.theme.grid}px ${p => p.theme.grid * 2}px;
   margin: 0;
-  border-radius: 0;
 `;
