559a1052934f0526905b7312c31d884d67b2cb48
Mark Story <mark@sentry.io>
2018-09-04 11:34:58 -0400

    feat(tests) Don't build celery app N times
    Rebuilding the CeleryApp instances multiple times creates bad
    side-effects.

--- tests/sentry/celery/test_app.py
+++ tests/sentry/celery/test_app.py
@@ -2,15 +2,13 @@ from __future__ import absolute_import
 
 import pytest
 from celery.beat import ScheduleEntry
-from sentry.celery import SentryCelery
+from sentry.celery import app
 from django.conf import settings
+
+app.loader.import_default_modules()
 
 
 @pytest.mark.parametrize('name,entry', settings.CELERYBEAT_SCHEDULE.items())
 def test_validate_celerybeat_schedule(name, entry):
-    app = SentryCelery('sentry')
-    app.config_from_object(settings)
-    app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-    app.loader.import_default_modules()
     entry = ScheduleEntry(name=name, app=app, **entry)
     assert entry.task in app.tasks
