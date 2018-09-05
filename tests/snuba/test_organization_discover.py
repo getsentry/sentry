45abac8dd66f15bfe5db0a4efc18a068b81bc1b3
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-05 16:50:54 -0700

    test: Fix discover tests (#9636)
    Need to add feature flag to tests since this is feature gated now

--- tests/snuba/test_organization_discover.py
+++ tests/snuba/test_organization_discover.py
@@ -63,14 +63,15 @@ class OrganizationDiscoverTest(APITestCase, SnubaTestCase):
         self.snuba_insert(events)
 
     def test(self):
-        url = reverse('sentry-api-0-organization-discover', args=[self.org.slug])
-        response = self.client.post(url, {
-            'projects': [self.project.id],
-            'fields': ['message', 'platform'],
-            'start': (datetime.now() - timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
-            'end': (datetime.now() + timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
-            'orderby': '-timestamp',
-        })
+        with self.feature('organizations:discover'):
+            url = reverse('sentry-api-0-organization-discover', args=[self.org.slug])
+            response = self.client.post(url, {
+                'projects': [self.project.id],
+                'fields': ['message', 'platform'],
+                'start': (datetime.now() - timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'end': (datetime.now() + timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'orderby': '-timestamp',
+            })
 
         assert response.status_code == 200, response.content
         assert len(response.data['data']) == 1
@@ -78,14 +79,15 @@ class OrganizationDiscoverTest(APITestCase, SnubaTestCase):
         assert response.data['data'][0]['platform'] == 'python'
 
     def test_array_join(self):
-        url = reverse('sentry-api-0-organization-discover', args=[self.org.slug])
-        response = self.client.post(url, {
-            'projects': [self.project.id],
-            'fields': ['message', 'exception_stacks.type'],
-            'start': (datetime.now() - timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
-            'end': (datetime.now() + timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
-            'orderby': '-timestamp',
-        })
+        with self.feature('organizations:discover'):
+            url = reverse('sentry-api-0-organization-discover', args=[self.org.slug])
+            response = self.client.post(url, {
+                'projects': [self.project.id],
+                'fields': ['message', 'exception_stacks.type'],
+                'start': (datetime.now() - timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'end': (datetime.now() + timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'orderby': '-timestamp',
+            })
         assert response.status_code == 200, response.content
         assert len(response.data['data']) == 1
         assert response.data['data'][0]['exception_stacks.type'] == 'ValidationError'
