f0287d8b964d34b04be00f72e31492abbc63b512
Jess MacQueen <jessmacqueen@gmail.com>
2018-09-05 10:09:57 -0700

    fix(github): Add gh specific feature flag to org repos endpoint

--- src/sentry/api/endpoints/organization_repositories.py
+++ src/sentry/api/endpoints/organization_repositories.py
@@ -100,9 +100,11 @@ class OrganizationRepositoriesEndpoint(OrganizationEndpoint):
             'organizations:bitbucket-integration', organization, actor=request.user)
         has_vsts = provider_id == 'integrations:vsts' and features.has(
             'organizations:vsts-integration', organization, actor=request.user)
+        has_github = provider_id == 'integrations:github' and features.has(
+            'organizations:github-apps', organization, actor=request.user)
 
         if features.has('organizations:internal-catchall', organization,
-                        actor=request.user) or has_ghe or has_bb or has_vsts:
+                        actor=request.user) or has_ghe or has_bb or has_vsts or has_github:
             if provider_id is not None and provider_id.startswith('integrations:'):
                 try:
                     provider_cls = bindings.get('integration-repository.provider').get(provider_id)
