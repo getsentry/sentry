6d07a234f93cd40998d421109e38b52bea40aa1b
Alex Hofsteede <alex@hofsteede.com>
2018-09-05 15:42:18 -0700

    fix: list of set, not set of list (#9634)

--- src/sentry/utils/snuba.py
+++ src/sentry/utils/snuba.py
@@ -245,7 +245,7 @@ def all_referenced_columns(conditions):
 
     # First flatten to remove the AND/OR nesting.
     flat_conditions = list(chain(*[[c] if is_condition(c) else c for c in conditions]))
-    return set(list(chain(*[columns_in_expr(c[0]) for c in flat_conditions])))
+    return list(set(chain(*[columns_in_expr(c[0]) for c in flat_conditions])))
 
 
 def columns_in_expr(expr):
