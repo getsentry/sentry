a5d7b9513ecf924f28fe4c7b9b46a7a947381f2f
James Cunningham <jtcunning@getsentry.com>
2018-09-06 16:29:14 -0700

    Don't return early when deleting children so that we don't lose our place in relations list.

--- src/sentry/deletions/base.py
+++ src/sentry/deletions/base.py
@@ -117,7 +117,7 @@ class BaseDeletionTask(object):
 
     def delete_children(self, relations):
         # Ideally this runs through the deletion manager
-        has_more = False
+        has_more = True
         for relation in relations:
             task = self.manager.get(
                 transaction_id=self.transaction_id,
@@ -126,9 +126,8 @@ class BaseDeletionTask(object):
                 task=relation.task,
                 **relation.params
             )
-            has_more = task.chunk()
-            if has_more:
-                return has_more
+            while has_more:
+                has_more = task.chunk()
         return has_more
 
     def mark_deletion_in_progress(self, instance_list):
