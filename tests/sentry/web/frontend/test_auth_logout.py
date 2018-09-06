a91e373e3428317788090979a55e994a4d171dcd
Mark Story <mark@sentry.io>
2018-09-06 10:24:09 -0400

    feat(auth) Use auth API for logout
    Update the ui to use the DELETE API endpoint to logout instead of the
    web frontend.

--- tests/sentry/web/frontend/test_auth_logout.py
+++ tests/sentry/web/frontend/test_auth_logout.py
@@ -12,6 +12,13 @@ class AuthLogoutTest(TestCase):
     def path(self):
         return reverse('sentry-logout')
 
+    def test_get_shows_page(self):
+        self.login_as(self.user)
+
+        resp = self.client.get(self.path)
+        assert resp.status_code == 200
+        assert self.client.session.keys(), 'Not logged out yet'
+
     def test_logs_user_out(self):
         self.login_as(self.user)
 
