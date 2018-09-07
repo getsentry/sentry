353e7f725ab4f033e7669592485cb961586f7929
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-07 12:28:40 -0700

    feat(discover): Implement download as CSV (#9659)

--- tests/js/spec/views/organizationDiscover/result/utils.spec.jsx
+++ tests/js/spec/views/organizationDiscover/result/utils.spec.jsx
@@ -5,6 +5,7 @@ import {
   getChartDataByDay,
   getDisplayValue,
   getDisplayText,
+  downloadAsCsv,
 } from 'app/views/organizationDiscover/result/utils';
 
 describe('Utils', function() {
@@ -181,6 +182,53 @@ describe('Utils', function() {
 
     testData.forEach(({input, expectedText}) => {
       expect(getDisplayText(input)).toBe(expectedText);
+    });
+  });
+
+  describe('downloadAsCsv()', function() {
+    let locationSpy;
+    beforeEach(function() {
+      locationSpy = jest.spyOn(window.location, 'assign').mockImplementation(_ => _);
+    });
+    afterEach(function() {
+      jest.restoreAllMocks();
+    });
+    it('handles raw data', function() {
+      const result = {
+        meta: [{name: 'message'}, {name: 'environment'}],
+        data: [
+          {message: 'test 1', environment: 'prod'},
+          {message: 'test 2', environment: 'test'},
+        ],
+      };
+      downloadAsCsv(result);
+      expect(locationSpy).toHaveBeenCalledWith(
+        expect.stringContaining(
+          encodeURI('message,environment\r\ntest 1,prod\r\ntest 2,test')
+        )
+      );
+    });
+    it('handles aggregations', function() {
+      const result = {
+        meta: [{type: 'UInt64', name: 'count'}],
+        data: [{count: 3}],
+      };
+      downloadAsCsv(result);
+      expect(locationSpy).toHaveBeenCalledWith(
+        expect.stringContaining(encodeURI('count\r\n3'))
+      );
+    });
+    it('quotes unsafe strings', function() {
+      const result = {
+        meta: [{name: 'message'}],
+        data: [{message: '=HYPERLINK(http://some-bad-website)'}],
+      };
+      downloadAsCsv(result);
+      expect(locationSpy).toHaveBeenCalledWith(
+        expect.stringContaining(
+          encodeURI("message\r\n'=HYPERLINK(http://some-bad-website)")
+        )
+      );
     });
   });
 });
