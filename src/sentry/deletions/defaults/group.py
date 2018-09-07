99dacaacecbec1a52e383e77356a65f74eb0aa0e
James Cunningham <cunninghamjt09@gmail.com>
2018-09-07 14:58:19 -0700

    Update GroupStatus.DELETION_IN_PROGRESS in bulk. (#9685)
    * Update GroupStatus.DELETION_IN_PROGRESS in bulk.
    
    * more style plz

--- src/sentry/deletions/defaults/group.py
+++ src/sentry/deletions/defaults/group.py
@@ -47,8 +47,12 @@ class GroupDeletionTask(ModelDeletionTask):
         return super(GroupDeletionTask, self).delete_instance(instance)
 
     def mark_deletion_in_progress(self, instance_list):
-        from sentry.models import GroupStatus
+        from sentry.models import Group, GroupStatus
 
-        for instance in instance_list:
-            if instance.status != GroupStatus.DELETION_IN_PROGRESS:
-                instance.update(status=GroupStatus.DELETION_IN_PROGRESS)
+        Group.objects.filter(
+            id__in=[i.id for i in instance_list],
+        ).exclude(
+            status=GroupStatus.DELETION_IN_PROGRESS,
+        ).update(
+            status=GroupStatus.DELETION_IN_PROGRESS,
+        )
