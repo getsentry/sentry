7a34fff7442c30564d45785ff81fff6323779b13
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-07 12:29:32 -0700

    feat(discover): Handle equality conditions on array fields (#9648)
    Use has() function when performing equality conditions on array fields

--- tests/snuba/test_organization_discover.py
+++ tests/snuba/test_organization_discover.py
@@ -91,3 +91,32 @@ class OrganizationDiscoverTest(APITestCase, SnubaTestCase):
         assert response.status_code == 200, response.content
         assert len(response.data['data']) == 1
         assert response.data['data'][0]['exception_stacks.type'] == 'ValidationError'
+
+    def test_array_condition_equals(self):
+        with self.feature('organizations:discover'):
+            url = reverse('sentry-api-0-organization-discover', args=[self.org.slug])
+            response = self.client.post(url, {
+                'projects': [self.project.id],
+                'conditions': [['exception_stacks.type', '=', 'ValidationError']],
+                'fields': ['message'],
+                'start': (datetime.now() - timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'end': (datetime.now() + timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'orderby': '-timestamp',
+            })
+        assert response.status_code == 200, response.content
+        assert len(response.data['data']) == 1
+
+    def test_array_condition_not_equals(self):
+        with self.feature('organizations:discover'):
+            url = reverse('sentry-api-0-organization-discover', args=[self.org.slug])
+            response = self.client.post(url, {
+                'projects': [self.project.id],
+                'conditions': [['exception_stacks.type', '!=', 'ValidationError']],
+                'fields': ['message'],
+                'start': (datetime.now() - timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'end': (datetime.now() + timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'orderby': '-timestamp',
+            })
+
+        assert response.status_code == 200, response.content
+        assert len(response.data['data']) == 0
