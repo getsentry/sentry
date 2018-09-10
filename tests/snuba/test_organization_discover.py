5655bc9b4a750208245e64775aa787bfd20ad3fa
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-10 14:24:42 -0700

    feat(discover): Cast boolean values in conditions to 0 / 1 (#9627)
    We need to cast boolean values in conditions to 0 / 1 for Snuba otherwise queries with these values will fail. This also fixes autocompletion on the UI

--- tests/snuba/test_organization_discover.py
+++ tests/snuba/test_organization_discover.py
@@ -78,6 +78,23 @@ class OrganizationDiscoverTest(APITestCase, SnubaTestCase):
         assert response.data['data'][0]['message'] == 'message!'
         assert response.data['data'][0]['platform'] == 'python'
 
+    def test_boolean_condition(self):
+        with self.feature('organizations:discover'):
+            url = reverse('sentry-api-0-organization-discover', args=[self.org.slug])
+            response = self.client.post(url, {
+                'projects': [self.project.id],
+                'fields': ['message', 'platform', 'exception_frames.in_app'],
+                'conditions': [['exception_frames.in_app', '=', True]],
+                'start': (datetime.now() - timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'end': (datetime.now() + timedelta(seconds=10)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'orderby': '-timestamp',
+            })
+
+        assert response.status_code == 200, response.content
+        assert len(response.data['data']) == 1
+        assert response.data['data'][0]['message'] == 'message!'
+        assert response.data['data'][0]['platform'] == 'python'
+
     def test_array_join(self):
         with self.feature('organizations:discover'):
             url = reverse('sentry-api-0-organization-discover', args=[self.org.slug])
