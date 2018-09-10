8daa25e1a6c0a366a74247a07c571911e4e59261
Brett Hoerner <brett@bretthoerner.com>
2018-09-10 11:03:45 -0500

    fix: Revert "Temporarily disable cleanup for tagstore v2 #7505" (#9073)

--- src/sentry/tagstore/v2/backend.py
+++ src/sentry/tagstore/v2/backend.py
@@ -83,8 +83,13 @@ class V2TagStorage(TagStorage):
         self.setup_receivers()
 
     def setup_cleanup(self):
-        # TODO: fix for sharded DB
-        pass
+        from sentry.runner.commands import cleanup
+
+        cleanup.EXTRA_BULK_QUERY_DELETES += [
+            (models.GroupTagValue, 'last_seen', None),
+            (models.TagValue, 'last_seen', None),
+            (models.EventTag, 'date_added', 'date_added', 50000),
+        ]
 
     def setup_deletions(self):
         from sentry.deletions import default_manager as deletion_manager
