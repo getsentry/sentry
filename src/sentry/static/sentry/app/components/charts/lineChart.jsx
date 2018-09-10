bf3188695c1f49ab6570396ba602bbee5ad8ba1b
Billy Vong <billyvg@users.noreply.github.com>
2018-09-10 14:03:11 -0700

    ref(charts): Move ECharts options as top level component props (#9652)
    This moves some properties out of the `options` object that is passed to ECharts, into a top level component property of BaseChart.
    
    This allows us to implement more default functionality into BaseChart
    (e.g. charting a line series for `previousPeriod`).

--- src/sentry/static/sentry/app/components/charts/lineChart.jsx
+++ src/sentry/static/sentry/app/components/charts/lineChart.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import BaseChart from './baseChart';
-import XAxis from './components/xAxis';
-import YAxis from './components/yAxis';
 import LineSeries from './series/lineSeries';
 
 export default class LineChart extends React.Component {
@@ -15,19 +13,12 @@ export default class LineChart extends React.Component {
     return (
       <BaseChart
         {...props}
-        options={{
-          xAxis: XAxis({
-            type: 'category',
-            boundaryGap: false,
-          }),
-          yAxis: YAxis({}),
-          series: series.map(s => {
-            return LineSeries({
-              name: s.seriesName,
-              data: s.data.map(({value, name}) => [name, value]),
-            });
-          }),
-        }}
+        series={series.map(s => {
+          return LineSeries({
+            name: s.seriesName,
+            data: s.data.map(({value, name}) => [name, value]),
+          });
+        })}
       />
     );
   }
