de51b78bac44cce7e66beb96b0237422f91e6aef
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-11 10:58:04 -0700

    feat(discover): Improve table layout (#9702)
    Improve table layout when columns do not fill full width by adding an additional column with remaining table width at the end.

---
+++ tests/js/spec/views/organizationDiscover/result/table.spec.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import {mount} from 'enzyme';
+
+import ResultTable from 'app/views/organizationDiscover/result/table';
+
+describe('ResultTable', function() {
+  let wrapper;
+  beforeEach(function() {
+    wrapper = mount(
+      <ResultTable
+        query={{aggregations: [], fields: []}}
+        data={{
+          data: [],
+          meta: [],
+        }}
+      />
+    );
+  });
+
+  it('getColumnWidths()', function() {
+    const mockCanvas = {
+      getContext: () => ({
+        measureText: () => ({width: 320}),
+      }),
+    };
+    wrapper.instance().canvas = mockCanvas;
+
+    wrapper.setProps({
+      data: {data: [{col1: 'foo'}], meta: [{name: 'col1'}]},
+      query: {fields: ['col1'], aggregations: []},
+    });
+    const widths = wrapper.instance().getColumnWidths(500);
+    expect(widths).toEqual([340, 40, 118]);
+  });
+});
