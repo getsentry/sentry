aaf81ba453d29dfdbf380608a2a2bbc6c0703d56
Adhiraj Somani <adhiraj@sentry.io>
2018-09-12 16:05:20 -0700

    fix(analytics): Change team joined event to organization joined
    Organization joined was what I was trying to capture.
    
    Fixes SENTRY-7MZ.

--- src/sentry/analytics/events/organization_joined.py
+++ src/sentry/analytics/events/organization_joined.py
@@ -3,14 +3,13 @@ from __future__ import absolute_import
 from sentry import analytics
 
 
-class TeamJoinedEvent(analytics.Event):
-    type = 'team.joined'
+class OrganizationJoinedEvent(analytics.Event):
+    type = 'organization.joined'
 
     attributes = (
         analytics.Attribute('user_id'),
         analytics.Attribute('organization_id'),
-        analytics.Attribute('team_id'),
     )
 
 
-analytics.register(TeamJoinedEvent)
+analytics.register(OrganizationJoinedEvent)
--- src/sentry/receivers/onboarding.py
+++ src/sentry/receivers/onboarding.py
@@ -181,7 +181,7 @@ def record_member_invited(member, user, **kwargs):
 
 
 @member_joined.connect(weak=False)
-def record_member_joined(member, **kwargs):
+def record_member_joined(member, organization, **kwargs):
     rows_affected, created = OrganizationOnboardingTask.objects.create_or_update(
         organization_id=member.organization_id,
         task=OnboardingTask.INVITE_MEMBER,
