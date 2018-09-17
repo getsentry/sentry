c014c4a6cabb5b906a00fb0e66bb0e9366192b01
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-17 14:02:10 -0700

    feat(discover): Add model for saved queries (#9772)

---
+++ tests/sentry/models/test_discoversavedquery.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import
+
+from sentry.testutils import TestCase
+from sentry.models import DiscoverSavedQuery, DiscoverSavedQueryProject
+
+
+class DiscoverSavedQueryTest(TestCase):
+    def test_create(self):
+        org = self.create_organization()
+        project_ids = [
+            self.create_project(organization=org).id,
+            self.create_project(organization=org).id
+        ]
+        query = {
+            'fields': ['test'],
+            'conditions': [],
+            'limit': 10
+        }
+
+        model = DiscoverSavedQuery.objects.create(organization=org, name="Test query", query=query)
+
+        model.add_projects(project_ids)
+
+        assert DiscoverSavedQuery.objects.get(id=model.id).query == query
+        assert sorted(
+            DiscoverSavedQueryProject.objects.all().values_list('project_id', flat=True)
+        ) == project_ids
