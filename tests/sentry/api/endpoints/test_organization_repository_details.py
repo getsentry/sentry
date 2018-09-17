02b607b2ae4a52ed4d966d6e2d2856a979e9302d
Meredith <meredith@getsentry.com>
2018-09-17 14:36:57 -0700

    add tests

--- tests/sentry/api/endpoints/test_organization_repository_details.py
+++ tests/sentry/api/endpoints/test_organization_repository_details.py
@@ -81,3 +81,77 @@ class OrganizationRepositoryDeleteTest(APITestCase):
             },
             countdown=3600,
         )
+
+    @patch('sentry.api.endpoints.organization_repository_details.get_transaction_id')
+    @patch('sentry.api.endpoints.organization_repository_details.delete_repository')
+    def test_delete_disabled_no_commits(self, mock_delete_repository, mock_get_transaction_id):
+        mock_get_transaction_id.return_value = '1'
+
+        self.login_as(user=self.user)
+
+        org = self.create_organization(owner=self.user, name='baz')
+        repo = Repository.objects.create(
+            name='example',
+            organization_id=org.id,
+            status=ObjectStatus.DISABLED,
+        )
+
+        url = reverse(
+            'sentry-api-0-organization-repository-details', args=[
+                org.slug,
+                repo.id,
+            ]
+        )
+        response = self.client.delete(url)
+        assert response.status_code == 202, (response.status_code, response.content)
+
+        repo = Repository.objects.get(id=repo.id)
+        assert repo.status == ObjectStatus.PENDING_DELETION
+
+        mock_delete_repository.apply_async.assert_called_with(
+            kwargs={
+                'object_id': repo.id,
+                'transaction_id': '1',
+                'actor_id': self.user.id,
+            },
+            countdown=0,
+        )
+
+    @patch('sentry.api.endpoints.organization_repository_details.get_transaction_id')
+    @patch('sentry.api.endpoints.organization_repository_details.delete_repository')
+    def test_delete_disabled_with_commits(self, mock_delete_repository, mock_get_transaction_id):
+        mock_get_transaction_id.return_value = '1'
+        self.login_as(user=self.user)
+
+        org = self.create_organization(owner=self.user, name='baz')
+        repo = Repository.objects.create(
+            name='example',
+            organization_id=org.id,
+            status=ObjectStatus.DISABLED,
+        )
+        Commit.objects.create(
+            repository_id=repo.id,
+            key='a' * 40,
+            organization_id=org.id,
+        )
+
+        url = reverse(
+            'sentry-api-0-organization-repository-details', args=[
+                org.slug,
+                repo.id,
+            ]
+        )
+        response = self.client.delete(url)
+
+        assert response.status_code == 202, (response.status_code, response.content)
+
+        repo = Repository.objects.get(id=repo.id)
+        assert repo.status == ObjectStatus.PENDING_DELETION
+        mock_delete_repository.apply_async.assert_called_with(
+            kwargs={
+                'object_id': repo.id,
+                'transaction_id': '1',
+                'actor_id': self.user.id,
+            },
+            countdown=3600,
+        )
