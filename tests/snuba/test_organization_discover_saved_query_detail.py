4c05bfd83b32274cc662815133e465cfcc1b5a16
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-24 11:44:51 -0700

    feat(discover): Add endpoint to get a saved query (#9831)

---
+++ tests/snuba/test_organization_discover_saved_query_detail.py
@@ -0,0 +1,59 @@
+from __future__ import absolute_import
+
+import six
+from sentry.testutils import APITestCase
+from django.core.urlresolvers import reverse
+from sentry.testutils import SnubaTestCase
+
+from sentry.models import DiscoverSavedQuery
+
+
+class OrganizationDiscoverSavedQueryDetailTest(APITestCase, SnubaTestCase):
+    def setUp(self):
+        super(OrganizationDiscoverSavedQueryDetailTest, self).setUp()
+        self.login_as(user=self.user)
+        self.org = self.create_organization(owner=self.user)
+        self.org_without_access = self.create_organization()
+        self.project_ids = [
+            self.create_project(organization=self.org).id,
+            self.create_project(organization=self.org).id
+        ]
+        query = {
+            'fields': ['test'],
+            'conditions': [],
+            'limit': 10
+        }
+
+        model = DiscoverSavedQuery.objects.create(
+            organization=self.org, name="Test query", query=query)
+
+        model.add_projects(self.project_ids)
+
+        self.query_id = model.id
+
+    def test_get(self):
+        with self.feature('organizations:discover'):
+            url = reverse(
+                'sentry-api-0-organization-discover-saved-query-detail',
+                args=[
+                    self.org.slug,
+                    self.query_id])
+            response = self.client.get(url)
+
+        assert response.status_code == 200, response.content
+        assert response.data['id'] == six.text_type(self.query_id)
+        assert response.data['projects'] == self.project_ids
+        assert response.data['fields'] == ['test']
+        assert response.data['conditions'] == []
+        assert response.data['limit'] == 10
+
+    def test_get_org_without_access(self):
+        with self.feature('organizations:discover'):
+            url = reverse(
+                'sentry-api-0-organization-discover-saved-query-detail',
+                args=[
+                    self.org_without_access.slug,
+                    self.query_id])
+            response = self.client.get(url)
+
+        assert response.status_code == 403, response.content
