bf73487c8dcd91c98e2f2f2ccfc8ffbe6e9d530a
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-25 14:16:08 -0700

    feat(discover): Move to tabs for sidebar (#9856)
    We need to move to a tab layout since we'll add another tab soon for saved search.
    
    Add some placeholder styles for saved search component.

--- src/sentry/static/sentry/app/components/navTabs.jsx
+++ src/sentry/static/sentry/app/components/navTabs.jsx
@@ -3,9 +3,11 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 
 function NavTabs(props) {
-  let {underlined, ...tabProps} = props;
-  let className = classnames('nav nav-tabs', {'border-bottom': underlined});
-  return <ul className={className} {...tabProps} />;
+  let {underlined, className, ...tabProps} = props;
+  let mergedClassName = classnames('nav nav-tabs', className, {
+    'border-bottom': underlined,
+  });
+  return <ul className={mergedClassName} {...tabProps} />;
 }
 
 NavTabs.propTypes = {
