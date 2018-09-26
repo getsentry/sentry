e75d97f7cfe1a9368d5b8c216ebb6c727a5bd1bf
Lyn Nagara <lyn.nagara@gmail.com>
2018-09-26 16:31:23 -0700

    add test cases for invalid scenarios

--- tests/snuba/test_organization_discover_saved_query_detail.py
+++ tests/snuba/test_organization_discover_saved_query_detail.py
@@ -31,6 +31,13 @@ class OrganizationDiscoverSavedQueryDetailTest(APITestCase, SnubaTestCase):
 
         self.query_id = model.id
 
+        invalid = DiscoverSavedQuery.objects.create(
+            organization=self.org_without_access, name="Query without access", query=query
+        )
+        invalid.set_projects(self.project_ids)
+
+        self.query_id_without_access = invalid.id
+
     def test_get(self):
         with self.feature('organizations:discover'):
             url = reverse(
@@ -84,6 +91,37 @@ class OrganizationDiscoverSavedQueryDetailTest(APITestCase, SnubaTestCase):
         assert response.data['conditions'] == []
         assert response.data['limit'] == 20
 
+    def test_put_query_without_access(self):
+        with self.feature('organizations:discover'):
+            url = reverse(
+                'sentry-api-0-organization-discover-saved-query-detail',
+                args=[
+                    self.org.slug,
+                    self.query_id_without_access])
+
+            response = self.client.put(url, {
+                'name': 'New query',
+                'projects': self.project_ids,
+                'range': '24h',
+            })
+
+            assert response.status_code == 404
+
+    def test_put_org_without_access(self):
+        with self.feature('organizations:discover'):
+            url = reverse(
+                'sentry-api-0-organization-discover-saved-query-detail',
+                args=[
+                    self.org_without_access.slug,
+                    self.query_id])
+            response = self.client.put(url, {
+                'name': 'New query',
+                'projects': self.project_ids,
+                'range': '24h',
+            })
+
+        assert response.status_code == 403, response.content
+
     def test_delete(self):
         with self.feature('organizations:discover'):
             url = reverse(
@@ -114,14 +152,25 @@ class OrganizationDiscoverSavedQueryDetailTest(APITestCase, SnubaTestCase):
 
         assert projects == []
 
-    def test_delete_invalid_query(self):
+    def test_delete_query_without_access(self):
         with self.feature('organizations:discover'):
             url = reverse(
                 'sentry-api-0-organization-discover-saved-query-detail',
                 args=[
                     self.org.slug,
-                    self.query_id + 1])
+                    self.query_id_without_access])
 
             response = self.client.delete(url)
 
             assert response.status_code == 404
+
+    def test_delete_org_without_access(self):
+        with self.feature('organizations:discover'):
+            url = reverse(
+                'sentry-api-0-organization-discover-saved-query-detail',
+                args=[
+                    self.org_without_access.slug,
+                    self.query_id])
+            response = self.client.delete(url)
+
+        assert response.status_code == 403, response.content
