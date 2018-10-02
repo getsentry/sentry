ed7a68609dc711e9cfcb2000c4b46c6800ddb046
Ayesha Omarali <ayesha.omarali@gmail.com>
2018-10-02 16:34:10 -0700

    feat(charts): Add Truncation Bool Prop for Chart Legend Labels (#9995)

--- src/sentry/static/sentry/app/components/charts/components/legend.jsx
+++ src/sentry/static/sentry/app/components/charts/components/legend.jsx
@@ -1,18 +1,20 @@
 import 'echarts/lib/component/legend';
 import 'echarts/lib/component/legendScroll';
 
-export default function Legend(props = {}) {
+export default function Legend({truncateLegend, ...props} = {}) {
   return {
     show: true,
     type: 'scroll',
     padding: 0,
-    formatter: function(name) {
-      if (name.length > 80) {
-        return name.substring(0, 80) + '...';
-      } else {
-        return name;
-      }
-    },
+    formatter: truncateLegend
+      ? function(name) {
+          if (name.length > 80) {
+            return name.substring(0, 80) + '...';
+          } else {
+            return name;
+          }
+        }
+      : null,
     ...props,
   };
 }
