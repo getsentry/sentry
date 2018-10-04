d981463a746f690bf0b0327f7a84786deb8d7868
Billy Vong <billy@sentry.io>
2018-10-03 18:25:50 -0700

    remove extra components

--- src/sentry/static/sentry/app/components/events/meta/dataContext.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import Annotated from 'app/components/events/meta/annotated';
-
-export default React.createContext(new Annotated(null, null));
--- src/sentry/static/sentry/app/components/events/meta/pathShape.jsx
+++
@@ -1,7 +0,0 @@
-import PropTypes from 'prop-types';
-
-export default PropTypes.oneOfType([
-  PropTypes.string,
-  PropTypes.number,
-  PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.string, PropTypes.number])),
-]);
