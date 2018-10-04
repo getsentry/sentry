e6fb5aca6433c71968402d22d14074ca35b10022
alex-hofsteede <alex@hofsteede.com>
2018-10-04 13:49:43 -0700

    ref(snuba): Refactor project events query to use snuba
    Replace the existing django queryset implementation with a snuba query.
    This also involves creating a SnubaEvent/SnubaEventSerializer to turn
    snuba result rows into recognizable API response objects, as well as a
    custom SnubaOffsetPaginator that allows paging through the result set
    using the offset/limit technique.

--- tests/snuba/api/endpoints/test_project_events.py
+++ tests/snuba/api/endpoints/test_project_events.py
@@ -1,22 +1,20 @@
 from __future__ import absolute_import
 
-import six
-
 from datetime import timedelta
 from django.utils import timezone
 from django.core.urlresolvers import reverse
 
-from sentry.testutils import APITestCase
+from sentry.testutils import APITestCase, SnubaTestCase
 
 
-class ProjectEventsTest(APITestCase):
+class ProjectEventsTest(APITestCase, SnubaTestCase):
     def test_simple(self):
         self.login_as(user=self.user)
 
         project = self.create_project()
         group = self.create_group(project=project)
-        event_1 = self.create_event('a' * 32, group=group)
-        event_2 = self.create_event('b' * 32, group=group)
+        event_1 = self.create_event('a' * 32, group=group, datetime=timezone.now() - timedelta(minutes=1))
+        event_2 = self.create_event('b' * 32, group=group, datetime=timezone.now() - timedelta(minutes=1))
 
         url = reverse(
             'sentry-api-0-project-events',
@@ -29,12 +27,34 @@ class ProjectEventsTest(APITestCase):
 
         assert response.status_code == 200, response.content
         assert len(response.data) == 2
-        assert sorted(map(lambda x: x['id'], response.data)) == sorted(
+        assert sorted(map(lambda x: x['eventID'], response.data)) == sorted(
             [
-                six.text_type(event_1.id),
-                six.text_type(event_2.id),
+                event_1.event_id,
+                event_2.event_id,
             ]
         )
+
+    def test_message_search(self):
+        self.login_as(user=self.user)
+
+        project = self.create_project()
+        group = self.create_group(project=project)
+        self.create_event('x' * 32, group=group, message="how to make fast", datetime=timezone.now() - timedelta(minutes=1))
+        event_2 = self.create_event('y' * 32, group=group, message="delet the data", datetime=timezone.now() - timedelta(minutes=1))
+
+        url = reverse(
+            'sentry-api-0-project-events',
+            kwargs={
+                'organization_slug': project.organization.slug,
+                'project_slug': project.slug,
+            }
+        )
+        response = self.client.get(url, {'query': 'delet'}, format='json')
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 1
+        assert response.data[0]['eventID'] == event_2.event_id
+        assert response.data[0]['message'] == 'delet the data'
 
     def test_filters_based_on_retention(self):
         self.login_as(user=self.user)
@@ -61,8 +81,4 @@ class ProjectEventsTest(APITestCase):
 
         assert response.status_code == 200, response.content
         assert len(response.data) == 1
-        assert sorted(map(lambda x: x['id'], response.data)) == sorted(
-            [
-                six.text_type(event_2.id),
-            ]
-        )
+        assert response.data[0]['eventID'] == event_2.event_id
