e3aad92c03af9b6c15d42b16de947bf66ce2d424
Ayesha Omarali <ayesha.omarali@gmail.com>
2018-10-04 14:06:05 -0700

    feat(discover): Add Boolean Support & Default Length for Echarts Truncation (#10024)

--- src/sentry/static/sentry/app/components/charts/components/legend.jsx
+++ src/sentry/static/sentry/app/components/charts/components/legend.jsx
@@ -1,15 +1,20 @@
 import 'echarts/lib/component/legend';
 import 'echarts/lib/component/legendScroll';
 
+const DEFAULT_TRUNCATE_LENGTH = 80;
+
 export default function Legend({truncate, ...props} = {}) {
+  let truncateLength =
+    truncate && typeof truncate === 'number' ? truncate : DEFAULT_TRUNCATE_LENGTH;
+
   return {
     show: true,
     type: 'scroll',
     padding: 0,
     formatter: truncate
       ? function(name) {
-          if (name.length > truncate) {
-            return name.substring(0, truncate) + '...';
+          if (name.length > truncateLength) {
+            return name.substring(0, truncateLength) + '...';
           } else {
             return name;
           }
