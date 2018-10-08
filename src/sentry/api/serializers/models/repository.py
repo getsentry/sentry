46c0da73b1916a3c3fbca5fb9b1766d44e1b3b26
MeredithAnya <meredith.a.heller@gmail.com>
2018-10-08 16:14:06 -0700

    fix: integration_id not repo id (#10042)

--- src/sentry/api/serializers/models/repository.py
+++ src/sentry/api/serializers/models/repository.py
@@ -11,7 +11,7 @@ class RepositorySerializer(Serializer):
     def serialize(self, obj, attrs, user):
         integration_id = None
         if obj.integration_id:
-            integration_id = six.text_type(obj.id)
+            integration_id = six.text_type(obj.integration_id)
         if obj.provider:
             provider = {
                 'id': obj.provider,
