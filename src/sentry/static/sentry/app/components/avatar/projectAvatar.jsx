6b7fb24cd8eb77b192ec0513469ba662cd6ad1b9
Billy Vong <billyvg@users.noreply.github.com>
2018-10-08 16:23:12 -0700

    Update projectAvatar.jsx

--- src/sentry/static/sentry/app/components/avatar/projectAvatar.jsx
+++ src/sentry/static/sentry/app/components/avatar/projectAvatar.jsx
@@ -11,10 +11,15 @@ class ProjectAvatar extends React.Component {
   };
 
   getPlatforms = project => {
+    // `platforms` is a list of platforms that come from events received in the project (in a certain timeframe)
+    // i.e. if you haven't received recent events with a platform, it could be an empty array.
     if (project && project.platforms && project.platforms.length > 0) {
       return project.platforms;
     }
 
+    // `platform` is a user selectable option that is performed during the onboarding process. The reason why this
+    // is not the default is because there currently is no way to update it. Fallback to this if project does not
+    // have recent events with a platform.
     if (project && project.platform) {
       return [project.platform];
     }
