e3cebae8eda885dddafe1bbe5d5d2cdaeff902f4
Billy Vong <billyvg@users.noreply.github.com>
2018-10-09 11:25:12 -0700

    feat(ui): Fallback to onboarding project for project avatars (ISSUE-121) (#10028)
    Attempt to get platforms based on recent events, but fallback to platform selected during onboarding if events do not exist

--- src/sentry/static/sentry/app/components/avatar/projectAvatar.jsx
+++ src/sentry/static/sentry/app/components/avatar/projectAvatar.jsx
@@ -10,10 +10,27 @@ class ProjectAvatar extends React.Component {
     ...BaseAvatar.propTypes,
   };
 
+  getPlatforms = project => {
+    // `platforms` is a list of platforms that come from events received in the project (in a certain timeframe)
+    // i.e. if you haven't received recent events with a platform, it could be an empty array.
+    if (project && project.platforms && project.platforms.length > 0) {
+      return project.platforms;
+    }
+
+    // `platform` is a user selectable option that is performed during the onboarding process. The reason why this
+    // is not the default is because there currently is no way to update it. Fallback to this if project does not
+    // have recent events with a platform.
+    if (project && project.platform) {
+      return [project.platform];
+    }
+
+    return [];
+  };
+
   render() {
     let {project, ...props} = this.props;
 
-    return <PlatformList platforms={(project && project.platforms) || []} {...props} />;
+    return <PlatformList platforms={this.getPlatforms(project)} {...props} />;
   }
 }
 export default ProjectAvatar;
