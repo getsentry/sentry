e5d9df004fa7904ec1c38af9b0139d1cfd02521f
Billy Vong <billy@sentry.io>
2018-10-10 14:44:54 -0700

    wip: multi project selector

--- src/sentry/static/sentry/app/components/autoComplete.jsx
+++ src/sentry/static/sentry/app/components/autoComplete.jsx
@@ -66,7 +66,14 @@ class AutoComplete extends React.Component {
     this.items = new Map();
   }
 
-  componentWillReceiveProps() {
+  componentWillReceiveProps(nextProps, nextState) {
+    // TODO(billy): Write a test for this
+    // If we do NOT want to close on select, then we should not reset highlight state
+    // when we select an item (when we select an item, `this.state.selectedItem` changes)
+    if (!nextProps.closeOnSelect && this.state.selectedItem !== nextState.selectedItem) {
+      return;
+    }
+
     this.resetHighlightState();
   }
 
--- src/sentry/static/sentry/app/components/organizations/multipleProjectSelector.jsx
+++ src/sentry/static/sentry/app/components/organizations/multipleProjectSelector.jsx
@@ -1,9 +1,13 @@
+import {Flex} from 'grid-emotion';
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'react-emotion';
+import styled, {cx} from 'react-emotion';
 
 import {t} from 'app/locale';
+import Button from 'app/components/button';
 import ProjectSelector from 'app/components/projectSelector';
+import TextOverflow from 'app/components/textOverflow';
+import space from 'app/styles/space';
 
 import HeaderItem from './headerItem';
 
@@ -22,15 +26,21 @@ export default class MultipleProjectSelector extends React.Component {
 
   constructor() {
     super();
+    this.state = {
+      hasChanges: false,
+    };
   }
 
-  onUpdate = () => {
+  handleUpdate = actions => {
     this.props.onUpdate();
+    actions.close();
+    this.setState({hasChanges: false});
   };
 
   handleMultiSelect = (selected, checked, e) => {
     const {onChange} = this.props;
     onChange(selected.map(({id}) => parseInt(id, 10)));
+    this.setState({hasChanges: true});
   };
 
   render() {
@@ -46,28 +56,72 @@ export default class MultipleProjectSelector extends React.Component {
         <ProjectSelector
           {...this.props}
           multi
+          multiOnly
+          showUpdate
           selectedProjects={selected}
           projects={projects}
           onSelect={this.handleMultiSelect}
           onMultiSelect={this.handleMultiSelect}
+          menuFooter={({actions}) => {
+            if (!this.state.hasChanges && selected.length === 0) {
+              return null;
+            }
+
+            return (
+              <Footer>
+                <Button
+                  priority="primary"
+                  tabIndex={1}
+                  size="small"
+                  type="button"
+                  onClick={this.handleUpdate.bind(this, actions)}
+                >
+                  {t('Update')}
+                </Button>
+
+                {selected.length > 0 && (
+                  <Button
+                    size="small"
+                    type="button"
+                    onClick={this.props.onChange.bind(this, [])}
+                  >
+                    {t('Clear')}
+                  </Button>
+                )}
+              </Footer>
+            );
+          }}
         >
-          {({getActorProps, selectedItem, activeProject, selectedProjects}) => (
-            <React.Fragment>
-              <Title {...getActorProps()}>
-                {selectedProjects.length
-                  ? selectedProjects.map(({slug}) => slug).join(', ')
-                  : t('None selected, using all')}
-              </Title>
-              <i className="icon-arrow-down" />
-            </React.Fragment>
-          )}
+          {({getActorProps, selectedItem, activeProject, selectedProjects}) => {
+            const hasSelected = !!selectedProjects.length;
+            const title = hasSelected
+              ? selectedProjects.map(({slug}) => slug).join(', ')
+              : t('None selected, using all');
+            return (
+              <React.Fragment>
+                <Flex {...getActorProps()}>
+                  <Title>{title}</Title>
+                  <ArrowDown />
+                </Flex>
+              </React.Fragment>
+            );
+          }}
         </ProjectSelector>
       </HeaderItem>
     );
   }
 }
+const ArrowDown = styled(({className, ...props}) => (
+  <i className={cx('icon-arrow-down', className)} {...props} />
+))`
+  margin-left: ${space(0.5)};
+`;
 
-const Title = styled.span`
-  padding-right: 40px;
-  max-width: 250px;
+const Title = styled(TextOverflow)`
+  width: 240px;
+`;
+
+const Footer = styled(Flex)`
+  justify-content: space-between;
+  padding: ${space(0.5)} 0;
 `;
