e1ec756c202246773085adcac54f3594cbf5f9ac
Jess MacQueen <jessmacqueen@gmail.com>
2018-10-11 12:10:15 -0700

    ref(integrations): Remove integration flags that are defaulted to on

--- src/sentry/api/endpoints/organization_config_integrations.py
+++ src/sentry/api/endpoints/organization_config_integrations.py
@@ -12,39 +12,12 @@ from django.conf import settings
 
 class OrganizationConfigIntegrationsEndpoint(OrganizationEndpoint):
     def get(self, request, organization):
-        has_bb = features.has('organizations:bitbucket-integration',
-                              organization,
-                              actor=request.user)
-        has_ghe = features.has('organizations:github-enterprise',
-                               organization,
-                               actor=request.user)
         has_catchall = features.has('organizations:internal-catchall',
                                     organization,
                                     actor=request.user)
-        has_github_apps = features.has('organizations:github-apps',
-                                       organization,
-                                       actor=request.user)
-        has_jira = features.has('organizations:jira-integration',
-                                organization,
-                                actor=request.user)
-        has_vsts = features.has('organizations:vsts-integration',
-                                organization,
-                                actor=request.user)
-
         providers = []
         for provider in integrations.all():
-            internal_integrations = {i for i in settings.SENTRY_INTERNAL_INTEGRATIONS}
-            if has_bb:
-                internal_integrations.remove('bitbucket')
-            if has_ghe:
-                internal_integrations.remove('github_enterprise')
-            if has_github_apps:
-                internal_integrations.remove('github')
-            if has_jira:
-                internal_integrations.remove('jira')
-            if has_vsts:
-                internal_integrations.remove('vsts')
-            if not has_catchall and provider.key in internal_integrations:
+            if not has_catchall and provider.key in settings.SENTRY_INTERNAL_INTEGRATIONS:
                 continue
 
             providers.append(provider)
