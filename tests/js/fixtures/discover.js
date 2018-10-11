66baa5f01e9d37ae3ad78cb2c8ad709b5edadd33
Lyn Nagara <lyn.nagara@gmail.com>
2018-10-11 11:37:52 -0700

    test(discover): Add tests for result component (#10070)

---
+++ tests/js/fixtures/discover.js
@@ -0,0 +1,9 @@
+export function DiscoverSavedQuery(params) {
+  return {
+    id: '1',
+    name: 'Saved query #1',
+    dateCreated: new Date().toString(),
+    dateUpdated: new Date().toString(),
+    fields: ['test'],
+  };
+}
