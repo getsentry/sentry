getsentry/sentry

commit 36cf8f8b040d54ed852343c318a1e4f706a104fe

fix(environments): Fix environment filter in group events page (#10115)
Fixes ISSUE-169

src/sentry/static/sentry/app/views/groupEvents.jsx
@@ -17,7 +17,7 @@ import Pagination from 'app/components/pagination';
 import SearchBar from 'app/components/searchBar';
 import SentryTypes from 'app/sentryTypes';
 import parseApiError from 'app/utils/parseApiError';
-import withEnvironment from 'app/utils/withEnvironment';
+import withEnvironmentInQueryString from 'app/utils/withEnvironmentInQueryString';
 
 const GroupEvents = createReactClass({
   displayName: 'GroupEvents',
@@ -94,9 +94,7 @@ const GroupEvents = createReactClass({
   },
 
   handleSearch(query) {
-    let targetQueryParams = {};
-    if (query !== '') targetQueryParams.query = query;
-
+    let targetQueryParams = {...this.props.location.query, query};
     let {groupId, orgId, projectId} = this.props.params;
     browserHistory.push({
       pathname: `/${orgId}/${projectId}/issues/${groupId}/events/`,
@@ -208,4 +206,4 @@ const GroupEvents = createReactClass({
 });
 
 export {GroupEvents}; // For tests
-export default withEnvironment(GroupEvents);
+export default withEnvironmentInQueryString(GroupEvents);

commit 12d8cca0ce29839c20e57f7b3cbd6170a9150065

fix(events): Fixes a bug in rendering events by environments per group
Fixes a bug introduced in #10115 causing All Environments to display no
result, and a potential infinite loop to occur when toggling
environments on this page.

Closes ISSUE-177

src/sentry/static/sentry/app/views/groupEvents.jsx
@@ -3,7 +3,11 @@ import React from 'react';
 import createReactClass from 'create-react-class';
 
 import {Panel, PanelBody} from 'app/components/panels';
-import {getQueryEnvironment, getQueryStringWithEnvironment} from 'app/utils/queryString';
+import {
+  getQueryEnvironment,
+  getQueryStringWithEnvironment,
+  getQueryStringWithoutEnvironment,
+} from 'app/utils/queryString';
 import {setActiveEnvironment} from 'app/actionCreators/environments';
 import {t, tct} from 'app/locale';
 import ApiMixin from 'app/mixins/apiMixin';
@@ -94,7 +98,9 @@ const GroupEvents = createReactClass({
   },
 
   handleSearch(query) {
-    let targetQueryParams = {...this.props.location.query, query};
+    query = getQueryStringWithoutEnvironment(query);
+    let targetQueryParams = {...this.props.location.query};
+    if (query !== '') targetQueryParams.query = query;
     let {groupId, orgId, projectId} = this.props.params;
     browserHistory.push({
       pathname: `/${orgId}/${projectId}/issues/${groupId}/events/`,