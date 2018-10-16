30e007e59e87a7064adb93650cd6a003528dd556
Jess MacQueen <jessmacqueen@gmail.com>
2018-10-15 18:11:11 -0700

    move getting dates from params into external function

--- src/sentry/api/endpoints/organization_events.py
+++ src/sentry/api/endpoints/organization_events.py
@@ -19,9 +19,42 @@ from sentry.search.utils import parse_datetime_string, InvalidQuery
 from sentry.utils.snuba import raw_query
 
 
+MIN_STATS_PERIOD = timedelta(hours=1)
+MAX_STATS_PERIOD = timedelta(days=90)
+
+
+class InvalidParams(Exception):
+    pass
+
+
+def get_date_range_from_params(params):
+    # Returns (start, end) or raises an `InvalidParams` exception
+    now = timezone.now()
+
+    end = now
+    start = now - MAX_STATS_PERIOD
+
+    stats_period = params.get('statsPeriod')
+    if stats_period is not None:
+        stats_period = parse_stats_period(params['statsPeriod'])
+        if stats_period is None or stats_period < MIN_STATS_PERIOD or stats_period >= MAX_STATS_PERIOD:
+            raise InvalidParams('Invalid statsPeriod')
+        start = now - stats_period
+    elif params.get('start') or params.get('end'):
+        if not all([params.get('start'), params.get('end')]):
+            raise InvalidParams('start and end are both required')
+        try:
+            start = parse_datetime_string(params['start'])
+            end = parse_datetime_string(params['end'])
+        except InvalidQuery as exc:
+            raise InvalidParams(exc.message)
+        if start > end:
+            raise InvalidParams('start must be before end')
+
+    return (start, end)
+
+
 class OrganizationEventsEndpoint(OrganizationEndpoint):
-    MIN_STATS_PERIOD = timedelta(hours=1)
-    MAX_STATS_PERIOD = timedelta(days=90)
 
     def get_project_ids(self, request, organization):
         project_ids = set(map(int, request.GET.getlist('project')))
@@ -65,27 +98,10 @@ class OrganizationEventsEndpoint(OrganizationEndpoint):
             conditions.append(
                 [['positionCaseInsensitive', ['message', "'%s'" % (query,)]], '!=', 0])
 
-        now = timezone.now()
-
-        end = now
-        start = now - self.MAX_STATS_PERIOD
-
-        stats_period = request.GET.get('statsPeriod')
-        if stats_period is not None:
-            stats_period = parse_stats_period(request.GET['statsPeriod'])
-            if stats_period is None or stats_period < self.MIN_STATS_PERIOD or stats_period >= self.MAX_STATS_PERIOD:
-                return Response({'detail': 'Invalid statsPeriod'}, status=400)
-            start = now - stats_period
-        elif request.GET.get('start') or request.GET.get('end'):
-            if not all([request.GET.get('start'), request.GET.get('end')]):
-                return Response({'detail': 'start and end are both required'}, status=400)
-            try:
-                start = parse_datetime_string(request.GET['start'])
-                end = parse_datetime_string(request.GET['end'])
-            except InvalidQuery as exc:
-                return Response({'detail': exc.message}, status=400)
-            if start > end:
-                return Response({'detail': 'start must be before end'}, status=400)
+        try:
+            start, end = get_date_range_from_params(request.GET)
+        except InvalidParams as exc:
+            return Response({'detail': exc.message}, status=400)
 
         try:
             project_ids = self.get_project_ids(request, organization)
