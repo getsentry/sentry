60a254c08ddc69e1edf453a5935db6373e559b80
denamwangi <dena@getsentry.com>
2018-10-16 17:28:29 -0700

    add analytics

--- src/sentry/static/sentry/app/views/projectReleases/releaseLanding.jsx
+++ src/sentry/static/sentry/app/views/projectReleases/releaseLanding.jsx
@@ -10,7 +10,9 @@ import {
   Contributors,
 } from 'sentry-dreamy-components';
 
+import {analytics} from 'app/utils/analytics';
 import ReleaseLandingCard from 'app/views/projectReleases/releaseLandingCard';
+import SentryTypes from 'app/sentryTypes';
 import withApi from 'app/utils/withApi';
 
 const StyledSuggestedAssignees = styled(SuggestedAssignees)`
@@ -57,6 +59,11 @@ const cards = [
 
 const ReleaseLanding = withApi(
   class ReleaseLanding extends React.Component {
+    static contextTypes = {
+      organization: SentryTypes.Organization,
+      project: SentryTypes.Project,
+    };
+
     constructor(props) {
       super(props);
       this.state = {
@@ -66,11 +73,19 @@ const ReleaseLanding = withApi(
 
     handleClick = () => {
       let {stepId} = this.state;
+      let {organization, project} = this.context;
 
+      let title = cards[stepId].title;
       if (stepId >= cards.length - 1) return;
       this.setState(state => ({
         stepId: state.stepId + 1,
       }));
+      analytics('releases.landing_card_clicked', {
+        org_id: parseInt(organization.id, 10),
+        project_id: parseInt(project.id, 10),
+        step_id: stepId,
+        step_title: title,
+      });
     };
 
     getCard = stepId => {
