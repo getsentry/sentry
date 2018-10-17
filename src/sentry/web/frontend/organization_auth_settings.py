9f53622c242feefeb2f1d10b62edbaf03c4e0470
David Cramer <dcramer@gmail.com>
2018-10-16 17:16:38 -0700

    fix(auth): Redirect to UI endpoint on SSO disable

--- src/sentry/web/frontend/organization_auth_settings.py
+++ src/sentry/web/frontend/organization_auth_settings.py
@@ -92,7 +92,7 @@ class OrganizationAuthSettingsView(OrganizationView):
                     OK_PROVIDER_DISABLED,
                 )
 
-                next_uri = reverse('sentry-organization-auth-settings', args=[organization.slug])
+                next_uri = u'/settings/{}/auth/'.format(organization.slug)
                 return self.redirect(next_uri)
             elif op == 'reinvite':
                 email_missing_links.delay(organization.id, request.user.id, provider.key)
