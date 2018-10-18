34a6c4d6eba3172820379418abbdff0e46918987
Brett Hoerner <brett@bretthoerner.com>
2018-10-18 17:01:04 -0500

    fix(unmerge): Handle events that don't have a release (#10135)
     Fixes SENTRY-6C6

--- src/sentry/tasks/unmerge.py
+++ src/sentry/tasks/unmerge.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import
 
 import logging
-from collections import defaultdict
+from collections import defaultdict, OrderedDict
 
 from django.db import transaction
 
@@ -296,7 +296,7 @@ def collect_group_environment_data(events):
     Find the first release for a each group and environment pair from a
     date-descending sorted list of events.
     """
-    results = {}
+    results = OrderedDict()
     for event in events:
         results[(event.group_id, get_environment_name(event))] = event.get_tag('sentry:release')
     return results
@@ -304,9 +304,11 @@ def collect_group_environment_data(events):
 
 def repair_group_environment_data(caches, project, events):
     for (group_id, env_name), first_release in collect_group_environment_data(events).items():
-        fields = {
-            'first_release_id': caches['Release'](project.organization_id, first_release).id,
-        }
+        fields = {}
+        if first_release:
+            fields['first_release_id'] = caches['Release'](
+                project.organization_id, first_release
+            ).id
 
         GroupEnvironment.objects.create_or_update(
             environment_id=caches['Environment'](project.organization_id, env_name).id,
@@ -317,7 +319,7 @@ def repair_group_environment_data(caches, project, events):
 
 
 def collect_tag_data(events):
-    results = {}
+    results = OrderedDict()
 
     for event in events:
         environment = get_environment_name(event)
@@ -383,7 +385,7 @@ def get_environment_name(event):
 
 
 def collect_release_data(caches, project, events):
-    results = {}
+    results = OrderedDict()
 
     for event in events:
         release = event.get_tag('sentry:release')
