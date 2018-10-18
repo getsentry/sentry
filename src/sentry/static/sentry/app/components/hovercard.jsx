a0c5813dfb00d16bd4520625201249fea283628c
Katie Byers <lobsterkatie@gmail.com>
2018-10-17 17:17:17 -0700

    move dimension and horizontal placement calculations to component

--- src/sentry/static/sentry/app/components/hovercard.jsx
+++ src/sentry/static/sentry/app/components/hovercard.jsx
@@ -10,10 +10,18 @@ class Hovercard extends React.Component {
     containerClassName: PropTypes.string,
     header: PropTypes.node,
     body: PropTypes.node,
+    minHeight: PropTypes.number,
+    minWidth: PropTypes.number,
+    maxWidth: PropTypes.number,
+    dynamicWidth: PropTypes.bool,
   };
 
   static defaultProps = {
     displayTimeout: 100,
+    minWidth: 295,
+    maxWidth: 295,
+    minHeight: 50,
+    dynamicWidth: false,
   };
 
   constructor(...args) {
@@ -54,6 +62,7 @@ class Hovercard extends React.Component {
 
   positionClasses() {
     if (!this.cardElement || !this.state.visible) return {};
+
     const rect = this.cardElement.getBoundingClientRect();
 
     const classes = {
@@ -68,6 +77,71 @@ class Hovercard extends React.Component {
     const currentClasses = intersection(this.cardElement.classList, Object.keys(classes));
 
     return currentClasses.length > 0 ? currentClasses : classes;
+  }
+
+  // figure out if we're dealing with hovercard-left or hovercard-right (in which case we don't
+  // want to center the hovercard) or hovercard or hovercard-bottom (in which case we do)
+  getMargin(classes) {
+    if (!this.cardElement || !this.state.visible) return {};
+
+    const rect = this.cardElement.getBoundingClientRect();
+
+    const rectWidth = this.isMultipleLines() ? rect.width : this.getSingleLineWidth();
+
+    if (classes.indexOf('left') !== -1 || classes.indexOf('right')) {
+      return -rectWidth / 2;
+    } else {
+      return 0;
+    }
+  }
+
+  isMultipleLines() {
+    if (!this.cardElement || !this.state.visible) return {};
+
+    // spans will create a new bounding box for each line of text
+    // if the text wraps, so this is a way to test for text wrapping
+    return this.cardBodySpan.getClientRects().length > 1;
+  }
+
+  //computed styles come back like "10px", so convert to the int 10
+  stylePxToNum(px) {
+    return parseInt(px.slice(0, -2), 10);
+  }
+
+  //this will be the width of the overall hovercard, based on the
+  //contents of the span in the body and all padding/borders around it
+  getSingleLineWidth() {
+    if (!this.cardElement || !this.state.visible) return {};
+
+    const spanStyle = getComputedStyle(this.cardBodySpan);
+    const spanPadding = 2 * this.stylePxToNum(spanStyle.padding);
+    const spanBorder =
+      this.stylePxToNum(spanStyle['border-left-width']) +
+      this.stylePxToNum(spanStyle['border-right-width']);
+
+    const bodyStyle = getComputedStyle(this.cardBody);
+    const bodyPadding = 2 * this.stylePxToNum(bodyStyle.padding);
+    const bodyBorder =
+      this.stylePxToNum(bodyStyle['border-left-width']) +
+      this.stylePxToNum(bodyStyle['border-right-width']);
+
+    const elementStyle = getComputedStyle(this.cardElement);
+    const elementPadding = 2 * this.stylePxToNum(elementStyle.padding);
+    const elementBorder =
+      this.stylePxToNum(elementStyle['border-left-width']) +
+      this.stylePxToNum(elementStyle['border-right-width']);
+
+    const spanWidth = this.cardBodySpan.getBoundingClientRect().width;
+
+    return (
+      spanWidth +
+      spanPadding +
+      spanBorder +
+      bodyPadding +
+      bodyBorder +
+      elementPadding +
+      elementBorder
+    );
   }
 
   render() {
@@ -93,10 +167,28 @@ class Hovercard extends React.Component {
             ref={e => {
               this.cardElement = e;
             }}
+            style={{
+              //if we're not using dynamicWidth or if we are but have multiple lines of text,
+              //then just use the given minWidth
+              minWidth:
+                !this.props.dynamicWidth || this.isMultipleLines()
+                  ? this.props.minWidth
+                  : this.getSingleLineWidth(),
+              maxWidth: this.props.maxWidth,
+              marginLeft: this.getMargin(cx),
+            }}
           >
             <div className="hovercard-hoverlap" />
             {header && <div className="hovercard-header">{header}</div>}
-            {body && <div className="hovercard-body">{body}</div>}
+            {body && (
+              <div
+                className="hovercard-body"
+                ref={e => (this.cardBody = e)}
+                style={{minHeight: this.props.minHeight}}
+              >
+                <span ref={e => (this.cardBodySpan = e)}>{body}</span>
+              </div>
+            )}
           </div>
         )}
       </span>
