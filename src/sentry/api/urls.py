77ca7a1ca078b46a5614ef1ca3d6fa64e0a71bdb
MeredithAnya <meredith.a.heller@gmail.com>
2018-10-19 11:17:49 -0700

    feat(app-platform): Add sentry app install details endpoint  (#10155)
    * feat(app-platform): Add sentry app install details endpoint

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -93,6 +93,7 @@ from .endpoints.organization_config_repositories import OrganizationConfigReposi
 from .endpoints.organization_repository_commits import OrganizationRepositoryCommitsEndpoint
 from .endpoints.organization_repository_details import OrganizationRepositoryDetailsEndpoint
 from .endpoints.organization_sentry_app_installations import OrganizationSentryAppInstallationsEndpoint
+from .endpoints.organization_sentry_app_installation_details import OrganizationSentryAppInstallationDetailsEndpoint
 from .endpoints.organization_stats import OrganizationStatsEndpoint
 from .endpoints.organization_teams import OrganizationTeamsEndpoint
 from .endpoints.organization_user_issues import OrganizationUserIssuesEndpoint
@@ -611,6 +612,11 @@ urlpatterns = patterns(
         r'^organizations/(?P<organization_slug>[^\/]+)/sentry-app-installations/$',
         OrganizationSentryAppInstallationsEndpoint.as_view(),
         name='sentry-api-0-organization-sentry-app-installations'
+    ),
+    url(
+        r'^organizations/(?P<organization_slug>[^\/]+)/sentry-app-installations/(?P<uuid>[^\/]+)/$',
+        OrganizationSentryAppInstallationDetailsEndpoint.as_view(),
+        name='sentry-api-0-organization-sentry-app-installations-details'
     ),
     url(
         r'^organizations/(?P<organization_slug>[^\/]+)/stats/$',
