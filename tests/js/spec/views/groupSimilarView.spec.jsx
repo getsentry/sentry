c29592e2fafa067021266fbf0ed2521da0491d7b
Billy Vong <billyvg@users.noreply.github.com>
2018-10-19 09:46:12 -0700

    fix(ui): Fix Similar Issues redirect (#10189)
    After a similar issues merge, we were never redirecting to parent issue. This was causing several issues.
    
    Fixes JAVASCRIPT-4DW
    Fixes JAVASCRIPT-4GR
    Fixes JAVASCRIPT-4EB

--- tests/js/spec/views/groupSimilarView.spec.jsx
+++ tests/js/spec/views/groupSimilarView.spec.jsx
@@ -1,5 +1,7 @@
 /* eslint-env jest */
+import {browserHistory} from 'react-router';
 import React from 'react';
+
 import {mount, shallow} from 'enzyme';
 import GroupSimilarView from 'app/views/groupSimilar/groupSimilarView';
 
@@ -30,14 +32,14 @@ describe('Issues Similar View', function() {
   let mock;
   beforeEach(function() {
     mock = MockApiClient.addMockResponse({
-      url: '/issues/groupId/similar/?limit=50',
+      url: '/issues/group-id/similar/?limit=50',
       body: mockData.similar,
     });
   });
 
   it('renders initially with loading component', function() {
     let component = shallow(
-      <GroupSimilarView params={{groupId: 'groupId'}} location={{}} />
+      <GroupSimilarView params={{groupId: 'group-id'}} location={{}} />
     );
 
     expect(component).toMatchSnapshot();
@@ -46,7 +48,7 @@ describe('Issues Similar View', function() {
   it('renders with mocked data', async function() {
     let wrapper = mount(
       <GroupSimilarView
-        params={{orgId: 'orgId', projectId: 'projectId', groupId: 'groupId'}}
+        params={{orgId: 'org-slug', projectId: 'project-slug', groupId: 'group-id'}}
         location={{}}
       />,
       TestStubs.routerContext()
@@ -57,5 +59,50 @@ describe('Issues Similar View', function() {
     wrapper.update();
     expect(mock).toHaveBeenCalled();
     expect(wrapper.find('GroupGroupingView')).toMatchSnapshot();
+  });
+
+  it('can merge and redirect to new parent', async function() {
+    let wrapper = mount(
+      <GroupSimilarView
+        params={{orgId: 'org-slug', projectId: 'project-slug', groupId: 'group-id'}}
+        location={{}}
+      />,
+      TestStubs.routerContext()
+    );
+    let merge = MockApiClient.addMockResponse({
+      method: 'PUT',
+      url: '/projects/org-slug/project-slug/issues/',
+      body: {
+        merge: {children: ['123'], parent: '321'},
+      },
+    });
+
+    await tick();
+    await tick();
+    wrapper.update();
+
+    wrapper
+      .find('[data-test-id="similar-item-row"]')
+      .first()
+      .simulate('click');
+
+    await tick();
+    wrapper.update();
+    wrapper.find('[data-test-id="merge"] a').simulate('click');
+    wrapper.find('Button[data-test-id="confirm-modal"]').simulate('click');
+
+    await tick();
+    wrapper.update();
+
+    expect(merge).toHaveBeenCalledWith(
+      '/projects/org-slug/project-slug/issues/',
+      expect.objectContaining({
+        data: {merge: 1},
+      })
+    );
+
+    expect(browserHistory.push).toHaveBeenCalledWith(
+      '/org-slug/project-slug/issues/321/similar/'
+    );
   });
 });
