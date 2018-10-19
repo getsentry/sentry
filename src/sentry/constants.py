42480b2606b39e8603c8f1e3fc365d0cfc38cdf5
MeredithAnya <meredith.a.heller@gmail.com>
2018-10-19 09:40:52 -0700

    ref(app-platform): Make slug max_length consistent (#10188)
    * ref(app-platform): Make slug max_length consistent

--- src/sentry/constants.py
+++ src/sentry/constants.py
@@ -79,6 +79,8 @@ MAX_EMAIL_FIELD_LENGTH = 75
 ENVIRONMENT_NAME_PATTERN = r'^[^\n\r\f\/]*$'
 ENVIRONMENT_NAME_MAX_LENGTH = 64
 
+SENTRY_APP_SLUG_MAX_LENGTH = 64
+
 # Team slugs which may not be used. Generally these are top level URL patterns
 # which we don't want to worry about conflicts on.
 RESERVED_ORGANIZATION_SLUGS = frozenset(
