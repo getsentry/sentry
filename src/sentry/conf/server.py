3ba784edd36011843029ce1378d9b8f62252e7d3
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-10-20 22:49:50 +0200

    fix: Show user info in internal errors

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1337,7 +1337,8 @@ def get_sentry_sdk_config():
         'in_app_include': [
             'sentry',
         ],
-        'debug': True
+        'debug': True,
+        'send_default_pii': True
     }
 
 
