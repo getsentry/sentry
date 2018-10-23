c33a52b9e4807fca4622729542a01d6f066aa85d
Billy Vong <billyvg@users.noreply.github.com>
2018-10-23 10:26:05 -0700

    feat(ui): Add "busy" to AsyncView search input (#10223)
    This adds a loading indicator to AsyncView search input.
    Also makes sure search extends the existing URL parameters.

--- src/sentry/static/sentry/app/components/asyncComponentSearchInput.jsx
+++ src/sentry/static/sentry/app/components/asyncComponentSearchInput.jsx
@@ -1,20 +1,24 @@
 import {debounce} from 'lodash';
+import {withRouter} from 'react-router';
 import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'react-emotion';
 
 import Input from 'app/views/settings/components/forms/controls/input';
+import LoadingIndicator from 'app/components/loadingIndicator';
 
 /**
  * This is a search input that can be easily used in AsyncComponent/Views.
  *
  * It probably doesn't make too much sense outside of an AsyncComponent atm.
  */
-export default class AsyncComponentSearchInput extends React.Component {
+class AsyncComponentSearchInput extends React.Component {
   static propTypes = {
     api: PropTypes.any.isRequired,
     url: PropTypes.string.isRequired,
     onSuccess: PropTypes.func.isRequired,
     onError: PropTypes.func.isRequired,
+    router: PropTypes.object.isRequired,
     placeholder: PropTypes.string,
     onSearchSubmit: PropTypes.func,
   };
@@ -27,16 +31,25 @@ export default class AsyncComponentSearchInput extends React.Component {
     super(props);
     this.state = {
       query: '',
+      busy: false,
     };
   }
 
   query = debounce(searchQuery => {
-    return this.props.api.request(`${this.props.url}?query=${searchQuery}`, {
+    let {router} = this.props;
+    this.setState({busy: true});
+    return this.props.api.request(`${this.props.url}`, {
       method: 'GET',
+      query: {
+        ...router.location.query,
+        query: searchQuery,
+      },
       success: (data, _, jqXHR) => {
+        this.setState({busy: false});
         this.props.onSuccess(data, jqXHR);
       },
       error: () => {
+        this.setState({busy: false});
         this.props.onError();
       },
     });
@@ -58,14 +71,27 @@ export default class AsyncComponentSearchInput extends React.Component {
   render() {
     let {placeholder, className} = this.props;
     return (
-      <form onSubmit={this.handleSearch}>
+      <Form onSubmit={this.handleSearch}>
         <Input
           value={this.state.query}
           onChange={this.handleChange}
           className={className}
           placeholder={placeholder}
         />
-      </form>
+        {this.state.busy && <StyledLoadingIndicator size="18px" mini />}
+      </Form>
     );
   }
 }
+
+const StyledLoadingIndicator = styled(LoadingIndicator)`
+  position: absolute;
+  right: -6px;
+  top: 3px;
+`;
+
+const Form = styled('form')`
+  position: relative;
+`;
+
+export default withRouter(AsyncComponentSearchInput);
