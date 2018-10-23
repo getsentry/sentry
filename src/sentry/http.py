82fce50362f51aa088d3857df5937966d458a903
Anton Ovchinnikov <anton@tonyo.info>
2018-10-23 13:22:11 +0200

    Update http.py

--- src/sentry/http.py
+++ src/sentry/http.py
@@ -2,7 +2,7 @@
 sentry.utils.http
 ~~~~~~~~~~~~~~~~~
 
-:copyright: (c) 2010-2014 by the Sentry Team, see AUTHORS for more details.
+:copyright: (c) 2010-2018 by the Sentry Team, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
 from __future__ import absolute_import
