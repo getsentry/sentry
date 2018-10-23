18fc1ec174a9e53e582701414faa3e8f0ed21da7
MeredithAnya <meredith.a.heller@gmail.com>
2018-10-23 10:09:28 -0700

    feat(app-platform): Add update to sentry app details endpoint  (#10187)
    * feat(app-platform): Add update to sentry app details endpoint

--- src/sentry/api/endpoints/sentry_app_details.py
+++ src/sentry/api/endpoints/sentry_app_details.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import
 
 from rest_framework.response import Response
-
 from sentry.api.bases.sentryapps import SentryAppDetailsEndpoint as BaseEndpoint
 from sentry.api.serializers import serialize
+from sentry.api.serializers.rest_framework import SentryAppSerializer
 from sentry.constants import SentryAppStatus
 from sentry.features.helpers import requires_feature
+from sentry.mediators.sentry_apps import Updater
 
 
 class SentryAppDetailsEndpoint(BaseEndpoint):
-
     @requires_feature('organizations:internal-catchall', any_org=True)
     def get(self, request, sentry_app):
         # Superusers have access to the app, published or unpublished. Other
@@ -18,3 +18,18 @@ class SentryAppDetailsEndpoint(BaseEndpoint):
             return Response(serialize(sentry_app, request.user))
 
         return Response(status=404)
+
+    @requires_feature('organizations:internal-catchall', any_org=True)
+    def put(self, request, sentry_app):
+        serializer = SentryAppSerializer(data=request.DATA, partial=True)
+        if serializer.is_valid():
+            result = serializer.object
+            updated_app = Updater.run(
+                sentry_app=sentry_app,
+                name=result.get('name'),
+                webhook_url=result.get('webhook_url'),
+                scopes=result.get('scopes'),
+            )
+            return Response(serialize(updated_app, request.user))
+
+        return Response(serializer.errors, status=400)
