431d614d91c74c30affaa751581207de2159031b
Megan Heskett <meg.heskett@gmail.com>
2018-10-24 09:47:42 -0700

    fix(ui): Add logout to Create a New Organization prompt (#10214)

--- src/sentry/static/sentry/app/components/narrowLayout.jsx
+++ src/sentry/static/sentry/app/components/narrowLayout.jsx
@@ -1,14 +1,34 @@
 import jQuery from 'jquery';
 import React from 'react';
+import PropTypes from 'prop-types';
+
+import {t} from 'app/locale';
+import {Client} from 'app/api';
+import styled from 'react-emotion';
 
 class NarrowLayout extends React.Component {
+  static propTypes = {
+    showLogout: PropTypes.bool,
+  };
+
   componentWillMount() {
+    this.api = new Client();
     jQuery(document.body).addClass('narrow');
   }
 
   componentWillUnmount() {
+    this.api.clear();
     jQuery(document.body).removeClass('narrow');
   }
+
+  handleLogout = () => {
+    this.api.request('/auth/', {
+      method: 'DELETE',
+      success: () => {
+        window.location = '/auth/login';
+      },
+    });
+  };
 
   render() {
     return (
@@ -20,6 +40,11 @@ class NarrowLayout extends React.Component {
               <a href="/">
                 <span className="icon-sentry-logo" />
               </a>
+              {this.props.showLogout && (
+                <a className="logout pull-right" onClick={this.handleLogout}>
+                  <Logout>{t('Sign out')}</Logout>
+                </a>
+              )}
             </div>
             <div className="box-content with-padding">{this.props.children}</div>
           </div>
@@ -28,5 +53,9 @@ class NarrowLayout extends React.Component {
     );
   }
 }
+
+const Logout = styled.span`
+  font-size: 16px;
+`;
 
 export default NarrowLayout;
