cb54e25ed329aed0e6f28343f824f5658d6cf62c
Ayesha Omarali <ayesha.omarali@gmail.com>
2018-10-24 14:56:16 -0700

    Feat(discover): Make Alpha Testing Feedback Alert Responsive (#10258)

--- src/sentry/static/sentry/app/views/organizationDiscover/earlyAdopterMessage.jsx
+++ src/sentry/static/sentry/app/views/organizationDiscover/earlyAdopterMessage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'react-emotion';
 import {Flex} from 'grid-emotion';
+import theme from 'app/utils/theme';
 
 import Alert from 'app/components/alert';
 import Link from 'app/components/link';
@@ -10,12 +11,12 @@ export default class EarlyAdopterMessage extends React.Component {
   render() {
     return (
       <StyledAlert type="info" icon="icon-labs">
-        <Flex justify="space-between">
-          {t('Sentry Discover is alpha software. Thanks for being an early adopter.')}
-          <Link href="mailto:feedback-discover@sentry.io">
+        <StyledAlertText>
+          {t('Sentry Discover is alpha software. Thanks for being an early adopter.')}&nbsp;
+          <StyledLink href="mailto:feedback-discover@sentry.io">
             {t('feedback-discover@sentry.io')}
-          </Link>
-        </Flex>
+          </StyledLink>
+        </StyledAlertText>
       </StyledAlert>
     );
   }
@@ -25,4 +26,20 @@ const StyledAlert = styled(Alert)`
   padding: ${p => p.theme.grid}px ${p => p.theme.grid * 2}px;
   margin: 0;
   align-items: center;
+`;
+
+const StyledAlertText = styled(Flex)`
+  justify-content: space-between;
+  white-space: nowrap;
+  @media (max-width: ${theme.breakpoints[2]}) {
+    justify-content: flex-start;
+    display: inline;
+    white-space: normal;
+  }
+`;
+
+const StyledLink = styled(Link)`
+  width: fit-content;
+  display: inline-block;
+  white-space: nowrap;
 `;
