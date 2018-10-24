51e2b09d0d07af548a89c2e9bd254b7e5d24d416
Bruno Garcia <bruno@brunogarcia.com>
2018-10-24 17:23:25 +0200

    test: unity snapshot

--- tests/acceptance/test_issue_details.py
+++ tests/acceptance/test_issue_details.py
@@ -20,10 +20,11 @@ class IssueDetailsTest(AcceptanceTestCase):
         )
         self.login_as(self.user)
 
-    def create_sample_event(self, platform, sample_name=None):
+    def create_sample_event(self, platform, default=None, sample_name=None):
         event = create_sample_event(
             project=self.project,
             platform=platform,
+            default=default,
             sample_name=sample_name,
             event_id='d964fdbd649a4cf8bfc35d18082b6b0e',
             timestamp=1452683305,
@@ -56,6 +57,19 @@ class IssueDetailsTest(AcceptanceTestCase):
         self.browser.wait_until('.entries')
         self.browser.wait_until('[data-test-id="loaded-device-name"]')
         self.browser.snapshot('issue details cocoa')
+
+    def test_unity_event(self):
+        event = self.create_sample_event(
+            default='unity',
+            platform='csharp'
+        )
+
+        self.browser.get(
+            u'/{}/{}/issues/{}/'.format(self.org.slug, self.project.slug, event.group.id)
+        )
+        self.browser.wait_until('.entries')
+        self.browser.wait_until('[data-test-id="loaded-device-name"]')
+        self.browser.snapshot('issue details unity')
 
     def test_javascript_specific_event(self):
         event = self.create_sample_event(
