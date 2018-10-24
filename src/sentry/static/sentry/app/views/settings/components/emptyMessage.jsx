603a369d4b9d1cc8725a505301a32ec4485f8609
ckj <chriskjennings@gmail.com>
2018-10-24 14:37:09 -0700

    Clean up margins

--- src/sentry/static/sentry/app/views/settings/components/emptyMessage.jsx
+++ src/sentry/static/sentry/app/views/settings/components/emptyMessage.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from 'react-emotion';
+import styled, {css} from 'react-emotion';
 
 import InlineSvg from 'app/components/inlineSvg';
 import TextBlock from 'app/views/settings/components/text/textBlock';
@@ -13,43 +13,51 @@ const Wrapper = styled.div`
   flex-direction: column;
   color: ${p => p.theme.gray4};
   padding: ${p => p.theme.grid * 4}px 15%;
-  line-height: 1;
   font-size: ${p =>
     p.size && p.size.large ? p.theme.fontSizeExtraLarge : p.theme.fontSizeLarge};
 `;
 
-const StyledInlineSvg = styled(InlineSvg)`
-  display: block;
-  color: ${p => p.theme.gray1};
+const MarginStyles = css`
   margin-bottom: ${space(2)};
+  &:last-child {
+    margin: 0;
+  }
 `;
 
-const Action = styled.div`
-  display: block;
+const Description = styled(TextBlock)`
+  ${MarginStyles};
 `;
 
 const Title = styled.div`
   font-weight: bold;
   font-size: 20px;
-  margin-bottom: ${space(2)};
   line-height: 1.2;
+  ${MarginStyles};
+
+  & + ${Description} {
+    margin-top: -${space(0.5)}; /* Remove the illusion of bad padding by offsetting line-height */
+  }
 `;
 
-const Description = styled(TextBlock)`
-  margin-top: -${space(0.5)}; /* Remove the illusion of bad padding by offsetting line-height */
-  margin-bottom: ${space(2)};
+const StyledInlineSvg = styled(InlineSvg)`
+  display: block;
+  color: ${p => p.theme.gray1};
+  ${MarginStyles};
+`;
+
+const Action = styled.div`
+  display: block;
+  ${MarginStyles};
 `;
 
 const EmptyMessage = ({title, description, icon, children, action, size}) => {
   return (
     <Wrapper size={size}>
       {icon && <StyledInlineSvg src={icon} size="36px" />}
-      <div className="ref-message">
-        {title && <Title>{title}</Title>}
-        {description && <Description noMargin>{description}</Description>}
-        {children && <Description noMargin>{children}</Description>}
-        {action && <Action>{action}</Action>}
-      </div>
+      {title && <Title>{title}</Title>}
+      {description && <Description>{description}</Description>}
+      {children && <Description noMargin>{children}</Description>}
+      {action && <Action>{action}</Action>}
     </Wrapper>
   );
 };
