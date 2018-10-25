cf963645390dfbd5b7c1cc90fed96089b1ea49f4
Chris Jennings <chriskjennings@gmail.com>
2018-10-25 14:28:24 -0700

    ref(ui): empty message polish (#10239)
    * improve padding, margin, and line-height
    
    * Clean up margins
    
    * remove superflous property
    
    * feat(ui): add dottedBorder option for panel to signify missing functionality (#10241)

--- docs-ui/components/emptyMessage.stories.js
+++ docs-ui/components/emptyMessage.stories.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Flex, Box} from 'grid-emotion';
 
 import {Panel, PanelHeader} from 'app/components/panels';
 import {storiesOf} from '@storybook/react';
@@ -50,13 +51,58 @@ storiesOf('UI|EmptyMessage', module)
     ))
   )
   .add(
-    'in panel with sub-description',
+    'in panel with title and description',
     withInfo('Put this in a panel for maximum effect')(() => (
       <Panel>
         <PanelHeader>Members</PanelHeader>
         <EmptyMessage
           title="Sentry is better with Friends"
           description="When you use sentry with friends, you'll find your world of possibilities expands!"
+        />
+      </Panel>
+    ))
+  )
+  .add(
+    'in panel with everything',
+    withInfo('Put this in a panel for maximum effect')(() => (
+      <Panel>
+        <PanelHeader>Members</PanelHeader>
+        <EmptyMessage
+          icon="icon-user"
+          title="Sentry is better with friends!"
+          description="When you use sentry with friends, you'll find your world of possibilities expands!"
+          action={
+            <Flex justify="center">
+              <Box mr={1}>
+                <Button priority="primary">Invite Members</Button>
+              </Box>
+              <Box>
+                <Button>Learn More</Button>
+              </Box>
+            </Flex>
+          }
+        />
+      </Panel>
+    ))
+  )
+  .add(
+    'in onboarding/missing functionality panel',
+    withInfo('Put this in a panel for maximum effect')(() => (
+      <Panel dottedBorder>
+        <EmptyMessage
+          icon="icon-discover"
+          title="You're missing out on crucial functionality!"
+          description="Enable this feature now to get the most out of Sentry. What are you waiting for? Do it!"
+          action={
+            <Flex justify="center">
+              <Box mr={1}>
+                <Button priority="primary">Enable it!</Button>
+              </Box>
+              <Box>
+                <Button>Learn More</Button>
+              </Box>
+            </Flex>
+          }
         />
       </Panel>
     ))
