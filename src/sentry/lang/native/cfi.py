7d0c32b3d3501d1d7ec0c3d01c1177fcbc57b61f
Jan Michael Auer <jan.auer@sentry.io>
2018-10-25 16:43:27 +0200

    meta: Add more description and comments to CFI processing

--- src/sentry/lang/native/cfi.py
+++ src/sentry/lang/native/cfi.py
@@ -26,26 +26,20 @@ MIN_TRUST = FrameTrust.fp
 NO_CFI_PLACEHOLDER = '__no_cfi__'
 
 
-def is_trusted_thread(thread):
-    return all(
-        getattr(FrameTrust, f.get('trust', ''), 0) >= MIN_TRUST
-        for f in thread.iter_frames()
-    )
-
-
 class ThreadRef(object):
+    """Cacheable and mutable reference to stack frames of an event thread."""
+
     def __init__(self, frames, modules):
         self.raw_frames = frames
         self.modules = modules
         self.resolved_frames = None
-        self.cache_key = self._get_cache_key()
 
     def _get_frame_key(self, frame):
         module = self.modules.find_object(frame['instruction_addr'])
 
         # If we cannot resolve a module for this frame, this means we're dealing
         # with an absolute address here. Since this address changes with every
-        # crash and would poison our cache, we skip it.
+        # crash and would poison our cache, we skip it for the key calculation.
         if not module:
             return None
 
@@ -54,13 +48,20 @@ class ThreadRef(object):
             rebase_addr(frame['instruction_addr'], module)
         )
 
-    def _get_cache_key(self):
+    @property
+    def _cache_key(self):
         values = [self._get_frame_key(f) for f in self.raw_frames]
+        # XXX: The seed is hard coded for a future refactor
         return 'st:%s' % hash_values(values, seed='MinidumpCfiProcessor')
 
     def _frame_from_cache(self, entry):
         debug_id, offset, trust = entry[:3]
         module = self.modules.get_object(debug_id)
+
+        # The debug_id can be None or refer to a missing module. If the module
+        # was missing, the stored offset was absolute as well. Otherwise, we
+        # have no choice but to assume an absolute address. In practice, the
+        # latter hopefully never happens.
         addr = module.addr + offset if module else offset
 
         return module, {
@@ -71,7 +72,12 @@ class ThreadRef(object):
         }
 
     def load_from_cache(self):
-        cached = cache.get(self.cache_key)
+        """Attempts to load the reprocessed stack trace from the cache. The
+        return value is ``True`` for a cache hit, and ``False`` for a miss.
+        The loaded addresses are rebased to the provided code modules.
+        """
+
+        cached = cache.get(self._cache_key)
         if cached is None:
             return False
 
@@ -83,11 +89,14 @@ class ThreadRef(object):
         return True
 
     def save_to_cache(self):
+        """Stores the reprocessed stack trace to the cache. For frames with
+        known code modules only relative offsets are stored, otherwise the
+        absolute address as fallback."""
         if self.resolved_frames is None:
             raise RuntimeError('save_to_cache called before resolving frames')
 
         if self.resolved_frames == NO_CFI_PLACEHOLDER:
-            cache.set(self.cache_key, NO_CFI_PLACEHOLDER)
+            cache.set(self._cache_key, NO_CFI_PLACEHOLDER)
             return
 
         values = []
@@ -98,19 +107,29 @@ class ThreadRef(object):
                 addr = '0x%x' % rebase_addr(addr, module)
             values.append((module_id, addr, frame['trust']))
 
-        cache.set(self.cache_key, values)
+        cache.set(self._cache_key, values)
 
-    def update_from_minidump(self, thread):
+    def load_from_minidump(self, thread):
+        """Loads the stack trace from a minidump process state thread."""
+
+        # Convert the entire thread into frames conforming to the `Frame`
+        # interface. Note that this is done with the same function as the
+        # initial ingestion to avoid normalization conflicts.
         frames = frames_from_minidump_thread(thread)
+
+        # Filter out stack traces that did not improve during reprocessing. For
+        # these cases we only store a marker. This also prevents us from
+        # destroying absolute addresses when restoring from the cache. Stack
+        # traces containing CFI frames are mapped to their modules and stored.
         if any(frame['trust'] in CFI_TRUSTS for frame in frames):
-            self.resolved_frames = [
-                (self.modules.find_object(frame['instruction_addr']), frame)
-                for frame in frames
-            ]
+            self.resolved_frames = [(self.modules.find_object(f['instruction_addr']), f)
+                                    for f in frames]
         else:
             self.resolved_frames = NO_CFI_PLACEHOLDER
 
     def apply_to_event(self):
+        """Writes the loaded stack trace back to the event's payload. Returns
+        ``True`` if the payload was changed, otherwise ``False``."""
         if self.resolved_frames is None:
             raise RuntimeError('apply_to_event called before resolving frames')
 
@@ -120,11 +139,29 @@ class ThreadRef(object):
         self.raw_frames[:] = [frame for module, frame in self.resolved_frames]
         return True
 
-    def iter_frames(self):
-        return iter(self.raw_frames)
+    @property
+    def needs_cfi(self):
+        """Indicates whether this thread requires reprocessing with CFI due to
+        scanned stack frames."""
+        return any(
+            getattr(FrameTrust, f.get('trust', ''), 0) < MIN_TRUST
+            for f in self.raw_frames
+        )
 
 
 class ThreadProcessingHandle(object):
+    """Helper object for processing all event threads.
+
+    This class offers a view on all threads in the given event payload,
+    including the crashing exception thread. Use ``iter_threads`` to iterate
+    pointers to the original threads' stack traces. Likewise, ``iter_modules``
+    returns references to all modules (images) loaded into the process.
+
+    The handle keeps track of changes to the original data. To signal mutation,
+    call ``indicate_change``. Finally, ``result`` returns the changed data or
+    None if it was not changed.
+    """
+
     def __init__(self, data):
         self.data = data
         self.modules = self._get_modules()
@@ -135,14 +172,19 @@ class ThreadProcessingHandle(object):
         return ObjectLookup(modules)
 
     def iter_modules(self):
+        """Returns an iterator over all code modules (images) loaded by the
+        process at the time of the crash. The values are of type ``ObjectRef``.
+        """
         return self.modules.iter_objects()
 
     def iter_threads(self):
-        exceptions = self.data.get('exception', {}).get('values', [])
-        exception = exceptions[0] if exceptions else {}
-
+        """Returns an iterator over all threads of the process at the time of
+        the crash, including the crashing thread. The values are of type
+        ``ThreadRef``."""
         for thread in self.data.get('threads', {}).get('values', []):
             if thread.get('crashed'):
+                exceptions = self.data.get('exception', {}).get('values', [])
+                exception = exceptions[0] if exceptions else {}
                 frames = exception.get('stacktrace', {}).get('frames')
             else:
                 frames = thread.get('stacktrace', {}).get('frames')
@@ -152,20 +194,30 @@ class ThreadProcessingHandle(object):
                 yield tid, ThreadRef(frames, self.modules)
 
     def indicate_change(self):
+        """Signals mutation of the data."""
         self.changed = True
 
     def result(self):
+        """Returns ``data`` if ``indicate_change`` was called, otherwise None.
+        """
         if self.changed:
             return self.data
 
 
 def reprocess_minidump_with_cfi(data):
+    """Reprocesses a minidump event if CFI(call frame information) is available
+    and viable. The event is only processed if there are stack traces that
+    contain scanned frames.
+    """
+
     handle = ThreadProcessingHandle(data)
 
-    # Check stacktrace caches first and skip all that do not need CFI
+    # Check stacktrace caches first and skip all that do not need CFI. This is
+    # either if a thread is trusted (i.e. it does not contain scanned frames) or
+    # since it can be fetched from the cache.
     threads = {}
     for tid, thread in handle.iter_threads():
-        if is_trusted_thread(thread):
+        if not thread.needs_cfi:
             continue
 
         if thread.load_from_cache():
@@ -197,17 +249,17 @@ def reprocess_minidump_with_cfi(data):
     if not cficaches:
         return handle.result()
 
-    # Put all caches into a frame info map for stackwalking
+    # Reprocess the minidump with CFI
     cfi_map = FrameInfoMap.new()
     for debug_id, cficache in six.iteritems(cficaches):
         cfi_map.add(debug_id, cficache)
-
-    # Reprocess the minidump with CFI and merge stacktraces
     state = process_minidump(minidump.data, cfi=cfi_map)
+
+    # Merge existing stack traces with new ones from the minidump
     for minidump_thread in state.threads():
         thread = threads.get(minidump_thread.thread_id)
         if thread:
-            thread.update_from_minidump(minidump_thread)
+            thread.load_from_minidump(minidump_thread)
             thread.save_to_cache()
             if thread.apply_to_event():
                 handle.indicate_change()
