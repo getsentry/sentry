f0de86e0137fe3ff3178d408ffd290878deac867
Billy Vong <billyvg@users.noreply.github.com>
2018-10-29 17:15:15 +0000

    Update based on review feedback.
    Co-Authored-By: markstory <mark@mark-story.com>

--- src/sentry/static/sentry/app/components/sidebar/sidebarDropdown/index.jsx
+++ src/sentry/static/sentry/app/components/sidebar/sidebarDropdown/index.jsx
@@ -38,7 +38,7 @@ const SidebarDropdown = withApi(
     };
 
     handleLogout = (...args) => {
-      logout(this.props.api).then(() => window.location = '/auth/login');
+      logout(this.props.api).then(() => (window.location = '/auth/login'));
     };
 
     render() {
