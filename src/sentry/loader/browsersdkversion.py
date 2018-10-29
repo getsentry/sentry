09bafccff5d4e3fbefcfff46f836958f01c63e85
Kamil Ogórek <kamil.ogorek@gmail.com>
2018-10-29 15:14:18 +0100

    feat: Pull latest SDK information from the registry (#10231)
    * feat: Pull latest SDK information from the registry
    
    * fix: Release registry fetch
    
    * Always perform a build and ignore registry file
    
    * fix: Do not read the version file on every request
    
    * fix: Override run() for command and fix logging
    
    * meta: Update MANIFEST.in

--- src/sentry/loader/browsersdkversion.py
+++ src/sentry/loader/browsersdkversion.py
@@ -1,23 +1,43 @@
 from __future__ import absolute_import
 
+import os
 import re
+import logging
+import json
+
+from functools32 import lru_cache
+
+import sentry
 
 from django.conf import settings
 
+logger = logging.getLogger('sentry')
 
 _version_regexp = re.compile(r'\d+')
+LOADER_FOLDER = os.path.abspath(os.path.join(os.path.dirname(sentry.__file__), 'loader'))
 DEFAULT_VERSION = '4.x'
+
+
+@lru_cache(maxsize=10)
+def load_registry(path):
+    if '/' in path:
+        return None
+    fn = os.path.join(LOADER_FOLDER, path + '.json')
+    try:
+        with open(fn, 'rb') as f:
+            return json.load(f)
+    except IOError:
+        return None
 
 
 def get_highest_browser_sdk_version():
     return max(get_browser_sdk_version_versions(),
-               key=lambda version: int(_version_regexp.match(version).group(
-                   0)) if _version_regexp.search(version) else -1
+               key=lambda version: int(_version_regexp.match(version).group(0))
+               if _version_regexp.search(version) else -1
                )
 
 
 def get_browser_sdk_version_versions():
-    # TODO(hazat): do request here to fetch versions
     return ['latest', DEFAULT_VERSION]
 
 
@@ -28,11 +48,21 @@ def get_browser_sdk_version_choices():
     return tuple(rv)
 
 
+def load_version_from_file():
+    data = load_registry('_registry')
+    return data['version']
+
+
 def get_browser_sdk_version(project_key):
-    # TODO(hazat): Right now we are only returing our conf version
     selected_version = get_selected_browser_sdk_version(project_key)
+
     if selected_version == DEFAULT_VERSION:
-        return settings.JS_SDK_LOADER_SDK_VERSION
+        try:
+            return load_version_from_file()
+        except BaseException:
+            logger.error('error ocurred while trying to read js sdk information from the registry')
+            return settings.JS_SDK_LOADER_SDK_VERSION
+
     return settings.JS_SDK_LOADER_SDK_VERSION
 
 
---
+++ src/sentry/utils/distutils/commands/build_js_sdk_registry.py
@@ -0,0 +1,61 @@
+# NOTE: This is run external to sentry as well as part of the setup
+# process.  Thus we do not want to import non stdlib things here.
+from __future__ import absolute_import
+
+import os
+import sys
+import json
+from distutils import log
+
+import sentry
+
+JS_SDK_REGISTRY_URL = 'https://release-registry.services.sentry.io/packages/npm:@sentry/browser/latest'
+LOADER_FOLDER = os.path.abspath(os.path.join(os.path.dirname(sentry.__file__), 'loader'))
+
+# We cannot leverage six here, so we need to vendor
+# bits that we need.
+if sys.version_info[0] == 3:
+
+    def iteritems(d, **kw):
+        return iter(d.items(**kw))
+
+    from urllib.request import urlopen
+
+else:
+
+    def iteritems(d, **kw):
+        return d.iteritems(**kw)  # NOQA
+
+    from urllib2 import urlopen
+
+
+def dump_registry(path, data):
+    fn = os.path.join(LOADER_FOLDER, path + '.json')
+    directory = os.path.dirname(fn)
+    try:
+        os.makedirs(directory)
+    except OSError:
+        pass
+    with open(fn, 'wb') as f:
+        json.dump(data, f, indent=2)
+        f.write('\n')
+
+
+def sync_registry():
+    body = urlopen(JS_SDK_REGISTRY_URL).read().decode('utf-8')
+    data = json.loads(body)
+    dump_registry('_registry', data)
+
+
+from .base import BaseBuildCommand
+
+
+class BuildJsSdkRegistryCommand(BaseBuildCommand):
+    description = 'build js sdk registry'
+
+    def run(self):
+        log.info('downloading js sdk information from the release registry')
+        try:
+            sync_registry()
+        except BaseException:
+            log.error('error ocurred while trying to fetch js sdk information from the registry')
