fc5ed17ebe2308aa0efbc439382f7fd4acd4208d
Megan Heskett <meg.heskett@gmail.com>
2018-10-31 14:12:04 -0700

    feat(ui): Add select all to teams (#10292)

--- src/sentry/static/sentry/app/views/settings/organizationMembers/inviteMember/index.jsx
+++ src/sentry/static/sentry/app/views/settings/organizationMembers/inviteMember/index.jsx
@@ -203,6 +203,28 @@ const InviteMember = createReactClass({
     });
   },
 
+  allSelected() {
+    let {teams} = this.getOrganization();
+    let {selectedTeams} = this.state;
+    return teams.length === selectedTeams.size;
+  },
+
+  handleSelectAll() {
+    let {teams} = this.getOrganization();
+
+    this.setState(state => {
+      let {selectedTeams} = state;
+      if (this.allSelected()) {
+        selectedTeams.clear();
+      } else {
+        selectedTeams = new Set(teams.map(({slug}) => slug));
+      }
+      return {
+        selectedTeams,
+      };
+    });
+  },
+
   render() {
     let {error, loading, roleList, selectedRole, selectedTeams} = this.state;
     let {teams} = this.getOrganization();
@@ -246,6 +268,8 @@ const InviteMember = createReactClass({
               teams={teams}
               selectedTeams={selectedTeams}
               toggleTeam={this.toggleTeam}
+              onSelectAll={this.handleSelectAll}
+              allSelected={this.allSelected}
             />
             <Button
               priority="primary"
