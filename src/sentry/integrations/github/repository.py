8a320821fa7ddd853569b5ed39ef54e8caa805f0
Mark Story <mark@mark-story.com>
2018-10-31 14:18:15 +0000

    fix(integrations) Record CommitFileChange records for GitHub releases (#10286)
    Releases that use commits from github repositories were not creating
    CommitFileChange records for commits that were net new. We need to
    record this data so that we can locate suspect commits in 'squash merge'
    and 'rebase merge' workflows.
    
    Refs APP-635

--- src/sentry/integrations/github/repository.py
+++ src/sentry/integrations/github/repository.py
@@ -58,20 +58,6 @@ class GitHubRepositoryProvider(providers.IntegrationRepositoryProvider):
             'integration_id': data['integration_id']
         }
 
-    def _format_commits(self, repo, commit_list):
-        return [
-            {
-                'id': c['sha'],
-                'repository': repo.name,
-                'author_email': c['commit']['author'].get('email'),
-                'author_name': c['commit']['author'].get('name'),
-                'message': c['commit']['message'],
-                'timestamp': dateutil.parser.parse(
-                    c['commit']['author'].get('date'),
-                ).astimezone(timezone.utc) if c['commit']['author'].get('date') else None,
-            } for c in commit_list
-        ]
-
     def compare_commits(self, repo, start_sha, end_sha):
         integration_id = repo.integration_id
         if integration_id is None:
@@ -82,35 +68,75 @@ class GitHubRepositoryProvider(providers.IntegrationRepositoryProvider):
 
         # use config name because that is kept in sync via webhooks
         name = repo.config['name']
-        if start_sha is None:
-            try:
+        try:
+            if start_sha is None:
                 res = client.get_last_commits(name, end_sha)
-            except Exception as e:
-                installation.raise_error(e)
+                return self._format_commits(client, name, res[:20])
             else:
-                return self._format_commits(repo, res[:10])
-        else:
-            try:
                 res = client.compare_commits(name, start_sha, end_sha)
-            except Exception as e:
-                installation.raise_error(e)
-            else:
-                return self._format_commits(repo, res['commits'])
+                return self._format_commits(client, name, res['commits'])
+        except Exception as e:
+            installation.raise_error(e)
 
-        def get_pr_commits(self, repo, number, actor=None):
-            # (not currently used by sentry)
-            integration_id = repo.integration_id
-            if integration_id is None:
-                raise NotImplementedError('GitHub apps requires an integration id to fetch commits')
-            integration = Integration.objects.get(id=integration_id)
-            installation = integration.get_installation(repo.organization_id)
-            client = installation.get_client()
+    def _format_commits(self, client, repo_name, commit_list):
+        """Convert GitHub commits into our internal format
 
-            # use config name because that is kept in sync via webhooks
-            name = repo.config['name']
-            try:
-                res = client.get_pr_commits(name, number)
-            except Exception as e:
-                installation.raise_error(e)
-            else:
-                return self._format_commits(repo, res)
+        For each commit in the list we have to fetch patch data, as the
+        compare API gives us all of the files changed in the commit
+        range but not which files changed in each commit. Without this
+        we cannot know which specific commit changed a given file.
+
+        See sentry.models.Release.set_commits
+        """
+        return [
+            {
+                'id': c['sha'],
+                'repository': repo_name,
+                'author_email': c['commit']['author'].get('email'),
+                'author_name': c['commit']['author'].get('name'),
+                'message': c['commit']['message'],
+                'timestamp': dateutil.parser.parse(
+                    c['commit']['author'].get('date'),
+                ).astimezone(timezone.utc) if c['commit']['author'].get('date') else None,
+                'patch_set': self._get_patchset(client, repo_name, c['sha'])
+            } for c in commit_list
+        ]
+
+    def _get_patchset(self, client, repo_name, sha):
+        """Get the modified files for a commit
+        """
+        commit = client.get_commit(repo_name, sha)
+        return self._transform_patchset(commit['files'])
+
+    def _transform_patchset(self, diff):
+        """Convert the patch data from GitHub into our internal format
+
+        See sentry.models.Release.set_commits
+        """
+        changes = []
+        for change in diff:
+            if change['status'] == 'modified':
+                changes.append({
+                    'path': change['filename'],
+                    'type': 'M',
+                })
+            if change['status'] == 'added':
+                changes.append({
+                    'path': change['filename'],
+                    'type': 'A',
+                })
+            if change['status'] == 'removed':
+                changes.append({
+                    'path': change['filename'],
+                    'type': 'D',
+                })
+            if change['status'] == 'renamed':
+                changes.append({
+                    'path': change['previous_filename'],
+                    'type': 'D',
+                })
+                changes.append({
+                    'path': change['filename'],
+                    'type': 'A',
+                })
+        return changes
