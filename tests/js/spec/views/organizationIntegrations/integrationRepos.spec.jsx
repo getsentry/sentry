57ee873a368923ac83f9796c704456e88e72938a
Mark Story <mark@sentry.io>
2018-11-02 12:16:26 -0400

    fix(integrations) Fix crash when adding a repository fails
    Fix usage of undefined variable so we can show an error indicator when
    saving a repository fails.
    
    Fixes JAVASCRIPT-4FS

--- tests/js/spec/views/organizationIntegrations/integrationRepos.spec.jsx
+++ tests/js/spec/views/organizationIntegrations/integrationRepos.spec.jsx
@@ -15,6 +15,92 @@ describe('IntegrationRepos', function() {
     const integration = TestStubs.GitHubIntegration();
     const routerContext = TestStubs.routerContext();
 
+    describe('successful save', async function() {
+      let addRepo = Client.addMockResponse({
+        url: `/organizations/${org.slug}/repos/`,
+        method: 'POST',
+        body: {
+          id: 9,
+          integrationId: '1',
+          name: 'getsentry/sentry',
+          url: 'https://github.com/getsentry/sentry',
+          provider: {
+            name: 'GitHub',
+            id: 'integrations:github',
+            status: 'active',
+            url: 'github.com/getsentry',
+          },
+        },
+      });
+      Client.addMockResponse({
+        url: `/organizations/${org.slug}/integrations/1/repos/`,
+        body: {
+          repos: [{identifier: 'getsentry/sentry', name: 'sentry'}],
+        },
+      });
+      Client.addMockResponse({
+        url: `/organizations/${org.slug}/repos/`,
+        method: 'GET',
+        body: [],
+      });
+
+      const wrapper = mount(
+        <IntegrationRepos integration={integration} />,
+        routerContext
+      );
+      wrapper.find('DropdownButton').simulate('click');
+      wrapper.find('StyledListElement').simulate('click');
+      await wrapper.update();
+
+      expect(addRepo).toHaveBeenCalledWith(
+        `/organizations/${org.slug}/repos/`,
+        expect.objectContaining({
+          data: {
+            installation: '1',
+            provider: 'integrations:github',
+            identifier: 'getsentry/sentry',
+          },
+        })
+      );
+      let repoRow = wrapper.find('RepoOption').first();
+      expect(repoRow.find('strong').text()).toEqual('getsentry/sentry');
+    });
+
+    describe('save failure', async function() {
+      let addRepo = Client.addMockResponse({
+        url: `/organizations/${org.slug}/repos/`,
+        method: 'POST',
+        statusCode: 400,
+        body: {
+          errors: {
+            __all__: 'Repository already exists.',
+          },
+        },
+      });
+      Client.addMockResponse({
+        url: `/organizations/${org.slug}/integrations/1/repos/`,
+        body: {
+          repos: [{identifier: 'getsentry/sentry', name: 'sentry'}],
+        },
+      });
+      Client.addMockResponse({
+        url: `/organizations/${org.slug}/repos/`,
+        method: 'GET',
+        body: [],
+      });
+
+      const wrapper = mount(
+        <IntegrationRepos integration={integration} />,
+        routerContext
+      );
+      wrapper.find('DropdownButton').simulate('click');
+      wrapper.find('StyledListElement').simulate('click');
+      await wrapper.update();
+
+      expect(addRepo).toHaveBeenCalled();
+      expect(wrapper.find('RepoOption')).toHaveLength(0);
+    });
+
     describe('migratable repo', function() {
       Client.addMockResponse({
         url: `/organizations/${org.slug}/repos/`,
