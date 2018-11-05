1ee8d2c0497ac866042dd57fda0676234db3ba00
Chris Clark <christopherchristopherclark@gmail.com>
2018-11-05 15:21:45 -0800

    feat(org-events): add PreviewFeature message (#10421)
    * add preview feature message
    
    * use alert component rather than css and forward a type prop
    
    * default type is info

--- src/sentry/static/sentry/app/components/previewFeature.jsx
+++ src/sentry/static/sentry/app/components/previewFeature.jsx
@@ -1,14 +1,26 @@
 import React, {Component} from 'react';
 import {t} from 'app/locale';
+import PropTypes from 'prop-types';
+import Alert from './alert';
 
 export default class PreviewFeature extends Component {
+  static propTypes = {
+    type: PropTypes.oneOf(['success', 'error', 'warning', 'info']),
+  };
+
+  static defaultProps = {
+    type: 'info',
+  };
+
   render() {
+    const {type} = this.props;
+
     return (
-      <div className="alert alert-block alert-warn">
+      <Alert type={type} icon="icon-labs">
         {t(
           'This feature is a preview and may change in the future. Thanks for being an early adopter!'
         )}
-      </div>
+      </Alert>
     );
   }
 }
