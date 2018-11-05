cb83124de7fd0adae410c6acd2969ad813c3e49d
Mark Story <mark@sentry.io>
2018-11-05 16:10:38 -0500

    Streamline null guard.

--- src/sentry/static/sentry/app/views/organizationIntegrations/integrationRepos.jsx
+++ src/sentry/static/sentry/app/views/organizationIntegrations/integrationRepos.jsx
@@ -286,9 +286,9 @@ export default class IntegrationRepos extends AsyncComponent {
                       </Box>
                       <Box>
                         <small>
-                          <a href={repo.url}>
-                            {repo.url && repo.url.replace('https://', '')}
-                          </a>
+                          {repo.url && (
+                            <a href={repo.url}>{repo.url.replace('https://', '')}</a>
+                          )}
                         </small>
                       </Box>
                     </Flex>
