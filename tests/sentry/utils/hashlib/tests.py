8d5eef87c9e1078a6425c9c62ea76de691292559
Jan Michael Auer <jan.auer@sentry.io>
2018-11-08 14:09:34 +0100

    fix(utils): Fix hashlib for nested structures

--- tests/sentry/utils/hashlib/tests.py
+++ tests/sentry/utils/hashlib/tests.py
@@ -2,8 +2,28 @@
 
 from __future__ import absolute_import
 
-from sentry.utils.hashlib import md5_text, sha1_text
+import pytest
+import six
+
+from sentry.utils.hashlib import md5_text, sha1_text, hash_values
 from sentry.testutils import TestCase
+
+
+HASHLIB_VALUES_TESTS = (
+    ('seed', None, '44689bafd59f4b1f0b0a152062b6dede'),
+    ('seed', True, '453224968c982a58ff5782853607f47f'),
+    ('seed', False, 'a65e1fcf12a02cee799b0591b060ce0b'),
+    ('seed', 42, 'c2fa989f1320b70f5c597a7358ce3a5a'),
+    ('seed', six.binary_type('test'), '008b2c3fd625055c3132bc72ea4a6f5e'),
+    ('seed', six.text_type('test'), '0ea4106eb2e5bcd3269be5cd7f8f3912'),
+    ('seed', (4, 2), '026ebc18a8e27193bc0ed80a18d0a7c1'),
+    ('seed', {'test': 42}, '6cd8da40ed2dd728b74cdcffa30b72d4'),
+)
+
+
+@pytest.mark.parametrize('seed,value,hash', HASHLIB_VALUES_TESTS)
+def test_hash_values(seed, value, hash):
+    assert hash_values(seed, [value]) == hash
 
 
 class HashlibTest(TestCase):
