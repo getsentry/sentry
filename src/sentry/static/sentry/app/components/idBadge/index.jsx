bdf72d54b1aa06e4c4ff331c095279c6bb0f4e74
Billy Vong <billy@sentry.io>
2018-11-09 16:17:10 -0500

    fix(ui): Fix IdBadge to make sure prop has valid value
    `IdBadge` now checks to make sure a model type has a valid value before
    assuming it is of that type.  e.g. You can call with `project` and `team`
    and it will make sure there is a value before assuming it is of that type.

--- src/sentry/static/sentry/app/components/idBadge/index.jsx
+++ src/sentry/static/sentry/app/components/idBadge/index.jsx
@@ -34,7 +34,9 @@ export default class IdBadge extends React.Component {
   render() {
     // Given the set of sentry types, find the prop name that was passed to this component,
     // of which we have a mapped component for
-    let propNameWithData = Object.keys(this.props).find(key => COMPONENT_MAP.has(key));
+    let [propNameWithData] = Object.entries(this.props).find(
+      ([key, value]) => COMPONENT_MAP.has(key) && typeof value !== 'undefined'
+    );
 
     if (!propNameWithData) {
       throw new Error(
