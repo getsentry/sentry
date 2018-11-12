12dcde293d6cec089da4481444009df6a5d54f35
Lauryn Brown <lauryndbrown@gmail.com>
2018-11-12 15:09:18 -0800

    removed specific check for apihosterror.

--- src/sentry/api/bases/integration.py
+++ src/sentry/api/bases/integration.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
 import six
+import sys
+import traceback
 
 from rest_framework.response import Response
-
-from sentry.integrations.exceptions import ApiHostError
 from sentry.utils.sdk import capture_exception
 
 from .organization import OrganizationEndpoint, OrganizationPermission
@@ -14,9 +14,7 @@ class IntegrationEndpoint(OrganizationEndpoint):
     permission_classes = (OrganizationPermission, )
 
     def handle_exception(self, request, exc):
-        if isinstance(exc, ApiHostError) or exc.code == 503:
-            import sys
-            import traceback
+        if exc.code == 503:
             sys.stderr.write(traceback.format_exc())
             event_id = capture_exception()
             context = {
