f41a22185f78e48f07712575ed24d805ff0e5396
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-11-13 14:44:17 +0100

    fix: Fix wrong comment, remove timestamp

--- tests/acceptance/test_issue_details.py
+++ tests/acceptance/test_issue_details.py
@@ -26,8 +26,7 @@ class IssueDetailsTest(AcceptanceTestCase):
             platform=platform,
             default=default,
             sample_name=sample_name,
-            event_id='d964fdbd649a4cf8bfc35d18082b6b0e',
-            timestamp=1452683305,
+            event_id='d964fdbd649a4cf8bfc35d18082b6b0e'
         )
         event.group.update(
             first_seen=datetime(2015, 8, 13, 3, 8, 25, tzinfo=timezone.utc),
