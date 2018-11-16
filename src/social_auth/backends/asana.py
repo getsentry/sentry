3810302a7dfe687c938a7ced90bad47eec271d78
Markus Unterwaditzer <markus@unterwaditzer.net>
2018-11-16 19:39:26 +0100

    fix: code does not exist on requests exc (#10623)

--- src/social_auth/backends/asana.py
+++ src/social_auth/backends/asana.py
@@ -59,20 +59,18 @@ class AsanaAuth(BaseOAuth2):
         """Completes loging process, must return user instance"""
         self.process_error(self.data)
         params = self.auth_complete_params(self.validate_state())
+
+        response = requests.post(self.ACCESS_TOKEN_URL, data=params,
+                                 headers=self.auth_headers())
+        if response.status_code == 400:
+            raise AuthCanceled(self)
+
+        response.raise_for_status()
+
         try:
-            response = requests.post(self.ACCESS_TOKEN_URL, data=params,
-                                     headers=self.auth_headers())
-            response.raise_for_status()
-        except requests.exceptions.HTTPError as e:
-            if e.code == 400:
-                raise AuthCanceled(self)
-            else:
-                raise
-        else:
-            try:
-                response = response.json()
-            except (ValueError, KeyError):
-                raise AuthUnknownError(self)
+            response = response.json()
+        except (ValueError, KeyError):
+            raise AuthUnknownError(self)
 
         response.pop('data')
         self.process_error(response)
