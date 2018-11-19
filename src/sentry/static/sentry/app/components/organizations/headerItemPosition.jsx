5a02185c05bc86955cfc7b8c998db7c29073af72
ckj <chriskjennings@gmail.com>
2018-11-19 13:20:28 -0800

    fix long project/environment strings from overflowing

--- src/sentry/static/sentry/app/components/organizations/headerItemPosition.jsx
+++ src/sentry/static/sentry/app/components/organizations/headerItemPosition.jsx
@@ -7,10 +7,12 @@ const HeaderItemPosition = styled.div`
   flex: 1;
   max-width: 450px;
   height: 100%;
+  min-width: 0;
 
   /* stylelint-disable-next-line no-duplicate-selectors */
   ${AutoCompleteRoot}, ${TimeRangeRoot} {
     flex: 1;
+    min-width: 0;
   }
 `;
 
