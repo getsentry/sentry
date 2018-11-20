1698e5ff532c80a4bd077ca293b97ca00e8a6a5b
Mark Story <mark@mark-story.com>
2018-11-20 10:13:31 -0500

    feat(jira) Start config views for Jira Server (#10637)
    This doesn't do much yet but I wanted to avoid making a huge pull
    request that does all the things at once.
    
    Refs APP-801

--- src/sentry/integrations/jira_server/integration.py
+++ src/sentry/integrations/jira_server/integration.py
@@ -2,12 +2,17 @@ from __future__ import absolute_import
 
 import logging
 
+from django import forms
 from django.utils.translation import ugettext as _
 
+from sentry.identity.pipeline import IdentityProviderPipeline
 from sentry.integrations import (
     IntegrationFeatures, IntegrationProvider, IntegrationMetadata, FeatureDescription,
 )
 from sentry.integrations.jira import JiraIntegration
+from sentry.pipeline import NestedPipelineView, PipelineView
+from sentry.utils.http import absolute_uri
+from sentry.web.helpers import render_to_response
 
 logger = logging.getLogger('sentry.integrations.jira_server')
 
@@ -53,6 +58,80 @@ metadata = IntegrationMetadata(
 )
 
 
+class InstallationForm(forms.Form):
+    url = forms.CharField(
+        label=_('Jira URL'),
+        help_text=_('The base URL for your Jira Server instance, including the host and protocol.'),
+        widget=forms.TextInput(
+            attrs={'placeholder': 'https://jira.example.com'}
+        ),
+    )
+    verify_ssl = forms.BooleanField(
+        label=_('Verify SSL'),
+        help_text=_('By default, we verify SSL certificates '
+                    'when making requests to your Jira instance.'),
+        widget=forms.CheckboxInput(),
+        required=False,
+        initial=True
+    )
+    client_id = forms.CharField(
+        label=_('Jira Consumer Key'),
+        widget=forms.TextInput(
+            attrs={'placeholder': _(
+                'sentry-consumer-key')}
+        )
+    )
+    private_key = forms.CharField(
+        label=_('Jira Consumer Private Key'),
+        widget=forms.Textarea(
+            attrs={'placeholder': _('--PRIVATE KEY--')}
+        )
+    )
+
+    def clean_url(self):
+        """Strip off trailing / as they cause invalid URLs downstream"""
+        return self.cleaned_data['url'].rstrip('/')
+
+
+class InstallationGuideView(PipelineView):
+    """
+    Display a setup guide for creating an OAuth client in Jira
+    """
+
+    def dispatch(self, request, pipeline):
+        if 'completed_guide' in request.GET:
+            return pipeline.next_step()
+        return render_to_response(
+            template='sentry/integrations/jira-server-config.html',
+            request=request,
+        )
+
+
+class InstallationConfigView(PipelineView):
+    """
+    Collect the OAuth client credentials from the user.
+    """
+
+    def dispatch(self, request, pipeline):
+        if request.method == 'POST':
+            form = InstallationForm(request.POST)
+            if form.is_valid():
+                form_data = form.cleaned_data
+
+                pipeline.bind_state('installation_data', form_data)
+                return pipeline.next_step()
+        else:
+            form = InstallationForm()
+
+        return render_to_response(
+            template='sentry/integrations/jira-server-config.html',
+            context={
+                'form': form,
+            },
+            request=request,
+        )
+
+
 class JiraServerIntegration(JiraIntegration):
     pass
 
@@ -61,19 +140,51 @@ class JiraServerIntegrationProvider(IntegrationProvider):
     key = 'jira_server'
     name = 'Jira Server'
     metadata = metadata
-    integration_cls = JiraIntegration
+    integration_cls = JiraServerIntegration
+
+    needs_default_identity = True
+
+    can_add = True
 
     features = frozenset([
         IntegrationFeatures.ISSUE_BASIC,
         IntegrationFeatures.ISSUE_SYNC
     ])
 
-    can_add = False
+    setup_dialog_config = {
+        'width': 1030,
+        'height': 1000,
+    }
+
+    def _make_identity_pipeline_view(self):
+        """
+        Make the nested identity provider view.
+
+        It is important that this view is not constructed until we reach this step and the
+        ``installation_data`` is available in the pipeline state. This
+        method should be late bound into the pipeline views.
+        """
+        identity_pipeline_config = dict(
+            redirect_url=absolute_uri('/extensions/jira_server/setup/'),
+            **self.pipeline.fetch_state('installation_data')
+        )
+
+        return NestedPipelineView(
+            bind_key='identity',
+            provider_key='jira_server',
+            pipeline_cls=IdentityProviderPipeline,
+            config=identity_pipeline_config,
+        )
 
     def get_pipeline_views(self):
-        return []
+        return [
+            InstallationGuideView(),
+            InstallationConfigView(),
+            # lambda: self._make_identity_pipeline_view()
+        ]
 
     def build_integration(self, state):
+        # TODO complete OAuth
         # TODO(lb): This is wrong. Not currently operational.
         # this should be implemented.
         user = state['identity']['data']
