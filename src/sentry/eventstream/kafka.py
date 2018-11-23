4c804a75b0c297d65af80e5645171fc72afb5c2c
Armin Ronacher <armin.ronacher@active-4.com>
2018-11-23 16:55:35 +0100

    fix: Actually fix it

--- src/sentry/eventstream/kafka.py
+++ src/sentry/eventstream/kafka.py
@@ -132,7 +132,7 @@ class KafkaEventStream(EventStream):
             'project_id': event.project_id,
             # TODO(mitsuhiko): We do not want to send this incorrect
             # message but this is what snuba needs at the moment.
-            'message': event.real_message,
+            'message': event.search_message,
             'platform': event.platform,
             'datetime': event.datetime,
             'data': dict(event.data.items()),
