ce1dcc3baacff1f6cfa43fde47f52c89d7f4da02
Lyn Nagara <lyn.nagara@gmail.com>
2018-11-28 11:29:11 -0800

    use action creator in store test

--- tests/js/spec/stores/globalSelectionStore.spec.jsx
+++ tests/js/spec/stores/globalSelectionStore.spec.jsx
@@ -1,13 +1,15 @@
 import GlobalSelectionStore from 'app/stores/globalSelectionStore';
+import {updateProjects} from 'app/actionCreators/globalSelection';
 
 describe('GlobalSelectionStore', function() {
   it('get()', function() {
     expect(GlobalSelectionStore.get()).toEqual({projects: []});
   });
 
-  it('updateProjects()', function() {
+  it('updateProjects()', async function() {
     expect(GlobalSelectionStore.get().projects).toEqual([]);
-    GlobalSelectionStore.updateProjects([1]);
+    updateProjects([1]);
+    await tick();
     expect(GlobalSelectionStore.get().projects).toEqual([1]);
   });
 });
