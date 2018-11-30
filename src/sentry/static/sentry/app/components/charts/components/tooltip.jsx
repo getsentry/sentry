8e3d1afcbe6f64ec2e7de03a786eca30de1f4622
Ayesha <ayesha.omarali@gmail.com>
2018-11-29 17:47:49 -0800

    add delimeters via toLocaleString to values in tooltip

--- src/sentry/static/sentry/app/components/charts/components/tooltip.jsx
+++ src/sentry/static/sentry/app/components/charts/components/tooltip.jsx
@@ -11,6 +11,10 @@ function formatAxisLabel(value, isTimestamp, utc) {
   return getFormattedDate(value, 'MMM D, YYYY', utc);
 }
 
+function formatDelimeters(value){
+  return value.toLocaleString();
+}
+
 function getFormatter({filter, isGroupedByDate, truncate, utc}) {
   const getFilter = seriesParam => {
     const value = seriesParam.data[1];
@@ -31,8 +35,7 @@ function getFormatter({filter, isGroupedByDate, truncate, utc}) {
         .filter(getFilter)
         .map(
           s =>
-            `<div>${s.marker} ${truncationFormatter(s.seriesName, truncate)}:  ${s
-              .data[1]}</div>`
+            `<div>${s.marker} ${truncationFormatter(s.seriesName, truncate)}:  ${formatDelimeters(s.data[1])}</div>`
         )
         .join(''),
     ].join('');
