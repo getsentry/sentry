759d2969903f7402d2f11821db4030063dafe079
Mark Story <mark@mark-story.com>
2018-11-30 14:11:49 -0500

    fix(ui) Improve disabled state of radio buttons. (#10831)
    Radio buttons should not change their cursor and labels should dim
    accordingly.

--- tests/js/spec/components/forms/radioGroup.spec.jsx
+++ tests/js/spec/components/forms/radioGroup.spec.jsx
@@ -20,6 +20,21 @@ describe('RadioGroup', function() {
       expect(wrapper).toMatchSnapshot();
     });
 
+    it('renders disabled', function() {
+      const wrapper = mount(
+        <RadioGroup
+          name="radio"
+          value="choice_one"
+          disabled={true}
+          choices={[['choice_one', 'Choice One']]}
+        />
+      );
+      expect(wrapper).toMatchSnapshot();
+
+      expect(wrapper.find('RadioLineText').props().disabled).toBe(true);
+      expect(wrapper.find('RadioLineButtonFill').props().disabled).toBe(true);
+    });
+
     it('can select a different item', function() {
       const wrapper = shallow(
         <RadioGroup
