15a44e2a68d0338b685366ed2fcb9ee9fefea052
Jess MacQueen <jessmacqueen@gmail.com>
2018-12-03 12:55:36 -0800

    docs(tagstore): Add docstring to get_tag_value_paginator_for_projects

--- src/sentry/tagstore/base.py
+++ src/sentry/tagstore/base.py
@@ -320,6 +320,8 @@ class TagStorage(Service):
     def get_tag_value_paginator_for_projects(self, projects, environments, key, start, end,
                                              query=None, order_by='-last_seen'):
         """
+        Includes tags and also snuba columns, with the arrayjoin when they are nested.
+        Also supports a query parameter to do a substring match on the tag/column values.
         >>> get_tag_value_paginator_for_projects([1], [2], 'environment', query='prod')
         """
         raise NotImplementedError
