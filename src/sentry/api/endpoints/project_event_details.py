f2787bf98a25e70aa17ac79b13b1e413bceb5f95
Alex Hofsteede <alex@hofsteede.com>
2018-12-04 16:16:41 -0800

    feat: Allow id or event_id in project events endpoint (#10898)
    * feat: Allow id or event_id in project events endpoint
    
    Reprise of a previous PR that failed because there is no index
    on event_id alone. Now I've added this functionality to the project
    event details endpoint where we know we always have the project id
    available, and I've made the fronted code use this endpoint instead
    of the bare /events/x/ endpoint.

--- src/sentry/api/endpoints/project_event_details.py
+++ src/sentry/api/endpoints/project_event_details.py
@@ -7,8 +7,9 @@ from rest_framework.response import Response
 from sentry.api.base import DocSection
 from sentry.api.bases.project import ProjectEndpoint
 from sentry.api.serializers import serialize
-from sentry.models import Event
+from sentry.models import Event, Release, UserReport
 from sentry.utils.apidocs import scenario, attach_scenarios
+from sentry.utils.validators import is_event_id
 
 
 @scenario('RetrieveEventForProject')
@@ -23,6 +24,20 @@ def retrieve_event_for_project_scenario(runner):
 class ProjectEventDetailsEndpoint(ProjectEndpoint):
     doc_section = DocSection.EVENTS
 
+    def _get_release_info(self, request, event):
+        version = event.get_tag('sentry:release')
+        if not version:
+            return None
+        try:
+            release = Release.objects.get(
+                projects=event.project,
+                organization_id=event.project.organization_id,
+                version=version,
+            )
+        except Release.DoesNotExist:
+            return {'version': version}
+        return serialize(release, request.user)
+
     @attach_scenarios([retrieve_event_for_project_scenario])
     def get(self, request, project, event_id):
         """
@@ -35,16 +50,33 @@ class ProjectEventDetailsEndpoint(ProjectEndpoint):
                                           event belongs to.
         :pparam string project_slug: the slug of the project the event
                                      belongs to.
-        :pparam string event_id: the hexadecimal ID of the event to
-                                 retrieve (as reported by the raven client).
+        :pparam string event_id: the id of the event to retrieve (either the
+                                 numeric primary-key or the hexadecimal id as
+                                 reported by the raven client)
         :auth: required
         """
-        try:
-            event = Event.objects.get(
-                event_id=event_id,
-                project_id=project.id,
-            )
-        except Event.DoesNotExist:
+
+        event = None
+        # If its a numeric string, check if it's an event Primary Key first
+        if event_id.isdigit():
+            try:
+                event = Event.objects.get(
+                    id=event_id,
+                    project_id=project.id,
+                )
+            except Event.DoesNotExist:
+                pass
+        # If it was not found as a PK, and its a possible event_id, search by that instead.
+        if event is None and is_event_id(event_id):
+            try:
+                event = Event.objects.get(
+                    event_id=event_id,
+                    project_id=project.id,
+                )
+            except Event.DoesNotExist:
+                pass
+
+        if event is None:
             return Response({'detail': 'Event not found'}, status=404)
 
         Event.objects.bind_nodes([event], 'data')
@@ -72,7 +104,17 @@ class ProjectEventDetailsEndpoint(ProjectEndpoint):
         except IndexError:
             prev_event = None
 
+        try:
+            user_report = UserReport.objects.get(
+                event_id=event.event_id,
+                project=event.project,
+            )
+        except UserReport.DoesNotExist:
+            user_report = None
+
         data = serialize(event, request.user)
+        data['userReport'] = serialize(user_report, request.user)
+        data['release'] = self._get_release_info(request, event)
 
         if next_event:
             data['nextEventID'] = six.text_type(next_event.event_id)
