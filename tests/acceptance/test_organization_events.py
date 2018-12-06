80f2a48a265047d7bffbef6c3d4fcf63e7e9ff84
Billy Vong <billy@sentry.io>
2018-12-06 15:29:09 -0800

    add project in test

--- tests/acceptance/test_organization_events.py
+++ tests/acceptance/test_organization_events.py
@@ -11,6 +11,11 @@ class OrganizationEventsTest(AcceptanceTestCase):
         self.user = self.create_user('foo@example.com')
         self.org = self.create_organization(owner=None, name='Rowdy Tiger')
         self.team = self.create_team(organization=self.org, name='Mariachi Band')
+        self.project = self.create_project(
+            organization=self.org,
+            teams=[self.team],
+            name='Bengal',
+        )
         self.create_member(
             user=self.user,
             organization=self.org,
