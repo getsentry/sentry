eb66a4f546d6617b31bacd4085d6bbf85a4263cb
Billy Vong <billyvg@users.noreply.github.com>
2018-12-11 08:33:00 -0800

    analytics(metrics): Do not record API paths (#10993)

--- src/sentry/static/sentry/app/api.jsx
+++ src/sentry/static/sentry/app/api.jsx
@@ -191,7 +191,6 @@ export class Client {
             name: 'app.api.request-success',
             start: `api-request-start-${id}`,
             data: {
-              path,
               status: xhr && xhr.status,
             },
           });
@@ -205,7 +204,6 @@ export class Client {
             name: 'app.api.request-error',
             start: `api-request-start-${id}`,
             data: {
-              path,
               status: xhr && xhr.status,
             },
           });
