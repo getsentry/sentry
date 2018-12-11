ca2dc484becd55d1ee7a6243b920cd003c06d29e
MeredithAnya <meredith.a.heller@gmail.com>
2018-12-11 11:18:01 -0800

    pass actor to feature (#11005)

--- src/sentry/api/endpoints/sentry_app_details.py
+++ src/sentry/api/endpoints/sentry_app_details.py
@@ -11,13 +11,19 @@ from sentry.mediators.sentry_apps import Updater
 
 class SentryAppDetailsEndpoint(SentryAppBaseEndpoint):
     def get(self, request, sentry_app):
-        if not features.has('organizations:internal-catchall', sentry_app.owner):
+        if not features.has('organizations:internal-catchall',
+                            sentry_app.owner,
+                            actor=request.user):
+
             return Response(status=404)
 
         return Response(serialize(sentry_app, request.user))
 
     def put(self, request, sentry_app):
-        if not features.has('organizations:internal-catchall', sentry_app.owner):
+        if not features.has('organizations:internal-catchall',
+                            sentry_app.owner,
+                            actor=request.user):
+
             return Response(status=404)
 
         serializer = SentryAppSerializer(data=request.DATA, partial=True)
