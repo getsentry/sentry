f7492bcdc28675f0dfbd0b1af28c81183dfb7217
Mark Story <mark@sentry.io>
2018-12-12 17:34:03 -0800

    feat(plugins) Hide plugins that also have integrations
    By hiding legacy plugins in the UI we make it a bit harder for customers
    to enable plugins we don't want them to use. This should help guide them
    towards the organization integrations which we plan on supporting more.
    
    Fixes APP-890

---
+++ tests/sentry/api/endpoints/test_project_plugins.py
@@ -0,0 +1,33 @@
+from __future__ import absolute_import
+
+from django.core.urlresolvers import reverse
+
+from sentry.testutils import APITestCase
+
+
+class ProjectPluginsTest(APITestCase):
+    def test_get(self):
+        project = self.create_project()
+
+        self.login_as(user=self.user)
+
+        url = reverse(
+            'sentry-api-0-project-plugins',
+            kwargs={
+                'organization_slug': project.organization.slug,
+                'project_slug': project.slug,
+            }
+        )
+        response = self.client.get(url)
+        assert response.status_code == 200, (response.status_code, response.content)
+        assert len(response.data) == 9
+        print response.data
+
+        auto_tag = response.data[0]
+        assert auto_tag['name'] == 'Auto Tag: Browsers'
+        assert auto_tag['enabled'] is True
+        assert auto_tag['isHidden'] is False
+        assert 'version' in auto_tag
+        assert 'author' in auto_tag
+        assert 'slug' in auto_tag
+        assert 'shortName' in auto_tag
