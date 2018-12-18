611f7ede9251ed952cc46061d2b4534faa23f379
Brett Hoerner <brett@bretthoerner.com>
2018-12-18 15:56:31 -0600

    Remove todo

--- src/sentry/search/snuba/backend.py
+++ src/sentry/search/snuba/backend.py
@@ -430,8 +430,6 @@ class SnubaSearchBackend(ds.DjangoSearchBackend):
         # in its queries...
         paginator_results.max_hits = max_results.max_hits
 
-        # TODO: Inject result count from max_results, issues with cursors though.
-
         # HACK: We're using the SequencePaginator to mask the complexities of going
         # back and forth between two databases. This causes a problem with pagination
         # because we're 'lying' to the SequencePaginator (it thinks it has the entire
