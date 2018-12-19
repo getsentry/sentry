13fadb9084323e5886f52b66ca93e8988eb88dfd
Lyn Nagara <lyn.nagara@gmail.com>
2018-12-18 16:29:59 -0800

    ref(releases): Reorder and codesplit release detail pages (#11083)
    - Move release files to same folder
    - Move tests to folder structure that mirrors source
    - Codesplit all project release detail pages
    - Move PullRequestLink to components since this isn't part of releases

--- tests/js/spec/views/releases/detail/releaseDetails.spec.jsx
+++ tests/js/spec/views/releases/detail/releaseDetails.spec.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import {mount} from 'enzyme';
 
-import ReleaseDetails from 'app/views/releaseDetails';
+import ReleaseDetails from 'app/views/releases/detail/releaseDetails';
 
 describe('ReleaseDetails', function() {
   let deleteMock;
