a2539981d615267eaf20f4003951c94f910a703a
Lyn Nagara <lyn.nagara@gmail.com>
2018-12-20 11:03:47 -0800

    feat(user-feedback): Organization user feedback
    Add global selection header to org user feedback page, allowing
    filtering on projects, environments and date range.

--- src/sentry/static/sentry/app/views/userFeedback/utils.jsx
+++ src/sentry/static/sentry/app/views/userFeedback/utils.jsx
@@ -1,38 +1,10 @@
-import {Client} from 'app/api';
+import {pick} from 'lodash';
 import qs from 'query-string';
 
 const DEFAULT_STATUS = 'unresolved';
 
 /**
- * Fetch user feedback for organization filtered by the query provided.
- * Either statsPeriod or start, end and utc values should be provided.
- *
- * @param {Object} organization
- * @param {Object} query
- * @param {Number[]} query.projects
- * @param {String[]} query.environments
- * @param {String} [query.statsPeriod]
- * @param {String} [query.start]
- * @param {String} [query.end]
- * @param {Boolean} [query.utc]
- * @returns {Promise<Array>}
- */
-export function fetchUserFeedback(organization, query = {}) {
-  const api = new Client();
-
-  return api.requestPromise(`/organizations/${organization.slug}/user-feedback/`, {
-    includeAllArgs: true,
-    query: {
-      per_page: 50,
-      ...query,
-    },
-  });
-}
-
-/**
  * Get query for API given the current location.search string
- * We are using qs.parse since location.query re-uses the same object making it
- * incorrectly seem like the query string has not changed
  *
  * @param {String} search
  * @returns {Object}
@@ -41,9 +13,19 @@ export function getQuery(search) {
   const query = qs.parse(search);
 
   const status = typeof query.status !== 'undefined' ? query.status : DEFAULT_STATUS;
-  const cursor = query.cursor;
 
-  const queryParams = {status, cursor};
+  const queryParams = {
+    status,
+    ...pick(query, [
+      'cursor',
+      'project',
+      'environment',
+      'statsPeriod',
+      'start',
+      'end',
+      'utc',
+    ]),
+  };
 
   return queryParams;
 }
