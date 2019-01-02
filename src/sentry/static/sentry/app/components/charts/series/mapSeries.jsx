56471849975d8616e07295a2f564a5516bf6519b
Billy Vong <billy@sentry.io>
2019-01-02 12:33:48 -0800

    remove commented code

--- src/sentry/static/sentry/app/components/charts/series/mapSeries.jsx
+++ src/sentry/static/sentry/app/components/charts/series/mapSeries.jsx
@@ -2,8 +2,6 @@ import 'echarts/lib/chart/map';
 import 'echarts/lib/component/visualMap';
 import 'echarts/map/js/world';
 
-// import theme from 'app/utils/theme';
-
 export default function MapSeries(props = {}) {
   return {
     roam: true,
