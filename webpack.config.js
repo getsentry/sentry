7daee7e9120b52cd007a0654d1bce606b788a72d
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-01-02 13:09:39 -0600

    ref(webpack): Remove less loader for modules (#11126)

--- webpack.config.js
+++ webpack.config.js
@@ -124,24 +124,6 @@ var appConfig = {
           },
         ],
       },
-      // loader for dynamic styles imported into components (embedded as js)
-      {
-        test: /\.less$/,
-        use: [
-          {
-            loader: 'style-loader',
-          },
-          {
-            loader: 'css-loader',
-            options: {
-              minimize: IS_PRODUCTION,
-            },
-          },
-          {
-            loader: 'less-loader',
-          },
-        ],
-      },
       {
         test: /\.css/,
         use: [
@@ -214,7 +196,7 @@ var appConfig = {
         : path.join(__dirname, 'src/sentry/integration-docs/_platforms.json'),
     },
     modules: [path.join(__dirname, staticPrefix), 'node_modules'],
-    extensions: ['.less', '.jsx', '.js', '.json'],
+    extensions: ['.jsx', '.js', '.json'],
   },
   output: {
     path: distPath,
