319668b51e04179053c8084a5359dbc8d2e8164f
Billy Vong <billy@sentry.io>
2019-01-03 11:07:57 -0800

    cleanup

--- tests/js/throw-on-react-error.js
+++ tests/js/throw-on-react-error.js
@@ -1,18 +1,13 @@
 // eslint-disable-next-line no-console
 const originalConsoleError = console.error;
+const BAD_ERRORS = /(Failed prop type|Failed child context type|React does not recognize the `[^`]+` prop on a DOM element)/;
 
 // eslint-disable-next-line no-console
 console.error = (message, ...args) => {
   originalConsoleError(message, ...args);
 
   // List of `console.error` messages to fail on
-  if (
-    /(Failed prop type|Failed child context type|React does not recognize the `[^`]+` prop on a DOM element)/.test(
-      message
-    )
-  ) {
+  if (BAD_ERRORS.test(message)) {
     throw new Error(message);
   }
 };
-
-// projectReleases, onboarding
