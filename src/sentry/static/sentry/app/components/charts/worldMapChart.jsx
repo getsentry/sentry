a9e0475a8d82968092691b189550e4cff246915a
Billy Vong <billyvg@users.noreply.github.com>
2019-01-03 08:19:34 -0800

    feat(charts): Add World Map "chart" (#11330)
    * feat(charts): Add World Map "chart"
    
    This adds a world map "chart" and a mapping of country codes to country name (for eCharts)
    
    * remove commented code
    
    * Update worldMapChart.jsx

---
+++ src/sentry/static/sentry/app/components/charts/worldMapChart.jsx
@@ -0,0 +1,79 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import BaseChart from './baseChart';
+import MapSeries from './series/mapSeries';
+
+export default class WorldMapChart extends React.Component {
+  static propTypes = {
+    ...BaseChart.propTypes,
+    seriesOptions: PropTypes.object,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      countryCodesMap: null,
+    };
+  }
+
+  async componentDidMount() {
+    const countryCodesMap = await import(/* webpackChunkName: "countryCodesMap" */ 'app/data/countryCodesMap');
+
+    // eslint-disable-next-line
+    this.setState({
+      countryCodesMap: countryCodesMap.default,
+    });
+  }
+
+  render() {
+    if (this.state.countryCodesMap === null) {
+      return null;
+    }
+
+    const {series, seriesOptions, ...props} = this.props;
+    const processedSeries = series.map(({seriesName, data, ...options}) => {
+      return MapSeries({
+        ...seriesOptions,
+        ...options,
+        mapType: 'world',
+        name: seriesName,
+        nameMap: this.state.countryCodesMap,
+        data,
+      });
+    });
+
+    return (
+      <BaseChart
+        options={{
+          visualMap: {
+            left: 'right',
+            min: 0,
+            max: 20,
+            inRange: {
+              color: [
+                '#313695',
+                '#4575b4',
+                '#74add1',
+                '#abd9e9',
+                '#e0f3f8',
+                '#ffffbf',
+                '#fee090',
+                '#fdae61',
+                '#f46d43',
+                '#d73027',
+                '#a50026',
+              ],
+            },
+            text: ['High', 'Low'],
+            calculable: true,
+          },
+        }}
+        {...props}
+        yAxis={null}
+        xAxis={null}
+        series={processedSeries}
+      />
+    );
+  }
+}
