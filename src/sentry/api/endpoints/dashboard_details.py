97b528b53a62c3f2756f285c7d8bff95da880f8e
Lauryn Brown <lauryndbrown@gmail.com>
2019-01-07 15:29:57 -0800

    quick outline of details endpoint.

---
+++ src/sentry/api/endpoints/dashboard_details.py
@@ -0,0 +1,65 @@
+from __future__ import absolute_import
+
+
+from django.http import Http404
+
+from sentry.api.base import DocSection
+from sentry.api.bases.organization import (
+    OrganizationEndpoint
+)
+from sentry.api.serializers import serialize
+from sentry.models import Dashboard, ObjectStatus
+
+
+class DashboardDetailsEndpoint(OrganizationEndpoint):
+
+    doc_section = DocSection.ORGANIZATIONS
+
+    def get(self, request, organization, dashboard_id):
+        """
+        Retrieve an Organization's Dashboard
+        ````````````````````````````````````
+
+        Return details on an individual organization's dashboard.
+
+        :pparam string organization_slug: the slug of the organization the
+                                          dashboard belongs to.
+        :auth: required
+        """
+        try:
+            dashboard = Dashboard.objects.get(
+                id=dashboard_id,
+                organization=organization,
+            )
+        except Dashboard.DoesNotExist:
+            raise Http404
+
+        return self.respond(serialize(dashboard, request.user))
+
+    def delete(self, request, organization, dashboard_id):
+        try:
+            dashboard = Dashboard.objects.get(
+                id=dashboard_id,
+                organization=organization,
+            )
+        except Dashboard.DoesNotExist:
+            raise Http404
+
+        Dashboard.objects.filter(
+            id=dashboard.id,
+            status=ObjectStatus.VISIBLE,
+        ).update(status=ObjectStatus.PENDING_DELETION)
+
+        return self.respond(status=204)
+
+    def put(self, request, organization, dashboard_id):
+        try:
+            Dashboard.objects.get(
+                id=dashboard_id,
+                organization=organization,
+            )
+        except Dashboard.DoesNotExist:
+            raise Http404
+
+        # update needs a serializer
+        return self.respond(status=200)
