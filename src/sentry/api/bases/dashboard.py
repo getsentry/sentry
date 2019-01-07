fe7db0ad66f1ab2f00e31f61e7544f0f77a58040
Lauryn Brown <lauryndbrown@gmail.com>
2019-01-07 15:58:27 -0800

    Added a bit more to serializers.

--- src/sentry/api/bases/dashboard.py
+++ src/sentry/api/bases/dashboard.py
@@ -27,14 +27,30 @@ class OrganizationDashboardEndpoint(OrganizationEndpoint):
 
 
 from rest_framework import serializers
-# from sentry.api.serializers.rest_framework import ListField
+from sentry.api.serializers.rest_framework import ListField
+from sentry.models import WidgetDisplayTypes
 
 
 class DashboardSerializer(serializers.Serializer):
     title = serializers.CharField(required=True)
+    data = ListField(
+        child=serializers.CharField(),
+        required=False,
+        allow_null=True,
+    )
 
 
 class WidgetSerializer(serializers.Serializer):
-    # dashboard_order = IntegerField()  # hmmm.... what do we use then?
+    dashboard_order = serializers.IntegerField(min_value=0, required=True)
     display_type = serializers.CharField(required=True)
-    # data = ??
+    data = ListField(
+        child=serializers.CharField(),
+        required=True,
+    )
+
+    def validate_display_type(self, attrs, source):
+        display_type = attrs[source]
+        if display_type not in WidgetDisplayTypes.__members__:
+            raise ValueError('Widget display_type %s not recognized.' % display_type)
+
+        return attrs
