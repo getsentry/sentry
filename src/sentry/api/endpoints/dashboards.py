f7f1603eac1323fe419d59ec7462a4eecdbdfe2c
Lauryn Brown <lauryndbrown@gmail.com>
2019-01-07 15:44:59 -0800

    Added post to the endpoint.

--- src/sentry/api/endpoints/dashboards.py
+++ src/sentry/api/endpoints/dashboards.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import
 
+
+from django.db import IntegrityError, transaction
 from sentry.api.base import DocSection
 from sentry.api.bases.organization import OrganizationEndpoint
+from sentry.api.bases.dashboard import DashboardSerializer
 from sentry.api.serializers import serialize
-from sentry.models import Dashboards
+from sentry.models import Dashboard
 from rest_framework.response import Response
 
 
@@ -22,7 +25,7 @@ class DashboardsEndpoint(OrganizationEndpoint):
         :qparam string query: the title of the dashboard being searched for.
         :auth: required
         """
-        dashboards = Dashboards.objects.filter(
+        dashboards = Dashboard.objects.filter(
             organization_id=organization.id
         )
         query = request.GET.get('query')
@@ -33,3 +36,29 @@ class DashboardsEndpoint(OrganizationEndpoint):
 
         context = serialize(dashboards, request.user)
         return Response(context)
+
+    def post(self, request, organization):
+        serializer = DashboardSerializer(data=request.DATA, context={'organization': organization})
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        result = serializer.object
+
+        try:
+            with transaction.atomic():
+                dashboard = Dashboard.objects.create(
+                    organization_id=organization.id,
+                    title=result['title'],
+                    owner=result.get('owner') or request.user,
+                    data=result.get('data'),
+                    date_added=result.get('dateAdded'),
+                )
+        except IntegrityError:
+            # dashboard, created = Dashboard.objects.get(
+            #     organization_id=organization.id,
+            #     version=result['title'],
+            # ), False
+            pass  # hmm what to do here?
+
+        return Response(serialize(dashboard, request.user), status=201)
