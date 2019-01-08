05d40f1210786cb4a334eecc7079030171be0cd7
Mark Story <mark@mark-story.com>
2019-01-08 10:21:53 -0500

    fix(plugins) Catch more errors when sending notifications. (#11357)
    The old hipchat plugin uses urllib2 which has a different HTTPError than
    urllib3.
    
    Fixes SENTRY-8en

--- src/sentry/plugins/bases/notify.py
+++ src/sentry/plugins/bases/notify.py
@@ -9,6 +9,7 @@ from __future__ import absolute_import, print_function
 
 import logging
 import six
+import urllib2
 from six.moves.urllib.parse import (
     urlparse,
     urlencode,
@@ -73,7 +74,7 @@ class NotificationPlugin(Plugin):
         try:
             return self.notify_users(event.group, event, triggering_rules=[
                                      r.label for r in notification.rules])
-        except (SSLError, HTTPError, ApiError, PluginError) as err:
+        except (SSLError, HTTPError, ApiError, PluginError, urllib2.HTTPError) as err:
             self.logger.info('notification-plugin.notify-failed.', extra={
                 'error': six.text_type(err),
                 'plugin': self.slug
