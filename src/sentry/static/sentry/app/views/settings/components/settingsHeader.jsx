bb9a453773099f1ef2acc2bd74b79eefb03c3079
ckj <chriskjennings@gmail.com>
2019-01-08 14:32:35 -0800

    Remove logo from breadcrumb

--- src/sentry/static/sentry/app/views/settings/components/settingsHeader.jsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsHeader.jsx
@@ -4,7 +4,7 @@ const SettingsHeader = styled.div`
   position: sticky;
   top: 0;
   z-index: ${p => p.theme.zIndex.header};
-  padding: ${p => p.theme.grid * 3}px 30px;
+  padding: 20px 30px;
   border-bottom: 1px solid ${p => p.theme.borderLight};
   background: #fff;
 `;
