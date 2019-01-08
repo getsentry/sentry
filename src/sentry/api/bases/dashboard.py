1149cdb265275aad449aee7f9ada0ec5069a5aea
Lauryn Brown <lauryndbrown@gmail.com>
2019-01-08 13:21:20 -0800

    removed widget serializer.

--- src/sentry/api/bases/dashboard.py
+++ src/sentry/api/bases/dashboard.py
@@ -27,30 +27,7 @@ class OrganizationDashboardEndpoint(OrganizationEndpoint):
 
 
 from rest_framework import serializers
-from sentry.models import WidgetDisplayTypes
 
 
 class DashboardSerializer(serializers.Serializer):
     title = serializers.CharField(required=True)
-    # TODO(lb): Can't figure out how to represent a json blob.
-    # data = ListField(
-    #     child=serializers.CharField(),
-    #     required=False,
-    #     allow_null=True,
-    # )
-
-
-class WidgetSerializer(serializers.Serializer):
-    dashboard_order = serializers.IntegerField(min_value=0, required=True)
-    display_type = serializers.CharField(required=True)
-    # TODO(lb): Can't figure out how to represent a json blob.
-    # data = ListField(
-    #     child=serializers.CharField(),
-    #     required=True,
-    # )
-
-    def validate_display_type(self, attrs, source):
-        display_type = attrs[source]
-        if display_type not in WidgetDisplayTypes.__members__:
-            raise ValueError('Widget display_type %s not recognized.' % display_type)
-        return attrs
