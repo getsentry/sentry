922a670010670e3a9a95df08d65ae1d6924975ff
Meredith <meredith@getsentry.com>
2019-01-09 13:45:11 -0800

    move logic into send_alert_event task

--- src/sentry/tasks/sentry_apps.py
+++ src/sentry/tasks/sentry_apps.py
@@ -5,11 +5,39 @@ from requests.exceptions import RequestException
 from sentry.http import safe_urlopen
 from sentry.tasks.base import instrumented_task, retry
 from sentry.utils.http import absolute_uri
-from sentry.models import SentryAppInstallation
+from sentry.models import SentryAppInstallation, SentryApp
 from sentry.api.serializers import AppPlatformEvent
 
 
 def notify_sentry_app(event, futures):
+    for f in futures:
+        sentry_app = f.kwargs['sentry_app']
+
+        send_alert_event.delay(
+            event=event,
+            rule=f.rule.label,
+            sentry_app_id=sentry_app.id,
+        )
+
+
+@instrumented_task(
+    name='sentry.tasks.sentry_apps.send_alert_event', default_retry_delay=60 * 5, max_retries=5
+)
+@retry(on=(RequestException, ))
+def send_alert_event(event, rule, sentry_app_id):
+    try:
+        sentry_app = SentryApp.objects.get(id=sentry_app_id)
+    except SentryApp.DoesNotExist:
+        return
+
+    try:
+        install = SentryAppInstallation.objects.get(
+            organization=event.project.organization_id,
+            sentry_app=sentry_app,
+        )
+    except SentryAppInstallation.DoesNotExist:
+        return
+
     group = event.group
     project = group.project
     project_url_base = absolute_uri(u'/{}/{}'.format(
@@ -36,32 +64,16 @@ def notify_sentry_app(event, futures):
     data = {
         'event': event_context,
     }
-    for f in futures:
-        sentry_app = f.kwargs['sentry_app']
-        try:
-            install = SentryAppInstallation.objects.get(
-                organization=event.project.organization_id,
-                sentry_app=sentry_app,
-            )
-        except SentryAppInstallation.DoesNotExist:
-            continue
 
-        data['triggered_rule'] = f.rule.label
+    data['triggered_rule'] = rule
 
-        request_data = AppPlatformEvent(
-            resource='event_alert',
-            action='triggered',
-            install=install,
-            data=data,
-        )
-        send_alert_event.delay(sentry_app=sentry_app, request_data=request_data)
+    request_data = AppPlatformEvent(
+        resource='event_alert',
+        action='triggered',
+        install=install,
+        data=data,
+    )
 
-
-@instrumented_task(
-    name='sentry.tasks.sentry_apps.send_alert_event', default_retry_delay=60 * 5, max_retries=5
-)
-@retry(on=(RequestException, ))
-def send_alert_event(sentry_app, request_data):
     safe_urlopen(
         url=sentry_app.webhook_url,
         data=request_data.body,
