2b5c661851c625f637602e054a4e842965f54605
Billy Vong <billyvg@users.noreply.github.com>
2019-01-09 09:12:32 -0800

    feat(discover): Zerofill queries that are grouped by time (#11384)
    This will zerofill results when a query has the `rollup` parameter. This will give us more accurate charts when there are time periods without any events.

--- tests/snuba/test_organization_discover_query.py
+++ tests/snuba/test_organization_discover_query.py
@@ -217,6 +217,44 @@ class OrganizationDiscoverQueryTest(APITestCase, SnubaTestCase):
         assert(response.data['data'][0]['project.name']) == 'bar'
         assert(response.data['data'][0]['count']) == 1
 
+    def test_zerofilled_dates_when_rollup_relative(self):
+        with self.feature('organizations:discover'):
+            url = reverse('sentry-api-0-organization-discover-query', args=[self.org.slug])
+            response = self.client.post(url, {
+                'projects': [self.project.id],
+                'aggregations': [['count()', '', 'count']],
+                'fields': ['project.name'],
+                'groupby': ['time'],
+                'orderby': 'time',
+                'range': '5d',
+                'rollup': 86400,
+            })
+        assert response.status_code == 200, response.content
+        assert len(response.data['data']) == 7
+        assert(response.data['data'][6]['time']) > response.data['data'][5]['time']
+        assert(response.data['data'][6]['project.name']) == 'bar'
+        assert(response.data['data'][6]['count']) == 1
+
+    def test_zerofilled_dates_when_rollup_absolute(self):
+        with self.feature('organizations:discover'):
+            url = reverse('sentry-api-0-organization-discover-query', args=[self.org.slug])
+            response = self.client.post(url, {
+                'projects': [self.project.id],
+                'aggregations': [['count()', '', 'count']],
+                'fields': ['project.name'],
+                'groupby': ['time'],
+                'orderby': '-time',
+                'start': (datetime.now() - timedelta(seconds=60)).strftime('%Y-%m-%dT%H:%M:%S'),
+                'end': (datetime.now()).strftime('%Y-%m-%dT%H:%M:%S'),
+                'rollup': 10,
+            })
+
+        assert response.status_code == 200, response.content
+        assert len(response.data['data']) == 8
+        assert(response.data['data'][1]['time']) > response.data['data'][2]['time']
+        assert(response.data['data'][1]['project.name']) == 'bar'
+        assert(response.data['data'][1]['count']) == 1
+
     def test_uniq_project_name(self):
         with self.feature('organizations:discover'):
             url = reverse('sentry-api-0-organization-discover-query', args=[self.org.slug])
