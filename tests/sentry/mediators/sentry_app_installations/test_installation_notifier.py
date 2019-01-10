1056fb33bf3f4b0aead10583a05e5f814650c226
MeredithAnya <meredith.a.heller@gmail.com>
2019-01-10 14:56:16 -0800

    ref(app-platform): change sentry app webhooks schema (#11426)
    * ref(app-platform): change sentry app webhook schema

--- tests/sentry/mediators/sentry_app_installations/test_installation_notifier.py
+++ tests/sentry/mediators/sentry_app_installations/test_installation_notifier.py
@@ -5,13 +5,22 @@ from mock import patch
 from sentry.mediators import sentry_apps
 from sentry.mediators.sentry_app_installations import Creator, InstallationNotifier
 from sentry.testutils import TestCase
+from sentry.testutils.helpers.faux import faux
+
+
+class DictContaining(object):
+    def __init__(self, *keys):
+        self.keys = keys
+
+    def __eq__(self, other):
+        return all([k in other.keys() for k in self.keys])
 
 
 class TestInstallationNotifier(TestCase):
     def setUp(self):
         super(TestInstallationNotifier, self).setUp()
 
-        self.user = self.create_user()
+        self.user = self.create_user(name='foo')
         self.org = self.create_organization(owner=self.user)
 
         self.sentry_app = sentry_apps.Creator.run(
@@ -34,24 +43,35 @@ class TestInstallationNotifier(TestCase):
             user=self.user,
         )
 
-        safe_urlopen.assert_called_once_with(
-            'https://example.com',
-            json={
-                'action': 'installation',
-                'installation': {
-                    'uuid': self.install.uuid,
-                },
-                'data': {
-                    'app': {
-                        'uuid': self.sentry_app.uuid,
-                        'slug': self.sentry_app.slug,
-                    },
-                    'organization': {
-                        'slug': self.org.slug,
-                    },
-                    'uuid': self.install.uuid,
-                    'code': self.install.api_grant.code,
-                },
+        data = faux(safe_urlopen).kwargs['data']
+
+        assert data == {
+            'action': 'created',
+            'installation': {
+                'uuid': self.install.uuid,
             },
-            timeout=5,
-        )
+            'data': {
+                'app': {
+                    'uuid': self.sentry_app.uuid,
+                    'slug': self.sentry_app.slug,
+                },
+                'organization': {
+                    'slug': self.org.slug,
+                },
+                'uuid': self.install.uuid,
+                'code': self.install.api_grant.code,
+            },
+            'actor': {
+                'id': self.user.id,
+                'name': self.user.name,
+                'type': 'user',
+            },
+        }
+
+        assert faux(safe_urlopen).kwarg_equals('headers', DictContaining(
+            'Content-Type',
+            'Request-ID',
+            'Sentry-Hook-Resource',
+            'Sentry-Hook-Timestamp',
+            'Sentry-Hook-Signature',
+        ))
