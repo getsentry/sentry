e167521e321c88890bff0db846c40c0ebc96e84c
Billy Vong <billy@sentry.io>
2019-01-10 16:37:57 -0800

    ref(charts): Make zoom reusable in charts
    This allows any chart to be able to zoom

--- src/sentry/static/sentry/app/components/charts/utils.jsx
+++ src/sentry/static/sentry/app/components/charts/utils.jsx
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 const DEFAULT_TRUNCATE_LENGTH = 80;
 
 export function truncationFormatter(value, truncate) {
@@ -9,4 +11,14 @@ export function truncationFormatter(value, truncate) {
   return value.length > truncationLength
     ? value.substring(0, truncationLength) + '…'
     : value;
+}
+
+export function useHourlyInterval(datetimeObj) {
+  const {period, start, end} = datetimeObj;
+
+  if (typeof period === 'string') {
+    return period.endsWith('h') || period === '1d';
+  }
+
+  return moment(end).diff(start, 'hours') <= 24;
 }
--- tests/js/helpers/charts.jsx
+++ tests/js/helpers/charts.jsx
@@ -28,8 +28,14 @@ export const chart = {
 
 // eslint-disable-next-line
 export const doZoom = (wrapper, chart = chart) => {
-  wrapper.instance().handleDataZoom({}, chart);
-  wrapper.instance().handleChartFinished();
+  wrapper
+    .find('ChartZoom')
+    .instance()
+    .handleDataZoom({}, chart);
+  wrapper
+    .find('ChartZoom')
+    .instance()
+    .handleChartFinished();
 };
 
 export const mockZoomRange = (rangeStart, rangeEnd) => {
