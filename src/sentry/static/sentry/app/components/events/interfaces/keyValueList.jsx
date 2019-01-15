6b6167887f6b4e491b018fa472eabb37e1593a24
Jan Michael Auer <jan.auer@sentry.io>
2019-01-15 18:28:01 +0100

    ref(ui): Move device name rendering to device context

--- src/sentry/static/sentry/app/components/events/interfaces/keyValueList.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/keyValueList.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import _ from 'lodash';
 
-import DeviceName from 'app/components/deviceName';
 import ContextData from 'app/components/contextData';
 
 class KeyValueList extends React.Component {
@@ -52,9 +51,7 @@ class KeyValueList extends React.Component {
                 <tr key={key}>
                   <td className="key">{key}</td>
                   <td className="value">
-                    <pre>
-                      <DeviceName>{'' + value || ' '}</DeviceName>
-                    </pre>
+                    <pre>{'' + value || ' '}</pre>
                   </td>
                 </tr>,
               ];
