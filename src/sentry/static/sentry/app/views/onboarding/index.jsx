ddbc898e442a7a5263cc670dd1f3f41b9ff7dcfa
Chris Clark <christopherchristopherclark@gmail.com>
2019-01-17 13:04:50 -0800

    fix(design-consistency): resolve background bug in onboarding flow for-realsies

--- src/sentry/static/sentry/app/views/onboarding/index.jsx
+++ src/sentry/static/sentry/app/views/onboarding/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import DocumentTitle from 'react-document-title';
+import styled from 'react-emotion';
 
 import ProgressNodes from 'app/views/onboarding/progress';
 
@@ -11,15 +12,24 @@ class OnboardingWizard extends React.Component {
 
   render() {
     return (
-      <div className="onboarding-container">
-        <DocumentTitle title={'Sentry'} />
-        <div className="step-container">
-          <ProgressNodes params={this.props.params} />
-          <div>{this.props.children}</div>
+      <OnboardingBackground>
+        <div className="onboarding-container">
+          <DocumentTitle title={'Sentry'} />
+          <div className="step-container">
+            <ProgressNodes params={this.props.params} />
+            <div>{this.props.children}</div>
+          </div>
         </div>
-      </div>
+      </OnboardingBackground>
     );
   }
 }
+
+const OnboardingBackground = styled('div')`
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  background: #fff;
+`;
 
 export default OnboardingWizard;
