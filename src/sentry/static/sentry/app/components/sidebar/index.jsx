getsentry/sentry

commit 30b1dcd7126d3429ed08668b66e9a6e9b77f42a6

fix(sidebar): Drop falsy value from sidebar when navigating (#11612)
Falsy values aren't valid in URLs for global selection pages

src/sentry/static/sentry/app/components/sidebar/index.jsx
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import {ThemeProvider} from 'emotion-theming';
-import {isEqual, pick} from 'lodash';
+import {isEqual, pick, identity} from 'lodash';
 import {withRouter, browserHistory} from 'react-router';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -157,7 +157,7 @@ class Sidebar extends React.Component {
 
   // Keep the global selection querystring values in the path
   navigateWithGlobalSelection = (pathname, evt) => {
-    const query = pick(this.props.location.query, Object.values(URL_PARAM));
+    const query = pick(this.props.location.query, Object.values(URL_PARAM) && identity);
 
     // Handle cmd-click (mac) and meta-click (linux)
     if (evt.metaKey) {

commit 8e1ef43b935d5d33f9260b9eed5500fcb1e5d97c

fix(sidebar): Fix sidebar navigation
Fixes a bug introduced in  https://github.com/getsentry/sentry/pull/11612
that causes the sidebar to set an incorrect query string on navigation.
This also broke back button behavior.

src/sentry/static/sentry/app/components/sidebar/index.jsx
@@ -1,6 +1,6 @@
 import $ from 'jquery';
 import {ThemeProvider} from 'emotion-theming';
-import {isEqual, pick, identity} from 'lodash';
+import {isEqual, pick, pickBy, identity} from 'lodash';
 import {withRouter, browserHistory} from 'react-router';
 import PropTypes from 'prop-types';
 import React from 'react';
@@ -157,7 +157,10 @@ class Sidebar extends React.Component {
 
   // Keep the global selection querystring values in the path
   navigateWithGlobalSelection = (pathname, evt) => {
-    const query = pick(this.props.location.query, Object.values(URL_PARAM) && identity);
+    const query = pickBy(
+      pick(this.props.location.query, Object.values(URL_PARAM)),
+      identity
+    );
 
     // Handle cmd-click (mac) and meta-click (linux)
     if (evt.metaKey) {