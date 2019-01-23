getsentry/sentry

commit 3e40e67ac7fa06ce20f776273b67157cf163f738

feat: Update compact issue component links (#11649)
Update compact issue component to conditionally render links to the new
issue pages if the user is in global views, otherwise render existing
links.

src/sentry/static/sentry/app/components/compactIssue.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import {withRouter} from 'react-router';
 import createReactClass from 'create-react-class';
 import Reflux from 'reflux';
 import {Flex, Box} from 'grid-emotion';
@@ -18,8 +19,6 @@ import {PanelItem} from 'app/components/panels';
 class CompactIssueHeader extends React.Component {
   static propTypes = {
     data: PropTypes.object.isRequired,
-    orgId: PropTypes.string.isRequired,
-    projectId: PropTypes.string.isRequired,
   };
 
   getTitle = () => {
@@ -63,9 +62,14 @@ class CompactIssueHeader extends React.Component {
   };
 
   render() {
-    let {orgId, projectId, data} = this.props;
+    let {data, params: {orgId, projectId}} = this.props;
 
     let styles = {};
+
+    let basePath = projectId
+      ? `/${orgId}/${projectId}/issues/`
+      : `/organizations/${orgId}/issues/`;
+
     if (data.subscriptionDetails && data.subscriptionDetails.reason === 'mentioned') {
       styles = {color: '#57be8c'};
     }
@@ -76,7 +80,7 @@ class CompactIssueHeader extends React.Component {
             <span className="error-level truncate" title={data.level} />
           </Box>
           <h3 className="truncate">
-            <ProjectLink to={`/${orgId}/${projectId}/issues/${data.id}/`}>
+            <ProjectLink to={`${basePath}${data.id}/`}>
               <span className="icon icon-soundoff" />
               <span className="icon icon-star-solid" />
               {this.getTitle()}
@@ -85,14 +89,11 @@ class CompactIssueHeader extends React.Component {
         </Flex>
         <div className="event-extra">
           <span className="project-name">
-            <ProjectLink to={`/${orgId}/${projectId}/`}>{data.project.slug}</ProjectLink>
+            <ProjectLink to={basePath}>{data.project.slug}</ProjectLink>
           </span>
           {data.numComments !== 0 && (
             <span>
-              <Link
-                to={`/${orgId}/${projectId}/issues/${data.id}/activity/`}
-                className="comments"
-              >
+              <Link to={`${basePath}${data.id}/activity/`} className="comments">
                 <span className="icon icon-comments" style={styles} />
                 <span className="tag-count">{data.numComments}</span>
               </Link>
@@ -111,7 +112,6 @@ const CompactIssue = createReactClass({
   propTypes: {
     data: PropTypes.object,
     id: PropTypes.string,
-    orgId: PropTypes.string,
     statsPeriod: PropTypes.string,
     showActions: PropTypes.bool,
   },
@@ -159,7 +159,7 @@ const CompactIssue = createReactClass({
 
     this.api.bulkUpdate(
       {
-        orgId: this.props.orgId,
+        orgId: this.props.params.orgId,
         projectId: issue.project.slug,
         itemIds: [issue.id],
         data,
@@ -195,8 +195,7 @@ const CompactIssue = createReactClass({
       className += ' with-graph';
     }
 
-    let {id, orgId} = this.props;
-    let projectId = issue.project.slug;
+    let {id, params} = this.props;
     let title = <span className="icon-more" />;
 
     return (
@@ -206,7 +205,7 @@ const CompactIssue = createReactClass({
         direction="column"
         style={{paddingTop: '12px', paddingBottom: '6px'}}
       >
-        <CompactIssueHeader data={issue} orgId={orgId} projectId={projectId} />
+        <CompactIssueHeader data={issue} params={params} />
         {this.props.statsPeriod && (
           <div className="event-graph">
             <GroupChart
@@ -242,19 +241,11 @@ const CompactIssue = createReactClass({
               </li>
               <li>
                 <SnoozeAction
-                  orgId={orgId}
-                  projectId={projectId}
+                  orgId={params.orgId}
                   groupId={id}
                   onSnooze={this.onSnooze}
                 />
               </li>
-              {false && (
-                <li>
-                  <a href="#">
-                    <span className="icon-user" />
-                  </a>
-                </li>
-              )}
             </DropdownLink>
           </div>
         )}
@@ -264,4 +255,5 @@ const CompactIssue = createReactClass({
   },
 });
 
-export default CompactIssue;
+export {CompactIssue};
+export default withRouter(CompactIssue);

tests/js/spec/views/userFeedback/__snapshots__/projectUserFeedback.spec.jsx.snap
@@ -16,7 +16,7 @@ exports[`projectUserFeedback renders 1`] = `
   }
   status="unresolved"
 >
-  <CompactIssue
+  <withRouter(CompactIssue)
     data={
       Object {
         "assignedTo": null,
@@ -52,8 +52,6 @@ exports[`projectUserFeedback renders 1`] = `
     }
     id="1"
     key="123"
-    orgId="org-slug"
-    projectId="project-slug"
   >
     <EventUserFeedback
       issueId="1"
@@ -106,6 +104,6 @@ exports[`projectUserFeedback renders 1`] = `
         }
       }
     />
-  </CompactIssue>
+  </withRouter(CompactIssue)>
 </UserFeedbackContainer>
 `;

src/sentry/static/sentry/app/components/events/userFeedback.jsx
@@ -11,12 +11,21 @@ class EventUserFeedback extends React.Component {
   static propTypes = {
     report: PropTypes.object.isRequired,
     orgId: PropTypes.string.isRequired,
-    projectId: PropTypes.string.isRequired,
     issueId: PropTypes.string.isRequired,
+    // Provided only in the single project scoped version of this component
+    projectId: PropTypes.string,
   };
 
+  getUrl() {
+    const {report, orgId, projectId, issueId} = this.props;
+
+    return projectId
+      ? `/${orgId}/${projectId}/issues/${issueId}/events/${report.event.id}/`
+      : `/organizations/${orgId}/issues/${issueId}/events/${report.event.id}/`;
+  }
+
   render() {
-    let {report, orgId, projectId, issueId} = this.props;
+    const {report} = this.props;
 
     return (
       <div className="user-report">
@@ -33,12 +42,7 @@ class EventUserFeedback extends React.Component {
                     {/* event_id might be undefined for legacy accounts */}
                     {report.event.id && (
                       <small>
-                        <Link
-                          to={`/${orgId}/${projectId}/issues/${issueId}/events/${report
-                            .event.id}`}
-                        >
-                          {t('View event')}
-                        </Link>
+                        <Link to={this.getUrl()}>{t('View event')}</Link>
                       </small>
                     )}
                   </div>

src/sentry/static/sentry/app/components/issueList.jsx
@@ -95,7 +95,7 @@ const IssueList = createReactClass({
 
   renderResults() {
     let body;
-    const {params, noBorder} = this.props;
+    const {noBorder} = this.props;
 
     if (this.state.loading) body = this.renderLoading();
     else if (this.state.error) body = <LoadingError onRetry={this.fetchData} />;
@@ -111,7 +111,6 @@ const IssueList = createReactClass({
                   key={issue.id}
                   id={issue.id}
                   data={issue}
-                  orgId={params.orgId}
                   statsPeriod={this.props.statsPeriod}
                   showActions={this.props.showActions}
                 />

src/sentry/static/sentry/app/views/userFeedback/organizationUserFeedback.jsx
@@ -44,21 +44,9 @@ class OrganizationUserFeedback extends AsyncView {
 
     const children = this.state.reportList.map(item => {
       const issue = item.issue;
-      const projectId = issue.project.slug;
       return (
-        <CompactIssue
-          key={item.id}
-          id={issue.id}
-          data={issue}
-          orgId={orgId}
-          projectId={projectId}
-        >
-          <EventUserFeedback
-            report={item}
-            orgId={orgId}
-            projectId={projectId}
-            issueId={issue.id}
-          />
+        <CompactIssue key={item.id} id={issue.id} data={issue}>
+          <EventUserFeedback report={item} orgId={orgId} issueId={issue.id} />
         </CompactIssue>
       );
     });

src/sentry/static/sentry/app/views/userFeedback/projectUserFeedback.jsx
@@ -180,13 +180,7 @@ const ProjectUserFeedback = createReactClass({
       const issue = item.issue;
 
       return (
-        <CompactIssue
-          key={item.id}
-          id={issue.id}
-          data={issue}
-          orgId={orgId}
-          projectId={projectId}
-        >
+        <CompactIssue key={item.id} id={issue.id} data={issue}>
           <EventUserFeedback
             report={item}
             orgId={orgId}

tests/js/spec/views/userFeedback/organizationUserFeedback.spec.jsx
@@ -4,6 +4,7 @@ import {mount} from 'enzyme';
 import {OrganizationUserFeedback} from 'app/views/userFeedback/organizationUserFeedback';
 
 describe('OrganizationUserFeedback', function() {
+  let organization, routerContext;
   beforeEach(function() {
     const pageLinks =
       '<https://sentry.io/api/0/organizations/sentry/user-feedback/?statsPeriod=14d&cursor=0:0:1>; rel="previous"; results="false"; cursor="0:0:1", ' +
@@ -19,6 +20,19 @@ describe('OrganizationUserFeedback', function() {
       url: '/organizations/org-slug/environments/',
       body: TestStubs.Environments(),
     });
+
+    organization = TestStubs.Organization();
+    routerContext = TestStubs.routerContext([
+      {
+        organization,
+        router: {
+          ...TestStubs.router(),
+          params: {
+            orgId: organization.slug,
+          },
+        },
+      },
+    ]);
   });
 
   it('renders', function() {
@@ -26,13 +40,10 @@ describe('OrganizationUserFeedback', function() {
       organization: TestStubs.Organization({features: ['sentry10']}),
       location: {query: {}, search: ''},
       params: {
-        orgId: 'org-slug',
+        orgId: organization.slug,
       },
     };
-    const wrapper = mount(
-      <OrganizationUserFeedback {...params} />,
-      TestStubs.routerContext()
-    );
+    const wrapper = mount(<OrganizationUserFeedback {...params} />, routerContext);
 
     expect(wrapper).toMatchSnapshot();
   });

commit a429a6e2d8613983ce8b7517baa9f8c322ee03af

fix(ui): Fix rendering incorrect links
Fixes a bug introduced in https://github.com/getsentry/sentry/pull/11649
which caused incorrect links to issues to be rendered in the assigned to me,
bookmarked and recently viewed sections.

src/sentry/static/sentry/app/components/compactIssue.jsx
@@ -1,6 +1,5 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import {withRouter} from 'react-router';
 import createReactClass from 'create-react-class';
 import Reflux from 'reflux';
 import {Flex, Box} from 'grid-emotion';
@@ -15,9 +14,13 @@ import Link from 'app/components/link';
 import ProjectLink from 'app/components/projectLink';
 import {t} from 'app/locale';
 import {PanelItem} from 'app/components/panels';
+import SentryTypes from 'app/sentryTypes';
+import withOrganization from 'app/utils/withOrganization';
 
 class CompactIssueHeader extends React.Component {
   static propTypes = {
+    organization: SentryTypes.Organization.isRequired,
+    projectId: PropTypes.string,
     data: PropTypes.object.isRequired,
   };
 
@@ -62,13 +65,15 @@ class CompactIssueHeader extends React.Component {
   };
 
   render() {
-    let {data, params: {orgId, projectId}} = this.props;
+    let {data, organization, projectId} = this.props;
+
+    let hasNewRoutes = new Set(organization.features).has('sentry10');
 
     let styles = {};
 
-    let basePath = projectId
-      ? `/${orgId}/${projectId}/issues/`
-      : `/organizations/${orgId}/issues/`;
+    let basePath = hasNewRoutes
+      ? `/${organization.slug}/${projectId}/issues/`
+      : `/organizations/${organization.slug}/issues/`;
 
     if (data.subscriptionDetails && data.subscriptionDetails.reason === 'mentioned') {
       styles = {color: '#57be8c'};
@@ -114,6 +119,7 @@ const CompactIssue = createReactClass({
     id: PropTypes.string,
     statsPeriod: PropTypes.string,
     showActions: PropTypes.bool,
+    organization: SentryTypes.Organization.isRequired,
   },
 
   mixins: [ApiMixin, Reflux.listenTo(GroupStore, 'onGroupChange')],
@@ -159,7 +165,7 @@ const CompactIssue = createReactClass({
 
     this.api.bulkUpdate(
       {
-        orgId: this.props.params.orgId,
+        orgId: this.props.organization.slug,
         projectId: issue.project.slug,
         itemIds: [issue.id],
         data,
@@ -174,6 +180,7 @@ const CompactIssue = createReactClass({
 
   render() {
     let issue = this.state.issue;
+    let {organization} = this.props;
 
     let className = 'issue';
     if (issue.isBookmarked) {
@@ -195,7 +202,7 @@ const CompactIssue = createReactClass({
       className += ' with-graph';
     }
 
-    let {id, params} = this.props;
+    let {id} = this.props;
     let title = <span className="icon-more" />;
 
     return (
@@ -205,7 +212,11 @@ const CompactIssue = createReactClass({
         direction="column"
         style={{paddingTop: '12px', paddingBottom: '6px'}}
       >
-        <CompactIssueHeader data={issue} params={params} />
+        <CompactIssueHeader
+          data={issue}
+          organization={organization}
+          projectId={issue.project.slug}
+        />
         {this.props.statsPeriod && (
           <div className="event-graph">
             <GroupChart
@@ -241,7 +252,7 @@ const CompactIssue = createReactClass({
               </li>
               <li>
                 <SnoozeAction
-                  orgId={params.orgId}
+                  orgId={organization.slug}
                   groupId={id}
                   onSnooze={this.onSnooze}
                 />
@@ -256,4 +267,4 @@ const CompactIssue = createReactClass({
 });
 
 export {CompactIssue};
-export default withRouter(CompactIssue);
+export default withOrganization(CompactIssue);

tests/js/spec/views/userFeedback/__snapshots__/organizationUserFeedback.spec.jsx.snap
@@ -1726,7 +1726,7 @@ exports[`OrganizationUserFeedback renders 1`] = `
                           <div
                             className="css-9vq8an-textStyles issue-list"
                           >
-                            <withRouter(CompactIssue)
+                            <WithOrganizationMockWrappeer
                               data={
                                 Object {
                                   "assignedTo": null,
@@ -1798,108 +1798,31 @@ exports[`OrganizationUserFeedback renders 1`] = `
                                   }
                                 }
                                 id="1"
-                                location={
+                                organization={
                                   Object {
-                                    "query": Object {},
-                                  }
-                                }
-                                params={
-                                  Object {
-                                    "orgId": "org-slug",
-                                  }
-                                }
-                                router={
-                                  Object {
-                                    "createHref": [MockFunction] {
-                                      "calls": Array [
-                                        Array [
-                                          Object {
-                                            "pathname": undefined,
-                                            "query": Object {},
-                                          },
-                                        ],
-                                        Array [
-                                          Object {
-                                            "pathname": undefined,
-                                            "query": Object {
-                                              "status": "",
-                                            },
-                                          },
-                                        ],
-                                        Array [
-                                          "/organizations/org-slug/issues/1/",
-                                        ],
-                                        Array [
-                                          "/organizations/org-slug/issues/",
-                                        ],
-                                        Array [
-                                          "/organizations/org-slug/issues/1/activity/",
-                                        ],
-                                        Array [
-                                          "/organizations/org-slug/issues/1/events/1/",
-                                        ],
-                                      ],
-                                      "results": Array [
-                                        Object {
-                                          "isThrow": false,
-                                          "value": undefined,
-                                        },
-                                        Object {
-                                          "isThrow": false,
-                                          "value": undefined,
-                                        },
-                                        Object {
-                                          "isThrow": false,
-                                          "value": undefined,
-                                        },
-                                        Object {
-                                          "isThrow": false,
-                                          "value": undefined,
-                                        },
-                                        Object {
-                                          "isThrow": false,
-                                          "value": undefined,
-                                        },
-                                        Object {
-                                          "isThrow": false,
-                                          "value": undefined,
-                                        },
-                                      ],
+                                    "access": Array [
+                                      "org:read",
+                                      "org:write",
+                                      "org:admin",
+                                      "project:read",
+                                      "project:write",
+                                      "project:admin",
+                                      "team:read",
+                                      "team:write",
+                                      "team:admin",
+                                    ],
+                                    "features": Array [],
+                                    "id": "3",
+                                    "name": "Organization Name",
+                                    "onboardingTasks": Array [],
+                                    "projects": Array [],
+                                    "scrapeJavaScript": true,
+                                    "slug": "org-slug",
+                                    "status": Object {
+                                      "id": "active",
+                                      "name": "active",
                                     },
-                                    "go": [MockFunction],
-                                    "goBack": [MockFunction],
-                                    "goForward": [MockFunction],
-                                    "isActive": [MockFunction],
-                                    "listen": [MockFunction],
-                                    "location": Object {
-                                      "query": Object {},
-                                    },
-                                    "params": Object {
-                                      "orgId": "org-slug",
-                                    },
-                                    "push": [MockFunction] {
-                                      "calls": Array [
-                                        Array [
-                                          Object {
-                                            "pathname": undefined,
-                                            "query": Object {
-                                              "environment": Array [],
-                                              "project": Array [],
-                                              "statsPeriod": "14d",
-                                              "utc": "true",
-                                            },
-                                          },
-                                        ],
-                                      ],
-                                      "results": Array [
-                                        Object {
-                                          "isThrow": false,
-                                          "value": undefined,
-                                        },
-                                      ],
-                                    },
-                                    "replace": [MockFunction],
-                                    "setRouteLeaveHook": [MockFunction],
+                                    "teams": Array [],
                                   }
                                 }
                               >
@@ -1969,11 +1892,34 @@ exports[`OrganizationUserFeedback renders 1`] = `
                                             "tags": Array [],
                                           }
                                         }
-                                        params={
+                                        organization={
                                           Object {
-                                            "orgId": "org-slug",
+                                            "access": Array [
+                                              "org:read",
+                                              "org:write",
+                                              "org:admin",
+                                              "project:read",
+                                              "project:write",
+                                              "project:admin",
+                                              "team:read",
+                                              "team:write",
+                                              "team:admin",
+                                            ],
+                                            "features": Array [],
+                                            "id": "3",
+                                            "name": "Organization Name",
+                                            "onboardingTasks": Array [],
+                                            "projects": Array [],
+                                            "scrapeJavaScript": true,
+                                            "slug": "org-slug",
+                                            "status": Object {
+                                              "id": "active",
+                                              "name": "active",
+                                            },
+                                            "teams": Array [],
                                           }
                                         }
+                                        projectId="project-slug"
                                       >
                                         <Flex
                                           align="center"
@@ -2393,7 +2339,7 @@ exports[`OrganizationUserFeedback renders 1`] = `
                                   </Base>
                                 </PanelItem>
                               </CompactIssue>
-                            </withRouter(CompactIssue)>
+                            </WithOrganizationMockWrappeer>
                           </div>
                         </PanelBody>
                       </div>

tests/js/spec/views/userFeedback/__snapshots__/projectUserFeedback.spec.jsx.snap
@@ -16,7 +16,7 @@ exports[`projectUserFeedback renders 1`] = `
   }
   status="unresolved"
 >
-  <withRouter(CompactIssue)
+  <WithOrganizationMockWrappeer
     data={
       Object {
         "assignedTo": null,
@@ -104,6 +104,6 @@ exports[`projectUserFeedback renders 1`] = `
         }
       }
     />
-  </withRouter(CompactIssue)>
+  </WithOrganizationMockWrappeer>
 </UserFeedbackContainer>
 `;