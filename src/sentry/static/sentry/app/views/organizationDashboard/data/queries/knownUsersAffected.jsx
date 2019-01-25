c64bbde7d643ffbcb3002daeb38f2a071742113f
Billy Vong <billy@sentry.io>
2019-01-25 11:53:03 -0800

    use constant

--- src/sentry/static/sentry/app/views/organizationDashboard/data/queries/knownUsersAffected.jsx
+++ src/sentry/static/sentry/app/views/organizationDashboard/data/queries/knownUsersAffected.jsx
@@ -1,10 +1,13 @@
+import {OPERATOR} from 'app/views/organizationDiscover/data';
+
 /**
  * Known affected users
  */
+
 const knownUsersAffectedQuery = {
   name: 'Known Users',
   fields: [],
-  conditions: [['user.email', 'IS NOT NULL', null]],
+  conditions: [['user.email', OPERATOR.IS_NOT_NULL, null]],
   aggregations: [['uniq', 'user.email', 'Known Users']],
   limit: 2000,
 
