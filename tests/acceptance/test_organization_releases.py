6e338ae43a1bc375e58ceda75d669555533cbe02
Lyn Nagara <lyn.nagara@gmail.com>
2019-01-25 13:45:40 -0800

    run test with feature

--- tests/acceptance/test_organization_releases.py
+++ tests/acceptance/test_organization_releases.py
@@ -23,21 +23,23 @@ class OrganizationReleasesTest(AcceptanceTestCase):
             self.org.slug)
 
     def test_with_releases(self):
-        release = self.create_release(
-            project=self.project,
-            version='1.0',
-        )
-        self.create_group(
-            first_release=release,
-            project=self.project,
-            message='Foo bar',
-        )
-        self.project.update(first_event=timezone.now())
-        self.browser.get(self.path)
-        self.browser.wait_until_not('.loading')
-        self.browser.snapshot('organization releases with releases')
+        with self.feature('organizations:sentry10'):
+            release = self.create_release(
+                project=self.project,
+                version='1.0',
+            )
+            self.create_group(
+                first_release=release,
+                project=self.project,
+                message='Foo bar',
+            )
+            self.project.update(first_event=timezone.now())
+            self.browser.get(self.path)
+            self.browser.wait_until_not('.loading')
+            self.browser.snapshot('organization releases with releases')
 
     def test_with_no_releases(self):
-        self.browser.get(self.path)
-        self.browser.wait_until_not('.loading')
-        self.browser.snapshot('organization releases without releases')
+        with self.feature('organizations:sentry10'):
+            self.browser.get(self.path)
+            self.browser.wait_until_not('.loading')
+            self.browser.snapshot('organization releases without releases')
