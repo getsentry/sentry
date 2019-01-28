64661d6d1350dc73f7c78b6be608e295bc5f1d57
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-01-28 15:46:12 +0100

    fix: Another None in eventtypes (#11743)

--- src/sentry/eventtypes/error.py
+++ src/sentry/eventtypes/error.py
@@ -10,7 +10,7 @@ from .base import BaseEvent
 
 def get_crash_file(stacktrace):
     default = None
-    for frame in reversed(stacktrace.get('frames') or ()):
+    for frame in reversed(get_path(stacktrace, 'frames', filter=True) or ()):
         fn = frame.get('filename') or frame.get('abs_path')
         if fn:
             if frame.get('in_app'):
