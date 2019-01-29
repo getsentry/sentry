2c9a452754ed984f87f170e920745df9969eeda2
Meredith <meredith@getsentry.com>
2019-01-29 13:38:03 -0800

    ref(api): Add project environment endpoints to docs

--- src/sentry/api/endpoints/project_environments.py
+++ src/sentry/api/endpoints/project_environments.py
@@ -2,13 +2,27 @@ from __future__ import absolute_import
 
 from rest_framework.response import Response
 
+from sentry.api.base import DocSection
 from sentry.api.bases.project import ProjectEndpoint
 from sentry.api.helpers.environments import environment_visibility_filter_options
 from sentry.api.serializers import serialize
 from sentry.models import EnvironmentProject
+from sentry.utils.apidocs import scenario, attach_scenarios
+
+
+@scenario('RetrieveEnvironmentsForProject')
+def retrieve_environments_for_project_scenario(runner):
+    runner.request(
+        method='GET',
+        path='/projects/%s/%s/environments/' %
+        (runner.org.slug, runner.default_project.slug)
+    )
 
 
 class ProjectEnvironmentsEndpoint(ProjectEndpoint):
+    doc_section = DocSection.PROJECTS
+
+    @attach_scenarios([retrieve_environments_for_project_scenario])
     def get(self, request, project):
         queryset = EnvironmentProject.objects.filter(
             project=project,
