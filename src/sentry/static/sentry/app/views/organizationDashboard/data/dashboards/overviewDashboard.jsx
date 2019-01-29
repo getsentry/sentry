b166c2d87a0d88ff22c6e2c0634661c8cb915c10
Billy Vong <billyvg@users.noreply.github.com>
2019-01-29 11:05:02 -0800

    feat(dashboard): Reorder widgets and combine devices/browsers (#11752)
    * Reorder dashboard widgets
    * Combine top devices with top browsers
    * Remove a release chart
    * Change `getChartDataForWidget` to use `, ` as field separators instead of just a space
    
    
    ![image](https://user-images.githubusercontent.com/79684/51873422-f1d20580-2311-11e9-97c6-6e17ad5866bc.png)

--- src/sentry/static/sentry/app/views/organizationDashboard/data/dashboards/overviewDashboard.jsx
+++ src/sentry/static/sentry/app/views/organizationDashboard/data/dashboards/overviewDashboard.jsx
@@ -1,22 +1,18 @@
 import affectedUsers from '../widgets/affectedUsers';
 import errorsByGeo from '../widgets/errorsByGeo';
 import eventsByReleasePercent from '../widgets/eventsByReleasePercent';
-import eventsByReleaseArea from '../widgets/eventsByReleaseArea';
 import events from '../widgets/events';
 import handledVsUnhandled from '../widgets/handledVsUnhandled';
-import topBrowsers from '../widgets/topBrowsers';
-import topDevices from '../widgets/topDevices';
+import topDevicesAndBrowsers from '../widgets/topDevicesAndBrowsers';
 
 const overviewDashboard = {
   widgets: [
     events,
-    handledVsUnhandled,
-    affectedUsers,
-    topBrowsers,
-    topDevices,
     eventsByReleasePercent,
-    eventsByReleaseArea,
+    affectedUsers,
     errorsByGeo,
+    handledVsUnhandled,
+    topDevicesAndBrowsers,
   ],
 };
 
