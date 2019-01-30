692446bc7eb424c8036aa2fa37561c0c90d85d59
James Cunningham <jtcunning@getsentry.com>
2019-01-30 15:29:09 -0800

    fix(deletions): Add EventAttachment to bulk child deletion.

--- src/sentry/deletions/__init__.py
+++ src/sentry/deletions/__init__.py
@@ -45,6 +45,7 @@ def load_defaults():
     default_manager.register(models.DiscoverSavedQueryProject, BulkModelDeletionTask)
     default_manager.register(models.EnvironmentProject, BulkModelDeletionTask)
     default_manager.register(models.Event, defaults.EventDeletionTask)
+    default_manager.register(models.EventAttachment, BulkModelDeletionTask)
     default_manager.register(models.EventMapping, BulkModelDeletionTask)
     default_manager.register(models.EventUser, BulkModelDeletionTask)
     default_manager.register(models.Group, defaults.GroupDeletionTask)
