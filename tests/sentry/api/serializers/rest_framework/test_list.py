f9e62ec983a6b9d50c847b90d620c03edc64c8f6
Lauryn Brown <lauryndbrown@gmail.com>
2019-01-31 12:27:12 -0800

    writing more tests for this class.

--- tests/sentry/api/serializers/rest_framework/test_list.py
+++ tests/sentry/api/serializers/rest_framework/test_list.py
@@ -1,9 +1,9 @@
 from __future__ import absolute_import
-from sentry.api.serializers.rest_framework import ListField
+from sentry.api.serializers.rest_framework import ListField, ValidationError
 
 from rest_framework import serializers
 
-from sentry.testutils import TestCase
+from sentry.testutils import TestCase, APITestCase
 
 
 class ListFieldTest(TestCase):
@@ -98,3 +98,64 @@ class ListFieldTest(TestCase):
             list_field = ListField(child=serializers.IntegerField(), required=False)
         serializer = DummySerializer(data={'list_field': [1, 2, 3]})
         self.assert_success(serializer, {'list_field': [1, 2, 3]})
+
+
+class ListFieldAPITest(APITestCase):
+    def setUp(self):
+        super(ListFieldAPITest, self).setUp()
+
+        class DummyChildSerializer(serializers.Serializer):
+            age = serializers.IntegerField()
+
+            def validate_age(self, attrs, source):
+                age = attrs[source]
+                if age > 5:
+                    raise ValidationError('age %d is not allowed', age)
+                return attrs
+
+        class DummySerializer(serializers.Serializer):
+            list_field = ListField(
+                required=False,
+                child=DummyChildSerializer(required=False),
+                allow_null=True,
+            )
+            name = serializers.CharField()
+
+        self.serializer_class = DummySerializer
+
+    def test_simple(self):
+        serializer = self.serializer_class(data={
+            'name': 'John',
+            'list_field': [{'age': 200}]
+        })
+        assert not serializer.is_valid()
+
+    def test_incorrect_value_before_correct_value(self):
+        serializer = self.serializer_class(data={
+            'name': 'John',
+            'list_field': [
+                {'age': 200}, {'age': 3}
+            ]
+        })
+        assert not serializer.is_valid()
+
+    def test_correct_value_before_incorrect_value(self):
+        serializer = self.serializer_class(data={
+            'name': 'John',
+            'list_field': [
+                {'age': 3}, {'age': 200}
+            ]
+        })
+        assert not serializer.is_valid()
+
+    def test_clears_child_errors_between_use(self):
+        serializer = self.serializer_class(data={
+            'name': 'John',
+            'list_field': [{'age': 200}]
+        })
+        assert not serializer.is_valid()
+        serializer = self.serializer_class(data={
+            'name': 'John',
+            'list_field': []
+        })
+        assert serializer.is_valid()
