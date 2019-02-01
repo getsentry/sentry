7d6635c5131dc7e0d38227d9a5efafd1fc37c8f5
alex-hofsteede <alex@hofsteede.com>
2019-01-31 16:53:55 -0800

    remove import

--- src/sentry/models/event.py
+++ src/sentry/models/event.py
@@ -13,7 +13,6 @@ import warnings
 
 from collections import OrderedDict
 from django.db import models
-from django.db.models import Q
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 from hashlib import md5
