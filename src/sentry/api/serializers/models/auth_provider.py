30db6c37d345fa4614549c898e1cebd8fd680d9c
Lyn Nagara <lyn.nagara@gmail.com>
2019-02-01 10:25:32 -0800

    feat:Update auth provider login url

--- src/sentry/api/serializers/models/auth_provider.py
+++ src/sentry/api/serializers/models/auth_provider.py
@@ -5,6 +5,7 @@ import six
 from django.core.urlresolvers import reverse
 from django.db.models import F
 
+from sentry import features
 from sentry.api.serializers import Serializer, register
 from sentry.models import AuthProvider, OrganizationMember
 from sentry.utils.http import absolute_uri
@@ -19,11 +20,16 @@ class AuthProviderSerializer(Serializer):
             flags=F('flags').bitand(~OrganizationMember.flags['sso:linked']),
         ).count()
 
+        if features.has('sentry10'):
+            login_url = reverse('sentry-organization-issue-list', args=[organization.slug])
+        else:
+            login_url = reverse('sentry-organization-home', args=[organization.slug])
+
         return {
             'id': six.text_type(obj.id),
             'provider_name': obj.provider,
             'pending_links_count': pending_links_count,
-            'login_url': absolute_uri(reverse('sentry-organization-home', args=[organization.slug])),
+            'login_url': absolute_uri(login_url),
             'default_role': organization.default_role,
             'require_link': not obj.flags.allow_unlinked,
         }
