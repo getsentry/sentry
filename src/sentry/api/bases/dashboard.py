7baa24947c6ed1cc51ba924ac01407500c23dd4c
Lauryn Brown <lauryndbrown@gmail.com>
2019-02-04 12:28:40 -0800

    feat(dashboards): Add Dashboard Widget Endpoint. (#11652)
    * Added widget endpoint.
    
    * Added put method.
    
    * organization dashboard widgets implemented with tests not passing.
    
    * got tests working for widgets post method.
    
    * Added docstrings and added a widget tests case.
    
    * Updated put tests to pass.
    
    * Fixed typo.
    
    * Tested delete widget.
    
    * Added 404 deletion test
    
    * Moved widget serializers into its own file. Using new get_respond method in tests.
    
    * Added get_response to dashboard widget test fixed typos in docstring.
    
    * Update src/sentry/api/endpoints/organization_dashboard_widget_details.py
    
    Co-Authored-By: lauryndbrown <lauryndbrown@gmail.com>
    
    * Update src/sentry/api/serializers/rest_framework/widget.py
    
    Co-Authored-By: lauryndbrown <lauryndbrown@gmail.com>
    
    * Changed widget details test to dan's format. Added empty array to serializer tests
    
    * Added several changes as suggested from reviews.
    
    * removed typo in tests

--- src/sentry/api/bases/dashboard.py
+++ src/sentry/api/bases/dashboard.py
@@ -1,10 +1,20 @@
 from __future__ import absolute_import
 
+from django.db.models import Max
+
 from sentry.api.bases.organization import (
     OrganizationEndpoint
 )
 from sentry.api.exceptions import ResourceDoesNotExist
-from sentry.models import Dashboard
+from sentry.models import Dashboard, Widget
+
+
+def get_next_dashboard_order(dashboard_id):
+    max_order = Widget.objects.filter(
+        dashboard_id=dashboard_id,
+    ).aggregate(Max('order'))['order__max']
+
+    return max_order + 1 if max_order else 1
 
 
 class OrganizationDashboardEndpoint(OrganizationEndpoint):
@@ -23,4 +33,24 @@ class OrganizationDashboardEndpoint(OrganizationEndpoint):
         return Dashboard.objects.get(
             id=dashboard_id,
             organization_id=organization.id
+        )
+
+
+class OrganizationDashboardWidgetEndpoint(OrganizationDashboardEndpoint):
+    def convert_args(self, request, organization_slug, dashboard_id, widget_id, *args, **kwargs):
+        args, kwargs = super(OrganizationDashboardWidgetEndpoint,
+                             self).convert_args(request, organization_slug, dashboard_id, widget_id, *args, **kwargs)
+
+        try:
+            kwargs['widget'] = self._get_widget(
+                request, kwargs['organization'], dashboard_id, widget_id)
+        except Widget.DoesNotExist:
+            raise ResourceDoesNotExist
+
+        return (args, kwargs)
+
+    def _get_widget(self, request, organization, dashboard_id, widget_id):
+        return Widget.objects.get(
+            id=widget_id,
+            dashboard_id=dashboard_id,
         )
