bc1195393c84b51a9d5d8d594619228decb5ee31
Mark Story <mark@sentry.io>
2019-02-04 15:55:32 -0500

    Update links in emails as well.

--- src/sentry/plugins/sentry_mail/activity/new_processing_issues.py
+++ src/sentry/plugins/sentry_mail/activity/new_processing_issues.py
@@ -46,7 +46,7 @@ class NewProcessingIssuesActivityEmail(ActivityEmail):
             self.activity.data['reprocessing_active'],
             'info_url':
             absolute_uri(
-                u'/{}/{}/settings/processing-issues/'.format(
+                u'/settings/{}/projects/{}/processing-issues/'.format(
                     self.organization.slug,
                     self.project.slug,
                 )
