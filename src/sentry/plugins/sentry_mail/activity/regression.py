614f8def3623ad2b438c5fe2d95d5916df3a7a79
Lyn Nagara <lyn.nagara@gmail.com>
2019-02-04 14:21:50 -0800

    feat(email): Update release link for Sentry 10 (#11880)

--- src/sentry/plugins/sentry_mail/activity/regression.py
+++ src/sentry/plugins/sentry_mail/activity/regression.py
@@ -1,5 +1,6 @@
 from __future__ import absolute_import
 
+from sentry import features
 from sentry.utils.html import escape
 from sentry.utils.http import absolute_uri
 
@@ -12,19 +13,26 @@ class RegressionActivityEmail(ActivityEmail):
 
     def get_description(self):
         data = self.activity.data
+
         if data.get('version'):
+            if features.has('organizations:sentry10', self.organization):
+                version_url = u'/organizations/{}/releases/{}/'.format(
+                    self.organization.slug,
+                    data['version']
+                )
+            else:
+                version_url = u'/{}/{}/releases/{}/'.format(
+                    self.organization.slug,
+                    self.project.slug,
+                    data['version'],
+                )
+
             return u'{author} marked {an issue} as a regression in {version}', {
                 'version': data['version']
             }, {
                 'version':
                 u'<a href="{}">{}</a>'.format(
-                    absolute_uri(
-                        u'/{}/{}/releases/{}/'.format(
-                            self.organization.slug,
-                            self.project.slug,
-                            data['version'],
-                        )
-                    ),
+                    absolute_uri(version_url),
                     escape(data['version']),
                 )
             }
