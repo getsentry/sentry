934ec5d26785370f1de38e2c2e3f528c83559c42
Alex Hofsteede <alex@hofsteede.com>
2019-02-05 14:29:51 -0800

    ref: Fetch events by id or event_id in a common Manager (#11891)
    ref: Fetch events by id or event_id in a Manager method
    
    In preparation for using Snuba as the source of truth for events,
    force Event.objects.get() type calls through a helper method that
    adds the ability to use hex ids in all API endpoints, while also
    allowing us to track the usage of the different id types.

--- src/sentry/api/endpoints/event_details.py
+++ src/sentry/api/endpoints/event_details.py
@@ -23,9 +23,8 @@ class EventDetailsEndpoint(Endpoint):
         is the event as it appears in the Sentry database and not the event
         ID that is reported by the client upon submission.
         """
-        try:
-            event = Event.objects.get(id=event_id)
-        except Event.DoesNotExist:
+        event = Event.objects.from_event_id(event_id, project_id=None)
+        if event is None:
             raise ResourceDoesNotExist
 
         self.check_object_permissions(request, event.group)
