af0d528fbb4d1deb793535060f74d1974fcac5f7
Ted Kaemming <ted@kaemming.com>
2019-02-05 15:46:26 -0800

    remove `event_accepted` signal

--- src/sentry/signals.py
+++ src/sentry/signals.py
@@ -54,7 +54,6 @@ class BetterSignal(Signal):
 
 
 buffer_incr_complete = BetterSignal(providing_args=["model", "columns", "extra", "result"])
-event_accepted = BetterSignal(providing_args=["ip", "data", "project"])
 event_discarded = BetterSignal(providing_args=["project"])
 event_dropped = BetterSignal(providing_args=["ip", "data", "project", "reason_code"])
 event_filtered = BetterSignal(providing_args=["ip", "data", "project"])
