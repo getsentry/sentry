1fbc148412fbbb5e7740e69288e6ab4153fda088
Ted Kaemming <ted@kaemming.com>
2019-02-05 15:19:39 -0800

    ref(store): Remove Kafka event publisher from `StoreView.process`

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -156,8 +156,6 @@ register('kafka-publisher.max-event-size', default=100000)
 register('eventstream.kafka.send-post_process-task', type=Bool, default=True)
 
 # Ingest refactor
-register('store.process-in-kafka', type=Bool, default=False)
-register('store.kafka-sample-rate', default=0.0)
 register('store.projects-normalize-in-rust-opt-in', type=Sequence, default=[])
 register('store.projects-normalize-in-rust-opt-out', type=Sequence, default=[])
 # positive value means stable opt-in in the range 0.0 to 1.0, negative value
