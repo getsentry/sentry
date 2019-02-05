3532db3ad341d273500750f997808e9dcfa23dfb
Lyn Nagara <lyn.nagara@gmail.com>
2019-02-04 17:33:04 -0800

    fix(releases): Fix releases links
    We need to check the Sentry 10 flag to determine which variant to
    display. We should only display the static (non-link) variant if both
    orgId and projectId are not passed. This caused an invalid link to be
    generated in some cases.

--- src/sentry/static/sentry/app/components/version.jsx
+++ src/sentry/static/sentry/app/components/version.jsx
@@ -1,12 +1,15 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 
+import SentryTypes from 'app/sentryTypes';
 import ProjectLink from 'app/components/projectLink';
 import Link from 'app/components/link';
 import {getShortVersion} from 'app/utils';
+import withOrganization from 'app/utils/withOrganization';
 
 class Version extends React.Component {
   static propTypes = {
+    organization: SentryTypes.Organization.isRequired,
     anchor: PropTypes.bool,
     version: PropTypes.string.isRequired,
     orgId: PropTypes.string,
@@ -20,11 +23,20 @@ class Version extends React.Component {
   };
 
   render() {
-    const {orgId, projectId, showShortVersion, version, anchor} = this.props;
+    const {
+      organization,
+      orgId,
+      projectId,
+      showShortVersion,
+      version,
+      anchor,
+    } = this.props;
     const versionTitle = showShortVersion ? getShortVersion(version) : version;
 
+    const hasSentry10 = new Set(organization.features).has('sentry10');
+
     if (anchor) {
-      if (projectId) {
+      if (projectId && !hasSentry10) {
         return (
           // NOTE: version is encoded because it can contain slashes "/",
           //       which can interfere with URL construction
@@ -35,14 +47,17 @@ class Version extends React.Component {
           </ProjectLink>
         );
       }
-      return (
-        <Link to={`/organizations/${orgId}/releases/${encodeURIComponent(version)}`}>
-          <span title={version}>{versionTitle}</span>
-        </Link>
-      );
+
+      if (orgId) {
+        return (
+          <Link to={`/organizations/${orgId}/releases/${encodeURIComponent(version)}`}>
+            <span title={version}>{versionTitle}</span>
+          </Link>
+        );
+      }
     }
     return <span title={version}>{versionTitle}</span>;
   }
 }
 
-export default Version;
+export default withOrganization(Version);
