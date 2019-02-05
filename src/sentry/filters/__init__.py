7299ddc23daa4da3e45e114393df4c381c574910
Ted Kaemming <ted@kaemming.com>
2019-02-05 11:02:08 -0800

    ref(filters): Remove registration API for inbound filters

--- src/sentry/filters/__init__.py
+++ src/sentry/filters/__init__.py
@@ -13,14 +13,13 @@ from .browser_extensions import BrowserExtensionsFilter
 from .legacy_browsers import LegacyBrowsersFilter
 from .web_crawlers import WebCrawlersFilter
 
-default_manager = FilterManager()
-default_manager.register(LocalhostFilter)
-default_manager.register(BrowserExtensionsFilter)
-default_manager.register(LegacyBrowsersFilter)
-default_manager.register(WebCrawlersFilter)
+default_manager = FilterManager([
+    LocalhostFilter,
+    BrowserExtensionsFilter,
+    LegacyBrowsersFilter,
+    WebCrawlersFilter,
+])
 
 all = default_manager.all
 exists = default_manager.exists
 get = default_manager.get
-register = default_manager.register
-unregister = default_manager.unregister
