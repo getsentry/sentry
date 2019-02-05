2cf329901d50ab0c9c499f8804af1c2b5918dc5a
Lauryn Brown <lauryndbrown@gmail.com>
2019-02-05 08:52:57 -0800

    changed class to class name. added logging statements

--- src/sentry/db/mixin.py
+++ src/sentry/db/mixin.py
@@ -2,23 +2,22 @@ from __future__ import absolute_import
 
 import six
 
-
 from uuid import uuid4
 
+from sentry.api.base import logger
 from sentry.models import OrganizationOption
 
 
 def delete_pending_deletion_option(instance, **kwargs):
-    if not issubclass(instance.__class__, PendingDeletionMixin):
-        return
-    instance.delete_pending_deletion_option()
+    if hasattr(instance, 'delete_pending_deletion_option'):
+        instance.delete_pending_deletion_option()
 
 
 class PendingDeletionMixin(object):
     _rename_fields_on_pending_delete = []
 
     def build_pending_deletion_key(self):
-        return 'pending-delete:%s:%s' % (self.__class__, self.id)
+        return 'pending-delete:%s:%s' % (self.__class__.__name__, self.id)
 
     def rename_on_pending_deletion(self, fields=None):
         fields = fields or self._rename_fields_on_pending_delete
@@ -30,11 +29,19 @@ class PendingDeletionMixin(object):
 
         self.save()
         original_data['id'] = self.id
-        original_data['model'] = self.__class__
+        original_data['model'] = self.__class__.__name__
         OrganizationOption.objects.create(
             organization_id=self.organization_id,
             key=self.build_pending_deletion_key(),
             value=original_data,
+        )
+        logger.info(
+            'rename-on-pending-deletion',
+            extra={
+                'organization_id': self.organization_id,
+                'model': original_data['model'],
+                'id': original_data['id'],
+            }
         )
 
     def get_pending_deletion_option(self):
@@ -44,13 +51,53 @@ class PendingDeletionMixin(object):
         )
 
     def reset_pending_deletion_field_names(self):
-        original_data = self.get_pending_deletion_option().value
+        try:
+            original_data = self.get_pending_deletion_option().value
+        except self.__class__.DoesNotExist:
+            logger.info(
+                'reset-on-pending-deletion:does-not-exist',
+                extra={
+                    'organization_id': self.organization_id,
+                    'model': self.__class__.__name__,
+                    'id': self.id,
+                }
+            )
+            return False
+
         for field_name, field_value in six.iteritems(original_data):
             if field_name in ('id', 'model'):
                 continue
             setattr(self, field_name, field_value)
         self.save()
+        logger.info(
+            'reset-on-pending-deletion:success',
+            extra={
+                'organization_id': self.organization_id,
+                'model': self.__class__.__name__,
+                'id': self.id,
+            }
+        )
+        return True
 
     def delete_pending_deletion_option(self):
-        option = self.get_pending_deletion_option()
+        try:
+            option = self.get_pending_deletion_option()
+        except self.__class__.DoesNotExist:
+            logger.info(
+                'delete-pending-deletion-option:does-not-exist',
+                extra={
+                    'organization_id': self.organization_id,
+                    'model': self.__class__.__name__,
+                    'id': self.id,
+                }
+            )
+            return
         option.delete()
+        logger.info(
+            'delete-pending-deletion-option:success',
+            extra={
+                'organization_id': self.organization_id,
+                'model': self.__class__.__name__,
+                'id': self.id,
+            }
+        )
