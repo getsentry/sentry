a129ba494106a48b20a2cd73273d0419fe6f0b71
Lyn Nagara <lyn.nagara@gmail.com>
2019-02-05 12:16:30 -0800

    feat(issues): Filter issue details page by active environments
    Issue details page supports filtering by multiple environments. This
    mostly affects the stats values displayed in the sidebar.

--- src/sentry/static/sentry/app/views/groupDetails/organization/index.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/organization/index.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import SentryTypes from 'app/sentryTypes';
 
 import withOrganization from 'app/utils/withOrganization';
+import withGlobalSelection from 'app/utils/withGlobalSelection';
 import GlobalSelectionHeader from 'app/components/organizations/globalSelectionHeader';
 import {PageContent} from 'app/styles/organization';
 import Feature from 'app/components/acl/feature';
@@ -10,19 +11,23 @@ import GroupDetails from '../shared/groupDetails';
 
 class OrganizationGroupDetails extends React.Component {
   static propTypes = {
-    organization: SentryTypes.Organization,
+    organization: SentryTypes.Organization.isRequired,
+    selection: SentryTypes.GlobalSelection.isRequired,
   };
 
   render() {
+    // eslint-disable-next-line no-unused-vars
+    const {selection, ...props} = this.props;
+
     return (
       <Feature features={['sentry10']} renderDisabled>
         <GlobalSelectionHeader organization={this.props.organization} />
         <PageContent>
-          <GroupDetails {...this.props} />
+          <GroupDetails environments={selection.environments} {...props} />
         </PageContent>
       </Feature>
     );
   }
 }
 
-export default withOrganization(OrganizationGroupDetails);
+export default withOrganization(withGlobalSelection(OrganizationGroupDetails));
