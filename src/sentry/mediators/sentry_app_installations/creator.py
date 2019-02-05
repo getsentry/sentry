02866b4ba14ffe6ca7b830e3baa7c1d5e2119769
Meredith <meredith@getsentry.com>
2019-02-05 14:55:38 -0800

    projects check

--- src/sentry/mediators/sentry_app_installations/creator.py
+++ src/sentry/mediators/sentry_app_installations/creator.py
@@ -47,14 +47,15 @@ class Creator(Mediator):
 
     def _create_service_hooks(self):
         projects = self.organization.project_set.all()
-        service_hooks.Creator.run(
-            application=self.api_application,
-            actor=self.install,
-            projects=projects,
-            organization=self.organization,
-            events=self.sentry_app.events,
-            url=self.sentry_app.webhook_url,
-        )
+        if projects:
+            service_hooks.Creator.run(
+                application=self.api_application,
+                actor=self.install,
+                projects=projects,
+                organization=self.organization,
+                events=self.sentry_app.events,
+                url=self.sentry_app.webhook_url,
+            )
 
     def _notify_service(self):
         installation_webhook.delay(self.install.id, self.user.id)
