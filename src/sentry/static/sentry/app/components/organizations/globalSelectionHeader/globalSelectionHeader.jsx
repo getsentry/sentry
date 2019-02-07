9203cc25ab4a9e4d1d27ea66e7f7841f140b4e32
chrissy <christopherchristopherclark@gmail.com>
2019-02-06 19:00:44 -0800

    check for sentry 10

--- src/sentry/static/sentry/app/components/organizations/globalSelectionHeader/globalSelectionHeader.jsx
+++ src/sentry/static/sentry/app/components/organizations/globalSelectionHeader/globalSelectionHeader.jsx
@@ -99,15 +99,19 @@ class GlobalSelectionHeader extends React.Component {
       return;
     }
 
-    AlertActions.addAlert({
-      message:
-        'Hi! You are seeing some new changes to Sentry as a member of our early adopter program. Click to read more.',
-      type: 'info',
-      url: 'https://forum.sentry.io/t/new-product-changes-now-available-for-preview/5805',
-      neverExpire: true,
-      noDuplicates: true,
-      id: 'visibility-changes-alert-message',
-    });
+    const hasSentry10 = new Set(this.props.organization.features).has('sentry10');
+
+    if (hasSentry10)
+      AlertActions.addAlert({
+        message:
+          'Hi! You are seeing some new changes to Sentry as a member of our early adopter program. Click to read more.',
+        type: 'info',
+        url:
+          'https://forum.sentry.io/t/new-product-changes-now-available-for-preview/5805',
+        neverExpire: true,
+        noDuplicates: true,
+        id: 'visibility-changes-alert-message',
+      });
 
     const hasMultipleProjectFeature = this.hasMultipleProjectSelection();
 
