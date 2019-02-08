0ee9046be44e465f548e39494ba50f728f9cdcdd
Meredith <meredith@getsentry.com>
2019-02-08 15:01:18 -0800

    forgot le import

--- src/sentry/south_migrations/0463_backfill_service_hook_project.py
+++ src/sentry/south_migrations/0463_backfill_service_hook_project.py
@@ -25,6 +25,7 @@ class Migration(DataMigration):
         # Note: Don't use "from appname.models import ModelName".
         # Use orm.ModelName to refer to models in this application,
         # and orm['appname.ModelName'] for models in other applications.
+        from sentry.utils.query import RangeQuerySetWrapperWithProgressBar
         ServiceHook = orm['sentry.ServiceHook']
         ServiceHookProject = orm['sentry.ServiceHookProject']
         Project = orm['sentry.Project']
