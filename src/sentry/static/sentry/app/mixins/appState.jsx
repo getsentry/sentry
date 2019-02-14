9cc6110da778c1340c6809f17fd4332b85d2032f
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-02-14 14:21:50 -0800

    ref(frontend): Remove completely unused appState mixin (#12076)

--- src/sentry/static/sentry/app/mixins/appState.jsx
+++
@@ -1,9 +0,0 @@
-import OrganizationsStore from 'app/stores/organizationsStore';
-
-const AppState = {
-  getOrganizationList() {
-    return OrganizationsStore.getAll();
-  },
-};
-
-export default AppState;
