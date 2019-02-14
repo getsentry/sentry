fed82f066c5d36045b8f1f6ee8e4e2c189d78cb0
Dan Fuller <dfuller@sentry.io>
2019-02-14 15:42:13 -0800

    feat(api): Add bitflag and api changes to allow orgs to opt out of sentry10/visibility
    We want this as a safety options in case any users are particularly mad about the sentry 10 changes.
    This gives us a flag that we can check in getsentry to just disable sentry10 for any orgs that have
    opted out.

--- src/sentry/api/endpoints/organization_details.py
+++ src/sentry/api/endpoints/organization_details.py
@@ -93,6 +93,7 @@ class OrganizationSerializer(serializers.Serializer):
     scrapeJavaScript = serializers.BooleanField(required=False)
     isEarlyAdopter = serializers.BooleanField(required=False)
     require2FA = serializers.BooleanField(required=False)
+    disableNewVisibilityFeatures = serializers.BooleanField(required=False)
     trustedRelays = ListField(child=serializers.CharField(), required=False)
 
     @memoize
@@ -230,6 +231,8 @@ class OrganizationSerializer(serializers.Serializer):
             org.flags.enhanced_privacy = self.init_data['enhancedPrivacy']
         if 'isEarlyAdopter' in self.init_data:
             org.flags.early_adopter = self.init_data['isEarlyAdopter']
+        if 'disableNewVisibilityFeatures' in self.init_data:
+            org.flags.disable_new_visibility_features = self.init_data['disableNewVisibilityFeatures']
         if 'require2FA' in self.init_data:
             org.flags.require_2fa = self.init_data['require2FA']
         if 'name' in self.init_data:
