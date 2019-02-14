62ea7bd935c0b0360fb903fb9f7135210e4c7df6
Lyn Nagara <lyn.nagara@gmail.com>
2019-02-14 12:24:59 -0800

    feat(discover): Ignore double quotes in Discover condition values (#12065)
    Since these are frequently entered by users, just ignore double quotes
    by default. Users will need to enter 2 sets of double quotes if they
    want to query for an actual value with double quotes.

--- tests/js/spec/views/organizationDiscover/conditions/utils.spec.jsx
+++ tests/js/spec/views/organizationDiscover/conditions/utils.spec.jsx
@@ -53,8 +53,12 @@ describe('Conditions', function() {
     });
 
     // datetime fields are expanded
-    const expected = ['timestamp', '=', '2018-05-05T00:00:00'];
-    expect(getExternal('timestamp = 2018-05-05', COLUMNS)).toEqual(expected);
+    const expectedTimestamp = ['timestamp', '=', '2018-05-05T00:00:00'];
+    expect(getExternal('timestamp = 2018-05-05', COLUMNS)).toEqual(expectedTimestamp);
+
+    // strips double quotes
+    const expectedValue = ['message', '=', 'test'];
+    expect(getExternal('message = "test"', COLUMNS)).toEqual(expectedValue);
   });
 
   it('getInternal()', function() {
