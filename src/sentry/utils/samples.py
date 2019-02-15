bce06c09d20d78685806e5cbe5c7ff4a415493ca
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-02-15 20:08:30 +0100

    fix(samples): Move code to not impact production

--- src/sentry/utils/samples.py
+++ src/sentry/utils/samples.py
@@ -202,8 +202,4 @@ def create_sample_event(project, platform=None, default=None,
 
     manager = EventManager(data)
     manager.normalize()
-    event = manager.save(project.id, raw=raw)
-    # Prevent Percy screenshot from constantly changing
-    event.datetime = datetime(2017, 9, 6, 0, 0)
-    event.save()
-    return event
+    return manager.save(project.id, raw=raw)
