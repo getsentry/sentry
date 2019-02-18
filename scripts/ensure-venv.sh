351e496a0b42014c782a3aa69fd952c459958ea2
Armin Ronacher <armin.ronacher@active-4.com>
2019-02-18 14:21:15 +0100

    feat: Added better virtualenv config

---
+++ scripts/ensure-venv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# if we already have a venv, we can leave
+if [ -f .venv/bin/python ]; then
+  exit 0
+fi
+
+# do we have a WORKON_HOME? Prime from there
+if [ "x$WORKON_HOME" != x ]; then
+  ln -s "${WORKON_HOME}/sentry" .venv
+  exit 0
+fi
+
+# otherwise make a new virtualenv from scratch
+virtualenv -ppython2.7 .venv
