416659bd57025b69b6eb28410b199b9524670954
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-02-18 09:29:11 +0100

    fix: Parse X-Sentry-Auth as latin1 (#12108)

--- src/sentry/utils/auth.py
+++ src/sentry/utils/auth.py
@@ -34,12 +34,14 @@ class AuthUserPasswordExpired(Exception):
 
 
 def _make_key_value(val):
-    return val.strip().split('=', 1)
+    return val.strip().split(u'=', 1)
 
 
 def parse_auth_header(header):
+    if isinstance(header, bytes):
+        header = header.decode('latin1')
     try:
-        return dict(map(_make_key_value, header.split(' ', 1)[1].split(',')))
+        return dict(map(_make_key_value, header.split(u' ', 1)[1].split(u',')))
     except Exception:
         return {}
 
