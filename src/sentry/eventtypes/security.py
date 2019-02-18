8c1ab1d1c932b753ab476ad589fd189819bb66bf
Armin Ronacher <armin.ronacher@active-4.com>
2019-02-18 22:14:00 +0100

    feat: Refactor eventtypes to take data by argument
    The current event type constructor needs a data dictionary even if we
    only want to deal with metadata.  This is no longer useful and we
    already pass an incorrect data dictionary in some cases when we only
    want to read the metadata.
    
    This moves the data so that its passed to the event type method
    directly.

--- src/sentry/eventtypes/security.py
+++ src/sentry/eventtypes/security.py
@@ -6,14 +6,14 @@ from .base import BaseEvent
 class CspEvent(BaseEvent):
     key = 'csp'
 
-    def has_metadata(self):
-        return self.data.get('csp') is not None
+    def has_metadata(self, data):
+        return data.get('csp') is not None
 
-    def get_metadata(self):
+    def get_metadata(self, data):
         from sentry.interfaces.security import Csp
         # TODO(dcramer): pull get message into here to avoid instantiation
         # or ensure that these get interfaces passed instead of raw data
-        csp = Csp.to_python(self.data['csp'])
+        csp = Csp.to_python(data['csp'])
 
         return {
             'directive': csp.effective_directive,
@@ -31,12 +31,12 @@ class CspEvent(BaseEvent):
 class HpkpEvent(BaseEvent):
     key = 'hpkp'
 
-    def has_metadata(self):
-        return self.data.get('hpkp') is not None
+    def has_metadata(self, data):
+        return data.get('hpkp') is not None
 
-    def get_metadata(self):
+    def get_metadata(self, data):
         from sentry.interfaces.security import Hpkp
-        hpkp = Hpkp.to_python(self.data['hpkp'])
+        hpkp = Hpkp.to_python(data['hpkp'])
         return {
             'origin': hpkp.get_origin(),
             'message': hpkp.get_message(),
@@ -52,12 +52,12 @@ class HpkpEvent(BaseEvent):
 class ExpectCTEvent(BaseEvent):
     key = 'expectct'
 
-    def has_metadata(self):
-        return self.data.get('expectct') is not None
+    def has_metadata(self, data):
+        return data.get('expectct') is not None
 
-    def get_metadata(self):
+    def get_metadata(self, data):
         from sentry.interfaces.security import ExpectCT
-        expectct = ExpectCT.to_python(self.data['expectct'])
+        expectct = ExpectCT.to_python(data['expectct'])
         return {
             'origin': expectct.get_origin(),
             'message': expectct.get_message(),
@@ -73,12 +73,12 @@ class ExpectCTEvent(BaseEvent):
 class ExpectStapleEvent(BaseEvent):
     key = 'expectstaple'
 
-    def has_metadata(self):
-        return self.data.get('expectstaple') is not None
+    def has_metadata(self, data):
+        return data.get('expectstaple') is not None
 
-    def get_metadata(self):
+    def get_metadata(self, data):
         from sentry.interfaces.security import ExpectStaple
-        expectstaple = ExpectStaple.to_python(self.data['expectstaple'])
+        expectstaple = ExpectStaple.to_python(data['expectstaple'])
         return {
             'origin': expectstaple.get_origin(),
             'message': expectstaple.get_message(),
