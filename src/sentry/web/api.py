536b5e330dd292ba2674636d9e690dea8bd8f979
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-02-19 10:41:31 +0100

    fix: Introduce hard limit on event size after normalization (#11846)

--- src/sentry/web/api.py
+++ src/sentry/web/api.py
@@ -549,6 +549,14 @@ class StoreView(APIView):
         self.pre_normalize(event_manager, helper)
         event_manager.normalize()
 
+        data = event_manager.get_data()
+        dict_data = dict(data)
+        data_size = len(json.dumps(dict_data))
+
+        if data_size > 10000000:
+            metrics.timing('events.size.rejected', data_size)
+            raise APIForbidden("Event size exceeded 10MB after normalization.")
+
         return process_event(event_manager, project,
                              key, remote_addr, helper, attachments)
 
