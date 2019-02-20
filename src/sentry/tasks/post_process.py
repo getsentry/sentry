927a693fc7d6464d93148feecfaec8a6db2f04de
Meredith <meredith@getsentry.com>
2019-02-20 10:25:39 -0800

    dont use values_list

--- src/sentry/tasks/post_process.py
+++ src/sentry/tasks/post_process.py
@@ -31,12 +31,12 @@ def _get_service_hooks(project_id):
     from sentry.models import ServiceHook
     cache_key = u'servicehooks:1:{}'.format(project_id)
     result = cache.get(cache_key)
+
     if result is None:
-
-        result = ServiceHook.objects.filter(
+        hooks = ServiceHook.objects.filter(
             servicehookproject__project_id=project_id,
-        ).values_list('id', 'events')
-
+        )
+        result = [(h.id, h.events) for h in hooks]
         cache.set(cache_key, result, 60)
     return result
 
