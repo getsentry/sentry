92f3a2fba655f65ba3766689e7184a7dfa6d5df7
Armin Ronacher <armin.ronacher@active-4.com>
2019-02-20 23:53:02 +0100

    feat: Provide a way to opt out of venv management (#12143)

--- scripts/ensure-venv.sh
+++ scripts/ensure-venv.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -xe
 
+# optionally opt out of virtualenv creation
+if [ "x$SENTRY_NO_VIRTUALENV_CREATION" == "x1" ]; then
+  exit 0
+fi
+
 # if we already have a venv, we can leave
 if .venv/bin/python --version &> /dev/null; then
   exit 0
