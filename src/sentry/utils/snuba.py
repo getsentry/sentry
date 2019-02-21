49973ee1bdc5b4af5e79df055399038c2c18f2c7
Dan Fuller <dfuller@sentry.io>
2019-02-20 17:24:23 -0800

    fix(api): Use tags[sentry:user] instead of sentry:user when querying snuba (APP-1151)
    Fixes SENTRY-9KW

--- src/sentry/utils/snuba.py
+++ src/sentry/utils/snuba.py
@@ -106,7 +106,7 @@ SENTRY_SNUBA_MAP = {
     'contexts.value': 'contexts.value',
     # misc
     'release': 'tags[sentry:release]',
-    'user': 'sentry:user',
+    'user': 'tags[sentry:user]',
 }
 
 
