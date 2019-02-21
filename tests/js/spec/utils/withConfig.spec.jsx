c4d1def6666bf581589f66ffe5f3033521150bff
Lyn Nagara <lyn.nagara@gmail.com>
2019-02-21 10:57:09 -0800

    ref: Introduce withConfig HoC (#12152)
    Provide an easier way to make config available to React components.
    Refactor a few components to use this instead of
    mixins/createReactClass.

---
+++ tests/js/spec/utils/withConfig.spec.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {mount} from 'enzyme';
+
+import ConfigStore from 'app/stores/configStore';
+import withConfig from 'app/utils/withConfig';
+
+describe('withConfig HoC', function() {
+  it('adds config prop', function() {
+    ConfigStore.init();
+    const MyComponent = () => null;
+    const Container = withConfig(MyComponent);
+    const wrapper = mount(<Container />);
+    expect(wrapper.find('MyComponent').prop('config')).toEqual({});
+    ConfigStore.set('user', 'foo');
+    expect(wrapper.find('MyComponent').prop('config')).toEqual({user: 'foo'});
+  });
+});
