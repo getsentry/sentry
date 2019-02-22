8c9787f3b879d7177e9c689832178d221f6d9cd7
Billy Vong <billy@sentry.io>
2019-02-22 11:11:18 -0800

    fix(events): Do not include previous period if requesting > 45 days
    Max is 90day range, so do not request previous period if > 45 days

--- src/sentry/static/sentry/app/views/organizationEvents/utils/eventsRequest.jsx
+++ src/sentry/static/sentry/app/views/organizationEvents/utils/eventsRequest.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import React from 'react';
 
 import {addErrorMessage} from 'app/actionCreators/indicator';
+import {canIncludePreviousPeriod} from 'app/views/organizationEvents/utils/canIncludePreviousPeriod';
 import {doEventsRequest} from 'app/actionCreators/events';
 import {t} from 'app/locale';
 import SentryTypes from 'app/sentryTypes';
@@ -160,7 +161,7 @@ class EventsRequest extends React.PureComponent {
    * Returns `null` if data does not exist
    */
   getData = data => {
-    const {includePrevious} = this.props;
+    const {period, includePrevious} = this.props;
 
     if (!data) {
       return {
@@ -169,7 +170,7 @@ class EventsRequest extends React.PureComponent {
       };
     }
 
-    const hasPreviousPeriod = includePrevious;
+    const hasPreviousPeriod = canIncludePreviousPeriod(includePrevious, period);
     // Take the floor just in case, but data should always be divisible by 2
     const dataMiddleIndex = Math.floor(data.length / 2);
 
