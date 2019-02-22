c909a0239349b93cbdc66109268f9707277e5e0e
Meredith <meredith@getsentry.com>
2019-02-22 11:09:57 -0800

    add more tests

--- tests/sentry/tasks/test_sentry_apps.py
+++ tests/sentry/tasks/test_sentry_apps.py
@@ -278,3 +278,29 @@ class TestWorkflowNotification(TestCase):
         assert faux(safe_urlopen).kwarg_equals('data.actor.type', 'application', format='json')
         assert faux(safe_urlopen).kwarg_equals('data.actor.id', 'sentry', format='json')
         assert faux(safe_urlopen).kwarg_equals('data.actor.name', 'Sentry', format='json')
+
+    def test_does_not_send_if_no_service_hook_exists(self, safe_urlopen):
+        sentry_app = self.create_sentry_app(
+            name='Another App',
+            organization=self.project.organization,
+            events=[],
+        )
+        install = self.create_sentry_app_installation(
+            organization=self.project.organization,
+            slug=sentry_app.slug,
+        )
+        workflow_notification(install.id, self.issue.id, 'assigned', self.user.id)
+        assert not safe_urlopen.called
+
+    def test_does_not_send_if_event_not_in_app_events(self, safe_urlopen):
+        sentry_app = self.create_sentry_app(
+            name='Another App',
+            organization=self.project.organization,
+            events=['issue.resolved', 'issue.ignored'],
+        )
+        install = self.create_sentry_app_installation(
+            organization=self.project.organization,
+            slug=sentry_app.slug,
+        )
+        workflow_notification(install.id, self.issue.id, 'assigned', self.user.id)
+        assert not safe_urlopen.called
