getsentry/sentry

commit 6a3d274062afea6c17e3562322b283f0adc1c759

feat(superuser): Do not store global selection for non org members (#12160)
Do not store any global selection values in local storage for users
who are not org members. This currently only affects superusers.

src/sentry/static/sentry/app/stores/globalSelectionStore.jsx
@@ -10,6 +10,7 @@ import {getStateFromQuery} from 'app/components/organizations/globalSelectionHea
 import {DEFAULT_STATS_PERIOD} from 'app/constants';
 import {isEqualWithDates} from 'app/utils/isEqualWithDates';
 import ConfigStore from 'app/stores/configStore';
+import OrganizationsStore from 'app/stores/organizationsStore';
 import GlobalSelectionActions from 'app/actions/globalSelectionActions';
 import localStorage from 'app/utils/localStorage';
 
@@ -139,10 +140,14 @@ const GlobalSelectionStore = Reflux.createStore({
   },
 
   updateLocalStorage() {
+    // Do nothing if no org is loaded or user is not an org member. Only
+    // organizations that a user has membership in will be available via the
+    // organizations store
+    if (!this.organization || OrganizationsStore.get(this.organization.slug)) {
+      return;
+    }
+
     try {
-      if (!this.organization) {
-        throw new Error('No organization loaded');
-      }
       const localStorageKey = `${LOCAL_STORAGE_KEY}:${this.organization.slug}`;
       localStorage.setItem(localStorageKey, JSON.stringify(this.selection));
     } catch (ex) {

commit 55ccdb6d32827cbe28a729fa666a83e65ef9e159

fix: Fix global selection persistence
Fixes a bug introduced in https://github.com/getsentry/sentry/pull/12160
where the organization member check was flipped around the wrong way.

src/sentry/static/sentry/app/stores/globalSelectionStore.jsx
@@ -143,7 +143,7 @@ const GlobalSelectionStore = Reflux.createStore({
     // Do nothing if no org is loaded or user is not an org member. Only
     // organizations that a user has membership in will be available via the
     // organizations store
-    if (!this.organization || OrganizationsStore.get(this.organization.slug)) {
+    if (!this.organization || !OrganizationsStore.get(this.organization.slug)) {
       return;
     }
 