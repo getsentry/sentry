2a61fb59a1b7354da09ff5d7c89c368a15463a5d
Matte Noble <me@mattenoble.com>
2019-02-26 10:35:54 -0800

    feat(app-platform): UI Component Endpoints (#12139)
    Adds an endpoint to expose Sentry App Components for a particular Sentry
    App.
    
    Also updates the Sentry App Creator to parse schema and create
    SentryAppComponent records.

--- src/sentry/testutils/fixtures.py
+++ src/sentry/testutils/fixtures.py
@@ -31,7 +31,7 @@ from sentry.models import (
     Activity, Environment, Event, EventError, EventMapping, Group, Organization, OrganizationMember,
     OrganizationMemberTeam, Project, ProjectBookmark, Team, User, UserEmail, Release, Commit, ReleaseCommit,
     CommitAuthor, Repository, CommitFileChange, ProjectDebugFile, File, UserPermission, EventAttachment,
-    UserReport
+    UserReport,
 )
 from sentry.utils.canonical import CanonicalKeyDict
 
@@ -751,6 +751,7 @@ class Fixtures(object):
             'scopes': scopes,
             'webhook_url': webhook_url,
             'events': [],
+            'schema': '{}',
         }
 
         _kwargs.update(kwargs)
@@ -773,6 +774,69 @@ class Fixtures(object):
             organization=organization,
             user=(user or self.create_user()),
         )
+
+    def create_issue_link_schema(self):
+        return {
+            'type': 'issue-link',
+            'link': {
+                'uri': '/sentry/issues/link',
+                'required_fields': [
+                    {
+                        'type': 'select',
+                        'name': 'assignee',
+                        'label': 'Assignee',
+                        'uri': '/sentry/members',
+                    },
+                ],
+            },
+
+            'create': {
+                'uri': '/sentry/issues/create',
+                'required_fields': [
+                    {
+                        'type': 'text',
+                        'name': 'title',
+                        'label': 'Title',
+                    },
+                    {
+                        'type': 'text',
+                        'name': 'summary',
+                        'label': 'Summary',
+                    },
+                ],
+
+                'optional_fields': [
+                    {
+                        'type': 'select',
+                        'name': 'points',
+                        'label': 'Points',
+                        'options': [
+                            ['1', '1'],
+                            ['2', '2'],
+                            ['3', '3'],
+                            ['5', '5'],
+                            ['8', '8'],
+                        ],
+                    },
+                    {
+                        'type': 'select',
+                        'name': 'assignee',
+                        'label': 'Assignee',
+                        'uri': '/sentry/members',
+                    },
+                ],
+            },
+        }
+
+    def create_alert_rule_action_schema(self):
+        return {
+            'type': 'alert-rule-action',
+            'required_fields': [{
+                'type': 'text',
+                'name': 'channel',
+                'label': 'Channel',
+            }],
+        }
 
     def create_service_hook(self, actor=None, org=None, project=None,
                             events=None, url=None, **kwargs):
