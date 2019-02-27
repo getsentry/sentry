1bc24e5c56de1b912465ec427d8db09a5bcad683
Dan Fuller <dfuller@sentry.io>
2019-02-26 19:23:46 -0800

    feat(api): Allow metacharacters to be escaped in event/issue searching (SEN-153)
    `fnmatch.translate` doesn't currently support escaping metacharacters. Found some discussion about
    this and a simple patch to allow escaping.
    
    As part of testing that this works as expected, also found that `positionCaseInsensitive` only
    accepts a needle (basically a string) to search for. This means that wildcard matching is doesn't
    work for message in both issue and event search. Converted it over to use `match`, and retained
    case insensitivity by using (?i) to set the case insensitive flag. Also added this to matching for
    other values.
    
    Also found that the handling of `message` in postgres also did not support regex matching. Switched
    over to using it for wildcard matches, but only when using postgres. Use the normal `contains`
    matching for non-wild card as usual.
    
    I also added a hack in to both postgres and snuba to strip the `^$` characters from the
    beginning/end of the regex, since I assume it makes sense to search for the regex all throughout
    the message. Not sure if this is required for other fields, if not we could just remove it from
    `translate` in general.

--- src/sentry/api/event_search.py
+++ src/sentry/api/event_search.py
@@ -24,7 +24,6 @@ WILDCARD_CHARS = re.compile(r'[\*\[\]\?]')
 
 def translate(pat):
     """Translate a shell PATTERN to a regular expression.
-    There is no way to quote meta-characters.
     modified from: https://github.com/python/cpython/blob/2.7/Lib/fnmatch.py#L85
     """
 
@@ -33,7 +32,13 @@ def translate(pat):
     while i < n:
         c = pat[i]
         i = i + 1
-        if c == '*':
+        # fnmatch.translate has no way to handle escaping metacharacters.
+        # Applied this basic patch to handle it:
+        # https://bugs.python.org/file27570/issue8402.1.patch
+        if c == '\\':
+            res += re.escape(pat[i])
+            i += 1
+        elif c == '*':
             res = res + '.*'
         elif c == '?':
             res = res + '.'
@@ -427,12 +432,19 @@ def convert_search_filter_to_snuba_query(search_filter):
         return env_conditions
 
     elif snuba_name == 'message':
-        # https://clickhouse.yandex/docs/en/query_language/functions/string_search_functions/#position-haystack-needle
-        # positionCaseInsensitive returns 0 if not found and an index of 1 or more if found
-        # so we should flip the operator here
-        operator = '=' if search_filter.operator == '!=' else '!='
-        # make message search case insensitive
-        return [['positionCaseInsensitive', ['message', "'%s'" % (value,)]], operator, 0]
+        if search_filter.value.is_wildcard():
+            # XXX: We don't want the '^$' values at the beginning and end of
+            # the regex since we want to find the pattern anywhere in the
+            # message. Strip off here
+            value = search_filter.value.value[1:-1]
+            return [['match', ['message', "'(?i)%s'" % (value,)]], search_filter.operator, 1]
+        else:
+            # https://clickhouse.yandex/docs/en/query_language/functions/string_search_functions/#position-haystack-needle
+            # positionCaseInsensitive returns 0 if not found and an index of 1 or more if found
+            # so we should flip the operator here
+            operator = '=' if search_filter.operator == '!=' else '!='
+            # make message search case insensitive
+            return [['positionCaseInsensitive', ['message', "'%s'" % (value,)]], operator, 0]
 
     else:
         value = int(to_timestamp(value)) * 1000 if isinstance(value,
@@ -449,7 +461,7 @@ def convert_search_filter_to_snuba_query(search_filter):
             snuba_name = ['ifNull', [snuba_name, "''"]]
 
         if search_filter.value.is_wildcard():
-            return [['match', [snuba_name, "'%s'" % (value,)]], search_filter.operator, 1]
+            return [['match', [snuba_name, "'(?i)%s'" % (value,)]], search_filter.operator, 1]
         else:
             return [snuba_name, search_filter.operator, value]
 
