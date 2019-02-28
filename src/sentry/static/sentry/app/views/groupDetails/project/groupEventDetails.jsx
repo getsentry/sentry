3b75e910ebcdfdce2fede894a2ada644357d547e
Lyn Nagara <lyn.nagara@gmail.com>
2019-02-27 17:36:11 -0800

    ref(issues): Refactor issue details to make environments a prop
    Refactor the issue details page so it always gets environments as a
    prop. Makes it easier to add more environment awareness to this page,
    which will be tackled soon.

--- src/sentry/static/sentry/app/views/groupDetails/project/groupEventDetails.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/project/groupEventDetails.jsx
@@ -1,4 +1,11 @@
+import React from 'react';
+
 import withEnvironmentInQueryString from 'app/utils/withEnvironmentInQueryString';
 import GroupEventDetails from '../shared/groupEventDetails';
 
-export default withEnvironmentInQueryString(GroupEventDetails);
+export default withEnvironmentInQueryString(props => {
+  const {environment, ...otherProps} = props;
+  return (
+    <GroupEventDetails {...otherProps} environments={environment ? [environment] : []} />
+  );
+});
