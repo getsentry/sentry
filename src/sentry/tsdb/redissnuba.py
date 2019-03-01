2387e0cbd6f2190d5988d669fa17022fec8b4fe1
Matt Robenolt <matt@ydekproductions.com>
2019-03-01 14:37:23 -0800

    fix(tsdb): Call super in RedisSnubaTSDB

--- src/sentry/tsdb/redissnuba.py
+++ src/sentry/tsdb/redissnuba.py
@@ -89,5 +89,6 @@ class RedisSnubaTSDBMeta(type):
 class RedisSnubaTSDB(BaseTSDB):
     def __init__(self, **options):
         self.dummy = DummyTSDB()
-        self.redis = RedisTSDB(**options.get('redis', {}))
-        self.snuba = SnubaTSDB(**options.get('snuba', {}))
+        self.redis = RedisTSDB(**options.pop('redis', {}))
+        self.snuba = SnubaTSDB(**options.pop('snuba', {}))
+        super(RedisSnubaTSDB, self).__init__(**options)
