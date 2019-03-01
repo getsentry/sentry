00a6ea73cf9a2417da562adbd93ce617693a72bb
Brett Hoerner <brett@bretthoerner.com>
2019-03-01 13:46:29 -0600

    move funcs

--- src/sentry/consumer.py
+++ src/sentry/consumer.py
@@ -8,54 +8,51 @@ import sentry.tasks.store as store_tasks
 from sentry.utils import json
 
 
-def handle_preprocess(message):
-    data = message['data']
-    event_id = data['event_id']
-    cache_key = message['attachments_cache_key']
-    start_time = message['start_time']
-    process_task = (
-        store_tasks.process_event_from_reprocessing
-        if message['from_reprocessing']
-        else store_tasks.process_event
-    )
-
-    store_tasks._do_preprocess_event(cache_key, data, start_time, event_id, process_task)
-
-
-def handle_process(message):
-    data = message['data']
-    event_id = data['event_id']
-    cache_key = message['attachments_cache_key']
-    start_time = message['start_time']
-    process_task = (
-        store_tasks.process_event_from_reprocessing
-        if message['from_reprocessing']
-        else store_tasks.process_event
-    )
-
-    store_tasks._do_process_event(cache_key, start_time, event_id, process_task, data=data)
-
-
-def handle_save(message):
-    data = message['data']
-    event_id = data['event_id']
-    cache_key = message['attachments_cache_key']
-    start_time = message['start_time']
-    project_id = data['project']
-
-    store_tasks._do_save_event(cache_key, data, start_time, event_id, project_id)
-
-
 class ConsumerWorker(AbstractBatchWorker):
     def __init__(self):
         self.dispatch = {}
         for key, handler in (
-            (settings.KAFKA_PREPROCESS, handle_preprocess),
-            (settings.KAFKA_PROCESS, handle_process),
-            (settings.KAFKA_SAVE, handle_save)
+            (settings.KAFKA_PREPROCESS, self.handle_preprocess),
+            (settings.KAFKA_PROCESS, self.handle_process),
+            (settings.KAFKA_SAVE, self.handle_save)
         ):
             topic = settings.KAFKA_TOPICS[key]['topic']
             self.dispatch[topic] = handler
+
+    def handle_preprocess(self, message):
+        data = message['data']
+        event_id = data['event_id']
+        cache_key = message['attachments_cache_key']
+        start_time = message['start_time']
+        process_task = (
+            store_tasks.process_event_from_reprocessing
+            if message['from_reprocessing']
+            else store_tasks.process_event
+        )
+
+        store_tasks._do_preprocess_event(cache_key, data, start_time, event_id, process_task)
+
+    def handle_process(self, message):
+        data = message['data']
+        event_id = data['event_id']
+        cache_key = message['attachments_cache_key']
+        start_time = message['start_time']
+        process_task = (
+            store_tasks.process_event_from_reprocessing
+            if message['from_reprocessing']
+            else store_tasks.process_event
+        )
+
+        store_tasks._do_process_event(cache_key, start_time, event_id, process_task, data=data)
+
+    def handle_save(self, message):
+        data = message['data']
+        event_id = data['event_id']
+        cache_key = message['attachments_cache_key']
+        start_time = message['start_time']
+        project_id = data['project']
+
+        store_tasks._do_save_event(cache_key, data, start_time, event_id, project_id)
 
     def process_message(self, message):
         topic = message.topic()
