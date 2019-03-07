74c7c28f918ba59ae06942307c6f42763d28ea93
Mark Story <mark@sentry.io>
2019-03-07 16:39:24 -0500

    fix: Fix problematic method signatures in serializers
    serialize() had `*args` following keyword arguments. This results in
    `*args` not being usable as python globs too many arguments into keyword
    args.
    
    Refs SEN-64

--- src/sentry/api/serializers/base.py
+++ src/sentry/api/serializers/base.py
@@ -14,10 +14,7 @@ def serialize(objects, user=None, serializer=None, *args, **kwargs):
     # sets aren't predictable, so generally you should use a list, but it's
     # supported out of convenience
     elif not isinstance(objects, (list, tuple, set, frozenset)):
-        return serialize([objects], user=user, serializer=serializer, *args, **kwargs)[0]
-
-    # elif isinstance(obj, dict):
-    #     return dict((k, serialize(v, request=request)) for k, v in six.iteritems(obj))
+        return serialize([objects], user, serializer, *args, **kwargs)[0]
 
     if serializer is None:
         # find the first object that is in the registry
@@ -33,13 +30,13 @@ def serialize(objects, user=None, serializer=None, *args, **kwargs):
     attrs = serializer.get_attrs(
         # avoid passing NoneType's to the serializer as they're allowed and
         # filtered out of serialize()
-        item_list=[o for o in objects if o is not None],
-        user=user,
+        [o for o in objects if o is not None],
+        user,
         *args,
         **kwargs
     )
 
-    return [serializer(o, attrs=attrs.get(o, {}), user=user, *args, **kwargs) for o in objects]
+    return [serializer(o, attrs.get(o, {}), user, *args, **kwargs) for o in objects]
 
 
 def register(type):
