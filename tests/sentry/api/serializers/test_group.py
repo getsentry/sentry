ba1e96c9ba466983845eef2b6b8bff5f0c497489
Mark Story <mark@sentry.io>
2019-03-11 16:42:34 -0400

    fix(ui) Fix platform icons not showing on issue list
    The project stub inside the group was lacking its platform property
    which is necessary to display the correct icon.
    
    Fixes SEN-254

--- tests/sentry/api/serializers/test_group.py
+++ tests/sentry/api/serializers/test_group.py
@@ -20,6 +20,17 @@ from sentry.testutils import TestCase
 
 
 class GroupSerializerTest(TestCase):
+    def test_project(self):
+        user = self.create_user()
+        group = self.create_group()
+
+        result = serialize(group, user)
+        assert result['project']
+        assert 'id' in result['project']
+        assert 'name' in result['project']
+        assert 'slug' in result['project']
+        assert 'platform' in result['project']
+
     def test_is_ignored_with_expired_snooze(self):
         now = timezone.now().replace(microsecond=0)
 
