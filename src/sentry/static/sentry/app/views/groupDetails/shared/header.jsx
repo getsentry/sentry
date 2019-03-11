da8080fc3c0e3952692f521485f2c04c26728486
Lyn Nagara <lyn.nagara@gmail.com>
2019-03-11 11:58:43 -0700

    fix(issues): Fix active tab highlighting
    Highlight active tab correctly on the issue details page. Need to pass
    isActive prop directly since default behavior won't work with
    querystring.

--- src/sentry/static/sentry/app/views/groupDetails/shared/header.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/shared/header.jsx
@@ -217,24 +217,42 @@ const GroupHeader = createReactClass({
           >
             {t('Details')}
           </ListLink>
-          <ListLink to={`${baseUrl}${groupId}/activity/${this.context.location.search}`}>
+          <ListLink
+            to={`${baseUrl}${groupId}/activity/${this.context.location.search}`}
+            isActive={() => location.pathname.includes('/activity/')}
+          >
             {t('Comments')} <span className="badge animated">{group.numComments}</span>
           </ListLink>
-          <ListLink to={`${baseUrl}${groupId}/feedback/${this.context.location.search}`}>
+          <ListLink
+            to={`${baseUrl}${groupId}/feedback/${this.context.location.search}`}
+            isActive={() => location.pathname.includes('/feedback/')}
+          >
             {t('User Feedback')}
             <span className="badge animated">{group.userReportCount}</span>
           </ListLink>
-          <ListLink to={`${baseUrl}${groupId}/tags/${this.context.location.search}`}>
+          <ListLink
+            to={`${baseUrl}${groupId}/tags/${this.context.location.search}`}
+            isActive={() => location.pathname.includes('/tags/')}
+          >
             {t('Tags')}
           </ListLink>
-          <ListLink to={`${baseUrl}${groupId}/events/${this.context.location.search}`}>
+          <ListLink
+            to={`${baseUrl}${groupId}/events/${this.context.location.search}`}
+            isActive={() => location.pathname.includes('/events/')}
+          >
             {t('Events')}
           </ListLink>
-          <ListLink to={`${baseUrl}${groupId}/merged/${this.context.location.search}`}>
+          <ListLink
+            to={`${baseUrl}${groupId}/merged/${this.context.location.search}`}
+            isActive={() => location.pathname.includes('/merged/')}
+          >
             {t('Merged')}
           </ListLink>
           {hasSimilarView && (
-            <ListLink to={`${baseUrl}${groupId}/similar/${this.context.location.search}`}>
+            <ListLink
+              to={`${baseUrl}${groupId}/similar/${this.context.location.search}`}
+              isActive={() => location.pathname.includes('/similar/')}
+            >
               {t('Similar Issues')}
             </ListLink>
           )}
