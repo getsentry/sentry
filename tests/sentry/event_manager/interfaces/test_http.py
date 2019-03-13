7ccd17126e4697adb10334a5962ae35d2c8ace21
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-03-13 17:22:26 +0100

    ref: Port test_http.py to snapshot tests

--- tests/sentry/event_manager/interfaces/test_http.py
+++ tests/sentry/event_manager/interfaces/test_http.py
@@ -2,237 +2,210 @@
 
 from __future__ import absolute_import
 
-from exam import fixture
+import pytest
 
-from sentry.interfaces.base import InterfaceValidationError
-from sentry.interfaces.http import Http
-from sentry.testutils import TestCase
 from sentry.models import Event
 from sentry.event_manager import EventManager
 
 
-def to_python(data):
-    mgr = EventManager(data={"request": data})
-    mgr.normalize()
-    evt = Event(data=mgr.get_data())
-    if evt.data.get('errors'):
-        raise InterfaceValidationError(evt.data.get('errors'))
+@pytest.fixture
+def make_http_snapshot(insta_snapshot):
+    def inner(data):
+        mgr = EventManager(data={"request": data})
+        mgr.normalize()
+        evt = Event(data=mgr.get_data())
 
-    return evt.interfaces.get('request') or Http.to_python({})
+        interface = evt.interfaces.get('request')
+
+        insta_snapshot({
+            'errors': evt.data.get('errors'),
+            'to_json': interface.to_json(),
+        })
+
+    return inner
 
 
-class HttpTest(TestCase):
-    @fixture
-    def interface(self):
-        return to_python(dict(
+def test_basic(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+    ))
+
+
+def test_full(make_http_snapshot):
+    make_http_snapshot(
+        dict(
+            method='GET',
             url='http://example.com',
-        ))
-
-    def test_path(self):
-        assert self.interface.get_path() == 'request'
-
-    def test_serialize_unserialize_behavior(self):
-        result = type(self.interface).to_python(self.interface.to_json())
-        assert result.to_json() == self.interface.to_json()
-
-    def test_basic(self):
-        result = self.interface
-        assert result.url == 'http://example.com/'
-        assert result.method is None
-        assert result.fragment == ''
-        assert result.query_string == []
-        assert result.data is None
-        assert result.cookies == []
-        assert result.headers == []
-        assert result.env == {}
-        assert result.full_url == result.url
-
-    def test_full(self):
-        result = to_python(
-            dict(
-                method='GET',
-                url='http://example.com',
-                query_string='foo=bar',
-                fragment='foobar',
-                headers={'x-foo-bar': 'baz'},
-                cookies={'foo': 'bar'},
-                env={'bing': 'bong'},
-                data='hello world',
-            )
+            query_string='foo=bar',
+            fragment='foobar',
+            headers={'x-foo-bar': 'baz'},
+            cookies={'foo': 'bar'},
+            env={'bing': 'bong'},
+            data='hello world',
         )
-        assert result.method == 'GET'
-        assert result.query_string == [('foo', 'bar')]
-        assert result.fragment == 'foobar'
-        assert result.cookies == [('foo', 'bar')]
-        assert result.headers == [('X-Foo-Bar', 'baz')]
-        assert result.env == {'bing': 'bong'}
-        assert result.data == 'hello world'
+    )
 
-    def test_query_string_as_dict(self):
-        result = to_python(dict(
+
+def test_query_string_as_dict(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        query_string={'foo': 'bar'},
+    ))
+
+
+def test_query_string_as_pairlist(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        query_string=[['foo', 'bar']],
+    ))
+
+
+def test_query_string_as_bytes(make_http_snapshot):
+    make_http_snapshot(
+        dict(
             url='http://example.com',
-            query_string={'foo': 'bar'},
-        ))
-        assert result.query_string == [('foo', 'bar')]
+            query_string=b'foo=\x00',
+        )
+    )
 
-    def test_query_string_as_pairlist(self):
-        result = to_python(dict(
+
+def test_data_as_dict(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        data={'foo': 'bar'},
+    ))
+
+
+def test_urlencoded_data(make_http_snapshot):
+    make_http_snapshot(
+        dict(
             url='http://example.com',
-            query_string=[['foo', 'bar']],
-        ))
-        assert result.query_string == [('foo', 'bar')]
-
-    def test_query_string_as_bytes(self):
-        result = to_python(
-            dict(
-                url='http://example.com',
-                query_string=b'foo=\x00',
-            )
+            headers={'Content-Type': 'application/x-www-form-urlencoded'},
+            data='foo=bar',
         )
-        assert result.query_string == [('foo', '\x00')]
+    )
 
-    def test_data_as_dict(self):
-        result = to_python(dict(
+
+def test_infer_urlencoded_content_type(make_http_snapshot):
+    make_http_snapshot(
+        dict(
             url='http://example.com',
-            data={'foo': 'bar'},
-        ))
-        assert result.data == {'foo': 'bar'}
-
-    def test_urlencoded_data(self):
-        result = to_python(
-            dict(
-                url='http://example.com',
-                headers={'Content-Type': 'application/x-www-form-urlencoded'},
-                data='foo=bar',
-            )
+            data='foo=bar',
         )
+    )
 
-        assert result.data == {'foo': 'bar'}
-        assert result.inferred_content_type == 'application/x-www-form-urlencoded'
 
-    def test_infer_urlencoded_content_type(self):
-        result = to_python(
-            dict(
-                url='http://example.com',
-                data='foo=bar',
-            )
-        )
-
-        assert result.data == {'foo': 'bar'}
-        assert result.inferred_content_type == 'application/x-www-form-urlencoded'
-
-    def test_json_data(self):
-        result = to_python(
-            dict(
-                url='http://example.com',
-                headers={'Content-Type': 'application/json'},
-                data='{"foo":"bar"}',
-            )
-        )
-
-        assert result.data == {'foo': 'bar'}
-        assert result.inferred_content_type == 'application/json'
-
-    def test_infer_json_content_type(self):
-        result = to_python(
-            dict(
-                url='http://example.com',
-                data='{"foo":"bar"}',
-            )
-        )
-
-        assert result.data == {'foo': 'bar'}
-        assert result.inferred_content_type == 'application/json'
-
-    def test_cookies_as_string(self):
-        result = to_python(dict(
+def test_json_data(make_http_snapshot):
+    make_http_snapshot(
+        dict(
             url='http://example.com',
-            cookies='a=b;c=d',
-        ))
-        assert result.cookies == [('a', 'b'), ('c', 'd')]
-        result = to_python(dict(
-            url='http://example.com',
-            cookies='a=b;c=d',
-        ))
-        assert result.cookies == [('a', 'b'), ('c', 'd')]
+            headers={'Content-Type': 'application/json'},
+            data='{"foo":"bar"}',
+        )
+    )
 
-    def test_cookies_in_header(self):
-        result = to_python(dict(
+
+def test_infer_json_content_type(make_http_snapshot):
+    make_http_snapshot(
+        dict(
+            url='http://example.com',
+            data='{"foo":"bar"}',
+        )
+    )
+
+
+def test_cookies_as_string(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        cookies='a=b;c=d',
+    ))
+    make_http_snapshot(dict(
+        url='http://example.com',
+        cookies='a=b;c=d',
+    ))
+
+
+def test_cookies_in_header(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        headers={'Cookie': 'a=b;c=d'},
+    ))
+
+
+def test_cookies_in_header2(make_http_snapshot):
+    make_http_snapshot(
+        dict(
             url='http://example.com',
             headers={'Cookie': 'a=b;c=d'},
-        ))
-        assert result.cookies == [('a', 'b'), ('c', 'd')]
-        result = to_python(
-            dict(
-                url='http://example.com',
-                headers={'Cookie': 'a=b;c=d'},
-                cookies={'foo': 'bar'},
-            )
+            cookies={'foo': 'bar'},
         )
-        assert result.cookies == [('foo', 'bar')]
+    )
 
-    def test_query_string_and_fragment_as_params(self):
-        result = to_python(
-            dict(
-                url='http://example.com',
-                query_string=u'foo\ufffd=bar\u2026',
-                fragment='fragment',
-            )
+
+def test_query_string_and_fragment_as_params(make_http_snapshot):
+    make_http_snapshot(
+        dict(
+            url='http://example.com',
+            query_string=u'foo\ufffd=bar\u2026',
+            fragment='fragment',
         )
-        assert result.url == 'http://example.com/'
-        assert result.full_url == 'http://example.com/?foo%EF%BF%BD=bar%E2%80%A6#fragment'
+    )
 
-    def test_query_string_and_fragment_in_url(self):
-        result = to_python(dict(
-            url=u'http://example.com?foo\ufffd=bar#fragment\u2026',
-        ))
-        assert result.url == 'http://example.com/'
-        assert result.full_url == 'http://example.com/?foo%EF%BF%BD=bar#fragment...'
 
-    def test_header_value_list(self):
-        result = to_python(dict(
-            url='http://example.com',
-            headers={'Foo': ['1', '2']},
-        ))
-        assert result.headers == [('Foo', '1,2')]
+def test_query_string_and_fragment_in_url(make_http_snapshot):
+    make_http_snapshot(dict(
+        url=u'http://example.com?foo\ufffd=bar#fragment\u2026',
+    ))
 
-    def test_header_value_str(self):
-        result = to_python(dict(url='http://example.com', headers={'Foo': 1}))
-        assert result.headers == [('Foo', '1')]
 
-    def test_method(self):
-        with self.assertRaises(InterfaceValidationError):
-            assert to_python(dict(
-                url='http://example.com',
-                method='1234',
-            ))
+def test_header_value_list(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        headers={'Foo': ['1', '2']},
+    ))
 
-        with self.assertRaises(InterfaceValidationError):
-            to_python(dict(
-                url='http://example.com',
-                method='A' * 33,
-            ))
 
-        with self.assertRaises(InterfaceValidationError):
-            to_python(dict(
-                url='http://example.com',
-                method='A',
-            ))
+def test_header_value_str(make_http_snapshot):
+    make_http_snapshot(dict(url='http://example.com', headers={'Foo': 1}))
 
-        result = to_python(dict(
-            url='http://example.com',
-            method='TEST',
-        ))
-        assert result.method == 'TEST'
 
-        result = to_python(dict(
-            url='http://example.com',
-            method='FOO-BAR',
-        ))
-        assert result.method == 'FOO-BAR'
+def test_invalid_method(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        method='1234',
+    ))
 
-        result = to_python(dict(
-            url='http://example.com',
-            method='FOO_BAR',
-        ))
-        assert result.method == 'FOO_BAR'
+
+def test_invalid_method2(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        method='A' * 33,
+    ))
+
+
+def test_invalid_method3(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        method='A',
+    ))
+
+
+def test_unknown_method(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        method='TEST',
+    ))
+
+
+def test_unknown_method2(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        method='FOO-BAR',
+    ))
+
+
+def test_unknown_method3(make_http_snapshot):
+    make_http_snapshot(dict(
+        url='http://example.com',
+        method='FOO_BAR',
+    ))
