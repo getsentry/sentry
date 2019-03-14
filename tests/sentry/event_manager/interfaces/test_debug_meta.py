8aaa78f7058643312c39d7a4873ce62107234633
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-03-14 10:09:11 +0100

    ref: Port test_debug_meta to snapshots

--- tests/sentry/event_manager/interfaces/test_debug_meta.py
+++ tests/sentry/event_manager/interfaces/test_debug_meta.py
@@ -2,184 +2,147 @@
 
 from __future__ import absolute_import
 
-from sentry.interfaces.base import InterfaceValidationError
-from sentry.interfaces.debug_meta import DebugMeta
-from sentry.testutils import TestCase
+import pytest
+
 from sentry.models import Event
 from sentry.event_manager import EventManager
 
 
-def to_python(data):
-    mgr = EventManager(data={"debug_meta": data})
-    mgr.normalize()
-    evt = Event(data=mgr.get_data())
-    if evt.data.get('errors'):
-        raise InterfaceValidationError(evt.data.get('errors'))
-    return evt.interfaces.get('debug_meta') or DebugMeta.to_python({})
+@pytest.fixture
+def make_debug_meta_snapshot(insta_snapshot):
+    def inner(data):
+        mgr = EventManager(data={"debug_meta": data})
+        mgr.normalize()
+        evt = Event(data=mgr.get_data())
+        interface = evt.interfaces.get('debug_meta')
+        insta_snapshot({
+            'errors': evt.data.get('errors'),
+            'to_json': interface and interface.to_json()
+        })
+
+    return inner
 
 
-class DebugMetaTest(TestCase):
-    def test_null_values(self):
-        assert to_python({}).to_json() == {}
-        assert to_python({"images": None}).to_json() == {}
+@pytest.mark.parametrize('input', [
+    {},
+    {"images": None},
+    # TODO(markus): Should eventually generate {"images": [None]}
+    {"images": [None]},
+])
+def test_null_values(make_debug_meta_snapshot, input):
+    make_debug_meta_snapshot(input)
 
-        # TODO(markus): Should eventually generate {"images": [None]}
-        assert to_python({"images": [None]}).to_json() == {}
 
-    def test_apple_behavior(self):
-        image_name = (
-            '/var/containers/Bundle/Application/'
-            'B33C37A8-F933-4B6B-9FFA-152282BFDF13/SentryTest.app/SentryTest'
-        )
-        interface = to_python(
-            {
-                "images": [
-                    {
-                        "type": "apple",
-                        "cpu_subtype": 0,
-                        "uuid": "C05B4DDD-69A7-3840-A649-32180D341587",
-                        "image_vmaddr": 4294967296,
-                        "image_addr": '0x100020000',
-                        "cpu_type": 16777228,
-                        "image_size": 32768,
-                        "name": image_name,
-                    }
-                ],
-                "sdk_info": {
-                    "sdk_name": "iOS",
-                    "version_major": 9,
-                    "version_minor": 3,
-                    "version_patchlevel": 0
-                }
-            }
-        )
-
-        assert len(interface.images) == 1
-        assert interface.images[0] == {
-            'type': 'apple',
-            'cpu_type': 16777228,
-            'cpu_subtype': 0,
-            'uuid': 'c05b4ddd-69a7-3840-a649-32180d341587',
-            'image_vmaddr': '0x100000000',
-            'image_addr': '0x100020000',
-            'image_size': 32768,
-            'name': image_name,
-        }
-        assert interface.sdk_info == {
-            'build': None,
-            'dsym_type': 'none',
-            'sdk_name': 'iOS',
-            'version_major': 9,
-            'version_minor': 3,
-            'version_patchlevel': 0,
-        }
-
-    def test_apple_behavior_with_arch(self):
-        image_name = (
-            '/var/containers/Bundle/Application/'
-            'B33C37A8-F933-4B6B-9FFA-152282BFDF13/SentryTest.app/SentryTest'
-        )
-        interface = to_python(
-            {
-                "images": [
-                    {
-                        "type": "apple",
-                        "arch": "x86_64",
-                        "cpu_subtype": 0,
-                        "uuid": "C05B4DDD-69A7-3840-A649-32180D341587",
-                        "image_vmaddr": 4294967296,
-                        "image_addr": '0x100020000',
-                        "cpu_type": 16777228,
-                        "image_size": 32768,
-                        "name": image_name,
-                    }
-                ],
-                "sdk_info": {
-                    "sdk_name": "iOS",
-                    "version_major": 9,
-                    "version_minor": 3,
-                    "version_patchlevel": 0
-                }
-            }
-        )
-
-        assert interface.images[0]['arch'] == 'x86_64'
-
-    def test_symbolic_behavior(self):
-        interface = to_python(
-            {
-                "images": [
-                    {
-                        "type": "symbolic",
-                        "id": "3249d99d-0c40-4931-8610-f4e4fb0b6936-1",
-                        "image_addr": 2752512,
-                        "image_size": 36864,
-                        "name": "C:\\projects\\breakpad-tools\\windows\\Release\\crash.exe",
-                    }
-                ],
-                "sdk_info": {
-                    "sdk_name": "Windows",
-                    "version_major": 10,
-                    "version_minor": 0,
-                    "version_patchlevel": 14393
-                }
-            }
-        )
-
-        assert len(interface.images) == 1
-        assert interface.images[0] == {
-            'type': 'symbolic',
-            'id': '3249d99d-0c40-4931-8610-f4e4fb0b6936-1',
-            'image_vmaddr': '0x0',
-            'image_addr': '0x2a0000',
-            'image_size': 36864,
-            'name': 'C:\\projects\\breakpad-tools\\windows\\Release\\crash.exe',
-        }
-
-        assert interface.sdk_info == {
-            'build': None,
-            'dsym_type': 'none',
-            'sdk_name': 'Windows',
-            'version_major': 10,
-            'version_minor': 0,
-            'version_patchlevel': 14393,
-        }
-
-    def test_symbolic_behavior_with_arch(self):
-        interface = to_python(
-            {
-                "images": [
-                    {
-                        "type": "symbolic",
-                        "arch": "x86",
-                        "id": "3249d99d-0c40-4931-8610-f4e4fb0b6936-1",
-                        "image_addr": 2752512,
-                        "image_size": 36864,
-                        "name": "C:\\projects\\breakpad-tools\\windows\\Release\\crash.exe",
-                    }
-                ],
-                "sdk_info": {
-                    "sdk_name": "Windows",
-                    "version_major": 10,
-                    "version_minor": 0,
-                    "version_patchlevel": 14393
-                }
-            }
-        )
-
-        assert interface.images[0]['arch'] == 'x86'
-
-    def test_proguard_behavior(self):
-        interface = to_python(
-            {
-                "images": [{
-                    "type": "proguard",
+def test_apple_behavior(make_debug_meta_snapshot):
+    image_name = (
+        '/var/containers/Bundle/Application/'
+        'B33C37A8-F933-4B6B-9FFA-152282BFDF13/SentryTest.app/SentryTest'
+    )
+    make_debug_meta_snapshot(
+        {
+            "images": [
+                {
+                    "type": "apple",
+                    "cpu_subtype": 0,
                     "uuid": "C05B4DDD-69A7-3840-A649-32180D341587",
-                }]
+                    "image_vmaddr": 4294967296,
+                    "image_addr": '0x100020000',
+                    "cpu_type": 16777228,
+                    "image_size": 32768,
+                    "name": image_name,
+                }
+            ],
+            "sdk_info": {
+                "sdk_name": "iOS",
+                "version_major": 9,
+                "version_minor": 3,
+                "version_patchlevel": 0
             }
-        )
+        }
+    )
 
-        assert len(interface.images) == 1
-        img = interface.images[0]
-        assert img['type'] == 'proguard'
-        assert img['uuid'] == 'c05b4ddd-69a7-3840-a649-32180d341587'
+
+def test_apple_behavior_with_arch(make_debug_meta_snapshot):
+    image_name = (
+        '/var/containers/Bundle/Application/'
+        'B33C37A8-F933-4B6B-9FFA-152282BFDF13/SentryTest.app/SentryTest'
+    )
+    make_debug_meta_snapshot(
+        {
+            "images": [
+                {
+                    "type": "apple",
+                    "arch": "x86_64",
+                    "cpu_subtype": 0,
+                    "uuid": "C05B4DDD-69A7-3840-A649-32180D341587",
+                    "image_vmaddr": 4294967296,
+                    "image_addr": '0x100020000',
+                    "cpu_type": 16777228,
+                    "image_size": 32768,
+                    "name": image_name,
+                }
+            ],
+            "sdk_info": {
+                "sdk_name": "iOS",
+                "version_major": 9,
+                "version_minor": 3,
+                "version_patchlevel": 0
+            }
+        }
+    )
+
+
+def test_symbolic_behavior(make_debug_meta_snapshot):
+    make_debug_meta_snapshot(
+        {
+            "images": [
+                {
+                    "type": "symbolic",
+                    "id": "3249d99d-0c40-4931-8610-f4e4fb0b6936-1",
+                    "image_addr": 2752512,
+                    "image_size": 36864,
+                    "name": "C:\\projects\\breakpad-tools\\windows\\Release\\crash.exe",
+                }
+            ],
+            "sdk_info": {
+                "sdk_name": "Windows",
+                "version_major": 10,
+                "version_minor": 0,
+                "version_patchlevel": 14393
+            }
+        }
+    )
+
+
+def test_symbolic_behavior_with_arch(make_debug_meta_snapshot):
+    make_debug_meta_snapshot(
+        {
+            "images": [
+                {
+                    "type": "symbolic",
+                    "arch": "x86",
+                    "id": "3249d99d-0c40-4931-8610-f4e4fb0b6936-1",
+                    "image_addr": 2752512,
+                    "image_size": 36864,
+                    "name": "C:\\projects\\breakpad-tools\\windows\\Release\\crash.exe",
+                }
+            ],
+            "sdk_info": {
+                "sdk_name": "Windows",
+                "version_major": 10,
+                "version_minor": 0,
+                "version_patchlevel": 14393
+            }
+        }
+    )
+
+
+def test_proguard_behavior(make_debug_meta_snapshot):
+    make_debug_meta_snapshot(
+        {
+            "images": [{
+                "type": "proguard",
+                "uuid": "C05B4DDD-69A7-3840-A649-32180D341587",
+            }]
+        }
+    )
