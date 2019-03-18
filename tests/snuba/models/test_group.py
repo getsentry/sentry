a04cdb7a9f52c0f1ceb292f70ab980880617d850
Lyn Nagara <lyn.nagara@gmail.com>
2019-03-18 16:20:11 -0700

    tests

--- tests/snuba/models/test_group.py
+++ tests/snuba/models/test_group.py
@@ -12,10 +12,10 @@ from sentry import tagstore, options
 from sentry.models import (
     Group, GroupRedirect, GroupSnooze, GroupStatus, Release, get_group_with_redirect
 )
-from sentry.testutils import SnubaTestCase
+from sentry.testutils import TestCase, SnubaTestCase
 
 
-class GroupTest(SnubaTestCase):
+class GroupTest(TestCase):
     def test_is_resolved(self):
         group = self.create_group(status=GroupStatus.RESOLVED)
         assert group.is_resolved()
@@ -87,48 +87,6 @@ class GroupTest(SnubaTestCase):
         assert group.get_latest_event().event_id == '3'
         assert group.get_oldest_event().event_id == '0'
 
-    def test_get_oldest_latest_for_environments(self):
-        options.set('snuba.events-queries.enabled', True)
-        project = self.create_project()
-
-        min_ago = (timezone.now() - timedelta(minutes=1)).isoformat()[:19]
-
-        self.store_event(
-            data={
-                'event_id': 'a' * 32,
-                'environment': 'production',
-                'timestamp': min_ago,
-                'fingerprint': ['group-1']
-            },
-            project_id=project.id
-        )
-        self.store_event(
-            data={
-                'event_id': 'b' * 32,
-                'environment': 'production',
-                'timestamp': min_ago,
-                'fingerprint': ['group-1']
-            },
-            project_id=project.id
-        )
-        self.store_event(
-            data={
-                'event_id': 'c' * 32,
-                'timestamp': min_ago,
-                'fingerprint': ['group-1']
-            },
-            project_id=project.id
-        )
-
-        group = Group.objects.first()
-
-        assert group.get_latest_event_for_environments().event_id == 'c' * 32
-        assert group.get_latest_event_for_environments(['staging']) is None
-        assert group.get_latest_event_for_environments(['production']).event_id == 'b' * 32
-        assert group.get_oldest_event_for_environments().event_id == 'a' * 32
-        assert group.get_oldest_event_for_environments(
-            ['staging', 'production']).event_id == 'a' * 32
-        assert group.get_oldest_event_for_environments(['staging']) is None
 
     def test_is_ignored_with_expired_snooze(self):
         group = self.create_group(
@@ -282,3 +240,48 @@ class GroupTest(SnubaTestCase):
         assert result == u'http://testserver/baz/{}/issues/{}/?environment=d%C3%A9v'.format(
             project.slug,
             group.id)
+
+
+class GroupTestSnuba(SnubaTestCase):
+    def test_get_oldest_latest_for_environments(self):
+        options.set('snuba.events-queries.enabled', True)
+        project = self.create_project()
+
+        min_ago = (timezone.now() - timedelta(minutes=1)).isoformat()[:19]
+
+        self.store_event(
+            data={
+                'event_id': 'a' * 32,
+                'environment': 'production',
+                'timestamp': min_ago,
+                'fingerprint': ['group-1']
+            },
+            project_id=project.id
+        )
+        self.store_event(
+            data={
+                'event_id': 'b' * 32,
+                'environment': 'production',
+                'timestamp': min_ago,
+                'fingerprint': ['group-1']
+            },
+            project_id=project.id
+        )
+        self.store_event(
+            data={
+                'event_id': 'c' * 32,
+                'timestamp': min_ago,
+                'fingerprint': ['group-1']
+            },
+            project_id=project.id
+        )
+
+        group = Group.objects.first()
+
+        assert group.get_latest_event_for_environments().event_id == 'c' * 32
+        assert group.get_latest_event_for_environments(['staging']) is None
+        assert group.get_latest_event_for_environments(['production']).event_id == 'b' * 32
+        assert group.get_oldest_event_for_environments().event_id == 'a' * 32
+        assert group.get_oldest_event_for_environments(
+            ['staging', 'production']).event_id == 'a' * 32
+        assert group.get_oldest_event_for_environments(['staging']) is None
