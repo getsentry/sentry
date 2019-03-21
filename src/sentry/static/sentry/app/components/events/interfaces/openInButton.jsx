6d7684fb5165426671436fc144851bcf9c34a54d
MeredithAnya <meredith.a.heller@gmail.com>
2019-03-21 08:31:28 -0700

    fixes JAVASCRIPT-5VV (#12481)
    * fixes JAVASCRIPT-5VV

--- src/sentry/static/sentry/app/components/events/interfaces/openInButton.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/openInButton.jsx
@@ -34,10 +34,10 @@ class OpenInButton extends React.Component {
   fetchIssueLinkComponents() {
     const {api, organization, group} = this.props;
     const hasOrganization = !!organization;
-    const features = new Set(organization.features);
-    const hasSentryApps = hasOrganization && features.has('sentry-apps');
+    const hasSentryApps =
+      hasOrganization && new Set(organization.features).has('sentry-apps');
 
-    if (hasSentryApps && group && group.project) {
+    if (hasSentryApps && group && group.project && group.project.id) {
       api
         .requestPromise(
           `/organizations/${organization.slug}/sentry-app-components/?filter=stacktrace-link&projectId=${group
