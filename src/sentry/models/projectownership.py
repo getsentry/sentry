10715560535551dee6099e949413618c7ededbbf
georgepsarakis <giwrgos.psarakis@gmail.com>
2019-03-28 17:39:23 +0200

    feat(ownership): Add auto-assignment flag in ProjectOwnership

--- src/sentry/models/projectownership.py
+++ src/sentry/models/projectownership.py
@@ -22,6 +22,7 @@ class ProjectOwnership(Model):
     raw = models.TextField(null=True)
     schema = JSONField(null=True)
     fallthrough = models.BooleanField(default=True)
+    auto_assignment = models.BooleanField(default=False)
     date_created = models.DateTimeField(default=timezone.now)
     last_updated = models.DateTimeField(default=timezone.now)
     is_active = models.BooleanField(default=True)
