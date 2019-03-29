ca1a17a363ca4fcae1787b706094cb291ed7e717
Billy Vong <billy@sentry.io>
2019-03-28 17:48:39 -0700

    add test

--- tests/js/spec/views/organizationDiscover/index.spec.jsx
+++ tests/js/spec/views/organizationDiscover/index.spec.jsx
@@ -1,13 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import {mount} from 'enzyme';
 import {browserHistory} from 'react-router';
+import PropTypes from 'prop-types';
+import React from 'react';
 
+import {mount} from 'enzyme';
+import {selectByValue} from 'app-test/helpers/select';
 import GlobalSelectionStore from 'app/stores/globalSelectionStore';
 import OrganizationDiscoverContainerWithStore, {
   OrganizationDiscoverContainer,
 } from 'app/views/organizationDiscover';
 import ProjectsStore from 'app/stores/projectsStore';
+import Table from 'app/views/organizationDiscover/result/table';
+
+jest.mock('app/views/organizationDiscover/result/table', () => jest.fn(() => null));
+
+const results = {
+  timing: {
+    duration_ms: 29,
+    timestamp: 1553819742,
+    marks_ms: {
+      execute: 11,
+      rate_limit: 2,
+      prepare_query: 11,
+      get_configs: 0,
+      dedupe_wait: 0,
+      cache_get: 0,
+      validate_schema: 3,
+    },
+  },
+  meta: [{type: 'string', name: 'project.name'}, {type: 'string', name: 'title'}],
+  data: [
+    {
+      'project.name': 'heart',
+      title: "Blocked 'script' from 'example.com'",
+    },
+    {
+      'project.name': 'heart',
+      title: 'Exception: This is a test exception sent from the Raven CLI.',
+    },
+  ],
+};
 
 describe('OrganizationDiscoverContainer', function() {
   beforeEach(function() {
@@ -135,6 +166,152 @@ describe('OrganizationDiscoverContainer', function() {
         pathname: '/organizations/org-slug/discover/saved/1/',
         query: {editing: 'true'},
       });
+    });
+  });
+
+  describe.only('updates results', function() {
+    let wrapper, request;
+    const organization = TestStubs.Organization({
+      projects: [TestStubs.Project()],
+      features: ['discover'],
+    });
+    const routerContext = TestStubs.routerContext([{organization}]);
+
+    beforeAll(async function() {
+      wrapper = mount(
+        <OrganizationDiscoverContainerWithStore
+          location={{query: {}, search: ''}}
+          params={{}}
+        />,
+        routerContext
+      );
+      await tick();
+      wrapper.update();
+    });
+
+    it('clears summarize fields', function() {
+      wrapper
+        .find('SelectControl[name="fields"] .Select-clear-zone')
+        .simulate('mouseDown', {button: 0});
+
+      expect(wrapper.find('SelectControl[name="fields"]').prop('value')).toEqual([]);
+    });
+
+    it('adds fields summarize fields', function() {
+      selectByValue(wrapper, 'project.name', {name: 'fields', control: true});
+      selectByValue(wrapper, 'title', {name: 'fields', control: true});
+
+      expect(wrapper.find('SelectControl[name="fields"]').prop('value')).toEqual([
+        'project.name',
+        'title',
+      ]);
+    });
+
+    it('adds count aggregation', function() {
+      wrapper.find('Aggregations AddText Link').simulate('click');
+      const AggregationSelect = wrapper.find('Aggregations SelectControl').first();
+
+      AggregationSelect.find('input[role="combobox"]').simulate('focus');
+      AggregationSelect.find('.Select-control').simulate('mouseDown', {button: 0});
+      wrapper
+        .find('Option')
+        .findWhere(el => el.prop('option') && el.prop('option').value === 'count')
+        .simulate('mouseDown');
+
+      expect(
+        wrapper
+          .find('Aggregations SelectControl')
+          .first()
+          .prop('value')
+      ).toEqual('count');
+    });
+
+    it('runs initial query', async function() {
+      request = MockApiClient.addMockResponse({
+        url: '/organizations/org-slug/discover/query/?per_page=1000&cursor=0:0:1',
+        method: 'POST',
+        body: results,
+      });
+      wrapper.find('NewQuery button[aria-label="Run"]').simulate('click');
+      await tick();
+      wrapper.update();
+      expect(request).toHaveBeenLastCalledWith(
+        expect.anything(),
+        expect.objectContaining({
+          data: expect.objectContaining({
+            aggregations: [['count()', null, 'count']],
+            fields: ['project.name', 'title'],
+          }),
+        })
+      );
+      expect(Table).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          data: expect.objectContaining({
+            data: [
+              {'project.name': 'heart', title: "Blocked 'script' from 'example.com'"},
+              {
+                'project.name': 'heart',
+                title: 'Exception: This is a test exception sent from the Raven CLI.',
+              },
+            ],
+          }),
+        }),
+        expect.anything()
+      );
+    });
+
+    it('changes summarize fields', function() {
+      wrapper
+        .find('SelectControl[name="fields"] .Select-clear-zone')
+        .simulate('mouseDown', {button: 0});
+
+      selectByValue(wrapper, 'project.name', {name: 'fields', control: true});
+      selectByValue(wrapper, 'message', {name: 'fields', control: true});
+
+      expect(wrapper.find('SelectControl[name="fields"]').prop('value')).toEqual([
+        'project.name',
+        'message',
+      ]);
+    });
+
+    it('runs 2nd query', async function() {
+      request = MockApiClient.addMockResponse({
+        url: '/organizations/org-slug/discover/query/?per_page=1000&cursor=0:0:1',
+        method: 'POST',
+        body: {
+          ...results,
+          data: results.data.map(obj => ({
+            'project.name': obj['project.name'],
+            message: obj.title,
+          })),
+        },
+      });
+      wrapper.find('NewQuery button[aria-label="Run"]').simulate('click');
+      await tick();
+      wrapper.update();
+      expect(request).toHaveBeenLastCalledWith(
+        expect.anything(),
+        expect.objectContaining({
+          data: expect.objectContaining({
+            aggregations: [['count()', null, 'count']],
+            fields: ['project.name', 'message'],
+          }),
+        })
+      );
+      expect(Table).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          data: expect.objectContaining({
+            data: [
+              {'project.name': 'heart', message: "Blocked 'script' from 'example.com'"},
+              {
+                'project.name': 'heart',
+                message: 'Exception: This is a test exception sent from the Raven CLI.',
+              },
+            ],
+          }),
+        }),
+        expect.anything()
+      );
     });
   });
 
