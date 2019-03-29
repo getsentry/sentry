e1c1e01c633a27f363dd5e902665293f850b61f1
Katie Byers <lobsterkatie@gmail.com>
2019-03-28 19:21:13 -0700

    chore(documentation): Add docstring to web crawler filter (#12581)
    Just so it's obvious that `True` means, "Yes, drop this," rather than, "Yes, this is a good one, so keep it."

--- src/sentry/filters/web_crawlers.py
+++ src/sentry/filters/web_crawlers.py
@@ -60,6 +60,8 @@ class WebCrawlersFilter(Filter):
             return ''
 
     def test(self, data):
+        """Return True if event with given user agent should be filtered out, False otherwise"""
+
         # TODO(dcramer): we could also look at UA parser and use the 'Spider'
         # device type
         user_agent = self.get_user_agent(data)
