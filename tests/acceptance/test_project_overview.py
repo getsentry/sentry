99554b4e01a5fe2d6a25bae32c359762030541f4
Mark Story <mark@sentry.io>
2019-04-02 16:23:44 -0400

    stub date closer to now
    Snuba does not share our acceptance test framework's mocked now. We need
    to use dates closer to the real time when interacting with snuba.

--- tests/acceptance/test_project_overview.py
+++ tests/acceptance/test_project_overview.py
@@ -1,13 +1,20 @@
 from __future__ import absolute_import
 
+import pytz
+
+from datetime import datetime
 from django.utils import timezone
+from mock import patch
 
-from sentry.testutils import AcceptanceTestCase
+from sentry.testutils import AcceptanceTestCase, SnubaTestMixin
+from sentry.testutils.skips import requires_snuba
 
 
-class ProjectOverviewTest(AcceptanceTestCase):
+@requires_snuba
+class ProjectOverviewTest(SnubaTestMixin, AcceptanceTestCase):
     def setUp(self):
         super(ProjectOverviewTest, self).setUp()
+        self.init_snuba()
         self.user = self.create_user('foo@example.com')
         self.org = self.create_organization(
             owner=self.user, name='Rowdy Tiger')
@@ -22,11 +29,18 @@ class ProjectOverviewTest(AcceptanceTestCase):
         self.path = u'/{}/{}/dashboard/'.format(
             self.org.slug, self.project.slug)
 
-    def test_with_issues(self):
-        self.project.update(first_event=timezone.now())
-        self.create_group(
-            project=self.project,
-            message='Foo bar',
+    @patch('django.utils.timezone.now')
+    def test_with_issues(self, mock_now):
+        mock_now.return_value = datetime.utcnow().replace(tzinfo=pytz.utc)
+
+        self.store_event(
+            data={
+                'message': 'Foo bar',
+                'level': 'error',
+                'timestamp': timezone.now().isoformat()[:19]
+            },
+            project_id=self.project.id,
+            assert_no_errors=False
         )
         self.browser.get(self.path)
         self.browser.wait_until('.chart-wrapper')
