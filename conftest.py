f0b3332d8576b1a513a2369b66efb44f3e05593f
Dan Fuller <dfuller@sentry.io>
2019-04-02 16:41:15 -0700

    ref: Refactor .travis.yml, add parallel builds
    Refactor .travis.yml to be DRYer. Add in parallel builds for the slowest builds. The slowest build
    after these changes is Django 1.8 (with migrations). Not actually sure why we run the migrations for
    1.8 when we don't run them for any of the other builds, potentially we can remove this test.

--- conftest.py
+++ conftest.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import
 
 import os
 import sys
+from hashlib import md5
+
+import pytest
 
 pytest_plugins = [
     'sentry.utils.pytest'
@@ -17,3 +20,10 @@ def pytest_configure(config):
     # XXX(dramer): Kombu throws a warning due to transaction.commit_manually
     # being used
     warnings.filterwarnings('error', '', Warning, r'^(?!(|kombu|raven|riak|sentry))')
+
+
+def pytest_collection_modifyitems(items):
+    for item in items:
+        total_groups = int(os.environ.get('TOTAL_TEST_GROUPS', 1))
+        group_num = int(md5(item.location[0]).hexdigest(), 16) % total_groups
+        item.add_marker(getattr(pytest.mark, 'group_%s' % group_num))
