9ee86801c51072f30c5e45ae5dc22581feb04af4
Mark Story <mark@sentry.io>
2019-04-03 14:09:31 -0400

    fix(plugins) Don't log errors when plugins fail with expected problems
    When plugin post processing fails with known exception types we
    shouldn't log errors. There is nothing we can do with these problems as
    they come from known & expected situations in plugin code.
    
    Fixes SENTRY-40X

--- src/sentry/utils/safe.py
+++ src/sentry/utils/safe.py
@@ -23,6 +23,7 @@ def safe_execute(func, *args, **kwargs):
     # TODO: we should make smart savepoints (only executing the savepoint server
     # side if we execute a query)
     _with_transaction = kwargs.pop('_with_transaction', True)
+    expected_errors = kwargs.pop('expected_errors', None)
     try:
         if _with_transaction:
             with transaction.atomic():
@@ -37,8 +38,11 @@ def safe_execute(func, *args, **kwargs):
 
         func_name = getattr(func, '__name__', six.text_type(func))
         cls_name = cls.__name__
-
         logger = logging.getLogger('sentry.safe.%s' % (cls_name.lower(), ))
+
+        if expected_errors and isinstance(e, expected_errors):
+            logger.info('%s.process_error_ignored', func_name, extra={'exception': e})
+            return
         logger.error('%s.process_error', func_name, exc_info=True, extra={'exception': e})
     else:
         return result
