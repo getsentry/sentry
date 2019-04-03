11ba5abe6b91f2395aaf769f3999fd4eecccac46
Matte Noble <me@mattenoble.com>
2019-04-03 14:25:53 -0700

    feat(app-platform): Delete External Issue UI (#12558)
    Implements the front-end functionality necessary to delete External
    Issues created through Sentry Apps.
    
    Also fixes a problem where the "Link" tab of the new new Issue modal
    wasn't submitting the correct data. It was relying on `initialData` to
    set static things like `action`, but that wasn't being udpated when the
    User navigated around the modal.
    
    Instead, `SentryAppExternalIssueForm` keeps track of it's own
    `FormModel` and updates fields on that directly. This was the only way I
    could figure out how to pass data that isn't to be rendered, to the
    form. Ideally I think this would be done via hidden input fields.

--- src/sentry/static/sentry/app/components/group/externalIssueForm.jsx
+++ src/sentry/static/sentry/app/components/group/externalIssueForm.jsx
@@ -4,14 +4,12 @@ import PropTypes from 'prop-types';
 import queryString from 'query-string';
 import {debounce} from 'lodash';
 
-import {addSuccessMessage, addErrorMessage} from 'app/actionCreators/indicator';
-import {addQueryParamsToExistingUrl} from 'app/utils/queryString';
+import {addSuccessMessage} from 'app/actionCreators/indicator';
 import AsyncComponent from 'app/components/asyncComponent';
 import FieldFromConfig from 'app/views/settings/components/forms/fieldFromConfig';
 import Form from 'app/views/settings/components/forms/form';
 import SentryTypes from 'app/sentryTypes';
 import {t} from 'app/locale';
-import ExternalIssueStore from 'app/stores/externalIssueStore';
 
 const MESSAGES_BY_ACTION = {
   link: t('Successfully linked issue.'),
@@ -183,104 +181,6 @@ class ExternalIssueForm extends AsyncComponent {
             {...this.getFieldProps(field)}
           />
         ))}
-      </Form>
-    );
-  }
-}
-
-export class SentryAppExternalIssueForm extends React.Component {
-  static propTypes = {
-    group: SentryTypes.Group.isRequired,
-    sentryAppInstallation: PropTypes.object,
-    config: PropTypes.object.isRequired,
-    action: PropTypes.oneOf(['link', 'create']),
-    onSubmitSuccess: PropTypes.func,
-  };
-
-  onSubmitSuccess = issue => {
-    ExternalIssueStore.add(issue);
-    this.props.onSubmitSuccess(issue);
-  };
-
-  onSubmitError = () => {
-    const appName = this.props.sentryAppInstallation.sentryApp.name;
-    addErrorMessage(t(`Unable to ${this.props.action} ${appName} issue.`));
-  };
-
-  getFieldDefault(field) {
-    const {group} = this.props;
-    if (field.type == 'textarea') {
-      field.maxRows = 10;
-      field.autosize = true;
-    }
-    switch (field.default) {
-      case 'issue.title':
-        return group.title;
-      case 'issue.description':
-        const queryParams = {referrer: this.props.sentryAppInstallation.sentryApp.name};
-        const url = addQueryParamsToExistingUrl(group.permalink, queryParams);
-        return `Sentry Issue: [${group.shortId}](${url})`;
-      default:
-        return '';
-    }
-  }
-
-  render() {
-    const {sentryAppInstallation} = this.props;
-    const config = this.props.config[this.props.action];
-    const requiredFields = config.required_fields || [];
-    const optionalFields = config.optional_fields || [];
-
-    if (!sentryAppInstallation) {
-      return '';
-    }
-
-    return (
-      <Form
-        apiEndpoint={`/sentry-app-installations/${sentryAppInstallation.uuid}/external-issues/`}
-        apiMethod="POST"
-        onSubmitSuccess={this.onSubmitSuccess}
-        onSubmitError={this.onSubmitError}
-        initialData={{
-          action: this.props.action,
-          groupId: this.props.group.id,
-          uri: config.uri,
-        }}
-      >
-        {requiredFields.map(field => {
-          field.choices = field.choices || [];
-          if (['text', 'textarea'].includes(field.type) && field.default) {
-            field.defaultValue = this.getFieldDefault(field);
-          }
-
-          return (
-            <FieldFromConfig
-              key={`${field.name}`}
-              field={field}
-              inline={false}
-              stacked
-              flexibleControlStateSize
-              required={true}
-            />
-          );
-        })}
-
-        {optionalFields.map(field => {
-          field.choices = field.choices || [];
-          if (['text', 'textarea'].includes(field.type) && field.default) {
-            field.defaultValue = this.getFieldDefault(field);
-          }
-
-          return (
-            <FieldFromConfig
-              key={`${field.name}`}
-              field={field}
-              inline={false}
-              stacked
-              flexibleControlStateSize
-            />
-          );
-        })}
       </Form>
     );
   }
