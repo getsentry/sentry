ded8e9c0c36cb2c5617fe341f995e270a81728c5
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-04-04 13:53:37 +0200

    fix: Fix TypeError when iterating over headers

--- src/sentry/filters/legacy_browsers.py
+++ src/sentry/filters/legacy_browsers.py
@@ -82,7 +82,7 @@ class LegacyBrowsersFilter(Filter):
 
     def get_user_agent(self, data):
         try:
-            for key, value in get_path(data, 'request', 'headers', filter=True):
+            for key, value in get_path(data, 'request', 'headers', filter=True) or ():
                 if key.lower() == 'user-agent':
                     return value
         except LookupError:
