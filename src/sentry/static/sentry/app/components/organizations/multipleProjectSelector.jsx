028caf6dcd552883cd309327ebc2f355f6ef5717
chrissy <christopherchristopherclark@gmail.com>
2019-04-05 12:03:34 -0700

    use translations

--- src/sentry/static/sentry/app/components/organizations/multipleProjectSelector.jsx
+++ src/sentry/static/sentry/app/components/organizations/multipleProjectSelector.jsx
@@ -164,7 +164,7 @@ export default class MultipleProjectSelector extends React.PureComponent {
                 size="xsmall"
                 priority="primary"
               >
-                Apply
+                {t('Apply')}
               </SubmitButton>
             </SubmitButtonContainer>
           )
