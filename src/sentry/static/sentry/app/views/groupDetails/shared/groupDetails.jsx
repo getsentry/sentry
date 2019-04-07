getsentry/sentry

commit 3707a0c71b844e6909414eb81f6604993c895c1d

ref(groups): Refactor project group details (#11422)
Split project group details files so the bulk of it can be reused for
organizations. Move group details files to new folder structure. This is
intended to make an organization level group details view easier to
implement in future.

src/sentry/static/sentry/app/views/groupDetails/shared/groupDetails.jsx
@@ -5,32 +5,21 @@ import Reflux from 'reflux';
 import {browserHistory} from 'react-router';
 import DocumentTitle from 'react-document-title';
 
-import {analytics} from 'app/utils/analytics';
 import ApiMixin from 'app/mixins/apiMixin';
-import GroupHeader from 'app/views/groupDetails/header';
 import GroupStore from 'app/stores/groupStore';
 import LoadingError from 'app/components/loadingError';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import SentryTypes from 'app/sentryTypes';
 import {t} from 'app/locale';
-import withEnvironment from 'app/utils/withEnvironment';
 
-let ERROR_TYPES = {
-  GROUP_NOT_FOUND: 'GROUP_NOT_FOUND',
-};
+import GroupHeader from '../shared/header';
+import {ERROR_TYPES} from '../shared/constants';
 
 const GroupDetails = createReactClass({
   displayName: 'GroupDetails',
 
   propTypes: {
-    setProjectNavSection: PropTypes.func,
-    memberList: PropTypes.array,
     environment: SentryTypes.Environment,
-  },
-
-  contextTypes: {
-    organization: SentryTypes.Organization,
-    project: SentryTypes.Project,
   },
 
   childContextTypes: {
@@ -39,12 +28,6 @@ const GroupDetails = createReactClass({
   },
 
   mixins: [ApiMixin, Reflux.listenTo(GroupStore, 'onGroupChange')],
-
-  getDefaultProps() {
-    return {
-      memberList: [],
-    };
-  },
 
   getInitialState() {
     return {
@@ -63,16 +46,7 @@ const GroupDetails = createReactClass({
   },
 
   componentWillMount() {
-    this.props.setProjectNavSection('stream');
     this.fetchData();
-  },
-
-  componentDidMount() {
-    analytics('issue_page.viewed', {
-      group_id: parseInt(this.props.params.groupId, 10),
-      org_id: parseInt(this.context.organization.id, 10),
-      project_id: parseInt(this.context.project.id, 10),
-    });
   },
 
   componentWillReceiveProps(nextProps) {
@@ -81,7 +55,7 @@ const GroupDetails = createReactClass({
     }
   },
 
-  componentDidUpdate(prevProps, prevState) {
+  componentDidUpdate(prevProps) {
     if (
       prevProps.params.groupId !== this.props.params.groupId ||
       prevProps.environment !== this.props.environment
@@ -212,14 +186,8 @@ const GroupDetails = createReactClass({
     return (
       <DocumentTitle title={this.getTitle()}>
         <div className={this.props.className}>
-          <GroupHeader
-            orgId={params.orgId}
-            projectId={params.projectId}
-            group={group}
-            memberList={this.props.memberList}
-          />
+          <GroupHeader orgId={params.orgId} projectId={group.project.id} group={group} />
           {React.cloneElement(this.props.children, {
-            memberList: this.props.memberList,
             group,
           })}
         </div>
@@ -228,4 +196,4 @@ const GroupDetails = createReactClass({
   },
 });
 
-export default withEnvironment(GroupDetails);
+export default GroupDetails;

src/sentry/static/sentry/app/routes.jsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 import App from 'app/views/app';
 import CreateProject from 'app/views/onboarding/createProject';
-import GroupDetails from 'app/views/groupDetails';
-import GroupEvents from 'app/views/groupEvents';
-import GroupEventDetails from 'app/views/groupEventDetails';
-import GroupMergedView from 'app/views/groupMerged/groupMergedView';
-import GroupSimilarView from 'app/views/groupSimilar/groupSimilarView';
-import GroupTagValues from 'app/views/groupTagValues';
-import GroupTags from 'app/views/groupTags';
-import GroupUserFeedback from 'app/views/groupUserFeedback';
+import ProjectGroupDetails from 'app/views/groupDetails/project/index';
+import ProjectGroupEvents from 'app/views/groupDetails/project/groupEvents';
+import ProjectGroupEventDetails from 'app/views/groupDetails/project/groupEventDetails';
+import ProjectGroupMergedView from 'app/views/groupDetails/project/groupMerged';
+import ProjectGroupSimilarView from 'app/views/groupDetails/project/groupSimilar';
+import ProjectGroupTagValues from 'app/views/groupDetails/project/groupTagValues';
+import ProjectGroupTags from 'app/views/groupDetails/project/groupTags';
+import ProjectGroupUserFeedback from 'app/views/groupDetails/project/groupUserFeedback';
 import HookStore from 'app/stores/hookStore';
 import LazyLoad from 'app/components/lazyLoad';
 import MyIssuesAssignedToMe from 'app/views/myIssues/assignedToMe';
@@ -1072,25 +1072,28 @@ function routes() {
           <Redirect from="group/:groupId/" to="issues/:groupId/" />
           <Route
             path="issues/:groupId/"
-            component={errorHandler(GroupDetails)}
+            component={errorHandler(ProjectGroupDetails)}
             ignoreScrollBehavior
           >
-            <IndexRoute component={errorHandler(GroupEventDetails)} />
+            <IndexRoute component={errorHandler(ProjectGroupEventDetails)} />
 
             <Route
               path="activity/"
               componentPromise={() =>
-                import(/* webpackChunkName: "GroupActivity" */ './views/groupActivity')}
+                import(/* webpackChunkName: "GroupActivity" */ './views/groupDetails/project/groupActivity')}
               component={errorHandler(LazyLoad)}
             />
 
-            <Route path="events/:eventId/" component={errorHandler(GroupEventDetails)} />
-            <Route path="events/" component={errorHandler(GroupEvents)} />
-            <Route path="tags/" component={errorHandler(GroupTags)} />
-            <Route path="tags/:tagKey/" component={errorHandler(GroupTagValues)} />
-            <Route path="feedback/" component={errorHandler(GroupUserFeedback)} />
-            <Route path="similar/" component={errorHandler(GroupSimilarView)} />
-            <Route path="merged/" component={errorHandler(GroupMergedView)} />
+            <Route
+              path="events/:eventId/"
+              component={errorHandler(ProjectGroupEventDetails)}
+            />
+            <Route path="events/" component={errorHandler(ProjectGroupEvents)} />
+            <Route path="tags/" component={errorHandler(ProjectGroupTags)} />
+            <Route path="tags/:tagKey/" component={errorHandler(ProjectGroupTagValues)} />
+            <Route path="feedback/" component={errorHandler(ProjectGroupUserFeedback)} />
+            <Route path="similar/" component={errorHandler(ProjectGroupSimilarView)} />
+            <Route path="merged/" component={errorHandler(ProjectGroupMergedView)} />
           </Route>
 
           <Route path="events/:eventId/" component={errorHandler(ProjectEventRedirect)} />

src/sentry/static/sentry/app/views/groupDetails/project/groupEventDetails.jsx
@@ -5,13 +5,14 @@ import {withMeta} from 'app/components/events/meta/metaProxy';
 import ApiMixin from 'app/mixins/apiMixin';
 import EventEntries from 'app/components/events/eventEntries';
 import GroupEventDetailsLoadingError from 'app/components/errors/groupEventDetailsLoadingError';
-import GroupEventToolbar from 'app/views/groupDetails/eventToolbar';
 import GroupSidebar from 'app/components/group/sidebar';
 import GroupState from 'app/mixins/groupState';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import MutedBox from 'app/components/mutedBox';
 import ResolutionBox from 'app/components/resolutionBox';
 import withEnvironmentInQueryString from 'app/utils/withEnvironmentInQueryString';
+
+import GroupEventToolbar from './eventToolbar';
 
 const GroupEventDetails = createReactClass({
   displayName: 'GroupEventDetails',

src/sentry/static/sentry/app/views/groupDetails/project/groupMerged/index.jsx
@@ -4,11 +4,12 @@ import Reflux from 'reflux';
 
 import {t} from 'app/locale';
 import ApiMixin from 'app/mixins/apiMixin';
-import MergedList from 'app/views/groupMerged/mergedList';
 import GroupingActions from 'app/actions/groupingActions';
 import GroupingStore from 'app/stores/groupingStore';
 import LoadingError from 'app/components/loadingError';
 import LoadingIndicator from 'app/components/loadingIndicator';
+
+import MergedList from './mergedList';
 
 const GroupMergedView = createReactClass({
   displayName: 'GroupMergedView',

commit e894cee090f156149e8661b8d9376ee9275b97e2

(fix) Fix issue_page.viewed analytics event
Seems to have broken in https://github.com/getsentry/sentry/pull/11422 with Sentry 10 changes.

src/sentry/static/sentry/app/views/groupDetails/shared/groupDetails.jsx
@@ -17,6 +17,7 @@ import LoadingError from 'app/components/loadingError';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import ProjectsStore from 'app/stores/projectsStore';
 import SentryTypes from 'app/sentryTypes';
+import {analytics} from 'app/utils/analytics';
 
 import {ERROR_TYPES} from '../shared/constants';
 import GroupHeader from '../shared/header';
@@ -32,6 +33,10 @@ const GroupDetails = createReactClass({
     environments: PropTypes.arrayOf(PropTypes.string),
     enableSnuba: PropTypes.bool,
     showGlobalHeader: PropTypes.bool,
+  },
+
+  contextTypes: {
+    organization: SentryTypes.Organization,
   },
 
   childContextTypes: {
@@ -135,6 +140,16 @@ const GroupDetails = createReactClass({
         });
 
         GroupStore.loadInitialData([data]);
+
+        const organization = this.props.organization || this.context.organization;
+        const payload = {
+          group_id: parseInt(data.id, 10),
+          org_id: parseInt(organization.id, 10),
+        };
+        if (project) {
+          payload.project_id = parseInt(project.id, 10);
+        }
+        analytics('issue_page.viewed', payload);
       },
       error: (_, _textStatus, errorThrown) => {
         let errorType = null;