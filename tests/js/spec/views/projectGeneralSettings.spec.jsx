f1df0b1a486e6de773fffe166cd3a7e1d3f4d46c
Armin Ronacher <armin.ronacher@active-4.com>
2019-04-08 16:16:41 +0200

    feat(grouping): Support for grouping enhancements and fingerprinting
    This PR adds support for grouping enhancements and server side
    fingerprinting. The former requires a newstyle grouping algorithm to be
    selected, the former works for all grouping strategies. This also adds
    UI to configure grouping enhancements and fingerprinting which is
    feature flagged currently.
    
    This UI is feature gated and likely never available to customers in the
    current form except for testing purposes. A subset might become
    available.

--- tests/js/spec/views/projectGeneralSettings.spec.jsx
+++ tests/js/spec/views/projectGeneralSettings.spec.jsx
@@ -14,6 +14,7 @@ describe('projectGeneralSettings', function() {
   const org = TestStubs.Organization();
   const project = TestStubs.ProjectDetails();
   const groupingConfigs = TestStubs.GroupingConfigs();
+  const groupingEnhancements = TestStubs.GroupingEnhancements();
   let routerContext;
   let putMock;
 
@@ -35,6 +36,11 @@ describe('projectGeneralSettings', function() {
       url: '/grouping-configs/',
       method: 'GET',
       body: groupingConfigs,
+    });
+    MockApiClient.addMockResponse({
+      url: '/grouping-enhancements/',
+      method: 'GET',
+      body: groupingEnhancements,
     });
     MockApiClient.addMockResponse({
       url: `/projects/${org.slug}/${project.slug}/`,
