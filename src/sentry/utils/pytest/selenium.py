5f3fd1e43d2f8ad082f66e7cb80a07e2b7a7a68d
Dan Fuller <dfuller@sentry.io>
2019-04-09 15:46:37 -0700

    refs: Split up acceptance tests again.
    We had a problem where failed builds couldn't be retried, since percy considered the build
    finalized. This changes the percy build to only finalize if the test run was successful. This change
    probably means we don't get screenshots on failed tests, not sure if that's something people care
    about?
    
    We also use build id + build run so that if someone re-runs all builds then they'll successfully get
    new snapshots.

--- src/sentry/utils/pytest/selenium.py
+++ src/sentry/utils/pytest/selenium.py
@@ -310,8 +310,15 @@ def percy(request):
     percy = percy.Runner(loader=loader, config=percy_config)
     percy.initialize_build()
 
-    request.addfinalizer(percy.finalize_build)
+    request.session._percy = percy
     return percy
+
+
+def pytest_sessionfinish(session, exitstatus):
+    # Always finalize if not running in parallel, otherwise only finalize if
+    # successful
+    if hasattr(session, "_percy") and ("PERCY_PARALLEL_TOTAL" not in os.environ or not exitstatus):
+        session._percy.finalize_build()
 
 
 @TimedRetryPolicy.wrap(timeout=15, exceptions=(WebDriverException,))
