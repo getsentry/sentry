0e140377280b312a12604b596b650b2e614769aa
MeredithAnya <meredith.a.heller@gmail.com>
2019-04-15 11:51:37 -0700

    ref(app-platform): New 'Open In' UI  (#12621)
    * new link interface
    
    * add SentryAppComponentsStore
    
    * use components store in external issue list

--- src/sentry/static/sentry/app/views/groupDetails/shared/groupEventDetails.jsx
+++ src/sentry/static/sentry/app/views/groupDetails/shared/groupEventDetails.jsx
@@ -14,6 +14,7 @@ import MutedBox from 'app/components/mutedBox';
 import withApi from 'app/utils/withApi';
 import withOrganization from 'app/utils/withOrganization';
 import fetchSentryAppInstallations from 'app/utils/fetchSentryAppInstallations';
+import {fetchSentryAppComponents} from 'app/actionCreators/sentryAppComponents';
 
 import GroupEventToolbar from './eventToolbar';
 import {fetchGroupEventAndMarkSeen, getEventEnvironment} from './utils';
@@ -83,6 +84,7 @@ class GroupEventDetails extends React.Component {
     const groupId = group.id;
     const orgSlug = organization.slug;
     const projSlug = project.slug;
+    const projectId = project.id;
 
     this.setState({
       loading: true,
@@ -112,6 +114,7 @@ class GroupEventDetails extends React.Component {
 
       if (features.has('sentry-apps')) {
         fetchSentryAppInstallations(api, orgSlug);
+        fetchSentryAppComponents(api, orgSlug, projectId);
       }
     }
   };
