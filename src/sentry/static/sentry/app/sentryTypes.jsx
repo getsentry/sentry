bd7de76d28a230542d242d1c81ce292af96dabb8
Lyn Nagara <lyn.nagara@gmail.com>
2019-04-24 12:25:18 -0700

    feat(incidents): Add SentryType for incidents
    Add Incident SentryType with the basic properties defined.

--- src/sentry/static/sentry/app/sentryTypes.jsx
+++ src/sentry/static/sentry/app/sentryTypes.jsx
@@ -395,6 +395,15 @@ export const SavedSearch = PropTypes.shape({
   type: PropTypes.oneOf([SEARCH_TYPES.ISSUE, SEARCH_TYPES.EVENTS]),
 });
 
+export const Incident = PropTypes.shape({
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  status: PropTypes.string.isRequired,
+  query: PropTypes.string,
+  projects: PropTypes.arrayOf(PropTypes.number),
+  dateCreated: PropTypes.string.isRequired,
+});
+
 export const GlobalSelection = PropTypes.shape({
   projects: PropTypes.arrayOf(PropTypes.number),
   environments: PropTypes.arrayOf(PropTypes.string),
@@ -926,6 +935,7 @@ const SentryTypes = {
   }),
   GlobalSelection,
   Group,
+  Incident,
   Tag,
   Monitor,
   PageLinks,
