5162f727488feb978007e080d8e5c34cc82d68c8
Mark Story <mark@sentry.io>
2019-04-25 10:29:30 -0400

    Remove unused code

--- src/sentry/api/endpoints/organization_repositories.py
+++ src/sentry/api/endpoints/organization_repositories.py
@@ -2,7 +2,6 @@ from __future__ import absolute_import
 
 from rest_framework.response import Response
 
-from sentry import features
 from sentry.api.base import DocSection
 from sentry.api.bases.organization import (
     OrganizationEndpoint,
@@ -30,12 +29,6 @@ class OrganizationRepositoriesEndpoint(OrganizationEndpoint):
         :pparam string organization_slug: the organization short name
         :auth: required
         """
-        if not self.has_feature(request, organization):
-            return self.respond({
-                'error_type': 'unavailable_feature',
-                'detail': ['You do not have that feature enabled']
-            }, status=403)
-
         queryset = Repository.objects.filter(
             organization_id=organization.id,
         )
@@ -87,13 +80,6 @@ class OrganizationRepositoriesEndpoint(OrganizationEndpoint):
     def post(self, request, organization):
         if not request.user.is_authenticated():
             return Response(status=401)
-
-        if not self.has_feature(request, organization):
-            return self.respond({
-                'error_type': 'unavailable_feature',
-                'detail': ['You do not have that feature enabled']
-            }, status=403)
-
         provider_id = request.DATA.get('provider')
 
         if provider_id is not None and provider_id.startswith('integrations:'):
