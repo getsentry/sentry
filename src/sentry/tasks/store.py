e4877f566ef25e592023184e651f67cd5c2a65f7
Alex Hofsteede <alex@hofsteede.com>
2019-04-29 14:25:03 -0700

    fix: don't incr empty list (#12984)
    fixes SENTRY-AKS, SENTRY-AKR

--- src/sentry/tasks/store.py
+++ src/sentry/tasks/store.py
@@ -453,7 +453,7 @@ def _do_save_event(cache_key=None, data=None, start_time=None, event_id=None,
     if project_id is None:
         project_id = data.pop('project')
 
-    key_id = data.get('key_id')
+    key_id = None if data is None else data.get('key_id')
     timestamp = to_datetime(start_time) if start_time is not None else None
 
     delete_raw_event(project_id, event_id, allow_hint_clear=True)
