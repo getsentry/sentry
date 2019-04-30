e307efb400ee655cd01315c02c7cfa6a1c376c41
Lyn Nagara <lyn.nagara@gmail.com>
2019-04-30 15:07:35 -0700

    feat(incidents): Implement basic incident details view (#12981)
    Implement basic incident details view skeleton

--- src/sentry/static/sentry/app/views/organizationIncidents/list/index.jsx
+++ src/sentry/static/sentry/app/views/organizationIncidents/list/index.jsx
@@ -1,18 +1,17 @@
 import React from 'react';
-import {withRouter} from 'react-router';
+import DocumentTitle from 'react-document-title';
 
 import {t} from 'app/locale';
-import AsyncView from 'app/views/asyncView';
+import AsyncComponent from 'app/components/asyncComponent';
 import {Panel, PanelBody, PanelItem} from 'app/components/panels';
 import Link from 'app/components/links/link';
 import EmptyStateWarning from 'app/components/emptyStateWarning';
 import Pagination from 'app/components/pagination';
+import {PageContent, PageHeader} from 'app/styles/organization';
+import PageHeading from 'app/components/pageHeading';
+import BetaTag from 'app/components/betaTag';
 
-class OrganizationIncidents extends AsyncView {
-  getTitle() {
-    return `Incidents - ${this.props.params.orgId}`;
-  }
-
+class OrganizationIncidentsBody extends AsyncComponent {
   getEndpoints() {
     const {params, location} = this.props;
     return [
@@ -63,4 +62,21 @@ class OrganizationIncidents extends AsyncView {
   }
 }
 
-export default withRouter(OrganizationIncidents);
+class OrganizationIncidents extends React.Component {
+  render() {
+    return (
+      <DocumentTitle title={`Incidents - ${this.props.params.orgId} - Sentry`}>
+        <PageContent>
+          <PageHeader>
+            <PageHeading withMargins>
+              {t('Incidents')} <BetaTag />
+            </PageHeading>
+          </PageHeader>
+          <OrganizationIncidentsBody {...this.props} />
+        </PageContent>
+      </DocumentTitle>
+    );
+  }
+}
+
+export default OrganizationIncidents;
