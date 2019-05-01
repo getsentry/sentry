0b1d29ecc6c0759db0d0f2043b84debff0dc0c66
Dan Fuller <dfuller@sentry.io>
2019-05-01 14:50:00 -0700

    feat(api): Add Incident manual creation api. Add model to support linking issues to Incidents. (SEN-552)
    This adds an endpoint for manually creating Incidents. Also adds in IncidentGroup to link groups to
    incidents. Finally we handle assigning the identifier automatically when creating an Incident. In
    this case, I'm assuming that in the majority of cases there won't be two incidents being created in
    an org at the same time, so just relying on the unique index to throw an error and retrying in
    rare cases where that happens.

--- src/sentry/api/endpoints/organization_incident_index.py
+++ src/sentry/api/endpoints/organization_incident_index.py
@@ -1,5 +1,9 @@
 from __future__ import absolute_import
 
+from rest_framework import serializers
+from rest_framework.exceptions import PermissionDenied
+from rest_framework.response import Response
+
 from sentry import features
 from sentry.api.bases.organization import (
     OrganizationEndpoint,
@@ -7,7 +11,14 @@ from sentry.api.bases.organization import (
 )
 from sentry.api.paginator import OffsetPaginator
 from sentry.api.serializers import serialize
-from sentry.incidents.models import Incident
+from sentry.api.serializers.rest_framework import ListField
+from sentry.incidents.logic import create_incident
+from sentry.incidents.models import (
+    Incident,
+    IncidentStatus,
+)
+from sentry.models.group import Group
+from sentry.models.project import Project
 
 
 class OrganizationIncidentPermission(OrganizationPermission):
@@ -15,6 +26,44 @@ class OrganizationIncidentPermission(OrganizationPermission):
         'GET': ['org:read', 'org:write', 'org:admin'],
         'POST': ['org:write', 'org:admin'],
     }
+
+
+class IncidentSerializer(serializers.Serializer):
+    projects = ListField(
+        child=serializers.CharField(),
+        required=False,
+        allow_null=True,
+        default=[],
+    )
+    groups = ListField(
+        child=serializers.CharField(),
+        required=True,
+        allow_null=False,
+        default=[],
+    )
+    title = serializers.CharField(required=True)
+    query = serializers.CharField(required=True)
+    dateStarted = serializers.DateTimeField(required=True)
+    dateDetected = serializers.DateTimeField(required=False)
+
+    def validate_projects(self, attrs, source):
+        slugs = attrs[source]
+        projects = Project.objects.filter(
+            organization=self.context['organization'],
+            slug__in=slugs,
+        )
+        if len(projects) != len(slugs):
+            raise serializers.ValidationError('Invalid project slug(s)')
+        attrs[source] = projects
+        return attrs
+
+    def validate_groups(self, attrs, source):
+        group_ids = attrs[source]
+        groups = Group.objects.filter(id__in=group_ids).select_related('project')
+        if len(groups) != len(group_ids):
+            raise serializers.ValidationError('Invalid group id(s)')
+        attrs[source] = groups
+        return attrs
 
 
 class OrganizationIncidentIndexEndpoint(OrganizationEndpoint):
@@ -43,3 +92,34 @@ class OrganizationIncidentIndexEndpoint(OrganizationEndpoint):
             paginator_cls=OffsetPaginator,
             on_results=lambda x: serialize(x, request.user),
         )
+
+    def post(self, request, organization):
+        if not features.has('organizations:incidents', organization, actor=request.user):
+            return self.respond(status=404)
+
+        serializer = IncidentSerializer(
+            data=request.DATA,
+            context={'organization': organization},
+        )
+
+        if serializer.is_valid():
+
+            result = serializer.object
+            groups = result['groups']
+            projects = result['projects']
+            all_projects = set(result['projects']) | set(g.project for g in result['groups'])
+            if any(p for p in all_projects if not request.access.has_project_access(p)):
+                raise PermissionDenied
+
+            incident = create_incident(
+                organization=organization,
+                status=IncidentStatus.CREATED,
+                title=result['title'],
+                query=result['query'],
+                date_started=result['dateStarted'],
+                date_detected=result.get('dateDetected', result['dateStarted']),
+                projects=projects,
+                groups=groups,
+            )
+            return Response(serialize(incident, request.user), status=201)
+        return Response(serializer.errors, status=400)
