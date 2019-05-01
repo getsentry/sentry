b1b12d77fe6169b0f653e603c92977682e3bae7b
adhiraj <693121+adhiraj@users.noreply.github.com>
2019-05-01 14:55:14 -0700

    Remove onboarding survey experiment logging (#13021)
    Removes experiment exposure logging for onboarding survey since we're rolling it out to everyone.

--- src/sentry/static/sentry/app/stores/hookStore.jsx
+++ src/sentry/static/sentry/app/stores/hookStore.jsx
@@ -14,7 +14,6 @@ const validHookNames = new Set([
   'amplitude:event',
   'analytics:event',
   'analytics:init-user',
-  'analytics:onboarding-survey-log',
 
   // Operational metrics
   'metrics:event',
