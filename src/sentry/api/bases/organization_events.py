8e2b0cc521a295bd3930b94da1470b16b7c2568f
Dan Fuller <dfuller@sentry.io>
2019-05-07 17:47:05 -0700

    feat(api): Support passing group ids to event search endpoint (SEN-584)
    When we want to view events related to an incident, we'll pass the following data from the incident
    to the organization events endpoint:
     - related project slugs
     - related group ids
     - query
     - start
     - end
    
    This way the user can filter the results however they want using the standard event search page.

--- src/sentry/api/bases/organization_events.py
+++ src/sentry/api/bases/organization_events.py
@@ -1,16 +1,30 @@
 from __future__ import absolute_import
 
+from rest_framework.exceptions import PermissionDenied
+
 from sentry import features
 from sentry.api.bases import OrganizationEndpoint, OrganizationEventsError
 from sentry.api.event_search import get_snuba_query_args, InvalidSearchQuery
+from sentry.models.project import Project
 
 
 class OrganizationEventsEndpointBase(OrganizationEndpoint):
 
     def get_snuba_query_args(self, request, organization):
         params = self.get_filter_params(request, organization)
-        query = request.GET.get('query')
 
+        group_ids = set(map(int, request.GET.getlist('group')))
+        if group_ids:
+            projects = Project.objects.filter(
+                organization=organization,
+                group__id__in=group_ids,
+            ).distinct()
+            if any(p for p in projects if not request.access.has_project_access(p)):
+                raise PermissionDenied
+            params['issue.id'] = list(group_ids)
+            params['project_id'] = list(set([p.id for p in projects] + params['project_id']))
+
+        query = request.GET.get('query')
         try:
             snuba_args = get_snuba_query_args(query=query, params=params)
         except InvalidSearchQuery as exc:
