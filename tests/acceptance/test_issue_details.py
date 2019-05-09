36dec8fe9c89bcbabf9d0350b5bd6a7abdd78cc3
Mark Story <mark@sentry.io>
2019-05-09 17:11:39 -0400

    Try and get tag meters to show up

--- tests/acceptance/test_issue_details.py
+++ tests/acceptance/test_issue_details.py
@@ -1,10 +1,12 @@
 from __future__ import absolute_import
 
 import json
+import pytz
 
 from datetime import datetime
 from django.conf import settings
 from django.utils import timezone
+from mock import patch
 
 from sentry.testutils import AcceptanceTestCase, SnubaTestCase
 from sentry.utils.samples import load_data
@@ -24,7 +26,7 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
         self.login_as(self.user)
         self.dismiss_assistant()
 
-    def create_sample_event(self, platform, default=None, sample_name=None):
+    def create_sample_event(self, platform, default=None, sample_name=None, time=None):
         event_data = load_data(platform, default=default, sample_name=sample_name)
         event_data['event_id'] = 'd964fdbd649a4cf8bfc35d18082b6b0e'
         event = self.store_event(
@@ -32,7 +34,9 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
             project_id=self.project.id,
             assert_no_errors=False,
         )
-        event.datetime = datetime(2017, 9, 6, 0, 0)
+        if time is None:
+            time = datetime(2017, 9, 6, 0, 0)
+        event.datetime = time
         event.save()
         event.group.update(
             first_seen=datetime(2015, 8, 13, 3, 8, 25, tzinfo=timezone.utc),
@@ -47,9 +51,13 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
         )
         self.wait_until_loaded()
 
-    def test_python_event(self):
+    @patch('django.utils.timezone.now')
+    def test_python_event(self, mock_now):
+        now = datetime.utcnow().replace(tzinfo=pytz.utc)
+        mock_now.return_value = now
         event = self.create_sample_event(
             platform='python',
+            time=now,
         )
         self.visit_issue(event.group.id)
         self.browser.snapshot('issue details python')
