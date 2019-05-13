789c840d58f8a7368e334fc93f16d400b7dc74d6
Meredith <meredith@getsentry.com>
2019-05-13 09:57:14 -0700

    associate token with install

--- src/sentry/mediators/token_exchange/grant_exchanger.py
+++ src/sentry/mediators/token_exchange/grant_exchanger.py
@@ -29,14 +29,7 @@ class GrantExchanger(Mediator):
         # Once it's exchanged it's no longer valid and should not be
         # exchangable, so we delete it.
         self._delete_grant()
-
-        self.token = ApiToken.objects.create(
-            user=self.user,
-            application=self.application,
-            scope_list=self.sentry_app.scope_list,
-            expires_at=token_expiration()
-        )
-
+        self._create_token()
         return self.token
 
     def record_analytics(self):
@@ -73,6 +66,16 @@ class GrantExchanger(Mediator):
 
     def _delete_grant(self):
         self.grant.delete()
+
+    def _create_token(self):
+        self.token = ApiToken.objects.create(
+            user=self.user,
+            application=self.application,
+            scope_list=self.sentry_app.scope_list,
+            expires_at=token_expiration()
+        )
+        self.install.api_token = self.token
+        self.install.save()
 
     @memoize
     def grant(self):
