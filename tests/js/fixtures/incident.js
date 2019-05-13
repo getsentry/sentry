3dd0d2fd2fd76e09108dca3f5e70fe4206d48fd8
Billy Vong <billy@sentry.io>
2019-05-13 14:57:33 -0700

    update text fixtures

--- tests/js/fixtures/incident.js
+++ tests/js/fixtures/incident.js
@@ -1,13 +1,12 @@
 export function Incident(params) {
   return {
-    id: '123',
-    identifier: '123',
+    id: '321',
+    identifier: 123,
     title: 'Too many Chrome errors',
     status: 0,
     projects: [],
-    suspects: [],
-    eventCount: 100,
-    usersAffected: 20,
+    totalEvents: 100,
+    uniqueUsers: 20,
     isSubscribed: true,
     ...params,
   };
