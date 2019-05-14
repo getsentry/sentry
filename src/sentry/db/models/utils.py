e85497d6256676ec1b89b34755e045cefee28552
Mark Story <mark@sentry.io>
2019-05-14 12:46:39 -0400

    Put back code getsentry is relying on
    getsentry is relying on importing these values. Put them back
    temporarily so that we don't end up in deployment/build hell.

--- src/sentry/db/models/utils.py
+++ src/sentry/db/models/utils.py
@@ -16,6 +16,8 @@ from django.utils.crypto import get_random_string
 from django.template.defaultfilters import slugify
 from uuid import uuid4
 
+# TODO(mark) remove once getsentry is no longer relying on this
+from django.db.models.expressions import Combinable as ExpressionNode  # noqa
 from sentry.db.exceptions import CannotResolveExpression
 
 
@@ -28,6 +30,45 @@ COMBINED_EXPRESSION_CALLBACKS = {
     CombinedExpression.BITAND: operator.and_,
     CombinedExpression.BITOR: operator.or_,
 }
+
+# TODO(mark) Remove this once getsentry no longer relies on it
+EXPRESSION_NODE_CALLBACKS = {
+    ExpressionNode.ADD: operator.add,
+    ExpressionNode.SUB: operator.sub,
+    ExpressionNode.MUL: operator.mul,
+    ExpressionNode.DIV: getattr(operator, 'floordiv', None) or operator.div,
+    ExpressionNode.MOD: operator.mod,
+    ExpressionNode.BITAND: operator.and_,
+    ExpressionNode.BITOR: operator.or_,
+}
+
+
+def resolve_expression_node(instance, node):
+    # TODO(mark) Remove this once getsentry no longer relies on it
+    def _resolve(instance, node):
+        if isinstance(node, Value):
+            return node.value
+        if isinstance(node, F):
+            return getattr(instance, node.name)
+        if isinstance(node, ExpressionNode):
+            return resolve_expression_node(instance, node)
+        return node
+
+    if isinstance(node, Value):
+        return node.value
+    if not hasattr(node, 'connector'):
+        raise CannotResolveExpression
+    op = EXPRESSION_NODE_CALLBACKS.get(node.connector, None)
+    if not op:
+        raise CannotResolveExpression
+    if hasattr(node, 'children'):
+        children = node.children
+    else:
+        children = [node.lhs, node.rhs]
+    runner = _resolve(instance, children[0])
+    for n in children[1:]:
+        runner = op(runner, _resolve(instance, n))
+    return runner
 
 
 def resolve_combined_expression(instance, node):
