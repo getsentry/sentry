31652ee04e3b6ed28bfeb21c292784631bd61eb7
Matt Robenolt <matt@ydekproductions.com>
2019-05-14 12:18:46 -0700

    fix(db): Fix behavior with ScoreClause since Django 1.8 (#13195)
    This was incorrectly assuming that Combinable replaced ExpressionNode,
    when CombinedExpression is more accurate. Combinable ends up being too
    generic of an "interface" that doesn't represent two nodes being
    combined, but any function.
    
    Include aliases so that getsentry doesn't break.
    
    Fixes SENTRY-AS2

--- src/bitfield/compat.py
+++ src/bitfield/compat.py
@@ -9,19 +9,3 @@ def bitand(a, b):
 
 def bitor(a, b):
     return a.bitor(b)
-
-
-try:
-    from django.db.models.expressions import ExpressionNode
-    ExpressionNode.BITAND  # noqa
-    del ExpressionNode
-except ImportError:
-    # Django >= 1.8
-    pass
-except AttributeError:
-    # Django < 1.5
-    def bitand(a, b):  # NOQA
-        return a & b
-
-    def bitor(a, b):  # NOQA
-        return a | b
