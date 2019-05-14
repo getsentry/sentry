643056e6132698be12c3cc420b1cbdb229908efe
Mark Story <mark@sentry.io>
2019-05-14 13:59:53 -0400

    feat(jira) Update Jira integration to comply with GDPR API changes (#13183)
    Update the Jira Integration to comply with the GDPR API changes that
    disallow using the `name` and `username` keys to search the Jira API,
    and remove `name` from the Jira API responses. Instead we need to use
    `accountId` which is an opaque userid when assigning or referencing
    users by id.
    
    The Jira-Server integration will continue using `name` as jira-server is
    not connected to atlassian's centralized authentication services.
    
    Fixes SEN-604
    
    * Remove print

--- src/sentry/integrations/jira/search.py
+++ src/sentry/integrations/jira/search.py
@@ -17,15 +17,20 @@ class JiraSearchEndpoint(IntegrationEndpoint):
             provider=self.provider,
         )
 
-    def _get_formatted_user(self, user):
-        display = '%s %s(%s)' % (
-            user.get('displayName', user['name']),
-            '- %s ' % user.get('emailAddress') if user.get('emailAddress') else '',
-            user['name'],
+    def _get_formatted_user(self, id_field, user):
+        # The name field can be blank in jira-cloud, and the id_field varies by
+        # jira-cloud and jira-server
+        name = user.get('name', '')
+        email = user.get('emailAddress')
+
+        display = '%s %s%s' % (
+            user.get('displayName', name),
+            '- %s ' % email if email else '',
+            '(%s)' % name if name else '',
         )
         return {
-            'value': user['name'],
-            'label': display,
+            'value': user[id_field],
+            'label': display.strip(),
         }
 
     def get(self, request, organization, integration_id):
@@ -56,7 +61,6 @@ class JiraSearchEndpoint(IntegrationEndpoint):
 
         if field in ('assignee', 'reporter'):
             jira_client = installation.get_client()
-            users = []
             try:
                 response = jira_client.search_users_for_project(
                     request.GET.get('project', ''),
@@ -65,9 +69,12 @@ class JiraSearchEndpoint(IntegrationEndpoint):
             except (ApiUnauthorized, ApiError):
                 return Response({'detail': 'Unable to fetch users from Jira'}, status=400)
 
-            for user in response:
-                if user.get('name'):
-                    users.append(self._get_formatted_user(user))
+            user_id_field = jira_client.user_id_field()
+            users = [
+                self._get_formatted_user(user_id_field, user)
+                for user in response
+                if user_id_field in user
+            ]
             return Response(users)
 
         # TODO(jess): handle other autocomplete urls
