e67c3d33d23c2ba597bb5da1b7bd2a921dcec5c7
Jan Michael Auer <jan.auer@sentry.io>
2019-05-16 13:02:46 +0200

    feat: Add config for attachments download permissions (#13221)
    Adds an organization option to configure who can download event attachments. 
    
    Attachments are potentially sensitive files containing significant PII or user data, such as native crash reports or log files. Some of our customers would like better control over who has access to those attachments. We decided to let users configure the membership role required to access attachment files.
    
    The new option allows to override the default setting of "Member" on an organization level. It is enforced in the download view but does not constrain listing attachments. Users with insufficient permissions will still see the attachment, but won't be able to download it.

--- src/sentry/api/endpoints/event_attachment_details.py
+++ src/sentry/api/endpoints/event_attachment_details.py
@@ -8,12 +8,48 @@ try:
 except ImportError:
     from django.http import StreamingHttpResponse
 
-from sentry import features, options
-from sentry.api.bases.project import ProjectEndpoint
-from sentry.models import Event, SnubaEvent, EventAttachment
+from sentry import features, options, roles
+from sentry.api.bases.project import ProjectEndpoint, ProjectPermission
+from sentry.api.serializers.models.organization import ATTACHMENTS_ROLE_DEFAULT
+from sentry.auth.superuser import is_active_superuser
+from sentry.auth.system import is_system_auth
+from sentry.models import Event, SnubaEvent, EventAttachment, OrganizationMember
+
+
+class EventAttachmentDetailsPermission(ProjectPermission):
+    def has_object_permission(self, request, view, project):
+        result = super(EventAttachmentDetailsPermission, self) \
+            .has_object_permission(request, view, project)
+
+        if not result:
+            return result
+
+        if is_system_auth(request.auth) or is_active_superuser(request):
+            return True
+
+        if not request.user.is_authenticated():
+            return False
+
+        organization = project.organization
+        required_role = organization.get_option('sentry:attachments_role') \
+            or ATTACHMENTS_ROLE_DEFAULT
+
+        try:
+            current_role = OrganizationMember.objects.filter(
+                organization=organization,
+                user=request.user,
+            ).values_list('role', flat=True).get()
+        except OrganizationMember.DoesNotExist:
+            return False
+
+        required_role = roles.get(required_role)
+        current_role = roles.get(current_role)
+        return current_role.priority >= required_role.priority
 
 
 class EventAttachmentDetailsEndpoint(ProjectEndpoint):
+    permission_classes = (EventAttachmentDetailsPermission, )
+
     def download(self, attachment):
         file = attachment.file
         fp = file.getfile()
