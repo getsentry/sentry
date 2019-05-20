cf56ff7cbb7192167f8d8f938a3c5aa2db3bbebe
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-20 16:58:05 -0700

    move v2 methods into main file

--- src/sentry/api/bases/__init__.py
+++ src/sentry/api/bases/__init__.py
@@ -4,7 +4,6 @@ from .group import *  # NOQA
 from .dashboard import *  # NOQA
 from .organization import *  # NOQA
 from .organization_events import *  # NOQA
-from .organization_events_v2 import * # NOQA
 from .organizationissues import *  # NOQA
 from .organizationmember import *  # NOQA
 from .project import *  # NOQA
--- src/sentry/api/endpoints/organization_events.py
+++ src/sentry/api/endpoints/organization_events.py
@@ -14,18 +14,17 @@ from sentry.models import SnubaEvent
 from sentry.utils.dates import parse_stats_period
 from sentry.utils.snuba import (
     raw_query,
+    transform_aliases_and_query,
     SnubaTSResult,
 )
 from sentry import features
-
-from sentry.api.endpoints.organization_events_v2 import OrganizationEventsV2Endpoint
 
 
 class OrganizationEventsEndpoint(OrganizationEventsEndpointBase):
 
     def get(self, request, organization):
         if features.has('organizations:events-v2', organization, actor=request.user):
-            return OrganizationEventsV2Endpoint().get(request, organization)
+            return self.get_v2(request, organization)
 
         # Check for a direct hit on event ID
         query = request.GET.get('query', '').strip()
@@ -70,6 +69,27 @@ class OrganizationEventsEndpoint(OrganizationEventsEndpointBase):
                 [SnubaEvent(row) for row in results], request.user, serializer),
             paginator=GenericOffsetPaginator(data_fn=data_fn)
         )
+
+    def get_v2(self, request, organization):
+        try:
+            snuba_args = self.get_snuba_query_args_v2(request, organization)
+        except OrganizationEventsError as exc:
+            return Response({'detail': exc.message}, status=400)
+        except NoProjects:
+            # return empty result if org doesn't have projects
+            # or user doesn't have access to projects in org
+            data_fn = lambda *args, **kwargs: []
+        else:
+            data_fn = partial(
+                lambda *args, **kwargs: transform_aliases_and_query(*args, **kwargs)['data'],
+                referrer='api.organization-events-v2',
+                **snuba_args
+            )
+
+            return self.paginate(
+                request=request,
+                paginator=GenericOffsetPaginator(data_fn=data_fn)
+            )
 
 
 class OrganizationEventsStatsEndpoint(OrganizationEventsEndpointBase):
