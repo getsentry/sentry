85a0623eb5a1689eb69abf9df7c5e5dc48dcf32c
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-21 10:23:23 -0700

    feat(events-v2): Add events V2 API (#13288)
    First pass at an events V2 API. This supports most of the existing
    parameters used in the current events API (i.e. - start, end, period,
    projects, environment, search query).
    
    It adds support for the following:
    "fields" - a custom list of columns to be returned,
    "groupby" - groups results according to the column names
    "orderby" - custom ordering of results
    
    This API is used instead of the existing events API if the organization has
    the "events-v2" flag activated.

--- src/sentry/api/endpoints/organization_events.py
+++ src/sentry/api/endpoints/organization_events.py
@@ -14,13 +14,18 @@ from sentry.models import SnubaEvent
 from sentry.utils.dates import parse_stats_period
 from sentry.utils.snuba import (
     raw_query,
+    transform_aliases_and_query,
     SnubaTSResult,
 )
+from sentry import features
 
 
 class OrganizationEventsEndpoint(OrganizationEventsEndpointBase):
 
     def get(self, request, organization):
+        if features.has('organizations:events-v2', organization, actor=request.user):
+            return self.get_v2(request, organization)
+
         # Check for a direct hit on event ID
         query = request.GET.get('query', '').strip()
 
@@ -64,6 +69,27 @@ class OrganizationEventsEndpoint(OrganizationEventsEndpointBase):
                 [SnubaEvent(row) for row in results], request.user, serializer),
             paginator=GenericOffsetPaginator(data_fn=data_fn)
         )
+
+    def get_v2(self, request, organization):
+        try:
+            snuba_args = self.get_snuba_query_args_v2(request, organization)
+        except OrganizationEventsError as exc:
+            return Response({'detail': exc.message}, status=400)
+        except NoProjects:
+            # return empty result if org doesn't have projects
+            # or user doesn't have access to projects in org
+            data_fn = lambda *args, **kwargs: []
+        else:
+            data_fn = partial(
+                lambda *args, **kwargs: transform_aliases_and_query(*args, **kwargs)['data'],
+                referrer='api.organization-events-v2',
+                **snuba_args
+            )
+
+            return self.paginate(
+                request=request,
+                paginator=GenericOffsetPaginator(data_fn=data_fn)
+            )
 
 
 class OrganizationEventsStatsEndpoint(OrganizationEventsEndpointBase):
