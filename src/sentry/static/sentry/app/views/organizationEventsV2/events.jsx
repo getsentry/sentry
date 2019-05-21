2db76b455e8de09ea4498f591f0df0a00354976b
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-21 11:14:29 -0700

    feat(events-v2): Add events components to UI
    Adds the search bar, tag distribution and table component to the events
    page. These are mostly unconnected components right now.

--- src/sentry/static/sentry/app/views/organizationEventsV2/events.jsx
+++ src/sentry/static/sentry/app/views/organizationEventsV2/events.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
+import styled from 'react-emotion';
 import SentryTypes from 'app/sentryTypes';
+import space from 'app/styles/space';
+import SearchBar from 'app/views/organizationEvents/searchBar';
+
+import Table from './table';
+import Tags from './tags';
 
 export default class Events extends React.Component {
   static propTypes = {
+    organization: SentryTypes.Organization.isRequired,
     view: SentryTypes.EventView.isRequired,
   };
   render() {
-    return this.props.view.name;
+    const {organization, view} = this.props;
+    return (
+      <Container>
+        <div>
+          <StyledSearchBar organization={organization} />
+          <Table view={view} organization={organization} />
+        </div>
+        <Tags view={view} />
+      </Container>
+    );
   }
 }
+
+const Container = styled('div')`
+  display: grid;
+  grid-template-columns: auto 300px;
+  grid-gap: ${space(2)};
+`;
+
+const StyledSearchBar = styled(SearchBar)`
+  margin-bottom: ${space(2)};
+`;
