ab86c1f04a7b8410ddad95d3a01b4ae32b0279ce
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-21 13:13:38 -0700

    feat(events-v2): Add function to transform events query for API
    Converts the columns specified by a view into a list of fields to be
    requested.

--- src/sentry/static/sentry/app/views/organizationEventsV2/utils.jsx
+++ src/sentry/static/sentry/app/views/organizationEventsV2/utils.jsx
@@ -1,6 +1,4 @@
-export function fetchOrganizationEvents(api, orgSlug, data) {
-  return api.requestPromise(`/organizations/${orgSlug}/events/`, data);
-}
+import {cloneDeep} from 'lodash';
 
 export const ALL_VIEWS = [
   {
@@ -55,6 +53,17 @@ export const ALL_VIEWS = [
 ];
 
 /**
+ * "Special fields" do not map 1:1 to an single column in the event database,
+ * they are a UI concept that combines the results of multiple fields and
+ * displays with some custom formatting. This map lists the underlying data
+ * that we need to fetch in order to populate each of these special field.
+ */
+const SPECIAL_FIELDS = {
+  event: ['id', 'title'],
+  user: ['user.email', 'user.ip'],
+};
+
+/**
  * Given a view id, return the corresponding view object
  *
  * @param {String} requestedView
@@ -63,4 +72,38 @@ export const ALL_VIEWS = [
  */
 export function getCurrentView(requestedView) {
   return ALL_VIEWS.find(view => view.id === requestedView) || ALL_VIEWS[0];
+}
+
+/**
+ * Fetch organization events given view object
+ *
+ * @param {Object} api
+ * @param {String} orgSlug
+ * @param {Object} data
+ * @returns {Promise<Object>}
+ */
+export function fetchOrganizationEvents(api, orgSlug, view) {
+  const query = getQuery(view);
+  return api.requestPromise(`/organizations/${orgSlug}/events/`, {
+    query,
+  });
+}
+
+/**
+ * Takes a view and converts it into the format required for the events API
+ *
+ * @param {Object} view
+ * @returns {Object}
+ */
+export function getQuery(view) {
+  const data = cloneDeep(view.data);
+  data.fields = data.fields.reduce((fields, field) => {
+    if (SPECIAL_FIELDS.hasOwnProperty(field)) {
+      fields.push(...SPECIAL_FIELDS[field]);
+    } else {
+      fields.push(field);
+    }
+    return fields;
+  }, []);
+  return data;
 }
