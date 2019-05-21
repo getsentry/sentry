96fe54321dc96ebec11659b14d11cf374a06cab4
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-21 10:22:42 -0700

    feat(events-v2): Add data for pre-defined events views (#13254)
    Add data model to specify the 3 pre-defined events views. Add tabs UI
    allowing toggling between the the 3 views.

--- src/sentry/static/sentry/app/views/organizationEventsV2/index.jsx
+++ src/sentry/static/sentry/app/views/organizationEventsV2/index.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
+import PropTypes from 'prop-types';
 
 import {t} from 'app/locale';
 import SentryTypes from 'app/sentryTypes';
@@ -7,14 +8,42 @@ import GlobalSelectionHeader from 'app/components/organizations/globalSelectionH
 import {PageContent, PageHeader} from 'app/styles/organization';
 import PageHeading from 'app/components/pageHeading';
 import BetaTag from 'app/components/betaTag';
+import NavTabs from 'app/components/navTabs';
+import ListLink from 'app/components/links/listLink';
+
+import Events from './events';
+import {ALL_VIEWS, getCurrentView} from './utils';
 
 export default class OrganizationEventsV2 extends React.Component {
   static propTypes = {
-    organization: SentryTypes.Organization,
+    organization: SentryTypes.Organization.isRequired,
+    location: PropTypes.object,
   };
 
-  render() {
+  renderTabs() {
     const {organization} = this.props;
+    const currentView = getCurrentView(this.props.location.query.view);
+
+    return (
+      <NavTabs underlined={true}>
+        {ALL_VIEWS.map(view => (
+          <ListLink
+            key={view.id}
+            to={{
+              pathname: `/organizations/${organization.slug}/events/`,
+              query: {...this.props.location.query, view: view.id},
+            }}
+            isActive={() => view.id === currentView.id}
+          >
+            {view.name}
+          </ListLink>
+        ))}
+      </NavTabs>
+    );
+  }
+
+  render() {
+    const {organization, location} = this.props;
 
     return (
       <DocumentTitle title={`Events - ${organization.slug} - Sentry`}>
@@ -26,6 +55,8 @@ export default class OrganizationEventsV2 extends React.Component {
                 {t('Events')} <BetaTag />
               </PageHeading>
             </PageHeader>
+            {this.renderTabs()}
+            <Events view={getCurrentView(location.query.view)} />
           </PageContent>
         </React.Fragment>
       </DocumentTitle>
