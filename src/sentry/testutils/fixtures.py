e4430bb1429c2588876893ca406369847eb31fc4
Matte Noble <me@mattenoble.com>
2019-05-21 13:03:49 -0700

    feat(app-platform): Internal Integrations Backend (#13234)
    * feat(app-platform): Internal Integrations Backend
    
    Adds the logic and API work to enable creating Internal Integrations.
    This consists of the following:
    
    - Creating a SentryApp with the appropriate "internal" status
    - Automatically creating an ApiToken
    - Automatically installing the integration to the Org
    - Omit installation webhook
    
    * fix(jira): Remove newlines from RSA key
    
    For some reason, tests started failing due to our crypto library
    thinking this key was an invalid format for RSA. The problem is just
    that there is a newline in the beginning, throwing off the lib's
    matching of `-----` in the beginning.
    
    Removing the newline fixes the issue. I'm still unsure what changed to
    have caused this. Possibly a change to the Cryptography dependency, but
    it seem like we're inheriting it from another lib, so I can't tell.

--- src/sentry/testutils/fixtures.py
+++ src/sentry/testutils/fixtures.py
@@ -188,6 +188,9 @@ class Fixtures(object):
     def create_sentry_app(self, *args, **kwargs):
         return Factories.create_sentry_app(*args, **kwargs)
 
+    def create_internal_integration(self, *args, **kwargs):
+        return Factories.create_internal_integration(*args, **kwargs)
+
     def create_sentry_app_installation(self, *args, **kwargs):
         return Factories.create_sentry_app_installation(*args, **kwargs)
 
