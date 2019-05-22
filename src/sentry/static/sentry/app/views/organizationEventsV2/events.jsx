85714c3c2d3a8ace840f4d524b68892291d9a705
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-22 14:55:22 -0700

    feat(events-v2): Move fetching event data to main events component (#13324)

--- src/sentry/static/sentry/app/views/organizationEventsV2/events.jsx
+++ src/sentry/static/sentry/app/views/organizationEventsV2/events.jsx
@@ -3,22 +3,40 @@ import styled from 'react-emotion';
 import SentryTypes from 'app/sentryTypes';
 import space from 'app/styles/space';
 import SearchBar from 'app/views/organizationEvents/searchBar';
+import AsyncComponent from 'app/components/asyncComponent';
 
 import Table from './table';
 import Tags from './tags';
+import {getQuery} from './utils';
 
-export default class Events extends React.Component {
+export default class Events extends AsyncComponent {
   static propTypes = {
     organization: SentryTypes.Organization.isRequired,
     view: SentryTypes.EventView.isRequired,
   };
-  render() {
+
+  getEndpoints() {
     const {organization, view} = this.props;
+    return [
+      [
+        'events',
+        `/organizations/${organization.slug}/events/`,
+        {
+          query: getQuery(view),
+        },
+      ],
+    ];
+  }
+
+  renderBody() {
+    const {organization, view} = this.props;
+    const {events} = this.state;
+
     return (
       <Container>
         <div>
           <StyledSearchBar organization={organization} />
-          <Table view={view} organization={organization} />
+          <Table view={view} organization={organization} data={events} />
         </div>
         <Tags view={view} />
       </Container>
