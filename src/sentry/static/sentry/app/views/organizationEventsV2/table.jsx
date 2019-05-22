de5b345ccdd4c19dbd679947360b91a679193b84
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-22 14:55:22 -0700

    feat(events-v2): Move fetching event data to main events component (#13324)

--- src/sentry/static/sentry/app/views/organizationEventsV2/table.jsx
+++ src/sentry/static/sentry/app/views/organizationEventsV2/table.jsx
@@ -2,58 +2,31 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styled, {css} from 'react-emotion';
 
-import withApi from 'app/utils/withApi';
 import SentryTypes from 'app/sentryTypes';
 import {Panel, PanelHeader, PanelBody, PanelItem} from 'app/components/panels';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import overflowEllipsis from 'app/styles/overflowEllipsis';
 
-import {fetchOrganizationEvents} from './utils';
-
-class Table extends React.Component {
+export default class Table extends React.Component {
   static propTypes = {
-    api: PropTypes.object.isRequired,
-    organization: SentryTypes.Organization.isRequired,
     view: SentryTypes.EventView.isRequired,
-  };
-
-  constructor(props) {
-    super(props);
-    this.state = {events: [], isLoading: true, hasError: false};
-  }
-
-  componentDidMount() {
-    this.fetchData();
-  }
-
-  fetchData = async () => {
-    const {api, organization, view} = this.props;
-    this.setState({isLoading: true, hasError: false});
-
-    try {
-      const events = await fetchOrganizationEvents(api, organization.slug, view);
-      this.setState({
-        events,
-        isLoading: false,
-      });
-    } catch (e) {
-      this.setState({isLoading: false, hasError: true});
-    }
+    data: PropTypes.arrayOf(PropTypes.object),
+    isLoading: PropTypes.bool,
   };
 
   renderBody() {
-    const {events, isLoading} = this.state;
-    const {fields} = this.props.view.data;
+    const {view, data, isLoading} = this.props;
+    const {fields} = view.data;
 
     if (isLoading) {
       return <LoadingIndicator />;
     }
 
-    return events.map((event, idx) => (
+    return data.map((row, idx) => (
       <Row key={idx} className={getGridStyle(fields.length)}>
         {fields.map(field => (
           <Cell key={field}>
-            <Data>{event[field]}</Data>
+            <Data>{row[field]}</Data>
           </Cell>
         ))}
       </Row>
@@ -75,8 +48,6 @@ class Table extends React.Component {
     );
   }
 }
-
-export default withApi(Table);
 
 function getGridStyle(colCount) {
   return css`
