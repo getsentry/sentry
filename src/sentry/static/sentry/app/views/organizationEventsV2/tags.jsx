fc666b05a07c7ae00a44efbbdde2f8b91df94204
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-22 10:02:35 -0700

    feat(events-v2): Add events components to UI (#13321)
    Adds the search bar, tag distribution and table component to the events
    page. These are mostly unconnected components right now.

---
+++ src/sentry/static/sentry/app/views/organizationEventsV2/tags.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import SentryTypes from 'app/sentryTypes';
+import TagDistributionMeter from 'app/components/tagDistributionMeter';
+
+export default class Tags extends React.Component {
+  static propTypes = {
+    view: SentryTypes.EventView.isRequired,
+  };
+
+  renderTag(tag) {
+    return (
+      <TagDistributionMeter key={tag} title={tag} segments={[]}>
+        {tag}
+      </TagDistributionMeter>
+    );
+  }
+
+  render() {
+    return <div>{this.props.view.tags.map(tag => this.renderTag(tag))}</div>;
+  }
+}
