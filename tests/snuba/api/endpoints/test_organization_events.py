14f68f488343e0ed4f7be5b38479ee08e6ace8c8
Lauryn Brown <lauryndbrown@gmail.com>
2019-05-23 10:13:20 -0700

    ref(event-search): Fixed unhandled parse error. (#13320)
    * Fixed unhandled parse error.:
    
    * shortended error message

--- tests/snuba/api/endpoints/test_organization_events.py
+++ tests/snuba/api/endpoints/test_organization_events.py
@@ -155,7 +155,7 @@ class OrganizationEventsEndpointTest(OrganizationEventsTestBase):
         response = self.client.get(url, {'query': 'hi \n there'}, format='json')
 
         assert response.status_code == 400, response.content
-        assert response.data['detail'] == "Parse error: 'search' (column 4)"
+        assert response.data['detail'] == "Parse error: 'search' (column 4). This is commonly caused by unmatched-parentheses."
 
     def test_project_filtering(self):
         user = self.create_user(is_staff=False, is_superuser=False)
