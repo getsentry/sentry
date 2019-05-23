7a5ccf937636726155910c7dfc60b9bf2e265d78
Lyn Nagara <lyn.nagara@gmail.com>
2019-05-23 09:29:18 -0700

    test(events-v2): Fix events not showing up in Percy

--- tests/acceptance/test_organization_events_v2.py
+++ tests/acceptance/test_organization_events_v2.py
@@ -1,8 +1,11 @@
 from __future__ import absolute_import
 
-from datetime import timedelta
+from datetime import datetime, timedelta
 from django.utils import timezone
+import pytz
+from mock import patch
 from sentry.testutils import AcceptanceTestCase, SnubaTestCase
+
 
 FEATURE_NAME = 'organizations:events-v2'
 
@@ -34,7 +37,9 @@ class OrganizationEventsTest(AcceptanceTestCase, SnubaTestCase):
             self.browser.wait_until_not('.loading-indicator')
             self.browser.snapshot('events-v2 - all events empty state')
 
-    def test_all_events(self):
+    @patch('django.utils.timezone.now')
+    def test_all_events(self, mock_now):
+        mock_now.return_value = datetime.utcnow().replace(tzinfo=pytz.utc)
         min_ago = (timezone.now() - timedelta(minutes=1)).isoformat()[:19]
         self.store_event(
             data={
