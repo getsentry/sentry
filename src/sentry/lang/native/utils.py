2de3b3c0f32a1a4df762403de26e85fef9e7e67c
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-05-24 17:26:11 +0200

    ref: Move symbolicator payload processing into enhancer

--- src/sentry/lang/native/utils.py
+++ src/sentry/lang/native/utils.py
@@ -10,6 +10,7 @@ from symbolic import parse_addr
 
 from sentry.interfaces.contexts import DeviceContextType
 from sentry.stacktraces.functions import trim_function_name
+from sentry.stacktraces.processing import find_stacktraces_in_data
 from sentry.utils.safe import get_path, trim
 
 logger = logging.getLogger(__name__)
@@ -20,7 +21,47 @@ VERSION_RE = re.compile(r'(\d+\.\d+\.\d+)\s+(.*)')
 # Regex to guess whether we're dealing with Windows or Unix paths.
 WINDOWS_PATH_RE = re.compile(r'^([a-z]:\\|\\\\)', re.IGNORECASE)
 
+# Event platforms that could contain native stacktraces
+NATIVE_PLATFORMS = ('cocoa', 'native')
+
+# Debug image types that can be handled by the symbolicator
+NATIVE_IMAGE_TYPES = (
+    'apple',     # Deprecated in favor of "macho"
+    'symbolic',  # Generic if type is not known
+    'elf',       # Linux
+    'macho',     # macOS, iOS
+    'pe'         # Windows
+)
+
 AppInfo = namedtuple('AppInfo', ['id', 'version', 'build', 'name'])
+
+
+def is_native_platform(platform):
+    return platform in NATIVE_PLATFORMS
+
+
+def is_native_image(image):
+    return bool(image) \
+        and image.get('type') in NATIVE_IMAGE_TYPES \
+        and image.get('image_addr') is not None \
+        and image.get('image_size') is not None \
+        and (image.get('debug_id') or image.get('id') or image.get('uuid')) is not None
+
+
+def native_images_from_data(data):
+    return get_path(data, 'debug_meta', 'images', default=(),
+                    filter=is_native_image)
+
+
+def is_native_event(data):
+    if is_native_platform(data.get('platform')):
+        return True
+
+    for stacktrace in find_stacktraces_in_data(data):
+        if any(is_native_platform(x) for x in stacktrace.platforms):
+            return True
+
+    return False
 
 
 def image_name(pkg):
