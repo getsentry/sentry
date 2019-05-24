1396062e107dcc938e4eb8a3b7f09961a9d2d60a
Jan Michael Auer <jan.auer@sentry.io>
2019-05-24 14:01:22 +0200

    feat(native): Add debug file sources to global search

--- src/sentry/static/sentry/app/views/projectDebugFiles.jsx
+++ src/sentry/static/sentry/app/views/projectDebugFiles.jsx
@@ -4,6 +4,7 @@ import React from 'react';
 import styled from 'react-emotion';
 
 import {Panel, PanelBody, PanelHeader, PanelItem} from 'app/components/panels';
+import {fields} from 'app/data/forms/projectDebugFiles';
 import {t} from 'app/locale';
 import Access from 'app/components/acl/access';
 import AsyncComponent from 'app/components/asyncComponent';
@@ -57,57 +58,6 @@ function getFeatureTooltip(feature) {
 const DebugSymbolDetails = styled('div')`
   margin-top: 4px;
 `;
-
-const formFields = [
-  {
-    name: 'builtinSymbolSources',
-    type: 'select',
-    multiple: true,
-    label: t('Built-in Repositories'),
-    help: t(
-      'Configures which built-in repositories Sentry should use to resolve debug files.'
-    ),
-    choices: [
-      ['microsoft', t('Microsoft')],
-      ['citrix', t('Citrix')],
-      ['intel', t('Intel')],
-      ['amd', t('AMD')],
-      ['nvidia', t('NVIDIA')],
-      ['chromium', t('Chromium')],
-      ['unity', t('Unity')],
-      ['mozilla', t('Mozilla')],
-      ['autodesk', t('Autodesk')],
-    ],
-  },
-  {
-    name: 'symbolSources',
-    type: 'string',
-    label: t('Custom Repositories'),
-    placeholder: t('Paste JSON here.'),
-    multiline: true,
-    monospace: true,
-    autosize: true,
-    inline: false,
-    maxRows: 10,
-    saveOnBlur: false,
-    saveMessageAlertType: 'info',
-    saveMessage: t('Updates will apply to future events only.'),
-    formatMessageValue: false,
-    help: t(
-      'Configures custom repositories containing debug files. At the moment, only Amazon S3 buckets are supported.'
-    ),
-    validate: ({id, form}) => {
-      try {
-        if (form[id].trim()) {
-          JSON.parse(form[id]);
-        }
-      } catch (e) {
-        return [[id, e.toString().replace(/^SyntaxError: JSON.parse: /, '')]];
-      }
-      return [];
-    },
-  },
-];
 
 class ProjectDebugSymbols extends AsyncComponent {
   static contextTypes = {
@@ -285,7 +235,7 @@ class ProjectDebugSymbols extends AsyncComponent {
                 features={features}
                 title={t('External Sources')}
                 disabled={!access.has('project:write')}
-                fields={formFields}
+                fields={[fields.builtinSymbolSources, fields.symbolSources]}
               />
             </Form>
           </>
