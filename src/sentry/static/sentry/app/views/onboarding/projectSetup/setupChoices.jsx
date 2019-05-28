d07e93bfcd7a6f9d6a68f209f4edfbd8a891eda1
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-05-28 09:59:00 -0700

    ref(onboarding): Simplify SetupChoices component (#13398)

--- src/sentry/static/sentry/app/views/onboarding/projectSetup/setupChoices.jsx
+++ src/sentry/static/sentry/app/views/onboarding/projectSetup/setupChoices.jsx
@@ -7,39 +7,28 @@ const itemsShape = PropTypes.shape({
   title: PropTypes.string.isRequired,
 });
 
-/**
- * Visually fancy choice-card component with an animated hover effect that
- * follows the focused choice. Uses react-pose to handle animations of the
- * background element by passing the computed rect
- */
-class SetupChoices extends React.Component {
-  static propTypes = {
-    choices: PropTypes.arrayOf(itemsShape),
-    selectedChoice: PropTypes.string,
-    onSelect: PropTypes.func,
-  };
+const SetupChoices = ({choices, selectedChoice, onSelect}) => (
+  <NavTabs underlined={true}>
+    {choices.map(({id, title}) => (
+      <li key={id} className={id === selectedChoice ? 'active' : null}>
+        <a
+          href="#"
+          onClick={e => {
+            onSelect(id);
+            e.preventDefault();
+          }}
+        >
+          {title}
+        </a>
+      </li>
+    ))}
+  </NavTabs>
+);
 
-  render() {
-    const {choices, selectedChoice, onSelect} = this.props;
-
-    return (
-      <NavTabs underlined={true}>
-        {choices.map(({id, title}) => (
-          <li key={id} className={id === selectedChoice ? 'active' : null}>
-            <a
-              href="#"
-              onClick={e => {
-                onSelect(id);
-                e.preventDefault();
-              }}
-            >
-              {title}
-            </a>
-          </li>
-        ))}
-      </NavTabs>
-    );
-  }
-}
+SetupChoices.propTypes = {
+  choices: PropTypes.arrayOf(itemsShape),
+  selectedChoice: PropTypes.string,
+  onSelect: PropTypes.func,
+};
 
 export default SetupChoices;
