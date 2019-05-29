93128ef9a6c27595cb8f84e656fb910d76b3caac
Katie Byers <katie.byers@sentry.io>
2019-05-29 11:25:12 -0700

    chore(SDK's): Update various SDK versions/urls (#13225)
    ... so that the UI will correctly report when the SDK used to capture an event is out of date.
    
    This is a band-aid on the real problem, which is that ideally these would get updated automatically. Working on that in a separate PR. (Will update this commit's PR with a link to it once it's up.)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1504,9 +1504,12 @@ SDK_VERSIONS = {
     'raven-ruby': '2.7.1',
     'sentry-cocoa': '3.11.1',
     'sentry-java': '1.6.4',
-    'sentry-laravel': '0.8.0',
-    'sentry-php': '1.8.2',
-    'sentry-python': '0.7.14',
+    'sentry.javascript.browser': '5.2.1',
+    'sentry.javascript.electron': '0.17.1',
+    'sentry.javascript.node': '5.2.0',
+    'sentry-laravel': '1.0.2',
+    'sentry-php': '2.0.1',
+    'sentry.python': '0.7.14',
 }
 
 SDK_URLS = {
@@ -1516,10 +1519,13 @@ SDK_URLS = {
     'raven-ruby': 'https://docs.sentry.io/clients/ruby/',
     'raven-swift': 'https://docs.sentry.io/clients/cocoa/',
     'sentry-java': 'https://docs.sentry.io/clients/java/',
-    'sentry-php': 'https://docs.sentry.io/clients/php/',
-    'sentry-laravel': 'https://docs.sentry.io/clients/php/integrations/laravel/',
+    'sentry.javascript.browser': 'https://docs.sentry.io/platforms/javascript/',
+    'sentry.javascript.electron': 'https://docs.sentry.io/platforms/javascript/electron/',
+    'sentry.javascript.node': 'https://docs.sentry.io/platforms/javascript/',
+    'sentry-php': 'https://docs.sentry.io/platforms/php/',
+    'sentry-laravel': 'https://docs.sentry.io/platforms/php/laravel/',
+    'sentry.python': 'https://docs.sentry.io/platforms/python/',
     'sentry-swift': 'https://docs.sentry.io/clients/cocoa/',
-    'sentry-python': 'https://docs.sentry.io/platforms/python/',
 }
 
 DEPRECATED_SDKS = {
@@ -1529,11 +1535,13 @@ DEPRECATED_SDKS = {
     'raven-java:log4j': 'sentry-java',
     'raven-java:log4j2': 'sentry-java',
     'raven-java:logback': 'sentry-java',
+    'raven-js': 'sentry.javascript.browser',
+    'raven-node': 'sentry.javascript.node',
     'raven-objc': 'sentry-swift',
     'raven-php': 'sentry-php',
+    'raven-python': 'sentry.python',
     'sentry-android': 'raven-java',
     'sentry-swift': 'sentry-cocoa',
-    'raven-python': 'sentry-python',
 
     # The Ruby SDK used to go by the name 'sentry-raven'...
     'sentry-raven': 'raven-ruby',
