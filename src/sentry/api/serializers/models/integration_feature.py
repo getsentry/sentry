e65f0bb6f97bb1d967764db8009f15ce159ebaca
Meredith <meredith@getsentry.com>
2019-05-29 13:10:34 -0700

    add tests

--- src/sentry/api/serializers/models/integration_feature.py
+++ src/sentry/api/serializers/models/integration_feature.py
@@ -5,9 +5,10 @@ from sentry.models import IntegrationFeature
 
 
 @register(IntegrationFeature)
-class ServiceHookSerializer(Serializer):
+class IntegrationFeatureSerializer(Serializer):
     def serialize(self, obj, attrs, user):
         return {
             'description': obj.description.strip(),
+            # feature gating work done in getsentry expects the format 'featureGate'
             'featureGate': obj.feature_str(),
         }
