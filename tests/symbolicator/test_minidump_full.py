d029ca97da5fbace50148d291083aead924f8ea1
Jan Michael Auer <jan.auer@sentry.io>
2019-06-03 12:41:09 +0200

    test(api): Add test for raw minidump ingestion

--- tests/symbolicator/test_minidump_full.py
+++ tests/symbolicator/test_minidump_full.py
@@ -86,6 +86,19 @@ class SymbolicatorMinidumpIntegrationTest(TransactionTestCase):
         assert minidump.file.type == 'event.minidump'
         assert minidump.file.checksum == '74bb01c850e8d65d3ffbc5bad5cabc4668fce247'
 
+    def test_raw_minidump(self):
+        self.project.update_option('sentry:store_crash_reports', True)
+        self.upload_symbols()
+
+        with self.feature('organizations:event-attachments'):
+            with open(get_fixture_path('windows.dmp'), 'rb') as f:
+                # Send as raw request body instead of multipart/form-data
+                resp = self._postMinidumpWithHeader(f, raw=True)
+                assert resp.status_code == 200
+
+        event = Event.objects.get()
+        insta_snapshot_stacktrace_data(self, event.data)
+
     def test_missing_dsym(self):
         with self.feature('organizations:event-attachments'):
             with open(get_fixture_path('windows.dmp'), 'rb') as f:
