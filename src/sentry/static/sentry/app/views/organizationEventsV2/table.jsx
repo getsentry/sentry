54768e3b3d63d4cc68efa6aebeb160c830289c05
Lyn Nagara <lyn.nagara@gmail.com>
2019-06-04 16:41:54 -0700

    fix(events-v2): Revert query element from button to link
    This was introduced in https://github.com/getsentry/sentry/pull/13515,
    however is causing problems with alignment/overflow.

--- src/sentry/static/sentry/app/views/organizationEventsV2/table.jsx
+++ src/sentry/static/sentry/app/views/organizationEventsV2/table.jsx
@@ -10,7 +10,7 @@ import {t} from 'app/locale';
 import space from 'app/styles/space';
 
 import {SPECIAL_FIELDS} from './data';
-import {QueryButton} from './styles';
+import {QueryLink} from './styles';
 
 export default class Table extends React.Component {
   static propTypes = {
@@ -45,9 +45,9 @@ export default class Table extends React.Component {
             {SPECIAL_FIELDS.hasOwnProperty(field) ? (
               SPECIAL_FIELDS[field].renderFunc(row, {organization, onSearch, location})
             ) : (
-              <QueryButton onClick={() => onSearch(`${field}:${row[field]}`)}>
+              <QueryLink onClick={() => onSearch(`${field}:${row[field]}`)}>
                 {row[field]}
-              </QueryButton>
+              </QueryLink>
             )}
           </Cell>
         ))}
