d708fbaaf5362bbbd41fe5c89d0bd04524d24269
Matte Noble <me@mattenoble.com>
2019-06-04 10:02:14 -0700

    fix(app-platform): Render unpublished integrations
    A bug was causing Issue pages in Orgs that had an unpublished
    Integration installed to error out. This was only case for one or two
    Orgs, as far as I can tell (our test Orgs).
    
    This fixes that bug and refactors some related things:
    
    - Retrieve SentryAppInstallations from Store instead of passing down
    through components
    - Handle when the "give me all SentryApps" and "give me all MY
    SentryApps" endpoints return the same records

--- src/sentry/static/sentry/app/actionCreators/sentryAppComponents.jsx
+++ src/sentry/static/sentry/app/actionCreators/sentryAppComponents.jsx
@@ -3,7 +3,8 @@ import SentryAppComponentsActions from 'app/actions/sentryAppComponentActions';
 export function fetchSentryAppComponents(api, orgSlug, projectId) {
   const componentsUri = `/organizations/${orgSlug}/sentry-app-components/?projectId=${projectId}`;
 
-  const promise = api.requestPromise(componentsUri);
-  promise.then(res => SentryAppComponentsActions.loadComponents(res));
-  return promise;
+  return api.requestPromise(componentsUri).then(res => {
+    SentryAppComponentsActions.loadComponents(res);
+    return res;
+  });
 }
