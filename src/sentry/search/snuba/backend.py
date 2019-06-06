2db548322e161685023f2e7c92312806f585be86
Alberto Leal <mail4alberto@gmail.com>
2019-06-06 12:33:03 -0400

    move comment

--- src/sentry/search/snuba/backend.py
+++ src/sentry/search/snuba/backend.py
@@ -341,8 +341,6 @@ class SnubaSearchBackend(SearchBackend):
                 has_groupenvironment_join = 'sentry_groupenvironment' in group_queryset.query.alias_map
                 has_sentry_release_join = 'sentry_release' in group_queryset.query.alias_map
 
-                # a groupenvironment left outer join may duplicate rows,
-                # so we would only want distinct groups
                 if has_groupenvironment_join and has_sentry_release_join:
 
                     # if a sentry_groupenvironment join exists in group_queryset,
@@ -355,6 +353,8 @@ class SnubaSearchBackend(SearchBackend):
                     # then demote the join to be an inner join
                     group_queryset.query.demote_joins(['sentry_release'])
 
+                    # a groupenvironment left outer join may duplicate rows,
+                    # so we would only want distinct groups
                     group_queryset = group_queryset.distinct()
 
         now = timezone.now()
