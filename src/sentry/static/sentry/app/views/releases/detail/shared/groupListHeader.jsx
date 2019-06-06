2c74c80754ae166db75818f40bc312a84f7c1108
Alberto Leal <mail4alberto@gmail.com>
2019-06-06 10:39:33 -0400

    fix(ui): Fix column title to be "Issue" instead of "Event" (#13546)
    Since the table is actually displaying the list of groups/issues.

--- src/sentry/static/sentry/app/views/releases/detail/shared/groupListHeader.jsx
+++ src/sentry/static/sentry/app/views/releases/detail/shared/groupListHeader.jsx
@@ -8,7 +8,7 @@ class GroupListHeader extends React.Component {
     return (
       <PanelHeader disablePadding>
         <Box w={[8 / 12, 8 / 12, 6 / 12]} mx={2} flex="1" className="toolbar-header">
-          {t('Event')}
+          {t('Issue')}
         </Box>
         <Box w={160} mx={2} className="toolbar-header hidden-xs hidden-sm">
           {t('Last 24 hours')}
