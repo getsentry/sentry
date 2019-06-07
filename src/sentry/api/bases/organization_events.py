db630de37cd1bc518dac6617f575f46a35747602
Lyn Nagara <lyn.nagara@gmail.com>
2019-06-07 13:05:33 -0700

    feat(events-v2): Add API validations (#13589)
    Return a validation error if:
    - an invalid groupby value is provided
    - no field or grouping is provided

--- src/sentry/api/bases/organization_events.py
+++ src/sentry/api/bases/organization_events.py
@@ -25,6 +25,8 @@ SPECIAL_FIELDS = {
     },
 }
 
+ALLOWED_GROUPINGS = frozenset(('issue.id', 'project.id'))
+
 
 class OrganizationEventsEndpointBase(OrganizationEndpoint):
 
@@ -74,6 +76,12 @@ class OrganizationEventsEndpointBase(OrganizationEndpoint):
         fields = request.GET.getlist('field')[:]
         aggregations = []
         groupby = request.GET.getlist('groupby')
+
+        if not fields and not groupby:
+            raise OrganizationEventsError('No fields or groupings provided')
+
+        if any(field for field in groupby if field not in ALLOWED_GROUPINGS):
+            raise OrganizationEventsError('Invalid groupby value requested')
 
         if fields:
             # If project.name is requested, get the project.id from Snuba so we
