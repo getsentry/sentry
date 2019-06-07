80588b0408215acc72fc46c875d80ef90e0014d1
Lyn Nagara <lyn.nagara@gmail.com>
2019-06-07 10:07:39 -0700

    ref(sentry10): Remove util functions related to project environments (#13580)

--- src/sentry/static/sentry/app/utils/queryString.jsx
+++ src/sentry/static/sentry/app/utils/queryString.jsx
@@ -5,36 +5,6 @@ export function formatQueryString(qs) {
   return qs.trim().replace(/\s+/g, ' ');
 }
 
-// returns environment name from query or null if not specified
-// Any character can be valid in an environment name but we need to
-// check for matching environments with the quotation marks first
-// to match the way tag searches are being done
-export function getQueryEnvironment(qs) {
-  // A match with quotes will lazily match any characters within quotation marks
-  const matchWithQuotes = qs.match(/(?:^|\s)environment:"(.*?)"/);
-  // A match without quotes will match any non space character
-  const matchWithoutQuotes = qs.match(/(?:^|\s)environment:([^\s]*)/);
-
-  if (matchWithQuotes) {
-    return matchWithQuotes[1];
-  } else if (matchWithoutQuotes) {
-    return matchWithoutQuotes[1];
-  } else {
-    return null;
-  }
-}
-
-export function getQueryStringWithEnvironment(qs, env) {
-  const qsWithoutEnv = qs.replace(/(?:^|\s)environment:[^\s]*/g, '');
-  return formatQueryString(
-    env === null ? qsWithoutEnv : `${qsWithoutEnv} environment:${env}`
-  );
-}
-
-export function getQueryStringWithoutEnvironment(qs) {
-  return formatQueryString(qs.replace(/(?:^|\s)environment:[^\s]*/g, ''));
-}
-
 export function addQueryParamsToExistingUrl(origUrl, queryParams) {
   const url = parseurl({url: origUrl});
   if (!url) {
@@ -50,8 +20,5 @@ export function addQueryParamsToExistingUrl(origUrl, queryParams) {
 
 export default {
   formatQueryString,
-  getQueryEnvironment,
-  getQueryStringWithEnvironment,
-  getQueryStringWithoutEnvironment,
   addQueryParamsToExistingUrl,
 };
