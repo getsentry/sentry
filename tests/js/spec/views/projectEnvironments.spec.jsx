832f5832ec10ce1c9bae7e6c3704a9b842517cf5
Lyn Nagara <lyn.nagara@gmail.com>
2019-06-11 09:11:12 -0700

    feat(sentry10): Remove the ability to set a default environment
    As far as I know this isn't useful in the Sentry 10 world since we now
    only use your local preferences to determine project/env defaults.

--- tests/js/spec/views/projectEnvironments.spec.jsx
+++ tests/js/spec/views/projectEnvironments.spec.jsx
@@ -28,7 +28,6 @@ function mountComponent(isHidden) {
 
 describe('ProjectEnvironments', function() {
   let project;
-  let updateDefaultMock;
 
   beforeEach(function() {
     project = TestStubs.Project({
@@ -37,10 +36,6 @@ describe('ProjectEnvironments', function() {
     MockApiClient.addMockResponse({
       url: '/projects/org-slug/project-slug/',
       body: project,
-    });
-    updateDefaultMock = MockApiClient.addMockResponse({
-      url: '/projects/org-slug/project-slug/',
-      method: 'PUT',
     });
   });
 
@@ -58,141 +53,13 @@ describe('ProjectEnvironments', function() {
       expect(wrapper.find('ProjectEnvironments')).toMatchSnapshot();
     });
 
-    it('renders environment list and sets staging as default env', async function() {
+    it('renders environment list', async function() {
       EnvironmentStore.loadInitialData(TestStubs.Environments(false));
       const wrapper = mountComponent(false);
 
-      // Production environment is default
       const productionRow = wrapper.find('EnvironmentRow[name="production"]');
-      const stagingRow = wrapper.find('EnvironmentRow[name="staging"]');
-
-      expect(productionRow.find('Tag').prop('children')).toBe('Default');
 
       expect(productionRow.find('Button')).toHaveLength(1);
-
-      expect(stagingRow.find('Tag')).toHaveLength(0);
-
-      expect(
-        stagingRow
-          .find('Button')
-          .first()
-          .text()
-      ).toBe('Set as default');
-
-      // Can set as default
-      stagingRow
-        .find('Button')
-        .first()
-        .simulate('click');
-
-      expect(updateDefaultMock).toHaveBeenCalledWith(
-        expect.anything(),
-        expect.objectContaining({
-          data: {
-            defaultEnvironment: 'staging',
-          },
-        })
-      );
-
-      expect(wrapper.find('EnvironmentRow[name="staging"] Tag')).toHaveLength(1);
-      expect(wrapper.find('EnvironmentRow[name="production"] Tag')).toHaveLength(0);
-      expect(wrapper.find('ProjectEnvironments')).toMatchSnapshot();
-    });
-
-    it('can set "(No Environment)" as default', function() {
-      EnvironmentStore.loadInitialData([
-        ...TestStubs.Environments(false),
-        {
-          id: 'no-environment-id',
-          name: '',
-          displayName: '(No Environment)',
-        },
-      ]);
-      const wrapper = mountComponent(false);
-
-      const noEnvironmentsRow = wrapper.find('EnvironmentRow[name=""]');
-
-      // Not default
-      expect(noEnvironmentsRow.find('Tag')).toHaveLength(0);
-
-      // Is able to hide
-      expect(noEnvironmentsRow.find('Button')).toHaveLength(2);
-      expect(
-        noEnvironmentsRow
-          .find('Button')
-          .first()
-          .text()
-      ).toBe('Set as default');
-      noEnvironmentsRow
-        .find('Button')
-        .first()
-        .simulate('click');
-
-      expect(updateDefaultMock).toHaveBeenCalledWith(
-        expect.anything(),
-        expect.objectContaining({
-          data: {
-            defaultEnvironment: '',
-          },
-        })
-      );
-
-      // Is default
-      expect(wrapper.find('EnvironmentRow[name=""]').find('Tag')).toHaveLength(1);
-    });
-
-    it('can set "All Environments" as default', function() {
-      EnvironmentStore.loadInitialData(TestStubs.Environments(false));
-      const wrapper = mountComponent(false);
-
-      const allEnvironmentsRow = wrapper.find(
-        `EnvironmentRow[name="${ALL_ENVIRONMENTS_KEY}"]`
-      );
-
-      // Not default
-      expect(allEnvironmentsRow.find('Tag')).toHaveLength(0);
-
-      // Should not have hide button
-      expect(allEnvironmentsRow.find('Button')).toHaveLength(1);
-      expect(allEnvironmentsRow.find('Button').text()).toBe('Set as default');
-
-      // Set as default
-      allEnvironmentsRow.find('Button').simulate('click');
-
-      expect(updateDefaultMock).toHaveBeenCalledWith(
-        expect.anything(),
-        expect.objectContaining({
-          data: {
-            defaultEnvironment: null,
-          },
-        })
-      );
-
-      expect(
-        wrapper.find(`EnvironmentRow[name="${ALL_ENVIRONMENTS_KEY}"]`).find('Tag')
-      ).toHaveLength(1);
-    });
-
-    it('displays invalid environment in list with no actions', function() {
-      project = TestStubs.Project({
-        defaultEnvironment: 'invalid-environment',
-      });
-      MockApiClient.addMockResponse({
-        url: '/projects/org-slug/project-slug/',
-        body: project,
-      });
-      EnvironmentStore.loadInitialData(TestStubs.Environments(false));
-      const wrapper = mountComponent(false);
-
-      const row = wrapper.find('EnvironmentRow[name="invalid-environment"]');
-
-      // Is default
-      expect(row.find('Tag')).toHaveLength(1);
-
-      // Can not hide or set as default
-      expect(row.find('Button')).toHaveLength(0);
-
-      expect(wrapper.find('InvalidDefaultEnvironmentIcon')).toHaveLength(1);
     });
   });
 
