a5a943d09b0739e820f14204abcfa8a1cb08bd16
Mark Story <mark@sentry.io>
2019-06-14 12:56:55 -0400

    tests: Add tags to python sample data (#13154)
    Add a few tags to the sample python data. This will populate the tag
    meters in percy and allow us catch issues similar to the tag bar
    problems yesterday.

--- tests/acceptance/test_issue_details.py
+++ tests/acceptance/test_issue_details.py
@@ -1,13 +1,18 @@
 from __future__ import absolute_import
 
 import json
+import pytz
 
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.conf import settings
 from django.utils import timezone
+from mock import patch
 
 from sentry.testutils import AcceptanceTestCase, SnubaTestCase
 from sentry.utils.samples import load_data
+
+event_time = (datetime.utcnow() - timedelta(days=3)).replace(tzinfo=pytz.utc)
+now = datetime.utcnow().replace(tzinfo=pytz.utc)
 
 
 class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
@@ -24,19 +29,29 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
         self.login_as(self.user)
         self.dismiss_assistant()
 
-    def create_sample_event(self, platform, default=None, sample_name=None):
+    def create_sample_event(self, platform, default=None, sample_name=None, time=None):
         event_data = load_data(platform, default=default, sample_name=sample_name)
         event_data['event_id'] = 'd964fdbd649a4cf8bfc35d18082b6b0e'
+
+        # Only set these properties if we were given a time.
+        # event processing will mark old time values as processing errors.
+        if time:
+            event_data['timestamp'] = time.isoformat()
+            event_data['received'] = time.isoformat()
+
+        # We need a fallback datetime for the event
+        if time is None:
+            time = datetime(2017, 9, 6, 0, 0)
         event = self.store_event(
             data=event_data,
             project_id=self.project.id,
             assert_no_errors=False,
         )
-        event.datetime = datetime(2017, 9, 6, 0, 0)
+        event.datetime = time
         event.save()
         event.group.update(
             first_seen=datetime(2015, 8, 13, 3, 8, 25, tzinfo=timezone.utc),
-            last_seen=datetime(2016, 1, 13, 3, 8, 25, tzinfo=timezone.utc),
+            last_seen=time
         )
         return event
 
@@ -47,11 +62,21 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
         )
         self.wait_until_loaded()
 
-    def test_python_event(self):
+    @patch('django.utils.timezone.now')
+    def test_python_event(self, mock_now):
+        # Event needs to be in the past.
+        mock_now.return_value = event_time
         event = self.create_sample_event(
             platform='python',
+            time=event_time,
         )
+
+        # Move time forward so our event is within range
+        mock_now.return_value = now
         self.visit_issue(event.group.id)
+
+        # Wait for tag bars to load
+        self.browser.wait_until('[data-test-id="loaded-device-name"]')
         self.browser.snapshot('issue details python')
 
     def test_python_rawbody_event(self):
