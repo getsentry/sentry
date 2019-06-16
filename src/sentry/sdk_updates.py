3ad80aa290b6e2d98fd30ca62bdca436040f8a56
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-06-16 16:06:42 +0200

    fix: Fix up merge conflicts

--- src/sentry/sdk_updates.py
+++ src/sentry/sdk_updates.py
@@ -236,7 +236,7 @@ def get_sdk_index():
 def get_sdk_versions():
     try:
         rv = settings.SDK_VERSIONS
-        rv.update((key, info['value']) for (key, info) in get_sdk_index().items())
+        rv.update((key, info['version']) for (key, info) in get_sdk_index().items())
         return rv
     except Exception:
         logger.exception("sentry-release-registry.sdk-versions")
