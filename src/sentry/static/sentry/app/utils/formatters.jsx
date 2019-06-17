d3bb31ed0a56c06f0584049eb8fa1310cff8715d
Alberto Leal <mail4alberto@gmail.com>
2019-06-17 11:51:01 -0400

    fix: Fix tooltip if either release commits has no author, or if an author's name/email of a commit contains just whitespace. (#13707)

--- src/sentry/static/sentry/app/utils/formatters.jsx
+++ src/sentry/static/sentry/app/utils/formatters.jsx
@@ -1,6 +1,17 @@
+import {get} from 'lodash';
+
+import {t} from 'app/locale';
+
 export function userDisplayName(user) {
-  let displayName = user.name;
-  if (user.email && user.email !== user.name) {
+  let displayName = String(get(user, 'name', t('Unknown author'))).trim();
+
+  if (displayName.length <= 0) {
+    displayName = t('Unknown author');
+  }
+
+  const email = String(get(user, 'email', '')).trim();
+
+  if (email.length > 0 && email !== displayName) {
     displayName += ' (' + user.email + ')';
   }
   return displayName;
