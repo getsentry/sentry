9cb80ae4e894b11bc6a15e384f3b4a570636932b
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-06-18 11:07:25 +0200

    ref: Apply review suggestions

--- src/sentry/static/sentry/app/components/events/sdkUpdates.jsx
+++ src/sentry/static/sentry/app/components/events/sdkUpdates.jsx
@@ -1,30 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import SentryTypes from 'app/sentryTypes';
+import Alert from 'app/components/alert';
+import ExternalLink from 'app/components/links/externalLink';
 
 import {t} from 'app/locale';
 
+export const EnableIntegrationSuggestion = PropTypes.shape({
+  type: PropTypes.string,
+  integrationName: PropTypes.string,
+  integrationUrl: PropTypes.string,
+});
+
+export const UpdateSdkSuggestion = PropTypes.shape({
+  type: PropTypes.string,
+  sdkName: PropTypes.string,
+  newSdkVersion: PropTypes.string,
+  sdkUrl: PropTypes.string,
+});
+
+export const ChangeSdkSuggestion = PropTypes.shape({
+  type: PropTypes.string,
+  newSdkName: PropTypes.string,
+  sdkUrl: PropTypes.string,
+});
+
+export const Suggestion = PropTypes.oneOfType([
+  EnableIntegrationSuggestion,
+  UpdateSdkSuggestion,
+  ChangeSdkSuggestion,
+]);
+
+function getSuggestionComponentKey(suggestion) {
+  return JSON.stringify(suggestion, Object.keys(suggestion).sort());
+}
+
 class EventSdkUpdateSuggestion extends React.Component {
   static propTypes = {
-    suggestion: PropTypes.object.isRequired,
+    event: SentryTypes.Event.isRequired,
+    suggestion: Suggestion.isRequired,
   };
 
   getSuggestionTitle() {
-    const {suggestion} = this.props;
+    const {event, suggestion} = this.props;
     switch (suggestion.type) {
       case 'updateSdk':
-        return <a href={suggestion.sdkUrl}>{t('update your SDK')}</a>;
+        return (
+          <ExternalLink href={suggestion.sdkUrl}>
+            {t(
+              'Update your SDK from version %s to version %s',
+              event.sdk.version,
+              suggestion.newSdkVersion
+            )}
+          </ExternalLink>
+        );
       case 'changeSdk':
         return (
-          <a href={suggestion.sdkUrl}>
-            {t("migrate to the '%s' SDK", suggestion.newSdkName)}
-          </a>
+          <ExternalLink href={suggestion.sdkUrl}>
+            {t("Migrate to the '%s' SDK", suggestion.newSdkName)}
+          </ExternalLink>
         );
       case 'enableIntegration':
         return (
-          <a href={suggestion.integrationUrl}>
-            {t("enable the '%s' integration", suggestion.integrationName)}
-          </a>
+          <ExternalLink href={suggestion.integrationUrl}>
+            {t("Enable the '%s' integration", suggestion.integrationName)}
+          </ExternalLink>
         );
       default:
         return null;
@@ -46,8 +86,8 @@ class EventSdkUpdateSuggestion extends React.Component {
         <ul>
           {suggestion.enables.map((suggestion2, i) => {
             return (
-              <li key={suggestion2}>
-                <EventSdkUpdateSuggestion suggestion={suggestion2} />
+              <li key={getSuggestionComponentKey(suggestion2)}>
+                <EventSdkUpdateSuggestion event={event} suggestion={suggestion2} />
               </li>
             );
           })}
@@ -70,10 +110,14 @@ class EventSdkUpdates extends React.Component {
       <div>
         {data.map(suggestion => {
           return (
-            <div className="alert-block alert-info box row" key={suggestion}>
+            <Alert
+              type="info"
+              icon="icon-upgrade"
+              key={getSuggestionComponentKey(suggestion)}
+            >
               {t('We recommend to ')}
-              <EventSdkUpdateSuggestion suggestion={suggestion} />
-            </div>
+              <EventSdkUpdateSuggestion event={event} suggestion={suggestion} />
+            </Alert>
           );
         })}
       </div>
