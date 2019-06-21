5f9083b55df44be3baa3709f8eae7c4263017ee8
Billy Vong <billy@sentry.io>
2019-06-20 17:55:55 -0700

    feat(ui): Use React Suspense + lazy for `<LazyLoad>`
    Refactors our `<LazyLoad>` to use `React.Suspense` and `React.lazy`.

--- src/sentry/static/sentry/app/components/lazyLoad.jsx
+++ src/sentry/static/sentry/app/components/lazyLoad.jsx
@@ -1,3 +1,4 @@
+import {hot} from 'react-hot-loader/root';
 import PropTypes from 'prop-types';
 import React from 'react';
 import styled from '@emotion/styled';
@@ -6,8 +7,6 @@ import * as Sentry from '@sentry/browser';
 import {isWebpackChunkLoadingError} from 'app/utils';
 import {t} from 'app/locale';
 import LoadingError from 'app/components/loadingError';
-import LoadingIndicator from 'app/components/loadingIndicator';
-import retryableImport from 'app/utils/retryableImport';
 
 class LazyLoad extends React.Component {
   static propTypes = {
@@ -39,7 +38,7 @@ class LazyLoad extends React.Component {
     this.fetchComponent();
   }
 
-  UNSAFE_componentWillReceiveProps(nextProps, nextState) {
+  UNSAFE_componentWillReceiveProps(nextProps) {
     // No need to refetch when component does not change
     if (nextProps.component && nextProps.component === this.props.component) {
       return;
@@ -56,89 +55,59 @@ class LazyLoad extends React.Component {
     if (nextProps.route && nextProps.route === this.props.route) {
       return;
     }
-
-    // If `this.fetchComponent` is not in callback,
-    // then there's no guarantee that new Component will be rendered
-    this.setState(
-      {
-        Component: null,
-      },
-      this.fetchComponent
-    );
-  }
-
-  componentDidCatch(error, info) {
-    Sentry.captureException(error);
-    this.handleError(error);
   }
 
   getComponentGetter = () => this.props.component || this.props.route.componentPromise;
 
-  handleFetchError = error => {
+  render() {
+    // eslint-disable-next-line no-unused-vars
+    const {hideBusy, hideError, component, ...otherProps} = this.props;
+    const Component = React.lazy(this.getComponentGetter());
+
+    return (
+      <LazyLoadErrorBoundary>
+        <React.Suspense fallback={!hideBusy ? <LoadingContainer /> : null}>
+          <Component {...otherProps} />
+        </React.Suspense>
+      </LazyLoadErrorBoundary>
+    );
+  }
+}
+
+class LazyLoadErrorBoundary extends React.Component {
+  static getDerivedStateFromError(error) {
+    return {
+      hasError: !!error,
+      error,
+    };
+  }
+
+  state = {
+    hasError: false,
+    error: null,
+  };
+
+  componentDidCatch(error, info) {
+    console.error(error); // eslint-disable-line no-console
+    console.error(info); // eslint-disable-line no-console
     Sentry.withScope(scope => {
       if (isWebpackChunkLoadingError(error)) {
         scope.setFingerprint(['webpack', 'error loading chunk']);
       }
       Sentry.captureException(error);
     });
-    this.handleError(error);
-  };
-
-  handleError = error => {
-    // eslint-disable-next-line no-console
-    console.error(error);
-    this.setState({
-      error,
-    });
-  };
-
-  async fetchComponent() {
-    const getComponent = this.getComponentGetter();
-
-    try {
-      const Component = await retryableImport(getComponent);
-      this.setState({
-        Component: Component.default || Component,
-      });
-    } catch (err) {
-      this.handleFetchError(err);
-    }
   }
 
-  fetchRetry = () => {
-    this.setState(
-      {
-        error: null,
-      },
-      () => this.fetchComponent()
-    );
-  };
-
   render() {
-    const {Component, error} = this.state;
-    // eslint-disable-next-line no-unused-vars
-    const {hideBusy, hideError, component, ...otherProps} = this.props;
-
-    if (error && !hideError) {
+    if (this.state.hasError) {
       return (
         <LoadingErrorContainer>
-          <LoadingError
-            onRetry={this.fetchRetry}
-            message={t('There was an error loading a component.')}
-          />
+          <LoadingError message={t('There was an error loading a component.')} />
         </LoadingErrorContainer>
       );
     }
 
-    if (!Component && !hideBusy) {
-      return (
-        <LoadingContainer>
-          <LoadingIndicator />
-        </LoadingContainer>
-      );
-    }
-
-    return <Component {...otherProps} />;
+    return this.props.children;
   }
 }
 
@@ -152,4 +121,4 @@ const LoadingErrorContainer = styled('div')`
   flex: 1;
 `;
 
-export default LazyLoad;
+export default hot(LazyLoad);
--- tests/js/spec/components/lazyLoad.spec.jsx
+++ tests/js/spec/components/lazyLoad.spec.jsx
@@ -1,40 +1,16 @@
 import React from 'react';
 
-import {shallow, mount} from 'sentry-test/enzyme';
+import {mount} from 'sentry-test/enzyme';
 import LazyLoad from 'app/components/lazyLoad';
 
 describe('LazyLoad', function() {
   it('renders with a loading indicator when promise is not resolved yet', function() {
     const promise = new Promise((resolve, reject) => {});
     const getComponent = () => promise;
-    const wrapper = shallow(<LazyLoad component={getComponent} />);
-
-    // Should be loading
-    expect(wrapper.find('LoadingIndicator')).toHaveLength(1);
-  });
-
-  it('renders when given a promise of a "button" component', async function() {
-    let res;
-    const promise = new Promise((resolve, reject) => {
-      res = resolve;
-    });
-    const getComponent = () => promise;
     const wrapper = mount(<LazyLoad component={getComponent} />);
 
     // Should be loading
-    expect(wrapper.find('LoadingIndicator')).toHaveLength(1);
-
-    // resolve with button
-    const ResolvedComponent = 'button';
-    res(ResolvedComponent);
-
-    await promise;
-    // Need to wait for `retryableImport` to resolve
-    await tick();
-    wrapper.update();
-    expect(wrapper.state('Component')).toEqual('button');
-    expect(wrapper.find('button')).toHaveLength(1);
-    expect(wrapper.find('LoadingIndicator')).toHaveLength(0);
+    expect(wrapper.find('LoadingContainer')).toHaveLength(1);
   });
 
   it('renders with error message when promise is rejected', async function() {
@@ -55,29 +31,11 @@ describe('LazyLoad', function() {
     // Need to wait for `retryableImport` to resolve
     await tick();
     wrapper.update();
-    expect(wrapper.find('LoadingIndicator')).toHaveLength(0);
+    expect(wrapper.find('LoadingContainer')).toHaveLength(0);
     expect(wrapper.find('LoadingError')).toHaveLength(1);
     // eslint-disable-next-line no-console
     expect(console.error).toHaveBeenCalled();
     // eslint-disable-next-line no-console
     console.error.mockRestore();
-  });
-
-  it('refetches when component changes', async function() {
-    const getComponent = jest.fn(() => new Promise());
-    const wrapper = mount(<LazyLoad component={getComponent} />);
-
-    // Should be loading
-    expect(wrapper.find('LoadingIndicator')).toHaveLength(1);
-    expect(getComponent).toHaveBeenCalled();
-
-    // Update component prop
-    const getComponent2 = jest.fn(() => new Promise());
-    wrapper.setProps({component: getComponent2});
-    expect(getComponent2).toHaveBeenCalledTimes(1);
-
-    // Does not refetch on other prop changes
-    wrapper.setProps({testProp: true});
-    expect(getComponent2).toHaveBeenCalledTimes(1);
   });
 });
