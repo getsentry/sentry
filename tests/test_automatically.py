cd0d3c8a329c28e022129d10205b4fc0f801347f
David Cramer <dcramer@gmail.com>
2019-06-25 11:04:16 -0700

    More fixes

--- tests/test_automatically.py
+++ tests/test_automatically.py
@@ -7,7 +7,10 @@ import subprocess
 def test_the_magic(db, live_server, default_user, default_group, default_event):
     from django.conf import settings
     # this is def gonna be unstable/hacky
+    import sentry
     settings.SENTRY_OPTIONS['system.url-prefix'] = live_server.url
+    settings.SENTRY_OPTIONS['beacon.anonymous'] = True
+    settings.SENTRY_OPTIONS['sentry:version-configured'] = sentry.get_version()
     cmd = os.path.join(os.path.dirname(__file__), os.pardir, 'bin', 'bug-catcher')
     subprocess.check_call([
         cmd,
