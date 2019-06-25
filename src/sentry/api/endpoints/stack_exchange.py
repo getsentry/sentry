ee2cff13e0f4f49b3b8c28b8e434656e01545f8f
Alberto Leal <mail4alberto@gmail.com>
2019-06-25 18:07:26 -0400

    refactor

--- src/sentry/api/endpoints/stack_exchange.py
+++ src/sentry/api/endpoints/stack_exchange.py
@@ -3,18 +3,45 @@ import json
 
 from rest_framework.response import Response
 
+from sentry import options
 from sentry.api.bases.project import ProjectEndpoint
+from sentry.models import Event, SnubaEvent
+
+
 # from sentry.api.permissions import ScopedPermission
 
 
 class StackExchangeEndpoint(ProjectEndpoint):
+    # TODO(dashed): what's this? is this necessary?
     # permission_classes = (ScopedPermission, )
 
     def get(self, request, project, event_id):
         # TODO: add docstrings
 
+        use_snuba = options.get('snuba.events-queries.enabled')
+
+        event_cls = SnubaEvent if use_snuba else Event
+
+        event = event_cls.objects.from_event_id(event_id, project.id)
+        if event is None:
+            return Response({'detail': 'Event not found'}, status=404)
+
+        exception_interface = event.get_interface('exception')
+
+        exception_string = exception_interface.to_string(event)
+
+        if not exception_string:
+            return Response({'detail': 'No results'}, status=404)
+
+        exception_string = exception_string.splitlines()[0].strip()
+
+        # for interface in six.itervalues(event.interfaces):
+        #     import logging
+        #     logging.info("interface: %s", interface.to_string(event))
+        #     exception_body = interface.to_email_html(event)
+
         import logging
-        logging.error("event_id: %s", event_id)
+        logging.info("exception: %s", exception_string)
 
         return Response(thing, status=200)
 
