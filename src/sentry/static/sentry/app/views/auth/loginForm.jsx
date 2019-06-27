d84a7231f1cae2711905ce1ec983a1a84646e842
Burak Yigit Kaya <ben@byk.im>
2019-06-27 20:29:06 +0300

    try redirects again

--- src/sentry/static/sentry/app/views/auth/loginForm.jsx
+++ src/sentry/static/sentry/app/views/auth/loginForm.jsx
@@ -26,7 +26,7 @@ class AuthLoginForm extends React.Component {
     formData.append('username', data.username);
     formData.append('password', data.password);
     try {
-      const response = await fetch('/auth/login/?post', {
+      const response = await fetch('/auth/login/?post=1', {
         method: 'POST',
         redirect: 'manual',
         body: formData,
