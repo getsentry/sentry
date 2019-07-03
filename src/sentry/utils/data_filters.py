741280ea6662768aca8e1aab23fb75126176495f
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2019-07-03 11:11:59 +0200

    Replace database access for ProjectOptions with relay_config access (#13880)

--- src/sentry/utils/data_filters.py
+++ src/sentry/utils/data_filters.py
@@ -48,12 +48,12 @@ class FilterTypes(object):
     RELEASES = 'releases'
 
 
-def is_valid_ip(project, ip_address):
+def is_valid_ip(relay_config, ip_address):
     """
     Verify that an IP address is not being blacklisted
     for the given project.
     """
-    blacklist = project.get_option('sentry:blacklisted_ips')
+    blacklist = relay_config.config.get('blacklisted_ips')
     if not blacklist:
         return True
 
@@ -77,12 +77,12 @@ def is_valid_ip(project, ip_address):
     return True
 
 
-def is_valid_release(project, release):
+def is_valid_release(relay_config, release):
     """
     Verify that a release is not being filtered
     for the given project.
     """
-    invalid_versions = project.get_option(u'sentry:{}'.format(FilterTypes.RELEASES))
+    invalid_versions = relay_config.config.get(FilterTypes.RELEASES)
     if not invalid_versions:
         return True
 
@@ -95,12 +95,12 @@ def is_valid_release(project, release):
     return True
 
 
-def is_valid_error_message(project, message):
+def is_valid_error_message(relay_config, message):
     """
     Verify that an error message is not being filtered
     for the given project.
     """
-    filtered_errors = project.get_option(u'sentry:{}'.format(FilterTypes.ERROR_MESSAGES))
+    filtered_errors = relay_config.config.get(FilterTypes.ERROR_MESSAGES)
     if not filtered_errors:
         return True
 
