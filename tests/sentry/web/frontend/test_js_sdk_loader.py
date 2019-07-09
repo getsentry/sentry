a3e3260a824c5854300d85ad750a22323af4daa6
Armin Ronacher <armin.ronacher@active-4.com>
2019-07-09 15:41:26 +0200

    feat(loader): Make the default for new js projects v5 of js sdk (#13327)
    * feat(loader): Make the default for new js projects v5 of js sdk
    
    * fix: Tests

--- tests/sentry/web/frontend/test_js_sdk_loader.py
+++ tests/sentry/web/frontend/test_js_sdk_loader.py
@@ -71,7 +71,7 @@ class JavaScriptSdkLoaderTest(TestCase):
         self.assertIn('s-maxage', resp['Cache-Control'])
         self.assertIn('max-age', resp['Cache-Control'])
         self.assertIn('project/%s' % self.projectkey.project_id, resp['Surrogate-Key'])
-        self.assertIn('sdk/%s' % mocked_version, resp['Surrogate-Key'])
+        self.assertIn('sdk/', resp['Surrogate-Key'])
         self.assertIn('sdk-loader', resp['Surrogate-Key'])
         assert 'Content-Encoding' not in resp
         assert 'Set-Cookie' not in resp
