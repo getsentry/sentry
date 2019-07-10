f50d0aeba91dfadaf4cbc8efc1280b5ec6c29bbb
Billy Vong <billy@sentry.io>
2019-07-09 19:28:36 -0700

    update tests

--- tests/js/spec/views/projectPlugins/projectPluginsRow.spec.jsx
+++ tests/js/spec/views/projectPlugins/projectPluginsRow.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {mount} from 'enzyme';
-import ProjectPluginRow from 'app/views/projectPlugins/projectPluginRow';
+import ProjectPluginRow from 'app/views/settings/projectPlugins/projectPluginRow';
 
 describe('ProjectPluginRow', function() {
   let wrapper;
