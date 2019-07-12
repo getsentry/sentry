49b884c9e13e24b969974c67045a8383ec0cb9f1
Billy Vong <billy@sentry.io>
2019-07-11 18:01:51 -0700

    fix(ui): Fix Organization/Project Settings breadcrumbs
    This fixes navigation issues with the breadcrumb inside of settings. Also fixes
    the problem when inside of an org specific resource and trying to switch orgs.
    
    Previously it would keep all of the router params the same and it would 404 since the resource would not exist in the target organization.

---
+++ tests/js/spec/views/settings/components/settingsBreadcrumb/findFirstRouteWithoutRouteParam.spec.jsx
@@ -0,0 +1,26 @@
+import findFirstRouteWithoutRouteParam from 'app/views/settings/components/settingsBreadcrumb/findFirstRouteWithoutRouteParam';
+
+describe('findFirstRouteWithoutRouteParam', function() {
+  const routes = [
+    {path: '/', childRoutes: []},
+    {childRoutes: []},
+    {path: '/foo/', childRoutes: []},
+    {childRoutes: []},
+    {path: ':bar', childRoutes: []},
+    {path: '/settings/', name: 'Settings'},
+    {name: 'Organizations', path: ':orgId/', childRoutes: []},
+    {childRoutes: []},
+    {path: 'api-keys/', name: 'API Key'},
+    {path: ':apiKey/', name: 'API Key Details'},
+  ];
+
+  it('finds the first route', function() {
+    expect(findFirstRouteWithoutRouteParam(routes).path).toBe('/');
+  });
+
+  it('finds the first route after the given route', function() {
+    expect(findFirstRouteWithoutRouteParam(routes, routes[2]).path).toBe('/foo/');
+    expect(findFirstRouteWithoutRouteParam(routes, routes[6]).path).toBe('api-keys/');
+    expect(findFirstRouteWithoutRouteParam(routes, routes[8]).path).toBe('api-keys/');
+  });
+});
