d6083a1cc4c78bc2fdc71338fd89ed3ec56aff6a
Mark Story <mark@sentry.io>
2019-07-22 12:43:59 -0400

    Remove spans references.
    They are not part of these changes.

--- src/sentry/static/sentry/app/components/events/eventEntries.jsx
+++ src/sentry/static/sentry/app/components/events/eventEntries.jsx
@@ -32,7 +32,6 @@ import SentryTypes from 'app/sentryTypes';
 import StacktraceInterface from 'app/components/events/interfaces/stacktrace';
 import TemplateInterface from 'app/components/events/interfaces/template';
 import ThreadsInterface from 'app/components/events/interfaces/threads';
-import SpansInterface from 'app/components/events/interfaces/spans';
 import withApi from 'app/utils/withApi';
 import withOrganization from 'app/utils/withOrganization';
 
@@ -49,7 +48,6 @@ export const INTERFACES = {
   breadcrumbs: BreadcrumbsInterface,
   threads: ThreadsInterface,
   debugmeta: DebugMetaInterface,
-  spans: SpansInterface,
 };
 
 class EventEntries extends React.Component {
