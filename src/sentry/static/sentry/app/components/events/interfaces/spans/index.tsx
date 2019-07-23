7c3a9d6474dca18ef52f8a098b3ba52ab2fd2505
Alberto Leal <mail4alberto@gmail.com>
2019-07-23 15:16:45 -0400

    clean up

--- src/sentry/static/sentry/app/components/events/interfaces/spans/index.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/index.tsx
@@ -1,6 +1,4 @@
 import React from 'react';
-// TODO: remove
-// import PropTypes from 'prop-types';
 
 import {t} from 'app/locale';
 import SentryTypes from 'app/sentryTypes';
@@ -17,20 +15,6 @@ type SpansInterfacePropTypes = {
 class SpansInterface extends React.Component<SpansInterfacePropTypes> {
   static propTypes = {
     event: SentryTypes.Event.isRequired,
-    // TODO: necessary?
-    // type: PropTypes.oneOf(['spans']).isRequired,
-    // data: PropTypes.arrayOf(
-    //   PropTypes.shape({
-    //     trace_id: PropTypes.string.isRequired,
-    //     parent_span_id: PropTypes.string,
-    //     span_id: PropTypes.string.isRequired,
-    //     start_timestamp: PropTypes.number.isRequired,
-    //     timestamp: PropTypes.number.isRequired, // same as end_timestamp
-    //     same_process_as_parent: PropTypes.bool.isRequired,
-    //     op: PropTypes.string.isRequired,
-    //     data: PropTypes.object.isRequired,
-    //   })
-    // ).isRequired,
   };
   render() {
     const {event} = this.props;
