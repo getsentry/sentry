7546cbb2204568488425e5871424f14c4fd28782
Mark Story <mark@sentry.io>
2019-07-24 14:40:20 -0400

    Add re-export shims for getsentry upsell pages

---
+++ src/sentry/static/sentry/app/views/organizationDiscover/discover.tsx
@@ -0,0 +1,4 @@
+// Temporary re-export shim for getsentry
+import Discover from 'app/views/discover/discover';
+
+export default Discover;
---
+++ src/sentry/static/sentry/app/views/organizationDiscover/queryBuilder.tsx
@@ -0,0 +1,4 @@
+// Temporary re-export shim for getsentry
+import createQueryBuilder from 'app/views/discover/queryBuilder';
+
+export default createQueryBuilder;
