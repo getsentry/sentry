9950383c48674634665ab159a77dc12eecbe6cf4
Stephen Cefali <scefali@sentry.io>
2019-07-25 09:26:24 -0700

    Feat/multiple stack trace integrations (#14079)

--- src/sentry/static/sentry/app/components/events/interfaces/openInContextLine.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/openInContextLine.jsx
@@ -21,26 +21,37 @@ class OpenInContextLine extends React.Component {
     };
   }
 
-  getUrl() {
-    const {filename, lineNo, components} = this.props;
+  getUrl(component) {
+    const {filename, lineNo} = this.props;
 
     const queryParams = {
       lineNo,
       filename,
     };
-    return addQueryParamsToExistingUrl(components[0].schema.url, queryParams);
+    return addQueryParamsToExistingUrl(component.schema.url, queryParams);
   }
+
+  renderOpenInLink = component => {
+    const url = this.getUrl(component);
+    const {slug} = component.sentryApp;
+    return (
+      <OpenInLink
+        key={component.uuid}
+        data-test-id={`stacktrace-link-${slug}`}
+        href={url}
+      >
+        <OpenInIcon slug={slug} />
+        <OpenInName>{t(`${component.sentryApp.name}`)}</OpenInName>
+      </OpenInLink>
+    );
+  };
 
   render() {
     const {components} = this.props;
-    const url = this.getUrl();
     return (
       <OpenInContainer>
         <span>{t('Open this line in:')}</span>
-        <OpenInLink data-test-id="stacktrace-link" href={url}>
-          <OpenInIcon slug={components[0].sentryApp.slug} />
-          <OpenInName>{t(`${components[0].sentryApp.name}`)}</OpenInName>
-        </OpenInLink>
+        {components.map(this.renderOpenInLink)}
       </OpenInContainer>
     );
   }
@@ -56,6 +67,8 @@ const OpenInContainer = styled('div')`
   background-color: white;
   color: ${p => p.theme.purple2};
   box-shadow: 0 1px 0 0 rgba(0, 0, 0, 0.05);
+  overflow: auto;
+  white-space: nowrap;
 `;
 
 const OpenInIcon = styled(SentryAppIcon)`
@@ -63,15 +76,23 @@ const OpenInIcon = styled(SentryAppIcon)`
   height: 15px;
   width: 15px;
   margin-left: ${space(1)};
+  margin-right: 4px;
 `;
 
 const OpenInLink = styled('a')`
   color: ${p => p.theme.gray2};
   cursor: pointer;
+  margin-left: 5px;
+  &:not(:last-child):after {
+    border-right: 1px solid ${p => p.theme.gray1};
+    content: '';
+    height: 60%;
+    margin-top: 15%;
+    margin-left: 10px;
+  }
 `;
 
 const OpenInName = styled('span')`
   font-weight: bold;
   color: ${p => p.theme.gray3};
-  margin-left: 5px;
 `;
