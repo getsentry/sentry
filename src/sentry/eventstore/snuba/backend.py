f48b53a444d408922998261c158ccde6cb8daf3a
Lyn Nagara <lyn.nagara@gmail.com>
2019-08-02 13:53:41 -0700

    use SnubaEvent.get_event implementation

--- src/sentry/eventstore/snuba/backend.py
+++ src/sentry/eventstore/snuba/backend.py
@@ -1,16 +1,7 @@
 from __future__ import absolute_import
 
-from datetime import datetime
-
-from sentry.utils import snuba
 from sentry.models import SnubaEvent
 from sentry.eventstore.base import EventStorage
-
-DEFAULT_START = datetime.utcfromtimestamp(0)  # will be clamped to project retention
-DEFAULT_END = datetime.utcnow()  # will be clamped to project retention
-DEFAULT_ORDERBY = ['-timestamp', '-event_id']
-DEFAULT_LIMIT = 100
-DEFAULT_OFFSET = 0
 
 
 class SnubaEventStorage(EventStorage):
@@ -25,22 +16,7 @@ class SnubaEventStorage(EventStorage):
         """
         cols = self.__get_columns(additional_columns)
 
-        result = snuba.raw_query(
-            start=DEFAULT_START,
-            end=DEFAULT_END,
-            selected_columns=cols,
-            filter_keys={
-                'event_id': [event_id],
-                'project_id': [project_id],
-            },
-            limit=1,
-            referrer='eventstore.get_event_by_id',
-        )
-
-        if 'error' not in result and len(result['data']):
-            return SnubaEvent(result['data'][0])
-
-        return None
+        return SnubaEvent.get_event(project_id, event_id, snuba_cols=cols)
 
     def __get_columns(self, additional_columns):
         columns = EventStorage.minimal_columns
