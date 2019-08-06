6d79dfa392302b58e590062e74ca4ca717f5a798
Armin Ronacher <armin.ronacher@active-4.com>
2019-08-06 10:14:56 +0200

    feat: Added variable based fingerprinting (#14126)

--- src/sentry/stacktraces/processing.py
+++ src/sentry/stacktraces/processing.py
@@ -390,6 +390,28 @@ def process_single_stacktrace(processing_task, stacktrace_info, processable_fram
     )
 
 
+def get_crash_frame_from_event_data(data, frame_filter=None):
+    frames = get_path(data, 'exception', 'values', -1, 'stacktrace', 'frames') or \
+        get_path(data, 'stacktrace', 'frames')
+    if not frames:
+        threads = get_path(data, 'threads', 'values')
+        if threads and len(threads) == 1:
+            frames = get_path(threads, 0)
+
+    default = None
+    for frame in reversed(frames or ()):
+        if frame_filter is not None:
+            if not frame_filter(frame):
+                continue
+        if frame.get('in_app'):
+            return frame
+        if default is None:
+            default = frame
+
+    if default:
+        return default
+
+
 def lookup_frame_cache(keys):
     rv = {}
     for key in keys:
