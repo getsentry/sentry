1cdbb9a794171b627c5f5cfb727b8ca39bf38a78
Lyn Nagara <lyn.nagara@gmail.com>
2019-08-07 10:45:33 -0700

    feat(eventstore): Add get_event_by_id (#14249)
    Adds eventstore function to fetch a single event given a project ID
    and event ID. This should eventually replace EventManager.from_event_id
    and SnubaEvent.get_event functions.

--- src/sentry/eventstore/snuba/backend.py
+++ src/sentry/eventstore/snuba/backend.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-
+from sentry.models import SnubaEvent
 from sentry.eventstore.base import EventStorage
 
 
@@ -8,3 +8,20 @@ class SnubaEventStorage(EventStorage):
     """
     Eventstore backend backed by Snuba
     """
+
+    def get_event_by_id(self, project_id, event_id, additional_columns=None):
+        """
+        Get an event given a project ID and event ID
+        Returns None if an event cannot be found
+        """
+        cols = self.__get_columns(additional_columns)
+
+        return SnubaEvent.get_event(project_id, event_id, snuba_cols=cols)
+
+    def __get_columns(self, additional_columns):
+        columns = EventStorage.minimal_columns
+
+        if additional_columns:
+            columns = set(columns + additional_columns)
+
+        return [col.value for col in columns]
