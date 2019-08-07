9500690527caf15445b076eedd077aa8e74d2fb1
Lyn Nagara <lyn.nagara@gmail.com>
2019-08-07 10:44:29 -0700

    ref(ts): Convert a few utils to TypeScript (#14303)

--- src/sentry/static/sentry/app/utils/getDisplayName.tsx
+++ src/sentry/static/sentry/app/utils/getDisplayName.tsx
@@ -1,6 +1,6 @@
 // Attempts to get a display name from a Component
 //
 // Use for HoCs
-export default function getDisplayName(WrappedComponent) {
+export default function getDisplayName(WrappedComponent: React.ComponentType): string {
   return WrappedComponent.displayName || WrappedComponent.name || 'Component';
 }
