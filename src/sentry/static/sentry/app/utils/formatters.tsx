bc5620323c8c31ab44a1c0188421580d0c2994d7
Lyn Nagara <lyn.nagara@gmail.com>
2019-08-07 17:18:45 -0700

    ref(ts): Convert formatters.jsx to TypeScript (#14302)
    Add the User and CommitAuthor type

--- src/sentry/static/sentry/app/utils/formatters.tsx
+++ src/sentry/static/sentry/app/utils/formatters.tsx
@@ -1,8 +1,9 @@
 import {get} from 'lodash';
 
 import {t} from 'app/locale';
+import {CommitAuthor, User} from 'app/types';
 
-export function userDisplayName(user) {
+export function userDisplayName(user: User | CommitAuthor): string {
   let displayName = String(get(user, 'name', t('Unknown author'))).trim();
 
   if (displayName.length <= 0) {
@@ -12,7 +13,7 @@ export function userDisplayName(user) {
   const email = String(get(user, 'email', '')).trim();
 
   if (email.length > 0 && email !== displayName) {
-    displayName += ' (' + user.email + ')';
+    displayName += ' (' + email + ')';
   }
   return displayName;
 }
