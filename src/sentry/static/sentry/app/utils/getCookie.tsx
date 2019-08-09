488044c4573d9403976b5ec0978af7a3609593d9
Lyn Nagara <lyn.nagara@gmail.com>
2019-08-09 11:22:58 -0700

    ref(ts): Convert a number of utility functions to TypeScript (#14331)
    - getCookie
    - getPeriod
    - logging
    - callIfFunction
    - parseLinkHeader

--- src/sentry/static/sentry/app/utils/getCookie.tsx
+++ src/sentry/static/sentry/app/utils/getCookie.tsx
@@ -1,15 +1,13 @@
-export default function getCookie(name) {
-  let cookieValue = null;
+export default function getCookie(name: string): string | null {
   if (document.cookie && document.cookie !== '') {
     const cookies = document.cookie.split(';');
     for (let i = 0; i < cookies.length; i++) {
       const cookie = cookies[i].trim();
       // Does this cookie string begin with the name we want?
       if (cookie.substring(0, name.length + 1) === name + '=') {
-        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-        break;
+        return decodeURIComponent(cookie.substring(name.length + 1));
       }
     }
   }
-  return cookieValue;
+  return null;
 }
