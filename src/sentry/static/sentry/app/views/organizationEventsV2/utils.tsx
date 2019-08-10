93648bd8196c01cd8f96f3fb193970797e3079b2
Alberto Leal <mail4alberto@gmail.com>
2019-08-10 00:57:43 -0400

    utils.tsx

--- src/sentry/static/sentry/app/views/organizationEventsV2/utils.tsx
+++ src/sentry/static/sentry/app/views/organizationEventsV2/utils.tsx
@@ -1,5 +1,8 @@
 import {partial, pick, get} from 'lodash';
+import {ReactRouterLocation} from 'app/types/reactRouter';
 
+import {Client} from 'app/api';
+import {EventView} from 'app/types';
 import {DEFAULT_PER_PAGE} from 'app/constants';
 import {URL_PARAM} from 'app/constants/globalSelectionHeader';
 import {ALL_VIEWS, SPECIAL_FIELDS, FIELD_FORMATTERS} from './data';
@@ -11,9 +14,16 @@ import {ALL_VIEWS, SPECIAL_FIELDS, FIELD_FORMATTERS} from './data';
  * @returns {Object}
  *
  */
-export function getCurrentView(requestedView?: string) {
+export function getCurrentView(requestedView?: string): EventView {
   return ALL_VIEWS.find(view => view.id === requestedView) || ALL_VIEWS[0];
 }
+
+export type EventQuery = {
+  field: Array<string>;
+  project?: string;
+  sort?: string;
+  query: string;
+};
 
 /**
  * Takes a view and converts it into the format required for the events API
@@ -21,11 +31,21 @@ export function getCurrentView(requestedView?: string) {
  * @param {Object} view
  * @returns {Object}
  */
-export function getQuery(view, location) {
-  const groupby = view.data.groupby ? [...view.data.groupby] : [];
-  const fields = get(view, 'data.fields', []);
+export function getQuery(view: EventView, location: ReactRouterLocation) {
+  const fields: Array<string> = get(view, 'data.fields', []);
 
-  const data = pick(location.query, [
+  type LocationQuery = {
+    project?: string;
+    environment?: string;
+    start?: string;
+    end?: string;
+    utc?: string;
+    statsPeriod?: string;
+    cursor?: string;
+    sort?: string;
+  };
+
+  const picked = pick<LocationQuery>(location.query, [
     'project',
     'environment',
     'start',
@@ -36,13 +56,12 @@ export function getQuery(view, location) {
     'sort',
   ]);
 
-  data.field = [...new Set(fields)];
-  data.groupby = groupby;
-  if (!data.sort) {
-    data.sort = view.data.sort;
-  }
-  data.per_page = DEFAULT_PER_PAGE;
-  data.query = getQueryString(view, location);
+  const data: EventQuery = Object.assign(picked, {
+    field: [...new Set(fields)],
+    sort: picked.sort ? picked.sort : view.data.sort,
+    per_page: DEFAULT_PER_PAGE,
+    query: getQueryString(view, location),
+  });
 
   return data;
 }
@@ -55,13 +74,27 @@ export function getQuery(view, location) {
  * @param {Location} browser location
  * @param {Object} additional parameters to merge into the query string.
  */
-export function getQueryString(view, location, additional): string {
-  const queryParts = [];
+export function getQueryString(
+  view: EventView,
+  location: ReactRouterLocation,
+  additional?: {[key: string]: string}
+): string {
+  const queryParts: Array<string> = [];
   if (view.data.query) {
     queryParts.push(view.data.query);
   }
   if (location.query && location.query.query) {
-    queryParts.push(location.query.query);
+    // there may be duplicate query in the query string
+    // e.g. query=hello&query=world
+    if (Array.isArray(location.query.query)) {
+      location.query.query.forEach(query => {
+        queryParts.push(query);
+      });
+    }
+
+    if (typeof location.query.query === 'string') {
+      queryParts.push(location.query.query);
+    }
   }
   if (additional) {
     Object.entries(additional).forEach(([key, value]) => {
@@ -83,7 +116,11 @@ export function getQueryString(view, location, additional): string {
  * @param {Object} browser location object.
  * @return {Object} router target
  */
-export function getEventTagSearchUrl(tagKey, tagValue, location) {
+export function getEventTagSearchUrl(
+  tagKey: string,
+  tagValue: string,
+  location: ReactRouterLocation
+) {
   const query = {...location.query};
   // Add tag key/value to search
   if (query.query) {
@@ -100,20 +137,39 @@ export function getEventTagSearchUrl(tagKey, tagValue, location) {
   };
 }
 
+export type TagTopValue = {
+  url: {
+    pathname: string;
+    query: any;
+  };
+  value: string;
+};
+
+export type Tag = {
+  topValues: Array<TagTopValue>;
+};
+
 /**
  * Fetches tag distributions for a single tag key
  *
  * @param {Object} api
  * @param {String} orgSlug
  * @param {String} key
- * @param {string} query
+ * @param {String} query
  * @returns {Promise<Object>}
  */
-export function fetchTagDistribution(api, orgSlug, key, query) {
+export function fetchTagDistribution(
+  api: Client,
+  orgSlug: string,
+  key: string,
+  query: EventQuery
+): Promise<Tag> {
   const urlParams = pick(query, Object.values(URL_PARAM));
 
+  const queryOption = {...urlParams, key, query: query.query};
+
   return api.requestPromise(`/organizations/${orgSlug}/events-distribution/`, {
-    query: {...urlParams, key, query: query.query},
+    query: queryOption,
   });
 }
 
@@ -125,14 +181,24 @@ export function fetchTagDistribution(api, orgSlug, key, query) {
  * @param {string} query
  * @returns {Promise<Number>}
  */
-export function fetchTotalCount(api, orgSlug, query) {
+export function fetchTotalCount(
+  api: Client,
+  orgSlug: String,
+  query: EventQuery
+): Promise<number> {
   const urlParams = pick(query, Object.values(URL_PARAM));
+
+  const queryOption = {...urlParams, query: query.query};
+
+  type Response = {
+    count: number;
+  };
 
   return api
     .requestPromise(`/organizations/${orgSlug}/events-meta/`, {
-      query: {...urlParams, query: query.query},
+      query: queryOption,
     })
-    .then(res => res.count);
+    .then((res: Response) => res.count);
 }
 
 /**
@@ -142,7 +208,7 @@ export function fetchTotalCount(api, orgSlug, query) {
  * @param {object} metadata mapping.
  * @returns {Function}
  */
-export function getFieldRenderer(field, meta) {
+export function getFieldRenderer(field: string, meta) {
   if (SPECIAL_FIELDS.hasOwnProperty(field)) {
     return SPECIAL_FIELDS[field].renderFunc;
   }
