08b3ee386258d74d0cc4149898e89b52dede798a
Burak Yigit Kaya <ben@byk.im>
2019-08-14 23:37:02 +0300

    fix test helper

--- src/sentry/testutils/helpers/socket.py
+++ src/sentry/testutils/helpers/socket.py
@@ -11,13 +11,16 @@ def override_blacklist(*ip_addresses):
     def decorator(func):
         def wrapper(*args, **kwargs):
             disallowed_ips = frozenset(net_socket.DISALLOWED_IPS)
+            whitelisted_ips = frozenset(net_socket.WHITELISTED_IPS)
             net_socket.DISALLOWED_IPS = frozenset(
                 ipaddress.ip_network(six.text_type(ip)) for ip in ip_addresses
             )
+            net_socket.WHITELISTED_IPS = frozenset()
             try:
                 func(*args, **kwargs)
             finally:
                 net_socket.DISALLOWED_IPS = disallowed_ips
+                net_socket.WHITELISTED_IPS = whitelisted_ips
                 # We end up caching these disallowed ips on this function, so
                 # make sure we clear the cache as part of cleanup
                 net_socket.is_ipaddress_allowed.cache_clear()
