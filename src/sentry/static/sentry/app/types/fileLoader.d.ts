d88bf688844ad39d24c72c8a4a55d156d25aca77
Alberto Leal <mail4alberto@gmail.com>
2019-08-14 15:00:58 -0400

    chore(ts): webpack file-loader compatibility for TS (#14387)

---
+++ src/sentry/static/sentry/app/types/fileLoader.d.ts
@@ -0,0 +1,4 @@
+// Reference: https://github.com/Microsoft/TypeScript-React-Starter/issues/12#issuecomment-327860151
+// TS compatibility for https://github.com/webpack-contrib/file-loader
+
+declare module '*.png';
