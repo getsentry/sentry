f89e6db76f078a1390e5f44b06390e5909555f5b
Burak Yigit Kaya <byk@sentry.io>
2019-08-21 19:52:02 +0200

    use the proxy mode for webpack

--- src/sentry/runner/commands/devserver.py
+++ src/sentry/runner/commands/devserver.py
@@ -71,6 +71,12 @@ def devserver(reload, watchers, workers, experimental_spa, styleguide, prefix, e
             )
 
     uwsgi_overrides = {
+        # Make sure uWSGI spawns an HTTP server for us as we don't
+        # have a proxy/load-balancer in front in dev mode.
+        'http': '%s:%s' % (host, port),
+        'protocol': 'uwsgi',
+        # This is needed to prevent https://git.io/fj7Lw
+        'uwsgi-socket': None,
         'http-keepalive': True,
         # Make sure we reload really quickly for local dev in case it
         # doesn't want to shut down nicely on it's own, NO MERCY
@@ -109,6 +115,8 @@ def devserver(reload, watchers, workers, experimental_spa, styleguide, prefix, e
         proxy_port = port
         port = port + 1
 
+        uwsgi_overrides['protocol'] = 'http'
+
         os.environ["SENTRY_WEBPACK_PROXY_PORT"] = "%s" % proxy_port
         os.environ["SENTRY_BACKEND_PORT"] = "%s" % port
 
@@ -119,6 +127,17 @@ def devserver(reload, watchers, workers, experimental_spa, styleguide, prefix, e
         )
 
         daemons = [w for w in daemons if w[0] != "webpack"] + [("webpack", webpack_config)]
+    else:
+        # If we are the bare http server, use the http option with uwsgi protocol
+        # See https://uwsgi-docs.readthedocs.io/en/latest/HTTP.html
+        uwsgi_overrides.update({
+            # Make sure uWSGI spawns an HTTP server for us as we don't
+            # have a proxy/load-balancer in front in dev mode.
+            'http': '%s:%s' % (host, port),
+            'protocol': 'uwsgi',
+            # This is needed to prevent https://git.io/fj7Lw
+            'uwsgi-socket': None,
+        })
 
     if workers:
         if settings.CELERY_ALWAYS_EAGER:
@@ -171,15 +190,6 @@ def devserver(reload, watchers, workers, experimental_spa, styleguide, prefix, e
         uwsgi_overrides["log-format"] = '"%(method) %(uri) %(proto)" %(status) %(size)'
     else:
         uwsgi_overrides["log-format"] = '[%(ltime)] "%(method) %(uri) %(proto)" %(status) %(size)'
-
-    uwsgi_overrides.update({
-        # Make sure uWSGI spawns an HTTP server for us as we don't
-        # have a proxy/load-balancer in front in dev mode.
-        'http': '%s:%s' % (host, port),
-        'protocol': 'uwsgi',
-        # This is needed to prevent https://git.io/fj7Lw
-        'uwsgi-socket': None,
-    })
 
     server = SentryHTTPServer(host=host, port=port, workers=1, extra_options=uwsgi_overrides)
 
