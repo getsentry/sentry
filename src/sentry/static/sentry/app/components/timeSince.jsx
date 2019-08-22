46fcc01128e8aaa357e203f5f835c23a42e723a8
Danny Lee <leedongwei@gmail.com>
2019-08-21 18:09:28 -0700

    fix(platform): Fixed error message caused by 78536f8

--- src/sentry/static/sentry/app/components/timeSince.jsx
+++ src/sentry/static/sentry/app/components/timeSince.jsx
@@ -38,6 +38,10 @@ class TimeSince extends React.PureComponent {
 
   constructor(props) {
     super(props);
+
+    this.state = {
+      relative: '',
+    };
   }
 
   static getDerivedStateFromProps(props) {
