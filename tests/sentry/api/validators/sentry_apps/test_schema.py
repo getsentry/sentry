e5caf1347d533760ff5634f6e00c6cddca367a52
Stephen Cefali <scefali@sentry.io>
2019-08-23 09:30:28 -0700

    adds unit tests

--- tests/sentry/api/validators/sentry_apps/test_schema.py
+++ tests/sentry/api/validators/sentry_apps/test_schema.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 
 from sentry.testutils import TestCase
 from sentry.api.validators.sentry_apps.schema import validateUiElementSchema
+from jsonschema.exceptions import ValidationError as SchemaValidationError
 
 
 class TestSchemaValidation(TestCase):
@@ -72,3 +73,38 @@ class TestSchemaValidation(TestCase):
 
     def test_valid_schema_with_options(self):
         validateUiElementSchema(self.schema)
+
+    def test_invalid_schema_elements_missing(self):
+        schema = {"type": "nothing"}
+        try:
+            validateUiElementSchema(schema)
+            raise Exception("schema validation should fail")
+        except SchemaValidationError as e:
+            assert e.message == "'elements' is a required property"
+
+    def test_invalid_schema_elements_not_array(self):
+        schema = {"elements": {"type": "issue-link"}}
+        try:
+            validateUiElementSchema(schema)
+            raise Exception("schema validation should fail")
+        except SchemaValidationError as e:
+            assert e.message == "'elements' should be an array of objects"
+
+    def test_invalid_schema_type_missing(self):
+        schema = {"elements": [{"key": "issue-link"}]}
+        try:
+            validateUiElementSchema(schema)
+            raise Exception("schema validation should fail")
+        except SchemaValidationError as e:
+            assert e.message == "Each element needs a 'type' field"
+
+    def test_invalid_schema_type_invalid(self):
+        schema = {"elements": [{"type": "other"}]}
+        try:
+            validateUiElementSchema(schema)
+            raise Exception("schema validation should fail")
+        except SchemaValidationError as e:
+            assert (
+                e.message
+                == "Element has type 'other'. Type must be one of the following: ['issue-link', 'alert-rule-action', 'issue-media', 'stacktrace-link']"
+            )
