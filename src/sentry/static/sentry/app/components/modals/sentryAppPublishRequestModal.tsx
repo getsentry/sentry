d61cfc0958e0a1beec3551ae3fd682551405bdc0
Stephen Cefali <scefali@sentry.io>
2019-09-05 09:09:32 -0700

    remove comment

--- src/sentry/static/sentry/app/components/modals/sentryAppPublishRequestModal.tsx
+++ src/sentry/static/sentry/app/components/modals/sentryAppPublishRequestModal.tsx
@@ -41,7 +41,6 @@ export default class SentryAppPublishRequestModal extends React.Component<Props>
 
   form = new PublishRequestFormModel();
 
-  //fake
   get formFields() {
     const {app} = this.props;
     //replace the : with a . so we can reserve the colon for the question
