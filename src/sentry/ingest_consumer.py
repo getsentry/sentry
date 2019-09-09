0148f376b3dc146b6e1610fb9c2074381a14131e
Radu Woinaroski <radu@sentry.io>
2019-09-09 11:46:41 +0200

    fix(ingest-consumer): remove superseded KeyboardInterrupt handling
    we now handle SIGINT and SIGTERM so there shouldn't be any KeyboardInterrupt

--- src/sentry/ingest_consumer.py
+++ src/sentry/ingest_consumer.py
@@ -64,9 +64,10 @@ def set_termination_request_handlers(handler):
     old_sigint = signal.signal(signal.SIGINT, handler)
     old_sigterm = signal.signal(signal.SIGTERM, handler)
     try:
+        # run the code inside the with context ( with the hooked handler)
         yield
     finally:
-        # set back the old handlers
+        # restore the old handlers when exiting the with context
         signal.signal(signal.SIGINT, old_sigint)
         signal.signal(signal.SIGTERM, old_sigterm)
 
@@ -99,67 +100,74 @@ def run_ingest_consumer(
     consumer = _create_consumer(consumer_group, consumer_type, settings)
 
     consumer.subscribe([ConsumerType.get_topic_name(consumer_type, settings)])
+    # setup a flag to mark
+    # see below why we use an array
     termination_signal_received = [False]
 
     def termination_signal_handler(_sig_id, _frame):
+        """
+        Function to use a hook for SIGINT and SIGTERM
+
+        This signal handler only remembers that the signal was emitted.
+        The batch processing loop detects that the signal was emitted
+        and stops once the whole batch is processed.
+        """
+        # We need to use an array so that terminal_signal_received is not a
+        # local variable assignment, but a lookup in the clojure's outer scope.
         termination_signal_received[0] = True
 
-    try:
-        with set_termination_request_handlers(termination_signal_handler):
-            while not (is_shutdown_requested() or termination_signal_received[0]):
-                # get up to commit_batch_size messages
-                messages = consumer.consume(
-                    num_messages=commit_batch_size, timeout=max_batch_time_seconds
+    with set_termination_request_handlers(termination_signal_handler):
+        while not (is_shutdown_requested() or termination_signal_received[0]):
+            # get up to commit_batch_size messages
+            messages = consumer.consume(
+                num_messages=commit_batch_size, timeout=max_batch_time_seconds
+            )
+
+            for message in messages:
+                message_error = message.error()
+                if message_error is not None:
+                    logger.error(
+                        "Received message with error on %s, error:'%s'",
+                        consumer_type,
+                        message_error,
+                    )
+                    raise ValueError(
+                        "Bad message received from consumer", consumer_type, message_error
+                    )
+
+                message = msgpack.unpackb(message.value(), use_list=False)
+                body = message["payload"]
+                start_time = float(message["start_time"])
+                event_id = message["event_id"]
+                project_id = message["project_id"]
+
+                # check that we haven't already processed this event (a previous instance of the forwarder
+                # died before it could commit the event queue offset)
+                deduplication_key = "ev:{}:{}".format(project_id, event_id)
+                if cache.get(deduplication_key) is not None:
+                    logger.warning(
+                        "pre-process-forwarder detected a duplicated event"
+                        " with id:%s for project:%s.",
+                        event_id,
+                        project_id,
+                    )
+                    continue
+
+                cache_key = cache_key_from_project_id_and_event_id(
+                    project_id=project_id, event_id=event_id
+                )
+                cache_timeout = 3600
+                default_cache.set(cache_key, body, cache_timeout, raw=True)
+                preprocess_event.delay(
+                    cache_key=cache_key, start_time=start_time, event_id=event_id
                 )
 
-                for message in messages:
-                    message_error = message.error()
-                    if message_error is not None:
-                        logger.error(
-                            "Received message with error on %s, error:'%s'",
-                            consumer_type,
-                            message_error,
-                        )
-                        raise ValueError(
-                            "Bad message received from consumer", consumer_type, message_error
-                        )
+                # remember for an 1 hour that we saved this event (deduplication protection)
+                cache.set(deduplication_key, "", 3600)
 
-                    message = msgpack.unpackb(message.value(), use_list=False)
-                    body = message["payload"]
-                    start_time = float(message["start_time"])
-                    event_id = message["event_id"]
-                    project_id = message["project_id"]
-
-                    # check that we haven't already processed this event (a previous instance of the forwarder
-                    # died before it could commit the event queue offset)
-                    deduplication_key = "ev:{}:{}".format(project_id, event_id)
-                    if cache.get(deduplication_key) is not None:
-                        logger.warning(
-                            "pre-process-forwarder detected a duplicated event"
-                            " with id:%s for project:%s.",
-                            event_id,
-                            project_id,
-                        )
-                        continue
-
-                    cache_key = cache_key_from_project_id_and_event_id(
-                        project_id=project_id, event_id=event_id
-                    )
-                    cache_timeout = 3600
-                    default_cache.set(cache_key, body, cache_timeout, raw=True)
-                    preprocess_event.delay(
-                        cache_key=cache_key, start_time=start_time, event_id=event_id
-                    )
-
-                    # remember for an 1 hour that we saved this event (deduplication protection)
-                    cache.set(deduplication_key, "", 3600)
-
-                if len(messages) > 0:
-                    # we have read some messages in the previous consume, commit the offset
-                    consumer.commit(asynchronous=False)
-
-    except KeyboardInterrupt:
-        pass
+            if len(messages) > 0:
+                # we have read some messages in the previous consume, commit the offset
+                consumer.commit(asynchronous=False)
 
     logger.debug("Closing ingest-consumer %s...", consumer_type)
     consumer.close()
