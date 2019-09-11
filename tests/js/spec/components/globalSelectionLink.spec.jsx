fdf09663c826e7b951d531bfef0f24087d06258f
Chris Fuller <cfuller@sentry.io>
2019-09-11 17:56:10 -0400

    Adding tests

--- tests/js/spec/components/globalSelectionLink.spec.jsx
+++ tests/js/spec/components/globalSelectionLink.spec.jsx
@@ -47,4 +47,56 @@ describe('GlobalSelectionLink', function() {
 
     expect(wrapper).toMatchSnapshot();
   });
+
+  it('combines query paramaters with custom query', function() {
+    const query = {
+      project: ['foo', 'bar'],
+      environment: 'staging',
+    };
+    const custom_query = {query: '"something"'};
+    const wrapper = shallow(
+      <GlobalSelectionLink to={{pathname: path, query: custom_query}}>
+        Go somewhere!
+      </GlobalSelectionLink>,
+      {
+        context: {
+          location: {
+            query,
+          },
+        },
+      }
+    );
+
+    const updatedToProp = wrapper.find('Link').prop('to');
+
+    expect(updatedToProp).toEqual({
+      pathname: path,
+      query: Object.assign(query, custom_query),
+    });
+
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('combines query paramaters with no query', function() {
+    const query = {
+      project: ['foo', 'bar'],
+      environment: 'staging',
+    };
+    const wrapper = shallow(
+      <GlobalSelectionLink to={{pathname: path}}>Go somewhere!</GlobalSelectionLink>,
+      {
+        context: {
+          location: {
+            query,
+          },
+        },
+      }
+    );
+
+    const updatedToProp = wrapper.find('Link').prop('to');
+
+    expect(updatedToProp).toEqual({pathname: path, query});
+
+    expect(wrapper).toMatchSnapshot();
+  });
 });
