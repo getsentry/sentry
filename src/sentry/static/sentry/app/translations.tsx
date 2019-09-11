75103a94d7da760f48faddda20d02ef4e5458c97
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-09-11 10:55:44 -0700

    chore(ts): Convert translations

--- src/sentry/static/sentry/app/translations.tsx
+++ src/sentry/static/sentry/app/translations.tsx
@@ -1,19 +1,20 @@
 import * as Sentry from '@sentry/browser';
 
 // zh-cn => zh_CN
-function convertToDjangoLocaleFormat(language) {
+function convertToDjangoLocaleFormat(language: string): string {
   const [left, right] = language.split('-');
   return left + (right ? '_' + right.toUpperCase() : '');
 }
 
-export function getTranslations(language) {
+// TODO(ts): The any return type should be the parsed PO object type
+export function getTranslations(language: string): any {
   language = convertToDjangoLocaleFormat(language);
 
   try {
     return require(`sentry-locale/${language}/LC_MESSAGES/django.po`);
   } catch (e) {
     Sentry.withScope(scope => {
-      scope.setLevel('warning');
+      scope.setLevel(Sentry.Severity.Warning);
       scope.setFingerprint(['sentry-locale-not-found']);
       scope.setExtra('locale', language);
       Sentry.captureException(e);
