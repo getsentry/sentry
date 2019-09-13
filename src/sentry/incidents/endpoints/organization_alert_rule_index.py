1a2b77895d9c3bfb1fe9f1fee252395a8ce8ce58
Dan Fuller <dfuller@sentry.io>
2019-09-12 18:32:36 -0700

    feat(api): Implement organization level alert rule endpoints (SEN-989)
    This implements the initial organization level CRUD endpoints. These don't handle the select all
    case, we'll implement that separately since it involves a data model change.

--- src/sentry/incidents/endpoints/organization_alert_rule_index.py
+++ src/sentry/incidents/endpoints/organization_alert_rule_index.py
@@ -1,12 +1,10 @@
 from __future__ import absolute_import
 
-from copy import deepcopy
-
 from rest_framework import status
 from rest_framework.response import Response
 
 from sentry import features
-from sentry.api.bases.project import ProjectEndpoint
+from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.paginator import OffsetPaginator
 from sentry.api.serializers import serialize
@@ -14,35 +12,32 @@ from sentry.incidents.models import AlertRule
 from sentry.incidents.endpoints.serializers import AlertRuleSerializer
 
 
-class ProjectAlertRuleIndexEndpoint(ProjectEndpoint):
-    def get(self, request, project):
+class OrganizationAlertRuleIndexEndpoint(OrganizationEndpoint):
+    def get(self, request, organization):
         """
-        Fetches alert rules for a project
+        Fetches alert rules for an organization
         """
-        if not features.has("organizations:incidents", project.organization, actor=request.user):
+        if not features.has("organizations:incidents", organization, actor=request.user):
             raise ResourceDoesNotExist
 
         return self.paginate(
             request,
-            queryset=AlertRule.objects.fetch_for_project(project),
+            queryset=AlertRule.objects.fetch_for_organization(organization),
             order_by="-date_added",
             paginator_cls=OffsetPaginator,
             on_results=lambda x: serialize(x, request.user),
             default_per_page=25,
         )
 
-    def post(self, request, project):
+    def post(self, request, organization):
         """
         Create an alert rule
         """
-        if not features.has("organizations:incidents", project.organization, actor=request.user):
+        if not features.has("organizations:incidents", organization, actor=request.user):
             raise ResourceDoesNotExist
 
-        data = deepcopy(request.data)
-        data["projects"] = [project.slug]
-
         serializer = AlertRuleSerializer(
-            context={"organization": project.organization, "access": request.access}, data=data
+            context={"organization": organization, "access": request.access}, data=request.data
         )
 
         if serializer.is_valid():
