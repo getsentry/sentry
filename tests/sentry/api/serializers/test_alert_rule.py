23b604fe1a8fe77d1e4aada36fa730251955a531
Dan Fuller <dfuller@sentry.io>
2019-09-13 16:11:34 -0700

    feat(api): Add org level details to the alert rules serializer, and add `DetailedAlertRuleSerializer` (SEN-989)
    This adds `organizationId` to the alert rules model serializer, and adds
    `DetailedAlertRuleSerializer` which includes all related project slugs for the rule.

--- tests/sentry/api/serializers/test_alert_rule.py
+++ tests/sentry/api/serializers/test_alert_rule.py
@@ -5,29 +5,17 @@ from __future__ import absolute_import
 import six
 
 from sentry.api.serializers import serialize
+from sentry.api.serializers.models.alert_rule import DetailedAlertRuleSerializer
 from sentry.incidents.logic import create_alert_rule
 from sentry.incidents.models import AlertRuleThresholdType
 from sentry.snuba.models import QueryAggregations
 from sentry.testutils import TestCase
 
 
-class AlertRuleSerializerTest(TestCase):
-    def test_simple(self):
-        alert_rule = create_alert_rule(
-            self.organization,
-            [self.project],
-            "hello",
-            AlertRuleThresholdType.ABOVE,
-            "level:error",
-            QueryAggregations.TOTAL,
-            10,
-            1000,
-            400,
-            1,
-        )
-        result = serialize(alert_rule)
-
+class BaseAlertRuleSerializerTest(object):
+    def assert_alert_rule_serialized(self, alert_rule, result):
         assert result["id"] == six.text_type(alert_rule.id)
+        assert result["organizationId"] == six.text_type(alert_rule.organization_id)
         assert result["projectId"] == six.text_type(
             alert_rule.query_subscriptions.first().project_id
         )
@@ -43,3 +31,41 @@ class AlertRuleSerializerTest(TestCase):
         assert result["thresholdPeriod"] == alert_rule.threshold_period
         assert result["dateModified"] == alert_rule.date_modified
         assert result["dateAdded"] == alert_rule.date_added
+
+
+class AlertRuleSerializerTest(BaseAlertRuleSerializerTest, TestCase):
+    def test_simple(self):
+        alert_rule = create_alert_rule(
+            self.organization,
+            [self.project],
+            "hello",
+            AlertRuleThresholdType.ABOVE,
+            "level:error",
+            QueryAggregations.TOTAL,
+            10,
+            1000,
+            400,
+            1,
+        )
+        result = serialize(alert_rule)
+        self.assert_alert_rule_serialized(alert_rule, result)
+
+
+class DetailedAlertRuleSerializerTest(BaseAlertRuleSerializerTest, TestCase):
+    def test_simple(self):
+        projects = [self.project, self.create_project()]
+        alert_rule = create_alert_rule(
+            self.organization,
+            projects,
+            "hello",
+            AlertRuleThresholdType.ABOVE,
+            "level:error",
+            QueryAggregations.TOTAL,
+            10,
+            1000,
+            400,
+            1,
+        )
+        result = serialize(alert_rule, serializer=DetailedAlertRuleSerializer())
+        self.assert_alert_rule_serialized(alert_rule, result)
+        assert result["projects"] == [p.slug for p in projects]
