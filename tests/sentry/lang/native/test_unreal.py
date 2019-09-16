4956e733587e2961a7a126c9c116c494d446ded5
Jan Michael Auer <jan.auer@sentry.io>
2019-09-16 08:47:57 +0200

    ref(unreal): Process unreal events in symbolicator (#13863)
    Send apple crash reports to symbolicator, and move the logic for processing them there. Apple crash reports are recognized with a separate attachment type and can be downloaded from the UI as such.

--- tests/sentry/lang/native/test_unreal.py
+++ tests/sentry/lang/native/test_unreal.py
@@ -1,15 +1,15 @@
 from __future__ import absolute_import
 import os
 
+from symbolic import Unreal4Crash
 
 from sentry.testutils import TestCase
 from sentry.lang.native.minidump import MINIDUMP_ATTACHMENT_TYPE
 from sentry.lang.native.unreal import (
-    process_unreal_crash,
+    merge_unreal_user,
     unreal_attachment_type,
     merge_unreal_context_event,
     merge_unreal_logs_event,
-    merge_apple_crash_report,
 )
 from sentry.models import UserReport
 
@@ -25,18 +25,6 @@ def get_fixture_path(name):
 
 def get_unreal_crash_file():
     return get_fixture_path("unreal_crash")
-
-
-def get_unreal_crash_apple_file():
-    return get_fixture_path("unreal_crash_apple")
-
-
-def test_process_minidump():
-    with open(get_unreal_crash_file(), "rb") as f:
-        unreal_crash = process_unreal_crash(f.read(), None, None, {})
-        process_state = unreal_crash.process_minidump()
-        assert 115 == process_state.module_count
-        assert 54 == process_state.thread_count
 
 
 def test_unreal_attachment_type_minidump():
@@ -57,22 +45,12 @@ class MockFile(TestCase):
 class UnrealIntegrationTest(TestCase):
     def test_merge_unreal_context_event(self):
         with open(get_unreal_crash_file(), "rb") as f:
-            event = {"event_id": MOCK_EVENT_ID}
+            event = {"event_id": MOCK_EVENT_ID, "environment": "Production"}
             user_id = "ebff51ef3c4878627823eebd9ff40eb4|2e7d369327054a448be6c8d3601213cb|C52DC39D-DAF3-5E36-A8D3-BF5F53A5D38F"
-            unreal_crash = process_unreal_crash(f.read(), user_id, "Production", event)
+            merge_unreal_user(event, user_id)
+            unreal_crash = Unreal4Crash.from_bytes(f.read())
             merge_unreal_context_event(unreal_crash.get_context(), event, self.project)
             self.insta_snapshot(event)
-
-    def test_merge_unreal_context_event_pcallstack_no_threads(self):
-        event = {}
-        unreal_context = {
-            "runtime_properties": {
-                "portable_call_stack": "0x00000000fc440000 + ffffffff PackageA 0x000000003fb70000 + e23831 PackageA 0x000000003fb70000 + 495d7b PackageA 0x000000003fb70000 + 1cbb89",
-                "threads": [],
-            }
-        }
-        merge_unreal_context_event(unreal_context, event, self.project)
-        assert event.get("threads") is None
 
     def test_merge_unreal_context_event_without_user(self):
         expected_message = "user comments"
@@ -117,8 +95,8 @@ class UnrealIntegrationTest(TestCase):
 
     def test_merge_unreal_logs_event(self):
         with open(get_unreal_crash_file(), "rb") as f:
-            event = {"event_id": MOCK_EVENT_ID}
-            unreal_crash = process_unreal_crash(f.read(), None, None, event)
+            event = {"event_id": MOCK_EVENT_ID, "environment": None}
+            unreal_crash = Unreal4Crash.from_bytes(f.read())
             merge_unreal_logs_event(unreal_crash.get_logs(), event)
             breadcrumbs = event["breadcrumbs"]["values"]
             assert len(breadcrumbs) == 100
@@ -128,10 +106,3 @@ class UnrealIntegrationTest(TestCase):
             assert breadcrumbs[99]["timestamp"] == "2018-11-20T11:47:15Z"
             assert breadcrumbs[99]["message"] == "Texture pool size now 1000 MB"
             assert breadcrumbs[99]["category"] == "LogContentStreaming"
-
-    def test_merge_apple_crash_report(self):
-        with open(get_unreal_crash_apple_file(), "rb") as f:
-            event = {"event_id": MOCK_EVENT_ID}
-            unreal_crash = process_unreal_crash(f.read(), None, None, event)
-            merge_apple_crash_report(unreal_crash.get_apple_crash_report(), event)
-            self.insta_snapshot(event)
