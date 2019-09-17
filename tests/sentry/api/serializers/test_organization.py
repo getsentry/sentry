83186a40c0fd606ed8a33feee07de9ade8313f5f
Jan Michael Auer <jan.auer@sentry.io>
2019-09-17 19:45:46 +0200

    feat(debug_files): Feature flag external symbol servers separately (#14747)

--- tests/sentry/api/serializers/test_organization.py
+++ tests/sentry/api/serializers/test_organization.py
@@ -28,6 +28,7 @@ class OrganizationSerializerTest(TestCase):
                 "sso-basic",
                 "sentry10",
                 "symbol-sources",
+                "custom-symbol-sources",
                 "tweak-grouping-config",
             ]
         )
