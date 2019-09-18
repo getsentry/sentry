d9471628b58ab93126465f8b0c93679fe1369874
Danny Lee <leedongwei@gmail.com>
2019-09-17 21:44:26 -0700

    feat(discover-v2): Add GridEditable component

---
+++ src/sentry/static/sentry/app/components/gridEditable/index.tsx
@@ -0,0 +1,12 @@
+import Grid from './grid';
+import gridModalEditColumn from './gridModalEditColumn';
+
+export default Grid;
+export {
+  GridColumn,
+  GridColumnHeader,
+  GridColumnOrder,
+  GridColumnSortBy,
+} from './gridTypes';
+export {GridEditableProps} from './grid';
+export {gridModalEditColumn};
