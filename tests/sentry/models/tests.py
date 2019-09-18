5474521450d06437bc3abad200bb4e6ee989435d
Lyn Nagara <lyn.nagara@gmail.com>
2019-09-18 13:26:11 -0700

    fix test

--- tests/sentry/models/tests.py
+++ tests/sentry/models/tests.py
@@ -15,10 +15,8 @@ from exam import fixture
 
 from sentry import nodestore
 from sentry.db.models.fields.node import NodeIntegrityFailure
-from sentry.models import ProjectKey, Event, LostPasswordHash
+from sentry.models import ProjectKey, Event, LostPasswordHash, RawEvent
 from sentry.testutils import TestCase
-from sentry.utils.compat import pickle
-from sentry.utils.strings import compress
 
 
 class ProjectKeyTest(TestCase):
@@ -92,59 +90,39 @@ class EventNodeStoreTest(TestCase):
     def test_event_node_id(self):
         # Create an event without specifying node_id. A node_id should be generated
         e1 = Event(project_id=1, event_id="abc", data={"foo": "bar"})
+
         e1.save()
         e1_node_id = e1.data.id
         assert e1.data.id is not None, "We should have generated a node_id for this event"
         e1_body = nodestore.get(e1_node_id)
-        assert e1_body == {"foo": "bar"}, "The event body should be in nodestore"
+        assert e1_body is None, "The event body is not in nodestore"
+
+        # Save event body to nodestore
+        e1.data.save()
 
         e1 = Event.objects.get(project_id=1, event_id="abc")
         assert e1.data.data == {"foo": "bar"}, "The event body should be loaded from nodestore"
         assert e1.data.id == e1_node_id, "The event's node_id should be the same after load"
 
-        # Create another event that references the same nodestore object as the first event.
-        e2 = Event(project_id=1, event_id="def", data={"node_id": e1_node_id})
-        assert e2.data.id == e1_node_id, "The event should use the provided node_id"
-        e2_body = nodestore.get(e1_node_id)
-        assert e2_body == {"foo": "bar"}, "The event body should be in nodestore already"
-        e2.save()
-        e2_body = nodestore.get(e1_node_id)
-        assert e2_body == {"foo": "bar"}, "The event body should not be overwritten by save"
-
-        e2 = Event.objects.get(project_id=1, event_id="def")
-        assert e2.data.data == {"foo": "bar"}, "The event body should be loaded from nodestore"
-        assert e2.data.id == e1_node_id, "The event's node_id should be the same after load"
-
-        # Create an event with a new event body that specifies the node_id to use.
-        e3 = Event(project_id=1, event_id="ghi", data={"baz": "quux", "node_id": "1:ghi"})
-        assert e3.data.id == "1:ghi", "Event should have the specified node_id"
-        assert e3.data.data == {
-            "baz": "quux"
-        }, "Event body should be the one provided (sans node_id)"
-        e3.save()
-        e3_body = nodestore.get("1:ghi")
-        assert e3_body == {"baz": "quux"}, "Event body should be saved to nodestore"
-
-        e3 = Event.objects.get(project_id=1, event_id="ghi")
-        assert e3.data.data == {"baz": "quux"}, "Event body should be loaded from nodestore"
-        assert e3.data.id == "1:ghi", "Loaded event should have the correct node_id"
-
-        # Try load it again, but using the pickled/compressed string we would expect to find
-        # in the column
-        e3_pickled_id = compress(pickle.dumps({"node_id": "1:ghi"}))
-        e3 = Event(project_id=1, event_id="jkl", data=e3_pickled_id)
-        assert e3.data.data == {"baz": "quux"}, "Event body should be loaded from nodestore"
+        e1.save()
+        e1_body = nodestore.get(e1_node_id)
+        assert e1_body == {"foo": "bar"}, "The event body should not be overwritten by save"
 
         # Event with no data should not be saved (or loaded) from nodestore
-        e4 = Event(project_id=1, event_id="mno", data=None)
-        e4.save()
-        assert nodestore.get("1:mno") is None, "We should not have saved anything to nodestore"
-        e4 = Event.objects.get(project_id=1, event_id="mno")
-        assert e4.data.id is None
-        assert e4.data.data == {}  # NodeData returns {} by default
-        Event.objects.bind_nodes([e4], "data")
-        assert e4.data.id is None
-        assert e4.data.data == {}
+        e2 = Event(project_id=1, event_id="def", data=None)
+        e2.save()
+        assert nodestore.get(e2.data.id) is None, "We should not have saved anything to nodestore"
+        e2 = Event.objects.get(project_id=1, event_id="def")
+        assert e2.data.data == {}  # NodeData returns {} by default
+        Event.objects.bind_nodes([e2], "data")
+        assert e2.data.data == {}
+
+        # Raw event still gets automatically saved in nodestore
+        e3 = RawEvent.objects.create(project_id=1, event_id="ghi", data={"foo": "bar"})
+        e3_node_id = e3.data.id
+        assert e3_node_id is not None, "We should have generated a node_id for this event"
+        e3_body = nodestore.get(e3_node_id)
+        assert e3_body == {"foo": "bar"}, "The event body should be in nodestore"
 
     def test_screams_bloody_murder_when_ref_fails(self):
         project1 = self.create_project()
