2831b5f6f645a5953a581410516e3ece2f6454ae
Billy Vong <billy@sentry.io>
2019-09-20 14:23:48 -0700

    remove array.peek

--- src/sentry/static/sentry/app/views/settings/project/serviceHookSettingsForm.jsx
+++ src/sentry/static/sentry/app/views/settings/project/serviceHookSettingsForm.jsx
@@ -64,7 +64,7 @@ export default class ServiceHookSettingsForm extends React.Component {
               {({value, onChange}) => (
                 <MultipleCheckbox
                   onChange={onChange}
-                  value={value.peek()}
+                  value={value}
                   choices={EVENT_CHOICES}
                 />
               )}
