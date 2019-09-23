6e84792b921ed89981bd19e64c1f7c41286698ca
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-09-23 16:20:22 +0200

    fix(relay/config): Send subscope as str (#14804)

--- src/sentry/quotas/redis.py
+++ src/sentry/quotas/redis.py
@@ -113,8 +113,8 @@ class BasicRedisQuota(object):
     def to_json(self):
         return prune_empty_keys(
             {
-                "prefix": self.prefix,
-                "subscope": self.subscope,
+                "prefix": six.text_type(self.prefix),
+                "subscope": six.text_type(self.subscope) if self.subscope is not None else None,
                 "limit": self.limit,
                 "window": self.window,
                 "reasonCode": self.reason_code,
