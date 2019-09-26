ceeb4975bbe3a6d369679d0768baf736a135c0fb
Billy Vong <billy@sentry.io>
2019-09-25 17:12:45 -0700

    ref(ts): Refactor `recreateRoute()` and `replaceRouterParams()` to typescript
    typescript me

--- src/sentry/static/sentry/app/utils/recreateRoute.tsx
+++ src/sentry/static/sentry/app/utils/recreateRoute.tsx
@@ -1,18 +1,36 @@
+import {Location} from 'history';
+import {PlainRoute} from 'react-router';
 import {findLastIndex} from 'lodash';
 
 import replaceRouterParams from 'app/utils/replaceRouterParams';
 
-// Given a route object or a string and a list of routes + params from router, this will attempt to
-// recreate a location string while replacing url params.
-//
-// Can additionally specify the number of routes to move back
-//
-// See tests for examples
+type Options = {
+  routes: Array<PlainRoute>;
+  location: Location;
+  params: any;
+
+  /**
+   * The number of routes to to pop off of `routes
+   * Must be < 0
+   */
+  stepBack?: number;
+};
+
+/**
+ * Given a route object or a string and a list of routes + params from router, this will attempt to recreate a location string while replacing url params.
+ * Can additionally specify the number of routes to move back
+ *
+ * See tests for examples
+ */
+// export default function recreateRoute(to: string | PlainRoute, options: Options): string {
 export default function recreateRoute(to, {routes, params, location, stepBack}) {
+  // const {routes, params, location, stepBack} = options;
   const paths = routes.map(({path}) => path || '');
-  let lastRootIndex;
-  let routeIndex;
+  let lastRootIndex: number;
+  let routeIndex: number | undefined;
   const routeToRoute = typeof to !== 'string';
+
+  // TODO(ts): typescript things
   if (routeToRoute) {
     routeIndex = routes.indexOf(to) + 1;
     lastRootIndex = findLastIndex(paths.slice(0, routeIndex), path => path[0] === '/');
