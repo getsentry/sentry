1930ce74b9f540b4b9e96c57d6a01d7308840dcf
Mark Story <mark@sentry.io>
2019-09-30 16:05:07 -0400

    Remove old table implementation
    Fix all fields being linked. Only the first field should be linked.

--- src/sentry/static/sentry/app/views/eventsV2/table/tableView.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/table/tableView.tsx
@@ -1,18 +1,9 @@
 import React from 'react';
 import {Location} from 'history';
-import {omit} from 'lodash';
-import styled from 'react-emotion';
 
-import {t} from 'app/locale';
 import {Organization} from 'app/types';
-import space from 'app/styles/space';
 
-import Alert from 'app/components/alert';
-import EmptyStateWarning from 'app/components/emptyStateWarning';
 import GridEditable from 'app/components/gridEditable';
-import LoadingContainer from 'app/components/loading/loadingContainer';
-import Panel from 'app/components/panels/panel';
-import Placeholder from 'app/components/placeholder';
 
 import {
   decodeColumnOrder,
@@ -35,9 +26,6 @@ export type TableViewProps = {
   eventView: EventView;
   tableData: TableData | null | undefined;
 };
-export type TableViewState = TableState & {
-  hasFlagQueryBuilder: boolean;
-};
 
 /**
  * `TableView` is currently in turmoil as it is containing 2 implementations
@@ -51,7 +39,7 @@ export type TableViewState = TableState & {
  * is coupled to the `Location` object and derives its state entirely from it.
  * It implements methods to mutate the column state in `Location.query`.
  */
-class TableView extends React.Component<TableViewProps, TableViewState> {
+class TableView extends React.Component<TableViewProps, TableState> {
   constructor(props) {
     super(props);
 
@@ -65,19 +53,13 @@ class TableView extends React.Component<TableViewProps, TableViewState> {
   state = {
     columnOrder: [],
     columnSortBy: [],
-    hasFlagQueryBuilder: false,
-  } as TableViewState;
+  } as TableState;
 
-  static getDerivedStateFromProps(props: TableViewProps): TableViewState {
-    const hasFlagQueryBuilder = props.organization.features.includes(
-      'discover-v2-query-builder'
-    );
-
+  static getDerivedStateFromProps(props: TableViewProps): TableState {
     // Avoid using props.location to get derived state.
     const {eventView} = props;
 
     return {
-      hasFlagQueryBuilder,
       columnOrder: decodeColumnOrder({
         field: eventView.getFieldNames(),
         alias: eventView.getFieldTitles(),
@@ -191,141 +173,21 @@ class TableView extends React.Component<TableViewProps, TableViewState> {
     column: TableColumn<keyof TableDataRow>,
     dataRow: TableDataRow
   ): React.ReactNode => {
-    const {location, organization, tableData} = this.props;
-
+    const {location, organization, tableData, eventView} = this.props;
     if (!tableData) {
       return dataRow[column.key];
     }
+    const hasLinkField = eventView.hasAutolinkField();
+    const forceLink =
+      !hasLinkField && eventView.getFieldNames().indexOf(column.field) === 0;
 
-    const fieldRenderer = getFieldRenderer(String(column.key), tableData.meta, true);
+    const fieldRenderer = getFieldRenderer(String(column.key), tableData.meta, forceLink);
     return fieldRenderer(dataRow, {organization, location});
   };
 
-  renderHeader = () => {
-    const {eventView, location, tableData} = this.props;
-
-    if (eventView.fields.length <= 0) {
-      return null;
-    }
-
-    const defaultSort = eventView.getDefaultSort() || eventView.fields[0].field;
-
-    return eventView.fields.map((field, index) => {
-      if (!tableData) {
-        return <PanelHeaderCell key={index}>{field.title}</PanelHeaderCell>;
-      }
-
-      const {meta} = tableData;
-      const sortKey = eventView.getSortKey(field.field, meta);
-
-      if (sortKey === null) {
-        return <PanelHeaderCell key={index}>{field.title}</PanelHeaderCell>;
-      }
-
-      return (
-        <PanelHeaderCell key={index}>
-          <SortLink
-            defaultSort={defaultSort}
-            sortKey={sortKey}
-            title={field.title}
-            location={location}
-          />
-        </PanelHeaderCell>
-      );
-    });
-  };
-
-  renderContent = (): React.ReactNode => {
-    const {
-      isLoading,
-      tableData: dataPayload,
-      eventView,
-      organization,
-      location,
-    } = this.props;
-
-    if (isLoading && !dataPayload) {
-      return (
-        <PanelGridInfo numOfCols={eventView.numOfColumns()}>
-          <Placeholder height="240px" width="100%" />
-        </PanelGridInfo>
-      );
-    }
-    if (!(dataPayload && dataPayload.data && dataPayload.data.length > 0)) {
-      return (
-        <PanelGridInfo numOfCols={eventView.numOfColumns()}>
-          <EmptyStateWarning>
-            <p>{t('No results found')}</p>
-          </EmptyStateWarning>
-        </PanelGridInfo>
-      );
-    }
-
-    const {meta} = dataPayload;
-    const fields = eventView.getFieldNames();
-    const lastRowIndex = dataPayload.data.length - 1;
-    const hasLinkField = eventView.hasAutolinkField();
-    const firstCellIndex = 0;
-    const lastCellIndex = fields.length - 1;
-
-    return dataPayload.data.map((row, rowIndex) => {
-      return (
-        <React.Fragment key={rowIndex}>
-          {fields.map((field, columnIndex) => {
-            const key = `${columnIndex}.${field}`;
-            const forceLinkField = !hasLinkField && columnIndex === 0;
-
-            const fieldRenderer = getFieldRenderer(field, meta, forceLinkField);
-            return (
-              <PanelItemCell
-                hideBottomBorder={rowIndex === lastRowIndex}
-                style={{
-                  paddingLeft: columnIndex === firstCellIndex ? space(1) : void 0,
-                  paddingRight: columnIndex === lastCellIndex ? space(1) : void 0,
-                }}
-                key={key}
-              >
-                {fieldRenderer(row, {organization, location})}
-              </PanelItemCell>
-            );
-          })}
-        </React.Fragment>
-      );
-    });
-  };
-
-  renderTable() {
-    const {isLoading, tableData: dataPayload} = this.props;
-    return (
-      <React.Fragment>
-        {this.renderHeader()}
-        {isLoading && (
-          <FloatingLoadingContainer
-            isLoading={true}
-            isReloading={isLoading && !!dataPayload}
-          />
-        )}
-        {this.renderContent()}
-      </React.Fragment>
-    );
-  }
-
-  renderError() {
-    const {error, eventView} = this.props;
-    return (
-      <React.Fragment>
-        <Alert type="error" icon="icon-circle-exclamation">
-          {error}
-        </Alert>
-        {this.renderHeader()}
-        <PanelGrid numOfCols={eventView.numOfColumns()}>{this.renderHeader()}</PanelGrid>
-      </React.Fragment>
-    );
-  }
-
   render() {
-    const {eventView, isLoading, error, tableData} = this.props;
-    const {hasFlagQueryBuilder, columnOrder, columnSortBy} = this.state;
+    const {isLoading, error, tableData} = this.props;
+    const {columnOrder, columnSortBy} = this.state;
     const {
       renderModalBodyWithForm,
       renderModalFooter,
@@ -334,124 +196,29 @@ class TableView extends React.Component<TableViewProps, TableViewState> {
       updateColumn: this._updateColumn,
     });
 
-    if (hasFlagQueryBuilder) {
-      return (
-        <GridEditable
-          isEditable={hasFlagQueryBuilder}
-          isLoading={isLoading}
-          error={error}
-          data={tableData ? tableData.data : []}
-          columnOrder={columnOrder}
-          columnSortBy={columnSortBy}
-          grid={{
-            renderHeaderCell: this._renderGridHeaderCell as any,
-            renderBodyCell: this._renderGridBodyCell as any,
-          }}
-          modalEditColumn={{
-            renderBodyWithForm: renderModalBodyWithForm as any,
-            renderFooter: renderModalFooter,
-          }}
-          actions={{
-            deleteColumn: this._deleteColumn,
-            moveColumn: this._moveColumn,
-          }}
-        />
-      );
-    }
-
-    // GridResizable has its own error-handling, but PanelGrid does not.
-    if (error) {
-      return this.renderError();
-    }
-
     return (
-      <PanelGrid numOfCols={eventView.numOfColumns()}>{this.renderTable()}</PanelGrid>
+      <GridEditable
+        isEditable={true}
+        isLoading={isLoading}
+        error={error}
+        data={tableData ? tableData.data : []}
+        columnOrder={columnOrder}
+        columnSortBy={columnSortBy}
+        grid={{
+          renderHeaderCell: this._renderGridHeaderCell as any,
+          renderBodyCell: this._renderGridBodyCell as any,
+        }}
+        modalEditColumn={{
+          renderBodyWithForm: renderModalBodyWithForm as any,
+          renderFooter: renderModalFooter,
+        }}
+        actions={{
+          deleteColumn: this._deleteColumn,
+          moveColumn: this._moveColumn,
+        }}
+      />
     );
   }
 }
 
 export default TableView;
-
-type PanelGridProps = {
-  numOfCols: number;
-};
-const PanelGrid = styled((props: PanelGridProps) => {
-  const otherProps = omit(props, 'numOfCols');
-  return <Panel {...otherProps} />;
-})<PanelGridProps>`
-  display: grid;
-
-  overflow-x: auto;
-
-  ${(props: PanelGridProps) => {
-    const firstColumn = '3fr';
-
-    function generateRestColumns(): string {
-      if (props.numOfCols <= 1) {
-        return '';
-      }
-
-      return `repeat(${props.numOfCols - 1}, auto)`;
-    }
-
-    return `
-      grid-template-columns:  ${firstColumn} ${generateRestColumns()};
-    `;
-  }};
-`;
-
-const PanelHeaderCell = styled('div')`
-  color: ${p => p.theme.gray3};
-  font-size: 13px;
-  font-weight: 600;
-  text-transform: uppercase;
-  border-bottom: 1px solid ${p => p.theme.borderDark};
-  background: ${p => p.theme.offWhite};
-  line-height: 1;
-
-  text-overflow: ellipsis;
-  overflow: hidden;
-  white-space: nowrap;
-
-  padding: ${space(2)};
-
-  /**
-   * By default, a grid item cannot be smaller than the size of its content.
-   * We override this by setting it to be 0.
-   */
-  min-width: 0;
-`;
-
-type PanelGridInfoProps = {
-  numOfCols: number;
-};
-
-const PanelGridInfo = styled('div')<PanelGridInfoProps>`
-  ${(props: PanelGridInfoProps) => {
-    return `grid-column: 1 / span ${props.numOfCols};`;
-  }};
-`;
-
-const PanelItemCell = styled('div')<{hideBottomBorder: boolean}>`
-  border-bottom: ${p =>
-    p.hideBottomBorder ? 'none' : `1px solid ${p.theme.borderLight}`};
-
-  font-size: ${p => p.theme.fontSizeMedium};
-
-  padding-top: ${space(1)};
-  padding-bottom: ${space(1)};
-
-  /**
-   * By default, a grid item cannot be smaller than the size of its content.
-   * We override this by setting it to be 0.
-   */
-  min-width: 0;
-`;
-
-const FloatingLoadingContainer = styled(LoadingContainer)<LoadingContainer['props']>`
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-`;
