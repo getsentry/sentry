7bbdd500d9bc35fa3e9f0bca87e00ac5375c56c1
Mark Story <mark@mark-story.com>
2019-09-30 09:52:50 -0400

    Fix formatting

--- src/sentry/static/sentry/app/components/count.tsx
+++ src/sentry/static/sentry/app/components/count.tsx
@@ -9,9 +9,9 @@ type Props = {
 };
 
 function Count(props: Props) {
-    const {value, className} = props;
+  const {value, className} = props;
 
-    return <span className={className}>{formatAbbreviatedNumber(value)}</span>;
+  return <span className={className}>{formatAbbreviatedNumber(value)}</span>;
 }
 Count.propTypes = {
   value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
