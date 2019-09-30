2dca9c3e64792b2494b78987fc843dad4414679f
Mark Story <mark@mark-story.com>
2019-09-30 10:34:31 -0400

    feat(discover2) Remove the discover query builder flag
    Remove the discover2 query builder flag and only show version=2 queries
    in the sidebar. I've not yet ironed out all the compatibility between
    old and new discover queries. Until that is fixed we'll hide old
    discover queries.
    
    Refs SEN-1060

--- tests/acceptance/test_organization_events_v2.py
+++ tests/acceptance/test_organization_events_v2.py
@@ -8,7 +8,7 @@ from sentry.utils.samples import load_data
 from sentry.testutils.helpers.datetime import iso_format, before_now
 
 
-FEATURE_NAMES = ("organizations:events-v2", "organizations:discover-v2-query-builder")
+FEATURE_NAMES = "organizations:events-v2"
 
 all_view = "field=title&field=event.type&field=project&field=user&field=timestamp&alias=title&alias=type&alias=project&alias=user&alias=time&name=All+Events&sort=-timestamp&tag=event.type&tag=release&tag=project.name&tag=user.email&tag=user.ip&tag=environment"
 error_view = "field=title&alias=error&field=count%28id%29&alias=events&field=count_unique%28user%29&alias=users&field=project&alias=project&field=last_seen&alias=last+seen&name=Errors&query=event.type%3Aerror&sort=-last_seen&sort=-title&tag=error.type&tag=project.name"
