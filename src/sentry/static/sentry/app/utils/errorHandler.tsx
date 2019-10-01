b18033c879d48226fdaadfaf472a3aea7c1628fc
Danny Lee <leedongwei@gmail.com>
2019-09-30 17:58:27 -0700

    feat(errors): Add Sentry.captureException to ErrorHandler

--- src/sentry/static/sentry/app/utils/errorHandler.tsx
+++ src/sentry/static/sentry/app/utils/errorHandler.tsx
@@ -1,29 +1,33 @@
 import React from 'react';
+import * as Sentry from '@sentry/browser';
+
 import RouteError from 'app/views/routeError';
 
 export default function errorHandler(Component) {
   class ErrorHandler extends React.Component {
-    static getDerivedStateFromError(error) {
-      // Update state so the next render will show the fallback UI.
+    // Update state so the next render will show the fallback UI.
+    static getDerivedStateFromError(error: Error) {
       return {
         hasError: true,
         error,
       };
     }
 
+    // we are explicit if an error has been thrown since errors thrown are not guaranteed
+    // to be truthy (e.g. throw null).
     state = {
-      // we are explicit if an error has been thrown since errors thrown are not guaranteed
-      // to be truthy (e.g. throw null).
       hasError: false,
       error: null,
     };
 
-    componentDidCatch(error, info) {
+    componentDidCatch(error: Error, info: {componentStack: string}) {
       // eslint-disable-next-line no-console
       console.error(
         'Component stack trace caught in <ErrorHandler />:',
         info.componentStack
       );
+
+      Sentry.captureException(error);
     }
 
     render() {
