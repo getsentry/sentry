c108f645b6b3a4f6187c7992fe9b92bf3181f0df
davidw1339 <davidw1339@gmail.com>
2019-10-02 11:49:42 -0700

    added endpoint for first event

---
+++ src/sentry/api/endpoints/organization_projects_sent_first_event.py
@@ -0,0 +1,29 @@
+from __future__ import absolute_import
+
+from rest_framework.response import Response
+
+from sentry.models import Project
+from sentry.api.base import DocSection
+from sentry.api.serializers import serialize
+from sentry.api.bases.organization import OrganizationEndpoint
+
+
+class OrganizationProjectsSentFirstEventEndpoint(OrganizationEndpoint):
+    doc_section = DocSection.ORGANIZATIONS
+
+    def get(self, request, organization):
+        """
+        Verify If Any Project Within An Organization Has Received a First Event
+        ```````````````````````````````````````````````````````````````````````
+
+        Returns true if any projects within the organization have received
+        a first event, false otherwise.
+
+        :pparam string organization_slug: the slug of the organization
+                                          containing the projects to check
+                                          for a first event from.
+        :auth: required
+        """
+        queryset = Project.objects.filter(organization=organization, first_event__isnull=False)
+        has_sent_event = len(list(queryset)) > 0
+        return Response(serialize({"sentFirstEvent": has_sent_event}, request.user))
--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -156,6 +156,9 @@ from .endpoints.organization_tags import OrganizationTagsEndpoint
 from .endpoints.organization_teams import OrganizationTeamsEndpoint
 from .endpoints.organization_user_teams import OrganizationUserTeamsEndpoint
 from .endpoints.organization_user_details import OrganizationUserDetailsEndpoint
+from .endpoints.organization_projects_sent_first_event import (
+    OrganizationProjectsSentFirstEventEndpoint,
+)
 from .endpoints.organization_user_issues import OrganizationUserIssuesEndpoint
 from .endpoints.organization_user_issues_search import OrganizationUserIssuesSearchEndpoint
 from .endpoints.organization_user_reports import OrganizationUserReportsEndpoint
@@ -957,6 +960,11 @@ urlpatterns = patterns(
                     r"^(?P<organization_slug>[^\/]+)/user-teams/$",
                     OrganizationUserTeamsEndpoint.as_view(),
                     name="sentry-api-0-organization-user-teams",
+                ),
+                url(
+                    r"^(?P<organization_slug>[^\/]+)/sent-first-event/$",
+                    OrganizationProjectsSentFirstEventEndpoint.as_view(),
+                    name="sentry-api-0-organization-sent-first-event",
                 ),
                 url(
                     r"^(?P<organization_slug>[^\/]+)/tags/$",
