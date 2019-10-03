7bdca4349c5fb8863e66906d6e8a26b13ae5a1e1
josh <josh@jrl.ninja>
2019-10-03 14:16:53 -0700

    py3(drf): bump djangorestframework to 3.1.x (#14881)

--- src/sentry/api/endpoints/user_authenticator_enroll.py
+++ src/sentry/api/endpoints/user_authenticator_enroll.py
@@ -27,6 +27,7 @@ class TotpRestSerializer(serializers.Serializer):
         help_text="Code from authenticator",
         required=True,
         max_length=20,
+        trim_whitespace=False,
     )
 
 
@@ -36,6 +37,7 @@ class SmsRestSerializer(serializers.Serializer):
         help_text="Phone number to send SMS code",
         required=True,
         max_length=20,
+        trim_whitespace=False,
     )
     otp = serializers.CharField(
         label="Authenticator code",
@@ -44,6 +46,7 @@ class SmsRestSerializer(serializers.Serializer):
         allow_null=True,
         allow_blank=True,
         max_length=20,
+        trim_whitespace=False,
     )
 
 
@@ -54,10 +57,11 @@ class U2fRestSerializer(serializers.Serializer):
         allow_null=True,
         allow_blank=True,
         max_length=60,
+        trim_whitespace=False,
         default=lambda: petname.Generate(2, " ", letters=10).title(),
     )
-    challenge = serializers.CharField(required=True)
-    response = serializers.CharField(required=True)
+    challenge = serializers.CharField(required=True, trim_whitespace=False)
+    response = serializers.CharField(required=True, trim_whitespace=False)
 
 
 serializer_map = {"totp": TotpRestSerializer, "sms": SmsRestSerializer, "u2f": U2fRestSerializer}
