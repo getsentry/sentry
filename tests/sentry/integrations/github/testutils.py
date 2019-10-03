eefaaa636e6d82630d4eceb706e318ff2bd0120e
Christian Clauss <cclauss@me.com>
2019-10-03 15:19:48 +0200

    Python 3: bytes can only contain ASCII literal chars (#14924)
    * Python 3: bytes can only contain ASCII literal chars
    
    __SyntaxError: bytes can only contain ASCII literal characters.__ in Python 3 as detected via #14906
    
    The comment says we will use a raw string but the code uses a bytes string.  This PR proposes using an r"string" instead of a b"string".
    
    * Python 3: bytes can only contain ASCII literal chars
    
    __SyntaxError: bytes can only contain ASCII literal characters.__ in Python 3 as detected via #14906
    
    The comment says we will use a raw string but the code uses a bytes string.  This PR proposes using an r"string" instead of a b"string".

--- tests/sentry/integrations/github/testutils.py
+++ tests/sentry/integrations/github/testutils.py
@@ -2,7 +2,7 @@
 from __future__ import absolute_import
 
 # we keep this as a raw string as order matters for hmac signing
-PUSH_EVENT_EXAMPLE_INSTALLATION = b"""{
+PUSH_EVENT_EXAMPLE_INSTALLATION = r"""{
   "ref": "refs/heads/changes",
   "installation" : {
     "id": 12345
