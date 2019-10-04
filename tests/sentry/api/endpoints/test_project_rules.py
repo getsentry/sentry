getsentry/sentry

commit c9d49e2e4074007fff25ef8aeaff1b876ccc789d

feat(workflow): Add availability matches for tag rules

src/sentry/rules/conditions/tagged_event.py
@@ -14,6 +14,8 @@ class MatchType(object):
     ENDS_WITH = "ew"
     CONTAINS = "co"
     NOT_CONTAINS = "nc"
+    IS_SET = "is"
+    NOT_SET = "ns"
 
 
 MATCH_CHOICES = OrderedDict(
@@ -24,6 +26,8 @@ MATCH_CHOICES = OrderedDict(
         (MatchType.ENDS_WITH, "ends with"),
         (MatchType.CONTAINS, "contains"),
         (MatchType.NOT_CONTAINS, "does not contain"),
+        (MatchType.IS_SET, "is set"),
+        (MatchType.NOT_SET, "is not set"),
     ]
 )
 
@@ -49,50 +53,69 @@ class TaggedEventCondition(EventCondition):
         match = self.get_option("match")
         value = self.get_option("value")
 
-        if not (key and match and value):
+        if not (key and match):
             return False
 
-        value = value.lower()
         key = key.lower()
 
         tags = (
+            k
+            for gen in (
+                (k.lower() for k, v in event.tags),
+                (tagstore.get_standardized_key(k) for k, v in event.tags),
+            )
+            for k in gen
+        )
+
+        if match == MatchType.IS_SET:
+            return key in tags
+
+        elif match == MatchType.NOT_SET:
+            return key not in tags
+
+        if not value:
+            return False
+
+        value = value.lower()
+
+        values = (
             v.lower()
             for k, v in event.tags
             if k.lower() == key or tagstore.get_standardized_key(k) == key
         )
 
         if match == MatchType.EQUAL:
-            for t_value in tags:
+            for t_value in values:
                 if t_value == value:
                     return True
             return False
 
         elif match == MatchType.NOT_EQUAL:
-            for t_value in tags:
+            for t_value in values:
                 if t_value == value:
                     return False
             return True
 
         elif match == MatchType.STARTS_WITH:
-            for t_value in tags:
+            for t_value in values:
                 if t_value.startswith(value):
                     return True
             return False
 
         elif match == MatchType.ENDS_WITH:
-            for t_value in tags:
+            for t_value in values:
                 if t_value.endswith(value):
                     return True
             return False
 
         elif match == MatchType.CONTAINS:
-            for t_value in tags:
+            for t_value in values:
                 if value in t_value:
                     return True
             return False
 
         elif match == MatchType.NOT_CONTAINS:
-            for t_value in tags:
+            for t_value in values:
                 if value in t_value:
                     return False
             return True

tests/sentry/rules/conditions/test_tagged_event.py
@@ -88,3 +88,20 @@ class TaggedEventConditionTest(RuleTestCase):
             data={"match": MatchType.NOT_CONTAINS, "key": "logger", "value": "bar.foo"}
         )
         self.assertPasses(rule, event)
+
+    def test_is_set(self):
+        event = self.get_event()
+
+        rule = self.get_rule(data={"match": MatchType.IS_SET, "key": "logger"})
+        self.assertPasses(rule, event)
+
+        rule = self.get_rule(data={"match": MatchType.IS_SET, "key": "missing"})
+        self.assertDoesNotPass(rule, event)
+
+    def test_is_not_set(self):
+        event = self.get_event()
+        rule = self.get_rule(data={"match": MatchType.NOT_SET, "key": "logger"})
+        self.assertDoesNotPass(rule, event)
+
+        rule = self.get_rule(data={"match": MatchType.NOT_SET, "key": "missing"})
+        self.assertPasses(rule, event)

commit 40d5221158cd5ce1329130e374d3f0be603a70e0

fix(alerts): Rules that check if tags is (not) set do not requ… (#14946)
This fixes a bug introduced with #14544 where making a rule with "is set" or "is not set" requires a value (even though logically it does not need one). The UI now hides the field, and the API no longer requires it in these cases.

Closes SEN-1055

src/sentry/rules/conditions/tagged_event.py
@@ -35,7 +35,16 @@ MATCH_CHOICES = OrderedDict(
 class TaggedEventForm(forms.Form):
     key = forms.CharField(widget=forms.TextInput())
     match = forms.ChoiceField(MATCH_CHOICES.items(), widget=forms.Select())
-    value = forms.CharField(widget=forms.TextInput())
+    value = forms.CharField(widget=forms.TextInput(), required=False)
+
+    def clean(self):
+        super(TaggedEventForm, self).clean()
+
+        match = self.cleaned_data.get("match")
+        value = self.cleaned_data.get("value")
+
+        if match not in (MatchType.IS_SET, MatchType.NOT_SET) and not value:
+            raise forms.ValidationError("This field is required.")
 
 
 class TaggedEventCondition(EventCondition):

src/sentry/static/sentry/app/views/settings/projectAlerts/ruleEditor/ruleNode.jsx
@@ -88,6 +88,16 @@ class RuleNode extends React.Component {
       }
 
       const key = part.slice(1, -1);
+
+      // If matcher is "is set" or "is not set", then we do not want to show the value input
+      // because it is not required
+      if (
+        key === 'value' &&
+        (this.props.data.match === 'is' || this.props.data.match === 'ns')
+      ) {
+        return null;
+      }
+
       return formFields[key] ? this.getField(key, formFields[key]) : part;
     });
   }

tests/sentry/api/endpoints/test_project_rules.py
@@ -180,3 +180,59 @@ class CreateProjectRuleTest(APITestCase):
         )
 
         assert response.status_code == 400, response.content
+
+    def test_match_values(self):
+        self.login_as(user=self.user)
+
+        project = self.create_project()
+
+        conditions = [
+            {
+                "id": "sentry.rules.conditions.tagged_event.TaggedEventCondition",
+                "key": "foo",
+                "match": "is",
+            }
+        ]
+
+        actions = [{"id": "sentry.rules.actions.notify_event.NotifyEventAction"}]
+
+        url = reverse(
+            "sentry-api-0-project-rules",
+            kwargs={"organization_slug": project.organization.slug, "project_slug": project.slug},
+        )
+        response = self.client.post(
+            url,
+            data={
+                "name": "hello world",
+                "actionMatch": "any",
+                "actions": actions,
+                "conditions": conditions,
+                "frequency": 30,
+            },
+            format="json",
+        )
+
+        assert response.status_code == 200, response.content
+
+        # should fail if using another match type
+        conditions = [
+            {
+                "id": "sentry.rules.conditions.tagged_event.TaggedEventCondition",
+                "key": "foo",
+                "match": "eq",
+            }
+        ]
+
+        response = self.client.post(
+            url,
+            data={
+                "name": "hello world",
+                "actionMatch": "any",
+                "actions": actions,
+                "conditions": conditions,
+                "frequency": 30,
+            },
+            format="json",
+        )
+
+        assert response.status_code == 400, response.content