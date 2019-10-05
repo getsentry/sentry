d86325890ce38e78aed536fdfa73741387078533
Lyn Nagara <lyn.nagara@gmail.com>
2019-10-04 18:25:13 -0700

    group tagkey details

--- tests/sentry/api/endpoints/test_group_tagkey_details.py
+++ tests/sentry/api/endpoints/test_group_tagkey_details.py
@@ -2,55 +2,24 @@ from __future__ import absolute_import
 
 import six
 
-from django.conf import settings
-
-from sentry import tagstore
 from sentry.models import Group
 from sentry.testutils import APITestCase, SnubaTestCase
+from sentry.testutils.helpers.datetime import iso_format, before_now
 
 
 class GroupTagDetailsTest(APITestCase, SnubaTestCase):
     def test_simple(self):
-
-        if settings.SENTRY_TAGSTORE in [
-            "sentry.tagstore.snuba.SnubaCompatibilityTagStorage",
-            "sentry.tagstore.snuba.SnubaTagStorage",
-        ]:
-            for i in xrange(3):
-                self.store_event(
-                    data={"tags": {"foo": "bar"}, "fingerprint": ["group1"]},
-                    project_id=self.project.id,
-                )
-
-            group = Group.objects.first()
-
-        else:
-            group = self.create_group()
-            group.data["tags"] = (["foo", "bar"],)
-            group.save()
-
-            key, value = group.data["tags"][0]
-            tagstore.create_tag_key(
-                project_id=group.project_id, environment_id=None, key=key, values_seen=2
+        for i in xrange(3):
+            self.store_event(
+                data={
+                    "tags": {"foo": "bar"},
+                    "fingerprint": ["group1"],
+                    "timestamp": iso_format(before_now(seconds=1)),
+                },
+                project_id=self.project.id,
             )
-            tagstore.create_tag_value(
-                project_id=group.project_id, environment_id=None, key=key, value=value, times_seen=4
-            )
-            tagstore.create_group_tag_key(
-                project_id=group.project_id,
-                group_id=group.id,
-                environment_id=None,
-                key=key,
-                values_seen=1,
-            )
-            tagstore.create_group_tag_value(
-                project_id=group.project_id,
-                group_id=group.id,
-                environment_id=None,
-                key=key,
-                value=value,
-                times_seen=3,
-            )
+
+        group = Group.objects.first()
 
         self.login_as(user=self.user)
 
