445491c686aa96bdefc9d61673e7b29bc77030d8
Lyn Nagara <lyn.nagara@gmail.com>
2019-10-07 11:32:37 -0700

    fix renaming

--- src/sentry/api/endpoints/organization_events.py
+++ src/sentry/api/endpoints/organization_events.py
@@ -54,7 +54,8 @@ class OrganizationEventsEndpoint(OrganizationEventsEndpointBase):
                     start=snuba_args["start"],
                     end=snuba_args["end"],
                     conditions=snuba_args["conditions"],
-                    **snuba_args["filter_keys"]
+                    project_ids=snuba_args.get("project_id", None),
+                    group_ids=snuba_args.get("issue", None),
                 ),
             )
 
