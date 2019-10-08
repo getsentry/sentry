2e39d9efa8a5fab3a58a2c6a3e1cafaf233890fd
Lyn Nagara <lyn.nagara@gmail.com>
2019-10-08 12:58:06 -0700

    ref(tagstore): Remove references to legacy tag storage (#14983)

--- src/sentry/tasks/unmerge.py
+++ src/sentry/tasks/unmerge.py
@@ -4,7 +4,6 @@ import logging
 from collections import defaultdict, OrderedDict
 
 from django.db import transaction
-from django.conf import settings
 
 from sentry import eventstore, eventstream, tagstore
 from sentry.app import tsdb
@@ -220,20 +219,9 @@ def migrate_events(
     for event in events:
         event.group = destination
 
-    if settings.SENTRY_TAGSTORE == "sentry.tagstore.legacy.LegacyTagStorage":
-        postgres_id_set = set(
-            Event.objects.filter(project_id=project.id, event_id__in=event_id_set).values_list(
-                "id", flat=True
-            )
-        )
+    event_id_set = set(event.event_id for event in events)
 
-        tagstore.update_group_for_events(
-            project_id=project.id, event_ids=postgres_id_set, destination_id=destination_id
-        )
-
-    event_event_id_set = set(event.event_id for event in events)
-
-    UserReport.objects.filter(project_id=project.id, event_id__in=event_event_id_set).update(
+    UserReport.objects.filter(project_id=project.id, event_id__in=event_id_set).update(
         group=destination_id
     )
 
