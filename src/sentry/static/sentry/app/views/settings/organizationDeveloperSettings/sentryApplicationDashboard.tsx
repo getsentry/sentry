74d414b8f5c3b7e12fb92c9199e2731659278c81
Nola Chen <chen.no@husky.neu.edu>
2019-10-08 09:51:08 -0700

    Remove since/until query args for stats endpoint on frontend

--- src/sentry/static/sentry/app/views/settings/organizationDeveloperSettings/sentryApplicationDashboard.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationDeveloperSettings/sentryApplicationDashboard.tsx
@@ -36,15 +36,8 @@ export default class SentryApplicationDashboard extends AsyncView<Props, State> 
   getEndpoints(): Array<[string, string, any] | [string, string]> {
     const {appSlug} = this.props.params;
     if (appSlug) {
-      // Default time range for now: 90 days ago to now
-      const now = Math.floor(new Date().getTime() / 1000);
-      const ninety_days_ago = 3600 * 24 * 90;
       return [
-        [
-          'stats',
-          `/sentry-apps/${appSlug}/stats/`,
-          {query: {since: now - ninety_days_ago, until: now}},
-        ],
+        ['stats', `/sentry-apps/${appSlug}/stats/`],
         ['errors', `/sentry-apps/${appSlug}/errors/`],
         ['app', `/sentry-apps/${appSlug}/`],
       ];
