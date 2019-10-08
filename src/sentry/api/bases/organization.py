0c44a933c467583d9c9ca7b315527388ed00e0ef
James Cunningham <jtcunning@getsentry.com>
2019-10-08 12:59:38 -0700

    only cache on a valid authn identifier

--- src/sentry/api/bases/organization.py
+++ src/sentry/api/bases/organization.py
@@ -298,14 +298,14 @@ class OrganizationReleasesBaseEndpoint(OrganizationEndpoint):
         on the projects to which the release is attached?
         """
         actor_id = None
+        has_perms = None
         if getattr(request, "user", None) and request.user.id:
             actor_id = "user:%s" % request.user.id
         if getattr(request, "auth", None) and request.auth.id:
             actor_id = "apikey:%s" % request.auth.id
-        if actor_id is None:
-            return False
-        key = "release_perms:1:%s" % hash_values([actor_id, organization.id, release.id])
-        has_perms = cache.get(key)
+        if actor_id is not None:
+            key = "release_perms:1:%s" % hash_values([actor_id, organization.id, release.id])
+            has_perms = cache.get(key)
         if has_perms is None:
             has_perms = ReleaseProject.objects.filter(
                 release=release, project__in=self.get_projects(request, organization)
