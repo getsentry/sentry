ec4a057cbb65bad56316546171e16f7cb713e476
Evan Purkhiser <evanpurkhiser@gmail.com>
2019-10-09 14:21:42 -0700

    ref(ts): Convert locale / translations (#15008)
    * Adds documentation to each translation function to make it much more
       clear how to use them
    
     * Includes some minor variable name cleanup, since these functions
       themselves are already pretty cryptic, this should help future
       readers to understand better what's going on here.
    
     * The types are mostly correct, gettext's return type could potentially
       be a React.ReactNode, however we rarely use it in that way and
       typically just expect a string back.

--- src/sentry/static/sentry/app/translations.tsx
+++ src/sentry/static/sentry/app/translations.tsx
@@ -1,19 +1,19 @@
 import * as Sentry from '@sentry/browser';
 
 // zh-cn => zh_CN
-function convertToDjangoLocaleFormat(language) {
+function convertToDjangoLocaleFormat(language: string) {
   const [left, right] = language.split('-');
   return left + (right ? '_' + right.toUpperCase() : '');
 }
 
-export function getTranslations(language) {
+export function getTranslations(language: string) {
   language = convertToDjangoLocaleFormat(language);
 
   try {
     return require(`sentry-locale/${language}/LC_MESSAGES/django.po`);
   } catch (e) {
     Sentry.withScope(scope => {
-      scope.setLevel('warning');
+      scope.setLevel(Sentry.Severity.Warning);
       scope.setFingerprint(['sentry-locale-not-found']);
       scope.setExtra('locale', language);
       Sentry.captureException(e);
