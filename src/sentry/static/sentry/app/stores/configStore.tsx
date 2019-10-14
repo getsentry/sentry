934fb6e3cdfd3e01291537607670b7141011aa0b
Lyn Nagara <lyn.nagara@gmail.com>
2019-10-14 15:26:33 -0700

    ref(ts): Convert ConfigStore to TypeScript (#15075)
    Also fixes the config features and permissions types which should be
    sets instead of arrays.

--- src/sentry/static/sentry/app/stores/configStore.tsx
+++ src/sentry/static/sentry/app/stores/configStore.tsx
@@ -2,28 +2,29 @@ import moment from 'moment-timezone';
 import Reflux from 'reflux';
 import qs from 'query-string';
 import {setLocale} from 'app/locale';
+import {Config} from 'app/types';
 
 const ConfigStore = Reflux.createStore({
-  init() {
+  init(): void {
     this.config = {};
   },
 
-  get(key) {
+  get(key: string): any {
     return this.config[key];
   },
 
-  set(key, value) {
+  set(key: string, value: any): void {
     this.config[key] = value;
     const out = {};
     out[key] = value;
     this.trigger(out);
   },
 
-  getConfig() {
+  getConfig(): Config {
     return this.config;
   },
 
-  loadInitialData(config) {
+  loadInitialData(config): void {
     config.features = new Set(config.features || []);
     this.config = config;
 
@@ -35,7 +36,7 @@ const ConfigStore = Reflux.createStore({
       config.user.permissions = new Set(config.user.permissions);
       moment.tz.setDefault(config.user.options.timezone);
 
-      let queryString = {};
+      let queryString: qs.ParsedQuery = {};
 
       // Parse query string for `lang`
       try {
@@ -46,7 +47,13 @@ const ConfigStore = Reflux.createStore({
         // e.g. unencoded "%"
       }
 
-      languageCode = queryString.lang || config.user.options.language || languageCode;
+      let queryStringLang = queryString.lang;
+
+      if (Array.isArray(queryStringLang)) {
+        queryStringLang = queryStringLang[0];
+      }
+
+      languageCode = queryStringLang || config.user.options.language || languageCode;
     }
 
     // Priority:
