ce5cc2cb14505185de8e96004a9ca6357035ae05
Joshua Li <josh@jrl.ninja>
2019-10-15 15:08:11 -0700

    remove todo

--- src/sentry/plugins/__init__.py
+++ src/sentry/plugins/__init__.py
@@ -1,8 +1,3 @@
 from __future__ import absolute_import
 
 HIDDEN_PLUGINS = ("bitbucket", "gitlab", "github", "slack", "vsts", "jira", "jira_ac")
-
-# TODO: remove once imports have been moved over
-# from sentry.plugins.base import *  # NOQA
-# from sentry.plugins.bases import *  # NOQA
-# from sentry.plugins.interfaces import *  # NOQA
