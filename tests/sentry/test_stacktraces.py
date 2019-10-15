5564d87f6e8e8cab199b7d6cf447db2dce1e20d7
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-10-15 08:51:52 +0200

    fix: Add test

--- tests/sentry/test_stacktraces.py
+++ tests/sentry/test_stacktraces.py
@@ -1,9 +1,12 @@
 from __future__ import absolute_import
 
+import pytest
+
 from sentry.grouping.api import get_default_grouping_config_dict, load_grouping_config
 from sentry.stacktraces.processing import (
     find_stacktraces_in_data,
     normalize_stacktraces_for_grouping,
+    get_crash_frame_from_event_data,
 )
 from sentry.testutils import TestCase
 
@@ -334,3 +337,19 @@ class NormalizeInApptest(TestCase):
         assert frames[4]["in_app"] is False
         assert frames[5]["in_app"] is True
         assert frames[6]["in_app"] is True
+
+
+@pytest.mark.parametrize(
+    "event",
+    [
+        {"threads": {"values": [{"stacktrace": {"frames": [{"in_app": True, "marco": "polo"}]}}]}},
+        {
+            "exception": {
+                "values": [{"stacktrace": {"frames": [{"in_app": True, "marco": "polo"}]}}]
+            }
+        },
+        {"stacktrace": {"frames": [{"in_app": True, "marco": "polo"}]}},
+    ],
+)
+def test_get_crash_frame(event):
+    assert get_crash_frame_from_event_data(event)["marco"] == "polo"
