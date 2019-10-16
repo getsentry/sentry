86d07326ed549294e1ff1424b2fd6659b7ccf277
Nola Chen <chen.no@husky.neu.edu>
2019-10-16 11:32:49 -0700

    add date filter back to query

--- src/sentry/api/endpoints/sentry_app_stats.py
+++ src/sentry/api/endpoints/sentry_app_stats.py
@@ -22,39 +22,32 @@ class SentryAppStatsEndpoint(SentryAppBaseEndpoint, StatsMixin):
 
         query_args = self._parse_args(request)
 
-        installations = list(
-            SentryAppInstallation.with_deleted.filter(sentry_app=sentry_app).values_list(
-                "date_added", "date_deleted", "organization_id"
-            )
-        )
+        installations = SentryAppInstallation.with_deleted.filter(
+            sentry_app=sentry_app, date_added__range=(query_args["start"], query_args["end"])
+        ).values_list("date_added", "date_deleted", "organization_id")
+        install_count = SentryAppInstallation.with_deleted.filter(sentry_app=sentry_app).count()
+        uninstall_count = SentryAppInstallation.with_deleted.filter(
+            sentry_app=sentry_app, date_deleted__isnull=False
+        ).count()
 
         rollup, series = tsdb.get_optimal_rollup_series(query_args["start"], query_args["end"])
-
-        install_counter = 0
-        uninstall_counter = 0
 
         install_stats = dict.fromkeys(series, 0)
         uninstall_stats = dict.fromkeys(series, 0)
 
         for date_added, date_deleted, organization_id in installations:
-            # Get the total counts for all time
-            install_counter += 1
-            uninstall_counter += 1 if date_deleted is not None else 0
+            install_norm_epoch = tsdb.normalize_to_epoch(date_added, rollup)
+            if install_norm_epoch in install_stats:
+                install_stats[install_norm_epoch] += 1
 
-            # Only gather stats for the installations within the date range
-            if date_added >= query_args["start"] and date_added <= query_args["end"]:
-                install_norm_epoch = tsdb.normalize_to_epoch(date_added, rollup)
-                if install_norm_epoch in install_stats:
-                    install_stats[install_norm_epoch] += 1
-
-                if date_deleted is not None:
-                    uninstall_norm_epoch = tsdb.normalize_to_epoch(date_deleted, rollup)
-                    if uninstall_norm_epoch in uninstall_stats:
-                        uninstall_stats[uninstall_norm_epoch] += 1
+            if date_deleted is not None:
+                uninstall_norm_epoch = tsdb.normalize_to_epoch(date_deleted, rollup)
+                if uninstall_norm_epoch in uninstall_stats:
+                    uninstall_stats[uninstall_norm_epoch] += 1
 
         result = {
-            "total_installs": install_counter,
-            "total_uninstalls": uninstall_counter,
+            "total_installs": install_count,
+            "total_uninstalls": uninstall_count,
             "install_stats": sorted(install_stats.items(), key=lambda x: x[0]),
             "uninstall_stats": sorted(uninstall_stats.items(), key=lambda x: x[0]),
         }
