ba85309f6c7a184c275cb0d30a0793718c72d21f
Alberto Leal <mail4alberto@gmail.com>
2019-10-18 03:59:46 -0400

    fix

--- src/sentry/static/sentry/app/stores/discoverSavedQueriesStore.tsx
+++ src/sentry/static/sentry/app/stores/discoverSavedQueriesStore.tsx
@@ -15,7 +15,7 @@ export type NewQuery = {
   range?: string;
   start?: string;
   end?: string;
-  environment?: string[] | string;
+  environment?: string[];
 };
 
 export type SavedQuery = NewQuery & {
