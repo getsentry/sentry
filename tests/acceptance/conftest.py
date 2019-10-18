529a642a7b760ff8c63eaf8ad880d730fe0838b6
Billy Vong <billyvg@users.noreply.github.com>
2019-10-18 12:39:56 -0700

    test(acceptance): Generate UI assets before running tests (#15028)
    This uses a session fixture to look at all collected tests to determine if any acceptance tests will be run. If so, generate frontend assets using webpack. This *will* run every time a file changes when `py.test` is run with `-f`. This sucks in the case where you are only editing the test cases and not the frontend.

---
+++ tests/acceptance/conftest.py
@@ -0,0 +1,60 @@
+from __future__ import absolute_import
+
+import os
+import json
+import subprocess
+import time
+
+
+def pytest_configure(config):
+    """
+    Generate frontend assets before running any acceptance tests
+
+    TODO: There is a bug if you run `py.test` with `-f` -- the built
+    assets will trigger another `py.test` run.
+    """
+
+    # Do not build in CI because tests are run w/ `make test-acceptance` which builds assets
+    # Can also skip with the env var `SKIP_ACCEPTANCE_UI_BUILD`
+    # `CI` is a default env var on Travis CI (see: https://docs.travis-ci.com/user/environment-variables/#default-environment-variables)
+    if os.environ.get("CI") or os.environ.get("SKIP_ACCEPTANCE_UI_BUILD"):
+        return
+
+    try:
+        with open("./.webpack.meta") as f:
+            data = json.load(f)
+
+            # If built within last hour, do not build again
+            last_built = int(time.time()) - data["built"]
+
+            if last_built <= 3600:
+                print (  # noqa: B314
+                    """
+###################
+#
+# Frontend assets last built %d seconds ago, skipping rebuilds for another %d seconds.
+# Delete the file: `.webpack.meta` to rebuild.
+#
+###################
+                """
+                    % (last_built, 3600 - last_built)
+                )
+                return
+    except IOError:
+        pass
+    except Exception:
+        pass
+
+    print (  # noqa: B314
+        """
+###################
+#
+# Running webpack to compile frontend assets - this will take awhile
+#
+###################
+    """
+    )
+
+    subprocess.call(
+        ["yarn", "webpack"], env={"NODE_ENV": "development", "PATH": os.environ["PATH"]}
+    )
