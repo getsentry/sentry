382fe9d9f659212be0e1f987a04e5443f04ef6c0
Lyn Nagara <lyn.nagara@gmail.com>
2019-10-18 20:29:38 +0000

    feat: Remove custom event_id formatting for transactions dataset (#15128)
    This responsibility is moving into Snuba with
    https://github.com/getsentry/snuba/pull/525. Snuba will always expose
    event_ids as hexadecimal strings.

--- src/sentry/utils/snuba.py
+++ src/sentry/utils/snuba.py
@@ -12,7 +12,6 @@ import re
 import six
 import time
 import urllib3
-import uuid
 
 from concurrent.futures import ThreadPoolExecutor
 from django.conf import settings
@@ -1042,12 +1041,6 @@ def constrain_condition_to_dataset(cond, dataset):
             if name is None:
                 return None
             cond[0] = name
-            # Reformat 32 byte uuids to 36 byte variants.
-            # The transactions dataset requires properly formatted uuid values.
-            # But the rest of sentry isn't aware of that requirement.
-            if dataset == Dataset.Transactions and name == "event_id" and len(cond[2]) == 32:
-                cond[2] = six.text_type(uuid.UUID(cond[2]))
-
             return cond
         if isinstance(cond[0], (list, tuple)):
             if get_function_index(cond[0]) is not None:
