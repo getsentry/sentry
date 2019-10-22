bd880d30882b8fe8a50b3d21147d2e2dc52ce2cb
Alberto Leal <mail4alberto@gmail.com>
2019-10-22 06:05:14 -0400

    asdfasf

--- src/sentry/static/sentry/app/views/eventsV2/utils.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/utils.tsx
@@ -102,9 +102,11 @@ export function fetchTagDistribution(
   key: string,
   query: EventQuery
 ): Promise<Tag> {
-  const urlParams = pick(query, Object.values(URL_PARAM));
+  // const urlParams = pick(query, Object.values(URL_PARAM));
 
-  const queryOption = {...urlParams, key, query: query.query};
+  const queryOption = {...query, key};
+
+  console.log(key, queryOption);
 
   return api.requestPromise(`/organizations/${orgSlug}/events-distribution/`, {
     query: queryOption,
