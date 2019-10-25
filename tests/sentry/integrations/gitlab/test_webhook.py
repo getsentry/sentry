0b4c7ac654d9a1d63b380c3a2ded97f055d5c3b7
Katie Byers <lobsterkatie@gmail.com>
2019-10-25 07:53:29 -0700

    turn off repo renaming for gitlab

--- tests/sentry/integrations/gitlab/test_webhook.py
+++ tests/sentry/integrations/gitlab/test_webhook.py
@@ -273,72 +273,74 @@ class WebhookTest(GitLabTestCase):
         self.assert_pull_request(pull, author)
         self.assert_group_link(group, pull)
 
-    def test_update_repo_name(self):
-        repo_out_of_date_name = self.create_repo(
-            name="Uncool Group / Sentry",  # name out of date
-            url="http://example.com/cool-group/sentry",
-        )
-        repo_out_of_date_name.update(
-            config=dict(repo_out_of_date_name.config, path="cool-group/sentry")
-        )
+    # TODO(kmclb): update the first test, add others back in once repo updating
+    # is fixed
+    # def test_update_repo_name(self):
+    #     repo_out_of_date_name = self.create_repo(
+    #         name="Uncool Group / Sentry",  # name out of date
+    #         url="http://example.com/cool-group/sentry",
+    #     )
+    #     repo_out_of_date_name.update(
+    #         config=dict(repo_out_of_date_name.config, path="cool-group/sentry")
+    #     )
 
-        response = self.client.post(
-            self.url,
-            data=PUSH_EVENT,
-            content_type="application/json",
-            HTTP_X_GITLAB_TOKEN=WEBHOOK_TOKEN,
-            HTTP_X_GITLAB_EVENT="Push Hook",
-        )
+    #     response = self.client.post(
+    #         self.url,
+    #         data=PUSH_EVENT,
+    #         content_type="application/json",
+    #         HTTP_X_GITLAB_TOKEN=WEBHOOK_TOKEN,
+    #         HTTP_X_GITLAB_EVENT="Push Hook",
+    #     )
 
-        assert response.status_code == 204
+    #     assert response.status_code == 204
 
-        # name has been updated
-        repo_out_of_date_name.refresh_from_db()
-        assert repo_out_of_date_name.name == "Cool Group / Sentry"
+    #     # name has been updated
+    #     repo_out_of_date_name.refresh_from_db()
+    #     assert repo_out_of_date_name.name == "Cool Group / Sentry"
 
-    def test_update_repo_path(self):
-        repo_out_of_date_path = self.create_repo(
-            name="Cool Group / Sentry", url="http://example.com/cool-group/sentry"
-        )
-        repo_out_of_date_path.update(
-            config=dict(
-                repo_out_of_date_path.config, path="uncool-group/sentry"  # path out of date
-            )
-        )
+    # def test_update_repo_path(self):
+    #     repo_out_of_date_path = self.create_repo(
+    #         name="Cool Group / Sentry", url="http://example.com/cool-group/sentry"
+    #     )
+    #     repo_out_of_date_path.update(
+    #         config=dict(
+    #             repo_out_of_date_path.config, path="uncool-group/sentry"  # path out of date
+    #         )
+    #     )
 
-        response = self.client.post(
-            self.url,
-            data=PUSH_EVENT,
-            content_type="application/json",
-            HTTP_X_GITLAB_TOKEN=WEBHOOK_TOKEN,
-            HTTP_X_GITLAB_EVENT="Push Hook",
-        )
+    #     response = self.client.post(
+    #         self.url,
+    #         data=PUSH_EVENT,
+    #         content_type="application/json",
+    #         HTTP_X_GITLAB_TOKEN=WEBHOOK_TOKEN,
+    #         HTTP_X_GITLAB_EVENT="Push Hook",
+    #     )
 
-        assert response.status_code == 204
+    #     assert response.status_code == 204
 
-        # path has been updated
-        repo_out_of_date_path.refresh_from_db()
-        assert repo_out_of_date_path.config["path"] == "cool-group/sentry"
+    #     # path has been updated
+    #     repo_out_of_date_path.refresh_from_db()
+    #     assert repo_out_of_date_path.config["path"] == "cool-group/sentry"
 
-    def test_update_repo_url(self):
-        repo_out_of_date_url = self.create_repo(
-            name="Cool Group / Sentry",
-            url="http://example.com/uncool-group/sentry",  # url out of date
-        )
-        repo_out_of_date_url.update(
-            config=dict(repo_out_of_date_url.config, path="cool-group/sentry")
-        )
+    # def test_update_repo_url(self):
+    #     repo_out_of_date_url = self.create_repo(
+    #         name="Cool Group / Sentry",
+    #         url="http://example.com/uncool-group/sentry",  # url out of date
+    #     )
+    #     repo_out_of_date_url.update(
+    #         config=dict(repo_out_of_date_url.config, path="cool-group/sentry")
+    #     )
 
-        response = self.client.post(
-            self.url,
-            data=PUSH_EVENT,
-            content_type="application/json",
-            HTTP_X_GITLAB_TOKEN=WEBHOOK_TOKEN,
-            HTTP_X_GITLAB_EVENT="Push Hook",
-        )
+    #     response = self.client.post(
+    #         self.url,
+    #         data=PUSH_EVENT,
+    #         content_type="application/json",
+    #         HTTP_X_GITLAB_TOKEN=WEBHOOK_TOKEN,
+    #         HTTP_X_GITLAB_EVENT="Push Hook",
+    #     )
 
-        assert response.status_code == 204
+    #     assert response.status_code == 204
 
-        # url has been updated
-        repo_out_of_date_url.refresh_from_db()
-        assert repo_out_of_date_url.url == "http://example.com/cool-group/sentry"
+    #     # url has been updated
+    #     repo_out_of_date_url.refresh_from_db()
+    #     assert repo_out_of_date_url.url == "http://example.com/cool-group/sentry"
