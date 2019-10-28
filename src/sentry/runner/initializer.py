3900ec396fa74afc83bcbaa07f4481da57ae7aaf
josh <josh@jrl.ninja>
2019-10-28 11:42:29 -0700

    py3(django): pytest startup and test collection on Django 1.9 (#15282)

--- src/sentry/runner/initializer.py
+++ src/sentry/runner/initializer.py
@@ -266,9 +266,6 @@ def initialize_app(config, skip_service_validation=False):
 
     if "south" in settings.INSTALLED_APPS:
         fix_south(settings)
-    monkeypatch_django_migrations()
-
-    apply_legacy_settings(settings)
 
     # Commonly setups don't correctly configure themselves for production envs
     # so lets try to provide a bit more guidance
@@ -306,9 +303,11 @@ def initialize_app(config, skip_service_validation=False):
 
     import django
 
-    if hasattr(django, "setup"):
-        # support for Django 1.7+
-        django.setup()
+    django.setup()
+
+    monkeypatch_django_migrations()
+
+    apply_legacy_settings(settings)
 
     bind_cache_to_option_store()
 
