fd1d70bc4c80fff1862a9d25cba8a45570ee5196
Stephen Cefali <scefali@sentry.io>
2019-10-28 14:06:30 -0700

    fix conftest.py

--- conftest.py
+++ conftest.py
@@ -18,6 +18,37 @@ def pytest_configure(config):
     # being used
     warnings.filterwarnings("error", "", Warning, r"^(?!(|kombu|raven|sentry))")
 
+    # if we are running any tests for plugins, we need to make sure we install them first
+    if any("tests/sentry_plugins" in s for s in config.getoption("file_or_dir")):
+        install_sentry_plugins()
+
+
+def install_sentry_plugins():
+    # Sentry's pytest plugin explicitly doesn't load plugins, so let's load all of them
+    # and ignore the fact that we're not *just* testing our own
+    # Note: We could manually register/configure INSTALLED_APPS by traversing our entry points
+    # or package directories, but this is easier assuming Sentry doesn't change APIs.
+    # Note: Order of operations matters here.
+    from sentry.runner.importer import install_plugin_apps
+    from django.conf import settings
+
+    install_plugin_apps("sentry.apps", settings)
+
+    from sentry.runner.initializer import register_plugins
+
+    register_plugins(settings)
+
+    settings.ASANA_CLIENT_ID = "abc"
+    settings.ASANA_CLIENT_SECRET = "123"
+    settings.BITBUCKET_CONSUMER_KEY = "abc"
+    settings.BITBUCKET_CONSUMER_SECRET = "123"
+    settings.GITHUB_APP_ID = "abc"
+    settings.GITHUB_API_SECRET = "123"
+    settings.GITHUB_APPS_APP_ID = "abc"
+    settings.GITHUB_APPS_API_SECRET = "123"
+    # this isn't the real secret
+    settings.SENTRY_OPTIONS["github.integration-hook-secret"] = "b3002c3e321d4b7880360d397db2ccfd"
+
 
 def pytest_collection_modifyitems(items):
     for item in items:
