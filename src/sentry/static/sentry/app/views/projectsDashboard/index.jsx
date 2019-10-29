eceb5a3f3140a05867f9494fd05bc21b2e3021bc
davidw1339 <davidw1339@gmail.com>
2019-10-29 13:15:55 -0700

    removing grid emotion

--- src/sentry/static/sentry/app/views/projectsDashboard/index.jsx
+++ src/sentry/static/sentry/app/views/projectsDashboard/index.jsx
@@ -1,4 +1,3 @@
-import {Flex} from 'grid-emotion';
 import {Link, browserHistory} from 'react-router';
 import LazyLoad from 'react-lazyload';
 import PropTypes from 'prop-types';
@@ -22,9 +21,9 @@ import withOrganization from 'app/utils/withOrganization';
 import withProjects from 'app/utils/withProjects';
 import withTeams from 'app/utils/withTeams';
 
+import getProjectsByTeams from './getProjectsByTeams';
 import Resources from './resources';
 import TeamSection from './teamSection';
-import getProjectsByTeams from './getProjectsByTeams';
 
 class Dashboard extends React.Component {
   static propTypes = {
@@ -131,9 +130,9 @@ class Dashboard extends React.Component {
 
 const OrganizationDashboard = props => {
   return (
-    <Flex flex="1" direction="column">
+    <OrganizationDashboardWrapper>
       <Dashboard {...props} />
-    </Flex>
+    </OrganizationDashboardWrapper>
   );
 };
 
@@ -147,6 +146,12 @@ const ProjectsHeader = styled('div')`
   display: flex;
   align-items: center;
   justify-content: space-between;
+`;
+
+const OrganizationDashboardWrapper = styled('div')`
+  display: flex;
+  flex: 1;
+  flex-direction: column;
 `;
 
 export {Dashboard};
