4dd829e264604dc2194f0fe75dc3d4c85427a803
Billy Vong <billyvg@users.noreply.github.com>
2019-10-29 09:59:52 -0700

    ref(ts): Refactor `app/utils/environment` (#15307)
    Refactor functions in `app/utils/environment` to typescript

--- src/sentry/static/sentry/app/utils/environment.tsx
+++ src/sentry/static/sentry/app/utils/environment.tsx
@@ -1,12 +1,13 @@
+import {Environment} from 'app/types';
 import {toTitleCase} from 'app/utils';
 
 const DEFAULT_EMPTY_ROUTING_NAME = 'none';
 const DEFAULT_EMPTY_ENV_NAME = '(No Environment)';
 
-export function getUrlRoutingName(env) {
+export function getUrlRoutingName(env: Environment) {
   return encodeURIComponent(env.name) || DEFAULT_EMPTY_ROUTING_NAME;
 }
 
-export function getDisplayName(env) {
+export function getDisplayName(env: Environment) {
   return toTitleCase(env.name) || DEFAULT_EMPTY_ENV_NAME;
 }
