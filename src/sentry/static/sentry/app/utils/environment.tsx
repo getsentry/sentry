477ae388f47deb83293581c83f418c159b0b6999
Billy Vong <billy@sentry.io>
2019-10-28 18:20:21 -0700

    ref(ts): Refactor `app/utils/environment`
    Refactor functions in `app/utils/environment` to typescript

--- src/sentry/static/sentry/app/utils/environment.tsx
+++ src/sentry/static/sentry/app/utils/environment.tsx
@@ -3,10 +3,10 @@ import {toTitleCase} from 'app/utils';
 const DEFAULT_EMPTY_ROUTING_NAME = 'none';
 const DEFAULT_EMPTY_ENV_NAME = '(No Environment)';
 
-export function getUrlRoutingName(env) {
+export function getUrlRoutingName(env: Environment) {
   return encodeURIComponent(env.name) || DEFAULT_EMPTY_ROUTING_NAME;
 }
 
-export function getDisplayName(env) {
+export function getDisplayName(env: Environment) {
   return toTitleCase(env.name) || DEFAULT_EMPTY_ENV_NAME;
 }
