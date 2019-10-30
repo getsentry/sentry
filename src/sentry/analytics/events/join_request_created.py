b25899deb6f42435b596f9978ff80cf76c300cfc
maheskett <meg.heskett@gmail.com>
2019-10-29 17:18:41 -0700

    Add fix

--- src/sentry/analytics/events/join_request_created.py
+++ src/sentry/analytics/events/join_request_created.py
@@ -6,7 +6,7 @@ from sentry import analytics
 class JoinRequestCreatedEvent(analytics.Event):
     type = "join_request.created"
 
-    attributes = (analytics.Attribute("member_id"),)
+    attributes = (analytics.Attribute("member_id"), analytics.Attribute("organization_id"))
 
 
 analytics.register(JoinRequestCreatedEvent)
