50c27b8d74e4cc3671d704980f72cef88b0a76aa
Alberto Leal <mail4alberto@gmail.com>
2019-10-30 02:05:00 -0400

    delete saved query

--- src/sentry/static/sentry/app/views/eventsV2/savedQueryButtonGroup.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/savedQueryButtonGroup.tsx
@@ -1,16 +1,23 @@
 import React from 'react';
 import {Location} from 'history';
 import styled from 'react-emotion';
+import {browserHistory} from 'react-router';
 
+import {Client} from 'app/api';
+import {t} from 'app/locale';
 import Button from 'app/components/button';
 import {Organization} from 'app/types';
+import {deleteSavedQuery} from 'app/actionCreators/discoverSavedQueries';
 import {SavedQuery} from 'app/stores/discoverSavedQueriesStore';
+import withApi from 'app/utils/withApi';
 import withDiscoverSavedQueries from 'app/utils/withDiscoverSavedQueries';
+import {addSuccessMessage} from 'app/actionCreators/indicator';
 
 import EventView from './eventView';
 import EventsSaveQueryButton from './saveQueryButton';
 
 type Props = {
+  api: Client;
   eventView: EventView;
   location: Location;
   organization: Organization;
@@ -21,11 +28,33 @@ class SavedQueryButtonGroup extends React.Component<Props> {
   isEditingExistingQuery = (): boolean => {
     const {savedQueries, eventView} = this.props;
 
+    const isValidId = typeof eventView.id === 'string';
+
     const index = savedQueries.findIndex(needle => {
       return needle.id === eventView.id;
     });
 
-    return index >= 0;
+    return index >= 0 && isValidId;
+  };
+
+  deleteQuery = (event: React.MouseEvent<Element>) => {
+    if (!this.isEditingExistingQuery()) {
+      return;
+    }
+
+    const {organization, api, eventView} = this.props;
+    event.preventDefault();
+    event.stopPropagation();
+    deleteSavedQuery(api, organization.slug, eventView.id!).then(() => {
+      addSuccessMessage(t('Query deleted'));
+
+      // redirect to the primary discover2 page
+
+      browserHistory.push({
+        pathname: location.pathname,
+        query: {},
+      });
+    });
   };
 
   renderDeleteButton = () => {
@@ -33,7 +62,7 @@ class SavedQueryButtonGroup extends React.Component<Props> {
       return null;
     }
 
-    return <Button icon="icon-trash" />;
+    return <Button icon="icon-trash" onClick={this.deleteQuery} />;
   };
 
   render() {
@@ -60,4 +89,4 @@ const ButtonGroup = styled('div')`
   }
 `;
 
-export default withDiscoverSavedQueries(SavedQueryButtonGroup);
+export default withApi(withDiscoverSavedQueries(SavedQueryButtonGroup));
