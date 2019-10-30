13a88fef96808b879cc36f5e57f8b00bc95c40eb
Stephen Cefali <scefali@sentry.io>
2019-10-30 15:40:21 -0700

    change comment

--- src/sentry/runner/initializer.py
+++ src/sentry/runner/initializer.py
@@ -20,7 +20,7 @@ def register_plugins(settings, test_plugins=False):
     #         'phabricator = sentry_phabricator.plugins:PhabricatorPlugin'
     #     ],
     # },
-    # TODO (Steve): Remove option for test_plugins after migration
+    # TODO (Steve): Remove option for sentry.new_plugins after migration
     entry_point = "sentry.new_plugins" if test_plugins else "sentry.plugins"
     for ep in iter_entry_points(entry_point):
         try:
