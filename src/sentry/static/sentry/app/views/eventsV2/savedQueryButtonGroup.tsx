64f5342d9393af667b57786b289464d60a71d468
Alberto Leal <mail4alberto@gmail.com>
2019-10-30 01:54:01 -0400

    button group wrapper

--- src/sentry/static/sentry/app/views/eventsV2/savedQueryButtonGroup.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/savedQueryButtonGroup.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import {Location} from 'history';
+import styled from 'react-emotion';
 
+import Button from 'app/components/button';
 import {Organization} from 'app/types';
 import {SavedQuery} from 'app/stores/discoverSavedQueriesStore';
 import withDiscoverSavedQueries from 'app/utils/withDiscoverSavedQueries';
@@ -19,13 +21,22 @@ const SavedQueryButtonGroup = (props: Props) => {
   const {location, organization, eventView, savedQueries} = props;
 
   return (
-    <EventsSaveQueryButton
-      location={location}
-      organization={organization}
-      eventView={eventView}
-      savedQueries={savedQueries}
-    />
+    <ButtonGroup>
+      <Button icon="icon-trash" />
+      <EventsSaveQueryButton
+        location={location}
+        organization={organization}
+        eventView={eventView}
+        savedQueries={savedQueries}
+      />
+    </ButtonGroup>
   );
 };
+
+const ButtonGroup = styled('div')`
+  > * + * {
+    margin-left: 8px;
+  }
+`;
 
 export default withDiscoverSavedQueries(SavedQueryButtonGroup);
