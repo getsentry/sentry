5c56909ffcbe1534a2fa003fa5ed01a0244f2314
Joshua Li <josh@jrl.ninja>
2019-10-31 16:49:02 -0700

    use wait_until in flaky test

--- tests/acceptance/test_organization_developer_settings.py
+++ tests/acceptance/test_organization_developer_settings.py
@@ -2,6 +2,10 @@ from __future__ import absolute_import
 
 from sentry.testutils import AcceptanceTestCase
 
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions
+
 
 class OrganizationDeveloperSettingsNewAcceptanceTest(AcceptanceTestCase):
     """
@@ -35,9 +39,11 @@ class OrganizationDeveloperSettingsNewAcceptanceTest(AcceptanceTestCase):
 
         self.browser.click('[aria-label="Save Changes"]')
 
-        self.browser.wait_until(".ref-success")
-
-        assert self.browser.find_element_by_xpath("//div[contains(text(), 'Client ID')]")
+        WebDriverWait(self.browser, 3).until(
+            expected_conditions.presence_of_element_located(
+                (By.XPATH, "//div[contains(text(), 'Client ID')]")
+            )
+        )
 
     def test_create_new_internal_integration(self):
         self.load_page(self.org_developer_settings_path)
