18accb11275ae8cd7c8713615c14e022131b322b
Nola Chen <chen.no@husky.neu.edu>
2019-10-31 16:55:12 -0700

    refactor buffer code

--- src/sentry/utils/sentryappwebhookrequests.py
+++ src/sentry/utils/sentryappwebhookrequests.py
@@ -15,20 +15,38 @@ BUFFER_SIZE = 100
 KEY_EXPIRY = 60 * 60 * 24 * 30  # 30 days
 
 
-class SentryAppWebhookRequestsBuffer(object):
+class _BaseBuffer(object):
+    """
+    Default dummy implementation of the requests buffer with all public facing methods
+    """
+
+    def __init__(self, sentry_app, client):
+        self.sentry_app = sentry_app
+        self.client = client
+
+    def get_errors(self, event=None):
+        return []
+
+    def get_requests(self, event=None):
+        return []
+
+    def add_request(self, response_code, org_id, event, url):
+        pass
+
+
+class _RedisBuffer(_BaseBuffer):
     """
     Create a data structure to store basic information about Sentry App webhook requests in Redis
     This should store the last 100 requests and last 100 errors (in different keys) for each event type, for each Sentry App
     """
 
-    def __init__(self, sentry_app):
-        self.sentry_app = sentry_app
+    def _get_redis_key(self, event, error=False):
+        sentry_app_id = self.sentry_app.id
 
-        cluster_id = getattr(settings, "SENTRY_WEBHOOK_LOG_REDIS_CLUSTER", None)
-        if cluster_id is None:
-            self.client = redis.clusters.get("default").get_local_client(0)
+        if error:
+            return "sentry-app-webhook-error:{{{0}}}:{1}".format(sentry_app_id, event)
         else:
-            self.client = redis.redis_clusters.get(cluster_id)
+            return "sentry-app-webhook-request:{{{0}}}:{1}".format(sentry_app_id, event)
 
     def _convert_redis_request(self, redis_request, event):
         """
@@ -49,34 +67,6 @@ class SentryAppWebhookRequestsBuffer(object):
         pipeline.lpush(buffer_key, json.dumps(item))
         pipeline.ltrim(buffer_key, 0, BUFFER_SIZE - 1)
         pipeline.expire(buffer_key, KEY_EXPIRY)
-
-    def add_request(self, response_code, org_id, event, url):
-        if event not in VALID_EVENTS:
-            return
-
-        request_key = self._get_redis_key(event)
-
-        time = timezone.now()
-        request_data = {
-            "date": six.binary_type(time),
-            "response_code": response_code,
-            "webhook_url": url,
-        }
-
-        # Don't store the org id for internal apps because it will always be the org that owns the app anyway
-        if not self.sentry_app.is_internal:
-            request_data["organization_id"] = org_id
-
-        pipe = self.client.pipeline()
-
-        self._add_to_buffer_pipeline(request_key, request_data, pipe)
-
-        # If it's an error add it to the error buffer
-        if 400 <= response_code <= 599:
-            error_key = self._get_redis_key(event, error=True)
-            self._add_to_buffer_pipeline(error_key, request_data, pipe)
-
-        pipe.execute()
 
     def _get_all_from_buffer(self, buffer_key, pipeline=None):
         """
@@ -116,15 +106,68 @@ class SentryAppWebhookRequestsBuffer(object):
             ]
 
     def get_errors(self, event=None):
+        if self.client is None:
+            return []
+
         return self._get_requests(event=event, error=True)
 
     def get_requests(self, event=None):
+        if self.client is None:
+            return []
+
         return self._get_requests(event=event, error=False)
 
-    def _get_redis_key(self, event, error=False):
-        sentry_app_id = self.sentry_app.id
+    def add_request(self, response_code, org_id, event, url):
+        if self.client is None:
+            return
 
-        if error:
-            return "sentry-app-webhook-error:{{{0}}}:{1}".format(sentry_app_id, event)
-        else:
-            return "sentry-app-webhook-request:{{{0}}}:{1}".format(sentry_app_id, event)
+        if event not in VALID_EVENTS:
+            return
+
+        request_key = self._get_redis_key(event)
+
+        time = timezone.now()
+        request_data = {
+            "date": six.binary_type(time),
+            "response_code": response_code,
+            "webhook_url": url,
+        }
+
+        # Don't store the org id for internal apps because it will always be the org that owns the app anyway
+        if not self.sentry_app.is_internal:
+            request_data["organization_id"] = org_id
+
+        pipe = self.client.pipeline()
+
+        self._add_to_buffer_pipeline(request_key, request_data, pipe)
+
+        # If it's an error add it to the error buffer
+        if 400 <= response_code <= 599:
+            error_key = self._get_redis_key(event, error=True)
+            self._add_to_buffer_pipeline(error_key, request_data, pipe)
+
+        pipe.execute()
+
+
+class SentryAppWebhookRequestsBuffer(object):
+    """
+    Create and delegate to a buffer to store webhook requests from Sentry Apps to webhooks
+    Only works if redis_clusters is available, otherwise a dummy buffer is created
+    """
+
+    def __init__(self, sentry_app):
+        cluster_id = getattr(settings, "SENTRY_WEBHOOK_LOG_REDIS_CLUSTER", "default")
+        try:
+            client = redis.redis_clusters.get(cluster_id)
+            self.buffer = _RedisBuffer(sentry_app, client)
+        except KeyError:
+            self.buffer = _BaseBuffer(sentry_app, client)
+
+    def get_errors(self, event=None):
+        return self.buffer.get_errors(event=event)
+
+    def get_requests(self, event=None):
+        return self.buffer.get_requests(event=event)
+
+    def add_request(self, response_code, org_id, event, url):
+        return self.buffer.add_request(response_code, org_id, event, url)
