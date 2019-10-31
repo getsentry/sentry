c80c65b08f827543a1b92f14e19facfd526171da
Joshua Li <josh@jrl.ninja>
2019-10-31 13:38:43 -0700

    Revert "do not autoescape this simple_tag test for readability"
    This reverts commit c08909f8de8f4b777699920358d85ced0b0dff99.

--- tests/sentry/templatetags/test_sentry_assets.py
+++ tests/sentry/templatetags/test_sentry_assets.py
@@ -15,13 +15,8 @@ class AssetsTest(TestCase):
     )
 
     def test_supported_foreign_lang(self):
-        # in Django 1.9+, simple_tag escapes the output by default.
-        # To keep this test readable, we set autoescape=False.
-        # If this rendering is safe, then we could mark_safe.
         result = self.TEMPLATE.render(
-            Context(
-                {"request": Mock(LANGUAGE_CODE="fr")}, autoescape=False
-            )  # French, in locale/catalogs.json
+            Context({"request": Mock(LANGUAGE_CODE="fr")})  # French, in locale/catalogs.json
         )
 
         assert '<script src="/_static/{version}/sentry/dist/locale/fr.js"></script>' in result
