e3fe876f19cbcb505877ee6d71c626d5f17c5cac
Chris Fuller <cfuller@sentry.io>
2019-11-03 11:00:52 -0500

    Using ServiceDelegator to run both backends

--- src/sentry/runner/initializer.py
+++ src/sentry/runner/initializer.py
@@ -535,7 +535,8 @@ def validate_snuba():
         return
 
     has_all_snuba_required_backends = (
-        settings.SENTRY_SEARCH == "sentry.search.snuba.SnubaSearchBackend"
+        settings.SENTRY_SEARCH
+        in ("sentry.search.snuba.SnubaSearchBackend", "sentry.utils.services.ServiceDelegator")
         and settings.SENTRY_TAGSTORE == "sentry.tagstore.snuba.SnubaTagStorage"
         and
         # TODO(mattrobenolt): Remove ServiceDelegator check
