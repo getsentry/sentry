ec64e06171888d22f6f291bd82fd6243f4d1c97b
Jan Michael Auer <jan.auer@sentry.io>
2019-11-08 09:58:37 +0100

    test(android): Add an integration test for android issue details

--- tests/acceptance/test_issue_details.py
+++ tests/acceptance/test_issue_details.py
@@ -82,6 +82,11 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
         self.visit_issue(event.group.id)
         self.browser.snapshot("issue details unity")
 
+    def test_android_event(self):
+        event = self.create_sample_event(default="android", platform="java")
+        self.visit_issue(event.group.id)
+        self.browser.snapshot("issue details android")
+
     def test_aspnetcore_event(self):
         event = self.create_sample_event(default="aspnetcore", platform="csharp")
         self.visit_issue(event.group.id)
