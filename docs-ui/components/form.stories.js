c3ed22c87b9ee29a00f21b4921c05f9ab388c0e7
Billy Vong <billyvg@users.noreply.github.com>
2019-11-12 14:25:46 -0800

    feat(ui): Add description to `<RadioGroup>` (#15543)
    This allows `<RadioGroup>`s to have a description, but also changes the styling a bit. Options are no longer bold, and font size is slightly larger, description has smaller text and a lighter color.
    
    Also converts to typescript.

--- docs-ui/components/form.stories.js
+++ docs-ui/components/form.stories.js
@@ -348,7 +348,7 @@ storiesOf('Forms|Fields', module)
         <SelectField
           name="select"
           label="Multi Select"
-          multiple={true}
+          multiple
           choices={[
             ['choice_one', 'Choice One'],
             ['choice_two', 'Choice Two'],
@@ -372,8 +372,8 @@ storiesOf('Forms|Fields', module)
               label={label}
               value={value}
               choices={[
-                ['choice_one', 'Choice One'],
-                ['choice_two', 'Choice Two'],
+                ['choice_one', 'Choice One', 'Description for Choice One'],
+                ['choice_two', 'Choice Two', 'Description for Choice Two'],
                 ['choice_three', 'Choice Three'],
               ]}
             />
--- tests/js/spec/components/forms/radioGroup.spec.jsx
+++ tests/js/spec/components/forms/radioGroup.spec.jsx
@@ -4,72 +4,80 @@ import {mount, shallow} from 'sentry-test/enzyme';
 import RadioGroup from 'app/views/settings/components/forms/controls/radioGroup';
 
 describe('RadioGroup', function() {
-  describe('render()', function() {
-    it('renders', function() {
-      const wrapper = shallow(
-        <RadioGroup
-          name="radio"
-          value="choice_one"
-          choices={[
-            ['choice_one', 'Choice One'],
-            ['choice_two', 'Choice Two'],
-            ['choice_three', 'Choice Three'],
-          ]}
-        />
-      );
-      expect(wrapper).toMatchSnapshot();
-    });
+  it('renders', function() {
+    const mock = jest.fn();
+    const wrapper = shallow(
+      <RadioGroup
+        name="radio"
+        label="test"
+        value="choice_one"
+        choices={[
+          ['choice_one', 'Choice One'],
+          ['choice_two', 'Choice Two'],
+          ['choice_three', 'Choice Three'],
+        ]}
+        onChange={mock}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 
-    it('renders disabled', function() {
-      const wrapper = mount(
-        <RadioGroup
-          name="radio"
-          value="choice_one"
-          disabled
-          choices={[['choice_one', 'Choice One']]}
-        />
-      );
-      expect(wrapper).toMatchSnapshot();
+  it('renders disabled', function() {
+    const mock = jest.fn();
+    const wrapper = mount(
+      <RadioGroup
+        name="radio"
+        label="test"
+        value="choice_one"
+        disabled
+        choices={[['choice_one', 'Choice One']]}
+        onChange={mock}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
 
-      expect(wrapper.find('RadioLineText').props().disabled).toBe(true);
-      expect(wrapper.find('RadioLineButtonFill').props().disabled).toBe(true);
-    });
+    expect(wrapper.find('RadioLineText').props().disabled).toBe(true);
+    expect(wrapper.find('RadioLineButtonFill').props().disabled).toBe(true);
+  });
 
-    it('can select a different item', function() {
-      const wrapper = shallow(
-        <RadioGroup
-          name="radio"
-          value="choice_three"
-          choices={[
-            ['choice_one', 'Choice One'],
-            ['choice_two', 'Choice Two'],
-            ['choice_three', 'Choice Three'],
-          ]}
-        />
-      );
-      expect(wrapper).toMatchSnapshot();
-    });
+  it('can select a different item', function() {
+    const mock = jest.fn();
+    const wrapper = shallow(
+      <RadioGroup
+        name="radio"
+        label="test"
+        value="choice_three"
+        choices={[
+          ['choice_one', 'Choice One'],
+          ['choice_two', 'Choice Two'],
+          ['choice_three', 'Choice Three'],
+        ]}
+        onChange={mock}
+      />
+    );
+    expect(wrapper).toMatchSnapshot();
+  });
 
-    it('calls onChange when clicked', function() {
-      const mock = jest.fn();
+  it('calls onChange when clicked', function() {
+    const mock = jest.fn();
 
-      const wrapper = mount(
-        <RadioGroup
-          name="radio"
-          value="choice_one"
-          choices={[
-            ['choice_one', 'Choice One'],
-            ['choice_two', 'Choice Two'],
-            ['choice_three', 'Choice Three'],
-          ]}
-          onChange={mock}
-        />
-      );
-      wrapper
-        .find('[role="radio"]')
-        .last()
-        .simulate('click');
-      expect(mock).toHaveBeenCalledWith(expect.any(String), expect.any(Object));
-    });
+    const wrapper = mount(
+      <RadioGroup
+        name="radio"
+        label="test"
+        value="choice_one"
+        choices={[
+          ['choice_one', 'Choice One'],
+          ['choice_two', 'Choice Two'],
+          ['choice_three', 'Choice Three'],
+        ]}
+        onChange={mock}
+      />
+    );
+    wrapper
+      .find('[role="radio"]')
+      .last()
+      .simulate('click');
+    expect(mock).toHaveBeenCalledWith(expect.any(String), expect.any(Object));
   });
 });
