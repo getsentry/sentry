5201eb9f2c242aebd5fd4ecbcff5fd4d493ac4fb
Chris Fuller <cfuller@sentry.io>
2019-11-12 18:32:30 -0500

    Removing broken / useless tests

--- src/sentry/search/snuba/backend.py
+++ src/sentry/search/snuba/backend.py
@@ -184,10 +184,10 @@ class SnubaSearchBackend(SearchBackend):
     transaction_name = "SnubaSearchBackend"
 
     def __init__(self):
-        # sentry_sdk.init(
-        #     dsn="http://c634da8347d94f5ca84f0a3964b9b0b5@dev.getsentry.net:8000/3",
-        #     traces_sample_rate=1,
-        # )
+        sentry_sdk.init(
+            dsn="http://c634da8347d94f5ca84f0a3964b9b0b5@dev.getsentry.net:8000/3",
+            traces_sample_rate=1,
+        )
         self.transaction_name = self.__class__.__name__ + "::query"
 
     def query(
@@ -203,7 +203,7 @@ class SnubaSearchBackend(SearchBackend):
         date_from=None,
         date_to=None,
     ):
-        with sentry_sdk.start_span(op="func", transaction=self.transaction_name) as span:
+        with sentry_sdk.start_span(op="func", transaction=self.transaction_name):
             search_filters, paginator_options, retention_window_start = self.initialize_variables(
                 projects,
                 environments,
@@ -249,7 +249,6 @@ class SnubaSearchBackend(SearchBackend):
                 date_to,
             )
 
-            span.finish()
             return results
 
     def initialize_variables(
