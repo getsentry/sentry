49623ba5c47b9624bf4227535a5d61b8abae4b38
Mark Story <mark@sentry.io>
2019-11-14 14:13:30 -0500

    fix(discover) Don't show v2 queries in discover1 (#15586)
    Filter saved queries client side to hide discover 2 queries from
    discover1. Ideally this would be done in the API endpoint but with the
    query being stored in a text field that is more challenging to do.

--- tests/js/spec/views/discover/sidebar/savedQueryList.spec.jsx
+++ tests/js/spec/views/discover/sidebar/savedQueryList.spec.jsx
@@ -30,13 +30,17 @@ describe('savedQueryList', function() {
     const savedQueries = [
       TestStubs.DiscoverSavedQuery({id: '1', name: 'one'}),
       TestStubs.DiscoverSavedQuery({id: '2', name: '2two'}),
+      TestStubs.DiscoverSavedQuery({id: '3', name: 'three v2', version: 2}),
     ];
     mockResponse.push(...savedQueries);
     const wrapper = mount(<SavedQueryList organization={organization} />);
     await tick();
-    savedQueries.forEach(query => {
-      expect(wrapper.text()).toContain(query.name);
-      expect(wrapper.text()).toContain('Updated Sep 24 00:00:00 (UTC)');
-    });
+
+    const text = wrapper.text();
+    expect(text).toContain('Updated Sep 24 00:00:00 (UTC)');
+    expect(text).toContain(savedQueries[0].name);
+    expect(text).toContain(savedQueries[1].name);
+    // v2 queries should not be present.
+    expect(text).not.toContain(savedQueries[2].name);
   });
 });
