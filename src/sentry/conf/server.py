a22a77906ccffb1133954ea5390c0256d05a62ca
Filippo Pacifici <fpacifici@sentry.io>
2019-11-14 09:26:58 -0800

    Upgrade Clickhouse in dev and CI (#15590)
    dev environment and CI are using a quite old version of Clickhouse. Upgrading it to 19.11

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1336,7 +1336,7 @@ SENTRY_DEVSERVICES = {
         "volumes": {"kafka": {"bind": "/var/lib/kafka"}},
     },
     "clickhouse": {
-        "image": "yandex/clickhouse-server:19.3",
+        "image": "yandex/clickhouse-server:19.11",
         "ports": {"9000/tcp": 9000, "9009/tcp": 9009, "8123/tcp": 8123},
         "ulimits": [{"name": "nofile", "soft": 262144, "hard": 262144}],
         "volumes": {"clickhouse": {"bind": "/var/lib/clickhouse"}},
