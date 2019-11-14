593467d4b6b70d3d0b009d719bf39653393a2061
Stephen Cefali <scefali@sentry.io>
2019-11-13 16:04:47 -0800

    chore(ts): convert sentryApplication

--- src/sentry/static/sentry/app/views/settings/components/forms/type.tsx
+++ src/sentry/static/sentry/app/views/settings/components/forms/type.tsx
@@ -31,7 +31,7 @@ type BaseField = {
   placeholder?: string | (() => string);
   multiline?: boolean;
   visible?: boolean | ((props: any) => boolean);
-  disabled?: boolean | (() => boolean);
+  disabled?: boolean | ((props: any) => boolean);
   disabledReason?: string;
   defaultValue?: FieldValue;
 
@@ -54,6 +54,8 @@ type BaseField = {
   setValue?: (value: FieldValue) => any;
 
   onChange?: (value: FieldValue) => void;
+
+  validate?: ({id: String, form: object}) => string[][];
 
   // TODO(ts): FormField prop?
   inline?: boolean;
