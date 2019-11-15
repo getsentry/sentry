4e39903a56785f7970371c19b4823024fa07d6ce
Lyn Nagara <lyn.nagara@gmail.com>
2019-11-15 20:15:48 +0000

    refactor

--- src/sentry/eventstore/utils.py
+++ src/sentry/eventstore/utils.py
@@ -2,12 +2,14 @@ from __future__ import absolute_import
 
 import logging
 
-methods_to_test = [
+get_by_id_methods = [
     "get_next_event_id",
     "get_prev_event_id",
     "get_earliest_event_id",
     "get_latest_event_id",
 ]
+
+methods_to_test = get_by_id_methods
 
 logger = logging.getLogger("sentry.eventstore")
 
@@ -23,14 +25,15 @@ def callback_func(context, method, callargs, backends, results):
     """
     Log if results are different
     """
-    if backends == ["snuba", "snuba_discover"] and method in methods_to_test:
-        if results[0].result() != results[1].result():
-            logger.info(
-                "discover.result-mismatch",
-                extra={
-                    "method": method,
-                    "callargs": callargs,
-                    "snuba": results[0].result(),
-                    "snuba_discover": results[1].result(),
-                },
-            )
+    if backends == ["snuba", "snuba_discover"]:
+        if method in get_by_id_methods:
+            if results[0].result() != results[1].result():
+                logger.info(
+                    "discover.result-mismatch",
+                    extra={
+                        "method": method,
+                        "callargs": callargs,
+                        "snuba": results[0].result(),
+                        "snuba_discover": results[1].result(),
+                    },
+                )
