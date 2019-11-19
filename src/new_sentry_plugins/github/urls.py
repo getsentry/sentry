2e237281af4ea5fe47f6352c6b48c7feb12fcf0c
josh <josh@jrl.ninja>
2019-11-18 16:25:24 -0800

    py3(django): remove patterns which is removed in Django 1.10 (#15652)

--- src/new_sentry_plugins/github/urls.py
+++ src/new_sentry_plugins/github/urls.py
@@ -1,11 +1,10 @@
 from __future__ import absolute_import
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 
 from .endpoints.webhook import GithubIntegrationsWebhookEndpoint, GithubWebhookEndpoint
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     url(r"^organizations/(?P<organization_id>[^\/]+)/webhook/$", GithubWebhookEndpoint.as_view()),
     url(r"^installations/webhook/$", GithubIntegrationsWebhookEndpoint.as_view()),
-)
+]
