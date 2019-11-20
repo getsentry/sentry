19663dc0d380c7d6cdd7cba3d03198c012423e4d
josh <josh@jrl.ninja>
2019-11-20 12:20:32 -0800

    chore: remove unused sentry.utils.query.merge_into (#15703)

--- tests/sentry/utils/query/tests.py
+++ tests/sentry/utils/query/tests.py
@@ -2,20 +2,9 @@ from __future__ import absolute_import
 
 from sentry.models import User
 from sentry.testutils import TestCase
-from sentry.utils.query import merge_into, RangeQuerySetWrapper
+from sentry.utils.query import RangeQuerySetWrapper
 
 from six.moves import xrange
-
-
-class MergeIntoTest(TestCase):
-    def test_all_the_things(self):
-        user_1 = self.create_user("foo@example.com")
-        user_2 = self.create_user("bar@example.com")
-
-        merge_into(user_1, user_2)
-
-        # make sure we didn't remove the instance
-        assert User.objects.filter(id=user_1.id).exists()
 
 
 class RangeQuerySetWrapperTest(TestCase):
