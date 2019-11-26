96aad9707cf918282d65d3c43bb022e0005253ea
Dan Fuller <dfuller@sentry.io>
2019-11-25 17:14:10 -0800

    fix: Make sure that `create_default_projects` doesn't error for single tenant.
    Single tenant sets `SENTRY_PROJECT` to `None`. This means that after every migration they get an
    integrity error, since we try to create the default project and the slug already exists in the
    default org. We guard against this by just setting a default id to fall back to.

--- tests/sentry/receivers/test_core.py
+++ tests/sentry/receivers/test_core.py
@@ -6,7 +6,7 @@ from django.apps import apps
 from django.conf import settings
 
 from sentry.models import Organization, Project, ProjectKey, Team, User
-from sentry.receivers.core import create_default_projects
+from sentry.receivers.core import create_default_projects, DEFAULT_SENTRY_PROJECT_ID
 from sentry.testutils import TestCase
 
 
@@ -54,3 +54,26 @@ class CreateDefaultProjectsTest(TestCase):
 
         # ensure that we dont hit an error here
         create_default_projects(config)
+
+    def test_no_sentry_project(self):
+        with self.settings(SENTRY_PROJECT=None):
+            User.objects.filter(is_superuser=True).delete()
+            Team.objects.filter(slug="sentry").delete()
+            Project.objects.filter(id=DEFAULT_SENTRY_PROJECT_ID).delete()
+            config = apps.get_app_config("sentry")
+
+            create_default_projects(config)
+
+            project = Project.objects.get(id=DEFAULT_SENTRY_PROJECT_ID)
+            assert project.public is False
+            assert project.name == "Internal"
+            assert project.slug == "internal"
+            team = project.teams.first()
+            assert team.slug == "sentry"
+
+            pk = ProjectKey.objects.get(project=project)
+            assert not pk.roles.api
+            assert pk.roles.store
+
+            # ensure that we dont hit an error here
+            create_default_projects(config)
