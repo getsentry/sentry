29068cb2d3f1d658357ac7da9f2feaa8f2bc9c2e
William Mak <william@wmak.io>
2019-11-26 17:24:51 -0500

    fix(environments): Hide environments with name='none'
    - This will hide existing Enviroments with the name none since there's
      now a blacklist for environment names. Introduced in
      getsentry/semaphore#320
    - Fixes SEN-1218
    - Fixes ISSUES-614

---
+++ src/sentry/migrations/0020_hide_environment_none_20191126.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+def hide_environent_none(apps, schema_editor):
+    """
+    Hide environments that are named none, since they're blacklisted and no longer can be created.
+    """
+    EnvironmentProject = apps.get_model("sentry", "EnvironmentProject")
+    EnvironmentProject.objects.filter(environment__name='none').update(is_hidden=True)
+
+
+class Migration(migrations.Migration):
+    # This flag is used to mark that a migration shouldn't be automatically run in
+    # production. We set this to True for operations that we think are risky and want
+    # someone from ops to run manually and monitor.
+    # General advice is that if in doubt, mark your migration as `is_dangerous`.
+    # Some things you should always mark as dangerous:
+    # - Adding indexes to large tables. These indexes should be created concurrently,
+    #   unfortunately we can't run migrations outside of a transaction until Django
+    #   1.10. So until then these should be run manually.
+    # - Large data migrations. Typically we want these to be run manually by ops so that
+    #   they can be monitored. Since data migrations will now hold a transaction open
+    #   this is even more important.
+    # - Adding columns to highly active tables, even ones that are NULL.
+    is_dangerous = False
+
+
+    dependencies = [
+        ("sentry", "0019_auto_20191114_2040"),
+    ]
+
+    operations = [
+        migrations.RunPython(hide_environent_none, migrations.RunPython.noop)
+    ]
