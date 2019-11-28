a59dbdc3bffb8a81c5f8059a9673bd9dcf874987
Alberto Leal <mail4alberto@gmail.com>
2019-11-28 15:01:10 -0500

    add span offsets

--- tests/acceptance/test_organization_events_v2.py
+++ tests/acceptance/test_organization_events_v2.py
@@ -68,7 +68,6 @@ def generate_transaction():
 
     start_datetime = before_now(minutes=1)
     end_datetime = start_datetime + timedelta(milliseconds=500)
-    span_end_datetime = start_datetime + timedelta(milliseconds=250)
 
     def generate_timestamp(date_time):
 
@@ -90,6 +89,18 @@ def generate_transaction():
         "e": {},
     }
 
+    time_offsets = {
+        "a": (timedelta(), timedelta(milliseconds=250)),
+        "aa": (timedelta(milliseconds=10), timedelta(milliseconds=20)),
+        "aaa": (timedelta(milliseconds=15), timedelta(milliseconds=30)),
+        "aaaa": (timedelta(milliseconds=20), timedelta(milliseconds=50)),
+        "aaaaa": (timedelta(milliseconds=25), timedelta(milliseconds=50)),
+        "b": (timedelta(milliseconds=100), timedelta(milliseconds=100)),
+        "c": (timedelta(milliseconds=350), timedelta(milliseconds=50)),
+        "d": (timedelta(milliseconds=375), timedelta(milliseconds=50)),
+        "e": (timedelta(milliseconds=400), timedelta(milliseconds=100)),
+    }
+
     def build_span_tree(span_tree, spans, parent_span_id):
 
         for span_id, child in span_tree.items():
@@ -98,19 +109,30 @@ def generate_transaction():
             # non-leaf node span
             span["parent_span_id"] = parent_span_id.ljust(16, "0")
             span["span_id"] = span_id.ljust(16, "0")
-            span["start_timestamp"] = generate_timestamp(start_datetime)
-            span["timestamp"] = generate_timestamp(span_end_datetime)
+
+            (start_delta, span_length) = time_offsets.get(span_id, (timedelta(), timedelta()))
+
+            span_start_time = start_datetime + start_delta
+            span["start_timestamp"] = generate_timestamp(span_start_time)
+            span["timestamp"] = generate_timestamp(span_start_time + span_length)
             spans.append(span)
 
             if isinstance(child, dict):
                 spans = build_span_tree(child, spans, span_id)
             elif isinstance(child, six.string_types):
+                parent_span_id = span_id
+                span_id = child
+
                 span = copy.deepcopy(reference_span)
                 # leaf node span
-                span["parent_span_id"] = span_id.ljust(16, "0")
-                span["span_id"] = child.ljust(16, "0")
-                span["start_timestamp"] = generate_timestamp(start_datetime)
-                span["timestamp"] = generate_timestamp(span_end_datetime)
+                span["parent_span_id"] = parent_span_id.ljust(16, "0")
+                span["span_id"] = span_id.ljust(16, "0")
+
+                (start_delta, span_length) = time_offsets.get(span_id, (timedelta(), timedelta()))
+
+                span_start_time = start_datetime + start_delta
+                span["start_timestamp"] = generate_timestamp(span_start_time)
+                span["timestamp"] = generate_timestamp(span_start_time + span_length)
                 spans.append(span)
 
         return spans
