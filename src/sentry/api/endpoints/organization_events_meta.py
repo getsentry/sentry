88820a80e356efbef515e487ec2ea1692e9dc5d6
Mark Story <mark@sentry.io>
2019-11-29 15:11:49 -0500

    ref(discover) Update events-meta endpoint to use new discover api (#15870)
    Update the events-meta endpoint to use the new discover API methods.
    This endpoint needed the count() with no additional fields applied, so
    I've added a flag to opt-out of the automatic field additions that are
    required by the main results page.

--- src/sentry/api/endpoints/organization_events_meta.py
+++ src/sentry/api/endpoints/organization_events_meta.py
@@ -3,23 +3,23 @@ from __future__ import absolute_import
 from rest_framework.response import Response
 
 from sentry.api.bases import OrganizationEventsEndpointBase, OrganizationEventsError, NoProjects
-from sentry.utils import snuba
+from sentry.snuba import discover
 
 
 class OrganizationEventsMetaEndpoint(OrganizationEventsEndpointBase):
     def get(self, request, organization):
         try:
             params = self.get_filter_params(request, organization)
-            snuba_args = self.get_snuba_query_args(request, organization, params)
         except OrganizationEventsError as exc:
             return Response({"detail": exc.message}, status=400)
         except NoProjects:
             return Response({"count": 0})
 
-        data = snuba.transform_aliases_and_query(
-            aggregations=[["count()", "", "count"]],
-            referrer="api.organization-event-meta",
-            **snuba_args
-        )["data"][0]
+        result = discover.query(
+            selected_columns=["count()"],
+            params=params,
+            query=request.query_params.get("query"),
+            referrer="api.organization-events-meta",
+        )
 
-        return Response({"count": data["count"]})
+        return Response({"count": result["data"][0]["count"]})
