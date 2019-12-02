2db0f05827c22dbc7f47dca245c5227d4680818d
Jan Michael Auer <account@jauer.org>
2019-12-02 15:10:11 +0100

    Revert "fix(javascript): Cache large sourcemaps on workers (#15848)"
    This reverts commit be20d1149e233881f2107f045379a32940b32c9e.

--- src/sentry/bgtasks/clean_releasefilecache.py
+++
@@ -1,9 +0,0 @@
-from __future__ import absolute_import
-
-from sentry.bgtasks.api import bgtask
-from sentry.models import ReleaseFile
-
-
-@bgtask()
-def clean_releasefilecache():
-    ReleaseFile.cache.clear_old_entries()
