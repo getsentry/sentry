02ad54a77a71280f90bfcc9876bc3831a6627d5f
Anton Ovchinnikov <anton@tonyo.info>
2019-12-02 10:23:25 +0100

    feat: Add a metric to track sizes and hits/misses

--- src/sentry/models/releasefile.py
+++ src/sentry/models/releasefile.py
@@ -10,6 +10,7 @@ from six.moves.urllib.parse import urlsplit, urlunsplit
 from sentry import options
 from sentry.db.models import BoundedPositiveIntegerField, FlexibleForeignKey, Model, sane_repr
 from sentry.models import clear_cached_files
+from sentry.utils import metrics
 from sentry.utils.hashlib import sha1_text
 
 
@@ -92,20 +93,25 @@ class ReleaseFileCache(object):
 
     def getfile(self, releasefile):
         cutoff = options.get("releasefile.cache-limit")
-        if releasefile.file.size < cutoff:
+        file_size = releasefile.file.size
+        if file_size < cutoff:
+            metrics.timer("release_file.cache.get.size", file_size, tags={"cutoff": True})
             return releasefile.file.getfile()
 
         file_id = six.text_type(releasefile.file_id)
         project_id = six.text_type(releasefile.project_id)
         file_path = os.path.join(self.cache_path, project_id, file_id)
 
+        hit = True
         try:
             os.stat(file_path)
         except OSError as e:
             if e.errno != errno.ENOENT:
                 raise
             releasefile.file.save_to(file_path)
+            hit = False
 
+        metrics.timer("release_file.cache.get.size", file_size, tags={"hit": hit, "cutoff": False})
         return open(file_path, "rb")
 
     def clear_old_entries(self):
