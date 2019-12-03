c025cb32bbdd9ed81241406c4e76aa7376d2533b
Katie Byers <katie.byers@sentry.io>
2019-12-03 15:47:01 -0800

    ref(models): Rename `LatestRelease` to better reflect its purpose (#15864)
    Per convos w Cramer, since this model isn't actually used anywhere*, it's a candidate for deletion. In the meantime, this PR clarifies its purpose and how it's used, so that no one else will need to spend the all the time both Matt and I spent trying to figure it out.
    
    *not used = we write to it but never read from it (links in the PR description)

--- src/sentry/models/latestreporeleaseenvironment.py
+++ src/sentry/models/latestreporeleaseenvironment.py
@@ -3,9 +3,10 @@ from __future__ import absolute_import, print_function
 from sentry.db.models import BoundedBigIntegerField, Model, sane_repr
 
 
-class LatestRelease(Model):
+class LatestRepoReleaseEnvironment(Model):
     """
-    Tracks the latest release of a given repository for a given environment.
+    For each environment, tracks the latest release which is associated with
+    commits in the given repo.
     """
 
     __core__ = False
@@ -14,7 +15,9 @@ class LatestRelease(Model):
     # 0 for 'all environments'
     environment_id = BoundedBigIntegerField()
     release_id = BoundedBigIntegerField()
+    # deploy_id and commit_id are nullable but in practice always have a value
     deploy_id = BoundedBigIntegerField(null=True)
+    # commit_id is the id of the ReleaseHeadCommit associated with the given release
     commit_id = BoundedBigIntegerField(null=True)
 
     class Meta:
@@ -22,4 +25,4 @@ class LatestRelease(Model):
         db_table = "sentry_latestrelease"
         unique_together = (("repository_id", "environment_id"),)
 
-    __repr__ = sane_repr("repository_id", "environment_id")
+    __repr__ = sane_repr("repository_id", "environment_id", "release_id")
