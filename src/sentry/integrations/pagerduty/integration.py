68ecfc1a6d7a9983d0757b81ef7e310346c04854
Meredith <meredith@getsentry.com>
2019-12-06 13:44:36 -0800

    ref(pagerduty): Fix typo

--- src/sentry/integrations/pagerduty/integration.py
+++ src/sentry/integrations/pagerduty/integration.py
@@ -62,7 +62,7 @@ class PagerDutyIntegration(IntegrationInstallation):
                 "name": "service_table",
                 "type": "table",
                 "label": "PagerDuty services with the Sentry integration enabled",
-                "help": "If a services needs to be updated, deleted, or added manually please do so here. Alert rules will need to be individually updated for any additions or deletions of services.",
+                "help": "If services needs to be updated, deleted, or added manually please do so here. Alert rules will need to be individually updated for any additions or deletions of services.",
                 "addButtonText": "",
                 "columnLabels": {"service": "Service", "integration_key": "Integration Key"},
                 "columnKeys": ["service", "integration_key"],
