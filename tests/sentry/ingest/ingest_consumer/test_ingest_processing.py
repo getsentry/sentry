6379f76e916145df5d5c047dbc1559a6df174e9b
Markus Unterwaditzer <markus@unterwaditzer.net>
2019-12-09 16:27:10 +0100

    feat: Ingest individual attachments (#16013)
    * feat: Ingest individual attachments
    
    * ref: More realistic attachment IDs in tests
    
    * fix: Differentiate between uninitialized data and missing data

--- tests/sentry/ingest/ingest_consumer/test_ingest_processing.py
+++ tests/sentry/ingest/ingest_consumer/test_ingest_processing.py
@@ -4,9 +4,14 @@ import pytest
 import time
 
 from sentry.utils import json
-from sentry.ingest.ingest_consumer import process_event, process_attachment_chunk
+from sentry.ingest.ingest_consumer import (
+    process_event,
+    process_attachment_chunk,
+    process_individual_attachment,
+)
 from sentry.attachments import attachment_cache
 from sentry.event_manager import EventManager
+from sentry.models import EventAttachment
 
 
 def get_normalized_event(data, project):
@@ -58,6 +63,7 @@ def test_deduplication_works(default_project, task_runner, monkeypatch, preproce
 def test_with_attachments(default_project, task_runner, monkeypatch, preprocess_event):
     payload = get_normalized_event({"message": "hello world"}, default_project)
     event_id = payload["event_id"]
+    attachment_id = "ca90fb45-6dd9-40a0-a18f-8693aa621abb"
     project_id = default_project.id
     start_time = time.time() - 3600
 
@@ -66,7 +72,7 @@ def test_with_attachments(default_project, task_runner, monkeypatch, preprocess_
             "payload": b"Hello ",
             "event_id": event_id,
             "project_id": project_id,
-            "id": 0,
+            "id": attachment_id,
             "chunk_index": 0,
         }
     )
@@ -76,7 +82,7 @@ def test_with_attachments(default_project, task_runner, monkeypatch, preprocess_
             "payload": b"World!",
             "event_id": event_id,
             "project_id": project_id,
-            "id": 0,
+            "id": attachment_id,
             "chunk_index": 1,
         }
     )
@@ -90,7 +96,7 @@ def test_with_attachments(default_project, task_runner, monkeypatch, preprocess_
             "remote_addr": "127.0.0.1",
             "attachments": [
                 {
-                    "id": 0,
+                    "id": attachment_id,
                     "name": "lol.txt",
                     "content_type": "text/plain",
                     "attachment_type": "custom.attachment",
@@ -115,3 +121,54 @@ def test_with_attachments(default_project, task_runner, monkeypatch, preprocess_
     assert att.name == "lol.txt"
     assert att.content_type == "text/plain"
     assert att.type == "custom.attachment"
+
+
+@pytest.mark.django_db
+def test_individual_attachments(default_project, monkeypatch):
+    event_id = "515539018c9b4260a6f999572f1661ee"
+    attachment_id = "ca90fb45-6dd9-40a0-a18f-8693aa621abb"
+    project_id = default_project.id
+
+    process_attachment_chunk(
+        {
+            "payload": b"Hello ",
+            "event_id": event_id,
+            "project_id": project_id,
+            "id": attachment_id,
+            "chunk_index": 0,
+        }
+    )
+
+    process_attachment_chunk(
+        {
+            "payload": b"World!",
+            "event_id": event_id,
+            "project_id": project_id,
+            "id": attachment_id,
+            "chunk_index": 1,
+        }
+    )
+
+    process_individual_attachment(
+        {
+            "type": "attachment",
+            "attachment": {
+                "attachment_type": "event.attachment",
+                "chunks": 2,
+                "content_type": "application/octet-stream",
+                "id": attachment_id,
+                "name": "foo.txt",
+            },
+            "event_id": event_id,
+            "project_id": project_id,
+        }
+    )
+
+    att1, = EventAttachment.objects.filter(project_id=project_id, event_id=event_id).select_related(
+        "file"
+    )
+    assert att1.file.type == "event.attachment"
+    assert att1.file.headers == {"Content-Type": "application/octet-stream"}
+    f = att1.file.getfile()
+    assert f.read() == b"Hello World!"
+    assert f.name == "foo.txt"
