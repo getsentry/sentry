a8fa9ca576340611a7d30a82c1607c6b110df0d7
josh <josh@jrl.ninja>
2019-12-11 13:10:30 -0800

    ref(django 1.10): modernize template rendering (#15936)

--- src/sentry/web/frontend/csrf_failure.py
+++ src/sentry/web/frontend/csrf_failure.py
@@ -11,7 +11,7 @@ from sentry.web.helpers import render_to_response
 class CsrfFailureView(View):
     @method_decorator(csrf_exempt)
     def dispatch(self, request, reason=""):
-        context = {"no_referer": reason == REASON_NO_REFERER, "request": request}
+        context = {"no_referer": reason == REASON_NO_REFERER}
 
         return render_to_response("sentry/403-csrf-failure.html", context, request, status=403)
 
