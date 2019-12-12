eefa5a01f22ac263b966dda9c2a580f808f5b5ba
Alberto Leal <mail4alberto@gmail.com>
2019-12-12 15:05:27 -0800

    revisions

--- src/sentry/static/sentry/app/views/eventsV2/eventDetails/transaction/eventBreakdown.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/eventDetails/transaction/eventBreakdown.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import get from 'lodash/get';
 
+import {t} from 'app/locale';
 import {Event} from 'app/types';
 import {
   SentryTransactionEvent,
   SpanEntry,
   SpanType,
 } from 'app/components/events/interfaces/spans/types';
+
+type EventBreakdownType = {
+  ops: {name: string; count: number; percentage: number; duration: number}[];
+  unknown: {count: number; percentage: number; duration: number} | undefined;
+};
 
 type Props = {
   event: Event;
@@ -23,30 +29,99 @@ class EventBreakdown extends React.Component<Props> {
     return undefined;
   }
 
-  generateCounts(spans: SpanType[]) {
-    // track number of spans with no operation
-    let numOfSpansWithNoOperations = 0;
+  generateCounts(): EventBreakdownType {
+    const event = this.getTransactionEvent();
 
-    const opCounter: {[key: string]: number} = spans.reduce((counter, span: SpanType) => {
-      const op = span.op;
+    if (!event) {
+      return {
+        ops: [],
+        unknown: undefined,
+      };
+    }
 
-      if (typeof op !== 'string') {
-        numOfSpansWithNoOperations += 1;
-        return counter;
+    const spanEntry: SpanEntry | undefined = event.entries.find(
+      (entry: {type: string}) => entry.type === 'spans'
+    );
+
+    const spans: SpanType[] = get(spanEntry, 'data', []);
+
+    // track stats on spans with no operation
+    const spansWithNoOperation = {count: 0, duration: 0};
+
+    const numOfSpans = spans.length;
+
+    type AggregateType = {
+      [opname: string]: {
+        count: number;
+        duration: number; // num of seconds
+      };
+    };
+
+    const aggregateByOp: AggregateType = spans.reduce(
+      (aggregate: AggregateType, span: SpanType) => {
+        const op = span.op;
+        const duration = Math.abs(span.timestamp - span.start_timestamp);
+
+        if (typeof op !== 'string') {
+          spansWithNoOperation.count += 1;
+          spansWithNoOperation.duration += duration;
+
+          return aggregate;
+        }
+        const opStats = aggregate[op];
+
+        if (!opStats) {
+          aggregate[op] = {
+            count: 1,
+            duration,
+          };
+          return aggregate;
+        }
+
+        aggregate[op].count += 1;
+        aggregate[op].duration += duration;
+
+        return aggregate;
+      },
+      {}
+    );
+
+    const ops = Object.keys(aggregateByOp).map(opName => {
+      return {
+        name: opName,
+        count: aggregateByOp[opName].count,
+        percentage: aggregateByOp[opName].count / numOfSpans,
+        duration: aggregateByOp[opName].duration,
+      };
+    });
+
+    // sort ops by most frequently ocurring to least frequently ocurring
+    ops.sort((firstOp, secondOp) => {
+      // sort in descending order based on count
+
+      if (firstOp.count === secondOp.count) {
+        return 0;
       }
 
-      const count = counter[op];
-      if (!count) {
-        counter[op] = 1;
-        return counter;
+      if (firstOp.count > secondOp.count) {
+        return -1;
       }
 
-      counter[op] = count + 1;
+      return 1;
+    });
 
-      return counter;
-    }, {});
-
-    return {opCounter, numOfSpansWithNoOperations};
+    return {
+      // use the first 3 most frequently occurring ops
+      ops: ops.slice(0, 4),
+      unknown:
+        spansWithNoOperation.count > 0
+          ? {
+              count: spansWithNoOperation.count,
+              percentage: spansWithNoOperation.count / numOfSpans,
+              duration: spansWithNoOperation.duration,
+            }
+          : undefined,
+    };
   }
 
   render() {
@@ -55,29 +130,11 @@ class EventBreakdown extends React.Component<Props> {
     if (!event) {
       return null;
     }
-    const spanEntry: SpanEntry | undefined = event.entries.find(
-      (entry: {type: string}) => entry.type === 'spans'
-    );
 
-    const spans: SpanType[] = get(spanEntry, 'data', []);
+    // TODO: Dora to take over
+    // const results = this.generateCounts();
 
-    const results = this.generateCounts(spans);
-    console.log('generateCounts');
-
-    const total = spans.length;
-
-    return (
-      <div>
-        <strong>EventBreakdown</strong>
-        {Object.keys(results.opCounter).map(key => {
-          return (
-            <div>
-              {key} - {(results.opCounter[key] / total) * 100}%
-            </div>
-          );
-        })}
-      </div>
-    );
+    return null;
   }
 }
 
