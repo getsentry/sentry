8a709dc4f9eda35f559e2f29d8958aa5636a4df4
David Wang <davidw1339@gmail.com>
2019-12-11 17:55:40 -0800

    feat(backend): Add additional query params to sent_first_event endpoint (#16066)
    The issues page does a couple operations on organization.projects (all projects) just to check for the existence of projects that have sent a first event. In order to decouple issues from organization.projects I have added some additional query params to mimic the operations that the frontend does on the list of all projects. This modified endpoint will be used instead of fetching organization.projects.

--- src/sentry/api/endpoints/organization_projects_sent_first_event.py
+++ src/sentry/api/endpoints/organization_projects_sent_first_event.py
@@ -22,7 +22,17 @@ class OrganizationProjectsSentFirstEventEndpoint(OrganizationEndpoint):
         :pparam string organization_slug: the slug of the organization
                                           containing the projects to check
                                           for a first event from.
+        :qparam boolean is_member:        An optional boolean to choose to filter on
+                                          projects which the user is a member of.
+        :qparam array[string] project:    An optional list of project ids to filter
         :auth: required
         """
+        is_member = request.GET.get("is_member")
+        project_ids = set(map(int, request.GET.getlist("project")))
         queryset = Project.objects.filter(organization=organization, first_event__isnull=False)
+        if is_member:
+            queryset = queryset.filter(teams__organizationmember__user=request.user)
+        if project_ids:
+            queryset = queryset.filter(id__in=project_ids)
+
         return Response(serialize({"sentFirstEvent": queryset.count() > 0}, request.user))
