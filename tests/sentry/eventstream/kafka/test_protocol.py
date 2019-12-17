e031c1bbffce139e0d19c2d0511e830045efdb3f
Lyn Nagara <lyn.nagara@gmail.com>
2019-12-16 18:56:25 -0800

    ref: Use SnubaEvent model for eventstream
    Use SnubaEvent instead of deprecated event model in eventstream. We need
    to remove this Event model in order to remove the sentry_message table.

--- tests/sentry/eventstream/kafka/test_protocol.py
+++ tests/sentry/eventstream/kafka/test_protocol.py
@@ -45,7 +45,7 @@ def test_get_task_kwargs_for_message_version_1():
     assert event.event_id == "00000000000010008080808080808080"
     assert event.message == "message"
     assert event.platform == "python"
-    assert event.datetime == datetime(2018, 7, 20, 21, 4, 27, 600640, tzinfo=pytz.utc)
+    assert event.datetime == datetime(2018, 7, 20, 21, 4, 27, tzinfo=pytz.utc)
     assert dict(event.data) == {}
 
     assert kwargs.pop("primary_hash") == "49f68a5c8493ec2c0bf489821c21fc3b"
