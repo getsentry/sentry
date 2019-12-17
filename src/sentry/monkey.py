3d75a9dd8d182d29c3a7fd581cfacf4d36e810c5
William Mak <william@wmak.io>
2019-12-17 16:58:24 -0500

    temp: Removing `patch_model_unpickle`
    - Will add this back in after deploying these functions independently
      first.

--- src/sentry/monkey.py
+++ src/sentry/monkey.py
@@ -120,10 +120,5 @@ def patch_model_unpickle():
     django.db.models.base.simple_class_factory = simple_class_factory_compat
 
 
-for patch in (
-    patch_parse_cookie,
-    patch_httprequest_repr,
-    patch_django_views_debug,
-    patch_model_unpickle,
-):
+for patch in (patch_parse_cookie, patch_httprequest_repr, patch_django_views_debug):
     patch()
