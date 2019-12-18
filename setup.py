6196ed7b584c671f7b631c27367a7c5d2a3b6a3b
Joshua Li <josh@jrl.ninja>
2019-12-18 15:03:49 -0800

    nothing seems to use the postgres extra

--- setup.py
+++ setup.py
@@ -134,7 +134,7 @@ setup(
     packages=find_packages("src"),
     zip_safe=False,
     install_requires=install_requires,
-    extras_require={"dev": dev_requires, "postgres": []},
+    extras_require={"dev": dev_requires},
     cmdclass=cmdclass,
     license="BSL-1.1",
     include_package_data=True,
