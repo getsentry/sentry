00e790d2ffa13110109ab27b3e92b8dc8624d8d0
josh <josh@jrl.ninja>
2019-12-19 10:58:52 -0800

    ref: revise setup.py, remove stray license headers (#16174)

--- src/sentry/management/commands/serve_normalize.py
+++ src/sentry/management/commands/serve_normalize.py
@@ -1,9 +1,3 @@
-"""
-sentry.management.commands.serve_normalize
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-:copyright: (c) 2018 by the Sentry Team, see AUTHORS for more details.
-:license: BSD, see LICENSE for more details.
-"""
 from __future__ import absolute_import, print_function
 
 import SocketServer
