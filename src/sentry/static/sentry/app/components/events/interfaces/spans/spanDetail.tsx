a8940b5f70eecf525fcca1f3318bf4320c6511d3
Alberto Leal <mail4alberto@gmail.com>
2019-12-19 14:16:47 -0500

    ref(apm): Update names of queries generated by "Search by trace" and "View Children" (#16177)

--- src/sentry/static/sentry/app/components/events/interfaces/spans/spanDetail.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/spanDetail.tsx
@@ -128,7 +128,7 @@ class SpanDetail extends React.Component<Props, State> {
 
     const eventView = EventView.fromSavedQuery({
       id: undefined,
-      name: t('Transactions'),
+      name: `Children from Span ID ${span.span_id}`,
       fields: [
         'transaction',
         'project',
@@ -170,7 +170,7 @@ class SpanDetail extends React.Component<Props, State> {
 
     const eventView = EventView.fromSavedQuery({
       id: undefined,
-      name: t('Transactions'),
+      name: `Transactions with Trace ID ${span.trace_id}`,
       fields: [
         'transaction',
         'project',
