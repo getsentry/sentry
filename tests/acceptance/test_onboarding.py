49fa380b4efe5d3931ce37e5ead9f33e9c8657a6
Billy Vong <billy@sentry.io>
2019-12-22 16:51:09 -0800

    test(acceptance): Fix onboarding test...
    ...this seems to fail to create an initial project

--- tests/acceptance/test_onboarding.py
+++ tests/acceptance/test_onboarding.py
@@ -10,7 +10,7 @@ class OrganizationOnboardingTest(AcceptanceTestCase):
     def setUp(self):
         super(OrganizationOnboardingTest, self).setUp()
         self.user = self.create_user("foo@example.com")
-        self.org = self.create_organization(name="Rowdy Tiger", owner=None)
+        self.org = self.create_organization(name="Rowdy Tiger", owner=self.user)
         self.team = self.create_team(organization=self.org, name="Mariachi Band")
         self.member = self.create_member(
             user=self.user, organization=self.org, role="owner", teams=[self.team]
