882bd387c1d1b4603a4ccbb20e8451b8750080de
Billy Vong <billy@sentry.io>
2020-01-02 14:16:27 -0800

    change selector

--- tests/acceptance/test_onboarding.py
+++ tests/acceptance/test_onboarding.py
@@ -48,5 +48,6 @@ class OrganizationOnboardingTest(AcceptanceTestCase):
         self.browser.snapshot(name="onboarding - invite members")
 
         self.browser.click('[data-test-id="onboarding-getting-started-learn-more"]')
-        self.browser.wait_until('input[name="email"]')
+        self.browser.wait_until('[name="email"]')
+
         self.browser.snapshot(name="onboarding - learn more")
