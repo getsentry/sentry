aad29a4f99e0f3bf5c69f71bbda461d694cd45f9
Pierre du Plessis <pierre@pcservice.co.za>
2020-01-03 12:59:52 +0200

    Fix patterns config

--- src/sentry/integrations/bitbucket_server/urls.py
+++ src/sentry/integrations/bitbucket_server/urls.py
@@ -1,13 +1,12 @@
 from __future__ import absolute_import
 
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from .webhook import BitbucketServerWebhookEndpoint
 
-urlpatterns = patterns(
-    "",
+urlpatterns = [
     url(
         r"^organizations/(?P<organization_id>[^\/]+)/webhook/(?P<integration_id>\d+)/$",
         BitbucketServerWebhookEndpoint.as_view(),
         name="sentry-extensions-bitbucketserver-webhook",
-    ),
-)
+    )
+]
