2946d0ed94c5ff6242a4c368a9ea9618775bf05f
Lyn Nagara <lyn.nagara@gmail.com>
2020-01-03 13:42:12 -0800

    revert message change

--- src/sentry/models/event.py
+++ src/sentry/models/event.py
@@ -493,9 +493,7 @@ class SnubaEvent(EventCommon):
     def message(self):
         if "message" in self.snuba_data:
             return self.snuba_data["message"]
-        from sentry.event_manager import EventManager
-        event_manager = EventManager(self.data.data)
-        return event_manager.get_search_message()
+        return self.data.get("message")
 
     @property
     def platform(self):
