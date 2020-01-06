c789fd2d3b274b56648d23cb76557b1e64b9c279
Billy Vong <billyvg@users.noreply.github.com>
2020-01-06 15:37:09 -0800

    test(percy): Fix percy snapshot for "Onboarding - learn more" (#16241)
    Add a wait until condition for the new form to appear before snapshotting, otherwise it could snapshot before the tab change fully completes.
    
    See https://percy.io/getsentry/sentry/builds/3616281\?utm_campaign\=getsentry\&utm_content\=sentry\&utm_source\=github_status_private

--- tests/acceptance/test_onboarding.py
+++ tests/acceptance/test_onboarding.py
@@ -46,7 +46,9 @@ class OrganizationOnboardingTest(AcceptanceTestCase):
         assert project.platform == "node"
 
         self.browser.click('[data-test-id="onboarding-getting-started-invite-members"]')
+        self.browser.wait_until('[name="email"]')
         self.browser.snapshot(name="onboarding - invite members")
 
         self.browser.click('[data-test-id="onboarding-getting-started-learn-more"]')
+        self.browser.wait_until_not('[name="email"]')
         self.browser.snapshot(name="onboarding - learn more")
