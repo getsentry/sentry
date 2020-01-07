ff4c85f45fd152d2c237dbc3906a7b2de904d2d7
Lyn Nagara <lyn.nagara@gmail.com>
2020-01-07 10:48:00 -0800

    fix test

--- tests/sentry/eventstream/kafka/test_protocol.py
+++ tests/sentry/eventstream/kafka/test_protocol.py
@@ -50,8 +50,6 @@ def test_get_task_kwargs_for_message_version_1():
     assert event.message == "message"
     assert event.platform == "python"
     assert event.datetime == datetime(2018, 7, 20, 21, 4, 27, 600640, tzinfo=pytz.utc)
-    assert dict(event.data) == {}
-
     assert kwargs.pop("primary_hash") == "49f68a5c8493ec2c0bf489821c21fc3b"
 
     assert kwargs.pop("is_new") is True
