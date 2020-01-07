82b41d0c7c9e688755a71a177d9254bbefce7817
Lyn Nagara <lyn.nagara@gmail.com>
2020-01-06 17:06:23 -0800

    fix message

--- tests/sentry/eventstream/kafka/test_protocol.py
+++ tests/sentry/eventstream/kafka/test_protocol.py
@@ -31,7 +31,11 @@ def test_get_task_kwargs_for_message_version_1():
         "message": "message",
         "platform": "python",
         "datetime": "2018-07-20T21:04:27.600640Z",
-        "data": {},
+        "data": {
+            "logentry": {"formatted": "message"},
+            "platform": "python",
+            "timestamp": 1532120667.60064,
+        },
         "extra": {},
         "primary_hash": "49f68a5c8493ec2c0bf489821c21fc3b",
     }
