af23ced355f2844c06af8c451b13f68e8b2c27bb
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-01-10 14:20:01 -0800

    feat(drf): Add CamelSnakeSerializer (#16397)

--- tests/sentry/api/serializers/rest_framework/test_base.py
+++ tests/sentry/api/serializers/rest_framework/test_base.py
@@ -2,8 +2,32 @@ from __future__ import absolute_import
 
 from django.contrib.contenttypes.models import ContentType
 from django.test import TestCase
+from rest_framework import serializers
 
-from sentry.api.serializers.rest_framework.base import CamelSnakeModelSerializer
+from sentry.api.serializers.rest_framework.base import (
+    CamelSnakeSerializer,
+    CamelSnakeModelSerializer,
+)
+
+
+class PersonSerializer(CamelSnakeSerializer):
+    name = serializers.CharField()
+    works_at = serializers.CharField()
+
+
+class CamelSnakeSerializerTest(TestCase):
+    def test_simple(self):
+        serializer = PersonSerializer(data={"name": "Rick", "worksAt": "Sentry"})
+        assert serializer.is_valid()
+        assert serializer.data == {"name": u"Rick", "works_at": u"Sentry"}
+
+    def test_error(self):
+        serializer = PersonSerializer(data={"worksAt": None})
+        assert not serializer.is_valid()
+        assert serializer.errors == {
+            "worksAt": [u"This field may not be null."],
+            "name": [u"This field is required."],
+        }
 
 
 class ContentTypeSerializer(CamelSnakeModelSerializer):
