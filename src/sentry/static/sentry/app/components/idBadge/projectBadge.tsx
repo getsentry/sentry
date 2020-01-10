21e09d9a73ccfbde98db08eb329939e84cd77378
Mark Story <mark@sentry.io>
2020-01-10 14:44:45 -0500

    ref(ts) Convert ProjectBadge to Typescript

--- src/sentry/static/sentry/app/components/idBadge/projectBadge.tsx
+++ src/sentry/static/sentry/app/components/idBadge/projectBadge.tsx
@@ -3,15 +3,25 @@ import PropTypes from 'prop-types';
 
 import BaseBadge from 'app/components/idBadge/baseBadge';
 import BadgeDisplayName from 'app/components/idBadge/badgeDisplayName';
+import {Project} from 'app/types';
 
-export default class ProjectBadge extends React.PureComponent {
+type Props = {
+  project: Project;
+  avatarSize: number;
+  className?: string;
+  hideAvatar?: boolean;
+  hideName?: boolean;
+  description?: React.ReactNode;
+  avatarClassName?: string | React.CSSProperties;
+  // If true, will use default max-width, or specify one as a string
+  hideOverflow?: boolean | string;
+};
+
+export default class ProjectBadge extends React.PureComponent<Props> {
   static propTypes = {
     ...BaseBadge.propTypes,
     project: BaseBadge.propTypes.project.isRequired,
     avatarSize: PropTypes.number,
-    /**
-     * If true, will use default max-width, or specify one as a string
-     */
     hideOverflow: PropTypes.oneOfType([PropTypes.bool, PropTypes.string]),
     hideAvatar: PropTypes.bool,
   };
