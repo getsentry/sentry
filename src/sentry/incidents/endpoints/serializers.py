907676f854c144173b68863a35b544ca8b3eec9e
Chris Fuller <cfuller@sentry.io>
2020-01-09 22:22:33 -0500

    Meow

--- src/sentry/incidents/endpoints/serializers.py
+++ src/sentry/incidents/endpoints/serializers.py
@@ -46,129 +46,6 @@ string_to_action_target_type = {v: k for (k, v) in action_target_type_to_string.
 CRITICAL_TRIGGER_LABEL = "critical"
 WARNING_TRIGGER_LABEL = "warning"
 
-class AlertRuleSerializer(CamelSnakeModelSerializer):
-    """
-    Serializer for creating/updating an alert rule. Required context:
-     - `organization`: The organization related to this alert rule.
-     - `access`: An access object (from `request.access`)
-    """
-
-    # XXX: ArrayFields aren't supported automatically until DRF 3.1
-    aggregations = serializers.ListField(child=serializers.IntegerField(), required=False)
-    # TODO: These might be slow for many projects, since it will query for each
-    # individually. If we find this to be a problem then we can look into batching.
-    projects = serializers.ListField(child=ProjectField(), required=False)
-    excluded_projects = serializers.ListField(child=ProjectField(), required=False)
-    threshold_type = serializers.IntegerField(required=False)
-    alert_threshold = serializers.IntegerField(required=False)
-    resolve_threshold = serializers.IntegerField(required=False)
-
-    class Meta:
-        model = AlertRule
-        fields = [
-            "name",
-            "threshold_type",
-            "query",
-            "time_window",
-            "alert_threshold",
-            "resolve_threshold",
-            "threshold_period",
-            "aggregation",
-            "aggregations",
-            "projects",
-            "include_all_projects",
-            "excluded_projects",
-        ]
-        extra_kwargs = {
-            "query": {"allow_blank": True, "required": True},
-            "threshold_period": {"default": 1, "min_value": 1, "max_value": 20},
-            "time_window": {
-                "min_value": 1,
-                "max_value": int(timedelta(days=1).total_seconds() / 60),
-                "required": True,
-            },
-            "aggregation": {"required": False},
-            "name": {"min_length": 1, "max_length": 64},
-            "include_all_projects": {"default": False},
-        }
-
-    def validate_aggregation(self, aggregation):
-        try:
-            return QueryAggregations(aggregation)
-        except ValueError:
-            raise serializers.ValidationError(
-                "Invalid aggregation, valid values are %s"
-                % [item.value for item in QueryAggregations]
-            )
-
-    def validate_aggregations(self, aggregations):
-        # TODO: Remove this once FE transitions
-        try:
-            return [QueryAggregations(agg) for agg in aggregations]
-        except ValueError:
-            raise serializers.ValidationError(
-                "Invalid aggregation, valid values are %s"
-                % [item.value for item in QueryAggregations]
-            )
-
-    def validate(self, data):
-        return self._handle_old_fields_transition(data)
-
-    def create(self, validated_data):
-        try:
-            # TODO: Remove this, just temporary while we're supporting both fields.
-            if "aggregation" not in validated_data:
-                raise serializers.ValidationError("aggregation is required")
-
-            return create_alert_rule(organization=self.context["organization"], **validated_data)
-        except AlertRuleNameAlreadyUsedError:
-            raise serializers.ValidationError("This name is already in use for this project")
-
-    def _remove_unchanged_fields(self, instance, validated_data):
-        for field_name, value in list(six.iteritems(validated_data)):
-            # Remove any fields that haven't actually changed
-            if field_name == "triggers":
-                continue  # No removal for triggers
-
-            if field_name == "projects":
-                project_slugs = Project.objects.filter(
-                    querysubscription__alert_rules=instance
-                ).values_list("slug", flat=True)
-                if set(project_slugs) == set([project.slug for project in value]):
-                    validated_data.pop(field_name)
-                continue
-            if field_name == "excluded_projects":
-                excluded_slugs = [
-                    p.project.slug for p in get_excluded_projects_for_alert_rule(instance)
-                ]
-                if set(excluded_slugs) == set(project.slug for project in value):
-                    validated_data.pop(field_name)
-                continue
-            if isinstance(value, Enum):
-                value = value.value
-            if getattr(instance, field_name) == value:
-                validated_data.pop(field_name)
-        return validated_data
-
-    def _handle_old_fields_transition(self, validated_data):
-        # Temporary methods for transitioning from multiple aggregations to a single
-        # aggregate
-        if "aggregations" in validated_data and "aggregation" not in validated_data:
-            validated_data["aggregation"] = validated_data["aggregations"][0]
-
-        validated_data.pop("aggregations", None)
-        # TODO: Remove after frontend stops using these fields
-        validated_data.pop("threshold_type", None)
-        validated_data.pop("alert_threshold", None)
-        validated_data.pop("resolve_threshold", None)
-        return validated_data
-
-    def update(self, instance, validated_data):
-        validated_data = self._remove_unchanged_fields(instance, validated_data)
-        return update_alert_rule(instance, **validated_data)
-
-
-
 
 class AlertRuleTriggerActionSerializer(CamelSnakeModelSerializer):
     """
@@ -351,6 +228,129 @@ class AlertRuleTriggerSerializer(CamelSnakeModelSerializer):
             raise serializers.ValidationError("This label is already in use for this alert rule")
 
 
+
+class AlertRuleSerializer(CamelSnakeModelSerializer):
+    """
+    Serializer for creating/updating an alert rule. Required context:
+     - `organization`: The organization related to this alert rule.
+     - `access`: An access object (from `request.access`)
+    """
+
+    # XXX: ArrayFields aren't supported automatically until DRF 3.1
+    aggregations = serializers.ListField(child=serializers.IntegerField(), required=False)
+    # TODO: These might be slow for many projects, since it will query for each
+    # individually. If we find this to be a problem then we can look into batching.
+    projects = serializers.ListField(child=ProjectField(), required=False)
+    excluded_projects = serializers.ListField(child=ProjectField(), required=False)
+    threshold_type = serializers.IntegerField(required=False)
+    alert_threshold = serializers.IntegerField(required=False)
+    resolve_threshold = serializers.IntegerField(required=False)
+
+    class Meta:
+        model = AlertRule
+        fields = [
+            "name",
+            "threshold_type",
+            "query",
+            "time_window",
+            "alert_threshold",
+            "resolve_threshold",
+            "threshold_period",
+            "aggregation",
+            "aggregations",
+            "projects",
+            "include_all_projects",
+            "excluded_projects",
+        ]
+        extra_kwargs = {
+            "query": {"allow_blank": True, "required": True},
+            "threshold_period": {"default": 1, "min_value": 1, "max_value": 20},
+            "time_window": {
+                "min_value": 1,
+                "max_value": int(timedelta(days=1).total_seconds() / 60),
+                "required": True,
+            },
+            "aggregation": {"required": False},
+            "name": {"min_length": 1, "max_length": 64},
+            "include_all_projects": {"default": False},
+        }
+
+    def validate_aggregation(self, aggregation):
+        try:
+            return QueryAggregations(aggregation)
+        except ValueError:
+            raise serializers.ValidationError(
+                "Invalid aggregation, valid values are %s"
+                % [item.value for item in QueryAggregations]
+            )
+
+    def validate_aggregations(self, aggregations):
+        # TODO: Remove this once FE transitions
+        try:
+            return [QueryAggregations(agg) for agg in aggregations]
+        except ValueError:
+            raise serializers.ValidationError(
+                "Invalid aggregation, valid values are %s"
+                % [item.value for item in QueryAggregations]
+            )
+
+    def validate(self, data):
+        return self._handle_old_fields_transition(data)
+
+    def create(self, validated_data):
+        try:
+            # TODO: Remove this, just temporary while we're supporting both fields.
+            if "aggregation" not in validated_data:
+                raise serializers.ValidationError("aggregation is required")
+
+            return create_alert_rule(organization=self.context["organization"], **validated_data)
+        except AlertRuleNameAlreadyUsedError:
+            raise serializers.ValidationError("This name is already in use for this project")
+
+    def _remove_unchanged_fields(self, instance, validated_data):
+        for field_name, value in list(six.iteritems(validated_data)):
+            # Remove any fields that haven't actually changed
+            if field_name == "triggers":
+                continue  # No removal for triggers
+
+            if field_name == "projects":
+                project_slugs = Project.objects.filter(
+                    querysubscription__alert_rules=instance
+                ).values_list("slug", flat=True)
+                if set(project_slugs) == set([project.slug for project in value]):
+                    validated_data.pop(field_name)
+                continue
+            if field_name == "excluded_projects":
+                excluded_slugs = [
+                    p.project.slug for p in get_excluded_projects_for_alert_rule(instance)
+                ]
+                if set(excluded_slugs) == set(project.slug for project in value):
+                    validated_data.pop(field_name)
+                continue
+            if isinstance(value, Enum):
+                value = value.value
+            if getattr(instance, field_name) == value:
+                validated_data.pop(field_name)
+        return validated_data
+
+    def _handle_old_fields_transition(self, validated_data):
+        # Temporary methods for transitioning from multiple aggregations to a single
+        # aggregate
+        if "aggregations" in validated_data and "aggregation" not in validated_data:
+            validated_data["aggregation"] = validated_data["aggregations"][0]
+
+        validated_data.pop("aggregations", None)
+        # TODO: Remove after frontend stops using these fields
+        validated_data.pop("threshold_type", None)
+        validated_data.pop("alert_threshold", None)
+        validated_data.pop("resolve_threshold", None)
+        return validated_data
+
+    def update(self, instance, validated_data):
+        validated_data = self._remove_unchanged_fields(instance, validated_data)
+        return update_alert_rule(instance, **validated_data)
+
+
 class UnifiedAlertRuleSerializer(AlertRuleSerializer):
     """
     Unified Serializer for creating/updating an alert rule - accepts trigger and action data, and does validation on it.
@@ -412,6 +412,7 @@ class UnifiedAlertRuleSerializer(AlertRuleSerializer):
                 raise serializers.ValidationError("aggregation is required")
 
             triggers_data = validated_data.pop('triggers')
+            #TODO: User super.create and don't duplicate the aggreagation + duplicate name check?
             alert_rule = create_alert_rule(organization=self.context["organization"], **validated_data)
             for trigger_data in triggers_data:
                 trigger_actions_data = trigger_data.pop("actions")
@@ -425,6 +426,7 @@ class UnifiedAlertRuleSerializer(AlertRuleSerializer):
     def update(self, instance, validated_data):
         validated_data = self._remove_unchanged_fields(instance, validated_data)
         triggers_data = validated_data.pop("triggers")
+        #TODO: User super.update and don't duplicate the aggreagation + duplicate name check?
         alert_rule = update_alert_rule(instance, **validated_data)
         for trigger_data in triggers_data:
             actions_data = trigger_data.pop("actions")
