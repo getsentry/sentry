3933bb9ac1b6b58248793326afac5939c4afe352
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-01-15 10:57:29 -0800

    🤡 Mock Post route and UI to trigger

--- src/sentry/api/endpoints/data_export.py
+++ src/sentry/api/endpoints/data_export.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import
 
-from rest_framework.response import Response
 from sentry.api.bases.incident import IncidentPermission
 from sentry.api.base import Endpoint
 
@@ -14,10 +13,24 @@ class DataExportEndpoint(Endpoint):
     def get(self, *args, **kwargs):
         compile_data()
         mock_response = {
-            # 'data_id': kwargs['data_tag'],
-            # 'org_id': kwargs['organization_slug'],
-            # 'user_id': 'user@email.com',
-            "url": "https://sentry.s3.us-east-1.amazonaws.com/{}".format(kwargs["data_tag"]),
-            "expiration": "2020-11-16T23:59:59.999Z",
+            "url": "https://sentry.s3.us-east-1.amazonaws.com/{}_{}".format(
+                kwargs["organization_slug"], kwargs["data_tag"]
+            ),
+            "expiration": "2020-11-16T23:47:29.999Z",
         }
-        return Response(mock_response)
+        return self.respond(mock_response)
+
+    def post(self, request, *args, **kwargs):
+        # {
+        # 	id: INT // For Postgres
+        # 	data_id: STRING // For a unique identifier
+        # 	org_id: STRING // For auth
+        # 	user_id: STRING // Trigger an email to this user
+        # 	expired_at: DATETIME // 2 days after creation
+        # 	url: STRING // Link to temp storage (S3, some GGP product, etc.)
+        # 	query: QUERY // Shape TBD, use this to prevent identical reqs
+        # }
+        # query = request.data.get('query')
+        # org_id = request.data.get
+        # print(request.data.get)
+        return self.respond("This endpoint will be used to begin CSV generation.")
--- src/sentry/static/sentry/app/views/dataExport/dataDownload.tsx
+++ src/sentry/static/sentry/app/views/dataExport/dataDownload.tsx
@@ -7,6 +7,8 @@ import {RouteComponentProps} from 'react-router/lib/Router';
 import AsyncView from 'app/views/asyncView';
 import withApi from 'app/utils/withApi';
 import withOrganization from 'app/utils/withOrganization';
+import withConfig from 'app/utils/withConfig';
+import withLatestContext from 'app/utils/withLatestContext';
 import {PageContent} from 'app/styles/organization';
 import {t} from 'app/locale';
 
@@ -43,7 +45,7 @@ class DataDownload extends AsyncView<Props, State> {
     };
   }
 
-  getTitle() {
+  getTitle(): string {
     return t('Download Center');
   }
 
@@ -52,10 +54,28 @@ class DataDownload extends AsyncView<Props, State> {
     return [['asset', `/organizations/${orgId}/data-export/${dataTag}/`]];
   }
 
-  handleDownload() {
+  handleDownload(): void {
     const {asset} = this.state;
     // eslint-disable-next-line
     alert(`Beginning download at ${asset && asset.url}`);
+  }
+
+  pingRequest(): void {
+    const {config, organization, params} = this.props;
+    const endpoint = `/organizations/${params.orgId}/data-export/${params.dataTag}/`;
+    const {id: user_id} = config.user;
+    const {slug: org_id} = organization;
+    this.api.request(endpoint, {
+      method: 'POST',
+      data: {
+        user_id,
+        org_id,
+      },
+      success(response) {
+        // eslint-disable-next-line
+        alert(response);
+      },
+    });
   }
 
   renderBody() {
@@ -83,8 +103,19 @@ class DataDownload extends AsyncView<Props, State> {
           </Button>
           <p>{t('Keep in mind, this link will no longer work after:')}</p>
           <p>
-            <b>{d.toLocaleDateString()}</b>
+            <b>{`${d.toLocaleDateString()}, ${d.toLocaleTimeString()}`}</b>
           </p>
+        </ContentContainer>
+        <ContentContainer>
+          <Button
+            priority="primary"
+            icon="icon-sentry"
+            size="large"
+            borderless
+            onClick={() => this.pingRequest()}
+          >
+            {t('Trigger Task')}
+          </Button>
         </ContentContainer>
       </PageContent>
     );
@@ -105,4 +136,4 @@ const ContentContainer = styled('div')`
   }
 `;
 
-export default withOrganization(withApi(DataDownload));
+export default withLatestContext(withConfig(withApi(withOrganization(DataDownload))));
