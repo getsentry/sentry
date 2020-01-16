0e522f9c0cbd579d1b5488e582640e71f02b6ccf
Matej Minar <matej.minar@sentry.io>
2020-01-16 13:49:43 +0100

    few tests, more after finished api

--- tests/acceptance/test_organization_health.py
+++ tests/acceptance/test_organization_health.py
@@ -4,6 +4,8 @@ from django.utils import timezone
 
 from sentry.testutils import AcceptanceTestCase
 
+FEATURE_NAME = "organizations:health"
+
 
 class OrganizationHealthTest(AcceptanceTestCase):
     def setUp(self):
@@ -18,8 +20,14 @@ class OrganizationHealthTest(AcceptanceTestCase):
         self.path = u"/organizations/{}/health/".format(self.org.slug)
         self.project.update(first_event=timezone.now())
 
-    def test(self):
-        # data is for now mocked/hardcoded in the UI
+    def test_no_access(self):
         self.browser.get(self.path)
-        self.browser.wait_until_not(".loading")
-        self.browser.snapshot("organization health list")
+        self.browser.wait_until_not(".loading-indicator")
+        self.browser.snapshot("global events - no access")
+
+    def test(self):
+        with self.feature(FEATURE_NAME):
+            # data is for now mocked/hardcoded in the UI
+            self.browser.get(self.path)
+            self.browser.wait_until_not(".loading")
+            self.browser.snapshot("organization health list")
