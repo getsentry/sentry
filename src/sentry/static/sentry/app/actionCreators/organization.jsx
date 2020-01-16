6ef0f9c948b4a3368766af275ad6ac9f585b5cfa
jeffkwoh <23180853+jeffkwoh@users.noreply.github.com>
2020-01-15 19:00:29 -0800

    Fix nit:spelling

--- src/sentry/static/sentry/app/actionCreators/organization.jsx
+++ src/sentry/static/sentry/app/actionCreators/organization.jsx
@@ -14,7 +14,7 @@ import TeamStore from 'app/stores/teamStore';
  * @param {String} slug The organization slug
  * @param {boolean} detailed Whether or not the detailed org details should be
  *                           retrieved
- * @param {boolean} silent Should we silenty update the organization (do not
+ * @param {boolean} silent Should we silently update the organization (do not
  *                         clear the current organization in the store)
  */
 export async function fetchOrganizationDetails(api, slug, detailed, silent) {
