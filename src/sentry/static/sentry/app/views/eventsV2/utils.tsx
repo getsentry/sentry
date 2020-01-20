708c3f56e3e47108214e0313bcd5fac1d828ccd1
Mark Story <mark@sentry.io>
2020-01-20 15:26:04 -0500

    feat(discover2) Event detail tags links should go to ungrouped results
    Clicking a tag link on the event details should take the user to a new
    result set that unpacks the current aggregate group. This behaves
    similar to how clicking a count() field on the result table does.
    
    I've refactored out the 'zoom in' condition building from the result
    table as I didn't want to duplicate it. In the process I've removed the
    addition of the `id` field as it was generally noisy and not helpful.
    
    Tag table 'zooming' is a best effort right now. If the user's previous
    query includes array fields, or fields from contexts that are not
    exposed as tags those fields will not be included in the generated
    conditions. This is because traversing the event object is not simple
    for many paths.

--- src/sentry/static/sentry/app/views/eventsV2/utils.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/utils.tsx
@@ -13,6 +13,7 @@ import {getUtcDateString} from 'app/utils/dates';
 import {URL_PARAM} from 'app/constants/globalSelectionHeader';
 import {disableMacros} from 'app/views/discover/result/utils';
 import {generateQueryWithTag} from 'app/utils';
+import {appendTagCondition} from 'app/utils/queryString';
 import {
   COL_WIDTH_UNDEFINED,
   COL_WIDTH_DEFAULT,
@@ -32,9 +33,9 @@ import {
   ALL_VIEWS,
   TRANSACTION_VIEWS,
 } from './data';
-import EventView, {Field as FieldType} from './eventView';
+import EventView, {Field as FieldType, Column} from './eventView';
 import {Aggregation, Field, AGGREGATIONS, FIELDS} from './eventQueryParams';
-import {TableColumn} from './table/types';
+import {TableColumn, TableDataRow} from './table/types';
 
 export type EventQuery = {
   field: string[];
@@ -57,13 +58,7 @@ function explodeFieldString(field: string): {aggregation: string; field: string}
   return {aggregation: '', field};
 }
 
-export function explodeField(
-  field: FieldType
-): {
-  aggregation: string;
-  field: string;
-  width: number;
-} {
+export function explodeField(field: FieldType): Column {
   const results = explodeFieldString(field.field);
 
   return {
@@ -405,4 +400,66 @@ export function downloadAsCsv(tableData, columnOrder, filename) {
   link.setAttribute('download', `${filename} ${getUtcDateString(now)}.csv`);
   link.click();
   link.remove();
+}
+
+export function getExpandedResults(
+  eventView: EventView,
+  additionalConditions: {[key: string]: string},
+  dataRow?: TableDataRow | Event
+): EventView {
+  let nextView = eventView.clone();
+  const fieldsToRemove: number[] = [];
+
+  // Workaround around readonly typing
+  const aggregateAliases: string[] = [...AGGREGATE_ALIASES];
+
+  nextView.fields.forEach((field: FieldType, index: number) => {
+    const column = explodeField(field);
+
+    // Remove aggregates as the expanded results have no aggregates.
+    if (column.aggregation || aggregateAliases.includes(column.field)) {
+      fieldsToRemove.push(index);
+      return;
+    }
+
+    const dataKey = getAggregateAlias(field.field);
+    // Append the current field as a condition if it exists in the dataRow
+    // Or is a simple key in the event. More complex deeply nested fields are
+    // more challenging to get at as their location in the structure does not
+    // match their name.
+    if (dataRow) {
+      if (dataRow[dataKey]) {
+        additionalConditions[column.field] = String(dataRow[dataKey]).trim();
+      }
+      // If we have an event, check tags as well.
+      if (dataRow && dataRow.tags && dataRow.tags instanceof Array) {
+        const tagIndex = dataRow.tags.findIndex(item => item.key === dataKey);
+        if (tagIndex > -1) {
+          additionalConditions[column.field] = dataRow.tags[tagIndex].value;
+        }
+      }
+    }
+  });
+
+  // Remove fields from view largest index to smallest to not
+  // disturbe lower indexes until the end.
+  fieldsToRemove.reverse().forEach((i: number) => {
+    nextView = nextView.withDeletedColumn(i, undefined);
+  });
+
+  // Tokenize conditions and append additional conditions provided + generated.
+  Object.keys(additionalConditions).forEach(key => {
+    if (key === 'project' || key === 'project.id') {
+      nextView.project = [...nextView.project, parseInt(additionalConditions[key], 10)];
+      return;
+    }
+    if (key === 'environment') {
+      nextView.environment = [...nextView.environment, additionalConditions[key]];
+      return;
+    }
+
+    nextView.query = appendTagCondition(nextView.query, key, additionalConditions[key]);
+  });
+
+  return nextView;
 }
