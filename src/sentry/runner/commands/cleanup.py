getsentry/sentry

commit 0a278e0508ee265e396b2a6e57f957dbb0cdd00c

ref: Remove event deletions (#15676)
We don't need to run event deletions, this table is being removed

src/sentry/runner/commands/cleanup.py
@@ -65,7 +65,6 @@ def multiprocess_worker(task_queue):
 
             skip_models = [
                 # Handled by other parts of cleanup
-                models.Event,
                 models.EventAttachment,
                 models.UserReport,
                 models.Group,
@@ -183,7 +182,7 @@ def cleanup(days, project, concurrency, silent, model, router, timed):
 
     # Deletions that use the `deletions` code path (which handles their child relations)
     # (model, datetime_field, order_by)
-    DELETES = ((models.Event, "datetime", "datetime"), (models.Group, "last_seen", "last_seen"))
+    DELETES = (models.Group, "last_seen", "last_seen")
 
     if not silent:
         click.echo("Removing expired values for LostPasswordHash")

src/sentry/deletions/defaults/group.py
@@ -87,8 +87,6 @@ class GroupDeletionTask(ModelDeletionTask):
             models.UserReport,
             models.EventAttachment,
             IncidentGroup,
-            # Event is last as its the most time consuming
-            models.Event,
         )
 
         relations.extend([ModelRelation(m, {"group_id": instance.id}) for m in model_list])

src/sentry/deletions/defaults/project.py
@@ -58,9 +58,6 @@ class ProjectDeletionTask(ModelDeletionTask):
             ]
         )
 
-        # special case event due to nodestore
-        relations.extend([ModelRelation(models.Event, {"project_id": instance.id})])
-
         # in bulk
         # Release needs to handle deletes after Group is cleaned up as the foreign
         # key is protected

commit 79000bea13b448311a4b80c3ded95b63392c7ff4

fix(deletion): Fix group deletion
Fixes a bug in group deletion introduced in https://github.com/getsentry/sentry/pull/15676

Fixes SENTRY-E81

src/sentry/runner/commands/cleanup.py
@@ -182,7 +182,7 @@ def cleanup(days, project, concurrency, silent, model, router, timed):
 
     # Deletions that use the `deletions` code path (which handles their child relations)
     # (model, datetime_field, order_by)
-    DELETES = (models.Group, "last_seen", "last_seen")
+    DELETES = ((models.Group, "last_seen", "last_seen"),)
 
     if not silent:
         click.echo("Removing expired values for LostPasswordHash")