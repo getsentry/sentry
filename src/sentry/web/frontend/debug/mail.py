5c00423324247cc11c68aa9af51f09bd9c4d5ab2
Lyn Nagara <lyn.nagara@gmail.com>
2020-01-21 10:33:10 -0800

    ref: Remove option to use Django event model in event_manager.save() (#16509)
    This flag has been set to False in prod, we can remove the old code
    path.

--- src/sentry/web/frontend/debug/mail.py
+++ src/sentry/web/frontend/debug/mail.py
@@ -16,7 +16,7 @@ from django.views.generic import View
 from loremipsum import Generator
 from random import Random
 
-from sentry import eventstore, options
+from sentry import eventstore
 from sentry.app import tsdb
 from sentry.constants import LOG_LEVELS
 from sentry.digests import Record
@@ -238,10 +238,7 @@ def alert(request):
     data = event_manager.get_data()
     event = event_manager.save(project.id)
     # Prevent Percy screenshot from constantly changing
-    if options.get("store.use-django-event"):
-        event.datetime = datetime(2017, 9, 6, 0, 0)
-    else:
-        event.data["timestamp"] = 1504656000.0  # datetime(2017, 9, 6, 0, 0)
+    event.data["timestamp"] = 1504656000.0  # datetime(2017, 9, 6, 0, 0)
     event_type = event_manager.get_event_type()
 
     group.message = event.search_message
