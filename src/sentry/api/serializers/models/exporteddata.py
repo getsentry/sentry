36f0f7df4ea70a09a8230fc99d2990d45a13778f
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-01-23 11:22:33 -0800

    ✨ Implement get_attrs method on serializer

--- src/sentry/api/serializers/models/exporteddata.py
+++ src/sentry/api/serializers/models/exporteddata.py
@@ -1,16 +1,29 @@
 from __future__ import absolute_import
 
-from sentry.api.serializers import Serializer, register
-from sentry.models import ExportedData
+from sentry.api.serializers import Serializer, serialize, register
+from sentry.models import ExportedData, User
 
 
 @register(ExportedData)
 class ExportedDataSerializer(Serializer):
+    def get_attrs(self, item_list, user, **kwagrs):
+        attrs = {}
+        for item in item_list:
+            user = User.objects.get(id=item.user.id)
+            serialized_user = serialize(user)
+            attrs[item] = {
+                "user": {
+                    "id": serialized_user["id"],
+                    "email": serialized_user["email"],
+                    "username": serialized_user["username"],
+                }
+            }
+        return attrs
+
     def serialize(self, obj, attrs, user, **kwargs):
         return {
             "id": obj.id,
-            # "user": 1,
-            # "organization": 2,
+            "user": attrs["user"],
             "dateCreated": obj.date_added,
             "dateFinished": obj.date_finished,
             "dateExpired": obj.date_expired,
