a365ce05889694aed7481be4e08f830a92272fc3
jeffkwoh <23180853+jeffkwoh@users.noreply.github.com>
2020-01-23 14:05:53 -0800

    Remove lodash/get from navigation.jsx

--- src/sentry/static/sentry/app/actionCreators/navigation.jsx
+++ src/sentry/static/sentry/app/actionCreators/navigation.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import get from 'lodash/get';
 
 import {openModal} from 'app/actionCreators/modal';
 import ContextPickerModal from 'app/components/contextPickerModal';
@@ -9,7 +8,7 @@ export function navigateTo(to, router) {
   // Check for placeholder params
   const needOrg = to.indexOf(':orgId') > -1;
   const needProject = to.indexOf(':projectId') > -1;
-  const comingFromProjectId = get(router, 'location.query.project');
+  const comingFromProjectId = router?.location?.query?.project;
 
   if (needOrg || needProject) {
     openModal(
