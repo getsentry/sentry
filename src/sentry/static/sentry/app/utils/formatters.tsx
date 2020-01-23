d0621f8c93e2a1db08f0b88e6c017edfe4de4e9d
jeffkwoh <23180853+jeffkwoh@users.noreply.github.com>
2020-01-23 15:03:37 -0800

    Remove lodash/get from formatters.tsx

--- src/sentry/static/sentry/app/utils/formatters.tsx
+++ src/sentry/static/sentry/app/utils/formatters.tsx
@@ -1,16 +1,14 @@
-import get from 'lodash/get';
-
 import {t} from 'app/locale';
 import {CommitAuthor, User} from 'app/types';
 
 export function userDisplayName(user: User | CommitAuthor): string {
-  let displayName = String(get(user, 'name', t('Unknown author'))).trim();
+  let displayName = String(user?.name ?? t('Unknown author')).trim();
 
   if (displayName.length <= 0) {
     displayName = t('Unknown author');
   }
 
-  const email = String(get(user, 'email', '')).trim();
+  const email = String(user?.email ?? '').trim();
 
   if (email.length > 0 && email !== displayName) {
     displayName += ' (' + email + ')';
