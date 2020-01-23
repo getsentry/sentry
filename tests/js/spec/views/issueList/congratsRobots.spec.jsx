a3effe2907a47bfb02933e5fae89f20f8cf2c0ae
Chris Jennings <chriskjennings@gmail.com>
2020-01-22 17:23:26 -0800

    ref(ui): Use video for congrats robot animation. Remove lottie. (#14868)

--- tests/js/spec/views/issueList/congratsRobots.spec.jsx
+++ tests/js/spec/views/issueList/congratsRobots.spec.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {mount} from 'sentry-test/enzyme';
+import {mountWithTheme} from 'sentry-test/enzyme';
 
 import CongratsRobots from 'app/views/issueList/congratsRobots';
 
 describe('CongratsRobots', function() {
   it('renders', function() {
-    const wrapper = mount(<CongratsRobots />);
+    const wrapper = mountWithTheme(<CongratsRobots />);
 
-    expect(wrapper.find('Lottie').exists()).toBe(true);
-    expect(wrapper.find('Description').exists()).toBe(true);
+    expect(wrapper.find('AnimatedScene').exists()).toBe(true);
+    expect(wrapper.find('StyledVideo').exists()).toBe(true);
   });
 });
