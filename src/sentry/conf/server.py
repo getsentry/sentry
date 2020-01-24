de87d538706ffc44723cbe74e791edeaf45bf0f4
jeffkwoh <23180853+jeffkwoh@users.noreply.github.com>
2020-01-24 10:36:54 -0800

    feat(switch): Add new feature switch for alerts

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -808,6 +808,8 @@ SENTRY_FEATURES = {
     "organizations:advanced-search": True,
     # Enable obtaining and using API keys.
     "organizations:api-keys": False,
+    # Enable customization of issue alerts.
+    "organizations:customizable-issue-alerts": False,
     # Enable explicit use of AND and OR in search.
     "organizations:boolean-search": False,
     # Enable creating organizations within sentry (if SENTRY_SINGLE_ORGANIZATION
--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -55,6 +55,7 @@ default_manager.add("organizations:create")
 # Organization scoped features
 default_manager.add("organizations:advanced-search", OrganizationFeature)  # NOQA
 default_manager.add("organizations:boolean-search", OrganizationFeature)  # NOQA
+default_manager.add("organizations:customizable-issue-alerts", OrganizationFeature)  # NOQA
 default_manager.add("organizations:api-keys", OrganizationFeature)  # NOQA
 default_manager.add("organizations:discover", OrganizationFeature)  # NOQA
 default_manager.add("organizations:events", OrganizationFeature)  # NOQA
