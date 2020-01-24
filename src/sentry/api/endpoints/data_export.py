e7c2966c4ab8eae76cd340a1efdfa26c9fe89024
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-01-23 17:23:51 -0800

    ✨ Model changes and primitive validation

--- src/sentry/api/endpoints/data_export.py
+++ src/sentry/api/endpoints/data_export.py
@@ -1,13 +1,21 @@
 from __future__ import absolute_import
 
+
 from django.db import IntegrityError, transaction
+from rest_framework import serializers
 from rest_framework.response import Response
 
 from sentry import features
 from sentry.api.bases.organization import OrganizationEndpoint, OrganizationEventPermission
 from sentry.api.serializers import serialize
+
 from sentry.models import ExportedData
 from sentry.tasks.data_export import compile_data
+
+
+class ExportedDataSerializer(serializers.Serializer):
+    query_type = serializers.CharField(max_length=32, required=True)
+    # TODO(Leander): Implement query_info validation with jsonschema
 
 
 class DataExportEndpoint(OrganizationEndpoint):
@@ -15,22 +23,30 @@ class DataExportEndpoint(OrganizationEndpoint):
 
     def post(self, request, organization):
         """
-        Create a new Asynchronous file export task, and
+        Create a new asynchronous file export task, and
         email user upon completion,
         """
 
         if not features.has("organizations:data-export", organization):
             return Response(status=404)
 
-        # TODO(Leander): Implement validator to clean the request
+        serializer = ExportedDataSerializer(
+            data=request.data, context={"organization": organization, "user": request.user}
+        )
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        data = serializer.validated_data
+
         try:
             with transaction.atomic():
                 # TODO(Leander): Prevent repeated requests for identical queries per organization, if one is in progress
                 data_export = ExportedData.objects.create(
                     organization=organization,
                     user=request.user,
-                    query_type=request.data["query"]["type"],
-                    query_info=request.data["query"]["info"],
+                    query_type=data["query_type"],
+                    query_info=data["query_info"],
                 )
         except IntegrityError:
             return Response("This exact export is already in progress.", status=409)
--- src/sentry/models/exporteddata.py
+++ src/sentry/models/exporteddata.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import
 
 import six
-from datetime import timedelta
 from enum import Enum
 
 from django.conf import settings
@@ -10,13 +9,15 @@ from django.utils import timezone
 
 from sentry.db.models import FlexibleForeignKey, JSONField, Model, sane_repr
 
-DEFAULT_EXPIRATION = timedelta(days=7)
-
 
 class ExportStatus(six.binary_type, Enum):
     Early = "EARLY"  # The download is being prepared
     Valid = "VALID"  # The download is ready for the user
     Expired = "EXPIRED"  # The download has been deleted
+
+
+# TODO(Leander): Connect PostiveSmallIntegerField to enum
+# class ExportQueryType(Enum):
 
 
 class ExportedData(Model):
@@ -33,7 +34,7 @@ class ExportedData(Model):
     date_finished = models.DateTimeField(null=True)
     date_expired = models.DateTimeField(null=True)
     storage_url = models.URLField(null=True)
-    query_type = models.CharField(max_length=32)
+    query_type = models.PositiveSmallIntegerField()
     # TODO(Leander): Define a jsonschema to enforce query shape
     query_info = JSONField()  # i.e.: sort rules, order-by's, custom settings
 
@@ -49,13 +50,5 @@ class ExportedData(Model):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_exporteddata"
-
-    # def set_completion_info(self, storage_url):
-    #     self.date_expired = timezone.now() + DEFAULT_EXPIRATION
-    #     self.date_finished = timezone.now()
-    #     self.storage_url = storage_url
-
-    # def set_expired_at(self, expire_time):
-    #     self.date_expired = expire_time
 
     __repr__ = sane_repr("data_id")
