498fc1ee2dded43dfb4613762e8221cc0388867e
Billy Vong <billy@sentry.io>
2020-01-24 18:14:11 -0800

    fix(ui): Fix LightweightOrg loading last used project
    LightweightOrganization would attempt to intialize global selection store with last selected project if projects request takes too long to fetch.

--- src/sentry/static/sentry/app/views/organizationContext.jsx
+++ src/sentry/static/sentry/app/views/organizationContext.jsx
@@ -203,6 +203,7 @@ const OrganizationContext = createReactClass({
       // We do not want to load the user's last used env/project in this case, otherwise will
       // lead to very confusing behavior.
       if (
+        organization.projects &&
         !this.props.routes.find(
           ({path}) => path && path.includes('/organizations/:orgId/issues/:groupId/')
         )
