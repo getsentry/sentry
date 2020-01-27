0c3c7b0b6dd58223eecbba91e1d71cb1106072ff
Matej Minar <matej.minar@sentry.io>
2020-01-27 16:18:39 +0100

    ref(ts): Convert <ExtraData> to typescript

--- src/sentry/static/sentry/app/components/events/extraData.tsx
+++ src/sentry/static/sentry/app/components/events/extraData.tsx
@@ -1,28 +1,34 @@
 import React from 'react';
 
+import {Event} from 'app/types';
 import {t} from 'app/locale';
 import ErrorBoundary from 'app/components/errorBoundary';
 import EventDataSection from 'app/components/events/eventDataSection';
 import KeyValueList from 'app/components/events/interfaces/keyValueList';
 import SentryTypes from 'app/sentryTypes';
 
-class EventExtraData extends React.Component {
+type Props = {
+  event: Event;
+};
+
+type State = {
+  raw: boolean;
+};
+
+class EventExtraData extends React.Component<Props, State> {
   static propTypes = {
     event: SentryTypes.Event.isRequired,
   };
 
-  constructor(...args) {
-    super(...args);
-    this.state = {
-      raw: false,
-    };
-  }
+  state: State = {
+    raw: false,
+  };
 
-  shouldComponentUpdate(nextProps, nextState) {
+  shouldComponentUpdate(nextProps: Props, nextState: State) {
     return this.props.event.id !== nextProps.event.id || this.state.raw !== nextState.raw;
   }
 
-  toggleRaw = shouldBeRaw => {
+  toggleRaw = (shouldBeRaw: boolean) => {
     this.setState({
       raw: shouldBeRaw,
     });
@@ -30,6 +36,7 @@ class EventExtraData extends React.Component {
 
   render() {
     const extraDataArray = Object.entries(this.props.event.context);
+
     return (
       <div className="extra-data">
         <EventDataSection
