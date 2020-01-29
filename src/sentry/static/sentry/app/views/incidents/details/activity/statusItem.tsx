0b72ef819b33783cbf4ca2e7da8464d4ddcf0db4
Billy Vong <billyvg@users.noreply.github.com>
2020-01-29 12:26:50 -0800

    feat(workflow): Update status item text (#16660)
    Removes created and status change items since neither case can occur anymore. 
    
    Slightly updated the text
    * update detected text
    * change incident to alert

--- src/sentry/static/sentry/app/views/incidents/details/activity/statusItem.tsx
+++ src/sentry/static/sentry/app/views/incidents/details/activity/statusItem.tsx
@@ -1,42 +1,42 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import {t} from 'app/locale';
+import {tct} from 'app/locale';
 import ActivityItem from 'app/components/activity/item';
 import getDynamicText from 'app/utils/getDynamicText';
 
-import {IncidentActivityType, IncidentStatus, ActivityType} from '../../types';
+import {Incident, IncidentActivityType, IncidentStatus, ActivityType} from '../../types';
 
 type Props = {
   activity: ActivityType;
-  authorName: string;
   showTime: boolean;
+
+  /**
+   * Author name can be undefined if there is no author, e.g. if it's a system activity
+   */
+  authorName?: string;
+  incident?: Incident;
 };
 
 /**
- * StatusItem renders status changes for Incidents
+ * StatusItem renders status changes for Alerts
  *
- * For example: incident created, detected, or closed
+ * For example: incident detected, or closed
  *
  * Note `activity.dateCreated` refers to when the activity was created vs.
  * `incident.dateStarted` which is when an incident was first detected or created
  */
 class StatusItem extends React.Component<Props> {
   render() {
-    const {activity, authorName, showTime} = this.props;
+    const {activity, authorName, incident, showTime} = this.props;
 
-    const isCreated = activity.type === IncidentActivityType.CREATED;
     const isDetected = activity.type === IncidentActivityType.DETECTED;
     const isClosed =
       activity.type === IncidentActivityType.STATUS_CHANGE &&
       activity.value === `${IncidentStatus.CLOSED}`;
-    const isReopened =
-      activity.type === IncidentActivityType.STATUS_CHANGE &&
-      activity.value === `${IncidentStatus.CREATED}` &&
-      activity.previousValue === `${IncidentStatus.CLOSED}`;
 
     // Unknown activity, don't render anything
-    if (!isCreated && !isDetected && !isClosed && !isReopened) {
+    if (!isDetected && !isClosed) {
       return null;
     }
 
@@ -52,10 +52,18 @@ class StatusItem extends React.Component<Props> {
         }}
         header={
           <div>
-            <AuthorName>{authorName}</AuthorName> {isCreated && t('created')}
-            {isDetected && t('detected')}
-            {isClosed && t('closed')}
-            {isReopened && t('re-opened')} {t('an Incident')}
+            {isClosed &&
+              tct('[user] resolved the alert', {
+                user: <AuthorName>{authorName}</AuthorName>,
+              })}
+            {isDetected &&
+              (incident?.alertRule
+                ? tct('[user] was triggered', {
+                    user: <AuthorName>{incident.alertRule.name}</AuthorName>,
+                  })
+                : tct('[user] detected an alert', {
+                    user: <AuthorName>{authorName}</AuthorName>,
+                  }))}
           </div>
         }
         date={getDynamicText({value: activity.dateCreated, fixed: new Date(0)})}
