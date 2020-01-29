d055fa3cd0bf2e6eb3495c053cbd0b6fb436fe18
Lyn Nagara <lyn.nagara@gmail.com>
2020-01-29 11:10:47 -0800

    rewrite group stat test

--- tests/sentry/api/endpoints/test_group_stats.py
+++ tests/sentry/api/endpoints/test_group_stats.py
@@ -1,31 +1,34 @@
 from __future__ import absolute_import
 
-from sentry import tsdb
-from sentry.testutils import APITestCase
+from sentry.testutils import APITestCase, SnubaTestCase
 
 
-class GroupStatsTest(APITestCase):
+class GroupStatsTest(APITestCase, SnubaTestCase):
+    def create_event(self):
+        return self.store_event(data={"fingerprint": ["group_i1"]}, project_id=self.project.id)
+
     def test_simple(self):
         self.login_as(user=self.user)
 
-        group1 = self.create_group()
-        group2 = self.create_group()
+        event = self.create_event()
+        group = event.group
 
-        url = u"/api/0/issues/{}/stats/".format(group1.id)
+        url = u"/api/0/issues/{}/stats/".format(group.id)
         response = self.client.get(url, format="json")
 
         assert response.status_code == 200, response.content
-        for point in response.data:
+        assert response.data[-1][1] == 1
+        for point in response.data[:-1]:
             assert point[1] == 0
         assert len(response.data) == 24
 
-        tsdb.incr(tsdb.models.group, group1.id, count=3)
-        tsdb.incr(tsdb.models.group, group2.id, count=5)
+        for i in range(3):
+            self.create_event()
 
         response = self.client.get(url, format="json")
 
         assert response.status_code == 200, response.content
-        assert response.data[-1][1] == 3, response.data
+        assert response.data[-1][1] == 4, response.data
         for point in response.data[:-1]:
             assert point[1] == 0
         assert len(response.data) == 24
