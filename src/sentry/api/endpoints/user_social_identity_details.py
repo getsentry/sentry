b2195d1567540d6e4268a72f214f9035a1cfb9c7
Joshua Li <josh@jrl.ninja>
2020-01-29 15:01:40 -0800

    add comment about disconnection hack

--- src/sentry/api/endpoints/user_social_identity_details.py
+++ src/sentry/api/endpoints/user_social_identity_details.py
@@ -27,6 +27,24 @@ class UserSocialIdentityDetailsEndpoint(UserEndpoint):
         except UserSocialAuth.DoesNotExist:
             return Response(status=404)
 
+        # HACK(joshuarli): I've tried every method i could come up with to call social
+        # auth's disconnect, but for reasons i fail to understand, the backend object
+        # (e.g. AsanaOAuth2) is strange while it's going through social auth's disconnect
+        # code. For example, you might run into it not having the "strategy" attribute,
+        # which I hacked around with:
+        #
+        #   class AsanaOAuth2(_AsanaOAuth2):
+        #   +     strategy = load_strategy()
+        #
+        # But then you'll likely encounter:
+        # unbound method setting() must be called with AsanaOAuth2 instance as first argument
+        # (got str instance instead)
+        #
+        # And then I haven't been able to figure out how to get past that.
+        # What essentially needs to happen is that the UserSocialAuth needs to be deleted,
+        # which is IMO a better hack than improperly using social auth disconnect.
+        # The only thing we'll lose from this approach is SOCIAL_AUTH_REVOKE_TOKENS_ON_DISCONNECT,
+        # but I don't see that being much of an issue as tokens (at least for asana) expire after an hour.
         auth.delete()
 
         # XXX(dcramer): we experienced an issue where the identity still existed,
