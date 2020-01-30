60f732ebe8269f67520248f7bbc39c05137cbc54
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2020-01-30 15:16:06 -0800

    update props handling

--- src/sentry/static/sentry/app/views/organizationIntegrations/integrationDirectoryAppInstallationDetail.tsx
+++ src/sentry/static/sentry/app/views/organizationIntegrations/integrationDirectoryAppInstallationDetail.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 
 import IntegrationDirectoryApplicationRow from 'app/views/settings/organizationDeveloperSettings/sentryApplicationRow/integrationDirectoryApplicationRow';
-import {Organization, SentryApp, SentryAppInstallation} from 'app/types';
 
-type Props = {
-  organization: Organization;
-  install?: SentryAppInstallation;
-  app: SentryApp;
-};
-
-const SentryAppInstallationDetail = (props: Props) => {
+const SentryAppInstallationDetail = (
+  props: Omit<IntegrationDirectoryApplicationRow['props'], 'isOnIntegrationPage'>
+) => {
   return (
     <React.Fragment>
       <IntegrationDirectoryApplicationRow {...props} isOnIntegrationPage />
