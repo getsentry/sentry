4f7a70b41f11de4628699af426a0069af4f3de49
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2020-01-30 15:09:47 -0800

    ref: Convert class component into functional component

--- src/sentry/static/sentry/app/views/organizationIntegrations/integrationDirectoryAppInstallationDetail.tsx
+++ src/sentry/static/sentry/app/views/organizationIntegrations/integrationDirectoryAppInstallationDetail.tsx
@@ -1,8 +1,6 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 
 import IntegrationDirectoryApplicationRow from 'app/views/settings/organizationDeveloperSettings/sentryApplicationRow/integrationDirectoryApplicationRow';
-import SentryTypes from 'app/sentryTypes';
 import {Organization, SentryApp, SentryAppInstallation} from 'app/types';
 
 type Props = {
@@ -11,27 +9,12 @@ type Props = {
   app: SentryApp;
 };
 
-class SentryAppInstallationDetail extends React.Component<Props> {
-  static propTypes = {
-    organization: SentryTypes.Organization.isRequired,
-    install: PropTypes.object,
-    app: PropTypes.object.isRequired,
-  };
-
-  render() {
-    const {organization, install, app} = this.props;
-
-    return (
-      <React.Fragment>
-        <IntegrationDirectoryApplicationRow
-          app={app}
-          organization={organization}
-          install={install}
-          isOnIntegrationPage
-        />
-      </React.Fragment>
-    );
-  }
-}
+const SentryAppInstallationDetail = (props: Props) => {
+  return (
+    <React.Fragment>
+      <IntegrationDirectoryApplicationRow {...props} isOnIntegrationPage />
+    </React.Fragment>
+  );
+};
 
 export default SentryAppInstallationDetail;
