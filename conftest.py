e5a5e29214a7655c370dcceaed7eb33d8e798173
Stephen Cefali <scefali@sentry.io>
2020-01-31 15:30:16 -0800

    feat(plugins): adds endpoint to return list of plugins with projectList (#16654)

--- conftest.py
+++ conftest.py
@@ -18,11 +18,8 @@ def pytest_configure(config):
     # being used
     warnings.filterwarnings("error", "", Warning, r"^(?!(|kombu|raven|sentry))")
 
-    # if we are running any tests for plugins, we need to make sure we install them first
-    # (for `py.test --version` or `py.test --help`, there are no files to test)
-    test_targets = config.getoption("file_or_dir")
-    if test_targets and any("tests/sentry_plugins" in s for s in test_targets):
-        install_sentry_plugins()
+    # always install plugins for the tests
+    install_sentry_plugins()
 
 
 def install_sentry_plugins():
