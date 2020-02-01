9c678a0134c4e4e58a0e9855578f11b78fd8dcce
Dora Chan <dora.lchan@gmail.com>
2020-01-31 18:34:21 -0800

    change to type

--- src/sentry/static/sentry/app/types/iconProps.tsx
+++ src/sentry/static/sentry/app/types/iconProps.tsx
@@ -1,7 +1,7 @@
-export interface IconProps extends React.SVGAttributes<SVGElement> {
+export type IconProps = React.SVGAttributes<SVGElement> & {
   color: string;
   size: 'xs' | 'sm' | 'md' | 'lg' | 'xl';
   direction?: 'up' | 'right' | 'down' | 'left';
   solid?: boolean;
   circle?: boolean;
-}
+};
