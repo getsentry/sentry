7b2808f6426f07dac0d43dae35ed66b3fc4b7d7e
William Mak <william@wmak.io>
2020-02-03 13:38:41 -0500

    ref: Adding a test for `!has:issue.id` too

--- tests/sentry/api/test_event_search.py
+++ tests/sentry/api/test_event_search.py
@@ -984,6 +984,11 @@ class GetSnubaQueryArgsTest(TestCase):
         assert has_issue_filter.group_ids == []
         assert has_issue_filter.conditions == [[["isNull", ["issue.id"]], "!=", 1]]
 
+    def test_not_has_issue_id(self):
+        has_issue_filter = get_filter("!has:issue.id")
+        assert has_issue_filter.group_ids == []
+        assert has_issue_filter.conditions == [[["isNull", ["issue.id"]], "=", 1]]
+
     def test_message_negative(self):
         assert get_filter('!message:"post_process.process_error HTTPError 403"').conditions == [
             [
