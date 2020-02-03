a68195aafdd99d287477505ac8c2269183715692
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-02-03 09:32:24 +0100

    ref: Bump Relay to 0.5.1 (#16753)
    * ref: Update sentry-relay
    
    * fix: Fix EventManager tests after Relay update
    
    * wip on fixing snuba tests

--- src/sentry/testutils/factories.py
+++ src/sentry/testutils/factories.py
@@ -466,10 +466,12 @@ class Factories(object):
         return useremail
 
     @staticmethod
-    def store_event(data, project_id, assert_no_errors=True):
+    def store_event(data, project_id, assert_no_errors=True, sent_at=None):
         # Like `create_event`, but closer to how events are actually
         # ingested. Prefer to use this method over `create_event`
-        manager = EventManager(data)
+        if sent_at is None:
+            sent_at = timezone.now()
+        manager = EventManager(data, sent_at=sent_at)
         manager.normalize()
         if assert_no_errors:
             errors = manager.get_data().get("errors")
