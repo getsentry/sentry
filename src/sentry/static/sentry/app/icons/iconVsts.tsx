6fd57b932291dbebc38bae8fae975c2a7f2f1c1c
Dora <dora.l.chan@gmail.com>
2020-02-03 16:03:41 -0800

    ref(icons): test library bundle size (#16735)
    * test icon library bundle
    
    * added to storybook
    
    * added to storybook
    
    * updated to absolute values and changed other to props
    
    * removed icon graph prop from iconProps and tested IconSettings in sidebar
    
    * remove old inlinesvgs after all
    
    * updating props of size and making color and size required
    
    * change to type
    
    * added lightning, show, and sync
    
    * update js to jsx
    
    * updated gap and index page

---
+++ src/sentry/static/sentry/app/icons/iconVsts.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import {IconProps} from 'app/types/iconProps';
+import theme from 'app/utils/theme';
+
+export const IconVsts: React.FC<IconProps> = ({
+  color: providedColor = 'currentColor',
+  size: providedSize = 'sm',
+  ...props
+}: IconProps) => {
+  const color = providedColor;
+  const size = theme.iconSizes[providedSize] ?? providedSize;
+
+  return (
+    <svg viewBox="0 0 16 16" fill={color} height={size} width={size} {...props}>
+      <polygon points="0 5.85 1.54 3.81 6.84 2.3 6.84 0 12.57 3.43 2.27 5.54 2.27 11.42 0 10.43 0 5.85" />
+      <polygon points="12.57 3.43 12.57 12.23 2.27 11.42 5.7 16 5.7 13.7 11.91 16 16 12.69 16 2.9 12.57 3.43" />
+    </svg>
+  );
+};
