e2228bbca67242e986a88afbd1e44d93f20ce1b3
Dora Chan <dora.lchan@gmail.com>
2020-02-04 08:54:41 -0800

    blocking off props

--- docs-ui/components/icons.stories.js
+++ docs-ui/components/icons.stories.js
@@ -4,13 +4,96 @@ import {withInfo} from '@storybook/addon-info';
 import styled from '@emotion/styled';
 
 import * as newIconset from 'app/icons';
+import {IconAdd} from 'app/icons/iconAdd';
+import {IconArrow} from 'app/icons/iconArrow';
+import {IconBookmark} from 'app/icons/iconBookmark';
+import {IconGroup} from 'app/icons/iconGroup';
+import {IconPin} from 'app/icons/iconPin';
 
 storiesOf('Style|Icons', module).add(
-  'SVG',
-  withInfo('Replace `InlineSvg` with the following Icon Components')(() => {
+  'Icons',
+  withInfo('Replace `InlineSvg` with the new Icon Components')(() => {
     return (
       <div>
-        <h4>New Icons</h4>
+        <h4>Icon Props</h4>
+        <Section>
+          <Examples>
+            <PropHeader>Circle Prop</PropHeader>
+            <Swatch>
+              <IconAdd />
+              <LabelWrapper>IconAdd</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconAdd circle />
+              <LabelWrapper>IconPin</LabelWrapper>
+            </Swatch>
+          </Examples>
+          <Examples>
+            <PropHeader>Color Prop</PropHeader>
+            <Swatch>
+              <IconBookmark solid />
+              <LabelWrapper>IconBookmark</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconBookmark solid color="#6C5FC7" />
+              <LabelWrapper>IconBookmark</LabelWrapper>
+            </Swatch>
+          </Examples>
+          <Examples>
+            <PropHeader>Solid Prop</PropHeader>
+            <Swatch>
+              <IconPin />
+              <LabelWrapper>IconPin</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconPin solid />
+              <LabelWrapper>IconPin</LabelWrapper>
+            </Swatch>
+          </Examples>
+          <Examples>
+            <PropHeader>Directionality Prop</PropHeader>
+            <Swatch>
+              <IconArrow />
+              <LabelWrapper>IconArrow</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconArrow direction="left" />
+              <LabelWrapper>IconArrow</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconArrow direction="down" />
+              <LabelWrapper>IconArrow</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconArrow direction="right" />
+              <LabelWrapper>IconArrow</LabelWrapper>
+            </Swatch>
+          </Examples>
+          <Examples>
+            <PropHeader>Size Prop</PropHeader>
+            <Swatch>
+              <IconGroup size="xs" />
+              <LabelWrapper>IconGroup size="xs"</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconGroup />
+              <LabelWrapper>IconGroup</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconGroup size="md" />
+              <LabelWrapper>IconGroup size="md"</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconGroup size="lg" />
+              <LabelWrapper>IconGroup size="lg"</LabelWrapper>
+            </Swatch>
+            <Swatch>
+              <IconGroup size="xl" />
+              <LabelWrapper>IconGroup size="xl"</LabelWrapper>
+            </Swatch>
+          </Examples>
+        </Section>
+        <h4>Icon Set</h4>
         <Swatches>
           {Object.entries(newIconset).map(([key, Icon]) => (
             <Swatch key={key}>
@@ -23,6 +106,14 @@ storiesOf('Style|Icons', module).add(
     );
   })
 );
+
+const Section = styled('div')`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  align-content: start;
+  grid-gap: 16px;
+  margin-bottom: 32px;
+`;
 
 const Swatches = styled('div')`
   display: grid;
@@ -40,4 +131,13 @@ const Swatch = styled('div')`
 const LabelWrapper = styled('div')`
   font-size: 12px;
   margin-left: 16px;
+`;
+
+const Examples = styled('div')`
+  border: 1px solid ${p => p.theme.gray1};
+  padding: 16px 24px;
+`;
+
+const PropHeader = styled('h5')`
+  margin-bottom: 8px;
 `;
