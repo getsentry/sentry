dfcfe141dddec8d5210dfc543a27c6bd121ce1b9
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-02-04 11:16:37 -0800

    ♻️ Update nomenclature to reference discover v2

--- src/sentry/tasks/data_export.py
+++ src/sentry/tasks/data_export.py
@@ -3,47 +3,54 @@ from __future__ import absolute_import
 import csv
 from datetime import datetime
 
-from sentry.api import client
 from sentry.constants import ExportQueryType
 from sentry.tasks.base import instrumented_task
+
+CURRENT_TIME = datetime.now()
 
 
 @instrumented_task(name="sentry.tasks.data_export.compile_data", queue="data_export")
 def compile_data(data_export):
-    if data_export.query_type == ExportQueryType.DISCOVER_V1:
-        process_discover_v1(data_export)
-
+    if data_export.query_type == ExportQueryType.DISCOVER_V2:
+        # TODO(Leander): Implement logic for discover v2 CSVs
+        return
     elif data_export.query_type == ExportQueryType.BILLING_REPORT:
         # TODO(Leander): Implement logic for billing report CSVs
         return
     elif data_export.query_type == ExportQueryType.ISSUE_BY_TAG:
-        # TODO(Leander): Implement logic for issue tag CSVs
+        process_issue_by_tag(data_export)
         return
 
 
-def process_discover_v1(data_export):
-    response = client.post(
-        path="/organizations/{}/discover/query/".format(data_export.organization.slug),
-        user=data_export.user,
-        data=data_export.query_info,
-    )
-    response.render()
-    data = response.data["data"]
-    keys = data[0].keys()
-    return convert_to_csv(type=ExportQueryType.DISCOVER_V1_STR, keys=keys, data=data)
+def process_discover_v2(data_export):
+    return
 
 
-def convert_to_csv(type, keys, data):
-    # TODO(Leander): Consider sorting the keys in a consistent order(?)
-    time_string = datetime.now().strftime("%m:%d:%Y__%H:%M")
-    file_path = "/Users/leanderrodrigues/Downloads/{}-{}.csv".format(type, time_string)
+def process_billing_report(data_export):
+    return
 
-    with open(file_path, "w") as csvfile:
+
+def process_issue_by_tag(data_export):
+    return
+
+
+def get_file_name(type, iteration=-1, extension="csv"):
+    time_string = CURRENT_TIME.strftime("%m:%d:%Y")
+    file_name = "{}-{}-{}.{}".format(type, time_string, iteration, extension)
+    return file_name
+
+
+def convert_to_csv(keys, data, file_name, include_header):
+    with open("/Users/leanderrodrigues/Downloads/" + file_name, "w") as csvfile:
         writer = csv.DictWriter(csvfile, fieldnames=keys)
-        writer.writeheader()
+        if include_header:
+            writer.writeheader()
         writer.writerows(data)
         csvfile.close()
-    # TODO(Leander): Upload the file to GCS and record the storage_url in ExportedData
+    return
+
+
+def combine_csvs(csv_list):
     return
 
 
