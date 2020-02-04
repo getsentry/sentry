38daff8526d7dc622d21506fb6e236005e65be77
Matej Minar <matej.minar@sentry.io>
2020-02-04 09:33:09 +0100

    feat(ui): Adds component for displaying version names in releasesV2 (#16769)

--- docs-ui/components/tooltip.stories.js
+++ docs-ui/components/tooltip.stories.js
@@ -12,68 +12,90 @@ class CustomThing extends React.Component {
   }
 }
 
-storiesOf('UI|Tooltip', module)
-  .add(
-    'Tooltip',
-    withInfo({
-      text: 'Adds a tooltip to any component,',
-      propTablesExclude: [CustomThing, Button, 'Button'],
-    })(() => {
-      const title = text('tooltip', 'Basic tooltip content');
-      const disabled = boolean('Disabled', false);
-      const displayMode = select('Container display mode', ['block', 'inline-block', 'inline']);
-      const position = select(
-        'position',
-        {top: 'top', bottom: 'bottom', left: 'left', right: 'right'},
-        'top'
-      );
+storiesOf('UI|Tooltip', module).add(
+  'Tooltip',
+  withInfo({
+    text: 'Adds a tooltip to any component,',
+    propTablesExclude: [CustomThing, Button, 'Button'],
+  })(() => {
+    const title = text('tooltip', 'Basic tooltip content');
+    const disabled = boolean('Disabled', false);
+    const displayMode = select('Container display mode', [
+      'block',
+      'inline-block',
+      'inline',
+    ]);
+    const position = select(
+      'position',
+      {top: 'top', bottom: 'bottom', left: 'left', right: 'right'},
+      'top'
+    );
+    const isHoverable = boolean('isHoverable', false);
 
-      return (
-        <React.Fragment>
-          <h3>With styled component trigger</h3>
-          <p>
-            <Tooltip title={title} position={position} disabled={disabled} containerDisplayMode={displayMode}>
-              <Button>Styled button</Button>
-            </Tooltip>
-          </p>
+    return (
+      <React.Fragment>
+        <h3>With styled component trigger</h3>
+        <p>
+          <Tooltip
+            title={title}
+            position={position}
+            disabled={disabled}
+            containerDisplayMode={displayMode}
+            isHoverable={isHoverable}
+          >
+            <Button>Styled button</Button>
+          </Tooltip>
+        </p>
 
-          <h3>With class component trigger</h3>
-          <p>
-            <Tooltip title={title} position={position} disabled={disabled}>
-              <CustomThing>Custom React Component</CustomThing>
-            </Tooltip>
-          </p>
+        <h3>With class component trigger</h3>
+        <p>
+          <Tooltip
+            title={title}
+            position={position}
+            disabled={disabled}
+            isHoverable={isHoverable}
+          >
+            <CustomThing>Custom React Component</CustomThing>
+          </Tooltip>
+        </p>
 
-          <h3>With an SVG element trigger</h3>
-          <p>
-            <svg
-              viewBox="0 0 100 100"
-              width="100"
-              height="100"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <Tooltip title={title} position={position} disabled={disabled} containerDisplayMode={displayMode}>
-                <circle cx="50" cy="50" r="50" />
-              </Tooltip>
-            </svg>
-          </p>
-
-          <h3>With element title and native html element</h3>
-          <p>
+        <h3>With an SVG element trigger</h3>
+        <p>
+          <svg
+            viewBox="0 0 100 100"
+            width="100"
+            height="100"
+            xmlns="http://www.w3.org/2000/svg"
+          >
             <Tooltip
-              title={
-                <span>
-                  <em>so strong</em>
-                </span>
-              }
-              containerDisplayMode={displayMode}
+              title={title}
               position={position}
               disabled={disabled}
+              containerDisplayMode={displayMode}
+              isHoverable={isHoverable}
             >
-              <button>Native button</button>
+              <circle cx="50" cy="50" r="50" />
             </Tooltip>
-          </p>
-        </React.Fragment>
-      );
-    })
-  );
+          </svg>
+        </p>
+
+        <h3>With element title and native html element</h3>
+        <p>
+          <Tooltip
+            title={
+              <span>
+                <em>so strong</em>
+              </span>
+            }
+            containerDisplayMode={displayMode}
+            position={position}
+            disabled={disabled}
+            isHoverable={isHoverable}
+          >
+            <button>Native button</button>
+          </Tooltip>
+        </p>
+      </React.Fragment>
+    );
+  })
+);
