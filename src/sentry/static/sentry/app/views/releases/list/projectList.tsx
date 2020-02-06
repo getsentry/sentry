11eb0317e5507372d70b44e0bd90dd08bb994b4a
Matej Minar <matej.minar@sentry.io>
2020-02-06 23:35:48 +0100

    hovercard styles

--- src/sentry/static/sentry/app/views/releases/list/projectList.tsx
+++ src/sentry/static/sentry/app/views/releases/list/projectList.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
 
-import {tct} from 'app/locale';
+import {tct, t} from 'app/locale';
 import ProjectBadge from 'app/components/idBadge/projectBadge';
 import {Project} from 'app/types';
 import space from 'app/styles/space';
 import GlobalSelectionLink from 'app/components/globalSelectionLink';
-import Hovercard from 'app/components/hovercard';
+import Hovercard, {Body as HoverCardBody} from 'app/components/hovercard';
 
 const MAX_PROJECTS_IN_HOVERCARD = 5;
 
@@ -29,6 +29,7 @@ const ProjectList = ({projects, orgId, version, maxLines = 2}: Props) => {
     hiddenProjects = projects.slice(maxLines - 1, projects.length);
   }
 
+  const hoverCardHead = t('Release Projects');
   const hovercardBody = (
     <HovercardContentWrapper>
       <ProjectList
@@ -47,6 +48,7 @@ const ProjectList = ({projects, orgId, version, maxLines = 2}: Props) => {
       ))}
       {hiddenProjects.length > 0 && (
         <StyledHovercard
+          header={hoverCardHead}
           body={hovercardBody}
           show={hiddenProjects.length <= MAX_PROJECTS_IN_HOVERCARD ? undefined : false}
         >
@@ -72,16 +74,19 @@ const StyledProjectBadge = styled(ProjectBadge)`
 const StyledHovercard = styled(Hovercard)`
   width: auto;
   max-width: 295px;
+  ${HoverCardBody} {
+    padding-bottom: 0;
+  }
 `;
 const HovercardContentWrapper = styled('div')`
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-start;
   flex-wrap: wrap;
+  font-size: ${p => p.theme.fontSizeMedium};
   ${StyledProjectBadge} {
-    margin-left: ${space(1)};
-    margin-right: ${space(1)};
-    margin-bottom: ${space(0.5)};
+    margin-right: ${space(3)};
+    margin-bottom: ${space(2)};
   }
 `;
 export default ProjectList;
