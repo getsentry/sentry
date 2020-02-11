38c9465b5a9880f7a4bd6e1eac4c01629873dc5f
Chris Fuller <cfuller@sentry.io>
2020-02-11 12:01:15 -0500

    Reverting settings change

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1068,8 +1068,7 @@ SENTRY_TSDB_OPTIONS = {}
 SENTRY_NEWSLETTER = "sentry.newsletter.base.Newsletter"
 SENTRY_NEWSLETTER_OPTIONS = {}
 
-# SENTRY_EVENTSTREAM = "sentry.eventstream.snuba.SnubaEventStream"
-SENTRY_EVENTSTREAM = "sentry.eventstream.kafka.KafkaEventStream"
+SENTRY_EVENTSTREAM = "sentry.eventstream.snuba.SnubaEventStream"
 SENTRY_EVENTSTREAM_OPTIONS = {}
 
 # rollups must be ordered from highest granularity to lowest
