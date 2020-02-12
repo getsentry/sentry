a06c9deffc46185e1874eea593ab1b6e35b3f4d0
Chris Fuller <cfuller@sentry.io>
2020-02-12 15:41:54 -0500

    Defaulting to 'open' status

--- src/sentry/static/sentry/app/views/alerts/list/index.tsx
+++ src/sentry/static/sentry/app/views/alerts/list/index.tsx
@@ -132,6 +132,13 @@ class IncidentsListContainer extends React.Component<Props> {
    * Incidents list is currently at the organization level, but the link needs to
    * go down to a specific project scope.
    */
+  // componentWillMount() {
+  //   const {params, location, router} = this.props;
+  //   if (location.query.status === undefined) {
+  //     navigateTo(`/organizations/${params && params.orgId}/alerts/?status=open`, router);
+  //   }
+  // }
+
   handleNavigateToSettings = (e: React.MouseEvent) => {
     const {router, params} = this.props;
     e.preventDefault();
