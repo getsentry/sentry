df4d757e64f24d29a244915266719e5ff76d575f
William Mak <william@wmak.io>
2020-02-12 16:45:55 -0500

    ref: Use getlist for project ids instead
    - While this means that we won't be checking that the actor has
      permissions before serving from cache. The check should still happen
      when cache is being set. Which should mean at worst an actor had
      permissions, set the cache and was revoked permissions. They'd have
      access to the release for only 60s.
      - They shouldn't have access to other projects since the key would
        never match, and won't get set by the cache.

--- src/sentry/api/bases/organization.py
+++ src/sentry/api/bases/organization.py
@@ -322,20 +322,19 @@ class OrganizationReleasesBaseEndpoint(OrganizationEndpoint):
         """
         actor_id = None
         has_perms = None
-        projects = self.get_projects(request, organization)
         if getattr(request, "user", None) and request.user.id:
             actor_id = "user:%s" % request.user.id
         if getattr(request, "auth", None) and request.auth.id:
             actor_id = "apikey:%s" % request.auth.id
         if actor_id is not None:
+            project_ids = sorted(set(map(int, request.GET.getlist("project"))))
             key = "release_perms:1:%s" % hash_values(
-                [actor_id, organization.id, release.id]
-                + sorted([project.id for project in projects])
+                [actor_id, organization.id, release.id] + project_ids
             )
             has_perms = cache.get(key)
         if has_perms is None:
             has_perms = ReleaseProject.objects.filter(
-                release=release, project__in=projects
+                release=release, project__in=self.get_projects(request, organization)
             ).exists()
             if actor_id is not None:
                 cache.set(key, has_perms, 60)
