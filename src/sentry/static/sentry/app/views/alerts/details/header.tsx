05744e8bd4b81639b34b742bc06fb826e8c2882e
Billy Vong <billyvg@users.noreply.github.com>
2020-02-12 10:52:05 -0800

    fix(workflow): Do not show "Resolve" dropdown for resolved alerts (#16929)
    I erroneously thought that `disabled` would accomplish what this does

--- src/sentry/static/sentry/app/views/alerts/details/header.tsx
+++ src/sentry/static/sentry/app/views/alerts/details/header.tsx
@@ -40,26 +40,27 @@ export default class DetailsHeader extends React.Component<Props> {
     const {incident, onStatusChange} = this.props;
 
     const isIncidentOpen = incident && isOpen(incident);
+    const statusLabel = incident ? <Status isSmall incident={incident} /> : null;
 
     return (
-      <Access
-        access={['org:write']}
-        renderNoAccessMessage={() =>
-          incident ? <Status isSmall incident={incident} /> : null
+      <Access access={['org:write']}>
+        {({hasAccess}) =>
+          hasAccess && isIncidentOpen ? (
+            <DropdownControl
+              data-test-id="status-dropdown"
+              label={statusLabel}
+              menuWidth="180px"
+              alignRight
+              buttonProps={{size: 'small', disabled: !incident}}
+            >
+              <StyledMenuItem onSelect={onStatusChange}>
+                <ResolveIcon src="icon-circle-check" /> {t('Resolve this incident')}
+              </StyledMenuItem>
+            </DropdownControl>
+          ) : (
+            statusLabel
+          )
         }
-      >
-        <DropdownControl
-          data-test-id="status-dropdown"
-          label={incident && <Status isSmall incident={incident} />}
-          menuWidth="180px"
-          alignRight
-          disabled={!isIncidentOpen}
-          buttonProps={{size: 'small', disabled: !incident}}
-        >
-          <StyledMenuItem onSelect={onStatusChange}>
-            <ResolveIcon src="icon-circle-check" /> {t('Resolve this incident')}
-          </StyledMenuItem>
-        </DropdownControl>
       </Access>
     );
   }
