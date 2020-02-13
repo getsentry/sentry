051ac54073bc9e77b8c2d4acd5c040ae39298d4a
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2020-02-13 10:54:05 -0800

    ref(UI): Comment out Tags from Integration Directory

--- src/sentry/static/sentry/app/views/organizationIntegrations/integrationListDirectory.tsx
+++ src/sentry/static/sentry/app/views/organizationIntegrations/integrationListDirectory.tsx
@@ -366,13 +366,13 @@ class OrganizationIntegrations extends AsyncComponent<
     const {reloading, displayedList} = this.state;
 
     const title = t('Integrations');
-    const tags = [
-      'Source Control',
-      'Ticketing',
-      'Data Forwarding',
-      'Release Management',
-      'Notifications',
-    ];
+    // const tags = [
+    //   'Source Control',
+    //   'Ticketing',
+    //   'Data Forwarding',
+    //   'Release Management',
+    //   'Notifications',
+    // ];
     return (
       <React.Fragment>
         <SentryDocumentTitle title={title} objSlug={orgId} />
@@ -384,17 +384,19 @@ class OrganizationIntegrations extends AsyncComponent<
           providers={this.providers}
           onInstall={this.onInstall}
         />
-        <SearchInput
-          value={this.state.searchInput || ''}
-          onChange={this.onSearchChange}
-          placeholder="Find a new integration, or one you already use."
-          width="100%"
-        />
-        <TagsContainer>
+        <SearchContainer>
+          <SearchInput
+            value={this.state.searchInput || ''}
+            onChange={this.onSearchChange}
+            placeholder="Find a new integration, or one you already use."
+            width="100%"
+          />
+        </SearchContainer>
+        {/* <TagsContainer>
           {tags.map(tag => (
             <Tag key={tag}>{tag}</Tag>
           ))}
-        </TagsContainer>
+        </TagsContainer> */}
         <Panel>
           <PanelHeader disablePadding>
             <Heading>{t('Integrations')}</Heading>
@@ -420,34 +422,40 @@ const Heading = styled('div')`
   padding-right: ${space(2)};
 `;
 
-const TagsContainer = styled('div')`
+const SearchContainer = styled('div')`
   display: flex;
-  flex-wrap: wrap;
-  padding-top: ${space(3)};
-  padding-bottom: ${space(1)};
+  width: 100%;
+  margin-bottom: ${space(2)};
 `;
 
-const Tag = styled('span')`
-  transition: border-color 0.15s ease;
-  font-size: 14px;
-  line-height: 1;
-  padding: ${space(1)};
-  margin: 0 ${space(1)} ${space(1)} 0;
-  border: 1px solid ${p => p.theme.borderDark};
-  border-radius: 30px;
-  height: 28px;
-  box-shadow: inset ${p => p.theme.dropShadowLight};
-  cursor: pointer;
+// const TagsContainer = styled('div')`
+//   display: flex;
+//   flex-wrap: wrap;
+//   padding-top: ${space(3)};
+//   padding-bottom: ${space(1)};
+// `;
 
-  &:focus {
-    outline: none;
-    border: 1px solid ${p => p.theme.gray1};
-  }
+// const Tag = styled('span')`
+//   transition: border-color 0.15s ease;
+//   font-size: 14px;
+//   line-height: 1;
+//   padding: ${space(1)};
+//   margin: 0 ${space(1)} ${space(1)} 0;
+//   border: 1px solid ${p => p.theme.borderDark};
+//   border-radius: 30px;
+//   height: 28px;
+//   box-shadow: inset ${p => p.theme.dropShadowLight};
+//   cursor: pointer;
 
-  &::placeholder {
-    color: ${p => p.theme.gray2};
-  }
-`;
+//   &:focus {
+//     outline: none;
+//     border: 1px solid ${p => p.theme.gray1};
+//   }
+
+//   &::placeholder {
+//     color: ${p => p.theme.gray2};
+//   }
+// `;
 
 export default withOrganization(OrganizationIntegrations);
 export {OrganizationIntegrations};
