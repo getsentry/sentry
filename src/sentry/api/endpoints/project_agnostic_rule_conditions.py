8ec24c0bf4b565411706742b2e731f3c66c1078e
jeffkwoh <23180853+jeffkwoh@users.noreply.github.com>
2020-02-12 16:40:25 -0800

    Sort imports to be PEP 8 compliant

--- src/sentry/api/endpoints/project_agnostic_rule_conditions.py
+++ src/sentry/api/endpoints/project_agnostic_rule_conditions.py
@@ -1,11 +1,11 @@
 from __future__ import absolute_import
 
+from rest_framework import status
+from rest_framework.response import Response
 
 from sentry import features
 from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.rules import rules
-from rest_framework import status
-from rest_framework.response import Response
 
 
 class ProjectAgnosticRuleConditionsEndpoint(OrganizationEndpoint):
