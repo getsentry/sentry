49ba6564497788a090fd98c3d26141614849fc0f
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2020-02-13 12:03:17 -0800

    shared styled components

---
+++ src/sentry/static/sentry/app/views/organizationIntegrations/shared/styledComponents.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import styled from '@emotion/styled';
+import {withTheme} from 'emotion-theming';
+import space from 'app/styles/space';
+
+const TagsContainer = styled('div')`
+  display: flex;
+  flex-wrap: wrap;
+  padding-top: ${space(3)};
+  padding-bottom: ${space(1)};
+`;
+
+const Tag = styled(
+  withTheme((props: {theme?: any}) => {
+    return <span />;
+  })
+)`
+  transition: border-color 0.15s ease;
+  font-size: 14px;
+  line-height: 1;
+  padding: ${space(1)};
+  margin: 0 ${space(1)} ${space(1)} 0;
+  border: 1px solid ${p => p.theme.borderDark};
+  border-radius: 30px;
+  height: 28px;
+  box-shadow: inset ${p => p.theme.dropShadowLight};
+  cursor: pointer;
+
+  &:focus {
+    outline: none;
+    border: 1px solid ${p => p.theme.gray1};
+  }
+
+  &::placeholder {
+    color: ${p => p.theme.gray2};
+  }
+`;
