66b12d4931b4724e9aedd7f722118dcfb2b99278
Mark Story <mark@sentry.io>
2020-02-14 11:44:27 -0500

    fix(discover) Track query failures in sentry (#17032)
    Capture the failures when queries fail to execute. We are no capturing
    a few kinds of errors:
    
    * Retention errors as we cannot help fix those.
    * Query syntax errors as those are invalid data from
      customers, and we can't fix those.
    * Rate limits and memory exceeded errors would also be captured in
      snuba.

--- src/sentry/api/endpoints/organization_events.py
+++ src/sentry/api/endpoints/organization_events.py
@@ -2,6 +2,8 @@ from __future__ import absolute_import
 
 import logging
 import six
+import sentry_sdk
+
 from functools import partial
 from django.utils.http import urlquote
 from rest_framework.response import Response
@@ -159,21 +161,13 @@ class OrganizationEventsV2Endpoint(OrganizationEventsEndpointBase):
             )
         except discover.InvalidSearchQuery as error:
             raise ParseError(detail=six.text_type(error))
-        except (snuba.SnubaError, snuba.QueryOutsideRetentionError) as error:
-            logger.info(
-                "organization.events.snuba-error",
-                extra={
-                    "organization_id": organization.id,
-                    "user_id": request.user.id,
-                    "error": six.text_type(error),
-                },
-            )
+        except snuba.QueryOutsideRetentionError:
+            raise ParseError(detail="Invalid date range. Please try a more recent date range.")
+        except snuba.QueryIllegalTypeOfArgument:
+            raise ParseError(detail="Invalid query. Argument to function is wrong type.")
+        except snuba.SnubaError as error:
             message = "Internal error. Please try again."
-            if isinstance(error, snuba.QueryIllegalTypeOfArgument):
-                message = "Invalid query. Argument to function is wrong type."
-            elif isinstance(error, snuba.QueryOutsideRetentionError):
-                message = "Invalid date range. Please try a more recent date range."
-            elif isinstance(
+            if isinstance(
                 error,
                 (
                     snuba.RateLimitExceeded,
@@ -190,7 +184,8 @@ class OrganizationEventsV2Endpoint(OrganizationEventsEndpointBase):
                     snuba.SchemaValidationError,
                 ),
             ):
-                message = "Invalid query."
+                sentry_sdk.capture_exception(error)
+                message = "Internal error. Your query failed to run."
 
             raise ParseError(detail=message)
 
