d4d50d50f9fed16b3463c469c136626d966433ef
Priscila Oliveira <priscila@Sentrys-MacBook-Pro.local>
2020-02-17 09:46:32 +0100

    fix(lint): fixed lint errors

--- tests/js/spec/views/projectInstall/issueAlertOptions.spec.jsx
+++ tests/js/spec/views/projectInstall/issueAlertOptions.spec.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {mountWithTheme} from 'sentry-test/enzyme';
 
+import {mountWithTheme} from 'sentry-test/enzyme';
 import IssueAlertOptions from 'app/views/projectInstall/issueAlertOptions';
 import {initializeOrg} from 'sentry-test/initializeOrg';
 import {
--- tests/js/spec/views/releases/list/projectList.spec.jsx
+++ tests/js/spec/views/releases/list/projectList.spec.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import {mountWithTheme} from 'sentry-test/enzyme';
 import ProjectList from 'app/views/releases/list/projectList.tsx';
 
