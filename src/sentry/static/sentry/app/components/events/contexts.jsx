a78bec2a6add01cdd96692227a3fb0f1408dab54
Priscila Oliveira <priscilawebdev@gmail.com>
2020-02-17 09:27:11 +0100

    ref(ui): Added tooltip - Datascrubbers v2 - Part 3 (#16974)

--- src/sentry/static/sentry/app/components/events/contexts.jsx
+++ src/sentry/static/sentry/app/components/events/contexts.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-
 import get from 'lodash/get';
+
 import {objectIsEmpty, toTitleCase, defined} from 'app/utils';
 import EventDataSection from 'app/components/events/eventDataSection';
 import plugins from 'app/plugins';
@@ -10,7 +10,7 @@ const CONTEXT_TYPES = {
   default: require('app/components/events/contexts/default').default,
   app: require('app/components/events/contexts/app').default,
   device: require('app/components/events/contexts/device').default,
-  os: require('app/components/events/contexts/os').default,
+  os: require('app/components/events/contexts/operatingSystem/operatingSystem').default,
   runtime: require('app/components/events/contexts/runtime').default,
   user: require('app/components/events/contexts/user').default,
   gpu: require('app/components/events/contexts/gpu').default,
