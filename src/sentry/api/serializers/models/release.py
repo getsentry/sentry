363717d1263c226ce6229f183d0b5772de36d179
Joshua Li <josh@jrl.ninja>
2020-02-18 11:58:08 -0800

    devservices: use six.moves for izip, zip_longest
    (cherry picked from commit af954a52244616f747be7760750c9c87f705fe11)

--- src/sentry/api/serializers/models/release.py
+++ src/sentry/api/serializers/models/release.py
@@ -1,10 +1,10 @@
 from __future__ import absolute_import
 
 import six
+from six.moves import zip
 
 from collections import defaultdict
 from django.db.models import Sum
-from itertools import izip
 
 from sentry import tagstore
 from sentry.api.serializers import Serializer, register, serialize
@@ -107,7 +107,7 @@ class ReleaseSerializer(Serializer):
         commit_ids = set((o.last_commit_id for o in item_list if o.last_commit_id))
         if commit_ids:
             commit_list = list(Commit.objects.filter(id__in=commit_ids).select_related("author"))
-            commits = {c.id: d for c, d in izip(commit_list, serialize(commit_list, user))}
+            commits = {c.id: d for c, d in zip(commit_list, serialize(commit_list, user))}
         else:
             commits = {}
 
@@ -143,7 +143,7 @@ class ReleaseSerializer(Serializer):
         deploy_ids = set((o.last_deploy_id for o in item_list if o.last_deploy_id))
         if deploy_ids:
             deploy_list = list(Deploy.objects.filter(id__in=deploy_ids))
-            deploys = {d.id: c for d, c in izip(deploy_list, serialize(deploy_list, user))}
+            deploys = {d.id: c for d, c in zip(deploy_list, serialize(deploy_list, user))}
         else:
             deploys = {}
 
