def069afd85284bc58963942bfb08965019440e4
Joshua Li <josh@jrl.ninja>
2020-02-20 15:24:16 -0800

    undo this false positive because reserved name was used

--- src/sentry/api/serializers/models/user.py
+++ src/sentry/api/serializers/models/user.py
@@ -20,13 +20,12 @@ from sentry.models import (
 )
 from sentry.auth.superuser import is_active_superuser
 from sentry.utils.avatar import get_gravatar_url
-from six.moves import filter
 
 
-def manytoone_to_dict(queryset, key, filter=None):
+def manytoone_to_dict(queryset, key, filter_func=None):
     result = defaultdict(list)
     for row in queryset:
-        if filter and not list(filter(row)):
+        if filter_func and not filter_func(row):
             continue
         result[getattr(row, key)].append(row)
     return result
