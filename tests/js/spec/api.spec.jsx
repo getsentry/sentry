eb2e68b42072252d6f941b61faf38ca7af9059b8
Billy Vong <billy@sentry.io>
2020-02-20 17:11:41 -0800

    feat(sentry): Create 2nd Sentry client for frontend http requests
    This was creating a lot of noise, and while I believe has some value, I think the noise outweighs its value. Lets move this to a separate project and maybe in the future we can evaluate if we can have APM transactions solve any needs.

--- tests/js/spec/api.spec.jsx
+++ tests/js/spec/api.spec.jsx
@@ -279,24 +279,5 @@ describe('api', function() {
         return {};
       });
     });
-
-    it('reports correct error and stacktrace to Sentry', async function() {
-      api.request('/some/url/');
-      await tick();
-
-      const errorObjectSentryCalled = Sentry.captureException.mock.calls[0][0];
-      expect(errorObjectSentryCalled.name).toBe('InternalServerError');
-      expect(errorObjectSentryCalled.message).toBe('GET /some/url/ 500');
-
-      // First line of stack should be this test case
-      expect(errorObjectSentryCalled.stack.split('\n')[1]).toContain('api.spec.jsx');
-    });
-
-    it('reports correct error and stacktrace to Sentry when using promises', async function() {
-      await expect(
-        api.requestPromise('/some/url/')
-      ).rejects.toThrowErrorMatchingInlineSnapshot('"GET /some/url/ 500"');
-      expect(Sentry.captureException).toHaveBeenCalled();
-    });
   });
 });
