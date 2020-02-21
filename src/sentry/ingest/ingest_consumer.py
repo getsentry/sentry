ba6b2be4ffae3bc48dd4155a3ea2d9cf345ebc89
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-02-21 14:31:05 +0100

    feat: Disable auto gc for ingest consumers

--- src/sentry/ingest/ingest_consumer.py
+++ src/sentry/ingest/ingest_consumer.py
@@ -238,5 +238,8 @@ def get_ingest_consumer(consumer_type, once=False, concurrency=None, **options):
     """
     topic_name = ConsumerType.get_topic_name(consumer_type)
     return create_batching_kafka_consumer(
-        topic_name=topic_name, worker=IngestConsumerWorker(concurrency=concurrency), **options
+        topic_name=topic_name,
+        worker=IngestConsumerWorker(concurrency=concurrency),
+        pause_gc=True,
+        **options
     )
