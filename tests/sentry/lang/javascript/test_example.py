648e2b137e18b376b0fa140cbabda04cc2fd588e
Radu Woinaroski <radu@sentry.io>
2020-02-24 18:53:44 +0100

    removed code duplication

--- tests/sentry/lang/javascript/test_example.py
+++ tests/sentry/lang/javascript/test_example.py
@@ -9,7 +9,7 @@ import pytest
 import requests
 
 from sentry import eventstore
-from sentry.testutils import TestCase, TransactionTestCase, adjust_settings_for_relay_tests
+from sentry.testutils import TransactionTestCase, adjust_settings_for_relay_tests
 from sentry.testutils.helpers import get_auth_header
 from sentry.testutils.helpers.datetime import iso_format, before_now
 
@@ -23,8 +23,10 @@ def load_fixture(name):
         return f.read()
 
 
-@pytest.mark.uses_sentry_store
-class ExampleTestCase_LEGACY(TestCase):
+class ExampleTestCase:
+    def post_and_retrieve_event(self, data):
+        raise NotImplemented("post and retrieve event should be implemented in dervied test class")
+
     @responses.activate
     def test_sourcemap_expansion(self):
         responses.add(
@@ -65,11 +67,7 @@ class ExampleTestCase_LEGACY(TestCase):
             },
         }
 
-        resp = self._postWithHeader(data)
-        assert resp.status_code == 200
-        event_id = json.loads(resp.content)["id"]
-
-        event = eventstore.get_event_by_id(self.project.id, event_id)
+        event = self.post_and_retrieve_event(data)
 
         exception = event.interfaces["exception"]
         frame_list = exception.values[0].stacktrace.frames
@@ -97,63 +95,19 @@ class ExampleTestCase_LEGACY(TestCase):
         assert frame_list[3].filename == "test.js"
 
 
-@pytest.mark.uses_relay_store
-class ExampleTestCase(TransactionTestCase):
-    def setUp(self):  # NOQA
-        self.auth_header = get_auth_header(
-            "TEST_USER_AGENT/0.0.0", self.projectkey.public_key, self.projectkey.secret_key, "7"
-        )
-        adjust_settings_for_relay_tests(self.settings)
+class ExampleTestCaseLegacy(ExampleTestCase, TransactionTestCase):
+    def post_and_retrieve_event(self, data):
+        resp = self._postWithHeader(data)
+        assert resp.status_code == 200
+        event_id = json.loads(resp.content)["id"]
 
-    @pytest.fixture(autouse=True)
-    def setup_fixtures(self, settings, live_server, get_relay_store_url, wait_for_ingest_consumer):
-        self.settings = settings
-        self.get_relay_store_url = get_relay_store_url  # noqa
-        self.wait_for_ingest_consumer = wait_for_ingest_consumer(settings)  # noqa
+        event = eventstore.get_event_by_id(self.project.id, event_id)
+        assert event is not None
+        return event
 
-    @responses.activate
-    def test_sourcemap_expansion(self):
-        responses.add(
-            responses.GET,
-            "http://example.com/test.js",
-            body=load_fixture("test.js"),
-            content_type="application/javascript",
-        )
-        responses.add(
-            responses.GET,
-            "http://example.com/test.min.js",
-            body=load_fixture("test.min.js"),
-            content_type="application/javascript",
-        )
-        responses.add(
-            responses.GET,
-            "http://example.com/test.map",
-            body=load_fixture("test.map"),
-            content_type="application/json",
-        )
-        responses.add(responses.GET, "http://example.com/index.html", body="Not Found", status=404)
 
-        min_ago = iso_format(before_now(minutes=1))
-
-        data = {
-            "timestamp": min_ago,
-            "message": "hello",
-            "platform": "javascript",
-            "exception": {
-                "values": [
-                    {
-                        "type": "Error",
-                        "stacktrace": {
-                            "frames": json.loads(load_fixture("minifiedError.json"))[::-1]
-                        },
-                    }
-                ]
-            },
-        }
-
-        # all requests are now passed through responses,
-        # we need the store request to go to the actual Relay server
-        # disable responses for the store URL
+class ExampleTestCaseRelay(ExampleTestCase, TransactionTestCase):
+    def post_and_retrieve_event(self, data):
         url = self.get_relay_store_url(self.project.id)
         responses.add_passthru(url)
         resp = requests.post(
@@ -169,28 +123,18 @@ class ExampleTestCase(TransactionTestCase):
         event = self.wait_for_ingest_consumer(
             lambda: eventstore.get_event_by_id(self.project.id, event_id)
         )
+        # check that we found it in Snuba
+        assert event is not None
+        return event
 
-        exception = event.interfaces["exception"]
-        frame_list = exception.values[0].stacktrace.frames
+    def setUp(self):  # NOQA
+        self.auth_header = get_auth_header(
+            "TEST_USER_AGENT/0.0.0", self.projectkey.public_key, self.projectkey.secret_key, "7"
+        )
+        adjust_settings_for_relay_tests(self.settings)
 
-        assert len(frame_list) == 4
-
-        import pprint
-
-        pprint.pprint(frame_list)
-
-        assert frame_list[0].function == "produceStack"
-        assert frame_list[0].lineno == 6
-        assert frame_list[0].filename == "index.html"
-
-        assert frame_list[1].function == "test"
-        assert frame_list[1].lineno == 20
-        assert frame_list[1].filename == "test.js"
-
-        assert frame_list[2].function == "invoke"
-        assert frame_list[2].lineno == 15
-        assert frame_list[2].filename == "test.js"
-
-        assert frame_list[3].function == "onFailure"
-        assert frame_list[3].lineno == 5
-        assert frame_list[3].filename == "test.js"
+    @pytest.fixture(autouse=True)
+    def setup_fixtures(self, settings, live_server, get_relay_store_url, wait_for_ingest_consumer):
+        self.settings = settings
+        self.get_relay_store_url = get_relay_store_url  # noqa
+        self.wait_for_ingest_consumer = wait_for_ingest_consumer(settings)  # noqa
