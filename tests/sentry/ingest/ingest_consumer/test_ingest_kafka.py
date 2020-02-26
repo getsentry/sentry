52524872452b2bcd24f92b70064f8aa790c0cff7
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-02-26 15:13:10 +0100

    ref(ingest): Completely inline save_event into consumer for transactions (#17280)
    Follow up to #17191

--- tests/sentry/ingest/ingest_consumer/test_ingest_kafka.py
+++ tests/sentry/ingest/ingest_consumer/test_ingest_kafka.py
@@ -19,40 +19,59 @@ logger = logging.getLogger(__name__)
 MAX_POLL_ITERATIONS = 100
 
 
-def _get_test_message(project):
+@pytest.fixture(params=["transaction", "event"])
+def get_test_message(request, default_project):
     """
     creates a test message to be inserted in a kafka queue
     """
-    now = datetime.datetime.now()
-    # the event id should be 32 digits
-    event_id = "{}".format(now.strftime("000000000000%Y%m%d%H%M%S%f"))
-    message_text = "some message {}".format(event_id)
-    project_id = project.id  # must match the project id set up by the test fixtures
-    event = {
-        "message": message_text,
-        "extra": {"the_id": event_id},
-        "project_id": project_id,
-        "event_id": event_id,
-    }
 
-    em = EventManager(event, project=project)
-    em.normalize()
-    normalized_event = dict(em.get_data())
-    message = {
-        "type": "event",
-        "start_time": time.time(),
-        "event_id": event_id,
-        "project_id": int(project_id),
-        "payload": json.dumps(normalized_event),
-    }
+    def inner(project=default_project):
+        now = datetime.datetime.now()
+        # the event id should be 32 digits
+        event_id = "{}".format(now.strftime("000000000000%Y%m%d%H%M%S%f"))
+        message_text = "some message {}".format(event_id)
+        project_id = project.id  # must match the project id set up by the test fixtures
+        if request.param == "transaction":
+            event = {
+                "type": "transaction",
+                "timestamp": now.isoformat(),
+                "start_timestamp": now.isoformat(),
+                "event_id": event_id,
+                "spans": [],
+                "contexts": {
+                    "trace": {
+                        "parent_span_id": "8988cec7cc0779c1",
+                        "type": "trace",
+                        "op": "foobar",
+                        "trace_id": "a7d67cf796774551a95be6543cacd459",
+                        "span_id": "babaae0d4b7512d9",
+                        "status": "ok",
+                    }
+                },
+            }
+        elif request.param == "event":
+            event = {"message": message_text, "extra": {"the_id": event_id}, "event_id": event_id}
 
-    val = msgpack.packb(message)
-    return val, event_id
+        em = EventManager(event, project=project)
+        em.normalize()
+        normalized_event = dict(em.get_data())
+        message = {
+            "type": request.param,
+            "start_time": time.time(),
+            "event_id": event_id,
+            "project_id": int(project_id),
+            "payload": json.dumps(normalized_event),
+        }
+
+        val = msgpack.packb(message)
+        return val, event_id
+
+    return inner
 
 
 @pytest.mark.django_db(transaction=True)
 def test_ingest_consumer_reads_from_topic_and_calls_celery_task(
-    task_runner, kafka_producer, kafka_admin, requires_kafka, default_project
+    task_runner, kafka_producer, kafka_admin, requires_kafka, default_project, get_test_message
 ):
     group_id = "test-consumer"
     topic_event_name = ConsumerType.get_topic_name(ConsumerType.Events)
@@ -63,9 +82,9 @@ def test_ingest_consumer_reads_from_topic_and_calls_celery_task(
 
     event_ids = set()
     for _ in range(3):
-        message, event_id = _get_test_message(default_project)
-        event_ids.add(event_id)
+        message, event_id = get_test_message()
         producer.produce(topic_event_name, message)
+        event_ids.add(event_id)
 
     consumer = get_ingest_consumer(
         max_batch_size=2,
@@ -88,5 +107,9 @@ def test_ingest_consumer_reads_from_topic_and_calls_celery_task(
     for event_id in event_ids:
         message = eventstore.get_event_by_id(default_project.id, event_id)
         assert message is not None
-        # check that the data has not been scrambled
-        assert message.data["extra"]["the_id"] == event_id
+        assert message.data["event_id"] == event_id
+        if message.data["type"] == "transaction":
+            assert message.data["spans"] == []
+            assert message.data["contexts"]["trace"]
+        else:
+            assert message.data["extra"]["the_id"] == event_id
