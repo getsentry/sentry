4c29d92ac7108acd03e31bae6a3abc0c27246335
Billy Vong <billy@sentry.io>
2020-02-26 16:21:01 -0800

    add header to alert settings and remove some redundant things

--- src/sentry/static/sentry/app/views/settings/projectAlerts/settings.jsx
+++ src/sentry/static/sentry/app/views/settings/projectAlerts/settings.jsx
@@ -1,18 +1,20 @@
+import PropTypes from 'prop-types';
 import React from 'react';
 
 import {PanelAlert} from 'app/components/panels';
 import {fields} from 'app/data/forms/projectAlerts';
 import {t} from 'app/locale';
-import routeTitleGen from 'app/utils/routeTitle';
-import withOrganization from 'app/utils/withOrganization';
-import Access from 'app/components/acl/access';
 import AlertLink from 'app/components/alertLink';
 import AsyncView from 'app/views/asyncView';
+import Button from 'app/components/button';
 import Form from 'app/views/settings/components/forms/form';
 import JsonForm from 'app/views/settings/components/forms/jsonForm';
 import PermissionAlert from 'app/views/settings/project/permissionAlert';
 import PluginList from 'app/components/pluginList';
 import SentryTypes from 'app/sentryTypes';
+import SettingsPageHeader from 'app/views/settings/components/settingsPageHeader';
+import Tooltip from 'app/components/tooltip';
+import routeTitleGen from 'app/utils/routeTitle';
 
 class ProjectAlertSettings extends AsyncView {
   static propTypes = {
@@ -22,6 +24,7 @@ class ProjectAlertSettings extends AsyncView {
     // rendered
     organization: SentryTypes.Organization,
     project: SentryTypes.Project,
+    canEditRule: PropTypes.bool.isRequired,
   };
 
   getEndpoints() {
@@ -69,72 +72,90 @@ class ProjectAlertSettings extends AsyncView {
 
   renderBody() {
     const {
+      canEditRule,
       organization,
       params: {orgId, projectId},
     } = this.props;
 
+    const basePath = `/settings/${orgId}/projects/${projectId}/alerts/`;
+
     return (
-      <Access access={['project:write']}>
-        {({hasAccess}) => (
-          <React.Fragment>
-            <PermissionAlert />
-            <AlertLink to="/settings/account/notifications/" icon="icon-mail">
-              {t(
-                'Looking to fine-tune your personal notification preferences? Visit your Account Settings'
-              )}
-            </AlertLink>
-
-            <Form
-              saveOnBlur
-              allowUndo
-              initialData={{
-                subjectTemplate: this.state.project.subjectTemplate,
-                digestsMinDelay: this.state.project.digestsMinDelay,
-                digestsMaxDelay: this.state.project.digestsMaxDelay,
-              }}
-              apiMethod="PUT"
-              apiEndpoint={`/projects/${orgId}/${projectId}/`}
+      <React.Fragment>
+        <SettingsPageHeader
+          title={t('Alerts Settings')}
+          action={
+            <Tooltip
+              disabled={canEditRule}
+              title={t('You do not have permission to edit alert rules.')}
             >
-              <JsonForm
-                disabled={!hasAccess}
-                title={t('Email Settings')}
-                fields={[fields.subjectTemplate]}
-              />
+              <Button
+                to={`${basePath}new/`}
+                disabled={!canEditRule}
+                priority="primary"
+                size="small"
+                icon="icon-circle-add"
+              >
+                {t('New Alert Rule')}
+              </Button>
+            </Tooltip>
+          }
+        />
+        <PermissionAlert />
+        <AlertLink to="/settings/account/notifications/" icon="icon-mail">
+          {t(
+            'Looking to fine-tune your personal notification preferences? Visit your Account Settings'
+          )}
+        </AlertLink>
 
-              <JsonForm
-                title={t('Digests')}
-                disabled={!hasAccess}
-                fields={[fields.digestsMinDelay, fields.digestsMaxDelay]}
-                renderHeader={() => (
-                  <PanelAlert type="info">
-                    {t(
-                      'Sentry will automatically digest alerts sent ' +
-                        'by some services to avoid flooding your inbox ' +
-                        'with individual issue notifications. To control ' +
-                        'how frequently notifications are delivered, use ' +
-                        'the sliders below.'
-                    )}
-                  </PanelAlert>
-                )}
-              />
-            </Form>
+        <Form
+          saveOnBlur
+          allowUndo
+          initialData={{
+            subjectTemplate: this.state.project.subjectTemplate,
+            digestsMinDelay: this.state.project.digestsMinDelay,
+            digestsMaxDelay: this.state.project.digestsMaxDelay,
+          }}
+          apiMethod="PUT"
+          apiEndpoint={`/projects/${orgId}/${projectId}/`}
+        >
+          <JsonForm
+            disabled={!canEditRule}
+            title={t('Email Settings')}
+            fields={[fields.subjectTemplate]}
+          />
 
-            {hasAccess && (
-              <PluginList
-                organization={organization}
-                project={this.state.project}
-                pluginList={this.state.pluginList.filter(
-                  p => p.type === 'notification' && p.hasConfiguration
+          <JsonForm
+            title={t('Digests')}
+            disabled={!canEditRule}
+            fields={[fields.digestsMinDelay, fields.digestsMaxDelay]}
+            renderHeader={() => (
+              <PanelAlert type="info">
+                {t(
+                  'Sentry will automatically digest alerts sent ' +
+                    'by some services to avoid flooding your inbox ' +
+                    'with individual issue notifications. To control ' +
+                    'how frequently notifications are delivered, use ' +
+                    'the sliders below.'
                 )}
-                onEnablePlugin={this.handleEnablePlugin}
-                onDisablePlugin={this.handleDisablePlugin}
-              />
+              </PanelAlert>
             )}
-          </React.Fragment>
+          />
+        </Form>
+
+        {canEditRule && (
+          <PluginList
+            organization={organization}
+            project={this.state.project}
+            pluginList={this.state.pluginList.filter(
+              p => p.type === 'notification' && p.hasConfiguration
+            )}
+            onEnablePlugin={this.handleEnablePlugin}
+            onDisablePlugin={this.handleDisablePlugin}
+          />
         )}
-      </Access>
+      </React.Fragment>
     );
   }
 }
 
-export default withOrganization(ProjectAlertSettings);
+export default ProjectAlertSettings;
