3211c1a357f19bd1e81f51b81e11ca91b4905b86
Joshua Li <josh@jrl.ninja>
2020-02-28 10:49:40 -0800

    types.ClassType is removed in py3

--- src/sentry/testutils/cases.py
+++ src/sentry/testutils/cases.py
@@ -26,7 +26,7 @@ import os.path
 import pytest
 import requests
 import six
-import types
+import inspect
 from sentry.utils.compat import mock
 
 from click.testing import CliRunner
@@ -654,7 +654,7 @@ class PluginTestCase(TestCase):
 
         # Old plugins, plugin is a class, new plugins, it's an instance
         # New plugins don't need to be registered
-        if isinstance(self.plugin, (type, types.ClassType)):
+        if inspect.isclass(self.plugin):
             plugins.register(self.plugin)
             self.addCleanup(plugins.unregister, self.plugin)
 
