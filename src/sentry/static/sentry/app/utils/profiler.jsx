a52fd74a1bfacb5be9323c89bad73d0fea9fbeef
Daniel Griesser <daniel.griesser.86@gmail.com>
2020-02-28 10:09:54 +0100

    ref: Add code comments

--- src/sentry/static/sentry/app/utils/profiler.jsx
+++ src/sentry/static/sentry/app/utils/profiler.jsx
@@ -26,8 +26,10 @@ export default function profiler() {
           op: 'react',
           description: `<${displayName}>`,
         },
-        500
+        500 // After this timeout we'll pop this activity regardless
       );
+      // For whatever reason it's not guaranteed that `finishProfile` will be called, that's why we need
+      // the previously described timeout to make sure our transaction will be finished.
 
       finishProfile = () => {
         if (!this.activity) {
