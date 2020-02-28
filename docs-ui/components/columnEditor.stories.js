651567f344d1ab21fe74f34391cfd304e099eae6
Mark Story <mark@sentry.io>
2020-02-28 17:06:33 -0500

    feat(discover) Add modal component to column picker (#17345)
    Add the modal window component for the new column editor. This component
    takes a list of columns and fires an onApply prop when the changes are
    ready to be applied.
    
    Once this component is hooked up to the discover results table, we'll
    update the EventView and navigate to the new results.

--- docs-ui/components/columnEditor.stories.js
+++ docs-ui/components/columnEditor.stories.js
@@ -2,51 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import {storiesOf} from '@storybook/react';
 import {withInfo} from '@storybook/addon-info';
+import {action} from '@storybook/addon-actions';
 
-import ColumnEditCollection from 'app/views/eventsV2/table/columnEditCollection';
-
-/**
- * Fake modal container. Later on this will be a stateful
- * container that will update the EventView column edits
- * are submitted.
- */
-class FakeModal extends React.Component {
-  static propTypes = {
-    organization: PropTypes.object,
-    tagKeys: PropTypes.arrayOf(PropTypes.string),
-  };
-
-  state = {
-    columns: [
-      {
-        field: 'event.type',
-      },
-      {
-        field: 'browser.name',
-      },
-      {
-        field: 'id',
-        aggregation: 'count',
-      },
-    ],
-  };
-
-  handleChange = columns => {
-    this.setState({columns});
-  };
-
-  render() {
-    const {tagKeys, organization} = this.props;
-    return (
-      <ColumnEditCollection
-        organization={organization}
-        columns={this.state.columns}
-        tagKeys={tagKeys}
-        onChange={this.handleChange}
-      />
-    );
-  }
-}
+import {openModal} from 'app/actionCreators/modal';
+import Button from 'app/components/button';
+import GlobalModal from 'app/components/globalModal';
+import ColumnEditModal from 'app/views/eventsV2/table/columnEditModal';
 
 storiesOf('Discover|ColumnEditor', module).add(
   'all',
@@ -58,10 +19,35 @@ storiesOf('Discover|ColumnEditor', module).add(
       features: ['transaction-events'],
     };
     const tags = ['browser.name', 'custom-field'];
+    const columns = [
+      {
+        field: 'event.type',
+      },
+      {
+        field: 'browser.name',
+      },
+      {
+        field: 'id',
+        aggregation: 'count',
+      },
+    ];
+
+    const showModal = () => {
+      openModal(modalProps => (
+        <ColumnEditModal
+          {...modalProps}
+          organization={organization}
+          tagKeys={tags}
+          columns={columns}
+          onApply={action('onApply')}
+        />
+      ));
+    };
 
     return (
       <div>
-        <FakeModal organization={organization} tagKeys={tags} />
+        <Button onClick={showModal}>Edit columns</Button>
+        <GlobalModal />
       </div>
     );
   })
