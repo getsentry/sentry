717c116dcaea2f453acb50d821a099c4fcc050bd
Chris Fuller <cfuller@sentry.io>
2020-03-04 11:15:46 -0500

    Fixing TS check

--- src/sentry/static/sentry/app/views/settings/incidentRules/triggers/chart/index.tsx
+++ src/sentry/static/sentry/app/views/settings/incidentRules/triggers/chart/index.tsx
@@ -86,7 +86,7 @@ class TriggersChart extends React.PureComponent<Props> {
                         },
                       }}
                       maxValue={maxValue ? maxValue.value : maxValue}
-                      data={timeseriesData.slice(-1, 1)}
+                      data={timeseriesData?.slice(-1, 1)}
                       triggers={triggers}
                     />
                   </React.Fragment>
