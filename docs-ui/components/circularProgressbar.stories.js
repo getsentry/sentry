1e70b93eec57470e5639817ee04f5afb471b56a8
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-03-05 10:46:13 -0800

    feat(ui): Improve CircularProgressbar (#17463)
    - Adds a text value to the center of the circle progress bar
    - Adds animation to the text value on changes
    - Adds animation to progress bar value changes
    - Simplifies the code by using `circle` svg elements
    - More customization via the  backgroundColor, progressColor, and
      textCss properties.

--- docs-ui/components/circularProgressbar.stories.js
+++ docs-ui/components/circularProgressbar.stories.js
@@ -1,31 +1,96 @@
 import React from 'react';
 import {storiesOf} from '@storybook/react';
 import {withInfo} from '@storybook/addon-info';
-import {number} from '@storybook/addon-knobs';
+import {number, text, boolean, color} from '@storybook/addon-knobs';
+import styled from '@emotion/styled';
+import {css} from '@emotion/core';
 
 import CircularProgressbar from 'app/components/circularProgressbar';
 
+class Ticker extends React.Component {
+  state = {
+    tickNumber: 0,
+  };
+
+  componentDidMount() {
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  tick = () =>
+    this.setState(s => ({
+      tickNumber: (s.tickNumber + 5) % 100,
+    }));
+
+  render() {
+    return this.props.children({tickNumber: this.state.tickNumber});
+  }
+}
+
 storiesOf('UI|CircularProgressbar', module).add(
   'default',
-  withInfo('Description')(() => {
+  withInfo('Circle style progress bar ')(() => {
     const value = number('Value', 29);
-    const size = number('Size', 20);
+    const size = number('Size', 40);
     const minValue = number('Min Value', 0);
     const maxValue = number('Max Value', 100);
-    const strokeWidth = number('Stroke Width', 3);
+    const barWidth = number('Bar Width', 3);
+    const textValue = text('Text Value', '');
+    const animateText = boolean('Animate Text', false);
+    const backgroundColor = color('Background color');
+    const progressColor = color('Progress color');
 
     return (
-      <React.Fragment>
+      <Grid>
         <CircularProgressbar
           value={value}
-          size={size}
           minValue={minValue}
           maxValue={maxValue}
-          strokeWidth={strokeWidth}
+          size={size}
+          barWidth={barWidth}
+          text={textValue}
+          animateText={animateText}
+          backgroundColor={backgroundColor}
+          progressColor={progressColor}
         />
-        <CircularProgressbar value={61} />
-        <CircularProgressbar value={85} />
-      </React.Fragment>
+        <Ticker>
+          {({tickNumber}) => (
+            <CircularProgressbar
+              animateText
+              value={tickNumber}
+              text={tickNumber}
+              size={38}
+              barWidth={4}
+              progressColor="#f58159"
+            />
+          )}
+        </Ticker>
+        <CircularProgressbar
+          value={65}
+          size={60}
+          barWidth={6}
+          text="BAD"
+          textCss={() => css`
+            font-size: 14px;
+            font-weight: bold;
+            fill: #ec5f5f;
+          `}
+          progressColor="#ff4d44"
+          backgroundColor="#fbe6e6"
+        />
+      </Grid>
     );
   })
 );
+
+const Grid = styled('div')`
+  margin-top: 8px;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 50px);
+  align-items: center;
+  justify-content: center;
+  grid-gap: 8px;
+`;
