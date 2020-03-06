227a02062fe1d049f576fd2b4109ea141e20e962
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-03-06 10:09:55 -0800

    ♻️ Explicit return none

--- src/sentry/models/exporteddata.py
+++ src/sentry/models/exporteddata.py
@@ -93,10 +93,11 @@ class ExportedData(Model):
 
         # The following condition should never be true, but it's a safeguard in case someone manually calls this method
         if self.date_finished is None or self.date_expired is None or self.file is None:
-            return logger.warning(
+            logger.warning(
                 "Notification email attempted on incomplete dataset",
                 extra={"data_export_id": self.id, "organization_id": self.organization_id},
             )
+            return
         url = absolute_uri(
             reverse("sentry-data-export-details", args=[self.organization.slug, self.id])
         )
