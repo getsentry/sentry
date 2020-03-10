6f42c722c8249a61686a916e86f325aeaa53bb49
Priscila Oliveira <priscila@Sentrys-MacBook-Pro.local>
2020-03-10 11:52:40 +0100

    feat(reserved-words): added reserved words list

--- src/sentry/static/sentry/app/utils/queryString.tsx
+++ src/sentry/static/sentry/app/utils/queryString.tsx
@@ -2,6 +2,8 @@ import queryString from 'query-string';
 import parseurl from 'parseurl';
 import isString from 'lodash/isString';
 
+import RESERVED_WORDS from './queryStringReservedWords';
+
 // remove leading and trailing whitespace and remove double spaces
 export function formatQueryString(qs: string): string {
   return qs.trim().replace(/\s+/g, ' ');
@@ -35,10 +37,12 @@ export function appendTagCondition(
   key: string,
   value: string
 ): string {
-  // Use the explicit tag syntax to handle the case when the tag name (key) is a
-  // reserved keyword.
-  // https://docs.sentry.io/workflow/search/?platform=node#explicit-tag-syntax
-  key = `tags[${key}]`;
+  if (RESERVED_WORDS.includes(key)) {
+    //Use the explicit tag syntax to handle the case when the tag name (key) is a
+    //reserved keyword.
+    //https://docs.sentry.io/workflow/search/?platform=node#explicit-tag-syntax
+    key = `tags[${key}]`;
+  }
 
   let currentQuery = Array.isArray(query) ? query.pop() : isString(query) ? query : '';
 
---
+++ src/sentry/static/sentry/app/utils/queryStringReservedWords.tsx
@@ -0,0 +1,7 @@
+// List all possible reserved words that should be treated differently when filtering by a tag
+// Please see: https://github.com/getsentry/sentry/pull/17507
+
+// Please add reserved words here if necessary
+const RESERVED_WORDS = ['email'];
+
+export default RESERVED_WORDS;
