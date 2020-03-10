5d80c280f5680eff8b276378cab9fda8831dbb0f
adhiraj <693121+adhiraj@users.noreply.github.com>
2020-03-10 09:11:31 -0700

    Update

--- tests/js/spec/views/incidents/list/index.spec.jsx
+++ tests/js/spec/views/incidents/list/index.spec.jsx
@@ -56,7 +56,7 @@ describe('IncidentsList', function() {
 
     wrapper = await createWrapper();
     expect(wrapper.find('PanelItem')).toHaveLength(0);
-    expect(wrapper.text()).toContain("You don't have any Alerts yet");
+    expect(wrapper.text()).toContain("You don't have any Metric Alerts yet");
   });
 
   it('toggles all/open', async function() {
