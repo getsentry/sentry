595d225631a0132e599b3705ad4233516bcb466d
maheskett <meg.heskett@gmail.com>
2020-03-10 03:37:56 -0700

    Refactor guides backend

--- tests/sentry/api/endpoints/test_assistant.py
+++ tests/sentry/api/endpoints/test_assistant.py
@@ -1,16 +1,19 @@
 from __future__ import absolute_import
 
 from copy import deepcopy
+from exam import fixture
 
 from django.core.urlresolvers import reverse
+from django.utils import timezone
 
 from sentry.assistant import manager
+from sentry.models import AssistantActivity
 from sentry.testutils import APITestCase
 
 
-class AssistantActivity(APITestCase):
+class AssistantActivityTest(APITestCase):
     def setUp(self):
-        super(AssistantActivity, self).setUp()
+        super(AssistantActivityTest, self).setUp()
         self.login_as(user=self.user)
         self.path = reverse("sentry-api-0-assistant")
         self.guides = manager.all()
@@ -49,3 +52,87 @@ class AssistantActivity(APITestCase):
                 steps_i = set(s["target"] for s in guides[i]["steps"])
                 steps_j = set(s["target"] for s in guides[j]["steps"])
                 assert not (steps_i & steps_j)
+
+
+class AssistantActivityV2Test(APITestCase):
+    endpoint = "sentry-api-0-assistant"
+
+    @fixture
+    def guides(self):
+        return manager.all()
+
+    def setUp(self):
+        super(AssistantActivityV2Test, self).setUp()
+        self.create_organization(owner=self.user)
+        self.login_as(user=self.user)
+
+    def test_simple(self):
+        resp = self.get_response(qs_params={"v2": 1})
+        assert resp.status_code == 200
+
+        assert len(resp.data) == len(manager.all())
+        for guide in resp.data:
+            assert guide["seen"] is False
+
+    def test_dismissed(self):
+        guide = "issue"
+        AssistantActivity.objects.create(
+            user=self.user, guide_id=self.guides[guide]["id"], dismissed_ts=timezone.now()
+        )
+        resp = self.get_response(qs_params={"v2": 1})
+        assert resp.status_code == 200
+        assert {"guide": guide, "seen": True} in resp.data
+
+    def test_viewed(self):
+        guide = "issue"
+        AssistantActivity.objects.create(
+            user=self.user, guide_id=self.guides[guide]["id"], viewed_ts=timezone.now()
+        )
+        resp = self.get_response(qs_params={"v2": 1})
+        assert resp.status_code == 200
+        assert {"guide": guide, "seen": True} in resp.data
+
+
+class AssistantActivityV2UpdateTest(APITestCase):
+    endpoint = "sentry-api-0-assistant"
+    method = "put"
+
+    @fixture
+    def guides(self):
+        return manager.all()
+
+    def setUp(self):
+        super(AssistantActivityV2UpdateTest, self).setUp()
+        self.create_organization(owner=self.user)
+        self.login_as(user=self.user)
+
+    def test_invalid_inputs(self):
+        resp = self.get_response(guide="guide_does_not_exist")
+        assert resp.status_code == 400
+
+        resp = self.get_response(guide="guide_does_not_exist", status="dismissed")
+        assert resp.status_code == 400
+
+        resp = self.get_response(status="dismissed")
+        assert resp.status_code == 400
+
+        resp = self.get_response(guide="issue", status="whats_my_name_again")
+        assert resp.status_code == 400
+
+    def test_dismissed(self):
+        guide = "issue_stream"
+        resp = self.get_response(guide=guide, status="dismissed")
+        assert resp.status_code == 201
+
+        activity = AssistantActivity.objects.get(user=self.user, guide_id=self.guides[guide]["id"])
+        assert activity.dismissed_ts
+        assert not activity.viewed_ts
+
+    def test_viewed(self):
+        guide = "issue_stream"
+        resp = self.get_response(guide=guide, status="viewed")
+        assert resp.status_code == 201
+
+        activity = AssistantActivity.objects.get(user=self.user, guide_id=self.guides[guide]["id"])
+        assert activity.viewed_ts
+        assert not activity.dismissed_ts
