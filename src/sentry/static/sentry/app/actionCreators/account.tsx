e4e77b05f2d1abf65c2853e11ced6e41a96da6a1
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-03-12 00:13:23 -0700

    chore(ts): COnvert actionCreators/account

---
+++ src/sentry/static/sentry/app/actionCreators/account.tsx
@@ -0,0 +1,32 @@
+import {Client} from 'app/api';
+import {addErrorMessage, addSuccessMessage} from 'app/actionCreators/indicator';
+import ConfigStore from 'app/stores/configStore';
+import {User} from 'app/types';
+
+export async function disconnectIdentity(identity: {id: string; providerLabel: string}) {
+  const api = new Client();
+
+  try {
+    await api.requestPromise(`/users/me/social-identities/${identity.id}/`, {
+      method: 'DELETE',
+    });
+    addSuccessMessage(`Disconnected ${identity.providerLabel}`);
+  } catch {
+    addErrorMessage('Error disconnecting identity');
+  }
+}
+
+export function updateUser(user: User) {
+  // Ideally we'd fire an action but this is gonna get refactored soon anyway
+  ConfigStore.set('user', user);
+}
+
+export function logout(api: Client) {
+  return api.requestPromise('/auth/', {method: 'DELETE'});
+}
+
+export function removeAuthenticator(api: Client, userId: string, authId: string) {
+  return api.requestPromise(`/users/${userId}/authenticators/${authId}/`, {
+    method: 'DELETE',
+  });
+}
