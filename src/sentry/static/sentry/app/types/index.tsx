4e57f2bf603cf79143b24963781c5fefea93538a
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-03-12 14:58:45 -0700

    feat(onboarding): Serialize onboarding task user with UserSerializer (#17600)

--- src/sentry/static/sentry/app/types/index.tsx
+++ src/sentry/static/sentry/app/types/index.tsx
@@ -951,7 +951,7 @@ export type OnboardingTaskDescriptor = {
 export type OnboardingTaskStatus = {
   task: OnboardingTaskKey;
   status: 'skipped' | 'pending' | 'complete';
-  user?: string | null;
+  user?: AvatarUser | null;
   dateCompleted?: string;
   completionSeen?: string;
   data?: object;
