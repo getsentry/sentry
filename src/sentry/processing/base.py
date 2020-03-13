ed1c9b6035eb29b6928d901f5147f670504b24fc
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-03-13 14:33:14 -0700

    ✨ Introduce IBT processor

--- src/sentry/processing/base.py
+++ src/sentry/processing/base.py
@@ -1,10 +1,19 @@
 from __future__ import absolute_import
 
-
-# TODO(Leander): Add environment checking to this module
-# TODO(Leander): Ensure this is used for group_tag_export.py
-# TODO(Leander): Update tests if need be
+from sentry.models import Project
 
 
 class ProcessingError(Exception):
     pass
+
+
+class BaseProcessor:
+    def get_project(self, project_id):
+        # TODO(Leander): Add environment checking to this module
+        # TODO(Leander): Ensure this is used for group_tag_export.py
+        # TODO(Leander): Update tests if need be
+        try:
+            project = Project.objects.get(id=project_id)
+            return project
+        except Project.DoesNotExist:
+            raise ProcessingError("Requested project does not exist")
--- src/sentry/processing/issues_by_tag.py
+++ src/sentry/processing/issues_by_tag.py
@@ -4,7 +4,7 @@ import six
 
 from sentry import tagstore
 from sentry.models import EventUser, Group, get_group_with_redirect, Project
-from .base import ProcessingError
+from .base import BaseProcessor, ProcessingError
 
 
 def get_project_and_group(project_id, group_id):
@@ -77,3 +77,92 @@ def serialize_issue(item, key):
         result["username"] = euser.username if euser else ""
         result["ip_address"] = euser.ip_address if euser else ""
     return result
+
+
+class IssuesByTagProcessor(BaseProcessor):
+    def __init__(self, project_id, group_id, key, environment_id):
+        self.project = self.get_project(project_id)
+        self.group = self.get_group(group_id)
+        self.key = key
+        self.environment_id = environment_id
+        self.fields = self.get_fields(key)
+        self.lookup_key = self.get_lookup_key(key)
+        # Ensure the tag key exists, as it may have been deleted
+        try:
+            tagstore.get_tag_key(project_id, environment_id, self.lookup_key)
+        except tagstore.TagKeyNotFound:
+            raise ProcessingError("Requested key does not exist")
+        self.callbacks = self.get_callbacks(key, self.group.project_id)
+
+    def get_group(self, group_id):
+        try:
+            group, _ = get_group_with_redirect(
+                group_id, queryset=Group.objects.filter(project=self.project)
+            )
+            return group
+        except Group.DoesNotExist:
+            raise ProcessingError("Requested group does not exist")
+
+    def get_fields(self, key):
+        if key == "user":
+            return [
+                "value",
+                "id",
+                "email",
+                "username",
+                "ip_address",
+                "times_seen",
+                "last_seen",
+                "first_seen",
+            ]
+        else:
+            return ["value", "times_seen", "last_seen", "first_seen"]
+
+    def get_lookup_key(self, key):
+        return six.text_type("sentry:{}").format(key) if tagstore.is_reserved_key(key) else key
+
+    def get_eventuser_callback(self, project_id):
+        def attach_eventuser(items):
+            users = EventUser.for_tags(project_id, [i.value for i in items])
+            for item in items:
+                item._eventuser = users.get(item.value)
+
+        return attach_eventuser
+
+    def get_callbacks(self, key, project_id):
+        return [self.get_eventuser_callback(project_id)] if key == "user" else []
+
+    def serialize_issue(self, item):
+        result = {
+            "value": item.value,
+            "times_seen": item.times_seen,
+            "last_seen": item.last_seen.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+            "first_seen": item.first_seen.strftime("%Y-%m-%dT%H:%M:%S.%fZ"),
+        }
+        if self.key == "user":
+            euser = item._eventuser
+            result["id"] = euser.ident if euser else ""
+            result["email"] = euser.email if euser else ""
+            result["username"] = euser.username if euser else ""
+            result["ip_address"] = euser.ip_address if euser else ""
+        return result
+
+    def get_raw_data(self, offset=0):
+        """
+        Returns list of GroupTagValues
+        """
+        return tagstore.get_group_tag_value_iter(
+            project_id=self.group.project_id,
+            group_id=self.group.id,
+            environment_id=self.environment_id,
+            key=self.lookup_key,
+            callbacks=self.callbacks,
+            offset=offset,
+        )
+
+    def get_serialized_data(self, offset=0):
+        """
+        Returns list of serialized GroupTagValue dictionaries
+        """
+        raw_data = self.get_raw_data(offset)
+        return [self.serialize_issue(item) for item in raw_data]
