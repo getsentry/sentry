252eaf0eef57a99227a5155c5610ce2b54f281d2
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-03-13 11:10:09 +0100

    ref: Update doc links for datascrubbing (#17648)
    * ref: Update doc links for datascrubbing
    
    * prettier

--- src/sentry/static/sentry/app/data/forms/projectGeneralSettings.jsx
+++ src/sentry/static/sentry/app/data/forms/projectGeneralSettings.jsx
@@ -349,7 +349,9 @@ export const fields = {
     help: tct(
       'Advanced JSON-based configuration for datascrubbing. Applied in addition to the settings above. [learn_more:Learn more]',
       {
-        learn_more: <a href="https://getsentry.github.io/relay/pii-config/" />,
+        learn_more: (
+          <a href="https://docs.sentry.io/data-management/advanced-datascrubbing/" />
+        ),
       }
     ),
     visible: ({features}) => features.has('datascrubbers-v2'),
