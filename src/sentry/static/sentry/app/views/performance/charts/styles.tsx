a84eaf24cfc70cb79a110ccaf31829d389859f06
Dora Chan <dora.lchan@gmail.com>
2020-03-16 00:16:29 -0700

    cleaned up perf chart

--- src/sentry/static/sentry/app/views/performance/charts/styles.tsx
+++ src/sentry/static/sentry/app/views/performance/charts/styles.tsx
@@ -2,10 +2,32 @@ import styled from '@emotion/styled';
 
 import space from 'app/styles/space';
 
-export const HeaderTitle = styled('h4')`
-  margin: 0;
-  font-size: ${p => p.theme.fontSizeMedium};
-  color: ${p => p.theme.gray3};
+export const HeaderTitle = styled('span')`
+  font-size: ${p => p.theme.fontSizeLarge};
+  color: ${p => p.theme.gray4};
+  padding: 0 ${space(1)};
+`;
 
-  padding: ${space(2)};
+export const SectionHeading = styled('h4')`
+  color: ${p => p.theme.gray3};
+  font-size: ${p => p.theme.fontSizeMedium};
+  margin: ${space(1)} 0;
+  padding-right: ${space(1)};
+  line-height: 1.2;
+`;
+
+export const ChartsContainer = styled('div')`
+  display: flex;
+  padding: ${space(2)} ${space(1.5)};
+`;
+
+export const ChartContainer = styled('div')`
+  flex-grow: 1;
+`;
+
+export const ChartControls = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  padding: ${space(1)} ${space(3)};
+  border-top: 1px solid ${p => p.theme.borderLight};
 `;
