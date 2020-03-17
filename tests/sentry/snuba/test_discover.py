fbe29640709238dcf8413100f94273e07274bb13
evanh <evanh@users.noreply.github.com>
2020-03-17 15:06:24 -0400

    fix(discover) Finalize available functions (#17593)
    * fix(discover) Finalize available functions
    
    We finalized the list of available functions, so reflect that change in the
    backend. We still need the discover shim for the old queries, but there
    is another PR incoming to migrate all of that over.
    
    * Remove parameters from UI code as well.
    
    Co-authored-by: Mark Story <mark@sentry.io>

--- tests/sentry/snuba/test_discover.py
+++ tests/sentry/snuba/test_discover.py
@@ -437,8 +437,8 @@ class QueryTransformTest(TestCase):
     @patch("sentry.snuba.discover.raw_query")
     def test_selected_columns_aggregate_alias(self, mock_query):
         mock_query.return_value = {
-            "meta": [{"name": "transaction"}, {"name": "percentile_transaction_duration_0_95"}],
-            "data": [{"transaction": "api.do_things", "percentile_transaction_duration_0_95": 200}],
+            "meta": [{"name": "transaction"}, {"name": "p95"}],
+            "data": [{"transaction": "api.do_things", "p95": 200}],
         }
         discover.query(
             selected_columns=["transaction", "p95", "count_unique(transaction)"],
@@ -449,7 +449,7 @@ class QueryTransformTest(TestCase):
         mock_query.assert_called_with(
             selected_columns=["transaction"],
             aggregations=[
-                ["quantile(0.95)", "duration", "percentile_transaction_duration_0_95"],
+                ["quantile(0.95)", "duration", "p95"],
                 ["uniq", "transaction", "count_unique_transaction"],
                 ["argMax", ["event_id", "timestamp"], "latest_event"],
                 ["argMax", ["project_id", "timestamp"], "projectid"],
@@ -477,8 +477,8 @@ class QueryTransformTest(TestCase):
     @patch("sentry.snuba.discover.raw_query")
     def test_selected_columns_aggregate_alias_with_brackets(self, mock_query):
         mock_query.return_value = {
-            "meta": [{"name": "transaction"}, {"name": "percentile_transaction_duration_0_95"}],
-            "data": [{"transaction": "api.do_things", "percentile_transaction_duration_0_95": 200}],
+            "meta": [{"name": "transaction"}, {"name": "p95"}],
+            "data": [{"transaction": "api.do_things", "p95": 200}],
         }
         discover.query(
             selected_columns=["transaction", "p95()", "count_unique(transaction)"],
@@ -489,7 +489,7 @@ class QueryTransformTest(TestCase):
         mock_query.assert_called_with(
             selected_columns=["transaction"],
             aggregations=[
-                ["quantile(0.95)", "duration", "percentile_transaction_duration_0_95"],
+                ["quantile(0.95)", "duration", "p95"],
                 ["uniq", "transaction", "count_unique_transaction"],
                 ["argMax", ["event_id", "timestamp"], "latest_event"],
                 ["argMax", ["project_id", "timestamp"], "projectid"],
@@ -894,8 +894,8 @@ class QueryTransformTest(TestCase):
             filter_keys={"project_id": [self.project.id]},
             groupby=["transaction"],
             dataset=Dataset.Discover,
-            aggregations=[["quantile(0.95)", "duration", "percentile_transaction_duration_0_95"]],
-            having=[["percentile_transaction_duration_0_95", ">", 5]],
+            aggregations=[["quantile(0.95)", "duration", "p95"]],
+            having=[["p95", ">", 5]],
             end=end_time,
             start=start_time,
             orderby=None,
@@ -925,8 +925,8 @@ class QueryTransformTest(TestCase):
             filter_keys={"project_id": [self.project.id]},
             groupby=["transaction"],
             dataset=Dataset.Discover,
-            aggregations=[["quantile(0.95)", "duration", "percentile_transaction_duration_0_95"]],
-            having=[["percentile_transaction_duration_0_95", ">", 5]],
+            aggregations=[["quantile(0.95)", "duration", "p95"]],
+            having=[["p95", ">", 5]],
             end=end_time,
             start=start_time,
             orderby=None,
