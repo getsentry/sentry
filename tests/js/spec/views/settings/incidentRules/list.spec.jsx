d5609f93bc4414c74357c5fca1e7b6bd6c144323
Chris Fuller <cfuller@sentry.io>
2020-03-17 09:01:53 -0400

    fix(workflow): Changing metric text in alert rule builder (#17700)
    * Changing metric text

--- tests/js/spec/views/settings/incidentRules/list.spec.jsx
+++ tests/js/spec/views/settings/incidentRules/list.spec.jsx
@@ -22,7 +22,7 @@ describe('Incident Rules List', function() {
 
     expect(req).toHaveBeenCalled();
     expect(wrapper.find('RuleLink').text()).toEqual('My Incident Rule');
-    expect(wrapper.find('MetricName').text()).toEqual('Events');
+    expect(wrapper.find('MetricName').text()).toEqual('Number of Events');
 
     expect(wrapper.find('Thresholds').text()).toEqual('70');
   });
