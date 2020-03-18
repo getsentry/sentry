30f69126798a1deeaf1405fe115d745bb68729f3
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-03-18 13:43:27 +0100

    ref: Remove IBAN ruletype (#17757)

--- src/sentry/static/sentry/app/views/settings/components/dataPrivacyRulesPanel/dataPrivacyRulesPanel.tsx
+++ src/sentry/static/sentry/app/views/settings/components/dataPrivacyRulesPanel/dataPrivacyRulesPanel.tsx
@@ -126,7 +126,7 @@ class DataPrivacyRulesPanel extends React.Component<Props, State> {
         ...prevState.rules,
         {
           id: prevState.rules.length + 1,
-          type: RULE_TYPE.IBAN,
+          type: RULE_TYPE.CREDITCARD,
           method: METHOD_TYPE.MASK,
           from: DEFAULT_RULE_FROM_VALUE,
         },
