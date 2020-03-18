2739e2c8a5c127e2040d2c353f948e487b73f29f
Alberto Leal <mail4alberto@gmail.com>
2020-03-18 01:54:05 -0400

    fix chart grid

--- src/sentry/static/sentry/app/views/performance/charts/index.tsx
+++ src/sentry/static/sentry/app/views/performance/charts/index.tsx
@@ -19,7 +19,7 @@ import {getUtcToLocalDateObject} from 'app/utils/dates';
 import {IconWarning} from 'app/icons';
 import theme from 'app/utils/theme';
 
-import {ChartsContainer} from './styles';
+import {ChartsContainer, ChartsGrid} from './styles';
 import Chart from './chart';
 import Footer from './footer';
 
@@ -132,25 +132,29 @@ class Container extends React.Component<Props, State> {
                 return <LoadingPanel data-test-id="events-request-loading" />;
               }
 
-              return YAXIS_OPTIONS.map(yAxis => (
-                <React.Fragment key={yAxis.label}>
-                  {getDynamicText({
-                    value: (
-                      <Chart
-                        loading={loading || reloading}
-                        yAxis={yAxis.label}
-                        data={results[yAxis.value]}
-                        router={router}
-                        statsPeriod={globalSelection.statsPeriod}
-                        utc={utc === 'true'}
-                        projects={globalSelection.project}
-                        environments={globalSelection.environment}
-                      />
-                    ),
-                    fixed: 'events chart',
-                  })}
-                </React.Fragment>
-              ));
+              return (
+                <ChartsGrid>
+                  {YAXIS_OPTIONS.map(yAxis => (
+                    <React.Fragment key={yAxis.label}>
+                      {getDynamicText({
+                        value: (
+                          <Chart
+                            loading={loading || reloading}
+                            yAxis={yAxis.label}
+                            data={results[yAxis.value]}
+                            router={router}
+                            statsPeriod={globalSelection.statsPeriod}
+                            utc={utc === 'true'}
+                            projects={globalSelection.project}
+                            environments={globalSelection.environment}
+                          />
+                        ),
+                        fixed: 'events chart',
+                      })}
+                    </React.Fragment>
+                  ))}
+                </ChartsGrid>
+              );
             }}
           </EventsRequest>
         </ChartsContainer>
