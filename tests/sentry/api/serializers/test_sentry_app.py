f6c1dd0f3752bd97f4568941ae4238db1ca90820
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2020-03-18 01:38:25 -0700

    add test for internal apps

--- tests/sentry/api/serializers/test_sentry_app.py
+++ tests/sentry/api/serializers/test_sentry_app.py
@@ -6,9 +6,11 @@ from sentry.api.serializers import serialize
 from sentry.api.serializers.models.sentry_app import SentryAppSerializer
 from sentry.testutils import TestCase
 
+# from sentry.constants import SentryAppStatus
+
 
 class SentryAppSerializerTest(TestCase):
-    def test_simple(self):
+    def test_published_app(self):
         user = self.create_user()
         organization = self.create_organization(owner=user)
         sentry_app = self.create_sentry_app(
@@ -26,5 +28,20 @@ class SentryAppSerializerTest(TestCase):
                 "featureGate": "integrations-api",
             }
         ]
-        assert result["author"] == "A Company"
         assert result["scopes"] == ["org:write", "team:admin"]
+        assert result.get("clientSecret") is None
+
+    def test_internal_app(self):
+        user = self.create_user()
+        org = self.create_organization(owner=user)
+        self.create_project(organization=org)
+        sentry_app = self.create_internal_integration(
+            name="La Croix App", organization=org, scopes=("org:write", "team:admin")
+        )
+        result = serialize(sentry_app, None, SentryAppSerializer(), access=None)
+
+        assert result["name"] == "La Croix App"
+        assert result["status"] == "internal"
+        assert result["featureData"] == []
+        assert result["scopes"] == ["org:write", "team:admin"]
+        assert result.get("clientSecret") is None
