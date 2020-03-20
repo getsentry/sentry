5860c0a1b70ac221e57b28c52bfd7aa5a590050d
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-03-20 16:51:05 -0400

    ✨ Introduce new data_export root feature folder

---
+++ src/sentry/data_export/base.py
@@ -0,0 +1,61 @@
+from __future__ import absolute_import
+
+import six
+from datetime import timedelta
+from enum import Enum
+
+from sentry.models import Project
+
+SNUBA_MAX_RESULTS = 1000
+DEFAULT_EXPIRATION = timedelta(weeks=4)
+
+
+class ExportError(Exception):
+    pass
+
+
+class ExportProcessor:
+    def get_project(self, project_id):
+        try:
+            project = Project.objects.get(id=project_id)
+            return project
+        except Project.DoesNotExist:
+            raise ExportError("Requested project does not exist")
+
+
+class ExportStatus(six.text_type, Enum):
+    Early = "EARLY"  # The download is being prepared
+    Valid = "VALID"  # The download is ready for the user
+    Expired = "EXPIRED"  # The download has been deleted
+
+
+class ExportQueryType(object):
+    ISSUES_BY_TAG = 0
+    DISCOVER = 1
+    ISSUES_BY_TAG_STR = "Issues-by-Tag"
+    DISCOVER_STR = "Discover"
+
+    @classmethod
+    def as_choices(cls):
+        return ((cls.ISSUES_BY_TAG, cls.ISSUES_BY_TAG_STR), (cls.DISCOVER, cls.DISCOVER_STR))
+
+    @classmethod
+    def as_str_choices(cls):
+        return (
+            (cls.ISSUES_BY_TAG_STR, cls.ISSUES_BY_TAG_STR),
+            (cls.DISCOVER_STR, cls.DISCOVER_STR),
+        )
+
+    @classmethod
+    def as_str(cls, integer):
+        if integer == cls.ISSUES_BY_TAG:
+            return cls.ISSUES_BY_TAG_STR
+        elif integer == cls.DISCOVER:
+            return cls.DISCOVER_STR
+
+    @classmethod
+    def from_str(cls, string):
+        if string == cls.ISSUES_BY_TAG_STR:
+            return cls.ISSUES_BY_TAG
+        elif string == cls.DISCOVER_STR:
+            return cls.DISCOVER
