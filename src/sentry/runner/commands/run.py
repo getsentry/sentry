fcb53ca4c559c6f322723f4ca29a78bffcb3a13b
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-03-23 10:05:16 +0100

    ref: Put the ingest consumer in devserver (#17726)
    * Allow `--consumer-type` to be specified multiple times to pull from multiple topics at once. (note: I am not particularly happy with that part of the interface but it's backwards compatible). At the same time the option is no longer mandatory: By default it pulls from all three topics at once.
    * devserver checks for `settings.SENTRY_USE_RELAY` and runs the consumer if relay is used.
    
    still open: do we really want to have a singular topic for all? What simplicity do we gain from this given that the consumer can now pull from all three?

--- src/sentry/runner/commands/run.py
+++ src/sentry/runner/commands/run.py
@@ -8,6 +8,7 @@ import click
 
 from sentry.runner.decorators import configuration, log_options
 from sentry.bgtasks.api import managed_bgtasks
+from sentry.ingest.types import ConsumerType
 
 
 class AddressParamType(click.ParamType):
@@ -412,11 +413,18 @@ def batching_kafka_options(group):
 @run.command("ingest-consumer")
 @log_options()
 @click.option(
+    "consumer_types",
     "--consumer-type",
-    default=None,
-    required=True,
-    help="Specify which type of consumer to create, i.e. from which topic to consume messages.",
-    type=click.Choice(["events", "transactions", "attachments"]),
+    default=[],
+    multiple=True,
+    help="Specify which type of consumer to create, i.e. from which topic to consume messages. By default all ingest-related topics are consumed ",
+    type=click.Choice(ConsumerType.all()),
+)
+@click.option(
+    "--all-consumer-types",
+    default=False,
+    is_flag=True,
+    help="Listen to all consumer types at once.",
 )
 @batching_kafka_options("ingest-consumer")
 @click.option(
@@ -426,25 +434,31 @@ def batching_kafka_options(group):
     help="Spawn this many threads to process messages. Defaults to 1.",
 )
 @configuration
-def ingest_consumer(consumer_type, **options):
+def ingest_consumer(consumer_types, all_consumer_types, **options):
     """
     Runs an "ingest consumer" task.
 
     The "ingest consumer" tasks read events from a kafka topic (coming from Relay) and schedules
     process event celery tasks for them
     """
-    from sentry.ingest.ingest_consumer import ConsumerType, get_ingest_consumer
+    from sentry.ingest.ingest_consumer import get_ingest_consumer
     from sentry.utils import metrics
 
-    if consumer_type == "events":
-        consumer_type = ConsumerType.Events
-    elif consumer_type == "transactions":
-        consumer_type = ConsumerType.Transactions
-    elif consumer_type == "attachments":
-        consumer_type = ConsumerType.Attachments
+    if all_consumer_types:
+        if consumer_types:
+            raise click.ClickException(
+                "Cannot specify --all-consumer types and --consumer-type at the same time"
+            )
+        else:
+            consumer_types = set(ConsumerType.all())
 
-    with metrics.global_tags(ingest_consumer_type=consumer_type, _all_threads=True):
-        get_ingest_consumer(consumer_type=consumer_type, **options).run()
+    if not all_consumer_types and not consumer_types:
+        raise click.ClickException("Need to specify --all-consumer-types or --consumer-type")
+
+    with metrics.global_tags(
+        ingest_consumer_types=",".join(sorted(consumer_types)), _all_threads=True
+    ):
+        get_ingest_consumer(consumer_types=consumer_types, **options).run()
 
 
 @run.command("outcomes-consumer")
