39f6e4a1513a11fd0b5b42acf933d1a96157e6b5
unsatisfiedpopcorn <jefferykwoh@gmail.com>
2020-03-24 06:41:21 +0800

    ref: Unnest and better defaults

--- src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/index.tsx
+++ src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/index.tsx
@@ -103,16 +103,15 @@ class IssueRuleEditor extends AsyncView<Props, State> {
   }
 
   getEndpoints() {
-    const {
-      params: {ruleId, projectId, orgId},
-      location: {query: {__has_issue_alerts_targeting} = {}} = {},
-    } = this.props;
+    const {params, location} = this.props;
+    const {ruleId, projectId, orgId} = params;
+    const {__has_issue_alerts_targeting = 0} = location.query ?? {};
+
     const endpoints = [
       ['environments', `/projects/${orgId}/${projectId}/environments/`],
       [
         'configs',
-        `/projects/${orgId}/${projectId}/rules/configuration/?issue_alerts_targeting=${__has_issue_alerts_targeting ??
-          0}`,
+        `/projects/${orgId}/${projectId}/rules/configuration/?issue_alerts_targeting=${__has_issue_alerts_targeting}`,
       ],
     ];
 
