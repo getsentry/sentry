1e37aa980a2c3418c372278eb5a080772c562e27
Matej Minar <matej.minar@sentry.io>
2020-03-24 15:45:18 +0100

    feat(ui): Add ability to pass own queryParams to groupList

--- src/sentry/static/sentry/app/views/releasesV2/detail/overview/index.tsx
+++ src/sentry/static/sentry/app/views/releasesV2/detail/overview/index.tsx
@@ -95,6 +95,7 @@ class ReleaseOverview extends AsyncView<Props, State> {
                       orgId={organization.slug}
                       projectId={project.id}
                       version={params.release}
+                      location={location}
                     />
                   </Main>
                   <Sidebar>
