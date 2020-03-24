4db4960fcea5f331346f01f4ff650f5c160dfa79
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-03-24 10:18:30 -0700

    chore(ts): Convert utils/retryableImport (#17863)

--- src/sentry/static/sentry/app/utils/retryableImport.tsx
+++ src/sentry/static/sentry/app/utils/retryableImport.tsx
@@ -2,7 +2,9 @@ import {isWebpackChunkLoadingError} from 'app/utils';
 
 const MAX_RETRIES = 2;
 
-export default async function retryableImport(fn) {
+export default async function retryableImport<T>(
+  fn: () => Promise<{default: T}>
+): Promise<T> {
   let retries = 0;
   const tryLoad = async () => {
     try {
