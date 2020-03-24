b9f5990e23d35a87e05afe5a631a8e0628e49d60
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-03-24 10:17:19 -0700

    ref(frontend): Move one-off pendingChangeQueue to where its used (#17866)

--- src/sentry/static/sentry/app/stores/groupStore.jsx
+++ src/sentry/static/sentry/app/stores/groupStore.jsx
@@ -4,13 +4,36 @@ import Reflux from 'reflux';
 
 import GroupActions from 'app/actions/groupActions';
 import IndicatorStore from 'app/stores/indicatorStore';
-import PendingChangeQueue from 'app/utils/pendingChangeQueue';
 import {t} from 'app/locale';
 
 function showAlert(msg, type) {
   IndicatorStore.add(msg, type, {
     duration: 4000,
   });
+}
+
+class PendingChangeQueue {
+  constructor() {
+    this.changes = [];
+  }
+
+  getForItem(itemId) {
+    return this.changes.filter(change => change[1] === itemId);
+  }
+
+  push(changeId, itemId, data) {
+    this.changes.push([changeId, itemId, data]);
+  }
+
+  remove(changeId, itemId) {
+    this.changes = this.changes.filter(
+      change => change[0] !== changeId || change[1] !== itemId
+    );
+  }
+
+  forEach() {
+    this.changes.forEach.apply(this.changes, arguments);
+  }
 }
 
 const GroupStore = Reflux.createStore({
