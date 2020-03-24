getsentry/sentry

commit 48ba4525e92706aaab7c459df89d29964a6700ad

ref(ts): Simplify `<Button>` types a bit (#17816)
Add adds missing button theme properties to make ts happy.

src/sentry/static/sentry/app/utils/theme.tsx
@@ -140,6 +140,7 @@ const button = {
   },
   primary: {
     color: colors.white,
+    colorActive: colors.white,
     background: colors.purple,
     backgroundActive: '#4e3fb4',
     border: '#3d328e',
@@ -150,6 +151,7 @@ const button = {
   },
   success: {
     color: colors.white,
+    colorActive: colors.white,
     background: '#3fa372',
     backgroundActive: colors.green,
     border: '#7ccca5',
@@ -160,6 +162,7 @@ const button = {
   },
   danger: {
     color: colors.white,
+    colorActive: colors.white,
     background: colors.red,
     backgroundActive: '#bf2a1d',
     border: '#bf2a1d',
@@ -170,17 +173,23 @@ const button = {
   },
   link: {
     color: colors.blue,
+    colorActive: colors.blue,
     background: 'transparent',
+    border: 'none',
+    borderActive: 'inherit',
     // border: '#3d328e',
     backgroundActive: 'transparent',
     // borderActive: '#352b7b',
+    focusShadow: false,
   },
   disabled: {
     color: aliases.disabled,
+    colorActive: aliases.disabled,
     border: '#e3e5e6',
     borderActive: '#e3e5e6',
     background: colors.white,
     backgroundActive: colors.white,
+    focusShadow: false,
   },
 } as const;
 

src/sentry/static/sentry/app/components/button.tsx
@@ -1,10 +1,11 @@
 import {Link} from 'react-router';
+import {css} from '@emotion/core';
 import PropTypes from 'prop-types';
 import React from 'react';
-import styled from '@emotion/styled';
-import {css} from '@emotion/core';
 import isPropValid from '@emotion/is-prop-valid';
+import styled from '@emotion/styled';
 
+import {Theme} from 'app/utils/theme';
 import ExternalLink from 'app/components/links/externalLink';
 import InlineSvg from 'app/components/inlineSvg';
 import Tooltip from 'app/components/tooltip';
@@ -133,8 +134,7 @@ class Button extends React.Component<ButtonProps, {}> {
 
       // destructure from `buttonProps`
       // not necessary, but just in case someone re-orders props
-      // eslint-disable-next-line no-unused-vars
-      onClick,
+      onClick: _onClick,
       ...buttonProps
     } = this.props;
     // For `aria-label`
@@ -210,7 +210,7 @@ ButtonForwardRef.displayName = 'forwardRef<Button>';
 
 export default ButtonForwardRef;
 
-type StyledButtonProps = ButtonProps & {theme?: any};
+type StyledButtonProps = ButtonProps & {theme: Theme};
 
 const getFontSize = ({size, theme}: StyledButtonProps) => {
   switch (size) {
@@ -329,7 +329,11 @@ const StyledButton = styled(
 /**
  * Get label padding determined by size
  */
-const getLabelPadding = ({size, priority, borderless}: StyledButtonProps) => {
+const getLabelPadding = ({
+  size,
+  priority,
+  borderless,
+}: Pick<StyledButtonProps, 'size' | 'priority' | 'borderless'>) => {
   if (priority === 'link') {
     return '0';
   }
@@ -352,11 +356,12 @@ const getLabelPadding = ({size, priority, borderless}: StyledButtonProps) => {
   }
 };
 
+const buttonLabelPropKeys = ['size', 'priority', 'borderless', 'align'];
 type ButtonLabelProps = Pick<ButtonProps, 'size' | 'priority' | 'borderless' | 'align'>;
 
-const ButtonLabel = styled(
-  ({size, priority, borderless, align, ...props}: ButtonLabelProps) => <span {...props} />
-)<Props>`
+const ButtonLabel = styled('span', {
+  shouldForwardProp: prop => isPropValid(prop) && !buttonLabelPropKeys.includes(prop),
+})<ButtonLabelProps>`
   display: grid;
   grid-auto-flow: column;
   align-items: center;
@@ -364,7 +369,7 @@ const ButtonLabel = styled(
   padding: ${getLabelPadding};
 `;
 
-type IconProps = Omit<React.HTMLProps<HTMLSpanElement>, 'size'> & {
+type IconProps = {
   size?: ButtonProps['size'];
   hasChildren?: boolean;
 };
@@ -378,7 +383,7 @@ const getIconMargin = ({size, hasChildren}: IconProps) => {
   return size && size.endsWith('small') ? '6px' : '8px';
 };
 
-const Icon = styled(({hasChildren, ...props}: IconProps) => <span {...props} />)`
+const Icon = styled('span')<IconProps>`
   margin-right: ${getIconMargin};
   display: flex;
 `;

tests/js/spec/components/__snapshots__/button.spec.jsx.snap
@@ -44,18 +44,11 @@ exports[`Button renders 1`] = `
             priority="primary"
             size="large"
           >
-            <Component
-              align="center"
+            <span
               className="css-ym5hp1-ButtonLabel edwq9my1"
-              priority="primary"
-              size="large"
             >
-              <span
-                className="css-ym5hp1-ButtonLabel edwq9my1"
-              >
-                Button
-              </span>
-            </Component>
+              Button
+            </span>
           </ButtonLabel>
         </button>
       </Component>
@@ -103,16 +96,11 @@ exports[`Button renders disabled normal link 1`] = `
           <ButtonLabel
             align="center"
           >
-            <Component
-              align="center"
+            <span
               className="css-zmpclt-ButtonLabel edwq9my1"
             >
-              <span
-                className="css-zmpclt-ButtonLabel edwq9my1"
-              >
-                Normal Link
-              </span>
-            </Component>
+              Normal Link
+            </span>
           </ButtonLabel>
         </a>
       </Component>
@@ -160,16 +148,11 @@ exports[`Button renders normal link 1`] = `
           <ButtonLabel
             align="center"
           >
-            <Component
-              align="center"
+            <span
               className="css-zmpclt-ButtonLabel edwq9my1"
             >
-              <span
-                className="css-zmpclt-ButtonLabel edwq9my1"
-              >
-                Normal Link
-              </span>
-            </Component>
+              Normal Link
+            </span>
           </ButtonLabel>
         </a>
       </Component>
@@ -227,16 +210,11 @@ exports[`Button renders react-router link 1`] = `
             <ButtonLabel
               align="center"
             >
-              <Component
-                align="center"
+              <span
                 className="css-zmpclt-ButtonLabel edwq9my1"
               >
-                <span
-                  className="css-zmpclt-ButtonLabel edwq9my1"
-                >
-                  Router Link
-                </span>
-              </Component>
+                Router Link
+              </span>
             </ButtonLabel>
           </a>
         </Link>

tests/js/spec/components/__snapshots__/confirmDelete.spec.jsx.snap
@@ -222,16 +222,11 @@ exports[`ConfirmDelete renders 1`] = `
                     <ButtonLabel
                       align="center"
                     >
-                      <Component
-                        align="center"
+                      <span
                         className="css-zmpclt-ButtonLabel edwq9my1"
                       >
-                        <span
-                          className="css-zmpclt-ButtonLabel edwq9my1"
-                        >
-                          Cancel
-                        </span>
-                      </Component>
+                        Cancel
+                      </span>
                     </ButtonLabel>
                   </button>
                 </Component>
@@ -288,17 +283,11 @@ exports[`ConfirmDelete renders 1`] = `
                       align="center"
                       priority="primary"
                     >
-                      <Component
-                        align="center"
+                      <span
                         className="css-zmpclt-ButtonLabel edwq9my1"
-                        priority="primary"
                       >
-                        <span
-                          className="css-zmpclt-ButtonLabel edwq9my1"
-                        >
-                          Confirm
-                        </span>
-                      </Component>
+                        Confirm
+                      </span>
                     </ButtonLabel>
                   </button>
                 </Component>

tests/js/spec/components/forms/__snapshots__/formField.spec.jsx.snap
@@ -325,17 +325,11 @@ exports[`FormField + model renders with Form 1`] = `
                           align="center"
                           priority="primary"
                         >
-                          <Component
-                            align="center"
+                          <span
                             className="css-zmpclt-ButtonLabel edwq9my1"
-                            priority="primary"
                           >
-                            <span
-                              className="css-zmpclt-ButtonLabel edwq9my1"
-                            >
-                              Save Changes
-                            </span>
-                          </Component>
+                            Save Changes
+                          </span>
                         </ButtonLabel>
                       </button>
                     </Component>

tests/js/spec/components/forms/__snapshots__/tableField.spec.jsx.snap
@@ -303,56 +303,44 @@ exports[`TableField renders renders with form context 1`] = `
                                                   align="center"
                                                   size="xsmall"
                                                 >
-                                                  <Component
-                                                    align="center"
+                                                  <span
                                                     className="css-cmi7y3-ButtonLabel edwq9my1"
-                                                    size="xsmall"
                                                   >
-                                                    <span
-                                                      className="css-cmi7y3-ButtonLabel edwq9my1"
+                                                    <Icon
+                                                      hasChildren={true}
+                                                      size="xsmall"
                                                     >
-                                                      <Icon
-                                                        hasChildren={true}
+                                                      <span
+                                                        className="css-5zck09-Icon edwq9my2"
                                                         size="xsmall"
                                                       >
-                                                        <Component
-                                                          className="css-5zck09-Icon edwq9my2"
-                                                          hasChildren={true}
-                                                          size="xsmall"
+                                                        <ForwardRef(IconAdd)
+                                                          circle={true}
+                                                          size="xs"
                                                         >
-                                                          <span
-                                                            className="css-5zck09-Icon edwq9my2"
-                                                            size="xsmall"
+                                                          <svg
+                                                            fill="currentColor"
+                                                            height="12px"
+                                                            viewBox="0 0 16 16"
+                                                            width="12px"
                                                           >
-                                                            <ForwardRef(IconAdd)
-                                                              circle={true}
-                                                              size="xs"
-                                                            >
-                                                              <svg
-                                                                fill="currentColor"
-                                                                height="12px"
-                                                                viewBox="0 0 16 16"
-                                                                width="12px"
-                                                              >
-                                                                <g>
-                                                                  <path
-                                                                    d="M11.28,8.75H4.72a.75.75,0,1,1,0-1.5h6.56a.75.75,0,1,1,0,1.5Z"
-                                                                  />
-                                                                  <path
-                                                                    d="M8,12a.76.76,0,0,1-.75-.75V4.72a.75.75,0,0,1,1.5,0v6.56A.76.76,0,0,1,8,12Z"
-                                                                  />
-                                                                  <path
-                                                                    d="M8,16a8,8,0,1,1,8-8A8,8,0,0,1,8,16ZM8,1.53A6.47,6.47,0,1,0,14.47,8,6.47,6.47,0,0,0,8,1.53Z"
-                                                                  />
-                                                                </g>
-                                                              </svg>
-                                                            </ForwardRef(IconAdd)>
-                                                          </span>
-                                                        </Component>
-                                                      </Icon>
-                                                      Add Thing
-                                                    </span>
-                                                  </Component>
+                                                            <g>
+                                                              <path
+                                                                d="M11.28,8.75H4.72a.75.75,0,1,1,0-1.5h6.56a.75.75,0,1,1,0,1.5Z"
+                                                              />
+                                                              <path
+                                                                d="M8,12a.76.76,0,0,1-.75-.75V4.72a.75.75,0,0,1,1.5,0v6.56A.76.76,0,0,1,8,12Z"
+                                                              />
+                                                              <path
+                                                                d="M8,16a8,8,0,1,1,8-8A8,8,0,0,1,8,16ZM8,1.53A6.47,6.47,0,1,0,14.47,8,6.47,6.47,0,0,0,8,1.53Z"
+                                                              />
+                                                            </g>
+                                                          </svg>
+                                                        </ForwardRef(IconAdd)>
+                                                      </span>
+                                                    </Icon>
+                                                    Add Thing
+                                                  </span>
                                                 </ButtonLabel>
                                               </button>
                                             </Component>
@@ -507,17 +495,11 @@ exports[`TableField renders renders with form context 1`] = `
                           align="center"
                           priority="primary"
                         >
-                          <Component
-                            align="center"
+                          <span
                             className="css-zmpclt-ButtonLabel edwq9my1"
-                            priority="primary"
                           >
-                            <span
-                              className="css-zmpclt-ButtonLabel edwq9my1"
-                            >
-                              Save Changes
-                            </span>
-                          </Component>
+                            Save Changes
+                          </span>
                         </ButtonLabel>
                       </button>
                     </Component>
@@ -743,56 +725,44 @@ exports[`TableField renders renders without form context 1`] = `
                                             align="center"
                                             size="xsmall"
                                           >
-                                            <Component
-                                              align="center"
+                                            <span
                                               className="css-cmi7y3-ButtonLabel edwq9my1"
-                                              size="xsmall"
                                             >
-                                              <span
-                                                className="css-cmi7y3-ButtonLabel edwq9my1"
+                                              <Icon
+                                                hasChildren={true}
+                                                size="xsmall"
                                               >
-                                                <Icon
-                                                  hasChildren={true}
+                                                <span
+                                                  className="css-5zck09-Icon edwq9my2"
                                                   size="xsmall"
                                                 >
-                                                  <Component
-                                                    className="css-5zck09-Icon edwq9my2"
-                                                    hasChildren={true}
-                                                    size="xsmall"
+                                                  <ForwardRef(IconAdd)
+                                                    circle={true}
+                                                    size="xs"
                                                   >
-                                                    <span
-                                                      className="css-5zck09-Icon edwq9my2"
-                                                      size="xsmall"
+                                                    <svg
+                                                      fill="currentColor"
+                                                      height="12px"
+                                                      viewBox="0 0 16 16"
+                                                      width="12px"
                                                     >
-                                                      <ForwardRef(IconAdd)
-                                                        circle={true}
-                                                        size="xs"
-                                                      >
-                                                        <svg
-                                                          fill="currentColor"
-                                                          height="12px"
-                                                          viewBox="0 0 16 16"
-                                                          width="12px"
-                                                        >
-                                                          <g>
-                                                            <path
-                                                              d="M11.28,8.75H4.72a.75.75,0,1,1,0-1.5h6.56a.75.75,0,1,1,0,1.5Z"
-                                                            />
-                                                            <path
-                                                              d="M8,12a.76.76,0,0,1-.75-.75V4.72a.75.75,0,0,1,1.5,0v6.56A.76.76,0,0,1,8,12Z"
-                                                            />
-                                                            <path
-                                                              d="M8,16a8,8,0,1,1,8-8A8,8,0,0,1,8,16ZM8,1.53A6.47,6.47,0,1,0,14.47,8,6.47,6.47,0,0,0,8,1.53Z"
-                                                            />
-                                                          </g>
-                                                        </svg>
-                                                      </ForwardRef(IconAdd)>
-                                                    </span>
-                                                  </Component>
-                                                </Icon>
-                                                Add Item
-                                              </span>
-                                            </Component>
+                                                      <g>
+                                                        <path
+                                                          d="M11.28,8.75H4.72a.75.75,0,1,1,0-1.5h6.56a.75.75,0,1,1,0,1.5Z"
+                                                        />
+                                                        <path
+                                                          d="M8,12a.76.76,0,0,1-.75-.75V4.72a.75.75,0,0,1,1.5,0v6.56A.76.76,0,0,1,8,12Z"
+                                                        />
+                                                        <path
+                                                          d="M8,16a8,8,0,1,1,8-8A8,8,0,0,1,8,16ZM8,1.53A6.47,6.47,0,1,0,14.47,8,6.47,6.47,0,0,0,8,1.53Z"
+                                                        />
+                                                      </g>
+                                                    </svg>
+                                                  </ForwardRef(IconAdd)>
+                                                </span>
+                                              </Icon>
+                                              Add Item
+                                            </span>
                                           </ButtonLabel>
                                         </button>
                                       </Component>

tests/js/spec/components/group/__snapshots__/externalIssueForm.spec.jsx.snap
@@ -173,17 +173,11 @@ exports[`ExternalIssueForm create renders 1`] = `
                             align="center"
                             priority="primary"
                           >
-                            <Component
-                              align="center"
+                            <span
                               className="css-zmpclt-ButtonLabel edwq9my1"
-                              priority="primary"
                             >
-                              <span
-                                className="css-zmpclt-ButtonLabel edwq9my1"
-                              >
-                                Create Issue
-                              </span>
-                            </Component>
+                              Create Issue
+                            </span>
                           </ButtonLabel>
                         </button>
                       </Component>
@@ -2901,17 +2895,11 @@ exports[`ExternalIssueForm link renders 1`] = `
                             align="center"
                             priority="primary"
                           >
-                            <Component
-                              align="center"
+                            <span
                               className="css-zmpclt-ButtonLabel edwq9my1"
-                              priority="primary"
                             >
-                              <span
-                                className="css-zmpclt-ButtonLabel edwq9my1"
-                              >
-                                Link Issue
-                              </span>
-                            </Component>
+                              Link Issue
+                            </span>
                           </ButtonLabel>
                         </button>
                       </Component>

tests/js/spec/components/modals/__snapshots__/integrationDetailsModal.spec.jsx.snap
@@ -402,17 +402,11 @@ exports[`IntegrationDetailsModal renders simple integration 1`] = `
                   align="center"
                   size="small"
                 >
-                  <Component
-                    align="center"
+                  <span
                     className="css-19gcr2f-ButtonLabel edwq9my1"
-                    size="small"
                   >
-                    <span
-                      className="css-19gcr2f-ButtonLabel edwq9my1"
-                    >
-                      Cancel
-                    </span>
-                  </Component>
+                    Cancel
+                  </span>
                 </ButtonLabel>
               </button>
             </Component>
@@ -811,18 +805,11 @@ exports[`IntegrationDetailsModal renders simple integration 1`] = `
                                   priority="primary"
                                   size="small"
                                 >
-                                  <Component
-                                    align="center"
+                                  <span
                                     className="css-19gcr2f-ButtonLabel edwq9my1"
-                                    priority="primary"
-                                    size="small"
                                   >
-                                    <span
-                                      className="css-19gcr2f-ButtonLabel edwq9my1"
-                                    >
-                                      Add Installation
-                                    </span>
-                                  </Component>
+                                    Add Installation
+                                  </span>
                                 </ButtonLabel>
                               </button>
                             </Component>

commit fd7137cf4b4dc48d1c0476f786b7b02482177361

fix(ui): Fix `<Button>` hover styles for link priority
Fixes a regression introduced in #17816

Hover/active/focus styles were incorrect for `<Button>` with `priority="link"`.

docs-ui/components/button.stories.js
@@ -59,6 +59,12 @@ storiesOf('UI|Buttons', module)
           </Item>
 
           <Item>
+            <Button priority="link" onClick={action('click link')}>
+              Link Button
+            </Button>
+          </Item>
+
+          <Item>
             <Button to="" disabled onClick={action('click disabled')}>
               Disabled Button
             </Button>

src/sentry/static/sentry/app/utils/theme.tsx
@@ -175,11 +175,9 @@ const button = {
     color: colors.blue,
     colorActive: colors.blue,
     background: 'transparent',
-    border: 'none',
-    borderActive: 'inherit',
-    // border: '#3d328e',
+    border: false,
+    borderActive: false,
     backgroundActive: 'transparent',
-    // borderActive: '#352b7b',
     focusShadow: false,
   },
   disabled: {