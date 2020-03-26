3918af92e6b046e7d57c88236816bffe89b4c82a
Matej Minar <matej.minar@sentry.io>
2020-03-26 11:29:43 +0100

    fix(ui): Fixed data massaging for keyValueList in richHttpContent

--- src/sentry/static/sentry/app/components/events/interfaces/richHttpContent/getTransformedData.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/richHttpContent/getTransformedData.tsx
@@ -2,11 +2,17 @@ import {defined} from 'app/utils';
 
 function getTransformedData(data: any) {
   if (Array.isArray(data)) {
-    return data.filter(dataValue => defined(dataValue));
-  }
-
-  if (typeof data === 'object') {
-    return Object.keys(data).map(key => [key, data[key]]);
+    return data
+      .filter(dataValue => defined(dataValue))
+      .map(dataValue => {
+        if (Array.isArray(dataValue)) {
+          return dataValue;
+        }
+        if (typeof data === 'object') {
+          return Object.keys(dataValue).flatMap(key => [key, dataValue[key]]);
+        }
+        return dataValue;
+      });
   }
 
   return [];
