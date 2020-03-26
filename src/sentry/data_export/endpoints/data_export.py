68065e480f0bb4272d3a56a7de2ec6ab14e99ef5
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-03-26 18:04:55 -0400

    ♻️ Refactor the serializer for POST reqs

--- src/sentry/data_export/endpoints/data_export.py
+++ src/sentry/data_export/endpoints/data_export.py
@@ -17,7 +17,8 @@ from ..models import ExportedData
 from ..tasks import assemble_download
 
 
-class ExportedDataSerializer(serializers.Serializer):
+class DataExportQuerySerializer(serializers.Serializer):
+    limit = serializers.IntegerField(allow_null=True)
     query_type = serializers.ChoiceField(choices=ExportQueryType.as_str_choices(), required=True)
     query_info = serializers.JSONField(required=True)
 
@@ -31,13 +32,12 @@ class DataExportEndpoint(OrganizationEndpoint, EnvironmentMixin):
         email user upon completion,
         """
 
+        # Ensure new data-exports features are enabled
         if not features.has("organizations:data-export", organization):
             return Response(status=404)
 
         limit = request.data.get("limit")
-        serializer = ExportedDataSerializer(
-            data=request.data, context={"organization": organization, "user": request.user}
-        )
+        serializer = DataExportQuerySerializer(data=request.data)
         try:
             environment_id = self._get_environment_id_from_request(request, organization.id)
         except Environment.DoesNotExist as error:
@@ -47,6 +47,12 @@ class DataExportEndpoint(OrganizationEndpoint, EnvironmentMixin):
             return Response(serializer.errors, status=400)
 
         data = serializer.validated_data
+
+        # Ensure discover features are enabled if necessary
+        if data["query_type"] == ExportQueryType.DISCOVER_STR and not features.has(
+            "organizations:discover-basic", organization, actor=request.user
+        ):
+            return Response({"detail": "You do not have access to discover features"}, status=403)
 
         try:
             # If this user has sent a sent a request with the same payload and organization,
