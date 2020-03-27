1858697eeb30318dca8655843c5dfefd9a3b6fc0
William Mak <william@wmak.io>
2020-03-27 17:12:52 -0400

    feat(performance) - Adding key transactions to event stats
    - Yanking the key transaction query conditions out into its own function
    - Reusing the conditions for event stats
    - Adding the same param that forces conditions on a query for timeseries
      too.

--- src/sentry/discover/endpoints/discover_key_transactions.py
+++ src/sentry/discover/endpoints/discover_key_transactions.py
@@ -8,7 +8,7 @@ from sentry.api.bases.organization import OrganizationPermission
 from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.discover.models import KeyTransaction
 from sentry.discover.endpoints.serializers import KeyTransactionSerializer
-from sentry.snuba.discover import query
+from sentry.snuba.discover import key_transaction_query, key_transaction_timeseries_query
 
 
 class IsKeyTransactionEndpoint(KeyTransactionBase):
@@ -71,40 +71,8 @@ class KeyTransactionEndpoint(KeyTransactionBase):
         if not queryset.exists():
             raise ResourceDoesNotExist
 
-        results = query(
-            fields,
-            request.GET.get("query"),
-            params,
-            orderby=orderby,
-            referrer="discover.key_transactions",
-            # The snuba query for transactions is of the form
-            # (transaction="1" AND project=1) OR (transaction="2" and project=2) ...
-            # which the schema intentionally doesn't support so we cannot do an AND in OR
-            # so here the "and" operator is being instead to do an AND in OR query
-            conditions=[
-                [
-                    # First layer is Ands
-                    [
-                        # Second layer is Ors
-                        [
-                            "and",
-                            [
-                                [
-                                    "equals",
-                                    # Without the outer ' here, the transaction will be treated as another column
-                                    # instead of a string. This isn't an injection risk since snuba is smart enough to
-                                    # handle escaping for us.
-                                    ["transaction", u"'{}'".format(transaction.transaction)],
-                                ],
-                                ["equals", ["project_id", transaction.project.id]],
-                            ],
-                        ],
-                        "=",
-                        1,
-                    ]
-                    for transaction in queryset
-                ]
-            ],
+        results = key_transaction_query(
+            fields, request.GET.get("query"), params, orderby, "discover.key_transactions", queryset
         )
 
         return Response(
@@ -130,3 +98,30 @@ class KeyTransactionEndpoint(KeyTransactionBase):
         model.delete()
 
         return Response(status=204)
+
+
+class KeyTransactionStatsEndpoint(KeyTransactionBase):
+    permission_classes = (OrganizationPermission,)
+
+    def get(self, request, organization):
+        """ Get the Key Transactions for a user """
+        if not self.has_feature(request, organization):
+            return self.response(status=404)
+
+        queryset = KeyTransaction.objects.filter(organization=organization, owner=request.user)
+        if not queryset.exists():
+            raise ResourceDoesNotExist
+
+        def get_event_stats(query_columns, query, params, rollup, reference_event=None):
+            return key_transaction_timeseries_query(
+                selected_columns=query_columns,
+                query=query,
+                params=params,
+                rollup=rollup,
+                referrer="api.organization-event-stats.key-transactions",
+                queryset=queryset,
+            )
+
+        return Response(
+            self.get_event_stats_data(request, organization, get_event_stats), status=200
+        )
