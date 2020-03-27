cceeef24cdf22f7a0667ab4f2f9f8cc4eb71514c
Dora <dora.l.chan@gmail.com>
2020-03-26 20:44:30 -0700

    fix(alert refactor): inline text and alignment fix for alerts (#17951)
    * fix alerts
    
    * added auto margin to vertically center text
    
    * update snapshots
    
    * refactor and replace with standard spacing
    
    Co-authored-by: Dora Chan <dora.lchan@gmail.com>

--- src/sentry/static/sentry/app/components/alertMessage.tsx
+++ src/sentry/static/sentry/app/components/alertMessage.tsx
@@ -32,7 +32,7 @@ const AlertMessage = ({alert, system}: Props) => {
 
   return (
     <StyledAlert type={type} icon={icon} system={system}>
-      {url ? <Link href={url}>{message}</Link> : message}
+      <StyledMessage>{url ? <Link href={url}>{message}</Link> : message}</StyledMessage>
       <StyledCloseButton
         icon={<IconClose size="md" circle />}
         aria-label={t('Close')}
@@ -51,12 +51,18 @@ const StyledAlert = styled(Alert)`
   margin: 0;
 `;
 
+const StyledMessage = styled('span')`
+  display: block;
+  margin: auto ${space(4)} auto 0;
+`;
+
 const StyledCloseButton = styled(Button)`
-  align-self: flex-start;
-  margin: ${space(0.5)} 0 ${space(0.5)} auto;
   background-color: transparent;
   opacity: 0.4;
   transition: opacity 0.1s linear;
+  position: absolute;
+  top: 0;
+  right: 0;
 
   &:hover,
   &:focus {
