d9b87b56730db2f9250fb73a1945acf844d17255
Mark Story <mark@sentry.io>
2020-03-29 20:51:25 -0400

    ref(ui) Use ButtonBar in more places
    Replace the `btn-group` LESS class in more places while also replacing
    `btn` with Button component, and LESS icons with SVG icons at the same time.

--- src/sentry/static/sentry/app/components/pagination.tsx
+++ src/sentry/static/sentry/app/components/pagination.tsx
@@ -4,7 +4,10 @@ import React from 'react';
 import styled from '@emotion/styled';
 import {Query} from 'history';
 
+import {IconChevron} from 'app/icons';
 import {t} from 'app/locale';
+import Button from 'app/components/button';
+import ButtonBar from 'app/components/buttonBar';
 import parseLinkHeader from 'app/utils/parseLinkHeader';
 import {callIfFunction} from 'app/utils/callIfFunction';
 
@@ -48,39 +51,30 @@ class Pagination extends React.Component<Props> {
     const location = this.context.location;
     const path = this.props.to || location.pathname;
     const query = location.query;
-
     const links = parseLinkHeader(pageLinks);
-
-    let previousPageClassName = 'btn btn-default btn-lg prev';
-    if (links.previous.results === false) {
-      previousPageClassName += ' disabled';
-    }
-
-    let nextPageClassName = 'btn btn-default btn-lg next';
-    if (links.next.results === false) {
-      nextPageClassName += ' disabled';
-    }
 
     return (
       <div className={className}>
-        <div className="btn-group">
-          <a
+        <ButtonBar merged>
+          <Button
+            aria-label={t('Previous')}
+            size="large"
+            icon={<IconChevron direction="left" size="sm" />}
+            disabled={links.previous.results === false}
             onClick={() => {
               callIfFunction(onCursor, links.previous.cursor, path, query, -1);
             }}
-            className={previousPageClassName}
-          >
-            <span title={t('Previous')} className="icon-arrow-left" />
-          </a>
-          <a
+          />
+          <Button
+            aria-label={t('Next')}
+            size="large"
+            disabled={links.next.results === false}
+            icon={<IconChevron direction="right" size="sm" />}
             onClick={() => {
               callIfFunction(onCursor, links.next.cursor, path, query, 1);
             }}
-            className={nextPageClassName}
-          >
-            <span title={t('Next')} className="icon-arrow-right" />
-          </a>
-        </div>
+          />
+        </ButtonBar>
       </div>
     );
   }
