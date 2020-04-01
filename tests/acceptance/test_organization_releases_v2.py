9dba31898bc85ec4e0c05c17474c308f7f26c0fd
Matej Minar <matej.minar@sentry.io>
2020-04-01 19:56:41 +0200

    comment

--- tests/acceptance/test_organization_releases_v2.py
+++ tests/acceptance/test_organization_releases_v2.py
@@ -54,6 +54,8 @@ class OrganizationReleasesV2Test(AcceptanceTestCase):
             self.browser.wait_until_not(".loading")
             self.browser.snapshot("organization releases v2 - detail - pick project")
 
+    # This is snapshotting feature of globalSelectionHeader project picker where we see only specified projects
+    # and a custom footer message saying "Only projects with this release are visible."
     def test_detail_global_header(self):
         with self.feature(FEATURE_NAME):
             release = self.create_release(
