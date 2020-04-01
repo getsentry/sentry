49847dd9d8b08a28aef9a3bdd35d7abc8c43ed7f
Leander Rodrigues <leander.rodrigues@sentry.io>
2020-04-01 13:08:24 -0400

    🚚 Change names and comments

--- src/sentry/data_export/endpoints/data_export.py
+++ src/sentry/data_export/endpoints/data_export.py
@@ -17,7 +17,7 @@ from ..models import ExportedData
 from ..tasks import assemble_download
 
 
-class ExportedDataSerializer(serializers.Serializer):
+class DataExportQuerySerializer(serializers.Serializer):
     query_type = serializers.ChoiceField(choices=ExportQueryType.as_str_choices(), required=True)
     query_info = serializers.JSONField(required=True)
 
@@ -30,12 +30,12 @@ class DataExportEndpoint(OrganizationEndpoint, EnvironmentMixin):
         Create a new asynchronous file export task, and
         email user upon completion,
         """
-
+        # Ensure new data-export features are enabled
         if not features.has("organizations:data-export", organization):
             return Response(status=404)
 
         limit = request.data.get("limit")
-        serializer = ExportedDataSerializer(
+        serializer = DataExportQuerySerializer(
             data=request.data, context={"organization": organization, "user": request.user}
         )
         try:
@@ -47,7 +47,11 @@ class DataExportEndpoint(OrganizationEndpoint, EnvironmentMixin):
             return Response(serializer.errors, status=400)
 
         data = serializer.validated_data
-
+        # Ensure discover features are enabled if necessary
+        if data["query_type"] == ExportQueryType.DISCOVER_STR and not features.has(
+            "organizations:discover-basic", organization, actor=request.user
+        ):
+            return Response({"detail": "You do not have access to discover features"}, status=403)
         try:
             # If this user has sent a sent a request with the same payload and organization,
             # we return them the latest one that is NOT complete (i.e. don't start another)
