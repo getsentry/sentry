1986f4732dcf63c25760eda22fabf6f81ba172af
Matej Minar <matej.minar@sentry.io>
2020-04-02 19:22:32 +0200

    feat(ui): Add footer to discover chart on release detail page (#18042)

--- src/sentry/static/sentry/app/views/releasesV2/detail/overview/chart/discoverChartContainer.tsx
+++ src/sentry/static/sentry/app/views/releasesV2/detail/overview/chart/discoverChartContainer.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Location} from 'history';
 import * as ReactRouter from 'react-router';
 import styled from '@emotion/styled';
+import isEqual from 'lodash/isEqual';
 
 import {Organization, GlobalSelection} from 'app/types';
 import space from 'app/styles/space';
@@ -10,9 +11,16 @@ import {Client} from 'app/api';
 import {formatVersion} from 'app/utils/formatters';
 import EventView from 'app/utils/discover/eventView';
 import {EventsChart} from 'app/views/events/eventsChart';
+import {fetchTotalCount} from 'app/actionCreators/events';
 import {getUtcDateString} from 'app/utils/dates';
 import {Panel} from 'app/components/panels';
 import getDynamicText from 'app/utils/getDynamicText';
+import {
+  ChartControls,
+  InlineContainer,
+  SectionHeading,
+  SectionValue,
+} from 'app/components/charts/styles';
 
 type Props = {
   organization: Organization;
@@ -23,7 +31,41 @@ type Props = {
   version: string;
 };
 
-class DiscoverChartContainer extends React.Component<Props> {
+type State = {
+  totalEvents: null | number;
+};
+
+class DiscoverChartContainer extends React.Component<Props, State> {
+  state: State = {
+    totalEvents: null,
+  };
+
+  componentDidMount() {
+    this.fetchTotalCount();
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    const {version, organization, selection} = this.props;
+    if (
+      prevProps.version !== version ||
+      !isEqual(prevProps.organization, organization) ||
+      !isEqual(prevProps.selection, selection)
+    ) {
+      this.fetchTotalCount();
+    }
+  }
+
+  async fetchTotalCount() {
+    const {api, organization, location} = this.props;
+
+    const totalEvents = await fetchTotalCount(
+      api,
+      organization.slug,
+      this.getEventView().getEventsAPIPayload(location)
+    );
+    this.setState({totalEvents});
+  }
+
   getEventView(): EventView {
     const {selection, version} = this.props;
     const {projects, environments, datetime} = selection;
@@ -47,28 +89,42 @@ class DiscoverChartContainer extends React.Component<Props> {
   }
 
   render() {
+    const {totalEvents} = this.state;
     const {organization, location, api, router, selection} = this.props;
     const {projects, environments, datetime} = selection;
     const {start, end, period, utc} = datetime;
+    const eventView = this.getEventView();
 
     return (
       <StyledPanel>
         {getDynamicText({
           value: (
-            <EventsChart
-              router={router}
-              organization={organization}
-              showLegend
-              yAxis={undefined}
-              query={this.getEventView().getEventsAPIPayload(location).query}
-              api={api}
-              projects={projects}
-              environments={environments}
-              start={start}
-              end={end}
-              period={period}
-              utc={utc}
-            />
+            <React.Fragment>
+              <EventsChart
+                router={router}
+                organization={organization}
+                showLegend
+                yAxis={eventView.getYAxis()}
+                query={eventView.getEventsAPIPayload(location).query}
+                api={api}
+                projects={projects}
+                environments={environments}
+                start={start}
+                end={end}
+                period={period}
+                utc={utc}
+                disablePrevious
+                currentSeriesName={t('Events')}
+              />
+              <ChartControls>
+                <InlineContainer>
+                  <SectionHeading>{t('Total Events')}</SectionHeading>
+                  <SectionValue>
+                    {totalEvents === null ? '\u2015' : totalEvents.toLocaleString()}
+                  </SectionValue>
+                </InlineContainer>
+              </ChartControls>
+            </React.Fragment>
           ),
           fixed: 'events chart',
         })}
