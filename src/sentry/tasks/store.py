039f69a34eaed23df4a06d48d72d290dfc77101f
Anton Ovchinnikov <anton@tonyo.info>
2020-04-02 20:48:12 +0200

    More metrics

--- src/sentry/tasks/store.py
+++ src/sentry/tasks/store.py
@@ -220,7 +220,7 @@ def _do_symbolicate_event(cache_key, start_time, event_id, symbolicate_task, dat
     from_reprocessing = symbolicate_task is symbolicate_event_from_reprocessing
 
     try:
-        with metrics.timer("tasks.store.symbolication", tags={"stage": "symbolicate"}):
+        with metrics.timer("tasks.store.symbolicate_event.symbolication"):
             symbolicated_data = safe_execute(
                 symbolication_function, data, _passthrough_errors=(RetrySymbolication,)
             )
@@ -408,9 +408,11 @@ def _do_process_event(
 
     try:
         if not new_process_behavior:
-            with metrics.timer("tasks.store.symbolication", tags={"stage": "process"}):
-                # Event enhancers.  These run before anything else.
-                for plugin in plugins.all(version=2):
+            # Event enhancers.  These run before anything else.
+            for plugin in plugins.all(version=2):
+                with metrics.timer(
+                    "tasks.store.process_event.enhancers", tags={"plugin": plugin.slug}
+                ):
                     enhancers = safe_execute(plugin.get_event_enhancers, data=data)
                     for enhancer in enhancers or ():
                         enhanced = safe_execute(
@@ -421,7 +423,8 @@ def _do_process_event(
                             has_changed = True
 
         # Stacktrace based event processors.
-        new_data = process_stacktraces(data)
+        with metrics.timer("tasks.store.process_event.stacktraces"):
+            new_data = process_stacktraces(data)
         if new_data is not None:
             has_changed = True
             data = new_data
@@ -490,14 +493,15 @@ def _do_process_event(
     # TODO(dcramer): ideally we would know if data changed by default
     # Default event processors.
     for plugin in plugins.all(version=2):
-        processors = safe_execute(
-            plugin.get_event_preprocessors, data=data, _with_transaction=False
-        )
-        for processor in processors or ():
-            result = safe_execute(processor, data)
-            if result:
-                data = result
-                has_changed = True
+        with metrics.timer("tasks.store.process_event.preprocessors", tags={"plugin": plugin.slug}):
+            processors = safe_execute(
+                plugin.get_event_preprocessors, data=data, _with_transaction=False
+            )
+            for processor in processors or ():
+                result = safe_execute(processor, data)
+                if result:
+                    data = result
+                    has_changed = True
 
     assert data["project"] == project_id, "Project cannot be mutated by plugins"
 
