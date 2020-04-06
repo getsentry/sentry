12a36d1997a6d5764e70ebb7b5fbbf4fae57fcc5
William Mak <william@wmak.io>
2020-04-06 16:11:08 -0400

    fix(performance-views) - Return nothing instead of 404 (#18092)
    * fix(performance-views) - Return nothing instead of 404
    
    - When there are no key transactions for a user return nothing with a
      200 instead of a 404
    - This means that we'll still zerofill results, but no snuba query is
      being made without a queryset
    - Adding multiple yaxis tests too

--- src/sentry/snuba/discover.py
+++ src/sentry/snuba/discover.py
@@ -690,21 +690,26 @@ def key_transaction_timeseries_query(selected_columns, query, params, rollup, re
         queryset (QuerySet) Filtered QuerySet of KeyTransactions
     """
     snuba_filter = get_timeseries_snuba_filter(selected_columns, query, params, rollup)
-    snuba_filter.conditions.extend(key_transaction_conditions(queryset))
 
-    result = raw_query(
-        aggregations=snuba_filter.aggregations,
-        conditions=snuba_filter.conditions,
-        filter_keys=snuba_filter.filter_keys,
-        start=snuba_filter.start,
-        end=snuba_filter.end,
-        rollup=rollup,
-        orderby="time",
-        groupby=["time"],
-        dataset=Dataset.Discover,
-        limit=10000,
-        referrer=referrer,
-    )
+    if queryset.exists():
+        snuba_filter.conditions.extend(key_transaction_conditions(queryset))
+
+        result = raw_query(
+            aggregations=snuba_filter.aggregations,
+            conditions=snuba_filter.conditions,
+            filter_keys=snuba_filter.filter_keys,
+            start=snuba_filter.start,
+            end=snuba_filter.end,
+            rollup=rollup,
+            orderby="time",
+            groupby=["time"],
+            dataset=Dataset.Discover,
+            limit=10000,
+            referrer=referrer,
+        )
+    else:
+        result = {"data": []}
+
     result = zerofill(result["data"], snuba_filter.start, snuba_filter.end, rollup, "time")
 
     return SnubaTSResult({"data": result}, snuba_filter.start, snuba_filter.end, rollup)
