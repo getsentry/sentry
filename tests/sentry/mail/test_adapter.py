a2f31ce6a5f535a6988db179e1b9058125e4b517
Dan Fuller <dfuller@sentry.io>
2020-04-07 16:13:34 -0700

    refs(mail): Copy `rule_notify` code into `MailAdapter`, and remove digest specific code from original.
    It's difficult to refactor the `rule_notify` code well, and isn't all that necessary since we're
    moving away from using the plugin soon enough. So just copying the original code.
    
    We also remove the digest related code from the original `rule_notify`, since it is only ever used
    with the `MailPlugin` and isn't needed in `NotificationPlugin` any more.

--- tests/sentry/mail/test_adapter.py
+++ tests/sentry/mail/test_adapter.py
@@ -26,6 +26,7 @@ from sentry.models import (
 from sentry.ownership import grammar
 from sentry.ownership.grammar import dump_schema, Matcher, Owner
 from sentry.plugins.base import Notification
+from sentry.rules.processor import RuleFuture
 from sentry.testutils import TestCase
 from sentry.testutils.helpers.datetime import before_now, iso_format
 from sentry.utils.email import MessageBuilder
@@ -501,3 +502,24 @@ class MailAdapterNotifyDigestTest(BaseMailAdapterTest, TestCase):
         msg = mail.outbox[0]
 
         assert msg.subject.startswith("[Example prefix]")
+
+
+class MailAdapterRuleNotifyTest(BaseMailAdapterTest, TestCase):
+    def test_normal(self):
+        event = self.store_event(data={}, project_id=self.project.id)
+        rule = Rule.objects.create(project=self.project, label="my rule")
+        futures = [RuleFuture(rule, {})]
+        with mock.patch.object(self.adapter, "notify") as notify:
+            self.adapter.rule_notify(event, futures)
+            notify.call_count == 1
+
+    @mock.patch("sentry.mail.adapter.digests")
+    def test_digest(self, digests):
+        digests.enabled.return_value = True
+
+        event = self.store_event(data={}, project_id=self.project.id)
+        rule = Rule.objects.create(project=self.project, label="my rule")
+
+        futures = [RuleFuture(rule, {})]
+        self.adapter.rule_notify(event, futures)
+        digests.add.call_count == 1
