7eae17be6bf49d8d60d84ac5905494297509f947
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2020-04-08 14:09:07 -0700

    fix(Integrations): Center the disabled Add button (#18136)
    Objective
    Center the disabled Add Project button when user doesn't have a sufficient plan.

--- src/sentry/static/sentry/app/views/organizationIntegrations/abstractIntegrationDetailedView.tsx
+++ src/sentry/static/sentry/app/views/organizationIntegrations/abstractIntegrationDetailedView.tsx
@@ -464,7 +464,7 @@ const DisableWrapper = styled('div')`
   align-self: center;
   display: flex;
   flex-direction: column;
-  align-items: flex-end;
+  align-items: center;
 `;
 
 const CreatedContainer = styled('div')`
