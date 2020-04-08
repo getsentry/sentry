5e819a1c41803a651f23f80762aacfcaa72475c1
Chris Fuller <cfuller@sentry.io>
2020-04-07 23:29:32 -0400

    Model and migration changes

--- src/sentry/incidents/models.py
+++ src/sentry/incidents/models.py
@@ -410,7 +410,12 @@ class AlertRule(Model):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_alertrule"
-        unique_together = (("organization", "name"),)
+        # This constraint does not match what is in migration 0061, since there is no
+        # way to declare an index on an expression. Therefore, tests would break depending
+        # on whether we run migrations - to work around this, we skip some tests if
+        # migrations have not been run. In migration 0061, this index is set to
+        # a partial index where status=0
+        unique_together = (("organization", "name", "status"),)
 
 
 class TriggerStatus(Enum):
