5ba8696d4e5d44d5a14cae9ea8592bffae9f38c4
Billy Vong <billy@sentry.io>
2020-04-09 16:47:43 -0700

    ref(ui): Remove `<ProjectContext>` from Project Install
    This removes `<ProjectContext>` as a dependency from `ProjectInstall` views, as it is only used to fetch project id from a slug.

--- src/sentry/static/sentry/app/views/projectInstall/platform.jsx
+++ src/sentry/static/sentry/app/views/projectInstall/platform.jsx
@@ -10,18 +10,16 @@ import Button from 'app/components/button';
 import LoadingError from 'app/components/loadingError';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import NotFound from 'app/components/errors/notFound';
+import Projects from 'app/utils/projects';
 import SentryDocumentTitle from 'app/components/sentryDocumentTitle';
-import SentryTypes from 'app/sentryTypes';
 import platforms from 'app/data/platforms';
 import space from 'app/styles/space';
 import withApi from 'app/utils/withApi';
 import withOrganization from 'app/utils/withOrganization';
-import withProject from 'app/utils/withProject';
 
 class ProjectInstallPlatform extends React.Component {
   static propTypes = {
     api: PropTypes.object,
-    project: SentryTypes.Project.isRequired,
   };
 
   state = {
@@ -71,7 +69,7 @@ class ProjectInstallPlatform extends React.Component {
   }
 
   render() {
-    const {project, params} = this.props;
+    const {params} = this.props;
     const {orgId, projectId} = params;
 
     const platform = platforms.find(p => p.id === params.platform);
@@ -80,7 +78,7 @@ class ProjectInstallPlatform extends React.Component {
       return <NotFound />;
     }
 
-    const issueStreamLink = `/organizations/${orgId}/issues/?project=${project.id}#welcome`;
+    const issueStreamLink = `/organizations/${orgId}/issues/`;
     const gettingStartedLink = `/organizations/${orgId}/projects/${projectId}/getting-started/`;
 
     return (
@@ -88,7 +86,7 @@ class ProjectInstallPlatform extends React.Component {
         <PanelHeader hasButtons>
           {t('Configure %(platform)s', {platform: platform.name})}
           <Actions>
-            <Button size="small" href={gettingStartedLink}>
+            <Button size="small" to={gettingStartedLink}>
               {t('< Back')}
             </Button>
             <Button size="small" href={platform.link} external>
@@ -126,9 +124,37 @@ class ProjectInstallPlatform extends React.Component {
           )}
 
           {this.isGettingStarted && (
-            <Button priority="primary" to={issueStreamLink} style={{marginTop: 20}}>
-              {t('Got it! Take me to the Issue Stream.')}
-            </Button>
+            <Projects
+              key={`${orgId}-${projectId}`}
+              orgId={orgId}
+              slugs={[projectId]}
+              passthroughPlaceholderProject={false}
+            >
+              {({projects, initiallyLoaded, fetching, fetchError}) => {
+                const projectsLoading = !initiallyLoaded && fetching;
+                const issueStreamLinkQuery =
+                  !projectsLoading && !fetchError && projects.length
+                    ? {
+                        project: projects[0].id,
+                      }
+                    : {};
+
+                return (
+                  <Button
+                    priority="primary"
+                    busy={projectsLoading}
+                    to={{
+                      pathname: issueStreamLink,
+                      query: issueStreamLinkQuery,
+                      hash: '#welcome',
+                    }}
+                    style={{marginTop: 20}}
+                  >
+                    {t('Got it! Take me to the Issue Stream.')}
+                  </Button>
+                );
+              }}
+            </Projects>
           )}
         </PanelBody>
       </Panel>
@@ -153,4 +179,4 @@ const Actions = styled('div')`
 `;
 
 export {ProjectInstallPlatform};
-export default withApi(withOrganization(withProject(ProjectInstallPlatform)));
+export default withApi(withOrganization(ProjectInstallPlatform));
