5410224e1e5c2d5f99dce6f7025cfdad6810ce0e
Chris Fuller <cfuller@sentry.io>
2020-04-10 12:24:23 -0400

    Changing manager reference

--- tests/sentry/incidents/endpoints/test_organization_alert_rule_details.py
+++ tests/sentry/incidents/endpoints/test_organization_alert_rule_details.py
@@ -389,8 +389,8 @@ class AlertRuleDetailsDeleteEndpointTest(AlertRuleDetailsBase, APITestCase):
             self.get_valid_response(self.organization.slug, self.alert_rule.id, status_code=204)
 
         assert not AlertRule.objects.filter(id=self.alert_rule.id).exists()
-        assert not AlertRule.objects_with_deleted.filter(name=self.alert_rule.name)
-        assert AlertRule.objects_with_deleted.filter(id=self.alert_rule.id).exists()
+        assert not AlertRule.objects_with_archived.filter(name=self.alert_rule.name)
+        assert AlertRule.objects_with_archived.filter(id=self.alert_rule.id).exists()
 
     def test_archive_and_create_new_with_same_name(self):
 
