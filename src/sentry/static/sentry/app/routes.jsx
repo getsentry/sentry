getsentry/sentry

commit 72be3f06bb595017986b1177b27ec41ccad70c65

feat(ui): Add progressive loading to "Events" table (#18138)
Because of the move to lightweight org, projects is not guaranteed at time of rendering. Use placeholders for the project avatar and make the link to event text until the projects are loaded.

* Revert "Revert "perf(ui): Move old "Events" to lightweight route tree… (#18137)"

This reverts commit 6e98fa72a2e2325ff1a62c8a54fe65e5a654a222.

src/sentry/static/sentry/app/routes.jsx
@@ -1199,6 +1199,21 @@ function routes() {
           />
         </Route>
 
+        <Route
+          path="/organizations/:orgId/events/"
+          componentPromise={() =>
+            import(/* webpackChunkName: "EventsContainer" */ 'app/views/events')
+          }
+          component={errorHandler(LazyLoad)}
+        >
+          <IndexRoute
+            componentPromise={() =>
+              import(/* webpackChunkName: "Events" */ 'app/views/events/events')
+            }
+            component={errorHandler(LazyLoad)}
+          />
+        </Route>
+
         {/* Settings routes */}
         <Route path="/settings/" name="Settings" component={SettingsWrapper}>
           <IndexRoute
@@ -1557,21 +1572,6 @@ function routes() {
                 import(
                   /* webpackChunkName: "PerformanceTransactionSummary" */ 'app/views/performance/transactionSummary'
                 )
-              }
-              component={errorHandler(LazyLoad)}
-            />
-          </Route>
-
-          <Route
-            path="/organizations/:orgId/events/"
-            componentPromise={() =>
-              import(/* webpackChunkName: "EventsContainer" */ 'app/views/events')
-            }
-            component={errorHandler(LazyLoad)}
-          >
-            <IndexRoute
-              componentPromise={() =>
-                import(/* webpackChunkName: "Events" */ 'app/views/events/events')
               }
               component={errorHandler(LazyLoad)}
             />

tests/js/spec/views/events/events.spec.jsx
@@ -30,7 +30,7 @@ const pageTwoLinks = generatePageLinks(100, 100);
 const EventsWithRouter = withRouter(Events);
 
 describe('EventsErrors', function() {
-  const {organization, router, routerContext} = initializeOrg({
+  const {organization, projects, router, routerContext} = initializeOrg({
     projects: [{isMember: true}, {isMember: true, slug: 'new-project', id: 3}],
     organization: {
       features: ['events'],
@@ -55,7 +55,7 @@ describe('EventsErrors', function() {
     });
     MockApiClient.addMockResponse({
       url: `/organizations/${organization.slug}/projects/`,
-      body: [],
+      body: projects,
     });
     MockApiClient.addMockResponse({
       url: `/organizations/${organization.slug}/recent-searches/`,
@@ -129,6 +129,8 @@ describe('EventsErrors', function() {
     expect(eventsStatsMock).toHaveBeenCalled();
     expect(eventsMetaMock).not.toHaveBeenCalled();
     expect(wrapper.find('LoadingIndicator')).toHaveLength(0);
+
+    // projects and user badges
     expect(wrapper.find('IdBadge')).toHaveLength(2);
   });
 

src/sentry/static/sentry/app/views/events/eventsTable.jsx
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import styled from '@emotion/styled';
 
-import {t} from 'app/locale';
 import {PanelBody, Panel, PanelHeader} from 'app/components/panels';
+import {t} from 'app/locale';
 import DateTime from 'app/components/dateTime';
 import EmptyStateWarning from 'app/components/emptyStateWarning';
 import IdBadge from 'app/components/idBadge';
 import LoadingIndicator from 'app/components/loadingIndicator';
+import Placeholder from 'app/components/placeholder';
 import SentryTypes from 'app/sentryTypes';
 import overflowEllipsis from 'app/styles/overflowEllipsis';
 import space from 'app/styles/space';
+import withProjects from 'app/utils/withProjects';
 
 class EventsTableBody extends React.PureComponent {
   static propTypes = {
@@ -27,25 +29,31 @@ class EventsTableBody extends React.PureComponent {
     return events.map((event, eventIdx) => {
       const project = projectsMap.get(event.projectID);
       const trimmedMessage = event.title || event.message.split('\n')[0].substr(0, 100);
-      const eventLink = `/organizations/${organization.slug}/projects/${project.slug}/events/${event.eventID}/`;
+      const eventLink = project
+        ? `/organizations/${organization.slug}/projects/${project?.slug}/events/${event.eventID}/`
+        : '';
 
       return (
-        <TableRow key={`${project.slug}-${event.eventID}`} first={eventIdx === 0}>
+        <TableRow key={`${project?.slug}-${event.eventID}`} first={eventIdx === 0}>
           <TableData>
             <EventTitle>
-              <Link to={eventLink}>{trimmedMessage}</Link>
+              {project ? <Link to={eventLink}>{trimmedMessage}</Link> : trimmedMessage}
             </EventTitle>
           </TableData>
 
           <TableData>{event['event.type']}</TableData>
 
           <TableData>
-            <IdBadge
-              project={project}
-              avatarSize={16}
-              displayName={<span>{project.slug}</span>}
-              avatarProps={{consistentWidth: true}}
-            />
+            {project ? (
+              <IdBadge
+                project={project}
+                avatarSize={16}
+                displayName={<span>{project?.slug}</span>}
+                avatarProps={{consistentWidth: true}}
+              />
+            ) : (
+              <Placeholder height="16px" width="50px" />
+            )}
           </TableData>
 
           <TableData>
@@ -68,6 +76,9 @@ class EventsTable extends React.Component {
     // Initial loading state
     loading: PropTypes.bool,
 
+    // projectsStore loading state of projects
+    loadingProjects: PropTypes.bool,
+
     // When initial data has been loaded, but params have changed
     reloading: PropTypes.bool,
 
@@ -89,7 +100,8 @@ class EventsTable extends React.Component {
     if (
       this.props.reloading !== nextProps.reloading ||
       this.props.zoomChanged !== nextProps.zoomChanged ||
-      this.props.loading !== nextProps.loading
+      this.props.loading !== nextProps.loading ||
+      this.props.loadingProjects !== nextProps.loadingProjects
     ) {
       return true;
     }
@@ -114,8 +126,8 @@ class EventsTable extends React.Component {
   }
 
   get projectsMap() {
-    const {organization, projects} = this.props;
-    const projectList = projects || organization.projects;
+    const {organization, loadingProjects, projects} = this.props;
+    const projectList = (!loadingProjects && projects) || organization.projects || [];
 
     return new Map(projectList.map(project => [project.id, project]));
   }
@@ -157,7 +169,7 @@ class EventsTable extends React.Component {
   }
 }
 
-export default withRouter(EventsTable);
+export default withProjects(withRouter(EventsTable));
 export {EventsTable};
 
 const StyledPanelBody = styled(PanelBody)`

src/sentry/static/sentry/app/views/events/index.jsx
@@ -9,7 +9,7 @@ import {t} from 'app/locale';
 import BetaTag from 'app/components/betaTag';
 import Feature from 'app/components/acl/feature';
 import GlobalSelectionHeader from 'app/components/organizations/globalSelectionHeader';
-import NoProjectMessage from 'app/components/noProjectMessage';
+import LightWeightNoProjectMessage from 'app/components/lightWeightNoProjectMessage';
 import SentryTypes from 'app/sentryTypes';
 import PageHeading from 'app/components/pageHeading';
 import withApi from 'app/utils/withApi';
@@ -67,7 +67,7 @@ class EventsContainer extends React.Component {
           resetParamsOnChange={['cursor']}
         />
         <PageContent>
-          <NoProjectMessage organization={organization}>
+          <LightWeightNoProjectMessage organization={organization}>
             <Body>
               <PageHeader>
                 <HeaderTitle>
@@ -83,7 +83,7 @@ class EventsContainer extends React.Component {
               </PageHeader>
               {children}
             </Body>
-          </NoProjectMessage>
+          </LightWeightNoProjectMessage>
         </PageContent>
       </Feature>
     );

commit 4650204eb5de1d880ef78e8dd9a0c55b71b74a2d

fix(ui): Fix search by event id on `<Events>`
This fixes a regression introduced in https://github.com/getsentry/sentry/pull/18138 - when searching by event id and there is a direct hit, the component used `organization.projects` (which no longer has a list of all projects on org) to lookup project slug by project id (id is returned by search results).

* Refactor to use `handleRequestSuccess` so that we redirect before updating state when a direct hit is found. Adds an API request for project slug lookup.
* Moves `<ProjectEventRedirect>` to lightweight routes
* Design touchups for `<ProjectEventRedirect>`

src/sentry/static/sentry/app/routes.jsx
@@ -1453,6 +1453,11 @@ function routes() {
           component={errorHandler(LazyLoad)}
         />
 
+        <Route
+          path="/organizations/:orgId/projects/:projectId/events/:eventId/"
+          component={errorHandler(ProjectEventRedirect)}
+        />
+
         {/* Admin/manage routes */}
         <Route
           path="/manage/"
@@ -1721,10 +1726,6 @@ function routes() {
               component={errorHandler(LazyLoad)}
             />
           </Route>
-          <Route
-            path="/organizations/:orgId/projects/:projectId/events/:eventId/"
-            component={errorHandler(ProjectEventRedirect)}
-          />
           <Route
             path="/organizations/:orgId/releases/"
             componentPromise={() =>

src/sentry/static/sentry/app/views/events/events.jsx
@@ -121,22 +121,31 @@ class Events extends AsyncView {
     return `Events - ${this.props.organization.slug}`;
   }
 
-  onRequestSuccess({data, jqXHR}) {
-    const {organization} = this.props;
-
-    // TODO: This is actually not optimal because `AsyncComponent.handleRequestSuccess`
-    // still gets called and updates state when the response may not be what the component
-    // expects.
-    //
-    // Ideally when a direct hit is found, we should not update state in `handleRequestSuccess`
+  async handleRequestSuccess({stateKey, data, jqXHR}, ...args) {
+    // When a direct hit is found, do not update state in `handleRequestSuccess`
     if (jqXHR.getResponseHeader('X-Sentry-Direct-Hit') === '1') {
+      const {organization} = this.props;
       const event = data[0];
-      const project = organization.projects.find(p => p.id === event.projectID);
 
-      browserHistory.replace(
-        `/organizations/${organization.slug}/projects/${project.slug}/events/${event.eventID}/`
+      const resp = await this.api.requestPromise(
+        `/organizations/${organization.slug}/projects/`,
+        {
+          query: {
+            query: `id:${event.projectID}`,
+          },
+        }
       );
+
+      if (resp && resp.length > 0) {
+        const project = resp[0];
+        browserHistory.replace(
+          `/organizations/${organization.slug}/projects/${project.slug}/events/${event.eventID}/`
+        );
+        return;
+      }
     }
+
+    super.handleRequestSuccess({stateKey, data, jqXHR}, ...args);
   }
 
   onRequestError(resp) {

src/sentry/static/sentry/app/views/projectEventRedirect.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
-import DetailedError from 'app/components/errors/detailedError';
+import {PageContent} from 'app/styles/organization';
 import {t} from 'app/locale';
+import DetailedError from 'app/components/errors/detailedError';
 
 /**
  * This component performs a client-side redirect to Event Details given only
@@ -70,7 +71,7 @@ class ProjectEventRedirect extends React.Component {
         />
       );
     }
-    return null;
+    return <PageContent />;
   }
 }
 

tests/js/spec/views/events/events.spec.jsx
@@ -450,6 +450,8 @@ describe('EventsContainer', function() {
       routerContext
     );
 
+    await tick();
+
     expect(eventsMock).toHaveBeenCalled();
     expect(browserHistory.replace).toHaveBeenCalledWith(
       `/organizations/org-slug/projects/project-slug/events/${eventId}/`