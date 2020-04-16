0475725592ac649d73c8524ba46552f1bddc15d3
Stephen Cefali <scefali@sentry.io>
2020-04-16 09:48:23 -0700

    feat(integrations): adds signing secret verification for slack webhooks (#18214)

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -121,6 +121,7 @@ register("cloudflare.secret-key", default="")
 register("slack.client-id", flags=FLAG_PRIORITIZE_DISK)
 register("slack.client-secret", flags=FLAG_PRIORITIZE_DISK)
 register("slack.verification-token", flags=FLAG_PRIORITIZE_DISK)
+register("slack.signing-secret", flags=FLAG_PRIORITIZE_DISK)
 
 # GitHub Integration
 register("github-app.id", default=0)
