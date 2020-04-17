360514d124688b4547d377b7270432f6e4399e5f
William Mak <william@wmak.io>
2020-04-16 20:25:09 -0400

    feature(performance): Top5 Events (#17987)
    * feature(performance): Adding the topEvents param to event-stats
    
    - Using gropuby to determine topEvents
    - Two snuba queries are made, one with limit=5 to determine what the top events are, and another for the timeseries
    - Should work with multi yaxis
    - Created a new discover function for top events

--- src/sentry/snuba/discover.py
+++ src/sentry/snuba/discover.py
@@ -19,7 +19,7 @@ from sentry.api.event_search import (
 
 from sentry import eventstore
 
-from sentry.models import Project, ProjectStatus
+from sentry.models import Project, ProjectStatus, Group
 from sentry.tagstore.base import TOP_VALUES_DEFAULT_LIMIT
 from sentry.utils.snuba import (
     Dataset,
@@ -40,6 +40,7 @@ __all__ = (
     "query",
     "key_transaction_query",
     "timeseries_query",
+    "top_events_timeseries",
     "get_pagination_ids",
     "get_facets",
     "transform_results",
@@ -692,7 +693,7 @@ def get_timeseries_snuba_filter(selected_columns, query, params, rollup, referen
             snuba_filter.conditions.extend(ref_conditions)
 
     # Resolve the public aliases into the discover dataset names.
-    snuba_filter, _ = resolve_discover_aliases(snuba_filter)
+    snuba_filter, translated_columns = resolve_discover_aliases(snuba_filter)
     if not snuba_filter.aggregations:
         raise InvalidSearchQuery("Cannot get timeseries result with no aggregation.")
 
@@ -701,7 +702,7 @@ def get_timeseries_snuba_filter(selected_columns, query, params, rollup, referen
     if len(snuba_filter.aggregations) == 1:
         snuba_filter.aggregations[0][2] = "count"
 
-    return snuba_filter
+    return snuba_filter, translated_columns
 
 
 def key_transaction_timeseries_query(selected_columns, query, params, rollup, referrer, queryset):
@@ -717,7 +718,7 @@ def key_transaction_timeseries_query(selected_columns, query, params, rollup, re
         referrer (str|None) A referrer string to help locate the origin of this query.
         queryset (QuerySet) Filtered QuerySet of KeyTransactions
     """
-    snuba_filter = get_timeseries_snuba_filter(selected_columns, query, params, rollup)
+    snuba_filter, _ = get_timeseries_snuba_filter(selected_columns, query, params, rollup)
 
     if queryset.exists():
         snuba_filter.conditions.extend(key_transaction_conditions(queryset))
@@ -765,8 +766,7 @@ def timeseries_query(selected_columns, query, params, rollup, reference_event=No
                     conditions based on the provided reference.
     referrer (str|None) A referrer string to help locate the origin of this query.
     """
-
-    snuba_filter = get_timeseries_snuba_filter(
+    snuba_filter, _ = get_timeseries_snuba_filter(
         selected_columns, query, params, rollup, reference_event
     )
 
@@ -786,6 +786,110 @@ def timeseries_query(selected_columns, query, params, rollup, reference_event=No
     result = zerofill(result["data"], snuba_filter.start, snuba_filter.end, rollup, "time")
 
     return SnubaTSResult({"data": result}, snuba_filter.start, snuba_filter.end, rollup)
+
+
+def top_events_timeseries(
+    timeseries_columns,
+    selected_columns,
+    user_query,
+    params,
+    orderby,
+    rollup,
+    limit,
+    organization,
+    referrer=None,
+):
+    """
+    High-level API for doing arbitrary user timeseries queries for a limited number of top events
+
+    Returns a dictionary of SnubaTSResult objects that have been zerofilled in
+    case of gaps. Each value of the dictionary should match the result of a timeseries query
+
+    timeseries_columns (Sequence[str]) List of public aliases to fetch for the timeseries query,
+                        usually matches the y-axis of the graph
+    selected_columns (Sequence[str]) List of public aliases to fetch for the events query,
+                        this is to determine what the top events are
+    user_query (str) Filter query string to create conditions from. needs to be user_query
+                        to not conflict with the function query
+    params (Dict[str, str]) Filtering parameters with start, end, project_id, environment,
+    orderby (Sequence[str]) The fields to order results by.
+    rollup (int) The bucket width in seconds
+    limit (int) The number of events to get timeseries for
+    organization (Organization) Used to map group ids to short ids
+    referrer (str|None) A referrer string to help locate the origin of this query.
+    """
+    top_events = query(
+        selected_columns,
+        query=user_query,
+        params=params,
+        orderby=orderby,
+        limit=limit,
+        referrer=referrer,
+    )
+
+    snuba_filter, translated_columns = get_timeseries_snuba_filter(
+        timeseries_columns + selected_columns, user_query, params, rollup
+    )
+
+    for field in selected_columns:
+        # project is handled by filter_keys already
+        if field in ["project", "project.id"]:
+            continue
+        values = list({event.get(field) for event in top_events["data"] if field in event})
+        if values and all(value is not None for value in values):
+            # timestamp needs special handling, creating a big OR instead
+            if field == "timestamp":
+                snuba_filter.conditions.append([["timestamp", "=", value] for value in values])
+            else:
+                snuba_filter.conditions.append([resolve_column(field), "IN", values])
+
+    result = raw_query(
+        aggregations=snuba_filter.aggregations,
+        conditions=snuba_filter.conditions,
+        filter_keys=snuba_filter.filter_keys,
+        start=snuba_filter.start,
+        end=snuba_filter.end,
+        rollup=rollup,
+        orderby="time",
+        groupby=["time"] + snuba_filter.groupby,
+        dataset=Dataset.Discover,
+        limit=10000,
+        referrer=referrer,
+    )
+
+    result = transform_results(result, translated_columns, snuba_filter)
+    issues = {}
+    if "issue" in selected_columns:
+        issues = Group.issues_mapping(
+            set([event["issue.id"] for event in top_events["data"]]),
+            params["project_id"],
+            organization,
+        )
+
+    translated_columns["project_id"] = "project"
+    translated_groupby = [translated_columns.get(field, field) for field in snuba_filter.groupby]
+    # so the result key is consistent
+    translated_groupby.sort()
+
+    results = {}
+    for row in result["data"]:
+        values = []
+        for field in translated_groupby:
+            if field == "issue.id":
+                values.append(issues.get(row["issue.id"], "unknown"))
+            else:
+                values.append(six.text_type(row.get(field)))
+        result_key = ",".join(values)
+        results.setdefault(result_key, []).append(row)
+    for key, item in six.iteritems(results):
+        results[key] = SnubaTSResult(
+            {"data": zerofill(item, snuba_filter.start, snuba_filter.end, rollup, "time")},
+            snuba_filter.start,
+            snuba_filter.end,
+            rollup,
+        )
+
+    return results
 
 
 def get_id(result):
