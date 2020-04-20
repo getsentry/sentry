69eef81bc80c481c1b90af5af876dc9e08b3b7e7
Evan Hicks <evan.hicks@sentry.io>
2020-04-20 14:47:33 -0400

    use issue search where possible

--- src/sentry/api/endpoints/organization_events_meta.py
+++ src/sentry/api/endpoints/organization_events_meta.py
@@ -1,16 +1,22 @@
 from __future__ import absolute_import
 
 import six
+from django.conf import settings
 
 from rest_framework.response import Response
 from rest_framework.exceptions import ParseError
 
 from sentry.api.base import EnvironmentMixin
 from sentry.api.bases import OrganizationEventsEndpointBase, OrganizationEventsError, NoProjects
-from sentry.models import Group, GroupStatus
+from sentry.api.helpers.group_index import build_query_params_from_request
+from sentry.api.event_search import parse_search_query
 from sentry.api.serializers import serialize
 from sentry.api.serializers.models.group import GroupSerializer
+from sentry.search.snuba.backend import EventsDatasetSnubaSearchBackend
 from sentry.snuba import discover
+
+
+IssueSeach = EventsDatasetSnubaSearchBackend(**settings.SENTRY_SEARCH_OPTIONS)
 
 
 class OrganizationEventsMetaEndpoint(OrganizationEventsEndpointBase):
@@ -57,20 +63,30 @@ class OrganizationEventsRelatedIssuesEndpoint(OrganizationEventsEndpointBase, En
                 status=400,
             )
 
-        groups = []
         try:
-            results = discover.get_related_issues(params=params, lookup_keys=lookup_keys)
-            group_ids = [r["group_id"] for r in results]
-            groups = Group.objects.filter(
-                id__in=group_ids,
-                project_id__in=params["project_id"],
-                status__in=[GroupStatus.UNRESOLVED, GroupStatus.RESOLVED, GroupStatus.IGNORED],
+            projects = self.get_projects(request, organization)
+            query_kwargs = build_query_params_from_request(
+                request, organization, projects, params["environment"]
             )
+            query_kwargs["limit"] = 5
+            try:
+                parsed_terms = parse_search_query(
+                    'transaction:"{}"'.format(lookup_keys["transaction"])
+                )
+            except ParseError as e:
+                return Response({"detail": "Invalid transaction search"}, status=400)
+
+            if query_kwargs.get("search_filters"):
+                query_kwargs["search_filters"].extend(parsed_terms)
+            else:
+                query_kwargs["search_filters"] = parsed_terms
+
+            results = IssueSeach.query(**query_kwargs)
         except discover.InvalidSearchQuery as err:
             raise ParseError(detail=six.text_type(err))
 
         context = serialize(
-            list(groups),
+            list(results),
             request.user,
             GroupSerializer(environment_func=self._get_environment_func(request, organization.id)),
         )
