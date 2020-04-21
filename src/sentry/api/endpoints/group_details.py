bdfa71543b6e5b457ac771720da139b17e413c3b
William Mak <william@wmak.io>
2020-04-21 19:19:10 -0400

    Fix(group-details): multiple tag value calls (#18383)
    - Currently we're calling snuba once per first/last release. This can be
      done in a single snuba query which should give us a significant time
      improvement

--- src/sentry/api/endpoints/group_details.py
+++ src/sentry/api/endpoints/group_details.py
@@ -160,8 +160,21 @@ class GroupDetailsEndpoint(GroupEndpoint, EnvironmentMixin):
                 version=version,
             )
         except Release.DoesNotExist:
-            return {"version": version}
+            release = {"version": version}
         return serialize(release, request.user)
+
+    def _get_first_last_release_info(self, request, group, versions):
+        releases = {
+            release.version: release
+            for release in Release.objects.filter(
+                projects=group.project,
+                organization_id=group.project.organization_id,
+                version__in=versions,
+            )
+        }
+        return serialize(
+            [releases.get(version, {"version": version}) for version in versions], request.user,
+        )
 
     @attach_scenarios([retrieve_aggregate_scenario])
     def get(self, request, group):
@@ -199,9 +212,13 @@ class GroupDetailsEndpoint(GroupEndpoint, EnvironmentMixin):
 
         action_list = self._get_actions(request, group)
 
-        if first_release:
+        if first_release is not None and last_release is not None:
+            first_release, last_release = self._get_first_last_release_info(
+                request, group, [first_release, last_release]
+            )
+        elif first_release is not None:
             first_release = self._get_release_info(request, group, first_release)
-        if last_release:
+        elif last_release is not None:
             last_release = self._get_release_info(request, group, last_release)
 
         get_range = functools.partial(tsdb.get_range, environment_ids=environment_ids)
