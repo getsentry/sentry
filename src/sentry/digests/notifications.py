e99f5628ffd5e1b4077e4ab922a5f41b99d42caa
Dan Fuller <dfuller@sentry.io>
2020-04-23 14:55:54 -0700

    refs(mail): Cleanup digests task key migration code. (#18429)
    We had transition code in place so that we could handle old-style and new-style digest keys. The
    metrics show that we're only using new-style keys now, so it's safe to remove this extra code.

--- src/sentry/digests/notifications.py
+++ src/sentry/digests/notifications.py
@@ -11,7 +11,6 @@ from six.moves import reduce
 from sentry.app import tsdb
 from sentry.digests import Record
 from sentry.models import Project, Group, GroupStatus, Rule
-from sentry.utils import metrics
 from sentry.utils.dates import to_timestamp
 
 logger = logging.getLogger("sentry.digests")
@@ -24,15 +23,8 @@ def split_key(key):
 
     key_parts = key.split(":", 4)
     project_id = key_parts[2]
-    if len(key_parts) == 5:
-        target_type = ActionTargetType(key_parts[3])
-        target_identifier = key_parts[4] if key_parts[4] else None
-        metrics.incr("digests.new_key_seen")
-    else:
-        metrics.incr("digests.old_key_seen")
-
-        target_type = ActionTargetType.ISSUE_OWNERS
-        target_identifier = None
+    target_type = ActionTargetType(key_parts[3])
+    target_identifier = key_parts[4] if key_parts[4] else None
     return Project.objects.get(pk=project_id), target_type, target_identifier
 
 
