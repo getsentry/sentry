8a504e238121343b4f943a05649e369dae815145
Mark Story <mark@sentry.io>
2020-04-23 11:26:14 -0400

    feat(ui) Add a short formatter for event id in discover results
    Format the eventid as a short sha and include a copy control to get the
    full value.

--- src/sentry/static/sentry/app/utils/discover/styles.tsx
+++ src/sentry/static/sentry/app/utils/discover/styles.tsx
@@ -1,8 +1,11 @@
+import React from 'react';
 import styled from '@emotion/styled';
 
+import Clipboard from 'app/components/clipboard';
 import DateTime from 'app/components/dateTime';
 import Link from 'app/components/links/link';
 import ShortId from 'app/components/shortId';
+import {IconCopy} from 'app/icons';
 import overflowEllipsis from 'app/styles/overflowEllipsis';
 import space from 'app/styles/space';
 
@@ -71,3 +74,22 @@ export const HeaderControls = styled('div')`
 export const StyledShortId = styled(ShortId)`
   justify-content: flex-start;
 `;
+
+const StyledIconCopy = styled(IconCopy)`
+  cursor: pointer;
+  margin-right: ${space(0.5)};
+`;
+
+export const EventId = ({value}: {value: string}) => {
+  const shortId = value.substring(0, 8);
+  return (
+    <React.Fragment>
+      <Clipboard value={value}>
+        <span>
+          <StyledIconCopy size="xs" />
+        </span>
+      </Clipboard>
+      {shortId}
+    </React.Fragment>
+  );
+};
