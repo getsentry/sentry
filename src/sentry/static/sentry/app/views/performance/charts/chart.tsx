963e3cbad0c2eea1f2edd973142ffd147a23b256
Mark Story <mark@sentry.io>
2020-04-24 09:53:51 -0400

    fix(perf-views) Link apdex and rpm graphs
    Use sub-grids on the performance overview charts. This lets us link the
    hover and zoom states for these graphs.

--- src/sentry/static/sentry/app/views/performance/charts/chart.tsx
+++ src/sentry/static/sentry/app/views/performance/charts/chart.tsx
@@ -3,38 +3,24 @@ import * as ReactRouter from 'react-router';
 
 import {Series} from 'app/types/echarts';
 import AreaChart from 'app/components/charts/areaChart';
+import {AREA_COLORS} from 'app/components/charts/utils';
 import ChartZoom from 'app/components/charts/chartZoom';
-import Tooltip from 'app/components/tooltip';
-
-import {HeaderTitle, ChartContainer, StyledIconQuestion} from '../styles';
 
 type Props = {
-  yAxis: string;
-  data: Series;
+  data: Series[];
   router: ReactRouter.InjectedRouter;
   statsPeriod: string | undefined;
   utc: boolean;
   projects: number[];
   environments: string[];
   loading: boolean;
-  tooltipCopy: string;
 };
 
 class Chart extends React.Component<Props> {
   render() {
-    const {
-      data,
-      yAxis,
-      router,
-      statsPeriod,
-      utc,
-      projects,
-      environments,
-      loading,
-      tooltipCopy,
-    } = this.props;
+    const {data, router, statsPeriod, utc, projects, environments, loading} = this.props;
 
-    if (!data || data.data.length <= 0) {
+    if (!data || data.length <= 0) {
       return null;
     }
 
@@ -42,46 +28,67 @@ class Chart extends React.Component<Props> {
       seriesOptions: {
         showSymbol: false,
       },
-      grid: {
-        left: '10px',
-        right: '10px',
-        top: '16px',
-        bottom: '0px',
+      grid: [
+        {
+          top: '8px',
+          left: '24px',
+          right: '52%',
+          bottom: '16px',
+        },
+        {
+          top: '8px',
+          left: '52%',
+          right: '24px',
+          bottom: '16px',
+        },
+      ],
+      axisPointer: {
+        // Link the two series x-axis together.
+        link: [{xAxisIndex: [0, 1]}],
       },
+      xAxes: [
+        {
+          gridIndex: 0,
+          type: 'time',
+        },
+        {
+          gridIndex: 1,
+          type: 'time',
+        },
+      ],
+      yAxes: [{gridIndex: 0}, {gridIndex: 1}],
       utc,
       isGroupedByDate: true,
       showTimeInTooltip: true,
     };
 
     if (loading) {
-      return (
-        <ChartContainer key="loading">
-          <HeaderTitle>{yAxis}</HeaderTitle>
-          <AreaChart series={[]} {...areaChartProps} />
-        </ChartContainer>
-      );
+      return <AreaChart series={[]} {...areaChartProps} />;
     }
+    const series = data.map((values, i: number) => ({
+      ...values,
+      yAxisIndex: i,
+      xAxisIndex: i,
+      color: AREA_COLORS[0].line,
+      areaStyle: {
+        color: AREA_COLORS[0].area,
+        opacity: 1.0,
+      },
+    }));
 
     return (
-      <ChartContainer key="loaded">
-        <HeaderTitle>
-          {yAxis}
-          <Tooltip position="top" title={tooltipCopy}>
-            <StyledIconQuestion size="sm" />
-          </Tooltip>
-        </HeaderTitle>
-        <ChartZoom
-          router={router}
-          period={statsPeriod}
-          utc={utc}
-          projects={projects}
-          environments={environments}
-        >
-          {zoomRenderProps => (
-            <AreaChart {...zoomRenderProps} series={[data]} {...areaChartProps} />
-          )}
-        </ChartZoom>
-      </ChartContainer>
+      <ChartZoom
+        router={router}
+        period={statsPeriod}
+        utc={utc}
+        projects={projects}
+        environments={environments}
+        xAxisIndex={[0, 1]}
+      >
+        {zoomRenderProps => (
+          <AreaChart {...zoomRenderProps} series={series} {...areaChartProps} />
+        )}
+      </ChartZoom>
     );
   }
 }
