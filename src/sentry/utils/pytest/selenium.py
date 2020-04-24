50dad2873c2de48dc2b9ea7ce797d14c54761ec1
Billy Vong <billyvg@users.noreply.github.com>
2020-04-24 11:41:19 -0700

    tests(acceptance): Add acceptance tests for global selection header (#18452)
    This adds some acceptance tests for global selection header, some tests are commented out as they are bugs that need to be fixed.

--- src/sentry/utils/pytest/selenium.py
+++ src/sentry/utils/pytest/selenium.py
@@ -69,12 +69,17 @@ class Browser(object):
         self._has_initialized_cookie_store = True
         return self
 
-    def element(self, selector):
+    def element(self, selector=None, xpath=None):
         """
         Get an element from the page. This method will wait for the element to show up.
         """
-        self.wait_until(selector)
-        return self.driver.find_element_by_css_selector(selector)
+
+        if xpath is not None:
+            self.wait_until(xpath=xpath)
+            return self.driver.find_element_by_xpath(xpath)
+        else:
+            self.wait_until(selector)
+            return self.driver.find_element_by_css_selector(selector)
 
     def element_exists(self, selector):
         """
@@ -100,8 +105,8 @@ class Browser(object):
         """
         return self.element_exists('[aria-label="%s"]' % (selector))
 
-    def click(self, selector):
-        self.element(selector).click()
+    def click(self, selector=None, xpath=None):
+        self.element(selector, xpath=xpath).click()
 
     def click_when_visible(self, selector=None, timeout=3):
         """
@@ -216,6 +221,22 @@ class Browser(object):
 
         self.percy.snapshot(name=name)
         return self
+
+    def get_local_storage_items(self):
+        """
+        Retrieve all items in local storage
+        """
+
+        return self.driver.execute_script(
+            "Object.fromEntries(Object.entries(window.localStorage));"
+        )
+
+    def get_local_storage_item(self, key):
+        """
+        Retrieve key from local storage, this will fail if you use single quotes in your keys.
+        """
+
+        return self.driver.execute_script(u"window.localStorage.getItem('{}')".format(key))
 
     def save_cookie(
         self,
