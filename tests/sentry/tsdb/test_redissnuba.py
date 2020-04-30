d89089e894d4e7df52119bad38f53ba4da66e4ee
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-04-30 10:57:38 +0200

    ref: revert test changes

--- tests/sentry/tsdb/test_redissnuba.py
+++ tests/sentry/tsdb/test_redissnuba.py
@@ -27,12 +27,12 @@ def test_redissnuba_connects_to_correct_backend():
 
     for method in methods:
         for model in should_resolve_to_redis:
-            assert "redis" == selector_func(method, get_callargs(model), None)
+            assert "redis" == selector_func(method, get_callargs(model))
 
         for model in should_resolve_to_snuba:
             read_or_write, _ = method_specifications.get(method)
 
             if read_or_write == READ:
-                assert "snuba" == selector_func(method, get_callargs(model), None)
+                assert "snuba" == selector_func(method, get_callargs(model))
             else:
-                assert "dummy" == selector_func(method, get_callargs(model), None)
+                assert "dummy" == selector_func(method, get_callargs(model))
