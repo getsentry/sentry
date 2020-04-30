2dd46a08a15fb23277c75cc3c275ccdf08384f3f
Mark Story <mark@sentry.io>
2020-04-30 14:24:17 -0400

    fix(discover) Update chart palette
    Use the same palette for discover + performance as we're using on
    releases v2.

--- src/sentry/static/sentry/app/components/charts/utils.tsx
+++ src/sentry/static/sentry/app/components/charts/utils.tsx
@@ -11,10 +11,6 @@ const DEFAULT_TRUNCATE_LENGTH = 80;
 const TWENTY_FOUR_HOURS = 1440;
 const ONE_HOUR = 60;
 
-export const AREA_COLORS = ['#FFE3FD', '#E8B0F2', '#BD81E6', '#5246A3', '#422C6F'];
-
-export const AREA_SINGLE_COLOR = AREA_COLORS[3];
-
 export type DateTimeObject = Partial<GlobalSelection['datetime']>;
 
 export function truncationFormatter(value: string, truncate: number): string {
