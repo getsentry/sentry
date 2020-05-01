b42cd4ea596fadeb4b5e359a8c69c9a609037e3b
Stephen Cefali <scefali@sentry.io>
2020-05-01 15:09:13 -0700

    All installations use bot app credentials

--- src/sentry/identity/slack/provider.py
+++ src/sentry/identity/slack/provider.py
@@ -1,7 +1,5 @@
 from __future__ import absolute_import
 
-from django.conf import settings
-
 from sentry import options
 from sentry.identity.oauth2 import OAuth2Provider
 
@@ -16,39 +14,20 @@ class SlackIdentityProvider(OAuth2Provider):
     oauth_scopes = ("identity.basic", "identity.email")
 
     def get_oauth_authorize_url(self):
-        if self.use_wst_app:
-            return "https://slack.com/oauth/authorize"
         return "https://slack.com/oauth/v2/authorize"
-
-    @property
-    def use_slack_v2(self):
-        # need to import here because of dependency issues
-        from sentry.integrations.slack.utils import use_slack_v2
-
-        return use_slack_v2(self.pipeline)
-
-    @property
-    def use_wst_app(self):
-        return settings.SLACK_INTEGRATION_USE_WST and not self.use_slack_v2
 
     # XXX(epurkhiser): While workspace tokens _do_ support the oauth.access
     # endpoint, it will no include the authorizing_user, so we continue to use
     # the deprecated oauth.token endpoint until we are able to migrate to a bot
     # app which uses oauth.access.
     def get_oauth_access_token_url(self):
-        if self.use_wst_app:
-            return "https://slack.com/api/oauth.token"
         return "https://slack.com/api/oauth.v2.access"
 
     def get_oauth_client_id(self):
-        if self.use_slack_v2:
-            return options.get("slack-v2.client-id")
-        return options.get("slack.client-id")
+        return options.get("slack-v2.client-id") or options.get("slack.client-id")
 
     def get_oauth_client_secret(self):
-        if self.use_slack_v2:
-            return options.get("slack-v2.client-secret")
-        return options.get("slack.client-secret")
+        return options.get("slack-v2.client-secret") or options.get("slack.client-secret")
 
     def get_oauth_data(self, payload):
         # TODO(epurkhiser): This flow isn't actually used right now in sentry.
--- src/sentry/integrations/slack/integration.py
+++ src/sentry/integrations/slack/integration.py
@@ -3,7 +3,6 @@ from __future__ import absolute_import
 import six
 
 from django.utils.translation import ugettext_lazy as _
-from django.conf import settings
 
 from sentry.identity.pipeline import IdentityProviderPipeline
 from sentry.integrations import (
@@ -18,7 +17,7 @@ from sentry.utils.http import absolute_uri
 from sentry.shared_integrations.exceptions import ApiError, IntegrationError
 
 from .client import SlackClient
-from .utils import logger, use_slack_v2
+from .utils import logger
 
 DESCRIPTION = """
 Connect your Sentry organization to one or more Slack workspaces, and start
@@ -79,21 +78,8 @@ class SlackIntegrationProvider(IntegrationProvider):
     features = frozenset([IntegrationFeatures.CHAT_UNFURL, IntegrationFeatures.ALERT_RULE])
     integration_cls = SlackIntegration
 
-    # Scopes differ depending on if it's a workspace app
-    wst_oauth_scopes = frozenset(
-        [
-            "channels:read",
-            "groups:read",
-            "users:read",
-            "chat:write",
-            "links:read",
-            "links:write",
-            "team:read",
-        ]
-    )
-
     # some info here: https://api.slack.com/authentication/quickstart
-    bot_oauth_scopes = frozenset(
+    identity_oauth_scopes = frozenset(
         [
             "channels:read",
             "groups:read",
@@ -109,16 +95,6 @@ class SlackIntegrationProvider(IntegrationProvider):
     )
 
     setup_dialog_config = {"width": 600, "height": 900}
-
-    @property
-    def use_wst_app(self):
-        return settings.SLACK_INTEGRATION_USE_WST and not use_slack_v2(self.pipeline)
-
-    @property
-    def identity_oauth_scopes(self):
-        if self.use_wst_app:
-            return self.wst_oauth_scopes
-        return self.bot_oauth_scopes
 
     def get_pipeline_views(self):
         identity_pipeline_config = {
@@ -154,14 +130,9 @@ class SlackIntegrationProvider(IntegrationProvider):
         access_token = data["access_token"]
         # bot apps have a different response format
         # see: https://api.slack.com/authentication/quickstart#installing
-        if self.use_wst_app:
-            user_id_slack = data["authorizing_user_id"]
-            team_name = data["team_name"]
-            team_id = data["team_id"]
-        else:
-            user_id_slack = data["authed_user"]["id"]
-            team_name = data["team"]["name"]
-            team_id = data["team"]["id"]
+        user_id_slack = data["authed_user"]["id"]
+        team_name = data["team"]["name"]
+        team_id = data["team"]["id"]
 
         scopes = sorted(self.identity_oauth_scopes)
         team_data = self.get_team_info(access_token)
@@ -171,10 +142,8 @@ class SlackIntegrationProvider(IntegrationProvider):
             "scopes": scopes,
             "icon": team_data["icon"]["image_132"],
             "domain_name": team_data["domain"] + ".slack.com",
+            "installation_type": "born_as_bot",
         }
-        # only set installation type for bot apps
-        if not self.use_wst_app:
-            metadata["installation_type"] = "born_as_bot"
 
         return {
             "name": team_name,
