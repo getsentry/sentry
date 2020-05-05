b20f62e76e9cdb0716acd2ced2476741080816c8
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-05-05 18:44:33 +0200

    wip

--- tests/acceptance/test_issue_details.py
+++ tests/acceptance/test_issue_details.py
@@ -80,7 +80,7 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
         self.browser.snapshot("issue details cocoa")
 
     def test_cocoa_event_breadcrumb_v2(self):
-        with self.feature('breadcrumbs-v2'):
+        with self.feature("breadcrumbs-v2"):
             # TODO: This should become its own "rust" platform type
             event = self.create_sample_event(platform="native", sample_name="Rust")
             self.page.visit_issue(self.org.slug, event.group.id)
@@ -92,7 +92,7 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
         self.browser.snapshot("issue details unity")
 
     def test_unity_event_breadcrumb_v2(self):
-        with self.feature('breadcrumbs-v2'):
+        with self.feature("breadcrumbs-v2"):
             event = self.create_sample_event(default="unity", platform="csharp")
             self.page.visit_issue(self.org.slug, event.group.id)
             self.browser.snapshot("issue details unity - - breadcrumbs v2")
@@ -127,8 +127,8 @@ class IssueDetailsTest(AcceptanceTestCase, SnubaTestCase):
 
         self.browser.snapshot("issue details rust")
 
-     def test_rust_event_breadcrumb_v2(self):
-        with self.feature('breadcrumbs-v2'):
+    def test_rust_event_breadcrumb_v2(self):
+        with self.feature("breadcrumbs-v2"):
             # TODO: This should become its own "rust" platform type
             event = self.create_sample_event(platform="native", sample_name="Rust")
             self.page.visit_issue(self.org.slug, event.group.id)
