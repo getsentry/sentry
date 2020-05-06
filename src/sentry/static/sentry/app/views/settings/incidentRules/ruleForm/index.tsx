73dcc8b4b6b0637b8828c4e304e9251ed858dcef
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-05-05 17:14:40 -0700

    ref(alerts): Use single environments for alerts (#18634)

--- src/sentry/static/sentry/app/views/settings/incidentRules/ruleForm/index.tsx
+++ src/sentry/static/sentry/app/views/settings/incidentRules/ruleForm/index.tsx
@@ -426,7 +426,10 @@ class RuleFormContainer extends AsyncComponent<Props, State> {
               aggregation: rule.aggregation,
               query: rule.query || '',
               timeWindow: rule.timeWindow,
-              environment: rule.environment || [],
+              environment:
+                (Array.isArray(rule.environment)
+                  ? rule.environment[0]
+                  : rule.environment) || '',
             }}
             saveOnBlur={false}
             onSubmit={this.handleSubmit}
