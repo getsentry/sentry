279d97d6595b5d48cfdce3a2cef0028d4a4d58f2
Sarah Lohmeier <sarah.lohmeier@sentry.io>
2020-05-07 17:01:27 -0700

    fix permissions

--- src/sentry/api/endpoints/project_create_sample.py
+++ src/sentry/api/endpoints/project_create_sample.py
@@ -2,13 +2,13 @@ from __future__ import absolute_import
 
 from rest_framework.response import Response
 
-from sentry.api.bases.project import ProjectEndpoint, ProjectPermission
+from sentry.api.bases.project import ProjectEndpoint, ProjectEventPermission
 from sentry.api.serializers import serialize
 from sentry.utils.samples import create_sample_event
 
 
 class ProjectCreateSampleEndpoint(ProjectEndpoint):
-    permission_classes = (ProjectPermission,)
+    permission_classes = (ProjectEventPermission,)
 
     def post(self, request, project):
         event = create_sample_event(project, platform=project.platform, default="javascript")
