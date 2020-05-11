80020be12f4786f4ec2da319cc4fcfa629d1f2cf
Priscila Oliveira <priscilawebdev@gmail.com>
2020-05-11 18:34:42 +0200

    fix(ui): added forwardRef in all svg icons (#18726)

--- src/sentry/static/sentry/app/icons/iconEllipsis.tsx
+++ src/sentry/static/sentry/app/icons/iconEllipsis.tsx
@@ -4,12 +4,12 @@ import SvgIcon from './svgIcon';
 
 type Props = React.ComponentProps<typeof SvgIcon>;
 
-const IconEllipsis = (props: Props) => (
-  <SvgIcon {...props}>
+const IconEllipsis = React.forwardRef<SVGSVGElement, Props>((props: Props, ref) => (
+  <SvgIcon {...props} ref={ref}>
     <circle cx="8" cy="8" r="1.31" />
     <circle cx="1.31" cy="8" r="1.31" />
     <circle cx="14.69" cy="8" r="1.31" />
   </SvgIcon>
-);
+));
 
 export {IconEllipsis};
