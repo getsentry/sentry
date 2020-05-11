1311970ed64c4b93e9ab6b020fe713d4fad31f83
Priscila Oliveira <priscilawebdev@gmail.com>
2020-05-11 18:34:42 +0200

    fix(ui): added forwardRef in all svg icons (#18726)

--- src/sentry/static/sentry/app/icons/iconGrabbable.tsx
+++ src/sentry/static/sentry/app/icons/iconGrabbable.tsx
@@ -4,8 +4,8 @@ import SvgIcon from './svgIcon';
 
 type Props = React.ComponentProps<typeof SvgIcon>;
 
-const IconGrabbable = (props: Props) => (
-  <SvgIcon {...props}>
+const IconGrabbable = React.forwardRef<SVGSVGElement, Props>((props: Props, ref) => (
+  <SvgIcon {...props} ref={ref}>
     <circle cx="4.73" cy="8" r="1.31" />
     <circle cx="4.73" cy="1.31" r="1.31" />
     <circle cx="11.27" cy="8" r="1.31" />
@@ -13,6 +13,6 @@ const IconGrabbable = (props: Props) => (
     <circle cx="4.73" cy="14.69" r="1.31" />
     <circle cx="11.27" cy="14.69" r="1.31" />
   </SvgIcon>
-);
+));
 
 export {IconGrabbable};
--- src/sentry/static/sentry/app/icons/iconMenu.tsx
+++ src/sentry/static/sentry/app/icons/iconMenu.tsx
@@ -4,12 +4,12 @@ import SvgIcon from './svgIcon';
 
 type Props = React.ComponentProps<typeof SvgIcon>;
 
-const IconMenu = (props: Props) => (
-  <SvgIcon {...props}>
+const IconMenu = React.forwardRef<SVGSVGElement, Props>((props: Props, ref) => (
+  <SvgIcon {...props} ref={ref}>
     <path d="M15.19,2.53H.81A.75.75,0,0,1,.81,1H15.19a.75.75,0,1,1,0,1.5Z" />
     <path d="M15.19,15H.81a.75.75,0,0,1,0-1.5H15.19a.75.75,0,1,1,0,1.5Z" />
     <path d="M15.19,8.75H.81a.75.75,0,1,1,0-1.5H15.19a.75.75,0,0,1,0,1.5Z" />
   </SvgIcon>
-);
+));
 
 export {IconMenu};
