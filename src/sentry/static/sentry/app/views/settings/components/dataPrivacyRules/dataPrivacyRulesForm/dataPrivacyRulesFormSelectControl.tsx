d2f9312768b8b11720abd92a8119b1bb6ab1b74b
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-05-13 12:11:50 +0200

    fix(ui): Added autocomplete off selectControl

--- src/sentry/static/sentry/app/views/settings/components/dataPrivacyRules/dataPrivacyRulesForm/dataPrivacyRulesFormSelectControl.tsx
+++ src/sentry/static/sentry/app/views/settings/components/dataPrivacyRules/dataPrivacyRulesForm/dataPrivacyRulesFormSelectControl.tsx
@@ -9,20 +9,40 @@ type Props = Pick<
   'value' | 'placeholder' | 'name' | 'onChange' | 'options' | 'isDisabled'
 >;
 
-const DataPrivacyRulesPanelFormSelectControl = ({...props}: Props) => (
-  <SelectControl
-    {...props}
-    isSearchable={false}
-    styles={{
-      control: (provided: {[x: string]: string | number | boolean}) => ({
-        ...provided,
-        minHeight: '40px',
-        height: '40px',
-      }),
-    }}
-    openOnFocus
-    required
-  />
-);
+class DataPrivacyRulesPanelFormSelectControl extends React.Component<Props> {
+  componentDidMount() {
+    if (!this.selectRef.current) {
+      return;
+    }
+
+    if (this.selectRef.current?.select) {
+      const input = this.selectRef.current.select?.inputRef;
+      if (input) {
+        input.autocomplete = 'off';
+      }
+    }
+  }
+
+  selectRef = React.createRef<typeof SelectControl>();
+
+  render() {
+    return (
+      <SelectControl
+        {...this.props}
+        isSearchable={false}
+        styles={{
+          control: (provided: {[x: string]: string | number | boolean}) => ({
+            ...provided,
+            minHeight: '40px',
+            height: '40px',
+          }),
+        }}
+        ref={this.selectRef}
+        openOnFocus
+        required
+      />
+    );
+  }
+}
 
 export default DataPrivacyRulesPanelFormSelectControl;
