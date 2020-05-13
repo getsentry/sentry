getsentry/sentry

commit 5641207b447f2fb212ca0818a6e9c404dde2b238

feat(ui): Refactor `<GlobalSelectionHeader>` (#18499)
This fixes a number of bugs with Global Selection Header including:

* Loading and saving of last used projects/envs with local storage. 
* race conditions with enforcement of a single project and the child view being mounted (and thus firing api requests before enforcing a single project)
* lost project context in certain situations eg after deleting a release.

src/sentry/static/sentry/app/actionCreators/globalSelection.tsx
@@ -2,12 +2,29 @@ import * as ReactRouter from 'react-router';
 import * as Sentry from '@sentry/browser';
 import isInteger from 'lodash/isInteger';
 import omit from 'lodash/omit';
+import pick from 'lodash/pick';
 import qs from 'query-string';
 
-import {Environment} from 'app/types';
+import {
+  DATE_TIME,
+  LOCAL_STORAGE_KEY,
+  URL_PARAM,
+} from 'app/constants/globalSelectionHeader';
+import {
+  Environment,
+  GlobalSelection,
+  MinimalProject,
+  Organization,
+  Project,
+} from 'app/types';
 import {defined} from 'app/utils';
+import {
+  getDefaultSelection,
+  getStateFromQuery,
+} from 'app/components/organizations/globalSelectionHeader/utils';
 import {getUtcDateString} from 'app/utils/dates';
 import GlobalSelectionActions from 'app/actions/globalSelectionActions';
+import localStorage from 'app/utils/localStorage';
 
 /**
  * Note this is the internal project.id, NOT the slug, but it is the stringified version of it
@@ -37,9 +54,6 @@ type DateTimeObject = {
   period?: string | null;
 };
 
-// Get Params type from `getParams` helper
-// type Params = Parameters<typeof getParams>[0];
-
 /**
  * Cast project ids to strings, as everything is assumed to be a string in URL params
  *
@@ -63,9 +77,138 @@ export function resetGlobalSelection() {
   GlobalSelectionActions.reset();
 }
 
+function getProjectIdFromProject(project: MinimalProject) {
+  return parseInt(project.id, 10);
+}
+
+type InitializeUrlStateParams = {
+  organization: Organization;
+  queryParams: ReactRouter.WithRouterProps['location']['query'];
+  router: ReactRouter.WithRouterProps['router'];
+  memberProjects: Project[];
+  shouldForceProject?: boolean;
+  shouldEnforceSingleProject: boolean;
+  /**
+   * If true, do not load from local storage
+   */
+  skipLastUsed?: boolean;
+  defaultSelection?: Partial<GlobalSelection>;
+  forceProject?: MinimalProject | null;
+};
+
+export function initializeUrlState({
+  organization,
+  queryParams,
+  router,
+  memberProjects,
+  skipLastUsed,
+  shouldForceProject,
+  shouldEnforceSingleProject,
+  defaultSelection,
+  forceProject,
+}: InitializeUrlStateParams) {
+  const orgSlug = organization.slug;
+  const query = pick(queryParams, [URL_PARAM.PROJECT, URL_PARAM.ENVIRONMENT]);
+  const hasProjectOrEnvironmentInUrl = Object.keys(query).length > 0;
+  const parsed = getStateFromQuery(queryParams);
+
+  let globalSelection: Omit<GlobalSelection, 'datetime'> & {
+    datetime: {
+      [K in keyof GlobalSelection['datetime']]: GlobalSelection['datetime'][K] | null;
+    };
+  } = {
+    ...getDefaultSelection(),
+    datetime: {
+      [DATE_TIME.START as 'start']: parsed.start || null,
+      [DATE_TIME.END as 'end']: parsed.end || null,
+      [DATE_TIME.PERIOD as 'period']: parsed.period || null,
+      [DATE_TIME.UTC as 'utc']: parsed.utc || null,
+    },
+    ...defaultSelection,
+  };
+
+  // We only save environment and project, so if those exist in
+  // URL, do not touch local storage
+  if (hasProjectOrEnvironmentInUrl) {
+    globalSelection.projects = parsed.project || [];
+    globalSelection.environments = parsed.environment || [];
+  } else if (!skipLastUsed) {
+    try {
+      const localStorageKey = `${LOCAL_STORAGE_KEY}:${orgSlug}`;
+      const storedValue = localStorage.getItem(localStorageKey);
+
+      if (storedValue) {
+        globalSelection = {
+          datetime: globalSelection.datetime,
+          ...JSON.parse(storedValue),
+        };
+      }
+    } catch (err) {
+      // use default if invalid
+      Sentry.captureException(err);
+      console.error(err); // eslint-disable-line no-console
+    }
+  }
+
+  const {projects, environments: environment, datetime} = globalSelection;
+  let newProject: number[] | null = null;
+  let project = projects;
+
+  /**
+   * Skip enforcing a single project if `shouldForceProject` is true,
+   * since a component is controlling what that project needs to be.
+   * This is true regardless if user has access to multi projects
+   */
+  if (shouldForceProject && forceProject) {
+    newProject = [getProjectIdFromProject(forceProject)];
+  } else if (shouldEnforceSingleProject && !shouldForceProject) {
+    /**
+     * If user does not have access to `global-views` (e.g. multi project select) *and* there is no
+     * `project` URL parameter, then we update URL params with:
+     * 1) the first project from the list of requested projects from URL params,
+     * 2) first project user is a member of from org
+     *
+     * Note this is intentionally skipped if `shouldForceProject == true` since we want to initialize store
+     * and wait for the forced project
+     */
+    if (projects && projects.length > 0) {
+      // If there is a list of projects from URL params, select first project from that list
+      newProject = typeof projects === 'string' ? [Number(projects)] : [projects[0]];
+    } else {
+      // When we have finished loading the organization into the props,  i.e. the organization slug is consistent with
+      // the URL param--Sentry will get the first project from the organization that the user is a member of.
+      newProject = [...memberProjects].slice(0, 1).map(getProjectIdFromProject);
+    }
+  }
+
+  if (newProject) {
+    globalSelection.projects = newProject;
+    project = newProject;
+  }
+
+  GlobalSelectionActions.initializeUrlState(globalSelection);
+  GlobalSelectionActions.setOrganization(organization);
+
+  // To keep URLs clean, don't push default period if url params are empty
+  const parsedWithNoDefaultPeriod = getStateFromQuery(queryParams, {
+    allowEmptyPeriod: true,
+  });
+
+  const newDatetime = {
+    ...datetime,
+    period:
+      !parsedWithNoDefaultPeriod.start &&
+      !parsedWithNoDefaultPeriod.end &&
+      !parsedWithNoDefaultPeriod.period
+        ? null
+        : datetime.period,
+    utc: !parsedWithNoDefaultPeriod.utc ? null : datetime.utc,
+  };
+  updateParamsWithoutHistory({project, environment, ...newDatetime}, router);
+}
+
 /**
- * Updates global project selection URL param if `router` is supplied
- * OTHERWISE fire action to update projects
+ * Updates store and global project selection URL param if `router` is supplied
  */
 export function updateProjects(
   projects: ProjectId[],
@@ -80,9 +223,7 @@ export function updateProjects(
     return;
   }
 
-  if (!router) {
-    GlobalSelectionActions.updateProjects(projects);
-  }
+  GlobalSelectionActions.updateProjects(projects);
   updateParams({project: projects}, router, options);
 }
 
@@ -91,8 +232,7 @@ function isProjectsValid(projects: ProjectId[]) {
 }
 
 /**
- * Updates global datetime selection URL param if `router` is supplied
- * OTHERWISE fire action to update projects
+ * Updates store and global datetime selection URL param if `router` is supplied
  *
  * @param {Object} datetime Object with start, end, range keys
  * @param {Object} [router] Router object
@@ -104,15 +244,14 @@ export function updateDateTime(
   router?: Router,
   options?: Options
 ) {
-  if (!router) {
-    GlobalSelectionActions.updateDateTime(datetime);
-  }
+  GlobalSelectionActions.updateDateTime(datetime);
+  // We only save projects/environments to local storage, do not
+  // save anything when date changes.
   updateParams(datetime, router, {...options, save: false});
 }
 
 /**
- * Updates global environment selection URL param if `router` is supplied
- * OTHERWISE fire action to update projects
+ * Updates store and updates global environment selection URL param if `router` is supplied
  *
  * @param {String[]} environments List of environments
  * @param {Object} [router] Router object
@@ -124,9 +263,7 @@ export function updateEnvironments(
   router?: Router,
   options?: Options
 ) {
-  if (!router) {
-    GlobalSelectionActions.updateEnvironments(environment);
-  }
+  GlobalSelectionActions.updateEnvironments(environment);
   updateParams({environment}, router, options);
 }
 

src/sentry/static/sentry/app/components/organizations/globalSelectionHeader/initializeGlobalSelectionHeader.tsx
@@ -1,0 +1,76 @@
+import React from 'react';
+import * as ReactRouter from 'react-router';
+
+import {initializeUrlState} from 'app/actionCreators/globalSelection';
+
+import GlobalSelectionHeader from './globalSelectionHeader';
+
+type Props = {
+  isDisabled: boolean;
+  shouldEnforceSingleProject: boolean;
+} & Pick<ReactRouter.WithRouterProps, 'location' | 'router'> &
+  Pick<
+    React.ComponentPropsWithoutRef<typeof GlobalSelectionHeader>,
+    | 'defaultSelection'
+    | 'forceProject'
+    | 'shouldForceProject'
+    | 'memberProjects'
+    | 'organization'
+  > & {
+    routes: Array<
+      ReactRouter.PlainRoute<any> & {
+        globalSelectionSkipLastUsed?: boolean;
+      }
+    >;
+  };
+
+/**
+ * Initializes GlobalSelectionHeader
+ *
+ * Calls an actionCreator to load project/environment from local storage if possible,
+ * otherwise populate with defaults.
+ *
+ * This should only happen when the header is mounted
+ * e.g. when changing views or organizations.
+ */
+class InitializeGlobalSelectionHeader extends React.Component<Props> {
+  componentDidMount() {
+    const {
+      location,
+      router,
+      routes,
+      organization,
+      defaultSelection,
+      forceProject,
+      memberProjects,
+      shouldForceProject,
+      shouldEnforceSingleProject,
+    } = this.props;
+
+    // Make an exception for routes (e.g. issue details, in the case where it is accessed directly (e.g. from email))
+    // We do not want to load the user's last used env/project in this case, otherwise will
+    // lead to very confusing behavior.
+    //
+    // `routes` is only ever undefined in tests
+    const skipLastUsed = !!routes?.find(
+      ({globalSelectionSkipLastUsed}) => globalSelectionSkipLastUsed
+    );
+    initializeUrlState({
+      organization,
+      queryParams: location.query,
+      router,
+      skipLastUsed,
+      memberProjects,
+      defaultSelection,
+      forceProject,
+      shouldForceProject,
+      shouldEnforceSingleProject,
+    });
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export default InitializeGlobalSelectionHeader;

src/sentry/static/sentry/app/stores/globalSelectionStore.jsx
@@ -1,75 +1,19 @@
-import isEqual from 'lodash/isEqual';
-import pick from 'lodash/pick';
 import Reflux from 'reflux';
+import isEqual from 'lodash/isEqual';
 
-import {
-  DATE_TIME,
-  URL_PARAM,
-  LOCAL_STORAGE_KEY,
-} from 'app/constants/globalSelectionHeader';
-import {getStateFromQuery} from 'app/components/organizations/globalSelectionHeader/utils';
-import {getParams} from 'app/components/organizations/globalSelectionHeader/getParams';
+import {LOCAL_STORAGE_KEY} from 'app/constants/globalSelectionHeader';
+import {getDefaultSelection} from 'app/components/organizations/globalSelectionHeader/utils';
 import {isEqualWithDates} from 'app/utils/isEqualWithDates';
-import OrganizationsStore from 'app/stores/organizationsStore';
 import GlobalSelectionActions from 'app/actions/globalSelectionActions';
+import OrganizationsStore from 'app/stores/organizationsStore';
 import localStorage from 'app/utils/localStorage';
-
-const DEFAULT_PARAMS = getParams({});
-
-const getDefaultSelection = () => ({
-  projects: [],
-  environments: [],
-  datetime: {
-    [DATE_TIME.START]: DEFAULT_PARAMS.start || null,
-    [DATE_TIME.END]: DEFAULT_PARAMS.end || null,
-    [DATE_TIME.PERIOD]: DEFAULT_PARAMS.statsPeriod || null,
-    [DATE_TIME.UTC]: DEFAULT_PARAMS.utc || null,
-  },
-});
-
-const getProjectsByIds = async (organization, projectIds, api) => {
-  const query = {};
-  query.query = projectIds.map(id => `id:${id}`).join(' ');
-  return await api.requestPromise(`/organizations/${organization.slug}/projects/`, {
-    query,
-  });
-};
-
-const isValidSelection = async (selection, organization, api) => {
-  if (organization.projects) {
-    const allowedProjects = new Set(
-      organization.projects
-        .filter(project => project.hasAccess)
-        .map(p => parseInt(p.id, 10))
-    );
-    if (
-      Array.isArray(selection.projects) &&
-      selection.projects.some(project => !allowedProjects.has(project))
-    ) {
-      return false;
-    }
-    return true;
-  } else {
-    // if the selection is [-1] (all projects) or [] (my projects) return true
-    if (selection.projects.length === 0 || selection.projects[0] === -1) {
-      return true;
-    }
-    // if we do not have organization.projects then make an API call to fetch projects based on id
-    const projects = await getProjectsByIds(organization, selection.projects, api);
-    if (
-      selection.projects.length !== projects.length ||
-      projects.some(project => !project.hasAccess)
-    ) {
-      return false;
-    }
-    return true;
-  }
-};
 
 const GlobalSelectionStore = Reflux.createStore({
   init() {
     this.reset(this.selection);
     this.listenTo(GlobalSelectionActions.reset, this.onReset);
+    this.listenTo(GlobalSelectionActions.initializeUrlState, this.onInitializeUrlState);
+    this.listenTo(GlobalSelectionActions.setOrganization, this.onSetOrganization);
     this.listenTo(GlobalSelectionActions.save, this.onSave);
     this.listenTo(GlobalSelectionActions.updateProjects, this.updateProjects);
     this.listenTo(GlobalSelectionActions.updateDateTime, this.updateDateTime);
@@ -77,67 +21,40 @@ const GlobalSelectionStore = Reflux.createStore({
   },
 
   reset(state) {
-    this._hasLoaded = false;
+    // Has passed the enforcement state
+    this._hasEnforcedProject = false;
+    this._hasInitialState = false;
     this.selection = state || getDefaultSelection();
   },
 
-  /**
-   * Initializes the global selection store
-   * If there are query params apply these, otherwise check local storage
-   */
-  loadInitialData(organization, queryParams, {api, skipLastUsed} = {}) {
-    this._hasLoaded = true;
-    this.organization = organization;
-    const query = pick(queryParams, Object.values(URL_PARAM));
-    const hasQuery = Object.keys(query).length > 0;
-
-    let globalSelection = getDefaultSelection();
-
-    if (hasQuery) {
-      const parsed = getStateFromQuery(queryParams);
-      globalSelection = {
-        projects: parsed.project || [],
-        environments: parsed.environment || [],
-        datetime: {
-          [DATE_TIME.START]: parsed.start || null,
-          [DATE_TIME.END]: parsed.end || null,
-          [DATE_TIME.PERIOD]: parsed.period || null,
-          [DATE_TIME.UTC]: parsed.utc || null,
-        },
-      };
-    } else if (!skipLastUsed) {
-      try {
-        const localStorageKey = `${LOCAL_STORAGE_KEY}:${organization.slug}`;
-
-        const storedValue = localStorage.getItem(localStorageKey);
-
-        const defaultDateTime = getDefaultSelection().datetime;
-
-        if (storedValue) {
-          globalSelection = {datetime: defaultDateTime, ...JSON.parse(storedValue)};
-        }
-      } catch (ex) {
-        console.error(ex); // eslint-disable-line no-console
-        // use default if invalid
-      }
-    }
-    this.loadSelectionIfValid(globalSelection, organization, api);
+  isReady() {
+    return this._hasInitialState;
   },
 
-  async loadSelectionIfValid(globalSelection, organization, api) {
-    if (await isValidSelection(globalSelection, organization, api)) {
-      this.selection = globalSelection;
-      this.trigger(this.selection);
-    }
+  onSetOrganization(organization) {
+    this.organization = organization;
+  },
+
+  /**
+   * Initializes the global selection store data
+   * Use query params if they exist, otherwise check local storage
+   */
+  onInitializeUrlState(newSelection) {
+    this._hasInitialState = true;
+    this.selection = newSelection;
+    this.trigger(this.get());
   },
 
   get() {
-    return this.selection;
+    return {
+      selection: this.selection,
+      isReady: this.isReady(),
+    };
   },
 
   onReset() {
     this.reset();
-    this.trigger(this.selection);
+    this.trigger(this.get());
   },
 
   updateProjects(projects = []) {
@@ -149,7 +66,7 @@ const GlobalSelectionStore = Reflux.createStore({
       ...this.selection,
       projects,
     };
-    this.trigger(this.selection);
+    this.trigger(this.get());
   },
 
   updateDateTime(datetime) {
@@ -161,7 +78,7 @@ const GlobalSelectionStore = Reflux.createStore({
       ...this.selection,
       datetime,
     };
-    this.trigger(this.selection);
+    this.trigger(this.get());
   },
 
   updateEnvironments(environments = []) {
@@ -173,7 +90,7 @@ const GlobalSelectionStore = Reflux.createStore({
       ...this.selection,
       environments,
     };
-    this.trigger(this.selection);
+    this.trigger(this.get());
   },
 
   /**

tests/acceptance/test_organization_global_selection_header.py
@@ -119,12 +119,9 @@ class OrganizationGlobalHeaderTest(AcceptanceTestCase, SnubaTestCase):
         # selecting an explicit project should load previously selected project
         # from local storage
         # TODO check environment as well
-        # FIXME below is currently broken
-        # self.issues_list.visit_issue_list(
-        #     self.org.slug
-        # )
-        # self.issues_list.wait_until_loaded()
-        # assert u"project={}".format(self.project_3.id) in self.browser.current_url
+        self.issues_list.visit_issue_list(self.org.slug)
+        self.issues_list.wait_until_loaded()
+        assert u"project={}".format(self.project_3.id) in self.browser.current_url
 
     def test_global_selection_header_loads_with_correct_project_with_multi_project(self):
         """
@@ -163,8 +160,7 @@ class OrganizationGlobalHeaderTest(AcceptanceTestCase, SnubaTestCase):
             # there has been no explicit project selection via UI
             self.issues_list.visit_issue_list(self.org.slug)
             assert u"project=" not in self.browser.current_url
-            # FIXME
-            # assert self.issues_list.global_selection.get_selected_project_slug() == "My Projects"
+            assert self.issues_list.global_selection.get_selected_project_slug() == "My Projects"
 
             # can select a different project
             self.issues_list.global_selection.select_project_by_slug(self.project_3.slug)
@@ -186,8 +182,7 @@ class OrganizationGlobalHeaderTest(AcceptanceTestCase, SnubaTestCase):
             self.issues_list.visit_issue_list(self.org.slug)
             self.issues_list.wait_until_loaded()
             # TODO check environment as well
-            # FIXME: This is current broken and is a bug
-            # assert u"project={}".format(self.project_3.id) in self.browser.current_url
+            assert u"project={}".format(self.project_3.id) in self.browser.current_url
             assert (
                 self.issues_list.global_selection.get_selected_project_slug() == self.project_3.slug
             )

commit 62f32e1f5068ec8d92077f90b3ad34bebb14d4d0

fix(ui): Fix browser navigation button breaking `<GlobalSelectionHeader>` (#18783)
This fixes browser navigation breaking the header and adds a test. Changes `updateProjects` to additionally accept an argument for updating environments as well as we generally need to update environments if we change projects.

This bug was introduced in #18499

src/sentry/static/sentry/app/actionCreators/globalSelection.tsx
@@ -215,11 +215,15 @@ export function initializeUrlState({
 
 /**
  * Updates store and global project selection URL param if `router` is supplied
+ *
+ * This accepts `environments` from `options` to also update environments simultaneously
+ * as environments are tied to a project, so if you change projects, you may need
+ * to clear environments.
  */
 export function updateProjects(
   projects: ProjectId[],
   router?: Router,
-  options?: Options
+  options?: Options & {environments?: EnvironmentId[]}
 ) {
   if (!isProjectsValid(projects)) {
     Sentry.withScope(scope => {
@@ -229,8 +233,8 @@ export function updateProjects(
     return;
   }
 
-  GlobalSelectionActions.updateProjects(projects);
-  updateParams({project: projects}, router, options);
+  GlobalSelectionActions.updateProjects(projects, options?.environments);
+  updateParams({project: projects, environment: options?.environments}, router, options);
 }
 
 function isProjectsValid(projects: ProjectId[]) {

src/sentry/static/sentry/app/components/organizations/globalSelectionHeader/globalSelectionHeader.tsx
@@ -284,8 +284,10 @@ class GlobalSelectionHeader extends React.Component<Props, State> {
     const {projects} = this.state;
 
     // Clear environments when switching projects
-    updateEnvironments([], this.getRouter(), this.getUpdateOptions());
-    updateProjects(projects || [], this.getRouter(), this.getUpdateOptions());
+    updateProjects(projects || [], this.getRouter(), {
+      ...this.getUpdateOptions(),
+      environments: [],
+    });
     this.setState({projects: null, environments: null});
     callIfFunction(this.props.onUpdateProjects, projects);
   };

src/sentry/static/sentry/app/components/organizations/globalSelectionHeader/initializeGlobalSelectionHeader.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import * as ReactRouter from 'react-router';
+import isEqual from 'lodash/isEqual';
+import pick from 'lodash/pick';
 
-import {initializeUrlState} from 'app/actionCreators/globalSelection';
+import {DATE_TIME_KEYS} from 'app/constants/globalSelectionHeader';
+import {
+  initializeUrlState,
+  updateProjects,
+  updateEnvironments,
+  updateDateTime,
+} from 'app/actionCreators/globalSelection';
 
+import {getStateFromQuery} from './utils';
 import GlobalSelectionHeader from './globalSelectionHeader';
 
 type Props = {
@@ -50,7 +59,6 @@ class InitializeGlobalSelectionHeader extends React.Component<Props> {
       showAbsolute,
     } = this.props;
 
-    //
     initializeUrlState({
       organization,
       queryParams: location.query,
@@ -63,6 +71,41 @@ class InitializeGlobalSelectionHeader extends React.Component<Props> {
       shouldEnforceSingleProject,
       showAbsolute,
     });
+  }
+
+  componentDidUpdate(prevProps: Props) {
+    /**
+     * This happens e.g. using browser's navigation button, in which case
+     * we need to update our store to reflect URL changes
+     */
+    if (prevProps.location.query !== this.props.location.query) {
+      const oldQuery = getStateFromQuery(prevProps.location.query, {
+        allowEmptyPeriod: true,
+      });
+      const newQuery = getStateFromQuery(this.props.location.query, {
+        allowEmptyPeriod: true,
+      });
+
+      const newEnvironments = newQuery.environment || [];
+      const newDateObject = pick(newQuery, DATE_TIME_KEYS);
+
+      /**
+       * Do not pass router to these actionCreators, as we do not want to update
+       * routes since these state changes are happening due to a change of routes
+       */
+      if (!isEqual(oldQuery.project, newQuery.project)) {
+        updateProjects(newQuery.project || [], null, {environments: newEnvironments});
+      }
+      if (!isEqual(oldQuery.environment, newQuery.project)) {
+        // Projects changing will also change environments, so only update environments
+        // by itself if projects is unchanged
+        updateEnvironments(newEnvironments);
+      }
+
+      if (!isEqual(pick(oldQuery, DATE_TIME_KEYS), newDateObject)) {
+        updateDateTime(newDateObject);
+      }
+    }
   }
 
   render() {

src/sentry/static/sentry/app/stores/globalSelectionStore.jsx
@@ -37,7 +37,6 @@ const GlobalSelectionStore = Reflux.createStore({
 
   /**
    * Initializes the global selection store data
-   * Use query params if they exist, otherwise check local storage
    */
   onInitializeUrlState(newSelection) {
     this._hasInitialState = true;
@@ -57,7 +56,7 @@ const GlobalSelectionStore = Reflux.createStore({
     this.trigger(this.get());
   },
 
-  updateProjects(projects = []) {
+  updateProjects(projects = [], environments = null) {
     if (isEqual(this.selection.projects, projects)) {
       return;
     }
@@ -65,6 +64,7 @@ const GlobalSelectionStore = Reflux.createStore({
     this.selection = {
       ...this.selection,
       projects,
+      environments: environments === null ? this.selection.environments : environments,
     };
     this.trigger(this.get());
   },

tests/acceptance/test_organization_global_selection_header.py
@@ -123,6 +123,34 @@ class OrganizationGlobalHeaderTest(AcceptanceTestCase, SnubaTestCase):
         self.issues_list.wait_until_loaded()
         assert u"project={}".format(self.project_3.id) in self.browser.current_url
 
+    def test_global_selection_header_navigates_with_browser_back_button(self):
+        """
+        Global Selection Header should:
+        1) load project from URL if it exists
+        2) enforce a single project if loading issues list with no project in URL
+           a) last selected project via local storage if it exists
+           b) otherwise need to just select first project
+        """
+        self.create_issues()
+        # Issues list with project 1 selected
+        self.issues_list.visit_issue_list(
+            self.org.slug, query="?project=" + six.text_type(self.project_1.id)
+        )
+        self.issues_list.visit_issue_list(self.org.slug)
+        assert self.issues_list.global_selection.get_selected_project_slug() == self.project_1.slug
+
+        # selects a different project
+        self.issues_list.global_selection.select_project_by_slug(self.project_3.slug)
+        self.issues_list.wait_until_loaded()
+        assert u"project={}".format(self.project_3.id) in self.browser.current_url
+        assert self.issues_list.global_selection.get_selected_project_slug() == self.project_3.slug
+
+        # simulate pressing the browser back button
+        self.browser.back()
+        self.issues_list.wait_until_loaded()
+        assert u"project={}".format(self.project_1.id) in self.browser.current_url
+        assert self.issues_list.global_selection.get_selected_project_slug() == self.project_1.slug
+
     def test_global_selection_header_loads_with_correct_project_with_multi_project(self):
         """
         Global Selection Header should:

tests/js/spec/actionCreators/globalSelection.spec.jsx
@@ -98,7 +98,10 @@ describe('GlobalSelection ActionCreators', function() {
   describe('updateProjects()', function() {
     it('updates', function() {
       updateProjects([1, 2]);
-      expect(GlobalSelectionActions.updateProjects).toHaveBeenCalledWith([1, 2]);
+      expect(GlobalSelectionActions.updateProjects).toHaveBeenCalledWith(
+        [1, 2],
+        undefined
+      );
     });
 
     it('does not update invalid projects', function() {

tests/js/spec/views/events/index.spec.jsx
@@ -174,9 +174,10 @@ describe('EventsContainer', function() {
       await tick();
       wrapper.update();
 
-      expect(router.push).toHaveBeenCalledWith({
+      expect(router.push).toHaveBeenLastCalledWith({
         pathname: '/organizations/org-slug/events/',
         query: {
+          environment: [],
           project: [2],
         },
       });
@@ -204,9 +205,10 @@ describe('EventsContainer', function() {
 
       wrapper.find('MultipleProjectSelector StyledChevron').simulate('click');
 
-      expect(router.push).toHaveBeenCalledWith({
+      expect(router.push).toHaveBeenLastCalledWith({
         pathname: '/organizations/org-slug/events/',
         query: {
+          environment: [],
           project: [2, 3],
         },
       });