d992ae2941cab3481187a1d086e583a8baa13ef7
Matej Minar <matej.minar@sentry.io>
2020-05-14 14:43:24 +0200

    fix(ui): Replace first-release with firstRelease

--- src/sentry/static/sentry/app/views/releasesV2/utils/index.tsx
+++ src/sentry/static/sentry/app/views/releasesV2/utils/index.tsx
@@ -113,7 +113,7 @@ export const getReleaseNewIssuesUrl = (
       project: projectId,
       query: stringifyQueryObject({
         query: [],
-        'first-release': [version],
+        firstRelease: [version],
       }),
     },
   };
