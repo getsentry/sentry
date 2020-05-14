50af67904a3898b23785411e27cf6bc4b1d8c4f8
Priscila Oliveira <priscilawebdev@gmail.com>
2020-05-14 23:56:29 +0200

    ref(ui): Added new breadcrumbs icons and colors (#18814)

---
+++ src/sentry/static/sentry/app/components/events/interfaces/breadcrumbsV2/breadcrumbCollapsed.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import {tct} from 'app/locale';
+import {IconEllipsis} from 'app/icons';
+import space from 'app/styles/space';
+
+import {GridCellLeft, IconWrapper} from './styles';
+
+type Props = {
+  onClick: () => void;
+  quantity: number;
+};
+
+const BreadcrumbCollapsed = ({quantity, onClick}: Props) => (
+  <Wrapper data-test-id="breadcrumb-collapsed" onClick={onClick}>
+    <IconWrapper>
+      <IconEllipsis />
+    </IconWrapper>
+    {tct('Show [quantity] collapsed crumbs', {quantity})}
+  </Wrapper>
+);
+
+export default BreadcrumbCollapsed;
+
+const Wrapper = styled(GridCellLeft)`
+  border-right: 1px solid ${p => p.theme.borderDark};
+  cursor: pointer;
+  background: ${p => p.theme.whiteDark};
+  font-size: ${p => p.theme.fontSizeMedium};
+  grid-column-start: 1;
+  grid-column-end: -1;
+  display: grid;
+  grid-gap: ${space(1.5)};
+  grid-template-columns: max-content 1fr;
+`;
