4db2908625bf9d00f6337810bab9ef7cc7461262
Matej Minar <matej.minar@sentry.io>
2020-05-20 11:13:13 +0200

    feat(ui): Collapse similar frames in event grouping info (#18899)
    This PR adds ability to collapse similar frames in event grouping info.
    
    Grouping info feature is behind grouping-info feature flag.

---
+++ src/sentry/static/sentry/app/components/events/groupingInfo/groupingComponentStacktrace.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import {EventGroupComponent} from 'app/types';
+
+import GroupingComponent, {GroupingComponentListItem} from './groupingComponent';
+import {groupingComponentFilter} from './utils';
+import GroupingComponentFrames from './groupingComponentFrames';
+
+type FrameGroup = {
+  key: string;
+  data: EventGroupComponent[];
+};
+
+type Props = {
+  component: EventGroupComponent;
+  showNonContributing: boolean;
+};
+
+const GroupingComponentStacktrace = ({component, showNonContributing}: Props) => {
+  const getFrameGroups = () => {
+    const frameGroups: FrameGroup[] = [];
+
+    (component.values as EventGroupComponent[])
+      .filter(value => groupingComponentFilter(value, showNonContributing))
+      .forEach(value => {
+        const key = (value.values as EventGroupComponent[])
+          .filter(v => groupingComponentFilter(v, showNonContributing))
+          .map(v => v.id)
+          .sort((a, b) => a.localeCompare(b))
+          .join('');
+
+        const lastGroup = frameGroups[frameGroups.length - 1];
+
+        if (lastGroup?.key === key) {
+          lastGroup.data.push(value);
+        } else {
+          frameGroups.push({key, data: [value]});
+        }
+      });
+
+    return frameGroups;
+  };
+
+  return (
+    <React.Fragment>
+      {getFrameGroups().map((group, index) => (
+        <GroupingComponentFrames
+          key={index}
+          items={group.data.map((v, idx) => (
+            <GroupingComponentListItem key={idx}>
+              <GroupingComponent
+                component={v}
+                showNonContributing={showNonContributing}
+              />
+            </GroupingComponentListItem>
+          ))}
+        />
+      ))}
+    </React.Fragment>
+  );
+};
+
+export default GroupingComponentStacktrace;
