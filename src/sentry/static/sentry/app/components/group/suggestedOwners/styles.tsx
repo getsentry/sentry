5e638527b6ad02aaca5d6d4a240cceff37c0bc57
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-05-20 13:08:16 +0200

    updated margin-bottom

--- src/sentry/static/sentry/app/components/group/suggestedOwners/styles.tsx
+++ src/sentry/static/sentry/app/components/group/suggestedOwners/styles.tsx
@@ -11,7 +11,7 @@ const Header = styled('div')`
   display: grid;
   grid-template-columns: max-content max-content;
   grid-gap: ${space(0.5)};
-  margin-bottom: ${space(1)};
+  margin-bottom: ${space(2)};
 `;
 
 const Wrapper = styled('div')`
