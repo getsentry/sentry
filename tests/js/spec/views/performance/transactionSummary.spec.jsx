cbafb547e6438b451e463974f40a2d7599639cea
Mark Story <mark@sentry.io>
2020-05-22 16:49:10 -0400

    Fix lint error.

--- tests/js/spec/views/performance/transactionSummary.spec.jsx
+++ tests/js/spec/views/performance/transactionSummary.spec.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 
 import {initializeOrg} from 'sentry-test/initializeOrg';
 import {mountWithTheme} from 'sentry-test/enzyme';
+
 import ProjectsStore from 'app/stores/projectsStore';
 import TransactionSummary from 'app/views/performance/transactionSummary';
 
