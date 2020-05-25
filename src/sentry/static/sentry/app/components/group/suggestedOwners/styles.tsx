cd0bdd57bd6a0f2c337f3d34239fe4af5fc710a1
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-05-25 10:21:29 +0200

    fix(ui): fixed Suggested assgnees alignment

--- src/sentry/static/sentry/app/components/group/suggestedOwners/styles.tsx
+++ src/sentry/static/sentry/app/components/group/suggestedOwners/styles.tsx
@@ -9,6 +9,7 @@ const Heading = styled('h6')`
 
 const Header = styled('div')`
   display: grid;
+  align-items: center;
   grid-template-columns: max-content max-content;
   grid-gap: ${space(0.5)};
   margin-bottom: ${space(2)};
