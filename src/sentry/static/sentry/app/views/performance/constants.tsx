e93df27849b4a16b31017a9a02eb82c473dc1a1b
evanh <evanh@users.noreply.github.com>
2020-05-26 15:52:56 -0400

    feat(performance) Convert EPM to TPM (#19007)
    It's possible that users will be confused by "events per" in performance, since
    the numbers will never include other event types. So instead change the naming
    to TPM or "transactions per" to make it crystal clear what is being shown.

--- src/sentry/static/sentry/app/views/performance/constants.tsx
+++ src/sentry/static/sentry/app/views/performance/constants.tsx
@@ -4,7 +4,7 @@ export const PERFORMANCE_TERMS: Record<string, string> = {
   apdex: t(
     'Apdex is the ratio of both satisfactory and tolerable response times to all response times.'
   ),
-  epm: t('Throughput is the number of recorded events per minute (epm).'),
+  tpm: t('TPM is the number of recorded transaction events per minute.'),
   errorRate: t(
     'Error rate is the percentage of recorded transactions that had a known and unsuccessful status.'
   ),
