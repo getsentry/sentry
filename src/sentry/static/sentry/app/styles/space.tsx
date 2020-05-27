27fc9d9be3e606babeb0f3a3d07584b2ac03c182
Scott Cooper <scttcper@gmail.com>
2020-05-26 19:08:25 -0700

    ref(ui): Add generic to space function (#19033)

--- src/sentry/static/sentry/app/styles/space.tsx
+++ src/sentry/static/sentry/app/styles/space.tsx
@@ -12,6 +12,8 @@ const SPACES = {
 
 export type ValidSize = keyof typeof SPACES;
 
-const space = (size: ValidSize): typeof SPACES[ValidSize] => SPACES[size];
+function space<S extends ValidSize>(size: S): typeof SPACES[S] {
+  return SPACES[size];
+}
 
 export default space;
