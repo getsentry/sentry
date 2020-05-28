b2cfc33eec7c959022392db8391026946d017128
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-05-28 17:31:59 +0200

    wip

--- src/sentry/static/sentry/app/components/events/interfaces/breadcrumbsV2/breadcrumb.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/breadcrumbsV2/breadcrumb.tsx
@@ -12,7 +12,7 @@ import Level from './level';
 import {GridCell, GridCellLeft} from './styles';
 import {BreadcrumbsWithDetails, BreadcrumbType} from './types';
 
-const MAX_BREADCRUMB_QUANTITY = 10;
+const MAX_BREADCRUMB_QUANTITY = 9;
 
 type Props = {
   breadcrumb: BreadcrumbsWithDetails[0];
@@ -29,16 +29,15 @@ type State = {
 class Breadcrumb extends React.Component<Props, State> {
   state: State = {
     breadcrumbs: this.props.breadcrumb?.breadcrumbs || [],
-    showCollapsedQuantity: this.props.breadcrumb?.breadcrumbs?.length || 0,
+    showCollapsedQuantity: 0,
   };
 
   handleClick = () => {
-    console.log('here');
     this.setState({
       showCollapsedQuantity:
         this.state.showCollapsedQuantity >= this.state.breadcrumbs.length
           ? this.state.showCollapsedQuantity
-          : this.state.showCollapsedQuantity + 1,
+          : this.state.showCollapsedQuantity + MAX_BREADCRUMB_QUANTITY,
     });
   };
 
@@ -48,10 +47,8 @@ class Breadcrumb extends React.Component<Props, State> {
     const crumbProps = {
       ...rest,
       hasError: breadcrumb.type === BreadcrumbType.ERROR,
-      // onClick: breadcrumb?.breadcrumbs ? this.handleClick : undefined,
+      onClick: breadcrumb?.breadcrumbs ? this.handleClick : undefined,
     };
-
-    console.log('props', breadcrumb, 'state', this.state.breadcrumbs);
 
     return (
       <React.Fragment>
@@ -70,11 +67,11 @@ class Breadcrumb extends React.Component<Props, State> {
         <GridCell {...crumbProps}>
           <Time timestamp={breadcrumb.timestamp} />
         </GridCell>
-        {/* {this.state.breadcrumbs
+        {this.state.breadcrumbs
           .slice(0, this.state.showCollapsedQuantity)
           .map((crumb, idx) => (
             <Breadcrumb key={idx} breadcrumb={crumb} event={event} orgId={orgId} />
-          ))} */}
+          ))}
       </React.Fragment>
     );
   }
