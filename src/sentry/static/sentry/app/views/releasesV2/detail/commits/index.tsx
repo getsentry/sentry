f0bcf33bb9852e71032f5e16e1e657c766887b80
Matej Minar <matej.minar@sentry.io>
2020-05-29 12:59:59 +0200

    Revert "feat(ui): Limit release files and commits (#19066)" (#19080)
    This reverts commit 8496b374cbe49db9d26697e26396acdcc85e7fbc.

--- src/sentry/static/sentry/app/views/releasesV2/detail/commits/index.tsx
+++ src/sentry/static/sentry/app/views/releasesV2/detail/commits/index.tsx
@@ -15,7 +15,6 @@ import AsyncView from 'app/views/asyncView';
 import routeTitleGen from 'app/utils/routeTitle';
 import {formatVersion} from 'app/utils/formatters';
 import withOrganization from 'app/utils/withOrganization';
-import Pagination from 'app/components/pagination';
 
 import {getCommitsByRepository, CommitsByRepository} from '../utils';
 import ReleaseNoCommitData from '../releaseNoCommitData';
@@ -122,7 +121,7 @@ class ReleaseCommits extends AsyncView<Props, State> {
 
   renderBody() {
     const {orgId} = this.props.params;
-    const {commits, commitsPageLinks, repos, activeRepo} = this.state;
+    const {commits, repos, activeRepo} = this.state;
 
     const commitsByRepository = getCommitsByRepository(commits);
     const reposToRender =
@@ -153,8 +152,6 @@ class ReleaseCommits extends AsyncView<Props, State> {
         {reposToRender.map(repoName =>
           this.renderCommitsForRepo(repoName, commitsByRepository)
         )}
-
-        <Pagination pageLinks={commitsPageLinks} />
       </React.Fragment>
     );
   }
