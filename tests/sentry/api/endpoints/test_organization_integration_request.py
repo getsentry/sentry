bc70346c1e040553c1746f22be8ba9246329b702
Marcos Gaeta <marcos.gaeta@sentry.io>
2020-05-29 13:35:53 -0700

    Add endpoint test

---
+++ tests/sentry/api/endpoints/test_organization_integration_request.py
@@ -0,0 +1,58 @@
+from __future__ import absolute_import
+
+from sentry.testutils import APITestCase
+
+
+class OrganizationIntegrationRequestTest(APITestCase):
+    """Unit tests for emailing organization owners asking them to install an integration."""
+    endpoint = "sentry-api-0-organization-integration-request"
+    method = "post"
+
+    def setUp(self):
+        self.owner = self.create_user(email="owner@example.com", is_superuser=True)
+        self.member = self.create_user(email="member@example.com")
+        self.org = self.create_organization(owner=self.owner, name="My Org")
+        self.create_member(user=self.member, organization=self.org, role="member")
+
+    def test_integration_request(self):
+        self.login_as(user=self.member)
+        response = self.get_response(
+            self.org.slug,
+            provider_slug="github",
+            provider_type="plugin",
+        )
+
+        assert response.status_code == 201, response.content
+
+    def test_integration_request_without_provider(self):
+        self.login_as(user=self.member)
+        response = self.get_response(
+            self.org.slug,
+            provider_slug="ERROR",
+            provider_type="plugin",
+        )
+
+        assert response.status_code == 400, response.content
+
+    def test_integration_request_as_owner(self):
+        """
+        This is a little counter-intuitive. If the user has permissions to add
+        integrations, then they should NOT be able to hit this endpoint.
+        """
+        self.login_as(user=self.owner)
+        response = self.get_response(
+            self.org.slug,
+            provider_slug="github",
+            provider_type="plugin",
+        )
+        assert response.status_code == 200, response.content
+        assert response.data["detail"] == "User can install integration"
+
+    def test_integration_request_without_permissions(self):
+        self.login_as(user=self.create_user(email="nonmember@example.com"))
+        response = self.get_response(
+            self.org.slug,
+            provider_slug="github",
+            provider_type="plugin",
+        )
+        assert response.status_code == 403, response.content
