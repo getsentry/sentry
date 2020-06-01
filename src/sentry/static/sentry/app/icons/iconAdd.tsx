a6b1cee50438464dd0ac429dd1acbc7e80dddc26
Dora <dora.l.chan@gmail.com>
2020-06-01 15:40:22 -0700

    fix(icon-sizing): matching weights of other icons (#19118)
    Co-authored-by: Dora Chan <dora.lchan@gmail.com>

--- src/sentry/static/sentry/app/icons/iconAdd.tsx
+++ src/sentry/static/sentry/app/icons/iconAdd.tsx
@@ -17,8 +17,7 @@ const IconAdd = React.forwardRef<SVGSVGElement, Props>(
         </React.Fragment>
       ) : (
         <React.Fragment>
-          <path d="M15.19,8.75H.81a.75.75,0,1,1,0-1.5H15.19a.75.75,0,0,1,0,1.5Z" />
-          <path d="M8,15.94a.76.76,0,0,1-.75-.75V.81a.75.75,0,0,1,1.5,0V15.19A.76.76,0,0,1,8,15.94Z" />
+          <path d="M8.75,7.25V2a.75.75,0,0,0-1.5,0V7.25H2a.75.75,0,0,0,0,1.5H7.25V14a.75.75,0,0,0,1.5,0V8.75H14a.75.75,0,0,0,0-1.5Z" />
         </React.Fragment>
       )}
     </SvgIcon>
