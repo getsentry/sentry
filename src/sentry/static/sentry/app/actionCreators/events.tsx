3a9e8cfa3433aa7d92936b4b5c87b607ab173c31
Mark Story <mark@sentry.io>
2020-06-02 12:42:24 -0400

    ref(ts) Convert EventsChart to typescript
    There are still some related components that have not been converted. We
    also have a jumble of types for falsey values with null | undefined
    being used all over the place. I'd like to make another pass and remove
    the null values.

--- src/sentry/static/sentry/app/actionCreators/events.tsx
+++ src/sentry/static/sentry/app/actionCreators/events.tsx
@@ -5,7 +5,12 @@ import {Client} from 'app/api';
 import {URL_PARAM} from 'app/constants/globalSelectionHeader';
 import {canIncludePreviousPeriod} from 'app/components/charts/utils';
 import {getPeriod} from 'app/utils/getPeriod';
-import {EventsStats, OrganizationSummary, MultiSeriesEventsStats} from 'app/types';
+import {
+  EventsStats,
+  DateString,
+  OrganizationSummary,
+  MultiSeriesEventsStats,
+} from 'app/types';
 
 function getBaseUrl(org: OrganizationSummary, keyTransactions: boolean | undefined) {
   if (keyTransactions) {
@@ -20,8 +25,8 @@ type Options = {
   project?: number[];
   environment?: string[];
   period?: string;
-  start?: Date;
-  end?: Date;
+  start?: DateString;
+  end?: DateString;
   interval?: string;
   includePrevious?: boolean;
   limit?: number;
