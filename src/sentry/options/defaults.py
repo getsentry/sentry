8929daa824c17f09ff280b8380bc117c5684a5fd
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-06-04 15:16:47 -0700

    feat(alerts): Register incidents-performance rollout option (#19197)

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -225,6 +225,10 @@ register("discover2.rollout-rate", default=0, flags=FLAG_PRIORITIZE_DISK)
 # Alerts / Workflow incremental rollout rate. Tied to feature handlers in getsentry
 register("workflow.rollout-rate", default=0, flags=FLAG_PRIORITIZE_DISK)
 
+# Performance metric alerts incremental rollout rate. Tied to feature handlers
+# in getsentry
+register("incidents-performance.rollout-rate", default=0, flags=FLAG_PRIORITIZE_DISK)
+
 # Max number of tags to combine in a single query in Discover2 tags facet.
 register("discover2.max_tags_to_combine", default=3, flags=FLAG_PRIORITIZE_DISK)
 
