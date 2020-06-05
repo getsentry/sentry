ca468f80c50167522e4674e5e7e997de0ac0db30
Mark Story <mark@sentry.io>
2020-06-05 14:38:47 -0400

    fix(discover) Fix negated issue conditions (#19175)
    The issue condition should support negation like other conditions. I've
    changed the semantics a bit on how the issue filter works as it no
    longer goes into the `group_ids` filter key, but from what I saw snuba
    can still promote the new condition into a prewhere.
    
    We still need isNull to prevent the prewhere promotion which excludes
    all the results we're interested in.
    
    Co-authored-by: evanh <evanh@users.noreply.github.com>

--- src/sentry/api/event_search.py
+++ src/sentry/api/event_search.py
@@ -760,6 +760,19 @@ def convert_search_filter_to_snuba_query(search_filter, key=None):
                 )
             )
         return [name, search_filter.operator, internal_value]
+    elif name == "issue.id":
+        # Handle "has" queries
+        if search_filter.value.raw_value == "":
+            if search_filter.operator == "=":
+                # Use isNull to get events with no issue (transactions)
+                return [["isNull", [name]], search_filter.operator, 1]
+            else:
+                # Compare to 0 as group_id is not nullable on issues.
+                return [name, search_filter.operator, 0]
+
+        # Skip isNull check on group_id value as we want to
+        # allow snuba's prewhere optimizer to find this condition.
+        return [name, search_filter.operator, value]
     else:
         value = (
             int(to_timestamp(value)) * 1000
@@ -862,17 +875,21 @@ def get_filter(query=None, params=None):
             elif name == ISSUE_ID_ALIAS and value != "":
                 # A blank term value means that this is a has filter
                 kwargs["group_ids"].extend(to_list(value))
-            elif name == ISSUE_ALIAS and value != "":
-                if params and "organization_id" in params:
+            elif name == ISSUE_ALIAS:
+                if value != "" and params and "organization_id" in params:
                     try:
                         group = Group.objects.by_qualified_short_id(
                             params["organization_id"], value
                         )
-                        kwargs["group_ids"].extend(to_list(group.id))
                     except Exception:
                         raise InvalidSearchQuery(
                             u"Invalid value '{}' for 'issue:' filter".format(value)
                         )
+                    else:
+                        value = group.id
+                term = SearchFilter(SearchKey("issue.id"), term.operator, SearchValue(value))
+                converted_filter = convert_search_filter_to_snuba_query(term)
+                kwargs["conditions"].append(converted_filter)
             elif name == USER_ALIAS:
                 # If the key is user, do an OR across all the different possible user fields
                 user_conditions = [
