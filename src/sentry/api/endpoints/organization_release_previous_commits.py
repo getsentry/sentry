d99c83c3b77c986550a35f3a326618005534b7ff
meredith <meredith@sentry.io>
2020-06-10 14:37:08 -0700

    get most recent release with commits

--- src/sentry/api/endpoints/organization_release_previous_commits.py
+++ src/sentry/api/endpoints/organization_release_previous_commits.py
@@ -6,21 +6,17 @@ from sentry.api.bases.organization import OrganizationReleasesBaseEndpoint
 from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.serializers import serialize
 from sentry.models import Release
-from sentry.utils.commiters import get_previous_releases
 
 
 class OrganizationReleasePreviousCommitsEndpoint(OrganizationReleasesBaseEndpoint):
     def get(self, request, organization, version):
         """
-        Retrieve an Organization's Previous Release that has commits
+        Retrieve an Organization's Most Recent Release with Commits
         ````````````````````````````````````````````````````````````
-
-        Return details on an individual release.
 
         :pparam string organization_slug: the slug of the organization the
                                           release belongs to.
         :pparam string version: the version identifier of the release.
-        :qparam array[string] project:    An optional list of project ids to filter
         :auth: required
         """
 
@@ -32,15 +28,23 @@ class OrganizationReleasePreviousCommitsEndpoint(OrganizationReleasesBaseEndpoin
         if not self.has_release_permission(request, organization, release):
             raise ResourceDoesNotExist
 
-        # project_ids = set(map(int, request.GET.getlist("project")))
-        prev_release_with_commit = None
-        prev_releases = []
-        for project in release.projects:
-            prev_releases = get_previous_releases(project, version)
+        start_date = release.date_released or release.date_added
 
-        for prev_release in prev_releases:
-            if prev_release.last_commit_id:
-                prev_release_with_commit = prev_release
-                break
+        prev_release_with_commits = (
+            Release.objects.filter(
+                organization_id=organization.id,
+                projects__in=release.projects.all(),
+                last_commit_id__isnull=False,
+            )
+            .extra(
+                select={"date": "COALESCE(date_released, date_added)"},
+                where=["COALESCE(date_released, date_added) <= %s"],
+                params=[start_date],
+            )
+            .extra(order_by=["-date"])[:1]
+        )
 
-        return Response(serialize(prev_release_with_commit, request.user,))
+        if not prev_release_with_commits:
+            return Response({})
+
+        return Response(serialize(prev_release_with_commits[0], request.user,))
