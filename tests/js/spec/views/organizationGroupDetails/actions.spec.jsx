018f96dcaf8877856ff95653b7704e540b96fd77
Scott Cooper <scttcper@gmail.com>
2020-06-11 16:49:33 -0700

    add test for bookmark button

--- tests/js/spec/views/organizationGroupDetails/actions.spec.jsx
+++ tests/js/spec/views/organizationGroupDetails/actions.spec.jsx
@@ -76,4 +76,45 @@ describe('GroupActions', function() {
       );
     });
   });
+
+  describe('bookmarking', function() {
+    let issuesApi;
+    beforeEach(function() {
+      issuesApi = MockApiClient.addMockResponse({
+        url: '/projects/org/project/issues/',
+        method: 'PUT',
+        body: TestStubs.Group({isBookmarked: false}),
+      });
+    });
+
+    it('can bookmark', function() {
+      const wrapper = mount(
+        <GroupActions
+          group={TestStubs.Group({
+            id: '1337',
+            pluginActions: [],
+            pluginIssues: [],
+          })}
+          project={TestStubs.ProjectDetails({
+            id: '2448',
+            name: 'project name',
+            slug: 'project',
+          })}
+          organization={TestStubs.Organization({
+            id: '4660',
+            slug: 'org',
+          })}
+        />
+      );
+      const btn = wrapper.find('.group-bookmark');
+      btn.simulate('click');
+
+      expect(issuesApi).toHaveBeenCalledWith(
+        expect.anything(),
+        expect.objectContaining({
+          data: {isBookmarked: true},
+        })
+      );
+    });
+  });
 });
