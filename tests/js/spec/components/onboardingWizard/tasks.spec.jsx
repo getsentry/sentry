d48de274d5e6558f47c80442a506908d30a8dc16
Sarah Lohmeier <sarah.lohmeier@sentry.io>
2020-06-11 11:57:54 -0700

    Fixup: move mock cleanup into afterEach

--- tests/js/spec/components/onboardingWizard/tasks.spec.jsx
+++ tests/js/spec/components/onboardingWizard/tasks.spec.jsx
@@ -63,82 +63,85 @@ describe('Task', () => {
       expect(wrapper.find('[data-test-id="send-event-prompt"]').exists()).toBe(true);
     });
 
-    it('links to create new project page before task is started', () => {
-      const tasks = getMergedTasks(org);
-      const second_platform_task = tasks.find(
-        t => t.task === OnboardingTaskKey.SECOND_PLATFORM
-      );
-      const wrapper = mountWithTheme(
-        <Task organization={org} task={second_platform_task} />,
-        TestStubs.routerContext()
-      );
-      wrapper
-        .find('[data-test-id="setup_second_platform"]')
-        .first()
-        .simulate('click');
+    describe('with navigateTo mocks', () => {
+      afterEach(() => {
+        navigateTo.mockClear();
+      });
 
-      expect(navigateTo).toHaveBeenCalledWith(
-        `${second_platform_task.location}?onboardingTask`,
-        expect.anything()
-      );
-      navigateTo.mockClear();
-    });
+      it('links to create new project page before task is started', () => {
+        const tasks = getMergedTasks(org);
+        const second_platform_task = tasks.find(
+          t => t.task === OnboardingTaskKey.SECOND_PLATFORM
+        );
+        const wrapper = mountWithTheme(
+          <Task organization={org} task={second_platform_task} />,
+          TestStubs.routerContext()
+        );
+        wrapper
+          .find('[data-test-id="setup_second_platform"]')
+          .first()
+          .simulate('click');
 
-    it('links to project configuration page after project is created', () => {
-      org.onboardingTasks = [
-        {
-          task: OnboardingTaskKey.SECOND_PLATFORM,
-          status: 'pending',
-          project: 1,
-        },
-      ];
-      const tasks = getMergedTasks(org);
-      const second_platform_task = tasks.find(
-        t => t.task === OnboardingTaskKey.SECOND_PLATFORM
-      );
+        expect(navigateTo).toHaveBeenCalledWith(
+          `${second_platform_task.location}?onboardingTask`,
+          expect.anything()
+        );
+      });
 
-      const wrapper = mountWithTheme(
-        <Task organization={org} task={second_platform_task} />,
-        TestStubs.routerContext()
-      );
-      wrapper
-        .find('[data-test-id="setup_second_platform"]')
-        .first()
-        .simulate('click');
+      it('links to project configuration page after project is created', () => {
+        org.onboardingTasks = [
+          {
+            task: OnboardingTaskKey.SECOND_PLATFORM,
+            status: 'pending',
+            project: 1,
+          },
+        ];
+        const tasks = getMergedTasks(org);
+        const second_platform_task = tasks.find(
+          t => t.task === OnboardingTaskKey.SECOND_PLATFORM
+        );
 
-      expect(navigateTo).toHaveBeenCalledWith(
-        `/settings/${org.slug}/projects/${project.slug}/install/?onboardingTask`,
-        expect.anything()
-      );
-      navigateTo.mockClear();
-    });
+        const wrapper = mountWithTheme(
+          <Task organization={org} task={second_platform_task} />,
+          TestStubs.routerContext()
+        );
+        wrapper
+          .find('[data-test-id="setup_second_platform"]')
+          .first()
+          .simulate('click');
 
-    it('passes :projectId to router when project id cannot be resolved', () => {
-      org.onboardingTasks = [
-        {
-          task: OnboardingTaskKey.SECOND_PLATFORM,
-          status: 'pending',
-        },
-      ];
-      const tasks = getMergedTasks(org);
-      const second_platform_task = tasks.find(
-        t => t.task === OnboardingTaskKey.SECOND_PLATFORM
-      );
+        expect(navigateTo).toHaveBeenCalledWith(
+          `/settings/${org.slug}/projects/${project.slug}/install/?onboardingTask`,
+          expect.anything()
+        );
+      });
 
-      const wrapper = mountWithTheme(
-        <Task organization={org} task={second_platform_task} />,
-        TestStubs.routerContext()
-      );
-      wrapper
-        .find('[data-test-id="setup_second_platform"]')
-        .first()
-        .simulate('click');
+      it('passes :projectId to router when project id cannot be resolved', () => {
+        org.onboardingTasks = [
+          {
+            task: OnboardingTaskKey.SECOND_PLATFORM,
+            status: 'pending',
+          },
+        ];
+        const tasks = getMergedTasks(org);
+        const second_platform_task = tasks.find(
+          t => t.task === OnboardingTaskKey.SECOND_PLATFORM
+        );
 
-      expect(navigateTo).toHaveBeenCalledWith(
-        `/settings/${org.slug}/projects/:projectId/install/?onboardingTask`,
-        expect.anything()
-      );
-      navigateTo.mockClear();
+        const wrapper = mountWithTheme(
+          <Task organization={org} task={second_platform_task} />,
+          TestStubs.routerContext()
+        );
+        wrapper
+          .find('[data-test-id="setup_second_platform"]')
+          .first()
+          .simulate('click');
+
+        expect(navigateTo).toHaveBeenCalledWith(
+          `/settings/${org.slug}/projects/:projectId/install/?onboardingTask`,
+          expect.anything()
+        );
+      });
     });
   });
 });
