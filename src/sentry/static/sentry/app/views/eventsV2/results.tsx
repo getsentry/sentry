b697d8daab8e393a7f4c61c8f44fa35ae1f3e8fa
k-fish <kevan.fisher@sentry.io>
2020-06-16 15:26:25 -0700

    ResultsChart should have its own client instance to avoid api.clear clearing requests from other components

--- src/sentry/static/sentry/app/views/eventsV2/results.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/results.tsx
@@ -253,7 +253,7 @@ class Results extends React.Component<Props, State> {
   };
 
   render() {
-    const {organization, location, router, api} = this.props;
+    const {organization, location, router} = this.props;
     const {eventView, error, errorCode, totalValues, showTags} = this.state;
     const query = location.query.query || '';
     const title = this.getDocumentTitle();
@@ -279,7 +279,6 @@ class Results extends React.Component<Props, State> {
                   onSearch={this.handleSearch}
                 />
                 <ResultsChart
-                  api={api}
                   router={router}
                   organization={organization}
                   eventView={eventView}
