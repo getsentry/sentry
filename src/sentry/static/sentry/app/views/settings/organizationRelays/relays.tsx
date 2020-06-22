e5866f274ada3bde2c03498e556359b533b0b8c7
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-06-22 13:33:48 +0200

    feat(relay): Added relay dialogs

--- src/sentry/static/sentry/app/views/settings/organizationRelays/relays.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationRelays/relays.tsx
@@ -1,30 +1,34 @@
 import React from 'react';
 import {RouteComponentProps} from 'react-router/lib/Router';
 import styled from '@emotion/styled';
+import {ClassNames} from '@emotion/core';
+import omit from 'lodash/omit';
 
-import {Panel, PanelBody, PanelHeader, PanelAlert} from 'app/components/panels';
+import theme from 'app/utils/theme';
+import {openModal} from 'app/actionCreators/modal';
+import {PanelTable} from 'app/components/panels';
 import {t, tct} from 'app/locale';
 import AsyncComponent from 'app/components/asyncComponent';
 import SettingsPageHeader from 'app/views/settings/components/settingsPageHeader';
-import {Organization} from 'app/types';
+import {Organization, Relay} from 'app/types';
 import ExternalLink from 'app/components/links/externalLink';
-import space from 'app/styles/space';
 import Button from 'app/components/button';
-import {IconDelete} from 'app/icons';
+import {addErrorMessage, addSuccessMessage} from 'app/actionCreators/indicator';
+import TextBlock from 'app/views/settings/components/text/textBlock';
+import TextOverflow from 'app/components/textOverflow';
+import Clipboard from 'app/components/clipboard';
+import {IconAdd, IconCopy, IconEdit, IconDelete} from 'app/icons';
+import DateTime from 'app/components/dateTime';
+import space from 'app/styles/space';
+import {defined} from 'app/utils';
+import Tooltip from 'app/components/tooltip';
+import QuestionTooltip from 'app/components/questionTooltip';
+import overflowEllipsis from 'app/styles/overflowEllipsis';
 
-import Time from './time';
+import Add from './modals/add';
+import Edit from './modals/edit';
 
 const RELAY_DOCS_LINK = 'https://getsentry.github.io/relay/';
-
-type Relay = {
-  public_key: string;
-  name: string;
-  created: string;
-  first_seen: string;
-  last_seen: string | null;
-  last_modified?: string;
-  description?: string;
-};
 
 type Props = {
   organization: Organization;
@@ -34,102 +38,190 @@ type State = AsyncComponent['state'] & {
   relays: Array<Relay>;
 };
 
-const relaysMock: Array<Relay> = [
-  {
-    public_key: '1:bb:6e:af:66:b4:38:e0:62:83:62:15:22:7',
-    name: 'First key',
-    description: 'optional description for the key',
-    first_seen: '2020-02-07T15:17:00Z',
-    last_seen: '2020-02-07T15:17:00Z',
-    created: '2020-02-07T15:17:00Z',
-  },
-  {
-    public_key: '2:bb:6e:af:66:b4:38:e0:62:83:62:15:22:7',
-    name: 'Second key',
-    description: 'optional description for the key',
-    first_seen: '2020-02-07T15:17:00Z',
-    last_seen: '2020-02-07T15:17:00Z',
-    last_modified: '2020-02-07T15:17:00Z',
-    created: '2020-02-07T15:17:00Z',
-  },
-];
-
 class Relays extends AsyncComponent<Props, State> {
+  constructor(props: Props, context: AsyncComponent['context']) {
+    super(props, context);
+    this.setRelays = this.setRelays.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
+    this.successfullySaved = this.successfullySaved.bind(this);
+    this.handleOpenEditDialog = this.handleOpenEditDialog.bind(this);
+    this.handleOpenAddDialog = this.handleOpenAddDialog.bind(this);
+  }
+
   getDefaultState() {
     return {
       ...super.getDefaultState(),
-      relays: relaysMock,
+      relays: [],
     };
   }
 
-  // TODO(Priscila): activate the code below as soon as the endpoint is provided
-  // getEndpoints(): ReturnType<AsyncComponent['getEndpoints']> {
-  //   return [['relays', `/organizations/${this.props.organization.slug}/relay-keys`]];
-  // }
+  getEndpoints(): ReturnType<AsyncComponent['getEndpoints']> {
+    return [['data', `/organizations/${this.props.organization.slug}/`]];
+  }
 
-  // handleDelete = (id: Relay['public_key']) => () => {};
+  onRequestSuccess({data}: {data: Organization}) {
+    this.setRelays(data.trustedRelays);
+  }
 
-  handleAdd = () => {};
+  setRelays(trustedRelays: Array<Relay>) {
+    this.setState({
+      relays: trustedRelays,
+    });
+  }
+
+  handleDelete(publicKey: Relay['publicKey']) {
+    return async () => {
+      const {relays} = this.state;
+
+      const trustedRelays = relays
+        .filter(relay => relay.publicKey !== publicKey)
+        .map(relay => omit(relay, ['created', 'lastModified']));
+
+      try {
+        const response = await this.api.requestPromise(
+          `/organizations/${this.props.organization.slug}/`,
+          {
+            method: 'PUT',
+            data: {trustedRelays},
+          }
+        );
+        addSuccessMessage('Successfully deleted relay public key');
+        this.setRelays(response.trustedRelays);
+      } catch {
+        addErrorMessage('An unknown error occurred while deleting relay public key');
+      }
+    };
+  }
+
+  successfullySaved(response: Organization, successMessage: string) {
+    addSuccessMessage(successMessage);
+    this.setRelays(response.trustedRelays);
+  }
+
+  handleOpenEditDialog(publicKey: Relay['publicKey']) {
+    return () => {
+      const editRelay = this.state.relays.find(relay => relay.publicKey === publicKey);
+
+      if (!editRelay) {
+        return;
+      }
+
+      openModal(modalProps => (
+        <Edit
+          {...modalProps}
+          savedRelays={this.state.relays}
+          api={this.api}
+          orgSlug={this.props.organization.slug}
+          relay={editRelay}
+          onSubmitSuccess={response => {
+            this.successfullySaved(response, t('Successfully updated relay public key'));
+          }}
+        />
+      ));
+    };
+  }
+
+  handleOpenAddDialog() {
+    openModal(modalProps => (
+      <Add
+        {...modalProps}
+        savedRelays={this.state.relays}
+        api={this.api}
+        orgSlug={this.props.organization.slug}
+        onSubmitSuccess={response => {
+          this.successfullySaved(response, t('Successfully added relay public key'));
+        }}
+      />
+    ));
+  }
 
   renderBody() {
     const {relays} = this.state;
+
     return (
       <React.Fragment>
-        <SettingsPageHeader title={t('Relays')} />
-        <Panel>
-          <PanelHeader>{t('Relays')}</PanelHeader>
-          <PanelAlert type="info">
-            {tct('For more details, see [linkToDocs].', {
-              linkToDocs: (
-                <ExternalLink href={RELAY_DOCS_LINK}>
-                  {t('full Relay documentation')}
-                </ExternalLink>
-              ),
-            })}
-          </PanelAlert>
-          <PanelBody>
-            {relays.map(
-              ({public_key, name, created, last_seen, first_seen, last_modified}) => (
-                <Content key={public_key}>
-                  <Info>
-                    <InfoItem>
-                      <Name>{name}</Name>
-                    </InfoItem>
-                    <InfoItem>
-                      <PublicKey>{public_key}</PublicKey>
-                    </InfoItem>
-                    <InfoItem>
-                      <Time label={t('Created:')} date={created} />
-                    </InfoItem>
-                    <InfoItem>
-                      <Time label={t('First Seen:')} date={first_seen} />
-                    </InfoItem>
-                    <InfoItem>
-                      <Time label={t('Last Seen:')} date={last_seen} />
-                    </InfoItem>
-                    <InfoItem>
-                      <Time label={t('Last modified:')} date={last_modified} />
-                    </InfoItem>
-                  </Info>
-                  <Button
-                    title={t('Delete Rule')}
-                    label={t('Delete Rule')}
-                    size="small"
-                    icon={<IconDelete />}
-                  />
-                </Content>
-              )
-            )}
-          </PanelBody>
-          <PanelAction>
-            <Button href={RELAY_DOCS_LINK} target="_blank">
-              {t('Read the docs')}
+        <SettingsPageHeader
+          title={t('Relays')}
+          action={
+            <Button
+              priority="primary"
+              size="small"
+              icon={<IconAdd size="xs" isCircled />}
+              onClick={this.handleOpenAddDialog}
+            >
+              {t('New Relay Key')}
             </Button>
-            <Button onClick={this.handleAdd} priority="primary">
-              {t('Add Relay')}
-            </Button>
-          </PanelAction>
-        </Panel>
+          }
+        />
+        <TextBlock>
+          {tct(
+            `Relay is a relay service built by Sentry. You can run this on-premise for your SDKs or server to customize data scrubbing, buffering retries and more. You can generate relay keys for access. For more on how to set this up, read the [link:docs].`,
+            {
+              link: <ExternalLink href={RELAY_DOCS_LINK} />,
+            }
+          )}
+        </TextBlock>
+        <ClassNames>
+          {({css}) => (
+            <PanelTable
+              isEmpty={relays.length === 0}
+              emptyMessage={t('No relays keys have been added yet.')}
+              headers={[t('Display Name'), t('Relay Key'), t('Date Created'), '']}
+              className={css`
+                grid-template-columns: repeat(3, auto) max-content;
+                > * {
+                  padding: ${space(1)};
+                  @media (min-width: ${theme.breakpoints[0]}) {
+                    padding: ${space(1)} ${space(2)};
+                  }
+                }
+              `}
+            >
+              {relays.map(({publicKey: key, name, created, description}) => {
+                const maskedKey = '*************************';
+                return (
+                  <React.Fragment key={key}>
+                    {description ? (
+                      <Name>
+                        <Text>{name}</Text>
+                        <QuestionTooltip position="top" size="sm" title={description} />
+                      </Name>
+                    ) : (
+                      <Text>{name}</Text>
+                    )}
+                    <KeyWrapper>
+                      <Key content={maskedKey}>{maskedKey}</Key>
+                      <IconWrapper>
+                        <Clipboard value={key}>
+                          <Tooltip title={t('Click to copy')} containerDisplayMode="flex">
+                            <IconCopy color="gray500" />
+                          </Tooltip>
+                        </Clipboard>
+                      </IconWrapper>
+                    </KeyWrapper>
+                    <Text>
+                      {!defined(created) ? t('Unknown') : <DateTime date={created} />}
+                    </Text>
+                    <Actions>
+                      <StyledButton
+                        title={t('Edit Key')}
+                        label={t('Edit Key')}
+                        icon={<IconEdit />}
+                        onClick={this.handleOpenEditDialog(key)}
+                      />
+                      <StyledButton
+                        title={t('Delete Key')}
+                        label={t('Delete Key')}
+                        onClick={this.handleDelete(key)}
+                        icon={<IconDelete />}
+                      />
+                    </Actions>
+                  </React.Fragment>
+                );
+              })}
+            </PanelTable>
+          )}
+        </ClassNames>
       </React.Fragment>
     );
   }
@@ -137,51 +229,46 @@ class Relays extends AsyncComponent<Props, State> {
 
 export default Relays;
 
-const Content = styled('div')`
+const KeyWrapper = styled('div')`
   display: grid;
-  grid-template-columns: 1fr max-content;
-  align-items: center;
-  border-bottom: 1px solid ${p => p.theme.borderDark};
-  padding: ${space(1)} ${space(2)};
-  :last-child {
-    border-bottom: 0;
-  }
-`;
-
-const Info = styled('div')`
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
+  grid-template-columns: auto 1fr;
   grid-gap: ${space(1)};
-  > *:nth-child(1),
-  > *:nth-child(2) {
-    grid-column: span 4;
-  }
+  align-items: center;
 `;
 
-const InfoItem = styled('div')`
+const IconWrapper = styled('div')`
+  justify-content: flex-start;
   display: flex;
-  align-items: center;
-  height: 100%;
+  cursor: pointer;
 `;
 
-const PanelAction = styled('div')`
-  padding: ${space(1)} ${space(2)};
+const Text = styled(TextOverflow)`
+  color: ${p => p.theme.gray700};
+  line-height: 40px;
+`;
+
+const Key = styled(Text)<{content: string}>`
+  visibility: hidden;
+  position: relative;
+  :after {
+    position: absolute;
+    top: 4px;
+    left: 0;
+    content: '${p => p.content}';
+    visibility: visible;
+    ${overflowEllipsis};
+  }
+`;
+
+const Actions = styled('div')`
   display: grid;
+  grid-template-columns: auto 1fr;
   grid-gap: ${space(1)};
-  grid-template-columns: auto auto;
-  justify-content: flex-end;
-  border-top: 1px solid ${p => p.theme.borderDark};
+  align-items: center;
 `;
 
-const Name = styled('h4')`
-  font-size: ${p => p.theme.fontSizeLarge} !important;
-  font-weight: 600;
-  margin-bottom: 0 !important;
-  color: ${p => p.theme.gray600};
+const StyledButton = styled(Button)`
+  width: 40px;
 `;
 
-const PublicKey = styled('h5')`
-  font-size: ${p => p.theme.fontSizeMedium} !important;
-  font-weight: 400;
-  margin-bottom: 0 !important;
-`;
+const Name = styled(Actions)``;
