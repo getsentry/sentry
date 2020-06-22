60224c43c35817a06798bf2b7f6f54c691068d6c
Radu Woinaroski <radu@sentry.io>
2020-06-22 11:17:06 +0200

    fix unit tests to work with the new trusted-relays config

--- tests/sentry/api/endpoints/test_relay_projectconfigs.py
+++ tests/sentry/api/endpoints/test_relay_projectconfigs.py
@@ -91,7 +91,9 @@ def call_endpoint(client, relay, private_key, default_project):
 
 @pytest.fixture
 def add_org_key(default_organization, relay):
-    default_organization.update_option("sentry:trusted-relays", [relay.public_key])
+    default_organization.update_option(
+        "sentry:trusted-relays", [{"public_key": relay.public_key, "name": "main-relay"}]
+    )
 
 
 @pytest.mark.django_db
@@ -229,7 +231,9 @@ def test_trusted_external_relays_should_receive_minimal_configs(
     assert safe.get_path(cfg, "slug") == default_project.slug
     assert safe.get_path(cfg, "rev") is not None
 
-    assert safe.get_path(cfg, "config", "trustedRelays") == [relay.public_key]
+    trusted_relay = safe.get_path(cfg, "config", "trustedRelays")
+    public_keys = [relay_info.get("public_key") for relay_info in trusted_relay]
+    assert public_keys == [relay.public_key]
     assert safe.get_path(cfg, "config", "filterSettings") is None
     assert safe.get_path(cfg, "config", "groupingConfig") is None
     assert safe.get_path(cfg, "config", "datascrubbingSettings", "scrubData") is not None
