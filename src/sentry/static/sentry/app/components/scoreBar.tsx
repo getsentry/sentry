4ed757803f07854744b1ece6a56561d20dc2edad
k-fish <kevan.fisher@sentry.io>
2020-06-22 08:20:17 -0700

    Switch ScoreBar to be a functional component

--- src/sentry/static/sentry/app/components/scoreBar.tsx
+++ src/sentry/static/sentry/app/components/scoreBar.tsx
@@ -4,69 +4,64 @@ import styled from '@emotion/styled';
 
 import theme from 'app/utils/theme';
 
-type DefaultProps = {
-  size: number;
-  thickness: number;
-  radius: number;
-  palette: Readonly<string[]>;
-};
-
-type Props = DefaultProps & {
-  vertical?: boolean;
+type Props = {
   score: number;
-  paletteClassNames?: string[];
+  size?: number;
+  thickness?: number;
+  radius?: number;
+  palette?: Readonly<string[]>;
   className?: string;
+  paletteClassNames?: string[];
+  vertical?: boolean;
 };
 
-class ScoreBar extends React.Component<Props> {
-  static propTypes = {
-    vertical: PropTypes.bool,
-    score: PropTypes.number.isRequired,
-    /** Array of strings */
-    palette: PropTypes.arrayOf(PropTypes.string),
-    /** Array of classNames whose index maps to score */
-    paletteClassNames: PropTypes.arrayOf(PropTypes.string),
-    size: PropTypes.number,
-    thickness: PropTypes.number,
-    radius: PropTypes.number,
+const ScoreBar = ({
+  score,
+  className,
+  vertical,
+  size = 40,
+  thickness = 4,
+  radius = 3,
+  palette = theme.similarity.colors,
+}: Props) => {
+  const maxScore = palette.length;
+
+  // Make sure score is between 0 and maxScore
+  const scoreInBounds = score >= maxScore ? maxScore : score <= 0 ? 0 : score;
+  // Make sure paletteIndex is 0 based
+  const paletteIndex = scoreInBounds - 1;
+
+  // Size of bar, depends on orientation, although we could just apply a transformation via css
+  const barProps = {
+    vertical,
+    thickness,
+    size,
+    radius,
   };
 
-  static defaultProps: DefaultProps = {
-    size: 40,
-    thickness: 4,
-    radius: 3,
-    palette: theme.similarity.colors,
-  };
+  return (
+    <div className={className}>
+      {[...Array(scoreInBounds)].map((_j, i) => (
+        <Bar {...barProps} key={i} color={palette[paletteIndex]} />
+      ))}
+      {[...Array(maxScore - scoreInBounds)].map((_j, i) => (
+        <Bar key={`empty-${i}`} {...barProps} empty />
+      ))}
+    </div>
+  );
+};
 
-  render() {
-    const {className, vertical, palette, score, size, thickness, radius} = this.props;
-    const maxScore = palette.length;
-
-    // Make sure score is between 0 and maxScore
-    const scoreInBounds = score >= maxScore ? maxScore : score <= 0 ? 0 : score;
-    // Make sure paletteIndex is 0 based
-    const paletteIndex = scoreInBounds - 1;
-
-    // Size of bar, depends on orientation, although we could just apply a transformation via css
-    const barProps = {
-      vertical,
-      thickness,
-      size,
-      radius,
-    };
-
-    return (
-      <div className={className}>
-        {[...Array(scoreInBounds)].map((_j, i) => (
-          <Bar {...barProps} key={i} color={palette[paletteIndex]} />
-        ))}
-        {[...Array(maxScore - scoreInBounds)].map((_j, i) => (
-          <Bar key={`empty-${i}`} {...barProps} empty />
-        ))}
-      </div>
-    );
-  }
-}
+ScoreBar.propTypes = {
+  vertical: PropTypes.bool,
+  score: PropTypes.number.isRequired,
+  /** Array of strings */
+  palette: PropTypes.arrayOf(PropTypes.string),
+  /** Array of classNames whose index maps to score */
+  paletteClassNames: PropTypes.arrayOf(PropTypes.string),
+  size: PropTypes.number,
+  thickness: PropTypes.number,
+  radius: PropTypes.number,
+};
 
 const StyledScoreBar = styled(ScoreBar)`
   display: flex;
@@ -79,12 +74,12 @@ const StyledScoreBar = styled(ScoreBar)`
 `;
 
 type BarProps = {
-  empty?: boolean;
-  color?: string;
-  vertical?: boolean;
-  thickness: number;
-  size: number;
   radius: number;
+  size: number;
+  thickness: number;
+  color?: string;
+  empty?: boolean;
+  vertical?: boolean;
 };
 
 const Bar = styled('div')<BarProps>`
