4242fa71d8c4dd8328ce0be31999e78ff10bddf9
k-fish <kevan.fisher@sentry.io>
2020-06-23 11:14:44 -0700

    ref(ts): Convert simpler input field variations to typescript (#19501)
    This converts some of the simple input field variations to typescript, as well as functional components.

--- src/sentry/static/sentry/app/views/settings/components/forms/dateTimeField.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import InputField from './inputField';
-
-export default class DateTimeField extends React.Component {
-  render() {
-    return <InputField {...this.props} type="datetime-local" />;
-  }
-}
