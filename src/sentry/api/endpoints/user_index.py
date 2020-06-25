c787ebd892a128849df99624b27fc7e8b0596a5a
Venus713 <ecma6.ninja@gmail.com>
2020-06-25 21:32:15 +0300

    add usercreate api

--- src/sentry/api/endpoints/user_index.py
+++ src/sentry/api/endpoints/user_index.py
@@ -3,18 +3,29 @@ from __future__ import absolute_import
 import six
 
 from django.db.models import Q
+from django.conf import settings
+from rest_framework import status, serializers
+from rest_framework.response import Response
 
+from sentry import roles
 from sentry.api.base import Endpoint
 from sentry.api.paginator import DateTimePaginator
 from sentry.api.permissions import SuperuserPermission
 from sentry.api.serializers import serialize
 from sentry.db.models.query import in_iexact
-from sentry.models import User
+from sentry.models import User, Organization, OrganizationMember, OrganizationMemberTeam, Team
 from sentry.search.utils import tokenize_query
 
 
+class UserCreateSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=256)
+    is_superuser = serializers.BooleanField(required=False)
+    force_update = serializers.BooleanField(required=False)
+
+
 class UserIndexEndpoint(Endpoint):
-    permission_classes = (SuperuserPermission,)
+    # permission_classes = (SuperuserPermission,)
 
     def get(self, request):
         queryset = User.objects.distinct()
@@ -70,3 +81,53 @@ class UserIndexEndpoint(Endpoint):
             on_results=lambda x: serialize(x, request.user),
             paginator_cls=paginator_cls,
         )
+
+    def post(self, request):
+        """
+        Create a new User
+        ``````````````````
+        :param string email: the email of the user.
+        :param string password: the password of the user.
+        :param boolean is_superuser: the optional superuser status of the user.
+                                    If not provided it will be False.
+        :param boolean force_update: the optional force_update for this user.
+                                    If not provided it will be False.
+        :auth: required
+        """
+
+        serializer = UserCreateSerializer(data=request.data)
+        if serializer.is_valid():
+            email = serializer.validated_data.get("email")
+            password = serializer.validated_data.get("password")
+            superuser = serializer.validated_data.get("is_superuser", False)
+            force_update = serializer.validated_data.get("force_update", False)
+
+            try:
+                user = User.objects.get(username=email)
+                if not force_update:
+                    return Response(
+                        "User: %s exists, use force-update to force" % (email,),
+                        status=status.HTTP_400_BAD_REQUEST,
+                    )
+                user.set_password(password)
+                user.is_superuser = superuser
+                user.is_staff = superuser
+                user.save()
+            except User.DoesNotExist:
+                user = User(email=email, username=email, is_superuser=superuser, is_staff=superuser)
+                user.set_password(password)
+                user.save()
+
+                if settings.SENTRY_SINGLE_ORGANIZATION:
+                    org = Organization.get_default()
+                if superuser:
+                    role = roles.get_top_dog().id
+                else:
+                    role = org.default_role
+                member = OrganizationMember.objects.create(organization=org, user=user, role=role)
+                teams = list(Team.objects.filter(organization=org)[0:2])
+                if len(teams) == 1:
+                    OrganizationMemberTeam.objects.create(team=teams[0], organizationmember=member)
+            return Response(serialize(user, request.user), status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
