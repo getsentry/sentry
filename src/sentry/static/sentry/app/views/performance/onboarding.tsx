d527a2c2c594258d02b6e77d51dd73e0113eea8d
Adam McKerlie <adam.mckerlie@sentry.io>
2020-06-26 16:35:07 -0400

    Updated Performance's empty state with new copy and image

--- src/sentry/static/sentry/app/views/performance/onboarding.tsx
+++ src/sentry/static/sentry/app/views/performance/onboarding.tsx
@@ -1,28 +1,50 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
 import OnboardingPanel from 'app/components/onboardingPanel';
 import Button from 'app/components/button';
-import {IconLightning} from 'app/icons';
+import space from 'app/styles/space';
 import {t} from 'app/locale';
+
+import emptyState from '../../../images/spot/performance-empty-state.svg';
 
 function Onboarding() {
   return (
-    <OnboardingPanel image={<IconLightning size="200px" />}>
-      <h3>{t('No transactions yet')}</h3>
+    <StyledOnboardingPanel>
+      <h3>{t('Pinpoint problems')}</h3>
       <p>
         {t(
-          'View transactions sorted by slowest duration time, related issues, and number of users having a slow experience in one consolidated view. Trace those 10-second page loads to poor-performing API calls and its children.'
+          "You've got this souped up plan. Now what? Get your software set up. We’ve got endpoints to track down."
         )}
       </p>
-      <Button
-        priority="primary"
-        target="_blank"
-        href="https://docs.sentry.io/performance-monitoring/setup/"
-      >
-        {t('Start Setup')}
-      </Button>
-    </OnboardingPanel>
+      <ButtonList>
+        <Button
+          priority="default"
+          target="_blank"
+          href="https://docs.sentry.io/performance-monitoring/setup/"
+        >
+          {t('Take the tour')}
+        </Button>
+        <Button
+          priority="primary"
+          target="_blank"
+          href="https://docs.sentry.io/performance-monitoring/setup/"
+        >
+          {t('Step 1: Install')}
+        </Button>
+      </ButtonList>
+    </StyledOnboardingPanel>
   );
 }
+
+const StyledOnboardingPanel = styled(OnboardingPanel)`
+  background: url(${emptyState}) no-repeat left center !important;
+`;
+
+const ButtonList = styled('div')`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(130px, max-content));
+  grid-gap: ${space(1)};
+`;
 
 export default Onboarding;
