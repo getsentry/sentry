d24ab552d780cc59682599a17f39aa1cd3e28228
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2020-06-26 14:04:03 -0700

    add precision to chart marklines (#19577)

--- src/sentry/static/sentry/app/views/alerts/details/chart.tsx
+++ src/sentry/static/sentry/app/views/alerts/details/chart.tsx
@@ -86,6 +86,24 @@ const Chart = (props: Props) => {
       ? criticalTrigger?.resolveThreshold
       : undefined;
 
+  const marklinePrecision = Math.max(
+    ...[
+      warningTriggerAlertThreshold,
+      warningTriggerResolveThreshold,
+      criticalTriggerAlertThreshold,
+      criticalTriggerResolveThreshold,
+    ].map(decimal => {
+      if (!decimal || !isFinite(decimal)) return 0;
+      let e = 1;
+      let p = 0;
+      while (Math.round(decimal * e) / e !== decimal) {
+        e *= 10;
+        p += 1;
+      }
+      return p;
+    })
+  );
+
   return (
     <LineChart
       isGroupedByDate
@@ -137,6 +155,7 @@ const Chart = (props: Props) => {
                   yAxis: warningTriggerAlertThreshold,
                 },
               ],
+              precision: marklinePrecision,
               label: {
                 show: true,
                 position: 'insideEndTop',
@@ -159,6 +178,7 @@ const Chart = (props: Props) => {
                   yAxis: warningTriggerResolveThreshold,
                 },
               ],
+              precision: marklinePrecision,
               label: {
                 show: true,
                 position: 'insideEndBottom',
@@ -181,6 +201,7 @@ const Chart = (props: Props) => {
                   yAxis: criticalTriggerAlertThreshold,
                 },
               ],
+              precision: marklinePrecision,
               label: {
                 show: true,
                 position: 'insideEndTop',
@@ -203,6 +224,7 @@ const Chart = (props: Props) => {
                   yAxis: criticalTriggerResolveThreshold,
                 },
               ],
+              precision: marklinePrecision,
               label: {
                 show: true,
                 position: 'insideEndBottom',
