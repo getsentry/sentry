3b7bc1bb065446cb042f2eac31d39bbd6310a962
Matej Minar <matej.minar@sentry.io>
2020-06-30 10:23:39 +0200

    feat(ui): Add type column to source maps (#19596)

--- src/sentry/static/sentry/app/views/settings/projectSourceMaps/list/sourceMapsArchiveRow.tsx
+++ src/sentry/static/sentry/app/views/settings/projectSourceMaps/list/sourceMapsArchiveRow.tsx
@@ -38,6 +38,7 @@ const SourceMapsArchiveRow = ({archive, orgId, projectId, onDelete}: Props) => {
       <ArtifactsColumn>
         <Count value={fileCount} />
       </ArtifactsColumn>
+      <Column>{t('release')}</Column>
       <Column>
         <DateTime date={date} />
       </Column>
