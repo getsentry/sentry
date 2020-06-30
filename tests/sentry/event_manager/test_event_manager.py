7ff70a51ebc4d273487639adabf35322d92aba13
Jan Michael Auer <mail@jauer.org>
2020-06-30 15:51:00 +0200

    test(event_manager): Test crash report limiting

--- tests/sentry/event_manager/test_event_manager.py
+++ tests/sentry/event_manager/test_event_manager.py
@@ -1062,6 +1062,60 @@ class EventManagerTest(TestCase):
         assert query(tsdb.models.organization_total_blacklisted, event.project.organization.id) == 1
         assert query(tsdb.models.project_total_blacklisted, event.project.id) == 1
 
+    def test_honors_crash_report_limit(self):
+        from sentry.utils.outcomes import track_outcome
+
+        mock_track_outcome = mock.Mock(wraps=track_outcome)
+
+        # Allow exactly one crash report
+        self.project.update_option("sentry:store_crash_reports", 1)
+
+        manager = EventManager(
+            make_event(message="foo", event_id="a" * 32, fingerprint=["a" * 32]),
+            project=self.project,
+        )
+        manager.normalize()
+
+        a1 = CachedAttachment(name="a1", data=b"hello", type="event.minidump")
+        a2 = CachedAttachment(name="a2", data=b"world")
+        cache_key = cache_key_for_event(manager.get_data())
+        attachment_cache.set(cache_key, attachments=[a1, a2])
+
+        with mock.patch("sentry.event_manager.track_outcome", mock_track_outcome):
+            with self.feature("organizations:event-attachments"):
+                with self.tasks():
+                    manager.save(self.project.id, cache_key=cache_key)
+
+        # The first minidump should be accepted, since the limit is 1
+        assert mock_track_outcome.call_count == 3
+        for o in mock_track_outcome.mock_calls:
+            assert o.kwargs["outcome"] == Outcome.ACCEPTED
+
+        mock_track_outcome.reset_mock()
+
+        manager = EventManager(
+            make_event(message="foo", event_id="b" * 32, fingerprint=["a" * 32]),
+            project=self.project,
+        )
+        manager.normalize()
+
+        cache_key = cache_key_for_event(manager.get_data())
+        attachment_cache.set(cache_key, attachments=[a1, a2])
+
+        with mock.patch("sentry.event_manager.track_outcome", mock_track_outcome):
+            with self.feature("organizations:event-attachments"):
+                with self.tasks():
+                    manager.save(self.project.id, cache_key=cache_key)
+
+        assert mock_track_outcome.call_count == 3
+        o = mock_track_outcome.mock_calls[0]
+        assert o.kwargs["outcome"] == Outcome.FILTERED
+        assert o.kwargs["category"] == DataCategory.ATTACHMENT
+        assert o.kwargs["reason"] == FilterStatKeys.CRASH_REPORT_LIMIT
+
+        for o in mock_track_outcome.mock_calls[1:]:
+            assert o.kwargs["outcome"] == Outcome.ACCEPTED
+
     def test_event_accepted_outcome(self):
         manager = EventManager(make_event(message="foo"))
         manager.normalize()
