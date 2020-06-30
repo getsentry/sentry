6bc168dfcf61846efa9a0ed4a1f9e229555bf6d6
Robin Rendle <robinjrendle@gmail.com>
2020-06-30 11:39:43 -0700

    fixed Text component styles

--- src/sentry/static/sentry/app/components/text.tsx
+++ src/sentry/static/sentry/app/components/text.tsx
@@ -1,12 +1,9 @@
 import styled from '@emotion/styled';
 
 import space from 'app/styles/space';
-import textStyles from 'app/styles/text';
 import Panel from 'app/components/panels/panel';
 
 const Text = styled('div')`
-  ${textStyles};
-
   ${/* sc-selector */ Panel} & {
     padding-left: ${space(2)};
     padding-right: ${space(2)};
