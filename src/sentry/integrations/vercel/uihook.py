7f5d1b141ef76752cf077c51d23c7999e363b3c2
Colleen O'Rourke <colleen@sentry.io>
2020-07-01 10:06:24 -0700

    add logging, update error handling

--- src/sentry/integrations/vercel/uihook.py
+++ src/sentry/integrations/vercel/uihook.py
@@ -1,8 +1,9 @@
 from __future__ import absolute_import
 
 import json
+import logging
 
-from django.http import HttpResponse, HttpResponseNotFound
+from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from sentry.api.base import Endpoint
@@ -10,6 +11,8 @@ from sentry.constants import ObjectStatus
 from sentry.models import Integration, Organization, OrganizationIntegration
 from sentry.web.api import allow_cors_options
 from sentry.web.helpers import render_to_response
+
+logger = logging.getLogger("sentry.integrations.vercel")
 
 
 class VercelUIHook(Endpoint):
@@ -21,30 +24,36 @@ class VercelUIHook(Endpoint):
         if request.method == "OPTIONS":
             return HttpResponse(status=200)
         if request.method != "POST":
-            return HttpResponseNotFound("Page not found")
+            return HttpResponse(status=405)
         body_unicode = request.body.decode("utf-8")
         body = json.loads(body_unicode)
         configuration_id = body["configurationId"]
         user_id = body["user"]["id"]
         team_id = body["teamId"]
-        integration = Integration.objects.get(external_id=team_id or user_id, provider="vercel")
-        if integration and integration.status == ObjectStatus.ACTIVE:
-            try:
-                organization = Organization.objects.get(
-                    id=integration.metadata["configurations"][configuration_id]["organization_id"]
-                )
-            except KeyError:
-                return HttpResponse("The requested integration does not exist.")
-            try:
-                OrganizationIntegration.objects.get(
-                    organization=organization.id, integration=integration.id
-                )
-            except OrganizationIntegration.DoesNotExist:
-                return HttpResponse("The requested integration does not exist.")
-            if request.method == "POST":
-                return render_to_response(
-                    "sentry/vercel-ui-hook.vercel",
-                    request=request,
-                    context={"org": organization.slug, "integration_id": integration.id},
-                )
-        return HttpResponse("The requested integration does not exist.")
+        try:
+            integration = Integration.objects.get(
+                external_id=team_id or user_id, provider="vercel", status=ObjectStatus.ACTIVE
+            )
+        except Integration.DoesNotExist:
+            return HttpResponse("The requested integration does not exist.")
+        try:
+            organization = Organization.objects.get(
+                id=integration.metadata["configurations"][configuration_id]["organization_id"]
+            )
+        except KeyError:
+            return HttpResponse("The requested integration does not exist.")
+        try:
+            OrganizationIntegration.objects.get(
+                organization=organization.id, integration=integration.id
+            )
+        except OrganizationIntegration.DoesNotExist:
+            logger.info(
+                "vercel.organization-integration.does-not-exist",
+                extra={"organization_id": organization.id, "integration_id": integration.id},
+            )
+            return HttpResponse("The requested integration does not exist.")
+        return render_to_response(
+            "sentry/vercel-ui-hook.vercel",
+            request=request,
+            context={"org": organization.slug, "integration_id": integration.id},
+        )
