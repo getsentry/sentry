c298af798eceb52cc9bf49ab4ba957c388f940d0
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-07-02 13:28:46 +0200

    use api_name

--- src/sentry/utils/outcomes.py
+++ src/sentry/utils/outcomes.py
@@ -176,5 +176,9 @@ def track_outcome(
     metrics.incr(
         "events.outcomes",
         skip_internal=True,
-        tags={"outcome": outcome.name.lower(), "reason": reason, "category": category or "null"},
+        tags={
+            "outcome": outcome.name.lower(),
+            "reason": reason,
+            "category": category.api_name() if category is not None else "null",
+        },
     )
