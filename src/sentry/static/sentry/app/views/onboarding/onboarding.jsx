1e33d70ecce090f2ba6a62490f8951b8c6593b42
Billy Vong <billyvg@users.noreply.github.com>
2020-07-06 08:17:20 -0700

    fix(ui): Fix disabling scroll animations in CI (#19704)
    `duration: 0` does not disable the animations, but `-1` does.

--- src/sentry/static/sentry/app/views/onboarding/onboarding.jsx
+++ src/sentry/static/sentry/app/views/onboarding/onboarding.jsx
@@ -134,7 +134,8 @@ class Onboarding extends React.Component {
     const step = this.activeStep;
     scrollToElement(`#onboarding_step_${step.id}`, {
       align: 'middle',
-      duration: process.env.IS_CI ? 0 : 300,
+      // Disable animations in CI - must be < 0 to disable
+      duration: process.env.IS_CI ? -1 : 300,
     });
   };
 
