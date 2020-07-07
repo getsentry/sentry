6a359022e79e9d75e70d55bfedcb7f683129dc9e
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2020-07-07 12:49:14 -0700

    fix: spelling

--- src/sentry/integrations/slack/action_endpoint.py
+++ src/sentry/integrations/slack/action_endpoint.py
@@ -20,7 +20,7 @@ from .utils import build_group_attachment, logger
 
 LINK_IDENTITY_MESSAGE = "Looks like you haven't linked your Sentry account with your Slack identity yet! <{associate_url}|Link your identity now> to perform actions in Sentry through Slack."
 
-UNLINK_IDENTITY_MESSAGE = "Looks like this Slack identity is linked to the Sentry user *{user_email}* who is not a member of organization *{org_name}* used with this Slack integration <{associate_url}|Unlink your identity now>."
+UNLINK_IDENTITY_MESSAGE = "Looks like this Slack identity is linked to the Sentry user *{user_email}* who is not a member of organization *{org_name}* used with this Slack integration. <{associate_url}|Unlink your identity now>."
 
 DEFAULT_ERROR_MESSAGE = "Sentry can't perform that action right now on your behalf!"
 
