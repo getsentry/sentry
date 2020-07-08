5dcb88e9db004a3edc8199858fa8939d33496559
Matej Minar <matej.minar@sentry.io>
2020-07-08 13:00:12 +0200

    feat(ui): Source maps better view on mobile

--- src/sentry/static/sentry/app/views/settings/projectSourceMaps/detail/index.tsx
+++ src/sentry/static/sentry/app/views/settings/projectSourceMaps/detail/index.tsx
@@ -23,6 +23,7 @@ import {decodeScalar} from 'app/utils/queryString';
 import Confirm from 'app/components/confirm';
 import Version from 'app/components/version';
 import TextOverflow from 'app/components/textOverflow';
+import space from 'app/styles/space';
 
 import SourceMapsArtifactRow from './sourceMapsArtifactRow';
 
@@ -207,11 +208,19 @@ class ProjectSourceMaps extends AsyncView<Props, State> {
 
 const StyledSettingsPageHeader = styled(SettingsPageHeader)`
   /*
-    ugly selector to make overflow ellipsis work
+    ugly selector to make header work on mobile
     we can refactor this once we start making other settings more responsive
   */
-  > div > div {
-    min-width: 0;
+  > div {
+    @media (max-width: ${p => p.theme.breakpoints[2]}) {
+      display: block;
+    }
+    > div {
+      min-width: 0;
+      @media (max-width: ${p => p.theme.breakpoints[2]}) {
+        margin-bottom: ${space(2)};
+      }
+    }
   }
 `;
 
@@ -221,7 +230,7 @@ const Title = styled('div')`
 `;
 
 const StyledPanelTable = styled(PanelTable)`
-  grid-template-columns: 1fr max-content 120px;
+  grid-template-columns: minmax(220px, 1fr) max-content 120px;
 `;
 
 const ReleaseButton = styled(Button)`
