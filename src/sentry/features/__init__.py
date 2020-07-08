569b8d55cf287cd910fca4b63088c42c8e2d74a0
MeredithAnya <meredith.a.heller@gmail.com>
2020-07-08 15:40:55 -0700

    ref(integrations): Add new feature flags (#19728)
    * ref(integrations): Add new feature flags

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -76,6 +76,9 @@ default_manager.add("organizations:incidents", OrganizationFeature)  # NOQA
 default_manager.add("organizations:integrations-event-hooks", OrganizationFeature)  # NOQA
 default_manager.add("organizations:integrations-issue-basic", OrganizationFeature)  # NOQA
 default_manager.add("organizations:integrations-issue-sync", OrganizationFeature)  # NOQA
+default_manager.add("organizations:integrations-alert-rule", OrganizationFeature)  # NOQA
+default_manager.add("organizations:integrations-chat-unfurl", OrganizationFeature)  # NOQA
+default_manager.add("organizations:integrations-incident-management", OrganizationFeature)  # NOQA
 default_manager.add("organizations:integrations-vercel", OrganizationFeature)  # NOQA
 default_manager.add("organizations:internal-catchall", OrganizationFeature)  # NOQA
 default_manager.add("organizations:invite-members", OrganizationFeature)  # NOQA
