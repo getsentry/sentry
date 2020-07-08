a3297e4d18248fefbabe509980dbb06872471ea7
Alberto Leal <mail4alberto@gmail.com>
2020-07-08 19:40:36 -0400

    prevent text selection of the span details pane via multi-clicking on the span row

--- src/sentry/static/sentry/app/components/events/interfaces/spans/spanBar.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/spanBar.tsx
@@ -875,6 +875,8 @@ const SpanRowCellContainer = styled('div')<SpanRowCellProps>`
   position: relative;
   height: ${SPAN_ROW_HEIGHT}px;
 
+  user-select: none;
+
   &:hover > div[data-type='span-row-cell'] {
     background-color: ${p => (p.showDetail ? p.theme.gray800 : p.theme.gray200)};
   }
