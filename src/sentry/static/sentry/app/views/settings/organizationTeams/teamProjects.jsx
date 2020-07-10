c4986c660d993628237778d81a762e44666a9ebc
Mark Story <mark@sentry.io>
2020-07-10 17:01:57 -0400

    ref(ui) Replace icon-circle-subtract with svg icons

--- src/sentry/static/sentry/app/views/settings/organizationTeams/teamProjects.jsx
+++ src/sentry/static/sentry/app/views/settings/organizationTeams/teamProjects.jsx
@@ -5,12 +5,12 @@ import styled from '@emotion/styled';
 import {Panel, PanelHeader, PanelBody, PanelItem} from 'app/components/panels';
 import {addErrorMessage, addSuccessMessage} from 'app/actionCreators/indicator';
 import {sortProjects} from 'app/utils';
+import {IconSubtract} from 'app/icons';
 import {t} from 'app/locale';
 import Button from 'app/components/button';
 import DropdownAutoComplete from 'app/components/dropdownAutoComplete';
 import DropdownButton from 'app/components/dropdownButton';
 import EmptyMessage from 'app/views/settings/components/emptyMessage';
-import InlineSvg from 'app/components/inlineSvg';
 import LoadingError from 'app/components/loadingError';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import Pagination from 'app/components/pagination';
@@ -148,11 +148,12 @@ class TeamProjects extends React.Component {
             <Button
               size="small"
               disabled={!canWrite}
+              icon={<StyledIconSubtract isCircled size="xs" />}
               onClick={() => {
                 this.handleLinkProject(project, 'remove');
               }}
             >
-              <RemoveIcon /> {t('Remove')}
+              {t('Remove')}
             </Button>
           </Tooltip>
         </StyledPanelItem>
@@ -221,11 +222,7 @@ class TeamProjects extends React.Component {
   }
 }
 
-const RemoveIcon = styled(props => (
-  <InlineSvg {...props} src="icon-circle-subtract">
-    {t('Remove')}
-  </InlineSvg>
-))`
+const StyledIconSubtract = styled(IconSubtract)`
   min-height: 1.25em;
   min-width: 1.25em;
   margin-right: ${space(1)};
