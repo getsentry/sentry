d02b116e47c1adfdad36b21863cc3f95183997b5
Chris Fuller <cfuller@sentry.io>
2020-07-13 10:15:52 -0400

    Adding org level combined rules endpoint

--- src/sentry/incidents/endpoints/organization_alert_rule_index.py
+++ src/sentry/incidents/endpoints/organization_alert_rule_index.py
@@ -6,10 +6,38 @@ from rest_framework.response import Response
 from sentry import features
 from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.api.exceptions import ResourceDoesNotExist
-from sentry.api.paginator import OffsetPaginator
-from sentry.api.serializers import serialize
+from sentry.api.paginator import OffsetPaginator, CombinedQuerysetPaginator
+from sentry.api.serializers import serialize, CombinedRuleSerializer
 from sentry.incidents.models import AlertRule
 from sentry.incidents.endpoints.serializers import AlertRuleSerializer
+from sentry.snuba.dataset import Dataset
+from sentry.models import Rule, RuleStatus
+
+
+class OrganizationCombinedRuleIndexEndpoint(OrganizationEndpoint):
+    def get(self, request, organization):
+        """
+        Fetches alert rules and legacy rules for an organization
+        """
+        project_ids = self.get_requested_project_ids(request) or None
+        alert_rules = AlertRule.objects.fetch_for_organization(organization, project_ids)
+        if not features.has("organizations:performance-view", organization):
+            # Filter to only error alert rules
+            alert_rules = alert_rules.filter(snuba_query__dataset=Dataset.Events.value)
+
+        return self.paginate(
+            request,
+            paginator_cls=CombinedQuerysetPaginator,
+            on_results=lambda x: serialize(x, request.user, CombinedRuleSerializer()),
+            default_per_page=25,
+            order_by="-date_added",
+            querysets=[
+                alert_rules,
+                Rule.objects.filter(
+                    project__in=[project_ids], status__in=[RuleStatus.ACTIVE, RuleStatus.INACTIVE]
+                ),
+            ],
+        )
 
 
 class OrganizationAlertRuleIndexEndpoint(OrganizationEndpoint):
@@ -21,10 +49,14 @@ class OrganizationAlertRuleIndexEndpoint(OrganizationEndpoint):
             raise ResourceDoesNotExist
 
         project_ids = self.get_requested_project_ids(request) or None
+        alert_rules = AlertRule.objects.fetch_for_organization(organization, project_ids)
+        if not features.has("organizations:performance-view", organization):
+            # Filter to only error alert rules
+            alert_rules = alert_rules.filter(snuba_query__dataset=Dataset.Events.value)
 
         return self.paginate(
             request,
-            queryset=AlertRule.objects.fetch_for_organization(organization, project_ids),
+            queryset=alert_rules,
             order_by="-date_added",
             paginator_cls=OffsetPaginator,
             on_results=lambda x: serialize(x, request.user),
