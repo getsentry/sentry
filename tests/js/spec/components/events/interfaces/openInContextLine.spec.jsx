9ab0be93f0b9547650bd0722ffd11649f6308aee
Priscila Oliveira <priscilawebdev@gmail.com>
2020-07-13 15:56:28 +0200

    Fix(openInContextLine): Replace link component with externalLink (#19824)

--- tests/js/spec/components/events/interfaces/openInContextLine.spec.jsx
+++ tests/js/spec/components/events/interfaces/openInContextLine.spec.jsx
@@ -63,7 +63,7 @@ describe('OpenInContextLine', function() {
       const stacktraceLinkFoo = wrapper.find(
         'OpenInLink[data-test-id="stacktrace-link-foo"]'
       );
-      expect(stacktraceLinkFoo.prop('to')).toEqual(url);
+      expect(stacktraceLinkFoo.prop('href')).toEqual(url);
       expect(stacktraceLinkFoo.text()).toEqual('Foo');
       expect(
         wrapper.find('OpenInLink[data-test-id="stacktrace-link-tesla"]').text()
