bc32f58ee9b95236acc888095a7c81ce12e91235
Billy Vong <billy@sentry.io>
2020-07-15 13:55:03 -0700

    test google cloud build

--- tests/js/setupFramework.ts
+++ tests/js/setupFramework.ts
@@ -1,6 +1,16 @@
 /* global process */
 import '@visual-snapshot/jest';
 
+export {};
+
+declare global {
+  namespace jest {
+    interface Matchers<R> {
+      toSnapshot(): CustomMatcherResult;
+    }
+  }
+}
+
 process.on('unhandledRejection', reason => {
   // eslint-disable-next-line no-console
   console.error(reason);
