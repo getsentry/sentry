a08311326f1a833ebd1f077dea6aeabf4d966fd1
Alberto Leal <mail4alberto@gmail.com>
2020-07-17 14:13:12 -0400

    fix(apm): Bulletproof span view from crashing whenever a span's span_id is identical to its parent_span_id (#19937)

--- src/sentry/static/sentry/app/components/events/interfaces/spans/spanTree.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/spanTree.tsx
@@ -137,7 +137,7 @@ class SpanTree extends React.Component<PropType> {
     numOfSpansOutOfViewAbove: number;
     numOfFilteredSpansAbove: number;
     span: Readonly<ProcessedSpanType>;
-    childSpans: Readonly<SpanChildrenLookupType>;
+    childSpans: SpanChildrenLookupType;
     generateBounds: (bounds: SpanBoundsType) => SpanGeneratedBoundsType;
     previousSiblingEndTimestamp: undefined | number;
   }): RenderedSpanTree => {
@@ -145,6 +145,13 @@ class SpanTree extends React.Component<PropType> {
 
     const spanBarColour: string = pickSpanBarColour(getSpanOperation(span));
     const spanChildren: Array<RawSpanType> = childSpans?.[getSpanID(span)] ?? [];
+
+    // Mark descendents as being rendered. This is to address potential recursion issues due to malformed data.
+    // For example if a span has a span_id that's identical to its parent_span_id.
+    childSpans = {
+      ...childSpans,
+    };
+    delete childSpans[getSpanID(span)];
 
     const bounds = generateBounds({
       startTimestamp: span.start_timestamp,
