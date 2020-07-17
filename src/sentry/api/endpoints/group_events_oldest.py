cc126243d58d45c2a89bcf3a0506a2af37174f1c
Alberto Leal <mail4alberto@gmail.com>
2020-07-17 09:20:14 -0400

    fix(issue): Ensure request query params are passed when fetching the oldest event of a group

--- src/sentry/api/endpoints/group_events_oldest.py
+++ src/sentry/api/endpoints/group_events_oldest.py
@@ -44,6 +44,7 @@ class GroupEventsOldestEndpoint(GroupEndpoint):
                     event.organization.slug, event.project.slug, event.event_id
                 ),
                 request=request,
+                data=request.query_params,
             )
         except client.ApiError as e:
             return Response(e.body, status=e.status_code)
