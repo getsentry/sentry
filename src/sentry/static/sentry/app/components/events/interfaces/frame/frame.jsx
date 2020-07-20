814d4fab721a6b3fb2975b622d02e071e5d590f0
Mark Story <mark@sentry.io>
2020-07-20 10:43:22 -0400

    Update src/sentry/static/sentry/app/components/events/interfaces/frame/frame.jsx
    Co-authored-by: Priscila Oliveira <priscilawebdev@gmail.com>

--- src/sentry/static/sentry/app/components/events/interfaces/frame/frame.jsx
+++ src/sentry/static/sentry/app/components/events/interfaces/frame/frame.jsx
@@ -418,7 +418,7 @@ const DefaultLine = styled('div')`
 
 const HintStatus = styled('span')`
   position: relative;
-  top: 2px;
+  top: ${space(0.25)};
   margin: 0 ${space(0.75)} 0 -${space(0.25)};
 `;
 
