d4f3941b6b91e5096591dc9303ff74f5618640a6
Stephen Cefali <scefali@sentry.io>
2020-07-24 10:51:24 -0700

    fix(vercel): fix bug with no commit (#20004)

--- tests/sentry/integrations/vercel/testutils.py
+++ tests/sentry/integrations/vercel/testutils.py
@@ -41,3 +41,33 @@ EXAMPLE_DEPLOYMENT_WEBHOOK_RESPONSE = """
         "userId": "cstd1xKmLGVMed0z0f3SHlD2"
     }
 """
+
+DEPLOYMENT_WEBHOOK_NO_COMMITS = """
+    {
+        "id": "uev_ZfbZKA3Ts2aEa2Cic6t6wSZx",
+        "ownerId": "cstd1xKmLGVMed0z0f3SHlD2",
+        "type": "deployment",
+        "createdAt": 1592335604941,
+        "payload": {
+            "deploymentId": "dpl_2p92SueSKLagubfcRtheS3CvmcjK",
+            "name": "nextjsblog-demo",
+            "project": "nextjsblog-demo",
+            "url": "nextjsblog-demo-gogovbsz1.vercel.app",
+            "projectId": "QmQPfU4xn5APjEsSje4ccPcSJCmVAByA8CDKfZRhYyVPAg",
+            "plan": "hobby",
+            "regions": ["sfo1"],
+            "target": "production",
+            "alias": ["nextjsblog-demo.now.sh","nextjsblog-demo.meredithanya.vercel.app","nextjsblog-demo-git-master.meredithanya.vercel.app","nextjsblog-demo.meredithanya.now.sh","nextjsblog-demo-git-master.meredithanya.now.sh"],
+            "type": "LAMBDAS",
+            "deployment": {
+                "id": "dpl_2p92SueSKLagubfcRtheS3CvmcjK",
+                "name": "nextjsblog-demo",
+                "url": "nextjsblog-demo-gogovbsz1.vercel.app",
+                "meta": {}
+            }
+        },
+        "region": "now-sfo",
+        "teamId": null,
+        "userId": "cstd1xKmLGVMed0z0f3SHlD2"
+    }
+"""
