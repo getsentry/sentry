54de9d8fd70c1a539fdb563b1b04a44afdcb2c41
William Mak <william@wmak.io>
2020-07-28 14:35:43 -0400

    fix(discover): Allow percentiles with more decimals (#20028)
    - Because we formatted percentiles to two decimal points more precise
      percentiles would get rounded
      - eg. 0.995 would become 0.99 instead
    - using the g type field in the format string to drop irrelevent zeros,
      mostly to reduce query length.

--- tests/sentry/api/test_event_search.py
+++ tests/sentry/api/test_event_search.py
@@ -2123,6 +2123,9 @@ class ResolveFieldListTest(unittest.TestCase):
             "percentile(transaction.duration, 0.75)",
             "percentile(transaction.duration, 0.95)",
             "percentile(transaction.duration, 0.99)",
+            "percentile(transaction.duration, 0.995)",
+            "percentile(transaction.duration, 0.99900)",
+            "percentile(transaction.duration, 0.99999)",
         ]
         result = resolve_field_list(fields, eventstore.Filter())
 
@@ -2141,6 +2144,13 @@ class ResolveFieldListTest(unittest.TestCase):
             ["quantile(0.75)", "transaction.duration", "percentile_transaction_duration_0_75"],
             ["quantile(0.95)", "transaction.duration", "percentile_transaction_duration_0_95"],
             ["quantile(0.99)", "transaction.duration", "percentile_transaction_duration_0_99"],
+            ["quantile(0.995)", "transaction.duration", "percentile_transaction_duration_0_995"],
+            ["quantile(0.999)", "transaction.duration", "percentile_transaction_duration_0_99900"],
+            [
+                "quantile(0.99999)",
+                "transaction.duration",
+                "percentile_transaction_duration_0_99999",
+            ],
         ]
         assert result["groupby"] == []
 
