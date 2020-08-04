84da289b8f9f1a993e0e8200c57d519c2ac99c04
Robin Rendle <robinjrendle@gmail.com>
2020-08-04 13:39:45 -0700

    fixed color

--- docs-ui/components/layout-thirds.stories.js
+++ docs-ui/components/layout-thirds.stories.js
@@ -155,7 +155,7 @@ SingleColumnMode.story = {
 };
 
 const Container = styled('div')`
-  background: ${p => p.theme.gray200};
+  background: ${p => p.theme.gray100};
   margin: ${space(2)};
   border: 1px solid ${p => p.theme.borderDark};
 `;
