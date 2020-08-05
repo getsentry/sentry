959b6e10a81cc77d03ad2892765dcf41e2f96411
Alberto Leal <mail4alberto@gmail.com>
2020-08-05 15:26:30 -0400

    add team delete

--- src/sentry/static/sentry/app/views/teamsDashboard/teamDetails/settings.tsx
+++ src/sentry/static/sentry/app/views/teamsDashboard/teamDetails/settings.tsx
@@ -3,6 +3,7 @@ import * as ReactRouter from 'react-router';
 import {Location} from 'history';
 
 import {t, tct} from 'app/locale';
+import {Panel, PanelHeader} from 'app/components/panels';
 import {Organization, Team} from 'app/types';
 import {addErrorMessage, addSuccessMessage} from 'app/actionCreators/indicator';
 import TeamModel from 'app/views/settings/organizationTeams/teamSettings/model';
@@ -10,8 +11,15 @@ import Form from 'app/views/settings/components/forms/form';
 import teamSettingsFields from 'app/data/forms/teamSettingsFields';
 import JsonForm from 'app/views/settings/components/forms/jsonForm';
 import {removeTeam, updateTeamSuccess} from 'app/actionCreators/teams';
+import Field from 'app/views/settings/components/forms/field';
+import Confirm from 'app/components/confirm';
+import Button from 'app/components/button';
+import {IconDelete} from 'app/icons';
+import withApi from 'app/utils/withApi';
+import {Client} from 'app/api';
 
 type Props = {
+  api: Client;
   location: Location;
   team: Team;
   organization: Organization;
@@ -29,22 +37,35 @@ class Settings extends React.Component<Props> {
   // TODO: needs a type
   model: any = undefined;
 
+  getTeamsRoute() {
+    const isAllTeams = this.props.location.pathname.includes('all-teams/');
+    return isAllTeams ? 'all-teams' : 'my-teams';
+  }
+
   handleTeamRename = (resp, model, id) => {
     updateTeamSuccess(resp.slug, resp);
     if (id === 'slug') {
       addSuccessMessage(t('Team name changed'));
 
-      const isAllTeams = location.pathname.includes('all-teams/');
-
-      const teamsRoute = isAllTeams ? 'all-teams' : 'my-teams';
-
       this.props.router.replace(
         `/organizations/${
           this.props.organization.slug
-        }/teams/${teamsRoute}/${model.getValue(id)}/settings/`
+        }/teams/${this.getTeamsRoute()}/${model.getValue(id)}/settings/`
       );
       this.setState({loading: true});
     }
+  };
+
+  handleRemoveTeam = async () => {
+    const {team, organization} = this.props;
+    await removeTeam(this.props.api, {
+      teamId: team.slug,
+      orgId: organization.slug,
+    });
+
+    this.props.router.replace(
+      `/organizations/${this.props.organization.slug}/teams/${this.getTeamsRoute()}/`
+    );
   };
 
   render() {
@@ -67,9 +88,37 @@ class Settings extends React.Component<Props> {
         >
           <JsonForm access={access} location={location} forms={teamSettingsFields} />
         </Form>
+
+        <Panel>
+          <PanelHeader>{t('Remove Team')}</PanelHeader>
+          <Field
+            help={t(
+              "This may affect team members' access to projects and associated alert delivery."
+            )}
+          >
+            <div>
+              <Confirm
+                disabled={!access.has('team:admin')}
+                onConfirm={this.handleRemoveTeam}
+                priority="danger"
+                message={tct('Are you sure you want to remove the team [team]?', {
+                  team: `#${team.slug}`,
+                })}
+              >
+                <Button
+                  icon={<IconDelete />}
+                  priority="danger"
+                  disabled={!access.has('team:admin')}
+                >
+                  {t('Remove Team')}
+                </Button>
+              </Confirm>
+            </div>
+          </Field>
+        </Panel>
       </div>
     );
   }
 }
 
-export default ReactRouter.withRouter(Settings);
+export default ReactRouter.withRouter(withApi(Settings));
