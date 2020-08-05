4eac57aec6565ce654da95efaa0b54766d6e37b2
Alberto Leal <mail4alberto@gmail.com>
2020-08-05 16:45:25 -0400

    add team goal stub

--- src/sentry/static/sentry/app/views/teamsDashboard/teamDetails/header/footer.tsx
+++ src/sentry/static/sentry/app/views/teamsDashboard/teamDetails/header/footer.tsx
@@ -5,6 +5,7 @@ import {t} from 'app/locale';
 import AvatarList from 'app/components/avatar/avatarList';
 import {User, Project, Environment} from 'app/types';
 import ProjectBadge from 'app/components/idBadge/projectBadge';
+import {IconArrow} from 'app/icons';
 
 import FooterItem from './footerItem';
 
@@ -30,6 +31,22 @@ const Footer = ({users, projects}: Props) => (
         <StyledAvatarList users={users} avatarSize={35} />
       </FooterItem>
     )}
+    <FooterItem title={t('Q3 Apdex Goal')}>
+      <GoalContainer>
+        <CurrentGoalNumber>0.964</CurrentGoalNumber>
+        <UpArrow>
+          <IconArrow direction="up" />
+        </UpArrow>
+      </GoalContainer>
+    </FooterItem>
+    <FooterItem title={t('Q3 Miserable Users Goal')}>
+      <GoalContainer>
+        <CurrentGoalNumber>{Number(2749).toLocaleString()}</CurrentGoalNumber>
+        <DownArrow>
+          <IconArrow direction="down" />
+        </DownArrow>
+      </GoalContainer>
+    </FooterItem>
   </Wrapper>
 );
 
@@ -38,7 +55,17 @@ export default Footer;
 const Wrapper = styled('div')`
   display: grid;
   grid-template-columns: max-content max-content max-content;
+  grid-auto-flow: column;
   grid-gap: 60px;
+`;
+
+const GoalContainer = styled('div')`
+  display: flex;
+  align-items: center;
+
+  > * + * {
+    margin-left: 8px;
+  }
 `;
 
 const StyledAvatarList = styled(AvatarList)`
@@ -46,4 +73,21 @@ const StyledAvatarList = styled(AvatarList)`
   .avatar {
     margin-left: 0;
   }
+`;
+
+const CurrentGoalNumber = styled('div')`
+  font-size: ${p => p.theme.headerFontSize};
+  line-height: 1.2;
+`;
+
+const UpArrow = styled('div')`
+  color: ${p => p.theme.green500};
+  display: flex;
+  align-items: center;
+`;
+
+const DownArrow = styled('div')`
+  color: ${p => p.theme.red500};
+  display: flex;
+  align-items: center;
 `;
