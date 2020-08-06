143e83060ee63664fb28028b2381e8e2b7ffcdcd
Evan Hicks <evan.hicks@sentry.io>
2020-08-06 17:13:21 -0400

    fix(discover) Remove function translation code
    Ran across this and it's not necessary anymore. I checked redash and no one is
    using the old aliases anymore, so I removed all this.

--- tests/snuba/api/endpoints/test_organization_events_stats.py
+++ tests/snuba/api/endpoints/test_organization_events_stats.py
@@ -846,7 +846,6 @@ class OrganizationEventsStatsTopNEvents(APITestCase, SnubaTestCase):
 
         assert response.status_code == 200, response.content
         assert len(data) == 5
-
         for index, event in enumerate(self.events[:5]):
             message = event.message or event.transaction
             results = data[",".join([message, event.project.slug])]
