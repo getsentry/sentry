e9caa0ae783546ef7839a5d9d81ac561e8d5b840
Mark Story <mark@sentry.io>
2020-08-10 17:12:48 -0400

    ref(ts) Convert SavedSearches store and action creator to typescript (#20146)
    * Change orgId to orgSlug
    * Consolidate types for saved searches.

--- src/sentry/static/sentry/app/types/index.tsx
+++ src/sentry/static/sentry/app/types/index.tsx
@@ -398,10 +398,31 @@ export type Environment = {
   id: string;
 };
 
-// TODO(ts): This type is incomplete
-export type SavedSearch = {
-  query?: string;
+export type RecentSearch = {
+  id: string;
+  organizationId: string;
+  type: SavedSearchType;
+  query: string;
+  lastSeen: string;
+  dateCreated: string;
 };
+
+// XXX: Deprecated Sentry 9 attributes are not included here.
+export type SavedSearch = {
+  id: string;
+  type: SavedSearchType;
+  name: string;
+  query: string;
+  isGlobal: boolean;
+  isPinned: boolean;
+  isOrgCustom: boolean;
+  dateCreated: string;
+};
+
+export enum SavedSearchType {
+  ISSUE = 0,
+  EVENT = 1,
+}
 
 export type PluginNoProject = {
   id: string;
