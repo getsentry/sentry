69fc8b36fad98ba92598dafbb70023a9d65077dc
Billy Vong <billy@sentry.io>
2020-08-19 07:00:31 -0700

    build(github): Change how jest snapshots are created

--- config/webpack.css.config.js
+++ config/webpack.css.config.js
@@ -5,5 +5,18 @@ const config = require('../webpack.config');
 config.entry = {
   sentry: 'less/jest-ci.less',
 };
+config.module = {
+  ...config.module,
+  rules: [
+    ...config.module.rules.filter(({test}) =>
+      ['/\\.css/', '/\\.less$/'].includes(test.toString())
+    ),
+
+    {
+      test: /\.(jpe?g|png|gif|ttf|eot|svg|woff(2)?)(\?[a-z0-9=&.]+)?$/,
+      use: 'base64-inline-loader?limit=10000&name=[name].[ext]',
+    },
+  ],
+};
 
 module.exports = config;
