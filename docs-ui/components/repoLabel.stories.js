2074330862b9894d0a7aeef6d030b4bddbc04d29
Mark Story <mark@sentry.io>
2020-08-21 09:12:12 -0400

    feat(onboarding) Add component for new tour designs (#20278)
    A new kind of feature tour is being trialed for performance, and if
    successful will replace the assistant guides we use on issues and the
    tour that releases uses.
    
    * First pass at implementing the tour modal.
    * Update src/sentry/static/sentry/app/components/modals/featureTourModal.tsx
    * Add tests for featureTourModal
    * Fix optional prop
    * Update based on review feedback, and fixes made during first usage.
    * Rework the API and fix close events not firing on background click
    * Fix action binding
    * Expand test coverage.
    
    Co-authored-by: Priscila Oliveira <priscilawebdev@gmail.com>

--- docs-ui/components/repoLabel.stories.js
+++ docs-ui/components/repoLabel.stories.js
@@ -4,7 +4,7 @@ import {withInfo} from '@storybook/addon-info';
 import RepoLabel from 'app/components/repoLabel';
 
 export default {
-  title: 'RepoLabel',
+  title: 'UI/RepoLabel',
 };
 
 export const Default = withInfo('A badge to use for repo names')(() => {
