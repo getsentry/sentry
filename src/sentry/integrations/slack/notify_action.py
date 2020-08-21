2d383036d134d7d8543f261a8670c0092998baef
meredith <meredith@sentry.io>
2020-08-21 10:55:04 -0700

    ref(slack): Allow users to pass in channel_id

--- src/sentry/integrations/slack/notify_action.py
+++ src/sentry/integrations/slack/notify_action.py
@@ -51,26 +51,36 @@ class SlackNotifyServiceForm(forms.Form):
         self._pending_save = False
 
     def clean(self):
+        channel_id = None
+        if self.data.get("channel_id"):
+            # default to "#" if they have the channel name without the prefix
+            channel_prefix = self.data["channel"][0] if self.data["channel"][0] == "@" else "#"
+            channel_id = self.data["channel_id"]
+
         cleaned_data = super(SlackNotifyServiceForm, self).clean()
 
         workspace = cleaned_data.get("workspace")
         channel = cleaned_data.get("channel", "")
 
-        try:
-            channel_prefix, channel_id, timed_out = self.channel_transformer(workspace, channel)
-        except DuplicateDisplayNameError as e:
-            integration = Integration.objects.get(id=workspace)
-            domain = integration.metadata["domain_name"]
+        # XXX(meredith): If the user is creating/updating a rule via the API and provides
+        # the channel_id in the request, we don't need to call the channel_transformer - we
+        # are assuming that they passed in the correct channel_id for the channel
+        if not channel_id:
+            try:
+                channel_prefix, channel_id, timed_out = self.channel_transformer(workspace, channel)
+            except DuplicateDisplayNameError as e:
+                integration = Integration.objects.get(id=workspace)
+                domain = integration.metadata["domain_name"]
 
-            params = {"channel": e.message, "domain": domain}
+                params = {"channel": e.message, "domain": domain}
 
-            raise forms.ValidationError(
-                _(
-                    'Multiple users were found with display name "%(channel)s". Please use your username, found at %(domain)s/account/settings.',
-                ),
-                code="invalid",
-                params=params,
-            )
+                raise forms.ValidationError(
+                    _(
+                        'Multiple users were found with display name "%(channel)s". Please use your username, found at %(domain)s/account/settings.',
+                    ),
+                    code="invalid",
+                    params=params,
+                )
 
         channel = strip_channel_name(channel)
 
