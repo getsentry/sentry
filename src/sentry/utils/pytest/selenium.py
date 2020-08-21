f7a4616f95933994bcc12d0087f470fc2982e083
Billy Vong <billy@sentry.io>
2020-08-21 16:08:11 -0700

    build(gha): Allow acceptance test snapshot dir to be configurable
    This allows configuration of the snapshot dir for acceptance tests. This will allow us to add more snapshot groups easily (e.g. diff resolutions, or diff media queries)

--- src/sentry/utils/pytest/selenium.py
+++ src/sentry/utils/pytest/selenium.py
@@ -315,19 +315,22 @@ class Browser(object):
             self.wait_for_images_loaded()
             self.wait_for_fonts_loaded()
 
+            snapshot_dir = os.environ.get(
+                "PYTEST_SNAPSHOTS_DIR", ".artifacts/visual-snapshots/acceptance"
+            )
             # Note: below will fail if these directories do not exist
 
             if not mobile_only:
                 # This will make sure we resize viewport height to fit contents
                 with self.full_viewport():
                     self.driver.find_element_by_tag_name("body").screenshot(
-                        u".artifacts/visual-snapshots/acceptance/{}.png".format(slugify(name))
+                        u"{}/{}.png".format(snapshot_dir, slugify(name))
                     )
 
             with self.mobile_viewport():
                 # switch to a mobile sized viewport
                 self.driver.find_element_by_tag_name("body").screenshot(
-                    u".artifacts/visual-snapshots/acceptance-mobile/{}.png".format(slugify(name))
+                    u"{}-mobile/{}.png".format(snapshot_dir, slugify(name))
                 )
 
         return self
