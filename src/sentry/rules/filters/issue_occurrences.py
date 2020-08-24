15abb5ed6d7f2e0b42df8579c38e3ca67be39d1c
David Wang <dwang@sentry.io>
2020-08-24 16:48:57 -0400

    ,

--- src/sentry/rules/filters/issue_occurrences.py
+++ src/sentry/rules/filters/issue_occurrences.py
@@ -23,7 +23,7 @@ class IssueOccurrencesFilter(EventFilter):
         except (TypeError, ValueError):
             return False
 
-        # This value is slightly delayed due to us batching writes to times_seen,
+        # This value is slightly delayed due to us batching writes to times_seen
         # which can cause the filter to be delayed or occasionally not work as expected.
         issue_occurrences = event.group.times_seen
         return issue_occurrences >= value
