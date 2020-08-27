dc16d386327ebba3da49086637772354d8bacb4d
Stephen Cefali <scefali@sentry.io>
2020-08-27 15:05:25 -0700

    adds S3 option for SQS

--- src/sentry_plugins/amazon_sqs/plugin.py
+++ src/sentry_plugins/amazon_sqs/plugin.py
@@ -29,6 +29,14 @@ def get_regions():
     return public_region_list + cn_region_list
 
 
+def map_reduced_sqs_payload(payload, url):
+    params_to_pass = ["eventID", "projectID", "message", "id", "size"]
+    new_payload = {"s3Url": url}
+    for param in params_to_pass:
+        new_payload[param] = payload[param]
+    return new_payload
+
+
 def track_response_metric(fn):
     # https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sqs.html#SQS.Queue.send_message
     # boto3's send_message doesn't return success/fail or http codes
@@ -78,9 +86,12 @@ class AmazonSQSPlugin(CorePluginMixin, DataForwardingPlugin):
                 "type": "select",
                 "choices": tuple((z, z) for z in get_regions()),
             },
-            get_secret_field_config(
-                name="access_key", label="Access Key", secret=self.get_option("access_key", project)
-            ),
+            {
+                "name": "access_key",
+                "label": "Access Key",
+                "type": "text",
+                "placeholder": "Access Key",
+            },
             get_secret_field_config(
                 name="secret_key", label="Secret Key", secret=self.get_option("secret_key", project)
             ),
@@ -90,6 +101,17 @@ class AmazonSQSPlugin(CorePluginMixin, DataForwardingPlugin):
                 "type": "text",
                 "required": False,
                 "placeholder": "Required for FIFO queues, exclude for standard queues",
+            },
+            {
+                "name": "s3_bucket",
+                "label": "S3 Bucket",
+                "type": "text",
+                "required": False,
+                "placeholder": "s3-bucket",
+                "help": (
+                    "Specify an S3 bucket to avoid events from being rejected because they are over the 256Kb limit for SQS."
+                    " The payload of the SQS item will contain a reference to the item in S3."
+                ),
             },
         ]
 
@@ -114,34 +136,50 @@ class AmazonSQSPlugin(CorePluginMixin, DataForwardingPlugin):
             logger.info("sentry_plugins.amazon_sqs.skip_unconfigured", extra=logging_params)
             return
 
-        # TODO(dcramer): Amazon doesnt support payloads larger than 256kb
-        # We could support this by simply trimming it and allowing upload
-        # to S3
-        message = json.dumps(payload)
-        if len(message) > 256 * 1024:
-            logger.info("sentry_plugins.amazon_sqs.skip_oversized", extra=logging_params)
-            return False
+        boto3_args = {
+            "aws_access_key_id": access_key,
+            "aws_secret_access_key": secret_key,
+            "region_name": region,
+        }
 
-        try:
-            client = boto3.client(
-                service_name="sqs",
-                aws_access_key_id=access_key,
-                aws_secret_access_key=secret_key,
-                region_name=region,
-            )
+        def s3_put_object(*args, **kwargs):
+            s3_client = boto3.client(service_name="s3", **boto3_args)
+            return s3_client.put_object(*args, **kwargs)
 
-            message = {"QueueUrl": queue_url, "MessageBody": message}
+        def sqs_send_message(message):
+            client = boto3.client(service_name="sqs", **boto3_args)
+            send_message_args = {"QueueUrl": queue_url, "MessageBody": message}
             # need a MessageGroupId for FIFO queues
             # note that if MessageGroupId is specified for non-FIFO, this will fail
             if message_group_id:
                 from uuid import uuid4
 
-                message["MessageGroupId"] = message_group_id
+                send_message_args["MessageGroupId"] = message_group_id
                 # if content based de-duplication is not enabled, we need to provide a
                 # MessageDeduplicationId
-                message["MessageDeduplicationId"] = uuid4().hex
-            logger.info("sentry_plugins.amazon_sqs.send_message", extra=logging_params)
-            client.send_message(**message)
+                send_message_args["MessageDeduplicationId"] = uuid4().hex
+            return client.send_message(**send_message_args)
+
+        # wrap S3 put_object and and SQS send message in one try/except
+        s3_bucket = self.get_option("s3_bucket", event.project)
+        try:
+            # if we have an S3 bucket, upload to S3
+            if s3_bucket:
+                date = event.datetime.strftime("%Y-%m-%d")
+                key = "{}/{}/{}".format(event.project.slug, date, event.event_id)
+                s3_put_object(Bucket=s3_bucket, Body=json.dumps(payload), Key=key)
+
+                url = u"https://{}.s3-{}.amazonaws.com/{}".format(s3_bucket, region, key)
+                # use a reduced payload for SQS now
+                payload = map_reduced_sqs_payload(payload, url)
+
+            message = json.dumps(payload)
+
+            if len(message) > 256 * 1024:
+                logger.info("sentry_plugins.amazon_sqs.skip_oversized", extra=logging_params)
+                return False
+
+            sqs_send_message(message)
         except ClientError as e:
             if six.text_type(e).startswith(
                 "An error occurred (InvalidClientTokenId)"
