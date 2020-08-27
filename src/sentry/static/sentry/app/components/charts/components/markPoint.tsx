8b6a15f3da54a0246f3bc3ce0c47fd541d8719c5
k-fish <kevan.fisher@sentry.io>
2020-08-27 14:44:08 -0700

    ref(ts): Convert more chart components to ts (#20448)
    This converts some more of the basic echart components over to typescript, there are a couple todos as the types aren't complete (at least for our version of echarts)

--- src/sentry/static/sentry/app/components/charts/components/markPoint.tsx
+++ src/sentry/static/sentry/app/components/charts/components/markPoint.tsx
@@ -1,11 +1,15 @@
 import 'echarts/lib/component/markPoint';
 
+import {EChartOption} from 'echarts';
+
 /**
  * eCharts markPoint
  *
  * See https://ecomfe.github.io/echarts-doc/public/en/option.html#series-line.markPoint
  */
-export default function MarkPoint(props) {
+export default function MarkPoint(
+  props: EChartOption.SeriesLine['markPoint']
+): EChartOption.SeriesLine['markPoint'] {
   return {
     ...props,
   };
