92a430269ea3112ffda8e4d0c4c2def4b0ee913f
Markus Unterwaditzer <markus@unterwaditzer.net>
2020-08-27 16:28:31 +0200

    Update src/sentry/event_manager.py
    Co-authored-by: Anton Ovchinnikov <anton@tonyo.info>

--- src/sentry/event_manager.py
+++ src/sentry/event_manager.py
@@ -1120,7 +1120,7 @@ def discard_event(job, attachments):
     metrics.incr(
         "events.discarded",
         skip_internal=True,
-        tags={"organization_id": project.organization_id, "platform": job["platform"]},
+        tags={"platform": job["platform"]},
     )
 
 
