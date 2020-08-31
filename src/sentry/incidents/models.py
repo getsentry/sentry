e888c2df442d0866eb3c04869b54743d53342991
Marcos Gaeta <31750075+mgaeta@users.noreply.github.com>
2020-08-31 14:59:27 -0700

    feat(integrations): Add Sentry apps to available-actions (#20263)

--- src/sentry/incidents/models.py
+++ src/sentry/incidents/models.py
@@ -539,6 +539,7 @@ class AlertRuleTriggerAction(Model):
         PAGERDUTY = 1
         SLACK = 2
         MSTEAMS = 3
+        SENTRY_APP = 4
 
     INTEGRATION_TYPES = frozenset((Type.PAGERDUTY.value, Type.SLACK.value, Type.MSTEAMS.value))
 
@@ -550,6 +551,8 @@ class AlertRuleTriggerAction(Model):
         # A specific team. This could be used to send an email to everyone associated
         # with a team.
         TEAM = 2
+        # A Sentry App instead of any of the above.
+        SENTRY_APP = 3
 
     TypeRegistration = namedtuple(
         "TypeRegistration",
