368979fda415c0b2c4d3d0e544b448e1b031d5e3
k-fish <kevan.fisher@sentry.io>
2020-09-01 10:23:49 -0700

    Only open tooltips that are not disabled and not in a list

--- src/sentry/static/sentry/app/stores/tooltipStore.tsx
+++ src/sentry/static/sentry/app/stores/tooltipStore.tsx
@@ -4,6 +4,7 @@ type TooltipStoreInterface = {
   openAllTooltips: () => boolean;
   closeAllTooltips: () => void;
   tooltips: React.Component[];
+  getOpenableSingleTooltips: () => React.Component[];
   init: () => TooltipStoreInterface;
 };
 
@@ -12,17 +13,24 @@ const MAX_TOOLTIPS_TO_OPEN = 100;
 const TooltipStore: TooltipStoreInterface = {
   tooltips: [],
 
+  getOpenableSingleTooltips() {
+    return this.tooltips.filter(tooltip => {
+      return (
+        !(tooltip.props as any).disabled && !(tooltip as any)._reactInternalFiber.key
+      );
+    });
+  },
+
   /**
    * Called via window.__openAllTooltips in selenium tests to check tooltip snapshots
    */
   openAllTooltips() {
-    if (!this.tooltips.length) {
+    const tooltips = this.getOpenableSingleTooltips();
+    if (!tooltips.length || tooltips.length > MAX_TOOLTIPS_TO_OPEN) {
+      // Pages with too many tooltip components will take too long to render and it isn't likely helpful anyway.
       return false;
     }
-    if (this.tooltips.length > MAX_TOOLTIPS_TO_OPEN) {
-      return false; // Pages with too many tooltip components will take too long to render and it isn't likely helpful anyway.
-    }
-    this.tooltips.forEach(tooltip => {
+    tooltips.forEach(tooltip => {
       tooltip.setState({
         isOpen: true,
         usesGlobalPortal: false,
@@ -35,7 +43,8 @@ const TooltipStore: TooltipStoreInterface = {
    * Called via window.__closeAllTooltips in selenium tests to cleanup tooltips after taking a snapshot
    */
   closeAllTooltips() {
-    this.tooltips.forEach(tooltip => {
+    const tooltips = this.getOpenableSingleTooltips();
+    tooltips.forEach(tooltip => {
       tooltip.setState({
         isOpen: false,
         usesGlobalPortal: true,
