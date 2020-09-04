ccafbb78dc0f2828e50adc3a229e71855542ff38
Billy Vong <billy@sentry.io>
2020-09-04 15:13:18 -0700

    convert to await/async

--- src/sentry/static/sentry/app/actionCreators/platformExternalIssues.tsx
+++ src/sentry/static/sentry/app/actionCreators/platformExternalIssues.tsx
@@ -1,24 +1,25 @@
 import {Client} from 'app/api';
 import PlatformExternalIssueActions from 'app/actions/platformExternalIssueActions';
 
-export function deleteExternalIssue(
+export async function deleteExternalIssue(
   api: Client,
   groupId: string,
   externalIssueId: string
 ) {
   PlatformExternalIssueActions.delete(groupId, externalIssueId);
 
-  return new Promise((resolve, reject) =>
-    api.request(`/issues/${groupId}/external-issues/${externalIssueId}/`, {
-      method: 'DELETE',
-      success: data => {
-        PlatformExternalIssueActions.deleteSuccess(data);
-        resolve(data);
-      },
-      error: error => {
-        PlatformExternalIssueActions.deleteError(error);
-        reject(error);
-      },
-    })
-  );
+  try {
+    const data = await api.requestPromise(
+      `/issues/${groupId}/external-issues/${externalIssueId}/`,
+      {
+        method: 'DELETE',
+      }
+    );
+
+    PlatformExternalIssueActions.deleteSuccess(data);
+    return data;
+  } catch (error) {
+    PlatformExternalIssueActions.deleteError(error);
+    throw error;
+  }
 }
