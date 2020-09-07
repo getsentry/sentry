22f9cc4127eb6bc5805f35270c33780b9276e08a
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-09-07 10:23:00 +0200

    wip

--- src/sentry/static/sentry/app/views/settings/components/settingsWrapper.tsx
+++ src/sentry/static/sentry/app/views/settings/components/settingsWrapper.tsx
@@ -1,17 +1,53 @@
 import React from 'react';
 import styled from '@emotion/styled';
 import {Location} from 'history';
+import PropTypes from 'prop-types';
 
 import ScrollToTop from 'app/views/settings/components/scrollToTop';
 import space from 'app/styles/space';
+import {Organization, Project} from 'app/types';
 
 type Props = {
   location: Location;
-  children: React.ReactNode;
+  organization?: Organization;
+  project?: Project;
 };
 
-const SettingsWrapper = ({children, ...props}: Props) => {
-  const handleShouldDisableScrollToTop = (location: Location, prevLocation: Location) => {
+type State = {
+  lastAppContext: 'project' | 'organization' | null;
+};
+
+class SettingsWrapper extends React.Component<Props, State> {
+  static childContextTypes = {
+    lastAppContext: PropTypes.oneOf(['project', 'organization']),
+  };
+
+  // save current context
+  state: State = {
+    lastAppContext: this.getLastAppContext(),
+  };
+
+  getChildContext() {
+    return {
+      lastAppContext: this.state.lastAppContext,
+    };
+  }
+
+  getLastAppContext() {
+    const {project, organization} = this.props;
+
+    if (!!project) {
+      return 'project';
+    }
+
+    if (!!organization) {
+      return 'organization';
+    }
+
+    return null;
+  }
+
+  handleShouldDisableScrollToTop = (location: Location, prevLocation: Location) => {
     // we do not want to scroll to top when user just perform a search
     return (
       location.pathname === prevLocation.pathname &&
@@ -19,14 +55,18 @@ const SettingsWrapper = ({children, ...props}: Props) => {
     );
   };
 
-  return (
-    <StyledSettingsWrapper>
-      <ScrollToTop location={props.location} disable={handleShouldDisableScrollToTop}>
-        {children}
-      </ScrollToTop>
-    </StyledSettingsWrapper>
-  );
-};
+  render() {
+    const {location, children} = this.props;
+
+    return (
+      <StyledSettingsWrapper>
+        <ScrollToTop location={location} disable={this.handleShouldDisableScrollToTop}>
+          {children}
+        </ScrollToTop>
+      </StyledSettingsWrapper>
+    );
+  }
+}
 
 export default SettingsWrapper;
 
