13291fba5904ea8f6c5355f798bd808838101f95
Scott Cooper <scttcper@gmail.com>
2020-09-08 13:47:27 -0700

    remove feature flag for demo

--- src/sentry/static/sentry/app/views/settings/incidentRules/triggers/chart/index.tsx
+++ src/sentry/static/sentry/app/views/settings/incidentRules/triggers/chart/index.tsx
@@ -7,7 +7,6 @@ import {t} from 'app/locale';
 import {Organization, Project} from 'app/types';
 import {SeriesDataUnit} from 'app/types/echarts';
 import {Panel, PanelBody} from 'app/components/panels';
-import Feature from 'app/components/acl/feature';
 import EventsRequest from 'app/components/charts/eventsRequest';
 import LoadingMask from 'app/components/loadingMask';
 import Placeholder from 'app/components/placeholder';
@@ -137,31 +136,28 @@ class TriggersChart extends React.PureComponent<Props> {
             <StickyWrapper>
               <StyledPanel>
                 <PanelBody withPadding>
-                  {/* TODO(scttcper): pick a feature flag name */}
-                  <Feature features={['discover-basic']} organization={organization}>
-                    <StyledSelectField
-                      inline={false}
-                      styles={{
-                        control: provided => ({
-                          ...provided,
-                          minHeight: '25px',
-                          height: '25px',
-                        }),
-                      }}
-                      isSearchable={false}
-                      isClearable={false}
-                      disabled={loading || reloading}
-                      name="statsPeriod"
-                      value={period}
-                      required
-                      flexibleControlStateSize
-                      choices={statsPeriodOptions.map(timePeriod => [
-                        timePeriod,
-                        TIME_PERIOD_MAP[timePeriod],
-                      ])}
-                      onChange={this.handleStatsPeriodChange}
-                    />
-                  </Feature>
+                  <StyledSelectField
+                    inline={false}
+                    styles={{
+                      control: provided => ({
+                        ...provided,
+                        minHeight: '25px',
+                        height: '25px',
+                      }),
+                    }}
+                    isSearchable={false}
+                    isClearable={false}
+                    disabled={loading || reloading}
+                    name="statsPeriod"
+                    value={period}
+                    required
+                    flexibleControlStateSize
+                    choices={statsPeriodOptions.map(timePeriod => [
+                      timePeriod,
+                      TIME_PERIOD_MAP[timePeriod],
+                    ])}
+                    onChange={this.handleStatsPeriodChange}
+                  />
 
                   {loading || reloading ? (
                     <ChartPlaceholder />
