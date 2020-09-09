60d641c1c4d28bce22e93ef5ed0ec7283a4d59e9
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-09-09 11:38:36 -0700

    ref(py3): Handle decoding from redis in tsdb (#20676)

--- tests/sentry/tsdb/test_redis.py
+++ tests/sentry/tsdb/test_redis.py
@@ -690,11 +690,11 @@ class RedisTSDBTest(TestCase):
         assert client.exists("1:e")
 
         assert CountMinScript(["1:i", "1:e"], ["RANKED"] + parameters, client=client) == [
-            ["iota", "9"],
-            ["theta", "8"],
-            ["eta", "7"],
-            ["zeta", "6"],
-            ["foxtrot", "6"],
+            [b"iota", b"9"],
+            [b"theta", b"8"],
+            [b"eta", b"7"],
+            [b"zeta", b"6"],
+            [b"foxtrot", b"6"],
         ]
 
     def test_frequency_table_import_export_source_estimators(self):
@@ -748,11 +748,11 @@ class RedisTSDBTest(TestCase):
         assert client.exists("1:e")
 
         assert CountMinScript(["1:i", "1:e"], ["RANKED"] + parameters, client=client) == [
-            ["iota", "9"],
-            ["baz", "9"],
-            ["theta", "8"],
-            ["eta", "7"],
-            ["bar", "7"],
+            [b"iota", b"9"],
+            [b"baz", b"9"],
+            [b"theta", b"8"],
+            [b"eta", b"7"],
+            [b"bar", b"7"],
         ]
 
     def test_frequency_table_import_export_destination_estimators(self):
@@ -806,9 +806,9 @@ class RedisTSDBTest(TestCase):
         assert client.exists("1:e")
 
         assert CountMinScript(["1:i", "1:e"], ["RANKED"] + parameters, client=client) == [
-            ["iota", "9"],
-            ["baz", "9"],
-            ["theta", "8"],
-            ["eta", "7"],
-            ["bar", "7"],
+            [b"iota", b"9"],
+            [b"baz", b"9"],
+            [b"theta", b"8"],
+            [b"eta", b"7"],
+            [b"bar", b"7"],
         ]
