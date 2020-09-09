229c0bb45101522ea5e9fa43dd6220e7b1edc76c
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-09-09 09:49:55 +0200

    added 1px

--- src/sentry/static/sentry/app/components/events/eventDataSection.tsx
+++ src/sentry/static/sentry/app/components/events/eventDataSection.tsx
@@ -133,7 +133,7 @@ const SectionHeader = styled('div')<{isCentered?: boolean}>`
   display: flex;
   flex-wrap: wrap;
   align-items: center;
-  margin-bottom: ${space(2)};
+  margin-bottom: ${space(1)};
 
   > * {
     margin-bottom: ${space(0.5)};
