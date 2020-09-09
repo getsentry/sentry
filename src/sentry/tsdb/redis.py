fbe9d3b0d6934282f9443f2d201b6df25391d885
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-09-09 11:38:36 -0700

    ref(py3): Handle decoding from redis in tsdb (#20676)

--- src/sentry/tsdb/redis.py
+++ src/sentry/tsdb/redis.py
@@ -11,6 +11,7 @@ from hashlib import md5
 
 import six
 from django.utils import timezone
+from django.utils.encoding import force_bytes
 from pkg_resources import resource_string
 
 from sentry.tsdb.base import BaseTSDB
@@ -173,9 +174,7 @@ class RedisTSDB(BaseTSDB):
         if isinstance(model_key, six.integer_types):
             vnode = model_key % self.vnodes
         else:
-            if isinstance(model_key, six.text_type):
-                model_key = model_key.encode("utf-8")
-            vnode = crc32(model_key) % self.vnodes
+            vnode = crc32(force_bytes(model_key)) % self.vnodes
 
         return (
             u"{prefix}{model}:{epoch}:{vnode}".format(
@@ -727,7 +726,9 @@ class RedisTSDB(BaseTSDB):
         results = {}
         cluster, _ = self.get_cluster(environment_id)
         for key, responses in cluster.execute_commands(commands).items():
-            results[key] = [(member, float(score)) for member, score in responses[0].value]
+            results[key] = [
+                (member.decode("utf-8"), float(score)) for member, score in responses[0].value
+            ]
 
         return results
 
@@ -757,7 +758,7 @@ class RedisTSDB(BaseTSDB):
             ]
 
         def unpack_response(response):
-            return {item: float(score) for item, score in response.value}
+            return {item.decode("utf-8"): float(score) for item, score in response.value}
 
         results = {}
         cluster, _ = self.get_cluster(environment_id)
