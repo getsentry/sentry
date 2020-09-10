85a7cc5783906c79b9b168b95c880f65e8065459
Scott Cooper <scttcper@gmail.com>
2020-09-10 09:07:58 -0700

    make environment optional

--- src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/index.tsx
+++ src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/index.tsx
@@ -212,7 +212,7 @@ class IssueRuleEditor extends AsyncView<Props, State> {
     const endpoint = `/projects/${organization.slug}/${project.slug}/rules/${ruleId}`;
 
     if (rule && rule.environment === ALL_ENVIRONMENTS_KEY) {
-      delete (rule as any).environment;
+      delete rule.environment;
     }
 
     addLoadingMessage();
