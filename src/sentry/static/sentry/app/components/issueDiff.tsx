6cad4eedb8dda4020686b2acfec4c728280c601b
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2020-09-15 14:12:44 +0200

    convert issuediff to typescript

--- src/sentry/static/sentry/app/components/issueDiff.tsx
+++ src/sentry/static/sentry/app/components/issueDiff.tsx
@@ -1,41 +1,53 @@
 import {css} from '@emotion/core';
-import PropTypes from 'prop-types';
 import React from 'react';
 import isPropValid from '@emotion/is-prop-valid';
 import styled from '@emotion/styled';
 
+import {Client} from 'app/api';
 import {addErrorMessage} from 'app/actionCreators/indicator';
 import {t} from 'app/locale';
 import withApi from 'app/utils/withApi';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import getStacktraceBody from 'app/utils/getStacktraceBody';
+import SplitDiff from 'app/components/splitDiff';
 
-class IssueDiff extends React.Component {
-  static propTypes = {
-    api: PropTypes.object,
-    baseIssueId: PropTypes.string.isRequired,
-    targetIssueId: PropTypes.string.isRequired,
-    baseEventId: PropTypes.string.isRequired,
-    targetEventId: PropTypes.string.isRequired,
-    orgId: PropTypes.string.isRequired,
-    projectId: PropTypes.string.isRequired,
-  };
+type Props = {
+  api: Client;
+  baseIssueId: string;
+  targetIssueId: string;
 
+  orgId: string;
+  projectId: string;
+
+  baseEventId?: string;
+  targetEventId?: string;
+  className?: string;
+};
+
+type State = {
+  loading: boolean;
+  baseEvent: Array<string>;
+  targetEvent: Array<string>;
+  SplitDiffAsync?: typeof SplitDiff;
+};
+
+class IssueDiff extends React.Component<Props, State> {
   static defaultProps = {
     baseEventId: 'latest',
     targetEventId: 'latest',
   };
 
-  constructor(...args) {
-    super(...args);
+  constructor(props: Props) {
+    super(props);
+
     this.state = {
       loading: true,
-      baseEvent: {},
-      targetEvent: {},
+      baseEvent: [],
+      targetEvent: [],
 
       // `SplitDiffAsync` is an async-loaded component
       // This will eventually contain a reference to the exported component from `./splitDiff`
-      SplitDiffAsync: null,
+      SplitDiffAsync: undefined,
     };
   }
 
@@ -45,8 +57,8 @@ class IssueDiff extends React.Component {
     // Fetch component and event data
     Promise.all([
       import(/* webpackChunkName: "splitDiff" */ './splitDiff'),
-      this.fetchData(baseIssueId, baseEventId),
-      this.fetchData(targetIssueId, targetEventId),
+      this.fetchData(baseIssueId, baseEventId || 'latest'),
+      this.fetchData(targetIssueId, targetEventId || 'latest'),
     ])
       .then(([{default: SplitDiffAsync}, baseEvent, targetEvent]) => {
         this.setState({
@@ -61,7 +73,7 @@ class IssueDiff extends React.Component {
       });
   }
 
-  getEndpoint(issueId, eventId) {
+  getEndpoint(issueId: string, eventId: string) {
     const {orgId, projectId} = this.props;
 
     if (eventId !== 'latest') {
@@ -71,19 +83,19 @@ class IssueDiff extends React.Component {
     return `/issues/${issueId}/events/${eventId}/`;
   }
 
-  fetchData(issueId, eventId) {
+  fetchData(issueId: string, eventId: string) {
     return this.props.api.requestPromise(this.getEndpoint(issueId, eventId));
   }
 
   render() {
     const {className} = this.props;
     const DiffComponent = this.state.SplitDiffAsync;
-    const diffReady = !this.state.loading && !!DiffComponent;
 
     return (
       <StyledIssueDiff className={className} loading={this.state.loading}>
         {this.state.loading && <LoadingIndicator />}
-        {diffReady &&
+        {!this.state.loading &&
+          DiffComponent &&
           this.state.baseEvent.map((value, i) => (
             <DiffComponent
               key={i}
@@ -108,9 +120,13 @@ const getLoadingStyle = p =>
     `) ||
   '';
 
+type StyledIssueDiffProps = {
+  loading: boolean;
+};
+
 const StyledIssueDiff = styled('div', {
   shouldForwardProp: p => isPropValid(p) && p !== 'loading',
-})`
+})<StyledIssueDiffProps>`
   background-color: #f7f8f9;
   overflow: auto;
   padding: 10px;
