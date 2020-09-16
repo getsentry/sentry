9f340f5e439e8992baaab4d5b61cafb864d590b4
k-fish <kevan.fisher@sentry.io>
2020-09-16 16:13:39 -0700

    Rename internals variable

--- src/sentry/static/sentry/app/stores/tooltipStore.tsx
+++ src/sentry/static/sentry/app/stores/tooltipStore.tsx
@@ -16,11 +16,11 @@ const TooltipStore: TooltipStoreInterface = {
   getOpenableSingleTooltips() {
     return this.tooltips.filter(tooltip => {
       // Filtering out disabled tooltips and lists of tooltips (which cause rendering issues for snapshots) using the internal 'key'
-      const internals =
+      const _internals =
         (tooltip as any)._reactInternalFiber || (tooltip as any)._reactInternals;
       return (
         !(tooltip.props as any).disabled &&
-        !internals.key &&
+        !_internals.key &&
         !(tooltip.props as any).disableForVisualTest
       );
     });
