436defeffa0a9f8337db415f37ffa7c6411010c8
meredith <meredith@sentry.io>
2020-09-23 12:28:29 -0700

    just use cli arguments

--- api-docs/index.js
+++ api-docs/index.js
@@ -84,7 +84,7 @@ let targetDirValue;
 const argv = process.argv.slice(2);
 
 originalFile = argv[0];
-const derefedFile = process.env.DEREFED_JSON_FILE || 'tests/apidocs/openapi-derefed.json';
+const derefedFile = argv[1] || 'tests/apidocs/openapi-derefed.json';
 
 try {
   if (!path.isAbsolute(originalFile)) {
