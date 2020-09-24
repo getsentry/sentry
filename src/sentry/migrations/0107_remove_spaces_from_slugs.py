865261f8c576f259c008384876510600cac60715
Marcos Gaeta <marcos.gaeta@sentry.io>
2020-09-24 10:22:25 -0700

    Use python instead of SQL

--- src/sentry/migrations/0107_remove_spaces_from_slugs.py
+++ src/sentry/migrations/0107_remove_spaces_from_slugs.py
@@ -1,7 +1,19 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.db import migrations
+from django.db import migrations, transaction
+
+
+def remove_trailing_spaces(apps, schema_editor):
+    """
+    There are currently only two organizations with trailing spaces so we're
+    updating them with python. Using SQL would lock the table for too long.
+    """
+    Organization = apps.get_model("sentry", "Organization")
+
+    for organization in Organization.objects.filter(slug__endswith=" "):
+        organization.slug = organization.slug.strip()
+        organization.save()
 
 
 class Migration(migrations.Migration):
@@ -20,12 +32,12 @@ class Migration(migrations.Migration):
     # By default we prefer to run in a transaction, but for migrations where you want
     # to `CREATE INDEX CONCURRENTLY` this needs to be set to False. Typically you'll
     # want to create an index concurrently when adding one to an existing table.
-    atomic = True
+    atomic = False
 
     dependencies = [("sentry", "0106_service_hook_project_id_nullable")]
 
     operations = [
-        migrations.RunSQL(
-            "UPDATE TABLE sentry_organization SET name=TRIM(slug) WHERE slug like '% '"
+        migrations.RunPython(
+            remove_trailing_spaces, reverse_code=migrations.RunPython.noop
         )
     ]
