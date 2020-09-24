a298a5e34bb2c12b48b11e1411386601d75c6a03
Stephen Cefali <scefali@sentry.io>
2020-09-24 16:51:39 -0600

    change x-frame-options header

--- src/sentry/integrations/jira/ui_hook.py
+++ src/sentry/integrations/jira/ui_hook.py
@@ -19,9 +19,9 @@ class JiraUiHookView(View):
     def get_response(self, context):
         context["ac_js_src"] = "https://connect-cdn.atl-paas.net/all.js"
         res = render_to_response("sentry/integrations/jira-config.html", context, self.request)
-        # X-Frame-Options is a deprecated for content-securyity policy
-        # but omitting this field breaks could break in certain browsers because we will set it to deny elsewhere
-        res["X-Frame-Options"] = "ALLOW-FROM %s" % self.request.GET["xdm_e"]
+        # we aren't actually displaying it on the same page but we don't want to set it to deny
+        # which security.py will do
+        res["X-Frame-Options"] = "SAMEORIGIN"
         res["Content-Security-Policy"] = u"frame-ancestors %s" % self.request.GET["xdm_e"]
         return res
 
