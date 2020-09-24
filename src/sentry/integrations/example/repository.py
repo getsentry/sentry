7bb181d84ea4fba0214f7998d855e0e7f65931f4
Stephen Cefali <scefali@sentry.io>
2020-09-24 12:31:25 -0600

    fix(integrations): handle floating repository (#20964)

--- src/sentry/integrations/example/repository.py
+++ src/sentry/integrations/example/repository.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import logging
+
 from sentry.plugins import providers
 from sentry.models import Integration
 
@@ -8,6 +10,7 @@ from sentry.shared_integrations.exceptions import IntegrationError
 
 class ExampleRepositoryProvider(providers.IntegrationRepositoryProvider):
     name = "Example"
+    logger = logging.getLogger("sentry.integrations.example")
 
     def compare_commits(self, repo, start_sha, end_sha):
         installation = Integration.objects.get(id=repo.integration_id).get_installation(
