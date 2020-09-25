ef3bf22051a1249e11608c4a3abd62fae8d62d9a
Scott Cooper <scttcper@gmail.com>
2020-09-24 17:23:02 -0700

    add tab nav thirds story

--- docs-ui/components/layout-thirds.stories.js
+++ docs-ui/components/layout-thirds.stories.js
@@ -5,6 +5,8 @@ import {withInfo} from '@storybook/addon-info';
 import Button from 'app/components/button';
 import ButtonBar from 'app/components/buttonBar';
 import Breadcrumbs from 'app/components/breadcrumbs';
+import NavTabs from 'app/components/navTabs';
+import Link from 'app/components/links/link';
 import * as Layout from 'app/components/layouts/thirds';
 import space from 'app/styles/space';
 
@@ -154,6 +156,47 @@ SingleColumnMode.story = {
   name: 'single column mode',
 };
 
+export const _6633WithTabNavigation = withInfo('Two column layout with tab navigation')(
+  () => (
+    <Container>
+      <BorderlessHeader>
+        <Layout.HeaderContent>
+          <Layout.Title>Alerts</Layout.Title>
+        </Layout.HeaderContent>
+        <Layout.HeaderActions>
+          <ButtonBar gap={1}>
+            <Button size="small">clicker</Button>
+            <Button size="small">clicker</Button>
+          </ButtonBar>
+        </Layout.HeaderActions>
+      </BorderlessHeader>
+      <TabLayoutHeader>
+        <StyledNavTabs underlined>
+          <li className="active">
+            <Link to="#">Active</Link>
+          </li>
+          <li>
+            <Link to="#">Inactive</Link>
+          </li>
+        </StyledNavTabs>
+      </TabLayoutHeader>
+      <Layout.Body>
+        <Layout.Main>
+          <h1>Content Region</h1>
+          <p>Some text here</p>
+        </Layout.Main>
+        <Layout.Side>
+          <h3>Sidebar content</h3>
+        </Layout.Side>
+      </Layout.Body>
+    </Container>
+  )
+);
+
+_6633WithTabNavigation.story = {
+  name: '66/33 with tab based nav',
+};
+
 const Container = styled('div')`
   background: ${p => p.theme.gray200};
   margin: ${space(2)};
@@ -162,4 +205,27 @@ const Container = styled('div')`
 
 const MarginedButtonBar = styled(ButtonBar)`
   margin-bottom: ${space(1)};
+`;
+
+const StyledNavTabs = styled(NavTabs)`
+  margin: 0;
+  border-bottom: 0 !important;
+  li {
+    margin-right: ${space(0.5)};
+  }
+  li > a {
+    padding: ${space(1)} ${space(2)};
+  }
+`;
+
+const BorderlessHeader = styled(Layout.Header)`
+  border-bottom: 0;
+`;
+
+const TabLayoutHeader = styled(Layout.Header)`
+  padding-top: 0;
+
+  @media (max-width: ${p => p.theme.breakpoints[1]}) {
+    padding-top: 0;
+  }
 `;
