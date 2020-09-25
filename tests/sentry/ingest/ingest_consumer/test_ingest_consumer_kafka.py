107396002ca7cadb85bcff82bef597edaf933a49
Anton Ovchinnikov <anton@tonyo.info>
2020-09-25 15:46:31 +0200

    Add another test

--- tests/sentry/ingest/ingest_consumer/test_ingest_consumer_kafka.py
+++ tests/sentry/ingest/ingest_consumer/test_ingest_consumer_kafka.py
@@ -7,7 +7,9 @@ import msgpack
 import pytest
 import random
 
+from confluent_kafka import KafkaError
 from django.conf import settings
+from django.test import override_settings
 
 from sentry import eventstore
 from sentry.event_manager import EventManager
@@ -87,13 +89,14 @@ def test_ingest_consumer_reads_from_topic_and_calls_celery_task(
         producer.produce(topic_event_name, message)
         event_ids.add(event_id)
 
-    consumer = get_ingest_consumer(
-        max_batch_size=2,
-        max_batch_time=5000,
-        group_id=group_id,
-        consumer_types=set([ConsumerType.Events]),
-        auto_offset_reset="earliest",
-    )
+    with override_settings(KAFKA_CONSUMER_AUTO_CREATE_TOPICS=True):
+        consumer = get_ingest_consumer(
+            max_batch_size=2,
+            max_batch_time=5000,
+            group_id=group_id,
+            consumer_types=set([ConsumerType.Events]),
+            auto_offset_reset="earliest",
+        )
 
     with task_runner():
         i = 0
@@ -114,3 +117,28 @@ def test_ingest_consumer_reads_from_topic_and_calls_celery_task(
             assert message.data["contexts"]["trace"]
         else:
             assert message.data["extra"]["the_id"] == event_id
+
+
+def test_ingest_consumer_fails_when_not_autocreating_topics(
+    kafka_admin, requires_kafka,
+):
+    group_id = "test-consumer-{}".format(random.randint(0, 2 ** 16))
+    topic_event_name = ConsumerType.get_topic_name(ConsumerType.Events)
+
+    admin = kafka_admin(settings)
+    admin.delete_topic(topic_event_name)
+
+    with override_settings(KAFKA_CONSUMER_AUTO_CREATE_TOPICS=False):
+        consumer = get_ingest_consumer(
+            max_batch_size=2,
+            max_batch_time=5000,
+            group_id=group_id,
+            consumer_types=set([ConsumerType.Events]),
+            auto_offset_reset="earliest",
+        )
+
+    with pytest.raises(Exception) as err:
+        consumer._run_once()
+
+    kafka_error = err.value.args[0]
+    assert kafka_error.code() == KafkaError.UNKNOWN_TOPIC_OR_PART
