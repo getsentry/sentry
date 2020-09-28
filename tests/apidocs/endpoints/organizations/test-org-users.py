209d3c5f9532c6157a114b44fda168d20015f7f1
Colleen O'Rourke <colleen@sentry.io>
2020-09-28 15:01:47 -0700

    feat(api-docs): Add tests for Organization endpoints

---
+++ tests/apidocs/endpoints/organizations/test-org-users.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+
+from django.core.urlresolvers import reverse
+from django.test.client import RequestFactory
+
+from tests.apidocs.util import APIDocsTestCase
+
+
+class OrganizationUsersDocs(APIDocsTestCase):
+    def setUp(self):
+        organization = self.create_organization()
+
+        self.url = reverse(
+            "sentry-api-0-organization-users", kwargs={"organization_slug": organization.slug},
+        )
+
+        self.login_as(user=self.user)
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        request = RequestFactory().get(self.url)
+
+        self.validate_schema(request, response)
