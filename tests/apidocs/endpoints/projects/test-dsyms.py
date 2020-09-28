9aa3c0ba40895f47910d0fa9c9ad937a082efeff
Colleen O'Rourke <colleen@sentry.io>
2020-09-28 12:56:25 -0700

    Fix the file upload to actually use a file

--- tests/apidocs/endpoints/projects/test-dsyms.py
+++ tests/apidocs/endpoints/projects/test-dsyms.py
@@ -26,8 +26,12 @@ class ProjectDsymsDocs(APIDocsTestCase):
         self.validate_schema(request, response)
 
     def test_post(self):
-        data = {"file": "debug.zip"}
-        response = self.client.post(self.url, data)
+
+        dif = self.create_dif_file(
+            debug_id="dfb8e43a-f242-3d73-a453-aeb6a777ef75", features=["debug", "unwind"]
+        )
+        data = {"file": dif.file}
+        response = self.client.post(self.url, data, content_type="multipart/form-data")
         request = RequestFactory().post(self.url, data)
 
         self.validate_schema(request, response)
