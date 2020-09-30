296044ec34c628fb2fc215dee8418a8ad779bf1e
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-09-30 13:18:17 +0200

    wip

--- src/sentry/static/sentry/app/views/settings/organizationRelay/modals/add/item.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationRelay/modals/add/item.tsx
@@ -8,44 +8,19 @@ type Props = {
   title: React.ReactNode;
   content: React.ReactElement;
   subtitle?: React.ReactNode;
-  /**
-   * The position of the step.
-   * The prop defaults to the value inherited from the parent Steps component.
-   */
-  index?: number;
 };
 
-const Item = ({title, subtitle, content, index}: Props) => (
+const Item = ({title, subtitle, content}: Props) => (
   <StyledListItem>
-    {index !== undefined && <Bullet>{index}</Bullet>}
-    <Content>
-      <div>{title}</div>
-      {subtitle && <small>{subtitle}</small>}
-      {content}
-    </Content>
+    {title}
+    {subtitle && <small>{subtitle}</small>}
+    {content}
   </StyledListItem>
 );
 
 export default Item;
 
 const StyledListItem = styled(ListItem)`
-  display: grid;
-  grid-template-columns: max-content 1fr;
-  grid-gap: ${space(1.5)};
-`;
-
-const Bullet = styled('div')`
-  width: 24px;
-  height: 24px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  border-radius: 50%;
-  background: ${p => p.theme.yellow400};
-  font-size: ${p => p.theme.fontSizeSmall};
-`;
-
-const Content = styled('div')`
   display: grid;
   grid-gap: ${space(1)};
 `;
