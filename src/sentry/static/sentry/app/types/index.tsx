9df08d358b5bf038634ab9629443794ee7fbf02e
Priscila Oliveira <priscilawebdev@gmail.com>
2020-10-02 18:03:52 +0200

    ref(relay): Add new design - Part 3 (#21090)

--- src/sentry/static/sentry/app/types/index.tsx
+++ src/sentry/static/sentry/app/types/index.tsx
@@ -114,6 +114,23 @@ export type Relay = {
   description?: string;
 };
 
+export type RelayActivity = {
+  publicKey: string;
+  relayId: string;
+  version: string;
+  firstSeen: string;
+  lastSeen: string;
+};
+
+export type RelaysByPublickey = {
+  [publicKey: string]: {
+    name: string;
+    activities: Array<RelayActivity>;
+    description?: string;
+    created?: string;
+  };
+};
+
 /**
  * Detailed organization (e.g. when requesting details for a single org)
  *
