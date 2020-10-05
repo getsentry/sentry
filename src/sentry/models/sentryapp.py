8b566583d51dd916f8395e13c1d88fd0704bb8ba
Stephen Cefali <scefali@sentry.io>
2020-10-05 13:41:54 -0700

    update migration

--- src/sentry/models/sentryapp.py
+++ src/sentry/models/sentryapp.py
@@ -119,7 +119,7 @@ class SentryApp(ParanoidModel, HasApiScopes):
     date_updated = models.DateTimeField(default=timezone.now)
     date_published = models.DateTimeField(null=True, blank=True)
 
-    creator_user_id = models.IntegerField(null=True)
+    creator_user = FlexibleForeignKey("sentry.User", null=True, on_delete=models.SET_NULL)
     creator_label = models.TextField(null=True)
 
     class Meta:
