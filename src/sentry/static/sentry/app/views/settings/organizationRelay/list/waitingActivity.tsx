ec3d555b168bb1d002d4638b04483f904153eae0
Priscila Oliveira <priscilawebdev@gmail.com>
2020-10-05 15:51:01 +0200

    ref(commandLine): Add CommandLine component (#21134)

--- src/sentry/static/sentry/app/views/settings/organizationRelay/list/waitingActivity.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationRelay/list/waitingActivity.tsx
@@ -4,9 +4,8 @@ import {t, tct} from 'app/locale';
 import {Panel} from 'app/components/panels';
 import Button from 'app/components/button';
 import EmptyMessage from 'app/views/settings/components/emptyMessage';
+import CommandLine from 'app/components/commandLine';
 import {IconRefresh} from 'app/icons';
-
-import Code from './code';
 
 type Props = {
   onRefresh: () => void;
@@ -16,8 +15,8 @@ const WaitingActivity = ({onRefresh}: Props) => (
   <Panel>
     <EmptyMessage
       title={t('Waiting on Activity!')}
-      description={tct('Run relay in your terminal with [command]', {
-        command: <Code>{'relay run'}</Code>,
+      description={tct('Run relay in your terminal with [commandLine]', {
+        commandLine: <CommandLine>{'relay run'}</CommandLine>,
       })}
       action={
         <Button icon={<IconRefresh />} onClick={onRefresh}>
