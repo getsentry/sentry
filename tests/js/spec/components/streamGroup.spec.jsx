88abe1c1479f924dc9cf354e5de0094ab2f9b2c3
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2020-10-06 17:03:33 -0700

    feat(workflow): Dynamic counts guide anchor for external release (#21160)
    * add guide anchor to feature
    
    * revert to use DEFAULT_GRAPH_STATS_PERIOD
    
    * style(): Auto eslint fix
    
    * style(): Auto eslint fix
    
    * validate auto
    
    * fix the backend guide
    
    * change id to 7
    
    * steps test not needed
    
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- tests/js/spec/components/streamGroup.spec.jsx
+++ tests/js/spec/components/streamGroup.spec.jsx
@@ -39,7 +39,7 @@ describe('StreamGroup', function () {
     );
 
     expect(component.find('GuideAnchor').exists()).toBe(true);
-    expect(component.find('GuideAnchor')).toHaveLength(2);
+    expect(component.find('GuideAnchor')).toHaveLength(3);
     expect(component).toSnapshot();
   });
 });
