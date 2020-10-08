0929129f13c930303b699a4a0bf77d87921078ea
Tony <Zylphrex@users.noreply.github.com>
2020-10-08 17:07:40 -0400

    feat(rum): Add button to allow the user to reset the zoom (#21229)
    Currently, users can zoom into the histogram by drag zooming. But there is no
    way of zooming back out. This change adds a reset view button to allow users
    to reset their zoom level back to the default.

--- src/sentry/static/sentry/app/views/performance/realUserMonitoring/content.tsx
+++ src/sentry/static/sentry/app/views/performance/realUserMonitoring/content.tsx
@@ -3,9 +3,11 @@ import {Location} from 'history';
 import {browserHistory} from 'react-router';
 import styled from '@emotion/styled';
 
+import Button from 'app/components/button';
 import CreateAlertButton from 'app/components/createAlertButton';
 import * as Layout from 'app/components/layouts/thirds';
 import {getParams} from 'app/components/organizations/globalSelectionHeader/getParams';
+import {t} from 'app/locale';
 import space from 'app/styles/space';
 import {Organization, Project} from 'app/types';
 import EventView from 'app/utils/discover/eventView';
@@ -58,10 +60,31 @@ class RumContent extends React.Component<Props, State> {
     this.setState({incompatibleAlertNotice});
   };
 
+  handleResetView = () => {
+    const {location} = this.props;
+
+    const queryParams = getParams({
+      ...(location.query || {}),
+    });
+
+    // reset the zoom parameters
+    delete queryParams.startMeasurements;
+    delete queryParams.endMeasurements;
+
+    browserHistory.push({
+      pathname: location.pathname,
+      query: queryParams,
+    });
+  };
+
   render() {
     const {transactionName, location, eventView, projects, organization} = this.props;
     const {incompatibleAlertNotice} = this.state;
     const query = decodeScalar(location.query.query) || '';
+
+    const isZoomed =
+      decodeScalar(location.query.startMeasurements) !== undefined ||
+      decodeScalar(location.query.endMeasurements) !== undefined;
 
     return (
       <React.Fragment>
@@ -79,13 +102,22 @@ class RumContent extends React.Component<Props, State> {
             <Layout.Main fullWidth>{incompatibleAlertNotice}</Layout.Main>
           )}
           <Layout.Main fullWidth>
-            <StyledSearchBar
-              organization={organization}
-              projectIds={eventView.project}
-              query={query}
-              fields={eventView.fields}
-              onSearch={this.handleSearch}
-            />
+            <StyledActions>
+              <StyledSearchBar
+                organization={organization}
+                projectIds={eventView.project}
+                query={query}
+                fields={eventView.fields}
+                onSearch={this.handleSearch}
+              />
+              <Button
+                onClick={this.handleResetView}
+                disabled={!isZoomed}
+                data-test-id="reset-view"
+              >
+                {t('Reset View')}
+              </Button>
+            </StyledActions>
             <TransactionVitals
               organization={organization}
               location={location}
@@ -99,6 +131,14 @@ class RumContent extends React.Component<Props, State> {
 }
 
 const StyledSearchBar = styled(SearchBar)`
+  flex-grow: 1;
+`;
+
+const StyledActions = styled('div')`
+  display: grid;
+  grid-gap: ${space(2)};
+  grid-template-columns: auto max-content;
+  align-items: center;
   margin-bottom: ${space(3)};
 `;
 
