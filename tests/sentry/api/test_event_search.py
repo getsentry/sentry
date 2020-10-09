a5858eb449f09bcdde74e1c05724bdc00a617af7
Tony <Zylphrex@users.noreply.github.com>
2020-10-09 11:14:10 -0400

    feat(discover): Allow measurements in Pxx functions (#21230)
    * feat/allow-measurements-in-pxx-functions
    
    The predefined Pxx functions is currently hard coded for transaction.duration
    but there is no reason why it can't work for any numeric column. This change
    adds support for any numeric column in these predefined Pxx functions while
    leaving the default as transaction.duration for backwards compatibility.
    
    * fix tests
    
    * added tests
    
    * remove currying

--- tests/sentry/api/test_event_search.py
+++ tests/sentry/api/test_event_search.py
@@ -2469,6 +2469,36 @@ class ResolveFieldListTest(unittest.TestCase):
             ],
         ]
 
+    def test_percentile_shortcuts(self):
+        columns = [
+            "",  # test default value
+            "transaction.duration",
+            "measurements.fp",
+            "measurements.fcp",
+            "measurements.lcp",
+            "measurements.fid",
+            "measurements.bar",
+        ]
+
+        for column in columns:
+            # if no column then use transaction.duration
+            snuba_column = column if column else "transaction.duration"
+            column_alias = column.replace(".", "_")
+
+            fields = [
+                field.format(column)
+                for field in ["p50({})", "p75({})", "p95({})", "p99({})", "p100({})"]
+            ]
+            result = resolve_field_list(fields, eventstore.Filter())
+
+            assert result["aggregations"] == [
+                ["quantile(0.5)", snuba_column, "p50_{}".format(column_alias).strip("_")],
+                ["quantile(0.75)", snuba_column, "p75_{}".format(column_alias).strip("_")],
+                ["quantile(0.95)", snuba_column, "p95_{}".format(column_alias).strip("_")],
+                ["quantile(0.99)", snuba_column, "p99_{}".format(column_alias).strip("_")],
+                ["max", snuba_column, "p100_{}".format(column_alias).strip("_")],
+            ]
+
     def test_rollup_with_unaggregated_fields(self):
         with pytest.raises(InvalidSearchQuery) as err:
             fields = ["message"]
@@ -2581,7 +2611,8 @@ class ResolveFieldListTest(unittest.TestCase):
 
 class DefaultFunctionArg(FunctionArg):
     def __init__(self, name, default):
-        super(DefaultFunctionArg, self).__init__(name, has_default=True)
+        super(DefaultFunctionArg, self).__init__(name)
+        self.has_default = True
         self.default = default
 
     def get_default(self, params):
