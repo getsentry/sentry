168590b89fad51fbae1e35252afb16f6c218d794
William Mak <william@wmak.io>
2020-10-13 19:42:50 -0400

    feat(trends): Add correct functions to trend autocomplete
    - Also not including trendView here since its including the functions
      from the other performance views, can add these back if we want
      though.

--- src/sentry/static/sentry/app/views/performance/trends/content.tsx
+++ src/sentry/static/sentry/app/views/performance/trends/content.tsx
@@ -5,6 +5,7 @@ import styled from '@emotion/styled';
 
 import {GlobalSelection, Organization} from 'app/types';
 import EventView from 'app/utils/discover/eventView';
+import {Field} from 'app/utils/discover/fields';
 import DropdownControl, {DropdownItem} from 'app/components/dropdownControl';
 import {t} from 'app/locale';
 import Feature from 'app/components/acl/feature';
@@ -92,6 +93,11 @@ class TrendsContent extends React.Component<Props, State> {
     const {previousTrendFunction} = this.state;
 
     const trendView = eventView.clone() as TrendView;
+    const fields = [
+      ...TRENDS_FUNCTIONS.map(({field}) => field),
+      'count()',
+      'absolute_correlation()',
+    ].map(field => ({field})) as Field[];
     const currentTrendFunction = getCurrentTrendFunction(location);
     const query = getTransactionSearchQuery(location);
     const showChangedProjects = hasMultipleProjects(selection);
@@ -104,7 +110,7 @@ class TrendsContent extends React.Component<Props, State> {
               organization={organization}
               projectIds={trendView.project}
               query={query}
-              fields={trendView.fields}
+              fields={fields}
               onSearch={this.handleSearch}
             />
             <TrendsDropdown>
