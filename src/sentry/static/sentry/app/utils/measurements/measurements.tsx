457a566af933245aa5cfa1ec0ca7787009f5fb48
Alberto Leal <mail4alberto@gmail.com>
2020-10-13 16:00:05 -0400

    feat(rum): Add CLS, TTFB, and Request Time measurements to Discover

--- src/sentry/static/sentry/app/utils/measurements/measurements.tsx
+++ src/sentry/static/sentry/app/utils/measurements/measurements.tsx
@@ -12,6 +12,12 @@ const MEASUREMENTS: MeasurementCollection = {
   'measurements.fcp': {name: 'First Contentful Paint', key: 'measurements.fcp'},
   'measurements.lcp': {name: 'Largest Contentful Paint', key: 'measurements.lcp'},
   'measurements.fid': {name: 'First Input Delay', key: 'measurements.fid'},
+  'measurements.cls': {name: 'Cumulative Layout Shift', key: 'measurements.cls'},
+  'measurements.ttfb': {name: 'Time to First Byte', key: 'measurements.ttfb'},
+  'measurements.ttfb.requesttime': {
+    name: 'Request Time',
+    key: 'measurements.ttfb.requesttime',
+  },
 };
 
 type ChildrenProps = {
