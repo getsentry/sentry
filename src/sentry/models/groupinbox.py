24007e4c18b244d298ae1f128c875895fc433709
Chris Fuller <cfuller@sentry.io>
2020-10-14 11:34:14 -0400

    Merging with master, using JSONField for reason details

--- src/sentry/models/groupinbox.py
+++ src/sentry/models/groupinbox.py
@@ -5,7 +5,7 @@ from enum import Enum
 from django.db import models
 from django.utils import timezone
 
-from sentry.db.models import FlexibleForeignKey, Model
+from sentry.db.models import FlexibleForeignKey, Model, JSONField
 
 
 class GroupInboxReason(Enum):
@@ -19,14 +19,11 @@ class GroupInbox(Model):
     A Group that is in the inbox.
     """
 
-    # TODO: What does this do?
     __core__ = False
 
     group = FlexibleForeignKey("sentry.Group", unique=True)
     reason = models.PositiveSmallIntegerField(null=False, default=GroupInboxReason.NEW.value)
-    # TODO: How does the frontend represent different "reasons"?
-    # Perhaps this should be number to map to a reason. If it's a string, how does it work with translations?
-    reason_label = models.CharField(max_length=256, blank=True, null=True)
+    reason_details = JSONField(null=True)
     date_added = models.DateTimeField(default=timezone.now)
 
     class Meta:
