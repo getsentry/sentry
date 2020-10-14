d73408cb98da4daec3121e4bd39e0cd060551141
Matej Minar <matej.minar@sentry.io>
2020-10-14 14:12:24 +0200

    ref: Remove now unused hasAttachmentsRole method

--- src/sentry/static/sentry/app/utils/attachmentUrl.tsx
+++ src/sentry/static/sentry/app/utils/attachmentUrl.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import {Organization, EventAttachment} from 'app/types';
-import ConfigStore from 'app/stores/configStore';
 import withOrganization from 'app/utils/withOrganization';
 import Role from 'app/components/acl/role';
 
@@ -14,27 +13,6 @@ type Props = {
 };
 
 class AttachmentUrl extends React.PureComponent<Props> {
-  hasAttachmentsRole() {
-    const user = ConfigStore.get('user');
-    if (!user) {
-      return false;
-    }
-
-    if (user.isSuperuser) {
-      return true;
-    }
-
-    const {availableRoles, attachmentsRole, role} = this.props.organization;
-    if (!Array.isArray(availableRoles)) {
-      return false;
-    }
-
-    const roleIds = availableRoles.map(r => r.id);
-    const requiredIndex = roleIds.indexOf(attachmentsRole);
-    const currentIndex = roleIds.indexOf(role || '');
-    return currentIndex >= requiredIndex;
-  }
-
   getDownloadUrl() {
     const {attachment, organization, eventId, projectId} = this.props;
     return `/api/0/projects/${organization.slug}/${projectId}/events/${eventId}/attachments/${attachment.id}/`;
