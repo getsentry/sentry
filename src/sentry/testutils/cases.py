33d120494d1469143646bdbb086c8fd7dddae18d
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2020-10-15 11:05:29 -0700

    fix(acceptance-tests): Add TimedRetryPolicy decorator to login_as (#21267)
    In this PR, I added a data-test-id to the Plugin Configuration Form (bc the test checks for it's existence not contents). So now, selenium looks for the specific data-test-id.
    
    For the ReadTimeoutError on `login_as`, I added a TimeoutRetryPolicy decorator.

--- src/sentry/testutils/cases.py
+++ src/sentry/testutils/cases.py
@@ -85,6 +85,7 @@ from sentry.tagstore.snuba import SnubaTagStorage
 from sentry.utils import json
 from sentry.utils.auth import SSO_SESSION_KEY
 from sentry.testutils.helpers.datetime import iso_format
+from sentry.utils.retries import TimedRetryPolicy
 
 from .fixtures import Fixtures
 from .factories import Factories
@@ -178,6 +179,7 @@ class BaseTestCase(Fixtures, Exam):
 
     # TODO(dcramer): ideally superuser_sso would be False by default, but that would require
     # a lot of tests changing
+    @TimedRetryPolicy.wrap(timeout=5)
     def login_as(
         self, user, organization_id=None, organization_ids=None, superuser=False, superuser_sso=True
     ):
