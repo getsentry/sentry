b6760f1f918565bc82fee0eca39538528f65a84d
Dan Fuller <dfuller@sentry.io>
2020-10-14 18:45:05 -0700

    add test and record stats period

--- src/sentry/incidents/logic.py
+++ src/sentry/incidents/logic.py
@@ -308,7 +308,7 @@ def create_incident_snapshot(incident, windowed_stats=False):
         return IncidentSnapshot.objects.create(
             incident=incident,
             event_stats_snapshot=TimeSeriesSnapshot.objects.create(
-                start=start, end=end, values=[], period=0,
+                start=start, end=end, values=[], period=incident.alert_rule.snuba_query.time_window,
             ),
             unique_users=0,
             total_events=0,
