2ab9cc164fb4e82de5619b15d57b58d6282bbf66
Stephen Cefali <scefali@sentry.io>
2020-10-15 10:35:03 -0700

    update comment

--- src/sentry/static/sentry/app/plugins/defaultPlugin.tsx
+++ src/sentry/static/sentry/app/plugins/defaultPlugin.tsx
@@ -2,7 +2,7 @@ import BasePlugin from 'app/plugins/basePlugin';
 
 class DefaultPlugin extends BasePlugin {
   static displayName = 'DefaultPlugin';
-  //should never be be callsed
+  //should never be be called since this is a non-issue plugin
   renderGroupActions() {
     return null;
   }
