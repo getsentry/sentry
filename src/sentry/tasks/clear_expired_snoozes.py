14fcec4f2019bd325ad2ae346a7f2f4299edb650
Scott Cooper <scttcper@gmail.com>
2020-10-16 11:55:05 -0700

    feat(workflow): Track group state change unignored (#21268)

--- src/sentry/tasks/clear_expired_snoozes.py
+++ src/sentry/tasks/clear_expired_snoozes.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, print_function
 
 from django.utils import timezone
 
+from sentry.signals import issue_unignored
 from sentry.models import Group, GroupSnooze, GroupStatus
 from sentry.tasks.base import instrumented_task
 
@@ -12,8 +13,18 @@ def clear_expired_snoozes():
         GroupSnooze.objects.filter(until__lte=timezone.now()).values_list("group", flat=True)
     )
 
+    ignored_groups = list(Group.objects.filter(id__in=group_list, status=GroupStatus.IGNORED))
     Group.objects.filter(id__in=group_list, status=GroupStatus.IGNORED).update(
         status=GroupStatus.UNRESOLVED
     )
 
     GroupSnooze.objects.filter(group__in=group_list).delete()
+
+    for group in ignored_groups:
+        issue_unignored.send_robust(
+            project=group.project,
+            user=None,
+            group=group,
+            transition_type="automatic",
+            sender="clear_expired_snoozes",
+        )
