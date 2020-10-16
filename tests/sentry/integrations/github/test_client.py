8936065f9ce82c6c57bd08a3bc10018a06f904ac
Colleen O'Rourke <colleen@sentry.io>
2020-10-16 11:01:03 -0700

    Add setUp method

--- tests/sentry/integrations/github/test_client.py
+++ tests/sentry/integrations/github/test_client.py
@@ -10,8 +10,7 @@ from sentry.models import Integration
 class GitHubAppsClientTest(TestCase):
     @mock.patch("sentry.integrations.github.client.get_jwt", return_value="jwt_token_1")
     @responses.activate
-    def test_save_token(self, get_jwt):
-
+    def setUp(self, get_jwt):
         integration = Integration.objects.create(
             provider="github",
             name="Github Test Org",
@@ -20,7 +19,11 @@ class GitHubAppsClientTest(TestCase):
         )
 
         install = integration.get_installation(organization_id="123")
-        client = install.get_client()
+        self.client = install.get_client()
+
+    @mock.patch("sentry.integrations.github.client.get_jwt", return_value="jwt_token_1")
+    @responses.activate
+    def test_save_token(self, get_jwt):
 
         responses.add(
             method=responses.POST,
@@ -30,28 +33,18 @@ class GitHubAppsClientTest(TestCase):
             content_type="application/json",
         )
 
-        token = client.get_token()
+        token = self.client.get_token()
         assert token == "12345token"
         assert len(responses.calls) == 1
 
         # Second get_token doesn't have to make an API call
-        token = client.get_token()
+        token = self.client.get_token()
         assert token == "12345token"
         assert len(responses.calls) == 1
 
     @mock.patch("sentry.integrations.github.client.get_jwt", return_value="jwt_token_1")
     @responses.activate
     def test_check_source_code_link(self, get_jwt):
-        integration = Integration.objects.create(
-            provider="github",
-            name="Github Test Org",
-            external_id="1",
-            metadata={"access_token": None, "expires_at": None},
-        )
-
-        install = integration.get_installation(organization_id="123")
-        client = install.get_client()
-
         responses.add(
             method=responses.POST,
             url="https://api.github.com/app/installations/1/access_tokens",
@@ -66,22 +59,12 @@ class GitHubAppsClientTest(TestCase):
             method=responses.HEAD, url=path, status=200,
         )
 
-        resp = client.check_source_code_link(path)
+        resp = self.client.check_source_code_link(path)
         assert resp == 200
 
     @mock.patch("sentry.integrations.github.client.get_jwt", return_value="jwt_token_1")
     @responses.activate
     def test_check_bad_source_code_link(self, get_jwt):
-        integration = Integration.objects.create(
-            provider="github",
-            name="Github Test Org",
-            external_id="1",
-            metadata={"access_token": None, "expires_at": None},
-        )
-
-        install = integration.get_installation(organization_id="123")
-        client = install.get_client()
-
         responses.add(
             method=responses.POST,
             url="https://api.github.com/app/installations/1/access_tokens",
@@ -96,5 +79,5 @@ class GitHubAppsClientTest(TestCase):
             method=responses.HEAD, url=path, status=404,
         )
 
-        resp = client.check_source_code_link(path)
+        resp = self.client.check_source_code_link(path)
         assert resp == 404
