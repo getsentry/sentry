813786b2a355bc60d2230cc9c98d3ed1a742de32
Scott Cooper <scttcper@gmail.com>
2020-10-16 14:06:03 -0700

    fix: Add missing analytics events (#21422)

---
+++ src/sentry/analytics/events/issue_unresolved.py
@@ -0,0 +1,18 @@
+from __future__ import absolute_import
+
+from sentry import analytics
+
+
+class IssueUnresolvedEvent(analytics.Event):
+    type = "issue.unresolved"
+
+    attributes = (
+        analytics.Attribute("user_id", type=int, required=False),
+        analytics.Attribute("default_user_id", type=int),
+        analytics.Attribute("organization_id", type=int),
+        analytics.Attribute("group_id"),
+        analytics.Attribute("transition_type"),
+    )
+
+
+analytics.register(IssueUnresolvedEvent)
