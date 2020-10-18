3908bc5e4bdfdc96b10f5e167f1276c695d9ac55
Armin Ronacher <armin.ronacher@active-4.com>
2020-10-18 21:15:19 +0200

    feat: Added ANSI color support to logfile renderer

--- src/sentry/static/sentry/app/components/events/logFileViewer.tsx
+++ src/sentry/static/sentry/app/components/events/logFileViewer.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import ansicolor from 'ansicolor';
 
 import {Event, EventAttachment} from 'app/types';
 import space from 'app/styles/space';
 import {PanelItem} from 'app/components/panels';
 import AsyncComponent from 'app/components/asyncComponent';
+import theme from 'app/utils/theme';
 
 type Props = {
   event: Event;
@@ -14,6 +16,23 @@ type Props = {
 } & AsyncComponent['props'];
 
 type State = AsyncComponent['state'];
+
+const COLORS = {
+  black: theme.black,
+  white: theme.white,
+  redDim: theme.red300,
+  red: theme.red400,
+  greenDim: theme.green300,
+  green: theme.green400,
+  yellowDim: theme.yellow300,
+  yellow: theme.yellow400,
+  blueDim: theme.blue400,
+  blue: theme.blue500,
+  magentaDim: theme.pink300,
+  magenta: theme.pink400,
+  cyanDim: theme.blue200,
+  cyan: theme.blue300,
+};
 
 export default class LogFileViewer extends AsyncComponent<Props, State> {
   getEndpoints(): [string, string][] {
@@ -32,9 +51,35 @@ export default class LogFileViewer extends AsyncComponent<Props, State> {
       return null;
     }
 
+    const spans = ansicolor
+      .parse(attachmentText)
+      .spans.map(({color, bgColor, text}, idx) => {
+        const style = {} as any;
+        if (color) {
+          if (color.name) {
+            style.color =
+              COLORS[color.name + (color.dim ? 'Dim' : '')] || COLORS[color.name] || '';
+          }
+          if (color.bright) {
+            style.fontWeight = 500;
+          }
+        }
+        if (bgColor && bgColor.name) {
+          style.background =
+            COLORS[bgColor.name + (bgColor.dim ? 'Dim' : '')] ||
+            COLORS[bgColor.name] ||
+            '';
+        }
+        return (
+          <span style={style} key={idx}>
+            {text}
+          </span>
+        );
+      });
+
     return (
       <LogPanelItem>
-        <CodeWrapper>{attachmentText}</CodeWrapper>
+        <CodeWrapper>{spans}</CodeWrapper>
       </LogPanelItem>
     );
   }
