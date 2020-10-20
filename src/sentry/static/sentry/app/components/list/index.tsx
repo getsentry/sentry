656430b44c58e6e747afdd941ea0f8f8a0f73676
Priscila Oliveira <priscilawebdev@gmail.com>
2020-10-20 12:15:11 +0200

    ref(list): Refactor List Component (#21319)

---
+++ src/sentry/static/sentry/app/components/list/index.tsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from '@emotion/styled';
+
+import space from 'app/styles/space';
+
+import {getListSymbolStyle, listSymbol} from './utils';
+
+type Props = {
+  children: Array<React.ReactElement>;
+  symbol?: keyof typeof listSymbol | React.ReactElement;
+  className?: string;
+};
+
+const List = styled(({children, className, symbol}: Props) => {
+  const getWrapperComponent = () => {
+    switch (symbol) {
+      case 'numeric':
+      case 'colored-numeric':
+        return 'ol';
+      default:
+        return 'ul';
+    }
+  };
+
+  const Wrapper = getWrapperComponent();
+
+  return (
+    <Wrapper className={className}>
+      {!symbol || typeof symbol === 'string'
+        ? children
+        : React.Children.map(children, child => {
+            if (!React.isValidElement(child)) {
+              return child;
+            }
+            return React.cloneElement(child as React.ReactElement, {
+              symbol,
+            });
+          })}
+    </Wrapper>
+  );
+})`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: grid;
+  grid-gap: ${space(0.5)};
+  ${p =>
+    typeof p.symbol === 'string' &&
+    listSymbol[p.symbol] &&
+    getListSymbolStyle(p.theme, p.symbol)}
+`;
+
+export default List;
