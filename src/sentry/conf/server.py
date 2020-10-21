efd3941c115b13f3dda01262723c6f513d95a04a
Taylan Gocmen <ta.gocmen@gmail.com>
2020-10-20 17:01:24 -0700

    set the default dynamic-counts flag to true

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -903,7 +903,7 @@ SENTRY_FEATURES = {
     # attachments
     "organizations:usage-stats-graph": False,
     # Enable dynamic issue counts and user counts in the issue stream
-    "organizations:dynamic-issue-counts": False,
+    "organizations:dynamic-issue-counts": True,
     # Return unhandled information on the issue level
     "organizations:unhandled-issue-flag": False,
     # Enable functionality to specify custom inbound filters on events.
