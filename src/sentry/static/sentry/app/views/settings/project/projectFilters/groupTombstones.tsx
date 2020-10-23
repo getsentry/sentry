bfa44a518652aee28189ab64ed43a68151ca7a30
Mark Story <mark@mark-story.com>
2020-10-23 13:24:39 -0400

    ref(ts) Convert Group Tombstones to typescript

--- src/sentry/static/sentry/app/views/settings/project/projectFilters/groupTombstones.tsx
+++ src/sentry/static/sentry/app/views/settings/project/projectFilters/groupTombstones.tsx
@@ -1,4 +1,3 @@
-import PropTypes from 'prop-types';
 import React from 'react';
 import styled from '@emotion/styled';
 
@@ -13,73 +12,75 @@ import {IconDelete} from 'app/icons';
 import {Panel, PanelItem} from 'app/components/panels';
 import EmptyMessage from 'app/views/settings/components/emptyMessage';
 import space from 'app/styles/space';
+import {GroupTombstone} from 'app/types';
 
-class GroupTombstoneRow extends React.Component {
-  static propTypes = {
-    data: PropTypes.object.isRequired,
-    onUndiscard: PropTypes.func.isRequired,
-  };
+type RowProps = {
+  data: GroupTombstone;
+  onUndiscard: (id: string) => void;
+};
 
-  render() {
-    const {data, onUndiscard} = this.props,
-      actor = data.actor;
+function GroupTombstoneRow({data, onUndiscard}: RowProps) {
+  const actor = data.actor;
 
-    return (
-      <PanelItem alignItems="center">
-        <StyledBox>
-          <EventOrGroupHeader
-            includeLink={false}
-            hideIcons
-            className="truncate"
-            data={data}
+  return (
+    <PanelItem alignItems="center">
+      <StyledBox>
+        <EventOrGroupHeader
+          includeLink={false}
+          hideIcons
+          className="truncate"
+          data={data}
+        />
+      </StyledBox>
+      <AvatarContainer>
+        {actor && (
+          <Avatar
+            user={actor}
+            hasTooltip
+            tooltip={t('Discarded by %s', actor.name || actor.email)}
           />
-        </StyledBox>
-        <AvatarContainer>
-          {actor && (
-            <Avatar
-              user={data.actor}
-              hasTooltip
-              tooltip={t('Discarded by %s', actor.name || actor.email)}
-            />
-          )}
-        </AvatarContainer>
-        <ActionContainer>
-          <Tooltip title={t('Undiscard')}>
-            <LinkWithConfirmation
-              title={t('Undiscard')}
-              className="group-remove btn btn-default btn-sm"
-              message={t(
-                'Undiscarding this issue means that ' +
-                  'incoming events that match this will no longer be discarded. ' +
-                  'New incoming events will count toward your event quota ' +
-                  'and will display on your issues dashboard. ' +
-                  'Are you sure you wish to continue?'
-              )}
-              onConfirm={() => {
-                onUndiscard(data.id);
-              }}
-            >
-              <IconDelete className="undiscard" />
-            </LinkWithConfirmation>
-          </Tooltip>
-        </ActionContainer>
-      </PanelItem>
-    );
-  }
+        )}
+      </AvatarContainer>
+      <ActionContainer>
+        <Tooltip title={t('Undiscard')}>
+          <LinkWithConfirmation
+            title={t('Undiscard')}
+            className="group-remove btn btn-default btn-sm"
+            message={t(
+              'Undiscarding this issue means that ' +
+                'incoming events that match this will no longer be discarded. ' +
+                'New incoming events will count toward your event quota ' +
+                'and will display on your issues dashboard. ' +
+                'Are you sure you wish to continue?'
+            )}
+            onConfirm={() => {
+              onUndiscard(data.id);
+            }}
+          >
+            <IconDelete className="undiscard" />
+          </LinkWithConfirmation>
+        </Tooltip>
+      </ActionContainer>
+    </PanelItem>
+  );
 }
 
-class GroupTombstones extends AsyncComponent {
-  static propTypes = {
-    orgId: PropTypes.string.isRequired,
-    projectId: PropTypes.string.isRequired,
-  };
+type Props = AsyncComponent['props'] & {
+  orgId: string;
+  projectId: string;
+};
 
-  getEndpoints() {
+type State = {
+  tombstones: GroupTombstone[];
+} & AsyncComponent['state'];
+
+class GroupTombstones extends AsyncComponent<Props, State> {
+  getEndpoints(): ReturnType<AsyncComponent['getEndpoints']> {
     const {orgId, projectId} = this.props;
     return [['tombstones', `/projects/${orgId}/${projectId}/tombstones/`]];
   }
 
-  handleUndiscard = tombstoneId => {
+  handleUndiscard = (tombstoneId: GroupTombstone['id']) => {
     const {orgId, projectId} = this.props;
     const path = `/projects/${orgId}/${projectId}/tombstones/${tombstoneId}/`;
     this.api
