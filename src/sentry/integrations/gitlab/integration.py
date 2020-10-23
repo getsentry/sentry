5892a0ffd34a717e3e2c6e4142d67807279cd55a
meredith <meredith@sentry.io>
2020-10-23 10:53:46 -0700

    ref(stacktrace-link): Add get_stacktrace_link for GH/GL

--- src/sentry/integrations/gitlab/integration.py
+++ src/sentry/integrations/gitlab/integration.py
@@ -92,6 +92,25 @@ class GitlabIntegration(IntegrationInstallation, GitlabIssueBasic, RepositoryMix
         resp = self.get_client().search_group_projects(group, query)
         return [{"identifier": repo["id"], "name": repo["name_with_namespace"]} for repo in resp]
 
+    def get_stacktrace_link(self, repo, filepath, default_version):
+        project_id = repo.config["project_id"]
+        try:
+            # repos are projects in GL so the project_id is the repo id which
+            # GL's API uses instead of slugs like GH
+            self.get_client().check_file(project_id, filepath, default_version)
+        except ApiError as e:
+            if e.code != 404:
+                raise
+            return None
+
+        base_url = self.model.metadata["base_url"]
+
+        # Must format the url ourselves since `check_file` is a head request
+        # "https://gitlab.com/gitlab-org/gitlab/blob/master/README.md"
+        web_url = "{}/{}/blob/{}/{}".format(base_url, repo.name, default_version, filepath)
+
+        return web_url
+
     def search_projects(self, query):
         client = self.get_client()
         group_id = self.get_group_id()
