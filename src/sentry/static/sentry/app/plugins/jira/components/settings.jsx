getsentry/sentry

commit 336463f93e2831a246754c19f1dd1f827baa7de5

chore(plugins): Move jira/sessionstack plugin UI to app (#15766)

src/sentry/static/sentry/app/plugins/jira/components/settings.jsx
@@ -1,28 +1,29 @@
 import React from 'react';
-import _ from 'lodash';
-import {Form, FormState, LoadingIndicator, plugins} from 'sentry';
+import isEqual from 'lodash/isEqual';
 
+import {Form, FormState} from 'app/components/forms';
+import BasePlugin from 'app/plugins/basePlugin';
+import LoadingIndicator from 'app/components/loadingIndicator';
 
-class Settings extends plugins.BasePlugin.DefaultSettings {
+class Settings extends BasePlugin.DefaultSettings {
   constructor(props) {
     super(props);
     this.PAGE_FIELD_LIST = {
       '0': ['instance_url', 'username', 'password'],
       '1': ['default_project'],
-      '2': ['ignored_fields','default_priority', 'default_issue_type', 'auto_create']
-    }
+      '2': ['ignored_fields', 'default_priority', 'default_issue_type', 'auto_create'],
+    };
 
     this.back = this.back.bind(this);
     this.startEditing = this.startEditing.bind(this);
     this.isLastPage = this.isLastPage.bind(this);
 
     Object.assign(this.state, {
-      page: 0
+      page: 0,
     });
   }
 
-  isConfigured(state) {
-    state = state || this.state;
+  isConfigured() {
     return !!(this.state.formData && this.state.formData.default_project);
   }
 
@@ -35,23 +36,26 @@ class Settings extends plugins.BasePlugin.DefaultSettings {
     // except for setting edit state
     this.api.request(this.getPluginEndpoint(), {
       success: data => {
-        let formData = {};
-        let initialData = {};
-        data.config.forEach((field) => {
+        const formData = {};
+        const initialData = {};
+        data.config.forEach(field => {
           formData[field.name] = field.value || field.defaultValue;
           initialData[field.name] = field.value;
         });
-        this.setState({
-          fieldList: data.config,
-          formData: formData,
-          initialData: initialData,
-          // start off in edit mode if there isn't a project set
-          editing: !(formData && formData.default_project),
-        // call this here to prevent FormState.READY from being
-        // set before fieldList is
-        }, this.onLoadSuccess);
+        this.setState(
+          {
+            fieldList: data.config,
+            formData,
+            initialData,
+            // start off in edit mode if there isn't a project set
+            editing: !(formData && formData.default_project),
+            // call this here to prevent FormState.READY from being
+            // set before fieldList is
+          },
+          this.onLoadSuccess
+        );
       },
-      error: this.onLoadError
+      error: this.onLoadError,
     });
   }
 
@@ -60,36 +64,36 @@ class Settings extends plugins.BasePlugin.DefaultSettings {
   }
 
   onSubmit() {
-    if (_.isEqual(this.state.initialData, this.state.formData)) {
+    if (isEqual(this.state.initialData, this.state.formData)) {
       if (this.isLastPage()) {
-        this.setState({editing: false, page: 0})
+        this.setState({editing: false, page: 0});
       } else {
         this.setState({page: this.state.page + 1});
       }
       this.onSaveSuccess(this.onSaveComplete);
       return;
     }
-    let formData = Object.assign({}, this.state.formData);
+    const body = Object.assign({}, this.state.data);
     // if the project has changed, it's likely these values aren't valid anymore
-    if (formData.default_project !== this.state.initialData.default_project) {
-      formData.default_issue_type = null;
-      formData.default_priority = null;
+    if (body.default_project !== this.state.initialData.default_project) {
+      body.default_issue_type = null;
+      body.default_priority = null;
     }
     this.api.request(this.getPluginEndpoint(), {
-      data: formData,
+      data: body,
       method: 'PUT',
       success: this.onSaveSuccess.bind(this, data => {
-        let formData = {};
-        let initialData = {};
-        data.config.forEach((field) => {
+        const formData = {};
+        const initialData = {};
+        data.config.forEach(field => {
           formData[field.name] = field.value || field.defaultValue;
           initialData[field.name] = field.value;
         });
-        let state = {
-          formData: formData,
-          initialData: initialData,
+        const state = {
+          formData,
+          initialData,
           errors: {},
-          fieldList: data.config
+          fieldList: data.config,
         };
         if (this.isLastPage()) {
           state.editing = false;
@@ -104,7 +108,7 @@ class Settings extends plugins.BasePlugin.DefaultSettings {
           errors: (error.responseJSON || {}).errors || {},
         });
       }),
-      complete: this.onSaveComplete
+      complete: this.onSaveComplete,
     });
   }
 
@@ -114,7 +118,7 @@ class Settings extends plugins.BasePlugin.DefaultSettings {
       return;
     }
     this.setState({
-      page: this.state.page - 1
+      page: this.state.page - 1,
     });
   }
 
@@ -126,12 +130,13 @@ class Settings extends plugins.BasePlugin.DefaultSettings {
     if (this.state.state === FormState.ERROR && !this.state.fieldList) {
       return (
         <div className="alert alert-error m-b-1">
-          An unknown error occurred. Need help with this? <a href="https://sentry.io/support/">Contact support</a>
+          An unknown error occurred. Need help with this?{' '}
+          <a href="https://sentry.io/support/">Contact support</a>
         </div>
       );
     }
 
-    let isSaving = this.state.state === FormState.SAVING;
+    const isSaving = this.state.state === FormState.SAVING;
 
     let fields;
     let onSubmit;
@@ -150,26 +155,35 @@ class Settings extends plugins.BasePlugin.DefaultSettings {
       submitLabel = 'Edit';
     }
     return (
-      <Form onSubmit={onSubmit}
-            submitDisabled={isSaving}
-            submitLabel={submitLabel}
-            extraButton={this.state.page === 0 ? null :
-                         <a href="#"
-                            className={'btn btn-default pull-left' + (isSaving ? ' disabled' : '')}
-                            onClick={this.back}>Back</a>}>
-        {this.state.errors.__all__ &&
+      <Form
+        onSubmit={onSubmit}
+        submitDisabled={isSaving}
+        submitLabel={submitLabel}
+        extraButton={
+          this.state.page === 0 ? null : (
+            <a
+              href="#"
+              className={'btn btn-default pull-left' + (isSaving ? ' disabled' : '')}
+              onClick={this.back}
+            >
+              Back
+            </a>
+          )
+        }
+      >
+        {this.state.errors.__all__ && (
           <div className="alert alert-block alert-error">
             <ul>
               <li>{this.state.errors.__all__}</li>
             </ul>
           </div>
-        }
+        )}
         {fields.map(f => {
           return this.renderField({
             config: f,
             formData: this.state.formData,
             formErrors: this.state.errors,
-            onChange: this.changeField.bind(this, f.name)
+            onChange: this.changeField.bind(this, f.name),
           });
         })}
       </Form>

src/sentry/static/sentry/app/plugins/sessionstack/components/settings.jsx
@@ -1,0 +1,93 @@
+import React from 'react';
+import isEqual from 'lodash/isEqual';
+
+import {Form, FormState} from 'app/components/forms';
+import LoadingIndicator from 'app/components/loadingIndicator';
+import BasePlugin from 'app/plugins/basePlugin';
+
+class Settings extends BasePlugin.DefaultSettings {
+  constructor(props) {
+    super(props);
+
+    this.REQUIRED_FIELDS = ['account_email', 'api_token', 'website_id'];
+    this.ON_PREMISES_FIELDS = ['api_url', 'player_url'];
+
+    this.toggleOnPremisesConfiguration = this.toggleOnPremisesConfiguration.bind(this);
+  }
+
+  renderFields(fields) {
+    return fields.map(f => {
+      return this.renderField({
+        config: f,
+        formData: this.state.formData,
+        formErrors: this.state.errors,
+        onChange: this.changeField.bind(this, f.name),
+      });
+    });
+  }
+
+  filterFields(fields, fieldNames) {
+    return fields.filter(field => {
+      return fieldNames.includes(field.name);
+    });
+  }
+
+  toggleOnPremisesConfiguration() {
+    this.setState({
+      showOnPremisesConfiguration: !this.state.showOnPremisesConfiguration,
+    });
+  }
+
+  render() {
+    if (this.state.state === FormState.LOADING) {
+      return <LoadingIndicator />;
+    }
+
+    if (this.state.state === FormState.ERROR && !this.state.fieldList) {
+      return (
+        <div className="alert alert-error m-b-1">
+          An unknown error occurred. Need help with this?{' '}
+          <a href="https://sentry.io/support/">Contact support</a>
+        </div>
+      );
+    }
+
+    const isSaving = this.state.state === FormState.SAVING;
+    const hasChanges = !isEqual(this.state.initialData, this.state.formData);
+
+    const requiredFields = this.filterFields(this.state.fieldList, this.REQUIRED_FIELDS);
+    const onPremisesFields = this.filterFields(
+      this.state.fieldList,
+      this.ON_PREMISES_FIELDS
+    );
+
+    return (
+      <Form onSubmit={this.onSubmit} submitDisabled={isSaving || !hasChanges}>
+        {this.state.errors.__all__ && (
+          <div className="alert alert-block alert-error">
+            <ul>
+              <li>{this.state.errors.__all__}</li>
+            </ul>
+          </div>
+        )}
+        {this.renderFields(requiredFields)}
+        {onPremisesFields.length > 0 ? (
+          <div className="control-group">
+            <button
+              className="btn btn-default"
+              type="button"
+              onClick={this.toggleOnPremisesConfiguration}
+            >
+              Configure on-premises
+            </button>
+          </div>
+        ) : null}
+        {this.state.showOnPremisesConfiguration
+          ? this.renderFields(onPremisesFields)
+          : null}
+      </Form>
+    );
+  }
+}
+
+export default Settings;

src/sentry_plugins/sessionstack/static/sessionstack/components/settings.jsx
@@ -1,83 +1,0 @@
-import React from 'react';
-import _ from 'lodash';
-import {Form, FormState, LoadingIndicator, plugins} from 'sentry';
-
-
-class Settings extends plugins.BasePlugin.DefaultSettings {
-  constructor(props) {
-    super(props);
-
-    this.REQUIRED_FIELDS = ['account_email', 'api_token', 'website_id'];
-    this.ON_PREMISES_FIELDS = ['api_url', 'player_url'];
-
-    this.toggleOnPremisesConfiguration = this.toggleOnPremisesConfiguration.bind(this);
-  }
-
-  renderFields(fields) {
-    return fields.map(f => {
-      return this.renderField({
-        config: f,
-        formData: this.state.formData,
-        formErrors: this.state.errors,
-        onChange: this.changeField.bind(this, f.name)
-      });
-    })
-  }
-
-  filterFields(fields, fieldNames) {
-    return fields.filter(field => {
-      return fieldNames.includes(field.name);
-    })
-  }
-
-  toggleOnPremisesConfiguration() {
-    this.setState({
-      showOnPremisesConfiguration: !this.state.showOnPremisesConfiguration
-    });
-  }
-
-  render() {
-    if (this.state.state === FormState.LOADING) {
-      return <LoadingIndicator />;
-    }
-
-    if (this.state.state === FormState.ERROR && !this.state.fieldList) {
-      return (
-        <div className="alert alert-error m-b-1">
-          An unknown error occurred. Need help with this? <a href="https://sentry.io/support/">Contact support</a>
-        </div>
-      );
-    }
-
-    let isSaving = this.state.state === FormState.SAVING;
-    let hasChanges = !_.isEqual(this.state.initialData, this.state.formData);
-
-    let requiredFields = this.filterFields(this.state.fieldList, this.REQUIRED_FIELDS);
-    let onPremisesFields = this.filterFields(this.state.fieldList, this.ON_PREMISES_FIELDS);
-
-    return (
-      <Form onSubmit={this.onSubmit}
-            submitDisabled={isSaving || !hasChanges}>
-        {this.state.errors.__all__ &&
-          <div className="alert alert-block alert-error">
-            <ul>
-              <li>{this.state.errors.__all__}</li>
-            </ul>
-          </div>
-        }
-        {this.renderFields(requiredFields)}
-        {onPremisesFields.length > 0 ?
-            <div className="control-group">
-              <button className="btn btn-default" type="button" onClick={this.toggleOnPremisesConfiguration}>
-                Configure on-premises
-              </button>
-            </div>
-            : null
-        }
-        {this.state.showOnPremisesConfiguration ? this.renderFields(onPremisesFields) : null}
-      </Form>
-    );
-  }
-}
-
-export default Settings;

src/sentry/static/sentry/app/plugins/index.jsx
@@ -3,8 +3,21 @@ import BasePlugin from 'app/plugins/basePlugin';
 import BaseContext from 'app/plugins/baseContext';
 import DefaultIssuePlugin from 'app/plugins/defaultIssuePlugin';
 
+import SessionStackPlugin from './sessionstack';
+import SessionStackContextType from './sessionstack/contexts/sessionstack';
+import Jira from './jira';
+
 const contexts = {};
 const registry = new Registry();
+
+// Register legacy plguins
+
+// Sessionstack
+registry.add('sessionstack', SessionStackPlugin);
+contexts.sessionstack = SessionStackContextType;
+
+// Jira
+registry.add('jira', Jira);
 
 export {BasePlugin, registry, DefaultIssuePlugin};
 

src/sentry/static/sentry/app/plugins/jira/components/issueActions.jsx
@@ -1,0 +1,117 @@
+import React from 'react';
+
+import {Form, FormState} from 'app/components/forms';
+import DefaultIssuePlugin from 'app/plugins/defaultIssuePlugin';
+
+class IssueActions extends DefaultIssuePlugin.DefaultIssueActions {
+  changeField(action, name, value) {
+    const key = action + 'FormData';
+    const formData = {
+      ...this.state[key],
+      [name]: value,
+    };
+    const state = {
+      [key]: formData,
+    };
+    if (name === 'issuetype') {
+      state.state = FormState.LOADING;
+      this.setState(
+        state,
+        this.onLoad.bind(this, () => {
+          this.api.request(
+            this.getPluginCreateEndpoint() + '?issuetype=' + encodeURIComponent(value),
+            {
+              success: data => {
+                // Try not to change things the user might have edited
+                // unless they're no longer valid
+                const oldData = this.state.createFormData;
+                const createFormData = {};
+                data.forEach(field => {
+                  let val;
+                  if (
+                    field.choices &&
+                    !field.choices.find(c => c[0] === oldData[field.name])
+                  ) {
+                    val = field.default;
+                  } else {
+                    val = oldData[field.name] || field.default;
+                  }
+                  createFormData[field.name] = val;
+                });
+                this.setState(
+                  {
+                    createFieldList: data,
+                    error: null,
+                    loading: false,
+                    createFormData,
+                  },
+                  this.onLoadSuccess
+                );
+              },
+              error: this.errorHandler,
+            }
+          );
+        })
+      );
+      return;
+    }
+    this.setState(state);
+  }
+
+  renderForm() {
+    let form;
+
+    // For create form, split into required and optional fields
+    if (this.props.actionType === 'create') {
+      if (this.state.createFieldList) {
+        const renderField = field => {
+          if (field.has_autocomplete) {
+            field = Object.assign(
+              {
+                url:
+                  '/api/0/issues/' +
+                  this.getGroup().id +
+                  '/plugins/' +
+                  this.props.plugin.slug +
+                  '/autocomplete',
+              },
+              field
+            );
+          }
+          return (
+            <div key={field.name}>
+              {this.renderField({
+                config: field,
+                formData: this.state.createFormData,
+                onChange: this.changeField.bind(this, 'create', field.name),
+              })}
+            </div>
+          );
+        };
+        const isRequired = f => {
+          return f.required !== null ? f.required : true;
+        };
+
+        const fields = this.state.createFieldList;
+        const requiredFields = fields.filter(f => isRequired(f)).map(f => renderField(f));
+        const optionalFields = fields
+          .filter(f => !isRequired(f))
+          .map(f => renderField(f));
+        form = (
+          <Form onSubmit={this.createIssue} submitLabel="Create Issue" footerClass="">
+            <h5>Required Fields</h5>
+            {requiredFields}
+            {optionalFields.length ? <h5>Optional Fields</h5> : null}
+            {optionalFields}
+          </Form>
+        );
+      }
+    } else {
+      form = super.renderForm();
+    }
+
+    return form;
+  }
+}
+
+export default IssueActions;

src/sentry/static/sentry/app/plugins/jira/index.jsx
@@ -1,0 +1,20 @@
+import React from 'react';
+
+import DefaultIssuePlugin from 'app/plugins/defaultIssuePlugin';
+
+import Settings from './components/settings';
+import IssueActions from './components/issueActions';
+
+class Jira extends DefaultIssuePlugin {
+  renderSettings(props) {
+    return <Settings plugin={this} {...props} />;
+  }
+
+  renderGroupActions(props) {
+    return <IssueActions plugin={this} {...props} />;
+  }
+}
+
+Jira.displayName = 'Jira';
+
+export default Jira;

src/sentry/static/sentry/app/plugins/sessionstack/contexts/sessionstack.jsx
@@ -1,55 +1,51 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
+import $ from 'jquery';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import PropTypes from 'prop-types';
 
 const ASPECT_RATIO = 16 / 9;
 
 class SessionStackContextType extends React.Component {
-  static propTypes() {
-    return {
-      alias: PropTypes.string.isRequired,
-      data: PropTypes.object.isRequired
-    };
-  }
+  propTypes = {
+    data: PropTypes.object.isRequired,
+  };
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      showIframe: false
-    };
-  }
+  state = {
+    showIframe: false,
+  };
 
   componentDidMount() {
+    // eslint-disable-next-line react/no-find-dom-node
     this.parentNode = ReactDOM.findDOMNode(this).parentNode;
-    window.addEventListener("resize", () => this.setIframeSize(), false);
+    window.addEventListener('resize', () => this.setIframeSize(), false);
     this.setIframeSize();
   }
 
   componentWillUnmount() {
-    window.removeEventListener("resize", () => this.setIframeSize(), false);
+    window.removeEventListener('resize', () => this.setIframeSize(), false);
   }
 
   setIframeSize() {
     if (!this.showIframe) {
-      let parentWidth = $(this.parentNode).width();
+      const parentWidth = $(this.parentNode).width();
 
       this.setState({
         width: parentWidth,
-        height: parentWidth / ASPECT_RATIO
+        height: parentWidth / ASPECT_RATIO,
       });
     }
   }
 
   playSession() {
     this.setState({
-      showIframe: true
+      showIframe: true,
     });
 
     this.setIframeSize();
   }
 
   render() {
-    let { session_url } = this.props.data;
+    const {session_url} = this.props.data;
 
     if (!session_url) {
       return <h4>Session not found.</h4>;
@@ -78,8 +74,6 @@ class SessionStackContextType extends React.Component {
   }
 }
 
-SessionStackContextType.getTitle = function(value) {
-  return "SessionStack";
-};
+SessionStackContextType.getTitle = () => 'SessionStack';
 
 export default SessionStackContextType;

src/sentry/static/sentry/app/plugins/sessionstack/index.jsx
@@ -1,0 +1,15 @@
+import React from 'react';
+
+import BasePlugin from 'app/plugins/basePlugin';
+
+import Settings from './components/settings';
+
+class SessionStackPlugin extends BasePlugin {
+  renderSettings(props) {
+    return <Settings plugin={this} {...props} />;
+  }
+}
+
+SessionStackPlugin.displayName = 'SessionStack';
+
+export default SessionStackPlugin;

commit 98742952b2cad922f8871e98f6d2d7b8da370ed7

fix(jira): Fix submitting forms for Jira plugin (legacy integration)
This fixes a regression introduced in #15766 which referenced a
non-existent `this.state.data` when it should have remained
`this.state.formData`.

Fixes #21463

src/sentry/static/sentry/app/plugins/jira/components/settings.jsx
@@ -73,7 +73,7 @@ class Settings extends DefaultSettings {
       this.onSaveSuccess(this.onSaveComplete);
       return;
     }
-    const body = Object.assign({}, this.state.data);
+    const body = Object.assign({}, this.state.formData);
     // if the project has changed, it's likely these values aren't valid anymore
     if (body.default_project !== this.state.initialData.default_project) {
       body.default_issue_type = null;