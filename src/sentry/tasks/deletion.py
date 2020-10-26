974a4f86cd7cb8e316a7e1621985c3b076c710d5
Burak Yigit Kaya <ben@byk.im>
2020-10-27 00:55:45 +0300

    ref(deletion): Set MAX_RETRIES to 5 for all env
    Follow up to #21581. Also removes the now incorrect comment.

--- src/sentry/tasks/deletion.py
+++ src/sentry/tasks/deletion.py
@@ -12,9 +12,8 @@ from sentry.exceptions import DeleteAborted
 from sentry.signals import pending_delete
 from sentry.tasks.base import instrumented_task, retry, track_group_async_operation
 
-# in prod we run with infinite retries to recover from errors
-# in debug/development, we assume these tasks generally shouldn't fail
-MAX_RETRIES = 1 if settings.DEBUG else 5
+
+MAX_RETRIES = 5
 
 
 @instrumented_task(name="sentry.tasks.deletion.run_scheduled_deletions", queue="cleanup")
