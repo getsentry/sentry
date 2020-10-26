fcfef218f89896089afdc0dc38edc10f9f5f88c8
Tony Xiao <txiao@sentry.io>
2020-10-26 14:43:15 -0400

    feat(vitals): Add beta badge to vitals
    The web vitals is now available to early adopters. Add a beta badge for better
    discoverability.

--- src/sentry/static/sentry/app/views/performance/transactionSummary/header.tsx
+++ src/sentry/static/sentry/app/views/performance/transactionSummary/header.tsx
@@ -5,6 +5,7 @@ import styled from '@emotion/styled';
 import {Organization, Project} from 'app/types';
 import EventView from 'app/utils/discover/eventView';
 import Feature from 'app/components/acl/feature';
+import FeatureBadge from 'app/components/featureBadge';
 import CreateAlertButton from 'app/components/createAlertButton';
 import * as Layout from 'app/components/layouts/thirds';
 import ButtonBar from 'app/components/buttonBar';
@@ -147,6 +148,7 @@ class TransactionHeader extends React.Component<Props> {
                     isActive={() => currentTab === Tab.RealUserMonitoring}
                   >
                     {t('Web Vitals')}
+                    <FeatureBadge type="beta" />
                   </ListLink>
                 )}
               </StyledNavTabs>
