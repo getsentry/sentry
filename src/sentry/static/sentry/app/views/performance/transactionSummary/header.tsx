39a4c328c7bddfeaa6f52729e919c2d7327ff048
Tony Xiao <txiao@sentry.io>
2020-10-26 15:02:13 -0400

    add alert for feedback

--- src/sentry/static/sentry/app/views/performance/transactionSummary/header.tsx
+++ src/sentry/static/sentry/app/views/performance/transactionSummary/header.tsx
@@ -4,13 +4,16 @@ import styled from '@emotion/styled';
 
 import {Organization, Project} from 'app/types';
 import EventView from 'app/utils/discover/eventView';
+import Alert from 'app/components/alert';
 import Feature from 'app/components/acl/feature';
 import FeatureBadge from 'app/components/featureBadge';
 import CreateAlertButton from 'app/components/createAlertButton';
 import * as Layout from 'app/components/layouts/thirds';
+import ExternalLink from 'app/components/links/externalLink';
 import ButtonBar from 'app/components/buttonBar';
 import ListLink from 'app/components/links/listLink';
 import NavTabs from 'app/components/navTabs';
+import {IconInfo} from 'app/icons';
 import {t} from 'app/locale';
 import {trackAnalyticsEvent} from 'app/utils/analytics';
 import Breadcrumb from 'app/views/performance/breadcrumb';
@@ -135,23 +138,35 @@ class TransactionHeader extends React.Component<Props> {
               query: location.query,
             });
             return (
-              <StyledNavTabs>
-                <ListLink
-                  to={summaryTarget}
-                  isActive={() => currentTab === Tab.TransactionSummary}
-                >
-                  {t('Overview')}
-                </ListLink>
-                {hasWebVitals && (
-                  <ListLink
-                    to={vitalsTarget}
-                    isActive={() => currentTab === Tab.RealUserMonitoring}
-                  >
-                    {t('Web Vitals')}
-                    <FeatureBadge type="beta" />
-                  </ListLink>
+              <React.Fragment>
+                {currentTab === Tab.RealUserMonitoring && (
+                  <Alert type="info" icon={<IconInfo size="md" />}>
+                    {t(
+                      "Web vitals is a new beta feature for organizations who have turned on Early Adopter in their account settings. We'd love to hear any feedback you have at"
+                    )}{' '}
+                    <ExternalLink href="mailto:performance-feedback@sentry.io">
+                      performance-feedback@sentry.io
+                    </ExternalLink>
+                  </Alert>
                 )}
-              </StyledNavTabs>
+                <StyledNavTabs>
+                  <ListLink
+                    to={summaryTarget}
+                    isActive={() => currentTab === Tab.TransactionSummary}
+                  >
+                    {t('Overview')}
+                  </ListLink>
+                  {hasWebVitals && (
+                    <ListLink
+                      to={vitalsTarget}
+                      isActive={() => currentTab === Tab.RealUserMonitoring}
+                    >
+                      {t('Web Vitals')}
+                      <FeatureBadge type="beta" />
+                    </ListLink>
+                  )}
+                </StyledNavTabs>
+              </React.Fragment>
             );
           }}
         </Feature>
