8afb41a4b5b223c80e89e7ae4d436e53f42526c4
Burak Yigit Kaya <ben@byk.im>
2020-10-28 21:04:37 +0300

    fix indents

--- src/sentry/runner/commands/run.py
+++ src/sentry/runner/commands/run.py
@@ -226,20 +226,22 @@ def worker(ignore_unknown_queues, **options):
             unknown_queues = options["queues"] - known_queues
             message = "Following queues are not found: %s".format(unkown_queues)
             if ignore_unknown_queues:
-                    options["queues"] -= unknown_queues
-                    click.echo(message)
-                else:
-                    raise click.ClickException(message)
+                options["queues"] -= unknown_queues
+                click.echo(message)
+            else:
+                raise click.ClickException(message)
 
     if options["exclude_queues"] is not None:
         if not options["exclude_queues"].issubset(known_queues):
             unknown_queues = options["exclude_queues"] - known_queues
-            message = "Following queues cannot be excluded as they don't exist: %s".format(unkown_queues)
+            message = "Following queues cannot be excluded as they don't exist: %s".format(
+                unkown_queues
+            )
             if ignore_unknown_queues:
-                    options["exclude_queues"] -= unknown_queues
-                    click.echo(message)
-                else:
-                    raise click.ClickException(message)
+                options["exclude_queues"] -= unknown_queues
+                click.echo(message)
+            else:
+                raise click.ClickException(message)
 
     if options["autoreload"]:
         from django.utils import autoreload
