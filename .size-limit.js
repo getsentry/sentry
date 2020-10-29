5b9d85e792b07e4a62f57146ad6a7db1c833845f
Billy Vong <billyvg@users.noreply.github.com>
2020-10-29 16:21:50 -0700

    build(gha): Add `sentry.css` to size limits (#21675)
    Also update size-limit action to skip comments if bundle changes is not more than 0.005%

--- .size-limit.js
+++ .size-limit.js
@@ -1,14 +1,21 @@
 module.exports = [
   {
     path: ['public/app.js'],
-    limit: '1.6MB',
+    limit: '500KB',
     webpack: false,
     gzip: true,
     brotli: false,
   },
   {
     path: ['public/vendor.js'],
-    limit: '4.0MB',
+    limit: '1.0MB',
+    webpack: false,
+    gzip: true,
+    brotli: false,
+  },
+  {
+    path: ['public/sentry.css'],
+    limit: '50KB',
     webpack: false,
     gzip: true,
     brotli: false,
