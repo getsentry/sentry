90da5f7f511bf52a4e36d4ffea23bd562bfd4ff8
Chris Fuller <cfuller@sentry.io>
2020-11-03 14:08:56 -0500

    feat(workflow): Amend GroupInbox model with organization and project (#21729)
    * Adding org + project to groupinbox model
    
    * Marking migration as dangerous
    
    * Nuking table before migration

--- src/sentry/models/groupinbox.py
+++ src/sentry/models/groupinbox.py
@@ -23,6 +23,9 @@ class GroupInbox(Model):
     __core__ = False
 
     group = FlexibleForeignKey("sentry.Group", unique=True, db_constraint=False)
+    project = FlexibleForeignKey("sentry.Project", null=True, db_constraint=False)
+    organization = FlexibleForeignKey("sentry.Organization", null=True, db_constraint=False)
+
     reason = models.PositiveSmallIntegerField(null=False, default=GroupInboxReason.NEW.value)
     reason_details = JSONField(null=True)
     date_added = models.DateTimeField(default=timezone.now)
