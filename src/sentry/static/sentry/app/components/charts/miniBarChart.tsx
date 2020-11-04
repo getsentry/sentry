a5798d6b0000fcc5d62fc719bbf89d4a95dec3d3
k-fish <kevan.fisher@sentry.io>
2020-11-04 14:48:34 -0800

    ref(ts): Convert barChart to typescript (#21526)
    * ref(ts): Convert barChart to typescript
    
    Converts barChart to typescript. Modified the incoming type for BarSeries to get around this temporarily as anything using BarSeries should be able to pass in correct (per the docs) options, and we can force the type for the time being

--- src/sentry/static/sentry/app/components/charts/miniBarChart.tsx
+++ src/sentry/static/sentry/app/components/charts/miniBarChart.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import {EChartOption} from 'echarts';
 import set from 'lodash/set';
 
 import theme from 'app/utils/theme';
 import {getFormattedDate} from 'app/utils/dates';
 
-import BarChart from './barChart';
+import BarChart, {BarChartSeries} from './barChart';
 import BaseChart from './baseChart';
 import {truncationFormatter} from './utils';
+import XAxis from './components/xAxis';
+import Tooltip from './components/tooltip';
 
 type Marker = {
   name: string;
@@ -35,7 +36,11 @@ const defaultProps = {
   stacked: false,
 };
 
-type Props = React.ComponentProps<typeof BaseChart> &
+type ChartProps = React.ComponentProps<typeof BaseChart>;
+
+type BarChartProps = React.ComponentProps<typeof BarChart>;
+
+type Props = Omit<ChartProps, 'series'> &
   typeof defaultProps & {
     /**
      * A list of series to be rendered as markLine components on the chart
@@ -52,6 +57,8 @@ type Props = React.ComponentProps<typeof BaseChart> &
      * You can use this prop to also shift colors on hover.
      */
     emphasisColors?: string[];
+
+    series?: BarChartProps['series'];
   };
 
 class MiniBarChart extends React.Component<Props> {
@@ -68,7 +75,10 @@ class MiniBarChart extends React.Component<Props> {
       series,
       ...props
     } = this.props;
-    let chartSeries: EChartOption.SeriesBar[] = [];
+
+    const {ref: _ref, ...barChartProps} = props;
+
+    let chartSeries: BarChartSeries[] = [];
 
     // Ensure bars overlap and that empty values display as we're disabling the axis lines.
     if (series && series.length) {
@@ -77,7 +87,7 @@ class MiniBarChart extends React.Component<Props> {
           ...original,
           cursor: 'normal',
           type: 'bar',
-        } as EChartOption.SeriesBar;
+        } as BarChartSeries;
 
         if (i === 0) {
           updated.barMinHeight = 1;
@@ -148,9 +158,9 @@ class MiniBarChart extends React.Component<Props> {
         };
 
     const chartOptions = {
-      tooltip: {
+      tooltip: Tooltip({
         trigger: 'axis',
-      },
+      }),
       yAxis: {
         max(value) {
           // This keeps small datasets from looking 'scary'
@@ -170,7 +180,7 @@ class MiniBarChart extends React.Component<Props> {
         left: markers ? 4 : 0,
         right: markers ? 4 : 0,
       },
-      xAxis: {
+      xAxis: XAxis({
         axisLine: {
           show: false,
         },
@@ -190,12 +200,12 @@ class MiniBarChart extends React.Component<Props> {
             width: 0,
           },
         },
-      },
+      }),
       options: {
         animation: false,
       },
     };
-    return <BarChart series={chartSeries} {...chartOptions} {...props} />;
+    return <BarChart series={chartSeries} {...chartOptions} {...barChartProps} />;
   }
 }
 
