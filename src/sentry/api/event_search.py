9d81c4082e117957e8f4c755772624f00ca9c753
William Mak <william@wmak.io>
2020-11-04 15:36:40 -0500

    fix(discover): Support numeric fields with decimals
    - Numeric fields can support decimals, so in general we should just use
      `float` instead of `int`

--- src/sentry/api/event_search.py
+++ src/sentry/api/event_search.py
@@ -437,10 +437,7 @@ class SearchVisitor(NodeVisitor):
 
         if self.is_numeric_key(search_key.name):
             try:
-                if is_measurement(search_key.name):
-                    search_value = SearchValue(float(search_value.text))
-                else:
-                    search_value = SearchValue(int(search_value.text))
+                search_value = SearchValue(float(search_value.text))
             except ValueError:
                 raise InvalidSearchQuery(u"Invalid numeric query: {}".format(search_key))
             return SearchFilter(search_key, operator, search_value)
