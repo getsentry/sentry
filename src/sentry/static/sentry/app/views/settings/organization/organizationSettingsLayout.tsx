eaf0264eb1ee5c0a011b3fe06a97fd04eb975935
Matej Minar <matej.minar@sentry.io>
2020-11-06 10:14:15 +0100

    ref(ts): Convert OrganizationSettingsLayout to typescript (#21812)

---
+++ src/sentry/static/sentry/app/views/settings/organization/organizationSettingsLayout.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {RouteComponentProps} from 'react-router/lib/Router';
+
+import OrganizationSettingsNavigation from 'app/views/settings/organization/organizationSettingsNavigation';
+import SettingsLayout from 'app/views/settings/components/settingsLayout';
+
+type Props = RouteComponentProps<{orgId: string}, {}> & {
+  children: React.ReactNode;
+};
+
+function OrganizationSettingsLayout(props: Props) {
+  return (
+    <SettingsLayout
+      {...props}
+      renderNavigation={() => <OrganizationSettingsNavigation />}
+    />
+  );
+}
+
+export default OrganizationSettingsLayout;
