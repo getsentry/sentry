94fe904739c0d96918aa3c10f8cf1796450454e9
Chris Fuller <cfuller@sentry.io>
2020-11-06 16:23:30 -0500

    Adding search utils test

--- tests/sentry/search/test_utils.py
+++ tests/sentry/search/test_utils.py
@@ -404,6 +404,12 @@ class ParseQueryTest(TestCase):
         result = self.parse_query("is:assigned")
         assert result == {"unassigned": False, "tags": {}, "query": ""}
 
+    def test_is_inbox(self):
+        result = self.parse_query("is:inbox")
+        assert result == {"inbox": True, "tags": {}, "query": ""}
+        result = self.parse_query("!is:inbox")
+        assert result == {"inbox": False, "tags": {}, "query": ""}
+
     def test_age_from(self):
         result = self.parse_query("age:-24h")
         assert result["age_from"] > timezone.now() - timedelta(hours=25)
