bb0e8dc857d8722c4e85390c997fbbf4934b373f
Marcos Gaeta <marcos.gaeta@sentry.io>
2020-11-05 17:16:27 -0800

    Add form fields

--- src/sentry/integrations/jira/notify_action.py
+++ src/sentry/integrations/jira/notify_action.py
@@ -4,37 +4,101 @@ import logging
 
 from django import forms
 
-from sentry.rules.actions.base import TicketEventAction
 from sentry.models import ExternalIssue
+from sentry.models.integration import Integration
+from sentry.rules.actions.base import TicketEventAction
+from sentry.shared_integrations.exceptions import IntegrationError
 from sentry.utils.http import absolute_uri
 
 logger = logging.getLogger("sentry.rules")
 
 
 class JiraNotifyServiceForm(forms.Form):
-    # TODO 1.0 Add form fields.
+    jira_integration = forms.ChoiceField(choices=(), widget=forms.Select())
 
     def __init__(self, *args, **kwargs):
+        integrations = [(i.id, i.name) for i in kwargs.pop("integrations")]
         super(JiraNotifyServiceForm, self).__init__(*args, **kwargs)
 
-    def clean(self):
-        return super(JiraNotifyServiceForm, self).clean()
+        if integrations:
+            self.fields["jira_integration"].initial = integrations[0][0]
+
+        self.fields["jira_integration"].choices = integrations
+        self.fields["jira_integration"].widget.choices = self.fields["jira_integration"].choices
 
 
 class JiraCreateTicketAction(TicketEventAction):
     form_cls = JiraNotifyServiceForm
-    label = u"TODO Create a {name} Jira ticket"
+    label = u"""Create a Jira ticket in the {jira_integration} account
+    and {project} project
+    of type {issuetype}
+    with components {components}
+    and due date {duedate}
+    with fixVersions {fixVersions}
+    assigned to {assignee}
+    reported by {reporter}
+    label {labels}
+    priority {priority}"""
     prompt = "Create a Jira ticket"
     provider = "jira"
     integration_key = "jira_integration"
 
     def __init__(self, *args, **kwargs):
         super(JiraCreateTicketAction, self).__init__(*args, **kwargs)
-        # TODO 1.1 Add form_fields
-        self.form_fields = {}
+        integration_choices = [(i.id, i.name) for i in self.get_integrations()]
+
+        if not self.get_integration_id() and integration_choices:
+            self.data[self.integration_key] = integration_choices[0][0]
+
+        self.form_fields = {
+            "jira_integration": {
+                "choices": integration_choices,
+                "default": self.get_integration_id(),
+                "type": "choice",
+                "updatesForm": True,
+            }
+        }
+
+        try:
+            integration = self.get_integration()
+        except Integration.DoesNotExist:
+            return
+
+        installation = integration.get_installation(self.project.organization.id)
+        if installation:
+            try:
+                fields = installation.get_create_issue_config_no_params()
+            except IntegrationError as e:
+                # TODO log when the API call fails.
+                logger.info(e)
+                return
+
+            self.update_form_fields_from_jira_fields(fields)
 
     def render_label(self):
         return self.label.format(name=self.get_integration_name())
+
+    def update_form_fields_from_jira_fields(self, fields_list):
+        """
+        The fields array from Jira doesn't exactly match the Alert Rules front
+        end's expected format. Massage the field names and types and put them in a dict.
+
+        :param fields_list: Create ticket fields from Jira as an array.
+        :return: The "create ticket" fields from Jira a dict.
+        """
+        self.form_fields.update(
+            {
+                field["name"]: {
+                    key: ({"select": "choice", "text": "string"}.get(value, value))
+                    if key == "type"
+                    else value
+                    for key, value in field.items()
+                    if key != "updatesForm"
+                }
+                for field in fields_list
+                if field["name"]
+            }
+        )
 
     def generate_footer(self, rule_url):
         return u"This ticket was automatically created by Sentry via [{}|{}]".format(
