232e1c342c149cd1f1b1f7d7d77cb9556a99e1f3
Colleen O'Rourke <colleen@sentry.io>
2020-11-05 17:22:33 -0800

    Rm hacked in form field stuff

--- src/sentry/integrations/jira/notify_action.py
+++ src/sentry/integrations/jira/notify_action.py
@@ -11,74 +11,29 @@ logger = logging.getLogger("sentry.rules")
 
 
 class JiraNotifyServiceForm(forms.Form):
-    jira_project = forms.ChoiceField(choices=(), widget=forms.Select())
-    issue_type = forms.ChoiceField(choices=(), widget=forms.Select())
+    # TODO 1.0 Add form fields.
 
     def __init__(self, *args, **kwargs):
-        projects_list = [(i.id, i.name) for i in kwargs.pop("integrations")]
         super(JiraNotifyServiceForm, self).__init__(*args, **kwargs)
-
-        if projects_list:
-            self.fields["jira_project"].initial = projects_list[0][0]
-
-        self.fields["jira_project"].choices = projects_list
-        self.fields["jira_project"].widget.choices = self.fields["jira_project"].choices
-
-        self.fields["issuetype"].choices = HARDCODED_ISSUE_TYPES
-        self.fields["issuetype"].widget.choices = self.fields["issuetype"].choices
 
     def clean(self):
         return super(JiraNotifyServiceForm, self).clean()
 
 
-# TODO instead of hard-coding things, lets automatically select the first integration.
-HARDCODED_ISSUE_TYPES = [
-    ("Bug", "Bug"),
-    ("Issue", "Issue"),
-    ("Task", "Task"),
-]
-
-
 class JiraCreateTicketAction(IntegrationEventAction):
     form_cls = JiraNotifyServiceForm
-    label = u"TODO Create a {jira_integration} {jira_project} Jira ticket, name: {name} type: {issuetype}!"
+    label = u"TODO Create a {name} Jira ticket"
     prompt = "Create a Jira ticket"
     provider = "jira"
     integration_key = "jira_integration"
 
     def __init__(self, *args, **kwargs):
         super(JiraCreateTicketAction, self).__init__(*args, **kwargs)
-        all_integrations = self.get_integrations()
-        integration_choices = [(i.id, i.name) for i in all_integrations]
-
-        self.form_fields = {
-            "jira_integration": {
-                "type": "choice",
-                "choices": integration_choices,
-                "default": integration_choices[0][0],
-                "updatesForm": True,
-            },
-            "jira_project": {
-                "type": "choice",
-                "choices": integration_choices,
-                "default": integration_choices[0][0],
-                "updatesForm": True,
-            },
-            "issuetype": {
-                "type": "choice",
-                "choices": HARDCODED_ISSUE_TYPES,
-                "default": HARDCODED_ISSUE_TYPES[0][0],
-            },
-            "name": {"type": "string"},
-        }
+        # TODO 1.1 Add form_fields
+        self.form_fields = {}
 
     def render_label(self):
-        return self.label.format(
-            name=self.get_integration_name(),
-            jira_integration="JIRA",
-            jira_project="JIRA",
-            issue_type="Bug",
-        )
+        return self.label.format(name=self.get_integration_name())
 
     def after(self, event, state):
         integration = self.get_integration()
