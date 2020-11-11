getsentry/sentry

commit e03f91cecb2b5ed07afcf966ed5c9b9c2fea7396

feat(ui): Allow manual opt in to dark mode (#21915)
Note, dark mode is not ready yet, but this allows you to use the Command Palette (staff only) to toggle dark mode. This will allow us to develop dark mode more easily.

src/sentry/static/sentry/app/main.tsx
@@ -1,12 +1,9 @@
 import {CacheProvider} from '@emotion/core'; // This is needed to set "speedy" = false (for percy)
-import {ThemeProvider} from 'emotion-theming';
 import {cache} from 'emotion'; // eslint-disable-line emotion/no-vanilla
 import React from 'react';
 import {Router, browserHistory} from 'react-router';
 
-import GlobalStyles from 'app/styles/global';
 import routes from 'app/routes';
-import theme from 'app/utils/theme';
 import {loadPreferencesState} from 'app/actionCreators/preferences';
 
 class Main extends React.Component {
@@ -17,10 +14,7 @@ class Main extends React.Component {
   render() {
     return (
       <CacheProvider value={cache}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles theme={theme} />
-          <Router history={browserHistory}>{routes()}</Router>
-        </ThemeProvider>
+        <Router history={browserHistory}>{routes()}</Router>
       </CacheProvider>
     );
   }

src/sentry/static/sentry/app/views/app/index.tsx
@@ -1,5 +1,6 @@
 import $ from 'jquery';
 import {RouteComponentProps} from 'react-router/lib/Router';
+import {ThemeProvider} from 'emotion-theming';
 import {browserHistory} from 'react-router';
 import Cookies from 'js-cookie';
 import PropTypes from 'prop-types';
@@ -21,11 +22,13 @@ import AlertActions from 'app/actions/alertActions';
 import ConfigStore from 'app/stores/configStore';
 import ErrorBoundary from 'app/components/errorBoundary';
 import GlobalModal from 'app/components/globalModal';
+import GlobalStyles from 'app/styles/global';
 import HookStore from 'app/stores/hookStore';
 import Indicators from 'app/components/indicators';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import NewsletterConsent from 'app/views/newsletterConsent';
 import OrganizationsStore from 'app/stores/organizationsStore';
+import theme, {darkTheme, Theme} from 'app/utils/theme';
 import withApi from 'app/utils/withApi';
 import withConfig from 'app/utils/withConfig';
 
@@ -58,6 +61,7 @@ type State = {
   error: boolean;
   needsUpgrade: boolean;
   newsletterConsentPrompt: boolean;
+  theme: Theme;
   user?: Config['user'];
 };
 
@@ -71,6 +75,7 @@ class App extends React.Component<Props, State> {
     error: false,
     needsUpgrade: ConfigStore.get('user')?.isSuperuser && ConfigStore.get('needsUpgrade'),
     newsletterConsentPrompt: ConfigStore.get('user')?.flags?.newsletter_consent_prompt,
+    theme: ConfigStore.get('theme') === 'dark' ? darkTheme : theme,
   };
 
   getChildContext() {
@@ -189,9 +194,15 @@ class App extends React.Component<Props, State> {
     if (config.needsUpgrade !== undefined) {
       newState.needsUpgrade = config.needsUpgrade;
     }
+
     if (config.user !== undefined) {
       newState.user = config.user;
     }
+
+    if (config.theme !== undefined) {
+      newState.theme = config.theme === 'dark' ? darkTheme : theme;
+    }
+
     if (Object.keys(newState).length > 0) {
       this.setState(newState);
     }
@@ -252,12 +263,15 @@ class App extends React.Component<Props, State> {
     }
 
     return (
-      <div className="main-container" tabIndex={-1} ref={this.mainContainerRef}>
-        <GlobalModal onClose={this.handleGlobalModalClose} />
-        <SystemAlerts className="messages-container" />
-        <Indicators className="indicators-container" />
-        <ErrorBoundary>{this.renderBody()}</ErrorBoundary>
-      </div>
+      <ThemeProvider<Theme> theme={this.state.theme}>
+        <GlobalStyles theme={this.state.theme} />
+        <div className="main-container" tabIndex={-1} ref={this.mainContainerRef}>
+          <GlobalModal onClose={this.handleGlobalModalClose} />
+          <SystemAlerts className="messages-container" />
+          <Indicators className="indicators-container" />
+          <ErrorBoundary>{this.renderBody()}</ErrorBoundary>
+        </div>
+      </ThemeProvider>
     );
   }
 }

src/sentry/static/sentry/app/types/index.tsx
@@ -656,6 +656,7 @@ export type EnrolledAuthenticator = {
 };
 
 export interface Config {
+  theme: 'light' | 'dark';
   languageCode: string;
   csrfCookieName: string;
   features: Set<string>;

.github/workflows/js-build-and-lint.yml
@@ -86,7 +86,6 @@ jobs:
 
       - name: tsc
         if: steps.changes.outputs.frontend == 'true'
-        continue-on-error: true
         run: |
           yarn tsc -p config/tsconfig.build.json
 

src/sentry/static/sentry/app/bootstrap.tsx
@@ -20,14 +20,13 @@ import * as Sentry from '@sentry/react';
 
 import {NODE_ENV, DISABLE_RR_WEB, SPA_DSN} from 'app/constants';
 import {metric} from 'app/utils/analytics';
+import {setupColorScheme} from 'app/utils/matchMedia';
 import {init as initApiSentryClient} from 'app/utils/apiSentryClient';
 import ConfigStore from 'app/stores/configStore';
 import Main from 'app/main';
 import ajaxCsrfSetup from 'app/utils/ajaxCsrfSetup';
 import plugins from 'app/plugins';
 import routes from 'app/routes';
-
-import {setupFavicon} from './favicon';
 
 if (NODE_ENV === 'development') {
   import(
@@ -134,9 +133,8 @@ const render = (Component: React.ComponentType) => {
   }
 };
 
-if (NODE_ENV === 'production') {
-  setupFavicon();
-}
+// setup darkmode + favicon
+setupColorScheme();
 
 // The password strength component is very heavyweight as it includes the
 // zxcvbn, a relatively byte-heavy password strength estimation library. Load

src/sentry/static/sentry/app/components/search/sources/commandSource.jsx
@@ -5,6 +5,7 @@ import {createFuzzySearch} from 'app/utils/createFuzzySearch';
 import {openSudo, openHelpSearchModal} from 'app/actionCreators/modal';
 import {toggleLocaleDebug} from 'app/locale';
 import Access from 'app/components/acl/access';
+import ConfigStore from 'app/stores/configStore';
 
 const ACTIONS = [
   {
@@ -24,6 +25,14 @@ const ACTIONS = [
       openSudo({
         superuser: true,
       }),
+  },
+
+  {
+    title: 'Toggle dark mode',
+    description: 'Toggle dark mode (superuser only atm)',
+    requiresSuperuser: true,
+    action: () =>
+      ConfigStore.set('theme', ConfigStore.get('theme') === 'dark' ? 'light' : 'dark'),
   },
 
   {

src/sentry/static/sentry/app/favicon.tsx
@@ -1,27 +1,0 @@
-function changeFavicon(theme: 'dark' | 'light'): void {
-  const n = document.querySelector<HTMLLinkElement>('[rel="icon"][type="image/png"]');
-  if (!n) {
-    return;
-  }
-
-  const path = n.href.split('/sentry/')[0];
-  n.href = `${path}/sentry/images/${theme === 'dark' ? 'favicon-dark' : 'favicon'}.png`;
-}
-
-function prefersDark(): boolean {
-  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-}
-
-function updateFavicon(): void {
-  changeFavicon(prefersDark() ? 'dark' : 'light');
-}
-
-export function setupFavicon(): void {
-  // Set favicon to dark on load
-  if (prefersDark()) {
-    changeFavicon('dark');
-  }
-
-  // Watch for changes in preferred color scheme
-  window.matchMedia('(prefers-color-scheme: dark)').addListener(updateFavicon);
-}

src/sentry/static/sentry/app/stores/configStore.tsx
@@ -28,7 +28,10 @@ const configStoreConfig: Reflux.StoreDefinition & ConfigStoreInterface = {
   },
 
   set(key, value) {
-    this.config[key] = value;
+    this.config = {
+      ...this.config,
+      [key]: value,
+    };
     this.trigger({[key]: value});
   },
 
@@ -37,8 +40,11 @@ const configStoreConfig: Reflux.StoreDefinition & ConfigStoreInterface = {
   },
 
   loadInitialData(config): void {
-    config.features = new Set(config.features || []);
-    this.config = config;
+    this.config = {
+      ...config,
+      features: new Set(config.features || []),
+      theme: 'light', // TODO(dark): The next step is to have this for staff: config.user.isStaff && prefersDark() ? 'dark' : 'light',
+    };
 
     // Language code is passed from django
     let languageCode = config.languageCode;

commit e6a9ffaf1fcc717379ea0cdb6e3f4fb8afe55231

fix(ui): Fix login page for SPA
The login page in our SPA mode (e.g. Vercel and `yarn dev-ui`) is broken because the lack of emotion theme. This regressed in https://github.com/getsentry/sentry/pull/21915

We did not move `<AuthLayout>` under the `app/views/app` route because it (`<App>`) expects the user to be logged in.  Instead move `<ThemeProvider>`  back into `app/main` where there is no such expectation.

src/sentry/static/sentry/app/main.tsx
@@ -1,23 +1,54 @@
 import {CacheProvider} from '@emotion/core'; // This is needed to set "speedy" = false (for percy)
+import {ThemeProvider} from 'emotion-theming';
 import {cache} from 'emotion'; // eslint-disable-line emotion/no-vanilla
 import React from 'react';
 import {Router, browserHistory} from 'react-router';
 
-import routes from 'app/routes';
+import {Config} from 'app/types';
 import {loadPreferencesState} from 'app/actionCreators/preferences';
+import ConfigStore from 'app/stores/configStore';
+import GlobalStyles from 'app/styles/global';
+import routes from 'app/routes';
+import theme, {darkTheme, Theme} from 'app/utils/theme';
+import withConfig from 'app/utils/withConfig';
 
-class Main extends React.Component {
+type Props = {
+  config: Config;
+};
+
+type State = {
+  theme: Theme;
+};
+
+class Main extends React.Component<Props, State> {
+  state = {
+    theme: ConfigStore.get('theme') === 'dark' ? darkTheme : theme,
+  };
+
   componentDidMount() {
     loadPreferencesState();
   }
 
+  componentDidUpdate(prevProps: Props) {
+    const {config} = this.props;
+    if (config.theme !== prevProps.config.theme) {
+      // eslint-disable-next-line
+      this.setState({
+        theme: config.theme === 'dark' ? darkTheme : theme,
+      });
+    }
+  }
+
   render() {
     return (
-      <CacheProvider value={cache}>
-        <Router history={browserHistory}>{routes()}</Router>
-      </CacheProvider>
+      <ThemeProvider<Theme> theme={this.state.theme}>
+        <GlobalStyles theme={this.state.theme} />
+        <CacheProvider value={cache}>
+          <Router history={browserHistory}>{routes()}</Router>
+        </CacheProvider>
+      </ThemeProvider>
     );
   }
 }
 
-export default Main;
+export default withConfig(Main);

src/sentry/static/sentry/app/views/app/index.tsx
@@ -1,6 +1,5 @@
 import $ from 'jquery';
 import {RouteComponentProps} from 'react-router/lib/Router';
-import {ThemeProvider} from 'emotion-theming';
 import {browserHistory} from 'react-router';
 import Cookies from 'js-cookie';
 import PropTypes from 'prop-types';
@@ -22,13 +21,11 @@ import AlertActions from 'app/actions/alertActions';
 import ConfigStore from 'app/stores/configStore';
 import ErrorBoundary from 'app/components/errorBoundary';
 import GlobalModal from 'app/components/globalModal';
-import GlobalStyles from 'app/styles/global';
 import HookStore from 'app/stores/hookStore';
 import Indicators from 'app/components/indicators';
 import LoadingIndicator from 'app/components/loadingIndicator';
 import NewsletterConsent from 'app/views/newsletterConsent';
 import OrganizationsStore from 'app/stores/organizationsStore';
-import theme, {darkTheme, Theme} from 'app/utils/theme';
 import withApi from 'app/utils/withApi';
 import withConfig from 'app/utils/withConfig';
 
@@ -61,7 +58,6 @@ type State = {
   error: boolean;
   needsUpgrade: boolean;
   newsletterConsentPrompt: boolean;
-  theme: Theme;
   user?: Config['user'];
 };
 
@@ -75,7 +71,6 @@ class App extends React.Component<Props, State> {
     error: false,
     needsUpgrade: ConfigStore.get('user')?.isSuperuser && ConfigStore.get('needsUpgrade'),
     newsletterConsentPrompt: ConfigStore.get('user')?.flags?.newsletter_consent_prompt,
-    theme: ConfigStore.get('theme') === 'dark' ? darkTheme : theme,
   };
 
   getChildContext() {
@@ -199,10 +194,6 @@ class App extends React.Component<Props, State> {
       newState.user = config.user;
     }
 
-    if (config.theme !== undefined) {
-      newState.theme = config.theme === 'dark' ? darkTheme : theme;
-    }
-
     if (Object.keys(newState).length > 0) {
       this.setState(newState);
     }
@@ -263,15 +254,12 @@ class App extends React.Component<Props, State> {
     }
 
     return (
-      <ThemeProvider<Theme> theme={this.state.theme}>
-        <GlobalStyles theme={this.state.theme} />
-        <div className="main-container" tabIndex={-1} ref={this.mainContainerRef}>
-          <GlobalModal onClose={this.handleGlobalModalClose} />
-          <SystemAlerts className="messages-container" />
-          <Indicators className="indicators-container" />
-          <ErrorBoundary>{this.renderBody()}</ErrorBoundary>
-        </div>
-      </ThemeProvider>
+      <div className="main-container" tabIndex={-1} ref={this.mainContainerRef}>
+        <GlobalModal onClose={this.handleGlobalModalClose} />
+        <SystemAlerts className="messages-container" />
+        <Indicators className="indicators-container" />
+        <ErrorBoundary>{this.renderBody()}</ErrorBoundary>
+      </div>
     );
   }
 }