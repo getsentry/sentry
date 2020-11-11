getsentry/sentry

commit e03f91cecb2b5ed07afcf966ed5c9b9c2fea7396

feat(ui): Allow manual opt in to dark mode (#21915)
Note, dark mode is not ready yet, but this allows you to use the Command Palette (staff only) to toggle dark mode. This will allow us to develop dark mode more easily.

src/sentry/static/sentry/app/utils/matchMedia.tsx
@@ -1,0 +1,44 @@
+import {NODE_ENV} from 'app/constants';
+
+function changeFavicon(theme: 'dark' | 'light'): void {
+  // only on prod because we have a development favicon
+  if (NODE_ENV !== 'production') {
+    return;
+  }
+
+  const n = document.querySelector<HTMLLinkElement>('[rel="icon"][type="image/png"]');
+  if (!n) {
+    return;
+  }
+
+  const path = n.href.split('/sentry/')[0];
+  n.href = `${path}/sentry/images/${theme === 'dark' ? 'favicon-dark' : 'favicon'}.png`;
+}
+
+function handleColorSchemeChange(e: MediaQueryListEvent): void {
+  const isDark = e.media === '(prefers-color-scheme: dark)' && e.matches;
+  const type = isDark ? 'dark' : 'light';
+  changeFavicon(type);
+  // TODO(dark): For now, you must opt into dark mode using command palette
+  // ConfigStore.set('theme', type);
+}
+
+export function prefersDark(): boolean {
+  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
+}
+
+export function setupColorScheme(): void {
+  // Set favicon to dark on load if necessary)
+  if (prefersDark()) {
+    changeFavicon('dark');
+    // TODO(dark): (currently manual opt in only): ConfigStore.set('theme', 'dark');
+  }
+
+  // Watch for changes in preferred color scheme
+  window
+    .matchMedia('(prefers-color-scheme: light)')
+    .addEventListener('change', handleColorSchemeChange);
+  window
+    .matchMedia('(prefers-color-scheme: dark)')
+    .addEventListener('change', handleColorSchemeChange);
+}

.github/workflows/js-build-and-lint.yml
@@ -86,7 +86,6 @@ jobs:
 
       - name: tsc
         if: steps.changes.outputs.frontend == 'true'
-        continue-on-error: true
         run: |
           yarn tsc -p config/tsconfig.build.json
 

src/sentry/static/sentry/app/bootstrap.tsx
@@ -20,14 +20,13 @@ import * as Sentry from '@sentry/react';
 
 import {NODE_ENV, DISABLE_RR_WEB, SPA_DSN} from 'app/constants';
 import {metric} from 'app/utils/analytics';
+import {setupColorScheme} from 'app/utils/matchMedia';
 import {init as initApiSentryClient} from 'app/utils/apiSentryClient';
 import ConfigStore from 'app/stores/configStore';
 import Main from 'app/main';
 import ajaxCsrfSetup from 'app/utils/ajaxCsrfSetup';
 import plugins from 'app/plugins';
 import routes from 'app/routes';
-
-import {setupFavicon} from './favicon';
 
 if (NODE_ENV === 'development') {
   import(
@@ -134,9 +133,8 @@ const render = (Component: React.ComponentType) => {
   }
 };
 
-if (NODE_ENV === 'production') {
-  setupFavicon();
-}
+// setup darkmode + favicon
+setupColorScheme();
 
 // The password strength component is very heavyweight as it includes the
 // zxcvbn, a relatively byte-heavy password strength estimation library. Load

src/sentry/static/sentry/app/components/search/sources/commandSource.jsx
@@ -5,6 +5,7 @@ import {createFuzzySearch} from 'app/utils/createFuzzySearch';
 import {openSudo, openHelpSearchModal} from 'app/actionCreators/modal';
 import {toggleLocaleDebug} from 'app/locale';
 import Access from 'app/components/acl/access';
+import ConfigStore from 'app/stores/configStore';
 
 const ACTIONS = [
   {
@@ -24,6 +25,14 @@ const ACTIONS = [
       openSudo({
         superuser: true,
       }),
+  },
+
+  {
+    title: 'Toggle dark mode',
+    description: 'Toggle dark mode (superuser only atm)',
+    requiresSuperuser: true,
+    action: () =>
+      ConfigStore.set('theme', ConfigStore.get('theme') === 'dark' ? 'light' : 'dark'),
   },
 
   {

src/sentry/static/sentry/app/favicon.tsx
@@ -1,27 +1,0 @@
-function changeFavicon(theme: 'dark' | 'light'): void {
-  const n = document.querySelector<HTMLLinkElement>('[rel="icon"][type="image/png"]');
-  if (!n) {
-    return;
-  }
-
-  const path = n.href.split('/sentry/')[0];
-  n.href = `${path}/sentry/images/${theme === 'dark' ? 'favicon-dark' : 'favicon'}.png`;
-}
-
-function prefersDark(): boolean {
-  return window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
-}
-
-function updateFavicon(): void {
-  changeFavicon(prefersDark() ? 'dark' : 'light');
-}
-
-export function setupFavicon(): void {
-  // Set favicon to dark on load
-  if (prefersDark()) {
-    changeFavicon('dark');
-  }
-
-  // Watch for changes in preferred color scheme
-  window.matchMedia('(prefers-color-scheme: dark)').addListener(updateFavicon);
-}

src/sentry/static/sentry/app/main.tsx
@@ -1,12 +1,9 @@
 import {CacheProvider} from '@emotion/core'; // This is needed to set "speedy" = false (for percy)
-import {ThemeProvider} from 'emotion-theming';
 import {cache} from 'emotion'; // eslint-disable-line emotion/no-vanilla
 import React from 'react';
 import {Router, browserHistory} from 'react-router';
 
-import GlobalStyles from 'app/styles/global';
 import routes from 'app/routes';
-import theme from 'app/utils/theme';
 import {loadPreferencesState} from 'app/actionCreators/preferences';
 
 class Main extends React.Component {
@@ -17,10 +14,7 @@ class Main extends React.Component {
   render() {
     return (
       <CacheProvider value={cache}>
-        <ThemeProvider theme={theme}>
-          <GlobalStyles theme={theme} />
-          <Router history={browserHistory}>{routes()}</Router>
-        </ThemeProvider>
+        <Router history={browserHistory}>{routes()}</Router>
       </CacheProvider>
     );
   }

src/sentry/static/sentry/app/stores/configStore.tsx
@@ -28,7 +28,10 @@ const configStoreConfig: Reflux.StoreDefinition & ConfigStoreInterface = {
   },
 
   set(key, value) {
-    this.config[key] = value;
+    this.config = {
+      ...this.config,
+      [key]: value,
+    };
     this.trigger({[key]: value});
   },
 
@@ -37,8 +40,11 @@ const configStoreConfig: Reflux.StoreDefinition & ConfigStoreInterface = {
   },
 
   loadInitialData(config): void {
-    config.features = new Set(config.features || []);
-    this.config = config;
+    this.config = {
+      ...config,
+      features: new Set(config.features || []),
+      theme: 'light', // TODO(dark): The next step is to have this for staff: config.user.isStaff && prefersDark() ? 'dark' : 'light',
+    };
 
     // Language code is passed from django
     let languageCode = config.languageCode;

src/sentry/static/sentry/app/types/index.tsx
@@ -656,6 +656,7 @@ export type EnrolledAuthenticator = {
 };
 
 export interface Config {
+  theme: 'light' | 'dark';
   languageCode: string;
   csrfCookieName: string;
   features: Set<string>;

commit f3c8315cf33c5ae06df690f891b1d541fc21fc74

fix(ui): Fix media query for Safari <= 13
This fixes a bug introduced in #21915 with dark mode media query. `addListener` is deprecated, however Safari does not support the newer method `addEventListener`.

Fixes JAVASCRIPT-236A

src/sentry/static/sentry/app/utils/matchMedia.tsx
@@ -35,10 +35,17 @@ export function setupColorScheme(): void {
   }
 
   // Watch for changes in preferred color scheme
-  window
-    .matchMedia('(prefers-color-scheme: light)')
-    .addEventListener('change', handleColorSchemeChange);
-  window
-    .matchMedia('(prefers-color-scheme: dark)')
-    .addEventListener('change', handleColorSchemeChange);
+  const lightMediaQuery = window.matchMedia('(prefers-color-scheme: light)');
+
+  const darkMediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+
+  try {
+    lightMediaQuery.addEventListener('change', handleColorSchemeChange);
+    darkMediaQuery.addEventListener('change', handleColorSchemeChange);
+  } catch (err) {
+    // Safari 13 (maybe lower too) does not support `addEventListener`
+    // `addListener` is deprecated
+    lightMediaQuery.addListener(handleColorSchemeChange);
+    darkMediaQuery.addListener(handleColorSchemeChange);
+  }
 }