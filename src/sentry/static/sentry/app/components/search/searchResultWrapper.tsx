2e756861821f0849d47377a8c76421fd8011f445
Billy Vong <billyvg@users.noreply.github.com>
2020-11-11 10:03:04 -0800

    ref(ts): Convert `<SearchResultWrapper>` (#21933)

--- src/sentry/static/sentry/app/components/search/searchResultWrapper.tsx
+++ src/sentry/static/sentry/app/components/search/searchResultWrapper.tsx
@@ -1,17 +1,20 @@
 import styled from '@emotion/styled';
 import {css} from '@emotion/core';
 import React from 'react';
-import omit from 'lodash/omit';
 
-const SearchResultWrapper = styled(props => (
+type Props = {
+  highlighted?: boolean;
+} & React.HTMLProps<HTMLDivElement>;
+
+const SearchResultWrapper = styled(({highlighted, ...props}: Props) => (
   <div
-    {...omit(props, 'highlighted')}
-    ref={element => props.highlighted && element?.scrollIntoView?.({block: 'nearest'})}
+    {...props}
+    ref={element => highlighted && element?.scrollIntoView?.({block: 'nearest'})}
   />
 ))`
   cursor: pointer;
   display: block;
-  color: ${p => p.theme.gray500};
+  color: ${p => p.theme.textColor};
   padding: 10px;
   scroll-margin: 120px;
 
