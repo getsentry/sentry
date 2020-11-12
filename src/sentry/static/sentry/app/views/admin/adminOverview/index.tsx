b4e9425fbdb1253ed45f80ba79c75f851269ace7
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-11-12 02:26:38 -0800

    chore(ts): Convert adminOverview/index (#21980)

---
+++ src/sentry/static/sentry/app/views/admin/adminOverview/index.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import DocumentTitle from 'react-document-title';
+
+import {Panel, PanelHeader, PanelBody} from 'app/components/panels';
+import {t} from 'app/locale';
+
+import ApiChart from './apiChart';
+import EventChart from './eventChart';
+
+const AdminOverview = () => {
+  const resolution = '1h';
+  const since = new Date().getTime() / 1000 - 3600 * 24 * 7;
+
+  return (
+    <DocumentTitle title="Admin Overview - Sentry">
+      <h3>{t('System Overview')}</h3>
+
+      <Panel key="events">
+        <PanelHeader>{t('Event Throughput')}</PanelHeader>
+        <PanelBody withPadding>
+          <EventChart since={since} resolution={resolution} />
+        </PanelBody>
+      </Panel>
+
+      <Panel key="api">
+        <PanelHeader>{t('API Responses')}</PanelHeader>
+        <PanelBody withPadding>
+          <ApiChart since={since} resolution={resolution} />
+        </PanelBody>
+      </Panel>
+    </DocumentTitle>
+  );
+};
+
+export default AdminOverview;
