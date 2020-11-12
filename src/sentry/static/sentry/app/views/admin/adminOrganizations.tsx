11d9ce4393481c3f1e9a35c23112777eb8637779
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-11-12 02:13:28 -0800

    chore(ts): Convert adminOrganizations (#21965)

---
+++ src/sentry/static/sentry/app/views/admin/adminOrganizations.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import {Link, RouteComponentProps} from 'react-router';
+
+import ResultGrid from 'app/components/resultGrid';
+import {t} from 'app/locale';
+
+type Props = RouteComponentProps<{}, {}>;
+
+const getRow = (row: any) => [
+  <td key={row.id}>
+    <strong>
+      <Link to={`/${row.slug}/`}>{row.name}</Link>
+    </strong>
+    <br />
+    <small>{row.slug}</small>
+  </td>,
+];
+
+const AdminOrganizations = (props: Props) => (
+  <div>
+    <h3>{t('Organizations')}</h3>
+    <ResultGrid
+      path="/manage/organizations/"
+      endpoint="/organizations/?show=all"
+      method="GET"
+      columns={[<th key="column-org">Organization</th>]}
+      columnsForRow={getRow}
+      hasSearch
+      sortOptions={[
+        ['date', 'Date Joined'],
+        ['members', 'Members'],
+        ['events', 'Events'],
+        ['projects', 'Projects'],
+        ['employees', 'Employees'],
+      ]}
+      defaultSort="date"
+      {...props}
+    />
+  </div>
+);
+
+export default AdminOrganizations;
