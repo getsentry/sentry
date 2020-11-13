8b70c04ed1769cbc0270fb7d10d2f4844180fa5c
Tony Xiao <txiao@sentry.io>
2020-11-13 13:26:01 -0500

    document props

--- src/sentry/static/sentry/app/components/discover/transactionsList.tsx
+++ src/sentry/static/sentry/app/components/discover/transactionsList.tsx
@@ -27,8 +27,17 @@ import {GridCell, GridCellNumber} from 'app/views/performance/styles';
 const DEFAULT_TRANSACTION_LIMIT = 5;
 
 export type DropdownOption = {
+  /**
+   * The sort to apply to the eventView when this is selected.
+   */
   sort: Sort;
+  /**
+   * The unique name to use for this option.
+   */
   value: string;
+  /**
+   * The label to display in the dropdown
+   */
   label: string;
 };
 
@@ -37,14 +46,41 @@ type Props = {
   location: Location;
   eventView: EventView;
   organization: Organization;
+  /**
+   * The prefix to use on the dropdown button.
+   */
   dropdownTitle: string;
+  /**
+   * The currently selected option on the dropdown.
+   */
   selected: DropdownOption;
+  /**
+   * The available options for the dropdown.
+   */
   options: DropdownOption[];
+  /**
+   * The callback for when the dropdown option changes.
+   */
   handleDropdownChange: any;
+  /**
+   * The name of the url parameter that contains the cursor info.
+   */
   cursorName: string;
+  /**
+   * The limit to the number of results to fetch.
+   */
   limit: number;
+  /**
+   * A list of preferred table headers to use over the field names.
+   */
   titles?: string[];
-  dataTestId?: string;
+  /**
+   * Alternate data-test-id to use for the optional links in the first column.
+   */
+  linkDataTestId?: string;
+  /**
+   * The callback to generate a link for the first column.
+   */
   generateFirstLink?: (
     organization: Organization,
     tableRow: TableDataRow,
@@ -118,7 +154,7 @@ class TransactionsList extends React.Component<Props> {
       cursorName,
       limit,
       titles,
-      dataTestId,
+      linkDataTestId,
       generateFirstLink,
     } = this.props;
     const sortedEventView = eventView.withSorts([selected.sort]);
@@ -141,7 +177,7 @@ class TransactionsList extends React.Component<Props> {
               isLoading={isLoading}
               tableData={tableData}
               titles={titles}
-              dataTestId={dataTestId}
+              linkDataTestId={linkDataTestId}
               generateFirstLink={generateFirstLink}
             />
             <StyledPagination
@@ -172,7 +208,7 @@ type TableProps = {
   isLoading: boolean;
   tableData: TableData | null | undefined;
   titles?: string[];
-  dataTestId?: string;
+  linkDataTestId?: string;
   generateFirstLink?: (
     organization: Organization,
     tableRow: TableDataRow,
@@ -214,7 +250,7 @@ class TransactionsTable extends React.PureComponent<TableProps> {
     columnOrder: TableColumn<React.ReactText>[],
     tableMeta: MetaType
   ): React.ReactNode[] {
-    const {organization, location, dataTestId, generateFirstLink} = this.props;
+    const {organization, location, linkDataTestId, generateFirstLink} = this.props;
 
     const resultsRow = columnOrder.map((column, index) => {
       const field = String(column.key);
@@ -231,7 +267,7 @@ class TransactionsTable extends React.PureComponent<TableProps> {
         if (isFirstCell) {
           const target = generateFirstLink(organization, row, location.query);
           rendered = (
-            <Link data-test-id={dataTestId ?? 'transactions-list-link'} to={target}>
+            <Link data-test-id={linkDataTestId ?? 'transactions-list-link'} to={target}>
               {rendered}
             </Link>
           );
