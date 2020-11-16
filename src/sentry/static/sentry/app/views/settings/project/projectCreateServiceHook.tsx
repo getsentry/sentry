14d5a2576f109e304be96736a1b348ff8f715cf3
Mark Story <mark@mark-story.com>
2020-11-16 09:52:14 -0500

    Fix page title.

--- src/sentry/static/sentry/app/views/settings/project/projectCreateServiceHook.tsx
+++ src/sentry/static/sentry/app/views/settings/project/projectCreateServiceHook.tsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import {WithRouterProps} from 'react-router';
+import DocumentTitle from 'react-document-title';
+import {RouteComponentProps} from 'react-router/lib/Router';
 
 import {t} from 'app/locale';
 import SettingsPageHeader from 'app/views/settings/components/settingsPageHeader';
 import ServiceHookSettingsForm from 'app/views/settings/project/serviceHookSettingsForm';
 
-type Props = WithRouterProps<{orgId: string; projectId: string}, {}>;
+type Props = RouteComponentProps<{orgId: string; projectId: string}, {}>;
 
 function ProjectCreateServiceHook({params}: Props) {
   const {orgId, projectId} = params;
   return (
-    <React.Fragment>
-      <SettingsPageHeader title={t('Create Service Hook')} />
-      <ServiceHookSettingsForm
-        orgId={orgId}
-        projectId={projectId}
-        initialData={{events: [], isActive: true}}
-      />
-    </React.Fragment>
+    <DocumentTitle title={t('Create Service Hook - Sentry')}>
+      <React.Fragment>
+        <SettingsPageHeader title={t('Create Service Hook')} />
+        <ServiceHookSettingsForm
+          orgId={orgId}
+          projectId={projectId}
+          initialData={{events: [], isActive: true}}
+        />
+      </React.Fragment>
+    </DocumentTitle>
   );
 }
 
