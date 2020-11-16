73ac44d712394a5d781c93d30c985836c54aacef
Alberto Leal <mail4alberto@gmail.com>
2020-11-16 18:21:18 -0500

    add create dashboard button

--- src/sentry/static/sentry/app/views/dashboardsV2/controls.tsx
+++ src/sentry/static/sentry/app/views/dashboardsV2/controls.tsx
@@ -1,8 +1,26 @@
 import React from 'react';
 
+import {t} from 'app/locale';
+import {IconAdd} from 'app/icons';
+import Button from 'app/components/button';
+
 class Controls extends React.Component {
   render() {
-    return <div>controls</div>;
+    return (
+      <Button
+        onClick={e => {
+          e.preventDefault();
+
+          console.log('create dashboard');
+        }}
+        priority="primary"
+        href="#create-dashboard"
+        icon={<IconAdd size="xs" isCircled />}
+        size="small"
+      >
+        {t('Create Dashboard')}
+      </Button>
+    );
   }
 }
 
