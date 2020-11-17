3ecf0eeb0ea86b1fac42504d4dbf4b219d7b7c03
Priscila Oliveira <priscilawebdev@gmail.com>
2020-11-17 14:09:33 +0100

    ref(ts): Convert sessionHistory to typescript - Part 2 (#22021)

---
+++ src/sentry/static/sentry/app/views/settings/account/accountSecurity/sessionHistory/utils.tsx
@@ -0,0 +1,8 @@
+import space from 'app/styles/space';
+
+export const tableLayout = `
+  display: grid;
+  grid-template-columns: auto 140px 140px;
+  grid-gap ${space(1)};
+  align-items: center;
+`;
