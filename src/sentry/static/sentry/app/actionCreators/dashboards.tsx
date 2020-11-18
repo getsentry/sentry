d6896afd98d00c9618270a54d373d18a4ada8b71
Alberto Leal <mail4alberto@gmail.com>
2020-11-18 17:57:46 -0500

    add updateDashboard action

--- src/sentry/static/sentry/app/actionCreators/dashboards.tsx
+++ src/sentry/static/sentry/app/actionCreators/dashboards.tsx
@@ -1,7 +1,12 @@
 import {Client} from 'app/api';
 import {t} from 'app/locale';
 import {addErrorMessage} from 'app/actionCreators/indicator';
-import {DashboardListItem, OrgDashboardResponse} from 'app/views/dashboardsV2/types';
+import {
+  DashboardListItem,
+  OrgDashboardResponse,
+  OrgDashboard,
+  OrgDashboardUpdate,
+} from 'app/views/dashboardsV2/types';
 
 export function createDashboard(
   api: Client,
@@ -23,6 +28,38 @@ export function createDashboard(
       addErrorMessage(errorResponse);
     } else {
       addErrorMessage(t('Unable to create dashboard'));
+    }
+  });
+
+  return promise;
+}
+
+export function updateDashboard(
+  api: Client,
+  orgId: string,
+  dashboard: OrgDashboard
+): Promise<OrgDashboardResponse> {
+  const data: OrgDashboardUpdate = {
+    title: dashboard.title,
+    // TODO: populate this properly
+    widgets: [],
+  };
+
+  const promise: Promise<OrgDashboardResponse> = api.requestPromise(
+    `/organizations/${orgId}/dashboards/${dashboard.id}/`,
+    {
+      method: 'PUT',
+      data,
+    }
+  );
+
+  promise.catch(response => {
+    const errorResponse = response?.responseJSON ?? null;
+
+    if (errorResponse) {
+      addErrorMessage(errorResponse);
+    } else {
+      addErrorMessage(t('Unable to update dashboard'));
     }
   });
 
