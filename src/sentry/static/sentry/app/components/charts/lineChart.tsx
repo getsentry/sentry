12fd7e0e7ffee0bc068fbe4ce97d4e6484207d90
k-fish <kevan.fisher@sentry.io>
2020-11-18 15:32:01 -0800

    ref(ts): Convert linechart (again)
    Fixing revert #21823 which started failing in master despite PR passing checks (likely merge issues)

--- src/sentry/static/sentry/app/components/charts/lineChart.tsx
+++ src/sentry/static/sentry/app/components/charts/lineChart.tsx
@@ -1,15 +1,25 @@
-import PropTypes from 'prop-types';
 import React from 'react';
+import {EChartOption} from 'echarts';
+
+import {Series} from 'app/types/echarts';
 
 import BaseChart from './baseChart';
 import LineSeries from './series/lineSeries';
 
-export default class LineChart extends React.Component {
-  static propTypes = {
-    ...BaseChart.propTypes,
-    seriesOptions: PropTypes.object,
+type ChartProps = React.ComponentProps<typeof BaseChart>;
+
+export type LineChartSeries = Series &
+  Omit<EChartOption.SeriesLine, 'data' | 'name' | 'lineStyle'> & {
+    dataArray?: EChartOption.SeriesLine['data'];
+    lineStyle?: any; // TODO(ts): Fix when echarts type is updated so that EchartOption.LineStyle matches SeriesLine['lineStyle']
   };
 
+type Props = Omit<ChartProps, 'series'> & {
+  series: LineChartSeries[];
+  seriesOptions?: EChartOption.SeriesLine;
+};
+
+export default class LineChart extends React.Component<Props> {
   render() {
     const {series, seriesOptions, ...props} = this.props;
 
