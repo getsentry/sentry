27edb1949764b4b2c5bfa2dfe421e3dbbcce5fd8
Mark Story <mark@mark-story.com>
2020-11-18 15:15:55 -0500

    fix(ownership) Fix rules being dropped
    When I removed the duplicate state I neglected to coalesce the
    initialText prop with state. This caused rules to be dropped if the add
    button was clicked before any changes were made in the text box.

--- tests/js/spec/views/ownershipInput.spec.jsx
+++ tests/js/spec/views/ownershipInput.spec.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
 import {mountWithTheme} from 'sentry-test/enzyme';
+import {openMenu, findOption} from 'sentry-test/select';
 
 import OwnerInput from 'app/views/settings/project/projectOwnership/ownerInput';
+import MemberListStore from 'app/stores/memberListStore';
 
 jest.mock('jquery');
 describe('Project Ownership Input', function () {
@@ -17,13 +19,20 @@ describe('Project Ownership Input', function () {
     MockApiClient.addMockResponse({
       url: '/organizations/org-slug/members/',
       method: 'GET',
-      body: [TestStubs.Members()],
+      body: TestStubs.Members(),
     });
     put = MockApiClient.addMockResponse({
       url: `/projects/${org.slug}/${project.slug}/ownership/`,
       method: 'PUT',
       body: {raw: 'url:src @dummy@example.com'},
     });
+    MemberListStore.loadInitialData([
+      TestStubs.User({id: '1', email: 'bob@example.com'}),
+    ]);
+  });
+
+  afterEach(function () {
+    MemberListStore.init();
   });
 
   it('renders', function () {
@@ -52,5 +61,46 @@ describe('Project Ownership Input', function () {
     expect(put).toHaveBeenCalled();
 
     expect(wrapper.find(OwnerInput)).toSnapshot();
+  });
+
+  it('updates on add preserving existing text', async function () {
+    const wrapper = mountWithTheme(
+      <OwnerInput
+        params={{orgId: org.slug, projectId: project.slug}}
+        organization={org}
+        initialText="url:src @dummy@example.com"
+        project={project}
+      />,
+      TestStubs.routerContext()
+    );
+
+    // Set a path, as path is selected bu default.
+    const pathInput = wrapper.find('RuleBuilder BuilderInput');
+    pathInput.simulate('change', {target: {value: 'file.js'}});
+
+    // Select the user.
+    let userSelect = wrapper.find('RuleBuilder SelectOwners');
+    openMenu(userSelect, {control: true});
+    await wrapper.update();
+
+    // Because this menu is async we can't use selectByValue()
+    userSelect = wrapper.find('RuleBuilder SelectOwners');
+    findOption(userSelect, {value: 'user:1'}, {control: true})
+      .at(0)
+      .simulate('mouseDown');
+    await wrapper.update();
+
+    // Add the new rule.
+    const button = wrapper.find('RuleBuilder AddButton button');
+    button.simulate('click');
+
+    expect(put).toHaveBeenCalledWith(
+      '/projects/org-slug/project-slug/ownership/',
+      expect.objectContaining({
+        data: {
+          raw: 'url:src @dummy@example.com' + '\n' + 'path:file.js bob@example.com',
+        },
+      })
+    );
   });
 });
