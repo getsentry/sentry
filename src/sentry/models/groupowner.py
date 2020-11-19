7f14fdc154d342b4a8fb0951840833db357ad15c
Chris Fuller <cfuller@sentry.io>
2020-11-19 15:06:36 -0500

    Updating model based on convo with Matt

--- src/sentry/models/groupowner.py
+++ src/sentry/models/groupowner.py
@@ -2,10 +2,11 @@ from __future__ import absolute_import
 
 from enum import Enum
 
+from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-from sentry.db.models import FlexibleForeignKey, Model, BoundedBigIntegerField
+from sentry.db.models import FlexibleForeignKey, Model
 
 
 class GroupOwnerType(Enum):
@@ -24,11 +25,30 @@ class GroupOwner(Model):
     project = FlexibleForeignKey("sentry.Project", null=False, db_constraint=False)
     organization = FlexibleForeignKey("sentry.Organization", null=False, db_constraint=False)
     owner_type = models.PositiveSmallIntegerField(null=False)
-    # ID of team/user, based on owner_type
-    # Wondering if this should just be team_id and user_id as nullable foreign keys and one is just not used per entry.
-    owner_id = BoundedBigIntegerField(null=False)
+    user = FlexibleForeignKey(settings.AUTH_USER_MODEL, related_name="sentry_owner_set", null=True)
+    team = FlexibleForeignKey("sentry.Team", related_name="sentry_owner_set", null=True)
     date_added = models.DateTimeField(default=timezone.now)
 
     class Meta:
         app_label = "sentry"
         db_table = "sentry_groupowner"
+
+    def save(self, *args, **kwargs):
+        assert not (self.user_id is not None and self.team_id is not None) and not (
+            self.user_id is None and self.team_id is None
+        ), "Must have Team or User, not both"
+        super(GroupOwner, self).save(*args, **kwargs)
+
+    def assigned_actor_id(self):
+        if self.user:
+            return u"user:{}".format(self.user_id)
+
+        if self.team:
+            return u"team:{}".format(self.team_id)
+
+        raise NotImplementedError("Unknown Owner")
+
+    def assigned_actor(self):
+        from sentry.api.fields.actor import Actor
+
+        return Actor.from_actor_identifier(self.assigned_actor_id())
