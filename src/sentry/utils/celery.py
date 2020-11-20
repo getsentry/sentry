7eadd0e61adb6af9e1fd1c90306be1f803894baf
Dan Fuller <dfuller@sentry.io>
2020-11-20 10:15:16 -0800

    chore(py3): Convert celerybeat tasks with large timedeltas to crontabs instead. (#22184)
    As part of Py3 we want to stop storing the db file for celerybeat. Most tasks with large
    timedeltas are more suited to crontabs anyway, so converting these to crontabs, and adding jitter to
    help prevent them all running at the same time.

---
+++ src/sentry/utils/celery.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import
+
+from random import randint
+
+from celery.schedules import crontab
+
+
+def crontab_with_minute_jitter(*args, **kwargs):
+    kwargs["minute"] = randint(0, 59)
+    return crontab(*args, **kwargs)
