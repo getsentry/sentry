14375816e11c26d96d746ef3c98ee04dec69b214
Chris Fuller <cfuller@sentry.io>
2020-11-23 18:36:11 -0500

    Some TODOs

--- src/sentry/tasks/groupowner.py
+++ src/sentry/tasks/groupowner.py
@@ -9,10 +9,19 @@ from sentry.utils.committers import get_serialized_event_file_committers
     name="sentry.tasks.process_suspect_commits", default_retry_delay=60 * 5, max_retries=5
 )
 def process_suspect_commits(event, **kwargs):
+    # TODO(Chris F.) Put a hard limit on the number of times we run this per group. This + the hash trick should prevent this from being overrun...
+    # But would probably need a way to 'refresh' the owners over time as new events come in if an issue is really old.
+    # so maybe it's better to just delete the Xth oldest as new ones come in...? If its older than Y days or something?
+
     # TODO(Chris F.) Check memcache to see if we have already processed this event hash.
     committers = get_serialized_event_file_committers(event.project, event)
+
     # TODO(Chris F.) Store committers in memcache with event hash as key.
     # create group owner reference for the found suspect commits
+
+    # TODO(Chris F.) We would like to store this commit information so that we can get perf gains
+    # and synced information on the Issue details page.
+
     for committer in committers:
         # GOTCHA: Some suspect commits can't be linked to sentry users and don't have an id.
         if "id" in committer["author"]:
@@ -25,6 +34,7 @@ def process_suspect_commits(event, **kwargs):
                 user_id=owner_id,
             )
         else:
+            # TODO(Chris F.) We actually need to store and display these too, somehow.
             pass
 
     # TODO(Chris F.): Store/Cache suspect commit info so we don't recalculate it on issue details load?
