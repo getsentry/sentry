756459f1879a3e4b23c87adc9cb499fdb757e919
Chris Fuller <cfuller@sentry.io>
2020-11-23 16:49:21 -0500

    Removing feature code

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -921,8 +921,6 @@ SENTRY_FEATURES = {
     "organizations:usage-stats-graph": False,
     # Enable inbox support in the issue stream
     "organizations:inbox": False,
-    # Enable "owner"/"suggested assignee" features.
-    "organizations:workflow-owners": False,
     # Return unhandled information on the issue level
     "organizations:unhandled-issue-flag": False,
     # Enable functionality to specify custom inbound filters on events.
