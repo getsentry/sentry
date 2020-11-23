b5dc418c7829e5e2a67875b4b7b79cca5eff89b5
David Wang <dwang@sentry.io>
2020-11-23 15:10:03 -0500

    41px placeholder padding

--- src/sentry/static/sentry/app/views/eventsV2/table/queryField.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/table/queryField.tsx
@@ -1,24 +1,24 @@
 import React, {CSSProperties} from 'react';
-import styled from '@emotion/styled';
-import cloneDeep from 'lodash/cloneDeep';
 // eslint import checks can't find types in the flow code.
 // eslint-disable-next-line import/named
-import {components, SingleValueProps, OptionProps} from 'react-select';
+import {components, OptionProps, SingleValueProps} from 'react-select';
+import styled from '@emotion/styled';
+import cloneDeep from 'lodash/cloneDeep';
 
-import Input from 'app/views/settings/components/forms/controls/input';
-import SelectControl from 'app/components/forms/selectControl';
-import {SelectValue} from 'app/types';
-import {t} from 'app/locale';
 import Badge from 'app/components/badge';
+import SelectControl from 'app/components/forms/selectControl';
+import {t} from 'app/locale';
 import space from 'app/styles/space';
+import {SelectValue} from 'app/types';
 import {
-  ColumnType,
   AggregateParameter,
+  ColumnType,
   QueryFieldValue,
   ValidateColumnTypes,
 } from 'app/utils/discover/fields';
+import Input from 'app/views/settings/components/forms/controls/input';
 
-import {FieldValueKind, FieldValue, FieldValueColumns} from './types';
+import {FieldValue, FieldValueColumns, FieldValueKind} from './types';
 
 type FieldOptions = Record<string, SelectValue<FieldValue>>;
 
@@ -510,13 +510,13 @@ class BufferedInput extends React.Component<InputProps, InputState> {
 // Set a min-width to allow shrinkage in grid.
 const StyledInput = styled(Input)`
   /* Match the height of the select boxes */
-  height: 40px;
+  height: 41px;
   min-width: 50px;
 `;
 
 const BlankSpace = styled('div')`
   /* Match the height of the select boxes */
-  height: 40px;
+  height: 41px;
   min-width: 50px;
   background: ${p => p.theme.backgroundSecondary};
   border-radius: ${p => p.theme.borderRadius};
