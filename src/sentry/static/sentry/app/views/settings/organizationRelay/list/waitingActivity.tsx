848be85afc66acff6d430a6507cb5b8c59df96fc
Priscila Oliveira <priscilawebdev@gmail.com>
2020-11-23 15:43:47 +0100

    ref(relay): Display a grayed relay content if user hasn't org:write access (#22222)

--- src/sentry/static/sentry/app/views/settings/organizationRelay/list/waitingActivity.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationRelay/list/waitingActivity.tsx
@@ -9,15 +9,20 @@ import EmptyMessage from 'app/views/settings/components/emptyMessage';
 
 type Props = {
   onRefresh: () => void;
+  disabled: boolean;
 };
 
-const WaitingActivity = ({onRefresh}: Props) => (
+const WaitingActivity = ({onRefresh, disabled}: Props) => (
   <Panel>
     <EmptyMessage
       title={t('Waiting on Activity!')}
-      description={tct('Run relay in your terminal with [commandLine]', {
-        commandLine: <CommandLine>{'relay run'}</CommandLine>,
-      })}
+      description={
+        disabled
+          ? undefined
+          : tct('Run relay in your terminal with [commandLine]', {
+              commandLine: <CommandLine>{'relay run'}</CommandLine>,
+            })
+      }
       action={
         <Button icon={<IconRefresh />} onClick={onRefresh}>
           {t('Refresh')}
