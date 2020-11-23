f1082f0a8c27193c6f727fdf06784d46a882a52e
Priscila Oliveira <priscilawebdev@gmail.com>
2020-11-23 15:43:47 +0100

    ref(relay): Display a grayed relay content if user hasn't org:write access (#22222)

--- src/sentry/static/sentry/app/views/settings/organizationRelay/index.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationRelay/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import Access from 'app/components/acl/access';
 import Feature from 'app/components/acl/feature';
 import FeatureDisabled from 'app/components/acl/featureDisabled';
 import {PanelAlert} from 'app/components/panels';
@@ -10,21 +9,19 @@ import withOrganization from 'app/utils/withOrganization';
 import RelayWrapper from './relayWrapper';
 
 const OrganizationRelay = ({organization, ...props}: RelayWrapper['props']) => (
-  <Access access={['org:admin']} organization={organization} renderNoAccessMessage>
-    <Feature
-      features={['relay']}
-      organization={organization}
-      renderDisabled={() => (
-        <FeatureDisabled
-          alert={PanelAlert}
-          features={organization.features}
-          featureName={t('Relay')}
-        />
-      )}
-    >
-      <RelayWrapper organization={organization} {...props} />
-    </Feature>
-  </Access>
+  <Feature
+    features={['relay']}
+    organization={organization}
+    renderDisabled={() => (
+      <FeatureDisabled
+        alert={PanelAlert}
+        features={organization.features}
+        featureName={t('Relay')}
+      />
+    )}
+  >
+    <RelayWrapper organization={organization} {...props} />
+  </Feature>
 );
 
 export default withOrganization(OrganizationRelay);
--- src/sentry/static/sentry/app/views/settings/organizationRelay/list/index.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationRelay/list/index.tsx
@@ -16,17 +16,25 @@ type WaitingActivityProps = React.ComponentProps<typeof WaitingActivity>;
 type Props = {
   relays: Array<Relay>;
   relayActivities: Array<RelayActivity>;
+  disabled: boolean;
 } & Pick<CardHeaderProps, 'onDelete' | 'onEdit'> &
   Pick<WaitingActivityProps, 'onRefresh'>;
 
-const List = ({relays, relayActivities, onRefresh, onDelete, onEdit}: Props) => {
+const List = ({
+  relays,
+  relayActivities,
+  onRefresh,
+  onDelete,
+  onEdit,
+  disabled,
+}: Props) => {
   const orderedRelays = orderBy(relays, relay => relay.created, ['desc']);
 
   const relaysByPublicKey = getRelaysByPublicKey(orderedRelays, relayActivities);
 
   const renderCardContent = (activities: Array<RelayActivity>) => {
     if (!activities.length) {
-      return <WaitingActivity onRefresh={onRefresh} />;
+      return <WaitingActivity onRefresh={onRefresh} disabled={disabled} />;
     }
 
     return <ActivityList activities={activities} />;
@@ -47,6 +55,7 @@ const List = ({relays, relayActivities, onRefresh, onDelete, onEdit}: Props) => 
               created={created}
               onEdit={onEdit}
               onDelete={onDelete}
+              disabled={disabled}
             />
             {renderCardContent(activities)}
           </Card>
