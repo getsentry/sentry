ce3529f26940b23576c03ba7f86a708a04052071
Mark Story <mark@sentry.io>
2020-11-26 11:30:52 -0500

    ref(ts) Convert Team Settings to typescript (#22296)

--- src/sentry/static/sentry/app/views/settings/organizationTeams/teamSettings/model.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationTeams/teamSettings/model.tsx
@@ -2,6 +2,15 @@ import {updateTeam} from 'app/actionCreators/teams';
 import FormModel from 'app/views/settings/components/forms/model';
 
 class TeamFormModel extends FormModel {
+  public orgId: string;
+  public teamId: string;
+
+  constructor(orgId: string, teamId: string) {
+    super();
+    this.orgId = orgId;
+    this.teamId = teamId;
+  }
+
   doApiRequest({data}) {
     return new Promise((resolve, reject) =>
       updateTeam(
