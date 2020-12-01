0d5458673fdd891fd7f2bac51b3847dded0d0d7d
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-12-01 01:18:49 -0800

    chore(ts): Convert accountSecurity/components/removeConfirm (#22352)

--- src/sentry/static/sentry/app/views/settings/account/accountSecurity/components/removeConfirm.tsx
+++ src/sentry/static/sentry/app/views/settings/account/accountSecurity/components/removeConfirm.tsx
@@ -5,6 +5,8 @@ import {t} from 'app/locale';
 import ConfirmHeader from 'app/views/settings/account/accountSecurity/components/confirmHeader';
 import TextBlock from 'app/views/settings/components/text/textBlock';
 
+type Props = React.ComponentProps<typeof Confirm>;
+
 const message = (
   <React.Fragment>
     <ConfirmHeader>{t('Do you want to remove this method?')}</ConfirmHeader>
@@ -16,6 +18,12 @@ const message = (
   </React.Fragment>
 );
 
-const RemoveConfirm = props => <Confirm {...props} message={message} />;
+class RemoveConfirm extends React.Component<Props> {
+  static defaultProps = Confirm.defaultProps;
+
+  render() {
+    return <Confirm {...this.props} message={message} />;
+  }
+}
 
 export default RemoveConfirm;
