4390545b7cb54f0d56b77be96351ef84d07453f2
Colleen O'Rourke <colleen@sentry.io>
2020-12-01 12:31:58 -0800

    Add search for reporter and assignee, add body text

--- src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/ticketRuleForm.tsx
+++ src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/ticketRuleForm.tsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import Modal from 'react-bootstrap/lib/Modal';
+import styled from '@emotion/styled';
+import debounce from 'lodash/debounce';
+import * as queryString from 'query-string';
 
 import Button from 'app/components/button';
+import ExternalLink from 'app/components/links/externalLink';
 import {IconSettings} from 'app/icons';
-import {t} from 'app/locale';
+import {t, tct} from 'app/locale';
+import space from 'app/styles/space';
+import {IssueConfigField} from 'app/types';
 import FieldFromConfig from 'app/views/settings/components/forms/fieldFromConfig';
 import Form from 'app/views/settings/components/forms/form';
 
 type Props = {
   data: any;
   instance: any;
+  onSubmitAction: any;
 };
 
 type State = {
@@ -48,10 +55,10 @@ class TicketRuleForm extends React.Component<Props, State> {
     const names = this.getNames();
     const formData = {};
     if (this.props.instance.hasOwnProperty('jira_integration')) {
-      formData.jira_integration = this.props.instance.jira_integration;
+      formData.jira_integration = this.props.instance?.jira_integration;
     }
     if (this.props.instance.hasOwnProperty('vsts_integration')) {
-      formData.vsts_integration = this.props.instance.vsts_integration;
+      formData.vsts_integration = this.props.instance?.vsts_integration;
     }
 
     for (const [key, value] of Object.entries(data)) {
@@ -68,36 +75,90 @@ class TicketRuleForm extends React.Component<Props, State> {
     this.closeModal();
   };
 
-  renderFields = () => {
-    const fields = [];
-    this.props.data.formFields.reporter.required = false; // this is a hack for now until I figure out how to populate it with loadOptions!!
-    const formFields = Object.values(this.props.data.formFields);
-
-    for (const key in formFields) {
-      if (formFields[key].hasOwnProperty('name')) {
-        fields.push(
-          <FieldFromConfig
-            key={formFields[key].name}
-            field={formFields[key]}
-            inline={false}
-            stacked
-            flexibleControlStateSize
-            // loadOptions={() => {console.log("helloooo")}}
-            // async={true}
-            // cache={false}
-            // onSelectResetsInput={false}
-            // onCloseResetsInput={false}
-            // onBlurResetsInput={false}
-            // autoload={true}
-          />
-        );
+  getOptions = (field: IssueConfigField, input: string) =>
+    new Promise((resolve, reject) => {
+      if (!input) {
+        const choices =
+          (field.choices as Array<[number | string, number | string]>) || [];
+        const options = choices.map(([value, label]) => ({value, label}));
+        return resolve({options});
       }
-    }
-    return fields;
+      return this.debouncedOptionLoad(field, input, (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+
+  debouncedOptionLoad = debounce(
+    async (
+      field: IssueConfigField,
+      input: string,
+      cb: (err: Error | null, result?) => void
+    ) => {
+      const options = this.props.instance;
+      const query = queryString.stringify({
+        project: options.project,
+        issuetype: options.issuetype,
+        field: field.name,
+        query: input,
+      });
+
+      const url = field.url || '';
+      const separator = url.includes('?') ? '&' : '?';
+      // We can't use the API client here since the URL is not scoped under the
+      // API endpoints (which the client prefixes)
+      try {
+        const response = await fetch(url + separator + query);
+        cb(null, {options: response.ok ? await response.json() : []});
+      } catch (err) {
+        cb(err);
+      }
+    },
+    200,
+    {trailing: true}
+  );
+
+  getFieldProps = (field: IssueConfigField) =>
+    field.url
+      ? {
+          loadOptions: (input: string) => this.getOptions(field, input),
+          async: true,
+          cache: false,
+          onSelectResetsInput: false,
+          onCloseResetsInput: false,
+          onBlurResetsInput: false,
+          autoload: true,
+        }
+      : {};
+
+  buildText = () => {
+    const bodyText = {};
+    bodyText.ticketType = this.props.instance.hasOwnProperty('jira_integration')
+      ? 'Jira issue'
+      : 'Azure DevOps work item';
+    bodyText.link = this.props.instance.hasOwnProperty('jira_integration')
+      ? 'https://docs.sentry.io/product/integrations/jira/#issue-sync'
+      : 'https://docs.sentry.io/product/integrations/azure-devops/#issue-sync';
+    return bodyText;
   };
 
   render() {
+    const {ticketType, link} = this.buildText();
+    const text = t(
+      'When this alert is triggered a %s will be created with the following fields. ',
+      ticketType
+    );
     const submitLabel = t('Apply Changes');
+    const formFields = Object.values(this.props.data.formFields);
+    const initialData = {};
+    formFields.forEach(field => {
+      // passing an empty array breaks multi select
+      // TODO(jess): figure out why this is breaking and fix
+      initialData[field.name] = field.multiple ? '' : field.default;
+    });
     return (
       <React.Fragment>
         <Button
@@ -118,20 +179,35 @@ class TicketRuleForm extends React.Component<Props, State> {
             <Modal.Title>Issue Link Settings</Modal.Title>
           </Modal.Header>
           <Modal.Body>
+            <BodyText>
+              {text}
+              {tct("It'll also [linkToDocs] with the new Sentry Issue.", {
+                linkToDocs: <ExternalLink href={link}>{t('stay in sync')}</ExternalLink>,
+              })}
+            </BodyText>
             <Form
-              // apiEndpoint={`/groups/${group.id}/integrations/${integration.id}/`}
-              // apiMethod={action === 'create' ? 'POST' : 'PUT'}
               onSubmit={this.onFormSubmit}
               // onSubmitSuccess={this.formSubmitSuccess}
-              initialData={this.props.data}
+              initialData={initialData}
               // onFieldChange={this.onFieldChange}
               submitLabel={submitLabel}
               // submitDisabled={this.state.reloading}
-              // footerClass="modal-footer"
+              footerClass="modal-footer"
               // onPreSubmit={this.handlePreSubmit}
               // onSubmitError={this.handleSubmitError}
             >
-              {this.renderFields}
+              {formFields
+                .filter(field => field.hasOwnProperty('name'))
+                .map(field => (
+                  <FieldFromConfig
+                    key={`${field.name}-${field.default}-${field.required}`}
+                    field={field}
+                    inline={false}
+                    stacked
+                    flexibleControlStateSize
+                    {...this.getFieldProps(field)}
+                  />
+                ))}
             </Form>
           </Modal.Body>
         </Modal>
@@ -139,5 +215,9 @@ class TicketRuleForm extends React.Component<Props, State> {
     );
   }
 }
+
+const BodyText = styled('div')`
+  margin-bottom: ${space(3)};
+`;
 
 export default TicketRuleForm;
