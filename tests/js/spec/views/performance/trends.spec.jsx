52e87cd85e99680a307b9a1ef0503ff6034bae55
k-fish <kevan.fisher@sentry.io>
2020-12-01 10:01:50 -0800

    Add tick and update before and after

--- tests/js/spec/views/performance/trends.spec.jsx
+++ tests/js/spec/views/performance/trends.spec.jsx
@@ -369,11 +369,14 @@ describe('Performance > Trends', function () {
       data.routerContext
     );
 
+    await tick();
+    wrapper.update();
+
     for (const trendFunction of TRENDS_FUNCTIONS) {
+      selectTrendFunction(wrapper, trendFunction.field);
+
       await tick();
       wrapper.update();
-
-      selectTrendFunction(wrapper, trendFunction.field);
 
       expect(browserHistory.push).toHaveBeenCalledWith({
         query: expect.objectContaining({
@@ -396,11 +399,14 @@ describe('Performance > Trends', function () {
       data.routerContext
     );
 
+    await tick();
+    wrapper.update();
+
     for (const confidenceLevel of CONFIDENCE_LEVELS) {
+      selectConfidenceLevel(wrapper, confidenceLevel.label);
+
       await tick();
       wrapper.update();
-
-      selectConfidenceLevel(wrapper, confidenceLevel.label);
 
       expect(browserHistory.push).toHaveBeenCalledWith({
         query: expect.objectContaining({
