da9431b469997931cf90ae354159f67c609492f3
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-12-01 01:20:27 -0800

    chore(ts): Convert settingsBreadcrumb/menuItem (#22348)

---
+++ src/sentry/static/sentry/app/views/settings/components/settingsBreadcrumb/menuItem.tsx
@@ -0,0 +1,10 @@
+import styled from '@emotion/styled';
+
+import overflowEllipsis from 'app/styles/overflowEllipsis';
+
+const MenuItem = styled('div')`
+  font-size: 14px;
+  ${overflowEllipsis};
+`;
+
+export default MenuItem;
