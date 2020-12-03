43c9e9d1162ad2e65cab20657ba18bdb6828c6e5
Priscila Oliveira <priscila.oliveira@sentry.io>
2020-12-03 12:10:33 +0100

    feat(issue-details-page): Add Reprocessing Component

--- src/sentry/static/sentry/app/views/organizationGroupDetails/header.tsx
+++ src/sentry/static/sentry/app/views/organizationGroupDetails/header.tsx
@@ -82,6 +82,7 @@ class GroupHeader extends React.Component<Props, State> {
     const projectFeatures = new Set(project ? project.features : []);
     const organizationFeatures = new Set(organization ? organization.features : []);
     const userCount = group.userCount;
+    const isReprocessing = group.status === 'reprocessing';
 
     const hasReprocessingV2Feature = projectFeatures.has('reprocessing-v2');
     const hasSimilarView = projectFeatures.has('similarity-view');
@@ -219,33 +220,34 @@ class GroupHeader extends React.Component<Props, State> {
         />
         <GroupActions group={group} project={project} disabled={isReprocessing} />
         <NavTabs>
-          <ListLink
+          <StyledListLink
             to={`${baseUrl}${location.search}`}
             isActive={() => currentTab === TAB.DETAILS}
           >
             {t('Details')}
-          </ListLink>
-          <ListLink
+          </StyledListLink>
+          <StyledListLink
             to={`${baseUrl}activity/${location.search}`}
             isActive={() => currentTab === TAB.COMMENTS}
           >
             {t('Activity')} <Badge text={group.numComments} />
-          </ListLink>
-          <ListLink
+          </StyledListLink>
+          <StyledListLink
             to={`${baseUrl}feedback/${location.search}`}
             isActive={() => currentTab === TAB.USER_FEEDBACK}
           >
             {t('User Feedback')} <Badge text={group.userReportCount} />
-          </ListLink>
+          </StyledListLink>
           {hasEventAttachments && (
-            <ListLink
+            <StyledListLink
               to={`${baseUrl}attachments/${location.search}`}
               isActive={() => currentTab === TAB.ATTACHMENTS}
               disabled={isReprocessing}
             >
               {t('Attachments')}
-            </ListLink>
+            </StyledListLink>
           )}
+<<<<<<< HEAD
           <ListLink
             to={`${baseUrl}tags/${location.search}`}
             isActive={() => currentTab === TAB.TAGS}
@@ -275,6 +277,37 @@ class GroupHeader extends React.Component<Props, State> {
             >
               {t('Similar Issues')}
             </ListLink>
+=======
+          {!isReprocessing && (
+            <React.Fragment>
+              <StyledListLink
+                to={`${baseUrl}tags/${location.search}`}
+                isActive={() => currentTab === TAB.TAGS}
+              >
+                {t('Tags')}
+              </StyledListLink>
+              <StyledListLink
+                to={eventRouteToObject}
+                isActive={() => currentTab === 'events'}
+              >
+                {t('Events')}
+              </StyledListLink>
+              <StyledListLink
+                to={`${baseUrl}merged/${location.search}`}
+                isActive={() => currentTab === TAB.MERGED}
+              >
+                {t('Merged Issues')}
+              </StyledListLink>
+              {hasSimilarView && (
+                <StyledListLink
+                  to={`${baseUrl}similar/${location.search}`}
+                  isActive={() => currentTab === TAB.SIMILAR_ISSUES}
+                >
+                  {t('Similar Issues')}
+                </StyledListLink>
+              )}
+            </React.Fragment>
+>>>>>>> feat(issue-details-page): Add Reprocessing Component
           )}
         </NavTabs>
       </div>
@@ -299,3 +332,21 @@ const StyledProjectBadge = styled(ProjectBadge)`
 const EventAnnotationWithSpace = styled(EventAnnotation)`
   margin-left: ${space(1)};
 `;
+<<<<<<< HEAD
+=======
+
+export {GroupHeader, TAB};
+
+export default withApi(GroupHeader);
+
+const StyledListLink = styled(ListLink)`
+  ${p =>
+    !p.to &&
+    `
+    &.active a, &.active a:focus &.active a:hover, a:hover, a.active {
+      color: #7c6a8e !important;
+      cursor: not-allowed !important;
+    }
+`}
+`;
+>>>>>>> feat(issue-details-page): Add Reprocessing Component
