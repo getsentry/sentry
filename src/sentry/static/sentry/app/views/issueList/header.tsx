10978e28094b6caaeacc62897a1d4173d25de207
Scott Cooper <scttcper@gmail.com>
2020-12-03 11:18:52 -0800

    fix(workflow): Change backlog to acknowledge (#22409)

--- src/sentry/static/sentry/app/views/issueList/header.tsx
+++ src/sentry/static/sentry/app/views/issueList/header.tsx
@@ -14,7 +14,7 @@ type Props = {
 };
 
 const queries = [
-  ['is:inbox is:unresolved', t('Inbox')],
+  ['is:inbox is:unresolved', t('Unacknowledged')],
   ['is:unresolved', t('Unresolved')],
   ['is:ignored', t('Ignored')],
 ];
