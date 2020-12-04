67971b03468f4b16275ef9c230f0c4e0cf1c97c0
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2020-12-04 11:52:00 -0800

    ref(ts): Convert contextSummary to Typescript (#22415)
    * ref(ts): Convert contextSummary to Typescript
    
    * filterContexts args types
    
    * Todo for future

--- src/sentry/static/sentry/app/components/events/contextSummary/filterContexts.tsx
+++ src/sentry/static/sentry/app/components/events/contextSummary/filterContexts.tsx
@@ -1,10 +1,14 @@
-function filterContexts(event, context) {
+import {Event} from 'app/types';
+
+import {Context} from './contextSummary';
+
+function filterContexts(event: Event, context: Context) {
   // if the operating system is macOS, we want to hide devices called "Mac" which don't have any additional info
   if (context.keys.includes('device')) {
-    const {model, arch, data_id} = event.contexts?.device || {};
+    const {model, arch} = event.contexts?.device || {};
     const {name: os} = event.contexts?.os || event.contexts?.client_os || {};
 
-    if (model === 'Mac' && !arch && !data_id && os?.toLowerCase().includes('mac')) {
+    if (model === 'Mac' && !arch && os?.toLowerCase().includes('mac')) {
       return false;
     }
   }
