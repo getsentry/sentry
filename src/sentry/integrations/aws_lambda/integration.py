0403f10949bd5dab360190b54bb78b1fb0b33698
Stephen Cefali <scefali@sentry.io>
2020-12-08 10:56:30 -0800

    basic version ready

--- src/sentry/integrations/aws_lambda/integration.py
+++ src/sentry/integrations/aws_lambda/integration.py
@@ -1,11 +1,9 @@
 from __future__ import absolute_import
 
-import boto3
 import logging
+import six
 import uuid
-import os
 
-from botocore.config import Config
 from django.utils.translation import ugettext_lazy as _
 
 
@@ -17,11 +15,14 @@ from sentry.integrations import (
     IntegrationMetadata,
     FeatureDescription,
 )
+from sentry.models import Project, ProjectKey
 from sentry.pipeline import PipelineView
 from sentry.shared_integrations.exceptions import IntegrationError
 from sentry.web.helpers import render_to_response
-from sentry.utils.http import absolute_uri
 from sentry.utils.compat import filter
+
+from .client import gen_aws_lambda_client
+from .utils import parse_arn
 
 logger = logging.getLogger("sentry.integrations.aws_lambda")
 
@@ -51,25 +52,6 @@ metadata = IntegrationMetadata(
 
 SUPPORTED_RUNTIMES = ["nodejs12.x", "nodejs10.x"]
 
-# Taken from: https://gist.github.com/gene1wood/5299969edc4ef21d8efcfea52158dd40
-def parse_arn(arn):
-    # http://docs.aws.amazon.com/general/latest/gr/aws-arns-and-namespaces.html
-    elements = arn.split(":", 5)
-    result = {
-        "arn": elements[0],
-        "partition": elements[1],
-        "service": elements[2],
-        "region": elements[3],
-        "account": elements[4],
-        "resource": elements[5],
-        "resource_type": None,
-    }
-    if "/" in result["resource"]:
-        result["resource_type"], result["resource"] = result["resource"].split("/", 1)
-    elif ":" in result["resource"]:
-        result["resource_type"], result["resource"] = result["resource"].split(":", 1)
-    return result
-
 
 class AwsLambdaIntegration(IntegrationInstallation):
     pass
@@ -84,7 +66,7 @@ class AwsLambdaIntegrationProvider(IntegrationProvider):
     features = frozenset([IntegrationFeatures.SERVERLESS])
 
     def get_pipeline_views(self):
-        return [AwsLambdaPipelineView(), SetupSubscriptionView()]
+        return [AwsLambdaCloudFormationPipelineView(), AwsLambdaSetupLayerPipelineView()]
 
     def build_integration(self, state):
         # TODO: unhardcode
@@ -93,86 +75,95 @@ class AwsLambdaIntegrationProvider(IntegrationProvider):
         arn = state["arn"]
         parsed_arn = parse_arn(arn)
         account_id = parsed_arn["account"]
+        region = parsed_arn["region"]
+
+        external_id = u"{}-{}".format(account_id, region)
 
         integration = {
             "name": integration_name,
-            "external_id": account_id,  # we might want the region as part of the external id
-            "metadata": {"arn": state["arn"]},
+            "external_id": external_id,
+            "metadata": {"arn": state["arn"], "aws_external_id": state["aws_external_id"]},
         }
         return integration
 
 
-class AwsLambdaPipelineView(PipelineView):
+class AwsLambdaCloudFormationPipelineView(PipelineView):
     def dispatch(self, request, pipeline):
-
+        # TODO: Find way so a user/org only gets one external id
         if request.method == "POST":
             arn = request.POST["arn"]
-            external_id = request.POST["external_id"]
+            aws_external_id = request.POST["aws_external_id"]
             pipeline.bind_state("arn", arn)
-            pipeline.bind_state("external_id", external_id)
+            pipeline.bind_state("aws_external_id", aws_external_id)
             return pipeline.next_step()
 
         template_url = options.get("aws-lambda.cloudformation-url")
-        external_id = uuid.uuid4()
-        external_id = "cef3b0f5-49be-4cc9-a786-15c62695fd2a"
+        aws_external_id = uuid.uuid4()
+        aws_external_id = "0fe88434-f339-4690-9903-929fe4369831"
+
         cloudformation_url = (
             "https://console.aws.amazon.com/cloudformation/home#/stacks/create/review?"
             "stackName=Sentry-Monitoring-Stack-Filter&templateURL=%s&param_ExternalId=%s"
-            % (template_url, external_id)
+            % (template_url, aws_external_id)
         )
 
         return render_to_response(
             template="sentry/integrations/aws-lambda-cloudformation.html",
             request=request,
-            context={"cloudformation_url": cloudformation_url, "external_id": external_id},
+            context={"cloudformation_url": cloudformation_url, "aws_external_id": aws_external_id},
         )
 
 
-
-class SetupSubscriptionView(PipelineView):
+class AwsLambdaSetupLayerPipelineView(PipelineView):
     def dispatch(self, request, pipeline):
+        organization = pipeline.organization
+
+        # TODO: make project selection part of flow
+        project = Project.objects.filter(
+            organization=organization, platform="node-awslambda"
+        ).first()
+        if not project:
+            raise IntegrationError("No valid project")
+
+        enabled_dsn = ProjectKey.get_default(project=project)
+        if not enabled_dsn:
+            raise IntegrationError("Project does not have DSN enabled")
+        sentry_project_dsn = enabled_dsn.get_dsn(public=True)
+
         arn = pipeline.fetch_state("arn")
+        aws_external_id = pipeline.fetch_state("aws_external_id")
 
-        external_id = pipeline.fetch_state("external_id")
+        lambda_client = gen_aws_lambda_client(arn, aws_external_id)
 
-        parsed_arn = parse_arn(arn)
-        account_id = parsed_arn["account"]
-        region = parsed_arn["region"]
-
-        role_arn = "arn:aws:iam::%s:role/SentryRole"%(account_id)
-
-        # this needs to either be done in a loop or in the SNS callback
-        client = boto3.client(
-            service_name="sts",
-            aws_access_key_id=options.get("aws-lambda.access-key-id"),
-            aws_secret_access_key=options.get("aws-lambda.secret-access-key"),
-            region_name=options.get("aws-lambda.region"),
+        lambda_functions = filter(
+            lambda x: x.get("Runtime") in SUPPORTED_RUNTIMES,
+            lambda_client.list_functions()["Functions"],
         )
-
-        assumed_role_object = client.assume_role(
-            RoleSessionName="MySession", RoleArn=role_arn, ExternalId=external_id
-        )
-
-        credentials = assumed_role_object["Credentials"]
-
-        tmp_access_key = credentials['AccessKeyId']
-        tmp_secret_key = credentials['SecretAccessKey']
-        security_token = credentials['SessionToken']
-
-        boto3_session = boto3.Session(
-            aws_access_key_id=tmp_access_key,
-            aws_secret_access_key=tmp_secret_key, aws_session_token=security_token
-        )
-
-        lambda_client = boto3_session.client(service_name='lambda', region_name=region)
-        log_client = boto3_session.client(service_name='logs', region_name=region)
-        iam_client = boto3_session.client(service_name='iam', region_name=region)
-
-        lambda_functions = filter(lambda x: x.get("Runtime") in SUPPORTED_RUNTIMES, lambda_client.list_functions()["Functions"])
-
 
         for function in lambda_functions:
             name = function["FunctionName"]
-            lambda_client.update_function_configuration(FunctionName=name, Layers=[options.get("aws-lambda.node-layer-arn")])
+            # TODO: load existing layers and environment and append to them
+            try:
+                lambda_client.update_function_configuration(
+                    FunctionName=name,
+                    Layers=[options.get("aws-lambda.node-layer-arn")],
+                    Environment={
+                        "Variables": {
+                            "NODE_OPTIONS": "-r @sentry/serverless/dist/auto",
+                            "SENTRY_DSN": sentry_project_dsn,
+                            "SENTRY_TRACES_SAMPLE_RATE": "1.0",
+                        }
+                    },
+                )
+            except Exception as e:
+                logger.info(
+                    "update_function_configuration.error",
+                    extra={
+                        "organization_id": organization.id,
+                        "lambda_name": name,
+                        "arn": arn,
+                        "error": six.text_type(e),
+                    },
+                )
 
         return pipeline.next_step()
