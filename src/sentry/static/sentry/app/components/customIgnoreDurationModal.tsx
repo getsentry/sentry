91394deee50c0f5baa68a226cde939c212693fae
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-12-07 16:44:53 -0800

    chore(ui): Normalize importing of bootstrap modal

--- src/sentry/static/sentry/app/components/customIgnoreDurationModal.tsx
+++ src/sentry/static/sentry/app/components/customIgnoreDurationModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Modal from 'react-bootstrap/lib/Modal';
+import {Modal} from 'react-bootstrap';
 import moment from 'moment';
 import {sprintf} from 'sprintf-js';
 
