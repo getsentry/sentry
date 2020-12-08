f4781354a9eb6c382e605e0b1010492e1de419c6
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-12-08 11:30:52 -0800

    feat(ui): Toggle chevron direction in the assigneeSelector (#22511)

--- src/sentry/static/sentry/app/components/assigneeSelector.tsx
+++ src/sentry/static/sentry/app/components/assigneeSelector.tsx
@@ -396,7 +396,7 @@ const AssigneeSelectorComponent = createReactClass<Props, State>({
             menuWithArrow
             emptyHidesInput
           >
-            {({getActorProps}) => (
+            {({getActorProps, isOpen}) => (
               <DropdownButton {...getActorProps({})}>
                 {assignedTo ? (
                   <ActorAvatar actor={assignedTo} className="avatar" size={24} />
@@ -405,7 +405,7 @@ const AssigneeSelectorComponent = createReactClass<Props, State>({
                 ) : (
                   <StyledIconUser size="20px" color="gray400" />
                 )}
-                <StyledChevron direction="down" size="xs" />
+                <StyledChevron direction={isOpen ? 'up' : 'down'} size="xs" />
               </DropdownButton>
             )}
           </DropdownAutoComplete>
