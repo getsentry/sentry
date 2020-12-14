0137e292c8c7d5dd6777ebea3af6c44bcdb27219
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2020-12-14 14:37:37 -0800

    update test

--- tests/sentry/api/endpoints/test_sentry_app_publish_request.py
+++ tests/sentry/api/endpoints/test_sentry_app_publish_request.py
@@ -52,7 +52,7 @@ class SentryAppPublishRequestTest(APITestCase):
         self.login_as(user=self.user)
         response = self.client.post(self.url, format="json")
         assert response.status_code == 400
-        assert response.data["detail"] == "Cannot publish already published. integration"
+        assert response.data["detail"] == "Cannot publish already published integration."
         send_mail.asssert_not_called()
 
     @mock.patch("sentry.utils.email.send_mail")
