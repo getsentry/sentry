b2ebfc7f725387909806e591efe7d96e6d1074c3
Matej Minar <matej.minar@sentry.io>
2020-12-14 11:23:41 +0100

    fix(withProject): Add undefined to project type (#22597)
    Following up on this hotfix: #22566 and #22595
    
    The project returned from withProject can be undefined.
    
    I changed withProject types to reflect this and removed withProject from project settings views as they get project from SettingsProjectProvider anyway.

--- src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/index.tsx
+++ src/sentry/static/sentry/app/views/settings/projectAlerts/issueEditor/index.tsx
@@ -32,7 +32,6 @@ import {
 import {getDisplayName} from 'app/utils/environment';
 import recreateRoute from 'app/utils/recreateRoute';
 import withOrganization from 'app/utils/withOrganization';
-import withProject from 'app/utils/withProject';
 import AsyncView from 'app/views/asyncView';
 import Input from 'app/views/settings/components/forms/controls/input';
 import Field from 'app/views/settings/components/forms/field';
@@ -724,7 +723,7 @@ class IssueRuleEditor extends AsyncView<Props, State> {
   }
 }
 
-export default withProject(withOrganization(IssueRuleEditor));
+export default withOrganization(IssueRuleEditor);
 
 const StyledForm = styled(Form)`
   position: relative;
