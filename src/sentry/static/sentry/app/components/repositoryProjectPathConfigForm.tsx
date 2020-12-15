753f7673c754865e4375980bf76db9f09570840d
Evan Purkhiser <evanpurkhiser@gmail.com>
2020-12-15 03:46:29 -0800

    ref(modals): Improve integrationCodeMappings modal (#22584)
    - Use openModal instead of maintaining 'modal is opened' state
     - Improve aesthetics of the modal
     - Improve aesthetics of the IntegrationCodeMappings panel header

--- src/sentry/static/sentry/app/components/repositoryProjectPathConfigForm.tsx
+++ src/sentry/static/sentry/app/components/repositoryProjectPathConfigForm.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from '@emotion/styled';
 import pick from 'lodash/pick';
 
 import {t} from 'app/locale';
@@ -10,16 +9,17 @@ import {
   Repository,
   RepositoryProjectPathConfig,
 } from 'app/types';
+import {FieldFromConfig} from 'app/views/settings/components/forms';
 import Form from 'app/views/settings/components/forms/form';
-import JsonForm from 'app/views/settings/components/forms/jsonForm';
-import {JsonFormObject} from 'app/views/settings/components/forms/type';
+import {Field} from 'app/views/settings/components/forms/type';
 
 type Props = {
   organization: Organization;
   integration: Integration;
   projects: Project[];
   repos: Repository[];
-  onSubmitSuccess: Form['onSubmitSuccess'];
+  onSubmitSuccess: Form['props']['onSubmitSuccess'];
+  onCancel: Form['props']['onCancel'];
   existingConfig?: RepositoryProjectPathConfig;
 };
 
@@ -35,72 +35,70 @@ export default class RepositoryProjectPathConfigForm extends React.Component<Pro
     };
   }
 
-  get formFields(): JsonFormObject {
+  get formFields(): Field[] {
     const {projects, repos} = this.props;
     const repoChoices = repos.map(({name, id}) => ({value: id, label: name}));
-    return {
-      title: t('Create Code Path'),
-      fields: [
-        {
-          name: 'projectId',
-          type: 'sentry_project_selector',
-          required: true,
-          label: t('Project'),
-          inline: false,
-          projects,
-        },
-        {
-          name: 'repositoryId',
-          type: 'select',
-          required: true,
-          label: t('Repo'),
-          inline: false,
-          placeholder: t('Choose repo'),
-          options: repoChoices,
-          deprecatedSelectControl: false,
-        },
-        {
-          name: 'defaultBranch',
-          type: 'string',
-          required: true,
-          label: t('Branch'),
-          placeholder: t('Type your branch'),
-          inline: false,
-          showHelpInTooltip: true,
-          help: t(
-            'If an event does not have a release tied to a commit, we will use this branch when linking to your source code.'
-          ),
-        },
-        {
-          name: 'stackRoot',
-          type: 'string',
-          required: false,
-          label: t('Stack Trace Root'),
-          placeholder: t('Type root path of your stack traces'),
-          inline: false,
-          showHelpInTooltip: true,
-          help: t(
-            'Any stack trace starting with this path will be mapped with this rule. An empty string will match all paths.'
-          ),
-        },
-        {
-          name: 'sourceRoot',
-          type: 'string',
-          required: false,
-          label: t('Source Code Root'),
-          placeholder: t('Type root path of your source code'),
-          inline: false,
-          showHelpInTooltip: true,
-          help: t(
-            'When a rule matches, the stack trace root is replaced with this path to get the path in your repository. Leaving this empty means replacing the stack trace root with an empty string.'
-          ),
-        },
-      ],
-    };
+    return [
+      {
+        name: 'projectId',
+        type: 'sentry_project_selector',
+        required: true,
+        label: t('Project'),
+        projects,
+      },
+      {
+        name: 'repositoryId',
+        type: 'select',
+        required: true,
+        label: t('Repo'),
+        placeholder: t('Choose repo'),
+        options: repoChoices,
+        deprecatedSelectControl: false,
+      },
+      {
+        name: 'defaultBranch',
+        type: 'string',
+        required: true,
+        label: t('Branch'),
+        placeholder: t('Type your branch'),
+        showHelpInTooltip: true,
+        help: t(
+          'If an event does not have a release tied to a commit, we will use this branch when linking to your source code.'
+        ),
+      },
+      {
+        name: 'stackRoot',
+        type: 'string',
+        required: false,
+        label: t('Stack Trace Root'),
+        placeholder: t('Type root path of your stack traces'),
+        showHelpInTooltip: true,
+        help: t(
+          'Any stack trace starting with this path will be mapped with this rule. An empty string will match all paths.'
+        ),
+      },
+      {
+        name: 'sourceRoot',
+        type: 'string',
+        required: false,
+        label: t('Source Code Root'),
+        placeholder: t('Type root path of your source code'),
+        showHelpInTooltip: true,
+        help: t(
+          'When a rule matches, the stack trace root is replaced with this path to get the path in your repository. Leaving this empty means replacing the stack trace root with an empty string.'
+        ),
+      },
+    ];
   }
 
   render() {
-    const {organization, integration, onSubmitSuccess, existingConfig} = this.props;
+    const {
+      organization,
+      integration,
+      onSubmitSuccess,
+      onCancel,
+      existingConfig,
+    } = this.props;
 
     // endpoint changes if we are making a new row or updating an existing one
     const baseEndpoint = `/organizations/${organization.slug}/integrations/${integration.id}/repo-project-path-configs/`;
@@ -110,24 +108,23 @@ export default class RepositoryProjectPathConfigForm extends React.Component<Pro
     const apiMethod = existingConfig ? 'PUT' : 'POST';
 
     return (
-      <StyledForm
+      <Form
         onSubmitSuccess={onSubmitSuccess}
         initialData={this.initialData}
         apiEndpoint={endpoint}
         apiMethod={apiMethod}
+        onCancel={onCancel}
       >
-        <JsonForm forms={[this.formFields]} />
-      </StyledForm>
+        {this.formFields.map(field => (
+          <FieldFromConfig
+            key={field.name}
+            field={field}
+            inline={false}
+            stacked
+            flexibleControlStateSize
+          />
+        ))}
+      </Form>
     );
   }
 }
-
-const StyledForm = styled(Form)`
-  label {
-    color: ${p => p.theme.subText};
-    font-family: Rubik;
-    font-size: 12px;
-    font-weight: 500;
-    text-transform: uppercase;
-  }
-`;
