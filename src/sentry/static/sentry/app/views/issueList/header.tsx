c565765377aca42d9ef29f851179b3bf152e5451
Priscila Oliveira <priscilawebdev@gmail.com>
2020-12-16 10:29:37 +0100

    feat(issue-list): Add issue list new buttons (#22642)

--- src/sentry/static/sentry/app/views/issueList/header.tsx
+++ src/sentry/static/sentry/app/views/issueList/header.tsx
@@ -1,37 +1,84 @@
 import React from 'react';
+import {InjectedRouter} from 'react-router';
 import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
 
+import {openModal} from 'app/actionCreators/modal';
 import Button from 'app/components/button';
+import ButtonBar from 'app/components/buttonBar';
+import ContextPickerModalContainer from 'app/components/contextPickerModal';
 import * as Layout from 'app/components/layouts/thirds';
 import QueryCount from 'app/components/queryCount';
-import {IconPause, IconPlay} from 'app/icons';
+import {IconPause, IconPlay, IconUser} from 'app/icons';
 import {t} from 'app/locale';
 import space from 'app/styles/space';
+import {Organization, Project} from 'app/types';
+import withProjects from 'app/utils/withProjects';
+
+import {Query} from './utils';
 
 type Props = {
   query: string;
   queryCount: number;
   queryMaxCount: number;
   realtimeActive: boolean;
+  orgSlug: Organization['slug'];
+  router: InjectedRouter;
+  projectIds: Array<string>;
+  projects: Array<Project>;
   onRealtimeChange: (realtime: boolean) => void;
   onTabChange: (query: string) => void;
 };
 
 const queries = [
-  ['is:needs_review is:unresolved', t('Needs Review')],
-  ['is:unresolved', t('Unresolved')],
-  ['is:ignored', t('Ignored')],
+  [Query.NEEDS_REVIEW, t('Needs Review')],
+  [Query.UNRESOLVED, t('Unresolved')],
+  [Query.IGNORED, t('Ignored')],
+  [Query.REPROCESSING, t('Reprocessing')],
 ];
 
 function IssueListHeader({
   query,
   queryCount,
   queryMaxCount,
+  orgSlug,
+  projectIds,
   realtimeActive,
   onTabChange,
   onRealtimeChange,
+  projects,
+  router,
 }: Props) {
-  const count = <StyledQueryCount count={queryCount} max={queryMaxCount} />;
+  const selectedProjectSlugs = projectIds
+    .map(projectId => projects.find(project => project.id === projectId)?.slug)
+    .filter(selectedProjectSlug => !!selectedProjectSlug) as Array<string>;
+
+  const selectedProjectSlug =
+    selectedProjectSlugs.length === 1 ? selectedProjectSlugs[0] : undefined;
+
+  function handleSelectProject(settingsPage: string) {
+    return function (event: React.MouseEvent) {
+      event.preventDefault();
+
+      openModal(modalProps => (
+        <ContextPickerModalContainer
+          {...modalProps}
+          nextPath={`/settings/${orgSlug}/projects/:projectId/${settingsPage}/`}
+          needProject
+          needOrg={false}
+          onFinish={path => {
+            modalProps.closeModal();
+            router.push(path);
+          }}
+          projectSlugs={
+            !!selectedProjectSlugs.length
+              ? selectedProjectSlugs
+              : projects.map(p => p.slug)
+          }
+        />
+      ));
+    };
+  }
 
   return (
     <React.Fragment>
@@ -40,13 +87,27 @@ function IssueListHeader({
           <StyledLayoutTitle>{t('Issues')}</StyledLayoutTitle>
         </StyledHeaderContent>
         <Layout.HeaderActions>
-          <Button
-            size="small"
-            title={t('%s real-time updates', realtimeActive ? t('Pause') : t('Enable'))}
-            onClick={() => onRealtimeChange(!realtimeActive)}
-          >
-            {realtimeActive ? <IconPause size="xs" /> : <IconPlay size="xs" />}
-          </Button>
+          <ButtonBar gap={1}>
+            <Button
+              size="small"
+              icon={<IconUser size="xs" />}
+              to={
+                selectedProjectSlug
+                  ? `/settings/${orgSlug}/projects/${selectedProjectSlug}/ownership/`
+                  : undefined
+              }
+              onClick={selectedProjectSlug ? undefined : handleSelectProject('ownership')}
+            >
+              {t('Issue Owners')}
+            </Button>
+            <Button
+              size="small"
+              title={t('%s real-time updates', realtimeActive ? t('Pause') : t('Enable'))}
+              onClick={() => onRealtimeChange(!realtimeActive)}
+            >
+              {realtimeActive ? <IconPause size="xs" /> : <IconPlay size="xs" />}
+            </Button>
+          </ButtonBar>
         </Layout.HeaderActions>
       </BorderlessHeader>
       <TabLayoutHeader>
@@ -54,7 +115,10 @@ function IssueListHeader({
           {queries.map(([tabQuery, queryName]) => (
             <li key={tabQuery} className={query === tabQuery ? 'active' : ''}>
               <a onClick={() => onTabChange(tabQuery)}>
-                {queryName} {query === tabQuery && count}
+                {queryName}{' '}
+                {query === tabQuery && (
+                  <StyledQueryCount count={queryCount} max={queryMaxCount} />
+                )}
               </a>
             </li>
           ))}
@@ -64,7 +128,12 @@ function IssueListHeader({
   );
 }
 
-export default IssueListHeader;
+export default withProjects(IssueListHeader);
+
+IssueListHeader.propTypes = {
+  projectIds: PropTypes.array.isRequired,
+  projects: PropTypes.array.isRequired,
+};
 
 const StyledLayoutTitle = styled(Layout.Title)`
   margin-top: ${space(0.5)};
@@ -89,6 +158,7 @@ const TabLayoutHeader = styled(Layout.Header)`
 
 const StyledHeaderContent = styled(Layout.HeaderContent)`
   margin-bottom: 0;
+  margin-right: ${space(2)};
 `;
 
 const StyledQueryCount = styled(QueryCount)`
