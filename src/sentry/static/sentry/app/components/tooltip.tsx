cbe98bcc27c428439425cbce263fc8d6066b22d3
Priscila Oliveira <priscilawebdev@gmail.com>
2020-12-22 09:32:50 +0100

    feat(components): Add Clipboard tooltip component (#22818)

--- src/sentry/static/sentry/app/components/tooltip.tsx
+++ src/sentry/static/sentry/app/components/tooltip.tsx
@@ -9,7 +9,7 @@ import PropTypes from 'prop-types';
 import {IS_ACCEPTANCE_TEST} from 'app/constants';
 import {domId} from 'app/utils/domId';
 
-const IS_HOVERABLE_DELAY = 50; // used if isHoverable is true (for hiding AND showing)
+export const IS_HOVERABLE_DELAY = 50; // used if isHoverable is true (for hiding AND showing)
 
 type DefaultProps = {
   /**
