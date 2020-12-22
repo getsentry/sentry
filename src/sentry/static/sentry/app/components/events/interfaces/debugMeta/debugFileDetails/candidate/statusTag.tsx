312b5622d95f522614df7bc81edc03736c9262bb
Priscila Oliveira <priscilawebdev@gmail.com>
2020-12-22 17:39:09 +0100

    ref(debugFileDetails): Add tooltip descriptions (#22820)

--- src/sentry/static/sentry/app/components/events/interfaces/debugMeta/debugFileDetails/candidate/statusTag.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/debugMeta/debugFileDetails/candidate/statusTag.tsx
@@ -1,32 +1,84 @@
 import React from 'react';
 
 import Tag from 'app/components/tag';
-import {t} from 'app/locale';
-import {CandidateDownloadStatus} from 'app/types/debugImage';
+import {t, tct} from 'app/locale';
+import {Candidate, CandidateDownloadStatus} from 'app/types/debugImage';
+
+import {INTERNAL_SOURCE} from '../utils';
+
+import StatusTagTooltip from './statusTagTooltip';
 
 type Props = {
-  status: CandidateDownloadStatus;
+  candidate: Candidate;
 };
 
-function StatusTag({status}: Props) {
-  switch (status) {
+function StatusTag({candidate}: Props) {
+  const {download, location, source} = candidate;
+
+  switch (download.status) {
     case CandidateDownloadStatus.OK: {
-      return <Tag type="success">{t('Successful')}</Tag>;
+      return (
+        <StatusTagTooltip
+          label={t('Download Details')}
+          description={location}
+          disabled={!location || source === INTERNAL_SOURCE}
+        >
+          <Tag type="success">{t('Successful')}</Tag>
+        </StatusTagTooltip>
+      );
     }
     case CandidateDownloadStatus.MALFORMED: {
-      return <Tag type="error">{t('Failed')}</Tag>;
+      const {details} = download;
+      return (
+        <StatusTagTooltip label={t('Download Details')} description={details}>
+          <Tag type="error">{t('Failed')}</Tag>
+        </StatusTagTooltip>
+      );
     }
     case CandidateDownloadStatus.NOT_FOUND: {
-      return <Tag>{t('Not Found')}</Tag>;
+      const {details} = download;
+      return (
+        <StatusTagTooltip
+          label={t('No debug file was not found at this location.')}
+          description={
+            <React.Fragment>
+              <div>{tct('Location: [location]', {location})}</div>
+              <div>{details}</div>
+            </React.Fragment>
+          }
+          disabled={!location || source === INTERNAL_SOURCE}
+        >
+          <Tag>{t('Not Found')}</Tag>
+        </StatusTagTooltip>
+      );
     }
     case CandidateDownloadStatus.NO_PERMISSION: {
-      return <Tag type="warning">{t('Permission')}</Tag>;
+      const {details} = download;
+      return (
+        <StatusTagTooltip label={t('Permission Error')} description={details}>
+          <Tag type="warning">{t('Permissions')}</Tag>
+        </StatusTagTooltip>
+      );
     }
     case CandidateDownloadStatus.DELETED: {
-      return <Tag type="error">{t('Deleted')}</Tag>;
+      return (
+        <StatusTagTooltip
+          label={t('This file was deleted after the issue was processed.')}
+        >
+          <Tag type="error">{t('Deleted')}</Tag>
+        </StatusTagTooltip>
+      );
     }
     default:
-      return <Tag type="highlight">{t('Not applied')}</Tag>;
+      return (
+        <StatusTagTooltip
+          label={t(
+            'This issue was processed before this debug information file was available. To apply new debug information, reprocess this issue. '
+          )}
+        >
+          <Tag type="highlight">{t('Unapplied')}</Tag>
+        </StatusTagTooltip>
+      );
   }
 }
 
