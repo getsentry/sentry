a569d80bcfc6eca5370e928acb3230e419b9f6d8
Taylan Gocmen <ta.gocmen@gmail.com>
2021-01-04 09:55:16 -0800

    use emotion styled

--- src/sentry/static/sentry/app/components/queryCount.tsx
+++ src/sentry/static/sentry/app/components/queryCount.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import PropTypes from 'prop-types';
 
-import styled from 'app/styled';
 import space from 'app/styles/space';
 import {defined} from 'app/utils';
 
