ff14f67f53d814f416c54ad7d8b114f5b785cc53
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-01-04 14:15:01 +0100

    fix(dropdownbutton): Fix incorrect border bottom color

--- src/sentry/static/sentry/app/components/events/interfaces/spans/filter.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/filter.tsx
@@ -101,7 +101,7 @@ class Filter extends React.Component<Props> {
           <FilterLabel>{t('Filter')}</FilterLabel>
         </React.Fragment>
       ),
-      priority: 'default',
+      priority: 'form',
       hasDarkBorderBottomColor: false,
     };
 
