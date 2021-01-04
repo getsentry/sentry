e263c8ccd3b96a99b213aa348771fd5a343b85b0
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2021-01-04 10:34:53 -0800

    feat(workflow): Update inbox reason tooltip (WOR-482) (#22860)
    * feat(inbox): Update inbox reason tooltip
    
    * don't show tooltip if no text or description and make 1k limit const
    
    * tack on badge orange
    
    * style(lint): Auto commit lint changes
    
    * style(lint): Auto commit lint changes
    
    * style(lint): Auto commit lint changes
    
    * use emotion styled
    
    Co-authored-by: sentry-internal-tools[bot] <66042841+sentry-internal-tools[bot]@users.noreply.github.com>

--- src/sentry/static/sentry/app/views/issueList/header.tsx
+++ src/sentry/static/sentry/app/views/issueList/header.tsx
@@ -13,6 +13,7 @@ import {IconPause, IconPlay, IconUser} from 'app/icons';
 import {t} from 'app/locale';
 import space from 'app/styles/space';
 import {Organization, Project} from 'app/types';
+import theme from 'app/utils/theme';
 import withProjects from 'app/utils/withProjects';
 
 import {Query, QueryCounts, TAB_MAX_COUNT} from './utils';
@@ -124,11 +125,9 @@ function IssueListHeader({
                   <StyledQueryCount
                     count={queryCounts[tabQuery].count}
                     max={queryCounts[tabQuery].hasMore ? TAB_MAX_COUNT : 1000}
-                    tagType={
-                      (tabQuery === Query.NEEDS_REVIEW && 'warning') ||
-                      (tabQuery === Query.UNRESOLVED && 'default') ||
-                      (tabQuery === Query.IGNORED && 'default') ||
-                      undefined
+                    backgroundColor={
+                      (tabQuery === Query.NEEDS_REVIEW && theme.yellow300) ||
+                      theme.gray100
                     }
                   />
                 )}
