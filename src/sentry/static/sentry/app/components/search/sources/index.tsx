c9f9d33b802f8acecab6e0a7a2e70d7c00c3f197
Mark Story <mark@sentry.io>
2021-01-08 10:39:22 -0500

    ref(ts) Convert search/source/index to typescript (#22976)
    Simplify a few of the sources by using fuse generics better and fix some
    missing properties that consumers of Source are expecting to get.

--- src/sentry/static/sentry/app/components/search/sources/index.tsx
+++ src/sentry/static/sentry/app/components/search/sources/index.tsx
@@ -1,23 +1,28 @@
 import React from 'react';
 import flatten from 'lodash/flatten';
-import PropTypes from 'prop-types';
 
-class SearchSources extends React.Component {
-  static propTypes = {
-    sources: PropTypes.array.isRequired,
-    query: PropTypes.string,
-    /**
-     * Render function the passes:
-     *
-     * `isLoading`
-     * `results` - Array of results
-     * `hasAnyResults` - if any results were found
-     */
-    children: PropTypes.func,
-  };
+import {Result} from './types';
 
+type ChildProps = {
+  results: Result[];
+  isLoading: boolean;
+  hasAnyResults: boolean;
+};
+
+type Props = {
+  sources: React.ComponentClass[];
+  query: string;
+  children: (props: ChildProps) => React.ReactElement;
+};
+
+type SourceResult = {
+  isLoading: boolean;
+  results: Result[];
+};
+
+class SearchSources extends React.Component<Props> {
   // `allSources` will be an array of all result objects from each source
-  renderResults(allSources) {
+  renderResults(allSources: SourceResult[]) {
     const {children} = this.props;
 
     // loading means if any result has `isLoading` OR any result is null
@@ -37,14 +42,14 @@ class SearchSources extends React.Component {
     });
   }
 
-  renderSources(sources, results, idx) {
+  renderSources(sources: Props['sources'], results: SourceResult[], idx: number) {
     if (idx >= sources.length) {
       return this.renderResults(results);
     }
     const Source = sources[idx];
     return (
       <Source {...this.props}>
-        {args => {
+        {(args: SourceResult) => {
           // Mutate the array instead of pushing because we don't know how often
           // this child function will be called and pushing will cause duplicate
           // results to be pushed for all calls down the chain.
