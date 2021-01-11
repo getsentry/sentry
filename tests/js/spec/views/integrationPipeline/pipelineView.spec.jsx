fb829502a1af54dc5a54a279edd41bc8c3212d2c
Stephen Cefali <scefali@sentry.io>
2021-01-11 13:08:32 -0800

    adds tests for AWS Lambda views (#23007)

---
+++ tests/js/spec/views/integrationPipeline/pipelineView.spec.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import {shallow} from 'sentry-test/enzyme';
+
+import AwsLambdaProjectSelect from 'app/views/integrationPipeline/awsLambdaProjectSelect';
+import PipelineView from 'app/views/integrationPipeline/pipelineView';
+
+describe('PipelineView', () => {
+  it('renders awsLambdaProjectSelect', () => {
+    const wrapper = shallow(
+      <PipelineView pipelineName="awsLambdaProjectSelect" someField="someVal" />,
+      TestStubs.routerContext()
+    );
+    expect(wrapper.find(AwsLambdaProjectSelect).prop('someField')).toBe('someVal');
+    expect(document.title).toBe('AWS Lambda Select Project');
+  });
+  it('errros on invalid pipelineName', () => {
+    expect(() =>
+      shallow(<PipelineView pipelineName="other" />, TestStubs.routerContext())
+    ).toThrow('Invalid pipeline name other');
+  });
+});
