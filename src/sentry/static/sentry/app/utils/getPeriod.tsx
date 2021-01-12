b72719535225aec27a876a3d420cc8841f26c477
Matej Minar <matej.minar@sentry.io>
2021-01-12 13:05:01 +0100

    ref(getPeriod): Remove the explicit return type, refactor params and comments

--- src/sentry/static/sentry/app/utils/getPeriod.tsx
+++ src/sentry/static/sentry/app/utils/getPeriod.tsx
@@ -3,24 +3,38 @@ import moment from 'moment';
 import {DEFAULT_STATS_PERIOD} from 'app/constants';
 import {getUtcDateString} from 'app/utils/dates';
 
+type DateObject = {
+  /**
+   * Relative period string in format "<int><unit>" (e.g. 4d for 4 days)
+   */
+  period?: string;
+  /**
+   * Starting date object
+   */
+  start?: string;
+  /**
+   * Ending date object
+   */
+  end?: string;
+};
+
+type Options = {
+  /**
+   * Doubles the given period (useful for getting previous period data)
+   */
+  shouldDoublePeriod?: boolean;
+};
+
 /**
  * Gets the period to query with if we need to double the initial period in order
  * to get data for the previous period
  *
- * @param {Object} dateObj The date object
- * @param {String} dateObj.period Relative period string in format "<int><unit>" (e.g. 4d for 4 days)
- * @param {Date} dateObj.start Starting date object
- * @param {Date} dateObj.end Ending date object
- * @param {Object} options Options
- * @param {Boolean} [options.shouldDoublePeriod] Doubles the given period (useful for getting previous period data)
- * @return {Object} Returns an object with either a period or start/end dates ({statsPeriod: string} or {start: string, end: string})
+ * Returns an object with either a period or start/end dates ({statsPeriod: string} or {start: string, end: string})
  */
-type Options = {shouldDoublePeriod?: boolean};
-
 export const getPeriod = (
-  {period, start, end},
+  {period, start, end}: DateObject,
   {shouldDoublePeriod}: Options = {}
-): {start?: string; end?: string; statsPeriod?: string} => {
+) => {
   if (!period && !start && !end) {
     period = DEFAULT_STATS_PERIOD;
   }
@@ -31,7 +45,7 @@ export const getPeriod = (
     if (!shouldDoublePeriod) {
       return {statsPeriod: period};
     }
-    const [, periodNumber, periodLength] = period.match(/([0-9]+)([mhdw])/);
+    const [, periodNumber, periodLength] = period.match(/([0-9]+)([mhdw])/)!;
 
     return {statsPeriod: `${parseInt(periodNumber, 10) * 2}${periodLength}`};
   }
