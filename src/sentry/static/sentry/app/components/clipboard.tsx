448e5b8dfc85ac0fbb98d96e292ff2136b430c04
Scott Cooper <scttcper@gmail.com>
2021-01-12 22:42:36 -0800

    feat(ui): Switch from clipboard to copy-text-to-clipboard

--- src/sentry/static/sentry/app/components/clipboard.tsx
+++ src/sentry/static/sentry/app/components/clipboard.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import Clip from 'clipboard';
-import PropTypes from 'prop-types';
+import copy from 'copy-text-to-clipboard';
 
 import {addErrorMessage, addSuccessMessage} from 'app/actionCreators/indicator';
 
@@ -12,95 +10,64 @@ type DefaultProps = {
 };
 
 type Props = {
+  children: React.ReactElement<{onClick: () => void}>;
   value: string;
   hideUnsupported?: boolean;
   onSuccess?: () => void;
   onError?: () => void;
 } & DefaultProps;
 
-class Clipboard extends React.Component<Props> {
-  static propTypes = {
-    value: PropTypes.string,
-    successMessage: PropTypes.string,
-    errorMessage: PropTypes.string,
-    hideMessages: PropTypes.bool,
-
-    /**
-     * Hide component if browser does not support "execCommand"
-     */
-    hideUnsupported: PropTypes.bool,
-    onSuccess: PropTypes.func,
-    onError: PropTypes.func,
-  };
-
-  static defaultProps: DefaultProps = {
-    hideMessages: false,
-    successMessage: 'Copied to clipboard',
-    errorMessage: 'Error copying to clipboard',
-  };
-
-  componentWillUnmount() {
-    if (this.clipboard) {
-      this.clipboard.destroy();
-    }
-  }
-
-  clipboard!: ClipboardJS;
-
-  handleMount = (ref: HTMLElement) => {
-    if (!ref) {
-      return;
-    }
-
-    const {hideMessages, successMessage, errorMessage, onSuccess, onError} = this.props;
-    const hasSuccessCb = typeof onSuccess === 'function';
-    const hasErrorCb = typeof onError === 'function';
-    const bindEventHandlers = !hideMessages || hasSuccessCb || hasErrorCb;
-
-    // eslint-disable-next-line react/no-find-dom-node
-    this.clipboard = new Clip(ReactDOM.findDOMNode(ref) as Element, {
-      text: () => this.props.value,
-    });
-
-    if (!bindEventHandlers) {
-      return;
-    }
-
-    this.clipboard
-      .on('success', () => {
-        if (!hideMessages) {
-          addSuccessMessage(successMessage);
-        }
-        if (onSuccess && hasSuccessCb) {
-          onSuccess();
-        }
-      })
-      .on('error', () => {
-        if (!hideMessages) {
-          addErrorMessage(errorMessage);
-        }
-        if (onError && hasErrorCb) {
-          onError();
-        }
-      });
-  };
-
-  render() {
-    const {children, hideUnsupported} = this.props;
-
-    // Browser doesn't support `execCommand`
-    if (hideUnsupported && !Clip.isSupported()) {
-      return null;
-    }
-
-    if (!React.isValidElement(children)) {
-      return null;
-    }
-
-    return React.cloneElement(children, {
-      ref: this.handleMount,
-    });
-  }
+/**
+ * copy-text-to-clipboard relies on `document.execCommand('copy')`
+ */
+function isSupported() {
+  const support = !!document.queryCommandSupported;
+  return support && !!document.queryCommandSupported('copy');
 }
+
+function Clipboard({
+  children,
+  successMessage,
+  errorMessage,
+  hideMessages,
+  hideUnsupported,
+  value,
+  onSuccess,
+  onError,
+}: Props) {
+  function handleClick() {
+    // Copy returns whether it succeeded to copy the text
+    const success = copy(value);
+    if (success) {
+      if (!hideMessages) {
+        addSuccessMessage(successMessage);
+      }
+      onSuccess?.();
+    } else {
+      if (!hideMessages) {
+        addErrorMessage(errorMessage);
+      }
+      onError?.();
+    }
+  }
+
+  if (hideUnsupported && !isSupported()) {
+    return null;
+  }
+
+  if (!React.isValidElement(children)) {
+    return null;
+  }
+
+  return React.cloneElement(children, {
+    onClick: () => handleClick(),
+  });
+}
+
+Clipboard.defaultProps = {
+  hideMessages: false,
+  successMessage: 'Copied to clipboard',
+  errorMessage: 'Error copying to clipboard',
+};
 
 export default Clipboard;
