e263bac84483ffa11ab762beb9cd3051b033e15f
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-01-13 14:15:38 +0100

    feat(filters-and-sampling): Add Transactions panel

---
+++ src/sentry/static/sentry/app/types/filtersAndSampling.tsx
@@ -0,0 +1,67 @@
+export enum RuleType {
+  /**
+   * The rule applies to traces (transaction events considered in the context of a trace)
+   */
+  TRACE = 'trace',
+  /**
+   *  The rule applies to transaction events considered independently
+   */
+  TRANSACTION = 'transaction',
+  /**
+   * The rule applies to error events (not transaction events)
+   */
+  ERROR = 'error',
+}
+
+enum ConditionOperator {
+  /**
+   * It uses glob matches for checking (e.g. releases use glob matching "1.1.*" will match release 1.1.1 and 1.1.2)
+   */
+  GLOB_MATCH = 'globMatch',
+  /**
+   * It uses simple equality for checking
+   */
+  EQUAL = 'equal',
+  /**
+   * It uses a case insensitive string comparison
+   */
+  STR_EQUAL_NO_CASE = 'strEqualNoCase',
+}
+
+type Condition = {
+  /**
+   * The function that will be applied to check if the condition is satisfied.
+   * Currently there are three operators defined.
+   */
+  operator: ConditionOperator;
+  /**
+   * The name of the filed to be used for comparison
+   */
+  name: string;
+  /**
+   * Values to be compared (using the operator) against the field (specified by the field name)
+   */
+  value: Array<string>;
+};
+
+export type Rule = {
+  /**
+   * It is a possibly empty list of project ids to which the rule applies.
+   * If the list is empty the rules applies for all projects in the organisation
+   */
+  projectIds: Array<number>;
+  /**
+   * It is a possibly empty list of conditions to which the rule applies.
+   * The conditions are combined using the and operator (so all the conditions must be satisfied for the rule to apply).
+   * If the conditions field is an empty list the rule applies for all events that satisfy the projectIds and the ty fields.
+   */
+  conditions: Array<Condition>;
+  /**
+   * it is the sampling rate that will be applied if the rule is selected
+   */
+  sampleRate: number;
+  /**
+   * Describes the type of rule
+   */
+  ty: RuleType;
+};
