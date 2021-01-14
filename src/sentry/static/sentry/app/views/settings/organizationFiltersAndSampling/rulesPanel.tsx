146f9aff1bc67650d227343a4b2f9f96a86b55e8
Priscila Oliveira <priscilawebdev@gmail.com>
2021-01-14 16:32:23 +0100

    Update src/sentry/static/sentry/app/views/settings/organizationFiltersAndSampling/rulesPanel.tsx

--- src/sentry/static/sentry/app/views/settings/organizationFiltersAndSampling/rulesPanel.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationFiltersAndSampling/rulesPanel.tsx
@@ -22,7 +22,7 @@ function RulesPanel({rules, docsUrl, onAddRule}: Props) {
       <Rules rules={rules} />
       <StyledPanelFooter>
         <ButtonBar gap={1}>
-          <Button href={docsUrl} target="_blank">
+          <Button href={docsUrl} external>
             {t('Read the docs')}
           </Button>
           <Button priority="primary" onClick={onAddRule}>
