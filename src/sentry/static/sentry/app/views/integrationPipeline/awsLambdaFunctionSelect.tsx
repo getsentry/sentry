39c0e631960760380bccf6559272b2dc3757b184
Stephen Cefali <scefali@sentry.io>
2021-01-14 14:56:24 -0800

    first stuff done

--- src/sentry/static/sentry/app/views/integrationPipeline/awsLambdaFunctionSelect.tsx
+++ src/sentry/static/sentry/app/views/integrationPipeline/awsLambdaFunctionSelect.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import styled from '@emotion/styled';
+import {Observer} from 'mobx-react';
 
 import {addLoadingMessage} from 'app/actionCreators/indicator';
+import Button from 'app/components/actions/button';
+import Alert from 'app/components/alert';
+import {IconSentry} from 'app/icons';
 import {t} from 'app/locale';
+import PluginIcon from 'app/plugins/components/pluginIcon';
 import Form from 'app/views/settings/components/forms/form';
 import JsonForm from 'app/views/settings/components/forms/jsonForm';
 import FormModel from 'app/views/settings/components/forms/model';
@@ -13,7 +18,7 @@ type Props = {
   lambdaFunctions: LambdaFunction[];
 };
 
-const getLabel = (func: LambdaFunction) => `${func.FunctionName} - ${func.Runtime}`;
+const getLabel = (func: LambdaFunction) => func.FunctionName;
 
 export default class AwsLambdaFunctionSelect extends React.Component<Props> {
   model = new FormModel({apiOptions: {baseUrl: window.location.origin}});
@@ -31,29 +36,74 @@ export default class AwsLambdaFunctionSelect extends React.Component<Props> {
     );
   }
   handlePreSubmit = () => addLoadingMessage(t('Submitting\u2026'));
+  handleSubmit = () => {
+    addLoadingMessage(t('Submitting\u2026'));
+    this.model.saveForm();
+    // window.location.reload();
+  };
+  renderWhatWeFound = () => {
+    const count = this.lambdaFunctions.length;
+    return <WhatWeFound>{t('We found %s functions', count)}</WhatWeFound>;
+  };
+  handleSubmitSuccess = () => {};
+  renderFooter = () => {
+    return (
+      <Footer>
+        <ButtonWrapper>
+          <StyledButton size="small">{t('View Docs')}</StyledButton>
+          <Observer>
+            {() => (
+              <StyledButton
+                priority="primary"
+                disabled={this.model.isError || this.model.isSaving}
+                type="submit"
+                onClick={this.handleSubmit}
+                size="small"
+              >
+                {t('Finish Setup')}
+              </StyledButton>
+            )}
+          </Observer>
+        </ButtonWrapper>
+      </Footer>
+    );
+  };
   render = () => {
     const model = this.model;
     const formFields: JsonFormObject = {
-      title: t('Select the lambda functions to install Sentry on'),
       fields: this.lambdaFunctions.map(func => {
         return {
           name: func.FunctionName,
           type: 'boolean',
           required: false,
           label: getLabel(func),
+          alignRight: true,
         };
       }),
     };
     return (
-      <StyledForm
-        initialData={this.initialData}
-        skipPreventDefault
-        model={model}
-        apiEndpoint="/extensions/aws_lambda/setup/"
-        onPreSubmit={this.handlePreSubmit}
-      >
-        <JsonForm forms={[formFields]} />
-      </StyledForm>
+      <Wrapper>
+        <Alert type="info">{t('Currently only supports Node runtimes')}</Alert>
+        <Header>
+          <IconWrapper>
+            <PluginIcon size={50} pluginId="aws_lambda" />
+            <StyledIconSentry />
+          </IconWrapper>
+          {this.renderWhatWeFound()}
+        </Header>
+        <StyledForm
+          initialData={this.initialData}
+          skipPreventDefault
+          model={model}
+          apiEndpoint="/extensions/aws_lambda/setup/"
+          // onPreSubmit={this.handlePreSubmit}
+          hideFooter
+          onSubmitSuccess={this.handleSubmitSuccess}
+        >
+          <JsonForm forms={[formFields]} />
+        </StyledForm>
+        {this.renderFooter()}
+      </Wrapper>
     );
   };
 }
@@ -61,4 +111,43 @@ export default class AwsLambdaFunctionSelect extends React.Component<Props> {
 const StyledForm = styled(Form)`
   margin: 50px;
   padding-bottom: 50px;
+`;
+
+const Wrapper = styled('div')``;
+
+//wrap in form so we can keep form submission behavior
+const Footer = styled('form')`
+  position: fixed;
+  bottom: 0;
+  width: 100%;
+  height: 60px;
+  z-index: 100;
+  background-color: ${p => p.theme.bodyBackground};
+  border-top: 1px solid ${p => p.theme.gray100};
+`;
+
+const StyledButton = styled(Button)`
+  padding: 0;
+  margin-right: 20px;
+`;
+
+const ButtonWrapper = styled('div')`
+  padding: 20px 0;
+  float: right;
+`;
+
+const WhatWeFound = styled('div')`
+  font-size: ${p => p.theme.headerFontSize};
+`;
+
+const IconWrapper = styled('div')``;
+
+const StyledIconSentry = styled(IconSentry)`
+  width: 50px;
+  height: 50px;
+  margin-left: 40px;
+`;
+
+const Header = styled('div')`
+  text-align: center;
 `;
