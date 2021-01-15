6933f62191df2e1aa7c139615e6f2884544be211
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-01-15 16:42:20 +0100

    applied cr feedback

--- src/sentry/static/sentry/app/views/settings/organizationFiltersAndSampling/rules/rule/projects.tsx
+++ src/sentry/static/sentry/app/views/settings/organizationFiltersAndSampling/rules/rule/projects.tsx
@@ -5,15 +5,28 @@ import {t} from 'app/locale';
 import {Organization, Project} from 'app/types';
 import Projects from 'app/utils/projects';
 import withOrganization from 'app/utils/withOrganization';
+import withProjects from 'app/utils/withProjects';
 
 type Props = {
   projectIds: Array<number>;
+  projects: Array<Project>;
   organization: Organization;
 };
 
-function ProjectList({projectIds, organization}: Props) {
+function ProjectList({projectIds, organization, ...props}: Props) {
   if (!projectIds.length) {
     return <React.Fragment>{t('All')}</React.Fragment>;
+  }
+
+  const filteresProjects = props.projects.filter(project =>
+    projectIds.includes(Number(project.id))
+  );
+
+  if (filteresProjects.length === projectIds.length) {
+    const projectPlatforms = filteresProjects.map(
+      projectPlatform => projectPlatform?.platform ?? 'other'
+    );
+    return <PlatformList platforms={projectPlatforms} />;
   }
 
   return (
@@ -29,4 +42,4 @@ function ProjectList({projectIds, organization}: Props) {
   );
 }
 
-export default withOrganization(ProjectList);
+export default withOrganization(withProjects(ProjectList));
