7a41aea9b222b824b1d57be03b08875ebe81683f
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-01-15 19:31:00 +0100

    fix: More nodestore stats fixes
    * sizes are not comparable if they compress differently, use sort_keys
    * Using the event class goes through renormalization while we don't want
    that
    * Going through interfaces, in all likelihood, mutates the event dict
    in-place

--- tests/sentry/eventstore/test_compressor.py
+++ tests/sentry/eventstore/test_compressor.py
@@ -23,6 +23,8 @@ def _assert_roundtrip(data, assert_extra_keys=None):
 
 
 def test_basic():
+    assert deduplicate({}) == ({}, {})
+
     _assert_roundtrip({})
     _assert_roundtrip({"debug_meta": {}})
     _assert_roundtrip({"debug_meta": None})
