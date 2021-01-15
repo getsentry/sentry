27af834d052037485a8c9b2c53796efe163da23a
Floris Bruynooghe <flub@sentry.io>
2021-01-15 12:34:19 +0100

    Re-add our stripping
    Somehow the entire response gets lost with this, and I don't know
    why yet.  Annoying I can't reproduce this locally.

--- src/sentry/lang/native/symbolicator.py
+++ src/sentry/lang/native/symbolicator.py
@@ -173,8 +173,7 @@ class Symbolicator(object):
                 metrics.timing(
                     "events.symbolicator.response.completed.size", len(json.dumps(json_response))
                 )
-                # return redact_internal_sources(json_response)
-                return json_response
+                return redact_internal_sources(json_response)
 
     def process_minidump(self, minidump):
         return self._process(lambda: self.sess.upload_minidump(minidump), "process_minidump")
