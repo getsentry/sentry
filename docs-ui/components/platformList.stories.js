1d5af69b8824be4d19ffc38b54dadf9393f99a5b
Priscila Oliveira <priscilawebdev@gmail.com>
2021-01-18 10:50:00 +0100

    ref(platformlist): Add show counter prop (#23097)

--- docs-ui/components/platformList.stories.js
+++ docs-ui/components/platformList.stories.js
@@ -1,16 +1,36 @@
 import React from 'react';
+import styled from '@emotion/styled';
 import {withInfo} from '@storybook/addon-info';
+import {boolean, number} from '@storybook/addon-knobs';
 
 import PlatformList from 'app/components/platformList';
+import space from 'app/styles/space';
 
 export default {
   title: 'UI/Platform List',
 };
 
+const platforms = ['java', 'php', 'javascript', 'cocoa', 'ruby'];
+
 export const _PlatformList = withInfo('Stacked list of platform and framework icons')(
   () => (
-    <div style={{padding: 20, backgroundColor: '#ffffff'}}>
-      <PlatformList platforms={['java', 'php', 'javascript', 'cocoa', 'ruby']} />
-    </div>
+    <Wrapper>
+      <PlatformList
+        platforms={platforms}
+        showCounter={boolean('Show Counter', false)}
+        max={number('Max', platforms.length)}
+        size={number('Size', 28)}
+      />
+    </Wrapper>
   )
 );
+
+_PlatformList.story = {
+  name: 'PlatformList',
+};
+
+const Wrapper = styled('div')`
+  padding: ${space(3)};
+  background: ${p => p.theme.white};
+  display: flex;
+`;
