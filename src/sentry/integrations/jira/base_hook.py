dab7a945ad131c46d4fe9bb49c5a432b1bfd1a05
Colleen O'Rourke <colleen@sentry.io>
2021-01-19 14:16:16 -0800

    fix(jira): add coop directive (#23171)

--- src/sentry/integrations/jira/base_hook.py
+++ src/sentry/integrations/jira/base_hook.py
@@ -11,9 +11,8 @@ class JiraBaseHook(View):
     def get_response(self, context):
         context["ac_js_src"] = "https://connect-cdn.atl-paas.net/all.js"
         res = render_to_response(self.html_file, context, self.request)
-        # we aren't actually displaying it on the same page but we don't want to set it to deny
-        # which security.py will do
-        res["X-Frame-Options"] = "SAMEORIGIN"
+        # COOP blocks the Jira glance view links from opening
+        res["Cross-Origin-Opener-Policy"] = "same-origin-allow-popups"
         res["Content-Security-Policy"] = u"frame-ancestors 'self' %s %s" % (
             self.request.GET["xdm_e"],
             options.get("system.url-prefix"),
