d26bb7d1bf44a92204869a0b360c42db02036ce7
Tony Xiao <txiao@sentry.io>
2021-01-19 12:26:16 -0500

    ref(perf): Use new vitals endpoint to simplify frontend query
    The existing query to eventsv2 requires multiple long fields. This change
    updates it to use the new vitals endpoint.

--- src/sentry/static/sentry/app/views/performance/vitalDetail/vitalsCardsDiscoverQuery.tsx
+++ src/sentry/static/sentry/app/views/performance/vitalDetail/vitalsCardsDiscoverQuery.tsx
@@ -1,59 +1,70 @@
 import React from 'react';
+import pick from 'lodash/pick';
 
+import {URL_PARAM} from 'app/constants/globalSelectionHeader';
 import {MetaType} from 'app/utils/discover/eventView';
 import {WebVital} from 'app/utils/discover/fields';
 import GenericDiscoverQuery, {
   DiscoverQueryProps,
+  GenericChildrenProps,
 } from 'app/utils/discover/genericDiscoverQuery';
 import withApi from 'app/utils/withApi';
-
-import {
-  vitalsBaseFields,
-  vitalsMehFields,
-  vitalsP75Fields,
-  vitalsPoorFields,
-} from './utils';
 
 export type TableDataRow = {
   id: string;
   [key: string]: React.ReactText;
 };
+
 export type TableData = {
   data: Array<TableDataRow>;
   meta?: MetaType;
 };
 
-type Props = DiscoverQueryProps & {
+export type VitalData = {
+  poor: number;
+  meh: number;
+  good: number;
+  total: number;
+  p75: number | null;
+};
+
+export type VitalsData = Record<string, VitalData>;
+
+type VitalsProps = {
   vitals: WebVital[];
 };
 
-function getRequestPayload(props: Props) {
+type RequestProps = DiscoverQueryProps & VitalsProps;
+
+type ChildrenProps = Omit<GenericChildrenProps<VitalsProps>, 'tableData'> & {
+  vitalsData: VitalsData | null;
+};
+
+type Props = RequestProps & {
+  children: (props: ChildrenProps) => React.ReactNode;
+};
+
+function getRequestPayload(props: RequestProps) {
   const {eventView, vitals} = props;
   const apiPayload = eventView?.getEventsAPIPayload(props.location);
-  const vitalFields: string[] = vitals
-    .map(vital =>
-      [
-        vitalsPoorFields[vital],
-        vitalsBaseFields[vital],
-        vitalsMehFields[vital],
-        vitalsP75Fields[vital],
-      ].filter(Boolean)
-    )
-    .reduce((fields, fs) => fields.concat(fs), []);
-  apiPayload.field = [...vitalFields];
-  delete apiPayload.sort;
-  return apiPayload;
+  return {
+    vital: vitals,
+    ...pick(apiPayload, ['query', ...Object.values(URL_PARAM)]),
+  };
 }
 
-function VitalsCardDiscoverQuery(props: Props) {
+function VitalsCardsDiscoverQuery(props: Props) {
   return (
-    <GenericDiscoverQuery<TableData, Props>
+    <GenericDiscoverQuery<VitalsData, VitalsProps>
       getRequestPayload={getRequestPayload}
-      route="eventsv2"
-      noPagination
+      route="events-vitals"
       {...props}
-    />
+    >
+      {({tableData, ...rest}) => {
+        return props.children({vitalsData: tableData, ...rest});
+      }}
+    </GenericDiscoverQuery>
   );
 }
 
-export default withApi(VitalsCardDiscoverQuery);
+export default withApi(VitalsCardsDiscoverQuery);
