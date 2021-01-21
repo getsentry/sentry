10c93b96659880ce10607a6691f6c9247b9ee4a0
Dan Fuller <dfuller@sentry.io>
2021-01-20 16:49:06 -0800

    dict not set

--- src/sentry/api/serializers/models/eventattachment.py
+++ src/sentry/api/serializers/models/eventattachment.py
@@ -9,7 +9,7 @@ from sentry.models import EventAttachment, File
 @register(EventAttachment)
 class EventAttachmentSerializer(Serializer):
     def get_attrs(self, item_list, user, **kwargs):
-        files = {f.id for f in File.objects.filter(id__in=[ea.file_id for ea in item_list])}
+        files = {f.id: f for f in File.objects.filter(id__in=[ea.file_id for ea in item_list])}
         return {ea: {"file": files[ea.file_id]} for ea in item_list}
 
     def serialize(self, obj, attrs, user):
