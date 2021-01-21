44b05a0776dfc075ef3d314a5e17c77bbd7a8cea
Dan Fuller <dfuller@sentry.io>
2021-01-20 17:25:25 -0800

    coerce to list

--- src/sentry/api/serializers/models/userreport.py
+++ src/sentry/api/serializers/models/userreport.py
@@ -55,7 +55,7 @@ class UserReportWithGroupSerializer(UserReportSerializer):
     def get_attrs(self, item_list, user):
         from sentry.api.serializers import GroupSerializer
 
-        groups = Group.objects.filter(id__in=[i.group_id for i in item_list if i.group_id])
+        groups = list(Group.objects.filter(id__in=[i.group_id for i in item_list if i.group_id]))
         serialized_groups = {}
         if groups:
             serialized_groups = {
