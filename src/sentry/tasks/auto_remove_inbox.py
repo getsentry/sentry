6487aa08cbc18694a743a2e7de92aca1f60c0f60
Scott Cooper <scttcper@gmail.com>
2021-01-22 13:40:59 -0800

    time_limit 120

--- src/sentry/tasks/auto_remove_inbox.py
+++ src/sentry/tasks/auto_remove_inbox.py
@@ -5,6 +5,6 @@ from sentry.models import GroupInbox
 from sentry.tasks.base import instrumented_task
 
 
-@instrumented_task(name="sentry.tasks.auto_remove_inbox", time_limit=45, soft_time_limit=30)
+@instrumented_task(name="sentry.tasks.auto_remove_inbox", time_limit=120, soft_time_limit=110)
 def auto_remove_inbox():
     BulkDeleteQuery(model=GroupInbox, days=7, dtfield="date_added").execute()
