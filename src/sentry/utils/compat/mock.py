7f0442af6553488fc53e05335504f0628056509a
Joshua Li <joshuarli98@gmail.com>
2021-01-22 12:00:52 -0800

    remove remaining requirements-based py2 branching

--- src/sentry/utils/compat/mock.py
+++ src/sentry/utils/compat/mock.py
@@ -1,7 +1,1 @@
-from __future__ import absolute_import
-
-try:
-    # TODO: remove when we drop Python 2.7 compat
-    from mock import *  # NOQA
-except ImportError:
-    from unittest.mock import *  # NOQA
+from unittest.mock import *  # NOQA
