4e27160e7c4670814b0159e97b7a2d5aed949f2f
Mark Story <mark@mark-story.com>
2021-01-26 11:59:15 -0500

    Add a js module back so getsentry can be transitioned.

---
+++ src/sentry/static/sentry/app/utils/silence-react-unsafe-warnings.js
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-console
+export const originalConsoleWarn = console.warn;
+
+const REACT_UNSAFE_WARNING_REGEX = /componentWill.* has been renamed, and is not recommended for use.*/;
+
+window.console.warn = (message, ...args) => {
+  if (typeof message === 'string' && REACT_UNSAFE_WARNING_REGEX.test(message)) {
+    return;
+  }
+
+  originalConsoleWarn(message, ...args);
+};
+
+export const silencedWarn = window.console.warn;
