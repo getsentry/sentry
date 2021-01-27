bfda30f95ce8518cca9a63514df294b69465bc12
Dora Chan <dora.lchan@gmail.com>
2021-01-27 09:59:06 -0800

    revert to 100px

--- src/sentry/static/sentry/app/components/scoreCard.tsx
+++ src/sentry/static/sentry/app/components/scoreCard.tsx
@@ -46,7 +46,7 @@ function getTrendColor(p: TrendProps & {theme: Theme}) {
 
 const StyledPanel = styled(Panel)`
   padding: ${space(2)} ${space(3)};
-  min-height: 105px;
+  min-height: 100px;
 `;
 
 const HeaderTitle = styled('div')`
