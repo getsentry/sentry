73058c747809db53c73f595348a31f30c21f7dc4
Stephen Cefali <scefali@sentry.io>
2021-01-28 09:25:29 -0800

    minor changes for aws lambda integration

--- src/sentry/integrations/aws_lambda/integration.py
+++ src/sentry/integrations/aws_lambda/integration.py
@@ -37,7 +37,7 @@ from .utils import (
 logger = logging.getLogger("sentry.integrations.aws_lambda")
 
 DESCRIPTION = """
-The AWS Lambda integration will automatically instrument your Lambda functions without any code changes. All you need to do is run a CloudFormation stack that we provide to get started. Note, currently only Node runtimes are supported.
+The AWS Lambda integration will automatically instrument your Lambda functions without any code changes. We use CloudFormation Stack ([Learn more about CloudFormation](https://aws.amazon.com/cloudformation/)) to create Sentry role and enable errors and transactions capture from your Lambda functions
 """
 
 
