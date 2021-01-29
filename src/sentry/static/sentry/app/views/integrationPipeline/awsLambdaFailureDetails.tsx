5762f370dba66c0c7c32b9e4840e575eed88fee8
Stephen Cefali <scefali@sentry.io>
2021-01-29 09:10:21 -0800

    fix doc link for aws-lambda (#23467)

--- src/sentry/static/sentry/app/views/integrationPipeline/awsLambdaFailureDetails.tsx
+++ src/sentry/static/sentry/app/views/integrationPipeline/awsLambdaFailureDetails.tsx
@@ -20,7 +20,7 @@ export default function AwsLambdaFailureDetails({
   lambdaFunctionFailures,
   successCount,
 }: Props) {
-  const baseDocsUrl = 'https://docs.sentry.io/product/integrations/aws_lambda/';
+  const baseDocsUrl = 'https://docs.sentry.io/product/integrations/aws-lambda/';
   return (
     <React.Fragment>
       <HeaderWithHelp docsUrl={baseDocsUrl} />
