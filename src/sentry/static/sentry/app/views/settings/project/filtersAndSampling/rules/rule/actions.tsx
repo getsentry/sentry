7ecd22c77589a236a2b8ce61769baa45b717be4c
Priscila Oliveira <priscilawebdev@gmail.com>
2021-01-29 16:57:50 +0100

    ref(dynamic-sampling): Add actions in the dropdown items (#23459)

--- src/sentry/static/sentry/app/views/settings/project/filtersAndSampling/rules/rule/actions.tsx
+++ src/sentry/static/sentry/app/views/settings/project/filtersAndSampling/rules/rule/actions.tsx
@@ -4,9 +4,21 @@ import styled from '@emotion/styled';
 import MenuItemActionLink from 'app/components/actions/menuItemActionLink';
 import Button from 'app/components/button';
 import ButtonBar from 'app/components/buttonBar';
+import Confirm from 'app/components/confirm';
 import DropdownLink from 'app/components/dropdownLink';
+import Tooltip from 'app/components/tooltip';
 import {IconDelete, IconDownload, IconEdit, IconEllipsis} from 'app/icons';
 import {t} from 'app/locale';
+
+const deleteRuleConfirmMessage = t(
+  'Are you sure you wish to delete this dynamic sampling rule?'
+);
+
+const deleteRuleMessage = t(
+  'You do not have permission to delete dynamic sampling rules.'
+);
+
+const editRuleMessage = t('You do not have permission to edit dynamic sampling rules.');
 
 type Props = {
   disabled: boolean;
@@ -24,14 +36,21 @@ function Actions({disabled, onEditRule, onDeleteRule}: Props) {
           onClick={onEditRule}
           icon={<IconEdit />}
           disabled={disabled}
+          title={disabled ? editRuleMessage : undefined}
         />
-        <Button
-          label={t('Delete Rule')}
-          size="small"
-          onClick={onDeleteRule}
-          icon={<IconDelete />}
+        <Confirm
+          priority="danger"
+          message={deleteRuleConfirmMessage}
+          onConfirm={onDeleteRule}
           disabled={disabled}
-        />
+        >
+          <Button
+            label={t('Delete Rule')}
+            size="small"
+            icon={<IconDelete />}
+            title={disabled ? deleteRuleMessage : undefined}
+          />
+        </Confirm>
       </StyledButtonbar>
       <StyledDropdownLink
         caret={false}
@@ -44,15 +63,39 @@ function Actions({disabled, onEditRule, onDeleteRule}: Props) {
           shouldConfirm={false}
           icon={<IconDownload size="xs" />}
           title={t('Edit')}
+          onClick={
+            !disabled
+              ? onEditRule
+              : event => {
+                  event?.stopPropagation();
+                }
+          }
+          disabled={disabled}
         >
-          {t('Edit')}
+          <Tooltip
+            disabled={!disabled}
+            title={editRuleMessage}
+            containerDisplayMode="block"
+          >
+            {t('Edit')}
+          </Tooltip>
         </MenuItemActionLink>
         <MenuItemActionLink
-          shouldConfirm={false}
+          onAction={onDeleteRule}
+          message={deleteRuleConfirmMessage}
           icon={<IconDownload size="xs" />}
           title={t('Delete')}
+          disabled={disabled}
+          priority="danger"
+          shouldConfirm
         >
-          {t('Delete')}
+          <Tooltip
+            disabled={!disabled}
+            title={deleteRuleMessage}
+            containerDisplayMode="block"
+          >
+            {t('Delete')}
+          </Tooltip>
         </MenuItemActionLink>
       </StyledDropdownLink>
     </React.Fragment>
@@ -65,7 +108,6 @@ const StyledButtonbar = styled(ButtonBar)`
   justify-content: flex-end;
   flex: 1;
   display: none;
-
   @media (min-width: ${p => p.theme.breakpoints[2]}) {
     display: flex;
   }
@@ -75,7 +117,6 @@ const StyledDropdownLink = styled(DropdownLink)`
   display: flex;
   align-items: center;
   transition: none;
-
   @media (min-width: ${p => p.theme.breakpoints[2]}) {
     display: none;
   }
