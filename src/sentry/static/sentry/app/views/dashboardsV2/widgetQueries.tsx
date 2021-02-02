0a70ca98580368c93d75a205e520c55d3e1c4495
Mark Story <mark@mark-story.com>
2021-02-02 18:07:54 -0500

    ref(dashboards) Rearrange a few dashboard components
    Split a big module up and extract methods so that `fetchData` doesn't
    have two levels of abstraction.

--- src/sentry/static/sentry/app/views/dashboardsV2/widgetQueries.tsx
+++ src/sentry/static/sentry/app/views/dashboardsV2/widgetQueries.tsx
@@ -140,6 +140,7 @@ class WidgetQueries extends React.Component<Props, State> {
     // Table, world map, and stat widgets use table results and need
     // to do a discover 'table' query instead of a 'timeseries' query.
     this.setState({tableResults: []});
+
     const promises = widget.queries.map(query => {
       const eventView = EventView.fromSavedQuery({
         id: undefined,
@@ -170,7 +171,9 @@ class WidgetQueries extends React.Component<Props, State> {
         delete params.per_page;
         params.referrer = 'api.dashboards.worldmapwidget';
       } else {
-        throw Error('Expected widget displayType to be either table or world_map');
+        throw Error(
+          'Expected widget displayType to be either big_number, table or world_map'
+        );
       }
 
       return doDiscoverQuery<TableData>(api, url, {
@@ -205,62 +208,66 @@ class WidgetQueries extends React.Component<Props, State> {
     });
   }
 
-  async fetchData() {
+  fetchTimeseriesData() {
     const {selection, api, organization, widget} = this.props;
+    this.setState({timeseriesResults: []});
+
+    const {environments, projects} = selection;
+    const {start, end, period: statsPeriod} = selection.datetime;
+    const interval = getWidgetInterval(widget.interval, {
+      start,
+      end,
+      period: statsPeriod,
+    });
+    const promises = widget.queries.map(query => {
+      const requestData = {
+        organization,
+        interval,
+        start,
+        end,
+        project: projects,
+        environment: environments,
+        period: statsPeriod,
+        query: query.conditions,
+        yAxis: query.fields,
+        orderby: query.orderby,
+        includePrevious: false,
+        referrer: 'api.dashboards.timeserieswidget',
+      };
+      return doEventsRequest(api, requestData);
+    });
+
+    let completed = 0;
+    promises.forEach(async (promise, i) => {
+      try {
+        const rawResults = await promise;
+        completed++;
+        this.setState(prevState => {
+          const timeseriesResults = prevState.timeseriesResults?.concat(
+            transformResult(widget.queries[i], rawResults)
+          );
+          return {
+            ...prevState,
+            timeseriesResults,
+            loading: completed === promises.length ? false : true,
+          };
+        });
+      } catch (err) {
+        const errorMessage = err?.responseJSON?.detail || t('An unknown error occurred.');
+        this.setState({errorMessage});
+      }
+    });
+  }
+
+  fetchData() {
+    const {widget} = this.props;
 
     this.setState({loading: true, errorMessage: undefined});
 
     if (['table', 'world_map', 'big_number'].includes(widget.displayType)) {
       this.fetchEventData();
     } else {
-      this.setState({timeseriesResults: []});
-
-      const {environments, projects} = selection;
-      const {start, end, period: statsPeriod} = selection.datetime;
-      const interval = getWidgetInterval(widget.interval, {
-        start,
-        end,
-        period: statsPeriod,
-      });
-      const promises = widget.queries.map(query => {
-        const requestData = {
-          organization,
-          interval,
-          start,
-          end,
-          project: projects,
-          environment: environments,
-          period: statsPeriod,
-          query: query.conditions,
-          yAxis: query.fields,
-          orderby: query.orderby,
-          includePrevious: false,
-          referrer: 'api.dashboards.timeserieswidget',
-        };
-        return doEventsRequest(api, requestData);
-      });
-
-      let completed = 0;
-      promises.forEach(async (promise, i) => {
-        try {
-          const rawResults = await promise;
-          completed++;
-          this.setState(prevState => {
-            const timeseriesResults = prevState.timeseriesResults?.concat(
-              transformResult(widget.queries[i], rawResults)
-            );
-            return {
-              ...prevState,
-              timeseriesResults,
-              loading: completed === promises.length ? false : true,
-            };
-          });
-        } catch (err) {
-          const errorMessage =
-            err?.responseJSON?.detail || t('An unknown error occurred.');
-          this.setState({errorMessage});
-        }
-      });
+      this.fetchTimeseriesData();
     }
   }
 
