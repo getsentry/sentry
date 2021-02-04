ea21e36968b67cf0883d3437a1c0912d2aaadc31
Mark Story <mark@mark-story.com>
2021-02-04 14:34:41 -0500

    Remove unused import

--- src/sentry/static/sentry/app/components/roleSelectControl.tsx
+++ src/sentry/static/sentry/app/components/roleSelectControl.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactSelect, {components, OptionProps} from 'react-select';
+import {components, OptionProps} from 'react-select';
 import styled from '@emotion/styled';
 
 import SelectControl from 'app/components/forms/selectControl';
