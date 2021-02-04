4bdd11bf5fc888fc2a2ad786af93e6841b2f4a57
William Mak <william@wmak.io>
2021-02-04 09:53:39 -0500

    chore(discover): Adding num_projects tag and grouped tags (#23579)
    - Grouped tags should help group these periods better in discover

--- src/sentry/static/sentry/app/views/performance/utils.tsx
+++ src/sentry/static/sentry/app/views/performance/utils.tsx
@@ -57,9 +57,16 @@ export function addRoutePerformanceContext(selection: GlobalSelection) {
     selection.datetime.start,
     selection.datetime.end
   );
-  const seconds = Math.floor(days * 86400);
+  const oneDay = 86400;
+  const seconds = Math.floor(days * oneDay);
 
   transaction?.setTag('query.period', seconds.toString());
+  let groupedPeriod = '>30d';
+  if (seconds <= oneDay) groupedPeriod = '<=1d';
+  else if (seconds <= oneDay * 7) groupedPeriod = '<=7d';
+  else if (seconds <= oneDay * 14) groupedPeriod = '<=14d';
+  else if (seconds <= oneDay * 30) groupedPeriod = '<=30d';
+  transaction?.setTag('query.period.grouped', groupedPeriod);
 }
 
 export function getTransactionName(location: Location): string | undefined {
