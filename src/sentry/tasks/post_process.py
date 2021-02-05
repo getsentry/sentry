329ce2b8757061f357a183845633dd809137f1c1
William Mak <william@wmak.io>
2021-02-05 11:22:32 -0500

    chore: Temporarily remove the rule_processor_apply transaction (#23657)
    - This is one of our busiest transactions, I want to see if removing
      this results in other transaction TPM increasing to debug an issue
      with backend throughput
    - Will be adding this back later

--- src/sentry/tasks/post_process.py
+++ src/sentry/tasks/post_process.py
@@ -1,5 +1,4 @@
 import logging
-import sentry_sdk
 
 from sentry import features
 from sentry.app import locks
@@ -263,10 +262,7 @@ def post_process_group(
             # objects back and forth isn't super efficient
             for callback, futures in rp.apply():
                 has_alert = True
-                with sentry_sdk.start_transaction(
-                    op="post_process_group", name="rule_processor_apply", sampled=True
-                ):
-                    safe_execute(callback, event, futures, _with_transaction=False)
+                safe_execute(callback, event, futures, _with_transaction=False)
 
             try:
                 lock = locks.get(
