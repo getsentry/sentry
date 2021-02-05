c3bdec57d3202a9a944c15e1c518a2a7543d2a3c
Mark Story <mark@mark-story.com>
2021-02-05 10:22:54 -0500

    Fix padding in multi value selects

--- src/sentry/static/sentry/app/components/forms/selectControl.jsx
+++ src/sentry/static/sentry/app/components/forms/selectControl.jsx
@@ -160,6 +160,7 @@ const SelectControl = props => {
       border: '1px solid #c2e0ff',
       display: 'flex',
       marginRight: '4px',
+      marginTop: '2px',
     }),
     multiValueLabel: provided => ({
       ...provided,
