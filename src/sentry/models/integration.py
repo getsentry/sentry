f0ad07c527ab89700f233165c80499ee2e9281f9
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2021-02-05 16:25:41 -0800

    ref(migration): Make repositoryprojectpathconfig organization_integration foreignkey nullable (#23673)
    Objective:
    We need to create a migration to make organization_integration_id nullable. This allows Code Path Mappings to be an integration-agnostic feature.

--- src/sentry/models/integration.py
+++ src/sentry/models/integration.py
@@ -51,7 +51,7 @@ class RepositoryProjectPathConfig(DefaultFieldsModel):
 
     repository = FlexibleForeignKey("sentry.Repository")
     project = FlexibleForeignKey("sentry.Project", db_constraint=False)
-    organization_integration = FlexibleForeignKey("sentry.OrganizationIntegration")
+    organization_integration = FlexibleForeignKey("sentry.OrganizationIntegration", null=True)
     stack_root = models.TextField()
     source_root = models.TextField()
     default_branch = models.TextField(null=True)
