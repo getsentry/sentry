66e54f3940ff83fa1f6d8b4116ef15c61e2faf61
Armen <armenzg@sentry.io>
2021-02-08 13:25:56 -0500

    Add old version back

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,6 +4,8 @@ set -eu
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd "$SCRIPT_DIR/.."
 
+# shellcheck disable=SC2034
+OLD_VERSION="$1"
 NEW_VERSION="$2"
 
 sed -i -e "s/^VERSION = "'".*"'"\$/VERSION = "'"'"$NEW_VERSION"'"'"/" setup.py
