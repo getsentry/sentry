29b2c6040978aa6c97f217915118a1ffdd05773c
Armen <armenzg@sentry.io>
2021-02-08 10:23:53 -0500

    build(pre-commit): Only run ShellCheck checks on files under scripts
    I also fixed the warnings that did not require testing.

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -2,9 +2,8 @@
 set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $SCRIPT_DIR/..
+cd "$SCRIPT_DIR/.."
 
-OLD_VERSION="$1"
 NEW_VERSION="$2"
 
 sed -i -e "s/^VERSION = "'".*"'"\$/VERSION = "'"'"$NEW_VERSION"'"'"/" setup.py
