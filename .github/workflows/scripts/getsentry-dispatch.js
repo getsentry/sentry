b99005a747cb450b3705794079555a2e4c94ca66
Billy Vong <billy@sentry.io>
2021-02-08 13:01:48 -0800

    small refactors

--- .github/workflows/scripts/getsentry-dispatch.js
+++ .github/workflows/scripts/getsentry-dispatch.js
@@ -1,18 +1,26 @@
 /* eslint-env node */
 
+/**
+ * List of workflows to dispatch to `getsentry`
+ *
+ * `pathFilterName` refers to the path filters in `.github/file-filters.yml` (`getsentry/paths-filter` action)
+ *
+ * TODO(billy): Refactor workflow files to be an enum so that we can integration test them. Otherwise if they are
+ *              deleted/renamed in `getsentry`, this will fail
+ */
+const DISPATCHES = [
+  {
+    workflow: 'js-build-and-lint.yml',
+    pathFilterName: 'frontend',
+  },
+  {
+    workflow: 'backend-test.yml',
+    pathFilterName: 'backend_dependencies',
+  },
+];
+
 module.exports = {
   dispatch: async ({github, context, fileChanges}) => {
-    const DISPATCHES = [
-      {
-        workflow: 'js-build-and-lint.yml',
-        pathFilterName: 'frontend',
-      },
-      {
-        workflow: 'backend-test.yml',
-        pathFilterName: 'backend_dependencies',
-      },
-    ];
-
     const shouldSkip = {
       frontend: fileChanges.frontend !== 'true',
       backend_dependencies: fileChanges.backend_dependencies !== 'true',
