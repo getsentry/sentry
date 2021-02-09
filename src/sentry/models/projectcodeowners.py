49e0f2a4bfe9b67187c90a92f7a4fa3a4c0c4f1f
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2021-02-09 11:03:13 -0800

    remove db constraint on Project table

--- src/sentry/models/projectcodeowners.py
+++ src/sentry/models/projectcodeowners.py
@@ -12,8 +12,8 @@ logger = logging.getLogger(__name__)
 
 class ProjectCodeOwners(DefaultFieldsModel):
     __core__ = False
-
-    project = FlexibleForeignKey("sentry.Project")
+    # no db constraint to prevent locks on the Project table
+    project = FlexibleForeignKey("sentry.Project", db_constraint=False)
     # projectcodeowners should work without a repo-based integration.
     organization_integration = FlexibleForeignKey("sentry.OrganizationIntegration", null=True)
     # repository_project_path_config ⇒ use this to transform CODEOWNERS paths to stacktrace paths
