336be6a3ef8bfe9cd52ded8c1e7af8f321d228dd
Stephen Cefali <scefali@sentry.io>
2021-02-09 14:48:18 -0800

    update stuff

--- src/sentry/static/sentry/app/utils/advancedAnalytics.tsx
+++ src/sentry/static/sentry/app/utils/advancedAnalytics.tsx
@@ -26,18 +26,18 @@ export const getAnalyticsSessionId = () =>
 const hasAnalyticsDebug = () => window.localStorage.getItem('DEBUG_ANALYTICS') === '1';
 
 //TODO: add more types/sources
-export type EventParameters = IntegrationEventParameters;
+type EventParameters = IntegrationEventParameters;
 
-export const allEventMap = {...integrationEventMap};
+const allEventMap = {...integrationEventMap};
 
 type AnalyticsKey = keyof EventParameters;
 
 /**
- * Tracks an event for analytics
- * Must be tied to an organization
- * Uses the current session ID or generates a new one if startSession == true
- * An analytics session corresponds to a single action funnel such as installation
- * Tracking by session allows us to track individual funnel attempts for a single user
+ * Tracks an event for analytics.
+ * Must be tied to an organization.
+ * Uses the current session ID or generates a new one if startSession == true.
+ * An analytics session corresponds to a single action funnel such as installation.
+ * Tracking by session allows us to track individual funnel attempts for a single user.
  */
 export function trackAdvancedAnalyticsEvent<T extends AnalyticsKey>(
   eventKey: T,
