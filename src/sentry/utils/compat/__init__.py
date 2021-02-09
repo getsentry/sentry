27a0416d47c7d9806f3afac5f78268078847b022
Joshua Li <joshuarli98@gmail.com>
2021-02-09 12:41:59 -0800

    fix infinite recursion, haha

--- src/sentry/utils/compat/__init__.py
+++ src/sentry/utils/compat/__init__.py
@@ -1,16 +1,21 @@
+_builtin_map = map
+_builtin_filter = filter
+_builtin_zip = zip
+
+
 def map(a, b, *c):
     # TODO(joshuarli): Remove all this.
-    return list(map(a, b, *c))
+    return list(_builtin_map(a, b, *c))
 
 
 def filter(a, b):
     # TODO(joshuarli): Remove all this.
-    return list(filter(a, b))
+    return list(_builtin_filter(a, b))
 
 
 def zip(*a):
     # TODO(joshuarli): Remove all this.
-    return list(zip(*a))
+    return list(_builtin_zip(*a))
 
 
 from binascii import crc32 as _crc32
