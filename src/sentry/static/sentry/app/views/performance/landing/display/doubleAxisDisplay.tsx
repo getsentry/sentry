e3815a616f441ee4da0213a8e09f10fb6df0099e
Dora <dora.l.chan@gmail.com>
2021-02-10 11:28:00 -0800

    removing extra white bg (#23774)
    Co-authored-by: Dora Chan <dora.lchan@gmail.com>

--- src/sentry/static/sentry/app/views/performance/landing/display/doubleAxisDisplay.tsx
+++ src/sentry/static/sentry/app/views/performance/landing/display/doubleAxisDisplay.tsx
@@ -88,7 +88,6 @@ const DoubleChartContainer = styled('div')`
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-gap: ${space(3)};
-  background-color: ${p => p.theme.background};
   min-height: 282px;
 `;
 
