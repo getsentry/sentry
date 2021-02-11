7c1ed932aedc501757e7396d2b3dd14b3556e628
Colleen O'Rourke <colleen@sentry.io>
2021-02-10 17:38:19 -0800

    Add tests

---
+++ tests/apidocs/endpoints/organizations/test-sentry-app-external-issues.py
@@ -0,0 +1,36 @@
+from django.core.urlresolvers import reverse
+from django.test.client import RequestFactory
+
+from tests.apidocs.util import APIDocsTestCase
+
+from sentry.models import SentryAppInstallation
+
+
+class OrganizationDetailsDocs(APIDocsTestCase):
+    def setUp(self):
+        self.org = self.create_organization(owner=self.user, name="Rowdy Tiger")
+        self.project = self.create_project(organization=self.org)
+        self.group = self.create_group(project=self.project)
+        self.sentry_app = self.create_sentry_app(
+            name="Hellboy App", published=True, organization=self.org
+        )
+        self.install = SentryAppInstallation(sentry_app=self.sentry_app, organization=self.org)
+        self.install.save()
+        self.url = reverse(
+            "sentry-api-0-sentry-app-installation-external-issues",
+            kwargs={"uuid": self.install.uuid},
+        )
+
+        self.login_as(user=self.user)
+
+    def test_post(self):
+        data = {
+            "groupId": self.group.id,
+            "webUrl": "https://somerandom.io/project/issue-id",
+            "project": "ExternalProj",
+            "identifier": "issue-1",
+        }
+        response = self.client.post(self.url, data)
+        request = RequestFactory().post(self.url, data)
+
+        self.validate_schema(request, response)
