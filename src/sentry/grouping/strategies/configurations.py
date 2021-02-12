fc8054ab9475e85f53189674df18e47c97ef9427
Armin Ronacher <armin.ronacher@active-4.com>
2021-02-12 22:00:45 +0100

    feat(grouping): Small grouping code refactor

--- src/sentry/grouping/strategies/configurations.py
+++ src/sentry/grouping/strategies/configurations.py
@@ -5,19 +5,63 @@ from sentry.grouping.strategies.base import (
 )
 
 
-# The classes of grouping algorithms
-CLASSES = []
-
 # The full mapping of all known configurations.
 CONFIGURATIONS = {}
+
+# The implied base strategy *every* strategy inherits from if no
+# base is defined.
+BASE_STRATEGY = create_strategy_configuration(
+    None,
+    strategies=[
+        "expect-ct:v1",
+        "expect-staple:v1",
+        "hpkp:v1",
+        "csp:v1",
+        "threads:v1",
+        "stacktrace:v1",
+        "chained-exception:v1",
+        "template:v1",
+        "message:v1",
+    ],
+    delegates=["frame:v1", "stacktrace:v1", "single-exception:v1"],
+    initial_context={
+        # This key in the context tells the system which variant should
+        # be produced.  TODO: phase this out.
+        "variant": None,
+        # This is a flag that can be used by any delegate to respond to
+        # a detected recursion.  This is currently used by the frame
+        # strategy to disable itself.  Recursion is detected by the outer
+        # strategy.
+        "is_recursion": False,
+        # This turns on the automatic message trimming by the message
+        # strategy.
+        "trim_message": False,
+        # newstyle: enables the legacy function logic.  This is only used
+        # by the newstyle:2019-04-05 strategy.  Once this is no longer used
+        # this can go away entirely.
+        "legacy_function_logic": False,
+        # newstyle: turns on some javascript fuzzing features.
+        "javascript_fuzzing": False,
+        # newstyle: platforms for which context line should be taken into
+        # account when grouping.
+        "contextline_platforms": (),
+        # newstyle: this detects anonymous classes in PHP code.
+        "php_detect_anonymous_classes": False,
+        # newstyle: turns on a bug that was present in some variants
+        "with_context_line_file_origin_bug": False,
+        # newstyle: turns on falling back to exception values when there
+        # is no stacktrace.
+        "with_exception_value_fallback": False,
+    },
+)
 
 
 def register_strategy_config(id, **kwargs):
     if kwargs.get("base") is not None:
         kwargs["base"] = CONFIGURATIONS[kwargs["base"]]
+    else:
+        kwargs["base"] = BASE_STRATEGY
     rv = create_strategy_configuration(id, **kwargs)
-    if rv.config_class not in CLASSES:
-        CLASSES.append(rv.config_class)
     CONFIGURATIONS[rv.id] = rv
     return rv
 
@@ -30,15 +74,9 @@ def register_strategy_config(id, **kwargs):
 register_strategy_config(
     id="legacy:2019-03-12",
     strategies=[
-        "expect-ct:v1",
-        "expect-staple:v1",
-        "hpkp:v1",
-        "csp:v1",
         "threads:legacy",
         "stacktrace:legacy",
         "chained-exception:legacy",
-        "template:v1",
-        "message:v1",
     ],
     delegates=["frame:legacy", "stacktrace:legacy", "single-exception:legacy"],
     changelog="""
@@ -58,19 +96,7 @@ register_strategy_config(
 
 register_strategy_config(
     id="newstyle:2019-05-08",
-    strategies=[
-        "expect-ct:v1",
-        "expect-staple:v1",
-        "hpkp:v1",
-        "csp:v1",
-        "threads:v1",
-        "stacktrace:v1",
-        "chained-exception:v1",
-        "template:v1",
-        "message:v1",
-    ],
     risk=RISK_LEVEL_HIGH,
-    delegates=["frame:v1", "stacktrace:v1", "single-exception:v1"],
     changelog="""
         * Uses source code information all platforms with reliable sources
           for grouping (JavaScript, Python, PHP and Ruby) and function
@@ -84,10 +110,8 @@ register_strategy_config(
         * C/C++ and other native stacktraces are more reliably grouped.
     """,
     initial_context={
-        "legacy_function_logic": False,
         "javascript_fuzzing": True,
         "contextline_platforms": ("javascript", "node", "python", "php", "ruby"),
-        "php_detect_anonymous_classes": False,
         "with_context_line_file_origin_bug": True,
         "trim_message": True,
         "with_exception_value_fallback": True,
@@ -97,7 +121,6 @@ register_strategy_config(
 register_strategy_config(
     id="newstyle:2019-10-29",
     base="newstyle:2019-05-08",
-    delegates=["frame:v1"],
     risk=RISK_LEVEL_MEDIUM,
     changelog="""
         * Better rules for when to take context lines into account for
@@ -118,18 +141,6 @@ register_strategy_config(
 
 register_strategy_config(
     id="newstyle:2019-04-05",
-    strategies=[
-        "expect-ct:v1",
-        "expect-staple:v1",
-        "hpkp:v1",
-        "csp:v1",
-        "threads:v1",
-        "stacktrace:v1",
-        "chained-exception:v1",
-        "template:v1",
-        "message:v1",
-    ],
-    delegates=["frame:v1", "stacktrace:v1", "single-exception:v1"],
     risk=RISK_LEVEL_HIGH,
     changelog="""
         * Experimental grouping algorithm (should not be used)
@@ -137,20 +148,12 @@ register_strategy_config(
     hidden=True,
     initial_context={
         "legacy_function_logic": True,
-        "javascript_fuzzing": False,
-        "contextline_platforms": (),
-        "php_detect_anonymous_classes": False,
-        "with_context_line_file_origin_bug": False,
-        "trim_message": False,
-        "with_exception_value_fallback": False,
     },
 )
 
 register_strategy_config(
     id="newstyle:2019-04-17",
     base="newstyle:2019-04-05",
-    strategies=["message:v1"],
-    delegates=["frame:v1", "single-exception:v1"],
     risk=RISK_LEVEL_HIGH,
     changelog="""
         * Experimental grouping algorithm (should not be used)
