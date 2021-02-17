b23628dad13aa944d2516970ea58f49b1809f5c7
Stephen Cefali <scefali@sentry.io>
2021-02-17 09:06:09 -0800

    adds tests

--- src/sentry/static/sentry/app/utils/promptIsDismissed.tsx
+++ src/sentry/static/sentry/app/utils/promptIsDismissed.tsx
@@ -2,11 +2,8 @@ import {PromptData} from 'app/actionCreators/prompts';
 
 import {snoozedDays} from './promptsActivity';
 
-export const promptIsDismissed = (prompt: PromptData, daysToSnooze?: number) => {
+export const promptIsDismissed = (prompt: PromptData, daysToSnooze: number = 14) => {
   const {snoozedTime, dismissedTime} = prompt || {};
-  if (typeof daysToSnooze === 'undefined') {
-    daysToSnooze = 14;
-  }
   // check if the prompt has been dismissed
   if (dismissedTime) {
     return true;
