1a41b661e6f8ea755aaba4899fed16a836db99ef
Alberto Leal <mail4alberto@gmail.com>
2021-02-17 12:36:29 -0500

    update scrollbar colours to support dark mode

--- src/sentry/static/sentry/app/components/events/interfaces/spans/header.tsx
+++ src/sentry/static/sentry/app/components/events/interfaces/spans/header.tsx
@@ -871,7 +871,8 @@ const ScrollBarContainer = styled('div')`
   left: 0;
   bottom: 0;
   & > div[data-type='virtual-scrollbar'].dragging > div {
-    background-color: ${p => p.theme.gray500};
+    background-color: ${p => p.theme.textColor};
+    opacity: 0.8;
     cursor: grabbing;
   }
 `;
@@ -905,7 +906,8 @@ const VirtualScrollBarGrip = styled('div')`
   width: 100%;
   border-radius: 20px;
   transition: background-color 150ms ease;
-  background-color: rgba(48, 40, 57, 0.5);
+  background-color: ${p => p.theme.textColor};
+  opacity: 0.5;
 `;
 
 export default TraceViewHeader;
