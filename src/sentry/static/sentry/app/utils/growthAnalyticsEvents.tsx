093b9bd37261739910ef6d5fd587dadf697ba88d
Stephen Cefali <scefali@sentry.io>
2021-02-17 12:31:34 -0800

    feat(mobile-prompt): banner and modal (#23895)

--- src/sentry/static/sentry/app/utils/growthAnalyticsEvents.tsx
+++ src/sentry/static/sentry/app/utils/growthAnalyticsEvents.tsx
@@ -1,4 +1,8 @@
 type MobilePromptBannerParams = {
+  matchedUserAgentString: string;
+};
+
+type CheckShowParams = MobilePromptBannerParams & {
   userAgentMatches: boolean;
   matchedUserAgentString: string;
   hasMobileProject: boolean;
@@ -7,11 +11,27 @@ type MobilePromptBannerParams = {
 
 //define the event key to payload mappings
 export type GrowthEventParameters = {
-  'growth.check_show_mobile_prompt_banner': MobilePromptBannerParams;
+  'growth.check_show_mobile_prompt_banner': CheckShowParams;
+  'growth.dismissed_mobile_prompt_banner': MobilePromptBannerParams;
+  'growth.opened_mobile_project_suggest_modal': MobilePromptBannerParams;
+  'growth.clicked_mobile_prompt_setup_project': MobilePromptBannerParams;
+  'growth.clicked_mobile_prompt_ask_teammate': MobilePromptBannerParams;
+  'growth.submitted_mobile_prompt_ask_teammate': MobilePromptBannerParams & {
+    email: string;
+  };
 };
 
 type GrowthAnalyticsKey = keyof GrowthEventParameters;
 
 export const growthEventMap: Record<GrowthAnalyticsKey, string> = {
   'growth.check_show_mobile_prompt_banner': 'Growth: Check Show Mobile Prompt Banner',
+  'growth.dismissed_mobile_prompt_banner': 'Growth: Dismissed Mobile Prompt Banner',
+  'growth.opened_mobile_project_suggest_modal':
+    'Growth: Open Mobile Project Suggest Modal',
+  'growth.clicked_mobile_prompt_setup_project':
+    'Growth: Clicked Mobile Prompt Setup Project',
+  'growth.clicked_mobile_prompt_ask_teammate':
+    'Growth: Clicked Mobile Prompt Ask Temmate',
+  'growth.submitted_mobile_prompt_ask_teammate':
+    'Growth: Submitted Mobile Prompt Ask Temmate',
 };
---
+++ tests/js/spec/components/suggestProjectCTA.spec.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+
+import {mountWithTheme} from 'sentry-test/enzyme';
+
+import {openModal} from 'app/actionCreators/modal';
+import SuggestProjectCTA from 'app/components/suggestProjectCTA';
+import ProjectsStore from 'app/stores/projectsStore';
+
+jest.mock('app/actionCreators/modal');
+
+function generateWrapper(inputProps) {
+  const projects = inputProps?.projects ?? [TestStubs.Project({platform: 'javascript'})];
+
+  jest.spyOn(ProjectsStore, 'getState').mockImplementation(() => ({
+    projects,
+    loading: false,
+  }));
+
+  const props = {
+    organization: TestStubs.Organization(),
+    event: TestStubs.Event({
+      entries: [{type: 'request', data: {headers: [['User-Agent', 'okhttp/123']]}}],
+    }),
+    ...inputProps,
+  };
+  return mountWithTheme(<SuggestProjectCTA {...props} />);
+}
+
+describe('SuggestProjectCTA', function () {
+  it('shows prompt and open modal', async () => {
+    MockApiClient.addMockResponse({
+      url: `/prompts-activity/`,
+      body: {},
+    });
+    const wrapper = generateWrapper();
+    await tick();
+    wrapper.update();
+    expect(wrapper.find('Alert')).toHaveLength(1);
+    wrapper.find('a').simulate('click');
+    expect(openModal).toHaveBeenCalled();
+  });
+  it('user agent does not match', async () => {
+    MockApiClient.addMockResponse({
+      url: `/prompts-activity/`,
+      body: {},
+    });
+    const wrapper = generateWrapper({
+      event: TestStubs.Event({
+        entries: [{type: 'request', data: {headers: [['User-Agent', 'sentry/123']]}}],
+      }),
+    });
+    await tick();
+    wrapper.update();
+    expect(wrapper.find('Alert')).toHaveLength(0);
+  });
+  it('has mobile project', async () => {
+    MockApiClient.addMockResponse({
+      url: `/prompts-activity/`,
+      body: {},
+    });
+    const projects = [TestStubs.Project({platform: 'android'})];
+    const wrapper = generateWrapper({
+      projects,
+    });
+    await tick();
+    wrapper.update();
+    expect(wrapper.find('Alert')).toHaveLength(0);
+  });
+  it('prompt is dismissed', async () => {
+    MockApiClient.addMockResponse({
+      url: `/prompts-activity/`,
+      body: {data: {dismissed_ts: 1234}},
+    });
+    const wrapper = generateWrapper();
+    await tick();
+    wrapper.update();
+    expect(wrapper.find('Alert')).toHaveLength(0);
+  });
+});
