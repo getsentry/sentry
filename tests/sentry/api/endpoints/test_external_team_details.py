359ce046720b62cf1239fcd1036fd37f9bc23cb5
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2021-02-17 16:19:52 -0800

    feat(external-team): PUT, POST, DELETE endpoints (#23755)
    Objective:
    We just created a migration to create the sentry_externalteam table. We will need to create createOne, deleteOne and updateOne endpoints for sentry_externalteam model.

---
+++ tests/sentry/api/endpoints/test_external_team_details.py
@@ -0,0 +1,36 @@
+from django.core.urlresolvers import reverse
+from sentry.models import ExternalTeam
+from sentry.testutils import APITestCase
+
+
+class OrganizationIntegrationRepositoryProjectPathConfigTest(APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.login_as(user=self.user)
+        self.org = self.create_organization(owner=self.user, name="baz")
+        self.team = self.create_team(organization=self.org, name="Mariachi Band")
+        self.external_team = ExternalTeam.objects.create(
+            team_id=str(self.team.id),
+            provider=ExternalTeam.get_provider_enum("github"),
+            external_id="@getsentry/ecosystem",
+        )
+        self.url = reverse(
+            "sentry-api-0-external-team-details",
+            args=[self.org.slug, self.team.slug, self.external_team.id],
+        )
+
+    def test_basic_delete(self):
+        resp = self.client.delete(self.url)
+        assert resp.status_code == 204
+        assert not ExternalTeam.objects.filter(id=str(self.external_team.id)).exists()
+
+    def test_basic_update(self):
+        resp = self.client.put(self.url, {"externalId": "@getsentry/growth"})
+        assert resp.status_code == 200
+        assert resp.data["id"] == str(self.external_team.id)
+        assert resp.data["externalId"] == "@getsentry/growth"
+
+    def test_invalid_provider_update(self):
+        resp = self.client.put(self.url, {"provider": "git"})
+        assert resp.status_code == 400
+        assert resp.data == {"provider": ['"git" is not a valid choice.']}
