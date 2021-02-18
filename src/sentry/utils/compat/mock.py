7b20bc808c70f93199f73e4e74b4f84abca38e75
Joshua Li <joshuarli98@gmail.com>
2021-02-17 23:08:01 -0800

    oh this accidentally snuck in

--- src/sentry/utils/compat/mock.py
+++ src/sentry/utils/compat/mock.py
@@ -1,4 +1,4 @@
-from unittest.mock import *  # NOQA
+from mock import *  # NOQA
 
 # We're still using mock in Python 3.6 because it contains a fix to Python issue37972.
 # We should be able to fully swap it out for stdlib once we're on 3.8.
