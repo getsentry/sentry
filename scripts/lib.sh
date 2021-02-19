c5a60633e903f6c43142047cf4aa0d7c74b69510
Armen <armenzg@sentry.io>
2021-02-19 17:01:16 -0500

    Shell scripts for version comparison

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -12,3 +12,44 @@ query_big_sur() {
     fi
     return 1
 }
+
+# Based on https://stackoverflow.com/a/4025065
+# https://creativecommons.org/licenses/by-sa/3.0/
+# Author: Dennis Williamson
+# Modifications: some double quotes, use echo instead of return
+# Return value:
+# - 0 if equal
+# - 1 if first item greater than second
+# - 2 if first item lesser than second
+vercomp () {
+    # shellcheck disable=SC2053
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    # shellcheck disable=SC2206
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} > 10#${ver2[i]}))
+        then
+            return 1
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 2
+        fi
+    done
+    return 0
+}
