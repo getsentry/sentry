3af8c7ed7f194a9d6d63e1e4c3333f9b02b3a517
MeredithAnya <meredith.a.heller@gmail.com>
2021-02-19 14:08:37 -0800

    ref(apm): Convert some transaction_start to SAMPLED_URL_NAMES (#23987)

--- src/sentry/api/endpoints/group_integration_details.py
+++ src/sentry/api/endpoints/group_integration_details.py
@@ -9,7 +9,6 @@ from sentry.integrations import IntegrationFeatures
 from sentry.shared_integrations.exceptions import IntegrationError, IntegrationFormError
 from sentry.models import Activity, ExternalIssue, GroupLink, Integration
 from sentry.signals import integration_issue_created, integration_issue_linked
-from sentry.web.decorators import transaction_start
 
 
 MISSING_FEATURE_MESSAGE = "Your organization does not have access to this feature."
@@ -42,7 +41,6 @@ class GroupIntegrationDetailsEndpoint(GroupEndpoint):
             data=issue_information,
         )
 
-    @transaction_start("GroupIntegrationDetailsEndpoint")
     def get(self, request, group, integration_id):
         if not self._has_issue_feature(group.organization, request.user):
             return Response({"detail": MISSING_FEATURE_MESSAGE}, status=400)
@@ -81,7 +79,6 @@ class GroupIntegrationDetailsEndpoint(GroupEndpoint):
             return Response({"detail": str(e)}, status=400)
 
     # was thinking put for link an existing issue, post for create new issue?
-    @transaction_start("GroupIntegrationDetailsEndpoint")
     def put(self, request, group, integration_id):
         if not self._has_issue_feature(group.organization, request.user):
             return Response({"detail": MISSING_FEATURE_MESSAGE}, status=400)
@@ -170,7 +167,6 @@ class GroupIntegrationDetailsEndpoint(GroupEndpoint):
         }
         return Response(context, status=201)
 
-    @transaction_start("GroupIntegrationDetailsEndpoint")
     def post(self, request, group, integration_id):
         if not self._has_issue_feature(group.organization, request.user):
             return Response({"detail": MISSING_FEATURE_MESSAGE}, status=400)
@@ -243,7 +239,6 @@ class GroupIntegrationDetailsEndpoint(GroupEndpoint):
         }
         return Response(context, status=201)
 
-    @transaction_start("GroupIntegrationDetailsEndpoint")
     def delete(self, request, group, integration_id):
         if not self._has_issue_feature(group.organization, request.user):
             return Response({"detail": MISSING_FEATURE_MESSAGE}, status=400)
