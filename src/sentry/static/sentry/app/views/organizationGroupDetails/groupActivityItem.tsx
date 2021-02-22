getsentry/sentry

commit 12f1cd8461b8662c2d941aa6321611c61d0de5b1

ref(ts): Convert groupActivityItem to ts (#22568)

src/sentry/static/sentry/app/views/organizationGroupDetails/groupActivityItem.tsx
@@ -1,0 +1,242 @@
+import React from 'react';
+
+import CommitLink from 'app/components/commitLink';
+import Duration from 'app/components/duration';
+import Link from 'app/components/links/link';
+import PullRequestLink from 'app/components/pullRequestLink';
+import Version from 'app/components/version';
+import {t, tct, tn} from 'app/locale';
+import MemberListStore from 'app/stores/memberListStore';
+import TeamStore from 'app/stores/teamStore';
+import {
+  GroupActivity,
+  GroupActivityAssigned,
+  GroupActivitySetIgnored,
+  GroupActivityType,
+  Organization,
+  Project,
+  User,
+} from 'app/types';
+
+type Props = {
+  author: React.ReactNode;
+  activity: GroupActivity;
+  orgSlug: Organization['slug'];
+  projectId: Project['id'];
+};
+
+function GroupActivityItem({author, activity, orgSlug, projectId}: Props) {
+  const issuesLink = `/organizations/${orgSlug}/issues/`;
+
+  function getIgnoredMessage(data: GroupActivitySetIgnored['data']) {
+    if (data.ignoreDuration) {
+      return t('%(author)s ignored this issue for %(duration)s', {
+        author,
+        duration: <Duration seconds={data.ignoreDuration * 60} />,
+      });
+    }
+
+    if (data.ignoreCount && data.ignoreWindow) {
+      return tct(
+        '[author] ignored this issue until it happens [count] time(s) in [duration]',
+        {
+          author,
+          count: data.ignoreCount,
+          duration: <Duration seconds={data.ignoreWindow * 60} />,
+        }
+      );
+    }
+
+    if (data.ignoreCount) {
+      return tct('[author] ignored this issue until it happens [count] time(s)', {
+        author,
+        count: data.ignoreCount,
+      });
+    }
+
+    if (data.ignoreUserCount && data.ignoreUserWindow) {
+      return tct(
+        '[author] ignored this issue until it affects [count] user(s) in [duration]',
+        {
+          author,
+          count: data.ignoreUserCount,
+          duration: <Duration seconds={data.ignoreUserWindow * 60} />,
+        }
+      );
+    }
+
+    if (data.ignoreUserCount) {
+      return tct('[author] ignored this issue until it affects [count] user(s)', {
+        author,
+        count: data.ignoreUserCount,
+      });
+    }
+
+    return t('%s ignored this issue', author);
+  }
+
+  function getAssignedMessage(data: GroupActivityAssigned['data']) {
+    let assignee: string | User | undefined = undefined;
+
+    if (data.assigneeType === 'team') {
+      const team = TeamStore.getById(data.assignee);
+      assignee = team ? team.slug : '<unknown-team>';
+
+      return t('%(author)s assigned this issue to #%(assignee)s', {
+        author,
+        assignee,
+      });
+    }
+
+    if (activity.user && activity.assignee === activity.user.id) {
+      return t('%s assigned this issue to themselves', author);
+    }
+
+    assignee = MemberListStore.getById(data.assignee);
+
+    if (typeof assignee === 'object' && assignee?.email) {
+      return t('%(author)s assigned this issue to %(assignee)s', {
+        author,
+        assignee: assignee.email,
+      });
+    }
+
+    return t('%s assigned this issue to an unknown user', author);
+  }
+
+  function renderContent() {
+    switch (activity.type) {
+      case GroupActivityType.NOTE:
+        return t('%s left a comment', author);
+      case GroupActivityType.SET_RESOLVED:
+        return t('%s marked this issue as resolved', author);
+      case GroupActivityType.SET_RESOLVED_BY_AGE:
+        return t('%(author)s marked this issue as resolved due to inactivity', {
+          author,
+        });
+      case GroupActivityType.SET_RESOLVED_IN_RELEASE:
+        return activity.data.version
+          ? t('%(author)s marked this issue as resolved in %(version)s', {
+              author,
+              version: (
+                <Version
+                  version={activity.data.version}
+                  projectId={projectId}
+                  tooltipRawVersion
+                />
+              ),
+            })
+          : t('%s marked this issue as resolved in the upcoming release', author);
+      case GroupActivityType.SET_RESOLVED_IN_COMMIT:
+        return t('%(author)s marked this issue as resolved in %(version)s', {
+          author,
+          version: (
+            <CommitLink
+              inline
+              commitId={activity.data.commit.id}
+              repository={activity.data.commit.repository}
+            />
+          ),
+        });
+      case GroupActivityType.SET_RESOLVED_IN_PULL_REQUEST: {
+        const {data} = activity;
+        const {pullRequest} = data;
+        return t('%(author)s marked this issue as resolved in %(version)s', {
+          author,
+          version: (
+            <PullRequestLink
+              inline
+              pullRequest={pullRequest}
+              repository={pullRequest.repository}
+            />
+          ),
+        });
+      }
+      case GroupActivityType.SET_UNRESOLVED:
+        return t('%s marked this issue as unresolved', author);
+      case GroupActivityType.SET_IGNORED: {
+        const {data} = activity;
+        return getIgnoredMessage(data);
+      }
+      case GroupActivityType.SET_PUBLIC:
+        return t('%s made this issue public', author);
+      case GroupActivityType.SET_PRIVATE:
+        return t('%s made this issue private', author);
+      case GroupActivityType.SET_REGRESSION: {
+        const {data} = activity;
+        return data.version
+          ? t('%(author)s marked this issue as a regression in %(version)s', {
+              author,
+              version: (
+                <Version version={data.version} projectId={projectId} tooltipRawVersion />
+              ),
+            })
+          : t('%s marked this issue as a regression', author);
+      }
+      case GroupActivityType.CREATE_ISSUE: {
+        const {data} = activity;
+        return t('%(author)s created an issue on %(provider)s titled %(title)s', {
+          author,
+          provider: data.provider,
+          title: <Link to={data.location}>{data.title}</Link>,
+        });
+      }
+      case GroupActivityType.UNMERGE_SOURCE: {
+        const {data} = activity;
+        const {destination, fingerprints} = data;
+        return tn(
+          '%2$s migrated %1$s fingerprint to %3$s',
+          '%2$s migrated %1$s fingerprints to %3$s',
+          fingerprints.length,
+          author,
+          destination ? (
+            <Link to={`${issuesLink}${destination.id}`}>{destination.shortId}</Link>
+          ) : (
+            t('a group')
+          )
+        );
+      }
+      case GroupActivityType.UNMERGE_DESTINATION: {
+        const {data} = activity;
+        const {source, fingerprints} = data;
+        return tn(
+          '%2$s migrated %1$s fingerprint from %3$s',
+          '%2$s migrated %1$s fingerprints from %3$s',
+          fingerprints.length,
+          author,
+          source ? (
+            <Link to={`${issuesLink}${source.id}`}>{source.shortId}</Link>
+          ) : (
+            t('a group')
+          )
+        );
+      }
+      case GroupActivityType.FIRST_SEEN:
+        return t('%s first saw this issue', author);
+      case GroupActivityType.ASSIGNED: {
+        const {data} = activity;
+        return getAssignedMessage(data);
+      }
+      case GroupActivityType.UNASSIGNED:
+        return t('%s unassigned this issue', author);
+      case GroupActivityType.MERGE:
+        return tn(
+          '%2$s merged %1$s issue into this issue',
+          '%2$s merged %1$s issues into this issue',
+          activity.data.issues.length,
+          author
+        );
+      case GroupActivityType.REPROCESS:
+        return t(
+          '%(author)s reprocessed this issue, some events may have moved into new issues',
+          {author}
+        );
+      default:
+        return ''; // should never hit (?)
+    }
+  }
+
+  return <React.Fragment>{renderContent()}</React.Fragment>;
+}
+
+export default GroupActivityItem;

src/sentry/static/sentry/app/types/index.tsx
@@ -835,20 +835,196 @@ type GroupFiltered = {
   userCount: number;
 };
 
-type GroupActivityData = {
-  eventCount?: number;
-  newGroupId?: number;
-  oldGroupId?: number;
-  text?: string;
-};
+export enum GroupActivityType {
+  NOTE = 'note',
+  SET_RESOLVED = 'set_resolved',
+  SET_RESOLVED_BY_AGE = 'set_resolved_by_age',
+  SET_RESOLVED_IN_RELEASE = 'set_resolved_in_release',
+  SET_RESOLVED_IN_COMMIT = 'set_resolved_in_commit',
+  SET_RESOLVED_IN_PULL_REQUEST = 'set_resolved_in_pull_request',
+  SET_UNRESOLVED = 'set_unresolved',
+  SET_IGNORED = 'set_ignored',
+  SET_PUBLIC = 'set_public',
+  SET_PRIVATE = 'set_private',
+  SET_REGRESSION = 'set_regression',
+  CREATE_ISSUE = 'create_issue',
+  UNMERGE_SOURCE = 'unmerge_source',
+  UNMERGE_DESTINATION = 'unmerge_destination',
+  FIRST_SEEN = 'first_seen',
+  ASSIGNED = 'assigned',
+  UNASSIGNED = 'unassigned',
+  MERGE = 'merge',
+  REPROCESS = 'reprocess',
+}
 
-type GroupActivity = {
-  data: GroupActivityData;
+type GroupActivityBase = {
   dateCreated: string;
   id: string;
-  type: string;
+  project: Project;
   user?: null | User;
+  assignee?: string;
+  issue?: Group;
 };
+
+type GroupActivityNote = GroupActivityBase & {
+  type: GroupActivityType.NOTE;
+  data: Record<string, any>;
+};
+
+type GroupActivitySetResolved = GroupActivityBase & {
+  type: GroupActivityType.SET_RESOLVED;
+  data: Record<string, any>;
+};
+
+type GroupActivitySetUnresolved = GroupActivityBase & {
+  type: GroupActivityType.SET_UNRESOLVED;
+  data: Record<string, any>;
+};
+
+type GroupActivitySetPublic = GroupActivityBase & {
+  type: GroupActivityType.SET_PUBLIC;
+  data: Record<string, any>;
+};
+
+type GroupActivitySetPrivate = GroupActivityBase & {
+  type: GroupActivityType.SET_PRIVATE;
+  data: Record<string, any>;
+};
+
+type GroupActivitySetByAge = GroupActivityBase & {
+  type: GroupActivityType.SET_RESOLVED_BY_AGE;
+  data: Record<string, any>;
+};
+
+type GroupActivityUnassigned = GroupActivityBase & {
+  type: GroupActivityType.UNASSIGNED;
+  data: Record<string, any>;
+};
+
+type GroupActivityFirstSeen = GroupActivityBase & {
+  type: GroupActivityType.FIRST_SEEN;
+  data: Record<string, any>;
+};
+
+type GroupActivityRegression = GroupActivityBase & {
+  type: GroupActivityType.SET_REGRESSION;
+  data: {
+    version?: string;
+  };
+};
+
+type GroupActivitySetByResolvedInRelease = GroupActivityBase & {
+  type: GroupActivityType.SET_RESOLVED_IN_RELEASE;
+  data: {
+    version?: string;
+  };
+};
+
+type GroupActivitySetByResolvedInCommit = GroupActivityBase & {
+  type: GroupActivityType.SET_RESOLVED_IN_COMMIT;
+  data: {
+    commit: Commit;
+  };
+};
+
+type GroupActivitySetByResolvedInPullRequest = GroupActivityBase & {
+  type: GroupActivityType.SET_RESOLVED_IN_PULL_REQUEST;
+  data: {
+    pullRequest: PullRequest;
+  };
+};
+
+export type GroupActivitySetIgnored = GroupActivityBase & {
+  type: GroupActivityType.SET_IGNORED;
+  data: {
+    ignoreDuration?: number;
+    ignoreUntil?: string;
+    ignoreUserCount?: number;
+    ignoreUserWindow?: number;
+    ignoreWindow?: number;
+    ignoreCount?: number;
+  };
+};
+
+export type GroupActivityReprocess = GroupActivityBase & {
+  type: GroupActivityType.REPROCESS;
+  data: {
+    eventCount: number;
+    newGroupId: number;
+    oldGroupId: number;
+  };
+};
+
+type GroupActivityUnmergeDestination = GroupActivityBase & {
+  type: GroupActivityType.UNMERGE_DESTINATION;
+  data: {
+    fingerprints: Array<string>;
+    source?: {
+      id: string;
+      shortId: string;
+    };
+  };
+};
+
+type GroupActivityUnmergeSource = GroupActivityBase & {
+  type: GroupActivityType.UNMERGE_SOURCE;
+  data: {
+    fingerprints: Array<string>;
+    destination?: {
+      id: string;
+      shortId: string;
+    };
+  };
+};
+
+type GroupActivityMerge = GroupActivityBase & {
+  type: GroupActivityType.MERGE;
+  data: {
+    issues: Array<any>;
+  };
+};
+
+export type GroupActivityAssigned = GroupActivityBase & {
+  type: GroupActivityType.ASSIGNED;
+  data: {
+    assignee: string;
+    assigneeType: string;
+    user: Team | User;
+  };
+};
+
+export type GroupActivityCreateIssue = GroupActivityBase & {
+  type: GroupActivityType.CREATE_ISSUE;
+  data: {
+    provider: string;
+    location: string;
+    title: string;
+  };
+};
+
+export type GroupActivity =
+  | GroupActivityNote
+  | GroupActivitySetResolved
+  | GroupActivitySetUnresolved
+  | GroupActivitySetIgnored
+  | GroupActivitySetByAge
+  | GroupActivitySetByResolvedInRelease
+  | GroupActivitySetByResolvedInRelease
+  | GroupActivitySetByResolvedInCommit
+  | GroupActivitySetByResolvedInPullRequest
+  | GroupActivityFirstSeen
+  | GroupActivityMerge
+  | GroupActivityReprocess
+  | GroupActivityUnassigned
+  | GroupActivityUnmergeDestination
+  | GroupActivitySetPublic
+  | GroupActivitySetPrivate
+  | GroupActivityRegression
+  | GroupActivityUnmergeSource
+  | GroupActivityAssigned
+  | GroupActivityCreateIssue;
+
+export type Activity = GroupActivity;
 
 // TODO(ts): incomplete
 export type Group = GroupFiltered & {
@@ -998,6 +1174,7 @@ export type PullRequest = {
   id: string;
   title: string;
   externalUrl: string;
+  repository: Repository;
 };
 
 type IntegrationDialog = {
@@ -1328,9 +1505,9 @@ export type Commit = {
   id: string;
   message: string | null;
   dateCreated: string;
+  releases: BaseRelease[];
   repository?: Repository;
   author?: User;
-  releases: BaseRelease[];
 };
 
 export type Committer = {
@@ -1628,16 +1805,6 @@ export type CrashFreeTimeBreakdown = {
   crashFreeUsers: number | null;
   totalUsers: number;
 }[];
-
-export type Activity = {
-  data: any;
-  dateCreated: string;
-  type: string;
-  id: string;
-  project: Project;
-  issue?: Group;
-  user?: User;
-};
 
 export type PlatformIntegration = {
   id: string;

src/sentry/static/sentry/app/views/organizationActivity/activityFeedItem.tsx
@@ -305,7 +305,11 @@ class ActivityItem extends React.Component<Props, State> {
     const {className, item} = this.props;
 
     const avatar = (
-      <ActivityAvatar type={!item.user ? 'system' : 'user'} user={item.user} size={36} />
+      <ActivityAvatar
+        type={!item.user ? 'system' : 'user'}
+        user={item.user ?? undefined}
+        size={36}
+      />
     );
     const author = {
       name: item.user ? item.user.name : 'Sentry',

src/sentry/static/sentry/app/views/organizationGroupDetails/groupActivity.tsx
@@ -173,7 +173,7 @@ class GroupActivity extends React.Component<Props, State> {
                       header={
                         <GroupActivityItem
                           author={<ActivityAuthor>{authorName}</ActivityAuthor>}
-                          item={item}
+                          activity={item}
                           orgSlug={this.props.params.orgId}
                           projectId={group.project.id}
                         />

src/sentry/static/sentry/app/views/organizationGroupDetails/groupActivityItem.jsx
@@ -1,195 +1,0 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
-import CommitLink from 'app/components/commitLink';
-import Duration from 'app/components/duration';
-import PullRequestLink from 'app/components/pullRequestLink';
-import Version from 'app/components/version';
-import {t, tct, tn} from 'app/locale';
-import MemberListStore from 'app/stores/memberListStore';
-import TeamStore from 'app/stores/teamStore';
-
-class GroupActivityItem extends React.Component {
-  static propTypes = {
-    author: PropTypes.node,
-    item: PropTypes.object,
-    orgSlug: PropTypes.string,
-    projectId: PropTypes.string,
-  };
-
-  render() {
-    const {author, item, orgSlug, projectId} = this.props;
-    const {data} = item;
-
-    const issuesLink = `/organizations/${orgSlug}/issues/`;
-
-    switch (item.type) {
-      case 'note':
-        return t('%s left a comment', author);
-      case 'set_resolved':
-        return t('%s marked this issue as resolved', author);
-      case 'set_resolved_by_age':
-        return t('%(author)s marked this issue as resolved due to inactivity', {
-          author,
-        });
-      case 'set_resolved_in_release':
-        return data.version
-          ? t('%(author)s marked this issue as resolved in %(version)s', {
-              author,
-              version: (
-                <Version version={data.version} projectId={projectId} tooltipRawVersion />
-              ),
-            })
-          : t('%s marked this issue as resolved in the upcoming release', author);
-      case 'set_resolved_in_commit':
-        return t('%(author)s marked this issue as resolved in %(version)s', {
-          author,
-          version: (
-            <CommitLink
-              inline
-              commitId={data.commit && data.commit.id}
-              repository={data.commit && data.commit.repository}
-            />
-          ),
-        });
-      case 'set_resolved_in_pull_request':
-        return t('%(author)s marked this issue as resolved in %(version)s', {
-          author,
-          version: (
-            <PullRequestLink
-              inline
-              pullRequest={data.pullRequest}
-              repository={data.pullRequest && data.pullRequest.repository}
-            />
-          ),
-        });
-      case 'set_unresolved':
-        return t('%s marked this issue as unresolved', author);
-      case 'set_ignored':
-        if (data.ignoreDuration) {
-          return t('%(author)s ignored this issue for %(duration)s', {
-            author,
-            duration: <Duration seconds={data.ignoreDuration * 60} />,
-          });
-        } else if (data.ignoreCount && data.ignoreWindow) {
-          return tct(
-            '[author] ignored this issue until it happens [count] time(s) in [duration]',
-            {
-              author,
-              count: data.ignoreCount,
-              duration: <Duration seconds={data.ignoreWindow * 60} />,
-            }
-          );
-        } else if (data.ignoreCount) {
-          return tct('[author] ignored this issue until it happens [count] time(s)', {
-            author,
-            count: data.ignoreCount,
-          });
-        } else if (data.ignoreUserCount && data.ignoreUserWindow) {
-          return tct(
-            '[author] ignored this issue until it affects [count] user(s) in [duration]',
-            {
-              author,
-              count: data.ignoreUserCount,
-              duration: <Duration seconds={data.ignoreUserWindow * 60} />,
-            }
-          );
-        } else if (data.ignoreUserCount) {
-          return tct('[author] ignored this issue until it affects [count] user(s)', {
-            author,
-            count: data.ignoreUserCount,
-          });
-        }
-        return t('%s ignored this issue', author);
-      case 'set_public':
-        return t('%s made this issue public', author);
-      case 'set_private':
-        return t('%s made this issue private', author);
-      case 'set_regression':
-        return data.version
-          ? t('%(author)s marked this issue as a regression in %(version)s', {
-              author,
-              version: (
-                <Version version={data.version} projectId={projectId} tooltipRawVersion />
-              ),
-            })
-          : t('%s marked this issue as a regression', author);
-      case 'create_issue':
-        return t('%(author)s created an issue on %(provider)s titled %(title)s', {
-          author,
-          provider: data.provider,
-          title: <a href={data.location}>{data.title}</a>,
-        });
-      case 'unmerge_source':
-        return tn(
-          '%2$s migrated %1$s fingerprint to %3$s',
-          '%2$s migrated %1$s fingerprints to %3$s',
-          data.fingerprints.length,
-          author,
-          data.destination ? (
-            <a href={`${issuesLink}${data.destination.id}`}>{data.destination.shortId}</a>
-          ) : (
-            t('a group')
-          )
-        );
-      case 'unmerge_destination':
-        return tn(
-          '%2$s migrated %1$s fingerprint from %3$s',
-          '%2$s migrated %1$s fingerprints from %3$s',
-          data.fingerprints.length,
-          author,
-          data.source ? (
-            <a href={`${issuesLink}${data.source.id}`}>{data.source.shortId}</a>
-          ) : (
-            t('a group')
-          )
-        );
-      case 'first_seen':
-        return t('%s first saw this issue', author);
-      case 'assigned':
-        let assignee;
-
-        if (data.assigneeType === 'team') {
-          const team = TeamStore.getById(data.assignee);
-          assignee = team ? team.slug : '<unknown-team>';
-
-          return t('%(author)s assigned this issue to #%(assignee)s', {
-            author,
-            assignee,
-          });
-        }
-
-        if (item.user && data.assignee === item.user.id) {
-          return t('%s assigned this issue to themselves', author);
-        } else {
-          assignee = MemberListStore.getById(data.assignee);
-          if (assignee && assignee.email) {
-            return t('%(author)s assigned this issue to %(assignee)s', {
-              author,
-              assignee: assignee.email,
-            });
-          } else {
-            return t('%s assigned this issue to an unknown user', author);
-          }
-        }
-      case 'unassigned':
-        return t('%s unassigned this issue', author);
-      case 'merge':
-        return tn(
-          '%2$s merged %1$s issue into this issue',
-          '%2$s merged %1$s issues into this issue',
-          data.issues.length,
-          author
-        );
-      case 'reprocess':
-        return t(
-          '%(author)s reprocessed this issue, some events may have moved into new issues',
-          {author}
-        );
-      default:
-        return ''; // should never hit (?)
-    }
-  }
-}
-
-export default GroupActivityItem;

src/sentry/static/sentry/app/views/organizationGroupDetails/groupEventDetails/groupEventDetails.tsx
@@ -16,8 +16,14 @@ import LoadingIndicator from 'app/components/loadingIndicator';
 import MutedBox from 'app/components/mutedBox';
 import ResolutionBox from 'app/components/resolutionBox';
 import SentryTypes from 'app/sentryTypes';
-import {Environment, Event, Group, Organization, Project} from 'app/types';
-import {defined} from 'app/utils';
+import {
+  Environment,
+  Event,
+  Group,
+  GroupActivityReprocess,
+  Organization,
+  Project,
+} from 'app/types';
 import {metric} from 'app/utils/analytics';
 import fetchSentryAppInstallations from 'app/utils/fetchSentryAppInstallations';
 
@@ -193,10 +199,9 @@ class GroupEventDetails extends React.Component<Props, State> {
         <div className="event-details-container">
           {hasReprocessingV2Feature &&
           status === 'reprocessing' &&
-          type === 'reprocess' &&
-          defined(data.eventCount) ? (
+          type === 'reprocess' ? (
             <ReprocessingProgress
-              totalEvents={data.eventCount}
+              totalEvents={(data as GroupActivityReprocess['data']).eventCount}
               pendingEvents={pendingEvents ?? 0}
             />
           ) : (

commit ba2dbc3b1b8c071667960943eccb033e047d843d

fix: fix issue created link 404
External links should use <a>, not <Link>.

Fixes: 12f1cd8461 ("ref(ts): Convert groupActivityItem to ts (#22568)")

src/sentry/static/sentry/app/views/organizationGroupDetails/groupActivityItem.tsx
@@ -180,7 +180,7 @@ function GroupActivityItem({activity, orgSlug, projectId, author}: Props) {
         return tct('[author] created an issue on [provider] titled [title]', {
           author,
           provider: data.provider,
-          title: <Link to={data.location}>{data.title}</Link>,
+          title: <a href={data.location}>{data.title}</a>,
         });
       }
       case GroupActivityType.UNMERGE_SOURCE: {