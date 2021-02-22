ad40a73163531ce478453f9e15c5fd85f6f956a1
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-02-22 14:19:16 -0800

    ref(js): use merge for chart options (#24019)

--- src/sentry/static/sentry/app/components/charts/components/grid.tsx
+++ src/sentry/static/sentry/app/components/charts/components/grid.tsx
@@ -1,18 +1,20 @@
 import {EChartOption} from 'echarts';
-
+import merge from 'lodash/merge';
 /**
  * Drawing grid in rectangular coordinates
  *
  * e.g. alignment of your chart?
  */
 export default function Grid(props: EChartOption.Grid = {}): EChartOption.Grid {
-  return {
-    top: 20,
-    bottom: 20,
-    // This should allow for sufficient space for Y-axis labels
-    left: '0%',
-    right: '0%',
-    containLabel: true,
-    ...props,
-  };
+  return merge(
+    {
+      top: 20,
+      bottom: 20,
+      // This should allow for sufficient space for Y-axis labels
+      left: '0%',
+      right: '0%',
+      containLabel: true,
+    },
+    props
+  );
 }
