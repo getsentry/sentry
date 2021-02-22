0dd5bd6f8ec054de8f2b5b46e4015c8e52997a50
Armen Zambrano G <armenzg@sentry.io>
2021-02-22 09:00:53 -0500

    Update scripts/python.sh
    Co-authored-by: josh <joshuarli98@gmail.com>

--- scripts/python.sh
+++ scripts/python.sh
@@ -23,8 +23,7 @@ install-py-dev() {
     require wheel || pip install wheel
     echo "--> Installing Sentry (for development)"
     # In Big Sur, versions of pip before 20.3 require SYSTEM_VERSION_COMPAT set
-    old_pip=$(python -c 'import sys; import pip; from pip._vendor.packaging.version import parse; print(1 if parse(pip.__version__) < parse("20.3") else 0)')
-    if query_big_sur && [[ $old_pip == 1 ]]; then
+    if query_big_sur && python -c 'from sys import exit; import pip; from pip._vendor.packaging.version import parse; exit(1 if parse(pip.__version__) < parse("20.3") else 0)'; then
         SENTRY_LIGHT_BUILD=1 SYSTEM_VERSION_COMPAT=1 pip install -e '.[dev]'
     else
         # SENTRY_LIGHT_BUILD=1 disables webpacking during setup.py.
