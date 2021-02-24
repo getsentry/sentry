a16ae2e029d24e8b87b302292302a2f15207cfe4
Matej Minar <matej.minar@sentry.io>
2021-02-24 11:11:50 +0100

    feat(ui): Add other releases series to all release detail charts (#23972)

--- src/sentry/static/sentry/app/views/releases/utils/sessionTerm.tsx
+++ src/sentry/static/sentry/app/views/releases/utils/sessionTerm.tsx
@@ -22,6 +22,14 @@ export const sessionTerm = {
   [SessionTerm.HEALTHY]: t('Healthy'),
   [SessionTerm.ERRORED]: t('Errored'),
   [SessionTerm.UNHANDLED]: t('Unhandled'),
+  duration: t('Session Duration'),
+  otherCrashed: t('Other Crashed'),
+  otherAbnormal: t('Other Abnormal'),
+  otherErrored: t('Other Errored'),
+  otherHealthy: t('Other Healthy'),
+  otherCrashFreeUsers: t('Other Crash Free Users'),
+  otherCrashFreeSessions: t('Other Crash Free Sessions'),
+  otherReleases: t('Other Releases'),
 };
 
 // This should never be used directly (except in tests)
