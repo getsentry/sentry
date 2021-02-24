a5f57b5489a141d527071aed2f58d36fda6a19ea
Ahmed Etefy <ahmed.etefy12@gmail.com>
2021-02-24 17:11:22 +0100

    fix (serverless): Better handling for missing role exception + fixed bug (#24034)
    * Added test for discovered bug with env variables when trying to re-enable already enabled aws lambda
    
    * Added logic that does not ovveride SENTRY_INITIAL_HANDLER when trying to re-enable already enabled aws lambda
    
    * Redefined override env variable check for re-enabling already enabled python func
    
    * Added better error handling for missing aws role
    
    * Added better handling for the err message with missing aws lambda role
    
    * Refactored custom error handling into a util function
    
    * Added test for missing role exception handling

--- tests/sentry/integrations/aws_lambda/test_integration.py
+++ tests/sentry/integrations/aws_lambda/test_integration.py
@@ -337,3 +337,63 @@ class AwsLambdaIntegrationTest(IntegrationTestCase):
         mock_react_view.assert_called_with(
             ANY, "awsLambdaFailureDetails", {"lambdaFunctionFailures": failures, "successCount": 0}
         )
+
+    @patch("sentry.integrations.aws_lambda.integration.get_supported_functions")
+    @patch("sentry.integrations.aws_lambda.integration.gen_aws_client")
+    @patch.object(PipelineView, "render_react_view", return_value=HttpResponse())
+    def test_lambda_setup_layer_missing_role_error(
+        self, mock_react_view, mock_gen_aws_client, mock_get_supported_functions
+    ):
+        class MockException(Exception):
+            pass
+
+        missing_role_err = (
+            "An error occurred (InvalidParameterValueException) when "
+            "calling the UpdateFunctionConfiguration operation: "
+            "The role defined for the function cannot be "
+            "assumed by Lambda."
+        )
+        mock_client = Mock()
+        mock_gen_aws_client.return_value = mock_client
+        mock_client.update_function_configuration = MagicMock(
+            side_effect=Exception(missing_role_err)
+        )
+        mock_client.describe_account = MagicMock(return_value={"Account": {"Name": "my_name"}})
+        mock_client.exceptions = MagicMock()
+        mock_client.exceptions.ResourceConflictException = MockException
+
+        mock_get_supported_functions.return_value = [
+            {
+                "FunctionName": "lambdaB",
+                "Runtime": "nodejs10.x",
+                "FunctionArn": "arn:aws:lambda:us-east-2:599817902985:function:lambdaB",
+            },
+        ]
+
+        aws_external_id = "12-323"
+        self.pipeline.state.step_index = 2
+        self.pipeline.state.data = {
+            "region": region,
+            "account_number": account_number,
+            "aws_external_id": aws_external_id,
+            "project_id": self.projectA.id,
+        }
+
+        resp = self.client.post(
+            self.setup_path,
+            {"lambdaB": True},
+            format="json",
+            HTTP_ACCEPT="application/json",
+            headers={"Content-Type": "application/json", "Accept": "application/json"},
+        )
+
+        assert resp.status_code == 200
+        assert not Integration.objects.filter(provider=self.provider.key).exists()
+
+        failures = [
+            {"name": "lambdaB", "error": "Invalid role associated with the lambda function"}
+        ]
+
+        mock_react_view.assert_called_with(
+            ANY, "awsLambdaFailureDetails", {"lambdaFunctionFailures": failures, "successCount": 0}
+        )
