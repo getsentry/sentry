3d0af6de0b34d30e87ee8393efa385690696cf03
Colleen O'Rourke <colleen@sentry.io>
2021-02-25 13:32:11 -0800

    ref(external issues): Add logging to get endpoint for external issues

--- src/sentry/api/endpoints/group_external_issues.py
+++ src/sentry/api/endpoints/group_external_issues.py
@@ -1,6 +1,10 @@
+import logging
+
 from sentry.api.bases.group import GroupEndpoint
 from sentry.api.serializers import serialize
 from sentry.models import PlatformExternalIssue
+
+logger = logging.getLogger("sentry.group_external_issues.api")
 
 
 class GroupExternalIssuesEndpoint(GroupEndpoint):
@@ -8,6 +12,12 @@ class GroupExternalIssuesEndpoint(GroupEndpoint):
 
         external_issues = PlatformExternalIssue.objects.filter(group_id=group.id)
 
+        logger.info(
+            "group_external_issue.get",
+            extra={
+                "user": request.user,
+            },
+        )
         return self.paginate(
             request=request,
             queryset=external_issues,
