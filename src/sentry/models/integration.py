cf300ac69ee61592590299dc7da3db1cfdcc299b
Colleen O'Rourke <colleen@sentry.io>
2021-02-26 14:42:47 -0800

    feat(Notifications Platform): Create NotificationSetting Table (#24107)
    * feat(notification platform): Create NotificationSetting table

--- src/sentry/models/integration.py
+++ src/sentry/models/integration.py
@@ -66,11 +66,15 @@ class RepositoryProjectPathConfig(DefaultFieldsModel):
 class ExternalProviders(Enum):
     GITHUB = 0
     GITLAB = 1
+    EMAIL = 100
+    SLACK = 110
 
 
 EXTERNAL_PROVIDERS = {
     ExternalProviders.GITHUB: "github",
     ExternalProviders.GITLAB: "gitlab",
+    ExternalProviders.EMAIL: "email",
+    ExternalProviders.SLACK: "slack",
 }
 
 
