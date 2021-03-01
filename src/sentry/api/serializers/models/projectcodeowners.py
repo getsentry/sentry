335d78ad7d46543428cde57c90fa13d4a33b1342
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2021-03-01 15:16:00 -0800

    attach_foreignkey

--- src/sentry/api/serializers/models/projectcodeowners.py
+++ src/sentry/api/serializers/models/projectcodeowners.py
@@ -1,23 +1,25 @@
 from sentry.api.serializers import Serializer, register
 from sentry.models import ProjectCodeOwners
-from sentry.models.integration import Integration
+from sentry.utils.db import attach_foreignkey
 
 
 @register(ProjectCodeOwners)
 class ProjectCodeOwnersSerializer(Serializer):
     def get_attrs(self, item_list, user, **kwargs):
-        attrs = {}
-        for item in item_list:
-            org_integration = item.repository_project_path_config.organization_integration
-            if org_integration:
-                try:
-                    integration = Integration.objects.get(id=org_integration.integration_id)
-                    attrs[item] = {"provider": integration.provider}
-                except Integration.DoesNotExist:
-                    attrs[item] = {"provider": "unknown"}
-            else:
-                attrs[item] = {"provider": "unknown"}
-        return attrs
+        attach_foreignkey(
+            item_list,
+            ProjectCodeOwners.repository_project_path_config,
+            related=("organization_integration",),
+        )
+
+        return {
+            item: {
+                "provider": item.repository_project_path_config.organization_integration.integration.provider
+                if item.repository_project_path_config.organization_integration
+                else "unknown"
+            }
+            for item in item_list
+        }
 
     def serialize(self, obj, attrs, user):
         data = {
