c69698ed17d74a404baf360ff206f9176eab2a69
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2021-03-01 16:22:01 -0800

    feat(migration): Remove oganization_integration field from projectcodeowners (part 1)(#24072)
    Objective
    We need to create a migration to remove the organization_integration column from sentry_projectcodeowners table. We already have a reference to it from repository_project_path_config foreignkey column and it is possible to update both tables independently, increasing the chances of a drift in consistency.
    
    This is part 1 of a 2 part migration.

--- src/sentry/api/serializers/models/projectcodeowners.py
+++ src/sentry/api/serializers/models/projectcodeowners.py
@@ -1,9 +1,26 @@
 from sentry.api.serializers import Serializer, register
 from sentry.models import ProjectCodeOwners
+from sentry.utils.db import attach_foreignkey
 
 
 @register(ProjectCodeOwners)
 class ProjectCodeOwnersSerializer(Serializer):
+    def get_attrs(self, item_list, user, **kwargs):
+        attach_foreignkey(
+            item_list,
+            ProjectCodeOwners.repository_project_path_config,
+            related=("organization_integration",),
+        )
+
+        return {
+            item: {
+                "provider": item.repository_project_path_config.organization_integration.integration.provider
+                if item.repository_project_path_config.organization_integration
+                else "unknown"
+            }
+            for item in item_list
+        }
+
     def serialize(self, obj, attrs, user):
         data = {
             "raw": obj.raw,
@@ -12,7 +29,7 @@ class ProjectCodeOwnersSerializer(Serializer):
             "codeMappingId": str(obj.repository_project_path_config_id),
             "provider": "unknown",
         }
-        if obj.organization_integration:
-            data["provider"] = obj.organization_integration.integration.provider
+
+        data["provider"] = attrs.get("provider", "unknown")
 
         return data
