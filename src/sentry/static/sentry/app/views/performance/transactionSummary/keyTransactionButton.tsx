9a0f9f5f4a773b744982e99c0dc07a45d1912569
Dora Chan <dora.lchan@gmail.com>
2021-03-02 14:40:30 -0800

    simplify

--- src/sentry/static/sentry/app/views/performance/transactionSummary/keyTransactionButton.tsx
+++ src/sentry/static/sentry/app/views/performance/transactionSummary/keyTransactionButton.tsx
@@ -128,12 +128,7 @@ class KeyTransactionButton extends React.Component<Props, State> {
 
     return (
       <Button
-        icon={
-          <IconStar
-            color={isKeyTransaction ? 'yellow300' : 'textColor'}
-            isSolid={!!isKeyTransaction}
-          />
-        }
+        icon={isKeyTransaction ? <IconStar color="yellow300" isSolid /> : <IconStar />}
         onClick={this.toggleKeyTransactionHandler}
       >
         {t('Key Transaction')}
