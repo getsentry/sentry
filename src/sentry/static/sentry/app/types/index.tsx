b23f783b5f8c2f4239fb79d7dcfb02a101c1713c
Stephen Cefali <scefali@sentry.io>
2021-03-02 12:05:42 -0800

    cleanup

--- src/sentry/static/sentry/app/types/index.tsx
+++ src/sentry/static/sentry/app/types/index.tsx
@@ -4,8 +4,6 @@ import Alert from 'app/components/alert';
 import {SymbolicatorStatus} from 'app/components/events/interfaces/types';
 import {API_ACCESS_SCOPES} from 'app/constants';
 import {PlatformKey} from 'app/data/platformCategories';
-import ConfigStore from 'app/stores/configStore';
-import HookStore from 'app/stores/hookStore';
 import {OrgExperiments, UserExperiments} from 'app/types/experiments';
 import {WIDGET_DISPLAY} from 'app/views/dashboards/constants';
 import {Query as DiscoverQuery} from 'app/views/discover/types';
@@ -66,9 +64,6 @@ declare global {
      * See sentry/js/ads.js for how this global is disabled.
      */
     adblockSuspected?: boolean;
-
-    HookStore: HookStore;
-    ConfigStore: ConfigStore;
   }
 }
 
