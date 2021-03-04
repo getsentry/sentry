37dfdc5a8193538d31be2abcc435f35c6eddab6e
Chris Fuller <cfuller@sentry.io>
2021-03-04 14:48:01 -0500

    pre save user and team signal

--- src/sentry/models/actor.py
+++ src/sentry/models/actor.py
@@ -1,12 +1,8 @@
-from enum import Enum
-
 from django.db import models
+from django.db.models.signals import pre_save
 from sentry.db.models import Model
 
-
-class ActorType(Enum):
-    TEAM = 0
-    USER = 1
+ACTOR_TYPES = {"team": 0, "user": 1}
 
 
 class Actor(Model):
@@ -14,11 +10,25 @@ class Actor(Model):
 
     type = models.PositiveSmallIntegerField(
         choices=(
-            (ActorType.TEAM, "team"),
-            (ActorType.USER, "user"),
+            (ACTOR_TYPES["team"], "team"),
+            (ACTOR_TYPES["user"], "user"),
         )
     )
 
     class Meta:
         app_label = "sentry"
         db_table = "sentry_actor"
+
+
+def handle_actor_pre_save(instance, **kwargs):
+    # we want to create an actor if we don't have one
+    if not instance.actor and not instance.actor_id:
+        instance.actor = Actor.objects.create(type=ACTOR_TYPES[type(instance).__name__.lower()])
+
+
+pre_save.connect(
+    handle_actor_pre_save, sender="sentry.Team", dispatch_uid="handle_actor_pre_save", weak=False
+)
+pre_save.connect(
+    handle_actor_pre_save, sender="sentry.User", dispatch_uid="handle_actor_pre_save", weak=False
+)
