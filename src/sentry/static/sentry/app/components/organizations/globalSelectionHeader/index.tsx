40a7fb26f9be9d5cb7e61293918b449db186da78
Mark Story <mark@sentry.io>
2021-03-04 09:44:09 -0500

    cleanup: Remove global selection logic for discover 1 (#24222)
    Discover 1 helped introduce the global selection header but it also had
    some quirky behavior and required custom affordances that we don't use
    anywhere else. With discover 1 gone we don't need this code anymore
    either.

--- src/sentry/static/sentry/app/components/organizations/globalSelectionHeader/index.tsx
+++ src/sentry/static/sentry/app/components/organizations/globalSelectionHeader/index.tsx
@@ -18,13 +18,6 @@ type GlobalSelectionHeaderProps = Omit<
 type Props = {
   organization: Organization;
   projects: Project[];
-
-  /**
-   * If this is true, do not attempt to control routing. This is only used in discover v1
-   *
-   * TODO(discoverv1): Removeme
-   */
-  hasCustomRouting?: boolean;
 } & ReactRouter.WithRouterProps &
   GlobalSelectionHeaderProps &
   Partial<
@@ -60,7 +53,6 @@ class GlobalSelectionHeaderContainer extends React.Component<Props> {
       defaultSelection,
       forceProject,
       shouldForceProject,
-      hasCustomRouting,
       skipLoadLastUsed,
       showAbsolute,
       ...props
@@ -79,9 +71,8 @@ class GlobalSelectionHeaderContainer extends React.Component<Props> {
             organization={organization}
             defaultSelection={defaultSelection}
             forceProject={forceProject}
-            isDisabled={!!hasCustomRouting}
             shouldForceProject={!!shouldForceProject}
-            shouldEnforceSingleProject={!hasCustomRouting && enforceSingleProject}
+            shouldEnforceSingleProject={enforceSingleProject}
             memberProjects={memberProjects}
             showAbsolute={showAbsolute}
           />
@@ -91,7 +82,7 @@ class GlobalSelectionHeaderContainer extends React.Component<Props> {
           loadingProjects={loadingProjects}
           location={location}
           organization={organization}
-          router={!hasCustomRouting ? router : null}
+          router={router}
           routes={routes}
           shouldForceProject={!!shouldForceProject}
           defaultSelection={defaultSelection}
