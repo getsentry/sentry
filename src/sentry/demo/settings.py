690c9d457f261d76731b54b139bff10d95147b18
Stephen Cefali <scefali@sentry.io>
2021-03-04 15:15:34 -0800

    adds comment

--- src/sentry/demo/settings.py
+++ src/sentry/demo/settings.py
@@ -3,6 +3,13 @@ from django.conf import settings
 
 from sentry.conf.server import *  # NOQA
 
+"""
+To get this file to load, add the follwing to your sentry.conf.py file:
+
+from sentry.demo.settings import *
+
+"""
+
 if settings.DEMO_MODE:
     settings.CELERYBEAT_SCHEDULE["demo_delete_users_orgs"] = {
         "task": "sentry.demo.tasks.delete_users_orgs",
