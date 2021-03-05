80235bf94d37ade3a06ac4cd1b7d7d8a0c08ab25
k-fish <kevan.fisher@sentry.io>
2021-03-05 10:02:25 -0800

    ref(perf): Add experimental endpoint for performance facets (#24225)
    * ref(perf): Add experimental endpoint for performance facets
    
    This is an experimental endpoint to test performance and usability of finding the most variable tag values. Trying out a sampled stddev instead of count to find tags that have high variability within them in the first place (although might need a count constraint too). Added a few simple tests just to make sure happy path won't error in prod

---
+++ src/sentry/api/endpoints/organization_events_facets_performance.py
@@ -0,0 +1,52 @@
+import sentry_sdk
+
+from collections import defaultdict
+from rest_framework.response import Response
+
+from sentry.api.bases import OrganizationEventsV2EndpointBase, NoProjects
+from sentry.snuba import discover
+from sentry import features, tagstore
+
+
+class OrganizationEventsFacetsPerformanceEndpoint(OrganizationEventsV2EndpointBase):
+    def has_feature(self, organization, request):
+        return features.has(
+            "organizations:performance-tag-explorer", organization, actor=request.user
+        )
+
+    def get(self, request, organization):
+        if not self.has_feature(organization, request):
+            return Response(status=404)
+
+        try:
+            params = self.get_snuba_params(request, organization)
+        except NoProjects:
+            return Response([])
+
+        aggregate_column = request.GET.get("aggregateColumn", "duration")
+        orderby = request.GET.getlist("order", None)
+
+        with sentry_sdk.start_span(op="discover.endpoint", description="discover_query"):
+            with self.handle_query_errors():
+                facets = discover.get_performance_facets(
+                    query=request.GET.get("query"),
+                    params=params,
+                    referrer="api.organization-events-facets-performance.top-tags",
+                    aggregate_column=aggregate_column,
+                    orderby=orderby,
+                )
+
+        with sentry_sdk.start_span(op="discover.endpoint", description="populate_results") as span:
+            span.set_data("facet_count", len(facets or []))
+            resp = defaultdict(lambda: {"key": "", "topValues": []})
+            for row in facets:
+                values = resp[row.key]
+                values["key"] = tagstore.get_standardized_key(row.key)
+                values["topValues"].append(
+                    {
+                        "name": tagstore.get_tag_value_label(row.key, row.value),
+                        "value": row.value,
+                        f"avg_{aggregate_column}": row.count,
+                    }
+                )
+        return Response(list(resp.values()))
