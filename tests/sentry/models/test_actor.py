6b6002f76f7045779bf9c3a2fa1abdf61fa3ef9a
Chris Fuller <cfuller@sentry.io>
2021-03-08 11:55:15 -0500

    Making test more explicit

--- tests/sentry/models/test_actor.py
+++ tests/sentry/models/test_actor.py
@@ -6,9 +6,11 @@ class ActorTest(TestCase):
     def test_pre_save(self):
         user = self.create_user(email="test@pre_save.com")
         assert user.actor is not None
+        assert type(user.actor) is Actor
 
         team = self.create_team(name="pre save team", organization=self.organization)
         assert team.actor is not None
+        assert type(team.actor) is Actor
 
         actor = Actor.objects.create(type=1)
         user2 = User.objects.create(username="meow", actor_id=actor.id)
