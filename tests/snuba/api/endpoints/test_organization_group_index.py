9d05ab310fa9d455af8f7cb377bce20799b7857a
Chris Fuller <cfuller@sentry.io>
2021-03-08 13:21:34 -0500

    Fixing bug

--- tests/snuba/api/endpoints/test_organization_group_index.py
+++ tests/snuba/api/endpoints/test_organization_group_index.py
@@ -874,17 +874,6 @@ class GroupListTest(APITestCase, SnubaTestCase):
             assert response.status_code == 200
             assert len(response.data) == 0
 
-            response = self.get_response(
-                sort="inbox",
-                limit=10,
-                query="is:unresolved is:for_review assigned_or_suggested:me_or_none",
-                expand=["inbox", "owners"],
-                start=iso_format(before_now(days=9)),
-                end=iso_format(before_now(days=8)),
-            )
-            assert response.status_code == 200
-            assert len(response.data) == 0
-
     def test_assigned_or_suggested_search(self):
         event = self.store_event(
             data={
