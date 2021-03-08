422000c6528d5cddc727cda610bb900e11c6e03c
Chris Fuller <cfuller@sentry.io>
2021-03-08 15:38:21 -0500

    feat(workflow): pre_save signal on User/Team for Actor (#24247)

--- src/sentry/models/actor.py
+++ src/sentry/models/actor.py
@@ -1,5 +1,6 @@
 from django.db import models
 from sentry.db.models import Model
+from django.db.models.signals import pre_save
 
 ACTOR_TYPES = {"team": 0, "user": 1}
 
@@ -17,3 +18,19 @@ class Actor(Model):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_actor"
+
+
+def handle_actor_pre_save(instance, **kwargs):
+    # we want to create an actor if we don't have one
+    if not instance.actor_id:
+        instance.actor_id = Actor.objects.create(
+            type=ACTOR_TYPES[type(instance).__name__.lower()]
+        ).id
+
+
+pre_save.connect(
+    handle_actor_pre_save, sender="sentry.Team", dispatch_uid="handle_actor_pre_save", weak=False
+)
+pre_save.connect(
+    handle_actor_pre_save, sender="sentry.User", dispatch_uid="handle_actor_pre_save", weak=False
+)
