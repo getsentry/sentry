c21f0bd9da26f38e380f8dadf0fdca86866a25c3
Josh Ferge <josh.ferge@sentry.io>
2021-03-09 15:46:38 -0800

    PR feedback

--- src/sentry/api/endpoints/organization_stats_v2.py
+++ src/sentry/api/endpoints/organization_stats_v2.py
@@ -2,23 +2,9 @@ from rest_framework.response import Response
 from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.api.utils import get_date_range_rollup_from_params
 from sentry.api.bases.project import ProjectEndpoint
-from sentry.models import Project, Team
 
 from sentry.snuba import outcomes
-from sentry_relay import DataCategory
-from sentry.utils.outcomes import Outcome
-
-from sentry.utils.snuba import (
-    naiveify_datetime,
-    to_naive_timestamp,
-)
-
-
-CATEGORY_NAME_MAP = {
-    DataCategory.ERROR: "statsErrors",
-    DataCategory.TRANSACTION: "statsTransactions",
-    DataCategory.ATTACHMENT: "statsAttachments",
-}
+from sentry.api.serializers.models.organization_stats import StatsResponse
 
 
 class OrganizationStatsEndpointV2(OrganizationEndpoint):
@@ -39,7 +25,6 @@ class OrganizationStatsEndpointV2(OrganizationEndpoint):
             stat_to_update = response.get(row["category"])
             stat_to_update.update(row)
 
-        # response.zerofill(start, end, rollup)
         return Response(response.build_fields())
 
 
@@ -47,12 +32,8 @@ class OrganizationProjectStatsIndex(OrganizationEndpoint):
     def get(self, request, organization):
         start, end, rollup = get_date_range_rollup_from_params(request.GET, "1h", round_range=True)
 
-        # TODO: see if there's a better way to get the user's projects
-        project_list = []
-        team_list = Team.objects.get_for_user(organization=organization, user=request.user)
-        for team in team_list:
-            project_list.extend(Project.objects.get_for_user(team=team, user=request.user))
-        project_ids = list({p.id for p in project_list})
+        projects = self.get_projects(request, organization)
+        project_ids = list({p.id for p in projects})
 
         result = outcomes.query(
             start=start,
@@ -78,12 +59,8 @@ class OrganizationProjectStatsDetails(ProjectEndpoint, OrganizationEndpoint):
     def get(self, request, project):
         start, end, rollup = get_date_range_rollup_from_params(request.GET, "1h", round_range=True)
 
-        # TODO: see if there's a better way to get the user's projects
-        project_list = []
-        team_list = Team.objects.get_for_user(organization=project.organization, user=request.user)
-        for team in team_list:
-            project_list.extend(Project.objects.get_for_user(team=team, user=request.user))
-        project_ids = list({p.id for p in project_list})
+        projects = self.get_projects(request, project.organization)
+        project_ids = list({p.id for p in projects})
 
         if project.id not in project_ids:
             # TODO: more info in response here?
@@ -104,136 +81,3 @@ class OrganizationProjectStatsDetails(ProjectEndpoint, OrganizationEndpoint):
             stat_to_update.update(row)
 
         return Response(response.build_fields())
-
-
-# TODO: good names for these classes
-class StatsResponse:
-    def __init__(self, start, end, rollup):
-        self.errors = TimeSeriesValues(start, end, rollup)
-        self.transactions = TimeSeriesValues(start, end, rollup)
-        self.attachments = TimeSeriesValues(start, end, rollup)
-
-    _GETTERS = {
-        DataCategory.ERROR: (lambda s: s.errors),
-        DataCategory.TRANSACTION: (lambda s: s.transactions),
-        DataCategory.ATTACHMENT: (lambda s: s.attachments),
-    }
-
-    ALL_FIELDS = frozenset(_GETTERS.keys())
-
-    def get(self, category):
-        return self._GETTERS[category](self)
-
-    def __iter__(self):
-        yield DataCategory.ERROR, self.errors
-        yield DataCategory.TRANSACTION, self.transactions
-        yield DataCategory.ATTACHMENT, self.attachments
-
-    def zerofill(self, start, end, rollup):
-        self.errors = zerofill(self.errors, start, end, rollup, "time")
-        self.transactions = zerofill(self.transactions, start, end, rollup, "time")
-        self.attachments = zerofill(self.attachments, start, end, rollup, "time")
-
-    def build_fields(self):
-        return {CATEGORY_NAME_MAP[category]: values.serialize() for category, values in self}
-
-
-# TODO: perhaps remove this class
-class TimeSeriesValues:
-    def __init__(self, start, end, rollup):
-        self.values = zerofill({}, start, end, rollup, "time")
-
-    def update(self, row):
-        self.values[row["time"]].update(row)
-
-    def serialize(self):
-        return [value.serialize() for value in self.values.values()]
-
-
-class StatMeasure:
-    def __init__(self, quantity=0, times_seen=0):
-        self.quantity = quantity
-        self.times_seen = times_seen
-
-
-class MeasureValue:
-    def __init__(self, time):
-        self.accepted = StatMeasure(0, 0)
-        self.filtered = StatMeasure(0, 0)
-        self.over_quota = StatMeasure(0, 0)
-        self.spike_protection = StatMeasure(0, 0)
-        self.other = StatMeasure(0, 0)
-        self.time = time
-
-    def serialize(self):
-        return {
-            "accepted": {
-                "quantity": self.accepted.quantity,
-                "times_seen": self.accepted.times_seen,
-            },
-            "filtered": {"quantity": self.filtered.quantity, "times_seen": self.filtered.quantity},
-            "dropped": {
-                "overQuota": {
-                    "quantity": self.over_quota.quantity,
-                    "times_seen": self.over_quota.quantity,
-                },
-                "spikeProtection": {
-                    "quantity": self.spike_protection.quantity,
-                    "times_seen": self.spike_protection.quantity,
-                },
-                "other": {"quantity": self.other.quantity, "times_seen": self.other.quantity},
-            },
-            "time": self.time,
-        }
-
-    def update(self, row):
-        if row["outcome"] == Outcome.RATE_LIMITED:
-            if row["reason"] in {"usage_exceeded", "grace_period"}:
-                self.over_quota.quantity = row["quantity"]
-                self.over_quota.times_seen = row["times_seen"]
-            elif row["reason"] in "smart_rate_limit":
-                self.spike_protection.quantity = row["quantity"]
-                self.spike_protection.times_seen = row["quantity"]
-            else:
-                self.other.quantity = row["quantity"]
-                self.other.times_seen = row["quantity"]
-
-        elif row["outcome"] == Outcome.ACCEPTED:
-            self.accepted.quantity = row["quantity"]
-            self.accepted.times_seen = row["times_seen"]
-        elif row["outcome"] == Outcome.FILTERED:
-            self.filtered.quantity = row["quantity"]
-            self.filtered.times_seen = row["times_seen"]
-
-    def rate_limited_reason_mapper(reason):
-        # billing logic ported over. TODO: combine usages of this into some abstracted module?
-        if reason in {"usage_exceeded", "grace_period"}:
-            reason_val = "overQuota"
-        elif reason == "smart_rate_limit":
-            reason_val = "spikeProtection"
-        else:
-            reason_val = "other"
-        return reason_val
-
-
-# TODO: verify what kind of timestamp we return to frontend
-def zerofill(data, start, end, rollup, orderby):
-    rv = {}
-    start = int(to_naive_timestamp(naiveify_datetime(start)) / rollup) * rollup
-    end = (int(to_naive_timestamp(naiveify_datetime(end)) / rollup) * rollup) + rollup
-    data_by_time = {}
-
-    for obj in data:
-        if obj["time"] in data_by_time:
-            data_by_time[obj["time"]].append(obj)
-        else:
-            data_by_time[obj["time"]] = [obj]
-    for key in range(start, end, rollup):
-        if key in data_by_time and len(data_by_time[key]) > 0:
-            rv = rv + data_by_time[key]
-            data_by_time[key] = []
-        else:
-            val = MeasureValue(key)
-            rv[key] = val
-
-    return rv
---
+++ src/sentry/api/serializers/models/organization_stats.py
@@ -0,0 +1,128 @@
+from sentry_relay import DataCategory
+from sentry.utils.outcomes import Outcome
+
+from sentry.utils.snuba import (
+    naiveify_datetime,
+    to_naive_timestamp,
+)
+
+CATEGORY_NAME_MAP = {
+    DataCategory.ERROR: "statsErrors",
+    DataCategory.TRANSACTION: "statsTransactions",
+    DataCategory.ATTACHMENT: "statsAttachments",
+}
+
+
+class StatsResponse:
+    def __init__(self, start, end, rollup):
+        self._values = {k: TimeSeriesValues(start, end, rollup) for k in CATEGORY_NAME_MAP}
+
+    def get(self, category):
+        return self._values[category]
+
+    def __iter__(self):
+        return iter(self._values.items())
+
+    def zerofill(self, start, end, rollup):
+        self.errors = zerofill(self.errors, start, end, rollup, "time")
+        self.transactions = zerofill(self.transactions, start, end, rollup, "time")
+        self.attachments = zerofill(self.attachments, start, end, rollup, "time")
+
+    def build_fields(self):
+        return {CATEGORY_NAME_MAP[category]: values.serialize() for category, values in self}
+
+
+# TODO: perhaps remove this class
+class TimeSeriesValues:
+    def __init__(self, start, end, rollup):
+        self.values = zerofill({}, start, end, rollup, "time")
+
+    def update(self, row):
+        self.values[row["time"]].update(row)
+
+    def serialize(self):
+        return [value.serialize() for value in self.values.values()]
+
+
+class StatMeasure:
+    def __init__(self, quantity=0, times_seen=0):
+        self.quantity = quantity
+        self.times_seen = times_seen
+
+    def serialize(self):
+        pass
+
+
+class MeasureValue:
+    def __init__(self, time):
+        self.accepted = StatMeasure()
+        self.filtered = StatMeasure()
+        self.over_quota = StatMeasure()
+        self.spike_protection = StatMeasure()
+        self.other = StatMeasure()
+        self.time = time
+
+    def serialize(self):
+        return {
+            "accepted": {
+                "quantity": self.accepted.quantity,
+                "times_seen": self.accepted.times_seen,
+            },
+            "filtered": {"quantity": self.filtered.quantity, "times_seen": self.filtered.quantity},
+            "dropped": {
+                "overQuota": {
+                    "quantity": self.over_quota.quantity,
+                    "times_seen": self.over_quota.quantity,
+                },
+                "spikeProtection": {
+                    "quantity": self.spike_protection.quantity,
+                    "times_seen": self.spike_protection.quantity,
+                },
+                "other": {"quantity": self.other.quantity, "times_seen": self.other.quantity},
+            },
+            "time": self.time,
+        }
+
+    def update(self, row):
+        def find_measure():
+            if row["outcome"] == Outcome.RATE_LIMITED:
+                if row["reason"] in {"usage_exceeded", "grace_period"}:
+                    return self.over_quota
+                elif row["reason"] == "smart_rate_limit":
+                    return self.spike_protection, False
+                else:
+                    return self.other
+            elif row["outcome"] == Outcome.ACCEPTED:
+                return self.accepted
+            elif row["outcome"] == Outcome.FILTERED:
+                return self.filtered
+            else:
+                return None  # or raise an error?
+
+        measure_to_update = find_measure()
+        if measure_to_update:
+            measure_to_update.quantity = row["quantity"]
+            measure_to_update.times_seen = row["times_seen"]
+
+
+# TODO: verify what kind of timestamp we return to frontend
+def zerofill(data, start, end, rollup, orderby):
+    rv = {}
+    start = int(to_naive_timestamp(naiveify_datetime(start)) / rollup) * rollup
+    end = (int(to_naive_timestamp(naiveify_datetime(end)) / rollup) * rollup) + rollup
+    data_by_time = {}
+
+    for obj in data:
+        if obj["time"] in data_by_time:
+            data_by_time[obj["time"]].append(obj)
+        else:
+            data_by_time[obj["time"]] = [obj]
+    for key in range(start, end, rollup):
+        if key in data_by_time and len(data_by_time[key]) > 0:
+            rv = rv + data_by_time[key]
+            data_by_time[key] = []
+        else:
+            val = MeasureValue(key)
+            rv[key] = val
+
+    return rv
