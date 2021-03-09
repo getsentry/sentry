619aaf70c603881d2d5fdb8f5eed6c13245b583f
Tony Xiao <txiao@sentry.io>
2021-03-08 22:44:12 -0500

    fix(span-view): Ops breakdown should appear only once
    The ops breakdown in the sidebar for the span view was moved for the performance
    page in #24273 but discover was not updated.

--- src/sentry/static/sentry/app/views/eventsV2/eventDetails/content.tsx
+++ src/sentry/static/sentry/app/views/eventsV2/eventDetails/content.tsx
@@ -250,9 +250,9 @@ class EventDetailsContent extends AsyncComponent<Props, State> {
                     projectId={this.projectId}
                   />
                   <RootSpanStatus event={event} />
+                  <OpsBreakdown event={event} />
                 </React.Fragment>
               )}
-              <OpsBreakdown event={event} />
               <EventVitals event={event} />
               {event.groupID && (
                 <LinkedIssue groupId={event.groupID} eventId={event.eventID} />
