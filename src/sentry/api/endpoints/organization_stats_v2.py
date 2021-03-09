5cc5c816aa5312def24b1bc48b829a508e2de5a1
Josh Ferge <josh.ferge@sentry.io>
2021-03-09 00:37:21 -0800

    add todos

--- src/sentry/api/endpoints/organization_stats_v2.py
+++ src/sentry/api/endpoints/organization_stats_v2.py
@@ -86,6 +86,7 @@ class OrganizationProjectStatsDetails(ProjectEndpoint, OrganizationEndpoint):
         project_ids = list({p.id for p in project_list})
 
         if project.id not in project_ids:
+            # TODO: more info in response here?
             return Response(status=404)
 
         result = outcomes.query(
@@ -105,35 +106,7 @@ class OrganizationProjectStatsDetails(ProjectEndpoint, OrganizationEndpoint):
         return Response(response.build_fields())
 
 
-# TODO: verify what kind of timestamp we return to frontend
-def zerofill(data, start, end, rollup, orderby):
-    rv = {}
-    start = int(to_naive_timestamp(naiveify_datetime(start)) / rollup) * rollup
-    end = (int(to_naive_timestamp(naiveify_datetime(end)) / rollup) * rollup) + rollup
-    data_by_time = {}
-
-    for obj in data:
-        if obj["time"] in data_by_time:
-            data_by_time[obj["time"]].append(obj)
-        else:
-            data_by_time[obj["time"]] = [obj]
-    for key in range(start, end, rollup):
-        if key in data_by_time and len(data_by_time[key]) > 0:
-            rv = rv + data_by_time[key]
-            data_by_time[key] = []
-        else:
-            val = MeasureValue(key)
-            rv[key] = val
-
-    return rv
-
-
-class StatMeasure:
-    def __init__(self, quantity=0, times_seen=0):
-        self.quantity = quantity
-        self.times_seen = times_seen
-
-
+# TODO: good names for these classes
 class StatsResponse:
     def __init__(self, start, end, rollup):
         self.errors = TimeSeriesValues(start, end, rollup)
@@ -163,6 +136,24 @@ class StatsResponse:
 
     def build_fields(self):
         return {CATEGORY_NAME_MAP[category]: values.serialize() for category, values in self}
+
+
+# TODO: perhaps remove this class
+class TimeSeriesValues:
+    def __init__(self, start, end, rollup):
+        self.values = zerofill({}, start, end, rollup, "time")
+
+    def update(self, row):
+        self.values[row["time"]].update(row)
+
+    def serialize(self):
+        return [value.serialize() for value in self.values.values()]
+
+
+class StatMeasure:
+    def __init__(self, quantity=0, times_seen=0):
+        self.quantity = quantity
+        self.times_seen = times_seen
 
 
 class MeasureValue:
@@ -225,12 +216,24 @@ class MeasureValue:
         return reason_val
 
 
-class TimeSeriesValues:
-    def __init__(self, start, end, rollup):
-        self.values = zerofill({}, start, end, rollup, "time")
+# TODO: verify what kind of timestamp we return to frontend
+def zerofill(data, start, end, rollup, orderby):
+    rv = {}
+    start = int(to_naive_timestamp(naiveify_datetime(start)) / rollup) * rollup
+    end = (int(to_naive_timestamp(naiveify_datetime(end)) / rollup) * rollup) + rollup
+    data_by_time = {}
 
-    def update(self, row):
-        self.values[row["time"]].update(row)
+    for obj in data:
+        if obj["time"] in data_by_time:
+            data_by_time[obj["time"]].append(obj)
+        else:
+            data_by_time[obj["time"]] = [obj]
+    for key in range(start, end, rollup):
+        if key in data_by_time and len(data_by_time[key]) > 0:
+            rv = rv + data_by_time[key]
+            data_by_time[key] = []
+        else:
+            val = MeasureValue(key)
+            rv[key] = val
 
-    def serialize(self):
-        return [value.serialize() for value in self.values.values()]
+    return rv
