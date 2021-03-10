1797fafa875e522b7b3c7f58af2e80f70daa1206
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-03-10 13:57:24 -0800

    Refactor endpoints to redirect writes

--- src/sentry/api/endpoints/user_notification_details.py
+++ src/sentry/api/endpoints/user_notification_details.py
@@ -1,12 +1,18 @@
 from collections import defaultdict
-from rest_framework import serializers
+from rest_framework import serializers, status
 from rest_framework.response import Response
 
 from sentry.api.bases.user import UserEndpoint
 from sentry.api.fields.empty_integer import EmptyIntegerField
 from sentry.api.serializers import serialize, Serializer
 from sentry.models import UserOption
-from sentry.notifications.legacy_mappings import USER_OPTION_SETTINGS
+from sentry.models.integration import ExternalProviders
+from sentry.models.notificationsetting import NotificationSetting
+from sentry.notifications.legacy_mappings import (
+    get_option_value_from_int,
+    get_type_from_user_option_settings_key,
+    USER_OPTION_SETTINGS,
+)
 from sentry.notifications.types import UserOptionsSettingsKey
 
 
@@ -61,15 +67,33 @@ class UserNotificationDetailsEndpoint(UserEndpoint):
     def put(self, request, user):
         serializer = UserNotificationDetailsSerializer(data=request.data)
 
-        if serializer.is_valid():
-            for key, value in serializer.validated_data.items():
-                db_key = USER_OPTION_SETTINGS[UserOptionsSettingsKey(key)]["key"]
-                (uo, created) = UserOption.objects.get_or_create(
-                    user=user, key=db_key, project=None, organization=None
-                )
-                # Convert integers and booleans to string representations of ints.
-                uo.update(value=str(int(value)))
-
-            return self.get(request, user)
-        else:
+        if not serializer.is_valid():
             return Response(serializer.errors, status=400)
+
+        for key, value in serializer.validated_data.items():
+            try:
+                key = UserOptionsSettingsKey(key)
+            except ValueError:
+                return Response(
+                    {"detail": "Unknown key: %s." % key},
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
+            if key in [UserOptionsSettingsKey.DEPLOY, UserOptionsSettingsKey.WORKFLOW]:
+                type = get_type_from_user_option_settings_key(key)
+                NotificationSetting.objects.update_settings(
+                    ExternalProviders.EMAIL,
+                    type,
+                    get_option_value_from_int(type, int(value)),
+                    user=user,
+                )
+            else:
+                user_option, _ = UserOption.objects.get_or_create(
+                    key=USER_OPTION_SETTINGS[key]["key"],
+                    user=user,
+                    project=None,
+                    organization=None,
+                )
+                user_option.update(value=str(int(value)))
+
+        return self.get(request, user)
