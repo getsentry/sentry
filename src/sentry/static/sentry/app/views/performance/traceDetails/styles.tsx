00a29e5d072e24bad48fa0bb945086df385923d7
Tony <txiao@sentry.io>
2021-03-11 11:37:59 -0500

    feat(trace-view): Render trace tree (#24377)
    This change adds in the panel where the full trace view will be rendered and
    begins the rendering of the full trace tree on the left. This includes support
    for the resizable divider as well.

---
+++ src/sentry/static/sentry/app/views/performance/traceDetails/styles.tsx
@@ -0,0 +1,33 @@
+import styled from '@emotion/styled';
+
+import {IconChevron} from 'app/icons';
+import space from 'app/styles/space';
+
+export {
+  ConnectorBar,
+  DividerLine,
+  DividerLineGhostContainer,
+  SpanBarTitle as TransactionBarTitle,
+  SpanBarTitleContainer as TransactionBarTitleContainer,
+  SpanRowCell as TransactionRowCell,
+  SpanRowCellContainer as TransactionRowCellContainer,
+  SpanTreeConnector as TransactionTreeConnector,
+  SpanTreeToggler as TransactionTreeToggle,
+  SpanTreeTogglerContainer as TransactionTreeToggleContainer,
+} from 'app/components/events/interfaces/spans/spanBar';
+export {
+  SPAN_ROW_HEIGHT as TRANSACTION_ROW_HEIGHT,
+  SPAN_ROW_PADDING as TRANSACTION_ROW_PADDING,
+  SpanRow as TransactionRow,
+} from 'app/components/events/interfaces/spans/styles';
+
+export const TraceViewContainer = styled('div')`
+  overflow-x: hidden;
+  border-bottom-left-radius: 3px;
+  border-bottom-right-radius: 3px;
+`;
+
+export const StyledIconChevron = styled(IconChevron)`
+  width: 7px;
+  margin-left: ${space(0.25)};
+`;
