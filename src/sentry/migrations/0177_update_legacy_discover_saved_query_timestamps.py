0cd755830a44fd1ffa6bc81f1f9906937ec5031e
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-03-12 11:09:26 -0500

    set atomic=False

--- src/sentry/migrations/0177_update_legacy_discover_saved_query_timestamps.py
+++ src/sentry/migrations/0177_update_legacy_discover_saved_query_timestamps.py
@@ -54,7 +54,7 @@ class Migration(migrations.Migration):
     # You'll also usually want to set this to `False` if you're writing a data
     # migration, since we don't want the entire migration to run in one long-running
     # transaction.
-    atomic = True
+    atomic = False
 
     dependencies = [
         ("sentry", "0176_remove_targets"),
