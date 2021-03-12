0a73f79e9a3080562f5b52117eac93a48d0a5d34
David Wang <dwang@sentry.io>
2021-03-12 15:29:42 -0500

    add team selector to metric alerts with proper permissions

---
+++ src/sentry/static/sentry/app/views/settings/incidentRules/ruleNameOwnerForm.tsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import {Panel, PanelBody, PanelHeader} from 'app/components/panels';
+import SelectMembers from 'app/components/selectMembers';
+import {t} from 'app/locale';
+import {Organization, Project} from 'app/types';
+import FormField from 'app/views/settings/components/forms/formField';
+import TextField from 'app/views/settings/components/forms/textField';
+
+type Props = {
+  disabled: boolean;
+  project: Project;
+  organization: Organization;
+  userTeamIds: Set<string>;
+};
+
+class RuleNameOwnerForm extends React.PureComponent<Props> {
+  render() {
+    const {disabled, project, organization, userTeamIds} = this.props;
+
+    return (
+      <Panel>
+        <PanelHeader>{t('Give your rule a name')}</PanelHeader>
+        <PanelBody>
+          <TextField
+            disabled={disabled}
+            name="name"
+            label={t('Rule Name')}
+            help={t('Give your rule a name so it is easy to manage later')}
+            placeholder={t('Something really bad happened')}
+            required
+          />
+          <FormField
+            name="owner"
+            label={t('Team')}
+            help={t('The team that owns this alert')}
+          >
+            {({model}) => {
+              const owner = model.getValue('owner');
+              const ownerId = owner && owner.split(':')[1];
+              return (
+                <SelectMembers
+                  showTeam
+                  project={project}
+                  organization={organization}
+                  value={ownerId}
+                  onChange={({value}) => {
+                    const ownerValue = value && `team:${value}`;
+                    model.setValue('owner', ownerValue);
+                  }}
+                  filteredTeamIds={userTeamIds}
+                  includeUnassigned
+                />
+              );
+            }}
+          </FormField>
+        </PanelBody>
+      </Panel>
+    );
+  }
+}
+
+export default RuleNameOwnerForm;
