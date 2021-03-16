badc4f8c0dfbac7ec7115e776163a7f550e5a05d
Stephen Cefali <scefali@sentry.io>
2021-03-16 08:49:45 -0700

    feat(demo): adds cta header for demo (#24379)

--- src/sentry/static/sentry/app/main.tsx
+++ src/sentry/static/sentry/app/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import {browserHistory, Router} from 'react-router';
 
+import DemoHeader from 'app/components/demo/demoHeader';
 import routes from 'app/routes';
+import ConfigStore from 'app/stores/configStore';
 
 import ThemeAndStyleProvider from './themeAndStyleProvider';
 
 export default function Main() {
   return (
     <ThemeAndStyleProvider>
+      {ConfigStore.get('demoMode') && <DemoHeader />}
       <Router history={browserHistory}>{routes()}</Router>
     </ThemeAndStyleProvider>
   );
