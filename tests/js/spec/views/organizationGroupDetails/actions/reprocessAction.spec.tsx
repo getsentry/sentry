d1b4d056b18730752c9bcb5bc52da665af306f17
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-03-18 10:52:36 +0100

    add reprocess action button tests

---
+++ tests/js/spec/views/organizationGroupDetails/actions/reprocessAction.spec.tsx
@@ -0,0 +1,176 @@
+import React from 'react';
+
+import {mountWithTheme} from 'sentry-test/enzyme';
+
+import ReprocessAction from 'app/views/organizationGroupDetails/actions/reprocessAction';
+
+describe('ReprocessAction', function () {
+  it('returns null in case of no event', function () {
+    const wrapper = mountWithTheme(
+      <ReprocessAction disabled={false} onClick={jest.fn()} event={undefined} />
+    );
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+
+  it('returns null if no exception entry is found', function () {
+    // @ts-expect-error
+    const event = TestStubs.EventStacktraceMessage();
+
+    const wrapper = mountWithTheme(
+      <ReprocessAction disabled={false} onClick={jest.fn()} event={event} />
+    );
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+
+  it('returns null if the event is not a mini-dump event or an Apple crash report event or a Native event', function () {
+    // @ts-expect-error
+    const event = TestStubs.EventStacktraceException();
+
+    const wrapper = mountWithTheme(
+      <ReprocessAction disabled={false} onClick={jest.fn()} event={event} />
+    );
+    expect(wrapper.isEmptyRender()).toBe(true);
+  });
+
+  describe('returns the ActionButton element', function () {
+    const onClick = jest.fn();
+
+    describe('native event', function () {
+      describe('event with defined platform', function () {
+        it('native', function () {
+          // @ts-expect-error
+          const event = TestStubs.EventStacktraceException({
+            platform: 'native',
+          });
+
+          const wrapper = mountWithTheme(
+            <ReprocessAction disabled={false} onClick={onClick} event={event} />
+          );
+
+          const actionButton = wrapper.find('ActionButton');
+          expect(actionButton).toBeTruthy();
+
+          actionButton.simulate('click');
+          expect(onClick).toHaveBeenCalled();
+
+          expect(wrapper.isEmptyRender()).toBe(false);
+        });
+
+        it('cocoa', function () {
+          // @ts-expect-error
+          const event = TestStubs.EventStacktraceException({
+            platform: 'cocoa',
+          });
+
+          const wrapper = mountWithTheme(
+            <ReprocessAction disabled={false} onClick={onClick} event={event} />
+          );
+
+          const actionButton = wrapper.find('ActionButton');
+          expect(actionButton).toBeTruthy();
+
+          actionButton.simulate('click');
+          expect(onClick).toHaveBeenCalled();
+
+          expect(wrapper.isEmptyRender()).toBe(false);
+        });
+      });
+
+      describe('event with undefined platform, but stack trace has platform', function () {
+        it('native', function () {
+          // @ts-expect-error
+          const event = TestStubs.EventStacktraceException({
+            platform: undefined,
+          });
+
+          event.entries[0].data.values[0].stacktrace.frames[0].platform = 'native';
+
+          const wrapper = mountWithTheme(
+            <ReprocessAction disabled={false} onClick={onClick} event={event} />
+          );
+
+          const actionButton = wrapper.find('ActionButton');
+          expect(actionButton).toBeTruthy();
+
+          actionButton.simulate('click');
+          expect(onClick).toHaveBeenCalled();
+
+          expect(wrapper.isEmptyRender()).toBe(false);
+        });
+
+        it('cocoa', function () {
+          // @ts-expect-error
+          const event = TestStubs.EventStacktraceException({
+            platform: undefined,
+          });
+
+          event.entries[0].data.values[0].stacktrace.frames[0].platform = 'cocoa';
+
+          const wrapper = mountWithTheme(
+            <ReprocessAction disabled={false} onClick={onClick} event={event} />
+          );
+
+          const actionButton = wrapper.find('ActionButton');
+          expect(actionButton).toBeTruthy();
+
+          actionButton.simulate('click');
+          expect(onClick).toHaveBeenCalled();
+
+          expect(wrapper.isEmptyRender()).toBe(false);
+        });
+      });
+    });
+
+    it('mini-dump event', function () {
+      // @ts-expect-error
+      const event = TestStubs.EventStacktraceException({
+        platform: undefined,
+      });
+
+      event.entries[0].data.values[0] = {
+        ...event.entries[0].data.values[0],
+        mechanism: {
+          type: 'minidump',
+        },
+      };
+
+      const wrapper = mountWithTheme(
+        <ReprocessAction disabled={false} onClick={onClick} event={event} />
+      );
+
+      const actionButton = wrapper.find('ActionButton');
+      expect(actionButton).toBeTruthy();
+
+      actionButton.simulate('click');
+      expect(onClick).toHaveBeenCalled();
+
+      expect(wrapper.isEmptyRender()).toBe(false);
+    });
+
+    it('apple crash report event', function () {
+      // @ts-expect-error
+      const event = TestStubs.EventStacktraceException({
+        platform: undefined,
+      });
+
+      event.entries[0].data.values[0] = {
+        ...event.entries[0].data.values[0],
+        mechanism: {
+          type: 'applecrashreport',
+        },
+      };
+
+      const wrapper = mountWithTheme(
+        <ReprocessAction disabled={false} onClick={onClick} event={event} />
+      );
+
+      const actionButton = wrapper.find('ActionButton');
+      expect(actionButton).toBeTruthy();
+
+      actionButton.simulate('click');
+      expect(onClick).toHaveBeenCalled();
+
+      expect(wrapper.isEmptyRender()).toBe(false);
+    });
+  });
+});
