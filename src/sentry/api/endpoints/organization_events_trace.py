f7b515e973982785eb3edac6f0e821bc407be169
William Mak <william@wmak.io>
2021-03-19 15:12:10 -0400

    feat(trace-view): Include orphan traces in the trace-view  (#24538)
    * chore(trace-view): Clean up and more sentry data
    
    - Adding a grouped num_transactions so we haev a better sense of
      trace size -> performance
    - Updating the query referrers to be more consistent with - vs _ usage
    
    * feat(trace-view): Include orphan traces in the trace-view
    
    - This adds multiple traces to the response of the trace-view endpoint
    - The frontend currently grabs the 0th element of the response to build
      the quick trace + trace summary, to prevent potential issues when
      transistioning to this extra data had to use an ordered dict for the
      results map

--- src/sentry/api/endpoints/organization_events_trace.py
+++ src/sentry/api/endpoints/organization_events_trace.py
@@ -1,7 +1,7 @@
 import logging
 import sentry_sdk
 
-from collections import defaultdict, deque
+from collections import defaultdict, deque, OrderedDict
 
 from rest_framework.response import Response
 
@@ -87,11 +87,18 @@ class OrganizationEventsTraceEndpointBase(OrganizationEventsV2EndpointBase):
                 orderby=["-root", "-timestamp", "id"],
                 params=params,
                 query=f"event.type:transaction trace:{trace_id}",
-                limit=MAX_TRACE_SIZE,
-                referrer="api.trace-view.get_ids",
+                # get 1 more so we know if the attempted trace was over 100
+                limit=MAX_TRACE_SIZE + 1,
+                referrer="api.trace-view.get-ids",
             )
             if len(result["data"]) == 0:
                 return Response(status=404)
+            len_transactions = len(result["data"])
+            sentry_sdk.set_tag("trace.num_transactions", len_transactions)
+            sentry_sdk.set_tag(
+                "trace.num_transactions.grouped",
+                "<10" if len_transactions < 10 else "<100" if len_transactions < 100 else ">100",
+            )
 
         warning_extra = {"trace": trace_id, "organization": organization}
 
@@ -104,6 +111,7 @@ class OrganizationEventsTraceEndpointBase(OrganizationEventsV2EndpointBase):
             snuba_event = None
 
         if not is_root(result["data"][0]):
+            sentry_sdk.set_tag("discover.trace-view.warning", "root.not-found")
             logger.warning(
                 "discover.trace-view.root.not-found",
                 extra=warning_extra,
@@ -120,6 +128,7 @@ class OrganizationEventsTraceEndpointBase(OrganizationEventsV2EndpointBase):
             else:
                 break
         if extra_roots > 0:
+            sentry_sdk.set_tag("discover.trace-view.warning", "root.extra-found")
             logger.warning(
                 "discover.trace-view.root.extra-found",
                 {"extra_roots": extra_roots, **warning_extra},
@@ -127,7 +136,8 @@ class OrganizationEventsTraceEndpointBase(OrganizationEventsV2EndpointBase):
 
         parent_map = defaultdict(list)
         for item in result["data"]:
-            parent_map[item["trace.parent_span"]].append(item)
+            if not is_root(item):
+                parent_map[item["trace.parent_span"]].append(item)
 
         # Temporarily feature flagging this out, since errors will impact performance
         if not features.has("organizations:trace-view-summary", organization, actor=request.user):
@@ -220,7 +230,7 @@ class OrganizationEventsTraceEndpoint(OrganizationEventsTraceEndpointBase):
                 limit=MAX_TRACE_SIZE,
                 # we can get project from the associated transaction, which can save us a db query
                 auto_fields=False,
-                referrer="api.trace-view.get_errors",
+                referrer="api.trace-view.get-errors",
             )
 
             # Use issue ids to get the error's short id
@@ -269,6 +279,16 @@ class OrganizationEventsTraceEndpoint(OrganizationEventsTraceEndpointBase):
             for [tag_key, tag_value] in nodestore_data.get("tags"):
                 event["tags"][tag_key] = tag_value
 
+    def update_generations(self, event):
+        parents = [event]
+        iteration = 0
+        while parents and iteration < MAX_TRACE_SIZE:
+            iteration += 1
+            parent = parents.pop()
+            for child in parent["children"]:
+                child["generation"] = parent["generation"] + 1
+                parents.append(child)
+
     def serialize(
         self,
         parent_map,
@@ -282,13 +302,40 @@ class OrganizationEventsTraceEndpoint(OrganizationEventsTraceEndpointBase):
     ):
         """ For the full event trace, we return the results as a graph instead of a flattened list """
         parent_events = {}
-        result = parent_events[root["id"]] = self.serialize_event(root, None, 0)
+        parent_events[root["id"]] = self.serialize_event(root, None, 0)
+        # TODO(wmak): Dictionary ordering in py3.6 is an implementation detail, using an OrderedDict because this way
+        # we can guarantee in py3.6 that the first item is the root
+        # So we can switch back to a normal dict when either the frontend doesn't depend on the root being the first
+        # element, or if we're on python 3.7
+        results_map = OrderedDict({None: [parent_events[root["id"]]]})
 
         with sentry_sdk.start_span(op="building.trace", description="full trace"):
             to_check = deque([root])
             iteration = 0
-            while to_check:
-                current_event = to_check.popleft()
+            has_orphans = False
+            while parent_map or to_check:
+                if len(to_check) == 0:
+                    has_orphans = True
+                    # Grab any set of events from the parent map
+                    parent_span_id, current_events = parent_map.popitem()
+
+                    current_event, *siblings = current_events
+                    # If there were any siblings put them back
+                    if siblings:
+                        parent_map[parent_span_id] = siblings
+
+                    previous_event = parent_events[current_event["id"]] = self.serialize_event(
+                        current_event, None, 0
+                    )
+
+                    # not using a defaultdict here as a DefaultOrderedDict isn't worth the effort
+                    if parent_span_id in results_map:
+                        results_map[parent_span_id].append(previous_event)
+                    else:
+                        results_map[parent_span_id] = [previous_event]
+                else:
+                    current_event = to_check.popleft()
+                    previous_event = parent_events[current_event["id"]]
 
                 # This is faster than doing a call to get_events, since get_event_by_id only makes a call to snuba
                 # when non transaction events are included.
@@ -297,7 +344,6 @@ class OrganizationEventsTraceEndpoint(OrganizationEventsTraceEndpointBase):
                         current_event["project.id"], current_event["id"]
                     )
 
-                previous_event = parent_events[current_event["id"]]
                 self.update_event_extra(previous_event, nodestore_event.data, detailed)
 
                 spans = nodestore_event.data.get("spans", [])
@@ -307,6 +353,9 @@ class OrganizationEventsTraceEndpoint(OrganizationEventsTraceEndpointBase):
                 for child in spans:
                     if child["span_id"] in error_map:
                         previous_event["errors"].extend(error_map.pop(child["span_id"]))
+                    # We need to connect back to an existing orphan trace
+                    if has_orphans and child["span_id"] in results_map:
+                        previous_event["children"].extend(results_map.pop(child["span_id"]))
                     if child["span_id"] not in parent_map:
                         continue
                     # Avoid potential span loops by popping, so we don't traverse the same nodes twice
@@ -323,12 +372,18 @@ class OrganizationEventsTraceEndpoint(OrganizationEventsTraceEndpointBase):
                 # Limit iterations just to be safe
                 iteration += 1
                 if iteration > MAX_TRACE_SIZE:
+                    sentry_sdk.set_tag("discover.trace-view.warning", "surpassed-trace-limit")
                     logger.warning(
                         "discover.trace-view.surpassed-trace-limit",
                         extra=warning_extra,
                     )
                     break
 
-        # TODO(wmak): return an array of traces with the same trace, which includes orphan traces (ie. not connected
-        # with the root)
-        return [result]
+        results = []
+        for result in results_map.values():
+            # Only need to update generation values when there are orphans since otherwise we stepped through in order
+            if has_orphans:
+                for root in result:
+                    self.update_generations(root)
+            results.extend(result)
+        return results
