ec3d00b73b1bfb10101b6ed6d8067d4a721cbd66
Alex Xu @ Sentry <alex.xu@sentry.io>
2021-03-20 09:44:14 -0400

    fix/ref: admin dark mode fixes and refactor
    Admin page also unnecessarily duplicates many settings components which can be
    reused, so refactor it.
    
    Some pages still aren't perfect but it should be an improvement.

--- src/sentry/static/sentry/app/views/admin/adminLayout.tsx
+++ src/sentry/static/sentry/app/views/admin/adminLayout.tsx
@@ -1,64 +1,64 @@
 import React from 'react';
 import DocumentTitle from 'react-document-title';
+import {RouteComponentProps} from 'react-router';
 import styled from '@emotion/styled';
 
-import space from 'app/styles/space';
+import SettingsLayout from 'app/views/settings/components/settingsLayout';
 import SettingsNavigation from 'app/views/settings/components/settingsNavigation';
 
-const AdminLayout: React.FC = ({children}) => (
-  <DocumentTitle title="Sentry Admin">
-    <Page>
-      <NavWrapper>
-        <SettingsNavigation
-          stickyTop="0"
-          navigationObjects={[
-            {
-              name: 'System Status',
-              items: [
-                {path: '/manage/', index: true, title: 'Overview'},
-                {path: '/manage/buffer/', title: 'Buffer'},
-                {path: '/manage/queue/', title: 'Queue'},
-                {path: '/manage/quotas/', title: 'Quotas'},
-                {path: '/manage/status/environment/', title: 'Environment'},
-                {path: '/manage/status/packages/', title: 'Packages'},
-                {path: '/manage/status/mail/', title: 'Mail'},
-                {path: '/manage/status/warnings/', title: 'Warnings'},
-                {path: '/manage/settings/', title: 'Settings'},
-              ],
-            },
-            {
-              name: 'Manage',
-              items: [
-                {path: '/manage/organizations/', title: 'Organizations'},
-                {path: '/manage/projects/', title: 'Projects'},
-                {path: '/manage/users/', title: 'Users'},
-              ],
-            },
-          ]}
-        />
-      </NavWrapper>
-      <Content>{children}</Content>
-    </Page>
-  </DocumentTitle>
+const AdminNavigation = () => (
+  <SettingsNavigation
+    stickyTop="0"
+    navigationObjects={[
+      {
+        name: 'System Status',
+        items: [
+          {path: '/manage/', index: true, title: 'Overview'},
+          {path: '/manage/buffer/', title: 'Buffer'},
+          {path: '/manage/queue/', title: 'Queue'},
+          {path: '/manage/quotas/', title: 'Quotas'},
+          {path: '/manage/status/environment/', title: 'Environment'},
+          {path: '/manage/status/packages/', title: 'Packages'},
+          {path: '/manage/status/mail/', title: 'Mail'},
+          {path: '/manage/status/warnings/', title: 'Warnings'},
+          {path: '/manage/settings/', title: 'Settings'},
+        ],
+      },
+      {
+        name: 'Manage',
+        items: [
+          {path: '/manage/organizations/', title: 'Organizations'},
+          {path: '/manage/projects/', title: 'Projects'},
+          {path: '/manage/users/', title: 'Users'},
+        ],
+      },
+    ]}
+  />
 );
 
-const NavWrapper = styled('div')`
-  flex-shrink: 0;
-  flex-grow: 0;
-  width: ${p => p.theme.settings.sidebarWidth};
-  background: ${p => p.theme.white};
-  border-right: 1px solid ${p => p.theme.border};
-`;
+type Props = {
+  children: React.ReactNode;
+} & RouteComponentProps<{}, {}>;
+
+class AdminLayout extends React.Component<Props> {
+  render() {
+    const {children, ...props} = this.props;
+    return (
+      <DocumentTitle title="Sentry Admin">
+        <Page>
+          <SettingsLayout renderNavigation={AdminNavigation} {...props}>
+            {children}
+          </SettingsLayout>
+        </Page>
+      </DocumentTitle>
+    );
+  }
+}
 
 const Page = styled('div')`
   display: flex;
   flex-grow: 1;
   margin-bottom: -20px;
-`;
-
-const Content = styled('div')`
-  flex-grow: 1;
-  padding: ${space(4)};
 `;
 
 export default AdminLayout;
