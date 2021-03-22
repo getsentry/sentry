1fe73c96e52abdd6c38c6c7fd36e906643bf5ba4
Matej Minar <matej.minar@sentry.io>
2021-03-22 10:24:20 +0100

    fallback to notAvailable

--- src/sentry/static/sentry/app/views/releases/list/releaseHealth/content.tsx
+++ src/sentry/static/sentry/app/views/releases/list/releaseHealth/content.tsx
@@ -121,8 +121,11 @@ const Content = ({
               id,
               activeDisplay
             );
-
             const adoption = getHealthData.getAdoption(releaseVersion, id, activeDisplay);
+            // we currently don't support sub-hour session intervals, we rather hide the count histogram than to show only two bars
+            const hasCountHistogram =
+              timeSeries?.[0].data.length > 7 &&
+              timeSeries[0].data.some(item => item.value > 0);
 
             return (
               <ProjectRow key={`${releaseVersion}-${slug}-health`}>
@@ -162,8 +165,7 @@ const Content = ({
                   <CountColumn>
                     {showPlaceholders ? (
                       <StyledPlaceholder />
-                    ) : // we currently don't support sub-hour session intervals, we rather hide the count histogram than to show only two bars
-                    timeSeries && timeSeries[0].data.length > 7 ? (
+                    ) : hasCountHistogram ? (
                       <ChartWrapper>
                         <HealthStatsChart
                           data={timeSeries}
