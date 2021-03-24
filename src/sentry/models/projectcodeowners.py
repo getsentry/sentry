143e521f2ac15482c93e95a455ddf4d1253c54e0
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2021-03-24 09:51:28 -0700

    feat(codeowners): Evaluate CodeOwner rules alongside IssuerOwner rules (#24409)
    Objective:
    We need to evaluate our CodeOwner rules wherever the Issuer Owner rules are currently being evaluated. The code is architected such that if we update the ProjectOwnership Model get_autoassign_owners and get_owners functions to include the CodeOwner rules, it updates everywhere they are called (EventOwnersEndpoint, post process task, etc.)

--- src/sentry/models/projectcodeowners.py
+++ src/sentry/models/projectcodeowners.py
@@ -1,15 +1,13 @@
 import logging
 
 from django.db import models
-from sentry.db.models import (
-    FlexibleForeignKey,
-    DefaultFieldsModel,
-    JSONField,
-)
 from django.utils import timezone
 
+from sentry.db.models import FlexibleForeignKey, DefaultFieldsModel, JSONField, sane_repr
+from sentry.utils.cache import cache
 
 logger = logging.getLogger(__name__)
+READ_CACHE_DURATION = 3600
 
 
 class ProjectCodeOwners(DefaultFieldsModel):
@@ -30,3 +28,28 @@ class ProjectCodeOwners(DefaultFieldsModel):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_projectcodeowners"
+
+    __repr__ = sane_repr("project_id", "id")
+
+    @classmethod
+    def get_cache_key(self, project_id):
+        return f"projectcodewoners_project_id:1:{project_id}"
+
+    @classmethod
+    def get_codeowners_cached(self, project_id):
+        """
+        Cached read access to sentry_projectcodeowners.
+
+        This method implements a negative cache which saves us
+        a pile of read queries in post_processing as most projects
+        don't have CODEOWNERS.
+        """
+        cache_key = self.get_cache_key(project_id)
+        codeowners = cache.get(cache_key)
+        if codeowners is None:
+            try:
+                codeowners = self.objects.get(project_id=project_id)
+            except self.DoesNotExist:
+                codeowners = False
+            cache.set(cache_key, codeowners, READ_CACHE_DURATION)
+        return codeowners or None
