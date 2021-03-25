795bff3208e63d99e91558bdd898194f5fbff85f
Alberto Leal <mail4alberto@gmail.com>
2021-03-25 09:17:34 -0400

    feat: Return default operation breakdown list from project options (#24025)

--- tests/sentry/relay/test_config.py
+++ tests/sentry/relay/test_config.py
@@ -28,27 +28,31 @@ PII_CONFIG = """
 
 
 @pytest.mark.django_db
-@pytest.mark.parametrize("full", [False, True])
-def test_get_project_config(default_project, insta_snapshot, full):
+@pytest.mark.parametrize("full", [False, True], ids=["slim_config", "full_config"])
+@pytest.mark.parametrize(
+    "has_ops_breakdown", [False, True], ids=["ops_breakdown", "no_ops_breakdown"]
+)
+def test_get_project_config(default_project, insta_snapshot, full, has_ops_breakdown):
     # We could use the default_project fixture here, but we would like to avoid 1) hitting the db 2) creating a mock
     default_project.update_option("sentry:relay_pii_config", PII_CONFIG)
     default_project.organization.update_option("sentry:relay_pii_config", PII_CONFIG)
     keys = ProjectKey.objects.filter(project=default_project)
 
-    cfg = get_project_config(default_project, full_config=full, project_keys=keys)
-    cfg = cfg.to_dict()
+    with Feature({"organizations:performance-ops-breakdown": has_ops_breakdown}):
+        cfg = get_project_config(default_project, full_config=full, project_keys=keys)
+        cfg = cfg.to_dict()
 
-    # Remove keys that change everytime
-    cfg.pop("lastChange")
-    cfg.pop("lastFetch")
-    cfg.pop("rev")
+        # Remove keys that change everytime
+        cfg.pop("lastChange")
+        cfg.pop("lastFetch")
+        cfg.pop("rev")
 
-    # public keys change every time
-    assert cfg.pop("projectId") == default_project.id
-    assert len(cfg.pop("publicKeys")) == len(keys)
-    assert cfg.pop("organizationId") == default_project.organization.id
+        # public keys change every time
+        assert cfg.pop("projectId") == default_project.id
+        assert len(cfg.pop("publicKeys")) == len(keys)
+        assert cfg.pop("organizationId") == default_project.organization.id
 
-    insta_snapshot(cfg)
+        insta_snapshot(cfg)
 
 
 @pytest.mark.django_db
