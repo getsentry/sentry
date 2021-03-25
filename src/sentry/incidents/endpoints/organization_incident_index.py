3ece09bf04658952a86435d478febab63916ce98
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2021-03-25 16:19:56 -0700

    feat(alerts): Add incident activity to alert rule details (#24085)
    * feat(alerts): Add incident activity to alert rule details
    
    * remove activities fetch from FE
    
    * lint changes
    
    * correct thresholds etc
    
    * style(lint): Auto commit lint changes
    
    * fix color
    
    * rename rich serializer
    
    * move cheat data into metric chart
    
    * render metric chart in events request
    
    * rebase
    
    * new rule serializer
    
    * one serializer
    
    * style(lint): Auto commit lint changes
    
    * style(lint): Auto commit lint changes
    
    * lint
    
    * expand serializer
    
    * style(lint): Auto commit lint changes
    
    * style(lint): Auto commit lint changes
    
    * expand all
    
    * style(lint): Auto commit lint changes
    
    * better serialization
    
    * serialize list
    
    * serialize seen by
    
    * style(lint): Auto commit lint changes
    
    * super expand for detailed serializer
    
    * addressing comments
    
    * style(lint): Auto commit lint changes
    
    * style(lint): Auto commit lint changes
    
    * sort seen by
    
    * style(lint): Auto commit lint changes
    
    * fix test
    
    * style(lint): Auto commit lint changes
    
    Co-authored-by: sentry-internal-tools[bot] <66042841+sentry-internal-tools[bot]@users.noreply.github.com>

--- src/sentry/incidents/endpoints/organization_incident_index.py
+++ src/sentry/incidents/endpoints/organization_incident_index.py
@@ -4,7 +4,7 @@ from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.paginator import OffsetPaginator
 from sentry.api.serializers import serialize
-from sentry.api.serializers.models.incident import DetailedIncidentSerializer
+from sentry.api.serializers.models.incident import IncidentSerializer
 from sentry.incidents.models import Incident, IncidentStatus
 from sentry.snuba.dataset import Dataset
 
@@ -60,19 +60,13 @@ class OrganizationIncidentIndexEndpoint(OrganizationEndpoint):
             # Filter to only error alerts
             incidents = incidents.filter(alert_rule__snuba_query__dataset=Dataset.Events.value)
 
-        query_detailed = request.GET.get("detailed")
-
-        def serialize_results(results):
-            if query_detailed:
-                return serialize(results, request.user, DetailedIncidentSerializer())
-            else:
-                return serialize(results, request.user)
+        expand = request.GET.getlist("expand", [])
 
         return self.paginate(
             request,
             queryset=incidents,
             order_by="-date_started",
             paginator_cls=OffsetPaginator,
-            on_results=serialize_results,
+            on_results=lambda x: serialize(x, request.user, IncidentSerializer(expand=expand)),
             default_per_page=25,
         )
