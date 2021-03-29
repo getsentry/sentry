a22dec1e16bd974bc5a4b1c3343ae31660a87499
Floris Bruynooghe <flub@sentry.io>
2021-03-29 13:20:58 +0200

    test(symbolication): Add tests for DIF candidates (#23325)
    This adds an integration test which checks DIF candidates can be
    requested from symbolicator.  The test uses a DIF on the sentry:project
    source which also asserts the custom URI scheme for these which the UI
    relies on.

--- tests/symbolicator/test_payload_full.py
+++ tests/symbolicator/test_payload_full.py
@@ -1,3 +1,5 @@
+import re
+
 import pytest
 import zipfile
 from sentry.utils.compat.mock import patch
@@ -185,3 +187,78 @@ class SymbolicatorResolvingIntegrationTest(RelayStoreHelper, TransactionTestCase
         event = self.post_and_retrieve_event(payload)
         assert event.data["culprit"] == "unknown"
         insta_snapshot_stacktrace_data(self, event.data)
+
+    def test_resolving_with_candidates_sentry_source(self):
+        # Checks the candidates with a sentry source URI for location
+        file = File.objects.create(
+            name="crash.pdb", type="default", headers={"Content-Type": "text/x-breakpad"}
+        )
+
+        path = get_fixture_path("windows.sym")
+        with open(path, "rb") as f:
+            file.putfile(f)
+
+        ProjectDebugFile.objects.create(
+            file=file,
+            object_name="crash.pdb",
+            cpu_name="x86",
+            project=self.project,
+            debug_id="3249d99d-0c40-4931-8610-f4e4fb0b6936-1",
+            code_id="5AB380779000",
+        )
+
+        self.login_as(user=self.user)
+
+        event_data = {
+            "contexts": {
+                "device": {"arch": "x86"},
+            },
+            "debug_meta": {
+                "images": [
+                    {
+                        "id": "3249d99d-0c40-4931-8610-f4e4fb0b6936-1",
+                        "image_addr": "0x2a0000",
+                        "image_size": 36864,
+                        "name": "C:\\projects\\breakpad-tools\\windows\\Release\\crash.exe",
+                        "type": "symbolic",
+                    }
+                ]
+            },
+            "exception": {
+                "stacktrace": {
+                    "frames": [
+                        {
+                            "instruction_addr": "0x2a2a3d",
+                        }
+                    ]
+                },
+                "type": "EXCEPTION_ACCESS_VIOLATION_WRITE",
+                "value": "Fatal Error: EXCEPTION_ACCESS_VIOLATION_WRITE",
+            },
+            "platform": "native",
+            "timestamp": iso_format(before_now(seconds=1)),
+        }
+
+        with self.feature("organizations:images-loaded-v2"):
+            event = self.post_and_retrieve_event(event_data)
+        assert event.data["culprit"] == "main"
+
+        candidates = event.data["debug_meta"]["images"][0]["candidates"]
+        redact_location(candidates)
+        self.insta_snapshot(candidates)
+
+
+def redact_location(candidates):
+    """Redacts the sentry location URI to be independent of the specific ID.
+
+    This modifies the data passed in, returns None.
+    """
+    location_re = re.compile("^sentry://project_debug_file/[0-9]+$")
+    for candidate in candidates:
+        try:
+            location = candidate["location"]
+        except KeyError:
+            continue
+        else:
+            if location_re.search(location):
+                candidate["location"] = "sentry://project_debug_file/x"
