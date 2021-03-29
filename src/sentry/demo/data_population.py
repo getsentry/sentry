18b5d4ce21a59a7684fa46e1316bd0c3f6863637
Stephen Cefali <scefali@sentry.io>
2021-03-29 11:12:45 -0700

    make context more realistic

--- src/sentry/demo/data_population.py
+++ src/sentry/demo/data_population.py
@@ -148,15 +148,8 @@ def get_list_of_names() -> List[str]:
 
 
 @functools.lru_cache(maxsize=None)
-def get_list_of_browsers():
-    file_path = get_data_file_path("browser_list.json")
-    with open(file_path) as f:
-        return json.load(f)
-
-
-@functools.lru_cache(maxsize=None)
-def get_list_of_os():
-    file_path = get_data_file_path("os_list.json")
+def get_list_of_base_contexts():
+    file_path = get_data_file_path("contexts.json")
     with open(file_path) as f:
         return json.load(f)
 
@@ -198,14 +191,8 @@ def gen_base_context():
     """
     Generates a base context from pure randomness
     """
-    browser_list = get_list_of_browsers()
-    os_list = get_list_of_os()
-    browser = random.choice(browser_list)
-    os = random.choice(os_list)
-    return {
-        "browser": browser,
-        "os": os,
-    }
+    contexts = get_list_of_base_contexts()
+    return random.choice(contexts)
 
 
 def get_release_from_time(org_id, timestamp):
