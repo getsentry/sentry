a378c5a7693a93616d6f0b5524f000604446f610
Danny Lee <dlee@sentry.io>
2021-03-29 15:16:36 -0700

    fix(pr)

--- src/sentry/static/sentry/app/components/clippedBox.tsx
+++ src/sentry/static/sentry/app/components/clippedBox.tsx
@@ -180,10 +180,10 @@ const ClipFade = styled('div')`
   text-align: center;
   border-bottom: ${space(1.5)} solid ${p => p.theme.background};
 
-  // Let pointer-events pass through ClipFade to visible elements underneath it
+  /* Let pointer-events pass through ClipFade to visible elements underneath it */
   pointer-events: none;
 
-  // Ensure pointer-events trigger event listeners on ClipFade's child elements
+  /* Ensure pointer-events trigger event listeners on "Expand" button */
   > * {
     pointer-events: auto;
   }
