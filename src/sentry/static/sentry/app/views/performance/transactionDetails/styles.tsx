576d5b93bd4206323c54d66678d6b0b2788d7ed1
William Mak <william@wmak.io>
2021-03-30 19:38:47 -0400

    feat(trace-view): Add beta badges (#24797)
    - in preparation for EAing the trace view add beta badges

--- src/sentry/static/sentry/app/views/performance/transactionDetails/styles.tsx
+++ src/sentry/static/sentry/app/views/performance/transactionDetails/styles.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styled from '@emotion/styled';
 
 import {SectionHeading} from 'app/components/charts/styles';
+import FeatureBadge from 'app/components/featureBadge';
 import QuestionTooltip from 'app/components/questionTooltip';
 import space from 'app/styles/space';
 
@@ -10,9 +11,16 @@ type MetaDataProps = {
   tooltipText: string;
   bodyText: React.ReactNode;
   subtext: React.ReactNode;
+  beta?: boolean;
 };
 
-export function MetaData({headingText, tooltipText, bodyText, subtext}: MetaDataProps) {
+export function MetaData({
+  headingText,
+  tooltipText,
+  bodyText,
+  subtext,
+  beta,
+}: MetaDataProps) {
   return (
     <HeaderInfo>
       <StyledSectionHeading>
@@ -23,6 +31,7 @@ export function MetaData({headingText, tooltipText, bodyText, subtext}: MetaData
           containerDisplayMode="block"
           title={tooltipText}
         />
+        {beta && <StyledFeatureBadge type="beta" />}
       </StyledSectionHeading>
       <SectionBody>{bodyText}</SectionBody>
       <SectionSubtext>{subtext}</SectionSubtext>
@@ -42,6 +51,10 @@ const SectionBody = styled('div')`
   font-size: ${p => p.theme.headerFontSize};
   padding: ${space(0.5)} 0;
   max-height: 32px;
+`;
+
+const StyledFeatureBadge = styled(FeatureBadge)`
+  margin: 0;
 `;
 
 export const SectionSubtext = styled('div')<{type?: 'error' | 'default'}>`
