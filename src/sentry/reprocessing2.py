a8a6e50895ecdca6ad35ae724597cdccc1792460
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-03-30 15:41:39 +0200

    remove useless data access for preprocess

--- src/sentry/reprocessing2.py
+++ src/sentry/reprocessing2.py
@@ -211,8 +211,11 @@ def reprocess_event(project_id, event_id, start_time):
         with sentry_sdk.start_span(op="reprocess_event.set_attachment_meta"):
             attachment_cache.set(cache_key, attachments=attachment_objects, timeout=CACHE_TIMEOUT)
 
-    preprocess_event_from_reprocessing.delay(
-        cache_key=cache_key, start_time=start_time, event_id=event_id
+    preprocess_event_from_reprocessing(
+        cache_key=cache_key,
+        start_time=start_time,
+        event_id=event_id,
+        data=data,
     )
 
 
