a33ad078f22e8051b8a1d0f8c46e8589fead35fc
meredith <meredith@sentry.io>
2021-04-01 17:20:31 -0700

    add back original endpoints

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -169,6 +169,12 @@ from .endpoints.organization_has_mobile_app_events import OrganizationHasMobileA
 from .endpoints.organization_index import OrganizationIndexEndpoint
 from .endpoints.organization_integration_details import OrganizationIntegrationDetailsEndpoint
 from .endpoints.organization_integration_repos import OrganizationIntegrationReposEndpoint
+from .endpoints.organization_integration_repository_project_path_config_details import (
+    OrganizationIntegrationRepositoryProjectPathConfigDetailsEndpoint,
+)
+from .endpoints.organization_integration_repository_project_path_configs import (
+    OrganizationIntegrationRepositoryProjectPathConfigEndpoint,
+)
 from .endpoints.organization_code_mapping_details import OrganizationCodeMappingDetailsEndpoint
 from .endpoints.organization_code_mappings import OrganizationCodeMappingsEndpoint
 from .endpoints.organization_integration_request import OrganizationIntegrationRequestEndpoint
@@ -717,6 +723,16 @@ urlpatterns = [
                     name="sentry-api-0-chunk-upload",
                 ),
                 # Code Path Mappings
+                url(
+                    r"^(?P<organization_slug>[^\/]+)/repo-project-path-configs/$",
+                    OrganizationIntegrationRepositoryProjectPathConfigEndpoint.as_view(),
+                    name="sentry-api-0-organization-repository-project-path-config",
+                ),
+                url(
+                    r"^(?P<organization_slug>[^\/]+)/repo-project-path-configs/(?P<config_id>[^\/]+)/$",
+                    OrganizationIntegrationRepositoryProjectPathConfigDetailsEndpoint.as_view(),
+                    name="sentry-api-0-organization-repository-project-path-config-details",
+                ),
                 url(
                     r"^(?P<organization_slug>[^\/]+)/code-mappings/$",
                     OrganizationCodeMappingsEndpoint.as_view(),
