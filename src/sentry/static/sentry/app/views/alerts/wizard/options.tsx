e36393de795e96e0d40684a823db80f0761da028
David Wang <davidw1339@gmail.com>
2021-04-05 17:45:15 -0400

    feat(alert): Update wizard examples and descriptions (#24960)

--- src/sentry/static/sentry/app/views/alerts/wizard/options.tsx
+++ src/sentry/static/sentry/app/views/alerts/wizard/options.tsx
@@ -70,10 +70,10 @@ export const AlertWizardPanelContent: Record<AlertType, PanelContent> = {
       'Issues are groups of errors that have a similar stacktrace. You can set an alert for new issues, issue state changes, and frequency of errors or users affected by an issue.'
     ),
     examples: [
-      t("When the triggering event's attribute X, send an email to a team."),
-      t('When an issue was seen 100 times in the last 2 days, send Slack notification.'),
+      t("When the triggering event's level is fatal."),
+      t('When an issue was seen 100 times in the last 2 days.'),
       t(
-        'When an issue changes state from resolved to unresolved and is assigned to member X, create a Jira ticket.'
+        'Create a JIRA ticket when an issue changes state from resolved to unresolved and is unassigned.'
       ),
     ],
   },
@@ -82,11 +82,8 @@ export const AlertWizardPanelContent: Record<AlertType, PanelContent> = {
       'Alert when the number of errors in a project matching your filters crosses a threshold. This is useful for monitoring the overall level or errors in your project or errors occurring in specific parts of your app.'
     ),
     examples: [
-      t('When the checkout page has more than 10k errors, email a user.'),
-      t(
-        'Send an email to a team when errors exceed 500k on parts of the codebase they own.'
-      ),
-      t('Notify a Slack channel when there are no errors at all.'),
+      t('When the signup page has more than 10k errors in 5 minutes.'),
+      t('When there are more than 500k errors in 10 minutes from a specific file.'),
     ],
   },
   users_experiencing_errors: {
@@ -94,23 +91,15 @@ export const AlertWizardPanelContent: Record<AlertType, PanelContent> = {
       'Alert when the number of users affected by errors in your project crosses a threshold.'
     ),
     examples: [
-      t('When 100k users experience an error, send a notification to a Slack channel.'),
-      t(
-        'Send an email to team when 100 users experience a problem on the Checkout page.'
-      ),
+      t('When 100k users experience an error in 1 hour.'),
+      t('When 100 users experience a problem on the Checkout page.'),
     ],
   },
   throughput: {
-    description: t(
-      'Alert when the number of transactions crosses a threshold within a configurable time window.'
-    ),
+    description: t('Throughput is the number of transactions in a period of time.'),
     examples: [
-      t(
-        'When total transactions across your app reaches 100k, send an alert to a team via Microsoft Teams.'
-      ),
-      t(
-        'When the latest release reaches 1.5m transactions, send an email to the oncall member.'
-      ),
+      t('When number of transactions on a key page exceeds 100k per minute.'),
+      t('When number of transactions drops below a threshold.'),
     ],
   },
   trans_duration: {
@@ -118,36 +107,28 @@ export const AlertWizardPanelContent: Record<AlertType, PanelContent> = {
       'Monitor how long it takes for transactions to complete. Use flexible aggregates like percentiles, averages, and min/max.'
     ),
     examples: [
-      t(
-        'When any transaction slower than 3 seconds occurs, create a Jira issue and assign it to a team.'
-      ),
-      t(
-        'When there’s no transaction slower than 250ms, send an alert to a Slack channel.'
-      ),
+      t('When any transaction is slower than 3 seconds.'),
+      t('When the 75th percentile response time is higher than 250 milliseconds.'),
     ],
   },
   apdex: {
     description: t(
-      'Apdex is an industry-standard metric used to track and measure user satisfaction based on your application response times. The Apdex score provides the ratio of satisfactory, tolerable, and frustrated requests in a specific transaction or endpoint. This metric provides a standard for you to compare transaction performance, understand which ones may require additional optimization or investigation, and set targets or goals for performance.'
+      'Apdex is a metric used to track and measure user satisfaction based on your application response times. The Apdex score provides the ratio of satisfactory, tolerable, and frustrated requests in a specific transaction or endpoint.'
     ),
-    examples: [t('When apdex is beneath 300, send an email to everyone on the team.')],
+    examples: [t('When apdex is below 300.')],
+    docsLink: 'https://docs.sentry.io/product/performance/metrics/#apdex',
   },
   failure_rate: {
-    description: t(
-      'Alert when the percentage of unsuccessful transactions reaches a threshold.'
-    ),
-    examples: [
-      t('When the failure rate reaches 10%, send a notification to a Slack channel.'),
-    ],
+    description: t('Failure rate is the percentage of unsuccessful transactions.'),
+    examples: [t('When the failure rate for an important endpoint reaches 10%.')],
+    docsLink: 'https://docs.sentry.io/product/performance/metrics/#failure-rate',
   },
   lcp: {
     description: t(
       'Largest Contentful Paint (LCP) measures loading performance. Specifically, it marks the point when the largest image or text block is visible within the viewport. A fast LCP helps reassure the user that the page is useful, and we recommend LCP is less than 2.5 seconds.'
     ),
     examples: [
-      t(
-        'When an LCP of more than 2.5s is detected on any key transaction, send an email to a team.'
-      ),
+      t('When the 75th percentile LCP of your homepage is longer than 2.5 seconds.'),
     ],
     docsLink: 'https://docs.sentry.io/product/performance/web-vitals',
   },
@@ -155,22 +136,14 @@ export const AlertWizardPanelContent: Record<AlertType, PanelContent> = {
     description: t(
       'First Input Delay (FID) measures interactivity as the response time when the user tries to interact with the viewport. A low FID helps ensure that a page is useful, and recommend it be less than 100 milliseconds.'
     ),
-    examples: [
-      t(
-        'When first input delay is longer than 4s, send notification to a Slack channel.'
-      ),
-    ],
+    examples: [t('When the average FID of a page is longer than 4 seconds.')],
     docsLink: 'https://docs.sentry.io/product/performance/web-vitals',
   },
   cls: {
     description: t(
       'Cumulative Layout Shift (CLS) measures visual stability by quantifying unexpected layout shifts that occur during the entire lifespan of the page. A CLS of less than 0.1 is a good user experience, while anything greater than 0.25 is poor.'
     ),
-    examples: [
-      t(
-        'When cumulative layout shift is greater than 0.5 on the Checkout page, send an email to all members of a team.'
-      ),
-    ],
+    examples: [t('When the CLS of a page is more than 0.5.')],
     docsLink: 'https://docs.sentry.io/product/performance/web-vitals',
   },
   fp: {
@@ -178,9 +151,7 @@ export const AlertWizardPanelContent: Record<AlertType, PanelContent> = {
       'First Paint (FP), like Largest Contentful Paint (LCP), measures loading performance. Specifically, it marks the point when the first pixel renders on the screen. We recommend an FP of less than 500 milliseconds.'
     ),
     examples: [
-      t(
-        'When first paint takes longer than 250ms, create a Jira ticket and assign it to a team.'
-      ),
+      t('When the 95th percentile FP of a page is longer than 250 milliseconds.'),
     ],
     docsLink: 'https://docs.sentry.io/product/performance/web-vitals',
   },
@@ -188,22 +159,14 @@ export const AlertWizardPanelContent: Record<AlertType, PanelContent> = {
     description: t(
       'First Contentful Paint (FCP), like Largest Contentful Paint (LCP), measures loading performance. Specifically, it marks the point when content such as text and images can first be seen on a page.'
     ),
-    examples: [
-      t(
-        'When first contentful paint of a specific page is above 0.25, send an email to the team.'
-      ),
-    ],
+    examples: [t('When the average FCP of a page is longer than 0.25 seconds.')],
     docsLink: 'https://docs.sentry.io/product/performance/web-vitals',
   },
   ttfb: {
     description: t(
       "Measures the time it takes for a user's browser to receive the first byte of page content. This includes time spent on DNS lookup, connection negotiation, network latency, and unloading the previous document."
     ),
-    examples: [
-      t(
-        'When the time to first byte is above 600ms, send a notification to a Slack channel.'
-      ),
-    ],
+    examples: [t('When the average TTFB of a page is longer than 600 millliseconds.')],
     docsLink: 'https://docs.sentry.io/product/performance/web-vitals',
   },
 };
