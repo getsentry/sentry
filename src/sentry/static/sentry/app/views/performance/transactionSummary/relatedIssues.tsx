3728e2ecf09282eb00f9853c52ea8af315d20cde
Taylan Gocmen <ta.gocmen@gmail.com>
2021-04-05 17:39:00 -0700

    remove txn related issues

--- src/sentry/static/sentry/app/views/performance/transactionSummary/relatedIssues.tsx
+++ src/sentry/static/sentry/app/views/performance/transactionSummary/relatedIssues.tsx
@@ -8,10 +8,8 @@ import {SectionHeading} from 'app/components/charts/styles';
 import EmptyStateWarning from 'app/components/emptyStateWarning';
 import GroupList from 'app/components/issues/groupList';
 import {Panel, PanelBody} from 'app/components/panels';
-import Tooltip from 'app/components/tooltip';
 import {DEFAULT_RELATIVE_PERIODS} from 'app/constants';
 import {URL_PARAM} from 'app/constants/globalSelectionHeader';
-import {IconInfo} from 'app/icons';
 import {t, tct} from 'app/locale';
 import space from 'app/styles/space';
 import {OrganizationSummary} from 'app/types';
@@ -109,9 +107,6 @@ class RelatedIssues extends React.Component<Props> {
       <React.Fragment>
         <ControlsWrapper>
           <SectionHeading>{t('Related Issues')}</SectionHeading>
-          <Tooltip title={t('Top issues containing events matching the metric.')}>
-            <IconInfo size="xs" />
-          </Tooltip>
           <Button
             data-test-id="issues-open"
             size="small"
