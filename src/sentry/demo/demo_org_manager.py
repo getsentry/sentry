7a4372ca21ad591bf74d9a51008beceb6893e0ce
Stephen Cefali <scefali@sentry.io>
2021-04-06 15:52:40 -0700

    force sampled=True

--- src/sentry/demo/demo_org_manager.py
+++ src/sentry/demo/demo_org_manager.py
@@ -31,7 +31,9 @@ logger = logging.getLogger(__name__)
 
 
 def create_demo_org(quick=False) -> Organization:
-    with sentry_sdk.start_transaction(op="create_demo_org", name="create_demo_org") as span:
+    with sentry_sdk.start_transaction(
+        op="create_demo_org", name="create_demo_org", sampled=True
+    ) as span:
         span.set_tag("quick", quick)
         # wrap the main org setup in transaction
         with transaction.atomic():
@@ -97,7 +99,7 @@ def create_demo_org(quick=False) -> Organization:
 
 
 def assign_demo_org() -> Tuple[Organization, User]:
-    with sentry_sdk.start_transaction(op="assign_demo_org", name="assign_demo_org"):
+    with sentry_sdk.start_transaction(op="assign_demo_org", name="assign_demo_org", sampled=True):
         from .tasks import build_up_org_buffer
 
         demo_org = None
