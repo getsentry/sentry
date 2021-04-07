75d445d13ebbdc064679c384a2f19a124de52a45
William Mak <william@wmak.io>
2021-04-07 12:10:12 -0400

    ref: Adding a handle query errors

--- src/sentry/api/endpoints/organization_tagkey_values.py
+++ src/sentry/api/endpoints/organization_tagkey_values.py
@@ -21,15 +21,16 @@ class OrganizationTagKeyValuesEndpoint(OrganizationEventsEndpointBase):
         except NoProjects:
             paginator = SequencePaginator([])
         else:
-            paginator = tagstore.get_tag_value_paginator_for_projects(
-                filter_params["project_id"],
-                filter_params.get("environment"),
-                key,
-                filter_params["start"],
-                filter_params["end"],
-                query=request.GET.get("query"),
-                include_transactions=request.GET.get("includeTransactions") == "1",
-            )
+            with self.handle_query_errors():
+                paginator = tagstore.get_tag_value_paginator_for_projects(
+                    filter_params["project_id"],
+                    filter_params.get("environment"),
+                    key,
+                    filter_params["start"],
+                    filter_params["end"],
+                    query=request.GET.get("query"),
+                    include_transactions=request.GET.get("includeTransactions") == "1",
+                )
 
         return self.paginate(
             request=request,
