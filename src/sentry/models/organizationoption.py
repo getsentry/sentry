3c306693daa14d18b132848dd8c8a00597be99ba
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-04-08 12:40:52 -0700

    ref(py): Sort imports in models module (84 files) (#25086)

--- src/sentry/models/organizationoption.py
+++ src/sentry/models/organizationoption.py
@@ -1,10 +1,10 @@
 from django.db import models
 
-from sentry.db.models import Model, FlexibleForeignKey, sane_repr
+from sentry.db.models import FlexibleForeignKey, Model, sane_repr
 from sentry.db.models.fields import EncryptedPickledObjectField
 from sentry.db.models.manager import OptionManager
-from sentry.utils.cache import cache
 from sentry.tasks.relay import schedule_update_config_cache
+from sentry.utils.cache import cache
 
 
 class OrganizationOptionManager(OptionManager):
