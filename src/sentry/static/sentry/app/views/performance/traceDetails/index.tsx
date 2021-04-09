6c591d864c5ae83ac186b0072a75f9be4cd9ac91
Tony <txiao@sentry.io>
2021-04-09 10:07:24 -0400

    fix(trace-view): Do not persist the absolute date (#25052)
    When going to the trace view, it sets the start/end times based on the event's
    timestamp. This means that when using the sidebar to navigate, these selections
    are persisted as global selections and is undesirable. This change ensures that
    they are not persisted by using a page start/end selection instead.

--- src/sentry/static/sentry/app/views/performance/traceDetails/index.tsx
+++ src/sentry/static/sentry/app/views/performance/traceDetails/index.tsx
@@ -35,10 +35,13 @@ class TraceSummary extends React.Component<Props> {
   renderContent() {
     const {location, organization, params} = this.props;
     const traceSlug = this.getTraceSlug();
-    const queryParams = getParams(location.query);
+    const queryParams = getParams(location.query, {
+      allowAbsolutePageDatetime: true,
+    });
     const start = decodeScalar(queryParams.start);
     const end = decodeScalar(queryParams.end);
     const statsPeriod = decodeScalar(queryParams.statsPeriod);
+    const dateSelected = Boolean(statsPeriod || (start && end));
 
     const content = ({
       isLoading,
@@ -54,16 +57,14 @@ class TraceSummary extends React.Component<Props> {
         organization={organization}
         params={params}
         traceSlug={traceSlug}
-        start={start}
-        end={end}
-        statsPeriod={statsPeriod}
+        dateSelected={dateSelected}
         isLoading={isLoading}
         error={error}
         traces={traces}
       />
     );
 
-    if (!statsPeriod && (!start || !end)) {
+    if (!dateSelected) {
       return content({
         isLoading: false,
         error: 'date selection not specified',
