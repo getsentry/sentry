49f935c7e9189fb4757b972476a42e6bebd9d821
Lyn <lyn.nagara@gmail.com>
2021-04-12 10:27:48 -0700

    more cases of wait_for_event_count

--- tests/acceptance/test_performance_summary.py
+++ tests/acceptance/test_performance_summary.py
@@ -44,7 +44,6 @@ class PerformanceSummaryTest(AcceptanceTestCase, SnubaTestCase):
         # Create a transaction
         event = make_event(load_data("transaction", timestamp=before_now(minutes=1)))
         self.store_event(data=event, project_id=self.project.id)
-        self.wait_for_event_count(self.project.id, 1)
 
         self.store_event(
             data={
@@ -162,8 +161,6 @@ class PerformanceSummaryTest(AcceptanceTestCase, SnubaTestCase):
         event_data["measurements"]["fid"]["value"] = 3000000000
         event_data["measurements"]["cls"]["value"] = 3000000000
         self.store_event(data=event_data, project_id=self.project.id)
-
-        self.wait_for_event_count(self.project.id, 5)
 
         with self.feature(FEATURE_NAMES):
             self.browser.get(vitals_path)
