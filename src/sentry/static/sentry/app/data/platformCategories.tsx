8f0924ce679e4b000f24e46157863fc1337cc9f9
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-04-12 13:22:10 -0700

    feat(project-picker): Support aliases

--- src/sentry/static/sentry/app/data/platformCategories.tsx
+++ src/sentry/static/sentry/app/data/platformCategories.tsx
@@ -160,6 +160,13 @@ export const performance: PlatformKey[] = [
   'node-connect',
 ];
 
+/**
+ * Additional aliases used for filtering in the platform picker
+ */
+export const filterAliases: Partial<Record<PlatformKey, string[]>> = {
+  native: ['cpp', 'c++'],
+};
+
 export type PlatformKey =
   | typeof popular[number]
   | typeof frontend[number]
