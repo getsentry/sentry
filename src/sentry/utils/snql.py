85dd7bceb5e2285f78195e635b98075e4cab75fc
Evan Hicks <evanh@users.noreply.github.com>
2021-04-12 12:46:10 -0400

    fix(snql) Add more sessions referrers to dry run (#25160)
    Add more of the session referrers to the SnQL dry run. All of them are very low
    volume referrers.

--- src/sentry/utils/snql.py
+++ src/sentry/utils/snql.py
@@ -3,7 +3,19 @@ from typing import Optional
 
 from sentry import options
 
-snql_referrers = {"sessions.stability-sort"}
+snql_referrers = {
+    "sessions.stability-sort",
+    "sessions.crash-free-breakdown",
+    "sessions.get-adoption",
+    "sessions.health-data-check",
+    "sessions.oldest-data-backfill",
+    "sessions.release-adoption-list",
+    "sessions.release-adoption-total-users-and-sessions",
+    "sessions.release-overview",
+    "sessions.release-sessions-time-bounds",
+    "sessions.release-stats",
+    "sessions.release-stats-details",
+}
 
 
 def should_use_snql(referrer: Optional[str]) -> bool:
