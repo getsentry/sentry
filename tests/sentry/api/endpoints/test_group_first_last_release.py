9414871d6dc9a5fcca7eeedab2bbb431e119a99f
Chris Fuller <cfuller@sentry.io>
2021-04-12 11:23:56 -0400

    Adding release test

--- tests/sentry/api/endpoints/test_group_first_last_release.py
+++ tests/sentry/api/endpoints/test_group_first_last_release.py
@@ -11,3 +11,13 @@ class GroupFirstLastTest(APITestCase, SnubaTestCase):
         assert response.data["id"] == str(group.id)
         assert response.data["firstRelease"] is None
         assert response.data["lastRelease"] is None
+
+        event = self.store_event(data={"release": "1.0"}, project_id=self.project.id)
+        group = event.group
+
+        url = f"/api/0/issues/{group.id}/"
+        response = self.client.get(url, format="json")
+        assert response.status_code == 200, response.content
+        assert response.data["id"] == str(group.id)
+        assert response.data["firstRelease"]["version"] == "1.0"
+        assert response.data["lastRelease"]["version"] == "1.0"
