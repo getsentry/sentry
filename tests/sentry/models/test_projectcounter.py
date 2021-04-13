3178d085d4a210126413c67f58dbae615fdf0e38
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-04-13 14:12:07 +0200

    ref(counter): Option to use pg 9.5+ upsert for short_id counters (#25182)

--- tests/sentry/models/test_projectcounter.py
+++ tests/sentry/models/test_projectcounter.py
@@ -1,13 +1,13 @@
+import pytest
+
+from sentry import options
 from sentry.models import Counter
-from sentry.testutils import TestCase
 
 
-class ProjectCounterTest(TestCase):
-    def test_increment(self):
-        user = self.create_user()
-        org = self.create_organization(owner=user)
-        team = self.create_team(organization=org)
-        project = self.create_project(teams=[team])
+@pytest.mark.django_db
+@pytest.mark.parametrize("upsert_sample_rate", [0, 1])
+def test_increment(default_project, upsert_sample_rate):
+    options.set("store.projectcounter-modern-upsert-sample-rate", upsert_sample_rate)
 
-        assert Counter.increment(project, 42) == 42
-        assert Counter.increment(project, 1) == 43
+    assert Counter.increment(default_project, 42) == 42
+    assert Counter.increment(default_project, 1) == 43
