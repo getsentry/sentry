7556c5cc3fca13af805dbda2aa1b31687e81d521
mikellykels <kelly@sentry.io>
2021-04-13 01:24:14 -0700

    changing this back, there's already an alert banner if a user doesn't have access

--- src/sentry/static/sentry/app/views/alerts/index.tsx
+++ src/sentry/static/sentry/app/views/alerts/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import Feature from 'app/components/acl/feature';
-import LightWeightNoProjectMessage from 'app/components/lightWeightNoProjectMessage';
 import {Organization} from 'app/types';
 import withOrganization from 'app/utils/withOrganization';
 
@@ -13,20 +12,18 @@ class AlertsContainer extends React.Component<Props> {
   render() {
     const {children, organization} = this.props;
     return (
-      <LightWeightNoProjectMessage organization={organization}>
-        <Feature organization={organization} features={['incidents']}>
-          {({hasFeature: hasMetricAlerts}) => (
-            <React.Fragment>
-              {children && React.isValidElement(children)
-                ? React.cloneElement(children, {
-                    organization,
-                    hasMetricAlerts,
-                  })
-                : children}
-            </React.Fragment>
-          )}
-        </Feature>
-      </LightWeightNoProjectMessage>
+      <Feature organization={organization} features={['incidents']}>
+        {({hasFeature: hasMetricAlerts}) => (
+          <React.Fragment>
+            {children && React.isValidElement(children)
+              ? React.cloneElement(children, {
+                  organization,
+                  hasMetricAlerts,
+                })
+              : children}
+          </React.Fragment>
+        )}
+      </Feature>
     );
   }
 }
