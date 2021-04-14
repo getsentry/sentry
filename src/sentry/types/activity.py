4901f28cd4ff34a684f8d781b2b5209357717353
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-04-14 13:09:23 -0700

    More explicit ActivityType CHOICES

--- src/sentry/types/activity.py
+++ src/sentry/types/activity.py
@@ -31,6 +31,30 @@ class ActivityType(Enum):
 
 # Warning: This must remain in this EXACT order.
 CHOICES = tuple(
-    (i, ActivityType(i).name.lower())
-    for i in [1, 15, 13, 16, 21, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 14, 17, 18, 19, 20, 22, 23]
+    (i.value, i.name.lower())
+    for i in [
+        ActivityType.SET_RESOLVED,  # 1
+        ActivityType.SET_RESOLVED_BY_AGE,  # 15
+        ActivityType.SET_RESOLVED_IN_RELEASE,  # 13
+        ActivityType.SET_RESOLVED_IN_COMMIT,  # 16
+        ActivityType.SET_RESOLVED_IN_PULL_REQUEST,  # 21
+        ActivityType.SET_UNRESOLVED,  # 2
+        ActivityType.SET_IGNORED,  # 3
+        ActivityType.SET_PUBLIC,  # 4
+        ActivityType.SET_PRIVATE,  # 5
+        ActivityType.SET_REGRESSION,  # 6
+        ActivityType.CREATE_ISSUE,  # 7
+        ActivityType.NOTE,  # 8
+        ActivityType.FIRST_SEEN,  # 9
+        ActivityType.RELEASE,  # 10
+        ActivityType.ASSIGNED,  # 11
+        ActivityType.UNASSIGNED,  # 12
+        ActivityType.MERGE,  # 14
+        ActivityType.DEPLOY,  # 17
+        ActivityType.NEW_PROCESSING_ISSUES,  # 18
+        ActivityType.UNMERGE_SOURCE,  # 19
+        ActivityType.UNMERGE_DESTINATION,  # 20
+        ActivityType.REPROCESS,  # 22
+        ActivityType.MARK_REVIEWED,  # 23
+    ]
 )
