ac2d2eac68e304b53c216c9d64ff0b69458e22ea
Kelly Carino <kelly@sentry.io>
2021-04-13 17:53:48 -0700

    feat(api): Add delete to Project Users endpoint for superusers (#25172)
    * feat(api): Add delete to Project Users endpoint for superusers
    
    Add delete method to Project Users endpoint for superusers. This is for a feature in _admin for the support team/customer ops to delete emails from Tags.
    
    FIXES ISSUE-1176
    
    * move to project_user_details endpoint and refactor
    
    * style(lint): Auto commit lint changes
    
    * refactor and add another test
    
    Co-authored-by: sentry-internal-tools[bot] <66042841+sentry-internal-tools[bot]@users.noreply.github.com>

--- tests/sentry/api/endpoints/test_project_user_details.py
+++ tests/sentry/api/endpoints/test_project_user_details.py
@@ -7,7 +7,6 @@ class ProjectUserDetailsTest(APITestCase):
 
     def setUp(self):
         super().setUp()
-        self.user = self.create_user()
         self.org = self.create_organization(owner=None)
         self.team = self.create_team(organization=self.org)
         self.project = self.create_project(organization=self.org, teams=[self.team])
@@ -15,9 +14,39 @@ class ProjectUserDetailsTest(APITestCase):
             user=self.user, organization=self.org, teams=[self.project.teams.first()]
         )
         self.euser = EventUser.objects.create(email="foo@example.com", project_id=self.project.id)
-
-        self.login_as(user=self.user)
+        self.euser2 = EventUser.objects.create(email="bar@example.com", project_id=self.project.id)
 
     def test_simple(self):
+        self.login_as(user=self.user)
         response = self.get_valid_response(self.org.slug, self.project.slug, self.euser.hash)
         assert response.data["id"] == str(self.euser.id)
+
+    def test_delete_event_user(self):
+        # Only delete an event user as a superuser
+        self.login_as(user=self.user, superuser="true")
+
+        assert EventUser.objects.count() == 2
+
+        path = f"/api/0/projects/{self.org.slug}/{self.project.slug}/users/{self.euser.hash}/"
+        response = self.client.delete(path)
+
+        assert response.status_code == 200
+        assert EventUser.objects.count() == 1
+
+        # User doesn't exist
+        path = f"/api/0/projects/{self.org.slug}/{self.project.slug}/users/1234567abcdefg"
+        response = self.client.delete(path)
+
+        assert response.status_code == 404
+        assert EventUser.objects.count() == 1
+
+        # Not a superuser
+        self.login_as(user=self.create_user(is_superuser=False))
+
+        assert EventUser.objects.count() == 1
+
+        path = f"/api/0/projects/{self.org.slug}/{self.project.slug}/users/{self.euser2.hash}/"
+        response = self.client.delete(path)
+
+        assert response.status_code == 403
+        assert EventUser.objects.count() == 1
