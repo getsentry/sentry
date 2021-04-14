6ace8a20c65bfa1106a1bd3a3d16b1165db1bdf7
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-04-14 03:24:12 -0700

    style(js): Use inline eslint-disable-line for all refluxbox imports (#25251)

--- src/sentry/static/sentry/app/components/stream/group.tsx
+++ src/sentry/static/sentry/app/components/stream/group.tsx
@@ -2,8 +2,7 @@ import React from 'react';
 import {css} from '@emotion/core';
 import styled from '@emotion/styled';
 import classNames from 'classnames';
-// eslint-disable-next-line no-restricted-imports
-import {Box} from 'reflexbox';
+import {Box} from 'reflexbox'; // eslint-disable-line no-restricted-imports
 
 import AssigneeSelector from 'app/components/assigneeSelector';
 import GuideAnchor from 'app/components/assistant/guideAnchor';
