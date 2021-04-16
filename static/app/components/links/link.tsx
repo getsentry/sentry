785e67d28e73f55168bace62d425ec1e9b0245f9
Mark Story <mark@mark-story.com>
2021-04-16 16:52:40 -0400

    ref(js) Remove contextTypes from more places
    Remove a few more usages of contextTypes.

--- static/app/components/links/link.tsx
+++ static/app/components/links/link.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import {Link as RouterLink} from 'react-router';
+import {Link as RouterLink, withRouter, WithRouterProps} from 'react-router';
 import isPropValid from '@emotion/is-prop-valid';
 import styled from '@emotion/styled';
 import * as Sentry from '@sentry/react';
 import {Location, LocationDescriptor} from 'history';
-import PropTypes from 'prop-types';
 
 type AnchorProps = React.HTMLProps<HTMLAnchorElement>;
 
 type ToLocationFunction = (location: Location) => LocationDescriptor;
 
-type Props = {
+type Props = WithRouterProps & {
   //URL
   to: ToLocationFunction | LocationDescriptor;
   // Styles applied to the component's root
@@ -22,12 +21,8 @@ type Props = {
  * back to <a> if there is no router present
  */
 class Link extends React.Component<Props> {
-  static contextTypes = {
-    location: PropTypes.object,
-  };
-
   componentDidMount() {
-    const isRouterPresent = this.context.location;
+    const isRouterPresent = this.props.location;
     if (!isRouterPresent) {
       Sentry.captureException(
         new Error('The link component was rendered without being wrapped by a <Router />')
@@ -36,10 +31,9 @@ class Link extends React.Component<Props> {
   }
 
   render() {
-    const {disabled, to, ref, ...props} = this.props;
-    const isRouterPresent = this.context.location;
+    const {disabled, to, ref, location, ...props} = this.props;
 
-    if (!disabled && isRouterPresent) {
+    if (!disabled && location) {
       return <RouterLink to={to} ref={ref as any} {...props} />;
     }
 
@@ -51,7 +45,7 @@ class Link extends React.Component<Props> {
   }
 }
 
-export default Link;
+export default withRouter(Link);
 
 const Anchor = styled('a', {
   shouldForwardProp: prop => isPropValid(prop) && prop !== 'disabled',
