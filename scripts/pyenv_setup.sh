d4568fb72e71f9394e892446232b90a64531e3f3
Armen <armenzg@sentry.io>
2021-04-16 19:52:23 -0400

    Automatic fixes for Pyenv shell script

--- scripts/pyenv_setup.sh
+++ scripts/pyenv_setup.sh
@@ -5,7 +5,10 @@
 # - This script assumes you're calling from the top directory of the repository
 set -eu
 
-HERE="$(cd "$(dirname "${BASH_SOURCE[0]}")"; pwd -P)"
+HERE="$(
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)"
 # shellcheck disable=SC1090
 source "${HERE}/lib.sh"
 
@@ -13,18 +16,19 @@ get_shell_startup_script() {
   local _startup_script=''
   if [[ -n "$SHELL" ]]; then
     case "$SHELL" in
-      */bash)
-        _startup_script="${HOME}/.bash_profile"
-        ;;
-      */zsh)
-        _startup_script="${HOME}/.zshrc"
-        ;;
-      */fish)
-        _startup_script="${HOME}/.config/fish/config.fish"
-        ;;
-      *)
-        echo "$SHELL is currently not supported."
-        exit 1
+    */bash)
+      _startup_script="${HOME}/.bash_profile"
+      ;;
+    */zsh)
+      _startup_script="${HOME}/.zshrc"
+      ;;
+    */fish)
+      _startup_script="${HOME}/.config/fish/config.fish"
+      ;;
+    *)
+      echo "$SHELL is currently not supported."
+      exit 1
+      ;;
     esac
   else
     echo "The environment variable \$SHELL needs to be defined."
@@ -36,16 +40,17 @@ get_shell_startup_script() {
 _append_to_startup_script() {
   if [[ -n "$SHELL" ]]; then
     case "$SHELL" in
-      */bash)
-        # shellcheck disable=SC2016
-        echo -e '\nif command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> "${1}"
-        ;;
-      */zsh)
-        # shellcheck disable=SC2016
-        echo -e '\nif command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> "${1}"
-        ;;
-      */fish)
-        echo -e '\n\n# pyenv init\nif command -v pyenv 1>/dev/null 2>&1\n  pyenv init - | source\nend' >> "$1"
+    */bash)
+      # shellcheck disable=SC2016
+      echo -e '\nif command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >>"${1}"
+      ;;
+    */zsh)
+      # shellcheck disable=SC2016
+      echo -e '\nif command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >>"${1}"
+      ;;
+    */fish)
+      echo -e '\n\n# pyenv init\nif command -v pyenv 1>/dev/null 2>&1\n  pyenv init - | source\nend' >>"$1"
+      ;;
     esac
 
     echo "--> Tail of ${1}"
@@ -71,7 +76,7 @@ install_pyenv() {
     echo "Installing Python (if missing) via pyenv"
     local pyenv_version
     pyenv_version=$(pyenv -v | awk '{print $2}')
-    python_version=$(xargs -n1 < .python-version)
+    python_version=$(xargs -n1 <.python-version)
 
     if query_big_sur; then
       local flag
@@ -81,7 +86,7 @@ install_pyenv() {
         flag="-L$(xcrun --show-sdk-path)/usr/lib ${LDFLAGS}"
       fi
       # cat is used since pyenv would finish to soon when the Python version is already installed
-      curl -sSL https://github.com/python/cpython/commit/8ea6353.patch | cat | \
+      curl -sSL https://github.com/python/cpython/commit/8ea6353.patch | cat |
         LDFLAGS="$flag" pyenv install --skip-existing --patch "$python_version"
     else
       pyenv install --skip-existing "$python_version"
@@ -104,7 +109,7 @@ setup_pyenv() {
   echo "Activating pyenv and validating Python version"
   eval "$(pyenv init -)"
   python_version=$(python -V | sed s/Python\ //g)
-  [[ $python_version == $(cat .python-version) ]] || \
+  [[ $python_version == $(cat .python-version) ]] ||
     (echo "Wrong Python version: $python_version. Please report in #discuss-dev-tooling" && exit 1)
 }
 
