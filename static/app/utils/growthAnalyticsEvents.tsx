255813d904efd1bddea869181f27e34bec062255
Stephen Cefali <scefali@sentry.io>
2021-04-16 10:55:02 -0700

    replace mobil prompt check event

--- static/app/utils/growthAnalyticsEvents.tsx
+++ static/app/utils/growthAnalyticsEvents.tsx
@@ -2,19 +2,18 @@ type MobilePromptBannerParams = {
   matchedUserAgentString: string;
 };
 
-type CheckShowParams = MobilePromptBannerParams & {
+type ShowParams = MobilePromptBannerParams & {
   userAgentMatches: boolean;
   matchedUserAgentString: string;
   hasMobileProject: boolean;
   snoozedOrDismissed: boolean;
   mobileEventBrowserName: string;
   mobileEventClientOsName: string;
-  showCTA: boolean;
 };
 
 //define the event key to payload mappings
 export type GrowthEventParameters = {
-  'growth.check_show_mobile_prompt_banner': CheckShowParams;
+  'growth.show_mobile_prompt_banner': ShowParams;
   'growth.dismissed_mobile_prompt_banner': MobilePromptBannerParams;
   'growth.opened_mobile_project_suggest_modal': MobilePromptBannerParams;
   'growth.clicked_mobile_prompt_setup_project': MobilePromptBannerParams;
@@ -26,7 +25,7 @@ export type GrowthEventParameters = {
 type GrowthAnalyticsKey = keyof GrowthEventParameters;
 
 export const growthEventMap: Record<GrowthAnalyticsKey, string> = {
-  'growth.check_show_mobile_prompt_banner': 'Growth: Check Show Mobile Prompt Banner',
+  'growth.show_mobile_prompt_banner': 'Growth: Show Mobile Prompt Banner',
   'growth.dismissed_mobile_prompt_banner': 'Growth: Dismissed Mobile Prompt Banner',
   'growth.opened_mobile_project_suggest_modal':
     'Growth: Open Mobile Project Suggest Modal',
