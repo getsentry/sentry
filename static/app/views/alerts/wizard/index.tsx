f0df7738132991562428f4ca85e2b05560f57eb8
robinrendle <robinjrendle@gmail.com>
2021-04-16 18:50:03 -0700

    removed css comment

--- static/app/views/alerts/wizard/index.tsx
+++ static/app/views/alerts/wizard/index.tsx
@@ -4,15 +4,13 @@ import styled from '@emotion/styled';
 
 import Feature from 'app/components/acl/feature';
 import CreateAlertButton from 'app/components/createAlertButton';
+import * as Layout from 'app/components/layouts/thirds';
 import ExternalLink from 'app/components/links/externalLink';
 import List from 'app/components/list';
 import ListItem from 'app/components/list/listItem';
-import PageHeading from 'app/components/pageHeading';
-import {Panel, PanelBody} from 'app/components/panels';
+import {Panel, PanelBody, PanelHeader} from 'app/components/panels';
 import SentryDocumentTitle from 'app/components/sentryDocumentTitle';
-import Tag from 'app/components/tag';
 import {t} from 'app/locale';
-import {PageContent, PageHeader} from 'app/styles/organization';
 import space from 'app/styles/space';
 import {Organization, Project} from 'app/types';
 import BuilderBreadCrumbs from 'app/views/alerts/builder/builderBreadCrumbs';
@@ -24,7 +22,6 @@ import {
   AlertWizardOptions,
   AlertWizardPanelContent,
   AlertWizardRuleTemplates,
-  WebVitalAlertTypes,
 } from './options';
 import RadioPanelGroup from './radioPanelGroup';
 
@@ -93,109 +90,95 @@ class AlertWizard extends React.Component<Props, State> {
     return (
       <React.Fragment>
         <SentryDocumentTitle title={title} projectSlug={projectId} />
-        <PageContent>
-          <Feature features={['organizations:alert-wizard']}>
-            <BuilderBreadCrumbs
-              hasMetricAlerts={hasMetricAlerts}
-              orgSlug={organization.slug}
-              projectSlug={projectId}
-              title={t('Create Alert Rule')}
-            />
-            <StyledPageHeader>
-              <StyledPageHeading>
-                {t('What should we alert you about?')}
-              </StyledPageHeading>
-            </StyledPageHeader>
-            <Styledh2>{t('Errors')}</Styledh2>
-            <WizardBody>
-              <WizardOptions>
-                {AlertWizardOptions.map(({categoryHeading, options}, i) => (
-                  <OptionsWrapper key={categoryHeading}>
-                    {i > 0 && <Styledh2>{categoryHeading}</Styledh2>}
-                    <RadioPanelGroup
-                      choices={options.map(alertType => {
-                        return [
-                          alertType,
-                          AlertWizardAlertNames[alertType],
-                          ...(WebVitalAlertTypes.has(alertType)
-                            ? [<Tag key={alertType}>{t('Web Vital')}</Tag>]
-                            : []),
-                        ] as [AlertType, string, React.ReactNode];
-                      })}
-                      onChange={this.handleChangeAlertOption}
-                      value={alertOption}
-                      label="alert-option"
-                    />
-                  </OptionsWrapper>
-                ))}
-              </WizardOptions>
-              <WizardPanel visible={!!panelContent && !!alertOption}>
-                <WizardPanelBody withPadding>
-                  {panelContent && alertOption && (
-                    <div>
-                      <PanelHeader>{AlertWizardAlertNames[alertOption]}</PanelHeader>
-                      <PanelDescription>
-                        {panelContent.description}{' '}
-                        {panelContent.docsLink && (
-                          <ExternalLink href={panelContent.docsLink}>
-                            {t('Learn more')}
-                          </ExternalLink>
-                        )}
-                      </PanelDescription>
-                      <WizardImage src={panelContent.illustration} />
-                      <ExampleHeader>{t('Examples')}</ExampleHeader>
-                      <ExampleList symbol="bullet">
-                        {panelContent.examples.map((example, i) => (
-                          <ExampleItem key={i}>{example}</ExampleItem>
-                        ))}
-                      </ExampleList>
-                    </div>
-                  )}
-                  {this.renderCreateAlertButton()}
-                </WizardPanelBody>
-              </WizardPanel>
-            </WizardBody>
-          </Feature>
-        </PageContent>
+
+        <Feature features={['organizations:alert-wizard']}>
+          <Layout.Header>
+            <Layout.HeaderContent>
+              <BuilderBreadCrumbs
+                hasMetricAlerts={hasMetricAlerts}
+                orgSlug={organization.slug}
+                projectSlug={projectId}
+                title={t('Create Alert Rule')}
+              />
+              <Layout.Title>{t('Select Alert Type')}</Layout.Title>
+            </Layout.HeaderContent>
+          </Layout.Header>
+          <StyledLayoutBody>
+            <Layout.Main fullWidth>
+              <List symbol="colored-numeric">
+                <StyledListItem>{t('What should we alert you about?')}</StyledListItem>
+                <WizardBody>
+                  <WizardOptions>
+                    <Styledh2>{t('Errors')}</Styledh2>
+                    {AlertWizardOptions.map(({categoryHeading, options}, i) => (
+                      <OptionsWrapper key={categoryHeading}>
+                        {i > 0 && <Styledh2>{categoryHeading}</Styledh2>}
+                        <RadioPanelGroup
+                          choices={options.map(alertType => {
+                            return [alertType, AlertWizardAlertNames[alertType]] as [
+                              string,
+                              React.ReactNode
+                            ];
+                          })}
+                          onChange={this.handleChangeAlertOption}
+                          value={alertOption}
+                          label="alert-option"
+                        />
+                      </OptionsWrapper>
+                    ))}
+                  </WizardOptions>
+                  <WizardPanel visible={!!panelContent && !!alertOption}>
+                    <WizardPanelBody>
+                      {panelContent && alertOption && (
+                        <div>
+                          <PanelHeader>{AlertWizardAlertNames[alertOption]}</PanelHeader>
+                          <PanelBody withPadding>
+                            <PanelDescription>
+                              {panelContent.description}{' '}
+                              {panelContent.docsLink && (
+                                <ExternalLink href={panelContent.docsLink}>
+                                  {t('Learn more')}
+                                </ExternalLink>
+                              )}
+                            </PanelDescription>
+                            <WizardImage src={panelContent.illustration} />
+                            <ExampleHeader>{t('Examples')}</ExampleHeader>
+                            <ExampleList symbol="bullet">
+                              {panelContent.examples.map((example, i) => (
+                                <ExampleItem key={i}>{example}</ExampleItem>
+                              ))}
+                            </ExampleList>
+                          </PanelBody>
+                        </div>
+                      )}
+                      <WizardButton>{this.renderCreateAlertButton()}</WizardButton>
+                    </WizardPanelBody>
+                  </WizardPanel>
+                </WizardBody>
+              </List>
+            </Layout.Main>
+          </StyledLayoutBody>
+        </Feature>
       </React.Fragment>
     );
   }
 }
 
-const StyledPageHeading = styled(PageHeading)`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  &:before {
-    content: '1';
-    width: 25px;
-    height: 25px;
-    font-weight: 500;
-    font-size: ${p => p.theme.fontSizeSmall};
-    line-height: 1.4;
-    background-color: ${p => p.theme.yellow300};
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 50%;
-    margin-right: ${space(1)};
-  }
+const StyledLayoutBody = styled(Layout.Body)`
+  margin-bottom: -20px;
 `;
 
-const StyledPageHeader = styled(PageHeader)`
-  margin-bottom: ${space(4)};
+const StyledListItem = styled(ListItem)`
+  font-size: ${p => p.theme.fontSizeExtraLarge};
+  margin-top: ${space(3)};
+  margin-bottom: ${space(3)};
+  max-width: 350px;
 `;
 
 const Styledh2 = styled('h2')`
   font-weight: normal;
-  font-size: ${p => p.theme.fontSizeExtraLarge};
+  font-size: ${p => p.theme.fontSizeLarge};
   margin-bottom: ${space(1)} !important;
-`;
-
-const PanelHeader = styled('h2')`
-  font-weight: normal;
-  font-size: ${p => p.theme.headerFontSize};
-  margin-bottom: ${space(0.5)} !important;
 `;
 
 const WizardBody = styled('div')`
@@ -204,9 +187,10 @@ const WizardBody = styled('div')`
 
 const WizardOptions = styled('div')`
   flex: 3;
+  margin-left: ${space(4)};
   margin-right: ${space(3)};
   padding-right: ${space(3)};
-  max-width: 400px;
+  max-width: 300px;
 `;
 
 const WizardImage = styled('img')`
@@ -214,7 +198,7 @@ const WizardImage = styled('img')`
 `;
 
 const WizardPanel = styled(Panel)<{visible?: boolean}>`
-  max-width: 800px;
+  max-width: 700px;
   position: sticky;
   top: 20px;
   flex: 5;
@@ -245,7 +229,6 @@ const WizardPanelBody = styled(PanelBody)`
   margin-bottom: 0;
   flex: 1;
   min-width: 100%;
-  padding: ${space(3)};
 `;
 
 const PanelDescription = styled('p')`
@@ -267,6 +250,11 @@ const OptionsWrapper = styled('div')`
   &:last-child {
     margin-bottom: 0;
   }
+`;
+
+const WizardButton = styled('div')`
+  border-top: 1px solid ${p => p.theme.border};
+  padding: ${space(1.5)} ${space(1.5)} ${space(1.5)} ${space(1.5)};
 `;
 
 export default AlertWizard;
