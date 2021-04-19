cd0b0bd37f88d7877f4a440dc78aaafe6e3bb003
Evan Hicks <evan.hicks@sentry.io>
2021-04-19 17:35:15 -0400

    fix(snql) Update SnQL dry run logic
    Add logic to the referrer code to whitelist referrers by prefix. Also add
    an optional entity so that I can test performance queries that are always
    locked to transactions.

--- src/sentry/utils/snql.py
+++ src/sentry/utils/snql.py
@@ -3,34 +3,48 @@ from typing import Optional
 
 from sentry import options
 
-snql_referrers = {
-    "sessions.stability-sort",
-    "sessions.crash-free-breakdown",
-    "sessions.get-adoption",
-    "sessions.health-data-check",
-    "sessions.oldest-data-backfill",
-    "sessions.release-adoption-list",
-    "sessions.release-adoption-total-users-and-sessions",
-    "sessions.release-overview",
-    "sessions.release-sessions-time-bounds",
-    "sessions.release-stats",
-    "sessions.release-stats-details",
-    "outcomes.timeseries",
-    "tsdb-modelid:500",
-    "incidents.get_incident_aggregates",
-    "tsdb-modelid:501",
-    "tsdb-modelid:502",
+referrer_blacklist = {}
+referrers_by_entity = {
+    "api.performance.durationpercentilechart": "discover_transactions",
+    "api.performance.vital-detail": "discover_transactions",
+    "api.performance.status-breakdown": "discover_transactions",
+    "api.trends.get-percentage-change": "discover_transactions",
+    "api.performance.transaction-summary": "discover_transactions",
+}
+referrer_prefixes = ["outcomes.", "sessions.", "tsdb-modelid:6", "tsdb-modelid:5", "incidents."]
+referrers = {
+    "eventstore.get_events",
     "group.filter_by_event_id",
-    "alertruleserializer.test_query",
-    "incidents.get_incident_event_stats",
+    "incidents.get_incident_aggregates",
     "tagstore.get_tag_value_paginator_for_projects",
+    "api.serializer.projects.get_stats",
+    "tsdb-modelid:407",
+    "testing.test",
 }
 
 
-def should_use_snql(referrer: Optional[str]) -> bool:
-    if referrer in snql_referrers:
-        use_snql = options.get("snuba.snql.referrer-rate")
-        assert isinstance(use_snql, float)
-        return random.random() <= use_snql
+# Returns None (do not use SnQL), "auto" (use dataset as entity) or a specific entity to use.
+def should_use_snql(referrer: Optional[str]) -> Optional[str]:
+    if referrer in referrer_blacklist:
+        return None
 
-    return False
+    use_snql = False
+    entity = "auto"
+    if referrer in referrers:
+        use_snql = True
+    elif referrer in referrers_by_entity:
+        use_snql = True
+        entity = referrers_by_entity[referrer]
+    else:
+        for prefix in referrer_prefixes:
+            if referrer.startswith(prefix):
+                use_snql = True
+                break
+
+    if use_snql:
+        snql_rate = options.get("snuba.snql.referrer-rate")
+        assert isinstance(snql_rate, float)
+        if random.random() <= snql_rate:
+            return entity
+
+    return None
--- src/sentry/utils/snuba.py
+++ src/sentry/utils/snuba.py
@@ -629,7 +629,6 @@ def raw_query(
     referrer=None,
     is_grouprelease=False,
     use_cache=False,
-    use_snql=None,
     **kwargs,
 ) -> Mapping[str, Any]:
     """
@@ -649,11 +648,10 @@ def raw_query(
         **kwargs,
     )
 
-    if use_snql is None:
-        use_snql = should_use_snql(referrer)
+    snql_entity = should_use_snql(referrer)
 
     return bulk_raw_query(
-        [snuba_params], referrer=referrer, use_cache=use_cache, use_snql=use_snql
+        [snuba_params], referrer=referrer, use_cache=use_cache, snql_entity=snql_entity
     )[0]
 
 
@@ -690,11 +688,11 @@ def bulk_raw_query(
     snuba_param_list: Sequence[SnubaQueryParams],
     referrer: Optional[str] = None,
     use_cache: Optional[bool] = False,
-    use_snql: Optional[bool] = None,
+    snql_entity: Optional[str] = None,
 ) -> ResultSet:
     params = map(_prepare_query_params, snuba_param_list)
     return _apply_cache_and_build_results(
-        params, referrer=referrer, use_cache=use_cache, use_snql=use_snql
+        params, referrer=referrer, use_cache=use_cache, snql_entity=snql_entity
     )
 
 
@@ -702,7 +700,7 @@ def _apply_cache_and_build_results(
     snuba_param_list: Sequence[SnubaQueryBody],
     referrer: Optional[str] = None,
     use_cache: Optional[bool] = False,
-    use_snql: Optional[bool] = None,
+    snql_entity: Optional[str] = None,
 ) -> ResultSet:
     headers = {}
     if referrer:
@@ -730,7 +728,7 @@ def _apply_cache_and_build_results(
         to_query = [(query_pos, query_params, None) for query_pos, query_params in query_param_list]
 
     if to_query:
-        query_results = _bulk_snuba_query(map(itemgetter(1), to_query), headers, use_snql)
+        query_results = _bulk_snuba_query(map(itemgetter(1), to_query), headers, snql_entity)
         for result, (query_pos, _, cache_key) in zip(query_results, to_query):
             if cache_key:
                 cache.set(cache_key, json.dumps(result), settings.SENTRY_SNUBA_CACHE_TTL_SECONDS)
@@ -745,7 +743,7 @@ def _apply_cache_and_build_results(
 def _bulk_snuba_query(
     snuba_param_list: Sequence[SnubaQueryBody],
     headers: Mapping[str, str],
-    use_snql: Optional[bool] = None,
+    snql_entity: Optional[str] = None,
 ) -> ResultSet:
     with sentry_sdk.start_span(
         op="start_snuba_query",
@@ -763,8 +761,10 @@ def _bulk_snuba_query(
         query_fn = _snuba_query
         if isinstance(snuba_param_list[0][0], Query):
             query_fn = _snql_query
-        elif use_snql:
+        elif snql_entity is not None:
             query_fn = _snql_dryrun_query
+            # hack to pass this value in for now
+            headers["snql_entity"] = snql_entity
 
         if len(snuba_param_list) > 1:
             query_results = list(
@@ -866,9 +866,13 @@ def _snql_dryrun_query(params: Tuple[SnubaQuery, Hub, Mapping[str, str]]) -> Raw
     query_params, forward, reverse = query_data
     og_debug = query_params.get("debug", False)
     referrer = headers.get("referer", "<unknown>")
+    snql_entity = headers.get("snql_entity")
     try:
+        if snql_entity is None or snql_entity == "auto":
+            snql_entity = query_params["dataset"]
+
         metrics.incr("snuba.snql.dryrun.incoming", tags={"referrer": referrer})
-        query = json_to_snql(query_params, query_params["dataset"])
+        query = json_to_snql(query_params, snql_entity)
         query.validate()  # Call this here just avoid it happening in the async all
     except Exception as e:
         logger.warning(
@@ -979,7 +983,6 @@ def query(
     selected_columns=None,
     totals=None,
     use_cache=False,
-    use_snql=None,
     **kwargs,
 ):
 
@@ -1000,7 +1003,6 @@ def query(
             selected_columns=selected_columns,
             totals=totals,
             use_cache=use_cache,
-            use_snql=use_snql,
             **kwargs,
         )
     except (QueryOutsideRetentionError, QueryOutsideGroupActivityError):
@@ -1173,7 +1175,6 @@ def _aliased_query_impl(
     dataset=None,
     orderby=None,
     condition_resolver=None,
-    use_snql=None,
     **kwargs,
 ):
     if dataset is None:
@@ -1226,7 +1227,6 @@ def _aliased_query_impl(
         having=having,
         dataset=dataset,
         orderby=orderby,
-        use_snql=use_snql,
         **kwargs,
     )
 
