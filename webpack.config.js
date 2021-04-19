5767d2ba893a45a0b6d639bffe1fb1230f0a3b83
Billy Vong <billyvg@users.noreply.github.com>
2021-04-19 16:34:04 -0700

    build: Use `django-manifest-loader` for static assets (#25234)
    This changes the way we handle static assets. Previously, the assets were versioned by either a `version` file (commit sha?) or a timestamp. This changes it so that *only* assets generated from `webpack` are served at `/_assets/` and include a hash (based on file contents) in their filename. `webpack` also produces a manifest.json file that maps a key -> hashed filename which `django-manifest-loader` uses to load static assets.
    
    This will allow us to have better long term caching on these `webpack` assets as they will be cached until the contents of their chunks changes. Contrast that to the existing behavior where all assets are cached by the current release.
    
    Assets that are not processed by `webpack` (generally static assets only referenced from django templates) are still served at the existing URL. In the future we may have these processed through `webpack` so they can also be cached beyond the current release. However, the reason we'll want to keep this other static URL is for django apps like `django.contrib.admin` so that they can be at least cached by version. Alternatively, we could also choose to not cache these at all. AFAICT it's only the admin where this is needed, so it won't be used that often.
    
    Old:
    
    ```
    /_static/{version}/sentry/dist/app.js
    ```
    
    New:
    ```
    /_assets/sentry/app.{appHash}.js
    ```
    
    * ~Check CDN (fastly) settings to make sure that non-webpack assets are not cached long term~ No longer applies as we will maintain the existing URL versioning for non-webpack assets.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const fs = require('fs');
 const path = require('path');
 
 const {CleanWebpackPlugin} = require('clean-webpack-plugin'); // installed via npm
+const {WebpackManifestPlugin} = require('webpack-manifest-plugin');
 const webpack = require('webpack');
 const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
@@ -289,7 +290,7 @@ let appConfig = {
             options: {
               // This needs to be `false` because of platformicons package
               esModule: false,
-              name: '[folder]/[name].[hash:6].[ext]',
+              name: '[folder]/[name].[contenthash:6].[ext]',
             },
           },
         ],
@@ -306,6 +307,8 @@ let appConfig = {
   plugins: [
     new CleanWebpackPlugin(),
 
+    new WebpackManifestPlugin({}),
+
     /**
      * jQuery must be provided in the global scope specifically and only for
      * bootstrap, as it will not import jQuery itself.
@@ -321,7 +324,9 @@ let appConfig = {
     /**
      * Extract CSS into separate files.
      */
-    new MiniCssExtractPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash:6].css',
+    }),
 
     /**
      * Defines environment specific flags.
@@ -401,12 +406,15 @@ let appConfig = {
   },
   output: {
     path: distPath,
-    filename: '[name].js',
+    publicPath: '',
+    filename: '[name].[contenthash].js',
+    chunkFilename: '[name].[contenthash].js',
     sourceMapFilename: '[name].js.map',
   },
   optimization: {
     chunkIds: 'named',
     moduleIds: 'named',
+    runtimeChunk: {name: 'runtime'},
     splitChunks: {
       // Only affect async chunks, otherwise webpack could potentially split our initial chunks
       // Which means the app will not load because we'd need these additional chunks to be loaded in our
@@ -480,19 +488,17 @@ if (
 
     appConfig.devServer = {
       ...appConfig.devServer,
-      publicPath: '/_webpack',
+      publicPath: '/_static/dist/sentry',
       // syntax for matching is using https://www.npmjs.com/package/micromatch
       proxy: {
         '/api/store/**': relayAddress,
         '/api/{1..9}*({0..9})/**': relayAddress,
         '/api/0/relays/outcomes/': relayAddress,
-        '!/_webpack': backendAddress,
+        '!/_static/dist/sentry/**': backendAddress,
       },
-      before: app =>
-        app.use((req, _res, next) => {
-          req.url = req.url.replace(/^\/_static\/[^\/]+\/sentry\/dist/, '/_webpack');
-          next();
-        }),
+      writeToDisk: filePath => {
+        return /manifest\.json/.test(filePath);
+      },
     };
   }
 }
