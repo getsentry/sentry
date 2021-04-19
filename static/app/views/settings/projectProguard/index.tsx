58ba38c7f2fc71381c9967555d8e06654704c0c8
Matej Minar <matej.minar@sentry.io>
2021-04-19 12:00:49 +0200

    remove unused Feature checks

--- static/app/views/settings/projectProguard/index.tsx
+++ static/app/views/settings/projectProguard/index.tsx
@@ -1,21 +1,10 @@
 import React from 'react';
 
-import Alert from 'app/components/alert';
-import {t} from 'app/locale';
-import {PageContent} from 'app/styles/organization';
 import withOrganization from 'app/utils/withOrganization';
 
 import ProjectProguard from './projectProguard';
 
 class ProjectProguardContainer extends React.Component<ProjectProguard['props']> {
-  renderNoAccess() {
-    return (
-      <PageContent>
-        <Alert type="warning">{t("You don't have access to this feature")}</Alert>
-      </PageContent>
-    );
-  }
-
   render() {
     return <ProjectProguard {...this.props} />;
   }
