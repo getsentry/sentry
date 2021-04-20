a5a8073aa7fc573674b2f7a6d3a131d1ce75e194
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2021-04-20 16:13:02 -0700

    fix(ui): Alert details tooltip to use timezone / data interval (#25434)
    * determine default period based on alert window
    
    * chart tooltip should respect the timezone
    
    * style(lint): Auto commit lint changes
    
    * style(lint): Auto commit lint changes
    
    * fix the alert details header item gap in safari (gap in flex isnt yet supported)
    
    * use user timezone
    
    * style(lint): Auto commit lint changes
    
    Co-authored-by: sentry-internal-tools[bot] <66042841+sentry-internal-tools[bot]@users.noreply.github.com>

--- static/app/views/alerts/rules/details/metricChart.tsx
+++ static/app/views/alerts/rules/details/metricChart.tsx
@@ -13,14 +13,19 @@ import MarkArea from 'app/components/charts/components/markArea';
 import MarkLine from 'app/components/charts/components/markLine';
 import EventsRequest from 'app/components/charts/eventsRequest';
 import LineChart, {LineChartSeries} from 'app/components/charts/lineChart';
+import {
+  parseStatsPeriod,
+  StatsPeriodType,
+} from 'app/components/organizations/globalSelectionHeader/getParams';
 import {Panel, PanelBody, PanelFooter} from 'app/components/panels';
 import Placeholder from 'app/components/placeholder';
 import {IconCheckmark, IconFire, IconWarning} from 'app/icons';
 import {t} from 'app/locale';
+import ConfigStore from 'app/stores/configStore';
 import space from 'app/styles/space';
 import {AvatarProject, Organization, Project} from 'app/types';
 import {ReactEchartsRef} from 'app/types/echarts';
-import {getFormattedDate, getUtcDateString} from 'app/utils/dates';
+import {getUtcDateString} from 'app/utils/dates';
 import theme from 'app/utils/theme';
 import {makeDefaultCta} from 'app/views/settings/incidentRules/incidentRulePresets';
 import {IncidentRule} from 'app/views/settings/incidentRules/types';
@@ -57,6 +62,13 @@ type State = {
   width: number;
   height: number;
 };
+
+function formatTooltipDate(date: moment.MomentInput, format: string): string {
+  const {
+    options: {timezone},
+  } = ConfigStore.get('user');
+  return moment.tz(date, timezone).format(format);
+}
 
 function createThresholdSeries(lineColor: string, threshold: number): LineChartSeries {
   return {
@@ -138,7 +150,7 @@ function createIncidentSeries(
     trigger: 'item',
     alwaysShowContent: true,
     formatter: ({value, marker}) => {
-      const time = getFormattedDate(value, 'MMM D, YYYY LT');
+      const time = formatTooltipDate(moment(value), 'MMM D, YYYY LT');
       return [
         `<div class="tooltip-series"><div>`,
         `<span class="tooltip-label">${marker} <strong>${t('Alert')} #${
@@ -307,6 +319,7 @@ class MetricChart extends React.PureComponent<Props, State> {
     maxThresholdValue: number,
     maxSeriesValue: number
   ) {
+    const {interval} = this.props;
     const {dateModified, timeWindow} = this.props.rule || {};
     return (
       <LineChart
@@ -334,9 +347,13 @@ class MetricChart extends React.PureComponent<Props, State> {
             const [pointX, pointY] = pointData as [number, number];
             const isModified = dateModified && pointX <= new Date(dateModified).getTime();
 
-            const startTime = getFormattedDate(new Date(pointX), 'MMM D LT');
-            const endTime = getFormattedDate(
-              moment(new Date(pointX)).add(timeWindow, 'minutes'),
+            const startTime = formatTooltipDate(moment(pointX), 'MMM D LT');
+            const {period, periodLength} = parseStatsPeriod(interval) ?? {
+              periodLength: 'm',
+              period: `${timeWindow}`,
+            };
+            const endTime = formatTooltipDate(
+              moment(pointX).add(parseInt(period, 10), periodLength as StatsPeriodType),
               'MMM D LT'
             );
             const title = isModified
