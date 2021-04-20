233caa2a44b219cba644d1e8328b9be6021cb8e7
Danny Lee <dlee@sentry.io>
2021-04-20 11:53:59 -0700

    ref(org-stats): Improving layout of page (#25443)

--- static/app/views/usageStats/index.tsx
+++ static/app/views/usageStats/index.tsx
@@ -5,13 +5,13 @@ import {LocationDescriptorObject} from 'history';
 import omit from 'lodash/omit';
 import pick from 'lodash/pick';
 
-import Alert from 'app/components/alert';
-import {DateTimeObject} from 'app/components/charts/utils';
+import {SectionHeading} from 'app/components/charts/styles';
+import {DateTimeObject, getInterval} from 'app/components/charts/utils';
+import DropdownControl, {DropdownItem} from 'app/components/dropdownControl';
 import ErrorBoundary from 'app/components/errorBoundary';
 import PageHeading from 'app/components/pageHeading';
 import SentryDocumentTitle from 'app/components/sentryDocumentTitle';
 import {DEFAULT_RELATIVE_PERIODS, DEFAULT_STATS_PERIOD} from 'app/constants';
-import {IconInfo} from 'app/icons';
 import {t, tct} from 'app/locale';
 import {PageContent, PageHeader} from 'app/styles/organization';
 import space from 'app/styles/space';
@@ -22,8 +22,9 @@ import {
   Project,
   RelativePeriod,
 } from 'app/types';
+import {parsePeriodToHours} from 'app/utils/dates';
 
-import {ChartDataTransform} from './usageChart';
+import {CHART_OPTIONS_DATACATEGORY, ChartDataTransform} from './usageChart';
 import UsageStatsLastMin from './UsageStatsLastMin';
 import UsageStatsOrg from './usageStatsOrg';
 import UsageStatsProjects from './usageStatsProjects';
@@ -153,6 +154,87 @@ class OrganizationStats extends React.Component<Props> {
     return nextLocation;
   };
 
+  renderPageControl() {
+    const {organization} = this.props;
+
+    // Might deviate from server-side but this is cosmetic at the moment
+    const interval = getInterval(this.dataPeriod);
+    const {period} = this.dataPeriod;
+
+    // Remove options for relative periods shorter than 1 week
+    const relativePeriods = Object.keys(DEFAULT_RELATIVE_PERIODS).reduce((acc, key) => {
+      const periodDays = parsePeriodToHours(key) / 24;
+      if (periodDays >= 7) {
+        acc[key] = DEFAULT_RELATIVE_PERIODS[key];
+      }
+      return acc;
+    }, {});
+
+    return (
+      <Header>
+        <HeaderItemRow>
+          <HeaderItemRow>
+            <ItemPagePeriod>
+              <SectionHeading>{t('Display')}</SectionHeading>
+              <DropdownControl
+                label={
+                  <DropdownLabel>
+                    {DEFAULT_RELATIVE_PERIODS[period || DEFAULT_STATS_PERIOD]}
+                  </DropdownLabel>
+                }
+              >
+                {Object.keys(relativePeriods).map(key => (
+                  <DropdownItem
+                    key={key}
+                    eventKey={key}
+                    onSelect={(val: string) =>
+                      this.setStateOnUrl({pagePeriod: val as RelativePeriod})
+                    }
+                  >
+                    {DEFAULT_RELATIVE_PERIODS[key]}
+                  </DropdownItem>
+                ))}
+              </DropdownControl>
+            </ItemPagePeriod>
+
+            <ItemDataCategory>
+              <SectionHeadingInRow>{t('of')}</SectionHeadingInRow>
+              <DropdownControl
+                label={<DropdownLabel>{this.dataCategoryName}</DropdownLabel>}
+              >
+                {CHART_OPTIONS_DATACATEGORY.map(option => (
+                  <DropdownItem
+                    key={option.value}
+                    eventKey={option.value}
+                    onSelect={(val: string) =>
+                      this.setStateOnUrl({dataCategory: val as DataCategory})
+                    }
+                  >
+                    {option.label}
+                  </DropdownItem>
+                ))}
+              </DropdownControl>
+            </ItemDataCategory>
+          </HeaderItemRow>
+          <HeaderItemColumn>
+            <SectionHeading>{t('Interval')}</SectionHeading>
+            <HeaderItemValue>
+              <span>{interval}</span>
+            </HeaderItemValue>
+          </HeaderItemColumn>
+        </HeaderItemRow>
+
+        <ErrorBoundary mini>
+          <UsageStatsLastMin
+            organization={organization}
+            dataCategory={this.dataCategory}
+            dataCategoryName={this.dataCategoryName}
+          />
+        </ErrorBoundary>
+      </Header>
+    );
+  }
+
   render() {
     const {organization} = this.props;
 
@@ -160,36 +242,16 @@ class OrganizationStats extends React.Component<Props> {
       <SentryDocumentTitle title="Usage Stats">
         <PageContent>
           <PageHeader>
-            <PageHeading>
-              {tct('Organization Usage Stats for [dataCategory]', {
-                dataCategory: this.dataCategoryName,
-              })}
-            </PageHeading>
+            <PageHeading>{t('Organization Usage Stats')}</PageHeading>
           </PageHeader>
 
-          <OrgTextWrapper>
-            <OrgText>
-              <p>
-                {t(
-                  'The chart below reflects events that Sentry has received across your entire organization. We collect usage metrics on three types of events: errors, transactions, and attachments. Sessions are not included in this chart.'
-                )}
-              </p>
-              <p>
-                {t(
-                  'Each type of event is broken down into three categories: accepted, filtered, and dropped. Accepted events were successfully processed by Sentry. Filtered events were blocked due to your project’s inbound data filter rules. Dropped events were discarded due to invalid data, rate limits, quotas, or spike protection.'
-                )}
-              </p>
-            </OrgText>
-            <OrgLastMin>
-              <ErrorBoundary mini>
-                <UsageStatsLastMin
-                  organization={organization}
-                  dataCategory={this.dataCategory}
-                  dataCategoryName={this.dataCategoryName}
-                />
-              </ErrorBoundary>
-            </OrgLastMin>
-          </OrgTextWrapper>
+          {this.renderPageControl()}
+
+          <p>
+            {t(
+              'The chart below reflects events that Sentry has received across your entire organization. We collect usage metrics on three types of events: errors, transactions, and attachments. Sessions are not included in this chart.'
+            )}
+          </p>
 
           <ErrorBoundary mini>
             <UsageStatsOrg
@@ -210,9 +272,7 @@ class OrganizationStats extends React.Component<Props> {
             </PageHeading>
           </PageHeader>
 
-          <Alert type="info" icon={<IconInfo size="md" />}>
-            {t('Only usage stats for your projects are displayed here.')}
-          </Alert>
+          <p>{t('Only usage stats for your projects are displayed here.')}</p>
 
           <ErrorBoundary mini>
             <UsageStatsProjects
@@ -233,28 +293,53 @@ class OrganizationStats extends React.Component<Props> {
 
 export default OrganizationStats;
 
-const OrgTextWrapper = styled('div')`
-  display: grid;
-  grid-auto-flow: row;
-
-  @media (min-width: ${p => p.theme.breakpoints[0]}) {
-    grid-auto-flow: column;
-    grid-template-columns: 75% 25%;
-  }
-`;
-
-const OrgText = styled('div')`
-  max-width: ${p => p.theme.breakpoints[0]};
-`;
-
-const OrgLastMin = styled('div')`
+const Header = styled('div')`
   display: flex;
-  justify-content: center;
-  padding: ${space(4)} 0;
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: space-between;
+  margin-bottom: ${space(3)};
+`;
+const HeaderItem = styled('div')`
+  display: flex;
+  margin-right: ${space(3)};
+`;
+const HeaderItemRow = styled(HeaderItem)`
+  flex-direction: row;
+  align-items: flex-end;
+  justify-content: flex-start;
+`;
+const HeaderItemColumn = styled(HeaderItem)`
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+`;
+const HeaderItemValue = styled('div')`
+  display: flex;
+  align-items: center;
+  min-height: 40px;
 
-  @media (min-width: ${p => p.theme.breakpoints[0]}) {
-    justify-content: flex-end;
-    align-items: center;
-    padding: 0 0 ${space(4)};
+  > span {
+    font-weight: 600;
+    font-size: ${p => p.theme.fontSizeMedium};
+    color: ${p => p.theme.gray500}; /* Make it same as dropdown */
   }
+`;
+
+const ItemPagePeriod = styled('div')`
+  display: flex;
+  flex-direction: column;
+  margin-right: ${space(2)};
+`;
+const ItemDataCategory = styled('div')`
+  display: flex;
+  flex-direction: row;
+  margin-right: ${space(2)};
+`;
+const SectionHeadingInRow = styled(SectionHeading)`
+  margin-right: ${space(2)};
+`;
+const DropdownLabel = styled('span')`
+  min-width: 100px;
+  text-align: left;
 `;
