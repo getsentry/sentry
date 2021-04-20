ec508429824019bb14635d33a230b7bd8e3b58ec
Scott Cooper <scttcper@gmail.com>
2021-04-19 23:08:33 -0700

    add another rule

--- tests/acceptance/test_organization_alert_rules.py
+++ tests/acceptance/test_organization_alert_rules.py
@@ -11,7 +11,6 @@ class OrganizationAlertRulesListTest(AcceptanceTestCase, SnubaTestCase):
     def setUp(self):
         super().setUp()
         self.login_as(self.user)
-        self.org = self.create_organization()
         self.path = f"/organizations/{self.organization.slug}/alerts/rules/"
 
     def test_empty_alert_rules(self):
@@ -33,9 +32,15 @@ class OrganizationAlertRulesListTest(AcceptanceTestCase, SnubaTestCase):
             self.browser.wait_until_not(".loading-indicator")
             self.browser.snapshot("alert rules - list")
 
-    def test_alert_rules_incidents(self):
+    def test_alert_rules_alert_list(self):
+        self.create_alert_rule(
+            name="My Alert Rule",
+            projects=[self.project],
+            date_added=timezone.now(),
+            user=self.user,
+        )
         alert_rule_critical = self.create_alert_rule(
-            organization=self.org,
+            organization=self.organization,
             projects=[self.project],
             name="some rule [crit]",
             query="",
