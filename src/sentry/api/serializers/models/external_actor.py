04c8d9e27f4f45929490676acd7bc66b1d8dd95d
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-04-20 15:54:06 -0700

    Make 'key' a keyword arguement

--- src/sentry/api/serializers/models/external_actor.py
+++ src/sentry/api/serializers/models/external_actor.py
@@ -1,4 +1,4 @@
-from typing import Any, Mapping
+from typing import Any, Mapping, Optional
 
 from sentry.api.serializers import Serializer, register
 from sentry.models import ExternalActor
@@ -8,7 +8,12 @@ from sentry.types.integrations import get_provider_string
 @register(ExternalActor)
 class ExternalActorSerializer(Serializer):
     def serialize(
-        self, obj: ExternalActor, attrs: Mapping[Any, Any], user: Any, **kwargs
+        self,
+        obj: ExternalActor,
+        attrs: Mapping[Any, Any],
+        user: Any,
+        key: Optional[str] = None,
+        **kwargs: Any,
     ) -> Mapping[str, Any]:
         provider = get_provider_string(obj.provider)
         data = {
@@ -21,7 +26,6 @@ class ExternalActorSerializer(Serializer):
             data["externalId"] = obj.external_id
 
         # Extra context `key` tells the API how to resolve actor_id.
-        key = kwargs.pop("key", None)
         if key == "user":
             data["userId"] = str(obj.actor.resolve().id)
         elif key == "team":
