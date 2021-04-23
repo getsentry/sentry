f2795f5bef901b7c07555d9103e91cb2461a34b8
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-04-23 10:47:20 -0700

    Avoid the need for bookkeeping

--- config/cdc/postgres-entrypoint.sh
+++ config/cdc/postgres-entrypoint.sh
@@ -19,9 +19,7 @@ cdc_setup_hba_conf() {
     PG_HBA="$PGDATA/pg_hba.conf"
     if [ ! -f "$PG_HBA" ]; then
         echo "DB not initialized. Postgres will take care of pg_hba"
-    fi
-
-    if [ "$(grep -c -E "^host\s+replication" "$PGDATA"/pg_hba.conf)" -ge 0 ]; then
+    elif [ "$(grep -c -E "^host\s+replication" "$PGDATA"/pg_hba.conf)" != 0 ]; then
         echo "Replication config already present in pg_hba. Not changing anything."
     else
         # Execute the same script we run on DB initialization
@@ -41,6 +39,7 @@ install_wal2json() {
 
     set +e
 
+    LATEST_VERSION_FILE="/wal2json/latest.so"
     ARCH=$(uname -m)
     FILE_NAME="wal2json-Linux-$ARCH.so"
     LATEST_VERSION=$(
@@ -55,16 +54,16 @@ install_wal2json() {
             if wget \
                 "https://github.com/getsentry/wal2json/releases/download/$LATEST_VERSION/$FILE_NAME" \
                 -P "/wal2json/$LATEST_VERSION/"; then
-                echo "$LATEST_VERSION" > /wal2json/latest_version
+                ln -s "/wal2json/$LATEST_VERSION/$FILE_NAME" "$LATEST_VERSION_FILE"
             fi
         fi
-        ln -s "/wal2json/$LATEST_VERSION/$FILE_NAME" /usr/local/lib/postgresql/wal2json.so
-    elif [ -f /wal2json/latest_version ]; then
+        ln -s "$LATEST_VERSION_FILE" /usr/local/lib/postgresql/wal2json.so
+    elif [ -f $LATEST_VERSION_FILE ]; then
         # We did not manage to detect the latest version or we failed at downloading.
         # Try to failover
-        LATEST_VERSION=$(cat /wal2json/latest_version)
-        echo "Cannot download latest version. Found $LATEST_VERSION on disk"
-        ln -s "/wal2json/$LATEST_VERSION/$FILE_NAME" /usr/local/lib/postgresql/wal2json.so
+        REAL_FILE=$(realpath $LATEST_VERSION_FILE)
+        echo "Cannot download latest version. Found $REAL_FILE on disk"
+        ln -s "$LATEST_VERSION_FILE" /usr/local/lib/postgresql/wal2json.so
     elif [ -f "/usr/local/lib/postgresql/wal2json.so" ]; then
         # Somehow our volume is not in a good state but there is still a version
         # in the library directory. We take that one.
