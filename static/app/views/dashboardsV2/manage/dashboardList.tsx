d5816e091b13c474b32d3c79290e3238a19e8c09
Shruthi <shruthilaya.jaganathan@sentry.io>
2021-04-23 08:37:43 -0400

    Address comments
    Co-authored-by: Priscila Oliveira <priscilawebdev@gmail.com>

--- static/app/views/dashboardsV2/manage/dashboardList.tsx
+++ static/app/views/dashboardsV2/manage/dashboardList.tsx
@@ -94,7 +94,7 @@ function DashboardList({
           renderWidgets={() => (
             <WidgetGrid>
               {dashboard.widgetDisplay.map((displayType, i) => {
-                return displayType === 'big_number' ? (
+                return displayType === DisplayType.BIG_NUMBER ? (
                   <BigNumberWidgetWrapper
                     key={`${i}-${displayType}`}
                     src={miniWidget(displayType)}
