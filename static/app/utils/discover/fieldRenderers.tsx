22f91310ffa0e46930c5082a90575336fbff70d8
k-fish <kevan.fisher@sentry.io>
2021-04-26 10:08:29 -0700

    ref(landing): Add new field renderer for unique users
    This is some cleanup for performance landing to add a rendered for unique users instead of a custom condition in the vital detail table.
    Refs VIS-427

--- static/app/utils/discover/fieldRenderers.tsx
+++ static/app/utils/discover/fieldRenderers.tsx
@@ -36,10 +36,12 @@ import KeyTransactionField from './keyTransactionField';
 import {
   BarContainer,
   Container,
+  FlexContainer,
   NumberContainer,
   OverflowLink,
   StyledDateTime,
   StyledShortId,
+  UserIcon,
   VersionContainer,
 } from './styles';
 
@@ -182,6 +184,7 @@ type SpecialFields = {
   project: SpecialField;
   user: SpecialField;
   'user.display': SpecialField;
+  'count_unique(user)': SpecialField;
   'issue.id': SpecialField;
   'error.handled': SpecialField;
   issue: SpecialField;
@@ -316,6 +319,24 @@ const SPECIAL_FIELDS: SpecialFields = {
 
         const badge = <UserBadge user={userObj} hideEmail avatarSize={16} />;
         return <Container>{badge}</Container>;
+      }
+
+      return <Container>{emptyValue}</Container>;
+    },
+  },
+  'count_unique(user)': {
+    sortField: 'count_unique(user)',
+    renderFunc: data => {
+      const count = data.count_unique_user;
+      if (count && typeof count === 'number') {
+        return (
+          <FlexContainer>
+            <NumberContainer>
+              <Count value={count} />
+            </NumberContainer>
+            <UserIcon size="20" />
+          </FlexContainer>
+        );
       }
 
       return <Container>{emptyValue}</Container>;
