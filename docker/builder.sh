76f59326aab5f24cc4e769d3849afb8a81273e32
Burak Yigit Kaya <ben@byk.im>
2021-04-26 21:47:10 +0300

    brute force

--- docker/builder.sh
+++ docker/builder.sh
@@ -2,16 +2,12 @@
 
 set -e
 
-# Ensure custom workspace
-ln -s $(pwd) /js/
-cd /js
-
-if [[ ! -f workspace/setup.py ]]; then
+if [[ ! -f setup.py ]]; then
     >&2 echo "Cannot find setup.py, make sure you have mounted your source dir to /workspace"
     exit 1
 fi
 
 export YARN_CACHE_FOLDER="$(mktemp -d)"
-python workspace/setup.py bdist_wheel --build-number 0
+python setup.py bdist_wheel --build-number 0
 rm -r "$YARN_CACHE_FOLDER"
-pkginfo -f requires_dist --single --sequence-delim=! workspace/dist/*.whl | tr ! \\n > workspace/dist/requirements.txt
+pkginfo -f requires_dist --single --sequence-delim=! dist/*.whl | tr ! \\n > dist/requirements.txt
