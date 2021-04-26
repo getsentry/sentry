a868b1a2536fb33c35fcde0ab9103e8ed7a6a303
Burak Yigit Kaya <ben@byk.im>
2021-04-26 18:30:06 +0300

    lol5

--- docker/builder.sh
+++ docker/builder.sh
@@ -3,17 +3,15 @@
 set -e
 
 # Ensure custom workspace
-echo $(pwd)
 ln -s $(pwd) /js/
-cd /js/workspace
-rm package.json
+cd /js
 
-if [[ ! -f setup.py ]]; then
+if [[ ! -f workspace/setup.py ]]; then
     >&2 echo "Cannot find setup.py, make sure you have mounted your source dir to /workspace"
     exit 1
 fi
 
 export YARN_CACHE_FOLDER="$(mktemp -d)"
-python setup.py bdist_wheel --build-number 0
+python workspace/setup.py bdist_wheel --build-number 0
 rm -r "$YARN_CACHE_FOLDER"
-pkginfo -f requires_dist --single --sequence-delim=! dist/*.whl | tr ! \\n > dist/requirements.txt
+pkginfo -f requires_dist --single --sequence-delim=! workspace/dist/*.whl | tr ! \\n > workspace/dist/requirements.txt
