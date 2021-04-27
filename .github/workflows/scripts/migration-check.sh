getsentry/sentry

commit 4530f41672418092bde6f2b76d588eaabc5a4cdf

ci(feat): New migrations without updated lockfile will fail  (#25499)
In order to prevent merging new migrations without updating the `migration_lockfile.txt` we need
to catch it in the CI.

Using `--check` with `sentry django makemigrations --check` now compares the listed migrations in `migration_lockfile.txt` against the latest migration on-disk (for each app). If they differ, it's because the lock file has not been updated.

The lockfile now only contains apps that we maintain.

Fixes #24828

.github/workflows/check-if-migration-is-required.yml
@@ -1,4 +1,4 @@
-name: check if migration is required
+name: check migration
 on:
   pull_request:
     paths:
@@ -8,7 +8,7 @@ on:
 
 jobs:
   main:
-    name: is migration required
+    name: check migration
     runs-on: ubuntu-20.04
 
     steps:
@@ -46,4 +46,9 @@ jobs:
           PGPASSWORD: postgres
         run: |
           # Below will exit with non-zero status if model changes are missing migrations
-          sentry django makemigrations -n ci_test --check --dry-run --no-input || (echo '::error::Error: Migration required -- to generate a migration, run `sentry django makemigrations -n <some_name>`' && exit 1)
+          sentry django makemigrations --check --dry-run --no-input
+          if [ "$?" == 1 ]; then
+            echo >&2 "::error::Error: Migration required -- to generate a migration, run `sentry django makemigrations -n <some_name> && git add migrations_lockfile.txt`" && exit 1
+          else:
+            echo >&2 "::error::Error: Migration lockfile mismatch -- run `sentry django makemigrations -n <some_name> && git add migrations_lockfile.txt`" && exit 1
+          fi

src/sentry/conf/server.py
@@ -2104,7 +2104,12 @@ SOUTH_MIGRATION_CONVERSIONS = (
 MIGRATIONS_TEST_MIGRATE = os.environ.get("MIGRATIONS_TEST_MIGRATE", "0") == "1"
 # Specifies the list of django apps to include in the lockfile. If Falsey then include
 # all apps with migrations
-MIGRATIONS_LOCKFILE_APP_WHITELIST = ()
+MIGRATIONS_LOCKFILE_APP_WHITELIST = (
+    "jira_ac",
+    "nodestore",
+    "sentry",
+    "social_auth",
+)
 # Where to write the lockfile to.
 MIGRATIONS_LOCKFILE_PATH = os.path.join(PROJECT_ROOT, os.path.pardir, os.path.pardir)
 

src/sentry/management/commands/makemigrations.py
@@ -1,4 +1,5 @@
 import os
+import sys
 
 from django.conf import settings
 from django.core.management.commands import makemigrations
@@ -15,6 +16,26 @@ will then be regenerated, and you should be able to merge without conflicts.
 """
 
 
+# We check that the latest migration is the one stored in the lockfile
+def validate(migrations_filepath, latest_migration_by_app):
+    infile = {}
+    with open(migrations_filepath, encoding="utf-8") as file:
+        for line in file:
+            try:
+                app_label, name = line.split(": ")
+                infile[app_label] = name.strip()
+            except ValueError:
+                pass
+
+    for app_label, name in sorted(latest_migration_by_app.items()):
+        if infile[app_label] != name:
+            print(  # noqa: B314
+                f"ERROR: The latest migration does not match the one in the lockfile -> `{app_label}` app: {name} vs {infile[app_label]}"
+            )
+            # makemigrations.Command --check exits with 1 if a migration needs to be generated
+            sys.exit(2)
+
+
 class Command(makemigrations.Command):
     """
     Generates a lockfile so that Git will detect merge conflicts if there's a migration
@@ -22,7 +43,7 @@ class Command(makemigrations.Command):
     """
 
     def handle(self, *app_labels, **options):
-        if not options["name"]:
+        if not options["name"] and not options.get("check_changes"):
             self.stderr.write(
                 "Please name your migrations using `-n <migration_name>`. "
                 "For example, `-n backfill_my_new_table`"
@@ -44,11 +65,16 @@ class Command(makemigrations.Command):
                 latest_migration_by_app.get(app_label, ""), name
             )
 
-        result = "\n".join(
-            f"{app_label}: {name}" for app_label, name in sorted(latest_migration_by_app.items())
+        migrations_filepath = os.path.join(
+            settings.MIGRATIONS_LOCKFILE_PATH, "migrations_lockfile.txt"
         )
+        if options.get("check_changes"):
+            validate(migrations_filepath, latest_migration_by_app)
+        else:
+            result = "\n".join(
+                f"{app_label}: {name}"
+                for app_label, name in sorted(latest_migration_by_app.items())
+            )
 
-        with open(
-            os.path.join(settings.MIGRATIONS_LOCKFILE_PATH, "migrations_lockfile.txt"), "w"
-        ) as f:
-            f.write(template % result)
+            with open(migrations_filepath, "w") as f:
+                f.write(template % result)

commit 7b0ced1100f65309df886cf616354d0f8402ffcd

fix(ci): Fix migration checks regression (#25633)
This fixes a regression introduced in #25499.

When `sentry django makemigrations --check` does not exit with exit code zero, the
conditional checks in the next line will not be evaluated.

This captures the stdout/stderr into a variable and the exit code into another one.

.github/workflows/check-if-migration-is-required.yml
@@ -5,6 +5,8 @@ on:
       # Matches all python files regardless of directory depth.
       - '**.py'
       - requirements*.txt
+      - .github/workflows/check-if-migration-is-required.yml
+      - .github/workflows/scripts/migration-check.sh
 
 jobs:
   main:
@@ -40,15 +42,9 @@ jobs:
         uses: ./.github/actions/setup-sentry
         id: setup
 
-      - name: Check if a migration is required
+      - name: Migration & lockfile checks
         env:
           SENTRY_LOG_LEVEL: ERROR
           PGPASSWORD: postgres
         run: |
-          # Below will exit with non-zero status if model changes are missing migrations
-          sentry django makemigrations --check --dry-run --no-input
-          if [ "$?" == 1 ]; then
-            echo >&2 "::error::Error: Migration required -- to generate a migration, run `sentry django makemigrations -n <some_name> && git add migrations_lockfile.txt`" && exit 1
-          else:
-            echo >&2 "::error::Error: Migration lockfile mismatch -- run `sentry django makemigrations -n <some_name> && git add migrations_lockfile.txt`" && exit 1
-          fi
+          ./.github/workflows/scripts/migration-check.sh

.github/workflows/scripts/migration-check.sh
@@ -1,0 +1,15 @@
+#!/bin/bash
+# This script expects devservices to be running. It's main purpose is to check
+# that migrations and lockfiles are correct
+set -eu
+# This will fail if a migration or the lockfile are missing in the PR
+exit_code=0
+sentry django makemigrations --check --dry-run --no-input || exit_code=$?
+if [ "$exit_code" == 1 ]; then
+    echo -e "::error::Error: Migration required -- to generate a migration, run:\n" \
+        "sentry django makemigrations -n <some_name> && git add migrations_lockfile.txt" >&2
+else
+    echo -e "::error::Error: Migration lockfile mismatch -- run:\n" \
+        "sentry django makemigrations -n <some_name> && git add migrations_lockfile.txt" >&2
+fi
+exit $exit_code