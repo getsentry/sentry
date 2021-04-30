22444e93dbbe99ec55624b6075c506be6e7276b4
Josh Ferge <josh.ferge@sentry.io>
2021-04-30 09:33:18 -0700

    init stubs

--- src/sentry/mediators/sentry_app_installations/destroyer.py
+++ src/sentry/mediators/sentry_app_installations/destroyer.py
@@ -66,6 +66,7 @@ class Destroyer(Mediator):
             # if the error is from a request exception, log the error and continue
             except RequestException as exc:
                 self.log(error=exc)
+        print(self.install)
         self.install.delete()
 
     def audit(self):
