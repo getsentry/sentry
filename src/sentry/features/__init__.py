a94132d4e5dee21104aad1772f8d9f1943c9e421
William Mak <william@wmak.io>
2021-05-04 11:43:20 -0400

    chore(performance): Clean up old feature flags (#25810)
    - These flags have been in GA for a while now, removing them

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -101,11 +101,9 @@ default_manager.add("organizations:notification-platform", OrganizationFeature) 
 default_manager.add("organizations:onboarding", OrganizationFeature)  # NOQA
 default_manager.add("organizations:org-saved-searches", OrganizationFeature)  # NOQA
 default_manager.add("organizations:org-subdomains", OrganizationFeature)  # NOQA
-default_manager.add("organizations:performance-landing-v2", OrganizationFeature)  # NOQA
 default_manager.add("organizations:performance-ops-breakdown", OrganizationFeature)  # NOQA
 default_manager.add("organizations:performance-tag-explorer", OrganizationFeature)  # NOQA
 default_manager.add("organizations:performance-view", OrganizationFeature)  # NOQA
-default_manager.add("organizations:performance-vitals-overview", OrganizationFeature)  # NOQA
 default_manager.add("organizations:related-events", OrganizationFeature)  # NOQA
 default_manager.add("organizations:relay", OrganizationFeature)  # NOQA
 default_manager.add("organizations:reprocessing-v2", OrganizationFeature)  # NOQA
