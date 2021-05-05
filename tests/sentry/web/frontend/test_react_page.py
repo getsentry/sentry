ecf3fc89970cc4ec3775d380c643f1613110c8a8
Ryan Skonnord <ryan.skonnord@sentry.io>
2021-05-05 13:15:23 -0700

    Make URL pattern narrower; update test

--- tests/sentry/web/frontend/test_react_page.py
+++ tests/sentry/web/frontend/test_react_page.py
@@ -73,15 +73,20 @@ class ReactPageViewTest(TestCase):
         self.assertTemplateUsed(resp, "sentry/bases/react.html")
         assert resp.context["request"]
 
-    def test_org_settings_captures_slug(self):
+    def test_org_subpages_capture_slug(self):
         owner = self.create_user("bar@example.com")
         org = self.create_organization(owner=owner)
         # User is *not* logged in. Check for redirect to org's auth login.
 
+        # TODO(RyanSkonnord): Generalize URL pattern; add
+        #  f"/organizations/{org.slug}/new_page_that_does_not_exist_yet/"
+        #  to list of test paths without causing other regressions.
+        #  See OrganizationReleasesTest.test_detail_global_header,
+        #  which exposes a buggy interaction with appendTrailingSlash
+        #  in static/app/routes.tsx.
         for path in [
             f"/organizations/{org.slug}/settings/",
             f"/organizations/{org.slug}/discover/",
-            f"/organizations/{org.slug}/new_page_that_does_not_exist_yet/",
             f"/settings/{org.slug}/developer-settings/",
             f"/settings/{org.slug}/new_page_that_does_not_exist_yet/",
         ]:
