d33194847a7269b4b2b2fc126926262f7d416335
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2021-05-06 09:58:20 -0700

    feat(alerts): Turn on alert details for all self hosted (#25888)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -984,7 +984,7 @@ SENTRY_FEATURES = {
     # Enable inbox support in the issue stream
     "organizations:inbox": True,
     # Enable the new alert details ux design
-    "organizations:alert-details-redesign": False,
+    "organizations:alert-details-redesign": True,
     # Enable the new images loaded design and features
     "organizations:images-loaded-v2": True,
     # Enable teams to have ownership of alert rules
