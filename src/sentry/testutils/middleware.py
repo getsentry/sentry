d0b0bba2c436bb6d4c42cf22ad2aae6e658f4391
Joshua Li <joshuarli98@gmail.com>
2021-05-10 15:06:08 -0700

    assume updated django-sudo, also remove unused test middleware

--- src/sentry/testutils/middleware.py
+++ src/sentry/testutils/middleware.py
@@ -1,24 +1,6 @@
-from django.utils.deprecation import MiddlewareMixin
-
 from sentry.middleware.sudo import SudoMiddleware as BaseSudoMiddleware
 
 
-class BrokenRequestMiddleware(MiddlewareMixin):
-    def process_request(self, request):
-        raise ImportError("request")
-
-
-class BrokenResponseMiddleware(MiddlewareMixin):
-    def process_response(self, request, response):
-        raise ImportError("response")
-
-
-class BrokenViewMiddleware(MiddlewareMixin):
-    def process_view(self, request, func, args, kwargs):
-        raise ImportError("view")
-
-
-# HACK: MiddlewareMixin is temporary, until I update diango-sudo.
-class SudoMiddleware(BaseSudoMiddleware, MiddlewareMixin):
+class SudoMiddleware(BaseSudoMiddleware):
     def has_sudo_privileges(self, request):
         return True
