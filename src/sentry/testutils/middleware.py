6579d4c3843b028528331dd1155c6d6812199802
Joshua Li <joshuarli98@gmail.com>
2021-05-10 14:05:04 -0700

    temporary patch for SudoMiddleware until django-sudo is updated

--- src/sentry/testutils/middleware.py
+++ src/sentry/testutils/middleware.py
@@ -1,21 +1,24 @@
+from django.utils.deprecation import MiddlewareMixin
+
 from sentry.middleware.sudo import SudoMiddleware as BaseSudoMiddleware
 
 
-class BrokenRequestMiddleware:
+class BrokenRequestMiddleware(MiddlewareMixin):
     def process_request(self, request):
         raise ImportError("request")
 
 
-class BrokenResponseMiddleware:
+class BrokenResponseMiddleware(MiddlewareMixin):
     def process_response(self, request, response):
         raise ImportError("response")
 
 
-class BrokenViewMiddleware:
+class BrokenViewMiddleware(MiddlewareMixin):
     def process_view(self, request, func, args, kwargs):
         raise ImportError("view")
 
 
-class SudoMiddleware(BaseSudoMiddleware):
+# HACK: MiddlewareMixin is temporary, until I update diango-sudo.
+class SudoMiddleware(BaseSudoMiddleware, MiddlewareMixin):
     def has_sudo_privileges(self, request):
         return True
