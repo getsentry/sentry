2f396c4130af78f0a5fc714e959b16bbc82c06bb
Alberto Leal <mail4alberto@gmail.com>
2021-05-11 17:51:03 -0400

    Update tests/js/spec/views/eventsV2/results.spec.jsx
    Co-authored-by: k-fish <kevan.fisher@sentry.io>

--- tests/js/spec/views/eventsV2/results.spec.jsx
+++ tests/js/spec/views/eventsV2/results.spec.jsx
@@ -649,6 +649,7 @@ describe('EventsV2 > Results', function () {
     wrapper.update();
 
     // Should load events again
+    expect(eventsStatsMock).toHaveBeenCalledTimes(2);
     expect(eventsStatsMock).toHaveBeenNthCalledWith(
       2,
       '/organizations/org-slug/events-stats/',
