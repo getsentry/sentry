96b7de1df85c4c9533cffc3df977745b69c13104
Scott Cooper <scttcper@gmail.com>
2021-05-12 13:50:03 -1000

    split filter into its own file

--- static/app/views/alerts/rules/index.tsx
+++ static/app/views/alerts/rules/index.tsx
@@ -5,8 +5,6 @@ import flatten from 'lodash/flatten';
 
 import {addErrorMessage} from 'app/actionCreators/indicator';
 import AsyncComponent from 'app/components/asyncComponent';
-import CheckboxFancy from 'app/components/checkboxFancy/checkboxFancy';
-import Input from 'app/components/forms/input';
 import * as Layout from 'app/components/layouts/thirds';
 import ExternalLink from 'app/components/links/externalLink';
 import Link from 'app/components/links/link';
@@ -17,7 +15,6 @@ import SearchBar from 'app/components/searchBar';
 import SentryDocumentTitle from 'app/components/sentryDocumentTitle';
 import {IconArrow, IconCheckmark} from 'app/icons';
 import {t, tct} from 'app/locale';
-import overflowEllipsis from 'app/styles/overflowEllipsis';
 import space from 'app/styles/space';
 import {GlobalSelection, Organization, Project, Team} from 'app/types';
 import {trackAnalyticsEvent} from 'app/utils/analytics';
@@ -29,8 +26,8 @@ import AlertHeader from '../list/header';
 import {CombinedMetricIssueAlerts} from '../types';
 import {isIssueAlert} from '../utils';
 
-import Filter from './filter';
 import RuleListRow from './row';
+import TeamFilter from './teamFilter';
 
 const DOCS_URL = 'https://docs.sentry.io/product/alerts-notifications/metric-alerts/';
 const ALERT_LIST_QUERY_DEFAULT_TEAMS = ['myteams', 'unassigned'];
@@ -164,73 +161,14 @@ class AlertRulesList extends AsyncComponent<Props, State & AsyncComponent['state
 
   renderFilterBar() {
     const {teams, location} = this.props;
-    const {teamFilterSearch} = this.state;
-    const selectedTeams = new Set(this.getTeamQuery());
-    const additionalOptions = [
-      {label: t('My Teams'), value: 'myteams'},
-      {label: t('Unassigned'), value: 'unassigned'},
-    ];
-    const optionValues = [
-      ...teams.map(({id}) => id),
-      ...additionalOptions.map(({value}) => value),
-    ];
-    const filteredTeams = teams.filter(({name}) =>
-      teamFilterSearch
-        ? name.toLowerCase().includes(teamFilterSearch.toLowerCase())
-        : true
-    );
+
     return (
       <FilterWrapper>
-        <Filter
-          header={
-            <StyledInput
-              autoFocus
-              placeholder={t('Filter by team name')}
-              onClick={event => {
-                event.stopPropagation();
-              }}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-                this.setState({teamFilterSearch: event.target.value});
-              }}
-              value={this.state.teamFilterSearch || ''}
-            />
-          }
-          headerLabel={t('Team')}
-          onFilterChange={this.handleChangeFilter}
-          filterList={optionValues}
-          selection={selectedTeams}
-        >
-          {({toggleFilter}) => (
-            <List>
-              {additionalOptions.map(({label, value}) => (
-                <ListItem
-                  key={value}
-                  isChecked={selectedTeams.has(value)}
-                  onClick={event => {
-                    event.stopPropagation();
-                    toggleFilter(value);
-                  }}
-                >
-                  <TeamName>{label}</TeamName>
-                  <CheckboxFancy isChecked={selectedTeams.has(value)} />
-                </ListItem>
-              ))}
-              {filteredTeams.map(({id, name}) => (
-                <ListItem
-                  key={id}
-                  isChecked={selectedTeams.has(id)}
-                  onClick={event => {
-                    event.stopPropagation();
-                    toggleFilter(id);
-                  }}
-                >
-                  <TeamName>{name}</TeamName>
-                  <CheckboxFancy isChecked={selectedTeams.has(id)} />
-                </ListItem>
-              ))}
-            </List>
-          )}
-        </Filter>
+        <TeamFilter
+          teams={teams}
+          selectedTeams={new Set(this.getTeamQuery())}
+          handleChangeFilter={this.handleChangeFilter}
+        />
         <StyledSearchBar
           placeholder={t('Search by name')}
           query={location.query?.name}
@@ -433,11 +371,6 @@ const StyledSortLink = styled(Link)`
   }
 `;
 
-const TeamName = styled('div')`
-  font-size: ${p => p.theme.fontSizeMedium};
-  ${overflowEllipsis};
-`;
-
 const FilterWrapper = styled('div')`
   display: flex;
   margin-bottom: ${space(1.5)};
@@ -446,42 +379,6 @@ const FilterWrapper = styled('div')`
 const StyledSearchBar = styled(SearchBar)`
   flex-grow: 1;
   margin-left: ${space(1.5)};
-`;
-
-const List = styled('ul')`
-  list-style: none;
-  margin: 0;
-  padding: 0;
-`;
-
-const StyledInput = styled(Input)`
-  border: none;
-  border-bottom: 1px solid ${p => p.theme.gray200};
-  border-radius: 0;
-`;
-
-const ListItem = styled('li')<{isChecked?: boolean}>`
-  display: grid;
-  grid-template-columns: 1fr max-content;
-  grid-column-gap: ${space(1)};
-  align-items: center;
-  padding: ${space(1)} ${space(2)};
-  border-bottom: 1px solid ${p => p.theme.border};
-  :hover {
-    background-color: ${p => p.theme.backgroundSecondary};
-  }
-  ${CheckboxFancy} {
-    opacity: ${p => (p.isChecked ? 1 : 0.3)};
-  }
-
-  &:hover ${CheckboxFancy} {
-    opacity: 1;
-  }
-
-  &:hover span {
-    color: ${p => p.theme.blue300};
-    text-decoration: underline;
-  }
 `;
 
 const StyledPanelTable = styled(PanelTable)<{
---
+++ static/app/views/alerts/rules/teamFilter.tsx
@@ -0,0 +1,128 @@
+import {useState} from 'react';
+import styled from '@emotion/styled';
+
+import CheckboxFancy from 'app/components/checkboxFancy/checkboxFancy';
+import Input from 'app/components/forms/input';
+import {t} from 'app/locale';
+import overflowEllipsis from 'app/styles/overflowEllipsis';
+import space from 'app/styles/space';
+import {Team} from 'app/types';
+
+import Filter from './filter';
+
+type Props = {
+  teams: Team[];
+  selectedTeams: Set<string>;
+  handleChangeFilter: (activeFilters: Set<string>) => void;
+};
+
+function TeamFilter({teams, selectedTeams, handleChangeFilter}: Props) {
+  const [teamFilterSearch, setTeamFilterSearch] = useState<string | undefined>();
+  const additionalOptions = [
+    {label: t('My Teams'), value: 'myteams'},
+    {label: t('Unassigned'), value: 'unassigned'},
+  ];
+  const optionValues = [
+    ...teams.map(({id}) => id),
+    ...additionalOptions.map(({value}) => value),
+  ];
+  const filteredTeams = teams.filter(({name}) =>
+    teamFilterSearch ? name.toLowerCase().includes(teamFilterSearch.toLowerCase()) : true
+  );
+
+  return (
+    <Filter
+      header={
+        <StyledInput
+          autoFocus
+          placeholder={t('Filter by team name')}
+          onClick={event => {
+            event.stopPropagation();
+          }}
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+            setTeamFilterSearch(event.target.value);
+          }}
+          value={teamFilterSearch || ''}
+        />
+      }
+      headerLabel={t('Team')}
+      onFilterChange={handleChangeFilter}
+      filterList={optionValues}
+      selection={selectedTeams}
+    >
+      {({toggleFilter}) => (
+        <List>
+          {additionalOptions.map(({label, value}) => (
+            <ListItem
+              key={value}
+              isChecked={selectedTeams.has(value)}
+              onClick={event => {
+                event.stopPropagation();
+                toggleFilter(value);
+              }}
+            >
+              <TeamName>{label}</TeamName>
+              <CheckboxFancy isChecked={selectedTeams.has(value)} />
+            </ListItem>
+          ))}
+          {filteredTeams.map(({id, name}) => (
+            <ListItem
+              key={id}
+              isChecked={selectedTeams.has(id)}
+              onClick={event => {
+                event.stopPropagation();
+                toggleFilter(id);
+              }}
+            >
+              <TeamName>{name}</TeamName>
+              <CheckboxFancy isChecked={selectedTeams.has(id)} />
+            </ListItem>
+          ))}
+        </List>
+      )}
+    </Filter>
+  );
+}
+
+export default TeamFilter;
+
+const List = styled('ul')`
+  list-style: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const StyledInput = styled(Input)`
+  border: none;
+  border-bottom: 1px solid ${p => p.theme.gray200};
+  border-radius: 0;
+`;
+
+const ListItem = styled('li')<{isChecked?: boolean}>`
+  display: grid;
+  grid-template-columns: 1fr max-content;
+  grid-column-gap: ${space(1)};
+  align-items: center;
+  padding: ${space(1)} ${space(2)};
+  border-bottom: 1px solid ${p => p.theme.border};
+  :hover {
+    background-color: ${p => p.theme.backgroundSecondary};
+  }
+  ${CheckboxFancy} {
+    opacity: ${p => (p.isChecked ? 1 : 0.3)};
+  }
+
+  &:hover ${CheckboxFancy} {
+    opacity: 1;
+  }
+
+  &:hover span {
+    color: ${p => p.theme.blue300};
+    text-decoration: underline;
+  }
+`;
+
+const TeamName = styled('div')`
+  font-size: ${p => p.theme.fontSizeMedium};
+  ${overflowEllipsis};
+`;
