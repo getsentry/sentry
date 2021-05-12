9c2701e47d8569def7dbb1ae5f06bdfa07f8935d
Ted Kaemming <ted@kaemming.com>
2021-05-12 11:25:08 -0700

    meh

--- src/sentry/utils/kvstore/redis.py
+++ src/sentry/utils/kvstore/redis.py
@@ -8,19 +8,19 @@ from sentry.utils.kvstore.abstract import KVStorage
 
 class RedisKVStorage(KVStorage[str, bytes]):
     def __init__(self, client: Redis[bytes]) -> None:
-        self.__client = client
+        self.client = client
 
     def get(self, key: str) -> Optional[bytes]:
-        return self.__client.get(key.encode("utf8"))
+        return self.client.get(key.encode("utf8"))
 
     def set(self, key: str, value: bytes, ttl: Optional[timedelta] = None) -> None:
-        self.__client.set(key.encode("utf8"), value, ex=ttl)
+        self.client.set(key.encode("utf8"), value, ex=ttl)
 
     def delete(self, key: str) -> None:
-        self.__client.delete(key.encode("utf8"))
+        self.client.delete(key.encode("utf8"))
 
     def bootstrap(self) -> None:
         pass  # nothing to do
 
     def destroy(self) -> None:
-        self.__client.flushdb()
+        self.client.flushdb()
