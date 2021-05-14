e80b95013935a15aefa138d73bf68afc89471146
Riya Chakraborty <riyamch@yahoo.com>
2021-05-14 16:53:46 -0500

    added pre-defined dict to better remove duplicate code in server.py

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -125,9 +125,18 @@ DEVSERVICES_CONFIG_DIR = os.path.normpath(
 )
 
 SENTRY_DISTRIBUTED_CLICKHOUSE_TABLES = False
-CLICKHOUSE_IMAGE = "yandex/clickhouse-server:20.3.9.70"
-CLICKHOUSE_PORTS = {"9000/tcp": 9000, "9009/tcp": 9009, "8123/tcp": 8123}
-CLICKHOUSE_ULIMITS = [{"name": "nofile", "soft": 262144, "hard": 262144}]
+_common_clickhouse_settings = {
+    "image": "yandex/clickhouse-server:20.3.9.70",
+    "pull": True,
+    "ports": {"9000/tcp": 9000, "9009/tcp": 9009, "8123/tcp": 8123},
+    "ulimits": [{"name": "nofile", "soft": 262144, "hard": 262144}],
+    "environment": {"MAX_MEMORY_USAGE_RATIO": "0.3"},
+}
+
+
+# CLICKHOUSE_IMAGE = "yandex/clickhouse-server:20.3.9.70"
+# CLICKHOUSE_PORTS = {"9000/tcp": 9000, "9009/tcp": 9009, "8123/tcp": 8123}
+# CLICKHOUSE_ULIMITS = [{"name": "nofile", "soft": 262144, "hard": 262144}]
 
 RELAY_CONFIG_DIR = os.path.join(DEVSERVICES_CONFIG_DIR, "relay")
 
@@ -1665,36 +1674,23 @@ SENTRY_DEVSERVICES = {
         ),
     },
     "clickhouse": {
-        "image": CLICKHOUSE_IMAGE,
-        "pull": True,
-        "ports": CLICKHOUSE_PORTS,
-        "ulimits": CLICKHOUSE_ULIMITS,
+        **_common_clickhouse_settings,
         "volumes": {
             "clickhouse": {"bind": "/var/lib/clickhouse"},
             os.path.join(DEVSERVICES_CONFIG_DIR, "clickhouse", "loc_config.xml"): {
                 "bind": "/etc/clickhouse-server/config.d/sentry.xml"
             },
         },
-        "environment": {
-            # This limits Clickhouse's memory to 30% of the host memory
-            # If you have high volume and your search return incomplete results
-            # You might want to change this to a higher value (and ensure your host has enough memory)
-            "MAX_MEMORY_USAGE_RATIO": "0.3"
-        },
         "only_if": lambda settings, options: (not settings.SENTRY_DISTRIBUTED_CLICKHOUSE_TABLES),
     },
     "clickhouse_dist": {
-        "image": CLICKHOUSE_IMAGE,
-        "pull": True,
-        "ports": CLICKHOUSE_PORTS,
-        "ulimits": CLICKHOUSE_ULIMITS,
+        **_common_clickhouse_settings,
         "volumes": {
             "clickhouse_dist": {"bind": "/var/lib/clickhouse"},
             os.path.join(DEVSERVICES_CONFIG_DIR, "clickhouse", "dist_config.xml"): {
                 "bind": "/etc/clickhouse-server/config.d/sentry.xml"
             },
         },
-        "environment": {"MAX_MEMORY_USAGE_RATIO": "0.3"},
         "only_if": lambda settings, options: (settings.SENTRY_DISTRIBUTED_CLICKHOUSE_TABLES),
     },
     "snuba": {
