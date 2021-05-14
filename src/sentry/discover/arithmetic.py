ff9fd944a7dee1f26daf856783f55fca2daba664
William Mak <william@wmak.io>
2021-05-14 11:02:47 -0400

    feat(arithmetic): Adding a parser for arithmetic
    - This is just a simple arithmetic parser that can handle just numbers
      for now
    - This is so that later it can be extended to handle variables to create
      more complicated Discover columns

---
+++ src/sentry/discover/arithmetic.py
@@ -0,0 +1,142 @@
+from typing import Optional
+
+from parsimonious.exceptions import ParseError
+from parsimonious.grammar import Grammar, NodeVisitor
+
+
+class ArithmeticError(Exception):
+    pass
+
+
+class MaxOperatorError(ArithmeticError):
+    """ Exceeded the maximum allowed operators """
+
+    pass
+
+
+class ArithmeticParseError(ArithmeticError):
+    """ Encountered an error trying to parse an equation """
+
+    pass
+
+
+class Operation:
+    __slots__ = "operator", "lhs", "rhs"
+
+    def __init__(self, operator, lhs=None, rhs=None):
+        self.operator = operator
+        self.lhs = lhs
+        self.rhs = rhs
+
+    def __repr__(self):
+        return repr([self.operator, repr(self.lhs), repr(self.rhs)])
+
+
+arithmetic_grammar = Grammar(
+    r"""
+term                 = spaces maybe_factor remaining_adds
+remaining_adds       = add_sub*
+add_sub              = add_sub_operator maybe_factor spaces
+maybe_factor         = (factor / primary)
+
+factor               = spaces primary remaining_muls
+remaining_muls       = mul_div+
+mul_div              = mul_div_operator primary spaces
+
+add_sub_operator     = spaces (plus / minus) spaces
+mul_div_operator     = spaces (multiply / divide) spaces
+# TODO(wmak) allow variables
+primary              = numeric_value
+
+# Operator names should match what's in clickhouse
+plus                 = "+"
+minus                = "-"
+multiply             = "*"
+divide               = "/"
+
+# TODO(wmak) share these with api/event_search and support decimals
+numeric_value        = ~r"[+-]?[0-9]+"
+spaces               = ~r"\ *"
+"""
+)
+
+
+class ArithmeticVisitor(NodeVisitor):
+    DEFAULT_MAX_OPERATORS = 10
+
+    # Don't wrap in VisitationErrors
+    unwrapped_exceptions = (MaxOperatorError,)
+
+    def __init__(self, max_operators):
+        super().__init__()
+        self.operators = 0
+        self.max_operators = max_operators if max_operators else self.DEFAULT_MAX_OPERATORS
+
+    def flatten(self, terms):
+        *remaining, term = terms
+        if term.lhs is None:
+            term.lhs = remaining[0]
+        return term
+
+    def visit_term(self, _, children):
+        _, maybe_factor, remaining_adds = children
+        maybe_factor = maybe_factor[0]
+        # remaining_adds is either a list containing an Operation, or a Node
+        if isinstance(remaining_adds, list):
+            # Update the operation with lhs and continue
+            remaining_adds[0].lhs = maybe_factor
+            return self.flatten(remaining_adds)
+        else:
+            # if remaining is a node lhs contains a factor so just return that
+            return maybe_factor
+
+    def visit_factor(self, _, children):
+        _, primary, remaining_muls = children
+        remaining_muls[0].lhs = primary
+        return self.flatten(remaining_muls)
+
+    def visited_operator(self):
+        """ We visited an operator, increment the count and error if we exceed max """
+        self.operators += 1
+        if self.operators > self.max_operators:
+            raise MaxOperatorError("Exceeded maximum number of operations")
+
+    def visit_add_sub(self, _, children):
+        add_sub_operator, maybe_factor, _ = children
+        self.visited_operator()
+        return Operation(add_sub_operator, rhs=maybe_factor[0])
+
+    def visit_mul_div(self, _, children):
+        mul_div_operator, primary, _ = children
+        self.visited_operator()
+        return Operation(mul_div_operator, rhs=primary)
+
+    @staticmethod
+    def parse_operator(children):
+        # Remove the optional spaces
+        _, operator, _ = children
+        # operator is a list but we'll only ever want the first
+        return operator[0].expr_name
+
+    def visit_add_sub_operator(self, _, children):
+        return self.parse_operator(children)
+
+    def visit_mul_div_operator(self, _, children):
+        return self.parse_operator(children)
+
+    def visit_numeric_value(self, node, _):
+        return float(node.text)
+
+    def generic_visit(self, node, children):
+        return children or node
+
+
+def parse_arithmetic(equation: str, max_operators: Optional[int] = None) -> Operation:
+    """ Given a string equation try to parse it into a set of Operations """
+    try:
+        tree = arithmetic_grammar.parse(equation)
+    except ParseError:
+        raise ArithmeticParseError(
+            "Unable to parse your equation, make sure it is well formed arithmetic"
+        )
+    return ArithmeticVisitor(max_operators).visit(tree)
---
+++ tests/sentry/discover/test_arithmetic.py
@@ -0,0 +1,115 @@
+import pytest
+
+from sentry.discover.arithmetic import ArithmeticParseError, MaxOperatorError, parse_arithmetic
+
+op_map = {
+    "+": "plus",
+    "-": "minus",
+    "*": "multiply",
+    "/": "divide",
+}
+
+
+def test_single_term():
+    result = parse_arithmetic("12")
+    assert result == 12.0
+
+
+@pytest.mark.parametrize(
+    "a,op,b",
+    [
+        ("12", "+", "34"),
+        (" 12 ", " + ", " 34 "),
+        ("12", "-", "34"),
+        (" 12 ", " - ", " 34 "),
+        ("12", "*", "34"),
+        (" 12 ", " * ", " 34 "),
+        ("12", "/", "34"),
+        (" 12 ", " / ", " 34 "),
+        ("-12", "+", "-34"),
+        ("+12", "+", "+34"),
+        ("-12", "+", "+34"),
+        ("+12", "+", "-34"),
+    ],
+)
+def test_simple_arithmetic(a, op, b):
+    equation = f"{a}{op}{b}"
+    result = parse_arithmetic(equation)
+    assert result.operator == op_map[op.strip()], equation
+    assert result.lhs == float(a), equation
+    assert result.rhs == float(b), equation
+
+
+@pytest.mark.parametrize(
+    "a,op1,b,op2,c",
+    [
+        ("12", "+", "34", "+", "56"),
+        ("12", "+", "34", "-", "56"),
+        ("12", "-", "34", "+", "56"),
+        ("12", "-", "34", "-", "56"),
+        ("12", "*", "34", "*", "56"),
+        ("12", "*", "34", "/", "56"),
+        ("12", "/", "34", "*", "56"),
+        ("12", "/", "34", "/", "56"),
+    ],
+)
+def test_homogenous_arithmetic(a, op1, b, op2, c):
+    """ Test that order of ops is respected assuming we don't have to worry about BEDMAS """
+    equation = f"{a}{op1}{b}{op2}{c}"
+    result = parse_arithmetic(equation)
+    assert result.operator == op_map[op2.strip()], equation
+    assert result.lhs.operator == op_map[op1.strip()], equation
+    assert result.lhs.lhs == float(a), equation
+    assert result.lhs.rhs == float(b), equation
+    assert result.rhs == float(c), equation
+
+
+def test_mixed_arithmetic():
+    result = parse_arithmetic("12 + 34 * 56")
+    result.operator == "plus"
+    result.lhs = 12.0
+    result.rhs.operator = "multiply"
+    result.rhs.lhs = 34.0
+    result.rhs.rhs = 56.0
+
+    result = parse_arithmetic("12 / 34 - 56")
+    result.operator == "subtract"
+    result.lhs.operator = "divide"
+    result.lhs.lhs = 12.0
+    result.lhs.rhs = 34.0
+    result.rhs = 56.0
+
+
+def test_four_terms():
+    result = parse_arithmetic("1 + 2 / 3 * 4")
+    assert result.operator == "plus"
+    assert result.lhs == 1.0
+    assert result.rhs.operator == "multiply"
+    assert result.rhs.lhs.operator == "divide"
+    assert result.rhs.lhs.lhs == 2.0
+    assert result.rhs.lhs.rhs == 3.0
+    assert result.rhs.rhs == 4.0
+
+
+def test_max_operators():
+    with pytest.raises(MaxOperatorError):
+        parse_arithmetic("1 + 2 * 3 * 4", 2)
+
+    # exactly 3 should be ok
+    parse_arithmetic("1 + 2 * 3 * 4", 3)
+
+
+@pytest.mark.parametrize(
+    "equation",
+    [
+        "1 +",
+        "+ 1 + 1",
+        "1 + 1 +",
+        "1 ** 2",
+        "1 -- 1",
+        "hello world",
+    ],
+)
+def test_bad_arithmetic(equation):
+    with pytest.raises(ArithmeticParseError):
+        parse_arithmetic(equation)
