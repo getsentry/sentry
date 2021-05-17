ba6fa79f2f9df5df10aa28460067dcffd7ddb291
Colleen O'Rourke <colleen@sentry.io>
2021-05-17 11:46:15 -0700

    feat(notifications platform): Send notification to team's Slack channel (#26024)
    * feat(notifications platform): Send notification to team's Slack channel

--- src/sentry/integrations/slack/notifications.py
+++ src/sentry/integrations/slack/notifications.py
@@ -1,9 +1,9 @@
 import logging
-from typing import AbstractSet, Any, Mapping, Set, Tuple
+from typing import AbstractSet, Any, Mapping, Set, Tuple, Union
 
 from sentry.integrations.slack.client import SlackClient  # NOQA
 from sentry.integrations.slack.message_builder.notifications import build_notification_attachment
-from sentry.models import ExternalActor, Organization, User
+from sentry.models import ExternalActor, Organization, Team, User
 from sentry.notifications.activity.base import ActivityNotification
 from sentry.notifications.base import BaseNotification
 from sentry.notifications.notify import register_notification_provider
@@ -18,25 +18,25 @@ SLACK_TIMEOUT = 5
 
 def get_context(
     notification: BaseNotification,
-    user: User,
+    recipient: Union[User, Team],
     shared_context: Mapping[str, Any],
     extra_context: Mapping[str, Any],
 ) -> Mapping[str, Any]:
     """ Compose the various levels of context and add slack-specific fields. """
     return {
         **shared_context,
-        **notification.get_user_context(user, extra_context),
+        **notification.get_user_context(recipient, extra_context),
     }
 
 
-def get_integrations_by_user_id(
-    organization: Organization, users: AbstractSet[User]
-) -> Mapping[User, ExternalActor]:
-    actor_mapping = {user.actor_id: user for user in users}
+def get_integrations_by_recipient_id(
+    organization: Organization, recipients: AbstractSet[Union[User, Team]]
+) -> Mapping[Union[User, Team], ExternalActor]:
+    actor_mapping = {recipient.actor_id: recipient for recipient in recipients}
 
     external_actors = ExternalActor.objects.filter(
         provider=ExternalProviders.SLACK.value,
-        actor_id__in=[user.actor_id for user in users],
+        actor_id__in=[recipient.actor_id for recipient in recipients],
         organization=organization,
     ).select_related("integration")
 
@@ -47,9 +47,10 @@ def get_integrations_by_user_id(
 
 
 def get_channel_and_token(
-    external_actors_by_user: Mapping[User, ExternalActor], user: User
+    external_actors_by_recipient: Mapping[Union[User, Team], ExternalActor],
+    recipient: Union[User, Team],
 ) -> Tuple[str, str]:
-    external_actor = external_actors_by_user.get(user)
+    external_actor = external_actors_by_recipient.get(recipient)
     channel = external_actor.external_id
     token = external_actor.integration.metadata["access_token"]
     return channel, token
@@ -67,31 +68,31 @@ def get_key(notification: BaseNotification) -> str:
 @register_notification_provider(ExternalProviders.SLACK)
 def send_notification_as_slack(
     notification: BaseNotification,
-    users: Set[User],
+    recipients: Union[Set[User], Set[Team]],
     shared_context: Mapping[str, Any],
     extra_context_by_user_id: Mapping[str, Any],
 ) -> None:
-    """ Send an "activity" or "alert rule" notification to a Slack user. """
-
-    external_actors_by_user = get_integrations_by_user_id(notification.organization, users)
-
+    """ Send an "activity" or "alert rule" notification to a Slack user or team. """
+    external_actors_by_recipient = get_integrations_by_recipient_id(
+        notification.organization, recipients
+    )
     client = SlackClient()
-    for user in users:
-        extra_context = (extra_context_by_user_id or {}).get(user.id, {})
+    for recipient in recipients:
+        extra_context = (extra_context_by_user_id or {}).get(recipient.id, {})
         try:
-            channel, token = get_channel_and_token(external_actors_by_user, user)
+            channel, token = get_channel_and_token(external_actors_by_recipient, recipient)
         except AttributeError as e:
             logger.info(
                 "notification.fail.invalid_slack",
                 extra={
                     "error": str(e),
                     "notification": notification,
-                    "user": user.id,
+                    "recipient": recipient.id,
                 },
             )
             continue
 
-        context = get_context(notification, user, shared_context, extra_context)
+        context = get_context(notification, recipient, shared_context, extra_context)
         attachment = [build_notification_attachment(notification, context)]
         payload = {
             "token": token,
@@ -107,7 +108,7 @@ def send_notification_as_slack(
                 extra={
                     "error": str(e),
                     "notification": notification,
-                    "user": user.id,
+                    "recipient": recipient.id,
                     "channel_id": channel,
                 },
             )
