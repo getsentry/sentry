632df57d7d3a5418c2a4d3af4c84585029cc02fc
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-05-17 10:04:34 -0400

    feat(transaction): Add Transaction Threshold model
    This model will store per transaction threshold and the
    metric to apply the threshold to. Transaction name is
    nullable. A null transaction will signify the project
    level fallback.

---
+++ src/sentry/models/transaction_threshold.py
@@ -0,0 +1,27 @@
+from enum import Enum
+
+from django.db import models
+
+from sentry.db.models import FlexibleForeignKey, Model
+
+
+class TransactionMetric(Enum):
+    DURATION = 1
+    LCP = 2
+    FCP = 3
+
+
+class TransactionThreshold(Model):
+    __core__ = False
+
+    # max_length here is based on the maximum for transactions in relay
+    transaction = models.CharField(max_length=200, null=True)
+    project = FlexibleForeignKey("sentry.Project", db_constraint=False)
+    organization = FlexibleForeignKey("sentry.Organization")
+    threshold = models.IntegerField()
+    metric = models.PositiveSmallIntegerField(default=TransactionMetric.DURATION.value)
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_transactionthreshold"
+        unique_together = (("project", "transaction"),)
