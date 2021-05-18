bcea3b78f9fe9d9ce2991c71e14804557b9135ba
Shruthi <shruthilaya.jaganathan@sentry.io>
2021-05-18 18:00:49 -0400

    feat(performance-view): Add feature flag for project level thresholds (#26110)
    Add feature flags for project level thresholds

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1001,6 +1001,8 @@ SENTRY_FEATURES = {
     "organizations:alert-wizard": True,
     # Enable the adoption chart in the releases page
     "organizations:release-adoption-chart": False,
+    # Enable the project level transaction thresholds
+    "organizations:project-transaction-threshold": False,
     # Enable percent displays in issue stream
     "organizations:issue-percent-display": False,
     # Adds additional filters and a new section to issue alert rules.
