60fbd65121f12f399faf0da86d50211062f23e66
Ted Kaemming <ted@kaemming.com>
2021-05-17 21:05:01 -0700

    ref(kvstore): Loosen value type of `RedisKVStorage`

--- src/sentry/utils/kvstore/redis.py
+++ src/sentry/utils/kvstore/redis.py
@@ -1,24 +1,29 @@
 from datetime import timedelta
-from typing import Optional
+from typing import Optional, TypeVar, Union
 
 from redis import Redis
 
 from sentry.utils.kvstore.abstract import KVStorage
 
+# The value type of the Redis client depends on the value of the
+# ``decode_responses`` parameter passed to the client constructor:
+# https://github.com/python/typeshed/blob/f0bf6ee/stubs/redis/redis/client.pyi#L62-L63
+V = TypeVar("V", bound=Union[str, bytes])
 
-class RedisKVStorage(KVStorage[str, bytes]):
+
+class RedisKVStorage(KVStorage[str, V]):
     """
     This class provides a key/value store backed by Redis (either a single node
     or cluster.)
     """
 
-    def __init__(self, client: "Redis[bytes]") -> None:
+    def __init__(self, client: "Redis[V]") -> None:
         self.client = client
 
-    def get(self, key: str) -> Optional[bytes]:
+    def get(self, key: str) -> Optional[V]:
         return self.client.get(key.encode("utf8"))
 
-    def set(self, key: str, value: bytes, ttl: Optional[timedelta] = None) -> None:
+    def set(self, key: str, value: V, ttl: Optional[timedelta] = None) -> None:
         self.client.set(key.encode("utf8"), value, ex=ttl)
 
     def delete(self, key: str) -> None:
