035f45a905e54c35f320bf07d1a8d5c08b066efc
Mark Story <mark@sentry.io>
2021-05-18 09:55:56 -0400

    Update static/app/views/organizationGroupDetails/actions/index.tsx
    Co-authored-by: Priscila Oliveira <priscilawebdev@gmail.com>

--- static/app/views/organizationGroupDetails/actions/index.tsx
+++ static/app/views/organizationGroupDetails/actions/index.tsx
@@ -319,10 +319,12 @@ const BookmarkButton = styled(ActionButton)<{isActive: boolean}>`
   ${p =>
     p.isActive &&
     `
-    background: ${p.theme.yellow100} !important;
-    color: ${p.theme.yellow300} !important;
-    border-color: ${p.theme.yellow300} !important;
-    text-shadow: 0 1px 0 rgba(0, 0, 0, 0.15);
+   && {
+ background: ${p.theme.yellow100};
+ color: ${p.theme.yellow300};
+ border-color: ${p.theme.yellow300};
+ text-shadow: 0 1px 0 rgba(0, 0, 0, 0.15);
+}
   `}
 `;
 
