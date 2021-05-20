2295d08098baf4bb24b88d0987d4454588591c2a
Tony Xiao <txiao@sentry.io>
2021-05-20 09:59:06 -0400

    ui(trace-view): Break header into multiple rows (#26144)
    This breaks the trace view header into multiple rows on smaller screens to make
    it more legible.

--- static/app/views/performance/traceDetails/styles.tsx
+++ static/app/views/performance/traceDetails/styles.tsx
@@ -38,7 +38,7 @@ export const TraceViewHeaderContainer = styled(SecondaryHeader)`
 
 export const TraceDetailHeader = styled('div')`
   display: grid;
-  grid-template-columns: repeat(3, 1fr);
+  grid-template-columns: 1fr;
   grid-gap: ${space(2)};
   margin-bottom: ${space(2)};
 
