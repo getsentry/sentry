5e9db2e6738044d49fce7cbf02808fbea15f6755
Stephen Cefali <scefali@sentry.io>
2021-05-25 11:28:41 -0700

    feat(leaky-bucket): disabled member redirect and auth (#26157)
    This PR checks if a member is disabled from the member limit during auth and loading front-end views. A member would be disabled when an organization downgrades plans (or lets a trial expire) when they've added additional members but their plan only allows 1 user. A disabled member would get redirected to the /organizations/:orgSlug/disabled-member/ view which will explain to the user what needs to be done to become re-enabled (asking their billing admin to upgrade their plan). That view only has a placeholder for now. The code to actually change members to disabled is here: getsentry/getsentry#5604.

---
+++ static/app/views/disabledMember/index.tsx
@@ -0,0 +1,30 @@
+import {RouteComponentProps} from 'react-router';
+
+import NotFound from 'app/components/errors/notFound';
+import HookOrDefault from 'app/components/hookOrDefault';
+import {OrganizationSummary} from 'app/types';
+import withOrganizations from 'app/utils/withOrganizations';
+
+type Props = {
+  organizations: OrganizationSummary[];
+} & RouteComponentProps<{orgId: string}, {}>;
+
+//getsentry will add the view
+const DisabledMemberComponent = HookOrDefault({
+  hookName: 'component:disabled-member',
+  defaultComponent: () => <NotFound />,
+});
+
+function DisabledMember(props: Props) {
+  const {
+    organizations,
+    params: {orgId},
+  } = props;
+  const org = organizations.find(o => o.slug === orgId);
+  if (!org) {
+    return <NotFound />;
+  }
+  return <DisabledMemberComponent organization={org} />;
+}
+
+export default withOrganizations(DisabledMember);
