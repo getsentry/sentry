d3b8ed406a521307672eb302fc10d1661eccbd98
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2021-05-26 09:41:59 -0400

    feat(discover): Improved display on errored tags (#26218)
    When there's an error ingesting a tag we currently just show an empty string instead of why it errored on the Discovery page. This is to improve the level of detail displayed on the errored tag.
    
    * Display invalid on errored tags instead of a blank string in discover
    * Display a tooltip with error reason on tag key and value on hover

--- static/app/components/tagsTable.tsx
+++ static/app/components/tagsTable.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import styled from '@emotion/styled';
 import {LocationDescriptor} from 'history';
+import capitalize from 'lodash/capitalize';
 
 import {SectionHeading} from 'app/components/charts/styles';
+import {getMeta, withMeta} from 'app/components/events/meta/metaProxy';
 import {KeyValueTable, KeyValueTableRow} from 'app/components/keyValueTable';
 import Link from 'app/components/links/link';
 import Tooltip from 'app/components/tooltip';
 import Version from 'app/components/version';
 import {t} from 'app/locale';
 import space from 'app/styles/space';
+import {MetaError} from 'app/types';
 import {Event, EventTag} from 'app/types/event';
 
 type Props = {
@@ -19,7 +22,27 @@ type Props = {
 };
 
 const TagsTable = ({event, query, generateUrl, title = t('Tag Details')}: Props) => {
-  const tags = event.tags;
+  const eventWithMeta = withMeta(event) as Event;
+  const tags = eventWithMeta.tags;
+
+  const formatErrorKind = (kind: string) => {
+    return capitalize(kind.replace(/_/g, ' '));
+  };
+
+  const getErrorMessage = (error: MetaError) => {
+    if (Array.isArray(error)) {
+      if (error[1]?.reason) {
+        return formatErrorKind(error[1].reason);
+      } else {
+        return formatErrorKind(error[0]);
+      }
+    }
+    return formatErrorKind(error);
+  };
+
+  const getTooltipTitle = (errors: Array<MetaError>) => {
+    return <TooltipTitle>{getErrorMessage(errors[0])}</TooltipTitle>;
+  };
 
   return (
     <StyledTagsTable>
@@ -28,6 +51,8 @@ const TagsTable = ({event, query, generateUrl, title = t('Tag Details')}: Props)
         {tags.map(tag => {
           const tagInQuery = query.includes(`${tag.key}:`);
           const target = tagInQuery ? undefined : generateUrl(tag);
+          const keyMetaData = getMeta(tag, 'key');
+          const valueMetaData = getMeta(tag, 'value');
 
           const renderTagValue = () => {
             switch (tag.key) {
@@ -37,13 +62,26 @@ const TagsTable = ({event, query, generateUrl, title = t('Tag Details')}: Props)
                 return tag.value;
             }
           };
-
           return (
             <KeyValueTableRow
               key={tag.key}
-              keyName={tag.key}
+              keyName={
+                keyMetaData?.err?.length ? (
+                  <Tooltip title={getTooltipTitle(keyMetaData.err)}>
+                    <i>{`<${t('invalid')}>`}</i>
+                  </Tooltip>
+                ) : (
+                  tag.key
+                )
+              }
               value={
-                tagInQuery ? (
+                valueMetaData?.err?.length ? (
+                  <Tooltip title={getTooltipTitle(valueMetaData.err)}>
+                    <i>{`<${t('invalid')}>`}</i>
+                  </Tooltip>
+                ) : keyMetaData?.err?.length ? (
+                  <span>{renderTagValue()}</span>
+                ) : tagInQuery ? (
                   <Tooltip title={t('This tag is in the current filter conditions')}>
                     <span>{renderTagValue()}</span>
                   </Tooltip>
@@ -59,8 +97,12 @@ const TagsTable = ({event, query, generateUrl, title = t('Tag Details')}: Props)
   );
 };
 
+export default TagsTable;
+
 const StyledTagsTable = styled('div')`
   margin-bottom: ${space(3)};
 `;
 
-export default TagsTable;
+const TooltipTitle = styled('div')`
+  text-align: left;
+`;
