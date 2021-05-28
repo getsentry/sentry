ee2530acdbb02b045c9de80c0adb27b91a847431
Armen Zambrano Gasparnian <armenzg@sentry.io>
2021-05-28 14:35:31 -0400

    fix(ci): Fix Docker initialization for Mac on dev env workflow
    Newer versions of Docker do not generate the same set of files.

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -16,57 +16,11 @@ configure-sentry-cli() {
     fi
 }
 
-query-mac() {
-    [[ $(uname -s) = 'Darwin' ]]
-}
-
 query_big_sur() {
     if require sw_vers && sw_vers -productVersion | grep -E "11\." >/dev/null; then
         return 0
     fi
     return 1
-}
-
-sudo-askpass() {
-    if [ -z "${sudo-askpass-x}" ]; then
-        sudo --askpass "$@"
-    else
-        sudo "$@"
-    fi
-}
-
-# After using homebrew to install docker, we need to do some magic to remove the need to interact with the GUI
-# See: https://github.com/docker/for-mac/issues/2359#issuecomment-607154849 for why we need to do things below
-init-docker() {
-    # Need to start docker if it was freshly installed or updated
-    # You will know that Docker is ready for devservices when the icon on the menu bar stops flashing
-    if query-mac && [ ! -f /Library/PrivilegedHelperTools/com.docker.vmnetd ]; then
-        echo "Making some changes to complete Docker initialization"
-        # allow the app to run without confirmation
-        xattr -d -r com.apple.quarantine /Applications/Docker.app
-
-        # preemptively do docker.app's setup to avoid any gui prompts
-        sudo-askpass /bin/mkdir -p /Library/PrivilegedHelperTools
-        sudo-askpass /bin/chmod 754 /Library/PrivilegedHelperTools
-        sudo-askpass /bin/cp /Applications/Docker.app/Contents/Library/LaunchServices/com.docker.vmnetd /Library/PrivilegedHelperTools/
-        sudo-askpass /bin/cp /Applications/Docker.app/Contents/Resources/com.docker.vmnetd.plist /Library/LaunchDaemons/
-        sudo-askpass /bin/chmod 544 /Library/PrivilegedHelperTools/com.docker.vmnetd
-        sudo-askpass /bin/chmod 644 /Library/LaunchDaemons/com.docker.vmnetd.plist
-        sudo-askpass /bin/launchctl load /Library/LaunchDaemons/com.docker.vmnetd.plist
-    fi
-    start-docker
-}
-
-# This is mainly to be used by CI
-# We need this for Mac since the executable docker won't work properly
-# until the app is opened once
-start-docker() {
-    if query-mac && ! docker system info &>/dev/null; then
-        echo "About to open Docker.app"
-        # At a later stage in the script, we're going to execute
-        # ensure_docker_server which waits for it to be ready
-        open -g -a Docker.app
-    fi
 }
 
 upgrade-pip() {
