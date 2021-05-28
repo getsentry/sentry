399f00a845414f4a3c476515f225700ffab58095
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-05-28 15:33:11 -0400

    remove noqa

--- src/sentry/search/events/fields.py
+++ src/sentry/search/events/fields.py
@@ -1329,16 +1329,16 @@ FUNCTIONS = {
                 {"name": "parameter_sum", "fn": lambda args: args["alpha"] + args["beta"]},
             ],
             transform=(
-                "ifNull(divide(plus(uniqIf(user, greater("  # NOQA
-                "multiIf("  # NOQA
+                "ifNull(divide(plus(uniqIf(user, greater("
+                "multiIf("
                 "equals(tupleElement({threshold_config}, 1), 'lcp'),"
-                "if(has(measurements.key, 'lcp'), arrayElement(measurements.value, indexOf(measurements.key, 'lcp')), NULL),"  # NOQA
-                "equals(tupleElement({threshold_config}, 1), 'fcp'),"  # NOQA
-                "if(has(measurements.key, 'fcp'), arrayElement(measurements.value, indexOf(measurements.key, 'fcp')), NULL),"  # NOQA
-                "duration"  # NOQA
-                "), "  # NOQA
-                "multiply(tupleElement({threshold_config}, 2), 4)"  # NOQA
-                ")), {alpha}), plus(uniq(user), {parameter_sum})), 0)"  # NOQA
+                "if(has(measurements.key, 'lcp'), arrayElement(measurements.value, indexOf(measurements.key, 'lcp')), NULL),"
+                "equals(tupleElement({threshold_config}, 1), 'fcp'),"
+                "if(has(measurements.key, 'fcp'), arrayElement(measurements.value, indexOf(measurements.key, 'fcp')), NULL),"
+                "duration"
+                "), "
+                "multiply(tupleElement({threshold_config}, 2), 4)"
+                ")), {alpha}), plus(uniq(user), {parameter_sum})), 0)"
             ),
             default_result_type="number",
         ),
