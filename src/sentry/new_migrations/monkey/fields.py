52f3ac671795d2e0134460c53c589f96425a70ae
Dan Fuller <dfuller@sentry.io>
2021-06-01 18:06:21 -0700

    comment

--- src/sentry/new_migrations/monkey/fields.py
+++ src/sentry/new_migrations/monkey/fields.py
@@ -5,6 +5,11 @@ original_deconstruct = Field.deconstruct
 
 
 def deconstruct(self):
+    """
+    Overrides the default field deconstruct method. This is used to pop unwanted
+    keyword arguments from the field during deconstruction, so that they will be ignored
+    when generating migrations.
+    """
     name, path, args, kwargs = original_deconstruct(self)
     for attr in IGNORED_ATTRS:
         kwargs.pop(attr, None)
