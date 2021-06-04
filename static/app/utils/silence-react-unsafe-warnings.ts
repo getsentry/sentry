8841d4c1f19341db6cff8604fc1a4a6c73fe329e
Mark Story <mark@sentry.io>
2021-06-04 13:14:59 -0400

    ref(ui) Reduce the number of warnings in tests (#26407)
    Ignore warnings from moment. We have many tests that check that 
    we're handling invalid date strings. Having these tests also emit errors is
    noisy and unhelpful.
    
    Ignoring moment failures when we're testing invalid inputs. Address
    a few other smaller warnings at the same time.

--- static/app/utils/silence-react-unsafe-warnings.ts
+++ static/app/utils/silence-react-unsafe-warnings.ts
@@ -2,9 +2,13 @@
 export const originalConsoleWarn = console.warn;
 
 const REACT_UNSAFE_WARNING_REGEX = /componentWill.* has been renamed, and is not recommended for use.*/;
+const MOMENT_INVALID_INPUT_REGEX = /moment construction falls back/;
 
 window.console.warn = (message: any, ...args: any[]) => {
-  if (typeof message === 'string' && REACT_UNSAFE_WARNING_REGEX.test(message)) {
+  if (
+    typeof message === 'string' &&
+    (REACT_UNSAFE_WARNING_REGEX.test(message) || MOMENT_INVALID_INPUT_REGEX.test(message))
+  ) {
     return;
   }
 
