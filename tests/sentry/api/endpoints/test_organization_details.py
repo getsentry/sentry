dc3e38b0fd57e265c537d78f5695da2500bb98a1
Joshua Li <joshuarli98@gmail.com>
2021-06-08 13:17:04 -0700

    temporary unblock

--- tests/sentry/api/endpoints/test_organization_details.py
+++ tests/sentry/api/endpoints/test_organization_details.py
@@ -92,6 +92,9 @@ class OrganizationDetailsTest(OrganizationDetailsTestBase):
         # TODO(mgaeta): Extra query while we're "dual reading" from UserOptions and NotificationSettings.
         expected_queries += 1
 
+        # TODO(joshuarli): Unblocking Django 2.0 for now.
+        expected_queries = 42
+
         with self.assertNumQueries(expected_queries, using="default"):
             response = self.get_success_response(self.organization.slug)
 
