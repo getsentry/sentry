bca8da42e79928719bf83aa755d720e9d1465d10
Joshua Li <joshuarli98@gmail.com>
2021-06-08 14:56:42 -0700

    fix test now that it's an error to pass ['1'] to urlquote

--- tests/sentry/api/bases/test_endpoint.py
+++ tests/sentry/api/bases/test_endpoint.py
@@ -1,3 +1,5 @@
+from django.http import QueryDict
+
 from sentry.api.base import Endpoint
 from sentry.testutils import TestCase
 from sentry.utils.compat.mock import Mock
@@ -6,26 +8,26 @@ from sentry.utils.compat.mock import Mock
 class EndpointTest(TestCase):
     def test_simple(self):
         request = Mock()
-        request.GET = {"member": ["1"]}
+        request.GET = QueryDict("member=1")
         request.method = "GET"
         request.path = "/api/0/organizations/"
         endpoint = Endpoint()
         result = endpoint.build_cursor_link(request, "next", "1492107369532:0:0")
 
         assert result == (
-            "<http://testserver/api/0/organizations/?member=%5B%271%27%5D&cursor=1492107369532:0:0>;"
+            "<http://testserver/api/0/organizations/?member=1&cursor=1492107369532:0:0>;"
             ' rel="next"; results="true"; cursor="1492107369532:0:0"'
         )
 
     def test_unicode_path(self):
         request = Mock()
-        request.GET = {"member": ["1"]}
+        request.GET = QueryDict("member=1")
         request.method = "GET"
         request.path = "/api/0/organizations/üuuuu/"
         endpoint = Endpoint()
         result = endpoint.build_cursor_link(request, "next", "1492107369532:0:0")
 
         assert result == (
-            "<http://testserver/api/0/organizations/%C3%BCuuuu/?member=%5B%271%27%5D&cursor=1492107369532:0:0>;"
+            "<http://testserver/api/0/organizations/%C3%BCuuuu/?member=1&cursor=1492107369532:0:0>;"
             ' rel="next"; results="true"; cursor="1492107369532:0:0"'
         )
