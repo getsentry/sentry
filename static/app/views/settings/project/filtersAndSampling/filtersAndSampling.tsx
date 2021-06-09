291f3e34082701ad04a03df45ea759dd1a4a431f
Dora <dora.l.chan@gmail.com>
2021-06-09 08:02:38 -0700

    Update static/app/views/settings/project/filtersAndSampling/filtersAndSampling.tsx
    Co-authored-by: Priscila Oliveira <priscilawebdev@gmail.com>

--- static/app/views/settings/project/filtersAndSampling/filtersAndSampling.tsx
+++ static/app/views/settings/project/filtersAndSampling/filtersAndSampling.tsx
@@ -236,7 +236,7 @@ class FiltersAndSampling extends AsyncView<Props, State> {
         <PermissionAlert />
         <TextBlock>
           {tct(
-            'Manage the inbound data you want to store. To change the sampling rate or rate limits, [link:update your SDK configuration]. The rules added below will apply on top of your SDK configuration. Any new rule may take a few minutes to propogate.',
+            'Manage the inbound data you want to store. To change the sampling rate or rate limits, [link:update your SDK configuration]. The rules added below will apply on top of your SDK configuration. Any new rule may take a few minutes to propagate.',
             {
               link: <ExternalLink href={DYNAMIC_SAMPLING_DOC_LINK} />,
             }
