f73e5e7084ec884f3842d512d116fa0972c84437
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2021-06-10 15:28:49 -0700

    added mobile project creation and demo setting

--- src/sentry/demo/settings.py
+++ src/sentry/demo/settings.py
@@ -48,3 +48,5 @@ DEMO_DATA_GEN_PARAMS = {
 # parameters for an org when quickly generating them synchronously
 DEMO_DATA_QUICK_GEN_PARAMS = DEMO_DATA_GEN_PARAMS.copy()
 DEMO_DATA_QUICK_GEN_PARAMS.update(MAX_DAYS=1, SCALE_FACTOR=0.25, NAME_STEP_SIZE=200)
+
+MOBILE_PROJECTS = False
