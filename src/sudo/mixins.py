d64809bf41afcc98189439a43241a28dba472fd5
Joshua Li <joshuarli98@gmail.com>
2021-06-15 15:35:39 -0700

    remove obvious unused code

--- src/sudo/mixins.py
+++
@@ -1,8 +0,0 @@
-from sudo.decorators import sudo_required
-
-
-class SudoMixin:
-    @classmethod
-    def as_view(cls, **initkwargs):
-        view = super().as_view(**initkwargs)
-        return sudo_required(view)
