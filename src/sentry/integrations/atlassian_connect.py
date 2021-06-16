8e9332623cf93795f6bfd172dc56e641019865c7
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-06-15 17:18:19 -0700

    Wrap InvalidSignatureError

--- src/sentry/integrations/atlassian_connect.py
+++ src/sentry/integrations/atlassian_connect.py
@@ -1,5 +1,7 @@
 import hashlib
 
+from jwt import InvalidSignatureError
+
 from sentry.models import Integration
 from sentry.utils import jwt
 from sentry.utils.http import percent_encode
@@ -56,7 +58,11 @@ def get_integration_from_jwt(token, path, provider, query_params, method="GET"):
     # alg field.  We only need the token + shared secret and do not want to provide an
     # audience to the JWT validation that is require to match.  Bitbucket does give us an
     # audience claim however, so disable verification of this.
-    decoded_verified = jwt.decode(token, integration.metadata["shared_secret"], audience=False)
+    try:
+        decoded_verified = jwt.decode(token, integration.metadata["shared_secret"], audience=False)
+    except InvalidSignatureError:
+        raise AtlassianConnectValidationError("Signature is invalid")
+
     # Verify the query has not been tampered by Creating a Query Hash
     # and comparing it against the qsh claim on the verified token.
 
