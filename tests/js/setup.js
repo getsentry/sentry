0eb2aec2d9851b6484bc5b670b32ef150075a44c
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-06-16 11:50:01 -0700

    tests(fixtures): Make loadFixtures more generic (#26667)

--- tests/js/setup.js
+++ tests/js/setup.js
@@ -1,4 +1,3 @@
-/* global __dirname */
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 import Enzyme from 'enzyme'; // eslint-disable-line no-restricted-imports
 import MockDate from 'mockdate';
@@ -45,8 +44,7 @@ MockDate.set(constantDate);
  * Load all files in `tests/js/fixtures/*` as a module.
  * These will then be added to the `TestStubs` global below
  */
-const fixturesPath = `${__dirname}/../fixtures/js-stubs`;
-const fixtures = loadFixtures(fixturesPath);
+const fixtures = loadFixtures('js-stubs', {flatten: true});
 
 /**
  * Global testing configuration
