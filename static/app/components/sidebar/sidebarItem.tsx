6ad13061595b5252b3ebe0ea053e32c1031834da
Stephen Cefali <scefali@sentry.io>
2021-06-17 16:36:03 -0700

    rename field

--- static/app/components/sidebar/sidebarItem.tsx
+++ static/app/components/sidebar/sidebarItem.tsx
@@ -68,7 +68,7 @@ type Props = ReactRouter.WithRouterProps & {
   /**
    * An optional prefix that can be used to reset the "new" indicator
    */
-  isNewCacheBustKey?: string;
+  isNewCacheKeySuffix?: string;
 };
 
 const SidebarItem = ({
@@ -86,7 +86,7 @@ const SidebarItem = ({
   collapsed,
   className,
   orientation,
-  isNewCacheBustKey,
+  isNewCacheKeySuffix,
   onClick,
   ...props
 }: Props) => {
@@ -112,7 +112,7 @@ const SidebarItem = ({
   const isTop = orientation === 'top';
   const placement = isTop ? 'bottom' : 'right';
 
-  const seenSuffix = isNewCacheBustKey ?? '';
+  const seenSuffix = isNewCacheKeySuffix ?? '';
   const isNewSeenKey = `sidebar-new-seen:${id}${seenSuffix}`;
   const showIsNew = isNew && !localStorage.getItem(isNewSeenKey);
 
