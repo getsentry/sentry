fd1d63d04f4367a54cba4d4a44e7eedfe2b1a6f3
Billy Vong <billy@sentry.io>
2021-06-17 14:54:26 -0700

    await initializeApp

--- static/app/bootstrap/initializeMain.tsx
+++ static/app/bootstrap/initializeMain.tsx
@@ -12,5 +12,5 @@ export async function initializeMain(config: Config) {
   // This is dynamically imported because we need to make sure locale is configured
   // before proceeding.
   const {initializeApp} = await import('./initializeApp');
-  initializeApp(config);
+  await initializeApp(config);
 }
