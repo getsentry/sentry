7b121c3ef30bcb848f68d554bbd709f71b9f976b
Priscila Oliveira <priscilawebdev@gmail.com>
2021-06-21 10:54:22 +0200

    ref(grouping-ui): Update design (#26709)

--- static/app/views/organizationGroupDetails/grouping/index.tsx
+++ static/app/views/organizationGroupDetails/grouping/index.tsx
@@ -4,7 +4,7 @@ import Feature from 'app/components/acl/feature';
 import Alert from 'app/components/alert';
 import {t} from 'app/locale';
 import {PageContent} from 'app/styles/organization';
-import {Group, Organization, Project} from 'app/types';
+import {Group, Organization} from 'app/types';
 import withOrganization from 'app/utils/withOrganization';
 
 import Grouping from './grouping';
@@ -13,10 +13,9 @@ type RouteParams = {groupId: Group['id']; orgId: Organization['slug']};
 
 type Props = RouteComponentProps<RouteParams, {}> & {
   organization: Organization;
-  project: Project;
 };
 
-function GroupingContainer({organization, params, location, project}: Props) {
+function GroupingContainer({organization, params, location}: Props) {
   return (
     <Feature
       features={['grouping-tree-ui']}
@@ -31,7 +30,6 @@ function GroupingContainer({organization, params, location, project}: Props) {
         location={location}
         groupId={params.groupId}
         organization={organization}
-        project={project}
       />
     </Feature>
   );
