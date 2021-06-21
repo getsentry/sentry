5bff7236f7963d2785073606f5b539ae86bc5f78
Mark Story <mark@mark-story.com>
2021-06-21 10:30:39 -0400

    Re-add the previous ranges.
    Being extra cautious

--- src/sentry/integrations/bitbucket/webhook.py
+++ src/sentry/integrations/bitbucket/webhook.py
@@ -22,7 +22,10 @@ BITBUCKET_IP_RANGES = (
     ipaddress.ip_network("104.192.136.0/21"),
     # Not documented in the webhook docs, but defined here:
     # https://bitbucket.org/blog/new-ip-addresses-bitbucket-cloud
-    # and in the outgoing IP list for atlassian cloud
+    ipaddress.ip_network("18.205.93.0/25"),
+    ipaddress.ip_network("18.234.32.128/25"),
+    ipaddress.ip_network("13.52.5.0/25"),
+    # Also accept any outbound IPs that atlassian has.
     # https://support.atlassian.com/organization-administration/docs/ip-addresses-and-domains-for-atlassian-cloud-products/
     ipaddress.ip_network("13.52.5.96/28"),
     ipaddress.ip_network("13.236.8.224/28"),
