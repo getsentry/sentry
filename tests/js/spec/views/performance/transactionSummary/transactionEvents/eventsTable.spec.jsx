042e4cdc7cf6a427800074f8ef115b62acff26a4
Edward Gou <edward.gou@sentry.io>
2021-06-21 14:38:57 -0400

    updated tests

--- tests/js/spec/views/performance/transactionSummary/transactionEvents/eventsTable.spec.jsx
+++ tests/js/spec/views/performance/transactionSummary/transactionEvents/eventsTable.spec.jsx
@@ -38,6 +38,7 @@ describe('Performance GridEditable Table', function () {
   let transactionsListTitles;
   let fields;
   let organization;
+  let data;
   const query =
     'transaction.duration:<15m event.type:transaction transaction:/api/0/organizations/{organization_slug}/eventsv2/';
   beforeEach(function () {
@@ -81,6 +82,36 @@ describe('Performance GridEditable Table', function () {
       url: `/organizations/org-slug/legacy-key-transactions-count/`,
       body: [],
     });
+    data = [
+      {
+        id: 'deadbeef',
+        'user.display': 'uhoh@example.com',
+        'transaction.duration': 400,
+        'project.id': 1,
+        timestamp: '2020-05-21T15:31:18+00:00',
+        trace: '1234',
+        'span_ops_breakdown.relative': '',
+        'spans.browser': 100,
+        'spans.db': 30,
+        'spans.http': 170,
+        'spans.resource': 100,
+        'spans.total.time': 400,
+      },
+      {
+        id: 'moredeadbeef',
+        'user.display': 'moreuhoh@example.com',
+        'transaction.duration': 600,
+        'project.id': 1,
+        timestamp: '2020-05-22T15:31:18+00:00',
+        trace: '4321',
+        'span_ops_breakdown.relative': '',
+        'spans.browser': 100,
+        'spans.db': 300,
+        'spans.http': 100,
+        'spans.resource': 100,
+        'spans.total.time': 600,
+      },
+    ];
     // Transaction list response
     MockApiClient.addMockResponse(
       {
@@ -98,24 +129,7 @@ describe('Performance GridEditable Table', function () {
             'project.id': 'integer',
             timestamp: 'date',
           },
-          data: [
-            {
-              id: 'deadbeef',
-              'user.display': 'uhoh@example.com',
-              'transaction.duration': 400,
-              'project.id': 1,
-              timestamp: '2020-05-21T15:31:18+00:00',
-              trace: '1234',
-            },
-            {
-              id: 'moredeadbeef',
-              'user.display': 'moreuhoh@example.com',
-              'transaction.duration': 600,
-              'project.id': 1,
-              timestamp: '2020-05-22T15:31:18+00:00',
-              trace: '4321',
-            },
-          ],
+          data,
         },
       },
       {
@@ -134,7 +148,7 @@ describe('Performance GridEditable Table', function () {
     jest.clearAllMocks();
   });
 
-  it('renders basic UI elements when feature flagged', async function () {
+  it('renders ops breakdown bar when querying for span_ops_breakdown.relative', async function () {
     const initialData = initializeData({features: ['performance-events-page']});
     const eventView = EventView.fromNewQueryWithLocation(
       {
@@ -164,5 +178,54 @@ describe('Performance GridEditable Table', function () {
     expect(wrapper.find('GridHeadCell')).toHaveLength(6);
     expect(wrapper.find('GridHeadCellStatic')).toHaveLength(0);
     expect(wrapper.find('OperationSort')).toHaveLength(1);
+    expect(wrapper.find('RelativeOpsBreakdown')).toHaveLength(2);
+  });
+
+  it('renders basic columns without ops breakdown when not querying for span_ops_breakdown.relative', async function () {
+    const initialData = initializeData({features: ['performance-events-page']});
+    fields = [
+      'id',
+      'user.display',
+      'transaction.duration',
+      'trace',
+      'timestamp',
+      'spans.http',
+    ];
+    data.forEach(result => {
+      delete result['span_ops_breakdown.relative'];
+      delete result['spans.resource'];
+      delete result['spans.browser'];
+      delete result['spans.db'];
+      delete result['spans.total.time'];
+    });
+    const eventView = EventView.fromNewQueryWithLocation(
+      {
+        id: undefined,
+        version: 2,
+        name: 'transactionName',
+        fields,
+        query,
+        projects: [],
+        orderby: '-timestamp',
+      },
+      initialData.router.location
+    );
+    const wrapper = mountWithTheme(
+      <EventsTable
+        eventView={eventView}
+        organization={organization}
+        location={initialData.router.location}
+        setError={this.setError}
+        columnTitles={transactionsListTitles}
+      />,
+      initialData.routerContext
+    );
+    await tick();
+    wrapper.update();
+
+    expect(wrapper.find('GridHeadCell')).toHaveLength(6);
+    expect(wrapper.find('GridHeadCellStatic')).toHaveLength(0);
+    expect(wrapper.find('OperationSort')).toHaveLength(0);
+    expect(wrapper.find('RelativeOpsBreakdown')).toHaveLength(0);
   });
 });
