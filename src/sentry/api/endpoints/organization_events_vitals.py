c844407da97ea2a422de8f3fdf502306b9f3baf5
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-06-21 16:06:20 -0400

    turn off auto aggregations

--- src/sentry/api/endpoints/organization_events_vitals.py
+++ src/sentry/api/endpoints/organization_events_vitals.py
@@ -54,7 +54,7 @@ class OrganizationEventsVitalsEndpoint(OrganizationEventsV2EndpointBase):
                 limit=1,
                 referrer="api.events.vitals",
                 auto_fields=True,
-                auto_aggregations=True,
+                auto_aggregations=False,
                 use_aggregate_conditions=False,
             )
 
