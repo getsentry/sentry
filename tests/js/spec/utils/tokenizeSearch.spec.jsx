6a7a87e85fa6b4a7a408261cf53320e0b13e5be8
Tony Xiao <txiao@sentry.io>
2021-06-24 11:01:26 -0400

    fix(performance): Escape tag values when adding to query (#26844)
    Tag values that are added to the query through the UI may contain special
    characters like asterisks and slashes that need to be escaped. This ensures that
    when adding them to the query, they are correctly escaped.

--- tests/js/spec/utils/tokenizeSearch.spec.jsx
+++ tests/js/spec/utils/tokenizeSearch.spec.jsx
@@ -218,8 +218,13 @@ describe('utils/tokenizeSearch', function () {
       results.addTagValues('d', ['d']);
       expect(results.formatString()).toEqual('a:a b:b c:c1 c:c2 d:d');
 
+      results.addTagValues('e', ['e1*e2\\e3']);
+      expect(results.formatString()).toEqual('a:a b:b c:c1 c:c2 d:d e:"e1\\*e2\\\\e3"');
+
       results.addStringTag('d:d2');
-      expect(results.formatString()).toEqual('a:a b:b c:c1 c:c2 d:d d:d2');
+      expect(results.formatString()).toEqual(
+        'a:a b:b c:c1 c:c2 d:d e:"e1\\*e2\\\\e3" d:d2'
+      );
     });
 
     it('add text searches to query object', function () {
