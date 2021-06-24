059ad961faf05b44bcb1df5ba076adbe8107128e
Marcos Gaeta <31750075+mgaeta@users.noreply.github.com>
2021-06-24 10:41:06 -0700

    types(python): Feature Flag types (#26672)

--- src/sentry/features/base.py
+++ src/sentry/features/base.py
@@ -1,24 +1,40 @@
 __all__ = ["Feature", "OrganizationFeature", "ProjectFeature", "ProjectPluginFeature"]
 
+from abc import ABC
+from typing import TYPE_CHECKING, Any
 
-class Feature:
-    def __init__(self, name):
+if TYPE_CHECKING:
+    from sentry.models import Organization, Project
+
+
+class Feature(ABC):
+    def __init__(self, name: str, *args: Any, **kwargs: Any) -> None:
+        """
+        `FeatureManager.get()` and `FeatureCheckBatch.get_feature_objects()`
+        expect to be able to pass a `Feature` arbitrary `args` and `kwargs`.
+        """
         self.name = name
+
+    def get_organization(self) -> "Organization":
+        raise NotImplementedError
 
 
 class OrganizationFeature(Feature):
-    def __init__(self, name, organization):
-        Feature.__init__(self, name)
+    def __init__(self, name: str, organization: "Organization") -> None:
+        super().__init__(name)
         self.organization = organization
 
+    def get_organization(self) -> "Organization":
+        return self.organization
 
-class ProjectFeature(Feature):
-    def __init__(self, name, project):
-        Feature.__init__(self, name)
+
+class ProjectFeature(OrganizationFeature):
+    def __init__(self, name: str, project: "Project") -> None:
+        super().__init__(name, organization=project.organization)
         self.project = project
 
 
 class ProjectPluginFeature(ProjectFeature):
-    def __init__(self, name, project, plugin):
-        ProjectFeature.__init__(self, name, project)
+    def __init__(self, name: str, project: "Project", plugin: Any) -> None:
+        super().__init__(name, project=project)
         self.plugin = plugin
