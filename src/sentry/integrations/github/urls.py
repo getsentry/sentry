21c70ae8e04b8b9ad424a465a9ca55c068550d0c
Aniket Das <aniket.das@sentry.io>
2021-06-25 11:49:12 -0700

    ref(github): Use signed URL for github installation [WIP]
    Update the github installation to use a signed URL. Unit tests are still needed
    
    Fixes API-1956

--- src/sentry/integrations/github/urls.py
+++ src/sentry/integrations/github/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url
 
+from sentry.web.frontend.github_extension_installation import GithubExtensionConfigurationView
+
 from .search import GitHubSearchEndpoint
 from .webhook import GitHubIntegrationsWebhookEndpoint
 
@@ -9,5 +11,10 @@ urlpatterns = [
         r"^search/(?P<organization_slug>[^\/]+)/(?P<integration_id>\d+)/$",
         GitHubSearchEndpoint.as_view(),
         name="sentry-extensions-github-search",
+    ),
+    url(
+        r"^extensions/external-install/$",
+        GithubExtensionConfigurationView.as_view(),
+        name="integration-installation",
     ),
 ]
