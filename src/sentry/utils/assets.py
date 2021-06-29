065928f31d7c042772801b911d9b2edfa31d3a99
Billy Vong <billyvg@users.noreply.github.com>
2021-06-29 13:54:31 -0700

    Revert "feat: Use unversioned frontend asset paths (#25994)" (#26974)
    This reverts commit 5cd4805719eb11ca7e533dcd91438cab58e0d0e2.

--- src/sentry/utils/assets.py
+++ src/sentry/utils/assets.py
@@ -1,18 +1,29 @@
 from django.conf import settings
+from manifest_loader.utils import _get_manifest, _load_from_manifest
 
 
-def get_unversioned_asset_url(module, key):
+def get_manifest_obj():
     """
-    Returns an asset URL that is unversioned. These assets should have a
-    `Cache-Control: no-cache` so that clients must validate with the origin
-    server before using their locally cached asset.
+    Returns the webpack asset manifest as a dict of <file key, hashed file name>
+
+    The `manifest_loader` library caches this (if `cache` settings is set)
+    """
+    return _get_manifest()
+
+
+def get_manifest_url(module, key):
+    """
+    Returns an asset URL that is produced by webpack. Uses webpack's manifest to map
+    `key` to the asset produced by webpack. Required if using file contents based hashing for filenames.
 
     Example:
-      {% unversioned_asset_url 'sentry' 'sentry.css' %}
-      =>  "/_static/dist/sentry/sentry.css"
+      {% manifest_asset_url 'sentry' 'sentry.css' %}
+      =>  "/_static/dist/sentry/sentry.filehash123.css"
     """
+    manifest_obj = get_manifest_obj()
+    manifest_value = _load_from_manifest(manifest_obj, key=key)
 
-    return "{}/{}/{}".format(settings.STATIC_UNVERSIONED_URL.rstrip("/"), module, key.lstrip("/"))
+    return "{}/{}/{}".format(settings.STATIC_MANIFEST_URL.rstrip("/"), module, manifest_value)
 
 
 def get_asset_url(module, path):
@@ -20,7 +31,7 @@ def get_asset_url(module, path):
     Returns a versioned asset URL (located within Sentry's static files).
 
     Example:
-    {% asset_url 'sentry' 'images/sentry.png' %}
-    =>  "/_static/74d127b78dc7daf2c51f/sentry/sentry.png"
+      {% asset_url 'sentry' 'images/sentry.png' %}
+      =>  "/_static/74d127b78dc7daf2c51f/sentry/sentry.png"
     """
     return "{}/{}/{}".format(settings.STATIC_URL.rstrip("/"), module, path.lstrip("/"))
