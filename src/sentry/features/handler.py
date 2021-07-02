4c7eaae4bc90249a8e5dfc13f32cb0ef990c20c3
Marcos Gaeta <31750075+mgaeta@users.noreply.github.com>
2021-07-02 14:37:37 -0700

    Revert "Revert "types(python): Feature Flag types (#26672)" (#26874)"
    This reverts commit ea0d6c7a281ec0daf20b2daa0c96ce9cab0247a5.

--- src/sentry/features/handler.py
+++ src/sentry/features/handler.py
@@ -1,24 +1,41 @@
 __all__ = ["FeatureHandler", "BatchFeatureHandler"]
 
+from typing import TYPE_CHECKING, Mapping, MutableSet, Optional, Sequence
+
+if TYPE_CHECKING:
+    from sentry.features.base import Feature
+    from sentry.features.manager import FeatureCheckBatch
+    from sentry.models import Organization, Project, User
+
 
 class FeatureHandler:
-    features = set()
+    features: MutableSet[str] = set()
 
-    def __call__(self, feature, actor):
+    def __call__(self, feature: "Feature", actor: "User") -> Optional[bool]:
         if feature.name not in self.features:
             return None
 
         return self.has(feature, actor)
 
-    def has(self, feature, actor):
+    def has(self, feature: "Feature", actor: "User") -> bool:
         raise NotImplementedError
 
-    def has_for_batch(self, batch):
+    def has_for_batch(self, batch: "FeatureCheckBatch") -> Mapping["Project", bool]:
         # If not overridden, iterate over objects in the batch individually.
         return {
             obj: self.has(feature, batch.actor)
             for (obj, feature) in batch.get_feature_objects().items()
         }
+
+    def batch_has(
+        self,
+        feature_names: Sequence[str],
+        actor: "User",
+        projects: Optional[Sequence["Project"]] = None,
+        organization: Optional["Organization"] = None,
+        batch: bool = True,
+    ) -> Optional[bool]:
+        raise NotImplementedError
 
 
 # It is generally better to extend BatchFeatureHandler if it is possible to do
@@ -28,13 +45,14 @@ class FeatureHandler:
 
 
 class BatchFeatureHandler(FeatureHandler):
-    def _check_for_batch(self, feature_name, organization, actor):
+    def _check_for_batch(
+        self, feature_name: str, organization: "Organization", actor: "User"
+    ) -> bool:
         raise NotImplementedError
 
-    def has(self, feature, actor):
-        organization = getattr(feature, "organization", None) or feature.project.organization
-        return self._check_for_batch(feature.name, organization, actor)
+    def has(self, feature: "Feature", actor: "User") -> bool:
+        return self._check_for_batch(feature.name, feature.get_organization(), actor)
 
-    def has_for_batch(self, batch):
+    def has_for_batch(self, batch: "FeatureCheckBatch") -> Mapping["Project", bool]:
         flag = self._check_for_batch(batch.feature_name, batch.organization, batch.actor)
         return {obj: flag for obj in batch.objects}
