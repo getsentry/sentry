d905dddea5bf98a8c7555c1e4775e72785d70810
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-07-02 11:08:53 +0200

    applied cr feedback

--- static/app/utils/getSdkUpdateSuggestion.tsx
+++ static/app/utils/getSdkUpdateSuggestion.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 import ExternalLink from 'app/components/links/externalLink';
 import {t, tct} from 'app/locale';
 import space from 'app/styles/space';
+import {UpdateSdkSuggestion} from 'app/types';
 import {Event} from 'app/types/event';
 
 type Props = {
@@ -19,26 +20,39 @@ function getSdkUpdateSuggestion({
   shortStyle = false,
   capitalized = false,
 }: Props) {
+  function getUpdateSdkContent(newSdkVersion: UpdateSdkSuggestion['newSdkVersion']) {
+    if (capitalized) {
+      return sdk
+        ? shortStyle
+          ? tct('Update to @v[new-sdk-version]', {
+              ['new-sdk-version']: newSdkVersion,
+            })
+          : tct('Update your SDK from @v[sdk-version] to @v[new-sdk-version]', {
+              ['sdk-version']: sdk.version,
+              ['new-sdk-version']: newSdkVersion,
+            })
+        : t('Update your SDK version');
+    }
+
+    return sdk
+      ? shortStyle
+        ? tct('update to @v[new-sdk-version]', {
+            ['new-sdk-version']: newSdkVersion,
+          })
+        : tct('update your SDK from @v[sdk-version] to @v[new-sdk-version]', {
+            ['sdk-version']: sdk.version,
+            ['new-sdk-version']: newSdkVersion,
+          })
+      : t('update your SDK version');
+  }
+
   const getTitleData = () => {
     switch (suggestion.type) {
-      case 'updateSdk': {
-        const updateWord = capitalized ? t('Update') : t('update');
+      case 'updateSdk':
         return {
           href: suggestion?.sdkUrl,
-          content: sdk
-            ? shortStyle
-              ? tct('[updateWord] to @v[new-sdk-version]', {
-                  updateWord,
-                  ['new-sdk-version']: suggestion.newSdkVersion,
-                })
-              : tct('[updateWord] your SDK from @v[sdk-version] to @v[new-sdk-version]', {
-                  updateWord,
-                  ['sdk-version']: sdk.version,
-                  ['new-sdk-version']: suggestion.newSdkVersion,
-                })
-            : tct('[updateWord] your SDK version', {updateWord}),
+          content: getUpdateSdkContent(suggestion.newSdkVersion),
         };
-      }
       case 'changeSdk':
         return {
           href: suggestion?.sdkUrl,
