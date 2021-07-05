c01e903a391350c945ecc57ea6cd9f07bf35a4b6
Priscila Oliveira <priscilawebdev@gmail.com>
2021-07-05 14:26:14 +0200

    ref(interfaces): Consolidate crumbs and images loaded search + filter (#26960)

--- static/app/components/events/interfaces/breadcrumbs/utils.tsx
+++ static/app/components/events/interfaces/breadcrumbs/utils.tsx
@@ -13,7 +13,61 @@ import {
   IconWarning,
 } from 'app/icons';
 import {t} from 'app/locale';
-import {BreadcrumbType} from 'app/types/breadcrumbs';
+import {Breadcrumb, BreadcrumbLevelType, BreadcrumbType} from 'app/types/breadcrumbs';
+import {defined} from 'app/utils';
+
+function convertCrumbType(breadcrumb: Breadcrumb): Breadcrumb {
+  if (breadcrumb.type === BreadcrumbType.EXCEPTION) {
+    return {
+      ...breadcrumb,
+      type: BreadcrumbType.ERROR,
+    };
+  }
+  // special case for 'ui.' and `sentry.` category breadcrumbs
+  // TODO: find a better way to customize UI around non-schema data
+  if (breadcrumb.type === BreadcrumbType.DEFAULT && defined(breadcrumb?.category)) {
+    const [category, subcategory] = breadcrumb.category.split('.');
+    if (category === 'ui') {
+      return {
+        ...breadcrumb,
+        type: BreadcrumbType.UI,
+      };
+    }
+
+    if (category === 'console') {
+      return {
+        ...breadcrumb,
+        type: BreadcrumbType.DEBUG,
+      };
+    }
+
+    if (category === 'navigation') {
+      return {
+        ...breadcrumb,
+        type: BreadcrumbType.NAVIGATION,
+      };
+    }
+
+    if (
+      category === 'sentry' &&
+      (subcategory === 'transaction' || subcategory === 'event')
+    ) {
+      return {
+        ...breadcrumb,
+        type: BreadcrumbType.TRANSACTION,
+      };
+    }
+  }
+
+  if (!Object.values(BreadcrumbType).includes(breadcrumb.type)) {
+    return {
+      ...breadcrumb,
+      type: BreadcrumbType.DEFAULT,
+    };
+  }
+
+  return breadcrumb;
+}
 
 function getCrumbDetails(type: BreadcrumbType) {
   switch (type) {
@@ -98,4 +152,15 @@ function getCrumbDetails(type: BreadcrumbType) {
   }
 }
 
-export default getCrumbDetails;
+export function transformCrumbs(breadcrumbs: Array<Breadcrumb>) {
+  return breadcrumbs.map((breadcrumb, index) => {
+    const convertedCrumbType = convertCrumbType(breadcrumb);
+    const crumbDetails = getCrumbDetails(convertedCrumbType.type);
+    return {
+      id: index,
+      ...convertedCrumbType,
+      ...crumbDetails,
+      level: convertedCrumbType?.level ?? BreadcrumbLevelType.UNDEFINED,
+    };
+  });
+}
