38b84a1ac6859efef88647ff3eac6c0f6a7320cc
William Mak <william@wmak.io>
2021-07-06 16:39:49 -0400

    fix(arithmetic): Don't intepret bracketed equations as aggregates (#27048)
    - The current regex interprets equations with brackets as equations,
      which means something like `(5 + 5)` looks like an aggregate when it
      really isn't
    - This fixes that by checking the character before the `(` is a valid
      character for a function

--- static/app/utils/discover/fields.tsx
+++ static/app/utils/discover/fields.tsx
@@ -689,9 +689,9 @@ export function getMeasurementSlug(field: string): string | null {
   return null;
 }
 
-const AGGREGATE_PATTERN = /^([^\(]+)\((.*)?\)$/;
+const AGGREGATE_PATTERN = /^(\w+)\((.*)?\)$/;
 // Identical to AGGREGATE_PATTERN, but without the $ for newline, or ^ for start of line
-const AGGREGATE_BASE = /([^\(]+)\((.*)?\)/g;
+const AGGREGATE_BASE = /(\w+)\((.*)?\)/g;
 
 export function getAggregateArg(field: string): string | null {
   // only returns the first argument if field is an aggregate
