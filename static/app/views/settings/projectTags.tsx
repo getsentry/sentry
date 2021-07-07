99ca4e8f96d8a9b1d0e4d863e308148703b99be6
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-07-06 23:46:48 -0700

    ref(ui): Fix double tooltip on tags page (#27180)

--- static/app/views/settings/projectTags.tsx
+++ static/app/views/settings/projectTags.tsx
@@ -7,7 +7,6 @@ import Button from 'app/components/button';
 import Confirm from 'app/components/confirm';
 import ExternalLink from 'app/components/links/externalLink';
 import {Panel, PanelBody, PanelHeader, PanelItem} from 'app/components/panels';
-import Tooltip from 'app/components/tooltip';
 import {IconDelete} from 'app/icons';
 import {t, tct} from 'app/locale';
 import space from 'app/styles/space';
@@ -102,27 +101,24 @@ class ProjectTags extends AsyncView<Props, State> {
                       <TagPanelItem key={key} data-test-id="tag-row">
                         <TagName>{key}</TagName>
                         <Actions>
-                          <Tooltip
-                            disabled={enabled}
-                            title={
-                              hasAccess
-                                ? t('This tag cannot be deleted.')
-                                : t('You do not have permission to remove tags.')
-                            }
+                          <Confirm
+                            message={t('Are you sure you want to remove this tag?')}
+                            onConfirm={this.handleDelete(key, idx)}
+                            disabled={!enabled}
                           >
-                            <Confirm
-                              message={t('Are you sure you want to remove this tag?')}
-                              onConfirm={this.handleDelete(key, idx)}
-                              disabled={!enabled}
-                            >
-                              <Button
-                                size="xsmall"
-                                title={t('Remove tag')}
-                                icon={<IconDelete size="xs" />}
-                                data-test-id="delete"
-                              />
-                            </Confirm>
-                          </Tooltip>
+                            <Button
+                              size="xsmall"
+                              title={
+                                enabled
+                                  ? t('Remove tag')
+                                  : hasAccess
+                                  ? t('This tag cannot be deleted.')
+                                  : t('You do not have permission to remove tags.')
+                              }
+                              icon={<IconDelete size="xs" />}
+                              data-test-id="delete"
+                            />
+                          </Confirm>
                         </Actions>
                       </TagPanelItem>
                     );
