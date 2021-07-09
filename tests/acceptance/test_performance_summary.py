0e2ef751acdb00e9518710a342a61621b8590d53
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-07-09 11:05:23 -0400

    fix snapshot test

--- tests/acceptance/test_performance_summary.py
+++ tests/acceptance/test_performance_summary.py
@@ -194,6 +194,7 @@ class PerformanceSummaryTest(AcceptanceTestCase, SnubaTestCase):
             (
                 "organizations:performance-view",
                 "organizations:project-transaction-threshold-override",
+                "organizations:project-transaction-threshold",
             )
         ):
             self.browser.get(self.path)
@@ -204,6 +205,5 @@ class PerformanceSummaryTest(AcceptanceTestCase, SnubaTestCase):
                 '[data-test-id="grid-editable"] [data-test-id="empty-state"]', timeout=2
             )
             # We have to wait for this again because there are loaders inside of the table
-            self.page.wait_until_loaded()
             self.browser.click('[data-test-id="set-transaction-threshold"]')
             self.browser.snapshot("transaction threshold modal")
