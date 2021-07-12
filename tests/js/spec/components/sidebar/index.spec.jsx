454aefca6a6d6ab960714393f603fb9ae0d2ecdf
Mark Story <mark@sentry.io>
2021-07-12 14:11:59 -0400

    fix(ui) Update sidebar menu (#27320)
    Add a link to help.sentry.io as customers are expecting to find it in
    the help menu. Remove the discord and forum links as they are present on
    help.sentry.io as well. Move 'contact support' (provided via a hook) to
    the bottom so that the menu is ordered by self-service and general
    solution time finding.
    
    Fixes RV-455

--- tests/js/spec/components/sidebar/index.spec.jsx
+++ tests/js/spec/components/sidebar/index.spec.jsx
@@ -112,7 +112,7 @@ describe('Sidebar', function () {
       const menu = wrapper.find('HelpMenu');
       expect(menu).toHaveLength(1);
       expect(wrapper).toSnapshot();
-      expect(menu.find('SidebarMenuItem')).toHaveLength(4);
+      expect(menu.find('SidebarMenuItem')).toHaveLength(2);
       wrapper.find('HelpActor').simulate('click');
       expect(wrapper.find('HelpMenu')).toHaveLength(0);
     });
