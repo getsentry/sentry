94f4114c54d3012ff8d3fd276bc2144ef05fd779
William Mak <william@wmak.io>
2021-07-14 17:42:03 -0400

    fix(arithmetic): Autocomplete breaks on invalid terms
    - When an arrow key is hit when the current term doesn't exist
      autocomplete errors
    - Fixes JAVASCRIPT-24ZG

--- tests/js/spec/views/eventsV2/table/arithmeticInput.spec.tsx
+++ tests/js/spec/views/eventsV2/table/arithmeticInput.spec.tsx
@@ -180,4 +180,13 @@ describe('ArithmeticInput', function () {
     input.simulate('blur');
     expect(query).toEqual(`transaction.duration + measurements.lcp `);
   });
+
+  it('handles autocomplete on invalid term', function () {
+    const input = wrapper.find('input');
+    input.simulate('focus');
+
+    const value = 'foo + bar';
+    input.simulate('change', {target: {value}});
+    input.simulate('keydown', {key: 'ArrowDown'});
+  });
 });
