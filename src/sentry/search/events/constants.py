6a241d2a8c3709aa926f5c1fd92bc63c2a5d706b
k-fish <kevan.fisher@sentry.io>
2021-07-14 16:31:41 -0400

    Revert "Revert "feat(mobile-vitals): Add mobile vitals cards to performance landing (#27252)" (#27361)"
    This reverts commit 46ca84e23318246988ffe06b66c6307cf8c73824.

--- src/sentry/search/events/constants.py
+++ src/sentry/search/events/constants.py
@@ -22,6 +22,8 @@ SEMVER_BUILD_ALIAS = "release.build"
 TIMESTAMP_TO_HOUR_ALIAS = "timestamp.to_hour"
 TIMESTAMP_TO_DAY_ALIAS = "timestamp.to_day"
 TRANSACTION_STATUS_ALIAS = "transaction.status"
+MEASUREMENTS_FRAMES_SLOW_RATE = "measurements.frames_slow_rate"
+MEASUREMENTS_FRAMES_FROZEN_RATE = "measurements.frames_frozen_rate"
 
 TAG_KEY_RE = re.compile(r"^tags\[(?P<tag>.*)\]$")
 # Based on general/src/protocol/tags.rs in relay
