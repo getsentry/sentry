59b635e14b5b4eb97933d52797dc35a8cf8989c9
Floris Bruynooghe <flub@sentry.io>
2021-07-15 10:12:11 +0200

    feat(appconnect): Add performance instrumentation
    Instrument appstore connect operations with our own performance
    monitoring.  This adds a bunch of spans which are useful as part of
    observing the behaviour of the App Store Connect functionality.
    
    This also allows separately instrumenting the task, so that we can
    have good data even when we do not yet have many customers using it.

--- src/sentry/tasks/app_store_connect.py
+++ src/sentry/tasks/app_store_connect.py
@@ -9,6 +9,8 @@ import pathlib
 import tempfile
 from typing import List, Mapping
 
+import sentry_sdk
+
 from sentry.lang.native import appconnect
 from sentry.models import AppConnectBuild, Project, ProjectOption, debugfile
 from sentry.tasks.base import instrumented_task
@@ -22,7 +24,7 @@ logger = logging.getLogger(__name__)
 # typing annotations.  So we do all the work outside of the decorated task function to work
 # around this.
 # Since all these args must be pickled we keep them to built-in types as well.
-@instrumented_task(name="sentry.tasks.app_store_connect.dsym_download", queue="appstoreconnect")  # type: ignore
+@instrumented_task(name="sentry.tasks.app_store_connect.dsym_download", queue="appstoreconnect", ignore_result=True)  # type: ignore
 def dsym_download(project_id: int, config_id: str) -> None:
     inner_dsym_download(project_id=project_id, config_id=config_id)
 
@@ -41,10 +43,14 @@ def inner_dsym_download(project_id: int, config_id: str) -> None:
 
     # persist all fetched builds into the database as "pending"
     builds = []
-    for build in client.list_builds():
-        build_state = get_or_create_persisted_build(project, config, build)
-        if not build_state.fetched:
-            builds.append((build, build_state))
+    listed_builds = client.list_builds()
+    with sentry_sdk.start_span(
+        op="appconnect-update-builds", description="Update AppStoreConnect builds in database"
+    ):
+        for build in listed_builds:
+            build_state = get_or_create_persisted_build(project, config, build)
+            if not build_state.fetched:
+                builds.append((build, build_state))
 
     itunes_client = client.itunes_client()
     for (build, build_state) in builds:
@@ -71,17 +77,17 @@ def inner_dsym_download(project_id: int, config_id: str) -> None:
 
 
 def create_difs_from_dsyms_zip(dsyms_zip: str, project: Project) -> None:
-    with open(dsyms_zip, "rb") as fp:
-        created = debugfile.create_files_from_dif_zip(fp, project, accept_unknown=True)
-        for proj_debug_file in created:
-            logger.debug("Created %r for project %s", proj_debug_file, project.id)
+    with sentry_sdk.start_span(op="dsym-difs", description="Extract difs dSYM zip"):
+        with open(dsyms_zip, "rb") as fp:
+            created = debugfile.create_files_from_dif_zip(fp, project, accept_unknown=True)
+            for proj_debug_file in created:
+                logger.debug("Created %r for project %s", proj_debug_file, project.id)
 
 
 def get_or_create_persisted_build(
     project: Project, config: appconnect.AppStoreConnectConfig, build: appconnect.BuildInfo
 ) -> AppConnectBuild:
-    """
-    Fetches the sentry-internal :class:`AppConnectBuild`.
+    """Fetches the sentry-internal :class:`AppConnectBuild`.
 
     The build corresponds to the :class:`appconnect.BuildInfo` as returned by the
     AppStore Connect API. If no build exists yet, a new "pending" build is created.
@@ -112,7 +118,9 @@ def get_or_create_persisted_build(
 # Untyped decorator would stop type-checking of entire function, split into an inner
 # function instead which can be type checked.
 @instrumented_task(  # type: ignore
-    name="sentry.tasks.app_store_connect.refresh_all_builds", queue="appstoreconnect"
+    name="sentry.tasks.app_store_connect.refresh_all_builds",
+    queue="appstoreconnect",
+    ignore_result=True,
 )
 def refresh_all_builds() -> None:
     inner_refresh_all_builds()
