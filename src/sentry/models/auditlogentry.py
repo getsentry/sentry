f693fbba18cb86c44862c60fdb4ca0a9d799fbd8
Josh Ferge <josh.ferge@sentry.io>
2021-07-16 15:54:48 -0700

    use index instead of index_together

--- src/sentry/models/auditlogentry.py
+++ src/sentry/models/auditlogentry.py
@@ -195,7 +195,7 @@ class AuditLogEntry(Model):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_auditlogentry"
-        index_together = (("organization", "datetime"),)
+        indexes = [models.Index(fields=["organization", "datetime"])]
 
     __repr__ = sane_repr("organization_id", "type")
 
