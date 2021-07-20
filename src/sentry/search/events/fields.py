f80ac96335531025b1a45c9c5922dd1268270099
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-07-20 15:40:57 -0400

    feat(discover): Add team key txn to SnQL
    Add query and filter support for Team Key
    Transactions in SnQL.

--- src/sentry/search/events/fields.py
+++ src/sentry/search/events/fields.py
@@ -2124,9 +2124,7 @@ class QueryFields(QueryBase):
             PROJECT_THRESHOLD_CONFIG_ALIAS: self._resolve_project_threshold_config,
             ERROR_UNHANDLED_ALIAS: self._resolve_error_unhandled_alias,
             ERROR_HANDLED_ALIAS: self._resolve_error_handled_alias,
-            # TODO: implement these
-            KEY_TRANSACTION_ALIAS: self._resolve_unimplemented_alias,
-            TEAM_KEY_TRANSACTION_ALIAS: self._resolve_unimplemented_alias,
+            TEAM_KEY_TRANSACTION_ALIAS: self._resolve_team_key_transaction_alias,
         }
 
     def resolve_select(self, selected_columns: Optional[List[str]]) -> List[SelectType]:
@@ -2396,6 +2394,46 @@ class QueryFields(QueryBase):
 
         return _project_threshold_config(PROJECT_THRESHOLD_CONFIG_ALIAS)
 
+    def _resolve_team_key_transaction_alias(self, _: str) -> SelectType:
+        org_id = self.params.get("organization_id")
+        project_ids = self.params.get("project_id")
+        team_ids = self.params.get("team_id")
+
+        if org_id is None or team_ids is None or project_ids is None:
+            raise TypeError("Team key transactions parameters cannot be None")
+
+        team_key_transactions = list(
+            TeamKeyTransaction.objects.filter(
+                organization_id=org_id,
+                project_team__in=ProjectTeam.objects.filter(
+                    project_id__in=project_ids, team_id__in=team_ids
+                ),
+            )
+            .order_by("transaction", "project_team__project_id")
+            .values_list("project_team__project_id", "transaction")
+            .distinct("transaction", "project_team__project_id")[
+                :MAX_QUERYABLE_TEAM_KEY_TRANSACTIONS
+            ]
+        )
+
+        count = len(team_key_transactions)
+
+        # NOTE: this raw count is not 100% accurate because if it exceeds
+        # `MAX_QUERYABLE_TEAM_KEY_TRANSACTIONS`, it will not be reflected
+        sentry_sdk.set_tag("team_key_txns.count", count)
+        sentry_sdk.set_tag(
+            "team_key_txns.count.grouped", format_grouped_length(count, [10, 100, 250, 500])
+        )
+
+        if count == 0:
+            return Function("toInt8", [0], TEAM_KEY_TRANSACTION_ALIAS)
+
+        return Function(
+            "in",
+            [(self.column("project_id"), self.column("transaction")), team_key_transactions],
+            TEAM_KEY_TRANSACTION_ALIAS,
+        )
+
     def _resolve_error_unhandled_alias(self, _: str) -> SelectType:
         return Function("notHandled", [], ERROR_UNHANDLED_ALIAS)
 
@@ -2405,9 +2443,3 @@ class QueryFields(QueryBase):
         return Function(
             "cast", [self.column("error.handled"), "Array(Nullable(UInt8))"], ERROR_HANDLED_ALIAS
         )
-
-    def _resolve_unimplemented_alias(self, alias: str) -> SelectType:
-        """Used in the interim as a stub for ones that have not be implemented in SnQL yet.
-        Can be deleted once all field aliases have been implemented.
-        """
-        raise NotImplementedError(f"{alias} not implemented in snql field parsing yet")
--- src/sentry/search/events/filter.py
+++ src/sentry/search/events/filter.py
@@ -1023,6 +1023,7 @@ class QueryFilter(QueryFields):
             ISSUE_ID_ALIAS: self._issue_id_filter_converter,
             ERROR_HANDLED_ALIAS: self._error_handled_filter_converter,
             ERROR_UNHANDLED_ALIAS: self._error_unhandled_filter_converter,
+            TEAM_KEY_TRANSACTION_ALIAS: self._key_transaction_filter_converter,
         }
 
     def resolve_where(self, query: Optional[str]) -> List[WhereType]:
@@ -1305,4 +1306,21 @@ class QueryFilter(QueryFields):
             return Condition(Function("notHandled", []), Op.EQ, 1)
         raise InvalidSearchQuery(
             "Invalid value for error.handled condition. Accepted values are 1, 0"
+        )
+
+    def _key_transaction_filter_converter(self, search_filter: SearchFilter) -> Optional[WhereType]:
+        value = search_filter.value.value
+        key_transaction_expr = self.resolve_field(TEAM_KEY_TRANSACTION_ALIAS)
+
+        if search_filter.value.raw_value == "":
+            return Condition(
+                key_transaction_expr, Op.NEQ if search_filter.operator == "!=" else Op.EQ, 0
+            )
+        if value in ("1", 1):
+            return Condition(key_transaction_expr, Op.EQ, 1)
+        if value in ("0", 0):
+            return Condition(key_transaction_expr, Op.EQ, 0)
+
+        raise InvalidSearchQuery(
+            "Invalid value for key_transaction condition. Accepted values are 1, 0"
         )
