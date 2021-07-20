4ba74dccd6d791202ef81207ead9c6d6b9c84275
Billy Vong <billyvg@users.noreply.github.com>
2021-07-20 14:06:31 -0700

    chore(deps): Upgrade jest to v27 (#26393)
    Upgrades to `jest@27`

--- tests/js/jest-pegjs-transform.js
+++ tests/js/jest-pegjs-transform.js
@@ -4,8 +4,12 @@
 const crypto = require('crypto');
 const peg = require('pegjs');
 
-function getCacheKey(fileData, _filePath, configString, _options) {
-  return crypto.createHash('md5').update(fileData).update(configString).digest('hex');
+function getCacheKey(fileData, _filePath, config, _options) {
+  return crypto
+    .createHash('md5')
+    .update(fileData)
+    .update(config.configString)
+    .digest('hex');
 }
 
 function process(sourceText) {
