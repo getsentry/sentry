a66d6a258c3a7ef3f2e3957834430314147998c1
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-07-21 16:42:29 -0700

    Add types

--- src/sentry/db/models/manager/__init__.py
+++ src/sentry/db/models/manager/__init__.py
@@ -9,28 +9,27 @@ __all__ = ("BaseManager", "OptionManager")
 
 
 def __prep_value(model: Any, key: str, value: Union[Model, int, str]) -> str:
+    val = value
     if isinstance(value, Model):
-        return value.pk
-    else:
-        value = str(value)
-    return value
+        val = value.pk
+    return str(val)
 
 
 def __prep_key(model: Any, key: str) -> str:
     if key == "pk":
-        return model._meta.pk.name
+        return str(model._meta.pk.name)
     return key
 
 
-def make_key(model: Any, prefix: str, kwargs: Mapping[str, Union[Model, int, str]]):
+def make_key(model: Any, prefix: str, kwargs: Mapping[str, Union[Model, int, str]]) -> str:
     kwargs_bits = []
     for k, v in sorted(kwargs.items()):
         k = __prep_key(model, k)
         v = smart_text(__prep_value(model, k, v))
         kwargs_bits.append(f"{k}={v}")
-    kwargs_bits = ":".join(kwargs_bits)
+    kwargs_bits_str = ":".join(kwargs_bits)
 
-    return f"{prefix}:{model.__name__}:{md5_text(kwargs_bits).hexdigest()}"
+    return f"{prefix}:{model.__name__}:{md5_text(kwargs_bits_str).hexdigest()}"
 
 
 # Exporting these classes at the bottom to avoid circular dependencies.
