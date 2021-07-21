f194ed47dc4d9256d63180074d332e05283c02c7
Shruthi <shruthilaya.jaganathan@sentry.io>
2021-07-21 17:07:44 -0400

    feat(discover): Add team key txn to SnQL (#27594)
    Add query and filter support for Team Key
    Transactions in SnQL.

--- src/sentry/search/events/filter.py
+++ src/sentry/search/events/filter.py
@@ -1024,6 +1024,7 @@ class QueryFilter(QueryFields):
             ISSUE_ID_ALIAS: self._issue_id_filter_converter,
             ERROR_HANDLED_ALIAS: self._error_handled_filter_converter,
             ERROR_UNHANDLED_ALIAS: self._error_unhandled_filter_converter,
+            TEAM_KEY_TRANSACTION_ALIAS: self._key_transaction_filter_converter,
         }
 
     def parse_query(self, query: Optional[str]) -> Optional[Sequence[SearchFilter]]:
@@ -1355,4 +1356,21 @@ class QueryFilter(QueryFields):
             return Condition(Function("notHandled", []), Op.EQ, 1)
         raise InvalidSearchQuery(
             "Invalid value for error.handled condition. Accepted values are 1, 0"
+        )
+
+    def _key_transaction_filter_converter(self, search_filter: SearchFilter) -> Optional[WhereType]:
+        value = search_filter.value.value
+        key_transaction_expr = self.resolve_field(TEAM_KEY_TRANSACTION_ALIAS)
+
+        if search_filter.value.raw_value == "":
+            return Condition(
+                key_transaction_expr, Op.NEQ if search_filter.operator == "!=" else Op.EQ, 0
+            )
+        if value in ("1", 1):
+            return Condition(key_transaction_expr, Op.EQ, 1)
+        if value in ("0", 0):
+            return Condition(key_transaction_expr, Op.EQ, 0)
+
+        raise InvalidSearchQuery(
+            "Invalid value for key_transaction condition. Accepted values are 1, 0"
         )
