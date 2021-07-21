0be8e229cc740ec3c7a48e9190715fef44d10ddb
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-07-21 10:17:06 +0200

    minor change

--- static/app/components/events/interfaces/frame/lineV2/wrapper.tsx
+++ static/app/components/events/interfaces/frame/lineV2/wrapper.tsx
@@ -9,19 +9,23 @@ const Wrapper = styled('div')<{
   display: grid;
   grid-template-columns: ${p =>
     p.haveFramesAtLeastOneGroupingBadge && p.haveFramesAtLeastOneExpandedFrame
-      ? '1.5fr 16px'
+      ? '1fr 16px'
       : p.haveFramesAtLeastOneGroupingBadge
-      ? '1fr 0.5fr'
+      ? '1fr'
       : p.haveFramesAtLeastOneExpandedFrame
       ? '1fr 16px'
       : '1fr'};
   grid-gap: ${space(1)};
   @media (min-width: ${props => props.theme.breakpoints[0]}) {
     align-items: center;
-    ${p =>
-      p.haveFramesAtLeastOneGroupingBadge &&
-      p.haveFramesAtLeastOneExpandedFrame &&
-      `grid-template-columns: 1.5fr 0.5fr 16px`};
+    grid-template-columns: ${p =>
+      p.haveFramesAtLeastOneGroupingBadge && p.haveFramesAtLeastOneExpandedFrame
+        ? '1.5fr 0.5fr 16px'
+        : p.haveFramesAtLeastOneGroupingBadge
+        ? '1fr 0.5fr'
+        : p.haveFramesAtLeastOneExpandedFrame
+        ? '1fr 16px'
+        : '1fr'};
   }
 `;
 
