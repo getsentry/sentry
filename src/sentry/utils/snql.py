131386f0d9fa92591909d211cdda0d2484e21d4e
Evan Hicks <evanh@users.noreply.github.com>
2021-07-22 11:50:05 -0400

    fix(snql) Update to latest version (#27598)

--- src/sentry/utils/snql.py
+++ src/sentry/utils/snql.py
@@ -9,4 +9,7 @@ def should_use_snql(referrer: Optional[str]) -> bool:
     if not isinstance(snql_rate, float):
         return False
 
+    if snql_rate == 1.0:
+        return True
+
     return random.random() <= snql_rate
