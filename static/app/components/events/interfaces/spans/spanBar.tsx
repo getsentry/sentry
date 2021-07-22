cd43955f5d8aa88aa95d4a64145f974b04a995ca
Alberto Leal <mail4alberto@gmail.com>
2021-07-22 16:53:22 -0400

    feat(spans): Be able to regroup nested spans

--- static/app/components/events/interfaces/spans/spanBar.tsx
+++ static/app/components/events/interfaces/spans/spanBar.tsx
@@ -119,6 +119,7 @@ type SpanBarProps = {
     | undefined;
   fetchEmbeddedChildrenState: FetchEmbeddedChildrenState;
   hasCollapsedSpanGroup: boolean;
+  toggleSpanGroup: (() => void) | undefined;
 };
 
 type SpanBarState = {
@@ -445,16 +446,42 @@ class SpanBar extends React.Component<SpanBarProps, SpanBarState> {
     errors: TraceError[] | null
   ) {
     const {generateContentSpanBarRef} = scrollbarManagerChildrenProps;
-    const {span, treeDepth} = this.props;
+    const {span, treeDepth, toggleSpanGroup} = this.props;
 
-    const operationName = getSpanOperation(span) ? (
-      <strong>
-        {getSpanOperation(span)}
-        {' \u2014 '}
-      </strong>
-    ) : (
-      ''
-    );
+    let titleFragments: React.ReactNode[] = [];
+
+    if (typeof toggleSpanGroup === 'function') {
+      titleFragments.push(
+        <Regroup
+          onClick={event => {
+            event.stopPropagation();
+            event.preventDefault();
+            toggleSpanGroup();
+          }}
+        >
+          <a
+            href="#regroup"
+            onClick={event => {
+              event.preventDefault();
+            }}
+          >
+            {t('Regroup')}
+          </a>
+        </Regroup>
+      );
+    }
+
+    const spanOperationName = getSpanOperation(span);
+    if (spanOperationName) {
+      titleFragments.push(spanOperationName);
+    }
+
+    titleFragments = titleFragments.reduce((acc: React.ReactNode[], current) => {
+      acc.push(current);
+      acc.push(' \u2014 ');
+      return acc;
+    }, []);
+
     const description = span?.description ?? getSpanID(span);
 
     const left = treeDepth * (TOGGLE_BORDER_BOX / 2) + MARGIN_LEFT;
@@ -473,7 +500,7 @@ class SpanBar extends React.Component<SpanBarProps, SpanBarState> {
           }}
         >
           <RowTitleContent errored={errored}>
-            {operationName}
+            <strong>{titleFragments}</strong>
             {description}
           </RowTitleContent>
         </RowTitle>
@@ -977,5 +1004,7 @@ const StyledIconWarning = styled(IconWarning)`
   margin-left: ${space(0.25)};
   margin-bottom: ${space(0.25)};
 `;
+
+const Regroup = styled('span')``;
 
 export default SpanBar;
