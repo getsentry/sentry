3539f0ecfeaef37be0bd83c9615dc3057c7a7a79
Joris Bayer <joris.bayer@sentry.io>
2021-07-27 13:24:10 +0200

    feat(grouping): Mark mobile strategy as "latest" to allow upgrading (#27668)
    Present beta grouping config as "latest" if the grouping-tree-ui feature
    flag is enabled.
    
    https://getsentry.atlassian.net/browse/INGEST-177

--- src/sentry/projectoptions/defaults.py
+++ src/sentry/projectoptions/defaults.py
@@ -14,6 +14,7 @@ LATEST_EPOCH = 7
 # epoch instead.
 LEGACY_GROUPING_CONFIG = "legacy:2019-03-12"
 DEFAULT_GROUPING_CONFIG = "newstyle:2019-10-29"
+BETA_GROUPING_CONFIG = "mobile:2021-02-12"
 register(
     key="sentry:grouping_config",
     epoch_defaults={
