0ab0a78998c5c293c12cf6785ed603ec5c6a83eb
Betty Da <bda@sentry.io>
2021-07-27 15:31:42 -0400

    update tests

---
+++ tests/sentry/models/test_lastappconnectbuildscheck.py
@@ -0,0 +1,156 @@
+import uuid
+from datetime import datetime
+
+import pytest
+from django.utils import timezone
+
+from sentry.lang.native import appconnect
+from sentry.models.latestappconnectbuildscheck import LatestAppConnectBuildsCheck
+
+
+class TestCreateOrUpdateBuildCheck:
+    @pytest.fixture
+    def config(self):
+        return appconnect.AppStoreConnectConfig(
+            type="appStoreConnect",
+            id=uuid.uuid4().hex,
+            name="Apple Honk Store Connect",
+            appconnectIssuer="abc123" * 6,
+            appconnectKey="abc123key",
+            appconnectPrivateKey="----BEGIN PRIVATE KEY---- honkhonk",
+            itunesUser="me@example.com",
+            itunesPassword="secrethonk",
+            itunesSession="THE-COOKIE",
+            itunesCreated=datetime.utcnow(),
+            appName="honk beep",
+            appId="123",
+            bundleId="com.honk.app",
+            orgPublicId="71105f98-7743-4844-ab70-2c901e2ea13d",
+            orgName="Honk Com",
+        )
+
+    @pytest.mark.django_db
+    def test_add_new_check_date(self, default_project, config):
+        with pytest.raises(LatestAppConnectBuildsCheck.DoesNotExist):
+            LatestAppConnectBuildsCheck.objects.get(project=default_project, source_id=config.id)
+
+        now = timezone.now()
+        LatestAppConnectBuildsCheck.objects.create_or_update(
+            project=default_project, source_id=config.id, values={"last_checked": now}
+        )
+
+        assert LatestAppConnectBuildsCheck.objects.count() == 1
+
+        try:
+            check_entry = LatestAppConnectBuildsCheck.objects.get(
+                project=default_project, source_id=config.id
+            )
+
+        except LatestAppConnectBuildsCheck.DoesNotExist:
+            raise AssertionError("Build check entry disappeared after update")
+
+        assert check_entry.last_checked == now
+
+    @pytest.mark.django_db
+    def test_update_existing_check_date(self, default_project, config):
+        old_build_check_date = datetime(2020, 2, 20, tzinfo=timezone.utc)
+
+        LatestAppConnectBuildsCheck.objects.create(
+            project=default_project,
+            source_id=config.id,
+            last_checked=old_build_check_date,
+        )
+
+        try:
+            LatestAppConnectBuildsCheck.objects.get(project=default_project, source_id=config.id)
+        except LatestAppConnectBuildsCheck.DoesNotExist:
+            raise AssertionError("Unable to pre-populate database with an existing check entry")
+
+        now = timezone.now()
+        LatestAppConnectBuildsCheck.objects.create_or_update(
+            project=default_project, source_id=config.id, values={"last_checked": now}
+        )
+
+        try:
+            updated_check_entry = LatestAppConnectBuildsCheck.objects.get(
+                project=default_project, source_id=config.id
+            )
+        except LatestAppConnectBuildsCheck.DoesNotExist:
+            raise AssertionError("Build check entry disappeared after update")
+
+        assert updated_check_entry.last_checked == now
+
+    @pytest.mark.django_db
+    def test_add_new_check_date_with_other_builds(self, default_project, config):
+        other_build_id = uuid.uuid4().hex
+        other_build_refresh_date = datetime(2020, 2, 20, tzinfo=timezone.utc)
+
+        LatestAppConnectBuildsCheck.objects.create(
+            project=default_project, source_id=other_build_id, last_checked=other_build_refresh_date
+        )
+
+        assert LatestAppConnectBuildsCheck.objects.count() == 1
+
+        now = timezone.now()
+        LatestAppConnectBuildsCheck.objects.create_or_update(
+            project=default_project, source_id=config.id, values={"last_checked": now}
+        )
+
+        assert LatestAppConnectBuildsCheck.objects.count() == 2
+
+        try:
+            updated_check_entry = LatestAppConnectBuildsCheck.objects.get(
+                project=default_project, source_id=config.id
+            )
+        except LatestAppConnectBuildsCheck.DoesNotExist:
+            raise AssertionError("Build check entry disappeared after update")
+
+        assert updated_check_entry.last_checked == now
+
+        try:
+            other_build_check = LatestAppConnectBuildsCheck.objects.get(
+                project=default_project, source_id=other_build_id
+            )
+        except LatestAppConnectBuildsCheck.DoesNotExist:
+            raise AssertionError("Other build check entry disappeared after update")
+
+        assert other_build_check.last_checked == other_build_refresh_date
+
+    @pytest.mark.django_db
+    def test_update_existing_check_date_with_other_builds(self, default_project, config):
+        other_build_id = uuid.uuid4().hex
+        other_build_refresh_date = datetime(2020, 2, 20, tzinfo=timezone.utc)
+
+        LatestAppConnectBuildsCheck.objects.create(
+            project=default_project, source_id=other_build_id, last_checked=other_build_refresh_date
+        )
+        LatestAppConnectBuildsCheck.objects.create(
+            project=default_project, source_id=config.id, last_checked=other_build_refresh_date
+        )
+
+        assert LatestAppConnectBuildsCheck.objects.count() == 2
+
+        now = timezone.now()
+        LatestAppConnectBuildsCheck.objects.create_or_update(
+            project=default_project, source_id=config.id, values={"last_checked": now}
+        )
+
+        assert LatestAppConnectBuildsCheck.objects.count() == 2
+
+        try:
+            updated_check_entry = LatestAppConnectBuildsCheck.objects.get(
+                project=default_project, source_id=config.id
+            )
+        except LatestAppConnectBuildsCheck.DoesNotExist:
+            raise AssertionError("Build check entry disappeared after update")
+
+        assert updated_check_entry.last_checked == now
+
+        try:
+            other_build_check = LatestAppConnectBuildsCheck.objects.get(
+                project=default_project, source_id=other_build_id
+            )
+        except LatestAppConnectBuildsCheck.DoesNotExist:
+            raise AssertionError("Other build check entry disappeared after update")
+
+        assert other_build_check.last_checked == other_build_refresh_date
--- tests/sentry/tasks/test_app_store_connect.py
+++ tests/sentry/tasks/test_app_store_connect.py
@@ -1,13 +1,11 @@
 import uuid
-from datetime import datetime, timezone
+from datetime import datetime
 
 import pytest
-from dateutil.parser import parse as parse_date
 
 from sentry.lang.native import appconnect
 from sentry.models.appconnectbuilds import AppConnectBuild
-from sentry.tasks.app_store_connect import get_or_create_persisted_build, update_build_refresh_date
-from sentry.utils import json
+from sentry.tasks.app_store_connect import get_or_create_persisted_build
 
 
 class TestUpdateDsyms:
@@ -40,83 +38,6 @@ class TestUpdateDsyms:
             build_number="20200220",
             uploaded_date=datetime.utcnow(),
         )
-
-    @pytest.mark.django_db
-    def test_update_build_refresh_date(self, default_project, config):
-        starting_option = default_project.get_option(
-            appconnect.APPSTORECONNECT_BUILD_REFRESHES_OPTION, default="{}"
-        )
-        assert starting_option == "{}"
-
-        update_build_refresh_date(default_project, config.id)
-
-        serialized = default_project.get_option(
-            appconnect.APPSTORECONNECT_BUILD_REFRESHES_OPTION, default="{}"
-        )
-        updated_option = json.loads(serialized)
-
-        # An explicit entry was created
-        assert len(updated_option) == 1
-        assert config.id in updated_option
-        # The entry has some sort of value
-        assert updated_option[config.id] is not None
-
-        new_date = parse_date(updated_option[config.id])
-        assert isinstance(new_date, datetime)
-
-    @pytest.mark.django_db
-    def test_update_existing_build_refresh_date(self, default_project, config):
-        old_build_refresh_date = datetime(2020, 2, 20, tzinfo=timezone.utc)
-
-        starting_option = default_project.update_option(
-            appconnect.APPSTORECONNECT_BUILD_REFRESHES_OPTION,
-            json.dumps({config.id: old_build_refresh_date}),
-        )
-        assert starting_option != "{}"
-
-        update_build_refresh_date(default_project, config.id)
-
-        serialized = default_project.get_option(
-            appconnect.APPSTORECONNECT_BUILD_REFRESHES_OPTION, default="{}"
-        )
-        updated_option = json.loads(serialized)
-
-        assert len(updated_option) == 1
-        assert updated_option[config.id] is not None
-
-        new_date = parse_date(updated_option[config.id])
-        assert isinstance(new_date, datetime)
-        assert new_date > old_build_refresh_date
-
-    @pytest.mark.django_db
-    def test_update_build_refresh_date_has_other_builds(self, default_project, config):
-        other_build_id = uuid.uuid4().hex
-        other_build_refresh_date = datetime(2020, 2, 20, tzinfo=timezone.utc)
-
-        starting_option = default_project.update_option(
-            appconnect.APPSTORECONNECT_BUILD_REFRESHES_OPTION,
-            json.dumps({other_build_id: other_build_refresh_date}),
-        )
-        assert starting_option != "{}"
-
-        update_build_refresh_date(default_project, config.id)
-
-        serialized = default_project.get_option(
-            appconnect.APPSTORECONNECT_BUILD_REFRESHES_OPTION, default="{}"
-        )
-        updated_option = json.loads(serialized)
-
-        # Existing entries are preserved
-        assert len(updated_option) == 2
-        assert parse_date(updated_option[other_build_id]) == other_build_refresh_date
-
-        # An explicit new entry was created
-        assert config.id in updated_option
-        # The entry has some sort of value
-        assert updated_option[config.id] is not None
-
-        new_date = parse_date(updated_option[config.id])
-        assert isinstance(new_date, datetime)
 
     @pytest.mark.django_db
     def create_new_persisted_build(self, default_project, config, build):
