9410469607c2791c2a44b86bf833b11a80e79d8b
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-07-27 17:48:42 -0700

    Add types to backends

--- src/sentry/digests/backends/dummy.py
+++ src/sentry/digests/backends/dummy.py
@@ -1,22 +1,36 @@
 from contextlib import contextmanager
+from typing import TYPE_CHECKING, Any, Iterable, Optional
 
+from sentry.digests import Record, ScheduleEntry
 from sentry.digests.backends.base import Backend
+
+if TYPE_CHECKING:
+    from sentry.models import Project
 
 
 class DummyBackend(Backend):
-    def add(self, key, record, increment_delay=None, maximum_delay=None):
+    def add(
+        self,
+        key: str,
+        record: Record,
+        increment_delay: Optional[int] = None,
+        maximum_delay: Optional[int] = None,
+        timestamp: Optional[float] = None,
+    ) -> bool:
         pass
 
-    def enabled(self, project):
+    def enabled(self, project: "Project") -> bool:
         return False
 
     @contextmanager
-    def digest(self, key, minimum_delay=None):
+    def digest(self, key: str, minimum_delay: Optional[int] = None) -> Any:
         yield []
 
-    def schedule(self, deadline):
-        return
-        yield  # make this a generator
+    def schedule(
+        self, deadline: float, timestamp: Optional[float] = None
+    ) -> Iterable[ScheduleEntry]:
+        return []
+        # yield  # TODO(mgaeta): make this a generator
 
-    def maintenance(self, deadline):
+    def maintenance(self, deadline: float, timestamp: Optional[float] = None) -> None:
         pass
