f14c7162c50a7f3c3312d6e64820d724549b2820
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2021-07-30 17:21:03 -0700

    feat(codeowners): Support multiple CODEOWNERS per project

--- static/app/views/settings/project/projectOwnership/index.tsx
+++ static/app/views/settings/project/projectOwnership/index.tsx
@@ -238,8 +238,6 @@ tags.sku_class:enterprise #enterprise`;
                   size="small"
                   priority="primary"
                   data-test-id="add-codeowner-button"
-                  // TODO(nisanthan): Remove disabled logic when implementing support for multiple CODEOWNERS
-                  disabled={codeowners && codeowners.length > 0}
                 >
                   {t('Add CODEOWNERS File')}
                 </CodeOwnerButton>
