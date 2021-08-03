f77340697c3ae82c32f6ba331476bed028bbe84d
josh <joshuarli98@gmail.com>
2021-08-03 17:15:26 +0000

    fix(django 2.1): preserve has_usable_password behavior (#27874)

--- src/sentry/models/user.py
+++ src/sentry/models/user.py
@@ -182,6 +182,15 @@ class User(BaseModel, AbstractBaseUser):
     def has_unverified_emails(self):
         return self.get_unverified_emails().exists()
 
+    def has_usable_password(self):
+        if self.password == "" or self.password is None:
+            # This is the behavior we've been relying on from Django 1.6 - 2.0.
+            # In 2.1, a "" or None password is considered usable.
+            # Removing this override requires identifying all the places
+            # to put set_unusable_password and a migration.
+            return False
+        return super().has_usable_password()
+
     def get_label(self):
         return self.email or self.username or self.id
 
