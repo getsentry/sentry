ec46517245a1a261691a1c3c1f374ba8d553ce2b
Zhixing Zhang <me@neoto.xin>
2021-08-09 16:29:27 -0700

    changes

--- src/sentry/api/endpoints/organization_sentry_function.py
+++ src/sentry/api/endpoints/organization_sentry_function.py
@@ -1,3 +1,4 @@
+import time
 from io import BytesIO
 from uuid import uuid4
 from zipfile import ZipFile
@@ -18,7 +19,18 @@ from rest_framework.response import Response
 
 from sentry.api.bases import OrganizationEndpoint
 
-
+indexJs = """
+const userFunc = require('./user.js');
+exports.start = (req, res) => {
+  if (!userFunc) {
+    res.status(500).send("Your code needs to export a function. module.export = () => {}");
+  }
+  if (typeof userFunc !== 'function') {
+    res.status(500).send("Your code needs to export a function. Instead, " + typeof userFunc + " was exported.");
+  }
+  userFunc();
+};
+"""
 class OrganizationSentryFunctionEndpoint(OrganizationEndpoint):
     def post(self, request, organization):
         funcBody = request.data.get("fn")
@@ -29,12 +41,14 @@ class OrganizationSentryFunctionEndpoint(OrganizationEndpoint):
 
         f = BytesIO()
         with ZipFile(f, "w") as codezip:
-            codezip.writestr("fn.json", funcBody)
+            codezip.writestr("user.js", funcBody)
+            codezip.writestr("index.js", indexJs)
 
         storage_client = storage.Client()
         bucket = storage_client.bucket("hackweek-sentry-functions-bucket")
         blob = bucket.blob(zipFilename)
         blob.upload_from_file(f, rewind=True, content_type="application/zip")
+        time.sleep(1)
 
         client = CloudFunctionsServiceClient()
         fn = CloudFunction(
@@ -42,6 +56,7 @@ class OrganizationSentryFunctionEndpoint(OrganizationEndpoint):
             description="created by api",
             source_archive_url=f"gs://hackweek-sentry-functions-bucket/" + zipFilename,
             runtime="nodejs14",
+            entry_point="start",
             https_trigger=HttpsTrigger(url=""),
         )
         client.create_function(
