e650e90c679932c38534da69f5b0fcb4d4bea813
Zhixing Zhang <me@neoto.xin>
2021-08-09 16:05:22 -0700

    changes

--- src/sentry/api/endpoints/organization_sentry_function.py
+++ src/sentry/api/endpoints/organization_sentry_function.py
@@ -1,8 +1,19 @@
+from io import BytesIO
+from uuid import uuid4
+from zipfile import ZipFile
+
+from google.cloud import storage
 from google.cloud.functions_v1.services.cloud_functions_service import CloudFunctionsServiceClient
 from google.cloud.functions_v1.services.cloud_functions_service.transports.base import (
     CloudFunctionsServiceTransport,
 )
-from google.cloud.functions_v1.types import CloudFunction, CreateFunctionRequest, HttpsTrigger
+from google.cloud.functions_v1.types import (
+    CloudFunction,
+    CreateFunctionRequest,
+    EventTrigger,
+    HttpsTrigger,
+    ListFunctionsRequest,
+)
 from rest_framework.response import Response
 
 from sentry.api.bases import OrganizationEndpoint
@@ -10,16 +21,32 @@ from sentry.api.bases import OrganizationEndpoint
 
 class OrganizationSentryFunctionEndpoint(OrganizationEndpoint):
     def post(self, request, organization):
+        funcBody = request.data.get("fn")
+        if not funcBody:
+            return Response(status=400)
+        funcId = uuid4().hex
+        zipFilename = funcId + ".zip"
+
+        f = BytesIO()
+        with ZipFile(f, "w") as codezip:
+            codezip.writestr("fn.json", funcBody)
+
+        storage_client = storage.Client()
+        bucket = storage_client.bucket("hackweek-sentry-functions-bucket")
+        blob = bucket.blob(zipFilename)
+        blob.upload_from_file(f, rewind=True, content_type="application/zip")
+
         client = CloudFunctionsServiceClient()
         fn = CloudFunction(
-            name="projects/hackweek-sentry-functions/locations/us-central1/functions/testfunc",
+            name="projects/hackweek-sentry-functions/locations/us-central1/functions/" + funcId,
             description="created by api",
-            source_archive_url="gs://hackweek-sentry-functions-bucket/test-func.zip",
-            runtime="nodejs14"
+            source_archive_url=f"gs://hackweek-sentry-functions-bucket/" + zipFilename,
+            runtime="nodejs14",
+            https_trigger=HttpsTrigger(url=""),
         )
-        funcRequest = CreateFunctionRequest(
+        client.create_function(
             function=fn,
             location="projects/hackweek-sentry-functions/locations/us-central1",
         )
-        client.create_function(funcRequest)
+
         return Response(status=201)
