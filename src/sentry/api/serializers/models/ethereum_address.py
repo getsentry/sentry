c18db26d1997ee9081306387e793a16569c9875e
Anton Ovchinnikov <anton@tonyo.info>
2021-08-10 16:14:40 +0200

    Allow blank description/abi

--- src/sentry/api/serializers/models/ethereum_address.py
+++ src/sentry/api/serializers/models/ethereum_address.py
@@ -4,9 +4,9 @@ from sentry.models import EthereumAddress
 
 
 class EthereumAddressSerializer(serializers.Serializer):
-    abiContents = serializers.CharField(required=False, default="")
+    abiContents = serializers.CharField(required=False, allow_blank=True, default="")
     address = serializers.CharField(max_length=40)
-    displayName = serializers.CharField(required=False, default="")
+    displayName = serializers.CharField(required=False, allow_blank=True, default="")
 
     class Meta:
         model = EthereumAddress
