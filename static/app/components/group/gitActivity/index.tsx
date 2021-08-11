1de9105d5a187ef82a7ef19f7a32ea508d0dbaf6
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-08-11 18:09:29 -0400

    hackweek: Add polling for git helpers
    Live updates, heck yeah. All the polling code borrowed
    from sidebar/broadcast.tsx

--- static/app/components/group/gitActivity/index.tsx
+++ static/app/components/group/gitActivity/index.tsx
@@ -1,11 +1,10 @@
-import {Fragment, useEffect, useState} from 'react';
+import {Component, Fragment} from 'react';
 import styled from '@emotion/styled';
 
 import {addErrorMessage, addSuccessMessage} from 'app/actionCreators/indicator';
 import {Client} from 'app/api';
 import Alert from 'app/components/alert';
 import Clipboard from 'app/components/clipboard';
-import LoadingIndicator from 'app/components/loadingIndicator';
 import TextOverflow from 'app/components/textOverflow';
 import Tooltip from 'app/components/tooltip';
 import {IconCopy, IconRefresh} from 'app/icons';
@@ -16,6 +15,8 @@ import SidebarSection from '../sidebarSection';
 
 import Activity from './activity';
 import UnlinkedActivity from './unlinked';
+
+const POLLER_DELAY = 2000;
 
 // https://docs.github.com/en/rest/reference/pulls
 export type GitActivity = {
@@ -34,52 +35,98 @@ type Props = {
   issueId: string;
 };
 
-function GitActivity({api, issueId}: Props) {
-  const [linkedActivities, setLinkedActivities] = useState<GitActivity[]>([]);
-  const [unlinkedActivities, setUnlinkedActivities] = useState<GitActivity[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState<undefined | string>(undefined);
-  const [branchName, setBranchName] = useState('');
+type State = {
+  linkedActivities: GitActivity[];
+  unlinkedActivities: GitActivity[];
+  error: undefined | string;
+  branchName: string;
+};
 
-  useEffect(() => {
-    fetchBranchName();
-    fetchActivities();
-  }, []);
+class GitManager extends Component<Props, State> {
+  state: State = {
+    linkedActivities: [],
+    unlinkedActivities: [],
+    branchName: '',
+    error: undefined,
+  };
 
-  async function fetchBranchName(reload = true) {
-    if (reload) {
-      setIsLoading(true);
+  componentDidMount() {
+    this.fetchBranchName();
+    this.fetchActivities();
+
+    document.addEventListener('visibilitychange', this.handleVisibilityChange);
+  }
+
+  componentWillUnmount() {
+    if (this.timer) {
+      window.clearTimeout(this.timer);
+      this.timer = null;
     }
 
-    try {
-      const response = await api.requestPromise(`/issues/${issueId}/branch-name/`);
-      setBranchName(response.branchName);
-      setIsLoading(false);
-    } catch {
-      setIsLoading(false);
-      setError(t('An error occurred while fetching the branch name'));
+    if (this.poller) {
+      this.stopPoll();
+    }
+    document.removeEventListener('visibilitychange', this.handleVisibilityChange);
+  }
+
+  poller: number | null = null;
+  timer: number | null = null;
+
+  startPoll() {
+    this.poller = window.setTimeout(this.fetchActivities, POLLER_DELAY);
+  }
+
+  stopPoll() {
+    if (this.poller) {
+      window.clearTimeout(this.poller);
+      this.poller = null;
     }
   }
 
-  async function fetchActivities() {
-    setIsLoading(true);
-    setError(undefined);
+  fetchBranchName = async () => {
+    const {api, issueId} = this.props;
+    try {
+      const response = await api.requestPromise(`/issues/${issueId}/branch-name/`);
+      this.setState({
+        branchName: response.branchName,
+      });
+    } catch {
+      this.setState({
+        error: t('An error occurred while fetching the branch name'),
+      });
+    }
+  };
+
+  fetchActivities = async () => {
+    if (this.poller) {
+      this.stopPoll();
+    }
+
+    const {api, issueId} = this.props;
     try {
       const response: GitActivity[] = await api.requestPromise(
         `/issues/${issueId}/github-activity/`
       );
       const activities = response.filter(gitActivity => gitActivity.visible);
-      setLinkedActivities(activities);
       const unlinked = response.filter(gitActivity => !gitActivity.visible);
-      setUnlinkedActivities(unlinked);
-      setIsLoading(false);
+      this.setState({
+        linkedActivities: activities,
+        unlinkedActivities: unlinked,
+      });
     } catch {
-      setIsLoading(false);
-      setError(t('An error occurred while fetching Git Activity'));
+      this.setState({
+        error: t('An error occurred while fetching the Git Activity'),
+      });
     }
-  }
 
-  async function handleUnlinkPullRequest(gitActivity: GitActivity) {
+    this.startPoll();
+  };
+
+  handleVisibilityChange = () => (document.hidden ? this.stopPoll() : this.startPoll());
+
+  handleUnlinkPullRequest = async (gitActivity: GitActivity) => {
+    const {api, issueId} = this.props;
+    const {linkedActivities, unlinkedActivities} = this.state;
     try {
       api.requestPromise(`/issues/${issueId}/github-activity/${gitActivity.id}/`, {
         method: 'PUT',
@@ -88,87 +135,92 @@ function GitActivity({api, issueId}: Props) {
       const newActivities = linkedActivities.filter(
         activity => activity.id !== gitActivity.id
       );
-      setLinkedActivities(newActivities);
       unlinkedActivities.push(gitActivity);
-      setUnlinkedActivities(unlinkedActivities);
+      this.setState({
+        linkedActivities: newActivities,
+        unlinkedActivities,
+      });
       addSuccessMessage(t('Pull Request was successfully unlinked'));
     } catch {
       addErrorMessage(t('An error occurred while unlinkig the Pull Request'));
     }
-  }
+  };
 
-  async function handleRelinkPullRequest(gitActivity: GitActivity) {
+  handleRelinkPullRequest = async (gitActivity: GitActivity) => {
+    const {api, issueId} = this.props;
+    const {linkedActivities, unlinkedActivities} = this.state;
     try {
       api.requestPromise(`/issues/${issueId}/github-activity/${gitActivity.id}/`, {
         method: 'PUT',
         data: {visible: 1},
       });
       linkedActivities.push(gitActivity);
-      setLinkedActivities(linkedActivities);
-
       const newUnlinkedActivities = unlinkedActivities.filter(
         activity => activity.id !== gitActivity.id
       );
-      setUnlinkedActivities(newUnlinkedActivities);
+      this.setState({
+        linkedActivities,
+        unlinkedActivities: newUnlinkedActivities,
+      });
       addSuccessMessage(t('Pull Request was successfully linked'));
     } catch {
       addErrorMessage(t('An error occurred while linkig the Pull Request'));
     }
-  }
+  };
 
-  function renderContent() {
+  render() {
+    const {error, branchName, linkedActivities, unlinkedActivities} = this.state;
     if (error) {
-      return <Alert type="error">{error}</Alert>;
+      return (
+        <SidebarSection title={t('Git Activity')}>
+          <Alert type="error">{error}</Alert>
+        </SidebarSection>
+      );
     }
-
-    if (isLoading) {
-      return <LoadingIndicator mini />;
-    }
-
     return (
-      <Fragment>
-        <IssueId>
-          {t('Branch Name')}
-          <BranchNameAndActions>
-            <StyledTooltip title={branchName}>
-              <BranchName>{branchName}</BranchName>
-            </StyledTooltip>
-            <Clipboard value={branchName}>
-              <Tooltip title={t('Copy branch name')} containerDisplayMode="inline-flex">
-                <StyledIconCopy />
+      <SidebarSection title={t('Git Activity')}>
+        <Fragment>
+          <IssueId>
+            {t('Branch Name')}
+            <BranchNameAndActions>
+              <StyledTooltip title={branchName}>
+                <BranchName>{branchName}</BranchName>
+              </StyledTooltip>
+              <Clipboard value={branchName}>
+                <Tooltip title={t('Copy branch name')} containerDisplayMode="inline-flex">
+                  <StyledIconCopy />
+                </Tooltip>
+              </Clipboard>
+              <Tooltip
+                title={t('Refresh to get a new branch name')}
+                containerDisplayMode="inline-flex"
+              >
+                <StyledIconRefresh onClick={() => this.fetchBranchName()} />
               </Tooltip>
-            </Clipboard>
-            <Tooltip
-              title={t('Refresh to get a new branch name')}
-              containerDisplayMode="inline-flex"
-            >
-              <StyledIconRefresh onClick={() => fetchBranchName(false)} />
-            </Tooltip>
-          </BranchNameAndActions>
-        </IssueId>
-        <Activities>
-          {linkedActivities.map(activity => (
-            <Activity
-              key={activity.id}
-              gitActivity={activity}
-              onUnlink={handleUnlinkPullRequest}
+            </BranchNameAndActions>
+          </IssueId>
+          <Activities>
+            {linkedActivities.map(activity => (
+              <Activity
+                key={activity.id}
+                gitActivity={activity}
+                onUnlink={this.handleUnlinkPullRequest}
+              />
+            ))}
+          </Activities>
+          {unlinkedActivities.length > 0 && (
+            <UnlinkedActivity
+              unlinkedActivities={unlinkedActivities}
+              onRelink={this.handleRelinkPullRequest}
             />
-          ))}
-        </Activities>
-        {unlinkedActivities.length > 0 && (
-          <UnlinkedActivity
-            unlinkedActivities={unlinkedActivities}
-            onRelink={handleRelinkPullRequest}
-          />
-        )}
-      </Fragment>
+          )}
+        </Fragment>
+      </SidebarSection>
     );
   }
-
-  return <SidebarSection title={t('Git Activity')}>{renderContent()}</SidebarSection>;
 }
 
-export default GitActivity;
+export default GitManager;
 
 const IssueId = styled('div')`
   font-size: ${p => p.theme.fontSizeSmall};
