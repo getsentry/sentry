2cfbc6aa1e986812e37bc21fda98bf456fd27804
Zhixing Zhang <me@neoto.xin>
2021-08-12 13:26:26 -0700

    integration

--- src/sentry/utils/cloudfunctions.py
+++ src/sentry/utils/cloudfunctions.py
@@ -25,6 +25,9 @@ exports.start = async (message, context) => {
   if (process.env.SENTRY_DSN) {
     Sentry.init({
       dsn: process.env.SENTRY_DSN,
+      integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+      ],
       tracesSampleRate: 1.0,
     });
   }
