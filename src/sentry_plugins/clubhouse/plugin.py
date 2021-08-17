3dfe0fcd9d000c771b60793fde40c07d7b1e5cbb
josh <joshuarli98@gmail.com>
2021-08-17 21:13:10 +0000

    ref(hackweek): upgrade pyupgrade and friends (#28059)

--- src/sentry_plugins/clubhouse/plugin.py
+++ src/sentry_plugins/clubhouse/plugin.py
@@ -146,7 +146,7 @@ class ClubhousePlugin(CorePluginMixin, IssuePlugin2):
         # TODO: Something about the search API won't allow an explicit number search.
         # Should it switch the search mechanism from search_stories(text) to get_story(id)?
         try:
-            response = client.search_stories(query=(f"project:{project} {query}").encode("utf-8"))
+            response = client.search_stories(query=(f"project:{project} {query}").encode())
         except Exception as e:
             return self.handle_api_error(e)
 
