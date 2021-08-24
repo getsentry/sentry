eb600be3d7033e6295bca7f3e3bfa67a158203bb
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-08-24 17:03:36 -0400

    Set atomic to dalse for the data migration

--- src/sentry/migrations/0227_backfill_visits.py
+++ src/sentry/migrations/0227_backfill_visits.py
@@ -58,7 +58,7 @@ class Migration(migrations.Migration):
     # You'll also usually want to set this to `False` if you're writing a data
     # migration, since we don't want the entire migration to run in one long-running
     # transaction.
-    atomic = True
+    atomic = False
 
     dependencies = [
         ("sentry", "0226_add_visits"),
