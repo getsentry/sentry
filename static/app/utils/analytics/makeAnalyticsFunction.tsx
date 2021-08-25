bf57d2a5eb7724181209c30e3f26d3e3ec8dcc21
Stephen Cefali <scefali@sentry.io>
2021-08-25 11:30:28 +0200

    handle localStorage null (#28167)
    Certain browsers may not allow the usage of localStorage so we need to check if it's null

--- static/app/utils/analytics/makeAnalyticsFunction.tsx
+++ static/app/utils/analytics/makeAnalyticsFunction.tsx
@@ -2,7 +2,7 @@ import {LightWeightOrganization} from 'app/types';
 import {Hooks} from 'app/types/hooks';
 import {trackAnalyticsEventV2} from 'app/utils/analytics';
 
-const hasAnalyticsDebug = () => window.localStorage.getItem('DEBUG_ANALYTICS') === '1';
+const hasAnalyticsDebug = () => window.localStorage?.getItem('DEBUG_ANALYTICS') === '1';
 
 type OptionalOrg = {organization: LightWeightOrganization | null};
 type Options = Parameters<Hooks['analytics:track-event-v2']>[1];
