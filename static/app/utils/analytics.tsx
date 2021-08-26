601ddbee40497939516d27c7fda795c6d67c5332
Stephen Cefali <scefali@sentry.io>
2021-08-26 12:12:11 +0200

    docs(analytics): update comments for analytics (#28181)
    Added comments regarding analytics hooks and using makeAnalyticsFunction

--- static/app/utils/analytics.tsx
+++ static/app/utils/analytics.tsx
@@ -27,18 +27,21 @@ import {Hooks} from 'app/types/hooks';
  * which includes Reload, Amplitude, and Google Analytics.
  * All events go to Reload. If eventName is defined, events also go to Amplitude.
  * For more details, refer to the API defined in hooks.
+ *
+ * Shold NOT be used directly.
+ * Instead, use makeAnalyticsFunction to generate an analytics function.
  */
 export const trackAnalyticsEventV2: Hooks['analytics:track-event-v2'] = (data, options) =>
   HookStore.get('analytics:track-event-v2').forEach(cb => cb(data, options));
 
 /**
- * @deprecated Prefer `trackAnalyticsEventV2`
+ * @deprecated Use a method generated from makeAnalyticsFunction
  */
 export const trackAnalyticsEvent: Hooks['analytics:track-event'] = options =>
   HookStore.get('analytics:track-event').forEach(cb => cb(options));
 
 /**
- * @deprecated Prefer `trackAnalyticsEventV2`
+ * @deprecated Use a method generated from makeAnalyticsFunction
  */
 export const trackAdhocEvent: Hooks['analytics:track-adhoc-event'] = options =>
   HookStore.get('analytics:track-adhoc-event').forEach(cb => cb(options));
