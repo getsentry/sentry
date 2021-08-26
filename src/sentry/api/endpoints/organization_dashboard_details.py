25186ca3faef2e9798e396a3f0fbe787ca3c2af8
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-08-26 09:23:25 -0400

    feat(discover): Add Discover/Dashboard Visits endpoint
    Add an endpoint to record each visit to a Dashboard
    or Discover Query

--- src/sentry/api/endpoints/organization_dashboard_details.py
+++ src/sentry/api/endpoints/organization_dashboard_details.py
@@ -1,4 +1,6 @@
 from django.db import IntegrityError, transaction
+from django.db.models import F
+from django.utils import timezone
 from rest_framework.response import Response
 
 from sentry import features
@@ -126,3 +128,39 @@ class OrganizationDashboardDetailsEndpoint(OrganizationEndpoint):
             return self.respond({"Dashboard with that title already exists."}, status=409)
 
         return self.respond(serialize(serializer.instance, request.user), status=200)
+
+
+class OrganizationDashboardVisitEndpoint(OrganizationEndpoint):
+    permission_classes = (OrganizationDashboardsPermission,)
+
+    def convert_args(self, request, organization_slug, dashboard_id, *args, **kwargs):
+        args, kwargs = super().convert_args(request, organization_slug)
+
+        try:
+            kwargs["dashboard"] = self._get_dashboard(request, kwargs["organization"], dashboard_id)
+        except (Dashboard.DoesNotExist, ValueError):
+            raise ResourceDoesNotExist
+
+        return (args, kwargs)
+
+    def _get_dashboard(self, request, organization, dashboard_id):
+        prebuilt = Dashboard.get_prebuilt(dashboard_id)
+        if prebuilt:
+            return prebuilt
+        return Dashboard.objects.get(id=dashboard_id, organization_id=organization.id)
+
+    def post(self, request, organization, dashboard):
+        """
+        Update last_visited and increment visits counter
+        """
+        if not features.has(EDIT_FEATURE, organization, actor=request.user):
+            return Response(status=404)
+
+        if isinstance(dashboard, dict):
+            return Response(status=204)
+
+        dashboard.visits = F("visits") + 1
+        dashboard.last_visited = timezone.now()
+        dashboard.save(update_fields=["visits", "last_visited"])
+
+        return Response(status=204)
--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -14,7 +14,10 @@ from sentry.discover.endpoints.discover_key_transactions import (
 )
 from sentry.discover.endpoints.discover_query import DiscoverQueryEndpoint
 from sentry.discover.endpoints.discover_saved_queries import DiscoverSavedQueriesEndpoint
-from sentry.discover.endpoints.discover_saved_query_detail import DiscoverSavedQueryDetailEndpoint
+from sentry.discover.endpoints.discover_saved_query_detail import (
+    DiscoverSavedQueryDetailEndpoint,
+    DiscoverSavedQueryVisitEndpoint,
+)
 from sentry.incidents.endpoints.organization_alert_rule_available_action_index import (
     OrganizationAlertRuleAvailableActionIndexEndpoint,
 )
@@ -149,7 +152,10 @@ from .endpoints.organization_code_mapping_details import OrganizationCodeMapping
 from .endpoints.organization_code_mappings import OrganizationCodeMappingsEndpoint
 from .endpoints.organization_config_integrations import OrganizationConfigIntegrationsEndpoint
 from .endpoints.organization_config_repositories import OrganizationConfigRepositoriesEndpoint
-from .endpoints.organization_dashboard_details import OrganizationDashboardDetailsEndpoint
+from .endpoints.organization_dashboard_details import (
+    OrganizationDashboardDetailsEndpoint,
+    OrganizationDashboardVisitEndpoint,
+)
 from .endpoints.organization_dashboard_widget_details import (
     OrganizationDashboardWidgetDetailsEndpoint,
 )
@@ -815,6 +821,11 @@ urlpatterns = [
                     name="sentry-api-0-discover-saved-query-detail",
                 ),
                 url(
+                    r"^(?P<organization_slug>[^\/]+)/discover/saved/(?P<query_id>\d+)/visit/$",
+                    DiscoverSavedQueryVisitEndpoint.as_view(),
+                    name="sentry-api-0-discover-saved-query-visit",
+                ),
+                url(
                     r"^(?P<organization_slug>[^\/]+)/key-transactions/$",
                     KeyTransactionEndpoint.as_view(),
                     name="sentry-api-0-organization-key-transactions",
@@ -859,6 +870,11 @@ urlpatterns = [
                     r"^(?P<organization_slug>[^\/]+)/dashboards/(?P<dashboard_id>[^\/]+)/$",
                     OrganizationDashboardDetailsEndpoint.as_view(),
                     name="sentry-api-0-organization-dashboard-details",
+                ),
+                url(
+                    r"^(?P<organization_slug>[^\/]+)/dashboards/(?P<dashboard_id>[^\/]+)/visit/$",
+                    OrganizationDashboardVisitEndpoint.as_view(),
+                    name="sentry-api-0-organization-dashboard-visit",
                 ),
                 url(
                     r"^(?P<organization_slug>[^\/]+)/shortids/(?P<short_id>[^\/]+)/$",
