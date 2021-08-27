9066a99c3fc29ceae29e60d0e2cf2cde6c4355d8
Shruthi <shruthilaya.jaganathan@sentry.io>
2021-08-27 10:05:15 -0400

    chore(performance): Remove new badge from txn threshold (#28240)
    Remove new badge since this feature has been in GA for
    a month now.

--- static/app/views/settings/project/navigationConfiguration.tsx
+++ static/app/views/settings/project/navigationConfiguration.tsx
@@ -116,8 +116,7 @@ export default function getConfiguration({
         {
           path: `${pathPrefix}/performance/`,
           title: t('Performance'),
-          badge: () => 'new',
-          show: () => !!organization?.features?.includes('project-transaction-threshold'),
+          show: () => !!organization?.features?.includes('performance-view'),
         },
       ],
     },
