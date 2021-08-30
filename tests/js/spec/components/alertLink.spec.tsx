5d5a925cdb644c3d436d68ddadf67d92b0f2daf7
Scott Cooper <scttcper@gmail.com>
2021-08-30 10:38:38 -0700

    ref(ui): Convert alertLink component to function, use RTL (#28258)

--- tests/js/spec/components/alertLink.spec.tsx
+++ tests/js/spec/components/alertLink.spec.tsx
@@ -1,24 +1,24 @@
-import {mountWithTheme} from 'sentry-test/enzyme';
+import {mountWithTheme} from 'sentry-test/reactTestingLibrary';
 
 import AlertLink from 'app/components/alertLink';
 import {IconMail} from 'app/icons';
 
 describe('AlertLink', function () {
   it('renders', function () {
-    const wrapper = mountWithTheme(
+    const {container} = mountWithTheme(
       <AlertLink to="/settings/accounts/notifications">
         This is an external link button
       </AlertLink>
     );
-    expect(wrapper).toSnapshot();
+    expect(container).toSnapshot();
   });
 
   it('renders with icon', function () {
-    const wrapper = mountWithTheme(
+    const {container} = mountWithTheme(
       <AlertLink to="/settings/accounts/notifications" icon={<IconMail />}>
         This is an external link button
       </AlertLink>
     );
-    expect(wrapper).toSnapshot();
+    expect(container).toSnapshot();
   });
 });
