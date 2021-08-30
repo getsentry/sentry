7d32b7d0fe571eb94825f79e28295896c4b28821
Leander Rodrigues <leandergrodrigues@gmail.com>
2021-08-30 16:43:26 -0400

    feat(codeowners): Add strict naming to user/team mappings for GH/GL (#28269)

--- src/sentry/api/validators/external_actor.py
+++ src/sentry/api/validators/external_actor.py
@@ -1,15 +1,33 @@
-from typing import Optional
+import re
+from typing import Optional, Set
 
 from rest_framework import serializers
 
+from sentry.api.exceptions import ParameterValidationError
+from sentry.api.validators.integrations import validate_provider
 from sentry.models import Organization, OrganizationIntegration
+from sentry.types.integrations import ExternalProviders
 
 EXTERNAL_ID_LENGTH_MIN = 1
 EXTERNAL_ID_LENGTH_MAX = 64
 
+EXTERNAL_NAME_LENGTH_MIN = 1
+EXTERNAL_NAME_LENGTH_MAX = 255
+EXTERNAL_NAME_REGEX = re.compile(r"^@[\w/\.-]+$")
+
 
 def _out_of_range_string(param: str, minimum: int, maximum: int, actual: int) -> str:
     return f"{param} has invalid length: {actual}. Length must be between {minimum} and {maximum}"
+
+
+def is_valid_provider(
+    provider: str, available_providers: Optional[Set[ExternalProviders]] = None
+) -> bool:
+    try:
+        validate_provider(provider, available_providers)
+    except ParameterValidationError:
+        return False
+    return True
 
 
 def validate_external_id_option(external_id: Optional[str]) -> Optional[str]:
@@ -27,6 +45,25 @@ def validate_external_id_option(external_id: Optional[str]) -> Optional[str]:
         )
 
     return external_id
+
+
+def validate_external_name(external_name: str) -> str:
+    if not (EXTERNAL_NAME_LENGTH_MIN <= len(external_name) <= EXTERNAL_NAME_LENGTH_MAX):
+        raise serializers.ValidationError(
+            _out_of_range_string(
+                "External Name",
+                EXTERNAL_NAME_LENGTH_MIN,
+                EXTERNAL_NAME_LENGTH_MAX,
+                len(external_name),
+            )
+        )
+
+    if EXTERNAL_NAME_REGEX.match(external_name) is None:
+        raise serializers.ValidationError(
+            "External Name must start with '@' and can't contain special characters or spaces."
+        )
+
+    return external_name
 
 
 def validate_integration_id(integration_id: str, organization: Organization) -> str:
