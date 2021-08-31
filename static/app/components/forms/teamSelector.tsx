2cda986767f17083e4bcc7586f91a78efcd6ebd1
David Wang <dwang@sentry.io>
2021-08-31 11:49:03 -0700

    new selector

---
+++ static/app/components/forms/teamSelector.tsx
@@ -0,0 +1,265 @@
+import * as React from 'react';
+import styled from '@emotion/styled';
+
+import {addTeamToProject} from 'app/actionCreators/projects';
+import {Client} from 'app/api';
+import Button from 'app/components/button';
+import SelectControl, {ControlProps} from 'app/components/forms/selectControl';
+import {IconAdd, IconUser} from 'app/icons';
+import {t} from 'app/locale';
+import space from 'app/styles/space';
+import {Organization, Project, Team} from 'app/types';
+import withApi from 'app/utils/withApi';
+import withOrganization from 'app/utils/withOrganization';
+import withTeams from 'app/utils/withTeams';
+
+import IdBadge from '../idBadge';
+import Tooltip from '../tooltip';
+
+const UnassignedWrapper = styled('div')`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledIconUser = styled(IconUser)`
+  margin-left: ${space(0.25)};
+  margin-right: ${space(1)};
+  color: ${p => p.theme.gray400};
+`;
+
+// An option to be unassigned on the team dropdown
+const unassignedOption = {
+  value: null,
+  label: (
+    <UnassignedWrapper>
+      <StyledIconUser size="20px" />
+      {t('Unassigned')}
+    </UnassignedWrapper>
+  ),
+  searchKey: 'unassigned',
+  actor: null,
+  disabled: false,
+};
+
+// Ensures that the svg icon is white when selected
+const unassignedSelectStyles = {
+  option: (provided, state: any) => ({
+    ...provided,
+    svg: {
+      color: state.isSelected && state.theme.white,
+    },
+  }),
+};
+
+type Props = {
+  api: Client;
+  organization: Organization;
+  teams: Team[];
+  onChange: (value: any) => any;
+  // Function to control whether a team should be shown in the dropdown
+  teamFilter?: (team: Team) => boolean;
+  // Include only the provided Project's teams and allow user to add teams to projects on the fly
+  project?: Project;
+  // Controls whether the value in the dropdown is an id instead of slug
+  useId?: boolean;
+  includeUnassigned?: boolean;
+} & ControlProps;
+
+type TeamActor = {
+  type: 'team';
+  id: string;
+  name: string;
+};
+
+type SelectableTeam = {
+  value: string;
+  label: React.ReactElement;
+  searchKey: string;
+  actor: TeamActor;
+  disabled?: boolean;
+};
+
+type UnselectableTeam = SelectableTeam & {
+  disabled: true;
+};
+
+type TeamOption = SelectableTeam | UnselectableTeam | typeof unassignedOption;
+
+type State = {
+  options: TeamOption[];
+};
+
+class TeamSelector extends React.Component<Props, State> {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      options: this.teamOptions,
+    };
+  }
+
+  // TODO(ts) This type could be improved when react-select types are better.
+  selectRef = React.createRef<any>();
+
+  get teamOptions() {
+    const {teams, teamFilter, includeUnassigned, project} = this.props;
+    const filteredTeams = teamFilter ? teams.filter(teamFilter) : teams;
+    if (project) {
+      const teamsInProjectIdSet = new Set(project.teams.map(({id}) => id));
+      const teamsInProject = filteredTeams.filter(({id}) => teamsInProjectIdSet.has(id));
+      const teamsNotInProject = filteredTeams.filter(
+        ({id}) => !teamsInProjectIdSet.has(id)
+      );
+
+      return [
+        ...teamsInProject.map(this.createTeamOption),
+        ...teamsNotInProject.map(this.createTeamOutsideProjectOption),
+        ...(includeUnassigned ? [unassignedOption] : []),
+      ];
+    }
+    const teamOptions: TeamOption[] = filteredTeams.map(this.createTeamOption);
+    if (includeUnassigned) {
+      teamOptions.push(unassignedOption);
+    }
+    return teamOptions;
+  }
+
+  createTeamOption = (team: Team): SelectableTeam => ({
+    value: this.props.useId ? team.id : team.slug,
+    label: <IdBadge team={team} />,
+    searchKey: `#${team.slug}`,
+    actor: {
+      type: 'team',
+      id: team.id,
+      name: team.slug,
+    },
+  });
+
+  /**
+   * Closes the select menu by blurring input if possible since that seems to be the only
+   * way to close it.
+   */
+  closeSelectMenu() {
+    if (!this.selectRef.current) {
+      return;
+    }
+
+    // @ts-ignore The types for react-select don't cover this property
+    // or the type of selectRef is incorrect.
+    const select = this.selectRef.current.select;
+    const input: HTMLInputElement = select.inputRef;
+    if (input) {
+      // I don't think there's another way to close `react-select`
+      input.blur();
+    }
+  }
+
+  handleAddTeamToProject = async (team: Team) => {
+    const {api, organization, project, value} = this.props;
+    const {options} = this.state;
+
+    // Copy old value
+    const oldValue = value ? [...value] : {value};
+
+    // Optimistic update
+    this.props.onChange?.(this.createTeamOption(team));
+
+    try {
+      // Try to add team to project
+      if (project) {
+        await addTeamToProject(api, organization.slug, project.slug, team);
+
+        // Remove add to project button without changing order
+        const newOptions = options!.map(option => {
+          if (option.actor?.id === team.id) {
+            option.disabled = false;
+            option.label = <IdBadge team={team} />;
+          }
+
+          return option;
+        });
+        this.setState({options: newOptions});
+      }
+    } catch (err) {
+      // Unable to add team to project, revert select menu value
+      this.props.onChange?.(oldValue);
+    }
+    this.closeSelectMenu();
+  };
+
+  createTeamOutsideProjectOption = (team: Team): UnselectableTeam => {
+    const {organization} = this.props;
+    const canAddTeam = organization.access.includes('project:write');
+
+    return {
+      ...this.createTeamOption(team),
+      disabled: true,
+      label: (
+        <TeamOutsideProject>
+          <DisabledLabel>
+            <Tooltip
+              position="left"
+              title={t('%s is not a member of project', `#${team.slug}`)}
+            >
+              <IdBadge team={team} />
+            </Tooltip>
+          </DisabledLabel>
+          <Tooltip
+            title={
+              canAddTeam
+                ? t('Add %s to project', `#${team.slug}`)
+                : t('You do not have permission to add team to project.')
+            }
+          >
+            <AddToProjectButton
+              type="button"
+              size="zero"
+              borderless
+              disabled={!canAddTeam}
+              onClick={() => this.handleAddTeamToProject(team)}
+              icon={<IconAdd isCircled />}
+            />
+          </Tooltip>
+        </TeamOutsideProject>
+      ),
+    };
+  };
+
+  render() {
+    const {includeUnassigned, styles, ...props} = this.props;
+    const {options} = this.state;
+    console.log(options);
+    return (
+      <SelectControl
+        ref={this.selectRef}
+        options={options}
+        isOptionDisabled={option => option.disabled}
+        styles={{
+          styles,
+          ...(includeUnassigned ? unassignedSelectStyles : {}),
+        }}
+        {...props}
+      />
+    );
+  }
+}
+
+const TeamOutsideProject = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+`;
+
+const DisabledLabel = styled('div')`
+  display: flex;
+  opacity: 0.5;
+  overflow: hidden; /* Needed so that "Add to team" button can fit */
+`;
+
+const AddToProjectButton = styled(Button)`
+  flex-shrink: 0;
+`;
+
+export {TeamSelector};
+
+export default withApi(withTeams(withOrganization(TeamSelector)));
--- static/app/components/selectMembers/index.tsx
+++ static/app/components/selectMembers/index.tsx
@@ -2,74 +2,38 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 import debounce from 'lodash/debounce';
 
-import {addTeamToProject} from 'app/actionCreators/projects';
 import {Client} from 'app/api';
-import Button from 'app/components/button';
 import SelectControl from 'app/components/forms/selectControl';
 import IdBadge from 'app/components/idBadge';
 import Tooltip from 'app/components/tooltip';
-import {IconAdd, IconUser} from 'app/icons';
 import {t} from 'app/locale';
 import MemberListStore from 'app/stores/memberListStore';
-import ProjectsStore from 'app/stores/projectsStore';
-import TeamStore from 'app/stores/teamStore';
-import space from 'app/styles/space';
-import {Member, Organization, Project, Team, User} from 'app/types';
+import {Member, Organization, Project, User} from 'app/types';
 import {callIfFunction} from 'app/utils/callIfFunction';
 import withApi from 'app/utils/withApi';
 
 const getSearchKeyForUser = (user: User) =>
   `${user.email && user.email.toLowerCase()} ${user.name && user.name.toLowerCase()}`;
 
-type Actor<T> = {
-  type: T;
-  id: string;
-  name: string;
-};
-
-type Mentionable<T> = {
+type MentionableUser = {
   value: string;
   label: React.ReactElement;
   searchKey: string;
-  actor: Actor<T>;
+  actor: {
+    type: 'user';
+    id: string;
+    name: string;
+  };
 };
-
-const UnassignedWrapper = styled('div')`
-  display: flex;
-  align-items: center;
-`;
-
-const StyledIconUser = styled(IconUser)`
-  margin-left: ${space(0.25)};
-  margin-right: ${space(1)};
-  color: ${p => p.theme.gray400};
-`;
-
-const unassignedOption = {
-  value: null,
-  label: (
-    <UnassignedWrapper>
-      <StyledIconUser size="20px" />
-      {t('Unassigned')}
-    </UnassignedWrapper>
-  ),
-  searchKey: 'unassigned',
-  actor: null,
-};
-type MentionableUnassigned = typeof unassignedOption;
 
 type Unmentionable = {
   disabled: boolean;
   label: React.ReactElement;
 };
 
-type MentionableTeam = Mentionable<'team'>;
-type UnmentionableTeam = MentionableTeam & Unmentionable;
-type MentionableUser = Mentionable<'user'>;
 type UnmentionableUser = MentionableUser & Unmentionable;
 
-type AllMentionable = (MentionableUser | MentionableTeam | MentionableUnassigned) &
-  Partial<Unmentionable>;
+type AllMentionable = MentionableUser & Partial<Unmentionable>;
 
 type Props = {
   api: Client;
@@ -82,9 +46,6 @@ type Props = {
   disabled?: boolean;
   placeholder?: string;
   styles?: {control?: (provided: any) => any};
-  filteredTeamIds?: Set<string>;
-  // Used to display an additional unassigned option
-  includeUnassigned?: boolean;
 };
 
 type State = {
@@ -114,9 +75,6 @@ class SelectMembers extends React.Component<Props, State> {
   componentWillUnmount() {
     this.unlisteners.forEach(callIfFunction);
   }
-
-  // TODO(ts) This type could be improved when react-select types are better.
-  selectRef = React.createRef<any>();
 
   unlisteners = [
     MemberListStore.listen(() => {
@@ -156,134 +114,8 @@ class SelectMembers extends React.Component<Props, State> {
     ),
   });
 
-  createMentionableTeam = (team: Team): MentionableTeam => ({
-    value: team.id,
-    label: <IdBadge team={team} />,
-    searchKey: `#${team.slug}`,
-    actor: {
-      type: 'team',
-      id: team.id,
-      name: team.slug,
-    },
-  });
-
-  createUnmentionableTeam = (team: Team): UnmentionableTeam => {
-    const {organization} = this.props;
-    const canAddTeam = organization.access.includes('project:write');
-
-    return {
-      ...this.createMentionableTeam(team),
-      disabled: true,
-      label: (
-        <UnmentionableTeam>
-          <DisabledLabel>
-            <Tooltip
-              position="left"
-              title={t('%s is not a member of project', `#${team.slug}`)}
-            >
-              <IdBadge team={team} />
-            </Tooltip>
-          </DisabledLabel>
-          <Tooltip
-            title={
-              canAddTeam
-                ? t('Add %s to project', `#${team.slug}`)
-                : t('You do not have permission to add team to project.')
-            }
-          >
-            <AddToProjectButton
-              type="button"
-              size="zero"
-              borderless
-              disabled={!canAddTeam}
-              onClick={this.handleAddTeamToProject.bind(this, team)}
-              icon={<IconAdd isCircled />}
-            />
-          </Tooltip>
-        </UnmentionableTeam>
-      ),
-    };
-  };
-
   getMentionableUsers() {
     return MemberListStore.getAll().map(this.createMentionableUser);
-  }
-
-  getMentionableTeams(): MentionableTeam[] {
-    const {project} = this.props;
-    const projectData = project && ProjectsStore.getBySlug(project.slug);
-
-    if (!projectData) {
-      return [];
-    }
-
-    return projectData.teams.map(this.createMentionableTeam);
-  }
-
-  /**
-   * Get list of teams that are not in the current project, for use in `MultiSelectMenu`
-   *
-   * @param {Team[]} teamsInProject A list of teams that are in the current project
-   */
-  getTeamsNotInProject(teamsInProject: MentionableTeam[] = []): UnmentionableTeam[] {
-    const teams: Team[] = TeamStore.getAll() || [];
-    const excludedTeamIds = teamsInProject.map(({actor}) => actor.id);
-
-    return teams
-      .filter(team => excludedTeamIds.indexOf(team.id) === -1)
-      .map(this.createUnmentionableTeam);
-  }
-
-  /**
-   * Closes the select menu by blurring input if possible since that seems to be the only
-   * way to close it.
-   */
-  closeSelectMenu() {
-    if (!this.selectRef.current) {
-      return;
-    }
-
-    // @ts-ignore The types for react-select don't cover this property
-    // or the type of selectRef is incorrect.
-    const select = this.selectRef.current.select.select;
-    const input: HTMLInputElement = select.inputRef;
-    if (input) {
-      // I don't think there's another way to close `react-select`
-      input.blur();
-    }
-  }
-
-  async handleAddTeamToProject(team: Team) {
-    const {api, organization, project, value} = this.props;
-    const {options} = this.state;
-
-    // Copy old value
-    const oldValue = value ? [...value] : {value};
-
-    // Optimistic update
-    this.props.onChange(this.createMentionableTeam(team));
-
-    try {
-      // Try to add team to project
-      if (project) {
-        await addTeamToProject(api, organization.slug, project.slug, team);
-
-        // Remove add to project button without changing order
-        const newOptions = options!.map(option => {
-          if (option.actor?.id === team.id) {
-            option.disabled = false;
-            option.label = <IdBadge team={team} />;
-          }
-
-          return option;
-        });
-        this.setState({options: newOptions});
-      }
-    } catch (err) {
-      // Unable to add team to project, revert select menu value
-      this.props.onChange(oldValue);
-    }
-    this.closeSelectMenu();
   }
 
   handleChange = newValue => {
@@ -318,21 +150,6 @@ class SelectMembers extends React.Component<Props, State> {
   }, 250);
 
   handleLoadOptions = (): Promise<AllMentionable[]> => {
-    const {showTeam, filteredTeamIds, includeUnassigned} = this.props;
-    if (showTeam) {
-      const teamsInProject = this.getMentionableTeams();
-      const teamsNotInProject = this.getTeamsNotInProject(teamsInProject);
-      const unfilteredOptions = [...teamsInProject, ...teamsNotInProject];
-      const options: AllMentionable[] = filteredTeamIds
-        ? unfilteredOptions.filter(({value}) => !value || filteredTeamIds.has(value))
-        : unfilteredOptions;
-      if (includeUnassigned) {
-        options.push(unassignedOption);
-      }
-      this.setState({options});
-      return Promise.resolve(options);
-    }
-
     const usersInProject = this.getMentionableUsers();
     const usersInProjectById = usersInProject.map(({actor}) => actor.id);
 
@@ -375,7 +192,6 @@ class SelectMembers extends React.Component<Props, State> {
 
     return (
       <StyledSelectControl
-        ref={this.selectRef}
         filterOption={(option: FilterOption<AllMentionable>, filterText: string) =>
           option?.data?.searchKey?.indexOf(filterText) > -1
         }
@@ -407,16 +223,6 @@ const DisabledLabel = styled('div')`
   display: flex;
   opacity: 0.5;
   overflow: hidden; /* Needed so that "Add to team" button can fit */
-`;
-
-const AddToProjectButton = styled(Button)`
-  flex-shrink: 0;
-`;
-
-const UnmentionableTeam = styled('div')`
-  display: flex;
-  justify-content: space-between;
-  align-items: flex-end;
 `;
 
 const StyledSelectControl = styled(SelectControl)`
