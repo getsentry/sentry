f0c33ad39e5c5213392386b06b3cfa8679f75c73
William Mak <william@wmak.io>
2021-09-02 13:44:26 -0400

    ref: updating order of ops + adding todo

--- src/sentry/snuba/discover.py
+++ src/sentry/snuba/discover.py
@@ -645,6 +645,7 @@ def top_events_timeseries(
             referrer=referrer,
         )
         if len(top_events["data"]) == limit and len(result.get("data", [])) and include_other:
+            # TODO(wmak): Conditions are incorrect in some cases, need to properly apply Demorgan's Law
             other_result = raw_query(
                 aggregations=snuba_filter.aggregations,
                 conditions=other_conditions,
@@ -661,7 +662,11 @@ def top_events_timeseries(
         else:
             other_result = {"data": []}
 
-    if not allow_empty and not len(result.get("data", [])) + len(other_result.get("data", [])):
+    if (
+        not allow_empty
+        and not len(result.get("data", []))
+        and not len(other_result.get("data", []))
+    ):
         return SnubaTSResult(
             {
                 "data": zerofill([], snuba_filter.start, snuba_filter.end, rollup, "time")
