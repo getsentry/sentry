6038e699f8e42fc8a64717f35abe624a3edae7a2
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-09-09 16:48:32 -0400

    fix(chart-unfurls): Fix blank unfurls caused by django update
    Fixes the blank unfurls.

--- src/sentry/integrations/slack/unfurl/discover.py
+++ src/sentry/integrations/slack/unfurl/discover.py
@@ -1,3 +1,4 @@
+import html
 import re
 from typing import Any, List, Mapping
 from urllib.parse import urlparse
@@ -142,6 +143,7 @@ def map_discover_query_args(url: str, args: Mapping[str, str]) -> Mapping[str, A
     """
     Extracts discover arguments from the discover link's query string
     """
+    url = html.unescape(url)
     parsed_url = urlparse(url)
     query = QueryDict(parsed_url.query).copy()
 
