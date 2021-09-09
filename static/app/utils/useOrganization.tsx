0e372653c1ab02139718f29e0903e12e452f1f88
Kev <kevan.fisher@sentry.io>
2021-09-09 09:29:02 -0700

    rfc(ui): Add useOrganization hook (#28108)
    * rfc(ui): Add useOrganization hook
    
    Organization rarely changes, and it gets drilled hundreds of times, adding a useOrganization hook might be helpful to not have to use HoC's and reduce the amount of times that organization shows up in component apis.
    
    Currently organization uses the legacy context, but I sidestepped that issue by just having the context container provide to the new createContext instead. This part is mostly what I'm looking for feedback on, since I didn't want to make a massive blocking change.

---
+++ static/app/utils/useOrganization.tsx
@@ -0,0 +1,11 @@
+import {useContext} from 'react';
+
+import {OrganizationContext} from 'app/views/organizationContext';
+
+export function useOrganization() {
+  const organization = useContext(OrganizationContext);
+  if (!organization) {
+    throw new Error('useOrganization called but organization is not set.');
+  }
+  return organization;
+}
