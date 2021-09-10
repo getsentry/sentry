473e00447b0e816336c3555a8fd64f22f7bc36d5
Colleen O'Rourke <colleen@sentry.io>
2021-09-10 13:59:57 -0700

    feat(sentry apps): Add AlertRuleAction creator

---
+++ src/sentry/mediators/alert_rule_actions/creator.py
@@ -0,0 +1,27 @@
+from sentry.mediators import Mediator, Param, external_requests
+from sentry.utils.cache import memoize
+
+
+class AlertRuleActionCreator(Mediator):
+    install = Param("sentry.models.SentryAppInstallation")
+    fields = Param(object)
+    uri = Param((str,))
+
+    def call(self):
+        self._make_external_request()
+        self._save_alert_rule_action()
+        return self.response
+
+    def _save_alert_rule_action(self):
+        pass
+
+    def _make_external_request(self):
+        self.response = external_requests.AlerRuleActionRequester.run(
+            install=self.install,
+            uri=self.uri,
+            fields=self.fields,
+        )
+
+    @memoize
+    def sentry_app(self):
+        return self.install.sentry_app
