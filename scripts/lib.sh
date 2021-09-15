e5f02c316829213cf355cb72208398bc5110c2f3
Armen Zambrano G <armenzg@sentry.io>
2021-09-15 15:28:56 -0400

    feat(dev):  Add make prerequisites target
    Create a new target which installs different libraries via brew depending on the context.
    
    For instance, on Apple M1 machines it should installed a couple more libraries.
    
    In the future, the bootstrap script and documentation should be using this target.
    
    Fixes #28606

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -216,6 +216,24 @@ reset-db() {
     apply-migrations
 }
 
+prerequisites() {
+    if [ -z "${CI+x}" ]; then
+        brew update -q && brew bundle -q
+        # psycopg2-binary does not have an arm64 wheel, thus, we need to build it locally
+        # by installing postgresql
+        # See details: https://github.com/psycopg/psycopg2/issues/1286
+        # Newer versions of psycopg2-binary do not need the CPP & LD flags
+        query-apple-m1 && brew install postgresql && \
+            CPPFLAGS="-I/opt/homebrew/opt/openssl@1.1/include" \
+            LDFLAGS="-L/opt/homebrew/opt/openssl@1.1/lib -L/opt/homebrew/opt/python@3.8/lib" \
+            pip install psycopg2-binary==2.8.6
+    else
+        # These are packages for the CI
+        # Needed for xmlsec since there's no wheels for it
+        brew install libxmlsec1 pkg-config
+    fi
+}
+
 direnv-help() {
     cat >&2 <<EOF
 If you're a Sentry employee and you're stuck or have questions, ask in #discuss-dev-tooling.
