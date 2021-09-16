f540453fbcb74d0106bc84552771c78e2c0058e9
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-09-16 15:54:45 -0700

    ref(teams): Update teamSelect to use useTeams hook (#28615)

--- static/app/views/settings/components/teamSelect.tsx
+++ static/app/views/settings/components/teamSelect.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import styled from '@emotion/styled';
 import debounce from 'lodash/debounce';
 
-import {Client} from 'app/api';
 import Button from 'app/components/button';
 import Confirm from 'app/components/confirm';
 import DropdownAutoComplete from 'app/components/dropdownAutoComplete';
@@ -11,16 +10,15 @@ import DropdownButton from 'app/components/dropdownButton';
 import TeamBadge from 'app/components/idBadge/teamBadge';
 import Link from 'app/components/links/link';
 import {Panel, PanelBody, PanelHeader, PanelItem} from 'app/components/panels';
-import {DEFAULT_DEBOUNCE_DURATION, TEAMS_PER_PAGE} from 'app/constants';
+import {DEFAULT_DEBOUNCE_DURATION} from 'app/constants';
 import {IconSubtract} from 'app/icons';
 import {t} from 'app/locale';
 import space from 'app/styles/space';
 import {Organization, Team} from 'app/types';
-import withApi from 'app/utils/withApi';
+import useTeams from 'app/utils/useTeams';
 import EmptyMessage from 'app/views/settings/components/emptyMessage';
 
 type Props = {
-  api: Client;
   organization: Organization;
   /**
    * Should button be disabled
@@ -38,12 +36,10 @@ type Props = {
    * Callback when teams are removed
    */
   onRemoveTeam: (teamSlug: string) => void;
-
   /**
    * Optional menu header.
    */
   menuHeader?: React.ReactElement;
-
   /**
    * Message to display when the last team is removed
    * if empty no confirm will be displayed.
@@ -51,98 +47,25 @@ type Props = {
   confirmLastTeamRemoveMessage?: string;
 };
 
-type State = {
-  loading: boolean;
-  teamsSearch: null | Team[];
-};
+function TeamSelect({
+  disabled,
+  selectedTeams,
+  menuHeader,
+  organization,
+  onAddTeam,
+  onRemoveTeam,
+  confirmLastTeamRemoveMessage,
+}: Props) {
+  const {teams, onSearch, fetching} = useTeams();
 
-class TeamSelect extends React.Component<Props, State> {
-  state: State = {
-    loading: true,
-    teamsSearch: null,
-  };
-
-  componentDidMount() {
-    this.fetchTeams();
-  }
-
-  fetchTeams = debounce(async (query?: string) => {
-    const {api, organization} = this.props;
-    const teamsSearch = await api.requestPromise(
-      `/organizations/${organization.slug}/teams/`,
-      {
-        query: {query, per_page: TEAMS_PER_PAGE},
-      }
-    );
-    this.setState({teamsSearch, loading: false});
-  }, DEFAULT_DEBOUNCE_DURATION);
-
-  handleQueryUpdate = (event: React.ChangeEvent<HTMLInputElement>) => {
-    this.setState({loading: true});
-    this.fetchTeams(event.target.value);
-  };
-
-  handleAddTeam = (option: Item) => {
-    const team = this.state.teamsSearch?.find(tm => tm.slug === option.value);
+  const handleAddTeam = (option: Item) => {
+    const team = teams.find(tm => tm.slug === option.value);
     if (team) {
-      this.props.onAddTeam(team);
+      onAddTeam(team);
     }
   };
 
-  handleRemove = (teamSlug: string) => {
-    this.props.onRemoveTeam(teamSlug);
-  };
-
-  renderTeamAddDropDown() {
-    const {disabled, selectedTeams, menuHeader} = this.props;
-    const {teamsSearch} = this.state;
-    const isDisabled = disabled;
-
-    let options: Item[] = [];
-    if (teamsSearch === null || teamsSearch.length === 0) {
-      options = [];
-    } else {
-      options = teamsSearch
-        .filter(
-          team => !selectedTeams.some(selectedTeam => selectedTeam.slug === team.slug)
-        )
-        .map((team, index) => ({
-          index,
-          value: team.slug,
-          searchKey: team.slug,
-          label: <DropdownTeamBadge avatarSize={18} team={team} />,
-        }));
-    }
-
-    return (
-      <DropdownAutoComplete
-        items={options}
-        busyItemsStillVisible={this.state.loading}
-        onChange={this.handleQueryUpdate}
-        onSelect={this.handleAddTeam}
-        emptyMessage={t('No teams')}
-        menuHeader={menuHeader}
-        disabled={isDisabled}
-        alignMenu="right"
-      >
-        {({isOpen}) => (
-          <DropdownButton
-            aria-label={t('Add Team')}
-            isOpen={isOpen}
-            size="xsmall"
-            disabled={isDisabled}
-          >
-            {t('Add Team')}
-          </DropdownButton>
-        )}
-      </DropdownAutoComplete>
-    );
-  }
-
-  renderBody() {
-    const {organization, selectedTeams, disabled, confirmLastTeamRemoveMessage} =
-      this.props;
-
+  const renderBody = () => {
     if (selectedTeams.length === 0) {
       return <EmptyMessage>{t('No Teams assigned')}</EmptyMessage>;
     }
@@ -156,25 +79,56 @@ class TeamSelect extends React.Component<Props, State> {
         key={team.slug}
         orgId={organization.slug}
         team={team}
-        onRemove={this.handleRemove}
+        onRemove={slug => onRemoveTeam(slug)}
         disabled={disabled}
         confirmMessage={confirmMessage}
       />
     ));
-  }
+  };
 
-  render() {
-    return (
-      <Panel>
-        <PanelHeader hasButtons>
-          {t('Team')}
-          {this.renderTeamAddDropDown()}
-        </PanelHeader>
+  // Only show options that aren't selected in the dropdown
+  const options = teams
+    .filter(team => !selectedTeams.some(selectedTeam => selectedTeam.slug === team.slug))
+    .map((team, index) => ({
+      index,
+      value: team.slug,
+      searchKey: team.slug,
+      label: <DropdownTeamBadge avatarSize={18} team={team} />,
+    }));
 
-        <PanelBody>{this.renderBody()}</PanelBody>
-      </Panel>
-    );
-  }
+  return (
+    <Panel>
+      <PanelHeader hasButtons>
+        {t('Team')}
+        <DropdownAutoComplete
+          items={options}
+          busyItemsStillVisible={fetching}
+          onChange={debounce<(e: React.ChangeEvent<HTMLInputElement>) => void>(
+            e => onSearch(e.target.value),
+            DEFAULT_DEBOUNCE_DURATION
+          )}
+          onSelect={handleAddTeam}
+          emptyMessage={t('No teams')}
+          menuHeader={menuHeader}
+          disabled={disabled}
+          alignMenu="right"
+        >
+          {({isOpen}) => (
+            <DropdownButton
+              aria-label={t('Add Team')}
+              isOpen={isOpen}
+              size="xsmall"
+              disabled={disabled}
+            >
+              {t('Add Team')}
+            </DropdownButton>
+          )}
+        </DropdownAutoComplete>
+      </PanelHeader>
+
+      <PanelBody>{renderBody()}</PanelBody>
+    </Panel>
+  );
 }
 
 type TeamRowProps = {
@@ -223,4 +177,4 @@ const StyledLink = styled(Link)`
   margin-right: ${space(1)};
 `;
 
-export default withApi(TeamSelect);
+export default TeamSelect;
