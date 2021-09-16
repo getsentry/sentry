a8fc83602196f37b14d023914f774ae43db5631a
Zhixing Zhang <me@neoto.xin>
2021-09-16 10:08:19 -0700

    change name

--- static/app/data/experimentConfig.tsx
+++ static/app/data/experimentConfig.tsx
@@ -26,7 +26,7 @@ export const experimentList = [
     assignments: [0, 1],
   },
   {
-    key: 'DefaultBusinessForAllExperiment',
+    key: 'DefaultBusinessForFreeOrgsExperiment',
     type: ExperimentType.Organization,
     parameter: 'exposed',
     assignments: [0, 1],
