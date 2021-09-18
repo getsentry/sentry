accb4d3faabe5349afeeb69f259cfae3b8959fb0
Dan Fuller <dfuller@sentry.io>
2021-09-17 17:13:42 -0700

    feat(comparison_alerts): Add `comparison_delta` field to `AlertRule`.
    This pr adds an extra field to `AlertRule` - `comparison_delta`. It's an optional integer
    representing seconds. We'll use it to calculate the date we should run the comparison period for,
    for each result that is processed by the subscription processor.

--- src/sentry/incidents/models.py
+++ src/sentry/incidents/models.py
@@ -384,6 +384,9 @@ class AlertRule(Model):
     threshold_type = models.SmallIntegerField(null=True)
     resolve_threshold = models.FloatField(null=True)
     threshold_period = models.IntegerField()
+    # This represents a time delta, in seconds. If not null, this is used to determine which time
+    # window  to query to compare the result from the current time_window to.
+    comparison_delta = models.IntegerField(null=True)
     date_modified = models.DateTimeField(default=timezone.now)
     date_added = models.DateTimeField(default=timezone.now)
 
