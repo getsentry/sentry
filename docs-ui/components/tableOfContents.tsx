d98d030dbb0f8cc327ce8447d18002f3d8286662
Vu Luong <vuluongj20@gmail.com>
2021-09-20 12:01:52 -0700

    stylelint fix: change selection syntax
    To avoid duplicate selector warning from stylelint.

--- docs-ui/components/tableOfContents.tsx
+++ docs-ui/components/tableOfContents.tsx
@@ -17,37 +17,35 @@ const Content = styled('div')`
   position: fixed;
   top: calc(${space(4)} * 3);
 
-  & {
-    > .toc-wrapper > .toc-list {
-      padding-left: 0;
-      border-left: solid 2px ${p => p.theme.gray100};
-    }
-    .toc-list-item {
-      position: relative;
-      list-style-type: none;
-      margin-left: ${space(2)};
-    }
-    .toc-list-item::before {
-      content: '';
-      position: absolute;
-      height: 100%;
-      top: 0;
-      left: 0;
-      transform: translateX(calc(-2px - ${space(2)}));
-      border-left: solid 2px ${p => p.theme.gray500};
-      opacity: 0;
-      transition: opacity 0.2s;
-    }
-    .toc-list-item.is-active-li::before {
-      opacity: 1;
-    }
-    .toc-list-item > a {
-      color: ${p => p.theme.gray300};
-    }
-    .toc-list-item.is-active-li > a {
-      font-weight: 600;
-      color: ${p => p.theme.gray500};
-    }
+  & > .toc-wrapper > .toc-list {
+    padding-left: 0;
+    border-left: solid 2px ${p => p.theme.gray100};
+  }
+  & .toc-list-item {
+    position: relative;
+    list-style-type: none;
+    margin-left: ${space(2)};
+  }
+  & .toc-list-item::before {
+    content: '';
+    position: absolute;
+    height: 100%;
+    top: 0;
+    left: 0;
+    transform: translateX(calc(-2px - ${space(2)}));
+    border-left: solid 2px ${p => p.theme.gray500};
+    opacity: 0;
+    transition: opacity 0.2s;
+  }
+  & .toc-list-item.is-active-li::before {
+    opacity: 1;
+  }
+  & .toc-list-item > a {
+    color: ${p => p.theme.gray300};
+  }
+  & .toc-list-item.is-active-li > a {
+    font-weight: 600;
+    color: ${p => p.theme.gray500};
   }
 `;
 
