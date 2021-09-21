7c6866c27860552fbd52d766c549141fa37ac509
Taylan Gocmen <ta.gocmen@gmail.com>
2021-09-21 09:46:19 -0700

    feat(alerts): Change alert condition selector for issue alerts

--- static/app/views/alerts/issueRuleEditor/ruleNodeList.tsx
+++ static/app/views/alerts/issueRuleEditor/ruleNodeList.tsx
@@ -16,6 +16,12 @@ import {EVENT_FREQUENCY_PERCENT_CONDITION} from 'app/views/projectInstall/issueA
 
 import RuleNode from './ruleNode';
 
+const CHANGE_ALERT_CONDITION_IDS = [
+  'sentry.rules.conditions.event_frequency.EventFrequencyCondition',
+  'sentry.rules.conditions.event_frequency.EventUniqueUserFrequencyCondition',
+  'sentry.rules.conditions.event_frequency.EventFrequencyPercentCondition',
+];
+
 type Props = {
   project: Project;
   organization: Organization;
@@ -42,14 +48,111 @@ type Props = {
 
 class RuleNodeList extends React.Component<Props> {
   getNode = (
-    id: string
+    id: string,
+    itemIdx: number
   ):
     | IssueAlertRuleActionTemplate
     | IssueAlertRuleConditionTemplate
     | null
     | undefined => {
-    const {nodes} = this.props;
-    return nodes ? nodes.find(node => node.id === id) : null;
+    const {nodes, items} = this.props;
+    const node = nodes ? nodes.find(n => n.id === id) : null;
+
+    if (!node) {
+      return null;
+    }
+
+    if (!CHANGE_ALERT_CONDITION_IDS.includes(node.id)) {
+      return node;
+    }
+
+    const item = items.find(i => i.id === node.id);
+
+    const changeAlertNode: IssueAlertRuleConditionTemplate = {
+      ...node,
+      label: node.label.replace('more than', '{comparisonType} than'),
+      formFields: {
+        ...node.formFields,
+        comparisonType: {
+          type: 'choice',
+          choices: [
+            ['count', 'more'],
+            ['percent', 'higher'],
+          ],
+          initial: 'count',
+        },
+        ...(node.formFields
+          ? {
+              interval: {
+                ...node.formFields.interval,
+                initial: node.formFields?.interval.initial || '5m',
+              },
+            }
+          : {}),
+      },
+    };
+
+    if (item?.comparisonType === 'percent') {
+      const intervalSelected = (
+        node.formFields?.interval as {choices?: [string, string][]}
+      ).choices?.find(([_interval]) => _interval === item.interval);
+
+      const formComparisonInterval = node.formFields?.comparisonInterval as {
+        choices?: [string, string][];
+      };
+      const comparisonIntervalSelected = formComparisonInterval?.choices?.find(
+        ([_comparisonInterval]) => _comparisonInterval === item.comparisonInterval
+      );
+
+      if (
+        intervalSelected &&
+        ['1d', '1w', '30d'].includes(intervalSelected[0]) &&
+        comparisonIntervalSelected &&
+        !['1d', '1w', '30d'].includes(comparisonIntervalSelected[0])
+      ) {
+        this.props.onPropertyChange(itemIdx, 'comparisonInterval', intervalSelected[0]);
+      }
+
+      let choices: [string, string][];
+      if (intervalSelected && intervalSelected[0] === '30d') {
+        choices = [['30d', '30 days']];
+      } else if (intervalSelected && intervalSelected[0] === '1w') {
+        choices = [
+          ['1w', 'one week'],
+          ['30d', '30 days'],
+        ];
+      } else if (intervalSelected && intervalSelected[0] === '1d') {
+        choices = [
+          ['1d', 'one day'],
+          ['1w', 'one week'],
+          ['30d', '30 days'],
+        ];
+      } else {
+        choices = [
+          intervalSelected || ['5m', '5 minutes'],
+          ['1d', 'one day'],
+          ['1w', 'one week'],
+          ['30d', '30 days'],
+        ];
+      }
+
+      return {
+        ...changeAlertNode,
+        label: changeAlertNode.label
+          .replace('times ', '')
+          .replace('{value}', '{value} %')
+          .concat(' compared to {comparisonInterval} before'),
+        formFields: {
+          ...changeAlertNode.formFields,
+          comparisonInterval: {
+            type: 'choice',
+            choices,
+          },
+        },
+      } as IssueAlertRuleConditionTemplate;
+    }
+
+    return changeAlertNode;
   };
 
   render() {
@@ -123,7 +226,7 @@ class RuleNodeList extends React.Component<Props> {
             <RuleNode
               key={idx}
               index={idx}
-              node={this.getNode(item.id)}
+              node={this.getNode(item.id, idx)}
               onDelete={onDeleteRow}
               onPropertyChange={onPropertyChange}
               onReset={onResetRow}
