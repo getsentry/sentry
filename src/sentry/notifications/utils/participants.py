f0552a591d6a64f7f8e98dea6cdd1787af1d1f64
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-09-21 13:53:18 -0700

    Rewrite get_send_to

--- src/sentry/notifications/utils/participants.py
+++ src/sentry/notifications/utils/participants.py
@@ -14,6 +14,7 @@ from typing import (
 
 from sentry import features
 from sentry.models import (
+    ActorTuple,
     Group,
     GroupSubscription,
     NotificationSetting,
@@ -39,7 +40,6 @@ from sentry.notifications.types import (
 )
 from sentry.types.integrations import ExternalProviders
 from sentry.utils import metrics
-from sentry.utils.cache import cache
 
 if TYPE_CHECKING:
     from sentry.eventstore.models import Event
@@ -159,63 +159,34 @@ def split_participants_and_context(
     return participants, extra_context
 
 
-def get_send_to(
-    project: Project,
-    target_type: ActionTargetType,
-    target_identifier: Optional[int] = None,
-    event: Optional["Event"] = None,
-) -> Mapping[ExternalProviders, Union[Set[User], Set[Team]]]:
-    """
-    Returns a mapping of providers to a list of user IDs for the users that
-    should receive notifications for the provided project. This result may come
-    from cached data.
-    """
-    if not (project and project.teams.exists()):
-        logger.debug("Tried to send notification to invalid project: %r", project)
-        return {}
-    if target_type == ActionTargetType.ISSUE_OWNERS:
-        if not event:
-            return get_send_to_all_in_project(project)
-        else:
-            return get_send_to_owners(event, project)
-    elif target_type == ActionTargetType.MEMBER:
-        return get_send_to_member(project, target_identifier)
-    elif target_type == ActionTargetType.TEAM:
-        return get_send_to_team(project, target_identifier)
-    return {}
+def get_owners(
+    project: Project, event: Optional["Event"] = None
+) -> Iterable[Union["Team", "User"]]:
+    """Given a project and an event, decide which users and teams are the owners."""
 
-
-def get_send_to_owners(
-    event: "Event", project: Project
-) -> Mapping[ExternalProviders, Union[Set["User"], Set["Team"]]]:
-    owners, _ = ProjectOwnership.get_owners(project.id, event.data)
-    if owners == ProjectOwnership.Everyone:
-        metrics.incr(
-            "features.owners.send_to",
-            tags={"organization": project.organization_id, "outcome": "everyone"},
-            skip_internal=True,
-        )
-        return get_send_to_all_in_project(project)
+    if event:
+        owners, _ = ProjectOwnership.get_owners(project.id, event.data)
+    else:
+        owners = ProjectOwnership.Everyone
 
     if not owners:
-        metrics.incr(
-            "features.owners.send_to",
-            tags={"organization": project.organization_id, "outcome": "empty"},
-            skip_internal=True,
-        )
-        return {}
+        outcome = "empty"
+        recipients = set()
+
+    elif owners == ProjectOwnership.Everyone:
+        outcome = "everyone"
+        recipients = User.objects.filter(id__in=project.member_set.values_list("user", flat=True))
+
+    else:
+        outcome = "match"
+        recipients = ActorTuple.resolve_many(owners)
 
     metrics.incr(
         "features.owners.send_to",
-        tags={"organization": project.organization_id, "outcome": "match"},
+        tags={"organization": project.organization_id, "outcome": outcome},
         skip_internal=True,
     )
-
-    # Explicitly typing to satisfy mypy.
-    mapping: Mapping[
-        ExternalProviders, Union[Set["User"], Set["Team"]]
-    ] = NotificationSetting.objects.filter_to_accepting_recipients(project, owners)
-    return mapping
+    return recipients
 
 
 def disabled_users_from_project(project: Project) -> Mapping[ExternalProviders, Set[User]]:
@@ -248,59 +219,53 @@ def disabled_users_from_project(project: Project) -> Mapping[ExternalProviders, 
     return output
 
 
-def get_send_to_team(
-    project: Project, target_identifier: Optional[Union[str, int]]
-) -> Mapping[ExternalProviders, Set[User]]:
+def determine_eligible_recipients(
+    project: "Project",
+    target_type: ActionTargetType,
+    target_identifier: Optional[int] = None,
+    event: Optional["Event"] = None,
+) -> Iterable[Union["Team", "User"]]:
     """
-    Get a team's notification settings. If not present, get settings for each subscribed user in the team.
-    :param project:
-    :param target_identifier: Optional. String or int representation of a team_id.
-    :returns: Mapping[ExternalProvider, Iterable[User]] A mapping of provider to
-        member that a notification should be sent to as a set.
+    Either get the individual recipient from the target type/id or user the the
+    owners as determined by rules for this project and event.
     """
+    if not (project and project.teams.exists()):
+        logger.debug(f"Tried to send notification to invalid project: {project}")
+
+    elif target_type == ActionTargetType.MEMBER:
+        user = get_user_from_identifier(project, target_identifier)
+        if user:
+            return {user}
+
+    elif target_type == ActionTargetType.TEAM:
+        team = get_team_from_identifier(project, target_identifier)
+        if team:
+            return {team}
+
+    else:
+        return get_owners(project, event)
+
+    return set()
+
+
+def get_send_to(
+    project: "Project",
+    target_type: ActionTargetType,
+    target_identifier: Optional[int] = None,
+    event: Optional["Event"] = None,
+) -> Mapping[ExternalProviders, Iterable[Union["Team", "User"]]]:
+    recipients = determine_eligible_recipients(project, target_type, target_identifier, event)
+    return get_recipients_by_provider(project, recipients)
+
+
+def get_user_from_identifier(
+    project: "Project", target_identifier: Optional[Union[str, int]]
+) -> Optional["User"]:
     if target_identifier is None:
-        return {}
+        return None
+
     try:
-        team = Team.objects.get(id=int(target_identifier), projectteam__project=project)
-    except Team.DoesNotExist:
-        return {}
-
-    team_notification_settings = NotificationSetting.objects.get_for_recipient_by_parent(
-        NotificationSettingTypes.ISSUE_ALERTS, parent=project, recipients=[team]
-    )
-    if team_notification_settings:
-        team_mapping = {
-            ExternalProviders(notification_setting.provider): {team}
-            for notification_setting in team_notification_settings
-        }
-        return team_mapping
-
-    # fallback to notifying each subscribed user if there aren't team notification settings
-    member_list = team.member_set.values_list("user_id", flat=True)
-    users = User.objects.filter(id__in=member_list)
-
-    mapping: Mapping[
-        ExternalProviders, Set[User]
-    ] = NotificationSetting.objects.filter_to_accepting_recipients(project, users)
-    return mapping
-
-
-def get_send_to_member(
-    project: Project, target_identifier: Optional[Union[int, str]]
-) -> Mapping[ExternalProviders, Set[User]]:
-    """
-    No checking for disabled users is done. If a user explicitly specifies a
-    member as a target to send to, it should overwrite the user's personal mail
-    settings.
-    :param project:
-    :param target_identifier: Optional. String or int representation of a user_id.
-    :returns: Mapping[ExternalProvider, Iterable[User]] A mapping of provider to
-        member that a notification should be sent to as a set.
-    """
-    if target_identifier is None:
-        return {}
-    try:
-        user = (
+        return (
             User.objects.filter(
                 id=int(target_identifier),
                 sentry_orgmember_set__teams__projectteam__project=project,
@@ -309,28 +274,85 @@ def get_send_to_member(
             .get()
         )
     except User.DoesNotExist:
-        return {}
-    notification_settings = NotificationSetting.objects.get_for_recipient_by_parent(
-        NotificationSettingTypes.ISSUE_ALERTS, parent=project, recipients=[user]
-    )
-    if notification_settings:
-        return {
-            ExternalProviders(notification_setting.provider): {user}
-            for notification_setting in notification_settings
-        }
-    # Fall back to email if there are no settings.
-    return {ExternalProviders.EMAIL: {user}}
+        return None
 
 
-def get_send_to_all_in_project(project: Project) -> Mapping[ExternalProviders, Set[User]]:
-    cache_key = f"mail:send_to:{project.pk}"
-    send_to_mapping: Optional[Mapping[ExternalProviders, Set[User]]] = cache.get(cache_key)
-    if send_to_mapping is None:
-        users_by_provider = NotificationSetting.objects.get_notification_recipients(project)
-        send_to_mapping = {
-            provider: {user for user in users if user}
-            for provider, users in users_by_provider.items()
-        }
-        cache.set(cache_key, send_to_mapping, 60)  # 1 minute cache
+def get_team_from_identifier(
+    project: "Project", target_identifier: Optional[Union[str, int]]
+) -> Optional["Team"]:
+    if target_identifier is None:
+        return None
 
-    return send_to_mapping
+    try:
+        return Team.objects.get(id=int(target_identifier), projectteam__project=project)
+    except Team.DoesNotExist:
+        return None
+
+
+def partition_recipients(
+    recipients: Iterable[Union["Team", "User"]]
+) -> Tuple[Iterable["Team"], Iterable["User"]]:
+    teams, users = set(), set()
+    for recipient in recipients:
+        if isinstance(recipient, User):
+            users.add(recipient)
+        else:
+            teams.add(recipient)
+    return teams, users
+
+
+def get_users_from_team_fall_back(
+    teams: Iterable["Team"],
+    recipients_by_provider: Mapping[ExternalProviders, Iterable[Union["Team", "User"]]],
+) -> Iterable["User"]:
+    teams_to_fall_back = set(teams)
+    for recipients in recipients_by_provider.values():
+        for recipient in recipients:
+            teams_to_fall_back.remove(recipient)
+
+    users = set()
+    for team in teams_to_fall_back:
+        # Fall back to notifying each subscribed user if there aren't team notification settings
+        member_list = team.member_set.values_list("user_id", flat=True)
+        users |= set(User.objects.filter(id__in=member_list))
+    return users
+
+
+def combine_recipients_by_provider(
+    teams_by_provider: Mapping[ExternalProviders, Iterable[Union["Team", "User"]]],
+    users_by_provider: Mapping[ExternalProviders, Iterable[Union["Team", "User"]]],
+) -> Mapping[ExternalProviders, Iterable[Union["Team", "User"]]]:
+    """TODO(mgaeta): Make this more generic and move it to utils."""
+    recipients_by_provider = defaultdict(set)
+    for provider, teams in teams_by_provider.items():
+        for team in teams:
+            recipients_by_provider[provider].add(team)
+    for provider, users in users_by_provider.items():
+        for user in users:
+            recipients_by_provider[provider].add(user)
+    return recipients_by_provider
+
+
+def get_recipients_by_provider(
+    project: Project, recipients: Iterable[Union["Team", "User"]]
+) -> Mapping[ExternalProviders, Iterable[Union["Team", "User"]]]:
+    """Get the lists of recipients that should receive an Issue Alert by ExternalProvider."""
+    teams, users = partition_recipients(recipients)
+
+    # First evaluate the teams.
+    teams_by_provider = NotificationSetting.objects.filter_to_accepting_recipients(project, teams)
+
+    # Teams cannot receive emails so omit EMAIL settings.
+    teams_by_provider = {
+        provider: teams
+        for provider, teams in teams_by_provider.items()
+        if provider != ExternalProviders.EMAIL
+    }
+
+    # If there are any teams that didn't get added, fall back and add all users.
+    users |= get_users_from_team_fall_back(teams, teams_by_provider)
+
+    # Repeat for users.
+    users_by_provider = NotificationSetting.objects.filter_to_accepting_recipients(project, users)
+
+    return combine_recipients_by_provider(teams_by_provider, users_by_provider)
