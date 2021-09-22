51897b016437a8bb68336ee8612c6a69c7bb6465
Leander Rodrigues <leander.rodrigues@sentry.io>
2021-09-22 16:21:21 -0400

    ✅ Add test and after function to new rule

--- src/sentry/rules/actions/notify_event_sentry_app.py
+++ src/sentry/rules/actions/notify_event_sentry_app.py
@@ -1,12 +1,14 @@
 """
 Used for notifying a *specific* sentry app with a custom webhook payload (i.e. specified UI components)
 """
+
 from sentry.api.serializers import serialize
 from sentry.api.serializers.models.sentry_app_component import SentryAppAlertRuleActionSerializer
 from sentry.coreapi import APIError
 from sentry.mediators import sentry_app_components
-from sentry.models import SentryAppComponent, SentryAppInstallation
+from sentry.models import SentryApp, SentryAppComponent, SentryAppInstallation
 from sentry.rules.actions.base import EventAction
+from sentry.tasks.sentry_apps import notify_sentry_app
 
 
 class NotifyEventSentryAppAction(EventAction):
@@ -37,3 +39,26 @@ class NotifyEventSentryAppAction(EventAction):
                 except APIError:
                     continue
         return action_list
+
+    def after(self, event, state):
+        sentry_app_installation_uuid = self.get_option("sentryAppInstallationUuid")
+
+        extra = {"event_id": event.event_id}
+
+        if not sentry_app_installation_uuid:
+            self.logger.info("rules.fail.is_configured", extra=extra)
+
+        app = None
+
+        try:
+            install = SentryAppInstallation.objects.get(uuid=sentry_app_installation_uuid)
+            app = SentryApp.objects.get(id=install.sentry_app_id)
+        except SentryAppInstallation.DoesNotExist:
+            self.logger.info("rules.fail.no_install", extra=extra)
+            pass
+        except SentryApp.DoesNotExist:
+            self.logger.info("rules.fail.no_app", extra=extra)
+            pass
+
+        kwargs = {"sentry_app": app}
+        yield self.future(notify_sentry_app, **kwargs)
