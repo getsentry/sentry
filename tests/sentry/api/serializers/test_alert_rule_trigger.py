0488225de368547687c1e21e36d5de2fe3348e52
Dan Fuller <dfuller@sentry.io>
2021-09-22 18:31:25 -0700

    feat(comparison_alerts): Translate delta percents into comparison percents in the api layer.
    The frontend represents percentages as delta change percents. For example:
     - 30% increase
     - 40% decrease.
    
    This makes sense from a user point of view, but doesn't fit as well into how our system handles
    thesholds. For example, with decrease, if we had threshold like:
    
    - critical at 40%
    - resolve at 30%
    
    This would fail our validation, since resolve thresholds on below alerts need to be greater than the
    critical/warning thresholds. It also makes comparisons in the subscription processor more difficult.
    
    To work around this, we convert these delta percents into comparison percents. This means that:
    
     - 30% increase -> 130% comparison percent
     - 40% decrease -> 60% comparison percent.
    
    This fits well into our validation, since now if we have a decrease % alert like
    
    - critical at 40% (60% comparison)
    - resolve at 30% (70% comparison)
    
    The resolve threshold is now higher than the critical, and validation will fire.
    
    Handling this complexity at the api layer seems to work the best, so that internally we can assume
    that all of these threshold are comparison percents.
    
    I also considered making increase deltas positive and decrease deltas negative, but I felt like this
    reads weirdly in terms of our data schema (we'd have a below trigger that looks like < -30%, if you
    just looked at the data).

--- tests/sentry/api/serializers/test_alert_rule_trigger.py
+++ tests/sentry/api/serializers/test_alert_rule_trigger.py
@@ -3,14 +3,20 @@ from sentry.api.serializers.models.alert_rule_trigger import DetailedAlertRuleTr
 from sentry.incidents.logic import create_alert_rule_trigger
 from sentry.testutils import TestCase
 
+NOT_SET = object()
+
 
 class BaseAlertRuleTriggerSerializerTest:
-    def assert_alert_rule_trigger_serialized(self, trigger, result):
+    def assert_alert_rule_trigger_serialized(self, trigger, result, alert_threshold=NOT_SET):
         assert result["id"] == str(trigger.id)
         assert result["alertRuleId"] == str(trigger.alert_rule_id)
         assert result["label"] == trigger.label
         assert result["thresholdType"] == trigger.alert_rule.threshold_type
-        assert result["alertThreshold"] == trigger.alert_threshold
+        assert (
+            result["alertThreshold"] == trigger.alert_threshold
+            if alert_threshold is NOT_SET
+            else alert_threshold
+        )
         assert result["resolveThreshold"] == trigger.alert_rule.resolve_threshold
         assert result["dateCreated"] == trigger.date_added
 
@@ -27,6 +33,18 @@ class AlertRuleTriggerSerializerTest(BaseAlertRuleTriggerSerializerTest, TestCas
         trigger = create_alert_rule_trigger(alert_rule, "hi", 1000.50)
         result = serialize(trigger)
         self.assert_alert_rule_trigger_serialized(trigger, result)
+
+    def test_comparison_above(self):
+        alert_rule = self.create_alert_rule(comparison_delta=60)
+        trigger = create_alert_rule_trigger(alert_rule, "hi", 80)
+        result = serialize(trigger)
+        self.assert_alert_rule_trigger_serialized(trigger, result, 180)
+
+    def test_comparison_below(self):
+        alert_rule = self.create_alert_rule(comparison_delta=60)
+        trigger = create_alert_rule_trigger(alert_rule, "hi", 80)
+        result = serialize(trigger)
+        self.assert_alert_rule_trigger_serialized(trigger, result, 20)
 
 
 class DetailedAlertRuleTriggerSerializerTest(BaseAlertRuleTriggerSerializerTest, TestCase):
