7eacb6f95f09f49839d16c5c312aa98ed3b9fc86
Aniket Das <aniket.das@sentry.io>
2021-09-23 14:23:09 -0700

    ref(api-logging): Log user id instead of user name
    User id is more useful for metrics

--- src/sentry/api/base.py
+++ src/sentry/api/base.py
@@ -173,7 +173,7 @@ class Endpoint(APIView):
                 method=self.request.method,
                 view=f"{view_obj.__module__}.{view_obj.__class__.__name__}",
                 response=self.response.status_code,
-                user=str(self.request.user),
+                user=str(self.request.user.id),
                 token_type=token_name,
             )
             api_access_logger.info("api.access", extra=log_metrics)
