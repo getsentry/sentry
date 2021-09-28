35f0e0c76b67916e377c714a41ca90548a9b89a3
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-09-28 10:36:08 -0700

    Clean up tests

--- tests/sentry/integrations/vsts/test_webhooks.py
+++ tests/sentry/integrations/vsts/test_webhooks.py
@@ -167,13 +167,12 @@ class VstsWebhookWorkItemTest(APITestCase):
                 data=work_item,
                 HTTP_SHARED_SECRET=self.shared_secret,
             )
-            assert resp.status_code == 200
-            group_ids = [g.id for g in groups]
-            assert (
-                len(Group.objects.filter(id__in=group_ids, status=GroupStatus.RESOLVED))
-                == num_groups
-            )
-            assert len(Activity.objects.filter(group_id__in=group_ids)) == num_groups
+        assert resp.status_code == 200
+        group_ids = [g.id for g in groups]
+        assert (
+            len(Group.objects.filter(id__in=group_ids, status=GroupStatus.RESOLVED)) == num_groups
+        )
+        assert len(Activity.objects.filter(group_id__in=group_ids)) == num_groups
 
     @responses.activate
     def test_inbound_status_sync_unresolve(self):
@@ -201,13 +200,12 @@ class VstsWebhookWorkItemTest(APITestCase):
                 data=work_item,
                 HTTP_SHARED_SECRET=self.shared_secret,
             )
-            assert resp.status_code == 200
-            group_ids = [g.id for g in groups]
-            assert (
-                len(Group.objects.filter(id__in=group_ids, status=GroupStatus.UNRESOLVED))
-                == num_groups
-            )
-            assert len(Activity.objects.filter(group_id__in=group_ids)) == num_groups
+        assert resp.status_code == 200
+        group_ids = [g.id for g in groups]
+        assert (
+            len(Group.objects.filter(id__in=group_ids, status=GroupStatus.UNRESOLVED)) == num_groups
+        )
+        assert len(Activity.objects.filter(group_id__in=group_ids)) == num_groups
 
     @responses.activate
     def test_inbound_status_sync_new_workitem(self):
