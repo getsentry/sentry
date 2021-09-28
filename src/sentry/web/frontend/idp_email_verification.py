81c40f5ddc95b0fb99b45c131edbe4d350e27f2f
Richard Ma <maxiuyuan1998@gmail.com>
2021-09-28 14:30:34 -0700

    removed IDPEmailVerificationView

--- src/sentry/web/frontend/idp_email_verification.py
+++ src/sentry/web/frontend/idp_email_verification.py
@@ -6,14 +6,13 @@ from sentry.auth.idpmigration import get_redis_key, verify_account
 from sentry.utils.auth import get_login_url
 
 
-class IDPEmailVerificationView:
-    def confirm_email(request: Request, key: str) -> Response:
-        verification_key = get_redis_key(key)
-        if verify_account(key):
-            request.session["confirm_account_verification_key"] = verification_key
-            # TODO Change so it redirects to a confirmation page that needs to be made: Simple page with a confirmation msg and redirect to login
-            login = get_login_url()
-            redirect = HttpResponseRedirect(login)
-            return redirect
-        # TODO add view that shows key not found
-        return HttpResponseNotFound()
+def idp_confirm_email(request: Request, key: str) -> Response:
+    verification_key = get_redis_key(key)
+    if verify_account(key):
+        request.session["confirm_account_verification_key"] = verification_key
+        # TODO Change so it redirects to a confirmation page that needs to be made: Simple page with a confirmation msg and redirect to login
+        login = get_login_url()
+        redirect = HttpResponseRedirect(login)
+        return redirect
+    # TODO add view that shows key not found
+    return HttpResponseNotFound()
