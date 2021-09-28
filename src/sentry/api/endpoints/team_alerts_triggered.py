f3bfa6a58934c45473286adda3d53f5b0c551df8
Chris Fuller <cfuller@sentry.io>
2021-09-28 16:24:39 -0400

    Updating query

--- src/sentry/api/endpoints/team_alerts_triggered.py
+++ src/sentry/api/endpoints/team_alerts_triggered.py
@@ -1,11 +1,16 @@
-from django.db.models import Count
+from django.db.models import Count, Q
 from django.db.models.functions import TruncDay
 from rest_framework.response import Response
 
 from sentry.api.base import EnvironmentMixin
 from sentry.api.bases.team import TeamEndpoint
 from sentry.api.utils import get_date_range_from_params
-from sentry.incidents.models import IncidentActivity, IncidentActivityType, IncidentStatus
+from sentry.incidents.models import (
+    IncidentActivity,
+    IncidentActivityType,
+    IncidentProject,
+    IncidentStatus,
+)
 from sentry.models import Project
 
 
@@ -17,20 +22,24 @@ class TeamAlertsTriggeredEndpoint(TeamEndpoint, EnvironmentMixin):
         project_list = Project.objects.get_for_team_ids([team.id])
         owner_ids = [team.actor_id] + [om.user.actor_id for om in team.member_set]
         start, end = get_date_range_from_params(request.GET)
-        bucketed_alert_counts = list(
+        bucketed_alert_counts = (
             IncidentActivity.objects.filter(
-                type__in=[
-                    IncidentActivityType.CREATED.value,
-                    IncidentActivityType.STATUS_CHANGE.value,
-                ],
-                value__in=[
-                    None,
-                    IncidentStatus.OPEN,
-                    IncidentStatus.CRITICAL,
-                    IncidentStatus.WARNING,
-                ],
+                (
+                    Q(type=IncidentActivityType.CREATED.value)
+                    | Q(
+                        type=IncidentActivityType.STATUS_CHANGE.value,
+                        value__in=[
+                            None,
+                            IncidentStatus.OPEN,
+                            IncidentStatus.CRITICAL,
+                            IncidentStatus.WARNING,
+                        ],
+                    )
+                ),
                 incident__alert_rule__owner__in=owner_ids,
-                incident__projects__in=project_list,
+                incident_id__in=IncidentProject.objects.filter(project__in=project_list).values(
+                    "incident_id"
+                ),
                 date_added__gte=start,
                 date_added__lte=end,
             )
@@ -39,4 +48,4 @@ class TeamAlertsTriggeredEndpoint(TeamEndpoint, EnvironmentMixin):
             .annotate(count=Count("id"))
         )
 
-        return Response(bucketed_alert_counts)
+        return Response(list(bucketed_alert_counts))
--- tests/sentry/api/endpoints/test_team_alerts_triggered.py
+++ tests/sentry/api/endpoints/test_team_alerts_triggered.py
@@ -3,8 +3,6 @@ from sentry.incidents.models import (
     IncidentActivity,
     IncidentActivityType,
     IncidentStatus,
-    IncidentTrigger,
-    TriggerStatus,
 )
 from sentry.models import ActorTuple
 from sentry.testutils import APITestCase
@@ -29,42 +27,32 @@ class TeamAlertsTriggeredTest(APITestCase):
             owner=ActorTuple.from_actor_identifier(self.user.id),
         )
         user_owned_incident = self.create_incident(status=20, alert_rule=user_owned_rule)
-        for i in range(1, 31):
-            trigger = self.create_alert_rule_trigger(user_owned_rule, i, 200)
-            trigger2 = self.create_alert_rule_trigger(user_owned_rule, -i, 200)
-            IncidentTrigger.objects.create(
-                incident=user_owned_incident,
-                alert_rule_trigger=trigger,
-                status=TriggerStatus.ACTIVE.value,
-                date_added=before_now(days=i),
+        activities = []
+        for i in range(1, 9):
+            activities.append(
+                IncidentActivity(
+                    incident=user_owned_incident,
+                    type=IncidentActivityType.CREATED.value,
+                    value=IncidentStatus.OPEN,
+                    date_added=before_now(days=i),
+                )
             )
-            IncidentTrigger.objects.create(
-                incident=user_owned_incident,
-                alert_rule_trigger=trigger2,
-                status=TriggerStatus.ACTIVE.value,
-                date_added=before_now(days=i),
-            )
-            IncidentActivity.objects.create(
-                incident=user_owned_incident,
-                type=IncidentActivityType.CREATED.value,
-                value=IncidentStatus.OPEN,
-                date_added=before_now(days=i),
-            )
+        IncidentActivity.objects.bulk_create(activities)
 
         self.login_as(user=self.user)
         url = f"/api/0/teams/{self.team.organization.slug}/{self.team.slug}/alerts-triggered/"
         response = self.client.get(url, format="json")
 
         assert response.status_code == 200
-        assert len(response.data) == 30
+        assert len(response.data) == 8
         for i in response.data:
             assert i["count"] == 1
 
-        url = f"/api/0/teams/{self.team.organization.slug}/{self.team.slug}/alerts-triggered/?statsPeriod=14d"
+        url = f"/api/0/teams/{self.team.organization.slug}/{self.team.slug}/alerts-triggered/?statsPeriod=7d"
         response = self.client.get(url, format="json")
 
         assert response.status_code == 200
-        assert len(response.data) == 13
+        assert len(response.data) == 6
         for i in response.data:
             assert i["count"] == 1
 
@@ -77,12 +65,14 @@ class TeamAlertsTriggeredTest(APITestCase):
             teams=[self.team], slug="foo"
         )  # This project will return counts for this team
         project2 = self.create_project(
-            teams=[team_with_user], slug="bar"
+            # teams=[team_with_user], slug="bar"
+            teams=[team_with_user],
+            slug="bar",
         )  # but not this project, cause this team isn't on it (but the user is)
 
         user_owned_rule = self.create_alert_rule(
             organization=self.organization,
-            projects=[project1],
+            projects=[project2],
             name="user owned rule",
             query="",
             aggregate="count()",
@@ -110,33 +100,6 @@ class TeamAlertsTriggeredTest(APITestCase):
         team_owned_incident = self.create_incident(
             projects=[project1], status=20, alert_rule=team_owned_rule
         )
-
-        trigger = self.create_alert_rule_trigger(team_owned_rule, "hi", 100)
-        trigger2 = self.create_alert_rule_trigger(team_owned_rule, "bye", 50)
-        trigger3 = self.create_alert_rule_trigger(user_owned_rule, "meow", 200)
-        trigger4 = self.create_alert_rule_trigger(user_owned_rule, "woof", 200)
-        IncidentTrigger.objects.create(
-            incident=user_owned_incident,
-            alert_rule_trigger=trigger,
-            status=TriggerStatus.RESOLVED.value,
-        )  # Resolved trigger doesn't get counted.
-        IncidentTrigger.objects.create(
-            incident=user_owned_incident,
-            alert_rule_trigger=trigger2,
-            status=TriggerStatus.ACTIVE.value,
-        )
-        IncidentTrigger.objects.create(
-            incident=team_owned_incident,
-            alert_rule_trigger=trigger3,
-            status=TriggerStatus.ACTIVE.value,
-            date_added=before_now(days=2),
-        )
-        IncidentTrigger.objects.create(
-            incident=user_owned_incident,
-            alert_rule_trigger=trigger4,
-            status=TriggerStatus.ACTIVE.value,
-            date_added=before_now(days=4),
-        )
         IncidentActivity.objects.create(
             incident=user_owned_incident,
             type=IncidentActivityType.CREATED.value,
@@ -155,4 +118,4 @@ class TeamAlertsTriggeredTest(APITestCase):
         assert response.status_code == 200
         assert (
             len(response.data) == 1
-        )  # only getting the team owned incident, because the user owned incident is for another project
+        )  # only getting the team owned incident, because the user owned incident is for another project that the team isn't on
