9fb4316aa0be48ffb789667e1d4892c1d81db9ea
Betty Da <bda@sentry.io>
2021-09-29 19:18:03 -0400

    compute also updates eligibility

--- src/sentry/tasks/low_priority_symbolication.py
+++ src/sentry/tasks/low_priority_symbolication.py
@@ -33,10 +33,10 @@ def _scan_for_suspect_projects() -> None:
 
     for project_id in realtime_metrics_store.projects():
         suspect_projects.add(project_id)
-        compute_lpq_eligibility(project_id).apply_async()
+        update_lpq_eligibility(project_id).apply_async()
 
     # Prune projects we definitely know shouldn't be in the queue any more.
-    # `compute_lpq_eligibility` should handle removing suspect projects from the list if it turns
+    # `update_lpq_eligibility` should handle removing suspect projects from the list if it turns
     # out they need to be evicted.
     current_lpq_projects = realtime_metrics_store.get_lpq_projects() or set()
     deleted_projects = current_lpq_projects.difference(suspect_projects)
@@ -57,20 +57,20 @@ def _scan_for_suspect_projects() -> None:
 
 
 @instrumented_task(
-    name="sentry.tasks.symbolicator.compute_lpq_eligibility",
+    name="sentry.tasks.symbolicator.update_lpq_eligibility",
     queue="symbolications.compute_low_priority_projects",
     ignore_result=True,
     soft_time_limit=10,
 )  # type: ignore
-def compute_lpq_eligibility(project_id: int) -> None:
+def update_lpq_eligibility(project_id: int) -> None:
     """
     Given a project ID, determines whether the project belongs in the low priority queue and
     removes or assigns it accordingly to the low priority queue.
     """
-    _compute_lpq_eligibility(project_id)
+    _update_lpq_eligibility(project_id)
 
 
-def _compute_lpq_eligibility(project_id: int) -> None:
+def _update_lpq_eligibility(project_id: int) -> None:
     counts = realtime_metrics_store.get_counts_for_project(project_id)
     durations = realtime_metrics_store.get_durations_for_project(project_id)
 
