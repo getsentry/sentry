42a57f78033029a30630bd927fbf6ebd914fd155
Chris Fuller <cfuller@sentry.io>
2021-09-28 20:32:16 -0400

    Zerofilling and new test

--- src/sentry/api/endpoints/team_release_count.py
+++ src/sentry/api/endpoints/team_release_count.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from datetime import timedelta
 
 from django.db.models import Count
 from django.db.models.functions import TruncDay
@@ -7,7 +8,7 @@ from rest_framework.response import Response
 from sentry.api.base import EnvironmentMixin
 from sentry.api.bases.team import TeamEndpoint
 from sentry.api.utils import get_date_range_from_params
-from sentry.models import Project, Release
+from sentry.models import Project, Release, ReleaseProject
 
 
 class TeamReleaseCountEndpoint(TeamEndpoint, EnvironmentMixin):
@@ -20,18 +21,32 @@ class TeamReleaseCountEndpoint(TeamEndpoint, EnvironmentMixin):
 
         bucketed_releases = (
             Release.objects.filter(
-                projects__in=project_list, date_added__gte=start, date_added__lte=end
+                id__in=ReleaseProject.objects.filter(project__in=project_list).values("release_id"),
+                date_added__gte=start,
+                date_added__lte=end,
             )
             .distinct()
             .annotate(bucket=TruncDay("date_added"))
+            .order_by("bucket")
             .values("projects", "bucket")
             .annotate(count=Count("id"))
         )
 
-        agg_project_counts = defaultdict(list)
+        agg_project_counts = defaultdict(dict)
         for bucket in bucketed_releases:
-            agg_project_counts[bucket["projects"]].append(
-                {"bucket": bucket["bucket"], "count": bucket["count"]}
-            )
+            agg_project_counts[bucket["projects"]][
+                str(bucket["bucket"].replace(tzinfo=None))
+            ] = bucket["count"]
+
+        current_day = start.replace(
+            hour=0, minute=0, second=0, microsecond=0, tzinfo=None
+        ) + timedelta(days=1)
+        end_day = end.replace(hour=0, minute=0, second=0, microsecond=0, tzinfo=None)
+        while current_day <= end_day:
+            key = str(current_day)
+            for project_id in list(agg_project_counts.keys()):
+                if key not in agg_project_counts[project_id]:
+                    agg_project_counts[project_id][key] = 0
+            current_day += timedelta(days=1)
 
         return Response(agg_project_counts)
--- tests/sentry/api/endpoints/test_team_release_count.py
+++ tests/sentry/api/endpoints/test_team_release_count.py
@@ -1,5 +1,3 @@
-from django.utils import timezone
-
 from sentry.models import Release
 from sentry.testutils import APITestCase
 from sentry.testutils.helpers.datetime import before_now
@@ -51,19 +49,129 @@ class TeamReleaseCountTest(APITestCase):
         )
         release5.add_project(project3)
         response = self.get_valid_response(org.slug, team1.slug)
-
-        assert response.status_code == 200
         assert len(response.data) == 2
         assert project2.id not in response.data
-        assert response.data[project3.id][0]["count"] == 2
-        assert response.data[project3.id][0]["bucket"] == before_now(days=5).replace(
-            hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc
+        assert len(response.data[project3.id]) == 90
+        assert (
+            response.data[project3.id][
+                str(before_now(days=5).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 2
         )
-        assert response.data[project1.id][0]["count"] == 1
-        assert response.data[project1.id][0]["bucket"] == before_now(days=15).replace(
-            hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc
+        assert (
+            response.data[project3.id][
+                str(before_now(days=0).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 0
         )
-        assert response.data[project1.id][1]["count"] == 1
-        assert response.data[project1.id][1]["bucket"] == before_now(days=10).replace(
-            hour=0, minute=0, second=0, microsecond=0, tzinfo=timezone.utc
+
+        assert (
+            response.data[project1.id][
+                str(before_now(days=15).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 1
+        )
+        assert (
+            response.data[project1.id][
+                str(before_now(days=10).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 1
+        )
+
+    def test_multi_project_release(self):
+        user = self.create_user(is_staff=False, is_superuser=False)
+        org = self.organization
+        org2 = self.create_organization()
+        org.flags.allow_joinleave = False
+        org.save()
+
+        team1 = self.create_team(organization=org)
+        team2 = self.create_team(organization=org)
+
+        project1 = self.create_project(teams=[team1], organization=org)
+        project2 = self.create_project(teams=[team2], organization=org2)
+        project3 = self.create_project(teams=[team1], organization=org)
+
+        self.create_member(teams=[team1], user=user, organization=org)
+        self.login_as(user=user)
+        release1 = Release.objects.create(
+            organization_id=org.id, version="1", date_added=before_now(days=15)
+        )
+        release1.add_project(project1)
+        release1.add_project(project3)
+
+        release2 = Release.objects.create(
+            organization_id=org2.id, version="2", date_added=before_now(days=12)
+        )  # This release isn't returned, its in another org
+        release2.add_project(project2)
+
+        release3 = Release.objects.create(
+            organization_id=org.id,
+            version="3",
+            date_added=before_now(days=10),
+            date_released=before_now(days=10),
+        )
+        release3.add_project(project1)
+
+        release4 = Release.objects.create(
+            organization_id=org.id, version="4", date_added=before_now(days=5)
+        )
+        release4.add_project(project3)
+        release5 = Release.objects.create(
+            organization_id=org.id, version="5", date_added=before_now(days=5)
+        )
+        release5.add_project(project3)
+        response = self.get_valid_response(org.slug, team1.slug)
+        assert len(response.data) == 2
+        assert project2.id not in response.data
+        assert (
+            response.data[project3.id][
+                str(before_now(days=15).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 1
+        )
+        assert (
+            response.data[project3.id][
+                str(before_now(days=2).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 0
+        )
+
+        assert (
+            response.data[project3.id][
+                str(before_now(days=5).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 2
+        )
+        assert (
+            response.data[project3.id][
+                str(before_now(days=0).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 0
+        )
+
+        assert (
+            response.data[project1.id][
+                str(before_now(days=15).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 1
+        )
+        assert (
+            response.data[project1.id][
+                str(before_now(days=5).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 0
+        )
+
+        assert (
+            response.data[project1.id][
+                str(before_now(days=10).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 1
+        )
+        assert (
+            response.data[project1.id][
+                str(before_now(days=0).replace(hour=0, minute=0, second=0, microsecond=0))
+            ]
+            == 0
         )
