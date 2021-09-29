139e06e2c62fa88fc92ff6a0f2d2efd92b50eb91
Kev <kevan.fisher@sentry.io>
2021-09-29 08:10:37 -0700

    feat(perf): Generic landing widgets (all transactions tab) (#28861)
    * feat(perf): Generic landing widgets
    
    This adds some initial generic landing widgets for the 'all transactions' tab of the new performance landing pages. It introduces a widget container component, which has widget definitions. These definitions will use a specialization of the genericPerformanceWidget (eg. singleFieldAreaWidget). Widgets are split into their queries and visualizations (such as charts and tables), so that data can be fetched or batched later independently of rendering ththe representation. For queries, all queries go through a QueryHandler which sets data from the queries onto a widgetData object which is available throughout the GenericPerformanceWidget. The query handler is currently designed to handle our waterfall style component apis as well as run transform functions to normalize the results into something the generic performance widget can use to switch between widget states.
    
    The query-handler should be somewhat temporary until we have a path to move away from waterfall queries or we decide to remake all our queries functionally.
    
    There is some memoization currently required in the specializations for the performance widget which should be able to be removed once I finish making the remaining widgets and can see what the final component api looks like.

---
+++ static/app/views/performance/landing/widgets/widgets/singleFieldAreaWidget.tsx
@@ -0,0 +1,107 @@
+import {Fragment, FunctionComponent, useMemo} from 'react';
+import {withRouter} from 'react-router';
+import styled from '@emotion/styled';
+import {Location} from 'history';
+
+import _EventsRequest from 'app/components/charts/eventsRequest';
+import {t} from 'app/locale';
+import {Organization} from 'app/types';
+import EventView from 'app/utils/discover/eventView';
+import withApi from 'app/utils/withApi';
+import _DurationChart from 'app/views/performance/charts/chart';
+
+import {GenericPerformanceWidget} from '../components/performanceWidget';
+import {transformEventsRequestToArea} from '../transforms/transformEventsToArea';
+import {WidgetDataResult} from '../types';
+
+type Props = {
+  title: string;
+  titleTooltip: string;
+  fields: string[];
+  chartColor?: string;
+
+  eventView: EventView;
+  location: Location;
+  organization: Organization;
+
+  ContainerActions: FunctionComponent<{isLoading: boolean}>;
+};
+
+type AreaDataType = {
+  chart: WidgetDataResult & ReturnType<typeof transformEventsRequestToArea>;
+};
+
+export function SingleFieldAreaWidget(props: Props) {
+  const {ContainerActions} = props;
+  const globalSelection = props.eventView.getGlobalSelection();
+
+  if (props.fields.length !== 1) {
+    throw new Error(`Single field area can only accept a single field (${props.fields})`);
+  }
+
+  const Queries = useMemo(() => {
+    return {
+      chart: {
+        fields: props.fields[0],
+        component: provided => (
+          <EventsRequest
+            {...provided}
+            limit={1}
+            includePrevious
+            includeTransformedData
+            partial
+            currentSeriesName={props.fields[0]}
+            query={props.eventView.getQueryWithAdditionalConditions()}
+          />
+        ),
+        transform: transformEventsRequestToArea,
+      },
+    };
+  }, [props.eventView, props.fields, props.organization.slug]);
+
+  return (
+    <GenericPerformanceWidget<AreaDataType>
+      {...props}
+      subtitle={
+        <Subtitle>{t('Compared to last %s ', globalSelection.datetime.period)}</Subtitle>
+      }
+      HeaderActions={provided => (
+        <Fragment>
+          <HighlightNumber color={props.chartColor}>
+            {provided.widgetData.chart?.hasData
+              ? provided.widgetData.chart?.dataMean?.[0].label
+              : null}
+          </HighlightNumber>
+          <ContainerActions {...provided.widgetData.chart} />
+        </Fragment>
+      )}
+      Queries={Queries}
+      Visualizations={[
+        {
+          component: provided => (
+            <DurationChart
+              {...provided.widgetData.chart}
+              {...provided}
+              disableMultiAxis
+              disableXAxis
+              chartColors={props.chartColor ? [props.chartColor] : undefined}
+            />
+          ),
+          height: 160,
+        },
+      ]}
+    />
+  );
+}
+
+const EventsRequest = withApi(_EventsRequest);
+const DurationChart = withRouter(_DurationChart);
+const Subtitle = styled('span')`
+  color: ${p => p.theme.gray300};
+  font-size: ${p => p.theme.fontSizeMedium};
+`;
+
+const HighlightNumber = styled('div')<{color?: string}>`
+  color: ${p => p.color};
+  font-size: ${p => p.theme.fontSizeExtraLarge};
+`;
