1de8f160793e77ab01b5de910a9a89a086454c57
Chris Fuller <cfuller@sentry.io>
2021-09-29 20:55:00 -0400

    issue status breakdown api

---
+++ src/sentry/api/endpoints/team_issue_breakdown.py
@@ -0,0 +1,53 @@
+import copy
+from datetime import timedelta
+
+from django.db.models import Count
+from django.db.models.functions import TruncDay
+from rest_framework.response import Response
+
+from sentry.api.base import EnvironmentMixin
+from sentry.api.bases.team import TeamEndpoint
+from sentry.api.utils import get_date_range_from_params
+from sentry.models import GroupHistory, GroupHistoryStatus, Project
+from sentry.models.grouphistory import ACTIONED_STATUSES
+
+
+class TeamIssueBreakdownEndpoint(TeamEndpoint, EnvironmentMixin):
+    def get(self, request, team):
+        """
+        Returns a dict of team projects, and a time-series dict of issue stat breakdowns for each.
+
+        Right now the stats we return are the count of reviewed issues and the total count of issues.
+        """
+        project_list = Project.objects.get_for_team_ids(team_ids=[team.id])
+        start, end = get_date_range_from_params(request.GET)
+        end = end.date() + timedelta(days=1)
+        start = start.date() + timedelta(days=1)
+        bucketed_issues = (
+            GroupHistory.objects.filter(
+                status__in=[GroupHistoryStatus.UNRESOLVED] + ACTIONED_STATUSES,
+                project__in=project_list,
+                date_added__gte=start,
+                date_added__lte=end,
+            )
+            .annotate(bucket=TruncDay("date_added"))
+            .order_by("bucket")
+            .values("project", "bucket", "status")
+            .annotate(count=Count("id"))
+        )
+
+        current_day, date_series_dict = start, {}
+        while current_day < end:
+            date_series_dict[str(current_day)] = {"reviewed": 0, "total": 0}
+            current_day += timedelta(days=1)
+
+        agg_project_counts = {
+            project.id: copy.deepcopy(date_series_dict) for project in project_list
+        }
+        for r in bucketed_issues:
+            date = str(r["bucket"].date())
+            agg_project_counts[r["project"]][date]["total"] += r["count"]
+            if r["status"] != GroupHistoryStatus.UNRESOLVED:
+                agg_project_counts[r["project"]][date]["reviewed"] += r["count"]
+
+        return Response(agg_project_counts)
---
+++ tests/sentry/api/endpoints/test_issue_breakdown.py
@@ -0,0 +1,170 @@
+from datetime import timedelta
+
+from django.utils.timezone import now
+from freezegun import freeze_time
+
+from sentry.models import GroupHistory, GroupHistoryStatus
+from sentry.testutils import APITestCase
+from sentry.testutils.helpers.datetime import before_now
+
+
+@freeze_time()
+class TeamIssueBreakdownTest(APITestCase):
+    endpoint = "sentry-api-0-team-issue-breakdown"
+
+    def test_simple(self):
+        project1 = self.create_project(teams=[self.team], slug="foo")
+        project2 = self.create_project(teams=[self.team], slug="bar")
+        group1 = self.create_group(checksum="a" * 32, project=project1, times_seen=10)
+        group2 = self.create_group(checksum="b" * 32, project=project2, times_seen=5)
+
+        gh1 = GroupHistory.objects.create(
+            organization=self.organization,
+            group=group1,
+            project=project1,
+            actor=self.user.actor,
+            date_added=before_now(days=5),
+            status=GroupHistoryStatus.UNRESOLVED,
+            prev_history=None,
+            prev_history_date=None,
+        )
+
+        GroupHistory.objects.create(
+            organization=self.organization,
+            group=group1,
+            project=project1,
+            actor=self.user.actor,
+            status=GroupHistoryStatus.RESOLVED,
+            prev_history=gh1,
+            prev_history_date=gh1.date_added,
+            date_added=before_now(days=2),
+        )
+
+        GroupHistory.objects.create(
+            organization=self.organization,
+            group=group1,
+            project=project1,
+            actor=self.user.actor,
+            status=GroupHistoryStatus.REGRESSED,
+            prev_history=gh1,
+            prev_history_date=gh1.date_added,
+            date_added=before_now(days=2),
+        )
+
+        gh2 = GroupHistory.objects.create(
+            organization=self.organization,
+            group=group2,
+            project=project2,
+            actor=self.user.actor,
+            date_added=before_now(days=10),
+            status=GroupHistoryStatus.UNRESOLVED,
+            prev_history=None,
+            prev_history_date=None,
+        )
+        gh2 = GroupHistory.objects.create(
+            organization=self.organization,
+            group=group2,
+            project=project2,
+            actor=self.user.actor,
+            date_added=before_now(days=1),
+            status=GroupHistoryStatus.UNRESOLVED,
+            prev_history=None,
+            prev_history_date=None,
+        )
+
+        GroupHistory.objects.create(
+            organization=self.organization,
+            group=group2,
+            project=project2,
+            actor=self.user.actor,
+            status=GroupHistoryStatus.RESOLVED,
+            prev_history=gh2,
+            prev_history_date=gh2.date_added,
+        )
+        GroupHistory.objects.create(
+            organization=self.organization,
+            group=group2,
+            project=project2,
+            actor=self.user.actor,
+            status=GroupHistoryStatus.RESOLVED,
+            prev_history=gh2,
+            prev_history_date=gh2.date_added,
+        )
+        GroupHistory.objects.create(
+            organization=self.organization,
+            group=group2,
+            project=project2,
+            actor=self.user.actor,
+            status=GroupHistoryStatus.IGNORED,
+            prev_history=gh2,
+            prev_history_date=gh2.date_added,
+        )
+        today = str(now().date())
+        yesterday = str((now() - timedelta(days=1)).date())
+        two_days_ago = str((now() - timedelta(days=2)).date())
+        self.login_as(user=self.user)
+        response = self.get_success_response(
+            self.team.organization.slug, self.team.slug, statsPeriod="7d"
+        )
+        assert len(response.data) == 2
+        assert response.data[project1.id][today]["reviewed"] == 0
+        assert response.data[project1.id][today]["total"] == 0
+        assert response.data[project1.id][yesterday]["reviewed"] == 0
+        assert response.data[project1.id][yesterday]["total"] == 0
+        assert response.data[project1.id][two_days_ago]["reviewed"] == 1
+        assert response.data[project1.id][two_days_ago]["reviewed"] == 1
+
+        assert response.data[project2.id][today]["reviewed"] == 3
+        assert response.data[project2.id][today]["total"] == 3
+        assert response.data[project2.id][yesterday]["reviewed"] == 0
+        assert response.data[project2.id][yesterday]["total"] == 1
+        assert response.data[project2.id][two_days_ago]["reviewed"] == 0
+        assert response.data[project2.id][two_days_ago]["total"] == 0
+
+        gh2 = GroupHistory.objects.create(
+            organization=self.organization,
+            group=group1,
+            project=project1,
+            actor=self.user.actor,
+            date_added=before_now(days=1),
+            status=GroupHistoryStatus.UNRESOLVED,
+            prev_history=None,
+            prev_history_date=None,
+        )
+        GroupHistory.objects.create(
+            organization=self.organization,
+            group=group2,
+            project=project2,
+            actor=self.user.actor,
+            status=GroupHistoryStatus.RESOLVED,
+            prev_history=gh2,
+            prev_history_date=gh2.date_added,
+        )
+
+        # making sure it doesnt bork anything
+        GroupHistory.objects.create(
+            organization=self.organization,
+            group=group2,
+            project=project2,
+            actor=self.user.actor,
+            status=GroupHistoryStatus.ASSIGNED,
+            prev_history=gh2,
+            prev_history_date=gh2.date_added,
+        )
+
+        response = self.get_success_response(self.team.organization.slug, self.team.slug)
+        assert len(response.data) == 2
+
+        assert response.data[project1.id][today]["reviewed"] == 0
+        assert response.data[project1.id][today]["total"] == 0
+        assert response.data[project1.id][yesterday]["reviewed"] == 0
+        assert response.data[project1.id][yesterday]["total"] == 1
+        assert response.data[project1.id][two_days_ago]["reviewed"] == 1
+        assert response.data[project1.id][two_days_ago]["reviewed"] == 1
+
+        assert response.data[project2.id][today]["reviewed"] == 4
+        assert response.data[project2.id][today]["total"] == 4
+        assert response.data[project2.id][yesterday]["reviewed"] == 0
+        assert response.data[project2.id][yesterday]["total"] == 1
+        assert response.data[project2.id][two_days_ago]["reviewed"] == 0
+        assert response.data[project2.id][two_days_ago]["total"] == 0
