getsentry/sentry

commit 2cd05e28edf1b8e8f326d9a72b6d4b76e815f489

ref(tasks): Safeguard post_process_group with try/except (#28986)

src/sentry/tasks/post_process.py
@@ -274,21 +274,30 @@ def post_process_group(
         _capture_stats(event, is_new)
 
         with sentry_sdk.start_span(op="tasks.post_process_group.add_group_to_inbox"):
-            if is_reprocessed and is_new:
-                add_group_to_inbox(event.group, GroupInboxReason.REPROCESSED)
+            try:
+                if is_reprocessed and is_new:
+                    add_group_to_inbox(event.group, GroupInboxReason.REPROCESSED)
+            except Exception:
+                logger.exception("Failed to add group to inbox for reprocessed groups")
 
         if not is_reprocessed:
             # we process snoozes before rules as it might create a regression
             # but not if it's new because you can't immediately snooze a new group
-            has_reappeared = False if is_new else process_snoozes(event.group)
-            if not has_reappeared:  # If true, we added the .UNIGNORED reason already
-                if is_new:
-                    add_group_to_inbox(event.group, GroupInboxReason.NEW)
-                elif is_regression:
-                    add_group_to_inbox(event.group, GroupInboxReason.REGRESSION)
+            try:
+                has_reappeared = False if is_new else process_snoozes(event.group)
+                if not has_reappeared:  # If true, we added the .UNIGNORED reason already
+                    if is_new:
+                        add_group_to_inbox(event.group, GroupInboxReason.NEW)
+                    elif is_regression:
+                        add_group_to_inbox(event.group, GroupInboxReason.REGRESSION)
+            except Exception:
+                logger.exception("Failed to add group to inbox for non-reprocessed groups")
 
             with sentry_sdk.start_span(op="tasks.post_process_group.handle_owner_assignment"):
-                handle_owner_assignment(event.project, event.group, event)
+                try:
+                    handle_owner_assignment(event.project, event.group, event)
+                except Exception:
+                    logger.exception("Failed to handle owner assignments")
 
             rp = RuleProcessor(
                 event, is_new, is_regression, is_new_group_environment, has_reappeared
@@ -376,7 +385,10 @@ def post_process_group(
 
         # Patch attachments that were ingested on the standalone path.
         with sentry_sdk.start_span(op="tasks.post_process_group.update_existing_attachments"):
-            update_existing_attachments(event)
+            try:
+                update_existing_attachments(event)
+            except Exception:
+                logger.exception("Failed to update existing attachments")
 
         if not is_reprocessed:
             event_processed.send_robust(

commit 3524c11b0e04d199c729d92b6f217e3ec3d3a2af

fix(post_process_group): Fix bug where `has_reappared` can be undefined
This was introduced in https://github.com/getsentry/sentry/pull/28986. If an exception occurs when
calling `process_snoozes` we never end up defining `has_reappeared`, which causes an error later on.

I tried to find instances of `Failed to add group to inbox for non-reprocessed groups` errors in
Sentry and couldn't. I'm not sure why they're not present, but I can't see any other way that
`has_reappeared` could be undefined.

Fixes SENTRY-SAR

src/sentry/tasks/post_process.py
@@ -283,8 +283,14 @@ def post_process_group(
         if not is_reprocessed:
             # we process snoozes before rules as it might create a regression
             # but not if it's new because you can't immediately snooze a new group
+            has_reappeared = not is_new
             try:
-                has_reappeared = False if is_new else process_snoozes(event.group)
+                if has_reappeared:
+                    has_reappeared = process_snoozes(event.group)
+            except Exception:
+                logger.exception("Failed to process snoozes for group")
+
+            try:
                 if not has_reappeared:  # If true, we added the .UNIGNORED reason already
                     if is_new:
                         add_group_to_inbox(event.group, GroupInboxReason.NEW)