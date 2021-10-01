5b26ee2be36a4a688afe8e555d5f23e5ebec1c71
relaxolotl <5597345+relaxolotl@users.noreply.github.com>
2021-10-01 18:54:13 -0400

    feat(symbolicator): Add the ability to manipulate the automatic killswitch for the LPQ (#28757)
    This adds in methods to RealtimeMetricsStore to fetch, add,
    and remove projects from the automatically-managed list 
    stored on redis. This list specifies which projects should be 
    sent to symbolicator's low priority queue.

--- src/sentry/processing/realtime_metrics/base.py
+++ src/sentry/processing/realtime_metrics/base.py
@@ -1,10 +1,19 @@
+from typing import Set
+
 from sentry.utils.services import Service
 
 
 class RealtimeMetricsStore(Service):  # type: ignore
     """A service for storing metrics about incoming requests within a given time window."""
 
-    __all__ = ("increment_project_event_counter", "increment_project_duration_counter", "validate")
+    __all__ = (
+        "increment_project_event_counter",
+        "increment_project_duration_counter",
+        "validate",
+        "get_lpq_projects",
+        "add_project_to_lpq",
+        "remove_projects_from_lpq",
+    )
 
     def increment_project_event_counter(self, project_id: int, timestamp: int) -> None:
         """Increment the event counter for the given project_id.
@@ -24,5 +33,33 @@ class RealtimeMetricsStore(Service):  # type: ignore
         The counter is used to track the processing time of events for the project.
         Calling this increments the counter of the current time-window bucket with "timestamp" providing
         the time of the event in seconds since the UNIX epoch and "duration" the processing time in seconds.
+        """
+        pass
+
+    def get_lpq_projects(self) -> Set[int]:
+        """
+        Fetches the list of projects that are currently using the low priority queue.
+
+        Returns a list of project IDs.
+        """
+        pass
+
+    def add_project_to_lpq(self, project_id: int) -> None:
+        """
+        Moves a project to the low priority queue.
+
+        This forces all symbolication events triggered by the specified project to be redirected to
+        the low priority queue, unless the project is manually excluded from the low priority queue
+        via the `store.symbolicate-event-lpq-never` kill switch.
+        """
+        pass
+
+    def remove_projects_from_lpq(self, project_ids: Set[int]) -> None:
+        """
+        Removes projects from the low priority queue.
+
+        This restores all specified projects back to the regular queue, unless they have been
+        manually forced into the low priority queue via the `store.symbolicate-event-lpq-always`
+        kill switch.
         """
         pass
