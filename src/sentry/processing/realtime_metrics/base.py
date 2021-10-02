a866823974f882331d0c26a268d80b4cbdb6fbde
relaxolotl <5597345+relaxolotl@users.noreply.github.com>
2021-10-01 20:31:10 -0400

    feat(symbolicator): Automatically add and remove projects to the LPQ based on their historical perf (#28714)
    Adds methods to the realtime metrics store to fetch
    the metrics it is collecting. Usages of these methods
    are also introduced in this change: periodic tasks
    which assign or remove projects from symbolicator's 
    low priority queue are also included.

--- src/sentry/processing/realtime_metrics/base.py
+++ src/sentry/processing/realtime_metrics/base.py
@@ -1,6 +1,36 @@
-from typing import Set
+import dataclasses
+from typing import Dict, Iterable, NewType, Set
 
 from sentry.utils.services import Service
+
+
+@dataclasses.dataclass(frozen=True)
+class BucketedCount:
+    """
+    Timestamp to count mapping. This represents some `count` amount of something performed
+    during `timestamp`. `timestamp` is stored in seconds.
+    """
+
+    timestamp: int
+    count: int
+
+
+# Duration to count mapping where the keys are durations and the values are counts. This represents
+# some `count` instances of some action where each individual instance some
+# [`duration`, `duration`+10) seconds of time to complete. `duration` is stored in seconds.
+BucketedDurations = NewType("BucketedDurations", Dict[int, int])
+
+
+@dataclasses.dataclass(frozen=True)
+class DurationHistogram:
+    """
+    Mapping of timestamp to histogram-like dict of durations. This represents some `count` amount of
+    some action performed during `timestamp`, where `counts` are grouped by how long that action
+    took. `timestamp` is stored in seconds.
+    """
+
+    timestamp: int
+    histogram: BucketedDurations
 
 
 class RealtimeMetricsStore(Service):  # type: ignore
@@ -23,7 +53,7 @@ class RealtimeMetricsStore(Service):  # type: ignore
         time-window bucket with "timestamp" providing the time of the event
         in seconds since the UNIX epoch (i.e., as returned by time.time()).
         """
-        pass
+        raise NotImplementedError
 
     def increment_project_duration_counter(
         self, project_id: int, timestamp: int, duration: int
@@ -34,7 +64,27 @@ class RealtimeMetricsStore(Service):  # type: ignore
         Calling this increments the counter of the current time-window bucket with "timestamp" providing
         the time of the event in seconds since the UNIX epoch and "duration" the processing time in seconds.
         """
-        pass
+        raise NotImplementedError
+
+    def projects(self) -> Iterable[int]:
+        """
+        Returns IDs of all projects that should be considered for the low priority queue.
+        """
+        raise NotImplementedError
+
+    def get_counts_for_project(self, project_id: int) -> Iterable[BucketedCount]:
+        """
+        Returns a sorted list of bucketed timestamps paired with the count of symbolicator requests
+        made during that time for some given project.
+        """
+        raise NotImplementedError
+
+    def get_durations_for_project(self, project_id: int) -> Iterable[DurationHistogram]:
+        """
+        Returns a sorted list of bucketed timestamps paired with a dictionary of symbolicator
+        durations grouped in 10 second durations made during that time for some given project.
+        """
+        raise NotImplementedError
 
     def get_lpq_projects(self) -> Set[int]:
         """
@@ -42,24 +92,39 @@ class RealtimeMetricsStore(Service):  # type: ignore
 
         Returns a list of project IDs.
         """
-        pass
+        raise NotImplementedError
 
-    def add_project_to_lpq(self, project_id: int) -> None:
+    def add_project_to_lpq(self, project_id: int) -> bool:
         """
-        Moves a project to the low priority queue.
+        Assigns a project to the low priority queue.
 
-        This forces all symbolication events triggered by the specified project to be redirected to
-        the low priority queue, unless the project is manually excluded from the low priority queue
-        via the `store.symbolicate-event-lpq-never` kill switch.
+        This registers an intent to redirect all symbolication events triggered by the specified
+        project to be redirected to the low priority queue.
+
+        Returns True if the project was a new addition to the list. Returns False if it was already
+        assigned to the low priority queue.
         """
-        pass
+        raise NotImplementedError
 
-    def remove_projects_from_lpq(self, project_ids: Set[int]) -> None:
+    def remove_project_from_lpq(self, project_id: int) -> bool:
+        """
+        Removes a project from the low priority queue.
+
+        This registers an intent to restore all specified projects back to the regular queue.
+
+        Returns True if the project was assigned to the queue prior to its removal. Returns False if
+        it wasn't assigned to the queue to begin with.
+        """
+        raise NotImplementedError
+
+    def remove_projects_from_lpq(self, project_ids: Set[int]) -> int:
         """
         Removes projects from the low priority queue.
 
-        This restores all specified projects back to the regular queue, unless they have been
-        manually forced into the low priority queue via the `store.symbolicate-event-lpq-always`
-        kill switch.
+        This registers an intent to restore all specified projects back to the regular queue.
+
+        Returns the number of projects that were actively removed from the queue. Any projects that
+        were not assigned to the low priority queue to begin with will be omitted from the return
+        value.
         """
-        pass
+        raise NotImplementedError
