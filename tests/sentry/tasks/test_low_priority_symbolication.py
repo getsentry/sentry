d7ce69141b5105a41997375540eb44d07acd7e9c
Sebastian Zivota <loewenheim@users.noreply.github.com>
2021-10-04 14:11:10 +0200

    fix: Erroneous apply_async call (#29032)

--- tests/sentry/tasks/test_low_priority_symbolication.py
+++ tests/sentry/tasks/test_low_priority_symbolication.py
@@ -1,13 +1,11 @@
-import pytest
-
-from sentry.tasks.low_priority_symbolication import calculation_magic
-from sentry.utils import redis
-
-
-@pytest.fixture
-def redis_cluster() -> redis._RedisCluster:
-    return redis.redis_clusters.get("default")
+from sentry.processing import realtime_metrics
+from sentry.tasks.low_priority_symbolication import calculation_magic, scan_for_suspect_projects
 
 
 def test_calculation_magic():
     assert not calculation_magic([], [])
+
+
+def test_scan_for_suspect_projects() -> None:
+    realtime_metrics.increment_project_event_counter(17, 0)
+    scan_for_suspect_projects()
