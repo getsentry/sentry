053db49d2a601e4cff870add9360e7f3a9ffb747
Joshua Li <joshuarli98@gmail.com>
2021-10-04 14:46:10 -0700

    move query-valid-python-version to lib, add sanity check for SENTRY_PYTHON_VERSION mismatch

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -13,6 +13,8 @@ if [ -z "${CI+x}" ]; then
     reset="$(tput sgr0)"
 fi
 
+venv_name=".venv"
+
 # NOTE: This file is sourced in CI across different repos (e.g. snuba),
 # so renaming this file or any functions can break CI!
 
@@ -55,6 +57,27 @@ get-pyenv-version() {
 }
 
 query-valid-python-version() {
+    if [[ -n "$SENTRY_PYTHON_VERSION" ]]; then
+        python_version=$(python3 -V 2>&1 | awk '{print $2}')
+        if [ "$python_version" != "$SENTRY_PYTHON_VERSION" ]; then
+            cat <<EOF
+    ${red}${bold}
+You have explicitly set a non-recommended Python version (${SENTRY_PYTHON_VERSION}),
+but it doesn't match the value of python's version: ${python_version}
+You should create a new ${SENTRY_PYTHON_VERSION} virtualenv by running  "rm -rf ${venv_name} && direnv allow".
+    ${reset}
+EOF
+            return 1
+        fi
+
+        cat <<EOF
+${yellow}${bold}
+You have explicitly set a non-recommended Python version (${SENTRY_PYTHON_VERSION}). You're on your own.
+${reset}
+EOF
+        return 0
+    fi
+
     python_version=$(python3 -V 2>&1 | awk '{print $2}')
     minor=$(echo "${python_version}" | sed 's/[0-9]*\.\([0-9]*\)\.\([0-9]*\)/\1/')
     patch=$(echo "${python_version}" | sed 's/[0-9]*\.\([0-9]*\)\.\([0-9]*\)/\2/')
@@ -62,13 +85,25 @@ query-valid-python-version() {
     # For Apple M1, we only allow 3.8 and at least patch version 10
     if query-apple-m1; then
         if [ "$minor" -ne 8 ] || [ "$patch" -lt 10 ]; then
+            cat <<EOF
+${red}${bold}
+WARNING! You're running a virtualenv with Python ${python_version}.
+On Apple M1 machines, we only support >= 3.8.10 < 3.9.
+Either run "rm -rf ${venv_name} && direnv allow" to
+OR set SENTRY_PYTHON_VERSION=${python_version} to an `.env` file to bypass this check."
+EOF
             return 1
         fi
-    # For everything else, we only allow 3.6
-    elif [ "$minor" -ne 6 ]; then
+    elif [ "$minor" -ne 6 ] && [ "$minor" -ne 8 ]; then
+        cat <<EOF
+${red}${bold}
+WARNING! You're running a virtualenv with Python ${python_version}.
+We only support 3.6 or 3.8.
+Either run "rm -rf ${venv_name} && direnv allow" to
+OR set SENTRY_PYTHON_VERSION=${python_version} to an .env file to bypass this check."
+EOF
         return 1
     fi
-    return 0
 }
 
 sudo-askpass() {
