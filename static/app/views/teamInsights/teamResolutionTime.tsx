dd374e914a1d3b49698c1eceacd32bc83bb2e6cd
Scott Cooper <scttcper@gmail.com>
2021-10-05 13:27:15 -0700

    feat(workflow): Add time to resolution graph

---
+++ static/app/views/teamInsights/teamResolutionTime.tsx
@@ -0,0 +1,123 @@
+import styled from '@emotion/styled';
+import chunk from 'lodash/chunk';
+
+import AsyncComponent from 'app/components/asyncComponent';
+import BarChart from 'app/components/charts/barChart';
+import {DateTimeObject} from 'app/components/charts/utils';
+import LoadingIndicator from 'app/components/loadingIndicator';
+import {getParams} from 'app/components/organizations/globalSelectionHeader/getParams';
+import {t} from 'app/locale';
+import space from 'app/styles/space';
+import {Organization} from 'app/types';
+import {getDuration} from 'app/utils/formatters';
+
+type TimeToResolution = Record<string, {count: number; avg: number}>;
+
+type Props = AsyncComponent['props'] & {
+  organization: Organization;
+  teamSlug: string;
+} & DateTimeObject;
+
+type State = AsyncComponent['state'] & {
+  resolutionTime: TimeToResolution | null;
+};
+
+class TeamResolutionTime extends AsyncComponent<Props, State> {
+  shouldRenderBadRequests = true;
+
+  getDefaultState(): State {
+    return {
+      ...super.getDefaultState(),
+      resolutionTime: null,
+    };
+  }
+
+  getEndpoints(): ReturnType<AsyncComponent['getEndpoints']> {
+    const {organization, start, end, period, utc, teamSlug} = this.props;
+    const datetime = {start, end, period, utc};
+
+    return [
+      [
+        'resolutionTime',
+        `/teams/${organization.slug}/${teamSlug}/time-to-resolution/`,
+        {
+          query: {
+            ...getParams(datetime),
+          },
+        },
+      ],
+    ];
+  }
+
+  renderLoading() {
+    return (
+      <ChartWrapper>
+        <LoadingIndicator />
+      </ChartWrapper>
+    );
+  }
+
+  renderBody() {
+    const {resolutionTime} = this.state;
+    const data = Object.entries(resolutionTime ?? {})
+      .map(([bucket, {avg}]) => ({
+        value: avg,
+        name: new Date(bucket).getTime(),
+      }))
+      .sort((a, b) => a.name - b.name);
+
+    // Convert from days to 7 day groups
+    const seriesData = chunk(data, 7).map(week => {
+      return {
+        name: week[0].name,
+        value: week.reduce(
+          (total, currentData) => total + currentData.value + Math.random() * 40000,
+          0
+        ),
+      };
+    });
+
+    return (
+      <ChartWrapper>
+        <BarChart
+          style={{height: 190}}
+          isGroupedByDate
+          useShortDate
+          period="7d"
+          tooltip={{
+            valueFormatter: (value: number) => getDuration(value, 1),
+          }}
+          yAxis={{
+            // Each yAxis marker will increase by 1 day
+            minInterval: 86400,
+            axisLabel: {
+              formatter: (value: number) => {
+                if (value === 0) {
+                  return '';
+                }
+
+                return getDuration(value, 0, true, true);
+              },
+            },
+          }}
+          legend={{right: 0, top: 0}}
+          xAxis={{
+            type: 'time',
+          }}
+          series={[
+            {
+              seriesName: t('Time to Resolution'),
+              data: seriesData,
+            },
+          ].reverse()}
+        />
+      </ChartWrapper>
+    );
+  }
+}
+
+export default TeamResolutionTime;
+
+const ChartWrapper = styled('div')`
+  padding: ${space(2)} ${space(2)} 0 ${space(2)};
+`;
