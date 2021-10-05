67c276e355f7fffe78d53606c5cc5cd842ef5a57
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2021-10-05 11:49:25 +0200

    fix test

--- src/sentry/reprocessing2.py
+++ src/sentry/reprocessing2.py
@@ -576,5 +576,5 @@ def get_progress(group_id):
     # Our internal sync counters are counting over *all* events, but the
     # progressbar in the frontend goes until max_events. Advance progressbar
     # proportionally.
-    pending = int(int(pending) * info["totalEvents"] / float(info.get("syncCount", 1)))
+    pending = int(int(pending) * info["totalEvents"] / float(info.get("syncCount") or 1))
     return pending, info
