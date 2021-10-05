8d8a46542b3af4d19afea3a7487f3db155323a88
Betty Da <bda@sentry.io>
2021-10-05 17:49:58 -0400

    feat(internal-metrics): Add gauge support

--- src/sentry/metrics/dummy.py
+++ src/sentry/metrics/dummy.py
@@ -9,3 +9,6 @@ class DummyMetricsBackend(MetricsBackend):
 
     def timing(self, key, value, instance=None, tags=None, rate=1):
         pass
+
+    def gauge(self, key, value, instance=None, tags=None, sample_rate=1):
+        pass
