d43065b12f313a3fccf83fb6e41848008590662c
Betty Da <bda@sentry.io>
2021-10-05 21:13:04 -0400

    slap a todo onto every single patch

--- tests/sentry/tasks/test_low_priority_symbolication.py
+++ tests/sentry/tasks/test_low_priority_symbolication.py
@@ -68,6 +68,7 @@ class TestScanForSuspectProjects(TestCase):
         assert realtime_metrics.get_lpq_projects() == set()
 
     @freeze_time(datetime.fromtimestamp(0))
+    # TODO: Remove patch and update test once calculation_magic is implemented
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     @mock.patch("sentry.processing.realtime_metrics", STORE)
     def test_has_metric_not_in_lpq(self) -> None:
@@ -80,6 +81,7 @@ class TestScanForSuspectProjects(TestCase):
         assert realtime_metrics.get_lpq_projects() == {17}
 
     @freeze_time(datetime.fromtimestamp(0))
+    # TODO: Remove patch and update test once calculation_magic is implemented
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     @mock.patch("sentry.processing.realtime_metrics", STORE)
     def test_has_metric_in_lpq(self) -> None:
@@ -93,6 +95,7 @@ class TestScanForSuspectProjects(TestCase):
         assert realtime_metrics.get_lpq_projects() == {17}
 
     @freeze_time(datetime.fromtimestamp(0))
+    # TODO: Remove patch and update test once calculation_magic is implemented
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     @mock.patch("sentry.processing.realtime_metrics", STORE)
     def test_add_one_project_remove_one_project(self) -> None:
@@ -119,8 +122,8 @@ class UpdateLpqEligibility(TestCase):
         _update_lpq_eligibility(17, 10)
         assert realtime_metrics.get_lpq_projects() == set()
 
-    # TODO: Remove patch and update test once calculation_magic is implemented
     @freeze_time(datetime.fromtimestamp(0))
+    # TODO: Remove patch and update test once calculation_magic is implemented
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     def test_some_counts_no_durations(self) -> None:
         realtime_metrics.increment_project_event_counter(17, 0)
@@ -129,8 +132,8 @@ class UpdateLpqEligibility(TestCase):
         _update_lpq_eligibility(17, 10)
         assert realtime_metrics.get_lpq_projects() == {17}
 
-    # TODO: Remove patch and update test once calculation_magic is implemented
     @freeze_time(datetime.fromtimestamp(0))
+    # TODO: Remove patch and update test once calculation_magic is implemented
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     def test_no_counts_some_durations(self) -> None:
         realtime_metrics.increment_project_duration_counter(17, 0, 10)
@@ -139,8 +142,8 @@ class UpdateLpqEligibility(TestCase):
         _update_lpq_eligibility(17, 10)
         assert realtime_metrics.get_lpq_projects() == {17}
 
-    # TODO: Remove patch and update test once calculation_magic is implemented
     @freeze_time(datetime.fromtimestamp(0))
+    # TODO: Remove patch and update test once calculation_magic is implemented
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     def test_is_eligible_in_lpq(self) -> None:
         realtime_metrics.add_project_to_lpq(17)
@@ -150,11 +153,11 @@ class UpdateLpqEligibility(TestCase):
 
     # TODO: Remove patch and update test once calculation_magic is implemented
     @freeze_time(datetime.fromtimestamp(0))
+    @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     def test_is_eligible_not_lpq(self) -> None:
         _update_lpq_eligibility(17, 10)
 
         assert realtime_metrics.get_lpq_projects() == {17}
-
 
     # TODO: Update once calculation_magic is implemented
     def test_not_eligible_in_lpq(self) -> None:
