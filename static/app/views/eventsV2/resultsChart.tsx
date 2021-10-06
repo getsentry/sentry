6bba4ee54860b0ed4495f0646ae2940a093d78aa
William Mak <william@wmak.io>
2021-10-06 12:00:47 -0400

    fix(discover): Update the top-5 label regardless of flag (#29093)
    - This fixes a bug where the top 5 displays weren't the correct value
      when mixed with the multiaxis feature flag

--- static/app/views/eventsV2/resultsChart.tsx
+++ static/app/views/eventsV2/resultsChart.tsx
@@ -185,6 +185,12 @@ class ResultsChartContainer extends Component<ContainerProps> {
       .map(opt => {
         // Can only use default display or total daily with multi y axis
         if (
+          hasTopEvents &&
+          [DisplayModes.TOP5, DisplayModes.DAILYTOP5].includes(opt.value as DisplayModes)
+        ) {
+          opt.label = DisplayModes.TOP5 === opt.value ? 'Top Period' : 'Top Daily';
+        }
+        if (
           yAxis.length > 1 &&
           ![DisplayModes.DEFAULT, DisplayModes.DAILY].includes(opt.value as DisplayModes)
         ) {
@@ -194,18 +200,6 @@ class ResultsChartContainer extends Component<ContainerProps> {
             tooltip: t(
               'Change the Y-Axis dropdown to display only 1 function to use this view.'
             ),
-          };
-        }
-        if (hasTopEvents && DisplayModes.TOP5 === opt.value) {
-          return {
-            value: opt.value,
-            label: 'Top Period',
-          };
-        }
-        if (hasTopEvents && DisplayModes.DAILYTOP5 === opt.value) {
-          return {
-            value: opt.value,
-            label: 'Top Daily',
           };
         }
         return opt;
