82ac5113f79052e6e9304dcdfe57948756233394
Betty Da <bda@sentry.io>
2021-10-05 21:00:20 -0400

    cleanup and readability

--- tests/sentry/tasks/test_low_priority_symbolication.py
+++ tests/sentry/tasks/test_low_priority_symbolication.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 from typing import TYPE_CHECKING, Any, Dict
 
 import pytest
@@ -29,9 +29,9 @@ def create_store() -> RedisRealtimeMetricsStore:
     return RedisRealtimeMetricsStore(
         cluster="default",
         counter_bucket_size=10,
-        counter_ttl=datetime.timedelta(milliseconds=400),
+        counter_ttl=timedelta(milliseconds=400),
         histogram_bucket_size=10,
-        histogram_ttl=datetime.timedelta(milliseconds=400),
+        histogram_ttl=timedelta(milliseconds=400),
     )
 
 
@@ -51,40 +51,48 @@ class TestScanForSuspectProjects(TestCase):
     @mock.patch("sentry.processing.realtime_metrics", STORE)
     def test_no_metrics_not_in_lpq(self) -> None:
         assert realtime_metrics.get_lpq_projects() == set()
+
         with TaskRunner():
             _scan_for_suspect_projects()
+
         assert realtime_metrics.get_lpq_projects() == set()
 
     @mock.patch("sentry.processing.realtime_metrics", STORE)
     def test_no_metrics_in_lpq(self) -> None:
         realtime_metrics.add_project_to_lpq(17)
         assert realtime_metrics.get_lpq_projects() == {17}
+
         with TaskRunner():
             _scan_for_suspect_projects()
+
         assert realtime_metrics.get_lpq_projects() == set()
 
-    @freeze_time(datetime.datetime.fromtimestamp(0))
+    @freeze_time(datetime.fromtimestamp(0))
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     @mock.patch("sentry.processing.realtime_metrics", STORE)
     def test_has_metric_not_in_lpq(self) -> None:
         realtime_metrics.increment_project_event_counter(17, 0)
         assert realtime_metrics.get_lpq_projects() == set()
+
         with TaskRunner():
             _scan_for_suspect_projects()
+
         assert realtime_metrics.get_lpq_projects() == {17}
 
-    @freeze_time(datetime.datetime.fromtimestamp(0))
+    @freeze_time(datetime.fromtimestamp(0))
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     @mock.patch("sentry.processing.realtime_metrics", STORE)
     def test_has_metric_in_lpq(self) -> None:
         realtime_metrics.increment_project_event_counter(17, 0)
         realtime_metrics.add_project_to_lpq(17)
         assert realtime_metrics.get_lpq_projects() == {17}
+
         with TaskRunner():
             _scan_for_suspect_projects()
+
         assert realtime_metrics.get_lpq_projects() == {17}
 
-    @freeze_time(datetime.datetime.fromtimestamp(0))
+    @freeze_time(datetime.fromtimestamp(0))
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     @mock.patch("sentry.processing.realtime_metrics", STORE)
     def test_add_one_project_remove_one_project(self) -> None:
@@ -92,8 +100,10 @@ class TestScanForSuspectProjects(TestCase):
         realtime_metrics.remove_projects_from_lpq([17])
         realtime_metrics.add_project_to_lpq(1)
         assert realtime_metrics.get_lpq_projects() == {1}
+
         with TaskRunner():
             _scan_for_suspect_projects()
+
         assert realtime_metrics.get_lpq_projects() == {17}
 
 
@@ -105,36 +115,51 @@ class UpdateLpqEligibility(TestCase):
     def test_no_counts_no_durations_in_lpq(self) -> None:
         realtime_metrics.add_project_to_lpq(17)
         assert realtime_metrics.get_lpq_projects() == {17}
+
         _update_lpq_eligibility(17, 10)
         assert realtime_metrics.get_lpq_projects() == set()
 
     # TODO: Remove patch and update test once calculation_magic is implemented
-    @freeze_time(datetime.datetime.fromtimestamp(0))
+    @freeze_time(datetime.fromtimestamp(0))
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     def test_some_counts_no_durations(self) -> None:
         realtime_metrics.increment_project_event_counter(17, 0)
         assert realtime_metrics.get_lpq_projects() == set()
+
         _update_lpq_eligibility(17, 10)
         assert realtime_metrics.get_lpq_projects() == {17}
 
     # TODO: Remove patch and update test once calculation_magic is implemented
-    @freeze_time(datetime.datetime.fromtimestamp(0))
+    @freeze_time(datetime.fromtimestamp(0))
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
     def test_no_counts_some_durations(self) -> None:
         realtime_metrics.increment_project_duration_counter(17, 0, 10)
         assert realtime_metrics.get_lpq_projects() == set()
+
         _update_lpq_eligibility(17, 10)
         assert realtime_metrics.get_lpq_projects() == {17}
 
     # TODO: Remove patch and update test once calculation_magic is implemented
-    @freeze_time(datetime.datetime.fromtimestamp(0))
+    @freeze_time(datetime.fromtimestamp(0))
     @mock.patch("sentry.tasks.low_priority_symbolication.calculation_magic", lambda x, y: True)
-    def test_is_eligible(self) -> None:
+    def test_is_eligible_in_lpq(self) -> None:
+        realtime_metrics.add_project_to_lpq(17)
+
         _update_lpq_eligibility(17, 10)
         assert realtime_metrics.get_lpq_projects() == {17}
 
+    # TODO: Remove patch and update test once calculation_magic is implemented
+    @freeze_time(datetime.fromtimestamp(0))
+    def test_is_eligible_not_lpq(self) -> None:
+        _update_lpq_eligibility(17, 10)
+
+        assert realtime_metrics.get_lpq_projects() == {17}
+
+
     # TODO: Update once calculation_magic is implemented
-    def test_not_eligible(self) -> None:
+    def test_not_eligible_in_lpq(self) -> None:
+        realtime_metrics.add_project_to_lpq(17)
+
         _update_lpq_eligibility(17, 10)
         assert realtime_metrics.get_lpq_projects() == set()
 
