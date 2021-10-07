a073acf10558e718ab9d3ece7e679e5ab2539eb4
Mark Story <mark@mark-story.com>
2021-10-07 11:02:37 -0400

    Remove logging

--- src/sentry/deletions/defaults/discoversavedquery.py
+++ src/sentry/deletions/defaults/discoversavedquery.py
@@ -3,11 +3,7 @@ from ..base import ModelDeletionTask, ModelRelation
 
 class DiscoverSavedQueryDeletionTask(ModelDeletionTask):
     def get_child_relations(self, instance):
-        import logging
-
         from sentry.discover.models import DiscoverSavedQueryProject
-
-        logging.info("created child relations for discoversavedquery")
 
         return [
             ModelRelation(DiscoverSavedQueryProject, {"discover_saved_query_id": instance.id}),
