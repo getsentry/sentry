getsentry/sentry

commit 1754b74378ff0632ad9fb84db7cb7c4167ad9306

devenv: allow for python 3.8, and prepare CI configuration for dual 3.6/3.8 (#29058)
People can now set `SENTRY_PYTHON_VERSION=3.8.12` in their `.env`, and follow what direnv has to say in order to set up a new 3.8 venv. For example if I have a 3.6.10 venv and do direnv allow:

```
ERROR: You have explicitly set a non-recommended Python version (3.8.10),
but it doesn't match the value of python's version: 3.6.13
You should create a new 3.8.10 virtualenv by running  "rm -rf .venv && direnv allow".
```

Then, we'll build+install 3.8.10 if we don't have it already, and finish bootstrapping the new venv.

I'm gonna follow up with getsentry, and then after that, a PR that adds required tests on 3.8 in addition to 3.6, and sending out a mail on how to move devenv to 3.8. It'll be pretty expensive, but we need to ensure dual compatibility during this transitional period. I won't merge that PR until a full 3.8 deploy is initiated, and after things look good I'll put up a PR to remove 3.6 entirely from devenv and tests.

scripts/pyenv_setup.sh
@@ -96,12 +96,11 @@ install_pyenv() {
 
     if query-apple-m1; then
       pyenv install --skip-existing "${PYENV_VERSION}"
-    elif query-big-sur; then
-      # We need to patch the source code on Big Sur before building Python
-      # We can remove this once we upgrade to newer versions of Python
-      # cat is used since pyenv would finish to soon when the Python version is already installed
-      curl -sSL https://github.com/python/cpython/commit/8ea6353.patch | cat |
-        pyenv install --skip-existing --patch "${PYENV_VERSION}"
+    elif query-big-sur && [ "${PYENV_VERSION}" = 3.6.10 ]; then
+      # TODO(joshuarli): Can be removed when 3.6 is removed.
+      curl -sSL https://github.com/python/cpython/commit/8ea6353.patch | \
+      cat | \
+      pyenv install --skip-existing --patch "${PYENV_VERSION}"
     else
       pyenv install --skip-existing "${PYENV_VERSION}"
     fi

.github/file-filters.yml
@@ -43,7 +43,6 @@ backend_dependencies: &backend_dependencies
 backend_build_changes: &backend_build_changes
   - 'Makefile'
   - '.pre-commit-config.yaml'
-  - '.python-version'
   - '.github/workflows/!(js-*)'
   - '.github/actions/setup-sentry/action.yml'
 

.github/workflows/acceptance.yml
@@ -147,7 +147,8 @@ jobs:
     strategy:
       matrix:
         instance: [0, 1, 2, 3]
-
+        # TODO(joshuarli): Add 3.8.12.
+        python-version: [3.6.13]
     env:
       VISUAL_SNAPSHOT_ENABLE: 1
       TEST_GROUP_STRATEGY: roundrobin
@@ -156,10 +157,11 @@ jobs:
       - name: Checkout sentry
         uses: actions/checkout@v2
 
-      - name: Setup sentry python env
+      - name: Setup sentry env (python ${{ matrix.python-version }})
         uses: ./.github/actions/setup-sentry
         id: setup
         with:
+          python-version: ${{ matrix.python-version }}
           pip-cache-version: ${{ secrets.PIP_CACHE_VERSION }}
           snuba: true
 
@@ -207,6 +209,8 @@ jobs:
     timeout-minutes: 30
     strategy:
       matrix:
+        # TODO(joshuarli): Add 3.8.12.
+        python-version: [3.6.13]
         instance: [0]
     env:
       VISUAL_SNAPSHOT_ENABLE: 1
@@ -218,10 +222,11 @@ jobs:
           # https://github.com/codecov/codecov-bash/blob/7100762afbc822b91806a6574658129fe0d23a7d/codecov#L891
           fetch-depth: '2'
 
-      - name: Setup sentry env
+      - name: Setup sentry env (python ${{ matrix.python-version }})
         uses: ./.github/actions/setup-sentry
         id: setup
         with:
+          python-version: ${{ matrix.python-version }}
           pip-cache-version: ${{ secrets.PIP_CACHE_VERSION }}
           chartcuterie: true
 

.github/workflows/api-docs-test.yml
@@ -1,11 +1,10 @@
 name: api docs
-
 on:
   push:
     branches:
       - master
-
   pull_request:
+    # TODO(joshuarli): Confirm it passes on 3.8.12.
     branches:
       - master
 
@@ -13,6 +12,10 @@ jobs:
   test:
     name: api docs test
     runs-on: ubuntu-20.04
+    strategy:
+      matrix:
+        # TODO(joshuarli): Add 3.8.12.
+        python-version: [3.6.13]
     steps:
       - uses: actions/checkout@v2
 
@@ -26,11 +29,12 @@ jobs:
       - uses: volta-cli/action@v1
         if: steps.changes.outputs.api_docs == 'true'
 
-      - name: Setup sentry python env
+      - name: Setup sentry python env (python ${{ matrix.python-version }})
         uses: ./.github/actions/setup-sentry
         id: setup
         if: steps.changes.outputs.api_docs == 'true'
         with:
+          python-version: ${{ matrix.python-version }}
           pip-cache-version: ${{ secrets.PIP_CACHE_VERSION }}
           snuba: true
 

.github/workflows/backend-lint.yml
@@ -10,7 +10,10 @@ jobs:
     name: backend lint
     runs-on: ubuntu-20.04
     timeout-minutes: 10
-
+    strategy:
+      matrix:
+        # TODO(joshuarli): Add 3.8.12.
+        python-version: [3.6.13]
     steps:
       - uses: actions/checkout@v2
 
@@ -37,10 +40,11 @@ jobs:
           token: ${{ github.token }}
           filters: .github/file-filters.yml
 
-      - name: Setup python
+      - name: Setup Python ${{ matrix.python-version }}
         uses: ./.github/actions/setup-python
         if: steps.changes.outputs.backend == 'true'
         with:
+          python-version: ${{ matrix.python-version }}
           # Note this uses a different cache key than other backend-based workflows because this workflows dependencies are different
           cache-files-hash: ${{ hashFiles('requirements-pre-commit.txt') }}
 

.github/workflows/backend-test.yml
@@ -10,10 +10,12 @@ jobs:
   test:
     name: backend test
     runs-on: ubuntu-20.04
-    timeout-minutes: 30
+    timeout-minutes: 20
     strategy:
       matrix:
-        instance: [0, 1]
+        # TODO(joshuarli): Add 3.8.12.
+        python-version: [3.6.13]
+        instance: [0, 1, 2]
 
     env:
       MIGRATIONS_TEST_MIGRATE: 1
@@ -41,11 +43,12 @@ jobs:
           token: ${{ github.token }}
           filters: .github/file-filters.yml
 
-      - name: Setup sentry env
+      - name: Setup sentry env (python ${{ matrix.python-version }})
         uses: ./.github/actions/setup-sentry
         if: steps.changes.outputs.backend == 'true'
         id: setup
         with:
+          python-version: ${{ matrix.python-version }}
           pip-cache-version: ${{ secrets.PIP_CACHE_VERSION }}
           snuba: true
 

.github/workflows/backend-typing.yml
@@ -10,7 +10,10 @@ jobs:
     name: backend typing
     runs-on: ubuntu-20.04
     timeout-minutes: 12
-
+    strategy:
+      matrix:
+        # TODO(joshuarli): Add 3.8.12.
+        python-version: [3.6.13]
     steps:
       - uses: actions/checkout@v2
 
@@ -29,9 +32,11 @@ jobs:
           token: ${{ github.token }}
           filters: .github/file-filters.yml
 
-      - name: Setup pip
+      - name: Setup Python ${{ matrix.python-version }}
         uses: ./.github/actions/setup-python
         if: steps.changes.outputs.backend == 'true'
+        with:
+          python-version: ${{ matrix.python-version }}
 
       # Since we don't call the setup-sentry action we need to install libxmlsec1-dev
       - name: Setup backend typing

.github/workflows/check-if-migration-is-required.yml
@@ -23,6 +23,10 @@ jobs:
   main:
     name: check migration
     runs-on: ubuntu-20.04
+    strategy:
+      matrix:
+        # TODO(joshuarli): Add 3.8.12.
+        python-version: [3.6.13]
     needs: [should-check]
     if: needs.should-check.outputs.changed == 'true'
 
@@ -30,10 +34,11 @@ jobs:
       - name: Checkout sentry
         uses: actions/checkout@v2
 
-      - name: Setup sentry env
+      - name: Setup sentry env (python ${{ matrix.python-version }})
         uses: ./.github/actions/setup-sentry
         id: setup
         with:
+          python-version: ${{ matrix.python-version }}
           pip-cache-version: ${{ secrets.PIP_CACHE_VERSION }}
 
       - name: Migration & lockfile checks

commit 4f82a7cb11f433934b4b5c8f5ea79654009e165d

fix(dev): Install Python 3.6.x with --patch on Big Sur
Fixes regression introduced in #29058.

On Big Sur, we cannot install Python 3.6.x without patching the source code first.

scripts/pyenv_setup.sh
@@ -94,13 +94,15 @@ install_pyenv() {
       exit 1
     fi
 
-    if query-apple-m1; then
-      pyenv install --skip-existing "${PYENV_VERSION}"
-    elif query-big-sur && [ "${PYENV_VERSION}" = 3.6.10 ]; then
-      # TODO(joshuarli): Can be removed when 3.6 is removed.
-      curl -sSL https://github.com/python/cpython/commit/8ea6353.patch | \
-      cat | \
-      pyenv install --skip-existing --patch "${PYENV_VERSION}"
+    # I'm assuming that SENTRY_PYTHON_VERSION won't be used to specify a 3.6 version to simplify the condition
+    # When a user sets a SENTRY_PYTHON_VERSION they intend to use a non-default version that I assume
+    # is newer, thus, can install with the non-source-patched default pyenv install command
+    if query-big-sur && [[ -z "${SENTRY_PYTHON_VERSION:-}" ]]; then
+      # For Python 3.6.x, we need to patch the source code on Big Sur before building Python
+      # We can remove this once we upgrade to newer versions of Python
+      # cat is used since pyenv would finish to soon when the Python version is already installed
+      curl -sSL https://github.com/python/cpython/commit/8ea6353.patch | cat |
+        pyenv install --skip-existing --patch "${PYENV_VERSION}"
     else
       pyenv install --skip-existing "${PYENV_VERSION}"
     fi