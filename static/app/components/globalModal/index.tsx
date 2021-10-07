0a481b2faa0b3817030325fd1a551fa2b7f1f8cc
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-10-07 00:30:29 -0700

    ref(js): Move logic for modal closing on browser history to a hook

--- static/app/components/globalModal/index.tsx
+++ static/app/components/globalModal/index.tsx
@@ -147,6 +147,9 @@ function GlobalModal({visible = false, options = {}, children, onClose}: Props) 
     return reset;
   }, [portal, handleEscapeClose, visible]);
 
+  // Close the modal when the browser history changes
+  React.useEffect(() => browserHistory.listen(() => actionCloseModal()), []);
+
   const renderedChild = children?.({
     CloseButton: makeCloseButton(closeModal),
     Header: makeClosableHeader(closeModal),
@@ -249,13 +252,7 @@ class GlobalModalContainer extends React.Component<Partial<Props>, State> {
     modalStore: ModalStore.get(),
   };
 
-  componentDidMount() {
-    // Listen for route changes so we can dismiss modal
-    this.unlistenBrowserHistory = browserHistory.listen(() => actionCloseModal());
-  }
-
   componentWillUnmount() {
-    this.unlistenBrowserHistory?.();
     this.unlistener?.();
   }
 
@@ -263,8 +260,6 @@ class GlobalModalContainer extends React.Component<Partial<Props>, State> {
     (modalStore: State['modalStore']) => this.setState({modalStore}),
     undefined
   );
-
-  unlistenBrowserHistory?: ReturnType<typeof browserHistory.listen>;
 
   render() {
     const {modalStore} = this.state;
