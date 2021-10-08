152ba656f99bc459b3f7d41c06ecefd040d08179
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-10-07 23:39:59 -0700

    ref(js): useTheme in IntegrationStatus (#29184)

--- static/app/views/organizationIntegrations/installedIntegration.tsx
+++ static/app/views/organizationIntegrations/installedIntegration.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {withTheme} from '@emotion/react';
+import {useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import Access from 'app/components/acl/access';
@@ -13,7 +13,6 @@ import {t} from 'app/locale';
 import space from 'app/styles/space';
 import {Integration, IntegrationProvider, ObjectStatus, Organization} from 'app/types';
 import {IntegrationAnalyticsKey} from 'app/utils/analytics/integrationAnalyticsEvents';
-import {Theme} from 'app/utils/theme';
 
 import AddIntegrationButton from './addIntegrationButton';
 import IntegrationItem from './integrationItem';
@@ -190,28 +189,27 @@ const IntegrationItemBox = styled('div')`
   flex: 1;
 `;
 
-const IntegrationStatus = withTheme(
-  (
-    props: React.HTMLAttributes<HTMLDivElement> & {theme: Theme; status: ObjectStatus}
-  ) => {
-    const {theme, status, ...p} = props;
-    const color = status === 'active' ? theme.success : theme.gray300;
-    const titleText =
-      status === 'active'
-        ? t('This Integration can be disabled by clicking the Uninstall button')
-        : t('This Integration has been disconnected from the external provider');
-    return (
-      <Tooltip title={titleText}>
-        <div {...p}>
-          <CircleIndicator size={6} color={color} />
-          <IntegrationStatusText>{`${
-            status === 'active' ? t('enabled') : t('disabled')
-          }`}</IntegrationStatusText>
-        </div>
-      </Tooltip>
-    );
-  }
-);
+const IntegrationStatus = (
+  props: React.HTMLAttributes<HTMLDivElement> & {status: ObjectStatus}
+) => {
+  const theme = useTheme();
+  const {status, ...p} = props;
+  const color = status === 'active' ? theme.success : theme.gray300;
+  const titleText =
+    status === 'active'
+      ? t('This Integration can be disabled by clicking the Uninstall button')
+      : t('This Integration has been disconnected from the external provider');
+  return (
+    <Tooltip title={titleText}>
+      <div {...p}>
+        <CircleIndicator size={6} color={color} />
+        <IntegrationStatusText>{`${
+          status === 'active' ? t('enabled') : t('disabled')
+        }`}</IntegrationStatusText>
+      </div>
+    </Tooltip>
+  );
+};
 
 const StyledIntegrationStatus = styled(IntegrationStatus)`
   display: flex;
