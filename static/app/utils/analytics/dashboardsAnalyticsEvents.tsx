99077cb50cdef4f95451eada242b4802b408e7bd
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2021-10-14 13:53:44 -0400

    feat(discover): Added analytics for connecting Discover and Dashboards (#29286)

--- static/app/utils/analytics/dashboardsAnalyticsEvents.tsx
+++ static/app/utils/analytics/dashboardsAnalyticsEvents.tsx
@@ -4,11 +4,21 @@ export type DashboardsEventParameters = {
     field: string;
     value: string;
   };
+  'dashboards_views.query_selector.opened': {
+    widget_type: string;
+  };
+  'dashboards_views.query_selector.selected': {
+    widget_type: string;
+  };
 };
 
 export type DashboardsEventKey = keyof DashboardsEventParameters;
 
 export const dashboardsEventMap: Record<DashboardsEventKey, string | null> = {
   'dashboards_views.add_widget_modal.change':
-    'dashboards Views: Field changed in Add Widget Modal',
+    'Dashboards2: Field changed in Add Widget Modal',
+  'dashboards_views.query_selector.opened':
+    'Dashboards2: Query Selector opened for Widget',
+  'dashboards_views.query_selector.selected':
+    'Dashboards2: Query selected in Query Selector',
 };
