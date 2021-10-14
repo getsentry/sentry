696fe113d7792aaa5446a62d7b0c18f5784cc144
Kelly Carino <kelly@sentry.io>
2021-10-14 16:21:43 -0700

    feat(team-insights): Add Number of Releases section (#29101)
    * feat(team-insights): Add Number of Releases section
    
    Add Number of Releases section. This is only the table - the chart will be added in a follow-up PR.
    
    FIXES WOR-1394
    
    * update test
    
    * update to use new release-count endpoint
    
    * refactor to use teamSlug instead of projects, add number of releases chart
    
    FIXES WOR-1413
    
    * update to use new new releases count endpoint

--- tests/js/spec/views/teamInsights/overview.spec.jsx
+++ tests/js/spec/views/teamInsights/overview.spec.jsx
@@ -77,6 +77,11 @@ describe('TeamInsightsOverview', () => {
       url: `/teams/org-slug/${team1.slug}/issue-breakdown/`,
       body: TestStubs.TeamIssuesReviewed(),
     });
+    MockApiClient.addMockResponse({
+      method: 'GET',
+      url: `/teams/org-slug/${team1.slug}/release-count/`,
+      body: [],
+    });
     act(() => void TeamStore.loadInitialData([team1, team2]));
   });
 
