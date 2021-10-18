698787f9a34fe8036aac1be46aae9244b54df000
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2021-10-18 15:56:30 +0200

    lint

--- src/sentry/api/endpoints/organization_releases.py
+++ src/sentry/api/endpoints/organization_releases.py
@@ -40,11 +40,7 @@ from sentry.search.events.constants import (
 )
 from sentry.search.events.filter import handle_operator_negation, parse_semver
 from sentry.signals import release_created
-from sentry.snuba.sessions import (
-    STATS_PERIODS,
-    _get_project_releases_by_stability,
-    _get_project_releases_count,
-)
+from sentry.snuba.sessions import STATS_PERIODS
 from sentry.utils.cache import cache
 from sentry.utils.compat import zip as izip
 from sentry.utils.sdk import bind_organization_context, configure_scope
