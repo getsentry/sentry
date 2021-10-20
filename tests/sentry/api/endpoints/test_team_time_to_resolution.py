8f67d81258ad2f64b00112a5580b30d0c90d4427
Dan Fuller <dfuller@sentry.io>
2021-10-20 16:51:16 -0700

    fix(insights): Fix time to resolution endpoint to use group date if no previous row available. (#29454)
    Since we're not backfilling so far, it's possible that resolved rows will be created without a
    relevant unresolved row. In this case we can just fall back to using the creating date of the group.

--- tests/sentry/api/endpoints/test_team_time_to_resolution.py
+++ tests/sentry/api/endpoints/test_team_time_to_resolution.py
@@ -3,7 +3,7 @@ from datetime import timedelta
 from django.utils.timezone import now
 from freezegun import freeze_time
 
-from sentry.models import GroupHistory, GroupHistoryStatus
+from sentry.models import GroupHistoryStatus
 from sentry.testutils import APITestCase
 from sentry.testutils.helpers.datetime import before_now
 
@@ -16,49 +16,37 @@ class TeamTimeToResolutionTest(APITestCase):
         project1 = self.create_project(teams=[self.team], slug="foo")
         project2 = self.create_project(teams=[self.team], slug="bar")
         group1 = self.create_group(checksum="a" * 32, project=project1, times_seen=10)
-        group2 = self.create_group(checksum="b" * 32, project=project2, times_seen=5)
-
-        gh1 = GroupHistory.objects.create(
-            organization=self.organization,
-            group=group1,
-            project=project1,
-            actor=self.user.actor,
-            date_added=before_now(days=5),
-            status=GroupHistoryStatus.UNRESOLVED,
-            prev_history=None,
-            prev_history_date=None,
+        group2 = self.create_group(
+            checksum="b" * 32, project=project2, times_seen=5, first_seen=before_now(days=20)
         )
 
-        GroupHistory.objects.create(
-            organization=self.organization,
-            group=group1,
-            project=project1,
+        gh1 = self.create_group_history(
+            group1,
+            GroupHistoryStatus.UNRESOLVED,
             actor=self.user.actor,
-            status=GroupHistoryStatus.RESOLVED,
+            date_added=before_now(days=5),
+        )
+
+        self.create_group_history(
+            group1,
+            GroupHistoryStatus.RESOLVED,
+            actor=self.user.actor,
             prev_history=gh1,
-            prev_history_date=gh1.date_added,
             date_added=before_now(days=2),
         )
 
-        gh2 = GroupHistory.objects.create(
-            organization=self.organization,
-            group=group2,
-            project=project2,
+        gh2 = self.create_group_history(
+            group2,
+            GroupHistoryStatus.UNRESOLVED,
             actor=self.user.actor,
             date_added=before_now(days=10),
-            status=GroupHistoryStatus.UNRESOLVED,
-            prev_history=None,
-            prev_history_date=None,
         )
 
-        GroupHistory.objects.create(
-            organization=self.organization,
-            group=group2,
-            project=project2,
+        self.create_group_history(
+            group2,
+            GroupHistoryStatus.RESOLVED,
             actor=self.user.actor,
-            status=GroupHistoryStatus.RESOLVED,
             prev_history=gh2,
-            prev_history_date=gh2.date_added,
         )
         today = str(now().date())
         yesterday = str((now() - timedelta(days=1)).date())
@@ -73,49 +61,35 @@ class TeamTimeToResolutionTest(APITestCase):
         assert response.data[yesterday]["avg"] == 0
 
         # Lower "todays" average by adding another resolution, but this time 5 days instead of 10 (avg is 7.5 now)
-        gh2 = GroupHistory.objects.create(
-            organization=self.organization,
-            group=group2,
-            project=project2,
+        gh2 = self.create_group_history(
+            group2,
+            GroupHistoryStatus.UNRESOLVED,
             actor=self.user.actor,
             date_added=before_now(days=5),
-            status=GroupHistoryStatus.UNRESOLVED,
-            prev_history=None,
-            prev_history_date=None,
         )
-        GroupHistory.objects.create(
-            organization=self.organization,
-            group=group2,
-            project=project2,
+        self.create_group_history(
+            group2,
+            GroupHistoryStatus.RESOLVED,
             actor=self.user.actor,
-            status=GroupHistoryStatus.RESOLVED,
             prev_history=gh2,
-            prev_history_date=gh2.date_added,
         )
 
         # making sure it doesnt bork anything
-        GroupHistory.objects.create(
-            organization=self.organization,
-            group=group2,
-            project=project2,
+        self.create_group_history(
+            group2,
+            GroupHistoryStatus.DELETED,
             actor=self.user.actor,
-            status=GroupHistoryStatus.DELETED,
             prev_history=gh2,
-            prev_history_date=gh2.date_added,
         )
         # Make sure that if we have a `GroupHistory` row with no prev history then we don't crash.
-        GroupHistory.objects.create(
-            organization=self.organization,
-            group=group2,
-            project=project2,
+        self.create_group_history(
+            group2,
+            GroupHistoryStatus.RESOLVED,
             actor=self.user.actor,
-            status=GroupHistoryStatus.DELETED,
-            prev_history=None,
-            prev_history_date=None,
         )
 
         response = self.get_success_response(self.team.organization.slug, self.team.slug)
         assert len(response.data) == 90
-        assert response.data[today]["avg"] == timedelta(days=7, hours=12).total_seconds()
+        assert response.data[today]["avg"] == timedelta(days=11, hours=16).total_seconds()
         assert response.data[two_days_ago]["avg"] == timedelta(days=3).total_seconds()
         assert response.data[yesterday]["avg"] == 0
