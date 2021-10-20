22ad0eb9dafd985d7f753f6fad7d58734255c562
meredith <meredith@sentry.io>
2021-10-20 09:48:49 -0700

    typing

--- src/sentry/sentry_metrics/indexer/tasks.py
+++ src/sentry/sentry_metrics/indexer/tasks.py
@@ -1,4 +1,5 @@
 import logging
+from typing import Any, Dict, List
 
 from sentry.tasks.base import instrumented_task
 from sentry.utils import metrics
@@ -6,11 +7,11 @@ from sentry.utils import metrics
 logger = logging.getLogger(__name__)
 
 
-@instrumented_task(
+@instrumented_task(  # type: ignore
     name="sentry.sentry_metrics.indexer.tasks.process_indexed_metrics",
     queue="sentry_metrics.indexer",
     default_retry_delay=5,
     max_retries=5,
 )
-def process_indexed_metrics(messages, **kwargs):
+def process_indexed_metrics(messages: List[Dict[str, Any]], **kwargs: Any) -> None:
     metrics.incr("sentry_metrics.indexer.task.processed_messages")
