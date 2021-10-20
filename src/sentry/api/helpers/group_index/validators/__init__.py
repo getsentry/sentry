60f71ac65f2005d2452010fd9106a3f850759e3a
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-10-19 17:37:58 -0700

    fix tests

--- src/sentry/api/helpers/group_index/validators/__init__.py
+++ src/sentry/api/helpers/group_index/validators/__init__.py
@@ -1,7 +1,9 @@
-from .group import GroupValidator
+# TODO(mgaeta): I'm being lazy and importing in non-alphabetical order.
 from .in_commit import InCommitValidator
 from .inbox_details import InboxDetailsValidator
 from .status_details import StatusDetailsValidator
+
+from .group import GroupValidator  # isort:skip
 
 
 class ValidationError(Exception):
