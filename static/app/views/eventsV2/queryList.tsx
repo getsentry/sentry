03d70de013ad4bd026d65e3f063d57ba98d72cd0
Edward Gou <edward.gou@sentry.io>
2021-10-21 11:47:22 -0400

    updated discover add to dashboard modal to use query sort order for table visualization

--- static/app/views/eventsV2/queryList.tsx
+++ static/app/views/eventsV2/queryList.tsx
@@ -97,11 +97,12 @@ class QueryList extends React.Component<Props> {
       event.preventDefault();
       event.stopPropagation();
 
+      const sort = eventView.sorts[0];
       const defaultWidgetQuery: WidgetQuery = {
         name: '',
         fields: savedQuery?.yAxis ?? ['count()'],
         conditions: eventView.query,
-        orderby: '',
+        orderby: sort ? `${sort.kind === 'desc' ? '-' : ''}${sort.field}` : '',
       };
 
       trackAdvancedAnalyticsEvent('discover_views.add_to_dashboard.modal_open', {
