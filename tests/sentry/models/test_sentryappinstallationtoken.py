ebe529f760b1608eeca6a14337f63a5ec864ec7a
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-10-22 15:43:44 -0700

    fix tests

--- tests/sentry/models/test_sentryappinstallationtoken.py
+++ tests/sentry/models/test_sentryappinstallationtoken.py
@@ -1,7 +1,6 @@
 from sentry.models import (
     ApiApplication,
     ApiToken,
-    SentryApp,
     SentryAppInstallation,
     SentryAppInstallationForProvider,
     SentryAppInstallationToken,
@@ -14,19 +13,22 @@ class SentryAppInstallationTokenTest(TestCase):
         self.application = ApiApplication.objects.create(owner=self.user)
         self.provider = "provider"
 
-        self.sentry_app = SentryApp.objects.create(
-            application=self.application,
-            name="NullDB",
-            proxy_user=self.proxy,
-            owner=self.org,
-            scope_list=("project:read",),
-            webhook_url="http://example.com",
+        sentry_app = self.create_internal_integration(
+            webhook_url=None,
+            name="Vercel Internal Integration",
+            organization=self.organization,
         )
 
-        self.install = SentryAppInstallation(
-            sentry_app=self.sentry_app, organization=self.organization
+        self.api_token = ApiToken.objects.create(
+            user=self.user, scope_list=(), refresh_token=None, expires_at=None
         )
-        self.installation_for_provider = SentryAppInstallationForProvider.objects.create(
+
+        self.install = SentryAppInstallation.objects.create(
+            sentry_app=sentry_app,
+            organization=self.organization,
+        )
+
+        SentryAppInstallationForProvider.objects.create(
             organization=self.organization,
             provider=self.provider,
             sentry_app_installation=self.install,
@@ -39,8 +41,8 @@ class SentryAppInstallationTokenTest(TestCase):
         assert not SentryAppInstallationToken.objects.get_token(self.organization.id, "")
 
     def test_get_token(self):
-        api_token = ApiToken.objects.create(
-            user=self.user, scope_list=(), refresh_token=None, expires_at=None
+        SentryAppInstallationToken.objects.create(
+            api_token=self.api_token, sentry_app_installation=self.install
         )
         token = SentryAppInstallationToken.objects.get_token(self.organization.id, self.provider)
-        assert token.token == api_token.token
+        assert token == self.api_token.token
