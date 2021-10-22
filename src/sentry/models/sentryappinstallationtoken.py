d3658bea1c4b33aec27ccde298b45de629b0f59a
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-10-22 15:43:13 -0700

    3.8!

--- src/sentry/models/sentryappinstallationtoken.py
+++ src/sentry/models/sentryappinstallationtoken.py
@@ -1,4 +1,6 @@
-from typing import TYPE_CHECKING, Optional
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from django.db.models import QuerySet
 
@@ -9,7 +11,7 @@ if TYPE_CHECKING:
 
 
 class SentryAppInstallationTokenManager(BaseManager):
-    def get_token(self, organization_id: int, provider: str) -> Optional[str]:
+    def get_token(self, organization_id: int, provider: str) -> str | None:
         """Find a token associated with the installation so we can use it for authentication."""
         sentry_app_installation_tokens = self.select_related("api_token").filter(
             sentry_app_installation__sentryappinstallationforprovider__organization_id=organization_id,
@@ -20,14 +22,14 @@ class SentryAppInstallationTokenManager(BaseManager):
 
         return sentry_app_installation_tokens[0].api_token.token
 
-    def _get_token(self, token: "ApiToken") -> Optional["SentryAppInstallationToken"]:
+    def _get_token(self, token: ApiToken) -> SentryAppInstallationToken | None:
         try:
             return self.select_related("sentry_app_installation").get(api_token=token)
         except SentryAppInstallationToken.DoesNotExist:
             pass
         return None
 
-    def get_projects(self, token: "ApiToken") -> QuerySet:
+    def get_projects(self, token: ApiToken) -> QuerySet:
         from sentry.models import Project
 
         install_token = self._get_token(token)
@@ -38,7 +40,7 @@ class SentryAppInstallationTokenManager(BaseManager):
             organization_id=install_token.sentry_app_installation.organization_id
         )
 
-    def has_organization_access(self, token: "ApiToken", organization: "Organization") -> bool:
+    def has_organization_access(self, token: ApiToken, organization: Organization) -> bool:
         install_token = self._get_token(token)
         if not install_token:
             return False
