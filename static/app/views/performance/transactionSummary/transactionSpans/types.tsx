19a2ddeb5a5ef69100d53e91558f6e9dfbf3e2b5
Tony Xiao <txiao@sentry.io>
2021-10-22 14:27:24 -0400

    feat(suspect-spans): Add suspect spans tab page (#29453)
    This replaces the place holder for the spans tab.

---
+++ static/app/views/performance/transactionSummary/transactionSpans/types.tsx
@@ -0,0 +1,33 @@
+import {GridColumnOrder} from 'app/components/gridEditable';
+
+export enum SpanSortPercentiles {
+  P50_EXCLUSIVE_TIME = 'p50ExclusiveTime',
+  P75_EXCLUSIVE_TIME = 'p75ExclusiveTime',
+  P95_EXCLUSIVE_TIME = 'p95ExclusiveTime',
+  P99_EXCLUSIVE_TIME = 'p99ExclusiveTime',
+}
+
+export enum SpanSortOthers {
+  COUNT = 'count',
+  SUM_EXCLUSIVE_TIME = 'sumExclusiveTime',
+}
+
+export type SpanSort = SpanSortPercentiles | SpanSortOthers;
+
+export type SpanSortOption = {
+  prefix: string;
+  label: string;
+  field: SpanSort;
+};
+
+export type SuspectSpanTableColumnKeys =
+  | 'id'
+  | 'timestamp'
+  | 'spanDuration'
+  | 'repeated'
+  | 'cumulativeDuration'
+  | 'spans';
+
+export type SuspectSpanTableColumn = GridColumnOrder<SuspectSpanTableColumnKeys>;
+
+export type SuspectSpanDataRow = Record<SuspectSpanTableColumnKeys, any>;
