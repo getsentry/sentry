bd300c1d2ed4d05a87dd3d7e66df8b069a15e82e
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-10-25 14:56:44 +0200

    fixed visual snapshot render error

--- tests/js/spec/components/events/crashContent.spec.jsx
+++ tests/js/spec/components/events/crashContent.spec.jsx
@@ -2,23 +2,27 @@ import {mountWithTheme} from 'sentry-test/enzyme';
 
 import CrashContent from 'app/components/events/interfaces/crashContent';
 import {withMeta} from 'app/components/events/meta/metaProxy';
+import {OrganizationContext} from 'app/views/organizationContext';
 
 describe('CrashContent', function () {
   const exc = TestStubs.ExceptionWithMeta({platform: 'cocoa'});
   const event = TestStubs.Event();
+  const organization = TestStubs.Organization();
 
   const proxiedExc = withMeta(exc);
 
   it('renders with meta data', function () {
     const wrapper = mountWithTheme(
-      <CrashContent
-        projectId="sentry"
-        stackView="full"
-        stackType="original"
-        event={event}
-        newestFirst
-        exception={proxiedExc.exception}
-      />
+      <OrganizationContext.Provider value={organization}>
+        <CrashContent
+          projectId="sentry"
+          stackView="full"
+          stackType="original"
+          event={event}
+          newestFirst
+          exception={proxiedExc.exception}
+        />
+      </OrganizationContext.Provider>
     );
 
     expect(wrapper).toSnapshot();
