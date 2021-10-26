2d98c67a9aaeb61238a4452c09be0d073771db85
k-fish <kevan.fisher@sentry.io>
2021-10-26 11:35:38 -0400

    Fix badge import

--- static/app/views/performance/transactionSummary/header.tsx
+++ static/app/views/performance/transactionSummary/header.tsx
@@ -6,7 +6,6 @@ import Feature from 'app/components/acl/feature';
 import {GuideAnchor} from 'app/components/assistant/guideAnchor';
 import ButtonBar from 'app/components/buttonBar';
 import {CreateAlertFromViewButton} from 'app/components/createAlertButton';
-import FeatureBadge from 'app/components/featureBadge';
 import * as Layout from 'app/components/layouts/thirds';
 import ListLink from 'app/components/links/listLink';
 import NavTabs from 'app/components/navTabs';
