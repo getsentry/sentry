4e84c73c67ce85efc0d8569aa60d1dac12170a63
Colleen O'Rourke <colleen@sentry.io>
2021-10-26 16:20:13 -0700

    add example, put unmigratable providers in a constant

--- src/sentry/api/serializers/models/plugin.py
+++ src/sentry/api/serializers/models/plugin.py
@@ -9,6 +9,7 @@ from sentry.utils.http import absolute_uri
 
 # Dict with the plugin_name as the key, and enabling_feature_name as the value
 SHADOW_DEPRECATED_PLUGINS = {
+    # "exampleslug": "organizations:integrations-ignore-exampleslug-deprecation"
 }
 
 
