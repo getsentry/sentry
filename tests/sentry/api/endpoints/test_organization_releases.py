e63cb3e66c52e6821f586b4a036feee1f888356c
Scott Cooper <scttcper@gmail.com>
2021-10-27 15:01:31 -0700

    feat(workflow): Enable several GA flags by default (#29557)

--- tests/sentry/api/endpoints/test_organization_releases.py
+++ tests/sentry/api/endpoints/test_organization_releases.py
@@ -817,12 +817,14 @@ class OrganizationReleasesStatsTest(APITestCase):
         )
         release1.add_project(project1)
         url = reverse("sentry-api-0-organization-releases", kwargs={"organization_slug": org.slug})
-        response = self.client.get(f"{url}?adoptionStages=1", format="json")
 
-        assert response.status_code == 200, response.content
-        assert len(response.data) == 1
-        # Not returned because we don't have the feature.
-        assert "adoptionStages" not in response.data[0]
+        with self.feature({"organizations:release-adoption-stage": False}):
+            response = self.client.get(f"{url}?adoptionStages=1", format="json")
+
+            assert response.status_code == 200, response.content
+            assert len(response.data) == 1
+            # Not returned because we don't have the feature.
+            assert "adoptionStages" not in response.data[0]
 
         with self.feature("organizations:release-adoption-stage"):
             response = self.client.get(url, format="json")
