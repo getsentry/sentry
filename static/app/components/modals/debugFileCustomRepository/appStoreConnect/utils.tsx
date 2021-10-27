471c74d72069e851e2de04748335bf48813a3762
Priscila Oliveira <priscila.oliveira@sentry.io>
2021-10-27 10:39:26 +0200

    ref(app-store-connect): Update error message mapping

--- static/app/components/modals/debugFileCustomRepository/appStoreConnect/utils.tsx
+++ static/app/components/modals/debugFileCustomRepository/appStoreConnect/utils.tsx
@@ -2,67 +2,130 @@ import * as Sentry from '@sentry/react';
 
 import {t} from 'app/locale';
 
-type ErrorCode =
+import {StepOneData} from './types';
+
+// since translations are done on the front-end we need to map  back-end error messages to front-end messages
+const fieldErrorMessageMapping = {
+  appconnectIssuer: {
+    issuer: {
+      'Ensure this field has at least 36 characters.': t(
+        'Ensure this field has at least 36 characters.'
+      ),
+      'Ensure this field has no more than 36 characters.': t(
+        'Ensure this field has no more than 36 characters.'
+      ),
+    },
+  },
+  appconnectKey: {
+    keyId: {
+      'Ensure this field has at least 2 characters.': t(
+        'Ensure this field has at least 2 characters.'
+      ),
+      'Ensure this field has no more than 20 characters.': t(
+        'Ensure this field has no more than 20 characters.'
+      ),
+    },
+  },
+};
+
+type ErrorCodeDetailed =
   | 'app-connect-authentication-error'
   | 'app-connect-multiple-sources-error'
   | 'itunes-authentication-error'
   | 'itunes-2fa-required'
   | 'itunes-sms-blocked-error';
 
-export type DetailedErrorResponse = {
-  status: number;
-  responseJSON?: {
-    detail: {
-      code: ErrorCode;
-      extra: Record<string, any>;
-      message: string;
-    };
+type ResponseJSONDetailed = {
+  detail: {
+    code: ErrorCodeDetailed;
+    extra: Record<string, any>;
+    message: string;
   };
 };
 
-type NotDetailedErrorResponse = {
+export type AppStoreConnectField = keyof typeof fieldErrorMessageMapping;
+
+type ResponseJSON = Record<AppStoreConnectField, string[]>;
+
+type Error = {
   status: number;
-  responseJSON?: {
-    code: string[];
-  };
+  responseJSON?: ResponseJSON | ResponseJSONDetailed;
 };
 
-type Error = DetailedErrorResponse | NotDetailedErrorResponse;
-
-const unexpectedErrorMessage = t(
+export const unexpectedErrorMessage = t(
   'An unexpected error occurred while configuring the app store connect'
 );
 
-export function getAppStoreErrorMessage(error: Error | string) {
+export function getAppStoreErrorMessage(
+  error: Error | string
+): string | Record<keyof StepOneData, string> {
   if (typeof error === 'string') {
     return error;
   }
 
-  if ((error as NotDetailedErrorResponse).responseJSON?.code) {
-    return (
-      (error as NotDetailedErrorResponse).responseJSON?.code[0] ?? unexpectedErrorMessage
-    );
-  }
+  const detailedErrorResponse = (error.responseJSON as undefined | ResponseJSONDetailed)
+    ?.detail;
 
-  switch ((error as DetailedErrorResponse).responseJSON?.detail.code) {
-    case 'app-connect-authentication-error':
-      return t(
-        'We could not establish a connection with App Store Connect. Please check the entered App Store Connect credentials'
-      );
-    case 'app-connect-multiple-sources-error':
-      return t('Only one Apple App Store Connect application is allowed in this project');
-    case 'itunes-authentication-error':
-      return t('The iTunes authentication failed. Please check the provided credentials');
-    case 'itunes-sms-blocked-error':
-      return t(
-        'Blocked from requesting more SMS codes for an unspecified period of time'
-      );
-    case 'itunes-2fa-required':
-      return t('The two factor authentication failed. Please check the entered code');
-    default: {
-      // this shall not happen
-      Sentry.captureException(new Error('Unknown app store connect error'));
-      return unexpectedErrorMessage;
+  if (detailedErrorResponse) {
+    switch (detailedErrorResponse.code) {
+      case 'app-connect-authentication-error':
+        return t(
+          'We could not establish a connection with App Store Connect. Please check the entered App Store Connect credentials'
+        );
+      case 'app-connect-multiple-sources-error':
+        return t(
+          'Only one Apple App Store Connect application is allowed in this project'
+        );
+      case 'itunes-authentication-error':
+        return t(
+          'The iTunes authentication failed. Please check the provided credentials'
+        );
+      case 'itunes-sms-blocked-error':
+        return t(
+          'Blocked from requesting more SMS codes for an unspecified period of time'
+        );
+      case 'itunes-2fa-required':
+        return t('The two factor authentication failed. Please check the entered code');
+      default: {
+        // this shall not happen
+        Sentry.captureException(new Error('Unknown app store connect error'));
+        return unexpectedErrorMessage;
+      }
     }
   }
+
+  const errorResponse = error.responseJSON as undefined | ResponseJSON;
+
+  if (!errorResponse) {
+    return unexpectedErrorMessage;
+  }
+
+  return (Object.keys(errorResponse) as AppStoreConnectField[]).reduce(
+    (acc, serverSideField) => {
+      const fieldErrorMessage = fieldErrorMessageMapping[serverSideField] ?? {};
+      const field = Object.keys(fieldErrorMessage)[0];
+
+      const errorMessages: string[] = errorResponse[serverSideField].map(errorMessage => {
+        if (fieldErrorMessage[field][errorMessage]) {
+          return fieldErrorMessage[field][errorMessage];
+        }
+
+        // This will be difficult to happen,
+        // but if it happens we will be able to see which message is not being mapped on the fron-tend
+        Sentry.withScope(scope => {
+          scope.setExtra('serverSideField', serverSideField);
+          scope.setExtra('message', errorMessage);
+          Sentry.captureException(
+            new Error('App Store Connect - Untranslated error message')
+          );
+        });
+
+        return errorMessage;
+      });
+
+      // the UI only displays one error message at a time
+      return {...acc, [field]: errorMessages[0]};
+    },
+    {}
+  ) as Record<keyof StepOneData, string>;
 }
