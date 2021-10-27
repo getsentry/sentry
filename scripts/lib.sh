463f14ba987560325309edd35cec011b0b5d47a5
Armen <armenzg@sentry.io>
2021-10-27 14:50:22 -0400

    Debug info

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -24,6 +24,10 @@ require() {
 }
 
 configure-sentry-cli() {
+    # XXX: For version 1.70.1 there's a bug hitting SENTRY_CLI_NO_EXIT_TRAP: unbound variable
+    # We can remove this after it's fixed
+    # https://github.com/getsentry/sentry-cli/pull/1059
+    export SENTRY_CLI_NO_EXIT_TRAP=${SENTRY_CLI_NO_EXIT_TRAP-0}
     if [ -n "${SENTRY_DSN+x}" ] && [ -z "${SENTRY_DEVENV_NO_REPORT+x}" ]; then
         if ! require sentry-cli; then
             curl -sL https://sentry.io/get-cli/ | bash
