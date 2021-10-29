9e8ecbcac1d05d47ed27f2f15380b8e36fb20a7b
Mark Story <mark@sentry.io>
2021-10-29 13:15:36 -0400

    ref(tests) Convert a few simple tests to RTL (#29646)
    Nothing complicated, just two more files complete.

--- tests/js/spec/components/textCopyInput.spec.jsx
+++ tests/js/spec/components/textCopyInput.spec.jsx
@@ -1,10 +1,11 @@
-import {mountWithTheme} from 'sentry-test/enzyme';
+import {mountWithTheme, screen} from 'sentry-test/reactTestingLibrary';
 
 import TextCopyInput from 'app/views/settings/components/forms/textCopyInput';
 
 describe('TextCopyInput', function () {
   it('renders', function () {
-    const wrapper = mountWithTheme(<TextCopyInput>Text to Copy</TextCopyInput>);
-    expect(wrapper).toSnapshot();
+    const {container} = mountWithTheme(<TextCopyInput>Text to Copy</TextCopyInput>);
+    expect(container).toSnapshot();
+    expect(screen.getByDisplayValue('Text to Copy')).toBeInTheDocument();
   });
 });
