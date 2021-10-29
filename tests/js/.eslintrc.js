2da62b60de02f2b9a9d3cfe51f38d385f4f80216
Matej Minar <matej.minar@sentry.io>
2021-10-29 12:52:50 +0200

    chore(deps): Upgrade eslint-config-sentry@1.65.0

---
+++ tests/js/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['plugin:testing-library/react'],
+  rules: {
+    'testing-library/render-result-naming-convention': 'off',
+    'testing-library/prefer-screen-queries': 'off',
+  },
+};
