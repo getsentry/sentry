69c923fede7a77762d6228be5b9145947f74ee5e
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2021-11-03 11:11:35 +0100

    WIP - working on schema structure

--- src/sentry/release_health/duplex.py
+++ src/sentry/release_health/duplex.py
@@ -1,8 +1,9 @@
 import inspect
 from copy import deepcopy
 from datetime import datetime
-from typing import Mapping, Optional, Sequence, Set, Union
+from typing import Any, List, Mapping, Optional, Sequence, Set, TypeVar, Union
 
+from dateutil import parser
 from typing_extensions import Literal
 
 from sentry.release_health.base import (
@@ -29,6 +30,9 @@ from sentry.release_health.metrics import MetricsReleaseHealthBackend
 from sentry.release_health.sessions import SessionsReleaseHealthBackend
 from sentry.snuba.sessions_v2 import QueryDefinition
 
+DateLike = TypeVar("DateLike", datetime, str)
+Schema = Union[str, List["Schema"], Mapping[str, "Schema"], Set["Schema"]]
+
 
 def _get_calling_method():
     """
@@ -39,8 +43,240 @@ def _get_calling_method():
     return inspect.stack()[2].function
 
 
-def compare_results(result, right, ignore=None):
-    pass
+def compare_entities(sessions, metrics, path: str) -> Optional[str]:
+    if sessions != metrics:
+        return f"field {path} contains different data sessions={sessions} metrics={metrics}"
+
+
+def compare_datetime(
+    sessions: Optional[DateLike], metrics: Optional[DateLike], rollup: int, path: str
+) -> Optional[str]:
+    if sessions is None and metrics is None:
+        return None
+    if sessions is None:
+        return f"Field {path} only present in metrics implementation"
+    if metrics is None:
+        return f"Field {path} missing from metrics implementation"
+    if type(sessions) != type(metrics):
+        return f"Field {path} inconsistent types return sessions={type(sessions)}, metrics={type(metrics)}"
+    if type(sessions) == str:
+        try:
+            sessions_d = parser.parse(sessions)
+            metrics_d = parser.parse(metrics)
+            dd = abs(sessions_d - metrics_d)
+        except parser.ParserError:
+            return f"Field {path} could not parse dates sessions={sessions}, metrics={metrics}"
+    else:
+        dd = abs(sessions - metrics)
+    if dd > rollup:
+        return f"Field {path} failed to mach datetimes sessions={sessions}, metrics={metrics} "
+    return None
+
+
+def compare_counters(sessions: Optional[int], metrics: Optional[int], path: str) -> Optional[str]:
+    """
+    Aprox
+
+    >>> compare_counters(100,110, "x.y")
+    'Fields with different values at x.y sessions=100, metrics=110'
+    >>> compare_counters(100,105, "x.y")
+    >>> compare_counters(100,96, "x.y")
+    >>> compare_counters(None,None, "x.y")
+    >>> compare_counters(None,1, "x.y")
+    'Field x.y only present in metrics implementation'
+    >>> compare_counters(0,None, "x.y")
+    'Field x.y missing from metrics implementation'
+    >>> compare_counters(1,3, "x.y")
+    >>> compare_counters(1,7, "x.y")
+    'Fields with different values at x.y sessions=1, metrics=7'
+    """
+    if sessions is None and metrics is None:
+        return None
+    if sessions is None:
+        return f"Field {path} only present in metrics implementation"
+    if metrics is None:
+        return f"Field {path} missing from metrics implementation"
+    if metrics < 0:
+        return f"Invalid field {path} value={metrics}, from metrics, only positive values are expected. "
+    if sessions < 0:
+        return f"Sessions ERROR, Invalid field {path} value = {sessions}, from sessions, only positive values are expected. "
+    if (sessions <= 10 and metrics > 10) or (metrics <= 10 and sessions > 10):
+        if abs(sessions - metrics) > 4:
+            return f"Fields with different values at {path} sessions={sessions}, metrics={metrics}"
+        else:
+            return None
+    if metrics <= 10:
+        if abs(sessions - metrics) > 3:
+            return f"Fields with different values at {path} sessions={sessions}, metrics={metrics}"
+        else:
+            return None
+    else:
+        if float(abs(sessions - metrics)) / metrics > 0.05:
+            return f"Fields with different values at {path} sessions={sessions}, metrics={metrics}"
+    return None
+
+
+def compare_ratios(sessions: Optional[float], metrics: Optional[float], path: str) -> Optional[str]:
+    if sessions is None and metrics is None:
+        return None
+    if sessions is None:
+        return f"Field {path} only present in metrics implementation"
+    if metrics is None:
+        return f"Field {path} missing from metrics implementation"
+    if metrics < 0:
+        return f"Invalid field {path} value = {metrics}, from metrics, only positive values are expected. "
+    if sessions < 0:
+        return f"Sessions ERROR, Invalid field {path} value = {sessions}, from sessions, only positive values are expected. "
+    if sessions == metrics == 0.0:
+        return None
+    if float(abs(sessions - metrics)) / max(metrics, sessions) > 0.01:
+        return f"Fields with different values at {path} sessions={sessions}, metrics={metrics}"
+    return None
+
+
+compare_quantiles = compare_ratios
+
+
+def compare_scalars(
+    sessions, metrics, rollup: int, path: str, schema: Optional[str]
+) -> Optional[str]:
+    if schema is None:
+        if type(sessions) in (str, int):
+            return compare_entities(sessions, metrics, path)
+        if type(sessions) == float:
+            return compare_ratios(sessions, metrics, path)
+        if type(sessions) == datetime:
+            return compare_datetime(sessions, metrics, rollup, path)
+    elif schema in ("counter", "c"):
+        return compare_counters(sessions, metrics, path)
+    elif schema in ("ratio", "r"):
+        return compare_ratios(sessions, metrics, path)
+    elif schema in ("quantile", "percentile", "q", "p"):
+        return compare_ratios(sessions, metrics, path)
+    elif schema == ("entity", "e"):
+        return compare_entities(sessions, metrics, path)
+    elif schema == ("datetime", "date", "d"):
+        return compare_datetime(sessions, metrics, rollup, path)
+
+
+def compare_arrays(
+    sessions, metrics, rollup: int, path: str, schema: Optional[List[Schema]]
+) -> List[str]:
+    if schema is None:
+        child_schema = None
+    else:
+        assert len(schema) == 1
+        child_schema = schema[0]
+
+    ret_val = []
+
+    for idx in range(len(sessions)):
+        elm_path = f"{path}[{idx}]"
+        ret_val += compare_results(sessions[idx], metrics[idx], rollup, elm_path, child_schema)
+
+    return ret_val
+
+
+def compare_tuple(
+    sessions, metrics, rollup: int, path: str, schema: Optional[Sequence[Schema]]
+) -> List[str]:
+    if type(sessions) != tuple or type(metrics) != tuple:
+        return [
+            f"Different length for metrics tuple on path {path}, sessions={len(sessions)}, metrics={len(metrics)}"
+        ]
+
+    ret_val = []
+    if schema is not None:
+        assert len(sessions) == len(schema)
+    for idx in range(len(sessions)):
+        elm_path = f"{path}[{idx}]"
+        if schema is None:
+            child_schema = None
+        else:
+            child_schema = schema[idx]
+        ret_val += compare_results(sessions[idx], metrics[idx], rollup, elm_path, child_schema)
+
+    return ret_val
+
+
+def compare_sets(sessions, metrics, path: str) -> List[str]:
+    if sessions != metrics:
+        return [f"Different values found at path {path} sessions={sessions}, metrics={metrics}"]
+    return []
+
+
+def compare_dicts(
+    sessions: Mapping[Any, Any],
+    metrics: Mapping[Any, Any],
+    rollup: int,
+    path: str,
+    schema: Optional[Mapping[str, Schema]],
+) -> List[str]:
+    if type(metrics) != dict:
+        return [
+            f"Invalid type of metrics at path {path} expecting a dictionary fouond a {type(metrics)}"
+        ]
+
+    if schema is None:
+        iterate_all = True
+        generic_item_schema = None
+        schema = {}
+    else:
+        iterate_all = "*" in schema
+        generic_item_schema = schema.get("*")
+
+    ret_val = []
+
+    if iterate_all:
+        if len(sessions) != len(metrics):
+            return [
+                f"Different number of keys in dictionaries sessions={len(sessions)}, metrics={len(metrics)}"
+            ]
+        for key, val in sessions.items():
+            child_path = f"{path}[{key}]"
+            child_schema = schema.get(key, generic_item_schema)
+            ret_val += compare_results(val, metrics.get(key), rollup, child_path, child_schema)
+    else:
+        for key, child_schema in schema.items():
+            child_path = f"{path}[{key}]"
+            ret_val += compare_results(
+                sessions.get(key), metrics.get(key), rollup, child_path, child_schema
+            )
+
+
+def compare_results(
+    sessions, metrics, rollup: int, path: Optional[str] = None, schema: Optional[Schema] = None
+) -> List[str]:
+    if path is None:
+        path = ""
+
+    errors = []
+
+    if schema is not None:
+        discriminator = schema
+    else:
+        discriminator = sessions
+
+    if discriminator is None:
+        if metrics is None:
+            return []
+        else:
+            return [f"Unmatched field at path {path}, sessions=None, metrics={metrics}"]
+
+    if type(discriminator) in {str, float, int, datetime}:
+        err = compare_scalars(sessions, metrics, rollup, path, schema)
+        if err is not None:
+            errors.append(err)
+    elif type(discriminator) == tuple:
+        return compare_tuple(sessions, metrics, rollup, path, schema)
+    elif type(discriminator) == list:
+        return compare_arrays(sessions, metrics, rollup, path, schema)
+    elif type(discriminator) == set:
+        return compare_sets(sessions, metrics, path)
+    elif type(discriminator) == dict:
+        return compare_dicts(sessions, metrics, rollup, path, schema)
+    else:
+        return [f"Invalid schema type={type(schema)} at path:'{path}'"]
 
 
 def log_exception(ex, result):
