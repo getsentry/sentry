9e6c67dd7addefe3aa2599a53a7d865c739bd9a5
relaxolotl <5597345+relaxolotl@users.noreply.github.com>
2021-11-03 17:32:37 -0400

    fix(appstore-connect): Relax timeout for build fetching (#29724)
    Tries to cut down on instances of SENTRY-FOR-SENTRY-F0V.
    
    We're requesting more data per build as of #29513, so we
    should make the timeout a little more lenient to reflect that.

--- src/sentry/utils/appleconnect/appstore_connect.py
+++ src/sentry/utils/appleconnect/appstore_connect.py
@@ -20,7 +20,7 @@ logger = logging.getLogger(__name__)
 
 AppConnectCredentials = namedtuple("AppConnectCredentials", ["key_id", "key", "issuer_id"])
 
-REQUEST_TIMEOUT = 15.0
+REQUEST_TIMEOUT = 30.0
 
 
 class RequestError(Exception):
@@ -137,7 +137,7 @@ def _get_appstore_json(
             full_url = ""
         full_url += url
         logger.debug(f"GET {full_url}")
-        with sentry_sdk.start_span(op="http", description="AppStoreConnect request"):
+        with sentry_sdk.start_transaction(op="http", description="AppStoreConnect request"):
             response = session.get(full_url, headers=headers, timeout=REQUEST_TIMEOUT)
         if not response.ok:
             err_info = {
