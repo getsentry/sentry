0b0ed90f80e160af9cd74f6150ff47cf077387dd
Matej Minar <matej.minar@sentry.io>
2021-11-03 19:22:20 +0100

    move rules to the eslint-config-sentry

--- tests/js/.eslintrc.js
+++ tests/js/.eslintrc.js
@@ -1,7 +1,3 @@
 module.exports = {
-  extends: ['plugin:testing-library/react'],
-  rules: {
-    'testing-library/render-result-naming-convention': 'off',
-    'testing-library/prefer-screen-queries': 'off',
-  },
+  extends: ['plugin:testing-library/react', 'sentry-app/strict'],
 };
