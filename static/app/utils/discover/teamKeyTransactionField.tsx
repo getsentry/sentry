45d57ca3610b435e1404d4b5fe4d6aeee7b82eaf
David Wang <davidw1339@gmail.com>
2021-11-03 15:29:09 -0700

    ref(ui): Change from withTeams to <Teams> for team key transactions (#29223)
    Due to how we fetch teams on the frontend we only load up to 100 into the TeamStore at app start. Only a handful of orgs have over 100 teams, but it still poses a problem for using certain features such as team key transactions. To fix this, we replace withTeams in favor of the Teams utility which handles fetching the correct teams and propagating changes back to the store to prevent duplicate fetching.

--- static/app/utils/discover/teamKeyTransactionField.tsx
+++ static/app/utils/discover/teamKeyTransactionField.tsx
@@ -7,10 +7,9 @@ import TeamKeyTransaction, {
 import * as TeamKeyTransactionManager from 'app/components/performance/teamKeyTransactionsManager';
 import Tooltip from 'app/components/tooltip';
 import {IconStar} from 'app/icons';
-import {Organization, Project, Team} from 'app/types';
+import {Organization, Project} from 'app/types';
 import {defined} from 'app/utils';
 import withProjects from 'app/utils/withProjects';
-import withTeams from 'app/utils/withTeams';
 
 class TitleStar extends Component<TitleProps> {
   render() {
@@ -34,7 +33,6 @@ class TitleStar extends Component<TitleProps> {
 }
 
 type BaseProps = {
-  teams: Team[];
   organization: Organization;
   isKeyTransaction: boolean;
 };
@@ -112,4 +110,4 @@ function TeamKeyTransactionFieldWrapper({
   );
 }
 
-export default withTeams(withProjects(TeamKeyTransactionFieldWrapper));
+export default withProjects(TeamKeyTransactionFieldWrapper);
