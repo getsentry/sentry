2425db2ba99d8e696c056dcf36fffcd984964daa
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-11-04 13:19:42 -0700

    test(js): Normalize fixtures (#29772)

--- tests/fixtures/js-stubs/broadcast.js
+++ tests/fixtures/js-stubs/broadcast.js
@@ -5,8 +5,7 @@ export function Broadcast(params = {}) {
     hasSeen: false,
     id: '8',
     isActive: true,
-    link:
-      'https://docs.sentry.io/clients/javascript/sourcemaps/#uploading-source-maps-to-sentry',
+    link: 'https://docs.sentry.io/clients/javascript/sourcemaps/#uploading-source-maps-to-sentry',
     message:
       'Source maps are JSON files that contain information on how to map your transpiled source code back to their original source.',
     title: 'Learn about Source Maps',
