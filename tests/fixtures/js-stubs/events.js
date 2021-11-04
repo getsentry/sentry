2803cf860cbb73ea283f6426ec06d57493301573
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-11-04 16:14:21 -0700

    ref(js): Fix weird TestStub definition

--- tests/fixtures/js-stubs/events.js
+++ tests/fixtures/js-stubs/events.js
@@ -13,7 +13,7 @@ export function Events(params = []) {
   ];
 }
 
-export function EventsStats(_query = {}, params) {
+export function EventsStats(params = {}) {
   return {
     data: [
       [new Date(), [{count: 321}, {count: 79}]],
