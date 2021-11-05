855fd5e47cc736d8e58bdf3f22743e5a8a0c0e3e
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-11-05 16:47:02 -0700

    ref(js): Normalize two missed js-stubs (#29838)

--- tests/fixtures/js-stubs/userIdentity.js
+++ tests/fixtures/js-stubs/userIdentity.js
@@ -1,10 +1,12 @@
-export const UserIdentity = () => ({
-  id: '52',
-  identityProvider: {
-    id: '4',
-    type: 'slack',
-    externalId: 'TA99AB9CD',
-  },
-  externalId: 'UA1J9RTE1',
-  status: 1,
-});
+export function UserIdentity() {
+  return {
+    id: '52',
+    identityProvider: {
+      id: '4',
+      type: 'slack',
+      externalId: 'TA99AB9CD',
+    },
+    externalId: 'UA1J9RTE1',
+    status: 1,
+  };
+}
