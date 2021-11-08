6f2b5d2d49f3bcb1c91c980a004accd462a32fa5
Stephen Cefali <scefali@sentry.io>
2021-11-08 08:29:04 -0800

    convert analytics on eventEntries and issueList/overview (#29762)
    Adds the following events to be tracked with the new `trackAdvancedAnalyticsEvent` function:
    
    - `issue_error_banner.viewed`
    - `issues_tab.viewed`
    - `issue_search.failed`
    - `search.searched`
    - `organization_saved_search.selected`
    - `Organization Saved Search: Selected saved search`
    - `issue.search_sidebar_clicked': 'Issue Search Sidebar Clicked`
    
    I also added logic to dedupe array entries for `issue_error_banner.viewed` as I found that we have certain huge events (25kb column size in `data`) with a bunch of duplicate array items.

--- static/app/utils/analytics/issueAnalyticsEvents.tsx
+++ static/app/utils/analytics/issueAnalyticsEvents.tsx
@@ -6,13 +6,45 @@ export type IssueEventParameters = {
   'event_cause.docs_clicked': {};
   'event_cause.snoozed': {};
   'event_cause.dismissed': {};
+  'issue_error_banner.viewed': {
+    error_type: string[];
+    error_message: string[];
+    group?: string;
+    platform?: string;
+  };
+  'issues_tab.viewed': {
+    tab: string;
+    num_issues: number;
+  };
+  'issue_search.failed': {
+    search_type: string;
+    search_source: string;
+    error: string;
+  };
+  'search.searched': {
+    query: string;
+    search_type: string;
+    search_source: string;
+  };
+  'organization_saved_search.selected': {
+    search_type: string;
+    id: number;
+  };
+  'issue.search_sidebar_clicked': {};
 };
 
 export type IssueEventKey = keyof IssueEventParameters;
 
 export const issueEventMap: Record<IssueEventKey, string | null> = {
-  'event_cause.viewed': null, // send to reload only due to high event volume
+  'event_cause.viewed': null, // send to main event store only due to high event volume
   'event_cause.docs_clicked': 'Event Cause Docs Clicked',
   'event_cause.snoozed': 'Event Cause Snoozed',
   'event_cause.dismissed': 'Event Cause Dismissed',
+  'issue_error_banner.viewed': 'Issue Error Banner Viewed',
+  'issues_tab.viewed': 'Viewed Issues Tab', // high volume but send to our secondary event store anyways
+  'issue_search.failed': 'Issue Search: Failed',
+  'search.searched': 'Search: Performed search',
+  'organization_saved_search.selected':
+    'Organization Saved Search: Selected saved search',
+  'issue.search_sidebar_clicked': 'Issue Search Sidebar Clicked',
 };
