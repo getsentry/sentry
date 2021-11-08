861f5076aeb737d105ffcfe9205af3930b05ec6c
Ryan Skonnord <ryan.skonnord@sentry.io>
2021-11-08 14:52:34 -0800

    feat(settings): Show logos for all Identity providers

---
+++ static/app/views/settings/components/identityIcon.tsx
@@ -0,0 +1,78 @@
+import styled from '@emotion/styled';
+import asana from 'sentry-logos/logo-asana.svg';
+import auth0 from 'sentry-logos/logo-auth0.svg';
+import vsts from 'sentry-logos/logo-azure.svg';
+import bitbucket from 'sentry-logos/logo-bitbucket.svg';
+import bitbucketserver from 'sentry-logos/logo-bitbucket-server.svg';
+import placeholder from 'sentry-logos/logo-default.svg';
+import github from 'sentry-logos/logo-github.svg';
+import githubEnterprise from 'sentry-logos/logo-github-enterprise.svg';
+import gitlab from 'sentry-logos/logo-gitlab.svg';
+import google from 'sentry-logos/logo-google.svg';
+import jiraserver from 'sentry-logos/logo-jira-server.svg';
+import msteams from 'sentry-logos/logo-msteams.svg';
+import okta from 'sentry-logos/logo-okta.svg';
+import onelogin from 'sentry-logos/logo-onelogin.svg';
+import rippling from 'sentry-logos/logo-rippling.svg';
+import saml2 from 'sentry-logos/logo-saml2.svg';
+import sentry from 'sentry-logos/logo-sentry.svg';
+import slack from 'sentry-logos/logo-slack.svg';
+import visualstudio from 'sentry-logos/logo-visualstudio.svg';
+
+// Map of plugin id -> logo filename
+export const DEFAULT_ICON = placeholder;
+export const ICON_PATHS = {
+  _default: DEFAULT_ICON,
+  sentry,
+  browsers: sentry,
+  device: sentry,
+  interface_types: sentry,
+  os: sentry,
+  urls: sentry,
+  webhooks: sentry,
+
+  'active-directory': vsts,
+  asana,
+  auth0,
+  bitbucket_server: bitbucketserver,
+  bitbucket,
+  github_enterprise: githubEnterprise,
+  github,
+  gitlab,
+  google,
+  jira_server: jiraserver,
+  msteams,
+  okta,
+  onelogin,
+  rippling,
+  saml2,
+  slack,
+  visualstudio,
+  vsts,
+};
+
+type Props = {
+  pluginId?: string;
+  size?: number;
+};
+
+const IdentityIcon = styled('div')<Props>`
+  position: relative;
+  height: ${p => p.size}px;
+  width: ${p => p.size}px;
+  border-radius: 2px;
+  border: 0;
+  display: inline-block;
+  background-size: contain;
+  background-position: center center;
+  background-repeat: no-repeat;
+  background-image: url(${({pluginId}) =>
+    (pluginId !== undefined && ICON_PATHS[pluginId]) || DEFAULT_ICON});
+`;
+
+IdentityIcon.defaultProps = {
+  pluginId: '_default',
+  size: 36,
+};
+
+export default IdentityIcon;
