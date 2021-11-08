e8233b3ae6df4e539877d8d7a6d75a0bf582b32c
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-11-08 15:57:43 -0800

    Create digest message builder

--- src/sentry/integrations/slack/message_builder/notifications.py
+++ src/sentry/integrations/slack/message_builder/notifications.py
@@ -2,17 +2,21 @@ from __future__ import annotations
 
 from typing import Any, Mapping
 
+from sentry.digests import Digest
+from sentry.digests.utils import get_groups
 from sentry.integrations.slack.message_builder import SlackBody
 from sentry.integrations.slack.message_builder.base.base import SlackMessageBuilder
 from sentry.integrations.slack.message_builder.issues import SlackIssuesMessageBuilder
 from sentry.models import Team, User
 from sentry.notifications.notifications.base import BaseNotification, ProjectNotification
+from sentry.notifications.notifications.digest import DigestNotification
 from sentry.utils import json
 
 
 def get_message_builder(klass: str) -> type[SlackNotificationsMessageBuilder]:
     """TODO(mgaeta): HACK to get around circular imports."""
     return {
+        "DigestNotificationMessageBuilder": DigestNotificationMessageBuilder,
         "IssueNotificationMessageBuilder": IssueNotificationMessageBuilder,
         "SlackNotificationsMessageBuilder": SlackNotificationsMessageBuilder,
     }[klass]
@@ -63,3 +67,32 @@ class IssueNotificationMessageBuilder(SlackNotificationsMessageBuilder):
             notification=self.notification,
             recipient=self.recipient,
         ).build()
+
+
+class DigestNotificationMessageBuilder(SlackNotificationsMessageBuilder):
+    def __init__(
+        self,
+        notification: DigestNotification,
+        context: Mapping[str, Any],
+        recipient: Team | User,
+    ) -> None:
+        super().__init__(notification, context, recipient)
+        self.notification: DigestNotification = notification
+
+    def build(self) -> SlackBody:
+        """
+        It's currently impossible in mypy to have recursive types so we need a
+        hack to get this to return a SlackBody.
+        """
+        digest: Digest = self.context.get(self.recipient.actor_id, {}).get("digest", {})
+        return [
+            SlackIssuesMessageBuilder(  # type: ignore
+                group=group,
+                event=event,
+                rules=[rule],
+                issue_details=True,
+                notification=self.notification,
+                recipient=self.recipient,
+            ).build()
+            for rule, group, event in get_groups(digest)
+        ]
