e21a1444eb660c92a9213f2d26ed4c89a2a5a85f
Stephen Cefali <scefali@sentry.io>
2021-11-09 15:26:16 -0800

    fix provider name in analytics (#29885)
    This PR fixes a bug where the provider in certain analytics events looks like ExternalProviders.EMAIL instead of just email

--- src/sentry/notifications/notifications/invitations/abstract_invite_request.py
+++ src/sentry/notifications/notifications/invitations/abstract_invite_request.py
@@ -90,5 +90,5 @@ class AbstractInviteRequestNotification(OrganizationRequestNotification):
             organization_id=self.organization.id,
             user_id=self.pending_member.inviter.id if self.pending_member.inviter else None,
             target_user_id=recipient.id,
-            providers=provider,
+            providers=provider.name.lower(),
         )
