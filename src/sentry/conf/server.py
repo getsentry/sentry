c6466d15a0e31644f89b1d1ad85832467d3d5c1d
Edward Gou <edward.gou@sentry.io>
2021-11-10 16:14:52 -0500

    feature flags for issues in dashboards

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1008,6 +1008,8 @@ SENTRY_FEATURES = {
     "organizations:dashboards-edit": True,
     # Enable dashboard widget library
     "organizations:widget-library": False,
+    # Enable issue widgets in dashboards
+    "organizations:issues-in-dashboards": True,
     # Enable navigation features between Discover and Dashboards
     "organizations:connect-discover-and-dashboards": False,
     # Enable experimental performance improvements.
