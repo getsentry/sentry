a5ae97730aa05444809fd97351163d52f7e637e8
Scott Cooper <scttcper@gmail.com>
2021-11-12 09:48:10 -0800

    remove createWrapper

--- tests/js/spec/components/organizations/timeRangeSelector/dateSummary.spec.tsx
+++ tests/js/spec/components/organizations/timeRangeSelector/dateSummary.spec.tsx
@@ -1,31 +1,29 @@
-import {mountWithTheme, screen} from 'sentry-test/reactTestingLibrary';
+import {cleanup, mountWithTheme, screen} from 'sentry-test/reactTestingLibrary';
 
 import DateSummary from 'app/components/organizations/timeRangeSelector/dateSummary';
 
 const start = new Date('2017-10-14T02:38:00.000Z');
 const end = new Date('2017-10-17T02:38:00.000Z'); // National Pasta Day
 
-describe('DateSummary', function () {
-  const getComponent = (props = {}) => <DateSummary start={start} end={end} {...props} />;
-  const createWrapper = (props = {}) => mountWithTheme(getComponent(props));
-
-  it('renders', async function () {
-    const {container} = createWrapper();
+describe('DateSummary', () => {
+  it('renders', async () => {
+    const {container} = mountWithTheme(<DateSummary start={start} end={end} />);
     expect(container).toSnapshot();
   });
 
-  it('does not show times when it is midnight for start date and 23:59:59 for end date', function () {
-    const {rerender} = createWrapper();
-    // Search by year because date may change depending on timezone
+  it('does not show times when it is midnight for start date and 23:59:59 for end date', () => {
+    mountWithTheme(<DateSummary start={start} end={end} />);
+    // Search by year because day may change depending on timezone
     expect(screen.getAllByText(/Oct.+2017/)[0].childElementCount).toBe(1);
+    cleanup();
 
     // Date Summary formats using system time
     // tests run on EST/EDT
-    rerender(
-      getComponent({
-        start: new Date('2017-10-14T00:00:00.000-0400'),
-        end: new Date('2017-10-17T23:59:59.000-0400'),
-      })
+    mountWithTheme(
+      <DateSummary
+        start={new Date('2017-10-14T00:00:00.000-0400')}
+        end={new Date('2017-10-17T23:59:59.000-0400')}
+      />
     );
 
     expect(screen.getAllByText(/Oct.+2017/)[0].childElementCount).toBe(0);
