9982050e6b79e392d2490de1f96f0d01c4e6ef97
Dan Fuller <dfuller@sentry.io>
2021-11-12 17:02:53 -0800

    fix(insights): Change issue breakdown and time to resolution endpoints to filter to assigned groups
    Currently these endpoints just filter to groups that belong to projects that your team is associated
    with. We'd prefer this to filter to groups that your team or team members are directly assigned to
    instead.

--- tests/sentry/models/test_grouphistory.py
+++ tests/sentry/models/test_grouphistory.py
@@ -1,5 +1,23 @@
-from sentry.models import GroupHistoryStatus, get_prev_history
+from sentry.models import GroupAssignee, GroupHistory, GroupHistoryStatus, get_prev_history
 from sentry.testutils import TestCase
+
+
+class FilterToTeamTest(TestCase):
+    def test(self):
+        other_org = self.create_organization()
+        other_team = self.create_team(other_org, members=[self.user])
+        other_project = self.create_project(organization=other_org, teams=[other_team])
+        GroupAssignee.objects.assign(self.group, self.user)
+
+        history = GroupHistory.objects.filter(group=self.group).get()
+        other_group = self.store_event(data={}, project_id=other_project.id).group
+        GroupAssignee.objects.assign(other_group, self.user)
+        other_history = GroupHistory.objects.filter(group=other_group).get()
+
+        # Even though the user is a member of both orgs, and is assigned to both groups, we should
+        # filter down to just the history that each team has access to here.
+        assert list(GroupHistory.objects.filter_to_team(self.team)) == [history]
+        assert list(GroupHistory.objects.filter_to_team(other_team)) == [other_history]
 
 
 class GetPrevHistoryTest(TestCase):
