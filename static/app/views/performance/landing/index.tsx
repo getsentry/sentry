8ad297a1f8338c8d1e14c170e91c131d2ed894b4
k-fish <kevan.fisher@sentry.io>
2021-11-15 13:32:07 -0500

    Add query batching to singleFieldAreaWidget and add code to support it in EventsRequest

--- static/app/views/performance/landing/index.tsx
+++ static/app/views/performance/landing/index.tsx
@@ -17,6 +17,7 @@ import space from 'app/styles/space';
 import {Organization, Project} from 'app/types';
 import EventView from 'app/utils/discover/eventView';
 import {generateAggregateFields} from 'app/utils/discover/fields';
+import {GenericQueryBatcher} from 'app/utils/performance/contexts/genericQueryBatcher';
 import {OpBreakdownFilterProvider} from 'app/utils/performance/contexts/operationBreakdownFilter';
 import useTeams from 'app/utils/useTeams';
 
@@ -150,7 +151,9 @@ export function PerformanceLanding(props: Props) {
                 selectedTeams={['myteams']}
                 selectedProjects={eventView.project.map(String)}
               >
-                <ViewComponent {...props} />
+                <GenericQueryBatcher>
+                  <ViewComponent {...props} />
+                </GenericQueryBatcher>
               </TeamKeyTransactionManager.Provider>
             ) : (
               <LoadingIndicator />
