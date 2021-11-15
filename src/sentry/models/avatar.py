764be205fd4935f66a3fd3e002eba63853a72b6a
Colleen O'Rourke <colleen@sentry.io>
2021-11-15 14:04:25 -0800

    ref(sentryappavatar): Add back avatar_type (#30028)
    * ref(sentryappavatar): Add back avatar_type

--- src/sentry/models/avatar.py
+++ src/sentry/models/avatar.py
@@ -13,8 +13,8 @@ from sentry.utils.db import atomic_transaction
 class AvatarBase(Model):
     """
     Base class for UserAvatar, OrganizationAvatar, TeamAvatar,
-    and ProjectAvatar models. Associates those entities with their
-    avatar preferences/files.
+    SentryAppAvatar, and ProjectAvatar models. Associates those entities with their
+    avatar preferences/files. If extending this class, ensure the model has avatar_type.
     """
 
     __include_in_export__ = False
