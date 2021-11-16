9899ff2a2dc76f9700e5daa40dd40961b4d5228f
Tony Xiao <txiao@sentry.io>
2021-11-16 11:30:12 -0500

    feat(suspect-spans): Show span op being filtered (#30019)
    This shows the span op being filtered on in the filter button.

--- tests/js/spec/views/performance/transactionSpans/opsFilter.spec.tsx
+++ tests/js/spec/views/performance/transactionSpans/opsFilter.spec.tsx
@@ -99,4 +99,29 @@ describe('Performance > Transaction Spans', function () {
     expect(handleOpChange).toHaveBeenCalledTimes(1);
     expect(handleOpChange).toHaveBeenCalledWith('op1');
   });
+
+  it('shows op being filtered on', async function () {
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/events-span-ops/',
+      body: [{op: 'op1'}, {op: 'op2'}],
+    });
+
+    const initialData = initializeData({query: {spanOp: 'op1'}});
+
+    const handleOpChange = jest.fn();
+
+    mountWithTheme(
+      <OpsFilter
+        location={initialData.router.location}
+        eventView={createEventView(initialData.router.location)}
+        organization={initialData.organization}
+        handleOpChange={handleOpChange}
+        transactionName="Test Transaction"
+      />,
+      {context: initialData.routerContext}
+    );
+
+    const filter = (await screen.findByText('Filter -')).parentElement;
+    expect(filter).toHaveTextContent('Filter - op1');
+  });
 });
