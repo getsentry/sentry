75d709688aa5e4ded920389bef00b739567f6b7c
Colleen O'Rourke <colleen@sentry.io>
2021-11-16 17:06:03 -0800

    feat(sentry apps): Don't allow publication without logos

--- src/sentry/api/endpoints/sentry_app_publish_request.py
+++ src/sentry/api/endpoints/sentry_app_publish_request.py
@@ -4,11 +4,13 @@ from sentry import options
 from sentry.api.bases.sentryapps import SentryAppBaseEndpoint
 from sentry.constants import SentryAppStatus
 from sentry.mediators.sentry_apps import Updater
+from sentry.models import SentryAppAvatar
 from sentry.utils import email
 
 
 class SentryAppPublishRequestEndpoint(SentryAppBaseEndpoint):
     def post(self, request, sentry_app):
+
         # check status of app to make sure it is unpublished
         if sentry_app.is_published:
             return Response({"detail": "Cannot publish already published integration."}, status=400)
@@ -18,6 +20,19 @@ class SentryAppPublishRequestEndpoint(SentryAppBaseEndpoint):
 
         if sentry_app.is_publish_request_inprogress:
             return Response({"detail": "Publish request in progress."}, status=400)
+
+        # TODO: add feature flag after other PR is merged
+        if not SentryAppAvatar.objects.filter(sentry_app=sentry_app, color=True).exists():
+            return Response({"detail": "Must upload a logo for the integration."}, status=400)
+
+        if (
+            self.is_issue_link_integration(sentry_app)
+            and not SentryAppAvatar.objects.filter(sentry_app=sentry_app, color=False).exists()
+        ):
+            return Response(
+                {"detail": "Must upload a black and white logo for issue linking integrations."},
+                status=400,
+            )
 
         Updater.run(
             user=request.user,
@@ -41,3 +56,14 @@ class SentryAppPublishRequestEndpoint(SentryAppBaseEndpoint):
         )
 
         return Response(status=201)
+
+    def is_issue_link_integration(self, sentry_app):
+        """Determine if the sentry app supports issue linking"""
+        if not sentry_app.schema:
+            return False
+
+        for element in sentry_app.schema.get("elements"):
+            if element.get("type") and element.get("type") == "issue-link":
+                return True
+
+        return False
