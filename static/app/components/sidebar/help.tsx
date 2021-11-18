f1efe638e7367e42e5a580be78f1ff7b64577765
Mark Story <mark@sentry.io>
2021-11-18 11:25:11 -0500

    fix(ui) Fix positioning of help popover in mobile layout (#30052)
    Position menu so it is in the viewport.
    
    Fixes #29986

--- static/app/components/sidebar/help.tsx
+++ static/app/components/sidebar/help.tsx
@@ -6,6 +6,7 @@ import Hook from 'app/components/hook';
 import SidebarItem from 'app/components/sidebar/sidebarItem';
 import {IconQuestion} from 'app/icons';
 import {t} from 'app/locale';
+import space from 'app/styles/space';
 import {Organization} from 'app/types';
 
 import SidebarDropdownMenu from './sidebarDropdownMenu.styled';
@@ -33,7 +34,7 @@ const SidebarHelp = ({orientation, collapsed, hidePanel, organization}: Props) =
         </HelpActor>
 
         {isOpen && (
-          <HelpMenu {...getMenuProps({})}>
+          <HelpMenu {...getMenuProps({})} orientation={orientation}>
             <SidebarMenuItem
               data-test-id="search-docs-and-faqs"
               onClick={() => openHelpSearchModal({organization})}
@@ -62,7 +63,9 @@ const HelpRoot = styled('div')`
 // how refs are handled.
 const HelpActor = styled('div')``;
 
-const HelpMenu = styled('div')`
+const HelpMenu = styled('div', {shouldForwardProp: p => p !== 'orientation'})<{
+  orientation: Props['orientation'];
+}>`
   ${SidebarDropdownMenu};
-  bottom: 100%;
+  ${p => (p.orientation === 'left' ? 'bottom: 100%' : `top: ${space(4)}; right: 0px;`)}
 `;
