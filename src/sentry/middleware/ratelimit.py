3335d90f68076e0285a36b2ec6ffcd679174fb2d
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2021-11-18 12:57:30 -0800

    feat(ratelimit): Create a global ratelimit key generation function (#29774)

--- src/sentry/middleware/ratelimit.py
+++ src/sentry/middleware/ratelimit.py
@@ -5,16 +5,48 @@
 
 from __future__ import annotations
 
+from django.conf import settings
 from django.utils.deprecation import MiddlewareMixin
 from rest_framework.request import Request
 
-from sentry.api.helpers.group_index.index import EndpointFunction, build_rate_limit_key
+from sentry.api.helpers.group_index.index import EndpointFunction
 from sentry.app import ratelimiter
 
 
-def get_rate_limit_key(view_func: EndpointFunction, request: Request):
+def get_rate_limit_key(view_func: EndpointFunction, request: Request) -> str | None:
     """Construct a consistent global rate limit key using the arguments provided"""
-    return build_rate_limit_key(view_func, request)
+
+    view = view_func.__qualname__
+    http_method = request.method
+
+    # This avoids touching user session, which means we avoid
+    # setting `Vary: Cookie` as a response header which will
+    # break HTTP caching entirely.
+    if request.path_info.startswith(settings.ANONYMOUS_STATIC_PREFIXES):
+        return None
+
+    request_user = getattr(request, "user", None)
+    user_id = getattr(request_user, "id", None)
+    is_sentry_app = getattr(request_user, "is_sentry_app", None)
+
+    request_access = getattr(request, "access", None)
+    org_id = getattr(request_access, "organization_id", None)
+
+    ip_address = request.META.get("REMOTE_ADDR")
+
+    if is_sentry_app and org_id is not None:
+        category = "org"
+        id = org_id
+    elif user_id is not None:
+        category = "user"
+        id = user_id
+    elif ip_address is not None:
+        category = "ip"
+        id = ip_address
+    # If IP address doesn't exist, skip ratelimiting for now
+    else:
+        return None
+    return f"{category}:{view}:{http_method}:{id}"
 
 
 def get_default_rate_limit() -> tuple[int, int]:
@@ -49,5 +81,7 @@ class RatelimitMiddleware(MiddlewareMixin):
             return
 
         key = get_rate_limit_key(view_func, request)
-        request.will_be_rate_limited = above_rate_limit_check(key)["is_limited"]
+        request.will_be_rate_limited = (
+            above_rate_limit_check(key)["is_limited"] if key is not None else False
+        )
         return
