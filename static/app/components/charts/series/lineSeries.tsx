c53e2676b60f3aacf42de4c86ef47a371b980e2f
Scott Cooper <scttcper@gmail.com>
2021-11-18 16:13:27 -0800

    disable AreaSeries highlight on hover

--- static/app/components/charts/series/lineSeries.tsx
+++ static/app/components/charts/series/lineSeries.tsx
@@ -11,11 +11,17 @@ export default function LineSeries(props: LineSeriesOption): LineSeriesOption {
     ...props,
     type: 'line',
     emphasis: {
+      ...props.emphasis,
       scale: false,
       lineStyle: {
         // disable color highlight on hover
         color: props.color,
         width: undefined,
+      },
+      areaStyle: {
+        // Disable AreaSeries highlight on hover
+        color: props.areaStyle?.color ?? (props.color as string),
+        opacity: props.areaStyle?.opacity,
       },
     },
   };
