274d52a8f77bfdf7ab86bdb50e8b405726b352f2
Kelly Carino <kelly@sentry.io>
2021-11-18 16:03:44 -0800

    feat(ui): Add sidebarSection to storybook (#30044)
    * feat(ui): Add sidebarSection to storybook
    
    Add new sidebarSection component to Storybook.
    
    FIXES WOR-1480
    
    * move to Views
    
    * move to /views/ and break out into separate stories
    
    * update controls

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -159,6 +159,7 @@ addParameters({
         [
           'Layout - Narrow',
           'Layout - Thirds',
+          'Sidebar Section',
           'Modals',
           'Activity',
           'Empty States',
