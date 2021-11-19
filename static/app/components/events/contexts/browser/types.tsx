aa57f329d54c5e45a0a3b0169d581c22a44d7d31
mikellykels <kelly@sentry.io>
2021-11-18 16:38:27 -0800

    remove type from types

--- static/app/components/events/contexts/browser/types.tsx
+++ static/app/components/events/contexts/browser/types.tsx
@@ -1,11 +1,9 @@
 export enum BrowserKnownDataType {
   NAME = 'name',
   VERSION = 'version',
-  TYPE = 'type',
 }
 
 export type BrowserKnownData = {
   name: string;
   version: string;
-  type: string;
 };
