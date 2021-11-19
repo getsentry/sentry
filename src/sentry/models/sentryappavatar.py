518b4da40b9e7f52ab737a93a3b0d41ec1535f24
Colleen O'Rourke <colleen@sentry.io>
2021-11-19 15:13:18 -0800

    need AVATAR_TYPES

--- src/sentry/models/sentryappavatar.py
+++ src/sentry/models/sentryappavatar.py
@@ -22,12 +22,12 @@ class SentryAppAvatar(AvatarBase):
     and specifies which type of logo it is.
     """
 
+    AVATAR_TYPES = SentryAppAvatarTypes.get_choices()
+
     FILE_TYPE = "avatar.file"
 
     sentry_app = FlexibleForeignKey("sentry.SentryApp", related_name="avatar")
-    avatar_type = models.PositiveSmallIntegerField(
-        default=0, choices=SentryAppAvatarTypes.get_choices()
-    )
+    avatar_type = models.PositiveSmallIntegerField(default=0, choices=AVATAR_TYPES)
     color = models.BooleanField(default=False)
     # e.g. issue linking logos will not have color
 
