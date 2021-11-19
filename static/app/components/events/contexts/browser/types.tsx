2d93d34491829e2e4e64f353c15234d6fa8f1c0d
mikellykels <kelly@sentry.io>
2021-11-18 16:33:10 -0800

    feat(ui): Add context type for browser
    Add known browser context for issue details.
    
    FIXES WOR-1163

---
+++ static/app/components/events/contexts/browser/types.tsx
@@ -0,0 +1,11 @@
+export enum BrowserKnownDataType {
+  NAME = 'name',
+  VERSION = 'version',
+  TYPE = 'type',
+}
+
+export type BrowserKnownData = {
+  name: string;
+  version: string;
+  type: string;
+};
