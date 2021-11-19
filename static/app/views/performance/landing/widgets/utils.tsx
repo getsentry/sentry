86aabf0962b81eb41a96aa95306eb92b9c61a649
Kev <kevan.fisher@sentry.io>
2021-11-19 11:42:59 -0800

    ref(perf): Change landing widget dropdown to exclude existing (#30102)
    This will exclude existing chart types from the dropdowns on the landing page.

--- static/app/views/performance/landing/widgets/utils.tsx
+++ static/app/views/performance/landing/widgets/utils.tsx
@@ -1,3 +1,7 @@
+import {PROJECT_PERFORMANCE_TYPE} from '../../utils';
+
+import {PerformanceWidgetSetting} from './widgetDefinitions';
+
 export const eventsRequestQueryProps = [
   'children',
   'organization',
@@ -8,3 +12,57 @@ export const eventsRequestQueryProps = [
   'environment',
   'project',
 ] as const;
+
+function setWidgetStorageObject(localObject: Record<string, string>) {
+  localStorage.setItem(getContainerLocalStorageObjectKey, JSON.stringify(localObject));
+}
+
+const getContainerLocalStorageObjectKey = 'landing-chart-container';
+const getContainerKey = (
+  index: number,
+  performanceType: PROJECT_PERFORMANCE_TYPE,
+  height: number
+) => `landing-chart-container#${performanceType}#${height}#${index}`;
+
+function getWidgetStorageObject() {
+  const localObject = JSON.parse(
+    localStorage.getItem(getContainerLocalStorageObjectKey) || '{}'
+  );
+  return localObject;
+}
+
+export const getChartSetting = (
+  index: number,
+  height: number,
+  performanceType: PROJECT_PERFORMANCE_TYPE,
+  defaultType: PerformanceWidgetSetting,
+  forceDefaultChartSetting?: boolean // Used for testing.
+): PerformanceWidgetSetting => {
+  if (forceDefaultChartSetting) {
+    return defaultType;
+  }
+  const key = getContainerKey(index, performanceType, height);
+  const localObject = getWidgetStorageObject();
+  const value = localObject?.[key];
+
+  if (
+    value &&
+    Object.values(PerformanceWidgetSetting).includes(value as PerformanceWidgetSetting)
+  ) {
+    const _value: PerformanceWidgetSetting = value as PerformanceWidgetSetting;
+    return _value;
+  }
+  return defaultType;
+};
+export const _setChartSetting = (
+  index: number,
+  height: number,
+  performanceType: PROJECT_PERFORMANCE_TYPE,
+  setting: PerformanceWidgetSetting
+) => {
+  const key = getContainerKey(index, performanceType, height);
+  const localObject = getWidgetStorageObject();
+  localObject[key] = setting;
+
+  setWidgetStorageObject(localObject);
+};
