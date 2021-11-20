4397a2076769d8367cb41e998fe289f23f81a7f9
Richard Ma <maxiuyuan1998@gmail.com>
2021-11-19 16:00:19 -0800

    fixed be test

--- src/sentry/api/endpoints/authenticator_index.py
+++ src/sentry/api/endpoints/authenticator_index.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
+from sentry import features
 from sentry.api.base import Endpoint
 from sentry.models import Authenticator
 
@@ -13,6 +14,7 @@ class AuthenticatorIndexEndpoint(Endpoint):
 
         # Currently just expose u2f challenge, not sure if it's necessary to list all
         # authenticator interfaces that are enabled
+        # this is a weird endpoint, looks like its only being used in test_user_authenticators_index.py
         try:
             interface = Authenticator.objects.get_interface(request.user, "u2f")
             if not interface.is_enrolled():
@@ -20,7 +22,20 @@ class AuthenticatorIndexEndpoint(Endpoint):
         except LookupError:
             return Response([])
 
-        challenge = interface.activate(request._request).challenge
+        orgs = request.user.get_orgs()
+        webauthn_ff = (
+            True
+            if any(
+                features.has("organizations:webauthn-login", org, actor=request.user)
+                for org in orgs
+            )
+            or any(
+                features.has("organizations:webauthn-register", org, actor=request.user)
+                for org in orgs
+            )
+            else False
+        )
+        challenge = interface.activate(request._request, webauthn_ff).challenge
 
         # I don't think we currently support multiple interfaces of the same type
         # but just future proofing I guess
