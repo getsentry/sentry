b4d3c21be401db8f6bbfb90e408275733980f17a
Marcos Gaeta <marcos.gaeta@sentry.io>
2021-11-23 16:17:50 -0800

    fix pyright

--- src/sentry/api/serializers/models/project.py
+++ src/sentry/api/serializers/models/project.py
@@ -530,14 +530,16 @@ class ProjectSummarySerializer(ProjectWithTeamSerializer):
             for release in bulk_fetch_project_latest_releases(item_list)
         }
 
+        deploys_by_project = None
         if not self._collapse(LATEST_DEPLOYS_KEY):
             deploys_by_project = self.get_deploys_by_project(item_list)
+
         for item in item_list:
             attrs[item]["latest_release"] = latest_release_versions.get(item.id)
-            if not self._collapse(LATEST_DEPLOYS_KEY):
-                attrs[item]["deploys"] = deploys_by_project.get(item.id)
             attrs[item]["environments"] = environments_by_project.get(item.id, [])
             attrs[item]["has_user_reports"] = item.id in projects_with_user_reports
+            if not self._collapse(LATEST_DEPLOYS_KEY):
+                attrs[item]["deploys"] = deploys_by_project.get(item.id)
 
         return attrs
 
