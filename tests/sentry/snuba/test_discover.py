286d9adba8e80fa4ae102caa9bbcecd5d3ca4bdf
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2021-11-24 12:42:38 -0500

    Update tests/sentry/snuba/test_discover.py
    Co-authored-by: Tony Xiao <txiao@sentry.io>

--- tests/sentry/snuba/test_discover.py
+++ tests/sentry/snuba/test_discover.py
@@ -3176,7 +3176,7 @@ class QueryIntegrationTest(SnubaTestCase, TestCase):
         project2 = self.create_project(organization=self.organization)
         event_data = load_data("transaction", timestamp=before_now(seconds=3))
         self.store_event(data=event_data, project_id=project2.id)
-        expected = ["bar", project2.slug]
+        expected = [self.project.slug, project2.slug]
 
         result = discover.query(
             selected_columns=["project"],
