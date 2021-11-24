0871db3195251bafe4b6f79d30edd6961fc8bda4
Betty Da <bda@sentry.io>
2021-11-24 18:07:34 -0500

    typing

--- src/sentry/processing/realtime_metrics/dummy.py
+++ src/sentry/processing/realtime_metrics/dummy.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Iterable, Set
+from typing import Any, Iterable, Set
 
 from . import base
 
@@ -7,7 +7,7 @@ logger = logging.getLogger(__name__)
 
 
 class DummyRealtimeMetricsStore(base.RealtimeMetricsStore):
-    def __init__(self, **kwargs) -> None:
+    def __init__(self, **kwargs: Any) -> None:
         pass
 
     def increment_project_event_counter(self, project_id: int, timestamp: int) -> None:
@@ -30,7 +30,7 @@ class DummyRealtimeMetricsStore(base.RealtimeMetricsStore):
         return base.BucketedDurationsHistograms(timestamp=-1, width=0, histograms=[])
 
     def get_lpq_projects(self) -> Set[int]:
-        return []
+        return set()
 
     def is_lpq_project(self, project_id: int) -> bool:
         return False
