5827c03347cb35b4f43b1f06db710ba62c3a1c3a
Tony Xiao <txiao@sentry.io>
2021-11-29 13:41:15 -0500

    fix(performance): Duration chart in transaction overview should refle… (#30243)
    The duration chart in transaction overview is always showing the transaction
    duration regardless of the current filter. This change ensures that when a span
    op filter is selected, the duration chart is updated to reflect the selection.

--- static/app/views/performance/transactionSummary/filter.tsx
+++ static/app/views/performance/transactionSummary/filter.tsx
@@ -27,6 +27,15 @@ export enum SpanOperationBreakdownFilter {
   Resource = 'resource',
 }
 
+export const SPAN_OPERATION_BREAKDOWN_FILTER_TO_FIELD: Partial<
+  Record<SpanOperationBreakdownFilter, string>
+> = {
+  [SpanOperationBreakdownFilter.Http]: 'spans.http',
+  [SpanOperationBreakdownFilter.Db]: 'spans.db',
+  [SpanOperationBreakdownFilter.Browser]: 'spans.browser',
+  [SpanOperationBreakdownFilter.Resource]: 'spans.resource',
+};
+
 const OPTIONS: SpanOperationBreakdownFilter[] = [
   SpanOperationBreakdownFilter.Http,
   SpanOperationBreakdownFilter.Db,
