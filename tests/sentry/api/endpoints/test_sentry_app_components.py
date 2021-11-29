934385991f815d3882fe5e9bd127a7e7e2c7d4a5
Leander Rodrigues <leander.rodrigues@sentry.io>
2021-11-29 15:52:12 -0800

    ✅ Fix failing test

--- tests/sentry/api/endpoints/test_sentry_app_components.py
+++ tests/sentry/api/endpoints/test_sentry_app_components.py
@@ -2,6 +2,7 @@ from unittest.mock import call, patch
 
 from sentry.constants import SentryAppInstallationStatus
 from sentry.coreapi import APIError
+from sentry.models.sentryappavatar import get_sentry_app_avatars
 from sentry.testutils import APITestCase
 
 
@@ -35,7 +36,7 @@ class SentryAppComponentsTest(APITestCase):
                 "uuid": self.sentry_app.uuid,
                 "slug": self.sentry_app.slug,
                 "name": self.sentry_app.name,
-                "avatars": self.sentry_app.avatars,
+                "avatars": get_sentry_app_avatars(sentry_app=self.sentry_app),
             },
         }
 
@@ -95,7 +96,7 @@ class OrganizationSentryAppComponentsTest(APITestCase):
                 "uuid": self.sentry_app1.uuid,
                 "slug": self.sentry_app1.slug,
                 "name": self.sentry_app1.name,
-                "avatars": self.sentry_app1.avatars,
+                "avatars": get_sentry_app_avatars(sentry_app=self.sentry_app1),
             },
         }
 
@@ -107,7 +108,7 @@ class OrganizationSentryAppComponentsTest(APITestCase):
                 "uuid": self.sentry_app2.uuid,
                 "slug": self.sentry_app2.slug,
                 "name": self.sentry_app2.name,
-                "avatars": self.sentry_app2.avatars,
+                "avatars": get_sentry_app_avatars(sentry_app=self.sentry_app2),
             },
         }
 
@@ -149,7 +150,7 @@ class OrganizationSentryAppComponentsTest(APITestCase):
                     "uuid": sentry_app.uuid,
                     "slug": sentry_app.slug,
                     "name": sentry_app.name,
-                    "avatars": sentry_app.avatars,
+                    "avatars": get_sentry_app_avatars(sentry_app=sentry_app),
                 },
             }
         ]
@@ -182,7 +183,7 @@ class OrganizationSentryAppComponentsTest(APITestCase):
                     "uuid": self.sentry_app2.uuid,
                     "slug": self.sentry_app2.slug,
                     "name": self.sentry_app2.name,
-                    "avatars": self.sentry_app2.avatars,
+                    "avatars": get_sentry_app_avatars(sentry_app=self.sentry_app2),
                 },
             }
         ]
