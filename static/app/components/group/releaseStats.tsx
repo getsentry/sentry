42e44d2b0f5314d8978022e8378dd64202b11e61
Stephen Cefali <scefali@sentry.io>
2021-11-29 09:27:58 -0800

    temp checkin

--- static/app/components/group/releaseStats.tsx
+++ static/app/components/group/releaseStats.tsx
@@ -1,15 +1,19 @@
 import {Fragment, memo} from 'react';
 import styled from '@emotion/styled';
+import moment from 'moment';
 
+import {openAddDashboardWidgetModal} from 'sentry/actionCreators/modal';
+import Feature from 'sentry/components/acl/feature';
 import GroupReleaseChart from 'sentry/components/group/releaseChart';
 import SeenInfo from 'sentry/components/group/seenInfo';
 import Placeholder from 'sentry/components/placeholder';
 import Tooltip from 'sentry/components/tooltip';
-import {IconQuestion} from 'sentry/icons';
+import {IconAdd, IconQuestion} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import space from 'sentry/styles/space';
 import {CurrentRelease, Environment, Group, Organization, Project} from 'sentry/types';
 import getDynamicText from 'sentry/utils/getDynamicText';
+import {DashboardWidgetSource, DisplayType} from 'sentry/views/dashboardsV2/types';
 
 import SidebarSection from './sidebarSection';
 
@@ -47,6 +51,53 @@ const GroupReleaseStats = ({
   const hasRelease = new Set(project.features).has('releases');
   const releaseTrackingUrl = `/settings/${organization.slug}/projects/${project.slug}/release-tracking/`;
 
+  const time30dAgo = moment().subtract(30, 'days');
+  const time24hrsAgo = moment().subtract(24, 'hours');
+
+  const generateGroupReleaseChartTitle = (title: string, startTime: moment.Moment) => {
+    const handleAddToDashboard = () => {
+      group &&
+        openAddDashboardWidgetModal({
+          organization,
+          widget: {
+            title: group.title,
+            displayType: DisplayType.AREA,
+            interval: '1h',
+            queries: [
+              {
+                name: '',
+                fields: ['count()'],
+                conditions: `issue.id:${group.id}`,
+                orderby: '',
+              },
+            ],
+          },
+          start: startTime.toDate().toString(),
+          end: moment().toDate().toString(),
+          source: DashboardWidgetSource.ISSUE_DETAILS,
+        });
+    };
+
+    return (
+      <Feature
+        features={[
+          'organizations:create-dashboard-widget-from-issue',
+          'organizations:dashboards-edit',
+        ]}
+        organization={organization}
+        renderDisabled={() => title}
+      >
+        <GroupReleaseChartWrapper>
+          <span>{title}</span>
+          <AddToDashboard onClick={handleAddToDashboard}>
+            <IconAdd size="9px" />
+            {t('Add to Dashboard')}
+          </AddToDashboard>
+        </GroupReleaseChartWrapper>
+      </Feature>
+    );
+  };
+
   return (
     <SidebarSection title={<span data-test-id="env-label">{environmentLabel}</span>}>
       {!group || !allEnvironments ? (
@@ -60,7 +111,7 @@ const GroupReleaseStats = ({
             release={currentRelease?.release}
             releaseStats={currentRelease?.stats}
             statsPeriod="24h"
-            title={t('Last 24 Hours')}
+            title={generateGroupReleaseChartTitle(t('Last 24 Hours'), time24hrsAgo)}
             firstSeen={group.firstSeen}
             lastSeen={group.lastSeen}
           />
@@ -71,7 +122,7 @@ const GroupReleaseStats = ({
             release={currentRelease?.release}
             releaseStats={currentRelease?.stats}
             statsPeriod="30d"
-            title={t('Last 30 Days')}
+            title={generateGroupReleaseChartTitle(t('Last 30 Days'), time30dAgo)}
             className="bar-chart-small"
             firstSeen={group.firstSeen}
             lastSeen={group.lastSeen}
@@ -161,4 +212,22 @@ const TooltipWrapper = styled('span')`
 const StyledIconQuest = styled(IconQuestion)`
   position: relative;
   top: 2px;
+`;
+
+const GroupReleaseChartWrapper = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+`;
+
+const AddToDashboard = styled('span')`
+  color: ${p => p.theme.blue300};
+  display: grid;
+  vertical-align: baseline;
+  grid-auto-flow: column;
+  align-items: center;
+`;
+
+const InnerTitleWrapper = styled('span')`
+  margin: auto;
 `;
