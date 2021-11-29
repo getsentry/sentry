22520627d0e42b1a921b1ff8a5330a1251db2a24
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-11-29 15:42:10 -0800

    ref(gsh): Move GSH initialization to gsh/index w/ useEffect

--- static/app/components/organizations/globalSelectionHeader/index.tsx
+++ static/app/components/organizations/globalSelectionHeader/index.tsx
@@ -1,28 +1,45 @@
-import * as React from 'react';
+import {ComponentPropsWithoutRef, useEffect, useRef} from 'react';
 import {withRouter, WithRouterProps} from 'react-router';
+import isEqual from 'lodash/isEqual';
 import partition from 'lodash/partition';
 
+import {
+  initializeUrlState,
+  updateDateTime,
+  updateEnvironments,
+  updateProjects,
+} from 'sentry/actionCreators/globalSelection';
+import {DATE_TIME_KEYS} from 'sentry/constants/globalSelectionHeader';
 import ConfigStore from 'sentry/stores/configStore';
 import {Organization, Project} from 'sentry/types';
 import withOrganization from 'sentry/utils/withOrganization';
 import withProjects from 'sentry/utils/withProjects';
 
 import GlobalSelectionHeader from './globalSelectionHeader';
-import InitializeGlobalSelectionHeader from './initializeGlobalSelectionHeader';
+import {getStateFromQuery} from './utils';
+
+const getDateObjectFromQuery = (query: Record<string, any>) =>
+  Object.fromEntries(
+    Object.entries(query).filter(([key]) => DATE_TIME_KEYS.includes(key))
+  );
 
 type GlobalSelectionHeaderProps = Omit<
-  React.ComponentPropsWithoutRef<typeof GlobalSelectionHeader>,
+  ComponentPropsWithoutRef<typeof GlobalSelectionHeader>,
   'router' | 'nonMemberProjects' | 'memberProjects' | 'selection'
 >;
 
 type Props = {
   organization: Organization;
   projects: Project[];
+  /**
+   * Skip loading from local storage
+   * An example is Issue Details, in the case where it is accessed directly (e.g. from email).
+   * We do not want to load the user's last used env/project in this case, otherwise will
+   * lead to very confusing behavior.
+   */
+  skipLoadLastUsed?: boolean;
 } & WithRouterProps &
-  GlobalSelectionHeaderProps &
-  Partial<
-    Pick<React.ComponentProps<typeof InitializeGlobalSelectionHeader>, 'skipLoadLastUsed'>
-  >;
+  GlobalSelectionHeaderProps;
 
 function GlobalSelectionHeaderContainer({
   organization,
@@ -41,6 +58,7 @@ function GlobalSelectionHeaderContainer({
 }: Props) {
   const {isSuperuser} = ConfigStore.get('user');
   const isOrgAdmin = organization.access.includes('org:admin');
+  const enforceSingleProject = !organization.features.includes('global-views');
 
   const specifiedProjects = specificProjectSlugs
     ? projects.filter(project => specificProjectSlugs.includes(project.slug))
@@ -53,41 +71,91 @@ function GlobalSelectionHeaderContainer({
 
   const nonMemberProjects = isSuperuser || isOrgAdmin ? otherProjects : [];
 
-  const enforceSingleProject = !organization.features.includes('global-views');
+  // Initializes GlobalSelectionHeader
+  //
+  // Calls an actionCreator to load project/environment from local storage if possible,
+  // otherwise populate with defaults.
+  //
+  // This should only happen when the header is mounted
+  // e.g. when changing views or organizations.
+  useEffect(() => {
+    // We can initialize before ProjectsStore is fully loaded if we don't need to
+    // enforce single project.
+    if (loadingProjects && (shouldForceProject || enforceSingleProject)) {
+      return;
+    }
 
-  // We can initialize before ProjectsStore is fully loaded if we don't need to enforce single project.
+    initializeUrlState({
+      organization,
+      queryParams: location.query,
+      router,
+      skipLoadLastUsed,
+      memberProjects,
+      defaultSelection,
+      forceProject,
+      shouldForceProject,
+      shouldEnforceSingleProject: enforceSingleProject,
+      showAbsolute,
+    });
+  }, [loadingProjects, shouldForceProject, enforceSingleProject]);
+
+  const lastQuery = useRef(location.query);
+
+  // This happens e.g. using browser's navigation button, in which case
+  // we need to update our store to reflect URL changes
+  useEffect(() => {
+    if (location.query === lastQuery.current) {
+      return;
+    }
+
+    const oldQuery = getStateFromQuery(lastQuery.current, {
+      allowEmptyPeriod: true,
+    });
+    const newQuery = getStateFromQuery(location.query, {
+      allowEmptyPeriod: true,
+    });
+
+    const newEnvironments = newQuery.environment || [];
+    const newDateObject = getDateObjectFromQuery(newQuery);
+    const oldDateObject = getDateObjectFromQuery(oldQuery);
+
+    /**
+     * Do not pass router to these actionCreators, as we do not want to update
+     * routes since these state changes are happening due to a change of routes
+     */
+    if (!isEqual(oldQuery.project, newQuery.project)) {
+      updateProjects(newQuery.project || [], null, {environments: newEnvironments});
+    } else if (!isEqual(oldQuery.environment, newQuery.environment)) {
+      /**
+       * When the project stays the same, it's still possible that the environment
+       * changed, so explictly update the enviornment
+       */
+      updateEnvironments(newEnvironments);
+    }
+
+    if (!isEqual(oldDateObject, newDateObject)) {
+      updateDateTime(newDateObject);
+    }
+
+    lastQuery.current = location.query;
+  }, [location.query]);
+
   return (
-    <React.Fragment>
-      {(!loadingProjects || (!shouldForceProject && !enforceSingleProject)) && (
-        <InitializeGlobalSelectionHeader
-          location={location}
-          skipLoadLastUsed={!!skipLoadLastUsed}
-          router={router}
-          organization={organization}
-          defaultSelection={defaultSelection}
-          forceProject={forceProject}
-          shouldForceProject={!!shouldForceProject}
-          shouldEnforceSingleProject={enforceSingleProject}
-          memberProjects={memberProjects}
-          showAbsolute={showAbsolute}
-        />
-      )}
-      <GlobalSelectionHeader
-        {...props}
-        loadingProjects={loadingProjects}
-        location={location}
-        organization={organization}
-        router={router}
-        routes={routes}
-        projects={projects}
-        shouldForceProject={!!shouldForceProject}
-        defaultSelection={defaultSelection}
-        forceProject={forceProject}
-        memberProjects={memberProjects}
-        nonMemberProjects={nonMemberProjects}
-        showAbsolute={showAbsolute}
-      />
-    </React.Fragment>
+    <GlobalSelectionHeader
+      {...props}
+      loadingProjects={loadingProjects}
+      location={location}
+      organization={organization}
+      router={router}
+      routes={routes}
+      projects={projects}
+      shouldForceProject={!!shouldForceProject}
+      defaultSelection={defaultSelection}
+      forceProject={forceProject}
+      memberProjects={memberProjects}
+      nonMemberProjects={nonMemberProjects}
+      showAbsolute={showAbsolute}
+    />
   );
 }
 
