cc4db24874bd363e357c20e320514cceaa91bb9e
Priscila Oliveira <priscilawebdev@gmail.com>
2021-11-29 10:30:05 +0100

    ref(performance-content): Convert performance content from class to func (#30061)

--- static/app/components/count.tsx
+++ static/app/components/count.tsx
@@ -9,7 +9,7 @@ function Count(props: Props) {
   const {value, className} = props;
 
   return (
-    <span className={className} title={value.toLocaleString()}>
+    <span className={className} title={value?.toLocaleString()}>
       {formatAbbreviatedNumber(value)}
     </span>
   );
