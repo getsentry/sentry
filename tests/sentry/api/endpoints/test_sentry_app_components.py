679c6fb1a8869fb870f24e81baf6fcfb668b1f75
Leander Rodrigues <leander.rodrigues@sentry.io>
2021-11-29 16:32:11 -0800

    🎨 consistency

--- tests/sentry/api/endpoints/test_sentry_app_components.py
+++ tests/sentry/api/endpoints/test_sentry_app_components.py
@@ -41,7 +41,7 @@ class SentryAppComponentsTest(APITestCase):
                 "uuid": self.sentry_app.uuid,
                 "slug": self.sentry_app.slug,
                 "name": self.sentry_app.name,
-                "avatars": get_sentry_app_avatars(sentry_app=self.sentry_app),
+                "avatars": get_sentry_app_avatars(self.sentry_app),
             },
         }
 
@@ -101,7 +101,7 @@ class OrganizationSentryAppComponentsTest(APITestCase):
                 "uuid": self.sentry_app1.uuid,
                 "slug": self.sentry_app1.slug,
                 "name": self.sentry_app1.name,
-                "avatars": get_sentry_app_avatars(sentry_app=self.sentry_app1),
+                "avatars": get_sentry_app_avatars(self.sentry_app1),
             },
         }
 
@@ -113,7 +113,7 @@ class OrganizationSentryAppComponentsTest(APITestCase):
                 "uuid": self.sentry_app2.uuid,
                 "slug": self.sentry_app2.slug,
                 "name": self.sentry_app2.name,
-                "avatars": get_sentry_app_avatars(sentry_app=self.sentry_app2),
+                "avatars": get_sentry_app_avatars(self.sentry_app2),
             },
         }
 
@@ -155,7 +155,7 @@ class OrganizationSentryAppComponentsTest(APITestCase):
                     "uuid": sentry_app.uuid,
                     "slug": sentry_app.slug,
                     "name": sentry_app.name,
-                    "avatars": get_sentry_app_avatars(sentry_app=sentry_app),
+                    "avatars": get_sentry_app_avatars(sentry_app),
                 },
             }
         ]
