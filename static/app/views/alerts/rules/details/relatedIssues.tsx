ea1c8432187fb2a93e2dfa6c5524cf31ad67522b
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2021-11-29 17:44:49 -0800

    fix(alerts): Don't make a related issues query for alert rule details (#30157)
    * fix(alerts): Don't make a related issues query for alert rule details
    
    * use search parser
    
    * finish comment
    
    * rebase

--- static/app/views/alerts/rules/details/relatedIssues.tsx
+++ static/app/views/alerts/rules/details/relatedIssues.tsx
@@ -16,7 +16,7 @@ import {OrganizationSummary, Project} from 'sentry/types';
 import {makeDefaultCta} from 'sentry/views/alerts/incidentRules/incidentRulePresets';
 import {IncidentRule} from 'sentry/views/alerts/incidentRules/types';
 import {
-  RELATED_ISSUES_QUERY_ERROR,
+  RELATED_ISSUES_BOOLEAN_QUERY_ERROR,
   RelatedIssuesNotAvailable,
 } from 'sentry/views/alerts/rules/details/relatedIssuesNotAvailable';
 import {isSessionAggregate} from 'sentry/views/alerts/utils';
@@ -35,7 +35,10 @@ class RelatedIssues extends Component<Props> {
   renderErrorMessage = ({detail}: {detail: string}, retry: () => void) => {
     const {rule, organization, projects, query, timePeriod} = this.props;
 
-    if (detail === RELATED_ISSUES_QUERY_ERROR && !isSessionAggregate(rule.aggregate)) {
+    if (
+      detail === RELATED_ISSUES_BOOLEAN_QUERY_ERROR &&
+      !isSessionAggregate(rule.aggregate)
+    ) {
       const ctaOpts = {
         orgSlug: organization.slug,
         projects,
