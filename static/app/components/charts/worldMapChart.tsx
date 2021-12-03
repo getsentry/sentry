8e5d317d86c0b7d94a0fe0d3b281e60b56b43f02
Scott Cooper <scttcper@gmail.com>
2021-12-03 11:40:33 -0800

    fix(echarts): World map emphasis styles (#30412)

--- static/app/components/charts/worldMapChart.tsx
+++ static/app/components/charts/worldMapChart.tsx
@@ -101,15 +101,15 @@ class WorldMapChart extends React.Component<Props, State> {
         itemStyle: {
           areaColor: theme.gray200,
           borderColor: theme.backgroundSecondary,
-          emphasis: {
+        },
+        emphasis: {
+          itemStyle: {
             areaColor: theme.pink300,
           },
-        } as any, // TODO(ts): Echarts types aren't correct for these colors as they don't allow for basic strings
-        label: {
-          emphasis: {
+          label: {
             show: false,
           },
-        } as any,
+        },
         data,
         silent: fromDiscoverQueryList,
         roam: !fromDiscoverQueryList,
