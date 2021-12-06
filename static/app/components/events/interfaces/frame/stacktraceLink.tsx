046f89e471c5c63c10b48d39d9bd165a6c55f8b7
Colleen O'Rourke <colleen@sentry.io>
2021-12-06 15:34:05 -0800

    remove a little more

--- static/app/components/events/interfaces/frame/stacktraceLink.tsx
+++ static/app/components/events/interfaces/frame/stacktraceLink.tsx
@@ -10,7 +10,6 @@ import {Body, Header, Hovercard} from 'sentry/components/hovercard';
 import {IconInfo} from 'sentry/icons';
 import {IconClose} from 'sentry/icons/iconClose';
 import {t, tct} from 'sentry/locale';
-import ConfigStore from 'sentry/stores/configStore';
 import space from 'sentry/styles/space';
 import {
   Frame,
@@ -329,15 +328,7 @@ class StacktraceLink extends AsyncComponent<Props, State> {
       <OpenInContainer columnQuantity={2}>
         <div>{t('Open this line in')}</div>
         <OpenInLink onClick={() => this.onOpenLink()} href={url} openInNewTab>
-          {({hasFeature}) => (
-            <StyledIconWrapper
-              isDark={ConfigStore.get('theme') === 'dark'}
-              hasFeature={hasFeature}
-            >
-              {getIntegrationIcon(config.provider.key)}
-            </StyledIconWrapper>
-          )}
-
+          <StyledIconWrapper>{getIntegrationIcon(config.provider.key)}</StyledIconWrapper>
           <OpenInName>{config.provider.name}</OpenInName>
         </OpenInLink>
       </OpenInContainer>
@@ -370,9 +361,8 @@ export const CodeMappingButtonContainer = styled(OpenInContainer)`
   justify-content: space-between;
 `;
 
-const StyledIconWrapper = styled('span')<{isDark: boolean; hasFeature: boolean}>`
-  color: ${({isDark, hasFeature}) =>
-    !hasFeature ? 'inherit' : isDark ? 'white' : 'black'};
+const StyledIconWrapper = styled('span')`
+  color: inherit;
   line-height: 0;
 `;
 
