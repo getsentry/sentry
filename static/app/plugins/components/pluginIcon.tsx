2e8e3f138dc8838a79c93cd53eeafeaa46c854a0
Colleen O'Rourke <colleen@sentry.io>
2021-12-08 09:52:55 -0800

    ref(sentryapps): Remove hard coded logos (#30487)

--- static/app/plugins/components/pluginIcon.tsx
+++ static/app/plugins/components/pluginIcon.tsx
@@ -1,15 +1,11 @@
 import styled from '@emotion/styled';
-import amixr from 'sentry-logos/logo-amixr.svg';
 import asana from 'sentry-logos/logo-asana.svg';
 import asayer from 'sentry-logos/logo-asayer.svg';
 import aws from 'sentry-logos/logo-aws.svg';
 import vsts from 'sentry-logos/logo-azure.svg';
 import bitbucket from 'sentry-logos/logo-bitbucket.svg';
 import bitbucketserver from 'sentry-logos/logo-bitbucket-server.svg';
-import calixa from 'sentry-logos/logo-calixa.svg';
 import campfire from 'sentry-logos/logo-campfire.svg';
-import clickup from 'sentry-logos/logo-clickup.svg';
-import clubhouse from 'sentry-logos/logo-clubhouse.svg';
 import datadog from 'sentry-logos/logo-datadog.svg';
 import placeholder from 'sentry-logos/logo-default.svg';
 import flowdock from 'sentry-logos/logo-flowdock.svg';
@@ -23,9 +19,7 @@ import heroku from 'sentry-logos/logo-heroku.svg';
 import insightfinder from 'sentry-logos/logo-insightfinder.svg';
 import jira from 'sentry-logos/logo-jira.svg';
 import jiraserver from 'sentry-logos/logo-jira-server.svg';
-import komodor from 'sentry-logos/logo-komodor.svg';
 import lighthouse from 'sentry-logos/logo-lighthouse.svg';
-import linear from 'sentry-logos/logo-linear.svg';
 import msteams from 'sentry-logos/logo-msteams.svg';
 import netlify from 'sentry-logos/logo-netlify.svg';
 import octohook from 'sentry-logos/logo-octohook.svg';
@@ -34,18 +28,12 @@ import pagerduty from 'sentry-logos/logo-pagerduty.svg';
 import phabricator from 'sentry-logos/logo-phabricator.svg';
 import pivotal from 'sentry-logos/logo-pivotaltracker.svg';
 import pushover from 'sentry-logos/logo-pushover.svg';
-import quill from 'sentry-logos/logo-quill.svg';
 import redmine from 'sentry-logos/logo-redmine.svg';
 import rocketchat from 'sentry-logos/logo-rocketchat.svg';
-import rookout from 'sentry-logos/logo-rookout.svg';
 import segment from 'sentry-logos/logo-segment.svg';
 import sentry from 'sentry-logos/logo-sentry.svg';
 import slack from 'sentry-logos/logo-slack.svg';
-import spikesh from 'sentry-logos/logo-spikesh.svg';
-import split from 'sentry-logos/logo-split.svg';
 import taiga from 'sentry-logos/logo-taiga.svg';
-import taskcall from 'sentry-logos/logo-taskcall.svg';
-import teamwork from 'sentry-logos/logo-teamwork.svg';
 import trello from 'sentry-logos/logo-trello.svg';
 import twilio from 'sentry-logos/logo-twilio.svg';
 import vercel from 'sentry-logos/logo-vercel.svg';
@@ -67,16 +55,12 @@ export const ICON_PATHS = {
   webhooks: sentry,
   'amazon-sqs': aws,
   aws_lambda: aws,
-  amixr,
   asana,
   asayer,
   bitbucket,
   bitbucket_pipelines: bitbucket,
   bitbucket_server: bitbucketserver,
-  calixa,
   campfire,
-  clickup,
-  clubhouse,
   datadog,
   flowdock,
   fullstory,
@@ -89,9 +73,7 @@ export const ICON_PATHS = {
   insightfinder,
   jira,
   jira_server: jiraserver,
-  komodor,
   lighthouse,
-  linear,
   msteams,
   netlify,
   octohook,
@@ -100,18 +82,11 @@ export const ICON_PATHS = {
   phabricator,
   pivotal,
   pushover,
-  quill,
   redmine,
   rocketchat,
-  rookout,
   segment,
-  shortcut: clubhouse,
   slack,
-  spikesh,
-  split,
   taiga,
-  taskcall,
-  teamwork,
   trello,
   twilio,
   visualstudio,
