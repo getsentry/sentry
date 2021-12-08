780f1c7346b80f18f3a168ffa95f9bf13771276b
Tony Xiao <txiao@sentry.io>
2021-12-08 13:40:46 -0500

    fix(tests): uniq will never return nulls for user_misery
    Previously, user_misery relied on a behaviour of the `uniq` function returning
    `NULL` in certain cases. This was identified as a bug in Clickhouse. See
    https://github.com/ClickHouse/ClickHouse/issues/2494 for more information. This
    change ensures user_misery will continue to behave as expected in newer versions
    of Clickhouse.

--- src/sentry/search/events/fields.py
+++ src/sentry/search/events/fields.py
@@ -3408,7 +3408,7 @@ class QueryFields(QueryBase):
                         Function(
                             "plus",
                             [
-                                Function("uniq", [self.column("user")]),
+                                Function("nullIf", [Function("uniq", [self.column("user")]), 0]),
                                 args["parameter_sum"],
                             ],
                         ),
