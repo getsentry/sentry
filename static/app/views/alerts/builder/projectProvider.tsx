a4e2347bf460f6b95d990f1e7fc93cfc9b954207
Scott Cooper <scttcper@gmail.com>
2021-12-08 13:09:47 -0800

    ref(ui): Switch projectProvider to useProjects

--- static/app/views/alerts/builder/projectProvider.tsx
+++ static/app/views/alerts/builder/projectProvider.tsx
@@ -1,13 +1,13 @@
-import * as React from 'react';
+import {cloneElement, isValidElement, useEffect} from 'react';
 import {RouteComponentProps} from 'react-router';
 
 import {fetchOrgMembers} from 'sentry/actionCreators/members';
 import Alert from 'sentry/components/alert';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
 import {t} from 'sentry/locale';
-import {Organization, Project} from 'sentry/types';
-import Projects from 'sentry/utils/projects';
+import {Organization} from 'sentry/types';
 import useApi from 'sentry/utils/useApi';
+import useProjects from 'sentry/utils/useProjects';
 import ScrollToTop from 'sentry/views/settings/components/scrollToTop';
 
 type Props = RouteComponentProps<RouteParams, {}> & {
@@ -25,39 +25,42 @@ function AlertBuilderProjectProvider(props: Props) {
 
   const {children, params, organization, ...other} = props;
   const {projectId} = params;
+  const {projects, initiallyLoaded, fetching, fetchError} = useProjects({
+    slugs: [projectId],
+  });
+  const project = projects.find(({slug}) => slug === projectId);
+
+  useEffect(() => {
+    if (!project) {
+      return;
+    }
+
+    // fetch members list for mail action fields
+    fetchOrgMembers(api, organization.slug, [project.id]);
+  }, [projects]);
+
+  if (!initiallyLoaded || fetching) {
+    return <LoadingIndicator />;
+  }
+
+  // if loaded, but project fetching states incomplete or project can't be found, project doesn't exist
+  if (!project || fetchError) {
+    return (
+      <Alert type="warning">{t('The project you were looking for was not found.')}</Alert>
+    );
+  }
 
   return (
-    <Projects orgId={organization.slug} allProjects>
-      {({projects, initiallyLoaded, isIncomplete}) => {
-        if (!initiallyLoaded) {
-          return <LoadingIndicator />;
-        }
-        const project = (projects as Project[]).find(({slug}) => slug === projectId);
-        // if loaded, but project fetching states incomplete or project can't be found, project doesn't exist
-        if (isIncomplete || !project) {
-          return (
-            <Alert type="warning">
-              {t('The project you were looking for was not found.')}
-            </Alert>
-          );
-        }
-        // fetch members list for mail action fields
-        fetchOrgMembers(api, organization.slug, [project.id]);
-
-        return (
-          <ScrollToTop location={props.location} disable={() => false}>
-            {children && React.isValidElement(children)
-              ? React.cloneElement(children, {
-                  ...other,
-                  ...children.props,
-                  project,
-                  organization,
-                })
-              : children}
-          </ScrollToTop>
-        );
-      }}
-    </Projects>
+    <ScrollToTop location={props.location} disable={() => false}>
+      {children && isValidElement(children)
+        ? cloneElement(children, {
+            ...other,
+            ...children.props,
+            project,
+            organization,
+          })
+        : children}
+    </ScrollToTop>
   );
 }
 
