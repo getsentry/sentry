1c36d5a521b2c1c23cb4967e3cd5d4cbb296e78d
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-12-09 12:05:21 -0800

    feat(ui-filters): Add selection-filters-v2 feature flag

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1044,6 +1044,8 @@ SENTRY_FEATURES = {
     # Enable usage of external relays, for use with Relay. See
     # https://github.com/getsentry/relay.
     "organizations:relay": True,
+    # Enables experimental new-style selection filters to replace the GSH
+    "organizations:selection-filters-v2": False,
     # Enable logging for weekly reports
     "organizations:weekly-report-debugging": False,
     # Enable Session Stats down to a minute resolution
