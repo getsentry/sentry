43f172e4c7cd87364ca5a372e37611d44641c05c
Leander Rodrigues <leander.rodrigues@sentry.io>
2021-12-10 15:01:54 -0800

    👀 why is this broken

--- src/sentry/api/serializers/models/doc_integration.py
+++ src/sentry/api/serializers/models/doc_integration.py
@@ -1,8 +1,7 @@
 from typing import Any, Mapping
 
 from sentry.api.serializers import Serializer, register, serialize
-
-# from sentry.api.validators.doc_integration import METADATA_TYPES
+from sentry.api.validators.doc_integration import METADATA_TYPES
 from sentry.models.integration import DocIntegration
 from sentry.models.integrationfeature import IntegrationFeature, IntegrationTypes
 from sentry.models.user import User
@@ -30,7 +29,8 @@ class DocIntegrationSerializer(Serializer):
 
         if obj.metadata:
             #  TODO(Leander): Fix imports here
-            for metadata_type in ["resources"]:
+            # for metadata_type in ["resources"]:
+            for metadata_type in METADATA_TYPES:
                 data[metadata_type] = obj.metadata.get("metadata_type")
 
         features = IntegrationFeature.objects.filter(
