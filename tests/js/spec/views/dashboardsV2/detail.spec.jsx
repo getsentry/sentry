f70d5d5edfe7c0103ae541d25d06d6a92c941503
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2021-12-10 09:18:50 -0500

    remove whiteline

--- tests/js/spec/views/dashboardsV2/detail.spec.jsx
+++ tests/js/spec/views/dashboardsV2/detail.spec.jsx
@@ -327,7 +327,6 @@ describe('Dashboards > Detail', function () {
 
       // Save changes
       wrapper.find('Controls Button[data-test-id="dashboard-commit"]').simulate('click');
-
       await tick();
 
       expect(updateMock).toHaveBeenCalled();
