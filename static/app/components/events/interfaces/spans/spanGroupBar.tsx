6a515563d5f1ccc3ea7eab5224ef4946a229ae8c
Vu Luong <vuluongj20@gmail.com>
2021-12-13 10:15:55 -0800

    fix(ui): spanGroupBar offset
    There is a slight offset on spanGroupBar (see PR #30498). This commit fixes that.

--- static/app/components/events/interfaces/spans/spanGroupBar.tsx
+++ static/app/components/events/interfaces/spans/spanGroupBar.tsx
@@ -247,9 +247,9 @@ class SpanGroupBar extends React.Component<Props> {
     connectorBars.push(
       <ConnectorBar
         style={{
-          right: '16px',
+          right: '15px',
           height: `${ROW_HEIGHT / 2}px`,
-          bottom: `-${ROW_HEIGHT / 2}px`,
+          bottom: `-${ROW_HEIGHT / 2 + 1}px`,
           top: 'auto',
         }}
         key="collapsed-span-group-row-bottom"
