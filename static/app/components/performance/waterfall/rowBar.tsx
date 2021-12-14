144fe5ac97da2ca269e18fc3a6edffb35bc7fd81
robinrendle <robinjrendle@gmail.com>
2021-12-13 16:35:39 -0800

    fixed alignment of spans in performance

--- static/app/components/performance/waterfall/rowBar.tsx
+++ static/app/components/performance/waterfall/rowBar.tsx
@@ -31,6 +31,7 @@ export const DurationPill = styled('div')<{
   font-size: ${p => p.theme.fontSizeExtraSmall};
   color: ${p => (p.showDetail === true ? p.theme.gray200 : p.theme.gray300)};
   font-variant-numeric: tabular-nums;
+  line-height: 1;
 
   ${getDurationPillAlignment}
 
