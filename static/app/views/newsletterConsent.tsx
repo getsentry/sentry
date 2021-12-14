2c0128201715dda966a8676c5d34608721dce484
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-12-14 14:10:31 -0800

    ref(js): Convert NewsletterConsent to a FC (#30639)

--- static/app/views/newsletterConsent.tsx
+++ static/app/views/newsletterConsent.tsx
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {useEffect} from 'react';
 
 import ExternalLink from 'sentry/components/links/externalLink';
 import NarrowLayout from 'sentry/components/narrowLayout';
@@ -11,53 +11,51 @@ type Props = {
   onSubmitSuccess?: () => void;
 };
 
-export default class NewsletterConsent extends Component<Props> {
-  componentDidMount() {
+function NewsletterConsent({onSubmitSuccess}: Props) {
+  useEffect(() => {
     document.body.classList.add('auth');
-  }
 
-  componentWillUnmount() {
-    document.body.classList.remove('auth');
-  }
+    return () => document.body.classList.remove('auth');
+  }, []);
 
   // NOTE: the text here is duplicated within ``RegisterForm`` on the backend
-  render() {
-    return (
-      <NarrowLayout>
-        <ApiForm
-          apiMethod="POST"
-          apiEndpoint="/users/me/subscriptions/"
-          onSubmitSuccess={() => this.props.onSubmitSuccess?.()}
-          submitLabel={t('Continue')}
-        >
-          <FieldWrapper stacked={false} hasControlState={false}>
-            {t('Pardon the interruption, we just need to get a quick answer from you.')}
-          </FieldWrapper>
-          <InputField
-            name="subscribed"
-            key="subscribed"
-            label={t('Email Updates')}
-            required
-            inline={false}
-            help={tct(
-              `We'd love to keep you updated via email with product and feature
+  return (
+    <NarrowLayout>
+      <ApiForm
+        apiMethod="POST"
+        apiEndpoint="/users/me/subscriptions/"
+        onSubmitSuccess={onSubmitSuccess}
+        submitLabel={t('Continue')}
+      >
+        <FieldWrapper stacked={false} hasControlState={false}>
+          {t('Pardon the interruption, we just need to get a quick answer from you.')}
+        </FieldWrapper>
+        <InputField
+          name="subscribed"
+          key="subscribed"
+          label={t('Email Updates')}
+          required
+          inline={false}
+          help={tct(
+            `We'd love to keep you updated via email with product and feature
                announcements, promotions, educational materials, and events. Our updates
                focus on relevant information, and we'll never sell your data to third
                parties. See our [link:Privacy Policy] for more details.
                `,
-              {link: <ExternalLink href="https://sentry.io/privacy/" />}
-            )}
-            field={fieldProps => (
-              <RadioBoolean
-                {...fieldProps}
-                label={t('Email Updates')}
-                yesLabel={t('Yes, I would like to receive updates via email')}
-                noLabel={t("No, I'd prefer not to receive these updates")}
-              />
-            )}
-          />
-        </ApiForm>
-      </NarrowLayout>
-    );
-  }
+            {link: <ExternalLink href="https://sentry.io/privacy/" />}
+          )}
+          field={fieldProps => (
+            <RadioBoolean
+              {...fieldProps}
+              label={t('Email Updates')}
+              yesLabel={t('Yes, I would like to receive updates via email')}
+              noLabel={t("No, I'd prefer not to receive these updates")}
+            />
+          )}
+        />
+      </ApiForm>
+    </NarrowLayout>
+  );
 }
+
+export default NewsletterConsent;
