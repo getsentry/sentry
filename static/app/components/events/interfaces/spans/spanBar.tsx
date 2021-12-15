446f8c40eac7d160808226d49daafd3170dc0612
Alberto Leal <mail4alberto@gmail.com>
2021-12-15 15:06:00 -0500

    fix(performance): Remove new badge from embedded transaction tooltip

--- static/app/components/events/interfaces/spans/spanBar.tsx
+++ static/app/components/events/interfaces/spans/spanBar.tsx
@@ -749,7 +749,6 @@ class SpanBar extends React.Component<SpanBarProps, SpanBarState> {
               {showEmbeddedChildren
                 ? t('This span is showing a direct child. Remove transaction to hide')
                 : t('This span has a direct child. Add transaction to view')}
-              <FeatureBadge type="new" noTooltip />
             </span>
           }
           position="top"
