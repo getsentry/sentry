d7b9bdcd3612bc01cd8b583e4d3bfa3116fd81de
Ahmed Etefy <ahmed.etefy12@gmail.com>
2021-12-16 20:34:40 +0100

    feat(cra-alerts): Add a new schema for subscription payloads

--- src/sentry/snuba/json_schemas.py
+++ src/sentry/snuba/json_schemas.py
@@ -58,4 +58,30 @@ SUBSCRIPTION_PAYLOAD_VERSIONS: Mapping[int, Mapping[str, Any]] = {
         "required": ["subscription_id", "request", "result", "timestamp"],
         "additionalProperties": False,
     },
+    3: {
+        "type": "object",
+        "properties": {
+            "subscription_id": {"type": "string", "minLength": 1},
+            "request": {"type": "object"},
+            "result": {
+                "type": "object",
+                "properties": {
+                    "data": {
+                        "type": "array",
+                        "minItems": 0,
+                        "items": {
+                            "type": "object",
+                            "minProperties": 1,
+                            "additionalProperties": {"type": ["number", "null"]},
+                        },
+                    }
+                },
+                "required": ["data"],
+            },
+            "timestamp": {"type": "string", "format": "date-time"},
+            "entity": {"type": "string", "minLength": 1},
+        },
+        "required": ["subscription_id", "request", "result", "timestamp", "entity"],
+        "additionalProperties": False,
+    },
 }
