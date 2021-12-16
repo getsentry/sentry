c342f6ec7dc2cfe2239b170d8ed14ceb903cc011
Scott Cooper <scttcper@gmail.com>
2021-12-15 16:02:50 -0800

    feat(teamStats): Add team issues age histogram (#30578)
    Adds a new component to the team insights page, this one calls two apis. One to fill in the histogram like chart of unresolved issues bucketed by age and the top 7 oldest issues in the table.

--- static/app/views/organizationStats/teamInsights/overview.tsx
+++ static/app/views/organizationStats/teamInsights/overview.tsx
@@ -27,6 +27,7 @@ import Header from '../header';
 
 import DescriptionCard from './descriptionCard';
 import TeamAlertsTriggered from './teamAlertsTriggered';
+import TeamIssuesAge from './teamIssuesAge';
 import TeamIssuesReviewed from './teamIssuesReviewed';
 import TeamMisery from './teamMisery';
 import TeamReleases from './teamReleases';
@@ -171,6 +172,8 @@ function TeamInsightsOverview({location, router}: Props) {
     );
   }
 
+  const isInsightsV2 = organization.features.includes('team-insights-v2');
+
   return (
     <Fragment>
       <Header organization={organization} activeTab="team" />
@@ -301,6 +304,16 @@ function TeamInsightsOverview({location, router}: Props) {
                 location={location}
               />
             </DescriptionCard>
+            {isInsightsV2 && (
+              <DescriptionCard
+                title={t('Age of Unresolved Issues')}
+                description={t(
+                  'How long ago since unresolved issues were first created.'
+                )}
+              >
+                <TeamIssuesAge organization={organization} teamSlug={currentTeam!.slug} />
+              </DescriptionCard>
+            )}
             <DescriptionCard
               title={t('Time to Resolution')}
               description={t(
