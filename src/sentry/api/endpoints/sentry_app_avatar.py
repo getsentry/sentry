d505baa4c6ca08fe3c16708f36e6dc7be4beca5b
Leander Rodrigues <leander.rodrigues@sentry.io>
2021-12-17 10:59:42 -0800

    feat(doc integrations): Create DocIntegrationAvatar endpoints + serializer (#30699)
    See API-2320
    
    This PR adds the endpoints and serializer for DocIntegrationAvatar. It also makes some changes to the existing BaseAvatar to make it a bit cleaner with the use of different serializer classes.

--- src/sentry/api/endpoints/sentry_app_avatar.py
+++ src/sentry/api/endpoints/sentry_app_avatar.py
@@ -1,11 +1,13 @@
 from sentry.api.bases import SentryAppBaseEndpoint
 from sentry.api.bases.avatar import AvatarMixin
+from sentry.api.serializers.rest_framework.sentry_app import SentryAppAvatarSerializer
 from sentry.models import SentryAppAvatar
 
 
 class SentryAppAvatarEndpoint(AvatarMixin, SentryAppBaseEndpoint):
     object_type = "sentry_app"
     model = SentryAppAvatar
+    serializer_cls = SentryAppAvatarSerializer
 
     def get(self, request, **kwargs):
         return super().get(request, access=request.access, **kwargs)
