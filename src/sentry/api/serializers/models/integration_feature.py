5acb8bf28731e9211bf536395d4712d3de466d02
Leander Rodrigues <leander.rodrigues@sentry.io>
2021-12-17 14:33:56 -0800

    ✨ Add new endpoint for IntegrationFeatures

--- src/sentry/api/serializers/models/integration_feature.py
+++ src/sentry/api/serializers/models/integration_feature.py
@@ -1,12 +1,19 @@
 from sentry.api.serializers import Serializer, register
 from sentry.models import IntegrationFeature
+from sentry.models.user import User
 
 
 @register(IntegrationFeature)
 class IntegrationFeatureSerializer(Serializer):
-    def serialize(self, obj, attrs, user):
-        return {
-            "description": obj.description.strip(),
+    def serialize(self, obj: IntegrationFeature, attrs, user: User, has_target: bool = True):
+        data = {
+            "featureId": obj.feature,
             # feature gating work done in getsentry expects the format 'featureGate'
             "featureGate": obj.feature_str(),
         }
+        if has_target:
+            # These properties require a target on the IntegrationFeature.
+            # If no target is provided, the serialized IntegrationFeature payload will be generic,
+            # and not only applicable to one target.
+            data.update({"description": obj.description.strip()})
+        return data
