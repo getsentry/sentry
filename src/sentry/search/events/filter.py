ae99259e4268f1004544ce94e3713faa0e64b71d
William Mak <william@wmak.io>
2021-12-16 20:57:28 -0500

    fix(discover): Always coerce tag values to strings
    - This is so we don't cause an error in Snuba
    - Still throw an error when this happens since this should only happen
      when there's a bug in our search grammar

--- src/sentry/search/events/filter.py
+++ src/sentry/search/events/filter.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from functools import reduce
 from typing import Callable, Dict, List, Mapping, Optional, Sequence, Tuple, Union
 
+import sentry_sdk
 from parsimonious.exceptions import ParseError
 from sentry_relay import parse_release as parse_release_relay
 from sentry_relay.consts import SPAN_STATUS_NAME_TO_CODE
@@ -1415,8 +1416,12 @@ class QueryFilter(QueryFields):
         # Tags are never null, but promoted tags are columns and so can be null.
         # To handle both cases, use `ifNull` to convert to an empty string and
         # compare so we need to check for empty values.
-        if search_filter.key.is_tag:
-            name = ["ifNull", [name, "''"]]
+        if lhs.subscriptable == "tags":
+            if not isinstance(value, str):
+                sentry_sdk.set_tag("query.lhs", lhs)
+                sentry_sdk.set_tag("query.rhs", value)
+                sentry_sdk.capture_message("Tag value was not a string", level="error")
+                value = str(value)
             lhs = Function("ifNull", [lhs, ""])
 
         # Handle checks for existence
