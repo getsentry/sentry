fab5e712cc129e9b169ec55812f82d24783cfba2
Evan Purkhiser <evanpurkhiser@gmail.com>
2021-12-21 23:19:48 -0800

    ref(js): Remove unused LatestContextStore.lastRoute

--- tests/js/spec/views/organizationRoot.spec.jsx
+++ tests/js/spec/views/organizationRoot.spec.jsx
@@ -1,6 +1,5 @@
 import {mountWithTheme} from 'sentry-test/reactTestingLibrary';
 
-import {setLastRoute} from 'sentry/actionCreators/navigation';
 import {setActiveProject} from 'sentry/actionCreators/projects';
 import OrganizationRoot from 'sentry/views/organizationRoot';
 
@@ -8,38 +7,10 @@ jest.mock('sentry/actionCreators/projects', () => ({
   setActiveProject: jest.fn(),
 }));
 
-jest.mock('sentry/actionCreators/navigation', () => ({
-  setLastRoute: jest.fn(),
-}));
-
 describe('OrganizationRoot', function () {
   it('sets active project as null when mounted', function () {
     mountWithTheme(<OrganizationRoot location={{}}>{null}</OrganizationRoot>);
 
     expect(setActiveProject).toHaveBeenCalledWith(null);
-  });
-
-  it('calls `setLastRoute` when unmounted', function () {
-    const {unmount} = mountWithTheme(
-      <OrganizationRoot location={{pathname: '/org-slug/dashboard/'}}>
-        {null}
-      </OrganizationRoot>
-    );
-
-    unmount();
-
-    expect(setLastRoute).toHaveBeenCalledWith('/org-slug/dashboard/');
-  });
-
-  it('calls `setLastRoute` when unmounted with query string', function () {
-    const {unmount} = mountWithTheme(
-      <OrganizationRoot location={{pathname: '/org-slug/dashboard/', search: '?test=1'}}>
-        {null}
-      </OrganizationRoot>
-    );
-
-    unmount();
-
-    expect(setLastRoute).toHaveBeenCalledWith('/org-slug/dashboard/?test=1');
   });
 });
