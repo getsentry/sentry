dfe5fcf5fc6266efea056106b3d4fd2afaf8a66e
Priscila Oliveira <priscilawebdev@gmail.com>
2021-12-23 17:04:46 +0100

    ref(test): Conver enzyme/js test to RTL + TS (#30832)

---
+++ tests/js/spec/views/settings/projectDebugFiles/index.spec.tsx
@@ -0,0 +1,80 @@
+import {initializeOrg} from 'sentry-test/initializeOrg';
+import {mountGlobalModal} from 'sentry-test/modal';
+import {mountWithTheme, screen, userEvent} from 'sentry-test/reactTestingLibrary';
+
+import ProjectDebugFiles from 'sentry/views/settings/projectDebugFiles';
+
+describe('ProjectDebugFiles', function () {
+  const {organization, project, routerContext, router} = initializeOrg();
+
+  const props = {
+    organization,
+    project,
+    params: {orgId: organization.slug, projectId: project.slug},
+    location: {
+      ...router.location,
+      query: {
+        query: '',
+      },
+    },
+    route: {},
+    router,
+    routes: [],
+    routeParams: {},
+  };
+
+  const endpoint = `/projects/${organization.slug}/${project.slug}/files/dsyms/`;
+
+  beforeEach(function () {
+    MockApiClient.addMockResponse({
+      url: endpoint,
+      body: [TestStubs.DebugFile()],
+    });
+  });
+
+  it('renders', function () {
+    mountWithTheme(<ProjectDebugFiles {...props} />, {context: routerContext});
+
+    expect(screen.getByText('Debug Information Files')).toBeInTheDocument();
+
+    // Uploaded debug files content
+    expect(screen.getByText('Uploaded debug information files')).toBeInTheDocument();
+    expect(screen.getByText('libS.so')).toBeInTheDocument();
+  });
+
+  it('renders empty', function () {
+    MockApiClient.addMockResponse({
+      url: endpoint,
+      body: [],
+    });
+
+    mountWithTheme(<ProjectDebugFiles {...props} />, {context: routerContext});
+
+    // Uploaded debug files content
+    expect(
+      screen.getByText('There are no debug symbols for this project.')
+    ).toBeInTheDocument();
+  });
+
+  it('deletes the file', async function () {
+    const deleteMock = MockApiClient.addMockResponse({
+      method: 'DELETE',
+      url: `/projects/${organization.slug}/${project.slug}/files/dsyms/?id=${
+        TestStubs.DebugFile().id
+      }`,
+    });
+
+    mountWithTheme(<ProjectDebugFiles {...props} />, {context: routerContext});
+
+    // Delete button
+    userEvent.click(screen.getByTestId('delete-dif'));
+
+    // Confirm Modal
+    mountGlobalModal();
+    await screen.findByRole('dialog');
+
+    userEvent.click(screen.getByTestId('confirm-button'));
+
+    expect(deleteMock).toHaveBeenCalled();
+  });
+});
