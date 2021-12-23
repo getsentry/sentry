b37830a8f4690b432dafd6afe049bb1727f7521a
Nar Saynorath <nar.saynorath@sentry.io>
2021-12-23 15:47:39 -0500

    * fix(performance-landing): Show different column headers in react native view (#30838)
    React Native has different columns than generic mobile views for now, so
    the column headers need to reference these

--- static/app/views/performance/landing/content.tsx
+++ static/app/views/performance/landing/content.tsx
@@ -37,6 +37,7 @@ import {
   REACT_NATIVE_COLUMN_TITLES,
 } from './data';
 import {
+  checkIsReactNative,
   getCurrentLandingDisplay,
   getDefaultDisplayFieldForPlatform,
   getDisplayAxes,
@@ -204,9 +205,7 @@ class LandingContent extends Component<Props, State> {
     const {leftAxis, rightAxis} = getDisplayAxes(axisOptions, location);
 
     // only react native should contain the stall percentage column
-    const isReactNative = Boolean(
-      eventView.getFields().find(field => field.includes('measurements.stall_percentage'))
-    );
+    const isReactNative = checkIsReactNative(eventView);
     const columnTitles = isReactNative
       ? REACT_NATIVE_COLUMN_TITLES
       : MOBILE_COLUMN_TITLES;
