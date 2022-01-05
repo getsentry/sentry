55599d7c26d121026124a1ca5269c5dac975a6ed
Edward Gou <edward.gou@sentry.io>
2022-01-05 16:59:17 -0500

    test

--- tests/js/spec/views/dashboardsV2/gridLayout/dashboard.spec.tsx
+++ tests/js/spec/views/dashboardsV2/gridLayout/dashboard.spec.tsx
@@ -3,6 +3,7 @@ import {initializeOrg} from 'sentry-test/initializeOrg';
 import {
   mountWithTheme as rtlMountWithTheme,
   screen,
+  userEvent,
 } from 'sentry-test/reactTestingLibrary';
 
 import Dashboard from 'sentry/views/dashboardsV2/dashboard';
@@ -43,7 +44,7 @@ describe('Dashboards > Dashboard', () => {
       {
         name: '',
         conditions: '',
-        fields: ['title'],
+        fields: ['title', 'assignee'],
         orderby: '',
       },
     ],
@@ -57,6 +58,47 @@ describe('Dashboards > Dashboard', () => {
       url: `/organizations/org-slug/dashboards/widgets/`,
       method: 'POST',
       body: [],
+    });
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/events-stats/',
+      method: 'GET',
+      body: [],
+    });
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/issues/',
+      method: 'GET',
+      body: [
+        {
+          id: '1',
+          title: 'Error: Registration failed',
+          project: {
+            id: '3',
+          },
+          owners: [
+            {
+              type: 'ownershipRule',
+              owner: 'user:2',
+            },
+          ],
+        },
+      ],
+    });
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/users/',
+      method: 'GET',
+      body: [
+        {
+          user: {
+            id: '2',
+            name: 'test@sentry.io',
+            email: 'test@sentry.io',
+            avatar: {
+              avatarType: 'letter_avatar',
+              avatarUuid: null,
+            },
+          },
+        },
+      ],
     });
   });
 
@@ -113,65 +155,75 @@ describe('Dashboards > Dashboard', () => {
     expect(mockHandleAddCustomWidget).toHaveBeenCalled();
   });
 
-  it('dashboard does not display issue widgets if the user does not have issue widgets feature flag', async () => {
-    const mockHandleAddCustomWidget = jest.fn();
-    const mockDashboardWithIssueWidget = {
-      ...mockDashboard,
-      widgets: [newWidget, issueWidget],
+  describe('Issue Widgets', () => {
+    const mount = (dashboard, mockedOrg = initialData.organization) => {
+      rtlMountWithTheme(
+        <Dashboard
+          paramDashboardId="1"
+          dashboard={dashboard}
+          organization={mockedOrg}
+          isEditing={false}
+          onUpdate={() => undefined}
+          handleUpdateWidgetList={() => undefined}
+          handleAddCustomWidget={() => undefined}
+          onSetWidgetToBeUpdated={() => undefined}
+          router={initialData.router}
+          location={initialData.location}
+          layout={[]}
+          onLayoutChange={() => undefined}
+          widgetLimitReached={false}
+        />
+      );
     };
-    rtlMountWithTheme(
-      <Dashboard
-        paramDashboardId="1"
-        dashboard={mockDashboardWithIssueWidget}
-        organization={initialData.organization}
-        isEditing={false}
-        onUpdate={() => undefined}
-        handleUpdateWidgetList={() => undefined}
-        handleAddCustomWidget={mockHandleAddCustomWidget}
-        onSetWidgetToBeUpdated={() => undefined}
-        router={initialData.router}
-        location={initialData.location}
-        layout={[]}
-        onLayoutChange={() => undefined}
-        widgetLimitReached={false}
-      />
-    );
-    expect(screen.getByText('Test Discover Widget')).toBeInTheDocument();
-    expect(screen.queryByText('Test Issue Widget')).not.toBeInTheDocument();
-  });
 
-  it('dashboard displays issue widgets if the user has issue widgets feature flag', async () => {
-    const mockHandleAddCustomWidget = jest.fn();
-    const organizationWithFlag = TestStubs.Organization({
-      features: [
-        'dashboards-basic',
-        'dashboards-edit',
-        'dashboard-grid-layout',
-        'issues-in-dashboards',
-      ],
+    it('dashboard does not display issue widgets if the user does not have issue widgets feature flag', async () => {
+      const mockDashboardWithIssueWidget = {
+        ...mockDashboard,
+        widgets: [newWidget, issueWidget],
+      };
+      mount(mockDashboardWithIssueWidget);
+      expect(screen.getByText('Test Discover Widget')).toBeInTheDocument();
+      expect(screen.queryByText('Test Issue Widget')).not.toBeInTheDocument();
     });
-    const mockDashboardWithIssueWidget = {
-      ...mockDashboard,
-      widgets: [newWidget, issueWidget],
-    };
-    rtlMountWithTheme(
-      <Dashboard
-        paramDashboardId="1"
-        dashboard={mockDashboardWithIssueWidget}
-        organization={organizationWithFlag}
-        isEditing={false}
-        onUpdate={() => undefined}
-        handleUpdateWidgetList={() => undefined}
-        handleAddCustomWidget={mockHandleAddCustomWidget}
-        onSetWidgetToBeUpdated={() => undefined}
-        router={initialData.router}
-        location={initialData.location}
-        layout={[]}
-        onLayoutChange={() => undefined}
-        widgetLimitReached={false}
-      />
-    );
-    expect(screen.getByText('Test Discover Widget')).toBeInTheDocument();
-    expect(screen.getByText('Test Issue Widget')).toBeInTheDocument();
+
+    it('dashboard displays issue widgets if the user has issue widgets feature flag', async () => {
+      const organizationWithFlag = TestStubs.Organization({
+        features: [
+          'dashboards-basic',
+          'dashboards-edit',
+          'dashboard-grid-layout',
+          'issues-in-dashboards',
+        ],
+      });
+      const mockDashboardWithIssueWidget = {
+        ...mockDashboard,
+        widgets: [newWidget, issueWidget],
+      };
+      mount(mockDashboardWithIssueWidget, organizationWithFlag);
+      expect(screen.getByText('Test Discover Widget')).toBeInTheDocument();
+      expect(screen.getByText('Test Issue Widget')).toBeInTheDocument();
+    });
+
+    it('renders suggested assignees', async () => {
+      const organizationWithFlag = TestStubs.Organization({
+        features: [
+          'dashboards-basic',
+          'dashboards-edit',
+          'dashboard-grid-layout',
+          'issues-in-dashboards',
+        ],
+      });
+      const mockDashboardWithIssueWidget = {
+        ...mockDashboard,
+        widgets: [{...issueWidget}],
+      };
+      mount(mockDashboardWithIssueWidget, organizationWithFlag);
+      await tick();
+      expect(screen.getByText('T')).toBeInTheDocument();
+      userEvent.hover(screen.getByText('T'));
+      expect(await screen.findByText('Suggestion:')).toBeInTheDocument();
+      expect(await screen.findByText('test@sentry.io')).toBeInTheDocument();
+      expect(await screen.findByText('Matching Issue Owners Rule')).toBeInTheDocument();
+    });
   });
 });
