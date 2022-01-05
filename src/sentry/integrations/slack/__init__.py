80e614443698c1d05f7d74f0683ea1f59284760a
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-01-05 13:43:45 -0800

    fix imports

--- src/sentry/integrations/slack/__init__.py
+++ src/sentry/integrations/slack/__init__.py
@@ -4,6 +4,6 @@ from sentry.utils.imports import import_submodules
 from .client import SlackClient  # NOQA
 from .notify_action import SlackNotifyServiceAction
 
-import_submodules(globals(), __name__, __path__)
+import_submodules(globals(), __name__, __path__)  # type: ignore
 
 rules.add(SlackNotifyServiceAction)
--- src/sentry/integrations/vsts/__init__.py
+++ src/sentry/integrations/vsts/__init__.py
@@ -1,3 +1,7 @@
+from sentry.rules import rules
 from sentry.utils.imports import import_submodules
 
-import_submodules(globals(), __name__, __path__)
+from .notify_action import AzureDevopsCreateTicketAction
+
+rules.add(AzureDevopsCreateTicketAction)
+import_submodules(globals(), __name__, __path__)  # type: ignore
