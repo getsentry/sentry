bf1b773d61bd311c1329cbb621939befe0633166
Tony Xiao <txiao@sentry.io>
2022-01-10 10:14:25 -0500

    address PR comments

--- static/app/views/performance/transactionSummary/transactionSpans/spanDetails/content.tsx
+++ static/app/views/performance/transactionSummary/transactionSpans/spanDetails/content.tsx
@@ -6,6 +6,9 @@ import {SectionHeading as _SectionHeading} from 'sentry/components/charts/styles
 import EmptyStateWarning from 'sentry/components/emptyStateWarning';
 import * as Layout from 'sentry/components/layouts/thirds';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {DurationPill, RowRectangle} from 'sentry/components/performance/waterfall/rowBar';
+import {pickBarColor, toPercent} from 'sentry/components/performance/waterfall/utils';
+import Tooltip from 'sentry/components/tooltip';
 import {t, tn} from 'sentry/locale';
 import overflowEllipsis from 'sentry/styles/overflowEllipsis';
 import space from 'sentry/styles/space';
@@ -29,7 +32,7 @@ import {SpanSlug} from '../types';
 import {getTotalsView} from '../utils';
 
 import SpanTable from './spanDetailsTable';
-import {emptyValue, SpanLabelContainer} from './styles';
+// import {emptyValue, SpanLabelContainer} from './styles';
 
 type Props = {
   location: Location;
@@ -311,3 +314,59 @@ const PercentileHeaderBodyWrapper = styled('div')`
   grid-template-columns: repeat(3, max-content);
   gap: ${space(3)};
 `;
+
+const DurationBar = styled('div')`
+  position: relative;
+  display: flex;
+  top: ${space(0.5)};
+  background-color: ${p => p.theme.gray100};
+`;
+
+const DurationBarSection = styled(RowRectangle)`
+  position: relative;
+  width: 100%;
+  top: 0;
+`;
+
+type SpanDurationBarProps = {
+  spanOp: string;
+  spanDuration: number;
+  transactionDuration: number;
+};
+
+export function SpanDurationBar(props: SpanDurationBarProps) {
+  const {spanOp, spanDuration, transactionDuration} = props;
+  const widthPercentage = spanDuration / transactionDuration;
+  const position = widthPercentage < 0.7 ? 'right' : 'inset';
+
+  return (
+    <DurationBar>
+      <div style={{width: toPercent(widthPercentage)}}>
+        <Tooltip title={formatPercentage(widthPercentage)} containerDisplayMode="block">
+          <DurationBarSection
+            spanBarHatch={false}
+            style={{backgroundColor: pickBarColor(spanOp)}}
+          >
+            <DurationPill
+              durationDisplay={position}
+              showDetail={false}
+              spanBarHatch={false}
+            >
+              <PerformanceDuration abbreviation milliseconds={spanDuration} />
+            </DurationPill>
+          </DurationBarSection>
+        </Tooltip>
+      </div>
+    </DurationBar>
+  );
+}
+
+export const SpanLabelContainer = styled('div')`
+  ${overflowEllipsis};
+`;
+
+const EmptyValueContainer = styled('span')`
+  color: ${p => p.theme.gray300};
+`;
+
+export const emptyValue = <EmptyValueContainer>{t('n/a')}</EmptyValueContainer>;
