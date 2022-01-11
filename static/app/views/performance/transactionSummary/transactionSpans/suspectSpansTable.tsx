45d6006e818eaa497d7e2a7451b3e3a300332b74
Tony Xiao <txiao@sentry.io>
2022-01-11 10:27:49 -0500

    rename table columns

--- static/app/views/performance/transactionSummary/transactionSpans/suspectSpansTable.tsx
+++ static/app/views/performance/transactionSummary/transactionSpans/suspectSpansTable.tsx
@@ -201,12 +201,12 @@ const COLUMN_ORDER: Record<SpanSort, TableColumnKey[]> = {
 const COLUMNS: Record<TableColumnKey, TableColumn> = {
   operation: {
     key: 'operation',
-    name: t('Operation'),
+    name: t('Span Operation'),
     width: COL_WIDTH_UNDEFINED,
   },
   description: {
     key: 'description',
-    name: t('Description'),
+    name: t('Span Name'),
     width: COL_WIDTH_UNDEFINED,
   },
   totalCount: {
