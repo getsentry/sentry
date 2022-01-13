99097f0448e6348f5f5c8ecc11568a905f72b7c6
Nar Saynorath <nar.saynorath@sentry.io>
2022-01-12 21:52:31 -0500

    Add more tests

--- tests/acceptance/page_objects/dashboard_detail.py
+++ tests/acceptance/page_objects/dashboard_detail.py
@@ -2,10 +2,11 @@ from .base import BasePage
 
 
 class DashboardDetailPage(BasePage):
-    def __init__(self, browser, client, organization):
+    def __init__(self, browser, client, **kwargs):
         super().__init__(browser)
         self.client = client
-        self.organization = organization
+        self.organization = kwargs.get("organization", None)
+        self.dashboard = kwargs.get("dashboard", None)
 
     def wait_until_loaded(self):
         self.browser.wait_until_not(".loading-indicator")
@@ -19,6 +20,10 @@ class DashboardDetailPage(BasePage):
         self.browser.get(f"/organizations/{self.organization.slug}/dashboards/new/")
         self.wait_until_loaded()
 
+    def visit_dashboard_detail(self):
+        self.browser.get(f"/organizations/{self.organization.slug}/dashboard/{self.dashboard.id}/")
+        self.wait_until_loaded()
+
     def enter_edit_state(self):
         button = self.browser.element('[data-test-id="dashboard-edit"]')
         button.click()
@@ -29,4 +34,8 @@ class DashboardDetailPage(BasePage):
 
     def click_dashboard_header_add_widget_button(self):
         button = self.browser.element('[data-test-id="add-widget-library"]')
+        button.click()
+
+    def save_dashboard(self):
+        button = self.browser.element('[data-test-id="dashboard-commit"]')
         button.click()
--- tests/acceptance/test_organization_dashboards.py
+++ tests/acceptance/test_organization_dashboards.py
@@ -22,6 +22,8 @@ EDIT_FEATURE = ["organizations:dashboards-edit"]
 
 GRID_LAYOUT_FEATURE = ["organizations:dashboard-grid-layout"]
 
+ISSUE_WIDGET_FEATURE = ["organizations:issues-in-dashboards"]
+
 WIDGET_LIBRARY_FEATURE = ["organizations:widget-library"]
 
 
@@ -33,7 +35,7 @@ class OrganizationDashboardsAcceptanceTest(AcceptanceTestCase):
             data={"event_id": "a" * 32, "message": "oh no", "timestamp": min_ago},
             project_id=self.project.id,
         )
-        self.page = DashboardDetailPage(self.browser, self.client, self.organization)
+        self.page = DashboardDetailPage(self.browser, self.client, organization=self.organization)
         self.login_as(self.user)
 
     def test_view_dashboard(self):
@@ -99,23 +101,18 @@ class OrganizationDashboardLayoutAcceptanceTest(AcceptanceTestCase):
         self.dashboard = Dashboard.objects.create(
             title="Dashboard 1", created_by=self.user, organization=self.organization
         )
+        self.page = DashboardDetailPage(
+            self.browser, self.client, organization=self.organization, dashboard=self.dashboard
+        )
         self.login_as(self.user)
-
-        self.default_path = f"/organizations/{self.organization.slug}/dashboard/{self.dashboard.id}"
-
-    def wait_until_loaded(self):
-        self.browser.wait_until_not(".loading-indicator")
-        self.browser.wait_until_not('[data-test-id="loading-placeholder"]')
 
     def test_create_new_dashboard_with_modified_widget_layout(self):
         with self.feature(FEATURE_NAMES + EDIT_FEATURE + GRID_LAYOUT_FEATURE):
             # Create a new dashboard
-            self.browser.get(f"/organizations/{self.organization.slug}/dashboards/new/")
-            self.wait_until_loaded()
+            self.page.visit_create_dashboard()
 
             # Add a widget
-            button = self.browser.element('[data-test-id="widget-add"]')
-            button.click()
+            self.page.click_dashboard_add_widget_button()
             title_input = self.browser.element('input[data-test-id="widget-title-input"]')
             title_input.send_keys("New Widget")
             button = self.browser.element('[data-test-id="add-widget"]')
@@ -127,28 +124,22 @@ class OrganizationDashboardLayoutAcceptanceTest(AcceptanceTestCase):
             action.drag_and_drop_by_offset(dragHandle, 1000, 0).perform()
 
             # Save this dashboard
-            button = self.browser.element('[data-test-id="dashboard-commit"]')
-            button.click()
+            self.page.save_dashboard()
 
             self.browser.snapshot("dashboards - save widget layout in new custom dashboard")
             self.browser.refresh()
-            self.wait_until_loaded()
+            self.page.wait_until_loaded()
             self.browser.snapshot(
                 "dashboards - save widget layout in new custom dashboard (refresh)"
             )
 
     def test_add_and_move_new_widget_on_existing_dashboard(self):
         with self.feature(FEATURE_NAMES + EDIT_FEATURE + GRID_LAYOUT_FEATURE):
-            self.browser.get(self.default_path)
-            self.wait_until_loaded()
-
-            # Go to edit mode.
-            button = self.browser.element('[data-test-id="dashboard-edit"]')
-            button.click()
+            self.page.visit_dashboard_detail()
+            self.page.enter_edit_state()
 
             # Add a widget
-            button = self.browser.element('[data-test-id="widget-add"]')
-            button.click()
+            self.page.click_dashboard_add_widget_button()
             title_input = self.browser.element('input[data-test-id="widget-title-input"]')
             title_input.send_keys("New Widget")
             button = self.browser.element('[data-test-id="add-widget"]')
@@ -159,12 +150,11 @@ class OrganizationDashboardLayoutAcceptanceTest(AcceptanceTestCase):
             action = ActionChains(self.browser.driver)
             action.drag_and_drop_by_offset(dragHandle, 1000, 0).perform()
 
-            button = self.browser.element('[data-test-id="dashboard-commit"]')
-            button.click()
+            self.page.save_dashboard()
 
             self.browser.snapshot("dashboards - save new widget layout in custom dashboard")
             self.browser.refresh()
-            self.wait_until_loaded()
+            self.page.wait_until_loaded()
             self.browser.snapshot(
                 "dashboards - save new widget layout in custom dashboard (refresh)"
             )
@@ -180,24 +170,19 @@ class OrganizationDashboardLayoutAcceptanceTest(AcceptanceTestCase):
         )
         DashboardWidgetQuery.objects.create(widget=existing_widget, fields=["count()"], order=0)
         with self.feature(FEATURE_NAMES + EDIT_FEATURE + GRID_LAYOUT_FEATURE):
-            self.browser.get(self.default_path)
-            self.wait_until_loaded()
-
-            # Go to edit mode.
-            button = self.browser.element('[data-test-id="dashboard-edit"]')
-            button.click()
+            self.page.visit_dashboard_detail()
+            self.page.enter_edit_state()
 
             dragHandle = self.browser.element(".widget-drag")
             # Drag to the right
             action = ActionChains(self.browser.driver)
             action.drag_and_drop_by_offset(dragHandle, 1000, 0).perform()
 
-            button = self.browser.element('[data-test-id="dashboard-commit"]')
-            button.click()
+            self.page.save_dashboard()
 
             self.browser.snapshot("dashboards - save modified widget layout in custom dashboard")
             self.browser.refresh()
-            self.wait_until_loaded()
+            self.page.wait_until_loaded()
             self.browser.snapshot(
                 "dashboards - save modified widget layout in custom dashboard (refresh)"
             )
@@ -206,12 +191,8 @@ class OrganizationDashboardLayoutAcceptanceTest(AcceptanceTestCase):
         with self.feature(
             FEATURE_NAMES + EDIT_FEATURE + WIDGET_LIBRARY_FEATURE + GRID_LAYOUT_FEATURE
         ):
-            self.browser.get(self.default_path)
-            self.wait_until_loaded()
-
-            # Go to edit mode.
-            button = self.browser.element('[data-test-id="add-widget-library"]')
-            button.click()
+            self.page.visit_dashboard_detail()
+            self.page.click_dashboard_header_add_widget_button()
 
             self.browser.element('[data-test-id="library-tab"]').click()
 
@@ -226,7 +207,7 @@ class OrganizationDashboardLayoutAcceptanceTest(AcceptanceTestCase):
                 "dashboards - widgets from widget library do not overlap when added"
             )
             self.browser.refresh()
-            self.wait_until_loaded()
+            self.page.wait_until_loaded()
             self.browser.snapshot(
                 "dashboards - widgets from widget library do not overlap when added (refresh)"
             )
@@ -242,12 +223,8 @@ class OrganizationDashboardLayoutAcceptanceTest(AcceptanceTestCase):
         )
         DashboardWidgetQuery.objects.create(widget=existing_widget, fields=["count()"], order=0)
         with self.feature(FEATURE_NAMES + EDIT_FEATURE + GRID_LAYOUT_FEATURE):
-            self.browser.get(self.default_path)
-            self.wait_until_loaded()
-
-            # Go to edit mode.
-            button = self.browser.element('[data-test-id="dashboard-edit"]')
-            button.click()
+            self.page.visit_dashboard_detail()
+            self.page.enter_edit_state()
 
             # Edit the first widget
             button = self.browser.element('[data-test-id="widget-edit"]')
@@ -262,15 +239,85 @@ class OrganizationDashboardLayoutAcceptanceTest(AcceptanceTestCase):
             action = ActionChains(self.browser.driver)
             action.drag_and_drop_by_offset(dragHandle, 1000, 0).perform()
 
-            button = self.browser.element('[data-test-id="dashboard-commit"]')
-            button.click()
+            self.page.save_dashboard()
 
             self.browser.snapshot("dashboards - save modified widget layout in custom dashboard")
             self.browser.refresh()
-            self.wait_until_loaded()
+            self.page.wait_until_loaded()
             self.browser.snapshot(
                 "dashboards - save modified widget layout in custom dashboard (refresh)"
             )
+
+    def test_add_issue_widgets_do_not_overlap(self):
+        def add_issue_widget(widget_title):
+            self.browser.wait_until_clickable('[data-test-id="widget-add"]')
+            self.page.click_dashboard_add_widget_button()
+            title_input = self.browser.element('input[data-test-id="widget-title-input"]')
+            title_input.send_keys(widget_title)
+            self.browser.element('[aria-label="issue"]').click()
+            button = self.browser.element('[data-test-id="add-widget"]')
+            button.click()
+
+        with self.feature(
+            FEATURE_NAMES + EDIT_FEATURE + ISSUE_WIDGET_FEATURE + GRID_LAYOUT_FEATURE
+        ):
+            self.page.visit_dashboard_detail()
+            self.page.enter_edit_state()
+
+            add_issue_widget("Issue Widget 1")
+            add_issue_widget("Issue Widget 2")
+            self.page.save_dashboard()
+
+            self.browser.snapshot("dashboards - issue widgets do not overlap")
+            self.browser.refresh()
+            self.page.wait_until_loaded()
+            self.browser.snapshot("dashboards - issue widgets do not overlap (refresh)")
+
+    def test_resize_new_and_existing_widgets(self):
+        existing_widget = DashboardWidget.objects.create(
+            dashboard=self.dashboard,
+            order=0,
+            title="Existing Widget",
+            display_type=DashboardWidgetDisplayTypes.LINE_CHART,
+            widget_type=DashboardWidgetTypes.DISCOVER,
+            interval="1d",
+        )
+        DashboardWidgetQuery.objects.create(widget=existing_widget, fields=["count()"], order=0)
+        with self.feature(FEATURE_NAMES + EDIT_FEATURE + GRID_LAYOUT_FEATURE):
+            self.page.visit_dashboard_detail()
+            self.page.enter_edit_state()
+
+            # Resize existing widget
+            resizeHandle = self.browser.element(".react-resizable-handle")
+            action = ActionChains(self.browser.driver)
+            action.drag_and_drop_by_offset(resizeHandle, 500, 0).perform()
+
+            # Add new widget
+            self.page.click_dashboard_add_widget_button()
+            title_input = self.browser.element('input[data-test-id="widget-title-input"]')
+            title_input.send_keys("New Widget")
+            button = self.browser.element('[data-test-id="add-widget"]')
+            button.click()
+
+            # Drag it to the left for consistency
+            dragHandle = self.browser.element(".react-grid-item:nth-of-type(2n) .widget-drag")
+            action = ActionChains(self.browser.driver)
+            action.drag_and_drop_by_offset(dragHandle, -500, 0).perform()
+
+            # Resize new widget, get the 2nd element instead of the "last" because the "last" is
+            # the add widget button
+            resizeHandle = self.browser.element(
+                ".react-grid-item:nth-of-type(2n) .react-resizable-handle"
+            )
+            action = ActionChains(self.browser.driver)
+            action.drag_and_drop_by_offset(resizeHandle, 500, 0).perform()
+
+            self.page.save_dashboard()
+
+            self.browser.snapshot("dashboards - resize new and existing widgets")
+            self.browser.refresh()
+            self.page.wait_until_loaded()
+            self.browser.snapshot("dashboards - resize new and existing widgets (refresh)")
 
 
 class OrganizationDashboardsManageAcceptanceTest(AcceptanceTestCase):
