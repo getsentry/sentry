getsentry/sentry

commit 0bbbfaba8415007b2cf3cfd9bf0e67d176376fd5

ref(pageFilters): Unify localstorage save logic (#30959)

static/app/actionCreators/pageFilters.tsx
@@ -8,9 +8,12 @@ import * as qs from 'query-string';
 import PageFiltersActions from 'sentry/actions/pageFiltersActions';
 import {
   getDefaultSelection,
+  getPageFilterStorage,
   getStateFromQuery,
+  setPageFiltersStorage,
 } from 'sentry/components/organizations/pageFilters/utils';
-import {DATE_TIME, LOCAL_STORAGE_KEY, URL_PARAM} from 'sentry/constants/pageFilters';
+import {DATE_TIME, URL_PARAM} from 'sentry/constants/pageFilters';
+import PageFiltersStore from 'sentry/stores/pageFiltersStore';
 import {
   Environment,
   MinimalProject,
@@ -20,10 +23,10 @@ import {
 } from 'sentry/types';
 import {defined} from 'sentry/utils';
 import {getUtcDateString} from 'sentry/utils/dates';
-import localStorage from 'sentry/utils/localStorage';
 
 /**
- * Note this is the internal project.id, NOT the slug, but it is the stringified version of it
+ * Note this is the internal project.id, NOT the slug, but it is the
+ * stringified version of it
  */
 type ProjectId = string | number;
 type EnvironmentId = Environment['id'];
@@ -145,20 +148,10 @@ export function initializeUrlState({
     pageFilters.projects = parsed.project || [];
     pageFilters.environments = parsed.environment || [];
   } else if (!skipLoadLastUsed) {
-    try {
-      const localStorageKey = `${LOCAL_STORAGE_KEY}:${orgSlug}`;
-      const storedValue = localStorage.getItem(localStorageKey);
+    const storedPageFilters = getPageFilterStorage(orgSlug);
 
-      if (storedValue) {
-        pageFilters = {
-          datetime: pageFilters.datetime,
-          ...JSON.parse(storedValue),
-        };
-      }
-    } catch (err) {
-      // use default if invalid
-      Sentry.captureException(err);
-      console.error(err); // eslint-disable-line no-console
+    if (storedPageFilters !== null) {
+      pageFilters = {...storedPageFilters, datetime: pageFilters.datetime};
     }
   }
 
@@ -307,7 +300,10 @@ export function updateParams(obj: UrlParams, router?: Router, options?: Options)
   }
 
   if (options?.save) {
-    PageFiltersActions.save(newQuery);
+    const {organization, selection} = PageFiltersStore.getState();
+    const orgSlug = organization?.slug ?? null;
+
+    setPageFiltersStorage(orgSlug, selection, newQuery);
   }
 
   router.push({

static/app/components/organizations/pageFilters/utils.tsx
@@ -1,11 +1,13 @@
+import * as Sentry from '@sentry/react';
 import {Location} from 'history';
 import identity from 'lodash/identity';
 import isEqual from 'lodash/isEqual';
 import pick from 'lodash/pick';
 import pickBy from 'lodash/pickBy';
 
-import {DATE_TIME_KEYS, LOCAL_STORAGE_KEY, URL_PARAM} from 'sentry/constants/pageFilters';
-import {PageFilters} from 'sentry/types';
+import {DATE_TIME_KEYS, URL_PARAM} from 'sentry/constants/pageFilters';
+import OrganizationsStore from 'sentry/stores/organizationsStore';
+import {Environment, PageFilters} from 'sentry/types';
 import {defined} from 'sentry/utils';
 import {getUtcToLocalDateObject} from 'sentry/utils/dates';
 import localStorage from 'sentry/utils/localStorage';
@@ -13,6 +15,8 @@ import localStorage from 'sentry/utils/localStorage';
 import {getParams} from './getParams';
 
 const DEFAULT_PARAMS = getParams({});
+
+const LOCAL_STORAGE_KEY = 'global-selection';
 
 // Parses URL query parameters for values relevant to page filters
 type GetStateFromQueryOptions = {
@@ -120,10 +124,85 @@ export function isSelectionEqual(selection: PageFilters, other: PageFilters): bo
   return true;
 }
 
+type ProjectId = string | number;
+type EnvironmentId = Environment['id'];
+
+type UpdateData = {
+  project?: ProjectId[] | null;
+  environment?: EnvironmentId[] | null;
+};
+
+/**
+ * Updates the localstorage page filters data
+ *
+ * e.g. if localstorage is empty, user loads issue details for project "foo"
+ * this should not consider "foo" as last used and should not save to local
+ * storage.
+ *
+ * However, if user then changes environment, it should...? Currently it will
+ * save the current project alongside environment to local storage. It's
+ * debatable if this is the desired behavior.
+ *
+ * This will be a no-op if a inaccessible organization slug is passed.
+ */
+export function setPageFiltersStorage(
+  orgSlug: string | null,
+  current: PageFilters,
+  update: UpdateData
+) {
+  const org = orgSlug && OrganizationsStore.get(orgSlug);
+
+  // Do nothing if no org is loaded or user is not an org member. Only
+  // organizations that a user has membership in will be available via the
+  // organizations store
+  if (!org) {
+    return;
+  }
+
+  const {project, environment} = update;
+  const validatedProject = project?.map(Number).filter(value => !isNaN(value));
+  const validatedEnvironment = environment;
+
+  const dataToSave = {
+    projects: validatedProject || current.projects,
+    environments: validatedEnvironment || current.environments,
+  };
+
+  const localStorageKey = `${LOCAL_STORAGE_KEY}:${org.slug}`;
+
+  try {
+    localStorage.setItem(localStorageKey, JSON.stringify(dataToSave));
+  } catch (ex) {
+    // Do nothing
+  }
+}
+
+/**
+ * Retrives the page filters from local storage
+ */
+export function getPageFilterStorage(orgSlug: string) {
+  const localStorageKey = `${LOCAL_STORAGE_KEY}:${orgSlug}`;
+  const value = localStorage.getItem(localStorageKey);
+
+  if (!value) {
+    return null;
+  }
+
+  try {
+    return JSON.parse(value) as Omit<PageFilters, 'datetime'>;
+  } catch (err) {
+    // use default if invalid
+    Sentry.captureException(err);
+    console.error(err); // eslint-disable-line no-console
+  }
+
+  return null;
+}
+
 /**
  * Removes page filters from localstorage
  */
-export function removePageFiltersStorage(orgId: string) {
-  const localStorageKey = `${LOCAL_STORAGE_KEY}:${orgId}`;
+export function removePageFiltersStorage(orgSlug: string) {
+  const localStorageKey = `${LOCAL_STORAGE_KEY}:${orgSlug}`;
   localStorage.removeItem(localStorageKey);
 }

tests/js/spec/components/organizations/globalSelectionHeader.spec.jsx
@@ -263,6 +263,7 @@ describe('GlobalSelectionHeader', function () {
     await tick();
 
     expect(PageFiltersStore.getState()).toEqual({
+      organization,
       isReady: true,
       selection: {
         datetime: {
@@ -290,6 +291,7 @@ describe('GlobalSelectionHeader', function () {
     await tick();
 
     expect(PageFiltersStore.getState()).toEqual({
+      organization,
       isReady: true,
       selection: {
         datetime: {
@@ -316,6 +318,7 @@ describe('GlobalSelectionHeader', function () {
     await tick();
 
     expect(PageFiltersStore.getState()).toEqual({
+      organization,
       isReady: true,
       selection: {
         datetime: {
@@ -361,6 +364,7 @@ describe('GlobalSelectionHeader', function () {
     expect(globalActions.updateEnvironments).not.toHaveBeenCalled();
 
     expect(PageFiltersStore.getState()).toEqual({
+      organization,
       isReady: true,
       selection: {
         datetime: {

static/app/constants/pageFilters.tsx
@@ -23,6 +23,4 @@ export const DATE_TIME = {
 
 export const DATE_TIME_KEYS = [...Object.values(DATE_TIME), 'statsPeriod'];
 
-export const LOCAL_STORAGE_KEY = 'global-selection';
-
 export const ALL_ACCESS_PROJECTS = -1;

static/app/actions/pageFiltersActions.tsx
@@ -7,7 +7,6 @@ const PageFiltersActions = Reflux.createActions([
   'updateProjects',
   'updateDateTime',
   'updateEnvironments',
-  'save',
 ]);
 
 export default PageFiltersActions;

static/app/stores/pageFiltersStore.tsx
@@ -3,21 +3,14 @@ import Reflux from 'reflux';
 
 import PageFiltersActions from 'sentry/actions/pageFiltersActions';
 import {getDefaultSelection} from 'sentry/components/organizations/pageFilters/utils';
-import {LOCAL_STORAGE_KEY} from 'sentry/constants/pageFilters';
-import OrganizationsStore from 'sentry/stores/organizationsStore';
 import {Organization, PageFilters} from 'sentry/types';
 import {isEqualWithDates} from 'sentry/utils/isEqualWithDates';
-import localStorage from 'sentry/utils/localStorage';
 
 import {CommonStoreInterface} from './types';
 
-type UpdateData = {
-  project: number[];
-  environment: string[];
-};
-
 type State = {
   selection: PageFilters;
+  organization: Organization | null;
   isReady: boolean;
 };
 
@@ -35,7 +28,6 @@ type PageFiltersStoreInterface = CommonStoreInterface<State> & {
   updateProjects(projects: PageFilters['projects'], environments: null | string[]): void;
   updateDateTime(datetime: PageFilters['datetime']): void;
   updateEnvironments(environments: string[]): void;
-  onSave(data: UpdateData): void;
 };
 
 const storeConfig: Reflux.StoreDefinition & Internals & PageFiltersStoreInterface = {
@@ -48,7 +40,6 @@ const storeConfig: Reflux.StoreDefinition & Internals & PageFiltersStoreInterfac
     this.listenTo(PageFiltersActions.reset, this.onReset);
     this.listenTo(PageFiltersActions.initializeUrlState, this.onInitializeUrlState);
     this.listenTo(PageFiltersActions.setOrganization, this.onSetOrganization);
-    this.listenTo(PageFiltersActions.save, this.onSave);
     this.listenTo(PageFiltersActions.updateProjects, this.updateProjects);
     this.listenTo(PageFiltersActions.updateDateTime, this.updateDateTime);
     this.listenTo(PageFiltersActions.updateEnvironments, this.updateEnvironments);
@@ -74,9 +65,9 @@ const storeConfig: Reflux.StoreDefinition & Internals & PageFiltersStoreInterfac
 
   getState() {
     const isReady = this._hasInitialState;
-    const {selection} = this;
+    const {selection, organization} = this;
 
-    return {selection, isReady};
+    return {selection, isReady, organization};
   },
 
   onReset() {
@@ -119,41 +110,6 @@ const storeConfig: Reflux.StoreDefinition & Internals & PageFiltersStoreInterfac
       environments: environments ?? [],
     };
     this.trigger(this.getState());
-  },
-
-  /**
-   * Save to local storage when user explicitly changes header values.
-   *
-   * e.g. if localstorage is empty, user loads issue details for project "foo"
-   * this should not consider "foo" as last used and should not save to local storage.
-   *
-   * However, if user then changes environment, it should...? Currently it will
-   * save the current project alongside environment to local storage. It's debatable if
-   * this is the desired behavior.
-   */
-  onSave(updateObj: UpdateData) {
-    // Do nothing if no org is loaded or user is not an org member. Only
-    // organizations that a user has membership in will be available via the
-    // organizations store
-    if (!this.organization || !OrganizationsStore.get(this.organization.slug)) {
-      return;
-    }
-
-    const {project, environment} = updateObj;
-    const validatedProject = typeof project === 'string' ? [Number(project)] : project;
-    const validatedEnvironment =
-      typeof environment === 'string' ? [environment] : environment;
-
-    try {
-      const localStorageKey = `${LOCAL_STORAGE_KEY}:${this.organization.slug}`;
-      const dataToSave = {
-        projects: validatedProject || this.selection.projects,
-        environments: validatedEnvironment || this.selection.environments,
-      };
-      localStorage.setItem(localStorageKey, JSON.stringify(dataToSave));
-    } catch (ex) {
-      // Do nothing
-    }
   },
 };
 

commit 1bb3d6d53852d46f4569709c2218526ce586e752

ref(pageFilters): Constrict types on updateParams better
This narrows the types on the updateParams function call to better
reflect what is actually going on

 - The UrlParams type has been renamed to  `UpdateParams` and
   `PageFilterQuery`, which better reflect how the parameters are
   transformed when being shuttled between query parameters and more
   structured 'state' objects.

   This is especially important when passing these parameters to the
   `setPageFiltersStorage` function which now specifically always wants
   the `project` parameters to be a string array.

 - Add a test case to cover the regression that occurred in 0bbbfaba84,
   where we could not call `.map` on a string, which typescript failed
   to catch since the types in the pageFilters actionCreators were too
   wide and in some cases incorrect.

   This test would have failed in 0bbbfaba84.

static/app/actionCreators/pageFilters.tsx
@@ -1,5 +1,6 @@
 import {InjectedRouter} from 'react-router';
 import * as Sentry from '@sentry/react';
+import {Location} from 'history';
 import isInteger from 'lodash/isInteger';
 import omit from 'lodash/omit';
 import pick from 'lodash/pick';
@@ -15,6 +16,7 @@ import {
 import {DATE_TIME, URL_PARAM} from 'sentry/constants/pageFilters';
 import PageFiltersStore from 'sentry/stores/pageFiltersStore';
 import {
+  DateString,
   Environment,
   MinimalProject,
   Organization,
@@ -52,9 +54,9 @@ type Options = {
 /**
  * Represents the datetime portion of page filters staate
  */
-type DateTimeObject = {
-  start?: Date | string | null;
-  end?: Date | string | null;
+type DateTimeUpdate = {
+  start?: DateString;
+  end?: DateString;
   statsPeriod?: string | null;
   utc?: string | boolean | null;
   /**
@@ -64,17 +66,25 @@ type DateTimeObject = {
 };
 
 /**
- * Cast project ids to strings, as everything is assumed to be a string in URL params
- *
- * We also handle internal types so Dates and booleans can show up in the start/end/utc
- * keys. Long term it would be good to narrow down these types.
+ * Object used to update the page filter parameters
  */
-type UrlParams = {
+type UpdateParams = {
   project?: ProjectId[] | null;
   environment?: EnvironmentId[] | null;
-} & DateTimeObject & {
-    [others: string]: any;
-  };
+} & DateTimeUpdate;
+
+/**
+ * Output object used for updating query parameters
+ */
+type PageFilterQuery = {
+  project?: string[] | null;
+  environment?: string[] | null;
+  start?: string | null;
+  end?: string | null;
+  statsPeriod?: string | null;
+  utc?: string | null;
+  [extra: string]: Location['query'][string];
+};
 
 /**
  * This can be null which will not perform any router side effects, and instead updates store.
@@ -94,7 +104,7 @@ function getProjectIdFromProject(project: MinimalProject) {
 
 type InitializeUrlStateParams = {
   organization: Organization;
-  queryParams: InjectedRouter['location']['query'];
+  queryParams: Location['query'];
   router: InjectedRouter;
   memberProjects: Project[];
   shouldForceProject?: boolean;
@@ -257,7 +267,7 @@ function isProjectsValid(projects: ProjectId[]) {
  * @param {String[]} [options.resetParams] List of parameters to remove when changing URL params
  */
 export function updateDateTime(
-  datetime: DateTimeObject,
+  datetime: DateTimeUpdate,
   router?: Router,
   options?: Options
 ) {
@@ -291,7 +301,7 @@ export function updateEnvironments(
  * @param [router] React router object
  * @param [options] Options object
  */
-export function updateParams(obj: UrlParams, router?: Router, options?: Options) {
+export function updateParams(obj: UpdateParams, router?: Router, options?: Options) {
   // Allow another component to handle routing
   if (!router) {
     return;
@@ -317,58 +327,63 @@ export function updateParams(obj: UrlParams, router?: Router, options?: Options)
 }
 
 /**
- * Creates a new query parameter object given new params and old params
- * Preserves the old query params, except for `cursor` (can be overriden with keepCursor option)
+ * Merges an UpdateParams object into a Location['query'] object. Results in a
+ * PageFilterQuery
+ *
+ * Preserves the old query params, except for `cursor` (can be overriden with
+ * keepCursor option)
  *
  * @param obj New query params
- * @param oldQueryParams Old query params
+ * @param currentQuery The current query parameters
  * @param [options] Options object
  */
 function getNewQueryParams(
-  obj: UrlParams,
-  oldQueryParams: UrlParams,
-  {resetParams, keepCursor}: Options = {}
+  obj: UpdateParams,
+  currentQuery: Location['query'],
+  options: Options = {}
 ) {
-  const {cursor, statsPeriod, ...oldQuery} = oldQueryParams;
-  const oldQueryWithoutResetParams = !!resetParams?.length
-    ? omit(oldQuery, resetParams)
-    : oldQuery;
+  const {resetParams, keepCursor} = options;
 
-  const newQuery = getParams({
-    ...oldQueryWithoutResetParams,
-    // Some views update using `period`, and some `statsPeriod`, we should make this uniform
-    period: !obj.start && !obj.end ? obj.period || statsPeriod : null,
-    ...obj,
+  const cleanCurrentQuery = !!resetParams?.length
+    ? omit(currentQuery, resetParams)
+    : currentQuery;
+
+  // Normalize existing query parameters
+  const currentQueryState = getStateFromQuery(cleanCurrentQuery, {
+    allowEmptyPeriod: true,
   });
 
-  if (newQuery.start) {
-    newQuery.start = getUtcDateString(newQuery.start);
-  }
+  // Extract non page filter parameters.
+  const cursorParam = !keepCursor ? 'cursor' : null;
+  const omittedParameters = [...Object.values(URL_PARAM), cursorParam].filter(defined);
 
-  if (newQuery.end) {
-    newQuery.end = getUtcDateString(newQuery.end);
-  }
+  const extraParams = omit(cleanCurrentQuery, omittedParameters);
 
-  if (keepCursor) {
-    newQuery.cursor = cursor;
-  }
+  // Override parameters
+  const {project, environment, start, end, utc} = {
+    ...currentQueryState,
+    ...obj,
+  };
 
-  return newQuery;
-}
+  // Only set a stats period if we don't have an absolute date
+  //
+  // `period` is deprecated as a url parameter, though some pages may still
+  // include it (need to actually validate this?), normalize period and stats
+  // period together
+  const statsPeriod =
+    !start && !end ? obj.statsPeriod || obj.period || currentQueryState.period : null;
 
-function getParams(params: UrlParams): UrlParams {
-  const {start, end, period, statsPeriod, ...otherParams} = params;
+  const newQuery: PageFilterQuery = {
+    project: project?.map(String),
+    environment,
+    start: statsPeriod ? null : start instanceof Date ? getUtcDateString(start) : start,
+    end: statsPeriod ? null : end instanceof Date ? getUtcDateString(end) : end,
+    utc: utc ? 'true' : null,
+    statsPeriod,
+    ...extraParams,
+  };
 
-  // `statsPeriod` takes precedence for now
-  const coercedPeriod = statsPeriod || period;
+  const paramEntries = Object.entries(newQuery).filter(([_, value]) => defined(value));
 
-  // Filter null values
-  return Object.fromEntries(
-    Object.entries({
-      statsPeriod: coercedPeriod,
-      start: coercedPeriod ? null : start,
-      end: coercedPeriod ? null : end,
-      ...otherParams,
-    }).filter(([, value]) => defined(value))
-  );
+  return Object.fromEntries(paramEntries) as PageFilterQuery;
 }

static/app/components/organizations/pageFilters/utils.tsx
@@ -7,7 +7,7 @@ import pickBy from 'lodash/pickBy';
 
 import {DATE_TIME_KEYS, URL_PARAM} from 'sentry/constants/pageFilters';
 import OrganizationsStore from 'sentry/stores/organizationsStore';
-import {Environment, PageFilters} from 'sentry/types';
+import {PageFilters} from 'sentry/types';
 import localStorage from 'sentry/utils/localStorage';
 
 import {normalizeDateTimeParams} from './parse';
@@ -85,8 +85,8 @@ function makeLocalStorageKey(orgSlug: string) {
 }
 
 type UpdateData = {
-  project?: Array<string | number> | string | number | null;
-  environment?: Environment['id'][] | null;
+  project?: string[] | null;
+  environment?: string[] | null;
 };
 
 /**
@@ -116,17 +116,9 @@ export function setPageFiltersStorage(
     return;
   }
 
-  const {project, environment} = update;
-  const validatedProject = project
-    ? (Array.isArray(project) ? project : [project])
-        .map(Number)
-        .filter(value => !isNaN(value))
-    : undefined;
-  const validatedEnvironment = environment;
-
   const dataToSave = {
-    projects: validatedProject || current.projects,
-    environments: validatedEnvironment || current.environments,
+    projects: update.project || current.projects,
+    environments: update.environment || current.environments,
   };
 
   const localStorageKey = makeLocalStorageKey(org.slug);

tests/js/spec/actionCreators/pageFilters.spec.jsx
@@ -46,7 +46,7 @@ describe('PageFilters ActionCreators', function () {
         expect.objectContaining({
           query: {
             environment: [],
-            project: [1],
+            project: ['1'],
           },
         })
       );
@@ -126,7 +126,7 @@ describe('PageFilters ActionCreators', function () {
         expect.objectContaining({
           query: {
             cursor: undefined,
-            project: [1],
+            project: ['1'],
             environment: [],
             statsPeriod: '1h',
           },
@@ -153,7 +153,7 @@ describe('PageFilters ActionCreators', function () {
         expect.objectContaining({
           query: {
             cursor: undefined,
-            project: [1],
+            project: ['1'],
             environment: [],
             start: '2020-03-22T00:53:38',
             end: '2020-04-21T00:53:38',
@@ -186,7 +186,7 @@ describe('PageFilters ActionCreators', function () {
         expect.objectContaining({
           query: {
             environment: [],
-            project: [1],
+            project: ['1'],
           },
         })
       );
@@ -336,7 +336,7 @@ describe('PageFilters ActionCreators', function () {
 
       expect(router.push).toHaveBeenCalledWith({
         pathname: '/test/',
-        query: {project: [1]},
+        query: {project: ['1']},
       });
     });
     it('does not update history when queries are the same', function () {
@@ -368,7 +368,7 @@ describe('PageFilters ActionCreators', function () {
 
       expect(router.replace).toHaveBeenCalledWith({
         pathname: '/test/',
-        query: {project: [1]},
+        query: {project: ['1']},
       });
     });
     it('does not update history when queries are the same', function () {

tests/js/spec/components/organizations/globalSelectionHeader.spec.jsx
@@ -7,6 +7,7 @@ import * as globalActions from 'sentry/actionCreators/pageFilters';
 import OrganizationActions from 'sentry/actions/organizationActions';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import ConfigStore from 'sentry/stores/configStore';
+import OrganizationsStore from 'sentry/stores/organizationsStore';
 import PageFiltersStore from 'sentry/stores/pageFiltersStore';
 import ProjectsStore from 'sentry/stores/projectsStore';
 import {getItem} from 'sentry/utils/localStorage';
@@ -62,6 +63,7 @@ describe('GlobalSelectionHeader', function () {
   beforeEach(function () {
     MockApiClient.clearMockResponses();
     ProjectsStore.loadInitialData(organization.projects);
+    OrganizationsStore.add(organization);
 
     getItem.mockImplementation(() => null);
     MockApiClient.addMockResponse({
@@ -139,6 +141,54 @@ describe('GlobalSelectionHeader', function () {
       },
       environments: [],
       projects: [],
+    });
+  });
+
+  it('can change environments with a project selected', async function () {
+    wrapper = mountWithTheme(
+      <PageFiltersContainer
+        organization={organization}
+        projects={organization.projects}
+      />,
+      routerContext
+    );
+
+    await tick();
+    wrapper.update();
+
+    mockRouterPush(wrapper, router);
+
+    // Open dropdown and select one project
+    wrapper.find('MultipleProjectSelector HeaderItem').simulate('click');
+    wrapper.find('MultipleProjectSelector CheckboxFancy').at(1).simulate('click');
+    wrapper.find('MultipleProjectSelector HeaderItem').simulate('click');
+
+    await tick();
+    wrapper.update();
+    expect(wrapper.find('MultipleProjectSelector Content').text()).toBe('project-3');
+
+    // Select environment
+    wrapper.find('MultipleEnvironmentSelector HeaderItem').simulate('click');
+    wrapper.find('MultipleEnvironmentSelector CheckboxFancy').at(0).simulate('click');
+    wrapper.find('MultipleEnvironmentSelector HeaderItem').simulate('click');
+    await tick();
+
+    expect(wrapper.find('MultipleEnvironmentSelector Content').text()).toBe('prod');
+
+    expect(PageFiltersStore.getState().selection).toEqual({
+      datetime: {
+        period: '14d',
+        utc: null,
+        start: null,
+        end: null,
+      },
+      environments: ['prod'],
+      projects: [3],
+    });
+    const query = wrapper.prop('location').query;
+    expect(query).toEqual({
+      environment: 'prod',
+      project: '3',
     });
   });
 
@@ -225,7 +275,7 @@ describe('GlobalSelectionHeader', function () {
         {id: 2, slug: 'prod-project', environments: ['prod']},
       ],
       router: {
-        location: {query: {project: [1]}},
+        location: {query: {project: ['1']}},
         params: {orgId: 'org-slug'},
       },
     });
@@ -406,7 +456,7 @@ describe('GlobalSelectionHeader', function () {
       expect.objectContaining({
         query: {
           environment: ['staging'],
-          project: [3],
+          project: ['3'],
         },
       })
     );
@@ -425,7 +475,7 @@ describe('GlobalSelectionHeader', function () {
         // we need this to be set to make sure org in context is same as
         // current org in URL
         params: {orgId: 'org-slug'},
-        location: {query: {project: [1, 2]}},
+        location: {query: {project: ['1', '2']}},
       },
     });
 
@@ -451,7 +501,7 @@ describe('GlobalSelectionHeader', function () {
         // we need this to be set to make sure org in context is same as
         // current org in URL
         params: {orgId: 'org-slug'},
-        location: {query: {project: [1, 2]}},
+        location: {query: {project: ['1', '2']}},
       },
     });
 
@@ -527,7 +577,7 @@ describe('GlobalSelectionHeader', function () {
           // we need this to be set to make sure org in context is same as
           // current org in URL
           params: {orgId: 'org-slug'},
-          location: {query: {project: [1]}},
+          location: {query: {project: ['1']}},
         },
       });
 
@@ -575,7 +625,7 @@ describe('GlobalSelectionHeader', function () {
 
       expect(initialData.router.replace).toHaveBeenLastCalledWith(
         expect.objectContaining({
-          query: {environment: [], project: [123]},
+          query: {environment: [], project: ['123']},
         })
       );
     });
@@ -586,7 +636,7 @@ describe('GlobalSelectionHeader', function () {
           // we need this to be set to make sure org in context is same as
           // current org in URL
           params: {orgId: 'org-slug'},
-          location: {query: {project: [1, 2]}},
+          location: {query: {project: ['1', '2']}},
         },
       });
 
@@ -597,7 +647,7 @@ describe('GlobalSelectionHeader', function () {
 
       expect(initializationObj.router.replace).toHaveBeenCalledWith(
         expect.objectContaining({
-          query: {environment: [], project: [1]},
+          query: {environment: [], project: ['1']},
         })
       );
     });
@@ -623,7 +673,7 @@ describe('GlobalSelectionHeader', function () {
 
       expect(initializationObj.router.replace).toHaveBeenCalledWith(
         expect.objectContaining({
-          query: {environment: [], project: [3]},
+          query: {environment: [], project: ['3']},
         })
       );
     });
@@ -717,7 +767,7 @@ describe('GlobalSelectionHeader', function () {
         // be the first project
         expect(initialData.router.replace).toHaveBeenLastCalledWith({
           pathname: undefined,
-          query: {cursor: undefined, environment: [], project: [2]},
+          query: {cursor: undefined, environment: [], project: ['2']},
         });
       });
 
@@ -738,7 +788,7 @@ describe('GlobalSelectionHeader', function () {
 
         expect(initialData.router.replace).toHaveBeenLastCalledWith({
           pathname: undefined,
-          query: {environment: [], project: [1]},
+          query: {environment: [], project: ['1']},
         });
 
         expect(initialData.router.replace).toHaveBeenCalledTimes(1);
@@ -754,7 +804,7 @@ describe('GlobalSelectionHeader', function () {
             location: {
               ...initialData.router.location,
               query: {
-                project: 321,
+                project: '321',
               },
             },
           },
@@ -779,7 +829,7 @@ describe('GlobalSelectionHeader', function () {
 
         expect(initialData.router.replace).toHaveBeenLastCalledWith({
           pathname: undefined,
-          query: {environment: [], project: [1]},
+          query: {environment: [], project: ['1']},
         });
       });
     });
@@ -826,7 +876,7 @@ describe('GlobalSelectionHeader', function () {
 
         expect(initialData.router.replace).toHaveBeenLastCalledWith({
           pathname: undefined,
-          query: {environment: [], project: [1], statsPeriod: '90d'},
+          query: {environment: [], project: ['1'], statsPeriod: '90d'},
         });
       });
     });
@@ -908,7 +958,7 @@ describe('GlobalSelectionHeader', function () {
 
         expect(initialData.router.replace).toHaveBeenLastCalledWith({
           pathname: undefined,
-          query: {environment: [], project: [1]},
+          query: {environment: [], project: ['1']},
         });
 
         expect(initialData.router.replace).toHaveBeenCalledTimes(1);

tests/js/spec/components/projectPageFilter.spec.tsx
@@ -56,7 +56,7 @@ describe('ProjectPageFilter', function () {
 
     // Verify we were redirected
     expect(router.push).toHaveBeenCalledWith(
-      expect.objectContaining({query: {environment: [], project: [2]}})
+      expect.objectContaining({query: {environment: [], project: ['2']}})
     );
   });
 });