f66e115773602f6abc5bf867f8f8980962f71670
Josh Ferge <josh.ferge@sentry.io>
2022-01-13 17:24:38 -0800

    document SCIM Members w/ new formatting

--- src/sentry/apidocs/constants.py
+++ src/sentry/apidocs/constants.py
@@ -5,3 +5,5 @@ RESPONSE_UNAUTHORIZED = OpenApiResponse(description="Unauthorized")
 RESPONSE_FORBIDDEN = OpenApiResponse(description="Forbidden")
 
 RESPONSE_NOTFOUND = OpenApiResponse(description="Not Found")
+
+RESPONSE_SUCCESS = OpenApiResponse(description="Success")
