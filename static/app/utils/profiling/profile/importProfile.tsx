7756e8ef010b5070e250ad8fdb3e6e0438e53a75
JonasBa <jonas@badalic.com>
2022-01-18 12:30:32 +0100

    feat(profile): add generic import fn

---
+++ static/app/utils/profiling/profile/importProfile.tsx
@@ -0,0 +1,54 @@
+import {EventedProfile} from './eventedProfile';
+import {JSSelfProfile} from './jsSelfProfile';
+import {Profile} from './profile';
+import {SampledProfile} from './sampledProfile';
+
+export interface ProfileGroup {
+  name: string;
+  traceID: string;
+  activeProfileIndex: number;
+  profiles: Profile[];
+}
+
+type ProfileType =
+  | Profiling.EventedProfile
+  | Profiling.SampledProfile
+  | JSSelfProfiling.Trace;
+
+interface Schema {
+  name: string;
+  activeProfileIndex: number;
+  profiles: ProfileType[];
+}
+
+function isEventedProfile(profile: ProfileType): profile is Profiling.EventedProfile {
+  return 'type' in profile && profile.type === 'evented';
+}
+
+function isSampledProfile(profile: ProfileType): profile is Profiling.SampledProfile {
+  return 'type' in profile && profile.type === 'sampled';
+}
+
+function isJSProfile(profile: ProfileType): profile is JSSelfProfiling.Trace {
+  return 'resources' in profile && Array.isArray(profile.resources);
+}
+
+export function importProfile(input: Schema, traceID: string): ProfileGroup {
+  return {
+    traceID,
+    name: input.name,
+    activeProfileIndex: input.activeProfileIndex ?? 0,
+    profiles: input.profiles.map(profile => {
+      if (isEventedProfile(profile)) {
+        return EventedProfile.FromProfile(profile);
+      }
+      if (isSampledProfile(profile)) {
+        return SampledProfile.FromProfile(profile);
+      }
+      if (isJSProfile(profile)) {
+        return JSSelfProfile.FromProfile(profile);
+      }
+      throw new Error('Unrecognized trace format');
+    }),
+  };
+}
