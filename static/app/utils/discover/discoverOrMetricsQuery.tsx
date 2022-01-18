e2f7989642ad4e31785ff4727f408f25633d6d79
Matej Minar <matej.minar@sentry.io>
2022-01-18 17:26:40 +0100

    feat(ui): Add metrics powered performance table

---
+++ static/app/utils/discover/discoverOrMetricsQuery.tsx
@@ -0,0 +1,43 @@
+import {
+  convertDiscoverFieldsToMetrics,
+  convertDiscoverFieldsToMetricsGroupBys,
+} from 'sentry/utils/metrics/fields';
+import MetricsRequest from 'sentry/utils/metrics/metricsRequest';
+import {useMetricsSwitch} from 'sentry/views/performance/metricsSwitch';
+
+import withApi from '../withApi';
+
+import DiscoverQuery, {DiscoverQueryPropsWithThresholds} from './discoverQuery';
+
+function DiscoverOrMetricsQuery(props: DiscoverQueryPropsWithThresholds) {
+  const {isMetricsData} = useMetricsSwitch(); // TODO(metrics): decision logic will be dynamic in the future, not switch toggle
+
+  if (isMetricsData) {
+    const {eventView, orgSlug, api} = props;
+    const {start, end, statsPeriod, project, environment} = eventView;
+    const fields = eventView.fields.map(({field}) => field);
+
+    return (
+      <MetricsRequest
+        api={api}
+        orgSlug={orgSlug}
+        start={start}
+        end={end}
+        statsPeriod={statsPeriod}
+        project={project}
+        environment={environment}
+        // query={eventView.getQueryWithAdditionalConditions()} TODO(metrics)
+        field={convertDiscoverFieldsToMetrics(fields)}
+        groupBy={convertDiscoverFieldsToMetricsGroupBys(fields)}
+        orderBy={undefined} // TODO(metrics): waiting for api team_key_transactions sorting
+        includeTabularData
+      >
+        {(props as any).children}
+      </MetricsRequest>
+    );
+  }
+
+  return <DiscoverQuery {...props} />;
+}
+
+export default withApi(DiscoverOrMetricsQuery);
