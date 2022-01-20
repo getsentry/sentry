dc54c627593a8c161ce61d140d278cd0253be2ef
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2022-01-20 10:59:12 -0800

    feat(webhooks): Add issue_id to event_alert and error.created webhooks (#31222)

--- tests/sentry/tasks/test_sentry_apps.py
+++ tests/sentry/tasks/test_sentry_apps.py
@@ -153,6 +153,7 @@ class TestSendAlertEvent(TestCase):
                         )
                     ),
                     issue_url=absolute_uri(f"/api/0/issues/{group.id}/"),
+                    issue_id=group.id,
                 ),
                 "triggered_rule": self.rule.label,
             },
@@ -306,6 +307,7 @@ class TestProcessResourceChange(TestCase):
         assert data["action"] == "created"
         assert data["installation"]["uuid"] == install.uuid
         assert data["data"]["error"]["event_id"] == event.event_id
+        assert data["data"]["error"]["issue_id"] == event.group_id
         assert faux(safe_urlopen).kwargs_contain("headers.Content-Type")
         assert faux(safe_urlopen).kwargs_contain("headers.Request-ID")
         assert faux(safe_urlopen).kwargs_contain("headers.Sentry-Hook-Resource")
