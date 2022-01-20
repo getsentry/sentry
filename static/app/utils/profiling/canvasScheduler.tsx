ed4831feb703f5783f062116344509e617fbf581
Jonas <jonas@badalic.com>
2022-01-20 09:53:52 +0100

    feat(profiling): add canvas scheduler (#30929)
    * feat(profiling): add gl utils and gl-matrix lib
    
    * test(utils): add some test coverage
    
    * feat(canvas): add canvas scheduler
    
    * feat(profiling): canvas scheduler test

---
+++ static/app/utils/profiling/canvasScheduler.tsx
@@ -0,0 +1,142 @@
+import {mat3} from 'gl-matrix';
+
+import {Rect} from './gl/utils';
+
+type DrawFn = () => void;
+type ArgumentTypes<F> = F extends (...args: infer A) => any ? A : never;
+
+export interface FlamegraphEvents {
+  transformConfigView: (transform: mat3) => void;
+  setConfigView: (configView: Rect) => void;
+  zoomIntoFrame: (frame: any) => void;
+  selectedNode: (frame: any | null) => void;
+  resetZoom: () => void;
+}
+
+type EventStore = {[K in keyof FlamegraphEvents]: Set<FlamegraphEvents[K]>};
+
+export class CanvasScheduler {
+  beforeFrameCallbacks: Set<DrawFn> = new Set();
+  afterFrameCallbacks: Set<DrawFn> = new Set();
+  requestAnimationFrame: number | null = null;
+
+  events: EventStore = {
+    setConfigView: new Set<FlamegraphEvents['setConfigView']>(),
+    transformConfigView: new Set<FlamegraphEvents['transformConfigView']>(),
+    zoomIntoFrame: new Set<FlamegraphEvents['zoomIntoFrame']>(),
+    selectedNode: new Set<FlamegraphEvents['selectedNode']>(),
+    resetZoom: new Set<FlamegraphEvents['resetZoom']>(),
+  };
+
+  on<K extends keyof FlamegraphEvents>(eventName: K, cb: FlamegraphEvents[K]): void {
+    const set = this.events[eventName] as unknown as Set<FlamegraphEvents[K]>;
+    if (set.has(cb)) {
+      return;
+    }
+    set.add(cb);
+  }
+
+  off<K extends keyof FlamegraphEvents>(eventName: K, cb: FlamegraphEvents[K]): void {
+    const set = this.events[eventName] as unknown as Set<FlamegraphEvents[K]>;
+    if (!set.has(cb)) {
+      return;
+    }
+    set.delete(cb);
+  }
+
+  dispatch<K extends keyof FlamegraphEvents>(
+    event: K,
+    args?: ArgumentTypes<FlamegraphEvents[K]>
+  ): void {
+    for (const handler of this.events[event]) {
+      // @ts-ignore
+      handler(args);
+    }
+  }
+
+  private registerCallback(cb: DrawFn, pool: Set<DrawFn>) {
+    if (pool.has(cb)) {
+      return;
+    }
+    pool.add(cb);
+  }
+
+  private unregisterCallback(cb: DrawFn, pool: Set<DrawFn>) {
+    if (pool.has(cb)) {
+      pool.delete(cb);
+    }
+  }
+
+  registerBeforeFrameCallback(cb: DrawFn): void {
+    this.registerCallback(cb, this.beforeFrameCallbacks);
+  }
+  unregisterBeforeFrameCallback(cb: DrawFn): void {
+    this.unregisterCallback(cb, this.beforeFrameCallbacks);
+  }
+  registerAfterFrameCallback(cb: DrawFn): void {
+    this.registerCallback(cb, this.afterFrameCallbacks);
+  }
+  unregisterAfterFrameCallback(cb: DrawFn): void {
+    this.unregisterCallback(cb, this.afterFrameCallbacks);
+  }
+
+  dispose(): void {
+    for (const type in this.events) {
+      this.events[type].clear();
+    }
+  }
+
+  drawSync(): void {
+    for (const cb of this.beforeFrameCallbacks) {
+      cb();
+    }
+
+    for (const cb of this.afterFrameCallbacks) {
+      cb();
+    }
+  }
+
+  draw(): void {
+    if (this.requestAnimationFrame) {
+      window.cancelAnimationFrame(this.requestAnimationFrame);
+    }
+
+    this.requestAnimationFrame = window.requestAnimationFrame(() => {
+      for (const cb of this.beforeFrameCallbacks) {
+        cb();
+      }
+
+      for (const cb of this.afterFrameCallbacks) {
+        cb();
+      }
+      this.requestAnimationFrame = null;
+    });
+  }
+}
+
+export class CanvasPoolManager {
+  schedulers: Set<CanvasScheduler> = new Set();
+
+  registerScheduler(scheduler: CanvasScheduler): void {
+    if (this.schedulers.has(scheduler)) {
+      return;
+    }
+    this.schedulers.add(scheduler);
+  }
+
+  dispatch<K extends keyof FlamegraphEvents>(
+    event: K,
+    args: ArgumentTypes<FlamegraphEvents[K]>
+  ): void {
+    for (const scheduler of this.schedulers) {
+      scheduler.dispatch(event, args);
+    }
+  }
+
+  unregisterScheduler(scheduler: CanvasScheduler): void {
+    if (this.schedulers.has(scheduler)) {
+      scheduler.dispose();
+      this.schedulers.delete(scheduler);
+    }
+  }
+}
