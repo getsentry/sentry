0df35a484b2fe43287b8942441d8dae49247f6fc
Joshua Li <joshuarli98@gmail.com>
2022-01-19 17:47:32 -0800

    fix one

--- tests/acceptance/sentry_plugins/test_github.py
+++ tests/acceptance/sentry_plugins/test_github.py
@@ -10,7 +10,7 @@ class GitHubTest(AcceptanceTestCase):
         self.project = self.create_project(organization=self.org, teams=[self.team], name="Bengal")
         self.create_member(user=self.user, organization=self.org, role="owner", teams=[self.team])
         self.login_as(self.user)
-        self.path = f"/{self.org.slug}/{self.project.slug}/settings/plugins/github/"
+        self.path = f"/settings/{self.org.slug}/projects/{self.project.slug}/plugins/github/"
 
     def test_simple(self):
         self.browser.get(self.path)
