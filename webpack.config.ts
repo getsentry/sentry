2af14fed864a2716d3efcf4f0bb7205d1e0d77a0
Scott Cooper <scttcper@gmail.com>
2022-01-20 19:17:45 -0800

    build(ui): Upgrade webpack, fix react hot reload
    fixes hot module reloading during development

--- webpack.config.ts
+++ webpack.config.ts
@@ -29,6 +29,7 @@ interface Configuration extends webpack.Configuration {
 const {env} = process;
 
 // Environment configuration
+env.NODE_ENV = env.NODE_ENV ?? 'development';
 const IS_PRODUCTION = env.NODE_ENV === 'production';
 const IS_TEST = env.NODE_ENV === 'test' || !!env.TEST_SUITE;
 const IS_STORYBOOK = env.STORYBOOK_BUILD === '1';
@@ -548,6 +549,10 @@ if (IS_UI_DEV_ONLY) {
       rewrites: [{from: /^\/.*$/, to: '/_assets/index.html'}],
     },
   };
+  appConfig.optimization = {
+    runtimeChunk: 'single',
+  };
+  appConfig.target = 'web';
 }
 
 if (IS_UI_DEV_ONLY || IS_DEPLOY_PREVIEW) {
