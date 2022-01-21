de14b34d6dc496d53325eade185ec38f15b89187
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2022-01-21 09:45:08 -0500

    fix(discover): Add to Dashboard from discover use the updated select option component to match new style (#31261)
    Fixes selector to use newer option style

--- static/app/components/modals/addDashboardWidgetModal.tsx
+++ static/app/components/modals/addDashboardWidgetModal.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import {browserHistory} from 'react-router';
-import {components, OptionProps} from 'react-select';
+import {OptionProps} from 'react-select';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 import cloneDeep from 'lodash/cloneDeep';
@@ -58,6 +58,7 @@ import RadioGroup from 'sentry/views/settings/components/forms/controls/radioGro
 import Field from 'sentry/views/settings/components/forms/field';
 import FieldLabel from 'sentry/views/settings/components/forms/field/fieldLabel';
 
+import Option from '../forms/selectOption';
 import Tooltip from '../tooltip';
 
 import {TAB, TabsButtonBar} from './dashboardWidgetLibraryModal/tabsButtonBar';
@@ -517,11 +518,7 @@ class AddDashboardWidgetModal extends React.Component<Props, State> {
                   containerDisplayMode="block"
                   position="right"
                 >
-                  <components.Option
-                    label={label}
-                    data={data}
-                    {...(optionProps as any)}
-                  />
+                  <Option label={label} data={data} {...(optionProps as any)} />
                 </Tooltip>
               ),
             }}
