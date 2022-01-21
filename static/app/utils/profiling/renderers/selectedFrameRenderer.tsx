a7f19b8fe3afa50f44675d0d4cc88e7073456873
Jonas <jonas@badalic.com>
2022-01-21 07:13:45 +0100

    feat(profiling): add selectedFrameRenderer (#31176)

---
+++ static/app/utils/profiling/renderers/selectedFrameRenderer.tsx
@@ -0,0 +1,41 @@
+import {mat3} from 'gl-matrix';
+
+import {getContext, Rect} from '../gl/utils';
+
+class SelectedFrameRenderer {
+  canvas: HTMLCanvasElement;
+  context: CanvasRenderingContext2D;
+
+  constructor(canvas: HTMLCanvasElement) {
+    this.canvas = canvas;
+    this.context = getContext(canvas, '2d');
+  }
+
+  // We allow for passing of different contexts, this allows us to use a
+  // single instance of the renderer to draw overlays on multiple canvases
+  draw(
+    frame: Rect,
+    style: {BORDER_WIDTH: number; BORDER_COLOR: string},
+    context: CanvasRenderingContext2D = this.context,
+    configToPhysicalSpace: mat3
+  ): void {
+    const frameInPhysicalSpace = frame.transformRect(configToPhysicalSpace);
+
+    // We draw the border in the center of the flamegraph, so we need to increase
+    // the width by border width and negatively offset it by half the border width
+    const borderRect = frameInPhysicalSpace
+      .withWidth(frameInPhysicalSpace.width - style.BORDER_WIDTH)
+      .withHeight(frameInPhysicalSpace.height - style.BORDER_WIDTH)
+      .translate(
+        frameInPhysicalSpace.x + style.BORDER_WIDTH / 2,
+        frameInPhysicalSpace.y + style.BORDER_WIDTH / 2
+      );
+
+    context.beginPath();
+    context.strokeStyle = style.BORDER_COLOR;
+    context.lineWidth = style.BORDER_WIDTH;
+    context.strokeRect(borderRect.x, borderRect.y, borderRect.width, borderRect.height);
+  }
+}
+
+export {SelectedFrameRenderer};
