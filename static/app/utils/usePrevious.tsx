6f7a5f250b915ff30c3d8e0a2f51753d157d9f2b
JonasBa <jonas@badalic.com>
2022-01-24 10:40:10 +0100

    ref(hook): allow usePrevious to initialize with value and add tests

--- static/app/utils/usePrevious.tsx
+++ static/app/utils/usePrevious.tsx
@@ -11,7 +11,7 @@ import {useEffect, useRef} from 'react';
 function usePrevious<T>(value: T) {
   // The ref object is a generic container whose current property is mutable ...
   // ... and can hold any value, similar to an instance property on a class
-  const ref = useRef<T>();
+  const ref = useRef<T>(value);
   // Store current value in ref
   useEffect(() => {
     ref.current = value;
