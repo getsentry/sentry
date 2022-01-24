cce1cf3a6d27ece4b8f03de0271ad6bfd18c1f22
Richard Ma <maxiuyuan1998@gmail.com>
2022-01-24 12:06:36 -0800

    no longer needs to serialize datetime obj

--- src/sentry/utils/transitional_serializer.py
+++ src/sentry/utils/transitional_serializer.py
@@ -1,4 +1,3 @@
-from datetime import datetime, timezone
 from json.decoder import JSONDecodeError
 from typing import Any, Dict
 
@@ -13,33 +12,13 @@ class TransitionalSerializer:
         self.pickle_serializer = PickleSerializer()
         self.json_serializer = JSONSerializer()
 
-    def _convert_datetime_to_timestamp(self, obj):
-        for key in obj:
-            if type(obj[key]) == datetime:
-                formatted_time = obj[key].timestamp()
-                obj[key] = f"timestamp:{formatted_time}"
-            elif type(obj[key]) == dict:
-                self._convert_datetime_to_timestamp(obj[key])
-        return obj
-
-    def _convert_timestamp_to_datetime(self, data):
-        for key in data:
-            if type(data[key]) is str and "timestamp:" in data[key]:
-                timestamp = float(data[key].replace("timestamp:", ""))
-                data[key] = datetime.fromtimestamp(timestamp, timezone.utc)
-            elif type(data[key]) == dict:
-                self._convert_timestamp_to_datetime(data[key])
-        return data
-
     def dumps(self, obj: Dict[str, Any]) -> bytes:
-        serializable_obj = self._convert_datetime_to_timestamp(obj)
-        return self.json_serializer.dumps(serializable_obj)
+        return self.json_serializer.dumps(obj)
 
     def loads(self, data: bytes) -> Dict[str, Any]:
         try:
             metrics.incr("transitional_serializer_json_read")
-            data_obj = self._convert_timestamp_to_datetime(self.json_serializer.loads(data))
-            return data_obj
+            return self.json_serializer.loads(data)
         except JSONDecodeError:
             metrics.incr("transitional_serializer_pickle_read")
             return self.pickle_serializer.loads(data)
--- tests/sentry/utils/test_transitional_serializer.py
+++ tests/sentry/utils/test_transitional_serializer.py
@@ -1,47 +1,24 @@
-import datetime
-
 from django.contrib.sessions.serializers import PickleSerializer
 from django.core.signing import JSONSerializer
 from django.test.testcases import TestCase
-from django.utils import timezone
 
 from sentry.utils.transitional_serializer import TransitionalSerializer
 
 
 class TransitionalSerializerTest(TestCase):
     obj = {"id": 1, "user": "user", "email": "email"}
-    time_obj = {"datetime": datetime.datetime(2019, 12, 19, 13, 56, 56, tzinfo=timezone.utc)}
-    nested_time_obj = {
-        "nested_obj": {"datetime": datetime.datetime(2019, 12, 19, 13, 56, 56, tzinfo=timezone.utc)}
-    }
     p_serializer = PickleSerializer()
     j_serializer = JSONSerializer()
     pickle_obj = p_serializer.dumps(obj)
-    pickle_time_obj = p_serializer.dumps(time_obj)
     json_obj = j_serializer.dumps(obj)
-    json_time_obj = b'{"datetime":"timestamp:1576763816.0"}'
-    json_nested_time_obj = b'{"nested_obj":{"datetime":"timestamp:1576763816.0"}}'
 
     transitional_serializer = TransitionalSerializer()
 
     def test_read_json(self):
         assert self.transitional_serializer.loads(self.json_obj) == self.obj
-        assert (
-            self.transitional_serializer.loads(self.json_time_obj)["datetime"]
-            == self.time_obj["datetime"]
-        )
 
     def test_read_pickle(self):
         assert self.transitional_serializer.loads(self.pickle_obj) == self.obj
 
-    def test_read_pickle_with_non_json_serializable_data(self):
-        assert self.transitional_serializer.loads(self.pickle_time_obj) == self.time_obj
-
     def test_write(self):
         assert self.transitional_serializer.dumps(self.obj) == self.json_obj
-
-    def test_write_datetime(self):
-        assert self.transitional_serializer.dumps(self.time_obj) == self.json_time_obj
-
-    def test_write_nested_datetime(self):
-        assert self.transitional_serializer.dumps(self.nested_time_obj) == self.json_nested_time_obj
