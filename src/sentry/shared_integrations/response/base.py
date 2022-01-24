20e08c9feac68ce2bf097c6ab67cc9e7c44da1bc
Colleen O'Rourke <colleen@sentry.io>
2022-01-24 13:17:02 -0800

    return empty BaseApiResponse

--- src/sentry/shared_integrations/response/base.py
+++ src/sentry/shared_integrations/response/base.py
@@ -60,7 +60,7 @@ class BaseApiResponse:
             if not allow_text:
                 raise ValueError(f"Not a valid response type: {response.text[:128]}")
             elif response.status_code == 301 and ignore_webhook_errors:
-                return
+                return BaseApiResponse()
             elif response.status_code < 200 or response.status_code >= 300:
                 raise ValueError(
                     f"Received unexpected plaintext response for code {response.status_code}"
