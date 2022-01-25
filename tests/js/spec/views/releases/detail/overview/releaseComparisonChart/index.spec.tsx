4bed701f2ddf6f493bb76a34cea9c2a33a4dd464
Matej Minar <matej.minar@sentry.io>
2022-01-25 17:26:16 +0100

    fix(ui): Change the order of release comparison toggle (#31350)

--- tests/js/spec/views/releases/detail/overview/releaseComparisonChart/index.spec.tsx
+++ tests/js/spec/views/releases/detail/overview/releaseComparisonChart/index.spec.tsx
@@ -123,7 +123,7 @@ describe('Releases > Detail > Overview > ReleaseComparison', () => {
       screen.getByTestId('release-comparison-table').textContent
     ).toMatchInlineSnapshot(
       // eslint-disable-next-line no-irregular-whitespace
-      `"DescriptionAll ReleasesThis ReleaseChangeCrash Free Session Rate 99.516%95.006%4.51% Healthy 98.564%94.001%4.563% Abnormal 0%0%0% —Errored 0.953%1.005%0.052% Crashed Session Rate 0.484%4.994%4.511% Crash Free User Rate 99.908%75%24.908% Healthy 98.994%72.022%26.972% Abnormal 0%0%0% —Errored 0.914%2.493%1.579% Crashed User Rate 0.092%25.485%25.393% Session Duration p50 37s195ms—Hide 2 OthersSession Count 205k9.8k—User Count 100k361—"`
+      `"DescriptionAll ReleasesThis ReleaseChangeCrash Free Session Rate 99.516%95.006%4.51% Healthy 98.564%94.001%4.563% Abnormal 0%0%0% Errored 0.953%1.005%0.052% Crashed Session Rate 0.484%4.994%4.511% Crash Free User Rate 99.908%75%24.908% Healthy 98.994%72.022%26.972% Abnormal 0%0%0% Errored 0.914%2.493%1.579% Crashed User Rate 0.092%25.485%25.393% Session Duration p50 37s195ms—Session Count 205k9.8k—User Count 100k361—Hide 2 Others"`
     );
 
     // toggle back
