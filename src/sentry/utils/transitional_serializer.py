d942fb6a9cae0a5e93e0ad38369549228095fec9
Richard Ma <maxiuyuan1998@gmail.com>
2022-01-25 12:01:26 -0800

    added metrics

--- src/sentry/utils/transitional_serializer.py
+++ src/sentry/utils/transitional_serializer.py
@@ -4,6 +4,8 @@ from typing import Any, Dict
 from django.contrib.sessions.serializers import PickleSerializer
 from django.core.signing import JSONSerializer
 
+from sentry.utils import metrics
+
 
 class TransitionalSerializer:
     def __init__(self) -> None:
@@ -17,6 +19,8 @@ class TransitionalSerializer:
 
     def loads(self, data: bytes) -> Dict[str, Any]:
         try:
+            metrics.incr("transitional_serializer_json_read")
             return self.json_serializer.loads(data)
         except JSONDecodeError:
+            metrics.incr("transitional_serializer_pickle_read")
             return self.pickle_serializer.loads(data)
