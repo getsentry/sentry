ebad1e671ea0ed68143d5c0cd2da0713a16da20b
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-01-26 10:57:39 -0500

    Have the svg scale to the maximum possible height

--- static/app/views/dashboardsV2/widgetCard/chart.tsx
+++ static/app/views/dashboardsV2/widgetCard/chart.tsx
@@ -167,7 +167,7 @@ class WidgetCardChart extends React.Component<WidgetCardChartProps> {
             width="100%"
             height="100%"
             viewBox={`0 0 ${w} ${h}`}
-            preserveAspectRatio="xMinYMin meet"
+            preserveAspectRatio="xMinYMax meet"
           >
             <foreignObject x="0" y="0" width="100%" height="100%">
               {rendered}
