3bf8fde44111c7fa08f7924b3a032a5fc86aeb9c
Stephen Cefali <scefali@sentry.io>
2022-01-27 13:42:49 -0800

    adds analytics instance

--- src/sentry/notifications/notifications/base.py
+++ src/sentry/notifications/notifications/base.py
@@ -126,8 +126,15 @@ class BaseNotification(abc.ABC):
     def get_callback_data(self) -> Mapping[str, Any] | None:
         return None
 
-    def record_analytics(self, event_name: str, **kwargs: Any) -> None:
-        analytics.record(event_name, **kwargs)
+    @property
+    def analytics_instance(self) -> Any | None:
+        """
+        Returns on instance for that can be used for analytics
+        """
+        return None
+
+    def record_analytics(self, event_name: str, *args: Any, **kwargs: Any) -> None:
+        analytics.record(event_name, *args, **kwargs)
 
     def record_notification_sent(self, recipient: Team | User, provider: ExternalProviders) -> None:
         # may want to explicitly pass in the parameters for this event
@@ -140,6 +147,7 @@ class BaseNotification(abc.ABC):
         if self.analytics_event:
             self.record_analytics(
                 self.analytics_event,
+                self.analytics_instance,
                 providers=provider.name.lower(),
                 **self.get_custom_analytics_params(recipient),
             )
