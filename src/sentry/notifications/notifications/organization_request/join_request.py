438d6a3461ecd3ca9a748059a268e0727920f149
Stephen Cefali <scefali@sentry.io>
2022-01-27 12:50:40 -0800

    adds recipient to slack methods for notifications

--- src/sentry/notifications/notifications/organization_request/join_request.py
+++ src/sentry/notifications/notifications/organization_request/join_request.py
@@ -1,4 +1,11 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from .abstract_invite_request import AbstractInviteRequestNotification
+
+if TYPE_CHECKING:
+    from sentry.models import Team, User
 
 
 class JoinRequestNotification(AbstractInviteRequestNotification):
@@ -8,8 +15,8 @@ class JoinRequestNotification(AbstractInviteRequestNotification):
     def get_filename(self) -> str:
         return "organization-join-request"
 
-    def build_attachment_title(self) -> str:
+    def build_attachment_title(self, recipient: Team | User) -> str:
         return "Request to Join"
 
-    def get_message_description(self) -> str:
+    def get_message_description(self, recipient: Team | User) -> str:
         return f"{self.pending_member.email} is requesting to join {self.organization.name}"
