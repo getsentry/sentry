3a8ece97764672f428fb89f196a99ca81478e187
William Mak <william@wmak.io>
2022-01-27 14:39:46 -0500

    feat(dashboards): Add a description to each template (#31373)
    - This changes the text under each template card from a count of the
      widgets to a general description of the widgets within which should
      help make them more appealing

--- static/app/views/dashboardsV2/manage/index.tsx
+++ static/app/views/dashboardsV2/manage/index.tsx
@@ -147,7 +147,7 @@ class ManageDashboards extends AsyncView<Props, State> {
           {DASHBOARDS_TEMPLATES.map(dashboard => (
             <TemplateCard
               title={dashboard.title}
-              widgetCount={dashboard.widgets.length}
+              description={dashboard.description}
               onPreview={() => this.onPreview(dashboard.id)}
               onAdd={() => this.onAdd(dashboard)}
               key={dashboard.title}
