ba230c750d608f04809f985c2d07469336fb7578
Richard Ma <maxiuyuan1998@gmail.com>
2022-01-28 11:32:12 -0800

    feat(SessionSerializer): Transitional serializer write to JSON object (#31362)

--- src/sentry/utils/transitional_serializer.py
+++ src/sentry/utils/transitional_serializer.py
@@ -13,10 +13,8 @@ class TransitionalSerializer:
         self.json_serializer = JSONSerializer()
 
     def dumps(self, obj: Dict[str, Any]) -> bytes:
-        # will have the write as pickle obj for now, will change to json in seperate deploy
-        # return self.json_serializer.dumps(obj)
-        metrics.incr("transitional_serializer.pickle_write")
-        return self.pickle_serializer.dumps(obj)
+        metrics.incr("transitional_serializer.json_write")
+        return self.json_serializer.dumps(obj)
 
     def loads(self, data: bytes) -> Dict[str, Any]:
         try:
--- tests/sentry/utils/test_transitional_serializer.py
+++ tests/sentry/utils/test_transitional_serializer.py
@@ -1,5 +1,3 @@
-import datetime
-
 from django.contrib.sessions.serializers import PickleSerializer
 from django.core.signing import JSONSerializer
 from django.test.testcases import TestCase
@@ -9,12 +7,9 @@ from sentry.utils.transitional_serializer import TransitionalSerializer
 
 class TransitionalSerializerTest(TestCase):
     obj = {"id": 1, "user": "user", "email": "email"}
-
-    time_obj = {"time": datetime.date, "datetime": datetime.datetime.today()}
     p_serializer = PickleSerializer()
     j_serializer = JSONSerializer()
     pickle_obj = p_serializer.dumps(obj)
-    pickle_time_obj = p_serializer.dumps(time_obj)
     json_obj = j_serializer.dumps(obj)
 
     transitional_serializer = TransitionalSerializer()
@@ -25,8 +20,5 @@ class TransitionalSerializerTest(TestCase):
     def test_read_pickle(self):
         assert self.transitional_serializer.loads(self.pickle_obj) == self.obj
 
-    def test_read_pickle_with_non_json_serializable_data(self):
-        assert self.transitional_serializer.loads(self.pickle_time_obj) == self.time_obj
-
     def test_write(self):
-        assert self.transitional_serializer.dumps(self.obj) == self.pickle_obj
+        assert self.transitional_serializer.dumps(self.obj) == self.json_obj
