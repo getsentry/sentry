c1573a89eb9748baa5c220f687dcf5b8213f71f2
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-01-28 12:49:57 -0800

    feat(pageFilters): Store pinned page filter state (#31437)

--- static/app/components/organizations/pageFilters/utils.tsx
+++ static/app/components/organizations/pageFilters/utils.tsx
@@ -1,4 +1,3 @@
-import * as Sentry from '@sentry/react';
 import {Location} from 'history';
 import identity from 'lodash/identity';
 import isEqual from 'lodash/isEqual';
@@ -8,10 +7,6 @@ import pickBy from 'lodash/pickBy';
 import {DEFAULT_STATS_PERIOD} from 'sentry/constants';
 import {DATE_TIME_KEYS, URL_PARAM} from 'sentry/constants/pageFilters';
 import {PageFilters} from 'sentry/types';
-import {getUtcDateString} from 'sentry/utils/dates';
-import localStorage from 'sentry/utils/localStorage';
-
-import {getStateFromQuery} from './parse';
 
 /**
  * Make a default page filters object
@@ -73,94 +68,4 @@ export function isSelectionEqual(selection: PageFilters, other: PageFilters): bo
   }
 
   return true;
-}
-
-function makeLocalStorageKey(orgSlug: string) {
-  return `global-selection:${orgSlug}`;
-}
-
-/**
- * Updates the localstorage page filters data
- *
- * e.g. if localstorage is empty, user loads issue details for project "foo"
- * this should not consider "foo" as last used and should not save to local
- * storage.
- *
- * However, if user then changes environment, it should...? Currently it will
- * save the current project alongside environment to local storage. It's
- * debatable if this is the desired behavior.
- */
-export function setPageFiltersStorage(orgSlug: string, selection: PageFilters) {
-  const {start: currentStart, end: currentEnd} = selection.datetime;
-
-  const start = currentStart ? getUtcDateString(currentStart) : null;
-  const end = currentEnd ? getUtcDateString(currentEnd) : null;
-  const period = !start && !end ? selection.datetime.period : null;
-
-  // XXX(epurkhiser): For legacy reasons the page filter state is stored
-  // similarly to how the URL query state is stored, but with different keys
-  // (projects, instead of project).
-  const dataToSave = {
-    projects: selection.projects,
-    environments: selection.environments,
-    start,
-    end,
-    period,
-    utc: selection.datetime.utc ? 'true' : null,
-  };
-
-  const localStorageKey = makeLocalStorageKey(orgSlug);
-
-  try {
-    localStorage.setItem(localStorageKey, JSON.stringify(dataToSave));
-  } catch (ex) {
-    // Do nothing
-  }
-}
-
-/**
- * Retrives the page filters from local storage
- */
-export function getPageFilterStorage(orgSlug: string) {
-  const localStorageKey = makeLocalStorageKey(orgSlug);
-  const value = localStorage.getItem(localStorageKey);
-
-  if (!value) {
-    return null;
-  }
-
-  let decoded: any;
-
-  try {
-    decoded = JSON.parse(value);
-  } catch (err) {
-    // use default if invalid
-    Sentry.captureException(err);
-    console.error(err); // eslint-disable-line no-console
-
-    return null;
-  }
-
-  const {projects, environments, start, end, period, utc} = decoded;
-
-  const state = getStateFromQuery(
-    {
-      project: projects,
-      environment: environments,
-      start,
-      end,
-      period,
-      utc,
-    },
-    {allowAbsoluteDatetime: true}
-  );
-
-  return state;
-}
-
-/**
- * Removes page filters from localstorage
- */
-export function removePageFiltersStorage(orgSlug: string) {
-  localStorage.removeItem(makeLocalStorageKey(orgSlug));
 }
