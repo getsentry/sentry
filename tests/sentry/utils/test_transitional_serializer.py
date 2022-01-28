f2d16551845b2d9fb105232a50ad69450e57c0f2
Richard Ma <maxiuyuan1998@gmail.com>
2022-01-28 14:47:48 -0800

    for rollback

--- tests/sentry/utils/test_transitional_serializer.py
+++ tests/sentry/utils/test_transitional_serializer.py
@@ -20,5 +20,6 @@ class TransitionalSerializerTest(TestCase):
     def test_read_pickle(self):
         assert self.transitional_serializer.loads(self.pickle_obj) == self.obj
 
-    def test_write(self):
-        assert self.transitional_serializer.dumps(self.obj) == self.json_obj
+    # for rollback, social_auth.backends break
+    # def test_write(self):
+    #     assert self.transitional_serializer.dumps(self.obj) == self.json_obj
