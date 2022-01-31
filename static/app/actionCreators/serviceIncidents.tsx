8a445c742b4994e66fc17855d0510dd324028afd
Mark Story <mark@mark-story.com>
2022-01-31 10:51:01 -0500

    fix(ui) Catch network errors when fetching status page
    When requests to statuspage.io fail we don't need to be alerted as there
    isn't anything we can do.
    
    Refs JAVASCRIPT-231K

--- static/app/actionCreators/serviceIncidents.tsx
+++ static/app/actionCreators/serviceIncidents.tsx
@@ -46,12 +46,17 @@ function getIncidentsFromIncidentResponse(statuspageIncidents: StatuspageInciden
 
 export async function loadIncidents(): Promise<SentryServiceStatus | null> {
   const cfg = ConfigStore.get('statuspage');
+  let response: Response | undefined = undefined;
   if (!cfg || !cfg.id) {
     return null;
   }
-  const response = await fetch(
-    `https://${cfg.id}.${cfg.api_host}/api/v2/incidents/unresolved.json`
-  );
+  try {
+    response = await fetch(
+      `https://${cfg.id}.${cfg.api_host}/api/v2/incidents/unresolved.json`
+    );
+  } catch (err) {
+    return null;
+  }
 
   if (!response.ok) {
     return null;
