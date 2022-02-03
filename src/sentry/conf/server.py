badff8e6589f0a8e0794306c661101e2c0e64c6c
Michal Kuffa <michal.kuffa@sentry.io>
2022-02-03 10:31:25 +0100

    Update pg image to v14

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1780,7 +1780,7 @@ SENTRY_DEVSERVICES = {
     ),
     "postgres": lambda settings, options: (
         {
-            "image": "postgres:9.6-alpine",
+            "image": "postgres:14-alpine",
             "pull": True,
             "ports": {"5432/tcp": 5432},
             "environment": {"POSTGRES_DB": "sentry", "POSTGRES_HOST_AUTH_METHOD": "trust"},
