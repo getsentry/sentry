09583f9f5132bbb69ce1929b9ad36199c13a8c7e
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-02-03 14:34:11 -0800

    Move mixins to a directory

---
+++ src/sentry/integrations/mixins/__init__.py
@@ -0,0 +1,20 @@
+"""
+Many of our integration providers offer competing products with the same
+features. For example, both Slack and MS Teams are "communication platforms".
+Both of these providers interact with Sentry in a similar way and should share
+an interface to the rest of the repository. Those shared interfaces should be
+defined as Mixins in this module. We should try to have as much shared business
+logic between providers.
+"""
+
+__all__ = (
+    "IssueBasicMixin",
+    "NotifyBasicMixin",
+    "RepositoryMixin",
+    "ServerlessMixin",
+)
+
+from .issues import IssueBasicMixin
+from .notifications import NotifyBasicMixin
+from .repositories import RepositoryMixin
+from .serverless import ServerlessMixin
