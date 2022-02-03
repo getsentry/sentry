d37dfaac9b459929728e248536e1ac07d4154433
Billy Vong <billy@sentry.io>
2022-02-03 13:15:18 -0500

    fix test, feature needs to be added before all tests

--- tests/sentry/api/endpoints/test_organization_config_integrations.py
+++ tests/sentry/api/endpoints/test_organization_config_integrations.py
@@ -8,6 +8,8 @@ class OrganizationConfigIntegrationsTest(APITestCase):
 
     def setUp(self):
         super().setUp()
+        features.add("organizations:integrations-feature-flag-integration", OrganizationFeature)
+
         self.login_as(self.user)
 
     def test_simple(self):
@@ -27,8 +29,6 @@ class OrganizationConfigIntegrationsTest(APITestCase):
         assert response.data["providers"][0]["name"] == "Example Server"
 
     def test_feature_flag_integration(self):
-        features.add("organizations:integrations-feature-flag-integration", OrganizationFeature)
-
         response = self.get_success_response(self.organization.slug)
         provider = [r for r in response.data["providers"] if r["key"] == "feature_flag_integration"]
         assert len(provider) == 0
