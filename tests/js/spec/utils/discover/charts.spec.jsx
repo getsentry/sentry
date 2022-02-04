245275e673be8507bcd5c6f441d6c292283bd402
Kelly Carino <kelly@sentry.io>
2022-02-04 11:20:52 -0800

    fix(ui): Time format to more human readable (#31584)
    * fix(ui): Time format to more human readable
    
    Fix time format to a more human readable form. Instead of 80s/35hr/8d, show as 1min/1d/1wk.
    
    FIXES ISSUE-1221
    
    * update tests

--- tests/js/spec/utils/discover/charts.spec.jsx
+++ tests/js/spec/utils/discover/charts.spec.jsx
@@ -10,7 +10,7 @@ describe('tooltipFormatter()', function () {
       ['p50()', 100, '100.00ms'],
       ['p50()', 100.23, '100.23ms'],
       ['p50()', 1200, '1.20s'],
-      ['p50()', 86400000, '24.00hr'],
+      ['p50()', 86400000, '1.00d'],
     ];
     for (const scenario of cases) {
       expect(tooltipFormatter(scenario[1], scenario[0])).toEqual(scenario[2]);
