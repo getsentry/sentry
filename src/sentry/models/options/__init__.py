ac19a774bda66a2002c2c571d82f82d5ff1f28d4
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-02-04 12:52:42 -0800

    Combine option models into a submodule

---
+++ src/sentry/models/options/__init__.py
@@ -0,0 +1,11 @@
+from .option import Option
+from .organizationoption import OrganizationOption
+from .projectoption import ProjectOption
+from .useroption import UserOption
+
+__all__ = (
+    "Option",
+    "OrganizationOption",
+    "ProjectOption",
+    "UserOption",
+)
