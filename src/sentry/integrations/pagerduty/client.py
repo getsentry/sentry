92d7e1c90f34e7b869b2dae64a12ea48653b3840
Colleen O'Rourke <colleen@sentry.io>
2022-02-04 13:49:32 -0800

    ref(pagerduty): Add more parameters to logs (#31611)
    * Add additional logging parameters to PagerDuty

--- src/sentry/integrations/pagerduty/client.py
+++ src/sentry/integrations/pagerduty/client.py
@@ -71,7 +71,12 @@ class PagerDutyClient(ApiClient):
         ):
             logger.info(
                 "resolve.received.pagerduty_metric_alert",
-                extra={"organization_id": organization.id, "status_code": response.status_code},
+                extra={
+                    "organization_id": organization.id,
+                    "status_code": response.status_code,
+                    "dedup_key": response.get("dedup_key"),
+                    "message": response.get("message"),
+                },
             )
         return response
 
