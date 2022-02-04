6579de627cc6cb6a6cadbb6617fe8369f058a5c7
Scott Cooper <scttcper@gmail.com>
2022-02-04 10:29:37 -0800

    add more types

--- src/sentry/rules/filters/latest_release.py
+++ src/sentry/rules/filters/latest_release.py
@@ -1,27 +1,27 @@
-from typing import Optional
+from typing import Any, Optional
 
 from django.db.models.signals import post_delete, post_save, pre_delete
 
 from sentry import tagstore
-from sentry.models import Environment, Release, ReleaseEnvironment, ReleaseProject
+from sentry.models import Environment, Event, Release, ReleaseEnvironment, ReleaseProject
 from sentry.rules.filters.base import EventFilter
 from sentry.search.utils import get_latest_release
 from sentry.utils.cache import cache
 
 
-def get_project_release_cache_key(project_id, environment_id=None) -> None:
+def get_project_release_cache_key(project_id: int, environment_id=None) -> str:
     if environment_id is None:
         return f"project:{project_id}:latest_release"
     return f"project:{project_id}:env:{environment_id}:latest_release"
 
 
 # clear the cache given a Release object
-def clear_release_cache(instance: Release, **kwargs) -> None:
+def clear_release_cache(instance: Release, **kwargs: Any) -> None:
     release_project_ids = instance.projects.values_list("id", flat=True)
     cache.delete_many([get_project_release_cache_key(proj_id) for proj_id in release_project_ids])
 
 
-def clear_release_environment_project_cache(instance: ReleaseEnvironment, **kwargs) -> None:
+def clear_release_environment_project_cache(instance: ReleaseEnvironment, **kwargs: Any) -> None:
     release_project_ids = instance.release.projects.values_list("id", flat=True)
     cache.delete_many(
         [
@@ -32,7 +32,7 @@ def clear_release_environment_project_cache(instance: ReleaseEnvironment, **kwar
 
 
 # clear the cache given a ReleaseProject object
-def clear_release_project_cache(instance: ReleaseProject, **kwargs) -> None:
+def clear_release_project_cache(instance: ReleaseProject, **kwargs: Any) -> None:
     proj_id = instance.project_id
     cache.delete(get_project_release_cache_key(proj_id))
 
@@ -40,7 +40,7 @@ def clear_release_project_cache(instance: ReleaseProject, **kwargs) -> None:
 class LatestReleaseFilter(EventFilter):
     label = "The event is from the latest release"
 
-    def get_latest_release(self, event) -> Optional[Release]:
+    def get_latest_release(self, event: Event) -> Optional[Release]:
         environment_id = None if self.rule is None else self.rule.environment_id
         cache_key = get_project_release_cache_key(event.group.project_id, environment_id)
         latest_release = cache.get(cache_key)
@@ -65,7 +65,7 @@ class LatestReleaseFilter(EventFilter):
                 cache.set(cache_key, False, 600)
         return latest_release
 
-    def passes(self, event, state) -> bool:
+    def passes(self, event: Event, state: Any) -> bool:
         latest_release = self.get_latest_release(event)
         if not latest_release:
             return False
