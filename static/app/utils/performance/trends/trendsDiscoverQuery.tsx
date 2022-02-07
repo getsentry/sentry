239bc07a57309e0e684ac157948381bd3a285bff
Dameli U <dameli.ushbayeva@sentry.io>
2022-02-07 17:15:52 -0500

    fix(trends): Revert to using the HOC to get projects

--- static/app/utils/performance/trends/trendsDiscoverQuery.tsx
+++ static/app/utils/performance/trends/trendsDiscoverQuery.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
+import {Project} from 'sentry/types';
 import GenericDiscoverQuery, {
   DiscoverQueryProps,
   GenericChildrenProps,
 } from 'sentry/utils/discover/genericDiscoverQuery';
-import useProjects from 'sentry/utils/useProjects';
 import withApi from 'sentry/utils/withApi';
+import withProjects from 'sentry/utils/withProjects';
 import {
   TrendChangeType,
   TrendFunctionField,
@@ -22,6 +23,7 @@ import {
 
 export type TrendsRequest = {
   eventView: Partial<TrendView>;
+  projects: Project[];
   trendChangeType?: TrendChangeType;
   trendFunctionField?: TrendFunctionField;
 };
@@ -48,8 +50,7 @@ type EventProps = RequestProps & {
 };
 
 export function getTrendsRequestPayload(props: RequestProps) {
-  const {projects} = useProjects();
-  const {eventView} = props;
+  const {eventView, projects} = props;
   const apiPayload: TrendsQuery = eventView?.getEventsAPIPayload(props.location);
   const trendFunction = getCurrentTrendFunction(props.location, props.trendFunctionField);
   const trendParameter = getCurrentTrendParameter(
@@ -95,6 +96,6 @@ function EventsDiscoverQuery(props: EventProps) {
   );
 }
 
-export const TrendsEventsDiscoverQuery = withApi(EventsDiscoverQuery);
+export const TrendsEventsDiscoverQuery = withApi(withProjects(EventsDiscoverQuery));
 
-export default withApi(TrendsDiscoverQuery);
+export default withApi(withProjects(TrendsDiscoverQuery));
