600a17c8e92ae69fb6e3adbd86092f2825c2735d
Scott Cooper <scttcper@gmail.com>
2022-02-07 14:28:15 -0800

    Revert "Revert "feat(alerts): Check latest release is in alert environment (#31560)" (#31647)"
    This reverts commit 7145e22b9b4290037d651ab96d9912a93de1d8e6.

--- tests/sentry/rules/filters/test_latest_release.py
+++ tests/sentry/rules/filters/test_latest_release.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from sentry.models import Release
+from sentry.models import Release, Rule
 from sentry.rules.filters.latest_release import LatestReleaseFilter, get_project_release_cache_key
 from sentry.testutils.cases import RuleTestCase
 from sentry.utils.cache import cache
@@ -81,3 +81,33 @@ class LatestReleaseFilterTest(RuleTestCase):
 
         # rule should pass again because the latest release is oldRelease
         self.assertPasses(rule, event)
+
+    def test_latest_release_with_environment(self):
+        event = self.get_event()
+        self.create_release(
+            project=event.group.project,
+            version="1",
+            date_added=datetime(2020, 9, 1, 3, 8, 24, 880386),
+            environments=[self.environment],
+        )
+
+        new_release = self.create_release(
+            project=event.group.project,
+            version="2",
+            date_added=datetime(2020, 9, 2, 3, 8, 24, 880386),
+            environments=[self.environment],
+        )
+
+        other_env_release = self.create_release(
+            project=event.group.project,
+            version="4",
+            date_added=datetime(2020, 9, 3, 3, 8, 24, 880386),
+        )
+
+        event.data["tags"] = (("release", new_release.version),)
+        environment_rule = self.get_rule(rule=Rule(environment_id=self.environment.id))
+        self.assertPasses(environment_rule, event)
+
+        event.data["tags"] = (("release", other_env_release.version),)
+        environment_rule = self.get_rule(rule=Rule(environment_id=self.environment.id))
+        self.assertDoesNotPass(environment_rule, event)
