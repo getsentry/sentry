14c60ed992a678b1b352c1a6067a828e78e892da
Nar Saynorath <nar.saynorath@sentry.io>
2022-02-07 13:28:26 -0500

    Swap to new big number

--- static/app/views/dashboardsV2/manage/chartPreviews/number.tsx
+++ static/app/views/dashboardsV2/manage/chartPreviews/number.tsx
@@ -1,13 +1,13 @@
 const NumberPreview = () => (
   <svg
-    viewBox="0 0 70 25"
+    viewBox="0 0 70 17"
     xmlns="http://www.w3.org/2000/svg"
     fill="#444674"
+    preserveAspectRatio="xMinYMin meet"
     height="100%"
     width="100%"
-    preserveAspectRatio="xMinYMin meet"
   >
-    <rect x="0" width="80%" height="80%" />
+    <rect width="40" height="17" rx="1" />
   </svg>
 );
 
