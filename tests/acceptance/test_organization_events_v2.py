92a4985cbfe762b389df98a1d1c7658974762770
vuluongj20 <vuluongj20@gmail.com>
2022-02-07 11:53:42 -0800

    fix(acceptance): Fix test_duplicate_query in events_v2
    The page needs to reload before we can see the new, duplicated query.

--- tests/acceptance/test_organization_events_v2.py
+++ tests/acceptance/test_organization_events_v2.py
@@ -567,10 +567,15 @@ class OrganizationEventsV2Test(AcceptanceTestCase, SnubaTestCase):
             card.find_element_by_css_selector('[data-test-id="duplicate"]').click()
 
             duplicate_name = f"{query.name} copy"
+
+            # Reload the page
+            self.browser.get(self.landing_path)
+
             # Wait for new element to show up.
             self.browser.element(f'[data-test-id="card-{duplicate_name}"]')
-        # Assert the new query exists and has 'copy' added to the name.
-        assert DiscoverSavedQuery.objects.filter(name=duplicate_name).exists()
+            
+            # Assert the new query exists and has 'copy' added to the name.
+            assert DiscoverSavedQuery.objects.filter(name=duplicate_name).exists()
 
     @pytest.mark.skip(reason="causing timeouts in github actions and travis")
     @patch("django.utils.timezone.now")
