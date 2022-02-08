b1f0bed26a47fd18af2c28c287c470f62939a143
Jesse Box <me@jessebox.net>
2022-02-08 09:52:01 +0100

    [Modified] label const

--- static/app/components/events/interfaces/threads/threadSelector/option.tsx
+++ static/app/components/events/interfaces/threads/threadSelector/option.tsx
@@ -23,7 +23,7 @@ type ThreadInfo = {
 };
 
 const Option = ({id, details, name, crashed, crashedInfo}: Props) => {
-  const {label = `<${t('unknown')}>`} = details;
+  const label = details.label ?? `<${t('unknown')}>`;
   const optionName = name || `<${t('unknown')}>`;
 
   return (
