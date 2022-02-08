94e0952fd6fca2e899b048a6dc571a871ce4efe9
Scott Cooper <scttcper@gmail.com>
2022-02-08 13:48:53 -0800

    feat(alerts): Check latest release is in alert environment (#31648)

--- src/sentry/rules/filters/latest_release.py
+++ src/sentry/rules/filters/latest_release.py
@@ -1,24 +1,38 @@
+from typing import Any, Optional
+
 from django.db.models.signals import post_delete, post_save, pre_delete
 
 from sentry import tagstore
-from sentry.api.serializers.models.project import bulk_fetch_project_latest_releases
-from sentry.models import Release, ReleaseProject
+from sentry.models import Environment, Event, Release, ReleaseEnvironment, ReleaseProject
 from sentry.rules.filters.base import EventFilter
+from sentry.search.utils import get_latest_release
 from sentry.utils.cache import cache
 
 
-def get_project_release_cache_key(project_id):
-    return f"project:{project_id}:latest_release"
+def get_project_release_cache_key(project_id: int, environment_id: Optional[int] = None) -> str:
+    if environment_id is None:
+        return f"project:{project_id}:latest_release"
+    return f"project:{project_id}:env:{environment_id}:latest_release"
 
 
 # clear the cache given a Release object
-def clear_release_cache(instance, **kwargs):
+def clear_release_cache(instance: Release, **kwargs: Any) -> None:
     release_project_ids = instance.projects.values_list("id", flat=True)
     cache.delete_many([get_project_release_cache_key(proj_id) for proj_id in release_project_ids])
 
 
+def clear_release_environment_project_cache(instance: ReleaseEnvironment, **kwargs: Any) -> None:
+    release_project_ids = instance.release.projects.values_list("id", flat=True)
+    cache.delete_many(
+        [
+            get_project_release_cache_key(proj_id, instance.environment_id)
+            for proj_id in release_project_ids
+        ]
+    )
+
+
 # clear the cache given a ReleaseProject object
-def clear_release_project_cache(instance, **kwargs):
+def clear_release_project_cache(instance: ReleaseProject, **kwargs: Any) -> None:
     proj_id = instance.project_id
     cache.delete(get_project_release_cache_key(proj_id))
 
@@ -26,11 +40,28 @@ def clear_release_project_cache(instance, **kwargs):
 class LatestReleaseFilter(EventFilter):
     label = "The event is from the latest release"
 
-    def get_latest_release(self, event):
-        cache_key = get_project_release_cache_key(event.group.project_id)
+    def get_latest_release(self, event: Event) -> Optional[Release]:
+        environment_id = None if self.rule is None else self.rule.environment_id
+        cache_key = get_project_release_cache_key(event.group.project_id, environment_id)
         latest_release = cache.get(cache_key)
         if latest_release is None:
-            latest_releases = bulk_fetch_project_latest_releases([event.group.project])
+            organization_id = event.group.project.organization_id
+            environments = None
+            if environment_id:
+                environments = [Environment.objects.get(id=environment_id)]
+            try:
+                latest_release_versions = get_latest_release(
+                    [event.group.project],
+                    environments,
+                    organization_id,
+                )
+            except Release.DoesNotExist:
+                return None
+            latest_releases = list(
+                Release.objects.filter(
+                    version=latest_release_versions[0], organization_id=organization_id
+                )
+            )
             if latest_releases:
                 cache.set(cache_key, latest_releases[0], 600)
                 return latest_releases[0]
@@ -38,7 +69,7 @@ class LatestReleaseFilter(EventFilter):
                 cache.set(cache_key, False, 600)
         return latest_release
 
-    def passes(self, event, state):
+    def passes(self, event: Event, state: Any) -> bool:
         latest_release = self.get_latest_release(event)
         if not latest_release:
             return False
@@ -61,3 +92,6 @@ pre_delete.connect(clear_release_cache, sender=Release, weak=False)
 
 post_save.connect(clear_release_project_cache, sender=ReleaseProject, weak=False)
 post_delete.connect(clear_release_project_cache, sender=ReleaseProject, weak=False)
+
+post_save.connect(clear_release_environment_project_cache, sender=ReleaseEnvironment, weak=False)
+post_delete.connect(clear_release_environment_project_cache, sender=ReleaseEnvironment, weak=False)
