4655573d38e78f26cd4b622b5fab2eb778957e89
Nar Saynorath <nar.saynorath@sentry.io>
2022-02-08 13:04:17 -0500

    Tables look better now

--- static/app/views/dashboardsV2/manage/chartPreviews/table.tsx
+++ static/app/views/dashboardsV2/manage/chartPreviews/table.tsx
@@ -1,7 +1,9 @@
 const TablePreview = () => (
   <svg
-    viewBox="0 0 157 86"
-    preserveAspectRatio="xMinYMin meet"
+    height="155px"
+    width="100%"
+    viewBox="0 0 157 155"
+    preserveAspectRatio="none"
     fill="none"
     xmlns="http://www.w3.org/2000/svg"
   >
@@ -10,24 +12,52 @@ const TablePreview = () => (
     <rect x="8" y="21" width="42" height="2" rx="1" fill="#D4D1EC" />
     <rect x="8" y="30" width="42" height="2" rx="1" fill="#D4D1EC" />
     <rect x="8" y="39" width="42" height="2" rx="1" fill="#D4D1EC" />
+    <rect x="8" y="92" width="42" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="8" y="101" width="42" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="8" y="110" width="42" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="8" y="119" width="42" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="136" y="92" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="136" y="101" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="136" y="110" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="136" y="119" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="8" y="128" width="42" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="8" y="137" width="42" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="8" y="146" width="42" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="8" y="155" width="42" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="136" y="128" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="136" y="137" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="136" y="146" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="136" y="155" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="118" y="92" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="118" y="101" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="118" y="110" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="118" y="119" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="118" y="128" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="118" y="137" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="118" y="146" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="118" y="155" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="100" y="92" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="100" y="101" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="100" y="110" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="100" y="119" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="100" y="128" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="100" y="137" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="100" y="146" width="12" height="2" rx="1" fill="#d4d1ec" />
+    <rect x="100" y="155" width="12" height="2" rx="1" fill="#d4d1ec" />
     <rect x="8" y="48" width="42" height="2" rx="1" fill="#D4D1EC" />
-
     <rect x="137" y="6" width="12" height="2" rx="1" fill="#9086D4" />
     <rect x="137" y="21" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="137" y="30" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="137" y="39" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="137" y="48" width="12" height="2" rx="1" fill="#D4D1EC" />
-
     <rect x="8" y="57" width="42" height="2" rx="1" fill="#D4D1EC" />
     <rect x="8" y="66" width="42" height="2" rx="1" fill="#D4D1EC" />
     <rect x="8" y="75" width="42" height="2" rx="1" fill="#D4D1EC" />
     <rect x="8" y="84" width="42" height="2" rx="1" fill="#D4D1EC" />
-
     <rect x="137" y="57" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="137" y="66" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="137" y="75" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="137" y="84" width="12" height="2" rx="1" fill="#D4D1EC" />
-
     <rect x="119" y="6" width="12" height="2" rx="1" fill="#9086D4" />
     <rect x="119" y="21" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="119" y="30" width="12" height="2" rx="1" fill="#D4D1EC" />
@@ -37,7 +67,6 @@ const TablePreview = () => (
     <rect x="119" y="66" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="119" y="75" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="119" y="84" width="12" height="2" rx="1" fill="#D4D1EC" />
-
     <rect x="101" y="6" width="12" height="2" rx="1" fill="#9086D4" />
     <rect x="101" y="21" width="12" height="2" rx="1" fill="#D4D1EC" />
     <rect x="101" y="30" width="12" height="2" rx="1" fill="#D4D1EC" />
