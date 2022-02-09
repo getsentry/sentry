4dfb90a207ca66b72f162e89179dcc4d3ab3516c
Jonas <jonas@badalic.com>
2022-02-09 08:18:07 +0100

    feat(profiling): add flamegraph view (#31626)
    * fix(profiling): ignore unused renderer in test
    
    * feat(profiling): add util hooks
    
    * ref(hook): add usememowithprevious tests
    
    * feat(profiling): zoom view
    
    * fix(flamegraphrenderer): adjust for profiles that do not start at 0 and trim text more accurately
    
    * fix(profile): fix type
    
    * ref(profiling): use styled component
    
    * ref(profiling): code review
    
    * test(profiling): fix test

---
+++ docs-ui/stories/components/profiling/flamegraphZoomView.stories.js
@@ -0,0 +1,28 @@
+import {FlamegraphZoomView} from 'sentry/components/profiling/FlamegraphZoomView';
+import {CanvasPoolManager} from 'sentry/utils/profiling/canvasScheduler';
+import {Flamegraph} from 'sentry/utils/profiling/flamegraph';
+import {LightFlamegraphTheme} from 'sentry/utils/profiling/flamegraph/FlamegraphTheme';
+import {importProfile} from 'sentry/utils/profiling/profile/importProfile';
+
+const trace = require('./EventedTrace.json');
+
+export default {
+  title: 'Components/Profiling/FlamegraphZoomView',
+};
+
+export const EventedTrace = () => {
+  const canvasPoolManager = new CanvasPoolManager();
+
+  const profiles = importProfile(trace);
+  const flamegraph = new Flamegraph(profiles.profiles[0]);
+
+  return (
+    <FlamegraphZoomView
+      flamegraph={flamegraph}
+      highlightRecursion="false"
+      colorCoding="by symbol name"
+      canvasPoolManager={canvasPoolManager}
+      flamegraphTheme={LightFlamegraphTheme}
+    />
+  );
+};
