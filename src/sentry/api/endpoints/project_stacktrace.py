6e1030909fc5e571b7ed0a3bf42bc447573cff20
Pierre Massat <pierre.massat@sentry.io>
2022-02-10 09:42:22 -0800

    Fix endpoint class name

--- src/sentry/api/endpoints/project_stacktrace.py
+++ src/sentry/api/endpoints/project_stacktrace.py
@@ -7,7 +7,7 @@ from sentry.api.bases.project import ProjectEndpoint
 from sentry.http import safe_urlopen
 
 
-class StacktraceEndpoint(ProjectEndpoint):
+class ProjectStacktraceEndpoint(ProjectEndpoint):
     def get(self, request: Request, project, transaction_id) -> Response:
         if not features.has("organizations:profiling", project.organization, actor=request.user):
             return Response(status=404)
