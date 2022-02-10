9eafae0eadd7061278ab36d5bee43867d483a4ab
Josh Ferge <josh.ferge@sentry.io>
2022-02-10 12:32:04 -0800

    add ability to provide user to query audit log data (#31593)

--- src/sentry/api/endpoints/organization_auditlogs.py
+++ src/sentry/api/endpoints/organization_auditlogs.py
@@ -1,3 +1,4 @@
+from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -5,9 +6,21 @@ from sentry.api.bases import OrganizationEndpoint
 from sentry.api.bases.organization import OrganizationAuditPermission
 from sentry.api.paginator import DateTimePaginator
 from sentry.api.serializers import serialize
+from sentry.db.models.fields.bounded import BoundedIntegerField
 from sentry.models import AuditLogEntry
 
 EVENT_REVERSE_MAP = {v: k for k, v in AuditLogEntry._meta.get_field("event").choices}
+
+
+class AuditLogQueryParamSerializer(serializers.Serializer):
+
+    event = serializers.CharField(required=False)
+    actor = serializers.IntegerField(required=False, max_value=BoundedIntegerField.MAX_VALUE)
+
+    def validate_event(self, event):
+        if event not in EVENT_REVERSE_MAP:
+            raise serializers.ValidationError("Invalid audit log event")
+        return EVENT_REVERSE_MAP[event]
 
 
 class OrganizationAuditLogsEndpoint(OrganizationEndpoint):
@@ -16,12 +29,18 @@ class OrganizationAuditLogsEndpoint(OrganizationEndpoint):
     def get(self, request: Request, organization) -> Response:
         queryset = AuditLogEntry.objects.filter(organization=organization).select_related("actor")
 
-        event = request.GET.get("event")
-        if event:
-            try:
-                queryset = queryset.filter(event=EVENT_REVERSE_MAP[event])
-            except KeyError:
-                queryset = queryset.none()
+        serializer = AuditLogQueryParamSerializer(data=request.GET)
+
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+
+        query = serializer.validated_data
+
+        if "actor" in query:
+            queryset = queryset.filter(actor=query["actor"])
+
+        if "event" in query:
+            queryset = queryset.filter(event=query["event"])
 
         return self.paginate(
             request=request,
--- tests/sentry/api/endpoints/test_organization_auditlogs.py
+++ tests/sentry/api/endpoints/test_organization_auditlogs.py
@@ -1,6 +1,7 @@
 from datetime import timedelta
 
 from django.utils import timezone
+from rest_framework.exceptions import ErrorDetail
 
 from sentry.models import AuditLogEntry, AuditLogEntryEvent
 from sentry.testutils import APITestCase
@@ -16,14 +17,16 @@ class OrganizationAuditLogsTest(APITestCase):
     def test_simple(self):
         now = timezone.now()
 
-        org = self.create_organization(owner=self.user, name="baz")
-        org2 = self.create_organization(owner=self.user, name="baz")
+        org2 = self.create_organization(owner=self.user)
 
         entry1 = AuditLogEntry.objects.create(
-            organization=org, event=AuditLogEntryEvent.ORG_EDIT, actor=self.user, datetime=now
+            organization=self.organization,
+            event=AuditLogEntryEvent.ORG_EDIT,
+            actor=self.user,
+            datetime=now,
         )
         entry2 = AuditLogEntry.objects.create(
-            organization=org,
+            organization=self.organization,
             event=AuditLogEntryEvent.ORG_EDIT,
             actor=self.user,
             datetime=now + timedelta(seconds=1),
@@ -32,7 +35,114 @@ class OrganizationAuditLogsTest(APITestCase):
             organization=org2, event=AuditLogEntryEvent.ORG_EDIT, actor=self.user, datetime=now
         )
 
-        response = self.get_success_response(org.slug)
+        response = self.get_success_response(self.organization.slug)
         assert len(response.data) == 2
         assert response.data[0]["id"] == str(entry2.id)
         assert response.data[1]["id"] == str(entry1.id)
+
+    def test_filter_by_event(self):
+        now = timezone.now()
+
+        entry1 = AuditLogEntry.objects.create(
+            organization=self.organization,
+            event=AuditLogEntryEvent.ORG_EDIT,
+            actor=self.user,
+            datetime=now,
+        )
+        AuditLogEntry.objects.create(
+            organization=self.organization,
+            event=AuditLogEntryEvent.ORG_ADD,
+            actor=self.user,
+            datetime=now + timedelta(seconds=1),
+        )
+
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"event": "org.edit"}
+        )
+        assert len(response.data) == 1
+        assert response.data[0]["id"] == str(entry1.id)
+
+    def test_filter_by_user(self):
+        now = timezone.now()
+
+        org = self.create_organization(owner=self.user)
+
+        user2 = self.create_user()
+
+        self.create_member(user=user2, organization=self.organization)
+
+        entry1 = AuditLogEntry.objects.create(
+            organization=org, event=AuditLogEntryEvent.ORG_EDIT, actor=self.user, datetime=now
+        )
+        AuditLogEntry.objects.create(
+            organization=org, event=AuditLogEntryEvent.ORG_EDIT, actor=user2, datetime=now
+        )
+
+        response = self.get_success_response(org.slug, qs_params={"actor": self.user.id})
+        assert len(response.data) == 1
+        assert response.data[0]["id"] == str(entry1.id)
+
+    def test_filter_by_user_and_event(self):
+        now = timezone.now()
+
+        org = self.create_organization(owner=self.user)
+
+        user2 = self.create_user()
+
+        self.create_member(user=user2, organization=self.organization)
+
+        entry1 = AuditLogEntry.objects.create(
+            organization=org, event=AuditLogEntryEvent.ORG_EDIT, actor=self.user, datetime=now
+        )
+        AuditLogEntry.objects.create(
+            organization=org, event=AuditLogEntryEvent.ORG_EDIT, actor=user2, datetime=now
+        )
+        AuditLogEntry.objects.create(
+            organization=org,
+            event=AuditLogEntryEvent.ORG_ADD,
+            actor=self.user,
+            datetime=now + timedelta(seconds=1),
+        )
+
+        response = self.get_success_response(
+            org.slug, qs_params={"event": "org.edit", "actor": self.user.id}
+        )
+        assert len(response.data) == 1
+        assert response.data[0]["id"] == str(entry1.id)
+
+    def test_invalid_event(self):
+        now = timezone.now()
+
+        AuditLogEntry.objects.create(
+            organization=self.organization,
+            event=AuditLogEntryEvent.ORG_EDIT,
+            actor=self.user,
+            datetime=now,
+        )
+
+        response = self.get_error_response(
+            self.organization.slug, qs_params={"event": "wrong"}, status_code=400
+        )
+        assert response.data == {"event": ["Invalid audit log event"]}
+
+    def test_user_out_of_bounds(self):
+        now = timezone.now()
+
+        AuditLogEntry.objects.create(
+            organization=self.organization,
+            event=AuditLogEntryEvent.ORG_EDIT,
+            actor=self.user,
+            datetime=now,
+        )
+
+        response = self.get_error_response(
+            self.organization.slug, qs_params={"actor": 111111111111}, status_code=400
+        )
+        assert response.data == {
+            "actor": [
+                ErrorDetail(
+                    string="Ensure this value is less than or equal to 2147483647.",
+                    code="max_value",
+                )
+            ]
+        }
