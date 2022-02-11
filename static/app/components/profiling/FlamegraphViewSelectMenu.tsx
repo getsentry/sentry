cc0ed3544d9ee1f92890a0dc4f5f3a760a003978
JonasBa <jonas@badalic.com>
2022-02-11 08:26:38 +0100

    fix(viewselectmenu): use locale

--- static/app/components/profiling/FlamegraphViewSelectMenu.tsx
+++ static/app/components/profiling/FlamegraphViewSelectMenu.tsx
@@ -1,11 +1,12 @@
 import Button from 'sentry/components/button';
 import ButtonBar from 'sentry/components/buttonBar';
+import {t} from 'sentry/locale';
 
 interface FlamegraphViewSelectMenuProps {
-  view: 'top down' | 'bottom up';
-  sorting: 'call order' | 'left heavy';
-  onViewChange: (view: FlamegraphViewSelectMenuProps['view']) => void;
   onSortingChange: (sorting: FlamegraphViewSelectMenuProps['sorting']) => void;
+  onViewChange: (view: FlamegraphViewSelectMenuProps['view']) => void;
+  sorting: 'call order' | 'left heavy';
+  view: 'top down' | 'bottom up';
 }
 
 function FlamegraphViewSelectMenu({
@@ -21,28 +22,28 @@ function FlamegraphViewSelectMenu({
         priority={sorting === 'call order' ? 'primary' : undefined}
         onClick={() => onSortingChange('call order')}
       >
-        Call Order
+        {t('Call Order')}
       </Button>
       <Button
         size="xsmall"
         priority={sorting === 'left heavy' ? 'primary' : undefined}
         onClick={() => onSortingChange('left heavy')}
       >
-        Left Heavy
+        {t('Left Heavy')}
       </Button>
       <Button
         size="xsmall"
         priority={view === 'bottom up' ? 'primary' : undefined}
         onClick={() => onViewChange('bottom up')}
       >
-        Bottom Up
+        {t('Bottom Up')}
       </Button>
       <Button
         size="xsmall"
         priority={view === 'top down' ? 'primary' : undefined}
         onClick={() => onViewChange('top down')}
       >
-        Top Down
+        {t('Top Down')}
       </Button>
     </ButtonBar>
   );
