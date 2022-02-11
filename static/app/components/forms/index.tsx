e4b539f64d941999fbd6f0131b233a833d9bb2bd
Mark Story <mark@mark-story.com>
2022-02-11 16:50:36 -0500

    Add shims for getsentry.
    I'll remove these once getsentry has been updated.

---
+++ static/app/components/forms/index.tsx
@@ -0,0 +1,12 @@
+import {
+  BooleanField,
+  DateTimeField,
+  Form,
+  NumberField,
+  SelectField,
+} from '../deprecatedforms';
+
+/**
+ * @deprecated Use imports from sentry/components/deprecatedforms. This shim is to support getsentry temporarily
+ */
+export {BooleanField, DateTimeField, Form, NumberField, SelectField};
---
+++ static/app/components/forms/numberField.tsx
@@ -0,0 +1,6 @@
+import NumberField from '../deprecatedforms/numberField';
+
+/**
+ * @deprecated Use imports from sentry/components/deprecatedforms. This shim is to support getsentry temporarily
+ */
+export default NumberField;
