48b295e04a370e11d3253e2ed8ffe0db14de060e
Nar Saynorath <nar.saynorath@sentry.io>
2022-02-14 12:57:20 -0500

    fix(dashboard-layout): Fix tooltips by removing CSS Transforms (#31762)
    Removes CSS Transforms that create stacking contexts, as well as increase the z-index of the
    add widget tile to prevent acceptance test flaking

--- static/app/views/dashboardsV2/dashboard.tsx
+++ static/app/views/dashboardsV2/dashboard.tsx
@@ -549,17 +549,21 @@ class Dashboard extends Component<Props, State> {
             <IconResize />
           </ResizeHandle>
         }
+        useCSSTransforms={false}
         isBounded
       >
         {widgetsWithLayout.map((widget, index) => this.renderWidget(widget, index))}
         {isEditing && !!!widgetLimitReached && (
-          <div key={ADD_WIDGET_BUTTON_DRAG_ID} data-grid={this.addWidgetLayout}>
+          <AddWidgetWrapper
+            key={ADD_WIDGET_BUTTON_DRAG_ID}
+            data-grid={this.addWidgetLayout}
+          >
             <AddWidget
               orgFeatures={organization.features}
               onAddWidget={this.handleStartAdd}
               onOpenWidgetBuilder={this.handleOpenWidgetBuilder}
             />
-          </div>
+          </AddWidgetWrapper>
         )}
       </GridLayout>
     );
@@ -639,19 +643,21 @@ const WidgetContainer = styled('div')`
   }
 `;
 
+// A widget being dragged has a z-index of 3
+// Allow the Add Widget tile to show above widgets when moved
+const AddWidgetWrapper = styled('div')`
+  z-index: 5;
+  background-color: ${p => p.theme.background};
+`;
+
 const GridItem = styled('div')`
   .react-resizable-handle {
     z-index: 2;
   }
 `;
 
-// HACK: to stack chart tooltips above other grid items
 const GridLayout = styled(WidthProvider(Responsive))`
   margin: -${space(2)};
-
-  .react-grid-item:hover {
-    z-index: 10;
-  }
 
   .react-resizable-handle {
     background-image: none;
