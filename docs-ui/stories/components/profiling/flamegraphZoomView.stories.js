4a3ae2830e1838c8ffe9638f2d4c4450262cb27f
Jonas <jonas@badalic.com>
2022-02-15 09:18:58 +0100

    feat(profiling): add view select menu (#31698)
    * feat(flamegraph): add view select menu
    
    * feat(profiling): use buttonbar
    
    * fix(viewselectmenu): use locale
    
    * test(gridrenderer): update tests

--- docs-ui/stories/components/profiling/flamegraphZoomView.stories.js
+++ docs-ui/stories/components/profiling/flamegraphZoomView.stories.js
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import {FlamegraphViewSelectMenu} from 'sentry/components/profiling/FlamegraphViewSelectMenu';
 import {FlamegraphZoomView} from 'sentry/components/profiling/FlamegraphZoomView';
 import {FlamegraphZoomViewMinimap} from 'sentry/components/profiling/FlamegraphZoomViewMinimap';
 import {CanvasPoolManager} from 'sentry/utils/profiling/canvasScheduler';
@@ -16,8 +17,16 @@ export default {
 export const EventedTrace = () => {
   const canvasPoolManager = new CanvasPoolManager();
 
+  const [view, setView] = React.useState({inverted: false, leftHeavy: false});
+
   const profiles = importProfile(trace);
-  const flamegraph = new Flamegraph(profiles.profiles[0]);
+
+  const flamegraph = new Flamegraph(
+    profiles.profiles[0],
+    0,
+    view.inverted,
+    view.leftHeavy
+  );
 
   return (
     <div
@@ -29,6 +38,18 @@ export const EventedTrace = () => {
         overscrollBehavior: 'contain',
       }}
     >
+      <div>
+        <FlamegraphViewSelectMenu
+          view={view.inverted ? 'bottom up' : 'top down'}
+          sorting={view.leftHeavy ? 'left heavy' : 'call order'}
+          onSortingChange={s => {
+            setView({...view, leftHeavy: s === 'left heavy'});
+          }}
+          onViewChange={v => {
+            setView({...view, inverted: v === 'bottom up'});
+          }}
+        />
+      </div>
       <div style={{height: 100, position: 'relative'}}>
         <FlamegraphZoomViewMinimap
           flamegraph={flamegraph}
