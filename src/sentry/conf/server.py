fd523265801bc0469cfba501ce4fe5be3358c0d9
Dameli U <dameli.ushbayeva@sentry.io>
2022-02-14 20:49:02 -0500

    revert backend flags

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1071,11 +1071,11 @@ SENTRY_FEATURES = {
     # Enable views for ops breakdown
     "organizations:performance-ops-breakdown": False,
     # Enable landing improvements for performance
-    # "organizations:performance-landing-widgets": True,
+    "organizations:performance-landing-widgets": True,
     # Enable interpolation of null data points in charts instead of zerofilling in performance
     "organizations:performance-chart-interpolation": False,
     # Enable mobile vitals
-    # "organizations:performance-mobile-vitals": False,
+    "organizations:performance-mobile-vitals": False,
     # Enable views for suspect tags
     "organizations:performance-suspect-spans-view": False,
     # Enable views for anomaly detection
