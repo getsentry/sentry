9a945ecd2c36c7b09c20605ea32fd3ad30eca11d
Pierre Massat <pierre.massat@sentry.io>
2022-02-14 20:03:11 -0800

    Use test helpers instead of custom code

--- tests/sentry/api/endpoints/test_organization_stacktraces.py
+++ tests/sentry/api/endpoints/test_organization_stacktraces.py
@@ -1,23 +1,20 @@
 from django.urls import reverse
 
-from sentry.models import ApiToken
 from sentry.testutils import APITestCase
 
 
 class OrganizationStacktracesTest(APITestCase):
 
     endpoints = (
-        ("sentry-api-0-organization-stacktraces",),
-        ("sentry-api-0-organization-stacktrace-filters",),
+        "sentry-api-0-organization-stacktraces",
+        "sentry-api-0-organization-stacktrace-filters",
     )
 
-    def send_get_request(self, token, endpoint, *args):
-        url = reverse(endpoint, args=(self.project.organization.slug,) + args)
-        return self.client.get(url, HTTP_AUTHORIZATION=f"Bearer {token.token}", format="json")
+    def setUp(self):
+        self.login_as(user=self.user)
 
     def test_feature_flag_disabled(self):
-        token = ApiToken.objects.create(user=self.user, scope_list=["org:read"])
-
         for endpoint in self.endpoints:
-            response = self.send_get_request(token, *endpoint)
+            url = reverse(endpoint, args=(self.project.organization.slug,))
+            response = self.client.get(url)
             assert response.status_code == 404
--- tests/sentry/api/endpoints/test_project_stacktrace.py
+++ tests/sentry/api/endpoints/test_project_stacktrace.py
@@ -1,13 +1,11 @@
 from django.urls import reverse
 
-from sentry.models import ApiToken
 from sentry.testutils import APITestCase
 
 
 class ProjectStacktraceTest(APITestCase):
-    def test_feature_flag_disabled(self):
-        token = ApiToken.objects.create(user=self.user, scope_list=["project:read"])
-        url = reverse(
+    def setUp(self):
+        self.endpoint = reverse(
             "sentry-api-0-project-stacktrace",
             args=(
                 self.project.organization.slug,
@@ -15,5 +13,8 @@ class ProjectStacktraceTest(APITestCase):
                 "randomtransactionid",
             ),
         )
-        response = self.client.get(url, HTTP_AUTHORIZATION=f"Bearer {token.token}", format="json")
+        self.login_as(user=self.user)
+
+    def test_feature_flag_disabled(self):
+        response = self.get_response(self.project.organization.slug)
         assert response.status_code == 404
