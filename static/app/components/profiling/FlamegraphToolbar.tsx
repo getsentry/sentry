69258c4e39e47c99496250b858e67a67ca0a0820
JonasBa <jonas@badalic.com>
2022-02-16 12:39:27 +0100

    feat(flamegraphs): add thread selector
    feat(flamegraphs): add thread selector

--- static/app/components/profiling/FlamegraphToolbar.tsx
+++ static/app/components/profiling/FlamegraphToolbar.tsx
@@ -1,5 +1,6 @@
 import styled from '@emotion/styled';
 
+<<<<<<< HEAD
 interface FlamegraphToolbarProps {
   children: React.ReactNode;
 }
@@ -12,4 +13,16 @@ export const FlamegraphToolbar = styled('div')<FlamegraphToolbarProps>`
   > div {
     flex: 1;
   }
+=======
+function FlamegraphToolbar(props): React.ReactElement {
+  return <FlamegraphToolbarContainer>{props.children}</FlamegraphToolbarContainer>;
+}
+
+export {FlamegraphToolbar};
+
+const FlamegraphToolbarContainer = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+>>>>>>> aed5bd995c (feat(flamegraphs): add thread selector)
 `;
