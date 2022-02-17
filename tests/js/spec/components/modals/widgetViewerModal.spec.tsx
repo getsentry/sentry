b97031a50c11766cd81d748d34334f74a8c32e93
Edward Gou <edward.gou@sentry.io>
2022-02-17 14:28:33 -0500

    fix test

--- tests/js/spec/components/modals/widgetViewerModal.spec.tsx
+++ tests/js/spec/components/modals/widgetViewerModal.spec.tsx
@@ -114,7 +114,10 @@ describe('Modals -> WidgetViewerModal', function () {
     it('redirects user to Discover when clicking Open in Discover', async function () {
       const openInDiscoverButton = await screen.findByText('Open in Discover');
       expect(openInDiscoverButton).toBeInTheDocument();
-      expect(openInDiscoverButton).toHaveAttribute('href', 'test');
+      expect(openInDiscoverButton.closest('a')).toHaveAttribute(
+        'href',
+        '/organizations/org-slug/discover/results/?field=count%28%29&field=failure_count%28%29&name=Test%20Widget&query=title%3A%2Forganizations%2F%3AorgId%2Fperformance%2Fsummary%2F&statsPeriod=14d&yAxis=count%28%29&yAxis=failure_count%28%29'
+      );
     });
   });
 
@@ -181,7 +184,10 @@ describe('Modals -> WidgetViewerModal', function () {
     it('redirects user to Issues when clicking Open in Issues', async function () {
       const openInIssuesButton = await screen.findByText('Open in Issues');
       expect(openInIssuesButton).toBeInTheDocument();
-      expect(openInIssuesButton).toHaveAttribute('href', 'test');
+      expect(openInIssuesButton.closest('a')).toHaveAttribute(
+        'href',
+        '/organizations/org-slug/issues/?query=is%3Aunresolved&sort=&statsPeriod=14d'
+      );
     });
   });
 });
