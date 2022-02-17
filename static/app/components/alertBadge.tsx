5ff53d9221a635d2c6a63e9bae575864f83e28db
Robin Rendle <robinjrendle@gmail.com>
2022-02-17 11:35:23 -0800

    Update static/app/components/alertBadge.tsx
    Co-authored-by: Scott Cooper <scttcper@gmail.com>

--- static/app/components/alertBadge.tsx
+++ static/app/components/alertBadge.tsx
@@ -11,7 +11,7 @@ import {t} from 'sentry/locale';
 import space from 'sentry/styles/space';
 import {Color} from 'sentry/utils/theme';
 
-import {IncidentStatus} from '../views/alerts/types';
+import {IncidentStatus} from 'sentry/views/alerts/types';
 
 type Props = {
   hideText?: boolean;
