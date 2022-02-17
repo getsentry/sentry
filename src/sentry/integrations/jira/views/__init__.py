218f9a54def90e89153f1d3db9751c73434feb95
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-02-16 16:03:19 -0800

    rename views

--- src/sentry/integrations/jira/views/__init__.py
+++ src/sentry/integrations/jira/views/__init__.py
@@ -2,8 +2,8 @@ UNABLE_TO_VERIFY_INSTALLATION = "Unable to verify installation"
 
 from .base import JiraBaseHook
 from .extension_configuration import JiraExtensionConfigurationView
-from .issue import JiraIssueHookView
-from .ui import JiraUiHookView
+from .issue_hook import JiraIssueHookView
+from .ui_hook import JiraUiHookView
 
 __all__ = (
     "JiraBaseHook",
