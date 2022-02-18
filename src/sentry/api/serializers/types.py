7345b4e2512ecb8854886535a65eaf13375cb2c9
Josh Ferge <josh.ferge@sentry.io>
2022-02-17 20:44:38 -0800

    move SCIMMeta to types.py

--- src/sentry/api/serializers/types.py
+++ src/sentry/api/serializers/types.py
@@ -6,3 +6,7 @@ from typing_extensions import TypedDict
 class SerializedAvatarFields(TypedDict):
     avatarType: str
     avatarUuid: Optional[str]
+
+
+class SCIMMeta(TypedDict):
+    resourceType: str
