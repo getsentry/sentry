518ffe13481e2ad3109367f805e8bff0dc9f39ca
JonasBa <jonas@badalic.com>
2022-02-18 10:11:00 +0100

    fix(flamegraphtoolbar): export toolbar directly

--- static/app/components/profiling/FlamegraphToolbar.tsx
+++ static/app/components/profiling/FlamegraphToolbar.tsx
@@ -1,6 +1,5 @@
 import styled from '@emotion/styled';
 
-<<<<<<< HEAD
 interface FlamegraphToolbarProps {
   children: React.ReactNode;
 }
@@ -13,16 +12,4 @@ export const FlamegraphToolbar = styled('div')<FlamegraphToolbarProps>`
   > div {
     flex: 1;
   }
-=======
-function FlamegraphToolbar(props): React.ReactElement {
-  return <FlamegraphToolbarContainer>{props.children}</FlamegraphToolbarContainer>;
-}
-
-export {FlamegraphToolbar};
-
-const FlamegraphToolbarContainer = styled('div')`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
->>>>>>> aed5bd995c (feat(flamegraphs): add thread selector)
 `;
