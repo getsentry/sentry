08d102d4f30941c93c3b78768cc4971236102793
Matej Minar <matej.minar@sentry.io>
2022-02-18 12:42:05 +0100

    feat(dam): Add support for legend alias (#31902)

--- static/app/utils/metrics/transformMetricsResponseToSeries.tsx
+++ static/app/utils/metrics/transformMetricsResponseToSeries.tsx
@@ -2,12 +2,15 @@ import {MetricsApiResponse} from 'sentry/types';
 import {Series} from 'sentry/types/echarts';
 
 export function transformMetricsResponseToSeries(
-  response: MetricsApiResponse | null
+  response: MetricsApiResponse | null,
+  queryAlias?: string
 ): Series[] {
   return (
     response?.groups.flatMap(group =>
       Object.keys(group.series).map(field => ({
-        seriesName: `${field}${Object.entries(group.by)
+        seriesName: `${queryAlias ? `${queryAlias}: ` : ''}${field}${Object.entries(
+          group.by
+        )
           .map(([key, value]) => `|${key}:${value}`)
           .join('')}`,
         data: response.intervals.map((interval, index) => ({
