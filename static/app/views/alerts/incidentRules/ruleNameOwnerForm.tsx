8a8257d7dbe41274f9d19bd2256edc6e0d646c25
Taylan Gocmen <ta.gocmen@gmail.com>
2022-02-22 15:52:09 -0800

    make functional component

--- static/app/views/alerts/incidentRules/ruleNameOwnerForm.tsx
+++ static/app/views/alerts/incidentRules/ruleNameOwnerForm.tsx
@@ -1,4 +1,4 @@
-import {Fragment, PureComponent} from 'react';
+import {Fragment} from 'react';
 import * as React from 'react';
 import styled from '@emotion/styled';
 
@@ -17,79 +17,68 @@ type Props = {
   project: Project;
 };
 
-export default class RuleNameOwnerForm extends PureComponent<Props> {
-  renderRuleName() {
-    const {disabled} = this.props;
-    return (
-      <TextField
-        disabled={disabled}
-        name="name"
-        label={t('Rule Name')}
-        help={t('Add a name so it’s easy to find later.')}
-        placeholder={t('Something really bad happened')}
-        required
-      />
-    );
-  }
+export function RuleNameOwnerForm({disabled, project, hasAlertWizardV3}: Props) {
+  const renderRuleName = () => (
+    <TextField
+      disabled={disabled}
+      name="name"
+      label={t('Rule Name')}
+      help={t('Add a name so it’s easy to find later.')}
+      placeholder={t('Something really bad happened')}
+      required
+    />
+  );
 
-  renderTeamSelect() {
-    const {disabled, project} = this.props;
+  const renderTeamSelect = () => (
+    <FormField
+      name="owner"
+      label={t('Team')}
+      help={t('The team that can edit this alert.')}
+      disabled={disabled}
+    >
+      {({model}) => {
+        const owner = model.getValue('owner');
+        const ownerId = owner && owner.split(':')[1];
+        return (
+          <TeamSelector
+            value={ownerId}
+            project={project}
+            onChange={({value}) => {
+              const ownerValue = value && `team:${value}`;
+              model.setValue('owner', ownerValue);
+            }}
+            teamFilter={(team: Team) => team.isMember || team.id === ownerId}
+            useId
+            includeUnassigned
+            disabled={disabled}
+          />
+        );
+      }}
+    </FormField>
+  );
 
-    return (
-      <FormField
-        name="owner"
-        label={t('Team')}
-        help={t('The team that can edit this alert.')}
-        disabled={disabled}
-      >
-        {({model}) => {
-          const owner = model.getValue('owner');
-          const ownerId = owner && owner.split(':')[1];
-          return (
-            <TeamSelector
-              value={ownerId}
-              project={project}
-              onChange={({value}) => {
-                const ownerValue = value && `team:${value}`;
-                model.setValue('owner', ownerValue);
-              }}
-              teamFilter={(team: Team) => team.isMember || team.id === ownerId}
-              useId
-              includeUnassigned
-              disabled={disabled}
-            />
-          );
-        }}
-      </FormField>
-    );
-  }
-
-  render() {
-    const {hasAlertWizardV3} = this.props;
-
-    return hasAlertWizardV3 ? (
-      <Fragment>
-        <StyledListItem>{t('Add a name')}</StyledListItem>
-        <Panel>
-          <PanelBody>{this.renderRuleName()}</PanelBody>
-        </Panel>
-        <StyledListItem>{t('Assign this alert')}</StyledListItem>
-        <Panel>
-          <PanelBody>{this.renderTeamSelect()}</PanelBody>
-        </Panel>
-      </Fragment>
-    ) : (
-      <Fragment>
-        <StyledListItem>{t('Add a rule name and team')}</StyledListItem>
-        <Panel>
-          <PanelBody>
-            {this.renderRuleName()}
-            {this.renderTeamSelect()}
-          </PanelBody>
-        </Panel>
-      </Fragment>
-    );
-  }
+  return hasAlertWizardV3 ? (
+    <Fragment>
+      <StyledListItem>{t('Add a name')}</StyledListItem>
+      <Panel>
+        <PanelBody>{renderRuleName()}</PanelBody>
+      </Panel>
+      <StyledListItem>{t('Assign this alert')}</StyledListItem>
+      <Panel>
+        <PanelBody>{renderTeamSelect()}</PanelBody>
+      </Panel>
+    </Fragment>
+  ) : (
+    <Fragment>
+      <StyledListItem>{t('Add a rule name and team')}</StyledListItem>
+      <Panel>
+        <PanelBody>
+          {renderRuleName()}
+          {renderTeamSelect()}
+        </PanelBody>
+      </Panel>
+    </Fragment>
+  );
 }
 
 const StyledListItem = styled(ListItem)`
