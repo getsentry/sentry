3c9d08dda0dab3d65fe8ce1457860a6d6582e165
Josh Ferge <josh.ferge@sentry.io>
2022-02-22 14:48:28 -0800

    Revert "feat(apidocs): type team serializer response (#31916)"
    This reverts commit 2aeba0d213c1b78c77c2ce0da40d5ffad566f47a.

--- src/sentry/api/serializers/models/organization_member.py
+++ src/sentry/api/serializers/models/organization_member.py
@@ -7,7 +7,6 @@ from sentry import roles
 from sentry.api.serializers import Serializer, register, serialize
 from sentry.api.serializers.models.external_actor import ExternalActorResponse
 from sentry.api.serializers.models.user import UserSerializerResponse
-from sentry.api.serializers.types import SCIMMeta
 from sentry.models import (
     ExternalActor,
     OrganizationMember,
@@ -240,6 +239,10 @@ class SCIMEmail(TypedDict):
     primary: bool
     value: str
     type: str
+
+
+class SCIMMeta(TypedDict):
+    resourceType: str
 
 
 class OrganizationMemberSCIMSerializerOptional(TypedDict, total=False):
