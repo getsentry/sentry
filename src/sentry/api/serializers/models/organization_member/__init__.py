634eb7200feb4c22a2c3be657f625f2649d077c2
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-02-23 15:42:32 -0800

    fix imports

--- src/sentry/api/serializers/models/organization_member/__init__.py
+++ src/sentry/api/serializers/models/organization_member/__init__.py
@@ -1,6 +1,5 @@
 from .base import OrganizationMemberSerializer
 from .expand.projects import OrganizationMemberWithProjectsSerializer
-from .expand.roles import OrganizationMemberWithRolesSerializer
 from .expand.teams import OrganizationMemberWithTeamsSerializer
 from .response import SCIMMeta
 from .scim import OrganizationMemberSCIMSerializer, OrganizationMemberSCIMSerializerResponse
@@ -9,7 +8,6 @@ __all__ = (
     "OrganizationMemberSCIMSerializer",
     "OrganizationMemberSerializer",
     "OrganizationMemberWithProjectsSerializer",
-    "OrganizationMemberWithRolesSerializer",
     "OrganizationMemberWithTeamsSerializer",
     "OrganizationMemberSCIMSerializerResponse",
     "SCIMMeta",
