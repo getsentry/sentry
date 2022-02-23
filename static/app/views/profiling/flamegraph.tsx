f892f4cac7ef89152622d35a29328120ecfb641c
JonasBa <jonas@badalic.com>
2022-02-23 16:10:54 +0100

    ref(flamegraph): just remove empty flamegraph

--- static/app/views/profiling/flamegraph.tsx
+++ static/app/views/profiling/flamegraph.tsx
@@ -1,15 +1,10 @@
-import {Flamegraph} from 'sentry/components/profiling/Flamegraph';
 import {FullScreenFlamegraphContainer} from 'sentry/components/profiling/FullScreenFlamegraphContainer';
 import {FlamegraphThemeProvider} from 'sentry/utils/profiling/flamegraph/FlamegraphThemeProvider';
 
 function FlamegraphView() {
   return (
     <FlamegraphThemeProvider>
-      <FullScreenFlamegraphContainer>
-        {/*
-        // @ts-ignore */}
-        <Flamegraph profiles={} />
-      </FullScreenFlamegraphContainer>
+      <FullScreenFlamegraphContainer>{null}</FullScreenFlamegraphContainer>
     </FlamegraphThemeProvider>
   );
 }
