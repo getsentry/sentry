338b42d8c0be4a4fe3680cbf21fbc1e4dfd31223
Jonas <jonas@badalic.com>
2022-02-23 11:00:17 +0100

    feat(flamegraph): select a thread (#31865)
    * feat(flamegraphs): add thread selector
    
    feat(flamegraphs): add thread selector
    
    * fix(autocomplete): allow null children
    
    * fix(flamegraphtoolbar): export toolbar directly
    
    * Update static/app/components/profiling/ThreadSelector.tsx
    
    Co-authored-by: Tony Xiao <txiao@sentry.io>
    
    * style(lint): Auto commit lint changes
    
    Co-authored-by: Tony Xiao <txiao@sentry.io>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

---
+++ static/app/components/profiling/FlamegraphToolbar.tsx
@@ -0,0 +1,11 @@
+import styled from '@emotion/styled';
+
+interface FlamegraphToolbarProps {
+  children: React.ReactNode;
+}
+
+export const FlamegraphToolbar = styled('div')<FlamegraphToolbarProps>`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
