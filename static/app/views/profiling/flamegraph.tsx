50e6b6eabef1d4055927b4ad16a5c698ba88175e
JonasBa <jonas@badalic.com>
2022-02-23 14:38:26 +0100

    ref(flamegraph): remove organization

--- static/app/views/profiling/flamegraph.tsx
+++ static/app/views/profiling/flamegraph.tsx
@@ -1,13 +1,8 @@
 import {Flamegraph} from 'sentry/components/profiling/Flamegraph';
 import {FullScreenFlamegraphContainer} from 'sentry/components/profiling/FullScreenFlamegraphContainer';
 import {FlamegraphThemeProvider} from 'sentry/utils/profiling/flamegraph/FlamegraphThemeProvider';
-import useOrganization from 'sentry/utils/useOrganization';
 
 function FlamegraphView() {
-  // eslint-disable-next-line
-  const organization = useOrganization();
-  // @TODO fetch data from backend. We need to get trace.id from qs, org and projects here.
-
   return (
     <FlamegraphThemeProvider>
       <FullScreenFlamegraphContainer>
