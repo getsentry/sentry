40179013ed03a850cb0bb87d5f617d7f603b7842
JonasBa <jonas@badalic.com>
2022-02-24 10:09:54 +0100

    feat(flamegraph): add js self profiling example

--- docs-ui/stories/components/profiling/flamegraphZoomView.stories.js
+++ docs-ui/stories/components/profiling/flamegraphZoomView.stories.js
@@ -3,19 +3,47 @@ import {FullScreenFlamegraphContainer} from 'sentry/components/profiling/FullScr
 import {FlamegraphPreferencesProvider} from 'sentry/utils/profiling/flamegraph/FlamegraphPreferencesProvider';
 import {FlamegraphThemeProvider} from 'sentry/utils/profiling/flamegraph/FlamegraphThemeProvider';
 import {importProfile} from 'sentry/utils/profiling/profile/importProfile';
+import {JSSelfProfile} from 'sentry/utils/profiling/profile/jsSelfProfile.tsx';
+import {createFrameIndex} from 'sentry/utils/profiling/profile/utils';
 
 export default {
   title: 'Components/Profiling/FlamegraphZoomView',
 };
 
-const profiles = importProfile(require('./EventedTrace.json'));
+const eventedProfiles = importProfile(require('./EventedTrace.json'));
 
 export const EventedTrace = () => {
   return (
     <FlamegraphPreferencesProvider>
       <FlamegraphThemeProvider>
         <FullScreenFlamegraphContainer>
-          <Flamegraph profiles={profiles} />
+          <Flamegraph profiles={eventedProfiles} />
+        </FullScreenFlamegraphContainer>
+      </FlamegraphThemeProvider>
+    </FlamegraphPreferencesProvider>
+  );
+};
+
+const jsSelfProfile = require('./JSSelfProfilingTrace.json');
+
+export const JSSelfProfiling = () => {
+  const profiles = {
+    name: 'JS Self Profiling',
+    activeProfileIndex: 0,
+    traceID: '',
+    profiles: [
+      JSSelfProfile.FromProfile(
+        jsSelfProfile,
+        createFrameIndex(jsSelfProfile.frames, jsSelfProfile)
+      ),
+    ],
+  };
+
+  return (
+    <FlamegraphPreferencesProvider>
+      <FlamegraphThemeProvider>
+        <FullScreenFlamegraphContainer>
+          {profiles ? <Flamegraph profiles={profiles} /> : null}
         </FullScreenFlamegraphContainer>
       </FlamegraphThemeProvider>
     </FlamegraphPreferencesProvider>
