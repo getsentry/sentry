10a43348e7c04b6e3454b8afaf09a9479675c4c5
Dan Fuller <dfuller@sentry.io>
2022-02-25 10:24:56 -0800

    types: Add type hints to some database fields (#32025)
    There's a fair bit to do to get all the fields types, just did some of the work for now.

--- src/sentry/db/models/fields/bounded.py
+++ src/sentry/db/models/fields/bounded.py
@@ -1,5 +1,8 @@
+from typing import cast
+
 from django.conf import settings
 from django.db import models
+from django.db.backends.base.base import BaseDatabaseWrapper
 from django.utils.translation import ugettext_lazy as _
 
 __all__ = (
@@ -11,77 +14,77 @@ __all__ = (
 )
 
 
-class BoundedIntegerField(models.IntegerField):
+class BoundedIntegerField(models.IntegerField):  # type: ignore
     MAX_VALUE = 2147483647
 
-    def get_prep_value(self, value):
+    def get_prep_value(self, value: int) -> int:
         if value:
             value = int(value)
             assert value <= self.MAX_VALUE
-        return super().get_prep_value(value)
+        return cast(int, super().get_prep_value(value))
 
 
-class BoundedPositiveIntegerField(models.PositiveIntegerField):
+class BoundedPositiveIntegerField(models.PositiveIntegerField):  # type: ignore
     MAX_VALUE = 2147483647
 
-    def get_prep_value(self, value):
+    def get_prep_value(self, value: int) -> int:
         if value:
             value = int(value)
             assert value <= self.MAX_VALUE
-        return super().get_prep_value(value)
+        return cast(int, super().get_prep_value(value))
 
 
-class BoundedAutoField(models.AutoField):
+class BoundedAutoField(models.AutoField):  # type: ignore
     MAX_VALUE = 2147483647
 
-    def get_prep_value(self, value):
+    def get_prep_value(self, value: int) -> int:
         if value:
             value = int(value)
             assert value <= self.MAX_VALUE
-        return super().get_prep_value(value)
+        return cast(int, super().get_prep_value(value))
 
 
 if settings.SENTRY_USE_BIG_INTS:
 
-    class BoundedBigIntegerField(models.BigIntegerField):
+    class BoundedBigIntegerField(models.BigIntegerField):  # type: ignore
         description = _("Big Integer")
 
         MAX_VALUE = 9223372036854775807
 
-        def get_internal_type(self):
+        def get_internal_type(self) -> str:
             return "BigIntegerField"
 
-        def get_prep_value(self, value):
+        def get_prep_value(self, value: int) -> int:
             if value:
                 value = int(value)
                 assert value <= self.MAX_VALUE
-            return super().get_prep_value(value)
+            return cast(int, super().get_prep_value(value))
 
-    class BoundedBigAutoField(models.AutoField):
+    class BoundedBigAutoField(models.AutoField):  # type: ignore
         description = _("Big Integer")
 
         MAX_VALUE = 9223372036854775807
 
-        def db_type(self, connection):
+        def db_type(self, connection: BaseDatabaseWrapper) -> str:
             return "bigserial"
 
-        def get_related_db_type(self, connection):
-            return BoundedBigIntegerField().db_type(connection)
+        def get_related_db_type(self, connection: BaseDatabaseWrapper) -> str:
+            return cast(str, BoundedBigIntegerField().db_type(connection))
 
-        def get_internal_type(self):
+        def get_internal_type(self) -> str:
             return "BigIntegerField"
 
-        def get_prep_value(self, value):
+        def get_prep_value(self, value: int) -> int:
             if value:
                 value = int(value)
                 assert value <= self.MAX_VALUE
-            return super().get_prep_value(value)
+            return cast(int, super().get_prep_value(value))
 
 
 else:
     # we want full on classes for these
-    class BoundedBigIntegerField(BoundedIntegerField):
+    class BoundedBigIntegerField(BoundedIntegerField):  # type: ignore
         pass
 
-    class BoundedBigAutoField(BoundedAutoField):
+    class BoundedBigAutoField(BoundedAutoField):  # type: ignore
         pass
--- src/sentry/db/models/fields/onetoone.py
+++ src/sentry/db/models/fields/onetoone.py
@@ -3,8 +3,10 @@ from django.db.models import OneToOneField
 
 __all__ = ("OneToOneCascadeDeletes",)
 
+from sentry.utils.types import Any
 
-class OneToOneCascadeDeletes(OneToOneField):
-    def __init__(self, *args, **kwargs):
+
+class OneToOneCascadeDeletes(OneToOneField):  # type: ignore
+    def __init__(self, *args: Any, **kwargs: Any):
         kwargs.setdefault("on_delete", models.CASCADE)
         super().__init__(*args, **kwargs)
