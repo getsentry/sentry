7be8d5eeb23b8f481205fe6b1990e969b9ffd853
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-02-25 12:25:59 -0500

    remove unused action

--- static/app/actions/metricsMetaActions.tsx
+++ static/app/actions/metricsMetaActions.tsx
@@ -1,8 +1,5 @@
 import Reflux from 'reflux';
 
-const MetricsMetaActions = Reflux.createActions([
-  'loadMetricsMetaError',
-  'loadMetricsMetaSuccess',
-]);
+const MetricsMetaActions = Reflux.createActions(['loadMetricsMetaSuccess']);
 
 export default MetricsMetaActions;
