ca9470c37d8a7a3cadfeadf59875aefff5750cc8
Stephen Cefali <scefali@sentry.io>
2022-02-25 17:18:03 -0800

    adds falling man

--- static/app/views/onboarding/targetedOnboarding/welcome.tsx
+++ static/app/views/onboarding/targetedOnboarding/welcome.tsx
@@ -16,6 +16,7 @@ import {Organization} from 'sentry/types';
 import trackAdvancedAnalyticsEvent from 'sentry/utils/analytics/trackAdvancedAnalyticsEvent';
 import testableTransition from 'sentry/utils/testableTransition';
 import withOrganization from 'sentry/utils/withOrganization';
+import FallingError from 'sentry/views/onboarding/components/fallingError';
 
 import WelcomeBackground from './components/welcomeBackground';
 
@@ -61,95 +62,110 @@ function TargetedOnboardingWelcome({organization}: Props) {
     });
   });
   return (
-    <Wrapper>
-      <WelcomeBackground />
-      <motion.h1 {...fadeAway}>{t('Welcome to Sentry')}</motion.h1>
-      <SubHeaderText {...fadeAway}>
-        {t('Your code is probably broken. Maybe not.')}
-        <br />
-        {t('Find out for sure. Get started Below.')}
-      </SubHeaderText>
-      <ActionItem>
-        <InnerAction
-          title={t('Install Sentry')}
-          subText={t(
-            'Select your lanaguages or frameworks and install the SDKs to start tracking issues'
+    <FallingError>
+      {({fallingError}) => (
+        <Wrapper>
+          <WelcomeBackground />
+          <motion.h1 {...fadeAway}>{t('Welcome to Sentry')}</motion.h1>
+          <SubHeaderText {...fadeAway}>
+            {t('Your code is probably broken. Maybe not.')}
+            <br />
+            {t('Find out for sure. Get started below.')}
+          </SubHeaderText>
+          <ActionItem>
+            <InnerAction
+              title={t('Install Sentry')}
+              subText={t(
+                'Select your lanaguages or frameworks and install the SDKs to start tracking issues'
+              )}
+              src={OnboardingInstall}
+              cta={
+                <ButtonWithFill
+                  onClick={() => {
+                    trackAdvancedAnalyticsEvent(
+                      'growth.onboarding_clicked_instrument_app',
+                      {
+                        organization,
+                        source,
+                      }
+                    );
+                    window.location.replace(
+                      `/onboarding/${organization.slug}/select-platform/`
+                    );
+                  }}
+                  priority="primary"
+                >
+                  {t('Start')}
+                </ButtonWithFill>
+              }
+            />
+          </ActionItem>
+          <ActionItem>
+            <InnerAction
+              title={t('Setup my team')}
+              subText={tct(
+                'Invite [friends] coworkers. You shouldn’t have to fix what you didn’t break',
+                {friends: <Strike>{t('friends')}</Strike>}
+              )}
+              src={OnboardingSetup}
+              cta={
+                <ButtonWithFill
+                  onClick={() => {
+                    openInviteMembersModal({source});
+                  }}
+                  priority="primary"
+                >
+                  {t('Invite Team')}
+                </ButtonWithFill>
+              }
+            />
+          </ActionItem>
+          {!organization.features.includes('sandbox-kill-switch') && (
+            <ActionItem>
+              <InnerAction
+                title={t('Preview before you (git) commit')}
+                subText={t(
+                  'Check out sample issue reports, transactions, and tour all of Sentry '
+                )}
+                src={OnboardingPreview}
+                cta={
+                  <React.Fragment>
+                    <SandboxWithFill scenario="oneIssue" priority="primary" {...{source}}>
+                      {t('Explore')}
+                    </SandboxWithFill>
+                    <PositionedFallingError>{fallingError}</PositionedFallingError>
+                  </React.Fragment>
+                }
+              />
+            </ActionItem>
           )}
-          src={OnboardingInstall}
-          cta={
-            <ButtonWithFill
-              onClick={() => {
-                trackAdvancedAnalyticsEvent('growth.onboarding_clicked_instrument_app', {
+          <motion.p>
+            {t("Gee, I've used Sentry before.")}
+            <br />
+            <Link
+              onClick={() =>
+                trackAdvancedAnalyticsEvent('growth.onboarding_clicked_skip', {
                   organization,
                   source,
-                });
-                window.location.replace(
-                  `/onboarding/${organization.slug}/select-platform/`
-                );
-              }}
-              priority="primary"
+                })
+              }
+              to={`/organizations/${organization.slug}/issues/`}
             >
-              {t('Start')}
-            </ButtonWithFill>
-          }
-        />
-      </ActionItem>
-      <ActionItem>
-        <InnerAction
-          title={t('Setup my team')}
-          subText={tct(
-            'Invite [friends] coworkers. You shouldn’t have to fix what you didn’t break',
-            {friends: <Strike>{t('friends')}</Strike>}
-          )}
-          src={OnboardingSetup}
-          cta={
-            <ButtonWithFill
-              onClick={() => {
-                openInviteMembersModal({source});
-              }}
-              priority="primary"
-            >
-              {t('Invite Team')}
-            </ButtonWithFill>
-          }
-        />
-      </ActionItem>
-      {!organization.features.includes('sandbox-kill-switch') && (
-        <ActionItem>
-          <InnerAction
-            title={t('Preview before you (git) commit')}
-            subText={t(
-              'Check out sample issue reports, transactions, and tour all of Sentry '
-            )}
-            src={OnboardingPreview}
-            cta={
-              <SandboxWithFill scenario="oneIssue" priority="primary" {...{source}}>
-                {t('Explore')}
-              </SandboxWithFill>
-            }
-          />
-        </ActionItem>
+              {t('Skip onboarding.')}
+            </Link>
+          </motion.p>
+        </Wrapper>
       )}
-      <motion.p>
-        {t("Gee, I've used Sentry before.")}
-        <br />
-        <Link
-          onClick={() =>
-            trackAdvancedAnalyticsEvent('growth.onboarding_clicked_skip', {
-              organization,
-              source,
-            })
-          }
-          to={`/organizations/${organization.slug}/issues/`}
-        >
-          {t('Skip onboarding.')}
-        </Link>
-      </motion.p>
-    </Wrapper>
+    </FallingError>
   );
 }
 
 export default withOrganization(TargetedOnboardingWelcome);
+
+const PositionedFallingError = styled('span')`
+  display: block;
+  z-index: 0;
+`;
 
 const Wrapper = styled(motion.div)`
   max-width: 400px;
@@ -210,8 +226,7 @@ const SubHeaderText = styled(motion.h6)`
 const ButtonWrapper = styled('div')`
   margin: ${space(1)};
   display: flex;
-
-  @media (min-width: ${p => p.theme.breakpoints[0]}) {
+  flex-diretion: @media (min-width: ${p => p.theme.breakpoints[0]}) {
     justify-content: center;
     align-items: center;
   }
