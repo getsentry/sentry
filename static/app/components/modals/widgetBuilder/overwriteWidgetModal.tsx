491d3f5d3cefe7b20643c64e276583e73d08c2a6
Nar Saynorath <nar.saynorath@sentry.io>
2022-02-28 15:03:50 -0500

    Add more styling to the modal

--- static/app/components/modals/widgetBuilder/overwriteWidgetModal.tsx
+++ static/app/components/modals/widgetBuilder/overwriteWidgetModal.tsx
@@ -1,6 +1,12 @@
+import React from 'react';
 import {css} from '@emotion/react';
+import styled from '@emotion/styled';
 
 import {closeModal, ModalRenderProps} from 'sentry/actionCreators/modal';
+import Button from 'sentry/components/button';
+import ButtonBar from 'sentry/components/buttonBar';
+import {t} from 'sentry/locale';
+import space from 'sentry/styles/space';
 import {Card} from 'sentry/views/dashboardsV2/widgetBuilder/widgetLibrary/card';
 import {WidgetTemplate} from 'sentry/views/dashboardsV2/widgetLibrary/data';
 
@@ -12,27 +18,41 @@ export type OverwriteWidgetModalProps = {
 
 type Props = ModalRenderProps & OverwriteWidgetModalProps;
 
-export default function OverwriteWidgetModal({onConfirm, widget, iconColor}: Props) {
+const MODAL_DESCRIPTION = t(
+  "You've already started building this widget and will lose unsaved changes. Are you sure you want to overwrite this widget with the template values?"
+);
+
+export default function OverwriteWidgetModal({
+  Header,
+  Body,
+  Footer,
+  onConfirm,
+  widget,
+  iconColor,
+}: Props) {
+  function handleSubmit() {
+    onConfirm();
+    closeModal();
+  }
+
   return (
-    <div>
-      <div>Overwrite Widget</div>
-      <div>
-        You've already started building this widget. Are you sure you want to overwrite
-        this widget with the template values?
-      </div>
-      <Card widget={widget} iconColor={iconColor} disableHoverAnimation />
-      <footer>
-        <button onClick={closeModal}>Cancel</button>
-        <button
-          onClick={() => {
-            onConfirm();
-            closeModal();
-          }}
-        >
-          Confirm
-        </button>
-      </footer>
-    </div>
+    <React.Fragment>
+      <Header>
+        <h4>{t('Overwrite Widget')}</h4>
+      </Header>
+      <Body>
+        {MODAL_DESCRIPTION}
+        <Card widget={widget} iconColor={iconColor} disableHoverAnimation />
+      </Body>
+      <Footer>
+        <Actions>
+          <Button onClick={closeModal}>{t('Cancel')}</Button>
+          <Button priority="primary" onClick={handleSubmit}>
+            {t('Confirm')}
+          </Button>
+        </Actions>
+      </Footer>
+    </React.Fragment>
   );
 }
 
@@ -40,4 +60,8 @@ export const modalCss = css`
   width: 100%;
   max-width: 700px;
   margin: 70px auto;
+`;
+
+const Actions = styled(ButtonBar)`
+  gap: ${space(1.5)};
 `;
