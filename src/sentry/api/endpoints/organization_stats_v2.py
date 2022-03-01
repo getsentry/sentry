717e8df9cab855d79aa73a1615cb74c0595a098c
Josh Ferge <josh.ferge@sentry.io>
2022-03-01 15:00:26 -0800

    feat(orgstats): document v2 API (#32128)
    Co-authored-by: Alberto Leal <mail4alberto@gmail.com>

--- src/sentry/api/endpoints/organization_stats_v2.py
+++ src/sentry/api/endpoints/organization_stats_v2.py
@@ -1,14 +1,24 @@
 from contextlib import contextmanager
+from typing import Any, Dict, List
 
 import sentry_sdk
+from drf_spectacular.utils import OpenApiExample, extend_schema
+from rest_framework import serializers
 from rest_framework.exceptions import ParseError
 from rest_framework.request import Request
 from rest_framework.response import Response
+from typing_extensions import TypedDict
 
 from sentry.api.bases import NoProjects, OrganizationEventsEndpointBase
+from sentry.apidocs.constants import RESPONSE_NOTFOUND, RESPONSE_UNAUTHORIZED
+from sentry.apidocs.decorators import public
+from sentry.apidocs.parameters import GLOBAL_PARAMS
+from sentry.apidocs.utils import inline_sentry_response_serializer
 from sentry.constants import ALL_ACCESS_PROJECTS
 from sentry.search.utils import InvalidQuery
 from sentry.snuba.outcomes import (
+    COLUMN_MAP,
+    GROUPBY_MAP,
     QueryDefinition,
     massage_outcomes_result,
     run_outcomes_query_timeseries,
@@ -16,8 +26,100 @@ from sentry.snuba.outcomes import (
 )
 from sentry.snuba.sessions_v2 import InvalidField, InvalidParams
 from sentry.types.ratelimit import RateLimit, RateLimitCategory
+from sentry.utils.outcomes import Outcome
 
 
+class OrgStatsQueryParamsSerializer(serializers.Serializer):
+    # TODO: refactor endpoint and use this to validate query parameters.
+    # TODO: define fields for date parameters for use elsewhere
+    # time params
+    statsPeriod = serializers.CharField(
+        help_text=(
+            "This defines the range of the time series, relative to now. "
+            "The range is given in a `<number><unit>` format. "
+            "For example `1d` for a one day range. Possible units are `m` for minutes, `h` for hours, `d` for days and `w` for weeks."
+        ),
+        required=False,
+    )
+    interval = serializers.CharField(
+        help_text=(
+            "This is the resolution of the time series, given in the same format as `statsPeriod`. "
+            "The default resolution is `1h` and the minimum resolution is currently restricted to `1h` as well. "
+            "Intervals larger than `1d` are not supported, and the interval has to cleanly divide one day."
+        ),
+        required=False,
+    )
+    start = serializers.DateTimeField(
+        help_text="This defines the start of the time series range as an explicit datetime.",
+        required=False,
+    )
+    end = serializers.DateTimeField(
+        help_text="This defines the end of the time series range as an explicit datetime.",
+        required=False,
+    )
+
+    groupBy = serializers.MultipleChoiceField(
+        list(GROUPBY_MAP.keys()),
+        required=True,
+        help_text=(
+            "can pass multiple groupBy parameters to group by multiple, e.g. `groupBy=project&groupBy=outcome` to group by multiple dimensions. "
+            "Note that grouping by project can cause missing rows if the number of projects / interval is large. "
+            "If you have a large number of projects, we recommend filtering and querying by them individually."
+        ),
+    )
+
+    field = serializers.ChoiceField(
+        list(COLUMN_MAP.keys()),
+        help_text=(
+            "the `sum(quantity)` field is bytes for attachments, and all others the 'event' count for those types of events.\n\n"
+            "`sum(times_seen)` sums the number of times an event has been seen. "
+            "For 'normal' event types, this will be equal to `sum(quantity)` for now. "
+            "For sessions, quantity will sum the total number of events seen in a session, while `times_seen` will be the unique number of sessions. "
+            "and for attachments, `times_seen` will be the total number of attachments, while quantity will be the total sum of attachment bytes."
+        ),
+    )
+
+    # filter parameters
+
+    project = serializers.ListField(
+        required=False,
+        help_text="The ID of the projects to filter by.\n\nUse `-1` to include all accessible projects.",
+    )
+
+    category = serializers.ChoiceField(
+        ("error", "transaction", "attachment"),
+        required=False,
+        help_text=(
+            "If filtering by attachments, you cannot filter by any other category due to quantity values becoming non-sensical (combining bytes and event counts).\n\n"
+            "If filtering by `error`, it will automatically add `default` and `security` as we currently roll those two categories into `error` for displaying."
+        ),
+    )
+    outcome = serializers.ChoiceField(
+        [o.name.lower() for o in Outcome],
+        required=False,
+        help_text="See https://docs.sentry.io/product/stats/ for more information on outcome statuses.",
+    )
+
+    reason = serializers.CharField(
+        required=False, help_text="The reason field will contain why an event was filtered/dropped."
+    )
+
+
+class _StatsGroup(TypedDict):  # this response is pretty dynamic, leaving generic
+    by: Dict[str, Any]
+    totals: Dict[str, Any]
+    series: Dict[str, Any]
+
+
+class StatsApiResponse(TypedDict):
+    start: str
+    end: str
+    intervals: List[str]
+    groups: List[_StatsGroup]
+
+
+@public({"GET"})
+@extend_schema(tags=["Organizations"])
 class OrganizationStatsEndpointV2(OrganizationEventsEndpointBase):
     enforce_rate_limit = True
     rate_limits = {
@@ -28,7 +130,39 @@ class OrganizationStatsEndpointV2(OrganizationEventsEndpointBase):
         }
     }
 
+    @extend_schema(
+        operation_id="Retrieve Event Counts for an Organization (v2)",
+        parameters=[GLOBAL_PARAMS.ORG_SLUG, OrgStatsQueryParamsSerializer],
+        request=None,
+        responses={
+            200: inline_sentry_response_serializer("Outcomes Response", StatsApiResponse),
+            401: RESPONSE_UNAUTHORIZED,
+            404: RESPONSE_NOTFOUND,
+        },
+        examples=[  # TODO: see if this can go on serializer object instead
+            OpenApiExample(
+                "Successful response",
+                value={
+                    "start": "2022-02-14T19:00:00Z",
+                    "end": "2022-02-28T18:03:00Z",
+                    "intervals": ["2022-02-28T00:00:00Z"],
+                    "groups": [
+                        {
+                            "by": {"outcome": "invalid"},
+                            "totals": {"sum(quantity)": 165665},
+                            "series": {"sum(quantity)": [165665]},
+                        }
+                    ],
+                },
+                status_codes=["200"],
+            ),
+        ],
+    )
     def get(self, request: Request, organization) -> Response:
+        """
+        Query event counts for your Organization.
+        Select a field, define a date range, and group or filter by columns.
+        """
         with self.handle_query_errors():
             with sentry_sdk.start_span(op="outcomes.endpoint", description="build_outcomes_query"):
                 query = self.build_outcomes_query(
