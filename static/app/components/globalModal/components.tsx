c97e3da3b3c138f23d504824a7b8b3390ca5f147
JonasBa <jonas@badalic.com>
2022-03-02 12:17:21 +0100

    ref(tsc): use HTMLAttributes

--- static/app/components/globalModal/components.tsx
+++ static/app/components/globalModal/components.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import styled, {CreateStyledComponent} from '@emotion/styled';
+import styled from '@emotion/styled';
 
 import Button, {ButtonPropsWithAriaLabel} from 'sentry/components/button';
 import {IconClose} from 'sentry/icons/iconClose';
@@ -60,7 +60,7 @@ const ModalFooter = styled('footer')`
   margin: ${space(3)} -${space(4)} -${space(4)};
 `;
 
-interface ClosableHeaderProps extends CreateStyledComponent<HTMLHeadingElement> {
+interface ClosableHeaderProps extends React.HTMLAttributes<HTMLHeadingElement> {
   /**
    * Show a close button in the header
    */
