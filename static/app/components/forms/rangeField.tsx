d631017df7a30a063998b02f1f8c77d63b37decd
Jonas <jonas@badalic.com>
2022-03-03 12:37:11 +0100

    ref(tsc): refactor union (#32153)
    * ref(tsc): refactor union
    
    * ref(tsc): add disabled prop
    
    * ref(tsc): improve type narrowing and pass the correct props
    
    * fix(link): centering
    
    * fix(ci): empty commit to trigger CI
    
    * ref(tsc): fix unions on booleanfield (#32158)
    
    * ref(tsc): refactor union
    
    * ref(tsc): fix inputField
    
    * ref(tsc): extend the right prop
    
    * ref(tsc): fix the types
    
    * ref(tsc): fix other field types
    
    * ref(tsc): remove generic
    
    * fix(fields): more accurate types
    
    * fix(mobx): remove object type
    
    * fix(choicemapper): fix type
    
    * fix(formmodel): order of props matters
    
    * fix(fieldFromConfig): read the right prop and add back types
    
    * ref(tsc): fix radiogroup (#32159)
    
    * ref(tsc): fix radioGroup
    
    * ref(radiogroup): reexport prop
    
    * ref(tsc): fix union on modalheader (#32203)
    
    * ref(tsc): fix union
    
    * ref(tsc): use HTMLAttributes
    
    * ref(tsc): remove large union on searchbar (#32204)
    
    * fix(searchbar): remove large union
    
    * ref(tsc): fix union on activityitem (#32205)
    
    * fix(settingsindex): stop leaking attribute

--- static/app/components/forms/rangeField.tsx
+++ static/app/components/forms/rangeField.tsx
@@ -1,21 +1,36 @@
 import * as React from 'react';
 
 import RangeSlider from 'sentry/components/forms/controls/rangeSlider';
-import InputField, {onEvent} from 'sentry/components/forms/inputField';
+import InputField, {InputFieldProps, onEvent} from 'sentry/components/forms/inputField';
 
-type DefaultProps = {
+interface DefaultProps {
   formatMessageValue?: false | Function;
-};
+}
 
-type DisabledFunction = (props: Omit<Props, 'formatMessageValue'>) => boolean;
+type DisabledFunction = (props: Omit<RangeFieldProps, 'formatMessageValue'>) => boolean;
 type PlaceholderFunction = (props: any) => React.ReactNode;
 
-type Props = DefaultProps &
-  Omit<React.ComponentProps<typeof RangeSlider>, 'value' | 'disabled' | 'placeholder'> &
-  Omit<InputField['props'], 'disabled' | 'field'> & {
-    disabled?: boolean | DisabledFunction;
-    placeholder?: string | PlaceholderFunction;
-  };
+export interface RangeFieldProps
+  extends DefaultProps,
+    Omit<
+      React.ComponentProps<typeof RangeSlider>,
+      'value' | 'disabled' | 'placeholder' | 'css'
+    >,
+    Omit<
+      InputFieldProps,
+      | 'disabled'
+      | 'field'
+      | 'step'
+      | 'onChange'
+      | 'max'
+      | 'min'
+      | 'onBlur'
+      | 'css'
+      | 'formatMessageValue'
+    > {
+  disabled?: boolean | DisabledFunction;
+  placeholder?: string | PlaceholderFunction;
+}
 
 function onChange(
   fieldOnChange: onEvent,
@@ -25,7 +40,7 @@ function onChange(
   fieldOnChange(value, e);
 }
 
-function defaultFormatMessageValue(value, props: Props) {
+function defaultFormatMessageValue(value, props: RangeFieldProps) {
   return (typeof props.formatLabel === 'function' && props.formatLabel(value)) || value;
 }
 
@@ -33,11 +48,11 @@ export default function RangeField({
   formatMessageValue = defaultFormatMessageValue,
   disabled,
   ...otherProps
-}: Props) {
+}: RangeFieldProps) {
   const resolvedDisabled =
     typeof disabled === 'function' ? disabled(otherProps) : disabled;
 
-  const props: InputField['props'] = {
+  const props: InputFieldProps = {
     ...otherProps,
     disabled: resolvedDisabled,
     formatMessageValue,
