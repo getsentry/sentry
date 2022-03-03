76b69dfc119fb3e7020a76ed7d1ad1bd698cf7f8
Marcos Gaeta <31750075+mgaeta@users.noreply.github.com>
2022-03-02 16:09:52 -0800

    fix(slack): Silence Error (#32097)

--- src/sentry_plugins/slack/client.py
+++ src/sentry_plugins/slack/client.py
@@ -1,32 +1,20 @@
-from sentry.shared_integrations.exceptions import ApiError
-from sentry_plugins.client import ApiClient
+from typing import Any, Mapping
 
-IGNORABLE_SLACK_ERRORS = [
-    "channel_is_archived",
-    "invalid_channel",
-    "invalid_token",
-    "action_prohibited",
-]
+from sentry_plugins.client import ApiClient
 
 
 class SlackApiClient(ApiClient):
     plugin_name = "slack"
     allow_redirects = False
 
-    def __init__(self, webhook, username, icon_url, channel):
+    def __init__(self, webhook: str, username: str, icon_url: str, channel: str) -> None:
         self.webhook = webhook
         self.username = username
         self.icon_url = icon_url
         self.channel = channel
         super().__init__()
 
-    def request(self, data):
-        try:
-            return self._request(
-                path=self.webhook, method="post", data=data, json=False, allow_text=True
-            )
-        except ApiError as e:
-            # Ignore 404 and ignorable errors from slack webhooks
-            if e.text and e.text in IGNORABLE_SLACK_ERRORS or e.code == 404:
-                return
-            raise e
+    def request(self, data: Mapping[str, Any]):
+        return self._request(
+            path=self.webhook, method="post", data=data, json=False, allow_text=True
+        )
