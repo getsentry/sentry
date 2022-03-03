b42591ad53bb1e4f0a4dae0d7d43d01eefe6f95f
Ryan Skonnord <ryan.skonnord@sentry.io>
2022-03-03 12:27:57 -0800

    Extract common stuff into new RoleSet class

--- src/sentry/roles/__init__.py
+++ src/sentry/roles/__init__.py
@@ -6,6 +6,11 @@ default_manager = RoleManager(
     settings.SENTRY_ROLES, settings.SENTRY_TEAM_ROLES, settings.SENTRY_DEFAULT_ROLE
 )
 
+get_organization_roles = default_manager.get_organization_roles
+get_team_roles = default_manager.get_team_roles
+get_entry_role = default_manager.get_entry_role
+
+# Deprecated: Prefer calling get_organization_roles, then RoleSet methods
 can_manage = default_manager.can_manage
 get = default_manager.get
 get_all = default_manager.get_all
@@ -14,5 +19,3 @@ get_default = default_manager.get_default
 get_top_dog = default_manager.get_top_dog
 with_scope = default_manager.with_scope
 with_any_scope = default_manager.with_any_scope
-get_team_role = default_manager.get_team_role
-get_entry_role = default_manager.get_entry_role
--- src/sentry/roles/manager.py
+++ src/sentry/roles/manager.py
@@ -2,7 +2,7 @@ import abc
 import re
 from collections import OrderedDict
 from dataclasses import dataclass
-from typing import Dict, FrozenSet, Iterable, Mapping, Optional, Sequence, Tuple
+from typing import Dict, FrozenSet, Generic, Iterable, Mapping, Optional, Sequence, Tuple, TypeVar
 
 from sentry.utils import warnings
 
@@ -50,6 +50,53 @@ class TeamRole(Role):
     is_entry_role_for: Optional[str] = None
 
 
+R = TypeVar("R", bound=Role)
+
+
+class RoleSet(Generic[R]):
+    def __init__(self, roles: Iterable[R], default_id: Optional[str] = None) -> None:
+        self._priority_seq = tuple(sorted(roles, key=lambda r: r.priority))
+        self._id_map = OrderedDict((r.id, r) for r in self._priority_seq)
+
+        self._choices = tuple((r.id, r.name) for r in self._priority_seq)
+        self._default = self._id_map[default_id] if default_id else self._priority_seq[0]
+        self._top_dog = self._priority_seq[-1]
+
+    def __iter__(self) -> Iterable[R]:
+        yield from self._priority_seq
+
+    def can_manage(self, role: str, other: str) -> bool:
+        return self.get(role).priority >= self.get(other).priority
+
+    def get(self, id: str) -> R:
+        return self._id_map[id]
+
+    def get_if_exists(self, id: str) -> Optional[R]:
+        return self._id_map.get(id)
+
+    def get_all(self) -> Sequence[R]:
+        return self._priority_seq
+
+    def get_choices(self) -> Sequence[Tuple[str, str]]:
+        return self._choices
+
+    def get_default(self) -> R:
+        return self._default
+
+    def get_top_dog(self) -> R:
+        return self._top_dog
+
+    def with_scope(self, scope: str) -> Iterable[R]:
+        for role in self.get_all():
+            if role.has_scope(scope):
+                yield role
+
+    def with_any_scope(self, scopes: Iterable[str]) -> Iterable[R]:
+        for role in self.get_all():
+            if any(role.has_scope(scope) for scope in scopes):
+                yield role
+
+
 class RoleManager:
     def __init__(
         self,
@@ -57,35 +104,25 @@ class RoleManager:
         team_config: Iterable[Mapping[str, str]],
         default_org_role: Optional[str] = None,
     ) -> None:
-        self._org_roles: Dict[str, OrganizationRole] = OrderedDict()
-        for idx, role_cfg in enumerate(org_config):
-            org_role = OrganizationRole.from_config(idx, **role_cfg)
-            self._org_roles[org_role.id] = org_role
+        self._org_roles: RoleSet[OrganizationRole] = RoleSet(
+            (
+                OrganizationRole.from_config(idx, **role_cfg)
+                for idx, role_cfg in enumerate(org_config)
+            ),
+            default_org_role,
+        )
 
-        self._choices = tuple((r.id, r.name) for r in self._org_roles.values())
-
-        if default_org_role:
-            self._default = self._org_roles[default_org_role]
-        else:
-            self._default = next(iter(self._org_roles.values()))
-
-        self._top_dog = next(iter(reversed(self._org_roles.values())))
-
-        self._team_roles: Dict[str, TeamRole] = OrderedDict()
-        for idx, role_cfg in enumerate(team_config):
-            team_role = TeamRole.from_config(idx, **role_cfg)
-            self._team_roles[team_role.id] = team_role
+        self._team_roles: RoleSet[TeamRole] = RoleSet(
+            TeamRole.from_config(idx, **role_cfg) for idx, role_cfg in enumerate(team_config)
+        )
 
         self._entry_role_map = self._make_entry_role_map()
 
     def _make_entry_role_map(self) -> Dict[str, str]:
-        team_default = next(iter(self._team_roles.values()))
-        team_top_dog = next(iter(reversed(self._team_roles.values())))
-
         def get_mapped_org_role(team_role: TeamRole) -> Optional[OrganizationRole]:
             if team_role.is_entry_role_for is None:
                 return None
-            org_role = self._org_roles.get(team_role.is_entry_role_for)
+            org_role = self._org_roles.get_if_exists(team_role.is_entry_role_for)
             if org_role is None:
                 warnings.warn(
                     f"Broken role mapping: {team_role.id}.is_entry_role_for = {team_role.is_entry_role_for}"
@@ -93,52 +130,51 @@ class RoleManager:
             return org_role
 
         def get_highest_available_team_role(org_role: OrganizationRole) -> TeamRole:
-            if org_role is self._top_dog:
-                return team_top_dog
-            for team_role in reversed(self._team_roles.values()):
+            if org_role is self._org_roles.get_top_dog():
+                return self._team_roles.get_top_dog()
+            for team_role in reversed(self._team_roles.get_all()):
                 mapped_org_role = get_mapped_org_role(team_role)
                 if mapped_org_role and mapped_org_role.priority <= org_role.priority:
                     return team_role
-            return team_default
+            return self._team_roles.get_default()
 
         return OrderedDict(
             (org_role.id, get_highest_available_team_role(org_role).id)
-            for org_role in self._org_roles.values()
+            for org_role in self._org_roles.get_all()
         )
 
     def __iter__(self) -> Iterable[OrganizationRole]:
-        yield from self._org_roles.values()
+        yield from self._org_roles
 
     def can_manage(self, role: str, other: str) -> bool:
-        return self.get(role).priority >= self.get(other).priority
+        return self._org_roles.can_manage(role, other)
 
     def get(self, id: str) -> OrganizationRole:
-        return self._org_roles[id]
+        return self._org_roles.get(id)
 
     def get_all(self) -> Sequence[OrganizationRole]:
-        return list(self._org_roles.values())
+        return self._org_roles.get_all()
 
     def get_choices(self) -> Sequence[Tuple[str, str]]:
-        return self._choices
+        return self._org_roles.get_choices()
 
     def get_default(self) -> OrganizationRole:
-        return self._default
+        return self._org_roles.get_default()
 
     def get_top_dog(self) -> OrganizationRole:
-        return self._top_dog
+        return self._org_roles.get_top_dog()
 
     def with_scope(self, scope: str) -> Iterable[OrganizationRole]:
-        for role in self.get_all():
-            if role.has_scope(scope):
-                yield role
+        return self._org_roles.with_scope(scope)
 
     def with_any_scope(self, scopes: Iterable[str]) -> Iterable[OrganizationRole]:
-        for role in self.get_all():
-            if any(role.has_scope(scope) for scope in scopes):
-                yield role
+        return self._org_roles.with_any_scope(scopes)
 
-    def get_team_role(self, role: str) -> TeamRole:
-        return self._team_roles[role]
+    def get_organization_roles(self) -> RoleSet[OrganizationRole]:
+        return self._org_roles
+
+    def get_team_roles(self) -> RoleSet[TeamRole]:
+        return self._team_roles
 
     def get_entry_role(self, org_role: str) -> TeamRole:
-        return self.get_team_role(self._entry_role_map[org_role])
+        return self._team_roles.get(self._entry_role_map[org_role])
