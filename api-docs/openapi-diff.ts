8a5d623122a55180c87ccde1cf7cec608f46e6ae
nathan hsieh <hsieh.nathan@gmail.com>
2022-03-03 13:58:34 -0800

    update script to be TS

---
+++ api-docs/openapi-diff.ts
@@ -0,0 +1,38 @@
+/* global process */
+/* eslint import/no-nodejs-modules:0 */
+/* eslint import/no-unresolved:0 */
+
+const fs = require('fs');
+const yaml = require('js-yaml');
+const jsonDiff = require('json-diff');
+const https = require('https');
+
+async function main() {
+  const readFile = fs.readFileSync('tests/apidocs/openapi-derefed.json', 'utf8');
+  // console.log('READ FILE: ', readFile)
+  const request = new Promise((resolve, reject) =>
+    https.get(
+      `https://raw.githubusercontent.com/getsentry/sentry-api-schema/main/openapi-derefed.json`,
+      res => {
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', chunk => (rawData += chunk));
+        res.on('end', () => {
+          try {
+            resolve(JSON.parse(rawData));
+          } catch (e) {
+            reject(e.message);
+          }
+        });
+      }
+    )
+  );
+
+  const openApiData = await request;
+  const target = yaml.safeLoad(readFile);
+
+  // eslint-disable-next-line no-console
+  console.log('JSON DIFF: ', jsonDiff.diffString(openApiData, target));
+}
+
+main();
