672bd0d215483bdf5548eaa674f27ae04cf3fb87
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-03-03 15:01:59 +0100

    ref(new-widget-builder-experience): Port unit/integration tests - Part 3.6 (#32259)

--- tests/js/spec/utils/performance/suspectSpans/suspectSpansQuery.spec.tsx
+++ tests/js/spec/utils/performance/suspectSpans/suspectSpansQuery.spec.tsx
@@ -45,7 +45,7 @@ describe('SuspectSpansQuery', function () {
     expect(getMock).toHaveBeenCalledTimes(1);
   });
 
-  it('fetches data with the right op filter', async function () {
+  it('fetches data with the right op filter', function () {
     const getMock = MockApiClient.addMockResponse({
       url: '/organizations/test-org/events-spans-performance/',
       // just asserting that the data is being fetched, no need for actual data here
@@ -67,7 +67,7 @@ describe('SuspectSpansQuery', function () {
     expect(getMock).toHaveBeenCalledTimes(1);
   });
 
-  it('fetches data with the right group filter', async function () {
+  it('fetches data with the right group filter', function () {
     const getMock = MockApiClient.addMockResponse({
       url: '/organizations/test-org/events-spans-performance/',
       // just asserting that the data is being fetched, no need for actual data here
@@ -89,7 +89,7 @@ describe('SuspectSpansQuery', function () {
     expect(getMock).toHaveBeenCalledTimes(1);
   });
 
-  it('fetches data with the right per suspect param', async function () {
+  it('fetches data with the right per suspect param', function () {
     const getMock = MockApiClient.addMockResponse({
       url: '/organizations/test-org/events-spans-performance/',
       // just asserting that the data is being fetched, no need for actual data here
