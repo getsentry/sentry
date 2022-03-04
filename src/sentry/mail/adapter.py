b9428e8e8ff2d1f55132fd963cf57214bc77af8c
Dan Fuller <dfuller@sentry.io>
2022-03-04 11:27:13 -0800

    feat(issue_alert_status): Add issue alert status page links to email alerts (#32302)
    This adds links to the new issue alert status pages to both the digest and individual alert emails.

--- src/sentry/mail/adapter.py
+++ src/sentry/mail/adapter.py
@@ -2,6 +2,7 @@ import logging
 from typing import Any, Mapping, Optional, Sequence
 
 from sentry import digests
+from sentry.digests import Digest
 from sentry.digests import get_option_key as get_digest_option_key
 from sentry.digests.notifications import event_to_record, unsplit_key
 from sentry.models import NotificationSetting, Project, ProjectOption
@@ -103,7 +104,7 @@ class MailAdapter:
     @staticmethod
     def notify_digest(
         project: Project,
-        digest: Any,
+        digest: Digest,
         target_type: ActionTargetType,
         target_identifier: Optional[int] = None,
     ) -> None:
--- tests/sentry/templatetags/test_sentry_helpers.py
+++ tests/sentry/templatetags/test_sentry_helpers.py
@@ -84,3 +84,17 @@ def test_date_handle_date_and_datetime():
     )
 
     assert result == "\n".join(["2021-04-16", "2021-04-17"])
+
+
+@pytest.mark.parametrize(
+    "a_dict,key,expected",
+    (
+        ({}, "", ""),
+        ({}, "hi", ""),
+        ({"hello": 1}, "hello", "1"),
+    ),
+)
+def test_get_item(a_dict, key, expected):
+    prefix = '{% load sentry_helpers %} {{ something|get_item:"' + key + '" }}'
+    result = engines["django"].from_string(prefix).render(context={"something": a_dict}).strip()
+    assert result == expected
