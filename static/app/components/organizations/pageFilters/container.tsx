05760e1966cf78e641f67284ad03cb42ac04cc29
David Wang <dwang@sentry.io>
2022-03-04 11:40:28 -0800

    feat(page-filters): Add top level alert for desynced filters (#32141)
    * top level alert for when filters are desynced
    
    * state now inside alert
    
    * address comments
    
    * fix test

--- static/app/components/organizations/pageFilters/container.tsx
+++ static/app/components/organizations/pageFilters/container.tsx
@@ -9,6 +9,7 @@ import {
   updateEnvironments,
   updateProjects,
 } from 'sentry/actionCreators/pageFilters';
+import DesyncedFilterAlert from 'sentry/components/organizations/pageFilters/desyncedFiltersAlert';
 import ConfigStore from 'sentry/stores/configStore';
 import PageFiltersStore from 'sentry/stores/pageFiltersStore';
 import {useLegacyStore} from 'sentry/stores/useLegacyStore';
@@ -170,6 +171,7 @@ function Container({skipLoadLastUsed, children, ...props}: Props) {
   return (
     <Fragment>
       {!hideGlobalHeader && <GlobalSelectionHeader {...props} {...additionalProps} />}
+      {hideGlobalHeader && <DesyncedFilterAlert router={router} />}
       {children}
     </Fragment>
   );
