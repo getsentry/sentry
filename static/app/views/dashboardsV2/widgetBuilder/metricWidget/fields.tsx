5131329c701aeea5e648b192412819f24ccf0251
Matej Minar <matej.minar@sentry.io>
2022-03-04 10:24:05 +0100

    feat(dam): Widget fields based on /metrics/meta response [DD-628] (#32264)

--- static/app/views/dashboardsV2/widgetBuilder/metricWidget/fields.tsx
+++ static/app/views/dashboardsV2/widgetBuilder/metricWidget/fields.tsx
@@ -1,97 +1,76 @@
-import {MetricMeta, SelectValue} from 'sentry/types';
+import {MetricMeta, MetricsOperation, SelectValue} from 'sentry/types';
 import {defined} from 'sentry/utils';
-import {SessionMetric} from 'sentry/utils/metrics/fields';
+import {METRICS_OPERATIONS} from 'sentry/utils/metrics/fields';
 import {FieldValue, FieldValueKind} from 'sentry/views/eventsV2/table/types';
 
-export const DEFAULT_METRICS_FIELDS: MetricMeta[] = [
-  {
-    name: SessionMetric.SENTRY_SESSIONS_SESSION,
-    operations: ['sum'],
-    type: 'counter',
-  },
-];
-
-export const METRICS_AGGREGATIONS = {
-  count_unique: {
-    parameters: [
-      {
-        kind: 'column',
-        columnTypes: ['set'],
-        defaultValue: SessionMetric.SENTRY_SESSIONS_USER,
-        required: true,
-      },
-    ],
-    outputType: 'number',
-    isSortable: true,
-    multiPlotType: 'area',
-  },
-  sum: {
-    parameters: [
-      {
-        kind: 'column',
-        columnTypes: ['counter'],
-        required: true,
-        defaultValue: SessionMetric.SENTRY_SESSIONS_SESSION,
-      },
-    ],
-    outputType: 'number',
-    isSortable: true,
-    multiPlotType: 'area',
-  },
-};
-
 export function generateMetricsWidgetFieldOptions(
-  fields: MetricMeta[] = DEFAULT_METRICS_FIELDS,
+  fields: MetricMeta[] = [],
   tagKeys?: string[]
 ) {
-  const aggregations = METRICS_AGGREGATIONS;
-  const functions = Object.keys(aggregations);
-  fields.sort((x, y) => x.name.localeCompare(y.name));
   const fieldOptions: Record<string, SelectValue<FieldValue>> = {};
 
-  // Index items by prefixed keys as custom tags can overlap both fields and
-  // function names. Having a mapping makes finding the value objects easier
-  // later as well.
-  functions.forEach(func => {
-    const ellipsis = aggregations[func].parameters.length ? '\u2026' : '';
-    const parameters = aggregations[func].parameters;
+  const fieldNames: string[] = [];
+  const operations = new Set<MetricsOperation>();
+  const knownOperations = Object.keys(METRICS_OPERATIONS);
 
-    fieldOptions[`function:${func}`] = {
-      label: `${func}(${ellipsis})`,
-      value: {
-        kind: FieldValueKind.FUNCTION,
-        meta: {
-          name: func,
-          parameters,
-        },
-      },
-    };
-  });
+  fields
+    .sort((a, b) => a.name.localeCompare(b.name))
+    .forEach(field => {
+      field.operations.forEach(operation => operations.add(operation));
+      fieldNames.push(field.name);
 
-  fields.forEach(field => {
-    fieldOptions[`field:${field.name}`] = {
-      label: field.name,
-      value: {
-        kind: FieldValueKind.METRICS,
-        meta: {
-          name: field.name,
-          dataType: field.type,
-        },
-      },
-    };
-  });
-
-  if (defined(tagKeys)) {
-    tagKeys.sort();
-    tagKeys.forEach(tag => {
-      fieldOptions[`tag:${tag}`] = {
-        label: tag,
+      fieldOptions[`field:${field.name}`] = {
+        label: field.name,
         value: {
-          kind: FieldValueKind.TAG,
-          meta: {name: tag, dataType: 'string'},
+          kind: FieldValueKind.METRICS,
+          meta: {
+            name: field.name,
+            dataType: field.type,
+          },
         },
       };
     });
+
+  Array.from(operations)
+    .filter(operation => knownOperations.includes(operation))
+    .sort((a, b) => a.localeCompare(b))
+    .forEach(operation => {
+      const defaultField = METRICS_OPERATIONS[operation].defaultValue;
+
+      fieldOptions[`function:${operation}`] = {
+        label: `${operation}(${'\u2026'})`,
+        value: {
+          kind: FieldValueKind.FUNCTION,
+          meta: {
+            name: operation,
+            parameters: [
+              {
+                kind: 'column',
+                columnTypes: METRICS_OPERATIONS[operation].metricsTypes,
+                required: true,
+                defaultValue: fieldNames.includes(defaultField)
+                  ? defaultField
+                  : fields.find(field => field.operations.includes(operation))?.name ??
+                    '',
+              },
+            ],
+          },
+        },
+      };
+    });
+
+  if (defined(tagKeys)) {
+    tagKeys
+      .sort((a, b) => a.localeCompare(b))
+      .forEach(tag => {
+        fieldOptions[`tag:${tag}`] = {
+          label: tag,
+          value: {
+            kind: FieldValueKind.TAG,
+            meta: {name: tag, dataType: 'string'},
+          },
+        };
+      });
   }
 
   return fieldOptions;
