9e433a4889ee0210929fe534098e7ed4142218b3
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-03-04 11:38:29 +0100

    ref(widget-builder-new-experience): Put back some revert style code (#32309)

--- static/app/views/dashboardsV2/widgetBuilder/widgetBuilder.tsx
+++ static/app/views/dashboardsV2/widgetBuilder/widgetBuilder.tsx
@@ -1014,22 +1014,26 @@ const BuildSteps = styled(List)`
 `;
 
 const Body = styled(Layout.Body)`
-  grid-template-rows: 1fr;
   && {
     gap: 0;
     padding: 0;
   }
+
+  @media (max-width: ${p => p.theme.breakpoints[3]}) {
+    grid-template-columns: 1fr;
+  }
 `;
 
 const Main = styled(Layout.Main)`
+  max-width: 1000px;
   padding: ${space(4)};
 
   @media (min-width: ${p => p.theme.breakpoints[2]}) {
-    border-right: 1px solid ${p => p.theme.gray200};
+    border-left: 1px solid ${p => p.theme.gray200};
   }
 
   @media (max-width: ${p => p.theme.breakpoints[2]}) {
-    border-bottom: 1px solid ${p => p.theme.gray200};
+    border-top: 1px solid ${p => p.theme.gray200};
   }
 `;
 
