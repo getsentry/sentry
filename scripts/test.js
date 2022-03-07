28b0de8fa9b19af907fafd8aee17bca4d82e8c53
Billy Vong <billy@sentry.io>
2022-03-07 15:25:49 -0500

    feat(dev): Remove implicit jest watch mode when running `yarn test`
    A few reasons for this change:
    
    * The behavior of `yarn test` was a bit unclear, there were a few conditions where it would not run in "watch" mode (CI, pre-commit, exporting coverage stats)
    * We were having issues in "watch" mode due to `jest-sentry-environment`. Our wrapper script called `jest` via node API w/ "watch" mode as an arg, while `jest-sentry-environment` checked the global `process.argv` for "watch" mode. This is because the run options are not passed to the jest environment, and there is no other way to determine "watch" mode. (Note: this is fixed in v28 of jest, but it is currently in pre-release: https://github.com/facebook/jest/issues/12271).
    
    Now instead of running "watch" mode depending on the runtime environment, add an explicit task `test-watch` to run `jest` in "watch" mode.

--- scripts/test.js
+++ scripts/test.js
@@ -19,9 +19,4 @@ const jest = require('jest');
 
 const argv = process.argv.slice(2);
 
-// Watch unless on CI or in coverage mode
-if (!process.env.CI && !process.env.SENTRY_PRECOMMIT && argv.indexOf('--coverage') < 0) {
-  argv.push('--watch');
-}
-
 jest.run(argv);
