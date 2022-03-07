aea1bb7b4025646bf6edc52d06faa1fb0b9133e0
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-03-07 15:13:05 -0800

    Add types to src/sentry/rules/conditions/

--- src/sentry/rules/conditions/base.py
+++ src/sentry/rules/conditions/base.py
@@ -1,8 +1,9 @@
-from sentry.rules.base import RuleBase
+from sentry.eventstore.models import Event
+from sentry.rules.base import EventState, RuleBase
 
 
 class EventCondition(RuleBase):
     rule_type = "condition/event"
 
-    def passes(self, event, state):
+    def passes(self, event: Event, state: EventState) -> bool:
         raise NotImplementedError
--- src/sentry/rules/conditions/regression_event.py
+++ src/sentry/rules/conditions/regression_event.py
@@ -1,8 +1,10 @@
+from sentry.eventstore.models import Event
+from sentry.rules import EventState
 from sentry.rules.conditions.base import EventCondition
 
 
 class RegressionEventCondition(EventCondition):
     label = "The issue changes state from resolved to unresolved"
 
-    def passes(self, event, state):
+    def passes(self, event: Event, state: EventState) -> bool:
         return state.is_regression
