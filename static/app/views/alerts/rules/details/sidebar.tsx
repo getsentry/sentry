56ec6447d19c2527ba375aae593891579d0f13b3
Taylan Gocmen <ta.gocmen@gmail.com>
2022-03-08 17:09:55 -0800

    ref(ui): Update Alert Details sidebar triggers to look more like Alert Status sidebar conditions

--- static/app/views/alerts/rules/details/sidebar.tsx
+++ static/app/views/alerts/rules/details/sidebar.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import {Fragment, PureComponent, ReactNode} from 'react';
 import styled from '@emotion/styled';
 
 import AlertBadge from 'sentry/components/alertBadge';
@@ -21,6 +21,8 @@ import {
   AlertRuleTriggerType,
   IncidentRule,
 } from 'sentry/views/alerts/incidentRules/types';
+import {AlertWizardAlertNames} from 'sentry/views/alerts/wizard/options';
+import {getAlertTypeFromAggregateDataset} from 'sentry/views/alerts/wizard/utils';
 
 import {Incident, IncidentStatus} from '../../types';
 
@@ -29,8 +31,8 @@ type Props = {
   incidents?: Incident[];
 };
 
-export default class Sidebar extends React.Component<Props> {
-  getTimeWindow(): React.ReactNode {
+export default class Sidebar extends PureComponent<Props> {
+  getTimeWindow(): ReactNode {
     const {rule} = this.props;
 
     if (!rule) {
@@ -44,12 +46,8 @@ export default class Sidebar extends React.Component<Props> {
     });
   }
 
-  renderTrigger(label: string, threshold: number, actions: Action[]): React.ReactNode {
+  renderTrigger(label: string, threshold: number, actions: Action[]): ReactNode {
     const {rule} = this.props;
-
-    if (!rule) {
-      return null;
-    }
 
     const status =
       label === AlertRuleTriggerType.CRITICAL
@@ -57,17 +55,18 @@ export default class Sidebar extends React.Component<Props> {
         : label === AlertRuleTriggerType.WARNING
         ? t('Warning')
         : t('Resolved');
-    const statusIcon =
-      label === AlertRuleTriggerType.CRITICAL ? (
-        <StyledIconDiamond color="red300" size="md" />
-      ) : label === AlertRuleTriggerType.WARNING ? (
-        <StyledIconDiamond color="yellow300" size="md" />
-      ) : (
-        <StyledIconDiamond color="green300" size="md" />
-      );
+
+    const statusIconColor =
+      label === AlertRuleTriggerType.CRITICAL
+        ? 'red300'
+        : label === AlertRuleTriggerType.WARNING
+        ? 'yellow300'
+        : 'green300';
+
+    const defaultAction = t('Change alert status to %s', status);
 
     const thresholdTypeText = (
-      label === 'resolved'
+      label === AlertRuleTriggerType.RESOLVE
         ? rule.thresholdType === AlertRuleThresholdType.BELOW
         : rule.thresholdType === AlertRuleThresholdType.ABOVE
     )
@@ -80,8 +79,9 @@ export default class Sidebar extends React.Component<Props> {
 
     const thresholdText = rule.comparisonDelta
       ? tct(
-          'When [threshold]% [comparisonType] in [timeWindow] compared to [comparisonDelta]',
+          '[metric] is [threshold]% [comparisonType] in [timeWindow] compared to [comparisonDelta]',
           {
+            metric: AlertWizardAlertNames[getAlertTypeFromAggregateDataset(rule)],
             threshold,
             comparisonType: thresholdTypeText,
             timeWindow: this.getTimeWindow(),
@@ -92,23 +92,34 @@ export default class Sidebar extends React.Component<Props> {
             ).label,
           }
         )
-      : tct('If  [condition] in [timeWindow]', {
+      : tct('[metric] is [condition] in [timeWindow]', {
+          metric: AlertWizardAlertNames[getAlertTypeFromAggregateDataset(rule)],
           condition: `${thresholdTypeText} ${threshold}`,
           timeWindow: this.getTimeWindow(),
         });
 
     return (
-      <TriggerConditionContainer>
-        {statusIcon}
-        <TriggerCondition>
-          {status}
-          <TriggerText>{thresholdText}</TriggerText>
-          {actions.map(
-            action =>
-              action.desc && <TriggerText key={action.id}>{action.desc}</TriggerText>
-          )}
-        </TriggerCondition>
-      </TriggerConditionContainer>
+      <TriggerContainer>
+        <TriggerTitle>
+          <IconDiamond color={statusIconColor} size="xs" />
+          <TriggerTitleText>{t('%s Conditions', status)}</TriggerTitleText>
+        </TriggerTitle>
+        <TriggerStep>
+          <TriggerTitleText>When</TriggerTitleText>
+          <TriggerActions>
+            <TriggerText>{thresholdText}</TriggerText>
+          </TriggerActions>
+        </TriggerStep>
+        <TriggerStep>
+          <TriggerTitleText>Then</TriggerTitleText>
+          <TriggerActions>
+            {actions.map(action => (
+              <TriggerText key={action.id}>{action.desc}</TriggerText>
+            ))}
+            <TriggerText>{defaultAction}</TriggerText>
+          </TriggerActions>
+        </TriggerStep>
+      </TriggerContainer>
     );
   }
 
@@ -138,7 +149,7 @@ export default class Sidebar extends React.Component<Props> {
     const teamActor = ownerId && {type: 'team' as Actor['type'], id: ownerId, name: ''};
 
     return (
-      <React.Fragment>
+      <Fragment>
         <StatusContainer>
           <HeaderItem>
             <Heading noMargin>{t('Alert Status')}</Heading>
@@ -157,75 +168,68 @@ export default class Sidebar extends React.Component<Props> {
             </Status>
           </HeaderItem>
         </StatusContainer>
-        <React.Fragment>
-          <SidebarGroup>
-            <Heading>{t('Thresholds')}</Heading>
-            {typeof criticalTrigger?.alertThreshold === 'number' &&
-              this.renderTrigger(
-                criticalTrigger.label,
-                criticalTrigger.alertThreshold,
-                criticalTrigger.actions
-              )}
-            {typeof warningTrigger?.alertThreshold === 'number' &&
-              this.renderTrigger(
-                warningTrigger.label,
-                warningTrigger.alertThreshold,
-                warningTrigger.actions
-              )}
-            {typeof rule.resolveThreshold === 'number' &&
-              this.renderTrigger('resolved', rule.resolveThreshold, [])}
-          </SidebarGroup>
+        <SidebarGroup>
+          {typeof criticalTrigger?.alertThreshold === 'number' &&
+            this.renderTrigger(
+              criticalTrigger.label,
+              criticalTrigger.alertThreshold,
+              criticalTrigger.actions
+            )}
+          {typeof warningTrigger?.alertThreshold === 'number' &&
+            this.renderTrigger(
+              warningTrigger.label,
+              warningTrigger.alertThreshold,
+              warningTrigger.actions
+            )}
+          {typeof rule.resolveThreshold === 'number' &&
+            this.renderTrigger(AlertRuleTriggerType.RESOLVE, rule.resolveThreshold, [])}
+        </SidebarGroup>
 
-          <SidebarGroup>
-            <Heading>{t('Alert Rule Details')}</Heading>
-            <KeyValueTable>
-              <KeyValueTableRow
-                keyName={t('Date created')}
-                value={
-                  <DateTime
-                    date={getDynamicText({
-                      value: rule.dateCreated,
-                      fixed: new Date('2021-04-20'),
-                    })}
-                    format="ll"
-                  />
-                }
-              />
-
-              {rule.createdBy && (
-                <KeyValueTableRow
-                  keyName={t('Created By')}
-                  value={<CreatedBy>{rule.createdBy.name ?? '-'}</CreatedBy>}
+        <SidebarGroup>
+          <Heading>{t('Alert Rule Details')}</Heading>
+          <KeyValueTable>
+            <KeyValueTableRow
+              keyName={t('Date created')}
+              value={
+                <DateTime
+                  date={getDynamicText({
+                    value: rule.dateCreated,
+                    fixed: new Date('2021-04-20'),
+                  })}
+                  format="ll"
                 />
-              )}
+              }
+            />
 
-              {rule.dateModified && (
-                <KeyValueTableRow
-                  keyName={t('Last Modified')}
-                  value={<TimeSince date={rule.dateModified} suffix={t('ago')} />}
-                />
-              )}
-
+            {rule.createdBy && (
               <KeyValueTableRow
-                keyName={t('Team')}
-                value={
-                  teamActor ? (
-                    <ActorAvatar actor={teamActor} size={24} />
-                  ) : (
-                    t('Unassigned')
-                  )
-                }
+                keyName={t('Created By')}
+                value={<CreatedBy>{rule.createdBy.name ?? '-'}</CreatedBy>}
               />
-            </KeyValueTable>
-          </SidebarGroup>
-        </React.Fragment>
-      </React.Fragment>
+            )}
+
+            {rule.dateModified && (
+              <KeyValueTableRow
+                keyName={t('Last Modified')}
+                value={<TimeSince date={rule.dateModified} suffix={t('ago')} />}
+              />
+            )}
+
+            <KeyValueTableRow
+              keyName={t('Team')}
+              value={
+                teamActor ? <ActorAvatar actor={teamActor} size={24} /> : t('Unassigned')
+              }
+            />
+          </KeyValueTable>
+        </SidebarGroup>
+      </Fragment>
     );
   }
 }
 
 const SidebarGroup = styled('div')`
-  margin-bottom: ${space(4)};
+  margin-bottom: ${space(3)};
 `;
 
 const HeaderItem = styled('div')`
@@ -251,42 +255,58 @@ const Status = styled('div')`
 const StatusContainer = styled('div')`
   height: 60px;
   display: flex;
-  margin-bottom: ${space(1.5)};
+  margin-bottom: ${space(1)};
 `;
 
 const Heading = styled(SectionHeading)<{noMargin?: boolean}>`
-  display: grid;
-  grid-template-columns: auto auto;
-  justify-content: flex-start;
   margin-top: ${p => (p.noMargin ? 0 : space(2))};
-  margin-bottom: ${space(0.5)};
-  line-height: 1;
-  gap: ${space(1)};
-`;
-
-const TriggerConditionContainer = styled('div')`
-  display: flex;
-  flex-direction: row;
-`;
-
-const TriggerCondition = styled('div')`
-  display: flex;
-  flex-direction: column;
-  margin-left: ${space(0.75)};
-  line-height: 1.4;
-  position: relative;
-  top: 2px;
-`;
-
-const TriggerText = styled('div')`
-  color: ${p => p.theme.subText};
-  font-size: ${p => p.theme.fontSizeMedium};
+  margin-bottom: ${p => (p.noMargin ? 0 : space(1))};
 `;
 
 const CreatedBy = styled('div')`
   ${overflowEllipsis}
 `;
 
-const StyledIconDiamond = styled(IconDiamond)`
-  margin-top: ${space(0.5)};
+const TriggerContainer = styled('div')`
+  display: grid;
+  grid-template-rows: auto auto auto;
+  gap: ${space(1)};
+  margin-top: ${space(4)};
+`;
+
+const TriggerTitle = styled('div')`
+  display: grid;
+  grid-template-columns: 20px 1fr;
+  align-items: center;
+`;
+
+const TriggerTitleText = styled('h4')`
+  color: ${p => p.theme.subText};
+  font-size: ${p => p.theme.fontSizeMedium};
+  margin: 0;
+  line-height: 24px;
+  min-width: 40px;
+`;
+
+const TriggerStep = styled('div')`
+  display: grid;
+  grid-template-columns: 40px 1fr;
+  align-items: flex-start;
+`;
+
+const TriggerActions = styled('div')`
+  display: grid;
+  grid-template-columns: repeat(1fr);
+  gap: ${space(0.25)};
+`;
+
+const TriggerText = styled('span')`
+  display: block;
+  background-color: ${p => p.theme.surface100};
+  padding: 0 ${space(0.75)};
+  border-radius: ${p => p.theme.borderRadius};
+  color: ${p => p.theme.textColor};
+  font-size: ${p => p.theme.fontSizeSmall};
+  width: 100%;
+  font-weight: 400;
 `;
