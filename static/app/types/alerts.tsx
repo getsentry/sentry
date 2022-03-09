bd137d4d5a0773bb2ac1844105b555cbb624691a
Taylan Gocmen <taylangocmen@users.noreply.github.com>
2022-03-09 14:30:04 -0800

    feat(alertStatus): Use ruleFireHistory to show last triggered in alert status page (#32396)
    * expand rule with last triggerd
    
    * add last triggered test
    
    * use lastTriggered in group history
    
    * add lastTriggered to group history test

--- static/app/types/alerts.tsx
+++ static/app/types/alerts.tsx
@@ -85,6 +85,7 @@ export type IssueAlertRule = UnsavedIssueAlertRule & {
   id: string;
   projects: string[];
   errors?: {detail: string}[];
+  lastTriggered?: string;
 };
 
 // Project's alert rule stats
