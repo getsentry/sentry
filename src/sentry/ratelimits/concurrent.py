53f4c744f55fc79e36e2c608fc3641553e726fbb
Vlad Kluev <vlad.kluev@sentry.io>
2022-03-10 15:50:39 -0800

    make fail open, add tests

--- src/sentry/ratelimits/concurrent.py
+++ src/sentry/ratelimits/concurrent.py
@@ -12,10 +12,11 @@ logger = logging.getLogger(__name__)
 
 
 ErrorLimit = float("inf")
+DEFAULT_MAX_TTL_SECONDS = 30
 
 
 class ConcurrentRateLimiter:
-    def __init__(self, max_tll_seconds: int = 30) -> None:
+    def __init__(self, max_tll_seconds: int = DEFAULT_MAX_TTL_SECONDS) -> None:
         cluster_key = getattr(settings, "SENTRY_RATE_LIMIT_REDIS_CLUSTER", "default")
         self.client = redis.redis_clusters.get(cluster_key)
         self.max_ttl_seconds = max_tll_seconds
@@ -26,27 +27,38 @@ class ConcurrentRateLimiter:
         except Exception as e:
             raise InvalidConfiguration(str(e))
 
-    # TODO: maybe pass in the request object? But maybe we don't want to tie this to requests
+    # TODO: returning the amoutn of concurrent requests is not that nice of an interface, should
+    #   use a struct of some sort instead
     def start_request(self, key: str, limit: int, request_uid: str) -> int:
         # TODO: This should fail open
-        cur_time = time()
-        p = self.client.pipeline()
-        p.zremrangebyscore(key, "-inf", cur_time - self.max_ttl_seconds)
-
-        # TODO: do this using a redis lua script to keep things atomic
-        p.zcard(key)
-        currently_executing_calls = p.execute()[1]
-
-        if currently_executing_calls >= limit:
-            return limit
-        else:
+        try:
+            cur_time = time()
             p = self.client.pipeline()
-            p.zadd(key, {request_uid: cur_time})
+            p.zremrangebyscore(key, "-inf", cur_time - self.max_ttl_seconds)
+
+            # TODO: do this using a redis lua script to keep things atomic
             p.zcard(key)
-            return p.execute()[1]
+            currently_executing_calls = p.execute()[1]
+
+            if currently_executing_calls >= limit:
+                return limit
+            else:
+                p = self.client.pipeline()
+                p.zadd(key, {request_uid: cur_time})
+                p.zcard(key)
+                return p.execute()[1]
+        except Exception:
+            logger.exception(
+                "Could not start request", dict(key=key, limit=limit, request_uid=request_uid)
+            )
+            return 20000
 
     def get_concurrent_requests(self, key: str):
+        # this can fail loudly as it is only meant for observability
         return self.client.zcard(key)
 
     def finish_request(self, key: str, request_uid: str) -> None:
-        self.client.zrem(key, request_uid)
+        try:
+            self.client.zrem(key, request_uid)
+        except Exception:
+            logger.exception("Could not finish request", dict(key=key, request_uid=request_uid))
--- tests/sentry/ratelimits/test_redis_concurrent.py
+++ tests/sentry/ratelimits/test_redis_concurrent.py
@@ -1,6 +1,8 @@
+from datetime import datetime, timedelta
+
 from freezegun import freeze_time
 
-from sentry.ratelimits.concurrent import ConcurrentRateLimiter
+from sentry.ratelimits.concurrent import DEFAULT_MAX_TTL_SECONDS, ConcurrentRateLimiter
 from sentry.testutils import TestCase
 
 
@@ -20,3 +22,37 @@ class ConcurrentLimiterTest(TestCase):
                 assert self.backend.get_concurrent_requests("foo") == 8
             self.backend.finish_request("foo", "request_id1")
             assert self.backend.get_concurrent_requests("foo") == 7
+
+    def test_fails_open(self):
+        class FakeClient:
+            def __init__(self, real_client):
+                self._client = real_client
+
+            def __getattr__(self, name):
+                return getattr(self._client, name)
+
+            def pipeline(self):
+                return self
+
+            def execute(self):
+                # TODO: When the script changes to lua,
+                # make this fail differently
+                raise Exception("OH NO")
+
+        limiter = ConcurrentRateLimiter()
+        limiter.client = FakeClient(limiter.client)
+        limiter.start_request("key", 100, "some_uid")
+        limiter.finish_request("key", "some_uid")
+
+    def test_cleanup_stale(self):
+        limit = 10
+        num_stale = 5
+        request_date = datetime(2000, 1, 1)
+        with freeze_time(request_date):
+            for i in range(1, num_stale + 1):
+                assert self.backend.start_request("foo", limit, f"request_id{i}") == i
+            assert self.backend.get_concurrent_requests("foo") == num_stale
+        with freeze_time(request_date + timedelta(seconds=DEFAULT_MAX_TTL_SECONDS + 1)):
+            # the old requests did not finish however they are past their TTL and therefore
+            # are irrelevant
+            assert self.backend.start_request("foo", limit, "updated_request") == 1
