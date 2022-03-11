366fcefa588105fbacf9c4366b362aba6adf31b2
Vlad Kluev <vlad.kluev@sentry.io>
2022-03-11 13:57:53 -0800

    namespace the redis key

--- src/sentry/ratelimits/concurrent.py
+++ src/sentry/ratelimits/concurrent.py
@@ -35,39 +35,41 @@ class ConcurrentRateLimiter:
         except Exception as e:
             raise InvalidConfiguration(str(e))
 
-    # TODO: returning the amoutn of concurrent requests is not that nice of an interface, should
-    #   use a struct of some sort instead
+    def namespaced_key(self, key: str) -> str:
+        return f"concurrent_limit:{key}"
+
     def start_request(self, key: str, limit: int, request_uid: str) -> ConcurrentLimitInfo:
-        # TODO: This should fail open
+        redis_key = self.namespaced_key(key)
         try:
             cur_time = time()
             p = self.client.pipeline()
-            p.zremrangebyscore(key, "-inf", cur_time - self.max_ttl_seconds)
+            p.zremrangebyscore(redis_key, "-inf", cur_time - self.max_ttl_seconds)
 
             # TODO: do this using a redis lua script to keep things atomic
-            p.zcard(key)
+            p.zcard(redis_key)
             currently_executing_calls = p.execute()[1]
 
             if currently_executing_calls >= limit:
                 return ConcurrentLimitInfo(limit, currently_executing_calls, True)
             else:
                 p = self.client.pipeline()
-                p.zadd(key, {request_uid: cur_time})
-                p.zcard(key)
+                p.zadd(redis_key, {request_uid: cur_time})
+                p.zcard(redis_key)
                 current_executions = p.execute()[1]
                 return ConcurrentLimitInfo(limit, current_executions, False)
         except Exception:
             logger.exception(
-                "Could not start request", dict(key=key, limit=limit, request_uid=request_uid)
+                "Could not start request", dict(key=redis_key, limit=limit, request_uid=request_uid)
             )
             return ConcurrentLimitInfo(limit, -1, False)
 
     def get_concurrent_requests(self, key: str) -> int:
+        redis_key = self.namespaced_key(key)
         # this can fail loudly as it is only meant for observability
-        return self.client.zcard(key)
+        return self.client.zcard(redis_key)
 
     def finish_request(self, key: str, request_uid: str) -> None:
         try:
-            self.client.zrem(key, request_uid)
+            self.client.zrem(self.namespaced_key(key), request_uid)
         except Exception:
             logger.exception("Could not finish request", dict(key=key, request_uid=request_uid))
