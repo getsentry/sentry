getsentry/sentry

commit 4f5c640b07caafe665bd1719315b0823613157db

sentry_runner(feat): Report development issues (#24512)
This change enables error reporting issues when using `sentry`'s CLI runner.

.envrc
@@ -43,7 +43,8 @@ report_to_sentry() {
     if ! require sentry-cli; then
         curl -sL https://sentry.io/get-cli/ | bash
     fi
-    sentry-cli send-event -m "$error_message" --logfile "$_SENTRY_LOG_FILE" --level $log_level
+    SENTRY_DSN=${SENTRY_DEVENV_DSN} \
+        sentry-cli send-event -m "$error_message" --logfile "$_SENTRY_LOG_FILE" --level $log_level
     rm "$_SENTRY_LOG_FILE"
 }
 
@@ -125,12 +126,19 @@ export NODE_OPTIONS=--max-old-space-size=4096
 # Enable this by default for development envs (CI/deploys do not use envrc)
 export SENTRY_UI_HOT_RELOAD=1
 
+### You can override the exported variables with a .env file
+# All exports should happen before here unless they're safeguarded (see devenv error reporting below)
+if [ -f '.env' ]; then
+    info ".env found. Reading it..."
+    dotenv .env
+fi
+
 ## Notify of reporting to Sentry
 if [ -n "${SENTRY_DEVENV_NO_REPORT+x}" ]; then
     info "No development environment errors will be reported (since you've defined SENTRY_DEVENV_NO_REPORT)."
 else
     info "Development errors will be reported to Sentry.io."$'\n'"        If you wish to opt-out, set SENTRY_DEVENV_NO_REPORT as an env variable."
-    export SENTRY_DSN=https://23670f54c6254bfd9b7de106637808e9@o1.ingest.sentry.io/1492057
+    export SENTRY_DEVENV_DSN=https://23670f54c6254bfd9b7de106637808e9@o1.ingest.sentry.io/1492057
 fi
 
 ### System ###
@@ -216,11 +224,6 @@ fi
 PATH_add node_modules/.bin
 
 ### Overrides ###
-
-if [ -f '.env' ]; then
-    info ".env found. Reading it..."
-    dotenv .env
-fi
 
 cat <<EOF
 ${green}${bold}direnv: SUCCESS!

src/sentry/runner/__init__.py
@@ -1,8 +1,10 @@
+import logging
 import os
 import click
 import sys
 import sentry
 import datetime
+import sentry_sdk
 from sentry.utils.imports import import_string
 from sentry.utils.compat import map
 
@@ -161,4 +163,31 @@ def call_command(name, obj=None, **kwargs):
 
 
 def main():
-    cli(prog_name=get_prog(), obj={}, max_content_width=100)
+    func = cli
+    kwargs = {
+        "prog_name": get_prog(),
+        "obj": {},
+        "max_content_width": 100,
+    }
+    # This variable is *only* set as part of direnv/.envrc, thus, we cannot affect production
+    if os.environ.get("SENTRY_DEVENV_DSN"):
+        # We do this here because `configure_structlog` executes later
+        logging.basicConfig(format="%(levelname)s:%(message)s", level=logging.INFO)
+        logger = logging.getLogger(__name__)
+
+        logger.info(
+            "The Sentry runner will report development issues to Sentry.io. "
+            "Use SENTRY_DEVENV_NO_REPORT to avoid reporting issues."
+        )
+        try:
+            func(**kwargs)
+        except Exception as e:
+            # This reports to the project sentry-dev-env
+            with sentry_sdk.init(dsn=os.environ["SENTRY_DEVENV_DSN"]):
+                if os.environ.get("USER"):
+                    sentry_sdk.set_user({"username": os.environ.get("USER")})
+                sentry_sdk.capture_exception(e)
+                logger.info("We have reported the error below to Sentry")
+            raise e
+    else:
+        func(**kwargs)

commit c6fed5e8c355f8bb3f4636410046d5caef2c32a9

fix(dev): Errors within lib.sh were not being reported to Sentry
This was regressed in #24512

.envrc
@@ -58,9 +58,8 @@ report_to_sentry() {
     if ! require sentry-cli; then
         curl -sL https://sentry.io/get-cli/ | bash
     fi
-    # Report to sentry-dev-env
-    SENTRY_DSN="https://9bdb053cb8274ea69231834d1edeec4c@o1.ingest.sentry.io/5723503" \
-        sentry-cli send-event -m "$error_message" --logfile "$_SENTRY_LOG_FILE" --level $log_level
+    # Report to sentry-dev-env project
+    sentry-cli send-event -m "$error_message" --logfile "$_SENTRY_LOG_FILE" --level $log_level
     rm "$_SENTRY_LOG_FILE"
 }
 
@@ -143,6 +142,8 @@ fi
 if [ -n "${SENTRY_DEVENV_NO_REPORT+x}" ]; then
     debug "No development environment errors will be reported (since you've defined SENTRY_DEVENV_NO_REPORT)."
 else
+    # This is necessary for the bash-hook in lib.sh to work
+    export SENTRY_DSN="https://9bdb053cb8274ea69231834d1edeec4c@o1.ingest.sentry.io/5723503"
     # Since direnv traps the EXIT signal we place the temp file under /tmp for the odd time
     # the script will use the EXIT path
     _SENTRY_LOG_FILE=$(mktemp /tmp/sentry.envrc.$$.out || mktemp /tmp/sentry.envrc.XXXXXXXX.out)

scripts/lib.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# NOTE: This file is sourced in CI across different repos (e.g. snuba),
+# thus, renaming this file or any functions can break CI!
+#
 # Module containing code shared across various shell scripts
 # Execute functions from this module via the script do.sh
 # shellcheck disable=SC2034 # Unused variables
@@ -15,19 +18,13 @@ fi
 
 venv_name=".venv"
 
-# NOTE: This file is sourced in CI across different repos (e.g. snuba),
-# so renaming this file or any functions can break CI!
-
 # Check if a command is available
 require() {
     command -v "$1" >/dev/null 2>&1
 }
 
 configure-sentry-cli() {
-    # XXX: For version 1.70.1 there's a bug hitting SENTRY_CLI_NO_EXIT_TRAP: unbound variable
-    # We can remove this after it's fixed
-    # https://github.com/getsentry/sentry-cli/pull/1059
-    export SENTRY_CLI_NO_EXIT_TRAP=${SENTRY_CLI_NO_EXIT_TRAP-0}
+    # SENTRY_DSN is currently exported in .envrc
     if [ -n "${SENTRY_DSN+x}" ] && [ -z "${SENTRY_DEVENV_NO_REPORT+x}" ]; then
         if ! require sentry-cli; then
             curl -sL https://sentry.io/get-cli/ | bash