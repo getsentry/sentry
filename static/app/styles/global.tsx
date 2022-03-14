ee06ca52ac3b623d509db595630917985a49ff3c
Alberto Leal <mail4alberto@gmail.com>
2022-03-14 11:16:47 -0400

    chore(less): Move background and font colour of pre and code css definitions (#32238)

--- static/app/styles/global.tsx
+++ static/app/styles/global.tsx
@@ -32,6 +32,19 @@ const styles = (theme: Theme, isDark: boolean) => css`
     border-top-color: ${theme.border};
   }
 
+  pre,
+  code {
+    background-color: ${theme.backgroundSecondary};
+  }
+
+  pre {
+    color: ${theme.textColor};
+  }
+
+  code {
+    background-color: transparent;
+  }
+
   /**
    * See https://web.dev/prefers-reduced-motion/
    */
@@ -162,11 +175,6 @@ const styles = (theme: Theme, isDark: boolean) => css`
         .nav-header a.help-link,
         .nav-header span.help-link a {
           color: ${theme.subText};
-        }
-        pre,
-        code {
-          background-color: ${theme.backgroundSecondary};
-          color: ${theme.textColor};
         }
         .search .search-input {
           background: ${theme.background};
