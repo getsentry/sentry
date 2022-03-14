d456cc0929cb6b3ef84cb0ac2ac47cc1f6504e84
Tony Xiao <txiao@sentry.io>
2022-03-14 15:21:53 -0400

    does this fix it?

--- static/app/views/dashboardsV2/widgetBuilder/groupBySelector.tsx
+++ static/app/views/dashboardsV2/widgetBuilder/groupBySelector.tsx
@@ -97,9 +97,10 @@ export function GroupBySelector({fieldOptions, columns = [], onChange}: Props) {
                 borderless
                 onClick={() => handleRemove(index)}
                 icon={<IconDelete />}
-                title={t('Remove group')}
                 aria-label={t('Remove group')}
-              />
+              >
+                {t('Remove group')}
+              </Button>
             )}
           </QueryFieldWrapper>
         ))}
