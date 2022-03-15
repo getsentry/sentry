35e0d3bfbd07c4f33019bb7249bb2f6de38716e9
Vu Luong <vuluongj20@gmail.com>
2022-03-15 16:14:37 -0700

    ref(alert): Reduce padding-right is trailingItems is present

--- static/app/components/alert.tsx
+++ static/app/components/alert.tsx
@@ -39,6 +39,7 @@ const TrailingItems = styled('div')`
 const alertStyles = ({
   theme,
   type = DEFAULT_TYPE,
+  trailingItems,
   system,
   opaque,
   expand,
@@ -56,6 +57,8 @@ const alertStyles = ({
     background: ${opaque
       ? `linear-gradient(${alertColors.backgroundLight}, ${alertColors.backgroundLight}), linear-gradient(${theme.background}, ${theme.background})`
       : `${alertColors.backgroundLight}`};
+
+    ${defined(trailingItems) && `padding-right: ${space(1.5)};`}
 
     a:not([role='button']) {
       color: ${theme.textColor};
