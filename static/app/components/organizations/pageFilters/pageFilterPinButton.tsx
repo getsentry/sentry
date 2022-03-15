ca9a83e891a2baff1d950bf95a0a3ad6c8aa463c
Vu Luong <vuluongj20@gmail.com>
2022-03-15 11:58:54 -0700

    ref(page-filters): Use IconLock for pin feature

--- static/app/components/organizations/pageFilters/pageFilterPinButton.tsx
+++ static/app/components/organizations/pageFilters/pageFilterPinButton.tsx
@@ -2,7 +2,8 @@ import styled from '@emotion/styled';
 
 import {pinFilter} from 'sentry/actionCreators/pageFilters';
 import Button, {ButtonProps} from 'sentry/components/button';
-import {IconPin} from 'sentry/icons';
+import Tooltip from 'sentry/components/tooltip';
+import {IconLock} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import PageFiltersStore from 'sentry/stores/pageFiltersStore';
 import {useLegacyStore} from 'sentry/stores/useLegacyStore';
@@ -23,25 +24,30 @@ function PageFilterPinButton({filter, size, className}: Props) {
   };
 
   return (
-    <PinButton
-      className={className}
-      aria-pressed={pinned}
-      aria-label={t('Pin')}
-      onClick={onPin}
-      size={size}
-      borderless={size === 'zero'}
-      icon={<IconPin size="xs" isSolid={pinned} />}
-    />
+    <Tooltip title="Apply filter across all pages" delay={1000}>
+      <PinButton
+        className={className}
+        aria-pressed={pinned}
+        aria-label={t('Pin')}
+        onClick={onPin}
+        size={size}
+        pinned={pinned}
+        borderless={size === 'zero'}
+        icon={<IconLock isSolid={pinned} />}
+      />
+    </Tooltip>
   );
 }
 
-const PinButton = styled(Button)<{size: 'xsmall' | 'zero'}>`
+const PinButton = styled(Button)<{pinned: boolean; size: 'xsmall' | 'zero'}>`
   display: block;
-  color: ${p => p.theme.gray300};
+  color: ${p => p.theme.subText};
+
   :hover {
-    color: ${p => p.theme.subText};
+    color: ${p => p.theme.textColor};
   }
   ${p => p.size === 'zero' && 'background: transparent'};
+  ${p => p.pinned && `&& {color: ${p.theme.active}}`};
 `;
 
 export default PageFilterPinButton;
