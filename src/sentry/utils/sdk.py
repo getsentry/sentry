466588d1f85ec8a6a1df0739891ee13125f33365
Josh Ferge <josh.ferge@sentry.io>
2022-03-15 09:19:40 -0700

    feat(devenv): set sentry SDK default off if dsn not provided and relay off (#32569)
    Co-authored-by: Alberto Leal <mail4alberto@gmail.com>

--- src/sentry/utils/sdk.py
+++ src/sentry/utils/sdk.py
@@ -257,6 +257,8 @@ def configure_sdk():
     if relay_dsn:
         transport = make_transport(get_options(dsn=relay_dsn, **sdk_options))
         relay_transport = patch_transport_for_instrumentation(transport, "relay")
+    elif settings.IS_DEV and not settings.SENTRY_USE_RELAY:
+        relay_transport = None
     elif internal_project_key and internal_project_key.dsn_private:
         transport = make_transport(get_options(dsn=internal_project_key.dsn_private, **sdk_options))
         relay_transport = patch_transport_for_instrumentation(transport, "relay")
