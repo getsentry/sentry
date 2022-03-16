cee1266d477faaaa47a93524be45fcb5060e3850
Scott Cooper <scttcper@gmail.com>
2022-03-16 14:23:22 -0700

    fix(ui): Remove route re-mount when orgId is undefined (#32668)

--- static/app/views/organizationDetails/index.tsx
+++ static/app/views/organizationDetails/index.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import {RouteComponentProps} from 'react-router';
 
 import {switchOrganization} from 'sentry/actionCreators/organizations';
@@ -11,7 +11,19 @@ type Props = RouteComponentProps<{orgId: string}, {}> &
 
 function OrganizationDetails({children, ...props}: Props) {
   // Switch organizations when the orgId changes
-  useEffect(() => void switchOrganization(), [props.params.orgId]);
+  const orgId = useRef(props.params.orgId);
+  useEffect(() => {
+    if (props.params.orgId && orgId.current !== props.params.orgId) {
+      // Only switch on: org1 -> org2
+      // Not on: undefined -> org1
+      // Also avoid: org1 -> undefined -> org1
+      if (orgId.current) {
+        switchOrganization();
+      }
+
+      orgId.current = props.params.orgId;
+    }
+  }, [props.params.orgId]);
 
   return (
     <OrganizationContextContainer includeSidebar useLastOrganization {...props}>
