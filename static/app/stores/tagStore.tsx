04bc83041276a3c7794cbc66b487dd894600822c
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-03-17 10:51:26 +0100

    ref(new-widget-builder-experience): Split components feedback

--- static/app/stores/tagStore.tsx
+++ static/app/stores/tagStore.tsx
@@ -54,6 +54,7 @@ type TagStoreInterface = {
   getAllTags(): TagCollection;
   getBuiltInTags(): TagCollection;
   getIssueAttributes(): TagCollection;
+  getState: () => TagCollection;
   onLoadTagsSuccess(data: Tag[]): void;
   reset(): void;
   state: TagCollection;
@@ -157,6 +158,10 @@ const storeConfig: Reflux.StoreDefinition & TagStoreInterface = {
   },
 
   getAllTags() {
+    return this.state;
+  },
+
+  getState() {
     return this.state;
   },
 
