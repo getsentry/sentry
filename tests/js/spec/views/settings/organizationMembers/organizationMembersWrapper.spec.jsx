5319a3c40a92c776f0a0086da0da10fca01cf38b
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-03-17 09:35:32 -0700

    Update tests and test for when the button is intentionally missing

--- tests/js/spec/views/settings/organizationMembers/organizationMembersWrapper.spec.jsx
+++ tests/js/spec/views/settings/organizationMembers/organizationMembersWrapper.spec.jsx
@@ -13,6 +13,7 @@ jest.mock('sentry/actionCreators/modal', () => ({
 describe('OrganizationMembersWrapper', function () {
   const member = TestStubs.Member();
   const organization = TestStubs.Organization({
+    features: ['invite-members'],
     access: ['member:admin', 'org:admin', 'member:write'],
     status: {
       id: 'active',
@@ -65,8 +66,24 @@ describe('OrganizationMembersWrapper', function () {
     expect(openInviteMembersModal).toHaveBeenCalled();
   });
 
+  it('can not invite members without the invite-members feature', function () {
+    const org = TestStubs.Organization({
+      features: [],
+      access: ['member:admin', 'org:admin', 'member:write'],
+      status: {
+        id: 'active',
+      },
+    });
+    const wrapper = mountWithTheme(
+      <OrganizationMembersWrapper organization={org} {...defaultProps} />
+    );
+
+    expect(wrapper.find('StyledButton').exists()).toBeFalsy();
+  });
+
   it('can invite without permissions', function () {
     const org = TestStubs.Organization({
+      features: ['invite-members'],
       access: [],
       status: {
         id: 'active',
