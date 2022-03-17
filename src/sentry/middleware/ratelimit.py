6ed71824163f61d53e9122b01d10246a17ebd2b4
Colleen O'Rourke <colleen@sentry.io>
2022-03-17 14:54:00 -0700

    add types

--- src/sentry/middleware/ratelimit.py
+++ src/sentry/middleware/ratelimit.py
@@ -30,7 +30,7 @@ class RatelimitMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
-    def __call__(self, request):
+    def __call__(self, request: Request) -> Response:
         response = self.get_response(request)
         self.process_response(request, response)
         return response
