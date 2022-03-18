63f00e4aa70f781ca8af712c54352f0097b831ed
Ash Anand <ash.anand@sentry.io>
2022-03-18 18:29:03 -0400

    Remove conditional logic in SpanRectangle

--- static/app/components/events/interfaces/spans/spanRectangle.tsx
+++ static/app/components/events/interfaces/spans/spanRectangle.tsx
@@ -23,30 +23,11 @@ export default function SpanRectangle({
   index: number;
   spanGrouping: EnhancedSpan[];
 }) {
-  // If an index is provided, we treat this as an individual block for a single span in the grouping
-  if (index !== undefined) {
-    const grouping = [spanGrouping[index]];
-    const bounds = getSpanGroupBounds(grouping, generateBounds);
+  const grouping = [spanGrouping[index]];
+  const bounds = getSpanGroupBounds(grouping, generateBounds);
 
-    // If this is not the last span in the grouping, the duration does not need to be rendered
-    if (index !== spanGrouping.length - 1) {
-      return (
-        <RowRectangle
-          spanBarHatch={false}
-          style={{
-            backgroundColor: theme.blue300,
-            left: `min(${toPercent(bounds.left || 0)}, calc(100% - 1px))`,
-            width: toPercent(bounds.width || 0),
-          }}
-        />
-      );
-    }
-
-    const {startTimestamp, endTimestamp} = getSpanGroupTimestamps(spanGrouping);
-    const duration = Math.abs(endTimestamp - startTimestamp);
-    const durationDisplay = getDurationDisplay(bounds);
-    const durationString = getHumanDuration(duration);
-
+  // If this is not the last span in the grouping, the duration does not need to be rendered
+  if (index !== spanGrouping.length - 1) {
     return (
       <RowRectangle
         spanBarHatch={false}
@@ -55,24 +36,15 @@ export default function SpanRectangle({
           left: `min(${toPercent(bounds.left || 0)}, calc(100% - 1px))`,
           width: toPercent(bounds.width || 0),
         }}
-      >
-        {index === spanGrouping.length - 1 && (
-          <DurationPill
-            durationDisplay={durationDisplay}
-            showDetail={false}
-            spanBarHatch={false}
-          >
-            {durationString}
-          </DurationPill>
-        )}
-      </RowRectangle>
+      />
     );
   }
-  const bounds = getSpanGroupBounds(spanGrouping, generateBounds);
-  const durationDisplay = getDurationDisplay(bounds);
+
   const {startTimestamp, endTimestamp} = getSpanGroupTimestamps(spanGrouping);
   const duration = Math.abs(endTimestamp - startTimestamp);
+  const durationDisplay = getDurationDisplay(bounds);
   const durationString = getHumanDuration(duration);
+
   return (
     <RowRectangle
       spanBarHatch={false}
@@ -82,7 +54,7 @@ export default function SpanRectangle({
         width: toPercent(bounds.width || 0),
       }}
     >
-      {
+      {index === spanGrouping.length - 1 && (
         <DurationPill
           durationDisplay={durationDisplay}
           showDetail={false}
@@ -90,7 +62,7 @@ export default function SpanRectangle({
         >
           {durationString}
         </DurationPill>
-      }
+      )}
     </RowRectangle>
   );
 }
