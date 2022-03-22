06838a70db693b1e42a6acffddb2b88d5538a8c0
Stephen Cefali <scefali@sentry.io>
2022-03-22 12:55:08 -0700

    adds tests

---
+++ tests/js/spec/views/onboarding/targetedOnboarding/onboarding.spec.jsx
@@ -0,0 +1,55 @@
+import {initializeOrg} from 'sentry-test/initializeOrg';
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import ProjectsStore from 'sentry/stores/projectsStore';
+import Onboarding from 'sentry/views/onboarding/targetedOnboarding/onboarding';
+import {OrganizationContext} from 'sentry/views/organizationContext';
+
+describe('Onboarding', function () {
+  it('renders the welcome page', function () {
+    const {organization, router, routerContext} = initializeOrg({
+      router: {
+        params: {
+          step: 'welcome',
+        },
+      },
+    });
+    const {container} = render(
+      <OrganizationContext.Provider value={organization}>
+        <Onboarding {...router} />
+      </OrganizationContext.Provider>,
+      {
+        context: routerContext,
+      }
+    );
+    expect(container).toSnapshot();
+    expect(screen.getByLabelText('Start')).toBeInTheDocument();
+    expect(screen.getByLabelText('Invite Team')).toBeInTheDocument();
+    expect(screen.getByLabelText('Explore')).toBeInTheDocument();
+  });
+});
+it('renders the setup docs step', function () {
+  const projects = [
+    TestStubs.Project({platform: 'javascript-nextjs', id: '4'}),
+    TestStubs.Project({platform: 'ruby', id: '5'}),
+  ];
+  const {organization, router, routerContext} = initializeOrg({
+    projects,
+    router: {
+      params: {
+        step: 'setup-docs',
+      },
+    },
+  });
+  ProjectsStore.loadInitialData(projects);
+  const {container} = render(
+    <OrganizationContext.Provider value={organization}>
+      <Onboarding {...router} />
+    </OrganizationContext.Provider>,
+    {
+      context: routerContext,
+    }
+  );
+  expect(container).toSnapshot();
+  expect(screen.getAllByText('Waiting for error')).toHaveLength(2);
+});
