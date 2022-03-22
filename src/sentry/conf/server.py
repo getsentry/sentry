935e9d7aedfb62a59a4218e04dec92478e311af9
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-03-22 14:03:18 -0700

    String together Hovercard and FeatureDisabled

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1048,7 +1048,7 @@ SENTRY_FEATURES = {
     # Enable the API to importing CODEOWNERS for a project
     "organizations:integrations-codeowners": False,
     # Enable inviting members to organizations.
-    "organizations:invite-members": False,
+    "organizations:invite-members": True,
     # Enable rate limits for inviting members.
     "organizations:invite-members-rate-limits": True,
     # Enable removing issue from issue list if action taken.
--- static/app/views/settings/organizationMembers/components/inviteMembersButton.tsx
+++ static/app/views/settings/organizationMembers/components/inviteMembersButton.tsx
@@ -1,14 +1,10 @@
 import React from 'react';
-import styled from '@emotion/styled';
 
+import FeatureDisabled from 'sentry/components/acl/featureDisabled';
 import Button from 'sentry/components/button';
-import Clipboard from 'sentry/components/clipboard';
-import ExternalLink from 'sentry/components/links/externalLink';
-import Tooltip from 'sentry/components/tooltip';
-import {CONFIG_DOCS_URL} from 'sentry/constants';
-import {IconCopy, IconMail} from 'sentry/icons';
-import {t, tct} from 'sentry/locale';
-import space from 'sentry/styles/space';
+import {Hovercard} from 'sentry/components/hovercard';
+import {IconMail} from 'sentry/icons';
+import {t} from 'sentry/locale';
 
 type InviteMembersButtonProps = {
   onClick: () => void;
@@ -30,64 +26,20 @@ function InviteMembersButton({disabled, onClick}: InviteMembersButtonProps) {
   );
 
   return disabled ? (
-    <DisabledInviteMembersTooltip>{action}</DisabledInviteMembersTooltip>
+    <Hovercard
+      position="left"
+      body={
+        <FeatureDisabled
+          features={['organizations:invite-members']}
+          featureName="Invite Members"
+        />
+      }
+    >
+      {action}
+    </Hovercard>
   ) : (
     action
   );
 }
-
-const installText = (features: string[], featureName: string): string =>
-  `# ${t('Enables the %s feature', featureName)}\n${features
-    .map(f => `SENTRY_FEATURES['${f}'] = True`)
-    .join('\n')}`;
-
-type DisabledInviteMembersTooltipProps = {
-  children: React.ReactNode;
-};
-
-function DisabledInviteMembersTooltip({children}: DisabledInviteMembersTooltipProps) {
-  const title = tct(
-    `Enable this feature on your sentry installation by adding the
-    following configuration into your [configFile:sentry.conf.py].
-    See [configLink:the configuration documentation] for more
-    details.`,
-    {
-      configFile: <code />,
-      configLink: <ExternalLink href={CONFIG_DOCS_URL} />,
-    }
-  );
-
-  return (
-    <Tooltip title={title} isHoverable>
-      <TooltipClipboardWrapper>
-        {children}
-
-        <Clipboard
-          value={installText(['organizations:invite-members'], 'Invite Members')}
-        >
-          <TooltipClipboardIconWrapper>
-            <IconCopy size="xs" aria-label={t('Copy to clipboard')} />
-          </TooltipClipboardIconWrapper>
-        </Clipboard>
-      </TooltipClipboardWrapper>
-    </Tooltip>
-  );
-}
-
-const TooltipClipboardWrapper = styled('div')`
-  display: grid;
-  grid-template-columns: auto max-content;
-  align-items: center;
-  gap: ${space(0.5)};
-`;
-
-const TooltipClipboardIconWrapper = styled('div')`
-  pointer-events: auto;
-  position: relative;
-  bottom: -${space(0.25)};
-  :hover {
-    cursor: pointer;
-  }
-`;
 
 export default InviteMembersButton;
