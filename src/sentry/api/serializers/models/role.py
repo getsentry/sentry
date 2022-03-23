ff532eb2b46d813126e8ee2571bcbd064fbd5c8f
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-03-23 14:15:58 -0700

    fix typeddict

--- src/sentry/api/serializers/models/role.py
+++ src/sentry/api/serializers/models/role.py
@@ -1,4 +1,6 @@
-from typing import Any, List, Mapping, TypedDict
+from typing import Any, List, Mapping
+
+from typing_extensions import TypedDict
 
 from sentry.api.serializers import Serializer
 from sentry.models import User
