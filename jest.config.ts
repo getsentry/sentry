b3b98b991293ed374fd13b6796abda1af5d44982
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-03-24 08:57:53 -0400

    only run alerts

--- jest.config.ts
+++ jest.config.ts
@@ -163,7 +163,7 @@ const config: Config.InitialOptions = {
     '<rootDir>/tests/js/setupFramework.ts',
     '@testing-library/jest-dom/extend-expect',
   ],
-  testMatch: testMatch || ['<rootDir>/tests/js/**/*(*.)@(spec|test).(js|ts)?(x)'],
+  testMatch: ['<rootDir>/tests/js/spec/views/alerts/create.spec.jsx'] || testMatch,
   testPathIgnorePatterns: ['<rootDir>/tests/sentry/lang/javascript/'],
 
   unmockedModulePathPatterns: [
