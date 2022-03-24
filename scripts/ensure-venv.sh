80688fc94988cf17327c610e314818981f84ff36
Armen Zambrano G <armenzg@sentry.io>
2022-03-24 12:36:40 -0400

    fix(dev): New Python virtualenvs to install Python 3.8.13 (#32883)
    This version bump is only for new development environments.
    
    If you run Monterrey and need a Python version via pyenv, the lowest version you can get is 3.8.13.
    
    Existing Python venvs do not need to be upgraded.
    
    See https://github.com/pyenv/pyenv/issues/2284 for details.

--- scripts/ensure-venv.sh
+++ scripts/ensure-venv.sh
@@ -20,9 +20,6 @@ EOF
 }
 
 if [[ -n "$VIRTUAL_ENV" ]]; then
-    # The developer is inside a virtualenv *and* has set a SENTRY_PYTHON_VERSION
-    # Let's assume that they know what they're doing
-
     # Let's make sure they know that they're not using a different version by mistake
     query-valid-python-version || exit 1
 else
@@ -31,6 +28,12 @@ else
     fi
     die "You have a virtualenv, but it doesn't seem to be activated. Please run: source ${venv_name}/bin/activate"
 fi
+
+
+if [ -L "${venv_name}/bin/python3" ] && [ ! -e "${venv_name}/bin/python3" ]; then
+    die "Your symlink ${venv_name}/bin/python3 seems to be pointing to a non-existing Python binary. Try: rm -rf ${venv_name} && direnv allow"
+fi
+
 
 # Somehow it does not succeed unless I exit with 0
 exit 0
