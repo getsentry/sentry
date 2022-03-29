e786190016510cc95ba839d6730eb43d1db1612b
Taylan Gocmen <ta.gocmen@gmail.com>
2022-03-28 20:07:02 -0700

    time selector period constants

--- static/app/views/alerts/rules/details/constants.tsx
+++ static/app/views/alerts/rules/details/constants.tsx
@@ -4,11 +4,20 @@ import {t} from 'sentry/locale';
 import {SelectValue} from 'sentry/types';
 import {TimePeriod, TimeWindow} from 'sentry/views/alerts/incidentRules/types';
 
+export const SELECTOR_RELATIVE_PERIODS = {
+  [TimePeriod.SIX_HOURS]: t('Last 6 hours'),
+  [TimePeriod.ONE_DAY]: t('Last 24 hours'),
+  [TimePeriod.THREE_DAYS]: t('Last 3 days'),
+  [TimePeriod.SEVEN_DAYS]: t('Last 7 days'),
+  [TimePeriod.FOURTEEN_DAYS]: t('Last 14 days'),
+}
+
 export const TIME_OPTIONS: SelectValue<string>[] = [
   {label: t('Last 6 hours'), value: TimePeriod.SIX_HOURS},
   {label: t('Last 24 hours'), value: TimePeriod.ONE_DAY},
   {label: t('Last 3 days'), value: TimePeriod.THREE_DAYS},
   {label: t('Last 7 days'), value: TimePeriod.SEVEN_DAYS},
+  {label: t('Last 14 days'), value: TimePeriod.FOURTEEN_DAYS},
 ];
 
 export const TIME_WINDOWS = {
@@ -16,8 +25,10 @@ export const TIME_WINDOWS = {
   [TimePeriod.ONE_DAY]: TimeWindow.ONE_DAY * 60 * 1000,
   [TimePeriod.THREE_DAYS]: TimeWindow.ONE_DAY * 3 * 60 * 1000,
   [TimePeriod.SEVEN_DAYS]: TimeWindow.ONE_DAY * 7 * 60 * 1000,
+  [TimePeriod.FOURTEEN_DAYS]: TimeWindow.ONE_DAY * 14 * 60 * 1000,
 };
 
+export const SELECTOR_DEFAULT_PERIOD = TimePeriod.FOURTEEN_DAYS;
 export const API_INTERVAL_POINTS_LIMIT = 10000;
 export const API_INTERVAL_POINTS_MIN = 150;
 
