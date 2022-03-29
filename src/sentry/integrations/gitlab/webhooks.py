1cd45622b6c5b9b50938e3d333ca84e3da17f5a4
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-03-29 10:38:24 -0700

    fix(tests): Fix flaky tests for GitLab updates (#33022)
    See API-2585
    
    Prev PR: #33000
    
    In a previous PR I'd suggested we make a change to the naming scheme of repositories coming from GitLab, but as it turns out, we enforce unique constraints on Repositories (with OrganizationId and Name), meaning it doesn't even make sense to be updating the name.
    
    Instead, I've removed the name updates and now we just perform updates on the link and path.

--- src/sentry/integrations/gitlab/webhooks.py
+++ src/sentry/integrations/gitlab/webhooks.py
@@ -49,12 +49,6 @@ class Webhook:
 
     def update_repo_data(self, repo, event):
         """
-        # TODO(kmclb): the name w namespace bit is currently causing false positives
-        # for people with three or more levels in their repo names. Need to research
-        # exactly how gitlab handles these cases (what's the name, what's the namespace,
-        # does the url reliably have the info we need?) and then fix this for those
-        # cases.
-
         Given a webhook payload, update stored repo data if needed.
 
         Assumes a 'project' key in event payload, with certain subkeys. Rework
@@ -63,17 +57,11 @@ class Webhook:
 
         project = event["project"]
 
-        name_from_event = "{} / {}".format(project["namespace"], project["name"])
         url_from_event = project["web_url"]
         path_from_event = project["path_with_namespace"]
 
-        if (
-            repo.name != name_from_event
-            or repo.url != url_from_event
-            or repo.config.get("path") != path_from_event
-        ):
+        if repo.url != url_from_event or repo.config.get("path") != path_from_event:
             repo.update(
-                name=name_from_event,
                 url=url_from_event,
                 config=dict(repo.config, path=path_from_event),
             )
@@ -91,9 +79,8 @@ class MergeEventWebhook(Webhook):
         if repo is None:
             return
 
-        # TODO(kmclb): turn this back on once tri-level repo problem has been solved
         # while we're here, make sure repo data is up to date
-        # self.update_repo_data(repo, event)
+        self.update_repo_data(repo, event)
 
         try:
             number = event["object_attributes"]["iid"]
@@ -152,9 +139,8 @@ class PushEventWebhook(Webhook):
         if repo is None:
             return
 
-        # TODO(kmclb): turn this back on once tri-level repo problem has been solved
         # while we're here, make sure repo data is up to date
-        # self.update_repo_data(repo, event)
+        self.update_repo_data(repo, event)
 
         authors = {}
 
