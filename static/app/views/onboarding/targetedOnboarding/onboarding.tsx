cae2b3e51a62e1e8d7c56b500c9c5b72378c4537
Stephen Cefali <scefali@sentry.io>
2022-03-29 13:35:15 -0700

    fix onboarding header (#33076)

--- static/app/views/onboarding/targetedOnboarding/onboarding.tsx
+++ static/app/views/onboarding/targetedOnboarding/onboarding.tsx
@@ -199,7 +199,8 @@ const Header = styled('header')`
   z-index: 100;
   box-shadow: 0 5px 10px rgba(0, 0, 0, 0.05);
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
+  grid-template-columns: max-content 1fr max-content;
+  justify-items: end;
 `;
 
 const LogoSvg = styled(LogoSentry)`
