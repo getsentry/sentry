d2ae4403bf191c7fa9a02fb6199860a92b17cbd3
Scott Cooper <scttcper@gmail.com>
2022-03-30 13:39:38 -0700

    feat(ui): Remove some unused exports types/non-types
    Mostly in alerts, but a few others
    
    using
    ```
    npx ts-prune -p config/tsconfig.build.json
    ```

--- static/app/views/alerts/types.tsx
+++ static/app/views/alerts/types.tsx
@@ -65,12 +65,6 @@ export enum IncidentStatus {
   CRITICAL = 20,
 }
 
-export enum IncidentStatusMethod {
-  MANUAL = 1,
-  RULE_UPDATED = 2,
-  RULE_TRIGGERED = 3,
-}
-
 export enum AlertRuleStatus {
   PENDING = 0,
   SNAPSHOT = 4,
