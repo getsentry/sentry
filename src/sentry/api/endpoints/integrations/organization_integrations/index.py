4367df2e978dc61e48680a144bc6fbcf32a31069
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-03-30 12:37:53 -0700

    add docstring

--- src/sentry/api/endpoints/integrations/organization_integrations/index.py
+++ src/sentry/api/endpoints/integrations/organization_integrations/index.py
@@ -6,9 +6,14 @@ from rest_framework.response import Response
 from sentry.api.bases.organization import OrganizationEndpoint, OrganizationIntegrationsPermission
 from sentry.api.paginator import OffsetPaginator
 from sentry.api.serializers import serialize
-from sentry.models import ObjectStatus, OrganizationIntegration
+from sentry.models import ObjectStatus, Organization, OrganizationIntegration
+
 
 def query_param_to_bool(value: bool | int | str | None, default: bool = False) -> bool:
+    """
+    Generic parser for boolean-like query parameters.
+    TODO(mgaeta): Move this to somewhere in utils.
+    """
     if value is None:
         return default
 
@@ -35,7 +40,20 @@ def query_param_to_bool(value: bool | int | str | None, default: bool = False) -
 class OrganizationIntegrationsEndpoint(OrganizationEndpoint):
     permission_classes = (OrganizationIntegrationsPermission,)
 
-    def get(self, request: Request, organization) -> Response:
+    def get(self, request: Request, organization: Organization) -> Response:
+        """
+        List the available Integrations for an Organization
+        ```````````````````````````````````````````````````
+
+        :pparam string organization_slug: The slug of the organization.
+
+        :qparam string provider_key: Filter by specific integration provider. (e.g. "slack")
+        :qparam string[] features: Filter by integration features names.
+        :qparam bool includeConfig: Should integrations configurations be fetched from third-party
+            APIs? This can add several thousand ms to the request round trip.
+
+        :auth: required
+        """
 
         # filter by integration provider features
         features = [feature.lower() for feature in request.GET.getlist("features", [])]
