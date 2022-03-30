fa6ce73eb279683aa7faff422c636efb6659a554
Vu Luong <vuluongj20@gmail.com>
2022-03-30 11:20:47 -0700

    fix(trimSlug): Handle long unhyphenated strings (#33132)
    * fix(trimSlug): Handle long unhyphenated strings
    
    * ref(test): Add test to trimSlug

--- tests/js/spec/utils/trimSlug.spec.tsx
+++ tests/js/spec/utils/trimSlug.spec.tsx
@@ -5,6 +5,10 @@ describe('trimSlug', function () {
     expect(trimSlug('javascript', 20)).toBe('javascript');
   });
 
+  it('trims long but unhyphenated slug', function () {
+    expect(trimSlug('javascriptfrontendproject', 20)).toBe('javascriptfrontendp…');
+  });
+
   it('trims slug from the middle, preserves whole words', function () {
     expect(trimSlug('symbol-collector-console', 20)).toBe('symbol…console');
     expect(trimSlug('symbol-collector-mobile', 20)).toBe('symbol…mobile');
