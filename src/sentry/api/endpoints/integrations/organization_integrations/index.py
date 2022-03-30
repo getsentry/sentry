24b9d4776edaa9bd993ba78846bda88ea8e32eb1
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-03-30 11:28:57 -0700

    rename submodule

---
+++ src/sentry/api/endpoints/integrations/organization_integrations/index.py
@@ -0,0 +1,62 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from sentry.api.bases.organization import OrganizationEndpoint, OrganizationIntegrationsPermission
+from sentry.api.paginator import OffsetPaginator
+from sentry.api.serializers import serialize
+from sentry.models import ObjectStatus, OrganizationIntegration
+
+
+class OrganizationIntegrationsEndpoint(OrganizationEndpoint):
+    permission_classes = (OrganizationIntegrationsPermission,)
+
+    def get(self, request: Request, organization) -> Response:
+
+        # filter by integration provider features
+        features = [feature.lower() for feature in request.GET.getlist("features", [])]
+
+        # show disabled org integrations but not ones being deleted
+        integrations = OrganizationIntegration.objects.filter(
+            organization=organization,
+            status__in=[ObjectStatus.VISIBLE, ObjectStatus.DISABLED, ObjectStatus.PENDING_DELETION],
+        )
+
+        if "provider_key" in request.GET:
+            integrations = integrations.filter(integration__provider=request.GET["provider_key"])
+
+        # include the configurations by default if no param
+        include_config = True
+        if request.GET.get("includeConfig") == "0":
+            include_config = False
+
+        def on_results(results):
+            if len(features):
+                return [
+                    serialize(
+                        i,
+                        request.user,
+                        include_config=include_config,
+                    )
+                    for i in filter(
+                        # check if any feature in query param is in the provider feature list
+                        lambda i: any(
+                            f
+                            in [
+                                feature.name.lower()
+                                for feature in list(i.integration.get_provider().features)
+                            ]
+                            for f in features
+                        ),
+                        results,
+                    )
+                ]
+
+            return serialize(results, request.user, include_config=include_config)
+
+        return self.paginate(
+            queryset=integrations,
+            request=request,
+            order_by="integration__name",
+            on_results=on_results,
+            paginator_cls=OffsetPaginator,
+        )
