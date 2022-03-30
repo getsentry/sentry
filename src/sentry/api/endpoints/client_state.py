80263fb04025a8911fd0e7bf77c6e5f385416f20
Zhixing Zhang <me@neoto.xin>
2022-03-30 15:12:15 -0700

    use configured cluster key

--- src/sentry/api/endpoints/client_state.py
+++ src/sentry/api/endpoints/client_state.py
@@ -1,5 +1,6 @@
 # This endpoint helps managing persisted client state with a TTL for a member, organization or user
 
+from django.conf import settings
 from rest_framework.exceptions import NotFound
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -17,6 +18,11 @@ STATE_CATEGORIES = {
 
 
 class ClientStateEndpoint(OrganizationEndpoint):
+    def __init__(self, **options) -> None:
+        cluster_key = getattr(settings, "SENTRY_CLIENT_STATE_REDIS_CLUSTER", "default")
+        self.client = redis.redis_clusters.get(cluster_key)
+        super().__init__(**options)
+
     def get_key(self, organization, category, user):
         if category not in STATE_CATEGORIES:
             return NotFound(detail="Category not found")
@@ -28,12 +34,10 @@ class ClientStateEndpoint(OrganizationEndpoint):
 
     def get(self, request: Request, organization, category) -> Response:
         key = self.get_key(organization.slug, category, request.user)
-        res = None
-        with redis.clusters.get("default").map() as client:
-            res = client.get(key)
-            client.expire(key, STATE_CATEGORIES[category]["ttl"])
-        if res.value:
-            return Response(res.value)
+        res = self.client.get(key)
+        if res:
+            self.client.expire(key, STATE_CATEGORIES[category]["ttl"])
+            return Response(json.loads(res))
         else:
             return Response({})
 
@@ -42,6 +46,5 @@ class ClientStateEndpoint(OrganizationEndpoint):
         data_to_write = json.dumps(request.data)
         if len(data_to_write) > STATE_CATEGORIES[category]["max_payload_size"]:
             return Response(status=413)
-        with redis.clusters.get("default").map() as client:
-            client.setex(key, STATE_CATEGORIES[category]["ttl"], data_to_write)
+        self.client.setex(key, STATE_CATEGORIES[category]["ttl"], data_to_write)
         return Response(status=201)
