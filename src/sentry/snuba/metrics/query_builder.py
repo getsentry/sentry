c1a56a7f35e06fbae408688cabb18e54d2911a13
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-03-31 09:33:48 +0200

    Update src/sentry/snuba/metrics/query_builder.py
    Co-authored-by: Joris Bayer <joris.bayer@sentry.io>

--- src/sentry/snuba/metrics/query_builder.py
+++ src/sentry/snuba/metrics/query_builder.py
@@ -183,8 +183,8 @@ class QueryDefinition:
             )
         histogram_from = query_params.get("histogramFrom", None)
         histogram_to = query_params.get("histogramTo", None)
-        self.histogram_from = int(histogram_from) if histogram_from is not None else None
-        self.histogram_to = int(histogram_to) if histogram_to is not None else None
+        self.histogram_from = float(histogram_from) if histogram_from is not None else None
+        self.histogram_to = float(histogram_to) if histogram_to is not None else None
         self.include_series = query_params.get("includeSeries", "1") == "1"
         self.include_totals = query_params.get("includeTotals", "1") == "1"
 
