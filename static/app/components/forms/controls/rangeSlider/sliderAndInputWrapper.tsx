cd136160f070619dcf525dd88c6fd1435646655e
Valery Brobbey <valery.brobbey@sentry.io>
2022-04-01 11:29:00 -0700

    feat(billing): export slider props and components for use in getsentry

--- static/app/components/forms/controls/rangeSlider/sliderAndInputWrapper.tsx
+++ static/app/components/forms/controls/rangeSlider/sliderAndInputWrapper.tsx
@@ -2,10 +2,12 @@ import styled from '@emotion/styled';
 
 import space from 'sentry/styles/space';
 
-export default styled('div')<{showCustomInput?: boolean}>`
+const SliderAndInputWrapper = styled('div')<{showCustomInput?: boolean}>`
   display: grid;
   align-items: center;
   grid-auto-flow: column;
   grid-template-columns: 4fr ${p => p.showCustomInput && '1fr'};
   gap: ${space(1)};
 `;
+
+export default SliderAndInputWrapper;
