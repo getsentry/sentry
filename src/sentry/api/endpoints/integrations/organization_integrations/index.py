7e8dc9956e1fdb9327d384db787cc08a637b840e
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-04-04 13:09:20 -0700

    Clean up endpoint

--- src/sentry/api/endpoints/integrations/organization_integrations/index.py
+++ src/sentry/api/endpoints/integrations/organization_integrations/index.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+from typing import Any, Mapping, Sequence
+
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -7,6 +9,33 @@ from sentry.api.bases.organization import OrganizationEndpoint, OrganizationInte
 from sentry.api.paginator import OffsetPaginator
 from sentry.api.serializers import serialize
 from sentry.models import ObjectStatus, Organization, OrganizationIntegration
+
+
+def prepare_feature_filters(features_raw: Sequence[str]) -> set[str]:
+    """Normalize feature names from query params."""
+    return {feature.lower().strip() for feature in features_raw}
+
+
+def prepare_features(organization_integration: OrganizationIntegration) -> set[str]:
+    """Normalize feature names Integration provider feature lists."""
+    return {
+        feature.name.lower().strip()
+        for feature in organization_integration.integration.get_provider().features
+    }
+
+
+def filter_by_features(
+    organization_integrations: Sequence[OrganizationIntegration],
+    feature_filters: Sequence[str],
+) -> Sequence[OrganizationIntegration]:
+    """Filter the list of organization integrations by feature."""
+    return [
+        organization_integration
+        for organization_integration in organization_integrations
+        if prepare_feature_filters(feature_filters).intersection(
+            prepare_features(organization_integration)
+        )
+    ]
 
 
 class OrganizationIntegrationsEndpoint(OrganizationEndpoint):
@@ -26,53 +55,35 @@ class OrganizationIntegrationsEndpoint(OrganizationEndpoint):
 
         :auth: required
         """
-
         feature_filters = request.GET.getlist("features", [])
         provider_key = request.GET.get("provider_key", "")
         include_config_raw = request.GET.get("includeConfig")
 
-        # filter by integration provider features
-        features = [feature.lower() for feature in feature_filters]
-
-        # show disabled org integrations but not ones being deleted
-        integrations = OrganizationIntegration.objects.filter(
+        # Show disabled organization integrations but not the ones currently
+        # undergoing deletion.
+        queryset = OrganizationIntegration.objects.filter(
             organization=organization,
-            status__in=[ObjectStatus.VISIBLE, ObjectStatus.DISABLED, ObjectStatus.PENDING_DELETION],
+            status__in=[
+                ObjectStatus.VISIBLE,
+                ObjectStatus.DISABLED,
+                ObjectStatus.PENDING_DELETION,
+            ],
         )
 
         if provider_key:
-            integrations = integrations.filter(integration__provider=provider_key.lower())
+            queryset = queryset.filter(integration__provider=provider_key.lower())
 
         # Include the configurations by default if includeConfig is not present.
         # TODO(mgaeta): HACK. We need a consistent way to get booleans from query parameters.
         include_config = include_config_raw != "0"
 
-        def on_results(results):
-            if len(features):
-                return [
-                    serialize(
-                        i,
-                        request.user,
-                        include_config=include_config,
-                    )
-                    for i in filter(
-                        # check if any feature in query param is in the provider feature list
-                        lambda i: any(
-                            f
-                            in [
-                                feature.name.lower()
-                                for feature in list(i.integration.get_provider().features)
-                            ]
-                            for f in features
-                        ),
-                        results,
-                    )
-                ]
-
+        def on_results(results: Sequence[OrganizationIntegration]) -> Sequence[Mapping[str, Any]]:
+            if feature_filters:
+                results = filter_by_features(results, feature_filters)
             return serialize(results, request.user, include_config=include_config)
 
         return self.paginate(
-            queryset=integrations,
+            queryset=queryset,
             request=request,
             order_by="integration__name",
             on_results=on_results,
