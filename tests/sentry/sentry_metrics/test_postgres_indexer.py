0acd3fd56a3d5c6f4bab508dbe63c4bbcdea6f49
Marcos Gaeta <31750075+mgaeta@users.noreply.github.com>
2022-04-04 14:03:07 -0700

    Don't assert order (#33284)

--- tests/sentry/sentry_metrics/test_postgres_indexer.py
+++ tests/sentry/sentry_metrics/test_postgres_indexer.py
@@ -19,12 +19,10 @@ class PostgresIndexerTest(TestCase):
         org_id = self.organization.id
         org_strings = {org_id: {"hello", "hey", "hi"}}
         results = PGStringIndexer().bulk_record(org_strings=org_strings)
-        obj_ids = list(
-            MetricsKeyIndexer.objects.filter(string__in=["hello", "hey", "hi"]).values_list(
-                "id", flat=True
-            )
+        obj_ids = MetricsKeyIndexer.objects.filter(string__in=["hello", "hey", "hi"]).values_list(
+            "id", flat=True
         )
-        assert list(results.values()) == obj_ids
+        assert set(results.values()) == set(obj_ids)
 
         # test resolve and reverse_resolve
         obj = MetricsKeyIndexer.objects.get(string="hello")
