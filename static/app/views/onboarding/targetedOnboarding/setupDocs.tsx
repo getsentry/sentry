0c9a0d79fd20423dbae570a4c53ff5e412917b69
k-fish <kevan.fisher@sentry.io>
2022-04-04 14:55:25 -0400

    ref(tsc): Remove default React imports
    As per JSX transform change, importing React like this is no longer necessary.

--- static/app/views/onboarding/targetedOnboarding/setupDocs.tsx
+++ static/app/views/onboarding/targetedOnboarding/setupDocs.tsx
@@ -1,4 +1,5 @@
-import React, {useEffect, useState} from 'react';
+import { useEffect, useState } from 'react';
+import * as React from 'react';
 import {browserHistory} from 'react-router';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
