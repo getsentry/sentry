282341894df90f9e926b0049eb833470ce421682
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-04-05 10:50:55 -0700

    feat(replay): Show a single <Breadcrumb> widget with data from all events

---
+++ static/app/views/replays/utils/mergeBreadcrumbsEntries.tsx
@@ -0,0 +1,20 @@
+import {Entry, EntryType, Event} from 'sentry/types/event';
+
+function isBreadcrumbs(entry: Entry) {
+  return entry.type === EntryType.BREADCRUMBS;
+}
+
+export default function mergeBreadcrumbsEntries(events: Event[]) {
+  const allValues = events
+    .flatMap(event => event.entries.filter(isBreadcrumbs))
+    .flatMap(entry => entry.data.values)
+    .map(value => JSON.stringify(value));
+  const deduped = new Set(allValues);
+
+  return {
+    type: EntryType.BREADCRUMBS,
+    data: {
+      values: Array.from(deduped).map(value => JSON.parse(value)),
+    },
+  } as Entry;
+}
