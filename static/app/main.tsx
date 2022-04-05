75e20049b816290b748144d0514177e2bd3438c0
JonasBa <jonas.badalic@sentry.io>
2022-04-05 18:22:20 -0400

    feat(configProvider): add provider to main

--- static/app/main.tsx
+++ static/app/main.tsx
@@ -5,11 +5,17 @@ import ThemeAndStyleProvider from 'sentry/components/themeAndStyleProvider';
 import routes from 'sentry/routes';
 import LegacyConfigStore from 'sentry/stores/configStore';
 
+import {ConfigProvider} from './stores/configStore/configProvider';
+
 function Main() {
   return (
     <ThemeAndStyleProvider>
       {LegacyConfigStore.get('demoMode') && <DemoHeader />}
-      <Router history={browserHistory}>{routes()}</Router>
+      <Router history={browserHistory}>
+        <ConfigProvider initialValue={LegacyConfigStore.config} bridgeReflux>
+          {routes()}
+        </ConfigProvider>
+      </Router>
     </ThemeAndStyleProvider>
   );
 }
