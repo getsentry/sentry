296ce12a88414e4429b0973bc3d386c1e4e79edf
JonasBa <jonas.badalic@sentry.io>
2022-04-06 09:30:51 -0400

    fix(store): temp rename to fix case insensitive fs

--- static/app/stores/commiters/CommittersContext.tmp.tsx
+++
@@ -1,7 +0,0 @@
-import {createContext} from 'react';
-
-import {CommittersAction, CommittersState} from './committersReducer';
-
-export const CommittersContext = createContext<
-  [CommittersState, React.Dispatch<CommittersAction>] | null
->(null);
