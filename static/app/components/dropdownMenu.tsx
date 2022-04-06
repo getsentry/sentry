c34da9c6ba34e729d180159b3f953efa3ecf8bfd
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-04-06 14:56:48 -0700

    chore(lint): Fix warning about property ordering (#33367)

--- static/app/components/dropdownMenu.tsx
+++ static/app/components/dropdownMenu.tsx
@@ -121,17 +121,17 @@ class DropdownMenu extends React.Component<Props, State> {
     isOpen: false,
   };
 
-  dropdownMenu: Element | null = null;
-  dropdownActor: Element | null = null;
-
-  mouseLeaveTimeout: number | undefined = undefined;
-  mouseEnterTimeout: number | undefined = undefined;
-
   componentWillUnmount() {
     window.clearTimeout(this.mouseLeaveTimeout);
     window.clearTimeout(this.mouseEnterTimeout);
     document.removeEventListener('click', this.checkClickOutside, true);
   }
+
+  dropdownMenu: Element | null = null;
+  dropdownActor: Element | null = null;
+
+  mouseLeaveTimeout: number | undefined = undefined;
+  mouseEnterTimeout: number | undefined = undefined;
 
   // Gets open state from props or local state when appropriate
   isOpen = () => {
