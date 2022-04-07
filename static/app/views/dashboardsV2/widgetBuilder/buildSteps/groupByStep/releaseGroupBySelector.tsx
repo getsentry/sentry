5cca2b66d504e0f20ed8fb857cc10c0e4624a59b
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-04-07 20:14:21 +0200

    cr feedback

--- static/app/views/dashboardsV2/widgetBuilder/buildSteps/groupByStep/releaseGroupBySelector.tsx
+++ static/app/views/dashboardsV2/widgetBuilder/buildSteps/groupByStep/releaseGroupBySelector.tsx
@@ -12,20 +12,21 @@ interface Props {
 export function ReleaseGroupBySelector({columns, onChange}: Props) {
   const {tags} = useMetricsContext();
 
-  const tagKeys = Object.values(tags)
+  // Tags are sorted alphabetically to make it easier to find the correct option
+  // and are converted to fieldOptions format
+  const fieldOptions = Object.values(tags)
     .map(({key}) => key)
-    .sort((a, b) => a.localeCompare(b));
-
-  const fieldOptions = tagKeys.reduce((acc, tagKey) => {
-    acc[`tag:${tagKey}`] = {
-      label: tagKey,
-      value: {
-        kind: FieldValueKind.TAG,
-        meta: {name: tagKey, dataType: 'string'},
-      },
-    };
-    return acc;
-  }, {});
+    .sort((a, b) => a.localeCompare(b))
+    .reduce((acc, tagKey) => {
+      acc[`tag:${tagKey}`] = {
+        label: tagKey,
+        value: {
+          kind: FieldValueKind.TAG,
+          meta: {name: tagKey, dataType: 'string'},
+        },
+      };
+      return acc;
+    }, {});
 
   return (
     <GroupBySelector columns={columns} fieldOptions={fieldOptions} onChange={onChange} />
