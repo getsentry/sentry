064169469a094b47e23d45b56e1dee740375ae33
Zhixing Zhang <me@neoto.xin>
2022-04-08 16:29:07 -0700

    Doing renames

--- static/app/stores/persistedStore.tsx
+++ static/app/stores/persistedStore.tsx
@@ -1,4 +1,4 @@
-import {createContext, useContext, useEffect, useState} from 'react';
+import React, {createContext, useCallback, useContext, useEffect, useMemo} from 'react';
 
 import useApi from 'sentry/utils/useApi';
 import useOrganization from 'sentry/utils/useOrganization';
@@ -6,52 +6,97 @@ import useOrganization from 'sentry/utils/useOrganization';
 import OrganizationStore from './organizationStore';
 import {useLegacyStore} from './useLegacyStore';
 
-type PersistedStore = {[key: string]: any};
-export const PersistedStoreContext = createContext<
-  [PersistedStore, ((string, any) => void) | null]
->([{}, null]);
+type PersistedStore = Readonly<{[key: string]: any}>;
+type PersistedStoreContextValue = [
+  PersistedStore,
+  React.Dispatch<React.SetStateAction<PersistedStore>>
+];
+
+export const PersistedStoreContext = createContext<PersistedStoreContextValue | null>(
+  null
+);
+
+function usePersistedStore(): PersistedStoreContextValue {
+  const context = useContext(PersistedStoreContext);
+
+  if (!context) {
+    throw new Error('usePersistedStore was called outside of PersistedStoreProvider');
+  }
+
+  return context;
+}
 
 // Client-only state with TTL persisted on the server side in a redis store.
 export function PersistedStoreProvider(props: {children: React.ReactNode}) {
-  const [contextValue, setContextValue] = useState<PersistedStore>({});
-  const setContextValueForCategory = (category: string, value: any) => {
-    setContextValue({...contextValue, [category]: value});
-  };
+  const [state, setState] = usePersistedStore();
+
   const api = useApi();
   const {organization} = useLegacyStore(OrganizationStore);
+
   useEffect(() => {
-    organization &&
-      api
-        .requestPromise(`/organizations/${organization.slug}/client-state/`)
-        .then(setContextValue);
+    if (!organization) {
+      return undefined;
+    }
+
+    let shouldCancelRequest = false;
+
+    api
+      .requestPromise(`/organizations/${organization.slug}/client-state/`)
+      .then((response: PersistedStore) => {
+        if (shouldCancelRequest) {
+          return;
+        }
+
+        setState(response);
+      });
+
+    return () => {
+      shouldCancelRequest = true;
+    };
   }, [organization]);
+
   return (
-    <PersistedStoreContext.Provider value={[contextValue, setContextValueForCategory]}>
+    <PersistedStoreContext.Provider value={[state, setState]}>
       {props.children}
     </PersistedStoreContext.Provider>
   );
 }
 
-export function usePersistedStore<T>(
-  category: string
-): [T | null, (next: T | null) => void] {
-  const [state, setStateForCategory] = useContext(PersistedStoreContext);
-  const org = useOrganization();
+type UsePersistedCategory<T> = [T | null, (nextState: T | null) => void];
+export function usePersistedStoreCategory<T>(category): UsePersistedCategory<T> {
   const api = useApi();
-  const setState = async (val: T | null) => {
-    if (val === null) {
-      await api.requestPromise(`/organizations/${org.slug}/client-state/${category}/`, {
-        method: 'DELETE',
-      });
-    } else {
-      await api.requestPromise(`/organizations/${org.slug}/client-state/${category}/`, {
-        method: 'PUT',
-        data: val,
-      });
+  const organization = useOrganization();
+  const [state, setState] = usePersistedStore();
+
+  const setCategoryState = useCallback(
+    (val: T | null) => {
+      setState(oldState => ({...oldState, [category]: val}));
+    },
+    [category, organization]
+  );
+
+  useEffect(() => {
+    // If a state is set with null, we can clear it from the server.
+    if (state[category] === null) {
+      api.requestPromise(
+        `/organizations/${organization.slug}/client-state/${category}/`,
+        {
+          method: 'DELETE',
+        }
+      );
+      return;
     }
 
-    setStateForCategory && setStateForCategory(category, val);
-  };
+    // Else we want to sync our state with the server
+    api.requestPromise(`/organizations/${organization.slug}/client-state/${category}/`, {
+      method: 'PUT',
+      data: state[category],
+    });
+  }, [state[category], organization]);
 
-  return [state[category] || null, setState];
+  const stableState: UsePersistedCategory<T> = useMemo(() => {
+    return [state[category] ?? null, setCategoryState];
+  }, [state[category], setCategoryState]);
+
+  return stableState;
 }
