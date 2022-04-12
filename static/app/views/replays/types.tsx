33010444077d32d4f9500bc38a38aaafc0a9c31c
Dublin Anondson <stratophone2112@gmail.com>
2022-04-12 12:53:08 -0500

    feat: updated the Replay type to contain values needed for the UserBadge Components (earlier commit is the tsx file, previous commit was the view update).

--- static/app/views/replays/types.tsx
+++ static/app/views/replays/types.tsx
@@ -1,8 +1,12 @@
 export type Replay = {
+  display: string;
+  email: string;
   eventID: string;
   id: string;
+  ip: string;
   projectID: string;
   replayId: string;
   timestamp: string;
   user: string;
+  username: string;
 };
