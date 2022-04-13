1988e98a511adb01aa622d3ee576b466bab27967
Ryan Skonnord <ryan.skonnord@sentry.io>
2022-04-13 15:30:28 -0700

    feat(access): Implement team-based role management and access (#33387)
    Introduce team-based roles in parallel to existing, organization-based
    roles. Split the levels into their own objects, accessible through the
    parent RoleManager object. Map org roles onto the corresponding minimum
    team roles, which each member with that org role enjoys automatically.
    
    Have Access.has_team_scope check scopes given by the member's team role,
    in addition to those given by their organization role. This differs from
    previous behavior, in that a member might enjoy a scope for a particular
    team that they would not if Access.has_scope were called.
    
    Introduce the "organizations:team-roles" feature flag. Organizations
    without this flag don't give any additional scopes for team roles.
    
    There is currently no way to assign team roles. API support is pending.

--- src/sentry/roles/__init__.py
+++ src/sentry/roles/__init__.py
@@ -2,8 +2,15 @@ from django.conf import settings
 
 from .manager import RoleManager
 
-default_manager = RoleManager(settings.SENTRY_ROLES, settings.SENTRY_DEFAULT_ROLE)
+default_manager = RoleManager(
+    settings.SENTRY_ROLES, settings.SENTRY_TEAM_ROLES, settings.SENTRY_DEFAULT_ROLE
+)
 
+organization_roles = default_manager.organization_roles
+team_roles = default_manager.team_roles
+get_minimum_team_role = default_manager.get_minimum_team_role
+
+# Deprecated: Prefer calling methods on organization_roles
 can_manage = default_manager.can_manage
 get = default_manager.get
 get_all = default_manager.get_all
