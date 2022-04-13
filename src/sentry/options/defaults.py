20e4cf28a45b4824f7a35c49e593622edec6d17a
Oliver Newland <onewland@gmail.com>
2022-04-13 12:06:22 -0700

    feat(events) - add partition override kill switches (#33589)
    add override kill switches

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -384,3 +384,8 @@ register("store.save-transactions-ingest-consumer-rate", default=0.0)
 
 # Drop delete_old_primary_hash messages for a particular project.
 register("reprocessing2.drop-delete-old-primary-hash", default=[])
+
+# Send event messages for specific project IDs to random partitions in Kafka
+# contents are a list of project IDs to message types to be randomly assigned
+# e.g. [{"project_id": 2, "message_type": "error"}, {"project_id": 3, "message_type": "transaction"}]
+register("kafka.send-project-events-to-random-partitions", default=[])
