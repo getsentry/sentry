22fb382c3e09d407297412d1e8e5478e927741d0
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-04-14 15:04:02 -0400

    fix failing tests

--- tests/js/spec/utils/metrics/transformMetricsResponseToTable.spec.tsx
+++ tests/js/spec/utils/metrics/transformMetricsResponseToTable.spec.tsx
@@ -3,71 +3,73 @@ import {transformMetricsResponseToTable} from 'sentry/utils/metrics/transformMet
 describe('transformMetricsResponseToTable', function () {
   it('transforms metrics into table', () => {
     expect(
-      transformMetricsResponseToTable(TestStubs.SessionUserCountByStatusByRelease())
+      transformMetricsResponseToTable(
+        TestStubs.MetricsSessionUserCountByStatusByRelease()
+      )
     ).toEqual({
       data: [
         {
-          'count_unique(user)': 1,
+          'count_unique(sentry.sessions.user)': 1,
           id: '0',
           release: '1',
           'session.status': 'crashed',
-          'sum(session)': 34,
+          'sum(sentry.sessions.session)': 34,
         },
         {
-          'count_unique(user)': 1,
+          'count_unique(sentry.sessions.user)': 1,
           id: '1',
           release: '1',
           'session.status': 'abnormal',
-          'sum(session)': 1,
+          'sum(sentry.sessions.session)': 1,
         },
         {
-          'count_unique(user)': 2,
+          'count_unique(sentry.sessions.user)': 2,
           id: '2',
           release: '1',
           'session.status': 'errored',
-          'sum(session)': 451,
+          'sum(sentry.sessions.session)': 451,
         },
         {
-          'count_unique(user)': 3,
+          'count_unique(sentry.sessions.user)': 3,
           id: '3',
           release: '1',
           'session.status': 'healthy',
-          'sum(session)': 5058,
+          'sum(sentry.sessions.session)': 5058,
         },
         {
-          'count_unique(user)': 2,
+          'count_unique(sentry.sessions.user)': 2,
           id: '4',
           release: '2',
           'session.status': 'crashed',
-          'sum(session)': 35,
+          'sum(sentry.sessions.session)': 35,
         },
         {
-          'count_unique(user)': 1,
+          'count_unique(sentry.sessions.user)': 1,
           id: '5',
           release: '2',
           'session.status': 'abnormal',
-          'sum(session)': 1,
+          'sum(sentry.sessions.session)': 1,
         },
         {
-          'count_unique(user)': 1,
+          'count_unique(sentry.sessions.user)': 1,
           id: '6',
           release: '2',
           'session.status': 'errored',
-          'sum(session)': 452,
+          'sum(sentry.sessions.session)': 452,
         },
         {
-          'count_unique(user)': 10,
+          'count_unique(sentry.sessions.user)': 10,
           id: '7',
           release: '2',
           'session.status': 'healthy',
-          'sum(session)': 5059,
+          'sum(sentry.sessions.session)': 5059,
         },
       ],
       meta: {
-        'count_unique(user)': 'integer',
+        'count_unique(sentry.sessions.user)': 'integer',
         release: 'string',
         'session.status': 'string',
-        'sum(session)': 'integer',
+        'sum(sentry.sessions.session)': 'integer',
       },
     });
   });
