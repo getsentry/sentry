13f2f26dfec44443cb72fd33f455d2da5b45c116
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-04-14 14:18:50 -0700

    feat(replay): Filter replay-event transactions from breadcrumbs

--- static/app/views/replays/utils/mergeBreadcrumbsEntries.tsx
+++ static/app/views/replays/utils/mergeBreadcrumbsEntries.tsx
@@ -4,11 +4,34 @@ function isBreadcrumbs(entry: Entry) {
   return entry.type === EntryType.BREADCRUMBS;
 }
 
-export default function mergeBreadcrumbsEntries(events: Event[]): Entry {
+function eventBreadcrumbEntries(event: Event) {
+  return event.entries.filter(isBreadcrumbs);
+}
+function entryValues(entry: Entry) {
+  return entry.data.values;
+}
+
+type FilterPredicate = Parameters<typeof Array.prototype.filter>[0];
+function not(predicate: FilterPredicate): FilterPredicate {
+  return (value, index, array) => !predicate(value, index, array);
+}
+
+export default function mergeBreadcrumbsEntries(
+  events: Event[],
+  rootEvent: Event
+): Entry {
+  const rrwebEventIds = events.map(({id}) => id);
+
+  const isRRWebTransaction = crumb =>
+    crumb.category === 'sentry.transaction' &&
+    (rootEvent.id === crumb.message || rrwebEventIds.includes(crumb.message));
+
   const allValues = events
-    .flatMap(event => event.entries.filter(isBreadcrumbs))
-    .flatMap(entry => entry.data.values)
+    .flatMap(eventBreadcrumbEntries)
+    .flatMap(entryValues)
+    .filter(not(isRRWebTransaction))
     .map(value => JSON.stringify(value));
+
   const deduped = new Set(allValues);
 
   return {
