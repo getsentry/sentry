1bc94ab5a4e74f106813bf8e1ede015812bfbcc4
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-04-15 14:46:38 -0700

    remove a few one-time-use functions

--- static/app/views/replays/utils/mergeBreadcrumbsEntries.tsx
+++ static/app/views/replays/utils/mergeBreadcrumbsEntries.tsx
@@ -1,19 +1,11 @@
 import {Entry, EntryType, Event} from 'sentry/types/event';
 
-function isBreadcrumbs(entry: Entry) {
-  return entry.type === EntryType.BREADCRUMBS;
+function eventBreadcrumbEntries(event: Event) {
+  return event.entries.filter(entry => entry.type === EntryType.BREADCRUMBS);
 }
 
-function eventBreadcrumbEntries(event: Event) {
-  return event.entries.filter(isBreadcrumbs);
-}
 function entryValues(entry: Entry) {
   return entry.data.values;
-}
-
-type FilterPredicate = Parameters<typeof Array.prototype.filter>[0];
-function not(predicate: FilterPredicate): FilterPredicate {
-  return (value, index, array) => !predicate(value, index, array);
 }
 
 export default function mergeBreadcrumbsEntries(
@@ -22,14 +14,16 @@ export default function mergeBreadcrumbsEntries(
 ): Entry {
   const rrwebEventIds = events.map(({id}) => id);
 
-  const isRRWebTransaction = crumb =>
-    crumb.category === 'sentry.transaction' &&
-    (rootEvent.id === crumb.message || rrwebEventIds.includes(crumb.message));
+  const notRRWebTransaction = crumb =>
+    !(
+      crumb.category === 'sentry.transaction' &&
+      (rootEvent.id === crumb.message || rrwebEventIds.includes(crumb.message))
+    );
 
   const allValues = events
     .flatMap(eventBreadcrumbEntries)
     .flatMap(entryValues)
-    .filter(not(isRRWebTransaction))
+    .filter(notRRWebTransaction)
     .map(value => JSON.stringify(value));
 
   const deduped = new Set(allValues);
