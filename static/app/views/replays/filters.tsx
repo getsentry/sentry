4b55163fdb4b87000f64e46e400b609564c54584
Dublin Anondson <stratophone2112@gmail.com>
2022-04-15 11:26:38 -0500

    refactor: removed a lot of redundant styling within ReplaysFilters and made the header responsive
    The header was previously the width of the screen and not responsive and now it's responsive.
    .
    
    Fixes GH-33270

--- static/app/views/replays/filters.tsx
+++ static/app/views/replays/filters.tsx
@@ -10,39 +10,23 @@ import space from 'sentry/styles/space';
 function ReplaysFilters() {
   return (
     <FilterContainer>
-      <SearchContainer>
-        <PageFilterBar>
-          <ProjectPageFilter />
-          <EnvironmentPageFilter alignDropdown="left" />
-          <DatePageFilter alignDropdown="left" />
-        </PageFilterBar>
-      </SearchContainer>
+      <PageFilterBar>
+        <ProjectPageFilter />
+        <EnvironmentPageFilter alignDropdown="left" />
+        <DatePageFilter alignDropdown="left" />
+      </PageFilterBar>
     </FilterContainer>
   );
 }
 
 const FilterContainer = styled('div')`
-  display: grid;
-  gap: ${space(1)};
-  margin-bottom: ${space(1)};
-`;
-
-const SearchContainer = styled('div')<{
-  hasPageFilters?: boolean;
-}>`
-  display: inline-grid;
-  gap: ${space(1)};
-  width: 100%;
+  width: max-content;
+  max-width: 100%;
   margin-bottom: ${space(1)};
 
-  ${p =>
-    p.hasPageFilters
-      ? `grid-template-columns: minmax(0, max-content) minmax(20rem, 1fr);`
-      : `
-    @media (min-width: ${p.theme.breakpoints[0]}) {
-      grid-template-columns: 1fr auto;
-    }
-  }`}
+  @media (min-width: ${p => p.theme.breakpoints[0]}) {
+    grid-template-columns: 1fr auto;
+  }
 
   @media (max-width: ${p => p.theme.breakpoints[0]}) {
     grid-template-columns: minmax(0, 1fr);
