6064e49dde4f0b25b0de26cf810088364be1cbe1
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-04-18 13:31:09 -0700

    fix(replay): Remove `undefined` from merged span list (#33684)
    In order to get a single view of all the spans that occurred through the life of a Replay we need to merge Entry data from each Event that was captured. The code that merged each Event didn't properly account for events where no span data was captured, those events don't need to be part of the final merged result.
    
    This change removes span data that is `undefined` and does things in a more type-aware way -> Typescript correctly infers that `const spans` has the type `RawSpanType[]`

--- static/app/views/replays/utils/mergeEventsWithSpans.tsx
+++ static/app/views/replays/utils/mergeEventsWithSpans.tsx
@@ -1,13 +1,12 @@
+import type {RawSpanType} from 'sentry/components/events/interfaces/spans/types';
 import {Entry, EntryType, Event} from 'sentry/types/event';
-
-function isSpanEntry(entry: Entry) {
-  return entry.type === EntryType.SPANS;
-}
 
 export default function mergeEventsWithSpans(events: Event[]): Event {
   // Get a merged list of all spans from all replay events
-  const spans = events.flatMap(
-    replayEvent => replayEvent.entries.find(isSpanEntry)?.data
+  const spans = events.flatMap(event =>
+    event.entries.flatMap((entry: Entry) =>
+      entry.type === EntryType.SPANS ? (entry.data as RawSpanType[]) : []
+    )
   );
 
   // Create a merged spans entry on the first replay event and fake the
