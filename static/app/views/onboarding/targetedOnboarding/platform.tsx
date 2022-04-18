499af7c9747a347563285d8d2f1624c4d8750597
Zhixing Zhang <me@neoto.xin>
2022-04-18 13:50:08 -0700

    Onboarding Quick Start item (#33642)
    * onboarding sidebar item
    
    * hide behind experiment flag
    
    * add analytics
    
    * revert to original styles
    
    * review suggestions
    
    * moved hook
    
    * remove flex gap
    
    * fix multiplatform select ordering
    
    * fix typo
    
    * change of copy
    
    * fix animation
    
    * update more copy
    
    * fix tests
    
    * Fix capitalization
    
    Co-authored-by: Armen Zambrano G <armenzg@sentry.io>

--- static/app/views/onboarding/targetedOnboarding/platform.tsx
+++ static/app/views/onboarding/targetedOnboarding/platform.tsx
@@ -10,7 +10,8 @@ import testableTransition from 'sentry/utils/testableTransition';
 import StepHeading from 'sentry/views/onboarding/components/stepHeading';
 
 import CreateProjectsFooter from './components/createProjectsFooter';
-import {StepProps, usePersistedOnboardingState} from './types';
+import {StepProps} from './types';
+import {usePersistedOnboardingState} from './utils';
 
 function OnboardingPlatform(props: StepProps) {
   const [selectedPlatforms, setSelectedPlatforms] = useState<PlatformKey[]>([]);
@@ -32,7 +33,7 @@ function OnboardingPlatform(props: StepProps) {
   return (
     <Wrapper>
       <StepHeading step={props.stepIndex}>
-        {t('Select all your projects platform')}
+        {t('Select the platforms you want to monitor')}
       </StepHeading>
       <motion.div
         transition={testableTransition()}
