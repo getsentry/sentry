51118a40b181745a2318fecb31b736f91f3087d3
Scott Cooper <scttcper@users.noreply.github.com>
2022-04-19 11:25:32 -0700

    fix(workflow): Remove hasMetricAlerts from settings route (#33651)

--- static/app/views/settings/projectAlerts/index.tsx
+++ static/app/views/settings/projectAlerts/index.tsx
@@ -1,30 +1,25 @@
-import * as React from 'react';
-import {RouteComponentProps} from 'react-router';
+import {cloneElement, Fragment, isValidElement} from 'react';
+import type {RouteComponentProps} from 'react-router';
 
 import Access from 'sentry/components/acl/access';
-import Feature from 'sentry/components/acl/feature';
 import {Organization} from 'sentry/types';
 
-type Props = {
+interface Props
+  extends RouteComponentProps<{organizationId: string; projectId: string}, {}> {
   children: React.ReactNode;
   organization: Organization;
-} & RouteComponentProps<{organizationId: string; projectId: string}, {}>;
+}
 
 const ProjectAlerts = ({children, organization}: Props) => (
   <Access organization={organization} access={['project:write']}>
     {({hasAccess}) => (
-      <Feature organization={organization} features={['incidents']}>
-        {({hasFeature: hasMetricAlerts}) => (
-          <React.Fragment>
-            {React.isValidElement(children) &&
-              React.cloneElement(children, {
-                organization,
-                canEditRule: hasAccess,
-                hasMetricAlerts,
-              })}
-          </React.Fragment>
-        )}
-      </Feature>
+      <Fragment>
+        {isValidElement(children) &&
+          cloneElement(children, {
+            organization,
+            canEditRule: hasAccess,
+          })}
+      </Fragment>
     )}
   </Access>
 );
