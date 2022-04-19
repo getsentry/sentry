3d73d73f1889a5c586e154e479c02d5cba4d4f44
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-04-19 14:18:23 +0200

    ref(mep): Move transaction thresholds to new tagging system
    In https://github.com/getsentry/relay/pull/1225 we introduced a
    general-purpose tagging system for transaction metrics. This may later
    be extended:
    
    * we will add outlier tagging for histograms soon
    * session metrics could be tagged the same way (unlikely to happen for now, needs changes in Relay)
    
    Here we move away from the purpose-built user satisfaction impl to the
    more generic one

---
+++ src/sentry/relay/config/metric_extraction.py
@@ -0,0 +1,130 @@
+from dataclasses import dataclass
+from typing import Any, List, Literal, Sequence, TypedDict, Union
+
+from sentry.api.endpoints.project_transaction_threshold import DEFAULT_THRESHOLD
+from sentry.models import (
+    Project,
+    ProjectTransactionThreshold,
+    ProjectTransactionThresholdOverride,
+    TransactionMetric,
+)
+
+
+class RuleConditionInner(TypedDict):
+    op: Literal["eq", "gt"]
+    name: str
+    value: Any
+
+
+# mypy does not support recursive types. type definition is a very small subset
+# of the values relay actually accepts
+class RuleCondition(TypedDict):
+    op: Literal["and"]
+    inner: Sequence[RuleConditionInner]
+
+
+class MetricConditionalTaggingRule(TypedDict):
+    condition: RuleCondition
+    targetMetrics: Sequence[str]
+    targetTag: str
+    tagValue: str
+
+
+_TRANSACTION_METRICS_TO_RULE_FIELD = {
+    TransactionMetric.LCP.value: "transaction.measurements.lcp",
+    TransactionMetric.DURATION.value: "transaction.duration",
+}
+
+_SATISFACTION_TARGET_METRICS = frozenset(
+    [
+        "s:transactions/user@none",
+        "d:transactions/duration@millisecond",
+    ]
+)
+
+_SATISFACTION_TARGET_TAG = "satisfaction"
+
+
+@dataclass
+class _DefaultThreshold:
+    metric: TransactionMetric
+    threshold: int
+
+
+_DEFAULT_THRESHOLD = _DefaultThreshold(
+    metric=TransactionMetric[DEFAULT_THRESHOLD["metric"].upper()].value,
+    threshold=int(DEFAULT_THRESHOLD["threshold"]),
+)
+
+
+def get_metric_conditional_tagging_rules(
+    project: Project,
+) -> Sequence[MetricConditionalTaggingRule]:
+    rules: List[MetricConditionalTaggingRule] = []
+
+    for threshold in project.projecttransactionthresholdoverride_set.all():
+        rules.extend(
+            _threshold_to_rules(
+                threshold,
+                [{"op": "eq", "name": "event.transaction", "value": threshold.transaction}],
+            )
+        )
+
+    try:
+        threshold = ProjectTransactionThreshold.objects.get(project=project)
+        rules.extend(_threshold_to_rules(threshold, []))
+    except ProjectTransactionThreshold.DoesNotExist:
+        rules.extend(_threshold_to_rules(_DEFAULT_THRESHOLD, []))
+        pass
+
+    return rules
+
+
+def _threshold_to_rules(
+    threshold: Union[
+        ProjectTransactionThreshold, ProjectTransactionThresholdOverride, _DefaultThreshold
+    ],
+    extra_conditions: Sequence[RuleConditionInner],
+) -> Sequence[MetricConditionalTaggingRule]:
+    frustrated: MetricConditionalTaggingRule = {
+        "condition": {
+            "op": "and",
+            "inner": [
+                {
+                    "op": "gt",
+                    "name": _TRANSACTION_METRICS_TO_RULE_FIELD[threshold.metric],
+                    # The frustration threshold is always four times the threshold
+                    # (see https://docs.sentry.io/product/performance/metrics/#apdex)
+                    "value": threshold.threshold * 4,
+                },
+                *extra_conditions,
+            ],
+        },
+        "targetMetrics": list(_SATISFACTION_TARGET_METRICS),
+        "targetTag": _SATISFACTION_TARGET_TAG,
+        "tagValue": "frustrated",
+    }
+    tolerated: MetricConditionalTaggingRule = {
+        "condition": {
+            "op": "and",
+            "inner": [
+                {
+                    "op": "gt",
+                    "name": _TRANSACTION_METRICS_TO_RULE_FIELD[threshold.metric],
+                    "value": threshold.threshold,
+                },
+                *extra_conditions,
+            ],
+        },
+        "targetMetrics": list(_SATISFACTION_TARGET_METRICS),
+        "targetTag": _SATISFACTION_TARGET_TAG,
+        "tagValue": "tolerated",
+    }
+    satisfied: MetricConditionalTaggingRule = {
+        "condition": {"op": "and", "inner": list(extra_conditions)},
+        "targetMetrics": list(_SATISFACTION_TARGET_METRICS),
+        "targetTag": _SATISFACTION_TARGET_TAG,
+        "tagValue": "satisfied",
+    }
+
+    return [frustrated, tolerated, satisfied]
