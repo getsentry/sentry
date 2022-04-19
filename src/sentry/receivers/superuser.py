92637cacabd9c704156208aed6fe424f73e6112c
Richard Ma <maxiuyuan1998@gmail.com>
2022-04-19 16:01:29 -0700

    enable superuser access upon login only for selfhosted

--- src/sentry/receivers/superuser.py
+++ src/sentry/receivers/superuser.py
@@ -1,4 +1,18 @@
-from django.contrib.auth.signals import user_logged_out
+from django.contrib.auth.signals import user_logged_in, user_logged_out
+
+from sentry.utils.settings import is_self_hosted
+
+
+# Upon login, we automatically want to enable superuser
+# status
+def enable_superuser(request, user, **kwargs):
+    if is_self_hosted():
+        su = getattr(request, "superuser", None)
+        if su:
+            if user.is_superuser:
+                su.set_logged_in(user)
+            else:
+                su._set_logged_out()
 
 
 def disable_superuser(request, user, **kwargs):
@@ -6,5 +20,7 @@ def disable_superuser(request, user, **kwargs):
     if su:
         su.set_logged_out()
 
+
+user_logged_in.connect(enable_superuser, dispatch_uid="enable_superuser", weak=False)
 
 user_logged_out.connect(disable_superuser, dispatch_uid="disable_superuser", weak=False)
