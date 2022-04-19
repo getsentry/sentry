4897f304cfad3ce521473370909b412f2096c770
Dan Fuller <dfuller@sentry.io>
2022-04-19 13:39:07 -0700

    feat(release_health): Move logic from `sum_sessions_and_releases` into `SessionReleaseMonitorBackend` (#33710)
    This moves the logic of fetching release health totals for a list of projects into our
    ReleaseMonitorBackend. This allows us to convert it over to metrics and compare results.

--- src/sentry/release_health/release_monitor/sessions.py
+++ src/sentry/release_health/release_monitor/sessions.py
@@ -6,7 +6,7 @@ from typing import Mapping, Sequence
 
 from snuba_sdk import Column, Condition, Direction, Entity, Granularity, Op, OrderBy, Query
 
-from sentry.release_health.release_monitor.base import BaseReleaseMonitorBackend
+from sentry.release_health.release_monitor.base import BaseReleaseMonitorBackend, Totals
 from sentry.utils import metrics
 from sentry.utils.snuba import raw_snql_query
 
@@ -67,3 +67,75 @@ class SessionReleaseMonitorBackend(BaseReleaseMonitorBackend):
                 )
 
         return aggregated_projects
+
+    def fetch_project_release_health_totals(
+        self, org_id: int, project_ids: Sequence[int]
+    ) -> Totals:
+        start_time = time.time()
+        offset = 0
+        totals: Totals = defaultdict(dict)
+        with metrics.timer(
+            "sentry.tasks.monitor_release_adoption.process_projects_with_sessions.loop"
+        ):
+            while (time.time() - start_time) < self.MAX_SECONDS:
+                with metrics.timer(
+                    "sentry.tasks.monitor_release_adoption.process_projects_with_sessions.query"
+                ):
+                    query = (
+                        Query(
+                            dataset="sessions",
+                            match=Entity("sessions"),
+                            select=[
+                                Column("sessions"),
+                            ],
+                            groupby=[
+                                Column("org_id"),
+                                Column("project_id"),
+                                Column("release"),
+                                Column("environment"),
+                            ],
+                            where=[
+                                Condition(
+                                    Column("started"),
+                                    Op.GTE,
+                                    datetime.utcnow() - timedelta(hours=6),
+                                ),
+                                Condition(Column("started"), Op.LT, datetime.utcnow()),
+                                Condition(Column("org_id"), Op.EQ, org_id),
+                                Condition(Column("project_id"), Op.IN, project_ids),
+                            ],
+                            granularity=Granularity(21600),
+                            orderby=[
+                                OrderBy(Column("org_id"), Direction.ASC),
+                                OrderBy(Column("project_id"), Direction.ASC),
+                            ],
+                        )
+                        .set_limit(self.CHUNK_SIZE + 1)
+                        .set_offset(offset)
+                    )
+
+                    data = raw_snql_query(
+                        query, referrer="tasks.process_projects_with_sessions.session_count"
+                    )["data"]
+                    count = len(data)
+                    more_results = count > self.CHUNK_SIZE
+                    offset += self.CHUNK_SIZE
+
+                    if more_results:
+                        data = data[:-1]
+
+                    for row in data:
+                        row_totals = totals[row["project_id"]].setdefault(
+                            row["environment"], {"total_sessions": 0, "releases": defaultdict(int)}
+                        )
+                        row_totals["total_sessions"] += row["sessions"]
+                        row_totals["releases"][row["release"]] += row["sessions"]
+
+                if not more_results:
+                    break
+            else:
+                logger.error(
+                    "process_projects_with_sessions.loop_timeout",
+                    extra={"org_id": org_id, "project_ids": project_ids},
+                )
+        return totals
