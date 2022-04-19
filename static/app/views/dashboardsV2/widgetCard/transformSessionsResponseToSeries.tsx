4f246b0edd1e261e30f2f9eda1063b41bbf1469c
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-04-19 10:58:31 -0400

    feat(dashboards): Make RH series names closer to events
    Massage the format of the series names so it resembles
    the events stats format.

--- static/app/views/dashboardsV2/widgetCard/transformSessionsResponseToSeries.tsx
+++ static/app/views/dashboardsV2/widgetCard/transformSessionsResponseToSeries.tsx
@@ -5,14 +5,17 @@ export function transformSessionsResponseToSeries(
   response: SessionApiResponse | null,
   queryAlias?: string
 ): Series[] {
+  function getSeriesName(field: string, group: SessionApiResponse['groups'][number]) {
+    const groupName = Object.entries(group.by)
+      .map(([_, value]) => `${value}`)
+      .join(', ');
+    const seriesName = groupName ? `${groupName} : ${field}` : field;
+    return `${queryAlias ? `${queryAlias} > ` : ''}${seriesName}`;
+  }
   return (
     response?.groups.flatMap(group =>
       Object.keys(group.series).map(field => ({
-        seriesName: `${queryAlias ? `${queryAlias}: ` : ''}${field}${Object.entries(
-          group.by
-        )
-          .map(([key, value]) => `|${key}:${value}`)
-          .join('')}`,
+        seriesName: getSeriesName(field, group),
         data: response.intervals.map((interval, index) => ({
           name: interval,
           value: group.series[field][index] ?? 0,
