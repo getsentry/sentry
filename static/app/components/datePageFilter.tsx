48703182866b07af6a70daf16c3e57ce00699d55
Vu Luong <vuluongj20@gmail.com>
2022-04-19 16:53:52 -0700

    ref(pageFilters): Separate datePageFilter from pageTimeRangeSelector (#33768)

--- static/app/components/datePageFilter.tsx
+++ static/app/components/datePageFilter.tsx
@@ -6,7 +6,6 @@ import PageFilterDropdownButton from 'sentry/components/organizations/pageFilter
 import TimeRangeSelector, {
   ChangeData,
 } from 'sentry/components/organizations/timeRangeSelector';
-import PageTimeRangeSelector from 'sentry/components/pageTimeRangeSelector';
 import {IconCalendar} from 'sentry/icons';
 import PageFiltersStore from 'sentry/stores/pageFiltersStore';
 import {useLegacyStore} from 'sentry/stores/useLegacyStore';
@@ -68,7 +67,7 @@ function DatePageFilter({router, resetParamsOnChange, ...props}: Props) {
   };
 
   return (
-    <StyledPageTimeRangeSelector
+    <TimeRangeSelector
       organization={organization}
       start={start}
       end={end}
@@ -78,21 +77,11 @@ function DatePageFilter({router, resetParamsOnChange, ...props}: Props) {
       label={<IconCalendar color="textColor" />}
       customDropdownButton={customDropdownButton}
       showPin
+      detached
       {...props}
     />
   );
 }
-
-const StyledPageTimeRangeSelector = styled(PageTimeRangeSelector)`
-  flex-grow: 0;
-  flex-shrink: 0;
-  flex-basis: fit-content;
-  position: relative;
-  height: 100%;
-  background: ${p => p.theme.background};
-  border: none;
-  box-shadow: none;
-`;
 
 const TitleContainer = styled('div')`
   overflow: hidden;
