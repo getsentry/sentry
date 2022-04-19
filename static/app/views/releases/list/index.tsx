7f9f89406237cdc34ac3429665570981fa74dd17
mikellykels <kelly@sentry.io>
2022-04-19 12:40:02 -0700

    fix(workflow): Handle filtering release list by date
    Currently, the date picker on the releases list only changes the graph. This adds the ability to filter by date using the global selection header.
    
    FIXES WOR-1786

--- static/app/views/releases/list/index.tsx
+++ static/app/views/releases/list/index.tsx
@@ -80,13 +80,17 @@ class ReleasesList extends AsyncView<Props, State> {
 
   getEndpoints(): ReturnType<AsyncView['getEndpoints']> {
     const {organization, location} = this.props;
-    const {statsPeriod} = location.query;
+    const {statsPeriod, start, end, utc} = location.query;
     const activeSort = this.getSort();
     const activeStatus = this.getStatus();
 
     const query = {
       ...pick(location.query, ['project', 'environment', 'cursor', 'query', 'sort']),
       summaryStatsPeriod: statsPeriod,
+      ...(statsPeriod && {statsPeriod}),
+      start,
+      end,
+      utc,
       per_page: 20,
       flatten: activeSort === ReleasesSortOption.DATE ? 0 : 1,
       adoptionStages: 1,
