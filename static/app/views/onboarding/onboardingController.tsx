bd0e1a182dbad80b7f2a5cd01125756631050c83
Zhixing Zhang <me@neoto.xin>
2022-04-20 09:03:18 -0700

    Launch TargetedOnboardingMultiSelectExperiment (#33708)

--- static/app/views/onboarding/onboardingController.tsx
+++ static/app/views/onboarding/onboardingController.tsx
@@ -1,5 +1,6 @@
 import {ComponentPropsWithoutRef} from 'react';
 
+import {logExperiment} from 'sentry/utils/analytics';
 import withOrganization from 'sentry/utils/withOrganization';
 
 import TargetedOnboarding from './targetedOnboarding/onboarding';
@@ -8,6 +9,10 @@ import Onboarding from './onboarding';
 type Props = Omit<ComponentPropsWithoutRef<typeof Onboarding>, 'projects'>;
 
 function OnboardingController({...rest}: Props) {
+  logExperiment({
+    key: 'TargetedOnboardingMultiSelectExperiment',
+    organization: rest.organization,
+  });
   if (rest.organization?.experiments.TargetedOnboardingMultiSelectExperiment) {
     return <TargetedOnboarding {...rest} />;
   }
