bb3568648d3cae54272a4c29dd45adffb343e899
Zhixing Zhang <me@neoto.xin>
2022-04-20 14:38:28 -0700

    up9date copy

--- static/app/views/onboarding/targetedOnboarding/platform.tsx
+++ static/app/views/onboarding/targetedOnboarding/platform.tsx
@@ -46,7 +46,8 @@ function OnboardingPlatform(props: StepProps) {
         <p>
           {tct(
             `Variety is the spice of application monitoring. Sentry SDKs integrate
-           with most languages and platforms your developer heart desires.
+           with most of the languages, platforms, and frameworks your developer heart desires.
+           Get started by selecting platforms for your Front End, Back End and/or Mobile applications.
            [link:View the full list].`,
             {link: <ExternalLink href="https://docs.sentry.io/platforms/" />}
           )}
