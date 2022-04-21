4e9a62f649f0d418e3940652a44a4332e7418b2c
Richard Ma <maxiuyuan1998@gmail.com>
2022-04-21 12:30:32 -0700

    added button to let COPS and CSM not fill out the form (#33817)

--- static/app/components/superuserAccessForm.tsx
+++ static/app/components/superuserAccessForm.tsx
@@ -1,22 +1,46 @@
 import {Component} from 'react';
 import styled from '@emotion/styled';
 
+import {Client} from 'sentry/api';
 import Alert from 'sentry/components/alert';
 import Form from 'sentry/components/forms/form';
 import Hook from 'sentry/components/hook';
 import ThemeAndStyleProvider from 'sentry/components/themeAndStyleProvider';
 import {ErrorCodes} from 'sentry/constants/superuserAccessErrors';
 import {t} from 'sentry/locale';
+import space from 'sentry/styles/space';
+import withApi from 'sentry/utils/withApi';
+
+import Button from './button';
+
+type Props = {
+  api: Client;
+};
 
 type State = {
   error: boolean;
   errorType: string;
 };
 
-class SuperuserAccessForm extends Component<State> {
+class SuperuserAccessForm extends Component<Props, State> {
   state: State = {
     error: false,
     errorType: '',
+  };
+
+  handleSubmit = async () => {
+    const {api} = this.props;
+    const data = {
+      isSuperuserModal: true,
+      superuserAccessCategory: 'cops_csm',
+      superuserReason: 'COPS and CSM use',
+    };
+    try {
+      await api.requestPromise('/auth/', {method: 'PUT', data});
+      this.handleSuccess();
+    } catch (err) {
+      this.handleError(err);
+    }
   };
 
   handleSuccess = () => {
@@ -51,6 +75,11 @@ class SuperuserAccessForm extends Component<State> {
           onSubmitSuccess={this.handleSuccess}
           onSubmitError={this.handleError}
           initialData={{isSuperuserModal: true}}
+          extraButton={
+            <BackWrapper>
+              <Button onClick={this.handleSubmit}>{t('COPS/CSM')}</Button>
+            </BackWrapper>
+          }
           resetOnError
         >
           {error && (
@@ -69,4 +98,9 @@ const StyledAlert = styled(Alert)`
   margin-bottom: 0;
 `;
 
-export default SuperuserAccessForm;
+const BackWrapper = styled('div')`
+  width: 100%;
+  margin-left: ${space(4)};
+`;
+
+export default withApi(SuperuserAccessForm);
