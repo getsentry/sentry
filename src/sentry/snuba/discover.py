5dd0d112d5ace54c11d076a279776eac71a8aee0
Nar Saynorath <nar.saynorath@sentry.io>
2022-04-21 16:08:34 -0400

    Rename parameter and add test to assert parameter is called with True

--- src/sentry/snuba/discover.py
+++ src/sentry/snuba/discover.py
@@ -209,7 +209,7 @@ def query(
     referrer=None,
     auto_fields=False,
     auto_aggregations=False,
-    auto_equation_fields=False,
+    include_equation_fields=False,
     allow_metric_aggregates=False,
     use_aggregate_conditions=False,
     conditions=None,
@@ -238,7 +238,7 @@ def query(
     auto_fields (bool) Set to true to have project + eventid fields automatically added.
     auto_aggregations (bool) Whether aggregates should be added automatically if they're used
                     in conditions, and there's at least one aggregate already.
-    auto_equation_fields (bool) Whether fields should be added automatically if they're used in
+    include_equation_fields (bool) Whether fields should be added automatically if they're used in
                     equations
     allow_metric_aggregates (bool) Ignored here, only used in metric enhanced performance
     use_aggregate_conditions (bool) Set to true if aggregates conditions should be used at all.
@@ -268,7 +268,7 @@ def query(
             functions_acl=functions_acl,
             limit=limit,
             offset=offset,
-            equation_config={"auto_add": auto_equation_fields},
+            equation_config={"auto_add": include_equation_fields},
         )
         if extra_snql_condition is not None:
             builder.add_conditions(extra_snql_condition)
@@ -291,7 +291,7 @@ def query(
         orderby,
         auto_fields,
         auto_aggregations,
-        auto_equation_fields,
+        include_equation_fields,
         use_aggregate_conditions,
         conditions,
         functions_acl,
@@ -335,7 +335,7 @@ def prepare_discover_query(
     orderby=None,
     auto_fields=False,
     auto_aggregations=False,
-    auto_equation_fields=False,
+    include_equation_fields=False,
     use_aggregate_conditions=False,
     conditions=None,
     functions_acl=None,
@@ -356,7 +356,7 @@ def prepare_discover_query(
         resolved_columns = selected_columns
         if equations is not None:
             resolved_equations, resolved_columns, _ = resolve_equation_list(
-                equations, selected_columns, auto_add=auto_equation_fields
+                equations, selected_columns, auto_add=include_equation_fields
             )
         else:
             resolved_equations = []
@@ -735,7 +735,7 @@ def top_events_timeseries(
                 auto_aggregations=True,
                 use_aggregate_conditions=True,
                 use_snql=use_snql,
-                auto_equation_fields=True,
+                include_equation_fields=True,
             )
 
     if use_snql:
