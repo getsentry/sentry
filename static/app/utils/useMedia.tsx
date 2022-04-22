040fcb8fda05545d0d8c42020a25bb8a2fe7eb27
JonasBa <jonas.badalic@sentry.io>
2022-04-22 09:08:28 -0400

    fix(matchmedia): safe access calls to check for browser support

--- static/app/utils/useMedia.tsx
+++ static/app/utils/useMedia.tsx
@@ -4,11 +4,11 @@ import {useEffect, useState} from 'react';
  * Hook that updates when a media query result changes
  */
 export default function useMedia(query: string) {
-  const [state, setState] = useState(() => window.matchMedia(query).matches);
+  const [state, setState] = useState(() => window?.matchMedia(query)?.matches);
 
   useEffect(() => {
     let mounted = true;
-    const mql = window.matchMedia(query);
+    const mql = window?.matchMedia?.(query);
     const onChange = () => {
       if (!mounted) {
         return;
