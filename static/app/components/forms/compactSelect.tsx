1fc34a7673211f227adb49507a2f4a7f3b44630e
Tony Xiao <txiao@sentry.io>
2022-04-25 14:57:05 -0400

    fix(ui): Update compact select label when options change (#33890)
    When the options change, the label needs to be updated as well.

--- static/app/components/forms/compactSelect.tsx
+++ static/app/components/forms/compactSelect.tsx
@@ -200,7 +200,7 @@ function CompactSelect<OptionType extends GeneralSelectValue = GeneralSelectValu
     const newValue = valueProp ?? internalValue;
     const newLabel = getLabel(newValue);
     setLabel(newLabel);
-  }, [valueProp ?? internalValue]);
+  }, [valueProp ?? internalValue, options]);
 
   // Calculate & update the trigger button's width, to be used as the
   // overlay's min-width
