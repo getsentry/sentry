2e0d2dcb6b82d308926b55b81d3bf9c10268289f
asottile-sentry <103459774+asottile-sentry@users.noreply.github.com>
2022-04-26 15:46:05 -0400

    fix: improve apple m1 detection for newer OS versions (#33977)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -4,12 +4,12 @@ These settings act as the default (base) settings for the Sentry-provided web-se
 
 import os
 import os.path
+import platform
 import re
 import socket
 import sys
 import tempfile
 from datetime import timedelta
-from platform import platform
 from urllib.parse import urlparse
 
 from django.conf.global_settings import *  # NOQA
@@ -1816,7 +1816,10 @@ def build_cdc_postgres_init_db_volume(settings):
     )
 
 
-APPLE_ARM64 = platform().startswith("mac") and platform().endswith("arm64-arm-64bit")
+# platform.processor() changed at some point between these:
+# 11.2.3: arm
+# 12.3.1: arm64
+APPLE_ARM64 = sys.platform == "darwin" and platform.processor() in {"arm", "arm64"}
 
 SENTRY_DEVSERVICES = {
     "redis": lambda settings, options: (
