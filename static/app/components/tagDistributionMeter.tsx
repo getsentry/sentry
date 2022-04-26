f56b0b2c8367a8d0113f734fda9cc4bd2159166a
Mark Story <mark@mark-story.com>
2022-04-26 09:26:45 -0400

    fix(ui) Fix tag bar segments having round corners. (#33933)
    An inadvertant side-effect of #33873 was that tag bar links were also
    rounded. This reverts the rounding on segments but retains the rounding
    on the outer bar shape.

--- static/app/components/tagDistributionMeter.tsx
+++ static/app/components/tagDistributionMeter.tsx
@@ -220,7 +220,7 @@ const TagSummary = styled('div')`
 const SegmentBar = styled('div')`
   display: flex;
   overflow: hidden;
-  border-radius: 2px;
+  border-radius: ${p => p.theme.borderRadius};
 `;
 
 const Title = styled('div')`
@@ -271,4 +271,5 @@ const Segment = styled(Link, {shouldForwardProp: isPropValid})<SegmentValue>`
   color: inherit;
   outline: none;
   background-color: ${p => COLORS[p.index]};
+  border-radius: 0;
 `;
