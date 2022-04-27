724df1615f3d42a687f5ef3222e40595d2220530
Dora Chan <dora.l.chan@gmail.com>
2022-04-27 16:49:45 -0700

    fixing search in trends

--- static/app/views/performance/trends/content.tsx
+++ static/app/views/performance/trends/content.tsx
@@ -275,7 +275,7 @@ class TrendsContent extends React.Component<Props, State> {
                     </DropdownItem>
                   ))}
                 </DropdownControl>
-                <SearchBar
+                <StyledSearchBar
                   searchSource="trends"
                   organization={organization}
                   projectIds={trendView.project}
@@ -362,6 +362,10 @@ const FilterActions = styled('div')`
     grid-template-columns: 1fr auto auto;
     grid-template-rows: auto auto;
   }
+`;
+
+const StyledSearchBar = styled(SearchBar)`
+  grid-column: 1/4;
 `;
 
 const ListContainer = styled('div')`
