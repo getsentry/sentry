013e3e38c3b695f4656461cf602a99ac15b4f69a
Zhixing Zhang <me@neoto.xin>
2022-04-27 16:07:50 -0700

    use convert args

--- src/sentry/api/endpoints/client_state.py
+++ src/sentry/api/endpoints/client_state.py
@@ -36,10 +36,18 @@ class ClientStateEndpoint(OrganizationEndpoint):
         self.client = get_redis_client()
         super().__init__(**options)
 
-    def get(self, request: Request, organization, category) -> Response:
+    def convert_args(self, request: Request, organization_slug, *args, **kwargs):
+        (args, kwargs) = super().convert_args(request, organization_slug, *args, **kwargs)
+        organization = kwargs["organization"]
+        category = kwargs["category"]
         key = get_client_state_key(organization.slug, category, request.user)
         if not key:
             raise NotFound(detail="Category not found")
+
+        kwargs["key"] = key
+        return (args, kwargs)
+
+    def get(self, request: Request, organization, category, key) -> Response:
         value = self.client.get(key)
         if value:
             response = HttpResponse(value)
@@ -48,19 +56,13 @@ class ClientStateEndpoint(OrganizationEndpoint):
         else:
             return Response({})
 
-    def put(self, request: Request, organization, category):
-        key = get_client_state_key(organization.slug, category, request.user)
-        if not key:
-            raise NotFound(detail="Category not found")
+    def put(self, request: Request, organization, category, key):
         data_to_write = json.dumps(request.data)
         if len(data_to_write) > STATE_CATEGORIES[category]["max_payload_size"]:
             return Response(status=413)
         self.client.setex(key, STATE_CATEGORIES[category]["ttl"], data_to_write)
         return Response(status=201)
 
-    def delete(self, request: Request, organization, category):
-        key = get_client_state_key(organization.slug, category, request.user)
-        if not key:
-            raise NotFound(detail="Category not found")
+    def delete(self, request: Request, organization, category, key):
         self.client.delete(key)
         return Response(status=204)
