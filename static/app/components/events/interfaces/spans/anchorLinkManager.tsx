bed2a057d62051af244430df2ed330271385c055
Ash Anand <ash.anand@sentry.io>
2022-04-28 12:35:06 -0400

    Add todo comment

--- static/app/components/events/interfaces/spans/anchorLinkManager.tsx
+++ static/app/components/events/interfaces/spans/anchorLinkManager.tsx
@@ -29,6 +29,7 @@ export class Provider extends Component<Props> {
       // If the anchored span is part of a group, need to call scrollToHash again, since the initial fn() call will only expand the group.
       // The function gets registered again after the group is expanded, which will allow the page to scroll to the span
       if (isSpanInGroup) {
+        // TODO: There's a possibility that this trick may not work when we upgrade to React 18
         setTimeout(() => this.scrollFns.get(hash)?.fn());
       }
     }
