4f896ddd2ec849810781d2c56c2eeb6a4b278db2
Zhixing Zhang <me@neoto.xin>
2022-04-28 13:30:58 -0700

    add url validation

--- src/sentry/web/frontend/home.py
+++ src/sentry/web/frontend/home.py
@@ -2,6 +2,7 @@ from django.http import HttpResponseRedirect
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from sentry.utils.auth import is_valid_redirect
 from sentry.utils.client_state import get_client_state_redirect_uri
 from sentry.web.frontend.base import BaseView
 
@@ -12,7 +13,9 @@ class HomeView(BaseView):
         organization = self.get_active_organization(request)
         if organization:
             redirect_uri = get_client_state_redirect_uri(organization.slug, None)
-            if redirect_uri:
+            if redirect_uri and is_valid_redirect(
+                redirect_uri, allowed_hosts=(request.get_host(),)
+            ):
                 return HttpResponseRedirect(redirect_uri)
 
         return self.redirect_to_org(request)
