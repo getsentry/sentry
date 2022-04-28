8e475a6b042c377c98ae654a6e140ec47aab9730
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-04-28 16:54:17 +0200

    feat(sessions): Pagination for metrics-based sessions v2 (#33971)
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- src/sentry/api/endpoints/organization_sessions.py
+++ src/sentry/api/endpoints/organization_sessions.py
@@ -8,24 +8,39 @@ from rest_framework.response import Response
 
 from sentry import release_health
 from sentry.api.bases import NoProjects, OrganizationEventsEndpointBase
+from sentry.api.paginator import GenericOffsetPaginator
 from sentry.api.utils import get_date_range_from_params
-from sentry.snuba.sessions_v2 import InvalidField, InvalidParams, QueryDefinition
+from sentry.models import Organization
+from sentry.snuba.sessions_v2 import SNUBA_LIMIT, InvalidField, InvalidParams, QueryDefinition
+from sentry.utils.cursors import Cursor, CursorResult
 
 
 # NOTE: this currently extends `OrganizationEventsEndpointBase` for `handle_query_errors` only, which should ideally be decoupled from the base class.
 class OrganizationSessionsEndpoint(OrganizationEventsEndpointBase):
     def get(self, request: Request, organization) -> Response:
-        with self.handle_query_errors():
-            with sentry_sdk.start_span(op="sessions.endpoint", description="build_sessions_query"):
-                query = self.build_sessions_query(request, organization)
+        def data_fn(offset: int, limit: int):
+            with self.handle_query_errors():
+                with sentry_sdk.start_span(
+                    op="sessions.endpoint", description="build_sessions_query"
+                ):
+                    query = self.build_sessions_query(
+                        request, organization, offset=offset, limit=limit
+                    )
 
-            result = release_health.run_sessions_query(
-                organization.id, query, span_op="sessions.endpoint"
-            )
+                return release_health.run_sessions_query(
+                    organization.id, query, span_op="sessions.endpoint"
+                )
 
-        return Response(result, status=200)
+        return self.paginate(
+            request,
+            paginator=SessionsDataSeriesPaginator(data_fn=data_fn),
+            default_per_page=SNUBA_LIMIT,
+            max_per_page=SNUBA_LIMIT,
+        )
 
-    def build_sessions_query(self, request: Request, organization):
+    def build_sessions_query(
+        self, request: Request, organization: Organization, offset: int, limit: int
+    ):
         try:
             params = self.get_filter_params(request, organization, date_filter_optional=True)
         except NoProjects:
@@ -42,6 +57,8 @@ class OrganizationSessionsEndpoint(OrganizationEventsEndpointBase):
         return QueryDefinition(
             query_params,
             params,
+            offset=offset,
+            limit=limit,
             query_config=query_config,
         )
 
@@ -53,3 +70,23 @@ class OrganizationSessionsEndpoint(OrganizationEventsEndpointBase):
                 yield
         except (InvalidField, InvalidParams, NoProjects) as error:
             raise ParseError(detail=str(error))
+
+
+class SessionsDataSeriesPaginator(GenericOffsetPaginator):
+    def get_result(self, limit, cursor=None):
+        assert limit > 0
+        offset = cursor.offset if cursor is not None else 0
+        data = self.data_fn(offset=offset, limit=limit + 1)
+
+        if isinstance(data.get("groups"), list):
+            has_more = len(data["groups"]) == limit + 1
+            if has_more:
+                data["groups"].pop()
+        else:
+            raise NotImplementedError
+
+        return CursorResult(
+            data,
+            prev=Cursor(0, max(0, offset - limit), True, offset > 0),
+            next=Cursor(0, max(0, offset + limit), False, has_more),
+        )
