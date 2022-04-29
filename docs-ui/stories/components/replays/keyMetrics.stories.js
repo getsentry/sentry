5d5de274909b6e39c7fe36cc3cf09fbb1db3fc09
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-04-28 22:38:24 -0700

    cleanup copypasta

--- docs-ui/stories/components/replays/keyMetrics.stories.js
+++ docs-ui/stories/components/replays/keyMetrics.stories.js
@@ -5,20 +5,11 @@ export default {
   component: KeyMetrics,
 };
 
-const Template = () => (
+export const Default = () => (
   <KeyMetrics>
     <KeyMetricData keyName="Timestamp" value="9 hours ago" />
     <KeyMetricData keyName="Duration" value="1min 52s" />
     <KeyMetricData keyName="Errors" value="2" />
   </KeyMetrics>
 );
-
-export const _ReplayController = Template.bind({});
-_ReplayController.parameters = {
-  docs: {
-    description: {
-      story:
-        'ReplayController is a component that contains play/pause buttons for the replay timeline',
-    },
-  },
-};
+Default.storyName = 'default';
