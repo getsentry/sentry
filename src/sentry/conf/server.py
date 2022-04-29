5e44389109bc0710225c76e27f3a5c332c904d32
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2022-04-29 09:47:10 -0400

    feat(dashboard): adds feature flag for widget viewer modal minimap (#34080)
    Feature flag for dashboard widget viewer minimap feature

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1052,6 +1052,8 @@ SENTRY_FEATURES = {
     "organizations:dashboards-releases": False,
     # Enable widget viewer modal in dashboards
     "organizations:widget-viewer-modal": False,
+    # Enable minimap in the widget viewer modal in dashboards
+    "organizations:widget-viewer-modal-minimap": False,
     # Enable experimental performance improvements.
     "organizations:enterprise-perf": False,
     # Enable the API to importing CODEOWNERS for a project
