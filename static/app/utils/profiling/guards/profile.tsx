cbb1cbe87a56949963aa1cfd74bd0a94257a1172
JonasBa <jonas.badalic@sentry.io>
2022-05-01 08:45:48 -0400

    feat(profiling): fix type guards

--- static/app/utils/profiling/guards/profile.tsx
+++ static/app/utils/profiling/guards/profile.tsx
@@ -19,15 +19,30 @@ export function isJSProfile(profile: any): profile is JSSelfProfiling.Trace {
   return !('type' in profile) && Array.isArray(profile.resources);
 }
 
-export function isChromeTraceFormat(input: any): input is ChromeTrace.ProfileType {
-  return isChromeTraceArrayFormat(input) || isChromeTraceObjectFormat(input);
-}
-
 export function isChromeTraceObjectFormat(input: any): input is ChromeTrace.ObjectFormat {
   return typeof input === 'object' && 'traceEvents' in input;
 }
 
-export function isChromeTraceArrayFormat(input: any): input is ChromeTrace.ArrayFormat {
-  // @TODO we need to check if the profile actually includes the v8 profile nodes.
-  return Array.isArray(input);
+export function isChromeTraceArrayFormat(input: any): input is ChromeTrace.ProfileType {
+  return (
+    Array.isArray(input) && input.find(p => p.ph === 'P' && p.name === 'ProfileChunk')
+  );
+}
+
+// Typescript uses only a subset of the event types, so we need to
+// inspect the contents of the trace to determine the type of the profile.
+export function isTypescriptChromeTraceArrayFormat(
+  input: any
+): input is ChromeTrace.ArrayFormat {
+  return (
+    Array.isArray(input) && !input.some(p => p.ph === 'P' && p.name === 'ProfileChunk')
+  );
+}
+
+export function isChromeTraceFormat(input: any): input is ChromeTrace.ArrayFormat {
+  return (
+    isTypescriptChromeTraceArrayFormat(input) ||
+    isChromeTraceObjectFormat(input) ||
+    isChromeTraceArrayFormat(input)
+  );
 }
