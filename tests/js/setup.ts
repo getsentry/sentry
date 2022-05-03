1efa56b48e5eaf358050f4f688c37134b6e63e2f
Scott Cooper <scttcper@gmail.com>
2022-05-03 13:00:45 -0700

    add basic style functionality, ignore pointer-events from globals

--- tests/js/setup.ts
+++ tests/js/setup.ts
@@ -273,15 +273,29 @@ Object.defineProperty(window, 'location', {
 
 // The JSDOM implementation is too slow
 // Especially for dropdowns that try to position themselves
-// https://github.com/jsdom/jsdom/issues/3234
+// perf issue - https://github.com/jsdom/jsdom/issues/3234
 Object.defineProperty(window, 'getComputedStyle', {
-  value: () => ({
-    getPropertyValue: () => {
-      return undefined;
-    },
-  }),
+  value: (el: HTMLElement) => {
+    /**
+     * This is based on the jsdom implementation of getComputedStyle
+     * https://github.com/jsdom/jsdom/blob/9dae17bf0ad09042cfccd82e6a9d06d3a615d9f4/lib/jsdom/browser/Window.js#L779-L820
+     *
+     * It is missing global style parsing and will only return styles applied directly to an element.
+     * Will not return styles that are global or from emotion
+     */
+    const declaration = new CSSStyleDeclaration();
+    const {style} = el;
+
+    Array.prototype.forEach.call(style, (property: string) => {
+      declaration.setProperty(
+        property,
+        style.getPropertyValue(property),
+        style.getPropertyPriority(property)
+      );
+    });
+
+    return declaration;
+  },
   configurable: true,
   writable: true,
 });
-// Related to removing getComputedStyle
-jest.mock('line-height');
