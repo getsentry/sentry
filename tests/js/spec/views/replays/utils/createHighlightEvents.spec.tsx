af0e54d683a055d204dcb63ba22e7791afdc824b
Billy Vong <billy@sentry.io>
2022-05-03 14:45:16 -0400

    test types

--- tests/js/spec/views/replays/utils/createHighlightEvents.spec.tsx
+++ tests/js/spec/views/replays/utils/createHighlightEvents.spec.tsx
@@ -1,47 +1,51 @@
 import createHighlightEvents from 'sentry/views/replays/utils/createHighlightEvents';
 
+function createSpan(extra: Record<string, any>) {
+  return {
+    span_id: 'spanid',
+    start_timestamp: 1,
+    timestamp: 2,
+    trace_id: 'traceid',
+    data: {},
+    ...extra,
+  };
+}
+
 it('returns a list of replay events for highlights', function () {
   const events = [
-    {
+    createSpan({
       op: 'foo',
-      data: {},
-      start_timestamp: 1,
-    },
-    {
+    }),
+    createSpan({
       op: 'largest-contentful-paint',
       data: {
         nodeId: 2,
       },
-      start_timestamp: 1,
-    },
-    {
+    }),
+    createSpan({
       op: 'largest-contentful-paint',
       data: {
         nodeId: null,
       },
-      start_timestamp: 1,
-    },
-    {
+    }),
+    createSpan({
       op: 'largest-contentful-paint',
       data: {
-        nodeId: -0,
+        nodeId: 0,
       },
-      start_timestamp: 1,
-    },
-    {
+    }),
+    createSpan({
       op: 'largest-contentful-paint',
       data: {
         nodeId: -1,
       },
-      start_timestamp: 1,
-    },
-    {
+    }),
+    createSpan({
       op: 'largest-contentful-paint',
       data: {
         nodeId: 10,
       },
-      start_timestamp: 1,
-    },
+    }),
   ];
 
   const results = createHighlightEvents(events);
--- tests/js/spec/views/replays/utils/mergeAndSortEvents.spec.tsx
+++ tests/js/spec/views/replays/utils/mergeAndSortEvents.spec.tsx
@@ -3,13 +3,13 @@ import mergeAndSortEvents from 'sentry/views/replays/utils/mergeAndSortEvents';
 it('merges and sorts multiple lists of events', function () {
   expect(
     mergeAndSortEvents(
-      [{type: 0, timestamp: 5}],
-      [{type: 1, timestamp: 1}],
-      [{type: 2, timestamp: 3}]
+      [{type: 0, data: {}, timestamp: 5}],
+      [{type: 1, data: {}, timestamp: 1}],
+      [{type: 2, data: {}, timestamp: 3}]
     )
   ).toEqual([
-    {type: 1, timestamp: 1},
-    {type: 2, timestamp: 3},
-    {type: 0, timestamp: 5},
+    {type: 1, data: {}, timestamp: 1},
+    {type: 2, data: {}, timestamp: 3},
+    {type: 0, data: {}, timestamp: 5},
   ]);
 });
