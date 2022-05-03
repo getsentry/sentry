442e95b15d22916859be1aac4fb99527bc5f309e
SDaian <scuarissid@gmail.com>
2022-05-03 18:09:48 -0300

    feat(replays): create event chapters component

--- static/app/views/replays/detail/userActionsNavigator.tsx
+++ static/app/views/replays/detail/userActionsNavigator.tsx
@@ -1,42 +1,95 @@
 import React from 'react';
+import styled from '@emotion/styled';
 
-import EventEntry from 'sentry/components/events/eventEntry';
-import {Entry, Event} from 'sentry/types/event';
-import useOrganization from 'sentry/utils/useOrganization';
-import {useRouteContext} from 'sentry/utils/useRouteContext';
+import Category from 'sentry/components/events/interfaces/breadcrumbs/breadcrumb/category';
+import Time from 'sentry/components/events/interfaces/breadcrumbs/breadcrumb/time';
+import Type from 'sentry/components/events/interfaces/breadcrumbs/breadcrumb/type';
+import {transformCrumbs} from 'sentry/components/events/interfaces/breadcrumbs/utils';
+import {
+  Panel as BasePanel,
+  PanelBody as BasePanelBody,
+  PanelHeader as BasePanelHeader,
+  PanelItem,
+} from 'sentry/components/panels';
+import space from 'sentry/styles/space';
+import {RawCrumb} from 'sentry/types/breadcrumbs';
+import {Event} from 'sentry/types/event';
 
 type Props = {
-  entry: Entry | undefined;
+  crumbs: RawCrumb[];
   event: Event | undefined;
 };
 
-function UserActionsNavigator({event, entry}: Props) {
-  // TODO(replay): New User Actions widget replaces this breadcrumb view
-
-  const {routes, router} = useRouteContext();
-  const organization = useOrganization();
-
-  if (!entry || !event) {
+function UserActionsNavigator({event, crumbs}: Props) {
+  if (!event) {
     return null;
   }
 
-  const projectSlug = getProjectSlug(event);
+  const transformedCrumbs = transformCrumbs(crumbs);
 
   return (
-    <EventEntry
-      projectSlug={projectSlug}
-      // group={group}
-      organization={organization}
-      event={event}
-      entry={entry}
-      route={routes[routes.length - 1]}
-      router={router}
-    />
+    <Panel>
+      <PanelHeader>Event Chapters</PanelHeader>
+
+      <PanelBody>
+        <div>
+          {transformedCrumbs.map(item => (
+            <PanelItemCenter key={item.id}>
+              <Wrapper>
+                <Type
+                  type={item.type}
+                  color={item.color}
+                  description={item.description}
+                />
+                <Category category={item.category} searchTerm="" />
+              </Wrapper>
+              {/* Probably we need to use or create another component to show the timestamp relative */}
+              <Time
+                timestamp={event.dateReceived}
+                relativeTime={item.timestamp}
+                searchTerm=""
+              />
+            </PanelItemCenter>
+          ))}
+        </div>
+      </PanelBody>
+    </Panel>
   );
 }
 
-function getProjectSlug(event: Event) {
-  return event.projectSlug || event['project.name']; // seems janky
-}
-
 export default UserActionsNavigator;
+
+const Panel = styled(BasePanel)`
+  width: 100%;
+  display: grid;
+  margin-bottom: 0;
+`;
+
+const PanelHeader = styled(BasePanelHeader)`
+  background-color: #fff;
+  border-bottom: none;
+  font-size: ${p => p.theme.fontSizeSmall};
+  color: ${p => p.theme.gray300};
+  text-transform: capitalize;
+  padding: ${space(1.5)} ${space(2)} ${space(0.5)};
+`;
+
+const PanelBody = styled(BasePanelBody)`
+  overflow-y: auto;
+`;
+
+const PanelItemCenter = styled(PanelItem)`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  border-bottom: none;
+  padding: ${space(1)} ${space(1.5)};
+`;
+
+const Wrapper = styled('div')`
+  display: flex;
+  align-items: center;
+  gap: ${space(1)};
+  font-size: ${p => p.theme.fontSizeMedium};
+  color: ${p => p.theme.gray500};
+`;
