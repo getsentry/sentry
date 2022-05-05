bb2100634d0b242a42416e06a83875604bbaba9c
Stephen Cefali <scefali@sentry.io>
2022-05-04 17:05:36 -0700

    fix style issue

--- static/app/views/onboarding/targetedOnboarding/components/integrationInstaller.tsx
+++ static/app/views/onboarding/targetedOnboarding/components/integrationInstaller.tsx
@@ -205,7 +205,7 @@ const DisableWrapper = styled('div')`
   align-self: center;
   display: flex;
   flex-direction: column;
-  align-items: center;
+  align-items: right;
 `;
 
 const IconCloseCircle = styled(IconClose)`
