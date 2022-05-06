c9f7a42f4ea9356dda6531d954401c44cf03f8a9
Daian Scuarissi <scuarissid@gmail.com>
2022-05-06 14:50:08 -0300

    feat(replays): create event chapters component (#34198)
    Closes #33898

---
+++ static/app/components/replays/playerRelativeTime.tsx
@@ -0,0 +1,24 @@
+import styled from '@emotion/styled';
+
+import {showPlayerTime} from './utils';
+
+type Props = {
+  relativeTime: number | undefined;
+  timestamp: string | undefined;
+};
+
+const PlayerRelativeTime = ({relativeTime, timestamp}: Props) => {
+  if (!timestamp || !relativeTime) {
+    return <div />;
+  }
+
+  return <Value>{showPlayerTime(timestamp, relativeTime)}</Value>;
+};
+
+const Value = styled('p')`
+  color: ${p => p.theme.subText};
+  font-size: ${p => p.theme.fontSizeSmall};
+  font-weight: normal;
+`;
+
+export default PlayerRelativeTime;
