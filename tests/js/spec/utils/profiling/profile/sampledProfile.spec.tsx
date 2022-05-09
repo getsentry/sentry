01579592c8b6c4a3ebb0fc3db5ef8d1b6e3a1bf9
Tony Xiao <txiao@sentry.io>
2022-05-09 12:30:18 -0400

    feat(profiling): Update thread selector with more details (#34382)
    The thread name and thread id were split up in the response so we need to handle
    that accordingly. And add some info about the duration of a thread and the
    number of samples in it.

--- tests/js/spec/utils/profiling/profile/sampledProfile.spec.tsx
+++ tests/js/spec/utils/profiling/profile/sampledProfile.spec.tsx
@@ -20,6 +20,7 @@ describe('SampledProfile', () => {
 
     expect(profile.duration).toBe(1000);
     expect(profile.name).toBe(trace.name);
+    expect(profile.threadId).toBe(trace.threadID);
     expect(profile.startedAt).toBe(0);
     expect(profile.endedAt).toBe(1000);
   });
