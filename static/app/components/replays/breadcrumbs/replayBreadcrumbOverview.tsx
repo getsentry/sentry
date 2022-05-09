ea23e9aca950ff029c11f8cda222ae772e527e6e
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-05-10 01:12:44 +0800

    feat(replay): Render a vertical bar ontop of the timeline indicating the current timestamp (#34363)
    Adds a visual indicator into the timeline component, so people can more clearly see the timing of events relative to the playback.

--- static/app/components/replays/breadcrumbs/replayBreadcrumbOverview.tsx
+++ static/app/components/replays/breadcrumbs/replayBreadcrumbOverview.tsx
@@ -6,9 +6,9 @@ import HorizontalMouseTracking from 'sentry/components/replays/player/horizontal
 import {TimelineScubber} from 'sentry/components/replays/player/scrubber';
 import {useReplayContext} from 'sentry/components/replays/replayContext';
 import StackedContent from 'sentry/components/replays/stackedContent';
+import TimelinePosition from 'sentry/components/replays/timelinePosition';
 import space from 'sentry/styles/space';
 import {Crumb} from 'sentry/types/breadcrumbs';
-import {EntryType} from 'sentry/types/event';
 
 import {countColumns, formatTime, getCrumbsByColumn} from '../utils';
 
@@ -22,7 +22,7 @@ type LineStyle = 'dotted' | 'solid' | 'none';
 
 function ReplayBreadcrumbOverview({className}: Props) {
   const {replay, duration} = useReplayContext();
-  const crumbs = replay?.getEntryType(EntryType.BREADCRUMBS)?.data.values || [];
+  const crumbs = replay?.getRawCrumbs() || [];
   const transformedCrumbs = transformCrumbs(crumbs);
 
   return (
@@ -44,8 +44,8 @@ function ReplayBreadcrumbOverview({className}: Props) {
               minWidth={50}
               lineStyle="solid"
             />
-
             <Events crumbs={transformedCrumbs} width={width} />
+            <TimelinePosition />
           </React.Fragment>
         )}
       </StackedContent>
--- static/app/components/replays/utils.tsx
+++ static/app/components/replays/utils.tsx
@@ -138,3 +138,13 @@ export function getCrumbsByColumn(crumbs: Crumb[], totalColumns: number) {
 
   return crumbsByColumn;
 }
+
+/**
+ * Divide two numbers safely
+ */
+export function divide(numerator: number, denominator: number | undefined) {
+  if (denominator === undefined || isNaN(denominator) || denominator === 0) {
+    return 0;
+  }
+  return numerator / denominator;
+}
