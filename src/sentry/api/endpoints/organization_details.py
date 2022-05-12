dd08f2e64eaac1d3cc297eb5917f77a3ce14d154
Alberto Leal <mail4alberto@gmail.com>
2022-05-12 19:57:57 -0400

    Update src/sentry/api/endpoints/organization_details.py

--- src/sentry/api/endpoints/organization_details.py
+++ src/sentry/api/endpoints/organization_details.py
@@ -188,7 +188,7 @@ class OrganizationSerializer(serializers.Serializer):
         if qs.exists():
             raise serializers.ValidationError(f'The slug "{value}" is already in use.')
 
-        contains_whitespace = any(c in self.initial_data["slug"] for c in string.whitespace)
+        contains_whitespace = any(c.isspace() for c in self.initial_data["slug"])
         if contains_whitespace:
             raise serializers.ValidationError(
                 f'The slug "{value}" should not contain any whitespace.'
