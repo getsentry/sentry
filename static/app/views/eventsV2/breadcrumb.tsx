a30168fb6a67b089a90cd1158dc56a3a4e4e5074
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-05-13 18:43:12 -0700

    ref(js): Convert discover eventsV2 Breadcrumbs to a FC (#34629)

--- static/app/views/eventsV2/breadcrumb.tsx
+++ static/app/views/eventsV2/breadcrumb.tsx
@@ -1,4 +1,3 @@
-import {Component} from 'react';
 import {Location} from 'history';
 
 import Breadcrumbs, {Crumb} from 'sentry/components/breadcrumbs';
@@ -8,59 +7,45 @@ import {Event} from 'sentry/types/event';
 import EventView from 'sentry/utils/discover/eventView';
 import {getDiscoverLandingUrl} from 'sentry/utils/discover/urls';
 
-type DefaultProps = {
-  event: Event | undefined;
-};
-
-type Props = DefaultProps & {
+type Props = {
   eventView: EventView;
   location: Location;
   organization: Organization;
+  event?: Event;
 };
 
-class DiscoverBreadcrumb extends Component<Props> {
-  static defaultProps: DefaultProps = {
-    event: undefined,
-  };
+function DiscoverBreadcrumb({eventView, event, organization, location}: Props) {
+  const crumbs: Crumb[] = [];
+  const discoverTarget = organization.features.includes('discover-query')
+    ? {
+        pathname: getDiscoverLandingUrl(organization),
+        query: {
+          ...location.query,
+          ...eventView.generateBlankQueryStringObject(),
+          ...eventView.getPageFiltersQuery(),
+        },
+      }
+    : null;
 
-  getCrumbs() {
-    const crumbs: Crumb[] = [];
-    const {eventView, event, organization, location} = this.props;
-    const discoverTarget = organization.features.includes('discover-query')
-      ? {
-          pathname: getDiscoverLandingUrl(organization),
-          query: {
-            ...location.query,
-            ...eventView.generateBlankQueryStringObject(),
-            ...eventView.getPageFiltersQuery(),
-          },
-        }
-      : null;
+  crumbs.push({
+    to: discoverTarget,
+    label: t('Discover'),
+  });
 
+  if (eventView && eventView.isValid()) {
     crumbs.push({
-      to: discoverTarget,
-      label: t('Discover'),
+      to: eventView.getResultsViewUrlTarget(organization.slug),
+      label: eventView.name || '',
     });
-
-    if (eventView && eventView.isValid()) {
-      crumbs.push({
-        to: eventView.getResultsViewUrlTarget(organization.slug),
-        label: eventView.name || '',
-      });
-    }
-
-    if (event) {
-      crumbs.push({
-        label: t('Event Detail'),
-      });
-    }
-
-    return crumbs;
   }
 
-  render() {
-    return <Breadcrumbs crumbs={this.getCrumbs()} />;
+  if (event) {
+    crumbs.push({
+      label: t('Event Detail'),
+    });
   }
+
+  return <Breadcrumbs crumbs={crumbs} />;
 }
 
 export default DiscoverBreadcrumb;
