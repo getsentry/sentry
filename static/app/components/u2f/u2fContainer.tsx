eb275a507c584d538e58c4d183755f364f80b2c0
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-05-13 18:44:38 -0700

    ref(js): Convert U2fContainer to a FC (#34630)

--- static/app/components/u2f/u2fContainer.tsx
+++ static/app/components/u2f/u2fContainer.tsx
@@ -1,5 +1,3 @@
-import {Component} from 'react';
-
 import {Authenticator} from 'sentry/types';
 
 import U2fSign from './u2fsign';
@@ -11,24 +9,20 @@ type Props = {
   displayMode?: U2fSign['props']['displayMode'];
 };
 
-class U2fContainer extends Component<Props> {
-  render() {
-    const {className, authenticators} = this.props;
-
-    if (!authenticators.length) {
-      return null;
-    }
-
-    return (
-      <div className={className}>
-        {authenticators.map(auth =>
-          auth.id === 'u2f' && auth.challenge ? (
-            <U2fSign key={auth.id} {...this.props} challengeData={auth.challenge} />
-          ) : null
-        )}
-      </div>
-    );
+function U2fContainer({className, authenticators, ...props}: Props) {
+  if (!authenticators.length) {
+    return null;
   }
+
+  return (
+    <div className={className}>
+      {authenticators.map(auth =>
+        auth.id === 'u2f' && auth.challenge ? (
+          <U2fSign key={auth.id} {...props} challengeData={auth.challenge} />
+        ) : null
+      )}
+    </div>
+  );
 }
 
 export default U2fContainer;
