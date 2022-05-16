6a19622e2798adb1e7e3945e6ca1ce35a52e44ad
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-05-16 11:18:00 -0700

    ref(js): Convert U2fSign to a FC (#34631)

--- static/app/components/u2f/u2fContainer.tsx
+++ static/app/components/u2f/u2fContainer.tsx
@@ -2,11 +2,13 @@ import {Authenticator} from 'sentry/types';
 
 import U2fSign from './u2fsign';
 
+type U2FSignProps = React.ComponentProps<typeof U2fSign>;
+
 type Props = {
   authenticators: Array<Authenticator>;
-  onTap: U2fSign['props']['onTap'];
+  onTap: U2FSignProps['onTap'];
   className?: string;
-  displayMode?: U2fSign['props']['displayMode'];
+  displayMode?: U2FSignProps['displayMode'];
 };
 
 function U2fContainer({className, authenticators, ...props}: Props) {
