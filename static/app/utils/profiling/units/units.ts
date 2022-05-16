d54f8ba94b0e8aaf69a00f40e858be3c94d0e9d5
JonasBa <jonas.badalic@sentry.io>
2022-05-16 13:12:07 -0400

    feat(profiling): add timeline formatter

--- static/app/utils/profiling/units/units.ts
+++ static/app/utils/profiling/units/units.ts
@@ -37,3 +37,37 @@ export function makeFormatter(from: Unit | string): (value: number) => string {
     return format(duration / 1e-9, 'ns', 2);
   };
 }
+
+function templateString(template: string, value: number) {
+  if (value === 0) {
+    return template;
+  }
+
+  const str = [...Math.floor(value).toString()].reverse();
+  const out: string[] = [];
+
+  for (let i = 0; i < template.length; i++) {
+    out[template.length - 1 - i] = str[i] || template.charAt(i);
+  }
+
+  return out.join('');
+}
+
+export function makeTimelineFormatter(from: Unit | string) {
+  const multiplier = durationMappings[from];
+
+  if (multiplier === undefined) {
+    throw new Error(`Cannot format from unit ${from}, duration mapping is not defined`);
+  }
+
+  return (value: number) => {
+    const s = value * multiplier;
+    const m = s / 60;
+    const ms = s * 1e3;
+
+    return `${templateString('00', m)}:${templateString('00', s % 60)}.${templateString(
+      '000',
+      ms % 1e3
+    )}`;
+  };
+}
