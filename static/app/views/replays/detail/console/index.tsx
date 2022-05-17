a5327d43aa66df4cc0679f736959336d83e4167a
Dublin Anondson <stratophone2112@gmail.com>
2022-05-17 15:18:44 -0500

    feat(replays): added search to console tab on replay details.

--- static/app/views/replays/detail/console/index.tsx
+++ static/app/views/replays/detail/console/index.tsx
@@ -1,6 +1,9 @@
+import {Fragment, useMemo, useState} from 'react';
 import styled from '@emotion/styled';
 
 import {Panel} from 'sentry/components/panels';
+import SearchBar from 'sentry/components/searchBar';
+import space from 'sentry/styles/space';
 import {BreadcrumbTypeDefault} from 'sentry/types/breadcrumbs';
 
 import ConsoleMessage from './consoleMessage';
@@ -10,16 +13,30 @@ interface Props {
 }
 
 function Console({breadcrumbs}: Props) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleSearch = query => setSearchTerm(query);
+  const filteredBreadCrumbs = useMemo(
+    () =>
+      !searchTerm
+        ? breadcrumbs
+        : breadcrumbs.filter(breadcrumb =>
+            breadcrumb.message?.toLowerCase().includes(searchTerm)
+          ),
+    [searchTerm, breadcrumbs]
+  );
   return (
-    <ConsoleTable>
-      {breadcrumbs.map((breadcrumb, i) => (
-        <ConsoleMessage
-          key={i}
-          isLast={i === breadcrumbs.length - 1}
-          breadcrumb={breadcrumb}
-        />
-      ))}
-    </ConsoleTable>
+    <Fragment>
+      <StyledSearchBar onChange={handleSearch} />
+      <ConsoleTable>
+        {filteredBreadCrumbs.map((breadcrumb, i) => (
+          <ConsoleMessage
+            key={i}
+            isLast={i === breadcrumbs.length - 1}
+            breadcrumb={breadcrumb}
+          />
+        ))}
+      </ConsoleTable>
+    </Fragment>
   );
 }
 
@@ -28,6 +45,10 @@ const ConsoleTable = styled(Panel)`
   grid-template-columns: max-content auto;
   font-family: ${p => p.theme.text.familyMono};
   font-size: 0.8em;
+`;
+
+const StyledSearchBar = styled(SearchBar)`
+  margin-bottom: ${space(1)};
 `;
 
 export default Console;
