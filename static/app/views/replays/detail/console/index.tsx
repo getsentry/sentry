899a875ce52efc1896b854ce3a2f6cf0cf5f84c3
Dublin Anondson <stratophone2112@gmail.com>
2022-05-17 15:54:44 -0500

    feat: added an empty message when no results are present.

--- static/app/views/replays/detail/console/index.tsx
+++ static/app/views/replays/detail/console/index.tsx
@@ -4,8 +4,10 @@ import debounce from 'lodash/debounce';
 
 import {Panel} from 'sentry/components/panels';
 import SearchBar from 'sentry/components/searchBar';
+import {t} from 'sentry/locale';
 import space from 'sentry/styles/space';
 import {BreadcrumbTypeDefault} from 'sentry/types/breadcrumbs';
+import EmptyMessage from 'sentry/views/settings/components/emptyMessage';
 
 import ConsoleMessage from './consoleMessage';
 
@@ -28,18 +30,26 @@ function Console({breadcrumbs}: Props) {
   return (
     <Fragment>
       <StyledSearchBar onChange={handleSearch} />
-      <ConsoleTable>
-        {filteredBreadcrumbs.map((breadcrumb, i) => (
-          <ConsoleMessage
-            key={i}
-            isLast={i === breadcrumbs.length - 1}
-            breadcrumb={breadcrumb}
-          />
-        ))}
-      </ConsoleTable>
+      {filteredBreadcrumbs.length > 0 ? (
+        <ConsoleTable>
+          {filteredBreadcrumbs.map((breadcrumb, i) => (
+            <ConsoleMessage
+              key={i}
+              isLast={i === breadcrumbs.length - 1}
+              breadcrumb={breadcrumb}
+            />
+          ))}
+        </ConsoleTable>
+      ) : (
+        <StyledEmptyMessage title={t('No results found.')} />
+      )}
     </Fragment>
   );
 }
+
+const StyledEmptyMessage = styled(EmptyMessage)`
+  align-items: center;
+`;
 
 const ConsoleTable = styled(Panel)`
   display: grid;
@@ -50,6 +60,7 @@ const ConsoleTable = styled(Panel)`
 
 const StyledSearchBar = styled(SearchBar)`
   margin-bottom: ${space(1)};
+  margin-top: 28px;
 `;
 
 export default Console;
