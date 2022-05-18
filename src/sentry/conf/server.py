30a6604fb95887648cfb602ba57d6afe0da4599b
Ash Anand <0Calories@users.noreply.github.com>
2022-05-18 09:53:23 -0400

    feat(feature-flag): Add feature flag for span tree autoscrolling (#34707)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1085,6 +1085,8 @@ SENTRY_FEATURES = {
     "organizations:performance-autogroup-sibling-spans": False,
     # Enable performance on-boarding checklist
     "organizations:performance-onboarding-checklist": False,
+    # Enable automatic horizontal scrolling on the span tree
+    "organizations:performance-span-tree-autoscroll": False,
     # Enable the new Related Events feature
     "organizations:related-events": False,
     # Enable usage of external relays, for use with Relay. See
