1965b49b991a38386c6a9c52f6cfb54aa9ef19fb
Dublin Anondson <stratophone2112@gmail.com>
2022-05-18 08:57:32 -0500

    proto: added log level filtering on the console tabs

--- static/app/views/replays/detail/console/index.tsx
+++ static/app/views/replays/detail/console/index.tsx
@@ -1,7 +1,10 @@
+import {Fragment, useMemo, useState} from 'react';
 import styled from '@emotion/styled';
 
+import CompactSelect from 'sentry/components/forms/compactSelect';
 import {Panel} from 'sentry/components/panels';
-import {BreadcrumbTypeDefault} from 'sentry/types/breadcrumbs';
+import {t} from 'sentry/locale';
+import {BreadcrumbLevelType, BreadcrumbTypeDefault} from 'sentry/types/breadcrumbs';
 
 import ConsoleMessage from './consoleMessage';
 
@@ -9,17 +12,45 @@ interface Props {
   breadcrumbs: BreadcrumbTypeDefault[];
 }
 
+const getDistinctLogLevels = breadcrumbs =>
+  Array.from(new Set(breadcrumbs.map(breadcrumb => breadcrumb.level)));
+
 function Console({breadcrumbs}: Props) {
+  const [logLevel, setLogLevel] = useState<BreadcrumbLevelType>();
+
+  const filteredBreadcrumbs = useMemo(
+    () =>
+      !logLevel
+        ? breadcrumbs
+        : breadcrumbs.filter(breadcrumb => breadcrumb.level === logLevel),
+    [logLevel, breadcrumbs]
+  );
+
   return (
-    <ConsoleTable>
-      {breadcrumbs.map((breadcrumb, i) => (
-        <ConsoleMessage
-          key={i}
-          isLast={i === breadcrumbs.length - 1}
-          breadcrumb={breadcrumb}
-        />
-      ))}
-    </ConsoleTable>
+    <Fragment>
+      <CompactSelect
+        triggerProps={{
+          size: 'small',
+          prefix: t('Log Level'),
+        }}
+        value={logLevel}
+        options={getDistinctLogLevels(breadcrumbs).map(breadcrumbLogLevel => ({
+          value: breadcrumbLogLevel,
+          label: `${breadcrumbLogLevel}`,
+          disabled: breadcrumbLogLevel === logLevel,
+        }))}
+        onChange={opt => setLogLevel(opt.value)}
+      />
+      <ConsoleTable>
+        {filteredBreadcrumbs.map((breadcrumb, i) => (
+          <ConsoleMessage
+            key={i}
+            isLast={i === breadcrumbs.length - 1}
+            breadcrumb={breadcrumb}
+          />
+        ))}
+      </ConsoleTable>
+    </Fragment>
   );
 }
 
