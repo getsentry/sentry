d7a1c2bf2f47da37d10c399f8ed8be31655a5e12
Dublin Anondson <Stratophone2112@gmail.com>
2022-05-18 08:54:47 -0500

    feat(replays): added search to console tab on replay details. (#34711)
    Closes #34660 
    Added a search bar to the console tab in replay details so we can filter console messages.
    
    Screenshots: 
    <img width="890" alt="image" src="https://user-images.githubusercontent.com/7014871/168903065-f24438fb-f080-41ad-bbfb-28049ddfcbd1.png">
    
    <img width="890" alt="image" src="https://user-images.githubusercontent.com/7014871/168908356-4dd249f3-abdc-48f6-a018-2f48dc5c3dda.png">

--- static/app/views/replays/detail/console/index.tsx
+++ static/app/views/replays/detail/console/index.tsx
@@ -1,7 +1,13 @@
+import {Fragment, useMemo, useState} from 'react';
 import styled from '@emotion/styled';
+import debounce from 'lodash/debounce';
 
 import {Panel} from 'sentry/components/panels';
+import SearchBar from 'sentry/components/searchBar';
+import {t} from 'sentry/locale';
+import space from 'sentry/styles/space';
 import {BreadcrumbTypeDefault} from 'sentry/types/breadcrumbs';
+import EmptyMessage from 'sentry/views/settings/components/emptyMessage';
 
 import ConsoleMessage from './consoleMessage';
 
@@ -10,24 +16,51 @@ interface Props {
 }
 
 function Console({breadcrumbs}: Props) {
+  const [searchTerm, setSearchTerm] = useState('');
+  const handleSearch = debounce(query => setSearchTerm(query), 150);
+  const filteredBreadcrumbs = useMemo(
+    () =>
+      !searchTerm
+        ? breadcrumbs
+        : breadcrumbs.filter(breadcrumb =>
+            breadcrumb.message?.toLowerCase().includes(searchTerm)
+          ),
+    [searchTerm, breadcrumbs]
+  );
   return (
-    <ConsoleTable>
-      {breadcrumbs.map((breadcrumb, i) => (
-        <ConsoleMessage
-          key={i}
-          isLast={i === breadcrumbs.length - 1}
-          breadcrumb={breadcrumb}
-        />
-      ))}
-    </ConsoleTable>
+    <Fragment>
+      <ConsoleSearch onChange={handleSearch} />
+      {filteredBreadcrumbs.length > 0 ? (
+        <ConsoleTable>
+          {filteredBreadcrumbs.map((breadcrumb, i) => (
+            <ConsoleMessage
+              key={i}
+              isLast={i === breadcrumbs.length - 1}
+              breadcrumb={breadcrumb}
+            />
+          ))}
+        </ConsoleTable>
+      ) : (
+        <StyledEmptyMessage title={t('No results found.')} />
+      )}
+    </Fragment>
   );
 }
+
+const StyledEmptyMessage = styled(EmptyMessage)`
+  align-items: center;
+`;
 
 const ConsoleTable = styled(Panel)`
   display: grid;
   grid-template-columns: max-content auto;
   font-family: ${p => p.theme.text.familyMono};
   font-size: 0.8em;
+`;
+
+const ConsoleSearch = styled(SearchBar)`
+  margin-bottom: ${space(1)};
+  margin-top: 28px;
 `;
 
 export default Console;
