9a2271d6de6cc114405ed613b2b5465937dd65c5
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-05-17 21:33:04 -0700

    ref(ts): Better types on useLocation (#34728)

--- static/app/utils/useLocation.tsx
+++ static/app/utils/useLocation.tsx
@@ -2,7 +2,11 @@ import {Location, Query} from 'history';
 
 import {useRouteContext} from 'sentry/utils/useRouteContext';
 
-export function useLocation<Q extends Query>(): Location<Q> {
+type DefaultQuery<T = string> = {
+  [key: string]: T | T[] | null | undefined;
+};
+
+export function useLocation<Q extends Query = DefaultQuery>(): Location<Q> {
   const route = useRouteContext();
   return route.location;
 }
