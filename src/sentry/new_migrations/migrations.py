0e2c6896e95f44c7dc9f9ee711101076f2b3f709
Richard Ma <maxiuyuan1998@gmail.com>
2022-05-19 14:23:35 -0700

    reverted check to True

--- src/sentry/new_migrations/migrations.py
+++ src/sentry/new_migrations/migrations.py
@@ -7,7 +7,7 @@ class CheckedMigration(Migration):
     won't cause production issues during deploy.
     """
 
-    checked = False
+    checked = True
 
     def apply(self, project_state, schema_editor, collect_sql=False):
         if self.checked:
