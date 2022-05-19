359a12a064c88b432b5981855231cfb310811a11
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-05-20 00:50:09 +0800

    feat(replays): Improve ReplayTimeline tooltips (#34747)
    Improving the Timeline/tooltips in a few ways::
    - Bigger surface areas for the mouse cursor to hover (2px to 4px, woah!)
    - Tooltip shows some info about the event (navigation destination, click selector, error value)
    - Icons that represent a group of events inherit the color of the 'most interesting' event inside -> we will see more red and green icons, fewer purple.

--- static/app/components/replays/breadcrumbs/replayTimelineEvents.tsx
+++ static/app/components/replays/breadcrumbs/replayTimelineEvents.tsx
@@ -1,17 +1,16 @@
-import React from 'react';
 import styled from '@emotion/styled';
-import first from 'lodash/first';
 
+import Type from 'sentry/components/events/interfaces/breadcrumbs/breadcrumb/type';
 import Icon from 'sentry/components/events/interfaces/breadcrumbs/breadcrumb/type/icon';
+import {Hovercard} from 'sentry/components/hovercard';
 import * as Timeline from 'sentry/components/replays/breadcrumbs/timeline';
-import Tooltip from 'sentry/components/tooltip';
 import space from 'sentry/styles/space';
-import {Crumb} from 'sentry/types/breadcrumbs';
+import {BreadcrumbType, Crumb} from 'sentry/types/breadcrumbs';
 import type {Color} from 'sentry/utils/theme';
 
 import {getCrumbsByColumn} from '../utils';
 
-const EVENT_STICK_MARKER_WIDTH = 2;
+const EVENT_STICK_MARKER_WIDTH = 4;
 
 type Props = {
   crumbs: Crumb[];
@@ -57,31 +56,76 @@ const EventColumn = styled(Timeline.Col)<{column: number}>`
   }
 `;
 
-function Event({crumbs}: {crumbs: Crumb[]; className?: string}) {
-  const breadcrumb = first(crumbs);
-  if (!breadcrumb) {
-    return null;
+function getCrumbDetail(crumb: Crumb) {
+  switch (crumb.type) {
+    case BreadcrumbType.USER:
+    case BreadcrumbType.UI:
+      return crumb.message ?? crumb.description;
+    case BreadcrumbType.NAVIGATION:
+      return crumb.data?.to ?? crumb.description;
+    case BreadcrumbType.ERROR:
+      return `${crumb.data?.type}: ${crumb.data?.value}`;
+    default:
+      return crumb.message;
   }
+}
 
-  const icon = crumbs.length === 1 ? <Icon type={breadcrumb.type} /> : crumbs.length;
+function sortByMostInterestingType(a: Crumb, b: Crumb) {
+  const rank = {
+    [BreadcrumbType.USER]: 0,
+    [BreadcrumbType.UI]: 0,
+    [BreadcrumbType.NAVIGATION]: 1,
+    [BreadcrumbType.ERROR]: 2,
+  };
+
+  return rank[a.type] - rank[b.type];
+}
+
+function Event({crumbs}: {crumbs: Crumb[]; className?: string}) {
+  const title = (
+    <HoverList>
+      {crumbs.map(crumb => (
+        <HoverListItem key={crumb.id}>
+          <Type type={crumb.type} color={crumb.color} description={crumb.description} />
+          <small>{getCrumbDetail(crumb)}</small>
+        </HoverListItem>
+      ))}
+    </HoverList>
+  );
+
+  const mostInteresting = crumbs.reduce((best, crumb) =>
+    sortByMostInterestingType(best, crumb) >= 1 ? best : crumb
+  );
+
+  const icon = crumbs.length === 1 ? <Icon type={mostInteresting.type} /> : crumbs.length;
 
   return (
     <IconPosition>
-      <Tooltip
-        key={breadcrumb.id}
-        title={`${breadcrumb.category}: ${breadcrumb.message}`}
-        skipWrapper
-        disableForVisualTest
-      >
-        <IconNode color={breadcrumb.color}>{icon}</IconNode>
-      </Tooltip>
+      <Hovercard key={mostInteresting.id} body={title}>
+        <IconNode color={mostInteresting.color}>{icon}</IconNode>
+      </Hovercard>
     </IconPosition>
   );
 }
 
+const HoverList = styled('ul')`
+  margin: 0;
+  padding: 0;
+`;
+const HoverListItem = styled('li')`
+  display: flex;
+  gap: ${space(1)};
+  padding: ${space(0.5)};
+  border-bottom: 1px solid ${p => p.theme.innerBorder};
+  &:last-child {
+    border-bottom: none;
+  }
+`;
+
 const IconPosition = styled('div')`
   position: absolute;
   transform: translate(-50%);
+  margin-left: ${EVENT_STICK_MARKER_WIDTH / 2}px;
 `;
 
 const IconNode = styled('div')<{color: Color}>`
