4bcbb751eb285c9e01c9da5f2135dd7c883a27a8
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2022-05-19 13:23:17 -0400

    feat(discover): adds a referrer for auth token requests (#34771)
    events requests authorized by an Auth Token are given an api.auth-token snuba referrer

--- src/sentry/api/endpoints/organization_events.py
+++ src/sentry/api/endpoints/organization_events.py
@@ -42,6 +42,8 @@ ALLOWED_EVENTS_GEO_REFERRERS = {
     "api.dashboards.worldmapwidget",
 }
 
+API_TOKEN_REFERRER = "api.auth-token.events"
+
 
 class OrganizationEventsV2Endpoint(OrganizationEventsV2EndpointBase):
     def get(self, request: Request, organization) -> Response:
@@ -154,7 +156,11 @@ class OrganizationEventsEndpoint(OrganizationEventsV2EndpointBase):
 
         sentry_sdk.set_tag("performance.metrics_enhanced", metrics_enhanced)
         allow_metric_aggregates = request.GET.get("preventMetricAggregates") != "1"
-        referrer = referrer if referrer in ALLOWED_EVENTS_REFERRERS else "api.organization-events"
+        # Force the referrer to "api.auth-token.events" for events requests authorized through a bearer token
+        if request.auth:
+            referrer = API_TOKEN_REFERRER
+        elif referrer not in ALLOWED_EVENTS_REFERRERS:
+            referrer = "api.organization-events"
 
         def data_fn(offset, limit):
             query_details = {
