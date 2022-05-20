668a627e9b8e6e53a63c8f2960f6da53f2985781
mdtro <matthew.trostel@sentry.io>
2022-05-20 14:54:30 -0500

    remove 3rd capture group

--- src/sentry/snuba/metrics/utils.py
+++ src/sentry/snuba/metrics/utils.py
@@ -59,7 +59,7 @@ TS_COL_GROUP = "bucketed_time"
 
 #: Max number of data points per time series:
 # ToDo modify this regex to only support the operations provided
-FIELD_REGEX = re.compile(r"^(\w+)\((([\w.:/@])+)\)$")
+FIELD_REGEX = re.compile(r"^(\w+)\(([\w.:/@]+)\)$")
 TAG_REGEX = re.compile(r"^([\w.]+)$")
 
 #: A function that can be applied to a metric
