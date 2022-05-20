c5af41d36d7a6a4f1e84d9a8f0f4ce69f2bed28c
Joshua Li <joshuarli98@gmail.com>
2022-05-20 15:38:10 -0700

    work by default and read from os.environ once

--- tests/sentry/eventstream/kafka/test_consumer.py
+++ tests/sentry/eventstream/kafka/test_consumer.py
@@ -24,21 +24,21 @@ except ImportError:
 
 from django.conf import settings
 
-settings.KAFKA_CLUSTERS["default"] = {
-    "common": {"bootstrap.servers": os.environ.get("SENTRY_KAFKA_HOSTS", "127.0.0.1:9092")}
-}
+SENTRY_KAFKA_HOSTS = os.environ.get("SENTRY_KAFKA_HOSTS", "127.0.0.1:9092")
+SENTRY_ZOOKEEPER_HOSTS = os.environ.get("SENTRY_ZOOKEEPER_HOSTS", "127.0.0.1:2181")
+settings.KAFKA_CLUSTERS["default"] = {"common": {"bootstrap.servers": SENTRY_KAFKA_HOSTS}}
 
 
 @contextmanager
 def create_topic(partitions=1, replication_factor=1):
     command = ["docker", "exec", "sentry_kafka", "kafka-topics"] + [
         "--zookeeper",
-        os.environ["SENTRY_ZOOKEEPER_HOSTS"],
+        SENTRY_ZOOKEEPER_HOSTS,
     ]
     topic = f"test-{uuid.uuid1().hex}"
     subprocess.check_call(
         command
-        + [
+        + (
             "--create",
             "--topic",
             topic,
@@ -46,12 +46,12 @@ def create_topic(partitions=1, replication_factor=1):
             f"{partitions}",
             "--replication-factor",
             f"{replication_factor}",
-        ]
+        )
     )
     try:
         yield topic
     finally:
-        subprocess.check_call(command + ["--delete", "--topic", topic])
+        subprocess.check_call(command + ("--delete", "--topic", topic))
 
 
 def test_consumer_start_from_partition_start(requires_kafka):
@@ -65,7 +65,7 @@ def test_consumer_start_from_partition_start(requires_kafka):
 
     producer = Producer(
         {
-            "bootstrap.servers": os.environ["SENTRY_KAFKA_HOSTS"],
+            "bootstrap.servers": SENTRY_KAFKA_HOSTS,
             "on_delivery": record_message_delivered,
         }
     )
@@ -150,7 +150,7 @@ def test_consumer_start_from_committed_offset(requires_kafka):
 
     producer = Producer(
         {
-            "bootstrap.servers": os.environ["SENTRY_KAFKA_HOSTS"],
+            "bootstrap.servers": SENTRY_KAFKA_HOSTS,
             "on_delivery": record_message_delivered,
         }
     )
@@ -164,7 +164,7 @@ def test_consumer_start_from_committed_offset(requires_kafka):
         assert producer.flush(5) == 0, "producer did not successfully flush queue"
 
         Consumer(
-            {"bootstrap.servers": os.environ["SENTRY_KAFKA_HOSTS"], "group.id": consumer_group}
+            {"bootstrap.servers": SENTRY_KAFKA_HOSTS, "group.id": consumer_group}
         ).commit(message=messages_delivered[topic][0], asynchronous=False)
 
         # Create the synchronized consumer.
@@ -247,7 +247,7 @@ def test_consumer_rebalance_from_partition_start(requires_kafka):
 
     producer = Producer(
         {
-            "bootstrap.servers": os.environ["SENTRY_KAFKA_HOSTS"],
+            "bootstrap.servers": SENTRY_KAFKA_HOSTS,
             "on_delivery": record_message_delivered,
         }
     )
@@ -361,7 +361,7 @@ def test_consumer_rebalance_from_committed_offset(requires_kafka):
 
     producer = Producer(
         {
-            "bootstrap.servers": os.environ["SENTRY_KAFKA_HOSTS"],
+            "bootstrap.servers": SENTRY_KAFKA_HOSTS,
             "on_delivery": record_message_delivered,
         }
     )
@@ -375,7 +375,7 @@ def test_consumer_rebalance_from_committed_offset(requires_kafka):
         assert producer.flush(5) == 0, "producer did not successfully flush queue"
 
         Consumer(
-            {"bootstrap.servers": os.environ["SENTRY_KAFKA_HOSTS"], "group.id": consumer_group}
+            {"bootstrap.servers": SENTRY_KAFKA_HOSTS, "group.id": consumer_group}
         ).commit(
             offsets=[
                 TopicPartition(message.topic(), message.partition(), message.offset() + 1)
@@ -524,7 +524,7 @@ def test_consumer_rebalance_from_uncommitted_offset(requires_kafka):
 
     producer = Producer(
         {
-            "bootstrap.servers": os.environ["SENTRY_KAFKA_HOSTS"],
+            "bootstrap.servers": SENTRY_KAFKA_HOSTS,
             "on_delivery": record_message_delivered,
         }
     )
