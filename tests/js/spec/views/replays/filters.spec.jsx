2b81a2254b10cbd015b53c62eda3c8c6270048b3
Dublin Anondson <stratophone2112@gmail.com>
2022-05-23 13:36:26 -0500

    test: added a test for the replay filters component

--- tests/js/spec/views/replays/filters.spec.jsx
+++ tests/js/spec/views/replays/filters.spec.jsx
@@ -4,20 +4,17 @@ import {OrganizationContext} from 'sentry/views/organizationContext';
 import ReplaysFilters from 'sentry/views/replays/filters';
 
 describe('ReplayFilters', function () {
-  let wrapper;
   it('renders', function () {
-    wrapper = render(
+    const organization = TestStubs.Organization();
+    const wrapper = render(
       <OrganizationContext.Provider value={TestStubs.Organization()}>
         <ReplaysFilters
-          organization={{
-            features: ['performance-view', 'discover-query'],
-            apdexThreshold: 400,
-          }}
+          organization={organization}
           handleSearchQuery={jest.fn()}
           query=""
         />
       </OrganizationContext.Provider>
     );
-    expect(wrapper).toSnapshot();
+    expect(wrapper.container).toSnapshot();
   });
 });
