69f5127d3954268d378c992d8779d5621e205023
Dublin Anondson <stratophone2112@gmail.com>
2022-05-23 11:19:10 -0500

    refactor: moved tests to the proper utils.spec file

--- tests/js/spec/components/replays/utils.spec.jsx
+++ tests/js/spec/components/replays/utils.spec.jsx
@@ -1,8 +1,11 @@
 import {
   countColumns,
+  divide,
   flattenSpans,
   formatTime,
   getCrumbsByColumn,
+  relativeTimeInMs,
+  showPlayerTime,
 } from 'sentry/components/replays/utils';
 
 const SECOND = 1000;
@@ -249,5 +252,55 @@ describe('flattenSpans', () => {
         startTimestamp: 10000,
       },
     ]);
+  });
+
+  describe('relativeTimeinMs', () => {
+    it('returns relative time in MS', () => {
+      expect(relativeTimeInMs('2022-05-11T23:04:27.576000Z', 347.90000009536743)).toEqual(
+        1652309919676
+      );
+    });
+
+    it('returns NaN without relative time', () => {
+      expect(relativeTimeInMs('2022-05-11T23:04:27.576000Z')).toEqual(NaN);
+    });
+
+    it('returns invalid date if date string is malformed', () => {
+      expect(relativeTimeInMs('202223:04:27.576000Z')).toEqual(NaN);
+    });
+  });
+
+  describe('showPlayerTime', () => {
+    it('returns time formatted for player', () => {
+      expect(showPlayerTime('2022-05-11T23:04:27.576000Z', 1652309918.676)).toEqual(
+        '00:05:48'
+      );
+    });
+
+    it('returns time without difference', () => {
+      expect(showPlayerTime('2022-05-11T23:04:27.576000Z')).toEqual('Invalid date');
+    });
+
+    it('returns invalid date if timestamp is malformed', () => {
+      expect(showPlayerTime('20223:04:27.576000Z')).toEqual('Invalid date');
+    });
+  });
+
+  describe('divide', () => {
+    it('divides numbers safely', () => {
+      expect(divide(81, 9)).toEqual(9);
+    });
+
+    it('dividing by zero returns', () => {
+      expect(divide(81, 0)).toEqual(0);
+    });
+
+    it('dividing undefined by number results in', () => {
+      expect(divide(undefined, 9)).toEqual(NaN);
+    });
+
+    it('divides by string results in NaN', () => {
+      expect(divide(9, 'number')).toEqual(0);
+    });
   });
 });
