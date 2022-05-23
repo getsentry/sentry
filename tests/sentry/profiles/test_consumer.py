bf275f77d2adf5073fe177eea8b64068218068f2
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-05-23 11:22:01 +0200

    Update ProfilesConsumerTest test

--- tests/sentry/profiles/test_consumer.py
+++ tests/sentry/profiles/test_consumer.py
@@ -15,6 +15,7 @@ class ProfilesConsumerTest(TestCase, SnubaTestCase):
         return {
             "organization_id": 1,
             "project_id": 1,
+            "key_id": 1,
             "received": int(datetime.utcnow().timestamp()),
             "payload": json.dumps({"platform": "android", "profile": ""}),
         }
@@ -23,9 +24,10 @@ class ProfilesConsumerTest(TestCase, SnubaTestCase):
         consumer = ProfilesConsumer()
         message = Mock()
         message.value.return_value = msgpack.packb(self.valid_message)
-        profile = consumer.process_message(message)
+        key_id, profile = consumer.process_message(message)
 
         for k in ["organization_id", "project_id", "received"]:
             assert k in profile
 
         assert isinstance(profile["received"], int)
+        assert isinstance(key_id, int)
