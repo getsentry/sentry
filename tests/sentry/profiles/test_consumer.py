305f86e60725d13c1d45961e2c2227a811b47124
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2022-05-23 17:13:43 +0200

    Add test for when key_id is None

--- tests/sentry/profiles/test_consumer.py
+++ tests/sentry/profiles/test_consumer.py
@@ -20,6 +20,15 @@ class ProfilesConsumerTest(TestCase, SnubaTestCase):
             "payload": json.dumps({"platform": "android", "profile": ""}),
         }
 
+    @fixture
+    def valid_message_no_key_id(self):
+        return {
+            "organization_id": 1,
+            "project_id": 1,
+            "received": int(datetime.utcnow().timestamp()),
+            "payload": json.dumps({"platform": "android", "profile": ""}),
+        }
+
     def test_process(self):
         consumer = ProfilesConsumer()
         message = Mock()
@@ -31,3 +40,15 @@ class ProfilesConsumerTest(TestCase, SnubaTestCase):
 
         assert isinstance(profile["received"], int)
         assert isinstance(key_id, int)
+
+    def test_process_no_key_id(self):
+        consumer = ProfilesConsumer()
+        message = Mock()
+        message.value.return_value = msgpack.packb(self.valid_message_no_key_id)
+        key_id, profile = consumer.process_message(message)
+
+        for k in ["organization_id", "project_id", "received"]:
+            assert k in profile
+
+        assert isinstance(profile["received"], int)
+        assert key_id is None
