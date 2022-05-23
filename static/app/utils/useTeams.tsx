dce91f36502eeeb554a5a94300b89c7de03ce73e
Vu Luong <vuluongj20@gmail.com>
2022-05-23 13:51:38 -0700

    ref(useTeams): Memoize filteredTeams (#34927)
    * ref(useTeams): Memoize filteredTeams
    
    * Only watch for changes from store.teams
    
    Co-authored-by: NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
    
    Co-authored-by: NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>

--- static/app/utils/useTeams.tsx
+++ static/app/utils/useTeams.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from 'react';
+import {useEffect, useMemo, useRef, useState} from 'react';
 import uniqBy from 'lodash/uniqBy';
 
 import {fetchUserTeams} from 'sentry/actionCreators/teams';
@@ -332,13 +332,15 @@ function useTeams({limit, slugs, ids, provideUserTeams}: Options = {}) {
 
   const isSuperuser = isActiveSuperuser();
 
-  const filteredTeams = slugs
-    ? store.teams.filter(t => slugs.includes(t.slug))
-    : ids
-    ? store.teams.filter(t => ids.includes(t.id))
-    : provideUserTeams && !isSuperuser
-    ? store.teams.filter(t => t.isMember)
-    : store.teams;
+  const filteredTeams = useMemo(() => {
+    return slugs
+      ? store.teams.filter(t => slugs.includes(t.slug))
+      : ids
+      ? store.teams.filter(t => ids.includes(t.id))
+      : provideUserTeams && !isSuperuser
+      ? store.teams.filter(t => t.isMember)
+      : store.teams;
+  }, [store.teams, ids, slugs, provideUserTeams, isSuperuser]);
 
   const result: Result = {
     teams: filteredTeams,
