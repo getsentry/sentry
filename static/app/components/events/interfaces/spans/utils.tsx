c148ff80005c2fef367b44c044fc066002400bba
Ash Anand <0Calories@users.noreply.github.com>
2022-05-25 09:15:04 -0400

    feat(span-tree): Horizontal autoscrolling on the span tree (#34706)
    This PR adds a new feature to the span tree, that will cause the view to adjust horizontally as the user scrolls down the tree.

--- static/app/components/events/interfaces/spans/utils.tsx
+++ static/app/components/events/interfaces/spans/utils.tsx
@@ -5,12 +5,14 @@ import isString from 'lodash/isString';
 import set from 'lodash/set';
 import moment from 'moment';
 
+import {TOGGLE_BORDER_BOX} from 'sentry/components/performance/waterfall/treeConnector';
 import {EntryType, EventTransaction} from 'sentry/types/event';
 import {assert} from 'sentry/types/utils';
 import {WEB_VITAL_DETAILS} from 'sentry/utils/performance/vitals/constants';
 import {getPerformanceTransaction} from 'sentry/utils/performanceForSentry';
 
 import {MERGE_LABELS_THRESHOLD_PERCENT} from './constants';
+import {MARGIN_LEFT} from './spanBar';
 import {
   EnhancedSpan,
   GapSpanType,
@@ -759,5 +761,72 @@ export function getSpanGroupBounds(
       const _exhaustiveCheck: never = bounds;
       return _exhaustiveCheck;
     }
+  }
+}
+
+export class SpansInViewMap {
+  spanDepthsInView: Map<string, number>;
+  treeDepthSum: number;
+  length: number;
+  isRootSpanInView: boolean;
+
+  constructor() {
+    this.spanDepthsInView = new Map();
+    this.treeDepthSum = 0;
+    this.length = 0;
+    this.isRootSpanInView = true;
+  }
+
+  /**
+   *
+   * @param spanId
+   * @param treeDepth
+   * @returns false if the span is already stored, true otherwise
+   */
+  addSpan(spanId: string, treeDepth: number): boolean {
+    if (this.spanDepthsInView.has(spanId)) {
+      return false;
+    }
+
+    this.spanDepthsInView.set(spanId, treeDepth);
+    this.length += 1;
+    this.treeDepthSum += treeDepth;
+
+    if (treeDepth === 0) {
+      this.isRootSpanInView = true;
+    }
+
+    return true;
+  }
+
+  /**
+   *
+   * @param spanId
+   * @returns false if the span does not exist within the span, true otherwise
+   */
+  removeSpan(spanId: string): boolean {
+    if (!this.spanDepthsInView.has(spanId)) {
+      return false;
+    }
+
+    const treeDepth = this.spanDepthsInView.get(spanId);
+    this.spanDepthsInView.delete(spanId);
+    this.length -= 1;
+    this.treeDepthSum -= treeDepth!;
+
+    if (treeDepth === 0) {
+      this.isRootSpanInView = false;
+    }
+
+    return true;
+  }
+
+  getScrollVal() {
+    if (this.isRootSpanInView) {
+      return 0;
+    }
+
+    const avgDepth = Math.round(this.treeDepthSum / this.length);
+    return avgDepth * (TOGGLE_BORDER_BOX / 2) + MARGIN_LEFT;
   }
 }
