c3e53ae932a421cf818dbb99da77c08082d47d3e
Dublin Anondson <stratophone2112@gmail.com>
2022-05-25 08:27:34 -0500

    style: adjusted styles so borders aren't clashing with the console row styling

--- static/app/views/replays/detail/console/consoleMessage.tsx
+++ static/app/views/replays/detail/console/consoleMessage.tsx
@@ -132,7 +132,12 @@ const ConsoleTimestamp = styled(Common)<{isLast: boolean; level: string}>`
 const Icon = styled(Common)<{isActive: boolean}>`
   padding: ${space(0.5)} ${space(1)};
   border-left: 4px solid
-${p => (p.isActive ? p.theme.purple300 : p.theme.background)};
+    ${p =>
+      p.isActive
+        ? p.theme.active
+        : ['warning', 'error'].includes(p.level)
+        ? p.theme.alert[p.level].backgroundLight
+        : 'inherit'};
 `;
 const Message = styled(Common)`
   padding: ${space(0.25)} 0;
