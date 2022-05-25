660a65c9b490002871b088cc469ae45bd88f67b5
Marcos Gaeta <marcos.gaeta@sentry.io>
2022-05-25 13:07:48 -0700

    fix commit order

--- src/sentry/models/releasecommit.py
+++ src/sentry/models/releasecommit.py
@@ -1,4 +1,28 @@
-from sentry.db.models import BoundedPositiveIntegerField, FlexibleForeignKey, Model, sane_repr
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+from django.db.models import QuerySet
+
+from sentry.db.models import (
+    BaseManager,
+    BoundedPositiveIntegerField,
+    FlexibleForeignKey,
+    Model,
+    sane_repr,
+)
+
+if TYPE_CHECKING:
+    from sentry.models import Release
+
+
+class ReleaseCommitManager(BaseManager):
+    def get_for_release(self, release: Release) -> QuerySet[ReleaseCommit]:
+        return (
+            self.filter(release=release)
+            .order_by("order")
+            .select_related("commit", "commit__author")
+        )
 
 
 class ReleaseCommit(Model):
@@ -10,6 +34,8 @@ class ReleaseCommit(Model):
     release = FlexibleForeignKey("sentry.Release")
     commit = FlexibleForeignKey("sentry.Commit")
     order = BoundedPositiveIntegerField()
+
+    objects = ReleaseCommitManager()
 
     class Meta:
         app_label = "sentry"
