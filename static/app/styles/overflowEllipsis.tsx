dcafca418108c0ae5b3c2229f331aa19bdbf156a
Vu Luong <vuluongj20@gmail.com>
2022-05-26 11:22:13 -0700

    ref(theme): Return overflowEllipsis.tsx with deprecation tag

---
+++ static/app/styles/overflowEllipsis.tsx
@@ -0,0 +1,15 @@
+/**
+ * CSS styles used to add a ellipsis overflow. This is deprecated, use the
+ * styles from theme instead (``p.theme.overflowEllipsis`).
+ *
+ * @deprecated
+ */
+const overflowEllipsis = `
+  display: block;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  width: 100%;
+`;
+
+export default overflowEllipsis;
