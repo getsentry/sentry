5613b340433ce0805ae2a92cfdbcb3d5077f8775
Alberto Leal <mail4alberto@gmail.com>
2022-05-30 16:58:53 -0400

    update config stub

--- fixtures/js-stubs/config.js
+++ fixtures/js-stubs/config.js
@@ -43,6 +43,8 @@ export function Config(params = {}) {
     apmSampling: 1,
     dsn_requests: '',
     demoMode: false,
+    apiUrl: 'sentry.io',
+    organizationUrl: 'foobar.sentry.io',
     ...params,
   };
 }
