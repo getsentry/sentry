875f5309a093d975a117a2ccaffa6488586b3a2d
Vu Luong <vuluongj20@gmail.com>
2022-05-31 11:16:59 -0700

    ref(types): Use MenuListItemProps in SelectValue

--- static/app/types/core.tsx
+++ static/app/types/core.tsx
@@ -5,6 +5,7 @@
  * or used in multiple views.
  */
 import type {getInterval} from 'sentry/components/charts/utils';
+import {MenuListItemProps} from 'sentry/components/menuListItem';
 import type {InternalTooltipProps} from 'sentry/components/tooltip';
 import type {API_ACCESS_SCOPES} from 'sentry/constants';
 
@@ -45,17 +46,12 @@ export type Writable<T> = {-readonly [K in keyof T]: T[K]};
 /**
  * The option format used by react-select based components
  */
-export type SelectValue<T> = {
+export type SelectValue<T> = MenuListItemProps & {
   label: string | number | React.ReactElement;
   value: T;
-  details?: React.ReactNode;
   disabled?: boolean;
-  leadingItems?: React.ReactNode;
-  leadingItemsSpanFullHeight?: boolean;
   tooltip?: React.ReactNode;
   tooltipOptions?: Omit<InternalTooltipProps, 'children' | 'title' | 'className'>;
-  trailingItems?: React.ReactNode;
-  trailingItemsSpanFullHeight?: boolean;
 };
 
 /**
