e9385d0a667f1cbd8bd18894c0f1b5474c7fb42a
Floris Bruynooghe <flub@sentry.io>
2022-05-31 11:39:13 +0200

    ref(projectconfig): Late acknowledge of project config computation (#35107)
    This is an experiment to only delete the debounce key after the
    project config is computed.  This results in cache invalidations
    getting lost but is a cheap way to experiment how the system would
    behave if we would debounce better.

--- tests/sentry/api/endpoints/test_relay_projectconfigs_v3.py
+++ tests/sentry/api/endpoints/test_relay_projectconfigs_v3.py
@@ -66,9 +66,9 @@ def call_endpoint(client, relay, private_key, default_projectkey):
 
 @pytest.fixture(autouse=True)
 def max_sample_rate():
-    import sentry.options
+    from sentry import options
 
-    sentry.options.set("relay.project-config-v3-enable", 1)
+    options.set("relay.project-config-v3-enable", 1)
 
 
 @pytest.fixture
@@ -181,6 +181,11 @@ def test_debounce_task_if_proj_config_not_cached_already_enqueued(
     assert task_mock.call_count == 0
 
 
+# This checks that if a race occurred during scheduling of the task and two were scheduled,
+# only one is actually executed since only one can delete the debouncing key.  This race is
+# allowed to happen for the experiment where we delete the debouncing key at the end of the
+# computation.
+@pytest.mark.xfail
 @patch("sentry.relay.projectconfig_cache.set_many")
 @pytest.mark.django_db
 def test_task_doesnt_run_if_not_debounced(
