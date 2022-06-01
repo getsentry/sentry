7f8f26df398808d9f7a993771f7f21790b457647
Jonas <jonas@badalic.com>
2022-06-01 10:44:21 -0400

    feat(profiling): zoom to frame from call tree table (#35060)
    * feat(profiling): add zoom into
    
    * Update static/app/components/profiling/FrameStack/frameStackContextMenu.tsx
    
    Co-authored-by: Tony Xiao <txiao@sentry.io>
    
    * Update static/app/components/profiling/FrameStack/frameStackContextMenu.tsx
    
    Co-authored-by: Tony Xiao <txiao@sentry.io>
    
    * feat(profiling): import t
    
    * feat(profiling): add keyboard nav to table (#35148)
    
    Co-authored-by: Tony Xiao <txiao@sentry.io>

--- static/app/utils/profiling/hooks/useKeyboardNavigation.tsx
+++ static/app/utils/profiling/hooks/useKeyboardNavigation.tsx
@@ -1,10 +1,53 @@
-import {useEffect, useState} from 'react';
+import {useCallback, useEffect, useState} from 'react';
+
+export function useRovingTabIndex(items: any[]) {
+  const [tabIndex, setTabIndex] = useState<number | null>(null);
+
+  const onKeyDown = useCallback(
+    (evt: React.KeyboardEvent) => {
+      if (items.length === 0) {
+        return;
+      }
+
+      if (evt.key === 'Escape') {
+        setTabIndex(null);
+      }
+
+      if (evt.key === 'ArrowDown' || evt.key === 'Tab') {
+        evt.preventDefault();
+
+        if (tabIndex === items.length - 1 || tabIndex === null) {
+          setTabIndex(0);
+        } else {
+          setTabIndex((tabIndex ?? 0) + 1);
+        }
+      }
+
+      if (evt.key === 'ArrowUp' || (evt.key === 'Tab' && evt.shiftKey)) {
+        evt.preventDefault();
+
+        if (tabIndex === 0 || tabIndex === null) {
+          setTabIndex(items.length - 1);
+        } else {
+          setTabIndex((tabIndex ?? 0) - 1);
+        }
+      }
+    },
+    [tabIndex, items]
+  );
+
+  return {
+    tabIndex,
+    setTabIndex,
+    onKeyDown,
+  };
+}
 
 export function useKeyboardNavigation() {
   const [menuRef, setMenuRef] = useState<HTMLDivElement | null>(null);
-  const [tabIndex, setTabIndex] = useState<number | null>(null);
-
   const items: {id: number; node: HTMLElement | null}[] = [];
+
+  const {tabIndex, setTabIndex, onKeyDown} = useRovingTabIndex(items);
 
   useEffect(() => {
     if (menuRef) {
@@ -14,43 +57,15 @@ export function useKeyboardNavigation() {
     }
   }, [menuRef, tabIndex]);
 
-  function getMenuKeyboardEventHandlers() {
+  function getMenuProps() {
     return {
       tabIndex: -1,
       ref: setMenuRef,
-      onKeyDown: (evt: React.KeyboardEvent) => {
-        if (items.length === 0) {
-          return;
-        }
-
-        if (evt.key === 'Escape') {
-          setTabIndex(null);
-        }
-
-        if (evt.key === 'ArrowDown' || evt.key === 'Tab') {
-          evt.preventDefault();
-
-          if (tabIndex === items.length - 1 || tabIndex === null) {
-            setTabIndex(0);
-          } else {
-            setTabIndex((tabIndex ?? 0) + 1);
-          }
-        }
-
-        if (evt.key === 'ArrowUp' || (evt.key === 'Tab' && evt.shiftKey)) {
-          evt.preventDefault();
-
-          if (tabIndex === 0 || tabIndex === null) {
-            setTabIndex(items.length - 1);
-          } else {
-            setTabIndex((tabIndex ?? 0) - 1);
-          }
-        }
-      },
+      onKeyDown,
     };
   }
 
-  function getMenuItemKeyboardEventHandlers() {
+  function getItemProps() {
     const idx = items.length;
     items.push({id: idx, node: null});
 
@@ -67,46 +82,14 @@ export function useKeyboardNavigation() {
       onMouseEnter: () => {
         setTabIndex(idx);
       },
-      onKeyDown: (evt: React.KeyboardEvent) => {
-        if (items.length === 0) {
-          return;
-        }
-
-        if (evt.key === 'Escape') {
-          setTabIndex(null);
-        }
-
-        if (evt.key === 'Enter' || evt.key === ' ') {
-          items?.[idx]?.node?.click?.();
-        }
-
-        if (evt.key === 'ArrowDown' || evt.key === 'Tab') {
-          evt.preventDefault();
-
-          if (tabIndex === items.length || tabIndex === null) {
-            setTabIndex(0);
-          } else {
-            setTabIndex((tabIndex ?? 0) + 1);
-          }
-        }
-
-        if (evt.key === 'ArrowUp' || (evt.key === 'Tab' && evt.shiftKey)) {
-          evt.preventDefault();
-
-          if (tabIndex === 0 || tabIndex === null) {
-            setTabIndex(items.length);
-          } else {
-            setTabIndex((tabIndex ?? 0) - 1);
-          }
-        }
-      },
+      onKeyDown,
     };
   }
 
   return {
     menuRef,
-    getMenuItemKeyboardEventHandlers,
-    getMenuKeyboardEventHandlers,
+    getItemProps,
+    getMenuProps,
     tabIndex,
     setTabIndex,
   };
