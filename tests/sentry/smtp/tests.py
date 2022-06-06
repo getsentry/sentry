2615c2e37137fabf968cb9d8d145e4c1f3934479
anthony sottile <anthony.sottile@sentry.io>
2022-06-06 11:59:41 -0400

    ref: fix test failures in sentry.smtp.tests

--- tests/sentry/smtp/tests.py
+++ tests/sentry/smtp/tests.py
@@ -51,7 +51,9 @@ class SentrySMTPTest(TestCase):
 class CaseInsensitiveSignerTests(TestCase):
     def test_it_works(self):
         with self.settings(SECRET_KEY="a"):
-            signer = _CaseInsensitiveSigner()
+            # the default salt is the module path
+            # the class was moved at some point so this sets a constant salt
+            signer = _CaseInsensitiveSigner(salt="sentry.utils.email._CaseInsensitiveSigner")
             assert signer.unsign(signer.sign("foo")) == "foo"
             assert signer.sign("foo") == "foo:wkpxg5djz3d4m0zktktfl9hdzw4"
             assert signer.unsign("foo:WKPXG5DJZ3D4M0ZKTKTFL9HDZW4") == "foo"
