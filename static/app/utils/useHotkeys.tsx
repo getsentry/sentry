2e9f37c5a00e20dc4677862b3304307fe974fa8d
jennmueng <ldrwarlock@gmail.com>
2022-06-06 10:11:47 -0700

    ref: Fix and cleanup from review

--- static/app/utils/useHotkeys.tsx
+++ static/app/utils/useHotkeys.tsx
@@ -18,7 +18,7 @@ export function useHotkeys(
   hotkeys: {callback: (e: KeyboardEvent) => void; match: string[] | string}[],
   deps: React.DependencyList
 ): void {
-  const keysPressedRef = useRef<number[]>([]);
+  const keysPressedRef = useRef<Set<number>>(new Set());
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   const memoizedHotkeys = useMemo(() => hotkeys, deps);
@@ -26,15 +26,19 @@ export function useHotkeys(
   const onKeyDown = useCallback(
     (e: KeyboardEvent) => {
       const removeKey = keyCode => {
-        keysPressedRef.current = keysPressedRef.current.filter(key => key !== keyCode);
+        keysPressedRef.current.delete(keyCode);
       };
 
-      const keysPressed = [...keysPressedRef.current, e.keyCode];
-
-      keysPressedRef.current = keysPressed;
+      keysPressedRef.current.add(e.keyCode);
 
       if (e.metaKey && !modifierKeys.includes(e.keyCode)) {
-        // If command is held, keyup does not work for other keys. https://web.archive.org/web/20160304022453/http://bitspushedaround.com/on-a-few-things-you-may-not-know-about-the-hellish-command-key-and-javascript-events/
+        /*
+          If command/metaKey is held, keyup does not get called for non-modifier keys. See:
+          https://web.archive.org/web/20160304022453/http://bitspushedaround.com/on-a-few-things-you-may-not-know-about-the-hellish-command-key-and-javascript-events/
+
+          So, if the metaKey is held, we just have it remove the key after a set timeout, this allows you to hold the command key down
+          and press the other key again after the timeout removes the key.
+        */
         setTimeout(() => {
           removeKey(e.keyCode);
         }, 500);
@@ -42,13 +46,13 @@ export function useHotkeys(
 
       for (const hotkey of memoizedHotkeys) {
         const matches = (Array.isArray(hotkey.match) ? hotkey.match : [hotkey.match]).map(
-          o => o.trim().split(/(?<!\\)\+/g)
+          o => o.trim().split('+')
         );
 
         for (const keys of matches) {
           if (
-            keys.length === keysPressed.length &&
-            keys.every(key => keysPressed.includes(getKeyCode(key)))
+            keys.length === keysPressedRef.current.size &&
+            keys.every(key => keysPressedRef.current.has(getKeyCode(key)))
           ) {
             hotkey.callback(e);
             break;
