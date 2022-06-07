eb9efae93914036b139b508c1bc9ef43c30dc961
Alberto Leal <mail4alberto@gmail.com>
2022-06-07 03:02:31 -0400

    Update tests/js/spec/stores/configStore.spec.tsx
    Co-authored-by: Mark Story <mark@mark-story.com>

--- tests/js/spec/stores/configStore.spec.tsx
+++ tests/js/spec/stores/configStore.spec.tsx
@@ -3,6 +3,6 @@ import ConfigStore from 'sentry/stores/configStore';
 describe('ConfigStore', () => {
   it('should have apiUrl and organizationUrl', () => {
     expect(ConfigStore.get('sentryUrl')).toEqual('https://sentry.io');
-    expect(ConfigStore.get('organizationUrl')).toEqual('https://foobar.sentry.io');
+    expect(ConfigStore.get('organizationUrl')).toEqual('https://foobar.us.sentry.io');
   });
 });
