66393890889c361f98efc016184189ee5cf7b42e
Armen <armenzg@sentry.io>
2022-06-07 14:27:43 -0400

    fix(dev): Do not export SENTRY_DSN in .envrc
    We started exporting SENTRY_DSN back in #32524 in order to fix errors in lib.sh not being reported.
    
    Having this variable exported can cause a lot of errors from a developers' development to show up as dev env errors.
    
    Exporting inside of lib.sh does not persist in the developers' environment.

--- scripts/do.sh
+++ scripts/do.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This script is an interface to any of the methods of lib.sh
 # Call this script as "do.sh method_from_lib" to execute any function from that library
-set -eu
+set -e
 HERE="$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     pwd -P
