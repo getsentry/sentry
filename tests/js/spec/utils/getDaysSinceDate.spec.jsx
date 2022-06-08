b4524a6a5d29f0f69f4f23f9e4a7d8189d0a94ed
Stephen Cefali <scefali@sentry.io>
2022-06-08 15:13:23 -0700

    adds comment

--- tests/js/spec/utils/getDaysSinceDate.spec.jsx
+++ tests/js/spec/utils/getDaysSinceDate.spec.jsx
@@ -2,6 +2,7 @@ import getDaysSinceDate from 'sentry/utils/getDaysSinceDate';
 
 jest.mock('moment', () => {
   const moment = jest.requireActual('moment');
+  // Jun 06 2022
   moment.now = jest.fn().mockReturnValue(1654492173000);
   return moment;
 });
