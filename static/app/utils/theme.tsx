a73e5640c6caed69909bfe9bccc32371d8840109
JonasBa <jonas.badalic@sentry.io>
2022-06-10 17:57:55 -0400

    test theme

--- static/app/utils/theme.tsx
+++ static/app/utils/theme.tsx
@@ -151,12 +151,12 @@ const generateAliases = (colors: BaseColors) => ({
   /**
    * Primary text color
    */
-  textColor: colors.gray400,
+  textColor: colors.red100,
 
   /**
    * Text that should not have as much emphasis
    */
-  subText: colors.gray300,
+  subText: colors.red300,
 
   /**
    * Background for the main content area of a page?
