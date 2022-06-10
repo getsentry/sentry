47bdc9c3b39b445b056758e54571e4a6caeedf3f
Richard Ma <maxiuyuan1998@gmail.com>
2022-06-10 16:31:51 -0700

    changes made based on pr review and added base case

--- tests/sentry/receivers/test_superuser.py
+++ tests/sentry/receivers/test_superuser.py
@@ -7,11 +7,11 @@ from sentry.testutils import TestCase
 class SuperuserReceiverTest(TestCase):
     def setUp(self):
         super().setUp()
-        self.superuser = User(is_superuser=True, email="test@sentry.io")
-        self.non_superuser = User(is_superuser=False, email="test@sentry.io")
+        self.superuser = User(is_superuser=True, email="superuser_test@sentry.io")
+        self.non__superuser = User(is_superuser=False, email="not_a_superuser_test@sentry.io")
 
         self.superuser_request = self.make_request(user=self.superuser)
-        self.non_superuser_request = self.make_request(user=self.non_superuser)
+        self.non__superuser_request = self.make_request(user=self.non__superuser)
 
     def test_enable_superuser_when_self_hosted_superuser(self):
         with self.settings(
@@ -29,12 +29,16 @@ class SuperuserReceiverTest(TestCase):
             enable_superuser(request=self.superuser_request, user=self.superuser)
             assert is_active_superuser(self.superuser_request)
 
+    def test_enable_superuser_saas_superuser(self):
+        enable_superuser(request=self.superuser_request, user=self.superuser)
+        assert not is_active_superuser(self.superuser_request)
+
     def test_enable_superuser_when_self_hosted_non_superuser(self):
         with self.settings(
             SENTRY_SELF_HOSTED=True, VALIDATE_SUPERUSER_ACCESS_CATEGORY_AND_REASON=False
         ):
-            enable_superuser(request=self.non_superuser_request, user=self.non_superuser)
-            assert not is_active_superuser(self.non_superuser_request)
+            enable_superuser(request=self.non__superuser_request, user=self.non__superuser)
+            assert not is_active_superuser(self.non__superuser_request)
 
     def test_enable_superuser_when_flag_on_non_superuser(self):
         with self.settings(
@@ -42,8 +46,12 @@ class SuperuserReceiverTest(TestCase):
             VALIDATE_SUPERUSER_ACCESS_CATEGORY_AND_REASON=False,
             ENABLE_SU_UPON_LOGIN_FOR_LOCAL_DEV=True,
         ):
-            enable_superuser(request=self.non_superuser_request, user=self.non_superuser)
-            assert not is_active_superuser(self.non_superuser_request)
+            enable_superuser(request=self.non__superuser_request, user=self.non__superuser)
+            assert not is_active_superuser(self.non__superuser_request)
+
+    def test_enable_superuser_saas_non_superuser(self):
+        enable_superuser(request=self.non__superuser_request, user=self.non__superuser)
+        assert not is_active_superuser(self.superuser_request)
 
     def test_disable_superuser_active_superuser(self):
         enable_superuser(request=self.superuser_request, user=self.superuser)
