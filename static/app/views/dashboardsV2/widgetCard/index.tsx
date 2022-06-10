6c17fc2f81270a7299ed718a3586c71e8c2b5cd7
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-06-10 07:59:06 -0400

    normalize queries

--- static/app/views/dashboardsV2/widgetCard/index.tsx
+++ static/app/views/dashboardsV2/widgetCard/index.tsx
@@ -29,7 +29,8 @@ import withOrganization from 'sentry/utils/withOrganization';
 import withPageFilters from 'sentry/utils/withPageFilters';
 
 import {DRAG_HANDLE_CLASS} from '../dashboard';
-import {Widget, WidgetType} from '../types';
+import {DisplayType, Widget, WidgetType} from '../types';
+import {DEFAULT_RESULTS_LIMIT, normalizeQueries} from '../widgetBuilder/utils';
 
 import {DashboardsMEPConsumer, DashboardsMEPProvider} from './dashboardsMEPContext';
 import WidgetCardChartContainer from './widgetCardChartContainer';
@@ -234,6 +235,26 @@ class WidgetCard extends Component<Props, State> {
         const prior = new Date(new Date().setDate(current.getDate() - periodInDays));
         showIncompleteDataAlert = prior < METRICS_BACKED_SESSIONS_START_DATE;
       }
+    }
+    if (widget.displayType === DisplayType.TOP_N) {
+      const newDisplayType = DisplayType.AREA;
+
+      const queries = normalizeQueries({
+        displayType: newDisplayType,
+        queries: widget.queries,
+        widgetType: widget.widgetType ?? WidgetType.DISCOVER,
+        widgetBuilderNewDesign: organization.features.includes(
+          'new-widget-builder-experience-design'
+        ),
+      }).map(query => ({
+        ...query,
+        // Use the last aggregate because that's where the y-axis is stored
+        aggregates: query.aggregates.length
+          ? [query.aggregates[query.aggregates.length - 1]]
+          : [],
+      }));
+      widget.queries = queries;
+      widget.limit = DEFAULT_RESULTS_LIMIT;
     }
     return (
       <ErrorBoundary
