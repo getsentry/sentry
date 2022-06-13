ada61c78219a4b521e23c72a7c9232f83bcd8a5e
Komal Saini <komalsaini@live.com>
2022-06-13 13:15:29 -0700

    BE endpoint to retrieve released commits within a project

---
+++ src/sentry/api/endpoints/project_commits.py
@@ -0,0 +1,42 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from sentry.api.bases.project import ProjectEndpoint, ProjectReleasePermission
+from sentry.api.exceptions import ResourceDoesNotExist
+from sentry.api.serializers import serialize
+from sentry.models import ReleaseCommit
+from sentry.ratelimits.config import SENTRY_RATELIMITER_GROUP_DEFAULTS, RateLimitConfig
+
+
+class ProjectCommitsEndpoint(ProjectEndpoint):
+    permission_classes = (ProjectReleasePermission,)
+    rate_limits = RateLimitConfig(
+        group="CLI", limit_overrides={"GET": SENTRY_RATELIMITER_GROUP_DEFAULTS["default"]}
+    )
+
+    def get(self, request: Request, project) -> Response:
+        """
+        List a Project's Commits
+        `````````````````````````
+
+        Retrieve a list of commits for a given project.
+
+        :pparam string organization_slug: the slug of the organization the
+                                          commit belongs to.
+        :pparam string project_slug: the slug of the project to list the
+                                     commits of.
+        """
+
+        try:
+            queryset = ReleaseCommit.objects.filter(
+                organization_id=project.organization_id, project_id=project.id
+            )
+        except ReleaseCommit.DoesNotExist:
+            raise ResourceDoesNotExist
+
+        return self.paginate(
+            request=request,
+            queryset=queryset,
+            order_by="order",
+            on_results=lambda x: serialize([rc.commit for rc in x], request.user),
+        )
---
+++ tests/sentry/api/endpoints/test_project_commits.py
@@ -0,0 +1,33 @@
+from django.urls import reverse
+
+from sentry.models import Commit, Release, ReleaseCommit, Repository
+from sentry.testutils import APITestCase
+
+
+class ProjectCommitListTest(APITestCase):
+    def test_simple(self):
+        project = self.create_project(name="komal")
+        version = "1.1"
+        repo = Repository.objects.create(organization_id=project.organization_id, name=project.name)
+        release = Release.objects.create(organization_id=project.organization_id, version=version)
+        commit = Commit.objects.create(
+            organization_id=project.organization_id, repository_id=repo.id, key="a" * 40
+        )
+        ReleaseCommit.objects.create(
+            organization_id=project.organization_id,
+            release=release,
+            commit=commit,
+            order=0,
+            project_id=project.id,
+        )
+
+        url = reverse(
+            "sentry-api-0-project-commits",
+            kwargs={"organization_slug": project.organization.slug, "project_slug": project.slug},
+        )
+
+        self.login_as(user=self.user)
+        response = self.client.get(url, format="json")
+
+        assert response.status_code == 200, response.content
+        assert response.data[0]["id"] == commit.key
