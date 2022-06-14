dbf974ac5aa643abf532e80a2b3b3be194adb60b
Richard Ma <maxiuyuan1998@gmail.com>
2022-06-14 14:42:03 -0700

    added extra _ for visibility

--- tests/sentry/receivers/test_superuser.py
+++ tests/sentry/receivers/test_superuser.py
@@ -13,14 +13,14 @@ class SuperuserReceiverTest(TestCase):
         self.superuser_request = self.make_request(user=self.superuser)
         self.non__superuser_request = self.make_request(user=self.non__superuser)
 
-    def test_enable_superuser_when_self_hosted_superuser(self):
+    def test_enable_superuser_when_self_hosted__superuser(self):
         with self.settings(
             SENTRY_SELF_HOSTED=True, VALIDATE_SUPERUSER_ACCESS_CATEGORY_AND_REASON=False
         ):
             enable_superuser(request=self.superuser_request, user=self.superuser)
             assert is_active_superuser(self.superuser_request)
 
-    def test_enable_superuser_when_flag_on_superuser(self):
+    def test_enable_superuser_when_flag_on__superuser(self):
         with self.settings(
             SENTRY_SELF_HOSTED=False,
             VALIDATE_SUPERUSER_ACCESS_CATEGORY_AND_REASON=False,
@@ -29,21 +29,21 @@ class SuperuserReceiverTest(TestCase):
             enable_superuser(request=self.superuser_request, user=self.superuser)
             assert is_active_superuser(self.superuser_request)
 
-    def test_enable_superuser_saas_superuser(self):
+    def test_enable_superuser_saas__superuser(self):
         with self.settings(
             SENTRY_SELF_HOSTED=False,
         ):
             enable_superuser(request=self.superuser_request, user=self.superuser)
             assert not is_active_superuser(self.superuser_request)
 
-    def test_enable_superuser_when_self_hosted_non_superuser(self):
+    def test_enable_superuser_when_self_hosted_non__superuser(self):
         with self.settings(
             SENTRY_SELF_HOSTED=True, VALIDATE_SUPERUSER_ACCESS_CATEGORY_AND_REASON=False
         ):
             enable_superuser(request=self.non__superuser_request, user=self.non__superuser)
             assert not is_active_superuser(self.non__superuser_request)
 
-    def test_enable_superuser_when_flag_on_non_superuser(self):
+    def test_enable_superuser_when_flag_on_non__superuser(self):
         with self.settings(
             SENTRY_SELF_HOSTED=False,
             VALIDATE_SUPERUSER_ACCESS_CATEGORY_AND_REASON=False,
@@ -52,14 +52,14 @@ class SuperuserReceiverTest(TestCase):
             enable_superuser(request=self.non__superuser_request, user=self.non__superuser)
             assert not is_active_superuser(self.non__superuser_request)
 
-    def test_enable_superuser_saas_non_superuser(self):
+    def test_enable_superuser_saas_non__superuser(self):
         with self.settings(
             SENTRY_SELF_HOSTED=False,
         ):
             enable_superuser(request=self.non__superuser_request, user=self.non__superuser)
             assert not is_active_superuser(self.superuser_request)
 
-    def test_disable_superuser_active_superuser(self):
+    def test_disable_superuser_active__superuser(self):
         enable_superuser(request=self.superuser_request, user=self.superuser)
         assert is_active_superuser(self.superuser_request)
 
