894ab352ed3759844ed3b687a13b4346fe292275
Scott Cooper <scttcper@gmail.com>
2022-06-14 16:36:16 -0700

    remove any in timeperiod

--- tests/js/spec/views/alerts/utils/getMetricRuleDiscoverUrl.spec.tsx
+++ tests/js/spec/views/alerts/utils/getMetricRuleDiscoverUrl.spec.tsx
@@ -2,7 +2,7 @@ import {Dataset} from 'sentry/views/alerts/rules/metric/types';
 import {getMetricRuleDiscoverQuery} from 'sentry/views/alerts/utils/getMetricRuleDiscoverUrl';
 
 describe('getMetricRuleDiscoverQuery', () => {
-  it('should use metric aggregate', () => {
+  it('should use metric aggregate in discover query', () => {
     const rule = TestStubs.MetricRule({
       aggregate: 'failure_rate()',
       dataset: Dataset.TRANSACTIONS,
@@ -11,7 +11,12 @@ describe('getMetricRuleDiscoverQuery', () => {
     const query = getMetricRuleDiscoverQuery({
       rule,
       projects,
-      timePeriod: {period: '7d', usingPeriod: true} as any,
+      timePeriod: {
+        period: '7d',
+        usingPeriod: true,
+        start: new Date().toISOString(),
+        end: new Date().toISOString(),
+      },
     });
     expect(query.valueOf()).toEqual(
       expect.objectContaining({
