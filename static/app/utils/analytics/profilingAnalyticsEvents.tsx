b4d82a34bdad1b74194567115c3a33a9464d5ef2
JonasBa <jonas.badalic@sentry.io>
2022-06-15 16:32:56 -0400

    fix(analytics): views -> Views

--- static/app/utils/analytics/profilingAnalyticsEvents.tsx
+++ static/app/utils/analytics/profilingAnalyticsEvents.tsx
@@ -8,8 +8,8 @@ type ProfilingViews =
 type EventKey = `${ProfilingPrefix}_views.${ProfilingViews}`;
 
 export const profilingEventMap: Record<EventKey, string> = {
-  'profiling_views.landing': 'Profiling views: Landing',
-  'profiling_views.flamegraph': 'Profiling views: Flamegraph',
-  'profiling_views.transaction_summary': 'Profiling views: Transaction Summary',
-  'profiling_views.profile_summary': 'Profiling views: Profile Summary',
+  'profiling_views.landing': 'Profiling Views: Landing',
+  'profiling_views.flamegraph': 'Profiling Views: Flamegraph',
+  'profiling_views.transaction_summary': 'Profiling Views: Transaction Summary',
+  'profiling_views.profile_summary': 'Profiling Views: Profile Summary',
 };
