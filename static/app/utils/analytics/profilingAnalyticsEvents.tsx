03f800ff7b68c9ef33d7618e656775dba08094d5
JonasBa <jonas.badalic@sentry.io>
2022-06-16 11:11:25 -0400

    ref(analytics): update naming

--- static/app/utils/analytics/profilingAnalyticsEvents.tsx
+++ static/app/utils/analytics/profilingAnalyticsEvents.tsx
@@ -1,15 +1,15 @@
 type ProfilingPrefix = 'profiling';
 type ProfilingViews =
   | 'landing'
-  | 'transaction_summary'
   | 'profile_summary'
-  | 'flamegraph';
+  | 'profile_details'
+  | 'profile_flamegraph';
 
 type EventKey = `${ProfilingPrefix}_views.${ProfilingViews}`;
 
 export const profilingEventMap: Record<EventKey, string> = {
   'profiling_views.landing': 'Profiling Views: Landing',
-  'profiling_views.flamegraph': 'Profiling Views: Flamegraph',
-  'profiling_views.transaction_summary': 'Profiling Views: Transaction Summary',
+  'profiling_views.profile_flamegraph': 'Profiling Views: Flamegraph',
   'profiling_views.profile_summary': 'Profiling Views: Profile Summary',
+  'profiling_views.profile_details': 'Profiling Views: Profile Details',
 };
