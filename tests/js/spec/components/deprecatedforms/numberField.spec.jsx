d214e52f1b54b3548cbc0e3d5b708594b97b9d88
Scott Cooper <scttcper@users.noreply.github.com>
2022-06-16 09:46:56 -0700

    test(ui): Convert Numberfield to RTL (#35730)

--- tests/js/spec/components/deprecatedforms/numberField.spec.jsx
+++ tests/js/spec/components/deprecatedforms/numberField.spec.jsx
@@ -1,4 +1,4 @@
-import {mountWithTheme} from 'sentry-test/enzyme';
+import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import {NumberField} from 'sentry/components/deprecatedforms';
 import Form from 'sentry/components/deprecatedforms/form';
@@ -6,37 +6,40 @@ import Form from 'sentry/components/deprecatedforms/form';
 describe('NumberField', function () {
   describe('render()', function () {
     it('renders', function () {
-      const wrapper = mountWithTheme(<NumberField name="fieldName" />);
-      expect(wrapper).toSnapshot();
+      const wrapper = render(<NumberField name="fieldName" />);
+      expect(wrapper.container).toSnapshot();
     });
 
     it('renders with optional attributes', function () {
-      const wrapper = mountWithTheme(<NumberField name="fieldName" min={0} max={100} />);
-      expect(wrapper).toSnapshot();
+      const wrapper = render(<NumberField name="fieldName" min="0" max="100" />);
+      expect(wrapper.container).toSnapshot();
     });
 
     it('renders with value', function () {
-      const wrapper = mountWithTheme(<NumberField name="fieldName" value={5} />);
-      expect(wrapper).toSnapshot();
+      render(<NumberField name="fieldName" value={5} />);
+      expect(screen.getByRole('spinbutton')).toHaveValue(5);
     });
 
     it('renders with form context', function () {
-      const wrapper = mountWithTheme(
+      render(
         <Form initialData={{fieldName: 5}}>
           <NumberField name="fieldName" />
         </Form>
       );
-      expect(wrapper).toSnapshot();
+      expect(screen.getByRole('spinbutton')).toHaveValue(5);
     });
 
     it('doesnt save `NaN` when new value is empty string', function () {
-      const wrapper = mountWithTheme(
-        <Form onSubmit={() => {}}>
-          <NumberField name="fieldName" defaultValue="2" />
+      render(
+        <Form initialData={{fieldName: 5}}>
+          <NumberField name="fieldName" defaultValue="5" />
         </Form>
       );
-      wrapper.find('input').simulate('change', {target: {value: ''}});
-      expect(wrapper.state('data').fieldName).toBe('');
+
+      expect(screen.getByRole('spinbutton')).toHaveValue(5);
+
+      userEvent.clear(screen.getByRole('spinbutton'));
+      expect(screen.getByRole('spinbutton')).toHaveValue(null);
     });
   });
 });
