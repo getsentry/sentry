cd071bf7b93216fc8bbeb4561e72029d44d5a001
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-06-16 23:04:22 +0200

    add one more try-except

--- src/sentry/models/team.py
+++ src/sentry/models/team.py
@@ -102,16 +102,21 @@ class TeamManager(BaseManager):
         self.process_resource_change(instance, **kwargs)
 
     def process_resource_change(self, instance, **kwargs):
+        from sentry.models import Organization, Project
         from sentry.tasks.codeowners import update_code_owners_schema
 
-        transaction.on_commit(
-            lambda: update_code_owners_schema.apply_async(
-                kwargs={
-                    "organization": instance.organization,
-                    "projects": instance.get_projects(),
-                }
-            )
-        )
+        def _spawn_task():
+            try:
+                update_code_owners_schema.apply_async(
+                    kwargs={
+                        "organization": instance.organization,
+                        "projects": instance.get_projects(),
+                    }
+                )
+            except (Organization.DoesNotExist, Project.DoesNotExist):
+                pass
+
+        transaction.on_commit(_spawn_task)
 
 
 # TODO(dcramer): pull in enum library
