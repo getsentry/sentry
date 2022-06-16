0283f5f8ce08262bd74410e255b8b1e631d502c8
Vu Luong <vuluongj20@gmail.com>
2022-06-16 14:18:37 -0700

    fix(discover): Always show the year for timestamp fields

--- static/app/utils/discover/fieldRenderers.tsx
+++ static/app/utils/discover/fieldRenderers.tsx
@@ -437,7 +437,7 @@ const SPECIAL_FIELDS: SpecialFields = {
     renderFunc: data => (
       <Container>
         {getDynamicText({
-          value: <FieldDateTime date={data['timestamp.to_hour']} format="lll z" />,
+          value: <FieldDateTime date={data['timestamp.to_hour']} year timeZone />,
           fixed: 'timestamp.to_hour',
         })}
       </Container>
@@ -448,7 +448,7 @@ const SPECIAL_FIELDS: SpecialFields = {
     renderFunc: data => (
       <Container>
         {getDynamicText({
-          value: <FieldDateTime date={data['timestamp.to_day']} dateOnly utc />,
+          value: <FieldDateTime date={data['timestamp.to_day']} dateOnly year utc />,
           fixed: 'timestamp.to_day',
         })}
       </Container>
