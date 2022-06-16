adea89c922ecc734298f74c8a0ab996d61eae438
William Mak <william@wmak.io>
2022-06-16 18:19:18 -0400

    feat(mep): Add support for groupby_columns
    - This is so you can change the groupby of a query without having to
      select those columns as well

--- tests/sentry/search/events/test_builder.py
+++ tests/sentry/search/events/test_builder.py
@@ -644,6 +644,35 @@ class QueryBuilderTest(TestCase):
         # field:a or (field:b and event.type:transaction)
         assert constants.QUERY_TIPS["CHAINED_OR"] not in query.tips["query"]
 
+    def test_group_by_not_in_select(self):
+        query = QueryBuilder(
+            Dataset.Discover,
+            self.params,
+            "",
+            selected_columns=[
+                "count()",
+                "event.type",
+            ],
+            groupby_columns=[
+                "transaction",
+            ],
+        )
+        snql_query = query.get_snql_query().query
+        self.assertCountEqual(
+            snql_query.select,
+            [
+                Function("count", [], "count"),
+                AliasedExpression(Column("type"), "event.type"),
+            ],
+        )
+        self.assertCountEqual(
+            snql_query.groupby,
+            [
+                AliasedExpression(Column("type"), "event.type"),
+                Column("transaction"),
+            ],
+        )
+
 
 def _metric_percentile_definition(
     org_id, quantile, field="transaction.duration", alias=None
@@ -1988,6 +2017,44 @@ class MetricQueryBuilderTest(MetricBuilderBaseTest):
             use_aggregate_conditions=True,
             # Use dry run for now to not hit indexer
             dry_run=True,
+        )
+
+    def test_group_by_not_in_select(self):
+        query = MetricsQueryBuilder(
+            self.params,
+            "",
+            selected_columns=[
+                "p90(transaction.duration)",
+                "project",
+            ],
+            groupby_columns=[
+                "transaction",
+            ],
+        )
+        snql_query = query.get_snql_query().query
+        project = Function(
+            "transform",
+            [
+                Column("project_id"),
+                [self.project.id],
+                [self.project.slug],
+                "",
+            ],
+            "project",
+        )
+        self.assertCountEqual(
+            snql_query.select,
+            [
+                _metric_percentile_definition(self.organization.id, "90"),
+                project,
+            ],
+        )
+        self.assertCountEqual(
+            snql_query.groupby,
+            [
+                project,
+                Column(f"tags[{indexer.resolve(self.organization.id, 'transaction')}]"),
+            ],
         )
 
 
