a9a5f985b0b2ecd2006de122788a5beb0b93129d
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-06-20 12:42:47 +0200

    push some review feedback

--- src/sentry/ratelimits/sliding_windows.py
+++ src/sentry/ratelimits/sliding_windows.py
@@ -45,12 +45,31 @@ class Quota:
     # contain the prefix of the RequestedQuota
     prefix_override: Optional[str] = None
 
-    def iter_granules(self, request_timestamp: int) -> Iterator[int]:
+    def __post__init__(self):
         assert self.window_seconds % self.granularity_seconds == 0
+
+    def iter_window(self, request_timestamp: int) -> Iterator[int]:
+        """
+        Iterate over the quota's window, yielding timestamps representing each granule.
+
+        This function is used to calculate keys for storing the number of
+        requests made in each granule.
+
+        The iteration is done in reverse-order (newest timestamp to oldest),
+        starting with the key to which a currently-processed request should be
+        added. That request's timestamp is `request_timestamp`.
+
+        * `request_timestamp / self.granularity_seconds`
+        * `request_timestamp / self.granularity_seconds - 1`
+        * `request_timestamp / self.granularity_seconds - 2`
+        * ...
+        """
+        value = int(request_timestamp / self.granularity_seconds)
+
         for granule_i in range(int(self.window_seconds / self.granularity_seconds)):
-            yield int(
-                request_timestamp / self.granularity_seconds
-            ) - granule_i * self.granularity_seconds
+            value -= 1
+            assert value >= 0
+            yield value
 
 
 @dataclass(frozen=True)
@@ -89,6 +108,15 @@ class SlidingWindowRateLimiter(Service):
         """
         Given a set of quotas requests and limits, compute how much quota could
         be consumed.
+
+        :param requests: The requests to return "grants" for.
+        :param timestamp: The timestamp of the incoming request. Defaults to
+            the current timestamp.
+
+            Providing a too old timestamp here _can_ effectively disable rate
+            limits, as the older request counts may no longer be stored.
+            However, consistently providing old timestamps here will work
+            correctly.
         """
         raise NotImplementedError()
 
@@ -99,11 +127,61 @@ class SlidingWindowRateLimiter(Service):
         Given a set of requests and the corresponding return values from
         `check_within_quotas`, consume the quotas.
 
-        This is a separate method because 1) the redis backend is not strongly
-        consistent anyway 2) the metrics string indexer may not want to
-        increment quotas if it crashes halfway through the database write. In
-        other words, it may want to first check quotas, execute the writes,
-        then apply the quotas.
+        :param requests: The requests that have previously been passed to
+            `check_within_quotas`.
+        :param timestamp: The request timestamp that has previously been passed
+            to `check_within_quotas`.
+        :param grants: The return value of `check_within_quotas` which
+            indicates how much quota should actually be consumed.
+
+        Why is checking quotas and using quotas two separate implementations?
+        Isn't that a time-of-check-time-of-use bug, and allows me to over-spend
+        quota when requests are happening concurrently?
+
+        1) It's desirable to first check quotas, then do a potentially fallible
+           operation, then consume quotas. This rate limiter is primarily going to
+           be used inside of the metrics string indexer to rate-limit database
+           writes. What we want to do there is: read DB, check rate limits,
+           write to DB, use rate limits.
+
+           If we did it any other way (the obvious one being to read DB,
+           check-and-use rate limits, write DB), crashes while writing to the
+           database can over-consume quotas. This is not a big problem if those
+           crashes are flukes, and especially not a problem if the crashes are
+           a result of an overloaded DB.
+
+           It is however a problem in case the consumer is crash-looping, or
+           crashing (quickly) for 100% of requests (e.g. due to schema
+           mismatches between code and DB that somehow don't surface during the
+           DB read). In that case the quotas would be consumed immediately and
+           incident recovery would require us to reset all quotas manually (or
+           disable rate limiting via some killswitch)
+
+        3) The redis backend (really the only backend we care about) already
+           has some consistency problems.
+
+           a) Redis only provides strong consistency and ability to
+              check-and-increment counters when all involved keys hit the same
+              Redis node. That means that a quota with prefix="org_id:123" can
+              only run on a single redis node. It also means that a global
+              quota (`prefix="global"`) would have to run on a single
+              (unchangeable) redis node to be strongly consistent. That's
+              however a problem for scalability.
+
+              There's no obvious way to make global quotas consistent with
+              per-org quotas this way, so right now it means that requests
+              containing multiple quotas with different `prefixes` cannot be
+              checked-and-incremented atomically even if we were to change the
+              rate-limiter's interface.
+
+           b) This is easily fixable, but because of the above, we
+              currently don't control Redis sharding at all, meaning that even
+              keys within a single quota's window will hit different Redis
+              node. This also helps further distribute the load internally.
+
+              Since we have given up on atomic check-and-increments in general
+              anyway, there's no reason to explicitly control sharding.
+
         """
         raise NotImplementedError()
 
@@ -111,7 +189,8 @@ class SlidingWindowRateLimiter(Service):
         self, requests: Sequence[RequestedQuota], timestamp: Optional[int] = None
     ) -> Sequence[GrantedQuota]:
         """
-        Check the quota requests in Redis and consume the quota in one go.
+        Check the quota requests in Redis and consume the quota in one go. See
+        `check_within_quotas` for parameters.
         """
         timestamp, grants = self.check_within_quotas(requests, timestamp)
         self.use_quotas(requests, grants, timestamp)
@@ -163,7 +242,7 @@ class RedisSlidingWindowRateLimiter(SlidingWindowRateLimiter):
             assert request.quotas
 
             for quota in request.quotas:
-                for granule in quota.iter_granules(timestamp):
+                for granule in quota.iter_window(timestamp):
                     keys_to_fetch.append(
                         self._build_redis_key(request=request, quota=quota, granule=granule)
                     )
@@ -182,7 +261,7 @@ class RedisSlidingWindowRateLimiter(SlidingWindowRateLimiter):
                         )
                         or 0
                     )
-                    for granule in quota.iter_granules(timestamp)
+                    for granule in quota.iter_window(timestamp)
                 )
 
                 granted_quota = max(0, min(granted_quota, quota.limit - used_quota))
@@ -203,7 +282,7 @@ class RedisSlidingWindowRateLimiter(SlidingWindowRateLimiter):
 
             for quota in request.quotas:
                 # Only incr most recent granule
-                granule = next(quota.iter_granules(timestamp))
+                granule = next(quota.iter_window(timestamp))
                 key = self._build_redis_key(request=request, quota=quota, granule=granule)
                 assert key not in keys_to_incr, "conflicting quotas specified"
                 keys_to_incr[key] = grant.granted, quota.window_seconds
