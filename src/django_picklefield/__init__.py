2b94d5d129cb5c0c14e769196ed16444699fb809
Aniket Das <aniket.das@sentry.io>
2022-06-21 09:41:29 -0700

    Remove versioning code

--- src/django_picklefield/__init__.py
+++ src/django_picklefield/__init__.py
@@ -1,10 +1,4 @@
-import django.utils.version
-
 from .constants import DEFAULT_PROTOCOL
 from .fields import PickledObjectField
 
-__all__ = "VERSION", "__version__", "DEFAULT_PROTOCOL", "PickledObjectField"
-
-VERSION = (3, 0, 1, "final", 0)
-
-__version__ = django.utils.version.get_version(VERSION)
+__all__ = "DEFAULT_PROTOCOL", "PickledObjectField"
