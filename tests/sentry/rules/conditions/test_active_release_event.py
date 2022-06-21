1428db6d1f102362ad531b72f5bbc095ae7deb21
Gilbert Szeto <gilbert.szeto@sentry.io>
2022-06-21 12:10:31 -0700

    active release event test

---
+++ tests/sentry/rules/conditions/test_active_release_event.py
@@ -0,0 +1,54 @@
+from sentry.eventstore import Filter
+from sentry.eventstore.snuba import SnubaEventStorage
+from sentry.models import Group, Rule
+from sentry.rules.conditions.active_release import ActiveReleaseEventCondition
+from sentry.testutils import RuleTestCase, SnubaTestCase
+
+
+class ActiveReleaseEventConditionTest(SnubaTestCase, RuleTestCase):
+    rule_cls = ActiveReleaseEventCondition
+
+    def setUp(self):
+        super().setUp()
+        self.eventstore = SnubaEventStorage()
+
+    def test_applies_correctly(self):
+        rule = self.get_rule(rule=Rule(environment_id=1))
+
+        self.assertDoesNotPass(rule, self.event, is_new=True)
+
+    # XXX(gilbert): delete this later
+    def test_event_group_last_release_version(self):
+        evt = self.store_event(
+            data={
+                "event_id": "a" * 32,
+                "message": "\u3053\u3093\u306b\u3061\u306f",
+            },
+            project_id=self.project.id,
+        )
+        evt2 = self.store_event(
+            data={
+                "event_id": "b" * 32,
+                "message": "\u3053\u3093\u306b\u3061\u306f",
+            },
+            project_id=self.project.id,
+        )
+
+        evts = self.eventstore.get_events(
+            filter=Filter(
+                project_ids=[self.project.id],
+                event_ids=[evt.event_id, evt2.event_id],
+            )
+        )
+
+        assert len(evts) == 2
+
+        assert evt.event_id != evt2.event_id
+        assert evt.group_id == 1
+        assert list(Group.objects.filter(id__in=(1, 2)).values_list("id", flat=True)) == [1]
+        assert self.group.id == 2
+        # assert self.group.id == evt.group_id
+
+        rule = self.get_rule(rule=Rule(environment_id=1))
+
+        self.assertDoesNotPass(rule, evt, is_new=True)
