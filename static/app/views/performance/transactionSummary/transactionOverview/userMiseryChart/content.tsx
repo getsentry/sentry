f835de82f01a2d6f9d30f17d7d2a985dc5ec42ac
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-06-22 11:17:25 -0400

    wip

---
+++ static/app/views/performance/transactionSummary/transactionOverview/userMiseryChart/content.tsx
@@ -0,0 +1,141 @@
+import {InjectedRouter} from 'react-router';
+import {Query} from 'history';
+
+import {AreaChart} from 'sentry/components/charts/areaChart';
+import ChartZoom from 'sentry/components/charts/chartZoom';
+import ErrorPanel from 'sentry/components/charts/errorPanel';
+import {LineChartProps} from 'sentry/components/charts/lineChart';
+import ReleaseSeries from 'sentry/components/charts/releaseSeries';
+import TransitionChart from 'sentry/components/charts/transitionChart';
+import TransparentLoadingMask from 'sentry/components/charts/transparentLoadingMask';
+import Placeholder from 'sentry/components/placeholder';
+import {IconWarning} from 'sentry/icons';
+import {Series} from 'sentry/types/echarts';
+import {axisLabelFormatter, tooltipFormatter} from 'sentry/utils/discover/charts';
+import getDynamicText from 'sentry/utils/getDynamicText';
+import {Theme} from 'sentry/utils/theme';
+
+type Props = {
+  errored: boolean;
+  loading: boolean;
+  queryExtra: Query;
+  reloading: boolean;
+  router: InjectedRouter;
+  theme: Theme;
+  series?: Series[];
+  timeFrame?: {
+    end: number;
+    start: number;
+  };
+} & Omit<React.ComponentProps<typeof ReleaseSeries>, 'children' | 'queryExtra'> &
+  Pick<LineChartProps, 'onLegendSelectChanged' | 'legend'>;
+
+function Content({
+  errored,
+  theme,
+  series: data,
+  timeFrame,
+  start,
+  end,
+  period,
+  projects,
+  environments,
+  loading,
+  reloading,
+  legend,
+  utc,
+  queryExtra,
+  router,
+  onLegendSelectChanged,
+}: Props) {
+  if (errored) {
+    return (
+      <ErrorPanel>
+        <IconWarning color="gray500" size="lg" />
+      </ErrorPanel>
+    );
+  }
+
+  const chartOptions = {
+    grid: {
+      left: '10px',
+      right: '10px',
+      top: '40px',
+      bottom: '0px',
+    },
+    seriesOptions: {
+      showSymbol: false,
+    },
+    tooltip: {
+      trigger: 'axis' as const,
+      valueFormatter: tooltipFormatter,
+    },
+    xAxis: timeFrame
+      ? {
+          min: timeFrame.start,
+          max: timeFrame.end,
+        }
+      : undefined,
+    yAxis: {
+      axisLabel: {
+        color: theme.chartLabel,
+        // p50() coerces the axis to be time based
+        formatter: (value: number) => axisLabelFormatter(value, 'p50()'),
+      },
+    },
+  };
+
+  const colors = (data && theme.charts.getColorPalette(data.length - 2)) || [];
+
+  // Create a list of series based on the order of the fields,
+  // We need to flip it at the end to ensure the series stack right.
+  const series = data
+    ? data
+        .map((values, i: number) => {
+          return {
+            ...values,
+            color: colors[i],
+            lineStyle: {
+              opacity: 0,
+            },
+          };
+        })
+        .reverse()
+    : [];
+
+  return (
+    <ChartZoom router={router} period={period} start={start} end={end} utc={utc}>
+      {zoomRenderProps => (
+        <ReleaseSeries
+          start={start}
+          end={end}
+          queryExtra={queryExtra}
+          period={period}
+          utc={utc}
+          projects={projects}
+          environments={environments}
+        >
+          {({releaseSeries}) => (
+            <TransitionChart loading={loading} reloading={reloading}>
+              <TransparentLoadingMask visible={reloading} />
+              {getDynamicText({
+                value: (
+                  <AreaChart
+                    {...zoomRenderProps}
+                    {...chartOptions}
+                    legend={legend}
+                    onLegendSelectChanged={onLegendSelectChanged}
+                    series={[...series, ...releaseSeries]}
+                  />
+                ),
+                fixed: <Placeholder height="200px" testId="skeleton-ui" />,
+              })}
+            </TransitionChart>
+          )}
+        </ReleaseSeries>
+      )}
+    </ChartZoom>
+  );
+}
+
+export default Content;
---
+++ static/app/views/performance/transactionSummary/transactionOverview/userMiseryChart/index.tsx
@@ -0,0 +1,154 @@
+import {Fragment} from 'react';
+import {browserHistory, withRouter, WithRouterProps} from 'react-router';
+import {useTheme} from '@emotion/react';
+import {Location, Query} from 'history';
+
+import EventsRequest from 'sentry/components/charts/eventsRequest';
+import {HeaderTitleLegend} from 'sentry/components/charts/styles';
+import {getInterval, getSeriesSelection} from 'sentry/components/charts/utils';
+import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
+import QuestionTooltip from 'sentry/components/questionTooltip';
+import {t, tct} from 'sentry/locale';
+import {OrganizationSummary} from 'sentry/types';
+import {getUtcToLocalDateObject} from 'sentry/utils/dates';
+import {useMEPSettingContext} from 'sentry/utils/performance/contexts/metricsEnhancedSetting';
+import useApi from 'sentry/utils/useApi';
+import {getMEPQueryParams} from 'sentry/views/performance/landing/widgets/utils';
+
+import {ViewProps} from '../../../types';
+import {SpanOperationBreakdownFilter} from '../../filter';
+
+import Content from './content';
+
+type Props = WithRouterProps &
+  ViewProps & {
+    currentFilter: SpanOperationBreakdownFilter;
+    location: Location;
+    organization: OrganizationSummary;
+    queryExtra: Query;
+    withoutZerofill: boolean;
+  };
+
+/**
+ * Fetch and render a stacked area chart that shows duration percentiles over
+ * the past 7 days
+ */
+function UserMiseryChart({
+  project,
+  environment,
+  location,
+  organization,
+  query,
+  statsPeriod,
+  router,
+  queryExtra,
+  currentFilter,
+  withoutZerofill,
+  start: propsStart,
+  end: propsEnd,
+}: Props) {
+  const api = useApi();
+  const theme = useTheme();
+  const mepContext = useMEPSettingContext();
+
+  function handleLegendSelectChanged(legendChange: {
+    name: string;
+    selected: Record<string, boolean>;
+    type: string;
+  }) {
+    const {selected} = legendChange;
+    const unselected = Object.keys(selected).filter(key => !selected[key]);
+
+    const to = {
+      ...location,
+      query: {
+        ...location.query,
+        unselectedSeries: unselected,
+      },
+    };
+
+    browserHistory.push(to);
+  }
+
+  const start = propsStart ? getUtcToLocalDateObject(propsStart) : null;
+  const end = propsEnd ? getUtcToLocalDateObject(propsEnd) : null;
+  const utc = normalizeDateTimeParams(location.query).utc === 'true';
+  const period = statsPeriod;
+
+  const legend = {right: 10, top: 5, selected: getSeriesSelection(location)};
+  const datetimeSelection = {start, end, period};
+
+  const contentCommonProps = {
+    theme,
+    router,
+    start,
+    end,
+    utc,
+    legend,
+    queryExtra,
+    period,
+    projects: project,
+    environments: environment,
+    onLegendSelectChanged: handleLegendSelectChanged,
+  };
+
+  const requestCommonProps = {
+    api,
+    start,
+    end,
+    project,
+    environment,
+    query,
+    period,
+    interval: getInterval(datetimeSelection, 'high'),
+  };
+
+  const header = (
+    <HeaderTitleLegend>
+      {currentFilter === SpanOperationBreakdownFilter.None
+        ? t('Duration Breakdown')
+        : tct('Span Operation Breakdown - [operationName]', {
+            operationName: currentFilter,
+          })}
+      <QuestionTooltip
+        size="sm"
+        position="top"
+        title={t(
+          `Duration Breakdown reflects transaction durations by percentile over time.`
+        )}
+      />
+    </HeaderTitleLegend>
+  );
+
+  const yAxis = 'user_misery()';
+
+  return (
+    <Fragment>
+      {header}
+      <EventsRequest
+        {...requestCommonProps}
+        organization={organization}
+        showLoading={false}
+        includePrevious={false}
+        yAxis={yAxis}
+        partial
+        withoutZerofill={withoutZerofill}
+        referrer="api.performance.transaction-summary.duration-chart"
+        queryExtras={getMEPQueryParams(mepContext)}
+      >
+        {({results, errored, loading, reloading, timeframe: timeFrame}) => (
+          <Content
+            series={results}
+            errored={errored}
+            loading={loading}
+            reloading={reloading}
+            timeFrame={timeFrame}
+            {...contentCommonProps}
+          />
+        )}
+      </EventsRequest>
+    </Fragment>
+  );
+}
+
+export default withRouter(UserMiseryChart);
