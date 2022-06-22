208718d979c370c59f5e6f7219dc106420b17481
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-22 19:12:34 -0400

    spelling: exhausted
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/sentry/testutils/helpers/response.py
+++ src/sentry/testutils/helpers/response.py
@@ -4,7 +4,7 @@ from django.http.response import StreamingHttpResponse
 def close_streaming_response(response: StreamingHttpResponse) -> None:
     """Exhausts the streamed file in a response.
 
-    When the file is exahusted, this underlying file descriptor is closed
+    When the file is exhausted, this underlying file descriptor is closed
     avoiding a `ResourceWarning`.
     """
     for _ in response.streaming_content:
