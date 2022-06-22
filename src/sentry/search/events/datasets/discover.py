14a1b8e4c1457fb9611efcfc0f4384a2d0db3a84
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-21 20:41:33 -0400

    spelling: critically
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- src/sentry/search/events/datasets/discover.py
+++ src/sentry/search/events/datasets/discover.py
@@ -1391,7 +1391,7 @@ class DiscoverDatasetConfig(DatasetConfig):
         Parses a release stage search and returns a snuba condition to filter to the
         requested releases.
         """
-        # TODO: Filter by project here as well. It's done elsewhere, but could critcally limit versions
+        # TODO: Filter by project here as well. It's done elsewhere, but could critically limit versions
         # for orgs with thousands of projects, each with their own releases (potentailly drowning out ones we care about)
 
         if "organization_id" not in self.builder.params:
