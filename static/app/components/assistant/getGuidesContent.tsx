e87afaac86ca974a7fe1a1482674d97ad4279595
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-06-22 10:58:15 -0400

    spell properly and add date threshold

--- static/app/components/assistant/getGuidesContent.tsx
+++ static/app/components/assistant/getGuidesContent.tsx
@@ -214,12 +214,13 @@ export default function getGuidesContent(orgSlug: string | null): GuidesContent 
     {
       guide: 'releases_widget',
       requiredTargets: ['releases_widget'],
+      dateThreshold: new Date('2022-06-22'),
       steps: [
         {
           title: t('Releases are here!'),
           target: 'releases_widget',
           description: t(
-            'Want to know how you latest release is doing? Monitor release health and crash rates in Dashboards.'
+            'Want to know how your latest release is doing? Monitor release health and crash rates in Dashboards.'
           ),
           nextText: t('Sounds Good'),
         },
