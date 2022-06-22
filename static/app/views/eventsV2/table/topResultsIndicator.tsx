2e0b6c8a64d918539ab452d253bfa5248f796163
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-22 19:13:25 -0400

    spelling: palette
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- static/app/views/eventsV2/table/topResultsIndicator.tsx
+++ static/app/views/eventsV2/table/topResultsIndicator.tsx
@@ -17,7 +17,7 @@ const TopResultsIndicator = styled('div')<TopResultsIndicatorProps>`
     // this background color needs to match the colors used in
     // app/components/charts/eventsChart so that the ordering matches
 
-    // the color pallete contains n + 2 colors, so we subtract 2 here
+    // the color palette contains n + 2 colors, so we subtract 2 here
     return p.theme.charts.getColorPalette(p.count - 2)[p.index];
   }};
 `;
