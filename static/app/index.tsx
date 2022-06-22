c8ff9855e369e086798db68bcee15b67ef095403
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-06-21 21:53:43 -0400

    spelling: client
    Signed-off-by: Josh Soref <jsoref@users.noreply.github.com>

--- static/app/index.tsx
+++ static/app/index.tsx
@@ -30,7 +30,7 @@
 //
 // 3. App initialization does the following...
 //
-//   a. Initialize the ConfigStore with clinet-config data.
+//   a. Initialize the ConfigStore with client-config data.
 //
 //   b. Setup the colorscheme event handlers, for matching the system
 //      colorscheme.
