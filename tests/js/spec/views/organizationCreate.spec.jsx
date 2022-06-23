94ba28dc7b120726d514e77c22c5df9f1f2a5335
Scott Cooper <scttcper@gmail.com>
2022-06-23 16:24:32 -0700

    test(ui): Convert organization create to RTL
    Adds a test that submits the form

--- tests/js/spec/views/organizationCreate.spec.jsx
+++ tests/js/spec/views/organizationCreate.spec.jsx
@@ -1,38 +1,53 @@
-import {mountWithTheme} from 'sentry-test/enzyme';
+import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import ConfigStore from 'sentry/stores/configStore';
 import OrganizationCreate from 'sentry/views/organizationCreate';
 
 describe('OrganizationCreate', function () {
-  let privacyUrl, termsUrl;
-
   beforeEach(() => {
-    termsUrl = ConfigStore.get('termsUrl', null);
-    privacyUrl = ConfigStore.get('privacyUrl', null);
+    ConfigStore.get('termsUrl', null);
+    ConfigStore.get('privacyUrl', null);
   });
 
   afterEach(() => {
-    ConfigStore.set('termsUrl', termsUrl);
-    ConfigStore.set('privacyUrl', privacyUrl);
+    MockApiClient.clearMockResponses();
   });
 
-  describe('render()', function () {
-    it('renders without terms', function () {
-      ConfigStore.set('termsUrl', null);
-      ConfigStore.set('privacyUrl', null);
-      const wrapper = mountWithTheme(<OrganizationCreate />, {
-        context: {router: TestStubs.router()},
-      });
-      expect(wrapper).toSnapshot();
-    });
+  it('renders without terms', function () {
+    const wrapper = render(<OrganizationCreate />);
+    expect(wrapper.container).toSnapshot();
+  });
 
-    it('renders with terms', function () {
-      ConfigStore.set('termsUrl', 'https://example.com/terms');
-      ConfigStore.set('privacyUrl', 'https://example.com/privacy');
-      const wrapper = mountWithTheme(<OrganizationCreate />, {
-        context: {router: TestStubs.router()},
-      });
-      expect(wrapper).toSnapshot();
+  it('renders with terms', function () {
+    ConfigStore.set('termsUrl', 'https://example.com/terms');
+    ConfigStore.set('privacyUrl', 'https://example.com/privacy');
+    const wrapper = render(<OrganizationCreate />);
+    expect(wrapper.container).toSnapshot();
+  });
+
+  it('creates a new org', function () {
+    const orgCreateMock = MockApiClient.addMockResponse({
+      url: '/organizations/',
+      method: 'POST',
     });
+    ConfigStore.set('termsUrl', 'https://example.com/terms');
+    ConfigStore.set('privacyUrl', 'https://example.com/privacy');
+    render(<OrganizationCreate />);
+    expect(screen.getByText('Create a New Organization')).toBeInTheDocument();
+
+    userEvent.paste(screen.getByPlaceholderText('e.g. My Company'), 'Good Burger');
+    userEvent.click(
+      screen.getByRole('checkbox', {
+        name: 'I agree to the Terms of Service and the Privacy Policy',
+      })
+    );
+    userEvent.click(screen.getByText('Create Organization'));
+
+    expect(orgCreateMock).toHaveBeenCalledWith(
+      '/organizations/',
+      expect.objectContaining({
+        data: {agreeTerms: true, defaultTeam: true, name: 'Good Burger'},
+      })
+    );
   });
 });
