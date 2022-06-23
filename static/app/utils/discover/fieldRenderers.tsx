0a270137adaaed2594fc5faed5115e85be8f17cf
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2022-06-23 15:51:29 -0400

    fix(performance): Fix user misery table field renderer (#35958)
    Updates the user misery field renderer to use the function format name instead of aggregate alias

--- static/app/utils/discover/fieldRenderers.tsx
+++ static/app/utils/discover/fieldRenderers.tsx
@@ -485,27 +485,27 @@ const SPECIAL_FUNCTIONS: SpecialFunctions = {
     let countMiserableUserField: string = '';
 
     let miseryLimit: number | undefined = parseInt(
-      userMiseryField.split('_').pop() || '',
+      userMiseryField.split('(').pop()?.slice(0, -1) || '',
       10
     );
     if (isNaN(miseryLimit)) {
-      countMiserableUserField = 'count_miserable_user';
+      countMiserableUserField = 'count_miserable(user)';
       if (projectThresholdConfig in data) {
         miseryLimit = data[projectThresholdConfig][1];
       } else {
         miseryLimit = undefined;
       }
     } else {
-      countMiserableUserField = `count_miserable_user_${miseryLimit}`;
+      countMiserableUserField = `count_miserable(user,${miseryLimit})`;
     }
 
-    const uniqueUsers = data.count_unique_user;
+    const uniqueUsers = data['count_unique(user)'];
 
     let miserableUsers: number | undefined;
 
     if (countMiserableUserField in data) {
       const countMiserableMiseryLimit = parseInt(
-        countMiserableUserField.split('_').pop() || '',
+        userMiseryField.split('(').pop()?.slice(0, -1) || '',
         10
       );
       miserableUsers =
