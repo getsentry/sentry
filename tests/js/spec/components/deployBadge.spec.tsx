b117bff96c73e0ce5f006a314cc95c73207d713a
Scott Cooper <scttcper@gmail.com>
2022-06-22 21:06:56 -0700

    test(ui): Convert tag and deploy badge to RTL

---
+++ tests/js/spec/components/deployBadge.spec.tsx
@@ -0,0 +1,45 @@
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import DeployBadge from 'sentry/components/deployBadge';
+import type {Deploy} from 'sentry/types';
+
+const deploy: Deploy = {
+  name: '85fedddce5a61a58b160fa6b3d6a1a8451e94eb9 to prod',
+  url: '',
+  environment: 'production',
+  dateStarted: '2020-05-11T18:12:00.025928Z',
+  dateFinished: '2020-05-11T18:12:00.025928Z',
+  version: '4.2.0',
+  id: '6348842',
+};
+
+describe('DeployBadge', () => {
+  it('renders', () => {
+    render(<DeployBadge deploy={deploy} />);
+
+    expect(screen.getByText('production')).toBeInTheDocument();
+    expect(screen.queryByRole('link')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('deploy-open-icon')).not.toBeInTheDocument();
+  });
+
+  it('renders with icon and link', () => {
+    const projectId = 1;
+
+    render(
+      <DeployBadge
+        deploy={deploy}
+        orgSlug="sentry"
+        version="1.2.3"
+        projectId={projectId}
+      />,
+      {context: TestStubs.routerContext()}
+    );
+
+    expect(screen.queryByRole('link')).toHaveAttribute(
+      'href',
+      '/organizations/sentry/issues/?environment=production&project=1&query=release%3A1.2.3'
+    );
+    expect(screen.getByText('production')).toBeInTheDocument();
+    expect(screen.getByTestId('deploy-open-icon')).toBeInTheDocument();
+  });
+});
