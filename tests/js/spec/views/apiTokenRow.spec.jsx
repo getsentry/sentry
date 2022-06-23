403497f8eac12709c8d1daa2cb7bb9069d4a359c
Scott Cooper <scttcper@gmail.com>
2022-06-23 10:06:39 -0700

    test(ui): Convert api token row to RTL

--- tests/js/spec/views/apiTokenRow.spec.jsx
+++ tests/js/spec/views/apiTokenRow.spec.jsx
@@ -1,24 +1,22 @@
-import {mountWithTheme} from 'sentry-test/enzyme';
+import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import ApiTokenRow from 'sentry/views/settings/account/apiTokenRow';
 
-describe('ApiTokenRow', function () {
-  it('renders', function () {
-    const wrapper = mountWithTheme(
+describe('ApiTokenRow', () => {
+  it('renders', () => {
+    const wrapper = render(
       <ApiTokenRow onRemove={() => {}} token={TestStubs.ApiToken()} />
     );
 
     // Should be loading
-    expect(wrapper).toSnapshot();
+    expect(wrapper.container).toSnapshot();
   });
 
-  it('calls onRemove callback when trash can is clicked', function () {
+  it('calls onRemove callback when trash can is clicked', () => {
     const cb = jest.fn();
-    const wrapper = mountWithTheme(
-      <ApiTokenRow onRemove={cb} token={TestStubs.ApiToken()} />
-    );
+    render(<ApiTokenRow onRemove={cb} token={TestStubs.ApiToken()} />);
 
-    wrapper.find('Button').simulate('click');
+    userEvent.click(screen.getByRole('button'));
     expect(cb).toHaveBeenCalled();
   });
 });
