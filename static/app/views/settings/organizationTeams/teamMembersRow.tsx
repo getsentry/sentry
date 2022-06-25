2e648f07c4549795c83fe89744199511e0a5e7f3
Danny Lee <dlee@sentry.io>
2022-06-25 00:46:15 -0700

    9 - Add Team Roles to teamMembers page

---
+++ static/app/views/settings/organizationTeams/teamMembersRow.tsx
@@ -0,0 +1,133 @@
+import styled from '@emotion/styled';
+
+import Button from 'sentry/components/button';
+import IdBadge from 'sentry/components/idBadge';
+import {PanelItem} from 'sentry/components/panels';
+import RoleSelectControl from 'sentry/components/roleSelectControl';
+import {IconSubtract} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import space from 'sentry/styles/space';
+import {Member, Organization, User} from 'sentry/types';
+import {
+  hasOrgRoleOverwrite,
+  RoleOverwriteIcon,
+} from 'sentry/views/settings/organizationTeams/roleOverwriteWarning';
+
+type Props = {
+  hasWriteAccess: boolean;
+  member: Member;
+  organization: Organization;
+  removeMember: (member: Member) => void;
+  updateMemberRole: (member: Member, newRole: string) => void;
+  user: User;
+};
+
+function TeamMembersRow(props: Props) {
+  const {organization, member, user, hasWriteAccess} = props;
+  const isSelf = member.email === user.email;
+  const canRemoveMember = hasWriteAccess || isSelf;
+
+  return (
+    <TeamRolesPanelItem key={member.id}>
+      <IdBadge avatarSize={36} member={member} useLink orgId={organization.slug} />
+      <TeamRoleSelect {...props} />
+      {canRemoveMember && <RemoveButton {...props} />}
+    </TeamRolesPanelItem>
+  );
+}
+
+function TeamRoleSelect(props: {
+  hasWriteAccess: boolean;
+  member: Member;
+  organization: Organization;
+  updateMemberRole: (member: Member, newRole: string) => void;
+}) {
+  const {hasWriteAccess, organization, member, updateMemberRole} = props;
+  const {orgRoleList, teamRoleList, features} = organization;
+  if (!features.includes('team-roles')) {
+    return null;
+  }
+
+  const {orgRole: orgRoleId} = member;
+  const orgRole = orgRoleList.find(r => r.id === orgRoleId);
+
+  const teamRoleId: any = (member as any).teamRole || orgRole?.minimumTeamRole; // FIXME
+  const teamRole = teamRoleList.find(r => r.id === teamRoleId) || teamRoleList[0];
+
+  if (
+    !hasWriteAccess ||
+    hasOrgRoleOverwrite({orgRole: orgRoleId, orgRoleList, teamRoleList})
+  ) {
+    return (
+      <RoleName>
+        {teamRole.name}
+        <IconWrapper>
+          <RoleOverwriteIcon
+            orgRole={orgRoleId}
+            orgRoleList={orgRoleList}
+            teamRoleList={teamRoleList}
+          />
+        </IconWrapper>
+      </RoleName>
+    );
+  }
+
+  return (
+    <RoleSelectWrapper>
+      <RoleSelectControl
+        roles={teamRoleList}
+        value={teamRole.id}
+        onChange={option => updateMemberRole(member, option.value)}
+        disableUnallowed
+      />
+    </RoleSelectWrapper>
+  );
+}
+
+function RemoveButton(props: {member: Member; removeMember: (member: Member) => void}) {
+  const {member, removeMember} = props;
+  return (
+    <div>
+      <Button
+        size="xs"
+        icon={<IconSubtract size="xs" isCircled />}
+        onClick={() => removeMember(member)}
+        aria-label={t('Remove')}
+      >
+        {t('Remove')}
+      </Button>
+    </div>
+  );
+}
+
+const RoleName = styled('div')`
+  display: flex;
+  align-items: center;
+`;
+const IconWrapper = styled('div')`
+  height: ${space(2)};
+  margin-left: ${space(1)};
+`;
+
+const RoleSelectWrapper = styled('div')`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+
+  > div:first-child {
+    flex-grow: 1;
+  }
+`;
+
+const TeamRolesPanelItem = styled(PanelItem)`
+  display: grid;
+  grid-template-columns: minmax(120px, 4fr) minmax(120px, 2fr) minmax(100px, 1fr);
+  gap: ${space(2)};
+  align-items: center;
+
+  > div:last-child {
+    margin-left: auto;
+  }
+`;
+
+export default TeamMembersRow;
