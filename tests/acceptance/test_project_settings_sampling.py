a2e96da1b27b0d3d4078b1909541167d2a4423d0
Vu Luong <vuluongj20@gmail.com>
2022-06-27 11:22:17 -0700

    fix(acceptance): Fix test_project_settings_sampling.py
    We need to close the conditions dropdown before interacting with the other elements.

--- tests/acceptance/test_project_settings_sampling.py
+++ tests/acceptance/test_project_settings_sampling.py
@@ -249,6 +249,8 @@ class ProjectSettingsSamplingTest(AcceptanceTestCase):
             self.browser.element('[data-test-id="event.web_crawlers"]').click()
             # Add Custom Tag
             self.browser.element('[aria-autocomplete="list"]').send_keys("Add cus", Keys.ENTER)
+            # Close conditions dropdown
+            self.browser.send_keys(Keys.ESCAPE)
 
             # Fill in Content Security Policy
             self.browser.element('[placeholder="ex. file://*, example.com (Multiline)"]').send_keys(
