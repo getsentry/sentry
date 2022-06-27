ee73b65e11022888542a815ca00913de6752d779
William Mak <william@wmak.io>
2022-06-27 11:11:12 -0400

    fix(custom-measurements): Update custom measurement logic
    - This updates the lgoic that determines if a mri represents a custom
      measurements to instead of checking for a custom namespace, that its a
      transaction MRI, but one that isn't in the hardcoded list in
      TransactionMRI

--- tests/sentry/snuba/metrics/test_naming_layer.py
+++ tests/sentry/snuba/metrics/test_naming_layer.py
@@ -2,7 +2,12 @@ import re
 
 import pytest
 
-from sentry.snuba.metrics.naming_layer.mri import MRI_SCHEMA_REGEX, ParsedMRI, parse_mri
+from sentry.snuba.metrics.naming_layer.mri import (
+    MRI_SCHEMA_REGEX,
+    ParsedMRI,
+    is_custom_measurement,
+    parse_mri,
+)
 from sentry.snuba.metrics.naming_layer.public import PUBLIC_NAME_REGEX
 
 
@@ -108,3 +113,28 @@ def test_invalid_mri_schema_regex(name):
 )
 def test_parse_mri(name, expected):
     assert parse_mri(name) == expected
+
+
+@pytest.mark.parametrize(
+    "name, expected",
+    [
+        (
+            ParsedMRI("d", "transactions", "measurements.stall_longest_time", "millisecond"),
+            False,
+        ),
+        (
+            ParsedMRI("d", "transactions", "breakdowns.span_ops.http", "millisecond"),
+            False,
+        ),
+        (
+            ParsedMRI("c", "transactions", "measurements.db_calls", "none"),
+            True,
+        ),
+        (
+            ParsedMRI("s", "sessions", "error", "none"),
+            False,
+        ),
+    ],
+)
+def test_is_custom_measurement(parsed_mri, expected):
+    assert is_custom_measurement(parsed_mri) == expected
