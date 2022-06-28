efde3bbbfd0169cc349970a264ede419b7204c1a
jesus4497 <jesuspadron4497@gmail.com>
2022-06-28 18:57:10 -0400

    fix: force possible array to string

--- static/app/views/replays/detail/console/consoleMessage.tsx
+++ static/app/views/replays/detail/console/consoleMessage.tsx
@@ -43,7 +43,9 @@ export function MessageFormatter({breadcrumb}: MessageFormatterProps) {
   if (!breadcrumb.data?.arguments) {
     // There is a possibility that we don't have arguments as we could be receiving an exception type breadcrumb.
     // In these cases we just need the message prop.
-    logMessage = renderString(breadcrumb.message || '') as string;
+
+    // There are cases in which our prop message is an array, we want to force it to become a string
+    logMessage = breadcrumb.message?.toString() || '';
     return <AnnotatedText meta={getMeta(breadcrumb, 'message')} value={logMessage} />;
   }
 
