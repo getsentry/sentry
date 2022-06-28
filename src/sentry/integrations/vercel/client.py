359ae1387241e37e4bb0605fe472d376d2f9b923
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-06-28 16:23:06 -0400

    ♻️ Update Vercel endpoints and change pagination

--- src/sentry/integrations/vercel/client.py
+++ src/sentry/integrations/vercel/client.py
@@ -11,13 +11,30 @@ class VercelClient(ApiClient):
     base_url = "https://api.vercel.com"
     integration_name = "vercel"
 
-    TEAMS_URL = "/v1/teams/%s"
-    USER_URL = "/www/user"
-    PROJECT_URL = "/v1/projects/%s"
-    PROJECTS_URL = "/v4/projects/"
-    ENV_VAR_URL = "/v7/projects/%s/env"
-    GET_ENV_VAR_URL = "/v7/projects/%s/env"
-    UPDATE_ENV_VAR_URL = "/v7/projects/%s/env/%s"
+    # Current User API (Read)
+    # https://vercel.com/docs/integrations/reference#using-the-vercel-api/scopes/user
+    GET_USER_URL = "/v2/user"
+
+    # Teams API Scope (Read)
+    # https://vercel.com/docs/rest-api#endpoints/teams/get-a-team
+    GET_TEAM_URL = "/v2/teams/%s"
+
+    # Projects API Scope (Read)
+    # https://vercel.com/docs/rest-api#endpoints/projects/find-a-project-by-id-or-name
+    GET_PROJECT_URL = "/v9/projects/%s"
+    # https://vercel.com/docs/rest-api#endpoints/projects/retrieve-a-list-of-projects
+    GET_PROJECTS_URL = "/v9/projects/"
+
+    # Project Environment Variables API Scope (Read/Write)
+    # https://vercel.com/docs/rest-api#endpoints/projects/retrieve-the-environment-variables-of-a-project-by-id-or-name
+    GET_ENV_VAR_URL = "/v9/projects/%s/env"
+    # https://vercel.com/docs/rest-api#endpoints/projects/create-one-or-more-environment-variables
+    CREATE_ENV_VAR_URL = "/v9/projects/%s/env"
+    # https://vercel.com/docs/rest-api#endpoints/projects/edit-an-environment-variable
+    UPDATE_ENV_VAR_URL = "/v9/projects/%s/env/%s"
+
+    # Integration Configuration API Scope (Read/Write)
+    # https://vercel.com/docs/rest-api#endpoints/integrations/delete-an-integration-configuration
     UNINSTALL = "/v1/integrations/configuration/%s"
 
     def __init__(self, access_token, team_id=None):
@@ -41,40 +58,35 @@ class VercelClient(ApiClient):
 
     def get_team(self):
         assert self.team_id
-        return self.get(self.TEAMS_URL % self.team_id)
+        return self.get(self.GET_TEAM_URL % self.team_id)
 
     def get_user(self):
-        return self.get(self.USER_URL)["user"]
+        return self.get(self.GET_USER_URL)["user"]
 
-    def paginate(self, url, type):
-        limit = 20
+    def get_from_pagination(self, url, data_key):
+        # Vercel Pagination Guide: https://vercel.com/docs/rest-api#introduction/api-basics/pagination
+        limit = 100
         params = {"limit": limit}
         results = []
-        # no one should have more than 200 results
-        for i in range(10):
-            resp = self.get(url, params=params)
-            results += resp[type]
-            # if we have less results than the limit, we are done
-            if resp["pagination"]["count"] < limit:
-                return results
-            # continue pagination by setting the until parameter
-            params = params.copy()
-            params["until"] = resp["pagination"]["next"]
-        # log the warning if this happens so we can look into solutions
-        logger.warning("Did not finish pagination", extra={"team_id": self.team_id, "url": url})
+        next_timestamp = ""
+        while next_timestamp is not None:
+            response = self.get(url, params=params)
+            results += response[data_key]
+            next_timestamp = response["pagination"]["next"]
+            params["until"] = next_timestamp
         return results
 
     def get_projects(self):
-        return self.paginate(self.PROJECTS_URL, "projects")
+        return self.get_from_pagination(self.GET_PROJECTS_URL, "projects")
 
     def get_project(self, vercel_project_id):
-        return self.get(self.PROJECT_URL % vercel_project_id)
+        return self.get(self.GET_PROJECT_URL % vercel_project_id)
 
     def get_env_vars(self, vercel_project_id):
         return self.get(self.GET_ENV_VAR_URL % vercel_project_id)
 
     def create_env_variable(self, vercel_project_id, data):
-        return self.post(self.ENV_VAR_URL % vercel_project_id, data=data)
+        return self.post(self.CREATE_ENV_VAR_URL % vercel_project_id, data=data)
 
     def update_env_variable(self, vercel_project_id, env_var_id, data):
         return self.patch(self.UPDATE_ENV_VAR_URL % (vercel_project_id, env_var_id), data=data)
