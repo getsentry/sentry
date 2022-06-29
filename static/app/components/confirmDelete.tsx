fc8f603d86b96dfa9cc6a327aa59773393ea465b
Scott Cooper <scttcper@gmail.com>
2022-06-29 13:45:14 -0700

    test(ui): Convert confirm components to RTL (#36197)

--- static/app/components/confirmDelete.tsx
+++ static/app/components/confirmDelete.tsx
@@ -6,12 +6,13 @@ import Input from 'sentry/components/forms/controls/input';
 import Field from 'sentry/components/forms/field';
 import {t} from 'sentry/locale';
 
-type Props = Omit<React.ComponentProps<typeof Confirm>, 'renderConfirmMessage'> & {
+interface Props
+  extends Omit<React.ComponentProps<typeof Confirm>, 'renderConfirmMessage'> {
   /**
    * The string that the user must enter to confirm the deletion
    */
   confirmInput: string;
-};
+}
 
 const ConfirmDelete = ({message, confirmInput, ...props}: Props) => (
   <Confirm
