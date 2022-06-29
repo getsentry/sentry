22618122a718820cf8677858ebe79c452894a9cb
Jonas <jonas@badalic.com>
2022-06-29 13:43:21 -0400

    feat(profiling): show onboarding page based on prompt check (#36190)
    * feat(prompts): add profiling prompt
    
    * feat(profiling): add show logic
    
    * fix(event): pass the corect action
    
    * fix(analtics): use single event

--- static/app/utils/analytics/profilingAnalyticsEvents.tsx
+++ static/app/utils/analytics/profilingAnalyticsEvents.tsx
@@ -2,6 +2,9 @@ export type ProfilingEventParameters = {
   'profiling_views.go_to_flamegraph': {source: string};
   'profiling_views.landing': {};
   'profiling_views.onboarding': {};
+  'profiling_views.onboarding_action': {
+    action: 'done' | 'dismissed';
+  };
   'profiling_views.profile_details': {};
   'profiling_views.profile_flamegraph': {};
   'profiling_views.profile_summary': {};
@@ -16,4 +19,5 @@ export const profilingEventMap: Record<EventKey, string> = {
   'profiling_views.profile_summary': 'Profiling Views: Profile Summary',
   'profiling_views.profile_details': 'Profiling Views: Profile Details',
   'profiling_views.go_to_flamegraph': 'Profiling Views: Go to Flamegraph',
+  'profiling_views.onboarding_action': 'Profiling Actions: Onboarding Action',
 };
