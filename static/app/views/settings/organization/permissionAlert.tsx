7b25e83c5e09cdc46e02c1673c417eca449ca719
Matej Minar <matej.minar@sentry.io>
2022-06-30 17:45:22 +0200

    feat(sampling): Onboarding modal step groundwork [TET-164] (#36251)

--- static/app/views/settings/organization/permissionAlert.tsx
+++ static/app/views/settings/organization/permissionAlert.tsx
@@ -1,18 +1,26 @@
+import {ReactNode} from 'react';
+
 import Access from 'sentry/components/acl/access';
 import Alert from 'sentry/components/alert';
 import {t} from 'sentry/locale';
 
 type Props = React.ComponentPropsWithoutRef<typeof Alert> &
-  Pick<React.ComponentProps<typeof Access>, 'access'>;
+  Pick<React.ComponentProps<typeof Access>, 'access'> & {
+    message?: ReactNode;
+  };
 
-const PermissionAlert = ({access = ['org:write'], ...props}: Props) => (
+const PermissionAlert = ({
+  access = ['org:write'],
+  message = t(
+    'These settings can only be edited by users with the organization owner or manager role.'
+  ),
+  ...props
+}: Props) => (
   <Access access={access}>
     {({hasAccess}) =>
       !hasAccess && (
         <Alert type="warning" showIcon {...props}>
-          {t(
-            'These settings can only be edited by users with the organization owner or manager role.'
-          )}
+          {message}
         </Alert>
       )
     }
