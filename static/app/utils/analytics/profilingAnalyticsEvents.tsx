8c74dae9e98609903b33c0ed44a13d89ab877146
Jonas <jonas@badalic.com>
2022-07-05 12:50:44 -0400

    feat(onboarding): add alpha onboarding page (#36152)
    * feat(onboarding): add alpha onboarding page
    
    * feat(onboarding): add actions
    
    * fix(comments): use external link and t
    
    * feat(onboarding): add alpha onboarding page
    
    * feat(onboarding): add actions
    
    * fix(comments): use external link and t
    
    * feat(profiling): track analytics event (#36186)
    
    * feat(profiling): track analytics event
    
    * fix(review): reset file
    
    * feat(profiling): show onboarding page based on prompt check (#36190)
    
    * feat(prompts): add profiling prompt
    
    * feat(profiling): add show logic
    
    * fix(event): pass the corect action
    
    * fix(analtics): use single event
    
    * fix(onboarding): use list component
    
    * fix(profiling): use openInNewTab

--- static/app/utils/analytics/profilingAnalyticsEvents.tsx
+++ static/app/utils/analytics/profilingAnalyticsEvents.tsx
@@ -1,6 +1,10 @@
 export type ProfilingEventParameters = {
   'profiling_views.go_to_flamegraph': {source: string};
   'profiling_views.landing': {};
+  'profiling_views.onboarding': {};
+  'profiling_views.onboarding_action': {
+    action: 'done' | 'dismissed';
+  };
   'profiling_views.profile_details': {};
   'profiling_views.profile_flamegraph': {};
   'profiling_views.profile_summary': {};
@@ -10,8 +14,10 @@ type EventKey = keyof ProfilingEventParameters;
 
 export const profilingEventMap: Record<EventKey, string> = {
   'profiling_views.landing': 'Profiling Views: Landing',
+  'profiling_views.onboarding': 'Profiling Views: Onboarding',
   'profiling_views.profile_flamegraph': 'Profiling Views: Flamegraph',
   'profiling_views.profile_summary': 'Profiling Views: Profile Summary',
   'profiling_views.profile_details': 'Profiling Views: Profile Details',
   'profiling_views.go_to_flamegraph': 'Profiling Views: Go to Flamegraph',
+  'profiling_views.onboarding_action': 'Profiling Actions: Onboarding Action',
 };
--- static/app/views/profiling/index.tsx
+++ static/app/views/profiling/index.tsx
@@ -1,9 +1,34 @@
+import {useCallback, useEffect, useState} from 'react';
+import * as Sentry from '@sentry/react';
+
+import {PromptData, promptsCheck, promptsUpdate} from 'sentry/actionCreators/prompts';
 import Feature from 'sentry/components/acl/feature';
 import Alert from 'sentry/components/alert';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
 import {t} from 'sentry/locale';
 import {PageContent} from 'sentry/styles/organization';
-import {Organization} from 'sentry/types';
+import {Organization, RequestState} from 'sentry/types';
+import trackAdvancedAnalyticsEvent from 'sentry/utils/analytics/trackAdvancedAnalyticsEvent';
+import useApi from 'sentry/utils/useApi';
 import withOrganization from 'sentry/utils/withOrganization';
+
+import ProfilingOnboarding from './profilingOnboarding';
+
+function renderNoAccess() {
+  return (
+    <PageContent>
+      <Alert type="warning">{t("You don't have access to this feature")}</Alert>
+    </PageContent>
+  );
+}
+
+function shouldShowProfilingOnboarding(state: RequestState<PromptData>): boolean {
+  if (state.type === 'resolved') {
+    return typeof state.data?.dismissedTime !== 'number';
+  }
+
+  return false;
+}
 
 type Props = {
   children: React.ReactChildren;
@@ -11,13 +36,56 @@ type Props = {
 };
 
 function ProfilingContainer({organization, children}: Props) {
-  function renderNoAccess() {
-    return (
-      <PageContent>
-        <Alert type="warning">{t("You don't have access to this feature")}</Alert>
-      </PageContent>
-    );
-  }
+  const api = useApi();
+
+  const [requestState, setRequestState] = useState<RequestState<PromptData>>({
+    type: 'initial',
+  });
+
+  // Fetch prompt data and see if we need to show the onboarding.
+  useEffect(() => {
+    setRequestState({type: 'loading'});
+    promptsCheck(api, {
+      organizationId: organization.id,
+      feature: 'profiling_onboarding',
+    })
+      .then(data => {
+        setRequestState({type: 'resolved', data});
+      })
+      .catch(e => {
+        Sentry.captureException(e);
+        setRequestState({type: 'errored', error: t('Error: Unable to load prompt data')});
+      });
+  }, [api, organization]);
+
+  // Eagerly update state and update check
+  const dismissPrompt = useCallback(
+    (status: 'done' | 'dismissed') => {
+      setRequestState({type: 'resolved', data: {dismissedTime: Date.now()}});
+      trackAdvancedAnalyticsEvent('profiling_views.onboarding_action', {
+        action: status,
+        organization,
+      });
+
+      return promptsUpdate(api, {
+        feature: 'profiling_onboarding',
+        organizationId: organization.id,
+        // This will always send dismissed, becuse we dont actually
+        // care about the snooze mechanism. It would be awkward to suddenly
+        // creep a full page into view.
+        status: 'dismissed',
+      });
+    },
+    [organization, api]
+  );
+
+  const handleDone = useCallback(() => {
+    dismissPrompt('done');
+  }, [dismissPrompt]);
+
+  const handleDismiss = useCallback(() => {
+    dismissPrompt('dismissed');
+  }, [dismissPrompt]);
 
   return (
     <Feature
@@ -26,7 +94,17 @@ function ProfilingContainer({organization, children}: Props) {
       organization={organization}
       renderDisabled={renderNoAccess}
     >
-      {children}
+      {requestState.type === 'loading' ? (
+        <LoadingIndicator />
+      ) : shouldShowProfilingOnboarding(requestState) ? (
+        <ProfilingOnboarding
+          organization={organization}
+          onDismissClick={handleDismiss}
+          onDoneClick={handleDone}
+        />
+      ) : (
+        children
+      )}
     </Feature>
   );
 }
