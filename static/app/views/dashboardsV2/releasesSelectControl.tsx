0caa78834e5a65c3cdf989308f34e9a8617d4140
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-07-06 18:31:14 +0530

    label

--- static/app/views/dashboardsV2/releasesSelectControl.tsx
+++ static/app/views/dashboardsV2/releasesSelectControl.tsx
@@ -1,15 +1,20 @@
-import {Fragment} from 'react';
+import {Fragment, useState} from 'react';
+import styled from '@emotion/styled';
 
+import Badge from 'sentry/components/badge';
 import CompactSelect from 'sentry/components/forms/compactSelect';
 import {IconReleases} from 'sentry/icons';
 import {t} from 'sentry/locale';
+import {Release} from 'sentry/types';
 import {useReleases} from 'sentry/utils/releases/releasesProvider';
 
 function ReleasesSelectControl() {
   const triggerIcon = <IconReleases key={0} />;
-  const triggerLabel = t('All Releases');
 
   const {releases, loading} = useReleases();
+  const [selectedReleases, setSelectedReleases] = useState<Release[]>([]);
+
+  const triggerLabel = selectedReleases.length ? selectedReleases[0] : t('All Releases');
 
   return (
     <CompactSelect
@@ -21,15 +26,30 @@ function ReleasesSelectControl() {
       options={
         releases.length
           ? releases.map(release => {
-              return {label: release.shortVersion, value: release.version};
+              return {
+                label: release.shortVersion ?? release.version,
+                value: release.version,
+              };
             })
           : []
       }
-      value={undefined}
-      triggerLabel={<Fragment>{triggerLabel}</Fragment>}
+      onChange={opts => setSelectedReleases(opts.map(opt => opt.value))}
+      value={selectedReleases}
+      triggerLabel={
+        <Fragment>
+          {triggerLabel}
+          {selectedReleases.length > 1 && (
+            <StyledBadge text={`+${selectedReleases.length - 1}`} />
+          )}
+        </Fragment>
+      }
       triggerProps={{icon: triggerIcon}}
     />
   );
 }
 
 export default ReleasesSelectControl;
+
+const StyledBadge = styled(Badge)`
+  flex-shrink: 0;
+`;
