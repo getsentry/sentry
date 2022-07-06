a74e14fca258cdd7606f227b4a663994c9e0bb18
Shruthi <shruthilaya.jaganathan@sentry.io>
2022-07-06 11:56:08 +0530

    ref(dashboards): Move y-axis into config (#36191)
    Move y-axis selection into config

--- static/app/views/dashboardsV2/widgetBuilder/buildSteps/yAxisStep/index.tsx
+++ static/app/views/dashboardsV2/widgetBuilder/buildSteps/yAxisStep/index.tsx
@@ -1,13 +1,11 @@
 import {t} from 'sentry/locale';
 import {Organization, TagCollection} from 'sentry/types';
 import {QueryFieldValue} from 'sentry/utils/discover/fields';
-import Measurements from 'sentry/utils/measurements/measurements';
 import {DisplayType, WidgetType} from 'sentry/views/dashboardsV2/types';
 
-import {DataSet, getAmendedFieldOptions} from '../../utils';
+import {DataSet} from '../../utils';
 import {BuildStep} from '../buildStep';
 
-import {ReleaseYAxisSelector} from './releaseYAxisSelector';
 import {YAxisSelector} from './yAxisSelector';
 
 interface Props {
@@ -23,11 +21,9 @@ interface Props {
 
 export function YAxisStep({
   displayType,
-  dataSet,
   queryErrors,
   aggregates,
   onYAxisChange,
-  organization,
   tags,
   widgetType,
 }: Props) {
@@ -46,28 +42,14 @@ export function YAxisStep({
           : t("This is the data you'd be visualizing in the display.")
       }
     >
-      {dataSet === DataSet.RELEASES ? (
-        <ReleaseYAxisSelector
-          widgetType={widgetType}
-          displayType={displayType}
-          aggregates={aggregates}
-          onChange={onYAxisChange}
-          errors={queryErrors}
-        />
-      ) : (
-        <Measurements>
-          {({measurements}) => (
-            <YAxisSelector
-              widgetType={widgetType}
-              displayType={displayType}
-              aggregates={aggregates}
-              fieldOptions={getAmendedFieldOptions({measurements, organization, tags})}
-              onChange={onYAxisChange}
-              errors={queryErrors}
-            />
-          )}
-        </Measurements>
-      )}
+      <YAxisSelector
+        widgetType={widgetType}
+        displayType={displayType}
+        aggregates={aggregates}
+        onChange={onYAxisChange}
+        tags={tags}
+        errors={queryErrors}
+      />
     </BuildStep>
   );
 }
