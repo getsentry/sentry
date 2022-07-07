111725833f617658f883a4ae371707df07e86229
Nar Saynorath <nar.saynorath@sentry.io>
2022-07-07 13:57:00 -0400

    Filters won't show unless there are values

--- src/sentry/api/serializers/models/dashboard.py
+++ src/sentry/api/serializers/models/dashboard.py
@@ -152,7 +152,6 @@ class DashboardDetailsSerializer(Serializer):
             "createdBy": serialize(obj.created_by, serializer=UserSerializer()),
             "widgets": attrs["widgets"],
             "projects": [project.id for project in obj.projects.all()],
-            "filters": {},
         }
 
         for key in page_filter_keys:
@@ -161,6 +160,8 @@ class DashboardDetailsSerializer(Serializer):
 
         for key in dashboard_filter_keys:
             if obj.filters is not None and obj.filters.get(key) is not None:
+                if data.get("filters") is None:
+                    data["filters"] = {}
                 data["filters"][key] = obj.filters[key]
 
         return data
