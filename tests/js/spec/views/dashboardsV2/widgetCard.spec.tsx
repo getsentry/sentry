219df908422e28f93a75f60a4976789f5a3fbc71
Edward Gou <edward.gou@sentry.io>
2022-07-07 18:20:41 -0400

    fixed test

--- tests/js/spec/views/dashboardsV2/widgetCard.spec.tsx
+++ tests/js/spec/views/dashboardsV2/widgetCard.spec.tsx
@@ -497,7 +497,7 @@ describe('Dashboards > WidgetCard', function () {
     );
   });
 
-  it('has sticky table headers', async function () {
+  it.only('has sticky table headers', async function () {
     const tableWidget: Widget = {
       title: 'Table Widget',
       interval: '5m',
@@ -532,7 +532,8 @@ describe('Dashboards > WidgetCard', function () {
         tableItemLimit={20}
       />
     );
-    await tick();
+    await waitFor(() => expect(eventsv2Mock).toHaveBeenCalled());
+
     expect(SimpleTableChart).toHaveBeenCalledWith(
       expect.objectContaining({stickyHeaders: true}),
       expect.anything()
