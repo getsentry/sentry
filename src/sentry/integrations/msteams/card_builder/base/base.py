357ff0b0fefac5512e4e521ff57dd425fa301eae
Varun Venkatesh <varun.venkatesh@sentry.io>
2022-07-07 21:42:56 -0700

    Refactor message builder

--- src/sentry/integrations/msteams/card_builder/base/base.py
+++ src/sentry/integrations/msteams/card_builder/base/base.py
@@ -7,23 +7,40 @@ from sentry.utils.assets import get_asset_url
 from sentry.utils.http import absolute_uri
 
 URL_FORMAT_STR = "[{text}]({url})"
+SENTRY_ICON_URL = "images/sentry-glyph-black.png"
 
 
-class TextSize(Enum):
+class TextSize(str, Enum):
     SMALL = "Small"
     MEDIUM = "Medium"
     LARGE = "Large"
 
 
-class TextWeight(Enum):
+class TextWeight(str, Enum):
     BOLDER = "Bolder"
     LIGHTER = "Lighter"
 
 
-class ActionType(Enum):
+class ActionType(str, Enum):
     OPEN_URL = "Action.OpenUrl"
     SUBMIT = "Action.Submit"
     SHOW_CARD = "Action.ShowCard"
+
+
+class ColumnWidth(str, Enum):
+    STRECH = "strech"
+    AUTO = "auto"
+
+
+class VerticalContentAlignment(str, Enum):
+    CENTER = "Center"
+
+
+REQUIRED_ACTION_PARAM = {
+    ActionType.OPEN_URL: "url",
+    ActionType.SUBMIT: "data",
+    ActionType.SHOW_CARD: "card",
+}
 
 
 class MSTeamsMessageBuilder(AbstractMessageBuilder, ABC):
@@ -32,19 +49,11 @@ class MSTeamsMessageBuilder(AbstractMessageBuilder, ABC):
         raise NotImplementedError
 
     @staticmethod
-    def get_text_block(
-        text: str, size: Optional[TextSize] = None, weight: Optional[TextWeight] = None
-    ) -> Any:
-        return {
-            "type": "TextBlock",
-            "text": text,
-            "wrap": True,
-            "size": size.value if size else None,
-            "weight": weight.value if weight else None,
-        }
+    def get_text_block(text: str, **kwargs) -> Any:
+        return {"type": "TextBlock", "text": text, "wrap": True, **kwargs}
 
     def get_logo_block(self) -> Any:
-        return self.get_image_block(get_asset_url("sentry", "images/sentry-glyph-black.png"))
+        return self.get_image_block(get_asset_url("sentry", SENTRY_ICON_URL))
 
     @staticmethod
     def get_image_block(url: str) -> Any:
@@ -55,29 +64,23 @@ class MSTeamsMessageBuilder(AbstractMessageBuilder, ABC):
         }
 
     @staticmethod
-    def get_column_block(*columns: Any) -> Any:
+    def get_column_block(item: Any, **kwargs) -> Any:
+        kwargs["width"] = kwargs.get("width", ColumnWidth.AUTO)
+
+        return {"type": "Column", "items": [item], **kwargs}
+
+    @staticmethod
+    def get_column_set_block(*columns: Any) -> Any:
         return {
             "type": "ColumnSet",
-            "columns": [
-                {"type": "Column", "items": [column], "width": "auto"} for column in columns
-            ],
+            "columns": list(columns),
         }
 
     @staticmethod
-    def get_action(action_type: ActionType, title: str, **kwargs):
-        action = {
-            "type": action_type.value,
-            "title": title,
-        }
+    def get_action_block(action_type: ActionType, title: str, **kwargs):
+        param = REQUIRED_ACTION_PARAM[action_type]
 
-        if action_type == ActionType.OPEN_URL:
-            action["url"] = kwargs.get("url")
-        elif action_type == ActionType.SUBMIT:
-            action["data"] = kwargs.get("data")
-        elif action_type == ActionType.SHOW_CARD:
-            action["card"] = kwargs.get("card")
-
-        return action
+        return {"type": action_type.value, "title": title, f"{param}": kwargs.get(param)}
 
     def _build(
         self,
