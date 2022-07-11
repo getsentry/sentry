1d06e69d17aa53c1eeee97a4d03b3fecfc499ad9
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-07-11 13:05:17 -0400

    Clean up tct usage

--- static/app/components/events/interfaces/spans/traceErrorList.tsx
+++ static/app/components/events/interfaces/spans/traceErrorList.tsx
@@ -1,11 +1,11 @@
-import {Fragment, memo} from 'react';
+import {memo} from 'react';
 import styled from '@emotion/styled';
 import flatten from 'lodash/flatten';
 import groupBy from 'lodash/groupBy';
 
 import List from 'sentry/components/list';
 import ListItem from 'sentry/components/list/listItem';
-import {t, tn} from 'sentry/locale';
+import {tct, tn} from 'sentry/locale';
 import {TraceError} from 'sentry/utils/performance/quickTrace/types';
 
 import {ParsedTraceType, SpanType} from './types';
@@ -21,21 +21,22 @@ function TraceErrorList({trace, errors, onClickSpan}: TraceErrorListProps) {
     <List symbol="bullet" data-test-id="trace-error-list">
       {flatten(
         Object.entries(groupBy(errors, 'span')).map(([spanId, spanErrors]) => {
-          const span = findSpanById(trace, spanId);
-
           return Object.entries(groupBy(spanErrors, 'level')).map(
             ([level, spanLevelErrors]) => (
               <ListItem key={`${spanId}-${level}`}>
-                {t('%d', spanLevelErrors.length)} {` ${level === 'error' ? '' : level} `}
-                {tn('error', 'errors', spanLevelErrors.length)}
-                {span ? (
-                  <Fragment>
-                    {t(' in ')}
+                {tct('[errors] [link]', {
+                  errors: tn(
+                    '%s %s error in ',
+                    '%s %s errors in ',
+                    spanLevelErrors.length,
+                    level === 'error' ? '' : level // Prevents awkward "error errors" copy if the level is "error"
+                  ),
+                  link: (
                     <ErrorLink onClick={event => onClickSpan(event, spanId)}>
-                      {span.op}
+                      {findSpanById(trace, spanId).op}
                     </ErrorLink>
-                  </Fragment>
-                ) : null}
+                  ),
+                })}
               </ListItem>
             )
           );
