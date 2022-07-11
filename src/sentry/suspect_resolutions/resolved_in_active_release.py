c3e48e41e644ecd0be14b7b1d947adef78540daa
Komal Saini <komalsaini@live.com>
2022-07-11 16:10:53 -0700

    check if resolved issue in recent release

---
+++ src/sentry/suspect_resolutions/resolved_in_active_release.py
@@ -0,0 +1,39 @@
+from datetime import timedelta
+
+from django.utils import timezone
+
+from sentry.models import Deploy, Group, GroupRelease, GroupStatus, Release
+
+
+def is_resolved_issue_within_active_release(issue_id: int) -> bool:
+    # check that the issue is resolved and is part of a release
+    if (not Group.objects.filter(id=issue_id, status=GroupStatus.RESOLVED).exists()) or len(
+        GroupRelease.objects.filter(id=issue_id).values_list("release_id", flat=True)
+    ) == 0:
+        return False
+    else:
+        # check that the issue was resolved in active release
+
+        # returns the release version associated with the issue
+        latest_release_version_issue = Group.objects.filter(id=issue_id).first().get_last_release()
+
+        # get release object from version
+        latest_release_issue = Release.objects.filter(version=latest_release_version_issue)
+
+        # get latest deploy of the release associated with the issue
+        latest_deploy_release: Deploy = (
+            Deploy.objects.filter(release_id=latest_release_issue.first().id)
+            .order_by("-date_finished")
+            .first()
+            or Deploy.objects.filter(id=latest_release_issue.first().last_deploy_id).first()
+        )
+
+        # get the time of the latest deploy
+        if latest_deploy_release:
+            latest_deploy_release_time = latest_deploy_release.date_finished
+        else:
+            return False
+
+        now_minus_1_hour = timezone.now() - timedelta(hours=1.0)
+
+        return bool(now_minus_1_hour <= latest_deploy_release_time <= timezone.now())
