c4cc0467974bcfb2b3c95120bd19c337aa977183
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-07-11 17:07:46 +0200

    feat(metrics_indexer): Add rate limits functionality to indexer [INGEST-1380] (#36263)
    * feat(metrics_indexer): Add rate limits functionality to indexer [INGEST-1380]
    
    The postgres string indexer now is able to rate limit writes using four
    sentry options. If that happens, `None` is returned in place of an
    integer, and the FetchType is RATE_LIMITED.
    
    The kafka consumer/message processor explicitly checks for those `None`
    values and throws away every message that references a rate-limited
    string. It logs a Sentry error for every dropped message just because
    that's already what we do for other kinds of dropped messages.
    
    Rate limiting and quota management currently creates a ton of
    dataclasses and that probably wastes time. There are a ton of
    low-hanging fruits:
    
    * the return value of _construct_quotas could be globally cached, as
      long as the cache is wiped when the sentry options change.
    
    * the same Quota object (for global limits) is referenced from multiple
      RequestedQuota instances (one for each org).
      `sentry.ratelimits.sliding_windows` could check the `id()` of the
      quota (if there is no prefix override) to avoid computing and checking
      the same quota multiple times.
    
    An even lower hanging fruit is that we're fetching the same keys from
    Redis multiple times, because multiple organizations (and therefore
    multiple RequestedQuota instances) adhere to the global quota. So that's
    been fixed, but as for the rest let's wait for timings from prod.
    
    * fix typo
    
    * fix typing
    
    * apply review feedback
    
    * fix typing, add test
    
    * fix tests
    
    * apply review feedback about logging too many msgs
    
    * fix leaking option in test
    
    * sike, more test failures

---
+++ src/sentry/sentry_metrics/indexer/ratelimiters.py
@@ -0,0 +1,151 @@
+from typing import MutableMapping, Optional, Sequence, Tuple
+
+from sentry import options
+from sentry.ratelimits.sliding_windows import (
+    GrantedQuota,
+    Quota,
+    RedisSlidingWindowRateLimiter,
+    RequestedQuota,
+    Timestamp,
+)
+from sentry.sentry_metrics.configuration import UseCaseKey, get_ingest_config
+from sentry.sentry_metrics.indexer.base import FetchType, FetchTypeExt, KeyCollection, KeyResult
+from sentry.utils import metrics
+
+OrgId = int
+
+
+def _build_quota_key(use_case_id: UseCaseKey, org_id: Optional[OrgId] = None) -> str:
+    use_case_str = {
+        UseCaseKey.PERFORMANCE: "performance",
+        UseCaseKey.RELEASE_HEALTH: "releasehealth",
+    }[use_case_id]
+
+    if org_id is not None:
+        return f"metrics-indexer-{use_case_str}-org-{org_id}"
+    else:
+        return f"metrics-indexer-{use_case_str}-global"
+
+
+@metrics.wraps("sentry_metrics.indexer.construct_quotas")
+def _construct_quotas(use_case_id: UseCaseKey) -> Sequence[Quota]:
+    """
+    Construct write limit's quotas based on current sentry options.
+
+    This value can potentially cached globally as long as it is invalidated
+    when sentry.options are.
+    """
+    if use_case_id == UseCaseKey.PERFORMANCE:
+        return [
+            Quota(prefix_override=_build_quota_key(UseCaseKey.PERFORMANCE, None), **args)
+            for args in options.get("sentry-metrics.writes-limiter.limits.performance.global")
+        ] + [
+            Quota(prefix_override=None, **args)
+            for args in options.get("sentry-metrics.writes-limiter.limits.performance.per-org")
+        ]
+    elif use_case_id == UseCaseKey.RELEASE_HEALTH:
+        return [
+            Quota(prefix_override=_build_quota_key(UseCaseKey.RELEASE_HEALTH, None), **args)
+            for args in options.get("sentry-metrics.writes-limiter.limits.releasehealth.global")
+        ] + [
+            Quota(prefix_override=None, **args)
+            for args in options.get("sentry-metrics.writes-limiter.limits.releasehealth.per-org")
+        ]
+    else:
+        raise ValueError(use_case_id)
+
+
+@metrics.wraps("sentry_metrics.indexer.construct_quota_requests")
+def _construct_quota_requests(
+    use_case_id: UseCaseKey, keys: KeyCollection
+) -> Tuple[Sequence[OrgId], Sequence[RequestedQuota]]:
+    org_ids = []
+    requests = []
+    quotas = _construct_quotas(use_case_id)
+
+    if quotas:
+        for org_id, strings in keys.mapping.items():
+            org_ids.append(org_id)
+            requests.append(
+                RequestedQuota(
+                    prefix=_build_quota_key(use_case_id, org_id),
+                    requested=len(strings),
+                    quotas=quotas,
+                )
+            )
+
+    return org_ids, requests
+
+
+RateLimitState = Tuple[UseCaseKey, Sequence[RequestedQuota], Sequence[GrantedQuota], Timestamp]
+
+
+class WritesLimiter:
+    def __init__(self) -> None:
+        self.rate_limiters: MutableMapping[UseCaseKey, RedisSlidingWindowRateLimiter] = {}
+
+    def _get_rate_limiter(self, use_case_id: UseCaseKey) -> RedisSlidingWindowRateLimiter:
+        if use_case_id not in self.rate_limiters:
+            options = get_ingest_config(use_case_id).writes_limiter_cluster_options
+            self.rate_limiters[use_case_id] = RedisSlidingWindowRateLimiter(**options)
+
+        return self.rate_limiters[use_case_id]
+
+    @metrics.wraps("sentry_metrics.indexer.check_write_limits")
+    def check_write_limits(
+        self, use_case_id: UseCaseKey, keys: KeyCollection
+    ) -> Tuple[RateLimitState, KeyCollection, Sequence[Tuple[KeyResult, FetchType, FetchTypeExt]]]:
+        """
+        Takes a KeyCollection and applies DB write limits as configured via sentry.options.
+
+        Returns a tuple of:
+
+        1. `RateLimitState` that needs to be passed to `apply_write_limits` in order
+          to commit quotas upon successful DB write.
+
+        2. A key collection containing all unmapped keys that passed through the
+          rate limiter.
+
+        3. All unmapped keys that did not pass through the rate limiter.
+        """
+
+        org_ids, requests = _construct_quota_requests(use_case_id, keys)
+        timestamp, grants = self._get_rate_limiter(use_case_id).check_within_quotas(requests)
+
+        granted_key_collection = dict(keys.mapping)
+        dropped_key_results = []
+
+        for org_id, request, grant in zip(org_ids, requests, grants):
+            allowed_strings = granted_key_collection[org_id]
+            if len(allowed_strings) > grant.granted:
+                allowed_strings = set(allowed_strings)
+
+                while len(allowed_strings) > grant.granted:
+                    dropped_key_results.append(
+                        (
+                            KeyResult(org_id=org_id, string=allowed_strings.pop(), id=None),
+                            FetchType.RATE_LIMITED,
+                            FetchTypeExt(
+                                is_global=any(
+                                    quota.prefix_override is not None
+                                    for quota in grant.reached_quotas
+                                ),
+                            ),
+                        )
+                    )
+
+            granted_key_collection[org_id] = allowed_strings
+
+        state = use_case_id, requests, grants, timestamp
+        return state, KeyCollection(granted_key_collection), dropped_key_results
+
+    @metrics.wraps("sentry_metrics.indexer.apply_write_limits")
+    def apply_write_limits(self, state: RateLimitState) -> None:
+        """
+        Consumes the rate limits returned by `check_write_limits`.
+        """
+        use_case_id, requests, grants, timestamp = state
+        self._get_rate_limiter(use_case_id).use_quotas(requests, grants, timestamp)
+
+
+writes_limiter = WritesLimiter()
