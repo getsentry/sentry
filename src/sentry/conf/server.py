0901dab0ff62fd04c3f56abe8e192a8d342b3504
Scott Cooper <scttcper@gmail.com>
2022-07-11 12:51:32 -0700

    feat(active-release): Send 1 hour deploy summary notification (#36404)
    The notification is sent 1 hour after a deploy is finished via a task that runs every 5 minutes

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -577,6 +577,7 @@ CELERY_IMPORTS = (
     "sentry.tasks.process_buffer",
     "sentry.tasks.relay",
     "sentry.tasks.release_registry",
+    "sentry.tasks.release_summary",
     "sentry.tasks.reports",
     "sentry.tasks.reprocessing",
     "sentry.tasks.reprocessing2",
@@ -718,6 +719,11 @@ CELERYBEAT_SCHEDULE = {
         "task": "sentry.tasks.digests.schedule_digests",
         "schedule": timedelta(seconds=30),
         "options": {"expires": 30},
+    },
+    "schedule-digest-release-summary": {
+        "task": "sentry.tasks.digest.release_summary",
+        "schedule": timedelta(minutes=5),
+        "options": {"expires": 60 * 5},
     },
     "check-monitors": {
         "task": "sentry.tasks.check_monitors",
