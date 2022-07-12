ceeba426e47148d81a5c2e7e68d1bd3f0122d649
Komal Saini <komalsaini@live.com>
2022-07-12 10:48:35 -0700

    testing other conditions

--- src/sentry/suspect_resolutions/resolved_in_active_release.py
+++ src/sentry/suspect_resolutions/resolved_in_active_release.py
@@ -2,44 +2,50 @@ from datetime import timedelta
 
 from django.utils import timezone
 
-from sentry.models import Deploy, Group, GroupRelease, GroupStatus, Release
+from sentry.models import Deploy, Group, GroupStatus, Release
 
 
 def is_resolved_issue_within_active_release(issue_id: int) -> bool:
     # check that the issue is resolved and is part of a release
-    if (not Group.objects.filter(id=issue_id, status=GroupStatus.RESOLVED).exists()) or len(
-        GroupRelease.objects.filter(id=issue_id).values_list("release_id", flat=True)
-    ) == 0:
+
+    issue = Group.objects.filter(id=issue_id).first()
+
+    if issue is None or issue.status != GroupStatus.RESOLVED or issue.get_last_release() is None:
+        print("first if statement")
+        print(issue)
+        if issue is not None:
+            print(issue.status)
+            print(issue.get_last_release)
+        print("if statement done")
         return False
-    else:
-        # check that the issue was resolved in active release
 
-        # returns the release version associated with the issue
-        latest_release_version_issue = Group.objects.filter(id=issue_id).first().get_last_release()
+    # returns the release version associated with the issue
+    latest_release_version_issue = issue.get_last_release()
 
-        # get release object from version
-        latest_release_issue = Release.objects.filter(version=latest_release_version_issue)
+    # get release object from version
+    latest_release_issue = Release.objects.filter(version=latest_release_version_issue)
 
-        # get latest deploy of the release associated with the issue
-        latest_deploy_release: Deploy = (
+    # get latest deploy of the release associated with the issue
+    latest_deploy_release: Deploy = (
             Deploy.objects.filter(release_id=latest_release_issue.first().id)
             .order_by("-date_finished")
             .first()
             or Deploy.objects.filter(id=latest_release_issue.first().last_deploy_id).first()
-        )
+    )
 
-        # get the time of the latest deploy
-        if not latest_deploy_release:
-            return False
+    # get the time of the latest deploy
+    if not latest_deploy_release:
+        print("latest_deploy_release is None")
+        return False
 
-        latest_deploy_release_time = latest_deploy_release.date_finished
+    latest_deploy_release_time = latest_deploy_release.date_finished
 
-        now_minus_1_hour = timezone.now() - timedelta(hours=1.0)
+    now_minus_1_hour = timezone.now() - timedelta(hours=1.0)
 
-        print("=" * 40)
-        print(now_minus_1_hour)
-        print(latest_deploy_release_time)
-        print(timezone.now())
-        print("=" * 40)
+    print("=" * 40)
+    print(now_minus_1_hour)
+    print(latest_deploy_release_time)
+    print(timezone.now())
+    print("=" * 40)
 
-        return bool(now_minus_1_hour <= latest_deploy_release_time <= timezone.now())
+    return now_minus_1_hour <= latest_deploy_release_time <= timezone.now()
