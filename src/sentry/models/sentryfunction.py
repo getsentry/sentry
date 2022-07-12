efef042766bd1ce3fbdca8688bb676abfe900217
vpandiarajan20 <vpandiarajan20@gmail.com>
2022-07-12 15:36:36 -0700

    feat(integrations): new table

---
+++ src/sentry/models/sentryfunction.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+from sentry.db.models import BaseManager, DefaultFieldsModel, FlexibleForeignKey
+
+
+class SentryFunctionManager(BaseManager):
+    def get_alertable_sentry_functions(self, organization_id):
+        # TODO: check events
+        functions = self.filter(
+            organization_id=organization_id,
+        )
+        return filter(lambda fn: "alert" in fn.events, list(functions))
+
+
+class SentryFunction(DefaultFieldsModel):
+    __include_in_export__ = False
+
+    organization = FlexibleForeignKey("sentry.Organization")
+    name = models.TextField()
+    slug = models.CharField(max_length=64, unique=True)
+    author = models.TextField(null=True)
+    external_id = models.CharField(max_length=128, unique=True)
+
+    objects = SentryFunctionManager()
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_sentryfunction"
+        unique_together = (("organization", "slug"),)
---
+++ tests/apidocs/endpoints/organizations/test_org_sentry_functions.py
@@ -0,0 +1,27 @@
+from django.test.client import RequestFactory
+from django.urls import reverse
+
+from fixtures.apidocs_test_case import APIDocsTestCase
+
+
+class OrganizationSentryFunctions(APIDocsTestCase):
+    def setUp(self):
+        organization = self.create_organization(owner=self.user, name="RowdyTiger")
+
+        self.url = reverse(
+            "sentry-api-0-organization-sentry-functions",
+            kwargs={"organization_slug": organization.slug},
+        )
+        project = self.create_project(name="foo", organization=organization, teams=[])
+        self.create_repo(project=project, name="getsentry/sentry")
+
+        self.login_as(user=self.user)
+
+    def test_post(self):
+        data = {"name": "foo", "author": "bar"}
+        response = self.client.post(self.url, data)
+        request = RequestFactory().post(self.url, data)
+        print(response)
+        print(request)
+
+        self.validate_schema(request, response)
