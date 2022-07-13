a906f3137222d3d49f07cdd7daba76febf1e2c81
Gilbert Szeto <gilbert.szeto@sentry.io>
2022-07-13 10:05:13 -0700

    remove commented out code

--- src/sentry/api/serializers/models/releaseactivity.py
+++ src/sentry/api/serializers/models/releaseactivity.py
@@ -7,16 +7,6 @@ from sentry.utils.json import JSONData
 
 @register(ReleaseActivity)
 class ReleaseActivitySerializer(Serializer):
-    # def get_attrs(self, item_list, user):
-    #     user_map = {d["id"]: d for d in serialize({i.user for i in item_list}, user)}
-    #
-    #     result = {}
-    #     for item in item_list:
-    #         result[item] = {"user": user_map[str(item.user_id)]}
-    #     return result
-    #
-    # def get_attrs(self, item_list: List[Any], user: Any, **kwargs: Any) -> MutableMapping[Any, Any]:
-
     def serialize(
         self, obj: Any, attrs: Mapping[Any, Any], user: Any, **kwargs: Any
     ) -> MutableMapping[str, JSONData]:
