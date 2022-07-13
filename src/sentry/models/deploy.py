f9f1962e46c3d842eb11d124c569bdb3b08db6e3
Gilbert Szeto <gilbert.szeto@sentry.io>
2022-07-12 22:34:40 -0700

    fix bad query

--- src/sentry/models/deploy.py
+++ src/sentry/models/deploy.py
@@ -94,7 +94,7 @@ class Deploy(Model):
                 activity.send_notification()
                 deploy.update(notified=True)
                 # XXX(workflow): delete this after WF 2.0 experiment over
-                org = Organization.objects.get(id=deploy.organization_id).first()
+                org = Organization.objects.filter(id=deploy.organization_id).first()
                 if org and features.has("organizations:active-release-monitor-alpha", org):
                     ReleaseActivity.objects.create(
                         type=ReleaseActivity.Type.deployed,
