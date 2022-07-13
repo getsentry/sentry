305d6d1fe94f898ec20aa1d33957bd8291980c70
Floris Bruynooghe <flub@sentry.io>
2022-07-13 14:04:32 +0200

    fix(projectconfig): Give the build task debounce key a short TTL (#36514)
    The TTL of the debounce key is made to match the hard timeout of the
    celery task.  This ensures that a debounce key can not live longer
    than the task can, and thus no sticky debounce key occurs when a task
    times out.  Because the schedule time is ignored this could result in
    unnecessary duplicate tasks, however if a task already took this long
    it probably wasn't going to succeed anyway.
    
    Secondly this sets a global expiry time for the task itself, an
    expired task will stay in RabbitMQ but the Celery worker will ack it
    without running the task.  This ensures that if workers are shut down
    during incidents or similar previous tasks will not be consuming
    computation resources.
    
    INGEST-1441
    INGEST-1442

--- src/sentry/relay/projectconfig_debounce_cache/__init__.py
+++ src/sentry/relay/projectconfig_debounce_cache/__init__.py
@@ -9,7 +9,12 @@ from .base import ProjectConfigDebounceCache
 backend = LazyServiceWrapper(
     ProjectConfigDebounceCache,
     settings.SENTRY_RELAY_PROJECTCONFIG_DEBOUNCE_CACHE,
-    settings.SENTRY_RELAY_PROJECTCONFIG_DEBOUNCE_CACHE_OPTIONS,
+    {
+        # The sentry.tasks.relay.build_project_config tasks scheduled on here has a deadline
+        # of 10s.  This debounce_ttl should match.
+        "debounce_ttl": 10,
+        **settings.SENTRY_RELAY_PROJECTCONFIG_DEBOUNCE_CACHE_OPTIONS,
+    },
 )
 
 backend.expose(locals())
