301fb63e5118e0a4093ce523e8b86562afcdd824
William Mak <william@wmak.io>
2022-07-14 15:04:27 +0200

    feat(mep): Generic metric dataset (#36623)
    - This properly switches the metric query builder over to the generic metrics dataset
    - This currently means that local metrics data will no longer work
    - This makes a bit of a TODO to come back and rename the
      Dataset.Metrics to Dataset.PerformanceMetrics
    - Also defaulting to Metrics for now as the default dataset to get tests
      passing, but will likely make it PerformanceMetrics instead in the
      long run
    - This also changes the hack to only happen when on the
      PerformanceMetrics dataset

--- src/sentry/snuba/metrics/datasource.py
+++ src/sentry/snuba/metrics/datasource.py
@@ -34,6 +34,7 @@ from sentry.snuba.metrics.query_builder import (
     translate_meta_results,
 )
 from sentry.snuba.metrics.utils import (
+    AVAILABLE_GENERIC_OPERATIONS,
     AVAILABLE_OPERATIONS,
     CUSTOM_MEASUREMENT_DATASETS,
     FIELD_ALIAS_MAPPINGS,
@@ -202,7 +203,9 @@ def get_custom_measurements(
                     MetricMeta(
                         name=parsed_mri.name,
                         type=metric_type,
-                        operations=AVAILABLE_OPERATIONS[METRIC_TYPE_TO_ENTITY[metric_type].value],
+                        operations=AVAILABLE_GENERIC_OPERATIONS[
+                            METRIC_TYPE_TO_ENTITY[metric_type].value
+                        ],
                         unit=parsed_mri.unit,
                         metric_id=row["metric_id"],
                     )
