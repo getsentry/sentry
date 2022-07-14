f5f8a4acdb851a6b5c7cd14d04df7d9386346fed
Edward Gou <edward.gou@sentry.io>
2022-07-14 09:57:57 +0200

    fixed sort

--- static/app/views/eventsV2/utils.tsx
+++ static/app/views/eventsV2/utils.tsx
@@ -520,7 +520,9 @@ export function generateFieldOptions({
   }
 
   if (customMeasurements !== undefined && customMeasurements !== null) {
-    customMeasurements.sort();
+    customMeasurements.sort(({key: currentKey}, {key: nextKey}) =>
+      currentKey > nextKey ? 1 : currentKey === nextKey ? 0 : -1
+    );
     customMeasurements.forEach(({key, functions: supportedFunctions}) => {
       fieldOptions[`measurement:${key}`] = {
         label: key,
