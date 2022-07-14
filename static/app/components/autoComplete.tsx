cb3e103afba5e7a1b992cbd8993f12061e815181
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-07-14 12:05:52 +0530

    tests

--- static/app/components/autoComplete.tsx
+++ static/app/components/autoComplete.tsx
@@ -378,9 +378,13 @@ class AutoComplete<T extends Item> extends Component<Props<T>, State<T>> {
   openMenu = (...args: Array<any>) => {
     const {onOpen, disabled} = this.props;
 
+    if (disabled) {
+      return;
+    }
+
     onOpen?.(...args);
 
-    if (disabled || this.isControlled) {
+    if (this.isControlled) {
       return;
     }
 
