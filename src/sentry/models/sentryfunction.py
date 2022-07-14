6a41524dbcdf3e318e21d21488960a235752d073
vpandiarajan20 <vpandiarajan20@gmail.com>
2022-07-14 10:57:34 -0700

    ref(integrations): remove extraneous custom manager

--- src/sentry/models/sentryfunction.py
+++ src/sentry/models/sentryfunction.py
@@ -3,16 +3,6 @@ from django.db import models
 from sentry.db.models import BaseManager, DefaultFieldsModel, FlexibleForeignKey
 
 
-class SentryFunctionManager(BaseManager):
-    def get_alertable_sentry_functions(self, organization_id):
-        # TODO: check events
-        functions = self.filter(
-            organization_id=organization_id,
-        )
-        return filter(lambda fn: "alert" in fn.organization, list(functions))
-        # needs to be changed
-
-
 class SentryFunction(DefaultFieldsModel):
     __include_in_export__ = False
 
@@ -22,7 +12,7 @@ class SentryFunction(DefaultFieldsModel):
     author = models.TextField()
     external_id = models.CharField(max_length=128, unique=True)
 
-    objects = SentryFunctionManager()
+    objects = BaseManager()
 
     class Meta:
         app_label = "sentry"
