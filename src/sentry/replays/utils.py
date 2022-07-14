5ca86dbb284a9420be1f209dd326bc1dd42b1b35
Josh Ferge <josh.ferge@sentry.io>
2022-07-14 16:03:44 -0700

    copy over retry and rename metric

--- src/sentry/replays/utils.py
+++ src/sentry/replays/utils.py
@@ -1,12 +1,44 @@
 from typing import Any, Dict, Optional
-from urllib.parse import urlencode
+from urllib.parse import urlencode, urlparse
 
+import urllib3
 from django.conf import settings
 from django.http import StreamingHttpResponse
 from urllib3.response import HTTPResponse
 
 from sentry.net.http import connection_from_url
-from sentry.utils.profiling import RetrySkipTimeout
+from sentry.utils import metrics
+
+
+class RetrySkipTimeout(urllib3.Retry):
+    """
+    urllib3 Retry class does not allow us to retry on read errors but to exclude
+    read timeout
+    """
+
+    def increment(
+        self, method=None, url=None, response=None, error=None, _pool=None, _stacktrace=None
+    ):
+        """
+        Just rely on the parent class unless we have a read timeout. In that case
+        immediately give up
+        """
+        if error and isinstance(error, urllib3.exceptions.ReadTimeoutError):
+            raise error.with_traceback(_stacktrace)
+
+        metrics.incr(
+            "replays.client.retry",
+            tags={"method": method, "path": urlparse(url).path if url else None},
+        )
+        return super().increment(
+            method=method,
+            url=url,
+            response=response,
+            error=error,
+            _pool=_pool,
+            _stacktrace=_stacktrace,
+        )
+
 
 _replays_pool = connection_from_url(
     settings.SENTRY_REPLAYS_SERVICE_URL,
