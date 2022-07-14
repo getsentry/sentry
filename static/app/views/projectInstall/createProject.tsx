6105e8bdef48aa67d340624861b1fb69030ab844
Zhixing Zhang <me@neoto.xin>
2022-07-14 15:31:18 -0700

    feat(alert): Create metric alerts on project creation (#36582)
    * UI & MakeUnqueriedContext
    
    * sending api request & patch teams
    
    * Add experiment & use Promise.all
    
    * rename events
    
    * use the new checkbox component

--- static/app/views/projectInstall/createProject.tsx
+++ static/app/views/projectInstall/createProject.tsx
@@ -19,12 +19,15 @@ import {inputStyles} from 'sentry/styles/input';
 import space from 'sentry/styles/space';
 import {Organization, Project, Team} from 'sentry/types';
 import {trackAnalyticsEvent} from 'sentry/utils/analytics';
+import trackAdvancedAnalyticsEvent from 'sentry/utils/analytics/trackAdvancedAnalyticsEvent';
 import getPlatformName from 'sentry/utils/getPlatformName';
 import slugify from 'sentry/utils/slugify';
 import withApi from 'sentry/utils/withApi';
 import withOrganization from 'sentry/utils/withOrganization';
 import withTeams from 'sentry/utils/withTeams';
 import IssueAlertOptions from 'sentry/views/projectInstall/issueAlertOptions';
+
+import {PRESET_AGGREGATES} from '../alerts/rules/metric/presets';
 
 const getCategoryName = (category?: string) =>
   categoryList.find(({id}) => id === category)?.id;
@@ -82,6 +85,12 @@ class CreateProject extends Component<Props, State> {
   get defaultCategory() {
     const {query} = this.props.location;
     return getCategoryName(query.category);
+  }
+
+  componentDidMount() {
+    trackAdvancedAnalyticsEvent('project_creation_page.viewed', {
+      organization: this.props.organization,
+    });
   }
 
   renderProjectForm() {
@@ -176,6 +185,7 @@ class CreateProject extends Component<Props, State> {
       actionMatch,
       frequency,
       defaultRules,
+      metricAlertPresets,
     } = dataFragment || {};
 
     this.setState({inFlight: true});
@@ -214,6 +224,50 @@ class CreateProject extends Component<Props, State> {
           }
         );
         ruleId = ruleData.id;
+      }
+      if (
+        !!organization.experiments.MetricAlertOnProjectCreationExperiment &&
+        metricAlertPresets &&
+        metricAlertPresets.length > 0
+      ) {
+        const presets = PRESET_AGGREGATES.filter(aggregate =>
+          metricAlertPresets.includes(aggregate.id)
+        );
+        const teamObj = this.props.teams.find(aTeam => aTeam.slug === team);
+        await Promise.all([
+          presets.map(preset => {
+            const context = preset.makeUnqueriedContext(
+              {
+                ...projectData,
+                teams: teamObj ? [teamObj] : [],
+              },
+              organization
+            );
+
+            return api.requestPromise(
+              `/projects/${organization.slug}/${projectData.slug}/alert-rules/?referrer=create_project`,
+              {
+                method: 'POST',
+                data: {
+                  aggregate: context.aggregate,
+                  comparisonDelta: context.comparisonDelta,
+                  dataset: context.dataset,
+                  eventTypes: context.eventTypes,
+                  name: context.name,
+                  owner: null,
+                  projectId: projectData.id,
+                  projects: [projectData.slug],
+                  query: '',
+                  resolveThreshold: null,
+                  thresholdPeriod: 1,
+                  thresholdType: context.thresholdType,
+                  timeWindow: context.timeWindow,
+                  triggers: context.triggers,
+                },
+              }
+            );
+          }),
+        ]);
       }
       this.trackIssueAlertOptionSelectedEvent(
         projectData,
