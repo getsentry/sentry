001e34f883462643def3d5282f15ae6db8c6d922
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-07-15 14:44:15 -0700

    remove some deadcode

--- static/app/views/replays/detail/tagPanel.tsx
+++ static/app/views/replays/detail/tagPanel.tsx
@@ -3,15 +3,10 @@ import styled from '@emotion/styled';
 
 import {withMeta} from 'sentry/components/events/meta/metaProxy';
 import {KeyValueTable} from 'sentry/components/keyValueTable';
-import {
-  Panel as BasePanel,
-  PanelHeader as BasePanelHeader,
-} from 'sentry/components/panels';
+import {Panel as BasePanel} from 'sentry/components/panels';
 import Placeholder from 'sentry/components/placeholder';
 import {useReplayContext} from 'sentry/components/replays/replayContext';
 import TagsTableRow from 'sentry/components/tagsTableRow';
-import {t} from 'sentry/locale';
-import space from 'sentry/styles/space';
 import FluidPanel from 'sentry/views/replays/detail/layout/fluidPanel';
 
 function TagPanel() {
@@ -30,7 +25,7 @@ function TagPanel() {
 
   return (
     <Panel>
-      <FluidPanel title={<PanelHeader>{t('Tags')}</PanelHeader>}>
+      <FluidPanel>
         <KeyValueTable>
           {tags.map(tag => (
             <TagsTableRow
@@ -51,16 +46,6 @@ const Panel = styled(BasePanel)`
   height: 100%;
   overflow: hidden;
   margin-bottom: 0;
-`;
-
-const PanelHeader = styled(BasePanelHeader)`
-  background-color: ${p => p.theme.background};
-  border-bottom: 1px solid ${p => p.theme.innerBorder};
-  font-size: ${p => p.theme.fontSizeSmall};
-  color: ${p => p.theme.gray500};
-  text-transform: capitalize;
-  padding: ${space(1)} ${space(1.5)} ${space(1)};
-  font-weight: 600;
 `;
 
 export default TagPanel;
