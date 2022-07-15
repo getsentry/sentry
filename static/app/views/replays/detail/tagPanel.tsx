59c3e4c3a666f5edad774adc9b08c3e71ee3e6b2
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-07-14 17:09:47 -0700

    keep up with TagTable changes

--- static/app/views/replays/detail/tagPanel.tsx
+++ static/app/views/replays/detail/tagPanel.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
 
+import {withMeta} from 'sentry/components/events/meta/metaProxy';
 import {KeyValueTable} from 'sentry/components/keyValueTable';
 import Placeholder from 'sentry/components/placeholder';
 import {useReplayContext} from 'sentry/components/replays/replayContext';
-import {TagsTableRow} from 'sentry/components/tagsTable';
+import TagsTableRow from 'sentry/components/tagsTableRow';
 import {t} from 'sentry/locale';
 import FluidPanel from 'sentry/views/replays/detail/layout/fluidPanel';
 
 function TagPanel() {
   const {replay} = useReplayContext();
-  const tags = replay?.getEvent().tags;
+  const event = replay?.getEvent();
+
+  if (!event) {
+    return <Placeholder height="400px" />;
+  }
+
+  const eventWithMeta = withMeta(event);
+  const tags = eventWithMeta.tags;
 
   const query = '';
   const generateUrl = () => '';
-
-  if (!tags) {
-    return <Placeholder height="400px" />;
-  }
 
   return (
     <FluidPanel panel title={t('Tags')}>
