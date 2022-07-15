cc3acd5dc18addc19d92b792d32ff3ebd80d0633
Malachi Willey <malwilley@gmail.com>
2022-07-15 13:09:47 -0500

    feat(ui): Allow user to select arbitrary relative time ranges in TimeRangeSelector (#36624)

--- static/app/components/organizations/timeRangeSelector/index.tsx
+++ static/app/components/organizations/timeRangeSelector/index.tsx
@@ -34,6 +34,7 @@ import getDynamicText from 'sentry/utils/getDynamicText';
 import getRouteStringFromRoutes from 'sentry/utils/getRouteStringFromRoutes';
 
 import SelectorItems from './selectorItems';
+import timeRangeAutoCompleteFilter from './timeRangeAutoCompleteFilter';
 
 const DateRangeHook = HookOrDefault({
   hookName: 'component:header-date-range',
@@ -437,6 +438,7 @@ class TimeRangeSelector extends PureComponent<Props, State> {
             {({css}) => (
               <StyledDropdownAutoComplete
                 allowActorToggle
+                autoCompleteFilter={timeRangeAutoCompleteFilter}
                 alignMenu={alignDropdown ?? (isAbsoluteSelected ? 'right' : 'left')}
                 isOpen={this.state.isOpen}
                 inputValue={this.state.inputValue}
@@ -449,7 +451,7 @@ class TimeRangeSelector extends PureComponent<Props, State> {
                 maxHeight={400}
                 detached={detached}
                 items={items}
-                searchPlaceholder={t('Filter time range')}
+                searchPlaceholder={t('Provide a time range')}
                 rootClassName={css`
                   position: relative;
                   display: flex;
