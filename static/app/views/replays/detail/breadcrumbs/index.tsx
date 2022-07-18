d93aa76d69ddd699b0370826a80a71415d919a08
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-07-18 11:32:47 -0700

    feat(replays): Fix issues in Replay Details v2 layout and replace the original layout styles (#36705)
    This is a big iteration on the v2 layout for Replay Details, so big that I've just gone and removed/replaced the v1 code so we've only got the one codebase going forward.
    
    Changes:
    - Remove `react-resize-panel` dependency, was about 300kb thanks to bringing in `cash-dom`
    - Create `<SplitPanel>` as a replacement for react-size-panel. 
        - It's got features for setting default/min/max sizes of each panel, expressed as pixels or percents
        - Mouse tracking is always in effect across basically the whole page now. So lots of CPU will be used up. Something to optimize later
    - Create `<FluidPanel>`  to make it easier to flow content within the `100vh` browser window
    - Migrated the new 'Condensed Header' code into the v2 design
        - This was merged into the v1 design, some refactoring and it's here now
    - Fixed up some padding/margin issues mostly around & inside the sidebar.
    - Don't need a title for "Tags" in the sidebar -> the tabs above serve as a title.
    - Added a 2nd resize handle in the `topbar` layout -> now you can resize the video & breadcrumbs columns.
    
    Fixes #36280

--- static/app/views/replays/detail/breadcrumbs/index.tsx
+++ static/app/views/replays/detail/breadcrumbs/index.tsx
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 
 import {
   Panel as BasePanel,
-  PanelBody as BasePanelBody,
   PanelHeader as BasePanelHeader,
 } from 'sentry/components/panels';
 import Placeholder from 'sentry/components/placeholder';
@@ -14,10 +13,9 @@ import space from 'sentry/styles/space';
 import {Crumb} from 'sentry/types/breadcrumbs';
 import {getPrevBreadcrumb} from 'sentry/utils/replays/getBreadcrumb';
 import {useCurrentItemScroller} from 'sentry/utils/replays/hooks/useCurrentItemScroller';
+import FluidPanel from 'sentry/views/replays/detail/layout/fluidPanel';
 
 import BreadcrumbItem from './breadcrumbItem';
-
-const TAB_HEADER_HEIGHT = 28;
 
 function CrumbPlaceholder({number}: {number: number}) {
   return (
@@ -107,25 +105,33 @@ function Breadcrumbs({}: Props) {
     [setCurrentTime, startTimestamp]
   );
 
+  const content = isLoaded ? (
+    <div>
+      {crumbs.map(crumb => (
+        <BreadcrumbItem
+          key={crumb.id}
+          crumb={crumb}
+          startTimestamp={startTimestamp}
+          isHovered={closestUserAction?.id === crumb.id}
+          isSelected={currentUserAction?.id === crumb.id}
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          onClick={handleClick}
+        />
+      ))}
+    </div>
+  ) : (
+    <CrumbPlaceholder number={4} />
+  );
+
   return (
     <Panel>
-      <PanelHeader>{t('Breadcrumbs')}</PanelHeader>
-      <PanelBody ref={crumbListContainerRef}>
-        {!isLoaded && <CrumbPlaceholder number={4} />}
-        {isLoaded &&
-          crumbs.map(crumb => (
-            <BreadcrumbItem
-              key={crumb.id}
-              crumb={crumb}
-              startTimestamp={startTimestamp}
-              isHovered={closestUserAction?.id === crumb.id}
-              isSelected={currentUserAction?.id === crumb.id}
-              onMouseEnter={handleMouseEnter}
-              onMouseLeave={handleMouseLeave}
-              onClick={handleClick}
-            />
-          ))}
-      </PanelBody>
+      <FluidPanel
+        bodyRef={crumbListContainerRef}
+        title={<PanelHeader>{t('Breadcrumbs')}</PanelHeader>}
+      >
+        {content}
+      </FluidPanel>
     </Panel>
   );
 }
@@ -134,6 +140,7 @@ const Panel = styled(BasePanel)`
   width: 100%;
   height: 100%;
   overflow: hidden;
+  margin-bottom: 0;
 `;
 
 const PanelHeader = styled(BasePanelHeader)`
@@ -144,12 +151,6 @@ const PanelHeader = styled(BasePanelHeader)`
   text-transform: capitalize;
   padding: ${space(1)} ${space(1.5)} ${space(1)};
   font-weight: 600;
-`;
-
-const PanelBody = styled(BasePanelBody)`
-  padding: ${space(0.5)};
-  overflow-y: auto;
-  max-height: calc(100% - ${TAB_HEADER_HEIGHT}px);
 `;
 
 const PlaceholderMargin = styled(Placeholder)`
