a4c36623926ef3433b62a7fc312bc94d5854a84a
William Mak <william@wmak.io>
2022-07-18 20:48:53 +0200

    fix(tests): skip flaking acceptance test (#36767)
    - This test is flaking quite often right now, skipping until someone can
      take a better look at it

--- tests/acceptance/test_project_settings_sampling.py
+++ tests/acceptance/test_project_settings_sampling.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
@@ -107,6 +108,7 @@ class ProjectSettingsSamplingTest(AcceptanceTestCase):
                 == serializer.validated_data["rules"][0]
             )
 
+    @pytest.mark.skip(reason="Flaking pretty consistently")
     def test_add_uniform_rule_with_custom_sampling_values(self):
         with self.feature(FEATURE_NAME):
             self.wait_until_page_loaded()
