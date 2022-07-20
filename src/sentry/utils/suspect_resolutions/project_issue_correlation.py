96f9702f73b391dbc9228f649c6252fb87b6b550
Komal Saini <komalsaini@live.com>
2022-07-20 11:23:53 -0700

    scan all issues in a project for correlated issues

---
+++ src/sentry/utils/suspect_resolutions/project_issue_correlation.py
@@ -0,0 +1,23 @@
+from sentry.models import Group
+from sentry.utils.suspect_resolutions.commit_correlation import is_issue_commit_correlated
+from sentry.utils.suspect_resolutions.metric_correlation import is_issue_error_rate_correlated
+
+
+def get_project_issues_with_correlated_commits_and_error_rate(
+    project_id: int, resolved_issue: Group
+):
+    if not Group.objects.filter(id=resolved_issue.id).exists():
+        return {}
+
+    all_project_issues = list(
+        Group.objects.filter(project=project_id).exclude(id=resolved_issue.id)
+    )
+
+    correlated_issues = {
+        issue.id
+        for issue in all_project_issues
+        if is_issue_commit_correlated(resolved_issue.id, issue.id)
+        and is_issue_error_rate_correlated(resolved_issue, issue)
+    }
+
+    return correlated_issues
