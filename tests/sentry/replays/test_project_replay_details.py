42b207a63fc959bd94f625fe035b7dd1176f9793
Colton Allen <colton.allen@sentry.io>
2022-07-21 11:46:56 -0500

    Add coverage for snuba aggreations on the details endpoint

--- tests/sentry/replays/test_project_replay_details.py
+++ tests/sentry/replays/test_project_replay_details.py
@@ -1,4 +1,5 @@
 import datetime
+import typing
 import uuid
 
 from django.urls import reverse
@@ -29,52 +30,26 @@ class OrganizationReplayDetailsTest(APITestCase, ReplaysSnubaTestCase):
             assert response.status_code == 404
 
     def test_get_replay(self):
-        timestamp = datetime.datetime.now() - datetime.timedelta(minutes=5)
+        seq1_timestamp = datetime.datetime.now() - datetime.timedelta(seconds=6)
+        seq2_timestamp = datetime.datetime.now() - datetime.timedelta(seconds=3)
+        seq3_timestamp = datetime.datetime.now() - datetime.timedelta(seconds=1)
+
         replay_id = str(self.replay_id)
 
+        self.store_replays(mock_replay(seq1_timestamp, self.project.id, replay_id))
         self.store_replays(
-            {
-                "datetime": int(timestamp.timestamp()),
-                "platform": "javascript",
-                "project_id": self.project.id,
-                "replay_id": replay_id,
-                "retention_days": 20,
-                "sequence_id": 0,
-                "trace_ids": ["ffb5344a-41dd-4b21-9288-187a2cd1ad6d"],
-                "data": {
-                    "timestamp": int(timestamp.timestamp()),
-                    "replay_id": replay_id,
-                    "environment": "production",
-                    "project_id": self.project.id,
-                    "sdk": {
-                        "name": "sentry.javascript.react",
-                        "version": "6.18.1",
-                        "integrations": [
-                            "InboundFilters",
-                            "FunctionToString",
-                            "TryCatch",
-                            "Breadcrumbs",
-                            "GlobalHandlers",
-                            "LinkedErrors",
-                            "Dedupe",
-                            "UserAgent",
-                            "Replay",
-                            "BrowserTracing",
-                        ],
-                        "packages": [{"name": "npm:@sentry/react", "version": "6.18.1"}],
-                    },
-                    "platform": "javascript",
-                    "version": "6.18.1",
-                    "type": "replay_event",
-                    "datetime": int(timestamp.timestamp()),
-                    "tags": [
-                        ["isReplayRoot", "yes"],
-                        ["skippedNormalization", "True"],
-                        ["transaction", "/"],
-                    ],
-                    "title": "test",
-                },
-            }
+            mock_replay(
+                seq2_timestamp,
+                self.project.id,
+                replay_id,
+                sequence_id=1,
+                trace_ids=["2a0dcb0e-a1fb-4350-b266-47ae1aa57dfb"],
+            )
+        )
+        self.store_replays(
+            mock_replay(
+                seq3_timestamp, self.project.id, replay_id, sequence_id=2, sdk_version="16.8.2"
+            )
         )
 
         with self.feature(REPLAYS_FEATURES):
@@ -99,17 +74,76 @@ class OrganizationReplayDetailsTest(APITestCase, ReplaysSnubaTestCase):
                 "user_hash": 0,
                 "user_name": "",
                 "sdk_name": "sentry.javascript.react",
-                "sdk_version": "6.18.1",
-                "trace_ids": ["ffb5344a-41dd-4b21-9288-187a2cd1ad6d"],
-                "started_at": datetime.datetime.strftime(timestamp, "%Y-%m-%dT%H:%M:%S+00:00"),
-                "finished_at": datetime.datetime.strftime(timestamp, "%Y-%m-%dT%H:%M:%S+00:00"),
-                "duration": 0,
-                "urls": [""],
-                "count_urls": 1,
-                "count_sequences": 1,
-                "tags": {"isReplayRoot": "yes", "skippedNormalization": "True", "transaction": "/"},
+                "sdk_version": "16.8.2",
+                "trace_ids": [
+                    "ffb5344a-41dd-4b21-9288-187a2cd1ad6d",
+                    "2a0dcb0e-a1fb-4350-b266-47ae1aa57dfb",
+                ],
+                "started_at": datetime.datetime.strftime(seq1_timestamp, "%Y-%m-%dT%H:%M:%S+00:00"),
+                "finished_at": datetime.datetime.strftime(
+                    seq3_timestamp, "%Y-%m-%dT%H:%M:%S+00:00"
+                ),
+                "duration": 5,
+                "urls": ["", "", ""],  # WRONG.
+                "count_urls": 3,  # WRONG.
+                "count_sequences": 3,
+                "tags": {
+                    "isReplayRoot": "yes",
+                    "skippedNormalization": "True",
+                    "transaction": "/",
+                },
                 "count_errors": 0,
                 "longest_transaction": 0,
             }
 
             utils.assert_expected_response(response_data["data"], expected_response)
+
+
+def mock_replay(
+    timestamp: datetime.datetime,
+    project_id: str,
+    replay_id: str,
+    **kwargs: typing.Dict[str, typing.Any],
+) -> typing.Dict[str, typing.Any]:
+    return {
+        "datetime": int(timestamp.timestamp()),
+        "platform": "javascript",
+        "project_id": project_id,
+        "replay_id": replay_id,
+        "retention_days": 20,
+        "sequence_id": kwargs.pop("sequence_id", 0),
+        "trace_ids": kwargs.pop("trace_ids", ["ffb5344a-41dd-4b21-9288-187a2cd1ad6d"]),
+        "data": {
+            "timestamp": int(timestamp.timestamp()),
+            "replay_id": replay_id,
+            "environment": "production",
+            "project_id": project_id,
+            "sdk": {
+                "name": kwargs.pop("sdk_name", "sentry.javascript.react"),
+                "version": kwargs.pop("sdk_version", "6.18.1"),
+                "integrations": [
+                    "InboundFilters",
+                    "FunctionToString",
+                    "TryCatch",
+                    "Breadcrumbs",
+                    "GlobalHandlers",
+                    "LinkedErrors",
+                    "Dedupe",
+                    "UserAgent",
+                    "Replay",
+                    "BrowserTracing",
+                ],
+                "packages": [{"name": "npm:@sentry/react", "version": "6.18.1"}],
+            },
+            "platform": "javascript",
+            "version": "6.18.1",
+            "type": "replay_event",
+            "datetime": int(timestamp.timestamp()),
+            "tags": [
+                ["isReplayRoot", "yes"],
+                ["skippedNormalization", "True"],
+                ["transaction", "/"],
+            ],
+            "title": "test",
+        },
+    }
