768553fed5b5de6be40fbf8f7009bebae92d16ae
Nar Saynorath <nar.saynorath@sentry.io>
2022-07-21 10:26:22 -0400

    feat(dashboard-filters): Save on dashboard creation (#36862)
    Pass page filter values along in dashboard create requests

--- static/app/views/dashboardsV2/detail.tsx
+++ static/app/views/dashboardsV2/detail.tsx
@@ -497,7 +497,31 @@ class DashboardDetail extends Component<Props, State> {
               was_previewed: true,
             });
           }
-          createDashboard(api, organization.slug, modifiedDashboard, this.isPreview).then(
+          let newModifiedDashboard = modifiedDashboard;
+          if (organization.features.includes('dashboards-top-level-filter')) {
+            const {project, environment, statsPeriod, start, end} = location.query;
+            newModifiedDashboard = {
+              ...cloneDashboard(modifiedDashboard),
+              // Ensure projects and environment are sent as arrays, or undefined in the request
+              // location.query will return a string if there's only one value
+              projects:
+                project === undefined
+                  ? []
+                  : typeof project === 'string'
+                  ? [Number(project)]
+                  : project.map(Number),
+              environment: typeof environment === 'string' ? [environment] : environment,
+              period: statsPeriod,
+              start,
+              end,
+            };
+          }
+          createDashboard(
+            api,
+            organization.slug,
+            newModifiedDashboard,
+            this.isPreview
+          ).then(
             (newDashboard: DashboardDetails) => {
               addSuccessMessage(t('Dashboard created'));
               trackAnalyticsEvent({
