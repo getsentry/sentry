getsentry/sentry

commit 6e060d6d0c806fe935e77904303763e959531528

feat(search): Redesign the search dropdown ui items (#35859)
Update the search dropdown UI to be way more informative including descriptions and field types. Also renders recent searches as pretty filter tokens. This will be followed up by a PR containing rich descriptions for each of the field keys.

Also changes the Issues screen default state to be the list of all available keys

static/app/views/issueList/searchBar.tsx
@@ -1,6 +1,5 @@
 import {Component} from 'react';
 
-import {fetchRecentSearches} from 'sentry/actionCreators/savedSearches';
 import {Client} from 'sentry/api';
 import SmartSearchBar from 'sentry/components/smartSearchBar';
 import {
@@ -11,8 +10,11 @@ import {
 import {ItemType, SearchItem} from 'sentry/components/smartSearchBar/types';
 import {t} from 'sentry/locale';
 import {Organization, SavedSearch, SavedSearchType, Tag} from 'sentry/types';
+import {getFieldDoc} from 'sentry/utils/discover/fields';
 import withApi from 'sentry/utils/withApi';
 import withOrganization from 'sentry/utils/withOrganization';
+
+import {FieldValueKind} from '../eventsV2/table/types';
 
 import {TagValueLoader} from './types';
 
@@ -54,6 +56,7 @@ type Props = React.ComponentProps<typeof SmartSearchBar> & {
   onSidebarToggle: (e: React.MouseEvent) => void;
   organization: Organization;
   sort: string;
+  supportedTags: {[key: string]: Tag};
   tagValueLoader: TagValueLoader;
   /**
    * Used to define the max height of the menu in px.
@@ -66,36 +69,37 @@ type Props = React.ComponentProps<typeof SmartSearchBar> & {
 type State = {
   defaultSearchItems: [SearchItem[], SearchItem[]];
   recentSearches: string[];
+  supportedTags: {[key: string]: Tag};
 };
 
 class IssueListSearchBar extends Component<Props, State> {
   state: State = {
     defaultSearchItems: [SEARCH_ITEMS, []],
     recentSearches: [],
+    supportedTags: {},
   };
 
   componentDidMount() {
     // Ideally, we would fetch on demand (e.g. when input gets focus)
     // but `<SmartSearchBar>` is a bit complicated and this is the easiest route
-    this.fetchData();
+    this.getSupportedTags();
   }
 
-  fetchData = async () => {
-    this.props.api.clear();
-    const resp = await this.getRecentSearches();
+  getSupportedTags = () => {
+    const {supportedTags} = this.props;
+
+    const newTags = Object.fromEntries(
+      Object.keys(supportedTags).map(key => [
+        key,
+        {
+          ...supportedTags[key],
+          kind: supportedTags[key].predefined ? FieldValueKind.TAG : FieldValueKind.FIELD,
+        },
+      ])
+    );
 
     this.setState({
-      defaultSearchItems: [
-        SEARCH_ITEMS,
-        resp
-          ? resp.map(query => ({
-              desc: query,
-              value: query,
-              type: ItemType.RECENT_SEARCH,
-            }))
-          : [],
-      ],
-      recentSearches: resp,
+      supportedTags: newTags,
     });
   };
 
@@ -109,21 +113,6 @@ class IssueListSearchBar extends Component<Props, State> {
     return values.map(({value}) => value);
   };
 
-  getRecentSearches = async (): Promise<string[]> => {
-    const {api, organization} = this.props;
-    const recent = await fetchRecentSearches(
-      api,
-      organization.slug,
-      SavedSearchType.ISSUE
-    );
-    return recent?.map(({query}) => query) ?? [];
-  };
-
-  handleSavedRecentSearch = () => {
-    // Reset recent searches
-    this.fetchData();
-  };
-
   render() {
     const {tagValueLoader: _, savedSearch, sort, onSidebarToggle, ...props} = this.props;
 
@@ -135,15 +124,15 @@ class IssueListSearchBar extends Component<Props, State> {
         hasRecentSearches
         savedSearchType={SavedSearchType.ISSUE}
         onGetTagValues={this.getTagValues}
-        defaultSearchItems={this.state.defaultSearchItems}
-        onSavedRecentSearch={this.handleSavedRecentSearch}
         actionBarItems={[
           makePinSearchAction({sort, pinnedSearch}),
           makeSaveSearchAction({sort}),
           makeSearchBuilderAction({onSidebarToggle}),
         ]}
-        maxMenuHeight={500}
         {...props}
+        maxMenuHeight={500}
+        supportedTags={this.state.supportedTags}
+        getFieldDoc={getFieldDoc}
       />
     );
   }

static/app/components/events/searchBar.tsx
@@ -7,7 +7,7 @@ import omit from 'lodash/omit';
 import {fetchTagValues} from 'sentry/actionCreators/tags';
 import SmartSearchBar from 'sentry/components/smartSearchBar';
 import {NEGATION_OPERATOR, SEARCH_WILDCARD} from 'sentry/constants';
-import {Organization, SavedSearchType, TagCollection} from 'sentry/types';
+import {Organization, SavedSearchType, Tag, TagCollection} from 'sentry/types';
 import {defined} from 'sentry/utils';
 import {
   Field,
@@ -22,11 +22,61 @@ import {
 import Measurements from 'sentry/utils/measurements/measurements';
 import useApi from 'sentry/utils/useApi';
 import withTags from 'sentry/utils/withTags';
+import {FieldValueKind} from 'sentry/views/eventsV2/table/types';
 
 const SEARCH_SPECIAL_CHARS_REGEXP = new RegExp(
   `^${NEGATION_OPERATOR}|\\${SEARCH_WILDCARD}`,
   'g'
 );
+
+const getFunctionTags = (fields: Readonly<Field[]>) =>
+  Object.fromEntries(
+    fields
+      .filter(
+        item => !Object.keys(FIELD_TAGS).includes(item.field) && !isEquation(item.field)
+      )
+      .map(item => [
+        item.field,
+        {key: item.field, name: item.field, kind: FieldValueKind.FUNCTION},
+      ])
+  );
+
+const getFieldTags = () =>
+  Object.fromEntries(
+    Object.keys(FIELD_TAGS).map(key => [
+      key,
+      {
+        ...FIELD_TAGS[key],
+        kind: FieldValueKind.FIELD,
+      },
+    ])
+  );
+
+const getMeasurementTags = (
+  measurements: Parameters<
+    React.ComponentProps<typeof Measurements>['children']
+  >[0]['measurements']
+) =>
+  Object.fromEntries(
+    Object.keys(measurements).map(key => [
+      key,
+      {
+        ...measurements[key],
+        kind: FieldValueKind.MEASUREMENT,
+      },
+    ])
+  );
+
+const getSemverTags = () =>
+  Object.fromEntries(
+    Object.keys(SEMVER_TAGS).map(key => [
+      key,
+      {
+        ...SEMVER_TAGS[key],
+        kind: FieldValueKind.FIELD,
+      },
+    ])
+  );
 
 export type SearchBarProps = Omit<React.ComponentProps<typeof SmartSearchBar>, 'tags'> & {
   organization: Organization;
@@ -103,30 +153,43 @@ function SearchBar(props: SearchBarProps) {
       React.ComponentProps<typeof Measurements>['children']
     >[0]['measurements']
   ) => {
-    const functionTags = fields
-      ? Object.fromEntries(
-          fields
-            .filter(
-              item =>
-                !Object.keys(FIELD_TAGS).includes(item.field) && !isEquation(item.field)
-            )
-            .map(item => [item.field, {key: item.field, name: item.field}])
-        )
-      : {};
+    const functionTags = getFunctionTags(fields ?? []);
+    const fieldTags = getFieldTags();
+    const measurementsWithKind = getMeasurementTags(measurements);
+    const semverTags = getSemverTags();
 
-    const fieldTags = organization.features.includes('performance-view')
-      ? Object.assign({}, measurements, FIELD_TAGS, functionTags)
-      : omit(FIELD_TAGS, TRACING_FIELDS);
+    const orgHasPerformanceView = organization.features.includes('performance-view');
 
-    const combined = assign({}, tags, fieldTags, SEMVER_TAGS);
-    combined.has = {
+    const combinedTags: Record<string, Tag> = orgHasPerformanceView
+      ? Object.assign({}, measurementsWithKind, fieldTags, functionTags)
+      : omit(fieldTags, TRACING_FIELDS);
+
+    const tagsWithKind = Object.fromEntries(
+      Object.keys(tags).map(key => [
+        key,
+        {
+          ...tags[key],
+          kind: FieldValueKind.TAG,
+        },
+      ])
+    );
+
+    assign(combinedTags, tagsWithKind, fieldTags, semverTags);
+
+    const sortedTagKeys = Object.keys(combinedTags);
+    sortedTagKeys.sort((a, b) => {
+      return a.toLowerCase().localeCompare(b.toLowerCase());
+    });
+
+    combinedTags.has = {
       key: 'has',
       name: 'Has property',
-      values: Object.keys(combined),
+      values: sortedTagKeys,
       predefined: true,
+      kind: FieldValueKind.FIELD,
     };
 
-    return omit(combined, omitTags ?? []);
+    return omit(combinedTags, omitTags ?? []);
   };
 
   return (

static/app/components/smartSearchBar/index.tsx
@@ -51,8 +51,9 @@ import {ItemType, SearchGroup, SearchItem, Shortcut, ShortcutType} from './types
 import {
   addSpace,
   createSearchGroups,
-  filterSearchGroupsByIndex,
   generateOperatorEntryMap,
+  getSearchGroupWithItemMarkedActive,
+  getTagItemsFromKeys,
   getValidOps,
   removeSpace,
   shortcuts,
@@ -678,21 +679,7 @@ class SmartSearchBar extends Component<Props, State> {
       evt.preventDefault();
 
       const {flatSearchItems, activeSearchItem} = this.state;
-      const searchGroups = [...this.state.searchGroups];
-
-      const [groupIndex, childrenIndex] = isSelectingDropdownItems
-        ? filterSearchGroupsByIndex(searchGroups, activeSearchItem)
-        : [];
-
-      // Remove the previous 'active' property
-      if (typeof groupIndex !== 'undefined') {
-        if (
-          childrenIndex !== undefined &&
-          searchGroups[groupIndex]?.children?.[childrenIndex]
-        ) {
-          delete searchGroups[groupIndex].children[childrenIndex].active;
-        }
-      }
+      let searchGroups = [...this.state.searchGroups];
 
       const currIndex = isSelectingDropdownItems ? activeSearchItem : 0;
       const totalItems = flatSearchItems.length;
@@ -705,23 +692,13 @@ class SmartSearchBar extends Component<Props, State> {
           ? (currIndex + 1) % totalItems
           : 0;
 
-      const [nextGroupIndex, nextChildrenIndex] = filterSearchGroupsByIndex(
-        searchGroups,
-        nextActiveSearchItem
-      );
+      // Clear previous selection
+      const prevItem = flatSearchItems[currIndex];
+      searchGroups = getSearchGroupWithItemMarkedActive(searchGroups, prevItem, false);
 
-      // Make sure search items exist (e.g. both groups could be empty) and
-      // attach the 'active' property to the item
-      if (
-        nextGroupIndex !== undefined &&
-        nextChildrenIndex !== undefined &&
-        searchGroups[nextGroupIndex]?.children
-      ) {
-        searchGroups[nextGroupIndex].children[nextChildrenIndex] = {
-          ...searchGroups[nextGroupIndex].children[nextChildrenIndex],
-          active: true,
-        };
-      }
+      // Set new selection
+      const activeItem = flatSearchItems[nextActiveSearchItem];
+      searchGroups = getSearchGroupWithItemMarkedActive(searchGroups, activeItem, true);
 
       this.setState({searchGroups, activeSearchItem: nextActiveSearchItem});
     }
@@ -733,15 +710,9 @@ class SmartSearchBar extends Component<Props, State> {
     ) {
       evt.preventDefault();
 
-      const {activeSearchItem, searchGroups} = this.state;
-      const [groupIndex, childrenIndex] = filterSearchGroupsByIndex(
-        searchGroups,
-        activeSearchItem
-      );
-      const item =
-        groupIndex !== undefined &&
-        childrenIndex !== undefined &&
-        searchGroups[groupIndex].children[childrenIndex];
+      const {activeSearchItem, flatSearchItems} = this.state;
+
+      const item = flatSearchItems[activeSearchItem];
 
       if (item) {
         if (item.callback) {
@@ -800,26 +771,28 @@ class SmartSearchBar extends Component<Props, State> {
     }
 
     evt.preventDefault();
-    const isSelectingDropdownItems = this.state.activeSearchItem > -1;
 
     if (!this.state.showDropdown) {
       this.blur();
       return;
     }
 
-    const {searchGroups, activeSearchItem} = this.state;
-    const [groupIndex, childrenIndex] = isSelectingDropdownItems
-      ? filterSearchGroupsByIndex(searchGroups, activeSearchItem)
-      : [];
+    const {flatSearchItems, activeSearchItem} = this.state;
+    const isSelectingDropdownItems = this.state.activeSearchItem > -1;
 
-    if (groupIndex !== undefined && childrenIndex !== undefined) {
-      delete searchGroups[groupIndex].children[childrenIndex].active;
+    let searchGroups = [...this.state.searchGroups];
+    if (isSelectingDropdownItems) {
+      searchGroups = getSearchGroupWithItemMarkedActive(
+        searchGroups,
+        flatSearchItems[activeSearchItem],
+        false
+      );
     }
 
     this.setState({
       activeSearchItem: -1,
       showDropdown: false,
-      searchGroups: [...this.state.searchGroups],
+      searchGroups,
     });
   };
 
@@ -917,8 +890,7 @@ class SmartSearchBar extends Component<Props, State> {
 
     const supportedTags = this.props.supportedTags ?? {};
 
-    // Return all if query is empty
-    let tagKeys = Object.keys(supportedTags).map(key => `${key}:`);
+    let tagKeys = Object.keys(supportedTags);
 
     if (query) {
       const preparedQuery =
@@ -929,19 +901,12 @@ class SmartSearchBar extends Component<Props, State> {
     // If the environment feature is active and excludeEnvironment = true
     // then remove the environment key
     if (this.props.excludeEnvironment) {
-      tagKeys = tagKeys.filter(key => key !== 'environment:');
+      tagKeys = tagKeys.filter(key => key !== 'environment');
     }
 
-    return [
-      tagKeys
-        .map(value => ({
-          value,
-          desc: value,
-          documentation: getFieldDoc?.(value.slice(0, -1)) ?? '',
-        }))
-        .sort((a, b) => a.value.localeCompare(b.value)),
-      supportedTagType ?? ItemType.TAG_KEY,
-    ];
+    const tagItems = getTagItemsFromKeys(tagKeys, supportedTags, getFieldDoc);
+
+    return [tagItems, supportedTagType ?? ItemType.TAG_KEY];
   }
 
   /**
@@ -1208,11 +1173,13 @@ class SmartSearchBar extends Component<Props, State> {
     const {query} = this.state;
     const [defaultSearchItems, defaultRecentItems] = this.props.defaultSearchItems!;
 
+    // Always clear searchTerm on showing default state.
+    this.setState({searchTerm: ''});
+
     if (!defaultSearchItems.length) {
       // Update searchTerm, otherwise <SearchDropdown> will have wrong state
       // (e.g. if you delete a query, the last letter will be highlighted if `searchTerm`
       // does not get updated)
-      this.setState({searchTerm: query});
 
       const [tagKeys, tagType] = this.getTagKeys('');
       const recentSearches = await this.getRecentSearches();
@@ -1223,8 +1190,6 @@ class SmartSearchBar extends Component<Props, State> {
 
       return;
     }
-    // cursor on whitespace show default "help" search terms
-    this.setState({searchTerm: ''});
 
     this.updateAutoCompleteState(
       defaultSearchItems,
@@ -1340,7 +1305,8 @@ class SmartSearchBar extends Component<Props, State> {
       tagName,
       type,
       maxSearchItems,
-      queryCharsLeft
+      queryCharsLeft,
+      true
     );
 
     this.setState(searchGroups);
@@ -1365,7 +1331,8 @@ class SmartSearchBar extends Component<Props, State> {
           tagName,
           type,
           maxSearchItems,
-          queryCharsLeft
+          queryCharsLeft,
+          false
         )
       )
       .reduce(

commit 96ac394d051769678869ebab3db18353c17f24f8

fix(search): Fix search not updating with latest tags after fetching (#36895)
Fixes a regression introduced in #35859 with derived supportedTags not updating when the props change after the TagStore fetches.

This PR makes supportedTags into a derived state computed on every prop change instead.

static/app/views/issueList/searchBar.tsx
@@ -51,6 +51,20 @@ const SEARCH_ITEMS: SearchItem[] = [
   },
 ];
 
+const getSupportedTags = (supportedTags: {[key: string]: Tag}) => {
+  const newTags = Object.fromEntries(
+    Object.keys(supportedTags).map(key => [
+      key,
+      {
+        ...supportedTags[key],
+        kind: supportedTags[key].predefined ? FieldValueKind.FIELD : FieldValueKind.TAG,
+      },
+    ])
+  );
+
+  return newTags;
+};
+
 type Props = React.ComponentProps<typeof SmartSearchBar> & {
   api: Client;
   onSidebarToggle: (e: React.MouseEvent) => void;
@@ -79,29 +93,11 @@ class IssueListSearchBar extends Component<Props, State> {
     supportedTags: {},
   };
 
-  componentDidMount() {
-    // Ideally, we would fetch on demand (e.g. when input gets focus)
-    // but `<SmartSearchBar>` is a bit complicated and this is the easiest route
-    this.getSupportedTags();
+  static getDerivedStateFromProps(props: Props) {
+    return {
+      supportedTags: getSupportedTags(props.supportedTags),
+    };
   }
-
-  getSupportedTags = () => {
-    const {supportedTags} = this.props;
-
-    const newTags = Object.fromEntries(
-      Object.keys(supportedTags).map(key => [
-        key,
-        {
-          ...supportedTags[key],
-          kind: supportedTags[key].predefined ? FieldValueKind.TAG : FieldValueKind.FIELD,
-        },
-      ])
-    );
-
-    this.setState({
-      supportedTags: newTags,
-    });
-  };
 
   /**
    * @returns array of tag values that substring match `query`