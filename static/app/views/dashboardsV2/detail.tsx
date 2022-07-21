43a76e05921eeef06f0260b06342d3151f83d066
Nar Saynorath <nar.saynorath@sentry.io>
2022-07-21 11:38:04 -0400

    fix(dashboard-filters): Only replace query params for page filters (#36881)
    In the dashboard response, filters is only applicable to the dashboard
    and is not injected into the URL. This field should not be part of the
    check or else if there are filters but no page filters, we'll
    continuously redirect

--- static/app/views/dashboardsV2/detail.tsx
+++ static/app/views/dashboardsV2/detail.tsx
@@ -68,7 +68,7 @@ import {
   Widget,
   WidgetType,
 } from './types';
-import {cloneDashboard, hasSavedFilters} from './utils';
+import {cloneDashboard, hasSavedPageFilters} from './utils';
 
 const UNSAVED_MESSAGE = t('You have unsaved changes, are you sure you want to leave?');
 
@@ -650,7 +650,7 @@ class DashboardDetail extends Component<Props, State> {
         }}
         skipLoadLastUsed={
           organization.features.includes('dashboards-top-level-filter') &&
-          hasSavedFilters(dashboard)
+          hasSavedPageFilters(dashboard)
         }
       >
         <PageContent>
@@ -743,7 +743,7 @@ class DashboardDetail extends Component<Props, State> {
           }}
           skipLoadLastUsed={
             organization.features.includes('dashboards-top-level-filter') &&
-            hasSavedFilters(dashboard)
+            hasSavedPageFilters(dashboard)
           }
         >
           <StyledPageContent>
