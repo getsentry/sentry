4cc77fca9418af5017683ba62d8a479a1c093b9a
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-07-22 10:28:29 +0200

    ref: Fix typing of indexer service, add string batch abstraction [INGEST-1380] (#36669)

--- src/sentry/release_health/release_monitor/metrics.py
+++ src/sentry/release_health/release_monitor/metrics.py
@@ -169,10 +169,10 @@ class MetricReleaseMonitorBackend(BaseReleaseMonitorBackend):
                             row[release_key], use_case_id=UseCaseKey.RELEASE_HEALTH
                         )
                         row_totals = totals[row["project_id"]].setdefault(
-                            env_name, {"total_sessions": 0, "releases": defaultdict(int)}
+                            env_name, {"total_sessions": 0, "releases": defaultdict(int)}  # type: ignore
                         )
                         row_totals["total_sessions"] += row["sessions"]
-                        row_totals["releases"][release_name] += row["sessions"]
+                        row_totals["releases"][release_name] += row["sessions"]  # type: ignore
 
                 if not more_results:
                     break
