b24a97c6fd484d5f3e2a90e26e3b7d0ec684460c
Joris Bayer <joris.bayer@sentry.io>
2022-07-22 15:38:55 +0200

    feat(projectconfig): Compress redis value (#36926)
    With transaction metrics and dynamic sampling config, the size of the
    project config values written to redis become a problem at scale.
    Applying zstandard compression should mitigate this problem.

--- tests/sentry/relay/test_projectconfig_cache.py
+++ tests/sentry/relay/test_projectconfig_cache.py
@@ -1,6 +1,9 @@
 from unittest import mock
 
+import pytest
+
 from sentry.relay.projectconfig_cache import redis
+from sentry.testutils.helpers import override_options
 
 
 def test_delete_count(monkeypatch):
@@ -14,3 +17,27 @@ def test_delete_count(monkeypatch):
     assert incr_mock.call_args == mock.call(
         "relay.projectconfig_cache.write", amount=1, tags={"action": "delete"}
     )
+
+
+@pytest.mark.django_db
+@pytest.mark.parametrize(
+    "enabled_dsns, sample_rate, should_use_compression",
+    [
+        (["fake-dsn-1", "fake-dsn-2"], 0.0, True),
+        (["fake-dsn-1", "fake-dsn-2"], 1.0, True),
+        ([], 1.0, True),
+        ([], 0.0, False),
+    ],
+)
+def test_read_write(enabled_dsns, sample_rate, should_use_compression):
+    cache = redis.RedisProjectConfigCache()
+    my_key = "fake-dsn-1"
+    with override_options(
+        {
+            "relay.project-config-cache-compress": enabled_dsns,
+            "relay.project-config-cache-compress-sample-rate": sample_rate,
+        }
+    ):
+        assert redis._use_compression(my_key) == should_use_compression
+        cache.set_many({my_key: "my-value"})
+        assert cache.get(my_key) == "my-value"
