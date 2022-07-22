6f42b31f4732fa96482652841ea1d07e64e59f38
Varun Venkatesh <varun.venkatesh@sentry.io>
2022-07-22 09:42:17 -0700

    Fix typing issue

--- src/sentry/integrations/msteams/card_builder/__init__.py
+++ src/sentry/integrations/msteams/card_builder/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from typing import Any, Mapping, Sequence, Tuple, Union
+from typing import Any, Mapping, Sequence, Union
 
 from sentry.integrations.metric_alerts import incident_attachment_info
 
