5444b000704f82dd4936ae1d9ce9a57c01ff83b2
Komal Saini <komalsaini@live.com>
2022-07-22 12:27:52 -0700

    pr changes

--- src/sentry/suspect_resolutions/resolved_in_active_release.py
+++ src/sentry/suspect_resolutions/resolved_in_active_release.py
@@ -5,26 +5,20 @@ from django.utils import timezone
 from sentry.models import Deploy, Group, GroupStatus, Project, Release
 
 
-def is_resolved_issue_within_active_release(issue_id: int, project: Project) -> bool:
-    # check that the issue is resolved and is part of a release
-
-    issue = Group.objects.filter(id=issue_id, project=project).first()
+def is_resolved_issue_within_active_release(issue: Group, project: Project) -> bool:
 
     if issue is None or issue.status != GroupStatus.RESOLVED or issue.get_last_release() is None:
         return False
 
-    # returns the release version associated with the issue
     latest_release_version_issue = issue.get_last_release()
 
-    # get release object from version
     latest_release_issue = Release.objects.filter(
-        version=latest_release_version_issue, project_id=project.id
+        version=latest_release_version_issue, organization_id=project.organization.id
     )
 
-    if len(latest_release_issue) != 0:
+    if len(latest_release_issue) == 0:
         return False
 
-    # get latest deploy of the release associated with the issue
     latest_deploy_release: Deploy = (
         Deploy.objects.filter(release_id=latest_release_issue.first().id)
         .order_by("-date_finished")
@@ -32,7 +26,6 @@ def is_resolved_issue_within_active_release(issue_id: int, project: Project) -> 
         or Deploy.objects.filter(id=latest_release_issue.first().last_deploy_id).first()
     )
 
-    # get the time of the latest deploy
     if not latest_deploy_release:
         return False
 
