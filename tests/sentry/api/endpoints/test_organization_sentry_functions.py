9cfbf523845a304d03a71bf72a1ea27861992bf5
Vignesh P <52717428+vpandiarajan20@users.noreply.github.com>
2022-07-25 11:36:18 -0700

    Sentry Functions: Adding Functionality to create Google Cloud Functions (#36865)
    * Feat(integrations): Initial GCP code commit
    
    * feat(integrations): generate upload url for files sentryFunctions
    
    * feat(integrations): creating cloud functions and updating requirements.txt
    
    * build(integrations): add cloud-functions package
    
    * pin back grpcio and freeze
    
    * build(python): update google-cloud-pubsub version
    
    * make freeze-requirements
    
    * feat(integrations): able to save GCP functions
    
    * feat(integration): attempting to use v2 cloud functions
    
    * feat(integrations): add additional google cloud functionality)
    
    * revert to old grpcio to prevent conflicts
    
    * added test for cloudfunctions
    
    Co-authored-by: Joshua Li <joshuarli98@gmail.com>
    Co-authored-by: anthony sottile <anthony.sottile@sentry.io>

--- tests/sentry/api/endpoints/test_organization_sentry_functions.py
+++ tests/sentry/api/endpoints/test_organization_sentry_functions.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from sentry.testutils import APITestCase
 from sentry.testutils.helpers import Feature
 
@@ -11,15 +13,23 @@ class OrganizationSentryFunctions(APITestCase):
         self.create_organization(owner=self.user, name="RowdyTiger")
         self.login_as(user=self.user)
 
-    def test_post_feature_true(self):
-        data = {"name": "foo", "author": "bar", "code": "baz", "overview": "qux"}
+    @patch("sentry.api.endpoints.organization_sentry_function.create_function")
+    def test_post_feature_true(self, mock_func):
+        defaultCode = "exports.yourFunction = (req, res) => {\n\tlet message = req.query.message || req.body.message || 'Hello World!';\n\tconsole.log('Query: ' + req.query);\n\tconsole.log('Body: ' + req.body);\n\tres.status(200).send(message);\n};"
+        data = {
+            "name": "foo",
+            "author": "bar",
+            "code": defaultCode,
+            "overview": "qux",
+        }
         with Feature("organizations:sentry-functions"):
             response = self.get_success_response(self.organization.slug, **data)
             assert response.status_code == 201
             assert response.data["name"] == "foo"
             assert response.data["author"] == "bar"
-            assert response.data["code"] == "baz"
+            assert response.data["code"] == defaultCode
             assert response.data["overview"] == "qux"
+            mock_func.assert_called_once_with(defaultCode, response.data["external_id"], "qux")
 
     def test_post_missing_params(self):
         data = {"name": "foo", "overview": "qux"}
