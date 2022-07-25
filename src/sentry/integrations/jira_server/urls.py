9f3832ebfc613ab4d1f0847a49f07a760d22c48e
Colleen O'Rourke <colleen@sentry.io>
2022-07-25 12:03:48 -0700

    jk I don't need that stuff

--- src/sentry/integrations/jira_server/urls.py
+++ src/sentry/integrations/jira_server/urls.py
@@ -1,24 +1,9 @@
 from django.conf.urls import url
 
-from sentry.integrations.jira.views import (
-    JiraExtensionConfigurationView,
-    JiraIssueHookView,
-    JiraUiHookView,
-)
-from sentry.integrations.jira.webhooks import (
-    JiraDescriptorEndpoint,
-    JiraInstalledEndpoint,
-    JiraUninstalledEndpoint,
-)
-
 from .search import JiraServerSearchEndpoint
 from .webhooks import JiraIssueUpdatedWebhook
 
 urlpatterns = [
-    url(r"^ui-hook/$", JiraUiHookView.as_view()),
-    url(r"^descriptor/$", JiraDescriptorEndpoint.as_view()),
-    url(r"^installed/$", JiraInstalledEndpoint.as_view(), name="sentry-extensions-jira-installed"),
-    url(r"^uninstalled/$", JiraUninstalledEndpoint.as_view()),
     url(
         r"^issue-updated/(?P<token>[^\/]+)/$",
         JiraIssueUpdatedWebhook.as_view(),
@@ -28,11 +13,5 @@ urlpatterns = [
         r"^search/(?P<organization_slug>[^\/]+)/(?P<integration_id>\d+)/$",
         JiraServerSearchEndpoint.as_view(),
         name="sentry-extensions-jiraserver-search",
-    ),
-    url(r"^configure/$", JiraExtensionConfigurationView.as_view()),
-    url(
-        r"^issue/(?P<issue_key>[^\/]+)/$",
-        JiraIssueHookView.as_view(),
-        name="sentry-extensions-jira-issue-hook",
     ),
 ]
