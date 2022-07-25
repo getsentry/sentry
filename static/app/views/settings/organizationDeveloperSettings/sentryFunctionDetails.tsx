52ce18bcb73ddba9aba20cafebc9752b31cf16e0
vpandiarajan20 <vpandiarajan20@gmail.com>
2022-07-25 15:48:32 -0700

    feat(integrations): fetch data and edit/delete capability

--- static/app/views/settings/organizationDeveloperSettings/sentryFunctionDetails.tsx
+++ static/app/views/settings/organizationDeveloperSettings/sentryFunctionDetails.tsx
@@ -1,4 +1,5 @@
 import React, {useRef} from 'react';
+import {browserHistory} from 'react-router';
 import Editor from '@monaco-editor/react';
 
 import {
@@ -8,12 +9,15 @@ import {
 } from 'sentry/actionCreators/indicator';
 import Feature from 'sentry/components/acl/feature';
 import AsyncComponent from 'sentry/components/asyncComponent';
+import Button from 'sentry/components/button';
 import Form from 'sentry/components/forms/form';
 import JsonForm from 'sentry/components/forms/jsonForm';
 import FormModel from 'sentry/components/forms/model';
 import {Field} from 'sentry/components/forms/type';
 import {Panel, PanelBody, PanelHeader} from 'sentry/components/panels';
 import {t} from 'sentry/locale';
+import {SentryFunction} from 'sentry/types';
+import useApi from 'sentry/utils/useApi';
 
 type Props = WrapperProps;
 const formFields: Field[] = [
@@ -42,10 +46,24 @@ const formFields: Field[] = [
 ];
 
 function SentryFunctionDetails(props: Props) {
+  const api = useApi();
   const form = useRef(new FormModel());
   const {orgId, functionSlug} = props.params;
+  const {sentryFunction} = props;
   const method = functionSlug ? 'PUT' : 'POST';
-  const endpoint = `/organizations/${orgId}/functions/`;
+  let endpoint = `/organizations/${orgId}/functions/`;
+  if (functionSlug) {
+    endpoint += `${functionSlug}/`;
+  }
+  const defaultCode = sentryFunction
+    ? sentryFunction.code
+    : `exports.yourFunction = (req, res) => {
+    let message = req.query.message || req.body.message || 'Hello World!';
+    console.log('Query: ' + req.query);
+    console.log('Body: ' + req.body);
+    res.status(200).send(message);
+};`;
+  const [code, setCode] = React.useState(defaultCode);
 
   const handleSubmitError = err => {
     let errorMessage = t('Unknown Error');
@@ -57,29 +75,46 @@ function SentryFunctionDetails(props: Props) {
 
   const handleSubmitSuccess = data => {
     addSuccessMessage(t('Sentry Function successfully saved.', data.name));
+    const baseUrl = `/settings/${orgId}/developer-settings/sentry-functions/`;
+    // TODO: should figure out where to redirect this
+    const url = `${baseUrl}${data.slug}/`;
+    if (sentryFunction) {
+      addSuccessMessage(t('%s successfully saved.', data.name));
+    } else {
+      addSuccessMessage(t('%s successfully created.', data.name));
+    }
+    browserHistory.push(url);
   };
 
   function handleEditorChange(value, _event) {
-    form.current.setValue('code', value);
+    setCode(value);
   }
 
-  const defaultCode = `exports.yourFunction = (req, res) => {
-    let message = req.query.message || req.body.message || 'Hello World!';
-    console.log('Query: ' + req.query);
-    console.log('Body: ' + req.body);
-    res.status(200).send(message);
-};`;
+  async function handleDelete() {
+    // figure out how to send a delete request
+    await api.requestPromise(endpoint, {
+      method: 'DELETE',
+    });
+    browserHistory.push(`/settings/${orgId}/developer-settings/`);
+  }
+
   return (
     <div>
       <Feature features={['organizations:sentry-functions']}>
         <h1>{t('Sentry Function Details')}</h1>
-        <h2>{props.params.orgId}</h2>
+        <h2>
+          {sentryFunction ? "Editing  '" + sentryFunction.name + "'" : 'New Function'}
+        </h2>
         <Form
           apiMethod={method}
           apiEndpoint={endpoint}
           model={form.current}
           onPreSubmit={() => {
+            form.current.setValue('code', code);
             addLoadingMessage(t('Saving changes..'));
+          }}
+          initialData={{
+            ...props.sentryFunction,
           }}
           onSubmitError={handleSubmitError}
           onSubmitSuccess={handleSubmitSuccess}
@@ -104,20 +139,43 @@ function SentryFunctionDetails(props: Props) {
             </PanelBody>
           </Panel>
         </Form>
+        {sentryFunction && (
+          <Button
+            onClick={handleDelete}
+            title={t('Delete Sentry Function')}
+            aria-label={t('Delete Sentry Function')}
+            type="button"
+            priority="danger"
+          >
+            Delete Sentry Function
+          </Button>
+        )}
       </Feature>
     </div>
   );
 }
 
-type WrapperState = {} & AsyncComponent['state'];
+type WrapperState = {
+  sentryFunction: SentryFunction;
+} & AsyncComponent['state'];
 
 type WrapperProps = {
   params: {orgId: string; functionSlug?: string};
+  sentryFunction?: SentryFunction;
 } & AsyncComponent['props'];
 
 class SentryFunctionsWrapper extends AsyncComponent<WrapperProps, WrapperState> {
+  getEndpoints(): ReturnType<AsyncComponent['getEndpoints']> {
+    const {functionSlug, orgId} = this.props.params;
+    if (functionSlug) {
+      return [['sentryFunction', `/organizations/${orgId}/functions/${functionSlug}/`]];
+    }
+    return [];
+  }
   renderBody() {
-    return <SentryFunctionDetails {...this.props} />;
+    return (
+      <SentryFunctionDetails sentryFunction={this.state.sentryFunction} {...this.props} />
+    );
   }
 }
 
