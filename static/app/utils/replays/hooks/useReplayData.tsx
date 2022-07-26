6175f9fd06a48363abf495bfa894e2454f078f36
Billy Vong <billyvg@users.noreply.github.com>
2022-07-26 13:39:48 -0400

    fix(replays): Fix uncompressed replays (#37028)
    Uncompressed replays were not being called with `mapRRWebAttachments`.

--- static/app/utils/replays/hooks/useReplayData.tsx
+++ static/app/utils/replays/hooks/useReplayData.tsx
@@ -162,7 +162,7 @@ function useReplayData({eventSlug, orgId}: Options): Result {
 
         // for non-compressed events, parse and return
         try {
-          return JSON.parse(response[0]) as ReplayAttachment;
+          return mapRRWebAttachments(JSON.parse(response[0]));
         } catch (error) {
           // swallow exception.. if we can't parse it, it's going to be compressed
         }
@@ -173,8 +173,7 @@ function useReplayData({eventSlug, orgId}: Options): Result {
           const responseBlob = await response[2]?.rawResponse.blob();
           const responseArray = (await responseBlob?.arrayBuffer()) as Uint8Array;
           const parsedPayload = JSON.parse(inflate(responseArray, {to: 'string'}));
-          const replayAttachments = mapRRWebAttachments(parsedPayload);
-          return replayAttachments;
+          return mapRRWebAttachments(parsedPayload);
         } catch (error) {
           return {};
         }
