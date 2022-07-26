011de13882b3767b532df9393c966b4e75099ecb
Lyn Nagara <lyn.nagara@gmail.com>
2022-07-26 08:29:28 -0700

    feat(post-process-forwarder): Support passing topic as CLI arg (#36983)
    We are going to split the transactions topic from the events topic so errors
    and transactions have independent infrastructure. We will do a gradual transition
    where a subset of projects will be migrated over first. In order to support this
    we will have to temporarily run two post process forwarders (one for transactions on
    the old topic and one for the new one). For this we need to be able to pass the topic to
    consume from via CLI rather than just using the default one.

--- tests/sentry/eventstream/kafka/test_consumer.py
+++ tests/sentry/eventstream/kafka/test_consumer.py
@@ -682,6 +682,7 @@ class BatchedConsumerTest(TestCase):
         consumer = eventstream._build_consumer(
             entity="all",
             consumer_group=consumer_group,
+            topic=None,
             commit_log_topic=self.commit_log_topic,
             synchronize_commit_group=synchronize_commit_group,
             commit_batch_size=1,
