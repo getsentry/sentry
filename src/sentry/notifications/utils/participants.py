1363454fd582f125278eadc24a10ac2e23a455ff
Gilbert Szeto <gilbert.szeto@sentry.io>
2022-07-26 16:16:06 -0700

    parameterize notification_type to be passed down for checking NotificationSettingTypes add more email tests add slack tests

--- src/sentry/notifications/utils/participants.py
+++ src/sentry/notifications/utils/participants.py
@@ -296,9 +296,10 @@ def get_send_to(
     target_type: ActionTargetType,
     target_identifier: int | None = None,
     event: Event | None = None,
+    notification_type: NotificationSettingTypes = NotificationSettingTypes.ISSUE_ALERTS,
 ) -> Mapping[ExternalProviders, set[Team | User]]:
     recipients = determine_eligible_recipients(project, target_type, target_identifier, event)
-    return get_recipients_by_provider(project, recipients)
+    return get_recipients_by_provider(project, recipients, notification_type)
 
 
 def get_user_from_identifier(project: Project, target_identifier: str | int | None) -> User | None:
@@ -373,13 +374,17 @@ def combine_recipients_by_provider(
 
 
 def get_recipients_by_provider(
-    project: Project, recipients: Iterable[Team | User]
+    project: Project,
+    recipients: Iterable[Team | User],
+    notification_type: NotificationSettingTypes = NotificationSettingTypes.ISSUE_ALERTS,
 ) -> Mapping[ExternalProviders, set[Team | User]]:
     """Get the lists of recipients that should receive an Issue Alert by ExternalProvider."""
     teams, users = partition_recipients(recipients)
 
     # First evaluate the teams.
-    teams_by_provider = NotificationSetting.objects.filter_to_accepting_recipients(project, teams)
+    teams_by_provider = NotificationSetting.objects.filter_to_accepting_recipients(
+        project, teams, notification_type
+    )
 
     # Teams cannot receive emails so omit EMAIL settings.
     teams_by_provider = {
@@ -392,6 +397,8 @@ def get_recipients_by_provider(
     users = set(users).union(get_users_from_team_fall_back(teams, teams_by_provider))
 
     # Repeat for users.
-    users_by_provider = NotificationSetting.objects.filter_to_accepting_recipients(project, users)
+    users_by_provider = NotificationSetting.objects.filter_to_accepting_recipients(
+        project, users, notification_type
+    )
 
     return combine_recipients_by_provider(teams_by_provider, users_by_provider)
