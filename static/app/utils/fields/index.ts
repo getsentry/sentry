5ab0b01e9450a87962f90784249f761148082e71
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2022-07-26 15:25:05 -0700

    Update static/app/utils/fields/index.ts
    Co-authored-by: Evan Purkhiser <evanpurkhiser@gmail.com>

--- static/app/utils/fields/index.ts
+++ static/app/utils/fields/index.ts
@@ -233,7 +233,7 @@ export const MEASUREMENT_FIELDS: Record<string, FieldDefinition> = {
   },
 };
 
-export const FIELDS: Record<string, FieldDefinition> = {
+export const FIELDS: Record<FieldKey, FieldDefinition> = {
   ...MEASUREMENT_FIELDS,
   [FieldKey.AGE]: {
     desc: t('The age of the issue in relative time'),
