debe4d44e445da1751489249058ebb2e68dfffff
Danny Lee <dlee@sentry.io>
2022-07-26 16:46:31 -0700

    10 - Address PR comments

--- src/sentry/api/endpoints/team_members.py
+++ src/sentry/api/endpoints/team_members.py
@@ -1,4 +1,4 @@
-from django.db.models import Q
+from django.db.models import Q, prefetch_related_objects
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -15,17 +15,15 @@ class DetailedOrganizationMemberTeamSerializer(Serializer):
         super().__init__(*args, **kwargs)
 
     def get_attrs(self, item_list, user, **kwargs):
+        prefetch_related_objects(item_list, "organizationmember")
+        prefetch_related_objects(item_list, "organizationmember__user")
         attrs = {}
         for org_member_team in item_list:
-            attrs[org_member_team] = {
-                "org_member": org_member_team.organizationmember,
-                "user": org_member_team.organizationmember.user,
-            }
+            attrs[org_member_team] = {"org_member": org_member_team.organizationmember}
         return attrs
 
     def serialize(self, obj, attrs, user):
         org_member = serialize(attrs["org_member"])
-        org_member["user"] = serialize(attrs["user"])
         org_member["teamRole"] = obj.role
         org_member["teamSlug"] = self.team.slug
         return org_member
@@ -33,16 +31,11 @@ class DetailedOrganizationMemberTeamSerializer(Serializer):
 
 class TeamMembersEndpoint(TeamEndpoint):
     def get(self, request: Request, team) -> Response:
-        queryset = (
-            OrganizationMemberTeam.objects.filter(
-                Q(organizationmember__user__is_active=True)
-                | Q(organizationmember__user__isnull=True),
-                organizationmember__organization=team.organization,
-                organizationmember__invite_status=InviteStatus.APPROVED.value,
-                team=team,
-            )
-            .prefetch_related("organizationmember")
-            .prefetch_related("organizationmember__user")
+        queryset = OrganizationMemberTeam.objects.filter(
+            Q(organizationmember__user__is_active=True) | Q(organizationmember__user__isnull=True),
+            organizationmember__organization=team.organization,
+            organizationmember__invite_status=InviteStatus.APPROVED.value,
+            team=team,
         )
 
         serializer = DetailedOrganizationMemberTeamSerializer(team=team)
