29b6e3f48b6463abd6a2b178dfe169fd580ed145
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-07-26 17:05:48 +0200

    ref(sampling): Attempt to fix e2e flaky test

--- tests/acceptance/test_project_settings_sampling.py
+++ tests/acceptance/test_project_settings_sampling.py
@@ -1,4 +1,3 @@
-import pytest
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.keys import Keys
 
@@ -16,7 +15,7 @@ uniform_rule_with_recommended_sampling_values = {
         "op": "and",
         "inner": [],
     },
-    "sampleRate": 1,
+    "sampleRate": 0.1,
 }
 
 uniform_rule_with_custom_sampling_values = {
@@ -78,13 +77,16 @@ class ProjectSettingsSamplingTest(AcceptanceTestCase):
         self.browser.get(self.path)
         self.browser.wait_until_not('[data-test-id="loading-indicator"]')
 
-    @pytest.mark.skip(reason="Flaky")
     def test_add_uniform_rule_with_recommended_sampling_values(self):
         with self.feature(FEATURE_NAME):
             self.wait_until_page_loaded()
 
             # Open uniform rate modal
             self.browser.element('[aria-label="Start Setup"]').click()
+
+            # Wait for modal to load
+            self.browser.wait_until('[role="dialog"]')
+            self.browser.wait_until_not('[data-test-id="loading-indicator"]')
 
             # Click on the recommended sampling values option
             self.browser.element('[id="sampling-recommended"]').click()
@@ -109,13 +111,16 @@ class ProjectSettingsSamplingTest(AcceptanceTestCase):
                 == serializer.validated_data["rules"][0]
             )
 
-    @pytest.mark.skip(reason="Flaking pretty consistently")
     def test_add_uniform_rule_with_custom_sampling_values(self):
         with self.feature(FEATURE_NAME):
             self.wait_until_page_loaded()
 
             # Open uniform rate modal
             self.browser.element('[aria-label="Start Setup"]').click()
+
+            # Wait for modal to load
+            self.browser.wait_until('[role="dialog"]')
+            self.browser.wait_until_not('[data-test-id="loading-indicator"]')
 
             # Enter a custom value for client side sampling
             self.browser.element('[id="recommended-client-sampling"]').clear()
