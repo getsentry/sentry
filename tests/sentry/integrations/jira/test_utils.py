2fbf550ec05c8501cbc9eca62e73526e717dcbdf
Colleen O'Rourke <colleen@sentry.io>
2022-07-26 11:52:46 -0700

    ref(Jira): Split Jira Cloud and Jira Server (#37034)
    * Split Jira Cloud and Jira Server

--- tests/sentry/integrations/jira/test_utils.py
+++ tests/sentry/integrations/jira/test_utils.py
@@ -4,20 +4,13 @@ from sentry.testutils import TestCase
 
 
 class BuildUserChoiceTest(TestCase):
-    def test_jira_server(self):
-        user_response = StubService.get_stub_data("jira", "jira_server_user.json")
-        assert build_user_choice(user_response, user_id_field="name") == (
-            "bob",
-            "Bobby - bob@example.org (bob)",
-        )
-
     def test_jira_cloud(self):
         user_response = StubService.get_stub_data("jira", "user.json")
-        assert build_user_choice(user_response, user_id_field="accountId") == (
+        assert build_user_choice(user_response, "accountId") == (
             "012345:00000000-1111-2222-3333-444444444444",
             "Saif Hakim",
         )
 
     def test_unexpected_id(self):
         user_response = StubService.get_stub_data("jira", "user.json")
-        assert build_user_choice(user_response, user_id_field="name") is None
+        assert build_user_choice(user_response, "name") is None
