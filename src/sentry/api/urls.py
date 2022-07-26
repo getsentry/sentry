04d9c6ad5cfb706de681f588375635fe4f89b8cc
Colton Allen <colton.allen@sentry.io>
2022-07-26 15:40:32 -0500

    Add replay recording segments endpoints

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -63,7 +63,12 @@ from sentry.incidents.endpoints.project_alert_rule_task_details import (
 )
 from sentry.replays.endpoints.organization_replay_index import OrganizationReplayIndexEndpoint
 from sentry.replays.endpoints.project_replay_details import ProjectReplayDetailsEndpoint
-from sentry.replays.endpoints.project_replay_recordings import ProjectReplayRecordingsEndpoint
+from sentry.replays.endpoints.project_replay_recording_segment_details import (
+    ProjectReplayRecordingSegmentDetailsEndpoint,
+)
+from sentry.replays.endpoints.project_replay_recording_segment_index import (
+    ProjectReplayRecordingSegmentIndexEndpoint,
+)
 from sentry.rules.history.endpoints.project_rule_group_history import (
     ProjectRuleGroupHistoryIndexEndpoint,
 )
@@ -2047,9 +2052,14 @@ urlpatterns = [
                     name="sentry-api-0-project-replay-details",
                 ),
                 url(
-                    r"^(?P<organization_slug>[^/]+)/(?P<project_slug>[^\/]+)/replays/(?P<replay_id>[\w-]+)/recordings/$",
-                    ProjectReplayRecordingsEndpoint.as_view(),
-                    name="sentry-api-0-project-replay-recordings",
+                    r"^(?P<organization_slug>[^/]+)/(?P<project_slug>[^\/]+)/replays/(?P<replay_id>[\w-]+)/recording-segments/$",
+                    ProjectReplayRecordingSegmentIndexEndpoint.as_view(),
+                    name="sentry-api-0-project-replay-recording-segment-index",
+                ),
+                url(
+                    r"^(?P<organization_slug>[^/]+)/(?P<project_slug>[^\/]+)/replays/(?P<replay_id>[\w-]+)/recording-segments/(?P<segment_id>\d+)/$",
+                    ProjectReplayRecordingSegmentDetailsEndpoint.as_view(),
+                    name="sentry-api-0-project-replay-recording-segment-details",
                 ),
                 url(
                     r"^(?P<organization_slug>[^\/]+)/(?P<project_slug>[^\/]+)/rules/configuration/$",
---
+++ tests/sentry/replays/test_project_replay_recording_segment_index.py
@@ -0,0 +1,55 @@
+from django.urls import reverse
+
+from sentry.models import File
+from sentry.replays.models import ReplayRecordingSegment
+from sentry.testutils import APITestCase
+
+
+class ProjectReplayRecordingSegmentTestCase(APITestCase):
+    endpoint = "sentry-api-0-project-replay-recording-segment-index"
+
+    def setUp(self):
+        super().setUp()
+
+        self.replay_id = "6f959c5c-bc77-4683-8723-6e3367b0cfac"
+        self.url = reverse(
+            self.endpoint,
+            args=(self.organization.slug, self.project.slug, self.replay_id),
+        )
+
+    def test_index(self):
+        self.login_as(user=self.user)
+
+        recording_segment = ReplayRecordingSegment.objects.create(
+            replay_id=self.replay_id.replace("-", ""),
+            project_id=self.project.id,
+            sequence_id=0,
+            file_id=File.objects.create(name="hello.png", type="image/png").id,
+        )
+        ReplayRecordingSegment.objects.create(
+            replay_id=self.replay_id.replace("-", ""),
+            project_id=self.project.id,
+            sequence_id=1,
+            file_id=File.objects.create(name="hello.png", type="image/png").id,
+        )
+        ReplayRecordingSegment.objects.create(
+            replay_id=self.replay_id.replace("-", ""),
+            project_id=self.project.id,
+            sequence_id=2,
+            file_id=File.objects.create(name="hello.png", type="image/png").id,
+        )
+
+        with self.feature("organizations:session-replay"):
+            response = self.client.get(self.url)
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 3
+        assert response.data[0]["id"] == str(recording_segment.id)
+        assert response.data[0]["replay_id"] == recording_segment.replay_id
+        assert response.data[0]["sequence_id"] == recording_segment.sequence_id
+        assert response.data[0]["project_id"] == recording_segment.project_id
+        assert response.data[0]["date_added"] == recording_segment.date_added
+
+        assert response.data[0]["sequence_id"] == 0
+        assert response.data[1]["sequence_id"] == 1
+        assert response.data[2]["sequence_id"] == 2
