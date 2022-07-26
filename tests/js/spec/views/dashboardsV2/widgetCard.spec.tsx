91bd16fafb654101b5d209f368b8cae021937362
Edward Gou <edward.gou@sentry.io>
2022-07-26 16:53:59 -0400

    test

--- tests/js/spec/views/dashboardsV2/widgetCard.spec.tsx
+++ tests/js/spec/views/dashboardsV2/widgetCard.spec.tsx
@@ -4,6 +4,7 @@ import {render, screen, userEvent, waitFor} from 'sentry-test/reactTestingLibrar
 
 import * as modal from 'sentry/actionCreators/modal';
 import {Client} from 'sentry/api';
+import * as LineChart from 'sentry/components/charts/lineChart';
 import SimpleTableChart from 'sentry/components/charts/simpleTableChart';
 import {DisplayType, Widget, WidgetType} from 'sentry/views/dashboardsV2/types';
 import WidgetCard from 'sentry/views/dashboardsV2/widgetCard';
@@ -820,6 +821,77 @@ describe('Dashboards > WidgetCard', function () {
           screen.getByText(/we've automatically adjusted your results/i)
         ).toBeInTheDocument();
       });
+    });
+
+    it('renders chart using axis and tooltip formatters from custom measurement meta', async function () {
+      const spy = jest.spyOn(LineChart, 'LineChart');
+      const eventsStatsMock = MockApiClient.addMockResponse({
+        url: '/organizations/org-slug/events-stats/',
+        body: {
+          data: [
+            [
+              1658262600,
+              [
+                {
+                  count: 24,
+                },
+              ],
+            ],
+          ],
+          meta: {
+            fields: {
+              time: 'date',
+              p95_measurements_custom: 'duration',
+            },
+            units: {
+              time: null,
+              p95_measurements_custom: 'minute',
+            },
+            isMetricsData: true,
+            tips: {},
+          },
+        },
+      });
+
+      render(
+        <WidgetCard
+          api={api}
+          organization={organization}
+          widget={{
+            title: '',
+            interval: '5m',
+            widgetType: WidgetType.DISCOVER,
+            displayType: DisplayType.LINE,
+            queries: [
+              {
+                conditions: '',
+                name: '',
+                fields: [],
+                columns: [],
+                aggregates: ['p95(measurements.custom)'],
+                orderby: '',
+              },
+            ],
+          }}
+          selection={selection}
+          isEditing={false}
+          onDelete={() => undefined}
+          onEdit={() => undefined}
+          onDuplicate={() => undefined}
+          renderErrorMessage={() => undefined}
+          isSorting={false}
+          currentWidgetDragging={false}
+          showContextMenu
+          widgetLimitReached={false}
+        />,
+        {context: routerContext}
+      );
+      await waitFor(function () {
+        expect(eventsStatsMock).toHaveBeenCalled();
+      });
+      const {tooltip, yAxis} = spy.mock.calls.pop()[0];
+      expect(tooltip.valueFormatter(24, 'duration')).toEqual('24.00ms');
+      expect(yAxis.axisLabel.formatter(24, 'duration')).toEqual('24ms');
     });
   });
 });
