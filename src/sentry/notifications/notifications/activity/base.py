687c6f42ae010344fe71f9f85eec2c0b8c95e6db
Varun Venkatesh <varun.venkatesh@sentry.io>
2022-07-26 12:48:03 -0700

    ref(notifications): Get title link based on provider

--- src/sentry/notifications/notifications/activity/base.py
+++ src/sentry/notifications/notifications/activity/base.py
@@ -183,9 +183,9 @@ class GroupActivityNotification(ActivityNotification, abc.ABC):
         return mark_safe(description.format(**context))
 
     def get_title_link(self, recipient: Team | User) -> str | None:
-        from sentry.integrations.slack.message_builder.issues import get_title_link
+        from sentry.integrations.message_builder import get_title_link
 
-        return get_title_link(self.group, None, False, True, self)
+        return get_title_link(self.group, None, False, True, self, self.provider)
 
     def build_attachment_title(self, recipient: Team | User) -> str:
         from sentry.integrations.message_builder import build_attachment_title
