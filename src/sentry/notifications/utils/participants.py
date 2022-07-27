f4856ad0479d272470f217616787becc1ebb7516
Gilbert Szeto <gilbert.szeto@sentry.io>
2022-07-27 16:08:57 -0700

    NotificationSettingTypes.ACTIVE_RELEASE should be project-level setting

--- src/sentry/notifications/utils/participants.py
+++ src/sentry/notifications/utils/participants.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import logging
 from collections import defaultdict
-from typing import TYPE_CHECKING, Any, Iterable, Mapping, MutableMapping, Sequence
+from typing import TYPE_CHECKING, Any, Iterable, Mapping, MutableMapping, Sequence, Union
 
 from sentry import features
 from sentry.models import (
@@ -20,6 +20,7 @@ from sentry.models import (
     UserOption,
 )
 from sentry.notifications.helpers import (
+    get_scope_type,
     get_settings_by_provider,
     get_values_by_provider_by_type,
     transform_to_notification_settings_by_recipient,
@@ -94,15 +95,35 @@ def get_participants_for_release(
     # Collect all users with verified emails on a team in the related projects.
     users = set(User.objects.get_team_members_with_verified_email_for_projects(projects))
 
+    parent_specific_scope_type = get_scope_type(notification_setting_type)
+
+    def parent_notification_settings_by_recipient(
+        note_setting_type: NotificationSettingTypes,
+        parent: Iterable[Union[Project, Organization]],
+        recipients: Iterable[Team | User],
+    ):
+        by_recipient = {}
+        for project_or_org in parent:
+            notification_settings = NotificationSetting.objects.get_for_recipient_by_parent(
+                note_setting_type,
+                recipients=recipients,
+                parent=project_or_org,
+            )
+            by_recipient.update(
+                transform_to_notification_settings_by_recipient(notification_settings, recipients)
+            )
+
+        return by_recipient
+
     # Get all the involved users' settings for deploy-emails (including
     # users' organization-independent settings.)
-    notification_settings = NotificationSetting.objects.get_for_recipient_by_parent(
-        notification_setting_type,
-        recipients=users,
-        parent=organization,
-    )
-    notification_settings_by_recipient = transform_to_notification_settings_by_recipient(
-        notification_settings, users
+
+    notification_settings_by_recipient = (
+        parent_notification_settings_by_recipient(notification_setting_type, projects, users)
+        if parent_specific_scope_type == NotificationScopeType.PROJECT
+        else parent_notification_settings_by_recipient(
+            notification_setting_type, [organization], users
+        )
     )
 
     # Map users to their setting value. Prioritize user/org specific, then
--- tests/sentry/mail/activity/test_release_summary.py
+++ tests/sentry/mail/activity/test_release_summary.py
@@ -57,6 +57,8 @@ class ReleaseSummaryTestCase(ActivityTestCase):
                 )
             )
 
+        assert NotificationSetting.objects.all().count() == 0
+
         # opt-in to getting active_release notifications
         NotificationSetting.objects.update_settings(
             ExternalProviders.EMAIL,
@@ -65,12 +67,20 @@ class ReleaseSummaryTestCase(ActivityTestCase):
             user=self.user1,
             project=self.project,
         )
+        assert (
+            NotificationSetting.objects.all()[0].type
+            == NotificationSettingTypes.ACTIVE_RELEASE.value
+        )
+
+        recipient_by_parent = NotificationSetting.objects.get_for_recipient_by_parent(
+            NotificationSettingTypes.ACTIVE_RELEASE, self.project, [self.user1, self.user2]
+        )
+        assert len(recipient_by_parent) == 1
+
         # user1 is included because they committed
-        participants = release_summary.get_participants_with_group_subscription_reason()[
-            ExternalProviders.EMAIL
-        ]
-        assert len(participants) == 1
-        assert participants == {
+        participants = release_summary.get_participants_with_group_subscription_reason()
+        assert len(participants[ExternalProviders.EMAIL]) == 1
+        assert participants[ExternalProviders.EMAIL] == {
             self.user1: GroupSubscriptionReason.committed,
         }
 
