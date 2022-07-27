7d887a43d3e3fbd31040aacc4980f58a3dff4198
Malachi Willey <malwilley@gmail.com>
2022-07-27 09:38:41 -0700

    Remove unncecessary organization provider

--- tests/js/spec/views/projectDetail/projectFilters.spec.jsx
+++ tests/js/spec/views/projectDetail/projectFilters.spec.jsx
@@ -1,6 +1,5 @@
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
-import {OrganizationContext} from 'sentry/views/organizationContext';
 import ProjectFilters from 'sentry/views/projectDetail/projectFilters';
 
 describe('ProjectDetail > ProjectFilters', () => {
@@ -12,7 +11,6 @@ describe('ProjectDetail > ProjectFilters', () => {
   });
 
   it('recommends semver search tag', async () => {
-    const organization = TestStubs.Organization();
     tagValueLoader.mockResolvedValue([
       {
         count: null,
@@ -24,10 +22,8 @@ describe('ProjectDetail > ProjectFilters', () => {
       },
     ]);
     render(
-      <OrganizationContext.Provider value={organization}>
-        <ProjectFilters query="" onSearch={onSearch} tagValueLoader={tagValueLoader} />
-      </OrganizationContext.Provider>,
-      {context: TestStubs.routerContext(), organization}
+      <ProjectFilters query="" onSearch={onSearch} tagValueLoader={tagValueLoader} />,
+      {context: TestStubs.routerContext(), organization: TestStubs.Organization()}
     );
 
     userEvent.click(screen.getByRole('textbox'));
