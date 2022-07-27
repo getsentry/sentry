c5535156242ad5840041483bfe8a41c640c9c634
Nar Saynorath <nar.saynorath@sentry.io>
2022-07-27 09:49:50 -0400

    fix(dashboard-filters): Unselecting all filters causes loading (#37085)
    We only want to redirect at the beginning of the render. Deselecting
    everything in page filters would remove values from the query params and
    trigger a loading state. Instead we should only block rendering if we're
    loading and meet those redirect conditions.

--- static/app/views/dashboardsV2/orgDashboards.tsx
+++ static/app/views/dashboardsV2/orgDashboards.tsx
@@ -188,11 +188,11 @@ class OrgDashboards extends AsyncComponent<Props, State> {
     }
 
     if (
-      loading ||
-      (organization.features.includes('dashboards-top-level-filter') &&
-        selectedDashboard &&
-        hasSavedPageFilters(selectedDashboard) &&
-        isEmpty(location.query))
+      loading &&
+      organization.features.includes('dashboards-top-level-filter') &&
+      selectedDashboard &&
+      hasSavedPageFilters(selectedDashboard) &&
+      isEmpty(location.query)
     ) {
       // Block dashboard from rendering if the dashboard has filters and
       // the URL does not contain filters yet. The filters can either match the
