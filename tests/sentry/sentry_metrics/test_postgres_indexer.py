c761184e930ce015fc6e79949b7c24c23f6e8dd4
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2022-07-28 11:48:19 +0200

    Revert "remove integration into postgres"
    This reverts commit 567b21e670907080c215d0eccb045ffcff057a72.

--- tests/sentry/sentry_metrics/test_postgres_indexer.py
+++ tests/sentry/sentry_metrics/test_postgres_indexer.py
@@ -20,6 +20,7 @@ from sentry.sentry_metrics.indexer.postgres_v2 import (
 )
 from sentry.sentry_metrics.indexer.strings import SHARED_STRINGS
 from sentry.testutils.cases import TestCase
+from sentry.testutils.helpers.options import override_options
 from sentry.utils.cache import cache
 
 
@@ -275,6 +276,70 @@ class PostgresIndexerV2Test(TestCase):
 
         assert indexer_cache.get(string.id, self.cache_namespace) is None
         assert indexer_cache.get(key, self.cache_namespace) is None
+
+    def test_rate_limited(self):
+        """
+        Assert that rate limits per-org and globally are applied at all.
+
+        Since we don't have control over ordering in sets/dicts, we have no
+        control over which string gets rate-limited. That makes assertions
+        quite awkward and imprecise.
+        """
+        org_strings = {1: {"a", "b", "c"}, 2: {"e", "f"}, 3: {"g"}}
+
+        with override_options(
+            {
+                "sentry-metrics.writes-limiter.limits.releasehealth.per-org": [
+                    {"window_seconds": 10, "granularity_seconds": 10, "limit": 1}
+                ],
+            }
+        ):
+            results = self.indexer.bulk_record(
+                use_case_id=self.use_case_id, org_strings=org_strings
+            )
+
+        assert len(results[1]) == 3
+        assert len(results[2]) == 2
+        assert len(results[3]) == 1
+        assert results[3]["g"] is not None
+
+        rate_limited_strings = set()
+
+        for org_id in 1, 2, 3:
+            for k, v in results[org_id].items():
+                if v is None:
+                    rate_limited_strings.add((org_id, k))
+
+        assert len(rate_limited_strings) == 3
+        assert (3, "g") not in rate_limited_strings
+
+        for org_id, string in rate_limited_strings:
+            assert results.get_fetch_metadata()[org_id][string] == Metadata(
+                id=None,
+                fetch_type=FetchType.RATE_LIMITED,
+                fetch_type_ext=FetchTypeExt(is_global=False),
+            )
+
+        org_strings = {1: rate_limited_strings}
+
+        with override_options(
+            {
+                "sentry-metrics.writes-limiter.limits.releasehealth.global": [
+                    {"window_seconds": 10, "granularity_seconds": 10, "limit": 2}
+                ],
+            }
+        ):
+            results = self.indexer.bulk_record(
+                use_case_id=self.use_case_id, org_strings=org_strings
+            )
+
+        rate_limited_strings2 = set()
+        for k, v in results[1].items():
+            if v is None:
+                rate_limited_strings2.add(k)
+
+        assert len(rate_limited_strings2) == 1
+        assert len(rate_limited_strings - rate_limited_strings2) == 2
 
 
 class KeyCollectionTest(TestCase):
