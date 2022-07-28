3ea393a9378291e8c527a79a587c6cb284ce06c0
Alberto Leal <mail4alberto@gmail.com>
2022-07-27 23:43:36 -0700

    feat(hybrid-cloud): Add regionUrl to organizationSummary

--- src/sentry/api/serializers/models/organization.py
+++ src/sentry/api/serializers/models/organization.py
@@ -19,7 +19,7 @@ from sentry.api.serializers.models.role import (
     TeamRoleSerializer,
 )
 from sentry.api.serializers.models.team import TeamSerializerResponse
-from sentry.api.utils import generate_organization_url
+from sentry.api.utils import generate_organization_url, generate_region_url
 from sentry.app import quotas
 from sentry.auth.access import Access
 from sentry.constants import (
@@ -151,6 +151,7 @@ class OrganizationSerializerResponse(TypedDict):
     dateCreated: datetime
     isEarlyAdopter: bool
     organizationUrl: str
+    regionUrl: str
     require2FA: bool
     requireEmailVerification: bool
     avatar: Any  # TODO replace with Avatar
@@ -246,6 +247,7 @@ class OrganizationSerializer(Serializer):  # type: ignore
             "dateCreated": obj.date_added,
             "isEarlyAdopter": bool(obj.flags.early_adopter),
             "organizationUrl": generate_organization_url(obj.slug),
+            "regionUrl": generate_region_url(),
             "require2FA": bool(obj.flags.require_2fa),
             "requireEmailVerification": bool(
                 features.has("organizations:required-email-verification", obj)
