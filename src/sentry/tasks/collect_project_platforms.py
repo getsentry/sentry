5ee2c8bf507644a2536e7bc866a3584b17afa50c
Richard Ma <maxiuyuan1998@gmail.com>
2022-07-29 14:32:41 -0700

    break collect_project_platforms into 2 parts

--- src/sentry/tasks/collect_project_platforms.py
+++ src/sentry/tasks/collect_project_platforms.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-from django.db.models import Max
+from django.db.models import Max, Min
 from django.utils import timezone
 
 from sentry.constants import VALID_PLATFORMS
@@ -8,13 +8,7 @@ from sentry.models import Group, Project, ProjectPlatform
 from sentry.tasks.base import instrumented_task
 
 
-@instrumented_task(name="sentry.tasks.collect_project_platforms", queue="stats")
-def collect_project_platforms(**kwargs):
-    now = timezone.now()
-
-    min_project_id = 0
-    max_project_id = Project.objects.using_replica().aggregate(x=Max("id"))["x"] or 0
-    step = 1000
+def _collect_project_platforms(min_project_id, max_project_id, now, step):
     while min_project_id <= max_project_id:
         queryset = (
             Group.objects.using_replica()
@@ -36,6 +30,33 @@ def collect_project_platforms(**kwargs):
                 project_id=project_id, platform=platform, values={"last_seen": now}
             )
         min_project_id += step
+
+
+@instrumented_task(name="sentry.tasks.collect_project_platforms", queue="stats")
+def collect_project_platforms(**kwargs):
+    now = timezone.now()
+
+    pre_snowflake_max_value = 2147483647
+
+    min_project_id = 0
+    max_project_id_before_snowflake = Project.objects.filter(
+        id__lt=pre_snowflake_max_value
+    ).aggregate(x=Max("id"))["x"]
+    step = 1000
+
+    _collect_project_platforms(min_project_id, max_project_id_before_snowflake, now, step)
+
+    min_project_id_after_snowflake = Project.objects.filter(
+        id__gt=pre_snowflake_max_value
+    ).aggregate(x=Min("id"))["x"]
+    max_project_id_after_snowflake = Project.objects.filter(
+        id__gt=pre_snowflake_max_value
+    ).aggregate(x=Max("id"))["x"]
+    step = max((max_project_id_after_snowflake - min_project_id_after_snowflake) / 1000, 1)
+
+    _collect_project_platforms(
+        min_project_id_after_snowflake, max_project_id_after_snowflake, now, step
+    )
 
     # remove (likely) unused platform associations
     ProjectPlatform.objects.filter(last_seen__lte=now - timedelta(days=90)).delete()
