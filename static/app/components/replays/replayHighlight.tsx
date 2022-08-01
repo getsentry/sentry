a336f5e599daf44af2ec0ad44814a3fdc0b9a63f
Jesus Padron <jesuspadron4497@gmail.com>
2022-08-01 17:09:01 -0400

    feat(replays): new replays table column (#37305)
    Added a new column for the replays table at the index page, showing an icon of interest.
    
    The way we score each replay doesn't really matter at this point. We're using some static/mock data, and we don't really have good examples other than sourcemaps.io to draw from. Expect a better 'interest algo' in a future interation.
    
    Closes #37070

---
+++ static/app/components/replays/replayHighlight.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import ScoreBar from 'sentry/components/scoreBar';
+import CHART_PALETTE from 'sentry/constants/chartPalette';
+import {ReplayDurationAndErrors} from 'sentry/views/replays/types';
+
+interface Props {
+  data: ReplayDurationAndErrors | undefined;
+}
+
+function replayHighlight({data}: Props) {
+  let score = 1;
+
+  if (data) {
+    // Mocked data 👇 - this will change with the new backend
+    const pagesVisited = 1;
+    const {count_if_event_type_equals_error: errors, 'equation[0]': durationInSeconds} =
+      data;
+
+    const pagesVisitedOverTime = pagesVisited / (durationInSeconds || 1);
+
+    score = (errors * 25 + pagesVisited * 5 + pagesVisitedOverTime) / 10;
+    score = Math.floor(Math.min(10, Math.max(1, score)));
+  }
+
+  const palette = new Array(10).fill([CHART_PALETTE[0][0]]);
+  return <ScoreBar size={20} score={score} palette={palette} radius={0} />;
+}
+
+export default replayHighlight;
