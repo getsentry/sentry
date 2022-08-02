32bb9b071894bd560a43151cf3249ae0779e1428
Andrii Soldatenko <andrii.soldatenko@sentry.io>
2022-08-02 13:34:37 +0200

    fix: after code review

--- src/sentry/api/endpoints/organization_dynamic_sampling_sdk_versions.py
+++ src/sentry/api/endpoints/organization_dynamic_sampling_sdk_versions.py
@@ -68,7 +68,7 @@ class OrganizationDynamicSamplingSDKVersionsEndpoint(OrganizationEndpoint):
 
         sample_rate_count_if = 'count_if(trace.client_sample_rate, notEquals, "")'
         avg_sample_rate_equation = f"{sample_rate_count_if} / count()"
-        transaction_source_count_if = 'count_if(transaction.source, enotEquals, "")'
+        transaction_source_count_if = 'count_if(transaction.source, notEquals, "")'
         avg_transaction_source_equation = f"{transaction_source_count_if} / count()"
 
         data = discover.query(
