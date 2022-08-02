c92655ae1bc357372bee70707c7a941746dfedfa
Scott Cooper <scttcper@gmail.com>
2022-08-02 08:41:04 -0700

    revert translate change

--- static/app/components/bulkController/bulkNotice.tsx
+++ static/app/components/bulkController/bulkNotice.tsx
@@ -1,7 +1,7 @@
 import {Fragment} from 'react';
 import styled from '@emotion/styled';
 
-import {t, tn} from 'sentry/locale';
+import {t, tct, tn} from 'sentry/locale';
 import {defined} from 'sentry/utils';
 
 import Button from '../button';
@@ -17,14 +17,22 @@ function getSelectAllText(allRowsCount?: number, bulkLimit?: number) {
 
   if (bulkLimit && allRowsCount > bulkLimit) {
     return {
-      noticeText: t('Selected up to the first %s items.', bulkLimit),
-      actionText: t('Select the first %s items.', bulkLimit),
+      noticeText: tct('Selected up to the first [count] items.', {
+        count: bulkLimit,
+      }),
+      actionText: tct('Select the first [count] items.', {
+        count: bulkLimit,
+      }),
     };
   }
 
   return {
-    noticeText: t('Selected all %s items.', allRowsCount),
-    actionText: t('Select all %s items.', allRowsCount),
+    noticeText: tct('Selected all [count] items.', {
+      count: allRowsCount,
+    }),
+    actionText: tct('Select all [count] items.', {
+      count: allRowsCount,
+    }),
   };
 }
 
