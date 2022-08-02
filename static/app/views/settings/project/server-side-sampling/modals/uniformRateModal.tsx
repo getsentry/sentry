f070e30b87a8ae67e45f9049ad117934f9b23b90
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-08-02 15:32:53 +0200

    feat(sampling): Add specify current client sample rate unit test -  (#37280)

--- static/app/views/settings/project/server-side-sampling/modals/uniformRateModal.tsx
+++ static/app/views/settings/project/server-side-sampling/modals/uniformRateModal.tsx
@@ -106,7 +106,6 @@ function UniformRateModal({
     if (loading) {
       return;
     }
-
     const clientDiscard = projectStats30d?.groups.some(
       g => g.by.outcome === Outcome.CLIENT_DISCARD
     );
