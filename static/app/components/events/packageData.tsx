5206388de2f889eb1b6f95ef8bef620c3c59838f
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-08-03 16:14:42 +0200

    ref(package-data): Replace getMeta (proxy) with _meta object (#37345)

--- static/app/components/events/packageData.tsx
+++ static/app/components/events/packageData.tsx
@@ -1,10 +1,7 @@
-import {Component} from 'react';
-
 import ClippedBox from 'sentry/components/clippedBox';
 import ErrorBoundary from 'sentry/components/errorBoundary';
 import EventDataSection from 'sentry/components/events/eventDataSection';
 import KeyValueList from 'sentry/components/events/interfaces/keyValueList';
-import {getMeta} from 'sentry/components/events/meta/metaProxy';
 import {t} from 'sentry/locale';
 import {Event} from 'sentry/types/event';
 
@@ -12,41 +9,33 @@ type Props = {
   event: Event;
 };
 
-class EventPackageData extends Component<Props> {
-  shouldComponentUpdate(nextProps: Props) {
-    return this.props.event.id !== nextProps.event.id;
+export function EventPackageData({event}: Props) {
+  let longKeys: boolean, title: string;
+
+  const packages = Object.entries(event.packages || {}).map(([key, value]) => ({
+    key,
+    value,
+    subject: key,
+    meta: event._meta?.packages?.[key]?.[''],
+  }));
+
+  switch (event.platform) {
+    case 'csharp':
+      longKeys = true;
+      title = t('Assemblies');
+      break;
+    default:
+      longKeys = false;
+      title = t('Packages');
   }
 
-  render() {
-    const {event} = this.props;
-    let longKeys: boolean, title: string;
-    const packages = Object.entries(event.packages || {}).map(([key, value]) => ({
-      key,
-      value,
-      subject: key,
-      meta: getMeta(event.packages, key),
-    }));
-
-    switch (event.platform) {
-      case 'csharp':
-        longKeys = true;
-        title = t('Assemblies');
-        break;
-      default:
-        longKeys = false;
-        title = t('Packages');
-    }
-
-    return (
-      <EventDataSection type="packages" title={title}>
-        <ClippedBox>
-          <ErrorBoundary mini>
-            <KeyValueList data={packages} longKeys={longKeys} />
-          </ErrorBoundary>
-        </ClippedBox>
-      </EventDataSection>
-    );
-  }
+  return (
+    <EventDataSection type="packages" title={title}>
+      <ClippedBox>
+        <ErrorBoundary mini>
+          <KeyValueList data={packages} longKeys={longKeys} />
+        </ErrorBoundary>
+      </ClippedBox>
+    </EventDataSection>
+  );
 }
-
-export default EventPackageData;
