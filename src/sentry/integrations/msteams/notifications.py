c07baf8bec09d74b9262f6321c985877b61c1431
Varun Venkatesh <97v.varun@gmail.com>
2022-08-03 15:40:04 -0700

    fix(msteams): Add analytics event (#37452)
    - Add analytics event.
    - Improve exception log.

--- src/sentry/integrations/msteams/notifications.py
+++ src/sentry/integrations/msteams/notifications.py
@@ -98,7 +98,9 @@ def send_notification_as_msteams(
 
                         notification.record_notification_sent(recipient, ExternalProviders.MSTEAMS)
                     except Exception as e:
-                        logger.error(f"Exception occured while trying to send the notification {e}")
+                        logger.error(
+                            "Exception occured while trying to send the notification", exc_info=e
+                        )
 
     metrics.incr(
         f"{notification.metrics_key}.notifications.sent",
