eeff0881116c9c90673e2728a66460a63af28ad7
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-08-03 10:16:26 -0700

    fix(replays): Reset the `currentTime` whenever the player re-mounts (#37394)
    What was happening is that when the layout is changed react was re-mounting the iframe, so we're basically destroying everything and re-building it again in a new spot on the page. 
    This means that the rrweb instance `new Replayer(...)` was also being recreated, and starting playback at timestamp=0.
    
    To fix things, when we are creating or re-creating the `new Replayer` instance we should set the timestamp back to where it was. 
    
    I was thinking that this could get messed up later if we use the same `<ReplayContext>` with different replay data (a different replay_id or something) but that's not happening yet, and we will probably need to clear out other state values at that time too. It's a matter of including `events` in all the deps arrays for all the hooks i think. 
    
    Fixes [#37361](https://github.com/getsentry/sentry/issues/37361)

--- static/app/components/replays/replayContext.tsx
+++ static/app/components/replays/replayContext.tsx
@@ -244,6 +244,13 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
     setIsPlaying(false);
   }, []);
 
+  const getCurrentTime = useCallback(
+    () => (replayerRef.current ? Math.max(replayerRef.current.getCurrentTime(), 0) : 0),
+    []
+  );
+
+  const currentPlayerTime = useCurrentTime(getCurrentTime);
+
   const initRoot = useCallback(
     (root: RootElem) => {
       if (events === undefined) {
@@ -305,8 +312,10 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
       if (unMountedRef.current) {
         unMountedRef.current = false;
       }
+
+      replayerRef.current.pause(getCurrentTime());
     },
-    [events, theme.purple200, setReplayFinished, hasNewEvents]
+    [events, theme.purple200, setReplayFinished, hasNewEvents, getCurrentTime]
   );
 
   useEffect(() => {
@@ -325,11 +334,6 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
       document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
   }, [initRoot, events]);
-
-  const getCurrentTime = useCallback(
-    () => (replayerRef.current ? Math.max(replayerRef.current.getCurrentTime(), 0) : 0),
-    []
-  );
 
   const setCurrentTime = useCallback(
     (requestedTimeMs: number) => {
@@ -416,6 +420,8 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
   }, []);
 
   // Only on pageload: set the initial playback timestamp
+  // Don't include `setCurrentTime` in the hook deps array because it changes
+  // on each play/pause state change.
   useEffect(() => {
     if (initialTimeOffset && events && replayerRef.current) {
       setCurrentTime(initialTimeOffset * 1000);
@@ -424,9 +430,7 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
     return () => {
       unMountedRef.current = true;
     };
-  }, [events, replayerRef.current]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  const currentPlayerTime = useCurrentTime(getCurrentTime);
+  }, [initialTimeOffset, events]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const [isBuffering, currentTime] =
     buffer.target !== -1 &&
