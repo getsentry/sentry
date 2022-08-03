8abafdfe5d129df9b1ee3f4f95ef0969658f41cd
jesus4497 <jesuspadron4497@gmail.com>
2022-08-03 17:56:38 -0400

    feat: consider window height for choosing default layout

--- static/app/views/replays/detail/layout/utils.tsx
+++ static/app/views/replays/detail/layout/utils.tsx
@@ -2,12 +2,11 @@ import theme from 'sentry/utils/theme';
 import {LayoutModes} from 'sentry/views/replays/detail/layout';
 
 export const getDefaultLayout = (): LayoutModes => {
-  const {innerWidth} = window;
+  const {innerWidth, innerHeight} = window;
   const sidebarWidth = parseInt(theme.sidebar.expandedWidth, 10);
-
   const windowsWidth = innerWidth - sidebarWidth;
 
-  if (windowsWidth < 1024) {
+  if (windowsWidth < 1024 || innerHeight > 950) {
     return 'topbar';
   }
 
