d0e369a4aced4c8ab9704a4a1e1646b6a9f98692
jesus4497 <jesuspadron4497@gmail.com>
2022-08-04 15:07:33 -0400

    feat: add new condition at choosing layout

--- static/app/views/replays/detail/layout/utils.tsx
+++ static/app/views/replays/detail/layout/utils.tsx
@@ -1,16 +1,25 @@
 import theme from 'sentry/utils/theme';
 import {LayoutModes} from 'sentry/views/replays/detail/layout';
 
+const DECIMAL_RADIX = 10;
+const RECOMMENDED_WIDTH = 1700;
+const MIN_RECOMMENDED_WIDTH = 1024;
+const MIN_RECOMMENDED_HEIGHT = 960;
+
 export const getDefaultLayout = (): LayoutModes => {
   const {innerWidth, innerHeight} = window;
-  const sidebarWidth = parseInt(theme.sidebar.expandedWidth, 10);
+  const sidebarWidth = parseInt(theme.sidebar.expandedWidth, DECIMAL_RADIX);
   const windowsWidth = innerWidth - sidebarWidth;
 
-  if (innerHeight > 960 && windowsWidth < 1700) {
+  if (windowsWidth < innerHeight) {
     return 'topbar';
   }
 
-  if (windowsWidth < 1024) {
+  if (innerHeight > MIN_RECOMMENDED_HEIGHT && windowsWidth < RECOMMENDED_WIDTH) {
+    return 'topbar';
+  }
+
+  if (windowsWidth < MIN_RECOMMENDED_WIDTH) {
     return 'topbar';
   }
 
