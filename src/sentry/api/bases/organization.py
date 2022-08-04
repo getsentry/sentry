add9479594ad367db3003aecf24ad5b932d8815e
Alberto Leal <mail4alberto@gmail.com>
2022-08-04 18:12:09 -0400

    chore(hybrid-cloud): Delete resolve_org_slug_region and parse_subdomain (#36996)

--- src/sentry/api/bases/organization.py
+++ src/sentry/api/bases/organization.py
@@ -5,7 +5,6 @@ from django.core.cache import cache
 from rest_framework.exceptions import ParseError, PermissionDenied
 from rest_framework.request import Request
 
-from sentry import options
 from sentry.api.base import Endpoint, resolve_region
 from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.helpers.environments import get_environments
@@ -156,29 +155,6 @@ class OrganizationAlertRulePermission(OrganizationPermission):
         "PUT": ["org:write", "org:admin", "alert_rule:write"],
         "DELETE": ["org:write", "org:admin", "alert_rule:write"],
     }
-
-
-def parse_subdomain(subdomain):
-    if subdomain is None:
-        return None, None
-    org_base_hostname = options.get("system.organization-base-hostname")
-    if not org_base_hostname or "{slug}" not in org_base_hostname:
-        return None, None
-    org_slug = subdomain
-    region = None
-    if "{region}" in org_base_hostname:
-        if "." in subdomain:
-            org_slug, _, region = subdomain.rpartition(".")
-        else:
-            return None, None
-    return org_slug, region
-
-
-def resolve_org_slug_region(request: Request, organization_slug: Optional[str] = None):
-    region = None
-    if organization_slug is None and request.subdomain is not None:
-        organization_slug, region = parse_subdomain(request.subdomain)
-    return organization_slug, region
 
 
 class OrganizationEndpoint(Endpoint):
