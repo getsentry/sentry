getsentry/sentry

commit d45838bdd36aed630c163e6417394a5a3e40a172

fix(replays): Reset the `currentTime` whenever the player re-mounts (#37394)
What was happening is that when the layout is changed react was re-mounting the iframe, so we're basically destroying everything and re-building it again in a new spot on the page. 
This means that the rrweb instance `new Replayer(...)` was also being recreated, and starting playback at timestamp=0.

To fix things, when we are creating or re-creating the `new Replayer` instance we should set the timestamp back to where it was. 

I was thinking that this could get messed up later if we use the same `<ReplayContext>` with different replay data (a different replay_id or something) but that's not happening yet, and we will probably need to clear out other state values at that time too. It's a matter of including `events` in all the deps arrays for all the hooks i think. 

Fixes [#37361](https://github.com/getsentry/sentry/issues/37361)

static/app/components/replays/replayContext.tsx
@@ -244,6 +244,13 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
     setIsPlaying(false);
   }, []);
 
+  const getCurrentTime = useCallback(
+    () => (replayerRef.current ? Math.max(replayerRef.current.getCurrentTime(), 0) : 0),
+    []
+  );
+
+  const currentPlayerTime = useCurrentTime(getCurrentTime);
+
   const initRoot = useCallback(
     (root: RootElem) => {
       if (events === undefined) {
@@ -305,8 +312,10 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
       if (unMountedRef.current) {
         unMountedRef.current = false;
       }
+
+      replayerRef.current.pause(getCurrentTime());
     },
-    [events, theme.purple200, setReplayFinished, hasNewEvents]
+    [events, theme.purple200, setReplayFinished, hasNewEvents, getCurrentTime]
   );
 
   useEffect(() => {
@@ -325,11 +334,6 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
       document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
   }, [initRoot, events]);
-
-  const getCurrentTime = useCallback(
-    () => (replayerRef.current ? Math.max(replayerRef.current.getCurrentTime(), 0) : 0),
-    []
-  );
 
   const setCurrentTime = useCallback(
     (requestedTimeMs: number) => {
@@ -416,6 +420,8 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
   }, []);
 
   // Only on pageload: set the initial playback timestamp
+  // Don't include `setCurrentTime` in the hook deps array because it changes
+  // on each play/pause state change.
   useEffect(() => {
     if (initialTimeOffset && events && replayerRef.current) {
       setCurrentTime(initialTimeOffset * 1000);
@@ -424,9 +430,7 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
     return () => {
       unMountedRef.current = true;
     };
-  }, [events, replayerRef.current]); // eslint-disable-line react-hooks/exhaustive-deps
-
-  const currentPlayerTime = useCurrentTime(getCurrentTime);
+  }, [initialTimeOffset, events]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const [isBuffering, currentTime] =
     buffer.target !== -1 &&

commit 157ee8f43737996eea81386cd1cca94c00adcd03

fix(replays): Fix currentTime setting in player when changing layout (#37442)
This fixes an issue that was introduced in #37394

The original problem was that: when you change the layout the currentTime was being reset back to `0:00`.
#37394 introduced a new issue that changing the layout failed to re-create the Replayer instance, so there was no iframe in the player area anymore.

This fixes the known problems by:
a) Use (as before) `replayerRef.current` in the useEffect hook dependencies. This is despite the lint rules warnings. What we want is a reliable way to track whether the Replayer instance is mounted or not, and we do get re-renders when that changes, so the lint rule isn't applicable.
b) Just read the `currentPlayerTime` which is a state variable and is already set, instead of pulling the time out again from the function.

Testing notes:
I did test this before with two steps:
1. Click into the timeline to set `currentTime` to be greater than zero
2. Use the dropdown in the top-right corner to change the layout
Expected that the `currentTime` would be unchanged after the layout is updated.

I'm not sure why the tests weren't working when I did #37394, but I made sure to fully restart my dev server and clear caches to know that things are working now.

static/app/components/replays/replayContext.tsx
@@ -313,9 +313,9 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
         unMountedRef.current = false;
       }
 
-      replayerRef.current.pause(getCurrentTime());
+      replayerRef.current.pause(currentPlayerTime);
     },
-    [events, theme.purple200, setReplayFinished, hasNewEvents, getCurrentTime]
+    [events, theme.purple200, setReplayFinished, hasNewEvents, currentPlayerTime]
   );
 
   useEffect(() => {
@@ -420,8 +420,11 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
   }, []);
 
   // Only on pageload: set the initial playback timestamp
-  // Don't include `setCurrentTime` in the hook deps array because it changes
+  // Do not include `setCurrentTime` in the hook deps array because it changes
   // on each play/pause state change.
+  // Do include replayerRef.current in the hook deps. The rule warns that since
+  // it is a ref changing it will not cause a re-render. However, when that
+  // value changes we will get a re-render because other state values have changed.
   useEffect(() => {
     if (initialTimeOffset && events && replayerRef.current) {
       setCurrentTime(initialTimeOffset * 1000);
@@ -430,7 +433,7 @@ export function Provider({children, replay, initialTimeOffset = 0, value = {}}: 
     return () => {
       unMountedRef.current = true;
     };
-  }, [initialTimeOffset, events]); // eslint-disable-line react-hooks/exhaustive-deps
+  }, [initialTimeOffset, events, replayerRef.current]); // eslint-disable-line react-hooks/exhaustive-deps
 
   const [isBuffering, currentTime] =
     buffer.target !== -1 &&