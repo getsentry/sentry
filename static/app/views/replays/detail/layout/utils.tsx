7562ff8782837b28b7b967a824caf202920cc04e
jesus4497 <jesuspadron4497@gmail.com>
2022-08-04 15:19:25 -0400

    fix: remove unnecesary const

--- static/app/views/replays/detail/layout/utils.tsx
+++ static/app/views/replays/detail/layout/utils.tsx
@@ -1,14 +1,13 @@
 import theme from 'sentry/utils/theme';
 import {LayoutModes} from 'sentry/views/replays/detail/layout';
 
-const DECIMAL_RADIX = 10;
 const RECOMMENDED_WIDTH = 1700;
 const MIN_RECOMMENDED_WIDTH = 1024;
 const MIN_RECOMMENDED_HEIGHT = 960;
 
 export const getDefaultLayout = (): LayoutModes => {
   const {innerWidth, innerHeight} = window;
-  const sidebarWidth = parseInt(theme.sidebar.expandedWidth, DECIMAL_RADIX);
+  const sidebarWidth = parseInt(theme.sidebar.expandedWidth, 10);
   const windowsWidth = innerWidth - sidebarWidth;
 
   if (windowsWidth < innerHeight) {
