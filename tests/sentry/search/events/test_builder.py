e4f07a479c4e213ff99a226d972bb5e0d056d8d7
William Mak <william@wmak.io>
2022-08-04 09:19:25 -0400

    fix(mep): Fallback on missing tag values (#37379)
    - Currently we're raising invalid search queries if a tag value is
      missing, but in these cases we should just try again with discover
      since the query might still be valid

--- tests/sentry/search/events/test_builder.py
+++ tests/sentry/search/events/test_builder.py
@@ -1116,7 +1116,7 @@ class MetricQueryBuilderTest(MetricBuilderBaseTest):
             pytest.skip("test does not apply if tag values are in clickhouse")
 
         with pytest.raises(
-            InvalidSearchQuery,
+            IncompatibleMetricsQuery,
             match=re.escape("Tag value was not found"),
         ):
             MetricsQueryBuilder(
@@ -1130,7 +1130,7 @@ class MetricQueryBuilderTest(MetricBuilderBaseTest):
         if options.get("sentry-metrics.performance.tags-values-are-strings"):
             pytest.skip("test does not apply if tag values are in clickhouse")
         with pytest.raises(
-            InvalidSearchQuery,
+            IncompatibleMetricsQuery,
             match=re.escape("Tag value was not found"),
         ):
             MetricsQueryBuilder(
@@ -2382,7 +2382,7 @@ class TimeseriesMetricQueryBuilderTest(MetricBuilderBaseTest):
         if options.get("sentry-metrics.performance.tags-values-are-strings"):
             pytest.skip("test does not apply if tag values are in clickhouse")
         with pytest.raises(
-            InvalidSearchQuery,
+            IncompatibleMetricsQuery,
             match=re.escape("Tag value was not found"),
         ):
             TimeseriesMetricQueryBuilder(
@@ -2397,7 +2397,7 @@ class TimeseriesMetricQueryBuilderTest(MetricBuilderBaseTest):
         if options.get("sentry-metrics.performance.tags-values-are-strings"):
             pytest.skip("test does not apply if tag values are in clickhouse")
         with pytest.raises(
-            InvalidSearchQuery,
+            IncompatibleMetricsQuery,
             match=re.escape("Tag value was not found"),
         ):
             TimeseriesMetricQueryBuilder(
