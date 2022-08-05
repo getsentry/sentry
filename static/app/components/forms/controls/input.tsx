7bbd62abd97da0adb945734aa87e19eb2ee297d1
Vu Luong <vuluongj20@gmail.com>
2022-08-05 13:27:46 -0700

    fix(getsentry): Add temporary Input re-export

---
+++ static/app/components/forms/controls/input.tsx
@@ -0,0 +1,5 @@
+import Input from 'sentry/components/input';
+
+// Temporary re-export to fix a gcloud build issue. Do not use this file. Import
+// Input directly from 'sentry/components/input' instead.
+export default Input;
