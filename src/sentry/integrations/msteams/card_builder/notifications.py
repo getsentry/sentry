0ed7a128e00ff64204429103fc2f96b764caa151
Varun Venkatesh <97v.varun@gmail.com>
2022-08-05 16:22:30 -0700

    feat(msteams): Add release/deploy notification

--- src/sentry/integrations/msteams/card_builder/notifications.py
+++ src/sentry/integrations/msteams/card_builder/notifications.py
@@ -1,22 +1,30 @@
 from __future__ import annotations
 
-from typing import Any, Mapping
+from typing import Any, Mapping, Sequence
 
 from sentry.integrations.message_builder import (
     build_attachment_text,
     build_attachment_title,
     get_title_link,
 )
-from sentry.integrations.msteams.card_builder import MSTEAMS_URL_FORMAT, ColumnSetBlock, TextBlock
+from sentry.integrations.msteams.card_builder import (
+    MSTEAMS_URL_FORMAT,
+    Action,
+    ColumnSetBlock,
+    TextBlock,
+)
 from sentry.integrations.msteams.card_builder.base import MSTeamsMessageBuilder
 from sentry.models import Team, User
 from sentry.notifications.notifications.activity.base import GroupActivityNotification
 from sentry.notifications.notifications.base import BaseNotification
+from sentry.notifications.utils.actions import MessageAction
 from sentry.types.integrations import ExternalProviders
 
 from .block import (
+    ActionType,
     TextSize,
     TextWeight,
+    create_action_block,
     create_column_set_block,
     create_footer_column_block,
     create_footer_logo_block,
@@ -48,14 +56,18 @@ class MSTeamsNotificationsMessageBuilder(MSTeamsMessageBuilder):
 
         return None
 
-    def create_attachment_title_block(self) -> TextBlock:
+    def create_attachment_title_block(self) -> TextBlock | None:
         title = self.notification.build_attachment_title(self.recipient)
         title_link = self.notification.get_title_link(self.recipient, ExternalProviders.MSTEAMS)
 
-        return create_text_block(
-            MSTEAMS_URL_FORMAT.format(text=title, url=title_link),
-            size=TextSize.LARGE,
-            weight=TextWeight.BOLDER,
+        return (
+            create_text_block(
+                MSTEAMS_URL_FORMAT.format(text=title, url=title_link),
+                size=TextSize.LARGE,
+                weight=TextWeight.BOLDER,
+            )
+            if title
+            else None
         )
 
     def create_title_block(self) -> TextBlock:
@@ -70,6 +82,21 @@ class MSTeamsNotificationsMessageBuilder(MSTeamsMessageBuilder):
             size=TextSize.MEDIUM,
         )
 
+    @staticmethod
+    def create_action_blocks(actions: Sequence[MessageAction]) -> Sequence[Action]:
+        action_blocks = []
+        for action in actions:
+            name, url = getattr(action, "name", None), getattr(action, "url", None)
+            if not (name and url):
+                raise NotImplementedError(
+                    "Only actions with 'name' and 'url' attributes are supported now."
+                )
+            action_blocks.append(
+                create_action_block(ActionType.OPEN_URL, title=action.name, url=action.url)
+            )
+
+        return action_blocks
+
     def build_notification_card(self):
         fields = [self.create_attachment_title_block(), self.create_description_block()]
 
@@ -78,6 +105,9 @@ class MSTeamsNotificationsMessageBuilder(MSTeamsMessageBuilder):
             title=self.create_title_block(),
             fields=fields,
             footer=self.create_footer_block(),
+            actions=self.create_action_blocks(
+                self.notification.get_message_actions(self.recipient, ExternalProviders.MSTEAMS)
+            ),
         )
 
 
@@ -91,16 +121,20 @@ class MSTeamsIssueNotificationsMessageBuilder(MSTeamsNotificationsMessageBuilder
         super().__init__(notification, context, recipient)
         self.group = getattr(notification, "group", None)
 
-    def create_attachment_title_block(self) -> TextBlock:
+    def create_attachment_title_block(self) -> TextBlock | None:
         title = build_attachment_title(self.group)
         title_link = get_title_link(
             self.group, None, False, True, self.notification, ExternalProviders.MSTEAMS
         )
 
-        return create_text_block(
-            MSTEAMS_URL_FORMAT.format(text=title, url=title_link),
-            size=TextSize.LARGE,
-            weight=TextWeight.BOLDER,
+        return (
+            create_text_block(
+                MSTEAMS_URL_FORMAT.format(text=title, url=title_link),
+                size=TextSize.LARGE,
+                weight=TextWeight.BOLDER,
+            )
+            if title
+            else None
         )
 
     def create_description_block(self) -> TextBlock | None:
