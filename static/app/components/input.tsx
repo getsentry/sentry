458d7d27d0163147287e60619465541fa250686a
Vu Luong <vuluongj20@gmail.com>
2022-08-08 11:57:11 -0700

    ref(input): Export inputStyles from input.tsx

--- static/app/components/input.tsx
+++ static/app/components/input.tsx
@@ -1,18 +1,57 @@
 import {forwardRef} from 'react';
 import isPropValid from '@emotion/is-prop-valid';
+import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
-import {FormSize} from 'sentry/utils/theme';
+import {FormSize, Theme} from 'sentry/utils/theme';
 
 export interface InputProps
   extends Omit<React.InputHTMLAttributes<HTMLInputElement>, 'size'> {
-  leadingWidth?: number;
   monospace?: boolean;
   nativeSize?: React.InputHTMLAttributes<HTMLInputElement>['size'];
   size?: FormSize;
-  trailingWidth?: number;
   type?: React.HTMLInputTypeAttribute;
 }
+
+export const inputStyles = (p: InputProps & {theme: Theme}) => css`
+  display: block;
+  width: 100%;
+  color: ${p.theme.formText};
+  background: ${p.theme.background};
+  border: 1px solid ${p.theme.border};
+  border-radius: ${p.theme.borderRadius};
+  box-shadow: inset ${p.theme.dropShadowLight};
+  resize: vertical;
+  transition: border 0.1s, box-shadow 0.1s;
+
+  ${p.monospace ? `font-family: ${p.theme.text.familyMono};` : ''}
+  ${p.readOnly ? 'cursor: default;' : ''}
+
+  ${p.theme.form[p.size ?? 'md']}
+  ${p.theme.formPadding[p.size ?? 'md']}
+
+  &::placeholder {
+    color: ${p.theme.formPlaceholder};
+    opacity: 1;
+  }
+
+  &[disabled] {
+    background: ${p.theme.backgroundSecondary};
+    color: ${p.theme.disabled};
+    cursor: not-allowed;
+
+    &::placeholder {
+      color: ${p.theme.disabled};
+    }
+  }
+
+  &:focus,
+  &.focus-visible {
+    outline: none;
+    border-color: ${p.theme.focusBorder};
+    box-shadow: ${p.theme.focusBorder} 0 0 0 1px;
+  }
+`;
 
 /**
  * Basic input component.
@@ -40,59 +79,9 @@ const Input = styled(
       ref
     ) => <input {...props} ref={ref} size={nativeSize} />
   ),
-  {
-    shouldForwardProp: prop => typeof prop === 'string' && isPropValid(prop),
-  }
+  {shouldForwardProp: prop => typeof prop === 'string' && isPropValid(prop)}
 )`
-  display: block;
-  width: 100%;
-  color: ${p => p.theme.formText};
-  background: ${p => p.theme.background};
-  border: 1px solid ${p => p.theme.border};
-  border-radius: ${p => p.theme.borderRadius};
-  box-shadow: inset ${p => p.theme.dropShadowLight};
-  resize: vertical;
-  transition: border 0.1s, box-shadow 0.1s;
-
-  ${p => (p.monospace ? `font-family: ${p.theme.text.familyMono}` : '')};
-  ${p => (p.readOnly ? 'cursor: default;' : '')};
-
-  ${p => p.theme.form[p.size ?? 'md']}
-  ${p => p.theme.formPadding[p.size ?? 'md']}
-  ${p =>
-    p.leadingWidth &&
-    `padding-left: calc(
-      ${p.leadingWidth}px +
-      ${p.theme.formPadding[p.size ?? 'md'].paddingLeft}px * 1.618
-    );`} /* 1.618 cause golden ratio! */
-  ${p =>
-    p.trailingWidth &&
-    `padding-right: calc(
-      ${p.trailingWidth}px +
-      ${p.theme.formPadding[p.size ?? 'md'].paddingRight}px * 1.618
-    );`} /* 1.618 cause golden ratio! */
-
-  &::placeholder {
-    color: ${p => p.theme.formPlaceholder};
-    opacity: 1;
-  }
-
-  &[disabled] {
-    background: ${p => p.theme.backgroundSecondary};
-    color: ${p => p.theme.disabled};
-    cursor: not-allowed;
-
-    &::placeholder {
-      color: ${p => p.theme.disabled};
-    }
-  }
-
-  &:focus,
-  &.focus-visible {
-    outline: none;
-    border-color: ${p => p.theme.focusBorder};
-    box-shadow: ${p => p.theme.focusBorder} 0 0 0 1px;
-  }
+  ${inputStyles}
 `;
 
 export default Input;
