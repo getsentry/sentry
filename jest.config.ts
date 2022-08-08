7d5d3ee481e37d8b7a4a84d2921f8f3ea1995b1d
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-08-08 08:53:38 -0400

    jest.config.ts

--- jest.config.ts
+++ jest.config.ts
@@ -174,7 +174,7 @@ const config: Config.InitialOptions = {
     '<rootDir>/tests/js/setupFramework.ts',
     '@testing-library/jest-dom/extend-expect',
   ],
-  testMatch: testMatch || ['<rootDir>/tests/js/**/*(*.)@(spec|test).(js|ts)?(x)'],
+  testMatch: testMatch || ['<rootDir>/static/**/*(*.)@(spec|test).(js|ts)?(x)'],
   testPathIgnorePatterns: ['<rootDir>/tests/sentry/lang/javascript/'],
 
   unmockedModulePathPatterns: [
