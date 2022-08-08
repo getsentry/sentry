bc5c8bdd11218e134d8172e2c521b21db2f8750f
Vu Luong <vuluongj20@gmail.com>
2022-08-08 11:42:38 -0700

    ref(forms): Delete controls/input.tsx (#37560)

--- static/app/components/forms/controls/input.tsx
+++
@@ -1,5 +0,0 @@
-import Input from 'sentry/components/input';
-
-// Temporary re-export to fix a gcloud build issue. Do not use this file. Import
-// Input directly from 'sentry/components/input' instead.
-export default Input;
