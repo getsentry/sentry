f1f7143c372124fdb2257e1e7a99fcd241703aa5
Lyn Nagara <lyn.nagara@gmail.com>
2022-08-09 16:11:33 -0700

    feat: Add cloudspanner indexer class (#37631)
    Just a placeholder, it doesn't do anything yet.
    
    May need to be updated if https://github.com/getsentry/sentry/pull/36501 gets merged first.

---
+++ src/sentry/sentry_metrics/indexer/cloudspanner.py
@@ -0,0 +1,33 @@
+from typing import Mapping, Optional, Set
+
+from sentry.sentry_metrics.configuration import UseCaseKey
+from sentry.sentry_metrics.indexer.base import KeyResult, KeyResults, StringIndexer
+
+
+class CloudSpannerIndexer(StringIndexer):
+    def bulk_record(
+        self, use_case_id: UseCaseKey, org_strings: Mapping[int, Set[str]]
+    ) -> KeyResults:
+        # Currently just calls record() on each item. We may want to consider actually recording
+        # in batches though.
+        key_results = KeyResults()
+
+        for (org_id, strings) in org_strings.items():
+            for string in strings:
+                result = self.record(use_case_id, org_id, string)
+                key_results.add_key_result(KeyResult(org_id, string, result))
+
+        return key_results
+
+    def record(self, use_case_id: UseCaseKey, org_id: int, string: str) -> Optional[int]:
+        raise NotImplementedError
+
+    def resolve(
+        self, org_id: int, string: str, use_case_id: UseCaseKey = UseCaseKey.RELEASE_HEALTH
+    ) -> Optional[int]:
+        raise NotImplementedError
+
+    def reverse_resolve(
+        self, id: int, use_case_id: UseCaseKey = UseCaseKey.RELEASE_HEALTH
+    ) -> Optional[str]:
+        raise NotImplementedError
