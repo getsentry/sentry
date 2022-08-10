1772d5bb6b3dd533b8a082f9c9c1279bf28dc54a
Shruthi <shruthilaya.jaganathan@sentry.io>
2022-08-10 16:11:10 -0400

    fix: `release:latest` query filter alias on metrics endpoint (#37661)
    UnresolvedQuery requires organization_id to support the `latest`
    filter alias on the release tag. UnresolvedQuery calls
    [release_filter_converter](https://github.com/getsentry/sentry/blob/57d8c2a0093ba6d13996c0abcbec1cb853cf30f2/src/sentry/search/events/datasets/filter_aliases.py#L33) to resolve the where clause for releases and
    only returns latest releases correctly if organization_id
    is passed.

--- tests/sentry/snuba/metrics/test_query_builder.py
+++ tests/sentry/snuba/metrics/test_query_builder.py
@@ -58,6 +58,8 @@ from sentry.snuba.metrics.naming_layer.mapping import get_mri
 from sentry.snuba.metrics.naming_layer.mri import SessionMRI
 from sentry.snuba.metrics.query import MetricField
 from sentry.snuba.metrics.query_builder import QueryDefinition
+from sentry.testutils import TestCase
+from sentry.testutils.helpers.datetime import before_now
 
 pytestmark = pytest.mark.sentry_metrics
 
@@ -961,3 +963,28 @@ def test_translate_meta_results_with_duplicates():
         ],
         key=lambda elem: elem["name"],
     )
+
+
+class QueryDefinitionTestCase(TestCase):
+    def test_valid_latest_release_alias_filter(self):
+        self.create_release(version="foo", project=self.project, date_added=before_now(days=4))
+        self.create_release(
+            version="bar", project=self.project, date_added=before_now(days=2)
+        )  # latest release
+
+        query_params = MultiValueDict(
+            {
+                "query": ["release:latest"],
+                "field": [
+                    "sum(sentry.sessions.session)",
+                ],
+            }
+        )
+        query = QueryDefinition([self.project], query_params)
+        assert query.parsed_query == [
+            Condition(
+                Column(name="release"),
+                Op.IN,
+                rhs=["bar"],
+            )
+        ]
