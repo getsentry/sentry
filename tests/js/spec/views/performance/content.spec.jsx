51fe20531345a262ea99e31e57f79b4f2e913a5b
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-08-10 10:23:40 -0400

    fix(perf): Fetch widget overall numbers from API (#37470)
    * Remove unused mean calculation
    * Fetch overall widget number from discover
    * Make `GenericQueryBatcher` more generic
    * Add batching support to `GenericDiscoverQuery`
    * Add `DiscoverQuery` batching

--- tests/js/spec/views/performance/content.spec.jsx
+++ tests/js/spec/views/performance/content.spec.jsx
@@ -271,6 +271,14 @@ describe('Performance > Content', function () {
       url: `/organizations/org-slug/key-transactions-list/`,
       body: [],
     });
+    MockApiClient.addMockResponse({
+      method: 'GET',
+      url: `/organizations/org-slug/events/`,
+      body: {
+        data: [{}],
+        meta: {},
+      },
+    });
   });
 
   afterEach(function () {
