6ede1fe6d254419f753e6f864f33dea5c4927a5a
Billy Vong <billyvg@users.noreply.github.com>
2022-08-11 17:18:05 -0400

    feat(replays): Add feature flag for enabling Replay SDK (#37736)
    This will be used to put the initialization of the Replay integration behind a feature flag as we move to enable Replay recording for internal Sentry employees.
    
    (Missed this in #37673)

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -139,6 +139,7 @@ default_manager.add("organizations:required-email-verification", OrganizationFea
 default_manager.add("organizations:rule-page", OrganizationFeature)
 default_manager.add("organizations:sentry-functions", OrganizationFeature, True)
 default_manager.add("organizations:session-replay", OrganizationFeature, True)
+default_manager.add("organizations:session-replay-sdk", OrganizationFeature, True)
 default_manager.add("organizations:set-grouping-config", OrganizationFeature)
 default_manager.add("organizations:slack-overage-notifications", OrganizationFeature, True)
 default_manager.add("organizations:symbol-sources", OrganizationFeature)
