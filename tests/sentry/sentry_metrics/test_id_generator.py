8fac4bd8daeb2569ba6cba7b9d367b2be2ba4dcb
Lyn Nagara <lyn.nagara@gmail.com>
2022-08-11 13:39:58 -0700

    feat(indexer): Postgres/CloudSpanner/ClickHouse ID compatibility (#37717)
    This change ensures the generated IDs fit in a 63 bit integer, and the
    leading bit of the get_id() function is always zero.
    
    For CloudSpanner, the codec now reverses the bits, shifts to the left by
    one and subtracts 2^63 in order to produce a well distributed signed
    64 bit int.
    
    For Postgres, we cannot do the same since data is already present in the
    table and we cannot change the mapping. But this is not a problem since
    our ID generator will never produce numbers larger than 2^63.

--- tests/sentry/sentry_metrics/test_id_generator.py
+++ tests/sentry/sentry_metrics/test_id_generator.py
@@ -1,17 +1,17 @@
 import time
 from unittest.mock import patch
 
-from sentry.sentry_metrics.indexer.id_generator import _EPOCH_START, get_id
+from sentry.sentry_metrics.indexer.id_generator import _INDEXER_EPOCH_START, get_id
 
 
 def test_get_id() -> None:
     # Function returns a different ID each time it's called
     assert get_id() != get_id()
 
-    # IDs fit in 64 bits
-    assert get_id() < pow(2, 64)
+    # IDs fit in 63 bits (leading bit must be a zero)
+    assert get_id() < pow(2, 63)
 
-    # Starts with 0100 (version)
+    # Starts with 0100 (leading zero + version)
     id_binary_string = bin(get_id())[2:].zfill(64)
     assert id_binary_string.startswith("0100")
 
@@ -32,6 +32,6 @@ def test_get_id_time_since() -> None:
         mock_time.return_value = hardcoded_time
 
         id_string = bin(get_id())[2:].zfill(64)
-        original_time = int(id_string[3:36], 2) + _EPOCH_START
+        original_time = int(id_string[3:36], 2) + _INDEXER_EPOCH_START
 
         assert original_time == int(hardcoded_time)
