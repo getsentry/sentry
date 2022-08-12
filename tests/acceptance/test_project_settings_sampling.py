6a028794cd4bff61bd66db96a30eb86c6945030b
Andrii Soldatenko <andrii.soldatenko@sentry.io>
2022-08-12 10:58:11 +0200

    give a try!

--- tests/acceptance/test_project_settings_sampling.py
+++ tests/acceptance/test_project_settings_sampling.py
@@ -78,31 +78,15 @@ class ProjectSettingsSamplingTest(AcceptanceTestCase):
             self.wait_until_page_loaded()
 
             # Open uniform rate modal
-            start_button = self.browser.element('[aria-label="Start Setup"]')
-            # TODO: remove it
-            if start_button:
-                start_button.click()
-            else:
-                print(self.browser.page_source)
-                raise ValueError('can\'t find [aria-label="Start Setup"]')
+            self.browser.click_when_visible('[aria-label="Start Setup"]')
 
             self.browser.wait_until('[id="recommended-client-sampling"]')
 
             # Click on the recommended sampling values option
-            button1 = self.browser.element('[id="sampling-recommended"]')
-            if button1:
-                button1.click()
-            else:
-                print(self.browser.page_source)
-                raise ValueError('[id="sampling-recommended"]')
+            self.browser.click_when_visible('[id="sampling-recommended"]')
 
             # Click on done button
-            btt2 = self.browser.element('[aria-label="Done"]')
-            if btt2:
-                btt2.click()
-            else:
-                print(self.browser.page_source)
-                raise ValueError('[aria-label="Done"]')
+            self.browser.click_when_visible('[aria-label="Done"]')
 
             # Wait the success message to show up
             self.browser.wait_until('[data-test-id="toast-success"]')
