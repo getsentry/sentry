8d8554c6d2fd787515273c88ee6b03e9e572c095
Nar Saynorath <nar.saynorath@sentry.io>
2022-08-12 14:05:24 -0400

    Add types

--- static/app/views/dashboardsV2/types.tsx
+++ static/app/views/dashboardsV2/types.tsx
@@ -78,10 +78,12 @@ export type DashboardListItem = {
 
 export enum DashboardFilterKeys {
   RELEASE = 'release',
+  RELEASE_ID = 'releaseId',
 }
 
 export type DashboardFilters = {
   [DashboardFilterKeys.RELEASE]?: string[];
+  [DashboardFilterKeys.RELEASE_ID]?: number[];
 };
 
 /**
