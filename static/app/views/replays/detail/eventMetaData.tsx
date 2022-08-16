ad2ab9326dd58409ed9c2dafa7fabba591da9a1e
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-08-16 13:01:22 -0700

    ref(replays): Ensure ReplayRecord has the most up-to-date field names (#37844)
    This PR kills `getEvent()` in ReplayReader in favor of `getReplay()`.
    
    `getReplay()` returns an object of type `ReplayRecord`. To generate that type I was able to set a bunch of fields to be `null`.
    
    The rest of the PR is renaming `.replayId` to be `.id` and the like.

--- static/app/views/replays/detail/eventMetaData.tsx
+++ static/app/views/replays/detail/eventMetaData.tsx
@@ -9,7 +9,7 @@ import {IconCalendar, IconClock, IconFire} from 'sentry/icons';
 import space from 'sentry/styles/space';
 import type {Crumb} from 'sentry/types/breadcrumbs';
 import {defined} from 'sentry/utils';
-import useProjects from 'sentry/utils/useProjects';
+import {useRouteContext} from 'sentry/utils/useRouteContext';
 import type {ReplayRecord} from 'sentry/views/replays/types';
 
 type Props = {
@@ -19,20 +19,17 @@ type Props = {
 };
 
 function EventMetaData({crumbs, durationMs, replayRecord}: Props) {
-  const {projects} = useProjects();
+  const {
+    params: {replaySlug},
+  } = useRouteContext();
+  const [slug, id] = replaySlug.split(':');
+
   const errors = crumbs?.filter(crumb => crumb.type === 'error').length;
 
   return (
     <KeyMetrics>
       {replayRecord ? (
-        <ProjectBadge
-          project={
-            projects.find(p => p.id === replayRecord.projectId) || {
-              slug: replayRecord.projectSlug || '',
-            }
-          }
-          avatarSize={16}
-        />
+        <ProjectBadge project={{slug, id}} avatarSize={16} />
       ) : (
         <HeaderPlaceholder />
       )}
