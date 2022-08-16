065957d94c13e0bcedc882676f8ea44598457809
Jonas <jonas@badalic.com>
2022-08-16 06:36:29 -0400

    ref(profiling): drop old onboarding (#37439)
    * feat(profiling): add select project step
    
    * feat(profiling): use platformicons
    
    * test(profilingOnboardingModal): fix test
    
    * ref(review): remove supported util, usecallback and opt to throw
    
    * fix(tsc): dont pass leading icon if platform is unknown
    
    * feat(profiling): add upload symbols step
    
    * feat(profiling): add extra steps to onboarding
    
    * fix(onboardingmodal): tag is a div
    
    * ref(profiling): drop old onboarding
    
    * fix(profilingonboarding): use the new flag and update wording (#37506)
    
    * fix(profilingonboarding): use the new flag and update wording
    
    * fix(profilingonboarding): invalidate flag if there is data
    
    * fix(profiling): properly use my projects and all projects
    
    * fix(profiling): remove redundant call

--- static/app/utils/profiling/routes.tsx
+++ static/app/utils/profiling/routes.tsx
@@ -7,14 +7,6 @@ export function generateProfilingRoute({orgSlug}: {orgSlug: Organization['slug']
   return `/organizations/${orgSlug}/profiling/`;
 }
 
-export function generateProfilingOnboardingRoute({
-  orgSlug,
-}: {
-  orgSlug: Organization['slug'];
-}): Path {
-  return `/organizations/${orgSlug}/profiling/onboarding/`;
-}
-
 export function generateProfileSummaryRoute({
   orgSlug,
   projectSlug,
