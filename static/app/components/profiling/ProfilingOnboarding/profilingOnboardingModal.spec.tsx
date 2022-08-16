1aa9909a7eb3bbd1a3310b8c017adef46525b546
JonasBa <jonas.badalic@sentry.io>
2022-08-16 08:19:58 -0400

    feat(profiling): fix test

--- static/app/components/profiling/ProfilingOnboarding/profilingOnboardingModal.spec.tsx
+++ static/app/components/profiling/ProfilingOnboarding/profilingOnboardingModal.spec.tsx
@@ -29,7 +29,12 @@ describe('ProfilingOnboarding', function () {
   });
 
   it('renders default step', () => {
-    render(<ProfilingOnboardingModal {...MockRenderModalProps} />);
+    render(
+      <ProfilingOnboardingModal
+        organization={TestStubs.Organization()}
+        {...MockRenderModalProps}
+      />
+    );
     expect(screen.getByText(/Select a Project/i)).toBeInTheDocument();
   });
 
@@ -38,7 +43,12 @@ describe('ProfilingOnboarding', function () {
       TestStubs.Project({name: 'iOS Project', platform: 'apple-ios'}),
     ]);
 
-    render(<ProfilingOnboardingModal {...MockRenderModalProps} />);
+    render(
+      <ProfilingOnboardingModal
+        organization={TestStubs.Organization()}
+        {...MockRenderModalProps}
+      />
+    );
     selectProject(TestStubs.Project({name: 'iOS Project'}));
     act(() => {
       userEvent.click(screen.getAllByText('Next')[0]!);
@@ -57,7 +67,12 @@ describe('ProfilingOnboarding', function () {
       TestStubs.Project({name: 'javascript', platform: 'javascript'}),
     ]);
 
-    render(<ProfilingOnboardingModal {...MockRenderModalProps} />);
+    render(
+      <ProfilingOnboardingModal
+        organization={TestStubs.Organization()}
+        {...MockRenderModalProps}
+      />
+    );
     selectProject(TestStubs.Project({name: 'javascript'}));
     act(() => {
       userEvent.click(screen.getAllByText('Next')[0]!);
