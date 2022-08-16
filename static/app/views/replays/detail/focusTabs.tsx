43eba5956a35d50272bd42c311d8cd54c4e145d8
Dane Grant <danecando@gmail.com>
2022-08-16 17:02:35 -0400

    fix(replays): Fix Replay view tab links (#37877)
    ### Changes
    - Changes tab href from hash param to full path + search params 
    
    Closes #37811

--- static/app/views/replays/detail/focusTabs.tsx
+++ static/app/views/replays/detail/focusTabs.tsx
@@ -1,8 +1,10 @@
 import {MouseEvent} from 'react';
+import queryString from 'query-string';
 
 import NavTabs from 'sentry/components/navTabs';
 import {t} from 'sentry/locale';
 import useActiveReplayTab, {TabKey} from 'sentry/utils/replays/hooks/useActiveReplayTab';
+import {useLocation} from 'sentry/utils/useLocation';
 
 const ReplayTabs: Record<TabKey, string> = {
   console: t('Console'),
@@ -16,14 +18,16 @@ const ReplayTabs: Record<TabKey, string> = {
 type Props = {};
 
 function FocusTabs({}: Props) {
+  const {pathname, query} = useLocation();
   const {getActiveTab, setActiveTab} = useActiveReplayTab();
   const activeTab = getActiveTab();
+
   return (
     <NavTabs underlined>
       {Object.entries(ReplayTabs).map(([tab, label]) => (
         <li key={tab} className={activeTab === tab ? 'active' : ''}>
           <a
-            href={`#${tab}`}
+            href={`${pathname}?${queryString.stringify({...query, t_main: tab})}`}
             onClick={(e: MouseEvent) => {
               setActiveTab(tab);
               e.preventDefault();
