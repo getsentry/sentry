1c00ffccbeadb3a4d968e0aca22e060457fe0fed
Dane Grant <danecando@gmail.com>
2022-08-16 15:04:50 -0400

    fix(replays): updates tab urls with proper search params

--- static/app/views/replays/detail/focusTabs.tsx
+++ static/app/views/replays/detail/focusTabs.tsx
@@ -3,6 +3,7 @@ import {MouseEvent} from 'react';
 import NavTabs from 'sentry/components/navTabs';
 import {t} from 'sentry/locale';
 import useActiveReplayTab, {TabKey} from 'sentry/utils/replays/hooks/useActiveReplayTab';
+import {useRouteContext} from 'sentry/utils/useRouteContext';
 
 const ReplayTabs: Record<TabKey, string> = {
   console: t('Console'),
@@ -16,23 +17,35 @@ const ReplayTabs: Record<TabKey, string> = {
 type Props = {};
 
 function FocusTabs({}: Props) {
+  const {location} = useRouteContext();
   const {getActiveTab, setActiveTab} = useActiveReplayTab();
   const activeTab = getActiveTab();
+
   return (
     <NavTabs underlined>
-      {Object.entries(ReplayTabs).map(([tab, label]) => (
-        <li key={tab} className={activeTab === tab ? 'active' : ''}>
-          <a
-            href={`#${tab}`}
-            onClick={(e: MouseEvent) => {
-              setActiveTab(tab);
-              e.preventDefault();
-            }}
-          >
-            <span>{label}</span>
-          </a>
-        </li>
-      ))}
+      {Object.entries(ReplayTabs).map(([tab, label]) => {
+        const href = Object.entries({
+          ...location.query,
+          t_main: tab,
+        }).reduce(
+          (acc, [queryParam, val], i) => `${acc}${i > 0 ? '&' : ''}${queryParam}=${val}`,
+          `${location.pathname}?`
+        );
+
+        return (
+          <li key={tab} className={activeTab === tab ? 'active' : ''}>
+            <a
+              href={href}
+              onClick={(e: MouseEvent) => {
+                setActiveTab(tab);
+                e.preventDefault();
+              }}
+            >
+              <span>{label}</span>
+            </a>
+          </li>
+        );
+      })}
     </NavTabs>
   );
 }
