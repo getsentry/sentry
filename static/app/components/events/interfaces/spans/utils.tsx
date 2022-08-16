d5d011b1922a5b07659630a7d220abb6283cf62b
Ash Anand <ash.anand@sentry.io>
2022-08-16 16:03:29 -0400

    Root span is in view by default

--- static/app/components/events/interfaces/spans/utils.tsx
+++ static/app/components/events/interfaces/spans/utils.tsx
@@ -789,7 +789,7 @@ export class SpansInViewMap {
     this.spanDepthsInView = new Map();
     this.treeDepthSum = 0;
     this.length = 0;
-    this.isRootSpanInView = false;
+    this.isRootSpanInView = true;
   }
 
   /**
