cbe1f1895d7f83548ee4790f1f168f644504e0a2
Richard Ma <maxiuyuan1998@gmail.com>
2022-08-17 14:09:03 -0700

    Revert "feat(superuser): Require u2f on superuser access form (#37520)" (#37947)

--- static/app/constants/superuserAccessErrors.tsx
+++ static/app/constants/superuserAccessErrors.tsx
@@ -2,6 +2,5 @@ export enum ErrorCodes {
   invalidPassword = 'Incorrect password',
   invalidSSOSession = 'Your SSO Session has expired, please reauthenticate',
   invalidAccessCategory = 'Please fill out the access category and reason correctly',
-  noAuthenticator = 'Please add a U2F authenticator to your account',
   unknownError = 'An error ocurred, please try again',
 }
