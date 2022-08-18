5d5a8d1624348d44e32249d8f63ef2d0826a8960
Snigdha Sharma <16563948+snigdhas@users.noreply.github.com>
2022-08-18 15:05:21 -0700

    feat(suggested-assignee): include release committers in suggested assignees (#37792)
    Include release committers from the first release as suggested assignees

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -129,6 +129,7 @@ default_manager.add("organizations:performance-extraneous-spans-poc", Organizati
 default_manager.add("organizations:profiling", OrganizationFeature, True)
 default_manager.add("organizations:project-event-date-limit", OrganizationFeature, True)
 default_manager.add("organizations:related-events", OrganizationFeature)
+default_manager.add("organizations:release-committer-assignees", OrganizationFeature, True)
 default_manager.add("organizations:release-comparison-performance", OrganizationFeature, True)
 default_manager.add("organizations:release-health-check-metrics", OrganizationFeature, True)
 default_manager.add("organizations:release-health-check-metrics-report", OrganizationFeature, True)
