00c106bbfb5db076a78d2eceea9da92070e4f079
Josh Ferge <josh.ferge@sentry.io>
2022-08-19 16:01:33 -0700

    prefetch files using threads

--- src/sentry/replays/endpoints/project_replay_recording_segment_index.py
+++ src/sentry/replays/endpoints/project_replay_recording_segment_index.py
@@ -1,7 +1,7 @@
-import io
 import zlib
+from concurrent.futures import ThreadPoolExecutor
 
-from django.http import HttpRequest, HttpResponse, StreamingHttpResponse
+from django.http import StreamingHttpResponse
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -13,35 +13,11 @@ from sentry.models.file import File
 from sentry.replays.models import ReplayRecordingSegment
 from sentry.replays.serializers import ReplayRecordingSegmentSerializer
 
+CHUNKSIZE = 4096
+
 
 class ProjectReplayRecordingSegmentIndexEndpoint(ProjectEndpoint):
     private = True
-
-    def segment_generator(self, recording_segments):
-        yield "["
-
-        for i, file in enumerate(recording_segments):
-            yield self.deflate(file)
-            if i < len(recording_segments) - 1:
-                yield ","
-
-        yield "]"
-
-    def deflate(self, blob):
-        if blob.read(1) == b"{":
-            blob.seek(0)
-            return blob.read()
-        blob.seek(0)
-        return zlib.decompress(blob.read()).decode("utf-8")
-
-    def on_results(self, results):
-        # can do prefetch_related? hm
-        files = File.objects.filter(id__in=[r.file_id for r in results])
-        blobs = []
-        for file in files:
-            blobs.append(file.getfile())
-
-        return iter(self.segment_generator(blobs))
 
     def get(self, request: Request, project, replay_id: str) -> Response:
         if not features.has(
@@ -57,16 +33,11 @@ class ProjectReplayRecordingSegmentIndexEndpoint(ProjectEndpoint):
                     replay_id=replay_id.replace("-", ""),
                 ),
                 order_by="segment_id",
-                on_results=self.on_results,
+                on_results=self.on_download_results,
                 response_type=StreamingHttpResponse,
                 response_type_kwargs={"content_type": "application_json"},
                 paginator_cls=OffsetPaginator,
             )
-            # segments = ReplayRecordingSegment.objects.filter(
-            #     project_id=project.id,
-            #     replay_id=replay_id.replace("-", ""),
-            # ).order_by("segment_id")
-            # return self.on_results(segments)
 
         else:
             return self.paginate(
@@ -81,3 +52,42 @@ class ProjectReplayRecordingSegmentIndexEndpoint(ProjectEndpoint):
                 },
                 paginator_cls=OffsetPaginator,
             )
+
+    def on_download_results(self, results):
+        recording_segment_files = File.objects.filter(id__in=[r.file_id for r in results])
+        with ThreadPoolExecutor(max_workers=4) as exe:
+            file_objects = list(
+                exe.map(lambda file: file.getfile(prefetch=True), recording_segment_files)
+            )
+
+            return iter(self.segment_generator(file_objects))
+
+    def segment_generator(self, recording_segments):
+        yield "["
+
+        for i, file in enumerate(recording_segments):
+            if self.is_compressed(file):
+                yield from self.decompress_blob_stream(file)
+            else:
+                yield file.decode("utf-8")
+
+            if i < len(recording_segments) - 1:
+                yield ","
+
+        yield "]"
+
+    @staticmethod
+    def is_compressed(blob):
+        first_char = blob.read(1)
+        blob.seek(0)
+        if first_char == b"{":
+            return False
+        return True
+
+    @staticmethod
+    def decompress_blob_stream(blob):
+        decompressobj = zlib.decompressobj()
+        buffer = blob.read(CHUNKSIZE)
+        while buffer:
+            yield decompressobj.decompress(buffer).decode("utf-8")
+            buffer = blob.read(CHUNKSIZE)
