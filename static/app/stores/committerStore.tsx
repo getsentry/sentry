156402c43b74bce77bd40ced3ba3f7d4bf2835ed
Scott Cooper <scttcper@gmail.com>
2022-08-19 10:02:11 -0700

    feat(active-release): Display release committers in suggested (#38040)

--- static/app/stores/committerStore.tsx
+++ static/app/stores/committerStore.tsx
@@ -1,6 +1,6 @@
 import {createStore} from 'reflux';
 
-import {Committer} from 'sentry/types';
+import type {Committer, ReleaseCommitter} from 'sentry/types';
 import {makeSafeRefluxStore} from 'sentry/utils/makeSafeRefluxStore';
 
 type State = {
@@ -9,6 +9,7 @@ type State = {
     committers?: Committer[];
     committersError?: Error;
     committersLoading?: boolean;
+    releaseCommitters?: ReleaseCommitter[];
   };
 };
 
@@ -32,7 +33,8 @@ interface CommitterStoreDefinition extends Reflux.StoreDefinition {
     orgSlug: string,
     projectSlug: string,
     eventId: string,
-    data: Committer[]
+    committers: Committer[],
+    releaseCommitters?: ReleaseCommitter[]
   ): void;
 
   state: State;
@@ -78,12 +80,19 @@ export const storeConfig: CommitterStoreDefinition = {
     this.trigger(this.state);
   },
 
-  loadSuccess(orgSlug: string, projectSlug: string, eventId: string, data: Committer[]) {
+  loadSuccess(
+    orgSlug: string,
+    projectSlug: string,
+    eventId: string,
+    committers: Committer[],
+    releaseCommitters?: ReleaseCommitter[]
+  ) {
     const key = getCommitterStoreKey(orgSlug, projectSlug, eventId);
     this.state = {
       ...this.state,
       [key]: {
-        committers: data,
+        committers,
+        releaseCommitters,
         committersLoading: false,
         committersError: undefined,
       },
