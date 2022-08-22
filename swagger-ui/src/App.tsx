cda3bc5da0fbd87c487c84907eb8edec83341a0c
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-08-22 10:31:10 -0400

    init swagger-ui

---
+++ swagger-ui/src/App.tsx
@@ -0,0 +1,6 @@
+import SwaggerUI from 'swagger-ui-react';
+import 'swagger-ui-react/swagger-ui.css';
+
+const App = () => <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />;
+
+export default App;
