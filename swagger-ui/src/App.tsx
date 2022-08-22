beb58633a786e263cf4e704d9061fcca37918c5a
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-08-22 11:23:44 -0400

    Added build script

--- swagger-ui/src/App.tsx
+++ swagger-ui/src/App.tsx
@@ -1,6 +1,6 @@
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
-const App = () => <SwaggerUI url="/open-api/openapi.json" />;
+const App = () => <SwaggerUI url="/open-api/openapi.yml" />;
 
 export default App;
