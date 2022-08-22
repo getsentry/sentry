21f1b10951b230b79024e2c528b367e630e21c58
Dora Chan <doralchan@FVFHP62DoraChan.users.lanbeforeti.me>
2022-08-21 20:13:29 -0700

    removed reference to columns

--- static/app/views/eventsV2/table/columnEditModal.tsx
+++ static/app/views/eventsV2/table/columnEditModal.tsx
@@ -72,7 +72,7 @@ function ColumnEditModal(props: Props) {
       <Body>
         <Instruction>
           {tct(
-            'To group events, add [functionLink: functions] f(x) that may take in additional parameters. [fieldTagLink: Tag and field] columns will help you view more details about the events (i.e. title).',
+            'To group events, add [functionLink: functions] f(x), which may take in additional parameters. [fieldTagLink: Tags and fields] will help you view more details about the events (i.e. title).',
             {
               functionLink: (
                 <ExternalLink href="https://docs.sentry.io/product/discover-queries/query-builder/#filter-by-table-columns" />
