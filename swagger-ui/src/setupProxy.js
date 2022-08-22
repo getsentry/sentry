459d5b848b22bc6e375e59a6d05a3e3da65dcd40
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-08-22 12:20:14 -0400

    cleanup proxy

--- swagger-ui/src/setupProxy.js
+++ swagger-ui/src/setupProxy.js
@@ -1,11 +1,10 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const {createProxyMiddleware} = require('http-proxy-middleware');
 
-module.exports = function(app) {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target: 'https://sentry.io',
-      changeOrigin: true,
-    })
-  );
+const localHostProxy = createProxyMiddleware({
+  target: 'https://sentry.io',
+  changeOrigin: true,
+});
+
+module.exports = function (app) {
+  app.use('/api', localHostProxy);
 };
