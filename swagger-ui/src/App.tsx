ee490b0eb01b5701646106f0db5df10f9461d048
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-08-22 10:45:51 -0400

    move spec files

--- swagger-ui/src/App.tsx
+++ swagger-ui/src/App.tsx
@@ -1,6 +1,6 @@
 import SwaggerUI from 'swagger-ui-react';
 import 'swagger-ui-react/swagger-ui.css';
 
-const App = () => <SwaggerUI url="https://petstore.swagger.io/v2/swagger.json" />;
+const App = () => <SwaggerUI url="/open-api/openapi.json" />;
 
 export default App;
