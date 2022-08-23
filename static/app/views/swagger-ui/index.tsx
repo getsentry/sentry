e31ffa551fc662e6427c0441ea34d72b9ac2789a
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-08-23 14:33:55 -0400

    cleanup

--- static/app/views/swagger-ui/index.tsx
+++ static/app/views/swagger-ui/index.tsx
@@ -5,27 +5,21 @@ import SwaggerUI from 'swagger-ui-react';
 
 import {EXPERIMENTAL_SPA} from 'sentry/constants';
 
-type OpenApiDoc = OpenAPIV3.Document;
-type ServerObject = OpenAPIV3.ServerObject;
-
 const swagger: OpenApiDoc = require('../../../../tests/apidocs/openapi-derefed.json');
 
-let servers: ServerObject[];
+type OpenApiDoc = OpenAPIV3.Document;
 
 const {hostname} = window.location;
 const isLocalHost = hostname === 'localhost';
 const isRunningOnDevUI = EXPERIMENTAL_SPA && isLocalHost;
 
 if (isRunningOnDevUI) {
-  servers = [{url: 'https://localhost:7999/'}];
+  swagger.servers = [{url: 'https://localhost:7999/'}];
 } else if (isLocalHost) {
-  servers = [{url: 'https://localhost:8000/'}];
+  swagger.servers = [{url: 'https://localhost:8000/'}];
 } else {
-  servers = [{url: 'https://sentry.io/'}];
+  swagger.servers = [{url: 'https://sentry.io/'}];
 }
-
-swagger.servers = servers;
-swagger.components = {};
 
 const SwaggerUIDocs = () => <SwaggerUI spec={swagger} />;
 
