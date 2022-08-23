f1ccd2fa0f403572b7f6c23495103ddc51ecf275
Billy Vong <billy@sentry.io>
2022-08-23 12:14:15 -0400

    webpack hacks

--- build-utils/hackweek.ts
+++ build-utils/hackweek.ts
@@ -39,22 +39,31 @@ class SentryRuntimeModule extends RuntimeModule {
     // });
 
     return Template.asString([
+      'globalThis["__sentry_calls__"] = globalThis["__sentry_calls__"] || [];',
+      'globalThis["__sentry_imports__"] = globalThis["__sentry_imports__"] || [];',
+      // 'globalThis["__sentry_calls__2"] = globalThis["__sentry_calls__2"] || [[]];',
       "if (typeof __webpack_require__ === 'undefined') { return; }",
       '__webpack_require__ = new Proxy(__webpack_require__, {',
       Template.indent([
         'apply: function(target, thisArg, argumentsList) {',
         Template.indent([
-          'const result = target.apply(thisArg, argumentsList);',
-          "if (argumentsList[0].indexOf('node_modules')>-1 || argumentsList[0].indexOf('./app/views') === -1) {",
-          'return result;',
-          '}',
+          'var _module = argumentsList[0];',
+          "var _is_node_modules = _module.indexOf('node_modules')>-1;",
+          // When __webpack_require__ is called, that means the module was imported
+          'if (!_is_node_modules) { __sentry_imports__.push(_module); }',
+          // if module is already cached, return cached exports
+          'if (__webpack_module_cache__[_module] !== undefined) { return __webpack_module_cache__[_module].exports; }',
+          'var result = target.apply(thisArg, argumentsList);',
+          // WIP: Ignore node_modules and only include app/views for now
+          'if (_is_node_modules) { return result; }',
+          "if (_module.indexOf('./app/views') === -1) { return result; }",
           "if (typeof result !== 'object') { return result; }",
           'try {',
-          'return new Proxy(result, {',
+          'var proxy = new Proxy(result, {',
           Template.indent([
             'get(target, prop, receiver) {',
             Template.indent([
-              'const prim = Reflect.get(target, prop);',
+              'var prim = Reflect.get(target, prop);',
 
               "if (typeof prim !== 'function') {",
               '    return prim;',
@@ -63,8 +72,23 @@ class SentryRuntimeModule extends RuntimeModule {
               'try {',
               'return new Proxy(prim, {',
               '    apply: function(tgt, thisArg, args) {',
-              "        console.log('Function called: ', argumentsList[0], tgt.name || prop);",
-              '        return tgt.apply(thisArg, args);',
+              '        var _sentry_hub = Sentry && Sentry.getCurrentHub();',
+              '        var _sentry_scope = _sentry_hub && _sentry_hub.getScope();',
+              '        var _sentry_transaction = _sentry_scope.getTransaction();',
+              '        var _sentry_instr = {file: _module, fn: tgt.name || prop, transaction: _sentry_transaction && _sentry_transaction.name, startTimestamp: (performance.timeOrigin + performance.now())/1000};',
+              // '__sentry_calls__2.push([]);',
+              // '        if (!__sentry_calls__.length) {',
+              // '          __sentry_calls__.push()'
+              //
+              // '        }',
+              // '        var _sentry_last = __sentry_calls__.length ? __sentry_calls__[__sentry_calls__.length - 1] : '
+              "        console.log('Function called: ', _module, tgt.name || prop, _sentry_transaction?.name);",
+              '        var __result = tgt.apply(thisArg, args);',
+              '        _sentry_instr.endTimestamp = (performance.timeOrigin + performance.now()) / 1000;',
+              '        __sentry_calls__.push(_sentry_instr);',
+              // 'var __call_stack = __sentry_calls__2.pop();',
+              // '__sentry_calls__2[__sentry_calls__2.length-1].push({file: _module, fn: tgt.name || prop, transaction: _sentry_transaction && _sentry_transaction.name, stack: __call_stack});',
+              '        return __result;',
               '    }',
               '});',
               '} catch(err) {',
@@ -75,6 +99,9 @@ class SentryRuntimeModule extends RuntimeModule {
             '},',
           ]),
           '});',
+          // overwrite cache
+          '__webpack_module_cache__[_module].exports = proxy;',
+          'return proxy;',
           '} catch(err) {',
           'console.error(err);',
           'return result;',
@@ -90,7 +117,7 @@ class SentryRuntimeModule extends RuntimeModule {
     //     'apply: function(target, thisArg, argumentsList) {',
     //     Template.indent([
     //       'var result = target.apply(thisArg, argumentsList);',
-    //       'console.log("hello", argumentsList[0], result);',
+    //       'console.log("hello", _module, result);',
     //       'return result;',
     //     ]),
     //     '}',
@@ -130,11 +157,11 @@ class HackweekPlugin {
 
     // compiler.hooks.normalModuleFactory.tap('MyPlugin', factory => {
     //   factory.hooks.parser.for('javascript/auto').tap('MyPlugin', (parser, options) => {
-        // parser.hooks.exportDeclaration.tap('MyPlugin', (statement, declaration) => {
-        //   // console.log({statement, declaration});
-        //
-        //   return statement;
-        // });
+    // parser.hooks.exportDeclaration.tap('MyPlugin', (statement, declaration) => {
+    //   // console.log({statement, declaration});
+    //
+    //   return statement;
+    // });
     //   });
     // });
   }
