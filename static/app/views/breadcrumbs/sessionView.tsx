e852b9310cf52ce00bd8c541ea85d75cd437fb19
Alberto Leal <mail4alberto@gmail.com>
2022-08-23 01:27:56 -0400

    useState

--- static/app/views/breadcrumbs/sessionView.tsx
+++ static/app/views/breadcrumbs/sessionView.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useMemo} from 'react';
+import {useEffect, useMemo, useState} from 'react';
 import {RouteComponentProps} from 'react-router';
 import styled from '@emotion/styled';
 
