d68b219a75328c9f619087c084f589aeba022e6e
Joris Bayer <joris.bayer@sentry.io>
2022-08-24 15:25:18 +0200

    check in missing file

---
+++ src/sentry/grouping/mypyc/utils.py
@@ -0,0 +1,82 @@
+from typing import TYPE_CHECKING, Any, Mapping, MutableMapping, Optional, Sequence
+
+from sentry.stacktraces.functions import get_function_name_for_frame
+from sentry.stacktraces.platform import get_behavior_family_for_platform
+from sentry.utils.safe import get_path
+
+if TYPE_CHECKING:
+    from sentry.grouping.mypyc.rule import Rule
+
+
+FrameData = Mapping[str, Any]
+MatchFrame = MutableMapping[str, Any]
+
+
+ExceptionData = Mapping[str, Any]
+MatchingCache = MutableMapping[str, Any]
+
+
+def cached(cache, function, *args, **kwargs):
+    """Calls ``function`` or retrieves its return value from the ``cache``.
+
+    This is similar to ``functools.cache``, but uses a custom cache instead
+    of a global one. The cache can be shared between multiple functions.
+    """
+    key = (function, args, tuple(sorted(kwargs.items())))
+
+    if key in cache:
+        rv = cache[key]
+    else:
+        rv = cache[key] = function(*args, **kwargs)
+
+    return rv
+
+
+def _get_function_name(frame_data: FrameData, platform: Optional[str]) -> str:
+
+    function_name = get_function_name_for_frame(frame_data, platform)
+
+    return function_name or "<unknown>"
+
+
+def create_match_frame(frame_data: FrameData, platform: Optional[str]) -> MatchFrame:
+    """Create flat dict of values relevant to matchers"""
+    match_frame = dict(
+        category=get_path(frame_data, "data", "category"),
+        family=get_behavior_family_for_platform(frame_data.get("platform") or platform),
+        function=_get_function_name(frame_data, platform),
+        in_app=frame_data.get("in_app"),
+        module=get_path(frame_data, "module"),
+        package=frame_data.get("package"),
+        path=frame_data.get("abs_path") or frame_data.get("filename"),
+    )
+
+    for key in list(match_frame.keys()):
+        value = match_frame[key]
+        if isinstance(value, str):
+            if key in ("package", "path"):
+                value = match_frame[key] = value.lower()
+
+    return match_frame
+
+
+# TODO: better typing
+def apply_modifications_to_frame(
+    modifier_rules: Sequence["Rule"],
+    frames: Sequence[FrameData],
+    platform: str,
+    exception_data: ExceptionData,
+) -> None:
+    """This applies the frame modifications to the frames itself.  This
+    does not affect grouping.
+    """
+
+    cache: MatchingCache = {}
+
+    match_frames = [create_match_frame(frame, platform) for frame in frames]
+
+    for rule in modifier_rules:
+        for idx, action in rule.get_matching_frame_actions(
+            match_frames, platform, exception_data, cache
+        ):
+            action.apply_modifications_to_frame(frames, match_frames, idx, rule=rule)
