9d22f5a703b90ff4d01a5fedb1b3e8aa1bd91418
Malachi Willey <malwilley@gmail.com>
2022-08-23 22:42:14 -0700

    Change webpack output to minimal for pokedex

--- packages/pokedex/webpack.config.js
+++ packages/pokedex/webpack.config.js
@@ -1,40 +1,38 @@
-const path = require("path");
-const { ProgressPlugin } = require("webpack");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
-const { merge } = require("webpack-merge");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
-const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
-const CompressionPlugin = require("compression-webpack-plugin");
-const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const path = require('path');
+const {ProgressPlugin} = require('webpack');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const {merge} = require('webpack-merge');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
+const CompressionPlugin = require('compression-webpack-plugin');
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 
 class HelloAsyncPlugin {
   apply(compiler) {
-    compiler.hooks.emit.tapAsync(
-      'HelloAsyncPlugin',
-      (compilation, callback) => {
-        // Do something async...
-        setTimeout(function () {
-          console.log('Done with async work...');
-          callback();
-        }, 10000);
-      }
-    );
+    compiler.hooks.emit.tapAsync('HelloAsyncPlugin', (compilation, callback) => {
+      // Do something async...
+      setTimeout(function () {
+        console.log('Done with async work...');
+        callback();
+      }, 10000);
+    });
   }
 }
 
-const deps = require("./package.json").dependencies;
+const deps = require('./package.json').dependencies;
 
-const configMode = (env) => require(`./configs/webpack.${env.mode}`)(env);
+const configMode = env => require(`./configs/webpack.${env.mode}`)(env);
 
-const baseConfig = ({ analyze, mode, compress }) => ({
+const baseConfig = ({analyze, mode, compress}) => ({
   mode,
   output: {
-    path: path.resolve(__dirname, "dist"),
-    publicPath: "http://localhost:3000/",
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: 'http://localhost:3000/',
     clean: true,
   },
+  stats: 'minimal',
   resolve: {
-    extensions: [".js", ".jsx"],
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
@@ -42,12 +40,12 @@ const baseConfig = ({ analyze, mode, compress }) => ({
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.(jpeg|jpg|png|gif|webp|svg)$/i,
-        type: "asset",
+        type: 'asset',
       },
     ],
   },
@@ -55,27 +53,27 @@ const baseConfig = ({ analyze, mode, compress }) => ({
     analyze && new BundleAnalyzerPlugin(),
     new ProgressPlugin(),
     new HtmlWebPackPlugin({
-      template: "public/index.html",
+      template: 'public/index.html',
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: "public/",
+          from: 'public/',
           globOptions: {
-            ignore: ["**/index.html"],
+            ignore: ['**/index.html'],
           },
         },
       ],
     }),
     new ModuleFederationPlugin({
-      name: "remote",
-      filename: "remotePokedex.js",
+      name: 'remote',
+      filename: 'remotePokedex.js',
       remotes: {},
       exposes: {
-        "./pokeRoot": "./src/index.jsx",
-        "./pokedex": "./src/routes/Router.jsx",
-        "./pokeHome": "./src/pages/Home/Home.jsx",
-        "./pokeProvider": "./src/contexts/PokemonProvider.jsx",
+        './pokeRoot': './src/index.jsx',
+        './pokedex': './src/routes/Router.jsx',
+        './pokeHome': './src/pages/Home/Home.jsx',
+        './pokeProvider': './src/contexts/PokemonProvider.jsx',
       },
       shared: {
         // ...deps,
@@ -84,20 +82,20 @@ const baseConfig = ({ analyze, mode, compress }) => ({
           eager: true,
           requiredVersion: deps.react,
         },
-        "react-dom": {
+        'react-dom': {
           singleton: true,
-          requiredVersion: deps["react-dom"],
+          requiredVersion: deps['react-dom'],
           eager: true,
         },
       },
     }),
-    new HelloAsyncPlugin({ options: true }),
+    new HelloAsyncPlugin({options: true}),
     compress &&
       new CompressionPlugin({
-        algorithm: "brotliCompress",
+        algorithm: 'brotliCompress',
         test: /\.(js|css|html|svg)$/,
       }),
   ].filter(Boolean),
 });
 
-module.exports = (env) => merge(baseConfig(env), configMode(env));
+module.exports = env => merge(baseConfig(env), configMode(env));
