14c2bdc6b10e42f719a7c1e2e999235fd400afe4
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-08-24 13:55:46 -0400

    add selected styling

--- static/app/components/swagger-ui/sentrySwaggerMenu.tsx
+++ static/app/components/swagger-ui/sentrySwaggerMenu.tsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 export interface MenuItem {
   title: string;
   href?: string;
@@ -8,26 +10,37 @@ export interface Props {
   onMenuItemClick: (itemName: MenuItem) => void;
 }
 
-const SentrySwaggerMenu = ({menuItems, onMenuItemClick}: Props) => (
-  <div className="toc">
-    <ul className="list-unstyled">
-      <li className="mb-3">
-        <ul className="list-unstyled">
-          {menuItems.map(item => (
-            <li key={`${item.title}`}>
-              <div
-                style={{cursor: 'pointer'}}
-                className="sidebar-link"
-                onClick={() => onMenuItemClick(item)}
-              >
-                {item.title}
-              </div>
-            </li>
-          ))}
-        </ul>
-      </li>
-    </ul>
-  </div>
-);
+const SentrySwaggerMenu = ({menuItems, onMenuItemClick}: Props) => {
+  const [selectedMenuItem, setSelectedMenuItem] = useState<string>(menuItems[0].title);
+
+  const handleMenuClick = item => {
+    onMenuItemClick(item);
+    setSelectedMenuItem(item.title);
+  };
+
+  return (
+    <div className="toc">
+      <ul className="list-unstyled">
+        <li className="mb-3">
+          <ul className="list-unstyled">
+            {menuItems.map(item => (
+              <li key={`${item.title}`}>
+                <div
+                  style={{cursor: 'pointer'}}
+                  className={`sidebar-link ${
+                    item.title === selectedMenuItem ? 'selected' : ''
+                  }`}
+                  onClick={() => handleMenuClick(item)}
+                >
+                  {item.title}
+                </div>
+              </li>
+            ))}
+          </ul>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 export default SentrySwaggerMenu;
