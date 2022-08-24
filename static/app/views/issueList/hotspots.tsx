4577b8998a48b4f31d350de1675bdb69fda95231
Anton Pirker <anton.pirker@sentry.io>
2022-08-24 16:54:47 +0200

    Read data from API endpoint

--- static/app/views/issueList/hotspots.tsx
+++ static/app/views/issueList/hotspots.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import * as d3 from 'd3-hierarchy';
 import {CustomChart} from 'echarts/charts';
 import {
@@ -13,6 +13,7 @@ import {SVGRenderer} from 'echarts/renderers';
 import ReactEChartsCore from 'echarts-for-react/lib/core';
 
 import * as Layout from 'sentry/components/layouts/thirds';
+import type {Project} from 'sentry/types';
 
 // Register the required components
 echarts.use([
@@ -25,235 +26,59 @@ echarts.use([
   SVGRenderer,
 ]);
 
-type Props = {};
+type Props = {
+  organizationSlug: string;
+  projects: Project[];
+};
 
-function IssueHotSpots({}: Props) {
+function IssueHotSpots({organizationSlug, projects}: Props) {
   const DELIMITER = '/';
-  const seriesData = [
-    {
-      id: '<root>',
-      depth: 0,
-      index: 0,
-    },
-    {
-      id: '<root>/billiard',
-      depth: 1,
-      index: 1,
-    },
-    {
-      id: '<root>/billiard/pool.py',
-      errorCount: 3,
-      depth: 2,
-      index: 2,
-    },
-    {
-      id: '<root>/getsentry',
-      depth: 1,
-      index: 3,
-    },
-    {
-      id: '<root>/getsentry/utils',
-      depth: 2,
-      index: 4,
-    },
-    {
-      id: '<root>/getsentry/utils/flagr.py',
-      errorCount: 10,
-      depth: 3,
-      index: 5,
-    },
-    {
-      id: '<root>/sentry',
-      depth: 1,
-      index: 6,
-    },
-    {
-      id: '<root>/sentry/api',
-      depth: 2,
-      index: 7,
-    },
-    {
-      id: '<root>/sentry/api/client.py',
-      errorCount: 13,
-      depth: 3,
-      index: 8,
-    },
-    {
-      id: '<root>/sentry/api/endpoints',
-      depth: 3,
-      index: 10,
-    },
-    {
-      id: '<root>/sentry/api/endpoints/release_deploys.py',
-      errorCount: 123,
-      depth: 4,
-      index: 11,
-    },
-    {
-      id: '<root>/sentry/api/fields',
-      depth: 3,
-      index: 12,
-    },
-    {
-      id: '<root>/sentry/api/fields/avatar.py',
-      errorCount: 123,
-      depth: 3,
-      index: 13,
-    },
-    {
-      id: '<root>/sentry/db',
-      depth: 1,
-      index: 14,
-    },
-    {
-      id: '<root>/sentry/db/models',
-      depth: 2,
-      index: 15,
-    },
-    {
-      id: '<root>/sentry/db/models/fields',
-      depth: 3,
-      index: 16,
-    },
-    {
-      id: '<root>/sentry/db/models/fields/bounded.py',
-      errorCount: 123,
-      depth: 4,
-      index: 17,
-    },
-    {
-      id: '<root>/sentry/identity',
-      depth: 2,
-      index: 17,
-    },
-    {
-      id: '<root>/sentry/identity/oauth2.py',
-      errorCount: 123,
-      depth: 2,
-      index: 18,
-    },
-    {
-      id: '<root>/sentry/interfaces',
-      depth: 2,
-      index: 19,
-    },
-    {
-      id: '<root>/sentry/interfaces/contexts.py',
-      errorCount: 123,
-      depth: 2,
-      index: 20,
-    },
-    {
-      id: '<root>/sentry/models',
-      depth: 2,
-      index: 21,
-    },
-    {
-      id: '<root>/sentry/models/organizationmember.py',
-      errorCount: 123,
-      depth: 2,
-      index: 22,
-    },
-    {
-      id: '<root>/sentry/models/releasefile.py',
-      errorCount: 123,
-      depth: 2,
-      index: 23,
-    },
-    {
-      id: '<root>/sentry/net',
-      depth: 2,
-      index: 24,
-    },
-    {
-      id: '<root>/sentry/net/socket.py',
-      errorCount: 123,
-      depth: 2,
-      index: 25,
-    },
-    {
-      id: '<root>/sentry/receivers',
-      depth: 2,
-      index: 26,
-    },
-    {
-      id: '<root>/sentry/receivers/releases.py',
-      errorCount: 123,
-      depth: 2,
-      index: 27,
-    },
-    {
-      id: '<root>/sentry/release_health',
-      depth: 2,
-      index: 28,
-    },
-    {
-      id: '<root>/sentry/release_health/tasks.py',
-      errorCount: 123,
-      depth: 2,
-      index: 29,
-    },
-    {
-      id: '<root>/sentry/shared_integrations',
-      depth: 2,
-      index: 30,
-    },
-    {
-      id: '<root>/sentry/shared_integrations/client',
-      depth: 3,
-      index: 31,
-    },
-    {
-      id: '<root>/sentry/shared_integrations/client/base.py',
-      errorCount: 123,
-      depth: 3,
-      index: 32,
-    },
-    {
-      id: '<root>/sentry/utils',
-      depth: 2,
-      index: 33,
-    },
-    {
-      id: '<root>/sentry/utils/json.py',
-      errorCount: 123,
-      depth: 2,
-      index: 34,
-    },
-    {
-      id: '<root>/sentry/utils/locking',
-      depth: 3,
-      index: 35,
-    },
-    {
-      id: '<root>/sentry/utils/locking/lock.py',
-      errorCount: 123,
-      depth: 3,
-      index: 36,
-    },
-    {
-      id: '<root>/sentry/utils/monitors.py',
-      errorCount: 123,
-      depth: 2,
-      index: 38,
-    },
-  ];
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [diagramData, setDiagramData] = useState();
+
+  const projectId = projects[0].id;
 
   useEffect(
     () => {
+      const hotspotsEndpoint = `/api/0/organizations/${organizationSlug}/issues-hotspots/?project=${projectId}&statsPeriod=90d`;
+      fetch(hotspotsEndpoint, {
+        credentials: 'include',
+        headers: {
+          Accept: 'application/json; charset=utf-8',
+          'Accept-Language': 'en-US,en;q=0.5',
+          'content-type': 'application/json',
+          'Sec-Fetch-Dest': 'empty',
+          'Sec-Fetch-Mode': 'cors',
+          'Sec-Fetch-Site': 'same-origin',
+        },
+        method: 'GET',
+        mode: 'cors',
+      })
+        .then(result => result.json())
+        .then(result => {
+          setDiagramData(result);
+          setIsLoading(false);
+        });
+
       return () => {
         // cleanup code
       };
     },
-    [] // dependencies werte die ich in useEffect hook brauch (wenn sich das uendert dann wird effect hook ausgefuehr)
+    [organizationSlug, projectId] // dependencies werte die ich in useEffect hook brauch (wenn sich das uendert dann wird effect hook ausgefuehr)
   );
+
+  if (isLoading) {
+    return null;
+  }
 
   function stratify() {
     const stratify_output = d3
       .stratify()
       .parentId(function (d) {
         return d.id.substring(0, d.id.lastIndexOf(DELIMITER));
-      })(seriesData)
+      })(diagramData)
       .sum(function (d) {
         return d.errorCount || 0;
       })
@@ -365,7 +190,7 @@ function IssueHotSpots({}: Props) {
 
   const option = {
     dataset: {
-      source: seriesData,
+      source: diagramData,
     },
     tooltip: {},
     visualMap: [
