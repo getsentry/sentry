4bb380431daeb45def09ea62d92e88164ab4b9de
Abhijeet Prasad <aprasad@sentry.io>
2022-08-24 11:04:32 -0400

    update table to add more fields

--- static/app/views/userJourneys/index.tsx
+++ static/app/views/userJourneys/index.tsx
@@ -1,5 +1,6 @@
 import {Fragment, useEffect, useMemo, useState} from 'react';
 import styled from '@emotion/styled';
+import {PlatformIcon} from 'platformicons';
 
 import DatePageFilter from 'sentry/components/datePageFilter';
 import EnvironmentPageFilter from 'sentry/components/environmentPageFilter';
@@ -20,19 +21,24 @@ import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import useApi from 'sentry/utils/useApi';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
+import useProjects from 'sentry/utils/useProjects';
 
 interface Props {}
 
 interface User {
   'count(did)': number;
+  'count_if(event.type,equals,error)': number;
+  'count_if(event.type,equals,transaction)': number;
   did: string;
   'last_seen()': string;
+  project: string;
 }
 
 function Breadcrumbs({}: Props) {
   const org = useOrganization();
   const api = useApi();
   const location = useLocation();
+  const {projects} = useProjects();
   const [users, setUsers] = useState<Array<User>>([]);
   const [isLoading, setLoading] = useState(false);
 
@@ -47,7 +53,14 @@ function Breadcrumbs({}: Props) {
         id: '',
         name: '',
         version: 2,
-        fields: ['did', 'count(did)', 'last_seen()'],
+        fields: [
+          'did',
+          'count(did)',
+          'last_seen()',
+          'project',
+          'count_if(event.type,equals,error)',
+          'count_if(event.type,equals,transaction)',
+        ],
         projects: [],
         orderby: '-last_seen',
         query: conditions.formatString(),
@@ -81,6 +94,16 @@ function Breadcrumbs({}: Props) {
     </div>
   );
 
+  function ProjectItem({user}: {user: User}) {
+    const project = projects.find(p => p.slug === user.project);
+    return (
+      <Fragment>
+        {project && <StyledProjectItem platform={project.platform || 'default'} />}
+        {user.project}
+      </Fragment>
+    );
+  }
+
   return (
     <PageFiltersContainer>
       <PageContent>
@@ -97,7 +120,13 @@ function Breadcrumbs({}: Props) {
         <PanelTable
           isLoading={isLoading}
           isEmpty={users.length === 0}
-          headers={[t('User'), t('Number of Events'), lastActivity]}
+          headers={[
+            t('User'),
+            t('Project'),
+            t('Number of Errors'),
+            t('Number of Transactions'),
+            lastActivity,
+          ]}
         >
           {users.map(user => (
             <Fragment key={user.did}>
@@ -106,7 +135,11 @@ function Breadcrumbs({}: Props) {
                   {user.did}
                 </Link>
               </Item>
-              <Item>{user['count(did)']}</Item>
+              <Item>
+                <ProjectItem user={user} />
+              </Item>
+              <Item>{user['count_if(event.type,equals,error)']}</Item>
+              <Item>{user['count_if(event.type,equals,transaction)']}</Item>
               <Item>
                 {FIELD_FORMATTERS.date.renderFunc('last_seen()', {
                   ['last_seen()']: user['last_seen()'],
@@ -130,6 +163,10 @@ const Header = styled('div')`
 const Item = styled('div')`
   display: flex;
   align-items: center;
+`;
+
+const StyledProjectItem = styled(PlatformIcon)`
+  margin-right: 5px;
 `;
 
 export default Breadcrumbs;
