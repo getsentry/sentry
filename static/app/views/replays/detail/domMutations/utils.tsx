d0a6bf9c35aa0b00d9d831226b8996fc1b3abed9
jesus4497 <jesuspadron4497@gmail.com>
2022-08-29 17:12:07 -0400

    fix: improve filter method and sort event types

--- static/app/views/replays/detail/domMutations/utils.tsx
+++ static/app/views/replays/detail/domMutations/utils.tsx
@@ -6,7 +6,9 @@ export type DomFilters = {
 };
 
 export const getDomMutationsTypes = (actions: Extraction[]) =>
-  Array.from(new Set<BreadcrumbType>(actions.map(mutation => mutation.crumb.type)));
+  Array.from(
+    new Set<BreadcrumbType>(actions.map(mutation => mutation.crumb.type).sort())
+  );
 
 export const getFilteredDomMutations = (
   actions: Extraction[],
@@ -16,8 +18,10 @@ export const getFilteredDomMutations = (
   if (!searchTerm && Object.keys(filters).length === 0) {
     return actions;
   }
+
+  const normalizedSearchTerm = searchTerm.toLowerCase();
+
   return actions.filter(mutation => {
-    const normalizedSearchTerm = searchTerm.toLowerCase();
     const doesMatch = mutation.html?.toLowerCase().includes(normalizedSearchTerm);
 
     for (const key in filters) {
