4849941d86ae110763e6902f5db18de06c3bbf69
Colton Allen <colton.allen@sentry.io>
2022-08-29 17:07:57 -0500

    Remove SearchFilter check

--- src/sentry/replays/endpoints/organization_replay_index.py
+++ src/sentry/replays/endpoints/organization_replay_index.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 
 from sentry import features
 from sentry.api.bases.organization import NoProjects, OrganizationEndpoint
-from sentry.api.event_search import SearchFilter, parse_search_query
+from sentry.api.event_search import parse_search_query
 from sentry.api.paginator import GenericOffsetPaginator
 from sentry.models.organization import Organization
 from sentry.replays.post_process import process_raw_response
@@ -30,7 +30,6 @@ class OrganizationReplayIndexEndpoint(OrganizationEndpoint):
             search_filters = parse_search_query(
                 request.query_params.get("query", ""), config=replay_url_parser_config
             )
-            search_filters = [term for term in search_filters if isinstance(term, SearchFilter)]
 
             return query_replays_collection(
                 project_ids=filter_params["project_id"],
