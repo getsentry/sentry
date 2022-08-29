2a17a23c3ea1e2b9f722e53c3e88363c058569da
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-08-29 16:07:11 -0400

    ref(js): Convert AvatarList to a FC (#38215)

--- static/app/components/avatar/avatarList.tsx
+++ static/app/components/avatar/avatarList.tsx
@@ -1,4 +1,3 @@
-import {Component} from 'react';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
@@ -6,70 +5,59 @@ import UserAvatar from 'sentry/components/avatar/userAvatar';
 import Tooltip from 'sentry/components/tooltip';
 import {AvatarUser} from 'sentry/types';
 
-const defaultProps = {
-  avatarSize: 28,
-  maxVisibleAvatars: 5,
-  typeMembers: 'users',
-  tooltipOptions: {},
-};
-
 type UserAvatarProps = React.ComponentProps<typeof UserAvatar>;
 
-type DefaultProps = Readonly<typeof defaultProps>;
-type Mutable<T> = {-readonly [P in keyof T]: T[P]};
-
 type Props = {
-  tooltipOptions: Mutable<UserAvatarProps['tooltipOptions']>;
   users: AvatarUser[];
+  avatarSize?: number;
   className?: string;
+  maxVisibleAvatars?: number;
   renderTooltip?: UserAvatarProps['renderTooltip'];
-} & DefaultProps;
+  tooltipOptions?: UserAvatarProps['tooltipOptions'];
+  typeMembers?: string;
+};
 
-export default class AvatarList extends Component<Props> {
-  static defaultProps = defaultProps;
+function AvatarList({
+  avatarSize = 28,
+  maxVisibleAvatars = 5,
+  typeMembers = 'users',
+  tooltipOptions = {},
+  className,
+  users,
+  renderTooltip,
+}: Props) {
+  const visibleUsers = users.slice(0, maxVisibleAvatars);
+  const numCollapsedUsers = users.length - visibleUsers.length;
 
-  render() {
-    const {
-      className,
-      users,
-      avatarSize,
-      maxVisibleAvatars,
-      renderTooltip,
-      typeMembers,
-      tooltipOptions,
-    } = this.props;
-
-    const visibleUsers = users.slice(0, maxVisibleAvatars);
-    const numCollapsedUsers = users.length - visibleUsers.length;
-
-    if (!tooltipOptions.position) {
-      tooltipOptions.position = 'top';
-    }
-
-    return (
-      <AvatarListWrapper className={className}>
-        {!!numCollapsedUsers && (
-          <Tooltip title={`${numCollapsedUsers} other ${typeMembers}`}>
-            <CollapsedUsers size={avatarSize} data-test-id="avatarList-collapsedusers">
-              {numCollapsedUsers < 99 && <Plus>+</Plus>}
-              {numCollapsedUsers}
-            </CollapsedUsers>
-          </Tooltip>
-        )}
-        {visibleUsers.map(user => (
-          <StyledAvatar
-            key={`${user.id}-${user.email}`}
-            user={user}
-            size={avatarSize}
-            renderTooltip={renderTooltip}
-            tooltipOptions={tooltipOptions}
-            hasTooltip
-          />
-        ))}
-      </AvatarListWrapper>
-    );
+  if (!tooltipOptions.position) {
+    tooltipOptions.position = 'top';
   }
+
+  return (
+    <AvatarListWrapper className={className}>
+      {!!numCollapsedUsers && (
+        <Tooltip title={`${numCollapsedUsers} other ${typeMembers}`}>
+          <CollapsedUsers size={avatarSize} data-test-id="avatarList-collapsedusers">
+            {numCollapsedUsers < 99 && <Plus>+</Plus>}
+            {numCollapsedUsers}
+          </CollapsedUsers>
+        </Tooltip>
+      )}
+      {visibleUsers.map(user => (
+        <StyledAvatar
+          key={`${user.id}-${user.email}`}
+          user={user}
+          size={avatarSize}
+          renderTooltip={renderTooltip}
+          tooltipOptions={tooltipOptions}
+          hasTooltip
+        />
+      ))}
+    </AvatarListWrapper>
+  );
 }
+
+export default AvatarList;
 
 // used in releases list page to do some alignment
 export const AvatarListWrapper = styled('div')`
