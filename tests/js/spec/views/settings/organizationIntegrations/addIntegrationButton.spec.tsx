0dd06eb109063ad86be36069fda1e6a6cf89ea8e
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-08-30 08:50:51 +0200

    add global global comment

--- tests/js/spec/views/settings/organizationIntegrations/addIntegrationButton.spec.tsx
+++ tests/js/spec/views/settings/organizationIntegrations/addIntegrationButton.spec.tsx
@@ -1,3 +1,4 @@
+/* global global */
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import {AddIntegrationButton} from 'sentry/views/organizationIntegrations/addIntegrationButton';
