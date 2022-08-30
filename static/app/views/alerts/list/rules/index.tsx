051d5b858784761b9786e5a6e601b8ed941d257d
Scott Cooper <scttcper@gmail.com>
2022-08-30 09:40:40 -0700

    fix(alerts): AsyncComponent type can be null, fix TypeError

--- static/app/views/alerts/list/rules/index.tsx
+++ static/app/views/alerts/list/rules/index.tsx
@@ -34,7 +34,7 @@ type Props = RouteComponentProps<{orgId: string}, {}> & {
 type State = {
   alertRuleCount?: number;
   issueRuleCount?: number;
-  ruleList?: CombinedMetricIssueAlerts[];
+  ruleList?: CombinedMetricIssueAlerts[] | null;
   teamFilterSearch?: string;
 };
 
@@ -75,9 +75,9 @@ class AlertRulesList extends AsyncComponent<Props, State & AsyncComponent['state
   }
 
   get projectsFromResults() {
-    const {ruleList = []} = this.state;
+    const ruleList = this.state.ruleList ?? [];
 
-    return [...new Set(ruleList?.map(({projects}) => projects).flat())];
+    return [...new Set(ruleList.map(({projects}) => projects).flat())];
   }
 
   handleChangeFilter = (activeFilters: string[]) => {
