c68275d79880bda8044185b42e585242acf99a90
Nikhar Saxena <84807402+nikhars@users.noreply.github.com>
2022-08-31 10:40:08 -0700

    Revert "ref(indexer): allow spanner indexer in prod via indexer_db cli arg" (#38323)
    Indexer is broken Reverts getsentry/sentry#37745

--- tests/sentry/sentry_metrics/test_configuration.py
+++ tests/sentry/sentry_metrics/test_configuration.py
@@ -1,13 +1,12 @@
 from sentry.sentry_metrics.configuration import (
     _METRICS_INGEST_CONFIG_BY_USE_CASE,
-    IndexerStorage,
     UseCaseKey,
     get_ingest_config,
 )
 
 
 def test_unique_namespaces() -> None:
-    get_ingest_config(UseCaseKey.RELEASE_HEALTH, IndexerStorage.POSTGRES)
+    get_ingest_config(UseCaseKey.RELEASE_HEALTH)
     namespaces = [
         config.writes_limiter_namespace for config in _METRICS_INGEST_CONFIG_BY_USE_CASE.values()
     ]
