77a69c95e444f2a69ba7df64c392f09248db3f54
Nikhar Saxena <84807402+nikhars@users.noreply.github.com>
2022-08-31 10:15:05 -0700

    Revert "ref(indexer): allow spanner indexer in prod via indexer_db cli arg (#37745)"
    This reverts commit 453e977949ab25ed03ed6763e58214ff2ad7ffa3.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1484,8 +1484,6 @@ SENTRY_METRICS_INDEXER = "sentry.sentry_metrics.indexer.postgres.postgres_v2.Pos
 SENTRY_METRICS_INDEXER_OPTIONS = {}
 SENTRY_METRICS_INDEXER_CACHE_TTL = 3600 * 2
 
-SENTRY_METRICS_INDEXER_SPANNER_OPTIONS = {}
-
 # Rate limits during string indexing for our metrics product.
 # Which cluster to use. Example: {"cluster": "default"}
 SENTRY_METRICS_INDEXER_WRITES_LIMITER_OPTIONS = {}
@@ -2422,11 +2420,6 @@ KAFKA_INGEST_PERFORMANCE_METRICS = "ingest-performance-metrics"
 KAFKA_SNUBA_GENERIC_METRICS = "snuba-generic-metrics"
 KAFKA_INGEST_REPLAYS_RECORDINGS = "ingest-replay-recordings"
 
-# topic for testing multiple indexer backends in parallel
-# in production. So far just testing backends for the perf data,
-# not release helth
-KAFKA_SNUBA_GENERICS_METRICS_CS = "snuba-metrics-generics-cloudspanner"
-
 KAFKA_SUBSCRIPTION_RESULT_TOPICS = {
     "events": KAFKA_EVENTS_SUBSCRIPTIONS_RESULTS,
     "transactions": KAFKA_TRANSACTIONS_SUBSCRIPTIONS_RESULTS,
@@ -2465,8 +2458,6 @@ KAFKA_TOPICS = {
     KAFKA_INGEST_PERFORMANCE_METRICS: {"cluster": "default"},
     KAFKA_SNUBA_GENERIC_METRICS: {"cluster": "default"},
     KAFKA_INGEST_REPLAYS_RECORDINGS: {"cluster": "default"},
-    # Metrics Testing Topics
-    KAFKA_SNUBA_GENERICS_METRICS_CS: {"cluster": "default"},
 }
 
 
--- tests/sentry/sentry_metrics/test_multiprocess_steps.py
+++ tests/sentry/sentry_metrics/test_multiprocess_steps.py
@@ -10,7 +10,7 @@ from arroyo.backends.kafka import KafkaPayload
 from arroyo.processing.strategies import MessageRejected
 from arroyo.types import Message, Partition, Topic
 
-from sentry.sentry_metrics.configuration import IndexerStorage, UseCaseKey, get_ingest_config
+from sentry.sentry_metrics.configuration import UseCaseKey, get_ingest_config
 from sentry.sentry_metrics.consumers.indexer.batch import invalid_metric_tags, valid_metric_name
 from sentry.sentry_metrics.consumers.indexer.common import (
     BatchMessages,
@@ -28,9 +28,7 @@ logger = logging.getLogger(__name__)
 
 pytestmark = pytest.mark.sentry_metrics
 
-MESSAGE_PROCESSOR = MessageProcessor(
-    get_ingest_config(UseCaseKey.RELEASE_HEALTH, IndexerStorage.POSTGRES)
-)
+MESSAGE_PROCESSOR = MessageProcessor(UseCaseKey.RELEASE_HEALTH)
 
 
 def compare_messages_ignoring_mapping_metadata(actual: Message, expected: Message) -> None:
@@ -297,7 +295,7 @@ def test_process_messages() -> None:
 
 
 def test_transform_step() -> None:
-    config = get_ingest_config(UseCaseKey.RELEASE_HEALTH, IndexerStorage.POSTGRES)
+    config = get_ingest_config(UseCaseKey.RELEASE_HEALTH)
 
     message_payloads = [counter_payload, distribution_payload, set_payload]
 
@@ -475,14 +473,13 @@ def test_process_messages_rate_limited(caplog, settings) -> None:
     last = message_batch[-1]
     outer_message = Message(last.partition, last.offset, message_batch, last.timestamp)
 
-    message_processor = MessageProcessor(
-        get_ingest_config(UseCaseKey.RELEASE_HEALTH, IndexerStorage.MOCK)
-    )
+    from sentry.sentry_metrics.indexer import backend
+
     # Insert a None-value into the mock-indexer to simulate a rate-limit.
-    message_processor._indexer.indexer._strings[1]["rate_limited_test"] = None
+    backend.indexer._strings[1]["rate_limited_test"] = None
 
     with caplog.at_level(logging.ERROR):
-        new_batch = message_processor.process_messages(outer_message=outer_message)
+        new_batch = MESSAGE_PROCESSOR.process_messages(outer_message=outer_message)
 
     # we expect just the counter_payload msg to be left, as that one didn't
     # cause/depend on string writes that have been rate limited
