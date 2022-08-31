dfc76712ba65535b8de67787a6825951012ef549
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-08-31 15:55:59 -0400

    merge static/app/components/events/interfaces/spans/utils.spec.tsx utils

--- static/app/components/events/interfaces/spans/utils.spec.tsx
+++ static/app/components/events/interfaces/spans/utils.spec.tsx
@@ -1,4 +1,31 @@
-import {getCumulativeAlertLevelFromErrors} from 'sentry/components/events/interfaces/spans/utils';
+import {
+  getCumulativeAlertLevelFromErrors,
+  getFormattedTimeRangeWithLeadingZero,
+} from 'sentry/components/events/interfaces/spans/utils';
+
+describe('test utility functions', function () {
+  it('getFormattedTimeRangeWithLeadingZero', function () {
+    let result = getFormattedTimeRangeWithLeadingZero(
+      1658925888.601534,
+      1658925888.60193
+    );
+
+    expect(result.start).toEqual('1658925888.601534');
+    expect(result.end).toEqual('1658925888.060193');
+
+    result = getFormattedTimeRangeWithLeadingZero(1658925888.601534, 165892588.060193);
+    expect(result.start).toEqual('1658925888.601534');
+    expect(result.end).toEqual('0165892588.060193');
+
+    result = getFormattedTimeRangeWithLeadingZero(16589258.6015, 1658925888.060193);
+    expect(result.start).toEqual('0016589258.006015');
+    expect(result.end).toEqual('1658925888.060193');
+
+    result = getFormattedTimeRangeWithLeadingZero(1658925888.601534, 1658925888.601935);
+    expect(result.start).toEqual('1658925888.601534');
+    expect(result.end).toEqual('1658925888.601935');
+  });
+});
 
 describe('getCumulativeAlertLevelFromErrors', () => {
   it('returns undefined for an empty array', () => {
