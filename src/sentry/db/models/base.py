af27756d1d205ad3e51a8e2ad7922f2cf7607b1d
Ryan Skonnord <ryan.skonnord@sentry.io>
2022-08-31 14:06:59 -0700

    ref(hybrid): Standardize nomenclature around silos (#38277)
    Having previously wished to avoid committing to the term "silo" in
    production code and instead used the terms "server component" and
    "server mode", standardize naming of classes and variables on the terms
    "silo" and "silo mode".
    
    Also clean up some related names around silo mode decorators, and use
    the terms "Model" and "Endpoint" (in reference to the decorated classes)
    in preference to synonyms such as "data", "API", and "view".

--- src/sentry/db/models/base.py
+++ src/sentry/db/models/base.py
@@ -6,7 +6,7 @@ from django.db import models
 from django.db.models import signals
 from django.utils import timezone
 
-from sentry.servermode import ModeLimited, ServerComponentMode
+from sentry.silo import SiloLimit, SiloMode
 
 from .fields.bounded import BoundedBigAutoField
 from .manager import BaseManager, M
@@ -142,19 +142,14 @@ signals.post_save.connect(__model_post_save)
 signals.class_prepared.connect(__model_class_prepared)
 
 
-class ModelAvailableOn(ModeLimited):
+class ModelSiloLimit(SiloLimit):
     def __init__(
         self,
-        *modes: ServerComponentMode,
-        read_only: ServerComponentMode | Iterable[ServerComponentMode] = (),
+        *modes: SiloMode,
+        read_only: SiloMode | Iterable[SiloMode] = (),
     ) -> None:
         super().__init__(*modes)
-        self.read_only = frozenset(
-            [read_only] if isinstance(read_only, ServerComponentMode) else read_only
-        )
-
-    class DataAvailabilityError(Exception):
-        pass
+        self.read_only = frozenset([read_only] if isinstance(read_only, SiloMode) else read_only)
 
     @staticmethod
     def _recover_model_name(obj: Any) -> str | None:
@@ -169,8 +164,8 @@ class ModelAvailableOn(ModeLimited):
     def handle_when_unavailable(
         self,
         original_method: Callable[..., Any],
-        current_mode: ServerComponentMode,
-        available_modes: Iterable[ServerComponentMode],
+        current_mode: SiloMode,
+        available_modes: Iterable[SiloMode],
     ) -> Callable[..., Any]:
         def handle(obj: Any, *args: Any, **kwargs: Any) -> None:
             model_name = self._recover_model_name(obj)
@@ -180,16 +175,16 @@ class ModelAvailableOn(ModeLimited):
                 f"Called `{method_name}` on server in {current_mode} mode. "
                 f"{model_name or 'The model'} is available only in: {mode_str}"
             )
-            raise self.DataAvailabilityError(message)
+            raise self.AvailabilityError(message)
 
         return handle
 
     def __call__(self, model_class: Any) -> type:
         if not (isinstance(model_class, type) and issubclass(model_class, BaseModel)):
-            raise TypeError("`@ModelAvailableOn ` must decorate a Model class")
+            raise TypeError("`@ModelSiloLimit ` must decorate a Model class")
         assert isinstance(model_class.objects, BaseManager)
 
-        model_class.objects = model_class.objects.create_mode_limited_copy(self, self.read_only)
+        model_class.objects = model_class.objects.create_silo_limited_copy(self, self.read_only)
 
         # On the model (not manager) class itself, find all methods that are tagged
         # with the `alters_data` meta-attribute and replace them with overrides.
@@ -206,12 +201,10 @@ class ModelAvailableOn(ModeLimited):
 
         # For internal tooling only. Having any production logic depend on this is
         # strongly discouraged.
-        model_class._meta.__mode_limit = self
+        model_class._meta.__silo_limit = self
 
         return model_class
 
 
-control_silo_model = ModelAvailableOn(
-    ServerComponentMode.CONTROL, read_only=ServerComponentMode.CUSTOMER
-)
-customer_silo_model = ModelAvailableOn(ServerComponentMode.CUSTOMER)
+control_silo_model = ModelSiloLimit(SiloMode.CONTROL, read_only=SiloMode.CUSTOMER)
+customer_silo_model = ModelSiloLimit(SiloMode.CUSTOMER)
