16fa07fb51d5379379217d0596c37ed97b0bc228
Scott Cooper <scttcper@gmail.com>
2022-09-01 11:47:51 -0700

    test(ui): Convert project install tests to RTL

--- static/app/views/projectInstall/newProject.spec.jsx
+++ static/app/views/projectInstall/newProject.spec.jsx
@@ -1,19 +1,12 @@
-import {mountWithTheme} from 'sentry-test/enzyme';
 import {initializeOrg} from 'sentry-test/initializeOrg';
+import {render} from 'sentry-test/reactTestingLibrary';
 
-import {Client} from 'sentry/api';
 import NewProject from 'sentry/views/projectInstall/newProject';
 
 describe('NewProjectPlatform', function () {
-  beforeEach(function () {
-    this.stubbedApiRequest = jest.spyOn(Client.prototype, 'request');
-  });
-
-  describe('render()', function () {
-    it('should render', function () {
-      const {routerContext} = initializeOrg();
-      const wrapper = mountWithTheme(<NewProject />, routerContext);
-      expect(wrapper).toSnapshot();
-    });
+  it('should render', function () {
+    const {routerContext} = initializeOrg();
+    const wrapper = render(<NewProject />, {context: routerContext});
+    expect(wrapper.container).toSnapshot();
   });
 });
