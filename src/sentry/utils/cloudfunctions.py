2cafaa1c9996d63fcd7043df206279768d88f5e5
vpandiarajan20 <vpandiarajan20@gmail.com>
2022-09-01 13:36:05 -0700

    add GCP monitoring

--- src/sentry/utils/cloudfunctions.py
+++ src/sentry/utils/cloudfunctions.py
@@ -18,51 +18,39 @@ from google.protobuf.field_mask_pb2 import FieldMask
 from sentry.utils import json
 
 WRAPPER_JS = """
+const Sentry = require('@sentry/serverless');
 const userFunc = require('./function.js');
-Object.assign(process.env, require('./env.json'));
-const Sentry = require("@sentry/node");
-const Tracing = require("@sentry/tracing");
-exports.start = async (message, context) => {
-  if (process.env.SENTRY_DSN) {
-    Sentry.init({
-      dsn: process.env.SENTRY_DSN,
-      integrations: [
-        new Sentry.Integrations.Http({ tracing: true }),
-      ],
-      tracesSampleRate: 1.0,
-    });
-  }
-  if (!userFunc) {
-    console.error("Your code needs to export a function. module.export = () => {}");
-    return;
-  }
-  if (typeof userFunc !== 'function') {
-    console.error("Your code needs to export a function. Instead, " + typeof userFunc + " was exported.");
-    return;
-  }
-  const event = JSON.parse(Buffer.from(message.data, 'base64').toString());
+
+if (process.env.SENTRY_DSN) {
+  Sentry.GCPFunction.init({
+    dsn: process.env.SENTRY_DSN,
+    tracesSampleRate: 1.0,
+  });
+}
+exports.start = Sentry.GCPFunction.wrapCloudEventFunction((context, callback) => {
+  const event = JSON.parse(Buffer.from(context.data, 'base64').toString());
   if (process.env.SENTRY_DSN) {
     const transaction = Sentry.startTransaction({
-        op: "func",
-        name: "Sentry Function Transaction",
+      op: 'faas',
+      name: 'Sentry Function Transaction',
     });
     try {
-        await userFunc(event.data, event.type);
+      userFunc(event.data, event.type);
     } catch (e) {
-        Sentry.captureException(e);
+      Sentry.captureException(e);
     } finally {
-        transaction.finish();
+      transaction.finish();
+      Sentry.flush();
     }
   } else {
-      await userFunc(event.data, event.type);
+    userFunc(event.data, event.type);
   }
-};
+});
 """
 
 PACKAGE_JSON = {
     "dependencies": {
-        "@sentry/node": "^6.11.0",
-        "@sentry/tracing": "^6.11.0",
+        "@sentry/serverless": "^5.26.0",
         "node-fetch": "^2.6.1",
     }
 }
@@ -92,13 +80,12 @@ def create_function_pubsub_topic(funcId):
     publisher.create_topic(name=function_pubsub_name(funcId))
 
 
-def upload_function_files(client, code, env_variables):
+def upload_function_files(client, code):
     f = BytesIO()
     with ZipFile(f, "w") as codezip:
+        codezip.writestr("index.js", WRAPPER_JS)
         codezip.writestr("function.js", code)
-        # codezip.writestr("index.js", WRAPPER_JS)
         codezip.writestr("package.json", json.dumps(PACKAGE_JSON))
-        codezip.writestr("env.json", json.dumps(env_variables))
     f.seek(0)
 
     logger = logging.getLogger("sentry.functions")
@@ -137,7 +124,7 @@ def update_function(code, funcId, description, env_variables):
 
 
 def subcreate_function(client, code, funcId, description, env_variables):
-    upload_url = upload_function_files(client, code, env_variables)
+    upload_url = upload_function_files(client, code)
     return CloudFunction(
         name=project_location_function_name(
             include_proj=True, include_loc=True, include_func=True, funcId=funcId
@@ -145,7 +132,7 @@ def subcreate_function(client, code, funcId, description, env_variables):
         description=description,
         source_upload_url=upload_url,
         runtime="nodejs16",
-        entry_point="yourFunction",
+        entry_point="start",
         event_trigger=EventTrigger(
             event_type="providers/cloud.pubsub/eventTypes/topic.publish",
             resource=function_pubsub_name(funcId),
