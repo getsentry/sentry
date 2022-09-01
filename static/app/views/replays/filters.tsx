db2f033ad294dfc4419090ded9e6b7cee4a64ae9
Dane Grant <danecando@gmail.com>
2022-09-01 15:03:50 -0400

    fix(replays): use custom search input with defined tags

--- static/app/views/replays/filters.tsx
+++ static/app/views/replays/filters.tsx
@@ -2,12 +2,13 @@ import styled from '@emotion/styled';
 
 import DatePageFilter from 'sentry/components/datePageFilter';
 import EnvironmentPageFilter from 'sentry/components/environmentPageFilter';
-import SearchBar from 'sentry/components/events/searchBar';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import ProjectPageFilter from 'sentry/components/projectPageFilter';
 import {t} from 'sentry/locale';
 import space from 'sentry/styles/space';
 import {Organization} from 'sentry/types';
+
+import SearchBar from './searchBar';
 
 type Props = {
   handleSearchQuery: (query: string) => void;
@@ -29,7 +30,6 @@ function ReplaysFilters({organization, handleSearchQuery, query}: Props) {
         query={query}
         placeholder={t('Search')}
         onSearch={handleSearchQuery}
-        omitTags={OMITTED_TAGS}
       />
     </FilterContainer>
   );
@@ -48,88 +48,3 @@ const FilterContainer = styled('div')`
 `;
 
 export default ReplaysFilters;
-
-const OMITTED_TAGS = [
-  'culprit',
-  'device.arch',
-  'device.battery_level',
-  'device.charging',
-  'device.locale',
-  'device.online',
-  'device.orientation',
-  'device.simulator',
-  'device.uuid',
-  'error',
-  'error.handled',
-  'error.mechanism',
-  'error.type',
-  'error.unhandled',
-  'error.value',
-  'event.type',
-  'geo',
-  'geo.city',
-  'geo.country_code',
-  'geo.region',
-  'has',
-  'http',
-  'http.method',
-  'http.referer',
-  'http.url',
-  'id',
-  'issue',
-  'level',
-  'location',
-  'measurements',
-  'measurements.app_start_cold',
-  'measurements.app_start_warm',
-  'measurements.cls',
-  'measurements.fcp',
-  'measurements.fid',
-  'measurements.fp',
-  'measurements.frames_frozen',
-  'measurements.frames_frozen_rate',
-  'measurements.frames_slow',
-  'measurements.frames_slow_rate',
-  'measurements.frames_total',
-  'measurements.lcp',
-  'measurements.stall_count',
-  'measurements.stall_longest_time',
-  'measurements.stall_percentage',
-  'measurements.stall_total_time',
-  'measurements.ttfb',
-  'measurements.ttfb.requesttime',
-  'message',
-  'platform',
-  'project',
-  'release.build',
-  'release.package',
-  'release.stage',
-  'release.version',
-  'timestamp',
-  'timestamp.to_day',
-  'timestamp.to_hour',
-  'spans',
-  'spans.browser',
-  'spans.db',
-  'spans.http',
-  'spans.resource',
-  'spans.ui',
-  'stack',
-  'stack.abs_path',
-  'stack.colno',
-  'stack.filename',
-  'stack.function',
-  'stack.in_app',
-  'stack.lineno',
-  'stack.module',
-  'stack.package',
-  'stack.stack_level',
-  'title',
-  'trace',
-  'trace.parent_span',
-  'trace.span',
-  'transaction',
-  'transaction.duration',
-  'transaction.op',
-  'transaction.status',
-];
---
+++ static/app/views/replays/searchBar.tsx
@@ -0,0 +1,144 @@
+import {useEffect} from 'react';
+import assign from 'lodash/assign';
+import flatten from 'lodash/flatten';
+import memoize from 'lodash/memoize';
+import omit from 'lodash/omit';
+
+import {fetchTagValues} from 'sentry/actionCreators/tags';
+import SmartSearchBar from 'sentry/components/smartSearchBar';
+import {NEGATION_OPERATOR, SEARCH_WILDCARD} from 'sentry/constants';
+import {Organization, TagCollection} from 'sentry/types';
+import {defined} from 'sentry/utils';
+import {FIELD_TAGS, isAggregateField} from 'sentry/utils/discover/fields';
+import {FieldKind} from 'sentry/utils/fields';
+import useApi from 'sentry/utils/useApi';
+import withTags from 'sentry/utils/withTags';
+
+const SEARCH_SPECIAL_CHARS_REGEXP = new RegExp(
+  `^${NEGATION_OPERATOR}|\\${SEARCH_WILDCARD}`,
+  'g'
+);
+
+const REPLAY_TAGS = [
+  'device.brand',
+  'device.family',
+  'device.name',
+  'dist',
+  'os.build',
+  'os.kernel_version',
+  'platform.name',
+  'release',
+  'sdk.name',
+  'sdk.version',
+  'user.display',
+  'user.email',
+  'user.id',
+  'user.ip',
+  'user.username',
+];
+
+const getReplayTags = () =>
+  Object.fromEntries(
+    REPLAY_TAGS.map(key => [
+      key,
+      {
+        ...FIELD_TAGS[key],
+        kind: FieldKind.FIELD,
+      },
+    ])
+  );
+
+export type SearchBarProps = Omit<React.ComponentProps<typeof SmartSearchBar>, 'tags'> & {
+  organization: Organization;
+  tags: TagCollection;
+  /**
+   * Used to define the max height of the menu in px.
+   */
+  maxMenuHeight?: number;
+  maxSearchItems?: React.ComponentProps<typeof SmartSearchBar>['maxSearchItems'];
+  omitTags?: string[];
+  projectIds?: number[] | Readonly<number[]>;
+};
+
+function SearchBar(props: SearchBarProps) {
+  const {maxSearchItems, organization, tags, omitTags, projectIds, maxMenuHeight} = props;
+
+  const api = useApi();
+
+  useEffect(() => {
+    // Clear memoized data on mount to make tests more consistent.
+    getEventFieldValues.cache.clear?.();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [projectIds]);
+
+  // Returns array of tag values that substring match `query`; invokes `callback`
+  // with data when ready
+  const getEventFieldValues = memoize(
+    (tag, query, endpointParams): Promise<string[]> => {
+      const projectIdStrings = (projectIds as Readonly<number>[])?.map(String);
+
+      if (isAggregateField(tag.key)) {
+        // We can't really auto suggest values for aggregate fields
+        // or measurements, so we simply don't
+        return Promise.resolve([]);
+      }
+
+      return fetchTagValues(
+        api,
+        organization.slug,
+        tag.key,
+        query,
+        projectIdStrings,
+        endpointParams
+      ).then(
+        results =>
+          flatten(results.filter(({name}) => defined(name)).map(({name}) => name)),
+        () => {
+          throw new Error('Unable to fetch event field values');
+        }
+      );
+    },
+    ({key}, query) => `${key}-${query}`
+  );
+
+  const getTagList = () => {
+    const replayTags = getReplayTags();
+
+    const tagsWithKind = Object.fromEntries(
+      Object.keys(tags).map(key => [
+        key,
+        {
+          ...tags[key],
+          kind: FieldKind.TAG,
+        },
+      ])
+    );
+
+    const combinedTags: TagCollection = assign({}, tagsWithKind, replayTags);
+
+    const sortedTagKeys = Object.keys(combinedTags);
+    sortedTagKeys.sort((a, b) => {
+      return a.toLowerCase().localeCompare(b.toLowerCase());
+    });
+
+    return omit(combinedTags, omitTags ?? []);
+  };
+
+  return (
+    <SmartSearchBar
+      hasRecentSearches
+      onGetTagValues={getEventFieldValues}
+      supportedTags={getTagList()}
+      prepareQuery={query => {
+        // Prepare query string (e.g. strip special characters like negation operator)
+        return query.replace(SEARCH_SPECIAL_CHARS_REGEXP, '');
+      }}
+      maxSearchItems={maxSearchItems}
+      excludeEnvironment
+      maxMenuHeight={maxMenuHeight ?? 300}
+      {...props}
+    />
+  );
+}
+
+export default withTags(SearchBar);
