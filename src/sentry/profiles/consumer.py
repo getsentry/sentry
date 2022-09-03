f508cadcbf982fda73fcded195176196e918c32c
Pierre Massat <pierre.massat@sentry.io>
2022-09-03 10:26:51 -0400

    Fix return type

--- src/sentry/profiles/consumer.py
+++ src/sentry/profiles/consumer.py
@@ -23,7 +23,7 @@ def get_profiles_consumer(
 class ProfilesConsumer(AbstractBatchWorker):  # type: ignore
     def process_message(
         self, message: Message
-    ) -> Tuple[Optional[int], Optional[MutableMapping[str, Any]]]:
+    ) -> Optional[Tuple[Optional[int], Optional[MutableMapping[str, Any]]]]:
         message = msgpack.unpackb(message.value(), use_list=False)
         if not message["payload"]:
             return None
