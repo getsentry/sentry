68b4e5dc78784983110e64483a74e2ae6e40f6b7
Malachi Willey <malwilley@gmail.com>
2022-09-06 15:02:33 -0500

    types(perf-issues): Add frontend types for issue category and type (#38480)
    Matches what was done here: https://github.com/getsentry/sentry/pull/38373

--- static/app/types/group.tsx
+++ static/app/types/group.tsx
@@ -42,6 +42,16 @@ export enum SavedSearchType {
   SESSION = 2,
 }
 
+export enum IssueCategory {
+  PERFORMANCE = 'performance',
+  ERROR = 'error',
+}
+
+export enum IssueType {
+  ERROR = 'error',
+  PERFORMANCE_N_PLUS_ONE = 'performance_n_plus_one',
+}
+
 // endpoint: /api/0/issues/:issueId/attachments/?limit=50
 export type IssueAttachment = {
   dateCreated: string;
@@ -423,6 +433,8 @@ export interface BaseGroup extends GroupRelease {
   isPublic: boolean;
   isSubscribed: boolean;
   isUnhandled: boolean;
+  issueCategory: IssueCategory;
+  issueType: IssueType;
   lastSeen: string;
   latestEvent: Event;
   level: Level;
