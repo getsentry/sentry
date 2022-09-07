d667f8361c0ad9dcf6dddeedd994a24ed9375e2b
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-09-07 10:36:07 -0600

    feat(perf-issues): Add 'issueSupports' helper (#38518)
    Co-authored-by: George Gritsouk <989898+gggritso@users.noreply.github.com>

--- static/app/types/group.tsx
+++ static/app/types/group.tsx
@@ -52,6 +52,29 @@ export enum IssueType {
   PERFORMANCE_N_PLUS_ONE = 'performance_n_plus_one',
 }
 
+/**
+ * Defines what capabilities a category of issue has. Not all categories of
+ * issues work the same.
+ */
+export type IssueCategoryCapabilities = {
+  /**
+   * Can the issue be deleted
+   */
+  delete: boolean;
+  /**
+   * Can the issue be deleted and discarded
+   */
+  deleteAndDiscard: boolean;
+  /**
+   * Can the issue be ignored
+   */
+  ignore: boolean;
+  /**
+   * Can the issue be merged
+   */
+  merge: boolean;
+};
+
 // endpoint: /api/0/issues/:issueId/attachments/?limit=50
 export type IssueAttachment = {
   dateCreated: string;
