d2ba1d8fb67220a2fca07f40b9ae254a4fb53a2c
Ash Anand <0Calories@users.noreply.github.com>
2022-09-07 16:16:20 -0400

    ref(perf-issues): Disable delete and discard & delete options in perf issues (#38488)

--- static/app/views/organizationGroupDetails/actions/actions.spec.tsx
+++ static/app/views/organizationGroupDetails/actions/actions.spec.tsx
@@ -2,12 +2,14 @@ import {render, screen, userEvent, waitFor} from 'sentry-test/reactTestingLibrar
 
 import ConfigStore from 'sentry/stores/configStore';
 import ModalStore from 'sentry/stores/modalStore';
+import {IssueCategory} from 'sentry/types';
 import GroupActions from 'sentry/views/organizationGroupDetails/actions';
 
 const group = TestStubs.Group({
   id: '1337',
   pluginActions: [],
   pluginIssues: [],
+  issueCategory: IssueCategory.ERROR,
 });
 
 const project = TestStubs.ProjectDetails({
