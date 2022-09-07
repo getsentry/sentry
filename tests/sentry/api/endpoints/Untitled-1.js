7da2cb74e55a8a22d824bd778ade46ba9f783b62
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-09-07 11:47:33 +0200

    fix tests

---
+++ tests/sentry/api/endpoints/Untitled-1.js
@@ -0,0 +1,3 @@
+{
+    'sentry:symbol_sources': [{'id': '4d5b27ba-73c5-434c-9867-c27eac74fa09', 'name': 'honk source', 'layout': {'type': 'native'}, 'filetypes': ['pe'], 'type': 'http', 'url': 'http://honk.beep', 'username': 'honkhonk', 'password': {'hidden-secret': True}}]}
+    {'sentry:symbol_sources': [{'id': '4d5b27ba-73c5-434c-9867-c27eac74fa09', 'name': 'honk source', 'layout': {'type': 'native'}, 'filetypes': ['pe'], 'type': 'http', 'url': 'http://honk.beep', 'username': 'honkhonk', 'password': {'hidden-secret': True}}], 'id': 41, 'slug': 'bar', 'name': 'Bar', 'status': 0, 'public': False}
