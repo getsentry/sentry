beb1f2cb2f5c2713a6d63a94a17d3f951528c692
Dameli U <dameli.ushbayeva@sentry.io>
2022-09-07 16:12:34 -0400

    fix for sure this time

--- tests/sentry/performance_issues/test_performance_issues.py
+++ tests/sentry/performance_issues/test_performance_issues.py
@@ -51,14 +51,14 @@ class EventManagerTest(TestCase, EventManagerTestMixin):
             group = event.groups[0]
             assert (
                 group.title
-                == "N+1 Query:SELECT `books_author`.`id`, `books_author`.`name` FROM `books_author` WHERE `books_author`.`id` = %s LIMIT 10"
+                == "N+1 Query:SELECT `books_book`.`id`, `books_book`.`title`, `books_book`.`author_id` FROM `books_book` LIMIT 10"
             )
             assert group.message == "/books/"
             assert group.culprit == "/books/"
             assert group.get_event_type() == "transaction"
             assert group.get_event_metadata() == {
                 "location": "/books/",
-                "title": "N+1 Query:SELECT `books_author`.`id`, `books_author`.`name` FROM `books_author` WHERE `books_author`.`id` = %s LIMIT 10",
+                "title": "N+1 Query:SELECT `books_book`.`id`, `books_book`.`title`, `books_book`.`author_id` FROM `books_book` LIMIT 10",
             }
             assert group.location() == "/books/"
             assert group.level == 40
