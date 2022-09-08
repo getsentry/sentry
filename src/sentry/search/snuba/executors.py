4840917bf220298208658e8e7f297156a495db42
Dan Fuller <dfuller@sentry.io>
2022-09-08 09:48:41 -0700

    word better

--- src/sentry/search/snuba/executors.py
+++ src/sentry/search/snuba/executors.py
@@ -348,9 +348,9 @@ class PostgresSnubaQueryExecutor(AbstractQueryExecutor):
 
         if not end:
             end = now + ALLOWED_FUTURE_DELTA
-            allow_issue_only_search = True
+            allow_postgres_only_search = True
         else:
-            allow_issue_only_search = features.has(
+            allow_postgres_only_search = features.has(
                 "organizations:issue-search-allow-postgres-only-search", projects[0].organization
             )
 
@@ -380,7 +380,7 @@ class PostgresSnubaQueryExecutor(AbstractQueryExecutor):
         # are no other Snuba-based search predicates, we can simply
         # return the results from Postgres.
         if (
-            allow_issue_only_search
+            allow_postgres_only_search
             and cursor is None
             and sort_by == "date"
             and
