b2df72ff0ffee0f26f5be614c7ea92c8430184ae
Kev <6111995+k-fish@users.noreply.github.com>
2022-09-08 12:10:46 -0400

    feat(perf-issues): Add non-flagr feature flag for ingest (#38559)
    ### Summary
    This splits out the creation/ingest portion of performance issues into a
    non-flagr feature flag, since we don't want to use flagr in the ingest
    pipeline for speed / stability purposes. This feature exists for the
    final internal / EA / GA rollout, and when an organization is opted in
    via a separate getsentry PR they will start having performance issues
    created immediately (since we are defaulting performance issue creation
    to 'On' for rollout, given it shouldn't have a cost impact).

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1143,6 +1143,8 @@ SENTRY_FEATURES = {
     "organizations:native-stack-trace-v2": False,
     # Enable performance issues
     "organizations:performance-issues": False,
+    # Enable the creation of performance issues in the ingest pipeline. Turning this on will eventually make performance issues be created with default settings.
+    "organizations:performance-issues-ingest": False,
     # Enable version 2 of reprocessing (completely distinct from v1)
     "organizations:reprocessing-v2": False,
     # Enable the UI for the overage alert settings
