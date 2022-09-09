31f2a9dc433895c0b1c39292708042b7ad4fa609
Dameli U <dameli.ushbayeva@sentry.io>
2022-09-09 12:49:24 -0400

    do not include span description in group title

--- tests/sentry/performance_issues/test_performance_issues.py
+++ tests/sentry/performance_issues/test_performance_issues.py
@@ -61,8 +61,7 @@ class EventManagerTest(TestCase, EventManagerTestMixin):
             assert group.get_event_type() == "transaction"
             assert group.get_event_metadata() == {
                 "location": "/books/",
-                "title": "N+1 Query: SELECT `books_author`.`id`, `books_author`.`name` FROM `books_author` WHERE `books_author`.`id` = %s LIMIT 21",
-                "type": "N+1 Query",
+                "title": "N+1 Query",
                 "value": "SELECT `books_author`.`id`, `books_author`.`name` FROM `books_author` WHERE `books_author`.`id` = %s LIMIT 21",
             }
             assert group.location() == "/books/"
@@ -107,8 +106,7 @@ class EventManagerTest(TestCase, EventManagerTestMixin):
             )
             assert group.get_event_metadata() == {
                 "location": "/books/",
-                "title": "N+1 Query: SELECT `books_author`.`id`, `books_author`.`name` FROM `books_author` WHERE `books_author`.`id` = %s LIMIT 21",
-                "type": "N+1 Query",
+                "title": "N+1 Query",
                 "value": "SELECT `books_author`.`id`, `books_author`.`name` FROM `books_author` WHERE `books_author`.`id` = %s LIMIT 21",
             }
             assert group.location() == "/books/"
