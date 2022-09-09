5e44c42149f60c5e309b21080fe06425d9f2bd5d
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-09-09 16:06:51 -0400

    ref(js): Slightly more obvious constants on ignore (#38648)

--- static/app/components/actions/ignore.tsx
+++ static/app/components/actions/ignore.tsx
@@ -20,12 +20,25 @@ import {
 } from 'sentry/types';
 import {getIssueCapability} from 'sentry/utils/groupCapabilities';
 
-const IGNORE_DURATIONS = [30, 120, 360, 60 * 24, 60 * 24 * 7];
+const ONE_HOUR = 60;
+
+/**
+ * Ignore durations are in munutes
+ */
+const IGNORE_DURATIONS = [
+  ONE_HOUR / 2,
+  ONE_HOUR * 2,
+  ONE_HOUR * 6,
+  ONE_HOUR * 24,
+  ONE_HOUR * 24 * 7,
+];
+
 const IGNORE_COUNTS = [1, 10, 100, 1000, 10000, 100000];
+
 const IGNORE_WINDOWS: SelectValue<number>[] = [
-  {value: 60, label: t('per hour')},
-  {value: 24 * 60, label: t('per day')},
-  {value: 24 * 7 * 60, label: t('per week')},
+  {value: ONE_HOUR, label: t('per hour')},
+  {value: ONE_HOUR * 24, label: t('per day')},
+  {value: ONE_HOUR * 24 * 7, label: t('per week')},
 ];
 
 type Props = {
