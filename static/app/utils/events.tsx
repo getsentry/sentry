dc0348fb7f63c31246c3869950a464e53c98e4f5
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-09-09 13:19:02 -0400

    fix tsc

--- static/app/utils/events.tsx
+++ static/app/utils/events.tsx
@@ -166,6 +166,7 @@ export function getTitle(
       return {
         title: customTitle ?? title,
         subtitle: event.issueCategory === 'performance' ? culprit : '',
+        treeLabel: undefined,
       };
     default:
       return {
