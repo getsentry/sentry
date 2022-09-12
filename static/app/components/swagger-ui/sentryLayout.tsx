cd80ac9c5edd76347a3080f2a95b868c6b744695
Dominik Buszowiecki <DominikB2014@gmail.com>
2022-09-12 14:09:19 -0400

    remove sidebar, remove unneccessary code

--- static/app/components/swagger-ui/sentryLayout.tsx
+++ static/app/components/swagger-ui/sentryLayout.tsx
@@ -1,8 +1,4 @@
-import {useState} from 'react';
-
-import DocsSelector, {Docs} from './docs/docsSelector';
 import SentrySwaggerHeader from './sentrySwaggerHeader';
-import SentrySwaggerMenu, {MenuItem} from './sentrySwaggerMenu';
 
 type SentryLayoutProps = {
   getComponent: Function;
@@ -10,48 +6,12 @@ type SentryLayoutProps = {
 
 const SentryLayout = ({getComponent}: SentryLayoutProps) => {
   const BaseLayout = getComponent('BaseLayout', true);
-  const HighlightCode = getComponent('highlightCode');
-
-  const [selectedMenuItem, setSelectedMenuItem] = useState<Docs | string>(
-    'API REFERENCE'
-  );
-
-  const menuItems: MenuItem[] = [
-    {
-      title: 'API REFERENCE',
-    },
-    {
-      title: Docs.Auth,
-      href: '/api/auth/',
-    },
-    {
-      title: Docs.Pagination,
-      href: '/api/pagination/',
-    },
-    {
-      title: Docs.Permissions,
-      href: '/api/pagination/',
-    },
-    {
-      title: Docs.RateLimits,
-      href: '/api/pagination/',
-    },
-    {
-      title: Docs.Request,
-      href: '/api/pagination/',
-    },
-  ];
-
-  const handleMenuClick = (item: MenuItem) => {
-    setSelectedMenuItem(item.title);
-  };
 
   return (
     <div className="swagger-ui-sentry">
       <div className="document-wrapper">
         <div className="sidebar">
           <SentrySwaggerHeader />
-          <SentrySwaggerMenu menuItems={menuItems} onMenuItemClick={handleMenuClick} />
         </div>
 
         <main role="main">
@@ -59,17 +19,11 @@ const SentryLayout = ({getComponent}: SentryLayoutProps) => {
             <div className="navbar-right-half">
               <div className="global-header" />
             </div>
-            <div className="content">
-              {selectedMenuItem !== 'API REFERENCE' && (
-                <DocsSelector
-                  docName={selectedMenuItem as Docs}
-                  HighlightCode={HighlightCode}
-                />
-              )}
-              {selectedMenuItem === 'API REFERENCE' && <BaseLayout />}
-            </div>
           </div>
         </main>
+        <div className="content">
+          <BaseLayout />
+        </div>
       </div>
     </div>
   );
