6d2561f815a33784e41f410b9ebbb2a8914292fb
Alberto Leal <mail4alberto@gmail.com>
2022-09-12 17:09:59 -0400

    fix(hybrid-cloud): For invalid subdomains, redirect to url-prefix instead of passthrough

--- tests/sentry/middleware/test_subdomain.py
+++ tests/sentry/middleware/test_subdomain.py
@@ -1,7 +1,14 @@
-from django.test import RequestFactory
+from django.conf import settings
+from django.conf.urls import url
+from django.http import HttpResponseRedirect
+from django.test import RequestFactory, override_settings
+from django.urls import reverse
+from rest_framework.permissions import AllowAny
+from rest_framework.response import Response
 
+from sentry.api.base import Endpoint
 from sentry.middleware.subdomain import SubdomainMiddleware
-from sentry.testutils import TestCase
+from sentry.testutils import APITestCase, TestCase
 
 
 class SubdomainMiddlewareTest(TestCase):
@@ -25,7 +32,9 @@ class SubdomainMiddlewareTest(TestCase):
             assert request_with_host("foo.bar.us.dev.getsentry.net:8000").subdomain == "foo.bar"
             assert request_with_host("foo.BAR.us.dev.getsentry.net:8000").subdomain == "foo.bar"
             # Invalid subdomain according to RFC 1034/1035.
-            assert request_with_host("_smtp._tcp.us.dev.getsentry.net:8000").subdomain is None
+            assert isinstance(
+                request_with_host("_smtp._tcp.us.dev.getsentry.net:8000"), HttpResponseRedirect
+            )
 
         with self.options({}):
             assert request_with_host("foobar").subdomain is None
@@ -33,4 +42,57 @@ class SubdomainMiddlewareTest(TestCase):
             assert request_with_host("us.dev.getsentry.net:8000").subdomain is None
             assert request_with_host("foobar.us.dev.getsentry.net:8000").subdomain is None
             assert request_with_host("foo.bar.us.dev.getsentry.net:8000").subdomain is None
-            assert request_with_host("_smtp._tcp.us.dev.getsentry.net:8000").subdomain is None
+            assert isinstance(
+                request_with_host("_smtp._tcp.us.dev.getsentry.net:8000"), HttpResponseRedirect
+            )
+
+
+class APITestEndpoint(Endpoint):
+    permission_classes = (AllowAny,)
+
+    def get(self, request):
+        # return HttpResponse(status=status.HTTP_200_OK)
+        return Response(
+            {
+                "subdomain": request.subdomain,
+            }
+        )
+
+
+urlpatterns = [
+    url(
+        r"^api/0/test/$",
+        APITestEndpoint.as_view(),
+        name="test-endpoint",
+    ),
+]
+
+
+@override_settings(
+    ROOT_URLCONF=__name__,
+    SENTRY_SELF_HOSTED=False,
+)
+class End2EndTest(APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.middleware = settings.MIDDLEWARE
+
+    def test_with_middleware_no_customer_domain(self):
+        self.create_organization(name="albertos-apples")
+
+        response = self.client.get(
+            reverse("test-endpoint"),
+            HTTP_HOST="albertos-apples.testserver",
+        )
+        assert response.status_code == 200
+        assert response.data == {
+            "subdomain": "albertos-apples",
+        }
+
+        response = self.client.get(
+            reverse("test-endpoint"),
+            SERVER_NAME="albertos_apples.testserver",
+            follow=True,
+        )
+        assert response.status_code == 200
+        assert response.redirect_chain == [("http://testserver", 302)]
