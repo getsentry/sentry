8bb423b52ebba5bfe988a8730e643c91604cd8b4
Alberto Leal <mail4alberto@gmail.com>
2022-09-12 18:48:53 -0400

    feat(hybrid-cloud): Update create new organization button

---
+++ static/app/utils/useResolveRoute.tsx
@@ -0,0 +1,41 @@
+import ConfigStore from 'sentry/stores/configStore';
+import {OrganizationSummary} from 'sentry/types';
+import useOrganization from 'sentry/utils/useOrganization';
+
+import shouldUseLegacyRoute from './shouldUseLegacyRoute';
+
+/**
+ * If organization is passed, then a URL with the route will be returned with the customer domain prefix attached if the
+ * organization has customer domain feature enabled.
+ * Otherwise, if the organization is not given, then if the current organization has customer domain enabled, then we
+ * use the sentry URL as the prefix.
+ */
+function useResolveRoute(route: string, organization?: OrganizationSummary) {
+  const {sentryUrl} = ConfigStore.get('links');
+  const currentOrganization = useOrganization();
+  const hasCustomerDomain = currentOrganization.features.includes('customer-domains');
+
+  if (!organization) {
+    if (hasCustomerDomain) {
+      return `${sentryUrl}${route}`;
+    }
+    return route;
+  }
+
+  const {links} = organization;
+  const {organizationUrl} = links;
+
+  const useLegacyRoute = shouldUseLegacyRoute(organization);
+  if (useLegacyRoute) {
+    if (hasCustomerDomain) {
+      // If the current org is a customer domain, then we need to change the hostname in addition to
+      // updating the path.
+
+      return `${sentryUrl}${route}`;
+    }
+    return route;
+  }
+  return `${organizationUrl}${route}`;
+}
+
+export default useResolveRoute;
