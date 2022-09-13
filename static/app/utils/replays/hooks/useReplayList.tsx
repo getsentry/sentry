876bc3c883a530fad9306761051e4397b2c1c590
Dane Grant <danecando@gmail.com>
2022-09-13 09:33:05 -0400

    fix(replays): only run query when search has changed

--- static/app/utils/replays/hooks/useReplayList.tsx
+++ static/app/utils/replays/hooks/useReplayList.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useState} from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
 
 import type {Organization} from 'sentry/types';
 import EventView from 'sentry/utils/discover/eventView';
@@ -19,6 +19,7 @@ type Result = State;
 function useReplayList({eventView, organization}: Options): Result {
   const api = useApi();
   const location = useLocation<ReplayListLocationQuery>();
+  const querySearchRef = useRef<string>();
 
   const [data, setData] = useState<State>({
     fetchError: undefined,
@@ -27,14 +28,26 @@ function useReplayList({eventView, organization}: Options): Result {
     replays: [],
   });
 
-  useEffect(() => {
-    fetchReplayList({
-      api,
-      organization,
-      location,
-      eventView,
-    }).then(setData);
+  const init = useCallback(async () => {
+    if (!querySearchRef.current || querySearchRef.current !== location.search) {
+      setData(prev => ({
+        ...prev,
+        isFetching: true,
+      }));
+      const response = await fetchReplayList({
+        api,
+        organization,
+        location,
+        eventView,
+      });
+      setData(response);
+      querySearchRef.current = location.search;
+    }
   }, [api, organization, location, eventView]);
+
+  useEffect(() => {
+    init();
+  }, [init]);
 
   return data;
 }
