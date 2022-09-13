2fe7edd12dbdeabe345050f698a8cb980a1a2616
Colleen O'Rourke <colleen@sentry.io>
2022-09-13 14:33:23 -0700

    feat(perf issues): Snuba search for errors + perf issues (#38595)
    * Search across the errors and transaction datasets in Snuba to return
    both types of issues.
    
    Co-authored-by: Dan Fuller <dfuller@sentry.io>

--- src/sentry/utils/snuba.py
+++ src/sentry/utils/snuba.py
@@ -1025,7 +1025,7 @@ def resolve_column(dataset) -> Callable[[str], str]:
 
         # Some dataset specific logic:
         if dataset == Dataset.Discover:
-            if isinstance(col, (list, tuple)) or col == "project_id":
+            if isinstance(col, (list, tuple)) or col in ("project_id", "group_id"):
                 return col
         else:
             if (
