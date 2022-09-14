836168707f44f90d811f09ddf3e280e44647d7ea
k-fish <kevan.fisher@sentry.io>
2022-09-14 14:58:14 -0400

    rfc(features): Add options check
    This came up since there was a pretty good pattern for using non-flagr features + rollout system options if we want to gradually rollout instead of relying on a deploy, which can take a long time.

--- src/sentry/features/handler.py
+++ src/sentry/features/handler.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 __all__ = ["FeatureHandler", "BatchFeatureHandler"]
 
 import abc
+from time import time
 from typing import TYPE_CHECKING, Mapping, MutableSet, Optional, Sequence
+
+from sentry import options
 
 if TYPE_CHECKING:
     from sentry.features.base import Feature
@@ -13,6 +16,25 @@ if TYPE_CHECKING:
 
 class FeatureHandler:
     features: MutableSet[str] = set()
+    rollout_options: MutableSet[str] = set()
+    option_ttl = 60  # cache the options for 60s
+
+    def __init__(self):
+        super().__init__()
+
+        self.last_updated = None
+        self.cached_rollouts = {}
+
+    def update_cache_options(self):
+        now = time()
+        if (
+            self.last_updated is None
+            or now - self.option_ttl < self.last_updated  # refetch if the cache is old
+        ):
+
+            self.last_updated = now
+            for rolloutOption in self.rollout_options:
+                self.cached_rollouts[rolloutOption] = options.get(rolloutOption)
 
     def __call__(self, feature: Feature, actor: User) -> Optional[bool]:
         if feature.name not in self.features:
@@ -25,6 +47,7 @@ class FeatureHandler:
         raise NotImplementedError
 
     def has_for_batch(self, batch: FeatureCheckBatch) -> Mapping[Project, bool]:
+        self.update_cache_options()
         # If not overridden, iterate over objects in the batch individually.
         return {
             obj: self.has(feature, batch.actor)
@@ -55,8 +78,10 @@ class BatchFeatureHandler(FeatureHandler):
         raise NotImplementedError
 
     def has(self, feature: Feature, actor: User, skip_entity: Optional[bool] = False) -> bool:
+        self.update_cache_options()
         return self._check_for_batch(feature.name, feature.get_subject(), actor)
 
     def has_for_batch(self, batch: FeatureCheckBatch) -> Mapping[Project, bool]:
+        self.update_cache_options()
         flag = self._check_for_batch(batch.feature_name, batch.subject, batch.actor)
         return {obj: flag for obj in batch.objects}
