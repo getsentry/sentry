d7e418366a5590dba4e8e57d6db0e39bed914ccb
Shruthi <shruthilaya.jaganathan@sentry.io>
2022-09-14 07:32:20 -0400

    feat(discover): Add toggle for processed event baseline  (#38669)
    Add the toggle component that is disabled if
    the MetricsCardinality conditions aren't met.
    
    Toggling on/off currently isn't hooked up to
    anything.
    
    ![Screen Shot 2022-09-12 at 3 31 47
    PM](https://user-images.githubusercontent.com/63818634/189740053-3f5c7f26-12b1-40e4-9b64-113894a40ba3.png)

--- static/app/views/eventsV2/chartFooter.tsx
+++ static/app/views/eventsV2/chartFooter.tsx
@@ -1,3 +1,7 @@
+import {Fragment} from 'react';
+import styled from '@emotion/styled';
+
+import Feature from 'sentry/components/acl/feature';
 import OptionSelector from 'sentry/components/charts/optionSelector';
 import {
   ChartControls,
@@ -5,9 +9,11 @@ import {
   SectionHeading,
   SectionValue,
 } from 'sentry/components/charts/styles';
+import Switch from 'sentry/components/switchButton';
 import {t} from 'sentry/locale';
 import {Organization, SelectValue} from 'sentry/types';
 import {TOP_EVENT_MODES} from 'sentry/utils/discover/types';
+import {useMetricsCardinalityContext} from 'sentry/utils/performance/contexts/metricsCardinality';
 
 type Props = {
   displayMode: string;
@@ -16,6 +22,8 @@ type Props = {
   onDisplayChange: (value: string) => void;
   onTopEventsChange: (value: string) => void;
   organization: Organization;
+  setShowBaseline: (value: boolean) => void;
+  showBaseline: boolean;
   topEvents: string;
   total: number | null;
   yAxisOptions: SelectValue<string>[];
@@ -32,7 +40,11 @@ export default function ChartFooter({
   onDisplayChange,
   onTopEventsChange,
   topEvents,
+  setShowBaseline,
+  showBaseline,
+  organization,
 }: Props) {
+  const metricsCardinality = useMetricsCardinalityContext();
   const elements: React.ReactNode[] = [];
 
   elements.push(<SectionHeading key="total-label">{t('Total Events')}</SectionHeading>);
@@ -54,6 +66,18 @@ export default function ChartFooter({
     <ChartControls>
       <InlineContainer>{elements}</InlineContainer>
       <InlineContainer>
+        <Feature organization={organization} features={['discover-metrics-baseline']}>
+          <Fragment>
+            <SwitchLabel>{t('Processed events')}</SwitchLabel>
+            <Switch
+              data-test-id="processed-events-toggle"
+              isActive={showBaseline}
+              isDisabled={metricsCardinality.outcome?.forceTransactionsOnly}
+              size="lg"
+              toggle={() => setShowBaseline(!showBaseline)}
+            />
+          </Fragment>
+        </Feature>
         <OptionSelector
           title={t('Display')}
           selected={displayMode}
@@ -92,3 +116,8 @@ export default function ChartFooter({
     </ChartControls>
   );
 }
+
+const SwitchLabel = styled('div')`
+  padding-right: 4px;
+  font-weight: bold;
+`;
