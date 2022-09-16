dec54ca79b07ed849849d75dfc3b0ceaaf0fd363
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-09-16 16:09:55 -0400

    confirm processed event legend gets added

--- static/app/views/eventsV2/metricsBaselineContainer.spec.tsx
+++ static/app/views/eventsV2/metricsBaselineContainer.spec.tsx
@@ -1,3 +1,5 @@
+import ReactEchartsCore from 'echarts-for-react/lib/core';
+
 import {initializeOrg} from 'sentry-test/initializeOrg';
 import {addMetricsDataMock} from 'sentry-test/performance/addMetricsDataMock';
 import {render, screen, waitFor} from 'sentry-test/reactTestingLibrary';
@@ -8,6 +10,12 @@ import EventView from 'sentry/utils/discover/eventView';
 import {MetricsCardinalityProvider} from 'sentry/utils/performance/contexts/metricsCardinality';
 
 import {MetricsBaselineContainer} from './metricsBaselineContainer';
+
+jest.mock('echarts-for-react/lib/core', () => {
+  return jest.fn(({style}) => {
+    return <div style={{...style, background: 'green'}}>echarts mock</div>;
+  });
+});
 
 export function renderMetricsBaselineContainer(
   organization: Organization,
@@ -65,9 +73,16 @@ describe('MetricsBaselineContainer', function () {
   let eventsStatsMock: jest.Mock | undefined;
 
   beforeEach(function () {
+    (ReactEchartsCore as jest.Mock).mockClear();
+
     eventsStatsMock = MockApiClient.addMockResponse({
       url: '/organizations/org-slug/events-stats/',
-      body: [],
+      body: {
+        data: [
+          [1663272000, [{count: 2066}]],
+          [1663272300, [{count: 2158}]],
+        ],
+      },
     });
 
     MockApiClient.addMockResponse({
@@ -103,6 +118,50 @@ describe('MetricsBaselineContainer', function () {
 
     expect(screen.getByText(/Processed events/i)).toBeInTheDocument();
     expect(screen.getByTestId('processed-events-toggle')).toBeEnabled();
+  });
+
+  it('displays metrics baseline', async function () {
+    addMetricsDataMock();
+    const organization = TestStubs.Organization({
+      features: [
+        ...features,
+        'discover-metrics-baseline',
+        'server-side-sampling',
+        'mep-rollout-flag',
+      ],
+    });
+    const yAxis = ['p50(transaction.duration)'];
+
+    renderMetricsBaselineContainer(organization, project, eventView, yAxis);
+
+    await waitFor(() => {
+      expect(eventsStatsMock).toHaveBeenCalledWith(
+        '/organizations/org-slug/events-stats/',
+        expect.objectContaining({
+          query: expect.objectContaining({
+            dataset: 'metrics',
+          }),
+        })
+      );
+    });
+
+    expect(screen.getByText(/Processed events/i)).toBeInTheDocument();
+    expect(screen.getByTestId('processed-events-toggle')).toBeEnabled();
+
+    await waitFor(() => {
+      expect(ReactEchartsCore).toHaveBeenLastCalledWith(
+        expect.objectContaining({
+          option: expect.objectContaining({
+            legend: expect.objectContaining({
+              data: expect.arrayContaining([
+                'processed events: p50(transaction.duration)',
+              ]),
+            }),
+          }),
+        }),
+        {}
+      );
+    });
   });
 
   it('disables processed baseline toggle if metrics cardinality conditions not met', function () {
