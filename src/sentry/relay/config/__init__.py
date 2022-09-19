64ab52c2fb20396981d789f555f466d85a08446a
Kev <6111995+k-fish@users.noreply.github.com>
2022-09-19 13:18:19 -0400

    feat(perf-vitals): Add INP to allowed measurements
    This allows the `INP` measurement to be sent w/o counting towards the measurement limit.

--- src/sentry/relay/config/__init__.py
+++ src/sentry/relay/config/__init__.py
@@ -427,6 +427,7 @@ ALL_MEASUREMENT_METRICS = frozenset(
         "d:transactions/measurements.app_start_warm@millisecond",
         "d:transactions/measurements.cls@none",
         "d:transactions/measurements.fid@millisecond",
+        "d:transactions/measurements.inp@millisecond",
         "d:transactions/measurements.fp@millisecond",
         "d:transactions/measurements.frames_frozen@none",
         "d:transactions/measurements.frames_frozen_rate@ratio",
