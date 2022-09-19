ed2147e09c210370d4ec71f70fee94586198f5e9
Armen <armenzg@sentry.io>
2022-09-19 08:45:58 -0400

    I forgot to commit

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -2151,12 +2151,12 @@ SENTRY_SDK_CONFIG = {
         "custom_measurements": True,
     },
 }
-SENTRY_SDK_DSN = os.environ.get("SENTRY_SDK")
-if SENTRY_SDK_DSN:
-    # In production, this value is *not* set via SENTRY_SDK
+SENTRY_DEV_DSN = os.environ.get("SENTRY_DEV_DSN")
+if SENTRY_DEV_DSN:
+    # In production, this value is *not* set via an env variable
     # https://github.com/getsentry/getsentry/blob/16a07f72853104b911a368cc8ae2b4b49dbf7408/getsentry/conf/settings/prod.py#L604-L606
     # This is used in case you want to report traces of your development set up to a project of your choice
-    SENTRY_SDK_CONFIG["dsn"] = SENTRY_SDK_DSN
+    SENTRY_SDK_CONFIG["dsn"] = SENTRY_DEV_DSN
 
 # Callable to bind additional context for the Sentry SDK
 #
