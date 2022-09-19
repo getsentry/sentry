5cb515524ede72944e6d7c7df914b45ab3459c2f
Rahul Chhabria <rahul.chhabria@sentry.io>
2022-09-19 10:59:48 -0700

    minor copy changes (#38433)
    title case -> sentence case for consistency
    
    
    
    
    <!-- Describe your PR here. -->
    
    
    
    <!--
    
      Sentry employees and contractors can delete or ignore the following.
    
    -->
    
    ### Legal Boilerplate
    
    Look, I get it. The entity doing business as "Sentry" was incorporated
    in the State of Delaware in 2015 as Functional Software, Inc. and is
    gonna need some rights from me in order to utilize my contributions in
    this here PR. So here's the deal: I retain all rights, title and
    interest in and to my contributions, and by keeping this boilerplate
    intact I confirm that Sentry can use, modify, copy, and redistribute my
    contributions, under Sentry's choice of terms.

--- static/app/components/assistant/getGuidesContent.tsx
+++ static/app/components/assistant/getGuidesContent.tsx
@@ -194,12 +194,12 @@ export default function getGuidesContent(orgSlug: string | null): GuidesContent 
       dateThreshold: new Date('2022-04-05'),
       steps: [
         {
-          title: t('Selection filters here now!'),
+          title: t('Selection filters here now'),
           target: 'new_page_filter_button',
           description: t(
             "Selection filters were at the top of the page. Now they're here. Because this is what's getting filtered. Obvi."
           ),
-          nextText: t('Sounds Good'),
+          nextText: t('Sounds good'),
         },
         {
           title: t('Pin your filters'),
@@ -207,7 +207,7 @@ export default function getGuidesContent(orgSlug: string | null): GuidesContent 
           description: t(
             "Want to keep the same filters between searches and sessions? Click this button. Don't want to? Don't click this button."
           ),
-          nextText: t('Got It'),
+          nextText: t('Got it'),
         },
       ],
     },
@@ -217,12 +217,12 @@ export default function getGuidesContent(orgSlug: string | null): GuidesContent 
       dateThreshold: new Date('2022-06-22'),
       steps: [
         {
-          title: t('Releases are here!'),
+          title: t('Releases are here'),
           target: 'releases_widget',
           description: t(
             'Want to know how your latest release is doing? Monitor release health and crash rates in Dashboards.'
           ),
-          nextText: t('Sounds Good'),
+          nextText: t('Sounds good'),
         },
       ],
     },
