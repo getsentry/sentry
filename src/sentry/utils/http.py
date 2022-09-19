b1f4d1e013baa89365169b8e057a87858df1cce4
Alberto Leal <mail4alberto@gmail.com>
2022-09-19 16:33:13 -0400

    chore(auth): Update sentry.identity.oauth2 to support customer domains

--- src/sentry/utils/http.py
+++ src/sentry/utils/http.py
@@ -3,6 +3,7 @@ from typing import Optional
 from urllib.parse import quote, urlencode, urljoin, urlparse
 
 from django.conf import settings
+from rest_framework.request import Request
 
 from sentry import options
 
@@ -15,6 +16,13 @@ def absolute_uri(url: Optional[str] = None, url_prefix=None) -> str:
     if not url:
         return url_prefix
     return urljoin(url_prefix.rstrip("/") + "/", url.lstrip("/"))
+
+
+def create_redirect_url(request: Request, redirect_url: str) -> str:
+    qs = request.META.get("QUERY_STRING") or ""
+    if qs:
+        qs = "?" + qs
+    return f"{redirect_url}{qs}"
 
 
 def origin_from_url(url):
