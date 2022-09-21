5ea9b7b220a57109ff1ec408e51a319e9c313bf8
Dane Grant <danecando@gmail.com>
2022-09-21 11:34:56 -0400

    feat(replays): Additional analytics tracking events (#38966)
    Adds logging for some new event types:
    - Replay play/pause
    - Details page tab changes (main and sidebar tabs)
    - Time spent on replay details page
    - Toggle Fullscreen mode
    
    Closes #38581

--- static/app/utils/replays/hooks/useReplayPageview.tsx
+++ static/app/utils/replays/hooks/useReplayPageview.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 
 import ConfigStore from 'sentry/stores/configStore';
 import {useLegacyStore} from 'sentry/stores/useLegacyStore';
@@ -11,13 +11,25 @@ function useReplayPageview() {
   const config = useLegacyStore(ConfigStore);
   const location = useLocation();
   const organization = useOrganization();
+  const startTimeRef = useRef(Date.now());
 
   useEffect(() => {
+    const startTime = startTimeRef.current;
+
     trackAdvancedAnalyticsEvent('replay.details-viewed', {
       organization,
       referrer: decodeScalar(location.query.referrer),
       user_email: config.user.email,
     });
+
+    return () => {
+      const endTime = Date.now();
+      trackAdvancedAnalyticsEvent('replay.details-time-spent', {
+        organization,
+        seconds: (endTime - startTime) / 1000,
+        user_email: config.user.email,
+      });
+    };
   }, [organization, location.query.referrer, config.user.email]);
 }
 
