aa1bf960a1d0fdcd87a840eb7e14896f00c5e32f
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-09-23 03:25:49 -0400

    ref(js): Extract State in repositoryStore (#39190)

--- static/app/stores/repositoryStore.tsx
+++ static/app/stores/repositoryStore.tsx
@@ -2,23 +2,20 @@ import {createStore, StoreDefinition} from 'reflux';
 
 import {Repository} from 'sentry/types';
 
+type State = {
+  orgSlug?: string;
+  repositories?: Repository[];
+  repositoriesError?: Error;
+  repositoriesLoading?: boolean;
+};
+
 interface RepositoryStoreDefinition extends StoreDefinition {
-  get(): {
-    orgSlug?: string;
-    repositories?: Repository[];
-    repositoriesError?: Error;
-    repositoriesLoading?: boolean;
-  };
+  get(): State;
   loadRepositories(orgSlug: string): void;
   loadRepositoriesError(error: Error): void;
   loadRepositoriesSuccess(data: Repository[]): void;
   resetRepositories(): void;
-  state: {
-    orgSlug?: string;
-    repositories?: Repository[];
-    repositoriesError?: Error;
-    repositoriesLoading?: boolean;
-  };
+  state: State;
 }
 
 const storeConfig: RepositoryStoreDefinition = {
