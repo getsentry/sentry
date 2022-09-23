c8f43c55ef06e6d860f5f173ba4f546b0280599b
Ryan Skonnord <ryan.skonnord@sentry.io>
2022-09-23 09:35:49 -0700

    Use region ID in snowflake ID generation

--- src/sentry/utils/snowflake.py
+++ src/sentry/utils/snowflake.py
@@ -7,6 +7,7 @@ from django.db import IntegrityError, transaction
 from rest_framework import status
 from rest_framework.exceptions import APIException
 
+from sentry.types.region import get_region_mapping
 from sentry.utils import redis
 
 _TTL = timedelta(minutes=5)
@@ -72,10 +73,12 @@ def msb_0_ordering(value, width):
 
 
 def generate_snowflake_id(redis_key: str) -> int:
-    segment_values = {
-        VERSION_ID: msb_0_ordering(settings.SNOWFLAKE_VERSION_ID, VERSION_ID.length),
-        REGION_ID: settings.SNOWFLAKE_REGION_ID,
-    }
+    segment_values = {}
+
+    segment_values[VERSION_ID] = msb_0_ordering(settings.SNOWFLAKE_VERSION_ID, VERSION_ID.length)
+
+    local_region = get_region_mapping().get_local_region()
+    segment_values[REGION_ID] = local_region.id if local_region else 0
 
     current_time = datetime.now().timestamp()
     # supports up to 130 years
--- tests/sentry/utils/test_snowflake.py
+++ tests/sentry/utils/test_snowflake.py
@@ -1,13 +1,19 @@
 from datetime import datetime
+from unittest import mock
 
 import pytest
 from django.conf import settings
+from django.test import override_settings
 from freezegun import freeze_time
 
+from sentry.silo import SiloMode
 from sentry.testutils import TestCase
+from sentry.types.region import Region, RegionCategory, RegionMapping
+from sentry.utils import snowflake
 from sentry.utils.snowflake import (
     _TTL,
     MAX_AVAILABLE_REGION_SEQUENCES,
+    SnowflakeBitSegment,
     generate_snowflake_id,
     get_redis_cluster,
 )
@@ -57,3 +63,27 @@ class SnowflakeUtilsTest(TestCase):
             generate_snowflake_id("test_redis_key")
 
         assert str(context.value) == "No available ID"
+
+    @freeze_time(CURRENT_TIME)
+    @mock.patch("sentry.utils.snowflake.get_region_mapping")
+    def test_generate_correct_ids_with_region_id(self, mock_get_region_mapping):
+        regions = [
+            Region("test-region-1", 1, "localhost:8001", RegionCategory.GEOGRAPHIC),
+            Region("test-region-2", 2, "localhost:8002", RegionCategory.GEOGRAPHIC),
+        ]
+        region_mapping = RegionMapping(regions)
+        mock_get_region_mapping.return_value = region_mapping
+
+        with override_settings(SILO_MODE=SiloMode.REGION, SENTRY_REGION="test-region-1"):
+            snowflake1 = generate_snowflake_id("test_redis_key")
+        with override_settings(SILO_MODE=SiloMode.REGION, SENTRY_REGION="test-region-2"):
+            snowflake2 = generate_snowflake_id("test_redis_key")
+
+        def recover_segment_value(segment: SnowflakeBitSegment, value: int) -> int:
+            for s in reversed(snowflake.BIT_SEGMENT_SCHEMA):
+                if s == segment:
+                    return value & ((1 << s.length) - 1)
+                value >>= s.length
+
+        assert recover_segment_value(snowflake.REGION_ID, snowflake1) == regions[0].id
+        assert recover_segment_value(snowflake.REGION_ID, snowflake2) == regions[1].id
