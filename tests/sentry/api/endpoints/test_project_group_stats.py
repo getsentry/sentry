98673f053b3f8ff69c2fe889769f88167098bead
Dan Fuller <dfuller@sentry.io>
2022-09-27 09:43:21 -0700

    fix(tests): Fix flaky stats tests (#39345)
    These tests all fail if run in the first minute of the hour. Tested this
    with `@freeze_time(before_now(days=1).replace(minute=0))`. For tests
    that fail at that time I've hardcoded them to run at a specific minute
    of the hour.

--- tests/sentry/api/endpoints/test_project_group_stats.py
+++ tests/sentry/api/endpoints/test_project_group_stats.py
@@ -1,3 +1,5 @@
+from freezegun import freeze_time
+
 from sentry.testutils import APITestCase
 from sentry.testutils.helpers.datetime import before_now, iso_format
 from sentry.testutils.silo import region_silo_test
@@ -5,6 +7,7 @@ from sentry.testutils.silo import region_silo_test
 
 @region_silo_test
 class ProjectGroupStatsTest(APITestCase):
+    @freeze_time(before_now(days=1).replace(minute=10))
     def test_simple(self):
         self.login_as(user=self.user)
 
