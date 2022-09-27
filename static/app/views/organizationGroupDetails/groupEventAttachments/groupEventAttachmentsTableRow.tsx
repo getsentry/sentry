03e68cd89a9e733db927e5e182d77e55b2d3ce6a
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-09-26 23:58:22 -0700

    ref(js): Move AttachmentUrl component to components (#39325)

--- static/app/views/organizationGroupDetails/groupEventAttachments/groupEventAttachmentsTableRow.tsx
+++ static/app/views/organizationGroupDetails/groupEventAttachments/groupEventAttachmentsTableRow.tsx
@@ -1,12 +1,12 @@
 import styled from '@emotion/styled';
 
+import AttachmentUrl from 'sentry/components/attachmentUrl';
 import DateTime from 'sentry/components/dateTime';
 import EventAttachmentActions from 'sentry/components/events/eventAttachmentActions';
 import FileSize from 'sentry/components/fileSize';
 import Link from 'sentry/components/links/link';
 import {t} from 'sentry/locale';
 import {IssueAttachment} from 'sentry/types';
-import AttachmentUrl from 'sentry/utils/attachmentUrl';
 import {types} from 'sentry/views/organizationGroupDetails/groupEventAttachments/types';
 
 type Props = {
