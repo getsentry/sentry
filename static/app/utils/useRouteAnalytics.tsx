4fe5a490f28a58d0a384a5ed193e2cf1b587351e
Stephen Cefali <scefali@sentry.io>
2022-09-27 15:08:39 -0600

    v4

--- static/app/utils/useRouteAnalytics.tsx
+++ static/app/utils/useRouteAnalytics.tsx
@@ -1,22 +1,14 @@
 import {useContext, useEffect} from 'react';
 
-import HookStore from 'sentry/stores/hookStore';
 import {OrganizationContext} from 'sentry/views/organizationContext';
-
-const DEFAULT_CONTEXT = {
-  setDisableRouteAnalytics: () => {},
-  setRouteAnalyticsParams: () => {},
-  setOrganization: () => {},
-};
+import {RouteAnalyticsContext} from 'sentry/views/routeAnalyticsContextProvider';
 
 // This is the hook that is used to pass route analytics functions to routes.
 export default function useRouteAnalytics() {
   const organization = useContext(OrganizationContext);
-  const useRouteActivatedHook = HookStore.get('react-hook:route-activated')[0];
-  const {setDisableRouteAnalytics, setRouteAnalyticsParams, setOrganization} =
-    useRouteActivatedHook?.() || DEFAULT_CONTEXT;
+  const {setOrganization, ...routeAnalytics} = useContext(RouteAnalyticsContext);
   useEffect(() => {
     organization && setOrganization(organization);
   }, [organization, setOrganization]);
-  return {...setDisableRouteAnalytics, setRouteAnalyticsParams};
+  return routeAnalytics;
 }
