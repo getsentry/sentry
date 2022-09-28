7668965b04f0d112c7185110d38fe411e5cebdf6
Nar Saynorath <nar.saynorath@sentry.io>
2022-09-28 16:05:13 -0400

    Remove unnecessary catch in update helper

--- static/app/actionCreators/discoverHomepageQueries.tsx
+++ static/app/actionCreators/discoverHomepageQueries.tsx
@@ -1,24 +1,13 @@
 import {Client} from 'sentry/api';
-import {t} from 'sentry/locale';
 import {NewQuery, SavedQuery} from 'sentry/types';
-
-import {addErrorMessage} from './indicator';
 
 export function updateHomepageQuery(
   api: Client,
   orgId: string,
   query: NewQuery
 ): Promise<SavedQuery> {
-  const promise: Promise<SavedQuery> = api.requestPromise(
-    `/organizations/${orgId}/discover/homepage/`,
-    {
-      method: 'PUT',
-      data: query,
-    }
-  );
-
-  promise.catch(() => {
-    addErrorMessage(t('Unable to set query as Discover home'));
+  return api.requestPromise(`/organizations/${orgId}/discover/homepage/`, {
+    method: 'PUT',
+    data: query,
   });
-  return promise;
 }
