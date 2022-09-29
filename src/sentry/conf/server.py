b652a50b86bdd2da8b70d7d6f120308c07db905a
Colton Allen <colton.allen@sentry.io>
2022-09-29 14:46:03 -0500

    feat(replays): Add delete method for the replay details resource (#39078)
    Deletes a replay's recording segments. A replay has a one to many
    relationships with its segments. The segment data is stored in blob
    storage and has to be removed. Due to the I/O requirements we perform
    the operation asynchronously to provide fast API responses and to give
    ourselves good retry semantics.
    
    closes https://github.com/getsentry/replay-backend/issues/136
    
    depends https://github.com/getsentry/snuba/pull/3180
    depends https://github.com/getsentry/snuba/pull/3181
    
    Co-authored-by: Josh Ferge <josh.ferge@sentry.io>

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -559,6 +559,7 @@ CELERY_IMPORTS = (
     "sentry.discover.tasks",
     "sentry.incidents.tasks",
     "sentry.snuba.tasks",
+    "sentry.replays.tasks",
     "sentry.tasks.app_store_connect",
     "sentry.tasks.assemble",
     "sentry.tasks.auth",
@@ -2465,6 +2466,7 @@ KAFKA_SNUBA_METRICS = "snuba-metrics"
 KAFKA_PROFILES = "profiles"
 KAFKA_INGEST_PERFORMANCE_METRICS = "ingest-performance-metrics"
 KAFKA_SNUBA_GENERIC_METRICS = "snuba-generic-metrics"
+KAFKA_INGEST_REPLAY_EVENTS = "ingest-replay-events"
 KAFKA_INGEST_REPLAYS_RECORDINGS = "ingest-replay-recordings"
 KAFKA_REGION_TO_CONTROL = "region-to-control"
 
@@ -2511,6 +2513,7 @@ KAFKA_TOPICS = {
     KAFKA_PROFILES: {"cluster": "default"},
     KAFKA_INGEST_PERFORMANCE_METRICS: {"cluster": "default"},
     KAFKA_SNUBA_GENERIC_METRICS: {"cluster": "default"},
+    KAFKA_INGEST_REPLAY_EVENTS: {"cluster": "default"},
     KAFKA_INGEST_REPLAYS_RECORDINGS: {"cluster": "default"},
     # Metrics Testing Topics
     KAFKA_SNUBA_GENERICS_METRICS_CS: {"cluster": "default"},
