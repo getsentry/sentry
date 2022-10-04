1f498f2a896eff2c033226c2fe7e6819cfb78033
Snigdha Sharma <snigdhasharma@sentry.io>
2022-10-04 14:42:54 -0700

    Filter out empty_stacktrace.js_console from /tags/ endpoint

--- src/sentry/api/endpoints/organization_tags.py
+++ src/sentry/api/endpoints/organization_tags.py
@@ -2,7 +2,7 @@ import sentry_sdk
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry import tagstore
+from sentry import features, tagstore
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases import NoProjects, OrganizationEventsEndpointBase
 from sentry.api.serializers import serialize
@@ -29,6 +29,16 @@ class OrganizationTagsEndpoint(OrganizationEventsEndpointBase):
                     # Defaults to True, because the frontend caches these tags globally
                     include_transactions=request.GET.get("include_transactions", "1") == "1",
                 )
+                if (
+                    not features.has(
+                        "organizations:javascript-console-error-tag",
+                        organization,
+                        actor=request.user,
+                    )
+                    and "empty_stacktrace.js_console" in results
+                ):
+                    results.remove("empty_stacktrace.js_console")
+
                 # Setting the tag for now since the measurement is still experimental
                 sentry_sdk.set_tag("custom_tags.count", len(results))
                 sentry_sdk.set_tag(
