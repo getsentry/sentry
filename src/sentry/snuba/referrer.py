14e65e4568f53cd2681883be909188d470c917d0
Andrii Soldatenko <andrii.soldatenko@gmail.io>
2022-10-05 15:00:16 +0200

    update snuba/referrer.py

--- src/sentry/snuba/referrer.py
+++ src/sentry/snuba/referrer.py
@@ -202,14 +202,20 @@ class Referrer(Enum):
     DATA_EXPORT_TASKS_DISCOVER = "data_export.tasks.discover"
     DELETIONS_GROUP = "deletions.group"
     DISCOVER = "discover"
-    DYNAMIC_SAMPLING_DISTRIBUTION_FETCH_PARENT_TRANSACTIONS = (
-        "dynamic-sampling.distribution.fetch-parent-transactions"
+    DYNAMIC_SAMPLING_DISTRIBUTION_FETCH_TRANSACTIONS = (
+        "dynamic-sampling.distribution.fetch-transactions"
     )
-    DYNAMIC_SAMPLING_DISTRIBUTION_FETCH_PARENT_TRANSACTIONS_COUNT = (
+    DYNAMIC_SAMPLING_DISTRIBUTION_FETCH_TRANSACTIONS_COUNT = (
         "dynamic-sampling.distribution.fetch-transactions-count"
     )
     DYNAMIC_SAMPLING_DISTRIBUTION_FETCH_PROJECT_BREAKDOWN = (
         "dynamic-sampling.distribution.fetch-project-breakdown"
+    )
+    DYNAMIC_SAMPLING_DISTRIBUTION_GET_MOST_RECENT_DAY_WITH_TRANSACTIONS = (
+        "dynamic-sampling.distribution.get-most-recent-day-with-transactions"
+    )
+    DYNAMIC_SAMPLING_DISTRIBUTION_FETCH_PROJECT_STATS = (
+        "dynamic-sampling.distribution.fetch-project-stats"
     )
     DYNAMIC_SAMPLING_DISTRIBUTION_FETCH_PROJECT_SDK_VERSIONS_INFO = (
         "dynamic-sampling.distribution.fetch-project-sdk-versions-info"
