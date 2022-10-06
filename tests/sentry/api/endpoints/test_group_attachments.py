ec69f7dd5e93f837dbb216b58a54600769b801d1
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2022-10-06 14:15:30 -0700

    feat(issues) add ability to filter attachments by event_ids (#39723)
    Adds a query param so that you can filter attachments within an issue by
    event ids.
    Required for the new all events tab, which will show attachments
    associated to each event, in the events tab.

--- tests/sentry/api/endpoints/test_group_attachments.py
+++ tests/sentry/api/endpoints/test_group_attachments.py
@@ -8,7 +8,7 @@ from sentry.testutils.silo import region_silo_test
 
 @region_silo_test
 class GroupEventAttachmentsTest(APITestCase):
-    def create_attachment(self, type=None):
+    def create_attachment(self, type=None, event_id=None):
         if type is None:
             type = "event.attachment"
 
@@ -16,7 +16,7 @@ class GroupEventAttachmentsTest(APITestCase):
         self.file.putfile(BytesIO(b"File contents here"))
 
         self.attachment = EventAttachment.objects.create(
-            event_id=self.event.event_id,
+            event_id=event_id or self.event.event_id,
             project_id=self.event.project_id,
             group_id=self.group.id,
             file_id=self.file.id,
@@ -26,12 +26,17 @@ class GroupEventAttachmentsTest(APITestCase):
 
         return self.attachment
 
-    def path(self, types=None):
+    def path(self, types=None, event_ids=None):
         path = f"/api/0/issues/{self.group.id}/attachments/"
 
-        query = [("types", t) for t in types or ()]
-        if query:
-            path += "?" + urlencode(query)
+        typesQuery = [("types", t) for t in types or ()]
+        eventIdQuery = [("event_id", id) for id in event_ids or ()]
+        if typesQuery and eventIdQuery:
+            path += "?" + urlencode(typesQuery) + "&" + urlencode(eventIdQuery)
+        elif typesQuery:
+            path += "?" + urlencode(typesQuery)
+        elif eventIdQuery:
+            path += "?" + urlencode(eventIdQuery)
 
         return path
 
@@ -68,3 +73,31 @@ class GroupEventAttachmentsTest(APITestCase):
             response = self.client.get(self.path())
 
         assert response.status_code == 404, response.content
+
+    def test_event_id_filter(self):
+        self.login_as(user=self.user)
+        attachment = self.create_attachment()
+        self.create_attachment(event_id="b" * 32)
+
+        with self.feature("organizations:event-attachments"):
+            response = self.client.get(self.path(event_ids=[attachment.event_id]))
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 1
+        assert response.data[0]["event_id"] == attachment.event_id
+
+    def test_multi_event_id_filter(self):
+        self.login_as(user=self.user)
+        attachment = self.create_attachment()
+        attachment2 = self.create_attachment(event_id="b" * 32)
+        self.create_attachment(event_id="c" * 32)
+
+        with self.feature("organizations:event-attachments"):
+            response = self.client.get(
+                self.path(event_ids=[attachment.event_id, attachment2.event_id])
+            )
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 2
+        assert response.data[0]["event_id"] == attachment2.event_id
+        assert response.data[1]["event_id"] == attachment.event_id
