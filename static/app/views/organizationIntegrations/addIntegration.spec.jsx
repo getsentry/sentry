88af564bc6c80e72828d70d533c43d2ffd481121
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-10-11 17:26:31 +1100

    test(js): Convert AddIntegration to RTL (#39850)

--- static/app/views/organizationIntegrations/addIntegration.spec.jsx
+++ static/app/views/organizationIntegrations/addIntegration.spec.jsx
@@ -1,5 +1,4 @@
-/* global global */
-import {mountWithTheme} from 'sentry-test/enzyme';
+import {render, waitFor} from 'sentry-test/reactTestingLibrary';
 
 import AddIntegration from 'sentry/views/organizationIntegrations/addIntegration';
 
@@ -7,14 +6,14 @@ describe('AddIntegration', function () {
   const provider = TestStubs.GitHubIntegrationProvider();
   const integration = TestStubs.GitHubIntegration();
 
-  it('Adds an integration on dialog completion', function () {
+  it('Adds an integration on dialog completion', async function () {
     const onAdd = jest.fn();
 
     const focus = jest.fn();
     const open = jest.fn().mockReturnValue({focus});
     global.open = open;
 
-    const wrapper = mountWithTheme(
+    render(
       <AddIntegration provider={provider} onInstall={onAdd}>
         {onClick => (
           <a href="#" onClick={onClick}>
@@ -25,20 +24,16 @@ describe('AddIntegration', function () {
     );
 
     const newIntegration = {
-      source: null,
-      origin: 'http://localhost',
-      data: {
-        success: true,
-        data: Object.assign({}, integration, {
-          id: '2',
-          domain_name: 'new-integration.github.com',
-          icon: 'http://example.com/new-integration-icon.png',
-          name: 'New Integration',
-        }),
-      },
+      success: true,
+      data: Object.assign({}, integration, {
+        id: '2',
+        domain_name: 'new-integration.github.com',
+        icon: 'http://example.com/new-integration-icon.png',
+        name: 'New Integration',
+      }),
     };
 
-    wrapper.instance().didReceiveMessage(newIntegration);
-    expect(onAdd).toHaveBeenCalledWith(newIntegration.data.data);
+    window.postMessage(newIntegration, '*');
+    await waitFor(() => expect(onAdd).toHaveBeenCalledWith(newIntegration.data));
   });
 });
