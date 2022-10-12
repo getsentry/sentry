b33b7fe7551ed158de9e140c3a5ad4bfc6129129
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-10-12 15:59:59 +0200

    feat(sampling): Add feature flag to show new UI as per recent changes (#39916)

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -82,6 +82,7 @@ default_manager.add("organizations:discover-metrics-baseline", OrganizationFeatu
 default_manager.add("organizations:discover-frontend-use-events-endpoint", OrganizationFeature, True)
 default_manager.add("organizations:discover-interval-selector", OrganizationFeature, True)
 default_manager.add("organizations:discover-query-builder-as-landing-page", OrganizationFeature, True)
+default_manager.add("organizations:dynamic-sampling-total-transaction-packaging", OrganizationFeature, True)
 default_manager.add("organizations:enterprise-perf", OrganizationFeature)
 default_manager.add("organizations:grouping-stacktrace-ui", OrganizationFeature, True)
 default_manager.add("organizations:grouping-title-ui", OrganizationFeature, True)
