6b0f96584891849811c95d800135d5edcc08a39a
ahmedetefy <ahmed.etefy12@gmail.com>
2022-10-13 15:14:20 +0200

    feat(dyn-sampling): Add implementation for `generate_uniform_rule`

---
+++ src/sentry/dynamic_sampling/utils.py
@@ -0,0 +1,32 @@
+from typing import Any, List, TypedDict
+
+from sentry import quotas
+from sentry.models import Project
+
+UNIFORM_RULE_RESERVED_ID = 0
+
+
+class Condition(TypedDict):
+    op: str
+    inner: List[Any]
+
+
+class UniformDSRule(TypedDict):
+    sampleRate: float
+    type: str
+    active: bool
+    condition: Condition
+    id: int
+
+
+def generate_uniform_rule(project: Project) -> UniformDSRule:
+    return {
+        "sampleRate": quotas.get_blended_sample_rate(project),
+        "type": "trace",
+        "active": True,
+        "condition": {
+            "op": "and",
+            "inner": [],
+        },
+        "id": UNIFORM_RULE_RESERVED_ID,
+    }
