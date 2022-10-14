7322957c386bf6df935c23da51c0eded206ea5b5
Riccardo Busetti <riccardo.busetti@sentry.io>
2022-10-14 11:08:26 +0200

    ref(metrics): Change default rule id from 0 to -1

--- static/app/views/settings/project/dynamicSampling/modals/specificConditionsModal/index.tsx
+++ static/app/views/settings/project/dynamicSampling/modals/specificConditionsModal/index.tsx
@@ -110,8 +110,9 @@ export function SpecificConditionsModal({
     const sampleRate = percentageToRate(samplePercentage)!;
 
     const newRule: SamplingRule = {
-      // All new/updated rules must have id equal to 0
-      id: 0,
+      // All new rules must have the default id set to -1, signaling to the backend that a proper id should
+      // be assigned.
+      id: -1,
       active: rule ? rule.active : false,
       type: SamplingRuleType.TRACE,
       condition: {
