5d8cbb8de23f7ea324afd7bdf587b0fc4fb2d204
Ahmed Etefy <ahmed.etefy@sentry.io>
2022-10-14 12:40:39 +0200

    fix(ds): Set unassigned rule id to -1 (#40034)
    Changes the unassigned rule id to -1 from the default of 0 because 0 is
    actually a valid rule id according to relay and it will be used as the
    reserved constant for DS uniform rules in dynamic sampling v2

--- src/sentry/api/endpoints/project_details.py
+++ src/sentry/api/endpoints/project_details.py
@@ -95,7 +95,8 @@ class DynamicSamplingRuleSerializer(serializers.Serializer):
     )
     condition = DynamicSamplingConditionSerializer()
     active = serializers.BooleanField(default=False)
-    id = serializers.IntegerField(min_value=0, required=False)
+    # Setting the min value here to -1 because -1 is the rule id value for unassigned rules.
+    id = serializers.IntegerField(min_value=-1, required=False)
 
 
 class DynamicSamplingSerializer(serializers.Serializer):
@@ -131,9 +132,13 @@ class DynamicSamplingSerializer(serializers.Serializer):
             rules = raw_dynamic_sampling.get("rules", [])
 
             for rule in rules:
-                rid = rule.get("id", 0)
+                # We are setting the unassigned id to -1. It used to be 0 but we are modifying the behavior as 0 is a
+                # valid id according to relay's rule validation which states the a rule id is an unsigned integer.
+                rid = rule.get("id", -1)
                 original_rule = original_rules_dict.get(rid)
-                if rid == 0 or original_rule is None:
+                # ToDo(ahmed): Temporarily allowing for 0 to be the unassigned rule id for backwards compatibility,
+                #  and will remove that once the UI changes are deployed.
+                if rid in {0, -1} or original_rule is None:
                     # a new or unknown rule give it a new id
                     rule["id"] = next_id
                     next_id += 1
