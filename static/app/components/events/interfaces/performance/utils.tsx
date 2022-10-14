0e55f9928cbd327f1515119322a978513190046b
Ash Anand <ash.anand@sentry.io>
2022-10-14 13:02:28 -0400

    Add all inclusive resources

--- static/app/components/events/interfaces/performance/utils.tsx
+++ static/app/components/events/interfaces/performance/utils.tsx
@@ -3,6 +3,13 @@ import {IssueType, PlatformType} from 'sentry/types';
 
 import {ResourceLink} from './resources';
 
+const ALL_INCLUSIVE_RESOURCE_LINKS: ResourceLink[] = [
+  {
+    text: t('Sentry Docs: N+1 Queries'),
+    link: 'https://docs.sentry.io/product/issues/performance-issues/n-one-queries/',
+  },
+];
+
 const RESOURCES_DESCRIPTIONS: Record<IssueType, string> = {
   [IssueType.PERFORMANCE_N_PLUS_ONE_DB_QUERIES]: t(
     "N+1 queries are extraneous queries (N) caused by a single, initial query (+1). In the Span Evidence above, we've identified the parent span where the extraneous spans are located and the extraneous spans themselves. To learn more about how to fix N+1 problems, check out these resources:"
@@ -53,9 +60,9 @@ export function getResourceLinks(
   issueType: IssueType,
   platform: PlatformType | undefined
 ): ResourceLink[] {
-  if (!platform || !RESOURCE_LINKS[issueType][platform]) {
-    return [];
+  if (!platform || !RESOURCE_LINKS[issueType] || !RESOURCE_LINKS[issueType][platform]) {
+    return ALL_INCLUSIVE_RESOURCE_LINKS;
   }
 
-  return RESOURCE_LINKS[issueType][platform] ?? [];
+  return [...ALL_INCLUSIVE_RESOURCE_LINKS, ...RESOURCE_LINKS[issueType][platform]!];
 }
