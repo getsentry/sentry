13f260f05bdbbd4622db05f82fbcd8571adef40b
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-10-14 11:53:10 +0200

    ref(sampling): Validate conditional rule sample rate - (#39974)

--- static/app/views/settings/project/dynamicSampling/modals/recommendedStepsModal.spec.tsx
+++ static/app/views/settings/project/dynamicSampling/modals/recommendedStepsModal.spec.tsx
@@ -1,11 +1,10 @@
 import {initializeOrg} from 'sentry-test/initializeOrg';
-import {renderGlobalModal, screen, userEvent} from 'sentry-test/reactTestingLibrary';
+import {renderGlobalModal, screen} from 'sentry-test/reactTestingLibrary';
 import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {openModal} from 'sentry/actionCreators/modal';
 import {Project} from 'sentry/types';
 import {RecommendedStepsModal} from 'sentry/views/settings/project/dynamicSampling/modals/recommendedStepsModal';
-import {SERVER_SIDE_SAMPLING_DOC_LINK} from 'sentry/views/settings/project/dynamicSampling/utils';
 
 function getMockData({projects, access}: {access?: string[]; projects?: Project[]} = {}) {
   return initializeOrg({
@@ -47,8 +46,6 @@ describe('Dynamic Sampling - Recommended Steps Modal', function () {
         projectId={projects[0].id}
         recommendedSdkUpgrades={TestStubs.DynamicSamplingConfig().recommendedSdkUpgrades}
         onReadDocs={jest.fn()}
-        onSubmit={jest.fn()}
-        clientSampleRate={0.5}
       />
     ));
 
@@ -84,112 +81,6 @@ describe('Dynamic Sampling - Recommended Steps Modal', function () {
       )
     ).toBeInTheDocument();
 
-    // Second recommended step
-    expect(
-      screen.getByRole('heading', {
-        name: 'Adjust your Client-Side (SDK) sample rate',
-      })
-    ).toBeInTheDocument();
-
-    expect(
-      screen.getByText(
-        'Here’s the new Client-Side (SDK) sample rate you specified in the previous step. To make this change, find the ‘tracesSampleRate’ option in your SDK Config, modify it’s value to what’s shown below and re-deploy.'
-      )
-    ).toBeInTheDocument();
-
-    expect(screen.getByText(textWithMarkupMatcher(/Sentry.init/))).toBeInTheDocument();
-    expect(screen.getByText('0.5')).toBeInTheDocument();
-    expect(screen.getByText('// 50%')).toBeInTheDocument();
-
-    // Footer
-    expect(screen.getByRole('button', {name: 'Read Docs'})).toHaveAttribute(
-      'href',
-      SERVER_SIDE_SAMPLING_DOC_LINK
-    );
-
-    expect(screen.getByRole('button', {name: 'Cancel'})).toBeInTheDocument();
-
-    expect(screen.getByRole('button', {name: 'Done'})).toBeInTheDocument();
-
-    expect(screen.queryByText('Step 2 of 2')).not.toBeInTheDocument();
-
-    expect(screen.queryByRole('button', {name: 'Back'})).not.toBeInTheDocument();
-  });
-
-  it('render only the last recommended step', function () {
-    const {organization, project} = getMockData();
-
-    renderGlobalModal();
-
-    openModal(modalProps => (
-      <RecommendedStepsModal
-        {...modalProps}
-        organization={organization}
-        projectId={project.id}
-        recommendedSdkUpgrades={[]}
-        onSubmit={jest.fn()}
-        onReadDocs={jest.fn()}
-        clientSampleRate={TestStubs.DynamicSamplingConfig().uniformRule.sampleRate}
-      />
-    ));
-
-    expect(
-      screen.queryByRole('heading', {
-        name: 'Update the following SDK versions',
-      })
-    ).not.toBeInTheDocument();
-
-    expect(
-      screen.getByRole('heading', {
-        name: 'Adjust your Client-Side (SDK) sample rate',
-      })
-    ).toBeInTheDocument();
-  });
-
-  it('render as a second step of a wizard', function () {
-    const {organization, project} = getMockData();
-
-    const onGoBack = jest.fn();
-
-    renderGlobalModal();
-
-    openModal(modalProps => (
-      <RecommendedStepsModal
-        {...modalProps}
-        organization={organization}
-        projectId={project.id}
-        recommendedSdkUpgrades={[]}
-        onGoBack={onGoBack}
-        onSubmit={jest.fn()}
-        onReadDocs={jest.fn()}
-        clientSampleRate={TestStubs.DynamicSamplingConfig().uniformRule.sampleRate}
-      />
-    ));
-
-    expect(screen.getByText('Step 2 of 2')).toBeInTheDocument();
-    userEvent.click(screen.getByRole('button', {name: 'Back'}));
-    expect(onGoBack).toHaveBeenCalled();
-  });
-
-  it('renders 3/3 footer', function () {
-    const {organization, project} = getMockData();
-
-    renderGlobalModal();
-
-    openModal(modalProps => (
-      <RecommendedStepsModal
-        {...modalProps}
-        organization={organization}
-        projectId={project.id}
-        recommendedSdkUpgrades={[]}
-        onGoBack={jest.fn()}
-        onSubmit={jest.fn()}
-        onReadDocs={jest.fn()}
-        clientSampleRate={TestStubs.DynamicSamplingConfig().uniformRule.sampleRate}
-        specifiedClientRate={0.1}
-      />
-    ));
-
-    expect(screen.getByText('Step 3 of 3')).toBeInTheDocument();
+    expect(screen.getByRole('button', {name: 'Got it'})).toBeInTheDocument();
   });
 });
