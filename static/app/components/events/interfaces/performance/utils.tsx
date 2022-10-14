75b57cbae9dabc9fd2bd14fb6c5e3401dacecfad
Ash Anand <0Calories@users.noreply.github.com>
2022-10-14 13:41:29 -0400

    Add all inclusive resources (#40047)

--- static/app/components/events/interfaces/performance/utils.tsx
+++ static/app/components/events/interfaces/performance/utils.tsx
@@ -3,6 +3,13 @@ import {IssueType, PlatformType} from 'sentry/types';
 
 import {ResourceLink} from './resources';
 
+const ALL_INCLUSIVE_RESOURCE_LINKS: ResourceLink[] = [
+  {
+    text: t('Sentry Docs: N+1 Queries'),
+    link: 'https://docs.sentry.io/product/issues/performance-issues/n-one-queries/',
+  },
+];
+
 const RESOURCES_DESCRIPTIONS: Record<IssueType, string> = {
   [IssueType.PERFORMANCE_N_PLUS_ONE_DB_QUERIES]: t(
     "N+1 queries are extraneous queries (N) caused by a single, initial query (+1). In the Span Evidence above, we've identified the parent span where the extraneous spans are located and the extraneous spans themselves. To learn more about how to fix N+1 problems, check out these resources:"
@@ -20,25 +27,11 @@ const RESOURCE_LINKS: Record<IssueType, PlatformSpecificResources> = {
         text: t('Finding and Fixing Django N+1 Problems'),
         link: 'https://blog.sentry.io/2020/09/14/finding-and-fixing-django-n-1-problems',
       },
-      {
-        text: t('Django select_related and prefetch_related'),
-        link: 'https://betterprogramming.pub/django-select-related-and-prefetch-related-f23043fd635d',
-      },
     ],
     'python-django': [
       {
         text: t('Finding and Fixing Django N+1 Problems'),
         link: 'https://blog.sentry.io/2020/09/14/finding-and-fixing-django-n-1-problems',
-      },
-      {
-        text: t('Django select_related and prefetch_related'),
-        link: 'https://betterprogramming.pub/django-select-related-and-prefetch-related-f23043fd635d',
-      },
-    ],
-    'ruby-rails': [
-      {
-        text: t('Rails Guide: Active Record Query Interface'),
-        link: 'https://guides.rubyonrails.org/active_record_querying.html#eager-loading-associations',
       },
     ],
   },
@@ -53,9 +46,9 @@ export function getResourceLinks(
   issueType: IssueType,
   platform: PlatformType | undefined
 ): ResourceLink[] {
-  if (!platform || !RESOURCE_LINKS[issueType][platform]) {
-    return [];
+  if (!platform || !RESOURCE_LINKS[issueType] || !RESOURCE_LINKS[issueType][platform]) {
+    return ALL_INCLUSIVE_RESOURCE_LINKS;
   }
 
-  return RESOURCE_LINKS[issueType][platform] ?? [];
+  return [...ALL_INCLUSIVE_RESOURCE_LINKS, ...RESOURCE_LINKS[issueType][platform]!];
 }
