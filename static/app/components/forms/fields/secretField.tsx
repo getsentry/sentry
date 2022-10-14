7cff4f51aff43fa60222f9e04884f17f15c3b2f7
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-10-14 18:20:53 +1100

    fix(ui): Give SecretField the textbox role (#40028)

--- static/app/components/forms/fields/secretField.tsx
+++ static/app/components/forms/fields/secretField.tsx
@@ -3,7 +3,12 @@ import InputField, {InputFieldProps} from './inputField';
 export interface SecretFieldProps extends Omit<InputFieldProps, 'type'> {}
 
 function SecretField(props: SecretFieldProps) {
-  return <InputField {...props} type="password" />;
+  // XXX: We explicitly give the password field a aria textbox role. This field
+  // does not typically have a role, but for testability reasons it's preferred
+  // for it to have a role. See [0]
+  //
+  // [0]: https://github.com/testing-library/dom-testing-library/issues/567
+  return <InputField {...props} type="password" role="textbox" />;
 }
 
 export default SecretField;
