2e577020252ee6b189030a48757c4436d6967776
Colleen O'Rourke <colleen@sentry.io>
2022-10-13 17:17:28 -0700

    add the rest of the email data

--- src/sentry/web/frontend/debug/debug_performance_issue.py
+++ src/sentry/web/frontend/debug/debug_performance_issue.py
@@ -2,18 +2,17 @@ import pytz
 from django.utils.safestring import mark_safe
 from django.views.generic import View
 
-from sentry.api.serializers.models.event import get_entries, get_problems  # , get_problems
-from sentry.event_manager import EventManager, get_event_type
-from sentry.models import Organization, Project, Rule
+from sentry.api.serializers.models.event import get_entries, get_problems
+from sentry.event_manager import EventManager
+from sentry.models import GroupHash, Organization, Project, Rule
 from sentry.notifications.utils import get_group_settings_link, get_rules
 from sentry.testutils.helpers import override_options
 from sentry.testutils.helpers.datetime import before_now
-from sentry.types.issues import GroupType
 from sentry.utils import json
 from sentry.utils.samples import load_data
 from sentry.web.helpers import render_to_string
 
-from .mail import MailPreview, get_random, make_group_generator
+from .mail import MailPreview
 
 COMMIT_EXAMPLE = """[
 {
@@ -50,13 +49,47 @@ COMMIT_EXAMPLE = """[
 
 
 class DebugPerformanceIssueEmailView(View):
-    def perf_to_email_html(self, transaction_data, **kwargs):
+    def perf_to_email_html(self, spans, problem):
+
+        # I'm aware this is not great duplication
+        def get_span_evidence_value_problem(problem):
+            value = "no value"
+            if not problem.op and problem.desc:
+                value = problem.desc
+            if problem.op and not problem.desc:
+                value = problem.op
+            if problem.op and problem.desc:
+                value = f"{problem.op} - {problem.desc}"
+            return value
+
+        def get_span_evidence_value(span):
+            value = "no value"
+            if not span.get("op") and span.get("description"):
+                value = span.get("description")
+            if span.get("op") and not span.get("description"):
+                value = span.get("op")
+            if span.get("op") and span.get("description"):
+                op = span.get("op")
+                desc = span.get("description")
+                value = f"{op} - {desc}"
+            return value
+
+        parent_span = None
+        repeating_spans = None
+        for span in spans:
+            if problem.parent_span_ids[0] == span.get("span_id"):
+                parent_span = span
+            if problem.offender_span_ids[0] == span.get("span_id"):
+                repeating_spans = span
+            if parent_span is not None and repeating_spans is not None:
+                break
+
         context = {
-            "transaction_name": transaction_data.get("description"),
-            "parent_span": "idk",
-            "preceding_span": "SELECT idk FROM idk",
-            "repeating_spans": "SELECT idk FROM idk WHERE i_truly_dont_know AND idk_man",
-            "num_repeating_spans": "4",
+            "transaction_name": get_span_evidence_value_problem(problem),
+            "parent_span": get_span_evidence_value(parent_span),
+            # "preceding_span": "SELECT idk FROM idk_what_a_preceding_span_is",
+            "repeating_spans": get_span_evidence_value(repeating_spans),
+            "num_repeating_spans": str(len(problem.offender_span_ids)),
         }
         return render_to_string("sentry/emails/transactions.html", context)
 
@@ -66,11 +99,7 @@ class DebugPerformanceIssueEmailView(View):
     def get(self, request):
         org = Organization(id=1, slug="example", name="Example")
         project = Project(id=1, slug="example", name="Example", organization=org)
-        random = get_random(request)
         project.update_option("sentry:performance_issue_creation_rate", 1.0)
-
-        perf_group = next(make_group_generator(random, project))
-        perf_group.type = GroupType.PERFORMANCE_N_PLUS_ONE.value
         with override_options(
             {
                 "performance.issues.all.problem-creation": 1.0,
@@ -83,32 +112,26 @@ class DebugPerformanceIssueEmailView(View):
             perf_event_manager.normalize()
             perf_data = perf_event_manager.get_data()
             perf_event = perf_event_manager.save(project.id)
-        print("perf event", perf_event, perf_event.groups)
-        perf_event = perf_event.for_group(perf_event.groups[0])
 
-        perf_event_type = get_event_type(perf_event.data)
-        perf_group.message = perf_event.search_message
-        perf_group.data = {
-            "type": perf_event_type.key,
-            "metadata": perf_event_type.get_metadata(perf_data),
-        }
+        perf_event = perf_event.for_group(perf_event.groups[0])
+        perf_group = perf_event.group
 
         rule = Rule(id=1, label="Example performance rule")
 
         entries = get_entries(perf_event, None)
-        transaction_data = {}
+        spans = []
         if len(entries):
-            transaction_data = [
-                entry.get("data") for entry in entries[0] if entry.get("type") == "spans"
-            ][0]
+            spans = [entry.get("data") for entry in entries[0] if entry.get("type") == "spans"][0]
 
-        problems = get_problems([perf_event])
-        print("problems", problems)
+        matched_problem = None
+        for problem in get_problems([perf_event]):
+            if problem.problem.fingerprint == GroupHash.objects.get(group=perf_group).hash:
+                matched_problem = problem
 
-        transaction_data = self.perf_to_email_html(transaction_data[0])
+        spans = self.perf_to_email_html(spans, matched_problem.problem)
 
         # XXX: this interface_list code needs to be the same as in
-        #      src/sentry/mail/adapter.py
+        # get_interface_list in src/sentry/notifications/utils/__init__.py
         interface_list = []
         for interface in perf_event.interfaces.values():
             body = interface.to_email_html(perf_event)
@@ -135,6 +158,6 @@ class DebugPerformanceIssueEmailView(View):
                 "tags": perf_event.tags,
                 "project_label": project.slug,
                 "commits": json.loads(COMMIT_EXAMPLE),
-                "transaction_data": [("Span Evidence", mark_safe(transaction_data), None)],
+                "transaction_data": [("Span Evidence", mark_safe(spans), None)],
             },
         ).render(request)
