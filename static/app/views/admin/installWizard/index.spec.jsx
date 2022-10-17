b07668d28df4b2a15c6ae127f0488f86bed479b4
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-10-18 10:13:21 +1100

    test(js): Fix installWizard test (#40156)

--- static/app/views/admin/installWizard/index.spec.jsx
+++ static/app/views/admin/installWizard/index.spec.jsx
@@ -41,12 +41,12 @@ describe('InstallWizard', function () {
     render(<InstallWizard onConfigured={jest.fn()} />);
     expect(
       screen.getByRole('radio', {
-        name: 'Select Please keep my usage information anonymous',
+        name: 'Please keep my usage information anonymous',
       })
     ).not.toBeChecked();
     expect(
       screen.getByRole('radio', {
-        name: 'Select Send my contact information along with usage statistics',
+        name: 'Send my contact information along with usage statistics',
       })
     ).not.toBeChecked();
   });
@@ -71,12 +71,12 @@ describe('InstallWizard', function () {
     render(<InstallWizard onConfigured={jest.fn()} />);
     expect(
       screen.getByRole('radio', {
-        name: 'Select Please keep my usage information anonymous',
+        name: 'Please keep my usage information anonymous',
       })
     ).not.toBeChecked();
     expect(
       screen.getByRole('radio', {
-        name: 'Select Send my contact information along with usage statistics',
+        name: 'Send my contact information along with usage statistics',
       })
     ).not.toBeChecked();
   });
