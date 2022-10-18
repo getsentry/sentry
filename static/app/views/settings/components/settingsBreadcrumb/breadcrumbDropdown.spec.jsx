b2ddd8271e0728f6c9969f3f816146e0ffc6b452
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-10-19 01:24:31 +1100

    ref(js): Simplify BreadcrumbDropdown (#40165)

--- static/app/views/settings/components/settingsBreadcrumb/breadcrumbDropdown.spec.jsx
+++ static/app/views/settings/components/settingsBreadcrumb/breadcrumbDropdown.spec.jsx
@@ -2,8 +2,6 @@ import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import BreadcrumbDropdown from 'sentry/views/settings/components/settingsBreadcrumb/breadcrumbDropdown';
 
-jest.useFakeTimers();
-
 describe('Settings Breadcrumb Dropdown', () => {
   const selectMock = jest.fn();
   const items = [
@@ -21,55 +19,39 @@ describe('Settings Breadcrumb Dropdown', () => {
     createWrapper();
     expect(screen.getByText('Test')).toBeInTheDocument();
     userEvent.hover(screen.getByText('Test'));
-    jest.runAllTimers();
     expect(screen.getByText('foo')).toBeInTheDocument();
     expect(screen.getByText('bar')).toBeInTheDocument();
-  });
-
-  it('closes after 200ms when mouse leaves crumb', () => {
-    createWrapper();
-    userEvent.hover(screen.getByText('Test'));
-    jest.runAllTimers();
-    expect(screen.getByText('foo')).toBeInTheDocument();
-
-    userEvent.unhover(screen.getByText('Test'));
-    jest.advanceTimersByTime(10);
-    expect(screen.queryByText('foo')).not.toBeInTheDocument();
   });
 
   it('closes immediately after selecting an item', () => {
     createWrapper();
     userEvent.hover(screen.getByText('Test'));
-    jest.runAllTimers();
     expect(screen.getByText('foo')).toBeInTheDocument();
 
     userEvent.click(screen.getByText('foo'));
+    expect(selectMock).toHaveBeenCalled();
+
     expect(screen.queryByText('foo')).not.toBeInTheDocument();
   });
 
   it('stays open when hovered over crumb and then into dropdown menu', () => {
     createWrapper();
     userEvent.hover(screen.getByText('Test'));
-    jest.runAllTimers();
     expect(screen.getByText('foo')).toBeInTheDocument();
 
     userEvent.hover(screen.getByText('foo'));
-    jest.runAllTimers();
     expect(screen.getByText('foo')).toBeInTheDocument();
   });
 
   it('closes after entering dropdown and then leaving dropdown', () => {
     createWrapper();
     userEvent.hover(screen.getByText('Test'));
-    jest.runAllTimers();
     expect(screen.getByText('foo')).toBeInTheDocument();
 
     userEvent.hover(screen.getByText('foo'));
-    jest.runAllTimers();
     expect(screen.getByText('foo')).toBeInTheDocument();
     userEvent.unhover(screen.getByText('foo'));
 
-    jest.runAllTimers();
     expect(screen.queryByText('foo')).not.toBeInTheDocument();
   });
 });
