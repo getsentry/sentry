b12347836755f2374bfc55348e5d4bfa90d4eb11
Malachi Willey <malwilley@gmail.com>
2022-10-18 12:23:02 -0500

    fix(settings): Ensure that slugified inputs remove invalid characters [UP-178] (#40118)
    It's pretty easy to submit invalid slugs in the settings pages, so this tightens up the `slugify` function to match the backend validation logic.

--- static/app/utils/slugify.tsx
+++ static/app/utils/slugify.tsx
@@ -1,5 +1,14 @@
-// XXX: This is NOT an exhaustive slugify function
-// Only forces lowercase and replaces spaces with hyphens
-export default function slugify(str: any): string {
-  return typeof str === 'string' ? str.toLowerCase().replace(' ', '-') : '';
+/**
+ * Transforms the given string to a slugified version. (e.g. "My Project" => "my-project")
+ *
+ * Allows only lowercase alphanumeric values, hyphens, and underscores (should match backend validation rules).
+ * Normalizes special characters to a-z where applicable (accents, ligatures, etc).
+ * Converts spaces to hyphens.
+ */
+export default function slugify(str: string): string {
+  return str
+    .normalize('NFKD') // Converts accents/ligatures/etc to latin alphabet
+    .toLowerCase()
+    .replace(' ', '-')
+    .replace(/[^a-z0-9-_]/g, ''); // Remove all invalid characters
 }
