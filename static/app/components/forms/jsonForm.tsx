9f12f339d8b063be2d2889ce0e4a58fa40850ed1
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-10-19 09:13:34 +1100

    ref(js): Remove UNSAFE_componentWillReceiveProps from JsonForm (#39213)

--- static/app/components/forms/jsonForm.tsx
+++ static/app/components/forms/jsonForm.tsx
@@ -44,9 +44,9 @@ class JsonForm extends Component<Props, State> {
     this.scrollToHash();
   }
 
-  UNSAFE_componentWillReceiveProps(nextProps: Props) {
-    if (nextProps.location && this.props.location.hash !== nextProps.location.hash) {
-      const hash = nextProps.location.hash;
+  componentDidUpdate(prevProps: Props) {
+    if (this.props.location && this.props.location.hash !== prevProps.location.hash) {
+      const hash = this.props.location.hash;
       this.scrollToHash(hash);
       this.setState({highlighted: hash});
     }
