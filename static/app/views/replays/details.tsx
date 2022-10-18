eceb84079106998e5ea7bcada9471a4ea8f538c9
jesus padron <jesuspadron4497@gmail.com>
2022-10-18 15:58:05 -0400

    feat: add unit tests for get initial timie offset func

--- static/app/views/replays/details.tsx
+++ static/app/views/replays/details.tsx
@@ -9,16 +9,15 @@ import {
   Provider as ReplayContextProvider,
   useReplayContext,
 } from 'sentry/components/replays/replayContext';
-import {relativeTimeInMs} from 'sentry/components/replays/utils';
 import {t} from 'sentry/locale';
 import {PageContent} from 'sentry/styles/organization';
-import {defined} from 'sentry/utils';
 import useReplayData from 'sentry/utils/replays/hooks/useReplayData';
 import useReplayLayout from 'sentry/utils/replays/hooks/useReplayLayout';
 import useReplayPageview from 'sentry/utils/replays/hooks/useReplayPageview';
 import Layout from 'sentry/views/replays/detail/layout';
 import Page from 'sentry/views/replays/detail/page';
 import type {ReplayRecord} from 'sentry/views/replays/types';
+import {getInitialTimeOffset} from 'sentry/views/replays/utils';
 
 type Props = RouteComponentProps<
   {orgId: string; replaySlug: string},
@@ -44,36 +43,6 @@ function ReplayDetails({
   });
 
   const startTimestampMs = replayRecord?.startedAt.getTime() ?? 0;
-
-  const getInitialTimeOffset = () => {
-    if (initialTimeOffset) {
-      return initialTimeOffset;
-    }
-
-    // If the user has navigated to the replay from an event, then we want to
-    // start the video at the time of the event.
-    if (defined(eventTimestamp) && startTimestampMs) {
-      // check if the event timestamp is the correct format
-      let eventTimestampMs = 0;
-      if (eventTimestamp.length === 13) {
-        eventTimestampMs = Number(eventTimestamp);
-      } else {
-        eventTimestampMs = new Date(eventTimestamp).getTime();
-      }
-
-      // check if our event timestamp is within the range of the replay
-      if (eventTimestampMs >= startTimestampMs) {
-        initialTimeOffset = relativeTimeInMs(eventTimestampMs, startTimestampMs) / 1000;
-      }
-    }
-
-    // if the event timestamp is not the correct format or no timestamps are provided, default to the start of the replay
-    if (!defined(initialTimeOffset) || Number.isNaN(initialTimeOffset)) {
-      initialTimeOffset = 0;
-    }
-
-    return initialTimeOffset;
-  };
 
   if (!fetching && !replay && fetchError) {
     if (fetchError.statusText === 'Not Found') {
@@ -135,7 +104,14 @@ function ReplayDetails({
   }
 
   return (
-    <ReplayContextProvider replay={replay} initialTimeOffset={getInitialTimeOffset()}>
+    <ReplayContextProvider
+      replay={replay}
+      initialTimeOffset={getInitialTimeOffset({
+        eventTimestamp,
+        initialTimeOffset,
+        startTimestampMs,
+      })}
+    >
       <LoadedDetails orgSlug={orgSlug} replayRecord={replayRecord} />
     </ReplayContextProvider>
   );
