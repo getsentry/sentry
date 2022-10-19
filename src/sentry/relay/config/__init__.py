557985995d94364f3d5045c7d2b03a7d01063b52
Andrii Soldatenko <andrii.soldatenko@sentry.io>
2022-10-19 17:31:42 +0200

    feat(dynamic-sampling): Handles updating `ProjectConfig` with uniform DS rule for v2 [TET-465] (#40180)
    This PR forces your uniform rule by your plan
    or respect old logic. If both feature flags are enabled
    `dynamic-sampling-basic` flag takes the highest precedence.
    
    Co-authored-by: Joris Bayer <joris.bayer@sentry.io>

--- src/sentry/relay/config/__init__.py
+++ src/sentry/relay/config/__init__.py
@@ -10,6 +10,7 @@ from sentry_sdk import Hub, capture_exception
 from sentry import features, killswitches, quotas, utils
 from sentry.constants import ObjectStatus
 from sentry.datascrubbing import get_datascrubbing_settings, get_pii_config
+from sentry.dynamic_sampling.utils import NoneSampleRateException, generate_uniform_rule
 from sentry.grouping.api import get_grouping_config_dict_for_project
 from sentry.ingest.inbound_filters import (
     FilterStatKeys,
@@ -178,27 +179,40 @@ def _get_project_config(project, full_config=True, project_keys=None):
         "organizations:server-side-sampling",
         project.organization,
     )
-    if allow_dynamic_sampling:
-        dynamic_sampling = project.get_option("sentry:dynamic_sampling")
-        if dynamic_sampling is not None:
-            # filter out rules that do not have active set to True
-            active_rules = []
-            for rule in dynamic_sampling["rules"]:
-                if rule.get("active"):
-                    inner_rule = rule["condition"]["inner"]
-                    if (
-                        inner_rule
-                        and inner_rule[0]["name"] == "trace.release"
-                        and inner_rule[0]["value"] == ["latest"]
-                    ):
-                        # get latest overall (no environments filters)
-                        environment = None
-                        rule["condition"]["inner"][0]["value"] = get_latest_release(
-                            [project], environment
-                        )
-                    active_rules.append(rule)
+    allow_dynamic_sampling_basic = features.has(
+        "organizations:dynamic-sampling-basic",
+        project.organization,
+    )
 
-            cfg["config"]["dynamicSampling"] = {"rules": active_rules}
+    # In this case we should override old conditionnal rules if they exists
+    # or just return uniform rule
+    if allow_dynamic_sampling_basic:
+        try:
+            cfg["config"]["dynamicSampling"] = {"rules": [generate_uniform_rule(project)]}
+        except NoneSampleRateException:
+            # just to be consistent with old code, where if there is no active active_rules
+            # we return empty list
+            cfg["config"]["dynamicSampling"] = {"rules": []}
+    elif allow_dynamic_sampling:
+        dynamic_sampling = project.get_option("sentry:dynamic_sampling")
+        # filter out rules that do not have active set to True
+        active_rules = []
+        for rule in dynamic_sampling["rules"]:
+            if rule.get("active"):
+                inner_rule = rule["condition"]["inner"]
+                if (
+                    inner_rule
+                    and inner_rule[0]["name"] == "trace.release"
+                    and inner_rule[0]["value"] == ["latest"]
+                ):
+                    # get latest overall (no environments filters)
+                    environment = None
+                    rule["condition"]["inner"][0]["value"] = get_latest_release(
+                        [project], environment
+                    )
+                active_rules.append(rule)
+
+        cfg["config"]["dynamicSampling"] = {"rules": active_rules}
 
     # Limit the number of custom measurements
     cfg["config"]["measurements"] = get_measurements_config()
