dccd35659f3515af0ff08aa46c47e4479332da51
Tony Xiao <txiao@sentry.io>
2022-10-19 11:41:36 -0400

    ref(profiling): Sample transactions for process.profiles celery task (#40207)

--- src/sentry/utils/sdk.py
+++ src/sentry/utils/sdk.py
@@ -114,6 +114,7 @@ SAMPLED_TASKS = {
     "sentry.replays.tasks.delete_recording_segments": settings.SAMPLED_DEFAULT_RATE,
     "sentry.tasks.weekly_reports.schedule_organizations": settings.SAMPLED_DEFAULT_RATE,
     "sentry.tasks.weekly_reports.prepare_organization_report": 0.1,
+    "sentry.profiles.task.process_profile": 0.01,
 }
 
 if settings.ADDITIONAL_SAMPLED_TASKS:
