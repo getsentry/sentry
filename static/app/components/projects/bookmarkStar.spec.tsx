398cbd38fb61a9019ec44e0a95319974f11e5883
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-10-19 12:29:26 +1100

    feat(ui): Make BookmarkStar have optimistic visual update
    Refs UP-179

--- static/app/components/projects/bookmarkStar.spec.tsx
+++ static/app/components/projects/bookmarkStar.spec.tsx
@@ -1,82 +1,90 @@
-import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
+import {render, screen, userEvent, waitFor} from 'sentry-test/reactTestingLibrary';
 
 import BookmarkStar from 'sentry/components/projects/bookmarkStar';
+import ProjectsStore from 'sentry/stores/projectsStore';
 
 describe('BookmarkStar', function () {
-  let projectMock: jest.Mock;
+  const project = TestStubs.Project();
 
   beforeEach(function () {
-    projectMock = MockApiClient.addMockResponse({
-      url: '/projects/org-slug/project-slug/',
-      method: 'PUT',
-      body: TestStubs.Project({isBookmarked: false, platform: 'javascript'}),
-    });
+    ProjectsStore.loadInitialData([project]);
   });
 
   afterEach(function () {
+    ProjectsStore.reset();
     MockApiClient.clearMockResponses();
   });
 
   it('renders', function () {
     const {container} = render(
-      <BookmarkStar
-        organization={TestStubs.Organization()}
-        project={TestStubs.Project()}
-      />
+      <BookmarkStar organization={TestStubs.Organization()} project={project} />
     );
 
     expect(container).toSnapshot();
   });
 
-  it('can star', function () {
-    render(
-      <BookmarkStar
-        organization={TestStubs.Organization()}
-        project={TestStubs.Project()}
-      />
-    );
+  it('can star', async function () {
+    render(<BookmarkStar organization={TestStubs.Organization()} project={project} />);
+
+    const projectMock = MockApiClient.addMockResponse({
+      url: '/projects/org-slug/project-slug/',
+      method: 'PUT',
+      body: TestStubs.Project({isBookmarked: true, platform: 'javascript'}),
+    });
 
     expect(screen.getByRole('button', {pressed: false})).toBeInTheDocument();
     userEvent.click(screen.getByRole('button'));
+
+    // Visually optimistically updated
+    expect(screen.getByRole('button', {pressed: true})).toBeInTheDocument();
 
     expect(projectMock).toHaveBeenCalledWith(
       '/projects/org-slug/project-slug/',
       expect.objectContaining({data: {isBookmarked: true}})
     );
+
+    // Not yet updated in the project store
+    expect(ProjectsStore.getBySlug(project.slug)?.isBookmarked).toBe(false);
+
+    // Project store is updated
+    await waitFor(() => {
+      const updatedProject = ProjectsStore.getBySlug(project.slug);
+      expect(updatedProject?.isBookmarked).toBe(true);
+    });
   });
 
-  it('can unstar', function () {
+  it('can unstar', async function () {
     render(
       <BookmarkStar
         organization={TestStubs.Organization()}
-        project={TestStubs.Project({
-          isBookmarked: true,
-        })}
+        project={TestStubs.Project({isBookmarked: true})}
       />
     );
 
+    const projectMock = MockApiClient.addMockResponse({
+      url: '/projects/org-slug/project-slug/',
+      method: 'PUT',
+      body: TestStubs.Project({isBookmarked: false, platform: 'javascript'}),
+    });
+
     expect(screen.getByRole('button', {pressed: true})).toBeInTheDocument();
     userEvent.click(screen.getByRole('button'));
+
+    // Visually optimistically updated
+    expect(screen.getByRole('button', {pressed: false})).toBeInTheDocument();
 
     expect(projectMock).toHaveBeenCalledWith(
       '/projects/org-slug/project-slug/',
       expect.objectContaining({data: {isBookmarked: false}})
     );
-  });
 
-  it('takes a manual isBookmarked prop', async function () {
-    render(
-      <BookmarkStar
-        organization={TestStubs.Organization()}
-        project={TestStubs.Project()}
-        isBookmarked
-      />
-    );
+    // Not yet updated in the project store
+    expect(ProjectsStore.getBySlug(project.slug)?.isBookmarked).toBe(false);
 
-    expect(screen.getByRole('button', {pressed: true})).toBeInTheDocument();
-    userEvent.click(screen.getByRole('button'));
-
-    // State if isBookmarked is maintained via the prop
-    expect(await screen.findByRole('button', {pressed: true})).toBeInTheDocument();
+    // Project store is updated
+    await waitFor(() => {
+      const updatedProject = ProjectsStore.getBySlug(project.slug);
+      expect(updatedProject?.isBookmarked).toBe(false);
+    });
   });
 });
