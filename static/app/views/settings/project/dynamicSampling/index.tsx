4690246a8a42b92cf6664f94cbc6f253aa26cdb3
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-10-20 14:37:56 +0200

    ref(sampling): Add new opinionated UI - (#40296)

--- static/app/views/settings/project/dynamicSampling/index.tsx
+++ static/app/views/settings/project/dynamicSampling/index.tsx
@@ -13,17 +13,21 @@ type Props = {
 export default function DynamicSamplingContainer({project}: Props) {
   const organization = useOrganization();
 
-  if (!organization.features.includes('dynamic-sampling-total-transaction-packaging')) {
-    return <ServerSideSampling project={project} />;
+  if (
+    organization.features.includes('server-side-sampling') &&
+    (organization.features.includes('dynamic-sampling-opinionated') ||
+      organization.features.includes('dynamic-sampling-basic'))
+  ) {
+    return (
+      <Feature
+        features={['server-side-sampling', 'server-side-sampling-ui']}
+        hookName="feature-disabled:dynamic-sampling-basic"
+        organization={organization}
+      >
+        <DynamicSampling project={project} />
+      </Feature>
+    );
   }
 
-  return (
-    <Feature
-      features={['server-side-sampling', 'server-side-sampling-ui']}
-      hookName="feature-disabled:dynamic-sampling-basic"
-      organization={organization}
-    >
-      <DynamicSampling project={project} />
-    </Feature>
-  );
+  return <ServerSideSampling project={project} />;
 }
--- static/app/views/settings/project/dynamicSampling/modals/styles.tsx
+++
@@ -1,9 +0,0 @@
-import {css} from '@emotion/react';
-
-import commonTheme from 'sentry/utils/theme';
-
-export const responsiveModal = css`
-  @media (max-width: ${commonTheme.breakpoints.small}) {
-    width: 100%;
-  }
-`;
