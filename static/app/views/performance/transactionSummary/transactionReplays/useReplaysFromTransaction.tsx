261805fe7266e564ad7e1a96ba1e6c577b613a6d
Dane Grant <danecando@gmail.com>
2022-10-20 17:45:12 -0400

    feat(replays): TX Replay List Percentile Filtering (#40138)
    This PR adds the percentiles filter to the Transaction Replay List view.
    The goal is to make it easier to find useful replays from transactions.
    
    ### Changes
    - Adds some new shared utility functions for duplicated functionality
    from Transaction Events. (`getPercentilesEventView`,
    `mapPercentileValues`)
    - Adds the percentile select field and handler to `ReplaysContent`
    - Adds getFilteredEventView utility that takes the search params and
    creates a proper `EventView` to query filtered tx events
    - Adds a `fetchPercentiles` utility to get the percentiles data that is
    used to create the filtered `EventView`
    - Adds 'event.type' filter to the events query to make sure only tx
    events are queried
    
    
    ### Test Plan
    - [x] No regressions to the Transaction Events table functionality
    - [x] No regressions to the Transactions Replays table functionality
    - [x] Filter actions is properly styled on desktop and mobile with added
    percentiles filter
    - [x] Percentiles filter handler creates a proper query on
    transaction.duration with the right percentile value to the tx events
    query/endpoint
    - [x] p100 is the returns same results as the default initial query 
    - [x] Styles are correct in dark mode
    
    ![Screen Shot 2022-10-18 at 10 06 46
    AM](https://user-images.githubusercontent.com/3721977/196453745-696ce174-a939-4fe9-9451-191add6ecb58.png)
    ![Screen Shot 2022-10-18 at 10 06 59
    AM](https://user-images.githubusercontent.com/3721977/196453749-e340c3e2-9bdd-43f0-ad18-6faae9f7e9c0.png)
    ![Screen Shot 2022-10-18 at 10 07 26
    AM](https://user-images.githubusercontent.com/3721977/196453752-93f4586b-04be-4bbb-8fcd-12eb26538ccf.png)
    ![Screen Shot 2022-10-18 at 10 07 39
    AM](https://user-images.githubusercontent.com/3721977/196453755-8be6ee37-f215-4706-9bd9-ee8cfb80ada2.png)
    
    
    Closes #39737

--- static/app/views/performance/transactionSummary/transactionReplays/useReplaysFromTransaction.tsx
+++ static/app/views/performance/transactionSummary/transactionReplays/useReplaysFromTransaction.tsx
@@ -1,4 +1,5 @@
 import {useCallback, useEffect, useState} from 'react';
+import * as Sentry from '@sentry/react';
 import {Location} from 'history';
 
 import type {Client} from 'sentry/api';
@@ -11,6 +12,7 @@ import fetchReplayList, {
   DEFAULT_SORT,
   REPLAY_LIST_FIELDS,
 } from 'sentry/utils/replays/fetchReplayList';
+import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import useApi from 'sentry/utils/useApi';
 import {ReplayListRecord} from 'sentry/views/replays/types';
 
@@ -18,21 +20,34 @@ export type ReplayListRecordWithTx = ReplayListRecord & {
   txEvent: {[x: string]: any};
 };
 
+import {SpanOperationBreakdownFilter} from '../filter';
+import {
+  EventsDisplayFilterName,
+  getEventsFilterOptions,
+  getPercentilesEventView,
+  mapPercentileValues,
+  PercentileValues,
+} from '../transactionEvents/utils';
+
 type State = Awaited<ReturnType<typeof fetchReplayList>> & {
   eventView: undefined | EventView;
   replays?: ReplayListRecordWithTx[];
 };
 
 type Options = {
+  eventsDisplayFilterName: EventsDisplayFilterName;
   eventsWithReplaysView: EventView;
   location: Location;
   organization: Organization;
+  spanOperationBreakdownFilter: SpanOperationBreakdownFilter;
 };
 
 function useReplaysFromTransaction({
   eventsWithReplaysView,
   location,
   organization,
+  spanOperationBreakdownFilter,
+  eventsDisplayFilterName,
 }: Options) {
   const api = useApi();
   const [data, setData] = useState<State>({
@@ -44,9 +59,24 @@ function useReplaysFromTransaction({
   });
 
   const load = useCallback(async () => {
+    const percentilesView = getPercentilesEventView(eventsWithReplaysView.clone());
+    const percentileData = await fetchPercentiles({
+      api,
+      eventView: percentilesView,
+      location,
+      organization,
+    });
+    const percentiles = mapPercentileValues(percentileData);
+    const filteredEventView = getFilteredEventView({
+      eventView: eventsWithReplaysView,
+      percentiles,
+      spanOperationBreakdownFilter,
+      eventsDisplayFilterName,
+    });
+
     const eventsData = await fetchEventsWithReplay({
       api,
-      eventView: eventsWithReplaysView,
+      eventView: filteredEventView ?? eventsWithReplaysView,
       location,
       organization,
     });
@@ -100,7 +130,14 @@ function useReplaysFromTransaction({
       eventView,
       replays,
     });
-  }, [api, eventsWithReplaysView, location, organization]);
+  }, [
+    api,
+    eventsWithReplaysView,
+    location,
+    organization,
+    spanOperationBreakdownFilter,
+    eventsDisplayFilterName,
+  ]);
 
   useEffect(() => {
     load();
@@ -129,8 +166,57 @@ async function fetchEventsWithReplay({
 
     return data.data;
   } catch (err) {
+    Sentry.captureException(err);
     return null;
   }
+}
+
+async function fetchPercentiles({
+  api,
+  eventView,
+  location,
+  organization,
+}: {
+  api: Client;
+  eventView: EventView;
+  location: Location;
+  organization: Organization;
+}) {
+  try {
+    const [data] = await doDiscoverQuery<TableData>(
+      api,
+      `/organizations/${organization.slug}/events/`,
+      eventView.getEventsAPIPayload(location)
+    );
+
+    return data.data[0];
+  } catch (err) {
+    Sentry.captureException(err);
+    return null;
+  }
+}
+
+function getFilteredEventView({
+  percentiles,
+  spanOperationBreakdownFilter,
+  eventsDisplayFilterName,
+  eventView,
+}: {
+  eventView: EventView;
+  eventsDisplayFilterName: EventsDisplayFilterName;
+  percentiles: PercentileValues;
+  spanOperationBreakdownFilter: SpanOperationBreakdownFilter;
+}) {
+  const filter = getEventsFilterOptions(spanOperationBreakdownFilter, percentiles)[
+    eventsDisplayFilterName
+  ];
+  const filteredEventView = eventView.clone();
+  if (filteredEventView && filter?.query) {
+    const query = new MutableSearch(filteredEventView.query);
+    filter.query.forEach(item => query.setFilterValues(item[0], [item[1]]));
+    filteredEventView.query = query.formatString();
+  }
+  return filteredEventView;
 }
 
 export default useReplaysFromTransaction;
