1c7fe5006a35aecef794edd88a6d0090b5a1b904
Malachi Willey <malwilley@gmail.com>
2022-10-21 09:07:50 -0700

    Revert "Remove some uses of OrganizationContext and RouteContext for demonstration"
    This reverts commit 6dce86738a3cf93c17f50d8b3fcec5b92d6eeeac.

--- static/app/components/events/eventEntries.spec.tsx
+++ static/app/components/events/eventEntries.spec.tsx
@@ -6,8 +6,9 @@ import EventEntries from 'sentry/components/events/eventEntries';
 import {Group, IssueCategory} from 'sentry/types';
 import {EntryType, Event} from 'sentry/types/event';
 import {OrganizationContext} from 'sentry/views/organizationContext';
+import {RouteContext} from 'sentry/views/routeContext';
 
-const {organization, project} = initializeData({
+const {organization, project, router} = initializeData({
   features: ['performance-issues'],
 });
 
@@ -15,13 +16,24 @@ const api = new MockApiClient();
 
 async function renderComponent(event: Event, errors?: Array<Error>) {
   render(
-    <EventEntries
-      organization={organization}
-      event={{...event, errors: errors ?? event.errors}}
-      project={project}
-      location={location}
-      api={api}
-    />
+    <OrganizationContext.Provider value={organization}>
+      <RouteContext.Provider
+        value={{
+          router,
+          location: router.location,
+          params: {},
+          routes: [],
+        }}
+      >
+        <EventEntries
+          organization={organization}
+          event={{...event, errors: errors ?? event.errors}}
+          project={project}
+          location={location}
+          api={api}
+        />
+      </RouteContext.Provider>
+    </OrganizationContext.Provider>
   );
 
   const alertSummaryInfo = await screen.findByTestId('event-error-alert');
@@ -345,14 +357,25 @@ describe('EventEntries', function () {
       };
 
       render(
-        <EventEntries
-          organization={organization}
-          event={newEvent}
-          project={project}
-          location={location}
-          api={api}
-          group={group}
-        />
+        <OrganizationContext.Provider value={organization}>
+          <RouteContext.Provider
+            value={{
+              router,
+              location: router.location,
+              params: {},
+              routes: [],
+            }}
+          >
+            <EventEntries
+              organization={organization}
+              event={newEvent}
+              project={project}
+              location={location}
+              api={api}
+              group={group}
+            />
+          </RouteContext.Provider>
+        </OrganizationContext.Provider>
       );
 
       const eventEntriesContainer = screen.getByTestId('event-entries-loading-false');
