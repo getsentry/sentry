f5811f946f7280f3610c5e7fbbee6de9aeb39b46
Lyn <lyn.nagara@gmail.com>
2022-10-21 10:56:12 -0700

    ref: This option is no longer used

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -422,8 +422,6 @@ register("eventstream:kafka-headers", default=True)
 # Post process forwarder options
 # Gets data from Kafka headers
 register("post-process-forwarder:kafka-headers", default=True)
-# Number of threads to use for post processing
-register("post-process-forwarder:concurrency", default=1)
 
 # Subscription queries sampling rate
 register("subscriptions-query.sample-rate", default=0.01)
