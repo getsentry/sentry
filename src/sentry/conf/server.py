69f966b5e0888738b228949f56627c1dd2fb5252
Snigdha Sharma <16563948+snigdhas@users.noreply.github.com>
2022-10-24 12:51:51 -0700

    feat(code-mappings): Add new task to find projects with missing code mappings (#40271)
    Add new task to find projects with missing code mappings.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -685,6 +685,7 @@ CELERY_QUEUES = [
     Queue("replays.delete_replay", routing_key="replays.delete_replay"),
     Queue("counters-0", routing_key="counters-0"),
     Queue("triggers-0", routing_key="triggers-0"),
+    Queue("derive_code_mappings", routing_key="derive_code_mappings"),
 ]
 
 for queue in CELERY_QUEUES:
