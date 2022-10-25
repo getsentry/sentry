ea04b586bff73258405c38e7f6fff55aed47d7b6
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-10-25 17:20:50 -0400

    Cancel API requests on unmount

--- static/app/components/performance/searchBar.tsx
+++ static/app/components/performance/searchBar.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {browserHistory} from 'react-router';
 import styled from '@emotion/styled';
 import debounce from 'lodash/debounce';
@@ -31,6 +31,11 @@ function SearchBar(props: SearchBarProps) {
 
   const api = useApi();
   const eventView = _eventView.clone();
+
+  useEffect(() => {
+    // Cancel in-flight requests when the component unmounts
+    api.clear();
+  });
 
   const prepareQuery = (query: string) => {
     const prependedChar = query[0] === '*' ? '' : '*';
