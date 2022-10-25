fa3e8243c20dad18b2767f48ced13eecffc333b7
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-10-25 18:37:38 -0400

    Add test for output

--- static/app/components/performance/searchBar.spec.tsx
+++ static/app/components/performance/searchBar.spec.tsx
@@ -96,4 +96,25 @@ describe('SearchBar', () => {
       })
     );
   });
+
+  it('Shows the returned data in the dropdown', async () => {
+    eventsMock = MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/eventsv2/`,
+      body: {
+        data: [{transaction: 'clients.call'}, {transaction: 'clients.fetch'}],
+      },
+    });
+
+    render(<SearchBar {...testProps} />);
+
+    userEvent.type(screen.getByRole('textbox'), 'proje');
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    await waitForElementToBeRemoved(() => screen.getByTestId('loading-indicator'));
+
+    expect(screen.getByText(textWithMarkupMatcher('clients.call'))).toBeInTheDocument();
+    expect(screen.getByText(textWithMarkupMatcher('clients.fetch'))).toBeInTheDocument();
+  });
 });
