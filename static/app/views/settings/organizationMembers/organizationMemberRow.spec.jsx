c08f37ea0c4d98f8fa2b21156686a421bc88a1c9
Evan Purkhiser <evanpurkhiser@gmail.com>
2022-10-25 17:31:17 +1100

    test(js): Convert OrganizationMemberRow to RTL (#40490)

--- static/app/views/settings/organizationMembers/organizationMemberRow.spec.jsx
+++ static/app/views/settings/organizationMembers/organizationMemberRow.spec.jsx
@@ -1,4 +1,4 @@
-import {mountWithTheme} from 'sentry-test/enzyme';
+import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import OrganizationMemberRow from 'sentry/views/settings/organizationMembers/organizationMemberRow';
 
@@ -41,195 +41,164 @@ describe('OrganizationMemberRow', function () {
     onLeave: () => {},
   };
 
-  const resendButton = 'StyledButton[aria-label="Resend invite"]';
-  const resendSsoButton = 'StyledButton[aria-label="Resend SSO link"]';
-  const leaveButton = 'StyledButton[aria-label="Leave"]';
-  const removeButton = 'StyledButton[aria-label="Remove"]';
+  function resendButton() {
+    return screen.queryByRole('button', {name: 'Resend invite'});
+  }
 
-  beforeEach(function () {});
+  function resendSsoButton() {
+    return screen.queryByRole('button', {name: 'Resend SSO link'});
+  }
 
-  it('does not have 2fa warning if user has 2fa', function () {
-    const wrapper = mountWithTheme(
-      <OrganizationMemberRow
-        {...defaultProps}
-        member={{
-          ...member,
-          user: {
-            ...member.user,
-            has2fa: true,
-          },
-        }}
-      />
-    );
-    expect(wrapper.find('IconCheckmark')).toHaveLength(1);
-    expect(wrapper.find('IconFlag')).toHaveLength(0);
-  });
+  function leaveButton() {
+    return screen.queryByRole('button', {name: 'Leave'});
+  }
 
-  it('has 2fa warning if user does not have 2fa enabled', function () {
-    const wrapper = mountWithTheme(
-      <OrganizationMemberRow
-        {...defaultProps}
-        member={{
-          ...member,
-          user: {
-            ...member.user,
-            has2fa: false,
-          },
-        }}
-      />
-    );
-    expect(wrapper.find('IconCheckmark')).toHaveLength(0);
-    expect(wrapper.find('IconFlag')).toHaveLength(1);
+  function removeButton() {
+    return screen.queryByRole('button', {name: 'Remove'});
+  }
+
+  describe('two factor', function () {
+    it('does not have 2fa warning if user has 2fa', function () {
+      render(
+        <OrganizationMemberRow
+          {...defaultProps}
+          member={{
+            ...member,
+            user: {...member.user, has2fa: true},
+          }}
+        />
+      );
+
+      expect(screen.getByText('2FA Enabled')).toBeInTheDocument();
+      expect(screen.queryByText('2FA Not Enabled')).not.toBeInTheDocument();
+    });
+
+    it('has 2fa warning if user does not have 2fa enabled', function () {
+      render(
+        <OrganizationMemberRow
+          {...defaultProps}
+          member={{
+            ...member,
+            user: {...member.user, has2fa: false},
+          }}
+        />
+      );
+
+      expect(screen.getByText('2FA Not Enabled')).toBeInTheDocument();
+      expect(screen.queryByText('2FA Enabled')).not.toBeInTheDocument();
+    });
   });
 
   describe('Pending user', function () {
     const props = {
       ...defaultProps,
-      member: {
-        ...member,
-        pending: true,
-      },
+      member: {...member, pending: true},
     };
 
     it('has "Invited" status, no "Resend Invite"', function () {
-      const wrapper = mountWithTheme(
-        <OrganizationMemberRow
-          {...props}
-          member={{
-            ...member,
-            pending: true,
-          }}
-        />
-      );
+      render(<OrganizationMemberRow {...props} />);
 
-      expect(wrapper.find('[data-test-id="member-role"]').text()).toBe('Invited Member');
-      expect(wrapper.find(resendButton).prop('disabled')).toBe(true);
+      expect(screen.getByTestId('member-role')).toHaveTextContent('Invited Member');
+      expect(resendButton()).toBeDisabled();
     });
 
     it('has "Resend Invite" button only if `canAddMembers` is true', function () {
-      const wrapper = mountWithTheme(<OrganizationMemberRow {...props} canAddMembers />);
+      render(<OrganizationMemberRow {...props} canAddMembers />);
 
-      expect(wrapper.find('[data-test-id="member-role"]').text()).toBe('Invited Member');
-      expect(wrapper.find(resendButton).prop('disabled')).toBe(false);
+      expect(screen.getByTestId('member-role')).toHaveTextContent('Invited Member');
+      expect(resendButton()).toBeEnabled();
     });
 
     it('has the right inviting states', function () {
-      let wrapper = mountWithTheme(<OrganizationMemberRow {...props} canAddMembers />);
+      render(<OrganizationMemberRow {...props} canAddMembers />);
 
-      expect(wrapper.find(resendButton).exists()).toBe(true);
+      expect(resendButton()).toBeInTheDocument();
+    });
 
-      wrapper = mountWithTheme(
-        <OrganizationMemberRow {...props} canAddMembers status="loading" />
-      );
-
-      // Should have loader
-      expect(wrapper.find('LoadingIndicator')).toHaveLength(1);
-      // No Resend Invite button
-      expect(wrapper.find(resendButton).exists()).toBe(false);
-
-      wrapper = mountWithTheme(
-        <OrganizationMemberRow {...props} canAddMembers status="success" />
-      );
+    it('has loading state', function () {
+      render(<OrganizationMemberRow {...props} canAddMembers status="loading" />);
 
       // Should have loader
-      expect(wrapper.find('LoadingIndicator')).toHaveLength(0);
+      expect(screen.getByTestId('loading-indicator')).toBeInTheDocument();
+
       // No Resend Invite button
-      expect(wrapper.find(resendButton).exists()).toBe(false);
-      expect(wrapper.find('[data-test-id="member-status"]').text()).toBe('Sent!');
+      expect(resendButton()).not.toBeInTheDocument();
+    });
+
+    it('has success status', function () {
+      render(<OrganizationMemberRow {...props} canAddMembers status="success" />);
+
+      // Should not have loader
+      expect(screen.queryByTestId('loading-indicator')).not.toBeInTheDocument();
+
+      // No Resend Invite button
+      expect(resendButton()).not.toBeInTheDocument();
+      expect(screen.getByTestId('member-status')).toHaveTextContent('Sent!');
     });
   });
 
   describe('Expired user', function () {
     it('has "Expired" status', function () {
-      const wrapper = mountWithTheme(
+      render(
         <OrganizationMemberRow
           {...defaultProps}
           canAddMembers
-          member={{
-            ...member,
-            pending: true,
-            expired: true,
-          }}
+          member={{...member, pending: true, expired: true}}
         />
       );
 
-      expect(wrapper.find('[data-test-id="member-role"]').text()).toBe('Expired Invite');
-      expect(wrapper.find(resendButton).prop('disabled')).toBe(false);
+      expect(screen.getByTestId('member-role')).toHaveTextContent('Expired Invite');
+      expect(resendButton()).toBeEnabled();
     });
   });
 
   describe('Requires SSO Link', function () {
     const props = {
       ...defaultProps,
-      flags: {
-        'sso:link': false,
-      },
+      flags: {'sso:link': false},
       requireLink: true,
     };
 
     it('shows "Invited" status if user has not registered and not linked', function () {
-      const wrapper = mountWithTheme(
+      render(
         <OrganizationMemberRow
           {...props}
           canAddMembers
-          member={{
-            ...member,
-            pending: true,
-          }}
+          member={{...member, pending: true}}
         />
       );
 
-      expect(wrapper.find('[data-test-id="member-role"]').text()).toBe('Invited Member');
-      expect(wrapper.find(resendButton).prop('disabled')).toBe(false);
+      expect(screen.getByTestId('member-role')).toHaveTextContent('Invited Member');
+      expect(resendButton()).toBeEnabled();
     });
 
     it('shows "missing SSO link" message if user is registered and needs link', function () {
-      const wrapper = mountWithTheme(
-        <OrganizationMemberRow
-          {...props}
-          member={{
-            ...member,
-          }}
-        />
-      );
+      render(<OrganizationMemberRow {...props} />);
 
-      expect(wrapper.find('[data-test-id="member-role"]').text()).toBe('Member');
-      expect(wrapper.find(resendSsoButton).prop('disabled')).toBe(true);
+      expect(screen.getByTestId('member-role')).toHaveTextContent('Member');
+      expect(resendSsoButton()).toBeDisabled();
     });
 
     it('has "Resend SSO link" button only if `canAddMembers` is true and no link', function () {
-      const wrapper = mountWithTheme(
-        <OrganizationMemberRow
-          {...props}
-          canAddMembers
-          member={{
-            ...member,
-          }}
-        />
-      );
+      render(<OrganizationMemberRow {...props} canAddMembers />);
 
-      expect(wrapper.find(resendSsoButton).prop('disabled')).toBe(false);
+      expect(resendSsoButton()).toBeEnabled();
     });
 
     it('has 2fa warning if user is linked does not have 2fa enabled', function () {
-      const wrapper = mountWithTheme(
+      render(
         <OrganizationMemberRow
           {...defaultProps}
           member={{
             ...member,
-            flags: {
-              'sso:linked': true,
-            },
-            user: {
-              ...member.user,
-              has2fa: false,
-            },
+            flags: {'sso:linked': true},
+            user: {...member.user, has2fa: false},
           }}
         />
       );
 
-      expect(wrapper.find('IconCheckmark')).toHaveLength(0);
-      expect(wrapper.find('IconFlag')).toHaveLength(1);
+      expect(screen.getByText('2FA Not Enabled')).toBeInTheDocument();
+      expect(screen.queryByText('2FA Enabled')).not.toBeInTheDocument();
     });
   });
 
@@ -243,19 +212,17 @@ describe('OrganizationMemberRow', function () {
     };
 
     it('has button to leave organization and no button to remove', function () {
-      const wrapper = mountWithTheme(<OrganizationMemberRow {...props} memberCanLeave />);
+      render(<OrganizationMemberRow {...props} memberCanLeave />);
 
-      expect(wrapper.find(leaveButton).exists()).toBe(true);
-      expect(wrapper.find(removeButton).exists()).toBe(false);
+      expect(leaveButton()).toBeInTheDocument();
+      expect(removeButton()).not.toBeInTheDocument();
     });
 
     it('has disabled button to leave organization and no button to remove when member can not leave', function () {
-      const wrapper = mountWithTheme(
-        <OrganizationMemberRow {...props} memberCanLeave={false} />
-      );
+      render(<OrganizationMemberRow {...props} memberCanLeave={false} />);
 
-      expect(wrapper.find(leaveButton).prop('disabled')).toBe(true);
-      expect(wrapper.find(removeButton).exists()).toBe(false);
+      expect(leaveButton()).toBeDisabled();
+      expect(removeButton()).not.toBeInTheDocument();
     });
   });
 
@@ -265,23 +232,21 @@ describe('OrganizationMemberRow', function () {
     };
 
     it('does not have Leave button', function () {
-      const wrapper = mountWithTheme(<OrganizationMemberRow {...props} memberCanLeave />);
+      render(<OrganizationMemberRow {...props} memberCanLeave />);
 
-      expect(wrapper.find(leaveButton).exists()).toBe(false);
+      expect(leaveButton()).not.toBeInTheDocument();
     });
 
     it('has Remove disabled button when `canRemoveMembers` is false', function () {
-      const wrapper = mountWithTheme(<OrganizationMemberRow {...props} />);
+      render(<OrganizationMemberRow {...props} />);
 
-      expect(wrapper.find(removeButton).prop('disabled')).toBe(true);
+      expect(removeButton()).toBeDisabled();
     });
 
     it('has Remove button when `canRemoveMembers` is true', function () {
-      const wrapper = mountWithTheme(
-        <OrganizationMemberRow {...props} canRemoveMembers />
-      );
+      render(<OrganizationMemberRow {...props} canRemoveMembers />);
 
-      expect(wrapper.find(removeButton).prop('disabled')).toBe(false);
+      expect(removeButton()).toBeEnabled();
     });
   });
 });
