f39a2e7c95c42f6d10cce5d48e7bedef3cf8105a
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-10-25 18:32:51 -0400

    Add spec for the API call

--- static/app/components/performance/searchBar.spec.tsx
+++ static/app/components/performance/searchBar.spec.tsx
@@ -1,12 +1,34 @@
-import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
+import {act, render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import SearchBar, {SearchBarProps} from 'sentry/components/performance/searchBar';
 import EventView from 'sentry/utils/discover/eventView';
 
+jest.mock('lodash/debounce', () => {
+  const debounceMap = new Map();
+  const mockDebounce =
+    (fn, timeout) =>
+    (...args) => {
+      if (debounceMap.has(fn)) {
+        clearTimeout(debounceMap.get(fn));
+      }
+      debounceMap.set(
+        fn,
+        setTimeout(() => {
+          fn.apply(fn, args);
+          debounceMap.delete(fn);
+        }, timeout)
+      );
+    };
+  return mockDebounce;
+});
+
 describe('SearchBar', () => {
+  let eventsMock;
+  const organization = TestStubs.Organization();
+
   const testProps: SearchBarProps = {
     onSearch: jest.fn(),
-    organization: TestStubs.Organization(),
+    organization,
     eventView: EventView.fromSavedQuery({
       id: '',
       name: '',
@@ -17,14 +39,53 @@ describe('SearchBar', () => {
     query: '',
   };
 
-  beforeEach(() => {
-    jest.resetAllMocks();
+  beforeAll(() => {
+    jest.useFakeTimers();
   });
 
-  it('Accepts user input', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+
+    eventsMock = MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/eventsv2/`,
+      body: {data: []},
+    });
+  });
+
+  afterAll(() => {
+    jest.useRealTimers();
+  });
+
+  it('Accepts user input', async () => {
+    render(<SearchBar {...testProps} />);
+
+    userEvent.type(screen.getByRole('textbox'), 'clie');
+    expect(screen.getByRole('textbox')).toHaveValue('clie');
+
+    act(() => {
+      jest.runAllTimers();
+    });
+
+    await screen.findByTestId('loading-indicator');
+  });
+
+  it('Sends user input as transaction search', async () => {
     render(<SearchBar {...testProps} />);
 
     userEvent.type(screen.getByRole('textbox'), 'proje');
-    expect(screen.getByRole('textbox')).toHaveValue('proje');
+    act(() => {
+      jest.runAllTimers();
+    });
+    await screen.findByTestId('loading-indicator');
+
+    expect(eventsMock).toHaveBeenCalledTimes(1);
+    expect(eventsMock).toHaveBeenCalledWith(
+      '/organizations/org-slug/eventsv2/',
+      expect.objectContaining({
+        query: expect.objectContaining({
+          query: 'transaction:*proje* event.type:transaction',
+        }),
+      })
+    );
   });
 });
