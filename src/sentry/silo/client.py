c43971c7684a9f36ea981b6c50a935b896e2f2f6
Leander Rodrigues <leander.rodrigues@sentry.io>
2022-10-25 13:20:32 -0700

    ✨ Introduce formal clients + docs

--- src/sentry/silo/client.py
+++ src/sentry/silo/client.py
@@ -1,75 +1,65 @@
 from __future__ import annotations
 
-from abc import ABC, abstractmethod
+from typing import List
 
 from django.conf import settings
-from requests import Response
 
-from sentry.http import build_session
+from sentry.shared_integrations.client.base import BaseApiClient
 from sentry.silo.base import SiloMode
 from sentry.types.region import get_region_by_name
-from sentry.utils import json
-
-"""
-Dev Notes:
-
-Set SENTRY_USE_RELAY = false
-Set SENTRY_CONTROL_ADDRESS
-Set SENTRY_REGION_CONFIG
-
-In Terminal:
-
-control: SENTRY_SILO_MODE=CONTROL sentry devserver"
-region:  SENTRY_DEVSERVER_BIND=localhost:8002 SENTRY_SILO_MODE=REGION SENTRY_REGION=region sentry devserver"
-"""
 
 DEFAULT_API_VERSION = "0"
 
 
-class BaseSiloClient(ABC):
+class SiloClientError(Exception):
+    """Indicates an error in processing a cross-silo HTTP request"""
+
+
+class BaseSiloClient(BaseApiClient):
+    integration_type = "silo_client"
+
     @property
-    @abstractmethod
-    def access_mode():
+    def access_modes() -> List[SiloMode]:
+        """
+        Limit access to the client to only the SiloModes set here.
+        """
         raise NotImplementedError
 
-    @property
-    def prefix(self):
-        return f"/api/{self.version}"
+    def create_base_url(self, address: str) -> str:
+        api_prefix = f"/api/{self.api_version}"
+        return f"{address}{api_prefix}"
 
-    def __init__(self, version: str = DEFAULT_API_VERSION):
-        if SiloMode.get_current_mode() != self.access_mode:
-            raise Exception("No Access")
-        self.version = version
-
-    def _build_url(self, path: str) -> str:
-        return f"{self.destination_url}{self.prefix}{path}"
-
-
-class SiloResponse:
-    def __init__(self, response: Response) -> None:
-        self.headers = response.headers
-        self.status_code = response.status_code
-        self.data = json.loads(response.text)
+    def __init__(self, api_version: str = DEFAULT_API_VERSION, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.api_version = api_version
+        if SiloMode.get_current_mode() not in self.access_modes:
+            access_mode_str = ", ".join(str(m) for m in self.access_modes)
+            raise SiloClientError(
+                f"Cannot invoke {self.__class__.__name__} from {SiloMode.get_current_mode()}. "
+                f"Only available in: {access_mode_str}"
+            )
 
 
 class RegionSiloClient(BaseSiloClient):
-    access_mode = SiloMode.CONTROL
+    access_modes = [SiloMode.CONTROL]
 
-    def __init__(self, region_name, **kwargs):
-        super().__init__(**kwargs)
+    datadog_prefix = "silo_client.region"
+    log_path = "sentry.silo.client.region"
+    silo_client_name = "region"
+
+    def __init__(self, region_name, api_version: str = DEFAULT_API_VERSION, *args, **kwargs):
+        super().__init__(api_version, *args, **kwargs)
         self.region = get_region_by_name(region_name)
-        self.destination_url = self.region.address
-
-    def req(self, path):
-        url = self._build_url(path)
-        with build_session() as session:
-            response = session.get(url)
-            return SiloResponse(response=response)
+        self.base_url = self.create_base_url(self.region.address)
 
 
 class ControlSiloClient(BaseSiloClient):
-    access_mode = SiloMode.REGION
+    access_modes = [SiloMode.REGION]
 
-    def __init__(self, region_name, **kwargs):
-        super().__init__(**kwargs)
-        self.destination_url = settings.SENTRY_CONTROL_ADDRESS
+    datadog_prefix = "silo_client.control"
+    log_path = "sentry.silo.client.control"
+    silo_client_name = "control"
+
+    def __init__(self, api_version: str = DEFAULT_API_VERSION, *args, **kwargs):
+        super().__init__(api_version, *args, **kwargs)
+        self.base_url = self.create_base_url(settings.SENTRY_CONTROL_ADDRESS)
