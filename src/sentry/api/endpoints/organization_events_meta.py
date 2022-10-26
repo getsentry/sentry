e4024a7d0ccc60e99955df54aea836f2bacc2702
William Mak <william@wmak.io>
2022-10-26 18:17:16 -0400

    ref(discover): Move transform to query builder (#40396)
    - This moves the meta and data transforms to the query builder, making
    the API a bit easier to use since you no longer need to update the
    functions from discover

--- src/sentry/api/endpoints/organization_events_meta.py
+++ src/sentry/api/endpoints/organization_events_meta.py
@@ -12,8 +12,7 @@ from sentry.api.event_search import parse_search_query
 from sentry.api.helpers.group_index import build_query_params_from_request
 from sentry.api.serializers import serialize
 from sentry.api.serializers.models.group import GroupSerializer
-from sentry.search.events.fields import get_function_alias
-from sentry.snuba import discover, metrics_performance
+from sentry.snuba import discover
 
 
 @region_silo_endpoint
@@ -33,91 +32,6 @@ class OrganizationEventsMetaEndpoint(OrganizationEventsEndpointBase):
             )
 
         return Response({"count": result["data"][0]["count"]})
-
-
-@region_silo_endpoint
-class OrganizationEventsMetricsCompatiblity(OrganizationEventsEndpointBase):
-    """Metrics data can contain less than great data like null or unparameterized transactions
-
-    This endpoint will return projects that have perfect data along with the overall counts of projects so the
-    frontend can make decisions about which projects to show and related info
-
-    DEPRECATED, replaced by 2 endpoints in organization_metrics_meta
-    """
-
-    private = True
-
-    def get(self, request: Request, organization) -> Response:
-        data = {
-            "compatible_projects": [],
-            "dynamic_sampling_projects": [],
-            "sum": {
-                "metrics": None,
-                "metrics_null": None,
-                "metrics_unparam": None,
-            },
-        }
-        try:
-            # This will be used on the perf homepage and contains preset queries, allow global views
-            params = self.get_snuba_params(request, organization, check_global_views=False)
-        except NoProjects:
-            return Response(data)
-        for project in params["project_objects"]:
-            dynamic_sampling = project.get_option("sentry:dynamic_sampling")
-            if dynamic_sampling is not None:
-                data["dynamic_sampling_projects"].append(project.id)
-                if len(data["dynamic_sampling_projects"]) > 50:
-                    break
-
-        # None of the projects had DS rules, nothing is compat the sum & compat projects list is useless
-        if len(data["dynamic_sampling_projects"]) == 0:
-            return Response(data)
-        data["dynamic_sampling_projects"].sort()
-
-        # Save ourselves some work, only query the projects that have DS rules
-        data["compatible_projects"] = params["project_id"]
-        params["project_id"] = data["dynamic_sampling_projects"]
-
-        with self.handle_query_errors():
-            count_unparam = "count_unparameterized_transactions()"
-            count_has_txn = "count_has_transaction_name()"
-            count_null = "count_null_transactions()"
-            compatible_results = metrics_performance.query(
-                selected_columns=[
-                    "project.id",
-                    count_null,
-                    count_has_txn,
-                ],
-                params=params,
-                query=f"{count_null}:0 AND {count_has_txn}:>0",
-                referrer="api.organization-events-metrics-compatibility.compatible",
-                functions_acl=["count_null_transactions", "count_has_transaction_name"],
-                use_aggregate_conditions=True,
-            )
-            data["compatible_projects"] = sorted(
-                row["project.id"] for row in compatible_results["data"]
-            )
-
-            sum_metrics = metrics_performance.query(
-                selected_columns=[count_unparam, count_null, "count()"],
-                params=params,
-                query="",
-                referrer="api.organization-events-metrics-compatibility.sum_metrics",
-                functions_acl=["count_unparameterized_transactions", "count_null_transactions"],
-                use_aggregate_conditions=True,
-            )
-            if len(sum_metrics["data"]) > 0:
-                data["sum"].update(
-                    {
-                        "metrics": sum_metrics["data"][0].get("count"),
-                        "metrics_null": sum_metrics["data"][0].get(get_function_alias(count_null)),
-                        "metrics_unparam": sum_metrics["data"][0].get(
-                            get_function_alias(count_unparam)
-                        ),
-                    }
-                )
-
-        return Response(data)
 
 
 UNESCAPED_QUOTE_RE = re.compile('(?<!\\\\)"')
