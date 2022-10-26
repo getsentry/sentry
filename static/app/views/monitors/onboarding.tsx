6b4fe02542befcbb480580be434c6b1686de2ad5
David Wang <dwang@sentry.io>
2022-10-26 12:04:56 -0700

    fix json format

--- static/app/views/monitors/onboarding.tsx
+++ static/app/views/monitors/onboarding.tsx
@@ -36,25 +36,25 @@ const MonitorOnboarding = ({monitor}: Props) => {
           <StyledListItem>
             <OnboardingText>
               {t(
-                'Supply one of the following bodies to the request depending on the job status to be reported'
+                'Supply one of the following JSON bodies to the POST request depending on the job status to be reported'
               )}
             </OnboardingText>
             <OnboardingText>
               {t('For the start of a job')}
               <CodeSnippet language="json" hideActionBar>
-                {`{ status: "in_progress" }`}
+                {`{ "status": "in_progress" }`}
               </CodeSnippet>
             </OnboardingText>
             <OnboardingText>
               {t('For job completion with optional duration in milliseconds')}
               <CodeSnippet language="json" hideActionBar>
-                {`{ status: "ok", duration: 3000 }`}
+                {`{ "status": "ok", "duration": 3000 }`}
               </CodeSnippet>
             </OnboardingText>
             <OnboardingText>
               {t('For a job failure with optional duration in milliseconds')}
               <CodeSnippet language="json" hideActionBar>
-                {`{ status: "error", duration: 3000 }`}
+                {`{ "status": "error", "duration": 3000 }`}
               </CodeSnippet>
             </OnboardingText>
           </StyledListItem>
