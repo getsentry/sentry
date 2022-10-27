f5340c8adea29d390116bfaf10619cc40e0a93b3
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-10-27 12:14:39 -0400

    Add test for basic issue details

--- tests/acceptance/test_performance_issues.py
+++ tests/acceptance/test_performance_issues.py
@@ -1,7 +1,58 @@
+import datetime
+from unittest.mock import patch
+
+import pytz
+
+from fixtures.page_objects.issue_details import IssueDetailsPage
+from sentry import options
 from sentry.testutils import AcceptanceTestCase, SnubaTestCase
+from sentry.utils import json
+from sentry.utils.samples import load_data
 
 
 class PerformanceIssuesTest(AcceptanceTestCase, SnubaTestCase):
     def setUp(self):
         super().setUp()
-        pass
+        self.org = self.create_organization(owner=self.user, name="Rowdy Tiger")
+        self.team = self.create_team(
+            organization=self.org, name="Mariachi Band", members=[self.user]
+        )
+        self.project = self.create_project(organization=self.org, teams=[self.team], name="Bengal")
+        # self.group = self.create_group(project=self.project)
+        self.login_as(self.user)
+
+        options.set("performance.issues.all.problem-detection", 1.0)
+        options.set("performance.issues.all.problem-creation", 1.0)
+        options.set("performance.issues.n_plus_one_db.problem-creation", 1.0)
+
+        self.page = IssueDetailsPage(self.browser, self.client)
+        self.dismiss_assistant()
+
+    @patch("django.utils.timezone.now")
+    def test_with_one_performance_issue(self, mock_now):
+        # Create a transaction with a performance problem
+        event = load_data("transaction")
+
+        data = json.loads(
+            self.load_fixture("events/performance_problems/n-plus-one-in-django-new-view.json")
+        )
+        event.update({"spans": data["spans"]})
+
+        mock_now.return_value = datetime.datetime.fromtimestamp(event["start_timestamp"]).replace(
+            tzinfo=pytz.utc
+        )
+
+        with self.feature(
+            [
+                "projects:performance-suspect-spans-ingestion",
+                "organizations:performance-issues",
+                "organizations:performance-issues-ingest",
+            ]
+        ):
+            # Process the event
+            event = self.store_event(data=event, project_id=self.project.id)
+
+            # self.wait_for_event_count(self.project.id, 1)
+            # Navigate to the issue details page
+            self.page.visit_issue(self.org.slug, event.groups[0].id)
+            self.browser.snapshot("performance issue details", desktop_only=True)
