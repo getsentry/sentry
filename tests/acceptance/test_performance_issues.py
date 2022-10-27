16d601135345e60f5a5d428d229de97c57174438
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-10-27 10:25:04 -0400

    Add a test stub

---
+++ tests/acceptance/test_performance_issues.py
@@ -0,0 +1,7 @@
+from sentry.testutils import AcceptanceTestCase, SnubaTestCase
+
+
+class PerformanceIssuesTest(AcceptanceTestCase, SnubaTestCase):
+    def setUp(self):
+        super().setUp()
+        pass
