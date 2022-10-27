3ec604ed658532cd82866a6cedb5e24f4d1ae228
Richard Ortenberg <richard.ortenberg@sentry.io>
2022-10-27 14:58:13 -0700

    added metrics + settings

--- src/sentry/event_manager.py
+++ src/sentry/event_manager.py
@@ -2165,13 +2165,15 @@ def _save_aggregate_performance(jobs: Sequence[PerformanceJob], projects):
 
             if new_grouphashes:
                 # temporary fix to limit group creation to grouphashes seen 3+ times in a 24-48 hour period
-                groups_to_create = new_grouphashes.copy()
-                client = redis.redis_clusters.get("default")
-                for new_grouphash in new_grouphashes:
-                    if should_create_group(client, new_grouphash) is not True:
-                        groups_to_create.remove(new_grouphash)
+                if settings.SENTRY_PERFORMANCE_ISSUES_REDUCE_NOISE:
+                    groups_to_create = new_grouphashes.copy()
+                    client = redis.redis_clusters.get("default")
+                    for new_grouphash in new_grouphashes:
+                        if should_create_group(client, new_grouphash) is not True:
+                            groups_to_create.remove(new_grouphash)
 
-                new_grouphashes = groups_to_create
+                    new_grouphashes = groups_to_create
+
                 new_grouphashes_count = len(new_grouphashes)
 
                 with metrics.timer("performance.performance_issue.check_write_limits"):
@@ -2268,12 +2270,14 @@ def _save_aggregate_performance(jobs: Sequence[PerformanceJob], projects):
                 EventPerformanceProblem(event, performance_problems_by_hash[problem_hash]).save()
 
 
+@metrics.wraps("performance.performance_issue.should_create_group")
 def should_create_group(client: RedisCluster, grouphash: str) -> bool:
     times_seen = client.incr(grouphash)
     if times_seen >= 3:
         client.delete(grouphash)
         return True
     else:
+        metrics.incr("performance.performance_issue.ignored", 1)
         client.expire(grouphash, 60 * 60 * 24)
         return False
 
