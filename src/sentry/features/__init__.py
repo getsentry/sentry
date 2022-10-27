1c43c96ec55a308ce6b4ef11ecc315507157464c
Malachi Willey <malwilley@gmail.com>
2022-10-27 12:27:26 -0700

    feat(saved-searches): Add feature flag for issue-list-saved-searches-v2 (#40667)

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -97,6 +97,7 @@ default_manager.add("organizations:issue-alert-test-notifications", Organization
 default_manager.add("organizations:issue-details-owners", OrganizationFeature, True)
 default_manager.add("organizations:issue-details-tag-improvements", OrganizationFeature, True)
 default_manager.add("organizations:issue-list-removal-action", OrganizationFeature, True)
+default_manager.add("organizations:issue-list-saved-searches-v2", OrganizationFeature, True)
 default_manager.add("organizations:issue-list-trend-sort", OrganizationFeature, True)
 default_manager.add("organizations:issue-search-allow-postgres-only-search", OrganizationFeature, True)
 default_manager.add("organizations:issue-search-use-cdc-primary", OrganizationFeature, True)
