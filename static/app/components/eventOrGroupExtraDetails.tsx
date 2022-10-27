b6c048eb1daf7ba96e6f9f3179d1f596bad16f48
Dane Grant <danecando@gmail.com>
2022-10-27 11:17:33 -0400

    feat(replays): Issue List Replay Count Indicator (#40586)
    Adds a Replay Count indicator to Issue List items. The Replay Count
    indicator links to the issue replays page and includes a tooltip on
    hover letting the user know that the indicator is for replays.
    
    Hovering over the replay count indicator
    ![Screen Shot 2022-10-27 at 9 18 47
    AM](https://user-images.githubusercontent.com/3721977/198295640-680b99f0-e986-4d51-a1bc-756637eecc0b.png)
    
    Issue list with replay count indicator
    ![Screen Shot 2022-10-26 at 3 30 44
    PM](https://user-images.githubusercontent.com/3721977/198119585-475e1732-f2b7-479d-b3bc-aa094a02d551.png)
    
    Closes #39730

--- static/app/components/eventOrGroupExtraDetails.tsx
+++ static/app/components/eventOrGroupExtraDetails.tsx
@@ -8,22 +8,31 @@ import InboxReason from 'sentry/components/group/inboxBadges/inboxReason';
 import InboxShortId from 'sentry/components/group/inboxBadges/shortId';
 import TimesTag from 'sentry/components/group/inboxBadges/timesTag';
 import UnhandledTag from 'sentry/components/group/inboxBadges/unhandledTag';
+import ReplayCount from 'sentry/components/group/replayCount';
 import ProjectBadge from 'sentry/components/idBadge/projectBadge';
 import Link from 'sentry/components/links/link';
 import Placeholder from 'sentry/components/placeholder';
 import {IconChat} from 'sentry/icons';
 import {tct} from 'sentry/locale';
 import space from 'sentry/styles/space';
-import {Group} from 'sentry/types';
+import {Group, Organization} from 'sentry/types';
 import {Event} from 'sentry/types/event';
+import withOrganization from 'sentry/utils/withOrganization';
 
 type Props = WithRouterProps<{orgId: string}> & {
   data: Event | Group;
+  organization: Organization;
   showAssignee?: boolean;
   showInboxTime?: boolean;
 };
 
-function EventOrGroupExtraDetails({data, showAssignee, params, showInboxTime}: Props) {
+function EventOrGroupExtraDetails({
+  data,
+  showAssignee,
+  params,
+  showInboxTime,
+  organization,
+}: Props) {
   const {
     id,
     lastSeen,
@@ -41,6 +50,7 @@ function EventOrGroupExtraDetails({data, showAssignee, params, showInboxTime}: P
   } = data as Group;
 
   const issuesPath = `/organizations/${params.orgId}/issues/`;
+  const isReplayEnabled = organization.features.includes('session-replay-ui');
 
   return (
     <GroupExtra>
@@ -74,6 +84,7 @@ function EventOrGroupExtraDetails({data, showAssignee, params, showInboxTime}: P
           <span>{numComments}</span>
         </CommentsLink>
       )}
+      {isReplayEnabled && <ReplayCount groupId={id} orgId={params.orgId} />}
       {logger && (
         <LoggerAnnotation>
           <GlobalSelectionLink
@@ -153,4 +164,4 @@ const LoggerAnnotation = styled(AnnotationNoMargin)`
   color: ${p => p.theme.textColor};
 `;
 
-export default withRouter(EventOrGroupExtraDetails);
+export default withRouter(withOrganization(EventOrGroupExtraDetails));
