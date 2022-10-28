7cc0cfe04fc1df1a0c43e378a87c5dabde2c97e4
Armen Zambrano G <armenzg@sentry.io>
2022-10-28 10:03:35 -0400

    ref(code_mappings): Move tests for code mappings into its own module
    This decouples testing code mapping generation from Github as an integration.
    
    This makes it simpler for adding support for other platforms and understanding the tests.

---
+++ tests/sentry/integrations/utils/test_code_mapping.py
@@ -0,0 +1,65 @@
+import os
+
+import pytest
+
+from sentry.integrations.utils.code_mapping import CodeMapping, derive_code_mappings
+from sentry.testutils import TestCase
+from sentry.utils import json
+
+with open(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "fixtures/sentry_tree.json")
+) as fd:
+    sentry_tree = json.load(fd)
+
+
+class TestDerivedCodeMappings(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.trees = {"sentry": sentry_tree}
+        self.expected_code_mappings = [
+            CodeMapping("Test-Organization/foo", "sentry", "src/sentry"),
+            CodeMapping("Test-Organization/foo", "sentry_plugins", "src/sentry_plugins"),
+        ]
+
+    def test_no_matches(self):
+        stacktraces = [
+            "getsentry/billing/tax/manager.py",
+            "requests/models.py",
+            "urllib3/connectionpool.py",
+            "ssl.py",
+        ]
+        code_mappings = derive_code_mappings(stacktraces, self.trees)
+        assert code_mappings == []
+
+    def test_more_than_one_match_does_not_derive(self):
+        stacktraces = [
+            # More than one file matches for this, thus, no stack traces will be produced
+            # - "src/sentry_plugins/slack/client.py",
+            # - "src/sentry/integrations/slack/client.py",
+            "sentry_plugins/slack/client.py",
+        ]
+        code_mappings = derive_code_mappings(stacktraces, self.trees)
+        assert code_mappings == []
+
+    def test_more_than_one_match_works_when_code_mapping_excludes_other_match(self):
+        stacktraces = [
+            "sentry/identity/oauth2.py",
+            # This file matches two files in the repo, however, because we first
+            # derive the sentry code mapping we can exclude one of the files
+            "sentry_plugins/slack/client.py",
+        ]
+        code_mappings = derive_code_mappings(stacktraces, self.trees)
+        assert code_mappings == self.expected_code_mappings
+
+    @pytest.mark.xfail("The order of stacktrace frames affects the result")
+    def test_more_than_one_match_works_with_different_order(self):
+        # We do *not* derive sentry_plugins because we don't derive sentry first
+        stacktraces = [
+            # This file matches two files in the repo and because we process it
+            # before we derive
+            "sentry_plugins/slack/client.py",
+            "sentry/identity/oauth2.py",
+        ]
+        code_mappings = derive_code_mappings(stacktraces, self.trees)
+        # Order matters, this is why we only derive one of the two code mappings
+        assert code_mappings == self.expected_code_mappings
