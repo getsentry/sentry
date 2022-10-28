292b074d0945adfcce6e618690b98cf79e15f57a
Snigdha Sharma <16563948+snigdhas@users.noreply.github.com>
2022-10-28 08:28:24 -0700

    feat(code-mappings): Add task to derive missing code mappings (#40528)
    Add task to derive and save missing code mappings.
    
    Fixes WOR-2236

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -208,6 +208,7 @@ default_manager.add("organizations:relay", OrganizationFeature)
 default_manager.add("organizations:sso-basic", OrganizationFeature)
 default_manager.add("organizations:sso-saml2", OrganizationFeature)
 default_manager.add("organizations:team-insights", OrganizationFeature)
+default_manager.add("organizations:derive-code-mappings", OrganizationFeature)
 
 # Project scoped features
 default_manager.add("projects:alert-filters", ProjectFeature)
