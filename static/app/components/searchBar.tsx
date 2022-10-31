getsentry/sentry

commit 0a2ab03f374484b8177a5594071a1f28eb2f25eb

ref(searchBar): Use InputGroup (#40663)
Less code yay!

Minimal UI diff, **before:**
<img width="780" alt="Screen Shot 2022-10-27 at 11 09 29 AM"
src="https://user-images.githubusercontent.com/44172267/198366214-de82c528-d380-4700-a08b-d79618616d07.png">

**After:**
<img width="780" alt="Screen Shot 2022-10-27 at 11 09 09 AM"
src="https://user-images.githubusercontent.com/44172267/198366153-c82983a3-a7bd-4d8a-928f-5a58753a37d8.png">

static/app/components/searchBar.tsx
@@ -1,12 +1,18 @@
 import {useCallback, useRef, useState} from 'react';
-import isPropValid from '@emotion/is-prop-valid';
 import styled from '@emotion/styled';
 
 import Button from 'sentry/components/button';
-import Input, {InputProps} from 'sentry/components/input';
+import {
+  Input,
+  InputGroup,
+  InputLeadingItems,
+  InputProps,
+  InputTrailingItems,
+} from 'sentry/components/inputGroup';
 import {IconSearch} from 'sentry/icons';
 import {IconClose} from 'sentry/icons/iconClose';
 import {t} from 'sentry/locale';
+import space from 'sentry/styles/space';
 
 interface SearchBarProps extends Omit<InputProps, 'onChange'> {
   defaultQuery?: string;
@@ -56,34 +62,33 @@ function SearchBar({
 
   return (
     <FormWrap onSubmit={onSubmit} className={className}>
-      <StyledInput
-        {...inputProps}
-        ref={inputRef}
-        type="text"
-        name="query"
-        autoComplete="off"
-        value={query}
-        onChange={onQueryChange}
-        width={width}
-        size={size}
-        showClearButton={!!query}
-      />
-      <StyledIconSearch
-        color="subText"
-        size={size === 'xs' ? 'xs' : 'sm'}
-        inputSize={size}
-      />
-      {!!query && (
-        <SearchClearButton
-          type="button"
-          priority="link"
-          onClick={clearSearch}
-          size="xs"
-          icon={<IconClose size="xs" />}
-          aria-label={t('Clear')}
-          inputSize={size}
+      <InputGroup>
+        <InputLeadingItems disablePointerEvents>
+          <IconSearch color="subText" size={size === 'xs' ? 'xs' : 'sm'} />
+        </InputLeadingItems>
+        <StyledInput
+          {...inputProps}
+          ref={inputRef}
+          type="text"
+          name="query"
+          autoComplete="off"
+          value={query}
+          onChange={onQueryChange}
+          width={width}
+          size={size}
         />
-      )}
+        <InputTrailingItems>
+          {!!query && (
+            <SearchClearButton
+              size="zero"
+              borderless
+              onClick={clearSearch}
+              icon={<IconClose size="xs" />}
+              aria-label={t('Clear')}
+            />
+          )}
+        </InputTrailingItems>
+      </InputGroup>
     </FormWrap>
   );
 }
@@ -93,44 +98,13 @@ const FormWrap = styled('form')`
   position: relative;
 `;
 
-const StyledInput = styled(Input)<{showClearButton: boolean}>`
-  width: ${p => (p.width ? p.width : undefined)};
-  padding-left: ${p => `calc(
-    ${p.theme.formPadding[p.size ?? 'md'].paddingLeft}px * 1.5 +
-    ${p.theme.iconSizes.sm}
-  )`};
-
-  ${p =>
-    p.showClearButton &&
-    `
-      padding-right: calc(
-        ${p.theme.formPadding[p.size ?? 'md'].paddingRight}px * 1.5 +
-        ${p.theme.iconSizes.xs}
-      );
-    `}
+const StyledInput = styled(Input)`
+  ${p => p.width && `width: ${p.width};`}
 `;
 
-const StyledIconSearch = styled(IconSearch, {
-  shouldForwardProp: prop => typeof prop === 'string' && isPropValid(prop),
-})<{inputSize: InputProps['size']}>`
-  position: absolute;
-  top: 50%;
-  left: ${p => p.theme.formPadding[p.inputSize ?? 'md'].paddingLeft}px;
-  transform: translateY(-50%);
-  pointer-events: none;
-`;
-
-const SearchClearButton = styled(Button)<{inputSize: InputProps['size']}>`
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  right: ${p => p.theme.formPadding[p.inputSize ?? 'md'].paddingRight}px;
-  font-size: ${p => p.theme.fontSizeLarge};
+const SearchClearButton = styled(Button)`
   color: ${p => p.theme.subText};
-
-  &:hover {
-    color: ${p => p.theme.textColor};
-  }
+  padding: ${space(0.5)};
 `;
 
 export default SearchBar;

commit 3399a257daa5c3215b4c1ac965f04df219e40c05

fix(searchbar): Search bar button needs type=button (#40773)
The button is inside a form so it implicitly has a type=submit. This
means input enter, the button gets clicked which triggers the clear
search.

Seems to have regressed in #40663 cc @vuluongj20

static/app/components/searchBar.tsx
@@ -80,6 +80,7 @@ function SearchBar({
         <InputTrailingItems>
           {!!query && (
             <SearchClearButton
+              type="button"
               size="zero"
               borderless
               onClick={clearSearch}