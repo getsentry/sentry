73b17a698a9ac47214f0b13804609cbea468d083
Kev <6111995+k-fish@users.noreply.github.com>
2022-11-02 11:07:57 -0400

    ref(mep): Remove split count in summary (#40891)
    ### Summary
    We'll make the transaction summary count remain entirely indexed based
    for launch of DS GA

--- static/app/views/performance/transactionSummary/transactionOverview/content.tsx
+++ static/app/views/performance/transactionSummary/transactionOverview/content.tsx
@@ -29,7 +29,6 @@ import {
   SPAN_OP_RELATIVE_BREAKDOWN_FIELD,
 } from 'sentry/utils/discover/fields';
 import {QueryError} from 'sentry/utils/discover/genericDiscoverQuery';
-import {canUseMetricsData} from 'sentry/utils/performance/contexts/metricsEnhancedSetting';
 import {decodeScalar} from 'sentry/utils/queryString';
 import {useRoutes} from 'sentry/utils/useRoutes';
 import withProjects from 'sentry/utils/withProjects';
@@ -60,7 +59,6 @@ import {
 import TransactionSummaryCharts from './charts';
 import RelatedIssues from './relatedIssues';
 import SidebarCharts from './sidebarCharts';
-import SidebarMEPCharts from './sidebarMEPCharts';
 import StatusBreakdown from './statusBreakdown';
 import SuspectSpans from './suspectSpans';
 import {TagExplorer} from './tagExplorer';
@@ -281,8 +279,6 @@ function SummaryContent({
     handleOpenAllEventsClick: handleAllEventsViewClick,
   };
 
-  const isUsingMetrics = canUseMetricsData(organization);
-
   return (
     <Fragment>
       <Layout.Main>
@@ -375,20 +371,6 @@ function SummaryContent({
         />
       </Layout.Main>
       <Layout.Side>
-        {(isUsingMetrics ?? null) && (
-          <Fragment>
-            <SidebarMEPCharts
-              organization={organization}
-              isLoading={isLoading}
-              error={error}
-              totals={totalValues}
-              eventView={eventView}
-              transactionName={transactionName}
-              isShowingMetricsEventCount
-            />
-            <SidebarSpacer />
-          </Fragment>
-        )}
         <UserStats
           organization={organization}
           location={location}
@@ -407,25 +389,14 @@ function SummaryContent({
           />
         )}
         <SidebarSpacer />
-        {isUsingMetrics ? (
-          <SidebarMEPCharts
-            organization={organization}
-            isLoading={isLoading}
-            error={error}
-            totals={totalValues}
-            eventView={eventView}
-            transactionName={transactionName}
-          />
-        ) : (
-          <SidebarCharts
-            organization={organization}
-            isLoading={isLoading}
-            error={error}
-            totals={totalValues}
-            eventView={eventView}
-            transactionName={transactionName}
-          />
-        )}
+        <SidebarCharts
+          organization={organization}
+          isLoading={isLoading}
+          error={error}
+          totals={totalValues}
+          eventView={eventView}
+          transactionName={transactionName}
+        />
         <SidebarSpacer />
         <Tags
           generateUrl={generateTagUrl}
