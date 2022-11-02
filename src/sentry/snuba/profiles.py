16039105540118c3af4e4d0316fb130c6e78de7d
William Mak <william@wmak.io>
2022-11-02 11:05:06 -0400

    fix bad merge lol

--- src/sentry/snuba/profiles.py
+++ src/sentry/snuba/profiles.py
@@ -3,7 +3,7 @@ from typing import Any, List, Optional
 
 from sentry.search.events.builder import ProfilesQueryBuilder, ProfilesTimeseriesQueryBuilder
 from sentry.search.events.fields import InvalidSearchQuery, get_json_meta_type
-from sentry.search.events.types import ParamsType, SnubaParams, WhereType
+from sentry.search.events.types import ParamsType, SnubaParams
 from sentry.snuba.discover import transform_tips, zerofill
 from sentry.utils.snuba import Dataset, SnubaTSResult
 
@@ -92,54 +92,3 @@ def timeseries_query(
         params["end"],
         rollup,
     )
-
-
-class ProfilesQueryBuilderProtocol(Protocol):
-    @property
-    def config(self) -> ProfilesDatasetConfig:
-        ...
-
-    @property
-    def params(self) -> SnubaParams:
-        ...
-
-    def column(self, name: str) -> Column:
-        ...
-
-    def resolve_params(self) -> List[WhereType]:
-        ...
-
-
-class ProfilesQueryBuilderMixin:
-    def resolve_column_name(self: ProfilesQueryBuilderProtocol, col: str) -> str:
-        # giving resolved a type here convinces mypy that the type is str
-        resolved: str = self.config.resolve_column(col)
-        return resolved
-
-    def resolve_params(self: ProfilesQueryBuilderProtocol) -> List[WhereType]:
-        if self.params.organization is None:
-            raise InvalidSearchQuery("Organization is required to query Profiles")
-        # not sure how to make mypy happy here as `super()`
-        # refers to the other parent query builder class
-        conditions: List[WhereType] = super().resolve_params()  # type: ignore
-
-        # the profiles dataset requires a condition
-        # on the organization_id in the query
-        conditions.append(
-            Condition(self.column("organization.id"), Op.EQ, self.params.organization.id)
-        )
-
-        return conditions
-
-    def get_field_type(self: ProfilesQueryBuilderProtocol, field: str) -> Optional[str]:
-        # giving resolved a type here convinces mypy that the type is str
-        resolved: Optional[str] = self.config.resolve_column_type(field)
-        return resolved
-
-
-class ProfilesQueryBuilder(ProfilesQueryBuilderMixin, QueryBuilder):
-    pass
-
-
-class ProfilesTimeseriesQueryBuilder(ProfilesQueryBuilderMixin, TimeseriesQueryBuilder):
-    pass
