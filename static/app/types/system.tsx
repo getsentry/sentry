4adbfad572649bd21776146088e6cc1c6960db5d
Alberto Leal <mail4alberto@gmail.com>
2022-11-02 18:19:20 -0400

    feat(hybrid-cloud): Add customerDomain type to config on frontend (#40227)

--- static/app/types/system.tsx
+++ static/app/types/system.tsx
@@ -112,9 +112,15 @@ declare global {
   }
 }
 
+interface CustomerDomain {
+  organizationUrl: string | undefined;
+  sentryUrl: string;
+  subdomain: string;
+}
 export interface Config {
   apmSampling: number;
   csrfCookieName: string;
+  customerDomain: CustomerDomain | null;
   demoMode: boolean;
   disableU2FForSUForm: boolean;
   distPrefix: string;
