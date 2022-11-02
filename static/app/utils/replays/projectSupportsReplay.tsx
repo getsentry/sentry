45175a4aa537272a0cd54eb4c64593bfeb2d931f
Dane Grant <danecando@gmail.com>
2022-11-02 17:30:36 -0400

    refactor(replays): checks against array of supported platforms

--- static/app/utils/replays/projectSupportsReplay.tsx
+++ static/app/utils/replays/projectSupportsReplay.tsx
@@ -1,7 +1,8 @@
+import {replayPlatforms} from 'sentry/data/platformCategories';
 import type {MinimalProject} from 'sentry/types';
 
 function projectSupportsReplay(project?: MinimalProject) {
-  return Boolean(project?.platform?.includes('javascript'));
+  return Boolean(project?.platform && replayPlatforms.includes(project.platform));
 }
 
 export default projectSupportsReplay;
