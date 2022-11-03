107c38390bdc7f4bb1809d501da97268ea293d5c
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2022-11-03 17:45:42 -0400

    sort environment

--- src/sentry/search/events/builder.py
+++ src/sentry/search/events/builder.py
@@ -2268,8 +2268,9 @@ class MetricsQueryBuilder(QueryBuilder):
         value = search_filter.value.value
         values_set = set(value if isinstance(value, (list, tuple)) else [value])
         # sorted for consistency
+        sorted_values = sorted(f"{value}" for value in values_set)
         values = []
-        for value in values_set:
+        for value in sorted_values:
             if value:
                 values.append(self._resolve_environment_filter_value(value))
             elif self.tag_values_are_strings:
