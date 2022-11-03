5bfcd6f0c692541ec038c6a1dde1c36dcc7102a9
Riccardo Busetti <riccardo.busetti@sentry.io>
2022-11-03 16:32:29 +0100

    feat(metrics): Add caching to project transaction thresholds

--- src/sentry/models/transaction_threshold.py
+++ src/sentry/models/transaction_threshold.py
@@ -3,6 +3,8 @@ from enum import Enum
 from django.db import models
 
 from sentry.db.models import DefaultFieldsModel, FlexibleForeignKey, region_silo_only_model
+from sentry.utils.cache import cache
+from sentry.utils.hashlib import md5_text
 
 
 class TransactionMetric(Enum):
@@ -35,6 +37,32 @@ class ProjectTransactionThresholdOverride(DefaultFieldsModel):
         db_table = "sentry_projecttransactionthresholdoverride"
         unique_together = (("project", "transaction"),)
 
+    @classmethod
+    def get_cache_key(cls, project_ids, org_id, order_by, value_list):
+        return "sentry_projecttransactionthreshold:{}".format(
+            md5_text(f"{project_ids}:{org_id}:{order_by}:{value_list}").hexdigest()
+        )
+
+    @classmethod
+    def filter(cls, project_id__in, organization_id, order_by, value_list):
+        cache_key = cls.get_cache_key(project_id__in, organization_id, order_by, value_list)
+
+        cache_result = cache.get(cache_key)
+        if cache_result is None:
+            result = list(
+                cls.objects.filter(
+                    project_id__in=project_id__in,
+                    organization_id=organization_id,
+                )
+                .order_by(*order_by)
+                .values_list(*value_list)
+            )
+
+            cache.set(cache_key, result, 3600)
+            return result
+        else:
+            return cache_result
+
 
 @region_silo_only_model
 class ProjectTransactionThreshold(DefaultFieldsModel):
@@ -51,3 +79,29 @@ class ProjectTransactionThreshold(DefaultFieldsModel):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_projecttransactionthreshold"
+
+    @classmethod
+    def get_cache_key(cls, project_ids, org_id, order_by, value_list):
+        return "sentry_projecttransactionthreshold:{}".format(
+            md5_text(f"{project_ids}:{org_id}:{order_by}:{value_list}").hexdigest()
+        )
+
+    @classmethod
+    def filter(cls, project_id__in, organization_id, order_by, value_list):
+        cache_key = cls.get_cache_key(project_id__in, organization_id, order_by, value_list)
+
+        cache_result = cache.get(cache_key)
+        if cache_result is None:
+            result = list(
+                cls.objects.filter(
+                    project_id__in=project_id__in,
+                    organization_id=organization_id,
+                )
+                .order_by(*order_by)
+                .values_list(*value_list)
+            )
+
+            cache.set(cache_key, result, 3600)
+            return result
+        else:
+            return cache_result
