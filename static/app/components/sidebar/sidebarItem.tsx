b3418b139b74d8663af1c623dc943d08330240fc
Alberto Leal <mail4alberto@gmail.com>
2022-11-03 04:28:05 -0400

    chore(sidebar): Fix toPathWithoutReferrer typo (#40949)

--- static/app/components/sidebar/sidebarItem.tsx
+++ static/app/components/sidebar/sidebarItem.tsx
@@ -110,13 +110,13 @@ const SidebarItem = ({
     labelString = label?.props?.children ?? label;
   }
   // take off the query params for matching
-  const toPathWithourReferrer = to?.split('?')[0];
+  const toPathWithoutReferrer = to?.split('?')[0];
   // If there is no active panel open and if path is active according to react-router
   const isActiveRouter =
     (!hasPanel &&
       router &&
-      toPathWithourReferrer &&
-      location.pathname.startsWith(toPathWithourReferrer)) ||
+      toPathWithoutReferrer &&
+      location.pathname.startsWith(toPathWithoutReferrer)) ||
     (labelString === 'Discover' && location.pathname.includes('/discover/')) ||
     (labelString === 'Dashboards' &&
       (location.pathname.includes('/dashboards/') ||
