6e783d7c3ca15122102b902674c5eea22d588164
Vu Luong <vuluongj20@gmail.com>
2022-11-03 12:58:08 -0700

    fix(forms): Fix form field alignment (#40941)
    Fix a form field issue where if `flexibleControlStateSize` is true, then
    the field control on the right side is misaligned.
    
    **Before:** the input control in the right hand side of the third row is
    misaligned
    <img width="716" alt="Screen Shot 2022-11-02 at 5 14 10 PM"
    src="https://user-images.githubusercontent.com/44172267/199625579-7e8e96bd-5f2c-43eb-bbd6-5a9a8e704879.png">
    
    **After:**
    <img width="716" alt="Screen Shot 2022-11-02 at 5 13 40 PM"
    src="https://user-images.githubusercontent.com/44172267/199625528-036f3346-fb7e-4327-86ab-9b6315f1a6d8.png">

--- static/app/components/forms/field/fieldControlState.tsx
+++ static/app/components/forms/field/fieldControlState.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import space from 'sentry/styles/space';
+
 import {FieldGroupProps} from './types';
 
 type FieldControlStateProps = Pick<FieldGroupProps, 'flexibleControlStateSize'>;
@@ -7,10 +9,11 @@ type FieldControlStateProps = Pick<FieldGroupProps, 'flexibleControlStateSize'>;
 const FieldControlState = styled('div')<FieldControlStateProps>`
   display: flex;
   position: relative;
-  ${p => !p.flexibleControlStateSize && `width: 36px`};
+  ${p => !p.flexibleControlStateSize && `width: 24px`};
   flex-shrink: 0;
-  justify-content: center;
+  justify-content: end;
   align-items: center;
+  margin-left: ${space(0.5)};
 `;
 
 export default FieldControlState;
