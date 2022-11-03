daf8272a88002cee8915bb5722454548da0795b8
Snigdha Sharma <snigdha.sharma@sentry.io>
2022-11-03 14:38:49 -0700

    feat(code-mappings): Log derived code mappings for dry run (#40979)
    Log derived code mappings for dry run. Sample log: 
    ```
    Dry run project.slug='honest-marlin': would create these code mapping based on stacktrace_paths=['sentry/models/release.py', 'sentry/tasks.py']: [CodeMapping(repo=Repo(name='repo', branch='master'), stacktrace_root='sentry/models', source_path='src/sentry/models')]
    ```
    
    WOR-2326

--- src/sentry/tasks/derive_code_mappings.py
+++ src/sentry/tasks/derive_code_mappings.py
@@ -2,6 +2,7 @@ import logging
 from datetime import timedelta
 from typing import Any, List, Mapping, Tuple
 
+import sentry_sdk
 from sentry_sdk import set_tag, set_user
 
 from sentry import features
@@ -59,7 +60,14 @@ def derive_code_mappings(
     trees: JSONData = installation.get_trees_for_org()
     trees_helper = CodeMappingTreesHelper(trees)
     code_mappings = trees_helper.generate_code_mappings(stacktrace_paths)
-    set_project_codemappings(code_mappings, organization_integration, project, dry_run)
+    if dry_run:
+        set_tag("project.slug", project.slug)
+        sentry_sdk.capture_message(
+            f"Dry run {project.slug=}: would create these code mapping based on {stacktrace_paths=}: {code_mappings}"
+        )
+        return
+
+    set_project_codemappings(code_mappings, organization_integration, project)
 
 
 def identify_stacktrace_paths(data: NodeData) -> List[str]:
@@ -120,7 +128,6 @@ def set_project_codemappings(
     code_mappings: List[CodeMapping],
     organization_integration: OrganizationIntegration,
     project: Project,
-    dry_run: bool,
 ) -> None:
     """
     Given a list of code mappings, create a new repository project path
