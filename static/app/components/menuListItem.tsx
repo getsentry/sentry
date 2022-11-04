getsentry/sentry

commit 5fa2c0119453c03c1ef65d90581e2f41e6ed4638

fix(ui) Fix text overflow in menu items (#35918)
Text in menus shouldn't overflow out of the menu.

static/app/components/menuListItem.tsx
@@ -2,7 +2,6 @@ import {forwardRef as reactForwardRef} from 'react';
 import isPropValid from '@emotion/is-prop-valid';
 import styled from '@emotion/styled';
 
-import overflowEllipsis from 'sentry/styles/overflowEllipsis';
 import space from 'sentry/styles/space';
 import {defined} from 'sentry/utils';
 import {Theme} from 'sentry/utils/theme';
@@ -278,6 +277,8 @@ const Label = styled('p')`
   margin-bottom: 0;
   line-height: 1.4;
   white-space: nowrap;
+
+  ${p => p.theme.overflowEllipsis}
 `;
 
 const Details = styled('p')<{isDisabled: boolean; priority: Priority}>`
@@ -285,7 +286,7 @@ const Details = styled('p')<{isDisabled: boolean; priority: Priority}>`
   color: ${p => p.theme.subText};
   line-height: 1.2;
   margin-bottom: 0;
-  ${overflowEllipsis}
+  ${p => p.theme.overflowEllipsis}
 
   ${p => p.priority !== 'default' && `color: ${getTextColor(p)};`}
 `;

commit 130655c0f635ea0a5b6ad90598f728fa62760d2e

fix(menuListItem): Allow details text to wrap (#41031)
https://getsentry.atlassian.net/browse/UP-205

Fix a regression from https://github.com/getsentry/sentry/pull/35918

**Before:**
<img width="444" alt="Screen Shot 2022-11-04 at 12 38 19 PM"
src="https://user-images.githubusercontent.com/44172267/200060901-802460c2-da54-420a-9fa2-2b025cb6fd42.png">


**After:**
<img width="444" alt="Screen Shot 2022-11-04 at 12 37 26 PM"
src="https://user-images.githubusercontent.com/44172267/200060775-332a9e92-48bd-403d-b7c9-09c72ed4847b.png">

static/app/components/menuListItem.tsx
@@ -353,7 +353,6 @@ const Details = styled('p')<{disabled: boolean; priority: Priority}>`
   color: ${p => p.theme.subText};
   line-height: 1.2;
   margin-bottom: 0;
-  ${p => p.theme.overflowEllipsis}
 
   ${p => p.priority !== 'default' && `color: ${getTextColor(p)};`}
 `;