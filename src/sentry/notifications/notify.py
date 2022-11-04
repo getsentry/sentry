20b5d6145d6c2b860575aef375139db99e1ae59a
Mike Ihbe <mike.ihbe@sentry.io>
2022-11-04 13:35:20 -0700

    Add temporary implementation of APIUser->User upclassing for entrypoints into the notification subsystem. This will allow us to disqualify certain classes of errors that could occur based on these refactorings

--- src/sentry/notifications/notify.py
+++ src/sentry/notifications/notify.py
@@ -2,12 +2,13 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, Any, Callable, Iterable, Mapping, MutableMapping, Optional, Union
 
+from sentry.models import User
 from sentry.notifications.notifications.base import BaseNotification
 from sentry.services.hybrid_cloud.user import APIUser
 from sentry.types.integrations import ExternalProviders
 
 if TYPE_CHECKING:
-    from sentry.models import Team, User
+    from sentry.models import Team
 
 # Shortcut so that types don't explode.
 Notifiable = Callable[
@@ -52,4 +53,22 @@ def notify(
     extra_context_by_actor_id: Mapping[int, Mapping[str, Any]] | None = None,
 ) -> None:
     """Send notifications to these users or team."""
+
+    """ ###################### Begin Hack ######################
+    # Temporary Hybrid Cloud hack that isolates the notification subsystem from APIUser
+    # type changes. With this in place, we can assert that changes so far don't affect
+    # Notification system behavior (only possibly who receives notifications).
+    # This will be removed in future work.
+
+    # Create a new list of recipients that are full Team and User objects (as opposed to APIUser)
+    """
+    user_ids = []
+    new_recipients = []
+    for r in recipients:
+        if isinstance(r, APIUser):
+            user_ids.append(r.id)
+        else:
+            new_recipients.append(r)
+    recipients = new_recipients + list(User.objects.filter(id__in=user_ids))
+    """ ###################### End Hack ###################### """
     registry[provider](notification, recipients, shared_context, extra_context_by_actor_id)
