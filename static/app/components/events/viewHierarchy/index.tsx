1bc6a7e632f0a28e3f87c77912e285e30d0e5c01
Nar Saynorath <nar.saynorath@sentry.io>
2022-11-04 13:59:39 -0400

    Some style changes

--- static/app/components/events/viewHierarchy/index.tsx
+++ static/app/components/events/viewHierarchy/index.tsx
@@ -1,19 +1,45 @@
+import {useState} from 'react';
 import styled from '@emotion/styled';
 
+import {IconPlay} from 'sentry/icons';
+
 function ViewHierarchy({hierarchy}) {
+  const [collapsed, setCollapsed] = useState(false);
   return (
     <Container>
-      <div>{hierarchy.title}</div>
-      {!!hierarchy.children.length &&
+      <div>
+        {!!hierarchy.children.length && (
+          <StyledExpandIcon
+            fill="black"
+            collapsed={collapsed}
+            onClick={() => setCollapsed(!collapsed)}
+          />
+        )}
+        {hierarchy.title}
+      </div>
+
+      {!collapsed &&
+        !!hierarchy.children.length &&
         hierarchy.children.map(child => (
-          <ViewHierarchy key={hierarchy.title} hierarchy={child} />
+          <Container key={hierarchy.title}>
+            <ViewHierarchy hierarchy={child} />
+          </Container>
         ))}
     </Container>
   );
 }
 
 const Container = styled('div')`
-  margin-left: 12px;
+  margin-left: 24px;
+`;
+
+const StyledExpandIcon = styled(IconPlay)<{collapsed: boolean}>`
+  rotate: 90deg;
+  ${p =>
+    p.collapsed &&
+    `
+    rotate: 0deg;
+  `}
 `;
 
 export default ViewHierarchy;
