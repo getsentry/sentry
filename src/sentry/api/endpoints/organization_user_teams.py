8d1dc89519b05c28c2c8828d909315c3b3879504
Ryan Skonnord <ryan.skonnord@sentry.io>
2022-11-06 14:39:22 -0800

    ref(access): Remove models from Access fields (#40940)
    Anticipating changes for Hybrid Cloud silo boundaries, change the public
    interface of the `Access` class to not expose any ORM models as
    dataclass fields. As a first step, replace all such objects with their
    raw IDs. (Credit to @corps for the underlying idea. Future steps:
    replace models as method parameters; replace raw IDs with API object
    representations.)

--- src/sentry/api/endpoints/organization_user_teams.py
+++ src/sentry/api/endpoints/organization_user_teams.py
@@ -25,8 +25,10 @@ class OrganizationUserTeamsEndpoint(OrganizationEndpoint):
             queryset = Team.objects.filter(
                 organization=organization, status=TeamStatus.VISIBLE
             ).order_by("slug")
-            return Response(serialize(list(queryset), request.user, TeamWithProjectsSerializer()))
         else:
-            return Response(
-                serialize(list(request.access.teams), request.user, TeamWithProjectsSerializer())
-            )
+            queryset = Team.objects.filter(
+                organization=organization,
+                status=TeamStatus.VISIBLE,
+                id__in=request.access.team_ids_with_membership,
+            ).order_by("slug")
+        return Response(serialize(list(queryset), request.user, TeamWithProjectsSerializer()))
--- src/sentry/api/serializers/models/organization.py
+++ src/sentry/api/serializers/models/organization.py
@@ -488,14 +488,11 @@ class DetailedOrganizationSerializerWithProjectsAndTeams(DetailedOrganizationSer
         return super().get_attrs(item_list, user)
 
     def _project_list(self, organization: Organization, access: Access) -> list[Project]:
-        member_projects = list(access.projects)
-        member_project_ids = [p.id for p in member_projects]
-        other_projects = list(
-            Project.objects.filter(organization=organization, status=ProjectStatus.VISIBLE).exclude(
-                id__in=member_project_ids
-            )
+        project_list = list(
+            Project.objects.filter(
+                organization=organization, status=ProjectStatus.VISIBLE
+            ).order_by("slug")
         )
-        project_list = sorted(other_projects + member_projects, key=lambda x: x.slug)  # type: ignore
 
         for project in project_list:
             project.set_cached_field_value("organization", organization)
@@ -503,14 +500,11 @@ class DetailedOrganizationSerializerWithProjectsAndTeams(DetailedOrganizationSer
         return project_list
 
     def _team_list(self, organization: Organization, access: Access) -> list[Team]:
-        member_teams = list(access.teams)
-        member_team_ids = [p.id for p in member_teams]
-        other_teams = list(
-            Team.objects.filter(organization=organization, status=TeamStatus.VISIBLE).exclude(
-                id__in=member_team_ids
+        team_list = list(
+            Team.objects.filter(organization=organization, status=TeamStatus.VISIBLE).order_by(
+                "slug"
             )
         )
-        team_list = sorted(other_teams + member_teams, key=lambda x: x.slug)  # type: ignore
 
         for team in team_list:
             team.set_cached_field_value("organization", organization)
