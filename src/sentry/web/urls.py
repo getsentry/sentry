6620a4866771d29a7e5bde066cf75a303b01510f
Alberto Leal <mail4alberto@gmail.com>
2022-11-07 15:30:19 -0500

    feat(hybrid-cloud): Add /replays/* Django route

--- src/sentry/web/urls.py
+++ src/sentry/web/urls.py
@@ -470,6 +470,8 @@ urlpatterns += [
     url(r"^discover/", react_page_view, name="discover"),
     # Request to join an organization
     url(r"^join-request/", react_page_view, name="join-request"),
+    # Replays
+    url(r"^replays/", react_page_view, name="replays"),
     # User Feedback
     url(r"^user-feedback/", react_page_view, name="user-feedback"),
     # Organizations
