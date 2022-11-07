45c6cbce52d2bdeb952e21945939580e942d6456
Alberto Leal <mail4alberto@gmail.com>
2022-11-07 15:15:02 -0500

    feat(hybrid-cloud): Add /monitors/* Django route

--- src/sentry/web/urls.py
+++ src/sentry/web/urls.py
@@ -470,6 +470,8 @@ urlpatterns += [
     url(r"^discover/", react_page_view, name="discover"),
     # Request to join an organization
     url(r"^join-request/", react_page_view, name="join-request"),
+    # Monitors
+    url(r"^monitors/", react_page_view, name="monitors"),
     # Organizations
     url(r"^(?P<organization_slug>[\w_-]+)/$", react_page_view, name="sentry-organization-home"),
     url(
