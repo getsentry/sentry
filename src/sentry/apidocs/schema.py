6ab36055d7c5f35db56bbab75c295ea600c307b3
Aniket Das <aniket.das@sentry.io>
2022-11-08 18:17:55 -0800

    feat(autodoc): Retrieve operation ID and description from docstring
    This change makes it so that the operation ID and description can be set as an endpoint's docstring.
    * Operation ID is the first line of the docstring
    * Description is the entire docstring including the Operation ID
    
    This does not override anything set in extend_schema

--- src/sentry/apidocs/schema.py
+++ src/sentry/apidocs/schema.py
@@ -1,7 +1,28 @@
 from drf_spectacular.openapi import AutoSchema
+from drf_spectacular.plumbing import get_doc
 
 
 class SentrySchema(AutoSchema):
     """DRF Documentation Schema for sentry endpoints"""
 
-    pass
+    @property
+    def view_func(self):
+        return getattr(self.view, self.method.lower())
+
+    def get_operation_id(self):
+        """
+        First line of an endpoint's docstring is the operation ID
+        """
+        docstring = self.view_func.__doc__.strip().splitlines()
+        if len(docstring) > 1:
+            return docstring[0]
+        return super().get_operation_id()
+
+    def get_description(self):
+        """
+        Docstring is used as a description for the endpoint. The operation ID is included in this.
+        """
+        docstring = self.view_func.__doc__.strip().splitlines()
+        if len(docstring) > 1:
+            return get_doc(self.view_func)
+        return super().get_description()
