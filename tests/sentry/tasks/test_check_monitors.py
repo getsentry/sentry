1e4f865f025a124c6ae32c161812ca4fd503c7c3
David Wang <dwang@sentry.io>
2022-11-09 15:50:21 -0800

    change to missed and add logic to create checkin when monitor misses checkin

--- tests/sentry/tasks/test_check_monitors.py
+++ tests/sentry/tasks/test_check_monitors.py
@@ -24,6 +24,9 @@ class CheckMonitorsTest(TestCase):
         check_monitors()
 
         assert Monitor.objects.filter(id=monitor.id, status=MonitorStatus.ERROR).exists()
+        assert MonitorCheckIn.objects.filter(
+            monitor=monitor.id, status=CheckInStatus.MISSED
+        ).exists()
 
     def test_missing_checkin_but_disabled(self):
         org = self.create_organization()
