847b01ffe0989a13669ea28e666e04cac517da20
Zach Collins <recursive.cookie.jar@gmail.com>
2022-11-10 08:30:33 -0700

    fix(hybrid-cloud): releases record activities correctly. (#41211)

--- src/sentry/models/grouphistory.py
+++ src/sentry/models/grouphistory.py
@@ -17,6 +17,7 @@ from sentry.types.activity import ActivityType
 
 if TYPE_CHECKING:
     from sentry.models import Group, Release, Team, User
+    from sentry.services.hybrid_cloud.user import APIUser
 
 
 class GroupHistoryStatus:
@@ -210,7 +211,7 @@ def record_group_history_from_activity_type(
 def record_group_history(
     group: "Group",
     status: int,
-    actor: Optional[Union["User", "Team"]] = None,
+    actor: Optional[Union["APIUser", "Team"]] = None,
     release: Optional["Release"] = None,
 ):
     prev_history = get_prev_history(group, status)
