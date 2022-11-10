fb3ce703bdd528c6c9a37c19eee739f2c3494ca8
Scott Cooper <scttcper@gmail.com>
2022-11-10 11:18:56 -0800

    feat(ecosystem): New sourcecode mapping modal

--- static/app/components/events/interfaces/frame/stacktraceLinkModal.tsx
+++ static/app/components/events/interfaces/frame/stacktraceLinkModal.tsx
@@ -1,18 +1,20 @@
 import {Fragment, useState} from 'react';
 import styled from '@emotion/styled';
 
-import {addErrorMessage, addSuccessMessage} from 'sentry/actionCreators/indicator';
+import {addSuccessMessage} from 'sentry/actionCreators/indicator';
 import {ModalRenderProps} from 'sentry/actionCreators/modal';
 import Alert from 'sentry/components/alert';
 import Button from 'sentry/components/button';
+import ButtonBar from 'sentry/components/buttonBar';
 import TextField from 'sentry/components/forms/fields/textField';
+import ExternalLink from 'sentry/components/links/externalLink';
+import Link from 'sentry/components/links/link';
+import List from 'sentry/components/list';
+import TextCopyInput from 'sentry/components/textCopyInput';
 import {t, tct} from 'sentry/locale';
 import space from 'sentry/styles/space';
 import type {Integration, Organization, Project} from 'sentry/types';
-import {
-  getIntegrationIcon,
-  trackIntegrationAnalytics,
-} from 'sentry/utils/integrationUtil';
+import {trackIntegrationAnalytics} from 'sentry/utils/integrationUtil';
 import useApi from 'sentry/utils/useApi';
 
 interface StacktraceLinkModalProps extends ModalRenderProps {
@@ -32,22 +34,36 @@ function StacktraceLinkModal({
   project,
   Header,
   Body,
+  Footer,
 }: StacktraceLinkModalProps) {
   const api = useApi();
+  const [error, setError] = useState<null | string>(null);
   const [sourceCodeInput, setSourceCodeInput] = useState('');
 
   const onHandleChange = (input: string) => {
     setSourceCodeInput(input);
   };
 
-  function onManualSetup(provider: string) {
+  const sourceCodeProviders = integrations.filter(integration =>
+    ['github', 'gitlab'].includes(integration.provider?.key)
+  );
+  // If they have more than one, they'll have to navigate themselves
+  const sourceUrl =
+    sourceCodeProviders.length === 1
+      ? `https://${sourceCodeProviders[0].domainName}`
+      : undefined;
+
+  const onManualSetup = () => {
     trackIntegrationAnalytics('integrations.stacktrace_manual_option_clicked', {
       view: 'stacktrace_issue_details',
       setup_type: 'manual',
-      provider,
+      provider:
+        sourceCodeProviders.length === 1
+          ? sourceCodeProviders[0].provider.name
+          : 'unknown',
       organization,
     });
-  }
+  };
 
   const handleSubmit = async () => {
     trackIntegrationAnalytics('integrations.stacktrace_submit_config', {
@@ -55,7 +71,6 @@ function StacktraceLinkModal({
       view: 'stacktrace_issue_details',
       organization,
     });
-
     const parsingEndpoint = `/projects/${organization.slug}/${project.slug}/repo-path-parsing/`;
     try {
       const configData = await api.requestPromise(parsingEndpoint, {
@@ -86,107 +101,140 @@ function StacktraceLinkModal({
       closeModal();
       onSubmit();
     } catch (err) {
-      const errors = err?.responseJSON
-        ? Array.isArray(err?.responseJSON)
-          ? err?.responseJSON
-          : Object.values(err?.responseJSON)
-        : [];
-      const apiErrors = errors.length > 0 ? `: ${errors.join(', ')}` : '';
-      addErrorMessage(t('Something went wrong%s', apiErrors));
+      setError(err?.responseJSON?.sourceUrl?.[0] ?? t('Unable to save configuration'));
     }
   };
 
   return (
     <Fragment>
       <Header closeButton>
-        <h4>{t('Link Stack Trace To Source Code')}</h4>
+        <h4>{t('Tell us where your source code is')}</h4>
       </Header>
       <Body>
         <ModalContainer>
-          <div>
-            <h6>{t('Automatic Setup')}</h6>
-            {tct(
-              'Enter the source code URL corresponding to stack trace filename [filename] so we can automatically set up stack trace linking for this project.',
-              {
-                filename: <StyledCode>{filename}</StyledCode>,
-              }
-            )}
-          </div>
-          <SourceCodeInput>
-            <StyledTextField
-              inline={false}
-              flexibleControlStateSize
-              stacked
-              name="source-code-input"
-              value={sourceCodeInput}
-              onChange={onHandleChange}
-              placeholder={`https://github.com/helloworld/Hello-World/blob/master/${filename}`}
-            />
-            <Button type="button" onClick={handleSubmit}>
-              {t('Submit')}
-            </Button>
-          </SourceCodeInput>
-          <div>
-            <h6>{t('Manual Setup')}</h6>
-            <Alert type="warning">
-              {t(
-                'We recommend this for more complicated configurations, like projects with multiple repositories.'
-              )}
-            </Alert>
-            {t(
-              "To manually configure stack trace linking, select the integration you'd like to use for mapping:"
-            )}
-          </div>
-          <ManualSetup>
-            {integrations.map(integration => (
-              <Button
-                key={integration.id}
-                type="button"
-                onClick={() => onManualSetup(integration.provider.key)}
-                to={{
-                  pathname: `/settings/${organization.slug}/integrations/${integration.provider.key}/${integration.id}/`,
-                  query: {tab: 'codeMappings', referrer: 'stacktrace-issue-details'},
-                }}
-              >
-                {getIntegrationIcon(integration.provider.key)}
-                <IntegrationName>{integration.name}</IntegrationName>
-              </Button>
-            ))}
-          </ManualSetup>
+          {error && (
+            <StyledAlert type="error" showIcon>
+              {error === 'Could not find repo'
+                ? tct(
+                    'We don’t have access to that GitHub repo. To fix this, [link:add your repo.]',
+                    {
+                      // TODO Add link to config page
+                      link: (
+                        <Link
+                          onClick={onManualSetup}
+                          to={
+                            sourceCodeProviders.length === 1
+                              ? `/settings/${organization.slug}/integrations/${sourceCodeProviders[0].provider.name}/${sourceCodeProviders[0].id}/`
+                              : `/settings/${organization.slug}/integrations/`
+                          }
+                        />
+                      ),
+                    }
+                  )
+                : error.includes('blank')
+                ? t('URL is required.')
+                : error}
+            </StyledAlert>
+          )}
+          {tct(
+            'We can’t find the file path for [filename] in your GitHub repo. Add the correct link below to enable git blame and suspect commits for this project.',
+            {
+              filename: <StyledCode>{filename}</StyledCode>,
+            }
+          )}
+          <StyledList symbol="colored-numeric">
+            <li>
+              <ItemContainer>
+                {sourceCodeProviders.length === 1
+                  ? tct('Go to [link]', {
+                      link: (
+                        <ExternalLink href={sourceUrl}>
+                          {sourceCodeProviders[0].provider.name}
+                        </ExternalLink>
+                      ),
+                    })
+                  : t('Go to your source code provider')}
+              </ItemContainer>
+            </li>
+            <li>
+              <ItemContainer>
+                <div>{t('Find the correct repo and path for the file')}</div>
+                <TextCopyInput>{filename}</TextCopyInput>
+              </ItemContainer>
+            </li>
+            <li>
+              <ItemContainer>
+                <StyledTextField
+                  inline={false}
+                  label={t('Copy the URL and paste it below')}
+                  name="source-code-input"
+                  value={sourceCodeInput}
+                  onChange={onHandleChange}
+                  placeholder={`https://github.com/helloworld/Hello-World/blob/master${
+                    filename.startsWith('/') ? '' : '/'
+                  }${filename}`}
+                />
+              </ItemContainer>
+            </li>
+          </StyledList>
         </ModalContainer>
       </Body>
+      <Footer>
+        <ButtonBar gap={1}>
+          <Button type="button" onClick={closeModal}>
+            {t('Cancel')}
+          </Button>
+          <Button type="button" priority="primary" onClick={handleSubmit}>
+            {t('Save')}
+          </Button>
+        </ButtonBar>
+      </Footer>
     </Fragment>
   );
 }
 
+const StyledList = styled(List)`
+  gap: ${space(2)};
+
+  & > li {
+    display: flex;
+    padding-left: 0;
+    gap: ${space(1)};
+  }
+
+  & > li:before {
+    position: relative;
+  }
+`;
+
+const ItemContainer = styled('div')`
+  display: flex;
+  gap: ${space(1)};
+  flex-direction: column;
+  margin-top: ${space(0.25)};
+  flex: 1;
+`;
+
 const ModalContainer = styled('div')`
   display: grid;
-  gap: ${space(3)};
+  gap: ${space(2)};
+`;
+
+const StyledAlert = styled(Alert)`
+  margin-bottom: 0;
 `;
 
 const StyledCode = styled('code')`
   word-break: break-word;
 `;
 
-const SourceCodeInput = styled('div')`
-  display: flex;
-  gap: ${space(1)};
-`;
-
-const ManualSetup = styled('div')`
-  display: flex;
-  gap: ${space(1)};
-  flex-direction: column;
-  align-items: center;
-`;
-
 const StyledTextField = styled(TextField)`
   padding: 0px;
   flex-grow: 1;
-`;
 
-const IntegrationName = styled('p')`
-  padding-left: 10px;
+  div {
+    margin-left: 0px;
+  }
 `;
 
 export default StacktraceLinkModal;
