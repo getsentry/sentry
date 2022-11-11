2a828e85f720ec5da93a727d6ade27937586178c
David Wang <dwang@sentry.io>
2022-11-11 01:46:47 -0800

    feat(monitors): Add icon tooltip

--- static/app/views/monitors/monitorIcon.tsx
+++ static/app/views/monitors/monitorIcon.tsx
@@ -1,8 +1,36 @@
 import styled from '@emotion/styled';
 
+import Tooltip from 'sentry/components/tooltip';
+import {t, tct} from 'sentry/locale';
+
 import {Status} from './types';
 
-export default styled('div')<{size: number; status: Status}>`
+interface Props {
+  size: number;
+  status: Status;
+}
+
+const statusToMessage = {
+  ok: t('Ok'),
+  error: t('Failed'),
+  disabled: t('Disabled'),
+  active: t('Active'),
+  missed_checkin: t('Missed Checkin'),
+};
+
+const MonitorIcon = ({size, status}: Props) => (
+  <Tooltip
+    title={tct('Monitor Status: [statusMessage]', {
+      statusMessage: statusToMessage[status],
+    })}
+  >
+    <MonitorBubble size={size} status={status} />
+  </Tooltip>
+);
+
+export default MonitorIcon;
+
+const MonitorBubble = styled('div')<{size: number; status: Status}>`
   display: inline-block;
   position: relative;
   border-radius: 50%;
