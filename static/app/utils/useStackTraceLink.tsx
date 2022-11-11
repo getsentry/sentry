d3deaecd0069d373b891c31c4805127ef4d7c138
JonasBa <jonas@badalic.com>
2022-11-11 11:04:36 -0500

    feat(stacktracelink): add useStacktraceLink hook

---
+++ static/app/utils/useStackTraceLink.tsx
@@ -0,0 +1,36 @@
+import type {Organization, Project, StacktraceLinkResult} from 'sentry/types';
+import {useQuery} from 'sentry/utils/queryClient';
+
+interface UseStackTraceLinkProps {
+  frame: {
+    absPath?: string;
+    filename?: string;
+    module?: string;
+    package?: string;
+  };
+  organization: Organization;
+  project: Project;
+  commitId?: string;
+  organizationSlug?: string;
+  platform?: string;
+  sdkName?: string;
+}
+
+export function useStackTraceLink(options: UseStackTraceLinkProps) {
+  const response = useQuery<StacktraceLinkResult>([
+    `/projects/${options.organization.slug}/${options.project.slug}/stacktrace-link/`,
+    {
+      query: {
+        file: options.frame.filename,
+        platform: options.platform,
+        commitId: options.commitId,
+        ...(options.sdkName && {sdkName: options.sdkName}),
+        ...(options.frame.absPath && {absPath: options.frame.absPath}),
+        ...(options.frame.module && {module: options.frame.module}),
+        ...(options.frame.package && {package: options.frame.package}),
+      },
+    },
+  ]);
+
+  return response;
+}
