996879fa7a4269ee6e93f7c8253b9ca1af945794
Albert Tugushev <albert@tugushev.ru>
2022-11-14 21:54:06 +0100

    Customize pip-compile header

--- tools/freeze_requirements.py
+++ tools/freeze_requirements.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import argparse
+import os
 from concurrent.futures import Future, ThreadPoolExecutor
 from os.path import abspath
 from subprocess import CalledProcessError, run
@@ -10,25 +11,13 @@ from tools.lib import gitroot
 
 
 def worker(args: tuple[str, ...]) -> None:
-    # pip-compile doesn't let you customize the header, so we write
-    # one ourselves. However, pip-compile needs -o DEST otherwise
-    # it will bump >= pins even if they're satisfied. So, we need to
-    # unfortunately rewrite the whole file.
-    dest = args[-1]
+    env = os.environ.copy()
+    env["CUSTOM_COMPILE_COMMAND"] = "make freeze-requirements"
+
     try:
-        run(args, check=True, capture_output=True)
+        run(args, check=True, capture_output=True, env=env)
     except CalledProcessError as e:
         raise e
-
-    with open(dest, "rb+") as f:
-        content = f.read()
-        f.seek(0, 0)
-        f.write(
-            b"""# DO NOT MODIFY. This file was generated with `make freeze-requirements`.
-
-"""
-            + content
-        )
 
 
 def check_futures(futures: list[Future[None]]) -> int:
@@ -61,7 +50,6 @@ def main(argv: Sequence[str] | None = None) -> int:
         "pip-compile",
         "--allow-unsafe",
         "--no-annotate",
-        "--no-header",
         "--quiet",
         "--strip-extras",
         "--index-url=https://pypi.devinfra.sentry.io/simple",
