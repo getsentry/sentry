0b3f39454f0254bf9c1052c85bf9c2380f045623
k-fish <kevan.fisher@sentry.io>
2022-11-15 07:40:09 -0500

    feat(perf-issues): Add detector switch to performance settings
    This adds the UI for a switch to turn on and off issue creations on a per-detector basis. This will give us better signal if specific detectors are too noisy for customers.

--- static/app/views/settings/projectPerformance/projectPerformance.tsx
+++ static/app/views/settings/projectPerformance/projectPerformance.tsx
@@ -60,7 +60,7 @@ class ProjectPerformance extends AsyncView<Props, State> {
       ['project', `/projects/${orgId}/${projectId}/`],
     ];
 
-    if (organization.features.includes('performance-issues-dev')) {
+    if (organization.features.includes('performance-issues')) {
       const performanceIssuesEndpoint = [
         'performance_issue_settings',
         `/projects/${orgId}/${projectId}/performance-issues/configure/`,
@@ -161,41 +161,16 @@ class ProjectPerformance extends AsyncView<Props, State> {
     ];
   }
 
-  get performanceIssueDetectorsFormFields(): Field[] {
+  get performanceIssueDetectorFormFields(): Field[] {
     return [
       {
-        name: 'n_plus_one_db_detection_rate',
-        type: 'range',
-        label: t('N+1 (DB) Detection Rate'),
-        min: 0.0,
-        max: 1.0,
-        step: 0.01,
-        defaultValue: 0,
-      },
-      {
-        name: 'n_plus_one_db_issue_rate',
-        type: 'range',
-        label: t('N+1 (DB) Issue Rate'),
-        min: 0.0,
-        max: 1.0,
-        step: 0.01,
-        defaultValue: 0,
-      },
-      {
-        name: 'n_plus_one_db_count',
-        type: 'number',
-        label: t('N+1 (DB) Minimum Count'),
-        min: 0,
-        max: 1000,
-        defaultValue: 5,
-      },
-      {
-        name: 'n_plus_one_db_duration_threshold',
-        type: 'number',
-        label: t('N+1 (DB) Duration Threshold'),
-        min: 0,
-        max: 1000000.0,
-        defaultValue: 500,
+        name: 'performance_issue_creation_enabled_n_plus_one_db',
+        type: 'boolean',
+        label: t('Allow N+1 (db) performance issues'),
+        defaultValue: true,
+        help: t(
+          'This determines whether new issues get created when n+1 performance issues are detected for database calls.'
+        ),
       },
     ];
   }
@@ -215,7 +190,7 @@ class ProjectPerformance extends AsyncView<Props, State> {
     const requiredScopes: Scope[] = ['project:write'];
 
     const projectEndpoint = this.getProjectEndpoint(params);
-    const performanceIssuesEndpoint = this.getPerformanceIssuesEndpoint(params);
+    const issuesEndpoint = this.getPerformanceIssuesEndpoint(params);
 
     return (
       <Fragment>
@@ -259,46 +234,44 @@ class ProjectPerformance extends AsyncView<Props, State> {
             )}
           </Access>
         </Form>
+        <Form
+          saveOnBlur
+          allowUndo
+          apiMethod="PUT"
+          apiEndpoint={issuesEndpoint}
+          initialData={{performance_issue_creation_enabled_n_plus_one_db: true}}
+        >
+          <Access access={requiredScopes}>
+            {({hasAccess}) => (
+              <JsonForm
+                title={t('Performance Issues')}
+                fields={this.performanceIssueDetectorFormFields}
+                disabled={!hasAccess}
+              />
+            )}
+          </Access>
+        </Form>
         <Feature features={['organizations:performance-issues-dev']}>
-          <Fragment>
-            <Form
-              saveOnBlur
-              allowUndo
-              initialData={{
-                performanceIssueCreationRate:
-                  this.state.project.performanceIssueCreationRate,
-              }}
-              apiMethod="PUT"
-              apiEndpoint={projectEndpoint}
-            >
-              <Access access={requiredScopes}>
-                {({hasAccess}) => (
-                  <JsonForm
-                    title={t('Performance Issues - All')}
-                    fields={this.performanceIssueFormFields}
-                    disabled={!hasAccess}
-                  />
-                )}
-              </Access>
-            </Form>
-            <Form
-              saveOnBlur
-              allowUndo
-              initialData={this.state.performance_issue_settings}
-              apiMethod="PUT"
-              apiEndpoint={performanceIssuesEndpoint}
-            >
-              <Access access={requiredScopes}>
-                {({hasAccess}) => (
-                  <JsonForm
-                    title={t('Performance Issues - Detector Settings')}
-                    fields={this.performanceIssueDetectorsFormFields}
-                    disabled={!hasAccess}
-                  />
-                )}
-              </Access>
-            </Form>
-          </Fragment>
+          <Form
+            saveOnBlur
+            allowUndo
+            initialData={{
+              performanceIssueCreationRate:
+                this.state.project.performanceIssueCreationRate,
+            }}
+            apiMethod="PUT"
+            apiEndpoint={projectEndpoint}
+          >
+            <Access access={requiredScopes}>
+              {({hasAccess}) => (
+                <JsonForm
+                  title={t('Performance Issues (Dev)')}
+                  fields={this.performanceIssueFormFields}
+                  disabled={!hasAccess}
+                />
+              )}
+            </Access>
+          </Form>
         </Feature>
       </Fragment>
     );
