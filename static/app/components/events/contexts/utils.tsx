90f6ba02f874cfc4d7ac88b02c032ed8ad9a656a
Priscila Oliveira <priscila.oliveira@sentry.io>
2022-11-15 10:56:57 +0100

    cr feedback

--- static/app/components/events/contexts/utils.tsx
+++ static/app/components/events/contexts/utils.tsx
@@ -1,5 +1,6 @@
 import {Fragment} from 'react';
 import styled from '@emotion/styled';
+import startCase from 'lodash/startCase';
 import moment from 'moment-timezone';
 
 import ContextData from 'sentry/components/contextData';
@@ -182,7 +183,7 @@ export function getUnknownData({
     .map(([key, value]) => ({
       key,
       value,
-      subject: key,
+      subject: startCase(key),
       meta: meta?.[key]?.[''],
     }));
 }
