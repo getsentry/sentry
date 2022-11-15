ae31b8fd516bf2749e3ad37a509e6f8c05d2e097
Alberto Leal <mail4alberto@gmail.com>
2022-11-15 14:38:42 -0500

    feat(hybrid-cloud): Add /data-export/* sub-page Django routes (#41370)

--- src/sentry/web/urls.py
+++ src/sentry/web/urls.py
@@ -544,6 +544,8 @@ urlpatterns += [
     url(r"^releases/", react_page_view, name="releases"),
     # User Feedback
     url(r"^user-feedback/", react_page_view, name="user-feedback"),
+    # Data Export
+    url(r"^data-export/", react_page_view, name="data-export"),
     # Disabled Member
     url(
         r"^disabled-member/",
