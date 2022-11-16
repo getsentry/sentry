dfbfb25f721f74957af6191a3b263e5bdab9a9f8
Oleksandr Kylymnychenko <oleksandr@sentry.io>
2022-11-16 16:31:34 +0100

    ref: review comments

--- tests/tools/test_devservices_healthcheck.py
+++ tests/tools/test_devservices_healthcheck.py
@@ -1,11 +1,13 @@
+import pytest
+
 from tools.devservices_healthcheck import run_cmd
 
 
 def test_cmd_run_fail():
-    result = run_cmd(["i_dont_exist"], retries=1)
-    assert result != 0
+    with pytest.raises(SystemExit) as se:
+        run_cmd(["i_dont_exist"], retries=1)
+    assert se.value.code != 0
 
 
 def test_cmd_run():
-    result = run_cmd(["date"])
-    assert result == 0
+    assert 0 == run_cmd(["date"])
--- tools/devservices_healthcheck.py
+++ tools/devservices_healthcheck.py
@@ -1,40 +1,42 @@
-import logging
+from __future__ import annotations
+
 import os
 import subprocess
 from time import sleep
-from typing import List
 
 
 def run_cmd(
-    args: List[str],
+    args: list[str],
     *,
     retries: int = 3,
     timeout: int = 5,
 ) -> int:
-    cmd = " ".join(args)
     for retry in range(1, retries + 1):
-        logging.info(f"Running {retry} health check {cmd!r}...")
         try:
             returncode = subprocess.call(args)
-        except Exception:
-            continue
+        except OSError:
+            raise SystemExit(1)
 
         if returncode != 0:
             sleep(timeout)
         else:
-            logging.info(f"Check: {cmd!r}\t[OK]")
             return 0
 
-    logging.info(f"Check {cmd!r}\t[FAIL]")
-    return 1
+    raise SystemExit(1)
 
 
 def main() -> None:
     # Available health checks
-    postgres_healthcheck = "docker exec sentry_postgres pg_isready -U postgres".split(" ")
-    kafka_healthcheck = (
-        "docker exec sentry_kafka kafka-topics --zookeeper 127.0.0.1:2181 --list".split(" ")
-    )
+    postgres_healthcheck = ["docker", "exec", "sentry_postgres", "pg_isready", "-U", "postgres"]
+    kafka_healthcheck = [
+        "docker",
+        "exec",
+        "sentry_kafka",
+        "kafka-topics",
+        "--zookeeper",
+        "127.0.0.1:2181",
+        "--list",
+    ]
 
     healthchecks = list()
     healthchecks.append(postgres_healthcheck)
@@ -42,11 +44,8 @@ def main() -> None:
         healthchecks.append(kafka_healthcheck)
 
     for check in healthchecks:
-        result = run_cmd(check)
-        if result != 0:
-            raise SystemExit(result)
+        run_cmd(check)
 
 
 if __name__ == "__main__":
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s: %(message)s")
-    main()
+    raise SystemExit(main())
