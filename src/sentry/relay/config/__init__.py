46aeaae2aa123bf5f09dd8a6ad03b75d7c0b1118
Jan Michael Auer <mail@jauer.org>
2022-11-17 15:47:43 +0100

    feat(relay): Enable total sampling for beta organizations

--- src/sentry/relay/config/__init__.py
+++ src/sentry/relay/config/__init__.py
@@ -178,7 +178,11 @@ def get_dynamic_sampling_config(project: Project) -> Optional[Mapping[str, Any]]
                         )
                     active_rules.append(rule)
 
-            return {"rules": active_rules}
+            config = {"rules": active_rules}
+
+            if features.has("organizations:dynamic-sampling-deprecated", project.organization):
+                config["mode"] = "total"
+            return config
 
     return None
 
