9f457553d68b4421ec6aecf3502fce27fc76f2da
Kev <6111995+k-fish@users.noreply.github.com>
2022-11-17 10:11:36 -0500

    tests(js): Convert ColumnEditModal to RTL (#41490)
    This converts columnEditModal to RTL, patching a few helper functions
    from select-new test helper which might be removable at this point, but
    we can do that in another PR. This has a few rough edges since there is
    a lot f test-id usage to select labels, and hasn't been converted to TS,
    but we can sort those out later.
    
    Also, should be the last one 🎉 .

--- static/app/views/eventsV2/table/queryField.tsx
+++ static/app/views/eventsV2/table/queryField.tsx
@@ -557,7 +557,7 @@ class QueryField extends Component<Props> {
     const requiredInputs = (gridColumns ?? inputs.length + 1) - inputs.length - 1;
     if (gridColumns !== undefined && requiredInputs > 0) {
       for (let i = 0; i < requiredInputs; i++) {
-        inputs.push(<BlankSpace key={i} />);
+        inputs.push(<BlankSpace key={i} data-test-id="blankSpace" />);
       }
     }
 
@@ -653,6 +653,7 @@ class QueryField extends Component<Props> {
           gridColumns={1}
           tripleLayout={false}
           error={error !== undefined}
+          data-test-id="queryField"
         >
           <ArithmeticInput
             name="arithmetic"
@@ -666,7 +667,7 @@ class QueryField extends Component<Props> {
           />
           {error ? (
             <ArithmeticError title={error}>
-              <IconWarning color="errorText" />
+              <IconWarning color="errorText" data-test-id="arithmeticErrorWarning" />
             </ArithmeticError>
           ) : null}
         </Container>
@@ -705,6 +706,7 @@ class QueryField extends Component<Props> {
         className={className}
         gridColumns={gridColumnsQuantity ?? containerColumns}
         tripleLayout={gridColumns === 3 && parameters.length > 2}
+        data-test-id="queryField"
       >
         {!hidePrimarySelector && (
           <SelectControl
