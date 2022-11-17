c59df392d9c707d8cad12796723a23c5be0846b8
Alberto Leal <mail4alberto@gmail.com>
2022-11-17 18:51:44 -0500

    feat(hybrid-cloud): Redirect to sentryUrl on invalid org slugs

--- static/app/views/app/index.spec.jsx
+++ static/app/views/app/index.spec.jsx
@@ -26,6 +26,16 @@ describe('App', function () {
       url: '/internal/options/?query=is:required',
       body: TestStubs.InstallWizard(),
     });
+    Object.defineProperty(window, 'location', {
+      writable: true,
+      value: {
+        replace: jest.fn(),
+      },
+    });
+  });
+
+  afterEach(function () {
+    jest.resetAllMocks();
   });
 
   it('renders', function () {
@@ -36,6 +46,7 @@ describe('App', function () {
     );
 
     expect(screen.getByText('placeholder content')).toBeInTheDocument();
+    expect(window.location.replace).not.toHaveBeenCalled();
   });
 
   it('renders NewsletterConsent', async function () {
@@ -73,5 +84,19 @@ describe('App', function () {
       'Complete setup by filling out the required configuration.'
     );
     expect(completeSetup).toBeInTheDocument();
+  });
+
+  it('redirects to sentryUrl on invalid org slug', function () {
+    const {sentryUrl} = ConfigStore.get('links');
+    render(
+      <App params={{orgId: 'albertos%2fapples'}}>
+        <div>placeholder content</div>
+      </App>
+    );
+
+    expect(screen.queryByText('placeholder content')).not.toBeInTheDocument();
+    expect(sentryUrl).toEqual('https://sentry.io');
+    expect(window.location.replace).toHaveBeenCalledWith('https://sentry.io');
+    expect(window.location.replace).toHaveBeenCalledTimes(1);
   });
 });
