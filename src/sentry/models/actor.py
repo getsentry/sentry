2f60c0ea8e044bd5d9682b50c38f5ce1ab38d554
Zachary Collins <zachary.collins@sentry.io>
2022-11-18 12:11:05 -0700

    More import simpification.

--- src/sentry/models/actor.py
+++ src/sentry/models/actor.py
@@ -6,10 +6,10 @@ from django.db.models.signals import pre_save
 from rest_framework import serializers
 
 from sentry.db.models import Model, region_silo_only_model
+from sentry.services.hybrid_cloud.user_service import APIUser, user_service
 
 if TYPE_CHECKING:
     from sentry.models import Team
-    from sentry.services.hybrid_cloud.user_service import APIUser, user_service
 
 ACTOR_TYPES = {"team": 0, "user": 1}
 
