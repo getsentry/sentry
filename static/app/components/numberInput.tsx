7c9e4435d40d65cae0986f26d0bd01e07a9dbc31
Vu Luong <vuluongj20@gmail.com>
2022-11-18 14:04:41 -0800

    ref(numberInput): Add custom incr/decr buttons

--- static/app/components/numberInput.tsx
+++ static/app/components/numberInput.tsx
@@ -1,10 +1,18 @@
 import {forwardRef, ForwardRefRenderFunction, useRef} from 'react';
+import styled from '@emotion/styled';
+import {useButton} from '@react-aria/button';
 import {useLocale} from '@react-aria/i18n';
-import {AriaNumberFieldProps, useNumberField} from '@react-aria/numberfield';
+import {useNumberField} from '@react-aria/numberfield';
 import {useNumberFieldState} from '@react-stately/numberfield';
+import {AriaNumberFieldProps} from '@react-types/numberfield';
 
-import Input, {InputStylesProps} from 'sentry/components/input';
+import Button from 'sentry/components/button';
+import {InputStylesProps} from 'sentry/components/input';
+import {Input, InputGroup, InputTrailingItems} from 'sentry/components/inputGroup';
+import {IconChevron} from 'sentry/icons/iconChevron';
+import space from 'sentry/styles/space';
 import mergeRefs from 'sentry/utils/mergeRefs';
+import {FormSize} from 'sentry/utils/theme';
 
 export interface NumberInputProps
   extends InputStylesProps,
@@ -31,21 +39,72 @@ const BaseNumberInput: ForwardRefRenderFunction<HTMLInputElement, NumberInputPro
   };
   const {locale} = useLocale();
   const state = useNumberFieldState({locale, ...ariaProps});
-  const {inputProps} = useNumberField(ariaProps, state, ref);
+  const {groupProps, inputProps, incrementButtonProps, decrementButtonProps} =
+    useNumberField(ariaProps, state, ref);
+
+  const incrementButtonRef = useRef<HTMLButtonElement>(null);
+  const {buttonProps: incrementProps} = useButton(
+    incrementButtonProps,
+    incrementButtonRef
+  );
+
+  const decrementButtonRef = useRef<HTMLButtonElement>(null);
+  const {buttonProps: decrementProps} = useButton(
+    decrementButtonProps,
+    decrementButtonRef
+  );
 
   return (
-    <Input
-      {...inputProps}
-      ref={mergeRefs([ref, forwardedRef])}
-      type="number"
-      size={size}
-      nativeSize={nativeSize}
-      monospace={monospace}
-      className={className}
-    />
+    <InputGroup {...groupProps}>
+      <Input
+        {...inputProps}
+        ref={mergeRefs([ref, forwardedRef])}
+        size={size}
+        nativeSize={nativeSize}
+        monospace={monospace}
+        className={className}
+      />
+      <InputTrailingItems>
+        <StepWrap size={size}>
+          <StepButton ref={incrementButtonRef} size="zero" borderless {...incrementProps}>
+            <StyledIconChevron direction="up" />
+          </StepButton>
+          <StepButton ref={decrementButtonRef} size="zero" borderless {...decrementProps}>
+            <StyledIconChevron direction="down" />
+          </StepButton>
+        </StepWrap>
+      </InputTrailingItems>
+    </InputGroup>
   );
 };
 
 const NumberInput = forwardRef(BaseNumberInput);
 
 export default NumberInput;
+
+const StepWrap = styled('div')<{size?: FormSize}>`
+  display: flex;
+  flex-direction: column;
+  height: ${p => p.theme.form[p.size ?? 'md'].height}px;
+  width: ${space(1.5)};
+  align-items: center;
+`;
+
+const StepButton = styled(Button)`
+  display: flex;
+  height: 50%;
+  padding: 0 ${space(0.5)};
+  color: ${p => p.theme.subText};
+`;
+
+const StyledIconChevron = styled(IconChevron)<{direction: 'up' | 'down'}>`
+  width: 0.6em;
+  height: 0.6em;
+
+  ${p =>
+    p.direction === 'up'
+      ? `
+    align-self: end;
+  `
+      : `align-self: start;`}
+`;
