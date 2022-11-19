84154be2422f3e96a0cfadc81fc326fa0f3530a2
Zachary Collins <zachary.collins@sentry.io>
2022-11-19 15:50:02 -0800

    feature(hybrid-cloud): hybrid compatible auth middleware

--- src/sentry/services/hybrid_cloud/auth/impl.py
+++ src/sentry/services/hybrid_cloud/auth/impl.py
@@ -1,14 +1,28 @@
 from __future__ import annotations
 
-from typing import List
+import base64
+from typing import List, Mapping
 
+from django.contrib.auth.models import AnonymousUser
 from django.db.models import F
 
 from sentry import roles
 from sentry.auth.access import get_permissions_for_user
-from sentry.models import AuthIdentity, AuthProvider, OrganizationMember
-from sentry.services.hybrid_cloud.auth import ApiAuthState, ApiMemberSsoState, AuthService
+from sentry.auth.system import SystemToken
+from sentry.middleware.auth import RequestAuthenticationMiddleware
+from sentry.models import ApiKey, ApiToken, AuthIdentity, AuthProvider, OrganizationMember, User
+from sentry.services.hybrid_cloud.auth import (
+    ApiAuthState,
+    ApiMemberSsoState,
+    AuthenticatedToken,
+    AuthenticationRequest,
+    AuthenticationResponse,
+    AuthService,
+)
 from sentry.services.hybrid_cloud.organization import ApiOrganizationMember
+from sentry.services.hybrid_cloud.user import user_service
+from sentry.silo import SiloMode
+from sentry.utils.auth import AuthUserPasswordExpired
 from sentry.utils.types import Any
 
 _SSO_BYPASS = ApiMemberSsoState(False, True)
@@ -59,15 +73,30 @@ def query_sso_state(
             # allow bypassing SSO if there are no other
             # owners with SSO enabled.
             if member.role == roles.get_top_dog().id:
+
+                def get_user_ids(org_id: int, mem_id: int) -> Any:
+                    return (
+                        org_member_class.objects.filter(
+                            organization_id=org_id,
+                            role=roles.get_top_dog().id,
+                            user__is_active=True,
+                        )
+                        .exclude(id=mem_id)
+                        .values_list("user_id")
+                    )
+
+                if SiloMode.get_current_mode() != SiloMode.MONOLITH:
+                    # Giant hack for now until we have control silo org membership table.
+                    from sentry.testutils.silo import exempt_from_silo_limits
+
+                    with exempt_from_silo_limits():
+                        user_ids = get_user_ids(member.organization_id, member.id)
+                else:
+                    user_ids = get_user_ids(member.organization_id, member.id)
+
                 requires_sso = AuthIdentity.objects.filter(
                     auth_provider=auth_provider,
-                    user__in=org_member_class.objects.filter(
-                        organization_id=member.organization_id,
-                        role=roles.get_top_dog().id,
-                        user__is_active=True,
-                    )
-                    .exclude(id=member.id)
-                    .values_list("user_id"),
+                    user__in=user_ids,
                 ).exists()
         else:
             sso_is_valid = auth_identity.is_valid(member)
@@ -76,8 +105,33 @@ def query_sso_state(
 
 
 class DatabaseBackedAuthService(AuthService):
-    # Monolith implementation that uses OrganizationMember and User tables freely, but in silo world
-    # this won't be possible.
+    def authenticate(self, *, request: AuthenticationRequest) -> AuthenticationResponse:
+        fake_request = FakeAuthenticationRequest(request)
+        handler: Any = RequestAuthenticationMiddleware()
+        expired_user: User | None = None
+        try:
+            handler.process_request(fake_request)
+        except AuthUserPasswordExpired as e:
+            expired_user = e.user
+        except Exception as e:
+            raise Exception("Unexpected error processing handler") from e
+
+        auth: AuthenticatedToken | None = None
+        if fake_request.auth is not None:
+            auth = AuthenticatedToken.from_token(fake_request.auth)
+
+        result = AuthenticationResponse(
+            auth=auth, user_from_signed_request=fake_request.user_from_signed_request
+        )
+
+        if expired_user is not None:
+            result.user = user_service.serialize_user(expired_user)
+            result.expired = True
+        elif fake_request.user is not None and not fake_request.user.is_anonymous:
+            result.user = user_service.serialize_user(fake_request.user)
+
+        return result
+
     def get_user_auth_state(
         self,
         *,
@@ -114,3 +168,76 @@ class DatabaseBackedAuthService(AuthService):
                 flags=F("flags").bitor(AuthProvider.flags.scim_enabled)
             ).values_list("organization_id", flat=True)
         )
+
+
+class FakeRequestDict:
+    d: Mapping[str, str | bytes | None]
+
+    def __init__(self, **d: Any):
+        self.d = d
+
+    def __getitem__(self, item: str) -> str | bytes:
+        result = self.d[item]
+        if result is None:
+            raise KeyError(f"Key '{item}' does not exist")
+        return result
+
+    def __contains__(self, item: str) -> bool:
+        return self.d.get(item, None) is not None
+
+    def get(self, key: str, default: str | bytes | None = None) -> str | bytes | None:
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+
+class FakeAuthenticationRequest:
+    session: FakeRequestDict
+    GET: FakeRequestDict
+    POST: FakeRequestDict
+    req: AuthenticationRequest
+    user: User | AnonymousUser | None
+    user_from_signed_request: bool = False
+    auth: Any
+
+    def build_absolute_uri(self) -> str:
+        return self.req.absolute_url
+
+    def __init__(self, req: AuthenticationRequest) -> None:
+        self.auth = None
+        self.req = req
+        self.session = FakeRequestDict(
+            _auth_user_id=req.user_id,
+            _auth_user_backend=req.backend,
+            _auth_user_hash=req.user_hash,
+            _nonce=req.nonce,
+        )
+        self.POST = FakeRequestDict(
+            _sentry_request_signature=req.signature,
+        )
+
+        self.GET = FakeRequestDict(
+            _=req.signature,
+        )
+
+        self.META = FakeRequestDict(
+            HTTP_AUTHORIZATION=_unwrap_b64(req.authorization_b64), REMOTE_ADDR=req.remote_addr
+        )
+        self.user_from_signed_request = False
+
+    @property
+    def path(self) -> str:
+        return self.req.path
+
+
+def _unwrap_b64(input: str | None) -> bytes | None:
+    if input is None:
+        return None
+
+    return base64.b64decode(input.encode("utf8"))
+
+
+AuthenticatedToken.register_kind("system", SystemToken)
+AuthenticatedToken.register_kind("api_token", ApiToken)
+AuthenticatedToken.register_kind("api_key", ApiKey)
