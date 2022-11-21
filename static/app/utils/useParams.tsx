53140013142cb832b7a1440e52c845f760e43dd3
Alberto Leal <mail4alberto@gmail.com>
2022-11-20 20:40:05 -0500

    feat(hybrid-cloud): Update useParams to inject orgId paramater whenever a customer domain is used

--- static/app/utils/useParams.tsx
+++ static/app/utils/useParams.tsx
@@ -1,6 +1,13 @@
+import {customerDomain, usingCustomerDomain} from 'sentry/constants';
 import {useRouteContext} from 'sentry/utils/useRouteContext';
 
 export function useParams() {
-  const route = useRouteContext();
-  return route.params;
+  const {params} = useRouteContext();
+
+  if (usingCustomerDomain && customerDomain) {
+    // We do not know if the caller of this hook requires orgId, so we populate orgId implicitly.
+    return {...params, orgId: customerDomain};
+  }
+
+  return params;
 }
