6abf19d4b1cdf1b1d0fccf03a96de1f72199aa6f
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2022-11-22 12:40:08 -0800

    dependency array

--- static/app/components/buttonClick.tsx
+++ static/app/components/buttonClick.tsx
@@ -1,25 +1,35 @@
 import {useCallback} from 'react';
 
+import {ButtonProps} from 'sentry/components/button';
 import HookStore from 'sentry/stores/hookStore';
 
-const useGetHook = ({disabled, busy, onClick}) => {
+type Props = ButtonProps;
+
+const useGetHook = () => {
   const useButtonTracking = HookStore.get('react-hook:track-buttons')[0];
-  const useButtonClick = () =>
-    useCallback((e: React.MouseEvent) => {
-      // Don't allow clicks when disabled or busy
-      if (disabled || busy) {
-        e.preventDefault();
-        e.stopPropagation();
-        return;
-      }
+  const useButtonClick = (
+    busy: boolean,
+    disabled: boolean,
+    onClick: (e: React.MouseEvent) => void
+  ) => {
+    useCallback(
+      (e: React.MouseEvent) => {
+        // Don't allow clicks when disabled or busy
+        if (disabled || busy) {
+          e.preventDefault();
+          e.stopPropagation();
+          return;
+        }
 
-      if (typeof onClick !== 'function') {
-        return;
-      }
+        if (typeof onClick !== 'function') {
+          return;
+        }
 
-      onClick(e);
-    }, []);
-
+        onClick(e);
+      },
+      [busy, disabled, onClick]
+    );
+  };
   const handleClick = useButtonTracking || useButtonClick;
 
   return handleClick;
@@ -35,12 +45,8 @@ export function useButtonHook({
   priority,
   href,
   analyticsParams,
-}) {
-  const useHook = useGetHook({
-    disabled,
-    busy,
-    onClick,
-  });
+}: Props) {
+  const useHook = useGetHook();
 
   const useClick = useHook({
     disabled,
