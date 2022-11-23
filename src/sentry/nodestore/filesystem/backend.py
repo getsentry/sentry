fe5ccc9b12f4259bf29640edec808ffbeeb1bfc5
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-11-23 16:03:56 -0500

    Actually working version

--- src/sentry/nodestore/filesystem/backend.py
+++ src/sentry/nodestore/filesystem/backend.py
@@ -1,29 +1,23 @@
 import os
 
 from sentry.nodestore.base import NodeStorage
-from sentry.utils import json
 
 dirname = os.path.dirname(__file__)
 STORAGE_PATH = "nodes"
 
 
 class FileSystemNodeStorage(NodeStorage):
-    """
-    A filesystem-backed backend for storing node data.
-    """
-
     def _get_bytes(self, id):
-        path = os.path.join(dirname, STORAGE_PATH, f"{id}.json")
-        print("GET", path)
-
-        with open(path, "rb") as file:
-            data = file.read()
-
-        return data
+        with open(FileSystemNodeStorage.path(id), "rb") as file:
+            return file.read()
 
     def _set_bytes(self, id, data, ttl=0):
-        path = os.path.join(dirname, STORAGE_PATH, f"{id}.json")
-        print("SET", path)
-
-        with open(path, "wb") as file:
+        with open(FileSystemNodeStorage.path(id), "wb") as file:
             file.write(data)
+
+    def delete(self, id):
+        os.remove(FileSystemNodeStorage.path(id))
+
+    @staticmethod
+    def path(id):
+        return os.path.join(dirname, STORAGE_PATH, f"{id}.json")
