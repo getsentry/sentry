5821aba25b534c7e6b6926af063bfd4ae362cba5
Malachi Willey <malwilley@gmail.com>
2022-11-23 12:25:06 -0800

    fix(issue-details): Move status banner above event content and improve alert margins

--- static/app/components/events/styles.tsx
+++ static/app/components/events/styles.tsx
@@ -6,13 +6,14 @@ import {Theme} from 'sentry/utils/theme';
 export const DataSection = styled('div')`
   display: flex;
   flex-direction: column;
-  margin: 0;
 
   /* Padding aligns with Layout.Body */
-  padding: ${space(1)} ${space(2)};
+  padding: 0 ${space(2)};
+  margin: ${space(1)} 0;
 
   @media (min-width: ${p => p.theme.breakpoints.medium}) {
-    padding: ${space(1.5)} ${space(4)};
+    padding: 0 ${space(4)};
+    margin: ${space(1.5)} 0;
   }
 `;
 
