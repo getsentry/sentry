6fc5778b4f7c489623c77f47da6c25dae1610a52
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-11-23 14:24:02 -0800

    update useCleanQueryParamsOnRouteLeave to match pr

--- static/app/utils/useCleanQueryParamsOnRouteLeave.tsx
+++ static/app/utils/useCleanQueryParamsOnRouteLeave.tsx
@@ -1,50 +1,66 @@
 import {useCallback, useEffect} from 'react';
 import {browserHistory} from 'react-router';
-import {Location} from 'history';
+import type {Location} from 'history';
 
 import {useLocation} from 'sentry/utils/useLocation';
 
 type Opts = {
-  fields: string[];
+  fieldsToClean: string[];
 };
 
-function useCleanQueryParamsOnRouteLeave({fields}: Opts) {
+export function handleRouteLeave<Q extends object>({
+  fieldsToClean,
+  newLocation,
+  oldPathname,
+}: {
+  fieldsToClean: string[];
+  newLocation: Location<Q>;
+  oldPathname: string;
+}) {
+  const hasSomeValues = fieldsToClean.some(
+    field => newLocation.query[field] !== undefined
+  );
+
+  if (
+    newLocation.pathname === oldPathname ||
+    (newLocation.pathname !== oldPathname && !hasSomeValues)
+  ) {
+    return;
+  }
+
+  // Removes fields from the URL on route leave so that the parameters will
+  // not interfere with other pages
+  const query = fieldsToClean.reduce(
+    (newQuery, field) => {
+      newQuery[field] = undefined;
+      return newQuery;
+    },
+    {...newLocation.query}
+  );
+
+  browserHistory.replace({
+    pathname: newLocation.pathname,
+    query,
+  });
+}
+
+function useCleanQueryParamsOnRouteLeave({fieldsToClean}: Opts) {
   const location = useLocation();
 
-  const handleRouteLeave = useCallback(
-    (newLocation: Location<{cursor?: string; level?: number}>) => {
-      const hasSomeValues = fields.some(field => newLocation.query[field] !== undefined);
-
-      if (
-        newLocation.pathname === location.pathname ||
-        (newLocation.pathname !== location.pathname && !hasSomeValues)
-      ) {
-        return true;
-      }
-
-      // Removes fields from the URL on route leave so that the parameters will
-      // not interfere with other pages
-      const query = fields.reduce(
-        (newQuery, field) => {
-          newQuery[field] = undefined;
-          return newQuery;
-        },
-        {...newLocation.query}
-      );
-
-      browserHistory.replace({
-        pathname: newLocation.pathname,
-        query,
+  const onRouteLeave = useCallback(
+    newLocation => {
+      handleRouteLeave({
+        fieldsToClean,
+        newLocation,
+        oldPathname: location.pathname,
       });
-
-      return false;
     },
-    [location.pathname, fields]
+    [location.pathname, fieldsToClean]
   );
 
   useEffect(() => {
-    return browserHistory.listen(handleRouteLeave);
-  }, [handleRouteLeave]);
+    return browserHistory.listen(onRouteLeave);
+  }, [onRouteLeave]);
 }
 
 export default useCleanQueryParamsOnRouteLeave;
