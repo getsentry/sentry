90a8e60b127310ac2cc12f14ec2b6367b1979ee0
Alberto Leal <mail4alberto@gmail.com>
2022-11-23 17:09:45 -0500

    feat(hybrid-cloud): Add isValidOrgSlug

---
+++ static/app/utils/isValidOrgSlug.spec.tsx
@@ -0,0 +1,14 @@
+import isValidOrgSlug from './isValidOrgSlug';
+
+describe('isValidOrgSlug', function () {
+  it('validates org slugs', function () {
+    // valid org slugs
+    expect(isValidOrgSlug('CaNaDa')).toBe(true);
+    expect(isValidOrgSlug('foobar123')).toBe(true);
+    expect(isValidOrgSlug('albertos-apples')).toBe(true);
+
+    expect(isValidOrgSlug('albertos_apples')).toBe(false);
+    expect(isValidOrgSlug('sentry-')).toBe(false);
+    expect(isValidOrgSlug('-sentry')).toBe(false);
+  });
+});
