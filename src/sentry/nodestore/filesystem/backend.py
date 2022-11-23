8bdf54dafd355f504a4a2aaa946118f5a6dac93d
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-11-23 15:10:42 -0500

    Crimes

--- src/sentry/nodestore/filesystem/backend.py
+++ src/sentry/nodestore/filesystem/backend.py
@@ -1,8 +1,10 @@
-import logging
+import os
 
 from sentry.nodestore.base import NodeStorage
+from sentry.utils import json
 
-logger = logging.getLogger("sentry")
+dirname = os.path.dirname(__file__)
+STORAGE_PATH = "nodes"
 
 
 class FileSystemNodeStorage(NodeStorage):
@@ -10,4 +12,26 @@ class FileSystemNodeStorage(NodeStorage):
     A filesystem-backed backend for storing node data.
     """
 
-    pass
+    def get(self, id):
+        path = os.path.join(dirname, STORAGE_PATH, f"{id}.json")
+        print("GET", path)
+        with open(path) as file:
+            data = json.loads(file.read())
+
+        return data
+
+    def _set(self, id, data):
+        path = os.path.join(dirname, STORAGE_PATH, f"{id}.json")
+        print("SET", path)
+
+        with open(path, "w") as file:
+            file.write(json.dumps(data))
+
+    def delete(self, id):
+        path = os.path.join(dirname, STORAGE_PATH, f"{id}.json")
+        print("DELETE", path)
+        os.remove(path)
+
+    save = _set
+    set = _set
+    set_subkeys = _set
