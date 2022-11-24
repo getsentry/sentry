f9f1639f8431c811c23825922f4841b5623516f3
Joris Bayer <joris.bayer@sentry.io>
2022-11-24 10:36:55 +0100

    first test

--- src/sentry/ingest/transaction_clusterer/tree.py
+++ src/sentry/ingest/transaction_clusterer/tree.py
@@ -1,7 +1,7 @@
 from collections import UserDict, defaultdict
-from typing import Iterable
+from typing import Iterable, List, Optional, Union
 
-from .base import Clusterer
+from .base import Clusterer, ReplacementRule
 
 __all__ = ["TreeClusterer"]
 
@@ -10,8 +10,11 @@ __all__ = ["TreeClusterer"]
 MERGED = None
 
 
+# TODO: Metrics for tree size, spans for merge, etc.
+
+
 class TreeClusterer(Clusterer):
-    def __init__(self, merge_threshold: int) -> None:
+    def __init__(self, *, merge_threshold: int) -> None:
         self._merge_threshold = merge_threshold
         self._graph = Node()
 
@@ -22,17 +25,42 @@ class TreeClusterer(Clusterer):
             for part in parts:
                 node = node.setdefault(part, Node())
 
-    def get_rules(self) -> None:
+    def get_rules(self) -> List[ReplacementRule]:
+        """Merge high-cardinality nodes in the graph and extract rules"""
         self._graph.merge(self._merge_threshold)
+        # Generate exactly 1 rule for every merge
+        rule_paths = [path for path in self._graph.paths() if path[-1] == MERGED]
+
+        # Sort by path length, descending (most specific rule first)
+        rule_paths.sort(key=len, reverse=True)
+
+        return [self._build_rule(path) for path in rule_paths]
+
+    @staticmethod
+    def _build_rule(path: List["Node.Key"]) -> ReplacementRule:
+        return ReplacementRule("/" + "/".join(["*" if key == MERGED else key for key in path]))
 
 
 class Node(UserDict):
     """Keys in this dict are names of the children"""
 
+    Key = Union[str, type(MERGED)]
+
+    def paths(self, ancestors: Optional[List[Key]] = None) -> Iterable[List[Key]]:
+        """Collect all paths and subpaths through the graph"""
+        if ancestors is None:
+            ancestors = []
+        for name, child in self.items():
+            path = ancestors + [name]
+            yield path
+            yield from child.paths(ancestors=path)
+
     def merge(self, merge_threshold: int) -> None:
         """Recursively merge children of high-cardinality nodes"""
         if len(self) >= merge_threshold:
-            self[:] = {MERGED: self._merge_nodes(self.values())}
+            merged_children = self._merge_nodes(self.values())
+            self.clear()
+            self[MERGED] = merged_children
 
         for child in self.values():
             child.merge(merge_threshold)
---
+++ tests/sentry/ingest/test_transaction_clusterer.py
@@ -0,0 +1,18 @@
+from sentry.ingest.transaction_clusterer.tree import TreeClusterer
+
+
+def test_multi_fanout():
+    clusterer = TreeClusterer(merge_threshold=3)
+    transaction_names = [
+        "/a/b0/c/d0/e",
+        "/a/b0/c/d1/e",
+        "/a/b0/c/d2/e",
+        "/a/b1/c/d0/e",
+        "/a/b1/c/d1/e",
+        "/a/b1/c/d2/e",
+        "/a/b2/c/d0/e",
+        "/a/b2/c/d1/e",
+        "/a/b2/c/d2/e",
+    ]
+    clusterer.add_input(transaction_names)
+    assert clusterer.get_rules() == ["/a/*/c/*", "/a/*"]
