213bd6d50f038463c5b1b4f65d6c98e73e0fcdd6
Joris Bayer <joris.bayer@sentry.io>
2022-11-28 14:12:03 +0100

    ref: Move snuba query to separate function

--- src/sentry/runner/commands/cluster_transaction_names.py
+++ src/sentry/runner/commands/cluster_transaction_names.py
@@ -3,7 +3,6 @@ from datetime import datetime, timedelta
 from typing import List, Literal, Mapping, Tuple, TypedDict
 
 import click
-from snuba_sdk import Column, Condition, Entity, Limit, Op, Query, Request
 
 from sentry.ingest.transaction_clusterer.base import ReplacementRule
 from sentry.runner.decorators import configuration
@@ -13,7 +12,6 @@ logger = logging.getLogger(__name__)
 
 RULE_RETENTION = timedelta(days=90)
 OPTION_NAME = "sentry:transaction_name_normalize_rules"
-TRANSACTION_SOURCE = "url"
 
 
 class RuleSpec(TypedDict):
@@ -54,49 +52,32 @@ def cluster_transaction_names(
 def _cluster_project(
     project, merge_threshold: int, time_range: Tuple[datetime, datetime], limit: int
 ):
+    from sentry.ingest.transaction_clusterer.datasource import fetch_unique_transaction_names
     from sentry.ingest.transaction_clusterer.tree import TreeClusterer
-    from sentry.utils.snuba import raw_snql_query
 
     clusterer = TreeClusterer(merge_threshold=merge_threshold)
 
-    then, now = time_range
-    snuba_request = Request(
-        "transactions",
-        app_id="transactions",
-        query=Query(
-            match=Entity("transactions"),
-            select=[Column("transaction")],
-            where=[
-                Condition(Column("project_id"), Op.EQ, project.id),
-                Condition(Column("finish_ts"), Op.GTE, then),
-                Condition(Column("finish_ts"), Op.LT, now),
-                Condition(Column("transaction_source"), Op.EQ, TRANSACTION_SOURCE),
-            ],
-            groupby=[Column("transaction")],
-            limit=Limit(limit),
-        ),
-    )
-    snuba_response = raw_snql_query(
-        snuba_request, referrer="sentry.ingest.cluster_transaction_names"
-    )
+    transaction_names = fetch_unique_transaction_names(project, time_range, limit)
 
-    clusterer.add_input(row["transaction"] for row in snuba_response["data"])
+    clusterer.add_input(transaction_names)
 
     # TODO: span for clustering
     rules = clusterer.get_rules()
 
-    _export_rules(project, rules, now)
+    _export_rules(project, rules, time_range[0])
     logger.debug("Generated %s new rules for project %s", len(rules), project.id)
 
 
 def _export_rules(project, new_rules: List[ReplacementRule], now: datetime):
+    from sentry.ingest.transaction_clusterer.datasource import TRANSACTION_SOURCE
+
     # Load existing rules and remove expired ones:
     now_timestamp = int(now.timestamp())
     new_expiry = int((now + RULE_RETENTION).timestamp())
     existing_rules: List[RuleSpec] = [
         rule
         for rule in project.get_option(OPTION_NAME).get("rules", [])
-        if rule["source"] == TRANSACTION_SOURCE
+        if rule.get("source") == TRANSACTION_SOURCE
         and rule["type"] == "glob"
         and now_timestamp < rule["expires"]
     ]
