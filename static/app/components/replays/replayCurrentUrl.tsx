eade32ca59c92fc07e48a8e28e1c0ace840c862c
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-11-29 09:04:23 -0800

    ref(replays): Remove dead css inside the ReplayCurrentUrl component (#41818)
    Dead css, the base component design changed so we don't need the
    overrides anymore

--- static/app/components/replays/replayCurrentUrl.tsx
+++ static/app/components/replays/replayCurrentUrl.tsx
@@ -1,20 +1,14 @@
-import styled from '@emotion/styled';
-
 import {useReplayContext} from 'sentry/components/replays/replayContext';
-import TextCopyInput, {
-  StyledCopyButton,
-  StyledInput,
-} from 'sentry/components/textCopyInput';
-import space from 'sentry/styles/space';
+import TextCopyInput from 'sentry/components/textCopyInput';
 import getCurrentUrl from 'sentry/utils/replays/getCurrentUrl';
 
 function ReplayCurrentUrl() {
   const {currentTime, replay} = useReplayContext();
   if (!replay) {
     return (
-      <UrlCopyInput size="sm" disabled>
+      <TextCopyInput size="sm" disabled>
         {''}
-      </UrlCopyInput>
+      </TextCopyInput>
     );
   }
 
@@ -22,31 +16,10 @@ function ReplayCurrentUrl() {
   const crumbs = replay.getRawCrumbs();
 
   return (
-    <UrlCopyInput size="sm">
+    <TextCopyInput size="sm">
       {getCurrentUrl(replayRecord, crumbs, currentTime)}
-    </UrlCopyInput>
+    </TextCopyInput>
   );
 }
-
-const UrlCopyInput = styled(TextCopyInput)`
-  font-size: ${p => p.theme.fontSizeMedium};
-  column-gap: ${space(1)};
-
-  ${StyledInput} {
-    border-right-width: 1px;
-    border-radius: 0.25em;
-    background-color: ${p => p.theme.background};
-    border-right-color: ${p => p.theme.border};
-
-    &:hover,
-    &:focus {
-      border-right-width: 1px;
-      background-color: ${p => p.theme.background};
-    }
-  }
-  ${StyledCopyButton} {
-    border-radius: 0.25em;
-  }
-`;
 
 export default ReplayCurrentUrl;
