41d58bf020ccabfac04022eba848bf29fe31f41e
Alberto Leal <mail4alberto@gmail.com>
2022-11-29 10:59:42 -0500

    fixup! static/app/views/performance/transactionSummary/transactionOverview/trendChart/index.tsx

--- static/app/views/performance/transactionSummary/transactionOverview/trendChart/index.tsx
+++ static/app/views/performance/transactionSummary/transactionOverview/trendChart/index.tsx
@@ -12,6 +12,7 @@ import {t} from 'sentry/locale';
 import {OrganizationSummary} from 'sentry/types';
 import {getUtcToLocalDateObject} from 'sentry/utils/dates';
 import useApi from 'sentry/utils/useApi';
+import {useLocation} from 'sentry/utils/useLocation';
 import {useRouteContext} from 'sentry/utils/useRouteContext';
 
 import {TrendFunctionField} from '../../../trends/types';
