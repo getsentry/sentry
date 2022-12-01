32a2ed1624dc1d44cae4a6c2cafed8e263df2d6b
Joris Bayer <joris.bayer@sentry.io>
2022-12-01 11:04:32 +0100

    Store and test

--- src/sentry/ingest/transaction_clusterer/collect/redis.py
+++ src/sentry/ingest/transaction_clusterer/collect/redis.py
@@ -1,7 +1,41 @@
 """ Write transactions into redis sets """
+from typing import Any, Set
 
-__all__ = ["store_transaction_name"]
+from django.conf import settings
+
+from sentry.models import Project
+from sentry.utils import redis
+
+__all__ = ["store_transaction_name", "get_transaction_names"]
 
 
-def store_transaction_name(project_id: int, transaction_name: str) -> None:
-    pass
+#: Maximum number of transaction names per project that we want
+#: to store in redis.
+MAX_SET_SIZE = 1000
+
+
+add_to_set = redis.load_script("utils/sadd_capped.lua")
+
+
+def get_redis_key(project: Project) -> str:
+    return f"txnames:{project.organization_id}:{project.id}"
+
+
+def get_redis_client() -> Any:
+    cluster_key = getattr(settings, "SENTRY_TRANSACTION_NAMES_REDIS_CLUSTER", "default")
+    return redis.redis_clusters.get(cluster_key)
+
+
+def store_transaction_name(project: Project, transaction_name: str) -> None:
+    # TODO: the caller of the func should check source==url, feature flags, etc.
+    client = get_redis_client()
+    redis_key = get_redis_key(project)
+    add_to_set(client, [redis_key], [transaction_name, MAX_SET_SIZE])
+
+
+def get_transaction_names(project: Project) -> Set[str]:
+    client = get_redis_client()
+    redis_key = get_redis_key(project)
+
+    # TODO: Not sure if this works for large sets in production
+    return set(client.smembers(redis_key))
--- tests/sentry/ingest/test_transaction_clusterer.py
+++ tests/sentry/ingest/test_transaction_clusterer.py
@@ -1,4 +1,11 @@
+from unittest import mock
+
+from sentry.ingest.transaction_clusterer.collect.redis import (
+    get_transaction_names,
+    store_transaction_name,
+)
 from sentry.ingest.transaction_clusterer.tree import TreeClusterer
+from sentry.models.project import Project
 
 
 def test_multi_fanout():
@@ -27,3 +34,26 @@ def test_single_leaf():
     ]
     clusterer.add_input(transaction_names)
     assert clusterer.get_rules() == ["/a/*/**"]
+
+
+@mock.patch("sentry.ingest.transaction_clusterer.collect.redis.MAX_SET_SIZE", 5)
+def test_collection():
+    project1 = Project(id=101, name="p1", organization_id=1)
+    project2 = Project(id=102, name="project2", organization_id=1)
+
+    for project in (project1, project2):
+        for i in range(len(project.name)):
+            store_transaction_name(project, f"tx-{project.name}-{i}")
+            store_transaction_name(project, f"tx-{project.name}-{i}")
+
+    set_entries1 = get_transaction_names(project1)
+    assert set(set_entries1) == {"tx-p1-0", "tx-p1-1"}
+
+    set_entries2 = get_transaction_names(project2)
+    assert len(set_entries2) == 5
+    # We don't know which entries made it into the final set:
+    for name in set_entries2:
+        assert name.startswith("tx-project2-")
+
+    # TODO: Test project without redis key
+    # TODO: clean up redis
