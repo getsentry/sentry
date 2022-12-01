8ceaf36d2566a6d21b506d2f45f755381d97fa24
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-12-01 11:26:21 -0500

    Samplify

--- src/sentry/nodestore/filesystem/backend.py
+++ src/sentry/nodestore/filesystem/backend.py
@@ -7,16 +7,21 @@ STORAGE_PATH = "nodes"
 
 
 class FileSystemNodeStorage(NodeStorage):
+    """
+    A simple backend that saves each node as a file. Only appropriate for
+    debugging and development!
+    """
+
     def _get_bytes(self, id):
-        with open(FileSystemNodeStorage.path(id), "rb") as file:
+        with open(self.path(id), "rb") as file:
             return file.read()
 
     def _set_bytes(self, id, data, ttl=0):
-        with open(FileSystemNodeStorage.path(id), "wb") as file:
+        with open(self.path(id), "wb") as file:
             file.write(data)
 
     def delete(self, id):
-        os.remove(FileSystemNodeStorage.path(id))
+        os.remove(self.path(id))
 
     @staticmethod
     def path(id):
