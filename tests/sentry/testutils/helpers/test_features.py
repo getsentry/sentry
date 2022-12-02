f70791d330b11b29049f0abbb519065092dab71e
Alberto Leal <mail4alberto@gmail.com>
2022-12-02 17:41:25 -0500

    chore(hybrid-cloud): Add test to check that ApiOrganization works for feature.has (#41981)

--- tests/sentry/testutils/helpers/test_features.py
+++ tests/sentry/testutils/helpers/test_features.py
@@ -1,4 +1,5 @@
 from sentry import features
+from sentry.services.hybrid_cloud.organization import ApiOrganization, organization_service
 from sentry.testutils import TestCase
 from sentry.testutils.helpers import with_feature
 
@@ -13,3 +14,25 @@ class TestTestUtilsFeatureHelper(TestCase):
     @with_feature("organizations:global-views")
     def test_with_feature(self):
         assert features.has("organizations:global-views", self.org)
+
+    def test_feature_with_api_organization(self):
+
+        with self.feature({"organizations:customer-domains": False}):
+            org_context = organization_service.get_organization_by_slug(
+                slug=self.org.slug, only_visible=False, user_id=None
+            )
+            assert org_context
+            assert org_context.organization
+            assert isinstance(org_context.organization, ApiOrganization)
+
+            assert features.has("organizations:customer-domains", org_context.organization) is False
+
+        with self.feature({"organizations:customer-domains": True}):
+            org_context = organization_service.get_organization_by_slug(
+                slug=self.org.slug, only_visible=False, user_id=None
+            )
+            assert org_context
+            assert org_context.organization
+            assert isinstance(org_context.organization, ApiOrganization)
+
+            assert features.has("organizations:customer-domains", org_context.organization)
