31e4e95f4fb5ec33660167c10a60c2ae14286cbf
mdtro <matthew.trostel@sentry.io>
2022-12-05 10:20:07 -0600

    fix(twilio): split on any whitespace between phone numbers

--- tests/sentry_plugins/twilio/test_plugin.py
+++ tests/sentry_plugins/twilio/test_plugin.py
@@ -22,9 +22,27 @@ class TwilioPluginSMSSplitTest(TestCase):
         actual = split_sms_to(to)
         assert expected == actual
 
+    def test_valid_split_sms_to_with_no_whitespace(self):
+        to = "330-509-3095,(330)-509-3095,+13305093095,4045550144"
+        expected = {"330-509-3095", "(330)-509-3095", "+13305093095", "4045550144"}
+        actual = split_sms_to(to)
+        assert expected == actual
+
     def test_split_sms_to_with_single_number(self):
         to = "555-555-5555"
         expected = {"555-555-5555"}
+        actual = split_sms_to(to)
+        assert expected == actual
+
+    def test_valid_split_sms_to_newline(self):
+        to = "330-509-3095,\n(330)-509-3095\n,+13305093095\n,\n4045550144"
+        expected = {"330-509-3095", "(330)-509-3095", "+13305093095", "4045550144"}
+        actual = split_sms_to(to)
+        assert expected == actual
+
+    def test_valid_split_sms_to_with_extra_newlines(self):
+        to = "330-509-3095\n\n\n\n\n,\n\n\n\n\n\n\n\n\n(330)-509-3095,\n\n\n\n+13305093095,\n\n4045550144"
+        expected = {"330-509-3095", "(330)-509-3095", "+13305093095", "4045550144"}
         actual = split_sms_to(to)
         assert expected == actual
 
