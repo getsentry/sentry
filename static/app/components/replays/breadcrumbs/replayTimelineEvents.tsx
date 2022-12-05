3ec4cd5db68ea5deafd3fb93106f1ffe03bae94a
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-12-05 08:54:56 -0800

    bug(replays): Fix overflow of breadcrumbs inside Timeline events (#41862)
    Before there was no overflow, the list of breadcrumbs would run off past
    the bottom of the page.
    
    Now we contain the list inside the page, and let it be scrollable.
    
    | Before | After |
    | --- | --- |
    | <img width="449" alt="SCR-20221129-hyk"
    src="https://user-images.githubusercontent.com/187460/204648374-3f907cbc-aa52-44b2-80d2-bfdf73c96589.png">
    | <img width="410" alt="SCR-20221129-i8g"
    src="https://user-images.githubusercontent.com/187460/204648380-184219f8-63b4-40d7-899e-670524c2da27.png">
    |

--- static/app/components/replays/breadcrumbs/replayTimelineEvents.tsx
+++ static/app/components/replays/breadcrumbs/replayTimelineEvents.tsx
@@ -72,13 +72,12 @@ function Event({
   crumbs: Crumb[];
   markerWidth: number;
   startTimestampMs: number;
-  className?: string;
 }) {
   const theme = useTheme();
   const {handleMouseEnter, handleMouseLeave, handleClick} =
     useCrumbHandlers(startTimestampMs);
 
-  const title = crumbs.map(crumb => (
+  const buttons = crumbs.map(crumb => (
     <BreadcrumbItem
       key={crumb.id}
       crumb={crumb}
@@ -90,6 +89,7 @@ function Event({
       onClick={handleClick}
     />
   ));
+  const title = <TooltipWrapper>{buttons}</TooltipWrapper>;
 
   const overlayStyle = css`
     /* We make sure to override existing styles */
@@ -175,6 +175,11 @@ const IconNode = styled('div')<{colors: Color[]; crumbCount: number}>`
   ${getBackgroundGradient}
   box-shadow: ${p => p.theme.dropShadowLightest};
   user-select: none;
+`;
+
+const TooltipWrapper = styled('div')`
+  max-height: calc(100vh - ${space(4)});
+  overflow: auto;
 `;
 
 export default ReplayTimelineEvents;
