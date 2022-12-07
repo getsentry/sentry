151884982d849dde68883a73fb477aca64568d0f
Alberto Leal <mail4alberto@gmail.com>
2022-12-07 16:19:17 -0500

    feat(hybrid-cloud): Add customer domain support for org creation flow (#42114)

--- static/app/views/organizationCreate.spec.tsx
+++ static/app/views/organizationCreate.spec.tsx
@@ -5,12 +5,13 @@ import OrganizationCreate from 'sentry/views/organizationCreate';
 
 describe('OrganizationCreate', function () {
   beforeEach(() => {
-    ConfigStore.get('termsUrl', null);
-    ConfigStore.get('privacyUrl', null);
+    ConfigStore.get('termsUrl');
+    ConfigStore.get('privacyUrl');
   });
 
   afterEach(() => {
     MockApiClient.clearMockResponses();
+    jest.resetAllMocks();
   });
 
   it('renders without terms', function () {
@@ -29,6 +30,7 @@ describe('OrganizationCreate', function () {
     const orgCreateMock = MockApiClient.addMockResponse({
       url: '/organizations/',
       method: 'POST',
+      body: TestStubs.Organization(),
     });
     ConfigStore.set('termsUrl', 'https://example.com/terms');
     ConfigStore.set('privacyUrl', 'https://example.com/privacy');
@@ -48,6 +50,43 @@ describe('OrganizationCreate', function () {
       expect.objectContaining({
         data: {agreeTerms: true, defaultTeam: true, name: 'Good Burger'},
       })
+    );
+    expect(window.location.assign).toHaveBeenCalledTimes(1);
+    expect(window.location.assign).toHaveBeenCalledWith(
+      '/organizations/org-slug/projects/new/'
+    );
+  });
+
+  it('creates a new org with customer domain feature', function () {
+    const orgCreateMock = MockApiClient.addMockResponse({
+      url: '/organizations/',
+      method: 'POST',
+      body: TestStubs.Organization({
+        features: ['customer-domains'],
+      }),
+    });
+    ConfigStore.set('termsUrl', 'https://example.com/terms');
+    ConfigStore.set('privacyUrl', 'https://example.com/privacy');
+    render(<OrganizationCreate />);
+    expect(screen.getByText('Create a New Organization')).toBeInTheDocument();
+
+    userEvent.paste(screen.getByPlaceholderText('e.g. My Company'), 'Good Burger');
+    userEvent.click(
+      screen.getByRole('checkbox', {
+        name: 'I agree to the Terms of Service and the Privacy Policy',
+      })
+    );
+    userEvent.click(screen.getByText('Create Organization'));
+
+    expect(orgCreateMock).toHaveBeenCalledWith(
+      '/organizations/',
+      expect.objectContaining({
+        data: {agreeTerms: true, defaultTeam: true, name: 'Good Burger'},
+      })
+    );
+    expect(window.location.assign).toHaveBeenCalledTimes(1);
+    expect(window.location.assign).toHaveBeenCalledWith(
+      'https://org-slug.sentry.io/projects/new/'
     );
   });
 });
