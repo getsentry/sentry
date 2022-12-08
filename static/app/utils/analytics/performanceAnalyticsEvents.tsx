755ae0269e2096b8741262021fc26ba5cafc68f4
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2022-12-08 12:20:24 -0500

    add new param to the widget analytics

--- static/app/utils/analytics/performanceAnalyticsEvents.tsx
+++ static/app/utils/analytics/performanceAnalyticsEvents.tsx
@@ -50,6 +50,7 @@ export type PerformanceEventParameters = {
     widget_type?: string;
   };
   'performance_views.landingv3.widget.switch': {
+    is_new_menu: boolean;
     from_default?: boolean;
     from_widget?: string;
     to_widget?: string;
