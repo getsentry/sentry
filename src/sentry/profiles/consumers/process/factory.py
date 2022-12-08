c6896122ce2f9b739821c055390265b7e1b2acb2
Pierre Massat <pierre.massat@sentry.io>
2022-12-08 10:20:57 -0500

    Simplify the consumer

--- src/sentry/profiles/consumers/process/factory.py
+++ src/sentry/profiles/consumers/process/factory.py
@@ -1,4 +1,3 @@
-import logging
 from typing import Mapping
 
 import msgpack
@@ -10,31 +9,28 @@ from arroyo.types import Commit, Message, Partition
 from sentry.profiles.task import process_profile_task
 from sentry.utils import json
 
-logger = logging.getLogger(__name__)
+
+def process_message(message: Message[KafkaPayload]) -> None:
+    message_dict = msgpack.unpackb(message.payload.value, use_list=False)
+    profile = json.loads(message_dict["payload"], use_rapid_json=True)
+    profile.update(
+        {
+            "organization_id": message_dict["organization_id"],
+            "project_id": message_dict["project_id"],
+            "received": message_dict["received"],
+        }
+    )
+
+    process_profile_task.s(profile=profile).apply_async()
 
 
 class ProcessProfileStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
-    def process_message(self, message: Message[KafkaPayload]) -> KafkaPayload:
-        message_dict = msgpack.unpackb(message.payload.value, use_list=False)
-        profile = json.loads(message_dict["payload"], use_rapid_json=True)
-        profile.update(
-            {
-                "organization_id": message_dict["organization_id"],
-                "project_id": message_dict["project_id"],
-                "received": message_dict["received"],
-            }
-        )
-
-        process_profile_task.s(profile=profile).apply_async()
-
-        return KafkaPayload(key=None, value=None, headers=[])
-
     def create_with_partitions(
         self,
         commit: Commit,
         partitions: Mapping[Partition, int],
     ) -> ProcessingStrategy[KafkaPayload]:
         return TransformStep(
-            function=self.process_message,
+            function=process_message,
             next_step=CommitOffsets(commit),
         )
