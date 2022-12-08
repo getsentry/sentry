1ff09a4a380b651acc4872f26597271d2ea66d89
Matt Quinn <matt.quinn@sentry.io>
2022-12-08 15:05:18 -0500

    feat(perf-issues): Detect MN+1 DB Queries (#41968)
    In practice a fairly common pattern similar to an N+1 DB Query (which we
    already detect) is MN+1 DB Queries, where the repeated DB query is not
    consecutive, but instead part of a repeating pattern. A common example
    is in a serializer, where the N+1 is caused by accessing a related
    object of each object being serialized. If two related objects are
    accessed in this way, you'll get an `A B A B A B...` pattern of repeated
    database access.
    
    This detector will detect potential problems but not create Issues yet.
    It is intended for finding matching data and examining them to determine
    if this detector is useful, what its thresholds should be, and how we
    should fingerprint the resulting Issues.
    
    This detector does not check parents, nor does it enforce that spans are
    in sequence. We've seen a practical MN+1 example where the spans are
    parallel (the Node.js GraphQL transaction used as test data). We'll look
    at what this detector detects to determine if that is appropriate long
    term or not.

--- src/sentry/utils/performance_issues/performance_detection.py
+++ src/sentry/utils/performance_issues/performance_detection.py
@@ -6,7 +6,7 @@ import os
 import random
 import re
 from abc import ABC, abstractmethod
-from collections import defaultdict
+from collections import defaultdict, deque
 from dataclasses import dataclass
 from datetime import timedelta
 from enum import Enum
@@ -63,6 +63,7 @@ class DetectorType(Enum):
     N_PLUS_ONE_API_CALLS = "n_plus_one_api_calls"
     CONSECUTIVE_DB_OP = "consecutive_db"
     FILE_IO_MAIN_THREAD = "file_io_main_thread"
+    M_N_PLUS_ONE_DB = "m_n_plus_one_db"
 
 
 DETECTOR_TYPE_TO_GROUP_TYPE = {
@@ -76,6 +77,7 @@ DETECTOR_TYPE_TO_GROUP_TYPE = {
     DetectorType.N_PLUS_ONE_API_CALLS: GroupType.PERFORMANCE_N_PLUS_ONE_API_CALLS,
     DetectorType.CONSECUTIVE_DB_OP: GroupType.PERFORMANCE_CONSECUTIVE_DB_OP,
     DetectorType.FILE_IO_MAIN_THREAD: GroupType.PERFORMANCE_FILE_IO_MAIN_THREAD,
+    DetectorType.M_N_PLUS_ONE_DB: GroupType.PERFORMANCE_M_N_PLUS_ONE_DB_QUERIES,
 }
 
 # Detector and the corresponding system option must be added to this list to have issues created.
@@ -329,6 +331,11 @@ def get_detection_settings(project_id: Optional[str] = None) -> Dict[DetectorTyp
             "concurrency_threshold": 5,  # ms
             "count": 10,
             "allowed_span_ops": ["http.client"],
+        },
+        DetectorType.M_N_PLUS_ONE_DB: {
+            "total_duration_threshold": 100.0,  # ms
+            "minimum_occurrences_of_pattern": 3,
+            "max_sequence_length": 5,
         },
     }
 
@@ -1444,6 +1451,200 @@ class FileIOMainThreadDetector(PerformanceDetector):
             return False
         # doing is True since the value can be any type
         return data.get("blocked_main_thread", False) is True
+
+
+class MNPlusOneState(ABC):
+    """Abstract base class for the MNPlusOneDBSpanDetector state machine."""
+
+    @abstractmethod
+    def next(self, span: Span) -> Tuple[MNPlusOneState, Optional[PerformanceProblem]]:
+        raise NotImplementedError
+
+    def finish(self) -> Optional[PerformanceProblem]:
+        return None
+
+    def _equivalent(self, a: Span, b: Span) -> bool:
+        """db spans are equivalent if their ops and hashes match. Other spans are
+        equivalent if their ops match."""
+        first_op = a.get("op") or None
+        second_op = b.get("op") or None
+        if not first_op or not second_op or first_op != second_op:
+            return False
+
+        if first_op.startswith("db"):
+            return a.get("hash") == b.get("hash")
+
+        return True
+
+
+class SearchingForMNPlusOne(MNPlusOneState):
+    """
+    The initial state for the MN+1 DB Query detector, and the state we return to
+    whenever there is no active repeating pattern being checked.
+
+    Keeps a list of recently seen spans until a repeat is found, at which point
+    it transitions to the ContinuingMNPlusOne state.
+    """
+
+    __slots__ = ("settings", "recent_spans")
+
+    def __init__(
+        self, settings: Dict[str, Any], initial_spans: Optional[Sequence[Span]] = None
+    ) -> None:
+        self.settings = settings
+        self.recent_spans = deque(initial_spans or [], self.settings["max_sequence_length"])
+
+    def next(self, span: Span) -> Tuple[MNPlusOneState, Optional[PerformanceProblem]]:
+        # Can't be a potential MN+1 without at least 2 previous spans.
+        if len(self.recent_spans) <= 1:
+            self.recent_spans.append(span)
+            return (self, None)
+
+        # Has an MN pattern begun to repeat itself? If so, transition to the
+        # ContinuingMNPlusOne state.
+        # Convert the recent_spans deque into a list for slicing. Skip the last
+        # item in the list because that would find an N+1 instead.
+        recent_span_list = list(self.recent_spans)
+        for i, recent_span in enumerate(recent_span_list[:-1]):
+            if self._equivalent(span, recent_span):
+                pattern = recent_span_list[i:]
+                if self._is_valid_pattern(pattern):
+                    return (ContinuingMNPlusOne(self.settings, pattern, span), None)
+
+        # We haven't found a pattern yet, so remember this span and keep
+        # looking.
+        self.recent_spans.append(span)
+        return (self, None)
+
+    def _is_valid_pattern(self, pattern: Sequence[Span]) -> bool:
+        """A valid pattern contains at least one db operation and is not all equivalent."""
+        found_db_op = False
+        found_different_span = False
+
+        for span in pattern:
+            op = span.get("op") or ""
+            description = span.get("description") or ""
+            found_db_op = found_db_op or (
+                op.startswith("db") and description and not description.endswith("...")
+            )
+            found_different_span = found_different_span or not self._equivalent(pattern[0], span)
+            if found_db_op and found_different_span:
+                return True
+
+        return False
+
+
+class ContinuingMNPlusOne(MNPlusOneState):
+    """
+    The state for when we think we might have found a pattern: a sequence of
+    spans that has begun to repeat.
+
+    When the sequence is broken (either by a mismatched span or span iteration
+    finishing), returns to the SearchingMNPlusOne state, possibly returning a
+    PerformanceProblem if the detected sequence met our thresholds.
+    """
+
+    __slots__ = ("settings", "pattern", "spans", "pattern_index")
+
+    def __init__(self, settings: Dict[str, Any], pattern: Sequence[Span], first_span: Span) -> None:
+        self.settings = settings
+        self.pattern = pattern
+
+        # The full list of spans involved in the MN pattern.
+        self.spans = pattern.copy()
+        self.spans.append(first_span)
+        self.pattern_index = 1
+
+    def next(self, span: Span) -> MNPlusOneState:
+        # If the MN pattern is continuing, carry on in this state.
+        pattern_span = self.pattern[self.pattern_index]
+        if self._equivalent(pattern_span, span):
+            self.spans.append(span)
+            self.pattern_index += 1
+            if self.pattern_index >= len(self.pattern):
+                self.pattern_index = 0
+            return (self, None)
+
+        # We've broken the MN pattern, so return to the Searching state. If it
+        # is a significant problem, also return a PerformanceProblem.
+        times_occurred = int(len(self.spans) / len(self.pattern))
+        self.spans.append(span)
+        start_index = len(self.pattern) * times_occurred
+        return (
+            SearchingForMNPlusOne(self.settings, self.spans[start_index:]),
+            self._maybe_performance_problem(),
+        )
+
+    def finish(self) -> Optional[PerformanceProblem]:
+        return self._maybe_performance_problem()
+
+    def _maybe_performance_problem(self) -> Optional[PerformanceProblem]:
+        times_occurred = int(len(self.spans) / len(self.pattern))
+        minimum_occurrences_of_pattern = self.settings["minimum_occurrences_of_pattern"]
+        if times_occurred < minimum_occurrences_of_pattern:
+            return None
+
+        offender_span_count = len(self.pattern) * times_occurred
+        offender_spans = self.spans[:offender_span_count]
+
+        total_duration_threshold = self.settings["total_duration_threshold"]
+        total_duration = sum(map(get_span_duration, offender_spans), timedelta(0))
+        if total_duration < timedelta(milliseconds=total_duration_threshold):
+            return None
+
+        db_span = self._first_db_span()
+        return PerformanceProblem(
+            fingerprint=self._fingerprint(db_span["hash"]),
+            op="db",
+            desc=db_span["description"],
+            type=GroupType.PERFORMANCE_M_N_PLUS_ONE_DB_QUERIES,
+            parent_span_ids=[],
+            cause_span_ids=[],
+            offender_span_ids=[span["span_id"] for span in offender_spans],
+        )
+
+    def _first_db_span(self) -> Optional[Span]:
+        for span in self.spans:
+            if span["op"].startswith("db"):
+                return span
+        return None
+
+    def _fingerprint(self, db_hash) -> str:
+        # TODO: Add more information to the hash. Since issues aren't being
+        # detected yet, this doesn't matter.
+        problem_class = GroupType.PERFORMANCE_M_N_PLUS_ONE_DB_QUERIES
+        full_fingerprint = hashlib.sha1(db_hash.encode("utf8")).hexdigest()
+        return f"1-{problem_class}-{full_fingerprint}"
+
+
+class MNPlusOneDBSpanDetector(PerformanceDetector):
+    """
+    Detects N+1 DB query issues where the repeated query is interspersed with
+    other spans (which may or may not be other queries) that all repeat together
+    (hence, MN+1).
+
+    Currently does not consider parent or source spans, and only looks for a
+    repeating pattern of spans (A B C A B C etc).
+
+    Uses a small state machine internally.
+    """
+
+    __slots__ = ("stored_problems", "state")
+
+    settings_key = DetectorType.M_N_PLUS_ONE_DB
+
+    def init(self):
+        self.stored_problems = {}
+        self.state = SearchingForMNPlusOne(self.settings)
+
+    def visit_span(self, span):
+        self.state, performance_problem = self.state.next(span)
+        if performance_problem:
+            self.stored_problems[performance_problem.fingerprint] = performance_problem
+
+    def on_complete(self) -> None:
+        if performance_problem := self.state.finish():
+            self.stored_problems[performance_problem.fingerprint] = performance_problem
 
 
 # Reports metrics and creates spans for detection
