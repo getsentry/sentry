e4c62a8b493e560e02d50eeb7235645f37e60e01
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-12-08 16:55:16 -0800

    feat(replays): Replay Details console uses virtualized list component & style update (#42153)
    Virtualize rendering, and all the style improvements
    
    **Hover a row, you'll see the View Issue link**
    <img width="601" alt="SCR-20221207-kbw"
    src="https://user-images.githubusercontent.com/187460/206312097-14e7c53c-85b6-4546-8fc2-2ceb93f1197a.png">
    
    **Hover the timestamp to reveal the Play icon**
    <img width="596" alt="SCR-20221207-kc0"
    src="https://user-images.githubusercontent.com/187460/206312099-36f02260-dc01-4e3a-960b-09872ed9f0ed.png">
    
    **I think the yellow font is too close to the background, probably a bad
    readability score (havn't checked yet)**
    <img width="614" alt="SCR-20221207-kc4"
    src="https://user-images.githubusercontent.com/187460/206312102-c0fdbf1f-e66e-43f3-ad09-7aaaaf6c2f69.png">
    
    
    Per figma designs here:
    https://www.figma.com/file/OPFnT04z0uy8BEUms9OU4f/Specs%3A-Details-v3?node-id=634%3A20250&t=01f23lg72Ei2TZld-0
    
    
    Missing from this is the expand/collapse rows feature. I will add that
    in a followup PR though, it's exciting enough as-is.
    
    References #38948

---
+++ static/app/views/replays/detail/timestampButton.tsx
@@ -0,0 +1,46 @@
+import styled from '@emotion/styled';
+
+import DateTime from 'sentry/components/dateTime';
+import {showPlayerTime} from 'sentry/components/replays/utils';
+import Tooltip from 'sentry/components/tooltip';
+import {IconPlay} from 'sentry/icons';
+import space from 'sentry/styles/space';
+
+type TimestampButtonProps = {
+  onClick: () => void;
+  startTimestampMs: number;
+  timestampMs: string | number | Date;
+};
+
+function TimestampButton({onClick, startTimestampMs, timestampMs}: TimestampButtonProps) {
+  return (
+    <Tooltip title={<DateTime date={timestampMs} />}>
+      <StyledButton onClick={onClick}>
+        <IconPlay size="xs" />
+        {showPlayerTime(timestampMs, startTimestampMs)}
+      </StyledButton>
+    </Tooltip>
+  );
+}
+
+const StyledButton = styled('button')`
+  background: none;
+  border: none;
+  color: ${p => p.theme.subText};
+  font-size: ${p => p.theme.fontSizeSmall};
+  font-variant-numeric: tabular-nums;
+
+  display: flex;
+  align-items: center;
+  gap: ${space(0.25)};
+  padding: 0;
+
+  & > svg {
+    visibility: hidden;
+  }
+  &:hover svg {
+    visibility: visible;
+  }
+`;
+
+export default TimestampButton;
