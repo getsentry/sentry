6379b8bd08ab72f80a9a83a9ac46d3995ce7ebd4
doralchan <dora.l.chan@gmail.com>
2022-12-09 12:06:44 -0800

    fix acceptance test

--- static/app/views/performance/transactionSummary/transactionSpans/index.spec.tsx
+++ static/app/views/performance/transactionSummary/transactionSpans/index.spec.tsx
@@ -112,7 +112,7 @@ describe('Performance > Transaction Spans', function () {
       const grid = await screen.findByTestId('grid-editable');
       expect(await within(grid).findByText('Span Operation')).toBeInTheDocument();
       expect(await within(grid).findByText('Span Name')).toBeInTheDocument();
-      expect(await within(grid).findByText('Total Count')).toBeInTheDocument();
+      expect(await within(grid).findByText('Span Count')).toBeInTheDocument();
       expect(await within(grid).findByText('Frequency')).toBeInTheDocument();
       expect(await within(grid).findByText('P75 Self Time')).toBeInTheDocument();
       expect(await within(grid).findByText('Total Self Time')).toBeInTheDocument();
