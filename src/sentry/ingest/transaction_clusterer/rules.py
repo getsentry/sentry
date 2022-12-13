bd370b1b0ba4b8727dacbd420fdeff8e564dbd3b
Joris Bayer <joris.bayer@sentry.io>
2022-12-13 15:09:54 +0100

    wip

--- src/sentry/ingest/transaction_clusterer/rules.py
+++ src/sentry/ingest/transaction_clusterer/rules.py
@@ -1,36 +1,99 @@
 from datetime import datetime, timezone
-from typing import MutableMapping, Sequence
+from typing import List, Mapping, NewType, Protocol, Sequence
 
 from sentry.ingest.transaction_clusterer.datasource.redis import get_redis_client
 from sentry.models import Project
 
 from .base import ReplacementRule
 
-#: Retention of a rule derived by the clusterer
-RULE_TTL = 90 * 24 * 60 * 60
-
-REDIS_KEY_PREFIX_RULES = "txrules:"
+#: Map from rule string to last_seen timestamp
+RuleSet = NewType("RuleSet", Mapping[ReplacementRule, int])
 
 
-def _get_rules_key(project: Project) -> str:
-    return f"{REDIS_KEY_PREFIX_RULES}o:{project.organization_id}:p:{project.id}"
+class RuleStore(Protocol):
+    def read(self, project: Project) -> RuleSet:
+        ...
+
+    def write(self, project: Project, rules: RuleSet) -> None:
+        ...
+
+
+class RedisRuleStore:
+    @staticmethod
+    def _get_rules_key(project: Project) -> str:
+        return f"txrules:o:{project.organization_id}:p:{project.id}"
+
+    def read(self, project: Project) -> RuleSet:
+        client = get_redis_client()
+        key = self._get_rules_key(project)
+        return client.hgetall(key)  # type: ignore
+
+    def write(self, project: Project, rules: RuleSet) -> None:
+        client = get_redis_client()
+        key = self._get_rules_key(project)
+
+        # TODO: Replace contents
+        client.hmset(key, rules)
+
+
+class ProjectOptionRuleStore:
+
+    _option_name = "TODO"
+
+    def read(self, project: Project) -> RuleSet:
+        return project.get_option(self._option_name)
+
+    def write(self, project: Project, rules: RuleSet) -> None:
+        project.update_option(self._option_name, rules)
+
+
+class CompositeRuleStore:
+    def __init__(self, stores: List[RuleStore]):
+        self._stores = stores
+
+    def read(self, project: Project) -> RuleSet:
+        merged_rules = {}
+        for store in self._stores:
+            rules = store.read(project)
+            for rule, last_seen in rules.items():
+                if rule not in merged_rules or merged_rules[rule] < last_seen:
+                    merged_rules[rule] = last_seen
+
+        return RuleSet(merged_rules)
+
+    def write(self, project: Project, rules: RuleSet) -> None:
+        for store in self._stores:
+            store.write(project, rules)
+
+    def merge(self, project: Project) -> None:
+        merged_rules = self.read(project)
+        self.write(project, merged_rules)
+
+
+class LocalRuleStore:
+    def __init__(self, rules: RuleSet):
+        self._rules = rules
+
+    def read(self, project: Project) -> RuleSet:
+        return self._rules
+
+    def write(self, project: Project, rules: RuleSet) -> None:
+        self._rules = rules
 
 
 def _now() -> int:
     return int(datetime.now(timezone.utc).timestamp())
 
 
-def get_rules(project: Project) -> MutableMapping[str, int]:
-    client = get_redis_client()
-    key = _get_rules_key(project)
-    return client.hgetall(key)  # type: ignore
-
-
 def update_rules(project: Project, new_rules: Sequence[ReplacementRule]) -> None:
-    new_expiry = _now() + RULE_TTL
+    last_seen = _now()
+    new_rule_set = RuleSet({rule: last_seen for rule in new_rules})
 
-    client = get_redis_client()
-    key = _get_rules_key(project)
-
-    # Update rules, overwrite existing entries to update their expiry dates
-    client.hmset(key, {rule: new_expiry for rule in new_rules})
+    rule_store = CompositeRuleStore(
+        [
+            RedisRuleStore(),
+            ProjectOptionRuleStore(),
+            LocalRuleStore(new_rule_set),
+        ]
+    )
+    rule_store.merge(project)
