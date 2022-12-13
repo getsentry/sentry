b2a85379833a2861725f34b537db405b2b40c904
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2022-12-13 12:43:07 -0500

    remove tooltip

--- static/app/views/performance/landing/widgets/widgetDefinitions.tsx
+++ static/app/views/performance/landing/widgets/widgetDefinitions.tsx
@@ -316,8 +316,7 @@ export const WIDGET_DEFINITIONS: ({
   },
   [PerformanceWidgetSetting.SPAN_OPERATIONS]: {
     title: t('Span Operations'),
-    // TODO remove me
-    titleTooltip: 'remove me',
+    titleTooltip: '',
     fields: SPAN_OP_BREAKDOWN_FIELDS.map(spanOp => `p75(${spanOp})`),
     dataType: GenericPerformanceWidgetDataType.stacked_bars,
   },
