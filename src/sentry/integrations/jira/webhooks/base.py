ddf0409bb8b7ecc80be8269e1d37b1ea64b1bb71
Armen Zambrano G <armenzg@sentry.io>
2022-12-14 14:04:46 -0500

    ref(jira): Report errors when failures occur (#42342)

--- src/sentry/integrations/jira/webhooks/base.py
+++ src/sentry/integrations/jira/webhooks/base.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import abc
+import logging
 
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
@@ -9,6 +10,8 @@ from rest_framework.response import Response
 
 from sentry.api.base import Endpoint
 from sentry.integrations.utils import AtlassianConnectValidationError
+
+logger = logging.getLogger(__name__)
 
 
 class JiraTokenError(Exception):
@@ -27,6 +30,9 @@ class JiraEndpointBase(Endpoint, abc.ABC):
     def handle_exception(self, request: Request, exc: Exception) -> Response:
         if isinstance(exc, (AtlassianConnectValidationError, JiraTokenError)):
             return self.respond(status=status.HTTP_400_BAD_REQUEST)
+        # Perhaps it makes sense to do this in the base class, however, I'm concerned
+        # it would create too many errors at once and may be grouped together
+        logger.exception("Unclear JIRA exception.")
         return super().handle_exception(request, exc)
 
     def get_token(self, request: Request) -> str:
