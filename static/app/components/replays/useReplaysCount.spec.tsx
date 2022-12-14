337c58a70d719243f964351e50cbfef84baebf2f
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-12-14 17:40:25 -0500

    add test to check that no request happens if groupId list is constant

--- static/app/components/replays/useReplaysCount.spec.tsx
+++ static/app/components/replays/useReplaysCount.spec.tsx
@@ -170,6 +170,52 @@ describe('useReplaysCount', () => {
     });
   });
 
+  it('should not request anything if there are no new ids to query', async () => {
+    const replayCountRequest = MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/issue-replay-count/`,
+      method: 'GET',
+      body: {},
+    });
+
+    const {result, rerender, waitForNextUpdate} = reactHooks.renderHook(useReplaysCount, {
+      initialProps: {
+        organization,
+        projectIds,
+        groupIds: mockGroupIds,
+      },
+    });
+
+    await waitForNextUpdate();
+
+    expect(replayCountRequest).toHaveBeenCalledTimes(1);
+    expect(replayCountRequest).toHaveBeenCalledWith(
+      '/organizations/org-slug/issue-replay-count/',
+      expect.objectContaining({
+        query: {
+          query: `issue.id:[123,456]`,
+          statsPeriod: '14d',
+          project: [2],
+        },
+      })
+    );
+    expect(result.current).toEqual({
+      123: 0,
+      456: 0,
+    });
+
+    rerender({
+      organization,
+      projectIds,
+      groupIds: mockGroupIds,
+    });
+
+    expect(replayCountRequest).toHaveBeenCalledTimes(1);
+    expect(result.current).toEqual({
+      123: 0,
+      456: 0,
+    });
+  });
+
   it('should query for transactionNames', async () => {
     const countRequest = MockApiClient.addMockResponse({
       url: `/organizations/${organization.slug}/events/`,
