aab0e2e634ea6ee1f73122d2e5d24af4511eed62
Malachi Willey <malwilley@gmail.com>
2022-12-15 12:01:25 -0800

    Fix test

--- static/app/views/issueList/overview.spec.jsx
+++ static/app/views/issueList/overview.spec.jsx
@@ -16,6 +16,7 @@ import StreamGroup from 'sentry/components/stream/group';
 import ProjectsStore from 'sentry/stores/projectsStore';
 import TagStore from 'sentry/stores/tagStore';
 import {SavedSearchVisibility} from 'sentry/types';
+import localStorageWrapper from 'sentry/utils/localStorage';
 import * as parseLinkHeader from 'sentry/utils/parseLinkHeader';
 import IssueListWithStores, {IssueListOverview} from 'sentry/views/issueList/overview';
 
@@ -74,6 +75,7 @@ describe('IssueList', function () {
     // eslint-disable-next-line no-console
     jest.spyOn(console, 'error').mockImplementation(jest.fn());
 
+    localStorageWrapper.clear();
     MockApiClient.clearMockResponses();
 
     savedSearch = TestStubs.Search({
