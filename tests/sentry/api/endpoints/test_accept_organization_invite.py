d9fdd1f1bfe6e5f3e362eca0ef62d5490b4c37b2
Mike Ihbe <mike.ihbe@sentry.io>
2022-12-15 20:23:49 -0800

    undo changes to accept org invite endpoint

--- tests/sentry/api/endpoints/test_accept_organization_invite.py
+++ tests/sentry/api/endpoints/test_accept_organization_invite.py
@@ -15,10 +15,10 @@ from sentry.models import (
 )
 from sentry.testutils import TestCase
 from sentry.testutils.factories import Factories
-from sentry.testutils.silo import control_silo_test, exempt_from_silo_limits
+from sentry.testutils.silo import exempt_from_silo_limits, region_silo_test
 
 
-@control_silo_test()
+@region_silo_test
 class AcceptInviteTest(TestCase):
     def setUp(self):
         super().setUp()
