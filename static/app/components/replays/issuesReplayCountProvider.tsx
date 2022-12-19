df1d596f2b209117cfa4d66f628997da8acd5e9b
Ryan Albrecht <ryan.albrecht@sentry.io>
2022-12-19 13:14:31 -0500

    skip the GroupStore stuff

--- static/app/components/replays/issuesReplayCountProvider.tsx
+++ static/app/components/replays/issuesReplayCountProvider.tsx
@@ -2,8 +2,8 @@ import {ReactNode, useMemo} from 'react';
 
 import ReplayCountContext from 'sentry/components/replays/replayCountContext';
 import useReplaysCount from 'sentry/components/replays/useReplaysCount';
-import GroupStore from 'sentry/stores/groupStore';
-import type {Group} from 'sentry/types';
+// import GroupStore from 'sentry/stores/groupStore';
+// import type {Group} from 'sentry/types';
 import useOrganization from 'sentry/utils/useOrganization';
 import useProjects from 'sentry/utils/useProjects';
 
@@ -33,23 +33,25 @@ export default function IssuesReplayCountProvider({children, groupIds}: Props) {
 
   // Only ask for the groupIds where the project have sent one or more replays.
   // For projects that don't support replay the count will always be zero.
-  const [groups, projectIds] = useMemo(() => {
-    const pIds = new Set<string>();
-    const gIds = groupIds
-      .map(id => GroupStore.get(id) as Group)
-      .filter(Boolean)
-      .filter(group => {
-        const proj = projectsById.get(group.project?.id);
-        if (proj?.hasReplays) {
-          pIds.add(group.project.id);
-          return true;
-        }
-        return false;
-      });
-    return [gIds, Array.from(pIds)];
-  }, [projectsById, groupIds]);
+  // const [groups, projectIds] = useMemo(() => {
+  //   const pIds = new Set<string>();
+  //   const gIds = groupIds
+  //     .map(id => GroupStore.get(id) as Group)
+  //     .filter(Boolean)
+  //     .filter(group => {
+  //       const proj = projectsById.get(group.project?.id);
+  //       if (proj?.hasReplays) {
+  //         pIds.add(group.project.id);
+  //         return true;
+  //       }
+  //       return false;
+  //     });
+  //   return [gIds, Array.from(pIds)];
+  // }, [projectsById, groupIds]);
 
-  const replayGroupIds = useMemo(() => groups.map(group => group.id), [groups]);
+  // const replayGroupIds = useMemo(() => groups.map(group => group.id), [groups]);
+  const replayGroupIds = groupIds;
+  const projectIds = Object.keys(projectsById);
 
   const counts = useReplaysCount({
     groupIds: replayGroupIds,
