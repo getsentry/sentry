7cfa64b4031cb43db6e61f520b3a0767648658f5
JonasBa <jonas@badalic.com>
2022-12-20 11:08:11 -0500

    feat(profiling): initialize canvas and pass props

--- static/app/components/profiling/flamegraph/flamegraph.tsx
+++ static/app/components/profiling/flamegraph/flamegraph.tsx
@@ -97,6 +97,8 @@ function Flamegraph(props: FlamegraphProps): ReactElement {
   const [flamegraphMiniMapOverlayCanvasRef, setFlamegraphMiniMapOverlayCanvasRef] =
     useState<HTMLCanvasElement | null>(null);
 
+  const [spansCanvasRef, setSpansCanvasRef] = useState<HTMLCanvasElement | null>(null);
+
   const canvasPoolManager = useMemo(() => new CanvasPoolManager(), []);
   const scheduler = useMemo(() => new CanvasScheduler(), []);
 
@@ -146,6 +148,13 @@ function Flamegraph(props: FlamegraphProps): ReactElement {
     }
     return new FlamegraphCanvas(flamegraphMiniMapCanvasRef, vec2.fromValues(0, 0));
   }, [flamegraphMiniMapCanvasRef]);
+
+  const spansCanvas = useMemo(() => {
+    if (!spansCanvasRef) {
+      return null;
+    }
+    return new FlamegraphCanvas(spansCanvasRef, vec2.fromValues(0, 0));
+  }, [spansCanvasRef]);
 
   const flamegraphView = useMemoWithPrevious<FlamegraphView | null>(
     previousView => {
@@ -411,7 +420,18 @@ function Flamegraph(props: FlamegraphProps): ReactElement {
       </FlamegraphToolbar>
 
       <FlamegraphLayout
-        spans={spanChart ? <FlamegraphSpans spanChart={spanChart} /> : null}
+        spans={
+          spanChart ? (
+            <FlamegraphSpans
+              spanChart={spanChart}
+              spansCanvas={spansCanvas}
+              spansCanvasRef={spansCanvasRef}
+              setSpansCanvasRef={setSpansCanvasRef}
+              canvasPoolManager={canvasPoolManager}
+              flamegraphView={flamegraphView}
+            />
+          ) : null
+        }
         minimap={
           <FlamegraphZoomViewMinimap
             canvasPoolManager={canvasPoolManager}
@@ -428,10 +448,10 @@ function Flamegraph(props: FlamegraphProps): ReactElement {
           <ProfileDragDropImport onImport={props.onImport}>
             <FlamegraphWarnings flamegraph={flamegraph} />
             <FlamegraphZoomView
-              flamegraphRenderer={flamegraphRenderer}
               canvasBounds={canvasBounds}
               canvasPoolManager={canvasPoolManager}
               flamegraph={flamegraph}
+              flamegraphRenderer={flamegraphRenderer}
               flamegraphCanvas={flamegraphCanvas}
               flamegraphCanvasRef={flamegraphCanvasRef}
               flamegraphOverlayCanvasRef={flamegraphOverlayCanvasRef}
@@ -444,10 +464,10 @@ function Flamegraph(props: FlamegraphProps): ReactElement {
         flamegraphDrawer={
           <FlamegraphDrawer
             profileGroup={props.profiles}
-            flamegraph={flamegraph}
+            getFrameColor={getFrameColor}
             referenceNode={referenceNode}
             rootNodes={rootNodes}
-            getFrameColor={getFrameColor}
+            flamegraph={flamegraph}
             formatDuration={flamegraph ? flamegraph.formatter : noopFormatDuration}
             canvasPoolManager={canvasPoolManager}
             canvasScheduler={scheduler}
---
+++ static/app/utils/profiling/renderers/spansRenderer.tsx
@@ -0,0 +1,66 @@
+import {mat3} from 'gl-matrix';
+
+import {Rect} from 'sentry/utils/profiling/gl/utils';
+import {SpanChart, SpanChartNode} from 'sentry/utils/profiling/spanChart';
+
+// Convert color component from 0-1 to 0-255 range
+function colorComponentsToRgba(color: number[]): string {
+  return `rgba(${Math.floor(color[0] * 255)}, ${Math.floor(color[1] * 255)}, ${Math.floor(
+    color[2] * 255
+  )}, ${color[3] ?? 1})`;
+}
+
+export class SpanChartRenderer2D {
+  canvas: HTMLCanvasElement | null;
+  spanChart: SpanChart;
+
+  spans: ReadonlyArray<SpanChartNode> = [];
+
+  constructor(canvas: HTMLCanvasElement, spanChart: SpanChart) {
+    this.canvas = canvas;
+    this.spanChart = spanChart;
+
+    this.init();
+  }
+
+  init() {
+    this.spans = [...this.spanChart.spans];
+  }
+
+  draw(configViewToPhysicalSpace: mat3) {
+    if (!this.canvas) {
+      throw new Error('No canvas to draw on');
+    }
+
+    const context = this.canvas.getContext('2d');
+
+    if (!context) {
+      throw new Error('Could not get canvas 2d context');
+    }
+
+    context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    const border = window.devicePixelRatio;
+
+    for (let i = 0; i < this.spans.length; i++) {
+      const span = this.spans[i];
+
+      const rect = new Rect(
+        span.start,
+        span.depth,
+        span.end - span.start,
+        1
+      ).transformRect(configViewToPhysicalSpace);
+
+      const colors = [1, 0, 0, 1];
+      const color = colorComponentsToRgba(colors);
+
+      context.fillStyle = color;
+      context.fillRect(
+        rect.x + border,
+        rect.y + border,
+        rect.width - border,
+        rect.height - border
+      );
+    }
+  }
+}
