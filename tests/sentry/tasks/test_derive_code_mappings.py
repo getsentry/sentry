getsentry/sentry

commit 69581a02a995537f1f628ff100932812aef0d67a

fix(derived_code_mappings): Allow empty or "./" stack trace root (#42389)
Fixes WOR-2522

Co-authored-by: Armen Zambrano G <armenzg@sentry.io>
Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

src/sentry/integrations/utils/code_mapping.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Dict, List, NamedTuple, Union
+from typing import Dict, List, NamedTuple, Tuple, Union
 
 from sentry.models.integrations.organization_integration import OrganizationIntegration
 from sentry.models.integrations.repository_project_path_config import RepositoryProjectPathConfig
@@ -266,6 +266,24 @@ class CodeMappingTreesHelper:
             assert source_code_root.endswith("/")
         return source_code_root
 
+    def _normalized_stack_and_source_roots(
+        self, stacktrace_root: str, source_path: str
+    ) -> Tuple[str, str]:
+        # We have a one to one code mapping (e.g. "app/" & "app/")
+        if source_path == stacktrace_root:
+            stacktrace_root = ""
+            source_path = ""
+        # stacktrace_root starts with "./"
+        elif stacktrace_root.startswith("./"):
+            without = stacktrace_root.replace("./", "")
+            if source_path == without:
+                stacktrace_root = "./"
+                source_path = ""
+            elif source_path.rfind(f"/{without}"):
+                stacktrace_root = "./"
+                source_path = source_path.replace(f"/{without}", "/")
+        return (stacktrace_root, source_path)
+
     def _generate_code_mapping_from_tree(
         self,
         repo_tree: RepoTree,
@@ -276,21 +294,22 @@ class CodeMappingTreesHelper:
             for src_path in repo_tree.files
             if self._potential_match(src_path, frame_filename)
         ]
+        if len(matched_files) != 1:
+            return []
+
+        stacktrace_root, source_path = self._normalized_stack_and_source_roots(
+            f"{frame_filename.root}/",
+            self._get_code_mapping_source_path(matched_files[0], frame_filename),
+        )
         # It is too risky generating code mappings when there's more
         # than one file potentially matching
-        return (
-            [
-                CodeMapping(
-                    repo=repo_tree.repo,
-                    stacktrace_root=f"{frame_filename.root}/",  # sentry
-                    source_path=self._get_code_mapping_source_path(
-                        matched_files[0], frame_filename
-                    ),
-                )
-            ]
-            if len(matched_files) == 1
-            else []
-        )
+        return [
+            CodeMapping(
+                repo=repo_tree.repo,
+                stacktrace_root=stacktrace_root,  # sentry/
+                source_path=source_path,  # src/sentry/
+            )
+        ]
 
     def _matches_current_code_mappings(self, src_file: str, frame_filename: FrameFilename) -> bool:
         # In some cases, once we have derived a code mapping we can exclude files that start with

tests/sentry/tasks/test_derive_code_mappings.py
@@ -41,12 +41,25 @@ class TestJavascriptDeriveCodeMappings(BaseDeriveCodeMappings):
                     "filename": "./app/utils/handleXhrErrorResponse.tsx",
                     "in_app": True,
                 },
+                {
+                    "filename": "some/path/Test.tsx",
+                    "in_app": True,
+                },
             ]
+        )
+        self.integration = self.create_integration(
+            organization=self.organization,
+            provider="github",
+            external_id=self.organization.id,
+            metadata={"domain_name": "github.com/Test-Org"},
         )
 
     def test_find_stacktrace_paths_single_project(self):
         stacktrace_paths = identify_stacktrace_paths(self.event_data)
-        assert stacktrace_paths == ["./app/utils/handleXhrErrorResponse.tsx"]
+        assert set(stacktrace_paths) == {
+            "./app/utils/handleXhrErrorResponse.tsx",
+            "some/path/Test.tsx",
+        }
 
     def test_find_stacktrace_paths_bad_data(self):
         data = self.generate_data([{}])
@@ -62,14 +75,8 @@ class TestJavascriptDeriveCodeMappings(BaseDeriveCodeMappings):
 
     @responses.activate
     @with_feature("organizations:derive-code-mappings")
-    def test_derive_code_mappings(self):
+    def test_derive_code_mappings_starts_with_period_slash(self):
         repo_name = "foo/bar"
-        self.create_integration(
-            organization=self.organization,
-            provider="github",
-            external_id=self.organization.id,
-            metadata={"domain_name": "github.com/Test-Org"},
-        )
         with patch(
             "sentry.integrations.github.client.GitHubClientMixin.get_trees_for_org"
         ) as mock_get_trees_for_org:
@@ -80,9 +87,45 @@ class TestJavascriptDeriveCodeMappings(BaseDeriveCodeMappings):
             }
             derive_code_mappings(self.project.id, self.event_data)
             code_mapping = RepositoryProjectPathConfig.objects.all()[0]
-            # ./app/foo.tsx -> foo.tsx -> static/app/foo.tsx
-            assert code_mapping.stack_root == "./app/"
-            assert code_mapping.source_root == "static/app/"
+            # ./app/foo.tsx -> app/foo.tsx -> static/app/foo.tsx
+            assert code_mapping.stack_root == "./"
+            assert code_mapping.source_root == "static/"
+            assert code_mapping.repository.name == repo_name
+
+    @responses.activate
+    @with_feature("organizations:derive-code-mappings")
+    def test_derive_code_mappings_starts_with_period_slash_no_containing_directory(self):
+        repo_name = "foo/bar"
+        with patch(
+            "sentry.integrations.github.client.GitHubClientMixin.get_trees_for_org"
+        ) as mock_get_trees_for_org:
+            mock_get_trees_for_org.return_value = {
+                repo_name: RepoTree(
+                    Repo(repo_name, "master"), ["app/utils/handleXhrErrorResponse.tsx"]
+                )
+            }
+            derive_code_mappings(self.project.id, self.event_data)
+            code_mapping = RepositoryProjectPathConfig.objects.all()[0]
+            # ./app/foo.tsx -> app/foo.tsx -> app/foo.tsx
+            assert code_mapping.stack_root == "./"
+            assert code_mapping.source_root == ""
+            assert code_mapping.repository.name == repo_name
+
+    @responses.activate
+    @with_feature("organizations:derive-code-mappings")
+    def test_derive_code_mappings_one_to_one_match(self):
+        repo_name = "foo/bar"
+        with patch(
+            "sentry.integrations.github.client.GitHubClientMixin.get_trees_for_org"
+        ) as mock_get_trees_for_org:
+            mock_get_trees_for_org.return_value = {
+                repo_name: RepoTree(Repo(repo_name, "master"), ["some/path/Test.tsx"])
+            }
+            derive_code_mappings(self.project.id, self.event_data)
+            code_mapping = RepositoryProjectPathConfig.objects.all()[0]
+            # some/path/Test.tsx -> Test.tsx -> some/path/Test.tsx
+            assert code_mapping.stack_root == ""
+            assert code_mapping.source_root == ""
             assert code_mapping.repository.name == repo_name
 
 

tests/sentry/integrations/utils/test_code_mapping.py
@@ -262,3 +262,40 @@ class TestDerivedCodeMappings(TestCase):
             },
         ]
         assert matches == expected_matches
+
+    def test_normalized_stack_and_source_roots_starts_with_period_slash(self):
+        stacktrace_root, source_path = self.code_mapping_helper._normalized_stack_and_source_roots(
+            "./app/", "static/app/"
+        )
+        assert stacktrace_root == "./"
+        assert source_path == "static/"
+
+    def test_normalized_stack_and_source_roots_starts_with_period_slash_no_containing_directory(
+        self,
+    ):
+        stacktrace_root, source_path = self.code_mapping_helper._normalized_stack_and_source_roots(
+            "./app/", "app/"
+        )
+        assert stacktrace_root == "./"
+        assert source_path == ""
+
+    def test_normalized_stack_and_source_not_matching(self):
+        stacktrace_root, source_path = self.code_mapping_helper._normalized_stack_and_source_roots(
+            "sentry/", "src/sentry/"
+        )
+        assert stacktrace_root == "sentry/"
+        assert source_path == "src/sentry/"
+
+    def test_normalized_stack_and_source_roots_equal(self):
+        stacktrace_root, source_path = self.code_mapping_helper._normalized_stack_and_source_roots(
+            "source/", "source/"
+        )
+        assert stacktrace_root == ""
+        assert source_path == ""
+
+    def test_normalized_stack_and_source_roots_starts_with_period_slash_two_levels(self):
+        stacktrace_root, source_path = self.code_mapping_helper._normalized_stack_and_source_roots(
+            "./app/", "app/foo/app/"
+        )
+        assert stacktrace_root == "./"
+        assert source_path == "app/foo/"

commit 4b5e582960c18010581e09c99d8d0ce67bf3e9cf

fix(derive_code_mappings): Do not allow empty stack root for packaged languages
This is a minor regression from https://github.com/getsentry/sentry/pull/42389

This was creating code mappings with an empty stack root for Python projects.

Unfortunately, all frames on Python issues are considered as in_app even if they're third party libraries. This would hide our ability to properly track how accurate the Python code mappings are.

For now, we will not allow empty stack roots for packaged languages.

src/sentry/integrations/utils/code_mapping.py
@@ -297,10 +297,12 @@ class CodeMappingTreesHelper:
         if len(matched_files) != 1:
             return []
 
-        stacktrace_root, source_path = self._normalized_stack_and_source_roots(
-            f"{frame_filename.root}/",
-            self._get_code_mapping_source_path(matched_files[0], frame_filename),
-        )
+        stacktrace_root = f"{frame_filename.root}/"
+        source_path = self._get_code_mapping_source_path(matched_files[0], frame_filename)
+        if frame_filename.frame_type() != "packaged":
+            stacktrace_root, source_path = self._normalized_stack_and_source_roots(
+                stacktrace_root, source_path
+            )
         # It is too risky generating code mappings when there's more
         # than one file potentially matching
         return [

tests/sentry/tasks/test_derive_code_mappings.py
@@ -337,3 +337,48 @@ class TestPythonDeriveCodeMappings(BaseDeriveCodeMappings):
 
         # We should not create the code mapping for dry runs
         assert not RepositoryProjectPathConfig.objects.filter(project_id=self.project.id).exists()
+
+    @responses.activate
+    @with_feature("organizations:derive-code-mappings")
+    def test_derive_code_mappings_stack_and_source_root_do_not_match(self):
+        self.create_integration(
+            organization=self.organization,
+            provider="github",
+            external_id=self.organization.id,
+            metadata={"domain_name": "github.com/Test-Org"},
+        )
+        repo_name = "foo/bar"
+        with patch(
+            "sentry.integrations.github.client.GitHubClientMixin.get_trees_for_org"
+        ) as mock_get_trees_for_org:
+            mock_get_trees_for_org.return_value = {
+                repo_name: RepoTree(Repo(repo_name, "master"), ["src/sentry/models/release.py"])
+            }
+            derive_code_mappings(self.project.id, self.test_data)
+            code_mapping = RepositoryProjectPathConfig.objects.all().first()
+            # sentry/models/release.py -> models/release.py -> src/sentry/models/release.py
+            assert code_mapping.stack_root == "sentry/"
+            assert code_mapping.source_root == "src/sentry/"
+
+    @responses.activate
+    @with_feature("organizations:derive-code-mappings")
+    def test_derive_code_mappings_no_normalization(self):
+        self.create_integration(
+            organization=self.organization,
+            provider="github",
+            external_id=self.organization.id,
+            metadata={"domain_name": "github.com/Test-Org"},
+        )
+        repo_name = "foo/bar"
+        with patch(
+            "sentry.integrations.github.client.GitHubClientMixin.get_trees_for_org"
+        ) as mock_get_trees_for_org:
+            mock_get_trees_for_org.return_value = {
+                repo_name: RepoTree(Repo(repo_name, "master"), ["sentry/models/release.py"])
+            }
+            derive_code_mappings(self.project.id, self.test_data)
+            code_mapping = RepositoryProjectPathConfig.objects.all().first()
+            # sentry/models/release.py -> models/release.py -> sentry/models/release.py
+            # If the normalization code was used these would be the empty stack root
+            assert code_mapping.stack_root == "sentry/"
+            assert code_mapping.source_root == "sentry/"