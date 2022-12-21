512182085b504698b94b16a630f53565d688dca3
JonasBa <jonas@badalic.com>
2022-12-21 09:11:44 -0500

    feat(profiling): initialize root to transaction

--- static/app/utils/profiling/spanChart.spec.tsx
+++ static/app/utils/profiling/spanChart.spec.tsx
@@ -1,4 +1,4 @@
-import {EntrySpans} from 'sentry/types/event';
+import {EntrySpans, EventOrGroupType, EventTransaction} from 'sentry/types/event';
 import {SpanChart} from 'sentry/utils/profiling/spanChart';
 import {SpanTree} from 'sentry/utils/profiling/spanTree';
 
@@ -20,9 +20,38 @@ function s(partial: Partial<EntrySpans['data'][0]>): EntrySpans['data'][0] {
   };
 }
 
+function txn(partial: Partial<EventTransaction>): EventTransaction {
+  return {
+    id: '',
+    projectID: '',
+    user: {},
+    contexts: {},
+    entries: [],
+    errors: [],
+    dateCreated: '',
+    startTimestamp: Date.now(),
+    endTimestamp: Date.now() + 1000,
+    title: '',
+    type: EventOrGroupType.TRANSACTION,
+    culprit: '',
+    dist: null,
+    eventID: '',
+    fingerprints: [],
+    dateReceived: new Date().toISOString(),
+    message: '',
+    metadata: {},
+    size: 0,
+    tags: [],
+    occurrence: null,
+    location: '',
+    crashFile: null,
+    ...partial,
+  };
+}
+
 describe('spanChart', () => {
   it('iterates over all spans with depth', () => {
-    const tree = new SpanTree([
+    const tree = new SpanTree(txn({startTimestamp: 0, endTimestamp: 1}), [
       s({span_id: '1', timestamp: 1, start_timestamp: 0}),
       s({span_id: '2', timestamp: 0.5, start_timestamp: 0}),
       s({span_id: '3', timestamp: 0.2, start_timestamp: 0}),
@@ -47,7 +76,7 @@ describe('spanChart', () => {
   });
 
   it('sets configView to duration of the spans', () => {
-    const tree = new SpanTree([
+    const tree = new SpanTree(txn({startTimestamp: 0, endTimestamp: 1}), [
       s({span_id: '1', timestamp: 1, start_timestamp: 0}),
       s({span_id: '2', timestamp: 0.5, start_timestamp: 0}),
       s({span_id: '3', timestamp: 0.2, start_timestamp: 0}),
@@ -61,7 +90,7 @@ describe('spanChart', () => {
   });
 
   it('tracks chart depth', () => {
-    const tree = new SpanTree([
+    const tree = new SpanTree(txn({startTimestamp: 0, endTimestamp: 1}), [
       s({span_id: '1', timestamp: 1, start_timestamp: 0}),
       s({span_id: '2', timestamp: 0.5, start_timestamp: 0}),
       s({span_id: '3', timestamp: 0.2, start_timestamp: 0}),
