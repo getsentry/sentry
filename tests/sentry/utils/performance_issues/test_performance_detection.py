d82846b420a42b84753fccec870a46018d794c8e
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2022-12-21 11:27:36 -0500

    (refactor) file io main thread detector to assert on detector output (#42496)
    Part of PERF-1874
    
    Refactor file io on main thread detector to assert on detector output vs
    tags
    Move tests to its own file

--- tests/sentry/utils/performance_issues/test_performance_detection.py
+++ tests/sentry/utils/performance_issues/test_performance_detection.py
@@ -295,35 +295,6 @@ class PerformanceDetectionTest(unittest.TestCase):
         _detect_performance_problems(truncated_duplicates_event, Mock())
         incr_mock.assert_has_calls([call("performance.performance_issue.truncated_np1_db")])
 
-    def test_detects_file_io_main_thread(self):
-        file_io_event = EVENTS["file-io-on-main-thread"]
-        sdk_span_mock = Mock()
-
-        _detect_performance_problems(file_io_event, sdk_span_mock)
-
-        assert sdk_span_mock.containing_transaction.set_tag.call_count == 5
-        sdk_span_mock.containing_transaction.set_tag.assert_has_calls(
-            [
-                call("_pi_all_issue_count", 1),
-                call("_pi_sdk_name", "sentry.java.android.timber"),
-                call("_pi_transaction", "c119e45a9d724b1891df4651ebf9e6db"),
-                call(
-                    "_pi_file_io_main_thread_fp",
-                    "1-GroupType.PERFORMANCE_FILE_IO_MAIN_THREAD-153198dd61706844cf3d9a922f6f82543df8125f",
-                ),
-                call("_pi_file_io_main_thread", "054ba3a374d543eb"),
-            ]
-        )
-
-    def test_does_not_detect_file_io_main_thread(self):
-        file_io_event = EVENTS["file-io-on-main-thread"]
-        file_io_event["spans"][0]["data"]["blocked_main_thread"] = False
-        sdk_span_mock = Mock()
-
-        _detect_performance_problems(file_io_event, sdk_span_mock)
-
-        assert sdk_span_mock.containing_transaction.set_tag.call_count == 0
-
 
 class PrepareProblemForGroupingTest(unittest.TestCase):
     def test(self):
