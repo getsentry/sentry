0348463bd8b5d1685fa5653330a83b2c437fa11b
Scott Cooper <scttcper@gmail.com>
2022-12-20 16:52:39 -0800

    feat(alerts): New default alerts set fallthrough choice (#42476)

--- src/sentry/receivers/rules.py
+++ src/sentry/receivers/rules.py
@@ -1,23 +1,39 @@
+from sentry import features
 from sentry.models import Rule
+from sentry.notifications.types import FallthroughChoiceType
 from sentry.signals import project_created
 
 DEFAULT_RULE_LABEL = "Send a notification for new issues"
+DEFAULT_RULE_ACTIONS = [
+    {
+        "id": "sentry.mail.actions.NotifyEmailAction",
+        "targetType": "IssueOwners",
+        "targetIdentifier": None,
+    }
+]
+FALLTHROUGH_RULE_ACTIONS = [
+    {
+        "id": "sentry.mail.actions.NotifyEmailAction",
+        "targetType": "IssueOwners",
+        "targetIdentifier": None,
+        "fallthroughType": FallthroughChoiceType.ACTIVE_MEMBERS.value,
+    }
+]
 DEFAULT_RULE_DATA = {
     "match": "all",
     "conditions": [{"id": "sentry.rules.conditions.first_seen_event.FirstSeenEventCondition"}],
-    "actions": [
-        {
-            "id": "sentry.mail.actions.NotifyEmailAction",
-            "targetType": "IssueOwners",
-            "targetIdentifier": None,
-        }
-    ],
+    "actions": DEFAULT_RULE_ACTIONS,
 }
 
 
 def create_default_rules(project, default_rules=True, RuleModel=Rule, **kwargs):
-    if default_rules:
-        RuleModel.objects.create(project=project, label=DEFAULT_RULE_LABEL, data=DEFAULT_RULE_DATA)
+    if not default_rules:
+        return
+
+    rule_data = DEFAULT_RULE_DATA
+    if features.has("organizations:issue-alert-fallback-targeting", project.organization):
+        rule_data = {**rule_data, "actions": FALLTHROUGH_RULE_ACTIONS}
+    RuleModel.objects.create(project=project, label=DEFAULT_RULE_LABEL, data=rule_data)
 
 
 project_created.connect(create_default_rules, dispatch_uid="create_default_rules", weak=False)
