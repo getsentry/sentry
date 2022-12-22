f805425f554ff6097f2cb2bb04a624493bc459c1
Mike Ihbe <mike.ihbe@sentry.io>
2022-12-22 00:19:19 -0500

    Annotate a bunch more tests

--- tests/sentry/api/endpoints/test_group_participants.py
+++ tests/sentry/api/endpoints/test_group_participants.py
@@ -3,7 +3,7 @@ from sentry.testutils import APITestCase
 from sentry.testutils.silo import region_silo_test
 
 
-@region_silo_test
+@region_silo_test(stable=True)
 class GroupParticipantsTest(APITestCase):
     endpoint = "sentry-api-0-group-stats"
 
