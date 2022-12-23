17644550024d6a2eb01356ee48ec0d3ef95c043d
Mike Ihbe <mikejihbe@gmail.com>
2022-12-23 17:34:18 +0100

    ref(hybrid-cloud): Additional test annotations: auth_index (#42425)
    Extends the hybrid cloud auth service to be usable in many more places (
    TY @corps)
    Annotate 30+ more api endpoint tests
    Co-authored-by: Mike Ihbe <mike.ihbe@sentry.io>
    Co-authored-by: Zachary Collins <zachary.collins@sentry.io>
    Co-authored-by: Zach Collins <recursive.cookie.jar@gmail.com>

--- src/sentry/api/endpoints/group_details.py
+++ src/sentry/api/endpoints/group_details.py
@@ -27,6 +27,7 @@ from sentry.models.groupinbox import get_inbox_details
 from sentry.models.groupowner import get_owner_details
 from sentry.plugins.base import plugins
 from sentry.plugins.bases import IssueTrackingPlugin2
+from sentry.services.hybrid_cloud.user import user_service
 from sentry.types.issues import GroupCategory
 from sentry.types.ratelimit import RateLimit, RateLimitCategory
 from sentry.utils import metrics
@@ -211,8 +212,9 @@ class GroupDetailsEndpoint(GroupEndpoint, EnvironmentMixin):
                 {
                     "activity": serialize(activity, request.user),
                     "seenBy": seen_by,
-                    "participants": serialize(
-                        GroupSubscriptionManager.get_participating_users(group), request.user
+                    "participants": user_service.serialize_users(
+                        user_ids=GroupSubscriptionManager.get_participating_user_ids(group),
+                        as_user=request.user,
                     ),
                     "pluginActions": action_list,
                     "pluginIssues": self._get_available_issue_plugins(request, group),
