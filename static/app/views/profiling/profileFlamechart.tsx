c353c64b9c5f8f7c55eb0b96b8f76cb22646050a
JonasBa <jonas@badalic.com>
2022-12-27 10:12:37 -0500

    fix(spans): convert to ns from unix timestamps

--- static/app/views/profiling/profileFlamechart.tsx
+++ static/app/views/profiling/profileFlamechart.tsx
@@ -74,9 +74,7 @@ function ProfileFlamegraph(): React.ReactElement {
   const profiledTransaction = useProfileTransaction();
 
   const hasFlameChartSpans = useMemo(() => {
-    return (
-      organization.features.includes('organizations:profiling-flamechart-spans') || true
-    );
+    return organization.features.includes('organizations:profiling-flamechart-spans');
   }, [organization.features]);
 
   const spanTree: SpanTree = useMemo(() => {
