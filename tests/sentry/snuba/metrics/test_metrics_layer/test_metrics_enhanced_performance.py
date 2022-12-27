75a62397007397d75b1c355041ce501f525ab85d
Oliver Newland <oliver.newland@sentry.io>
2022-12-27 14:06:32 -0800

    maybe a fix?

--- tests/sentry/snuba/metrics/test_metrics_layer/test_metrics_enhanced_performance.py
+++ tests/sentry/snuba/metrics/test_metrics_layer/test_metrics_enhanced_performance.py
@@ -38,12 +38,15 @@ from sentry.testutils.helpers.datetime import before_now
 
 pytestmark = pytest.mark.sentry_metrics
 
+ONE_DAY_AGO = timezone.now() - timedelta(days=1)
+MOCK_DATETIME = ONE_DAY_AGO.replace(hour=10, minute=0)
 
-@freeze_time("2022-09-29 10:00:00")
+
+@freeze_time(MOCK_DATETIME)
 class PerformanceMetricsLayerTestCase(BaseMetricsLayerTestCase, TestCase):
     @property
     def now(self):
-        return timezone.now()
+        return MOCK_DATETIME
 
     def test_valid_filter_include_meta_derived_metrics(self):
         query_params = MultiValueDict(
@@ -1187,15 +1190,18 @@ class PerformanceMetricsLayerTestCase(BaseMetricsLayerTestCase, TestCase):
             use_case_id=UseCaseKey.PERFORMANCE,
         )
         assert data == {
-            "start": FakeDatetime(2022, 9, 28, 10, 30),
-            "end": FakeDatetime(2022, 9, 28, 16, 30),
+            "start": FakeDatetime(day_ago.year, day_ago.month, day_ago.day, 10, 30),
+            "end": FakeDatetime(day_ago.year, day_ago.month, day_ago.day, 16, 30),
             "intervals": [
-                FakeDatetime(2022, 9, 28, 10, 0, tzinfo=timezone.utc),
-                FakeDatetime(2022, 9, 28, 11, 0, tzinfo=timezone.utc),
-                FakeDatetime(2022, 9, 28, 12, 0, tzinfo=timezone.utc),
-                FakeDatetime(2022, 9, 28, 13, 0, tzinfo=timezone.utc),
-                FakeDatetime(2022, 9, 28, 14, 0, tzinfo=timezone.utc),
-                FakeDatetime(2022, 9, 28, 15, 0, tzinfo=timezone.utc),
+                FakeDatetime(
+                    day_ago.year,
+                    day_ago.month,
+                    day_ago.day,
+                    hour,
+                    0,
+                    tzinfo=timezone.utc,
+                )
+                for hour in range(10, 16)
             ],
             "groups": [
                 {
@@ -1432,7 +1438,7 @@ class PerformanceMetricsLayerTestCase(BaseMetricsLayerTestCase, TestCase):
                 name=TransactionMRI.DURATION.value,
                 tags={"transaction": transaction},
                 value=value,
-                minutes_before_now=minutes,
+                minutes_before_now=-minutes,
             )
 
         metrics_query = self.build_metrics_query(
