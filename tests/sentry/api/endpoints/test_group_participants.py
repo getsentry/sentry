1c10115bb1df2e0ce305ed5eb4cd89c45be1da15
Cathy Teng <70817427+cathyteng17@users.noreply.github.com>
2023-01-03 11:56:26 -0800

    ref(hybrid-cloud): add organization_slug to GroupParticipants (#42403)

--- tests/sentry/api/endpoints/test_group_participants.py
+++ tests/sentry/api/endpoints/test_group_participants.py
@@ -1,15 +1,23 @@
+from django.urls import reverse
+
 from sentry.models import GroupSubscription
 from sentry.testutils import APITestCase
 from sentry.testutils.silo import region_silo_test
 
 
-@region_silo_test
+@region_silo_test(stable=True)
 class GroupParticipantsTest(APITestCase):
-    endpoint = "sentry-api-0-group-stats"
-
     def setUp(self):
         super().setUp()
         self.login_as(self.user)
+
+    def _get_path_functions(self):
+        return (
+            lambda group: reverse("sentry-api-0-group-stats", args=[group.id]),
+            lambda group: reverse(
+                "sentry-api-0-group-stats-with-org", args=[self.organization.slug, group.id]
+            ),
+        )
 
     def test_simple(self):
         group = self.create_group()
@@ -17,6 +25,10 @@ class GroupParticipantsTest(APITestCase):
             user=self.user, group=group, project=group.project, is_active=True
         )
 
-        response = self.get_success_response(group.id)
-        assert len(response.data) == 1
-        assert response.data[0]["id"] == str(self.user.id)
+        for path_func in self._get_path_functions():
+            path = path_func(group)
+
+            response = self.client.get(path)
+
+            assert len(response.data) == 1
+            assert response.data[0]["id"] == str(self.user.id)
