8e4cbae8e007a6538d8e0fff286b748ec79fa216
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2023-01-04 16:40:17 -0500

    test to ensure sessions is called properly

--- static/app/views/projectDetail/projectCharts.spec.tsx
+++ static/app/views/projectDetail/projectCharts.spec.tsx
@@ -1,14 +1,22 @@
 import {initializeOrg} from 'sentry-test/initializeOrg';
-import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
+import {render, screen, userEvent, waitFor} from 'sentry-test/reactTestingLibrary';
 
 import ProjectCharts from 'sentry/views/projectDetail/projectCharts';
 
-function renderProjectCharts(features?: string[], platforms?: string[]) {
+function renderProjectCharts(
+  features?: string[],
+  platforms?: string[],
+  chartDisplay?: string
+) {
   const {organization, router, project} = initializeOrg({
     organization: TestStubs.Organization({features}),
     projects: [{platforms}],
     router: {
-      params: {orgId: 'org-slug', groupId: 'group-id'},
+      params: {orgId: 'org-slug', projectId: 'project-slug'},
+      location: {
+        pathname: '/organizations/org-slug/projects/project-slug/',
+        query: {chart1: chartDisplay ? chartDisplay : 'crash_free'},
+      },
     },
   } as Parameters<typeof initializeOrg>[0]);
 
@@ -28,13 +36,14 @@ function renderProjectCharts(features?: string[], platforms?: string[]) {
 }
 
 describe('ProjectDetail > ProjectCharts', () => {
+  let mockSessions;
   beforeEach(() => {
     MockApiClient.addMockResponse({
       url: `/organizations/org-slug/releases/stats/`,
       body: [],
     });
 
-    MockApiClient.addMockResponse({
+    mockSessions = MockApiClient.addMockResponse({
       method: 'GET',
       url: '/organizations/org-slug/sessions/',
       body: TestStubs.SessionsField({
@@ -63,5 +72,58 @@ describe('ProjectDetail > ProjectCharts', () => {
 
     expect(screen.queryByText('Foreground ANR Rate')).not.toBeInTheDocument();
     expect(screen.queryByText('ANR Rate')).not.toBeInTheDocument();
+  });
+
+  it('makes the right ANR sessions request', async () => {
+    const responseBody = {
+      query: '',
+      intervals: [
+        '2021-03-05T00:00:00Z',
+        '2021-03-06T00:00:00Z',
+        '2021-03-07T00:00:00Z',
+        '2021-03-08T00:00:00Z',
+        '2021-03-09T00:00:00Z',
+        '2021-03-10T00:00:00Z',
+        '2021-03-11T00:00:00Z',
+        '2021-03-12T00:00:00Z',
+        '2021-03-13T00:00:00Z',
+        '2021-03-14T00:00:00Z',
+        '2021-03-15T00:00:00Z',
+        '2021-03-16T00:00:00Z',
+        '2021-03-17T00:00:00Z',
+        '2021-03-18T00:00:00Z',
+      ],
+      groups: [
+        {
+          by: {},
+          totals: {
+            'anr_rate()': 492,
+            'count_unique(user)': 3,
+          },
+          series: {
+            'anr_rate()': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 490],
+            'count_unique(user)': [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2, 1],
+          },
+        },
+      ],
+    };
+    mockSessions = MockApiClient.addMockResponse({
+      method: 'GET',
+      url: '/organizations/org-slug/sessions/',
+      body: responseBody,
+    });
+    renderProjectCharts(['anr-rate'], ['python', 'android'], 'anr_rate');
+    expect(screen.getByText('ANR Rate')).toBeInTheDocument();
+
+    await waitFor(() =>
+      expect(mockSessions).toHaveBeenCalledWith(
+        '/organizations/org-slug/sessions/',
+        expect.objectContaining({
+          query: expect.objectContaining({
+            field: ['anr_rate()', 'count_unique(user)'],
+          }),
+        })
+      )
+    );
   });
 });
