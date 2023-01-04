cfb74dfca80b5f1586d3115f22217478df76e04d
Nar Saynorath <nar.saynorath@sentry.io>
2023-01-04 12:23:23 -0500

    Fix tests

--- static/app/components/events/viewHierarchy/tree.spec.tsx
+++ static/app/components/events/viewHierarchy/tree.spec.tsx
@@ -2,15 +2,19 @@ import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import {ViewHierarchyTree} from './tree';
 
+const DEFAULT_VALUES = {alpha: 1, height: 1, width: 1, x: 1, y: 1, visible: true};
 const MOCK_DATA = {
+  ...DEFAULT_VALUES,
   id: 'parent',
   type: 'Container',
   children: [
     {
+      ...DEFAULT_VALUES,
       id: 'intermediate',
       type: 'Nested Container',
       children: [
         {
+          ...DEFAULT_VALUES,
           id: 'leaf',
           type: 'Text',
           children: [],
