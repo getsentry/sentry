395032030fa0941e28aec161913bb26a19d963c5
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-01-05 12:28:47 -0800

    C

--- src/sentry/issues/constants.py
+++ src/sentry/issues/constants.py
@@ -4,8 +4,10 @@ from sentry.types.issues import GroupCategory
 ISSUE_TSDB_GROUP_MODELS = {
     GroupCategory.ERROR: tsdb.models.group,
     GroupCategory.PERFORMANCE: tsdb.models.group_performance,
+    GroupCategory.PROFILE: tsdb.models.group,
 }
 ISSUE_TSDB_USER_GROUP_MODELS = {
     GroupCategory.ERROR: tsdb.models.users_affected_by_group,
     GroupCategory.PERFORMANCE: tsdb.models.users_affected_by_perf_group,
+    GroupCategory.PROFILE: tsdb.models.users_affected_by_group,
 }
