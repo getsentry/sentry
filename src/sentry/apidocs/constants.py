87915fa41a51c69a563c50b10cb329e8d0f837e6
David Cramer <dcramer@gmail.com>
2023-01-05 13:33:19 -0800

    feat: Add OpenAPI docs for monitor endpoints

--- src/sentry/apidocs/constants.py
+++ src/sentry/apidocs/constants.py
@@ -2,10 +2,21 @@ from drf_spectacular.utils import OpenApiResponse
 
 RESPONSE_UNAUTHORIZED = OpenApiResponse(description="Unauthorized")
 
+# 400
+RESPONSE_BAD_REQUEST = OpenApiResponse(description="Bad Request")
+
 RESPONSE_FORBIDDEN = OpenApiResponse(description="Forbidden")
 
 RESPONSE_NOTFOUND = OpenApiResponse(description="Not Found")
 
+# 200
 RESPONSE_SUCCESS = OpenApiResponse(description="Success")
 
+# 201 - Created
 RESPONSE_NO_CONTENT = OpenApiResponse(description="No Content")
+
+# 202 - Accepted (not yet acted on fully)
+RESPONSE_ACCEPTED = OpenApiResponse(description="Accepted")
+
+# 208
+RESPONSE_ALREADY_REPORTED = OpenApiResponse(description="Already Reported")
