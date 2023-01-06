ed81f3e5d8979f1c01c37d08a4416d2dcb28a215
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-01-06 15:56:47 -0800

    ref(replays): Refactor useVirtualizedGrid() to encapsulate some more resize logic

--- static/app/views/replays/detail/useVirtualizedGrid.tsx
+++ static/app/views/replays/detail/useVirtualizedGrid.tsx
@@ -1,37 +1,87 @@
-import {DependencyList, RefObject, useEffect, useMemo} from 'react';
+import {
+  DependencyList,
+  RefObject,
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
 import {CellMeasurerCache, CellMeasurerCacheParams, MultiGrid} from 'react-virtualized';
 
 type Opts = {
+  /**
+   * Options for the CellMeasurerCache constructor
+   */
   cellMeasurer: CellMeasurerCacheParams;
+  /**
+   * How many columns are being rendered
+   * */
+  columnCount: number;
+  /**
+   * List of other values that should trigger re-computing column sizes
+   * */
   deps: DependencyList;
-  ref: RefObject<MultiGrid>;
-  wrapperRef: RefObject<HTMLDivElement>;
+  /**
+   * There must be one column with a dynamic width, so the table can fill all available width inside the container
+   */
+  dyanmicColumnIndex: number;
+  /**
+   * The <MultiGrid> elem.
+   */
+  gridRef: RefObject<MultiGrid>;
 };
-function useVirtualizedGrid({cellMeasurer, deps, ref, wrapperRef}: Opts) {
+function useVirtualizedGrid({
+  cellMeasurer,
+  columnCount,
+  deps,
+  dyanmicColumnIndex,
+  gridRef,
+}: Opts) {
   const cache = useMemo(() => new CellMeasurerCache(cellMeasurer), [cellMeasurer]);
+  const [scrollBarWidth, setScrollBarWidth] = useState(0);
 
-  // Clear cache when items changes
+  // Recompute the width of the dynamic column when deps change (ie: a search/filter is applied)
   useEffect(() => {
     cache.clearAll();
-    ref.current?.recomputeGridSize({columnIndex: 1});
-  }, [cache, ref, deps]);
+    gridRef.current?.recomputeGridSize({columnIndex: dyanmicColumnIndex});
+  }, [cache, gridRef, deps, dyanmicColumnIndex]);
 
-  // Clear cache when wrapper div is resized
-  useEffect(() => {
-    if (!wrapperRef.current) {
-      return () => {};
-    }
+  const onWrapperResize = useCallback(() => {
+    // TODO: debounce?
+    gridRef.current?.recomputeGridSize({columnIndex: dyanmicColumnIndex});
+  }, [gridRef, dyanmicColumnIndex]);
 
-    const observer = new ResizeObserver(() => {
-      ref.current?.recomputeGridSize({columnIndex: 1});
-    });
-    observer.observe(wrapperRef.current);
+  const onScrollbarPresenceChange = useCallback(({vertical, size}) => {
+    setScrollBarWidth(vertical ? size : 0);
+  }, []);
 
-    return () => observer.disconnect();
-  }, [ref, wrapperRef, deps]);
+  const getColumnWidth = useCallback(
+    (width: number) =>
+      ({index}) => {
+        if (index === dyanmicColumnIndex) {
+          const colWidth = Math.max(
+            Array.from(new Array(columnCount)).reduce(
+              (remaining, _, i) =>
+                i === dyanmicColumnIndex
+                  ? remaining
+                  : remaining - cache.columnWidth({index: i}),
+              width - scrollBarWidth
+            ),
+            200
+          );
+          return colWidth;
+        }
+
+        return cache.columnWidth({index});
+      },
+    [cache, columnCount, dyanmicColumnIndex, scrollBarWidth]
+  );
 
   return {
     cache,
+    getColumnWidth,
+    onScrollbarPresenceChange,
+    onWrapperResize,
   };
 }
 
