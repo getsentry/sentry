1862f420037c3cbeb8f6ebbe6a0a38ebd9cadec3
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-01-06 15:09:47 -0500

    Add test coverage for N+1 API calls table

--- static/app/components/events/interfaces/performance/spanEvidenceKeyValueList.spec.tsx
+++ static/app/components/events/interfaces/performance/spanEvidenceKeyValueList.spec.tsx
@@ -59,4 +59,56 @@ describe('SpanEvidenceKeyValueList', () => {
       ).not.toBeInTheDocument();
     });
   });
+
+  describe('N+1 API Calls', () => {
+    it('Renders relevant fields', () => {
+      render(
+        <SpanEvidenceKeyValueList
+          issueType={IssueType.PERFORMANCE_N_PLUS_ONE_API_CALLS}
+          transactionName="/"
+          parentSpan={
+            new MockSpan({
+              startTimestamp: 0,
+              endTimestamp: 200,
+              op: 'pageload',
+            }).span
+          }
+          offendingSpans={[
+            new MockSpan({
+              startTimestamp: 10,
+              endTimestamp: 2100,
+              op: 'http.client',
+              description: 'GET http://service.api/book/?book_id=7',
+            }).span,
+            new MockSpan({
+              startTimestamp: 10,
+              endTimestamp: 2100,
+              op: 'http.client',
+              description: 'GET http://service.api/book/?book_id=8',
+            }).span,
+          ]}
+        />
+      );
+
+      expect(screen.getByRole('cell', {name: 'Transaction'})).toBeInTheDocument();
+      expect(
+        screen.getByTestId('span-evidence-key-value-list.transaction-name')
+      ).toHaveTextContent('/');
+
+      expect(screen.getByRole('cell', {name: 'Parent Span'})).toBeInTheDocument();
+      expect(
+        screen.getByTestId('span-evidence-key-value-list.parent-name')
+      ).toHaveTextContent('pageload');
+
+      expect(screen.getByRole('cell', {name: 'Offending Span'})).toBeInTheDocument();
+      expect(
+        screen.getByTestId('span-evidence-key-value-list.offending-spans')
+      ).toHaveTextContent('GET http://service.api/book/?book_id=7');
+
+      expect(screen.queryByRole('cell', {name: 'Problem Parameter'})).toBeInTheDocument();
+      expect(
+        screen.getByTestId('span-evidence-key-value-list.problem-parameters')
+      ).toHaveTextContent('[ "book_id=7", "book_id=8" ]');
+    });
+  });
 });
