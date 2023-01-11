4ed18fc61851575ee26e52796bf2da22395239a8
Snigdha Sharma <snigdhasharma@sentry.io>
2023-01-11 13:43:30 -0800

    Add new response fields for StacktraceLinkResult

--- static/app/types/integrations.tsx
+++ static/app/types/integrations.tsx
@@ -144,8 +144,27 @@ export type SentryAppSchemaStacktraceLink = {
   params?: Array<string>;
 };
 
+export enum Coverage {
+  NOT_COVERED = 0,
+  COVERED = 1,
+  PARTIAL = 2,
+}
+
+export enum CodecovStatusCode {
+  COVERAGE_EXISTS = 200,
+  NO_INTEGRATION = 404,
+  NO_COVERAGE_DATA = 400,
+}
+
+export type LineCoverage = {
+  coverage: Coverage;
+  lineNo: number;
+};
+
 export type StacktraceLinkResult = {
+  codecovStatusCode: CodecovStatusCode;
   integrations: Integration[];
+  lineCoverage: LineCoverage[];
   attemptedUrl?: string;
   config?: RepositoryProjectPathConfigWithIntegration;
   error?: StacktraceErrorMessage;
