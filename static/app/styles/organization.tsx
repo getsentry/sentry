7dc2ad1e535d8d0912a388e41ae3f6b11b7b3cd6
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-01-12 10:43:06 -0800

    ref(js): Remove basically unused org/styles/PageHeader (#43156)

--- static/app/styles/organization.tsx
+++ static/app/styles/organization.tsx
@@ -10,14 +10,6 @@ export const PageContent = styled('main')`
   padding: ${space(3)} ${space(4)};
 `;
 
-export const PageHeader = styled('header')`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: ${space(2)};
-  min-height: 32px;
-`;
-
 export const HeaderTitle = styled('h4')`
   ${p => p.theme.text.pageTitle};
   color: ${p => p.theme.headingColor};
