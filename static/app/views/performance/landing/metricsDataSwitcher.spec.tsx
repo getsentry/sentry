b98b233587d4b74d25c138562cf89a2bedf29b15
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-01-12 11:47:31 -0500

    fix(perf): missing transactions in landing page due to default filter (#43029)
    Fixes #42692

--- static/app/views/performance/landing/metricsDataSwitcher.spec.tsx
+++ static/app/views/performance/landing/metricsDataSwitcher.spec.tsx
@@ -150,23 +150,6 @@ describe('Performance > Landing > MetricsDataSwitcher', function () {
     expect(await screen.findByTestId('transaction-search-bar')).toBeInTheDocument();
   });
 
-  it('renders with feature flag and no metrics data', async function () {
-    addMetricsDataMock({
-      metricsCount: 0,
-      nullCount: 0,
-      unparamCount: 0,
-    });
-    const project = TestStubs.Project();
-    const data = initializeData({
-      project: project.id,
-      projects: [project],
-      features,
-    });
-
-    wrapper = render(<WrappedComponent data={data} />, data.routerContext);
-    expect(await screen.findByTestId('smart-search-bar')).toBeInTheDocument();
-  });
-
   it('renders with feature flag and checking dynamic sampled projects exist', async function () {
     addMetricsDataMock({
       metricsCount: 100,
@@ -199,7 +182,7 @@ describe('Performance > Landing > MetricsDataSwitcher', function () {
     });
 
     wrapper = render(<WrappedComponent data={data} />, data.routerContext);
-    expect(await screen.findByTestId('smart-search-bar')).toBeInTheDocument();
+    expect(await screen.findByTestId('transaction-search-bar')).toBeInTheDocument();
     expect(
       await screen.findByTestId('landing-mep-alert-single-project-incompatible')
     ).toBeInTheDocument();
@@ -221,7 +204,7 @@ describe('Performance > Landing > MetricsDataSwitcher', function () {
     });
 
     wrapper = render(<WrappedComponent data={data} />, data.routerContext);
-    expect(await screen.findByTestId('smart-search-bar')).toBeInTheDocument();
+    expect(await screen.findByTestId('transaction-search-bar')).toBeInTheDocument();
     expect(
       await screen.findByTestId('landing-mep-alert-multi-project-incompatible')
     ).toBeInTheDocument();
@@ -243,7 +226,7 @@ describe('Performance > Landing > MetricsDataSwitcher', function () {
     });
 
     wrapper = render(<WrappedComponent data={data} />, data.routerContext);
-    expect(await screen.findByTestId('smart-search-bar')).toBeInTheDocument();
+    expect(await screen.findByTestId('transaction-search-bar')).toBeInTheDocument();
     expect(
       await screen.findByTestId('landing-mep-alert-multi-project-all-incompatible')
     ).toBeInTheDocument();
@@ -263,7 +246,7 @@ describe('Performance > Landing > MetricsDataSwitcher', function () {
     });
 
     wrapper = render(<WrappedComponent data={data} />, data.routerContext);
-    expect(await screen.findByTestId('smart-search-bar')).toBeInTheDocument();
+    expect(await screen.findByTestId('transaction-search-bar')).toBeInTheDocument();
     expect(
       await screen.findByTestId('landing-mep-alert-unnamed-discover')
     ).toBeInTheDocument();
