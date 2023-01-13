7fa3638806080395b3557fb01aeea4e90bd2ab35
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-01-13 14:24:34 -0800

    fix(integrations): Use organization_id for finding the integration (#43279)
    Fixes https://sentry.io/organizations/sentry/issues/3873673216/ -
    SENTRY-XTS
    
    The error came up because the organization_id wasn't being used to find
    the integration. The get_org_context method would find the first
    integration, and attempt to connect it to the organization, which would
    fail.

--- tests/sentry/hybrid_cloud/test_integration.py
+++ tests/sentry/hybrid_cloud/test_integration.py
@@ -238,6 +238,18 @@ class OrganizationIntegrationServiceTest(BaseIntegrationServiceTest):
         )
         assert result is None
 
+    def test_get_organization_context(self):
+        new_org = self.create_organization()
+        with exempt_from_silo_limits():
+            org_integration = self.integration3.add_organization(new_org)
+
+        result_integration, result_org_integration = integration_service.get_organization_context(
+            organization_id=new_org.id,
+            provider="example",
+        )
+        self.verify_integration_result(result=result_integration, expected=self.integration3)
+        self.verify_org_integration_result(result=result_org_integration, expected=org_integration)
+
     @freeze_time()
     def test_update_organization_integrations(self):
         now = datetime.now(timezone.utc)
