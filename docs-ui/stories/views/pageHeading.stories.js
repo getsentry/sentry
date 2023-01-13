baa30fcdcc66b8fcc1e3807cc6cc0d3bdcad59cd
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-01-13 07:45:38 -0800

    ref(ui): Remove now unused PageHeading (#43227)

--- docs-ui/stories/views/pageHeading.stories.js
+++
@@ -1,9 +0,0 @@
-import PageHeading from 'sentry/components/pageHeading';
-
-export default {
-  title: 'Views/Page Heading',
-};
-
-export const Default = () => <PageHeading withMargins>Page Header</PageHeading>;
-
-Default.storyName = 'Page Heading';
