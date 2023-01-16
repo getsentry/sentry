afe413d2413eeb5e9564d4ed45f0ee6972382c9f
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-01-16 13:09:16 -0500

    fix

--- src/sentry/utils/performance_issues/performance_detection.py
+++ src/sentry/utils/performance_issues/performance_detection.py
@@ -957,8 +957,7 @@ class ConsecutiveDBSpanDetector(PerformanceDetector):
 
     def _fingerprint(self) -> str:
         hashed_spans = fingerprint_spans(self.consecutive_db_spans)
-        problem_class = "performance-consecutive-db-queries"  # Hardcoded intentionally as it should never change
-        return f"1-{problem_class}-{hashed_spans}"
+        return f"1-{GroupType.PERFORMANCE_CONSECUTIVE_DB_OP.value}-{hashed_spans}"
 
     def on_complete(self) -> None:
         self._validate_and_store_performance_problem()
