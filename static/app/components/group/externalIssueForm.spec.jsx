bacccefac83614b34403aa0c93229077ce58f618
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-01-17 14:26:45 -0800

    feat(ui): Add plainTextLabel for select items (#43288)

--- static/app/components/group/externalIssueForm.spec.jsx
+++ static/app/components/group/externalIssueForm.spec.jsx
@@ -182,12 +182,12 @@ describe('ExternalIssueForm', () => {
         renderComponent('Link');
         jest.useFakeTimers();
         userEvent.click(screen.getAllByText('Issue').at(1));
-        userEvent.type(screen.getByRole('textbox', {name: 'Issue'}), 'doOT');
+        userEvent.type(screen.getByRole('textbox', {name: 'Issue'}), 'faster');
         expect(window.fetch).toHaveBeenCalledTimes(0);
         jest.advanceTimersByTime(300);
         expect(window.fetch).toHaveBeenCalledTimes(1);
         expect(window.fetch).toHaveBeenCalledWith(
-          '/search?field=externalIssue&query=doOT&repo=scefali%2Ftest'
+          '/search?field=externalIssue&query=faster&repo=scefali%2Ftest'
         );
         expect(await screen.findByText('#2345 perf: Make it faster')).toBeInTheDocument();
       });
