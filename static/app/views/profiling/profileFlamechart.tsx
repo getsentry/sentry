8dec1397181a506802d59dab870891353f1a8ef8
Jonas <jonas.badalic@sentry.io>
2023-01-17 09:49:40 -0500

    feat(uiframes): create a class to hold UI frames (#43192)
    Creates a class that hold a reference to all the frames that we need to
    render on the timeline

--- static/app/views/profiling/profileFlamechart.tsx
+++ static/app/views/profiling/profileFlamechart.tsx
@@ -8,8 +8,6 @@ import {Flamegraph} from 'sentry/components/profiling/flamegraph/flamegraph';
 import {ProfileDragDropImportProps} from 'sentry/components/profiling/flamegraph/flamegraphOverlays/profileDragDropImport';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {t} from 'sentry/locale';
-import {EntryType} from 'sentry/types';
-import {EntrySpans, EventTransaction} from 'sentry/types/event';
 import {DeepPartial} from 'sentry/types/utils';
 import trackAdvancedAnalyticsEvent from 'sentry/utils/analytics/trackAdvancedAnalyticsEvent';
 import {
@@ -26,71 +24,25 @@ import {
 import {FlamegraphThemeProvider} from 'sentry/utils/profiling/flamegraph/flamegraphThemeProvider';
 import {ProfileGroup} from 'sentry/utils/profiling/profile/importProfile';
 import {Profile} from 'sentry/utils/profiling/profile/profile';
-import {SpanTree} from 'sentry/utils/profiling/spanTree';
 import {useLocalStorageState} from 'sentry/utils/useLocalStorageState';
 import useOrganization from 'sentry/utils/useOrganization';
 
-import {
-  useProfileGroup,
-  useProfileTransaction,
-  useSetProfileGroup,
-} from './profileGroupProvider';
-
-function collectAllSpanEntriesFromTransaction(
-  transaction: EventTransaction
-): EntrySpans['data'] {
-  if (!transaction.entries.length) {
-    return [];
-  }
-
-  const spans = transaction.entries.filter(
-    (e): e is EntrySpans => e.type === EntryType.SPANS
-  );
-
-  let allSpans: EntrySpans['data'] = [];
-
-  for (const span of spans) {
-    allSpans = allSpans.concat(span.data);
-  }
-
-  return allSpans;
-}
+import {useProfileGroup, useSetProfileGroup} from './profileGroupProvider';
 
 const LoadingGroup: ProfileGroup = {
   name: 'Loading',
   activeProfileIndex: 0,
   transactionID: null,
   metadata: {},
+  measurements: {},
   traceID: '',
   profiles: [Profile.Empty],
 };
-
-const LoadingSpanTree = SpanTree.Empty;
 
 function ProfileFlamegraph(): React.ReactElement {
   const organization = useOrganization();
   const profileGroup = useProfileGroup();
   const setProfileGroup = useSetProfileGroup();
-  const profiledTransaction = useProfileTransaction();
-
-  const hasFlameChartSpans = useMemo(() => {
-    return organization.features.includes('profiling-flamechart-spans');
-  }, [organization.features]);
-
-  const hasUIFrames = useMemo(() => {
-    return organization.features.includes('profiling-ui-frames');
-  }, [organization.features]);
-
-  const spanTree: SpanTree = useMemo(() => {
-    if (profiledTransaction.type === 'resolved' && profiledTransaction.data) {
-      return new SpanTree(
-        profiledTransaction.data,
-        collectAllSpanEntriesFromTransaction(profiledTransaction.data)
-      );
-    }
-
-    return LoadingSpanTree;
-  }, [profiledTransaction]);
 
   const [storedPreferences] = useLocalStorageState<DeepPartial<FlamegraphState>>(
     FLAMEGRAPH_LOCALSTORAGE_PREFERENCES_KEY,
@@ -155,23 +107,13 @@ function ProfileFlamegraph(): React.ReactElement {
               </Alert>
             ) : profileGroup.type === 'loading' ? (
               <Fragment>
-                <Flamegraph
-                  onImport={onImport}
-                  profiles={LoadingGroup}
-                  spanTree={hasFlameChartSpans ? LoadingSpanTree : null}
-                  uiFrames={null}
-                />
+                <Flamegraph onImport={onImport} profiles={LoadingGroup} />
                 <LoadingIndicatorContainer>
                   <LoadingIndicator />
                 </LoadingIndicatorContainer>
               </Fragment>
             ) : profileGroup.type === 'resolved' ? (
-              <Flamegraph
-                onImport={onImport}
-                profiles={profileGroup.data}
-                spanTree={hasFlameChartSpans ? spanTree : null}
-                uiFrames={hasUIFrames ? null : null}
-              />
+              <Flamegraph onImport={onImport} profiles={profileGroup.data} />
             ) : null}
           </FlamegraphContainer>
         </FlamegraphThemeProvider>
