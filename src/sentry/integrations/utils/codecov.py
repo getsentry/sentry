9eda056550480bb8497225b45813317066380315
Jodi Jang <jodi.jang@sentry.io>
2023-01-18 15:18:17 -0800

    ref: Fix capitalization

--- src/sentry/integrations/utils/codecov.py
+++ src/sentry/integrations/utils/codecov.py
@@ -8,14 +8,14 @@ CODECOV_URL = "https://api.codecov.io/api/v2/{service}/{owner_username}/repos/{r
 
 
 def get_codecov_line_coverage(repo: str, service: str, branch: str, path: str) -> Any:
-    CODECOV_TOKEN = options.get("codecov.client-secret")
+    codecov_token = options.get("codecov.client-secret")
     owner_username, repo_name = repo.split("/")
     if service == "github":
         service = "gh"
     url = CODECOV_URL.format(service=service, owner_username=owner_username, repo_name=repo_name)
     params = {"branch": branch, "path": path}
     response = requests.get(
-        url, params=params, headers={"Authorization": f"tokenAuth {CODECOV_TOKEN}"}
+        url, params=params, headers={"Authorization": f"tokenAuth {codecov_token}"}
     )
     response.raise_for_status()
 
