5b227381bcbdf4aa9b6ccbb9519f2c13d4324542
Malachi Willey <malwilley@gmail.com>
2023-01-18 11:51:53 -0600

    fix(ui): Prevent all events table from overflowing (#43210)
    Fixes https://github.com/getsentry/sentry/issues/42570

--- static/app/views/organizationGroupDetails/groupEvents.tsx
+++ static/app/views/organizationGroupDetails/groupEvents.tsx
@@ -109,16 +109,14 @@ class GroupEvents extends Component<Props, State> {
     return (
       <Layout.Body>
         <Layout.Main fullWidth>
-          <Wrapper>
-            {this.renderSearchBar()}
-            <AllEventsTable
-              issueId={this.props.group.id}
-              location={this.props.location}
-              organization={this.props.organization}
-              group={this.props.group}
-              excludedTags={excludedTags}
-            />
-          </Wrapper>
+          <AllEventsFilters>{this.renderSearchBar()}</AllEventsFilters>
+          <AllEventsTable
+            issueId={this.props.group.id}
+            location={this.props.location}
+            organization={this.props.organization}
+            group={this.props.group}
+            excludedTags={excludedTags}
+          />
         </Layout.Main>
       </Layout.Body>
     );
@@ -131,9 +129,8 @@ const FilterSection = styled('div')`
   grid-template-columns: max-content 1fr;
 `;
 
-const Wrapper = styled('div')`
-  display: grid;
-  gap: ${space(2)};
+const AllEventsFilters = styled('div')`
+  margin-bottom: ${space(2)};
 `;
 
 export {GroupEvents};
