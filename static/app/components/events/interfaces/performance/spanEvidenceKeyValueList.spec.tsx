d0eaf9f20f7b98dc10f5a63a8643cfa9ac781dd1
Ash Anand <ash.anand@sentry.io>
2023-01-19 14:36:57 -0500

    fix test

--- static/app/components/events/interfaces/performance/spanEvidenceKeyValueList.spec.tsx
+++ static/app/components/events/interfaces/performance/spanEvidenceKeyValueList.spec.tsx
@@ -250,9 +250,9 @@ describe('SpanEvidenceKeyValueList', () => {
         screen.getByTestId('span-evidence-key-value-list.transaction')
       ).toHaveTextContent('/');
 
-      expect(screen.getByRole('cell', {name: 'Slow Span'})).toBeInTheDocument();
+      expect(screen.getByRole('cell', {name: 'Slow DB Query'})).toBeInTheDocument();
       expect(
-        screen.getByTestId('span-evidence-key-value-list.slow-span')
+        screen.getByTestId('span-evidence-key-value-list.slow-db-query')
       ).toHaveTextContent('SELECT pokemon FROM pokedex');
     });
   });
