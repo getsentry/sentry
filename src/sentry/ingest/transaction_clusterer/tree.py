88d859d265a4ddf7ba5e7c597b44d24b0fbef723
Joris Bayer <joris.bayer@sentry.io>
2023-01-20 09:55:16 +0100

    Revert "fix(txnames): Escape * in transaction name rules" (#43497)
    Reverts getsentry/sentry#43426
    
    The concern of the PR was legitimate, but we overlooked the fact that as
    of https://github.com/getsentry/sentry/pull/43332, we feed the output of
    the normalization mechanism (i.e. transactions names with source
    `sanitized`) back into the clusterer as input, with the intention of
    creating more specific rules.
    
    For example, a first run
    
    ```
    /orgs/
        /foo/
        /bar/
        /baz/
    ```
    
    might result in a rule `/orgs/*/**` which would be immediately enforced.
    A second run
    
    ```
    /orgs/
        /*/
            /p1/
            /p2/
            /p3/   
    ```
    
    should then result in the more specific rule `/orgs/*/*/**`. But with
    the current change it would become `/orgs/\*/*/**`, which would never
    match anything.

--- src/sentry/ingest/transaction_clusterer/tree.py
+++ src/sentry/ingest/transaction_clusterer/tree.py
@@ -117,9 +117,7 @@ class TreeClusterer(Clusterer):
 
     @staticmethod
     def _build_rule(path: List["Edge"]) -> ReplacementRule:
-        path_str = SEP.join(
-            ["*" if isinstance(key, Merged) else key.replace("*", r"\*") for key in path]
-        )
+        path_str = SEP.join(["*" if isinstance(key, Merged) else key for key in path])
         path_str += "/**"
         return ReplacementRule(path_str)
 
