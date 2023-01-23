580460555ffcecbdac732ca4e95775170eb5fb6b
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-01-23 10:31:06 +0100

    Improve comment

--- src/sentry/snuba/metrics/query_builder.py
+++ src/sentry/snuba/metrics/query_builder.py
@@ -403,7 +403,10 @@ def get_metric_object_from_metric_field(
 
 class AliasMetaType(Enum):
     """
-    Enum representing the meta type of an alias returned from a SNQL query.
+    Enum representing the type of an alias returned from a SNQL query.
+
+    This type does not refer to the result type but rather the type of element to which the alias refers in the query
+    (e.g., MetricField, MetricGroupByField, special columns, or simple tag).
     """
 
     TAG = 0
