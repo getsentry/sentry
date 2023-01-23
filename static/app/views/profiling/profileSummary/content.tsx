ef8f2064fa7cd055837df03a21a6f2c3f03d715d
Elias Hussary <elias.hussary@sentry.io>
2023-01-23 10:18:07 -0500

    feat(profiling): add profiles by percentile chart to profile summary (#43341)
    ## Summary
    Adds `<ProfileCharts/>` to the Profile Summary page; more specifically
    we show "Profiles by Percentile".
    
    
    ![image](https://user-images.githubusercontent.com/7349258/212970349-c6aad337-7f3a-47f6-97c2-71d91d81cf81.png)

--- static/app/views/profiling/profileSummary/content.tsx
+++ static/app/views/profiling/profileSummary/content.tsx
@@ -18,6 +18,7 @@ import {
   useProfileEvents,
 } from 'sentry/utils/profiling/hooks/useProfileEvents';
 import {decodeScalar} from 'sentry/utils/queryString';
+import {ProfileCharts} from 'sentry/views/profiling/landing/profileCharts';
 
 const FUNCTIONS_CURSOR_NAME = 'functionsCursor';
 
@@ -101,6 +102,7 @@ function ProfileSummaryContent(props: ProfileSummaryContentProps) {
 
   return (
     <Layout.Main fullWidth>
+      <ProfileCharts query={props.query} hideCount />
       <TableHeader>
         <CompactSelect
           triggerProps={{prefix: t('Filter'), size: 'xs'}}
