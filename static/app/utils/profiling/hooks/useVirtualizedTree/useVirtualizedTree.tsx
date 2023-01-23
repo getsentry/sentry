e453d4fcc5bebc00b0ba614470900e15fbd6800a
Nar Saynorath <nar.saynorath@sentry.io>
2023-01-23 13:39:05 -0500

    feat(view-hierarchy): Add hover and select in virtualized tree (#43409)
    Reuse keyboard actions and hover/clicked states
    from useVirtualizedTree. Also selects the first item
    by default.

--- static/app/utils/profiling/hooks/useVirtualizedTree/useVirtualizedTree.tsx
+++ static/app/utils/profiling/hooks/useVirtualizedTree/useVirtualizedTree.tsx
@@ -41,6 +41,7 @@ export interface UseVirtualizedListProps<T extends TreeLike> {
   scrollContainer: HTMLElement | null;
   tree: T[];
   expanded?: boolean;
+  initialSelectedNodeIndex?: number;
   overscroll?: number;
   skipFunction?: (node: VirtualizedTreeNode<T>) => boolean;
   sortFunction?: (a: VirtualizedTreeNode<T>, b: VirtualizedTreeNode<T>) => number;
@@ -55,7 +56,7 @@ export function useVirtualizedTree<T extends TreeLike>(
 
   const [state, dispatch] = useReducer(VirtualizedTreeReducer, {
     roots: props.tree,
-    selectedNodeIndex: null,
+    selectedNodeIndex: props.initialSelectedNodeIndex ?? null,
     scrollTop: 0,
     overscroll: props.overscroll ?? DEFAULT_OVERSCROLL_ITEMS,
     scrollHeight: props.scrollContainer?.getBoundingClientRect()?.height ?? 0,
