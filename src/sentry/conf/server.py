18378aa9a61bd2a20453f2e6e8c217271f1a6267
Dan Fuller <dfuller@sentry.io>
2023-01-23 13:27:51 -0800

    chore(issue-platform): Add feature flag for ingesting profile main thread blocked issues (#43533)
    This adds a feature flag to control whether we ingest profile main
    thread blocked issues

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1016,6 +1016,8 @@ SENTRY_FEATURES = {
     "organizations:profiling-ui-frames": False,
     # Enable the profiling dashboard redesign
     "organizations:profiling-dashboard-redesign": False,
+    # Whether to enable ingest for profile blocked main thread issues
+    "organizations:profile-blocked-main-thread-ingest": False,
     # Enable multi project selection
     "organizations:global-views": False,
     # Enable experimental new version of Merged Issues where sub-hashes are shown
