023066cebfb7ff1034a2c03b7d438c470534d07b
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2023-01-25 06:19:05 +0900

    feat(tags): Use new tag facets in Discover results page and Transactions page (#43549)
    Updates the discover results page to use the new tag facets component in
    the results page and Transaction page.
    
    Also includes some improvements to tag facets:
    - display n/a instead of blank whitespace when the tag value is null
    - debounce hover on bar and legend components
    - ensure only 4 segments maximum are visible to reduce visual clutter
    - cleaned up some unused props/components

--- static/app/views/discover/tags.tsx
+++ static/app/views/discover/tags.tsx
@@ -8,8 +8,8 @@ import {Client} from 'sentry/api';
 import ErrorPanel from 'sentry/components/charts/errorPanel';
 import {SectionHeading} from 'sentry/components/charts/styles';
 import EmptyStateWarning from 'sentry/components/emptyStateWarning';
+import TagFacetsDistributionMeter from 'sentry/components/group/tagFacets/tagFacetsDistributionMeter';
 import Placeholder from 'sentry/components/placeholder';
-import TagDistributionMeter from 'sentry/components/tagDistributionMeter';
 import {IconWarning} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import space from 'sentry/styles/space';
@@ -93,7 +93,7 @@ class Tags extends Component<Props, State> {
     trackAdvancedAnalyticsEvent('discover_v2.facet_map.clicked', {organization, tag});
   };
 
-  renderTag(tag: Tag) {
+  renderTag(tag: Tag, index: number) {
     const {generateUrl, totalValues} = this.props;
 
     const segments: TagSegment[] = tag.topValues.map(segment => {
@@ -108,14 +108,12 @@ class Tags extends Component<Props, State> {
         ? Math.max(Number(totalValues), segments[0].count)
         : totalValues;
     return (
-      <TagDistributionMeter
+      <TagFacetsDistributionMeter
         key={tag.key}
         title={tag.key}
         segments={segments}
         totalValues={Number(maxTotalValues)}
-        renderLoading={() => <StyledPlaceholder height="16px" />}
-        onTagClick={this.handleTagClick}
-        showReleasePackage
+        expandByDefault={index === 0}
       />
     );
   }
@@ -147,7 +145,7 @@ class Tags extends Component<Props, State> {
     }
 
     if (tags.length > 0) {
-      return tags.map(tag => this.renderTag(tag));
+      return tags.map((tag, index) => this.renderTag(tag, index));
     }
 
     return <StyledEmptyStateWarning small>{t('No tags found')}</StyledEmptyStateWarning>;
