096158c7825f86606a7b02f1b1b93518d7059ff8
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2023-01-25 10:28:40 -0500

    fix tests

--- tests/sentry/utils/performance_issues/test_performance_detection.py
+++ tests/sentry/utils/performance_issues/test_performance_detection.py
@@ -79,20 +79,20 @@ class PerformanceDetectionTest(TestCase):
     @patch("sentry.utils.performance_issues.performance_detection._detect_performance_problems")
     def test_options_disabled(self, mock):
         event = {}
-        detect_performance_problems(event)
+        detect_performance_problems(event, self.project)
         assert mock.call_count == 0
 
     @patch("sentry.utils.performance_issues.performance_detection._detect_performance_problems")
     def test_options_enabled(self, mock):
         event = {}
         with override_options({"performance.issues.all.problem-detection": 1.0}):
-            detect_performance_problems(event)
+            detect_performance_problems(event, self.project)
         assert mock.call_count == 1
 
     @override_options(BASE_DETECTOR_OPTIONS)
     def test_no_feature_flag_disables_creation(self):
         self.features = []
-        n_plus_one_event = get_event["n-plus-one-in-django-index-view"]
+        n_plus_one_event = get_event("n-plus-one-in-django-index-view")
         sdk_span_mock = Mock()
 
         perf_problems = _detect_performance_problems(n_plus_one_event, sdk_span_mock)
