f8e05cf426ada8d58038ad9a65d22aaae23f632f
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-01-25 13:51:48 -0500

    ensure php is not allowed to detect issues

--- src/sentry/utils/performance_issues/performance_detection.py
+++ src/sentry/utils/performance_issues/performance_detection.py
@@ -1047,6 +1047,10 @@ class ConsecutiveDBSpanDetector(PerformanceDetector):
     def is_creation_allowed_for_project(self, project: Project) -> bool:
         return True  # Detection always allowed by project for now
 
+    def is_event_eligible(cls, event):
+        sdk_name = get_sdk_name(event) or ""
+        return "php" not in sdk_name.lower()
+
 
 class NPlusOneDBSpanDetector(PerformanceDetector):
     """
