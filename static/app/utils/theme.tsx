5086e76984c1b3a2b8a3bcf13f2efd85c389c213
Vu Luong <vuluongj20@gmail.com>
2023-01-30 12:53:02 -0800

    ref(theme): Update surface colors

--- static/app/utils/theme.tsx
+++ static/app/utils/theme.tsx
@@ -12,15 +12,17 @@ export const lightColors = {
   black: '#1D1127',
   white: '#FFFFFF',
 
-  surface100: '#FAF9FB',
-  surface200: '#FFFFFF',
+  surface100: '#F5F3F7',
+  surface200: '#FAF9FB',
   surface300: '#FFFFFF',
+  surface400: '#FFFFFF',
+
   /**
    * Hover color. Deprecated – use <InteractionStateLayer /> instead for interaction
    * (hover/press) states.
    * @deprecated
    */
-  surface400: '#F5F3F7',
+  surface500: '#F5F3F7',
 
   gray500: '#2B2233',
   gray400: '#3E3446',
@@ -74,15 +76,17 @@ export const darkColors = {
   black: '#1D1127',
   white: '#FFFFFF',
 
-  surface100: '#1A141F',
-  surface200: '#241D2A',
-  surface300: '#2C2433',
+  surface100: '#18121C',
+  surface200: '#1A141F',
+  surface300: '#241D2A',
+  surface400: '#2C2433',
+
   /**
    * Hover color. Deprecated – use <InteractionStateLayer /> instead for interaction
    * (hover/press) states.
    * @deprecated
    */
-  surface400: '#362E3E',
+  surface500: '#362E3E',
 
   gray500: '#EBE6EF',
   gray400: '#D6D0DC',
@@ -205,27 +209,27 @@ const generateAliases = (colors: BaseColors) => ({
   /**
    * Background for the main content area of a page?
    */
-  bodyBackground: colors.surface100,
+  bodyBackground: colors.surface200,
 
   /**
    * Primary background color
    */
-  background: colors.surface200,
+  background: colors.surface300,
 
   /**
    * Elevated background color
    */
-  backgroundElevated: colors.surface300,
+  backgroundElevated: colors.surface400,
 
   /**
    * Secondary background color used as a slight contrast against primary background
    */
-  backgroundSecondary: colors.surface100,
+  backgroundSecondary: colors.surface200,
 
   /**
    * Background for the header of a page
    */
-  headerBackground: colors.surface200,
+  headerBackground: colors.surface300,
 
   /**
    * Primary border color
@@ -275,7 +279,7 @@ const generateAliases = (colors: BaseColors) => ({
    * interaction (hover/press) states.
    * @deprecated
    */
-  hover: colors.surface400,
+  hover: colors.surface500,
 
   /**
    * Indicates that something is "active" or "selected"
@@ -317,7 +321,7 @@ const generateAliases = (colors: BaseColors) => ({
   /**
    *
    */
-  rowBackground: colors.surface300,
+  rowBackground: colors.surface400,
 
   /**
    * Color of lines that flow across the background of the chart to indicate axes levels
@@ -348,7 +352,7 @@ const generateAliases = (colors: BaseColors) => ({
   /**
    * Overlay for partial opacity
    */
-  overlayBackgroundAlpha: color(colors.surface100).alpha(0.7).string(),
+  overlayBackgroundAlpha: color(colors.surface200).alpha(0.7).string(),
 
   /**
    * Tag progress bars
@@ -487,7 +491,7 @@ const generateBadgeTheme = (colors: BaseColors) => ({
 
 const generateTagTheme = (colors: BaseColors) => ({
   default: {
-    background: colors.surface300,
+    background: colors.surface400,
     border: colors.gray200,
     iconColor: colors.gray300,
   },
