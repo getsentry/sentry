de6e8dbea122651a65c03fd56c352c5cfc6aaefd
Elias Hussary <elias.hussary@sentry.io>
2023-01-31 11:21:01 -0500

    feat(profiling): add Slowest Transactions panel on profiling landing (#43642)
    ## Summary
    
    Relates to: https://github.com/getsentry/team-profiling/issues/138
    
    Adds a `profilingSlowestTransactionsPanel` to the `profiling` landing
    page. There was some refactoring included in this PR moving components
    to a more logical space for us to work with it within profiling, namely
    `Flex`.
    
    
    ![image](https://user-images.githubusercontent.com/7349258/214942891-0ac320cd-9d6c-4767-a136-3b3c337600e0.png)
    
    
    https://user-images.githubusercontent.com/7349258/214942920-186fe1cd-765e-4ac5-ac8c-713fbb8e1816.mov

---
+++ static/app/components/profiling/functionsMiniGrid.tsx
@@ -0,0 +1,118 @@
+import {CSSProperties, Fragment} from 'react';
+import {Link} from 'react-router';
+import styled from '@emotion/styled';
+
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import PerformanceDuration from 'sentry/components/performanceDuration';
+import {Flex} from 'sentry/components/profiling/flex';
+import {t} from 'sentry/locale';
+import space from 'sentry/styles/space';
+import {Organization, Project} from 'sentry/types';
+import {SuspectFunction} from 'sentry/types/profiling/core';
+import {generateProfileFlamechartRouteWithHighlightFrame} from 'sentry/utils/profiling/routes';
+
+interface FunctionsMiniGridProps {
+  functions: SuspectFunction[] | null;
+  organization: Organization;
+  project: Project;
+}
+
+export function FunctionsMiniGrid(props: FunctionsMiniGridProps) {
+  const {organization, project, functions} = props;
+
+  const linkToFlamechartRoute = (
+    profileId: string,
+    frameName: string,
+    framePackage: string
+  ) => {
+    return generateProfileFlamechartRouteWithHighlightFrame({
+      orgSlug: organization.slug,
+      projectSlug: project.slug,
+      profileId,
+      frameName,
+      framePackage,
+    });
+  };
+  return (
+    <FunctionsMiniGridContainer>
+      <FunctionsMiniGridHeader>{t('Slowest app functions')}</FunctionsMiniGridHeader>
+      <FunctionsMiniGridHeader align="right">{t('P99')}</FunctionsMiniGridHeader>
+      <FunctionsMiniGridHeader align="right">{t('Count')}</FunctionsMiniGridHeader>
+
+      {functions &&
+        functions.map((f, idx) => {
+          const [exampleProfileIdRaw] = f.examples;
+          const exampleProfileId = exampleProfileIdRaw.replaceAll('-', '');
+          return (
+            <Fragment key={idx}>
+              <FunctionsMiniGridCell title={f.name}>
+                <FunctionNameTextTruncate>
+                  <Link to={linkToFlamechartRoute(exampleProfileId, f.name, f.package)}>
+                    {f.name}
+                  </Link>
+                </FunctionNameTextTruncate>
+              </FunctionsMiniGridCell>
+              <FunctionsMiniGridCell align="right">
+                <PerformanceDuration nanoseconds={f.p99} abbreviation />
+              </FunctionsMiniGridCell>
+              <FunctionsMiniGridCell align="right">
+                <NumberContainer>{f.count}</NumberContainer>
+              </FunctionsMiniGridCell>
+            </Fragment>
+          );
+        })}
+    </FunctionsMiniGridContainer>
+  );
+}
+
+export function FunctionsMiniGridLoading() {
+  return (
+    <Flex align="stretch" justify="center" column h="100%">
+      <Flex align="center" justify="center">
+        <LoadingIndicator mini />
+      </Flex>
+    </Flex>
+  );
+}
+
+export function FunctionsMiniGridEmptyState() {
+  return (
+    <Flex align="stretch" justify="center" column h="100%">
+      <Flex align="center" justify="center">
+        {t('No functions data')}
+      </Flex>
+    </Flex>
+  );
+}
+
+export const FunctionsMiniGridContainer = styled('div')`
+  display: grid;
+  grid-template-columns: 60% 20% 20%;
+`;
+
+export const FunctionsMiniGridHeader = styled('span')<{
+  align?: CSSProperties['textAlign'];
+}>`
+  text-transform: uppercase;
+  font-size: ${p => p.theme.fontSizeExtraSmall};
+  font-weight: 600;
+  color: ${p => p.theme.subText};
+  text-align: ${p => p.align};
+`;
+
+export const FunctionsMiniGridCell = styled('div')<{align?: CSSProperties['textAlign']}>`
+  font-size: ${p => p.theme.fontSizeSmall};
+  text-align: ${p => p.align};
+  padding: ${space(0.5)} 0px;
+`;
+
+const NumberContainer = styled(`div`)`
+  text-align: right;
+`;
+
+const FunctionNameTextTruncate = styled('div')`
+  min-width: 0;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+`;
--- static/app/views/profiling/content.tsx
+++ static/app/views/profiling/content.tsx
@@ -37,6 +37,7 @@ import usePageFilters from 'sentry/utils/usePageFilters';
 import useProjects from 'sentry/utils/useProjects';
 
 import {ProfileCharts} from './landing/profileCharts';
+import {ProfilingSlowestTransactionsPanel} from './landing/profilingSlowestTransactionsPanel';
 import {ProfilingOnboardingPanel} from './profilingOnboardingPanel';
 
 interface ProfilingContentProps {
@@ -116,6 +117,12 @@ function ProfilingContent({location}: ProfilingContentProps) {
     );
   }, [selection.projects, projects]);
 
+  const isNewProfilingDashboardEnabled = organization.features.includes(
+    'profiling-dashboard-redesign'
+  );
+
+  // isNewProfilingDashboardEnabled = false;
+
   return (
     <SentryDocumentTitle title={t('Profiling')} orgSlug={organization.slug}>
       <PageFiltersContainer>
@@ -191,7 +198,22 @@ function ProfilingContent({location}: ProfilingContentProps) {
                   </ProfilingOnboardingPanel>
                 ) : (
                   <Fragment>
-                    <ProfileCharts query={query} selection={selection} />
+                    {isNewProfilingDashboardEnabled ? (
+                      <PanelsGrid>
+                        <ProfilingSlowestTransactionsPanel />
+                        <ProfileCharts
+                          query={query}
+                          selection={selection}
+                          hideCount={isNewProfilingDashboardEnabled}
+                        />
+                      </PanelsGrid>
+                    ) : (
+                      <ProfileCharts
+                        query={query}
+                        selection={selection}
+                        hideCount={isNewProfilingDashboardEnabled}
+                      />
+                    )}
                     <ProfileEventsTable
                       columns={FIELDS.slice()}
                       data={
@@ -241,6 +263,17 @@ const ActionBar = styled('div')`
   gap: ${space(2)};
   grid-template-columns: min-content auto;
   margin-bottom: ${space(2)};
+`;
+
+// TODO: another simple primitive that can easily be <Grid columns={2} />
+const PanelsGrid = styled('div')`
+  display: grid;
+  grid-template-columns: 50% 50%;
+  gap: ${space(2)};
+
+  @media (max-width: ${p => p.theme.breakpoints.small}) {
+    grid-template-columns: 100%;
+  }
 `;
 
 export default ProfilingContent;
