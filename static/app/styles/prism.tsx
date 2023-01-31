25e98e3ca340f87fc6b16b91b02ebe42346c30bc
Vu Luong <vuluongj20@gmail.com>
2023-01-31 14:28:03 -0800

    ref(codeSnippet): Update UI & remove `<HTMLCode />` (#43802)
    (Part of https://github.com/getsentry/sentry/issues/43765)
    
    Update `<CodeSnippet />`'s UI (specifically the header & copy button) +
    remove `<HTMLCode />` since `<CodeSnippet />` does the same thing when
    `languge="html"`.
    
    **Before ——**
    <img width="858" alt="Screenshot 2023-01-27 at 11 24 10 AM"
    src="https://user-images.githubusercontent.com/44172267/215179227-9a03b7f0-d03e-4726-bed2-f71535070288.png">
    
    **After ——**
    <img width="858" alt="Screenshot 2023-01-27 at 10 25 31 AM"
    src="https://user-images.githubusercontent.com/44172267/215165240-06879f7b-deda-4626-a941-5a63f637f1fc.png">

--- static/app/styles/prism.tsx
+++ static/app/styles/prism.tsx
@@ -12,12 +12,13 @@ export const prismStyles = (theme: Theme) => css`
   }
 
   /* Use dark Prism theme for code snippets imported from Sentry Docs */
-  .gatsby-highlight {
+  .gatsby-highlight,
+  .prism-dark {
     ${theme.prismDarkVariables};
   }
 
   pre[class*='language-'] {
-    overflow-x: scroll;
+    overflow-x: auto;
     padding: ${space(1)} ${space(2)};
     border-radius: ${theme.borderRadius};
     box-shadow: none;
