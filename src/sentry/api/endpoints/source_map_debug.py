328f21492cf70664fda422585cd637d48e0efcc9
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2023-02-02 15:35:31 -0800

    add more data to response

--- src/sentry/api/endpoints/source_map_debug.py
+++ src/sentry/api/endpoints/source_map_debug.py
@@ -116,7 +116,12 @@ class SourceMapDebugEndpoint(ProjectEndpoint):
                 issue=SourceMapProcessingIssue.URL_NOT_VALID, data={"absPath": abs_path}
             )
 
-        release_artifacts = ReleaseFile.objects.filter(release_id=release.id)
+        release_artifacts = ReleaseFile.objects.filter(release_id=release.id).exclude(
+            artifact_count=0
+        )
+
+        artifact_names = [artifact.name for artifact in release_artifacts]
+
         full_matches, partial_matches = self._find_matches(
             release_artifacts, self._unify_url(urlparts)
         )
@@ -129,11 +134,18 @@ class SourceMapDebugEndpoint(ProjectEndpoint):
                         "absPath": abs_path,
                         "partialMatchPath": partial_matches[0].name,
                         "fileName": filename,
+                        "unifiedPath": self._unify_url(urlparts),
+                        "artifactNames": artifact_names,
                     },
                 )
             return self._create_response(
                 issue=SourceMapProcessingIssue.NO_URL_MATCH,
-                data={"absPath": abs_path, "fileName": filename},
+                data={
+                    "absPath": abs_path,
+                    "fileName": filename,
+                    "unifiedPath": self._unify_url(urlparts),
+                    "artifactNames": artifact_names,
+                },
             )
 
         artifact = full_matches[0]
