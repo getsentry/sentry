edfad9781c40eef832960ba64e43ccf949e0bc5e
Kamil Ogórek <kamil.ogorek@gmail.com>
2023-02-02 14:47:18 +0100

    feat(api): Allow for querying release files/archives with checksums (#44016)
    Last year, sentry-cli introduced a feature, that queries our Sentry
    releases in order to decide what files should be uploaded.
    Prior to this change, no matter what was the state of an archive,
    everything was sent straight away, whether it was already there or not.
    
    The problem, however, is that this endpoint is paginated, and there are
    cases, where people have thousands of files, which result in hundreds of
    API calls, draining their quota and sometimes resulting in 429s.
    
    The change works by allowing to query our `/files` endpoint for the
    presence of a predefined set of checksums. This way we can easily filter
    already uploaded files on the client-side, without the need to pull
    _all_ the files in the first place, and then do `sha1` filtering. It
    should be enough, as we rarely send thousands of files _at once_, and
    even if we do, usually those are duplicates, which we will drop thanks
    to this query.
    
    Yes, it will still be paginated, but will drastically reduce the number
    of requests for the majority of cases.
    It will also fix our symbolicator case for mapping JS files, as for this
    purpose, we only need a handful of files, which will definitely fit into
    a single request page.
    
    Note that `query` was always there, but not documented. (tbh I'd prefer
    to call it `name`, but didn't see a reason to break it)
    
    Replaces: https://github.com/getsentry/sentry/pull/43778

--- tests/sentry/api/endpoints/test_project_release_files.py
+++ tests/sentry/api/endpoints/test_project_release_files.py
@@ -141,7 +141,7 @@ class ReleaseFilesListTest(APITestCase):
         assert response.data[0]["name"] == "~/index.js"
         assert response.data[1]["name"] == "~/index.js.map"
 
-    def test_archive_search(self):
+    def test_archive_name_search(self):
         self.login_as(user=self.user)
         url = reverse(
             "sentry-api-0-project-release-files",
@@ -156,6 +156,68 @@ class ReleaseFilesListTest(APITestCase):
         assert response.status_code == 200, response.content
         assert len(response.data) == 1
         assert response.data[0]["name"] == "~/index.js.map"
+
+    def test_archive_checksum_search(self):
+        self.login_as(user=self.user)
+        url = reverse(
+            "sentry-api-0-project-release-files",
+            kwargs={
+                "organization_slug": self.project.organization.slug,
+                "project_slug": self.project.slug,
+                "version": self.release.version,
+            },
+        )
+        self.create_release_archive()
+
+        response = self.client.get(url + "?checksum=3004341003e829253143f75eac9367167ef8d5ea")
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 1
+        assert response.data[0]["name"] == "~/index.js"
+
+        response = self.client.get(url + "?checksum=4a1d80f5e1e09c9de78ca449b666e833193d84d7")
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 1
+        assert response.data[0]["name"] == "~/index.js.map"
+
+        response = self.client.get(
+            url
+            + "?checksum=3004341003e829253143f75eac9367167ef8d5ea&checksum=4a1d80f5e1e09c9de78ca449b666e833193d84d7"
+        )
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 2
+        assert response.data[0]["name"] == "~/index.js"
+        assert response.data[1]["name"] == "~/index.js.map"
+
+        response = self.client.get(url + "?checksum=0000111122223333444455556666777788889999")
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 0
+
+    def test_archive_queries_should_be_narrowing_search(self):
+        self.login_as(user=self.user)
+        url = reverse(
+            "sentry-api-0-project-release-files",
+            kwargs={
+                "organization_slug": self.project.organization.slug,
+                "project_slug": self.project.slug,
+                "version": self.release.version,
+            },
+        )
+        self.create_release_archive()
+
+        # Found `index.js` and `index.js.map` by name, but only `index.js` by checksum.
+        response = self.client.get(
+            url + "?query=index&checksum=3004341003e829253143f75eac9367167ef8d5ea"
+        )
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 1
+        assert response.data[0]["name"] == "~/index.js"
+
+        # Found `index.js` and `index.js.map` by name, but nothing by checksum.
+        response = self.client.get(
+            url + "?query=index&checksum=0000111122223333444455556666777788889999"
+        )
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 0
 
     def test_archive_paging(self):
         self.login_as(user=self.user)
