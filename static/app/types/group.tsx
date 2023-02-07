d23996a3baaaf90b6c845b5a0ed6ca58eb506710
Malachi Willey <malwilley@gmail.com>
2023-02-07 16:45:56 -0600

    feat(issues): Update issue config to support features segmented by both type and category (#44259)
    Ref https://github.com/getsentry/sentry/issues/44054
    
    We have a config system right now called `groupCapabilities` which
    segments by issue category and only supports enabling/disabling certain
    features. Issue types are becoming more differentiated so this now needs
    to segment by issue type and support more kinds of configs, like
    changing section titles and adding resource links.
    
    I also decided to rename from `groupCapabilities` to `issueTypeConfig`
    since this isn't just enabling/disabling things anymore. It also should
    be easier to discover with that name.

--- static/app/types/group.tsx
+++ static/app/types/group.tsx
@@ -65,66 +65,6 @@ export enum IssueType {
   PERFORMANCE_UNCOMPRESSED_ASSET = 'performance_uncompressed_assets',
 }
 
-type CapabilityInfo = {
-  enabled: boolean;
-  disabledReason?: string;
-};
-
-/**
- * Defines what capabilities a category of issue has. Not all categories of
- * issues work the same.
- */
-export type IssueCategoryCapabilities = {
-  /**
-   * Is the Attachments tab shown for this issue
-   */
-  attachments: CapabilityInfo;
-  /**
-   * Are codeowner features shown for this issue
-   */
-  codeowners: CapabilityInfo;
-  /**
-   * Can the issue be deleted
-   */
-  delete: CapabilityInfo;
-  /**
-   * Can the issue be deleted and discarded
-   */
-  deleteAndDiscard: CapabilityInfo;
-  /**
-   * Is the Grouping tab shown for this issue
-   */
-  grouping: CapabilityInfo;
-  /**
-   * Can the issue be ignored (and the dropdown options)
-   */
-  ignore: CapabilityInfo;
-  /**
-   * Can the issue be merged
-   */
-  merge: CapabilityInfo;
-  /**
-   * Is the Merged Issues tab shown for this issue
-   */
-  mergedIssues: CapabilityInfo;
-  /**
-   * Is the Replays tab shown for this issue
-   */
-  replays: CapabilityInfo;
-  /**
-   * Can the issue be shared
-   */
-  share: CapabilityInfo;
-  /**
-   * Is the Similar Issues tab shown for this issue
-   */
-  similarIssues: CapabilityInfo;
-  /**
-   * Is the User Feedback tab enabled for this issue
-   */
-  userFeedback: CapabilityInfo;
-};
-
 // endpoint: /api/0/issues/:issueId/attachments/?limit=50
 export type IssueAttachment = {
   dateCreated: string;
