35e3dfc8a6289290aa112ba4c40818a9c697155d
Cathy Teng <70817427+cathyteng17@users.noreply.github.com>
2023-02-07 14:25:09 -0800

    separate out owner_recipient_strategy

--- src/sentry/notifications/notifications/strategies/owner_recipient_strategy.py
+++ src/sentry/notifications/notifications/strategies/owner_recipient_strategy.py
@@ -2,11 +2,8 @@ from __future__ import annotations
 
 from typing import Iterable
 
-from django.db.models import Q
-
 from sentry import roles
 from sentry.models import OrganizationMember
-from sentry.models.organizationmemberteam import OrganizationMemberTeam
 
 from .role_based_recipient_strategy import RoleBasedRecipientStrategy
 
@@ -14,17 +11,9 @@ from .role_based_recipient_strategy import RoleBasedRecipientStrategy
 class OwnerRecipientStrategy(RoleBasedRecipientStrategy):
     def determine_member_recipients(self) -> Iterable[OrganizationMember]:
         # Explicitly typing to satisfy mypy.
-        # get owner teams
-        owner_teams = self.organization.get_teams_with_org_role(role=roles.get_top_dog().id)
-
-        # get owners from owner teams
-        owner_team_members = list(
-            OrganizationMemberTeam.objects.filter(
-                team_id__in=owner_teams,
-            ).values_list("organizationmember_id", flat=True)
+        members: Iterable[
+            OrganizationMember
+        ] = OrganizationMember.objects.get_contactable_members_for_org(self.organization.id).filter(
+            role=roles.get_top_dog().id,
         )
-
-        members: Iterable[OrganizationMember] = OrganizationMember.get_contactable_members_for_org(
-            self.organization.id
-        ).filter(Q(role=roles.get_top_dog().id) | Q(id__in=owner_team_members))
         return members
