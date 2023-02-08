8cd15e754e5f74b701d5f97cce1600eaffb70cfb
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-02-07 16:22:32 -0800

    test(issue-platform): add a sample profiling issue occurrence event for testing (#44250)
    Includes an event that more closely looks like what we'll be consuming
    in prod.
    
    Resolves sentry#43330

--- src/sentry/api/endpoints/issue_occurrence.py
+++ src/sentry/api/endpoints/issue_occurrence.py
@@ -86,7 +86,6 @@ class IssueOccurrenceEndpoint(Endpoint):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
-        occurrence = {}
         if request.query_params.get("dummyOccurrence") == "True":
             occurrence = {
                 "id": "55f1419e73884cd2b45c79918f4b6dc5",
@@ -125,18 +124,18 @@ class IssueOccurrenceEndpoint(Endpoint):
                 status=status.HTTP_400_BAD_REQUEST,
             )
 
+        event.setdefault("data", dict())
         event_serializer = BasicEventSerializer(data=event)
         if not event_serializer.is_valid():
             return Response(event_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         event = event_serializer.validated_data
-
-        # hack to put in unserialized data
-        unserialized_data = event.pop("data")
-        if unserialized_data:
-            event.update(unserialized_data)
+        # lift whatever is in 'data' dict as top-level event attrs
+        event.update(event["data"])
 
         occurrence["event_id"] = str(event["event_id"])
+        # XXX: not sure if evidence_data is required or not
+        occurrence.setdefault("evidence_data", dict())
         occurrence_serializer = IssueOccurrenceSerializer(data=occurrence)
         if not occurrence_serializer.is_valid():
             return Response(occurrence_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
