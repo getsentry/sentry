08f6bbf3e48c9d756ff50d59b3b5a6a5e5d565c4
Hubert Deng <hubertdeng123@gmail.com>
2023-02-08 13:48:35 -0800

    add TypeError handling for PickleObjectField to fix backup/restore workflow

---
+++ tests/sentry/runner/commands/test_backup.py
@@ -0,0 +1,26 @@
+import os
+
+from sentry.runner.commands.backup import export, import_
+from sentry.testutils import CliTestCase
+
+
+class BackupTest(CliTestCase):
+    command = export
+    tmp_backup_filename = "test_backup.json"
+
+    def test_export(self):
+        rv = self.invoke(self.tmp_backup_filename)
+        assert rv.exit_code == 0, rv.output
+        assert os.path.exists(self.tmp_backup_filename)
+
+
+class RestoreTest(CliTestCase):
+    command = import_
+    tmp_backup_filename = "test_backup.json"
+
+    def test_export(self):
+        rv = self.invoke(self.tmp_backup_filename)
+        assert rv.exit_code == 0, rv.output
+
+    if os.path.exists(tmp_backup_filename):
+        os.remove(tmp_backup_filename)
