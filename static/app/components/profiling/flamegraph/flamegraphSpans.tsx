f44599a239902b94ae7ac359167d56ca3bd660d6
Tony Xiao <txiao@sentry.io>
2023-02-09 17:30:32 -0500

    fix(profiling): Clean up rendering around grid renderer (#44374)
    The spans canvas had height 100% which was overlapping with the
    flamegraph canvas and the grid renderer had opacity on the background
    color so when the flamechart is scrolled, it was visible under the grid.
    
    # Screenshots
    
    ## Light mode
    
    ### Before
    
    
    ![image](https://user-images.githubusercontent.com/10239353/217941540-3cfbe347-b9a4-431f-97f2-a3dbf33bf137.png)
    
    ### After
    
    
    ![image](https://user-images.githubusercontent.com/10239353/217941205-1b2aec14-1a07-4485-a1b0-b70df6fced2b.png)
    
    ## Dark mode
    
    ### Before
    
    
    ![image](https://user-images.githubusercontent.com/10239353/217941420-91631814-568c-42b7-9c5f-caaffe2a9b2a.png)
    
    ### After
    
    
    ![image](https://user-images.githubusercontent.com/10239353/217941345-aac1fbe4-7cf0-43bf-a1ae-9894d2162865.png)

--- static/app/components/profiling/flamegraph/flamegraphSpans.tsx
+++ static/app/components/profiling/flamegraph/flamegraphSpans.tsx
@@ -405,7 +405,7 @@ export function FlamegraphSpans({
 
 const Canvas = styled('canvas')<{cursor?: CSSProperties['cursor']}>`
   width: 100%;
-  height: 100%;
+  height: calc(100% - 20px);
   position: absolute;
   left: 0;
   top: 0;
