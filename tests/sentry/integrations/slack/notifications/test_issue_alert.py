4592cc0ae9f33660fea6b948bb567ce26fc1cafc
Scott Cooper <scttcper@gmail.com>
2023-02-08 17:06:30 -0800

    expt

--- tests/sentry/integrations/slack/notifications/test_issue_alert.py
+++ tests/sentry/integrations/slack/notifications/test_issue_alert.py
@@ -760,6 +760,6 @@ class SlackIssueAlertNotificationTest(SlackActivityNotificationTest, Performance
             fallthrough_choice=FallthroughChoiceType.ACTIVE_MEMBERS,
         )
         context = notification.get_context()
-        assert "&ref_fallback=exp" in context["notification_settings_link"]
+        assert "&ref_fallback=expt" in context["notification_settings_link"]
         params = notification.get_log_params(self.user)
         assert params["fallback_experiment"] == "expt"
