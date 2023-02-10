22bb264f9e8df9c465da42c77a7dd87c164b1372
Malachi Willey <malwilley@gmail.com>
2023-02-10 16:28:04 -0600

    feat(profile-issues): Add FE config for profile issues (#44380)
    Applies frontend customizations to profile issues (evidence section
    title, resources, disabling merge/delete)

--- static/app/components/events/eventEvidence.tsx
+++ static/app/components/events/eventEvidence.tsx
@@ -3,7 +3,7 @@ import KeyValueList from 'sentry/components/events/interfaces/keyValueList';
 import {Event, Group} from 'sentry/types';
 import {getConfigForIssueType} from 'sentry/utils/issueTypeConfig';
 
-type EvidenceProps = {event: Event; group?: Group};
+type EvidenceProps = {event: Event; group: Group};
 
 /**
  * This component is rendered whenever an `event.occurrence.evidenceDisplay` is present,
@@ -12,14 +12,15 @@ type EvidenceProps = {event: Event; group?: Group};
  * When we figure out how we want to genericize the UI for performance issues we can revisit.
  */
 export const EventEvidence = ({event, group}: EvidenceProps) => {
+  const config = getConfigForIssueType(group).evidence;
   const evidenceDisplay = event.occurrence?.evidenceDisplay;
 
-  if (!group || !evidenceDisplay?.length || !getConfigForIssueType(group).evidence) {
+  if (!evidenceDisplay?.length || !config) {
     return null;
   }
 
   return (
-    <EventDataSection title="Evidence" type="evidence">
+    <EventDataSection title={config.title} type="evidence" help={config.helpText}>
       <KeyValueList
         data={evidenceDisplay.map(item => ({
           subject: item.name,
