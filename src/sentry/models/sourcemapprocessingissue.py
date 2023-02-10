70432aa56587c9159811d49f18de7ef7b246d215
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2023-02-10 13:33:42 -0800

    feat(sourcemaps): adds checks for sourcemap files to sourcemap debug endpoint (#44143)
    this pr adds the checks for if the sourcemap files exist to the
    sourcemap debug endpoint. as the final check in the endpoint, we will
    try to find the sourcemap file that is found in the releasefile
    artifact. if we are able to find the sourcemaps then we will say that
    there should be no issue with how sourcemaps are set up.

--- src/sentry/models/sourcemapprocessingissue.py
+++ src/sentry/models/sourcemapprocessingissue.py
@@ -13,6 +13,7 @@ class SourceMapProcessingIssue:
     NO_URL_MATCH = "no_url_match"
     PARTIAL_MATCH = "partial_match"
     DIST_MISMATCH = "dist_mismatch"
+    SOURCEMAP_NOT_FOUND = "sourcemap_not_found"
 
     _messages = {
         UNKNOWN_ERROR: "Unknown error",
@@ -23,6 +24,7 @@ class SourceMapProcessingIssue:
         NO_URL_MATCH: "The absolute path url does not match any source maps",
         PARTIAL_MATCH: "The absolute path url is a partial match",
         DIST_MISMATCH: "The dist values do not match",
+        SOURCEMAP_NOT_FOUND: "The sourcemap could not be found",
     }
 
     @classmethod
