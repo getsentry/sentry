c768d93d275af3cda0e5cf28c837c6bd8a8fdb06
Armin Ronacher <armin.ronacher@active-4.com>
2023-02-11 00:14:26 +0100

    fix: allow another metric with bad tags

--- src/sentry/utils/metrics.py
+++ src/sentry/utils/metrics.py
@@ -35,6 +35,7 @@ _METRICS_THAT_CAN_HAVE_BAD_TAGS = frozenset(
         "process_message",
         "commit_log_msg_latency",
         "process_message.normalized",
+        "batching_consumer.batch.size",
     ]
 )
 
