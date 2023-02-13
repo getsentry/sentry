ace8d1d1175d09f13c69f58df02e6c94bd0c070f
anthony sottile <anthony.sottile@sentry.io>
2023-02-13 14:35:38 -0500

    ref: normalize tuple vs. list in rule serialization

--- src/sentry/ingest/transaction_clusterer/rules.py
+++ src/sentry/ingest/transaction_clusterer/rules.py
@@ -56,7 +56,9 @@ class ProjectOptionRuleStore:
     _option_name = "sentry:transaction_name_cluster_rules"
 
     def read_sorted(self, project: Project) -> List[Tuple[ReplacementRule, int]]:
-        return project.get_option(self._option_name, default=[])  # type: ignore
+        ret = project.get_option(self._option_name, default=[])
+        # normalize tuple vs. list (write_pickle vs. write_json)
+        return [tuple(lst) for lst in ret]  # type: ignore[misc]
 
     def read(self, project: Project) -> RuleSet:
         return {rule: last_seen for rule, last_seen in self.read_sorted(project)}
