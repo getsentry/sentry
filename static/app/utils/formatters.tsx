e84ff2767b31f2b7394a626be7ad8335be270edf
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-02-13 02:13:54 -0800

    ref(js): Improve readability in getExactDuration (#44477)

--- static/app/utils/formatters.tsx
+++ static/app/utils/formatters.tsx
@@ -149,45 +149,42 @@ export function getExactDuration(seconds: number, abbreviation: boolean = false)
 
     if (value >= WEEK) {
       const {quotient, remainder} = divideBy(WEEK);
+      const suffix = abbr ? t('wk') : ` ${tn('week', 'weeks', quotient)}`;
 
-      return `${quotient}${
-        abbr ? t('wk') : ` ${tn('week', 'weeks', quotient)}`
-      } ${convertDuration(remainder / 1000, abbr)}`;
+      return `${quotient}${suffix} ${convertDuration(remainder / 1000, abbr)}`;
     }
     if (value >= DAY) {
       const {quotient, remainder} = divideBy(DAY);
+      const suffix = abbr ? t('d') : ` ${tn('day', 'days', quotient)}`;
 
-      return `${quotient}${
-        abbr ? t('d') : ` ${tn('day', 'days', quotient)}`
-      } ${convertDuration(remainder / 1000, abbr)}`;
+      return `${quotient}${suffix} ${convertDuration(remainder / 1000, abbr)}`;
     }
     if (value >= HOUR) {
       const {quotient, remainder} = divideBy(HOUR);
+      const suffix = abbr ? t('hr') : ` ${tn('hour', 'hours', quotient)}`;
 
-      return `${quotient}${
-        abbr ? t('hr') : ` ${tn('hour', 'hours', quotient)}`
-      } ${convertDuration(remainder / 1000, abbr)}`;
+      return `${quotient}${suffix} ${convertDuration(remainder / 1000, abbr)}`;
     }
     if (value >= MINUTE) {
       const {quotient, remainder} = divideBy(MINUTE);
+      const suffix = abbr ? t('min') : ` ${tn('minute', 'minutes', quotient)}`;
 
-      return `${quotient}${
-        abbr ? t('min') : ` ${tn('minute', 'minutes', quotient)}`
-      } ${convertDuration(remainder / 1000, abbr)}`;
+      return `${quotient}${suffix} ${convertDuration(remainder / 1000, abbr)}`;
     }
     if (value >= SECOND) {
       const {quotient, remainder} = divideBy(SECOND);
+      const suffix = abbr ? t('s') : ` ${tn('second', 'seconds', quotient)}`;
 
-      return `${quotient}${
-        abbr ? t('s') : ` ${tn('second', 'seconds', quotient)}`
-      } ${convertDuration(remainder / 1000, abbr)}`;
+      return `${quotient}${suffix} ${convertDuration(remainder / 1000, abbr)}`;
     }
 
     if (value === 0) {
       return '';
     }
 
-    return `${msValue}${abbr ? t('ms') : ` ${tn('millisecond', 'milliseconds', value)}`}`;
+    const suffix = abbr ? t('ms') : ` ${tn('millisecond', 'milliseconds', value)}`;
+
+    return `${msValue}${suffix}`;
   };
 
   const result = convertDuration(seconds, abbreviation).trim();
