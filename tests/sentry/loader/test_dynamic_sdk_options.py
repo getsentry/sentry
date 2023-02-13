af9abd5b008ac3958980bb09ba4200bcf9c7d3cb
Abhijeet Prasad <aprasad@sentry.io>
2023-02-13 15:14:19 +0100

    fix assertion error in test

--- tests/sentry/loader/test_dynamic_sdk_options.py
+++ tests/sentry/loader/test_dynamic_sdk_options.py
@@ -6,9 +6,9 @@ from sentry.testutils import TestCase
 class DynamicSdkOptions(TestCase):
     def test_default_get_dynamic_sdk_loader_option(self):
         key = ProjectKey(project_id=1, public_key="public", secret_key="secret")
-        assert get_dynamic_sdk_loader_option(key, DynamicSdkLoaderOption.HAS_REPLAY)
-        assert get_dynamic_sdk_loader_option(key, DynamicSdkLoaderOption.HAS_PERFORMANCE)
-        assert get_dynamic_sdk_loader_option(key, DynamicSdkLoaderOption.HAS_DEBUG)
+        assert not get_dynamic_sdk_loader_option(key, DynamicSdkLoaderOption.HAS_REPLAY)
+        assert not get_dynamic_sdk_loader_option(key, DynamicSdkLoaderOption.HAS_PERFORMANCE)
+        assert not get_dynamic_sdk_loader_option(key, DynamicSdkLoaderOption.HAS_DEBUG)
 
     def test_get_dynamic_sdk_loader_option(self):
         dynamic_sdk_loader_options = {}
