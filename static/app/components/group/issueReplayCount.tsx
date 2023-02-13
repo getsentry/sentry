0e1148d2ba598f9d06f3948e34248de815b2bcb2
Scott Cooper <scttcper@gmail.com>
2023-02-13 09:32:58 -0800

    fix(issues): Use tooltip skipWrapper to avoid styled tooltip (#44481)
    skipWrapper removes the span and puts the aria-describedby on the child
    element

--- static/app/components/group/issueReplayCount.tsx
+++ static/app/components/group/issueReplayCount.tsx
@@ -32,7 +32,7 @@ function IssueReplayCount({groupId}: Props) {
     count
   );
   return (
-    <StyledTooltip title={count > 50 ? titleOver50 : title50OrLess}>
+    <Tooltip skipWrapper title={count > 50 ? titleOver50 : title50OrLess}>
       <ReplayCountLink
         to={`/organizations/${organization.slug}/issues/${groupId}/replays/`}
         aria-label="replay-count"
@@ -40,14 +40,9 @@ function IssueReplayCount({groupId}: Props) {
         <IconPlay size="xs" />
         {countDisplay}
       </ReplayCountLink>
-    </StyledTooltip>
+    </Tooltip>
   );
 }
-
-const StyledTooltip = styled(Tooltip)`
-  display: flex;
-  align-items: center;
-`;
 
 const ReplayCountLink = styled(Link)`
   display: inline-flex;
