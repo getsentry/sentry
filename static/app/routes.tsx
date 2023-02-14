d6ea847615ec34b8298ed2baa7742a5e549b0a8f
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-02-14 00:32:37 -0800

    ref(js): Consistent naming for issueList container (#44564)
    Most other routes just call these `index`

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -20,7 +20,7 @@ import withDomainRequired from 'sentry/utils/withDomainRequired';
 import App from 'sentry/views/app';
 import AuthLayout from 'sentry/views/auth/layout';
 import {Tab, TabPaths} from 'sentry/views/issueDetails/types';
-import IssueListContainer from 'sentry/views/issueList/container';
+import IssueListContainer from 'sentry/views/issueList';
 import IssueListOverview from 'sentry/views/issueList/overview';
 import OrganizationContextContainer from 'sentry/views/organizationContextContainer';
 import OrganizationDetails from 'sentry/views/organizationDetails';
