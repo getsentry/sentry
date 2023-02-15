24c6f72f6266f1da257f21e7da1198c3ba3da0b5
Armen Zambrano G <armenzg@sentry.io>
2023-02-15 14:06:07 -0500

    Reduce changes

--- src/sentry/integrations/github/client.py
+++ src/sentry/integrations/github/client.py
@@ -161,15 +161,11 @@ class GitHubClientMixin(ApiClient):  # type: ignore
         trees: Dict[str, RepoTree] = {}
         extra = {"gh_org": gh_org}
         repositories = self._populate_repositories(gh_org, cache_seconds)
-        extra.update({"repos_num": str(len(repositories))})
         trees = self._populate_trees(repositories)
-        logger.info("Using cached trees for Github org.", extra=extra)
 
-        try:
-            rate_limit = self.get_rate_limit()
-            extra.update({"remaining": str(rate_limit.remaining)})
-        except ApiError:
-            logger.warning("Failed to get latest rate limit info. Let's keep going.")
+        rate_limit = self.get_rate_limit()
+        extra.update({"remaining": str(rate_limit.remaining), "repos_num": str(len(repositories))})
+        logger.info("Using cached trees for Github org.", extra=extra)
 
         return trees
 
@@ -183,11 +179,8 @@ class GitHubClientMixin(ApiClient):  # type: ignore
                 {"full_name": repo["full_name"], "default_branch": repo["default_branch"]}
                 for repo in self.get_repositories(fetch_max_pages=True)
             ]
-            if not repositories:
-                logger.warning("Fetching repositories returned an empty list.")
-            else:
-                cache.set(cache_key, repositories, cache_seconds)
-                logger.info("Cached repositories.", extra={"repos_count": len(repositories)})
+            cache.set(cache_key, repositories, cache_seconds)
+            logger.info("Cached repositories.")
 
         return repositories
 
