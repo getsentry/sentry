4bde66cd23800b40656d6dcf7c6408cd75523487
Colton Allen <colton.allen@sentry.io>
2023-02-15 14:53:34 -0600

    Remove old file specific behaviors

--- src/sentry/replays/models.py
+++ src/sentry/replays/models.py
@@ -1,9 +1,5 @@
-import mimetypes
-
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils import timezone
-from django.utils.functional import cached_property
 
 from sentry.db.models import BoundedBigIntegerField, Model, region_silo_only_model, sane_repr
 from sentry.db.models.fields.bounded import BoundedIntegerField, BoundedPositiveIntegerField
@@ -36,31 +32,4 @@ class ReplayRecordingSegment(Model):
             ("project_id", "replay_id", "segment_id"),
         )
 
-    __repr__ = sane_repr("replay_id", "segment_id", "file_id")
-
-    @cached_property
-    def mimetype(self):
-        from sentry.models import File
-
-        file = File.objects.get(id=self.file_id)
-        rv = file.headers.get("Content-Type")
-        if rv:
-            return rv.split(";")[0].strip()
-        return mimetypes.guess_type(self.name)[0] or "application/octet-stream"
-
-    def delete(self, *args, **kwargs):
-        from sentry.models import File
-
-        try:
-            file = File.objects.get(id=self.file_id)
-        except ObjectDoesNotExist:
-            # It's possible that the File itself was deleted
-            # before we were deleted when the object is in memory
-            # This seems to be a case that happens during deletion
-            # code.
-            pass
-        else:
-            file.delete()
-
-        rv = super().delete(*args, **kwargs)
-        return rv
+    __repr__ = sane_repr("replay_id", "segment_id")
