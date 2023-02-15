733efc55aa6a51e8ac2959f9bddac402d9521913
Lyn Nagara <lyn.nagara@gmail.com>
2023-02-15 10:43:01 -0800

    ref(post-process-forwarder): Use RunTaskInThreads strategy from Arroyo (#44636)
    Use Arroyo's built in RunTaskInThreads strategy to reduce boilerplate in
    the post process forwarder. Also deletes some unused code.

--- src/sentry/eventstream/kafka/postprocessworker.py
+++ src/sentry/eventstream/kafka/postprocessworker.py
@@ -10,14 +10,11 @@ from threading import Lock
 from typing import Any, Generator, Mapping, MutableMapping, Optional, Tuple
 
 from sentry import options
-from sentry.eventstream.base import GroupStates
 from sentry.eventstream.kafka.protocol import (
     get_task_kwargs_for_message,
     get_task_kwargs_for_message_from_headers,
 )
-from sentry.tasks.post_process import post_process_group
 from sentry.utils import metrics
-from sentry.utils.cache import cache_key_for_event
 
 logger = logging.getLogger(__name__)
 
@@ -87,45 +84,3 @@ def _record_metrics(partition: int, task_kwargs: Mapping[str, Any]) -> None:
                 tags={"partition": partition, "type": event_type},
                 sample_rate=1,
             )
-
-
-def dispatch_post_process_group_task(
-    event_id: str,
-    project_id: int,
-    group_id: Optional[int],
-    is_new: bool,
-    is_regression: Optional[bool],
-    is_new_group_environment: bool,
-    primary_hash: Optional[str],
-    queue: str,
-    skip_consume: bool = False,
-    group_states: Optional[GroupStates] = None,
-    occurrence_id: Optional[str] = None,
-) -> None:
-    if skip_consume:
-        logger.info("post_process.skip.raw_event", extra={"event_id": event_id})
-    else:
-        cache_key = cache_key_for_event({"project": project_id, "event_id": event_id})
-
-        post_process_group.apply_async(
-            kwargs={
-                "is_new": is_new,
-                "is_regression": is_regression,
-                "is_new_group_environment": is_new_group_environment,
-                "primary_hash": primary_hash,
-                "cache_key": cache_key,
-                "group_id": group_id,
-                "group_states": group_states,
-                "occurrence_id": occurrence_id,
-            },
-            queue=queue,
-        )
-
-
-def _get_task_kwargs_and_dispatch(message: Message) -> None:
-    task_kwargs = _get_task_kwargs(message)
-    if not task_kwargs:
-        return None
-
-    _record_metrics(message.partition(), task_kwargs)
-    dispatch_post_process_group_task(**task_kwargs)
