4edc0cefb30ed0f170f4cea436e64e87a38817b5
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-02-15 12:18:53 -0800

    feat(replays): Cleanup special casing related to replay beta status badge (#44315)
    Cleanup the special casing in
    https://github.com/getsentry/sentry/pull/44191

--- static/app/components/replays/replaysFeatureBadge.tsx
+++ static/app/components/replays/replaysFeatureBadge.tsx
@@ -1,14 +1,8 @@
 import FeatureBadge from 'sentry/components/featureBadge';
-import useOrganization from 'sentry/utils/useOrganization';
 
 function ReplaysFeatureBadge(
   props: Omit<React.ComponentProps<typeof FeatureBadge>, 'type'>
 ) {
-  // TODO(replay): Remove this special-case for our internal demo org
-  const organization = useOrganization();
-  if (organization.slug === 'testorg-az') {
-    return null;
-  }
   return <FeatureBadge {...props} type="new" />;
 }
 
