353669d16e7a8af59e2b5e63325686dc74d4908f
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-02-16 10:34:11 -0500

    ref: make Authenticator config compatible with write_json=True (#44606)
    we'd like to turn `write_json=True` everywhere -- this model field
    currently pickles a custom bytes subclass

--- tests/sentry/models/test_authenticator.py
+++ tests/sentry/models/test_authenticator.py
@@ -1,5 +1,9 @@
+from fido2.ctap2 import AuthenticatorData
+from fido2.utils import sha256
+
 from sentry.auth.authenticators import RecoveryCodeInterface, TotpInterface
-from sentry.models import Authenticator
+from sentry.auth.authenticators.u2f import create_credential_object
+from sentry.models.authenticator import Authenticator, AuthenticatorConfig
 from sentry.testutils import TestCase
 from sentry.testutils.silo import control_silo_test
 
@@ -32,3 +36,44 @@ class AuthenticatorTest(TestCase):
             user2.id: False,
             9999: False,
         }
+
+
+def test_authenticator_config_compatibility():
+    field_pickle = AuthenticatorConfig(write_json=False)
+    field_json = AuthenticatorConfig(write_json=True)
+
+    value = {
+        "devices": [
+            {
+                "binding": {
+                    "publicKey": "publickey",
+                    "keyHandle": "aowerkoweraowerkkro",
+                    "appId": "https://dev.getsentry.net:8000/auth/2fa/u2fappid.json",
+                },
+                "name": "Sentry",
+                "ts": 1512505334,
+            },
+            {
+                "name": "Alert Escargot",
+                "ts": 1512505334,
+                "binding": AuthenticatorData.create(
+                    sha256(b"test"),
+                    0x41,
+                    1,
+                    create_credential_object(
+                        {
+                            "publicKey": "webauthn",
+                            "keyHandle": "webauthn",
+                        }
+                    ),
+                ),
+            },
+        ]
+    }
+
+    enc_pickle = field_pickle.get_db_prep_value(value)
+    enc_json = field_json.get_db_prep_value(value)
+
+    for field in (field_pickle, field_json):
+        for enc in (enc_pickle, enc_json):
+            assert value == field.to_python(enc)
