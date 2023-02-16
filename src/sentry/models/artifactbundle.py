64f7794b539478ed51e867f8ff464c3ad26d142a
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-02-16 13:52:20 +0100

    Add date information

--- src/sentry/models/artifactbundle.py
+++ src/sentry/models/artifactbundle.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils import timezone
 
 from sentry.db.models import (
     BoundedBigIntegerField,
@@ -29,19 +30,28 @@ class ArtifactBundle(Model):
             ("organization_id", "bundle_id"),
             ("organization_id", "release_name"),
         )
-        index_together = (("organization_id", "release_name"),)
 
 
 @region_silo_only_model
 class DebugIdArtifactBundle(Model):
     __include_in_export__ = False
 
-    debug_id = models.UUIDField(unique=True, db_index=True)
+    debug_id = models.UUIDField()
     artifact_bundle = FlexibleForeignKey("sentry.ArtifactBundle")
+    upload_date = models.DateTimeField(default=timezone.now)
 
     class Meta:
         app_label = "sentry"
         db_table = "sentry_debugidartifactbundle"
+
+        # We can have the same debug_id pointing to different artifact_bundle(s) because the user might upload
+        # the same artifacts twice, or they might have certain build files that don't change across builds.
+        unique_together = (
+            (
+                "debug_id",
+                "artifact_bundle",
+            ),
+        )
 
 
 @region_silo_only_model
