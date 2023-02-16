a41ba089b17084d55804db37167ee257aff65875
Armen Zambrano G <armenzg@sentry.io>
2023-02-16 09:10:11 -0500

    Fix logic

--- src/sentry/tasks/derive_code_mappings.py
+++ src/sentry/tasks/derive_code_mappings.py
@@ -39,18 +39,21 @@ def process_error(error: ApiError, extra: Dict[str, str]) -> None:
 
     if msg == "Not Found":
         logger.warning("The org has uninstalled the Sentry App.", extra=extra)
-
-    if msg == "This installation has been suspended":
+        return
+    elif msg == "This installation has been suspended":
         logger.warning("The org has suspended the Sentry App.", extra=extra)
-
-    if msg == "Server Error":
+        return
+    elif msg == "Server Error":
         logger.warning("Github failed to respond.", extra=extra)
-
-    if msg.startswith("Although you appear to have the correct authorization credentials"):
+    elif msg.startswith("Although you appear to have the correct authorization credentials"):
         # Although you appear to have the correct authorization credentials, the
         # <github_org_here> organization has an IP allow list enabled, and
         # <ip_address_here> is not permitted to access this resource.
         logger.warning("The org has suspended the Sentry App. See code comment.", extra=extra)
+        return
+    elif msg.startswith("Due to U.S. trade controls law restrictions, this GitHub"):
+        logger.warning("Github has blocked this org. We will not continue.", extra=extra)
+        return
 
     # Logging the exception and returning is better than re-raising the error
     # Otherwise, API errors would not group them since the HTTPError in the stack
