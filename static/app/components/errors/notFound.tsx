1eb5f0375c1e3e5e4616810276e3e851e3f4454d
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-02-17 14:57:11 -0800

    feat(ui): Improve not found view padding (#44795)

--- static/app/components/errors/notFound.tsx
+++ static/app/components/errors/notFound.tsx
@@ -7,7 +7,7 @@ import {t, tct} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 
 const NotFound = () => (
-  <NotFoundAlert type="error" showIcon>
+  <Alert type="error" showIcon>
     <Heading>{t('Page Not Found')}</Heading>
     <p>{t('The page you are looking for was not found.')}</p>
     <p>{t('You may wish to try the following:')}</p>
@@ -37,12 +37,8 @@ const NotFound = () => (
         link: <Link to="/" />,
       })}
     </p>
-  </NotFoundAlert>
+  </Alert>
 );
-
-const NotFoundAlert = styled(Alert)`
-  margin: ${space(3)} 0;
-`;
 
 const Heading = styled('h1')`
   font-size: ${p => p.theme.fontSizeLarge};
