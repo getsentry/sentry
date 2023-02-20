74876cfbda734b762ce7f45f3d9fbc8143390d51
Andrii Soldatenko <andrii.soldatenko@sentry.io>
2023-02-20 17:19:21 +0100

    rename to projects_with_tx_count

--- src/sentry/dynamic_sampling/tasks.py
+++ src/sentry/dynamic_sampling/tasks.py
@@ -27,8 +27,8 @@ logger = logging.getLogger(__name__)
 def prioritise_projects() -> None:
     metrics.incr("sentry.tasks.dynamic_sampling.prioritise_projects.start", sample_rate=1.0)
     with metrics.timer("sentry.tasks.dynamic_sampling.prioritise_projects", sample_rate=1.0):
-        for org_id, project_id_with_count_per_root in fetch_projects_with_total_volumes().items():
-            process_projects_sample_rates(org_id, project_id_with_count_per_root)
+        for org_id, projects_with_tx_count in fetch_projects_with_total_volumes().items():
+            process_projects_sample_rates(org_id, projects_with_tx_count)
 
 
 @instrumented_task(
@@ -38,7 +38,7 @@ def prioritise_projects() -> None:
     max_retries=5,
 )  # type: ignore
 def process_projects_sample_rates(
-    organization_id: int, project_id_with_count_per_root: Sequence[Sequence[int]]
+    organization_id: int, projects_with_tx_count: Sequence[Sequence[int]]
 ) -> None:
     """
     Takes a single org id and a list of project ids
@@ -47,19 +47,17 @@ def process_projects_sample_rates(
     # Check if feature flag is enabled:
     if features.has("organizations:ds-prioritise-by-project-bias", organization):
         with metrics.timer("sentry.tasks.dynamic_sampling.process_projects_sample_rates.core"):
-            adjust_sample_rates(organization_id, project_id_with_count_per_root)
+            adjust_sample_rates(organization_id, projects_with_tx_count)
 
 
-def adjust_sample_rates(
-    org_id: int, project_id_with_count_per_root: Sequence[Sequence[int]]
-) -> None:
+def adjust_sample_rates(org_id: int, projects_with_tx_count: Sequence[Sequence[int]]) -> None:
     """
     This function apply model and adjust sample rate per project in org
     and store it in DS redis cluster, then we invalidate project config
     so relay can reread it, and we'll inject it from redis cache.
     """
     projects = []
-    for project_id, count_per_root in project_id_with_count_per_root:
+    for project_id, count_per_root in projects_with_tx_count:
         project = Project.objects.get_from_cache(id=project_id)
         sample_rate = quotas.get_blended_sample_rate(project)
         if sample_rate is None:
