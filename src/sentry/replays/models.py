f745fc0888313eb4402eb8179a23edc229b12ee1
Colton Allen <colton.allen@sentry.io>
2023-02-21 15:56:02 -0600

    Reset model to master

--- src/sentry/replays/models.py
+++ src/sentry/replays/models.py
@@ -1,23 +1,23 @@
+import mimetypes
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.utils import timezone
+from django.utils.functional import cached_property
 
 from sentry.db.models import BoundedBigIntegerField, Model, region_silo_only_model, sane_repr
 from sentry.db.models.fields.bounded import BoundedIntegerField, BoundedPositiveIntegerField
 
 
+# Based heavily on EventAttachment
 @region_silo_only_model
 class ReplayRecordingSegment(Model):
     __include_in_export__ = False
 
-    FILESTORE = 0
-    STORAGE = 1
-    DRIVER_CHOICES = [(FILESTORE, "filestore"), (STORAGE, "storage")]
-
-    driver = models.SmallIntegerField(choices=DRIVER_CHOICES, default=FILESTORE)
     project_id = BoundedBigIntegerField()
     replay_id = models.CharField(max_length=32, db_index=True)
+    file_id = BoundedBigIntegerField(db_index=True)
     segment_id = BoundedIntegerField(db_column="sequence_id")
-    file_id = BoundedBigIntegerField(db_index=True, null=True)
     date_added = models.DateTimeField(default=timezone.now, db_index=True)
     size = BoundedPositiveIntegerField(null=True)
 
@@ -30,7 +30,31 @@ class ReplayRecordingSegment(Model):
             ("project_id", "replay_id", "segment_id"),
         )
 
-    __repr__ = sane_repr("replay_id", "segment_id")
+    __repr__ = sane_repr("replay_id", "segment_id", "file_id")
+
+    @cached_property
+    def mimetype(self):
+        from sentry.models import File
+
+        file = File.objects.get(id=self.file_id)
+        rv = file.headers.get("Content-Type")
+        if rv:
+            return rv.split(";")[0].strip()
+        return mimetypes.guess_type(self.name)[0] or "application/octet-stream"
 
     def delete(self, *args, **kwargs):
-        return super().delete(*args, **kwargs)
+        from sentry.models import File
+
+        try:
+            file = File.objects.get(id=self.file_id)
+        except ObjectDoesNotExist:
+            # It's possible that the File itself was deleted
+            # before we were deleted when the object is in memory
+            # This seems to be a case that happens during deletion
+            # code.
+            pass
+        else:
+            file.delete()
+
+        rv = super().delete(*args, **kwargs)
+        return rv
