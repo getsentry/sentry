ecf30ca0523db48aa9a88112b52928cea903ccd9
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-02-21 14:57:18 -0800

    feat(codecov-v2): Require GH for Codecov integration (#44862)
    WOR-2729

--- tests/sentry/integrations/utils/test_codecov.py
+++ tests/sentry/integrations/utils/test_codecov.py
@@ -3,7 +3,8 @@ from unittest.mock import patch
 import responses
 
 from sentry import options
-from sentry.integrations.utils.codecov import has_codecov_integration
+from sentry.integrations.utils.codecov import CodecovIntegrationError, has_codecov_integration
+from sentry.models.integrations.integration import Integration
 from sentry.testutils.cases import APITestCase
 
 
@@ -15,6 +16,13 @@ class TestCodecovIntegration(APITestCase):
             external_id="extid",
         )
         options.set("codecov.client-secret", "supersecrettoken")
+
+    def test_no_github_integration(self):
+        Integration.objects.all().delete()
+
+        has_integration, error = has_codecov_integration(self.organization)
+        assert not has_integration
+        assert error == CodecovIntegrationError.MISSING_GH.value
 
     @responses.activate
     @patch(
@@ -28,7 +36,9 @@ class TestCodecovIntegration(APITestCase):
             status=404,
         )
 
-        assert not has_codecov_integration(self.organization)
+        has_integration, error = has_codecov_integration(self.organization)
+        assert not has_integration
+        assert error == CodecovIntegrationError.MISSING_CODECOV.value
 
     @responses.activate
     @patch(
@@ -42,4 +52,5 @@ class TestCodecovIntegration(APITestCase):
             status=200,
         )
 
-        assert has_codecov_integration(self.organization)
+        has_integration, _ = has_codecov_integration(self.organization)
+        assert has_integration
