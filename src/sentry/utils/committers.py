14163c8e152c68cbd3b79f27cd797a656b2e8a4a
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-02-21 11:00:06 -0800

    Add suspect commit type to CommitSerialier (#44805)
    Adds `via commit in release` or `via SCM integration` to the serialized
    commit so we can indicate the source of suspect commits in the UI.
    
    [WOR-2717](https://getsentry.atlassian.net/browse/WOR-2717)
    
    [WOR-2717]:
    https://getsentry.atlassian.net/browse/WOR-2717?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/sentry/utils/committers.py
+++ src/sentry/utils/committers.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import operator
 from collections import defaultdict
+from enum import Enum
 from functools import reduce
 from typing import (
     Any,
@@ -148,6 +149,13 @@ class AuthorCommitsWithReleaseSerialized(TypedDict):
 class AnnotatedFrame(TypedDict):
     frame: str
     commits: Sequence[Tuple[Commit, int]]
+
+
+class SuspectCommitType(Enum):
+    """Used to distinguish old suspect commits from the newer commits obtained via the commit_context."""
+
+    RELEASE_COMMIT = "via commit in release"
+    INTEGRATION_COMMIT = "via SCM integration"
 
 
 def _get_committers(
@@ -322,7 +330,15 @@ def get_serialized_event_file_committers(
         return [
             {
                 "author": author,
-                "commits": [serialize(commit, serializer=CommitSerializer(exclude=["author"]))],
+                "commits": [
+                    serialize(
+                        commit,
+                        serializer=CommitSerializer(
+                            exclude=["author"],
+                            type=SuspectCommitType.INTEGRATION_COMMIT.value,
+                        ),
+                    )
+                ],
             }
         ]
 
@@ -340,7 +356,11 @@ def get_serialized_event_file_committers(
         )
         commits = [commit for committer in committers for commit in committer["commits"]]
         serialized_commits: Sequence[MutableMapping[str, Any]] = serialize(
-            [c for (c, score) in commits], serializer=CommitSerializer(exclude=["author"])
+            [c for (c, score) in commits],
+            serializer=CommitSerializer(
+                exclude=["author"],
+                type=SuspectCommitType.RELEASE_COMMIT.value,
+            ),
         )
 
         serialized_commits_by_id = {}
