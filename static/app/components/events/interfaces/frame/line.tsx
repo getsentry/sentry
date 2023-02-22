getsentry/sentry

commit 2cbbdcd94dcc17c1cc411fc18740bbc42c5d87e4

chore(stacktrace): replace refresh with repeat' (#44771)
- Consolidating Icon Refresh and Icon Repeat to be used in stack traces
- Flipping refresh to be clockwise

**Before:**
![Screenshot 2023-02-16 at 1 06 39
PM](https://user-images.githubusercontent.com/4830259/219490277-263db49a-b35f-418d-898d-f174a31f5d32.png)
![Screenshot 2023-02-16 at 1 22 13
PM](https://user-images.githubusercontent.com/4830259/219490022-baceac11-db84-45e9-9702-64a467ab4b97.png)
![Screenshot 2023-02-16 at 1 08 23
PM](https://user-images.githubusercontent.com/4830259/219490076-670c41f9-3a3c-48d4-b4e2-455248e875d8.png)

**After:**
![Screenshot 2023-02-16 at 1 06 18
PM](https://user-images.githubusercontent.com/4830259/219490304-ff38ffdb-0cf3-421f-b5dd-9543ecfb9e09.png)
![Screenshot 2023-02-16 at 1 22 28
PM](https://user-images.githubusercontent.com/4830259/219489971-57bf4ab5-bde1-43ef-8ab9-20278f1e8a53.png)
![Screenshot 2023-02-16 at 1 06 49
PM](https://user-images.githubusercontent.com/4830259/219490096-4371102f-041e-4b6b-b362-ca4815ad6418.png)

static/app/components/events/interfaces/frame/line.tsx
@@ -452,20 +452,12 @@ const PackageInfo = styled('div')`
 
 const RepeatedFrames = styled('div')`
   display: inline-block;
-  border-radius: 50px;
-  padding: 1px 3px;
-  margin-left: ${space(1)};
-  border-width: thin;
-  border-style: solid;
-  border-color: ${p => p.theme.pink200};
-  color: ${p => p.theme.pink400};
-  background-color: ${p => p.theme.backgroundSecondary};
-  white-space: nowrap;
 `;
 
 const VertCenterWrapper = styled('div')`
   display: flex;
   align-items: center;
+  justify-content: space-between;
 `;
 
 const RepeatedContent = styled(VertCenterWrapper)`

docs-ui/stories/assets/icons/data.tsx
@@ -194,7 +194,7 @@ export const icons: IconData[] = [
   {
     id: 'refresh',
     groups: ['action'],
-    keywords: ['reload', 'restart'],
+    keywords: ['reload', 'restart', 'repeat'],
   },
   {
     id: 'bookmark',
@@ -694,11 +694,6 @@ export const icons: IconData[] = [
     id: 'slow',
     groups: ['status'],
     keywords: ['frame', 'mobile'],
-  },
-  {
-    id: 'repeat',
-    groups: ['status', 'action'],
-    keywords: ['frame', 'mobile', 'cycle'],
   },
   {
     id: 'teamwork',

static/app/components/events/interfaces/nativeFrame.tsx
@@ -22,7 +22,7 @@ import {SLOW_TOOLTIP_DELAY} from 'sentry/constants';
 import {IconCheckmark} from 'sentry/icons/iconCheckmark';
 import {IconChevron} from 'sentry/icons/iconChevron';
 import {IconFileBroken} from 'sentry/icons/iconFileBroken';
-import {IconRepeat} from 'sentry/icons/iconRepeat';
+import {IconRefresh} from 'sentry/icons/iconRefresh';
 import {IconWarning} from 'sentry/icons/iconWarning';
 import {t} from 'sentry/locale';
 import DebugMetaStore from 'sentry/stores/debugMetaStore';
@@ -298,7 +298,7 @@ function NativeFrame({
           <GroupingCell>
             {isUsedForGrouping && (
               <Tooltip title={t('This frame is repeated in every event of this issue')}>
-                <IconRepeat size="sm" color="textColor" />
+                <IconRefresh size="sm" color="textColor" />
               </Tooltip>
             )}
           </GroupingCell>

static/app/icons/iconRefresh.tsx
@@ -5,8 +5,8 @@ import {SvgIcon, SVGIconProps} from './svgIcon';
 const IconRefresh = forwardRef<SVGSVGElement, SVGIconProps>((props, ref) => {
   return (
     <SvgIcon {...props} ref={ref}>
-      <path d="M11.14,6.35a.76.76,0,0,1-.75-.75v-4a.75.75,0,0,1,.75-.75h4a.75.75,0,1,1,0,1.5H11.89V5.6A.76.76,0,0,1,11.14,6.35Z" />
-      <path d="M8,15.9A7.9,7.9,0,0,1,8,.1.75.75,0,0,1,8,1.6a6.39,6.39,0,1,0,2.81.65A.75.75,0,1,1,11.48.91,7.83,7.83,0,0,1,15.9,8,7.91,7.91,0,0,1,8,15.9Z" />
+      <path d="m9.66,4.87c0,.41.34.75.75.75h4.01c.41,0,.75-.34.75-.75V.85c0-.41-.34-.75-.75-.75s-.75.34-.75.75v3.26h-3.26c-.41,0-.75.34-.75.75Z" />
+      <path d="m.11,8c0,4.35,3.54,7.9,7.9,7.9s7.9-3.54,7.9-7.9c0-.41-.34-.75-.75-.75s-.75.34-.75.75c0,3.53-2.87,6.4-6.4,6.4S1.61,11.53,1.61,8,4.48,1.6,8.01,1.6c2.46,0,4.66,1.37,5.75,3.59.18.37.63.53,1,.34.37-.18.53-.63.34-1C13.76,1.8,11.04.1,8.01.1c-4.35,0-7.9,3.54-7.9,7.9Z" />
     </SvgIcon>
   );
 });

static/app/icons/iconRepeat.tsx
@@ -1,17 +1,0 @@
-import {forwardRef} from 'react';
-
-import {SvgIcon, SVGIconProps} from './svgIcon';
-
-const IconRepeat = forwardRef<SVGSVGElement, SVGIconProps>((props, ref) => {
-  return (
-    <SvgIcon {...props} ref={ref}>
-      <path d="m8,10.29c-.05-.05-.08-.11-.08-.19v-5.7l-1.67,1.29s-.1.05-.15.05c-.09,0-.16-.04-.22-.12l-.46-.59c-.04-.06-.05-.11-.05-.17,0-.09.04-.16.12-.22l2.46-1.9c.07-.05.16-.08.27-.08h.97c.08,0,.15.02.19.07.05.05.07.11.07.19v7.17c0,.07-.02.14-.07.19-.05.05-.11.08-.19.08h-1c-.07,0-.14-.03-.19-.08Z" />
-      <path d="m3.66,16.04c-.19,0-.38-.07-.53-.22L.26,12.94c-.29-.29-.29-.77,0-1.06l2.88-2.88c.29-.29.77-.29,1.06,0s.29.77,0,1.06l-2.34,2.34,2.34,2.35c.29.29.29.77,0,1.06-.15.15-.34.22-.53.22Z" />
-      <path d="m13.21,13.16H.79c-.41,0-.75-.34-.75-.75s.34-.75.75-.75h12.42c.69,0,1.25-.56,1.25-1.25V2.83c0-.69-.56-1.25-1.25-1.25H2.8c-.69,0-1.25.56-1.25,1.25v5.81c0,.41-.34.75-.75.75s-.75-.34-.75-.75V2.83C.05,1.31,1.28.08,2.8.08h10.42c1.52,0,2.75,1.23,2.75,2.75v7.58c0,1.52-1.23,2.75-2.75,2.75Z" />
-    </SvgIcon>
-  );
-});
-
-IconRepeat.displayName = 'IconRepeat';
-
-export {IconRepeat};

static/app/icons/index.tsx
@@ -80,7 +80,6 @@ export {IconProject} from './iconProject';
 export {IconQuestion} from './iconQuestion';
 export {IconRefresh} from './iconRefresh';
 export {IconReleases} from './iconReleases';
-export {IconRepeat} from './iconRepeat';
 export {IconResize} from './iconResize';
 export {IconReturn} from './iconReturn';
 export {IconRewind10} from './iconRewind10';

commit bb24ef1ab21d7120d2955d56ac901b67bd6e9d3e

fix(stacktrace): Fix frame title formatting when there is a source map warning (#44930)
Regression caused in https://github.com/getsentry/sentry/pull/44771,
`justify-content: space-between` was added to push the repeat icon to
the right, but this breaks down when there is an extra source map
warning icon on the left.

static/app/components/events/interfaces/frame/line.tsx
@@ -289,19 +289,21 @@ export class Line extends Component<Props, State> {
     return (
       <StrictClick onClick={this.isExpandable() ? this.toggleContext : undefined}>
         <DefaultLine className="title" data-test-id="title">
-          <VertCenterWrapper>
-            <SourceMapWarning frame={data} debugFrames={debugFrames} />
-            <div>
-              {this.renderLeadHint()}
-              <DefaultTitle
-                frame={data}
-                platform={this.props.platform ?? 'other'}
-                isHoverPreviewed={isHoverPreviewed}
-                meta={this.props.frameMeta}
-              />
-            </div>
+          <DefaultLineTitleWrapper>
+            <LeftLineTitle>
+              <SourceMapWarning frame={data} debugFrames={debugFrames} />
+              <div>
+                {this.renderLeadHint()}
+                <DefaultTitle
+                  frame={data}
+                  platform={this.props.platform ?? 'other'}
+                  isHoverPreviewed={isHoverPreviewed}
+                  meta={this.props.frameMeta}
+                />
+              </div>
+            </LeftLineTitle>
             {this.renderRepeats()}
-          </VertCenterWrapper>
+          </DefaultLineTitleWrapper>
           {this.renderExpander()}
         </DefaultLine>
       </StrictClick>
@@ -440,13 +442,18 @@ const RepeatedFrames = styled('div')`
   display: inline-block;
 `;
 
-const VertCenterWrapper = styled('div')`
+const DefaultLineTitleWrapper = styled('div')`
   display: flex;
   align-items: center;
   justify-content: space-between;
 `;
 
-const RepeatedContent = styled(VertCenterWrapper)`
+const LeftLineTitle = styled('div')`
+  display: flex;
+  align-items: center;
+`;
+
+const RepeatedContent = styled(LeftLineTitle)`
   justify-content: center;
 `;
 