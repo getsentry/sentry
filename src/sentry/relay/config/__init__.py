6019c9bc4ae1bc1cbb2e4df512ef821eef0b067b
Andrii Soldatenko <andrii.soldatenko@sentry.io>
2023-02-23 17:16:10 +0100

    remove  "c:transactions/count_per_root_project@none" from relay list

--- src/sentry/relay/config/__init__.py
+++ src/sentry/relay/config/__init__.py
@@ -497,7 +497,6 @@ TRANSACTION_METRICS = frozenset(
     [
         "s:transactions/user@none",
         "d:transactions/duration@millisecond",
-        "c:transactions/count_per_root_project@none",
     ]
 )
 
