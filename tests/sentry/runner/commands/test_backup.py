5d329e9a61d2b85e1d5897834f37e8bd287a20cb
Hubert Deng <hubertdeng123@gmail.com>
2023-02-22 22:49:07 -0800

    add test case to handle duplicate key violation during import

--- tests/sentry/runner/commands/test_backup.py
+++ tests/sentry/runner/commands/test_backup.py
@@ -1,10 +1,13 @@
+import atexit
 import os
 
 import pytest
 from click.testing import CliRunner
+from django.db import IntegrityError
 
 from sentry.runner.commands.backup import export, import_
 from sentry.testutils import CliTestCase
+from sentry.utils import json
 
 tmp_backup_filename = "test_backup.json"
 
@@ -26,5 +29,28 @@ def test_import():
     assert rv.exit_code == 0, rv.output
 
 
-if os.path.exists(tmp_backup_filename):
-    os.remove(tmp_backup_filename)
+@pytest.mark.django_db
+def test_import_duplicate_key():
+    # Adding an element with the same key as the last item in the backed up file
+    # to force a duplicate key violation exception
+    with open(tmp_backup_filename) as backup_file:
+        contents = json.load(backup_file)
+        duplicate_key_item = contents[-1]
+        duplicate_key_item["pk"] += 1
+        contents.append(duplicate_key_item)
+    with open(tmp_backup_filename, "w") as backup_file:
+        backup_file.write(json.dumps(contents))
+    rv = CliRunner().invoke(import_, tmp_backup_filename)
+    assert (
+        rv.output
+        == ">> Are you restoring from a clean database? If not, we highly recommend doing so.\n"
+    )
+    assert isinstance(rv.exception, IntegrityError)
+    assert rv.exit_code == 1
+
+
+# cleanup backup file once test suite finishes
+@atexit.register
+def cleanup_backup_file():
+    if os.path.exists(tmp_backup_filename):
+        os.remove(tmp_backup_filename)
