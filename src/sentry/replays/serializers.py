cd419d1914d876e30b1a55a4d94611eee0ba7cd9
Colton Allen <colton.allen@sentry.io>
2023-02-22 22:10:13 -0600

    Drop recording segment serializer

--- src/sentry/replays/serializers.py
+++ src/sentry/replays/serializers.py
@@ -1,19 +1,5 @@
 from rest_framework import serializers
 
-from sentry.api.serializers import Serializer
-from sentry.replays.lib.storage import RecordingSegmentStorageMeta
-
-
-class ReplayRecordingSegmentSerializer(Serializer):
-    def serialize(self, obj: RecordingSegmentStorageMeta, attrs, user):
-        return {
-            "replayId": obj.replay_id,
-            "segmentId": obj.segment_id,
-            "projectId": str(obj.project_id),
-            "dateAdded": obj.date_added,
-        }
-
-
 VALID_FIELD_SET = {
     "id",
     "title",
