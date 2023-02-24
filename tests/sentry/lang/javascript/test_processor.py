87724d59ef5fca994e55d9019087d4b6e92e7cc4
Abhijeet Prasad <aprasad@sentry.io>
2023-02-24 09:11:50 +0100

    feat(processor): Add meta field to react minified processor changes (#45030)
    Continuing our work from https://github.com/getsentry/sentry/pull/44884,
    this PR takes the `add_remark` implementation from there and uses it to
    add a substitution remark every time we process a react minified event
    message.
    
    In addition, as per feedback, we also test to make sure that store
    normalization works properly with this new set up.

--- tests/sentry/lang/javascript/test_processor.py
+++ tests/sentry/lang/javascript/test_processor.py
@@ -9,8 +9,10 @@ from unittest.mock import ANY, MagicMock, call, patch
 import pytest
 import responses
 from requests.exceptions import RequestException
+from sentry_relay.processing import StoreNormalizer
 
 from sentry import http, options
+from sentry.constants import DEFAULT_STORE_NORMALIZER_ARGS
 from sentry.event_manager import get_tag
 from sentry.lang.javascript.errormapping import REACT_MAPPING_URL, rewrite_exception
 from sentry.lang.javascript.processor import (
@@ -1422,6 +1424,123 @@ class ErrorMappingTest(unittest.TestCase):
         assert data["exception"]["values"][0]["value"] == (
             '<redacted>.getChildContext(): key "<redacted>" is not defined in ' "childContextTypes."
         )
+
+    @responses.activate
+    def test_react_error_adds_meta(self):
+        responses.add(
+            responses.GET,
+            REACT_MAPPING_URL,
+            body=r"""
+        {
+          "108": "%s.getChildContext(): key \"%s\" is not defined in childContextTypes.",
+          "109": "%s.render(): A valid React element (or null) must be returned. You may have returned undefined, an array or some other invalid object.",
+          "110": "Stateless function components cannot have refs."
+        }
+        """,
+            content_type="application/json",
+        )
+
+        value_109 = (
+            "Minified React error #109; visit http://facebook"
+            ".github.io/react/docs/error-decoder.html?invariant="
+            "109&args[]=Component for the full message or use "
+            "the non-minified dev environment for full errors "
+            "and additional helpful warnings."
+        )
+
+        value_108 = (
+            "Minified React error #108; visit http://facebook"
+            ".github.io/react/docs/error-decoder.html?\u2026"
+        )
+
+        data = {
+            "platform": "javascript",
+            "transaction": "fancy",
+            "exception": {
+                "values": [
+                    # Set first item to be None to check that we handle this case.
+                    None,
+                    {
+                        "type": "InvariantViolation",
+                        "value": value_109,
+                        "stacktrace": {
+                            "frames": [
+                                {
+                                    "abs_path": "http://example.com/foo.js",
+                                    "filename": "foo.js",
+                                    "lineno": 4,
+                                    "colno": 0,
+                                },
+                                {
+                                    "abs_path": "http://example.com/foo.js",
+                                    "filename": "foo.js",
+                                    "lineno": 1,
+                                    "colno": 0,
+                                },
+                            ]
+                        },
+                    },
+                    # Non react minified error
+                    {
+                        "type": "Error",
+                        "value": "this is not a react minified error",
+                        "stacktrace": {
+                            "frames": [
+                                {
+                                    "abs_path": "http://example.com/foo.js",
+                                    "filename": "foo.js",
+                                    "lineno": 1,
+                                    "colno": 0,
+                                },
+                            ],
+                        },
+                    },
+                    {
+                        "type": "InvariantViolation",
+                        "value": value_108,
+                        "stacktrace": {
+                            "frames": [
+                                {
+                                    "abs_path": "http://example.com/foo.js",
+                                    "filename": "foo.js",
+                                    "lineno": 4,
+                                    "colno": 0,
+                                }
+                            ]
+                        },
+                    },
+                ]
+            },
+            "_meta": {
+                "transaction": {
+                    "": {
+                        "err": ["existing", "additional"],
+                    }
+                }
+            },
+        }
+
+        assert rewrite_exception(data)
+
+        # run data through normalization to ensure that the meta is set properly
+        normalizer = StoreNormalizer(
+            remove_other=False, is_renormalize=True, **DEFAULT_STORE_NORMALIZER_ARGS
+        )
+        data = normalizer.normalize_event(dict(data))
+
+        assert data["_meta"] == {
+            "exception": {
+                "values": {
+                    "1": {"value": {"": {"rem": [["@processing:react", "s"]], "val": value_109}}},
+                    "3": {"value": {"": {"rem": [["@processing:react", "s"]], "val": value_108}}},
+                }
+            },
+            "transaction": {
+                "": {
+                    "err": ["existing", "additional"],
+                }
+            },
+        }
 
     @responses.activate
     def test_skip_none_values(self):
