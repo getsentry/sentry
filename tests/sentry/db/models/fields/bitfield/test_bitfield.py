9eb499dab01ebf64b4560162eb997827bd55c0e9
Hubert Deng <hubert.deng@sentry.io>
2023-02-23 17:16:45 -0800

    fix(backup/restore): Warn users of integrity violations during backup/restore (#44705)
    Some users are getting this error when restoring from a backup
    ```
    UniqueViolation('duplicate key value violates unique constraint "sentry_...
    ```
    
    We should wrap the model saves in one single atomic transaction so that
    none of the data is saved if an error happens. On integrity error, let's
    warn users to use a clean db.
    
    Also added some quick tests are backup/restore logic to make sure it
    doesn't error out
    
    ---------
    
    Co-authored-by: Chad Whitacre <chadwhitacre@sentry.io>

--- tests/sentry/db/models/fields/bitfield/test_bitfield.py
+++ tests/sentry/db/models/fields/bitfield/test_bitfield.py
@@ -26,6 +26,13 @@ class BitFieldTestModelForm(forms.ModelForm):
         exclude = tuple()
 
 
+class BitFieldTestModelWithDefaultsAsKeyNames(models.Model):
+    class Meta:
+        app_label = "fixtures"
+
+    flags = BitField(flags=("FLAG_0", "FLAG_1", "FLAG_2", "FLAG_3"), default=("FLAG_1", "FLAG_2"))
+
+
 class BitHandlerTest(unittest.TestCase):
     def test_comparison(self):
         bithandler_1 = BitHandler(0, ("FLAG_0", "FLAG_1", "FLAG_2", "FLAG_3"))
@@ -331,16 +338,12 @@ class BitFieldTest(TestCase):
         pytest.raises(ValueError, BitField, flags={"1": "non_int_key"})
 
     def test_defaults_as_key_names(self):
-        class TestModel(models.Model):
-            class Meta:
-                app_label = "fixtures"
-
-            flags = BitField(
-                flags=("FLAG_0", "FLAG_1", "FLAG_2", "FLAG_3"), default=("FLAG_1", "FLAG_2")
-            )
-
-        field = TestModel._meta.get_field("flags")
-        self.assertEqual(field.default, TestModel.flags.FLAG_1 | TestModel.flags.FLAG_2)
+        field = BitFieldTestModelWithDefaultsAsKeyNames._meta.get_field("flags")
+        self.assertEqual(
+            field.default,
+            BitFieldTestModelWithDefaultsAsKeyNames.flags.FLAG_1
+            | BitFieldTestModelWithDefaultsAsKeyNames.flags.FLAG_2,
+        )
 
     def test_pickle_integration(self):
         inst = BitFieldTestModel.objects.create(flags=1)
