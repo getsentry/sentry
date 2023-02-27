9eeda642c2d89abe83d5fa414fefc06177bdebfd
Ash Anand <0Calories@users.noreply.github.com>
2023-02-27 09:38:18 -0500

    feat(perf-issues): Make autogroups in Issue Details red if they contain a problem span (#45104)

--- static/app/components/performance/waterfall/utils.tsx
+++ static/app/components/performance/waterfall/utils.tsx
@@ -99,13 +99,25 @@ export const getToggleTheme = ({
   disabled,
   errored,
   isSpanGroupToggler,
+  spanBarType,
 }: {
   disabled: boolean;
   errored: boolean;
   isExpanded: boolean;
   theme: Theme;
   isSpanGroupToggler?: boolean;
+  spanBarType?: SpanBarType;
 }) => {
+  if (spanBarType) {
+    const {primary} = getSpanBarColours(spanBarType, theme);
+    return `
+    background: ${primary};
+    border: 2px solid ${theme.button.default.border};
+    color: ${theme.button.primary.color};
+    cursor: pointer;
+  `;
+  }
+
   const buttonTheme = isExpanded ? theme.button.default : theme.button.primary;
   const errorTheme = theme.button.danger;
 
@@ -124,7 +136,7 @@ export const getToggleTheme = ({
   if (isSpanGroupToggler) {
     return `
     background: ${theme.blue300};
-    border: 1px solid ${theme.button.default.border};
+    border: 2px solid ${theme.button.default.border};
     color: ${color};
     cursor: pointer;
   `;
@@ -133,7 +145,7 @@ export const getToggleTheme = ({
   if (disabled) {
     return `
     background: ${background};
-    border: 1px solid ${border};
+    border: 2px solid ${border};
     color: ${color};
     cursor: default;
   `;
@@ -141,7 +153,7 @@ export const getToggleTheme = ({
 
   return `
     background: ${background};
-    border: 1px solid ${border};
+    border: 2px solid ${border};
     color: ${color};
   `;
 };
