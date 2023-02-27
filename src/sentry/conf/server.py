3d0007860c1d4d50d1d83e2e65589b2f6cb4a1ec
Elias Hussary <elias.hussary@sentry.io>
2023-02-27 13:34:17 -0500

    feat: add profiling-dashboard-redesign-flamegraph flag

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1035,6 +1035,8 @@ SENTRY_FEATURES = {
     "organizations:profiling-ui-frames": False,
     # Enable the profiling dashboard redesign
     "organizations:profiling-dashboard-redesign": False,
+    # Enable the profiling dashboard redesign flamegraph
+    "organizations:profiling-dashboard-redesign-flamegraph": False,
     # Enable the profiling previews
     "organizations:profiling-previews": False,
     # Enable the transactions backed profiling views
