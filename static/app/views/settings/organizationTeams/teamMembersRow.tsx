130ae477700133b589b512d5f3e824fd8f9e6d9a
Cathy Teng <70817427+cathyteng17@users.noreply.github.com>
2023-02-27 11:20:46 -0800

    update team members rows

--- static/app/views/settings/organizationTeams/teamMembersRow.tsx
+++ static/app/views/settings/organizationTeams/teamMembersRow.tsx
@@ -14,6 +14,8 @@ import {
 } from 'sentry/views/settings/organizationTeams/roleOverwriteWarning';
 
 const TeamMembersRow = (props: {
+  hasOrgAdminAccess: boolean;
+  hasTeamOrgRole: boolean;
   hasWriteAccess: boolean;
   member: TeamMember;
   organization: Organization;
@@ -21,8 +23,16 @@ const TeamMembersRow = (props: {
   updateMemberRole: (member: Member, newRole: string) => void;
   user: User;
 }) => {
-  const {organization, member, user, hasWriteAccess, removeMember, updateMemberRole} =
-    props;
+  const {
+    organization,
+    member,
+    user,
+    hasWriteAccess,
+    hasTeamOrgRole,
+    hasOrgAdminAccess,
+    removeMember,
+    updateMemberRole,
+  } = props;
 
   return (
     <TeamRolesPanelItem key={member.id}>
@@ -40,6 +50,8 @@ const TeamMembersRow = (props: {
       <div>
         <RemoveButton
           hasWriteAccess={hasWriteAccess}
+          hasTeamOrgRole={hasTeamOrgRole}
+          hasOrgAdminAccess={hasOrgAdminAccess}
           onClick={() => removeMember(member)}
           member={member}
           user={user}
@@ -98,12 +110,15 @@ const TeamRoleSelect = (props: {
 };
 
 const RemoveButton = (props: {
+  hasOrgAdminAccess: boolean;
+  hasTeamOrgRole: boolean;
   hasWriteAccess: boolean;
   member: TeamMember;
   onClick: () => void;
   user: User;
 }) => {
-  const {member, user, hasWriteAccess, onClick} = props;
+  const {member, user, hasWriteAccess, hasTeamOrgRole, hasOrgAdminAccess, onClick} =
+    props;
 
   const isSelf = member.email === user.email;
   const canRemoveMember = hasWriteAccess || isSelf;
@@ -111,7 +126,21 @@ const RemoveButton = (props: {
     return null;
   }
 
-  if (member.flags['idp:provisioned']) {
+  const buttonHelpText = () => {
+    if (member.flags['idp:provisioned']) {
+      return t(
+        "Membership to this team is managed through your organization's identity provider."
+      );
+    }
+    if (hasTeamOrgRole && !hasOrgAdminAccess) {
+      return t(
+        'Membership to a team with an organization role is managed by organization owners.'
+      );
+    }
+    return undefined;
+  };
+
+  if (member.flags['idp:provisioned'] || (hasTeamOrgRole && !hasOrgAdminAccess)) {
     return (
       <Button
         size="xs"
@@ -119,15 +148,12 @@ const RemoveButton = (props: {
         icon={<IconSubtract size="xs" isCircled />}
         onClick={onClick}
         aria-label={t('Remove')}
-        title={t(
-          "Membership to this team is managed through your organization's identity provider."
-        )}
+        title={buttonHelpText()}
       >
         {t('Remove')}
       </Button>
     );
   }
-
   return (
     <Button
       size="xs"
