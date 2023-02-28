de6d5d5a79765218804b5d02b2e228a7f71aa3a3
Andrii Soldatenko <andrii.soldatenko@sentry.io>
2023-02-28 13:24:24 +0100

    feat(dyn-sampling): add new bias toggle to project details for prioritise by tx name [TET-717] (#44944)
    This PR adds new toggle for project details for prioritise by tx name
    
    
    Tested locally:
    
    <img width="758" alt="image"
    src="https://user-images.githubusercontent.com/1374633/220940820-5cd7f09a-dc69-439a-8742-540f1e5b9770.png">
    
    ---------
    
    Co-authored-by: Priscila Oliveira <priscila.oliveira@sentry.io>
    Co-authored-by: Radu Woinaroski <5281987+RaduW@users.noreply.github.com>

--- tests/sentry/api/endpoints/test_project_details.py
+++ tests/sentry/api/endpoints/test_project_details.py
@@ -1325,6 +1325,7 @@ class TestProjectDetailsDynamicSamplingBiases(TestProjectDetailsDynamicSamplingB
                 },
                 {"id": "ignoreHealthChecks", "active": True},
                 {"id": "boostKeyTransactions", "active": True},
+                {"id": "boostLowVolumeTransactions", "active": False},
             ]
 
     def test_get_dynamic_sampling_biases_with_previously_assigned_biases(self):
@@ -1351,6 +1352,7 @@ class TestProjectDetailsDynamicSamplingBiases(TestProjectDetailsDynamicSamplingB
                 },
                 {"id": "ignoreHealthChecks", "active": True},
                 {"id": "boostKeyTransactions", "active": True},
+                {"id": "boostLowVolumeTransactions", "active": False},
             ]
 
     def test_dynamic_sampling_bias_activation(self):
@@ -1468,6 +1470,7 @@ class TestProjectDetailsDynamicSamplingBiases(TestProjectDetailsDynamicSamplingB
             },
             {"id": "ignoreHealthChecks", "active": False},
             {"id": "boostKeyTransactions", "active": False},
+            {"id": "boostLowVolumeTransactions", "active": False},
         ]
         with Feature(
             {
