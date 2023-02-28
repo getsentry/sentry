95b6cb0381502dca9db9feaa4ecd928461a0f086
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-02-28 08:25:56 +0100

    feat(sourcemaps): Implement new tables supporting debug ids (#44572)

---
+++ src/sentry/models/artifactbundle.py
@@ -0,0 +1,93 @@
+from enum import Enum
+
+from django.db import models
+from django.utils import timezone
+
+from sentry.db.models import (
+    BoundedBigIntegerField,
+    BoundedPositiveIntegerField,
+    FlexibleForeignKey,
+    Model,
+    region_silo_only_model,
+)
+
+
+class SourceFileType(Enum):
+    SOURCE = 1
+    MINIFIED_SOURCE = 2
+    SOURCE_MAP = 3
+    INDEXED_RAM_BUNDLE = 4
+
+    @classmethod
+    def choices(cls):
+        return [(key.value, key.name) for key in cls]
+
+
+@region_silo_only_model
+class ArtifactBundle(Model):
+    __include_in_export__ = False
+
+    organization_id = BoundedBigIntegerField(db_index=True)
+    bundle_id = models.UUIDField(null=True)
+    file = FlexibleForeignKey("sentry.File")
+    artifact_count = BoundedPositiveIntegerField()
+    date_added = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_artifactbundle"
+
+        unique_together = (("organization_id", "bundle_id"),)
+
+
+@region_silo_only_model
+class ReleaseArtifactBundle(Model):
+    __include_in_export__ = False
+
+    organization_id = BoundedBigIntegerField(db_index=True)
+    release_name = models.CharField(max_length=250)
+    dist_id = BoundedBigIntegerField(null=True)
+    artifact_bundle = FlexibleForeignKey("sentry.ArtifactBundle")
+    date_added = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_releaseartifactbundle"
+
+        unique_together = (("organization_id", "release_name", "dist_id", "artifact_bundle"),)
+
+
+@region_silo_only_model
+class DebugIdArtifactBundle(Model):
+    __include_in_export__ = False
+
+    organization_id = BoundedBigIntegerField(db_index=True)
+    debug_id = models.UUIDField()
+    artifact_bundle = FlexibleForeignKey("sentry.ArtifactBundle")
+    source_file_type = models.IntegerField(choices=SourceFileType.choices())
+    date_added = models.DateTimeField(default=timezone.now)
+    date_last_accessed = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_debugidartifactbundle"
+
+        # We can have the same debug_id pointing to different artifact_bundle(s) because the user might upload
+        # the same artifacts twice, or they might have certain build files that don't change across builds.
+        unique_together = (("debug_id", "artifact_bundle", "source_file_type"),)
+
+
+@region_silo_only_model
+class ProjectArtifactBundle(Model):
+    __include_in_export__ = False
+
+    organization_id = BoundedBigIntegerField(db_index=True)
+    project_id = BoundedBigIntegerField(db_index=True)
+    artifact_bundle = FlexibleForeignKey("sentry.ArtifactBundle")
+    date_added = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_projectartifactbundle"
+
+        unique_together = (("project_id", "artifact_bundle"),)
