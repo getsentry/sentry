7b7fab7a90540822b5f3960b397a52897ea2768f
Jan Michael Auer <mail@jauer.org>
2023-03-01 17:13:52 +0100

    feat(monitors): Support check-in creation via Kafka

--- src/sentry/monitors/consumers/check_in.py
+++ src/sentry/monitors/consumers/check_in.py
@@ -1,3 +1,4 @@
+import datetime
 import logging
 from typing import Mapping
 
@@ -9,7 +10,8 @@ from arroyo.processing.strategies.run_task import RunTask
 from arroyo.types import Commit, Message, Partition
 from django.db import transaction
 
-from sentry.models import CheckInStatus, Monitor, MonitorCheckIn, MonitorStatus
+from sentry.models import CheckInStatus, Monitor, MonitorCheckIn, MonitorStatus, Project
+from sentry.signals import first_cron_checkin_received, first_cron_monitor_created
 from sentry.utils import json
 from sentry.utils.dates import to_datetime
 
@@ -23,30 +25,62 @@ def process_message(message: Message[KafkaPayload]) -> None:
     start_time = to_datetime(float(wrapper["start_time"]))
     project_id = int(wrapper["project_id"])
 
-    # TODO: Same as MonitorCheckInDetailsEndpoint.put. Keep in sync or factor out.
+    # TODO: Same as the check-in endpoints. Keep in sync or factor out.
     try:
         with transaction.atomic():
             try:
-                check_in = MonitorCheckIn.objects.select_related("monitor").get(
-                    project_id=project_id,
-                    monitor__guid=params["monitor_id"],
-                    guid=params["check_in_id"],
+                monitor = Monitor.objects.select_for_update().get(
+                    guid=params["monitor_id"], project_id=project_id
                 )
-            except MonitorCheckIn.DoesNotExist:
-                logger.debug("check-in or monitor do not exist: %s", params["check_in_id"])
+            except Monitor.DoesNotExist:
+                logger.debug("monitor does not exist: %s", params["monitor_id"])
                 return
 
-            # Map attributes from raw JSON to the model.
-            params["status"] = params["status"].upper()
-            if "duration" in params:
-                params["duration"] = int(params["duration"])
-            else:
-                params["duration"] = int((start_time - check_in.date_added).total_seconds() * 1000)
+            status = getattr(CheckInStatus, params["status"].upper())
+            duration = int(params["duration"]) if params.get("duration") is not None else None
 
-            monitor = check_in.monitor
-            check_in.update(**params)
+            try:
+                check_in = MonitorCheckIn.objects.select_for_update().get(
+                    guid=params["check_in_id"],
+                    project_id=project_id,
+                    monitor=monitor,
+                )
 
-            if check_in.status == CheckInStatus.ERROR:
+                if duration is None:
+                    duration = int((start_time - check_in.date_added).total_seconds() * 1000)
+
+                check_in.update(status=status, duration=duration)
+
+            except MonitorCheckIn.DoesNotExist:
+                # Infer the original start time of the check-in from the duration.
+                # Note that the clock of this worker may be off from what Relay is reporting.
+                date_added = start_time
+                if duration is not None:
+                    date_added -= datetime.timedelta(seconds=duration)
+
+                check_in = MonitorCheckIn.objects.create(
+                    project_id=project_id,
+                    monitor=monitor,
+                    guid=params["check_in_id"],
+                    duration=duration,
+                    status=status,
+                    date_added=date_added,
+                    date_updated=start_time,
+                )
+
+                project = Project.objects.get_from_cache(id=project_id)
+                if not project.flags.has_cron_checkins:
+                    # Backfill users that already have cron monitors
+                    if not project.flags.has_cron_monitors:
+                        first_cron_monitor_created.send_robust(
+                            project=project, user=None, sender=Project
+                        )
+
+                    first_cron_checkin_received.send_robust(
+                        project=project, monitor_id=str(monitor.guid), sender=Project
+                    )
+
+            if check_in.status == CheckInStatus.ERROR and monitor.status != MonitorStatus.DISABLED:
                 monitor.mark_failed(start_time)
                 return
 
@@ -55,7 +89,7 @@ def process_message(message: Message[KafkaPayload]) -> None:
                 "next_checkin": monitor.get_next_scheduled_checkin(start_time),
             }
 
-            if check_in.status == CheckInStatus.OK:
+            if check_in.status == CheckInStatus.OK and monitor.status != MonitorStatus.DISABLED:
                 monitor_params["status"] = MonitorStatus.OK
 
             Monitor.objects.filter(id=monitor.id).exclude(last_checkin__gt=start_time).update(
