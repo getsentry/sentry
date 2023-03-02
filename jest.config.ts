2b6ad6a4f7f28dfe7f2ea90393cf39064484eeae
Scott Cooper <scttcper@gmail.com>
2023-03-02 15:26:00 -0800

    entirely remove the package

--- jest.config.ts
+++ jest.config.ts
@@ -203,7 +203,7 @@ if (
  * node_modules, but some packages which use ES6 syntax only NEED to be
  * transformed.
  */
-const ESM_NODE_MODULES = ['copy-text-to-clipboard'];
+const ESM_NODE_MODULES = [];
 
 const config: Config.InitialOptions = {
   verbose: false,
