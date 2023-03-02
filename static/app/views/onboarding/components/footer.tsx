d17b372c947e19b30714b127c13901c697d8696f
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-03-02 15:14:35 +0100

    feat(onboarding): Add new working wizard styles

--- static/app/views/onboarding/components/footer.tsx
+++ static/app/views/onboarding/components/footer.tsx
@@ -1,10 +1,11 @@
-import {useCallback, useState} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import {RouteComponentProps} from 'react-router';
 import isPropValid from '@emotion/is-prop-valid';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 import {Location} from 'history';
 
+import {addSuccessMessage} from 'sentry/actionCreators/indicator';
 import {openModal} from 'sentry/actionCreators/modal';
 import {Button} from 'sentry/components/button';
 import Link from 'sentry/components/links/link';
@@ -17,9 +18,22 @@ import {Group, Project} from 'sentry/types';
 import trackAdvancedAnalyticsEvent from 'sentry/utils/analytics/trackAdvancedAnalyticsEvent';
 import {useQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
+import {useSessionStorage} from 'sentry/utils/useSessionStorage';
 
-import {usePersistedOnboardingState} from '../../utils';
-import GenericFooter from '../genericFooter';
+import {usePersistedOnboardingState} from '../utils';
+
+import GenericFooter from './genericFooter';
+
+enum OnboardingStatus {
+  WAITING = 'waiting',
+  PROCESSING = 'processing',
+  PROCESSED = 'processed',
+}
+
+type OnboardingState = {
+  status: OnboardingStatus;
+  firstIssueId?: string;
+};
 
 const DEFAULT_POLL_INTERVAL = 5000;
 
@@ -32,9 +46,7 @@ async function openChangeRouteModal(
   router: RouteComponentProps<{}, {}>['router'],
   nextLocation: Location
 ) {
-  const mod = await import(
-    'sentry/views/onboarding/components/heartbeatFooter/changeRouteModal'
-  );
+  const mod = await import('sentry/views/onboarding/components/changeRouteModal');
 
   const {ChangeRouteModal} = mod;
 
@@ -43,17 +55,28 @@ async function openChangeRouteModal(
   ));
 }
 
-export function Footer({projectSlug, router, newOrg}: Props) {
+export function Footer({projectSlug, router, route, newOrg}: Props) {
   const organization = useOrganization();
   const preferences = useLegacyStore(PreferencesStore);
   const [clientState, setClientState] = usePersistedOnboardingState();
   const [firstError, setFirstError] = useState<string | null>(null);
   const [firstIssue, setFirstIssue] = useState<Group | undefined>(undefined);
 
+  const onboarding_sessionStorage_key = `onboarding-${projectSlug}`;
+
+  const [sessionStorage, setSessionStorage] = useSessionStorage<OnboardingState>(
+    onboarding_sessionStorage_key,
+    {
+      status: OnboardingStatus.WAITING,
+      firstIssueId: undefined,
+    }
+  );
+
   useQuery<Project>([`/projects/${organization.slug}/${projectSlug}/`], {
     staleTime: 0,
     refetchInterval: DEFAULT_POLL_INTERVAL,
-    enabled: !!projectSlug && !firstError, // Fetch only if the project is available and we have not yet received an error,
+    enabled:
+      !!projectSlug && !firstError && sessionStorage.status === OnboardingStatus.WAITING, // Fetch only if the project is available and we have not yet received an error,
     onSuccess: data => {
       setFirstError(data.firstEvent);
     },
@@ -65,7 +88,10 @@ export function Footer({projectSlug, router, newOrg}: Props) {
   // event given that event datetime
   useQuery<Group[]>([`/projects/${organization.slug}/${projectSlug}/issues/`], {
     staleTime: 0,
-    enabled: !!firstError && !firstIssue, // Only fetch if an error event is received and we have not yet located the first issue,
+    enabled:
+      !!firstError &&
+      !firstIssue &&
+      sessionStorage.status === OnboardingStatus.PROCESSING, // Only fetch if an error event is received and we have not yet located the first issue,
     onSuccess: data => {
       setFirstIssue(data.find((issue: Group) => issue.firstSeen === firstError));
     },
@@ -73,7 +99,7 @@ export function Footer({projectSlug, router, newOrg}: Props) {
 
   // The explore button is only showed if Sentry has not yet received any errors OR the issue is still being processed
   const handleExploreSentry = useCallback(() => {
-    trackAdvancedAnalyticsEvent('heartbeat.onboarding_explore_sentry_button_clicked', {
+    trackAdvancedAnalyticsEvent('onboarding.explore_sentry_button_clicked', {
       organization,
     });
 
@@ -83,17 +109,88 @@ export function Footer({projectSlug, router, newOrg}: Props) {
     });
   }, [router, organization]);
 
+  useEffect(() => {
+    if (!firstError) {
+      return;
+    }
+
+    if (sessionStorage.status !== OnboardingStatus.WAITING) {
+      return;
+    }
+
+    trackAdvancedAnalyticsEvent('onboarding.first_error_received', {
+      organization,
+      new_organization: !!newOrg,
+    });
+
+    setSessionStorage({status: OnboardingStatus.PROCESSING});
+    addSuccessMessage(t('First error received'));
+  }, [firstError, newOrg, organization, setSessionStorage, sessionStorage]);
+
+  useEffect(() => {
+    if (!firstIssue) {
+      return;
+    }
+
+    if (sessionStorage.status !== OnboardingStatus.PROCESSING) {
+      return;
+    }
+
+    trackAdvancedAnalyticsEvent('onboarding.first_error_processed', {
+      organization,
+      new_organization: !!newOrg,
+    });
+
+    setSessionStorage({status: OnboardingStatus.PROCESSED, firstIssueId: firstIssue.id});
+    addSuccessMessage(t('First error processed'));
+  }, [firstIssue, newOrg, organization, setSessionStorage, sessionStorage]);
+
+  useEffect(() => {
+    const onUnload = (nextLocation?: Location) => {
+      if (location.pathname.startsWith('onboarding')) {
+        return true;
+      }
+
+      // If the user has already sent an error, then we don't show the dialog
+      if (
+        sessionStorage.status === OnboardingStatus.PROCESSING ||
+        sessionStorage.status === OnboardingStatus.PROCESSED
+      ) {
+        return true;
+      }
+
+      // Next Location is always available when user clicks on a item with a new route
+      if (!nextLocation) {
+        return true;
+      }
+
+      if (nextLocation.query.setUpRemainingOnboardingTasksLater) {
+        return true;
+      }
+
+      // If users are in the onboarding of existing orgs &&
+      // have started the SDK instrumentation &&
+      // clicks elsewhere else to change the route,
+      // then we display the 'are you sure?' dialog.
+      openChangeRouteModal(router, nextLocation);
+
+      return false;
+    };
+
+    router.setRouteLeaveHook(route, onUnload);
+  }, [router, route, organization, sessionStorage.status]);
+
   const handleViewError = useCallback(() => {
-    trackAdvancedAnalyticsEvent('heartbeat.onboarding_go_to_my_error_button_clicked', {
+    trackAdvancedAnalyticsEvent('onboarding.view_error_button_clicked', {
       organization,
       new_organization: !!newOrg,
     });
 
     router.push({
       ...router.location,
-      pathname: `/organizations/${organization.slug}/issues/${firstIssue}/?referrer=onboarding-first-event-footer`,
+      pathname: `/organizations/${organization.slug}/issues/${sessionStorage.firstIssueId}/?referrer=onboarding-first-event-footer`,
     });
-  }, [organization, newOrg, router, firstIssue]);
+  }, [organization, newOrg, router, sessionStorage]);
 
   return (
     <Wrapper newOrg={!!newOrg} sidebarCollapsed={!!preferences.collapsed}>
@@ -115,25 +212,25 @@ export function Footer({projectSlug, router, newOrg}: Props) {
         {t('Skip Onboarding')}
       </SkipOnboardingLink>
       <Statuses>
-        {!firstError ? (
+        {sessionStorage.status === OnboardingStatus.WAITING ? (
           <WaitingForErrorStatus>
             <IconCircle size="sm" />
             {t('Waiting for error')}
           </WaitingForErrorStatus>
-        ) : firstIssue ? (
+        ) : sessionStorage.status === OnboardingStatus.PROCESSED ? (
           <ErrorProcessedStatus>
             <IconCheckmark isCircled size="sm" color="green300" />
             {t('Error Processed!')}
           </ErrorProcessedStatus>
         ) : (
           <ErrorProcessingStatus>
-            <IconRefresh size="sm" />
+            <RefreshIcon size="sm" />
             {t('Processing error')}
           </ErrorProcessingStatus>
         )}
       </Statuses>
       <Actions>
-        {firstIssue ? (
+        {sessionStorage.status === OnboardingStatus.PROCESSED ? (
           <Button priority="primary" onClick={handleViewError}>
             {t('View Error')}
           </Button>
@@ -218,9 +315,20 @@ const WaitingForErrorStatus = styled('div')`
 const ErrorProcessingStatus = styled(WaitingForErrorStatus)`
   border-color: ${p => p.theme.gray200};
   color: ${p => p.theme.gray300};
+  position: relative;
+
+  @keyframes rotate {
+    100% {
+      transform: rotate(360deg);
+    }
+  }
 `;
 
 const ErrorProcessedStatus = styled(WaitingForErrorStatus)`
   background: ${p => p.theme.gray500};
   color: ${p => p.theme.white};
+`;
+
+const RefreshIcon = styled(IconRefresh)`
+  animation: rotate 1s linear infinite;
 `;
