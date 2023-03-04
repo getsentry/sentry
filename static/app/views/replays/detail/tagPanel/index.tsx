1cf2fd403a28a1052d5d2639bb63fe3a556757dd
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-03-03 16:28:34 -0800

    feat(replays): Link from Replay Details tags to replay search results for the tag value

--- static/app/views/replays/detail/tagPanel/index.tsx
+++ static/app/views/replays/detail/tagPanel/index.tsx
@@ -1,4 +1,6 @@
+import {useCallback} from 'react';
 import styled from '@emotion/styled';
+import {LocationDescriptor} from 'history';
 
 import EmptyMessage from 'sentry/components/emptyMessage';
 import {KeyValueTable} from 'sentry/components/keyValueTable';
@@ -7,11 +9,25 @@ import Placeholder from 'sentry/components/placeholder';
 import {useReplayContext} from 'sentry/components/replays/replayContext';
 import ReplayTagsTableRow from 'sentry/components/replays/replayTagsTableRow';
 import {t} from 'sentry/locale';
+import useOrganization from 'sentry/utils/useOrganization';
+import {normalizeUrl} from 'sentry/utils/withDomainRequired';
 import FluidPanel from 'sentry/views/replays/detail/layout/fluidPanel';
 
 function TagPanel() {
+  const organization = useOrganization();
   const {replay} = useReplayContext();
   const replayRecord = replay?.getReplay();
+
+  const generateUrl = useCallback(
+    (name: string, value: string) =>
+      ({
+        pathname: normalizeUrl(`/organizations/${organization.slug}/replays/`),
+        query: {
+          query: `${name}:"${value}"`,
+        },
+      } as LocationDescriptor),
+    [organization.slug]
+  );
 
   if (!replayRecord) {
     return <Placeholder testId="replay-tags-loading-placeholder" height="100%" />;
@@ -24,8 +40,13 @@ function TagPanel() {
       <FluidPanel>
         {tags.length ? (
           <KeyValueTable>
-            {tags.map(([key, value]) => (
-              <ReplayTagsTableRow key={key} tag={{key, value}} />
+            {tags.map(([key, values]) => (
+              <ReplayTagsTableRow
+                key={key}
+                name={key}
+                values={values}
+                generateUrl={generateUrl}
+              />
             ))}
           </KeyValueTable>
         ) : (
