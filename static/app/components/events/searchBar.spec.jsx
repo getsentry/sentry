21e59d93b0e7b47b9c72b76d31bc1912585fddd8
Malachi Willey <malwilley@gmail.com>
2023-03-06 15:05:14 -0800

    Fix events/searchBar.spec.jsx

--- static/app/components/events/searchBar.spec.jsx
+++ static/app/components/events/searchBar.spec.jsx
@@ -13,7 +13,7 @@ const selectNthAutocompleteItem = async index => {
 
 async function setQuery(query) {
   const input = screen.getByTestId('smart-search-input');
-  await userEvent.click(input);
+  await userEvent.click(input, {delay: null});
   await userEvent.type(input, query);
 }
 
