9fdc046a7c92f42e15ead59be4c2f6cd578a4ae4
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-03-06 16:20:51 +0100

    Fix test

--- tests/sentry/dynamic_sampling/test_utils.py
+++ tests/sentry/dynamic_sampling/test_utils.py
@@ -16,7 +16,7 @@ def test_apply_dynamic_factor_with_valid_params(base_sample_rate, x, expected):
     assert apply_dynamic_factor(base_sample_rate, x) == pytest.approx(expected)
 
 
-@pytest.mark.parametrize("base_sample_rate", [-0.1, 1.1])
-def test_apply_dynamic_factor_with_invalid_params(base_sample_rate):
+@pytest.mark.parametrize(["base_sample_rate", "x"], [(-0.1, 1.5), (1.1, 2.5), (0.5, 0)])
+def test_apply_dynamic_factor_with_invalid_params(base_sample_rate, x):
     with pytest.raises(Exception):
-        apply_dynamic_factor(base_sample_rate, 1)
+        apply_dynamic_factor(base_sample_rate, x)
