fce79b40d087b181cdceb58890bae61f73dd3a2a
Shruthi <shruthilaya.jaganathan@sentry.io>
2023-03-06 11:08:19 -0500

    feat(anr): Display thread state in thread selector (#45371)
    Displays thread state in the thread selector if available.
    Requires https://github.com/getsentry/sentry/pull/45215 before we
    actually start seeing thread states
    
    ![Screenshot 2023-03-03 at 4 10 57
    PM](https://user-images.githubusercontent.com/63818634/222830193-cee9e8e0-7a03-4540-8b0d-f25002e13cb8.png)
    ![Screenshot 2023-03-03 at 4 11 03
    PM](https://user-images.githubusercontent.com/63818634/222830201-da1894cc-9d60-4e9a-9cd7-d28d9be3cc83.png)
    
    
    ![Screenshot 2023-03-03 at 4 11 11
    PM](https://user-images.githubusercontent.com/63818634/222830227-47654424-f915-480c-a8b0-aad9cda33e87.png)
    ![Screenshot 2023-03-03 at 4 11 16
    PM](https://user-images.githubusercontent.com/63818634/222830235-7f342709-8265-4415-8060-12eaea2f5285.png)

--- static/app/components/events/interfaces/threads/threadSelector/header.tsx
+++ static/app/components/events/interfaces/threads/threadSelector/header.tsx
@@ -2,12 +2,17 @@ import {t} from 'sentry/locale';
 
 import {Grid, GridCell} from './styles';
 
-const Header = () => (
-  <Grid>
+type Props = {
+  hasThreadStates: boolean;
+};
+
+const Header = ({hasThreadStates}: Props) => (
+  <Grid hasThreadStates={hasThreadStates}>
     <GridCell />
     <GridCell>{t('Id')}</GridCell>
     <GridCell>{t('Name')}</GridCell>
     <GridCell>{t('Label')}</GridCell>
+    {hasThreadStates && <GridCell>{t('State')}</GridCell>}
   </Grid>
 );
 
