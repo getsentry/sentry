dbefee3e4a788892e348c595c426e46178596235
Scott Cooper <scttcper@gmail.com>
2023-03-07 10:17:18 -0800

    fix(stacktrace): Prevent overflow for stacktrace frame (#45435)

--- static/app/components/events/interfaces/frame/contextLine.tsx
+++ static/app/components/events/interfaces/frame/contextLine.tsx
@@ -122,7 +122,10 @@ const StyledLi = styled('li')`
 `;
 
 // TODO(scttcper): The parent component should be a grid, currently has too many other children
+// If the grid was higher up we wouldn't have to hardcode the line number width and could fit larger line numbers
+const lineNumberWidth = '58px';
+const padding = space(2);
 const LineContent = styled('div')`
   display: grid;
-  grid-template-columns: 58px 1fr;
+  grid-template-columns: ${lineNumberWidth} calc(100% - ${lineNumberWidth} - ${padding});
 `;
