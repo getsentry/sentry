21c29d5404cc1513b8000c3b97a856091ae4144d
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-03-06 20:48:16 -0800

    fix(monitors): Re-enables MonitorEnvironment Creation (#45453)

--- tests/sentry/models/test_monitorenvironment.py
+++ tests/sentry/models/test_monitorenvironment.py
@@ -1,3 +1,4 @@
+from sentry.models import Environment, Monitor, MonitorEnvironment, MonitorType, ScheduleType
 from sentry.testutils import TestCase
 from sentry.testutils.silo import region_silo_test
 
@@ -5,20 +6,18 @@ from sentry.testutils.silo import region_silo_test
 @region_silo_test(stable=True)
 class MonitorEnvironmentTestCase(TestCase):
     def test_monitor_environment(self):
-        # Commenting out until we get MonitorEnvironment DB into a stable state
-        # project = self.project
-        # environment = Environment.get_or_create(project, "production")
-        #
-        # monitor = Monitor.objects.create(
-        #     organization_id=self.organization.id,
-        #     project_id=self.project.id,
-        #     type=MonitorType.CRON_JOB,
-        #     config={"schedule": [1, "month"], "schedule_type": ScheduleType.INTERVAL},
-        # )
-        #
-        # production_monitor = MonitorEnvironment.objects.create(
-        #     monitor=monitor, environment=environment
-        # )
-        #
-        # assert type(production_monitor) == MonitorEnvironment
-        pass
+        project = self.project
+        environment = Environment.get_or_create(project, "production")
+
+        monitor = Monitor.objects.create(
+            organization_id=self.organization.id,
+            project_id=self.project.id,
+            type=MonitorType.CRON_JOB,
+            config={"schedule": [1, "month"], "schedule_type": ScheduleType.INTERVAL},
+        )
+
+        production_monitor = MonitorEnvironment.objects.create(
+            monitor=monitor, environment=environment
+        )
+
+        assert type(production_monitor) == MonitorEnvironment
