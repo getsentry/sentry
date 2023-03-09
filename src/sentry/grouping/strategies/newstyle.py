75cd1cd29aee140d4573e8435be194b3b78ac113
Alexander Dinauer <alexander.dinauer@sentry.io>
2023-03-09 12:13:57 +0100

    Introduce flag to enable/disable regexes

--- src/sentry/grouping/strategies/newstyle.py
+++ src/sentry/grouping/strategies/newstyle.py
@@ -192,7 +192,7 @@ def get_module_component(
             module = _java_cglib_enhancer_re.sub(r"\1<auto>", module)
             module = _java_assist_enhancer_re.sub(r"\1<auto>", module)
             module = _clojure_enhancer_re.sub(r"\1<auto>", module)
-            if context.get_strategy_id() == "newstyle:2023-01-11":
+            if context["java_cglib_hibernate_logic"]:
                 module = _java_enhancer_by_re.sub(r"\1<auto>", module)
                 module = _java_fast_class_by_re.sub(r"\1<auto>", module)
                 module = _java_hibernate_proxy_re.sub(r"\1<auto>", module)
