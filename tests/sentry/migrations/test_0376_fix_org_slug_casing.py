9d8ed7d4b262c22bf87d1979f0b57a73373fd4d2
Mark Story <mark@mark-story.com>
2023-03-10 17:07:37 -0500

    Fix mistakes in slug update migration

--- tests/sentry/migrations/test_0376_fix_org_slug_casing.py
+++ tests/sentry/migrations/test_0376_fix_org_slug_casing.py
@@ -1,3 +1,4 @@
+from sentry.models.organization import Organization
 from sentry.testutils.cases import TestMigrations
 
 
@@ -7,10 +8,15 @@ class TestOrgSlugMigration(TestMigrations):
 
     def setup_before_migration(self, apps):
         self.ok_org = self.create_organization(slug="good-slug")
-        self.rename_org = self.create_organization(slug="bAdSluG")
+        self.rename_org = self.create_organization(slug="badslug")
 
         self.has_lower = self.create_organization(slug="taken")
-        self.is_dupe = self.create_organization(slug="TakeN")
+        self.is_dupe = self.create_organization(slug="taken-dupe")
+
+        # Organization.save() corrects our bad slugs, so
+        # we need to sneak by django and coerce bad states
+        Organization.objects.filter(id=self.rename_org.id).update(slug="bAdSluG")
+        Organization.objects.filter(id=self.is_dupe.id).update(slug="TakeN")
 
     def test(self):
         self.ok_org.refresh_from_db()
@@ -21,5 +27,7 @@ class TestOrgSlugMigration(TestMigrations):
 
         self.has_lower.refresh_from_db()
         self.is_dupe.refresh_from_db()
-        assert self.is_dupe.slug.startswith("taken")
+
+        assert self.has_lower.slug == "taken"
+        assert self.is_dupe.slug.startswith("taken-")
         assert self.is_dupe.slug != "taken"
