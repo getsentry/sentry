301eba4c286f3bc4644cdeb2d2f2b2a50c8e5f21
Colton Allen <colton.allen@sentry.io>
2023-03-10 15:00:52 -0600

    Add dom_index as submodule of the ingest module

--- src/sentry/replays/usecases/ingest/__init__.py
+++ src/sentry/replays/usecases/ingest/__init__.py
@@ -2,11 +2,9 @@ from __future__ import annotations
 
 import dataclasses
 import logging
-import time
-import uuid
 from datetime import datetime, timezone
 from io import BytesIO
-from typing import Iterator, List, Literal, TypedDict, Union
+from typing import TypedDict, Union
 
 from django.conf import settings
 from django.db.utils import IntegrityError
@@ -18,9 +16,8 @@ from sentry.models.project import Project
 from sentry.replays.cache import RecordingSegmentCache, RecordingSegmentParts
 from sentry.replays.models import ReplayRecordingSegment as ReplayRecordingSegmentModel
 from sentry.signals import first_replay_received
-from sentry.utils import json, kafka_config, metrics
+from sentry.utils import json, metrics
 from sentry.utils.outcomes import Outcome, track_outcome
-from sentry.utils.pubsub import KafkaPublisher
 
 logger = logging.getLogger("sentry.replays")
 
@@ -268,115 +265,3 @@ def process_headers(bytes_with_headers: bytes) -> tuple[RecordingSegmentHeaders,
 
 def replay_recording_segment_cache_id(project_id: int, replay_id: str, segment_id: str) -> str:
     return f"{project_id}:{replay_id}:{segment_id}"
-
-
-class ReplayActionsPayloadAction(TypedDict):
-    dom_action: str
-    dom_element: str
-    dom_id: str
-    dom_classes: List[str]
-    dom_aria_label: str
-    dom_aria_role: str
-    dom_role: str
-    dom_text_content: str
-    dom_node_id: int
-    timestamp: int
-    event_hash: str
-
-
-class ReplayActionsPayload(TypedDict):
-    type: Literal["replay_actions"]
-    event_id: str
-    replay_id: str
-    segment_id: int
-    actions: Iterator[ReplayActionsPayloadAction]
-
-
-class ReplayActionsEvent(TypedDict):
-    type: Literal["replay_event"]
-    start_time: float
-    replay_id: str
-    project_id: int
-    retention_days: int
-    payload: List[bytes]
-
-
-def create_replay_actions_event(
-    replay_id: str,
-    project_id: int,
-    retention_days: int,
-    payload: ReplayActionsPayload,
-) -> ReplayActionsEvent:
-    return {
-        "type": "replay_event",
-        "start_time": time.time(),
-        "replay_id": replay_id,
-        "project_id": project_id,
-        "retention_days": retention_days,
-        "payload": list(bytes(json.dumps(payload).encode())),
-    }
-
-
-def create_replay_actions_payload(
-    replay_id: str,
-    segment_id: int,
-    actions: Iterator[ReplayActionsPayloadAction],
-) -> ReplayActionsPayload:
-    return {
-        "type": "replay_actions",
-        "event_id": uuid.uuid4().hex,
-        "replay_id": replay_id,
-        "segment_id": segment_id,
-        "actions": actions,
-    }
-
-
-def iter_user_actions(segment_data: bytes) -> Iterator[ReplayActionsPayloadAction]:
-    """Return a list of ReplayActionsPayloadAction types."""
-    events = json.loads(segment_data)
-    for event in events:
-        if event["type"] == 5 and event["data"]["tag"] == "breadcrumb":
-            payload = event["data"]["payload"]
-            if payload["category"] == "ui.click":
-                node = payload["node"]
-                attributes = node.get("attributes", {})
-                yield {
-                    "dom_action": "click",
-                    "dom_element": node["tagName"],
-                    "dom_id": attributes.get("id", ""),
-                    "dom_classes": attributes.get("class", "").split(" "),
-                    "dom_aria_label": attributes.get("aria_label", ""),
-                    "dom_aria_role": attributes.get("aria_role", ""),
-                    "dom_role": attributes.get("role", ""),
-                    "dom_text_content": node["text_content"],
-                    "dom_node_id": node["id"],
-                    "timestamp": int(payload["timestamp"]),
-                    "event_hash": uuid.uuid4().hex,
-                }
-
-
-def emit_replay_actions(
-    project_id: int,
-    replay_id: str,
-    segment_id: int,
-    retention_days: int,
-    actions: Iterator[ReplayActionsPayloadAction],
-):
-    payload = create_replay_actions_payload(replay_id, segment_id, actions)
-    event = create_replay_actions_event(replay_id, project_id, retention_days, payload)
-
-    publisher = _initialize_publisher()
-    publisher.publish("ingest-replay-events", json.dumps(event))
-
-
-def _initialize_publisher() -> KafkaPublisher:
-    global replay_publisher
-
-    if replay_publisher is None:
-        config = settings.KAFKA_TOPICS[settings.KAFKA_INGEST_REPLAY_EVENTS]
-        replay_publisher = KafkaPublisher(
-            kafka_config.get_kafka_producer_cluster_options(config["cluster"]),
-            asynchronous=False,
-        )
-
-    return replay_publisher
---
+++ src/sentry/replays/usecases/ingest/dom_index.py
@@ -0,0 +1,125 @@
+import time
+import uuid
+import zlib
+from typing import Iterator, List, Literal, TypedDict
+
+from django.conf import settings
+
+from sentry.utils import json, kafka_config
+from sentry.utils.pubsub import KafkaPublisher
+
+
+class ReplayActionsPayloadAction(TypedDict):
+    dom_action: str
+    dom_element: str
+    dom_id: str
+    dom_classes: List[str]
+    dom_aria_label: str
+    dom_aria_role: str
+    dom_role: str
+    dom_text_content: str
+    dom_node_id: int
+    timestamp: int
+    event_hash: str
+
+
+class ReplayActionsPayload(TypedDict):
+    type: Literal["replay_actions"]
+    replay_id: str
+    segment_id: int
+    actions: Iterator[ReplayActionsPayloadAction]
+
+
+class ReplayActionsEvent(TypedDict):
+    type: Literal["replay_event"]
+    start_time: float
+    replay_id: str
+    project_id: int
+    retention_days: int
+    payload: List[bytes]
+
+
+def parse_and_emit_replay_actions(
+    project_id: int,
+    replay_id: str,
+    segment_id: int,
+    retention_days: int,
+    segment_bytes: bytes,
+) -> None:
+    actions = list(iter_user_actions(segment_bytes))
+    if len(actions) == 0:
+        return None
+
+    payload = create_replay_actions_payload(replay_id, segment_id, actions)
+    wrapper = create_replay_actions_event(replay_id, project_id, retention_days, payload)
+
+    publisher = _initialize_publisher()
+    publisher.publish("ingest-replay-events", json.dumps(wrapper))
+
+
+def create_replay_actions_event(
+    replay_id: str,
+    project_id: int,
+    retention_days: int,
+    payload: ReplayActionsPayload,
+) -> ReplayActionsEvent:
+    return {
+        "type": "replay_event",
+        "start_time": time.time(),
+        "replay_id": replay_id,
+        "project_id": project_id,
+        "retention_days": retention_days,
+        "payload": list(bytes(json.dumps(payload).encode())),
+    }
+
+
+def create_replay_actions_payload(
+    replay_id: str,
+    segment_id: int,
+    actions: Iterator[ReplayActionsPayloadAction],
+) -> ReplayActionsPayload:
+    return {
+        "type": "replay_actions",
+        "replay_id": replay_id,
+        "segment_id": segment_id,
+        "actions": actions,
+    }
+
+
+def iter_user_actions(segment_data: bytes) -> Iterator[ReplayActionsPayloadAction]:
+    """Return a list of ReplayActionsPayloadAction types."""
+    decompressed_bytes = zlib.decompress(segment_data, zlib.MAX_WBITS | 32)
+    events = json.loads(decompressed_bytes)
+
+    for event in events:
+        if event.get("type") == 5 and event.get("data", {}).get("tag") == "breadcrumb":
+            payload = event["data"].get("payload", {})
+            if payload.get("category") == "ui.click":
+                node = payload.get("node", {})
+                attributes = node.get("attributes", {})
+                yield {
+                    "dom_action": "click",
+                    "dom_element": node["tagName"],
+                    "dom_id": attributes.get("id", ""),
+                    "dom_classes": attributes.get("class", "").split(" "),
+                    "dom_aria_label": attributes.get("aria_label", ""),
+                    "dom_aria_role": attributes.get("aria_role", ""),
+                    "dom_role": attributes.get("role", ""),
+                    "dom_text_content": node["text_content"],
+                    "dom_node_id": node["id"],
+                    "timestamp": int(payload["timestamp"]),
+                    "event_hash": uuid.uuid4().hex,
+                }
+
+
+def _initialize_publisher() -> KafkaPublisher:
+    global replay_publisher
+
+    if replay_publisher is None:
+        config = settings.KAFKA_TOPICS[settings.KAFKA_INGEST_REPLAY_EVENTS]
+        replay_publisher = KafkaPublisher(
+            kafka_config.get_kafka_producer_cluster_options(config["cluster"]),
+            asynchronous=False,
+        )
+
+    return replay_publisher
