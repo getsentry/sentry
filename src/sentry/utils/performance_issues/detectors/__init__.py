6a637b8a9de93a75e6fdc0ce2466ea9861d5b3ab
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-03-10 11:56:56 -0500

    SlowDBQueryDetector

--- src/sentry/utils/performance_issues/detectors/__init__.py
+++ src/sentry/utils/performance_issues/detectors/__init__.py
@@ -1,4 +1,5 @@
 from .consecutive_db_detector import ConsecutiveDBSpanDetector  # NOQA
 from .consecutive_http_detector import ConsecutiveHTTPSpanDetector  # NOQA
 from .n_plus_one_api_calls_detector import NPlusOneAPICallsDetector  # NOQA
+from .slow_db_query_detector import SlowDBQueryDetector  # NOQA
 from .uncompressed_asset_detector import UncompressedAssetSpanDetector  # NOQA
--- src/sentry/utils/performance_issues/performance_detection.py
+++ src/sentry/utils/performance_issues/performance_detection.py
@@ -19,7 +19,6 @@ from sentry.issues.grouptype import (
     PerformanceMNPlusOneDBQueriesGroupType,
     PerformanceNPlusOneGroupType,
     PerformanceRenderBlockingAssetSpanGroupType,
-    PerformanceSlowDBQueryGroupType,
 )
 from sentry.models import Organization, Project, ProjectDebugFile, ProjectOption
 from sentry.projectoptions.defaults import DEFAULT_PROJECT_PERFORMANCE_DETECTION_SETTINGS
@@ -27,18 +26,12 @@ from sentry.utils import metrics
 from sentry.utils.event_frames import get_sdk_name
 from sentry.utils.safe import get_path
 
-from .base import (
-    DETECTOR_TYPE_TO_GROUP_TYPE,
-    DetectorType,
-    PerformanceDetector,
-    fingerprint_resource_span,
-    fingerprint_span,
-    get_span_duration,
-)
+from .base import DetectorType, PerformanceDetector, fingerprint_resource_span, get_span_duration
 from .detectors import (
     ConsecutiveDBSpanDetector,
     ConsecutiveHTTPSpanDetector,
     NPlusOneAPICallsDetector,
+    SlowDBQueryDetector,
     UncompressedAssetSpanDetector,
 )
 from .performance_problem import PerformanceProblem
@@ -346,82 +339,6 @@ def total_span_time(span_list: List[Dict[str, Any]]) -> float:
     # Add the remaining duration
     total_duration += current_max - current_min
     return total_duration * 1000
-
-
-class SlowDBQueryDetector(PerformanceDetector):
-    """
-    Check for slow spans in a certain type of span.op (eg. slow db spans)
-    """
-
-    __slots__ = "stored_problems"
-
-    type: DetectorType = DetectorType.SLOW_DB_QUERY
-    settings_key = DetectorType.SLOW_DB_QUERY
-
-    def init(self):
-        self.stored_problems = {}
-
-    def visit_span(self, span: Span):
-        settings_for_span = self.settings_for_span(span)
-        if not settings_for_span:
-            return
-        op, span_id, op_prefix, span_duration, settings = settings_for_span
-        duration_threshold = settings.get("duration_threshold")
-
-        fingerprint = fingerprint_span(span)
-
-        if not fingerprint:
-            return
-
-        if not SlowDBQueryDetector.is_span_eligible(span):
-            return
-
-        description = span.get("description", None)
-        description = description.strip()
-
-        if span_duration >= timedelta(
-            milliseconds=duration_threshold
-        ) and not self.stored_problems.get(fingerprint, False):
-            spans_involved = [span_id]
-
-            hash = span.get("hash", "")
-            type = DETECTOR_TYPE_TO_GROUP_TYPE[self.settings_key]
-
-            self.stored_problems[fingerprint] = PerformanceProblem(
-                type=type,
-                fingerprint=self._fingerprint(hash),
-                op=op,
-                desc=description,
-                cause_span_ids=[],
-                parent_span_ids=[],
-                offender_span_ids=spans_involved,
-            )
-
-    def is_creation_allowed_for_organization(self, organization: Optional[Organization]) -> bool:
-        return features.has("organizations:performance-slow-db-issue", organization, actor=None)
-
-    def is_creation_allowed_for_project(self, project: Optional[Project]) -> bool:
-        return True
-
-    @classmethod
-    def is_span_eligible(cls, span: Span) -> bool:
-        description = span.get("description", None)
-        if not description:
-            return False
-
-        description = description.strip()
-        if description[:6].upper() != "SELECT":
-            return False
-
-        if description.endswith("..."):
-            return False
-
-        return True
-
-    def _fingerprint(self, hash):
-        signature = (str(hash)).encode("utf-8")
-        full_fingerprint = hashlib.sha1(signature).hexdigest()
-        return f"1-{PerformanceSlowDBQueryGroupType.type_id}-{full_fingerprint}"
 
 
 class RenderBlockingAssetSpanDetector(PerformanceDetector):
