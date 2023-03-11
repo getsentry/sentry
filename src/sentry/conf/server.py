975c12de6a4a9d34bb0d42a9bd876d0983d526d5
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-03-10 17:22:08 -0800

    Remove auto_enable_codecov from celery queue

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -737,7 +737,6 @@ CELERY_QUEUES = [
         "dynamicsampling",
         routing_key="dynamicsampling",
     ),
-    Queue("auto_enable_codecov", routing_key="auto_enable_codecov"),
 ]
 
 for queue in CELERY_QUEUES:
