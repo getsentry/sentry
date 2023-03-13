faa1c08f805ff907db036ad6d8449414bdd2250c
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2023-03-13 11:37:00 +0100

    Change to Bias protocol

--- src/sentry/dynamic_sampling/rules/biases/boost_rare_transactions_rule.py
+++ src/sentry/dynamic_sampling/rules/biases/boost_rare_transactions_rule.py
@@ -1,59 +1,65 @@
 from typing import List
 
-from sentry.dynamic_sampling.rules.biases.base import Bias, BiasParams
+from sentry.dynamic_sampling.rules.biases.base import (
+    Bias,
+    BiasData,
+    BiasDataProvider,
+    BiasParams,
+    BiasRulesGenerator,
+)
 from sentry.dynamic_sampling.rules.helpers.prioritize_transactions import (
     get_transactions_resampling_rates,
 )
 from sentry.dynamic_sampling.rules.utils import RESERVED_IDS, PolymorphicRule, Rule, RuleType
-from sentry.models import Project
 
 
-def get_rules(project: Project, sample_rate: float) -> List[PolymorphicRule]:
-    proj_id = project.id
-    org_id = project.organization.id
-    transaction_map, rate = get_transactions_resampling_rates(
-        org_id=org_id, proj_id=proj_id, default_rate=sample_rate
-    )
-    ret_val: List[Rule] = []
+class RareTransactionsDataProvider(BiasDataProvider):
+    def get_bias_data(self, bias_params: BiasParams) -> BiasData:
+        return {"project": bias_params.project, "sample_rate": bias_params.base_sample_rate}
 
-    if len(transaction_map) == 0:
-        return ret_val  # no point returning any rules the project rule should take over
 
-    idx = 0
-    for name, transaction_rate in transaction_map.items():
-        # add a rule for each rebalanced transaction
-        ret_val.append(
-            {
-                "samplingValue": {
-                    "type": "sampleRate",
-                    "value": transaction_rate,
-                },
-                "type": "transaction",
-                "condition": {
-                    "op": "or",
-                    "inner": [
-                        {
-                            "op": "eq",
-                            "name": "event.transaction",
-                            "value": [name],
-                            "options": {"ignoreCase": True},
-                        }
-                    ],
-                },
-                "id": RESERVED_IDS[RuleType.BOOST_LOW_VOLUME_TRANSACTIONS] + idx,
-            }
+class RareTransactionsRulesGenerator(BiasRulesGenerator):
+    def _generate_bias_rules(self, bias_data: BiasData) -> List[PolymorphicRule]:
+        project = bias_data["project"]
+        sample_rate = bias_data["sample_rate"]
+        proj_id = project.id
+        org_id = project.organization.id
+        transaction_map, rate = get_transactions_resampling_rates(
+            org_id=org_id, proj_id=proj_id, default_rate=sample_rate
         )
-        idx += 1
-    return ret_val
+        ret_val: List[Rule] = []
+
+        if len(transaction_map) == 0:
+            return ret_val  # no point returning any rules the project rule should take over
+
+        idx = 0
+        for name, transaction_rate in transaction_map.items():
+            # add a rule for each rebalanced transaction
+            ret_val.append(
+                {
+                    "samplingValue": {
+                        "type": "sampleRate",
+                        "value": transaction_rate,
+                    },
+                    "type": "transaction",
+                    "condition": {
+                        "op": "or",
+                        "inner": [
+                            {
+                                "op": "eq",
+                                "name": "event.transaction",
+                                "value": [name],
+                                "options": {"ignoreCase": True},
+                            }
+                        ],
+                    },
+                    "id": RESERVED_IDS[RuleType.BOOST_LOW_VOLUME_TRANSACTIONS] + idx,
+                }
+            )
+            idx += 1
+        return ret_val
 
 
 class RareTransactionsRulesBias(Bias):
-    """
-    Useless adapter for Bias "infrastructure"
-    """
-
     def __init__(self) -> None:
-        pass
-
-    def get_rules(self, bias_params: BiasParams) -> List[PolymorphicRule]:
-        return get_rules(bias_params.project, bias_params.base_sample_rate)
+        super().__init__(RareTransactionsDataProvider, RareTransactionsRulesGenerator)
