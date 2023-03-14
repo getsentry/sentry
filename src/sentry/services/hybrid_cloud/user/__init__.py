8b48afe97219a429482684d91b412d8917a170b5
Mike Ihbe <mikejihbe@gmail.com>
2023-03-15 00:06:54 +0100

    ref(hybrid-cloud): Denormalize user avatars 1/3 (#45685)
    Denormalizing user avatars for hybrid cloud. This allows us to only run
    the filestore service on region silos and avoids cross region queries.
    
    Broken out from https://github.com/getsentry/sentry/pull/45115.
    
    Process will be:
    * Add migration and model changes. Dual write to new columns. <-- We are
    here
    * Backfill
    * Change serializer to new model
    
    ---------
    
    Co-authored-by: Mike Ihbe <mike.ihbe@sentry.io>

--- src/sentry/services/hybrid_cloud/user/__init__.py
+++ src/sentry/services/hybrid_cloud/user/__init__.py
@@ -113,6 +113,11 @@ class UserFilterArgs(TypedDict, total=False):
     emails: List[str]
 
 
+class UserUpdateArgs(TypedDict, total=False):
+    avatar_url: str
+    avatar_type: int
+
+
 class UserService(
     FilterQueryInterface[UserFilterArgs, RpcUser, UserSerializeType], InterfaceWithLifecycle
 ):
@@ -156,6 +161,16 @@ class UserService(
 
     @abstractmethod
     def get_by_actor_ids(self, *, actor_ids: List[int]) -> List[RpcUser]:
+        pass
+
+    @abstractmethod
+    def update_user(
+        self,
+        *,
+        user_id: int,
+        attrs: UserUpdateArgs,
+    ) -> Any:
+        # Returns a serialized user
         pass
 
     def get_user(self, user_id: int) -> Optional[RpcUser]:
