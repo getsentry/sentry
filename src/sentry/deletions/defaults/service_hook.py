28d6600567bf9d744c42a0bf33b26c40614a9dd0
Zach Collins <recursive.cookie.jar@gmail.com>
2023-03-14 13:06:02 -0700

    Update src/sentry/deletions/defaults/service_hook.py
    Co-authored-by: Mark Story <mark@mark-story.com>

--- src/sentry/deletions/defaults/service_hook.py
+++ src/sentry/deletions/defaults/service_hook.py
@@ -3,6 +3,6 @@ from ..base import ModelDeletionTask
 
 class ServiceHookDeletionTask(ModelDeletionTask):
     # This subclass just represents an intentional decision to not cascade service hook deletions, and to
-    # mark status using ObjectStatus on deletion.  The behavior is identical to the base class the task is explicit
-    # so that it is a clear intention.
+    # mark status using ObjectStatus on deletion.  The behavior is identical to the base class
+    # so that intentions are clear.
     pass
