3e7cbe354b072c62f460e3172f73a82b2d8f4d8b
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-03-13 21:48:53 -0400

    Update src/sentry/conf/server.py

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1239,7 +1239,7 @@ SENTRY_FEATURES = {
     # Enable consecutive db performance issue type
     "organizations:performance-consecutive-db-issue": False,
     # Enable consecutive http performance issue type
-    "organizations:performance-consecutive-http-issue": False,
+    "organizations:performance-consecutive-http-detector": False,
     # Enable slow DB performance issue type
     "organizations:performance-slow-db-issue": False,
     # Enable N+1 API Calls performance issue type
