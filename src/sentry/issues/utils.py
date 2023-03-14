9c5ccf59a8e21a344573baa79226fd0ad6d47d75
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2023-03-14 16:52:01 -0400

    add typing

--- src/sentry/issues/utils.py
+++ src/sentry/issues/utils.py
@@ -1,7 +1,13 @@
+from typing import Any, Mapping
+
 from sentry.issues.grouptype import GroupCategory, get_group_types_by_category
+from sentry.issues.issue_occurrence import IssueOccurrenceData
+from sentry.models import Project
 
 
-def skip_group_processing(occurrence, options, project):
+def skip_group_processing(
+    occurrence: IssueOccurrenceData, options: Mapping[str, Any], project: Project
+) -> bool:
     return (
         occurrence.type.type_id in get_group_types_by_category(GroupCategory.PERFORMANCE.value)
         and options.get("performance.issues.send_to_issues_platform", False)
