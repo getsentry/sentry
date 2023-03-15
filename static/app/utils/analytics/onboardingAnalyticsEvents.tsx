f00f9e44793360f3c1e83c542a873be4a6750aeb
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-03-15 12:37:11 +0100

    feat(onboarding): Add view sample error button new design - (#45820)

--- static/app/utils/analytics/onboardingAnalyticsEvents.tsx
+++ static/app/utils/analytics/onboardingAnalyticsEvents.tsx
@@ -3,15 +3,24 @@ export type OnboardingEventParameters = {
     from: string;
     to: string;
   };
-  'onboarding.explore_sentry_button_clicked': {};
+  'onboarding.explore_sentry_button_clicked': {
+    project_slug: string;
+  };
   'onboarding.first_error_processed': {
     new_organization: boolean;
+    project_slug: string;
   };
   'onboarding.first_error_received': {
     new_organization: boolean;
+    project_slug: string;
   };
   'onboarding.view_error_button_clicked': {
     new_organization: boolean;
+    project_slug: string;
+  };
+  'onboarding.view_sample_error_button_clicked': {
+    new_organization: boolean;
+    project_slug: string;
   };
 };
 
@@ -20,5 +29,7 @@ export const onboardingEventMap: Record<keyof OnboardingEventParameters, string>
   'onboarding.first_error_received': 'Onboarding: First Error Received',
   'onboarding.first_error_processed': 'Onboarding: First Error Processed',
   'onboarding.view_error_button_clicked': 'Onboarding: Go To Issues Button Clicked',
+  'onboarding.view_sample_error_button_clicked':
+    'Onboarding: View Sample Error Button Clicked',
   'onboarding.back_button_clicked': 'Onboarding: Back Button Clicked',
 };
