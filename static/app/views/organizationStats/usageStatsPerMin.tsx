90feab2bf85a3c5e0adc01bc6f78765f3764878e
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-03-15 15:44:21 -0700

    ✨ Add project to query for per minute stats

--- static/app/views/organizationStats/usageStatsPerMin.tsx
+++ static/app/views/organizationStats/usageStatsPerMin.tsx
@@ -10,6 +10,7 @@ import {formatUsageWithUnits, getFormatUsageOptions} from './utils';
 type Props = {
   dataCategory: DataCategoryInfo['plural'];
   organization: Organization;
+  projectIds: number[];
 } & AsyncComponent['props'];
 
 type State = {
@@ -37,10 +38,12 @@ class UsageStatsPerMin extends AsyncComponent<Props, State> {
   }
 
   get endpointQuery() {
+    const {projectIds} = this.props;
     return {
       statsPeriod: '5m', // Any value <1h will return current hour's data
       interval: '1m',
       groupBy: ['category', 'outcome'],
+      project: projectIds,
       field: ['sum(quantity)'],
     };
   }
