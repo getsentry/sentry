5b4764536313b7ecad3d27721ead102a57c85c55
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-03-17 13:11:17 -0700

    ref(hc): Introduce RpcActor for notifications (#45674)
    Introduce an RpcActor class to represent values with an actor_id,
    replacing several messy unions of User, Team, and/or RpcUser. This
    mainly affects the notifications module, where RpcActor represents
    recipients and participants.
    
    Beside making type annotations clearer, the main benefit is to prepare
    for situations where we won't want to pass a raw ORM model to a hybrid
    cloud service. It also means we don't have to worry about the more
    complex RpcUser object being hashable, where RpcActor replaces it.
    
    As supplemental refactoring to make the main task easier, introduce the
    ParticipantMap class to encapsulate `Mapping[ExternalProviders,
    Mapping[Team | User, int]]` and various near-equivalents.
    
    ---------
    
    Co-authored-by: Zachary Collins <zachary.collins@sentry.io>

--- src/sentry/notifications/notifications/organization_request/join_request.py
+++ src/sentry/notifications/notifications/organization_request/join_request.py
@@ -1,14 +1,10 @@
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
-
 from sentry.notifications.class_manager import register
+from sentry.services.hybrid_cloud.actor import RpcActor
 from sentry.types.integrations import ExternalProviders
 
 from .abstract_invite_request import AbstractInviteRequestNotification
-
-if TYPE_CHECKING:
-    from sentry.models import Team, User
 
 
 @register()
@@ -17,8 +13,8 @@ class JoinRequestNotification(AbstractInviteRequestNotification):
     metrics_key = "join_request"
     template_path = "sentry/emails/organization-join-request"
 
-    def build_attachment_title(self, recipient: Team | User) -> str:
+    def build_attachment_title(self, recipient: RpcActor) -> str:
         return "Request to Join"
 
-    def get_message_description(self, recipient: Team | User, provider: ExternalProviders) -> str:
+    def get_message_description(self, recipient: RpcActor, provider: ExternalProviders) -> str:
         return f"{self.pending_member.email} is requesting to join {self.organization.name}"
