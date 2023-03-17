516738969e4ed188e89f5dc4025a0e0ede54cc57
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-03-17 12:49:45 -0700

    Simplify RpcModelTest

--- tests/sentry/services/test_hybrid_cloud.py
+++ tests/sentry/services/test_hybrid_cloud.py
@@ -1,69 +1,17 @@
 from collections import deque
 
-from sentry.services.hybrid_cloud import (
-    RpcModel,
-    app,
-    auth,
-    identity,
-    integration,
-    log,
-    notifications,
-    organization,
-    organization_mapping,
-    tombstone,
-    user,
-    user_option,
-)
+from sentry.services.hybrid_cloud import RpcModel
 from sentry.testutils import TestCase
 
 
 class RpcModelTest(TestCase):
-    INTERFACE_CLASSES = frozenset(
-        [
-            app.RpcSentryApp,
-            app.RpcSentryAppComponent,
-            app.RpcSentryAppInstallation,
-            app.RpcSentryAppService,
-            auth.RpcAuthIdentity,
-            auth.RpcAuthProvider,
-            auth.RpcAuthProviderFlags,
-            auth.RpcAuthState,
-            auth.RpcMemberSsoState,
-            auth.RpcOrganizationAuthConfig,
-            identity.RpcIdentity,
-            identity.RpcIdentityProvider,
-            integration.RpcIntegration,
-            integration.RpcOrganizationIntegration,
-            log.AuditLogEvent,
-            log.UserIpEvent,
-            notifications.RpcNotificationSetting,
-            organization.RpcOrganization,
-            organization.RpcOrganizationFlags,
-            organization.RpcOrganizationInvite,
-            organization.RpcOrganizationMember,
-            organization.RpcOrganizationMemberFlags,
-            organization.RpcOrganizationSummary,
-            organization.RpcProject,
-            organization.RpcTeam,
-            organization.RpcTeamMember,
-            organization.RpcUserOrganizationContext,
-            organization_mapping.RpcOrganizationMapping,
-            tombstone.RpcTombstone,
-            user.RpcAuthenticator,
-            user.RpcAvatar,
-            user.RpcUser,
-            user.RpcUserEmail,
-            user_option.RpcUserOption,
-        ]
-    )
-
     def test_schema_generation(self):
-        for api_type in self.INTERFACE_CLASSES:
+        for api_type in self._get_rpc_model_subclasses():
             # We're mostly interested in whether an error occurs
             schema = api_type.schema_json()
             assert schema
 
-    def test_interface_class_coverage(self):
+    def _get_rpc_model_subclasses(self):
         subclasses = set()
         stack = deque([RpcModel])
         while stack:
@@ -73,5 +21,4 @@ class RpcModelTest(TestCase):
                 stack += next_class.__subclasses__()
 
         subclasses.remove(RpcModel)
-        uncovered = subclasses.difference(self.INTERFACE_CLASSES)
-        assert uncovered == set(), "RpcModel subclasses exist that are not tested"
+        return subclasses
