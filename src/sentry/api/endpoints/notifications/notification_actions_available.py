7ece876e76cdf8ddce631107bfe7153839aad22a
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-03-19 14:09:44 -0700

    ✨ Setup new registration

--- src/sentry/api/endpoints/notifications/notification_actions_available.py
+++ src/sentry/api/endpoints/notifications/notification_actions_available.py
@@ -1,5 +1,3 @@
-from collections import defaultdict
-
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -8,17 +6,17 @@ from sentry.api.bases.organization_flag import FlaggedOrganizationEndpoint
 from sentry.models.notificationaction import NotificationAction
 from sentry.models.organization import Organization
 
-# from sentry.services.hybrid_cloud.integration import integration_service
-
 
 @region_silo_endpoint
 class NotificationActionsAvailableEndpoint(FlaggedOrganizationEndpoint):
     feature_flags = ["organizations:notification-actions"]
 
     def get(self, request: Request, organization: Organization) -> Response:
-        payload = defaultdict(list)
-        # integrations = integration_service.get_integrations(organization_id=organization.id)
-        for _, trigger_text in NotificationAction.get_trigger_types():
-            payload[trigger_text].append({})
-
+        """
+        Responds with a payload serialized directly from running the 'descriptor' functions
+        in the NotificationAction registry.
+        """
+        payload = []
+        for registration in NotificationAction.get_registry().values():
+            payload.append(registration.serialize_available(organization=organization))
         return Response(payload)
