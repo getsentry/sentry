d87493608aaec148f52a1dbebe652152755a4d14
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-03-21 08:32:29 +0100

    feat(sourcemaps): Add sorting to old source maps endpoint (#46043)

--- src/sentry/api/endpoints/debug_files.py
+++ src/sentry/api/endpoints/debug_files.py
@@ -443,10 +443,16 @@ class SourceMapsEndpoint(ProjectEndpoint):
                 [expose_release(r, file_count_map.get(r["id"], 0)) for r in results], request.user
             )
 
+        sort_by = request.GET.get("sortBy", "-date_added")
+        if sort_by not in {"-date_added", "date_added"}:
+            return Response(
+                {"error": "You can either sort via 'date_added' or '-date_added'"}, status=400
+            )
+
         return self.paginate(
             request=request,
             queryset=queryset,
-            order_by="-date_added",
+            order_by=sort_by,
             paginator_cls=OffsetPaginator,
             default_per_page=10,
             on_results=serialize_results,
