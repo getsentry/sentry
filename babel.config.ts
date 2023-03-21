a1e4d1c2374c084fb9c3538d152be92c948b7c03
Scott Cooper <scttcper@gmail.com>
2023-03-21 13:32:58 -0700

    test(ui): Disable emotion sourcemaps for tests (#46141)

--- babel.config.ts
+++ babel.config.ts
@@ -54,8 +54,18 @@ const config: TransformOptions = {
       ],
     },
     test: {
-      // Required, see https://github.com/facebook/jest/issues/9430
-      plugins: ['dynamic-import-node'],
+      plugins: [
+        // Required, see https://github.com/facebook/jest/issues/9430
+        'dynamic-import-node',
+        // Disable emotion sourcemaps in tests
+        // Since emotion spends lots of time parsing and inserting sourcemaps
+        [
+          '@emotion/babel-plugin',
+          {
+            sourceMap: false,
+          },
+        ],
+      ],
     },
   },
 };
