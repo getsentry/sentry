d561da80a4962855eff24148bd26a3570dad5e2e
Jonas <jonas.badalic@sentry.io>
2023-03-21 16:28:30 -0400

    ref(profiling): remove the rest of exports (#46143)

--- static/app/components/events/viewHierarchy/wireframe.tsx
+++ static/app/components/events/viewHierarchy/wireframe.tsx
@@ -15,10 +15,8 @@ import {IconAdd, IconSubtract} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {Project} from 'sentry/types';
-import {
-  getCenterScaleMatrixFromConfigPosition,
-  Rect,
-} from 'sentry/utils/profiling/gl/utils';
+import {getCenterScaleMatrixFromConfigPosition} from 'sentry/utils/profiling/gl/utils';
+import {Rect} from 'sentry/utils/profiling/speedscope';
 
 const MIN_BORDER_SIZE = 20;
 const MOUSE_DRAG_THRESHOLD = 3;
