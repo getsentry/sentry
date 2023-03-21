f2855681cf908b23ff240c75fa83387b88db7d45
Alberto Leal <mail4alberto@gmail.com>
2023-03-21 17:01:47 -0400

    fix(inbound-filters): Ensure legacy browser subfilters are always a set (#46146)

--- tests/sentry/api/endpoints/test_project_filter_details.py
+++ tests/sentry/api/endpoints/test_project_filter_details.py
@@ -22,3 +22,40 @@ class ProjectFilterDetailsTest(APITestCase):
         )
 
         assert project.get_option("filters:browser-extensions") == "1"
+
+    def test_put_legacy_browsers(self):
+        org = self.create_organization(name="baz", slug="1", owner=self.user)
+        team = self.create_team(organization=org, name="foo", slug="foo")
+        project = self.create_project(name="Bar", slug="bar", teams=[team])
+
+        project.update_option(
+            "filters:legacy-browsers",
+            [
+                "ie10",
+                "ie9",
+                "android_pre_4",
+                "ie_pre_9",
+                "opera_pre_15",
+                "safari_pre_6",
+                "ie11",
+                "opera_mini_pre_8",
+            ],
+        )
+
+        new_subfilters = [
+            "safari_pre_6",
+            "opera_mini_pre_8",
+            "ie10",
+            "ie11",
+            "opera_pre_15",
+        ]
+
+        self.get_success_response(
+            org.slug,
+            project.slug,
+            "legacy-browsers",
+            subfilters=new_subfilters,
+            status_code=201,
+        )
+
+        assert set(project.get_option("filters:legacy-browsers")) == set(new_subfilters)
