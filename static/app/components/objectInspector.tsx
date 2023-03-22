e14f52c0e36079733640e0d3a10ac20090bddf3b
Billy Vong <billy@sentry.io>
2023-03-22 16:27:08 -0400

    refactor things

--- static/app/components/objectInspector.tsx
+++ static/app/components/objectInspector.tsx
@@ -1,24 +1,39 @@
-import {ComponentPropsWithoutRef, useMemo} from 'react';
+import {ComponentProps, useMemo} from 'react';
 import {
   chromeDark,
   chromeLight,
   ObjectInspector as OrigObjectInspector,
 } from 'react-inspector';
+import {useTheme} from '@emotion/react';
 
 import ConfigStore from 'sentry/stores/configStore';
+import {useLegacyStore} from 'sentry/stores/useLegacyStore';
 
-type Props = Omit<ComponentPropsWithoutRef<typeof OrigObjectInspector>, 'theme'>;
+type Props = Omit<ComponentProps<typeof ReactObjectInspector>, 'theme'> & {
+  theme?: Record<string, any>;
+};
 
-function ObjectInspector({data}: Props) {
-  const isDark = ConfigStore.get('theme') === 'dark';
+function ObjectInspector({data, theme, ...props}: Props) {
+  const config = useLegacyStore(ConfigStore);
+  const emotionTheme = useTheme();
+  const isDark = config.theme === 'dark';
 
   const INSPECTOR_THEME = useMemo(
     () => ({
       ...(isDark ? chromeDark : chromeLight),
+
+      // Reset some theme values
+      BASE_COLOR: 'inherit',
+      ERROR_COLOR: emotionTheme.red400,
+      TREENODE_FONT_FAMILY: 'inherit',
+      TREENODE_FONT_SIZE: 'inherit',
+      TREENODE_LINE_HEIGHT: 'inherit',
       BASE_BACKGROUND_COLOR: 'none',
+
       OBJECT_PREVIEW_OBJECT_MAX_PROPERTIES: 1,
+      ...theme,
     }),
-    [isDark]
+    [isDark, theme, emotionTheme.red400]
   );
 
   return (
@@ -26,6 +41,7 @@ function ObjectInspector({data}: Props) {
       data={data}
       // @ts-expect-error
       theme={INSPECTOR_THEME}
+      {...props}
     />
   );
 }
--- static/app/views/replays/detail/console/messageFormatter.tsx
+++ static/app/views/replays/detail/console/messageFormatter.tsx
@@ -1,124 +1,61 @@
-import {Fragment} from 'react';
-import {chromeLight, ObjectInspector} from 'react-inspector';
+import isObject from 'lodash/isObject';
 
 import {AnnotatedText} from 'sentry/components/events/meta/annotatedText';
 import {getMeta} from 'sentry/components/events/meta/metaProxy';
 import {BreadcrumbTypeDefault, Crumb} from 'sentry/types/breadcrumbs';
 import {objectIsEmpty} from 'sentry/utils';
 
+import Format from './format';
+
 interface Props {
   breadcrumb: Extract<Crumb, BreadcrumbTypeDefault>;
   onDimensionChange?: () => void;
 }
 
-const formatRegExp = /%[sdj%]/g;
-function isObject(arg) {
-  return typeof arg === 'object' && arg !== null;
-}
-function isNull(arg) {
-  return arg === null;
-}
-const format = function (onDimensionChange, ...args) {
-  const INSPECTOR_THEME = {
-    ...chromeLight,
-    BASE_BACKGROUND_COLOR: 'none',
-    OBJECT_PREVIEW_OBJECT_MAX_PROPERTIES: 1,
-  };
-
-  const f = args[0];
-  if (typeof f !== 'string') {
-    const objects: any[] = [];
-    for (let i = 0; i < arguments.length; i++) {
-      objects.push(
-        <ObjectInspector
-          key={i}
-          data={arguments[i]}
-          // @ts-expect-error
-          theme={INSPECTOR_THEME}
-          onExpand={onDimensionChange}
-        />
-      );
-    }
-    return <Fragment>{objects}</Fragment>;
-  }
-
-  let i = 1;
-  const len = args.length;
-  const pieces: any[] = [];
-
-  // @ts-expect-error
-  const str = String(f).replace(formatRegExp, function (x) {
-    if (x === '%%') {
-      return '%';
-    }
-    if (i >= len) {
-      return x;
-    }
-    switch (x) {
-      case '%s':
-        return String(args[i++]);
-      case '%d':
-        return Number(args[i++]);
-      case '%j':
-        try {
-          return JSON.stringify(args[i++]);
-        } catch (_) {
-          return '[Circular]';
-        }
-      default:
-        return x;
-    }
-  });
-  pieces.push(str);
-  for (let x = args[i]; i < len; x = args[++i]) {
-    if (isNull(x) || !isObject(x)) {
-      pieces.push(' ' + x);
-    } else {
-      pieces.push(' ');
-      pieces.push(
-        <ObjectInspector
-          data={x}
-          // @ts-expect-error
-          theme={INSPECTOR_THEME}
-          onExpand={onDimensionChange}
-        />
-      );
-    }
-  }
-  return <Fragment>{pieces}</Fragment>;
-};
-
 /**
  * Attempt to emulate the browser console as much as possible
  */
 function MessageFormatter({breadcrumb, onDimensionChange}: Props) {
-  let logMessage: any = '';
+  let args = breadcrumb.data?.arguments;
 
-  if (!breadcrumb.data?.arguments) {
+  if (!args) {
     // There is a possibility that we don't have arguments as we could be receiving an exception type breadcrumb.
     // In these cases we just need the message prop.
 
     // There are cases in which our prop message is an array, we want to force it to become a string
-    logMessage = breadcrumb.message?.toString() || '';
-    return <AnnotatedText meta={getMeta(breadcrumb, 'message')} value={logMessage} />;
+    return (
+      <AnnotatedText
+        meta={getMeta(breadcrumb, 'message')}
+        value={breadcrumb.message?.toString() || ''}
+      />
+    );
   }
 
   // There is a special case where `console.error()` is called with an Error object.
   // The SDK uses the Error's `message` property as the breadcrumb message, but we lose the Error type,
-  // resulting in an empty object in the breadcrumb arguments. In this case, we
-  // only want to use `breadcrumb.message`.
-  if (
+  // resulting in an empty object in the breadcrumb arguments.
+  //
+  // In this special case, we re-create the error object
+  const isSerializedError =
     breadcrumb.message &&
-    breadcrumb.data?.arguments.length === 1 &&
-    isObject(breadcrumb.data.arguments[0]) &&
-    objectIsEmpty(breadcrumb.data.arguments[0])
-  ) {
-    logMessage = breadcrumb.message;
-  } else {
-    logMessage = format(onDimensionChange, ...breadcrumb.data.arguments);
+    typeof breadcrumb.message === 'string' &&
+    args.length <= 2 &&
+    isObject(args[0]) &&
+    objectIsEmpty(args[0]);
+
+  if (isSerializedError && breadcrumb.message) {
+    // Turn this back into an Error object so <Format> can pretty print it
+    const fakeError = new Error(breadcrumb.message.split('\n')[0].trim());
+
+    try {
+      fakeError.stack = args[1];
+    } catch {
+      // Some browsers won't allow you to write to stack property}
+    }
+    args = [fakeError];
   }
 
-  return logMessage;
+  return <Format onExpand={onDimensionChange} args={args} />;
 }
 
 export default MessageFormatter;
