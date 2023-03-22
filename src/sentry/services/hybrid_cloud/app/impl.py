674ee9e55ffcb28f7bd5f8de4876947df3e1540d
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-03-22 12:41:00 -0700

    Coerce UUID to string in find_app_components

--- src/sentry/services/hybrid_cloud/app/impl.py
+++ src/sentry/services/hybrid_cloud/app/impl.py
@@ -45,7 +45,7 @@ class DatabaseBackedAppService(AppService):
     def find_app_components(self, *, app_id: int) -> List[RpcSentryAppComponent]:
         return [
             RpcSentryAppComponent(
-                uuid=c.uuid,
+                uuid=str(c.uuid),
                 sentry_app_id=c.sentry_app_id,
                 type=c.type,
                 app_schema=c.schema,
