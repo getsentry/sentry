3d80d9470759d37e300ab55692e919b491ce8826
JonasBa <jonas@badalic.com>
2023-03-23 15:40:53 -0400

    ref(profiling): drop setHighlightedFrames

--- static/app/components/profiling/flamegraph/flamegraph.tsx
+++ static/app/components/profiling/flamegraph/flamegraph.tsx
@@ -9,6 +9,7 @@ import {
 } from 'react';
 import * as Sentry from '@sentry/react';
 import {mat3, vec2} from 'gl-matrix';
+import {FlamegraphSearch as FlamegraphSearchType} from 'static/app/utils/profiling/flamegraph/flamegraphStateProvider/reducers/flamegraphSearch';
 
 import {ProfileDragDropImport} from 'sentry/components/profiling/flamegraph/flamegraphOverlays/profileDragDropImport';
 import {FlamegraphOptionsMenu} from 'sentry/components/profiling/flamegraph/flamegraphToolbar/flamegraphOptionsMenu';
@@ -33,9 +34,9 @@ import {
 } from 'sentry/utils/profiling/canvasScheduler';
 import {CanvasView} from 'sentry/utils/profiling/canvasView';
 import {Flamegraph as FlamegraphModel} from 'sentry/utils/profiling/flamegraph';
-import {FlamegraphProfiles} from 'sentry/utils/profiling/flamegraph/flamegraphStateProvider/reducers/flamegraphProfiles';
 import {useFlamegraphPreferences} from 'sentry/utils/profiling/flamegraph/hooks/useFlamegraphPreferences';
 import {useFlamegraphProfiles} from 'sentry/utils/profiling/flamegraph/hooks/useFlamegraphProfiles';
+import {useFlamegraphSearch} from 'sentry/utils/profiling/flamegraph/hooks/useFlamegraphSearch';
 import {useDispatchFlamegraphState} from 'sentry/utils/profiling/flamegraph/hooks/useFlamegraphState';
 import {useFlamegraphZoomPosition} from 'sentry/utils/profiling/flamegraph/hooks/useFlamegraphZoomPosition';
 import {useFlamegraphTheme} from 'sentry/utils/profiling/flamegraph/useFlamegraphTheme';
@@ -126,7 +127,7 @@ type FlamegraphCandidate = {
 
 function findLongestMatchingFrame(
   flamegraph: FlamegraphModel,
-  focusFrame: FlamegraphProfiles['highlightFrames']
+  focusFrame: FlamegraphSearchType['highlightFrames']
 ): FlamegraphFrame | null {
   if (focusFrame === null) {
     return null;
@@ -174,7 +175,8 @@ function Flamegraph(): ReactElement {
   const position = useFlamegraphZoomPosition();
   const profiles = useFlamegraphProfiles();
   const {colorCoding, sorting, view} = useFlamegraphPreferences();
-  const {threadId, selectedRoot, highlightFrames} = useFlamegraphProfiles();
+  const {highlightFrames} = useFlamegraphSearch();
+  const {threadId, selectedRoot} = useFlamegraphProfiles();
 
   const [flamegraphCanvasRef, setFlamegraphCanvasRef] =
     useState<HTMLCanvasElement | null>(null);
