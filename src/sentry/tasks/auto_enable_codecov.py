65497ce76038800f6a219c7b37b2b288bc5e368e
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-03-23 14:08:50 -0700

    fix(codecov-v2): Add more logging and increase schedule frequency (#46283)
    Increase the schedule frequency for ease of debugging. This will be
    modified again before we release.
    
    WOR-2728

--- src/sentry/tasks/auto_enable_codecov.py
+++ src/sentry/tasks/auto_enable_codecov.py
@@ -22,15 +22,23 @@ def schedule_organizations(dry_run=False) -> None:
     Note that this is not gated by the V2 flag so we can enable the V2
     features independently of the auto-enablement.
     """
+    logger.info("Starting task for sentry.tasks.auto_enable_codecov.schedule_organizations")
+
     organizations = Organization.objects.filter(status=OrganizationStatus.ACTIVE)
+    logger.info(f"Processing {len(organizations)} organizations for codecov auto-enable")
     for _, organization in enumerate(
         RangeQuerySetWrapper(organizations, step=1000, result_value_getter=lambda item: item.id)
     ):
-
         if not features.has("organizations:codecov-stacktrace-integration", organization):
+            logger.warning(
+                f"Skipping {organizations.id}: organizations:codecov-stacktrace-integration is False"
+            )
             continue
 
         if not features.has("organizations:auto-enable-codecov", organization):
+            logger.warning(
+                f"Processing {len(organizations)}: organizations:auto-enable-codecov is False"
+            )
             continue
 
         # Create a celery task per organization
@@ -41,7 +49,7 @@ def schedule_organizations(dry_run=False) -> None:
 @instrumented_task(  # type: ignore
     name="sentry.tasks.auto_enable_codecov.enable_for_organization",
     queue="auto_enable_codecov",
-    max_retries=0,
+    max_retries=5,
 )
 def enable_for_organization(organization_id: int, dry_run=False) -> None:
     """
@@ -52,11 +60,13 @@ def enable_for_organization(organization_id: int, dry_run=False) -> None:
         organization = Organization.objects.get(id=organization_id)
         has_integration, _ = has_codecov_integration(organization)
         if not has_integration:
-            logger.info(f"No codecov integration exists for organization {organization_id}")
+            logger.warning(f"No codecov integration exists for organization {organization_id}")
             return
 
         if organization.flags.codecov_access.is_set:
-            logger.info(f"Codecov Access flag already set to {organization.flags.codecov_access}")
+            logger.warning(
+                f"Codecov Access flag already set to {organization.flags.codecov_access}"
+            )
             return
 
         organization.flags.codecov_access = True
