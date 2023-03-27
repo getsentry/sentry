f49caf5ce074b89b597ea4b2305b62de9a4e30ed
Colton Allen <colton.allen@sentry.io>
2023-03-27 12:36:39 -0500

    Remove redundant usage of bytes function

--- src/sentry/replays/usecases/ingest/dom_index.py
+++ src/sentry/replays/usecases/ingest/dom_index.py
@@ -84,7 +84,7 @@ def create_replay_actions_event(
         "replay_id": replay_id,
         "project_id": project_id,
         "retention_days": retention_days,
-        "payload": list(bytes(json.dumps(payload).encode())),  # type: ignore
+        "payload": list(json.dumps(payload).encode()),  # type: ignore
     }
 
 
