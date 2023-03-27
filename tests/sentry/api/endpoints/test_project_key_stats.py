0f23a83b497d83e6673f9976a63ad04103eb509f
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2023-03-27 11:54:46 -0700

    test: skip test_project_key_stats->test_date_conditions due to flakiness

--- tests/sentry/api/endpoints/test_project_key_stats.py
+++ tests/sentry/api/endpoints/test_project_key_stats.py
@@ -1,4 +1,5 @@
 import freezegun
+import pytest
 
 from sentry.constants import DataCategory
 from sentry.models import ProjectKey
@@ -126,6 +127,7 @@ class ProjectKeyStatsTest(OutcomesSnubaTest, SnubaTestCase, APITestCase):
         response = self.client.get(url)
         assert response.status_code == 400
 
+    @pytest.mark.skip(reason="flakey: https://github.com/getsentry/sentry/issues/46402")
     def test_date_conditions(self):
         self.store_outcomes(
             {
