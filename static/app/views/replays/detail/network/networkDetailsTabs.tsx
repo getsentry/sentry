ef0c3eacb9e9808941a6cd5b823975355845726b
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-03-28 06:11:16 -0700

    feat(replays): Add UI to display network request details (#46219)
    New 'Network details' panel. Figma per
    [figma](https://www.figma.com/file/SVyNvm8p17xGfIC7r7l2YS/Specs%3A-Network-Request-Details?node-id=4%3A7443&t=VjNMXfMJQKHfj0ks-0)
    specs
    
    There are a few states so observe in the screen shots, and some followup
    tasks
    
    Note that:
    - You can only open the details panel for xhr & fetch network types
    - The data is a string that's run through the pii scrubber, we try to
    parse it with `JSON.parse`, but if that fails you'll just get a string
    
    
    
    
    | State | Img |
    | --- | --- |
    | Click a network row to open the Details. Notice that the row you
    clicked is Bold. The lower area with the data is scrollable, strings or
    objects will display |
    ![SCR-20230323-km4](https://user-images.githubusercontent.com/187460/227372507-b093de12-66df-4383-a46f-650a488c7821.png)
    |
    | The X button will close the panel |
    ![SCR-20230323-kmb](https://user-images.githubusercontent.com/187460/227372508-18954b0a-c593-4aa8-9eca-49a99f191685.png)
    |
    | Click+drag (don't click on the tabs or the X button) to make the panel
    larger/smaller |
    ![SCR-20230323-kmd](https://user-images.githubusercontent.com/187460/227372509-a3318199-01ef-486d-a00b-ece5008ca465.png)
    |
    
    
    
    
    
    
    Related to https://github.com/getsentry/sentry-javascript/pull/7589
    
    Fixes #46054

---
+++ static/app/views/replays/detail/network/networkDetailsTabs.tsx
@@ -0,0 +1,45 @@
+import queryString from 'query-string';
+
+import ListLink from 'sentry/components/links/listLink';
+import ScrollableTabs from 'sentry/components/replays/scrollableTabs';
+import {t} from 'sentry/locale';
+import {useLocation} from 'sentry/utils/useLocation';
+import useUrlParams from 'sentry/utils/useUrlParams';
+
+type Props = {
+  className?: string;
+  underlined?: boolean;
+};
+
+const TABS = {
+  request: t('Request'),
+  response: t('Response'),
+};
+
+export type TabKey = keyof typeof TABS;
+
+function NetworkRequestTabs({className, underlined = true}: Props) {
+  const {pathname, query} = useLocation();
+  const {getParamValue, setParamValue} = useUrlParams('n_details_tab', 'request');
+  const activeTab = getParamValue();
+
+  return (
+    <ScrollableTabs className={className} underlined={underlined}>
+      {Object.entries(TABS).map(([tab, label]) => (
+        <ListLink
+          key={tab}
+          isActive={() => tab === activeTab}
+          to={`${pathname}?${queryString.stringify({...query, t_main: tab})}`}
+          onClick={e => {
+            e.preventDefault();
+            setParamValue(tab);
+          }}
+        >
+          {label}
+        </ListLink>
+      ))}
+    </ScrollableTabs>
+  );
+}
+
+export default NetworkRequestTabs;
