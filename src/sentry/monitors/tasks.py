002c4c9f7508aa3e8a90a9a7a30c2478520ae2e6
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-03-29 10:28:46 -0700

    more logic

--- src/sentry/monitors/tasks.py
+++ src/sentry/monitors/tasks.py
@@ -8,7 +8,6 @@ from sentry.utils import metrics
 
 from .models import (
     CheckInStatus,
-    Monitor,
     MonitorCheckIn,
     MonitorEnvironment,
     MonitorFailure,
@@ -39,30 +38,8 @@ def check_monitors(current_datetime=None):
     if current_datetime is None:
         current_datetime = timezone.now()
 
-    qs = Monitor.objects.filter(
-        type__in=[MonitorType.CRON_JOB], next_checkin__lt=current_datetime
-    ).exclude(
-        status__in=[
-            MonitorStatus.DISABLED,
-            MonitorStatus.PENDING_DELETION,
-            MonitorStatus.DELETION_IN_PROGRESS,
-        ]
-    )[
-        :MONITOR_LIMIT
-    ]
-    metrics.gauge("sentry.monitors.tasks.check_monitors.missing_count", qs.count())
-    for monitor in qs:
-        logger.info("monitor.missed-checkin", extra={"monitor_id": monitor.id})
-        # add missed checkin
-        checkin = MonitorCheckIn.objects.create(
-            project_id=monitor.project_id,
-            monitor=monitor,
-            status=CheckInStatus.MISSED,
-        )
-        monitor.mark_failed(reason=MonitorFailure.MISSED_CHECKIN)
-
     qs = MonitorEnvironment.objects.filter(
-        type__in=[MonitorType.CRON_JOB], next_checkin__lt=current_datetime
+        monitor__type__in=[MonitorType.CRON_JOB], next_checkin__lt=current_datetime
     ).exclude(
         status__in=[
             MonitorStatus.DISABLED,
@@ -73,15 +50,17 @@ def check_monitors(current_datetime=None):
         :MONITOR_LIMIT
     ]
     metrics.gauge("sentry.monitors.tasks.check_monitors.missing_count", qs.count())
-    for monitor in qs:
-        logger.info("monitor.missed-checkin", extra={"monitor_id": monitor.id})
+    for monitor_environment in qs:
+        logger.info(
+            "monitor.missed-checkin", extra={"monitor_environment_id": monitor_environment.id}
+        )
         # add missed checkin
         checkin = MonitorCheckIn.objects.create(
-            project_id=monitor.project_id,
-            monitor=monitor,
+            project_id=monitor_environment.monitor.project_id,
+            monitor=monitor_environment,
             status=CheckInStatus.MISSED,
         )
-        monitor.mark_failed(reason=MonitorFailure.MISSED_CHECKIN)
+        monitor_environment.mark_failed(reason=MonitorFailure.MISSED_CHECKIN)
 
     qs = MonitorCheckIn.objects.filter(status=CheckInStatus.IN_PROGRESS).select_related("monitor")[
         :CHECKINS_LIMIT
@@ -93,9 +72,10 @@ def check_monitors(current_datetime=None):
         if checkin.date_updated > current_datetime - timeout:
             continue
 
-        monitor = checkin.monitor
+        monitor_environment = checkin.monitor_environment
         logger.info(
-            "monitor.checkin-timeout", extra={"monitor_id": monitor.id, "checkin_id": checkin.id}
+            "monitor.checkin-timeout",
+            extra={"monitor_environment_id": monitor_environment.id, "checkin_id": checkin.id},
         )
         affected = MonitorCheckIn.objects.filter(
             id=checkin.id, status=CheckInStatus.IN_PROGRESS
@@ -106,9 +86,9 @@ def check_monitors(current_datetime=None):
         # we only mark the monitor as failed if a newer checkin wasn't responsible for the state
         # change
         has_newer_result = MonitorCheckIn.objects.filter(
-            monitor=monitor.id,
+            monitor_environment=monitor_environment.id,
             date_added__gt=checkin.date_added,
             status__in=[CheckInStatus.OK, CheckInStatus.ERROR],
         ).exists()
         if not has_newer_result:
-            monitor.mark_failed(reason=MonitorFailure.DURATION)
+            monitor_environment.mark_failed(reason=MonitorFailure.DURATION)
