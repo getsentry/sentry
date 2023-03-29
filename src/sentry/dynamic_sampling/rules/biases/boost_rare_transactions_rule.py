b173d8ba37d3d6e782f8dd3ad56a61bda56e572e
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2023-03-29 17:35:06 +0200

    add missing prioritise transactions rule for implicit transactions

--- src/sentry/dynamic_sampling/rules/biases/boost_rare_transactions_rule.py
+++ src/sentry/dynamic_sampling/rules/biases/boost_rare_transactions_rule.py
@@ -12,7 +12,7 @@ class RareTransactionsRulesBias(Bias):
     def generate_rules(self, project: Project, base_sample_rate: float) -> List[PolymorphicRule]:
         proj_id = project.id
         org_id = project.organization.id
-        transaction_map, rate = get_transactions_resampling_rates(
+        transaction_map, implicit_rate = get_transactions_resampling_rates(
             org_id=org_id, proj_id=proj_id, default_rate=base_sample_rate
         )
         ret_val: List[Rule] = []
@@ -47,5 +47,21 @@ class RareTransactionsRulesBias(Bias):
                 }
             )
             idx += 1
+        if base_sample_rate != 0:
+            rate = implicit_rate / base_sample_rate
+            ret_val.append(
+                {
+                    "samplingValue": {
+                        "type": "factor",
+                        "value": rate,
+                    },
+                    "type": "transaction",
+                    "condition": {
+                        "op": "and",
+                        "inner": [],
+                    },
+                    "id": RESERVED_IDS[RuleType.BOOST_LOW_VOLUME_TRANSACTIONS] + idx,
+                }
+            )
 
         return ret_val
