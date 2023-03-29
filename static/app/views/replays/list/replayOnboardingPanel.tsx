0d6f3f130df7d0c1ee0de32922acc75956a14fcb
Elias Hussary <elias.hussary@sentry.io>
2023-03-29 15:47:29 -0400

    ref: handle create project messaging

--- static/app/views/replays/list/replayOnboardingPanel.tsx
+++ static/app/views/replays/list/replayOnboardingPanel.tsx
@@ -83,8 +83,9 @@ export default function ReplayOnboardingPanel() {
       {hasSelectedProjects && allSelectedProjectsUnsupported && (
         <Alert icon={<IconInfo />}>
           {tct(
-            `[projectMsg] Select a project using our [link], or equivalent framework SDK.`,
+            `[projectMsg] [action] a project using our [link], or equivalent framework SDK.`,
             {
+              action: primaryAction === 'create' ? t('Create') : t('Select'),
               projectMsg: (
                 <strong>
                   {t(`Session Replay isn't available for %s.`, selectedProjects[0].slug)}
