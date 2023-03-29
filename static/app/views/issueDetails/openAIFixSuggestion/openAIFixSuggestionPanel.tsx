102929d9b42d2fc99b6afc4d744fcffda7ebbaba
Matej Minar <matej.minar@sentry.io>
2023-03-29 14:22:18 +0200

    feat(ai): Add loading indicator

--- static/app/views/issueDetails/openAIFixSuggestion/openAIFixSuggestionPanel.tsx
+++ static/app/views/issueDetails/openAIFixSuggestion/openAIFixSuggestionPanel.tsx
@@ -1,12 +1,13 @@
 import {Fragment, useCallback, useEffect, useState} from 'react';
 import styled from '@emotion/styled';
 
+import aiLoaderImg from 'sentry-images/spot/ai-loader.gif';
+
 import {addSuccessMessage} from 'sentry/actionCreators/indicator';
 import {Button} from 'sentry/components/button';
 import FeatureBadge from 'sentry/components/featureBadge';
 import {feedbackClient} from 'sentry/components/featureFeedback/feedbackModal';
 import LoadingError from 'sentry/components/loadingError';
-import LoadingIndicator from 'sentry/components/loadingIndicator';
 import {Panel, PanelBody, PanelFooter, PanelHeader} from 'sentry/components/panels';
 import {IconHappy, IconMeh, IconSad} from 'sentry/icons';
 import {IconChevron} from 'sentry/icons/iconChevron';
@@ -17,6 +18,7 @@ import marked from 'sentry/utils/marked';
 import {useQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 import useRouter from 'sentry/utils/useRouter';
+import {AiLoaderMessage} from 'sentry/views/issueDetails/openAIFixSuggestion/aiLoaderMessage';
 import {useCustomerPolicies} from 'sentry/views/issueDetails/openAIFixSuggestion/useCustomerPolicies';
 import {useOpenAISuggestionLocalStorage} from 'sentry/views/issueDetails/openAIFixSuggestion/useOpenAISuggestionLocalStorage';
 import {experimentalFeatureTooltipDesc} from 'sentry/views/issueDetails/openAIFixSuggestion/utils';
@@ -115,7 +117,13 @@ export function OpenAIFixSuggestionPanel({eventID, projectSlug}: Props) {
         <Fragment>
           <PanelBody withPadding>
             {dataIsLoading ? (
-              <LoadingIndicator />
+              <AiLoaderWrapper>
+                <AiLoader
+                  src={aiLoaderImg}
+                  alt={t('Loading indicator, Sentry spinning wheel')}
+                />
+                <AiLoaderMessage />
+              </AiLoaderWrapper>
             ) : dataIsError ? (
               <LoadingErrorWithoutMarginBottom onRetry={dataRefetch} />
             ) : (
@@ -178,7 +186,7 @@ export function OpenAIFixSuggestionPanel({eventID, projectSlug}: Props) {
 
 const FixSuggestionPanel = styled(Panel)`
   margin-top: ${space(1.5)};
-  margin-bottom: 0;
+  margin-bottom: ${space(1.5)};
   overflow: hidden;
 `;
 
@@ -206,4 +214,15 @@ const LoadingErrorWithoutMarginBottom = styled(LoadingError)`
 
 const FeatureBadgeNotUppercase = styled(FeatureBadge)`
   text-transform: capitalize;
+`;
+
+const AiLoaderWrapper = styled('div')`
+  text-align: center;
+  padding-bottom: ${space(3)};
+`;
+
+const AiLoader = styled('img')`
+  max-width: 350px;
+  margin-top: -30px;
+  margin-bottom: -45px;
 `;
