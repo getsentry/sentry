c4cc7195683dec20cc2a702917b307ee9349006d
Scott Cooper <scttcper@gmail.com>
2023-03-29 14:16:39 -0700

    rename production to ci:master

--- jest.config.ts
+++ jest.config.ts
@@ -279,7 +279,7 @@ const config: Config.InitialOptions = {
         // jest project under Sentry organization (dev productivity team)
         dsn: 'https://3fe1dce93e3a4267979ebad67f3de327@sentry.io/4857230',
         // Use production env to reduce sampling of commits on master
-        environment: CI ? (IS_MASTER_BRANCH ? 'production' : 'ci') : 'local',
+        environment: CI ? (IS_MASTER_BRANCH ? 'ci:master' : 'ci:pull_request') : 'local',
         tracesSampleRate: CI ? 1 : 0.5,
         profilesSampleRate: 0.1,
         transportOptions: {keepAlive: true},
