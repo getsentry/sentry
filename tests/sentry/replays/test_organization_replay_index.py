6d5185846d939bb5a35a48ef5cab9c7cd61b7972
Colton Allen <colton.allen@sentry.io>
2023-03-31 15:07:21 -0500

    Add coverage for no-quotes and single quotes are attribute selectors

--- tests/sentry/replays/test_organization_replay_index.py
+++ tests/sentry/replays/test_organization_replay_index.py
@@ -922,7 +922,12 @@ class OrganizationReplayIndexTest(APITestCase, ReplaysSnubaTestCase):
                 "replay_click.textContent:Hello",
                 "replay_click.title:MyTitle",
                 "replay_click.selector:div#myid",
+                # No quotes around button.
+                "replay_click.selector:div[role=button]",
+                # Single quotes around button.
+                "replay_click.selector:div[role='button']",
                 "replay_click.selector:div#myid.class1.class2",
+                "replay_click.selector:div#myid.class1.class2[role=button][aria-label='AriaLabel']",
             ]
             for query in queries:
                 response = self.client.get(self.url + f"?field=id&query={query}")
