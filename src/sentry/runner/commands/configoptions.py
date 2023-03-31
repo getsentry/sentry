e8dca5c4a17d9cc7fe5557e9895607c4340c723d
Kyle Owens <owensk@gmail.com>
2023-03-31 10:33:25 -0700

    Using returns, better click.echo w/ more information.

--- src/sentry/runner/commands/configoptions.py
+++ src/sentry/runner/commands/configoptions.py
@@ -3,7 +3,7 @@ import click
 from sentry.runner.decorators import configuration
 
 
-def create_key_value_generator(data, newline_separator, kv_separator):
+def create_key_value_generator(data: str, newline_separator: str, kv_separator: str):
     return (line.split(kv_separator) for line in data.split(newline_separator) if line)
 
 
@@ -15,7 +15,7 @@ def configoptions():
 @configoptions.command()
 @click.option("--key", "-k", required=True, help="The key of the option to fetch.")
 @configuration
-def fetch(key):
+def fetch(key: str):
     "Fetches the option for the given key."
     click.echo(get(key))
 
@@ -41,7 +41,7 @@ def list():
     help="Output exactly what changes would be made and in which order.",
 )
 @configuration
-def patch(filename, dryrun):
+def patch(filename: str, dryrun: bool):
     "Updates, gets, and deletes options that are each subsectioned in the given file."
     import yaml
 
@@ -80,7 +80,7 @@ def patch(filename, dryrun):
     help="Output exactly what changes would be made and in which order.",
 )
 @configuration
-def strict(filename, dryrun):
+def strict(filename: str, dryrun: bool) -> bool:
     "Deletes everything not in the uploaded file, and applies all of the changes in the file."
     import yaml
 
@@ -97,8 +97,11 @@ def strict(filename, dryrun):
         db_keys = (opt.name for opt in options.all())
         for key in db_keys:
             if key not in kv_generator:
-                click.echo(delete(key, dryrun))
-                click.echo(f"Successfully deleted: {key} (dry run:{dryrun})")
+                success = delete(key, dryrun)
+                if success:
+                    click.echo(f"Successfully deleted: {key} (dry run:{dryrun})")
+                else:
+                    click.echo(f"Failed to deleted: {key} (dry run:{dryrun})")
 
         kv_generator = create_key_value_generator(data, "\n", ": ")
         for line in kv_generator:
@@ -113,7 +116,7 @@ def strict(filename, dryrun):
                 click.echo(f"Failed to update: {key} = {val} (dry run:{dryrun})")
 
 
-def get(key, dryrun=False):
+def get(key: str, dryrun=False) -> str:
     from sentry import options
     from sentry.options.manager import UnknownOption
 
@@ -126,9 +129,12 @@ def get(key, dryrun=False):
         return None
 
 
-def set(key, val, dryrun=False):
+def set(key: str, val: str, dryrun=False) -> bool:
     from sentry import options
     from sentry.options.manager import UnknownOption
+
+    if not can_change(key):
+        return False
 
     success = False
     try:
@@ -158,18 +164,30 @@ def set(key, val, dryrun=False):
     return success
 
 
-def delete(key, dryrun=False):
+def delete(key: str, dryrun=False) -> bool:
     from sentry import options
     from sentry.options.manager import UnknownOption
 
+    if not can_change(key):
+        return
+
+    success = False
     try:
         options.lookup_key(key)
         if not dryrun:
             options.delete(key)
-        return f"Deleted key: {key}"
+        click.echo(f"Deleted key: {key}")
+        success = True
     except UnknownOption:
-        return "unknown option: %s" % key
+        click.echo(f"Unknown Option:: {key}")
+    return success
 
 
-def test():
-    pass
+def can_change(key: str) -> bool:
+    changable = False
+    # TODO: Figure out how to look this up
+    if changable:
+        click.echo(f"Key({key}) is mutable!")
+    else:
+        click.echo(f"Key({key}) is not mutable!")
+    return changable
