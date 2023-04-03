670aae8ada26b14752e66d143201a0ac9dd6e678
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-04-03 15:37:46 -0400

    Crimes

--- src/sentry/spans/grouping/strategy/wildcard_replacement.py
+++ src/sentry/spans/grouping/strategy/wildcard_replacement.py
@@ -1,2 +1,65 @@
+import re
+from typing import Pattern
+
+SINGLE_STAR = "([^/]*?)"
+DOUBLE_STAR = "(?:.*?)"
+
+
 def glob_replace(source: str, rule: str) -> str:
-    return "/api/0/issues/*/details"
+
+    # Replace instances of
+    compiled_rule = compile_rule(rule)
+    match = re.search(compiled_rule, source)
+    new_source = ""
+
+    curr = 0
+    for i in range(1, compiled_rule.groups + 1):
+
+        group = match.group(i)
+        print("group", group)
+        span = match.span(i)
+        print("span", span)
+
+        if i == 0:
+            continue
+
+        new_source += source[curr : span[0]]
+        print("new source", new_source)
+        new_source += "*"
+        curr = span[1]
+
+    print("done iterating", curr)
+    print("new source", new_source)
+
+    print(curr)
+    print(source[curr:])
+
+    new_source += source[curr:]
+
+    print("new source", new_source)
+
+    return new_source
+
+
+# TODO: ? replacement support, maybe
+REPLACEMENT_REGEX = re.compile(r"\*\*|\*")
+
+
+def compile_rule(rule: str) -> Pattern[str]:
+    # TODO: Improve the code
+    # TODO: Allow escaping wildcards
+    print("compiling rule")
+
+    new_rule = re.sub(REPLACEMENT_REGEX, replace, rule)
+    print("made rule", f"^{new_rule}$")
+    return re.compile(f"^{new_rule}$")
+
+
+def replace(match):
+    # print("replacing", match)
+    # print(match == "**")
+    if match.group() == "**":
+        return DOUBLE_STAR
+
+    if match.group() == "*":
+        return SINGLE_STAR
--- tests/sentry/spans/grouping/test_wilcard_replacement.py
+++ tests/sentry/spans/grouping/test_wilcard_replacement.py
@@ -12,11 +12,12 @@ from sentry.spans.grouping.strategy.wildcard_replacement import glob_replace
             "/api/0/issues/*/details",
         ),
         (
-            "GET /api/0/organization/sentry/projects/sentry/info",
-            "/api/0/organizations/*/projects/*/**",
-            "/api/0/organizations/*/projects/*/info",
+            "GET /api/0/organizations/sentry/projects/javascript/info",
+            "GET /api/0/organizations/*/projects/*/**",
+            "GET /api/0/organizations/*/projects/*/info",
         ),
     ],
 )
+@pytest.mark.django_db
 def test_wildcard_replacement(source: str, rule: str, result) -> None:
     assert glob_replace(source, rule) == result
