a880b3519b74ffedf7e88738bd468db830ed7e74
Riya Chakraborty <riya.chakraborty@sentry.io>
2023-04-03 14:43:58 -0700

    create starter file

---
+++ src/sentry/sentry_metrics/generic_metrics.py
@@ -0,0 +1,11 @@
+# import register_use_case
+
+# def emit_counter() -> MetricCounter
+# build the Kafka payload
+# verify the use case is valid
+# send to DummyBackend
+
+# def emit_set() -> MetricSet
+
+
+# def emit_distribution() -> MetricDistribution
