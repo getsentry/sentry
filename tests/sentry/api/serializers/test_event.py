651e3156ab58563904152f8f07e19bed43de7dfb
Malachi Willey <malwilley@gmail.com>
2023-04-03 12:18:35 -0700

    feat(breadcrumbs): Format SQL queries in breadcrumbs

--- tests/sentry/api/serializers/test_event.py
+++ tests/sentry/api/serializers/test_event.py
@@ -469,3 +469,28 @@ class DetailedEventSerializerTest(TestCase):
 
         result = json.loads(json.dumps(serialize(group_event, None, DetailedEventSerializer())))
         assert result["perfProblem"] is None
+
+    def test_event_breadcrumb_formatting(self):
+        with self.feature("organizations:issue-breadcrumbs-sql-format"):
+            event = self.store_event(
+                data={
+                    "breadcrumbs": [
+                        {"category": "generic", "message": "should not format this"},
+                        {
+                            "category": "query",
+                            "message": "select * from table where something = $1",
+                        },
+                    ]
+                },
+                project_id=self.project.id,
+            )
+            result = serialize(event, None, DetailedEventSerializer())
+
+            assert result["entries"][0]["type"] == "breadcrumbs"
+            # First breadcrumb should not have a message_formatted property
+            assert result["entries"][0]["data"]["values"][0]["message"] == "should not format this"
+            # Second breadcrumb should have whitespace added in message_formatted
+            assert (
+                result["entries"][0]["data"]["values"][1]["message"]
+                == "select *\n  from table\n where something = $1"
+            )
