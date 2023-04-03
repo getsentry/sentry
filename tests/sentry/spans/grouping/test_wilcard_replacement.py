dac3abd32ad1c42337cc4cb0e9ebd01625b6cdfb
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-04-03 11:10:51 -0400

    Add another test case

--- tests/sentry/spans/grouping/test_wilcard_replacement.py
+++ tests/sentry/spans/grouping/test_wilcard_replacement.py
@@ -11,6 +11,11 @@ from sentry.spans.grouping.strategy.wildcard_replacement import glob_replace
             "/api/0/issues/*/**",
             "/api/0/issues/*/details",
         ),
+        (
+            "GET /api/0/organization/sentry/projects/sentry/info",
+            "/api/0/organizations/*/projects/*/**",
+            "/api/0/organizations/*/projects/*/info",
+        ),
     ],
 )
 def test_wildcard_replacement(source: str, rule: str, result) -> None:
