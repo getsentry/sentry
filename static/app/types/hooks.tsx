b15238dc6c70f2e878753589e1444b0461346c91
Jonas <jonas.badalic@sentry.io>
2023-04-04 12:29:54 -0400

    feat(profiling): add billing banner to prep for GA (#46869)
    Adds the billing banner component when GA flag is enabled
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/types/hooks.tsx
+++ static/app/types/hooks.tsx
@@ -85,6 +85,10 @@ type ReplayGracePeriodAlertProps = {organization: Organization};
 
 type ReplayOnboardinCTAProps = {children: React.ReactNode; organization: Organization};
 
+type ProfilingBetaAlertBannerProps = {
+  organization: Organization;
+};
+
 type SetUpSdkDocProps = {
   location: Location;
   organization: Organization;
@@ -139,6 +143,7 @@ export type ComponentHooks = {
   'component:issue-priority-feedback': () => React.ComponentType<QualitativeIssueFeedbackProps>;
   'component:member-list-header': () => React.ComponentType<MemberListHeaderProps>;
   'component:org-stats-banner': () => React.ComponentType<DashboardHeadersProps>;
+  'component:profiling-billing-banner': () => React.ComponentType<ProfilingBetaAlertBannerProps>;
   'component:replay-beta-grace-period-alert': () => React.ComponentType<ReplayGracePeriodAlertProps>;
   'component:replay-feedback-button': () => React.ComponentType<{}>;
   'component:replay-onboarding-cta': () => React.ComponentType<ReplayOnboardinCTAProps>;
