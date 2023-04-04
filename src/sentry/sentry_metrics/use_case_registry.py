a195e70b86741da854ea39352a57d60f398fa040
Riya Chakraborty <riya.chakraborty@sentry.io>
2023-04-04 10:52:13 -0700

    add use case registry, first attempt

---
+++ src/sentry/sentry_metrics/use_case_registry.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from typing import Any, Iterator
+
+_HARDCODED_USE_CASES = {"PERFORMANCE": "performance"}
+
+_REGISTERED_USE_CASES: dict[str, str] = {}
+
+
+class _UseCaseID(type):
+    def __getattr__(self, attr: str) -> UseCaseID:
+        if attr not in _HARDCODED_USE_CASES and attr not in _REGISTERED_USE_CASES:
+            raise AttributeError(attr)
+
+        return UseCaseID(attr.lower())
+
+    def __iter__(self) -> Iterator[UseCaseID]:
+        return iter(
+            UseCaseID(value)
+            for value in {
+                **_HARDCODED_USE_CASES,
+                **_REGISTERED_USE_CASES,
+            }.values()
+        )
+
+
+class UseCaseID(metaclass=_UseCaseID):
+    def __init__(self, value: str):
+        self.value = value
+
+    def __hash__(self) -> int:
+        return hash(self.value)
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, UseCaseID) and other.value == self.value
+
+    def __repr__(self) -> str:
+        return f"UseCaseID.{self.value.upper()}"
+
+
+def register_use_case(key: str) -> UseCaseID:
+    _REGISTERED_USE_CASES[key.upper()] = key.lower()
+    return UseCaseID(key)
