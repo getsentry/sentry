5806467eb3b31257db87a838c4d30d1256ff87b0
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-04-04 11:02:26 -0400

    Cleanup

--- tests/sentry/spans/grouping/test_wilcard_replacement.py
+++ tests/sentry/spans/grouping/test_wilcard_replacement.py
@@ -7,17 +7,17 @@ from sentry.spans.grouping.strategy.wildcard_replacement import glob_replace
     "source,rule,result",
     [
         (
-            "GET /api/0/issues/sentry/details",
+            "/api/0/issues/sentry/details",
             "/api/0/issues/*/**",
             "/api/0/issues/*/details",
         ),
         (
-            "GET /api/0/organizations/sentry/projects/javascript/info",
-            "GET /api/0/organizations/*/projects/*/**",
-            "GET /api/0/organizations/*/projects/*/info",
+            "/api/0/organizations/sentry/projects/javascript/info",
+            "/api/0/organizations/*/projects/*/**",
+            "/api/0/organizations/*/projects/*/info",
         ),
     ],
 )
 @pytest.mark.django_db
-def test_wildcard_replacement(source: str, rule: str, result) -> None:
+def test_glob_replace(source: str, rule: str, result) -> None:
     assert glob_replace(source, rule) == result
