2aee73d395091143447f2425fe9d4f7cee6f2574
Mike Ihbe <mike.ihbe@sentry.io>
2023-04-03 21:53:06 -0700

    Add backfill migration for new actor columns

---
+++ tests/sentry/migrations/test_0398_backfill_actors.py
@@ -0,0 +1,24 @@
+from sentry.models.actor import Actor
+from sentry.models.user import User
+from sentry.testutils.cases import TestMigrations
+
+
+class BackfillActorsTest(TestMigrations):
+    migrate_from = "0397_actor_fk_inversion"
+    migrate_to = "0398_backfill_actors"
+
+    def setup_initial_state(self):
+        self.new_user = self.create_user("b@example.com")
+        organization = self.create_organization(name="New Org", owner=self.new_user)
+        self.new_team = self.create_team(
+            organization=organization, name="New Team", members=[self.user]
+        )
+
+    def setup_before_migration(self, apps):
+        pass
+
+    def test(self):
+        self.new_user = User.objects.get(id=self.new_user.id)
+        assert self.new_user.actor_id
+        assert Actor.objects.get(user_id=self.new_user.id).id == self.new_user.actor_id
+        assert Actor.objects.get(team_id=self.new_team.id).id == self.new_team.actor_id
