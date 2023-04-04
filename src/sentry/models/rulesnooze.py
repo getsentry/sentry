0c40ff308d948c5ba6e22d8f1fc2306d5de1e640
Colleen O'Rourke <colleen@sentry.io>
2023-04-04 13:35:32 -0700

    feat(rulesnooze): Create RuleSnooze table (#46846)
    Create a new table called `RuleSnooze` for a new [mute alerts
    project](https://github.com/getsentry/sentry/issues/45935).
    
    To clarify, the `Rule` table is issue alerts, and the `AlertRule` table
    is metric alerts. Initially we'll only support issue alerts but will add
    in support for metric alerts later on.
    
    Closes #46827

---
+++ src/sentry/models/rulesnooze.py
@@ -0,0 +1,33 @@
+from django.db import models
+from django.db.models import CheckConstraint, Q
+
+from sentry.db.models import FlexibleForeignKey, Model, region_silo_only_model, sane_repr
+from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
+
+
+@region_silo_only_model
+class RuleSnooze(Model):
+    """
+    Duration an issue alert or metric alert is snoozed for a user. Null `until` value means snoozed forever.
+    """
+
+    __include_in_export__ = True
+
+    user_id = HybridCloudForeignKey("sentry.User", on_delete="CASCADE")
+    rule = FlexibleForeignKey("sentry.Rule", null=True)
+    alert_rule = FlexibleForeignKey("sentry.AlertRule", null=True)
+    until = models.DateTimeField(null=True, db_index=True)
+
+    class Meta:
+        db_table = "sentry_rulesnooze"
+        app_label = "sentry"
+        unique_together = (("user_id", "rule"), ("user_id", "alert_rule"))
+        constraints = [
+            CheckConstraint(
+                check=Q(rule__isnull=False, alert_rule__isnull=True)
+                | Q(rule__isnull=True, alert_rule__isnull=False),
+                name="rule_or_alert_rule",
+            )
+        ]
+
+    __repr__ = sane_repr("user_id", "rule_id", "alert_rule_id", "until")
