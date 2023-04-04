6332cdc4388171cc731757953ee855bcbb55da93
Riya Chakraborty <riya.chakraborty@sentry.io>
2023-04-04 10:03:42 -0700

    scaffold for interface

--- src/sentry/sentry_metrics/generic_metrics.py
+++ src/sentry/sentry_metrics/generic_metrics.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 # import register_use_case
 
 # def emit_counter() -> MetricCounter
@@ -9,3 +11,15 @@
 
 
 # def emit_distribution() -> MetricDistribution
+
+
+class MetricsInterface:
+    def __init__(self, metrics: Any) -> None:
+        # internally used metrics backend
+        self.__metrics = metrics
+
+    # def emit_counter():
+
+    # def emit_set():
+
+    # def emit_distribution():
