b4cba1153ee9fa53dfebd1860a9866c911c04469
Zachary Collins <zachary.collins@sentry.io>
2023-04-03 17:53:25 -0700

    ref(hc): Enable indexes for new actor columns

--- src/sentry/models/actor.py
+++ src/sentry/models/actor.py
@@ -77,14 +77,13 @@ class Actor(Model):
         )
     )
     user_id = HybridCloudForeignKey(
-        settings.AUTH_USER_MODEL, on_delete="CASCADE", db_index=False, unique=False, null=True
+        settings.AUTH_USER_MODEL, on_delete="CASCADE", db_index=True, unique=True, null=True
     )
     team = FlexibleForeignKey(
         "sentry.Team",
         related_name="actor_from_team",
-        db_index=False,
-        db_constraint=False,
-        unique=False,
+        db_index=True,
+        unique=True,
         null=True,
         on_delete=models.CASCADE,
     )
