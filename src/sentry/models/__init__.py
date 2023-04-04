bea2f5dd7d512c1af94c6b1697d36d13823ea0c8
Colleen O'Rourke <colleen@sentry.io>
2023-04-04 13:35:32 -0700

    feat(rulesnooze): Create RuleSnooze table (#46846)
    Create a new table called `RuleSnooze` for a new [mute alerts
    project](https://github.com/getsentry/sentry/issues/45935).
    
    To clarify, the `Rule` table is issue alerts, and the `AlertRule` table
    is metric alerts. Initially we'll only support issue alerts but will add
    in support for metric alerts later on.
    
    Closes #46827

--- src/sentry/models/__init__.py
+++ src/sentry/models/__init__.py
@@ -98,6 +98,7 @@ from .repository import *  # NOQA
 from .reprocessingreport import *  # NOQA
 from .rule import *  # NOQA
 from .rulefirehistory import RuleFireHistory  # NOQA
+from .rulesnooze import RuleSnooze  # NOQA
 from .savedsearch import *  # NOQA
 from .scheduledeletion import *  # NOQA
 from .scheduledjob import *  # NOQA
