90e3c21ae8ec286864fe0c7d98d32d7483aa7e1e
Colleen O'Rourke <colleen@sentry.io>
2023-04-05 11:53:15 -0700

    add date_added and owner_id

--- src/sentry/models/rulesnooze.py
+++ src/sentry/models/rulesnooze.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models import CheckConstraint, Q, UniqueConstraint
+from django.utils import timezone
 
 from sentry.db.models import FlexibleForeignKey, Model, region_silo_only_model, sane_repr
 from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
@@ -16,9 +17,11 @@ class RuleSnooze(Model):
     __include_in_export__ = True
 
     user_id = HybridCloudForeignKey("sentry.User", on_delete="CASCADE", null=True)
+    owner_id = HybridCloudForeignKey("sentry.User", on_delete="SET_NULL", null=True)
     rule = FlexibleForeignKey("sentry.Rule", null=True)
     alert_rule = FlexibleForeignKey("sentry.AlertRule", null=True)
     until = models.DateTimeField(null=True, db_index=True)
+    date_added = models.DateTimeField(default=timezone.now)
 
     class Meta:
         db_table = "sentry_rulesnooze"
@@ -45,4 +48,4 @@ class RuleSnooze(Model):
             ),
         ]
 
-    __repr__ = sane_repr("user_id", "rule_id", "alert_rule_id", "until")
+    __repr__ = sane_repr("user_id", "owner_id", "rule_id", "alert_rule_id", "until", "date_added")
--- tests/sentry/models/test_rulesnooze.py
+++ tests/sentry/models/test_rulesnooze.py
@@ -21,11 +21,14 @@ class RuleSnoozeTest(APITestCase):
         """Test that a rule can be snoozed by a user and globally"""
         issue_alert_rule_snooze_user = RuleSnooze.objects.create(
             user_id=self.user.id,
+            owner_id=self.user.id,
             rule=self.issue_alert_rule,
             until=datetime.now() + timedelta(days=10),
         )
         issue_alert_rule_snooze_all = RuleSnooze.objects.create(
-            rule=self.issue_alert_rule, until=datetime.now() + timedelta(days=1)
+            owner_id=self.user2.id,
+            rule=self.issue_alert_rule,
+            until=datetime.now() + timedelta(days=1),
         )
         assert RuleSnooze.objects.filter(id=issue_alert_rule_snooze_user.id).exists()
         assert RuleSnooze.objects.filter(id=issue_alert_rule_snooze_all.id).exists()
@@ -33,6 +36,7 @@ class RuleSnoozeTest(APITestCase):
     def test_issue_alert_until_and_forever(self):
         issue_alert_rule_snooze_user_until = RuleSnooze.objects.create(
             user_id=self.user.id,
+            owner_id=self.user.id,
             rule=self.issue_alert_rule,
             until=datetime.now() + timedelta(days=1),
         )
@@ -42,13 +46,14 @@ class RuleSnoozeTest(APITestCase):
             label="test rule", project=self.project, owner=self.team.actor
         )
         issue_alert_rule_snooze_user_forever = RuleSnooze.objects.create(
-            user_id=self.user.id, rule=issue_alert_rule2
+            user_id=self.user.id, owner_id=self.user.id, rule=issue_alert_rule2
         )
         assert RuleSnooze.objects.filter(id=issue_alert_rule_snooze_user_forever.id).exists()
 
     def test_metric_alert_until_and_forever(self):
         metric_alert_rule_snooze_user = RuleSnooze.objects.create(
             user_id=self.user.id,
+            owner_id=self.user.id,
             alert_rule=self.metric_alert_rule,
             until=datetime.now() + timedelta(days=1),
         )
@@ -58,7 +63,7 @@ class RuleSnoozeTest(APITestCase):
             organization=self.project.organization, projects=[self.project]
         )
         metric_alert_rule_snooze_user = RuleSnooze.objects.create(
-            user_id=self.user.id, alert_rule=metric_alert_rule2
+            user_id=self.user.id, owner_id=self.user.id, alert_rule=metric_alert_rule2
         )
         assert RuleSnooze.objects.filter(id=metric_alert_rule_snooze_user.id).exists()
 
@@ -78,20 +83,27 @@ class RuleSnoozeTest(APITestCase):
 
     def test_errors(self):
         # ensure no dupes
-        RuleSnooze.objects.create(alert_rule=self.metric_alert_rule)
+        RuleSnooze.objects.create(owner_id=self.user.id, alert_rule=self.metric_alert_rule)
         with pytest.raises(IntegrityError), transaction.atomic():
             RuleSnooze.objects.create(alert_rule=self.metric_alert_rule)
 
-        RuleSnooze.objects.create(rule=self.issue_alert_rule)
+        RuleSnooze.objects.create(owner_id=self.user.id, rule=self.issue_alert_rule)
         with pytest.raises(IntegrityError), transaction.atomic():
             RuleSnooze.objects.create(rule=self.issue_alert_rule)
 
         # ensure valid data
         with pytest.raises(IntegrityError), transaction.atomic():
-            RuleSnooze.objects.create(rule=self.issue_alert_rule, alert_rule=self.metric_alert_rule)
+            RuleSnooze.objects.create(
+                owner_id=self.user.id, rule=self.issue_alert_rule, alert_rule=self.metric_alert_rule
+            )
 
         with pytest.raises(IntegrityError), transaction.atomic():
-            RuleSnooze.objects.create(user_id=self.user.id)
+            RuleSnooze.objects.create(
+                user_id=self.user.id,
+                owner_id=self.user.id,
+            )
 
         with pytest.raises(IntegrityError), transaction.atomic():
-            RuleSnooze.objects.create(until=datetime.now() + timedelta(days=1))
+            RuleSnooze.objects.create(
+                owner_id=self.user.id, until=datetime.now() + timedelta(days=1)
+            )
