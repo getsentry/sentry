f04f5ec8c058520d95624305a4609efb9d3f950b
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-04-06 09:21:12 +0200

    Fix: clear redis entries when no rules

--- src/sentry/ingest/transaction_clusterer/rules.py
+++ src/sentry/ingest/transaction_clusterer/rules.py
@@ -47,16 +47,14 @@ class RedisRuleStore:
         return {rule: int(timestamp) for rule, timestamp in data.items()}
 
     def write(self, project: Project, rules: RuleSet) -> None:
-        if len(rules) == 0:
-            return
-
         client = get_redis_client()
         key = self._get_rules_key(project)
 
         with client.pipeline() as p:
             # to be consistent with other stores, clear previous hash entries:
             p.delete(key)
-            p.hmset(key, rules)
+            if len(rules) > 0:
+                p.hmset(key, rules)
 
 
 class ProjectOptionRuleStore:
