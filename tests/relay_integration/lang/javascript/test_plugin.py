941db2a0d3eb49cc3ea17a3e301f1175487c702b
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-04-06 11:54:22 +0200

    wip

--- tests/relay_integration/lang/javascript/test_plugin.py
+++ tests/relay_integration/lang/javascript/test_plugin.py
@@ -88,6 +88,7 @@ class TestJavascriptIntegration(RelayStoreHelper):
         assert contexts.get("os") == {"name": "Windows", "version": "8", "type": "os"}
         assert contexts.get("device") is None
 
+    @pytest.mark.skip(reason="temp disable test due to circular dependency before merging PR: https://github.com/getsentry/relay/pull/2004")
     def test_adds_contexts_with_device(self):
         data = {
             "timestamp": self.min_ago,
