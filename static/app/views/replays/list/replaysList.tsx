1dac1e582b085a23bbd504184cc979aeb16f47c7
Elias Hussary <elias.hussary@sentry.io>
2023-04-06 16:36:42 -0400

    feat(replays): add alert when project sdk does not support replay_click searches

--- static/app/views/replays/list/replaysList.tsx
+++ static/app/views/replays/list/replaysList.tsx
@@ -2,10 +2,19 @@ import {Fragment, useMemo} from 'react';
 import {browserHistory} from 'react-router';
 import {Location} from 'history';
 
+import Alert from 'sentry/components/alert';
+import ExternalLink from 'sentry/components/links/externalLink';
 import Pagination from 'sentry/components/pagination';
-import type {Organization} from 'sentry/types';
+import {tct} from 'sentry/locale';
+import type {
+  Organization,
+  Project,
+  ProjectSdkUpdates,
+  UpdateSdkSuggestion,
+} from 'sentry/types';
 import trackAdvancedAnalyticsEvent from 'sentry/utils/analytics/trackAdvancedAnalyticsEvent';
 import EventView from 'sentry/utils/discover/eventView';
+import {semverCompare} from 'sentry/utils/profiling/units/versions';
 import {decodeScalar} from 'sentry/utils/queryString';
 import {DEFAULT_SORT} from 'sentry/utils/replays/fetchReplayList';
 import useReplayList from 'sentry/utils/replays/hooks/useReplayList';
@@ -13,6 +22,9 @@ import {useHaveSelectedProjectsSentAnyReplayEvents} from 'sentry/utils/replays/h
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import useProjects from 'sentry/utils/useProjects';
+import withSdkUpdates from 'sentry/utils/withSdkUpdates';
 import ReplayOnboardingPanel from 'sentry/views/replays/list/replayOnboardingPanel';
 import ReplayTable from 'sentry/views/replays/replayTable';
 import {ReplayColumns} from 'sentry/views/replays/replayTable/types';
@@ -72,6 +84,7 @@ function ReplaysListTable({
 
   return (
     <Fragment>
+      <ReplayTableAlert />
       <ReplayTable
         fetchError={fetchError}
         isFetching={isFetching}
@@ -102,5 +115,101 @@ function ReplaysListTable({
     </Fragment>
   );
 }
+
+interface ReplayTableAlertProps {
+  sdkUpdates?: ProjectSdkUpdates[] | null;
+}
+
+const MIN_REPLAY_CLICK_SDK = '7.44.0';
+
+const ReplayTableAlert = withSdkUpdates(function ReplayTableAlert({
+  sdkUpdates,
+}: ReplayTableAlertProps) {
+  const {selection} = usePageFilters();
+  const projects = useProjects();
+  const location = useLocation();
+  const conditions = useMemo(() => {
+    return new MutableSearch(decodeScalar(location.query.query, ''));
+  }, [location.query]);
+
+  const hasReplayClick = conditions.getFilterKeys().some(k => k.includes('replay_click'));
+
+  if (!hasReplayClick) {
+    return null;
+  }
+
+  const selectedProjectsWithSdkUpdates = sdkUpdates?.reduce((acc, sdkUpdate) => {
+    if (!selection.projects.includes(Number(sdkUpdate.projectId))) {
+      return acc;
+    }
+
+    const project = projects.projects.find(p => p.id === sdkUpdate.projectId);
+    // should never really happen but making ts happy
+    if (!project) {
+      return acc;
+    }
+
+    acc.push({
+      project,
+      sdkUpdate,
+    });
+
+    return acc;
+  }, [] as Array<{project: Project; sdkUpdate: ProjectSdkUpdates}>);
+
+  const doesNotMeetMinSDK =
+    selectedProjectsWithSdkUpdates &&
+    selectedProjectsWithSdkUpdates.length > 0 &&
+    selectedProjectsWithSdkUpdates.every(({sdkUpdate}) => {
+      return semverCompare(sdkUpdate.sdkVersion, MIN_REPLAY_CLICK_SDK) === -1;
+    });
+
+  if (!doesNotMeetMinSDK) {
+    return null;
+  }
+
+  const sdkUpdateAction = selectedProjectsWithSdkUpdates?.[0]?.sdkUpdate.suggestions.find(
+    suggestion => suggestion.type === 'updateSdk'
+  ) as UpdateSdkSuggestion | undefined;
+
+  if (sdkUpdateAction) {
+    return (
+      <Alert>
+        {tct(
+          'Searching by replay_click requires a minimum SDK version of [sdkName]@v[minSdkVersion]. [action]',
+          {
+            sdkName: sdkUpdateAction.sdkName,
+            newSdkVersion: sdkUpdateAction.newSdkVersion,
+            minSdkVersion: MIN_REPLAY_CLICK_SDK,
+            action: sdkUpdateAction.sdkUrl ? (
+              <ExternalLink href={sdkUpdateAction.sdkUrl}>
+                {tct('Update to [sdkName]@v[newSdkVersion]', {
+                  sdkName: sdkUpdateAction.sdkName,
+                  newSdkVersion: sdkUpdateAction.newSdkVersion,
+                })}
+              </ExternalLink>
+            ) : (
+              tct('Update to [sdkName]@v[newSdkVersion]', {
+                sdkName: sdkUpdateAction.sdkName,
+                newSdkVersion: sdkUpdateAction.newSdkVersion,
+              })
+            ),
+          }
+        )}
+      </Alert>
+    );
+  }
+
+  return (
+    <Alert>
+      {tct(
+        'Searching by replay_click requires a minimum SDK version of v[minSdkVersion].',
+        {
+          minSdkVersion: MIN_REPLAY_CLICK_SDK,
+        }
+      )}
+    </Alert>
+  );
+});
 
 export default ReplaysList;
