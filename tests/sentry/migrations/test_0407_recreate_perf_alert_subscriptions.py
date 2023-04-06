5f05509377c9972165b92dba9c58d2a5b05b7294
Dan Fuller <dfuller@sentry.io>
2023-04-06 16:17:37 -0700

    fix(metric-alerts): Recreate performance alert subscriptions that have an environment (#46982)
    Due to a bug fixed in https://github.com/getsentry/sentry/pull/46857 we
    have a few hundred perf subscriptions that might not be working as
    expected. The bug is specific to performance alerts running on metrics
    and have an environment set. We just need to recreate the subscriptions
    here so that the queries are made correctly in snuba.
    
    Based off of
    https://github.com/getsentry/sentry/blob/82b1e3759b2a8f2e0772f0fdd648a8b901675980/src/sentry/migrations/0293_restore_metrics_based_alerts.py

---
+++ tests/sentry/migrations/test_0407_recreate_perf_alert_subscriptions.py
@@ -0,0 +1,47 @@
+from unittest import mock
+
+import pytest
+
+from sentry.snuba.dataset import Dataset
+from sentry.snuba.models import QuerySubscription, SnubaQuery
+from sentry.snuba.tasks import _create_in_snuba
+from sentry.testutils.cases import TestMigrations
+
+pytestmark = pytest.mark.sentry_metrics
+
+
+class BackfillPerfSubscriptionsTest(TestMigrations):
+    migrate_from = "0406_monitor_cleanup"
+    migrate_to = "0407_recreate_perf_alert_subscriptions"
+
+    def setUp(self):
+        super().setUp()
+        self.logging_patch = mock.patch(
+            "sentry.migrations.0407_recreate_perf_alert_subscriptions.logging"
+        )
+        self.logging_mock = self.logging_patch.__enter__()
+
+    def setup_initial_state(self):
+        snuba_query = SnubaQuery.objects.create(
+            type=SnubaQuery.Type.PERFORMANCE.value,
+            dataset=Dataset.PerformanceMetrics.value,
+            query="",
+            time_window=60,
+            resolution=60,
+            environment=self.environment,
+            aggregate="count()",
+        )
+        subscription = QuerySubscription.objects.create(
+            project=self.project,
+            snuba_query=snuba_query,
+            type="something",
+        )
+        subscription.subscription_id = _create_in_snuba(subscription)
+        subscription.save()
+
+    def test(self):
+        assert self.logging_mock.exception.call_count == 0
+
+    def tearDown(self):
+        super().tearDown()
+        self.logging_patch.__exit__(None, None, None)
