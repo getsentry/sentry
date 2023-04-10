ac8147e5810a8b96dc0966cadf58ae0d2d2ea026
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-04-10 15:56:34 -0700

    Break out logic to handle archived until escalating

---
+++ src/sentry/issues/ignored.py
@@ -0,0 +1,25 @@
+from __future__ import annotations
+
+from typing import Sequence
+
+from sentry.models import User
+from sentry.models.groupinbox import GroupInboxRemoveAction, remove_group_from_inbox
+from sentry.utils import metrics
+
+
+def handle_archived_until_escalating(
+    group_ids: Sequence[int],
+    acting_user: User | None,
+):
+    """
+    Handle issues that are archived until escalating and create a forecast for them.
+
+    Issues that are marked as ignored with `archiveDuration: until_escalating`
+    in the statusDetail are treated as `archived_until_escalating`.
+    """
+    metrics.incr("group.archived_until_escalating", skip_internal=True)
+    for group in group_ids:
+        remove_group_from_inbox(group, action=GroupInboxRemoveAction.IGNORED, user=acting_user)
+    # TODO(snigdha): create a forecast for this group
+
+    return
