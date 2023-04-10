bbbe3224a4d1669975b958327440707e593ca0ad
Scott Cooper <scttcper@gmail.com>
2023-04-10 16:33:32 -0700

    feat(escalating-issues): Add archive to issue actions
    Adds archive in place of ignored supporting a similar set of props like confirmation.

---
+++ static/app/components/actions/archive.tsx
@@ -0,0 +1,151 @@
+import styled from '@emotion/styled';
+
+import {Button} from 'sentry/components/button';
+import ButtonBar from 'sentry/components/buttonBar';
+import {openConfirmModal} from 'sentry/components/confirm';
+import {DropdownMenu} from 'sentry/components/dropdownMenu';
+import {IconArchive, IconChevron} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {
+  GroupStatusResolution,
+  ResolutionStatus,
+  ResolutionStatusDetails,
+} from 'sentry/types';
+
+interface ArchiveActionProps {
+  onUpdate: (params: GroupStatusResolution) => void;
+  className?: string;
+  confirmLabel?: string;
+  confirmMessage?: (
+    statusDetails: ResolutionStatusDetails | undefined
+  ) => React.ReactNode;
+  disableTooltip?: boolean;
+  disabled?: boolean;
+  hideIcon?: boolean;
+  isArchived?: boolean;
+  shouldConfirm?: boolean;
+  size?: 'xs' | 'sm';
+}
+
+export function getArchiveActions({
+  shouldConfirm,
+  confirmLabel,
+  confirmMessage,
+  onUpdate,
+}: Pick<
+  ArchiveActionProps,
+  'shouldConfirm' | 'confirmMessage' | 'onUpdate' | 'confirmLabel'
+>) {
+  const onArchive = (statusDetails: GroupStatusResolution['statusDetails']) => {
+    if (shouldConfirm && confirmMessage) {
+      openConfirmModal({
+        onConfirm: () =>
+          onUpdate({
+            status: ResolutionStatus.IGNORED,
+            statusDetails,
+          }),
+        message: confirmMessage(statusDetails),
+        confirmText: confirmLabel,
+      });
+    } else {
+      onUpdate({
+        status: ResolutionStatus.IGNORED,
+        statusDetails,
+      });
+    }
+  };
+
+  return {
+    onArchive,
+    dropdownItems: [
+      {
+        key: 'untilEscalating',
+        label: t('Until it escalates'),
+        onAction: () => onArchive({untilEscalating: true}),
+      },
+      {
+        key: 'forever',
+        label: t('Forever'),
+        onAction: () => onArchive({}),
+      },
+    ],
+  };
+}
+
+function ArchiveActions({
+  size = 'xs',
+  disabled,
+  disableTooltip,
+  className,
+  hideIcon,
+  shouldConfirm,
+  confirmLabel,
+  isArchived,
+  confirmMessage,
+  onUpdate,
+}: ArchiveActionProps) {
+  if (isArchived) {
+    return (
+      <Button
+        priority="primary"
+        size="xs"
+        title={t('Change status to unresolved')}
+        onClick={() => onUpdate({status: ResolutionStatus.UNRESOLVED, statusDetails: {}})}
+        aria-label={t('Unarchive')}
+        icon={<IconArchive size="xs" />}
+      />
+    );
+  }
+
+  const {dropdownItems, onArchive} = getArchiveActions({
+    confirmLabel,
+    onUpdate,
+    shouldConfirm,
+    confirmMessage,
+  });
+
+  return (
+    <ButtonBar className={className} merged>
+      <ArchiveButton
+        size={size}
+        tooltipProps={{delay: 300, disabled: disabled || disableTooltip}}
+        title={t(
+          'Silences alerts for this issue and removes it from the issue stream by default.'
+        )}
+        icon={hideIcon ? null : <IconArchive size={size} />}
+        onClick={() => onArchive({untilEscalating: true})}
+        disabled={disabled}
+      >
+        {t('Archive')}
+      </ArchiveButton>
+      <DropdownMenu
+        size="sm"
+        trigger={triggerProps => (
+          <DropdownTrigger
+            {...triggerProps}
+            aria-label={t('Archive options')}
+            size={size}
+            icon={<IconChevron direction="down" size="xs" />}
+            disabled={disabled}
+          />
+        )}
+        menuTitle={t('Archive')}
+        items={dropdownItems}
+        isDisabled={disabled}
+      />
+    </ButtonBar>
+  );
+}
+
+export default ArchiveActions;
+
+const ArchiveButton = styled(Button)`
+  box-shadow: none;
+  border-radius: ${p => p.theme.borderRadiusLeft};
+`;
+
+const DropdownTrigger = styled(Button)`
+  box-shadow: none;
+  border-radius: ${p => p.theme.borderRadiusRight};
+  border-left: none;
+`;
--- static/app/views/issueDetails/actions/index.tsx
+++ static/app/views/issueDetails/actions/index.tsx
@@ -13,6 +13,7 @@ import {
 import {Client} from 'sentry/api';
 import Feature from 'sentry/components/acl/feature';
 import FeatureDisabled from 'sentry/components/acl/featureDisabled';
+import ArchiveActions, {getArchiveActions} from 'sentry/components/actions/archive';
 import ActionButton from 'sentry/components/actions/button';
 import IgnoreActions, {getIgnoreActions} from 'sentry/components/actions/ignore';
 import ResolveActions from 'sentry/components/actions/resolve';
@@ -367,10 +368,14 @@ class Actions extends Component<Props> {
       share: shareCap,
     } = getConfigForIssueType(group).actions;
 
+    const hasEscalatingIssues = organization.features.includes('escalating-issues');
     const hasDeleteAccess = organization.access.includes('event:admin');
     const activeSuperUser = isActiveSuperuser();
 
     const {dropdownItems, onIgnore} = getIgnoreActions({onUpdate: this.onUpdate});
+    const {dropdownItems: archiveDropdownItems} = getArchiveActions({
+      onUpdate: this.onUpdate,
+    });
     return (
       <ActionWrapper>
         <DropdownMenu
@@ -381,7 +386,7 @@ class Actions extends Component<Props> {
             size: 'sm',
           }}
           items={[
-            ...(isIgnored
+            ...(isIgnored || hasEscalatingIssues
               ? []
               : [
                   {
@@ -398,6 +403,18 @@ class Actions extends Component<Props> {
                       },
                       ...dropdownItems,
                     ],
+                  },
+                ]),
+            ...(isIgnored && hasEscalatingIssues
+              ? []
+              : [
+                  {
+                    key: 'Archive',
+                    className: 'hidden-sm hidden-md hidden-lg',
+                    label: t('Archive'),
+                    isSubmenu: true,
+                    disabled,
+                    children: archiveDropdownItems,
                   },
                 ]),
             {
@@ -548,7 +565,17 @@ class Actions extends Component<Props> {
           </ActionButton>
         ) : (
           <Fragment>
-            <GuideAnchor target="ignore_delete_discard" position="bottom" offset={20}>
+            {hasEscalatingIssues ? (
+              <ArchiveActions
+                className="hidden-xs"
+                size="sm"
+                isArchived={isIgnored}
+                onUpdate={this.onUpdate}
+                disabled={disabled}
+                hideIcon
+                disableTooltip
+              />
+            ) : (
               <IgnoreActions
                 className="hidden-xs"
                 isIgnored={isIgnored}
@@ -558,7 +585,7 @@ class Actions extends Component<Props> {
                 hideIcon
                 disableTooltip
               />
-            </GuideAnchor>
+            )}
             <GuideAnchor target="resolve" position="bottom" offset={20}>
               <ResolveActions
                 disableTooltip
