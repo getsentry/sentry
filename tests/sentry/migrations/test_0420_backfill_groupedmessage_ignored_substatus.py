c4f947f9124f4519a186e0b3cb18b0ef10c070b4
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-04-11 12:30:22 -0700

    remove extraneous commas

--- tests/sentry/migrations/test_0420_backfill_groupedmessage_ignored_substatus.py
+++ tests/sentry/migrations/test_0420_backfill_groupedmessage_ignored_substatus.py
@@ -7,14 +7,14 @@ class GroupIgnoredSubstatusBackfillTest(TestMigrations):
     migrate_to = "0420_backfill_groupedmessage_ignored_substatus"
 
     def setup_before_migration(self, apps):
-        self.create_group(status=GroupStatus.IGNORED, substatus=GroupSubStatus.UNTIL_ESCALATING),
-        self.create_group(status=GroupStatus.MUTED, substatus=GroupSubStatus.UNTIL_ESCALATING),
+        self.create_group(status=GroupStatus.IGNORED, substatus=GroupSubStatus.UNTIL_ESCALATING)
+        self.create_group(status=GroupStatus.MUTED, substatus=GroupSubStatus.UNTIL_ESCALATING)
 
         # below substates shouldn't be legal but for the purposes of this backfill, we'll null them out
-        self.create_group(status=GroupStatus.IGNORED, substatus=GroupSubStatus.ONGOING),
-        self.create_group(status=GroupStatus.MUTED, substatus=GroupSubStatus.ONGOING),
-        self.create_group(status=GroupStatus.IGNORED, substatus=GroupSubStatus.ESCALATING),
-        self.create_group(status=GroupStatus.MUTED, substatus=GroupSubStatus.ESCALATING),
+        self.create_group(status=GroupStatus.IGNORED, substatus=GroupSubStatus.ONGOING)
+        self.create_group(status=GroupStatus.MUTED, substatus=GroupSubStatus.ONGOING)
+        self.create_group(status=GroupStatus.IGNORED, substatus=GroupSubStatus.ESCALATING)
+        self.create_group(status=GroupStatus.MUTED, substatus=GroupSubStatus.ESCALATING)
 
         # nulled substatus should stay the same
         self.create_group(status=GroupStatus.IGNORED)
