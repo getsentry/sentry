38e802bb478d96f18589f1d3ec9d86addb79c15b
Stephen Cefali <scefali@sentry.io>
2023-04-11 14:05:49 -0700

    ref(analytics): convert user feedback (#47178)

--- static/app/utils/analytics/coreuiAnalyticsEvents.tsx
+++ static/app/utils/analytics/coreuiAnalyticsEvents.tsx
@@ -6,11 +6,23 @@ export type CoreUIEventParameters = {
     filter: PinnedPageFilter;
     pin: boolean;
   };
+  'user_feedback.dialog_opened': {
+    projects: string;
+  };
+  'user_feedback.docs_clicked': {
+    projects: string;
+  };
+  'user_feedback.viewed': {
+    projects: string;
+  };
 };
 
 type CoreUIAnalyticsKey = keyof CoreUIEventParameters;
 
-export const coreUIEventMap: Record<CoreUIAnalyticsKey, string> = {
+export const coreUIEventMap: Record<CoreUIAnalyticsKey, string | null> = {
   'number_drag_control.clicked': 'Number Drag Control: Clicked',
   'page_filters.pin_click': 'Page Filters: Pin Button Clicked',
+  'user_feedback.docs_clicked': 'User Feedback Docs Clicked',
+  'user_feedback.dialog_opened': 'User Feedback Dialog Opened',
+  'user_feedback.viewed': null, // volume high
 };
