868f74cc61518ace1d95c8d838e5552450a73955
Stephen Cefali <scefali@sentry.io>
2023-04-11 14:16:15 -0700

    convert selector analytics

--- static/app/utils/analytics/coreuiAnalyticsEvents.tsx
+++ static/app/utils/analytics/coreuiAnalyticsEvents.tsx
@@ -1,6 +1,28 @@
 import {PinnedPageFilter} from 'sentry/types';
 
+interface DateSelector {
+  field_changed: 'start' | 'end';
+  path: string;
+  time: string;
+}
+
 export type CoreUIEventParameters = {
+  'dateselector.time_changed': DateSelector;
+  'dateselector.utc_changed': {
+    path: string;
+    utc: boolean;
+  };
+  'environmentselector.direct_selection': {
+    path: string;
+  };
+  'environmentselector.toggle': {
+    action: 'removed' | 'added';
+    path: string;
+  };
+  'environmentselector.update': {
+    count: number;
+    path: string;
+  };
   'number_drag_control.clicked': {};
   'page_filters.pin_click': {
     filter: PinnedPageFilter;
@@ -25,4 +47,9 @@ export const coreUIEventMap: Record<CoreUIAnalyticsKey, string | null> = {
   'user_feedback.docs_clicked': 'User Feedback Docs Clicked',
   'user_feedback.dialog_opened': 'User Feedback Dialog Opened',
   'user_feedback.viewed': null, // volume high
+  'dateselector.utc_changed': null, // volume high
+  'dateselector.time_changed': null, // volume high
+  'environmentselector.toggle': null, // volume high
+  'environmentselector.update': null, // volume high
+  'environmentselector.direct_selection': null, // volume high
 };
