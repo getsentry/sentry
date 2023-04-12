b8419efb8e09df3580190fbb74ee4dea8d53dd2d
Jodi Jang <jodi.jang@sentry.io>
2023-04-12 08:35:27 -0700

    fix: Do not remove unused queue

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -712,6 +712,7 @@ CELERY_QUEUES = [
     Queue("post_process_errors", routing_key="post_process_errors"),
     Queue("post_process_issue_platform", routing_key="post_process_issue_platform"),
     Queue("post_process_transactions", routing_key="post_process_transactions"),
+    Queue("process_owner_assignments", routing_key="process_owner_assignments"),
     Queue("relay_config", routing_key="relay_config"),
     Queue("relay_config_bulk", routing_key="relay_config_bulk"),
     Queue("reports.deliver", routing_key="reports.deliver"),
