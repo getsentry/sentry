7e0107fe6bbff750b9f4de8ce551a27d4eafd2ff
Colleen O'Rourke <colleen@sentry.io>
2023-04-12 11:08:10 -0700

    feat(rulesnooze): Add task to remove expired rulesnoozes (#47123)
    When a `RuleSnooze` object's `until` time is up, we don't need the row
    anymore. This PR creates a task that cleans up expired rows.
    
    Closes #47045

---
+++ src/sentry/tasks/clear_expired_rulesnoozes.py
@@ -0,0 +1,10 @@
+from django.utils import timezone
+
+from sentry.models import RuleSnooze
+from sentry.tasks.base import instrumented_task
+
+
+@instrumented_task(name="sentry.tasks.clear_expired_rulesnoozes", time_limit=65, soft_time_limit=60)
+def clear_expired_rulesnoozes():
+    rule_snooze_ids = RuleSnooze.objects.filter(until__lte=timezone.now()).values_list("id")[:1000]
+    RuleSnooze.objects.filter(id__in=rule_snooze_ids).delete()
