077dca419f8c0ba377a0eae88c34b541949618f4
Nar Saynorath <nar.saynorath@sentry.io>
2023-04-12 16:43:53 -0400

    fix(issue-detectors): Filter out None values from tags (#47270)
    Filters out None values from the tags array so we don't attempt to index
    None.
    
    Fixes SENTRY-FOR-SENTRY-RZM

--- tests/sentry/utils/performance_issues/test_uncompressed_assets_detector.py
+++ tests/sentry/utils/performance_issues/test_uncompressed_assets_detector.py
@@ -50,6 +50,43 @@ class UncompressedAssetsDetectorTest(TestCase):
         run_detector_on_data(detector, event)
         return list(detector.stored_problems.values())
 
+    def test_detects_uncompressed_asset_with_none_tag(self):
+        event = {
+            "event_id": "a" * 16,
+            "project": PROJECT_ID,
+            "tags": [None, ["browser.name", "chrome"]],
+            "spans": [
+                create_asset_span(
+                    duration=1000.0,
+                    data={
+                        "Transfer Size": 1_000_000,
+                        "Encoded Body Size": 1_000_000,
+                        "Decoded Body Size": 1_000_000,
+                    },
+                ),
+                create_compressed_asset_span(),
+            ],
+        }
+
+        assert self.find_problems(event) == [
+            PerformanceProblem(
+                fingerprint="1-1012-6893fb5a8a875d692da96590f40dc6bddd6fcabc",
+                op="resource.script",
+                desc="https://s1.sentry-cdn.com/_static/dist/sentry/entrypoints/app.js",
+                type=PerformanceUncompressedAssetsGroupType,
+                parent_span_ids=[],
+                cause_span_ids=[],
+                offender_span_ids=["bbbbbbbbbbbbbbbb"],
+                evidence_data={
+                    "op": "resource.script",
+                    "parent_span_ids": [],
+                    "cause_span_ids": [],
+                    "offender_span_ids": ["bbbbbbbbbbbbbbbb"],
+                },
+                evidence_display=[],
+            )
+        ]
+
     def test_detects_uncompressed_asset(self):
         event = {
             "event_id": "a" * 16,
