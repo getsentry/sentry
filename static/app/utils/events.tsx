be6baec6e38fe83255bb146fdcc6e06861a314a3
Malachi Willey <malwilley@gmail.com>
2023-04-12 16:57:08 -0700

    rename to group_num_user_feedback

--- static/app/utils/events.tsx
+++ static/app/utils/events.tsx
@@ -325,6 +325,7 @@ export type CommonGroupAnalyticsData = {
   error_count: number;
   group_has_replay: boolean;
   group_id: number;
+  group_num_user_feedback: number;
   has_external_issue: boolean;
   has_owner: boolean;
   integration_assignment_source: string;
@@ -334,7 +335,6 @@ export type CommonGroupAnalyticsData = {
   issue_type: IssueType;
   num_comments: number;
   num_participants: number;
-  num_user_feedback: number;
   num_viewers: number;
   is_assigned?: boolean;
   issue_level?: string;
@@ -363,6 +363,6 @@ export function getAnalyticsDataForGroup(group?: Group | null): CommonGroupAnaly
     integration_assignment_source: group ? getAssignmentIntegration(group) : '',
     num_participants: group?.participants.length ?? 0,
     num_viewers: group?.seenBy.filter(user => user.id !== activeUser?.id).length ?? 0,
-    num_user_feedback: group?.userReportCount ?? 0,
+    group_num_user_feedback: group?.userReportCount ?? 0,
   };
 }
