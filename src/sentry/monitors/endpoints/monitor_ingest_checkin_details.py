ecd13df2251215e231c23a974ef94ad309490364
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-04-12 08:40:46 -0700

    revert change

--- src/sentry/monitors/endpoints/monitor_ingest_checkin_details.py
+++ src/sentry/monitors/endpoints/monitor_ingest_checkin_details.py
@@ -102,8 +102,8 @@ class MonitorIngestCheckInDetailsEndpoint(MonitorIngestEndpoint):
             checkin.update(**params)
 
             if checkin.status == CheckInStatus.ERROR:
-                monitor.mark_failed(current_datetime)
-                monitor_failed = monitor_environment.mark_failed(current_datetime)
+                monitor_failed = monitor.mark_failed(current_datetime)
+                monitor_environment.mark_failed(current_datetime)
                 if not monitor_failed:
                     return self.respond(serialize(checkin, request.user), status=208)
             else:
