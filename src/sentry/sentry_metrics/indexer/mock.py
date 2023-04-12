816411b7f73a1728f188777bba9f11a7465491d2
John Yang <john.yang@sentry.io>
2023-04-12 13:46:08 -0700

    implement use case aware RawSimpleIndexer

--- src/sentry/sentry_metrics/indexer/mock.py
+++ src/sentry/sentry_metrics/indexer/mock.py
@@ -5,10 +5,13 @@ from typing import DefaultDict, Dict, Mapping, Optional, Set
 from sentry.sentry_metrics.configuration import UseCaseKey
 from sentry.sentry_metrics.indexer.base import (
     FetchType,
-    KeyCollection,
     KeyResult,
     KeyResults,
+    OrgId,
     StringIndexer,
+    UseCaseId,
+    UseCaseKeyCollection,
+    UseCaseKeyResults,
 )
 from sentry.sentry_metrics.indexer.strings import StaticStringIndexer
 
@@ -19,23 +22,25 @@ class RawSimpleIndexer(StringIndexer):
 
     def __init__(self) -> None:
         self._counter = itertools.count(start=10000)
-        self._strings: DefaultDict[int, DefaultDict[str, Optional[int]]] = defaultdict(
-            lambda: defaultdict(self._counter.__next__)
-        )
+        self._strings: DefaultDict[
+            UseCaseId, DefaultDict[OrgId, DefaultDict[str, Optional[int]]]
+        ] = defaultdict(lambda: defaultdict(lambda: defaultdict(self._counter.__next__)))
         self._reverse: Dict[int, str] = {}
 
     def bulk_record(
-        self, use_case_id: UseCaseKey, org_strings: Mapping[int, Set[str]]
-    ) -> KeyResults:
-        db_read_keys = KeyCollection(org_strings)
-        db_read_key_results = KeyResults()
-        for org_id, strs in org_strings.items():
-            for string in strs:
-                id = self.resolve(use_case_id, org_id, string)
-                if id is not None:
-                    db_read_key_results.add_key_result(
-                        KeyResult(org_id=org_id, string=string, id=id), fetch_type=FetchType.DB_READ
-                    )
+        self, strings: Mapping[UseCaseId, Mapping[OrgId, Set[str]]]
+    ) -> UseCaseKeyResults:
+        db_read_keys = UseCaseKeyCollection(strings)
+        db_read_key_results = UseCaseKeyResults()
+        for use_case_id, org_strs in strings.items():
+            for org_id, strs in org_strs.items():
+                for string in strs:
+                    id = self.resolve(use_case_id, org_id, string)
+                    if id is not None:
+                        db_read_key_results.add_key_result(
+                            KeyResult(org_id=org_id, string=string, id=id),
+                            fetch_type=FetchType.DB_READ,
+                        )
 
         db_write_keys = db_read_key_results.get_unmapped_keys(db_read_keys)
 
@@ -52,17 +57,17 @@ class RawSimpleIndexer(StringIndexer):
         return db_read_key_results.merge(db_write_key_results)
 
     def record(self, use_case_id: UseCaseKey, org_id: int, string: str) -> Optional[int]:
-        return self._record(org_id, string)
+        return self._record(use_case_id, org_id, string)
 
     def resolve(self, use_case_id: UseCaseKey, org_id: int, string: str) -> Optional[int]:
-        strs = self._strings[org_id]
+        strs = self._strings[use_case_id][org_id]
         return strs.get(string)
 
     def reverse_resolve(self, use_case_id: UseCaseKey, org_id: int, id: int) -> Optional[str]:
         return self._reverse.get(id)
 
-    def _record(self, org_id: int, string: str) -> Optional[int]:
-        index = self._strings[org_id][string]
+    def _record(self, use_case_id: UseCaseId, org_id: OrgId, string: str) -> Optional[int]:
+        index = self._strings[use_case_id][org_id][string]
         if index is not None:
             self._reverse[index] = string
         return index
