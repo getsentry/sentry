2cf20e295599a98edc48b3a506c4b1d0c60d0108
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-04-14 00:22:18 +0800

    also update test stubs

--- fixtures/js-stubs/replayRecord.ts
+++ fixtures/js-stubs/replayRecord.ts
@@ -24,7 +24,7 @@ export function ReplayRecord(replayRecord: Partial<TReplayRecord> = {}): TReplay
     error_ids: ['5c83aaccfffb4a708ae893bad9be3a1c'],
     finished_at: new Date('Sep 22, 2022 5:00:03 PM UTC'),
     id: '761104e184c64d439ee1014b72b4d83b',
-    longest_transaction: 0,
+    is_archived: false,
     os: {
       name: 'Other',
       version: '',
