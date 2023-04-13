0ebad20ead9a93b51129d7c5b88ed9ab23dd437e
Dan Fuller <dfuller@sentry.io>
2023-04-13 10:43:45 -0700

    fix(migration-runner): Use migration models instead of real models when manually running migrations
    Currently when a backfill migration runs as part of the deploy it uses migration models, but when
    ops run them manually it uses real models. This causes weirdness like signals firing.

--- src/sentry/runner/commands/migrations.py
+++ src/sentry/runner/commands/migrations.py
@@ -16,14 +16,14 @@ def migrations():
 def run(ctx, app_name, migration_name):
     "Manually run a single data migration. Will error if migration is not data only."
 
-    from django.apps import apps
     from django.db import connections
     from django.db.migrations import RunPython
     from django.db.migrations.executor import MigrationExecutor
 
-    migration = MigrationExecutor(connections["default"]).loader.get_migration_by_prefix(
-        app_name, migration_name
-    )
+    executor = MigrationExecutor(connections["default"])
+    apps = executor.loader.project_state(migration_name).apps
+    migration = executor.loader.get_migration_by_prefix(app_name, migration_name)
+
     for op in migration.operations:
         if not isinstance(op, RunPython):
             raise click.ClickException(
