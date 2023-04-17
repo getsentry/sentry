bea6472480883a46909e8ae375560fd7ed3eb94f
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-04-17 16:38:56 +0200

    feat(onboarding): Add feature flag for SDK selection (#47463)

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -232,6 +232,7 @@ default_manager.add("organizations:onboarding-docs-with-product-selection", Orga
 default_manager.add("organizations:onboarding-project-deletion-on-back-click", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:onboarding-remove-multiselect-platform", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:onboarding-project-loader", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
+default_manager.add("organizations:onboarding-sdk-selection", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:open-ai-suggestion", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:open-ai-suggestion-new-design", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:performance-view", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
