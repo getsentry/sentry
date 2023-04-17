2be5424c2499d03235d70d23e50efbd993059535
Scott Cooper <scttcper@gmail.com>
2023-04-17 16:03:05 -0700

    test failure

--- static/app/actionCreators/tags.spec.tsx
+++ static/app/actionCreators/tags.spec.tsx
@@ -1,11 +1,24 @@
+import * as indicators from 'sentry/actionCreators/indicator';
 import TagStore from 'sentry/stores/tagStore';
 
 import {loadOrganizationTags} from './tags';
 
 describe('loadOrganizationTags', () => {
   const api = new MockApiClient();
+  const selection = {
+    datetime: {
+      end: new Date().toISOString(),
+      period: null,
+      start: new Date().toISOString(),
+      utc: null,
+    },
+    environments: [],
+    projects: [],
+  };
+
   afterEach(() => {
     TagStore.reset();
+    jest.resetAllMocks();
   });
 
   it('should load tags into the store', async () => {
@@ -15,17 +28,18 @@ describe('loadOrganizationTags', () => {
     });
     expect(TagStore.getState().device).toBeUndefined();
 
-    await loadOrganizationTags(api, 'org-slug', {
-      datetime: {
-        end: new Date().toISOString(),
-        period: null,
-        start: new Date().toISOString(),
-        utc: null,
-      },
-      environments: [],
-      projects: [],
-    });
+    await loadOrganizationTags(api, 'org-slug', selection);
 
     expect(TagStore.getState().device).toBeTruthy();
+  });
+  it('should show an alert on failure', async () => {
+    jest.spyOn(indicators, 'addErrorMessage');
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/tags/',
+      statusCode: 403,
+    });
+
+    await loadOrganizationTags(api, 'org-slug', selection);
+    expect(indicators.addErrorMessage).toHaveBeenCalledWith('Unable to load tags');
   });
 });
