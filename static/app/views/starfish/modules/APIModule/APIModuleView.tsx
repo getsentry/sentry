6be01814c35d8d18ef66e1ad531a615c1f98bca2
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-04-17 15:09:02 -0400

    Add rudimentary span duration percentiles graph

--- static/app/views/starfish/modules/APIModule/APIModuleView.tsx
+++ static/app/views/starfish/modules/APIModule/APIModuleView.tsx
@@ -1,9 +1,12 @@
+import {Fragment} from 'react';
 import {useQuery} from '@tanstack/react-query';
 import {Location} from 'history';
 
 import GridEditable, {GridColumnHeader} from 'sentry/components/gridEditable';
+import {Series} from 'sentry/types/echarts';
+import Chart from 'sentry/views/starfish/components/chart';
 
-import {ENDPOINT_LIST_QUERY} from './queries';
+import {ENDPOINT_GRAPH_QUERY, ENDPOINT_LIST_QUERY} from './queries';
 
 const HOST = 'http://localhost:8080';
 
@@ -36,18 +39,68 @@ export default function APIModuleView({location}: Props) {
     initialData: [],
   });
 
+  const {isLoading: isGraphLoading, data: graphData} = useQuery({
+    queryKey: ['graph'],
+    queryFn: () =>
+      fetch(`${HOST}/?query=${ENDPOINT_GRAPH_QUERY}`).then(res => res.json()),
+    retry: false,
+    initialData: [],
+  });
+
+  const quantiles = ['p50', 'p75', 'p95', 'p99'];
+
+  const seriesByQuantile: {[quantile: string]: Series} = {};
+  quantiles.forEach(quantile => {
+    seriesByQuantile[quantile] = {
+      seriesName: quantile,
+      data: [],
+    };
+  });
+
+  graphData.forEach(datum => {
+    quantiles.forEach(quantile => {
+      seriesByQuantile[quantile].data.push({
+        value: datum[quantile],
+        name: datum.interval,
+      });
+    });
+  });
+
+  const data = Object.values(seriesByQuantile);
+
   return (
-    <GridEditable
-      isLoading={areEndpointsLoading}
-      data={endpointsData}
-      columnOrder={COLUMN_ORDER}
-      columnSortBy={[]}
-      grid={{
-        renderHeadCell,
-        renderBodyCell,
-      }}
-      location={location}
-    />
+    <Fragment>
+      <Chart
+        statsPeriod="24h"
+        height={180}
+        data={data}
+        start=""
+        end=""
+        loading={isGraphLoading}
+        utc={false}
+        grid={{
+          left: '0',
+          right: '0',
+          top: '16px',
+          bottom: '8px',
+        }}
+        disableMultiAxis
+        definedAxisTicks={4}
+        stacked
+      />
+
+      <GridEditable
+        isLoading={areEndpointsLoading}
+        data={endpointsData}
+        columnOrder={COLUMN_ORDER}
+        columnSortBy={[]}
+        grid={{
+          renderHeadCell,
+          renderBodyCell,
+        }}
+        location={location}
+      />
+    </Fragment>
   );
 }
 
