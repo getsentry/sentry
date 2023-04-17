a6d5f7819d93f1d359acba1e339b219ea2ce53b6
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-04-17 10:27:45 -0700

    Fix status default

--- src/sentry/models/grouphistory.py
+++ src/sentry/models/grouphistory.py
@@ -219,8 +219,8 @@ def record_group_history_from_activity_type(
 
     # Substatus-based GroupHistory should overritde activity-based GroupHistory since it's more specific.
     if group.substatus:
-        status_str = GROUP_SUBSTATUS_TO_GROUP_HISTORY_STATUS.get(group.substatus, status)
-        status = string_to_status_lookup.get(status_str, None)
+        status_str = GROUP_SUBSTATUS_TO_GROUP_HISTORY_STATUS.get(group.substatus, None)
+        status = string_to_status_lookup.get(status_str, status)
 
     if status is not None:
         return record_group_history(group, status, actor, release)
