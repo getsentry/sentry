bbcb2b9db477d26e2f4ada3a7cbc7575f062e24b
William Mak <william@wmak.io>
2023-04-18 17:14:54 -0400

    feat(starfish): Rename operation to action (#47590)
    - Operation has been renamed to action

--- static/app/views/starfish/modules/databaseModule/index.tsx
+++ static/app/views/starfish/modules/databaseModule/index.tsx
@@ -34,28 +34,28 @@ function getOptions() {
     'UPDATE',
     'connect',
     'delete',
-  ].map(operation => {
+  ].map(action => {
     return {
-      value: operation,
+      value: action,
       prefix,
-      label: operation,
+      label: action,
     };
   });
 }
 
 type State = {
-  operation: string;
+  action: string;
   transaction: string;
 };
 
 class DatabaseModule extends Component<Props, State> {
   state: State = {
-    operation: 'SELECT',
+    action: 'SELECT',
     transaction: '',
   };
 
   handleOptionChange(value) {
-    this.setState({operation: value});
+    this.setState({action: value});
   }
 
   handleSearch(query) {
@@ -74,7 +74,7 @@ class DatabaseModule extends Component<Props, State> {
 
   render() {
     const {location, organization} = this.props;
-    const {operation, transaction} = this.state;
+    const {action, transaction} = this.state;
     const eventView = EventView.fromLocation(location);
 
     return (
@@ -91,7 +91,7 @@ class DatabaseModule extends Component<Props, State> {
               <PageErrorAlert />
               <DatabaseChartView location={location} />
               <CompactSelect
-                value={operation}
+                value={action}
                 options={getOptions()}
                 onChange={opt => this.handleOptionChange(opt.value)}
               />
@@ -103,7 +103,7 @@ class DatabaseModule extends Component<Props, State> {
               />
               <DatabaseTableView
                 location={location}
-                operation={operation}
+                action={action}
                 transaction={transaction}
               />
             </Layout.Main>
