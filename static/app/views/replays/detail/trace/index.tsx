73e994f62f4935a6b8b490b7cf5ad3d0ae98be7b
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-04-18 23:26:32 +0800

    remove the loading counter

--- static/app/views/replays/detail/trace/index.tsx
+++ static/app/views/replays/detail/trace/index.tsx
@@ -1,7 +1,4 @@
-import styled from '@emotion/styled';
-
 import Placeholder from 'sentry/components/placeholder';
-import {tct} from 'sentry/locale';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import TraceView from 'sentry/views/performance/traceDetails/traceView';
@@ -13,7 +10,7 @@ type Props = {
   replayRecord: undefined | ReplayRecord;
 };
 
-function Transactions({replayRecord}: Props) {
+function Trace({replayRecord}: Props) {
   const location = useLocation();
   const organization = useOrganization();
   const {state, eventView} = useTransactionData();
@@ -21,13 +18,6 @@ function Transactions({replayRecord}: Props) {
   if (!replayRecord || !state.traces?.length) {
     return <Placeholder height="100%" />;
   }
-
-  const loading =
-    state.detailsRequests !== state.detailsResponses ? (
-      <LoadingCount>
-        {tct('Loaded [detailsResponses] of [detailsRequests]', state)}
-      </LoadingCount>
-    ) : null;
 
   return (
     <FluidHeight>
@@ -39,16 +29,8 @@ function Transactions({replayRecord}: Props) {
         traceEventView={eventView!}
         traceSlug="Replay"
       />
-      {loading}
     </FluidHeight>
   );
 }
 
-const LoadingCount = styled('div')`
-  position: absolute;
-  bottom: 0;
-  right: 0;
-  background: red;
-`;
-
-export default Transactions;
+export default Trace;
