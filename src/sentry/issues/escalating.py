46a0ee3388329ac464b814454f237ebc60215838
Armen Zambrano G <armenzg@sentry.io>
2023-04-19 15:51:23 -0400

    ref(escalating): Clean up the code (#47625)
    These code changes are in preparation to optimizing the Snuba query.

--- src/sentry/issues/escalating.py
+++ src/sentry/issues/escalating.py
@@ -1,3 +1,7 @@
+"""This module has the logic for querying Snuba for the hourly event count for a list of groups.
+This is later used for generating group forecasts for determining when a group may be escalating.
+"""
+
 from datetime import datetime, timedelta
 from typing import Dict, List, Tuple, TypedDict
 
@@ -17,11 +21,15 @@ from snuba_sdk import (
 
 from sentry.issues.escalating_issues_alg import GroupCount
 from sentry.models import Group
+from sentry.snuba.dataset import Dataset, EntityKey
 from sentry.utils.snuba import raw_snql_query
 
+__all__ = ["query_groups_past_counts", "parse_groups_past_counts"]
+
+REFERRER = "sentry.issues.escalating"
 QUERY_LIMIT = 10000  # This is the maximum value for Snuba
 # The amount of data needed to generate a group forecast
-SEVEN_DAYS_IN_HOURS = 7 * 24
+BUCKETS_PER_GROUP = 7 * 24
 
 GroupsCountResponse = TypedDict(
     "GroupsCountResponse",
@@ -33,15 +41,21 @@ ParsedGroupsCount = Dict[int, GroupCount]
 
 def query_groups_past_counts(groups: List[Group]) -> List[GroupsCountResponse]:
     """Query Snuba for the counts for every group bucketed into hours"""
-    offset = 0
-    all_results = []
     start_date, end_date = _start_and_end_dates()
     project_ids, group_ids = _extract_project_and_group_ids(groups)
+    return _query_with_pagination(project_ids, group_ids, start_date, end_date)
 
+
+def _query_with_pagination(
+    project_ids: List[int], group_ids: List[int], start_date: datetime, end_date: datetime
+) -> List[GroupsCountResponse]:
+
+    all_results = []
+    offset = 0
     while True:
-        query = _generate_query(group_ids, project_ids, offset, start_date, end_date)
-        request = Request(dataset="events", app_id="sentry.issues.escalating", query=query)
-        results = raw_snql_query(request, referrer="sentry.issues.escalating")["data"]
+        query = _generate_query(project_ids, group_ids, offset, start_date, end_date)
+        request = Request(dataset=Dataset.Events.value, app_id=REFERRER, query=query)
+        results = raw_snql_query(request, referrer=REFERRER)["data"]
         if not results:
             break
         else:
@@ -74,8 +88,8 @@ def parse_groups_past_counts(response: List[GroupsCountResponse]) -> ParsedGroup
 
 
 def _generate_query(
-    group_ids: List[int],
     project_ids: List[int],
+    group_ids: List[int],
     offset: int,
     start_date: datetime,
     end_date: datetime,
@@ -84,7 +98,7 @@ def _generate_query(
     group_id_col = Column("group_id")
     proj_id_col = Column("project_id")
     return Query(
-        match=Entity("events"),
+        match=Entity(EntityKey.Events.value),
         select=[
             proj_id_col,
             group_id_col,
@@ -108,7 +122,7 @@ def _generate_query(
     )
 
 
-def _start_and_end_dates(hours: int = SEVEN_DAYS_IN_HOURS) -> Tuple[datetime, datetime]:
+def _start_and_end_dates(hours: int = BUCKETS_PER_GROUP) -> Tuple[datetime, datetime]:
     """Return the start and end date of N hours time range."""
     end_datetime = datetime.now()
     return end_datetime - timedelta(hours=hours), end_datetime
