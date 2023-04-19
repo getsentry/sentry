17baf3c8dcd503cad53ea190118f5826a8fc706a
Stephen Cefali <scefali@sentry.io>
2023-04-19 11:56:10 -0700

    ref(analytics): kill off trackAdvancedAnalyticsEvent (#47648)
    This PR finally kills off `trackAdvancedAnalyticsEvent`
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/views/performance/transactionDetails/finishSetupAlert.tsx
+++ static/app/views/performance/transactionDetails/finishSetupAlert.tsx
@@ -6,7 +6,7 @@ import {IconLightning} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {Organization} from 'sentry/types';
-import trackAdvancedAnalyticsEvent from 'sentry/utils/analytics/trackAdvancedAnalyticsEvent';
+import {trackAnalytics} from 'sentry/utils/analytics';
 
 export default function FinishSetupAlert({
   organization,
@@ -29,7 +29,7 @@ export default function FinishSetupAlert({
         external
         href="https://docs.sentry.io/performance-monitoring/getting-started/"
         onClick={() =>
-          trackAdvancedAnalyticsEvent('growth.sample_transaction_docs_link_clicked', {
+          trackAnalytics('growth.sample_transaction_docs_link_clicked', {
             project_id: projectId,
             organization,
           })
