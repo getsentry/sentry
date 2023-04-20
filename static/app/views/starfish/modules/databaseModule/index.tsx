4549505f6d47e43f5bd8835ca4584e369687a31f
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-04-20 11:18:46 -0400

    feat(starfish): add table name to db module

--- static/app/views/starfish/modules/databaseModule/index.tsx
+++ static/app/views/starfish/modules/databaseModule/index.tsx
@@ -44,8 +44,28 @@ function getOptions() {
   });
 }
 
+function getTableOptions() {
+  const prefix = <span>{t('Table')}</span>;
+
+  return [
+    'auth_user',
+    'sentry_auditlogentry',
+    'accounts_charge',
+    'sentry_organizationmember_teams',
+    'sentry_team',
+    'sentry_groupmeta',
+  ].map(action => {
+    return {
+      value: action,
+      prefix,
+      label: action,
+    };
+  });
+}
+
 type State = {
   action: string;
+  table: string;
   transaction: string;
   selectedRow?: DataRow;
 };
@@ -54,10 +74,14 @@ class DatabaseModule extends Component<Props, State> {
   state: State = {
     action: 'SELECT',
     transaction: '',
+    table: 'auth_user',
   };
 
   handleOptionChange(value) {
     this.setState({action: value});
+  }
+  handleTableChange(value) {
+    this.setState({table: value});
   }
 
   handleSearch(query) {
@@ -76,7 +100,7 @@ class DatabaseModule extends Component<Props, State> {
 
   render() {
     const {location, organization} = this.props;
-    const {action, transaction} = this.state;
+    const {table, action, transaction} = this.state;
     const eventView = EventView.fromLocation(location);
     const setSelectedRow = (row: DataRow) => this.setState({selectedRow: row});
     const unsetSelectedSpanGroup = () => this.setState({selectedRow: undefined});
@@ -99,6 +123,11 @@ class DatabaseModule extends Component<Props, State> {
                 options={getOptions()}
                 onChange={opt => this.handleOptionChange(opt.value)}
               />
+              <CompactSelect
+                value={table}
+                options={getTableOptions()}
+                onChange={opt => this.handleTableChange(opt.value)}
+              />
               <TransactionNameSearchBar
                 organization={organization}
                 eventView={eventView}
@@ -108,6 +137,7 @@ class DatabaseModule extends Component<Props, State> {
               <DatabaseTableView
                 location={location}
                 action={action}
+                table={table}
                 transaction={transaction}
                 onSelect={setSelectedRow}
               />
