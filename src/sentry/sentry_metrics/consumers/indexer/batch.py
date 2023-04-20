b45677e53e9a9a9e08fd0a7a45c0891caba950b5
John Yang <john.yang@sentry.io>
2023-04-20 15:35:06 -0700

    fix schema validation ordering

--- src/sentry/sentry_metrics/consumers/indexer/batch.py
+++ src/sentry/sentry_metrics/consumers/indexer/batch.py
@@ -121,16 +121,6 @@ class IndexerBatch:
                 )
                 continue
             try:
-                parsed_payload["use_case_id"] = extract_use_case_id(parsed_payload["name"])
-            except ValidationError:
-                self.skipped_offsets.add(partition_offset)
-                logger.error(
-                    "process_messages.invalid_metric_resource_identifier",
-                    extra={"payload_value": str(msg.payload.value)},
-                    exc_info=True,
-                )
-                continue
-            try:
                 if self.__input_codec:
                     self.__input_codec.validate(parsed_payload)
             except ValidationError:
@@ -144,6 +134,16 @@ class IndexerBatch:
                     extra={"payload_value": str(msg.payload.value)},
                     exc_info=True,
                 )
+            try:
+                parsed_payload["use_case_id"] = extract_use_case_id(parsed_payload["name"])
+            except ValidationError:
+                self.skipped_offsets.add(partition_offset)
+                logger.error(
+                    "process_messages.invalid_metric_resource_identifier",
+                    extra={"payload_value": str(msg.payload.value)},
+                    exc_info=True,
+                )
+                continue
             self.parsed_payloads_by_offset[partition_offset] = parsed_payload
 
     @metrics.wraps("process_messages.filter_messages")
