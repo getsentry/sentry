bddb9b65365394159e25c8d751da9af52e69d36b
Ash Anand <0Calories@users.noreply.github.com>
2023-04-20 12:12:22 -0400

    feat(starfish): Create clickable sections on the failure rate chart (#47672)
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

---
+++ static/app/views/starfish/utils/insertClickableAreasIntoSeries.tsx
@@ -0,0 +1,55 @@
+import MarkArea from 'sentry/components/charts/components/markArea';
+import {LineChartSeries} from 'sentry/components/charts/lineChart';
+
+export function insertClickableAreasIntoSeries(series: LineChartSeries[], color: string) {
+  const {data} = series[0];
+  const startTime = data[0].name;
+  const endTime = data[data.length - 1].name;
+  const intervals = divideIntoIntervals(startTime as number, endTime as number, 8);
+
+  const areaMarkData = intervals.map(([start, end]) => [
+    {name: 'start', xAxis: start, emphasis: {disabled: false}},
+    {name: 'end', xAxis: end, emphasis: {disabled: false}},
+  ]);
+
+  series.push({
+    seriesName: 'Clickable Area',
+    color,
+    data: [],
+    silent: false,
+    emphasis: {disabled: false},
+    markArea: MarkArea({
+      silent: false,
+      itemStyle: {
+        color,
+        opacity: 0.2,
+      },
+      label: {
+        show: false,
+      },
+      emphasis: {disabled: false},
+      // I know this is gross but we don't have access to the types needed to satisfy the linter
+      data: areaMarkData as any,
+    }),
+  });
+}
+
+function divideIntoIntervals(startTime: number, endTime: number, numSections: number) {
+  const spaceBetweenIntervals = 10000000;
+  const diff = endTime - startTime;
+  const intervalLength = Math.round(diff / numSections);
+
+  const intervals: number[][] = [];
+  let currentTime = startTime;
+
+  while (currentTime < endTime) {
+    const currentInterval = [
+      currentTime,
+      currentTime + intervalLength - spaceBetweenIntervals,
+    ];
+    intervals.push(currentInterval);
+    currentTime += intervalLength;
+  }
+
+  return intervals;
+}
