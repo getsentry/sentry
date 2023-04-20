05996af05b3de9c5fa5970363799e7a26a89cf50
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-04-21 00:04:00 +0800

    feat(replays): Improve loading and error display for the Trace tab on Replay Details (#47704)
    There are three UI stages that you'll see when loading the trace tab
    inside replay.
    1. Page is initializing, loading has not started yet (no spinner)
    - we're waiting for the replayRecord, once we have that record we can
    show the header
    2. We're starting to fetch data (spinner)
    - We're waiting for the first page of traceIds, and then some
    traceDetails to arrive
    3. One of a) no traces b) errors happened c) data is loading
    incrementally
    - Some trace specific endpoints have returned, and we can show results.
    
    
    Screen shots of each:
    | Example | Stage 1 - Page init | Stage 2 - Data fetch | Stage 3 -
    Result |
    | --- | --- | --- | --- |
    | Result is no traces | <img width="1364" alt="preload"
    src="https://user-images.githubusercontent.com/187460/233372694-f613cdce-9969-4928-acf8-5e6148321964.png">
    | <img width="1364" alt="fetch started"
    src="https://user-images.githubusercontent.com/187460/233372783-17e9afbc-8552-4c65-88d2-bf9e95c340b3.png">
    | <img width="1364" alt="no traces"
    src="https://user-images.githubusercontent.com/187460/233372830-f46e4ff3-693f-4113-914b-b441c07dd978.png">
    |
    | Results in errors | <img width="1364" alt="preload"
    src="https://user-images.githubusercontent.com/187460/233372694-f613cdce-9969-4928-acf8-5e6148321964.png">
    | <img width="1364" alt="fetch started"
    src="https://user-images.githubusercontent.com/187460/233372783-17e9afbc-8552-4c65-88d2-bf9e95c340b3.png">
    | <img width="1364" alt="with error"
    src="https://user-images.githubusercontent.com/187460/233372877-032b7c5b-651d-4c9a-acc3-443a70777ca0.png">
    |
    | Results in seeing the trace! | <img width="1364" alt="preload"
    src="https://user-images.githubusercontent.com/187460/233372694-f613cdce-9969-4928-acf8-5e6148321964.png">
    | <img width="1364" alt="fetch started"
    src="https://user-images.githubusercontent.com/187460/233372783-17e9afbc-8552-4c65-88d2-bf9e95c340b3.png">
    | <img width="1364" alt="done"
    src="https://user-images.githubusercontent.com/187460/233372889-65ed8dbf-08cc-4d70-9c72-9a5ec7582a50.png">
    |
    
    
    This also replaces `location` as a dependency in the hook with an empty
    object. We don't rely on the url to setup our pagination/sort/etc for
    this component, but having that var as a dependency meant that the
    memoized `useCallback` results would change too often. Without that
    dependency we don't change object identity anymore, and re-fetch the
    data. Data is scoped to each instance of the context provider.
    
    Fixes https://github.com/getsentry/team-replay/issues/60

--- static/app/views/replays/detail/trace/trace.tsx
+++ static/app/views/replays/detail/trace/trace.tsx
@@ -2,9 +2,12 @@ import styled from '@emotion/styled';
 
 import Loading from 'sentry/components/loadingIndicator';
 import Placeholder from 'sentry/components/placeholder';
+import {IconSad} from 'sentry/icons';
+import {t} from 'sentry/locale';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import TraceView from 'sentry/views/performance/traceDetails/traceView';
+import EmptyState from 'sentry/views/replays/detail/emptyState';
 import FluidHeight from 'sentry/views/replays/detail/layout/fluidHeight';
 import {
   useFetchTransactions,
@@ -19,15 +22,41 @@ type Props = {
 function Trace({replayRecord}: Props) {
   const location = useLocation();
   const organization = useOrganization();
-  const {state, eventView} = useTransactionData();
+  const {
+    state: {didInit, errors, isFetching, traces},
+    eventView,
+  } = useTransactionData();
 
   useFetchTransactions();
 
-  if (!replayRecord || !state.traces?.length) {
+  if (!replayRecord || !didInit || isFetching) {
+    // Show the blank screen until we start fetching, thats when you get a spinner
     return (
       <StyledPlaceholder height="100%">
-        {state.isFetching && <Loading />}
+        {isFetching ? <Loading /> : null}
       </StyledPlaceholder>
+    );
+  }
+
+  if (errors.length) {
+    // Same style as <EmptyStateWarning>
+    return (
+      <BorderedSection>
+        <EmptyState withIcon={false}>
+          <IconSad legacySize="54px" />
+          <p>{t('Unable to retrieve traces')}</p>
+        </EmptyState>
+      </BorderedSection>
+    );
+  }
+
+  if (!traces?.length) {
+    return (
+      <BorderedSection>
+        <EmptyState>
+          <p>{t('No traces found')}</p>
+        </EmptyState>
+      </BorderedSection>
     );
   }
 
@@ -35,7 +64,7 @@ function Trace({replayRecord}: Props) {
     <FluidHeight>
       <TraceView
         meta={null}
-        traces={state.traces ?? null}
+        traces={traces ?? null}
         location={location}
         organization={organization}
         traceEventView={eventView!}
@@ -45,7 +74,14 @@ function Trace({replayRecord}: Props) {
   );
 }
 
+// This has the gray background, to match other loaders on Replay Details
 const StyledPlaceholder = styled(Placeholder)`
+  border: 1px solid ${p => p.theme.border};
+  border-radius: ${p => p.theme.borderRadius};
+`;
+
+// White background, to match the loaded component
+const BorderedSection = styled(FluidHeight)`
   border: 1px solid ${p => p.theme.border};
   border-radius: ${p => p.theme.borderRadius};
 `;
