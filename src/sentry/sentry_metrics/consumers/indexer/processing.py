6157c58e661d48b9fe0e2b474c80ca063e620dec
John <john.yang@sentry.io>
2023-04-20 12:48:56 -0700

    feat(generic metrics): Implement use case logic in IndexerBatch (#47113)
    ### Overview
    - Implement use case logic in `IndexerBatch`
    
    ### Changes
    - Use `ParsedMessage` type instead of `IngestMetric` to serve as
    internal representation of a parsed ingest metric message for indexing
    pipeline to support generic metrics.
    - `_extract_messages` method now parse metric name to obtain the use
    case ID for each `ParsedMessage`, and inserts the use case ID into the
    `use_case_id` field.
    - `extract_strings` method now returns mapping with a higher level of
    indirection with `use_case_id` on top
    
    Closes [SNS-2214](https://getsentry.atlassian.net/browse/SNS-2214)
    Closes [SNS-2215](https://getsentry.atlassian.net/browse/SNS-2215)
    
    [SNS-2214]:
    https://getsentry.atlassian.net/browse/SNS-2214?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    [SNS-2126]:
    https://getsentry.atlassian.net/browse/SNS-2126?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    [SNS-2215]:
    https://getsentry.atlassian.net/browse/SNS-2215?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ

--- src/sentry/sentry_metrics/consumers/indexer/processing.py
+++ src/sentry/sentry_metrics/consumers/indexer/processing.py
@@ -86,7 +86,6 @@ class MessageProcessor:
         is_output_sliced = self._config.is_output_sliced or False
 
         batch = IndexerBatch(
-            self._config.use_case_id,
             outer_message,
             should_index_tag_values=should_index_tag_values,
             is_output_sliced=is_output_sliced,
@@ -100,7 +99,7 @@ class MessageProcessor:
         ):
             cardinality_limiter = cardinality_limiter_factory.get_ratelimiter(self._config)
             cardinality_limiter_state = cardinality_limiter.check_cardinality_limits(
-                batch.use_case_id, batch.parsed_payloads_by_offset
+                self._config.use_case_id, batch.parsed_payloads_by_offset
             )
 
         sdk.set_measurement(
@@ -108,7 +107,8 @@ class MessageProcessor:
         )
         batch.filter_messages(cardinality_limiter_state.keys_to_remove)
 
-        org_strings = batch.extract_strings()
+        extracted_strings = batch.extract_strings()
+        org_strings = next(iter(extracted_strings.values())) if extracted_strings else {}
 
         sdk.set_measurement("org_strings.len", len(org_strings))
 
