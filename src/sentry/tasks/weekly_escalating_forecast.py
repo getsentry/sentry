48aef2d1cd94d2b93a70f45515416aae2baec831
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2023-04-21 15:13:59 -0700

    feat(escalating-issues): Optimize the query getting the until-escalating groups

--- src/sentry/tasks/weekly_escalating_forecast.py
+++ src/sentry/tasks/weekly_escalating_forecast.py
@@ -4,9 +4,10 @@ from typing import Dict, List, TypedDict
 from sentry_sdk.crons.decorator import monitor
 
 from sentry.issues.forecasts import generate_and_save_forecasts
-from sentry.models import Group, GroupStatus
+from sentry.models import Group, GroupStatus, ObjectStatus, Project
 from sentry.tasks.base import instrumented_task
 from sentry.types.group import GroupSubStatus
+from sentry.utils.query import RangeQuerySetWrapper
 
 
 class GroupCount(TypedDict):
@@ -30,20 +31,28 @@ def run_escalating_forecast() -> None:
     Run the escalating forecast algorithm on archived until escalating issues.
     """
     logger.info("Starting task for sentry.tasks.weekly_escalating_forecast.run_escalating_forecast")
-    # TODO: Do not limit to project id = 1 and limit 10 once these topics are clarified
-    # TODO: If possible, fetch group_id instead of the entire group model
-    until_escalating_groups = list(
-        Group.objects.filter(
-            status=GroupStatus.IGNORED,
-            substatus=GroupSubStatus.UNTIL_ESCALATING,
-            project__id=1,
-        )[:10]
-    )
-    logger.info(
-        "Checking for archived until escalating groups",
-        extra={"num_groups": len(until_escalating_groups)},
-    )
-    if not until_escalating_groups:
-        return
 
-    generate_and_save_forecasts(until_escalating_groups)
+    for projects in RangeQuerySetWrapper(
+        Project.objects.filter(status=ObjectStatus.VISIBLE).values_list("id", flat=True),
+        result_value_getter=lambda item: item,
+        step=10000,
+    ):
+        generate_forecasts_for_projects(projects)
+
+
+@instrumented_task(
+    name="sentry.tasks.weekly_escalating_forecast.generate_forecasts_for_projects",
+    queue="weekly_escalating_forecast",
+    max_retries=3,
+    default_retry_delay=60,
+)  # type: ignore
+def generate_forecasts_for_projects(projects: List[int]) -> None:
+    for project_id in projects:
+        for until_escalating_groups in RangeQuerySetWrapper(
+            Group.objects.filter(
+                status=GroupStatus.IGNORED,
+                substatus=GroupSubStatus.UNTIL_ESCALATING,
+                project__id=project_id,
+            )
+        ):
+            generate_and_save_forecasts(until_escalating_groups)
