6877a9b3172bebe2d4063cf303743e6db3242634
Scott Cooper <scttcper@gmail.com>
2023-04-21 14:10:34 -0700

    fix(workflow): Replace ignored with archived in issue status

--- static/app/components/mutedBox.tsx
+++ static/app/components/mutedBox.tsx
@@ -4,19 +4,26 @@ import {BannerContainer, BannerSummary} from 'sentry/components/events/styles';
 import {IconMute} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {ResolutionStatusDetails} from 'sentry/types';
+import useOrganization from 'sentry/utils/useOrganization';
 
 type Props = {
   statusDetails: ResolutionStatusDetails;
 };
 
 function MutedBox({statusDetails}: Props) {
+  const organization = useOrganization();
+
   function renderReason() {
     const {ignoreUntil, ignoreCount, ignoreWindow, ignoreUserCount, ignoreUserWindow} =
       statusDetails;
 
+    const hasEscalatingUi = organization.features.includes('escalating-issues-ui');
+    const ignoredOrArchived = hasEscalatingUi ? t('Archived') : t('Ignored');
+
     if (ignoreUntil) {
       return t(
-        'This issue has been ignored until %s',
+        'This issue has been %s until %s',
+        ignoredOrArchived,
         <strong>
           <DateTime date={ignoreUntil} />
         </strong>
@@ -24,7 +31,8 @@ function MutedBox({statusDetails}: Props) {
     }
     if (ignoreCount && ignoreWindow) {
       return t(
-        'This issue has been ignored until it occurs %s time(s) in %s',
+        'This issue has been %s until it occurs %s time(s) in %s',
+        ignoredOrArchived,
         <strong>{ignoreCount.toLocaleString()}</strong>,
         <strong>
           <Duration seconds={ignoreWindow * 60} />
@@ -33,13 +41,15 @@ function MutedBox({statusDetails}: Props) {
     }
     if (ignoreCount) {
       return t(
-        'This issue has been ignored until it occurs %s more time(s)',
+        'This issue has been %s until it occurs %s more time(s)',
+        ignoredOrArchived,
         <strong>{ignoreCount.toLocaleString()}</strong>
       );
     }
     if (ignoreUserCount && ignoreUserWindow) {
       return t(
-        'This issue has been ignored until it affects %s user(s) in %s',
+        'This issue has been %s until it affects %s user(s) in %s',
+        ignoredOrArchived,
         <strong>{ignoreUserCount.toLocaleString()}</strong>,
         <strong>
           <Duration seconds={ignoreUserWindow * 60} />
@@ -48,12 +58,13 @@ function MutedBox({statusDetails}: Props) {
     }
     if (ignoreUserCount) {
       return t(
-        'This issue has been ignored until it affects %s more user(s)',
+        'This issue has been %s until it affects %s more user(s)',
+        ignoredOrArchived,
         <strong>{ignoreUserCount.toLocaleString()}</strong>
       );
     }
 
-    return t('This issue has been ignored');
+    return t('This issue has been %s', ignoredOrArchived);
   }
 
   return (
