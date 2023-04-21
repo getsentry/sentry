65cc16916ec1709fea8a3fd05c2012003afed3c5
Valery Brobbey <valery.brobbey@sentry.io>
2023-04-21 11:14:16 -0700

    feat(hc): denormalize user and team into NotificationSetting (#47118)

--- src/sentry/receivers/outbox/region.py
+++ src/sentry/receivers/outbox/region.py
@@ -18,6 +18,7 @@ from sentry.models import (
     Project,
     process_region_outbox,
 )
+from sentry.models.team import Team
 from sentry.receivers.outbox import maybe_process_tombstone
 from sentry.services.hybrid_cloud.identity import identity_service
 from sentry.services.hybrid_cloud.log import AuditLogEvent, UserIpEvent
@@ -66,6 +67,13 @@ def process_organization_member_updates(
         return
 
     organizationmember_mapping_service.create_with_organization_member(org_member=org_member)
+
+
+@receiver(process_region_outbox, sender=OutboxCategory.TEAM_UPDATE)
+def process_team_updates(
+    object_identifier: int, payload: Any, shard_identifier: int, **kwargs: Any
+):
+    maybe_process_tombstone(Team, object_identifier)
 
 
 @receiver(process_region_outbox, sender=OutboxCategory.ORGANIZATION_UPDATE)
