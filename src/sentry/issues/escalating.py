5285a8b1b478cf0b814f63f4478bbcb291a0a4c6
Armen Zambrano G <armenzg@sentry.io>
2023-04-24 10:20:27 -0400

    ref(escalating_issues): Improve code and typing

--- src/sentry/issues/escalating.py
+++ src/sentry/issues/escalating.py
@@ -1,9 +1,8 @@
 """This module has the logic for querying Snuba for the hourly event count for a list of groups.
 This is later used for generating group forecasts for determining when a group may be escalating.
 """
-
 from datetime import datetime, timedelta
-from typing import Dict, List, Tuple, TypedDict
+from typing import Dict, List, Sequence, Tuple, TypedDict
 
 from snuba_sdk import (
     Column,
@@ -27,7 +26,7 @@ from sentry.utils.snuba import raw_snql_query
 __all__ = ["query_groups_past_counts", "parse_groups_past_counts"]
 
 REFERRER = "sentry.issues.escalating"
-QUERY_LIMIT = 10000  # This is the maximum value for Snuba
+ELEMENTS_PER_SNUBA_PAGE = 10000  # This is the maximum value for Snuba
 # The amount of data needed to generate a group forecast
 BUCKETS_PER_GROUP = 7 * 24
 
@@ -39,7 +38,7 @@ GroupsCountResponse = TypedDict(
 ParsedGroupsCount = Dict[int, GroupCount]
 
 
-def query_groups_past_counts(groups: List[Group]) -> List[GroupsCountResponse]:
+def query_groups_past_counts(groups: Sequence[Group]) -> List[GroupsCountResponse]:
     """Query Snuba for the counts for every group bucketed into hours"""
     start_date, end_date = _start_and_end_dates()
     project_ids, group_ids = _extract_project_and_group_ids(groups)
@@ -47,25 +46,25 @@ def query_groups_past_counts(groups: List[Group]) -> List[GroupsCountResponse]:
 
 
 def _query_with_pagination(
-    project_ids: List[int], group_ids: List[int], start_date: datetime, end_date: datetime
+    project_ids: Sequence[int], group_ids: Sequence[int], start_date: datetime, end_date: datetime
 ) -> List[GroupsCountResponse]:
-
+    """Query Snuba for event counts for the given list of project ids and groups ids in
+    a time range."""
     all_results = []
     offset = 0
     while True:
         query = _generate_query(project_ids, group_ids, offset, start_date, end_date)
         request = Request(dataset=Dataset.Events.value, app_id=REFERRER, query=query)
         results = raw_snql_query(request, referrer=REFERRER)["data"]
-        if not results:
+        all_results += results
+        offset += ELEMENTS_PER_SNUBA_PAGE
+        if not results or len(results) < ELEMENTS_PER_SNUBA_PAGE:
             break
-        else:
-            all_results += results
-            offset += QUERY_LIMIT
 
     return all_results
 
 
-def parse_groups_past_counts(response: List[GroupsCountResponse]) -> ParsedGroupsCount:
+def parse_groups_past_counts(response: Sequence[GroupsCountResponse]) -> ParsedGroupsCount:
     """
     Return the parsed snuba response for groups past counts to be used in generate_issue_forecast.
     ParsedGroupCount is of the form {<group_id>: {"intervals": [str], "data": [int]}}.
@@ -88,8 +87,8 @@ def parse_groups_past_counts(response: List[GroupsCountResponse]) -> ParsedGroup
 
 
 def _generate_query(
-    project_ids: List[int],
-    group_ids: List[int],
+    project_ids: Sequence[int],
+    group_ids: Sequence[int],
     offset: int,
     start_date: datetime,
     end_date: datetime,
@@ -112,7 +111,7 @@ def _generate_query(
             Condition(Column("timestamp"), Op.GTE, start_date),
             Condition(Column("timestamp"), Op.LT, end_date),
         ],
-        limit=Limit(QUERY_LIMIT),
+        limit=Limit(ELEMENTS_PER_SNUBA_PAGE),
         offset=Offset(offset),
         orderby=[
             OrderBy(proj_id_col, Direction.ASC),
@@ -128,7 +127,7 @@ def _start_and_end_dates(hours: int = BUCKETS_PER_GROUP) -> Tuple[datetime, date
     return end_datetime - timedelta(hours=hours), end_datetime
 
 
-def _extract_project_and_group_ids(groups: List[Group]) -> Tuple[List[int], List[int]]:
+def _extract_project_and_group_ids(groups: Sequence[Group]) -> Tuple[List[int], List[int]]:
     """Return all project and group IDs from a list of Group"""
     group_ids = []
     project_ids = []
