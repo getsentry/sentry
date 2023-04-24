24aac4da7fc2cf3ca07221bf92608907de04e696
Katie Byers <lobsterkatie@gmail.com>
2023-04-24 08:56:26 -0700

    log instead of sending to sentry for atlassian security bot

--- src/sentry/integrations/jira/webhooks/base.py
+++ src/sentry/integrations/jira/webhooks/base.py
@@ -6,6 +6,7 @@ from typing import Any, Mapping
 
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework import status
+from rest_framework.exceptions import MethodNotAllowed
 from rest_framework.request import Request
 from rest_framework.response import Response
 from sentry_sdk import Scope
@@ -42,6 +43,21 @@ class JiraWebhookBase(Endpoint, abc.ABC):
     ) -> Response:
         if isinstance(exc, (AtlassianConnectValidationError, JiraTokenError)):
             return self.respond(status=status.HTTP_400_BAD_REQUEST)
+
+        # Atlassian has an automated tool which tests to make sure integrations with Jira
+        # (like ours) pass certain security requirements, which leads them to probe certain
+        # of our endpoints to make sure we're not accepting insecure GET requests. Not
+        # actionable on our part and therefore not worth sending to Sentry.
+        if isinstance(
+            exc, MethodNotAllowed
+        ) and "github.com/atlassian-labs/connect-security-req-tester" in request.headers.get(
+            "User-Agent", ""
+        ):
+            logger.info(
+                "Atlassian Connect Security Request Tester tried disallowed method",
+                extra={"path": request.path, "method": request.method},
+            )
+            return self.respond(status=status.HTTP_405_METHOD_NOT_ALLOWED)
         # Perhaps it makes sense to do this in the base class, however, I'm concerned
         # it would create too many errors at once and may be grouped together
         logger.exception("Unclear JIRA exception.")
