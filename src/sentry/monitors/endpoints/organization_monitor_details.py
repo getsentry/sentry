dfbc0263c75208bb1a823dcb1a40617b0914a8cb
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-04-25 13:30:45 -0700

    docs(crons): Add environment param to delete monitor docs (#47914)
    Adds environment param to `DELETE` request for monitor endpoint

--- src/sentry/monitors/endpoints/organization_monitor_details.py
+++ src/sentry/monitors/endpoints/organization_monitor_details.py
@@ -122,10 +122,11 @@ class OrganizationMonitorDetailsEndpoint(MonitorEndpoint):
         return self.respond(serialize(monitor, request.user))
 
     @extend_schema(
-        operation_id="Delete a monitor or monitor environment",
+        operation_id="Delete a monitor or monitor environments",
         parameters=[
             GLOBAL_PARAMS.ORG_SLUG,
             MONITOR_PARAMS.MONITOR_SLUG,
+            GLOBAL_PARAMS.ENVIRONMENT,
         ],
         request=MonitorValidator,
         responses={
@@ -137,14 +138,14 @@ class OrganizationMonitorDetailsEndpoint(MonitorEndpoint):
     )
     def delete(self, request: Request, organization, project, monitor) -> Response:
         """
-        Delete a monitor or monitor environment.
+        Delete a monitor or monitor environments.
         """
-        environment_name = request.query_params.get("environment")
+        environment_names = request.query_params.getlist("environment")
         with transaction.atomic():
-            if environment_name:
+            if environment_names:
                 monitor_object = (
                     MonitorEnvironment.objects.filter(
-                        environment__name=environment_name, monitor__id=monitor.id
+                        environment__name__in=environment_names, monitor__id=monitor.id
                     )
                     .exclude(
                         monitor__status__in=[
