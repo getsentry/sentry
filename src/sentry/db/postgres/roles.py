b79647c08e08eba9037af1d3929404e1c0a249c2
Alberto Leal <mail4alberto@gmail.com>
2023-04-25 16:05:45 -0400

    ignore tests/sentry/models/test_organizationmember.py

--- src/sentry/db/postgres/roles.py
+++ src/sentry/db/postgres/roles.py
@@ -26,3 +26,24 @@ def in_test_psql_role_override(role_name: str, using: str | None = None):
             yield
         finally:
             conn.execute("SET ROLE %s", [cur])
+
+
+def set_test_psql_role_override(role_name: str, using: str | None = None):
+    """
+    During test runs, the role of the current connection will be swapped with role_name, and then swapped
+    back to its original.  Has no effect in production.
+    """
+
+    if "pytest" not in sys.modules or os.environ.get("DB", "postgres") != "postgres":
+        return
+
+    with get_connection(using).cursor() as conn:
+        conn.execute("SELECT user")
+        (cur,) = conn.fetchone()
+        conn.execute("SET ROLE %s", [role_name])
+
+        def restore():
+            with get_connection(using).cursor() as conn:
+                conn.execute("SET ROLE %s", [cur])
+
+        return restore
