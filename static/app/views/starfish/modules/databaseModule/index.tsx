a247ca88371668fefdabae693774e46ca3baf80b
William Mak <william@wmak.io>
2023-04-25 11:06:50 -0400

    feat(starfish): yet another db workflow (#47845)
    - This updates the db workflow so there's a clearer path from operation
    graph -> table graph -> the table of queries

--- static/app/views/starfish/modules/databaseModule/index.tsx
+++ static/app/views/starfish/modules/databaseModule/index.tsx
@@ -1,18 +1,13 @@
 import {Component} from 'react';
 import {Location} from 'history';
 
-import {CompactSelect} from 'sentry/components/compactSelect';
 import * as Layout from 'sentry/components/layouts/thirds';
-import TransactionNameSearchBar from 'sentry/components/performance/searchBar';
 import {t} from 'sentry/locale';
-import {Organization} from 'sentry/types';
-import EventView from 'sentry/utils/discover/eventView';
 import {
   PageErrorAlert,
   PageErrorProvider,
 } from 'sentry/utils/performance/contexts/pageError';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
-import withOrganization from 'sentry/utils/withOrganization';
 
 import DatabaseChartView from './databaseChartView';
 import DatabaseTableView, {DataRow} from './databaseTableView';
@@ -20,60 +15,7 @@ import QueryDetail from './panel';
 
 type Props = {
   location: Location;
-  organization: Organization;
 };
-
-function getOptions() {
-  const prefix = <span>{t('Operation')}</span>;
-
-  return [
-    'ALL',
-    'DELETE',
-    'INSERT',
-    'ROLLBACK',
-    'SAVEPOINT',
-    'SELECT',
-    'UPDATE',
-    'connect',
-    'delete',
-  ].map(action => {
-    return {
-      value: action,
-      prefix,
-      label: action,
-    };
-  });
-}
-
-function getTableOptions() {
-  const prefix = <span>{t('Table')}</span>;
-
-  return [
-    'ALL',
-    'auth_user',
-    'sentry_useroption',
-    'sentry_organizationmember',
-    'sentry_organization',
-    'sentry_project',
-    'sentry_useremail',
-    'sentry_auditlogentry',
-    'accounts_charge',
-    'sentry_organizationmember_teams',
-    'sentry_team',
-    'sentry_useravatar',
-    'sentry_groupmeta',
-    'sentry_release_project',
-    'auth_authenticator',
-    'policy_policy',
-    'sentry_grouplink',
-  ].map(action => {
-    return {
-      value: action,
-      prefix,
-      label: action,
-    };
-  });
-}
 
 type State = {
   action: string;
@@ -89,13 +31,6 @@ class DatabaseModule extends Component<Props, State> {
     table: 'ALL',
   };
 
-  handleOptionChange(value) {
-    this.setState({action: value});
-  }
-  handleTableChange(value) {
-    this.setState({table: value});
-  }
-
   handleSearch(query) {
     const conditions = new MutableSearch(query);
     const transactionValues = conditions.getFilterValues('transaction');
@@ -103,7 +38,6 @@ class DatabaseModule extends Component<Props, State> {
       return this.setState({transaction: transactionValues[0]});
     }
     if (conditions.freeText.length > 0) {
-      // raw text query will be wrapped in wildcards in generatePerformanceEventView
       // so no need to wrap it here
       return this.setState({transaction: conditions.freeText.join(' ')});
     }
@@ -111,9 +45,8 @@ class DatabaseModule extends Component<Props, State> {
   }
 
   render() {
-    const {location, organization} = this.props;
+    const {location} = this.props;
     const {table, action, transaction} = this.state;
-    const eventView = EventView.fromLocation(location);
     const setSelectedRow = (row: DataRow) => this.setState({selectedRow: row});
     const unsetSelectedSpanGroup = () => this.setState({selectedRow: undefined});
 
@@ -129,22 +62,18 @@ class DatabaseModule extends Component<Props, State> {
           <Layout.Body>
             <Layout.Main fullWidth>
               <PageErrorAlert />
-              <DatabaseChartView location={location} />
-              <CompactSelect
-                value={action}
-                options={getOptions()}
-                onChange={opt => this.handleOptionChange(opt.value)}
-              />
-              <CompactSelect
-                value={table}
-                options={getTableOptions()}
-                onChange={opt => this.handleTableChange(opt.value)}
-              />
-              <TransactionNameSearchBar
-                organization={organization}
-                eventView={eventView}
-                onSearch={(query: string) => this.handleSearch(query)}
-                query={transaction}
+              <DatabaseChartView
+                location={location}
+                action={action}
+                table={table}
+                onChange={(key, val) => {
+                  if (key === 'action') {
+                    this.setState({action: val});
+                    this.setState({table: 'ALL'});
+                  } else {
+                    this.setState({table: val});
+                  }
+                }}
               />
               <DatabaseTableView
                 location={location}
@@ -165,4 +94,4 @@ class DatabaseModule extends Component<Props, State> {
   }
 }
 
-export default withOrganization(DatabaseModule);
+export default DatabaseModule;
