fb96b8a6064cbe1ced6a73b11a105c4dfc5a44ef
JonasBa <jonas@badalic.com>
2023-04-25 10:08:09 -0400

    fix(profiling): use new badge

--- static/app/views/profiling/content.tsx
+++ static/app/views/profiling/content.tsx
@@ -179,7 +179,11 @@ function ProfilingContent({location}: ProfilingContentProps) {
                     'Profiling collects detailed information in production about the functions executing in your application and how long they take to run, giving you code-level visibility into your hot paths.'
                   )}
                 />
-                {isProfilingGA ? null : <FeatureBadge type="beta" />}
+                {isProfilingGA ? (
+                  <FeatureBadge type="new" />
+                ) : (
+                  <FeatureBadge type="beta" />
+                )}
               </Layout.Title>
             </Layout.HeaderContent>
             <Layout.HeaderActions>
