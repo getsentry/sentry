ce0daec3409a86ca1c110994072130ab3a08ca87
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-04-25 14:49:45 -0700

    replay(ref): Replace calls to getPrevReplayEvent() in Replay Details console/network/dom tabs. (#47911)
    This fixes some issues where the current/hover line wasn't appearing for
    the first & last items in console/network/dom lists
    
    Example where I've clicked on the first Console item in the list, at
    time 0:00:
    | Before | After |
    | --- | --- |
    |
    ![SCR-20230425-kjek](https://user-images.githubusercontent.com/187460/234368251-273010d3-61d7-420f-8f92-b0e4ff25410c.png)
    |
    ![SCR-20230425-kjhj](https://user-images.githubusercontent.com/187460/234368275-bbfba4bf-6c92-412a-915f-356252ba3d53.png)
    |
    
    Now with the css powering things the line appears!
    
    It's also more reliable to show the purple line when timestamps are the
    'same', and near errors:
    | Before | After |
    | --- | --- |
    |
    ![SCR-20230425-kkor](https://user-images.githubusercontent.com/187460/234369066-c3594771-96b0-459c-8e0f-864803d3620c.png)
    |
    ![SCR-20230425-kktn](https://user-images.githubusercontent.com/187460/234369073-c61ff466-f578-4c63-ba2a-c2cec4431af9.png)
    |
    
    Havn't done any profiling, but i hope that there's a savings from not
    calling `getPrevReplayEvent`.
    Also, as a followup we should really be caching all the `new
    Date(breadcrumb.timestamp)` and `relativeTimeInMs()` calls. This should
    happen inside breadcrumbFactory probably and would affect the type we're
    using, which is why it's a good followup and not part of this.

--- static/app/views/replays/detail/domMutations/index.tsx
+++ static/app/views/replays/detail/domMutations/index.tsx
@@ -5,12 +5,10 @@ import {
   List as ReactVirtualizedList,
   ListRowProps,
 } from 'react-virtualized';
-import styled from '@emotion/styled';
 
 import Placeholder from 'sentry/components/placeholder';
 import {useReplayContext} from 'sentry/components/replays/replayContext';
 import {t} from 'sentry/locale';
-import {getPrevReplayEvent} from 'sentry/utils/replays/getReplayEvent';
 import useExtractedCrumbHtml from 'sentry/utils/replays/hooks/useExtractedCrumbHtml';
 import type ReplayReader from 'sentry/utils/replays/replayReader';
 import DomFilters from 'sentry/views/replays/detail/domMutations/domFilters';
@@ -18,6 +16,7 @@ import DomMutationRow from 'sentry/views/replays/detail/domMutations/domMutation
 import useDomFilters from 'sentry/views/replays/detail/domMutations/useDomFilters';
 import FluidHeight from 'sentry/views/replays/detail/layout/fluidHeight';
 import NoRowRenderer from 'sentry/views/replays/detail/noRowRenderer';
+import TabItemContainer from 'sentry/views/replays/detail/tabItemContainer';
 import useVirtualizedList from 'sentry/views/replays/detail/useVirtualizedList';
 
 type Props = {
@@ -42,38 +41,6 @@ function DomMutations({replay, startTimestampMs}: Props) {
 
   const listRef = useRef<ReactVirtualizedList>(null);
 
-  const itemLookup = useMemo(
-    () =>
-      items &&
-      items
-        .map(({timestamp}, i) => [+new Date(timestamp || ''), i])
-        .sort(([a], [b]) => a - b),
-    [items]
-  );
-
-  const breadcrumbs = useMemo(() => items.map(({crumb}) => crumb), [items]);
-  const current = useMemo(
-    () =>
-      getPrevReplayEvent({
-        itemLookup,
-        items: breadcrumbs,
-        targetTimestampMs: startTimestampMs + currentTime,
-      }),
-    [itemLookup, breadcrumbs, currentTime, startTimestampMs]
-  );
-
-  const hovered = useMemo(
-    () =>
-      currentHoverTime
-        ? getPrevReplayEvent({
-            itemLookup,
-            items: breadcrumbs,
-            targetTimestampMs: startTimestampMs + currentHoverTime,
-          })
-        : null,
-    [itemLookup, breadcrumbs, currentHoverTime, startTimestampMs]
-  );
-
   const deps = useMemo(() => [items], [items]);
   const {cache, updateList} = useVirtualizedList({
     cellMeasurer,
@@ -93,8 +60,8 @@ function DomMutations({replay, startTimestampMs}: Props) {
         rowIndex={index}
       >
         <DomMutationRow
-          isCurrent={mutation.crumb.id === current?.id}
-          isHovered={mutation.crumb.id === hovered?.id}
+          currentTime={currentTime}
+          currentHoverTime={currentHoverTime}
           mutation={mutation}
           startTimestampMs={startTimestampMs}
           style={style}
@@ -106,7 +73,7 @@ function DomMutations({replay, startTimestampMs}: Props) {
   return (
     <FluidHeight>
       <DomFilters actions={actions} {...filterProps} />
-      <MutationItemContainer>
+      <TabItemContainer>
         {isLoading || !actions ? (
           <Placeholder height="100%" />
         ) : (
@@ -133,17 +100,9 @@ function DomMutations({replay, startTimestampMs}: Props) {
             )}
           </AutoSizer>
         )}
-      </MutationItemContainer>
+      </TabItemContainer>
     </FluidHeight>
   );
 }
-
-const MutationItemContainer = styled('div')`
-  position: relative;
-  height: 100%;
-  overflow: hidden;
-  border: 1px solid ${p => p.theme.border};
-  border-radius: ${p => p.theme.borderRadius};
-`;
 
 export default memo(DomMutations);
