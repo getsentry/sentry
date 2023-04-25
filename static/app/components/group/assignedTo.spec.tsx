69e8b00774087c9a113535fde5e46c4ee5d22da6
Scott Cooper <scttcper@gmail.com>
2023-04-25 14:23:15 -0700

    fix update tests to allow for the new requests

--- static/app/components/group/assignedTo.spec.tsx
+++ static/app/components/group/assignedTo.spec.tsx
@@ -59,6 +59,14 @@ describe('Group > AssignedTo', () => {
       url: '/organizations/org-slug/users/',
       body: [{user: USER_1}, {user: USER_2}],
     });
+    MockApiClient.addMockResponse({
+      url: `/projects/${organization.slug}/${project.slug}/events/${event.id}/committers/`,
+      body: {},
+    });
+    MockApiClient.addMockResponse({
+      url: `/projects/${organization.slug}/${project.slug}/events/${event.id}/owners/`,
+      body: {owners: [], rules: []},
+    });
   });
 
   afterEach(() => {
@@ -76,18 +84,19 @@ describe('Group > AssignedTo', () => {
     });
   };
 
-  it('renders unassigned', () => {
+  it('renders unassigned', async () => {
     render(<AssignedTo project={project} group={GROUP_1} />, {organization});
-    expect(screen.getByText('No one')).toBeInTheDocument();
+    expect(await screen.findByText('No one')).toBeInTheDocument();
   });
 
-  it('does not render chevron when disableDropdown prop is passed', () => {
+  it('does not render chevron when disableDropdown prop is passed', async () => {
     render(
       <AssignedTo disableDropdown project={project} group={GROUP_1} event={event} />,
       {
         organization,
       }
     );
+    expect(await screen.findByText('No one')).toBeInTheDocument();
     expect(screen.queryByTestId('assigned-to-chevron-icon')).not.toBeInTheDocument();
   });
 
