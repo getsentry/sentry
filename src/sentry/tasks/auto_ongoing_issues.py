ef58ccb068518c575dd30c6343a0aa2a19ea615e
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-04-25 16:20:36 -0700

    break up task into paged iterations

--- src/sentry/tasks/auto_ongoing_issues.py
+++ src/sentry/tasks/auto_ongoing_issues.py
@@ -4,23 +4,33 @@ import pytz
 
 from sentry import features
 from sentry.issues.ongoing import transition_new_to_ongoing
-from sentry.models import Group, GroupInboxReason, Project
+from sentry.models import Group, GroupInbox, GroupInboxReason, Organization, Project
 from sentry.tasks.base import instrumented_task
-from sentry.utils.query import RangeQuerySetWrapper
 
 
 @instrumented_task(name="sentry.tasks.schedule_auto_transition", time_limit=75, soft_time_limit=60)
 def schedule_auto_transition():
     now = datetime.now(tz=pytz.UTC)
-    for project in Project.objects.filter(
-        groupinbox__date_added__lte=now - timedelta(days=3),
-        groupinbox__reason__in=(GroupInboxReason.NEW.value, GroupInboxReason.REPROCESSED.value),
-    ).distinct():
-        if features.has(
-            "organizations:issue-states-auto-transition-new-ongoing", project.organization
-        ):
+    for project_id in (
+        GroupInbox.objects.filter(
+            date_added__lte=now - timedelta(days=3),
+            reason__in=(GroupInboxReason.NEW.value, GroupInboxReason.REPROCESSED.value),
+        )
+        .distinct()
+        .values_list("project_id", flat=True)
+    ):
+        try:
+            org = Organization.objects.get_from_cache(
+                id=Project.objects.get_from_cache(id=project_id).organization_id
+            )
+        except Exception:
+            org = Organization.objects.get(project_id=project_id)
+
+        if features.has("organizations:issue-states-auto-transition-new-ongoing", org):
             auto_transition_issues_new_to_ongoing.delay(
-                project_id=project.id, expires=now + timedelta(hours=1)
+                project_id=project_id,
+                date_added_lte=int(now.timestamp()),
+                expires=now + timedelta(hours=1),
             )
 
 
@@ -28,15 +38,32 @@ def schedule_auto_transition():
     name="sentry.tasks.auto_transition_issues_new_to_ongoing", time_limit=75, soft_time_limit=60
 )
 def auto_transition_issues_new_to_ongoing(
-    project_id: int, cutoff=None, chunk_size: int = 1000, **kwargs
+    project_id: int,
+    date_added_lte: int,
+    date_added_gte: int = None,
+    chunk_size: int = 1000,
+    **kwargs,
 ) -> None:
-    now = datetime.now(tz=pytz.UTC)
 
-    for group in RangeQuerySetWrapper(
-        Group.objects.filter(
-            groupinbox__project_id=project_id,
-            groupinbox__date_added__lte=now - timedelta(days=3),
-            groupinbox__reason__in=(GroupInboxReason.NEW.value, GroupInboxReason.REPROCESSED.value),
-        ).distinct()
-    ):
+    queryset = GroupInbox.objects.filter(
+        project_id=project_id,
+        date_added__lte=datetime.utcfromtimestamp(date_added_lte),
+        reason__in=(GroupInboxReason.NEW.value, GroupInboxReason.REPROCESSED.value),
+    ).order_by("date_added")
+
+    if date_added_gte:
+        queryset = queryset.filter(date_added__gte=datetime.utcfromtimestamp(date_added_gte))
+
+    new_inbox = queryset[:chunk_size]
+
+    for group in Group.object.filter(id__in=list({inbox.group_id for inbox in new_inbox})):
         transition_new_to_ongoing(group)
+
+    if len(new_inbox) == chunk_size:
+        auto_transition_issues_new_to_ongoing.delay(
+            project_id=project_id,
+            date_added_lte=datetime.utcfromtimestamp(date_added_lte),
+            date_added_gte=new_inbox[chunk_size - 1].date_added,
+            chunk_size=chunk_size,
+            expires=datetime.now(tz=pytz.UTC) + timedelta(hours=1),
+        )
