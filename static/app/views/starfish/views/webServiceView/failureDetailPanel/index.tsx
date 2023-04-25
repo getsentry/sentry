679f9d833e1dbe4c83e99ba47202dac187d08cc4
Ash Anand <ash.anand@sentry.io>
2023-04-25 16:40:35 -0400

    Cleanup

--- static/app/views/starfish/views/webServiceView/failureDetailPanel/index.tsx
+++ static/app/views/starfish/views/webServiceView/failureDetailPanel/index.tsx
@@ -1,5 +1,3 @@
-import styled from '@emotion/styled';
-
 import {t} from 'sentry/locale';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
@@ -7,7 +5,6 @@ import Detail from 'sentry/views/starfish/components/detailPanel';
 import FailureDetailTable from 'sentry/views/starfish/views/webServiceView/failureDetailPanel/failureDetailTable';
 import {FailureSpike} from 'sentry/views/starfish/views/webServiceView/types';
 
-// For demo purposes. Delete later if necessary
 export default function FailureDetailPanel({
   spike,
   onClose,
@@ -19,26 +16,16 @@ export default function FailureDetailPanel({
   const organization = useOrganization();
 
   return (
-    <Detail detailKey={spike as any} onClose={onClose}>
+    <Detail detailKey={spike?.startTimestamp.toString()} onClose={onClose}>
       <div>
-        <h2>{t('Failure Detail')}</h2>
+        <h2>{t('Error Spike Detail')}</h2>
         <p>
           {t(
             'Detailed summary of failure rate spike. Detailed summary of failure rate spike. Detailed summary of failure rate spike. Detailed summary of failure rate spike. Detailed summary of failure rate spike. Detailed summary of failure rate spike.'
           )}
         </p>
-        <SubHeader>{t('Failure Description')}</SubHeader>
-        <FailureDetailTable
-          location={location}
-          organization={organization}
-          // onSelect={() => {}}
-        />
+        <FailureDetailTable location={location} organization={organization} />
       </div>
     </Detail>
   );
 }
-
-const SubHeader = styled('h3')`
-  color: ${p => p.theme.gray300};
-  font-size: ${p => p.theme.fontSizeLarge};
-`;
