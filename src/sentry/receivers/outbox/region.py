8ffe770792e481ecdcde32b3a38f0faeb2eb7e83
Alberto Leal <mail4alberto@gmail.com>
2023-04-25 14:36:02 -0400

    add organizationmember_mapping_service.delete_by_org_id to process_organization_updates

--- src/sentry/receivers/outbox/region.py
+++ src/sentry/receivers/outbox/region.py
@@ -82,6 +82,7 @@ def process_team_updates(
 @receiver(process_region_outbox, sender=OutboxCategory.ORGANIZATION_UPDATE)
 def process_organization_updates(object_identifier: int, **kwds: Any):
     if (org := maybe_process_tombstone(Organization, object_identifier)) is None:
+        organizationmember_mapping_service.delete_by_org_id(organization_id=object_identifier)
         return
 
     update = update_organization_mapping_from_instance(org)
