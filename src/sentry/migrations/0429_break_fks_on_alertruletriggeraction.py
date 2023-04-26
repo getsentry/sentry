b7abb90b8fc620566921291d56da8214fc77d6f8
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-04-25 17:31:48 -0700

    Set is_dangerous to True

--- src/sentry/migrations/0429_break_fks_on_alertruletriggeraction.py
+++ src/sentry/migrations/0429_break_fks_on_alertruletriggeraction.py
@@ -67,7 +67,7 @@ class Migration(CheckedMigration):
     # - Adding indexes to large tables. Since this can take a long time, we'd generally prefer to
     #   have ops run this and not block the deploy. Note that while adding an index is a schema
     #   change, it's completely safe to run the operation after the code has deployed.
-    is_dangerous = False
+    is_dangerous = True
 
     dependencies = [
         ("sentry", "0428_backfill_denormalize_notification_actor"),
