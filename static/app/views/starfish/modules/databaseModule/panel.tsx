8adcb6119a25f2db45578b6c89e8a75460e86734
William Mak <william@wmak.io>
2023-04-26 14:55:50 -0400

    fix

--- static/app/views/starfish/modules/databaseModule/panel.tsx
+++ static/app/views/starfish/modules/databaseModule/panel.tsx
@@ -253,7 +253,7 @@ function QueryDetailBody({row}: EndpointDetailBodyProps) {
   );
 }
 
-const throughputQueryToChartData = (data: any, startTime, endTime): Series[] => {
+const throughputQueryToChartData = (data: any, startTime: moment.Moment, endTime: moment.Moment): Series[] => {
   const countSeries: Series = {seriesName: 'count()', data: [] as any[]};
   const p75Series: Series = {seriesName: 'p75()', data: [] as any[]};
   data.forEach(({count, p75, interval}: any) => {
