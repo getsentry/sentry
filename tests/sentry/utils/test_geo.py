f27918fc888973293327a7df5a36c9156b0404f7
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2023-04-26 19:04:39 -0400

    fix geo tests

--- tests/sentry/utils/test_geo.py
+++ tests/sentry/utils/test_geo.py
@@ -19,6 +19,7 @@ class TestGeo(TestCase):
             "country_code": "US",
             "region": "CA",
             "city": "Beverly Hills",
+            "subdivision": "California",
             "latitude": 34.09109878540039,
             "longitude": -118.41169738769531,
         }
