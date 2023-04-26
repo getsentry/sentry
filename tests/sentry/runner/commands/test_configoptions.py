5331db10941c12d442866515538b98c931d6ca3a
Neo Huang <126607112+kneeyo1@users.noreply.github.com>
2023-04-26 10:54:08 -0700

    Sentry commands for get/set/deleting options. (#46469)
    Sentry Runner Command for making options more self-service. Takes in a
    yaml file as input with updated options.
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>
    Co-authored-by: Kyle Owens <owensk@gmail.com>
    Co-authored-by: David Wang <dwang@sentry.io>
    Co-authored-by: Evan Purkhiser <evanpurkhiser@gmail.com>
    Co-authored-by: Colleen O'Rourke <colleen@sentry.io>
    Co-authored-by: Matt Gaunt-Seo @ Sentry.io <112419115+mattgauntseo-sentry@users.noreply.github.com>
    Co-authored-by: Scott Cooper <scttcper@gmail.com>
    Co-authored-by: Dameli Ushbayeva <d.ushbayeva@gmail.com>
    Co-authored-by: Snigdha Sharma <snigdha.sharma@sentry.io>
    Co-authored-by: getsentry-bot <bot@sentry.io>
    Co-authored-by: barkbarkimashark <101606877+barkbarkimashark@users.noreply.github.com>
    Co-authored-by: Riccardo Busetti <riccardo.busetti@sentry.io>
    Co-authored-by: Francesco Novy <francesco.novy@sentry.io>
    Co-authored-by: Tor <torberge@outlook.com>
    Co-authored-by: Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
    Co-authored-by: Tony Xiao <txiao@sentry.io>
    Co-authored-by: Mark Story <mark@mark-story.com>
    Co-authored-by: edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
    Co-authored-by: Josh Ferge <josh.ferge@sentry.io>
    Co-authored-by: Nar Saynorath <nar.saynorath@sentry.io>
    Co-authored-by: Gilbert Szeto <gilbert.szeto@sentry.io>
    Co-authored-by: Richard Ortenberg <richard.ortenberg@sentry.io>
    Co-authored-by: wedamija <6288560+wedamija@users.noreply.github.com>
    Co-authored-by: Elias Hussary <elias.hussary@sentry.io>
    Co-authored-by: Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
    Co-authored-by: Stephen Cefali <scefali@sentry.io>
    Co-authored-by: Vu Luong <vuluongj20@gmail.com>
    Co-authored-by: Dan Fuller <dfuller@sentry.io>
    Co-authored-by: Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
    Co-authored-by: Priscila Oliveira <priscila.oliveira@sentry.io>
    Co-authored-by: Malachi Willey <malwilley@gmail.com>
    Co-authored-by: Lyn Nagara <lyn.nagara@gmail.com>
    Co-authored-by: Colton Allen <colton.allen@sentry.io>
    Co-authored-by: Sentry Bot <getsentry-bot@users.noreply.github.com>
    Co-authored-by: Mike Ihbe <mike.ihbe@sentry.io>
    Co-authored-by: Zach Collins <recursive.cookie.jar@gmail.com>
    Co-authored-by: Cathy Teng <70817427+cathteng@users.noreply.github.com>
    Co-authored-by: Matt Johnson-Pint <matt.johnson-pint@sentry.io>
    Co-authored-by: Kamil Ogórek <kamil.ogorek@gmail.com>
    Co-authored-by: Andrii Soldatenko <andrii.soldatenko@sentry.io>
    Co-authored-by: Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
    Co-authored-by: Danny Lee <dlee@sentry.io>
    Co-authored-by: Shruthi <shruthilaya.jaganathan@sentry.io>
    Co-authored-by: Katie Byers <katie.byers@sentry.io>
    Co-authored-by: Armen Zambrano G <armenzg@sentry.io>
    Co-authored-by: Ryan Albrecht <ryan.albrecht@sentry.io>
    Co-authored-by: Billy Vong <billyvg@users.noreply.github.com>
    Co-authored-by: bmckerry <110857332+bmckerry@users.noreply.github.com>
    Co-authored-by: Jonas <jonas.badalic@sentry.io>
    Co-authored-by: corps <593850+corps@users.noreply.github.com>
    Co-authored-by: Matej Minar <matej.minar@sentry.io>
    Co-authored-by: Arpad Borsos <arpad.borsos@sentry.io>
    Co-authored-by: Michal Kuffa <beezz@users.noreply.github.com>
    Co-authored-by: William Mak <william@wmak.io>
    Co-authored-by: Jodi Jang <116035587+jangjodi@users.noreply.github.com>
    Co-authored-by: @npusarla <neha.pusarla@sentry.io>

---
+++ tests/sentry/runner/commands/test_configoptions.py
@@ -0,0 +1,99 @@
+from functools import cached_property
+
+import pytest
+from django.core.cache.backends.locmem import LocMemCache
+
+from sentry.options.manager import OptionsManager
+from sentry.options.store import OptionsStore
+from sentry.runner.commands.configoptions import configoptions
+from sentry.testutils import CliTestCase
+
+
+class ConfigOptionsTest(CliTestCase):
+    command = configoptions
+
+    @cached_property
+    def store(self):
+        c = LocMemCache("test", {})
+        c.clear()
+        return OptionsStore(cache=c)
+
+    @cached_property
+    def manager(self):
+        return OptionsManager(store=self.store)
+
+    @pytest.fixture(autouse=True)
+    def register(self):
+        self.store.flush_local_cache()
+
+    def test_get(self):
+        #  github-login.require-verified-email: False
+        rv = self.invoke("get", "github-login.require-verified-email")
+        assert rv.exit_code == 0, rv.output
+        assert "False" in rv.output
+
+    def test_bad_get(self):
+        rv = self.invoke("get", "testkey3")
+        assert rv.exit_code != 0, rv.output
+
+    def test_list(self):
+        rv = self.invoke("list")
+        assert rv.exit_code == 0, rv.output
+
+    def test_set(self):
+        rv = self.invoke("set", "github-login.base-domain", "testVal")
+        assert rv.exit_code == 0, rv.output
+        assert "Updated key" in rv.output
+        rv = self.invoke("get", "github-login.base-domain")
+        assert rv.exit_code == 0, rv.output
+        assert "testVal" in rv.output
+        self.invoke("delete", "github-login.base-domain")
+
+    def test_bad_set(self):
+        rv = self.invoke("set", "badKey", "val")
+        assert rv.exit_code != 0, rv.output
+
+    def test_delete(self):
+        rv = self.invoke("delete", "github-login.api-domain")
+        assert rv.exit_code == 0, rv.output
+        assert "Deleted key" in rv.output
+        rv = self.invoke("get", "github-login.api-domain")
+
+    def test_bad_delete(self):
+        rv = self.invoke("delete", "badKey")
+        assert rv.exit_code != 0, rv.output
+
+    def test_strict(self):
+        rv = self.invoke("strict", "tests/sentry/runner/commands/strict.yaml")
+        assert rv.exit_code == 0, rv.output
+        assert "Updated key: github-login.base-domain" in rv.output
+        assert "Updated key: github-login.extended-permissions" in rv.output
+        assert "Deleted key: system.admin-email" in rv.output
+        rv = self.invoke("get", "github-login.extended-permissions")
+        assert rv.exit_code == 0, rv.output
+        assert "['test1', 'test2']" in rv.output
+
+        self.invoke("delete", "github-login.base-domain")
+        self.invoke("delete", "github-login.extended-permissions")
+        self.invoke("delete", "symbolserver.options")
+
+    def test_bad_strict(self):
+        rv = self.invoke("strict", "tests/sentry/runner/commands/badstrict.yaml")
+        assert rv.exit_code != 0, rv.output
+
+    def test_patch(self):
+        rv = self.invoke("patch", "tests/sentry/runner/commands/patch.yaml")
+        assert rv.exit_code == 0, rv.output
+        assert "Fetched Key: github-login.api-domain" in rv.output
+        assert "Updated key: github-login.api-domain" in rv.output
+        assert "Deleted key: symbolserver.options" in rv.output
+
+        rv = self.invoke("get", "github-login.api-domain")
+        assert rv.exit_code == 0, rv.output
+        assert "testing" in rv.output
+        self.invoke("delete", "github-login.api-domain")
+        self.invoke("nodedata.cache-sample-rate")
+
+    def test_bad_patch(self):
+        rv = self.invoke("patch", "tests/sentry/runner/commands/badpatch.yaml")
+        assert rv.exit_code != 0, rv.output
