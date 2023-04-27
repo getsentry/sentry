98dffea9b2db3d9b3cbe1ff7f1612f49115af84e
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-04-27 11:12:45 -0700

    Restore custom migration script

--- src/sentry/migrations/0430_break_fks_on_alertruletriggeraction.py
+++ src/sentry/migrations/0430_break_fks_on_alertruletriggeraction.py
@@ -6,6 +6,61 @@ import sentry.db.models.fields.hybrid_cloud_foreign_key
 from sentry.new_migrations.migrations import CheckedMigration
 
 
+def alertruletriggeraction_migrations():
+    database_operations = [
+        migrations.AlterField(
+            model_name="alertruletriggeraction",
+            name="integration",
+            field=sentry.db.models.fields.foreignkey.FlexibleForeignKey(
+                "sentry.Integration",
+                db_constraint=False,
+                blank=True,
+                db_index=True,
+                null=True,
+                on_delete="CASCADE",
+            ),
+        ),
+        migrations.AlterField(
+            model_name="alertruletriggeraction",
+            name="sentry_app",
+            field=sentry.db.models.fields.foreignkey.FlexibleForeignKey(
+                to="sentry.SentryApp", db_constraint=False, blank=True, db_index=True, null=True
+            ),
+        ),
+    ]
+
+    state_operations = [
+        migrations.AlterField(
+            model_name="alertruletriggeraction",
+            name="integration",
+            field=sentry.db.models.fields.hybrid_cloud_foreign_key.HybridCloudForeignKey(
+                "sentry.Integration", blank=True, db_index=True, null=True, on_delete="CASCADE"
+            ),
+        ),
+        migrations.RenameField(
+            model_name="alertruletriggeraction",
+            old_name="integration",
+            new_name="integration_id",
+        ),
+        migrations.AlterField(
+            model_name="alertruletriggeraction",
+            name="sentry_app",
+            field=sentry.db.models.fields.hybrid_cloud_foreign_key.HybridCloudForeignKey(
+                "sentry.SentryApp", blank=True, db_index=True, null=True, on_delete="CASCADE"
+            ),
+        ),
+        migrations.RenameField(
+            model_name="alertruletriggeraction",
+            old_name="sentry_app",
+            new_name="sentry_app_id",
+        ),
+    ]
+
+    return database_operations + [
+        migrations.SeparateDatabaseAndState(state_operations=state_operations)
+    ]
+
+
 class Migration(CheckedMigration):
     # This flag is used to mark that a migration shouldn't be automatically run in production. For
     # the most part, this should only be used for operations where it's safe to run the migration
@@ -17,33 +72,10 @@ class Migration(CheckedMigration):
     # - Adding indexes to large tables. Since this can take a long time, we'd generally prefer to
     #   have ops run this and not block the deploy. Note that while adding an index is a schema
     #   change, it's completely safe to run the operation after the code has deployed.
-    is_dangerous = False
+    is_dangerous = True
 
     dependencies = [
         ("sentry", "0429_fix_broken_external_issues"),
     ]
 
-    operations = [
-        migrations.RemoveField(
-            model_name="alertruletriggeraction",
-            name="integration",
-        ),
-        migrations.RemoveField(
-            model_name="alertruletriggeraction",
-            name="sentry_app",
-        ),
-        migrations.AddField(
-            model_name="alertruletriggeraction",
-            name="integration_id",
-            field=sentry.db.models.fields.hybrid_cloud_foreign_key.HybridCloudForeignKey(
-                "sentry.Integration", blank=True, db_index=True, null=True, on_delete="CASCADE"
-            ),
-        ),
-        migrations.AddField(
-            model_name="alertruletriggeraction",
-            name="sentry_app_id",
-            field=sentry.db.models.fields.hybrid_cloud_foreign_key.HybridCloudForeignKey(
-                "sentry.SentryApp", blank=True, db_index=True, null=True, on_delete="CASCADE"
-            ),
-        ),
-    ]
+    operations = alertruletriggeraction_migrations()
