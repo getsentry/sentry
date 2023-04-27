168e209f43161ef9db72eeff33c1e816b21b8071
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-04-27 10:11:42 -0700

    ref(js): Convert acl/access to a FC (#48031)

--- static/app/components/acl/access.tsx
+++ static/app/components/acl/access.tsx
@@ -1,116 +1,58 @@
-import {Component} from 'react';
+import {Fragment} from 'react';
 
-import {Alert} from 'sentry/components/alert';
-import {t} from 'sentry/locale';
-import {Config, Organization, Scope} from 'sentry/types';
+import ConfigStore from 'sentry/stores/configStore';
+import {useLegacyStore} from 'sentry/stores/useLegacyStore';
+import {Scope} from 'sentry/types';
 import {isRenderFunc} from 'sentry/utils/isRenderFunc';
-import withConfig from 'sentry/utils/withConfig';
-import withOrganization from 'sentry/utils/withOrganization';
-
-const DEFAULT_NO_ACCESS_MESSAGE = (
-  <Alert type="error" showIcon>
-    {t('You do not have sufficient permissions to access this.')}
-  </Alert>
-);
+import useOrganization from 'sentry/utils/useOrganization';
 
 // Props that function children will get.
-export type ChildRenderProps = {
+type ChildRenderProps = {
   hasAccess: boolean;
   hasSuperuser: boolean;
 };
 
-type ChildFunction = (props: ChildRenderProps) => React.ReactNode;
-
-type DefaultProps = {
-  /**
-   * List of required access levels
-   */
-  access: Scope[];
-
-  /**
-   * Custom renderer function for "no access" message OR `true` to use
-   * default message. `false` will suppress message.
-   */
-  renderNoAccessMessage: ChildFunction | boolean;
-
-  /**
-   * Requires superuser
-   */
-  isSuperuser?: boolean;
-
-  /**
-   * Should the component require all access levels or just one or more.
-   */
-  requireAll?: boolean;
-};
-
-const defaultProps: DefaultProps = {
-  renderNoAccessMessage: false,
-  isSuperuser: false,
-  requireAll: true,
-  access: [],
-};
+type ChildFunction = (props: ChildRenderProps) => JSX.Element;
 
 type Props = {
   /**
-   * Configuration from ConfigStore
+   * List of required access levels
    */
-  config: Config;
-
-  /**
-   * Current Organization
-   */
-  organization: Organization;
-
+  access?: Scope[];
   /**
    * Children can be a node or a function as child.
    */
   children?: React.ReactNode | ChildFunction;
-} & Partial<DefaultProps>;
+  /**
+   * Requires superuser
+   */
+  isSuperuser?: boolean;
+};
 
 /**
  * Component to handle access restrictions.
  */
-class Access extends Component<Props> {
-  static defaultProps = defaultProps;
+function Access({children, isSuperuser = false, access = []}: Props) {
+  const config = useLegacyStore(ConfigStore);
+  const organization = useOrganization();
 
-  render() {
-    const {
-      organization,
-      config,
-      access,
-      requireAll,
-      isSuperuser,
-      renderNoAccessMessage,
-      children,
-    } = this.props;
+  const {access: orgAccess} = organization || {access: []};
 
-    const {access: orgAccess} = organization || {access: []};
-    const method = requireAll ? 'every' : 'some';
+  const hasAccess = !access || access.every(acc => orgAccess.includes(acc));
+  const hasSuperuser = !!(config.user && config.user.isSuperuser);
 
-    const hasAccess = !access || access[method](acc => orgAccess.includes(acc));
-    const hasSuperuser = !!(config.user && config.user.isSuperuser);
+  const renderProps: ChildRenderProps = {
+    hasAccess,
+    hasSuperuser,
+  };
 
-    const renderProps: ChildRenderProps = {
-      hasAccess,
-      hasSuperuser,
-    };
+  const render = hasAccess && (!isSuperuser || hasSuperuser);
 
-    const render = hasAccess && (!isSuperuser || hasSuperuser);
-
-    if (!render && typeof renderNoAccessMessage === 'function') {
-      return renderNoAccessMessage(renderProps);
-    }
-    if (!render && renderNoAccessMessage) {
-      return DEFAULT_NO_ACCESS_MESSAGE;
-    }
-
-    if (isRenderFunc<ChildFunction>(children)) {
-      return children(renderProps);
-    }
-
-    return render ? children : null;
+  if (isRenderFunc<ChildFunction>(children)) {
+    return children(renderProps);
   }
+
+  return <Fragment>{render ? children : null}</Fragment>;
 }
 
-export default withOrganization(withConfig(Access));
+export default Access;
