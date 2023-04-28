502410d05640516c701476b0f193c1151fefd96b
davidenwang <dwang@sentry.io>
2023-04-28 12:13:45 -0700

    fix space

--- static/app/views/monitors/components/detailsSidebar.tsx
+++ static/app/views/monitors/components/detailsSidebar.tsx
@@ -8,7 +8,7 @@ import Text from 'sentry/components/text';
 import TimeSince from 'sentry/components/timeSince';
 import {IconCopy} from 'sentry/icons';
 import {t, tn} from 'sentry/locale';
-import space from 'sentry/styles/space';
+import {space} from 'sentry/styles/space';
 import {defined} from 'sentry/utils';
 import {getFormattedDate} from 'sentry/utils/dates';
 import {DEFAULT_MAX_RUNTIME} from 'sentry/views/monitors/components/monitorForm';
