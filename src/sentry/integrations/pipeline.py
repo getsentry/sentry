d196a8150e181f105f198428996210e706cb08e1
Stephen Cefali <scefali@sentry.io>
2023-04-28 09:06:07 -0700

    fix test

--- src/sentry/integrations/pipeline.py
+++ src/sentry/integrations/pipeline.py
@@ -179,7 +179,7 @@ class IntegrationPipeline(Pipeline):
 
     def _dialog_response(self, data, success):
         document_origin = "document.origin"
-        if features.has("organizations:customer-domains", self.organization) or True:
+        if features.has("organizations:customer-domains", self.organization):
             document_origin = f'"{generate_organization_url(self.organization.slug)}"'
         context = {
             "payload": {"success": success, "data": data},
