fe19491dc948e06dd6f5ddd29c714d61c47d96ae
Danny Lee <dlee@sentry.io>
2023-04-28 12:05:39 -0700

    feat(roles): Access checks for Team and Project (#47757)
    Depends on #47698 and #47448
    
    With team-level roles, an "org-member" can be a "team-admin". This will
    grant them additional access scopes for specific teams and projects
    (i.e. I can have `project:admin` for a few projects, but not all the
    projects in my org)

--- static/app/components/acl/access.spec.jsx
+++ static/app/components/acl/access.spec.jsx
@@ -40,7 +40,79 @@ describe('Access', function () {
       });
     });
 
-    it('handles no org/project', function () {
+    it('read access from team', function () {
+      const org = TestStubs.Organization({access: []});
+      const nextRouterContext = TestStubs.routerContext([{organization: org}]);
+
+      const team1 = TestStubs.Team({access: []});
+      render(
+        <Access access={['team:admin']} team={team1}>
+          {childrenMock}
+        </Access>,
+        {context: nextRouterContext, organization: org}
+      );
+
+      expect(childrenMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          hasAccess: false,
+          hasSuperuser: false,
+        })
+      );
+
+      const team2 = TestStubs.Team({
+        access: ['team:read', 'team:write', 'team:admin'],
+      });
+      render(
+        <Access access={['team:admin']} team={team2}>
+          {childrenMock}
+        </Access>,
+        {context: nextRouterContext, organization: org}
+      );
+
+      expect(childrenMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          hasAccess: true,
+          hasSuperuser: false,
+        })
+      );
+    });
+
+    it('read access from project', function () {
+      const org = TestStubs.Organization({access: []});
+      const nextRouterContext = TestStubs.routerContext([{organization: org}]);
+
+      const proj1 = TestStubs.Project({access: []});
+      render(
+        <Access access={['project:read']} project={proj1}>
+          {childrenMock}
+        </Access>,
+        {context: nextRouterContext, organization: org}
+      );
+
+      expect(childrenMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          hasAccess: false,
+          hasSuperuser: false,
+        })
+      );
+
+      const proj2 = TestStubs.Project({access: ['project:read']});
+      render(
+        <Access access={['project:read']} project={proj2}>
+          {childrenMock}
+        </Access>,
+        {context: nextRouterContext, organization: org}
+      );
+
+      expect(childrenMock).toHaveBeenCalledWith(
+        expect.objectContaining({
+          hasAccess: true,
+          hasSuperuser: false,
+        })
+      );
+    });
+
+    it('handles no org', function () {
       render(<Access access={['org:write']}>{childrenMock}</Access>, {
         context: routerContext,
         organization,
@@ -111,6 +183,17 @@ describe('Access', function () {
       expect(screen.getByText('The Child')).toBeInTheDocument();
     });
 
+    it('has no access', function () {
+      render(
+        <Access access={['org:write']}>
+          <p>The Child</p>
+        </Access>,
+        {context: routerContext, organization}
+      );
+
+      expect(screen.queryByText('The Child')).not.toBeInTheDocument();
+    });
+
     it('has superuser', function () {
       ConfigStore.config = {
         user: {isSuperuser: true},
@@ -123,17 +206,6 @@ describe('Access', function () {
       );
 
       expect(screen.getByText('The Child')).toBeInTheDocument();
-    });
-
-    it('has no access', function () {
-      render(
-        <Access access={['org:write']}>
-          <p>The Child</p>
-        </Access>,
-        {context: routerContext, organization}
-      );
-
-      expect(screen.queryByText('The Child')).not.toBeInTheDocument();
     });
 
     it('has no superuser', function () {
