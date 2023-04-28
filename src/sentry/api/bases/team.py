9c1c9a53eb35c6518e09237585c99b005341105f
Seiji Chew <67301797+schew2381@users.noreply.github.com>
2023-04-28 15:23:23 -0700

    fix(api): Fix comment typo (#48217)

--- src/sentry/api/bases/team.py
+++ src/sentry/api/bases/team.py
@@ -25,7 +25,7 @@ class TeamPermission(OrganizationPermission):
         has_org_scope = super().has_object_permission(request, view, team.organization)
         if has_org_scope:
             # Org-admin has "team:admin", but they can only act on their teams
-            # Org-owners and Org-owners has no restrictions due to team memberships
+            # Org-owners and Org-managers have no restrictions due to team memberships
             return request.access.has_team_access(team)
 
         return has_team_permission(request, team, self.scope_map)
