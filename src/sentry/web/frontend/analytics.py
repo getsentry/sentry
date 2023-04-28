96f31b50127fa4ed8fd44f3e781c5217d0d4df90
Francesco Novy <francesco.novy@sentry.io>
2023-04-28 09:55:00 +0200

    ref(loader): Add analytics when loader is rendered

---
+++ src/sentry/web/frontend/analytics.py
@@ -0,0 +1,18 @@
+from sentry import analytics
+
+
+class JsSdkLoaderRendered(analytics.Event):
+    type = "js_sdk_loader.rendered"
+
+    attributes = (
+        analytics.Attribute("org_id"),
+        analytics.Attribute("is_lazy"),
+        analytics.Attribute("has_performance"),
+        analytics.Attribute("has_replay"),
+        analytics.Attribute("has_debug"),
+        analytics.Attribute("sdk_version"),
+        analytics.Attribute("tmpl_instance"),
+    )
+
+
+analytics.register(JsSdkLoaderRendered)
