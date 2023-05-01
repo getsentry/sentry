a8c39b03851a7e47a43de88dc0e96bbff194d2bb
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-05-01 16:16:27 -0700

    feat(issue-states): add task to transition 14+ day old regressed issues to ONGOING (#48042)
    Nearly identical logic to https://github.com/getsentry/sentry/pull/47881
    just with some minor adjustments. Did some minor refactoring in this PR
    as well to account for the similarities.
    
    Depends on https://github.com/getsentry/getsentry/pull/10403
    
    ---------
    
    Co-authored-by: Snigdha Sharma <snigdha.sharma@sentry.io>

--- tests/sentry/issues/test_ongoing.py
+++ tests/sentry/issues/test_ongoing.py
@@ -1,7 +1,7 @@
 from typing import Any
 from unittest.mock import patch
 
-from sentry.issues.ongoing import transition_new_to_ongoing
+from sentry.issues.ongoing import transition_new_to_ongoing, transition_regressed_to_ongoing
 from sentry.models import (
     Activity,
     GroupHistory,
@@ -36,3 +36,26 @@ class TransitionNewToOngoingTest(TestCase):  # type: ignore
         assert GroupHistory.objects.filter(
             group=group, status=GroupHistoryStatus.UNRESOLVED
         ).exists()
+
+    @patch("sentry.signals.inbox_in.send_robust")
+    def test_regressed_to_ongoing(self, inbox_in: Any) -> None:
+        group = self.create_group(status=GroupStatus.UNRESOLVED, substatus=GroupSubStatus.REGRESSED)
+        add_group_to_inbox(group, GroupInboxReason.REGRESSION)
+
+        assert GroupInbox.objects.filter(
+            group=group, reason=GroupInboxReason.REGRESSION.value
+        ).exists()
+        assert inbox_in.called
+
+        transition_regressed_to_ongoing(group)
+        assert GroupInbox.objects.filter(
+            group=group, reason=GroupInboxReason.ONGOING.value
+        ).exists()
+        assert inbox_in.called
+        assert Activity.objects.filter(
+            group=group, type=ActivityType.AUTO_SET_ONGOING.value
+        ).exists()
+        assert GroupHistory.objects.filter(
+            group=group, status=GroupHistoryStatus.UNRESOLVED
+        ).exists()
+        assert inbox_in.call_count == 2
