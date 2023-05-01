015f607c2a3956dea86d2d47d879971925597fca
Zach Collins <recursive.cookie.jar@gmail.com>
2023-05-01 10:51:14 -0700

    Breaking more keys (#46960)
    Breaks more foreign keys.
    
    ---------
    
    Co-authored-by: Mark Story <mark@mark-story.com>

--- src/sentry/incidents/logic.py
+++ src/sentry/incidents/logic.py
@@ -1342,9 +1342,9 @@ def get_available_action_integrations_for_org(organization):
 
 def get_pagerduty_services(organization_id, integration_id):
     return PagerDutyService.objects.filter(
-        organization_integration__organization_id=organization_id,
-        organization_integration__integration_id=integration_id,
-    ).values("id", "service_name")
+        organization_id=organization_id,
+        integration_id=integration_id,
+    )
 
 
 # TODO: This is temporarily needed to support back and forth translations for snuba / frontend.
