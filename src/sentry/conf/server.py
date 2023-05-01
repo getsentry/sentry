6cf1f0670b6c767d0d3e7077d0881f71efc1ed9d
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-05-01 11:56:00 -0700

    chore(crons): Remove bandaid for backlogged checkins [INC-350] (#48226)
    Requires a few getsentry PRs first
    
    - https://github.com/getsentry/getsentry/pull/10392
    - https://github.com/getsentry/getsentry/pull/10391
      (Migration must be run by ops **merging is not enough to be ✅**)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -3237,9 +3237,6 @@ SENTRY_FEATURE_ADOPTION_CACHE_OPTIONS = {
     "options": {"cluster": "default"},
 }
 
-# Killswitch to ignore checkins for explicit monitors
-SENTRY_MONITORS_IGNORED_MONITORS = []
-
 # Raise schema validation errors and make the indexer crash (only useful in
 # tests)
 SENTRY_METRICS_INDEXER_RAISE_VALIDATION_ERRORS = False
