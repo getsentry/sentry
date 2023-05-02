7b0a782254938174974803df61c360b3ee8ef8e8
Katie Byers <lobsterkatie@gmail.com>
2023-05-02 07:26:09 -0700

    add `bind_org_context_from_integration` helper

--- src/sentry/integrations/utils/scope.py
+++ src/sentry/integrations/utils/scope.py
@@ -7,6 +7,8 @@ from sentry_sdk import configure_scope
 
 from sentry.models.organization import Organization
 from sentry.services.hybrid_cloud.integration import integration_service
+from sentry.shared_integrations.exceptions import IntegrationError
+from sentry.utils.sdk import bind_organization_context
 
 logger = logging.getLogger(__name__)
 
@@ -46,3 +48,24 @@ def get_orgs_from_integration(integration_id: int) -> list[Organization]:
     )
 
     return cast("list[Organization]", orgs)
+
+
+def bind_org_context_from_integration(integration_id: int) -> None:
+    """
+    Given the id of an Integration, get the associated org(s) and bind that data to the scope.
+
+    Note: An `Integration` is an instance of given provider's integration, tied to a single entity
+    on the provider's end (for example, an instance of the GitHub integration tied to a particular
+    GitHub org, or an instance of the Slack integration tied to a particular Slack workspace), which
+    can be shared by multiple orgs.
+    """
+
+    orgs = get_orgs_from_integration(integration_id)
+
+    if len(orgs) == 0:
+        raise IntegrationError(f"No orgs are associated with integration id={integration_id}")
+    elif len(orgs) == 1:
+        bind_organization_context(orgs[0])
+    else:
+        # skip this case for now
+        pass
--- tests/sentry/integrations/utils/test_scope.py
+++ tests/sentry/integrations/utils/test_scope.py
@@ -1,5 +1,13 @@
-from sentry.integrations.utils.scope import get_orgs_from_integration
+from unittest.mock import MagicMock, patch
+
+import pytest
+
+from sentry.integrations.utils.scope import (
+    bind_org_context_from_integration,
+    get_orgs_from_integration,
+)
 from sentry.models.integrations.integration import Integration
+from sentry.shared_integrations.exceptions import IntegrationError
 from sentry.testutils import TestCase
 
 
@@ -30,3 +38,23 @@ class GetOrgsFromIntegrationTest(TestCase):
         found_orgs = get_orgs_from_integration(integration.id)
 
         assert found_orgs == []
+
+
+class BindOrgContextFromIntegrationTest(TestCase):
+    @patch("sentry.integrations.utils.scope.bind_organization_context")
+    def test_binds_org_context_with_single_org(self, mock_bind_org_context: MagicMock):
+        org = self.create_organization(slug="dogsaregreat")
+        integration = Integration.objects.create(name="squirrelChasers")
+        integration.add_organization(org)
+
+        bind_org_context_from_integration(integration.id)
+
+        mock_bind_org_context.assert_called_with(org)
+
+    def test_raises_error_if_no_orgs_found(self):
+        integration = Integration.objects.create(name="squirrelChasers")
+
+        with pytest.raises(
+            IntegrationError, match=f"No orgs are associated with integration id={integration.id}"
+        ):
+            bind_org_context_from_integration(integration.id)
