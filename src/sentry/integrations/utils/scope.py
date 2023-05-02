aed74e8c58b1d26c868d5c3e9810880e48269909
Katie Byers <lobsterkatie@gmail.com>
2023-05-02 07:26:09 -0700

    add `get_orgs_from_integration` helper

--- src/sentry/integrations/utils/scope.py
+++ src/sentry/integrations/utils/scope.py
@@ -1,6 +1,12 @@
+from __future__ import annotations
+
 import logging
+from typing import cast
 
 from sentry_sdk import configure_scope
+
+from sentry.models.organization import Organization
+from sentry.services.hybrid_cloud.integration import integration_service
 
 logger = logging.getLogger(__name__)
 
@@ -20,3 +26,23 @@ def clear_tags_and_context() -> None:
 
         if reset_values:
             logger.info("We've reset the context and tags.")
+
+
+def get_orgs_from_integration(integration_id: int) -> list[Organization]:
+    """
+    Given the id of an `Integration`, return a list of associated `Organization` objects.
+
+    Note: An `Integration` is an instance of given provider's integration, tied to a single entity
+    on the provider's end (for example, an instance of the GitHub integration tied to a particular
+    GitHub org, or an instance of the Slack integration tied to a particular Slack workspace), which
+    can be shared by multiple orgs.
+    """
+
+    _, org_integrations = integration_service.get_organization_contexts(
+        integration_id=integration_id
+    )
+    orgs = Organization.objects.get_many_from_cache(
+        [org_integration.organization_id for org_integration in org_integrations]
+    )
+
+    return cast("list[Organization]", orgs)
