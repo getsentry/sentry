fb777fb86de0d85d974282bb826a2fc037586460
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-05-03 13:08:49 -0700

    ref(replays): Cleanup the old session-replay-slim-table feature flag (FE) (#48409)
    Followed by: https://github.com/getsentry/sentry/pull/48410
    
    Relates to https://github.com/getsentry/sentry/issues/47756

--- static/app/views/replays/types.tsx
+++ static/app/views/replays/types.tsx
@@ -107,16 +107,18 @@ export type ReplayListLocationQuery = {
 export type ReplayListRecord = Pick<
   ReplayRecord,
   | 'activity'
+  | 'browser'
   | 'count_errors'
   | 'duration'
   | 'finished_at'
   | 'id'
   | 'is_archived'
+  | 'os'
   | 'project_id'
   | 'started_at'
+  | 'urls'
   | 'user'
-> &
-  Partial<Pick<ReplayRecord, 'browser' | 'count_urls' | 'os' | 'urls'>>;
+>;
 
 // Sync with ReplayListRecord above
 export const REPLAY_LIST_FIELDS: ReplayRecordNestedFieldName[] = [
@@ -124,7 +126,6 @@ export const REPLAY_LIST_FIELDS: ReplayRecordNestedFieldName[] = [
   'browser.name',
   'browser.version',
   'count_errors',
-  'count_urls',
   'duration',
   'finished_at',
   'id',
