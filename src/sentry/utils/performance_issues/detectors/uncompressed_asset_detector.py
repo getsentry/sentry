26b16d6f10a6549c87deb7c0934536319b9dfacd
Kev <6111995+k-fish@users.noreply.github.com>
2023-05-04 09:36:18 -0400

    fix(perf): Fix uncompressed asset file types (#48514)
    ### Summary
    Switched to an allowlist since we're apparently getting png/jpg (and
    probably others) under the resource.css op
    
    Fixes PERF-2046

--- src/sentry/utils/performance_issues/detectors/uncompressed_asset_detector.py
+++ src/sentry/utils/performance_issues/detectors/uncompressed_asset_detector.py
@@ -8,7 +8,7 @@ from ..base import DetectorType, PerformanceDetector, fingerprint_resource_span,
 from ..performance_problem import PerformanceProblem
 from ..types import Span
 
-FILE_EXTENSION_DENYLIST = ("woff", "woff2")
+FILE_EXTENSION_ALLOWLIST = ("css", "json", "js")
 
 
 class UncompressedAssetSpanDetector(PerformanceDetector):
@@ -68,7 +68,7 @@ class UncompressedAssetSpanDetector(PerformanceDetector):
             return
 
         # Ignore assets with certain file extensions
-        if description.endswith(FILE_EXTENSION_DENYLIST):
+        if not description.endswith(FILE_EXTENSION_ALLOWLIST):
             return
 
         # Ignore assets under a certain duration threshold
