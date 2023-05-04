f6f626094c0cf363e824e62a4729e1130150dccf
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2023-05-04 10:05:37 -0400

    feat(starfish): Adds some columns to spans explorer table (#48471)
    Adds additional columns to spans explorer page table

--- static/app/views/starfish/views/spans/spansView.tsx
+++ static/app/views/starfish/views/spans/spansView.tsx
@@ -1,4 +1,8 @@
+import styled from '@emotion/styled';
 import {Location} from 'history';
+
+import DatePageFilter from 'sentry/components/datePageFilter';
+import {space} from 'sentry/styles/space';
 
 import SpansTable from './spansTable';
 
@@ -7,5 +11,19 @@ type Props = {
 };
 
 export default function SpansView(props: Props) {
-  return <SpansTable location={props.location} />;
+  return (
+    <div>
+      <FilterOptionsContainer>
+        <DatePageFilter alignDropdown="left" />
+      </FilterOptionsContainer>
+      <SpansTable location={props.location} />;
+    </div>
+  );
 }
+
+const FilterOptionsContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  gap: ${space(1)};
+  margin-bottom: ${space(2)};
+`;
