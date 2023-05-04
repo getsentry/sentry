d625fd9f139000b42d921a91e5177099951f4cde
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-05-04 09:18:28 +0200

    Add tests

--- tests/sentry/dynamic_sampling/rules/helpers/test_sliding_window.py
+++ tests/sentry/dynamic_sampling/rules/helpers/test_sliding_window.py
@@ -1,3 +1,4 @@
+import pytest
 from freezegun import freeze_time
 
 from sentry.dynamic_sampling.rules.helpers.sliding_window import extrapolate_monthly_volume
@@ -5,17 +6,26 @@ from sentry.dynamic_sampling.rules.helpers.sliding_window import extrapolate_mon
 
 @freeze_time("2023-02-03 12:00:00")
 def test_extrapolate_monthly_volume_with_28_days():
-    result = extrapolate_monthly_volume(10)
+    result = extrapolate_monthly_volume(volume=10, hours=24)
     assert result == 280
 
 
+@pytest.mark.parametrize(
+    "volume, hours, expected_result",
+    [
+        (10, 48, 150),
+        (10, 24, 300),
+        (10, 12, 600),
+        (10, 1, 7200),
+    ],
+)
 @freeze_time("2023-04-03 12:00:00")
-def test_extrapolate_monthly_volume_with_30_days():
-    result = extrapolate_monthly_volume(10)
-    assert result == 300
+def test_extrapolate_monthly_volume_with_30_days(volume, hours, expected_result):
+    result = extrapolate_monthly_volume(volume=volume, hours=hours)
+    assert result == expected_result
 
 
 @freeze_time("2023-05-03 12:00:00")
 def test_extrapolate_monthly_volume_with_31_days():
-    result = extrapolate_monthly_volume(10)
+    result = extrapolate_monthly_volume(volume=10, hours=24)
     assert result == 310
