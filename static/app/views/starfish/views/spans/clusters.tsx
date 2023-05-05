22dc03885724a904ce10c85d2d729d1790b1e67f
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-05-05 09:58:26 -0400

    feat(starfish): Span hierarchy explorer (#48556)
    ~

---
+++ static/app/views/starfish/views/spans/clusters.tsx
@@ -0,0 +1,74 @@
+export const CLUSTERS = {
+  top: {
+    name: 'top',
+    label: 'All',
+    condition: () => '',
+    grouping_column: "module IN ['db', 'http'] ? concat('top.',  module) : 'top.other'",
+  },
+  'top.db': {
+    name: 'top.db',
+    label: 'DB',
+    condition: () => "module == 'db'",
+    grouping_column:
+      "action IN ['SELECT', 'INSERT'] ? concat('db.',  lower(action)) : 'db.other'",
+  },
+  'db.select': {
+    name: 'db.select',
+    label: 'SELECT',
+    condition: () => "action == 'SELECT'",
+  },
+  'db.insert': {
+    name: 'db.insert',
+    label: 'INSERT',
+    condition: () => "action == 'INSERT'",
+  },
+  'db.other': {
+    name: 'db.other',
+    label: 'Other',
+    condition: () => "action NOT IN ['SELECT', 'INSERT']",
+    grouping_column: 'action',
+    grouping_condition: value => () => `action == '${value}'`,
+  },
+  'top.http': {
+    name: 'top.http',
+    label: 'HTTP',
+    condition: () => "module == 'http'",
+    grouping_column: 'span_operation',
+  },
+  'top.other': {
+    name: 'top.other',
+    label: 'Other',
+    condition: () => "module NOT IN ['http', 'db']",
+    grouping_column: "splitByChar('.', span_operation)[1]",
+    grouping_condition: value => () => `splitByChar('.', span_operation)[1] = '${value}'`,
+  },
+  'http.client': {
+    name: 'http.client',
+    label: 'Client',
+    condition: () => "span_operation == 'http.client'",
+    grouping_column:
+      "action IN ['GET', 'POST'] ? concat('http.client.', lower(action)) : 'http.client.other'",
+  },
+  'http.client.get': {
+    name: 'http.client.get',
+    label: 'GET',
+    condition: () => "action == 'GET'",
+    grouping_column: 'domain',
+    grouping_condition: value => () => `domain = '${value}'`,
+  },
+  'http.client.post': {
+    name: 'http.client.post',
+    label: 'POST',
+    condition: () => "action == 'POST'",
+  },
+  'http.client.other': {
+    name: 'http.client.other',
+    label: 'Other',
+    condition: () => "action NOT IN ['GET', 'POST']",
+  },
+  'http.server': {
+    name: 'http.server',
+    label: 'Server',
+    condition: () => "span_operation == 'http.server'",
+  },
+};
