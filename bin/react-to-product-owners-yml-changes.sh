34ef87a699dc431c76a9a706f23414e58c011728
Chad Whitacre <chadwhitacre@sentry.io>
2023-05-04 21:33:40 -0400

    Tidy up PR creation

--- bin/react-to-product-owners-yml-changes.sh
+++ bin/react-to-product-owners-yml-changes.sh
@@ -7,9 +7,10 @@ sha=$(git -C /tmp/security-as-code rev-parse @)
 pip3 install pyyaml==6.0
 ./bin/react-to-product-owners-yml-changes.py /tmp/security-as-code/rbac/lib/product-owners.yml
 branch="getsantry/update-product-areas-${sha:0:8}"
+message = "meta(meta) Sync with product-owners.yml in security-as-code@${sha:0:8}"
 git checkout -b "$branch"
 git add .
-git commit -n -m "meta(meta) Sync with product-owners.yml in security-as-code@${sha:0:8}"
+git commit -n -m $message
 git push --set-upstream origin "$branch"
-gh pr create --fill
+gh pr create --title "$message" --body="https://www.notion.so/473791bae5bf43399d46093050b77bf0"
 gh pr merge --squash --auto
