4dd25fc7c10b9973aef1efe3fb96fac9876ffd5d
Chad Whitacre <chadwhitacre@sentry.io>
2023-05-04 21:22:48 -0400

    Implement updating of labels and issue templates

--- bin/react-to-product-owners-yml-changes.sh
+++ bin/react-to-product-owners-yml-changes.sh
@@ -6,10 +6,10 @@ gh repo clone https://github.com/getsentry/security-as-code /tmp/security-as-cod
 sha=$(git -C /tmp/security-as-code rev-parse @)
 pip3 install pyyaml==6.0
 ./bin/react-to-product-owners-yml-changes.py /tmp/security-as-code/rbac/lib/product-owners.yml
-branch="getsantry/update-product-area-labels-${sha:0:8}"
+branch="getsantry/update-product-areas-${sha:0:8}"
 git checkout -b "$branch"
 git add .
-git commit -m "Sync with product-owners.yml in security-as-code@${sha:0:8}"
+git commit -n -m "meta(meta) Sync with product-owners.yml in security-as-code@${sha:0:8}"
 git push --set-upstream origin "$branch"
 gh pr create --fill
 gh pr merge --squash --auto
