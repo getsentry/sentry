08ec8bfa116bb8a4d1d8c2755ed20840dfbc8509
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-05-05 09:40:55 -0700

    added booleans

--- src/sentry/mail/forms/member_team.py
+++ src/sentry/mail/forms/member_team.py
@@ -65,6 +65,8 @@ class MemberTeamForm(forms.Form):
 
         if targetType == self.memberValue and not user_service.get_many(
             filter={
+                "is_active": True,
+                "is_active_memberteam": True,
                 "organiztion_id": self.project.organization.id,
                 "project_ids": [self.project.id],
                 "user_ids": [int(targetIdentifier)],
