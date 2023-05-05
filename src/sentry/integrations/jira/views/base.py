6fbc19fad3e6679c700e513128dcee6527cb96f5
Alexander Tarasov <alex.tarasov@sentry.io>
2023-05-05 10:51:08 +0200

    Initial code parts for CSP implementation for sentry and self-hosted (#48507)
    Another attempt of https://github.com/getsentry/sentry/pull/47980 (which
    was reverted due to bugs).
    
    This PR adds some preliminary code for adding a
    `Content-Security-Policy-Report-Only` header with minimal required
    permissions (at least I could not find any violations on `sentry
    devserver` and self-hosted).
    - The CSP middleware is disabled (commented in the `MIDDLEWARE`)
    - There is no report collecting enabled by default (`CSP_REPORT_URI` is
    not set), the intent is to customize it depending on the use case.

--- src/sentry/integrations/jira/views/base.py
+++ src/sentry/integrations/jira/views/base.py
@@ -1,3 +1,5 @@
+from csp.middleware import CSPMiddleware
+from django.conf import settings
 from django.views.generic import View
 
 from sentry import options
@@ -15,12 +17,33 @@ class JiraSentryUIBaseView(View):
         add the requisite CSP headers before returning it.
         """
 
-        context["ac_js_src"] = "https://connect-cdn.atl-paas.net/all.js"
-        response = render_to_response(self.html_file, context, self.request)
         sources = [
             self.request.GET.get("xdm_e"),
             options.get("system.url-prefix"),
         ]
-        sources_string = " ".join(s for s in sources if s)  # Filter out None
-        response["Content-Security-Policy"] = f"frame-ancestors 'self' {sources_string}"
+
+        settings.CSP_FRAME_ANCESTORS = [
+            "'self'",
+        ] + [s for s in sources if s and ";" not in s]
+        settings.CSP_STYLE_SRC = [
+            # same as default (server.py)
+            "'self'",
+            "'unsafe-inline'",
+        ]
+
+        if settings.STATIC_FRONTEND_APP_URL.startswith("https://"):
+            origin = "/".join(settings.STATIC_FRONTEND_APP_URL.split("/")[0:3])
+            settings.CSP_STYLE_SRC.append(origin)
+
+        header = "Content-Security-Policy"
+        if getattr(settings, "CSP_REPORT_ONLY", False):
+            header += "-Report-Only"
+
+        middleware = CSPMiddleware()
+        middleware.process_request(self.request)  # adds nonce
+
+        context["ac_js_src"] = "https://connect-cdn.atl-paas.net/all.js"
+        response = render_to_response(self.html_file, context, self.request)
+
+        response[header] = middleware.build_policy(request=self.request, response=response)
         return response
