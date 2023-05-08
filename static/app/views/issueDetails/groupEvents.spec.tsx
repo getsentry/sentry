bcd8fb240a912f1d069b3bed90267f0e1d2443a0
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2023-05-08 11:53:26 -0400

    fix

--- static/app/views/issueDetails/groupEvents.spec.tsx
+++ static/app/views/issueDetails/groupEvents.spec.tsx
@@ -135,8 +135,7 @@ describe('groupEvents', () => {
     expect(wrapper.container).toSnapshot();
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('handles search', async () => {
+  it('handles search', async () => {
     render(
       <GroupEvents
         {...baseProps}
@@ -232,8 +231,7 @@ describe('groupEvents', () => {
     );
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('does not make attachments request, async when feature not enabled', async () => {
+  it('does not make attachments request, async when feature not enabled', async () => {
     const org = initializeOrg();
 
     render(
@@ -251,8 +249,7 @@ describe('groupEvents', () => {
     expect(requests.attachments).not.toHaveBeenCalled();
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('does not display attachments column with no attachments', async () => {
+  it('does not display attachments column with no attachments', async () => {
     render(
       <GroupEvents
         {...baseProps}
@@ -268,8 +265,7 @@ describe('groupEvents', () => {
     expect(requests.attachments).toHaveBeenCalled();
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('does not display minidump column with no minidumps', async () => {
+  it('does not display minidump column with no minidumps', async () => {
     render(
       <GroupEvents
         {...baseProps}
@@ -284,8 +280,7 @@ describe('groupEvents', () => {
     expect(minidumpColumn).not.toBeInTheDocument();
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('displays minidumps', async () => {
+  it('displays minidumps', async () => {
     requests.attachments = MockApiClient.addMockResponse({
       url: '/api/0/issues/1/attachments/?per_page=50&types=event.minidump&event_id=id123',
       body: [
@@ -318,8 +313,7 @@ describe('groupEvents', () => {
     expect(minidumpColumn).toBeInTheDocument();
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('does not display attachments but displays minidump', async () => {
+  it('does not display attachments but displays minidump', async () => {
     requests.attachments = MockApiClient.addMockResponse({
       url: '/api/0/issues/1/attachments/?per_page=50&types=event.minidump&event_id=id123',
       body: [
@@ -444,8 +438,7 @@ describe('groupEvents', () => {
     expect(screen.getByTestId('loading-error')).toHaveTextContent('Internal Error');
   });
 
-  // eslint-disable-next-line jest/no-disabled-tests
-  it.skip('requests for backend columns if backend project', async () => {
+  it('requests for backend columns if backend project', async () => {
     const group = TestStubs.Group();
     group.project.platform = 'node-express';
     render(
