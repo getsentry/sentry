96a4d2fef704d17952b453d2754d19e9419d4935
John Yang <john.yang@sentry.io>
2023-05-08 19:03:15 -0700

    update test_indexer_cache.py

--- tests/sentry/sentry_metrics/test_indexer_cache.py
+++ tests/sentry/sentry_metrics/test_indexer_cache.py
@@ -1,8 +1,8 @@
 import pytest
 from django.conf import settings
 
-from sentry.sentry_metrics.configuration import UseCaseKey
 from sentry.sentry_metrics.indexer.cache import StringIndexerCache
+from sentry.sentry_metrics.use_case_id_registry import UseCaseID
 from sentry.utils.cache import cache
 from sentry.utils.hashlib import md5_text
 
@@ -17,39 +17,49 @@ indexer_cache = StringIndexerCache(
 
 @pytest.fixture
 def use_case_id() -> str:
-    return UseCaseKey.RELEASE_HEALTH.value
+    return UseCaseID.SESSIONS.value
 
 
 def test_cache(use_case_id: str) -> None:
     cache.clear()
-    assert indexer_cache.get("blah", use_case_id) is None
-    indexer_cache.set("blah", 1, use_case_id)
-    assert indexer_cache.get("blah", use_case_id) == 1
+    assert indexer_cache.get(f"{use_case_id}:1:blah:123") is None
+    indexer_cache.set(f"{use_case_id}:1:blah:123", 1)
+    assert indexer_cache.get(f"{use_case_id}:1:blah:123") == 1
 
-    indexer_cache.delete("blah", use_case_id)
-    assert indexer_cache.get("blah", use_case_id) is None
+    indexer_cache.delete(f"{use_case_id}:1:blah:123")
+    assert indexer_cache.get(f"{use_case_id}:1:blah:123") is None
 
 
 def test_cache_many(use_case_id: str) -> None:
     cache.clear()
-    values = {"hello": 2, "bye": 3}
-    assert indexer_cache.get_many(list(values.keys()), use_case_id) == {"hello": None, "bye": None}
-    indexer_cache.set_many(values, use_case_id)
-    assert indexer_cache.get_many(list(values.keys()), use_case_id) == values
+    values = {f"{use_case_id}:100:hello": 2, f"{use_case_id}:100:bye": 3}
+    assert indexer_cache.get_many(values.keys()) == {
+        f"{use_case_id}:100:hello": None,
+        f"{use_case_id}:100:bye": None,
+    }
+    indexer_cache.set_many(values)
+    assert indexer_cache.get_many(list(values.keys())) == values
 
-    indexer_cache.delete_many(list(values.keys()), use_case_id)
-    assert indexer_cache.get_many(list(values.keys()), use_case_id) == {"hello": None, "bye": None}
+    indexer_cache.delete_many(list(values.keys()))
+    assert indexer_cache.get_many(values.keys()) == {
+        f"{use_case_id}:100:hello": None,
+        f"{use_case_id}:100:bye": None,
+    }
 
 
 def test_make_cache_key(use_case_id: str) -> None:
-    key = indexer_cache.make_cache_key("blah", "release-health")
-    assert key == f"indexer:test:org:str:{use_case_id}:{md5_text('blah').hexdigest()}"
+    orgId = 1
+    string = ":blah:blah"
+    nameSpace = md5_text(f"{orgId}:{string}").hexdigest()
+    key = indexer_cache.make_cache_key(f"{use_case_id}:{orgId}:{string}")
+
+    assert key == f"indexer:test:org:str:{use_case_id}:{nameSpace}"
 
 
 def test_formatted_results(use_case_id: str) -> None:
-    values = {"hello": 2, "bye": 3}
-    results = {indexer_cache.make_cache_key(k, use_case_id): v for k, v in values.items()}
-    assert indexer_cache._format_results(list(values.keys()), results, use_case_id) == values
+    values = {f"{use_case_id}::hello": 2, f"{use_case_id}::bye": 3}
+    results = {indexer_cache.make_cache_key(k): v for k, v in values.items()}
+    assert indexer_cache._format_results(list(values.keys()), results) == values
 
 
 def test_ttl_jitter() -> None:
@@ -66,8 +76,8 @@ def test_ttl_jitter() -> None:
 
 
 def test_separate_namespacing() -> None:
-    indexer_cache.set("a", 1, UseCaseKey.RELEASE_HEALTH.value)
-    assert indexer_cache.get("a", UseCaseKey.RELEASE_HEALTH.value) == 1
-    indexer_cache.set("a", 2, UseCaseKey.PERFORMANCE.value)
-    assert indexer_cache.get("a", UseCaseKey.RELEASE_HEALTH.value) == 1
-    assert indexer_cache.get("a", UseCaseKey.PERFORMANCE.value) == 2
+    indexer_cache.set("sessions:3:what", 1)
+    assert indexer_cache.get("sessions:3:what") == 1
+    indexer_cache.set("transactions:3:what", 2)
+    assert indexer_cache.get("sessions:3:what") == 1
+    assert indexer_cache.get("transactions:3:what") == 2
