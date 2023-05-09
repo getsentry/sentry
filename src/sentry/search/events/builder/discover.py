cf628d0709b06abade8916ff050fd350004e256d
Shruthilaya <shruthilaya.jaganathan@sentry.io>
2023-05-09 13:35:27 -0400

    type ignore

--- src/sentry/search/events/builder/discover.py
+++ src/sentry/search/events/builder/discover.py
@@ -1471,7 +1471,7 @@ class QueryBuilder(BaseQueryBuilder):
         return value
 
     def run_query(self, referrer: str, use_cache: bool = False) -> Any:
-        if referrer is None:
+        if referrer is None:  # type: ignore
             InvalidSearchQuery("Query missing referrer.")
         return raw_snql_query(self.get_snql_query(), referrer, use_cache)
 
