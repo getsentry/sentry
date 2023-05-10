50d1012f97ba2ed84f85780d4b05b3c15509804e
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2023-05-10 11:04:57 -0700

    feat(escalating-issues): Add informational popup key (#48796)
    Backend for #48795 
    
    PR for https://github.com/getsentry/sentry/issues/48305

--- src/sentry/assistant/guides.py
+++ src/sentry/assistant/guides.py
@@ -25,6 +25,7 @@ GUIDES = {
     "releases_widget": 26,
     "activate_sampling_rule": 27,
     "create_conditional_rule": 28,
+    "explain_archive_button_issue_details": 29,
 }
 
 # demo mode has different guides
