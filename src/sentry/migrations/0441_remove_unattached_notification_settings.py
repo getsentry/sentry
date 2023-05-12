a46d462b4453425172ba17403fd7e08a21c134df
Valery Brobbey <valery.brobbey@sentry.io>
2023-05-12 10:15:15 -0700

    add delete gate

--- src/sentry/migrations/0441_remove_unattached_notification_settings.py
+++ src/sentry/migrations/0441_remove_unattached_notification_settings.py
@@ -4,10 +4,14 @@ from django.db import migrations
 
 from sentry.new_migrations.migrations import CheckedMigration
 
+NUMBER_RECORDS_APPROX = 11310
+
 
 def remove_unattached_notification_settings(apps, schema_editor):
     NotificationSetting = apps.get_model("sentry", "NotificationSetting")
-    NotificationSetting.objects.filter(user_id__isnull=True, team_id__isnull=True).delete()
+    query_set = NotificationSetting.objects.filter(user_id__isnull=True, team_id__isnull=True)
+    if len(query_set) <= NUMBER_RECORDS_APPROX:
+        query_set.delete()
 
 
 class Migration(CheckedMigration):
