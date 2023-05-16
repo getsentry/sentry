503b8ff6457aa95ee1974be53908356b3b76bc48
John Yang <john.yang@sentry.io>
2023-05-16 11:49:15 -0700

    update test

--- tests/sentry/migrations/test_0445_backfill_indexer_use_case_id.py
+++ tests/sentry/migrations/test_0445_backfill_indexer_use_case_id.py
@@ -3,13 +3,17 @@ from sentry.testutils.cases import TestMigrations
 
 
 class PerfIndexerUseCaseIdBackfillTest(TestMigrations):
-    migrate_from = "0428_backfill_denormalize_notification_actor"
-    migrate_to = "0429_backfill_indexer_use_case_id"
+    migrate_from = "0444_remove_next_checkin_last_checkin_from_monitor"
+    migrate_to = "0445_backfill_indexer_use_case_id"
 
     def setup_before_migration(self, apps):
         PerfStringIndexer = apps.get_model("sentry", "PerfStringIndexer")
-        PerfStringIndexer.objects.create(string="hello", organization_id=12)
-        PerfStringIndexer.objects.create(string="bye", organization_id=12)
+        PerfStringIndexer.objects.create(
+            string="hello", organization_id=12, use_case_id="performance"
+        )
+        PerfStringIndexer.objects.create(
+            string="bye", organization_id=12, use_case_id="performance"
+        )
 
     def test(self):
         for i in PerfStringIndexer.objects.all():
