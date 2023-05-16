21d415f7aa7261d9ddb8977efbd325576705fc32
Katie Byers <katie.byers@sentry.io>
2023-05-15 18:12:02 -0700

    ref(js): Restore `handleXhrErrorResponse` as wrapper for `getXhrErrorResponseHandler` (#49154)
    This recreates the `handleXhrErrorResponse` helper renamed to `getXhrErrorResponseHandler` in https://github.com/getsentry/sentry/pull/49100 as a two-argument wrapper for `getXhrErrorResponseHandler`. In that wrapper it calls `getXhrErrorResponseHandler` to get a handler and then applies that handler, so that it can finally live up to its name and actually do the handling of error responses.
    
    This also switches all uses of `getXhrErrorResponseHandler` to instead use the new `handleXhrErrorResponse`.

--- static/app/views/onboarding/onboarding.tsx
+++ static/app/views/onboarding/onboarding.tsx
@@ -20,7 +20,7 @@ import {space} from 'sentry/styles/space';
 import {OnboardingSelectedSDK} from 'sentry/types';
 import {defined} from 'sentry/utils';
 import {trackAnalytics} from 'sentry/utils/analytics';
-import getXhrErrorResponseHandler from 'sentry/utils/handleXhrErrorResponse';
+import {handleXhrErrorResponse} from 'sentry/utils/handleXhrErrorResponse';
 import Redirect from 'sentry/utils/redirect';
 import testableTransition from 'sentry/utils/testableTransition';
 import useApi from 'sentry/utils/useApi';
@@ -249,7 +249,7 @@ function Onboarding(props: Props) {
         project_id: recentCreatedProject.id,
       });
     } catch (error) {
-      getXhrErrorResponseHandler(t('Unable to delete project in onboarding'))(error);
+      handleXhrErrorResponse(t('Unable to delete project in onboarding'), error);
       // we don't give the user any feedback regarding this error as this shall be silent
     }
   }, [api, organization, recentCreatedProject, onboardingContext]);
