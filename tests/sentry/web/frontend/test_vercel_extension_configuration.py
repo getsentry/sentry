aa56c6063a1df726687a8d0e4a4cc4cad2a76dd4
Alberto Leal <mail4alberto@gmail.com>
2023-05-15 22:30:21 -0400

    tests/sentry/web/frontend/test_vercel_extension_configuration.py

--- tests/sentry/web/frontend/test_vercel_extension_configuration.py
+++ tests/sentry/web/frontend/test_vercel_extension_configuration.py
@@ -2,6 +2,7 @@ from urllib.parse import parse_qs, urlparse
 
 import responses
 
+from sentry.db.postgres.roles import in_test_psql_role_override
 from sentry.identity.vercel import VercelIdentityProvider
 from sentry.integrations.vercel import VercelClient
 from sentry.models import OrganizationMember
@@ -18,7 +19,8 @@ class VercelExtensionConfigurationTest(TestCase):
         self.user = self.create_user()
         self.org = self.create_organization()
 
-        OrganizationMember.objects.create(user=self.user, organization=self.org, role="admin")
+        with in_test_psql_role_override("postgres"):
+            OrganizationMember.objects.create(user=self.user, organization=self.org, role="admin")
 
         responses.reset()
         # need oauth mocks
@@ -68,9 +70,10 @@ class VercelExtensionConfigurationTest(TestCase):
         assert resp.url.endswith("?next=https%3A%2F%2Fexample.com")
 
     def test_logged_in_as_member(self):
-        OrganizationMember.objects.filter(user=self.user, organization=self.org).update(
-            role="member"
-        )
+        with in_test_psql_role_override("postgres"):
+            OrganizationMember.objects.filter(user=self.user, organization=self.org).update(
+                role="member"
+            )
         self.login_as(self.user)
 
         resp = self.client.get(self.path, self.params)
@@ -89,7 +92,8 @@ class VercelExtensionConfigurationTest(TestCase):
         self.login_as(self.user)
 
         org = self.create_organization()
-        OrganizationMember.objects.create(user=self.user, organization=org)
+        with in_test_psql_role_override("postgres"):
+            OrganizationMember.objects.create(user=self.user, organization=org)
 
         resp = self.client.get(self.path, self.params)
 
@@ -108,7 +112,8 @@ class VercelExtensionConfigurationTest(TestCase):
         self.login_as(self.user)
 
         org = self.create_organization()
-        OrganizationMember.objects.create(user=self.user, organization=org)
+        with in_test_psql_role_override("postgres"):
+            OrganizationMember.objects.create(user=self.user, organization=org)
         self.params["orgSlug"] = org.slug
 
         resp = self.client.get(self.path, self.params)
