30d207d334f5b9ec88390ee432ef245d94e5369e
Elias Hussary <elias.hussary@sentry.io>
2023-05-16 14:37:07 -0400

    fix(replays): use ALL_ACCESS_PROJECTS by default in fetchreplaylist (#49202)
    ## Summary 
    Fixes an issue where we would display a replayCount but the list would
    not load due to a missing query param.
    
    Slack thread:
    https://sentry.slack.com/archives/C03USURCFBJ/p1684220401490959

--- static/app/utils/replays/fetchReplayList.tsx
+++ static/app/utils/replays/fetchReplayList.tsx
@@ -2,6 +2,7 @@ import * as Sentry from '@sentry/react';
 import type {Location} from 'history';
 
 import type {Client} from 'sentry/api';
+import {ALL_ACCESS_PROJECTS} from 'sentry/constants/pageFilters';
 import type {Organization} from 'sentry/types';
 import type EventView from 'sentry/utils/discover/eventView';
 import {mapResponseToReplayRecord} from 'sentry/utils/replays/replayDataUtils';
@@ -51,6 +52,7 @@ async function fetchReplayList({
         ...payload,
         cursor: location.query.cursor,
         queryReferrer,
+        project: ALL_ACCESS_PROJECTS,
       },
     });
 
