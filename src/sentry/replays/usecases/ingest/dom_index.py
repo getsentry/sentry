003c51116a9a0c3825085d8d4e59580471848aeb
Colton Allen <colton.allen@sentry.io>
2023-05-16 11:51:08 -0500

    Rename key to msg

--- src/sentry/replays/usecases/ingest/dom_index.py
+++ src/sentry/replays/usecases/ingest/dom_index.py
@@ -139,6 +139,7 @@ def get_user_actions(
             if category == "ui.slowClickDetected":
                 payload["project_id"] = project_id
                 payload["replay_id"] = replay_id
+                payload["msg"] = payload.pop("message")
                 logger.info("Slow click detected", extra=payload)
             elif category == "ui.click":
                 node = payload.get("data", {}).get("node")
