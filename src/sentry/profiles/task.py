60d6046e2ec5adece998028c4052acb8f3aa05cc
Pierre Massat <pierre.massat@sentry.io>
2023-05-17 18:41:02 -0400

    Add a comment for get_frame_index_map

--- src/sentry/profiles/task.py
+++ src/sentry/profiles/task.py
@@ -576,6 +576,9 @@ The sorting order is callee to caller (child to parent)
 def get_frame_index_map(frames: List[dict[str, Any]]) -> dict[int, List[int]]:
     index_map: dict[int, List[int]] = {}
     for i, frame in enumerate(frames):
+        # In case we don't have an `original_index` field, we default to using
+        # the index of the frame in order to still produce a data structure
+        # with the right shape.
         index_map.setdefault(frame.get("original_index", i), []).append(i)
     return index_map
 
