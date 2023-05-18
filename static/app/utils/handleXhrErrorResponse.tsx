e11885abef280dbf9bc959e8ece1e92a29f64e82
Katie Byers <katie.byers@sentry.io>
2023-05-18 07:30:01 -0700

    ref(js): Streamline `handleXhrErrorResponse` (#49359)
    This simplifies and cleans up `handleXhrErrorResponse`, prior to other work being done on it. Key changes:
    
    - Consolidate return conditions
    - Log the full `responseJSON` object rather than just parts of it
    - Remove conditions on capturing an event. We weren't actually using the values we were checking for other than as context data, and the differences there don't matter if we log `responseJSON` in its entirety.

--- static/app/utils/handleXhrErrorResponse.tsx
+++ static/app/utils/handleXhrErrorResponse.tsx
@@ -3,39 +3,20 @@ import * as Sentry from '@sentry/react';
 import RequestError from 'sentry/utils/requestError/requestError';
 
 export function handleXhrErrorResponse(message: string, err: RequestError): void {
-  if (!err) {
-    return;
-  }
-  if (!err.responseJSON) {
+  // Sudo errors are handled separately elsewhere
+  // @ts-ignore Property 'code' does not exist on type 'string'
+  if (!err || !err.responseJSON || err.responseJSON?.detail?.code === 'sudo-required') {
     return;
   }
 
-  const {responseJSON} = err;
+  const {responseJSON, status} = err;
 
-  // If this is a string then just capture it as error
-  if (typeof responseJSON.detail === 'string') {
-    Sentry.withScope(scope => {
-      scope.setExtra('status', err.status);
-      scope.setExtra('detail', responseJSON.detail);
-      Sentry.captureException(new Error(message));
+  Sentry.withScope(scope => {
+    scope.setExtras({
+      status,
+      responseJSON,
     });
-    return;
-  }
 
-  // Ignore sudo-required errors
-  if (responseJSON.detail && responseJSON.detail.code === 'sudo-required') {
-    return;
-  }
-
-  if (responseJSON.detail && typeof responseJSON.detail.message === 'string') {
-    Sentry.withScope(scope => {
-      scope.setExtra('status', err.status);
-      scope.setExtra('detail', responseJSON.detail);
-      // @ts-ignore Property 'code' does not exist on type 'string' (god knows why
-      // it's not mad at the other places in this function we do this, but ¯\_(ツ)_/¯)
-      scope.setExtra('code', responseJSON.detail?.code);
-      Sentry.captureException(new Error(message));
-    });
-    return;
-  }
+    Sentry.captureException(new Error(message));
+  });
 }
