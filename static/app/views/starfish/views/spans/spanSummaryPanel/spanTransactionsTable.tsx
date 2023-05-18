94f1511c02a7e41ab2a4cd5b3cc5330156d098cd
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-05-18 13:21:09 -0400

    Remove bad typing

--- static/app/views/starfish/views/spans/spanSummaryPanel/spanTransactionsTable.tsx
+++ static/app/views/starfish/views/spans/spanSummaryPanel/spanTransactionsTable.tsx
@@ -64,7 +64,7 @@ export function SpanTransactionsTable({span}: Props) {
   return (
     <GridEditable
       isLoading={isLoading}
-      data={spanTransactionsWithMetrics as unknown as Row[]}
+      data={spanTransactionsWithMetrics}
       columnOrder={COLUMN_ORDER}
       columnSortBy={[]}
       grid={{
