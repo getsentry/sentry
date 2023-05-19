10bdd3e8d07b380a9a6703ee1569010b58422e91
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-05-19 10:20:55 -0400

    ref: instrument GzippedDict with anti-pickle (#46896)
    this turns on checking of writes to gzipped dict columns as we move to
    convert them to json
    
    all violations which triggered CI failures have been fixed in sentry and
    getsentry

--- src/sentry/db/models/fields/gzippeddict.py
+++ src/sentry/db/models/fields/gzippeddict.py
@@ -1,14 +1,42 @@
+from __future__ import annotations
+
 import logging
 import pickle
+import random
+from typing import Any
 
+import sentry_sdk
+from django.conf import settings
 from django.db.models import TextField
 
+from sentry.db.models.fields import jsonfield
 from sentry.db.models.utils import Creator
+from sentry.utils import json
 from sentry.utils.strings import compress, decompress
 
 __all__ = ("GzippedDictField",)
 
 logger = logging.getLogger("sentry")
+
+PICKLE_WRITE_JSON = False
+VALIDATE_JSON_SAMPLE_RATE = 0.001
+
+
+def _validate_roundtrip(o: object) -> None:
+    try:
+        s = json.dumps(o, default=jsonfield.default)
+    except Exception:
+        raise TypeError(
+            "Tried to serialize a pickle field with a value that cannot be serialized as JSON"
+        )
+    else:
+        rt = json.loads(s)
+        if o != rt:
+            raise TypeError(
+                f"json serialized database value was not the same after deserializing:\n"
+                f"- {type(o)=}\n"
+                f"- {type(rt)=}"
+            )
 
 
 class GzippedDictField(TextField):
@@ -16,6 +44,11 @@ class GzippedDictField(TextField):
     Slightly different from a JSONField in the sense that the default
     value is a dictionary.
     """
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        self.write_json = kwargs.pop("write_json", PICKLE_WRITE_JSON)
+        self.disable_pickle_validation = kwargs.pop("disable_pickle_validation", False)
+        super().__init__(*args, **kwargs)
 
     def contribute_to_class(self, cls, name):
         """
@@ -26,15 +59,20 @@ class GzippedDictField(TextField):
         setattr(cls, name, Creator(self))
 
     def to_python(self, value):
-        if isinstance(value, str) and value:
-            try:
-                value = pickle.loads(decompress(value))
-            except Exception as e:
-                logger.exception(e)
+        try:
+            if not value:
                 return {}
-        elif not value:
-            return {}
-        return value
+            return json.loads(value)
+        except (ValueError, TypeError):
+            if isinstance(value, str) and value:
+                try:
+                    value = pickle.loads(decompress(value))
+                except Exception as e:
+                    logger.exception(e)
+                    return {}
+            elif not value:
+                return {}
+            return value
 
     def from_db_value(self, value, expression, connection):
         return self.to_python(value)
@@ -43,10 +81,26 @@ class GzippedDictField(TextField):
         if not value and self.null:
             # save ourselves some storage
             return None
-        # enforce strings to guarantee consistency
-        if isinstance(value, bytes):
-            value = str(value)
-        # db values need to be in unicode
+        elif isinstance(value, bytes):
+            value = value.decode("utf-8")
+        if self.write_json:
+            if value is None and self.null:
+                return None
+            return json.dumps(value, default=jsonfield.default)
+
+        if not self.disable_pickle_validation and (
+            settings.PICKLED_OBJECT_FIELD_COMPLAIN_ABOUT_BAD_USE_OF_PICKLE
+            or random.random() < VALIDATE_JSON_SAMPLE_RATE
+        ):
+            try:
+                _validate_roundtrip(value)
+            except Exception as e:
+                if settings.PICKLED_OBJECT_FIELD_COMPLAIN_ABOUT_BAD_USE_OF_PICKLE:
+                    raise
+                else:
+                    sentry_sdk.capture_exception(e)
+
+        # pickle path
         return compress(pickle.dumps(value))
 
     def value_to_string(self, obj):
