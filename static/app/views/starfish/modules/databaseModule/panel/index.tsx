517bbb50611aa09fc73204ac683c5a133ed3b9ff
Ash Anand <ash.anand@sentry.io>
2023-05-19 11:33:38 -0400

    use options obj in database panel

--- static/app/views/starfish/modules/databaseModule/panel/index.tsx
+++ static/app/views/starfish/modules/databaseModule/panel/index.tsx
@@ -180,41 +180,41 @@ function QueryDetailBody({
     endTime
   );
 
-  const spmTransactionSeries = queryToSeries(
-    sparklineData,
-    'transaction',
-    'spm',
+  const spmTransactionSeries = queryToSeries({
+    data: sparklineData,
+    groupByProperty: 'transaction',
+    seriesValueProperty: 'spm',
     startTime,
     endTime,
-    SPARKLINE_INTERVAL
-  );
+    interval: SPARKLINE_INTERVAL,
+  });
 
-  const spanp50TransactionSeries = queryToSeries(
-    sparklineData,
-    'transaction',
-    'p50',
+  const spanp50TransactionSeries = queryToSeries({
+    data: sparklineData,
+    groupByProperty: 'transaction',
+    seriesValueProperty: 'p50',
     startTime,
     endTime,
-    SPARKLINE_INTERVAL
-  );
+    interval: SPARKLINE_INTERVAL,
+  });
 
-  const tpmTransactionSeries = queryToSeries(
-    transactionGraphData,
-    'group',
-    'epm()',
+  const tpmTransactionSeries = queryToSeries({
+    data: transactionGraphData,
+    groupByProperty: 'group',
+    seriesValueProperty: 'epm()',
     startTime,
     endTime,
-    SPARKLINE_INTERVAL
-  );
+    interval: SPARKLINE_INTERVAL,
+  });
 
-  const p50TransactionSeries = queryToSeries(
-    transactionGraphData,
-    'group',
-    'p50(transaction.duration)',
+  const p50TransactionSeries = queryToSeries({
+    data: transactionGraphData,
+    groupByProperty: 'group',
+    seriesValueProperty: 'p50(transaction.duration)',
     startTime,
     endTime,
-    SPARKLINE_INTERVAL
-  );
+    interval: SPARKLINE_INTERVAL,
+  });
 
   const markLine =
     spmTransactionSeries?.[0]?.data && (isNew || isOld)
