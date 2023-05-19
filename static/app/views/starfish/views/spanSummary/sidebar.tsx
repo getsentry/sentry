5008e225a5cfcf51a8eaf214643087c1f3ea1c2f
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-05-19 10:39:11 -0400

    fix(starfish): fix clicking on facet removes transaction (#49439)

--- static/app/views/starfish/views/spanSummary/sidebar.tsx
+++ static/app/views/starfish/views/spanSummary/sidebar.tsx
@@ -9,6 +9,7 @@ import TagDistributionMeter from 'sentry/components/tagDistributionMeter';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {formatPercentage} from 'sentry/utils/formatters';
+import {useLocation} from 'sentry/utils/useLocation';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import Chart from 'sentry/views/starfish/components/chart';
 import {queryToSeries} from 'sentry/views/starfish/modules/databaseModule/utils';
@@ -39,6 +40,7 @@ export default function Sidebar({
 }: Props) {
   const theme = useTheme();
   const pageFilter = usePageFilters();
+  const location = useLocation();
 
   const {isLoading: isFacetBreakdownLoading, data: facetBreakdownData} =
     useQueryGetFacetsBreakdown({groupId, transactionName});
@@ -157,13 +159,13 @@ export default function Sidebar({
                   const valueCount = values.filter(v => v === uniqueValue).length;
                   totalValues += valueCount;
 
+                  const newQuery = {...location.query, [facet]: uniqueValue};
+
                   return {
                     key: facet,
                     name: uniqueValue,
                     value: uniqueValue,
-                    url: `/starfish/span/${groupId}?${qs.stringify({
-                      [facet]: uniqueValue,
-                    })}`,
+                    url: `/starfish/span/${groupId}?${qs.stringify(newQuery)}`,
                     count,
                   };
                 }),
