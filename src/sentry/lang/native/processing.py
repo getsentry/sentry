4b9a528b0dc7cdcfc3e0d19a661ea5d0953c7dcd
Arpad Borsos <arpad.borsos@sentry.io>
2023-05-24 17:22:52 +0200

    Revert "Correctly symbolicate mixed Stack Traces (eg JS/wasm) (#49676)"
    This reverts commit 307b5616bf7f5a712c6695b01532d7bd7346043e.

--- src/sentry/lang/native/processing.py
+++ src/sentry/lang/native/processing.py
@@ -1,6 +1,6 @@
 import logging
 import posixpath
-from typing import Any, Callable, Optional, Set
+from typing import Any, Callable, Set
 
 from symbolic import ParseDebugIdError, normalize_debug_id
 
@@ -446,7 +446,7 @@ def process_native_stacktraces(symbolicator: Symbolicator, data: Any) -> Any:
     return data
 
 
-def get_native_symbolication_function(data) -> Optional[Callable[[Symbolicator, Any], Any]]:
+def get_native_symbolication_function(data) -> Callable[[Symbolicator, Any], Any]:
     if is_minidump_event(data):
         return process_minidump
     elif is_applecrashreport_event(data):
