8e308bbb054ee8d834b81b60c4c442e334d308fd
Tony Xiao <txiao@sentry.io>
2023-05-24 10:41:10 -0400

    chore(settings): Define defaults for sentry settings (#49637)
    In preparation for getsentry/getsentry#10632, this ensures that all
    settings have a default value.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -107,6 +107,13 @@ SENTRY_DISALLOWED_IPS = ()
 # search domains.
 SENTRY_ENSURE_FQDN = False
 
+SENTRY_DYNAMIC_SAMPLING_RULES_REDIS_CLUSTER = "default"
+SENTRY_INCIDENT_RULES_REDIS_CLUSTER = "default"
+SENTRY_RATE_LIMIT_REDIS_CLUSTER = "default"
+SENTRY_RULE_TASK_REDIS_CLUSTER = "default"
+SENTRY_TRANSACTION_NAMES_REDIS_CLUSTER = "default"
+SENTRY_WEBHOOK_LOG_REDIS_CLUSTER = "default"
+
 # Hosts that are allowed to use system token authentication.
 # http://en.wikipedia.org/wiki/Reserved_IP_addresses
 INTERNAL_SYSTEM_IPS = (
@@ -1678,6 +1685,8 @@ SENTRY_SMTP_DISABLED_BACKENDS = frozenset(
     )
 )
 
+SENTRY_UPLOAD_RETRY_TIME = 60  # 1 min
+
 # Should users without superuser permissions be allowed to
 # make projects public
 SENTRY_ALLOW_PUBLIC_PROJECTS = True
@@ -1813,6 +1822,7 @@ SENTRY_METRICS_OPTIONS = {}
 SENTRY_METRICS_SAMPLE_RATE = 1.0
 SENTRY_METRICS_PREFIX = "sentry."
 SENTRY_METRICS_SKIP_INTERNAL_PREFIXES = []  # Order this by most frequent prefixes.
+SENTRY_METRICS_SKIP_ALL_INTERNAL = False
 SENTRY_METRICS_DISALLOW_BAD_TAGS = IS_DEV
 
 # Metrics product
