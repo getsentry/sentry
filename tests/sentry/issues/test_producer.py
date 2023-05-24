4295389c435f90e6ec13b50576f88b611aa38318
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2023-05-24 19:35:45 +0200

    ref: Add SingletonProducer (#49537)
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- tests/sentry/issues/test_producer.py
+++ tests/sentry/issues/test_producer.py
@@ -1,40 +1,11 @@
 import uuid
-from collections import deque
-from unittest.mock import Mock, patch
-
-from django.test.utils import override_settings
 
 from sentry.issues.issue_occurrence import IssueOccurrence
-from sentry.issues.producer import produce_occurrence_to_kafka, track_occurrence_producer_futures
+from sentry.issues.producer import produce_occurrence_to_kafka
 from sentry.testutils import TestCase
 from sentry.testutils.helpers.datetime import before_now
 from sentry.utils.samples import load_data
 from tests.sentry.issues.test_utils import OccurrenceTestMixin
-
-
-def test_track_occurrence_producer_futures() -> None:
-    with patch("sentry.issues.producer.occurrence_producer_futures", new=deque()):
-        future_mock = Mock()
-        future_mock.result = Mock()
-        track_occurrence_producer_futures(future_mock)
-        future_mock.result.assert_called_once_with()
-
-
-@override_settings(SENTRY_ISSUE_PLATFORM_FUTURES_MAX_LIMIT=2)  # type: ignore
-def test_track_occurrence_producer_futures_with_multiple() -> None:
-    with patch("sentry.issues.producer.occurrence_producer_futures", new=deque()):
-        first_future_mock = Mock()
-        first_future_mock.result = Mock()
-
-        second_future_mock = Mock()
-        second_future_mock.result = Mock()
-
-        track_occurrence_producer_futures(first_future_mock)
-        first_future_mock.result.assert_not_called()
-
-        track_occurrence_producer_futures(second_future_mock)
-        first_future_mock.result.assert_called_once_with()
-        second_future_mock.assert_not_called()
 
 
 class TestProduceOccurrenceToKafka(TestCase, OccurrenceTestMixin):  # type: ignore
