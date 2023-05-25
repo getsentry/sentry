31dc45bf780c5f65c2062da7b010ac8506253aac
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-05-25 14:16:36 -0400

    feat(starfish): Add selectors to span view (#49771)
    Allow changing the span operation, domain, and action via dropdowns! Add
    some intelligence so the labels make sense in the context of a provided
    module (if one is given). e.g., the 'Domain' dropdown becomes 'Table' or
    'Host' depending on the `moduleName=` query parameter.
    
    The selector components are all very similar so the code is copypasta,
    but the differences are annoying to extract. I'll work on that later.

--- static/app/views/starfish/views/spans/index.tsx
+++ static/app/views/starfish/views/spans/index.tsx
@@ -1,21 +1,19 @@
 import {useState} from 'react';
 import {RouteComponentProps} from 'react-router';
-import styled from '@emotion/styled';
 import {Location} from 'history';
 
 import * as Layout from 'sentry/components/layouts/thirds';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import {t} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
 import {
   PageErrorAlert,
   PageErrorProvider,
 } from 'sentry/utils/performance/contexts/pageError';
-import {Filter} from 'sentry/views/starfish/views/spans/filter';
+import {ModuleName} from 'sentry/views/starfish/types';
 import SpanDetail from 'sentry/views/starfish/views/spans/spanDetails';
 import {SpanDataRow} from 'sentry/views/starfish/views/spans/spansTable';
 
-import SpansView, {SPAN_FILTER_KEY_LABELS} from './spansView';
+import SpansView from './spansView';
 
 type State = {
   selectedRow?: SpanDataRow;
@@ -31,31 +29,12 @@ export default function Spans(props: Props) {
   const {selectedRow} = state;
   const setSelectedRow = (row: SpanDataRow) => setState({selectedRow: row});
 
-  const appliedFilters = Object.keys(props.location.query)
-    .map(queryKey => {
-      const queryKeyLabel = SPAN_FILTER_KEY_LABELS[queryKey];
-      const queryValue = props.location.query[queryKey];
-
-      return queryKeyLabel && queryValue
-        ? {kkey: queryKeyLabel, value: queryValue}
-        : null;
-    })
-    .filter((item): item is NonNullable<typeof item> => Boolean(item));
-
   return (
     <Layout.Page>
       <PageErrorProvider>
         <Layout.Header>
           <Layout.HeaderContent>
             <Layout.Title>{t('Spans')}</Layout.Title>
-            {appliedFilters.length > 0 ? (
-              <FiltersContainer>
-                Applied Filters:
-                {appliedFilters.map(filterProps => {
-                  return <Filter key={filterProps.kkey} {...filterProps} />;
-                })}
-              </FiltersContainer>
-            ) : null}
           </Layout.HeaderContent>
         </Layout.Header>
 
@@ -63,7 +42,12 @@ export default function Spans(props: Props) {
           <Layout.Main fullWidth>
             <PageErrorAlert />
             <PageFiltersContainer>
-              <SpansView location={props.location} onSelect={setSelectedRow} />
+              <SpansView
+                location={props.location}
+                onSelect={setSelectedRow}
+                moduleName={props.location.query.moduleName ?? ModuleName.ALL}
+                appliedFilters={props.location.query}
+              />
               <SpanDetail row={selectedRow} onClose={unsetSelectedSpanGroup} />
             </PageFiltersContainer>
           </Layout.Main>
@@ -72,9 +56,3 @@ export default function Spans(props: Props) {
     </Layout.Page>
   );
 }
-
-const FiltersContainer = styled('span')`
-  display: flex;
-  gap: ${space(1)};
-  padding: ${space(1)};
-`;
