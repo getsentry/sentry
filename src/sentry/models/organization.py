0f7cb90b23f0ef02a48dc70b0aef6142b945c4ec
Gabe Villalobos <gabriel.villalobos@sentry.io>
2023-05-25 15:22:22 -0700

    Fixes transaction wrapping for org update outbox generation

--- src/sentry/models/organization.py
+++ src/sentry/models/organization.py
@@ -240,6 +240,10 @@ class Organization(Model, SnowflakeIdMixin):
 
     snowflake_redis_key = "organization_snowflake_key"
 
+    def save_with_update_outbox(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        Organization.outbox_for_update(self.id).save()
+
     def save(self, *args, **kwargs):
         slugify_target = None
         if not self.slug:
@@ -254,15 +258,14 @@ class Organization(Model, SnowflakeIdMixin):
 
         # Run the save + outbox queueing in a transaction to ensure the control-silo is notified
         # when a change is made to the organization model.
-        with transaction.atomic():
-            if SENTRY_USE_SNOWFLAKE:
-                self.save_with_snowflake_id(
-                    self.snowflake_redis_key,
-                    lambda: super(Organization, self).save(*args, **kwargs),
-                )
-            else:
-                super().save(*args, **kwargs)
-            Organization.outbox_for_update(self.id).save()
+        if SENTRY_USE_SNOWFLAKE:
+            self.save_with_snowflake_id(
+                self.snowflake_redis_key,
+                lambda: self.save_with_update_outbox(*args, **kwargs),
+            )
+        else:
+            with transaction.atomic():
+                self.save_with_update_outbox(*args, **kwargs)
 
     @classmethod
     def reserve_snowflake_id(cls):
