0e40afbe18a62d5537b5b2fc4780bf9d7f375815
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-05-29 14:39:54 -0400

    add duration chart

--- static/app/views/starfish/views/spans/spanSummaryPage/sampleList.tsx
+++ static/app/views/starfish/views/spans/spanSummaryPage/sampleList.tsx
@@ -1,15 +1,24 @@
+import {t} from 'sentry/locale';
 import Detail from 'sentry/views/starfish/components/detailPanel';
+import DurationChart from 'sentry/views/starfish/views/spans/spanSummaryPage/durationChart';
 
 type Props = {
   groupId: string;
   onClose: () => void;
+  spanDescription: string;
   transactionName: string;
 };
 
-function SampleList({onClose, groupId, transactionName}: Props) {
+function SampleList({onClose, groupId, transactionName, spanDescription}: Props) {
   return (
     <Detail detailKey={groupId} onClose={onClose}>
       <h3>{transactionName}</h3>
+      <h5>{t('Duration (p50)')}</h5>
+      <DurationChart
+        groupId={groupId}
+        transactionName={transactionName}
+        spanDescription={spanDescription}
+      />
     </Detail>
   );
 }
