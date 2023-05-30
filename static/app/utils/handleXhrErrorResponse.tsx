c468fca65395791d4366095083f405a9835f918d
Katie Byers <lobsterkatie@gmail.com>
2023-05-30 14:14:18 -0700

    stop `handleXhrErrorResponse` from bailing when handling empty response data

--- static/app/utils/handleXhrErrorResponse.tsx
+++ static/app/utils/handleXhrErrorResponse.tsx
@@ -5,7 +5,7 @@ import RequestError from 'sentry/utils/requestError/requestError';
 export function handleXhrErrorResponse(message: string, err: RequestError): void {
   // Sudo errors are handled separately elsewhere
   // @ts-ignore Property 'code' does not exist on type 'string'
-  if (!err || !err.responseJSON || err.responseJSON?.detail?.code === 'sudo-required') {
+  if (!err || err.responseJSON?.detail?.code === 'sudo-required') {
     return;
   }
 
