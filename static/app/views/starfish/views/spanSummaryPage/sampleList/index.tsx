0fdf50e37b01352069e83af8bfe1b5ef31e34756
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-05-30 12:42:56 -0400

    update folder structure

--- static/app/views/starfish/views/spanSummaryPage/sampleList/index.tsx
+++ static/app/views/starfish/views/spanSummaryPage/sampleList/index.tsx
@@ -3,9 +3,9 @@ import omit from 'lodash/omit';
 import {t} from 'sentry/locale';
 import useRouter from 'sentry/utils/useRouter';
 import DetailPanel from 'sentry/views/starfish/components/detailPanel';
-import DurationChart from 'sentry/views/starfish/views/spanSummaryPage/durationChart';
-import SampleInfo from 'sentry/views/starfish/views/spanSummaryPage/sampleInfo';
-import SampleTable from 'sentry/views/starfish/views/spanSummaryPage/sampleList/sampleTable';
+import DurationChart from 'sentry/views/starfish/views/spanSummaryPage/sampleList/durationChart';
+import SampleInfo from 'sentry/views/starfish/views/spanSummaryPage/sampleList/sampleInfo';
+import SampleTable from 'sentry/views/starfish/views/spanSummaryPage/sampleList/sampleTable/sampleTable';
 
 type Props = {
   groupId: string;
