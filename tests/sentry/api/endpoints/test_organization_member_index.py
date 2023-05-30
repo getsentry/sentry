5cc2909567afc9c35bef4925e8a7ba341e1e9eab
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-05-29 23:19:26 -0700

    feat(api): Support member query by user.id (#49889)
    Similar to https://github.com/getsentry/sentry/pull/49877, we want to be
    able (from the frontend) to query for members with a specific user ID

--- tests/sentry/api/endpoints/test_organization_member_index.py
+++ tests/sentry/api/endpoints/test_organization_member_index.py
@@ -166,6 +166,16 @@ class OrganizationMemberListTest(OrganizationMemberListTestBase, HybridCloudTest
         assert len(response.data) == 1
         assert response.data[0]["email"] == "billy@localhost"
 
+    def test_user_id_query(self):
+        user = self.create_user("zoo@localhost", username="zoo")
+        OrganizationMember.objects.create(user=user, organization=self.organization)
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"query": f"user.id:{user.id}"}
+        )
+
+        assert len(response.data) == 1
+        assert response.data[0]["email"] == "zoo@localhost"
+
     def test_query_null_user(self):
         OrganizationMember.objects.create(email="billy@localhost", organization=self.organization)
         response = self.get_success_response(self.organization.slug, qs_params={"query": "bill"})
