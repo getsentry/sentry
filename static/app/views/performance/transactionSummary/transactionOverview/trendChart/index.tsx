b7bcfb503f06916ebf6cd6c0e9f4f9b5c0eb5f16
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2023-05-30 16:38:04 +0200

    check for compatibility before using new trends

--- static/app/views/performance/transactionSummary/transactionOverview/trendChart/index.tsx
+++ static/app/views/performance/transactionSummary/transactionOverview/trendChart/index.tsx
@@ -15,6 +15,7 @@ import {getUtcToLocalDateObject} from 'sentry/utils/dates';
 import EventView from 'sentry/utils/discover/eventView';
 import {DURATION_UNITS, SIZE_UNITS} from 'sentry/utils/discover/fieldRenderers';
 import {getAggregateAlias} from 'sentry/utils/discover/fields';
+import {useMetricsCardinalityContext} from 'sentry/utils/performance/contexts/metricsCardinality';
 import TrendsDiscoverQuery from 'sentry/utils/performance/trends/trendsDiscoverQuery';
 import useApi from 'sentry/utils/useApi';
 import {useLocation} from 'sentry/utils/useLocation';
@@ -65,6 +66,10 @@ function TrendChart({
   const location = useLocation();
   const api = useApi();
   const theme = useTheme();
+
+  const {isLoading: isCardinalityCheckLoading, outcome} = useMetricsCardinalityContext();
+  const shouldGetBreakpoint =
+    withBreakpoint && !isCardinalityCheckLoading && !outcome?.forceTransactionsOnly;
 
   function handleLegendSelectChanged(legendChange: {
     name: string;
@@ -170,14 +175,14 @@ function TrendChart({
   return (
     <Fragment>
       {header}
-      {withBreakpoint ? (
+      {shouldGetBreakpoint ? (
         // queries events-trends-statsv2 for breakpoint data (feature flag only)
         <TrendsDiscoverQuery
           eventView={trendView}
           orgSlug={organization.slug}
           location={location}
           limit={1}
-          withBreakpoint={withBreakpoint}
+          withBreakpoint
         >
           {({isLoading, trendsData}) => {
             const events = normalizeTrends(
@@ -221,7 +226,7 @@ function TrendChart({
               <Content
                 series={timeSeriesMetricsData}
                 errored={!trendsData && !isLoading}
-                loading={isLoading}
+                loading={isLoading || isCardinalityCheckLoading}
                 reloading={isLoading}
                 timeFrame={metricsTimeFrame}
                 withBreakpoint
@@ -276,7 +281,7 @@ function TrendChart({
               <Content
                 series={timeseriesData}
                 errored={errored}
-                loading={loading}
+                loading={loading || isCardinalityCheckLoading}
                 reloading={reloading}
                 timeFrame={timeFrame}
                 {...contentCommonProps}
