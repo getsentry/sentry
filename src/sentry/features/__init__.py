85d82be4b4615b68d350b0d045da5f15a5d123ec
Elias Hussary <elias.hussary@sentry.io>
2023-05-30 15:49:04 -0400

    feat(replays): add click search banner rollout flag (#49903)
    ## Summary
    Adds feature flags to support the rollout of the click search banner
    Related: https://github.com/getsentry/sentry/pull/49708

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -164,6 +164,7 @@ default_manager.add("organizations:scim-team-roles", OrganizationFeature, Featur
 default_manager.add("organizations:org-roles-for-teams", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:sentry-functions", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
+default_manager.add("organizations:session-replay-click-search-banner-rollout", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay-ui", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay-beta-grace", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:session-replay-ga", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
