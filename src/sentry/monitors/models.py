a79db1ddb98e8fb44c367d727d6b8876c52c074e
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-05-31 14:49:12 -0700

    fixed platform

--- src/sentry/monitors/models.py
+++ src/sentry/monitors/models.py
@@ -528,7 +528,7 @@ class MonitorEnvironment(Model):
                 {
                     "environment": self.environment.name,
                     "event_id": occurrence.event_id,
-                    "platform": "",
+                    "platform": "other",
                     "project_id": self.monitor.project_id,
                     "received": current_timestamp.isoformat(),
                     "sdk": None,
