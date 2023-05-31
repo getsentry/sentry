6d72abcf168cf32faf9bfe4d779745ba3b5867b2
Alberto Leal <mail4alberto@gmail.com>
2023-05-31 17:05:04 -0400

    add test_middleware_as_non_superuser

--- tests/sentry/auth/test_superuser.py
+++ tests/sentry/auth/test_superuser.py
@@ -323,6 +323,22 @@ class SuperuserTestCase(TestCase):
             domain=COOKIE_DOMAIN,
         )
 
+    def test_middleware_as_non_superuser(self):
+        user = self.create_user("foo@example.com", is_superuser=False)
+        request = self.build_request(user=user)
+
+        delattr(request, "superuser")
+        delattr(request, "is_superuser")
+
+        middleware = SuperuserMiddleware()
+        middleware.process_request(request)
+        assert not request.superuser.is_active
+        assert not request.is_superuser()
+
+        response = Mock()
+        middleware.process_response(request, response)
+        assert not response.set_signed_cookie.called
+
     def test_changed_user(self):
         request = self.build_request()
         superuser = Superuser(request, allowed_ips=())
