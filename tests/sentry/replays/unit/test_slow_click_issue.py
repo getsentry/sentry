6054c806254d4c12715f323130a92eea943b30b6
Colton Allen <colton.allen@sentry.io>
2023-06-01 07:59:42 -0500

    Add tests

---
+++ tests/sentry/replays/unit/test_slow_click_issue.py
@@ -0,0 +1,78 @@
+import time
+
+import pytest
+
+from sentry import options
+from sentry.replays.usecases.ingest.slow_click import report_slow_click_issue
+
+
+@pytest.mark.django_db
+def test_report_slow_click_issue_a_tag():
+    options.set("replay.issues.slow_click", True)
+
+    event = {
+        "data": {
+            "payload": {
+                "data": {"node": {"tagName": "a"}, "endReason": "timeout"},
+                "message": "div.xyz > a",
+                "timestamp": time.time(),
+            }
+        }
+    }
+
+    reported = report_slow_click_issue(project_id=1, event=event)
+    assert reported is True
+
+
+@pytest.mark.django_db
+def test_report_slow_click_issue_other_tag():
+    options.set("replay.issues.slow_click", True)
+
+    event = {
+        "data": {
+            "payload": {
+                "data": {"node": {"tagName": "div"}, "endReason": "timeout"},
+                "message": "div.xyz > a",
+                "timestamp": time.time(),
+            }
+        }
+    }
+
+    reported = report_slow_click_issue(project_id=1, event=event)
+    assert reported is False
+
+
+@pytest.mark.django_db
+def test_report_slow_click_issue_mutation_reason():
+    options.set("replay.issues.slow_click", True)
+
+    event = {
+        "data": {
+            "payload": {
+                "data": {"node": {"tagName": "a"}, "endReason": "mutation"},
+                "message": "div.xyz > a",
+                "timestamp": time.time(),
+            }
+        }
+    }
+
+    reported = report_slow_click_issue(project_id=1, event=event)
+    assert reported is False
+
+
+@pytest.mark.django_db
+def test_report_slow_click_issue_disabled_option():
+    options.set("replay.issues.slow_click", False)
+
+    event = {
+        "data": {
+            "payload": {
+                "data": {"node": {"tagName": "a"}, "endReason": "timeout"},
+                "message": "div.xyz > a",
+                "timestamp": time.time(),
+            }
+        }
+    }
+
+    reported = report_slow_click_issue(project_id=1, event=event)
+    assert reported is False
