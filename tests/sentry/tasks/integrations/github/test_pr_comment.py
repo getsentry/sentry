e75017911ecd300c9a240e56881c9ade8868fe89
Aniket Das <aniket.das@sentry.io>
2023-06-01 11:54:37 -0700

    Added more tests

--- tests/sentry/tasks/integrations/github/test_pr_comment.py
+++ tests/sentry/tasks/integrations/github/test_pr_comment.py
@@ -65,7 +65,9 @@ class TestPrToIssueQuery(TestCase):
         self.commit_sha += 1
         return commit
 
-    def add_pr_to_commit(self, commit: Commit):
+    def add_pr_to_commit(self, commit: Commit, date_added=None):
+        if date_added is None:
+            date_added = iso_format(before_now(minutes=1))
         pr = PullRequest.objects.create(
             organization_id=commit.organization_id,
             repository_id=commit.repository_id,
@@ -74,6 +76,7 @@ class TestPrToIssueQuery(TestCase):
             message="foo",
             title="bar",
             merge_commit_sha=commit.key,
+            date_added=date_added,
         )
         self.pr_key += 1
         return pr
@@ -94,10 +97,84 @@ class TestPrToIssueQuery(TestCase):
         """one pr with one issue"""
         commit = self.add_commit_to_repo(self.gh_repo, self.user, self.project)
         pr = self.add_pr_to_commit(commit)
-        pr.date_added = iso_format(before_now(minutes=1))
-        pr.save()
         groupowner = self.add_groupowner_to_commit(commit, self.project, self.user)
 
         results = pr_comment.pr_to_issue_query()
 
         assert results[0] == (self.gh_repo.id, pr.key, self.organization.id, [groupowner.id])
+
+    def test_multiple_issues(self):
+        """one pr with multiple issues"""
+        commit = self.add_commit_to_repo(self.gh_repo, self.user, self.project)
+        pr = self.add_pr_to_commit(commit)
+        groupowner_1 = self.add_groupowner_to_commit(commit, self.project, self.user)
+        groupowner_2 = self.add_groupowner_to_commit(commit, self.project, self.user)
+        groupowner_3 = self.add_groupowner_to_commit(commit, self.project, self.user)
+
+        results = pr_comment.pr_to_issue_query()
+
+        assert results[0][0:3] == (self.gh_repo.id, pr.key, self.organization.id)
+        assert (
+            groupowner_1.id in results[0][3]
+            and groupowner_2.id in results[0][3]
+            and groupowner_3.id in results[0][3]
+        )
+
+    def test_multiple_prs(self):
+        """multiple elligible PRs with one issue each"""
+        commit_1 = self.add_commit_to_repo(self.gh_repo, self.user, self.project)
+        commit_2 = self.add_commit_to_repo(self.gh_repo, self.user, self.project)
+        pr_1 = self.add_pr_to_commit(commit_1)
+        pr_2 = self.add_pr_to_commit(commit_2)
+        groupowner_1 = self.add_groupowner_to_commit(commit_1, self.project, self.user)
+        groupowner_2 = self.add_groupowner_to_commit(commit_2, self.project, self.user)
+
+        results = pr_comment.pr_to_issue_query()
+
+        assert results[0] == (self.gh_repo.id, pr_1.key, self.organization.id, [groupowner_1.id])
+        assert results[1] == (self.gh_repo.id, pr_2.key, self.organization.id, [groupowner_2.id])
+
+    def test_non_gh_repo(self):
+        """Repos that aren't GH should be omitted"""
+
+        commit = self.add_commit_to_repo(self.not_gh_repo, self.user, self.project)
+        self.add_pr_to_commit(commit)
+        self.add_groupowner_to_commit(commit, self.project, self.user)
+
+        results = pr_comment.pr_to_issue_query()
+
+        assert len(results) == 0
+
+    def test_pr_too_old(self):
+        """PRs that are too old should be omitted"""
+
+        commit = self.add_commit_to_repo(self.gh_repo, self.user, self.project)
+        self.add_pr_to_commit(commit, date_added=iso_format(before_now(days=30, seconds=1)))
+        self.add_groupowner_to_commit(commit, self.project, self.user)
+
+        results = pr_comment.pr_to_issue_query()
+
+        assert len(results) == 0
+
+    def test_multiple_orgs(self):
+        """Results should be across multiple orgs"""
+        commit_1 = self.add_commit_to_repo(self.gh_repo, self.user, self.project)
+        commit_2 = self.add_commit_to_repo(
+            self.another_org_repo, self.another_org_user, self.another_org_project
+        )
+        pr_1 = self.add_pr_to_commit(commit_1)
+        pr_2 = self.add_pr_to_commit(commit_2)
+        groupowner_1 = self.add_groupowner_to_commit(commit_1, self.project, self.user)
+        groupowner_2 = self.add_groupowner_to_commit(
+            commit_2, self.another_org_project, self.another_org_user
+        )
+
+        results = pr_comment.pr_to_issue_query()
+
+        assert results[0] == (self.gh_repo.id, pr_1.key, self.organization.id, [groupowner_1.id])
+        assert results[1] == (
+            self.another_org_repo.id,
+            pr_2.key,
+            self.another_organization.id,
+            [groupowner_2.id],
+        )
