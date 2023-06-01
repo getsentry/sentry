07eddd8d981ea722f1b786ddd1b4152d54e21285
Stephen Cefali <scefali@sentry.io>
2023-06-01 09:58:16 -0700

    fix(sentry-apps): pass org (#50170)
    Seems like this was a bug caused from some refactoring of the
    organization store

--- static/app/components/modals/sentryAppDetailsModal.tsx
+++ static/app/components/modals/sentryAppDetailsModal.tsx
@@ -178,7 +178,7 @@ export default class SentryAppDetailsModal extends AsyncComponent<Props, State> 
                     {t('Cancel')}
                   </Button>
 
-                  <Access access={['org:integrations']}>
+                  <Access access={['org:integrations']} organization={organization}>
                     {({hasAccess}) =>
                       hasAccess && (
                         <Button
