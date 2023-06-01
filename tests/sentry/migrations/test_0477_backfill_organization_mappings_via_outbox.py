d2d9056198b0a81a343c1fcbe702c6a318ec3385
Gabe Villalobos <gabriel.villalobos@sentry.io>
2023-06-01 14:25:07 -0700

    feat(HC) Adds backfill for organization mappings with unique org IDs and slugs

---
+++ tests/sentry/migrations/test_0477_backfill_organization_mappings_via_outbox.py
@@ -0,0 +1,53 @@
+from sentry.models import OrganizationMapping
+from sentry.testutils.cases import TestMigrations
+from sentry.testutils.outbox import outbox_runner
+
+
+class BackfillNotificationSettingTest(TestMigrations):
+    migrate_from = "0476_convert_unresolved_to_set_escalating_activitytype"
+    migrate_to = "0477_backfill_organization_mappings_via_outbox"
+
+    def setup_initial_state(self):
+        self.org_without_mapping = self.create_organization(name="foo", slug="foo-slug")
+        self.org_with_existing_mapping = self.create_organization(name="bar", slug="bar-slug")
+        self.org_with_mismatching_mapping = self.create_organization(
+            name="foobar", slug="foobar-slug"
+        )
+
+        # Delete the org mapping for one of the organizations
+        OrganizationMapping.objects.get(organization_id=self.org_without_mapping.id).delete()
+
+        mismatch_mapping = OrganizationMapping.objects.get(
+            organization_id=self.org_with_mismatching_mapping.id
+        )
+        mismatch_mapping.name = "old_name"
+        mismatch_mapping.slug = "old-slug"
+        mismatch_mapping.save()
+
+    def test_backfill_of_org_mappings(self):
+        with outbox_runner():
+            pass
+
+        newly_created_org_mapping = OrganizationMapping.objects.get(
+            organization_id=self.org_without_mapping.id
+        )
+        assert newly_created_org_mapping.slug == self.org_without_mapping.slug
+        assert newly_created_org_mapping.name == self.org_without_mapping.name
+        assert newly_created_org_mapping.customer_id == self.org_without_mapping.customer_id
+        assert newly_created_org_mapping.status == self.org_without_mapping.status
+
+        updated_org_mapping = OrganizationMapping.objects.get(
+            organization_id=self.org_with_mismatching_mapping.id
+        )
+        assert updated_org_mapping.slug == self.org_with_mismatching_mapping.slug
+        assert updated_org_mapping.name == self.org_with_mismatching_mapping.name
+        assert updated_org_mapping.customer_id == self.org_with_mismatching_mapping.customer_id
+        assert updated_org_mapping.status == self.org_with_mismatching_mapping.status
+
+        untouched_org_mapping = OrganizationMapping.objects.get(
+            organization_id=self.org_with_existing_mapping.id
+        )
+        assert untouched_org_mapping.slug == self.org_with_existing_mapping.slug
+        assert untouched_org_mapping.name == self.org_with_existing_mapping.name
+        assert untouched_org_mapping.customer_id == self.org_with_existing_mapping.customer_id
+        assert untouched_org_mapping.status == self.org_with_existing_mapping.status
