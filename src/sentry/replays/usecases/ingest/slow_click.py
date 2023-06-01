d307e5d2cbf1a8285364648de612d59d2666e9c5
Colton Allen <colton.allen@sentry.io>
2023-06-01 07:59:10 -0500

    Create an issue when a slow click event is seen

---
+++ src/sentry/replays/usecases/ingest/slow_click.py
@@ -0,0 +1,68 @@
+import datetime
+from typing import Any, Dict
+
+from sentry import options
+from sentry.issues.grouptype import ReplaySlowClickType
+from sentry.replays.usecases.ingest.events import SentryEvent
+from sentry.replays.usecases.issue import new_issue_occurrence
+
+
+def report_slow_click_issue(project_id: int, event: SentryEvent) -> bool:
+    # Only report slow clicks if the option is enabled.
+    if not options.get("replay.issues.slow_click"):
+        return False
+
+    payload = event["data"]["payload"]
+
+    # Only timeout reasons on <a> and <button> tags are accepted.
+    if payload["data"]["node"]["tagName"] not in ("a", "button"):
+        return False
+    elif payload["data"]["endReason"] != "timeout":
+        return False
+
+    # Seconds since epoch is UTC.
+    timestamp = datetime.datetime.fromtimestamp(payload["timestamp"])
+    timestamp = timestamp.replace(tzinfo=datetime.timezone.utc)
+
+    _report_slow_click_issue(
+        environment="prod",
+        fingerprint=payload["message"],
+        project_id=project_id,
+        release="",
+        subtitle=payload["message"],
+        timestamp=timestamp,
+        extra_event_data={
+            "level": "info",
+            "user": {
+                "id": "1",
+                "username": "Test User",
+                "email": "test.user@sentry.io",
+            },
+        },
+    )
+
+    return True
+
+
+def _report_slow_click_issue(
+    environment: str,
+    fingerprint: str,
+    project_id: int,
+    release: str,
+    subtitle: str,
+    timestamp: datetime.datetime,
+    extra_event_data: Dict[str, Any],
+) -> None:
+    """Produce a new slow click issue occurence to Kafka."""
+    new_issue_occurrence(
+        environment=environment,
+        fingerprint=fingerprint,
+        issue_type=ReplaySlowClickType,
+        platform="javascript",
+        project_id=project_id,
+        release=release,
+        subtitle=subtitle,
+        timestamp=timestamp,
+        title="[TEST] Slow Click Detected",
+        extra_event_data=extra_event_data,
+    )
---
+++ src/sentry/replays/usecases/issue.py
@@ -0,0 +1,53 @@
+import datetime
+import uuid
+from typing import Any, Dict, Type
+
+from sentry.issues.grouptype import GroupType
+from sentry.issues.issue_occurrence import IssueOccurrence
+from sentry.issues.producer import produce_occurrence_to_kafka
+
+
+def new_issue_occurrence(
+    environment: str,
+    fingerprint: str,
+    issue_type: Type[GroupType],
+    platform: str,
+    project_id: int,
+    release: str,
+    subtitle: str,
+    timestamp: datetime.datetime,
+    title: str,
+    extra_event_data: Dict[str, Any],
+) -> None:
+    """Produce a new issue occurence to Kafka."""
+    event_id = uuid.uuid4().hex
+    extra_event_data["event_id"] = event_id
+
+    occurrence = IssueOccurrence(
+        id=uuid.uuid4().hex,
+        project_id=project_id,
+        event_id=event_id,
+        fingerprint=fingerprint,
+        issue_title=title,
+        subtitle=subtitle,
+        resource_id=None,
+        evidence_data=[],
+        evidence_display=[],
+        type=issue_type,
+        detection_time=timestamp,
+        level="info",
+        culprit=None,
+    )
+
+    event_data = {
+        "id": event_id,
+        "environment": environment,
+        "platform": platform,
+        "project_id": project_id,
+        "received": timestamp.isoformat(),
+        "release": release,
+        "timestamp": timestamp.isoformat(),
+    }
+    event_data.update(extra_event_data)
+
+    produce_occurrence_to_kafka(occurrence, event_data=event_data)
