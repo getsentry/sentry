af6f6a7882c3ac1108c5d3ccf48c72d8b7437e93
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-06-01 14:47:28 -0700

    fix(crons): Truncates Monitor alert rule name (#50178)
    Truncates monitor alert rule name to make sure it passes validation

--- src/sentry/monitors/models.py
+++ src/sentry/monitors/models.py
@@ -36,7 +36,7 @@ from sentry.issues.grouptype import (
     MonitorCheckInTimeout,
 )
 from sentry.locks import locks
-from sentry.models import Environment, Organization, Rule, RuleSource
+from sentry.models import Environment, Organization, Rule, RuleSource, RuleStatus
 from sentry.utils.retries import TimedRetryPolicy
 
 logger = logging.getLogger(__name__)
@@ -303,7 +303,10 @@ class Monitor(Model):
         alert_rule_id = self.config.get("alert_rule_id")
         if alert_rule_id:
             alert_rule = Rule.objects.filter(
-                project_id=self.project_id, id=alert_rule_id, source=RuleSource.CRON_MONITOR
+                project_id=self.project_id,
+                id=alert_rule_id,
+                source=RuleSource.CRON_MONITOR,
+                status__in=[RuleStatus.ACTIVE, RuleStatus.INACTIVE],
             ).first()
             if alert_rule:
                 return alert_rule
