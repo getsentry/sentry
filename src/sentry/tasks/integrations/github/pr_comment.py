f7cbe283839f9996f6d40fa12e03620ffb858cf8
Cathy Teng <cathy.teng@sentry.io>
2023-06-01 15:21:05 -0700

    fixes from review

--- src/sentry/tasks/integrations/github/pr_comment.py
+++ src/sentry/tasks/integrations/github/pr_comment.py
@@ -1,23 +1,42 @@
-from typing import List, Tuple
+from dataclasses import dataclass
+from typing import List
 
 from django.db import connection
 
 from sentry.models import GroupOwnerType
 
 
-def format_comment(issues: List[Tuple[str]]):
-    body = "## Suspect Issues\nThis pull request has been deployed and Sentry has observed the following issues:\n\n"
+@dataclass
+class PullRequestIssue:
+    title: str
+    subtitle: str
+    url: str
 
-    for issue in issues:
-        title = issue[0]
-        subtitle = issue[1]
-        formatted_subtitle = subtitle[:47] + "..." if len(subtitle) > 50 else subtitle
-        url = issue[2]
-        body += f"- ‼️ **{title}** `{formatted_subtitle}` [View Issue]({url})\n"
 
-    body += "\n<sub>Did you find this useful? React with a 👍 or 👎</sub>"
+body = """## Suspect Issues
+This pull request has been deployed and Sentry has observed the following issues
 
-    return body
+{issue_list}
+
+<sub>Did you find this useful? React with a \U0001F44D or \U0001F44E</sub>"""
+
+single_issue_template = "- ‼️ **{title}** `{subtitle}` [View Issue]({url})"
+
+
+def format_comment(issues: List[PullRequestIssue]):
+    def format_subtitle(subtitle):
+        return subtitle[:47] + "..." if len(subtitle) > 50 else subtitle
+
+    issue_list = "\n".join(
+        [
+            single_issue_template.format(
+                title=issue.title, subtitle=format_subtitle(issue.subtitle), url=issue.url
+            )
+            for issue in issues
+        ]
+    )
+
+    return body.format(issue_list=issue_list)
 
 
 def pr_to_issue_query():
