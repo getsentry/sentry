b6012d3663e13181d7e527e687e2188a68a50ac5
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-06-02 10:51:36 -0700

    less typing when loading fixtures

--- fixtures/js-stubs/types.tsx
+++ fixtures/js-stubs/types.tsx
@@ -5,9 +5,7 @@ import type {
   ReplaySpan,
 } from 'sentry/views/replays/types';
 
-import type * as BreadcrumbFrameData from './replayBreadcrumbFrameData';
-import type * as ReplayFrameEvents from './replayFrameEvents';
-import type * as ReplaySpanFrameData from './replaySpanFrameData';
+import type Replay from './replay';
 
 type SimpleStub<T = any> = () => T;
 
@@ -39,18 +37,13 @@ type TestStubFixtures = {
   Authenticators: SimpleStub;
   AvailableNotificationActions: OverridableStub;
   BitbucketIntegrationConfig: SimpleStub;
-  BlurFrame: typeof BreadcrumbFrameData.BlurFrame;
   Breadcrumb: OverridableStub;
-  BreadcrumbFrame: typeof ReplayFrameEvents.BreadcrumbFrame;
-  BreadcrumbFrameEvent: typeof ReplayFrameEvents.BreadcrumbFrameEvent;
   Broadcast: OverridableStub;
   BuiltInSymbolSources: OverridableStubList;
-  ClickFrame: typeof BreadcrumbFrameData.ClickFrame;
   CodeOwner: OverridableStub;
   Commit: OverridableStub;
   CommitAuthor: OverridableStub;
   Config: OverridableStub;
-  ConsoleFrame: typeof BreadcrumbFrameData.ConsoleFrame;
   DataScrubbingRelayPiiConfig: SimpleStub;
   DebugFile: OverridableStub;
   DebugSymbols: OverridableStub;
@@ -72,8 +65,6 @@ type TestStubFixtures = {
   EventsStats: OverridableStub;
   ExceptionWithMeta: OverridableStubList;
   ExceptionWithRawStackTrace: OverridableStub;
-  FetchFrame: typeof BreadcrumbFrameData.FetchFrame;
-  FocusFrame: typeof BreadcrumbFrameData.FocusFrame;
   GitHubIntegration: OverridableStub;
   GitHubIntegrationConfig: SimpleStub;
   GitHubIntegrationProvider: OverridableStub;
@@ -84,30 +75,20 @@ type TestStubFixtures = {
   GroupingConfigs: SimpleStub;
   GroupingEnhancements: SimpleStub;
   Groups: SimpleStub;
-  HistoryData: typeof ReplaySpanFrameData.HistoryData;
   Incident: OverridableStub;
   IncidentActivity: OverridableStub;
   IncidentStats: OverridableStub;
   IncidentTrigger: OverridableStub;
-  InputFrame: typeof BreadcrumbFrameData.InputFrame;
   InstallWizard: OverridableStub;
   JiraIntegration: OverridableStub;
   JiraIntegrationProvider: OverridableStub;
-  KeyboardEventFrame: typeof BreadcrumbFrameData.KeyboardEventFrame;
-  LargestContentfulPaintData: typeof ReplaySpanFrameData.LargestContentfulPaintData;
   Member: OverridableStub;
   Members: OverridableStubList;
-  MemoryData: typeof ReplaySpanFrameData.MemoryData;
   MetricRule: OverridableStub;
   MetricsField: OverridableStub;
   MetricsMeta: OverridableStub;
   MetricsSessionUserCountByStatusByRelease: SimpleStub;
   MetricsTotalCountByReleaseIn24h: SimpleStub;
-  MutationFrame: typeof BreadcrumbFrameData.MutationFrame;
-  NavigationData: typeof ReplaySpanFrameData.NavigationData;
-  NetworkRequestData: typeof ReplaySpanFrameData.NetworkRequestData;
-  OptionFrame: typeof ReplayFrameEvents.OptionFrame;
-  OptionFrameEvent: typeof ReplayFrameEvents.OptionFrameEvent;
   OrgOwnedApps: SimpleStub;
   OrgRoleList: OverridableStub;
   Organization: OverridableStub;
@@ -118,7 +99,6 @@ type TestStubFixtures = {
   OutcomesWithLowProcessedEvents: SimpleStub;
   OutcomesWithReason: SimpleStub;
   OutcomesWithoutClientDiscarded: SimpleStub;
-  PaintData: typeof ReplaySpanFrameData.PaintData;
   PhabricatorCreate: SimpleStub;
   PhabricatorPlugin: SimpleStub;
   PlatformExternalIssue: OverridableStub;
@@ -135,6 +115,7 @@ type TestStubFixtures = {
   PublishedApps: SimpleStub;
   PullRequest: OverridableStub;
   Release: (params?: any, healthParams?: any) => any;
+  Replay: typeof Replay;
   ReplayError: OverridableStub;
   ReplayList: OverridableStubList<ReplayListRecord>;
   ReplayRRWebDivHelloWorld: OverridableStub;
@@ -156,7 +137,6 @@ type TestStubFixtures = {
   >;
   Repository: OverridableStub;
   RepositoryProjectPathConfig: OverridableStub;
-  ResourceData: typeof ReplaySpanFrameData.ResourceData;
   Search: OverridableStub;
   Searches: OverridableStubList;
   SentryApp: OverridableStub;
@@ -180,14 +160,11 @@ type TestStubFixtures = {
   SessionsField: OverridableStub;
   SesssionTotalCountByReleaseIn24h: SimpleStub;
   ShortIdQueryResult: OverridableStub;
-  SlowClickFrame: typeof BreadcrumbFrameData.SlowClickFrame;
   SourceMapArchive: OverridableStub;
   SourceMapArtifact: OverridableStub;
   SourceMapsDebugIDBundles: OverridableStub;
   SourceMapsDebugIDBundlesArtifacts: OverridableStub;
   Span: OverridableStub;
-  SpanFrame: typeof ReplayFrameEvents.SpanFrame;
-  SpanFrameEvent: typeof ReplayFrameEvents.SpanFrameEvent;
   Subscriptions: OverridableStubList;
   TagValues: OverridableStubList;
   Tags: OverridableStubList;
@@ -210,7 +187,6 @@ type TestStubFixtures = {
   VstsCreate: SimpleStub;
   VstsIntegrationProvider: OverridableStub;
   VstsPlugin: SimpleStub;
-  XhrFrame: typeof BreadcrumbFrameData.XhrFrame;
 
   // TODO: These need propertly typed still
   // Widget(queries = {...DEFAULT_QUERIES}, options)
