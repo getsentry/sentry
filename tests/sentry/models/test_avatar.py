ebf50ec548f5e47ec43bd8ae8348e93e2e0c8407
Mike Ihbe <mike.ihbe@sentry.io>
2023-06-02 08:42:15 -0700

    Fix tests

--- tests/sentry/models/test_avatar.py
+++ tests/sentry/models/test_avatar.py
@@ -11,16 +11,22 @@ from sentry.testutils.silo import control_silo_test, region_silo_test
 @control_silo_test(stable=True)
 class UserAvatarTestCase(TestCase):
     def test_set_null(self):
-        user = self.create_user("foo@example.com")
-        afile = ControlFile.objects.create(name="avatar.png", type=UserAvatar.FILE_TYPE)
-        avatar = UserAvatar.objects.create(user=user, control_file_id=afile.id)
+        with self.options(
+            {
+                "filestore.control.backend": options_store.get("filestore.backend"),
+                "filestore.control.options": options_store.get("filestore.options"),
+            }
+        ):
+            user = self.create_user("foo@example.com")
+            afile = ControlFile.objects.create(name="avatar.png", type=UserAvatar.FILE_TYPE)
+            avatar = UserAvatar.objects.create(user=user, control_file_id=afile.id)
 
-        assert avatar.get_file() == afile
+            assert avatar.get_file() == afile
 
-        afile.delete()
-        assert avatar.get_file() is None
-        assert UserAvatar.objects.get(id=avatar.id).control_file_id is None
-        assert UserAvatar.objects.get(id=avatar.id).get_file() is None
+            afile.delete()
+            assert avatar.get_file() is None
+            assert UserAvatar.objects.get(id=avatar.id).control_file_id is None
+            assert UserAvatar.objects.get(id=avatar.id).get_file() is None
 
 
 # Not siloed, this logic will be removed after data is moved.
