f8a508477021055f2c1f3e6bc05dfb1e6afc87cb
Scott Cooper <scttcper@gmail.com>
2023-06-02 13:45:33 -0700

    fix(alerts): Remove issue-alert-incompatible-rules flag
    No longer used

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1306,8 +1306,6 @@ SENTRY_FEATURES = {
     "organizations:rule-page": False,
     # Enable incidents feature
     "organizations:incidents": False,
-    # Enable issue alert incompatible rule check
-    "organizations:issue-alert-incompatible-rules": False,
     # Enable issue alert previews
     "organizations:issue-alert-preview": False,
     # Enable issue alert test notifications
