d087730706131c7e275b67b4dc53e2db2fd9464e
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-06-02 17:17:01 -0700

    fix import

--- fixtures/js-stubs/replay.ts
+++ fixtures/js-stubs/replay.ts
@@ -2,10 +2,8 @@ import * as BreadcrumbFrameData from './replay/replayBreadcrumbFrameData';
 import * as ReplayFrameEvents from './replay/replayFrameEvents';
 import * as ReplaySpanFrameData from './replay/replaySpanFrameData';
 
-const Stubs = {
-  ...ReplayFrameEvents,
+export const Replay = {
   ...BreadcrumbFrameData,
+  ...ReplayFrameEvents,
   ...ReplaySpanFrameData,
 };
-
-export default Stubs;
