5462ad4de4cad1493b2508cf04a71bb1843c0329
Tony Xiao <txiao@sentry.io>
2023-06-05 12:25:41 -0400

    sort frames first

--- static/app/utils/profiling/profile/sentrySampledProfile.tsx
+++ static/app/utils/profiling/profile/sentrySampledProfile.tsx
@@ -14,6 +14,46 @@ function sortSentrySampledProfileSamples(
   stacks: Profiling.SentrySampledProfile['profile']['stacks'],
   frames: Profiling.SentrySampledProfile['profile']['frames']
 ) {
+  const frameIds = [...Array(frames.length).keys()].sort((a, b) => {
+    const frameA = frames[a];
+    const frameB = frames[b];
+
+    if (defined(frameA.function) && defined(frameB.function)) {
+      // sort alphabetically first
+      const ret = frameA.function.localeCompare(frameB.function);
+      if (ret !== 0) {
+        return ret;
+      }
+
+      // break ties using the line number
+      if (defined(frameA.lineno) && defined(frameB.lineno)) {
+        return frameA.lineno - frameB.lineno;
+      }
+
+      if (defined(frameA.lineno)) {
+        return -1;
+      }
+
+      if (defined(frameB.lineno)) {
+        return 1;
+      }
+    } else if (defined(frameA.function)) {
+      // if only frameA is defined, it goes first
+      return -1;
+    } else if (defined(frameB.function)) {
+      // if only frameB is defined, it goes first
+      return 1;
+    }
+
+    // if neither functions are defined, they're treated as equal
+    return 0;
+  });
+
+  const framesMapping = frameIds.reduce((acc, frameId, idx) => {
+    acc[frameId] = idx;
+    return acc;
+  }, {});
+
   return [...samples].sort((a, b) => {
     // same stack id, these are the same
     if (a.stack_id === b.stack_id) {
@@ -34,35 +74,15 @@ function sortSentrySampledProfileSamples(
         continue;
       }
 
-      const frameA = frames[frameIdA];
-      const frameB = frames[frameIdB];
+      const frameIdxA = framesMapping[frameIdA];
+      const frameIdxB = framesMapping[frameIdB];
 
-      if (defined(frameA.function) && defined(frameB.function)) {
-        // sort alphabetically first
-        const ret = frameA.function.localeCompare(frameB.function);
-        if (ret !== 0) {
-          return ret;
-        }
-
-        // break ties using the line number
-        if (defined(frameA.lineno) && defined(frameB.lineno)) {
-          return frameA.lineno - frameB.lineno;
-        }
-
-        if (defined(frameA.lineno)) {
-          return -1;
-        }
-
-        if (defined(frameB.lineno)) {
-          return 1;
-        }
-      } else if (defined(frameA.function)) {
-        // if only one has the function name defined, the defined one goes first
-        return -1;
-      } else if (defined(frameB.function)) {
-        // if only one has the function name defined, the defined one goes first
-        return 1;
+      // same frame idx, so check the next frame in the stack
+      if (frameIdxA === frameIdxB) {
+        continue;
       }
+
+      return frameIdxA - frameIdxB;
     }
 
     // if all frames up to the depth of the shorter stack matches,
