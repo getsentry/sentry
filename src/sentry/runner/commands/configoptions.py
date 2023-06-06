5b525062607c095ba6b60edd53e7582e324c0d4c
Filippo Pacifici <fpacifici@sentry.io>
2023-06-05 21:58:31 -0700

    Wrap tests

--- src/sentry/runner/commands/configoptions.py
+++ src/sentry/runner/commands/configoptions.py
@@ -1,4 +1,4 @@
-from typing import TYPE_CHECKING, Any, Mapping, MutableMapping, Optional
+from typing import TYPE_CHECKING, Any, Mapping, Set
 
 import click
 import yaml
@@ -9,45 +9,67 @@ if TYPE_CHECKING:
     from sentry.options import NotWritableReason
 
 
-class InvalidConfigFile(Exception):
-    def __init__(self, message: str, reason: "NotWritableReason") -> None:
-        super().__init__(message)
+DRIFT_MSG = "[DRIFT] Option %s drifted and cannot be updated."
+CHANNEL_UPDATE_MSG = "[CHANNEL UPDATE] Option: %s value unchanged. Last update channel updated."
+UPDATE_MSG = "[UPDATE] Option: %s updated."
+UNSET_MSG = "[UNSET] Option %s unset."
+
+
+class InvalidOption(Exception):
+    def __init__(self, key: str, reason: "NotWritableReason") -> None:
+        super.__init__(f"Invalid Option: {key}. Reason: {key}")
+        self.key = key
         self.reason = reason
 
-    def __str__(self):
-        return f"{str(super())}. Reason: {self.reason}"
 
-
-def _validate_options(content: Mapping[str, Any]) -> Mapping[str, "NotWritableReason"]:
-
+def _validate_options(content: Mapping[str, Any]) -> Set[str]:
     from sentry import options
 
-    not_writable_reasons: MutableMapping[str, Optional[options.NotWritableReason]] = {}
+    drifted_options = set()
 
     for key, value in content.items():
         not_writable_reason = options.can_update(key, value, options.UpdateChannel.AUTOMATOR)
 
         if not_writable_reason and not_writable_reason != options.NotWritableReason.DRIFTED:
-            raise InvalidConfigFile(f"Invalid entry. Option {key}", not_writable_reason)
+            raise InvalidOption(key, not_writable_reason)
+        elif not_writable_reason == options.NotWritableReason.DRIFTED:
+            drifted_options.add(key)
 
-        not_writable_reasons[key] = not_writable_reason
-
-    return not_writable_reasons
+    return drifted_options
 
 
-def _perform_update(key: str, value: Any, dry_run: bool) -> str:
+def _attempt_update(key: str, value: Any, drifted_options: Set[str], dry_run: bool) -> None:
     from sentry import options
 
+    if key in drifted_options:
+        click.echo(DRIFT_MSG % key)
+        return
+
     if options.get(key) == value:
-        last_update_channel = options.get_last_update_channel(key)
-        if last_update_channel != options.UpdateChannel.AUTOMATOR:
+        if options.get_last_update_channel(key) != options.UpdateChannel.AUTOMATOR:
             if not dry_run:
                 options.set(key, value, coerce=False, channel=options.UpdateChannel.AUTOMATOR)
-        return f"Option: {key} value unchanged. Last update channel updated."
+            click.echo(CHANNEL_UPDATE_MSG % key)
+        return
 
     if not dry_run:
         options.set(key, value, coerce=False, channel=options.UpdateChannel.AUTOMATOR)
-    return f"Option: {key} updated."
+    click.echo(UPDATE_MSG % key)
+
+
+def _load_options_file(filename: str) -> Mapping[str, Any]:
+    with open(filename) as stream:
+        data = yaml.safe_load(stream)
+
+    # This is to support the legacy structure of the options file that
+    # contained multiple sections.
+    # Those sections are not needed anymore as the list of options that
+    # must be provided by the file are all the options flagged as
+    # FLAG_AUTOMATOR_MODIFIABLE.
+    options_to_update: Mapping[str, Any] = data.get("update", {})
+    if not options_to_update:
+        options_to_update = data
+    return options_to_update
 
 
 @click.group()
@@ -58,82 +80,52 @@ def configoptions() -> None:
 @configoptions.command()
 @click.argument("filename", required=True)
 @click.option(
-    "--dryrun",
+    "--dry-run",
     is_flag=True,
     required=False,
     help="Output exactly what changes would be made and in which order.",
 )
 @configuration
-def patch(filename: str, dryrun: bool) -> None:
+def patch(filename: str, dry_run: bool) -> None:
     "Updates, gets, and deletes options that are each subsectioned in the given file."
 
-    if dryrun:
-        click.echo("Dryrun flag on.")
-    with open(filename) as stream:
-        data = yaml.safe_load(stream)
+    if dry_run:
+        click.echo("!!! Dry-run flag on. No update will be performed.")
 
-    # This is to support the legacy structure of the options file that
-    # contained multiple sections.
-    # Those sections are not needed anymore as the list of options that
-    # must be provided by the file are all the options flagged as
-    # FLAG_AUTOMATOR_MODIFIABLE.
-    options_to_update: Mapping[str, Any] = data.get("update", {})
-    if not options_to_update:
-        options_to_update = data
-
-    not_writable_reasons = _validate_options(options_to_update)
+    options_to_update = _load_options_file(filename)
+    drifted_options = _validate_options(options_to_update)
 
     for key, value in options_to_update.items():
-        if not_writable_reasons[key] is not None:
-            click.echo(f"Option {key} cannot be updated. Reason: {not_writable_reasons[key].value}")
-        else:
-            output = _perform_update(key, value, dryrun)
-            click.echo(output)
+        _attempt_update(key, value, drifted_options, dry_run)
 
 
 @configoptions.command()
 @click.argument("filename", required=True)
 @click.option(
-    "--dryrun",
+    "--dry-run",
     is_flag=True,
     default=False,
     required=False,
     help="Output exactly what changes would be made and in which order.",
 )
 @configuration
-def sync(filename: str, dryrun: bool):
+def sync(filename: str, dry_run: bool):
     "Deletes everything not in the uploaded file, and applies all of the changes in the file."
-    import yaml
 
     from sentry import options
 
-    if dryrun:
-        click.echo("Dryrun flag on.")
-    with open(filename) as stream:
-        data = yaml.safe_load(stream)
+    if dry_run:
+        click.echo("!!! Dry-run flag on. No update will be performed.")
 
-    # This is to support the legacy structure of the options file that
-    # contained multiple sections.
-    # Those sections are not needed anymore as the list of options that
-    # must be provided by the file are all the options flagged as
-    # FLAG_AUTOMATOR_MODIFIABLE.
-    options_to_update: Mapping[str, Any] = data.get("update", {})
-    if not options_to_update:
-        options_to_update = data
+    options_to_update = _load_options_file(filename)
 
-    not_writable_reasons = _validate_options(options_to_update)
+    drifted_options = _validate_options(options_to_update)
     all_options = options.filter(options.FLAG_AUTOMATOR_MODIFIABLE)
 
     for opt in all_options:
         if opt.name in options_to_update:
-            if not_writable_reasons[opt.name] is not None:
-                click.echo(
-                    f"Option {opt.name} cannot be updated. Reason: {not_writable_reasons[opt.name].value}"
-                )
-            else:
-                output = _perform_update(opt.name, options_to_update[opt.name], dryrun)
-                click.echo(output)
+            _attempt_update(opt.name, options_to_update[opt.name], drifted_options, dry_run)
         else:
-            if not dryrun:
+            if not dry_run:
                 options.delete(opt.name)
-            click.echo(f"Option {opt.name} unset.")
+            click.echo(UNSET_MSG % opt.name)
--- tests/sentry/runner/commands/test_configoptions.py
+++ tests/sentry/runner/commands/test_configoptions.py
@@ -1,45 +1,17 @@
 from sentry import options
 from sentry.options.manager import UpdateChannel
-from sentry.runner.commands.configoptions import configoptions
+from sentry.runner.commands.configoptions import (
+    CHANNEL_UPDATE_MSG,
+    DRIFT_MSG,
+    UNSET_MSG,
+    UPDATE_MSG,
+    configoptions,
+)
 from sentry.testutils import CliTestCase
 
 
 class ConfigOptionsTest(CliTestCase):
     command = configoptions
-
-    def test_sync(self):
-        options.delete("hybrid_cloud.outbox_rate")
-        options.delete("sourcemaps.enable-artifact-bundles")
-        options.delete("dynamic-sampling:boost-latest-release")
-        options.delete("sentry-metrics.cardinality-limiter.limits.releasehealth.per-org")
-        options.delete("dynamic-sampling:sliding_window.size")
-
-        options.set("hybrid_cloud.outbox_rate", 0.5, channel=UpdateChannel.CLI)
-        options.set("dynamic-sampling:sliding_window.size", 12, channel=UpdateChannel.AUTOMATOR)
-
-        rv = self.invoke("sync", "tests/sentry/runner/commands/valid_patch.yaml")
-        assert rv.exit_code == 0, rv.output
-        assert "Option: sourcemaps.enable-artifact-bundles updated" in rv.output
-        assert "Option: dynamic-sampling:boost-latest-release updated" in rv.output
-        assert (
-            "Option: sentry-metrics.cardinality-limiter.limits.releasehealth.per-org updated"
-            in rv.output
-        )
-        assert "Option hybrid_cloud.outbox_rate cannot be updated. Reason: drifted" in rv.output
-        assert "Option dynamic-sampling:sliding_window.size unset" in rv.output
-
-        assert options.isset("sourcemaps.enable-artifact-bundles")
-        assert options.get("sourcemaps.enable-artifact-bundles") == 0.1
-        assert options.isset("dynamic-sampling:boost-latest-release")
-        assert options.get("dynamic-sampling:boost-latest-release") is True
-        assert options.isset("sentry-metrics.cardinality-limiter.limits.releasehealth.per-org")
-        assert options.get("sentry-metrics.cardinality-limiter.limits.releasehealth.per-org") == [
-            "a",
-            "b",
-        ]
-        assert options.get("hybrid_cloud.outbox_rate") == 0.5
-
-        assert not options.isset("dynamic-sampling:sliding_window.size")
 
     def test_patch(self):
         def clean_cache() -> None:
@@ -56,6 +28,9 @@ class ConfigOptionsTest(CliTestCase):
                     "sentry-metrics.cardinality-limiter.limits.releasehealth.per-org"
                 )
             )
+            options.default_store.delete_cache(
+                options.lookup_key("backpressure.monitor_queues.enable")
+            )
 
         def assert_not_set() -> None:
             clean_cache()
@@ -67,18 +42,22 @@ class ConfigOptionsTest(CliTestCase):
 
         def assert_output(rv):
             assert rv.exit_code == 0, rv.output
-            assert "Option: sourcemaps.enable-artifact-bundles updated" in rv.output
-            assert "Option: dynamic-sampling:boost-latest-release updated" in rv.output
-            assert (
-                "Option: sentry-metrics.cardinality-limiter.limits.releasehealth.per-org updated"
-                in rv.output
+            output = "\n".join(
+                [
+                    DRIFT_MSG % "hybrid_cloud.outbox_rate",
+                    UPDATE_MSG % "sourcemaps.enable-artifact-bundles",
+                    UPDATE_MSG % "dynamic-sampling:boost-latest-release",
+                    UPDATE_MSG % "sentry-metrics.cardinality-limiter.limits.releasehealth.per-org",
+                    CHANNEL_UPDATE_MSG % "backpressure.monitor_queues.enable",
+                ]
             )
-            assert "Option hybrid_cloud.outbox_rate cannot be updated. Reason: drifted" in rv.output
+            assert output in rv.output
 
         options.set("hybrid_cloud.outbox_rate", 0.5, channel=UpdateChannel.CLI)
+        options.set("backpressure.monitor_queues.enable", True, channel=UpdateChannel.CLI)
 
         assert_not_set()
-        rv = self.invoke("patch", "--dryrun", "tests/sentry/runner/commands/valid_patch.yaml")
+        rv = self.invoke("patch", "--dry-run", "tests/sentry/runner/commands/valid_patch.yaml")
         assert_output(rv)
 
         assert_not_set()
@@ -97,6 +76,36 @@ class ConfigOptionsTest(CliTestCase):
             "b",
         ]
         assert options.get("hybrid_cloud.outbox_rate") == 0.5
+        assert options.get("backpressure.monitor_queues.enable") is True
+
+    def test_sync(self):
+        options.delete("sourcemaps.enable-artifact-bundles")
+        options.delete("dynamic-sampling:boost-latest-release")
+        options.delete("sentry-metrics.cardinality-limiter.limits.releasehealth.per-org")
+
+        options.set("hybrid_cloud.outbox_rate", 0.5, channel=UpdateChannel.CLI)
+        options.set("dynamic-sampling:sliding_window.size", 12, channel=UpdateChannel.AUTOMATOR)
+
+        rv = self.invoke("sync", "tests/sentry/runner/commands/valid_patch.yaml")
+        assert rv.exit_code == 0, rv.output
+        assert UPDATE_MSG % "sourcemaps.enable-artifact-bundles" in rv.output
+        assert UPDATE_MSG % "dynamic-sampling:boost-latest-release" in rv.output
+        assert (
+            UPDATE_MSG % "sentry-metrics.cardinality-limiter.limits.releasehealth.per-org"
+            in rv.output
+        )
+        assert DRIFT_MSG % "hybrid_cloud.outbox_rate" in rv.output
+        assert UNSET_MSG % "dynamic-sampling:sliding_window.size" in rv.output
+
+        assert options.get("sourcemaps.enable-artifact-bundles") == 0.1
+        assert options.get("dynamic-sampling:boost-latest-release") is True
+        assert options.get("sentry-metrics.cardinality-limiter.limits.releasehealth.per-org") == [
+            "a",
+            "b",
+        ]
+        assert options.get("hybrid_cloud.outbox_rate") == 0.5
+
+        assert not options.isset("dynamic-sampling:sliding_window.size")
 
     def test_bad_patch(self):
         rv = self.invoke("patch", "tests/sentry/runner/commands/badpatch.yaml")
