f891fc7a2c28f0dabb2a7462d67ea39cbc1797cf
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-06-06 10:21:24 -0700

    chore(ts): Use UPPER_CASE for ExperimentType (#50387)

--- static/app/data/experimentConfig.tsx
+++ static/app/data/experimentConfig.tsx
@@ -14,19 +14,19 @@ export const unassignedValue = -1;
 export const experimentList = [
   {
     key: 'OnboardingNewFooterExperiment',
-    type: ExperimentType.Organization,
+    type: ExperimentType.ORGANIZATION,
     parameter: 'scenario',
     assignments: ['baseline', 'variant1', 'variant2'],
   },
   {
     key: 'ExtendTrialByInvitingMemberExperiment',
-    type: ExperimentType.Organization,
+    type: ExperimentType.ORGANIZATION,
     parameter: 'exposed',
     assignments: [0, 1],
   },
   {
     key: 'ProjectCreationForAllExperiment',
-    type: ExperimentType.Organization,
+    type: ExperimentType.ORGANIZATION,
     parameter: 'exposed',
     assignments: [0, 1],
   },
