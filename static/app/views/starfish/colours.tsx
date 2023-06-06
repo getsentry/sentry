bdcf344082b302242def241a07b1cb980f865fa0
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-06-06 12:45:15 -0400

    feat(starfish): update app breakdown chart to match mocks (#50307)

--- static/app/views/starfish/colours.tsx
+++ static/app/views/starfish/colours.tsx
@@ -1,6 +1,13 @@
 import {CHART_PALETTE} from 'sentry/constants/chartPalette';
+import {ModuleName} from 'sentry/views/starfish/types';
 
 export const THROUGHPUT_COLOR = CHART_PALETTE[0][0];
 export const P50_COLOR = CHART_PALETTE[2][2];
 export const P95_COLOR = CHART_PALETTE[2][1];
 export const ERRORS_COLOR = CHART_PALETTE[2][2];
+export const MODULE_COLOR: Record<ModuleName, string> = {
+  [ModuleName.HTTP]: CHART_PALETTE[3][3],
+  [ModuleName.DB]: CHART_PALETTE[3][2],
+  [ModuleName.NONE]: CHART_PALETTE[3][1],
+  [ModuleName.ALL]: CHART_PALETTE[3][0],
+};
