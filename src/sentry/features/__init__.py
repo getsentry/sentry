bd30d8503862a21b5d7cabbe0886ede39db8632e
David Wang <dwang@sentry.io>
2023-06-06 10:37:00 -0700

    feat(crons): Add feature flag for new listing page (#50327)
    will be used to determine if we show a new experimental listing page for
    crons

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -71,6 +71,7 @@ default_manager.add("organizations:auto-enable-codecov", OrganizationFeature, Fe
 default_manager.add("organizations:crash-rate-alerts", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:mute-alerts", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:crons-issue-platform", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
+default_manager.add("organizations:crons-timeline-listing-page", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:customer-domains", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:dashboards-mep", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:dashboards-rh-widget", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
