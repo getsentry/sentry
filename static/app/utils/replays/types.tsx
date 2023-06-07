e2afed199cbfa62078ad066a18915f8097d2dd7a
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-06-06 20:49:34 -0700

    Export all the same types as SDK

--- static/app/utils/replays/types.tsx
+++ static/app/utils/replays/types.tsx
@@ -1,4 +1,7 @@
-export {EventType, eventWithTime} from '@sentry-internal/rrweb';
+export {EventType} from '@sentry-internal/rrweb';
+export {NodeType} from '@sentry-internal/rrweb-snapshot';
+export type {eventWithTime, fullSnapshotEvent} from '@sentry-internal/rrweb';
+export type {serializedNodeWithId} from '@sentry-internal/rrweb-snapshot';
 
 export type {
   BreadcrumbFrame,
