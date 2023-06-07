320d867653f319cb4ad86bd89e9655bd20b9f17b
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-06-07 16:04:56 -0700

    test(replay): Use new mocks to test ReplayReader (#50271)

--- fixtures/js-stubs/replay.ts
+++ fixtures/js-stubs/replay.ts
@@ -1,9 +1,13 @@
+import * as Helpers from './replay/helpers';
 import * as BreadcrumbFrameData from './replay/replayBreadcrumbFrameData';
 import * as ReplayFrameEvents from './replay/replayFrameEvents';
 import * as ReplaySpanFrameData from './replay/replaySpanFrameData';
+import * as RRweb from './replay/rrweb';
 
 export const Replay = {
   ...BreadcrumbFrameData,
+  ...Helpers,
   ...ReplayFrameEvents,
   ...ReplaySpanFrameData,
+  ...RRweb,
 };
