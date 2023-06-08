e407b1cfe5e5125ed9c4fe89283c2b2e84769fac
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-06-08 16:03:23 -0400

    fix(starfish): query failing (#50586)

--- static/app/views/starfish/queries/useSpanList.tsx
+++ static/app/views/starfish/queries/useSpanList.tsx
@@ -49,7 +49,6 @@ export const useSpanList = (
     endTime,
     dateFilters,
     transaction,
-    orderBy,
     limit
   );
   const eventView = getEventView(
@@ -79,7 +78,6 @@ function getQuery(
   endTime: Moment,
   dateFilters: string,
   transaction?: string,
-  orderBy?: string,
   limit?: number
 ) {
   const conditions = buildQueryConditions(moduleName, location).filter(Boolean);
@@ -89,7 +87,7 @@ function getQuery(
     span_operation as "span.operation",
     description as "span.description",
     domain as "span.domain",
-    sum(exclusive_time) as "sum(span.duration)"
+    sum(exclusive_time) as "sum(span.duration)",
     quantile(0.95)(exclusive_time) as "p95(span.duration)",
     divide(count(), ${
       moment(endTime ?? undefined).unix() - moment(startTime).unix()
@@ -101,7 +99,7 @@ function getQuery(
     ${transaction ? `AND transaction = '${transaction}'` : ''}
     ${dateFilters}
     GROUP BY group_id, span_operation, domain, description
-    ORDER BY ${orderBy ?? 'count'} desc
+    ORDER BY count() desc
     ${limit ? `LIMIT ${limit}` : ''}`;
 }
 
