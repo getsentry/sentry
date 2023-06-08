15eec2e35cb8d1564b3312d2ac1e60f8c88cbc5a
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-06-08 13:08:11 -0400

    Load production span metric series

--- static/app/views/starfish/views/spanSummaryPage/sampleList/durationChart/index.tsx
+++ static/app/views/starfish/views/spanSummaryPage/sampleList/durationChart/index.tsx
@@ -1,16 +1,9 @@
 import {useTheme} from '@emotion/react';
-import moment from 'moment';
 
 import {Series} from 'sentry/types/echarts';
-import usePageFilters from 'sentry/utils/usePageFilters';
 import {P95_COLOR} from 'sentry/views/starfish/colours';
 import Chart from 'sentry/views/starfish/components/chart';
-import {PERIOD_REGEX} from 'sentry/views/starfish/utils/dates';
-import {queryDataToChartData} from 'sentry/views/starfish/utils/queryDataToChartData';
-import {
-  useQueryGetSpanSeriesData,
-  useQuerySpansInTransaction,
-} from 'sentry/views/starfish/views/spanSummaryPage/queries';
+import {useSpanMetricSeries} from 'sentry/views/starfish/queries/useSpanMetricsSeries';
 import {useQueryGetSpanTransactionSamples} from 'sentry/views/starfish/views/spanSummaryPage/sampleList/queries';
 
 type Props = {
@@ -19,24 +12,14 @@ type Props = {
   spanDescription?: string;
 };
 
-function DurationChart({groupId, transactionName, spanDescription}: Props) {
-  const pageFilter = usePageFilters();
-  const {isLoading, data} = useQuerySpansInTransaction({groupId});
+function DurationChart({groupId, transactionName}: Props) {
   const theme = useTheme();
 
-  const spanGroupOperation = data?.[0]?.span_operation;
-  const module = data?.[0]?.module;
-  const {startTime, endTime} = getStartAndEndTime(pageFilter);
-
-  const {isLoading: isLoadingSeriesData, data: seriesData} = useQueryGetSpanSeriesData({
-    groupId,
-    spanGroupOperation,
-    transactionName,
-    description: spanDescription,
-    module,
-  });
-
-  const {p95: p95Series} = queryDataToChartData(seriesData, startTime, endTime);
+  const {isLoading, data: spanMetricsSeriesData} = useSpanMetricSeries(
+    {group: groupId},
+    {transactionName},
+    'sidebar-span-metrics'
+  );
 
   const {data: sampleListData, isLoading: isSamplesLoading} =
     useQueryGetSpanTransactionSamples({
@@ -63,10 +46,10 @@ function DurationChart({groupId, transactionName, spanDescription}: Props) {
     <Chart
       statsPeriod="24h"
       height={140}
-      data={p95Series ? [p95Series] : []}
+      data={[spanMetricsSeriesData?.['p95(span.duration)']]}
       start=""
       end=""
-      loading={isLoading || isLoadingSeriesData}
+      loading={isLoading}
       scatterPlot={isSamplesLoading ? undefined : sampledSpanDataSeries}
       utc={false}
       chartColors={[P95_COLOR]}
@@ -75,15 +58,5 @@ function DurationChart({groupId, transactionName, spanDescription}: Props) {
     />
   );
 }
-
-const getStartAndEndTime = pageFilter => {
-  const [_, num, unit] = pageFilter.selection.datetime.period?.match(PERIOD_REGEX) ?? [];
-  const startTime =
-    num && unit
-      ? moment().subtract(num, unit as 'h' | 'd')
-      : moment(pageFilter.selection.datetime.start);
-  const endTime = moment(pageFilter.selection.datetime.end ?? undefined);
-  return {startTime, endTime};
-};
 
 export default DurationChart;
