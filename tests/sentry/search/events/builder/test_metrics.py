6d3817f232628e791c816f7aa9e0bd751786ca5b
William Mak <william@wmak.io>
2023-06-09 12:54:53 -0400

    fix(mep): Granularity was going to the wrong bucket size (#50642)
    - This fixes a bug where we'd pick the wrong granularity for
    statsPeriod=1h queries
    - What would happen is that the start and end would be 16:00:37 to
    17:00:37, which would mean we'd pick the 17:00 bucket, which wouldn't
    have any data yet.

--- tests/sentry/search/events/builder/test_metrics.py
+++ tests/sentry/search/events/builder/test_metrics.py
@@ -494,6 +494,16 @@ class MetricQueryBuilderTest(MetricBuilderBaseTest):
         end = datetime.datetime(2015, 5, 28, 1, 57, 0, tzinfo=timezone.utc)
         assert get_granularity(start, end) == 3600, "On the hour, long period"
 
+        # Hour to hour should only happen at the precise hour
+        start = datetime.datetime(2015, 5, 18, 10, 0, 0, tzinfo=timezone.utc)
+        end = datetime.datetime(2015, 5, 18, 18, 0, 0, tzinfo=timezone.utc)
+        assert get_granularity(start, end) == 3600, "precisely hour to hour"
+
+        # Even a few seconds means we need to switch back to minutes since the latter bucket may not be filled
+        start = datetime.datetime(2015, 5, 18, 10, 0, 1, tzinfo=timezone.utc)
+        end = datetime.datetime(2015, 5, 18, 18, 0, 1, tzinfo=timezone.utc)
+        assert get_granularity(start, end) == 60, "hour to hour but with seconds"
+
         # Even though this is >24h of data, because its a random hour in the middle of the day to the next we use minute
         # granularity
         start = datetime.datetime(2015, 5, 18, 10, 15, 1, tzinfo=timezone.utc)
