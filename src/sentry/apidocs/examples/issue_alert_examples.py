367de1a0f49e60a83a8175116eb687fee444e801
schew2381 <seiji.chew@sentry.io>
2023-06-09 12:09:49 -0700

    ref(api): Create issue alert examples

---
+++ src/sentry/apidocs/examples/issue_alert_examples.py
@@ -0,0 +1,11 @@
+from drf_spectacular.utils import OpenApiExample
+
+
+class IssueAlertExamples:
+    GENERIC_SUCCESS_RESPONSE = [
+        OpenApiExample(
+            "Successful response",
+            value={},
+            status_codes=["200"],
+        )
+    ]
