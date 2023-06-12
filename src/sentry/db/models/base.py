8e08986c0b65791aec43837b2dfdd8a6b56b6b89
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-06-12 10:07:40 -0400

    ref: make typing on `sane_repr` more generic (#50730)
    <!-- Describe your PR here. -->

--- src/sentry/db/models/base.py
+++ src/sentry/db/models/base.py
@@ -25,11 +25,11 @@ __all__ = (
 )
 
 
-def sane_repr(*attrs: str) -> Callable[[models.Model], str]:
+def sane_repr(*attrs: str) -> Callable[[object], str]:
     if "id" not in attrs and "pk" not in attrs:
         attrs = ("id",) + attrs
 
-    def _repr(self: models.Model) -> str:
+    def _repr(self: object) -> str:
         cls = type(self).__name__
 
         pairs = (f"{a}={getattr(self, a, None)!r}" for a in attrs)
