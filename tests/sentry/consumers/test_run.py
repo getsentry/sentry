29a05d3edb7151b56698c9638048d4ea727e1841
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-06-12 16:23:07 +0200

    add newstyle consumers for everything but ppf and indexer

--- tests/sentry/consumers/test_run.py
+++ tests/sentry/consumers/test_run.py
@@ -6,10 +6,13 @@ from sentry.utils.imports import import_string
 
 
 @pytest.mark.parametrize("consumer_def", list(consumers.KAFKA_CONSUMERS.items()))
-def test_all_importable(consumer_def):
+def test_all_importable(consumer_def, settings):
     name: str
     defn: consumers.ConsumerDefinition
     name, defn = consumer_def
 
     factory = import_string(defn["strategy_factory"])
     assert issubclass(factory, ProcessingStrategyFactory)
+
+    topic = defn["topic"]
+    assert topic is None or topic in settings.KAFKA_TOPICS
