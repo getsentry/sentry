624f6b914a98e254e77f6668f55398ba9178c98f
nathan hsieh <hsieh.nathan@gmail.com>
2023-06-12 10:39:43 -0700

    duplicate oauth2 code for fly

---
+++ src/sentry/auth/providers/fly/constants.py
@@ -0,0 +1,13 @@
+AUTHORIZE_URL = "https://api.fly.io/oauth/authorize"
+
+ACCESS_TOKEN_URL = "https://api.fly.io/oauth/token"
+
+ERR_INVALID_DOMAIN = (
+    "The domain for your Fly account (%s) is not allowed to authenticate with this provider."
+)
+
+ERR_INVALID_RESPONSE = "Unable to fetch user information from Fly.  Please check the log."
+
+SCOPE = "read"
+
+DATA_VERSION = "1"
--- src/sentry/auth/providers/fly/provider.py
+++ src/sentry/auth/providers/fly/provider.py
@@ -1,15 +1,95 @@
-from sentry.auth.partnership_config import SPONSOR_OAUTH_NAME, ChannelName
-from sentry.auth.providers.oauth2 import OAuth2Provider
+from sentry import options
+from sentry.auth.provider import MigratingIdentityId
+from sentry.auth.providers.oauth2 import OAuth2Callback, OAuth2Login, OAuth2Provider
+
+from .constants import ACCESS_TOKEN_URL, AUTHORIZE_URL, DATA_VERSION, SCOPE
+from .views import FetchUser, FlyConfigureView
+
+
+class FlyOAuth2Login(OAuth2Login):
+    # GET https://api.fly.io/oauth/authorize?
+    #   client_id=123&
+    #   response_type=code&
+    #   redirect_uri=https://logjam.io/flyio/callback&
+    #   scope=read
+    authorize_url = AUTHORIZE_URL
+    scope = SCOPE
+
+    def __init__(self, client_id, domains=None):
+        self.domains = domains
+        super().__init__(client_id=client_id)
+
+    def get_authorize_params(self, state, redirect_uri):
+        params = super().get_authorize_params(state, redirect_uri)
+        # TODO(dcramer): ideally we could look at the current resulting state
+        # when an existing auth happens, and if they're missing a refresh_token
+        # we should re-prompt them a second time with ``approval_prompt=force``
+        params["approval_prompt"] = "force"
+        params["access_type"] = "offline"
+        return params
 
 
 class FlyOAuth2Provider(OAuth2Provider):
+    name = "Fly"
 
-    name = SPONSOR_OAUTH_NAME[ChannelName.FLY_IO]
-    is_partner = True
+    def __init__(self, domain=None, domains=None, version=None, **config):
+        if domain:
+            if domains:
+                domains.append(domain)
+            else:
+                domains = [domain]
+        self.domains = domains
+        # if a domain is not configured this is part of the setup pipeline
+        # this is a bit complex in Sentry's SSO implementation as we don't
+        # provide a great way to get initial state for new setup pipelines
+        # vs missing state in case of migrations.
+        if domains is None:
+            version = DATA_VERSION
+        else:
+            version = None
+        self.version = version
+        super().__init__(**config)
 
-    # TODO: (nhsiehgit) Replace boilerplate code
-    def get_refresh_token_url(self) -> str:
-        return ""
+    def get_client_id(self):
+        return options.get("auth-fly.client-id")
+
+    def get_client_secret(self):
+        return options.get("auth-fly.client-secret")
+
+    def get_configure_view(self):
+        return FlyConfigureView.as_view()
+
+    def get_auth_pipeline(self):
+        return [
+            FlyOAuth2Login(domains=self.domains, client_id=self.get_client_id()),
+            OAuth2Callback(
+                access_token_url=ACCESS_TOKEN_URL,
+                client_id=self.get_client_id(),
+                client_secret=self.get_client_secret(),
+            ),
+            FetchUser(domains=self.domains, version=self.version),
+        ]
+
+    def get_refresh_token_url(self):
+        return ACCESS_TOKEN_URL
+
+    def build_config(self, state):
+        return {"domains": [state["domain"]], "version": DATA_VERSION}
 
     def build_identity(self, state):
-        return {"id": "", "email": ""}
+        # TODO: Discuss with Fly on what this looks like
+        data = state["data"]
+        user_data = state["user"]
+
+        # XXX(epurkhiser): We initially were using the email as the id key.
+        # This caused account dupes on domain changes. Migrate to the
+        # account-unique sub key.
+        user_id = MigratingIdentityId(id=user_data["sub"], legacy_id=user_data["email"])
+
+        return {
+            "id": user_id,
+            "email": user_data["email"],
+            "name": user_data["email"],
+            "data": self.get_oauth_data(data),
+            "email_verified": user_data["email_verified"],
+        }
