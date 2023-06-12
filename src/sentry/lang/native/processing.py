9440c6a301cdb30871a27718afd48019de14870d
bitsandfoxes <reg@bitfox.at>
2023-06-12 11:51:04 +0200

    don't overwrite a previously set level

--- src/sentry/lang/native/processing.py
+++ src/sentry/lang/native/processing.py
@@ -195,7 +195,8 @@ def _merge_system_info(data, system_info):
 
 def _merge_full_response(data, response):
     data["platform"] = "native"
-    if response.get("crashed") is not None:
+    # Do not overwrite the event's level if it has been set previously
+    if response.get("crashed") is not None and data.get("level") is None:
         data["level"] = "fatal" if response["crashed"] else "info"
 
     if response.get("system_info"):
