4105142e4a872795ff14bea0bff4f1d5efee44e3
Athena Moghaddam <athena.moghaddam@sentry.io>
2023-06-13 15:00:07 -0700

    feat(fly-auth): implement abstract methods

--- src/sentry/auth/partnership_config.py
+++ src/sentry/auth/partnership_config.py
@@ -5,18 +5,4 @@ class ChannelName(Enum):
     FLY_IO = "fly.io"
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
 SPONSOR_OAUTH_NAME = {ChannelName.FLY_IO: "Fly.io"}
-=======
-SPONSORSHIP_TO_CHANNEL_MAP = {
-    4: ChannelName.FLY_IO
-}
-
-SPONSOR_OAUTH_NAME = {
-    ChannelName.FLY_IO: "Fly IO"
-}
->>>>>>> aef2fff949 ([WIP]feat(fly-auth): remove fly auth from list)
-=======
-SPONSOR_OAUTH_NAME = {ChannelName.FLY_IO: "Fly IO"}
->>>>>>> dfedb6f768 (feat(fly-auth): remove check for partner orgs)
