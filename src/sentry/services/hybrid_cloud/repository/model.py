c6f13fc8c7a53b628ed51fa2d79c70572ac82cc6
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-06-13 11:32:19 -0700

    Have RpcRepository match nullable external_id

--- src/sentry/services/hybrid_cloud/repository/model.py
+++ src/sentry/services/hybrid_cloud/repository/model.py
@@ -3,6 +3,8 @@
 # in modules such as this one where hybrid cloud data models or service classes are
 # defined, because we want to reflect on type annotations and avoid forward references.
 
+from typing import Optional
+
 from sentry.services.hybrid_cloud import RpcModel
 
 
@@ -10,4 +12,4 @@ class RpcRepository(RpcModel):
     id: int
     organization_id: int
     name: str
-    external_id: str
+    external_id: Optional[str]
