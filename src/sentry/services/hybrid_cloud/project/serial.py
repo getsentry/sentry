5cc1ce460915f4708450fb364f944eb408e86722
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-06-13 09:56:58 -0700

    Fix type-checking problem in serialize_project

--- src/sentry/services/hybrid_cloud/project/serial.py
+++ src/sentry/services/hybrid_cloud/project/serial.py
@@ -7,7 +7,7 @@ from sentry.services.hybrid_cloud.project import RpcProject
 def serialize_project(project: Project) -> RpcProject:
     return RpcProject(
         id=project.id,
-        slug=project.slug,
+        slug=project.slug or "",
         name=project.name,
         organization_id=project.organization_id,
         status=project.status,
