589670b6d9391754b53ac0f442f6b275ef116c83
Athena Moghaddam <athena.moghaddam@sentry.io>
2023-06-14 11:21:56 -0700

    feat(organization): add migration test

--- tests/sentry/migrations/test_0487_add_is_test_field_to_org.py
+++ tests/sentry/migrations/test_0487_add_is_test_field_to_org.py
@@ -1,3 +1,4 @@
+from sentry.models.organization import Organization
 from sentry.testutils.cases import TestMigrations
 
 
@@ -5,12 +6,11 @@ class AddIsTestFieldToOrgMigrationTest(TestMigrations):
     migrate_from = "0486_integer_pr_comment_issue_list"
     migrate_to = "0487_add_is_test_field_to_org"
 
-    def setup_before_migration(self, apps):
-        Organization = apps.get_model("sentry", "Organization")
-        self.organization_with_defalult = Organization.objects.create(slug="my_organization")
+    def test_default(self):
+        organization_with_defalult = Organization.objects.create(slug="my-organization")
+        assert organization_with_defalult.slug == "my-organization"
+        assert organization_with_defalult.is_test is False
 
-    def test(self):
-        # Test state after migration
-        self.organization_with_defalult.refresh_from_db()
-        assert self.organization_with_defalult.slug == "my_organization"
-        assert self.organization_with_defalult.is_test is False
+    def test_test_org(self):
+        test_organization = Organization.objects.create(slug="test-org", is_test=True)
+        assert test_organization.is_test is True
