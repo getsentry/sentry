99689dcf277fc2cbaeedd77c1cf6f6607141c2d9
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-06-14 13:35:15 -0700

    migration(crons): Add dangerous migration to index timeout for check-ins (#50597)
    Indexes `timeout_at` for more efficient querying for `in_progress`
    check-ins.

--- src/sentry/monitors/models.py
+++ src/sentry/monitors/models.py
@@ -399,6 +399,7 @@ class MonitorCheckIn(Model):
         db_table = "sentry_monitorcheckin"
         indexes = [
             models.Index(fields=["monitor", "date_added", "status"]),
+            models.Index(fields=["timeout_at", "status"]),
         ]
 
     __repr__ = sane_repr("guid", "project_id", "status")
