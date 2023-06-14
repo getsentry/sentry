f84ba42f56eefaee960791bc892d79045c4ef0a5
Athena Moghaddam <athena.moghaddam@sentry.io>
2023-06-14 11:38:38 -0700

    feat(organization): resolve conflict

--- tests/sentry/migrations/test_0489_add_is_test_to_org.py
+++ tests/sentry/migrations/test_0489_add_is_test_to_org.py
@@ -3,8 +3,8 @@ from sentry.testutils.cases import TestMigrations
 
 
 class AddIsTestFieldToOrgMigrationTest(TestMigrations):
-    migrate_from = "0486_integer_pr_comment_issue_list"
-    migrate_to = "0487_add_is_test_field_to_org"
+    migrate_from = "0488_add_orgauthtoken"
+    migrate_to = "0489_add_is_test_to_org"
 
     def test_default(self):
         organization_with_defalult = Organization.objects.create(slug="my-organization")
