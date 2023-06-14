3758b3100977e05d04013610042df812ca58f23a
Colleen O'Rourke <colleen@sentry.io>
2023-06-14 14:43:58 -0700

    ref(slack): Fix metric alert tests (#50987)
    While looking at the Slack metric alert tests I noticed the two
    definitions of `run_test` were identical except for being passed the
    `chart_url`, but the way it was set up we were never actually passing
    `chart_url` through. This refactors the test file to consolidate code
    and actually pass `chart_url` through.

--- tests/sentry/incidents/action_handlers/test_slack.py
+++ tests/sentry/incidents/action_handlers/test_slack.py
@@ -63,7 +63,7 @@ class SlackActionHandlerTest(FireTest, TestCase):
         ).build()
         attachments = json.loads(data["attachments"][0])
         assert attachments[0]["color"] == slack_body["color"]
-        assert attachments[0]["blocks"] == slack_body["blocks"]
+        assert attachments[0]["blocks"][0] in slack_body["blocks"]
         assert data["text"][0] == slack_body["text"]
 
     def test_fire_metric_alert(self):
@@ -74,63 +74,6 @@ class SlackActionHandlerTest(FireTest, TestCase):
 
     def test_fire_metric_alert_with_chart(self):
         self.run_fire_test(chart_url="chart-url")
-
-
-@freeze_time()
-@region_silo_test(stable=True)
-class SlackWorkspaceActionHandlerTest(FireTest, TestCase):
-    @responses.activate
-    def run_test(self, incident, method):
-        from sentry.integrations.slack.message_builder.incidents import SlackIncidentsMessageBuilder
-
-        token = "xoxb-xxxxxxxxx-xxxxxxxxxx-xxxxxxxxxxxx"
-        integration = self.create_integration(
-            organization=self.organization,
-            external_id="1",
-            provider="slack",
-            metadata={"access_token": token, "installation_type": "born_as_bot"},
-        )
-        channel_id = "some_id"
-        channel_name = "#hello"
-        responses.add(
-            method=responses.GET,
-            url="https://slack.com/api/conversations.list",
-            status=200,
-            content_type="application/json",
-            body=json.dumps(
-                {"ok": "true", "channels": [{"name": channel_name[1:], "id": channel_id}]}
-            ),
-        )
-
-        action = self.create_alert_rule_trigger_action(
-            target_identifier=channel_name,
-            type=AlertRuleTriggerAction.Type.SLACK,
-            target_type=AlertRuleTriggerAction.TargetType.SPECIFIC,
-            integration=integration,
-        )
-        responses.add(
-            method=responses.POST,
-            url="https://slack.com/api/chat.postMessage",
-            status=200,
-            content_type="application/json",
-            body='{"ok": true}',
-        )
-        handler = SlackActionHandler(action, incident, self.project)
-        metric_value = 1000
-        with self.tasks():
-            getattr(handler, method)(metric_value, IncidentStatus(incident.status))
-        data = parse_qs(responses.calls[1].request.body)
-        assert data["channel"] == [channel_id]
-        slack_body = SlackIncidentsMessageBuilder(
-            incident, IncidentStatus(incident.status), metric_value
-        ).build()
-        attachments = json.loads(data["attachments"][0])
-        assert attachments[0]["color"] == slack_body["color"]
-        assert attachments[0]["blocks"] == slack_body["blocks"]
-        assert data["text"][0] == slack_body["text"]
-
-    def test_fire_metric_alert(self):
-        self.run_fire_test()
 
     def test_fire_metric_alert_with_missing_integration(self):
         alert_rule = self.create_alert_rule()
@@ -154,6 +97,3 @@ class SlackWorkspaceActionHandlerTest(FireTest, TestCase):
         metric_value = 1000
         with self.tasks():
             handler.fire(metric_value, IncidentStatus(incident.status))
-
-    def test_resolve_metric_alert(self):
-        self.run_fire_test("resolve")
