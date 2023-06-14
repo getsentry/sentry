5b3fcbab61880d1e56bf062138a953fc4f1337c8
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-06-14 13:46:43 -0700

    🔊 Better logs and use full path for signature

--- src/sentry/api/endpoints/internal/integration_proxy.py
+++ src/sentry/api/endpoints/internal/integration_proxy.py
@@ -45,11 +45,10 @@ class InternalIntegrationProxyEndpoint(Endpoint):
         """
         Returns True if the sender is deemed sufficiently trustworthy.
         """
-        log_extra = {"path": request.path, "host": request.headers.get("Host")}
         signature = request.headers.get(PROXY_SIGNATURE_HEADER)
         identifier = request.headers.get(PROXY_OI_HEADER)
         if signature is None or identifier is None:
-            logger.error("invalid_sender_headers", extra=log_extra)
+            logger.error("invalid_sender_headers", extra=self.log_extra)
             return False
         is_valid = verify_subnet_signature(
             request_body=request.body,
@@ -58,7 +57,7 @@ class InternalIntegrationProxyEndpoint(Endpoint):
             provided_signature=signature,
         )
         if not is_valid:
-            logger.error("invalid_sender_signature", extra=log_extra)
+            logger.error("invalid_sender_signature", extra=self.log_extra)
 
         return is_valid
 
@@ -68,14 +67,12 @@ class InternalIntegrationProxyEndpoint(Endpoint):
         """
         from sentry.shared_integrations.client.proxy import IntegrationProxyClient
 
-        log_extra = {"path": self.proxy_path}
-
         # Get the organization integration
         org_integration_id = request.headers.get(PROXY_OI_HEADER)
         if org_integration_id is None:
-            logger.error("missing_org_integration", extra=log_extra)
+            logger.error("missing_org_integration", extra=self.log_extra)
             return False
-        log_extra["org_integration_id"] = org_integration_id
+        self.log_extra["org_integration_id"] = org_integration_id
 
         self.org_integration = (
             OrganizationIntegration.objects.filter(
@@ -86,14 +83,14 @@ class InternalIntegrationProxyEndpoint(Endpoint):
             .first()
         )
         if self.org_integration is None:
-            logger.error("invalid_org_integration", extra=log_extra)
+            logger.error("invalid_org_integration", extra=self.log_extra)
             return False
-        log_extra["integration_id"] = self.org_integration.integration_id
+        self.log_extra["integration_id"] = self.org_integration.integration_id
 
         # Get the integration
         self.integration = self.org_integration.integration
         if not self.integration or self.integration.status is not ObjectStatus.ACTIVE:
-            logger.error("invalid_integration", extra=log_extra)
+            logger.error("invalid_integration", extra=self.log_extra)
             return False
 
         # Get the integration client
@@ -102,9 +99,9 @@ class InternalIntegrationProxyEndpoint(Endpoint):
         )
         self.client: IntegrationProxyClient = installation.get_client()
         client_type = type(self.client)
-        log_extra["client_type"] = client_type
+        self.log_extra["client_type"] = client_type
         if not issubclass(client_type, IntegrationProxyClient):
-            logger.error("invalid_client", extra=log_extra)
+            logger.error("invalid_client", extra=self.log_extra)
             return False
 
         return True
@@ -133,11 +130,17 @@ class InternalIntegrationProxyEndpoint(Endpoint):
         """
         Catch-all workaround instead of explicitly setting handlers for each method (GET, POST, etc.)
         """
-        self.proxy_path = trim_leading_slashes(request.path[len(PROXY_BASE_PATH) :])
+        self.proxy_path = trim_leading_slashes(request.get_full_path()[len(PROXY_BASE_PATH) :])
+        self.log_extra = {
+            "method": request.method,
+            "path": self.proxy_path,
+            "host": request.headers.get("Host"),
+        }
         if not self._should_operate(request):
             raise Http404
 
         full_url = f"{self.client.base_url}/{self.proxy_path}"
+        self.log_extra["full_url"] = full_url
         headers = clean_outbound_headers(request.headers)
 
         prepared_request = Request(
@@ -167,4 +170,5 @@ class InternalIntegrationProxyEndpoint(Endpoint):
         for header, value in valid_headers.items():
             response[header] = value
         metrics.incr("hc.integration_proxy.success")
+        logger.info("proxy_success", extra=self.log_extra)
         return response
