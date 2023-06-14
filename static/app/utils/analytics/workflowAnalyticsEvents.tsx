5c3680e6b7b41727b85592f18cca83f2844238fe
Roman Zavarnitsyn <rom4ek93@gmail.com>
2023-06-14 15:21:30 +0200

    feat(analytics): Track symbolicated and events with source context (#50139)

--- static/app/utils/analytics/workflowAnalyticsEvents.tsx
+++ static/app/utils/analytics/workflowAnalyticsEvents.tsx
@@ -22,8 +22,10 @@ export type BaseEventAnalyticsParams = {
   has_commit: boolean;
   has_exception_group: boolean;
   has_release: boolean;
+  has_source_context: boolean;
   has_source_maps: boolean;
   has_trace: boolean;
+  is_symbolicated: boolean;
   num_commits: number;
   num_in_app_stack_frames: number;
   num_stack_frames: number;
