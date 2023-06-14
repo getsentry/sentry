ee69fc04e0e969c63c6cf730b53c261c615ce651
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-06-14 12:39:15 -0700

    update migration

--- src/sentry/migrations/0489_index_checkin_timeout.py
+++ src/sentry/migrations/0489_index_checkin_timeout.py
@@ -19,7 +19,7 @@ class Migration(CheckedMigration):
     is_dangerous = True
 
     dependencies = [
-        ("sentry", "0483_backfill_organization_member_user_email"),
+        ("sentry", "0488_add_orgauthtoken"),
     ]
 
     operations = [
