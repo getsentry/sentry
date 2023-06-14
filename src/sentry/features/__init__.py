24bdfb13e1b2eefb444077fdbd7a943518fbcf71
Stephen Cefali <scefali@sentry.io>
2023-06-14 09:40:14 -0700

    feat(Slack): escape slack messages for everyone (#50433)
    Now that the feature flag has been active on Sentry for a while, I think
    we are OK with pushing this change to everyone

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -177,7 +177,6 @@ default_manager.add("organizations:session-replay-sdk-errors-only", Organization
 default_manager.add("organizations:session-replay-recording-scrubbing", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay-index-subquery", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:set-grouping-config", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
-default_manager.add("organizations:slack-escape-messages", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:slack-overage-notifications", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:sdk-crash-detection", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:starfish-view", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
