c73091159ce0318767547646fac5348d92135db9
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-06-15 13:08:19 +0200

    feat(project-creation): Make product selection feature available to everyone - Part 2 (#50806)

--- static/app/components/onboarding/productSelection.tsx
+++ static/app/components/onboarding/productSelection.tsx
@@ -1,5 +1,4 @@
 import {Fragment, useCallback, useEffect} from 'react';
-import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import {Alert} from 'sentry/components/alert';
@@ -19,17 +18,51 @@ export enum PRODUCT {
   SESSION_REPLAY = 'session-replay',
 }
 
-type Props = {
+export type DisabledProduct = {
+  product: PRODUCT;
+  reason: string;
+};
+
+type ProductProps = {
+  checked: boolean;
+  disabled: boolean;
+  label: string;
+  onClick?: () => void;
+  permanentDisabled?: boolean;
+};
+
+function Product({disabled, permanentDisabled, checked, label, onClick}: ProductProps) {
+  return (
+    <ProductWrapper
+      permanentDisabled={permanentDisabled}
+      onClick={disabled ? undefined : onClick}
+      disabled={disabled}
+    >
+      <Checkbox
+        checked={checked}
+        disabled={permanentDisabled ? false : disabled}
+        aria-label={label}
+        size="xs"
+        readOnly
+      />
+      <span>{label}</span>
+    </ProductWrapper>
+  );
+}
+
+type ProductSelectionProps = {
   defaultSelectedProducts?: PRODUCT[];
+  disabledProducts?: DisabledProduct[];
   lazyLoader?: boolean;
   skipLazyLoader?: () => void;
 };
 
 export function ProductSelection({
   defaultSelectedProducts,
+  disabledProducts,
   lazyLoader,
   skipLazyLoader,
-}: Props) {
+}: ProductSelectionProps) {
   const router = useRouter();
   const products = decodeList(router.location.query.product);
 
@@ -62,6 +95,15 @@ export function ProductSelection({
     },
     [router, products]
   );
+
+  const performanceProductDisabled = disabledProducts?.find(
+    disabledProduct => disabledProduct.product === PRODUCT.PERFORMANCE_MONITORING
+  );
+
+  const sessionReplayProductDisabled = disabledProducts?.find(
+    disabledProduct => disabledProduct.product === PRODUCT.SESSION_REPLAY
+  );
+
   return (
     <Fragment>
       <TextBlock>
@@ -76,63 +118,51 @@ export function ProductSelection({
       </TextBlock>
       <Products>
         <Tooltip title={t("Let's admit it, we all have errors.")}>
-          <Product
-            disabled
-            data-test-id={`product-${PRODUCT.ERROR_MONITORING}-${PRODUCT.PERFORMANCE_MONITORING}-${PRODUCT.SESSION_REPLAY}`}
-          >
-            <Checkbox checked readOnly size="xs" disabled />
-            <div>{t('Error Monitoring')}</div>
-          </Product>
+          <Product disabled checked permanentDisabled label={t('Error Monitoring')} />
         </Tooltip>
         <Tooltip
           title={
-            <TooltipDescription>
-              {t(
-                'Automatic performance issue detection with context like who it impacts and the release, line of code, or function causing the slowdown.'
-              )}
-              <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/react/performance/">
-                {t('Read the Docs')}
-              </ExternalLink>
-            </TooltipDescription>
+            performanceProductDisabled?.reason ?? (
+              <TooltipDescription>
+                {t(
+                  'Automatic performance issue detection with context like who it impacts and the release, line of code, or function causing the slowdown.'
+                )}
+                <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/react/performance/">
+                  {t('Read the Docs')}
+                </ExternalLink>
+              </TooltipDescription>
+            )
           }
           isHoverable
         >
           <Product
             onClick={() => handleClickProduct(PRODUCT.PERFORMANCE_MONITORING)}
-            data-test-id={`product-${PRODUCT.PERFORMANCE_MONITORING}`}
-          >
-            <Checkbox
-              checked={products.includes(PRODUCT.PERFORMANCE_MONITORING)}
-              size="xs"
-              readOnly
-            />
-            {t('Performance Monitoring')}
-          </Product>
+            disabled={!!performanceProductDisabled}
+            checked={products.includes(PRODUCT.PERFORMANCE_MONITORING)}
+            label={t('Performance Monitoring')}
+          />
         </Tooltip>
         <Tooltip
           title={
-            <TooltipDescription>
-              {t(
-                'Video-like reproductions of user sessions with debugging context to help you confirm issue impact and troubleshoot faster.'
-              )}
-              <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/react/session-replay/">
-                {t('Read the Docs')}
-              </ExternalLink>
-            </TooltipDescription>
+            sessionReplayProductDisabled?.reason ?? (
+              <TooltipDescription>
+                {t(
+                  'Video-like reproductions of user sessions with debugging context to help you confirm issue impact and troubleshoot faster.'
+                )}
+                <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/react/session-replay/">
+                  {t('Read the Docs')}
+                </ExternalLink>
+              </TooltipDescription>
+            )
           }
           isHoverable
         >
           <Product
             onClick={() => handleClickProduct(PRODUCT.SESSION_REPLAY)}
-            data-test-id={`product-${PRODUCT.SESSION_REPLAY}`}
-          >
-            <Checkbox
-              checked={products.includes(PRODUCT.SESSION_REPLAY)}
-              size="xs"
-              readOnly
-            />
-            {t('Session Replay')}
-          </Product>
+            disabled={!!sessionReplayProductDisabled}
+            checked={products.includes(PRODUCT.SESSION_REPLAY)}
+            label={t('Session Replay')}
+          />
         </Tooltip>
       </Products>
       {lazyLoader && (
@@ -159,23 +189,33 @@ const Products = styled('div')`
   gap: ${space(1)};
 `;
 
-const Product = styled('div')<{disabled?: boolean}>`
+const ProductWrapper = styled('div')<{disabled?: boolean; permanentDisabled?: boolean}>`
   display: grid;
   grid-template-columns: repeat(3, max-content);
   gap: ${space(1)};
   align-items: center;
   ${p => p.theme.buttonPadding.xs};
-  background: ${p => p.theme.purple100};
-  border: 1px solid ${p => p.theme.purple300};
+  background: ${p =>
+    p.disabled && !p.permanentDisabled ? p.theme.background : p.theme.purple100};
+  border: 1px solid
+    ${p =>
+      p.disabled && !p.permanentDisabled ? p.theme.disabledBorder : p.theme.purple300};
   border-radius: 6px;
-  cursor: pointer;
-  ${p =>
-    p.disabled &&
-    css`
-      > *:not(:last-child) {
-        opacity: 0.5;
-      }
-    `};
+  cursor: ${p => (p.disabled ? 'not-allowed' : 'pointer')};
+  font-weight: 500;
+  color: ${p =>
+    p.disabled && !p.permanentDisabled ? p.theme.textColor : p.theme.purple300};
+  input {
+    cursor: ${p => (p.disabled ? 'not-allowed' : 'pointer')};
+  }
+
+  > *:first-child {
+    opacity: ${p => (p.permanentDisabled ? 0.5 : 1)};
+  }
+
+  > *:last-child {
+    opacity: ${p => (p.disabled ? 0.5 : 1)};
+  }
 `;
 
 const Divider = styled('hr')`
