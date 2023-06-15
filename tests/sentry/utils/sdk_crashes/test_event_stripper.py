95086b406dec79e6bcef45f299a3e92f727da2c0
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-06-15 09:22:41 +0200

    feat(sdk-crash-detection): Add event stripper
    Add class event stripper, which only keeps event data relevant for
    the SDK crash.

---
+++ tests/sentry/utils/sdk_crashes/test_event_stripper.py
@@ -0,0 +1,186 @@
+import abc
+from unittest.mock import Mock
+
+from fixtures.sdk_crash_detection.crash_event import (
+    IN_APP_FRAME,
+    get_crash_event,
+    get_crash_event_with_frames,
+    get_frames,
+)
+from sentry.testutils import TestCase
+from sentry.testutils.cases import BaseTestCase
+from sentry.testutils.silo import region_silo_test
+from sentry.utils.safe import get_path
+from sentry.utils.sdk_crashes.cocoa_sdk_crash_detector import CocoaSDKCrashDetector
+from sentry.utils.sdk_crashes.event_stripper import strip_event_data
+
+
+class BaseEventStripperMixin(BaseTestCase, metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def create_event(self, data, project_id, assert_no_errors=True):
+        pass
+
+    def execute_test(self):
+        pass
+
+
+class EventStripperTestMixin(BaseEventStripperMixin):
+    def test_strip_event_data_keeps_allowed_keys(self):
+        event = self.create_event(
+            data=get_crash_event(),
+            project_id=self.project.id,
+        )
+
+        stripped_event_data = strip_event_data(event.data, CocoaSDKCrashDetector())
+
+        keys_removed = {"tags", "user", "threads", "breadcrumbs", "environment"}
+        for key in keys_removed:
+            assert stripped_event_data.get(key) is None, f"key {key} should be removed"
+
+        keys_kept = {
+            "type",
+            "timestamp",
+            "platform",
+            "sdk",
+            "exception",
+            "debug_meta",
+            "contexts",
+        }
+
+        for key in keys_kept:
+            assert stripped_event_data.get(key) is not None, f"key {key} should be kept"
+
+    def test_strip_event_data_without_debug_meta(self):
+        event_data = get_crash_event()
+        debug_meta = dict(get_path(event_data, "debug_meta"))
+        debug_meta.pop("images")
+        event_data["debug_meta"] = debug_meta
+
+        event = self.create_event(
+            data=event_data,
+            project_id=self.project.id,
+        )
+
+        stripped_event_data = strip_event_data(event.data, CocoaSDKCrashDetector())
+
+        debug_meta_images = get_path(stripped_event_data, "debug_meta", "images")
+        assert debug_meta_images is None
+
+    def test_strip_event_data_strips_context(self):
+        event = self.create_event(
+            data=get_crash_event(),
+            project_id=self.project.id,
+        )
+
+        stripped_event_data = strip_event_data(event.data, CocoaSDKCrashDetector())
+
+        assert stripped_event_data.get("contexts") == {
+            "os": {
+                "name": "iOS",
+                "version": "16.3",
+                "build": "20D47",
+            },
+            "device": {
+                "family": "iOS",
+                "model": "iPhone14,8",
+                "arch": "arm64e",
+            },
+        }
+
+    def test_strip_event_data_strips_sdk(self):
+        event = self.create_event(
+            data=get_crash_event(),
+            project_id=self.project.id,
+        )
+
+        stripped_event_data = strip_event_data(event.data, CocoaSDKCrashDetector())
+
+        assert stripped_event_data.get("sdk") == {
+            "name": "sentry.cocoa",
+            "version": "8.1.0",
+        }
+
+    def test_strip_event_data_strips_value_if_not_simple_type(self):
+        event = self.create_event(
+            data=get_crash_event(),
+            project_id=self.project.id,
+        )
+        event.data["type"] = {"foo": "bar"}
+
+        stripped_event_data = strip_event_data(event.data, CocoaSDKCrashDetector())
+
+        assert stripped_event_data.get("type") is None
+
+    def test_strip_event_data_keeps_simple_types(self):
+        event = self.create_event(
+            data=get_crash_event(),
+            project_id=self.project.id,
+        )
+        event.data["type"] = True
+        event.data["datetime"] = 0.1
+        event.data["timestamp"] = 1
+        event.data["platform"] = "cocoa"
+
+        stripped_event_data = strip_event_data(event.data, CocoaSDKCrashDetector())
+
+        assert stripped_event_data.get("type") is True
+        assert stripped_event_data.get("datetime") == 0.1
+        assert stripped_event_data.get("timestamp") == 1
+        assert stripped_event_data.get("platform") == "cocoa"
+
+    def test_strip_event_data_strips_non_referenced_dsyms(self):
+        event = self.create_event(
+            data=get_crash_event(),
+            project_id=self.project.id,
+        )
+
+        stripped_event_data = strip_event_data(event.data, Mock())
+
+        debug_meta_images = get_path(stripped_event_data, "debug_meta", "images")
+
+        image_addresses = set(map(lambda image: image["image_addr"], debug_meta_images))
+        expected_image_addresses = {"0x1a4e8f000", "0x100304000", "0x100260000"}
+        assert image_addresses == expected_image_addresses
+
+    def test_strip_frames_sentry_in_app_frame_kept(self):
+        frames = get_frames("SentryCrashMonitor_CPPException.cpp", sentry_frame_in_app=True)
+        self._execute_strip_frames_test(frames)
+
+    def test_strip_frames_sentry_non_in_app_frame_kept(self):
+        frames = get_frames("SentryCrashMonitor_CPPException.cpp", sentry_frame_in_app=False)
+        self._execute_strip_frames_test(frames)
+
+    def _execute_strip_frames_test(self, frames):
+        event_data = get_crash_event_with_frames(frames)
+
+        event = self.create_event(
+            data=event_data,
+            project_id=self.project.id,
+        )
+
+        stripped_event_data = strip_event_data(event.data, CocoaSDKCrashDetector())
+
+        stripped_frames = get_path(
+            stripped_event_data, "exception", "values", -1, "stacktrace", "frames"
+        )
+
+        assert len(stripped_frames) == 6
+        assert (
+            len(
+                [
+                    frame
+                    for frame in stripped_frames
+                    if frame["function"] == IN_APP_FRAME["function"]
+                ]
+            )
+            == 0
+        ), "in_app frame should be removed"
+
+
+@region_silo_test
+class EventStripperTest(
+    TestCase,
+    EventStripperTestMixin,
+):
+    def create_event(self, data, project_id, assert_no_errors=True):
+        return self.store_event(data=data, project_id=project_id, assert_no_errors=assert_no_errors)
