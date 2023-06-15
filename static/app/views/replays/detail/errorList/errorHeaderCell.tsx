fd43c5cfb03d36a3f0dd98bf12637122a77b1777
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-06-15 07:52:19 -0700

    ref(replay): Deduplicate components used in Replay Details>Error & Network tabs (#50884)
    This is following up on a comment from
    https://github.com/getsentry/sentry/pull/50701

--- static/app/views/replays/detail/errorList/errorHeaderCell.tsx
+++ static/app/views/replays/detail/errorList/errorHeaderCell.tsx
@@ -1,10 +1,8 @@
-import {ComponentProps, CSSProperties, forwardRef, ReactNode} from 'react';
-import styled from '@emotion/styled';
+import {ComponentProps, CSSProperties, forwardRef} from 'react';
 
+import HeaderCell from 'sentry/components/replays/virtualizedGrid/headerCell';
 import {Tooltip} from 'sentry/components/tooltip';
-import {IconArrow, IconInfo} from 'sentry/icons';
 import {t} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
 import useSortErrors from 'sentry/views/replays/detail/errorList/useSortErrors';
 
 type SortConfig = ReturnType<typeof useSortErrors>['sortConfig'];
@@ -14,10 +12,6 @@ type Props = {
   sortConfig: SortConfig;
   style: CSSProperties;
 };
-
-const SizeInfoIcon = styled(IconInfo)`
-  display: block;
-`;
 
 const COLUMNS: {
   field: SortConfig['by'];
@@ -32,54 +26,21 @@ const COLUMNS: {
 
 export const COLUMN_COUNT = COLUMNS.length;
 
-function CatchClicks({children}: {children: ReactNode}) {
-  return <div onClick={e => e.stopPropagation()}>{children}</div>;
-}
-
 const ErrorHeaderCell = forwardRef<HTMLButtonElement, Props>(
   ({handleSort, index, sortConfig, style}: Props, ref) => {
     const {field, label, tooltipTitle} = COLUMNS[index];
     return (
-      <HeaderButton style={style} onClick={() => handleSort(field)} ref={ref}>
-        {label}
-        {tooltipTitle ? (
-          <Tooltip isHoverable title={<CatchClicks>{tooltipTitle}</CatchClicks>}>
-            <SizeInfoIcon size="xs" />
-          </Tooltip>
-        ) : null}
-        <IconArrow
-          color="gray300"
-          size="xs"
-          direction={sortConfig.by === field && !sortConfig.asc ? 'down' : 'up'}
-          style={{visibility: sortConfig.by === field ? 'visible' : 'hidden'}}
-        />
-      </HeaderButton>
+      <HeaderCell
+        ref={ref}
+        handleSort={handleSort}
+        field={field}
+        label={label}
+        tooltipTitle={tooltipTitle}
+        sortConfig={sortConfig}
+        style={style}
+      />
     );
   }
 );
-
-const HeaderButton = styled('button')`
-  border: 0;
-  border-bottom: 1px solid ${p => p.theme.border};
-  background: ${p => p.theme.backgroundSecondary};
-  color: ${p => p.theme.subText};
-
-  font-size: ${p => p.theme.fontSizeSmall};
-  font-weight: 600;
-  line-height: 16px;
-  text-align: unset;
-  text-transform: uppercase;
-  white-space: nowrap;
-
-  width: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding: ${space(0.5)} ${space(1)} ${space(0.5)} ${space(1.5)};
-
-  svg {
-    margin-left: ${space(0.25)};
-  }
-`;
 
 export default ErrorHeaderCell;
