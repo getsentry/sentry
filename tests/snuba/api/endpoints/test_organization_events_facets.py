79ad62e3bb69194ad79c142b1a676fd2a5f83ad3
Nar Saynorath <nar.saynorath@sentry.io>
2023-06-16 10:10:13 -0400

    fix(events-facets): Use the pagination helper
    The initial PR to expose page size and cursor allow us to paginate, but
    it's missing data about whether or not there are more results. The
    pagination helper puts that information in the response header so we can
    inspect it in the frontend and know if there's more data.

--- tests/snuba/api/endpoints/test_organization_events_facets.py
+++ tests/snuba/api/endpoints/test_organization_events_facets.py
@@ -670,39 +670,54 @@ class OrganizationEventsFacetsEndpointTest(SnubaTestCase, APITestCase):
         ]
         self.assert_facet(response, "device.class", expected)
 
-    def test_with_per_page_and_cursor_parameters(self):
+    def test_with_cursor_parameter(self):
         test_project = self.create_project()
+        test_tags = {
+            "a": "one",
+            "b": "two",
+            "c": "three",
+            "d": "four",
+            "e": "five",
+            "f": "six",
+            "g": "seven",
+            "h": "eight",
+            "i": "nine",
+            "j": "ten",
+            "k": "eleven",
+        }
+
         self.store_event(
-            data={
-                "event_id": uuid4().hex,
-                "timestamp": self.min_ago_iso,
-                "tags": {
-                    "a": "one",
-                    "b": "two",
-                    "c": "three",
-                    "d": "four",
-                    "e": "five",
-                    "f": "six",
-                    "g": "seven",
-                    "h": "eight",
-                    "i": "nine",
-                    "j": "ten",
-                    "k": "eleven",
-                },
-            },
+            data={"event_id": uuid4().hex, "timestamp": self.min_ago_iso, "tags": test_tags},
             project_id=test_project.id,
         )
 
+        # Test the default query fetches the first 10 results
+        with self.feature(self.features):
+            response = self.client.get(self.url, format="json", data={"project": test_project.id})
+
+        assert response.status_code == 200, response.content
+        assert len(response.data) == 10
+
+        # Loop over the first 10 tags to ensure they're in the results
+        for tag_key in list(test_tags.keys())[:10]:
+            expected = [
+                {"count": 1, "name": test_tags[tag_key], "value": test_tags[tag_key]},
+            ]
+            self.assert_facet(response, tag_key, expected)
+
+        # Get the next page
         with self.feature(self.features):
             response = self.client.get(
-                self.url,
-                format="json",
-                data={"project": test_project.id, "cursor": 5, "per_page": 1},
+                self.url, format="json", data={"project": test_project.id, "cursor": "0:1:0"}
             )
 
         assert response.status_code == 200, response.content
-        assert len(response.data) == 1
+        assert len(response.data) == 2
         expected = [
-            {"count": 1, "name": "six", "value": "six"},
+            {"count": 1, "name": "eleven", "value": "eleven"},
         ]
-        self.assert_facet(response, "f", expected)
+        self.assert_facet(response, "k", expected)
+        expected = [
+            {"count": 1, "name": "error", "value": "error"},
+        ]
+        self.assert_facet(response, "level", expected)
