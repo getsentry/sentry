8c290a4acb77e822d9e267c4d615f0a6432b8cd5
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-06-19 20:17:18 +0200

    remove unused import

--- src/sentry/eventstream/kafka/backend.py
+++ src/sentry/eventstream/kafka/backend.py
@@ -20,7 +20,6 @@ from django.conf import settings
 
 from sentry import options
 from sentry.eventstream.base import EventStreamEventType, GroupStates
-from sentry.eventstream.kafka.dispatch import _get_task_kwargs_and_dispatch
 from sentry.eventstream.snuba import KW_SKIP_SEMANTIC_PARTITIONING, SnubaProtocolEventStream
 from sentry.killswitches import killswitch_matches_context
 from sentry.post_process_forwarder import PostProcessForwarder, PostProcessForwarderType
