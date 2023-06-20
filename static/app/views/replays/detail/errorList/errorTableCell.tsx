d6629804c1d17b1b8a84d9819ff87e54457fd271
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-06-20 16:26:07 -0700

    fix(replays): Fix icon alignment in Replay>Details Error tab (#51293)
    **Before:**
    <img width="282" alt="SCR-20230620-lbez"
    src="https://github.com/getsentry/sentry/assets/187460/147897fa-5326-4016-9ee9-fdf6abbbaec8">
    
    
    **After:**
    <img width="282" alt="SCR-20230620-jutb"
    src="https://github.com/getsentry/sentry/assets/187460/0cf0743a-0bff-4097-bfdf-54944e3347ee">
    
    
    Truncation of the column to the left is fixed in
    https://github.com/getsentry/sentry/pull/51214

--- static/app/views/replays/detail/errorList/errorTableCell.tsx
+++ static/app/views/replays/detail/errorList/errorTableCell.tsx
@@ -153,24 +153,26 @@ const ErrorTableCell = forwardRef<HTMLDivElement, Props>(
         </Cell>
       ),
       () => (
-        <Cell {...columnProps} gap={0.5}>
-          <AvatarWrapper>
-            <Avatar project={project} size={16} />
-          </AvatarWrapper>
-          <QuickContextHovercard
-            dataRow={{
-              'issue.id': groupId,
-              issue: groupShortId,
-            }}
-            contextType={ContextType.ISSUE}
-            organization={organization}
-          >
-            {issueUrl ? (
-              <Link to={issueUrl}>{groupShortId}</Link>
-            ) : (
-              <span>{groupShortId}</span>
-            )}
-          </QuickContextHovercard>
+        <Cell {...columnProps}>
+          <Text>
+            <AvatarWrapper>
+              <Avatar project={project} size={16} />
+            </AvatarWrapper>
+            <QuickContextHovercard
+              dataRow={{
+                'issue.id': groupId,
+                issue: groupShortId,
+              }}
+              contextType={ContextType.ISSUE}
+              organization={organization}
+            >
+              {issueUrl ? (
+                <Link to={issueUrl}>{groupShortId}</Link>
+              ) : (
+                <span>{groupShortId}</span>
+              )}
+            </QuickContextHovercard>
+          </Text>
         </Cell>
       ),
       () => (
