18e9915b8a682ff59595b46e238554cc0a11ac88
Francesco Novy <francesco.novy@sentry.io>
2023-06-20 09:58:18 +0200

    fix(org-tokens): Ensure all users can update token names
    This was actually specced out different than I initially implemented this, which I noticed just now - so fixing this to how it _should_ be. Note that as of now you can only update the token name, which is very low-risk, which is why we allow this for everyone (otherwise, users could not adjust the name of tokens they created). In the future, we _may_ adjust this so that users can only edit tokens they created themselves, but for now this is fine.

--- src/sentry/api/bases/organization.py
+++ src/sentry/api/bases/organization.py
@@ -177,7 +177,7 @@ class OrgAuthTokenPermission(OrganizationPermission):
     scope_map = {
         "GET": ["org:read", "org:write", "org:admin"],
         "POST": ["org:read", "org:write", "org:admin"],
-        "PUT": ["org:write", "org:admin"],
+        "PUT": ["org:read", "org:write", "org:admin"],
         "DELETE": ["org:write", "org:admin"],
     }
 
