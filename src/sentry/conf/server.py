24c7539731eb1e81d5023eda64e2202ef243676d
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-06-21 15:21:00 -0700

    test(hc): Add env var to force siloed tests
    This is a convenience for running tests manually in a local environment.
    If you want to observe how a test would behave if marked stable, rather
    than temporarily editing the decorator, you can do
    `SENTRY_FORCE_SILOED_TESTS=true pytest tests/sentry/test_whatever.py`
    on the shell.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -3479,6 +3479,9 @@ SLICED_KAFKA_TOPICS: Mapping[Tuple[str, int], Mapping[str, Any]] = {}
 # decorator.
 SINGLE_SERVER_SILO_MODE = False
 
+# Used by silo tests -- activate all silo mode test decorators even if not marked stable
+FORCE_SILOED_TESTS = os.environ.get("SENTRY_FORCE_SILOED_TESTS", False)
+
 # Set the URL for signup page that we redirect to for the setup wizard if signup=1 is in the query params
 SENTRY_SIGNUP_URL = None
 
