a4f433ae112837fc1855ed9be61ace44fa5808fc
Kamil Ogórek <kamil.ogorek@gmail.com>
2023-06-21 16:54:31 +0200

    Basic polling support with real recap api

--- src/sentry/tasks/recap_servers.py
+++ src/sentry/tasks/recap_servers.py
@@ -4,6 +4,7 @@ import uuid
 from datetime import datetime
 
 import pytz
+import urllib3
 from django.db.models import Q
 
 from sentry import http, options
@@ -13,8 +14,9 @@ from sentry.models import Project, ProjectOption
 from sentry.tasks import store
 from sentry.tasks.base import instrumented_task
 from sentry.utils import json
-from sentry.utils.assets import get_asset_url
-from sentry.utils.http import absolute_uri
+
+# from sentry.utils.assets import get_asset_url
+# from sentry.utils.http import absolute_uri
 from sentry.utils.safe import safe_execute
 
 # import sentry_sdk
@@ -26,6 +28,7 @@ from sentry.utils.safe import safe_execute
 # TODO(recap): Add SDK context (project_id etc.) and metrics based on `tasks/store.py`
 
 RECAP_SERVER_OPTION_KEY = "sentry:recap_server"
+RECAP_SERVER_MOST_RECENT_POLLED_ID_KEY = "sentry:recap_server_poll_id"
 
 
 @instrumented_task(
@@ -67,55 +70,85 @@ def poll_project_recap_server(project_id: int, **kwargs) -> None:
         print(">", "Missing project:", project_id)  # NOQA: S002
         return
 
+    recap_server = project.get_option(RECAP_SERVER_OPTION_KEY)
     print(">", project, project_id)  # NOQA: S002
-    print(  # NOQA: S002
-        ">", "Configured recap server url:", project.get_option(RECAP_SERVER_OPTION_KEY)
-    )
+    print(">", "Configured recap server url:", recap_server)  # NOQA: S002
 
-    # TODO(recap): Fake it, till you make it. This will use local static server, which we do not wait for
-    # so will fail initially until the server is up. Works for now, it's fine.
-    url = absolute_uri(get_asset_url("sentry", "recap_sever_payload.json"))
+    latest_id = project.get_option(RECAP_SERVER_MOST_RECENT_POLLED_ID_KEY, 0)
+    print(">", "Most recent polled ID:", latest_id)  # NOQA: S002
+
+    # TODO(recap): Remove me
+    urllib3.disable_warnings()  # unsafe certificate
+
+    # We want to make sure that our initial /crashes query is *inclusive* with id:0
+    if latest_id == 0:
+        bound_type = "%5B"  # Inclusive bound - [
+    else:
+        bound_type = "%7B"  # Exclusive bound - {
+
+    url = (
+        recap_server.strip().rstrip("/")
+        + "/rest/v1/crashes;q=id:"
+        + bound_type
+        + str(latest_id)
+        + "%20TO%20*%5D"
+    )
     print("> Fetching JSON payload from:", url)  # NOQA: S002
 
-    result = http.fetch_file(url)
-    payload = json.loads(result.body)
+    result = http.fetch_file(url, headers={"Accept": "*/*"}, verify_ssl=False)
+    crashes = json.loads(result.body)
 
-    processed_stacktrace = []
-    for frame in payload["detailedStackTrace"]:
-        processed_frame = {
-            "filename": frame["sourceFile"],
-            "lineno": frame["sourceLine"],
-            "instruction_addr": frame["absoluteAddress"],
-            "module": frame["moduleName"],
-            "function": frame["resolvedSymbol"],
-            "raw_function": frame["displayValue"],
-        }
-        processed_stacktrace.append(processed_frame)
+    if crashes["results"] == 0:
+        print("> No new crashes found:", url)  # NOQA: S002
+        return
 
-    detailed_st = {}
-    for count, value in enumerate(payload["detailedStackTrace"]):
-        detailed_st["frame" + str(count)] = value
+    print("> Found", crashes["results"], "new crashes")  # NOQA: S002
+    latest_id = 0
+    for crash in crashes["_embedded"]["crash"]:
+        latest_id = max(latest_id, crash["id"])
+        store_crash(crash, project, url)
+
+    print("> Updating most recent ID to poll:", latest_id)  # NOQA: S002
+    project.update_option(RECAP_SERVER_MOST_RECENT_POLLED_ID_KEY, latest_id)
+
+
+def store_crash(crash, project: Project, url: str) -> None:
+    # processed_stacktrace = []
+    # for frame in payload["detailedStackTrace"]:
+    #     processed_frame = {
+    #         "filename": frame["sourceFile"],
+    #         "lineno": frame["sourceLine"],
+    #         "instruction_addr": frame["absoluteAddress"],
+    #         "module": frame["moduleName"],
+    #         "function": frame["resolvedSymbol"],
+    #         "raw_function": frame["displayValue"],
+    #     }
+    #     processed_stacktrace.append(processed_frame)
+
+    # detailed_st = {}
+    # for count, value in enumerate(payload["detailedStackTrace"]):
+    #     detailed_st["frame" + str(count)] = value
 
     event = {
         "event_id": uuid.uuid4().hex,
-        "project": project_id,
+        "project": project.id,
         "exception": {
             "values": [
                 {
-                    "type": payload["stopReason"],
-                    "value": "Unknown exception",
-                    "stacktrace": {
-                        "frames": processed_stacktrace,
-                    },
+                    "type": crash["stopReason"],
+                    "value": "Unknown exception (id#{})".format(crash["id"]),
+                    # "stacktrace": {
+                    #     "frames": processed_stacktrace,
+                    # },
                 }
             ]
         },
         "contexts": {
-            "_links": payload["_links"],
-            "detailedStackTrace": detailed_st,
+            "_links": crash["_links"],
+            # "detailedStackTrace": detailed_st,
             "user": {"password": "should_be_redacted"},
         },
-        "tags": {"url": url},
+        "tags": {"url": url, "id": crash["id"]},
     }
 
     project_id = event["project"]
@@ -133,7 +166,7 @@ def poll_project_recap_server(project_id: int, **kwargs) -> None:
     event_manager.normalize(project_id)
     data = event_manager.get_data()
 
-    return store.save_event(
+    store.save_event(
         project_id=project_id,
         event_id=event_id,
         data=data,
