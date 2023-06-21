7dad823723c563ce258b71f5a54b87c4b553eab6
Kev <6111995+k-fish@users.noreply.github.com>
2023-06-21 11:43:38 -0400

    flag(perf): Trace view flag w/o perf (#51355)
    Adds flag for 'tracing without performance' UI and backend changes

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1517,6 +1517,8 @@ SENTRY_FEATURES = {
     "organizations:performance-issues-render-blocking-assets-detector": False,
     # Enable MN+1 DB performance issue type
     "organizations:performance-issues-m-n-plus-one-db-detector": False,
+    # Enable FE/BE for tracing without performance
+    "organizations:performance-tracing-without-performance": False,
     # Enable the new Related Events feature
     "organizations:related-events": False,
     # Enable usage of external relays, for use with Relay. See
