d2949066d91b2cd33a1177705d84a03b6f9a4526
Francesco Novy <francesco.novy@sentry.io>
2023-06-22 12:50:37 +0200

    fix secret generation

--- src/sentry/utils/security/orgauthtoken_token.py
+++ src/sentry/utils/security/orgauthtoken_token.py
@@ -17,7 +17,7 @@ def generate_token(org_slug: str, region_url: str):
         "region_url": region_url,
         "org": org_slug,
     }
-    secret = b64encode(secrets.token_urlsafe(nbytes=32).encode("utf-8"))
+    secret = b64encode(secrets.token_bytes(nbytes=32)).decode("ascii").rstrip("=")
 
     json_str = json.dumps(payload)
     payload_hashed = base64_encode_str(json_str)
@@ -29,10 +29,7 @@ def parse_token(token: str):
     if not token.startswith(SENTRY_ORG_AUTH_TOKEN_PREFIX) or token.count("_") != 2:
         return None
 
-    # Note: We add == to the end of the string, because we remove the base64 padding when generating the token
-    # But python expects the correct amount of padding to be present, erroring out otherwise
-    # However, any _excess_ padding is ignored, so we just add the max. amount of padding and it works
-    payload_hashed = token[len(SENTRY_ORG_AUTH_TOKEN_PREFIX) : token.rindex("_")] + "=="
+    payload_hashed = token[len(SENTRY_ORG_AUTH_TOKEN_PREFIX) : token.rindex("_")]
 
     try:
         payload_str = b64decode((payload_hashed).encode("ascii")).decode("ascii")
@@ -45,4 +42,4 @@ def parse_token(token: str):
 
 
 def base64_encode_str(str):
-    return b64encode(str.encode("ascii")).decode("ascii").rstrip("=")
+    return b64encode(str.encode("ascii")).decode("ascii")
