899398e3656217f4e4ad6fcf5067f5d934fc4ebe
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-06-22 14:32:55 -0400

    Use generic field rendering

--- static/app/views/starfish/views/spanSummaryPage/spanTransactionsTable.tsx
+++ static/app/views/starfish/views/spanSummaryPage/spanTransactionsTable.tsx
@@ -13,20 +13,16 @@ import Link from 'sentry/components/links/link';
 import Pagination from 'sentry/components/pagination';
 import Truncate from 'sentry/components/truncate';
 import {t} from 'sentry/locale';
+import {getFieldRenderer} from 'sentry/utils/discover/fieldRenderers';
 import {useLocation} from 'sentry/utils/useLocation';
-import DurationCell from 'sentry/views/starfish/components/tableCells/durationCell';
-import ThroughputCell from 'sentry/views/starfish/components/tableCells/throughputCell';
-import {TimeSpentCell} from 'sentry/views/starfish/components/tableCells/timeSpentCell';
+import useOrganization from 'sentry/utils/useOrganization';
 import type {IndexedSpan} from 'sentry/views/starfish/queries/types';
 import {
   SpanTransactionMetrics,
   useSpanTransactionMetrics,
 } from 'sentry/views/starfish/queries/useSpanTransactionMetrics';
-import {SpanMetricsFields} from 'sentry/views/starfish/types';
 import {extractRoute} from 'sentry/views/starfish/utils/extractRoute';
 import {DataTitles} from 'sentry/views/starfish/views/spans/types';
-
-const {SPAN_SELF_TIME} = SpanMetricsFields;
 
 type Row = {
   metrics: SpanTransactionMetrics;
@@ -56,9 +52,11 @@ export function SpanTransactionsTable({
   method,
 }: Props) {
   const location = useLocation();
+  const organization = useOrganization();
 
   const {
     data: spanTransactionMetrics,
+    meta,
     isLoading,
     pageLinks,
   } = useSpanTransactionMetrics(span, endpoint ? [endpoint] : undefined);
@@ -75,18 +73,33 @@ export function SpanTransactionsTable({
   };
 
   const renderBodyCell = (column: TableColumnHeader, row: Row) => {
-    return (
-      <BodyCell
-        span={span}
-        column={column}
-        row={row}
-        openSidebar={openSidebar}
-        onClickTransactionName={onClickTransaction}
-        endpoint={endpoint}
-        method={method}
-        location={location}
-      />
-    );
+    if (column.key === 'transaction') {
+      return (
+        <TransactionCell
+          endpoint={endpoint}
+          method={method}
+          span={span}
+          row={row}
+          column={column}
+          openSidebar={openSidebar}
+          onClickTransactionName={onClickTransaction}
+          location={location}
+        />
+      );
+    }
+
+    if (!meta || !meta?.fields) {
+      return row[column.key];
+    }
+
+    const renderer = getFieldRenderer(column.key, meta.fields, false);
+    const rendered = renderer(row, {
+      location,
+      organization,
+      unit: meta.units?.[column.key],
+    });
+
+    return rendered;
   };
 
   return (
@@ -129,61 +142,6 @@ type CellProps = {
   onClickTransactionName?: (row: Row) => void;
   openSidebar?: boolean;
 };
-
-function BodyCell({
-  span,
-  column,
-  row,
-  openSidebar,
-  onClickTransactionName,
-  endpoint,
-  method,
-  location,
-}: CellProps) {
-  if (column.key === 'transaction') {
-    return (
-      <TransactionCell
-        endpoint={endpoint}
-        method={method}
-        span={span}
-        row={row}
-        column={column}
-        openSidebar={openSidebar}
-        onClickTransactionName={onClickTransactionName}
-        location={location}
-      />
-    );
-  }
-
-  if (column.key === 'p95(transaction.duration)') {
-    return (
-      <DurationCell
-        milliseconds={row.metrics?.[`p95(${SPAN_SELF_TIME})`]}
-        delta={row.metrics?.[`percentile_percent_change(${SPAN_SELF_TIME}, 0.95)`]}
-      />
-    );
-  }
-
-  if (column.key === 'sps()') {
-    return (
-      <ThroughputCell
-        throughputPerSecond={row.metrics?.['sps()']}
-        delta={row.metrics?.['sps_percent_change()']}
-      />
-    );
-  }
-
-  if (column.key === 'time_spent_percentage(local)') {
-    return (
-      <TimeSpentCell
-        timeSpentPercentage={row.metrics?.['time_spent_percentage(local)']}
-        totalSpanTime={row.metrics?.[`sum(${SPAN_SELF_TIME})`]}
-      />
-    );
-  }
-
-  return <span>{row[column.key]}</span>;
-}
 
 function TransactionCell({span, column, row, endpoint, method, location}: CellProps) {
   return (
