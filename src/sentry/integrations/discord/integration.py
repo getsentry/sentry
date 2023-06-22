b34e2b41ae3462b18d4aa00754a67722b246f5fc
Spencer Murray <spencer.murray@sentry.io>
2023-06-22 13:08:40 -0700

    Implement initial discord install pipeline

--- src/sentry/integrations/discord/integration.py
+++ src/sentry/integrations/discord/integration.py
@@ -4,6 +4,7 @@ from typing import Any, Mapping, Sequence
 
 from django.utils.translation import ugettext_lazy as _
 
+from sentry import options
 from sentry.integrations import (
     FeatureDescription,
     IntegrationFeatures,
@@ -12,6 +13,7 @@ from sentry.integrations import (
     IntegrationProvider,
 )
 from sentry.pipeline.views.base import PipelineView
+from sentry.utils.http import absolute_uri
 
 DESCRIPTION = "Discord’s your place to collaborate, share, and just talk about your day – or commiserate about app errors. Connect Sentry to your Discord server and get [alerts](https://docs.sentry.io/product/alerts/alert-types/) in a channel of your choice or via direct message when sh%t hits the fan."
 
@@ -46,8 +48,39 @@ class DiscordIntegrationProvider(IntegrationProvider):
     features = frozenset([IntegrationFeatures.CHAT_UNFURL])
     requires_feature_flag = True  # remove this when we remove the discord feature flag
 
+    # https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+    oauth_scopes = frozenset(["applications.commands", "bot"])
+
+    # Visit the bot tab of your app in the Discord developer portal for a tool to generate this integer
+    bot_permissions = 2048
+
+    setup_dialog_config = {"width": 600, "height": 900}
+
     def get_pipeline_views(self) -> Sequence[PipelineView]:
-        return super().get_pipeline_views()
+        return [DiscordInstallPipeline(self.get_bot_install_url())]
 
     def build_integration(self, state: Mapping[str, Any]) -> Mapping[str, Any]:
-        return super().build_integration(state)
+        guild_id = str(state.get("guild_id"))
+        return {
+            # "name": add guild name once have API Client and can fetch it
+            "external_id": guild_id
+        }
+
+    def get_bot_install_url(self):
+        application_id = options.get("discord.application-id")
+        setup_url = absolute_uri("extensions/discord/setup/")
+
+        return f"https://discord.com/api/oauth2/authorize?client_id={application_id}&permissions={self.bot_permissions}&redirect_uri={setup_url}&response_type=code&scope={'%20'.join(self.oauth_scopes)}"
+
+
+class DiscordInstallPipeline(PipelineView):
+    def __init__(self, install_url: str):
+        self.install_url = install_url
+        super().__init__()
+
+    def dispatch(self, request, pipeline):
+        if "guild_id" in request.GET:
+            pipeline.bind_state("guild_id", request.GET["guild_id"])
+            return pipeline.next_step()
+
+        return self.redirect(self.install_url)
