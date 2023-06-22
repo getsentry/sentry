4881e849df76f6b3d2780e50e2ea09f4de678ffd
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2023-06-22 21:59:56 +0200

    fix: Type sentry.snuba.outcomes (#51474)

--- src/sentry/snuba/outcomes.py
+++ src/sentry/snuba/outcomes.py
@@ -13,9 +13,9 @@ from snuba_sdk.function import Function
 from snuba_sdk.query import Query
 
 from sentry.constants import DataCategory
+from sentry.release_health.base import AllowedResolution
 from sentry.search.utils import InvalidQuery
 from sentry.snuba.sessions_v2 import (
-    AllowedResolution,
     InvalidField,
     SimpleGroupBy,
     get_constrained_date_range,
@@ -260,7 +260,7 @@ class QueryDefinition:
         stats_period: Optional[str] = None,
         organization_id: Optional[int] = None,
         project_ids: Optional[List[int]] = None,
-        key_id: Optional[int] = None,
+        key_id: Optional[str | int] = None,
         interval: Optional[str] = None,
         outcome: Optional[List[str]] = None,
         group_by: Optional[List[str]] = None,
