17b4b49d158c2457dbaaca4167de39c87d016eee
schew2381 <seiji.chew@sentry.io>
2023-06-23 16:37:20 -0700

    fix(api): Add back nested list

--- src/sentry/api/serializers/models/project_key.py
+++ src/sentry/api/serializers/models/project_key.py
@@ -52,7 +52,7 @@ class ProjectKeySerializerResponse(TypedDict):
     rateLimit: Optional[RateLimit]
     dsn: DSN
     browserSdkVersion: str
-    # browserSdk: BrowserSDK
+    browserSdk: BrowserSDK
     dateCreated: Optional[datetime]
     dynamicSdkLoaderOptions: DynamicSDKLoaderOptions
 
@@ -89,7 +89,7 @@ class ProjectKeySerializer(Serializer):
                 "cdn": obj.js_sdk_loader_cdn_url,
             },
             "browserSdkVersion": get_selected_browser_sdk_version(obj),
-            # "browserSdk": {"choices": get_browser_sdk_version_choices(obj.project)},
+            "browserSdk": {"choices": get_browser_sdk_version_choices(obj.project)},
             "dateCreated": obj.date_added,
             "dynamicSdkLoaderOptions": {
                 "hasReplay": get_dynamic_sdk_loader_option(obj, DynamicSdkLoaderOption.HAS_REPLAY),
