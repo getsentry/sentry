dc36e1ec40392f7796efab07cde9c15754c53b78
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-06-23 15:34:10 -0400

    Remove unused delta props

--- static/app/views/starfish/components/tableCells/throughputCell.tsx
+++ static/app/views/starfish/components/tableCells/throughputCell.tsx
@@ -1,25 +1,17 @@
 import {t} from 'sentry/locale';
 import {NumberContainer} from 'sentry/utils/discover/styles';
-import {formatAbbreviatedNumber, formatPercentage} from 'sentry/utils/formatters';
-import {ComparisonLabel} from 'sentry/views/starfish/components/samplesTable/common';
+import {formatAbbreviatedNumber} from 'sentry/utils/formatters';
 
 type Props = {
-  delta?: number;
   throughputPerSecond?: number;
 };
 
-export default function ThroughputCell({throughputPerSecond, delta}: Props) {
+export default function ThroughputCell({throughputPerSecond}: Props) {
   const throughput = throughputPerSecond ? throughputPerSecond.toFixed(2) : '--';
+
   return (
     <NumberContainer>
-      <span>{`${formatAbbreviatedNumber(throughput)}/${t('s')}`}</span>
-      {delta ? (
-        // Don't highlight throughput red or green, since throughput delta isn't good or bad
-        <ComparisonLabel value={0}>
-          {delta > 0 ? '+' : ''}
-          {formatPercentage(delta)}
-        </ComparisonLabel>
-      ) : null}
+      {formatAbbreviatedNumber(throughput)}/${t('s')}
     </NumberContainer>
   );
 }
