bd5c80da02ef6814946b1a566f9ab6f012e4f3f4
Eric Hasegawa <erichasegawa1@gmail.com>
2023-06-26 16:30:35 -0700

    ref(auth): Update ApiApplication serializer (ER-1448) (#51570)
    `allowed_origins` and `redirect_uris` are stored as character fields in
    our DB. I _think_ we accept both space separated and newline separated
    lists. This PR updates our outbound serializers to have consistent
    returns for these fields.
    
    Note that we're using this model for a new endpoint for instance level
    OAuth clients (i.e. to eventually allow users to sign in to CodeCov with
    Sentry), but it's [also used for creating API Applications
    ](https://sentry.io/settings/account/api/applications/). The code for
    this was written 6-8 years ago, and there might be subtle side effects
    of this change somewhere.
    
    Although this passes all tests and shouldn't be breaking, I'd feel
    better if someone familiar with that part of our codebase/product could
    take a look, if anyone knows who that may be please let me know!

--- src/sentry/models/apiapplication.py
+++ src/sentry/models/apiapplication.py
@@ -109,12 +109,17 @@ class ApiApplication(Model):
         return False
 
     def get_default_redirect_uri(self):
-        return self.redirect_uris.split("\n", 1)[0]
+        return self.redirect_uris.split()[0]
 
     def get_allowed_origins(self):
         if not self.allowed_origins:
             return []
-        return [a for a in self.allowed_origins.split("\n") if a]
+        return [origin for origin in self.allowed_origins.split()]
+
+    def get_redirect_uris(self):
+        if not self.redirect_uris:
+            return []
+        return [redirect_uri for redirect_uri in self.redirect_uris.split()]
 
     def get_audit_log_data(self):
         return {
