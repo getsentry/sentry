ff1ad4d3b509b3c94d0cf1dabbdd77b611ea1514
Ash Anand <ash.anand@sentry.io>
2023-06-27 17:08:39 -0400

    Remove list container component

--- static/app/views/starfish/views/webServiceView/spanGroupBreakdown.tsx
+++ static/app/views/starfish/views/webServiceView/spanGroupBreakdown.tsx
@@ -1,24 +1,12 @@
 import {useEffect, useState} from 'react';
-import {Link} from 'react-router';
-import {useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
 import cloneDeep from 'lodash/cloneDeep';
-import * as qs from 'query-string';
 
-import Checkbox from 'sentry/components/checkbox';
 import {CompactSelect, SelectOption} from 'sentry/components/compactSelect';
-import TextOverflow from 'sentry/components/textOverflow';
-import {Tooltip} from 'sentry/components/tooltip';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {Series} from 'sentry/types/echarts';
-import {defined} from 'sentry/utils';
-import {getUtcDateString} from 'sentry/utils/dates';
 import {tooltipFormatterUsingAggregateOutputType} from 'sentry/utils/discover/charts';
-import {NumberContainer} from 'sentry/utils/discover/styles';
-import {formatPercentage} from 'sentry/utils/formatters';
-import usePageFilters from 'sentry/utils/usePageFilters';
-import {RightAlignedCell} from 'sentry/views/performance/landing/widgets/components/selectableList';
 import Chart from 'sentry/views/starfish/components/chart';
 import {DataRow} from 'sentry/views/starfish/views/webServiceView/spanGroupBreakdownContainer';
 
@@ -41,16 +29,14 @@ export enum DataDisplayType {
 }
 
 export function SpanGroupBreakdown({
-  tableData: transformedData,
-  totalCumulativeTime: totalValues,
+  // tableData: transformedData,
+  // totalCumulativeTime: totalValues,
   topSeriesData: data,
   initialShowSeries,
   transaction,
   isTimeseriesLoading,
   errored,
 }: Props) {
-  const {selection} = usePageFilters();
-  const theme = useTheme();
   const [showSeriesArray, setShowSeriesArray] = useState<boolean[]>(initialShowSeries);
   const options: SelectOption<DataDisplayType>[] = [
     {label: 'Total Duration', value: DataDisplayType.CUMULATIVE_DURATION},
@@ -72,7 +58,6 @@ export function SpanGroupBreakdown({
       visibleSeries.push(series);
     }
   }
-  const colorPalette = theme.charts.getColorPalette(transformedData.length - 2);
 
   const dataAsPercentages = cloneDeep(visibleSeries);
   const numDataPoints = data[0]?.data?.length ?? 0;
@@ -104,6 +89,7 @@ export function SpanGroupBreakdown({
         <Chart
           statsPeriod="24h"
           height={210}
+          showLegend
           data={
             dataDisplayType === DataDisplayType.PERCENTAGE
               ? dataAsPercentages
@@ -133,94 +119,9 @@ export function SpanGroupBreakdown({
           }}
         />
       </ChartPadding>
-      <ListContainer>
-        {transformedData.map((row, index) => {
-          const checkedValue = showSeriesArray[index];
-          const group = row.group;
-          const {start, end, utc, period} = selection.datetime;
-          const spansLinkQueryParams =
-            start && end
-              ? {start: getUtcDateString(start), end: getUtcDateString(end), utc}
-              : {statsPeriod: period};
-          if (['db', 'http'].includes(group['span.category'])) {
-            spansLinkQueryParams['span.module'] = group['span.category'];
-          } else {
-            spansLinkQueryParams['span.module'] = 'Other';
-          }
-          spansLinkQueryParams['span.category'] = group['span.category'];
-
-          const spansLink = `/starfish/spans/?${qs.stringify(spansLinkQueryParams)}`;
-          return (
-            <StyledLineItem key={`${group['span.category']}`}>
-              <ListItemContainer>
-                <Checkbox
-                  size="sm"
-                  checkboxColor={colorPalette[index]}
-                  inputCss={{backgroundColor: 'red'}}
-                  checked={checkedValue}
-                  onChange={() => {
-                    const updatedSeries = [...showSeriesArray];
-                    updatedSeries[index] = !checkedValue;
-                    setShowSeriesArray(updatedSeries);
-                  }}
-                />
-                <TextAlignLeft>
-                  {defined(transaction) ? (
-                    <TextOverflow>{group['span.category']}</TextOverflow>
-                  ) : (
-                    <Link to={spansLink}>
-                      <TextOverflow>{group['span.category']}</TextOverflow>
-                    </Link>
-                  )}
-                </TextAlignLeft>
-                <RightAlignedCell>
-                  <Tooltip
-                    title={t(
-                      '%s time spent on %s',
-                      formatPercentage(row.cumulativeTime / totalValues, 1),
-                      group['span.category']
-                    )}
-                    containerDisplayMode="block"
-                    position="top"
-                  >
-                    <NumberContainer
-                      style={{textDecoration: 'underline', textDecorationStyle: 'dotted'}}
-                    >
-                      {formatPercentage(row.cumulativeTime / totalValues, 1)}
-                    </NumberContainer>
-                  </Tooltip>
-                </RightAlignedCell>
-              </ListItemContainer>
-            </StyledLineItem>
-          );
-        })}
-      </ListContainer>
     </FlexRowContainer>
   );
 }
-
-const StyledLineItem = styled('li')`
-  line-height: ${p => p.theme.text.lineHeightBody};
-`;
-
-const ListItemContainer = styled('div')`
-  display: flex;
-  padding: ${space(1)} ${space(2)};
-  font-size: ${p => p.theme.fontSizeMedium};
-`;
-
-const ListContainer = styled('ul')`
-  padding: ${space(1)} 0 0 0;
-  margin: 0;
-  border-left: 1px solid ${p => p.theme.border};
-  list-style-type: none;
-`;
-
-const TextAlignLeft = styled('span')`
-  text-align: left;
-  width: 100%;
-  padding: 0 ${space(1.5)};
-`;
 
 const ChartPadding = styled('div')`
   padding: 0 ${space(2)};
