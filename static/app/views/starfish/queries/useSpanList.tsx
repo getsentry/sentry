313737d1a238e87a8d0596396ea5471f090aed42
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-06-27 22:34:30 -0400

    Include Redis spans in Other category

--- static/app/views/starfish/queries/useSpanList.tsx
+++ static/app/views/starfish/queries/useSpanList.tsx
@@ -130,6 +130,16 @@ function buildEventViewQuery(
     .map(key => {
       const value = query[key];
       const isArray = Array.isArray(value);
+
+      if (key === '!span.category' && isArray && value.includes('db')) {
+        // When omitting database spans, explicitly allow `db.redis` spans, because
+        // we're not including those spans in the database category
+        const categoriesAsideFromDatabase = value.filter(v => v !== 'db');
+        return `(!span.category:db OR span.op:db.redis) !span.category:[${categoriesAsideFromDatabase.join(
+          ','
+        )}]`;
+      }
+
       return `${key}:${isArray ? `[${value}]` : value}`;
     });
 
