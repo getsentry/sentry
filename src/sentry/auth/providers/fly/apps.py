160e4fbacdd83abffbe22cb7083ac12df1896544
nathan hsieh <hsieh.nathan@gmail.com>
2023-06-28 10:49:14 -0700

    register the fly options & disable analytics emit

--- src/sentry/auth/providers/fly/apps.py
+++ src/sentry/auth/providers/fly/apps.py
@@ -5,8 +5,16 @@ class Config(AppConfig):
     name = "sentry.auth.providers.fly"
 
     def ready(self):
-        from sentry import auth
+        from sentry import auth, options
 
         from .provider import FlyOAuth2Provider
 
         auth.register("fly", FlyOAuth2Provider)
+
+        options.register(
+            "auth-fly.client-id", flags=options.FLAG_ALLOW_EMPTY | options.FLAG_PRIORITIZE_DISK
+        )
+        options.register(
+            "auth-fly.client-secret",
+            flags=options.FLAG_ALLOW_EMPTY | options.FLAG_PRIORITIZE_DISK,
+        )
