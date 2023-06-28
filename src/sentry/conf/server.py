ba371da3b89de6a8aff837c0db59b37067159516
Mike Ihbe <mike.ihbe@sentry.io>
2023-06-28 22:13:00 +0200

    feat(hybrid-cloud): Rollout avatar migration to 100% (#51800)
    Remove avatar migration rollout flag. This unblocks the complete
    backfill script.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -3507,8 +3507,6 @@ MAX_ENVIRONMENTS_PER_MONITOR = 1000
 # tests)
 SENTRY_METRICS_INDEXER_RAISE_VALIDATION_ERRORS = False
 
-SENTRY_FILE_COPY_ROLLOUT_RATE = 0.5
-
 # The project ID for SDK Crash Monitoring to save the detected SDK crashed to.
 # Currently, this is a single value, as the SDK Crash Detection feature only detects crashes for the Cocoa SDK.
 # Once we start detecting crashes for other SDKs, this will be a mapping of SDK name to project ID or something similar.
