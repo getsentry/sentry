8a61dc74b18bc3f6d1ef176af59d502bc2d41925
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-06-28 22:15:44 +0300

    ref(js): Convert sanitizeQuerySelector.spec to tsx (#51812)

--- static/app/utils/sanitizeQuerySelector.spec.tsx
+++ static/app/utils/sanitizeQuerySelector.spec.tsx
@@ -8,8 +8,4 @@ describe('sanitizeQuerySelector', function () {
   it('replaces colons with a hyphen', function () {
     expect(sanitizeQuerySelector('foo:bar:baz bar foo')).toBe('foo-bar-baz-bar-foo');
   });
-
-  it('returns an empty string if passed undefined', function () {
-    expect(sanitizeQuerySelector()).toBe('');
-  });
 });
