92dca113f8ee9f19b38cc85c13790f1cb9420d9f
Scott Cooper <scttcper@gmail.com>
2023-06-28 16:57:50 -0700

    fix(escalating): Put archive copy in the right place (#51849)
    resolve button was the wrong place

--- static/app/components/actions/resolve.tsx
+++ static/app/components/actions/resolve.tsx
@@ -239,9 +239,7 @@ function ResolveActions({
         <ResolveButton
           priority={priority}
           size={size}
-          title={t(
-            'We’ll nag you with a notification if the issue gets worse. All archived issues can be found in the Archived tab.'
-          )}
+          title={t("We'll nag you with a notification if another event is seen.")}
           tooltipProps={{delay: 1000, disabled}}
           onClick={() =>
             openConfirmModal({
