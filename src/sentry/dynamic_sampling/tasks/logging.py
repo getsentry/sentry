18c124d9a41ce18259444fef7df98c829f00564c
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-06-29 14:24:07 +0200

    feat(dynamic-sampling): Improve logging of recalibration

--- src/sentry/dynamic_sampling/tasks/logging.py
+++ src/sentry/dynamic_sampling/tasks/logging.py
@@ -46,13 +46,12 @@ def log_recalibrate_orgs_errors(errors: Dict[str, List[str]]) -> None:
 
 
 def log_recalibrate_org_state(
-    org_id: int, previous_factor: float, effective_sample_rate: float, target_sample_rate: float
+    prev_factor: float, effective_sample_rate: float, target_sample_rate: float
 ) -> None:
     logger.info(
         "dynamic_sampling.recalibrate_org_state",
         extra={
-            "org_id": org_id,
-            "previous_factor": previous_factor,
+            "previous_factor": prev_factor,
             "effective_sample_rate": effective_sample_rate,
             "target_sample_rate": target_sample_rate,
             "target_effective_ratio": target_sample_rate / effective_sample_rate,
