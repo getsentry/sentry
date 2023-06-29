0860e4a53051f5641dd59d6681d95939f331f112
Ash Anand <ash.anand@sentry.io>
2023-06-29 12:31:11 -0400

    Update the selected project in route and pagefilter

--- static/app/views/starfish/components/starfishProjectSelector.tsx
+++ static/app/views/starfish/components/starfishProjectSelector.tsx
@@ -1,14 +1,17 @@
+import {updateProjects} from 'sentry/actionCreators/pageFilters';
 import {CompactSelect} from 'sentry/components/compactSelect';
 import ProjectBadge from 'sentry/components/idBadge/projectBadge';
 import {Project} from 'sentry/types';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import useProjects from 'sentry/utils/useProjects';
+import useRouter from 'sentry/utils/useRouter';
 
 const ALLOWED_PLATFORMS = new Set(['python']);
 
 export function StarfishProjectSelector() {
   const {projects} = useProjects();
   const {selection} = usePageFilters();
+  const router = useRouter();
 
   const projectOptions = projects
     .filter(project => ALLOWED_PLATFORMS.has(project.platform ?? '') && project.isMember)
@@ -21,7 +24,16 @@ export function StarfishProjectSelector() {
     selection.projects.includes(parseInt(option.value, 10))
   );
 
-  return <CompactSelect options={projectOptions} defaultValue={selectedOption?.value} />;
+  const handleProjectChange = option =>
+    updateProjects([parseInt(option.value, 10)], router);
+
+  return (
+    <CompactSelect
+      options={projectOptions}
+      defaultValue={selectedOption?.value}
+      onChange={handleProjectChange}
+    />
+  );
 }
 
 function ProjectOptionLabel({project}: {project: Project}) {
