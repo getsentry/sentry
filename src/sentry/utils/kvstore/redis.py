d69cc421c4d1a6006b8d443c88b57e8c9b2c81d7
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2023-06-30 17:35:54 +0200

    fix typing once more

--- src/sentry/utils/kvstore/redis.py
+++ src/sentry/utils/kvstore/redis.py
@@ -1,26 +1,28 @@
 from __future__ import annotations
 
 from datetime import timedelta
-from typing import Optional
+from typing import Optional, TypeVar
 
 from sentry_redis_tools.clients import RedisCluster, StrictRedis
 
 from sentry.utils.kvstore.abstract import KVStorage
 
+T = TypeVar("T", str, bytes)
 
-class RedisKVStorage(KVStorage[str, bytes]):
+
+class RedisKVStorage(KVStorage[str, T]):
     """
     This class provides a key/value store backed by Redis (either a single node
     or cluster.)
     """
 
-    def __init__(self, client: StrictRedis | RedisCluster) -> None:
-        self.client = client
+    def __init__(self, client: StrictRedis[T] | RedisCluster[T]) -> None:
+        self.client: StrictRedis[T] | RedisCluster[T] = client
 
-    def get(self, key: str) -> Optional[bytes]:
+    def get(self, key: str) -> Optional[T]:
         return self.client.get(key.encode("utf8"))
 
-    def set(self, key: str, value: bytes, ttl: Optional[timedelta] = None) -> None:
+    def set(self, key: str, value: T, ttl: Optional[timedelta] = None) -> None:
         self.client.set(key.encode("utf8"), value, ex=ttl)
 
     def delete(self, key: str) -> None:
