getsentry/sentry

commit 98f756884220ab5393e5ab0786cb2198e0d1a73a

ref(gridEditable): Remove unnecessary overflow: hidden (#50515)
Remove unnecessary `overflow: hidden` declarations in `GridEditable`, to
fix an overflow issue with dropdown menus inside these grids:

**Before ——**
<img width="613" alt="Screenshot 2023-06-07 at 1 36 49 PM"
src="https://github.com/getsentry/sentry/assets/44172267/16501374-4bf9-42d3-92e5-8dbf4178675a">

**After ——**
<img width="613" alt="Screenshot 2023-06-07 at 1 36 22 PM"
src="https://github.com/getsentry/sentry/assets/44172267/af11ddb2-cd48-446c-8642-b9984c290a8f">

static/app/components/gridEditable/styles.tsx
@@ -51,7 +51,6 @@ export const Body = styled(({children, ...props}) => (
     <PanelBody>{children}</PanelBody>
   </Panel>
 ))`
-  overflow: hidden;
   z-index: ${Z_INDEX_PANEL};
 `;
 
@@ -79,8 +78,12 @@ export const Grid = styled('table')<{height?: string | number; scrollable?: bool
   margin: 0;
 
   z-index: ${Z_INDEX_GRID};
-  overflow-x: auto;
-  overflow-y: ${p => (p.scrollable ? 'scroll' : 'hidden')};
+  ${p =>
+    p.scrollable &&
+    `
+    overflow-x: auto;
+    overflow-y: scroll;
+    `}
   ${p =>
     p.height
       ? `

commit 0cab591affccdb1c1c3467a36a6750ecf316bd3c

fix(gridEditable): Fix overflow issue (#51851)
Fix an overflow issue caused by
https://github.com/getsentry/sentry/pull/50515.

**Before ——** right edge overflows without scrolling
<img width="791" alt="image"
src="https://github.com/getsentry/sentry/assets/44172267/360f30df-8029-48dd-aca0-0b929fd15b5c">

**After ——**
<img width="791" alt="image"
src="https://github.com/getsentry/sentry/assets/44172267/34430e2f-9384-421a-88c1-93663ff7e654">

static/app/components/gridEditable/styles.tsx
@@ -51,6 +51,7 @@ export const Body = styled(({children, ...props}) => (
     <PanelBody>{children}</PanelBody>
   </Panel>
 ))`
+  overflow-x: auto;
   z-index: ${Z_INDEX_PANEL};
 `;
 