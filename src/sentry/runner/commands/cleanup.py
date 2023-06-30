1f4634e0fc07464468618937a1248d6679086a39
Hubert Deng <hubertdeng123@gmail.com>
2023-06-30 13:13:59 -0700

    tweak comment about skipping cleanup of certain models

--- src/sentry/runner/commands/cleanup.py
+++ src/sentry/runner/commands/cleanup.py
@@ -260,7 +260,7 @@ def cleanup(days, project, concurrency, silent, model, router, timed):
         if project:
             click.echo("Bulk NodeStore deletion not available for project selection", err=True)
             project_id = get_project(project)
-            # Metrics span across projects, so skip these models
+            # These models span across projects, so let's skip them
             BULK_QUERY_DELETES.remove((metrics_indexer_models.StringIndexer, "last_seen", None))
             BULK_QUERY_DELETES.remove((metrics_indexer_models.PerfStringIndexer, "last_seen", None))
             DELETES.remove((models.ArtifactBundle, "date_added", "date_added"))
