911a65166ab244ac2a766edf73c6d2c2f216f3ca
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-06-30 14:36:38 -0400

    revert removal

--- src/sentry/monitors/endpoints/monitor_ingest_checkin_index.py
+++ src/sentry/monitors/endpoints/monitor_ingest_checkin_index.py
@@ -86,6 +86,7 @@ class MonitorIngestCheckInIndexEndpoint(MonitorIngestEndpoint):
         project: Project,
         monitor_slug: str,
         monitor: Monitor | None,
+        organization_slug: str | None = None,
     ) -> Response:
         """
         Creates a new check-in for a monitor.
