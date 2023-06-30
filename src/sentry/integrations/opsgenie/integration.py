820f8a1a613d09da9b6d85b079999ba058328bd5
Michelle Fu <michelle.fu@sentry.io>
2023-06-30 16:24:35 -0700

    add integration description

---
+++ src/sentry/integrations/opsgenie/integration.py
@@ -0,0 +1,26 @@
+from __future__ import annotations
+
+# from django import forms
+
+# import logging
+# from typing import Any
+
+# from sentry import options
+# from sentry.integrations.base import (
+#     FeatureDescription,
+#     IntegrationFeatures,
+#     IntegrationInstallation,
+#     IntegrationMetadata,
+#     IntegrationProvider,
+# )
+
+# from sentry.models import Integration, OrganizationIntegration
+# from sentry.pipeline import PipelineView
+
+DESCRIPTION = """
+Trigger alerts in Opsgenie from Sentry.
+
+Opsgenie is a cloud-based service for dev & ops teams, providing reliable
+alerts, on-call schedule management and escalations. Opsgenie integrates with
+monitoring tools & services, ensures the right people are notified.
+"""
