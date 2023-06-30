15d29b21e50e77e552b0ac35b5bf10ff8f157e4d
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-06-30 17:04:01 -0400

    ref: type api_gateway.proxy and its test (#52046)
    <!-- Describe your PR here. -->

--- src/sentry/testutils/helpers/response.py
+++ src/sentry/testutils/helpers/response.py
@@ -1,11 +1,15 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+
 from django.http.response import StreamingHttpResponse
 
 
-def close_streaming_response(response: StreamingHttpResponse) -> None:
+def close_streaming_response(response: StreamingHttpResponse) -> bytes:
     """Exhausts the streamed file in a response.
 
     When the file is exahusted, this underlying file descriptor is closed
     avoiding a `ResourceWarning`.
     """
-    for _ in response.streaming_content:
-        pass
+    assert isinstance(response.streaming_content, Iterable)
+    return b"".join(response.streaming_content)
