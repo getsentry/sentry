2c768b6b58c0a7a17dbdc30e09c3d2c4059e286c
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-07-03 16:06:07 +0200

    feat(sdk-crash-detection): Set in app for SDK frames (#52083)
    Set in_app to true only for SDK frames and to false for all others.
    
    Closes #51024

--- tests/sentry/utils/sdk_crashes/test_event_stripper.py
+++ tests/sentry/utils/sdk_crashes/test_event_stripper.py
@@ -161,6 +161,40 @@ class EventStripperTestMixin(BaseEventStripperMixin):
             },
         }
 
+    def test_set_in_app_only_for_sdk_frames(self):
+        frames = get_frames("SentryCrashMonitor_CPPException.cpp", sentry_frame_in_app=False)
+
+        system_frame_in_app = [
+            {
+                "abs_path": "/System/Library/PrivateFrameworks/UIKitCore.framework/UIKitCore",
+                "in_app": True,
+            }
+        ]
+
+        event_data = get_crash_event_with_frames(system_frame_in_app + list(frames))
+
+        event = self.create_event(
+            data=event_data,
+            project_id=self.project.id,
+        )
+
+        stripped_event_data = strip_event_data(event.data, CocoaSDKCrashDetector())
+
+        stripped_frames = get_path(
+            stripped_event_data, "exception", "values", -1, "stacktrace", "frames"
+        )
+
+        for stripped_frame in stripped_frames[0::-1]:
+            assert stripped_frame["in_app"] is False
+
+        cocoa_sdk_frame = stripped_frames[-1]
+        assert cocoa_sdk_frame == {
+            "function": "SentryCrashMonitor_CPPException.cpp",
+            "package": "/private/var/containers/Bundle/Application/59E988EF-46DB-4C75-8E08-10C27DC3E90E/iOS-Swift.app/Frameworks/Sentry.framework/Sentry",
+            "in_app": True,
+            "image_addr": "0x100304000",
+        }
+
     def test_strip_event_data_keeps_exception_stacktrace(self):
         event = self.create_event(
             data=get_crash_event(),
@@ -229,7 +263,7 @@ class EventStripperTestMixin(BaseEventStripperMixin):
         assert cocoa_sdk_frame == {
             "function": "SentryCrashMonitor_CPPException.cpp",
             "package": "/private/var/containers/Bundle/Application/59E988EF-46DB-4C75-8E08-10C27DC3E90E/iOS-Swift.app/Frameworks/Sentry.framework/Sentry",
-            "in_app": False,
+            "in_app": True,
             "image_addr": "0x100304000",
         }
 
