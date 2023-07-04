2172caa50381a658d9099286f809e90506c14437
Eric Hasegawa <erichasegawa1@gmail.com>
2023-07-04 15:00:56 -0700

    feat(auth): Add model for OIDC tokens (#52064)
    Implementing "Login with Sentry" for CodeCov requires us issuing them
    OIDC tokens. This PR creates a model for those tokens and a means to get
    a symmetrically signed JWT that can be sent to the client.
    
    OpenID Connect can take some time to understand, if you're curious or
    want to understand what this is doing better, you can check out the full
    core spec [here](https://openid.net/specs/openid-connect-core-1_0.html),
    and Auth0 has a good crash course on OIDC
    [here](https://auth0.com/resources/ebooks/the-openid-connect-handbook).
    
    I am not sure how we handle secrets in production (will get help on this
    ASAP) - and it's HS256 encrypted so we'll need one to decode it. I
    figured HS256 is fine since the main reason people would use an
    asymmetric encryption method like RS256 would be to avoid sharing
    secrets, but since the relying party (RP) in our situation is CodeCov, I
    figured it'd be fine.
    
    Also @getsentry/security I'd really appreciate some eyes on this just to
    be on the safe side. It's a pretty simple/direct implementation but I'd
    rather err on the side of caution :)
    
    Final note - this is just a first iteration, soon I'll add in more
    fields like `auth_time`, `acr`, `amr`, `azp`, `at_hash`, `c_hash` etc.

---
+++ tests/sentry/web/frontend/test_openidtoken.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from sentry.testutils import TestCase
+from sentry.utils import jwt as jwt_utils
+from sentry.web.frontend.openidtoken import OpenIDToken
+
+
+class OpenIDTokenTest(TestCase):
+    def test_get_encrypted_id_token(self):
+        id_token = OpenIDToken("ex_client_id", self.user.id, nonce="abcd")
+        encrypted_id_token = id_token.get_encrypted_id_token()
+        assert encrypted_id_token.count(".") == 2
+
+        decrypted_id_token = jwt_utils.decode(encrypted_id_token, "secret", audience="ex_client_id")
+
+        now = datetime.now()
+        current_timestamp = datetime.timestamp(now)
+
+        assert decrypted_id_token["aud"] == "ex_client_id"
+        assert decrypted_id_token["iss"] == "https://sentry.io"
+        assert decrypted_id_token["nonce"] == "abcd"
+        assert isinstance(decrypted_id_token["sub"], int)
+        assert decrypted_id_token["exp"] > current_timestamp
+        assert decrypted_id_token["iat"] < current_timestamp
