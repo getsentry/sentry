1e0f1ba9511f4fb0b556d7c001ef05889be0a3b2
Athena Moghaddam <athena.moghaddam@sentry.io>
2023-07-05 16:04:46 -0700

    redirect instead of calling view

--- src/sentry/web/frontend/auth_organization_id_login.py
+++ src/sentry/web/frontend/auth_organization_id_login.py
@@ -1,4 +1,3 @@
-from django.db import transaction
 from django.http import HttpResponse
 from django.urls import reverse
 from django.views.decorators.cache import never_cache
@@ -11,18 +10,20 @@ from sentry.web.frontend.auth_organization_login import AuthOrganizationLoginVie
 
 class AuthOrganizationIdentifierLoginView(AuthOrganizationLoginView):
     @never_cache
-    @transaction.atomic
     def handle(self, request: Request, organization_id) -> HttpResponse:
         organization_context = organization_service.get_organization_by_id(
             id=organization_id,
         )
 
+        if organization_context is None:
+            return self.redirect(reverse("sentry-login"))
+
         if request.user.is_authenticated:
             next_uri = self.get_next_uri(request)
             if is_valid_redirect(next_uri, allowed_hosts=(request.get_host())):
                 return self.redirect(next_uri)
-            else:
-                return self.redirect(reverse("issues"))
-        return AuthOrganizationLoginView.handle(
-            self, request, organization_slug=organization_context.organization.slug
+            return self.redirect(reverse("issues"))
+
+        return self.redirect(
+            reverse("sentry-auth-organization", args=[organization_context.organization.slug])
         )
