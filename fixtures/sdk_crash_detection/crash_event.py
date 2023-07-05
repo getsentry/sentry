7bfc4889f542fd921b188be7fba52e4afaa6e13b
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-07-05 12:22:18 +0200

    feat(sdk-crash): Require sentry-cocoa 8.2.0 (#52247)
    The CocoaSDKCrashDetector requires minimum sentry-cocoa 8.2.0 to work
    properly, cause only since that version the SDK sends the full paths
    required for detecting system frames in is_system_library_frame.
    
    ---------
    
    Co-authored-by: Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>

--- fixtures/sdk_crash_detection/crash_event.py
+++ fixtures/sdk_crash_detection/crash_event.py
@@ -232,7 +232,7 @@ def get_crash_event_with_frames(
         "environment": "test-app",
         "sdk": {
             "name": "sentry.cocoa",
-            "version": "8.1.0",
+            "version": "8.2.0",
             "integrations": [
                 "Crash",
                 "PerformanceTracking",
