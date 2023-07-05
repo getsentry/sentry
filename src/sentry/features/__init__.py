baf6b2ace0dd2c6f2f27052cbb6e072248fff3bb
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2023-07-05 09:35:55 +0200

    ref(inbound-filters) Remove unused inbound filter health-check feature (#52099)

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -254,7 +254,6 @@ default_manager.add("organizations:codecov-integration", OrganizationFeature, Fe
 default_manager.add("organizations:codecov-commit-sha-from-git-blame", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:ds-sliding-window", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:ds-sliding-window-org", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
-default_manager.add("organizations:health-check-filter", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:pr-comment-bot", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:ds-org-recalibration", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 
