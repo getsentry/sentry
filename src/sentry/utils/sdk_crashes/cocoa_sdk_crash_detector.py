92cdc94be306348d707e10adb45e7337443f2423
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-07-05 10:50:04 +0200

    feat(sdk-crash): Require sentry-cocoa 8.2.0
    The CocoaSDKCrashDetector requires minimum sentry-cocoa 8.2.0 to work
    properly, cause only since that version the SDK sends the full paths required
    for detecting system frames in is_system_library_frame.

--- src/sentry/utils/sdk_crashes/cocoa_sdk_crash_detector.py
+++ src/sentry/utils/sdk_crashes/cocoa_sdk_crash_detector.py
@@ -1,10 +1,44 @@
 from typing import Any, Mapping, Sequence
 
+from packaging.version import InvalidVersion, Version
+
+from sentry.db.models import NodeData
 from sentry.utils.glob import glob_match
+from sentry.utils.safe import get_path
 from sentry.utils.sdk_crashes.sdk_crash_detector import SDKCrashDetector
 
 
 class CocoaSDKCrashDetector(SDKCrashDetector):
+    def should_detect_sdk_crash(self, event_data: NodeData) -> bool:
+        sdk_name = get_path(event_data, "sdk", "name")
+        if sdk_name and sdk_name != "sentry.cocoa":
+            return False
+
+        sdk_version = get_path(event_data, "sdk", "version")
+        if not sdk_version:
+            return False
+
+        try:
+            # Since changing the debug image type to macho (https://github.com/getsentry/sentry-cocoa/pull/2701)
+            # released in sentry-cocoa 8.2.0 (https://github.com/getsentry/sentry-cocoa/blob/main/CHANGELOG.md#820),
+            # the frames contain the full paths required for detecting system frames in is_system_library_frame.
+            # Therefore, we require at least sentry-cocoa 8.2.0.
+            minimum_cocoa_sdk_version = Version("8.2.0")
+            cocoa_sdk_version = Version(sdk_version)
+
+            if cocoa_sdk_version < minimum_cocoa_sdk_version:
+                return False
+        except InvalidVersion:
+            return False
+
+        is_unhandled = (
+            get_path(event_data, "exception", "values", -1, "mechanism", "handled") is False
+        )
+        if is_unhandled is False:
+            return False
+
+        return True
+
     def is_sdk_crash(self, frames: Sequence[Mapping[str, Any]]) -> bool:
         if not frames:
             return False
