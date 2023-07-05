5ca013e6f093d3501b074a26a3f5dd4f5a5edd33
schew2381 <seiji.chew@sentry.io>
2023-07-05 15:35:22 -0700

    feat(api): Document add/remove team to/from project

--- tests/sentry/api/endpoints/test_project_team_details.py
+++ tests/sentry/api/endpoints/test_project_team_details.py
@@ -34,12 +34,14 @@ class ProjectTeamDetailsPostTest(ProjectTeamDetailsTest):
     def test_add_team_not_found(self):
         project = self.create_project()
 
-        self.get_error_response(
+        response = self.get_error_response(
             project.organization.slug,
             project.slug,
             "not-a-team",
             status_code=status.HTTP_404_NOT_FOUND,
         )
+
+        assert response.data["detail"] == "Team does not exist."
 
     @with_feature("organizations:team-roles")
     def test_add_team_with_team_role(self):
@@ -131,12 +133,14 @@ class ProjectTeamDetailsDeleteTest(ProjectTeamDetailsTest):
     def test_remove_team_not_found(self):
         project = self.create_project()
 
-        self.get_error_response(
+        response = self.get_error_response(
             project.organization.slug,
             project.slug,
             "not-a-team",
             status_code=status.HTTP_404_NOT_FOUND,
         )
+
+        assert response.data["detail"] == "Team does not exist."
 
     @with_feature("organizations:team-roles")
     def test_remove_team_with_team_role(self):
