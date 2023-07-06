646f09e01dd007119fcac49d780431f5f7ce12f8
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-07-06 10:42:43 -0700

    ✨ Setup parser

--- src/sentry/middleware/integrations/parsers/base.py
+++ src/sentry/middleware/integrations/parsers/base.py
@@ -11,7 +11,7 @@ from rest_framework import status
 
 from sentry.models.integrations import Integration
 from sentry.models.integrations.organization_integration import OrganizationIntegration
-from sentry.models.outbox import ControlOutbox, WebhookProviderIdentifier
+from sentry.models.outbox import ControlOutbox, WebhookProviderIdentifier, outbox_context
 from sentry.services.hybrid_cloud.organization import RpcOrganizationSummary
 from sentry.services.hybrid_cloud.organization_mapping import organization_mapping_service
 from sentry.silo import SiloLimit, SiloMode
@@ -114,12 +114,15 @@ class BaseRequestParser(abc.ABC):
         Used to create outboxes for provided regions to handle the webhooks asynchronously.
         Responds to the webhook provider with a 202 Accepted status.
         """
-        for outbox in ControlOutbox.for_webhook_update(
-            webhook_identifier=self.webhook_identifier,
-            region_names=[region.name for region in regions],
-            request=self.request,
-        ):
-            outbox.save()
+        if len(regions) > 0:
+            with outbox_context():
+                for outbox in ControlOutbox.for_webhook_update(
+                    webhook_identifier=self.webhook_identifier,
+                    region_names=[region.name for region in regions],
+                    request=self.request,
+                ):
+                    outbox.save()
+
         return HttpResponse(status=status.HTTP_202_ACCEPTED)
 
     # Required Overrides
--- src/sentry/middleware/integrations/parsers/bitbucket.py
+++ src/sentry/middleware/integrations/parsers/bitbucket.py
@@ -2,10 +2,11 @@ from __future__ import annotations
 
 import logging
 
+from sentry.integrations.bitbucket import BitbucketWebhookEndpoint
 from sentry.middleware.integrations.parsers.base import BaseRequestParser
-from sentry.models.integrations.integration import Integration
+from sentry.models.organizationmapping import OrganizationMapping
 from sentry.models.outbox import WebhookProviderIdentifier
-from sentry.services.hybrid_cloud.util import control_silo_function
+from sentry.types.region import get_region_by_name
 
 logger = logging.getLogger(__name__)
 
@@ -14,9 +15,32 @@ class BitbucketRequestParser(BaseRequestParser):
     provider = "bitbucket"
     webhook_identifier = WebhookProviderIdentifier.BITBUCKET
 
-    @control_silo_function
-    def get_integration_from_request(self) -> Integration | None:
-        pass
+    def get_bitbucket_webhook_response(self):
+        # The organization is provided in the path, so we can skip inferring organizations
+        # from the integration credentials
+        organization_id = self.match.kwargs.get("organization_id")
+        logging_extra = {"path": self.request.path}
+        if not organization_id:
+            logger.info("no_organization_id", extra=logging_extra)
+            return self.get_response_from_control_silo()
+
+        mapping: OrganizationMapping = OrganizationMapping.objects.get(
+            organization_id=organization_id
+        )
+        if not mapping:
+            logging_extra["organization_id"] = organization_id
+            logger.info("no_mapping", extra=logging_extra)
+            return self.get_response_from_control_silo()
+
+        region = get_region_by_name(mapping.region_name)
+        if not region:
+            logging_extra["mapping_id"] = mapping.id
+            logger.info("no_region", extra=logging_extra)
+            return self.get_response_from_control_silo()
+        return self.get_response_from_outbox_creation(regions=[region])
 
     def get_response(self):
+        view_class = self.match.func.view_class  # type: ignore
+        if view_class == BitbucketWebhookEndpoint:
+            return self.get_bitbucket_webhook_response()
         return self.get_response_from_control_silo()
