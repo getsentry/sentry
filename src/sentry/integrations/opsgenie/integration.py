e2359cf3411cb856e0f9274c060e1a517cad0675
Michelle Fu <michelle.fu@sentry.io>
2023-07-06 13:12:35 -0700

    first installation view

--- src/sentry/integrations/opsgenie/integration.py
+++ src/sentry/integrations/opsgenie/integration.py
@@ -3,7 +3,10 @@ from __future__ import annotations
 import logging
 from typing import Any, Mapping, MutableMapping, Sequence
 
+# from django import forms
+from django.http import HttpResponse
 from django.utils.translation import gettext_lazy as _
+from rest_framework.request import Request
 
 from sentry.integrations.base import (
     FeatureDescription,
@@ -13,6 +16,8 @@ from sentry.integrations.base import (
     IntegrationProvider,
 )
 from sentry.pipeline import PipelineView
+from sentry.utils.http import absolute_uri
+from sentry.web.helpers import render_to_response
 
 logger = logging.getLogger("sentry.integrations.opsgenie")
 
@@ -51,6 +56,30 @@ metadata = IntegrationMetadata(
 )
 
 
+class InstallationConfigView(PipelineView):
+    pass
+
+
+class InstallationGuideView(PipelineView):
+    def dispatch(self, request: Request, pipeline) -> HttpResponse:
+        if "completed_installation_guide" in request.GET:
+            return pipeline.next_step()
+        return render_to_response(
+            template="sentry/integrations/opsgenie-config.html",
+            context={
+                "next_url": f'{absolute_uri("/extensions/opsgenie/setup/")}?completed_installation_guide',
+                "setup_values": [
+                    {"label": "Name", "value": "Sentry"},
+                    {
+                        "label": "Access rights",
+                        "value": "Read, Create and update, Delete, Configuration access",
+                    },
+                ],
+            },
+            request=request,
+        )
+
+
 class OpsgenieIntegration(IntegrationInstallation):
     def update_organization_config(self, data: MutableMapping[str, Any]) -> None:
         """
@@ -72,7 +101,7 @@ class OpsgenieIntegrationProvider(IntegrationProvider):
     # requires_feature_flag = True  # limited release
 
     def get_pipeline_views(self) -> Sequence[PipelineView]:
-        return super().get_pipeline_views()
+        return [InstallationGuideView()]
 
     def build_integration(self, state: Mapping[str, Any]) -> Mapping[str, Any]:
         """
