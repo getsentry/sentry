613a1227985c572418ac31a4f2722c61a50d31a5
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-06 14:38:52 -0400

    Add `PageFilters` JS stub
    Returns `PageFilters` objects.

--- fixtures/js-stubs/types.tsx
+++ fixtures/js-stubs/types.tsx
@@ -99,6 +99,7 @@ type TestStubFixtures = {
   OutcomesWithLowProcessedEvents: SimpleStub;
   OutcomesWithReason: SimpleStub;
   OutcomesWithoutClientDiscarded: SimpleStub;
+  PageFilters: OverridableStub;
   PhabricatorCreate: SimpleStub;
   PhabricatorPlugin: SimpleStub;
   PlatformExternalIssue: OverridableStub;
