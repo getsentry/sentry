942d2a7c9d3dd0a0dd607ff164a5c03ff8795f18
Alex Zaslavsky <azaslavsky@users.noreply.github.com>
2023-07-06 13:11:56 -0700

    Remove accidental dup in backup.py

--- src/sentry/runner/commands/backup.py
+++ src/sentry/runner/commands/backup.py
@@ -1,23 +1,15 @@
 from __future__ import annotations
 
-from difflib import unified_diff
 from io import StringIO
-from typing import NewType
 
 import click
 from django.apps import apps
 from django.core import management, serializers
 from django.db import IntegrityError, connection, transaction
-from pydantic import PositiveInt
 
 from sentry.runner.decorators import configuration
-from sentry.utils.json import JSONData, JSONEncoder, better_default_encoder
 
 EXCLUDED_APPS = frozenset(("auth", "contenttypes"))
-INDENT = 2
-JSON_PRETTY_PRINTER = JSONEncoder(
-    default=better_default_encoder, indent=INDENT, ignore_nan=True, sort_keys=True
-)
 
 
 @click.command(name="import")
@@ -184,113 +176,3 @@ def export(dest, silent, indent, exclude):
     serializers.serialize(
         "json", yield_objects(), indent=indent, stream=dest, use_natural_foreign_keys=True
     )
-
-
-ComparatorName = NewType("ComparatorName", str)
-ModelName = NewType("ModelName", str)
-
-
-# TODO(team-ospo/#155): Figure out if we are going to use `pk` as part of the identifier, or some other kind of sequence number internal to the JSON export instead.
-class InstanceID:
-    """Every entry in the generated backup JSON file should have a unique model+pk combination, which serves as its identifier."""
-
-    def __init__(self, model: ModelName, pk: PositiveInt):
-        self.model = model
-        self.pk = pk
-
-    def __eq__(self, other):
-        return (
-            hasattr(other, "model")
-            and self.model == other.model
-            and hasattr(other, "pk")
-            and self.pk == other.pk
-        )
-
-    def __hash__(self):
-        return hash((self.model, self.pk))
-
-    def print(self):
-        return f'InstanceID(model: "{self.model}", pk: {self.pk})'
-
-
-class ComparatorFinding:
-    """Store all information about a single failed matching between expected and actual output."""
-
-    def __init__(self, name: ComparatorName, on: InstanceID, reason: str | None = ""):
-        self.name = name
-        self.on = on
-        self.reason = reason
-
-    def print(self):
-        return f'Finding(\n\tname: "{self.name}",\n\ton: {self.on.print()},\n\treason: {self.reason}\n)'
-
-
-class ComparatorFindings:
-    """A wrapper type for a list of 'ComparatorFinding' which enables pretty-printing in asserts."""
-
-    def __init__(self, findings: list[ComparatorFinding]):
-        self.findings = findings
-
-    def append(self, finding: ComparatorFinding):
-        self.findings.append(finding)
-
-    def assert_on_findings(self):
-        if self.findings:
-            assert False, self.print()
-
-    def print(self):
-        return "\n".join(map(lambda f: f.print(), self.findings))
-
-
-# TODO(team-ospo/#155): Move this out of the test suite, and into its own standalone module, since eventually it will be used to compare live JSON as well.
-def validate(expect: JSONData, actual: JSONData) -> ComparatorFindings:
-    """Ensures that originally imported data correctly matches actual outputted data, and produces a list of reasons why not when it doesn't"""
-
-    def json_lines(obj: JSONData) -> list[str]:
-        """Take a JSONData object and pretty-print it as JSON."""
-
-        return JSON_PRETTY_PRINTER.encode(obj).splitlines()
-
-    findings = ComparatorFindings([])
-    exp_models = {}
-    act_models = {}
-    for model in expect:
-        id = InstanceID(ModelName(model["model"]), PositiveInt(model["pk"]))
-        exp_models[id] = model
-
-    # Ensure that the actual JSON contains no duplicates - we assume that the expected JSON did not.
-    for model in actual:
-        id = InstanceID(ModelName(model["model"]), PositiveInt(model["pk"]))
-        if id in act_models:
-            findings.append(ComparatorFinding(ComparatorName("duplicate_entry"), id))
-        else:
-            act_models[id] = model
-
-    # Report unexpected and missing entries in the actual JSON.
-    act_ids = list(act_models)
-    exp_ids = list(exp_models)
-    for id in act_ids:
-        if id not in exp_ids:
-            del act_models[id]
-            findings.append(ComparatorFinding(ComparatorName("unexpected_entry"), id))
-    for id in exp_ids:
-        if id not in act_ids:
-            del exp_models[id]
-            findings.append(ComparatorFinding(ComparatorName("missing_entry"), id))
-
-    # We only perform custom comparisons and JSON diffs on non-duplicate entries that exist in both
-    # outputs.
-    for id, act in act_models.items():
-        exp = exp_models[id]
-
-        # Finally, perform a diff on the remaining JSON.
-        diff = list(
-            unified_diff(json_lines(exp["fields"]), json_lines(act["fields"]), n=3, lineterm="\n")
-        )
-        if diff:
-            findings.append(
-                ComparatorFinding(ComparatorName("json_diff"), id, "\n    " + "\n    ".join(diff))
-            )
-
-    findings.assert_on_findings()
-    return findings
