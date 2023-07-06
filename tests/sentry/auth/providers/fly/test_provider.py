009147c00e14eb00ad77a123a0b896003e72f852
Nathan Hsieh <6186377+nhsiehgit@users.noreply.github.com>
2023-07-06 10:26:14 -0700

    nit: Update fly build config (#52301)
    cleans up the build_config method

--- tests/sentry/auth/providers/fly/test_provider.py
+++ tests/sentry/auth/providers/fly/test_provider.py
@@ -27,32 +27,8 @@ class FlyOAuth2ProviderTest(TestCase):
 
     def test_build_config(self):
         provider = self.auth_provider.get_provider()
-        state = {
-            "state": "9da4041848844e8088864eaea3c3a705",
-            "data": {
-                "access_token": "fo1_6xgeCrB8ew8vFQ86vdaakBSFTVDGCzOUvebUbvgPGhI",
-                "token_type": "Bearer",
-                "expires_in": 7200,
-                "refresh_token": "PmUkAB75UPLKGZplERMq8WwOHnsTllZ5HveY4RvNUTk",
-                "scope": "read",
-                "created_at": 1686786353,
-                "user": {
-                    "resource_owner_id": "k9d01lp82rky6vo2",
-                    "scope": ["read"],
-                    "expires_in": 7200,
-                    "application": {"uid": "elMJpuhA5bXbR59ZaKdXrxXGFVKTypGHuJ4h6Rfw1Qk"},
-                    "created_at": 1686786353,
-                    "user_id": "k9d01lp82rky6vo2",
-                    "user_name": "Nathan",
-                    "email": "k9d01lp82rky6vo2@customer.fly.io",
-                    "organizations": [
-                        {"id": "nathans-org", "role": "member"},
-                        {"id": "0vogzmzoj1k5xp29", "role": "admin"},
-                    ],
-                },
-            },
-        }
-        result = provider.build_config(state)
+        resource = {"id": "nathans-org", "role": "member"}
+        result = provider.build_config(resource=resource)
         assert result == {"org": {"id": "nathans-org"}}
 
     def test_build_identity(self):
