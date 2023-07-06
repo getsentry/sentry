de638aff0701c315c2ed09d9b2495fe5f283b19a
Jan Michael Auer <mail@jauer.org>
2023-07-06 14:18:50 +0200

    fix: Make tests work again

--- tests/sentry/search/events/builder/test_metrics.py
+++ tests/sentry/search/events/builder/test_metrics.py
@@ -24,7 +24,7 @@ from sentry.snuba.dataset import Dataset
 from sentry.snuba.metrics.extraction import QUERY_HASH_KEY
 from sentry.testutils.cases import MetricsEnhancedPerformanceTestCase
 
-# pytestmark = pytest.mark.sentry_metrics
+pytestmark = pytest.mark.sentry_metrics
 
 
 def _metric_percentile_definition(
