7fbda4021801bece54c924e74c1ad4fe5283c71f
Alberto Leal <mail4alberto@gmail.com>
2023-07-06 12:15:51 -0400

    VstsRequestParser

--- src/sentry/middleware/integrations/parsers/vsts.py
+++ src/sentry/middleware/integrations/parsers/vsts.py
@@ -1,2 +1,40 @@
-class VstsRequestParser:
-    pass
+from __future__ import annotations
+
+import logging
+
+from django.http import HttpResponse
+
+from sentry.integrations.vsts.webhooks import VstsWebhookMixin, WorkItemWebhook
+from sentry.middleware.integrations.parsers.base import BaseRequestParser
+from sentry.models.integrations.integration import Integration
+from sentry.models.outbox import WebhookProviderIdentifier
+from sentry.services.hybrid_cloud.util import control_silo_function
+
+logger = logging.getLogger(__name__)
+
+
+class VstsRequestParser(BaseRequestParser, VstsWebhookMixin):
+    provider = "vsts"
+    webhook_identifier = WebhookProviderIdentifier.VSTS
+
+    region_view_classes = [WorkItemWebhook]
+
+    @control_silo_function
+    def get_integration_from_request(self) -> Integration | None:
+        try:
+            external_id = self.get_external_id(request=self.request)
+        except Exception:
+            return None
+        return Integration.objects.filter(external_id=external_id, provider=self.provider).first()
+
+    def get_response(self) -> HttpResponse:
+        view_class = self.match.func.view_class  # type: ignore
+        if view_class not in self.region_view_classes:
+            return self.get_response_from_control_silo()
+
+        regions = self.get_regions_from_organizations()
+        if len(regions) == 0:
+            logger.error("no_regions", extra={"path": self.request.path})
+            return self.get_response_from_control_silo()
+
+        return self.get_response_from_outbox_creation(regions=regions)
