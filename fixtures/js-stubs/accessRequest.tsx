aa5786d241a294a708f1849d4715fd206a605052
JonasBa <jonas@badalic.com>
2023-07-06 14:45:21 -0400

    ref(tsc): convert some fixtures to tsx

---
+++ fixtures/js-stubs/accessRequest.tsx
@@ -0,0 +1,20 @@
+import {AccessRequest as AccessRequestType} from 'sentry/types';
+
+import {Member} from './member';
+import {Team} from './team';
+
+export function AccessRequest(
+  params: Partial<AccessRequestType> = {}
+): AccessRequestType {
+  return {
+    id: '123',
+    member: Member(),
+    team: Team(),
+    requester: params.requester ?? {
+      email: 'requester@email.com',
+      name: 'requester',
+      username: 'requester_username',
+    },
+    ...params,
+  };
+}
