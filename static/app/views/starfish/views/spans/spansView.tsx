121e3c9bd5e5938268e86db10c99ad6c67bae70c
Ash Anand <0Calories@users.noreply.github.com>
2023-07-07 10:53:38 -0400

    feat(starfish): Add service selector component again (#52313)

--- static/app/views/starfish/views/spans/spansView.tsx
+++ static/app/views/starfish/views/spans/spansView.tsx
@@ -2,11 +2,13 @@ import {Fragment} from 'react';
 import styled from '@emotion/styled';
 import pick from 'lodash/pick';
 
+import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import {space} from 'sentry/styles/space';
 import {fromSorts} from 'sentry/utils/discover/eventView';
 import type {Sort} from 'sentry/utils/discover/fields';
 import {useLocation} from 'sentry/utils/useLocation';
 import StarfishDatePicker from 'sentry/views/starfish/components/datePicker';
+import {StarfishProjectSelector} from 'sentry/views/starfish/components/starfishProjectSelector';
 import {ModuleName} from 'sentry/views/starfish/types';
 import {QueryParameterNames} from 'sentry/views/starfish/views/queryParameters';
 import {ActionSelector} from 'sentry/views/starfish/views/spans/selectors/actionSelector';
@@ -50,9 +52,10 @@ export default function SpansView(props: Props) {
 
   return (
     <Fragment>
-      <FilterOptionsContainer>
+      <StyledPageFilterBar condensed>
+        <StarfishProjectSelector />
         <StarfishDatePicker />
-      </FilterOptionsContainer>
+      </StyledPageFilterBar>
 
       <PaddedContainer>
         <SpanTimeCharts
@@ -101,5 +104,10 @@ const FilterOptionsContainer = styled(PaddedContainer)`
   display: flex;
   flex-direction: row;
   gap: ${space(1)};
+  margin-bottom: ${space(2)};
+`;
+
+const StyledPageFilterBar = styled(PageFilterBar)`
+  margin: 0 ${space(2)};
   margin-bottom: ${space(2)};
 `;
