84cb206403f1fc2291b1fbc8750b3da31b446631
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-07-07 12:52:17 +0200

    fix test for getsentry

--- static/app/data/platforms.tsx
+++ static/app/data/platforms.tsx
@@ -10,9 +10,9 @@ const migratedJavascriptPlatforms = {
   id: 'javascript',
   name: 'Browser JavaScript',
   integrations: [
-    ...integrationDocsPlatforms.platforms
-      .filter(platform => platform.id === 'javascript')[0]
-      .integrations.filter(integration => integration.id !== 'javascript-react'),
+    ...(integrationDocsPlatforms.platforms
+      .filter(platform => platform.id === 'javascript')?.[0]
+      ?.integrations?.filter(integration => integration.id !== 'javascript-react') ?? []),
     {
       id: 'javascript-react',
       link: 'https://docs.sentry.io/platforms/javascript/guides/react/',
@@ -36,7 +36,9 @@ const otherPlatform = {
 };
 
 const platformIntegrations: PlatformIntegration[] = [
-  ...integrationDocsPlatforms.platforms.filter(platform => platform.id !== 'javascript'),
+  ...(integrationDocsPlatforms.platforms.filter(
+    platform => platform.id !== 'javascript'
+  ) ?? []),
   migratedJavascriptPlatforms,
   otherPlatform,
 ]
