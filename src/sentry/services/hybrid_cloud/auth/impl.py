c8c82591b55b98a2f7bfd02ab6543cc652d8cfbf
Mark Story <mark@mark-story.com>
2023-07-07 11:13:55 -0400

    Restore connection management with db selection
    Without this logic acceptance tests all failed.

--- src/sentry/services/hybrid_cloud/auth/impl.py
+++ src/sentry/services/hybrid_cloud/auth/impl.py
@@ -4,6 +4,7 @@ import base64
 from typing import Any, List, Mapping
 
 from django.contrib.auth.models import AnonymousUser
+from django.db import connections, router, transaction
 from django.db.models import Count, F, Q
 from django.http import HttpResponse
 
@@ -40,6 +41,7 @@ from sentry.services.hybrid_cloud.organization import (
 )
 from sentry.services.hybrid_cloud.user import RpcUser
 from sentry.services.hybrid_cloud.user.service import user_service
+from sentry.silo.base import SiloMode
 from sentry.utils.auth import AuthUserPasswordExpired
 
 _SSO_BYPASS = RpcMemberSsoState(is_required=False, is_valid=True)
@@ -189,7 +191,11 @@ class DatabaseBackedAuthService(AuthService):
             result.user = self._load_auth_user(expired_user)
             result.expired = True
         elif fake_request.user is not None and not fake_request.user.is_anonymous:
-            result.user = self._load_auth_user(fake_request.user)
+            with transaction.atomic(using=router.db_for_read(User)):
+                result.user = self._load_auth_user(fake_request.user)
+                transaction.set_rollback(True)
+            if SiloMode.single_process_silo_mode():
+                connections.close_all()
 
         return result
 
