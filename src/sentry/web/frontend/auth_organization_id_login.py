bd180d7d8114b602e8edfb84882164be43153178
Athena Moghaddam <132939361+sentaur-athena@users.noreply.github.com>
2023-07-07 09:37:47 -0700

    feat(auth): Create a homepage url that works with org_id  (#52053)
    It turns out it's not a good idea to have a mutable identifier like
    slug. For example, in fly.io partnership, fly needs to send their
    customers to the customer's sentry org and they need something immutable
    to store in their databse to use in their product. I'm suggesting they
    store id and we make our first entrypoint work with both slug and id.
    
    Some possible questions you might have?
    Q. Why change this in auth endpoint and not issues endpoint?
    A. Because any endpoint for not logged in user lands on auth/login with
    a redirect url. With putting this code in auth we fixed it for every
    endpoint rather than just one.
    
    Q. Why not make the existing endpoint work for id?
    A. See previous conversation but basically because there are orgs with
    numerical slugs 🤦‍♀️
    
    Test plan:
    Added integration test for the redirects

---
+++ src/sentry/web/frontend/auth_organization_id_login.py
@@ -0,0 +1,31 @@
+from django.urls import reverse
+from django.views.decorators.cache import never_cache
+
+from sentry.models.organization import Organization
+from sentry.services.hybrid_cloud.organization import organization_service
+from sentry.utils.auth import is_valid_redirect
+from sentry.web.frontend.auth_organization_login import AuthOrganizationLoginView
+
+
+class AuthOrganizationIdentifierLoginView(AuthOrganizationLoginView):
+    @never_cache
+    def handle(self, request, organization_id):
+        if request.subdomain is not None:
+            return self.redirect(reverse("sentry-auth-organization", args=[request.subdomain]))
+
+        organization_context = organization_service.get_organization_by_id(
+            id=organization_id,
+        )
+
+        if organization_context is None:
+            return self.redirect(reverse("sentry-login"))
+
+        if request.user.is_authenticated:
+            next_uri = self.get_next_uri(request)
+            if is_valid_redirect(next_uri, allowed_hosts=(request.get_host())):
+                return self.redirect(next_uri)
+            return self.redirect(Organization.get_url(slug=organization_context.organization.slug))
+
+        return self.redirect(
+            reverse("sentry-auth-organization", args=[organization_context.organization.slug])
+        )
