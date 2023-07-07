db01d9fa99e0e0072744ba70b63923dd2a5a08e9
JonasBa <jonas@badalic.com>
2023-07-07 15:28:26 -0400

    ref(stubs): refactor authprovider

---
+++ fixtures/js-stubs/authProvider.tsx
@@ -0,0 +1,11 @@
+import type {AuthProvider as AuthProviderType} from 'sentry/types';
+
+export function AuthProvider(params: Partial<AuthProviderType> = {}): AuthProviderType {
+  return {
+    disables2FA: false,
+    key: 'auth_provider_key',
+    name: 'auth_provider_name',
+    requiredFeature: 'auth_provider_required_feature',
+    ...params,
+  };
+}
