c1de59037c2501aa080e0b22524b90625d85fc61
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-07-10 13:04:41 -0700

    feature flag default on

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1298,7 +1298,7 @@ SENTRY_FEATURES = {
     # Enables tagging javascript errors from the browser console.
     "organizations:javascript-console-error-tag": False,
     # Enables separate filters for user and user's teams
-    "organizations:assign-to-me": False,
+    "organizations:assign-to-me": True,
     # Enables the cron job to auto-enable codecov integrations.
     "organizations:auto-enable-codecov": False,
     # The overall flag for codecov integration, gated by plans.
