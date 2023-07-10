83cd1041ca62e3ea23f48dbd15008679f3b15425
Alberto Leal <mail4alberto@gmail.com>
2023-07-10 17:41:28 -0400

    region global configuration validation

--- src/sentry/types/region.py
+++ src/sentry/types/region.py
@@ -1,11 +1,13 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 from enum import Enum
-from typing import Any, Collection, Container, Iterable, Set
+from typing import Any, Collection, Container, Iterable, List, Optional, Set
 from urllib.parse import urljoin
 
+import sentry_sdk
 from django.conf import settings
+from pydantic.dataclasses import dataclass
+from pydantic.tools import parse_obj_as
 
 from sentry.services.hybrid_cloud.util import control_silo_function
 from sentry.silo import SiloMode
@@ -55,7 +57,7 @@ class Region:
     """The region's category."""
 
     # TODO: Possibly change auth schema in final implementation.
-    api_token: str | None = None
+    api_token: Optional[str] = None
 
     def validate(self) -> None:
         from sentry import options
@@ -117,7 +119,8 @@ class GlobalRegionDirectory:
 
 def _parse_config(region_config: Any) -> Iterable[Region]:
     if isinstance(region_config, (str, bytes)):
-        config_values = json.loads(region_config)
+        json_config_values = json.loads(region_config)
+        config_values = parse_obj_as(List[Region], json_config_values)
     else:
         config_values = region_config
 
@@ -133,8 +136,12 @@ def _parse_config(region_config: Any) -> Iterable[Region]:
 
 
 def load_from_config(region_config: Any) -> GlobalRegionDirectory:
-    region_objs = list(_parse_config(region_config))
-    return GlobalRegionDirectory(region_objs)
+    try:
+        region_objs = list(_parse_config(region_config))
+        return GlobalRegionDirectory(region_objs)
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
+        raise RegionConfigurationError("Unable to parse region_config.")
 
 
 _global_regions: GlobalRegionDirectory | None = None
