2f63d948adf7cc92f16d703aef0133bb87282628
Jodi Jang <jodi.jang@sentry.io>
2023-07-10 09:47:33 -0700

    ref: Remove unused functions

--- tests/sentry/issues/test_merge.py
+++ tests/sentry/issues/test_merge.py
@@ -1,13 +1,10 @@
-from datetime import datetime, timedelta
-from typing import Any, List
+from typing import Any
 from unittest.mock import patch
 
 import pytest
 import rest_framework
 
-from sentry.issues.escalating import GroupsCountResponse
 from sentry.issues.escalating_group_forecast import EscalatingGroupForecast
-from sentry.issues.escalating_issues_alg import GroupCount, generate_issue_forecast
 from sentry.issues.forecasts import generate_and_save_forecasts
 from sentry.issues.grouptype import PerformanceNPlusOneGroupType
 from sentry.issues.merge import handle_merge
@@ -27,43 +24,6 @@ class HandleIssueMergeTest(TestCase):
             group = self.create_group()
             add_group_to_inbox(group, GroupInboxReason.NEW)
             self.groups.append(group)
-
-    def mock_get_query_past_counts(
-        self, groups: List[Group], start_time: datetime, count: int = 200, num_days=8
-    ) -> List[GroupsCountResponse]:
-        """
-        Return a mock past counts query response with count events per day for the past num_days.
-        """
-        mock_past_counts = []
-        for group in groups:
-            for i in range(num_days, -1, -1):
-                past_count: GroupsCountResponse = {
-                    "project_id": self.project.id,
-                    "group_id": group.id,
-                    "hourBucket": (start_time - timedelta(i)).strftime("%Y-%m-%dT%H:%M:%S%f%z")
-                    + "+00:00",
-                    "count()": count,
-                }
-                mock_past_counts.append(past_count)
-        return mock_past_counts
-
-    def get_expected_primary_group_forecast(
-        self, start_time: datetime, num_groups: int, count: int = 200, num_days=8
-    ) -> List[int]:
-        """
-        Return the expected primary group forecast, given the number of merged groups, event count,
-        and number of days.
-        """
-        intervals = []
-        data = []
-        for i in range(num_days, -1, -1):
-            intervals.append(
-                (start_time - timedelta(i)).strftime("%Y-%m-%dT%H:%M:%S%f%z") + "+00:00"
-            )
-            data.append(count * num_groups)
-        merged_past_counts: GroupCount = {"intervals": intervals, "data": data}
-        primary_group_forecast = generate_issue_forecast(merged_past_counts, start_time)
-        return [forecast["forecasted_value"] for forecast in primary_group_forecast]
 
     @patch("sentry.tasks.merge.merge_groups.delay")
     def test_handle_merge(self, merge_groups: Any) -> None:
