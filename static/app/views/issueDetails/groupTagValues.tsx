d0492d6817a3b300e2fe0c722e2062345a9320ea
Malachi Willey <malwilley@gmail.com>
2023-07-10 13:30:29 -0700

    ref(query-client): Deprecate AsyncComponent and AsyncView (#52550)
    `AsyncComponent` and `AsyncView` are no longer recommended for future
    use. They require class components (use functional components instead!),
    depend on inheritance, and do not include request caching.
    
    The [docs page on network
    requests](https://develop.sentry.dev/frontend/network-requests/#quick-start)
    should have all the info necessary for devs to use `useApiQuery` as an
    alternative.

--- static/app/views/issueDetails/groupTagValues.tsx
+++ static/app/views/issueDetails/groupTagValues.tsx
@@ -2,10 +2,10 @@ import {Fragment} from 'react';
 import {RouteComponentProps, WithRouterProps} from 'react-router';
 import styled from '@emotion/styled';
 
-import AsyncComponent from 'sentry/components/asyncComponent';
 import {Button} from 'sentry/components/button';
 import ButtonBar from 'sentry/components/buttonBar';
 import DataExport, {ExportQueryType} from 'sentry/components/dataExport';
+import DeprecatedAsyncComponent from 'sentry/components/deprecatedAsyncComponent';
 import {DeviceName} from 'sentry/components/deviceName';
 import {DropdownMenu} from 'sentry/components/dropdownMenu';
 import GlobalSelectionLink from 'sentry/components/globalSelectionLink';
@@ -56,11 +56,11 @@ type State = {
 
 const DEFAULT_SORT = 'count';
 
-class GroupTagValues extends AsyncComponent<
-  Props & AsyncComponent['props'] & WithRouterProps,
-  State & AsyncComponent['state']
+class GroupTagValues extends DeprecatedAsyncComponent<
+  Props & DeprecatedAsyncComponent['props'] & WithRouterProps,
+  State & DeprecatedAsyncComponent['state']
 > {
-  getEndpoints(): ReturnType<AsyncComponent['getEndpoints']> {
+  getEndpoints(): ReturnType<DeprecatedAsyncComponent['getEndpoints']> {
     const {environments: environment} = this.props;
     const {groupId, tagKey} = this.props.params;
     return [
