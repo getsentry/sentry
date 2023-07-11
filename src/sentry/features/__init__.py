9d17aba9d06d8e4f1ddf9aba612bfa480eae69c7
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-07-11 16:27:06 -0700

    feat(replay): Configure a feature flag for session-replay-trace-table (#52688)

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -179,6 +179,7 @@ default_manager.add("organizations:session-replay-sdk-errors-only", Organization
 default_manager.add("organizations:session-replay-recording-scrubbing", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay-weekly-email", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay-issue-emails", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
+default_manager.add("organizations:session-replay-trace-table", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:set-grouping-config", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:slack-overage-notifications", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:sdk-crash-detection", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
