896d5ee86dfc094778c7921b4b8e17efeab13967
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-11 14:28:56 -0400

    feat(starfish): Allow Snuba (#52636)
    Self-explanatory. Ingest-side PR is separate.

--- static/app/views/starfish/allowedProjects.ts
+++ static/app/views/starfish/allowedProjects.ts
@@ -1,7 +1,7 @@
 export const ALLOWED_PROJECT_IDS_FOR_ORG_SLUG: {
   [slug: string]: string[];
 } = {
-  sentry: ['1'],
+  sentry: ['1', '300688'],
   codecov: ['5215654'],
   peated: ['4504120414765056'],
 };
