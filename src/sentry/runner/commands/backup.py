e91cdaea4e5b125dbfcd1fa5c39888de8c2c11e6
Alex Zaslavsky <azaslavsky@users.noreply.github.com>
2023-07-11 09:43:58 -0700

    feat(backup): Support alert rules
    Adds a number of tests for exportable AlertRule* Sentry models, which
    necessitates testing for SnubaQuery* and QuerySubscription models as
    well, as these are included in AlertRule instances by default.
    
    Issue: getsentry/team-ospo#156

--- src/sentry/runner/commands/backup.py
+++ src/sentry/runner/commands/backup.py
@@ -150,6 +150,8 @@ class DateUpdatedComparator(JSONScrubbingComparator):
 ComparatorList = List[JSONScrubbingComparator]
 ComparatorMap = Dict[str, ComparatorList]
 DEFAULT_COMPARATORS: ComparatorMap = {
+    "sentry.alertrule": [DateUpdatedComparator("date_modified")],
+    "sentry.querysubscription": [DateUpdatedComparator("date_updated")],
     "sentry.userrole": [DateUpdatedComparator("date_updated")],
     "sentry.userroleuser": [DateUpdatedComparator("date_updated")],
 }
