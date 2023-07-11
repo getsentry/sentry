3a02e2d9dbf7b32946dc2000a08a21e4893140c8
Alberto Leal <mail4alberto@gmail.com>
2023-07-10 20:24:39 -0400

    add type annotation to date_added

--- src/sentry/services/hybrid_cloud/lost_password_hash/model.py
+++ src/sentry/services/hybrid_cloud/lost_password_hash/model.py
@@ -5,6 +5,9 @@
 
 import datetime
 
+from django.utils import timezone
+from pydantic.fields import Field
+
 from sentry.models import LostPasswordHash
 from sentry.services.hybrid_cloud import RpcModel
 
@@ -13,7 +16,7 @@ class RpcLostPasswordHash(RpcModel):
     id: int = -1
     user_id: int = -1
     hash: str = ""
-    date_added = datetime.datetime
+    date_added: datetime = Field(default_factory=timezone.now)
 
     def get_absolute_url(self, mode: str = "recover") -> str:
         return LostPasswordHash.get_lostpassword_url(self.user_id, self.hash, mode)
