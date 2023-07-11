8047740ab67c256dcc78a7d57e19b2bfcde42bf6
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-07-11 14:54:27 +0200

    ref(getting-started-docs): Add configure source maps step to gatsby

--- static/app/gettingStartedDocs/javascript/gatsby.tsx
+++ static/app/gettingStartedDocs/javascript/gatsby.tsx
@@ -1,5 +1,6 @@
 import {Layout, LayoutProps} from 'sentry/components/onboarding/gettingStartedDoc/layout';
 import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/step';
+import {getUploadSourceMapsStep} from 'sentry/components/onboarding/gettingStartedDoc/utils';
 import {ProductSolution} from 'sentry/components/onboarding/productSelection';
 import {t, tct} from 'sentry/locale';
 
@@ -92,6 +93,9 @@ export const steps = ({
       },
     ],
   },
+  getUploadSourceMapsStep(
+    'https://docs.sentry.io/platforms/javascript/guides/gatsby/sourcemaps/'
+  ),
   {
     language: 'javascript',
     type: StepType.VERIFY,
