6fcfe4342315ac60e1575831aa73ef3c227063ee
Arpad Borsos <arpad.borsos@sentry.io>
2023-07-11 16:57:36 +0200

    Simplify Debug File renewal code (#52623)
    This removes the feature flag that was just there temporarily when
    landing these features.
    
    Also simplifies the code a bit, and fixes the case where an UPDATE query
    was issued for debug file refresh even though the corresponding SELECT
    did not yield anything.

--- src/sentry/debug_files/artifact_bundles.py
+++ src/sentry/debug_files/artifact_bundles.py
@@ -9,7 +9,6 @@ from django.db import router
 from django.db.models import Count
 from django.utils import timezone
 
-from sentry import options
 from sentry.models.artifactbundle import (
     INDEXING_THRESHOLD,
     ArtifactBundle,
@@ -170,12 +169,6 @@ def _index_urls_in_bundle(
 
 
 def maybe_renew_artifact_bundles(used_artifact_bundles: Dict[int, datetime]):
-    if options.get("sourcemaps.artifact-bundles.enable-renewal") == 1.0:
-        with metrics.timer("artifact_bundle_renewal"):
-            renew_artifact_bundles(used_artifact_bundles)
-
-
-def renew_artifact_bundles(used_artifact_bundles: Dict[int, datetime]):
     # We take a snapshot in time that MUST be consistent across all updates.
     now = timezone.now()
     # We compute the threshold used to determine whether we want to renew the specific bundle.
@@ -185,40 +178,46 @@ def renew_artifact_bundles(used_artifact_bundles: Dict[int, datetime]):
         # We perform the condition check also before running the query, in order to reduce the amount of queries to the database.
         if date_added > threshold_date:
             continue
-        metrics.incr("artifact_bundle_renewal.need_renewal")
-        # We want to use a transaction, in order to keep the `date_added` consistent across multiple tables.
-        with atomic_transaction(
-            using=(
-                router.db_for_write(ArtifactBundle),
-                router.db_for_write(ProjectArtifactBundle),
-                router.db_for_write(ReleaseArtifactBundle),
-                router.db_for_write(DebugIdArtifactBundle),
-                router.db_for_write(ArtifactBundleIndex),
-            )
-        ):
-            # We check again for the date_added condition in order to achieve consistency, this is done because
-            # the `can_be_renewed` call is using a time which differs from the one of the actual update in the db.
-            updated_rows_count = ArtifactBundle.objects.filter(
-                id=artifact_bundle_id, date_added__lte=threshold_date
-            ).update(date_added=now)
-            # We want to make cascading queries only if there were actual changes in the db.
-            if updated_rows_count > 0:
-                ProjectArtifactBundle.objects.filter(
-                    artifact_bundle_id=artifact_bundle_id, date_added__lte=threshold_date
-                ).update(date_added=now)
-                ReleaseArtifactBundle.objects.filter(
-                    artifact_bundle_id=artifact_bundle_id, date_added__lte=threshold_date
-                ).update(date_added=now)
-                DebugIdArtifactBundle.objects.filter(
-                    artifact_bundle_id=artifact_bundle_id, date_added__lte=threshold_date
-                ).update(date_added=now)
-                ArtifactBundleIndex.objects.filter(
-                    artifact_bundle_id=artifact_bundle_id, date_added__lte=threshold_date
-                ).update(date_added=now)
 
-        # If the transaction succeeded, and we did actually modify some rows, we want to track the metric.
+        with metrics.timer("artifact_bundle_renewal"):
+            renew_artifact_bundle(artifact_bundle_id, threshold_date, now)
+
+
+def renew_artifact_bundle(artifact_bundle_id: int, threshold_date: datetime, now: datetime):
+    metrics.incr("artifact_bundle_renewal.need_renewal")
+    # We want to use a transaction, in order to keep the `date_added` consistent across multiple tables.
+    with atomic_transaction(
+        using=(
+            router.db_for_write(ArtifactBundle),
+            router.db_for_write(ProjectArtifactBundle),
+            router.db_for_write(ReleaseArtifactBundle),
+            router.db_for_write(DebugIdArtifactBundle),
+            router.db_for_write(ArtifactBundleIndex),
+        )
+    ):
+        # We check again for the date_added condition in order to achieve consistency, this is done because
+        # the `can_be_renewed` call is using a time which differs from the one of the actual update in the db.
+        updated_rows_count = ArtifactBundle.objects.filter(
+            id=artifact_bundle_id, date_added__lte=threshold_date
+        ).update(date_added=now)
+        # We want to make cascading queries only if there were actual changes in the db.
         if updated_rows_count > 0:
-            metrics.incr("artifact_bundle_renewal.were_renewed")
+            ProjectArtifactBundle.objects.filter(
+                artifact_bundle_id=artifact_bundle_id, date_added__lte=threshold_date
+            ).update(date_added=now)
+            ReleaseArtifactBundle.objects.filter(
+                artifact_bundle_id=artifact_bundle_id, date_added__lte=threshold_date
+            ).update(date_added=now)
+            DebugIdArtifactBundle.objects.filter(
+                artifact_bundle_id=artifact_bundle_id, date_added__lte=threshold_date
+            ).update(date_added=now)
+            ArtifactBundleIndex.objects.filter(
+                artifact_bundle_id=artifact_bundle_id, date_added__lte=threshold_date
+            ).update(date_added=now)
+
+    # If the transaction succeeded, and we did actually modify some rows, we want to track the metric.
+    if updated_rows_count > 0:
+        metrics.incr("artifact_bundle_renewal.were_renewed")
 
 
 # ===== Querying of Artifact Bundles =====
