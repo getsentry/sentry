5a1681f9594fbdce0aeb0afc45db6062bfb9d15b
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-07-12 14:52:02 -0700

    fix(hc): Tolerate OrganizationMapping not found when resolving region (#52560)
    Introduce the `return_none_if_mapping_not_found` option to the
    `regional_rpc_method`, which causes the decorated method to return
    `None` indicating the queried object does not exist if we cannot find a
    region in which to look for it in the first place.

--- src/sentry/services/hybrid_cloud/region.py
+++ src/sentry/services/hybrid_cloud/region.py
@@ -2,20 +2,21 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import TYPE_CHECKING
+from typing import Any
 
 from django.conf import settings
 
 from sentry.services.hybrid_cloud import ArgumentDict
 from sentry.services.hybrid_cloud.rpc import RpcServiceUnimplementedException
-from sentry.types.region import Region, RegionResolutionError, get_region_by_name
+from sentry.types.region import (
+    Region,
+    RegionMappingNotFound,
+    RegionResolutionError,
+    get_region_by_name,
+)
 
-if TYPE_CHECKING:
-    from sentry.db.models import BaseManager
-    from sentry.models import OrganizationMapping
 
-
-class RegionResolution(ABC):
+class RegionResolutionStrategy(ABC):
     """Interface for directing a service call to a remote region."""
 
     @abstractmethod
@@ -24,31 +25,30 @@ class RegionResolution(ABC):
         raise NotImplementedError
 
     @staticmethod
-    def _resolve_from_mapping(mapping: OrganizationMapping) -> Region:
-        return get_region_by_name(mapping.region_name)
-
-    @property
-    def organization_mapping_manager(self) -> BaseManager[OrganizationMapping]:
+    def _get_from_mapping(**query: Any) -> Region:
         from sentry.models import OrganizationMapping
 
-        # Convenience method to avoid repeating the local import
-        return OrganizationMapping.objects
+        try:
+            mapping = OrganizationMapping.objects.get(**query)
+        except OrganizationMapping.DoesNotExist as e:
+            raise RegionMappingNotFound from e
+
+        return get_region_by_name(mapping.region_name)
 
 
 @dataclass(frozen=True)
-class ByOrganizationObject(RegionResolution):
+class ByOrganizationObject(RegionResolutionStrategy):
     """Resolve from a parameter representing an organization object."""
 
     parameter_name: str = "organization"
 
     def resolve(self, arguments: ArgumentDict) -> Region:
         value = arguments[self.parameter_name]
-        mapping = self.organization_mapping_manager.get(organization_id=value.id)
-        return self._resolve_from_mapping(mapping)
+        return self._get_from_mapping(organization_id=value.id)
 
 
 @dataclass(frozen=True)
-class ByRegionName(RegionResolution):
+class ByRegionName(RegionResolutionStrategy):
     """Resolve from an `str` parameter representing a region's name"""
 
     parameter_name: str = "region_name"
@@ -59,31 +59,29 @@ class ByRegionName(RegionResolution):
 
 
 @dataclass(frozen=True)
-class ByOrganizationId(RegionResolution):
+class ByOrganizationId(RegionResolutionStrategy):
     """Resolve from an `int` parameter representing an organization ID."""
 
     parameter_name: str = "organization_id"
 
     def resolve(self, arguments: ArgumentDict) -> Region:
         organization_id = arguments[self.parameter_name]
-        mapping = self.organization_mapping_manager.get(organization_id=organization_id)
-        return self._resolve_from_mapping(mapping)
+        return self._get_from_mapping(organization_id=organization_id)
 
 
 @dataclass(frozen=True)
-class ByOrganizationSlug(RegionResolution):
+class ByOrganizationSlug(RegionResolutionStrategy):
     """Resolve from a `str` parameter representing an organization slug."""
 
     parameter_name: str = "slug"
 
     def resolve(self, arguments: ArgumentDict) -> Region:
         slug = arguments[self.parameter_name]
-        mapping = self.organization_mapping_manager.get(slug=slug)
-        return self._resolve_from_mapping(mapping)
+        return self._get_from_mapping(slug=slug)
 
 
 @dataclass(frozen=True)
-class ByOrganizationIdAttribute(RegionResolution):
+class ByOrganizationIdAttribute(RegionResolutionStrategy):
     """Resolve from an object with an organization ID as one of its attributes."""
 
     parameter_name: str
@@ -92,11 +90,10 @@ class ByOrganizationIdAttribute(RegionResolution):
     def resolve(self, arguments: ArgumentDict) -> Region:
         argument = arguments[self.parameter_name]
         organization_id = getattr(argument, self.attribute_name)
-        mapping = self.organization_mapping_manager.get(organization_id=organization_id)
-        return self._resolve_from_mapping(mapping)
+        return self._get_from_mapping(organization_id=organization_id)
 
 
-class RequireSingleOrganization(RegionResolution):
+class RequireSingleOrganization(RegionResolutionStrategy):
     """Resolve to the only region in a single-organization environment.
 
     Calling a service method with this resolution strategy will cause an error if the
@@ -105,13 +102,13 @@ class RequireSingleOrganization(RegionResolution):
     """
 
     def resolve(self, arguments: ArgumentDict) -> Region:
+        from sentry.models import OrganizationMapping
+
         if not settings.SENTRY_SINGLE_ORGANIZATION:
             raise RegionResolutionError("Method is available only in single-org environment")
 
         all_region_names = list(
-            self.organization_mapping_manager.all()
-            .values_list("region_name", flat=True)
-            .distinct()[:2]
+            OrganizationMapping.objects.all().values_list("region_name", flat=True).distinct()[:2]
         )
         if len(all_region_names) == 0:
             return get_region_by_name(settings.SENTRY_MONOLITH_REGION)
@@ -122,7 +119,7 @@ class RequireSingleOrganization(RegionResolution):
         return get_region_by_name(single_region_name)
 
 
-class UnimplementedRegionResolution(RegionResolution):
+class UnimplementedRegionResolution(RegionResolutionStrategy):
     """Indicate that a method's region resolution logic has not been implemented yet.
 
     A remote call to the method will be interrupted and will default to the
