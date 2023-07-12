91ef2aa3af636fd2d2fb9074e6a14724240314f0
Colleen O'Rourke <colleen@sentry.io>
2023-07-12 16:09:00 -0700

    oop

--- static/app/views/dashboards/datasetConfig/issues.tsx
+++ static/app/views/dashboards/datasetConfig/issues.tsx
@@ -70,15 +70,10 @@ function disableSortOptions(_widgetQuery: WidgetQuery) {
 }
 
 function getTableSortOptions(_organization: Organization, _widgetQuery: WidgetQuery) {
-  const hasBetterPrioritySort = organization.features.includes(
-    'issue-list-better-priority-sort'
-  );
   const sortOptions = [
     IssueSortOptions.DATE,
     IssueSortOptions.NEW,
-    ...(hasBetterPrioritySort
-      ? [IssueSortOptions.BETTER_PRIORITY]
-      : [IssueSortOptions.PRIORITY]), // show better priority for EA orgs
+    IssueSortOptions.BETTER_PRIORITY,
     IssueSortOptions.FREQ,
     IssueSortOptions.USER,
   ];
