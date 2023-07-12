3b8f4cecd8e6e59e69ffd3dd5efde33a49271d73
Malachi Willey <malwilley@gmail.com>
2023-07-12 15:34:13 -0700

    feat(issue-details): Add analytics for clicking open replay from issue details (#52762)

--- static/app/components/events/eventReplay/index.tsx
+++ static/app/components/events/eventReplay/index.tsx
@@ -2,8 +2,10 @@ import {useCallback} from 'react';
 
 import ErrorBoundary from 'sentry/components/errorBoundary';
 import LazyLoad from 'sentry/components/lazyLoad';
-import {Organization} from 'sentry/types';
+import {Group, Organization} from 'sentry/types';
 import {Event} from 'sentry/types/event';
+import {trackAnalytics} from 'sentry/utils/analytics';
+import {getAnalyticsDataForEvent, getAnalyticsDataForGroup} from 'sentry/utils/events';
 import {useHasOrganizationSentAnyReplayEvents} from 'sentry/utils/replays/hooks/useReplayOnboarding';
 import {projectCanLinkToReplay} from 'sentry/utils/replays/projectSupportsReplay';
 import useProjectFromSlug from 'sentry/utils/useProjectFromSlug';
@@ -13,9 +15,16 @@ type Props = {
   organization: Organization;
   projectSlug: string;
   replayId: undefined | string;
+  group?: Group;
 };
 
-export default function EventReplay({replayId, organization, projectSlug, event}: Props) {
+export default function EventReplay({
+  replayId,
+  organization,
+  projectSlug,
+  event,
+  group,
+}: Props) {
   const hasReplaysFeature = organization.features.includes('session-replay');
   const {hasOrgSentReplays, fetching} = useHasOrganizationSentAnyReplayEvents();
 
@@ -48,6 +57,13 @@ export default function EventReplay({replayId, organization, projectSlug, event}
         replaySlug={`${projectSlug}:${replayId}`}
         orgSlug={organization.slug}
         event={event}
+        onClickOpenReplay={() => {
+          trackAnalytics('issue_details.open_replay_details_clicked', {
+            ...getAnalyticsDataForEvent(event),
+            ...getAnalyticsDataForGroup(group),
+            organization,
+          });
+        }}
       />
     </ErrorBoundary>
   );
