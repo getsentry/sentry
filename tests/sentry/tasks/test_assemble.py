getsentry/sentry

commit 5a60934451b50c3c0a35bba90a5b3545629eb071

ref(sourcemaps): Refactor and improve bundle assembling (#52433)

tests/sentry/tasks/test_assemble.py
@@ -19,9 +19,10 @@ from sentry.models.artifactbundle import (
 from sentry.models.debugfile import ProjectDebugFile
 from sentry.models.releasefile import read_artifact_index
 from sentry.tasks.assemble import (
+    ArtifactBundlePostAssembler,
+    AssembleResult,
     AssembleTask,
     ChunkFileState,
-    _index_bundle_if_needed,
     assemble_artifacts,
     assemble_dif,
     assemble_file,
@@ -270,6 +271,52 @@ class AssembleArtifactsTest(BaseAssembleTest):
             DebugIdArtifactBundle.objects.all().delete()
             ReleaseArtifactBundle.objects.all().delete()
             ProjectArtifactBundle.objects.all().delete()
+
+    @patch("sentry.tasks.assemble.ArtifactBundlePostAssembler.post_assemble")
+    def test_assembled_bundle_is_deleted_if_post_assembler_error_occurs(self, post_assemble):
+        post_assemble.side_effect = Exception
+
+        bundle_file = self.create_artifact_bundle_zip(
+            fixture_path="artifact_bundle_debug_ids", project=self.project.id
+        )
+        blob1 = FileBlob.from_file(ContentFile(bundle_file))
+        total_checksum = sha1(bundle_file).hexdigest()
+
+        assemble_artifacts(
+            org_id=self.organization.id,
+            project_ids=[self.project.id],
+            version="1.0",
+            dist="android",
+            checksum=total_checksum,
+            chunks=[blob1.checksum],
+            upload_as_artifact_bundle=True,
+        )
+
+        files = File.objects.filter()
+        assert len(files) == 0
+
+    @patch("sentry.tasks.assemble.ArtifactBundleArchive")
+    def test_assembled_bundle_is_deleted_if_archive_is_invalid(self, artifact_bundle_archive):
+        artifact_bundle_archive.side_effect = Exception
+
+        bundle_file = self.create_artifact_bundle_zip(
+            fixture_path="artifact_bundle_debug_ids", project=self.project.id
+        )
+        blob1 = FileBlob.from_file(ContentFile(bundle_file))
+        total_checksum = sha1(bundle_file).hexdigest()
+
+        assemble_artifacts(
+            org_id=self.organization.id,
+            project_ids=[self.project.id],
+            version="1.0",
+            dist="android",
+            checksum=total_checksum,
+            chunks=[blob1.checksum],
+            upload_as_artifact_bundle=True,
+        )
+
+        files = File.objects.filter()
+        assert len(files) == 0
 
     def test_upload_artifacts_with_duplicated_debug_ids(self):
         bundle_file = self.create_artifact_bundle_zip(
@@ -756,16 +803,39 @@ class ArtifactBundleIndexingTest(TestCase):
 
         return artifact_bundle
 
+    def mock_assemble_result(self) -> AssembleResult:
+        bundle_file = self.create_artifact_bundle_zip(
+            fixture_path="artifact_bundle_debug_ids", project=self.project.id
+        )
+        blob1 = FileBlob.from_file(ContentFile(bundle_file))
+        total_checksum = sha1(bundle_file).hexdigest()
+        return assemble_file(
+            task=AssembleTask.ARTIFACT_BUNDLE,
+            org_or_project=self.organization,
+            name="bundle.zip",
+            checksum=total_checksum,
+            chunks=[blob1.checksum],
+            file_type="artifact.bundle",
+        )
+
     @patch("sentry.tasks.assemble.index_artifact_bundles_for_release")
     def test_index_if_needed_with_no_bundles(self, index_artifact_bundles_for_release):
         release = "1.0"
         dist = "android"
 
-        _index_bundle_if_needed(
-            org_id=self.organization.id, release=release, dist=dist, date_snapshot=datetime.now()
-        )
+        with ArtifactBundlePostAssembler(
+            assemble_result=self.mock_assemble_result(),
+            organization=self.organization,
+            release=release,
+            dist=dist,
+            project_ids=[],
+        ) as post_assembler:
+            post_assembler._index_bundle_if_needed(
+                release=release, dist=dist, date_snapshot=datetime.now()
+            )
 
         index_artifact_bundles_for_release.assert_not_called()
+        post_assembler.close()
 
     @patch("sentry.tasks.assemble.index_artifact_bundles_for_release")
     def test_index_if_needed_with_lower_bundles_than_threshold(
@@ -782,11 +852,19 @@ class ArtifactBundleIndexingTest(TestCase):
             date=datetime.now() - timedelta(hours=1),
         )
 
-        _index_bundle_if_needed(
-            org_id=self.organization.id, release=release, dist=dist, date_snapshot=datetime.now()
-        )
+        with ArtifactBundlePostAssembler(
+            assemble_result=self.mock_assemble_result(),
+            organization=self.organization,
+            release=release,
+            dist=dist,
+            project_ids=[],
+        ) as post_assembler:
+            post_assembler._index_bundle_if_needed(
+                release=release, dist=dist, date_snapshot=datetime.now()
+            )
 
         index_artifact_bundles_for_release.assert_not_called()
+        post_assembler.close()
 
     @patch("sentry.tasks.assemble.index_artifact_bundles_for_release")
     def test_index_if_needed_with_higher_bundles_than_threshold(
@@ -811,9 +889,16 @@ class ArtifactBundleIndexingTest(TestCase):
             date=datetime.now() - timedelta(hours=1),
         )
 
-        _index_bundle_if_needed(
-            org_id=self.organization.id, release=release, dist=dist, date_snapshot=datetime.now()
-        )
+        with ArtifactBundlePostAssembler(
+            assemble_result=self.mock_assemble_result(),
+            organization=self.organization,
+            release=release,
+            dist=dist,
+            project_ids=[],
+        ) as post_assembler:
+            post_assembler._index_bundle_if_needed(
+                release=release, dist=dist, date_snapshot=datetime.now()
+            )
 
         index_artifact_bundles_for_release.assert_called_with(
             organization_id=self.organization.id,
@@ -821,6 +906,7 @@ class ArtifactBundleIndexingTest(TestCase):
             release=release,
             dist=dist,
         )
+        post_assembler.close()
 
     @patch("sentry.tasks.assemble.index_artifact_bundles_for_release")
     def test_index_if_needed_with_bundles_already_indexed(self, index_artifact_bundles_for_release):
@@ -843,9 +929,16 @@ class ArtifactBundleIndexingTest(TestCase):
             date=datetime.now() - timedelta(hours=1),
         )
 
-        _index_bundle_if_needed(
-            org_id=self.organization.id, release=release, dist=dist, date_snapshot=datetime.now()
-        )
+        with ArtifactBundlePostAssembler(
+            assemble_result=self.mock_assemble_result(),
+            organization=self.organization,
+            release=release,
+            dist=dist,
+            project_ids=[],
+        ) as post_assembler:
+            post_assembler._index_bundle_if_needed(
+                release=release, dist=dist, date_snapshot=datetime.now()
+            )
 
         index_artifact_bundles_for_release.assert_not_called()
 
@@ -882,9 +975,16 @@ class ArtifactBundleIndexingTest(TestCase):
             date=datetime.now() + timedelta(hours=1),
         )
 
-        _index_bundle_if_needed(
-            org_id=self.organization.id, release=release, dist=dist, date_snapshot=datetime.now()
-        )
+        with ArtifactBundlePostAssembler(
+            assemble_result=self.mock_assemble_result(),
+            organization=self.organization,
+            release=release,
+            dist=dist,
+            project_ids=[],
+        ) as post_assembler:
+            post_assembler._index_bundle_if_needed(
+                release=release, dist=dist, date_snapshot=datetime.now()
+            )
 
         index_artifact_bundles_for_release.assert_called_with(
             organization_id=self.organization.id,

src/sentry/models/artifactbundle.py
@@ -1,6 +1,6 @@
 import zipfile
 from enum import Enum
-from typing import IO, Callable, Dict, List, Mapping, Optional, Tuple
+from typing import IO, Callable, Dict, List, Mapping, Optional, Set, Tuple
 
 from django.db import models
 from django.db.models.signals import post_delete
@@ -197,8 +197,15 @@ class ArtifactBundleArchive:
         self._fileobj = fileobj
         self._zip_file = zipfile.ZipFile(self._fileobj)
         self.manifest = self._read_manifest()
+        self.artifact_count = len(self.manifest.get("files", {}))
         if build_memory_map:
             self._build_memory_maps()
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc, value, tb):
+        self.close()
 
     def close(self):
         self._zip_file.close()
@@ -232,7 +239,6 @@ class ArtifactBundleArchive:
         self._entries_by_debug_id = {}
         self._entries_by_url = {}
 
-        # TODO(iambriccardo): generalize the manifest reading methods across assemble and processor.
         files = self.manifest.get("files", {})
         for file_path, info in files.items():
             # Building the map for debug_id lookup.
@@ -254,6 +260,43 @@ class ArtifactBundleArchive:
 
             # Building the map for url lookup.
             self._entries_by_url[info.get("url")] = (file_path, info)
+
+    def extract_debug_ids_from_manifest(
+        self,
+    ) -> Tuple[Optional[str], Set[Tuple[SourceFileType, str]]]:
+        # We use a set, since we might have the same debug_id and file_type.
+        debug_ids_with_types = set()
+
+        # We also want to extract the bundle_id which is also known as the bundle debug_id. This id is used to uniquely
+        # identify a specific ArtifactBundle in case for example of future deletion.
+        #
+        # If no id is found, it means that we must have an associated release to this ArtifactBundle, through the
+        # ReleaseArtifactBundle table.
+        bundle_id = self._extract_bundle_id()
+
+        files = self.manifest.get("files", {})
+        for file_path, info in files.items():
+            headers = self.normalize_headers(info.get("headers", {}))
+            if (debug_id := headers.get("debug-id")) is not None:
+                debug_id = self.normalize_debug_id(debug_id)
+                file_type = info.get("type")
+                if (
+                    debug_id is not None
+                    and file_type is not None
+                    and (source_file_type := SourceFileType.from_lowercase_key(file_type))
+                    is not None
+                ):
+                    debug_ids_with_types.add((source_file_type, debug_id))
+
+        return bundle_id, debug_ids_with_types
+
+    def _extract_bundle_id(self):
+        bundle_id = self.manifest.get("debug_id")
+
+        if bundle_id is not None:
+            bundle_id = self.normalize_debug_id(bundle_id)
+
+        return bundle_id
 
     def get_files(self) -> Dict[str, dict]:
         return self.manifest.get("files", {})
@@ -329,4 +372,5 @@ class ArtifactBundleArchive:
     def get_file_url_by_file_path(self, file_path):
         files = self.manifest.get("files", {})
         file_info = files.get(file_path, {})
+
         return file_info.get("url")

src/sentry/models/releasefile.py
@@ -198,6 +198,9 @@ class ReleaseArchive:
         return self
 
     def __exit__(self, exc, value, tb):
+        self.close()
+
+    def close(self):
         self._zip_file.close()
         self._fileobj.close()
 

tests/sentry/api/endpoints/test_dif_assemble.py
@@ -191,9 +191,13 @@ class DifAssembleEndpoint(APITestCase):
             }
         )
 
-        file = assemble_file(
+        assemble_result = assemble_file(
             AssembleTask.DIF, self.project, "test", total_checksum, chunks, "project.dif"
-        )[0]
+        )
+
+        assert assemble_result is not None
+
+        file = assemble_result.bundle
         status, _ = get_assemble_status(AssembleTask.DIF, self.project.id, total_checksum)
         assert status != ChunkFileState.ERROR
         assert file.checksum == total_checksum

commit 1b0c935c26f8150fbdf978e8fb8dc600678e7712

ref: fix file descriptor leak in post_assemble crash (#52740)
Resolves https://sentry.sentry.io/issues/4202736422/
regressed in #52433

pyproject.toml
@@ -1400,7 +1400,6 @@ module = [
     "tests.sentry.tasks.deletion.test_groups",
     "tests.sentry.tasks.deletion.test_scheduled",
     "tests.sentry.tasks.integrations.github.test_pr_comment",
-    "tests.sentry.tasks.test_assemble",
     "tests.sentry.tasks.test_derive_code_mappings",
     "tests.sentry.tasks.test_groupowner",
     "tests.sentry.tasks.test_merge",

src/sentry/models/files/abstractfile.py
@@ -314,6 +314,7 @@ class AbstractFile(Model):
             self.checksum = new_checksum.hexdigest()
 
             if checksum != self.checksum:
+                tf.close()
                 raise AssembleChecksumMismatch("Checksum mismatch")
 
         metrics.timing("filestore.file-size", offset)

src/sentry/tasks/assemble.py
@@ -60,6 +60,7 @@ class AssembleResult(NamedTuple):
 
     def delete_bundle(self):
         self.bundle.delete()
+        self.bundle_temp_file.close()
 
 
 def assemble_file(

tests/sentry/tasks/test_assemble.py
@@ -102,11 +102,11 @@ class AssembleDifTest(BaseAssembleTest):
 
         # find all blobs
         for reference, checksum in files:
-            blob = FileBlob.objects.get(checksum=checksum)
+            file_blob = FileBlob.objects.get(checksum=checksum)
             ref_bytes = reference.getvalue()
-            with blob.getfile() as f:
+            with file_blob.getfile() as f:
                 assert f.read(len(ref_bytes)) == ref_bytes
-            FileBlobOwner.objects.filter(blob=blob, organization_id=self.organization.id).get()
+            FileBlobOwner.objects.filter(blob=file_blob, organization_id=self.organization.id).get()
 
         rv = assemble_file(
             AssembleTask.DIF,
@@ -129,7 +129,7 @@ class AssembleDifTest(BaseAssembleTest):
         FileBlob.from_files(files, organization=self.organization)
 
         # assemble a second time
-        f, tmp = assemble_file(
+        rv = assemble_file(
             AssembleTask.DIF,
             self.project,
             "testfile",
@@ -137,6 +137,8 @@ class AssembleDifTest(BaseAssembleTest):
             [x[1] for x in files],
             "dummy.type",
         )
+        assert rv is not None
+        f, tmp = rv
         tmp.close()
         assert f.checksum == file_checksum.hexdigest()
 
@@ -154,11 +156,11 @@ class AssembleDifTest(BaseAssembleTest):
 
         # find all blobs
         for reference, checksum in files:
-            blob = FileBlob.objects.get(checksum=checksum)
+            file_blob = FileBlob.objects.get(checksum=checksum)
             ref_bytes = reference.getvalue()
-            with blob.getfile() as f:
+            with file_blob.getfile() as f:
                 assert f.read(len(ref_bytes)) == ref_bytes
-            FileBlobOwner.objects.filter(blob=blob, organization_id=self.organization.id).get()
+            FileBlobOwner.objects.filter(blob=file_blob, organization_id=self.organization.id).get()
 
         rv = assemble_file(
             AssembleTask.DIF,
@@ -304,6 +306,26 @@ class AssembleArtifactsTest(BaseAssembleTest):
         )
         blob1 = FileBlob.from_file(ContentFile(bundle_file))
         total_checksum = sha1(bundle_file).hexdigest()
+
+        assemble_artifacts(
+            org_id=self.organization.id,
+            project_ids=[self.project.id],
+            version="1.0",
+            dist="android",
+            checksum=total_checksum,
+            chunks=[blob1.checksum],
+            upload_as_artifact_bundle=True,
+        )
+
+        files = File.objects.filter()
+        assert len(files) == 0
+
+    def test_assembled_bundle_is_deleted_if_checksum_mismatches(self):
+        bundle_file = self.create_artifact_bundle_zip(
+            fixture_path="artifact_bundle_debug_ids", project=self.project.id
+        )
+        blob1 = FileBlob.from_file(ContentFile(bundle_file))
+        total_checksum = "a" * 40
 
         assemble_artifacts(
             org_id=self.organization.id,
@@ -680,6 +702,7 @@ class AssembleArtifactsTest(BaseAssembleTest):
                 if min_files == 1:
                     # An archive was saved
                     index = read_artifact_index(self.release, dist=None)
+                    assert index is not None
                     archive_ident = index["files"]["~/index.js"]["archive_ident"]
                     releasefile = ReleaseFile.objects.get(
                         release_id=self.release.id, ident=archive_ident
@@ -809,7 +832,7 @@ class ArtifactBundleIndexingTest(TestCase):
         )
         blob1 = FileBlob.from_file(ContentFile(bundle_file))
         total_checksum = sha1(bundle_file).hexdigest()
-        return assemble_file(
+        rv = assemble_file(
             task=AssembleTask.ARTIFACT_BUNDLE,
             org_or_project=self.organization,
             name="bundle.zip",
@@ -817,6 +840,8 @@ class ArtifactBundleIndexingTest(TestCase):
             chunks=[blob1.checksum],
             file_type="artifact.bundle",
         )
+        assert rv is not None
+        return rv
 
     @patch("sentry.tasks.assemble.index_artifact_bundles_for_release")
     def test_index_if_needed_with_no_bundles(self, index_artifact_bundles_for_release):