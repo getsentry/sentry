f8b0d322f854b7fbe29f343f2007e31dc3d1eb5c
Alberto Leal <mail4alberto@gmail.com>
2023-07-11 20:20:57 -0400

    re-introduce previous implementation of PagerDutyClient

--- src/sentry/integrations/pagerduty/client.py
+++ src/sentry/integrations/pagerduty/client.py
@@ -4,6 +4,7 @@ from typing import Any
 
 from sentry.api.serializers import ExternalEventSerializer, serialize
 from sentry.eventstore.models import Event, GroupEvent
+from sentry.integrations.client import ApiClient
 from sentry.shared_integrations.client.base import BaseApiResponseX
 from sentry.shared_integrations.client.proxy import IntegrationProxyClient
 
@@ -34,6 +35,59 @@ class PagerDutyProxyClient(IntegrationProxyClient):
         if headers is None:
             headers = {"Content-Type": "application/json"}
         return self._request(method, *args, headers=headers, **kwargs)
+
+    def send_trigger(self, data):
+        # expected payload: https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
+        if isinstance(data, (Event, GroupEvent)):
+            source = data.transaction or data.culprit or "<unknown>"
+            group = data.group
+            level = data.get_tag("level") or "error"
+            custom_details = serialize(data, None, ExternalEventSerializer())
+            summary = custom_details["message"][:1024] or custom_details["title"]
+            payload = {
+                "routing_key": self.integration_key,
+                "event_action": "trigger",
+                "dedup_key": group.qualified_short_id,
+                "payload": {
+                    "summary": summary,
+                    "severity": LEVEL_SEVERITY_MAP[level],
+                    "source": source,
+                    "component": group.project.slug,
+                    "custom_details": custom_details,
+                },
+                "links": [
+                    {
+                        "href": group.get_absolute_url(
+                            params={"referrer": "pagerduty_integration"}
+                        ),
+                        "text": "View Sentry Issue Details",
+                    }
+                ],
+            }
+        else:
+            # the payload is for a metric alert
+            payload = data
+
+        return self.post("/", data=payload)
+
+
+# This exists since getsentry depends on the previous implementation of the API client.
+
+
+class PagerDutyClient(ApiClient):
+    allow_redirects = False
+    integration_name = "pagerduty"
+    base_url = "https://events.pagerduty.com/v2/enqueue"
+
+    def __init__(self, integration_key):
+        self.integration_key = integration_key
+        super().__init__()
+
+    def request(self, method, path, headers=None, data=None, params=None):
+        if not headers:
+            headers = {"Content-Type": "application/json"}
+
+        return self._request(method, path, headers=headers, data=data, params=params)
 
     def send_trigger(self, data):
         # expected payload: https://v2.developer.pagerduty.com/docs/send-an-event-events-api-v2
