79dc748a9c2b1687334be83cd2debea0bbfb0632
nathan hsieh <hsieh.nathan@gmail.com>
2023-07-12 17:09:52 -0700

    clean up tests

--- tests/sentry/web/frontend/test_auth_channel_login.py
+++ tests/sentry/web/frontend/test_auth_channel_login.py
@@ -1,22 +1,26 @@
-from functools import cached_property
-
 from django.urls import reverse
 
+from sentry.auth.providers.fly.provider import FlyOAuth2Provider
+from sentry.models.authprovider import AuthProvider
 from sentry.testutils.cases import TestCase
 from sentry.testutils.silo import region_silo_test
 
 
 @region_silo_test
-class AuthOrganizationIdentifierLoginTest(TestCase):
-    @cached_property
-    def organization(self):
-        return self.create_organization(name="test org", owner=self.user)
-
-    @cached_property
-    def path(self):
-        return reverse("sentry-auth-organization-id", args=[self.organization.id])
+class AuthOrganizationChannelLoginTest(TestCase):
+    def setup(self):
+        self.organization = self.create_organization(name="test org", owner=self.user)
+        self.partner_org_id = "foobar"
+        config_data = FlyOAuth2Provider.build_config(resource={"id": self.partner_org_id})
+        self.auth_provider = AuthProvider.objects.create(
+            organization_id=self.organization.id, provider="fly", config=config_data
+        )
+        self.path = (
+            f"{reverse('sentry-auth-channel',args=['fly'])}?organization_id={self.partner_org_id}"
+        )
 
     def test_redirect_for_logged_in_user(self):
+        self.setup()
         self.login_as(self.user)
         response = self.client.get(self.path, follow=True)
         assert response.status_code == 200
@@ -25,6 +29,7 @@ class AuthOrganizationIdentifierLoginTest(TestCase):
         ]
 
     def test_redirect_for_logged_out_user(self):
+        self.setup()
         response = self.client.get(self.path, follow=True)
         assert response.status_code == 200
         assert response.redirect_chain == [
@@ -32,17 +37,20 @@ class AuthOrganizationIdentifierLoginTest(TestCase):
         ]
 
     def test_with_next_uri(self):
+        self.setup()
         self.login_as(self.user)
-        response = self.client.get(self.path + "?next=/projects/", follow=True)
+        response = self.client.get(self.path + "&next=/projects/", follow=True)
         assert response.status_code == 200
         assert response.redirect_chain == [
             ("/projects/", 302),
         ]
 
     def test_subdomain_precedence(self):
+        self.setup()
         another_org = self.create_organization(name="another org")
+        path = f"{reverse('sentry-auth-channel',args=['fly'])}?organization_id={another_org.id}"
         response = self.client.get(
-            reverse("sentry-auth-organization-id", args=[another_org.id]),
+            path,
             HTTP_HOST=f"{self.organization.slug}.testserver",
             follow=True,
         )
