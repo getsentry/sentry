b96ae6e26b24bb52fe57ec1b43683679b6e68d6d
Arpad Borsos <arpad.borsos@sentry.io>
2023-07-13 10:59:58 +0200

    only store indices, test merge and unmerge

--- src/sentry/debug_files/bundle_index.py
+++ src/sentry/debug_files/bundle_index.py
@@ -1,76 +1,107 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
 from datetime import datetime
-from typing import Dict, List
+from typing import Dict, List, TypeVar
 
 from sentry.models.artifactbundle import ArtifactBundleArchive, SourceFileType
 
 
+@dataclass(frozen=True)
+class BundleMeta:
+    id: int
+    timestamp: datetime
+
+
+T = TypeVar("T")
+
+
 class BundleIndex:
     def __init__(self) -> None:
-        self.bundles: Dict[int, datetime] = {}
-        self.files_by_url: Dict[str, List[dict]] = {}
-        self.files_by_debug_id: Dict[str, List[dict]] = {}
+        # A list of bundle metadata
+        self._bundles: List[BundleMeta] = []
+        # Lookup tables by `url` and `debug_id/file_type` to a list of `self.bundles` indices,
+        # sorted by the `timestamp` of the bundle.
+        self._files_by_url: Dict[str, List[int]] = {}
+        self._files_by_debug_id: Dict[(str, str), List[int]] = {}
+
+    def get_meta_by_url(self, url: str) -> BundleMeta | None:
+        entries = self._files_by_url.get(url)
+        if entries:
+            idx = entries[-1]
+            return self._bundles[idx]
 
     # TODO: load from JSON
     # TODO: write as JSON
 
     def merge_bundle_manifest(
         self,
-        bundle_id: int,
-        bundle_date: datetime,
+        bundle_meta: BundleMeta,
         manifest: dict,
         index_urls=True,
         index_debugids=True,
     ):
         """
-        Merges the Bundle given by its `bundle_id`, `bundle_date` and `manifest` into the Index.
+        Merges the Bundle with the given `BundleMeta` and `manifest` into the Index.
         """
 
-        # The bundle is already part of the index
-        if self.bundles.get(bundle_id):
+        # The bundle (matched by id) is already part of the index
+        if any(meta.id == bundle_meta.id for meta in self._bundles):
             return
 
-        self.bundles[bundle_id] = bundle_date
+        self._bundles.append(bundle_meta)
+        bundle_idx = len(self._bundles) - 1
 
         # Index the files of the manifest
         for info in manifest.get("files", {}).values():
-            headers = ArtifactBundleArchive.normalize_headers(info.get("headers", {}))
-
             if index_urls and (url := info.get("url")):
-                self._add_sorted_entry(
-                    self.files_by_url, url, {"bundle": bundle_id, "headers": headers}
-                )
+                self._add_sorted_entry(self._files_by_url, url, bundle_idx)
 
+            headers = ArtifactBundleArchive.normalize_headers(info.get("headers", {}))
             if index_debugids and (debug_id := headers.get("debug-id")):
                 debug_id = self.normalize_debug_id(debug_id)
                 file_type = info.get("type", "")
+
                 if debug_id and SourceFileType.from_lowercase_key(file_type):
                     self._add_sorted_entry(
-                        self.files_by_debug_id, debug_id, {"bundle": bundle_id, "type": file_type}
+                        self._files_by_debug_id, (debug_id, file_type), bundle_idx
                     )
 
         # Maybe Re-sort the indexes, because why not?
-        # self.bundles = dict(sorted(self.bundles.items(), key=lambda i: i[1]))
         # self.files_by_url = dict(sorted(self.files_by_url.items()))
         # self.files_by_debug_id = dict(sorted(self.files_by_debug_id.items()))
 
-    def _add_sorted_entry(self, collection: Dict[str, List[dict]], key: str, entry: dict):
+    def _add_sorted_entry(self, collection: Dict[T, List[int]], key: T, bundle_idx: int):
         entries = collection.get(key, [])
-        entries.append(entry)
-        entries.sort(key=lambda e: self.bundles[e["bundle"]])
-        collection[key] = entry
+        entries.append(bundle_idx)
+        entries.sort(key=lambda e: self._bundles[e].timestamp)
+        collection[key] = entries
 
     def remove_bundle_from_index(self, bundle_id: int):
         """
         Removes all the entries with the given `bundle_id` from the Index.
         """
-        del self.bundles[bundle_id]
-        self.files_by_url = self._remove_entry(self.files_by_url, bundle_id)
-        self.files_by_debug_id = self._remove_entry(self.files_by_debug_id, bundle_id)
+        removed_idx = None
+        for idx, bundle_meta in enumerate(self._bundles):
+            if bundle_meta.id == bundle_id:
+                removed_idx = idx
+                break
 
-    def _remove_entry(self, collection: Dict[str, List[dict]], bundle_id: int):
+        if removed_idx is None:
+            return
+
+        del self._bundles[removed_idx]
+
+        self._files_by_url = self._remove_entry(self._files_by_url, removed_idx)
+        self._files_by_debug_id = self._remove_entry(self._files_by_debug_id, removed_idx)
+
+    def _remove_entry(self, collection: Dict[T, List[int]], removed_idx: int):
         updated_collection = {}
         for key, entries in collection.items():
-            entries = [e for e in entries if e["bundle"] != bundle_id]
+            # We removed `removed_idx` from `self.bundles`, this means we have to
+            # patch up all the references: remove the direct reference, and shift
+            # all higher indexes by 1 to account for the gap
+            entries = [e if e < removed_idx else e - 1 for e in entries if e != removed_idx]
             if entries:
                 updated_collection[key] = entries
 
