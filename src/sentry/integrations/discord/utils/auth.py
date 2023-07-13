82d5ae05aa24da2401f9e37e9160b378f1987f9c
Spencer Murray <spencer.murray@sentry.io>
2023-07-12 17:35:52 -0700

    Remove unnecessary comment

--- src/sentry/integrations/discord/utils/auth.py
+++ src/sentry/integrations/discord/utils/auth.py
@@ -4,7 +4,6 @@ from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
 
 def verify_signature(public_key_string: str, signature: str, message: str) -> bool:
     try:
-        # https://cryptography.io/en/latest/hazmat/primitives/asymmetric/ed25519/#cryptography.hazmat.primitives.asymmetric.ed25519.Ed25519PublicKey
         public_key = Ed25519PublicKey.from_public_bytes(bytes.fromhex(public_key_string))
         public_key.verify(bytes.fromhex(signature), message.encode("utf-8"))
         return True
