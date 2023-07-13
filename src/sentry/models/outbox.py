710c9eca2e7c4353288fa5fea0811201371aa7f3
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-07-13 11:51:14 -0700

    Update src/sentry/models/outbox.py
    Co-authored-by: Alberto Leal <mail4alberto@gmail.com>

--- src/sentry/models/outbox.py
+++ src/sentry/models/outbox.py
@@ -399,7 +399,7 @@ class ControlOutbox(OutboxBase):
     __repr__ = sane_repr(*coalesced_columns)
 
     @classmethod
-    def get_webhook_payload_from_request(self, request: HttpRequest) -> OutboxWebhookPayload:
+    def get_webhook_payload_from_request(cls, request: HttpRequest) -> OutboxWebhookPayload:
         return OutboxWebhookPayload(
             method=request.method,
             path=request.get_full_path(),
