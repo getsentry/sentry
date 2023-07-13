a20dac60425abcfd6f411d9697f47894337ded04
Malachi Willey <malwilley@gmail.com>
2023-07-13 13:15:47 -0700

    fix(helpful-event): Prevent event from being refetched when using All Events tab (#52752)
    Fixes https://github.com/getsentry/sentry/issues/52738
    Fixes SENTRY-131D
    
    `useApiQuery` refetches whenever its query key changes. Since the
    `query` part of the URL can change when you search in the "All Events"
    tab, this was causing refetches of the helpful event. This does two
    things to prevent that from occurring:
    
    1. Disables the event details query when we are not on the details tab
    2. Clears `query` from the query string when navigating away from the
    All Events tab

--- static/app/views/issueDetails/groupDetails.tsx
+++ static/app/views/issueDetails/groupDetails.tsx
@@ -242,34 +242,41 @@ function useEventApiQuery({
 }) {
   const organization = useOrganization();
   const location = useLocation<{query?: string}>();
+  const router = useRouter();
   const hasMostHelpfulEventFeature = organization.features.includes(
     'issue-details-most-helpful-event'
   );
   const eventIdUrl = eventId ?? (hasMostHelpfulEventFeature ? 'helpful' : 'latest');
+  const helpfulEventQuery =
+    hasMostHelpfulEventFeature && typeof location.query.query === 'string'
+      ? location.query.query
+      : undefined;
 
   const queryKey: ApiQueryKey = [
     `/issues/${groupId}/events/${eventIdUrl}/`,
     {
       query: getGroupEventDetailsQueryData({
         environments,
-        query: hasMostHelpfulEventFeature ? location.query.query : undefined,
+        query: helpfulEventQuery,
       }),
     },
   ];
 
-  const isLatestOrHelpfulEvent = eventIdUrl === 'latest' || eventIdUrl === 'helpful';
+  const tab = getCurrentTab({router});
+  const isOnDetailsTab = tab === Tab.DETAILS;
 
+  const isLatestOrHelpfulEvent = eventIdUrl === 'latest' || eventIdUrl === 'helpful';
   const latestOrHelpfulEvent = useApiQuery<Event>(queryKey, {
     // Latest/helpful event will change over time, so only cache for 30 seconds
     staleTime: 30000,
     cacheTime: 30000,
-    enabled: isLatestOrHelpfulEvent,
+    enabled: isOnDetailsTab && isLatestOrHelpfulEvent,
     retry: (_, error) => error.status !== 404,
   });
   const otherEventQuery = useApiQuery<Event>(queryKey, {
     // Oldest/specific events will never change
     staleTime: Infinity,
-    enabled: !isLatestOrHelpfulEvent,
+    enabled: isOnDetailsTab && !isLatestOrHelpfulEvent,
     retry: (_, error) => error.status !== 404,
   });
 
@@ -340,7 +347,11 @@ function useFetchGroupDetails(): FetchGroupDetailsState {
     isLoading: loadingEvent,
     isError,
     refetch: refetchEvent,
-  } = useEventApiQuery({groupId, eventId: params.eventId, environments});
+  } = useEventApiQuery({
+    groupId,
+    eventId: params.eventId,
+    environments,
+  });
 
   const {
     data: groupData,
