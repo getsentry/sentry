343e2d87d0916f73ca9a93d6aef95e8d83c72738
Alberto Leal <mail4alberto@gmail.com>
2023-07-13 16:58:30 -0400

    fixup! add fixtures

--- fixtures/slack.py
+++ fixtures/slack.py
@@ -1,54 +1,46 @@
-MISSING_COMMAND = (
-    {
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Here are the commands you can use. Commands not working? Re-install the app!",
-                },
+MISSING_COMMAND = {
+    "blocks": [
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Here are the commands you can use. Commands not working? Re-install the app!",
             },
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": "*Direct Message Commands:*"},
+        },
+        {"type": "section", "text": {"type": "mrkdwn", "text": "*Direct Message Commands:*"}},
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "`/sentry help`: View this list of commands.\n`/sentry link`: Link your Slack identity to your Sentry account to receive notifications. You'll also be able to perform actions in Sentry through Slack.\n`/sentry unlink`: Unlink your Slack identity from your Sentry account.",
             },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "`/sentry help`: View this list of commands.\n`/sentry link`: Link your Slack identity to your Sentry account to receive notifications. You'll also be able to perform actions in Sentry through Slack.\n`/sentry unlink`: Unlink your Slack identity from your Sentry account.",
-                },
+        },
+        {"type": "section", "text": {"type": "mrkdwn", "text": "*Channel Commands:*"}},
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "`/sentry link team`: Get your Sentry team's issue alert notifications in the channel this command is typed in.\n`/sentry unlink team`: Unlink a team from the channel this command is typed in.",
             },
-            {
-                "type": "section",
-                "text": {"type": "mrkdwn", "text": "*Channel Commands:*"},
+        },
+        {"type": "section", "text": {"type": "mrkdwn", "text": "*Contact:*"}},
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Let us know if you have feedback: ecosystem-feedback@sentry.io",
             },
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "`/sentry link team`: Get your Sentry team's issue alert notifications in the channel this command is typed in.\n`/sentry unlink team`: Unlink a team from the channel this command is typed in.",
-                },
+        },
+        {"type": "divider"},
+        {
+            "type": "section",
+            "text": {
+                "type": "mrkdwn",
+                "text": "Just want to learn more about Sentry? Check out our <https://docs.sentry.io/product/integrations/notification-incidents/slack/|documentation>.",
             },
-            {"type": "section", "text": {"type": "mrkdwn", "text": "*Contact:*"}},
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Let us know if you have feedback: ecosystem-feedback@sentry.io",
-                },
-            },
-            {"type": "divider"},
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "Just want to learn more about Sentry? Check out our <https://docs.sentry.io/product/integrations/notification-incidents/slack/|documentation>.",
-                },
-            },
-        ]
-    },
-)
+        },
+    ]
+}
 
 INVALID_COMMAND = {
     "blocks": [
