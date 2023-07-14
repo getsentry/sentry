d11be9a1351bac5306d1f5ef136e4cca803e9521
Danny Lee <dlee@sentry.io>
2023-07-14 09:44:22 -0700

    chore(roles): Default team-roles flag to True (#52830)
    We should let self-hosted Sentry use [team-level
    roles](https://github.com/getsentry/sentry/discussions/49028).

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1636,7 +1636,7 @@ SENTRY_FEATURES = {
     # Enable project creation for all and puts organization into test group
     "organizations:team-project-creation-all-allowlist": False,
     # Enable setting team-level roles and receiving permissions from them
-    "organizations:team-roles": False,
+    "organizations:team-roles": True,
     # Enable team member role provisioning through scim
     "organizations:scim-team-roles": False,
     # Enable the setting of org roles for team
