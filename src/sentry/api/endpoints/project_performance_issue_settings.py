54c71736ca7acde9502ba3d794a1b3076d1a87fb
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2023-07-14 12:57:35 -0400

    feat(perf-detector-threshold-configuration) Backend changes to add protection against curl abuse

--- src/sentry/api/endpoints/project_performance_issue_settings.py
+++ src/sentry/api/endpoints/project_performance_issue_settings.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from typing import Dict, Type
 
 from rest_framework import serializers, status
@@ -29,20 +30,58 @@ TEN_SECONDS = 10000  # ten seconds in milliseconds
 TEN_MB = 10000000  # ten MB in bytes
 SETTINGS_PROJECT_OPTION_KEY = "sentry:performance_issue_settings"
 
+
 # These options should only be accessible internally and used by
 # support to enable/disable performance issue detection for an outlying project
 # on a case-by-case basis.
-map_internal_only_project_settings_to_group: Dict[str, Type[GroupType]] = {
-    "uncompressed_assets_detection_enabled": PerformanceUncompressedAssetsGroupType,
-    "consecutive_http_spans_detection_enabled": PerformanceConsecutiveHTTPQueriesGroupType,
-    "large_http_payload_detection_enabled": PerformanceLargeHTTPPayloadGroupType,
-    "n_plus_one_db_queries_detection_enabled": PerformanceNPlusOneGroupType,
-    "n_plus_one_api_calls_detection_enabled": PerformanceNPlusOneAPICallsGroupType,
-    "db_on_main_thread_detection_enabled": PerformanceDBMainThreadGroupType,
-    "file_io_on_main_thread_detection_enabled": PerformanceFileIOMainThreadGroupType,
-    "consecutive_db_queries_detection_enabled": PerformanceConsecutiveDBQueriesGroupType,
-    "large_render_blocking_asset_detection_enabled": PerformanceRenderBlockingAssetSpanGroupType,
-    "slow_db_queries_detection_enabled": PerformanceSlowDBQueryGroupType,
+class InternalProjectOptions(Enum):
+    N_PLUS_ONE_DB = "n_plus_one_db_queries_detection_enabled"
+    UNCOMPRESSED_ASSET = "uncompressed_assets_detection_enabled"
+    CONSECUTIVE_HTTP_SPANS = "consecutive_http_spans_detection_enabled"
+    LARGE_HTTP_PAYLOAD = "large_http_payload_detection_enabled"
+    N_PLUS_ONE_API_CALLS = "n_plus_one_api_calls_detection_enabled"
+    DB_ON_MAIN_THREAD = "db_on_main_thread_detection_enabled"
+    FILE_IO_MAIN_THREAD = "file_io_on_main_thread_detection_enabled"
+    CONSECUTIVE_DB_QUERIES = "consecutive_db_queries_detection_enabled"
+    RENDER_BLOCKING_ASSET = "large_render_blocking_asset_detection_enabled"
+    SLOW_DB_QUERY = "slow_db_queries_detection_enabled"
+
+
+class ConfigurableThresholds(Enum):
+    N_PLUS_ONE_DB_DURATION = "n_plus_one_db_duration_threshold"
+    UNCOMPRESSED_ASSET_DURATION = "uncompressed_asset_duration_threshold"
+    UNCOMPRESSED_ASSET_SIZE = "uncompressed_asset_size_threshold"
+    LARGE_HTTP_PAYLOAD_SIZE = "large_http_payload_size_threshold"
+    DB_ON_MAIN_THREAD_DURATION = "db_on_main_thread_duration_threshold"
+    FILE_IO_MAIN_THREAD_DURATION = "file_io_on_main_thread_duration_threshold"
+    CONSECUTIVE_DB_QUERIES_MIN_TIME_SAVED = "consecutive_db_min_time_saved_threshold"
+    RENDER_BLOCKING_ASSET_FCP_RATIO = "render_blocking_fcp_ratio"
+    SLOW_DB_QUERY_DURATION = "slow_db_query_duration_threshold"
+
+
+internal_only_project_settings_to_group_map: Dict[str, Type[GroupType]] = {
+    InternalProjectOptions.UNCOMPRESSED_ASSET.value: PerformanceUncompressedAssetsGroupType,
+    InternalProjectOptions.CONSECUTIVE_HTTP_SPANS.value: PerformanceConsecutiveHTTPQueriesGroupType,
+    InternalProjectOptions.LARGE_HTTP_PAYLOAD.value: PerformanceLargeHTTPPayloadGroupType,
+    InternalProjectOptions.N_PLUS_ONE_DB.value: PerformanceNPlusOneGroupType,
+    InternalProjectOptions.N_PLUS_ONE_API_CALLS.value: PerformanceNPlusOneAPICallsGroupType,
+    InternalProjectOptions.DB_ON_MAIN_THREAD.value: PerformanceDBMainThreadGroupType,
+    InternalProjectOptions.FILE_IO_MAIN_THREAD.value: PerformanceFileIOMainThreadGroupType,
+    InternalProjectOptions.CONSECUTIVE_DB_QUERIES.value: PerformanceConsecutiveDBQueriesGroupType,
+    InternalProjectOptions.RENDER_BLOCKING_ASSET.value: PerformanceRenderBlockingAssetSpanGroupType,
+    InternalProjectOptions.SLOW_DB_QUERY.value: PerformanceSlowDBQueryGroupType,
+}
+
+configurable_thresholds_to_internal_settings_map: Dict[str, str] = {
+    ConfigurableThresholds.N_PLUS_ONE_DB_DURATION.value: InternalProjectOptions.N_PLUS_ONE_DB.value,
+    ConfigurableThresholds.UNCOMPRESSED_ASSET_DURATION.value: InternalProjectOptions.UNCOMPRESSED_ASSET.value,
+    ConfigurableThresholds.UNCOMPRESSED_ASSET_SIZE.value: InternalProjectOptions.UNCOMPRESSED_ASSET.value,
+    ConfigurableThresholds.LARGE_HTTP_PAYLOAD_SIZE.value: InternalProjectOptions.LARGE_HTTP_PAYLOAD.value,
+    ConfigurableThresholds.DB_ON_MAIN_THREAD_DURATION.value: InternalProjectOptions.DB_ON_MAIN_THREAD.value,
+    ConfigurableThresholds.FILE_IO_MAIN_THREAD_DURATION.value: InternalProjectOptions.FILE_IO_MAIN_THREAD.value,
+    ConfigurableThresholds.CONSECUTIVE_DB_QUERIES_MIN_TIME_SAVED.value: InternalProjectOptions.CONSECUTIVE_DB_QUERIES.value,
+    ConfigurableThresholds.RENDER_BLOCKING_ASSET_FCP_RATIO.value: InternalProjectOptions.RENDER_BLOCKING_ASSET.value,
+    ConfigurableThresholds.SLOW_DB_QUERY_DURATION.value: InternalProjectOptions.SLOW_DB_QUERY.value,
 }
 
 
@@ -93,6 +132,20 @@ class ProjectPerformanceIssueSettingsSerializer(serializers.Serializer):
     slow_db_queries_detection_enabled = serializers.BooleanField(required=False)
 
 
+def get_disabled_threshold_options(payload, current_settings):
+    options = []
+    internal_only_settings = [setting.value for setting in InternalProjectOptions]
+    for option in payload:
+        if option not in internal_only_settings:
+            internal_setting_for_option = configurable_thresholds_to_internal_settings_map.get(
+                option
+            )
+            is_threshold_enabled = current_settings.get(internal_setting_for_option)
+            if not is_threshold_enabled:
+                options.append(option)
+    return options
+
+
 @region_silo_endpoint
 class ProjectPerformanceIssueSettingsEndpoint(ProjectEndpoint):
     permission_classes = (ProjectOwnerOrSuperUserPermissions,)
@@ -124,12 +177,20 @@ class ProjectPerformanceIssueSettingsEndpoint(ProjectEndpoint):
         if not self.has_feature(project, request):
             return self.respond(status=status.HTTP_404_NOT_FOUND)
 
-        body_has_admin_options = any(
-            [
-                option in request.data
-                for option in map_internal_only_project_settings_to_group.keys()
-            ]
+        internal_only_settings = [setting.value for setting in InternalProjectOptions]
+        threshold_settings = [setting.value for setting in ConfigurableThresholds]
+        allowed_settings_options = [*internal_only_settings, *threshold_settings]
+
+        body_has_invalid_options = not request.data or any(
+            [option not in allowed_settings_options for option in request.data]
         )
+        if body_has_invalid_options:
+            return Response(
+                {"detail": "Invalid settings option"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+        body_has_admin_options = any([option in request.data for option in internal_only_settings])
         if body_has_admin_options and not is_active_superuser(request):
             return Response(
                 {"detail": "Passed options are only modifiable internally"},
@@ -150,7 +211,18 @@ class ProjectPerformanceIssueSettingsEndpoint(ProjectEndpoint):
             SETTINGS_PROJECT_OPTION_KEY, default=performance_issue_settings_default
         )
 
+        current_settings = {**performance_issue_settings_default, **performance_issue_settings}
+
         data = serializer.validated_data
+
+        payload_contains_disabled_threshold_setting = any(
+            [option in get_disabled_threshold_options(data, current_settings) for option in data]
+        )
+        if payload_contains_disabled_threshold_setting:
+            return Response(
+                {"detail": "Disabled options can not be modified"},
+                status=status.HTTP_403_FORBIDDEN,
+            )
 
         project.update_option(
             SETTINGS_PROJECT_OPTION_KEY,
@@ -174,13 +246,18 @@ class ProjectPerformanceIssueSettingsEndpoint(ProjectEndpoint):
             return self.respond(status=status.HTTP_404_NOT_FOUND)
 
         project_settings = project.get_option(SETTINGS_PROJECT_OPTION_KEY, default={})
+        threshold_options = [setting.value for setting in ConfigurableThresholds]
+        internal_only_settings = [setting.value for setting in InternalProjectOptions]
+        disabled_options = get_disabled_threshold_options(threshold_options, project_settings)
 
         if project_settings:
-            settings_only_with_admin_options = {
-                option: project_settings[option]
-                for option in project_settings
-                if option in map_internal_only_project_settings_to_group.keys()
-            }
-            project.update_option(SETTINGS_PROJECT_OPTION_KEY, settings_only_with_admin_options)
+            unchanged_options = (
+                {  # internal settings and disabled threshold settings can not be reset
+                    option: project_settings[option]
+                    for option in project_settings
+                    if option in internal_only_settings or option in disabled_options
+                }
+            )
+            project.update_option(SETTINGS_PROJECT_OPTION_KEY, unchanged_options)
 
         return Response(status=status.HTTP_204_NO_CONTENT)
