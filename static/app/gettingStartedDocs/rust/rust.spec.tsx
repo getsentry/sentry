dcdb42cc5da8bd1c88ed9c5390e5414cafa80f62
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-07-14 11:14:23 +0200

    ref(getting-started-docs): Migrate rust doc to sentry main repo (#52857)

---
+++ static/app/gettingStartedDocs/rust/rust.spec.tsx
@@ -0,0 +1,20 @@
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
+
+import {GettingStartedWithRust, steps} from './rust';
+
+describe('GettingStartedWithRust', function () {
+  it('all products are selected', function () {
+    const {container} = render(<GettingStartedWithRust dsn="test-dsn" />);
+
+    // Steps
+    for (const step of steps()) {
+      expect(
+        screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
+      ).toBeInTheDocument();
+    }
+
+    expect(container).toSnapshot();
+  });
+});
