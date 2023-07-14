453b6389edeb56a0b14156eccf93aba57d08d91d
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-07-14 14:55:59 +0200

    ref(getting-started-docs): Migrate php doc to sentry main repo (#52873)

---
+++ static/app/gettingStartedDocs/php/php.spec.tsx
@@ -0,0 +1,20 @@
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
+
+import {GettingStartedWithPHP, steps} from './php';
+
+describe('GettingStartedWithPHP', function () {
+  it('renders doc correctly', function () {
+    const {container} = render(<GettingStartedWithPHP dsn="test-dsn" />);
+
+    // Steps
+    for (const step of steps()) {
+      expect(
+        screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
+      ).toBeInTheDocument();
+    }
+
+    expect(container).toSnapshot();
+  });
+});
