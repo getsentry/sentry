ffed746a98cb0b08e839774de79496e70f97a893
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-07-14 15:46:15 -0700

    ✅ Add test

--- src/sentry/middleware/integrations/parsers/bitbucket_server.py
+++ src/sentry/middleware/integrations/parsers/bitbucket_server.py
@@ -5,21 +5,18 @@ import logging
 from django.http import HttpResponse
 
 from sentry.integrations.bitbucket_server.webhook import BitbucketServerWebhookEndpoint
-from sentry.middleware.integrations.parsers.base import BaseRequestParser
+from sentry.middleware.integrations.parsers.bitbucket import BitbucketRequestParser
 from sentry.models.outbox import WebhookProviderIdentifier
 
 logger = logging.getLogger(__name__)
 
 
-class BitbucketServerRequestParser(BaseRequestParser):
+class BitbucketServerRequestParser(BitbucketRequestParser):
     provider = "bitbucket_server"
     webhook_identifier = WebhookProviderIdentifier.BITBUCKET_SERVER
-
-    def get_bitbucket_server_webhook_response(self):
-        pass
 
     def get_response(self) -> HttpResponse:
         view_class = self.match.func.view_class  # type: ignore
         if view_class == BitbucketServerWebhookEndpoint:
-            return self.get_bitbucket_server_webhook_response()
+            return self.get_bitbucket_webhook_response()
         return self.get_response_from_control_silo()
