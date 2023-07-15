1e09758ba02f3cdf7c90a6ddd32fb70459e48bf4
Nisanthan Nanthakumar <nisanthan.nanthakumar@sentry.io>
2023-07-14 17:33:41 -0700

    ref(escalating): Remove threshold experiment

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -217,7 +217,6 @@ default_manager.add("organizations:discover-basic", OrganizationFeature, Feature
 default_manager.add("organizations:discover-query", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:dynamic-sampling", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:escalating-issues", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
-default_manager.add("organizations:escalating-issues-experiment-group", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:escalating-issues-msteams", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:escalating-issues-v2", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:issue-states", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
