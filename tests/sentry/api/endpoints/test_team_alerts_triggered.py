5b8c2b29fcf6ef7f6d5e718dd05c71447eb4c2e5
Matt Duncan <matt.duncan@sentry.io>
2023-07-14 17:08:39 -0700

    fix(hc): Mark TeamAlertsTriggeredTotalsEndpointTest stable

--- tests/sentry/api/endpoints/test_team_alerts_triggered.py
+++ tests/sentry/api/endpoints/test_team_alerts_triggered.py
@@ -14,7 +14,7 @@ from sentry.testutils.silo import region_silo_test
 
 
 @freeze_time()
-@region_silo_test
+@region_silo_test(stable=True)
 class TeamAlertsTriggeredTotalsEndpointTest(APITestCase):
     endpoint = "sentry-api-0-team-alerts-triggered"
 
@@ -185,7 +185,7 @@ class TeamAlertsTriggeredTotalsEndpointTest(APITestCase):
 
 
 @freeze_time()
-@region_silo_test
+@region_silo_test(stable=True)
 class TeamAlertsTriggeredIndexEndpointTest(APITestCase):
     endpoint = "sentry-api-0-team-alerts-triggered-index"
 
