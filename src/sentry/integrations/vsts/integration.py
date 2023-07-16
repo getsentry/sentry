199e40783beb4359adb33597271a251b236fbf98
Jian Yuan Lee <jianyuan@gmail.com>
2023-07-16 23:59:19 +0100

    Stack trace linking for VSTS

--- src/sentry/integrations/vsts/integration.py
+++ src/sentry/integrations/vsts/integration.py
@@ -4,6 +4,7 @@ import logging
 import re
 from time import time
 from typing import Any, Collection, Mapping, MutableMapping, Sequence
+from urllib.parse import quote, urlencode
 
 from django import forms
 from django.http import HttpRequest, HttpResponse
@@ -86,6 +87,13 @@ FEATURES = [
         will resolve your linked workitems and vice versa.
         """,
         IntegrationFeatures.ISSUE_SYNC,
+    ),
+    FeatureDescription(
+        """
+        Link your Sentry stack traces back to your Azure DevOps source code with stack
+        trace linking.
+        """,
+        IntegrationFeatures.STACKTRACE_LINK,
     ),
     FeatureDescription(
         """
@@ -329,6 +337,18 @@ class VstsIntegration(IntegrationInstallation, RepositoryMixin, VstsIssueSync):
         config["sync_status_forward"] = sync_status_forward
         return config
 
+    def format_source_url(self, repo: Repository, filepath: str, branch: str) -> str:
+        filepath = filepath.lstrip("/")
+        project = quote(repo.config["project"])
+        repo_id = quote(repo.config["name"])
+        query_string = urlencode(
+            {
+                "path": f"/{filepath}",
+                "version": f"GB{branch}",
+            }
+        )
+        return f"{self.instance}{project}/_git/{repo_id}?{query_string}"
+
     @property
     def instance(self) -> str:
         return self.model.metadata["domain_name"]
@@ -355,6 +375,7 @@ class VstsIntegrationProvider(IntegrationProvider):
             IntegrationFeatures.COMMITS,
             IntegrationFeatures.ISSUE_BASIC,
             IntegrationFeatures.ISSUE_SYNC,
+            IntegrationFeatures.STACKTRACE_LINK,
             IntegrationFeatures.TICKET_RULES,
         ]
     )
