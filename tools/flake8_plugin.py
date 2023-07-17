9626d01277b9ff96262603f8ca344561aecc8d1f
Zachary Collins <zachary.collins@sentry.io>
2023-07-17 04:18:40 -0700

    ref(hc): Add flake8 that finds transaction.atomic without using

--- tools/flake8_plugin.py
+++ tools/flake8_plugin.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import ast
-from typing import Any, Generator
+from typing import Any, Generator, List
 
 S001_fmt = (
     "S001 Avoid using the {} mock call as it is "
@@ -20,11 +20,21 @@ S004_methods = frozenset(("assertRaises", "assertRaisesRegex"))
 
 S005_msg = "S005 Do not import models from sentry.models but the actual module"
 
+S006_methods = frozenset(
+    (
+        "django.db.transaction.atomic",
+        "django.db.transaction.get_connection",
+        "sentry.utils.db.atomic_transaction",
+    )
+)
+S006_msg = "S006 Always specify a using= argument to transactions and connections"
+
 
 class SentryVisitor(ast.NodeVisitor):
     def __init__(self, filename: str) -> None:
         self.errors: list[tuple[int, int, str]] = []
         self.filename = filename
+        self.names_stack: List[dict[str, str]] = [{}]
 
     def visit_ImportFrom(self, node: ast.ImportFrom) -> None:
         if node.module and not node.level:
@@ -38,12 +48,23 @@ class SentryVisitor(ast.NodeVisitor):
             ):
                 self.errors.append((node.lineno, node.col_offset, S005_msg))
 
+            for name in node.names:
+                self.names_stack[-1][name.asname or name.name] = f"{node.module}.{name.name}"
+
         self.generic_visit(node)
+
+    def visit_FunctionDef(self, node: ast.FunctionDef) -> Any:
+        self.names_stack.append(dict(**self.names_stack[-1]))
+        self.generic_visit(node)
+        self.names_stack.pop()
 
     def visit_Import(self, node: ast.Import) -> None:
         for alias in node.names:
             if alias.name.split(".")[0] in S003_modules:
                 self.errors.append((node.lineno, node.col_offset, S003_msg))
+
+        for name in node.names:
+            self.names_stack[-1][name.asname or name.name] = name.name
 
         self.generic_visit(node)
 
@@ -58,6 +79,28 @@ class SentryVisitor(ast.NodeVisitor):
     def visit_Name(self, node: ast.Name) -> None:
         if node.id == "print":
             self.errors.append((node.lineno, node.col_offset, S002_msg))
+
+        self.generic_visit(node)
+
+    def visit_Call(self, node: ast.Call) -> None:
+        name_list: List[str] = []
+        func = node.func
+        while isinstance(func, ast.Attribute):
+            name_list.append(func.attr)
+            func = func.value
+        if not isinstance(func, ast.Name):
+            return
+        name_list.append(func.id)
+        name_list.reverse()
+
+        name = ""
+        for part in name_list:
+            name = part if not name else f"{name}.{part}"
+            name = self.names_stack[-1].get(name, name)
+
+        if name in S006_methods:
+            if len(node.args) == 0 or not any(k.arg == "using" for k in node.keywords):
+                self.errors.append((node.lineno, node.col_offset, S006_msg))
 
         self.generic_visit(node)
 
