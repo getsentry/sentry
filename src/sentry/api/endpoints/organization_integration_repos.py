13e9c2e97bb5537f5e4205e3a0afa3a87522d85e
Cathy Teng <cathy.teng@sentry.io>
2023-07-17 11:44:42 -0700

    renaming variables

--- src/sentry/api/endpoints/organization_integration_repos.py
+++ src/sentry/api/endpoints/organization_integration_repos.py
@@ -48,7 +48,7 @@ class OrganizationIntegrationReposEndpoint(RegionOrganizationIntegrationBaseEndp
         installed_repos = Repository.objects.filter(integration_id=integration.id).exclude(
             status=ObjectStatus.HIDDEN
         )
-        external_names = {hidden_repo.name for hidden_repo in installed_repos}
+        repo_names = {installed_repo.name for installed_repo in installed_repos}
 
         install = integration_service.get_installation(
             integration=integration, organization_id=organization.id
@@ -67,7 +67,7 @@ class OrganizationIntegrationReposEndpoint(RegionOrganizationIntegrationBaseEndp
                     defaultBranch=repo.get("default_branch"),
                 )
                 for repo in repositories
-                if repo["identifier"] not in external_names
+                if repo["identifier"] not in repo_names
             ]
             context = {"repos": serializedRepositories, "searchable": install.repo_search}
             return self.respond(context)
