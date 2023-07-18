8a85e901e085e2e3b0d9a04cd858348bdddfeb6a
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2023-07-18 16:07:29 -0700

    feat(github-growth): github invite feature flag (#53105)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1471,6 +1471,8 @@ SENTRY_FEATURES = {
     "organizations:widget-viewer-modal-minimap": False,
     # Enable experimental performance improvements.
     "organizations:enterprise-perf": False,
+    # Enables inviting new members based on GitHub commit activity.
+    "organizations:gh-invite": False,
     # Enable the API to importing CODEOWNERS for a project
     "organizations:integrations-codeowners": False,
     # Enable inviting members to organizations.
