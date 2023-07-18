078be042e51c2db4c23a6a133f0c4e4d882f089d
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-07-18 14:39:34 -0400

    feat(starfish): add document titles to db module (#53098)
    Adds titles to match the breadcrumbs
    i.e
    <img width="353" alt="image"
    src="https://github.com/getsentry/sentry/assets/44422760/d134433e-cd54-4268-b056-926491840ca6">

--- static/app/views/starfish/modules/DBModule.tsx
+++ static/app/views/starfish/modules/DBModule.tsx
@@ -1,32 +1,39 @@
 import * as Layout from 'sentry/components/layouts/thirds';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
+import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {t} from 'sentry/locale';
 import {
   PageErrorAlert,
   PageErrorProvider,
 } from 'sentry/utils/performance/contexts/pageError';
+import useOrganization from 'sentry/utils/useOrganization';
 import {ModuleName} from 'sentry/views/starfish/types';
+import {ROUTE_NAMES} from 'sentry/views/starfish/utils/routeNames';
 import SpansView from 'sentry/views/starfish/views/spans/spansView';
 
 export default function DBModule() {
-  return (
-    <Layout.Page>
-      <PageErrorProvider>
-        <Layout.Header>
-          <Layout.HeaderContent>
-            <Layout.Title>{t('Database Queries')}</Layout.Title>
-          </Layout.HeaderContent>
-        </Layout.Header>
+  const organization = useOrganization();
 
-        <Layout.Body>
-          <Layout.Main fullWidth>
-            <PageErrorAlert />
-            <PageFiltersContainer>
-              <SpansView moduleName={ModuleName.DB} />
-            </PageFiltersContainer>
-          </Layout.Main>
-        </Layout.Body>
-      </PageErrorProvider>
-    </Layout.Page>
+  return (
+    <SentryDocumentTitle title={ROUTE_NAMES.database} orgSlug={organization.slug}>
+      <Layout.Page>
+        <PageErrorProvider>
+          <Layout.Header>
+            <Layout.HeaderContent>
+              <Layout.Title>{t('Database Queries')}</Layout.Title>
+            </Layout.HeaderContent>
+          </Layout.Header>
+
+          <Layout.Body>
+            <Layout.Main fullWidth>
+              <PageErrorAlert />
+              <PageFiltersContainer>
+                <SpansView moduleName={ModuleName.DB} />
+              </PageFiltersContainer>
+            </Layout.Main>
+          </Layout.Body>
+        </PageErrorProvider>
+      </Layout.Page>
+    </SentryDocumentTitle>
   );
 }
