158095a147c2897c1a4640b2aadd36ba740f267c
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-07-18 11:51:12 -0700

    fix min vs max

--- src/sentry/monitors/utils.py
+++ src/sentry/monitors/utils.py
@@ -41,7 +41,7 @@ def get_timeout_at(
 ) -> Optional[datetime]:
     if status == CheckInStatus.IN_PROGRESS:
         return date_added.replace(second=0, microsecond=0) + timedelta(
-            minutes=max(((monitor_config or {}).get("max_runtime") or TIMEOUT), MAX_TIMEOUT)
+            minutes=min(((monitor_config or {}).get("max_runtime") or TIMEOUT), MAX_TIMEOUT)
         )
 
     return None
