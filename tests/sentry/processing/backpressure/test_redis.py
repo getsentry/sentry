1861ddb86c1ad308d4d81b931060c4b1ba87de67
Michal Kuffa <michal.kuffa@sentry.io>
2023-07-18 13:43:10 +0200

    Help mypy

--- tests/sentry/processing/backpressure/test_redis.py
+++ tests/sentry/processing/backpressure/test_redis.py
@@ -1,14 +1,17 @@
 from django.test.utils import override_settings
 
 from sentry.processing.backpressure.memory import iter_cluster_memory_usage
-from sentry.processing.backpressure.monitor import load_service_definitions
+from sentry.processing.backpressure.monitor import Redis, load_service_definitions
 
 
 def test_returns_some_usage() -> None:
     with override_settings(SENTRY_PROCESSING_SERVICES={"redis": {"redis": "default"}}):
         services = load_service_definitions()
 
-    usage = [usage for usage in iter_cluster_memory_usage(services["redis"].cluster)]
+    redis_service = services["redis"]
+    assert isinstance(redis_service, Redis)
+
+    usage = [usage for usage in iter_cluster_memory_usage(redis_service.cluster)]
     assert len(usage) > 0
     memory = usage[0]
     assert memory.used > 0
