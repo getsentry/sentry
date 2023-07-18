7365d96e8375bbc417bf78743239b8c2847fcc5f
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2023-07-18 13:07:51 -0700

    feat(opsgenie): add basic configuration flow (#52940)
    Add a basic configuration flow to the Opsgenie integration. Users can
    manually add teams and API keys, which are saved to the
    OrganizationIntegration config.
    
    Fixes ER-1709
    Fixes ER-1710
    
    <img width="1512" alt="Screenshot 2023-07-17 at 12 01 36 PM"
    src="https://github.com/getsentry/sentry/assets/83109586/fb8d332c-5d06-4e13-937c-5681f09226d0">
    <img width="1512" alt="Screenshot 2023-07-17 at 12 01 50 PM"
    src="https://github.com/getsentry/sentry/assets/83109586/dad305b3-efbf-43bb-8357-cb25e9a8b313">
    
    ---------
    
    Co-authored-by: Mark Story <mark@mark-story.com>

--- src/sentry/integrations/opsgenie/client.py
+++ src/sentry/integrations/opsgenie/client.py
@@ -1,6 +1,12 @@
 from __future__ import annotations
 
+from urllib.parse import quote
+
 from sentry.integrations.client import ApiClient
+from sentry.models import Integration
+from sentry.services.hybrid_cloud.integration.model import RpcIntegration
+from sentry.shared_integrations.client.base import BaseApiResponseX
+from sentry.shared_integrations.client.proxy import IntegrationProxyClient
 
 OPSGENIE_API_VERSION = "v2"
 
@@ -22,3 +28,28 @@ class OpsgenieSetupClient(ApiClient):
     def get_account(self):
         headers = {"Authorization": "GenieKey " + self.api_key}
         return self.get(path="/account", headers=headers)
+
+
+class OpsgenieClient(IntegrationProxyClient):
+    integration_name = "opsgenie"
+
+    def __init__(
+        self, integration: RpcIntegration | Integration, org_integration_id: int | None
+    ) -> None:
+        self.integration = integration
+        self.base_url = f"{self.metadata['base_url']}{OPSGENIE_API_VERSION}"
+        self.api_key = self.metadata["api_key"]
+        super().__init__(org_integration_id=org_integration_id)
+
+    @property
+    def metadata(self):
+        return self.integration.metadata
+
+    # This doesn't work if the team name is "." or "..", which Opsgenie allows for some reason
+    # despite their API not working with these names.
+    def get_team_id(self, integration_key: str, team_name: str) -> BaseApiResponseX:
+        params = {"identifierType": "name"}
+        quoted_name = quote(team_name)
+        path = f"/teams/{quoted_name}"
+        headers = {"Authorization": "GenieKey " + integration_key}
+        return self.get(path=path, headers=headers, params=params)
