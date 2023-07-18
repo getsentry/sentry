cff0a8e5154839819f3f065020668bc192d69376
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2023-07-18 14:58:15 -0700

    feat(github-growth): activate hidden repo in create_repository (#53022)

--- src/sentry/plugins/providers/integration_repository.py
+++ src/sentry/plugins/providers/integration_repository.py
@@ -18,6 +18,7 @@ from sentry.models import Integration, Repository
 from sentry.services.hybrid_cloud.integration import integration_service
 from sentry.shared_integrations.exceptions import IntegrationError
 from sentry.signals import repo_linked
+from sentry.utils import metrics
 
 
 class RepoExistsError(APIException):
@@ -82,18 +83,36 @@ class IntegrationRepositoryProvider:
     ):
         result = self.build_repository_config(organization=organization, data=repo_config)
 
+        integration_id = result.get("integration_id")
+        external_id = result.get("external_id")
+
         repo_update_params = {
-            "external_id": result.get("external_id"),
+            "external_id": external_id,
             "url": result.get("url"),
             "config": result.get("config") or {},
             "provider": self.id,
-            "integration_id": result.get("integration_id"),
+            "integration_id": integration_id,
         }
 
-        # first check if there is a repository without an integration that matches
+        # first check if there is an existing hidden repository with an integration that matches
+        existing_repo = Repository.objects.filter(
+            organization_id=organization.id,
+            name=result["name"],
+            integration_id=integration_id,
+            external_id=external_id,
+            status=ObjectStatus.HIDDEN,
+        ).first()
+        if existing_repo:
+            existing_repo.status = ObjectStatus.ACTIVE
+            existing_repo.save()
+            metrics.incr("sentry.integration_repo_provider.repo_relink")
+            return result, existing_repo
+
+        # then check if there is a repository without an integration that matches
         repo = Repository.objects.filter(
             organization_id=organization.id, name=result["name"], integration_id=None
         ).first()
+
         if repo:
             if self.logger:
                 self.logger.info(
