d597e9399a157a391bc076b43fae0658045417c6
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-07-18 14:57:28 +0200

    ref(getting-started-docs): Migrate python fra. docs to sentry main repo

--- static/app/gettingStartedDocs/python/bottle.tsx
+++ static/app/gettingStartedDocs/python/bottle.tsx
@@ -8,9 +8,9 @@ import {t, tct} from 'sentry/locale';
 const introduction = (
   <p>
     {tct(
-      'The Pyramid integration adds support for the [link:Pyramid Web Framework]. It requires Pyramid 1.6 or later.',
+      "The Bottle integration adds support for the Bottle Web Framework. Currently it works well with the stable version of Bottle (0.12). However the integration with the development version (0.13) doesn't work properly.",
       {
-        link: <ExternalLink href="https://trypyramid.com/" />,
+        link: <ExternalLink href="https://bottlepy.org/docs/dev/" />,
       }
     )}
   </p>
@@ -23,50 +23,46 @@ export const steps = ({
 } = {}): LayoutProps['steps'] => [
   {
     type: StepType.INSTALL,
-    description: <p>{tct('Install [code:sentry-sdk] from PyPI:', {code: <code />})}</p>,
+    description: (
+      <p>
+        {tct('Install [code:sentry-sdk] from PyPI with the [code:bottle] extra:', {
+          code: <code />,
+        })}
+      </p>
+    ),
     configurations: [
       {
         language: 'bash',
-        code: '$ pip install --upgrade sentry-sdk',
+        code: 'pip install --upgrade sentry-sdk[chalice]',
       },
     ],
   },
   {
     type: StepType.CONFIGURE,
     description: t(
-      'To configure the SDK, initialize it with the integration before or after your app has been created:'
+      'To configure the SDK, initialize it with the integration before your app has been initialized:'
     ),
     configurations: [
       {
         language: 'python',
         code: `
-from pyramid.config import Configurator
-from wsgiref.simple_server import make_server
-
 import sentry_sdk
-from sentry_sdk.integrations.pyramid import PyramidIntegration
+
+from bottle import Bottle, run
+from sentry_sdk.integrations.bottle import BottleIntegration
 
 sentry_sdk.init(
   dsn="${dsn}",
   integrations=[
-    PyramidIntegration(),
+    BottleIntegration(),
   ],
-
   # Set traces_sample_rate to 1.0 to capture 100%
   # of transactions for performance monitoring.
-  # We recommend adjusting this value in production.
+  # We recommend adjusting this value in production,
   traces_sample_rate=1.0,
 )
 
-def sentry_debug(request):
-division_by_zero = 1 / 0
-
-with Configurator() as config:
-config.add_route('sentry-debug', '/')
-config.add_view(sentry_debug, route_name='sentry-debug')
-app = config.make_wsgi_app()
-server = make_server('0.0.0.0', 6543, app)
-server.serve_forever()
+app = Bottle()
       `,
       },
     ],
@@ -74,8 +70,8 @@ server.serve_forever()
 ];
 // Configuration End
 
-export function GettingStartedWithPyramid({dsn, ...props}: ModuleProps) {
+export function GettingStartedWithBottle({dsn, ...props}: ModuleProps) {
   return <Layout steps={steps({dsn})} introduction={introduction} {...props} />;
 }
 
-export default GettingStartedWithPyramid;
+export default GettingStartedWithBottle;
