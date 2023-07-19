21f2869dc398d38e76508227b26c615037e7aee5
Alberto Leal <mail4alberto@gmail.com>
2023-07-18 23:51:37 -0400

    chore(hybrid-cloud): Update migrate_repo task

--- src/sentry/tasks/integrations/migrate_repo.py
+++ src/sentry/tasks/integrations/migrate_repo.py
@@ -1,6 +1,7 @@
 from sentry.constants import ObjectStatus
 from sentry.models import Integration, Organization, Repository
 from sentry.services.hybrid_cloud.organization import organization_service
+from sentry.services.hybrid_cloud.repository import repository_service
 from sentry.tasks.base import instrumented_task, retry
 from sentry.tasks.integrations import logger
 
@@ -13,9 +14,15 @@ from sentry.tasks.integrations import logger
 )
 @retry(exclude=(Integration.DoesNotExist, Repository.DoesNotExist, Organization.DoesNotExist))
 def migrate_repo(repo_id: int, integration_id: int, organization_id: int) -> None:
+    from sentry.mediators.plugins import Migrator
+
     integration = Integration.objects.get(id=integration_id)
     installation = integration.get_installation(organization_id=organization_id)
-    repo = Repository.objects.get(id=repo_id)
+
+    repo = repository_service.get_repository(organization_id=organization_id, id=repo_id)
+    if repo is None:
+        raise Repository.DoesNotExist
+
     if installation.has_repo_access(repo):
         # This probably shouldn't happen, but log it just in case.
         if repo.integration_id is not None and repo.integration_id != integration_id:
@@ -46,8 +53,8 @@ def migrate_repo(repo_id: int, integration_id: int, organization_id: int) -> Non
             },
         )
 
-        from sentry.mediators.plugins import Migrator
+        organization = organization_service.get(id=organization_id)
+        if organization is None:
+            raise Organization.DoesNotExist
 
-        Migrator.run(
-            integration=integration, organization=organization_service.get(id=organization_id)
-        )
+        Migrator.run(integration=integration, organization=organization)
