3e886de6b775761f0587544a625414b53e10d10c
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-07-19 09:57:49 -0400

    ♻️ Move jira server search to control

--- src/sentry/middleware/integrations/parsers/jira_server.py
+++ src/sentry/middleware/integrations/parsers/jira_server.py
@@ -2,15 +2,12 @@ from __future__ import annotations
 
 import logging
 
-from sentry.integrations.jira_server.search import JiraServerSearchEndpoint
 from sentry.integrations.jira_server.webhooks import (
     JiraServerIssueUpdatedWebhook,
     get_integration_from_token,
 )
 from sentry.middleware.integrations.parsers.base import BaseRequestParser
-from sentry.models.organizationmapping import OrganizationMapping
 from sentry.models.outbox import WebhookProviderIdentifier
-from sentry.types.region import RegionResolutionError, get_region_by_name
 
 logger = logging.getLogger(__name__)
 
@@ -18,24 +15,6 @@ logger = logging.getLogger(__name__)
 class JiraServerRequestParser(BaseRequestParser):
     provider = "jira_server"
     webhook_identifier = WebhookProviderIdentifier.JIRA_SERVER
-
-    def get_response_from_search_endpoint(self):
-        organization_slug = self.match.kwargs.get("organization_slug")
-        mapping: OrganizationMapping = OrganizationMapping.objects.filter(
-            slug=organization_slug
-        ).first()
-        if not mapping:
-            logger.error("no_mapping", extra={"slug": organization_slug})
-            return self.get_response_from_control_silo()
-        try:
-            region = get_region_by_name(name=mapping.region_name)
-        except RegionResolutionError:
-            logger.error(
-                "no_region",
-                extra={"slug": organization_slug, "mapping_region": mapping.region_name},
-            )
-            return self.get_response_from_control_silo()
-        return self.get_response_from_region_silo(region=region)
 
     def get_response_from_issue_update_webhook(self):
         token = self.match.kwargs.get("token")
@@ -51,6 +30,5 @@ class JiraServerRequestParser(BaseRequestParser):
     def get_response(self):
         if self.match.func.view_class == JiraServerIssueUpdatedWebhook:  # type: ignore
             return self.get_response_from_issue_update_webhook()
-        elif self.match.func.view_class == JiraServerSearchEndpoint:  # type: ignore
-            return self.get_response_from_search_endpoint()
-        return self.get_response_from_control_silo()
+        else:
+            return self.get_response_from_control_silo()
