4b1cd2c54e3dca22d9a63a7fdc9066e65ba54989
Mark Story <mark@mark-story.com>
2023-07-19 11:44:17 -0400

    feat(typing) First pass at narrowing types on django APIs
    We will be enforcing usage of `using` in transaction.atomic() and
    get_connection() soon via monkeypatching. This mypy plugin allows us
    to reflect the new type requirements in mypy allowing developers to get
    feedback earlier than runtime. The errors from these type extensions
    look like
    
    ```
    src/sentry/silo/safety.py:43: error: Missing positional argument "using" in call to "get_connection"  [call-arg]
    src/sentry/testutils/hybrid_cloud.py:164: error: Argument 1 to "get_connection" has incompatible type "Optional[str]"; expected "str"  [arg-type]
    src/sentry_plugins/github/webhooks/events/installation.py:16: error: All overload variants of "atomic" require at least one argument  [call-overload]
    src/sentry_plugins/github/webhooks/events/installation.py:16: note: Possible overload variants:
    src/sentry_plugins/github/webhooks/events/installation.py:16: note:     def [_C] atomic(using: _C) -> _C
    src/sentry_plugins/github/webhooks/events/installation.py:16: note:     def atomic(using: str, savepoint: bool = ..., durable: bool = ...) -> Atomic
    ```

---
+++ src/sentry/types/mypy.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+from typing import Callable
+
+from mypy.nodes import ARG_POS
+from mypy.plugin import FunctionSigContext, Plugin
+from mypy.types import CallableType, FunctionLike, Instance
+
+
+def replace_transaction_atomic_sig_callback(ctx: FunctionSigContext) -> CallableType:
+    signature = ctx.default_signature
+
+    using_arg = signature.argument_by_name("using")
+    if not using_arg:
+        # No using arg in the signature, bail
+        return signature
+
+    # We care about context managers.
+    ret_type = signature.ret_type
+    if not isinstance(ret_type, Instance):
+        return signature
+
+    # Replace the type and remove the default value of using.
+    str_type = ctx.api.named_generic_type("builtins.str", [])
+
+    arg_types = signature.arg_types[1:]
+    arg_kinds = signature.arg_kinds[1:]
+
+    return signature.copy_modified(
+        arg_kinds=[ARG_POS, *arg_kinds],
+        arg_types=[str_type, *arg_types],
+    )
+
+
+def replace_get_connection_sig_callback(ctx: FunctionSigContext) -> CallableType:
+    signature = ctx.default_signature
+    using_arg = signature.argument_by_name("using")
+    if not using_arg:
+        ctx.api.fail("The using parameter is required", ctx.context)
+
+    str_type = ctx.api.named_generic_type("builtins.str", [])
+
+    return signature.copy_modified(arg_kinds=[ARG_POS], arg_types=[str_type])
+
+
+class SentryMypyPlugin(Plugin):
+    def get_function_signature_hook(
+        self, fullname: str
+    ) -> Callable[[FunctionSigContext], FunctionLike] | None:
+        if fullname == "django.db.transaction.atomic":
+            return replace_transaction_atomic_sig_callback
+        if fullname == "django.db.transaction.get_connection":
+            return replace_get_connection_sig_callback
+        return None
+
+
+def plugin(version: str):
+    return SentryMypyPlugin
