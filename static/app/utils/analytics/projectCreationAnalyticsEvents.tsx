1a8ef7f48047001b47847f10594f50b890cca089
Priscila Oliveira <priscila.oliveira@sentry.io>
2023-07-20 11:17:04 +0200

    feat(source-maps-cli): Add analytics code to the copy to clip. button (#53252)

--- static/app/utils/analytics/projectCreationAnalyticsEvents.tsx
+++ static/app/utils/analytics/projectCreationAnalyticsEvents.tsx
@@ -24,6 +24,10 @@ export type ProjectCreationEventParameters = {
   'project_creation.select_framework_modal_skip_button_clicked': {
     platform: string;
   };
+  'project_creation.source_maps_wizard_button_copy_clicked': {
+    platform: string;
+    project_id: string;
+  };
 };
 
 export const projectCreationEventMap: Record<
@@ -46,4 +50,6 @@ export const projectCreationEventMap: Record<
     'Project Creation: Data Removal Modal Rendered',
   'project_creation.data_removed': 'Project Creation: Data Removed',
   'project_creation.back_button_clicked': 'Project Creation: Back Button Clicked',
+  'project_creation.source_maps_wizard_button_copy_clicked':
+    'Project Creation: Source Maps Wizard Button Copy Clicked',
 };
