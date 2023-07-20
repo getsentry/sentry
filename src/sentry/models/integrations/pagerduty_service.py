b2c05d4b357f4954fba7d8ea48f35981bc49f725
Zach Collins <recursive.cookie.jar@gmail.com>
2023-07-19 20:43:46 -0700

    ref(hc): PageDurtyService silo swap (#53250)
    This is https://github.com/getsentry/sentry/pull/53084 but with the
    additional commit
    https://github.com/getsentry/sentry/commit/7c0ad043c9a0dd0799e4f59c3195fdf7349cebb8
    due to the issue in production caused by bad tests that don't simulate
    the form behavior.

--- src/sentry/models/integrations/pagerduty_service.py
+++ src/sentry/models/integrations/pagerduty_service.py
@@ -1,21 +1,24 @@
 from django.db import models
+from django.db.models import CASCADE
 from django.utils import timezone
 
-from sentry.db.models import BoundedBigIntegerField, DefaultFieldsModel, region_silo_only_model
+from sentry.db.models import BoundedBigIntegerField, DefaultFieldsModel, FlexibleForeignKey
+from sentry.db.models.base import ModelSiloLimit
 from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
-from sentry.models.integrations.organization_integrity_backfill_mixin import (
-    OrganizationIntegrityBackfillMixin,
-)
+from sentry.silo import SiloMode
 
 
-@region_silo_only_model
-class PagerDutyService(OrganizationIntegrityBackfillMixin, DefaultFieldsModel):
+# Temporary -- this will become a control silo model again after a future getsentry merge.
+@ModelSiloLimit(SiloMode.CONTROL, SiloMode.REGION)
+class PagerDutyService(DefaultFieldsModel):
     __include_in_export__ = False
 
-    organization_integration_id = HybridCloudForeignKey(
-        "sentry.OrganizationIntegration", on_delete="CASCADE"
+    organization_integration = FlexibleForeignKey(
+        "sentry.OrganizationIntegration", on_delete=CASCADE, db_constraint=False
     )
-    organization_id = BoundedBigIntegerField(db_index=True)
+
+    organization_id = HybridCloudForeignKey("sentry.Organization", on_delete="cascade")
+
     # From a region point of view, you really only have per organization scoping.
     integration_id = BoundedBigIntegerField(db_index=False)
     integration_key = models.CharField(max_length=255)
