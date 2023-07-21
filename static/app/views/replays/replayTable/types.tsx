2cc6ab990f7bbf569373aa5bf46866564e029631
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-07-21 12:20:39 -0700

    feat(replays): add initial replay cards to replay list (#53323)
    closes #53086
    
    created two experimental tables on the replay list page: 
    1. top 3 most erroneous replays within the last 7 days
    2. top 3 most dead click replays within the last 7 days
    (can change these time periods, I just thought 1 day was too little time
    for a useful sample size since a lot of numbers showed 0)
    
    notes:
    - these tables are static when compared to the big replay list table, so
    pagination doesn't affect them (see below for screenshots showing this)
    - in theory, rage clicks always < dead clicks, which is why I chose to
    sort by dead clicks
    
    page 1:
    <img width="1250" alt="SCR-20230721-jybf"
    src="https://github.com/getsentry/sentry/assets/56095982/1c2f6981-e504-4d90-9f88-4c4dfc4523f5">
    
    
    page 2 (same tables, different replays on big list):
    <img width="1238" alt="SCR-20230721-jxge"
    src="https://github.com/getsentry/sentry/assets/56095982/d4b231e3-8786-4144-8122-cc57af0ef3ca">

--- static/app/views/replays/replayTable/types.tsx
+++ static/app/views/replays/replayTable/types.tsx
@@ -5,6 +5,8 @@ export enum ReplayColumn {
   COUNT_ERRORS = 'countErrors',
   COUNT_RAGE_CLICKS = 'countRageClicks',
   DURATION = 'duration',
+  MOST_ERRONEOUS_REPLAYS = 'mostErroneousReplays',
+  MOST_DEAD_CLICKS = 'mostDeadClicks',
   OS = 'os',
   REPLAY = 'replay',
   SLOWEST_TRANSACTION = 'slowestTransaction',
