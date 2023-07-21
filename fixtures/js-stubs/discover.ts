6322f3e749be60147bab5ab50cb451d4442fa5de
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-21 11:41:09 -0400

    TestStubs.DiscoverSavedQuery

--- fixtures/js-stubs/discover.ts
+++ fixtures/js-stubs/discover.ts
@@ -1,10 +1,13 @@
-export function DiscoverSavedQuery(params = {}) {
+import {SavedQuery} from 'sentry/types';
+
+export function DiscoverSavedQuery(params = {}): SavedQuery {
   return {
     id: '1',
     name: 'Saved query #1',
     dateCreated: '2018-09-24T00:00:00.000Z',
     dateUpdated: '2018-09-24T00:00:00.000Z',
     fields: ['test'],
+    version: 2,
     ...params,
   };
 }
