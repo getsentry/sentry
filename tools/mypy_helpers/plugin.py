0bab8a66d8459851d2b41341652d694a14c40a19
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-07-21 15:25:54 -0400

    ref: hack around mypy's mistreatment of descriptors in unions (#53308)
    working around https://github.com/python/mypy/issues/5570
    
    this is especially important as more things are being moved to unions of
    `HC | Model`

--- tools/mypy_helpers/plugin.py
+++ tools/mypy_helpers/plugin.py
@@ -2,8 +2,8 @@ from __future__ import annotations
 
 from typing import Callable
 
-from mypy.nodes import ARG_POS
-from mypy.plugin import FunctionSigContext, Plugin
+from mypy.nodes import ARG_POS, TypeInfo
+from mypy.plugin import FunctionSigContext, MethodSigContext, Plugin
 from mypy.types import CallableType, FunctionLike, Instance
 
 
@@ -48,11 +48,45 @@ _FUNCTION_SIGNATURE_HOOKS = {
 }
 
 
+def field_descriptor_no_overloads(ctx: MethodSigContext) -> FunctionLike:
+    # ignore the class / non-model instance descriptor overloads
+    signature = ctx.default_signature
+    # replace `def __get__(self, inst: Model, owner: Any) -> _GT:`
+    # with `def __get__(self, inst: Any, owner: Any) -> _GT:`
+    if str(signature.arg_types[0]) == "django.db.models.base.Model":
+        return signature.copy_modified(arg_types=[signature.arg_types[1]] * 2)
+    else:
+        return signature
+
+
 class SentryMypyPlugin(Plugin):
     def get_function_signature_hook(
         self, fullname: str
     ) -> Callable[[FunctionSigContext], FunctionLike] | None:
         return _FUNCTION_SIGNATURE_HOOKS.get(fullname)
+
+    def get_method_signature_hook(
+        self, fullname: str
+    ) -> Callable[[MethodSigContext], FunctionLike] | None:
+        if fullname == "django.db.models.fields.Field":
+            return field_descriptor_no_overloads
+
+        clsname, _, methodname = fullname.rpartition(".")
+        if methodname != "__get__":
+            return None
+
+        clsinfo = self.lookup_fully_qualified(clsname)
+        if clsinfo is None or not isinstance(clsinfo.node, TypeInfo):
+            return None
+
+        fieldinfo = self.lookup_fully_qualified("django.db.models.fields.Field")
+        if fieldinfo is None:
+            return None
+
+        if fieldinfo.node in clsinfo.node.mro:
+            return field_descriptor_no_overloads
+        else:
+            return None
 
 
 def plugin(version: str) -> type[SentryMypyPlugin]:
