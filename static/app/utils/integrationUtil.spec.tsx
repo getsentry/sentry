936ab13a0ad97fa536140c32a336802767a03485
Jian Yuan Lee <jianyuan@gmail.com>
2023-07-21 01:04:59 +0100

    Enable stack trace linking to Bitbucket and VSTS repos on the frontend (#52899)
    The frontend currently assumes GitHub or GitHub Enterprise for stack
    trace linking. This change extends support to include Bitbucket and VSTS
    version control systems.
    
    
    Sample stack trace links:
    1. Bitbucket:
    https://bitbucket.org/jianyuan/sentry-frontend-monitoring/src/master/src/components/App.js#lines-14
    2. VSTS:
    https://jianyuanlee.visualstudio.com/_git/sentry%20frontend%20monitoring?path=/src/components/App.js&version=GBmaster&line=14&lineEnd=15&lineStartColumn=1&lineEndColumn=1&lineStyle=plain&_a=contents

---
+++ static/app/utils/integrationUtil.spec.tsx
@@ -0,0 +1,34 @@
+import {getIntegrationSourceUrl} from 'sentry/utils/integrationUtil';
+
+describe('getIntegrationSourceUrl()', function () {
+  it('returns the correct url for Bitbucket', function () {
+    const result = getIntegrationSourceUrl('bitbucket', 'https://example.com', 10);
+    expect(result).toEqual('https://example.com#lines-10');
+  });
+
+  it('returns the correct url for Bitbucket Server', function () {
+    const result = getIntegrationSourceUrl('bitbucket_server', 'https://example.com', 10);
+    expect(result).toEqual('https://example.com#lines-10');
+  });
+
+  it('returns the correct url for GitHub', function () {
+    const result = getIntegrationSourceUrl('github', 'https://example.com', 10);
+    expect(result).toEqual('https://example.com#L10');
+  });
+
+  it('returns the correct url for GitHub Enterprise', function () {
+    const result = getIntegrationSourceUrl(
+      'github_enterprise',
+      'https://example.com',
+      10
+    );
+    expect(result).toEqual('https://example.com#L10');
+  });
+
+  it('returns the correct url for VSTS', function () {
+    const result = getIntegrationSourceUrl('vsts', 'https://example.com', 10);
+    expect(result).toEqual(
+      'https://example.com/?line=10&lineEnd=11&lineStartColumn=1&lineEndColumn=1&lineStyle=plain&_a=contents'
+    );
+  });
+});
