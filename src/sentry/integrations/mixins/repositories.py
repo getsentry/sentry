986a24ac81c8cb0e9bdd44f0ca36845a62e29958
Alberto Leal <mail4alberto@gmail.com>
2023-07-21 20:05:06 -0400

    fix

--- src/sentry/integrations/mixins/repositories.py
+++ src/sentry/integrations/mixins/repositories.py
@@ -5,7 +5,6 @@ from typing import Collection, Mapping, Sequence
 from sentry_sdk import configure_scope
 
 from sentry.auth.exceptions import IdentityNotValid
-from sentry.constants import ObjectStatus
 from sentry.models import Identity, Repository
 from sentry.services.hybrid_cloud.integration import integration_service
 from sentry.services.hybrid_cloud.repository import RpcRepository, repository_service
