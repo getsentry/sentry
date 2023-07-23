d37f5e70623719a81c81ada73b6b1e361267b991
Spencer Murray <spencer.murray@sentry.io>
2023-07-23 15:05:29 -0700

    Remove unnecessary provider methods

--- src/sentry/identity/discord/provider.py
+++ src/sentry/identity/discord/provider.py
@@ -5,11 +5,5 @@ class DiscordIdentityProvider(OAuth2Provider):
     key = "discord"
     name = "Discord"
 
-    # https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
-    oauth_scope = ("identity", "email")
-
-    def get_oauth_authorize_url(self):
-        return "https://discord.com/oauth2/authorize"
-
-    def get_oauth_token_url(self):
-        return "https://discord.com/api/oauth2/token"
+    # We don't actually use OAuth2 for Discord identities, so this class is
+    # just here so we can register a provider.
