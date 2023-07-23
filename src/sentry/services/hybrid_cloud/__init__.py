62643d3551d5f74c1be495dcb98d7c92f7244c03
Zach Collins <recursive.cookie.jar@gmail.com>
2023-07-23 16:07:55 -0700

    fix(hc): Support split db monolith mode via smarter delegators (#53395)
    Same as https://github.com/getsentry/sentry/pull/53365/files but
    conditionalizing the testutil import to prevent issue in prod.

--- src/sentry/services/hybrid_cloud/__init__.py
+++ src/sentry/services/hybrid_cloud/__init__.py
@@ -25,10 +25,13 @@ from typing import (
 
 import pydantic
 import sentry_sdk
+from django.db import router, transaction
+from django.db.models import Model
 from typing_extensions import Self
 
 from sentry.db.postgres.transactions import in_test_assert_no_transaction
 from sentry.silo import SiloMode
+from sentry.utils.env import in_test_environment
 
 logger = logging.getLogger(__name__)
 
@@ -226,6 +229,47 @@ class DelegatedBySiloMode(Generic[ServiceInterface]):
         raise KeyError(f"No implementation found for {cur_mode}.")
 
 
+class DelegatedByOpenTransaction(Generic[ServiceInterface]):
+    """
+    It is possible to run monolith mode in a split database scenario -- in this case, the silo mode does not help
+    select the correct implementation to ensure non mingled transactions.  This helper picks a backing implementation
+    by checking if an open transaction exists for the routing of the given model for a backend implementation.
+
+    If no transactions are open, it uses a given default implementation instead.
+    """
+
+    _constructors: Mapping[Type[Model], Callable[[], ServiceInterface]]
+    _default: Callable[[], ServiceInterface]
+
+    def __init__(
+        self,
+        mapping: Mapping[Type[Model], Callable[[], ServiceInterface]],
+        default: Callable[[], ServiceInterface],
+    ):
+        self._constructors = mapping
+        self._default = default
+
+    def __getattr__(self, item: str) -> Any:
+        for model, constructor in self._constructors.items():
+            if in_test_environment():
+                from sentry.testutils.hybrid_cloud import simulated_transaction_watermarks
+
+                open_transaction = (
+                    simulated_transaction_watermarks.connection_transaction_depth_above_watermark(
+                        using=router.db_for_write(model)
+                    )
+                    > 0
+                )
+            else:
+                open_transaction = transaction.get_connection(
+                    router.db_for_write(model)
+                ).in_atomic_block
+
+            if open_transaction:
+                return getattr(constructor(), item)
+        return getattr(self._default(), item)
+
+
 hc_test_stub: Any = threading.local()
 
 
@@ -299,8 +343,31 @@ def silo_mode_delegation(
     """
     Simply creates a DelegatedBySiloMode from a mapping object, but casts it as a ServiceInterface matching
     the mapping values.
+
+    In split database mode, it will also inject DelegatedByOpenTransaction in for the monolith mode implementation.
     """
-    return cast(ServiceInterface, DelegatedBySiloMode(mapping))
+
+    def delegator() -> ServiceInterface:
+        from sentry.models import Organization, User
+
+        return cast(
+            ServiceInterface,
+            DelegatedByOpenTransaction(
+                {
+                    User: mapping[SiloMode.CONTROL],
+                    Organization: mapping[SiloMode.REGION],
+                },
+                mapping[SiloMode.MONOLITH],
+            ),
+        )
+
+    # We need to retain a closure around the original mapping passed in, so we'll use a new variable here
+    final_mapping: Mapping[SiloMode, Callable[[], ServiceInterface]] = {
+        SiloMode.MONOLITH: delegator,
+        **({k: v for k, v in mapping.items() if k != SiloMode.MONOLITH}),
+    }
+
+    return cast(ServiceInterface, DelegatedBySiloMode(final_mapping))
 
 
 def coerce_id_from(m: object | int | None) -> int | None:
