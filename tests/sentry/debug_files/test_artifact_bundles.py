1d4888ff1625148458d3e3e6ed76ab43631f2aa1
Arpad Borsos <arpad.borsos@sentry.io>
2023-07-24 12:11:27 +0200

    Validate assemble_artifact arguments earlier (#53333)
    - Ensures that we always have a `Project` for ArtifactBundles
    - Ensures that there is always a release for a Releasebundle
    - Defaults the `bundle_id` earlier
    
    Co-authored-by: Riccardo Busetti <riccardo.busetti@sentry.io>

--- tests/sentry/debug_files/test_artifact_bundles.py
+++ tests/sentry/debug_files/test_artifact_bundles.py
@@ -97,6 +97,10 @@ class ArtifactLookupTest(TestCase):
                     "url": "~/path/to/other1.js",
                     "content": b"other1_idx1",
                 },
+                "path/in/zip/baz": {
+                    "url": "~/path/to/only1.js",
+                    "content": b"only1_idx1",
+                },
             }
         )
         upload_bundle(bundle, self.project, "1.0.0")
@@ -127,12 +131,14 @@ class ArtifactLookupTest(TestCase):
         bundles = get_artifact_bundles(self.project, "1.0.0")
         assert len(bundles) == 2
         indexed = get_indexed_files(self.project, "1.0.0", distinct=True)
-        assert len(indexed) == 2
+        assert len(indexed) == 3
 
         assert indexed[0].url == "~/path/to/app.js"
         assert indexed[0].artifact_bundle == bundles[1]
-        assert indexed[1].url == "~/path/to/other1.js"
-        assert indexed[1].artifact_bundle == bundles[1]
+        assert indexed[1].url == "~/path/to/only1.js"
+        assert indexed[1].artifact_bundle == bundles[0]
+        assert indexed[2].url == "~/path/to/other1.js"
+        assert indexed[2].artifact_bundle == bundles[1]
 
         bundle = make_compressed_zip_file(
             {
@@ -152,13 +158,15 @@ class ArtifactLookupTest(TestCase):
         bundles = get_artifact_bundles(self.project, "1.0.0")
         assert len(bundles) == 3
         indexed = get_indexed_files(self.project, "1.0.0", distinct=True)
-        assert len(indexed) == 3
+        assert len(indexed) == 4
 
         # here, we use the more recent bundle for the shared file,
         # all other files are disjoint in this example
         assert indexed[0].url == "~/path/to/app.js"
         assert indexed[0].artifact_bundle == bundles[2]
-        assert indexed[1].url == "~/path/to/other1.js"
-        assert indexed[1].artifact_bundle == bundles[1]
-        assert indexed[2].url == "~/path/to/other2.js"
-        assert indexed[2].artifact_bundle == bundles[2]
+        assert indexed[1].url == "~/path/to/only1.js"
+        assert indexed[1].artifact_bundle == bundles[0]
+        assert indexed[2].url == "~/path/to/other1.js"
+        assert indexed[2].artifact_bundle == bundles[1]
+        assert indexed[3].url == "~/path/to/other2.js"
+        assert indexed[3].artifact_bundle == bundles[2]
