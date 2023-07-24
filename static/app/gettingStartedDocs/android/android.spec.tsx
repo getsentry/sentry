88daabf286effaf14d06da55882b8412fec93c4c
Roman Zavarnitsyn <rom4ek93@gmail.com>
2023-07-24 09:40:28 +0200

    ref(getting-started-docs): Migrate android wizard to the main sentry repo (#53351)

---
+++ static/app/gettingStartedDocs/android/android.spec.tsx
@@ -0,0 +1,20 @@
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
+
+import {GettingStartedWithAndroid, steps} from './android';
+
+describe('GettingStartedWithAndroid', function () {
+  it('renders doc correctly', function () {
+    const {container} = render(<GettingStartedWithAndroid dsn="test-dsn" />);
+
+    // Steps
+    for (const step of steps()) {
+      expect(
+        screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
+      ).toBeInTheDocument();
+    }
+
+    expect(container).toSnapshot();
+  });
+});
