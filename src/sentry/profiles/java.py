177a38f6725071e617d7dd16937129960f729a2a
Pierre Massat <pierre.massat@sentry.io>
2023-07-24 12:47:49 -0400

    Handle corner cases

--- src/sentry/profiles/java.py
+++ src/sentry/profiles/java.py
@@ -20,25 +20,25 @@ def parse_obfuscated_signature(signature: str) -> Tuple[List[str], str]:
     parameter_types, return_type = signature.rsplit(")", 1)
     types = []
     i = 0
-    is_array = False
+    arrays = 0
 
     while i < len(parameter_types):
         t = parameter_types[i]
 
         if t in JAVA_BASE_TYPES:
-            start_index = i - int(is_array)
+            start_index = i - arrays
             types.append(parameter_types[start_index : i + 1])
-            is_array = False
+            arrays = 0
         elif t == "L":
-            start_index = i - int(is_array)
+            start_index = i - arrays
             end_index = parameter_types[i:].index(";")
             types.append(parameter_types[start_index : i + end_index + 1])
-            is_array = False
+            arrays = 0
             i += end_index
         elif t == "[":
-            is_array = True
+            arrays += 1
         else:
-            is_array = False
+            arrays = 0
 
         i += 1
 
@@ -48,7 +48,7 @@ def parse_obfuscated_signature(signature: str) -> Tuple[List[str], str]:
 # format_signature formats the types into a human-readable signature
 def format_signature(parameter_java_types: List[str], return_java_type: str) -> str:
     signature = f"({', '.join(parameter_java_types)})"
-    if return_java_type != "void":
+    if return_java_type and return_java_type != "void":
         signature += f": {return_java_type}"
     return signature
 
@@ -68,6 +68,9 @@ def byte_code_type_to_java_type(byte_code_type: str) -> str:
 # map_obfucated_signature will parse then deobfuscated a signature and
 # format it appropriately
 def map_obfuscated_signature(mapper, signature: str) -> str:
+    if not signature:
+        return ""
+
     parameter_types, return_type = parse_obfuscated_signature(signature)
     parameter_java_types = []
 
@@ -78,7 +81,7 @@ def map_obfuscated_signature(mapper, signature: str) -> str:
             new_class = mapped
         parameter_java_types.append(new_class)
 
-    return_java_type = byte_code_type_to_java_type(return_type)
+    return_java_type = byte_code_type_to_java_type(return_type) if return_type else ""
     mapped = mapper.remap_class(return_java_type)
     if mapped:
         return_java_type = mapped
