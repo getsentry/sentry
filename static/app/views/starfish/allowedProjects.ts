7c8d0335a06a43c18ecc18dd975c6dd2701fa8c0
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-24 14:47:30 -0400

    Add project ID annotations

--- static/app/views/starfish/allowedProjects.ts
+++ static/app/views/starfish/allowedProjects.ts
@@ -3,7 +3,10 @@ import {StarfishType} from 'sentry/views/starfish/types';
 export const ALLOWED_PROJECT_IDS_FOR_ORG_SLUG: {
   [slug: string]: string[];
 } = {
-  sentry: ['1', '300688'],
+  sentry: [
+    '1', // Sentry
+    '300688', // Snuba
+  ],
   codecov: ['5215654'],
   peated: ['4505138082349056'],
   'sentry-sdks': ['5428557'],
