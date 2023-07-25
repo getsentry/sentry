32d6206991bf66102a1c6f05f36ec87d4ca20dae
Roman Zavarnitsyn <rom4ek93@gmail.com>
2023-07-25 11:25:36 +0200

    ref(getting-started-docs): Migrate apple wizards to the main sentry repo (#53436)

---
+++ static/app/gettingStartedDocs/apple/apple-macos.spec.tsx
@@ -0,0 +1,20 @@
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
+
+import {GettingStartedWithMacos, steps} from './apple-macos';
+
+describe('GettingStartedWithMacos', function () {
+  it('renders doc correctly', function () {
+    const {container} = render(<GettingStartedWithMacos dsn="test-dsn" />);
+
+    // Steps
+    for (const step of steps()) {
+      expect(
+        screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
+      ).toBeInTheDocument();
+    }
+
+    expect(container).toSnapshot();
+  });
+});
