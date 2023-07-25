c962a23ba48e755a30c3941afba4c5d2c709ba79
Spencer Murray <62224025+spalmurray@users.noreply.github.com>
2023-07-25 22:15:33 +0000

    feat(discord): Add Discord client secret option (#53565)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1460,7 +1460,7 @@ SENTRY_FEATURES = {
     # Allow orgs to create a Discord integration
     "organizations:integrations-discord": False,
     # Enable Discord integration notifications
-    "organizations:integrations-discord-notifications"
+    "organizations:integrations-discord-notifications": False,
     # Enable Opsgenie integration
     "organizations:integrations-opsgenie": False,
     # Limit project events endpoint to only query back a certain number of days
