4495818ff9a80f2954a35cab086b419bd7d98d02
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-25 15:20:18 -0400

    Revert "Improve abbreviated number formatting"
    This reverts commit 2220f56b3979081afb2e182eff7f48f1c9a012e4.

--- static/app/utils/formatters.tsx
+++ static/app/utils/formatters.tsx
@@ -286,7 +286,7 @@ const numberFormats = [
   [1000, 'k'],
 ] as const;
 
-export function formatAbbreviatedNumber(number: number | string): string {
+export function formatAbbreviatedNumber(number: number | string) {
   number = Number(number);
 
   let lookup: (typeof numberFormats)[number];
@@ -302,11 +302,11 @@ export function formatAbbreviatedNumber(number: number | string): string {
     }
 
     return shortValue / 10 > 1 || !fitsBound
-      ? `${formatFloat(shortValue, 1)}${suffix}`
+      ? `${shortValue}${suffix}`
       : `${formatFloat(number / suffixNum, 1)}${suffix}`;
   }
 
-  return `${formatFloat(number, 1)}`;
+  return number.toLocaleString();
 }
 
 export function formatRate(value: number, unit?: RateUnits) {
