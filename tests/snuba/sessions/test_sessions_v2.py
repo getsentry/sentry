84acc73e9718cb73703132cf9113cc438180b4ff
Cathy Teng <cathy.teng@sentry.io>
2023-07-25 13:55:35 -0700

    update test

--- tests/snuba/sessions/test_sessions_v2.py
+++ tests/snuba/sessions/test_sessions_v2.py
@@ -69,11 +69,12 @@ def test_inclusive_end():
 
 
 @freeze_time("2021-03-05T11:00:00.000Z")
-def test_future_start():
-    with pytest.raises(InvalidParams):
-        get_constrained_date_range(
-            {"start": "2021-03-05T12:00:00", "end": "2021-03-05T13:00:00", "interval": "1h"},
-        )
+def test_future_request():
+    start, end, interval = get_constrained_date_range(
+        {"start": "2021-03-05T12:00:00", "end": "2021-03-05T13:00:00", "interval": "1h"},
+    )
+    assert start == datetime(2021, 3, 5, 11, tzinfo=pytz.utc)
+    assert end == datetime(2021, 3, 5, 11, 1, tzinfo=pytz.utc)
 
 
 @freeze_time("2021-03-05T11:14:17.105Z")
