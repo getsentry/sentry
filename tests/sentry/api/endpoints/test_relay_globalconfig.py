8dab454125f9352593f2eefe995864ceec20b92e
Tor Berge Saebjoernsen <tor.saebjoernsen@sentry.io>
2023-07-25 10:46:37 +0200

    wip

--- tests/sentry/api/endpoints/test_relay_globalconfig.py
+++ tests/sentry/api/endpoints/test_relay_globalconfig.py
@@ -1,16 +1,44 @@
+from uuid import uuid4
+
 import pytest
 from django.urls import reverse
+from sentry_relay.auth import generate_key_pair
 
+from sentry.models.relay import Relay
 from sentry.relay.config.measurements import BUILTIN_MEASUREMENTS
 from sentry.relay.config.metric_extraction import HISTOGRAM_OUTLIER_RULES
 from sentry.utils import json
 from sentry.utils.pytest.fixtures import django_db_all
-from tests.sentry.api.endpoints.conftest import private_key, relay
 
 
 @pytest.fixture
-def call_global_config():
-    def inner(client):
+def key_pair():
+    return generate_key_pair()
+
+
+@pytest.fixture
+def public_key(key_pair):
+    return key_pair[1]
+
+
+@pytest.fixture
+def private_key(key_pair):
+    return key_pair[0]
+
+
+@pytest.fixture
+def relay_id():
+    return str(uuid4())
+
+
+@pytest.fixture
+def relay(relay_id, public_key):
+    return Relay.objects.create(relay_id=relay_id, public_key=str(public_key), is_internal=True)
+
+
+@pytest.fixture
+def call_global_config(client, relay, private_key):
+    def inner():
         path = reverse("sentry-api-0-relay-projectconfigs")
 
         raw_json, signature = private_key.pack({"globalConfig": True})
