641ccc33a970ac50b3b656b055b429bfca59e3dd
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-07-25 10:56:17 -0400

    fix(starfish): add quotes around transaction name (#53516)
    Add quotes around transaction name to prevent errors when the
    transaction name has a space in it.

--- static/app/views/starfish/queries/useSpanMetrics.tsx
+++ static/app/views/starfish/queries/useSpanMetrics.tsx
@@ -58,7 +58,7 @@ function getEventView(
       name: '',
       query: `${SPAN_GROUP}:${group}${
         queryFilters?.transactionName
-          ? ` transaction:${queryFilters?.transactionName}`
+          ? ` transaction:"${queryFilters?.transactionName}"`
           : ''
       }${
         queryFilters?.['transaction.method']
