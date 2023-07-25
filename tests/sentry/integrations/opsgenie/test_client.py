fafd9d0b50f4da3f2e7dd9d5a87b04ca9eb132a5
Michelle Fu <michelle.fu@sentry.io>
2023-07-25 16:18:38 -0700

    add notification tests

--- tests/sentry/integrations/opsgenie/test_client.py
+++ tests/sentry/integrations/opsgenie/test_client.py
@@ -1,7 +1,8 @@
 import responses
 
-from sentry.models import Integration, OrganizationIntegration
+from sentry.models import Integration, OrganizationIntegration, Rule
 from sentry.testutils import APITestCase
+from sentry.utils import json
 
 EXTERNAL_ID = "test-app"
 METADATA = {
@@ -46,3 +47,51 @@ class OpsgenieClientTest(APITestCase):
 
         resp = client.get_team_id(team_name="cool-team")
         assert resp == resp_data
+
+    @responses.activate
+    def test_send_notification(self):
+        resp_data = {
+            "result": "Request will be processed",
+            "took": 0.302,
+            "requestId": "43a29c5c-3dbf-4fa4-9c26-f4f71023e120",
+        }
+        responses.add(responses.POST, url="https://api.opsgenie.com/v2/alerts", json=resp_data)
+
+        event = self.store_event(
+            data={
+                "message": "Hello world",
+                "level": "warning",
+                "platform": "python",
+                "culprit": "foo.bar",
+            },
+            project_id=self.project.id,
+        )
+        group = event.group
+        assert group is not None
+
+        rule = Rule.objects.create(project=self.project, label="my rule")
+        client = self.installation.get_client(integration_key="1234-5678")
+        with self.options({"system.url-prefix": "http://example.com"}):
+            client.send_notification(event, [rule])
+
+        request = responses.calls[0].request
+        payload = json.loads(request.body)
+        group_id = str(group.id)
+        assert payload == {
+            "tags": ["level:warning"],
+            "entity": "foo.bar",
+            "alias": "sentry: %s" % group_id,
+            "details": {
+                "Project Name": self.project.name,
+                "Triggering Rules": "my rule",
+                "Sentry Group": "Hello world",
+                "Sentry ID": group_id,
+                "Logger": "",
+                "Level": "warning",
+                "Project ID": "bar",
+                "URL": "http://example.com/organizations/baz/issues/%s/" % group_id,
+                "Release": event.release,
+            },
+            "message": "Hello world",
+            "source": "Sentry",
+        }
---
+++ tests/sentry/integrations/opsgenie/test_notify_action.py
@@ -0,0 +1,160 @@
+import responses
+
+from sentry.integrations.opsgenie import OpsgenieNotifyTeamAction
+from sentry.models import Integration, OrganizationIntegration
+from sentry.silo import SiloMode
+from sentry.testutils.cases import PerformanceIssueTestCase, RuleTestCase
+from sentry.testutils.silo import assume_test_silo_mode, region_silo_test
+from sentry.utils import json
+
+METADATA = {
+    "api_key": "1234-ABCD",
+    "base_url": "https://api.opsgenie.com/",
+    "domain_name": "test-app.app.opsgenie.com",
+}
+
+
+@region_silo_test(stable=True)
+class OpsgenieNotifyTeamTest(RuleTestCase, PerformanceIssueTestCase):
+    rule_cls = OpsgenieNotifyTeamAction
+
+    def create_integration(self, name):
+        integration = Integration.objects.create(
+            provider="opsgenie", name=name, external_id=name, metadata=METADATA
+        )
+        integration.add_organization(self.organization, self.user)
+        return integration
+
+    def setUp(self):
+        self.team1 = {"id": "123-id", "team": "cool-team", "integration_key": "1234-5678"}
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            self.integration = self.create_integration(name="test-app")
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            self.org_integration = OrganizationIntegration.objects.get(
+                organization_id=self.organization.id, integration_id=self.integration.id
+            )
+            self.org_integration.config = {"team_table": [self.team1]}
+            self.org_integration.save()
+        self.installation = self.integration.get_installation(self.organization.id)
+
+    @responses.activate
+    def test_applies_correctly(self):
+        event = self.store_event(
+            data={
+                "message": "Hello world",
+                "level": "warning",
+                "platform": "python",
+                "culprit": "foo.bar",
+            },
+            project_id=self.project.id,
+        )
+
+        rule = self.get_rule(data={"account": self.integration.id, "team": self.team1["id"]})
+        results = list(rule.after(event=event, state=self.get_state()))
+        assert len(results) == 1
+
+        responses.add(
+            responses.POST,
+            url="https://api.opsgenie.com/v2/alerts",
+            json={},
+            status=200,
+        )
+
+        results[0].callback(event, futures=[])
+        data = json.loads(responses.calls[0].request.body)
+
+        assert data["message"] == event.message
+        assert data["details"]["Sentry ID"] == str(event.group.id)
+
+    @responses.activate
+    def test_render_label(self):
+        rule = self.get_rule(data={"account": self.integration.id, "team": self.team1["id"]})
+
+        assert (
+            rule.render_label()
+            == "Send a notification to Opsgenie account test-app and team cool-team"
+        )
+
+    @responses.activate
+    def test_render_label_without_integration(self):
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            self.integration.delete()
+
+        rule = self.get_rule(data={"account": self.integration.id, "team": self.team1["id"]})
+
+        label = rule.render_label()
+        assert label == "Send a notification to Opsgenie account [removed] and team [removed]"
+
+    @responses.activate
+    def test_valid_team_options(self):
+        new_org = self.create_organization(name="New Org", owner=self.user)
+
+        new_project = self.create_project(name="new proj", organization=new_org)
+        self.project = new_project
+
+        team2 = {"id": "456-id", "team": "cooler-team", "integration_key": "1234-7890"}
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            self.integration.add_organization(new_org, self.user)
+            oi = OrganizationIntegration.objects.get(organization_id=new_org.id)
+            oi.config = {"team_table": [team2]}
+            oi.save()
+
+        rule = self.get_rule(data={"account": self.integration.id})
+        team_options = rule.get_teams()
+        assert team_options == [(team2["id"], team2["team"])]
+        assert "choice" == rule.form_fields["team"]["type"]
+        assert team_options == rule.form_fields["team"]["choices"]
+
+    @responses.activate
+    def test_valid_team_selected(self):
+        rule = self.get_rule(data={"account": self.integration.id, "team": self.team1["id"]})
+        form = rule.get_form_instance()
+        assert form.is_valid()
+
+    @responses.activate
+    def test_notifies_with_multiple_og_accounts(self):
+        team2 = {"id": "456-id", "team": "cooler-team", "integration_key": "1234-7890"}
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            integration = self.create_integration("test-app-2")
+            org_integration = OrganizationIntegration.objects.get(
+                organization_id=self.organization.id, integration_id=integration.id
+            )
+            org_integration.config = {"team_table": [team2]}
+            org_integration.save()
+        self.installation = integration.get_installation(self.organization.id)
+        event = self.get_event()
+        rule = self.get_rule(data={"account": integration.id, "team": team2["id"]})
+
+        results = list(rule.after(event=event, state=self.get_state()))
+        assert len(results) == 1
+
+        responses.add(
+            responses.POST,
+            url="https://api.opsgenie.com/v2/alerts",
+            json={},
+            status=200,
+        )
+
+        results[0].callback(event, futures=[])
+        data = json.loads(responses.calls[0].request.body)
+
+        assert data["message"] == event.message
+        assert data["details"]["Sentry ID"] == str(event.group.id)
+
+    @responses.activate
+    def test_invalid_team_selected(self):
+        team2 = {"id": "456-id", "team": "cooler-team", "integration_key": "1234-7890"}
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            integration = self.create_integration("test-app-2")
+            org_integration = OrganizationIntegration.objects.get(
+                organization_id=self.organization.id, integration_id=integration.id
+            )
+            org_integration.config = {"team_table": [team2]}
+            org_integration.save()
+        self.installation = integration.get_installation(self.organization.id)
+
+        rule = self.get_rule(data={"account": self.integration.id, "team": team2["id"]})
+
+        form = rule.get_form_instance()
+        assert not form.is_valid()
+        assert len(form.errors) == 1
