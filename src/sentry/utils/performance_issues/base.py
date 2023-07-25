0597a70fad2c896995b42c9da51a895e11be7aea
Abdkhan14 <60121741+Abdkhan14@users.noreply.github.com>
2023-07-25 11:25:35 -0400

    feat(perf-detector-threshold-configuration) Added new thresholds and … (#53460)
    For project: [Detector Threshold
    Configurations](https://www.notion.so/sentry/Detector-Threshold-Configuration-f8cb07e7cceb42388cedb09ea05fc116)
    Lowered defaults values and effects:
    [link](https://www.notion.so/sentry/Detector-Dry-Run-Results-40dc7a3e4d8b4b9ea8da90608fe54747)
    
    - We are GA'ing the project with lowered default values for thresholds
    and adding new thresholds.
    - Changed default threshold for Consecutive HTTP to 500ms to match prod
    sentry_option value:
    [query](https://redash.getsentry.net/queries/4546/source)
    - Added lowered defaults for N+1 DB duration (iterating down to 50ms),
    Slow DB Queries duration(iterating down to 500ms), Consecutive http span
    duration (iterating down to 500ms)
    - Updated Consecutive HTTP Detector to add new total min time saved
    threshold with a default of 2000s.
    - Updated Consecutive HTTP Detector tests to ensure new total duration
    threshold is respected.
    - Removed Extended Consecutive HTTP Detector  and tests.
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- src/sentry/utils/performance_issues/base.py
+++ src/sentry/utils/performance_issues/base.py
@@ -37,7 +37,6 @@ class DetectorType(Enum):
     N_PLUS_ONE_API_CALLS = "n_plus_one_api_calls"
     CONSECUTIVE_DB_OP = "consecutive_db"
     CONSECUTIVE_HTTP_OP = "consecutive_http"
-    CONSECUTIVE_HTTP_OP_EXTENDED = "consecutive_http_ext"
     LARGE_HTTP_PAYLOAD = "large_http_payload"
     FILE_IO_MAIN_THREAD = "file_io_main_thread"
     M_N_PLUS_ONE_DB = "m_n_plus_one_db"
@@ -57,7 +56,6 @@ DETECTOR_TYPE_TO_GROUP_TYPE = {
     DetectorType.M_N_PLUS_ONE_DB: PerformanceMNPlusOneDBQueriesGroupType,
     DetectorType.UNCOMPRESSED_ASSETS: PerformanceUncompressedAssetsGroupType,
     DetectorType.CONSECUTIVE_HTTP_OP: PerformanceConsecutiveHTTPQueriesGroupType,
-    DetectorType.CONSECUTIVE_HTTP_OP_EXTENDED: PerformanceConsecutiveHTTPQueriesGroupType,
     DetectorType.DB_MAIN_THREAD: PerformanceDBMainThreadGroupType,
     DetectorType.LARGE_HTTP_PAYLOAD: PerformanceLargeHTTPPayloadGroupType,
     DetectorType.HTTP_OVERHEAD: PerformanceHTTPOverheadGroupType,
