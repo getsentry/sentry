d56e3f102cf3fd7cf9574e964034f91124ca40fc
17hogeju <17hogeju@gmail.com>
2023-07-25 15:45:03 -0700

    Add tests for stacktrace improvements

--- static/app/components/events/interfaces/crashContent/stackTrace/content.spec.tsx
+++ static/app/components/events/interfaces/crashContent/stackTrace/content.spec.tsx
@@ -155,6 +155,68 @@ describe('StackTrace', function () {
     expect(omittedFrames).toBeInTheDocument();
   });
 
+  describe('with stacktrace improvements feature flag enabled', function () {
+    const organization = TestStubs.Organization({
+      features: ['issue-details-stacktrace-improvements'],
+    });
+
+    it('does not render non in app tags', function () {
+      const dataFrames = [...data.frames];
+      dataFrames[0] = {...dataFrames[0], inApp: false};
+
+      const newData = {
+        ...data,
+        frames: dataFrames,
+      };
+
+      renderedComponent({
+        organization,
+        data: newData,
+      });
+
+      expect(screen.queryByText('System')).not.toBeInTheDocument();
+    });
+
+    it('displays a toggle button when there is more than one non-inapp frame', function () {
+      const dataFrames = [...data.frames];
+      dataFrames[0] = {...dataFrames[0], inApp: true};
+
+      const newData = {
+        ...data,
+        frames: dataFrames,
+      };
+
+      renderedComponent({
+        organization,
+        data: newData,
+        includeSystemFrames: false,
+      });
+
+      expect(screen.getByText('Show 3 more frames')).toBeInTheDocument();
+    });
+
+    it('does not display a toggle button when there is only one non-inapp frame', function () {
+      const dataFrames = [...data.frames];
+      dataFrames[0] = {...dataFrames[0], inApp: true};
+      dataFrames[2] = {...dataFrames[2], inApp: true};
+      dataFrames[4] = {...dataFrames[4], inApp: true};
+
+      const newData = {
+        ...data,
+        frames: dataFrames,
+      };
+
+      renderedComponent({
+        organization,
+        data: newData,
+        includeSystemFrames: false,
+      });
+
+      expect(screen.queryByText('more frames')).not.toBeInTheDocument();
+      expect(screen.queryByText('more frame')).not.toBeInTheDocument();
+    });
+  });
+
   describe('if there is a frame with in_app equal to true, display only in_app frames', function () {
     it('displays crashed from only', function () {
       const dataFrames = [...data.frames];
