d4a5ef58e8053b192e4ff1cae7d748eceeb6561e
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-26 13:38:50 -0400

    Revert "Allow code blocks to wrap"
    This reverts commit 9f0d60a1c6ebcaa208a0b6833dbc7cee263482e0.

--- static/app/styles/prism.tsx
+++ static/app/styles/prism.tsx
@@ -38,7 +38,7 @@ export const prismStyles = (theme: Theme) => css`
     font-family: ${theme.text.familyMono};
     direction: ltr;
     text-align: left;
-    white-space: pre-wrap;
+    white-space: pre;
     word-spacing: normal;
     word-break: normal;
     -moz-tab-size: 4;
