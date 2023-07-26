feb9103a665054c27f6951f2c8625894cba6ea95
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-07-26 15:00:32 -0700

    Fix serialization bugs arising from GroupSerializerTest
    (cherry picked from commit b666ec996f903546d0317c373c5adaffe2616278)

--- src/sentry/api/serializers/models/incidentseen.py
+++ src/sentry/api/serializers/models/incidentseen.py
@@ -1,25 +1,17 @@
 from sentry.api.serializers import Serializer, register
 from sentry.incidents.models import IncidentSeen
-from sentry.services.hybrid_cloud.user import RpcUser
-from sentry.services.hybrid_cloud.user.serial import serialize_rpc_user
+from sentry.services.hybrid_cloud.user.serial import serialize_generic_user
 from sentry.services.hybrid_cloud.user.service import user_service
 
 
 @register(IncidentSeen)
 class IncidentSeenSerializer(Serializer):
     def get_attrs(self, item_list, user):
-        serialize_as_user = (
-            None
-            if user.id is None
-            else user
-            if isinstance(user, RpcUser)
-            else serialize_rpc_user(user)
-        )
         item_users = user_service.serialize_many(
             filter={
                 "user_ids": [i.user_id for i in item_list],
             },
-            as_user=serialize_as_user,
+            as_user=serialize_generic_user(user),
         )
         user_map = {d["id"]: d for d in item_users}
 
