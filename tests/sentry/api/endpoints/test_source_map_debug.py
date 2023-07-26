09272ea9561b8fb9a9a3addfe20cc055caad70a5
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2023-07-26 11:00:34 -0700

    fix(source-map-debug): Make index check more robust (#53582)
    this pr adds in a check to make sure frame_idx and exception_idx are
    integers.
    
    Resolves SENTRY-141S

--- tests/sentry/api/endpoints/test_source_map_debug.py
+++ tests/sentry/api/endpoints/test_source_map_debug.py
@@ -71,6 +71,33 @@ class SourceMapDebugEndpointTestCase(APITestCase):
         )
         assert resp.data["detail"] == "Query parameter 'frame_idx' is required"
 
+    def test_non_integer_frame_given(self):
+        event = self.store_event(
+            data={"event_id": "a" * 32, "release": "my-release"}, project_id=self.project.id
+        )
+        resp = self.get_error_response(
+            self.organization.slug,
+            self.project.slug,
+            event.event_id,
+            frame_idx="hello",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+        assert resp.data["detail"] == "Query parameter 'frame_idx' must be an integer"
+
+    def test_non_integer_exception_given(self):
+        event = self.store_event(
+            data={"event_id": "a" * 32, "release": "my-release"}, project_id=self.project.id
+        )
+        resp = self.get_error_response(
+            self.organization.slug,
+            self.project.slug,
+            event.event_id,
+            frame_idx=0,
+            exception_idx="hello",
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+        assert resp.data["detail"] == "Query parameter 'exception_idx' must be an integer"
+
     def test_frame_out_of_bounds(self):
         event = self.store_event(
             data=self.base_data,
