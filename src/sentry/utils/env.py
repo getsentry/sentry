5563968a22a25ecb6b30d13967f75801dd0609fd
Alex Zaslavsky <azaslavsky@users.noreply.github.com>
2023-07-27 09:32:33 -0700

    test(backup): Add release version testgen script
    The new `generate-backup-test-for-release` binary is meant to be run
    after every new release version is cut to create a new PR with a backup
    test for the new version added. Because we already test the script it
    uses to generate the test case with the `test_at_head` test method, we
    can be sure that this new test will succeed at the time that it is
    created.
    
    As of this commit, this script must be invoked manual, but an upcoming
    change will modify the "release" GitHub Action for Sentry to
    auto-generate this PR and send it team-ospo for review.
    
    Each run of the script makes two modifications to the source: 1. it
    creates a new fixture for the just cut version at
    `fixtures/backup/X.Y.Z.json`, and 2. it appends a test method like `def
    test_at_X_Y_Z(self):` to `tests/sentry/backup/test_releases.py`. If
    either of these already exist, the script execution is a functional noop.
    
    This script was tested by being invoked to create the 23.7.1 tests seen
    in this commit. A second run of the script produced no changes (save
    "date_added" in the JSON file), confirming the functional noop nature
    when tests already exist.
    
    Issue: getsentry/team-ospo#159

--- src/sentry/utils/env.py
+++ src/sentry/utils/env.py
@@ -2,4 +2,5 @@ import sys
 
 
 def in_test_environment():
-    return "pytest" in sys.argv[0] or "vscode" in sys.argv[0]
+    test_env_breadcrumbs = {"pytest", "vscode", "generate-backup-test-for-release"}
+    return any(needle in sys.argv[0] for needle in test_env_breadcrumbs)
