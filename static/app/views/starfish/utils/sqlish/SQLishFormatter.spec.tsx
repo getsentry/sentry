171cba113ac8c87a377715aa7744f5cd3e959d61
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-27 10:23:43 -0400

    Improve test suite

--- static/app/views/starfish/utils/sqlish/SQLishFormatter.spec.tsx
+++ static/app/views/starfish/utils/sqlish/SQLishFormatter.spec.tsx
@@ -4,18 +4,20 @@ describe('SQLishFormatter', function () {
   const formatter = new SQLishFormatter();
 
   it('Formats basic SQL', () => {
-    expect(formatter.toString('SELECT hello FROM users;')).toEqual(
-      'SELECT hello \nFROM users;'
-    );
+    expect(formatter.toString('SELECT hello;')).toEqual('SELECT hello;');
   });
 
   it('Formats wildcards', () => {
-    expect(formatter.toString('SELECT * FROM users;')).toEqual('SELECT * \nFROM users;');
+    expect(formatter.toString('SELECT *;')).toEqual('SELECT *;');
   });
 
   it('Formats lists of bare column names', () => {
-    expect(formatter.toString('SELECT id, name FROM sentry_organization;')).toEqual(
-      'SELECT id, name \nFROM sentry_organization;'
+    expect(formatter.toString('SELECT id, name;')).toEqual('SELECT id, name;');
+  });
+
+  it('Adds newlines for keywords', () => {
+    expect(formatter.toString('SELECT hello FROM users LIMIT 1;')).toEqual(
+      'SELECT hello \nFROM users \nLIMIT 1;'
     );
   });
 });
