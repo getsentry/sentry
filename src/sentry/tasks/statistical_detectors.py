dbcffae813abd351726b237401f1a5ad5e750071
Tony Xiao <txiao@sentry.io>
2023-07-27 15:13:50 -0400

    run snuba query

--- src/sentry/tasks/statistical_detectors.py
+++ src/sentry/tasks/statistical_detectors.py
@@ -1,10 +1,14 @@
 import logging
-from typing import List
+from datetime import datetime, timedelta
+from typing import Any, Dict, List
+
+from django.utils import timezone
 
 from sentry.constants import ObjectStatus
 from sentry.models import Project
+from sentry.snuba import functions
+from sentry.snuba.referrer import Referrer
 from sentry.tasks.base import instrumented_task
-from sentry.utils.iterators import chunked
 from sentry.utils.query import RangeQuerySetWrapper
 
 logger = logging.getLogger("sentry.tasks.statistical_detectors")
@@ -19,17 +23,38 @@ ITERATOR_CHUNK = 10_000
     max_retries=0,
 )
 def run_detection() -> None:
+    now = timezone.now()
+
+    performance_projects = []
+    profiling_projects = []
+
     # TODO: iterate over a predefined list of projects for testing
-    for project_ids in chunked(
-        RangeQuerySetWrapper(
-            Project.objects.filter(status=ObjectStatus.ACTIVE).values_list("id", flat=True),
-            result_value_getter=lambda item: item,
-            step=ITERATOR_CHUNK,
-        ),
-        ITERATOR_CHUNK,
+    for project in RangeQuerySetWrapper(
+        Project.objects.filter(status=ObjectStatus.ACTIVE),
+        result_value_getter=lambda item: item.id,
+        step=ITERATOR_CHUNK,
     ):
-        _detect_regressed_transactions(project_ids)
-        _detect_regressed_functions(project_ids)
+        if project.flags.has_transactions:
+            performance_projects.append(project.id)
+
+            if len(performance_projects) >= ITERATOR_CHUNK:
+                detect_regressed_transactions.delay(performance_projects)
+                performance_projects = []
+
+        if project.flags.has_profiles:
+            profiling_projects.append(project.id)
+
+            if len(profiling_projects) >= ITERATOR_CHUNK:
+                detect_regressed_functions.delay(profiling_projects, now)
+                profiling_projects = []
+
+    # make sure to dispatch a task to handle the remaining projects
+    if performance_projects:
+        detect_regressed_transactions.delay(performance_projects)
+        performance_projects = []
+    if profiling_projects:
+        detect_regressed_functions.delay(profiling_projects, now)
+        profiling_projects = []
 
 
 @instrumented_task(
@@ -37,8 +62,9 @@ def run_detection() -> None:
     queue="performance.statistical_detector",
     max_retries=0,
 )
-def _detect_regressed_transactions(project_ids: List[int]) -> None:
-    logger.info("running transaction regression detection", extra={"projects": project_ids})
+def detect_regressed_transactions(project_ids: List[int], **kwargs) -> None:
+    for project_id in project_ids:
+        _detect_regressed_transactions(project_id)
 
 
 @instrumented_task(
@@ -46,5 +72,45 @@ def _detect_regressed_transactions(project_ids: List[int]) -> None:
     queue="performance.statistical_detector",
     max_retries=0,
 )
-def _detect_regressed_functions(project_ids: List[int]) -> None:
-    logger.info("running function regression detection", extra={"projects": project_ids})
+def detect_regressed_functions(project_ids: List[int], start: datetime, **kwargs) -> None:
+
+    for project in Project.objects.filter(id__in=project_ids):
+        _detect_regressed_functions(project, start)
+
+
+def _detect_regressed_transactions(project_id: int) -> None:
+    pass
+
+
+def _detect_regressed_functions(project: Project, start: datetime) -> None:
+    params = _get_regressed_function_query_params(project, start)
+
+    functions.query(
+        selected_columns=[
+            "fingerprint",
+            "count()",
+            "p95()",
+        ],
+        query="is_application:1",
+        params=params,
+        orderby=["-count()"],
+        limit=100,
+        referrer=Referrer.API_PROFILING_FUNCTION_DETECTOR.value,
+        auto_aggregations=True,
+        use_aggregate_conditions=True,
+        transform_alias_to_input_format=True,
+    )
+
+
+def _get_regressed_function_query_params(project: Project, start: datetime) -> Dict[str, Any]:
+    # the functions dataset
+    start = start - timedelta(minutes=30)
+    start = start.replace(minute=0, second=0, microsecond=0)
+
+    return {
+        "start": start,
+        "end": start + timedelta(minutes=1),
+        "project_id": [project.id],
+        "project_objects": [project],
+        "organization_id": project.organization_id,
+    }
