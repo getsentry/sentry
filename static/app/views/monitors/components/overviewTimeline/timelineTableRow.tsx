ce8ef59a981c82e7d22f61beb9978e206b619211
Vu Luong <vuluongj20@gmail.com>
2023-07-28 16:06:53 -0700

    Move TimelinePlaceholder out into separate file

--- static/app/views/monitors/components/overviewTimeline/timelineTableRow.tsx
+++ static/app/views/monitors/components/overviewTimeline/timelineTableRow.tsx
@@ -1,6 +1,5 @@
 import {useState} from 'react';
 import {Link} from 'react-router';
-import {keyframes} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import {Button} from 'sentry/components/button';
@@ -13,6 +12,7 @@ import {scheduleAsText} from 'sentry/views/monitors/utils';
 import {statusIconMap} from 'sentry/views/monitors/utils/constants';
 
 import {CheckInTimeline, CheckInTimelineProps} from './checkInTimeline';
+import {TimelinePlaceholder} from './timelinePlaceholder';
 import {MonitorBucket} from './types';
 
 interface Props extends Omit<CheckInTimelineProps, 'bucketedData' | 'environment'> {
@@ -86,22 +86,6 @@ function MonitorDetails({monitor}: {monitor: Monitor}) {
       <Name>{monitor.name}</Name>
       <Schedule>{schedule}</Schedule>
     </DetailsContainer>
-  );
-}
-
-function TimelinePlaceholder({count}: {count: number}) {
-  return (
-    <TimelinePlaceholderContainer>
-      {[...new Array(count)].map((_, i) => (
-        <PlaceholderTick
-          key={i}
-          style={{
-            left: `${(i * (100 / count)).toFixed(2)}%`,
-            animationDelay: `${(i / count).toFixed(2)}s`,
-          }}
-        />
-      ))}
-    </TimelinePlaceholderContainer>
   );
 }
 
@@ -179,48 +163,4 @@ const TimelineEnvContainer = styled('div')`
   opacity: 0;
   animation: ${fadeIn} 1.5s ease-out forwards;
   contain: content;
-`;
-
-const TimelinePlaceholderContainer = styled('div')`
-  display: flex;
-  align-items: center;
-  position: relative;
-  height: 100%;
-`;
-
-const placeholderTickKeyframes = keyframes`
-  0% {
-    opacity: 0;
-    transform: scale(0.75) translateZ(0);
-    filter: blur(12px);
-  }
-  33.33% {
-    opacity: 1;
-    transform: scale(1) translateZ(0);
-    filter: blur(0px);
-  }
-  66.66% {
-    opacity: 1;
-    transform: scale(1) translateZ(0);
-    filter: blur(0px);
-  }
-  100% {
-    opacity: 0;
-    transform: scale(0.75) translateZ(0);
-    filter: blur(12px);
-  }
-`;
-
-const PlaceholderTick = styled('div')`
-  position: absolute;
-  margin-top: 1px;
-  background: ${p => p.theme.translucentBorder};
-  width: 4px;
-  height: 14px;
-  border-radius: 2px;
-
-  opacity: 0;
-  transform: scale(0.75) translateZ(0);
-  filter: blur(12px);
-  animation: ${placeholderTickKeyframes} 2s ease-out forwards infinite;
 `;
