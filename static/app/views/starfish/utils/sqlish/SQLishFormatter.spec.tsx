fd2ff178a647bd8b3f30192714ff5d5340d1df05
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-07-28 13:24:09 -0400

    feat(starfish): Gently highlight SQL in span table (#53782)
    We are convinced that links in tables should look like links, so we do
    _not_ want to fully syntax highlight these. We _do_ want to _lightly_
    highlight them, though! This is a nice little compromise.
    
    ## Changes
    
    - Break up the files a bit
    - Introduce simple markup formatter
    - Bold keywords
    - Fix rendering of collapsed columns
    - Capitalize keywords
    - Fix whitespace between tokens and join commands
    - Render descriptions with simple markup

--- static/app/views/starfish/utils/sqlish/SQLishFormatter.spec.tsx
+++ static/app/views/starfish/utils/sqlish/SQLishFormatter.spec.tsx
@@ -1,3 +1,7 @@
+import React from 'react';
+
+import {render} from 'sentry-test/reactTestingLibrary';
+
 import {SQLishFormatter} from 'sentry/views/starfish/utils/sqlish/SQLishFormatter';
 
 describe('SQLishFormatter', function () {
@@ -12,6 +16,27 @@ describe('SQLishFormatter', function () {
       expect(
         formatter.toString('SELECT hello FROM users ORDER BY name DESC LIMIT 1;')
       ).toEqual('SELECT hello \nFROM users \nORDER BY name DESC \nLIMIT 1;');
+    });
+  });
+
+  describe('SQLishFormatter.toSimpleMarkup()', () => {
+    const formatter = new SQLishFormatter();
+    const getMarkup = (markup: any): string => {
+      const {container} = render(<React.Fragment>{markup}</React.Fragment>);
+
+      return container.innerHTML;
+    };
+
+    it('Capitalizes keywords', () => {
+      expect(getMarkup(formatter.toSimpleMarkup('select hello'))).toEqual(
+        '<b>SELECT</b><span> </span><span>hello</span>'
+      );
+    });
+
+    it('Wraps every token in a `<span>` element', () => {
+      expect(getMarkup(formatter.toSimpleMarkup('SELECT hello;'))).toEqual(
+        '<b>SELECT</b><span> </span><span>hello;</span>'
+      );
     });
   });
 });
