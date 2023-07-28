a594beb4fbfa668ccf83db845409a249633d5ae8
JonasBa <jonas@badalic.com>
2023-07-28 09:38:10 -0400

    feat(profiling): draw first lines for cpu chart

--- static/app/utils/profiling/flamegraphChart.tsx
+++ static/app/utils/profiling/flamegraphChart.tsx
@@ -51,7 +51,6 @@ export class FlamegraphChart {
 
     this.domains.y[1] = 100;
     this.configSpace = configSpace.withHeight(this.domains.y[1] - this.domains.y[0]);
-
     this.formatter = makeFormatter(measurement.unit);
   }
 }
