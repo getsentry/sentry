31fdc70103cf9011cd39f15b26fcbbf5db761c1e
Spencer Murray <spencer.murray@sentry.io>
2023-07-31 14:36:19 -0700

    Test DiscordSelectMenu

--- tests/sentry/integrations/discord/test_message_builder.py
+++ tests/sentry/integrations/discord/test_message_builder.py
@@ -13,10 +13,15 @@ from sentry.integrations.discord.message_builder.base.component.button import (
     DiscordButton,
     DiscordButtonStyle,
 )
+from sentry.integrations.discord.message_builder.base.component.select_menu import (
+    DiscordSelectMenu,
+    DiscordSelectMenuOption,
+)
 from sentry.integrations.discord.message_builder.base.embed.base import DiscordMessageEmbed
 from sentry.integrations.discord.message_builder.base.embed.field import DiscordMessageEmbedField
 from sentry.integrations.discord.message_builder.base.embed.footer import DiscordMessageEmbedFooter
 from sentry.integrations.discord.message_builder.base.flags import DiscordMessageFlags
+from sentry.integrations.discord.requests.base import DiscordMessageComponentTypes
 from sentry.testutils.cases import TestCase
 
 
@@ -322,4 +327,59 @@ class TestDiscordMessageEmbed(TestCase):
                     "inline": True,
                 }
             ],
+        }
+
+
+class TestDiscordSelectMenu(TestCase):
+    def test_empty(self):
+        menu = DiscordSelectMenu("custom-id", [])
+        result = menu.build()
+        assert result == {
+            "type": DiscordMessageComponentTypes.SELECT,
+            "custom_id": "custom-id",
+            "options": [],
+            "min_values": 1,
+            "max_values": 1,
+            "disabled": False,
+        }
+
+    def test_disabled(self):
+        menu = DiscordSelectMenu("custom-id", [], disabled=True)
+        result = menu.build()
+        assert result == {
+            "type": DiscordMessageComponentTypes.SELECT,
+            "custom_id": "custom-id",
+            "options": [],
+            "min_values": 1,
+            "max_values": 1,
+            "disabled": True,
+        }
+
+    def test_non_empty(self):
+        option = DiscordSelectMenuOption("option", "first", "descriptionnn", True)
+        other_option = DiscordSelectMenuOption("other", "second")
+        menu = DiscordSelectMenu(
+            "custom-id", [option, other_option], placeholder="place being held"
+        )
+        result = menu.build()
+        assert result == {
+            "type": DiscordMessageComponentTypes.SELECT,
+            "custom_id": "custom-id",
+            "options": [
+                {
+                    "label": "option",
+                    "value": "first",
+                    "description": "descriptionnn",
+                    "default": True,
+                },
+                {
+                    "label": "other",
+                    "value": "second",
+                    "default": False,
+                },
+            ],
+            "placeholder": "place being held",
+            "min_values": 1,
+            "max_values": 1,
+            "disabled": False,
         }
