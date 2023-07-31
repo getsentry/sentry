aefcd4d854993924aeab29a96f70a20c6a0e90af
Jan Michael Auer <mail@jauer.org>
2023-07-31 14:40:31 +0200

    Remove redundant example

--- tests/sentry/sentry_metrics/query_experimental/test_query.py
+++ tests/sentry/sentry_metrics/query_experimental/test_query.py
@@ -21,48 +21,6 @@ from sentry.testutils import BaseMetricsLayerTestCase, TestCase
 pytestmark = pytest.mark.sentry_metrics
 
 
-def __example_to_remove():
-    """
-    sum(transactions{status="error"}) / sum(transactions)
-    """
-
-    expr = Function(
-        ArithmeticFn.DIVIDE.value,
-        [
-            Function(
-                AggregationFn.SUM.value,
-                [
-                    Function(
-                        FILTER,
-                        [
-                            Column("transactions"),
-                            Condition(
-                                lhs=Column("status"),
-                                op=Op.EQ,
-                                rhs="error",
-                            ),
-                        ],
-                    )
-                ],
-            ),
-            Function(AggregationFn.SUM.value, [Column("transactions")]),
-        ],
-    )
-
-    query = SeriesQuery(
-        scope=MetricQueryScope(org_id=1, project_ids=[1]),
-        expressions=[expr],
-        filters=[],
-        groups=[],
-        start=datetime(2023, 1, 1),
-        end=datetime(2023, 1, 2),
-        interval=3600,
-    )
-
-    result = get_series(query)
-    assert result
-
-
 # Set yesterday at noon as the current time. This is within the allowed
 # timeframe for backdating metrics, which is required to store metrics.
 MOCK_DATETIME = (datetime.now() - timedelta(days=1)).replace(
