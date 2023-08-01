09ac23afd616b29c9e713dc6ed198b3b119cfcfd
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-08-01 14:01:23 -0400

    Show formatted query on hover

--- static/app/views/starfish/components/tableCells/spanDescriptionCell.tsx
+++ static/app/views/starfish/components/tableCells/spanDescriptionCell.tsx
@@ -1,6 +1,14 @@
+import {Fragment} from 'react';
+import {createPortal} from 'react-dom';
 import {Link} from 'react-router';
+import {useTheme} from '@emotion/react';
+import styled from '@emotion/styled';
+import {AnimatePresence} from 'framer-motion';
 import * as qs from 'query-string';
 
+import {CodeSnippet} from 'sentry/components/codeSnippet';
+import {Overlay, PositionWrapper} from 'sentry/components/overlay';
+import {useHoverOverlay} from 'sentry/utils/useHoverOverlay';
 import {useLocation} from 'sentry/utils/useLocation';
 import {OverflowEllipsisTextContainer} from 'sentry/views/starfish/components/textAlign';
 import {ModuleName, StarfishFunctions} from 'sentry/views/starfish/types';
@@ -27,12 +35,13 @@ export function SpanDescriptionCell({
 }: Props) {
   const location = useLocation();
 
+  const theme = useTheme();
+
+  const {wrapTrigger, isOpen, overlayProps, placement, arrowData, arrowProps} =
+    useHoverOverlay('overlay', OVERLAY_OPTIONS);
+
   if (!description) {
     return NULL_DESCRIPTION;
-  }
-
-  if (!group) {
-    return <OverflowEllipsisTextContainer>{description}</OverflowEllipsisTextContainer>;
   }
 
   const queryString = {
@@ -51,19 +60,48 @@ export function SpanDescriptionCell({
     );
   }
 
-  return (
-    <OverflowEllipsisTextContainer>
-      <Link
-        to={`/starfish/${extractRoute(location) ?? 'spans'}/span/${group}${
-          queryString ? `?${qs.stringify(queryString)}` : ''
-        }`}
+  const formattedDescription =
+    moduleName === ModuleName.DB ? formatter.toSimpleMarkup(description) : description;
+
+  const overlayContent = moduleName === ModuleName.DB && isOpen && (
+    <PositionWrapper zIndex={theme.zIndex.tooltip} {...overlayProps}>
+      <OverlayContent
+        animated
+        originPoint={arrowData}
+        arrowProps={arrowProps}
+        placement={placement}
       >
-        {moduleName === ModuleName.DB
-          ? formatter.toSimpleMarkup(description)
-          : description}
-      </Link>
-    </OverflowEllipsisTextContainer>
+        <CodeSnippet language="sql">{formatter.toString(description)}</CodeSnippet>
+      </OverlayContent>
+    </PositionWrapper>
+  );
+
+  return (
+    <Fragment>
+      <OverflowEllipsisTextContainer>
+        {wrapTrigger(
+          group ? (
+            <Link
+              to={`/starfish/${extractRoute(location) ?? 'spans'}/span/${group}${
+                queryString ? `?${qs.stringify(queryString)}` : ''
+              }`}
+            >
+              {formattedDescription}
+            </Link>
+          ) : (
+            formattedDescription
+          )
+        )}
+      </OverflowEllipsisTextContainer>
+      {createPortal(<AnimatePresence>{overlayContent}</AnimatePresence>, document.body)}
+    </Fragment>
   );
 }
 
 const NULL_DESCRIPTION = <span>&lt;null&gt;</span>;
+
+const OVERLAY_OPTIONS = {position: 'right', isHoverable: true} as const;
+
+const OverlayContent = styled(Overlay)`
+  max-width: 500px;
+`;
