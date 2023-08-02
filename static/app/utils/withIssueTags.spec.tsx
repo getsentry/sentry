834a54d5a4c4690f83c9827c8e20e4e85ea3d24a
Scott Cooper <scttcper@gmail.com>
2023-08-02 12:11:59 -0700

    consolidate tag tests

--- static/app/utils/withIssueTags.spec.tsx
+++ static/app/utils/withIssueTags.spec.tsx
@@ -1,6 +1,6 @@
 import {act, render, screen, waitFor} from 'sentry-test/reactTestingLibrary';
 
-import {SearchGroup} from 'sentry/components/smartSearchBar/types';
+import type {SearchGroup} from 'sentry/components/smartSearchBar/types';
 import MemberListStore from 'sentry/stores/memberListStore';
 import TagStore from 'sentry/stores/tagStore';
 import TeamStore from 'sentry/stores/teamStore';
@@ -16,7 +16,11 @@ function MyComponent(props: MyComponentProps) {
       {'is: ' + props.tags?.is?.values?.[0]}
       {'mechanism: ' + props.tags?.mechanism?.values?.join(', ')}
       {'bookmarks: ' + props.tags?.bookmarks?.values?.join(', ')}
-      {'assigned: ' + props.tags?.assigned?.values?.join(', ')}
+      {'assigned: ' +
+        (props.tags?.assigned?.values as SearchGroup[])
+          .flatMap(x => x.children)
+          .map(x => x.desc)
+          ?.join(', ')}
       {'stack filename: ' + props.tags?.['stack.filename'].name}
     </div>
   );
@@ -90,15 +94,7 @@ describe('withIssueTags HoC', function () {
   });
 
   it('groups assignees and puts suggestions first', function () {
-    const Container = withIssueTags(({tags}: MyComponentProps) => (
-      <div>
-        {(tags?.assigned?.values as SearchGroup[])?.map(searchGroup => (
-          <div data-test-id={searchGroup.title} key={searchGroup.title}>
-            {searchGroup.children?.map(item => item.desc).join(', ')}
-          </div>
-        ))}
-      </div>
-    ));
+    const Container = withIssueTags(MyComponent);
     TeamStore.loadInitialData([
       TestStubs.Team({id: 1, slug: 'best-team', name: 'Best Team', isMember: true}),
       TestStubs.Team({id: 2, slug: 'worst-team', name: 'Worst Team', isMember: false}),
