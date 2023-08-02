4e81b4ac29dc9eda053a1fb165e8b6a3f24b5453
Alex Zaslavsky <azaslavsky@users.noreply.github.com>
2023-08-02 16:19:56 -0700

    ref(backup): Move helpers out of testutils (#54008)
    Some helpers that were previously testutils actually fit better in the
    mainline source, as we will use them to "auto assign" comparators where
    possible.
    
    Issue: getsentry/team-ospo#155

---
+++ src/sentry/backup/helpers.py
@@ -0,0 +1,30 @@
+from __future__ import annotations
+
+from typing import Type
+
+
+def get_final_derivations_of(model: Type) -> set[Type]:
+    """A "final" derivation of the given `model` base class is any non-abstract class for the
+    "sentry" app with `BaseModel` as an ancestor. Top-level calls to this class should pass in
+    `BaseModel` as the argument."""
+
+    out = set()
+    for sub in model.__subclasses__():
+        subs = sub.__subclasses__()
+        if subs:
+            out.update(get_final_derivations_of(sub))
+        if not sub._meta.abstract and sub._meta.db_table and sub._meta.app_label == "sentry":
+            out.add(sub)
+    return out
+
+
+def get_exportable_final_derivations_of(model: Type) -> set[Type]:
+    """Like `get_final_derivations_of`, except that it further filters the results to include only
+    `__include_in_export__ = True`."""
+
+    return set(
+        filter(
+            lambda c: getattr(c, "__include_in_export__") is True,
+            get_final_derivations_of(model),
+        )
+    )
--- tests/sentry/backup/test_releases.py
+++ tests/sentry/backup/test_releases.py
@@ -8,6 +8,7 @@ from django.core.management import call_command
 from django.db import router
 from sentry_relay.auth import generate_key_pair
 
+from sentry.backup.helpers import get_exportable_final_derivations_of
 from sentry.db.models import BaseModel
 from sentry.incidents.models import (
     IncidentActivity,
@@ -63,10 +64,7 @@ from sentry.monitors.models import (
 from sentry.sentry_apps.apps import SentryAppUpdater
 from sentry.silo import unguarded_write
 from sentry.testutils.cases import TransactionTestCase
-from sentry.testutils.helpers.backups import (
-    get_exportable_final_derivations_of,
-    import_export_then_validate,
-)
+from sentry.testutils.helpers.backups import import_export_then_validate
 from sentry.utils.json import JSONData
 from tests.sentry.backup import targets
 
