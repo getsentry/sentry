57245cfb79144a34efe08224579a1bd2729e4956
Joshua Li <joshuarli98@gmail.com>
2023-08-03 09:48:07 -0700

    remove pull and fast so that we always start with a fresh container, fix healthcheck tool

--- tools/devservices_healthcheck.py
+++ tools/devservices_healthcheck.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import os
 import subprocess
 from time import sleep
 
@@ -12,7 +11,7 @@ def run_cmd(
     timeout: int = 5,
 ) -> None:
     for retry in range(1, retries + 1):
-        returncode = subprocess.call(args)
+        returncode = subprocess.call(args, stdout=subprocess.DEVNULL)
 
         if returncode != 0:
             sleep(timeout)
@@ -23,23 +22,18 @@ def run_cmd(
 
 
 def main() -> None:
-    # Available health checks
-    postgres_healthcheck = ["docker", "exec", "sentry_postgres", "pg_isready", "-U", "postgres"]
-    kafka_healthcheck = [
-        "docker",
-        "exec",
-        "sentry_kafka",
-        "kafka-topics",
-        "--zookeeper",
-        "127.0.0.1:2181",
-        "--list",
-    ]
-
-    healthchecks = [postgres_healthcheck]
-    if os.getenv("NEED_KAFKA") == "true":
-        healthchecks.append(kafka_healthcheck)
-
-    for check in healthchecks:
+    for check in (
+        ("docker", "exec", "sentry_postgres", "pg_isready", "-U", "postgres"),
+        (
+            "docker",
+            "exec",
+            "sentry_kafka",
+            "kafka-topics",
+            "--zookeeper",
+            "sentry_zookeeper:2181",
+            "--list",
+        ),
+    ):
         run_cmd(check)
 
 
