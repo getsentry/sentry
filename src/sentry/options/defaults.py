0f2af8f95900017e4a8468af63c37e5586ae46ac
Tony Xiao <txiao@sentry.io>
2023-08-03 17:05:01 -0400

    chore(statistical-detectors): Update option flags
    The admin UI is being sunsetted in favour of the automator. So remove the admin
    UI flag. Also prioritize disk so we can overwrite it in `SENTRY_OPTIONS`.

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -1427,17 +1427,17 @@ register("issues.group_attributes.send_kafka", default=False, flags=FLAG_MODIFIA
 register(
     "statistical_detectors.enable",
     default=False,
-    flags=FLAG_MODIFIABLE_BOOL | FLAG_AUTOMATOR_MODIFIABLE,
+    flags=FLAG_PRIORITIZE_DISK | FLAG_AUTOMATOR_MODIFIABLE,
 )
 register(
     "statistical_detectors.enable.projects.performance",
     type=Sequence,
     default=[],
-    flags=FLAG_AUTOMATOR_MODIFIABLE,
+    flags=FLAG_PRIORITIZE_DISK | FLAG_AUTOMATOR_MODIFIABLE,
 )
 register(
     "statistical_detectors.enable.projects.profiling",
     type=Sequence,
     default=[],
-    flags=FLAG_AUTOMATOR_MODIFIABLE,
+    flags=FLAG_PRIORITIZE_DISK | FLAG_AUTOMATOR_MODIFIABLE,
 )
