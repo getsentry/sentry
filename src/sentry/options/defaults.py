35be41e471a36cd8ac1df4356e1bc3b64095a8be
Gilbert Szeto <gilbert.szeto@sentry.io>
2023-08-03 13:11:06 -0700

    forgot a flag

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -1421,7 +1421,11 @@ register(
 )
 
 # The flag activates whether to send group attributes messages to kafka
-register("issues.group_attributes.send_kafka", default=False, flags=FLAG_MODIFIABLE_BOOL)
+register(
+    "issues.group_attributes.send_kafka",
+    default=False,
+    flags=FLAG_MODIFIABLE_BOOL | FLAG_AUTOMATOR_MODIFIABLE,
+)
 
 # Enables statistical detectors for a project
 register(
