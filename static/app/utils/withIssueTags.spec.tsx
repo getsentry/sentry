9ccf204c80f90cc3e5eb6c2178c078722193a411
Scott Cooper <scttcper@gmail.com>
2023-08-03 13:25:58 -0700

    feat(issues): GA search shortcuts (#53903)

--- static/app/utils/withIssueTags.spec.tsx
+++ static/app/utils/withIssueTags.spec.tsx
@@ -1,6 +1,6 @@
 import {act, render, screen, waitFor} from 'sentry-test/reactTestingLibrary';
 
-import {SearchGroup} from 'sentry/components/smartSearchBar/types';
+import type {SearchGroup} from 'sentry/components/smartSearchBar/types';
 import MemberListStore from 'sentry/stores/memberListStore';
 import TagStore from 'sentry/stores/tagStore';
 import TeamStore from 'sentry/stores/teamStore';
@@ -16,7 +16,11 @@ function MyComponent(props: MyComponentProps) {
       {'is: ' + props.tags?.is?.values?.[0]}
       {'mechanism: ' + props.tags?.mechanism?.values?.join(', ')}
       {'bookmarks: ' + props.tags?.bookmarks?.values?.join(', ')}
-      {'assigned: ' + props.tags?.assigned?.values?.join(', ')}
+      {'assigned: ' +
+        (props.tags?.assigned?.values as SearchGroup[])
+          .flatMap(x => x.children)
+          .map(x => x.desc)
+          ?.join(', ')}
       {'stack filename: ' + props.tags?.['stack.filename'].name}
     </div>
   );
@@ -90,16 +94,7 @@ describe('withIssueTags HoC', function () {
   });
 
   it('groups assignees and puts suggestions first', function () {
-    const Container = withIssueTags(({tags}: MyComponentProps) => (
-      <div>
-        {(tags?.assigned?.values as SearchGroup[])?.map(searchGroup => (
-          <div data-test-id={searchGroup.title} key={searchGroup.title}>
-            {searchGroup.children?.map(item => item.desc).join(', ')}
-          </div>
-        ))}
-      </div>
-    ));
-    const organization = TestStubs.Organization({features: ['issue-search-shortcuts']});
+    const Container = withIssueTags(MyComponent);
     TeamStore.loadInitialData([
       TestStubs.Team({id: 1, slug: 'best-team', name: 'Best Team', isMember: true}),
       TestStubs.Team({id: 2, slug: 'worst-team', name: 'Worst Team', isMember: false}),
@@ -108,16 +103,14 @@ describe('withIssueTags HoC', function () {
       TestStubs.User(),
       TestStubs.User({username: 'joe@example.com'}),
     ]);
-    render(<Container organization={organization} forwardedValue="value" />, {
-      organization,
-    });
-
-    expect(screen.getByTestId('Suggested Values')).toHaveTextContent(
-      'me, my_teams, [me, my_teams, none], #best-team'
+    const {container} = render(
+      <Container organization={TestStubs.Organization()} forwardedValue="value" />
     );
 
-    expect(screen.getByTestId('All Values')).toHaveTextContent(
-      'foo@example.com, joe@example.com, #worst-team'
+    expect(container).toHaveTextContent(
+      'assigned: me, my_teams, [me, my_teams, none], #best-team'
     );
+    // Has the other teams/members
+    expect(container).toHaveTextContent('foo@example.com, joe@example.com, #worst-team');
   });
 });
