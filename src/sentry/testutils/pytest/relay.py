1dfb2eee950fa936112223d249782a8874f1d49a
Joshua Li <joshuarli98@gmail.com>
2023-08-04 16:56:46 -0700

    Revert "debug"
    This reverts commit e260ddd315ca20c46ea1c0d5e6624ddcfdd53685.

--- src/sentry/testutils/pytest/relay.py
+++ src/sentry/testutils/pytest/relay.py
@@ -120,9 +120,9 @@ def relay_server_setup(live_server, tmpdir_factory):
 
     # cleanup
     shutil.rmtree(config_path)
-
-    import subprocess
-    subprocess.call(("docker", "logs", "sentry_test_relay_server"))
+    if not environ.get("RELAY_TEST_KEEP_CONTAINER", False):
+        with get_docker_client() as docker_client:
+            _remove_container_if_exists(docker_client, container_name)
 
 
 @pytest.fixture(scope="function")
