f84411a9a1ff56cac46e21f16e905c6ddd47f676
kneeyo1 <neo.huang@sentry.io>
2023-08-04 09:38:32 -0500

    cleaner test case to test that everything is implemented in presenterdelegator

--- tests/sentry/runner/commands/presenters/test_presenterdelegator.py
+++ tests/sentry/runner/commands/presenters/test_presenterdelegator.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 import pytest
 
 from sentry.runner.commands.presenters.presenterdelegator import PresenterDelegator
@@ -10,40 +12,22 @@ class TestPresenterDelegator:
     """
 
     @pytest.fixture(autouse=True)
+    @patch("sentry.runner.commands.presenters.slackpresenter.SLACK_WEBHOOK_URL", "https://test/")
     def setup(self) -> None:
         self.presenterDelagator = PresenterDelegator()
 
-    def test_valid_attribute(self):
-        self.presenterDelagator.set("key", "val")
-        self.presenterDelagator.unset("key")
-        self.presenterDelagator.update("key", "db_val", "val")
-        self.presenterDelagator.channel_update("key")
-        self.presenterDelagator.drift("key", "val")
-        self.presenterDelagator.error("key", "error msg")
-        self.presenterDelagator.unregistered("key")
-        self.presenterDelagator.invalid_type("key", "got_type", "expected_type")
-        self.presenterDelagator.flush()
+    def test_contains_attributes(self):
+        expected_methods = [
+            "set",
+            "unset",
+            "update",
+            "channel_update",
+            "drift",
+            "error",
+            "unregistered",
+            "invalid_type",
+            "flush",
+        ]
 
-    def test_invalid_attribute(self):
-        with pytest.raises(AttributeError):
-            self.presenterDelagator.random()
-        with pytest.raises(TypeError):
-            self.presenterDelagator.update("key")
-        with pytest.raises(TypeError):
-            self.presenterDelagator.update("key", "value")
-        with pytest.raises(TypeError):
-            self.presenterDelagator.set("key")
-        with pytest.raises(TypeError):
-            self.presenterDelagator.unset("key", "value")
-        with pytest.raises(TypeError):
-            self.presenterDelagator.update("key")
-        with pytest.raises(TypeError):
-            self.presenterDelagator.update("key", "val")
-        with pytest.raises(TypeError):
-            self.presenterDelagator.channel_update()
-        with pytest.raises(TypeError):
-            self.presenterDelagator.drift("key")
-        with pytest.raises(TypeError):
-            self.presenterDelagator.error("key")
-        with pytest.raises(TypeError):
-            self.presenterDelagator.unregistered("key", "val")
+        for method_name in expected_methods:
+            assert hasattr(self.presenterDelagator, method_name)
