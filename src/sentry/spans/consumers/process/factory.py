776816d90d49e33ced24e97e2a943e3ef7a65597
Pierre Massat <pierre.massat@sentry.io>
2023-08-03 21:03:07 -0400

    feat(spans): Consume spans and save contexts and tags to nodestore

---
+++ src/sentry/spans/consumers/process/factory.py
@@ -0,0 +1,43 @@
+from typing import Mapping
+
+import msgpack
+from arroyo.backends.kafka.consumer import KafkaPayload
+from arroyo.processing.strategies.abstract import ProcessingStrategy, ProcessingStrategyFactory
+from arroyo.processing.strategies.commit import CommitOffsets
+from arroyo.processing.strategies.produce import Produce
+from arroyo.processing.strategies.run_task import RunTask
+from arroyo.typps import Commit, Message, Partition, Topic
+from confluent_kafka import Producer
+
+from sentry.spans.span import Span
+from sentry.utils import json, kafka_config
+
+
+def process_message(message: Message[KafkaPayload]) -> None:
+    payload = msgpack.unpackb(message.payload.value)
+    span_dict = json.loads(payload["span"])
+    Span.from_dict(span_dict).save()
+
+
+class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
+    def __init__(self, output_topic: str):
+        super().__init__()
+        snuba_spans = kafka_config.get_topic_definition(output_topic)
+        self.__topic = Topic(name=output_topic)
+        self.__producer = Producer(
+            kafka_config.get_kafka_producer_cluster_options(snuba_spans["cluster"]),
+        )
+
+    def create_with_partitions(
+        self,
+        commit: Commit,
+        partitions: Mapping[Partition, int],
+    ) -> ProcessingStrategy[KafkaPayload]:
+        return RunTask(
+            function=process_message,
+            next_step=Produce(
+                producer=self.__producer,
+                topic=self.__topic,
+                next_step=CommitOffsets(commit),
+            ),
+        )
---
+++ src/sentry/spans/span.py
@@ -0,0 +1,60 @@
+from __future__ import annotations
+
+import hashlib
+from dataclasses import dataclass
+from typing import Mapping, Optional, Sequence, TypedDict
+
+from sentry import nodestore
+
+
+class SpanData(TypedDict):
+    id: str
+    contexts: Mapping[str, object]
+    project_id: int
+    tags: Mapping[str, str]
+
+
+@dataclass(frozen=True)
+class Span:
+    contexts: Mapping[str, object]
+    id: str
+    project_id: int
+    tags: Mapping[str, str]
+
+    def to_dict(self) -> SpanData:
+        return {
+            "contexts": self.contexts,
+            "id": self.id,
+            "project_id": self.project_id,
+            "tags": self.tags,
+        }
+
+    @classmethod
+    def from_dict(cls, data: SpanData) -> Span:
+        return cls(
+            data["contexts"],
+            data["id"],
+            data["project_id"],
+            data["tags"],
+        )
+
+    @classmethod
+    def build_storage_identifier(cls, span_id: str, project_id: int) -> str:
+        identifier = hashlib.md5(f"{span_id}::{project_id}".encode()).hexdigest()
+        return f"s:{identifier}"
+
+    def save(self) -> None:
+        nodestore.set(self.build_storage_identifier(self.id, self.project_id), self.to_dict())
+
+    @classmethod
+    def fetch(cls, span_id: str, project_id: int) -> Optional[Span]:
+        results = nodestore.get(cls.build_storage_identifier(span_id, project_id))
+        if results:
+            return Span.from_dict(results)
+        return None
+
+    @classmethod
+    def fetch_multi(cls, ids: Sequence[str], project_id: int) -> Sequence[Optional[Span]]:
+        ids = [cls.build_storage_identifier(id, project_id) for id in ids]
+        results = nodestore.get_multi(ids)
+        return [Span.from_dict(results[id]) if results.get(id) else None for id in ids]
