fab1da44d2867b5747ad2a0f2bd84b97870fb54f
Nar Saynorath <nar.saynorath@sentry.io>
2023-08-04 16:53:49 -0400

    fix(subscriptions): Clear snuba query on deletion
    Building the query builder during deletion would trigger validation, and
    if there was an issue in validation then the subscription could not be
    deleted.
    
    This PR clears the query to avoid the query triggering validation errors
    for entities (e.g. issues) that no longer exist.
    
    Fixes SENTRY-XZZ

--- src/sentry/snuba/tasks.py
+++ src/sentry/snuba/tasks.py
@@ -173,6 +173,9 @@ def delete_subscription_from_snuba(query_subscription_id, **kwargs):
 
     if subscription.subscription_id is not None:
         query_dataset = Dataset(subscription.snuba_query.dataset)
+
+        # Clear the query because we don't need it to delete the subscription
+        subscription.snuba_query.query = ""
         entity_key = get_entity_key_from_snuba_query(
             subscription.snuba_query, subscription.project.organization_id, subscription.project_id
         )
