115871d94b338ab65e452007fc50d3f949cb57f3
Pierre Massat <pierre.massat@sentry.io>
2023-08-07 13:28:22 -0400

    Add consumer definition where appropriate

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -3092,6 +3092,10 @@ KAFKA_EVENTSTREAM_GENERIC = "generic-events"
 KAFKA_GENERIC_EVENTS_COMMIT_LOG = "snuba-generic-events-commit-log"
 KAFKA_GROUP_ATTRIBUTES = "group-attributes"
 
+# spans
+KAFKA_INGEST_SPANS = "ingest-spans"
+KAFKA_SNUBA_SPANS = "snuba-spans"
+
 KAFKA_SUBSCRIPTION_RESULT_TOPICS = {
     "events": KAFKA_EVENTS_SUBSCRIPTIONS_RESULTS,
     "transactions": KAFKA_TRANSACTIONS_SUBSCRIPTIONS_RESULTS,
@@ -3137,6 +3141,8 @@ KAFKA_TOPICS: Mapping[str, Optional[TopicDefinition]] = {
     KAFKA_EVENTSTREAM_GENERIC: {"cluster": "default"},
     KAFKA_GENERIC_EVENTS_COMMIT_LOG: {"cluster": "default"},
     KAFKA_GROUP_ATTRIBUTES: {"cluster": "default"},
+    KAFKA_INGEST_SPANS: {"cluster": "default"},
+    KAFKA_SNUBA_SPANS: {"cluster": "default"},
 }
 
 
