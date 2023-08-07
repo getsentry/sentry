3d6690b286c5baeb179702701c0b6fbb46f9ce45
Colleen O'Rourke <colleen@sentry.io>
2023-08-07 15:19:59 -0700

    ref(alerts): Swap usage of ProjectAlertRuleDetailsEndpoint PUT method

--- static/app/views/alerts/rules/metric/ruleForm.tsx
+++ static/app/views/alerts/rules/metric/ruleForm.tsx
@@ -630,7 +630,6 @@ class RuleFormContainer extends DeprecatedAsyncComponent<Props, State> {
       const [data, , resp] = await addOrUpdateRule(
         this.api,
         organization.slug,
-        project.slug,
         {
           ...rule,
           ...model.getTransformedData(),
