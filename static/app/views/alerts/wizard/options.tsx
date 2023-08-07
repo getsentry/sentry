c6982276ca81d817b283a85033dbe8556d5771dc
Ogi <86684834+obostjancic@users.noreply.github.com>
2023-08-07 16:27:06 +0200

    chartcuterie fix

--- static/app/views/alerts/wizard/options.tsx
+++ static/app/views/alerts/wizard/options.tsx
@@ -11,7 +11,7 @@ import {
   SpanOpBreakdown,
   WebVital,
 } from 'sentry/utils/fields';
-import {ON_DEMAND_METRICS_SUPPORTED_TAGS} from 'sentry/utils/onDemandMetrics';
+import {STANDARD_SEARCH_FIELD_KEYS} from 'sentry/utils/onDemandMetrics.const';
 import {
   Dataset,
   EventTypes,
@@ -260,7 +260,7 @@ export function datasetSupportedTags(
 
 function transactionSupportedTags(org: Organization) {
   if (org.features.includes('on-demand-metrics-extraction')) {
-    return Array.from(ON_DEMAND_METRICS_SUPPORTED_TAGS);
+    return STANDARD_SEARCH_FIELD_KEYS;
   }
   return TRANSACTION_SUPPORTED_TAGS;
 }
