ec340d794ffb3bb95607b159a5bfe3bc029cb95c
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-08-09 12:39:53 -0400

    Rename ambiguous variable

--- static/app/views/starfish/utils/sqlish/formatters/string.ts
+++ static/app/views/starfish/utils/sqlish/formatters/string.ts
@@ -3,21 +3,21 @@ import type {Token} from 'sentry/views/starfish/utils/sqlish/types';
 export function string(tokens: Token[]): string {
   let accumulator = '';
 
-  function contentize(content: Token): void {
-    if (Array.isArray(content.content)) {
-      content.content.forEach(contentize);
+  function contentize(token: Token): void {
+    if (Array.isArray(token.content)) {
+      token.content.forEach(contentize);
       return;
     }
 
-    if (typeof content.content === 'string') {
-      if (content.type === 'Keyword' && NEWLINE_KEYWORDS.has(content.content)) {
+    if (typeof token.content === 'string') {
+      if (token.type === 'Keyword' && NEWLINE_KEYWORDS.has(token.content)) {
         accumulator += '\n';
-        accumulator += content.content;
-      } else if (content.type === 'Whitespace') {
+        accumulator += token.content;
+      } else if (token.type === 'Whitespace') {
         // Convert all whitespace to single spaces
         accumulator += ' ';
       } else {
-        accumulator += content.content;
+        accumulator += token.content;
       }
 
       return;
