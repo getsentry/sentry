8d214d747475e6d417f353b1876c33d4793cf028
Mark Story <mark@mark-story.com>
2023-08-09 13:16:06 -0700

    feat(hybridcloud) Use control file for user/sentryapp avatars v2 (#54448)
    Mulligan on #54118. The first merge of this failed as the new logic is
    incompatible with a test in getsentry.
    
    This reverts commit 4e10fc336ac18e20584494c41e1fa8faa0e4535d.
    
    Requires https://github.com/getsentry/getsentry/pull/11392

--- tests/sentry/web/frontend/test_user_avatar.py
+++ tests/sentry/web/frontend/test_user_avatar.py
@@ -3,6 +3,7 @@ from io import BytesIO
 from django.urls import reverse
 
 from sentry.models import File, UserAvatar
+from sentry.models.files.control_file import ControlFile
 from sentry.silo import SiloMode
 from sentry.testutils.cases import TestCase
 from sentry.testutils.silo import assume_test_silo_mode, control_silo_test
@@ -17,6 +18,19 @@ class UserAvatarTest(TestCase):
             photo = File.objects.create(name="test.png", type="avatar.file")
             photo.putfile(BytesIO(b"test"))
         avatar = UserAvatar.objects.create(user=user, file_id=photo.id)
+        url = reverse("sentry-user-avatar-url", kwargs={"avatar_id": avatar.ident})
+        response = self.client.get(url)
+        assert response.status_code == 200
+        assert response["Cache-Control"] == FOREVER_CACHE
+        assert response.get("Vary") is None
+        assert response.get("Set-Cookie") is None
+
+    def test_headers_control_file(self):
+        user = self.create_user(email="a@example.com")
+        photo = ControlFile.objects.create(name="test.png", type="avatar.file")
+        photo.putfile(BytesIO(b"test"))
+        avatar = UserAvatar.objects.create(user=user, control_file_id=photo.id)
+
         url = reverse("sentry-user-avatar-url", kwargs={"avatar_id": avatar.ident})
         response = self.client.get(url)
         assert response.status_code == 200
