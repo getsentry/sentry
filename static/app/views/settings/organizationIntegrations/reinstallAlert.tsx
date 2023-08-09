4f7ac2e3f477a8f7489bdb11bb70bbce16fd41d5
Chloe <chloe.ho@sentry.io>
2023-08-09 15:07:41 -0700

    removing excess props

--- static/app/views/settings/organizationIntegrations/reinstallAlert.tsx
+++ static/app/views/settings/organizationIntegrations/reinstallAlert.tsx
@@ -1,25 +1,18 @@
-import {ReactNode} from 'react';
-
 import {Alert} from 'sentry/components/alert';
 import {t} from 'sentry/locale';
 import {Integration} from 'sentry/types';
 import {getIntegrationStatus} from 'sentry/utils/integrationUtil';
 
-type Props = React.ComponentPropsWithoutRef<typeof Alert> & {
+type Props = {
   integrations: Integration[] | null;
-  message?: ReactNode;
 };
 
-function ReinstallAlert({
-  message = t('Reinstall required for disabled integrations.'),
-  integrations = [],
-  ...props
-}: Props) {
+function ReinstallAlert({integrations = []}: Props) {
   const statusList = integrations?.map(getIntegrationStatus);
   if (statusList?.includes('disabled')) {
     return (
-      <Alert data-test-id="disabled-alert" type="warning" showIcon {...props}>
-        {message}
+      <Alert data-test-id="disabled-alert" type="warning" showIcon>
+        {t('Reinstall required for disabled integrations.')}
       </Alert>
     );
   }
