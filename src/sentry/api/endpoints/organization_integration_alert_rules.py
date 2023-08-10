c3f289674f764374a44e8b36aeafe4775e09c721
Michelle Fu <michelle.fu@sentry.io>
2023-08-10 14:03:06 -0700

    create API endpoint

---
+++ src/sentry/api/endpoints/organization_integration_alert_rules.py
@@ -0,0 +1,32 @@
+from typing import Any
+
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from sentry.api.base import region_silo_endpoint
+from sentry.api.bases.organization_integrations import RegionOrganizationIntegrationBaseEndpoint
+from sentry.integrations.mixins import NotifyBasicMixin
+from sentry.models import Organization
+
+
+@region_silo_endpoint
+class OrganizationIntegrationAlertRulesEndpoint(RegionOrganizationIntegrationBaseEndpoint):
+    def put(
+        self,
+        request: Request,
+        organization: Organization,
+        integration_id: int,
+        **kwds: Any,
+    ) -> Response:
+        """
+        Migrate API keys and alert rules from plugin to integration
+        ```````````````````````````````````````````````````````````
+        :pparam string organization: the organization the integration is installed in
+        :pparam string integration_id: the id of the integration
+        """
+        integration = self.get_integration(organization.id, integration_id)
+        installation = integration.get_installation(organization_id=organization.id)
+        if isinstance(installation, NotifyBasicMixin):
+            installation.migrate_alert_rules()
+            return Response(status=204)
+        return Response(status=400)
