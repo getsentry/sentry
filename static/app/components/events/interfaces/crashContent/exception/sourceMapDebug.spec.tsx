6cbe042fda32355255c3de0120cb49ef020a73b5
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2023-08-10 09:17:20 -0700

    update copy

--- static/app/components/events/interfaces/crashContent/exception/sourceMapDebug.spec.tsx
+++ static/app/components/events/interfaces/crashContent/exception/sourceMapDebug.spec.tsx
@@ -197,12 +197,12 @@ describe('SourceMapDebug', () => {
     });
 
     expect(
-      await screen.findByText("Sentry isn't Sentry without source maps")
+      await screen.findByText("You're not a computer, so why parse minified code?")
     ).toBeInTheDocument();
     expect(
       screen.getByText(
         textWithMarkupMatcher(
-          "You're not a computer, so why parse minified code? Upload source maps with the Sentry Wizard to unlock readable stack traces and better error grouping. Learn more"
+          'Upload source maps with the Sentry Wizard to unlock readable stack traces and better error grouping. Learn more'
         )
       )
     ).toBeInTheDocument();
