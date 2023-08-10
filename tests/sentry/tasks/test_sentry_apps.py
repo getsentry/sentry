4e0770a5d8b3a53bf64fa3de60d0b4e1fc5da4e5
Chloe <chloe.ho@sentry.io>
2023-08-09 17:00:38 -0700

    xfail

--- tests/sentry/tasks/test_sentry_apps.py
+++ tests/sentry/tasks/test_sentry_apps.py
@@ -879,6 +879,7 @@ class TestWebhookRequests(TestCase):
     @patch(
         "sentry.utils.sentry_apps.webhooks.safe_urlopen", return_value=MockFailureResponseInstance
     )
+    @pytest.mark.xfail(reason="This test is flaky")
     def test_slow_broken_not_disable(self, safe_urlopen):
         """
         Tests that the integration is broken after 10 days of errors but still enabled since flag is off
