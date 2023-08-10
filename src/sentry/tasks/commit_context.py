2595d3c24a74949ab3a12c697f5b41bd54fbd741
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2023-08-09 17:32:15 -0700

    Update src/sentry/tasks/commit_context.py
    Co-authored-by: Alberto Leal <mail4alberto@gmail.com>

--- src/sentry/tasks/commit_context.py
+++ src/sentry/tasks/commit_context.py
@@ -93,7 +93,7 @@ def queue_comment_task_if_needed(
         )
         return
 
-    pr = pr_query[0]
+    pr = pr_query.first()
     if pr.date_added >= datetime.now(tz=timezone.utc) - timedelta(days=PR_COMMENT_WINDOW) and (
         not pr.pullrequestcomment_set.exists()
         or group_owner.group_id not in pr.pullrequestcomment_set.get().group_ids
