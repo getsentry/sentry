5d66094e04cacc76e10f1d1df80de4c0ae3b5b12
Tony Xiao <txiao@sentry.io>
2023-08-10 10:32:37 -0400

    feat(statistical-detectors): Detect potential function regression usi… (#54230)
    …ng EMAs
    
    This PR builds on top of #53632 to run a statistical detector on the
    function durations. The basic approach is as follows.
    
    We track 2 moving averages.
    - 1 weighted more for the short term
    - 1 weighted more for the long term
    
    Whenever the short-term moving average crosses from below to above the
    long-term moving average, this is an indication that there is an upwards
    trend. So this is a good place to start the detailed analysis.
    
    # Note
    
    This is not connected to the celery task yet as we're not ready to write
    to Redis yet.

---
+++ src/sentry/profiles/statistical_detectors.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+
+import logging
+from dataclasses import dataclass
+from datetime import datetime
+from enum import Enum
+from typing import Any, List, Mapping, MutableMapping, Optional, Tuple
+
+from sentry_redis_tools.clients import RedisCluster, StrictRedis
+
+from sentry.models.project import Project
+from sentry.utils.math import ExponentialMovingAverage
+
+logger = logging.getLogger("sentry.tasks.statistical_detectors")
+
+KEY_TTL = 24 * 60 * 60  # 1 day TTL
+MIN_DATA_POINTS = 6
+VERSION = 1
+
+
+class TrendType(Enum):
+    Regressed = "regressed"
+    Improved = "improved"
+    Unchanged = "unchanged"
+
+
+@dataclass
+class TrendState:
+    timestamp: Optional[datetime]
+    count: int
+    short_ma: float
+    long_ma: float
+
+    FIELD_TIMESTAMP = "T"
+    FIELD_COUNT = "N"
+    FIELD_SHORT_TERM = "S"
+    FIELD_LONG_TERM = "L"
+
+    def as_dict(self) -> Mapping[str | bytes, str | float | int]:
+        d: MutableMapping[str | bytes, str | float | int] = {
+            TrendState.FIELD_COUNT: self.count,
+            TrendState.FIELD_SHORT_TERM: self.short_ma,
+            TrendState.FIELD_LONG_TERM: self.long_ma,
+        }
+        if self.timestamp is not None:
+            d[TrendState.FIELD_TIMESTAMP] = self.timestamp.isoformat()
+        return d
+
+    @staticmethod
+    def from_dict(d: Any) -> TrendState:
+        try:
+            count = int(d.get(TrendState.FIELD_COUNT, 0))
+        except ValueError:
+            count = 0
+
+        try:
+            short_ma = float(d.get(TrendState.FIELD_SHORT_TERM, 0))
+        except ValueError:
+            short_ma = 0
+
+        try:
+            long_ma = float(d.get(TrendState.FIELD_LONG_TERM, 0))
+        except ValueError:
+            long_ma = 0
+
+        try:
+            timestamp = datetime.fromisoformat(d.get(TrendState.FIELD_TIMESTAMP, ""))
+        except ValueError:
+            timestamp = None
+
+        return TrendState(timestamp, count, short_ma, long_ma)
+
+
+@dataclass
+class TrendPayload:
+    group: str | int
+    count: float
+    p95: float
+    timestamp: datetime
+
+
+def run_functions_trend_detection(
+    client: RedisCluster | StrictRedis,
+    project: Project,
+    start: datetime,
+    payloads: List[TrendPayload],
+) -> Tuple[List[TrendPayload], List[TrendPayload]]:
+    with client.pipeline() as pipeline:
+        for payload in payloads:
+            key = make_key(project.id, payload, VERSION)
+            pipeline.hgetall(key)
+        results = pipeline.execute()
+
+    old_states = [TrendState.from_dict(result) for result in results]
+    new_states, regressed_functions, improved_functions = compute_new_trend_states(
+        project.id, old_states, payloads
+    )
+
+    with client.pipeline() as pipeline:
+        for key, value in new_states:
+            pipeline.hmset(key, value.as_dict())
+            pipeline.expire(key, KEY_TTL)
+
+        pipeline.execute()
+
+    return regressed_functions, improved_functions
+
+
+def compute_new_trend_states(
+    project_id: int,
+    old_states: List[TrendState],
+    payloads: List[TrendPayload],
+) -> Tuple[List[Tuple[str, TrendState]], List[TrendPayload], List[TrendPayload]]:
+    new_states: List[Tuple[str, TrendState]] = []
+    regressed_functions: List[TrendPayload] = []
+    improved_functions: List[TrendPayload] = []
+
+    for payload, old_state in zip(payloads, old_states):
+        if old_state.timestamp is not None and old_state.timestamp > payload.timestamp:
+            # In the event that the timestamp is before the payload's timestamps,
+            # we do not want to process this payload.
+            #
+            # This should not happen other than in some error state.
+            logger.warning(
+                "Function trend detection out of order. Processing %s, but last processed was %s",
+                payload.timestamp.isoformat(),
+                old_state.timestamp.isoformat(),
+            )
+            continue
+
+        key = make_key(project_id, payload, VERSION)
+        trend, value = detect_trend(old_state, payload)
+
+        if trend == TrendType.Regressed:
+            regressed_functions.append(payload)
+        elif trend == TrendType.Improved:
+            improved_functions.append(payload)
+
+        new_states.append((key, value))
+
+    return new_states, regressed_functions, improved_functions
+
+
+def make_key(project_id: int, payload: TrendPayload, version: int) -> str:
+    return f"statdtr:v:{version}:p:{project_id}:f:{payload.group}"
+
+
+def detect_trend(state: TrendState, payload: TrendPayload) -> Tuple[TrendType, TrendState]:
+    """
+    Detect if a change has occurred using the moving average cross over.
+    See https://en.wikipedia.org/wiki/Moving_average_crossover.
+
+    We keep track of 2 moving averages, one with a shorter period and one with
+    a longer period. The shorter period moving average is a faster moving average
+    that is more sensitive to immediate changes in the timeseries. The longer
+    period moving average is a slower moving average that smooths out more of the
+    noise that may be present in the fast moving average.
+
+    By tracking both of these moving averages, we're looking for when the fast
+    moving average cross the slow moving average from below. Whenever this happens,
+    it is an indication that the timeseries is trending upwards.
+
+    Here, we choose to use the exponential moving average as it is less sensitive
+    to large variations in the data and allows us weigh the newly seen data more
+    heavily than old data that is no longer relevant.
+    """
+
+    # This EMA uses a shorter period to follow the timeseries more closely.
+    ema_short = ExponentialMovingAverage(smoothing=2, period=20)
+    ema_short.set(state.short_ma, state.count)
+    ema_short.update(payload.p95)
+
+    # This EMA uses a longer period to follow the overal trend of the timeseries.
+    ema_long = ExponentialMovingAverage(smoothing=2, period=40)
+    ema_long.set(state.long_ma, state.count)
+    ema_long.update(payload.p95)
+
+    # The heuristic isn't stable initially, so ensure we have a minimum
+    # number of data points before looking for a regression.
+    stablized = state.count > MIN_DATA_POINTS
+
+    if stablized and ema_short.value > ema_long.value and state.short_ma <= state.long_ma:
+        # The new fast moving average is above the new slow moving average.
+        # The old fast moving average is below the old slow moving average.
+        # This indicates an upwards trend.
+        trend = TrendType.Regressed
+    elif stablized and ema_short.value < ema_long.value and state.short_ma >= state.long_ma:
+        # The new fast moving average is below the new slow moving average
+        # The old fast moving average is above the old slow moving average
+        # This indicates an downwards trend.
+        trend = TrendType.Improved
+    else:
+        trend = TrendType.Unchanged
+
+    new_state = TrendState(payload.timestamp, state.count + 1, ema_short.value, ema_long.value)
+
+    return trend, new_state
---
+++ tests/sentry/profiles/test_statistical_detectors.py
@@ -0,0 +1,121 @@
+from datetime import datetime, timedelta
+
+import pytest
+
+from sentry.profiles.statistical_detectors import TrendPayload, TrendState, compute_new_trend_states
+
+
+@pytest.mark.parametrize(
+    "data,expected",
+    [
+        pytest.param(
+            {},
+            {
+                TrendState.FIELD_COUNT: 0,
+                TrendState.FIELD_SHORT_TERM: 0,
+                TrendState.FIELD_LONG_TERM: 0,
+            },
+            id="empty dict",
+        ),
+        pytest.param(
+            {
+                TrendState.FIELD_COUNT: "1",
+                TrendState.FIELD_SHORT_TERM: "2",
+                TrendState.FIELD_LONG_TERM: "3",
+                TrendState.FIELD_TIMESTAMP: datetime(2023, 8, 9, 14, 17).isoformat(),
+            },
+            {
+                TrendState.FIELD_COUNT: 1,
+                TrendState.FIELD_SHORT_TERM: 2,
+                TrendState.FIELD_LONG_TERM: 3,
+                TrendState.FIELD_TIMESTAMP: datetime(2023, 8, 9, 14, 17).isoformat(),
+            },
+            id="with timestamp",
+        ),
+        pytest.param(
+            {
+                TrendState.FIELD_COUNT: "1",
+                TrendState.FIELD_SHORT_TERM: "2",
+                TrendState.FIELD_LONG_TERM: "3",
+            },
+            {
+                TrendState.FIELD_COUNT: 1,
+                TrendState.FIELD_SHORT_TERM: 2,
+                TrendState.FIELD_LONG_TERM: 3,
+            },
+            id="no timestamp",
+        ),
+        pytest.param(
+            {
+                TrendState.FIELD_COUNT: "foo",
+                TrendState.FIELD_SHORT_TERM: "bar",
+                TrendState.FIELD_LONG_TERM: "baz",
+                TrendState.FIELD_TIMESTAMP: "qux",
+            },
+            {
+                TrendState.FIELD_COUNT: 0,
+                TrendState.FIELD_SHORT_TERM: 0,
+                TrendState.FIELD_LONG_TERM: 0,
+            },
+            id="bad values",
+        ),
+    ],
+)
+def test_trend_state(data, expected):
+    state = TrendState.from_dict(data)
+    d = state.as_dict()
+    assert d == expected
+
+
+@pytest.mark.parametrize(
+    "initial,p95s,regressed_indices,improved_indices",
+    [
+        pytest.param(
+            TrendState(None, 0, 0, 0),
+            [1 for _ in range(10)] + [2 for _ in range(10)],
+            [10],
+            [],
+            id="stepwise increase",
+        ),
+        pytest.param(
+            TrendState(None, 0, 0, 0),
+            [2 for _ in range(10)] + [1 for _ in range(10)],
+            [],
+            [10],
+            id="stepwise decrease",
+        ),
+        pytest.param(
+            TrendState(None, 0, 0, 0),
+            [(i / 10) ** 2 for i in range(-10, 20)],
+            [23],
+            [],
+            id="quadratic increase",
+        ),
+        pytest.param(
+            TrendState(None, 0, 0, 0),
+            [-((i / 10) ** 2) for i in range(-10, 20)],
+            [],
+            [23],
+            id="quadratic decrease",
+        ),
+    ],
+)
+def test_run_functions_trend_detection(initial, p95s, regressed_indices, improved_indices):
+    states = [initial]
+    all_regressed = []
+    all_improved = []
+
+    now = datetime.now()
+
+    payloads = [
+        TrendPayload(0, i + 1, p95, now + timedelta(hours=i + 1)) for i, p95 in enumerate(p95s)
+    ]
+
+    for payload in payloads:
+        new_states, regressed, improved = compute_new_trend_states(1, states, [payload])
+        states = [state for _, state in new_states]
+        all_regressed.extend(regressed)
+        all_improved.extend(improved)
+
+    assert all_regressed == [payloads[i] for i in regressed_indices]
+    assert all_improved == [payloads[i] for i in improved_indices]
