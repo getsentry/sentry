fc9b887d1c9dbd4f2cee6899e7a9933bd74e6cf9
Colton Allen <colton.allen@sentry.io>
2023-08-10 16:17:18 -0500

    Remove note

--- src/sentry/replays/usecases/query/configs/scalar.py
+++ src/sentry/replays/usecases/query/configs/scalar.py
@@ -1,7 +1,1 @@
-"""Aggregate query filtering configuration module.
-
-Currently not defined.  You would define fields here (just like the aggregate configuration) for
-our sub-query optimization (assuming that optimization is still needed).  This file is left blank
-for now and can be filled in at a later date if/when the subquery optimization becomes necessary
-under the new condition system.
-"""
+"""Aggregate query filtering configuration module."""
