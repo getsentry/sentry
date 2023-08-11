8bf08c8f6ece9e105f30e9b26e14074fcfb0762e
Joshua Li <joshuarli98@gmail.com>
2023-08-11 16:40:52 -0700

    Revert "wait why isnt port forwarding sufficient"
    This reverts commit 42df951c622e6285d529aaffe5ac1436f2d64ed5.

--- src/sentry/runner/commands/devservices.py
+++ src/sentry/runner/commands/devservices.py
@@ -308,7 +308,14 @@ def _prepare_containers(
                 click.secho(f"! Skipping {name} due to only_if condition", err=True, fg="cyan")
             continue
 
-        options["network"] = project
+        if DARWIN:
+            # This isn't ideal, but it's an easy solution to make everything work
+            # in CI. docker-py doesn't support resolving the magic "host-internal" string
+            # so we can't extra_host that as host.docker.internal.
+            options["network"] = project
+        else:
+            options["network_mode"] = "host"
+
         options["detach"] = True
         options["name"] = project + "_" + name
         options.setdefault("ports", {})
