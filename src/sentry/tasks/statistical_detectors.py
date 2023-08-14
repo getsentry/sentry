dd83c44d2fa3e6afce4076f6b9b330e2ae9c2df3
Tony Xiao <txiao@sentry.io>
2023-08-14 13:11:18 -0400

    feat(statistical-detectors): Batch functions query
    Querying 1 project at a time scales linearly in terms of the number of projects
    and will be able to handle the necessary volume. This creates batches of 100
    projects per query to minimize the number of queries to clickhouse.

--- src/sentry/tasks/statistical_detectors.py
+++ src/sentry/tasks/statistical_detectors.py
@@ -1,4 +1,5 @@
 import logging
+from collections import defaultdict
 from datetime import datetime, timedelta
 from typing import Any, Dict, List
 
@@ -11,9 +12,13 @@ from sentry.snuba import functions
 from sentry.snuba.referrer import Referrer
 from sentry.statistical_detectors.detector import TrendPayload
 from sentry.tasks.base import instrumented_task
+from sentry.utils.iterators import chunked
 
 logger = logging.getLogger("sentry.tasks.statistical_detectors")
 
+
+FUNCTIONS_PER_PROJECT = 100
+PROJECTS_PER_QUERY = 100
 
 ITERATOR_CHUNK = 1_000
 
@@ -86,9 +91,9 @@ def detect_function_trends(project_ids: List[int], start: datetime, **kwargs) ->
     if not options.get("statistical_detectors.enable"):
         return
 
-    for project in Project.objects.filter(id__in=project_ids):
+    for projects in chunked(Project.objects.filter(id__in=project_ids), PROJECTS_PER_QUERY):
         try:
-            query_functions(project, start)
+            query_functions(projects, start)
         except Exception as e:
             sentry_sdk.capture_exception(e)
 
@@ -97,14 +102,15 @@ def query_transactions(project_id: int) -> None:
     pass
 
 
-def query_functions(project: Project, start: datetime) -> List[TrendPayload]:
-    params = _get_function_query_params(project, start)
+def query_functions(projects: List[Project], start: datetime) -> Dict[int, List[TrendPayload]]:
+    params = _get_function_query_params(projects, start)
 
     # TODOs:
     # - format and return this for further processing
     # - handle any errors
-    results = functions.query(
+    query_results = functions.query(
         selected_columns=[
+            "project.id",
             "timestamp",
             "fingerprint",
             "count()",
@@ -112,26 +118,28 @@ def query_functions(project: Project, start: datetime) -> List[TrendPayload]:
         ],
         query="is_application:1",
         params=params,
-        orderby=["-count()"],
-        limit=100,
+        orderby=["project.id", "-count()"],
+        limit=FUNCTIONS_PER_PROJECT * len(projects),
         referrer=Referrer.API_PROFILING_FUNCTIONS_STATISTICAL_DETECTOR.value,
         auto_aggregations=True,
         use_aggregate_conditions=True,
         transform_alias_to_input_format=True,
     )
 
-    return [
-        TrendPayload(
+    function_results = defaultdict(list)
+    for row in query_results["data"]:
+        payload = TrendPayload(
             group=row["fingerprint"],
             count=row["count()"],
             p95=row["p95()"],
             timestamp=datetime.fromisoformat(row["timestamp"]),
         )
-        for row in results["data"]
-    ]
+        function_results[row["project.id"]].append(payload)
+
+    return function_results
 
 
-def _get_function_query_params(project: Project, start: datetime) -> Dict[str, Any]:
+def _get_function_query_params(projects: List[Project], start: datetime) -> Dict[str, Any]:
     # The functions dataset only supports 1 hour granularity.
     # So we always look back at the last full hour that just elapsed.
     # And since the timestamps are truncated to the start of the hour
@@ -142,7 +150,6 @@ def _get_function_query_params(project: Project, start: datetime) -> Dict[str, A
     return {
         "start": start,
         "end": start + timedelta(minutes=1),
-        "project_id": [project.id],
-        "project_objects": [project],
-        "organization_id": project.organization_id,
+        "project_id": [project.id for project in projects],
+        "project_objects": projects,
     }
