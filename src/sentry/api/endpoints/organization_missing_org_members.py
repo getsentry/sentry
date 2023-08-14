436617a29fa8fb6acaa78e3c9918fb7943c2bfa1
Cathy Teng <cathy.teng@sentry.io>
2023-08-14 08:17:56 -0700

    fix typing

--- src/sentry/api/endpoints/organization_missing_org_members.py
+++ src/sentry/api/endpoints/organization_missing_org_members.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from datetime import timedelta
 from email.headerregistry import Address
 
@@ -59,13 +61,13 @@ class OrganizationMissingMembersEndpoint(OrganizationEndpoint):
             .order_by("-commit_count")
         )
 
-    def _get_shared_email_domain(self, organization):
+    def _get_shared_email_domain(self, organization) -> str | None:
         # if a member has user_email=None, then they have yet to accept an invite
         org_owners = organization.get_members_with_org_roles(
             roles=[roles.get_top_dog().id]
         ).exclude(Q(user_email=None) | Q(user_email=""))
 
-        def _get_email_domain(email: str) -> str:
+        def _get_email_domain(email: str) -> str | None:
             try:
                 domain = Address(addr_spec=email).domain
             except Exception:
