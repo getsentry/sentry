e7aa88b71bcafc5980119d3867cdf650fb659bdd
Joshua Li <joshuarli98@gmail.com>
2023-08-14 15:38:59 -0700

    ensure rosetta is running

--- scripts/start-colima.py
+++ scripts/start-colima.py
@@ -13,6 +13,16 @@ def main(argv: Sequence[str] | None = None) -> int:
     # 12.3.1: arm64
     APPLE_ARM64 = sys.platform == "darwin" and platform.processor() in {"arm", "arm64"}
 
+    if APPLE_ARM64:
+        # We need to ensure that Rosetta is running, because colima will only WARN
+        # if --vz-rosetta and continue on using Virtualization.Framework without Rosetta.
+        # Clickhouse images do not work without Rosetta.
+        if subprocess.call(("pgrep", "oahd")) != 0:
+            # Going through this is the only way I've found to start rosetta, even
+            # if it is installed.
+            sys.stdout.write("Rosetta 2 isn't running. Expect a prompt to install/start it.\n")
+            subprocess.check_call(("softwareupdate", "--install-rosetta"))
+
     cpus = int(subprocess.run(("sysctl", "-n", "hw.ncpu"), check=True, capture_output=True).stdout)
     memsize_bytes = int(
         subprocess.run(("sysctl", "-n", "hw.memsize"), check=True, capture_output=True).stdout
@@ -24,11 +34,13 @@ def main(argv: Sequence[str] | None = None) -> int:
         f"{memsize_bytes//(2*1024**3)}",
     ]
     if APPLE_ARM64:
-        args = [*args, "--vm-type=vz", "--vz-rosetta", "--mount-type=virtiofs"]
+        # Note that --arch=aarch64 is redundant but I want to emphasize it.
+        args = [*args, "--arch=aarch64", "--vm-type=vz", "--vz-rosetta", "--mount-type=virtiofs"]
     return subprocess.call(
         (
             "colima",
             "start",
+            "-v",
             f"--mount=/var/folders:w,/private/tmp/colima:w,{os.path.expanduser('~')}:r",
             *args,
         )
