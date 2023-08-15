e7586016d9de09c7cf594872fdf4865ddfb5cb70
Stephen Cefali <scefali@sentry.io>
2023-08-15 10:24:56 -0700

    cleanup

--- src/sentry/models/__init__.py
+++ src/sentry/models/__init__.py
@@ -62,7 +62,7 @@ from .integrations import *  # NOQA
 from .latestappconnectbuildscheck import *  # NOQA
 from .latestreporeleaseenvironment import *  # NOQA
 from .lostpasswordhash import *  # NOQA
-from .notificationsetting import *  # NOQANOQA
+from .notificationsetting import *  # NOQA
 from .notificationsettingoption import *  # NOQA
 from .notificationsettingprovider import *  # NOQA
 from .options import *  # NOQA
