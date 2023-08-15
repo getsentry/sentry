9af62996ca0e72c80c47cb1e84a7f6de96b09b1e
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2023-08-15 12:04:34 -0700

    feat(commit-context): Do not create if older than 1 year (#54624)
    ## Objective:
    Do not create a suspect commit if the resulting commit is over 1 year
    old.

--- tests/sentry/tasks/test_post_process.py
+++ tests/sentry/tasks/test_post_process.py
@@ -1303,7 +1303,9 @@ class AssignmentTestMixin(BasePostProgressGroupMixin):
 class ProcessCommitsTestMixin(BasePostProgressGroupMixin):
     github_blame_return_value = {
         "commitId": "asdfwreqr",
-        "committedDate": "",
+        "committedDate": (datetime.now(timezone.utc) - timedelta(days=2)).strftime(
+            "%Y-%m-%dT%H:%M:%SZ"
+        ),
         "commitMessage": "placeholder commit message",
         "commitAuthorName": "",
         "commitAuthorEmail": "admin@localhost",
