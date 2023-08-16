193648e9d4018f4819b7600100e2ac3324906480
isabellaenriquez <isabella.enriquez@sentry.io>
2023-08-16 12:38:02 -0700

    change mock to use TestStubs + add test for commits without message

--- static/app/components/lastCommit.spec.tsx
+++ static/app/components/lastCommit.spec.tsx
@@ -1,40 +1,30 @@
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import LastCommit from 'sentry/components/lastCommit';
-import {Commit, Repository} from 'sentry/types';
 
 describe('LastCommit', function () {
-  const mockedCommit: Commit = {
-    dateCreated: '2020-11-30T18:46:31Z',
-    id: 'f7f395d14b2fe29a4e253bf1d3094d61e6ad4434',
-    message: 'ref(commitRow): refactor to fc\n',
-    repository: {
-      id: '1',
-      integrationId: '2',
-      name: 'getsentry/sentry',
-      dateCreated: '2019-11-30T18:46:31Z',
-      url: 'https://www.github.com/getsentry/sentry',
-      provider: {
-        id: 'github',
-        name: 'GitHub',
-      },
-    } as Repository,
-    releases: [],
-  };
+  const mockedCommit = TestStubs.Commit();
+  const mockedCommitTitle = '(improve) Add Links to Spike-Protection Email (#2408)';
 
   it('renders', function () {
     const wrapper = render(<LastCommit commit={mockedCommit} />);
-
     expect(wrapper.container).toSnapshot();
   });
 
-  it('links to the commit in GitHub', async function () {
-    const mockedCommitURL = mockedCommit.repository?.url + '/commit/' + mockedCommit.id;
+  it('links to the commit in GitHub', function () {
+    mockedCommit.repository.provider = {id: 'github'};
+    const mockedCommitURL = `${mockedCommit.repository?.url}/commit/${mockedCommit.id}`;
+
     render(<LastCommit commit={mockedCommit} />);
-    expect(await screen.getByText('ref(commitRow): refactor to fc')).toBeInTheDocument();
-    expect(screen.getByText('ref(commitRow): refactor to fc')).toHaveAttribute(
-      'href',
-      mockedCommitURL
-    );
+    expect(screen.getByText(mockedCommitTitle)).toBeInTheDocument();
+    expect(screen.getByText(mockedCommitTitle)).toHaveAttribute('href', mockedCommitURL);
+  });
+
+  it('displays the commit with its shortened ID if it has no message', function () {
+    mockedCommit.message = null;
+
+    render(<LastCommit commit={mockedCommit} />);
+    expect(screen.queryByText(mockedCommitTitle)).not.toBeInTheDocument();
+    expect(screen.getByText(mockedCommit.id.slice(0, 7))).toBeInTheDocument();
   });
 });
