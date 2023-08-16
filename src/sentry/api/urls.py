113f27cd3cdce5901fdb8eb55fd5cedd2c665eca
Colton Allen <colton.allen@sentry.io>
2023-08-16 14:47:03 -0500

    feat(replays): Add selector aggregation endpoint (#53530)
    Adds click selector aggregation to the replays products. Reading from
    this API will show you the top DOM selectors by rage and dead click
    counts.
    
    ---------
    
    Co-authored-by: Ryan Albrecht <ryan.albrecht@sentry.io>

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -93,6 +93,9 @@ from sentry.replays.endpoints.organization_replay_events_meta import (
     OrganizationReplayEventsMetaEndpoint,
 )
 from sentry.replays.endpoints.organization_replay_index import OrganizationReplayIndexEndpoint
+from sentry.replays.endpoints.organization_replay_selector_index import (
+    OrganizationReplaySelectorIndexEndpoint,
+)
 from sentry.replays.endpoints.project_replay_clicks_index import ProjectReplayClicksIndexEndpoint
 from sentry.replays.endpoints.project_replay_details import ProjectReplayDetailsEndpoint
 from sentry.replays.endpoints.project_replay_recording_segment_details import (
@@ -1718,6 +1721,11 @@ ORGANIZATION_URLS = [
         r"^(?P<organization_slug>[^\/]+)/replays/$",
         OrganizationReplayIndexEndpoint.as_view(),
         name="sentry-api-0-organization-replay-index",
+    ),
+    re_path(
+        r"^(?P<organization_slug>[^\/]+)/replay-selectors/$",
+        OrganizationReplaySelectorIndexEndpoint.as_view(),
+        name="sentry-api-0-organization-replay-selectors-index",
     ),
     re_path(
         r"^(?P<organization_slug>[^\/]+)/replay-count/$",
