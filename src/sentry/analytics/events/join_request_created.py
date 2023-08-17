caafba6b212df0569d1c8f3889d2e0d4e24b9202
Nathan Hsieh <6186377+nhsiehgit@users.noreply.github.com>
2023-08-17 10:23:09 -0700

    add referrer to analytics record (#54947)
    Realized we aren't passing referrer into the join request event

--- src/sentry/analytics/events/join_request_created.py
+++ src/sentry/analytics/events/join_request_created.py
@@ -4,7 +4,11 @@ from sentry import analytics
 class JoinRequestCreatedEvent(analytics.Event):
     type = "join_request.created"
 
-    attributes = (analytics.Attribute("member_id"), analytics.Attribute("organization_id"))
+    attributes = (
+        analytics.Attribute("member_id"),
+        analytics.Attribute("organization_id"),
+        analytics.Attribute("referrer", required=False),
+    )
 
 
 analytics.register(JoinRequestCreatedEvent)
