8d0d856da125b9ef57509f06a570018e2c335e9d
Mark Story <mark@mark-story.com>
2023-08-17 11:49:10 -0400

    feat(hybridcloud) Add silo limiters for views
    Add silo limiting decorators for frontend views. Having these will
    resolve a few 500s in the test environment and create 404s instead.
    
    We'll still need to change the host we request avatars from in order to
    get them fully working.

--- src/sentry/web/frontend/team_avatar.py
+++ src/sentry/web/frontend/team_avatar.py
@@ -1,6 +1,7 @@
 from sentry.models import TeamAvatar
-from sentry.web.frontend.base import AvatarPhotoView
+from sentry.web.frontend.base import AvatarPhotoView, region_silo_view
 
 
+@region_silo_view
 class TeamAvatarPhotoView(AvatarPhotoView):
     model = TeamAvatar
