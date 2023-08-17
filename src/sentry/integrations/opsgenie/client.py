3edb8f6bf25ba9cc4244cd932c35a47aa0475925
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2023-08-17 15:00:47 -0700

    feat(opsgenie): Update installation flow (#54997)
    Update the Opsgenie integration install flow to remove the need for a
    management API key.
    
    <img width="1512" alt="Screenshot 2023-08-17 at 2 09 44 PM"
    src="https://github.com/getsentry/sentry/assets/83109586/22e5aa07-884f-410f-b64b-76ad49463cd8">
    <img width="1512" alt="Screenshot 2023-08-17 at 2 09 52 PM"
    src="https://github.com/getsentry/sentry/assets/83109586/c451daaa-3959-4717-8e2b-3fd7004a5111">
    
    Fixes ER-1848

--- src/sentry/integrations/opsgenie/client.py
+++ src/sentry/integrations/opsgenie/client.py
@@ -3,32 +3,12 @@ from __future__ import annotations
 from urllib.parse import quote
 
 from sentry.eventstore.models import Event, GroupEvent
-from sentry.integrations.client import ApiClient
 from sentry.models import Group, Integration
 from sentry.services.hybrid_cloud.integration.model import RpcIntegration
 from sentry.shared_integrations.client.base import BaseApiResponseX
 from sentry.shared_integrations.client.proxy import IntegrationProxyClient
 
 OPSGENIE_API_VERSION = "v2"
-
-
-class OpsgenieSetupClient(ApiClient):
-    """
-    API Client that doesn't require an installation.
-    This client is used during integration setup to fetch data
-    needed to build installation metadata
-    """
-
-    integration_name = "opsgenie_setup"
-
-    def __init__(self, base_url: str, api_key: str) -> None:
-        super().__init__()
-        self.base_url = f"{base_url}{OPSGENIE_API_VERSION}"
-        self.api_key = api_key
-
-    def get_account(self):
-        headers = {"Authorization": "GenieKey " + self.api_key}
-        return self.get(path="/account", headers=headers)
 
 
 class OpsgenieClient(IntegrationProxyClient):
