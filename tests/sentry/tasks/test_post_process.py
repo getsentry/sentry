5e70962ba5540b81145cbea9a4bb4a6020eadcbb
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2023-08-17 07:07:28 -0700

    feat(commit-context): Do not create if older than 1 year old (#54866)
    ## Objective
    
    Update on the reverted PR: #54624 
    
    The original PR had errors from GitLab date strings not being parsed
    correctly bc they are formatted differently from GitHub. So this PR
    returns the datetime object for internal usage and will make date
    comparisons easier without needing to know which integration it came
    from.

--- tests/sentry/tasks/test_post_process.py
+++ tests/sentry/tasks/test_post_process.py
@@ -1303,7 +1303,7 @@ class AssignmentTestMixin(BasePostProgressGroupMixin):
 class ProcessCommitsTestMixin(BasePostProgressGroupMixin):
     github_blame_return_value = {
         "commitId": "asdfwreqr",
-        "committedDate": "",
+        "committedDate": (datetime.now(timezone.utc) - timedelta(days=2)),
         "commitMessage": "placeholder commit message",
         "commitAuthorName": "",
         "commitAuthorEmail": "admin@localhost",
