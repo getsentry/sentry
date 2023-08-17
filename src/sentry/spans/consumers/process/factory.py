e3e25f1ea62d6537d00f321884a2c9aff5e2d01a
Pierre Massat <pierre.massat@sentry.io>
2023-08-17 14:55:08 -0400

    fix(spans): Read values from the right fields of the Relay span (#54941)

--- src/sentry/spans/consumers/process/factory.py
+++ src/sentry/spans/consumers/process/factory.py
@@ -21,13 +21,17 @@ from sentry.utils.kafka_config import get_kafka_producer_cluster_options, get_to
 
 TAG_MAPPING = {
     "span.action": "action",
+    "span.description": "description",
     "span.domain": "domain",
     "span.group": "group",
     "span.module": "module",
+    "span.op": "op",
+    "span.status": "status",
     "span.status_code": "status_code",
     "span.system": "system",
-    "transaction.method": "http.method",
-    "transaction.op": "transaction_op",
+    "transaction": "transaction",
+    "transaction.method": "transaction.method",
+    "transaction.op": "transaction.op",
 }
 SPAN_SCHEMA_VERSION = 1
 DEFAULT_SPAN_RETENTION_DAYS = 90
@@ -45,7 +49,6 @@ def _build_snuba_span(relay_span: Mapping[str, Any]) -> MutableMapping[str, Any]
 
     snuba_span: MutableMapping[str, Any] = {}
     snuba_span["description"] = relay_span.get("description")
-    snuba_span["event_id"] = relay_span.get("event_id", "")
     snuba_span["exclusive_time_ms"] = int(relay_span.get("exclusive_time", 0))
     snuba_span["is_segment"] = not relay_span.get("parent_span_id")
     snuba_span["organization_id"] = organization.id
@@ -53,7 +56,7 @@ def _build_snuba_span(relay_span: Mapping[str, Any]) -> MutableMapping[str, Any]
     snuba_span["project_id"] = relay_span["project_id"]
     snuba_span["retention_days"] = retention_days
     snuba_span["segment_id"] = relay_span.get("segment_id", "0")
-    snuba_span["span_id"] = int(relay_span.get("span_id", 0), 16)
+    snuba_span["span_id"] = relay_span.get("span_id", "0")
     snuba_span["tags"] = relay_span.get("tags")
     snuba_span["trace_id"] = uuid.UUID(relay_span["trace_id"]).hex
     snuba_span["version"] = SPAN_SCHEMA_VERSION
@@ -71,21 +74,27 @@ def _build_snuba_span(relay_span: Mapping[str, Any]) -> MutableMapping[str, Any]
         snuba_span["group_raw"] = trace_context.get("hash")
 
     sentry_tags: MutableMapping[str, Any] = {}
-    if tags := relay_span.get("tags"):
+    if tags := relay_span.get("data"):
         for relay_tag, snuba_tag in TAG_MAPPING.items():
             if relay_tag in tags:
                 sentry_tags[snuba_tag] = tags.get(relay_tag)
-    sentry_tags["op"] = relay_span.get("op")
-    sentry_tags["status"] = relay_span.get("status")
     snuba_span["sentry_tags"] = sentry_tags
 
     return snuba_span
+
+
+def _format_event_id(payload: Mapping[str, Any]) -> str:
+    event_id = payload.get("event_id")
+    if not event_id:
+        return ""
+    return uuid.UUID(event_id).hex
 
 
 def _process_message(message: Message[KafkaPayload]) -> KafkaPayload:
     payload = msgpack.unpackb(message.payload.value)
     relay_span = payload["span"]
     relay_span["project_id"] = payload["project_id"]
+    relay_span["event_id"] = _format_event_id(payload)
     snuba_span = _build_snuba_span(relay_span)
     snuba_payload = json.dumps(snuba_span).encode("utf-8")
     return KafkaPayload(key=None, value=snuba_payload, headers=[])
