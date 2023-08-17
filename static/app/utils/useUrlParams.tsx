09840e1044590c5ca2ddd4389a28939c135f6cdc
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-08-17 08:13:49 -0700

    fix: Improve return type of useUrlParams

--- static/app/utils/useUrlParams.tsx
+++ static/app/utils/useUrlParams.tsx
@@ -5,22 +5,22 @@ function useUrlParams(
   defaultKey: string,
   defaultValue: string
 ): {
-  getParamValue: () => string;
+  getParamValue: () => string | undefined;
   setParamValue: (value: string) => void;
 };
 function useUrlParams(defaultKey: string): {
-  getParamValue: () => string;
+  getParamValue: () => string | undefined;
   setParamValue: (value: string) => void;
 };
 function useUrlParams(): {
-  getParamValue: (key: string) => string;
+  getParamValue: (key: string) => string | undefined;
   setParamValue: (key: string, value: string) => void;
 };
 function useUrlParams(defaultKey?: string, defaultValue?: string) {
   const getParamValue = useCallback(
     (key: string) => {
       const location = browserHistory.getCurrentLocation();
-      return location.query[key] || defaultValue;
+      return location.query[key] ?? defaultValue;
     },
     [defaultValue]
   );
