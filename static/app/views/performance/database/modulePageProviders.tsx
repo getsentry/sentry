e549f12a9a5a035b94528cb3384e1d622a494bf1
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-08-17 11:29:26 -0400

    feat(perf): Database view (#54848)
    Adds a new Database view to Performance. Right now the view has no entry
    points, and is hidden behind a feature flag. The view is powered by span
    metrics that were created by Starfish, and all the view _contents_ are
    in fact using Starfish components.
    
    Using the same underlying components creates more-or-less a copy of the
    corresponding Starfish, but this is important for us to _insulate_ this
    LA release from active development. The alternative would be to make the
    Starfish versions hyper-configurable (new routing contexts, passing in
    the filter controls, more toggles based on module type, etc.) which
    would have created a mess of UI conditionals. Plus, the Starfish version
    of the Database module will be removed soon in favour of this one,
    anyway.
    
    This is LA preparation, so code cleanup, test coverage, and so on are
    coming up next.

---
+++ static/app/views/performance/database/modulePageProviders.tsx
@@ -0,0 +1,34 @@
+import Feature from 'sentry/components/acl/feature';
+import * as Layout from 'sentry/components/layouts/thirds';
+import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
+import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
+import useOrganization from 'sentry/utils/useOrganization';
+import {NoAccess} from 'sentry/views/performance/database/noAccess';
+import {RoutingContextProvider} from 'sentry/views/starfish/utils/routingContext';
+
+interface Props {
+  children: React.ReactNode;
+  title: string;
+}
+
+export function ModulePageProviders({title, children}: Props) {
+  const organization = useOrganization();
+
+  return (
+    <RoutingContextProvider value={{baseURL: '/performance/database'}}>
+      <PageFiltersContainer>
+        <SentryDocumentTitle title={title} orgSlug={organization.slug}>
+          <Layout.Page>
+            <Feature
+              features={['performance-database-view']}
+              organization={organization}
+              renderDisabled={NoAccess}
+            >
+              {children}
+            </Feature>
+          </Layout.Page>
+        </SentryDocumentTitle>
+      </PageFiltersContainer>
+    </RoutingContextProvider>
+  );
+}
