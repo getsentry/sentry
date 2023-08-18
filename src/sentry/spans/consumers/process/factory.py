876379768e39046890ef856a0e897523992970ce
Pierre Massat <pierre.massat@sentry.io>
2023-08-18 09:26:33 -0400

    Set a default empty value for group_raw instead

--- src/sentry/spans/consumers/process/factory.py
+++ src/sentry/spans/consumers/process/factory.py
@@ -50,6 +50,7 @@ def _build_snuba_span(relay_span: Mapping[str, Any]) -> MutableMapping[str, Any]
     snuba_span: MutableMapping[str, Any] = {}
     snuba_span["description"] = relay_span.get("description")
     snuba_span["exclusive_time_ms"] = int(relay_span.get("exclusive_time", 0))
+    snuba_span["group_raw"] = "0"
     snuba_span["is_segment"] = relay_span.get("is_segment", False)
     snuba_span["organization_id"] = organization.id
     snuba_span["parent_span_id"] = relay_span.get("parent_span_id", "0")
@@ -67,16 +68,6 @@ def _build_snuba_span(relay_span: Mapping[str, Any]) -> MutableMapping[str, Any]
     snuba_span["duration_ms"] = max(
         int((end_timestamp - start_timestamp).total_seconds() * 1e3),
         0,
-    )
-
-    trace_context = relay_span.get("data", {}).get("trace")
-    snuba_span["group_raw"] = (
-        trace_context.get(
-            "hash",
-            "0",
-        )
-        if trace_context
-        else "0"
     )
 
     sentry_tags: MutableMapping[str, Any] = {}
