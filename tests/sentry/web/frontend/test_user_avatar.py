a1058325db597c0f87efae12cea86fea0afbc68c
Mark Story <mark@mark-story.com>
2023-08-18 15:02:59 -0400

    feat(hybridcloud) Add silo limiters for views (#54946)
    Add silo limiting decorators for frontend views. Having these will
    resolve a few 500s in the test environment and create 404s instead.
    
    We'll still need to change the host we request avatars from in order to
    get them fully working.

--- tests/sentry/web/frontend/test_user_avatar.py
+++ tests/sentry/web/frontend/test_user_avatar.py
@@ -10,9 +10,13 @@ from sentry.testutils.silo import assume_test_silo_mode, control_silo_test
 from sentry.web.frontend.generic import FOREVER_CACHE
 
 
-@control_silo_test
+@control_silo_test(stable=True)
 class UserAvatarTest(TestCase):
     def test_headers(self):
+        # We cannot read File from Control silo
+        if SiloMode.get_current_mode() == SiloMode.CONTROL:
+            return
+
         user = self.create_user(email="a@example.com")
         with assume_test_silo_mode(SiloMode.REGION):
             photo = File.objects.create(name="test.png", type="avatar.file")
