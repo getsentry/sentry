60a2aef3c55fe93545301ef5406f8f3d282d1f99
Mark Story <mark@mark-story.com>
2023-08-18 13:40:28 -0400

    Use RegionScheduledDeletion in monolith mode.
    The current plan is to bring up control silo workers first so having
    monolith mode code us RegionScheduledDeletion will allow for a more
    graceful transition to controlsilo + monolith.

--- src/sentry/models/scheduledeletion.py
+++ src/sentry/models/scheduledeletion.py
@@ -160,6 +160,6 @@ class RegionScheduledDeletion(BaseScheduledDeletion):
 
 
 def get_regional_scheduled_deletion(mode: SiloMode) -> Type[BaseScheduledDeletion]:
-    if mode == SiloMode.REGION:
+    if mode != SiloMode.CONTROL:
         return RegionScheduledDeletion
     return ScheduledDeletion
