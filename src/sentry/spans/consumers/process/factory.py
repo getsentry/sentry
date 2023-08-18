935706a55b3780a4c2693f7eea02519e7e8c69ae
Pierre Massat <pierre.massat@sentry.io>
2023-08-18 10:02:52 -0400

    fix(spans): Read is_segment field from the Relay span and set default value for group_raw (#55024)

--- src/sentry/spans/consumers/process/factory.py
+++ src/sentry/spans/consumers/process/factory.py
@@ -50,7 +50,8 @@ def _build_snuba_span(relay_span: Mapping[str, Any]) -> MutableMapping[str, Any]
     snuba_span: MutableMapping[str, Any] = {}
     snuba_span["description"] = relay_span.get("description")
     snuba_span["exclusive_time_ms"] = int(relay_span.get("exclusive_time", 0))
-    snuba_span["is_segment"] = not relay_span.get("parent_span_id")
+    snuba_span["group_raw"] = "0"
+    snuba_span["is_segment"] = relay_span.get("is_segment", False)
     snuba_span["organization_id"] = organization.id
     snuba_span["parent_span_id"] = relay_span.get("parent_span_id", "0")
     snuba_span["project_id"] = relay_span["project_id"]
@@ -68,10 +69,6 @@ def _build_snuba_span(relay_span: Mapping[str, Any]) -> MutableMapping[str, Any]
         int((end_timestamp - start_timestamp).total_seconds() * 1e3),
         0,
     )
-
-    trace_context = relay_span.get("data", {}).get("trace")
-    if trace_context:
-        snuba_span["group_raw"] = trace_context.get("hash")
 
     sentry_tags: MutableMapping[str, Any] = {}
     if tags := relay_span.get("data"):
