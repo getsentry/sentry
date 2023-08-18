f3ff30e988fe54a5b31c727b1504b8f9aed6e007
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-08-18 14:35:33 -0700

    ref: convert metricRule.js to typescript

--- fixtures/js-stubs/metricRule.tsx
+++ fixtures/js-stubs/metricRule.tsx
@@ -1,21 +1,25 @@
+import {Dataset, SavedMetricRule} from 'sentry/views/alerts/rules/metric/types';
+
 import {IncidentTrigger} from './incidentTrigger';
 
-export function MetricRule(params = {}) {
+export function MetricRule(params: Partial<SavedMetricRule> = {}): SavedMetricRule {
   return {
     status: 0,
     dateCreated: '2019-07-31T23:02:02.731Z',
-    dataset: 'events',
+    dataset: Dataset.ERRORS,
     query: '',
     id: '4',
     name: 'My Incident Rule',
     timeWindow: 60,
-    aggregation: 0,
     aggregate: 'count()',
     projects: ['project-slug'],
     dateModified: '2019-07-31T23:02:02.731Z',
     triggers: [IncidentTrigger()],
     resolveThreshold: 36,
+    snooze: false,
     thresholdType: 0,
+    environment: '',
+    thresholdPeriod: 3,
     ...params,
   };
 }
