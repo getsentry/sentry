7fb9e10c340afd7639ead36c0682dbef3e0170be
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-08-18 16:52:29 -0700

    ref: convert searches.js to typescript (#55106)
    ૮₍´｡ᵔ ꈊ ᵔ｡`₎ა

--- fixtures/js-stubs/searches.tsx
+++ fixtures/js-stubs/searches.tsx
@@ -1,6 +1,8 @@
+import {SavedSearch} from 'sentry/types';
+
 import {Search} from './search';
 
-export function Searches(params = []) {
+export function Searches(params: SavedSearch[] = []): SavedSearch[] {
   return [
     Search({
       name: 'Needs Triage',
