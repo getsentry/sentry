0b007c20edbaa401c195eea1ecb6ec5786aaf6c2
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-08-18 16:06:05 -0700

    ref: convert jiraIntegrationProvider.js to typescript (#55090)
    ```
        /\_/\   
       ( ˶•o•˶)  
        ଘ(ა🍜)  
    ```

--- fixtures/js-stubs/jiraIntegrationProvider.tsx
+++ fixtures/js-stubs/jiraIntegrationProvider.tsx
@@ -1,13 +1,25 @@
-export function JiraIntegrationProvider(params = {}) {
+import {IntegrationProvider} from 'sentry/types';
+
+export function JiraIntegrationProvider(
+  params: Partial<IntegrationProvider> = {}
+): IntegrationProvider {
   return {
     key: 'jira',
     name: 'Jira',
     canAdd: false,
-    config: [],
     features: [],
+    slug: 'jira',
+    canDisable: true,
+    setupDialog: {
+      url: '/jira-integration-setup-uri/',
+      width: 100,
+      height: 100,
+    },
     metadata: {
       description: '*markdown* formatted Jira _description_',
-      features: [{description: '*markdown* feature description'}],
+      features: [
+        {description: '*markdown* feature description', featureGate: '', featureId: 3},
+      ],
       author: 'Rick',
       noun: 'Instance',
       issue_url: 'http://example.com/jira_integration_issue_url',
