b38aa918c9ada1742aa14532af5834c62b97c576
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-08-18 14:30:52 -0700

    ref: convert traceError.js to typescript (#55083)
    /)   /)
    ( ᵔ ᵕ ᵔ )
    / づ  づ ~ ♡

--- fixtures/js-stubs/traceError.tsx
+++ fixtures/js-stubs/traceError.tsx
@@ -1,4 +1,6 @@
-export function TraceError(params = {}) {
+import {TraceError as TraceErrorType} from 'sentry/utils/performance/quickTrace/types';
+
+export function TraceError(params: Partial<TraceErrorType> = {}): TraceErrorType {
   return {
     event_id: '08384ee83c9145e79b5f6fbed5c37a51',
     issue_id: 62,
@@ -7,6 +9,7 @@ export function TraceError(params = {}) {
     project_slug: 'santry',
     title: 'Error: Something went wrong',
     level: 'error',
+    issue: '',
     ...params,
   };
 }
