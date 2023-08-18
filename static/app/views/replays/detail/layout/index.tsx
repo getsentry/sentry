getsentry/sentry

commit d47daf2647ec2ac9648dfb7fc159ae0dff6e19d9

ref: Replace MeasureSize with useDimensions (#54892)

static/app/views/replays/detail/layout/index.tsx
@@ -6,12 +6,12 @@ import ReplayTimeline from 'sentry/components/replays/breadcrumbs/replayTimeline
 import ReplayView from 'sentry/components/replays/replayView';
 import {space} from 'sentry/styles/space';
 import {LayoutKey} from 'sentry/utils/replays/hooks/useReplayLayout';
+import {useDimensions} from 'sentry/utils/useDimensions';
 import useFullscreen from 'sentry/utils/window/useFullscreen';
 import FluidHeight from 'sentry/views/replays/detail/layout/fluidHeight';
 import FluidPanel from 'sentry/views/replays/detail/layout/fluidPanel';
 import FocusArea from 'sentry/views/replays/detail/layout/focusArea';
 import FocusTabs from 'sentry/views/replays/detail/layout/focusTabs';
-import MeasureSize from 'sentry/views/replays/detail/layout/measureSize';
 import SidebarArea from 'sentry/views/replays/detail/layout/sidebarArea';
 import SideTabs from 'sentry/views/replays/detail/layout/sideTabs';
 import SplitPanel from 'sentry/views/replays/detail/layout/splitPanel';
@@ -34,6 +34,9 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
   const {toggle: toggleFullscreen} = useFullscreen({
     elementRef: fullscreenRef,
   });
+
+  const measureRef = useRef<HTMLDivElement>(null);
+  const {width, height} = useDimensions({elementRef: measureRef});
 
   const timeline = (
     <ErrorBoundary mini>
@@ -74,12 +77,14 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
     </ErrorBoundary>
   );
 
+  const hasSize = width + height;
+
   if (layout === LayoutKey.NO_VIDEO) {
     return (
       <BodyContent>
         {timeline}
-        <MeasureSize>
-          {({width}) => (
+        <div ref={measureRef}>
+          {hasSize ? (
             <SplitPanel
               key={layout}
               availableSize={width}
@@ -91,8 +96,8 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
               }}
               right={sidebarArea}
             />
-          )}
-        </MeasureSize>
+          ) : null}
+        </div>
       </BodyContent>
     );
   }
@@ -101,8 +106,8 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
     return (
       <BodyContent>
         {timeline}
-        <MeasureSize>
-          {({height, width}) => (
+        <div ref={measureRef}>
+          {hasSize ? (
             <SplitPanel
               key={layout}
               availableSize={width}
@@ -126,8 +131,8 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
               }}
               right={focusArea}
             />
-          )}
-        </MeasureSize>
+          ) : null}
+        </div>
       </BodyContent>
     );
   }
@@ -136,8 +141,8 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
   return (
     <BodyContent>
       {timeline}
-      <MeasureSize>
-        {({height, width}) => (
+      <div ref={measureRef}>
+        {hasSize ? (
           <SplitPanel
             key={layout}
             availableSize={height}
@@ -160,8 +165,8 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
             }}
             bottom={focusArea}
           />
-        )}
-      </MeasureSize>
+        ) : null}
+      </div>
     </BodyContent>
   );
 }
@@ -172,6 +177,7 @@ const BodyContent = styled('main')`
   height: 100%;
   display: grid;
   grid-template-rows: auto 1fr;
+  gap: ${space(2)};
   overflow: hidden;
   padding: ${space(2)};
 `;

static/app/components/replays/breadcrumbs/replayTimeline.tsx
@@ -33,7 +33,7 @@ function ReplayTimeline({}: Props) {
   const {width} = useDimensions<HTMLDivElement>({elementRef: stackedRef});
 
   if (!replay) {
-    return <Placeholder height="54px" bottomGutter={2} />;
+    return <Placeholder height="54px" />;
   }
 
   const durationMs = replay.getDurationMs();
@@ -42,19 +42,19 @@ function ReplayTimeline({}: Props) {
   const networkFrames = replay.getNetworkFrames();
 
   return (
-    <Panel ref={panelRef} {...mouseTrackingProps}>
+    <PanelNoMargin ref={panelRef} {...mouseTrackingProps}>
       <Stacked ref={stackedRef}>
         <MinorGridlines durationMs={durationMs} width={width} />
         <MajorGridlines durationMs={durationMs} width={width} />
         <TimelineScrubber />
-        <UnderTimestamp paddingTop="36px">
+        <div style={{paddingTop: '36px'}}>
           <ReplayTimelineSpans
             durationMs={durationMs}
             frames={networkFrames}
             startTimestampMs={startTimestampMs}
           />
-        </UnderTimestamp>
-        <UnderTimestamp paddingTop="26px">
+        </div>
+        <div style={{paddingTop: '26px'}}>
           <ReplayTimelineEvents
             durationMs={durationMs}
             frames={chapterFrames}
@@ -67,15 +67,14 @@ function ReplayTimeline({}: Props) {
             startTimestampMs={startTimestampMs}
             width={width}
           />
-        </UnderTimestamp>
+        </div>
       </Stacked>
-    </Panel>
+    </PanelNoMargin>
   );
 }
 
-const UnderTimestamp = styled('div')<{paddingTop: string}>`
-  /* Weird size to put equal space above/below a <small> node that MajorGridlines emits */
-  padding-top: ${p => p.paddingTop};
+const PanelNoMargin = styled(Panel)`
+  margin: 0;
 `;
 
 export default ReplayTimeline;

static/app/utils/useDimensions.tsx
@@ -1,8 +1,6 @@
 import {RefObject, useCallback, useState} from 'react';
 import {useResizeObserver} from '@react-aria/utils';
 
-export type Dimensions = {height: number; width: number};
-
 interface Props<Element extends HTMLElement> {
   elementRef: RefObject<Element>;
 }
@@ -13,14 +11,16 @@ interface Props<Element extends HTMLElement> {
 export function useDimensions<Element extends HTMLElement>({elementRef}: Props<Element>) {
   const [dimensions, setDimensions] = useState({height: 0, width: 0});
 
+  const element = elementRef.current;
+
   const onResize = useCallback(() => {
     setDimensions({
-      height: elementRef.current?.clientHeight || 0,
-      width: elementRef.current?.clientWidth || 0,
+      height: element?.clientHeight || 0,
+      width: element?.clientWidth || 0,
     });
-  }, [elementRef, setDimensions]);
+  }, [element]);
 
   useResizeObserver({ref: elementRef, onResize});
 
-  return {elementRef, ...dimensions};
+  return dimensions;
 }

static/app/views/replays/detail/layout/measureSize.tsx
@@ -1,33 +1,0 @@
-import {ReactNode, useEffect, useRef, useState} from 'react';
-import styled from '@emotion/styled';
-
-type Sizes = {height: number; width: number};
-type ChildFn = (props: Sizes) => ReactNode;
-
-/**
- * Similar to <AutoSizer> from 'react-virtualized` but works better with flex & grid parents
- */
-const MeasureSize = styled(
-  ({children, className}: {children: ChildFn; className?: string}) => {
-    const outerRef = useRef<HTMLDivElement>(null);
-    const [sizes, setSizes] = useState<Sizes>();
-    useEffect(() => {
-      if (outerRef.current) {
-        const {height, width} = outerRef.current.getBoundingClientRect();
-        setSizes({height, width});
-      }
-    }, []);
-
-    return (
-      <div ref={outerRef} className={className}>
-        {sizes ? children(sizes) : null}
-      </div>
-    );
-  }
-)`
-  height: 100%;
-  width: 100%;
-  overflow: hidden;
-`;
-
-export default MeasureSize;

commit 158a21a5d768022551fb46b2710f53923973f902

fix(replay): Fix scrolling inside the Replay Details Trace tab (#55056)
This became broken after https://github.com/getsentry/sentry/pull/54892

Fixes https://github.com/getsentry/team-replay/issues/149

static/app/views/replays/detail/layout/index.tsx
@@ -83,7 +83,7 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
     return (
       <BodyContent>
         {timeline}
-        <div ref={measureRef}>
+        <FluidHeight ref={measureRef}>
           {hasSize ? (
             <SplitPanel
               key={layout}
@@ -97,7 +97,7 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
               right={sidebarArea}
             />
           ) : null}
-        </div>
+        </FluidHeight>
       </BodyContent>
     );
   }
@@ -106,7 +106,7 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
     return (
       <BodyContent>
         {timeline}
-        <div ref={measureRef}>
+        <FluidHeight ref={measureRef}>
           {hasSize ? (
             <SplitPanel
               key={layout}
@@ -132,7 +132,7 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
               right={focusArea}
             />
           ) : null}
-        </div>
+        </FluidHeight>
       </BodyContent>
     );
   }
@@ -141,7 +141,7 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
   return (
     <BodyContent>
       {timeline}
-      <div ref={measureRef}>
+      <FluidHeight ref={measureRef}>
         {hasSize ? (
           <SplitPanel
             key={layout}
@@ -166,7 +166,7 @@ function ReplayLayout({layout = LayoutKey.TOPBAR}: Props) {
             bottom={focusArea}
           />
         ) : null}
-      </div>
+      </FluidHeight>
     </BodyContent>
   );
 }