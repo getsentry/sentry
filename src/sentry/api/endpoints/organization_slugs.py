ffaf085295b7be215d11adf2607d26809dee3014
schew2381 <seiji.chew@sentry.io>
2023-08-18 18:30:01 -0700

    finish mass project slug updates

--- src/sentry/api/endpoints/organization_slugs.py
+++ src/sentry/api/endpoints/organization_slugs.py
@@ -1,12 +1,21 @@
 from django.core.exceptions import ValidationError
-from django.core.validators import validate_slug
-from django.db import router, transaction
+from django.core.validators import RegexValidator
+from django.db import IntegrityError, router, transaction
+from django.utils.regex_helper import _lazy_re_compile
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry.api.base import region_silo_endpoint
+from sentry.api.base import DEFAULT_SLUG_ERROR_MESSAGE, DEFAULT_SLUG_PATTERN, region_silo_endpoint
 from sentry.api.bases.organization import OrganizationEndpoint
+from sentry.api.exceptions import ConflictError
 from sentry.models import Project
+from sentry.utils.snowflake import MaxSnowflakeRetryError
+
+_validate_sentry_slug = RegexValidator(
+    _lazy_re_compile(DEFAULT_SLUG_PATTERN),
+    DEFAULT_SLUG_ERROR_MESSAGE,
+    "invalid",
+)
 
 
 @region_silo_endpoint
@@ -27,9 +36,9 @@ class SlugsUpdateEndpoint(OrganizationEndpoint):
         for project_id, slug in slugs.items():
             slug = slug.lower()
             try:
-                validate_slug(slug)
+                _validate_sentry_slug(slug)
             except ValidationError:
-                return Response({"detail": 'invalid slug "%s"' % slug}, status=400)
+                return Response({"detail": 'Invalid slug "%s".' % slug}, status=400)
             slugs[project_id] = slug
 
         if len(slugs) != len(set(slugs.values())):
@@ -40,31 +49,38 @@ class SlugsUpdateEndpoint(OrganizationEndpoint):
         rv = {}
 
         with transaction.atomic(router.db_for_write(Project)):
-            projects = {}
+            try:
+                projects = {}
 
-            # Clear out all slugs first so that we can move them
-            # around through the uniqueness
-            for project in project_q:
-                projects[str(project.id)] = project
-                project.slug = None
-                project.save()
+                # Clear out all slugs first so that we can move them
+                # around through the uniqueness
+                for project in project_q:
+                    projects[str(project.id)] = project
+                    project.slug = None
+                    project.save()
 
-            # Set new ones
-            for project_id, slug in slugs.items():
-                project = projects.get(project_id)
-                if project is None:
-                    continue
-                other = (
-                    Project.objects.filter(slug=slug, organization=organization)
-                    .exclude(id=project.id)
-                    .first()
+                # Set new ones
+                for project_id, slug in slugs.items():
+                    project = projects.get(project_id)
+                    if project is None:
+                        continue
+                    other = (
+                        Project.objects.filter(slug=slug, organization=organization)
+                        .exclude(id=project.id)
+                        .first()
+                    )
+                    if other is not None:
+                        if len(slugs) != len(slugs.values()):
+                            return Response({"detail": "Duplicate slug %s." % slug}, status=400)
+                    project.slug = slug
+                    project.update_option("sentry:reviewed-slug", True)
+                    project.save()
+                    rv[project_id] = slug
+            except (IntegrityError, MaxSnowflakeRetryError):
+                raise ConflictError(
+                    {
+                        "detail": f'A project with slug "{slug}" already exists.',
+                    }
                 )
-                if other is not None:
-                    if len(slugs) != len(slugs.values()):
-                        return Response({"detail": "Duplicate slug %s" % slug}, status=400)
-                project.slug = slug
-                project.update_option("sentry:reviewed-slug", True)
-                project.save()
-                rv[project_id] = slug
 
         return Response({"updated_slugs": rv})
