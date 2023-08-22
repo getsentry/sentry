24e16c6d4a463189585a28fb960496da1515e8a1
Chloe <chloe.ho@sentry.io>
2023-08-22 09:57:32 -0700

    updates

--- src/sentry/api/endpoints/funnel_details.py
+++ src/sentry/api/endpoints/funnel_details.py
@@ -14,10 +14,12 @@ from sentry.snuba.referrer import Referrer
 from sentry.utils import json
 
 
-def get_issues_from_user(user, dataset, query, snuba_params, params):
-    return dataset.query(
-        selected_columns=["issue"],
-        query=f"user:{user} AND {query}",
+def get_issues_from_users(users, dataset, query, snuba_params, params):
+    userquery = " OR ".join([f"user:{user}" for user in users])
+    print(userquery)
+    ret =  dataset.query(
+        selected_columns=["issue", "timestamp","user"],
+        query=f"{userquery} AND ({query})",
         params=params,
         snuba_params=snuba_params,
         equations=[],
@@ -35,7 +37,13 @@ def get_issues_from_user(user, dataset, query, snuba_params, params):
         on_demand_metrics_enabled=False,
         referrer=Referrer.API_ORGANIZATION_EVENTS_V2.value,
     )["data"]
+    print(ret)
+    retdict = defaultdict(list)
+    for issue in ret:
+        retdict[issue["issue.id"]] += [issue["user"]]
 
+
+    return retdict
 
 class FunnelDetailsEndpoint(OrganizationEventsV2EndpointBase):
     def get(self, request, organization, funnel_slug):
@@ -101,23 +109,37 @@ class FunnelDetailsEndpoint(OrganizationEventsV2EndpointBase):
 
         total_starts = 0
         total_completions = 0
-        issues = defaultdict(list)
         for user, min_start_time in min_start_time_per_user.items():
             total_starts += 1
-            userissues = get_issues_from_user(user, dataset, query, snuba_params, params)
-            if userissues:
-                issues[str(userissues[0])] += [user]
             if user in max_end_time_per_user:
                 if max_end_time_per_user[user] > min_start_time:
                     total_completions += 1
 
-        print(issues)
+        allusers = [*min_start_time_per_user.keys(), *max_end_time_per_user.keys()]
+        userissues = get_issues_from_users(allusers, dataset, query, snuba_params, params)
+        print(userissues)
+        retissues = {}
+        for issue, users in userissues.items():
+            starts = 0
+            completes = 0
+            for user in users:
+                if user in min_start_time_per_user.keys():
+                    starts += 1
+                    if user in max_end_time_per_user.keys():
+                        completes += 1
+
+            print(issue, starts, completes)
+            retissues[issue] = {"starts": starts, "completes": completes}
+
+
+        print(retissues)
+
         return self.respond(
             {
                 "totalStarts": total_starts,
                 "totalCompletions": total_completions,
-                "funnel": serialize(funnel, request.user, serializer=FunnelSerializer()),
-                "issues": issues.keys(),
+                "funnel": serialize(funnel, request.user, serializer=FunnelSerializer())
+              #  "issues": userissues.,
             },
             status=200,
         )
