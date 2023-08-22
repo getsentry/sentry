54d86f545416189cf9cbad61b85d4e5232137754
schew2381 <seiji.chew@sentry.io>
2023-08-22 11:00:25 -0700

    add mixin

--- tests/sentry/api/endpoints/test_organization_teams.py
+++ tests/sentry/api/endpoints/test_organization_teams.py
@@ -262,4 +262,11 @@ class OrganizationTeamsCreateTest(APITestCase):
     def test_generated_slug_not_entirely_numeric(self):
         response = self.get_success_response(self.organization.slug, name="1234", status_code=201)
         team = Team.objects.get(id=response.data["id"])
-        assert team.slug.startswith("1234" + "-")
+        assert team.slug == "1234"
+
+        with self.feature("app:enterprise-prevent-numeric-slugs"):
+            response = self.get_success_response(
+                self.organization.slug, name="1234", status_code=201
+            )
+            team = Team.objects.get(id=response.data["id"])
+            assert team.slug.startswith("1234" + "-")
