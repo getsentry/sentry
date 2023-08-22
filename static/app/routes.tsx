321733688841e7fec114dfe56b56bcc3508429f3
Alexander Tarasov <alex.tarasov@sentry.io>
2023-08-22 18:27:37 +0200

    separate page for NEL headers setup

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -604,6 +604,13 @@ function buildRoutes() {
           () => import('sentry/views/settings/project/projectUserFeedback')
         )}
       />
+      <Route
+        path="nel/"
+        name={t('Network Error Logging')}
+        component={make(
+          () => import('sentry/views/settings/project/networkErrorLogging')
+        )}
+      />
       <Redirect from="csp/" to="security-headers/" />
       <Route path="security-headers/" name={t('Security Headers')}>
         <IndexRoute
