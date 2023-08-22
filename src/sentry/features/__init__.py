af71be40fdba20ab0bf10da04a612bf001474419
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-08-22 10:55:58 -0700

    feat(replay): Configure a feature flag for session-replay-trial-ended-banner (#55153)

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -189,6 +189,7 @@ default_manager.add("organizations:session-replay-slack-new-issue", Organization
 default_manager.add("organizations:session-replay-weekly-email", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay-issue-emails", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay-trace-table", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
+default_manager.add("organizations:session-replay-trial-ended-banner", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:session-replay-optimized-search", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:set-grouping-config", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:slack-overage-notifications", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
