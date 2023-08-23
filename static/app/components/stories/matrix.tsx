dde8c31ad92275001edb408e214cc77329ed7d1f
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-08-23 14:54:42 -0700

    working on Matrix

--- static/app/components/stories/matrix.tsx
+++ static/app/components/stories/matrix.tsx
@@ -1,12 +1,14 @@
-import type {ElementType} from 'react';
+import {type ElementType, useState} from 'react';
 import styled from '@emotion/styled';
 
+import {CompactSelect} from 'sentry/components/compactSelect';
+import {InputGroup} from 'sentry/components/inputGroup';
 import SizingWindow from 'sentry/components/stories/sizingWindow';
 import {space} from 'sentry/styles/space';
 
 interface Props {
   component: ElementType;
-  propMatrix: Record<string, any[]>;
+  propMatrix: Record<string, unknown[]>;
 }
 
 export default function Matrix({component, propMatrix}: Props) {
@@ -47,12 +49,17 @@ export default function Matrix({component, propMatrix}: Props) {
       ));
 
   return (
-    <div>
-      <ul>
-        {Object.keys(propMatrix).map(key => (
-          <li key={key}>{key}</li>
-        ))}
-      </ul>
+    <form onSubmit={() => {}}>
+      <InputGroup>
+        <ul>
+          {Object.keys(propMatrix).map(key => (
+            <li key={key}>
+              {key}
+              <InputForType prop={key} values={propMatrix[key]} />
+            </li>
+          ))}
+        </ul>
+      </InputGroup>
 
       <Grid
         style={{
@@ -61,8 +68,49 @@ export default function Matrix({component, propMatrix}: Props) {
       >
         {items}
       </Grid>
-    </div>
+    </form>
   );
+}
+
+function InputForType(key: string, values: unknown[]) {
+  const isAllString = areAllString(values);
+  const isAllNumber = areAllNumber(values);
+  const isAllBoolean = areAllBoolean(values);
+
+  const [value, setValue] = useState<string>('');
+
+  if (isAllString || isAllNumber || isAllBoolean) {
+    return (
+      <CompactSelect
+        triggerLabel={key}
+        onChange={selected => setValue(selected.value)}
+        options={values.map(v => ({
+          label: toLabel(v),
+          value: toLabel(v),
+        }))}
+        size="sm"
+        value={value}
+      />
+    );
+  }
+  return <InputGroup.Input />;
+}
+
+function areAllString(values: unknown[]): values is string[] {
+  return values.every(val => typeof val === 'string');
+}
+function areAllNumber(values: unknown[]): values is number[] {
+  return values.every(val => typeof val === 'number');
+}
+function areAllBoolean(values: unknown[]): values is boolean[] {
+  return values.every(val => typeof val === 'boolean');
+}
+
+function toLabel(value: string | boolean | number) {
+  if (typeof value === 'boolean') {
+    return value ? 'true' : 'false';
+  }
+  return String(value);
 }
 
 const Grid = styled('div')`
