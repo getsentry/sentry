43fff3848ce006a8fd0231967a448af34cbced2e
Colton Allen <colton.allen@sentry.io>
2023-08-23 10:02:50 -0500

    Apply relocation scope

--- src/sentry/models/file_part.py
+++ src/sentry/models/file_part.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.utils import timezone
 
+from sentry.backup.scopes import RelocationScope
 from sentry.db.models import Model, region_silo_only_model
 
 
 @region_silo_only_model
 class FilePartModel(Model):
     __include_in_export__ = False
+    __relocation_scope__ = RelocationScope.Excluded
 
     created_at = models.DateTimeField(default=timezone.now, db_index=True)
     dek = models.CharField(max_length=64, null=True)
