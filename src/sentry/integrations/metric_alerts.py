35df2f49b7662162678d53762885638aa555cca3
Spencer Murray <spencer.murray@sentry.io>
2023-08-24 13:20:32 -0700

    Fix type

--- src/sentry/integrations/metric_alerts.py
+++ src/sentry/integrations/metric_alerts.py
@@ -136,7 +136,7 @@ def metric_alert_attachment_info(
     alert_rule: AlertRule,
     selected_incident: Optional[Incident] = None,
     new_status: Optional[IncidentStatus] = None,
-    metric_value: Optional[str] = None,
+    metric_value: Optional[int] = None,
 ):
     latest_incident = None
     if selected_incident is None:
