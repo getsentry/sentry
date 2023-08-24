73d5c49b5155b1a618b57da097b25f6329c55424
Edward Gou <edward.gou@sentry.io>
2023-08-24 13:21:27 -0400

    scrollbar hack

--- static/app/components/performance/waterfall/miniHeader.tsx
+++ static/app/components/performance/waterfall/miniHeader.tsx
@@ -10,7 +10,7 @@ const MINI_HEADER_HEIGHT = 20;
 export const ScrollbarContainer = styled('div')`
   display: block;
   width: 100%;
-  height: ${MINI_HEADER_HEIGHT + 50}px;
+  height: 2000px;
   & > div[data-type='virtual-scrollbar'].dragging > div {
     background-color: ${p => p.theme.textColor};
     opacity: 0.8;
