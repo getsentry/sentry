cbce5ca7804dad67e4d830924d533a16f2f550ad
Pierre Massat <pierre.massat@sentry.io>
2023-08-27 21:09:27 +0200

    chore(issue-platform): Release frame drop issue

--- src/sentry/issues/grouptype.py
+++ src/sentry/issues/grouptype.py
@@ -380,6 +380,15 @@ class ProfileFrameDropExperimentalType(GroupType):
 
 
 @dataclass(frozen=True)
+class ProfileFrameDropType(GroupType):
+    type_id = 2009
+    slug = "profile_frame_drop"
+    description = "A Frame Drop Occurred"
+    category = GroupCategory.PERFORMANCE.value
+    released = True
+
+
+@dataclass(frozen=True)
 class MonitorCheckInFailure(GroupType):
     type_id = 4001
     slug = "monitor_check_in_failure"
