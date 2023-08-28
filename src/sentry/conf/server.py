f2d87e6b001ad9f1ee5ba1ba55d1e450f83029fc
Tony Xiao <txiao@sentry.io>
2023-08-28 17:14:41 -0400

    chore(statistical-detectors): Add setting for redis cluster (#55282)
    This setting points towards the redis cluster to use for statistical
    detectors.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -118,6 +118,7 @@ SENTRY_ARTIFACT_BUNDLES_INDEXING_REDIS_CLUSTER = "default"
 SENTRY_INTEGRATION_ERROR_LOG_REDIS_CLUSTER = "default"
 SENTRY_DEBUG_FILES_REDIS_CLUSTER = "default"
 SENTRY_MONITORS_REDIS_CLUSTER = "default"
+SENTRY_STATISTICAL_DETECTORS_REDIS_CLUSTER = "default"
 
 # Hosts that are allowed to use system token authentication.
 # http://en.wikipedia.org/wiki/Reserved_IP_addresses
