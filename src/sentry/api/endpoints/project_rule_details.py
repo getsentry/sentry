59012dc632a458bddcdcfd98b5856b7342c56f32
Colleen O'Rourke <colleen@sentry.io>
2023-08-28 12:58:24 -0700

    ref(alerts): Prevent duplicate and no action alerts from being created (#54515)
    Prevent alerts with no actions and alerts that are exact duplicates of
    another alert in the same project from being created, or editing an
    alert such that it ends up being the exact same as another alert in the
    same project.
    
    <img width="815" alt="Screenshot 2023-08-09 at 2 45 03 PM"
    src="https://github.com/getsentry/sentry/assets/29959063/1e705552-57f8-48ce-aa7b-58f557b53bee">
    
    
    <img width="855" alt="Screenshot 2023-08-09 at 5 33 01 PM"
    src="https://github.com/getsentry/sentry/assets/29959063/f5c7472f-ae5e-4b7b-94a3-a4a2c81c5500">
    
    Closes #55113
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- src/sentry/api/endpoints/project_rule_details.py
+++ src/sentry/api/endpoints/project_rule_details.py
@@ -7,6 +7,7 @@ from rest_framework.response import Response
 from sentry import audit_log
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases.rule import RuleEndpoint
+from sentry.api.endpoints.project_rules import find_duplicate_rule
 from sentry.api.serializers import serialize
 from sentry.api.serializers.models.rule import RuleSerializer
 from sentry.api.serializers.rest_framework.rule import RuleSerializer as DrfRuleSerializer
@@ -116,6 +117,16 @@ class ProjectRuleDetailsEndpoint(RuleEndpoint):
         if serializer.is_valid():
             data = serializer.validated_data
 
+            if not data.get("actions", []):
+                return Response(
+                    {
+                        "actions": [
+                            "You must add an action for this alert to fire.",
+                        ]
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
             # combine filters and conditions into one conditions criteria for the rule object
             conditions = data.get("conditions", [])
             if "filters" in data:
@@ -131,6 +142,17 @@ class ProjectRuleDetailsEndpoint(RuleEndpoint):
                 "actions": data["actions"],
                 "frequency": data.get("frequency"),
             }
+            duplicate_rule = find_duplicate_rule(kwargs, project)
+            if duplicate_rule:
+                return Response(
+                    {
+                        "name": [
+                            f"This rule is an exact duplicate of '{duplicate_rule.label}' in this project and may not be created.",
+                        ]
+                    },
+                    status=status.HTTP_400_BAD_REQUEST,
+                )
+
             owner = data.get("owner")
             if owner:
                 try:
