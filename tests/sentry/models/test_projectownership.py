b0f1ff954d3aa96536330619017a4d68e10b3b97
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2023-08-29 10:14:12 -0700

    ref: Allow auto-assignment after manual assignment if called from API (#53476)
    Allow auto-assignment after manual assignment if called from force
    auto-assignment API in _admin

--- tests/sentry/models/test_projectownership.py
+++ tests/sentry/models/test_projectownership.py
@@ -561,6 +561,53 @@ class ProjectOwnershipTestCase(TestCase):
         )
         assert ProjectOwnership.get_owners(self.project.id, {}) == (ProjectOwnership.Everyone, None)
 
+    def test_force_handle_auto_assignment(self):
+        # Run auto-assignment first
+        self.code_mapping = self.create_code_mapping(project=self.project)
+
+        rule_a = Rule(Matcher("path", "*.py"), [Owner("team", self.team.slug)])
+
+        self.create_codeowners(
+            self.project, self.code_mapping, raw="*.py @tiger-team", schema=dump_schema([rule_a])
+        )
+
+        self.event = self.store_event(
+            data=self.python_event_data(),
+            project_id=self.project.id,
+        )
+        assert self.event.group is not None
+
+        GroupOwner.objects.create(
+            group=self.event.group,
+            type=GroupOwnerType.CODEOWNERS.value,
+            user_id=None,
+            team_id=self.team.id,
+            project=self.project,
+            organization=self.project.organization,
+            context={"rule": str(rule_a)},
+        )
+
+        ProjectOwnership.handle_auto_assignment(self.project.id, self.event)
+        assert len(GroupAssignee.objects.all()) == 1
+        assignee = GroupAssignee.objects.get(group=self.event.group)
+        assert assignee.team_id == self.team.id
+
+        # Unassign the auto-assigned user
+        GroupAssignee.objects.deassign(self.event.group)
+        assert len(GroupAssignee.objects.all()) == 0
+
+        # Manually assign the group to someone else
+        GroupAssignee.objects.assign(self.event.group, self.user)
+        assert len(GroupAssignee.objects.all()) == 1
+        assignee = GroupAssignee.objects.get(group=self.event.group)
+        assert assignee.user_id == self.user.id
+
+        # Run force auto-assignment
+        ProjectOwnership.handle_auto_assignment(self.project.id, group=self.event.group)
+        assert len(GroupAssignee.objects.all()) == 1
+        assignee = GroupAssignee.objects.get(group=self.event.group)
+        assert assignee.team_id == self.team.id
+
 
 class ResolveActorsTestCase(TestCase):
     def test_no_actors(self):
