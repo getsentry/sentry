9e966f8d497aaccc51b98970dc82538b0f64c499
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-08-29 08:24:08 -0700

    fix(replays): show explanation tooltip if replay URL contains Filtered (#55303)

--- static/app/components/replays/replayCurrentUrl.tsx
+++ static/app/components/replays/replayCurrentUrl.tsx
@@ -3,6 +3,8 @@ import * as Sentry from '@sentry/react';
 
 import {useReplayContext} from 'sentry/components/replays/replayContext';
 import TextCopyInput from 'sentry/components/textCopyInput';
+import {Tooltip} from 'sentry/components/tooltip';
+import {t} from 'sentry/locale';
 import getCurrentUrl from 'sentry/utils/replays/getCurrentUrl';
 
 function ReplayCurrentUrl() {
@@ -24,6 +26,18 @@ function ReplayCurrentUrl() {
       <TextCopyInput size="sm" disabled>
         {''}
       </TextCopyInput>
+    );
+  }
+
+  if (url.includes('[Filtered]')) {
+    return (
+      <Tooltip
+        title={t(
+          'Warning! This URL contains content scrubbed by our PII filters and may no longer be valid.'
+        )}
+      >
+        <TextCopyInput size="sm">{url}</TextCopyInput>
+      </Tooltip>
     );
   }
 
