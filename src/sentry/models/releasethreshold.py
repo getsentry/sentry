6c13c53fc8be88243e79215970987b40cb328dbf
Eric Hasegawa <eric.hasegawa@sentry.io>
2023-08-30 11:50:33 -0700

    the migrations will continue until morale improves

--- src/sentry/models/releasethreshold.py
+++ src/sentry/models/releasethreshold.py
@@ -2,7 +2,12 @@ from django.db import models
 from django.utils import timezone
 
 from sentry.backup.scopes import RelocationScope
-from sentry.db.models import FlexibleForeignKey, Model, region_silo_only_model
+from sentry.db.models import (
+    BoundedPositiveIntegerField,
+    FlexibleForeignKey,
+    Model,
+    region_silo_only_model,
+)
 
 
 class ReleaseThresholdType:
@@ -47,8 +52,8 @@ class ValueType:
 class ReleaseThreshold(Model):
     __relocation_scope__ = RelocationScope.Excluded
 
-    threshold_type = models.BoundedPositiveIntegerField(choices=ReleaseThresholdType.as_choices())
-    threshold_trigger_type = models.BoundedPositiveIntegerField(choices=ValueType.as_choices())
+    threshold_type = BoundedPositiveIntegerField(choices=ReleaseThresholdType.as_choices())
+    threshold_trigger_type = BoundedPositiveIntegerField(choices=ValueType.as_choices())
     threshold_value = models.IntegerField()
     window_in_seconds = models.IntegerField()
 
