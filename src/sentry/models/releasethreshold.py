a641c784f0859cb489d293e7aa3ccd67f5e2d1cb
Eric Hasegawa <eric.hasegawa@sentry.io>
2023-08-30 11:41:24 -0700

    address comments

--- src/sentry/models/releasethreshold.py
+++ src/sentry/models/releasethreshold.py
@@ -1,35 +1,57 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.translation import gettext_lazy as _
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import FlexibleForeignKey, Model, region_silo_only_model
+
+
+class ReleaseThresholdType:
+    TOTAL_ERROR_COUNT = 0
+    NEW_ISSUE_COUNT = 1
+    UNHANDLED_ISSUE_COUNT = 2
+    REGRESSED_ISSUE_COUNT = 3
+    FAILURE_RATE = 4
+    CRASH_FREE_SESSION_RATE = 5
+    CRASH_FREE_USER_RATE = 6
+
+    @classmethod
+    def as_choices(cls):
+        return (
+            (cls.TOTAL_ERROR_COUNT, "Total Error Count"),
+            (cls.NEW_ISSUE_COUNT, "New Issue Count"),
+            (cls.UNHANDLED_ISSUE_COUNT, "Unhandled Issue Count"),
+            (cls.REGRESSED_ISSUE_COUNT, "Regressed Issue Count"),
+            (cls.FAILURE_RATE, "Failure Rate"),
+            (cls.CRASH_FREE_SESSION_RATE, "Crash Free Session Rate"),
+            (cls.CRASH_FREE_USER_RATE, "Crash Free User Rate"),
+        )
+
+
+class ValueType:
+    PERCENT_OVER = 0
+    PERCENT_UNDER = 1
+    ABSOLUTE_OVER = 2
+    ABSOLUTE_UNDER = 3
+
+    @classmethod
+    def as_choices(cls):
+        return (
+            (cls.PERCENT_OVER, "Percent Over"),
+            (cls.PERCENT_UNDER, "Percent Under"),
+            (cls.ABSOLUTE_OVER, "Absolute Over"),
+            (cls.ABSOLUTE_UNDER, "Absolute Under"),
+        )
 
 
 @region_silo_only_model
 class ReleaseThreshold(Model):
     __relocation_scope__ = RelocationScope.Excluded
 
-    class ReleaseThresholdType(models.TextChoices):
-        TOTAL_ERROR_COUNT = "TEC", _("Total Error Count")
-        NEW_ISSUE_COUNT = "NIC", _("New Issue Count")
-        UNHANDLED_ISSUE_COUNT = "UIC", _("Unhandled Issue Count")
-        REGRESSED_ISSUE_COUNT = "RIC", _("Regressed Issue Count")
-        FAILURE_RATE = "FR", _("Failure Rate")
-        CRASH_FREE_SESSION_RATE = "CFSR", _("Crash Free Session Rate")
-        CRASH_FREE_USER_RATE = "CFUR", _("Crash Free User Rate")
+    threshold_type = models.BoundedPositiveIntegerField(choices=ReleaseThresholdType.as_choices())
+    threshold_trigger_type = models.BoundedPositiveIntegerField(choices=ValueType.as_choices())
+    threshold_value = models.IntegerField()
+    window_in_seconds = models.IntegerField()
 
-    class ValueType(models.TextChoices):
-        PERCENT_OVER = "PO", _("Percent Over")
-        PERCENT_UNDER = "PU", _("Percent Under")
-        ABSOLUTE_OVER = "AO", _("Absolute Over")
-        ABSOLUTE_UNDER = "AU", _("Absolute Under")
-
-    threshold_type = models.CharField(choices=ReleaseThresholdType.choices, max_length=120)
-    value_type = models.CharField(choices=ValueType.choices, max_length=120)
-    value = models.IntegerField()
-    window = models.IntegerField()  # in seconds
-
-    project = FlexibleForeignKey("sentry.Project")
-    environment = FlexibleForeignKey("sentry.Environment", null=True)
+    project = FlexibleForeignKey("sentry.Project", db_index=True)
+    environment = FlexibleForeignKey("sentry.Environment", null=True, db_index=True)
     date_added = models.DateTimeField(default=timezone.now)
