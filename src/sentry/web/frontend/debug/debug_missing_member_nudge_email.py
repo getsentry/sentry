16a094f0b41f1146b79e7496b143c7ca85a183bf
Cathy Teng <cathy.teng@sentry.io>
2023-08-30 16:14:39 -0700

    add debug mail view

---
+++ src/sentry/web/frontend/debug/debug_missing_member_nudge_email.py
@@ -0,0 +1,49 @@
+from django.http import HttpRequest, HttpResponse
+from django.views.generic import View
+
+from sentry.models import Organization
+
+from .mail import MailPreview
+
+
+class DebugMissingMemberNudgeView(View):
+    def get(self, request: HttpRequest) -> HttpResponse:
+        self.organization = Organization(id=1, slug="organization", name="My Company")
+        commit_authors = [
+            {
+                "email": "test@sentry.io",
+                "external_id": "test",
+                "commit_count": 5,
+            },
+            {
+                "email": "hello@sentry.io",
+                "external_id": "hello",
+                "commit_count": 4,
+            },
+            {
+                "email": "abcd@sentry.io",
+                "external_id": "abcd",
+                "commit_count": 3,
+            },
+            {
+                "email": "link@sentry.io",
+                "external_id": "link",
+                "commit_count": 2,
+            },
+            {
+                "email": "hola@sentry.io",
+                "external_id": "hola",
+                "commit_count": 1,
+            },
+        ]
+
+        return MailPreview(
+            html_template="sentry/emails/missing-members-nudge.html",
+            text_template="sentry/emails/missing-members-nudge.txt",
+            context={
+                "organization": self.organization,
+                "missing_members": commit_authors[0:3],
+                "missing_members_count": len(commit_authors),
+                "members_list_url": "https://sentry.io/",
+            },
+        ).render(request)
