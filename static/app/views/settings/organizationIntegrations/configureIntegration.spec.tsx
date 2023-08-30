dcaf9f230792b20682dc038b7a6fa3c02ad1df73
Michelle Fu <michelle.fu@sentry.io>
2023-08-30 09:11:35 -0700

    move responses to their own file

--- static/app/views/settings/organizationIntegrations/configureIntegration.spec.tsx
+++ static/app/views/settings/organizationIntegrations/configureIntegration.spec.tsx
@@ -12,95 +12,13 @@ describe('OpsgenieMigrationButton', function () {
     MockApiClient.addMockResponse({
       url: `/organizations/${org.slug}/config/integrations/`,
       body: {
-        providers: [
-          {
-            key: 'opsgenie',
-            slug: 'opsgenie',
-            name: 'Opsgenie (Integration)',
-            metadata: {
-              description: 'hi',
-              features: [
-                {
-                  description:
-                    'Manage incidents and outages by sending Sentry notifications to Opsgenie.',
-                  featureGate: 'integrations-incident-management',
-                },
-                {
-                  description:
-                    'Configure rule based Opsgenie alerts that automatically trigger and notify specific teams.',
-                  featureGate: 'integrations-alert-rule',
-                },
-              ],
-              author: 'The Sentry Team',
-              noun: 'Installation',
-              issue_url:
-                'https://github.com/getsentry/sentry/issues/new?assignees=&labels=Component:%20Integrations&template=bug.yml&title=Integration%20Problem',
-              source_url:
-                'https://github.com/getsentry/sentry/tree/master/src/sentry/integrations/opsgenie',
-              aspects: {},
-            },
-            canAdd: true,
-            canDisable: false,
-            features: ['alert-rule', 'incident-management'],
-            setupDialog: {
-              url: '/organizations/sentry/integrations/opsgenie/setup/',
-              width: 600,
-              height: 600,
-            },
-          },
-        ],
+        providers: [TestStubs.OpsgenieIntegrationProvider()],
       },
     });
 
     MockApiClient.addMockResponse({
       url: `/organizations/${org.slug}/integrations/${integrationId}/`,
-      body: {
-        id: '1',
-        name: 'hello-world',
-        icon: null,
-        domainName: 'hello-world.app.opsgenie.com',
-        accountType: null,
-        scopes: null,
-        status: 'active',
-        provider: {
-          key: 'opsgenie',
-          slug: 'opsgenie',
-          name: 'Opsgenie (Integration)',
-          canAdd: true,
-          canDisable: false,
-          features: ['alert-rule', 'incident-management'],
-          aspects: {},
-        },
-        configOrganization: [
-          {
-            name: 'team_table',
-            type: 'table',
-            label: 'Opsgenie integrations',
-            help: "If integration keys need to be updated, deleted, or added manually please do so here. Your keys must be associated with a 'Sentry' Integration in Opsgenie.                 Alert rules will need to be individually updated for any key additions or deletions.",
-            addButtonText: '',
-            columnLabels: {
-              team: 'Label',
-              integration_key: 'Integration Key',
-            },
-            columnKeys: ['team', 'integration_key'],
-            confirmDeleteMessage:
-              'Any alert rules associated with this integration will stop working. The rules will still exist but will show a `removed` team.',
-          },
-        ],
-        configData: {
-          team_table: [
-            {
-              team: 'python [MIGRATED]',
-              id: '1-python',
-              integration_key: 'abcdef',
-            },
-          ],
-        },
-        externalId: 'hello-world',
-        organizationId: 1,
-        organizationIntegrationStatus: 'active',
-        gracePeriodEnd: null,
-      },
+      body: TestStubs.OpsgenieIntegration(),
     });
 
     MockApiClient.addMockResponse({
