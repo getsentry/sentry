4d864b21ccf389465a7ba14490faefdd9acf5ffc
ArthurKnaus <arthur.knaus@sentry.io>
2023-08-31 12:51:03 +0200

    feat(onboarding): Add profiling to platform node (#55530)
    Add profiling product selection to platform node.
    
    Closes https://github.com/getsentry/sentry/issues/55441
    
    ---------
    
    Co-authored-by: Lukas Stracke <lukas.stracke@sentry.io>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/components/onboarding/productSelection.tsx
+++ static/app/components/onboarding/productSelection.tsx
@@ -63,6 +63,7 @@ export const platformProductAvailability = {
     ProductSolution.PERFORMANCE_MONITORING,
     ProductSolution.SESSION_REPLAY,
   ],
+  node: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   python: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   'python-aiohttp': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   'python-awslambda': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
