f4f0f448fdd76072e4c2a49cfc08b3f9af614152
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2023-08-31 13:53:37 -0700

    fix(github-growth): check for integration in missing members API (#55545)

--- tests/sentry/api/test_organization_missing_members.py
+++ tests/sentry/api/test_organization_missing_members.py
@@ -2,9 +2,11 @@ from datetime import timedelta
 
 from django.utils import timezone
 
+from sentry.constants import ObjectStatus
 from sentry.models.organizationmember import OrganizationMember
+from sentry.silo.base import SiloMode
 from sentry.testutils.cases import APITestCase
-from sentry.testutils.silo import region_silo_test
+from sentry.testutils.silo import assume_test_silo_mode, region_silo_test
 
 
 @region_silo_test(stable=True)
@@ -39,7 +41,12 @@ class OrganizationMissingMembersTestCase(APITestCase):
         self.nonmember_commit_author2.external_id = "d"
         self.nonmember_commit_author2.save()
 
-        self.repo = self.create_repo(project=self.project, provider="integrations:github")
+        self.integration = self.create_integration(
+            organization=self.organization, provider="github", name="Github", external_id="github:1"
+        )
+        self.repo = self.create_repo(
+            project=self.project, provider="integrations:github", integration_id=self.integration.id
+        )
         self.create_commit(repo=self.repo, author=self.member_commit_author)
         self.create_commit(repo=self.repo, author=self.nonmember_commit_author1)
         self.create_commit(repo=self.repo, author=self.nonmember_commit_author1)
@@ -74,6 +81,9 @@ class OrganizationMissingMembersTestCase(APITestCase):
     def test_filters_github_only(self):
         repo = self.create_repo(project=self.project, provider="integrations:bitbucket")
         self.create_commit(repo=repo, author=self.nonmember_commit_author1)
+        self.create_integration(
+            organization=self.organization, provider="bitbucket", external_id="bitbucket:1"
+        )
 
         response = self.get_success_response(self.organization.slug)
         assert response.data[0]["integration"] == "github"
@@ -115,6 +125,9 @@ class OrganizationMissingMembersTestCase(APITestCase):
     def test_no_authors(self):
         org = self.create_organization(owner=self.create_user())
         self.create_member(user=self.user, organization=org, role="manager")
+        self.create_integration(
+            organization=org, provider="github", name="Github", external_id="github:2"
+        )
 
         response = self.get_success_response(org.slug)
         assert response.data[0]["integration"] == "github"
@@ -184,4 +197,63 @@ class OrganizationMissingMembersTestCase(APITestCase):
         assert response.data[0]["users"] == [
             {"email": "c@example.com", "externalId": "c", "commitCount": 2},
             {"email": "c2@example.com", "externalId": "c@example.com", "commitCount": 1},
+        ]
+
+    def test_no_github_integration(self):
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            self.integration.delete()
+
+        response = self.get_success_response(self.organization.slug)
+        assert len(response.data) == 0
+
+    def test_disabled_integration(self):
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            self.integration.status = ObjectStatus.DISABLED
+            self.integration.save()
+
+        response = self.get_success_response(self.organization.slug)
+        assert len(response.data) == 0
+
+    def test_nongithub_integration(self):
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            self.integration.delete()
+
+        integration = self.create_integration(
+            organization=self.organization,
+            provider="bitbucket",
+            name="Bitbucket",
+            external_id="bitbucket:1",
+        )
+        repo = self.create_repo(
+            project=self.project, provider="integrations:github", integration_id=integration.id
+        )
+        self.create_commit(repo=repo, author=self.member_commit_author)
+        self.create_commit(repo=repo, author=self.nonmember_commit_author1)
+        self.create_commit(repo=repo, author=self.nonmember_commit_author1)
+        self.create_commit(repo=repo, author=self.nonmember_commit_author2)
+
+        response = self.get_success_response(self.organization.slug)
+        assert len(response.data) == 0
+
+    def test_filters_disabled_github_integration(self):
+        integration = self.create_integration(
+            organization=self.organization,
+            provider="github",
+            name="Github",
+            external_id="github:2",
+            status=ObjectStatus.DISABLED,
+        )
+        repo = self.create_repo(
+            project=self.project, provider="integrations:github", integration_id=integration.id
+        )
+        self.create_commit(repo=repo, author=self.member_commit_author)
+        self.create_commit(repo=repo, author=self.nonmember_commit_author1)
+        self.create_commit(repo=repo, author=self.nonmember_commit_author1)
+        self.create_commit(repo=repo, author=self.nonmember_commit_author2)
+
+        response = self.get_success_response(self.organization.slug)
+        assert response.data[0]["integration"] == "github"
+        assert response.data[0]["users"] == [
+            {"email": "c@example.com", "externalId": "c", "commitCount": 2},
+            {"email": "d@example.com", "externalId": "d", "commitCount": 1},
         ]
