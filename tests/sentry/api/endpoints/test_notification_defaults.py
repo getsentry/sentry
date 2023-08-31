f3d2e6071f53054e3c8401d56b5d3a610c502262
Cathy Teng <cathy.teng@sentry.io>
2023-08-31 08:20:32 -0700

    fix test

--- tests/sentry/api/endpoints/test_notification_defaults.py
+++ tests/sentry/api/endpoints/test_notification_defaults.py
@@ -16,6 +16,7 @@ class NotificationDefaultTest(APITestCase):
                 "alerts": "always",
                 "approval": "always",
                 "deploy": "committed_only",
+                "missingMembers": "always",
                 "quota": "always",
                 "quotaAttachments": "always",
                 "quotaErrors": "always",
