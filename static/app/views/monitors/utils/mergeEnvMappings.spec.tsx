d121c0ce45fb8f7fe892834892f560db0fbf92f3
David Wang <dwang@sentry.io>
2023-08-31 14:41:20 -0700

    feat(crons): Display in-progress ticks on frontend (#54995)
    <img width="1252" alt="image"
    src="https://github.com/getsentry/sentry/assets/9372512/54ebdc3e-97a8-4304-8731-775d4ae6f180">

--- static/app/views/monitors/utils/mergeEnvMappings.spec.tsx
+++ static/app/views/monitors/utils/mergeEnvMappings.spec.tsx
@@ -12,7 +12,7 @@ describe('mergeEnvMappings', function () {
   it('merges one empty mapping with one filled mapping', function () {
     const envMappingA = {};
     const envMappingB = {
-      prod: {ok: 1, missed: 0, timeout: 0, error: 0},
+      prod: {in_progress: 0, ok: 1, missed: 0, timeout: 0, error: 0},
     };
     const mergedMapping = mergeEnvMappings(envMappingA, envMappingB);
 
@@ -21,13 +21,13 @@ describe('mergeEnvMappings', function () {
 
   it('merges two filled mappings', function () {
     const envMappingA = {
-      prod: {ok: 0, missed: 1, timeout: 2, error: 1},
+      prod: {in_progress: 0, ok: 0, missed: 1, timeout: 2, error: 1},
     };
     const envMappingB = {
-      prod: {ok: 1, missed: 1, timeout: 0, error: 2},
+      prod: {in_progress: 2, ok: 1, missed: 1, timeout: 0, error: 2},
     };
     const expectedMerged = {
-      prod: {ok: 1, missed: 2, timeout: 2, error: 3},
+      prod: {in_progress: 2, ok: 1, missed: 2, timeout: 2, error: 3},
     };
     const mergedMapping = mergeEnvMappings(envMappingA, envMappingB);
 
@@ -36,12 +36,12 @@ describe('mergeEnvMappings', function () {
 
   it('merges two filled mappings with differing envs', function () {
     const envMappingA = {
-      prod: {ok: 0, missed: 1, timeout: 2, error: 1},
+      prod: {in_progress: 1, ok: 0, missed: 1, timeout: 2, error: 1},
     };
-    const envMappingB = {dev: {ok: 1, missed: 1, timeout: 0, error: 2}};
+    const envMappingB = {dev: {in_progress: 0, ok: 1, missed: 1, timeout: 0, error: 2}};
     const expectedMerged = {
-      prod: {ok: 0, missed: 1, timeout: 2, error: 1},
-      dev: {ok: 1, missed: 1, timeout: 0, error: 2},
+      prod: {in_progress: 1, ok: 0, missed: 1, timeout: 2, error: 1},
+      dev: {in_progress: 0, ok: 1, missed: 1, timeout: 0, error: 2},
     };
     const mergedMapping = mergeEnvMappings(envMappingA, envMappingB);
 
