23b37b6943e12ee2de50266d90af66334ba092f2
Arthur Knaus <arthur.knaus@sentry.io>
2023-08-31 14:07:26 +0200

    feat(onboarding): Add profiling to platform node-express

--- static/app/components/onboarding/productSelection.tsx
+++ static/app/components/onboarding/productSelection.tsx
@@ -65,6 +65,7 @@ export const platformProductAvailability = {
   ],
   node: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   'node-connect': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
+  'node-express': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   python: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   'python-aiohttp': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   'python-awslambda': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
