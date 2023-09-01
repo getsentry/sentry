7a1db16e03a5f30352a15d1e806f587bcb48b5cf
ArthurKnaus <arthur.knaus@sentry.io>
2023-09-01 11:11:33 +0200

    fix(onboarding): Fix malformed url regex (#55610)
    * Fix malformed url regex
    * Minor fixes to the react configuration snippet

--- static/app/gettingStartedDocs/javascript/svelte.tsx
+++ static/app/gettingStartedDocs/javascript/svelte.tsx
@@ -29,7 +29,7 @@ replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire sess
 const performanceIntegration = `
 new Sentry.BrowserTracing({
   // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-  tracePropagationTargets: ["localhost", "https:yourserver.io/api/"],
+  tracePropagationTargets: ["localhost", /^https:\\/\\/yourserver\\.io\\/api/],
 }),
 `;
 
