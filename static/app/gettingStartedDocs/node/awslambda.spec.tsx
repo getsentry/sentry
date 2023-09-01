b1ff7225196e0ec531174629c103a050d47b33fa
ArthurKnaus <arthur.knaus@sentry.io>
2023-09-01 11:00:06 +0200

    feat(onboarding): Add profiling to serverless node platforms (#55538)
    * Add profiling to platform node-awslambda
    * Add profiling to platform node-gcpfunctions
    
    Closes https://github.com/getsentry/sentry/issues/55442
    Closes https://github.com/getsentry/sentry/issues/55446

--- static/app/gettingStartedDocs/node/awslambda.spec.tsx
+++ static/app/gettingStartedDocs/node/awslambda.spec.tsx
@@ -9,7 +9,11 @@ describe('GettingStartedWithAwsLambda', function () {
     const {container} = render(<GettingStartedWithAwsLambda dsn="test-dsn" />);
 
     // Steps
-    for (const step of steps()) {
+    for (const step of steps({
+      installSnippet: 'test-install-snippet',
+      importContent: 'test-import-content',
+      initContent: 'test-init-content',
+    })) {
       expect(
         screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
       ).toBeInTheDocument();
