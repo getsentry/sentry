c8a7424adbd35328f8a3a4ff69963f4b174e3e74
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-09-05 14:48:54 -0700

    add in aggregation: 0 again

--- fixtures/js-stubs/metricRule.ts
+++ fixtures/js-stubs/metricRule.ts
@@ -15,6 +15,7 @@ export function MetricRule<
     id: '4',
     name: 'My Incident Rule',
     timeWindow: 60,
+    aggregation: 0,
     aggregate: 'count()',
     projects: ['project-slug'],
     dateModified: '2019-07-31T23:02:02.731Z',
