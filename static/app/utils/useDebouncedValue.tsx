3a05fc7d08308eeae7d5bade4da5c9a7e4fad309
davidenwang <dwang@sentry.io>
2023-09-05 15:34:03 -0700

    feat(ui): Add useDebouncedValue hook

---
+++ static/app/utils/useDebouncedValue.tsx
@@ -0,0 +1,19 @@
+import {useEffect, useState} from 'react';
+
+/**
+ * Given value and delay in ms, returns a version of that value which receives debounced updates
+ * according to the specified delay
+ */
+export function useDebouncedValue<T>(value: T, delay: number): T {
+  const [debouncedValue, setDebouncedValue] = useState<T>(value);
+
+  useEffect(() => {
+    const timer = setTimeout(() => setDebouncedValue(value), delay);
+
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+}
