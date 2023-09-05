1e8363713b0e9fb5849f568d8f7b2cf0d1e51750
Cathy Teng <cathy.teng@sentry.io>
2023-09-05 11:26:33 -0700

    ref missing members API

--- src/sentry/api/endpoints/organization_missing_org_members.py
+++ src/sentry/api/endpoints/organization_missing_org_members.py
@@ -35,66 +35,62 @@ class MissingMembersPermission(OrganizationPermission):
     scope_map = {"GET": ["org:write"]}
 
 
+def _get_missing_organization_members(
+    organization: Organization, provider: str, integration_ids: Sequence[int]
+) -> QuerySet[CommitAuthor]:
+    member_emails = set(organization.member_set.exclude(email=None).values_list("email", flat=True))
+    member_emails.update(
+        set(organization.member_set.exclude(user_email=None).values_list("user_email", flat=True))
+    )
+    nonmember_authors = CommitAuthor.objects.filter(organization_id=organization.id).exclude(
+        Q(email__in=member_emails) | Q(external_id=None)
+    )
+
+    org_repos = Repository.objects.filter(
+        provider="integrations:" + provider,
+        organization_id=organization.id,
+        integration_id__in=integration_ids,
+    ).values_list("id", flat=True)
+
+    return (
+        nonmember_authors.filter(
+            commit__repository_id__in=set(org_repos),
+            commit__date_added__gte=timezone.now() - timedelta(days=30),
+        )
+        .annotate(commit_count=Count("commit"))
+        .order_by("-commit_count")
+    )
+
+
+def _get_shared_email_domain(organization: Organization) -> str | None:
+    # if a member has user_email=None, then they have yet to accept an invite
+    org_owners = organization.get_members_with_org_roles(roles=[roles.get_top_dog().id]).exclude(
+        Q(user_email=None) | Q(user_email="")
+    )
+
+    def _get_email_domain(email: str) -> str | None:
+        try:
+            domain = Address(addr_spec=email).domain
+        except Exception:
+            return None
+
+        return domain
+
+    owner_email_domains = {_get_email_domain(owner.user_email) for owner in org_owners}
+
+    # all owners have the same email domain
+    if len(owner_email_domains) == 1:
+        return owner_email_domains.pop()
+
+    return None
+
+
 @region_silo_endpoint
 class OrganizationMissingMembersEndpoint(OrganizationEndpoint):
     publish_status = {
         "GET": ApiPublishStatus.UNKNOWN,
     }
     permission_classes = (MissingMembersPermission,)
-
-    def _get_missing_members(
-        self, organization: Organization, provider: str, integration_ids: Sequence[int]
-    ) -> QuerySet[CommitAuthor]:
-        member_emails = set(
-            organization.member_set.exclude(email=None).values_list("email", flat=True)
-        )
-        member_emails.update(
-            set(
-                organization.member_set.exclude(user_email=None).values_list(
-                    "user_email", flat=True
-                )
-            )
-        )
-        nonmember_authors = CommitAuthor.objects.filter(organization_id=organization.id).exclude(
-            Q(email__in=member_emails) | Q(external_id=None)
-        )
-
-        org_repos = Repository.objects.filter(
-            provider="integrations:" + provider,
-            organization_id=organization.id,
-            integration_id__in=integration_ids,
-        ).values_list("id", flat=True)
-
-        return (
-            nonmember_authors.filter(
-                commit__repository_id__in=set(org_repos),
-                commit__date_added__gte=timezone.now() - timedelta(days=30),
-            )
-            .annotate(commit_count=Count("commit"))
-            .order_by("-commit_count")
-        )
-
-    def _get_shared_email_domain(self, organization) -> str | None:
-        # if a member has user_email=None, then they have yet to accept an invite
-        org_owners = organization.get_members_with_org_roles(
-            roles=[roles.get_top_dog().id]
-        ).exclude(Q(user_email=None) | Q(user_email=""))
-
-        def _get_email_domain(email: str) -> str | None:
-            try:
-                domain = Address(addr_spec=email).domain
-            except Exception:
-                return None
-
-            return domain
-
-        owner_email_domains = {_get_email_domain(owner.user_email) for owner in org_owners}
-
-        # all owners have the same email domain
-        if len(owner_email_domains) == 1:
-            return owner_email_domains.pop()
-
-        return None
 
     def get(self, request: Request, organization: Organization) -> Response:
         # ensure the organization has an integration with the commit feature
@@ -116,7 +112,7 @@ class OrganizationMissingMembersEndpoint(OrganizationEndpoint):
             provider_reducer, integrations, defaultdict(list)
         )
 
-        shared_domain = self._get_shared_email_domain(organization)
+        shared_domain = _get_shared_email_domain(organization)
 
         missing_org_members = []
 
@@ -125,7 +121,7 @@ class OrganizationMissingMembersEndpoint(OrganizationEndpoint):
             if integration_provider != "github":
                 continue
 
-            queryset = self._get_missing_members(
+            queryset = _get_missing_organization_members(
                 organization, integration_provider, integration_ids
             )
 
