1f012f71e3a65efbb6c5c98c07778023a18f64c9
isabellaenriquez <isabella.enriquez@sentry.io>
2023-09-06 15:32:20 -0700

    Rename team_id to team

--- src/sentry/models/groupsubscription.py
+++ src/sentry/models/groupsubscription.py
@@ -179,7 +179,7 @@ class GroupSubscription(Model):
     project = FlexibleForeignKey("sentry.Project", related_name="subscription_set")
     group = FlexibleForeignKey("sentry.Group", related_name="subscription_set")
     user_id = HybridCloudForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete="CASCADE")
-    team_id = FlexibleForeignKey("sentry.Team", null=True, db_index=True, on_delete=models.CASCADE)
+    team = FlexibleForeignKey("sentry.Team", null=True, db_index=True, on_delete=models.CASCADE)
     is_active = models.BooleanField(default=True)
     reason = BoundedPositiveIntegerField(default=GroupSubscriptionReason.unknown)
     date_added = models.DateTimeField(default=timezone.now, null=True)
@@ -189,7 +189,7 @@ class GroupSubscription(Model):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_groupsubscription"
-        unique_together = (("group", "user_id"), ("group", "team_id"))
+        unique_together = (("group", "user_id"), ("group", "team"))
         constraints = [
             models.CheckConstraint(
                 check=models.Q(team_id__isnull=False, user_id__isnull=True)
