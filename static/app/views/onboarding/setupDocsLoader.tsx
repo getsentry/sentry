3c93a3988ca8dcb6eee78180a960e90698f74a43
ArthurKnaus <arthur.knaus@sentry.io>
2023-09-06 11:58:35 +0200

    fix(onboarding): js loader script configuration (#55753)
    Make js loader script configuration section react to changes of the
    product selector.
    
    Closes https://github.com/getsentry/sentry/issues/55677

--- static/app/views/onboarding/setupDocsLoader.tsx
+++ static/app/views/onboarding/setupDocsLoader.tsx
@@ -198,14 +198,31 @@ function ProjectKeyInfo({
 
   const loaderLink = projectKey.dsn.cdn;
   const currentPlatform = platform ?? project?.platform ?? 'other';
+  const hasPerformance = products.includes(ProductSolution.PERFORMANCE_MONITORING);
+  const hasSessionReplay = products.includes(ProductSolution.SESSION_REPLAY);
 
   const configCodeSnippet = beautify.html(
     `<script>
 Sentry.onLoad(function() {
-  Sentry.init({
-    // No need to configure DSN here, it is already configured in the loader script
-    // You can add any additional configuration here
-    sampleRate: 0.5,
+  Sentry.init({${
+    !(hasPerformance || hasSessionReplay)
+      ? `
+    // You can add any additional configuration here`
+      : ''
+  }${
+      hasPerformance
+        ? `
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions, reduce in production!`
+        : ''
+    }${
+      hasSessionReplay
+        ? `
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.`
+        : ''
+    }
   });
 });
 </script>`,
