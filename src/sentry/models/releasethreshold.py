41b1cda1f1dce39f188c59a5c9ec717e55bf57fc
Eric Hasegawa <eric.hasegawa@sentry.io>
2023-09-06 11:01:35 -0700

    feat(releases): Update threshold and trigger types

--- src/sentry/models/releasethreshold.py
+++ src/sentry/models/releasethreshold.py
@@ -2,12 +2,8 @@ from django.db import models
 from django.utils import timezone
 
 from sentry.backup.scopes import RelocationScope
-from sentry.db.models import (
-    BoundedPositiveIntegerField,
-    FlexibleForeignKey,
-    Model,
-    region_silo_only_model,
-)
+from sentry.db.models import FlexibleForeignKey, Model, region_silo_only_model
+from sentry.db.models.fields.bounded import BoundedPositiveIntegerField
 
 
 class ReleaseThresholdType:
@@ -19,16 +15,44 @@ class ReleaseThresholdType:
     CRASH_FREE_SESSION_RATE = 5
     CRASH_FREE_USER_RATE = 6
 
+    TOTAL_ERROR_COUNT_STR = "total_error_count"
+    NEW_ISSUE_COUNT_STR = "new_issue_count"
+    UNHANDLED_ISSUE_COUNT_STR = "unhandled_issue_count"
+    REGRESSED_ISSUE_COUNT_STR = "regressed_issue_count"
+    FAILURE_RATE_STR = "failure_rate"
+    CRASH_FREE_SESSION_RATE_STR = "crash_free_session_rate"
+    CRASH_FREE_USER_RATE_STR = "crash_free_user_rate"
+
+    STRING_TO_INT = {
+        TOTAL_ERROR_COUNT_STR: TOTAL_ERROR_COUNT,
+        NEW_ISSUE_COUNT_STR: NEW_ISSUE_COUNT,
+        UNHANDLED_ISSUE_COUNT_STR: UNHANDLED_ISSUE_COUNT,
+        REGRESSED_ISSUE_COUNT_STR: REGRESSED_ISSUE_COUNT,
+        FAILURE_RATE_STR: FAILURE_RATE,
+        CRASH_FREE_SESSION_RATE_STR: CRASH_FREE_SESSION_RATE,
+        CRASH_FREE_USER_RATE_STR: CRASH_FREE_USER_RATE,
+    }
+
+    INT_TO_STRING = {
+        TOTAL_ERROR_COUNT: TOTAL_ERROR_COUNT_STR,
+        NEW_ISSUE_COUNT: NEW_ISSUE_COUNT_STR,
+        UNHANDLED_ISSUE_COUNT: UNHANDLED_ISSUE_COUNT_STR,
+        REGRESSED_ISSUE_COUNT: REGRESSED_ISSUE_COUNT_STR,
+        FAILURE_RATE: FAILURE_RATE_STR,
+        CRASH_FREE_SESSION_RATE: CRASH_FREE_SESSION_RATE_STR,
+        CRASH_FREE_USER_RATE: CRASH_FREE_USER_RATE_STR,
+    }
+
     @classmethod
-    def as_choices(cls):
+    def as_str_choices(cls):
         return (
-            (cls.TOTAL_ERROR_COUNT, "Total Error Count"),
-            (cls.NEW_ISSUE_COUNT, "New Issue Count"),
-            (cls.UNHANDLED_ISSUE_COUNT, "Unhandled Issue Count"),
-            (cls.REGRESSED_ISSUE_COUNT, "Regressed Issue Count"),
-            (cls.FAILURE_RATE, "Failure Rate"),
-            (cls.CRASH_FREE_SESSION_RATE, "Crash Free Session Rate"),
-            (cls.CRASH_FREE_USER_RATE, "Crash Free User Rate"),
+            (cls.TOTAL_ERROR_COUNT_STR, cls.TOTAL_ERROR_COUNT_STR),
+            (cls.NEW_ISSUE_COUNT_STR, cls.NEW_ISSUE_COUNT_STR),
+            (cls.UNHANDLED_ISSUE_COUNT_STR, cls.UNHANDLED_ISSUE_COUNT_STR),
+            (cls.REGRESSED_ISSUE_COUNT_STR, cls.REGRESSED_ISSUE_COUNT_STR),
+            (cls.FAILURE_RATE_STR, cls.FAILURE_RATE_STR),
+            (cls.CRASH_FREE_SESSION_RATE_STR, cls.CRASH_FREE_SESSION_RATE_STR),
+            (cls.CRASH_FREE_USER_RATE_STR, cls.CRASH_FREE_USER_RATE_STR),
         )
 
 
@@ -38,13 +62,32 @@ class TriggerType:
     ABSOLUTE_OVER = 2
     ABSOLUTE_UNDER = 3
 
+    PERCENT_OVER_STR = "percent_over"
+    PERCENT_UNDER_STR = "percent_under"
+    ABSOLUTE_OVER_STR = "absolute_over"
+    ABSOLUTE_UNDER_STR = "absolute_under"
+
+    STRING_TO_INT = {
+        PERCENT_OVER_STR: PERCENT_OVER,
+        PERCENT_UNDER_STR: PERCENT_UNDER,
+        ABSOLUTE_OVER_STR: ABSOLUTE_OVER,
+        ABSOLUTE_UNDER_STR: ABSOLUTE_UNDER,
+    }
+
+    INT_TO_STRING = {
+        PERCENT_OVER: PERCENT_OVER_STR,
+        PERCENT_UNDER: PERCENT_UNDER_STR,
+        ABSOLUTE_OVER: ABSOLUTE_OVER_STR,
+        ABSOLUTE_UNDER: ABSOLUTE_UNDER_STR,
+    }
+
     @classmethod
-    def as_choices(cls):
+    def as_str_choices(cls):
         return (
-            (cls.PERCENT_OVER, "Percent Over"),
-            (cls.PERCENT_UNDER, "Percent Under"),
-            (cls.ABSOLUTE_OVER, "Absolute Over"),
-            (cls.ABSOLUTE_UNDER, "Absolute Under"),
+            (cls.PERCENT_OVER_STR, cls.PERCENT_OVER_STR),
+            (cls.PERCENT_UNDER_STR, cls.PERCENT_UNDER_STR),
+            (cls.ABSOLUTE_OVER_STR, cls.ABSOLUTE_OVER_STR),
+            (cls.ABSOLUTE_UNDER_STR, cls.ABSOLUTE_UNDER_STR),
         )
 
 
