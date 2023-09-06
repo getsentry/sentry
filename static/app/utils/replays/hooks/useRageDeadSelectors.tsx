6855b6373a1d64b410fc76633ee16c798b93c605
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-09-05 17:24:40 -0700

    move type outside function

--- static/app/utils/replays/hooks/useRageDeadSelectors.tsx
+++ static/app/utils/replays/hooks/useRageDeadSelectors.tsx
@@ -2,16 +2,16 @@ import {useApiQuery} from 'sentry/utils/queryClient';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 
+type RageDeadSelectorResponse = {
+  count_dead_clicks: number;
+  count_rage_clicks: number;
+  dom_element: string;
+};
+
 export function useRageDeadSelectors(limit: number, sort: string) {
   const organization = useOrganization();
   const location = useLocation();
   const {query} = location;
-
-  type RageDeadSelectorResponse = {
-    count_dead_clicks: number;
-    count_rage_clicks: number;
-    dom_element: string;
-  };
 
   return useApiQuery<RageDeadSelectorResponse[]>(
     [
