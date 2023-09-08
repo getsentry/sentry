31bd8197b1a610e7d518e8b189d49991f65eed29
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-09-08 13:29:17 -0700

    Revert "adds chunks"
    This reverts commit 2e0a9a9db84fd335d7bacb7ec598178118bec95b.

--- src/sentry/monitors/tasks.py
+++ src/sentry/monitors/tasks.py
@@ -37,9 +37,6 @@ CHECKINS_LIMIT = 10_000
 # This key is used to store the last timestamp that the tasks were triggered.
 MONITOR_TASKS_LAST_TRIGGERED_KEY = "sentry.monitors.last_tasks_ts"
 
-# Size of the chunk to use for the check_missing_environment task
-MISSING_CHUNK_SIZE = 10
-
 
 def _get_monitor_checkin_producer() -> KafkaProducer:
     cluster_name = get_topic_definition(settings.KAFKA_INGEST_MONITORS)["cluster"]
@@ -207,7 +204,8 @@ def check_missing(current_datetime: datetime):
         )[:MONITOR_LIMIT]
     )
     metrics.gauge("sentry.monitors.tasks.check_missing.count", qs.count(), sample_rate=1.0)
-    check_missing_environment.chunks(qs.values_list("id", flat=True), MISSING_CHUNK_SIZE).delay()
+    for monitor_environment in qs:
+        check_missing_environment.delay(monitor_environment.id)
 
 
 @instrumented_task(
