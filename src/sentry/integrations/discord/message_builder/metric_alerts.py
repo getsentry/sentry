76ae32b829696a2a7b078facf21fcdcd7cccdb77
17hogeju <17hogeju@gmail.com>
2023-09-08 10:57:08 -0700

    Add tests for metrics message builder

--- src/sentry/integrations/discord/message_builder/metric_alerts.py
+++ src/sentry/integrations/discord/message_builder/metric_alerts.py
@@ -3,35 +3,42 @@ from __future__ import annotations
 import time
 from datetime import datetime
 
-from sentry.incidents.models import Incident, IncidentStatus
+from pyparsing import Optional
+
+from sentry.incidents.models import AlertRule, Incident, IncidentStatus
 from sentry.integrations.discord.message_builder import INCIDENT_COLOR_MAPPING, LEVEL_TO_COLOR
 from sentry.integrations.discord.message_builder.base.base import DiscordMessageBuilder
 from sentry.integrations.discord.message_builder.base.embed.base import DiscordMessageEmbed
 from sentry.integrations.discord.message_builder.base.embed.image import DiscordMessageEmbedImage
-from sentry.integrations.metric_alerts import incident_attachment_info
+from sentry.integrations.metric_alerts import metric_alert_attachment_info
+from sentry.integrations.slack.utils.escape import escape_slack_text
 
 
 class DiscordMetricAlertMessageBuilder(DiscordMessageBuilder):
     def __init__(
         self,
-        incident: Incident,
-        new_status: IncidentStatus,
-        metric_value: int | None = None,
-        chart_url: str | None = None,
+        alert_rule: AlertRule,
+        incident: Optional[Incident] = None,
+        new_status: Optional[IncidentStatus] = None,
+        metric_value: Optional[int] = None,
+        chart_url: Optional[str] = None,
     ) -> None:
+        self.alert_rule = alert_rule
         self.incident = incident
         self.metric_value = metric_value
         self.new_status = new_status
         self.chart_url = chart_url
 
     def build(self) -> dict[str, object]:
-        data = incident_attachment_info(self.incident, self.new_status, self.metric_value)
+        data = metric_alert_attachment_info(
+            self.alert_rule, self.incident, self.new_status, self.metric_value
+        )
 
         embeds = [
             DiscordMessageEmbed(
                 title=data["title"],
                 url=f"{data['title_link']}&referrer=discord",
-                description=f"{data['text']}\n\n {get_started_at(data['ts'])}",
+                description=f"<{data['title_link']}|*{escape_slack_text(data['title'])}*>  \n{data['text']}",
                 color=LEVEL_TO_COLOR[INCIDENT_COLOR_MAPPING.get(data["status"], "")],
                 image=DiscordMessageEmbedImage(self.chart_url) if self.chart_url else None,
             )
