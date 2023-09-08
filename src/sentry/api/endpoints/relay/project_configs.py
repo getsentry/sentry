4e5ec8edc3a0aacfda2eba51e78ccc96fc1acbcf
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-09-08 15:11:47 +0200

    simplify code

--- src/sentry/api/endpoints/relay/project_configs.py
+++ src/sentry/api/endpoints/relay/project_configs.py
@@ -59,7 +59,7 @@ class RelayProjectConfigsEndpoint(Endpoint):
         version = request.GET.get("version") or "1"
         set_tag("relay_protocol_version", version)
 
-        if version in ["3"] and request.relay_request_data.get("global"):
+        if version == "3" and request.relay_request_data.get("global"):
             response["global"] = get_global_config()
 
         if self._should_post_or_schedule(version, request):
