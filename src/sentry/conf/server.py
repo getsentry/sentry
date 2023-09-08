0ca02ee7320cb8befd7ee5cbc73249ce8d9324e6
Mark Story <mark@mark-story.com>
2023-09-08 11:30:45 -0400

    feat(hybridcloud) Add feature flag for frontend domain splitting
    Add a feature flag for the work to split up frontend requests between
    region and control silo domains.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1401,6 +1401,8 @@ SENTRY_FEATURES = {
     "organizations:escalating-issues-v2": False,
     # Enable emiting escalating data to the metrics backend
     "organizations:escalating-metrics-backend": False,
+    # Enable the frontend to request from region & control silo domains.
+    "organizations:frontend-domainsplit": False,
     # Allows an org to have a larger set of project ownership rules per project
     "organizations:higher-ownership-limit": False,
     # Enable Monitors (Crons) view
