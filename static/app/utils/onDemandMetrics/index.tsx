f28faf3ef2068b2ecc4806726fda82a761862414
Ogi <86684834+obostjancic@users.noreply.github.com>
2023-09-08 10:06:32 +0200

    feat(alerts): on demand alerts UI (#55838)

--- static/app/utils/onDemandMetrics/index.tsx
+++ static/app/utils/onDemandMetrics/index.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from '@emotion/styled';
 
 import {
   ParseResult,
@@ -7,8 +6,6 @@ import {
   Token,
   TokenResult,
 } from 'sentry/components/searchSyntax/parser';
-import {Tooltip} from 'sentry/components/tooltip';
-import {IconWarning} from 'sentry/icons';
 import {Organization} from 'sentry/types';
 import {FieldKey, getFieldDefinition} from 'sentry/utils/fields';
 import {
@@ -97,18 +94,10 @@ function getTokenKeyValuePair(
   return null;
 }
 
-const EXTRAPOLATED_AREA_STRIPE_IMG =
-  'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAoAAABkAQMAAACFAjPUAAAAAXNSR0IB2cksfwAAAAlwSFlzAAALEwAACxMBAJqcGAAAAAZQTFRFpKy5SVlzL3npZAAAAA9JREFUeJxjsD/AMIqIQwBIyGOd43jaDwAAAABJRU5ErkJggg==';
-
-export const extrapolatedAreaStyle = {
-  color: {
-    repeat: 'repeat',
-    image: EXTRAPOLATED_AREA_STRIPE_IMG,
-    rotation: 0.785,
-    scaleX: 0.5,
-  },
-  opacity: 1.0,
-};
+export function getOnDemandKeys(query: string): string[] {
+  const searchFilterKeys = getSearchFilterKeys(query);
+  return searchFilterKeys.filter(isOnDemandSearchKey);
+}
 
 export function hasOnDemandMetricAlertFeature(organization: Organization) {
   return organization.features.includes('on-demand-metrics-extraction');
@@ -120,17 +109,3 @@ export function hasOnDemandMetricWidgetFeature(organization: Organization) {
     organization.features.includes('on-demand-metrics-extraction-experimental')
   );
 }
-
-export function OnDemandWarningIcon({msg}: {msg: React.ReactNode}) {
-  return (
-    <Tooltip title={msg}>
-      <HoverableIconWarning color="gray300" />
-    </Tooltip>
-  );
-}
-
-const HoverableIconWarning = styled(IconWarning)`
-  &:hover {
-    cursor: pointer;
-  }
-`;
