c7cc307aeb0e7f7ae46d99b70bf6702863b3f103
Steven Eubank <47563310+smeubank@users.noreply.github.com>
2023-09-08 15:31:34 +0200

    Wizard only onboarding (#55844)
    ~Remix~, SvelteKit, and Nextjs
    
    Someone can validate if these changes, by someone I mean a real
    developer :D
    
    SvelteKit: local
    <img width="1204" alt="image"
    src="https://github.com/getsentry/sentry/assets/47563310/6f6f8abb-b439-4f64-ab38-328fa8ed25f4">
    
    NextJs: local
    <img width="1203" alt="image"
    src="https://github.com/getsentry/sentry/assets/47563310/ccd6f39c-cd0b-447e-be6d-f71803019735">
    
    ~Remix: local~
    <img width="1216" alt="image"
    src="https://github.com/getsentry/sentry/assets/47563310/379815d1-cbdc-49ae-9e39-d8efc40e9211">
    
    
    closes #55777
    
    EDIT: Remix is still open, waiting on
    https://github.com/getsentry/sentry-wizard/issues/433, merging this
    without remix. Remix change is tracked in #54615
    
    ---------
    
    Co-authored-by: ArthurKnaus <arthur.knaus@sentry.io>

--- static/app/gettingStartedDocs/javascript/nextjs.spec.tsx
+++ static/app/gettingStartedDocs/javascript/nextjs.spec.tsx
@@ -1,41 +1,17 @@
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
-import {ProductSolution} from 'sentry/components/onboarding/productSelection';
 
-import {GettingStartedWithNextJs, nextSteps, steps} from './nextjs';
+import {GettingStartedWithNextJs, steps} from './nextjs';
 
 describe('GettingStartedWithNextJs', function () {
-  it('all products are selected', function () {
-    const {container} = render(
-      <GettingStartedWithNextJs
-        dsn="test-dsn"
-        activeProductSelection={[
-          ProductSolution.PERFORMANCE_MONITORING,
-          ProductSolution.SESSION_REPLAY,
-        ]}
-      />
-    );
+  it('renders doc correctly', function () {
+    const {container} = render(<GettingStartedWithNextJs dsn="test-dsn" />);
 
     // Steps
     for (const step of steps()) {
       expect(
         screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
-      ).toBeInTheDocument();
-    }
-
-    // Next Steps
-    const filteredNextStepsLinks = nextSteps.filter(
-      nextStep =>
-        ![
-          ProductSolution.PERFORMANCE_MONITORING,
-          ProductSolution.SESSION_REPLAY,
-        ].includes(nextStep.id as ProductSolution)
-    );
-
-    for (const filteredNextStepsLink of filteredNextStepsLinks) {
-      expect(
-        screen.getByRole('link', {name: filteredNextStepsLink.name})
       ).toBeInTheDocument();
     }
 
