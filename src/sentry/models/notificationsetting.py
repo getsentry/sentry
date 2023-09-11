getsentry/sentry

commit a69b7f0a87d053d3fabfb449fea97840d61f2658

feat(hc): rename NotificationSetting.target to target_id (#48256)

src/sentry/models/notificationsetting.py
@@ -59,8 +59,8 @@ class NotificationSetting(Model):
         null=False,
     )
     scope_identifier = BoundedBigIntegerField(null=False)
-    target = FlexibleForeignKey(
-        "sentry.Actor", db_index=True, unique=False, null=False, on_delete=models.CASCADE
+    target_id = HybridCloudForeignKey(
+        "sentry.Actor", db_index=True, unique=False, null=False, on_delete="CASCADE"
     )
     team_id = HybridCloudForeignKey("sentry.Team", null=True, db_index=True, on_delete="CASCADE")
     user = FlexibleForeignKey(
@@ -112,7 +112,7 @@ class NotificationSetting(Model):
             (
                 "scope_type",
                 "scope_identifier",
-                "target",
+                "target_id",
                 "provider",
                 "type",
             ),

src/sentry/notifications/manager.py
@@ -332,7 +332,7 @@ class NotificationsManager(BaseManager["NotificationSetting"]):
                 scope_identifier__in=team_ids,
             ),
             type=type_.value,
-            target__in=actor_ids,
+            target_id__in=actor_ids,
         )
 
     def filter_to_accepting_recipients(

src/sentry/migrations/0434_notification_setting_target_id.py
@@ -1,0 +1,49 @@
+# Generated by Django 2.2.28 on 2023-04-28 23:18
+
+from django.db import migrations
+
+import sentry.db.models.fields.hybrid_cloud_foreign_key
+from sentry.new_migrations.migrations import CheckedMigration
+
+
+class Migration(CheckedMigration):
+    # This flag is used to mark that a migration shouldn't be automatically run in production. For
+    # the most part, this should only be used for operations where it's safe to run the migration
+    # after your code has deployed. So this should not be used for most operations that alter the
+    # schema of a table.
+    # Here are some things that make sense to mark as dangerous:
+    # - Large data migrations. Typically we want these to be run manually by ops so that they can
+    #   be monitored and not block the deploy for a long period of time while they run.
+    # - Adding indexes to large tables. Since this can take a long time, we'd generally prefer to
+    #   have ops run this and not block the deploy. Note that while adding an index is a schema
+    #   change, it's completely safe to run the operation after the code has deployed.
+    is_dangerous = False
+
+    dependencies = [
+        ("sentry", "0433_set_monitor_status_to_object_status_only"),
+    ]
+
+    operations = [
+        migrations.SeparateDatabaseAndState(
+            state_operations=[
+                migrations.AddField(
+                    model_name="notificationsetting",
+                    name="target_id",
+                    field=sentry.db.models.fields.hybrid_cloud_foreign_key.HybridCloudForeignKey(
+                        "sentry.Actor", db_index=True, null=False, on_delete="CASCADE"
+                    ),
+                ),
+                migrations.RenameField(
+                    model_name="notificationsetting",
+                    old_name="target",
+                    new_name="target_id",
+                ),
+                migrations.AlterUniqueTogether(
+                    name="notificationsetting",
+                    unique_together={
+                        ("scope_type", "scope_identifier", "target_id", "provider", "type")
+                    },
+                ),
+            ],
+        ),
+    ]

src/sentry/models/team.py
@@ -302,7 +302,7 @@ class Team(Model, SnowflakeIdMixin):
                 with transaction.atomic(), in_test_psql_role_override("postgres"):
                     cursor.execute("DELETE FROM sentry_team WHERE id = %s", [self.id])
                     self.outbox_for_update().save()
-                cursor.execute("DELETE FROM sentry_actor WHERE team_id = %s", [new_team.id])
+                    cursor.execute("DELETE FROM sentry_actor WHERE team_id = %s", [new_team.id])
             finally:
                 cursor.close()
 

src/sentry/services/hybrid_cloud/notifications/impl.py
@@ -75,7 +75,7 @@ class DatabaseBackedNotificationsService(NotificationsService):
         value: NotificationSettingOptionValues,
     ) -> List[RpcNotificationSetting]:
         settings = NotificationSetting.objects.filter(
-            target__in=[u.actor_id for u in users],
+            target_id__in=[u.actor_id for u in users],
             type__in=types,
             value=value.value,
             scope_type=NotificationScopeType.USER.value,
@@ -104,7 +104,7 @@ class DatabaseBackedNotificationsService(NotificationsService):
                 scope_identifier__in=team_ids,
             ),
             type=type.value,
-            target__in=actor_ids,
+            target_id__in=actor_ids,
         )
 
         return [self.serialize_notification_setting(s) for s in notification_settings]

tests/sentry/db/test_silo_models.py
@@ -1,5 +1,5 @@
 from sentry.api.serializers.base import registry
-from sentry.models import Actor, NotificationSetting, OrganizationMember, User
+from sentry.models import Actor, OrganizationMember, User
 from sentry.models.integrations import ExternalActor, ExternalIssue, Integration
 from sentry.testutils.silo import (
     validate_models_have_silos,
@@ -12,7 +12,6 @@ fk_exemptions = {
     (OrganizationMember, User),
     (Integration, ExternalActor),
     (Integration, ExternalIssue),
-    (NotificationSetting, Actor),
     (User, Actor),
 }
 

tests/sentry/integrations/slack/test_link_team.py
@@ -125,7 +125,7 @@ class SlackIntegrationLinkTeamTest(SlackIntegrationLinkTeamTestBase):
 
         with exempt_from_silo_limits():
             team_settings = NotificationSetting.objects.filter(
-                scope_type=NotificationScopeType.TEAM.value, target=self.team.actor.id
+                scope_type=NotificationScopeType.TEAM.value, target_id=self.team.actor.id
             )
             assert len(team_settings) == 1
 
@@ -213,7 +213,7 @@ class SlackIntegrationUnlinkTeamTest(SlackIntegrationLinkTeamTestBase):
 
         with exempt_from_silo_limits():
             team_settings = NotificationSetting.objects.filter(
-                scope_type=NotificationScopeType.TEAM.value, target=self.team.actor.id
+                scope_type=NotificationScopeType.TEAM.value, target_id=self.team.actor.id
             )
         assert len(team_settings) == 0
 
@@ -256,7 +256,7 @@ class SlackIntegrationUnlinkTeamTest(SlackIntegrationLinkTeamTestBase):
 
         with exempt_from_silo_limits():
             team_settings = NotificationSetting.objects.filter(
-                scope_type=NotificationScopeType.TEAM.value, target=self.team.actor.id
+                scope_type=NotificationScopeType.TEAM.value, target_id=self.team.actor.id
             )
         assert len(team_settings) == 0
 

tests/sentry/models/test_notificationsetting.py
@@ -1,4 +1,4 @@
-from sentry.models import NotificationSetting
+from sentry.models.notificationsetting import NotificationSetting
 from sentry.models.user import User
 from sentry.notifications.types import (
     NotificationScopeType,
@@ -6,6 +6,7 @@ from sentry.notifications.types import (
     NotificationSettingTypes,
 )
 from sentry.services.hybrid_cloud.actor import RpcActor
+from sentry.tasks.deletion.hybrid_cloud import schedule_hybrid_cloud_foreign_key_jobs
 from sentry.testutils import TestCase
 from sentry.testutils.outbox import outbox_runner
 from sentry.testutils.silo import control_silo_test
@@ -49,6 +50,9 @@ class NotificationSettingTest(TestCase):
         # Deletion is deferred and tasks aren't run in tests.
         with outbox_runner():
             self.team.delete()
+
+        with self.tasks():
+            schedule_hybrid_cloud_foreign_key_jobs()
 
         assert_no_notification_settings()
 

commit 6e782879b65a01afe5cdad9db9be493d57287487

ref: don't set target_id when clearing notification settings (#55878)
target_id is always None otherwise when created:
https://github.com/getsentry/sentry/blob/f24b7a071bfc9ca2386d828310fdd536731ad7ac/src/sentry/notifications/manager.py#L105-L113

by setting a target_id this can result in multiple rows for a single
user's notifications. this usually isn't a problem except for
get_settings does not order the results leading to undefined behaviour:
https://github.com/getsentry/sentry/blob/f24b7a071bfc9ca2386d828310fdd536731ad7ac/src/sentry/notifications/manager.py#L69-L71

this can reproduce a test failure with enough tests (which cause
postgres to pack the table differently resulting in a different order).
I've attached a testlist which results in this failure mode by running:

```
pytest $(cat testlist.txt)
```


[testlist.txt](https://github.com/getsentry/sentry/files/12553253/testlist.txt)

eventually failing with:

```
=========================================================== FAILURES ============================================================
_______________________________________ SlackUninstallTest.test_uninstall_slack_and_email _______________________________________
tests/sentry/integrations/slack/test_uninstall.py:97: in test_uninstall_slack_and_email
    self.assert_settings(ExternalProviders.SLACK, NotificationSettingOptionValues.NEVER)
tests/sentry/integrations/slack/test_uninstall.py:72: in assert_settings
    assert self.get_setting(self.user, provider) == value
E   assert <NotificationSettingOptionValues.ALWAYS: 20> == <NotificationSettingOptionValues.NEVER: 10>
E    +  where <NotificationSettingOptionValues.ALWAYS: 20> = <bound method SlackUninstallTest.get_setting of <tests.sentry.integrations.slack.test_uninstall.SlackUninstallTest testMethod=test_uninstall_slack_and_email>>(<User at 0x12dd28520: id=1641>, <ExternalProviders.SLACK: 110>)
E    +    where <bound method SlackUninstallTest.get_setting of <tests.sentry.integrations.slack.test_uninstall.SlackUninstallTest testMethod=test_uninstall_slack_and_email>> = <tests.sentry.integrations.slack.test_uninstall.SlackUninstallTest testMethod=test_uninstall_slack_and_email>.get_setting
E    +    and   <User at 0x12dd28520: id=1641> = <tests.sentry.integrations.slack.test_uninstall.SlackUninstallTest testMethod=test_uninstall_slack_and_email>.user
----------------------------------------------------- Captured stderr call ------------------------------------------------------
%6|1694115645.304|FAIL|rdkafka#producer-1| [thrd:127.0.0.1:9092/bootstrap]: 127.0.0.1:9092/bootstrap: Disconnected while requesting ApiVersion: might be caused by incorrect security.protocol configuration (connecting to a SSL listener?) or broker version is < 0.10 (see api.version.request) (after 0ms in state APIVERSION_QUERY, 3 identical error(s) suppressed)
==================================================== short test summary info ====================================================
FAILED tests/sentry/integrations/slack/test_uninstall.py::SlackUninstallTest::test_uninstall_slack_and_email - assert <NotificationSettingOptionValues.ALWAYS: 20> == <NotificationSettingOptionValues.NEVER: 10>
========================================== 1 failed, 699 passed in 1158.45s (0:19:18) ===========================================
```

after this change the test passes

I also adjusted the repr for `NotificationSetting` since it was broken
by a69b7f0a87d053d3fabfb449fea97840d61f2658 and made this very difficult

src/sentry/models/notificationsetting.py
@@ -130,7 +130,7 @@ class NotificationSetting(Model):
     __repr__ = sane_repr(
         "scope_str",
         "scope_identifier",
-        "target",
+        "target_id",
         "provider_str",
         "type_str",
         "value_str",

src/sentry/notifications/manager.py
@@ -18,7 +18,6 @@ from django.db.models import Q, QuerySet
 
 from sentry import analytics
 from sentry.db.models.manager import BaseManager
-from sentry.models.actor import get_actor_id_for_user
 from sentry.models.notificationsettingoption import NotificationSettingOption
 from sentry.models.notificationsettingprovider import NotificationSettingProvider
 from sentry.models.team import Team
@@ -649,7 +648,6 @@ class NotificationsManager(BaseManager["NotificationSetting"]):  # noqa: F821
                     type=type.value,
                     scope_type=NotificationScopeType.USER.value,
                     scope_identifier=user.id,
-                    target_id=get_actor_id_for_user(user),
                     user_id=user.id,
                     defaults={"value": NotificationSettingOptionValues.NEVER.value},
                 )