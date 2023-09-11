b190478c20890805b5c9910cc44d8b662fb68633
Colleen O'Rourke <colleen@sentry.io>
2023-09-11 12:43:07 -0700

    feat(alerts): Add feature flag for noisy alert warning (#56036)
    Add a flag for https://github.com/getsentry/sentry/pull/55832/

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1553,6 +1553,8 @@ SENTRY_FEATURES = {
     "organizations:mobile-cpu-memory-in-transactions": False,
     # Enable new page filter UI
     "organizations:new-page-filter": False,
+    # Display warning banner for every event issue alerts
+    "organizations:noisy-alert-warning": False,
     # Prefix host with organization ID when giving users DSNs (can be
     # customized with SENTRY_ORG_SUBDOMAIN_TEMPLATE)
     "organizations:org-subdomains": False,
