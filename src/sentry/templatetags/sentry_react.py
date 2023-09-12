3e6f10500c3545a9fa55b9c9bc00a182ba1acba1
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-09-12 10:29:22 -0700

    rm backend change

--- src/sentry/templatetags/sentry_react.py
+++ src/sentry/templatetags/sentry_react.py
@@ -9,4 +9,5 @@ register = template.Library()
 @register.simple_tag(takes_context=True)
 def get_react_config(context):
     context = get_client_config(context.get("request", None))
+
     return json.dumps_htmlsafe(context)
