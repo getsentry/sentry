e5e77ad6208206aa3a132b8b3c0e688c7f60d573
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2023-09-12 10:20:47 -0700

    fix(actionable-items): Fix error alert check (#56042)
    this pr fixes an issue with checking is there are any error alerts.
    initially it was checking for warnings, and it wasn't changed when i
    moved it to be a check for if there are error alerts

--- static/app/components/events/interfaces/crashContent/exception/actionableItems.tsx
+++ static/app/components/events/interfaces/crashContent/exception/actionableItems.tsx
@@ -433,10 +433,11 @@ export function ActionableItems({event, project, isShare}: ActionableItemsProps)
     });
   };
 
-  const hasErrorAlert = Object.keys(errorMessages).some(error =>
-    ActionableItemWarning.includes(
-      error as ProguardProcessingErrors | NativeProcessingErrors | GenericSchemaErrors
-    )
+  const hasErrorAlert = Object.keys(errorMessages).some(
+    error =>
+      !ActionableItemWarning.includes(
+        error as ProguardProcessingErrors | NativeProcessingErrors | GenericSchemaErrors
+      )
   );
 
   for (const errorKey in Object.keys(errorMessages)) {
