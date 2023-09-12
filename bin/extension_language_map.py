5194dd1c6a8bf191969e49a57b793448d9a50a68
Cathy Teng <cathy.teng@sentry.io>
2023-09-12 13:26:16 -0700

    fix typing

--- bin/extension_language_map.py
+++ bin/extension_language_map.py
@@ -2,6 +2,8 @@
 Generate a mapping from file extensions to language for languages that are part of platforms supported by Sentry.
 """
 
+from typing import Dict
+
 import requests
 import yaml
 
@@ -52,7 +54,7 @@ languages = [
     "hcl",
 ]
 
-EXTENSION_LANGUAGE_MAP = {}
+EXTENSION_LANGUAGE_MAP: Dict[str, str] = {}
 
 yaml_languages = languages_dict.keys()
 
