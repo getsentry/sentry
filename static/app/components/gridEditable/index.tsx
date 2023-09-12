3f671c2972c94c430a8ea80eec57eca35b09b97c
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-09-12 10:30:17 -0700

    rm extra filechange

--- static/app/components/gridEditable/index.tsx
+++ static/app/components/gridEditable/index.tsx
@@ -65,6 +65,7 @@ type GridEditableProps<DataRow, ColumnKey> = {
   columnOrder: GridColumnOrder<ColumnKey>[];
   columnSortBy: GridColumnSortBy<ColumnKey>[];
   data: DataRow[];
+
   /**
    * GridEditable allows the parent component to determine how to display the
    * data within it. Note that this is optional.
