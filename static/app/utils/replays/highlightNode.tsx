b1e963d69f46e97f8cdd445f1d48c39c806ab6f8
Billy Vong <billy@sentry.io>
2023-09-12 12:57:51 -0400

    format

--- static/app/utils/replays/highlightNode.tsx
+++ static/app/utils/replays/highlightNode.tsx
@@ -82,7 +82,7 @@ export function highlightNode(replayer: Replayer, props: AddHighlightParams) {
   // want on top of the replay. We may need to revisit this strategy as we
   // create a new canvas for every highlight. See additional notes in
   // removeHighlight() method.
-  const element = node.nodeType === Node.ELEMENT_NODE ? node as HTMLElement : null;
+  const element = node.nodeType === Node.ELEMENT_NODE ? (node as HTMLElement) : null;
 
   if (!element) {
     return null;
