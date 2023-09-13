ad39545cee5ec1ef2c35e4289697c4cf7a76ed74
Edward Gou <edward@kwhdyt9ywf.lan>
2023-09-13 11:53:12 -0400

    Adds an initial web vitals/page loads module to starfish side nav

---
+++ static/app/views/performance/browser/webVitals/utils/types.tsx
@@ -0,0 +1,9 @@
+export type Row = {
+  'count()': number;
+  'p75(measurements.app_init_long_tasks)': number;
+  'p75(measurements.cls)': number;
+  'p75(measurements.fcp)': number;
+  'p75(measurements.lcp)': number;
+  transaction: string;
+  'transaction.op': string;
+};
---
+++ static/app/views/performance/browser/webVitals/webVitalsLandingPage.tsx
@@ -0,0 +1,70 @@
+import Breadcrumbs from 'sentry/components/breadcrumbs';
+import DatePageFilter from 'sentry/components/datePageFilter';
+import FeatureBadge from 'sentry/components/featureBadge';
+import * as Layout from 'sentry/components/layouts/thirds';
+import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
+import ProjectPageFilter from 'sentry/components/projectPageFilter';
+import {t} from 'sentry/locale';
+import useOrganization from 'sentry/utils/useOrganization';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import {normalizeUrl} from 'sentry/utils/withDomainRequired';
+import {PagePerformanceTables} from 'sentry/views/performance/browser/webVitals/pagePerformanceTables';
+import {PerformanceScoreChart} from 'sentry/views/performance/browser/webVitals/performanceScoreChart';
+import {calculatePerformanceScore} from 'sentry/views/performance/browser/webVitals/utils/calculatePerformanceScore';
+import {useProjectWebVitalsQuery} from 'sentry/views/performance/browser/webVitals/utils/useProjectWebVitalsQuery';
+import WebVitalMeters from 'sentry/views/performance/browser/webVitals/webVitalMeters';
+import {ModulePageProviders} from 'sentry/views/performance/database/modulePageProviders';
+
+export default function WebVitalsLandingPage() {
+  const organization = useOrganization();
+  const pageFilters = usePageFilters();
+
+  const {data: projectData} = useProjectWebVitalsQuery({pageFilters});
+
+  const projectScore = calculatePerformanceScore({
+    'p75(measurements.lcp)': projectData?.data[0]['p75(measurements.lcp)'] as number,
+    'p75(measurements.fcp)': projectData?.data[0]['p75(measurements.fcp)'] as number,
+    'p75(measurements.cls)': projectData?.data[0]['p75(measurements.cls)'] as number,
+    'p75(measurements.app_init_long_tasks)': projectData?.data[0][
+      'p75(measurements.app_init_long_tasks)'
+    ] as number,
+  });
+
+  return (
+    <ModulePageProviders title={[t('Performance'), t('Web Vitals')].join(' — ')}>
+      <Layout.Header>
+        <Layout.HeaderContent>
+          <Breadcrumbs
+            crumbs={[
+              {
+                label: 'Performance',
+                to: normalizeUrl(`/organizations/${organization.slug}/performance/`),
+                preservePageFilters: true,
+              },
+              {
+                label: 'Web Vitals',
+              },
+            ]}
+          />
+
+          <Layout.Title>
+            {t('Web Vitals')}
+            <FeatureBadge type="alpha" />
+          </Layout.Title>
+        </Layout.HeaderContent>
+      </Layout.Header>
+
+      <Layout.Body>
+        <Layout.Main fullWidth>
+          <PageFilterBar condensed>
+            <ProjectPageFilter />
+            <DatePageFilter alignDropdown="left" />
+          </PageFilterBar>
+          <PerformanceScoreChart projectScore={projectScore} />
+          <WebVitalMeters projectData={projectData} projectScore={projectScore} />
+          <PagePerformanceTables />
+        </Layout.Main>
+      </Layout.Body>
+    </ModulePageProviders>
+  );
+}
