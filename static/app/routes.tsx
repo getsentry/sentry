ee5d5122cfef945e322243ddb3480d7a35885f0a
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-09-14 13:11:04 -0700

    feat(replays): empty list pages for rage and dead click selectors (#56274)
    ## The vision:
    
    - These are the pages the user will see once they click "show more" on
    the widgets (on the Replays Index page).
    - It will be populated with a list of all selectors with rage/dead
    clicks.
    - Clicking on a specific selector row will route back to the Replays
    Index page, with the search bar filter set.
    
    
    <img width="1451" alt="SCR-20230914-kebx"
    src="https://github.com/getsentry/sentry/assets/56095982/10173e5e-fb06-42e4-9444-d83716c83f07">
    <img width="1362" alt="SCR-20230914-kjts"
    src="https://github.com/getsentry/sentry/assets/56095982/ce491092-f4d5-4d8d-88e9-af6b922c6d33">
    
    
    Orgs who don't fall under the feature flag will see this:
    <img width="1419" alt="SCR-20230914-kujc"
    src="https://github.com/getsentry/sentry/assets/56095982/f2d0c302-1551-4291-aba9-8d1d29e3ef00">
    
    Closes 
    https://github.com/getsentry/team-replay/issues/181

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -1363,6 +1363,14 @@ function buildRoutes() {
     <Fragment>
       <IndexRoute component={make(() => import('sentry/views/replays/list'))} />
       <Route
+        path="dead-clicks/"
+        component={make(() => import('sentry/views/replays/deadClickList'))}
+      />
+      <Route
+        path="rage-clicks/"
+        component={make(() => import('sentry/views/replays/rageClickList'))}
+      />
+      <Route
         path=":replaySlug/"
         component={make(() => import('sentry/views/replays/details'))}
       />
