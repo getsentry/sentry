776f376f03d5ccb0b568d04e7ace4566c0fc4406
Matt Gaunt-Seo <matt.gaunt@sentry.io>
2023-09-14 16:05:13 -0700

    chore(test): Add requires_snuba fixture

--- tests/sentry/api/endpoints/test_team_groups_old.py
+++ tests/sentry/api/endpoints/test_team_groups_old.py
@@ -3,6 +3,9 @@ from datetime import datetime, timedelta, timezone
 from sentry.models import GroupAssignee, GroupEnvironment, GroupStatus
 from sentry.testutils.cases import APITestCase
 from sentry.testutils.silo import region_silo_test
+from sentry.testutils.skips import requires_snuba
+
+pytestmark = [requires_snuba]
 
 
 @region_silo_test(stable=True)
