f4fbca5d88aa2b31add1dde94b5899ce28075eee
Enoch Tang <enoch.tang@sentry.io>
2023-09-16 15:40:14 -0400

    use new setter methods

--- src/sentry/snuba/metrics_layer/metrics_layer.py
+++ src/sentry/snuba/metrics_layer/metrics_layer.py
@@ -22,24 +22,39 @@ def run_query(request: Request, tenant_ids: Optional[Dict[str, Any]] = None):
         tenant_ids["use_case_id"] = metrics_query.scope.use_case_id
 
     # process intervals
+    assert metrics_query.rollup is not None
     if metrics_query.rollup.interval:
         start, end, _num_intervals = to_intervals(
             metrics_query.start, metrics_query.end, metrics_query.rollup.interval
         )
-        metrics_query.set_start(start)
-        metrics_query.set_end(end)
+        metrics_query = metrics_query.set_start(start)
+        metrics_query = metrics_query.set_end(end)
+
+    # TODO: support for multiple entity and ORDER BY problem
 
     # Resolve all MRIs in metrics_query
+    assert metrics_query.query is not None
     metric = metrics_query.query.metric
     op = metrics_query.query.aggregate
     scope = metrics_query.scope
     if not metric.public_name and metric.mri:
         public_name = get_public_name_from_mri(metric.mri)
-        metric.public_name = public_name
+        metrics_query = metrics_query.set_query(
+            metrics_query.query.set_metric(metric.set_public_name(public_name))
+        )
     if not metric.mri and metric.public_name:
         mri = get_mri(metric.public_name)
-        metric.mri = mri
+        metrics_query = metrics_query.set_query(metrics_query.query.set_metric(metric.set_mri(mri)))
 
     metrics_object = metric_object_factory(op, metric.public_name)
     assert isinstance(metrics_object, RawMetric)  # only support raw metrics for now
-    metrics_object.generate_metric_ids(scope.project_ids, scope.use_case_id)
+    metric_id = metrics_object.generate_metric_ids(scope.project_ids, scope.use_case_id)[
+        0
+    ]  # for raw metrics, there will always only be one metric_id
+    metrics_query = metrics_query.set_query(
+        metrics_query.query.set_metric(metric.set_id(metric_id))
+    )
+
+    # resolve entity and dataset
+
+    # build referrer string
