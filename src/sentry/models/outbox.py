7d798f3d271d23d3de811ccc5cdb7bda31212594
Zachary Collins <zachary.collins@sentry.io>
2023-09-16 02:06:03 -0700

    ref(hc): Generalize replication logic

--- src/sentry/models/outbox.py
+++ src/sentry/models/outbox.py
@@ -243,6 +243,8 @@ class OutboxCategory(IntEnum):
                     shard_identifier = model.id
                 elif hasattr(model, "organization_id"):
                     shard_identifier = model.organization_id
+                elif hasattr(model, "auth_provider_id"):
+                    shard_identifier = model.auth_provider_id
             if scope == OutboxScope.USER_SCOPE:
                 if isinstance(model, User):
                     shard_identifier = model.id
@@ -281,8 +283,9 @@ class OutboxScope(IntEnum):
             OutboxCategory.POST_ORGANIZATION_PROVISION,
             OutboxCategory.DISABLE_AUTH_PROVIDER,
             OutboxCategory.ORGANIZATION_MAPPING_CUSTOMER_ID_UPDATE,
-            OutboxCategory.AUTH_PROVIDER_UPDATE,
             OutboxCategory.TEAM_UPDATE,
+            OutboxCategory.AUTH_PROVIDER_UPDATE,
+            OutboxCategory.AUTH_IDENTITY_UPDATE,
         },
     )
     USER_SCOPE = scope_categories(
@@ -292,7 +295,6 @@ class OutboxScope(IntEnum):
             OutboxCategory.UNUSED_ONE,
             OutboxCategory.UNUSED_TWO,
             OutboxCategory.UNUSUED_THREE,
-            OutboxCategory.AUTH_IDENTITY_UPDATE,
         },
     )
     WEBHOOK_SCOPE = scope_categories(2, {OutboxCategory.WEBHOOK_PROXY})
--- src/sentry/services/hybrid_cloud/replica/impl.py
+++ src/sentry/services/hybrid_cloud/replica/impl.py
@@ -1,11 +1,93 @@
-from django.db import router, transaction
+from typing import Any, Iterator, List, Mapping, Type, Union
 
+from django.db import router, transaction
+from django.db.models import Model
+
+from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
+from sentry.db.models.outboxes import ReplicatedControlModel, ReplicatedRegionModel
 from sentry.db.postgres.transactions import enforce_constraints
-from sentry.models import AuthIdentityReplica, AuthProviderReplica, Organization
+from sentry.models import (
+    AuthIdentity,
+    AuthIdentityReplica,
+    AuthProvider,
+    AuthProviderReplica,
+    Organization,
+    OutboxCategory,
+    Team,
+    User,
+)
 from sentry.models.teamreplica import TeamReplica
 from sentry.services.hybrid_cloud.auth import RpcAuthIdentity, RpcAuthProvider
 from sentry.services.hybrid_cloud.organization import RpcTeam
 from sentry.services.hybrid_cloud.replica.service import ControlReplicaService, RegionReplicaService
+
+
+def get_foreign_key_column(
+    destination: Model,
+    *source_models: Type[Model],
+) -> str:
+    destination_model: Type[Model] = type(destination)
+    for field in destination_model._meta.get_fields():
+        if isinstance(field, HybridCloudForeignKey):
+            if field.foreign_model in source_models:
+                return field.name
+    raise TypeError(
+        f"replication to {destination_model} lacking required HybridCloudForeignKey to {source_models}"
+    )
+
+
+def get_conflicting_unique_columns(
+    destination: Model,
+    category: OutboxCategory,
+) -> Iterator[List[str]]:
+    destination_model: Type[Model] = type(destination)
+
+    if not destination_model._meta.unique_together:
+        return
+
+    scope = category.get_scope()
+    scope_controlled_column: str
+    if scope == scope.USER_SCOPE:
+        scope_controlled_column = get_foreign_key_column(destination, User)
+    elif scope == scope.ORGANIZATION_SCOPE:
+        scope_controlled_column = get_foreign_key_column(destination, Organization, AuthProvider)
+    else:
+        raise TypeError(
+            f"replication to {destination_model} includes unique index that is not scoped by shard!"
+        )
+
+    for columns in destination_model._meta.unique_together:
+        if scope_controlled_column not in columns:
+            raise TypeError(
+                f"replication to {destination_model} includes unique index that is not scoped by shard: {columns}!"
+            )
+        yield list(columns)
+
+
+def handle_replication(
+    source_model: Union[Type[ReplicatedControlModel], Type[ReplicatedRegionModel]],
+    destination: Model,
+):
+    category: OutboxCategory = source_model.category
+    destination_model: Type[Model] = type(destination)
+    fk = get_foreign_key_column(destination, source_model)
+    dest_filter: Mapping[str, Any] = {fk: getattr(destination, fk)}
+
+    with enforce_constraints(transaction.atomic(router.db_for_write(destination_model))):
+        for columns in get_conflicting_unique_columns(destination, category):
+            destination_model.objects.filter(
+                **{c: getattr(destination, c) for c in columns}
+            ).exclude(**dest_filter).delete()
+        existing = destination_model.objects.filter(**dest_filter).first()
+        if existing:
+            update: Mapping[str, Any] = {
+                field.name: getattr(destination, field.name)
+                for field in destination_model._meta.get_fields()
+                if field.editable and field.name not in ("id", "date_added")
+            }
+            existing.update(**update)
+        else:
+            destination.save()
 
 
 class DatabaseBackedRegionReplicaService(RegionReplicaService):
@@ -13,82 +95,48 @@ class DatabaseBackedRegionReplicaService(RegionReplicaService):
         self, *, auth_provider: RpcAuthProvider, region_name: str
     ) -> None:
         try:
-            with enforce_constraints(transaction.atomic(router.db_for_write(AuthProviderReplica))):
-                organization = Organization.objects.get(id=auth_provider.organization_id)
-                # Deletes do not cascade immediately -- if we removed and add a new provider
-                # we should just clear that older provider.
-                AuthProviderReplica.objects.filter(organization=organization).exclude(
-                    auth_provider_id=auth_provider.id
-                ).delete()
-                existing = AuthProviderReplica.objects.filter(
-                    auth_provider_id=auth_provider.id
-                ).first()
-                update = {
-                    "organization": organization,
-                    "provider": auth_provider.provider,
-                    "config": auth_provider.config,
-                    "default_role": auth_provider.default_role,
-                    "default_global_access": auth_provider.default_global_access,
-                    "allow_unlinked": auth_provider.flags.allow_unlinked,
-                    "scim_enabled": auth_provider.flags.scim_enabled,
-                }
-
-                if not existing:
-                    AuthProviderReplica.objects.create(auth_provider_id=auth_provider.id, **update)
-                    return
-
-                existing.update(**update)
+            organization = Organization.objects.get(id=auth_provider.organization_id)
         except Organization.DoesNotExist:
             return
+
+        destination = AuthProviderReplica(
+            auth_provider_id=auth_provider.id,
+            provider=auth_provider.provider,
+            organization_id=organization.id,
+            config=auth_provider.config,
+            default_role=auth_provider.default_role,
+            default_global_access=auth_provider.default_global_access,
+            allow_unlinked=auth_provider.flags.allow_unlinked,
+            scim_enabled=auth_provider.flags.scim_enabled,
+        )
+
+        handle_replication(AuthProvider, destination)
 
     def upsert_replicated_auth_identity(
         self, *, auth_identity: RpcAuthIdentity, region_name: str
     ) -> None:
-        with enforce_constraints(transaction.atomic(router.db_for_write(AuthIdentityReplica))):
-            # Since coalesced outboxes won't replicate the precise ordering of changes, these
-            # unique keys can cause a deadlock in updates.  To address this, we just delete
-            # any conflicting items and allow future outboxes to carry the updates
-            # for the auth identities that should follow (given they will share the same shard).
-            AuthIdentityReplica.objects.filter(
-                ident=auth_identity.ident,
-                auth_provider_id=auth_identity.auth_provider_id,
-            ).exclude(auth_identity_id=auth_identity.id).delete()
-            AuthIdentityReplica.objects.filter(
-                user_id=auth_identity.user_id,
-                auth_provider_id=auth_identity.auth_provider_id,
-            ).exclude(auth_identity_id=auth_identity.id).delete()
 
-            existing = AuthIdentityReplica.objects.filter(auth_identity_id=auth_identity.id).first()
-            update = {
-                "user_id": auth_identity.user_id,
-                "auth_provider_id": auth_identity.auth_provider_id,
-                "ident": auth_identity.ident,
-                "data": auth_identity.data,
-                "last_verified": auth_identity.last_verified,
-            }
+        destination = AuthIdentityReplica(
+            auth_identity_id=auth_identity.id,
+            user_id=auth_identity.user_id,
+            auth_provider_id=auth_identity.auth_provider_id,
+            ident=auth_identity.ident,
+            data=auth_identity.data,
+            last_verified=auth_identity.last_verified,
+        )
 
-            if not existing:
-                AuthIdentityReplica.objects.create(auth_identity_id=auth_identity.id, **update)
-                return
-
-            existing.update(**update)
+        handle_replication(AuthIdentity, destination)
 
 
 class DatabaseBackedControlReplicaService(ControlReplicaService):
     def upsert_replicated_team(self, *, team: RpcTeam) -> None:
-        with enforce_constraints(transaction.atomic(router.db_for_write(TeamReplica))):
-            # Handle unique index -- since TEAM_UPDATEs use the Organization scope, this is safe.
-            TeamReplica.objects.filter(
-                organization_id=team.organization_id, slug=team.slug
-            ).exclude(team_id=team.id).delete()
+        destination = TeamReplica(
+            team_id=team.id,
+            organization_id=team.organization_id,
+            slug=team.slug,
+            name=team.name,
+            status=team.status,
+            org_role=team.org_role,
+        )
 
-            TeamReplica.objects.create_or_update(
-                team_id=team.id,
-                values=dict(
-                    organization_id=team.organization_id,
-                    slug=team.slug,
-                    name=team.name,
-                    status=team.status,
-                    org_role=team.org_role,
-                ),
-            )
+        handle_replication(Team, destination)
