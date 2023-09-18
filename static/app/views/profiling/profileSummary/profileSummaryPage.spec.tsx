87e0be01bfa37201383f0e2eda4185c7a5b8e5cb
JonasBa <jonas@badalic.com>
2023-09-18 10:07:26 +0200

    feat(profiling): rename old view and init new

---
+++ static/app/views/profiling/profileSummary/profileSummaryPage.spec.tsx
@@ -0,0 +1,56 @@
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import OrganizationStore from 'sentry/stores/organizationStore';
+import ProfileSummaryPage from 'sentry/views/profiling/profileSummary';
+
+describe('ProfileSummaryPage', () => {
+  it('renders legacy page', async () => {
+    const organization = TestStubs.Organization({
+      features: [],
+      projects: [TestStubs.Project()],
+    });
+    OrganizationStore.onUpdate(organization);
+
+    MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/projects/`,
+      body: [TestStubs.Project()],
+    });
+
+    MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/profiling/filters/`,
+      body: [],
+    });
+
+    render(
+      <ProfileSummaryPage
+        params={{}}
+        selection={TestStubs.GlobalSelection()}
+        location={TestStubs.location()}
+      />,
+      {
+        organization,
+        context: TestStubs.routerContext(),
+      }
+    );
+
+    expect(await screen.findByTestId(/profile-summary-legacy/i)).toBeInTheDocument();
+  });
+
+  it('renders new page', () => {
+    render(
+      <ProfileSummaryPage
+        params={{}}
+        selection={TestStubs.GlobalSelection()}
+        location={TestStubs.location()}
+      />,
+      {
+        organization: TestStubs.Organization({
+          features: ['profiling-summary-redesign'],
+        }),
+        context: TestStubs.routerContext(),
+      }
+    );
+
+    expect(screen.getByTestId(/profile-summary-redesign/i)).toBeInTheDocument();
+  });
+});
