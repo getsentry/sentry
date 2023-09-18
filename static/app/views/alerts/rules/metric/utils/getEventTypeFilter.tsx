5336b8c9383ed7e4446b2a8ce47d798b30210ffa
Scott Cooper <scttcper@gmail.com>
2023-09-18 16:47:50 -0700

    feat(alerts): Wrap metric alert query with "AND" (#56307)

--- static/app/views/alerts/rules/metric/utils/getEventTypeFilter.tsx
+++ static/app/views/alerts/rules/metric/utils/getEventTypeFilter.tsx
@@ -3,7 +3,7 @@ import {convertDatasetEventTypesToSource} from 'sentry/views/alerts/utils';
 import {DATASET_EVENT_TYPE_FILTERS, DATASOURCE_EVENT_TYPE_FILTERS} from '../constants';
 import {Dataset, Datasource, EventTypes, MetricRule} from '../types';
 
-export function extractEventTypeFilterFromRule(metricRule: MetricRule) {
+export function extractEventTypeFilterFromRule(metricRule: MetricRule): string {
   const {dataset, eventTypes} = metricRule;
   return getEventTypeFilter(dataset, eventTypes);
 }
@@ -11,7 +11,7 @@ export function extractEventTypeFilterFromRule(metricRule: MetricRule) {
 export function getEventTypeFilter(
   dataset: Dataset,
   eventTypes: EventTypes[] | undefined
-) {
+): string {
   if (eventTypes) {
     return DATASOURCE_EVENT_TYPE_FILTERS[
       convertDatasetEventTypesToSource(dataset, eventTypes) ?? Datasource.ERROR
