9b879e9092265989d2c97668f7f02b9b025caec5
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-09-18 14:07:35 -0700

    use useLegacyStore in theme switcher button

--- static/app/components/stories/themeSwitcher.tsx
+++ static/app/components/stories/themeSwitcher.tsx
@@ -1,32 +1,21 @@
-import {useCallback, useEffect, useState} from 'react';
+import {useCallback} from 'react';
 
 import {Button} from 'sentry/components/button';
 import {t} from 'sentry/locale';
 import ConfigStore from 'sentry/stores/configStore';
+import {useLegacyStore} from 'sentry/stores/useLegacyStore';
 
 export default function ThemeSwitcher() {
-  const [theme, setTheme] = useState(() => ConfigStore.get('theme'));
+  const config = useLegacyStore(ConfigStore);
+  const isDark = config.theme === 'dark';
 
-  const isDark = theme === 'dark';
-  const label = isDark ? t('Switch to Light Mode') : t('Switch to Dark Mode');
   const handleClick = useCallback(() => {
     ConfigStore.set('theme', isDark ? 'light' : 'dark');
   }, [isDark]);
 
-  useEffect(() => {
-    const unsubscribe = ConfigStore.listen(change => {
-      if ('theme' in change) {
-        setTheme(change.theme);
-      }
-    }, {});
-    return () => {
-      unsubscribe();
-    };
-  });
-
   return (
     <Button size="xs" onClick={handleClick}>
-      {label}
+      {isDark ? t('Switch to Light Mode') : t('Switch to Dark Mode')}
     </Button>
   );
 }
