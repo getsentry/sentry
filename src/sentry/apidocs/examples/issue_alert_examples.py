d6a7c60e473bcead7f3a8b8bf92d37a384f507ca
isabellaenriquez <isabella.enriquez@sentry.io>
2023-09-19 13:42:57 -0700

    Update example

--- src/sentry/apidocs/examples/issue_alert_examples.py
+++ src/sentry/apidocs/examples/issue_alert_examples.py
@@ -17,7 +17,6 @@ class IssueAlertExamples:
             value=[
                 {
                     "id": "3",
-                    "name": "High Number of Issues with Production",
                     "conditions": [
                         {
                             "interval": "1h",
@@ -39,7 +38,6 @@ class IssueAlertExamples:
                             "name": "The issue's category is equal to 2",
                         },
                     ],
-                    "filterMatch": "any",
                     "actions": [
                         {
                             "targetType": "Team",
@@ -50,50 +48,21 @@ class IssueAlertExamples:
                         }
                     ],
                     "actionMatch": "any",
-                    "createdBy": {"id": 2435786, "name": "Kitty White", "email": "meow@test.io"},
-                    "dateCreated": "2023-01-15T06:45:34.353346Z",
-                    "environment": "prod",
+                    "filterMatch": "any",
                     "frequency": 60,
+                    "name": "High Number of Issues with Production",
+                    "dateCreated": "2023-01-15T06:45:34.353346Z",
                     "owner": "team:63562",
+                    "createdBy": {
+                        "id": 2435786,
+                        "name": "John Doe",
+                        "email": "john.doe@example.com",
+                    },
+                    "environment": "prod",
                     "projects": ["melody"],
+                    "status": "active",
                     "lastTriggered": "2023-07-15T00:00:00.351236Z",
                     "snooze": False,
-                    "snoozeCreatedBy": None,
-                    "snoozeForEveryone": None,
-                    "status": "active",
-                },
-                {
-                    "id": "6",
-                    "name": "Escalating from Archived",
-                    "conditions": [
-                        {
-                            "id": "sentry.rules.conditions.reappeared_event.ReappearedEventCondition",
-                            "name": "The issue changes state from ignored to unresolved",
-                        }
-                    ],
-                    "filters": [],
-                    "filterMatch": "all",
-                    "actions": [
-                        {
-                            "targetType": "IssueOwners",
-                            "fallthroughType": "AllMembers",
-                            "id": "sentry.mail.actions.NotifyEmailAction",
-                            "targetIdentifier": "",
-                            "name": "Send a notification to IssueOwners and if none can be found then send a notification to AllMembers",
-                        }
-                    ],
-                    "actionMatch": "any",
-                    "createdBy": {"id": 2435, "name": "John Doe", "email": "johndoe@email.com"},
-                    "dateCreated": "2022-09-18T23:27:02.253030Z",
-                    "environment": None,
-                    "frequency": 180,
-                    "owner": "team:63457",
-                    "projects": ["squirrels"],
-                    "lastTriggered": "2023-07-15T00:00:00.351236Z",
-                    "snooze": False,
-                    "snoozeCreatedBy": None,
-                    "snoozeForEveryone": None,
-                    "status": "active",
                 },
             ],
             status_codes=["200"],
@@ -106,7 +75,6 @@ class IssueAlertExamples:
             "Issue alert successfully created",
             value={
                 "id": "1",
-                "name": "Owner Alert",
                 "conditions": [
                     {
                         "id": "sentry.rules.conditions.first_seen_event.FirstSeenEventCondition",
@@ -121,7 +89,6 @@ class IssueAlertExamples:
                         "name": "The issue is assigned to Unassigned",
                     }
                 ],
-                "filterMatch": "all",
                 "actions": [
                     {
                         "targetType": "Member",
@@ -132,17 +99,16 @@ class IssueAlertExamples:
                     }
                 ],
                 "actionMatch": "any",
-                "createdBy": {"id": 24601, "name": "Jean Valjean", "email": "jean@test.io"},
-                "dateCreated": "2023-09-08T20:00:07.244602Z",
-                "environment": None,
+                "filterMatch": "all",
                 "frequency": 1440,
+                "name": "Owner Alert",
+                "dateCreated": "2023-09-08T20:00:07.244602Z",
                 "owner": "team:74234",
+                "createdBy": {"id": 24601, "name": "Jean Valjean", "email": "jean@example.com"},
+                "environment": None,
                 "projects": ["python"],
-                "lastTriggered": None,
-                "snooze": False,
-                "snoozeCreatedBy": None,
-                "snoozeForEveryone": None,
                 "status": "active",
+                "snooze": False,
             },
             status_codes=["201"],
             response_only=True,
