feabebf6b2311f420c86dd20697a6caf89736d64
Scott Cooper <scttcper@gmail.com>
2023-09-18 17:24:45 -0700

    deps(ui): Upgrade to prettier v3, formatting changes (#56356)

--- static/app/views/monitors/utils/getAggregateStatus.spec.tsx
+++ static/app/views/monitors/utils/getAggregateStatus.spec.tsx
@@ -7,7 +7,7 @@ type StatusCounts = [
   ok: number,
   missed: number,
   timeout: number,
-  error: number
+  error: number,
 ];
 
 export function generateEnvMapping(name: string, counts: StatusCounts) {
