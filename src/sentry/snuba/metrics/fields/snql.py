bea516ae06436b73afbdf7d72f7aff6ec2f69f48
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2023-09-19 10:20:09 -0400

    ref(metrics_extraction): Refactor snql factory a bit

--- src/sentry/snuba/metrics/fields/snql.py
+++ src/sentry/snuba/metrics/fields/snql.py
@@ -802,36 +802,50 @@ def max_timestamp(aggregate_filter, org_id, use_case_id, alias=None):
     return timestamp_column_snql("maxIf", aggregate_filter, org_id, use_case_id, alias)
 
 
-def on_demand_failure_rate_snql_factory(aggregate_filter, org_id, use_case_id, alias=None):
+def on_demand_failure_count_snql_factory(
+    aggregate_filter: Function, org_id: int, use_case_id: UseCaseID, alias: str = None
+) -> Function:
+    """Count the number of transactions where the failure tag is set to true."""
+    return (
+        Function(
+            "sumIf",
+            [
+                Column("value"),
+                Function(
+                    "and",
+                    [
+                        Function(
+                            "equals",
+                            [
+                                Column(resolve_tag_key(use_case_id, org_id, "failure")),
+                                resolve_tag_value(use_case_id, org_id, "true"),
+                            ],
+                        ),
+                        aggregate_filter,
+                    ],
+                ),
+            ],
+        ),
+    )
+
+
+def on_demand_failure_rate_snql_factory(
+    aggregate_filter: Function, org_id: int, use_case_id: UseCaseID, alias: str = None
+) -> Function:
+    """Divide the number of transactions that failed from the total."""
     return Function(
         "divide",
         [
-            Function(
-                "sumIf",
-                [
-                    Column("value"),
-                    Function(
-                        "and",
-                        [
-                            Function(
-                                "equals",
-                                [
-                                    Column(resolve_tag_key(use_case_id, org_id, "failure")),
-                                    resolve_tag_value(use_case_id, org_id, "true"),
-                                ],
-                            ),
-                            aggregate_filter,
-                        ],
-                    ),
-                ],
-            ),
+            on_demand_failure_count_snql_factory(aggregate_filter, org_id, use_case_id, alias),
             Function("sumIf", [Column("value"), aggregate_filter]),
         ],
         alias=alias,
     )
 
 
-def on_demand_apdex_snql_factory(aggregate_filter, org_id, use_case_id, alias=None):
+def on_demand_apdex_snql_factory(
+    aggregate_filter: Function, org_id: int, use_case_id: UseCaseID, alias: str = None
+) -> Function:
     # For more information about the formula, check https://docs.sentry.io/product/performance/metrics/#apdex.
 
     satisfactory = Function(
--- src/sentry/snuba/metrics/utils.py
+++ src/sentry/snuba/metrics/utils.py
@@ -273,7 +273,13 @@ OPERATIONS_PERCENTILES = (
     "p95",
     "p99",
 )
-DERIVED_OPERATIONS = (
+OPERATIONS = (
+    "avg",
+    "count_unique",
+    "count",
+    "max",
+    "min",
+    "sum",
     "histogram",
     "rate",
     "count_web_vitals",
@@ -287,19 +293,7 @@ DERIVED_OPERATIONS = (
     # Custom operations used for on demand derived metrics.
     "on_demand_failure_rate",
     "on_demand_apdex",
-)
-OPERATIONS = (
-    (
-        "avg",
-        "count_unique",
-        "count",
-        "max",
-        "min",
-        "sum",
-    )
-    + OPERATIONS_PERCENTILES
-    + DERIVED_OPERATIONS
-)
+) + OPERATIONS_PERCENTILES
 
 DEFAULT_AGGREGATES: Dict[MetricOperationType, Optional[Union[int, List[Tuple[float]]]]] = {
     "avg": None,
