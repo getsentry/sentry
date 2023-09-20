64d0710ae29649e02cffce2f055361c570ab82fd
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2023-09-20 17:36:32 +0200

    endpoint implemented
    no testing done

--- src/sentry/api/endpoints/custom_rules.py
+++ src/sentry/api/endpoints/custom_rules.py
@@ -51,10 +51,12 @@ class CustomRulesInputSerializer(serializers.Serializer):
         # check that the project exists
         invalid_projects = []
 
-        for project_id in data["projects"]:
-            try:
-                Project.objects.get_from_cache(id=project_id)
-            except Project.DoesNotExist:
+        requested_projects = data["projects"]
+        available_projects = {
+            p.id for p in Project.objects.get_many_from_cache(ids=data["projects"])
+        }
+        for project_id in requested_projects:
+            if project_id not in available_projects:
                 invalid_projects.append(f"invalid project id: {project_id}")
 
         if invalid_projects:
@@ -82,6 +84,7 @@ class CustomRulesEndpoint(OrganizationEndpoint):
 
     publish_status = {
         "POST": ApiPublishStatus.EXPERIMENTAL,
+        "GET": ApiPublishStatus.EXPERIMENTAL,
     }
 
     def post(self, request: Request, organization: Organization) -> Response:
@@ -122,18 +125,7 @@ class CustomRulesEndpoint(OrganizationEndpoint):
                 sample_rate=1.0,
             )
 
-            response_data = {
-                "ruleId": rule.external_rule_id,
-                "condition": json.loads(rule.condition),
-                "startDate": rule.start_date.strftime(CUSTOM_RULE_DATE_FORMAT),
-                "endDate": rule.end_date.strftime(CUSTOM_RULE_DATE_FORMAT),
-                "numSamples": rule.num_samples,
-                "sampleRate": rule.sample_rate,
-                "dateAdded": rule.date_added.strftime(CUSTOM_RULE_DATE_FORMAT),
-                "projects": [project.id for project in rule.projects.all()],
-                "orgId": rule.organization_id,
-            }
-            return Response(response_data, status=200)
+            return _rule_to_response(rule)
 
         except DatabaseError:
             return Response(
@@ -151,3 +143,73 @@ class CustomRulesEndpoint(OrganizationEndpoint):
             )
         except ValueError as e:
             return Response({"query": ["Could not convert to rule", str(e)]}, status=400)
+
+    def get(self, request: Request, organization: Organization) -> Response:
+        requested_projects = request.GET.getlist("project")
+        query = request.GET.get("query")
+
+        if requested_projects:
+            org_rule = False
+            invalid_projects = []
+            available_projects = {
+                p.id for p in Project.objects.get_many_from_cache(ids=requested_projects)
+            }
+            for project_id in requested_projects:
+                if project_id not in available_projects:
+                    invalid_projects.append(f"invalid project id: {project_id}")
+
+            if invalid_projects:
+                raise serializers.ValidationError({"projects": invalid_projects})
+        else:
+            # no project specified (it is an org rule)
+            org_rule = True
+
+        try:
+            tokens = parse_search_query(query)
+        except InvalidSearchQuery as e:
+            return Response({"query": [str(e)]}, status=400)
+
+        try:
+            converter = SearchQueryConverter(tokens)
+            condition = converter.convert()
+        except ValueError as e:
+            return Response({"query": ["Could not convert to rule", str(e)]}, status=400)
+
+        rule = CustomDynamicSamplingRule.get_rule_for_org(condition, organization.id)
+
+        if rule is None:
+            return Response(status=204)  # no rule found, nothing to reutrn
+
+        # we have a rule, check to see if the projects match
+
+        if rule.is_org_level:
+            # a rule org covers all projects
+            return _rule_to_response(rule)
+
+        if not rule.is_org_level and org_rule:
+            # we need an org rule and we have a simple rule return not found
+            return Response(status=204)
+
+        # project rule request and project rule found # see if we have all projects
+        available_projects = {p.id for p in rule.projects.all()}
+        for project_id in requested_projects:
+            if project_id not in available_projects:
+                return Response(status=204)
+
+        # the rule covers all projects
+        return _rule_to_response(rule)
+
+
+def _rule_to_response(rule: CustomDynamicSamplingRule) -> Response:
+    response_data = {
+        "ruleId": rule.external_rule_id,
+        "condition": json.loads(rule.condition),
+        "startDate": rule.start_date.strftime(CUSTOM_RULE_DATE_FORMAT),
+        "endDate": rule.end_date.strftime(CUSTOM_RULE_DATE_FORMAT),
+        "numSamples": rule.num_samples,
+        "sampleRate": rule.sample_rate,
+        "dateAdded": rule.date_added.strftime(CUSTOM_RULE_DATE_FORMAT),
+        "projects": [project.id for project in rule.projects.all()],
+        "orgId": rule.organization.id,
+    }
+    return Response(response_data, status=200)
