07a3951d6381f2d2fa342af8c2faf9c03a7da602
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-09-20 16:10:36 -0700

    remove storylist because of future maintenance

--- static/app/components/logoSentry.stories.tsx
+++ static/app/components/logoSentry.stories.tsx
@@ -1,11 +1,8 @@
 import LogoSentry from 'sentry/components/logoSentry';
 import SizingWindow from 'sentry/components/stories/sizingWindow';
-import StoryList from 'sentry/components/stories/storyList';
 import storyBook from 'sentry/stories/storyBook';
 
 export default storyBook('LogoSentry', story => {
-  story('See Also', () => <StoryList links={['app/icons/icons.stories.tsx']} />);
-
   story('Default', () => (
     <SizingWindow>
       <LogoSentry />
