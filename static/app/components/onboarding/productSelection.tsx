a071d6af7926765f95f0df71849a91c1f22a4e97
Arthur Knaus <arthur.knaus@sentry.io>
2023-09-20 14:50:30 +0200

    fix(platform): remove comment + icon story

--- static/app/components/onboarding/productSelection.tsx
+++ static/app/components/onboarding/productSelection.tsx
@@ -119,7 +119,6 @@ export const platformProductAvailability = {
   'node-koa': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   php: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   'php-laravel': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
-  // TODO(arthur): cleanup naming missmatch between php-symfony and php-symfony2
   ['php-symfony']: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   python: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
   'python-aiohttp': [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
