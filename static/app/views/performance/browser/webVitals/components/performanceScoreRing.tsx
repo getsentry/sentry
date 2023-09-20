ce873e73de1e779f054d340a6adf92c9b48c2a51
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2023-09-20 15:08:16 -0400

    feat(starfish): Updates perf score chart to show breakdown (#56506)
    Updates perf score chart to show breakdown

--- static/app/views/performance/browser/webVitals/components/performanceScoreRing.tsx
+++ static/app/views/performance/browser/webVitals/components/performanceScoreRing.tsx
@@ -158,9 +158,7 @@ const RingBar = styled('circle')<{
   stroke-dasharray: ${p => p.circumference} ${p => p.circumference};
   transform: rotate(${p => p.rotate}deg);
   transform-origin: 50% 50%;
-  transition:
-    stroke-dashoffset 200ms,
-    stroke 100ms;
+  transition: stroke-dashoffset 200ms, stroke 100ms;
 `;
 
 export default PerformanceScoreRing;
