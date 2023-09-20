333a24f7838fe841f1007dddfc7f6f4356915187
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-09-20 10:00:51 -0700

    migration(crons): Add MonitorIncident table (#56435)
    Adds `MonitorIncidentTable` to keep track of open monitor incidents +
    display them on the timeline view.
    
    If a `failure` threshold is set, we will open an `incident` and tag it
    with the appropriate `hash` and reference the `checkin` that caused it
    to pass the threshold.
    
    If a `recovery` threshold is set, we will find any `incident` with the
    appropriate `hash` and set it as the `closing_checkin`.
    
    In the future, if no thresholds are set, we will move to this behavior.
    But for now, we will keep existing behavior as-is.
    
    The `thresholds` can be derived from the `checkin` configs, though we
    may want to add them as top level fields.

--- src/sentry/monitors/models.py
+++ src/sentry/monitors/models.py
@@ -591,3 +591,25 @@ def check_monitor_environment_limits(sender, instance, **kwargs):
         raise MonitorEnvironmentLimitsExceeded(
             f"You may not exceed {settings.MAX_ENVIRONMENTS_PER_MONITOR} environments per monitor"
         )
+
+
+@region_silo_only_model
+class MonitorIncident(Model):
+    __relocation_scope__ = RelocationScope.Excluded
+
+    monitor = FlexibleForeignKey("sentry.Monitor")
+    monitor_environment = FlexibleForeignKey("sentry.MonitorEnvironment")
+    starting_checkin = FlexibleForeignKey(
+        "sentry.MonitorCheckIn", null=True, related_name="created_incidents"
+    )
+    starting_timestamp = models.DateTimeField(null=True)
+    resolving_checkin = FlexibleForeignKey(
+        "sentry.MonitorCheckIn", null=True, related_name="resolved_incidents"
+    )
+    resolving_timestamp = models.DateTimeField(null=True)
+    grouphash = models.CharField(max_length=32)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_monitorincident"
