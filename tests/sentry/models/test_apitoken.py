getsentry/sentry

commit 80c5f777a2986bebfa0190920b90ad2fbf3cff5e

ref(hc):  RPC in tests uses test client and hits rpc endpoint (#53237)
RPC Services use the django test client in tests rather than falling
back to local service.
Several refactors and lots of code removal related to this in addition
to fixing several service issues.

---------

Co-authored-by: Ryan Skonnord <ryan.skonnord@sentry.io>
Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

src/sentry/models/apitoken.py
@@ -7,6 +7,7 @@ from django.db import models, router, transaction
 from django.utils import timezone
 from django.utils.encoding import force_str
 
+from sentry.constants import SentryAppStatus
 from sentry.db.models import (
     BaseManager,
     FlexibleForeignKey,
@@ -76,6 +77,27 @@ class ApiToken(Model, HasApiScopes):
             expires_at = timezone.now() + DEFAULT_EXPIRATION
 
         self.update(token=generate_token(), refresh_token=generate_token(), expires_at=expires_at)
+
+    @property
+    def organization_id(self) -> int | None:
+        from sentry.models import SentryAppInstallation, SentryAppInstallationToken
+
+        try:
+            installation = SentryAppInstallation.objects.get_by_api_token(self.id).get()
+        except SentryAppInstallation.DoesNotExist:
+            return None
+
+        # TODO(nisanthan): Right now, Internal Integrations can have multiple ApiToken, so we use the join table `SentryAppInstallationToken` to map the one to many relationship. However, for Public Integrations, we can only have 1 ApiToken per installation. So we currently don't use the join table for Public Integrations. We should update to make records in the join table for Public Integrations so that we can have a common abstraction for finding an installation by ApiToken.
+        if installation.sentry_app.status == SentryAppStatus.INTERNAL:
+            try:
+                install_token = SentryAppInstallationToken.objects.select_related(
+                    "sentry_app_installation"
+                ).get(api_token_id=self.id)
+            except SentryAppInstallationToken.DoesNotExist:
+                return None
+            return install_token.sentry_app_installation.organization_id
+
+        return installation.organization_id
 
 
 def is_api_token_auth(auth: object) -> bool:

commit 5f03783f87877681a59d4ebdb88ce267a247124c

fix(apitoken): Get correct organization ID from internal integration tokens (#56532)
Fixes https://github.com/getsentry/sentry/issues/56618

## Problem

Requests to fetch issues using internal integration tokens work
inconsistently. The first token created when the app is created will
work as intended, but subsequent tokens created in that app will have
different behavior. For example, issues returned from the API will have
a correct `permalink` property for the first token, but it will be null
when fetched by subsequent tokens.

## Why is this happening?

In the Group model, we can see that `authorized` is being marked as
False, which causes the null value:


https://github.com/getsentry/sentry/blob/ba3cbe9605bd2ee553e1d22ff3ace470cda62b90/src/sentry/api/serializers/models/group.py#L735-L740

Looking at the logic where `authorized` is determined,
`token_has_org_access` must be returning False for the subsequent
internal integration tokens.


https://github.com/getsentry/sentry/blob/8e39468b9a658bcff317f63709eec0971357f349/src/sentry/api/serializers/models/group.py#L754-L755

`AuthenticatedToken` simply checks if `organization_id` matches:


https://github.com/getsentry/sentry/blob/8e39468b9a658bcff317f63709eec0971357f349/src/sentry/services/hybrid_cloud/auth/model.py#L203-L204

Which comes from the computed property on the ApiToken model:


https://github.com/getsentry/sentry/blob/8e39468b9a658bcff317f63709eec0971357f349/src/sentry/models/apitoken.py#L90-L108

So it appears that the
`SentryAppInstallation.objects.get_by_api_token(self.id)` doesn't work
correctly for subsequent internal integration tokens, since they are not
linked directly to the SentryAppInstallation model. The relationship for
them is defined in the SentryAppInstallationToken model, the logic for
which already exists below but isn't hit due to the early return
statement.

I believe this most likely regressed in
https://github.com/getsentry/sentry/pull/53237

src/sentry/models/apitoken.py
@@ -112,10 +112,10 @@ class ApiToken(Model, HasApiScopes):
         try:
             installation = SentryAppInstallation.objects.get_by_api_token(self.id).get()
         except SentryAppInstallation.DoesNotExist:
-            return None
+            installation = None
 
         # TODO(nisanthan): Right now, Internal Integrations can have multiple ApiToken, so we use the join table `SentryAppInstallationToken` to map the one to many relationship. However, for Public Integrations, we can only have 1 ApiToken per installation. So we currently don't use the join table for Public Integrations. We should update to make records in the join table for Public Integrations so that we can have a common abstraction for finding an installation by ApiToken.
-        if installation.sentry_app.status == SentryAppStatus.INTERNAL:
+        if not installation or installation.sentry_app.status == SentryAppStatus.INTERNAL:
             try:
                 install_token = SentryAppInstallationToken.objects.select_related(
                     "sentry_app_installation"

tests/sentry/models/test_apitoken.py
@@ -3,6 +3,7 @@ from datetime import timedelta
 from django.utils import timezone
 
 from sentry.models import ApiToken
+from sentry.models.integrations.sentry_app_installation import SentryAppInstallation
 from sentry.testutils.cases import TestCase
 from sentry.testutils.silo import region_silo_test
 
@@ -28,3 +29,24 @@ class ApiTokenTest(TestCase):
 
         token = ApiToken(scope_list=["project:read"])
         assert token.get_scopes() == ["project:read"]
+
+
+class ApiTokenInternalIntegrationTest(TestCase):
+    def setUp(self):
+        self.user = self.create_user()
+        self.proxy = self.create_user()
+        self.org = self.create_organization()
+        self.internal_app = self.create_internal_integration(
+            name="Internal App",
+            organization=self.org,
+        )
+        self.install = SentryAppInstallation.objects.get(sentry_app=self.internal_app)
+
+    def test_multiple_tokens_have_correct_organization_id(self):
+        # First token is created automatically with the application
+        token_1 = self.internal_app.installations.first().api_token
+        # Second token is created manually and isn't directly linked from the SentryAppInstallation model
+        token_2 = self.create_internal_integration_token(install=self.install, user=self.user)
+
+        assert token_1.organization_id == self.org.id
+        assert token_2.organization_id == self.org.id