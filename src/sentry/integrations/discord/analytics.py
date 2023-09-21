9f27b642a3dc94c1deace8df71dd19022768138c
Scott Cooper <scttcper@gmail.com>
2023-09-21 11:27:47 -0700

    fix(alerts): Add additional discord notification analytics (#56522)

--- src/sentry/integrations/discord/analytics.py
+++ src/sentry/integrations/discord/analytics.py
@@ -7,6 +7,7 @@ class DiscordIntegrationNotificationSent(analytics.Event):
     attributes = (
         analytics.Attribute("organization_id"),
         analytics.Attribute("project_id"),
+        analytics.Attribute("category"),
         analytics.Attribute("group_id"),
         analytics.Attribute("notification_uuid"),
         analytics.Attribute("alert_id", required=False),
