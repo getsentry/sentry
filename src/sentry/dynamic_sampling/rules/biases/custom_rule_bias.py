407515bc7c8cff7a2ef939e6835802e6fa9d07e6
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2023-09-21 13:39:33 +0200

    switch to constant decayingFn until reservoir becomes available

--- src/sentry/dynamic_sampling/rules/biases/custom_rule_bias.py
+++ src/sentry/dynamic_sampling/rules/biases/custom_rule_bias.py
@@ -29,10 +29,12 @@ class CustomRuleBias(Bias):
                         "start": rule.start_date.strftime(CUSTOM_RULE_DATE_FORMAT),
                         "end": rule.end_date.strftime(CUSTOM_RULE_DATE_FORMAT),
                     },
-                    "decayingFn": {
-                        "type": "reservoir",
-                        "limit": rule.num_samples,
-                    },
+                    # TODO: (Raduw) switch to reservoir sampling when available
+                    "decayingFn": {"type": "constant"},
+                    # "decayingFn": {
+                    #     "type": "reservoir",
+                    #     "limit": rule.num_samples,
+                    # },
                 }
             )
 
--- tests/sentry/dynamic_sampling/rules/biases/test_custom_rule_bias.py
+++ tests/sentry/dynamic_sampling/rules/biases/test_custom_rule_bias.py
@@ -2,15 +2,15 @@ from datetime import timedelta
 from unittest import mock
 
 from django.utils import timezone
-from freezegun import freeze_time
 
 from sentry.dynamic_sampling.rules.biases.custom_rule_bias import CustomRuleBias
-from sentry.models import CUSTOM_RULE_START, CustomDynamicSamplingRule, Project
+from sentry.models import CustomDynamicSamplingRule, Project
+from sentry.testutils.helpers.datetime import freeze_time
 
 
 @freeze_time("2023-09-19 10:00:00")
 @mock.patch("sentry.models.CustomDynamicSamplingRule.get_project_rules")
-def test_custom_rule_bias(custom_dynamic_sampling_rule_mock):
+def test_custom_rule_bias(custom_dynamic_sampling_rule_mock, insta_snapshot):
     """
     Test that the custom rule bias transforms the rules from the model into the expected format
     """
@@ -40,15 +40,4 @@ def test_custom_rule_bias(custom_dynamic_sampling_rule_mock):
     rules = bias.generate_rules(p, 0.5)
 
     assert len(rules) == 2
-
-    for rule in rules:
-        assert rule["samplingValue"]["type"] == "sampleRate"
-        assert rule["type"] == "transaction"
-        assert rule["condition"]["op"] == "equals"
-        assert rule["condition"]["name"] == "environment"
-        assert rule["condition"]["value"] in ["prod1", "prod2"]
-        assert rule["id"] in [CUSTOM_RULE_START + 1, CUSTOM_RULE_START + 2]
-        assert rule["timeRange"]["start"] == "2023-09-19T09:00:00.000000Z"
-        assert rule["timeRange"]["end"] == "2023-09-19T11:00:00.000000Z"
-        assert rule["decayingFn"]["type"] == "reservoir"
-        assert rule["decayingFn"]["limit"] in [100, 101]
+    insta_snapshot(rules)
