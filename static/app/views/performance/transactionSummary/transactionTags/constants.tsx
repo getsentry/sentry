1c6d66d3c97bc29ea048203c6d5dfcdb130d811b
Kev <6111995+k-fish@users.noreply.github.com>
2023-09-20 23:29:21 -0400

    feat(perf): Add more measurements to tags UI
    This adds the additional measurements to the tags selector.

--- static/app/views/performance/transactionSummary/transactionTags/constants.tsx
+++ static/app/views/performance/transactionSummary/transactionTags/constants.tsx
@@ -5,5 +5,8 @@ import {XAxisOption} from './types';
 
 export const X_AXIS_SELECT_OPTIONS: SelectValue<XAxisOption>[] = [
   {label: t('LCP'), value: XAxisOption.LCP},
+  {label: t('CLS'), value: XAxisOption.CLS},
+  {label: t('FID'), value: XAxisOption.FID},
+  {label: t('FCP'), value: XAxisOption.FCP},
   {label: t('Duration'), value: XAxisOption.DURATION},
 ];
