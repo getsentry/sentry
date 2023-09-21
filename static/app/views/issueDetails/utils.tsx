e1b251f16814a585eead78392e33685ba5ca8192
Nar Saynorath <nar.saynorath@sentry.io>
2023-09-21 10:37:11 -0400

    feat(stat-detectors): Use events-facets for tags sidebar (#56503)
    Update `useFetchIssueTagsForDetailsPage` to use the `events-facets`
    endpoint to get tags for statistical detectors. This required some data
    manipulation because the two endpoints don't provide the same
    detail. It also filters the transactions to the definition we're using as
    "problematic" for now.

--- static/app/views/issueDetails/utils.tsx
+++ static/app/views/issueDetails/utils.tsx
@@ -154,10 +154,17 @@ export const useFetchIssueTagsForDetailsPage = (
     groupId,
     orgSlug,
     environment = [],
+    isStatisticalDetector = false,
+    statisticalDetectorParameters,
   }: {
     environment: string[];
     orgSlug: string;
     groupId?: string;
+    isStatisticalDetector?: boolean;
+    statisticalDetectorParameters?: {
+      durationBaseline: number;
+      transaction: string;
+    };
   },
   {enabled = true}: {enabled?: boolean} = {}
 ) => {
@@ -168,6 +175,8 @@ export const useFetchIssueTagsForDetailsPage = (
       environment,
       readable: true,
       limit: 4,
+      isStatisticalDetector,
+      statisticalDetectorParameters,
     },
     {enabled}
   );
