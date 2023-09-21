32f79df32b484950fd5ab639e56c1d4de0c13490
Arpad Borsos <arpad.borsos@sentry.io>
2023-09-21 12:22:03 +0200

    Refresh chunk timestamps on assemble poll (#56564)
    This will also refresh any chunks of a to-be-uploaded file when checking
    for which chunks to upload. That way, those chunks won't be reclaimed,
    racing with the upload itself.

--- src/sentry/debug_files/upload.py
+++ src/sentry/debug_files/upload.py
@@ -1,10 +1,16 @@
 from typing import Sequence
 
-from sentry.models.files.fileblobowner import FileBlobOwner
+from django.utils import timezone
+
+from sentry.models.files import FileBlob, FileBlobOwner
 
 
 def find_missing_chunks(organization_id: int, chunks: Sequence[str]):
     """Returns a list of chunks which are missing for an org."""
+    # refresh the timestamp of all files we are interested in,
+    # so they don't disappear from under us
+    FileBlob.objects.filter(checksum__in=chunks).update(timestamp=timezone.now())
+
     owned = set(
         FileBlobOwner.objects.filter(
             blob__checksum__in=chunks, organization_id=organization_id
