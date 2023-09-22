faa77cabdaba2600924aef147264b27ef9429c0a
Colleen O'Rourke <colleen@sentry.io>
2023-09-22 14:50:16 -0700

    tdd bby

--- tests/sentry/api/endpoints/test_project_rule_enable.py
+++ tests/sentry/api/endpoints/test_project_rule_enable.py
@@ -92,6 +92,53 @@ class ProjectRuleEnableTestCase(APITestCase):
             == f"This rule is an exact duplicate of '{rule.label}' in this project and may not be enabled unless it's edited."
         )
 
+    def test_duplicate_rule_diff_env(self):
+        """Test that we do allow enabling a rule that's the exact duplicate of another
+        rule in the same project EXCEPT that the environment is different"""
+        conditions = [
+            {
+                "id": "sentry.rules.conditions.first_seen_event.FirstSeenEventCondition",
+            }
+        ]
+        actions = [
+            {
+                "targetType": "IssueOwners",
+                "fallthroughType": "ActiveMembers",
+                "id": "sentry.mail.actions.NotifyEmailAction",
+                "targetIdentifier": "",
+            }
+        ]
+        dev_env = self.create_environment(self.project, name="dev", organization=self.organization)
+        prod_env = self.create_environment(
+            self.project, name="prod", organization=self.organization
+        )
+        rule = self.create_project_rule(
+            project=self.project,
+            action_match=actions,
+            condition_match=conditions,
+            environment_id=dev_env.id,
+        )
+
+        rule2 = self.create_project_rule(
+            project=self.project,
+            action_match=actions,
+            condition_match=conditions,
+            environment_id=prod_env.id,
+        )
+        rule2.status = ObjectStatus.DISABLED
+        rule2.save()
+
+        response = self.get_error_response(
+            self.organization.slug,
+            self.project.slug,
+            rule2.id,
+            status_code=status.HTTP_400_BAD_REQUEST,
+        )
+        assert (
+            response.data["detail"]
+            == f"This rule is an exact duplicate of '{rule.label}' in this project and may not be enabled unless it's edited."
+        )
+
     def test_no_action_rule(self):
         """Test that we do not allow enabling a rule that has no action(s)"""
         conditions = [
