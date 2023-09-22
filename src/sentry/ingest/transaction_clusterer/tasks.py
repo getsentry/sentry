f797c56ffa04c414547ed77e62814166399e0c93
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-09-22 15:47:56 +0200

    ref(txcluster): Emit good clustering metric

--- src/sentry/ingest/transaction_clusterer/tasks.py
+++ src/sentry/ingest/transaction_clusterer/tasks.py
@@ -88,6 +88,7 @@ def cluster_projects(projects: Sequence[Project]) -> None:
                 redis.clear_samples(ClustererNamespace.TRANSACTIONS, project)
             num_clustered += 1
     finally:
+        metrics.incr("txcluster.cluster_projects.clustered", amount=num_clustered, sample_rate=1.0)
         unclustered = len(projects) - num_clustered
         if unclustered > 0:
             metrics.incr(
