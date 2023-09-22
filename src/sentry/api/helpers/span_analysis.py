bae046c7bb08a85af04f8bfbaad9e5e084ee2e77
Neha Pusarla <npusarla98@gmail.com>
2023-09-22 13:19:46 -0700

    add some more fine tuning changes

--- src/sentry/api/helpers/span_analysis.py
+++ src/sentry/api/helpers/span_analysis.py
@@ -7,8 +7,6 @@
 # {'period': 1, 'k': 'paint', 'txn_count': 2978, 'sum_v': 0, 'count_v': 5151, 'avg_v': 0},
 # {'period': 1, 'k': 'resource.css', 'txn_count': 2978, 'sum_v': 449854.7821, 'count_v': 8926, 'avg_v': 50.39825029}]
 
-import collections
-
 
 def span_analysis(data):
 
@@ -35,23 +33,24 @@ def span_analysis(data):
         row["relative_freq"] = relative_freq[i]
         row["score"] = score_col[i]
         row["avg_duration"] = avg_col[i]
-        row["span_id"] = span_groups[i]
+        row["span_key"] = span_keys[i]
 
     # get constant, removed, and new spans
-    spans_before = {row["span_id"] for row in data if row["period"] == 0}
-    spans_after = {row["span_id"] for row in data if row["period"] == 1}
+    spans_before = {row["span_key"] for row in data if row["period"] == 0}
+    spans_after = {row["span_key"] for row in data if row["period"] == 1}
     constant_spans = spans_before.intersection(spans_after)
 
+    # TODO: Add logic to surface removed/new spans
     # removed_spans = [x for x in spans_before if x not in spans_after]
     # new_spans = [x for x in spans_after if x not in spans_before]
 
     # create two dataframes for period 0 and 1 and keep only the same spans in both periods
 
     span_data_p0 = {
-        row["span_id"]: row for row in data if row["period"] == 0 and row["k"] in constant_spans
+        row["span_key"]: row for row in data if row["period"] == 0 and row["span_key"] in constant_spans
     }
     span_data_p1 = {
-        row["span_id"]: row for row in data if row["period"] == 1 and row["k"] in constant_spans
+        row["span_key"]: row for row in data if row["period"] == 1 and row["span_key"] in constant_spans
     }
 
     # merge the dataframes to do span analysis
@@ -77,7 +76,9 @@ def span_analysis(data):
 
         problem_spans.append(
             {
-                "span_id": key,
+                "span_op": key.split(",")[0],
+                "span_group": key.split(",")[1],
+                "sample_event_id": row1["sample_event_id"],
                 "score_delta": score_delta,
                 "freq_delta": freq_delta,
                 "duration_delta": duration_delta,
