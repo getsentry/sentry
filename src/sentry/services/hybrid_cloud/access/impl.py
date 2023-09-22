431e406a39eb3853b02d1149dffc98d23ad47e2e
Zachary Collins <zachary.collins@sentry.io>
2023-09-22 14:06:10 -0700

    ref(hc): Organization role logic now uses replicas

--- src/sentry/services/hybrid_cloud/access/impl.py
+++ src/sentry/services/hybrid_cloud/access/impl.py
@@ -1,4 +1,4 @@
-from typing import FrozenSet, Optional
+from typing import FrozenSet, List, Optional, Set
 
 from django.db.models import Q
 
@@ -10,6 +10,10 @@ from sentry.models import (
     AuthProviderReplica,
     OrganizationMember,
     OrganizationMemberMapping,
+    OrganizationMemberTeam,
+    OrganizationMemberTeamReplica,
+    Team,
+    TeamReplica,
 )
 from sentry.services.hybrid_cloud.access.service import AccessService
 from sentry.services.hybrid_cloud.auth import RpcAuthIdentity, RpcAuthProvider
@@ -19,14 +23,42 @@ from sentry.services.hybrid_cloud.auth.serial import (
     serialize_auth_provider,
     serialize_auth_provider_replica,
 )
-from sentry.services.hybrid_cloud.organization import (
-    RpcOrganizationMemberSummary,
-    organization_service,
-)
+from sentry.services.hybrid_cloud.organization import RpcOrganizationMemberSummary
 from sentry.services.hybrid_cloud.user.service import user_service
 
 
 class ControlAccessService(AccessService):
+    def get_all_org_roles(self, member_id: int, organization_id: int) -> List[str]:
+        try:
+            member = OrganizationMemberMapping.objects.get(
+                organizationmember_id=member_id, organization_id=organization_id
+            )
+        except OrganizationMemberMapping.DoesNotExist:
+            return []
+
+        team_ids = OrganizationMemberTeamReplica.objects.filter(
+            organizationmember_id=member_id, organization_id=organization_id
+        ).values_list("team_id", flat=True)
+        all_roles: Set[str] = set(
+            TeamReplica.objects.filter(team_id__in=team_ids)
+            .exclude(org_role=None)
+            .values_list("org_role", flat=True)
+        )
+        all_roles.add(member.role)
+        return list(all_roles)
+
+    def get_top_dog_team_member_ids(self, organization_id: int) -> List[int]:
+        owner_teams = list(
+            TeamReplica.objects.filter(
+                organization_id=organization_id, org_role=roles.get_top_dog().id
+            ).values_list("id", flat=True)
+        )
+        return list(
+            OrganizationMemberTeamReplica.objects.filter(team_id__in=owner_teams).values_list(
+                "organizationmember_id", flat=True
+            )
+        )
+
     def get_permissions_for_user(self, user_id: int) -> FrozenSet[str]:
         user = user_service.get_user(user_id)
         if user is None:
@@ -59,18 +91,19 @@ class ControlAccessService(AccessService):
         """
 
         # Get more org role related data into control to reduce this inter silo rpc stuff....
-        org_roles = organization_service.get_all_org_roles(
+        org_roles = self.get_all_org_roles(
             member_id=member.id, organization_id=member.organization_id
         )
         if roles.get_top_dog().id not in org_roles:
             return False
 
-        all_top_dogs_from_teams = organization_service.get_top_dog_team_member_ids(
+        all_top_dogs_from_teams = self.get_top_dog_team_member_ids(
             organization_id=member.organization_id
         )
         user_ids = (
             OrganizationMemberMapping.objects.filter(
-                Q(id__in=all_top_dogs_from_teams) | Q(role=roles.get_top_dog().id),
+                Q(organizationmember_id__in=all_top_dogs_from_teams)
+                | Q(role=roles.get_top_dog().id),
                 organization_id=member.organization_id,
                 user__is_active=True,
             )
@@ -83,6 +116,35 @@ class ControlAccessService(AccessService):
 
 
 class RegionAccessService(AccessService):
+    def get_all_org_roles(self, member_id: int, organization_id: int) -> List[str]:
+        try:
+            member = OrganizationMember.objects.get(id=member_id, organization_id=organization_id)
+        except OrganizationMember.DoesNotExist:
+            return []
+
+        team_ids = OrganizationMemberTeam.objects.filter(
+            organizationmember_id=member.id
+        ).values_list("team_id", flat=True)
+        all_roles: Set[str] = set(
+            Team.objects.filter(id__in=team_ids)
+            .exclude(org_role=None)
+            .values_list("org_role", flat=True)
+        )
+        all_roles.add(member.role)
+        return list(all_roles)
+
+    def get_top_dog_team_member_ids(self, organization_id: int) -> List[int]:
+        owner_teams = list(
+            Team.objects.filter(
+                organization_id=organization_id, org_role=roles.get_top_dog().id
+            ).values_list("id", flat=True)
+        )
+        return list(
+            OrganizationMemberTeam.objects.filter(team_id__in=owner_teams).values_list(
+                "organizationmember_id", flat=True
+            )
+        )
+
     def get_auth_provider(self, organization_id: int) -> Optional[RpcAuthProvider]:
         try:
             ap = AuthProviderReplica.objects.get(organization_id=organization_id)
@@ -102,13 +164,13 @@ class RegionAccessService(AccessService):
     def can_override_sso_as_owner(
         self, auth_provider: RpcAuthProvider, member: RpcOrganizationMemberSummary
     ) -> bool:
-        org_roles = organization_service.get_all_org_roles(
+        org_roles = self.get_all_org_roles(
             member_id=member.id, organization_id=member.organization_id
         )
         if roles.get_top_dog().id not in org_roles:
             return False
 
-        all_top_dogs_from_teams = organization_service.get_top_dog_team_member_ids(
+        all_top_dogs_from_teams = self.get_top_dog_team_member_ids(
             organization_id=member.organization_id
         )
         user_ids = (
