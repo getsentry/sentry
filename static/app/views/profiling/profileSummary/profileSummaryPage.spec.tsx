ed9fbfca7202b9ab35bb26ece3f4dcf6d4a576f3
Jonas <jonas.badalic@sentry.io>
2023-09-25 16:24:26 +0200

    feat(summary): add aggregate flamegraph (#56457)
    Add aggregate flamegraph to page
    
    <img width="1512" alt="CleanShot 2023-09-19 at 10 37 59@2x"
    src="https://github.com/getsentry/sentry/assets/9317857/a8277a8d-ffee-4638-84f2-760e97d16f70">

--- static/app/views/profiling/profileSummary/profileSummaryPage.spec.tsx
+++ static/app/views/profiling/profileSummary/profileSummaryPage.spec.tsx
@@ -3,6 +3,39 @@ import {render, screen} from 'sentry-test/reactTestingLibrary';
 import OrganizationStore from 'sentry/stores/organizationStore';
 import ProfileSummaryPage from 'sentry/views/profiling/profileSummary';
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation(query => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // Deprecated
+    removeListener: jest.fn(), // Deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+// Replace the webgl renderer with a dom renderer for tests
+jest.mock('sentry/utils/profiling/renderers/flamegraphRendererWebGL', () => {
+  const {
+    FlamegraphRendererDOM,
+  } = require('sentry/utils/profiling/renderers/flamegraphRendererDOM');
+
+  return {
+    FlamegraphRendererWebGL: FlamegraphRendererDOM,
+  };
+});
+
+window.ResizeObserver =
+  window.ResizeObserver ||
+  jest.fn().mockImplementation(() => ({
+    disconnect: jest.fn(),
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+  }));
+
 describe('ProfileSummaryPage', () => {
   it('renders legacy page', async () => {
     const organization = TestStubs.Organization({
@@ -50,6 +83,11 @@ describe('ProfileSummaryPage', () => {
 
     MockApiClient.addMockResponse({
       url: `/organizations/${organization.slug}/events-stats/`,
+      body: [],
+    });
+
+    MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/profiling/flamegraph/`,
       body: [],
     });
 
