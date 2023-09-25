c20ca6ee71e2b54fdca3cb5c83f4fb22dedf1ac2
Pierre Massat <pierre.massat@sentry.io>
2023-09-25 18:21:17 -0400

    Disable release for this issue type

--- src/sentry/issues/grouptype.py
+++ src/sentry/issues/grouptype.py
@@ -395,7 +395,6 @@ class ProfileFrameDropType(GroupType):
     description = "Frame Drop"
     category = GroupCategory.PERFORMANCE.value
     noise_config = NoiseConfig(ignore_limit=25)
-    released = True
 
 
 @dataclass(frozen=True)
