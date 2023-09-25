baffcd9ada927ca62b4b25bd56353392b73da9a3
Arpad Borsos <arpad.borsos@sentry.io>
2023-09-25 17:54:38 +0200

    Only bump outdated FileBlob timestamps (#56860)
    This will debounce the timestamp updates, trying not to overwhelm the
    database.

--- src/sentry/debug_files/upload.py
+++ src/sentry/debug_files/upload.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from typing import Sequence
 
 from django.utils import timezone
@@ -9,7 +10,9 @@ def find_missing_chunks(organization_id: int, chunks: Sequence[str]):
     """Returns a list of chunks which are missing for an org."""
     # refresh the timestamp of all files we are interested in,
     # so they don't disappear from under us
-    FileBlob.objects.filter(checksum__in=chunks).update(timestamp=timezone.now())
+    now = timezone.now()
+    threshold = now - timedelta(hours=12)
+    FileBlob.objects.filter(checksum__in=chunks, timestamp__lte=threshold).update(timestamp=now)
 
     owned = set(
         FileBlobOwner.objects.filter(
