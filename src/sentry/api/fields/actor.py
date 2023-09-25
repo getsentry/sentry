9d7387d50d540266970d7aeb37bbdafb947bcf91
Isabella Enriquez <45607721+isabellaenriquez@users.noreply.github.com>
2023-09-25 12:14:39 -0700

    feat(api): Publish `ProjectRulesEndpoint` (#55956)
    This PR publishes the `ProjectRulesEndpoint` for both POST and GET
    requests as part of #55920.
    
    Closes #56674

--- src/sentry/api/fields/actor.py
+++ src/sentry/api/fields/actor.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from drf_spectacular.utils import extend_schema_field
 from rest_framework import serializers
 
 from sentry.models import ActorTuple, OrganizationMember, Team, User
@@ -10,6 +11,7 @@ if TYPE_CHECKING:
     from sentry.services.hybrid_cloud.user import RpcUser
 
 
+@extend_schema_field(str)
 class ActorField(serializers.Field):
     def __init__(self, *args, **kwds):
         self.as_actor = kwds.pop("as_actor", False)
--- src/sentry/apidocs/examples/issue_alert_examples.py
+++ src/sentry/apidocs/examples/issue_alert_examples.py
@@ -10,3 +10,99 @@ class IssueAlertExamples:
             response_only=True,
         )
     ]
+
+    LIST_PROJECT_RULES = [
+        OpenApiExample(
+            "List issue alert rules for a project",
+            value=[
+                {
+                    "id": "3",
+                    "conditions": [
+                        {
+                            "interval": "1h",
+                            "id": "sentry.rules.conditions.event_frequency.EventFrequencyCondition",
+                            "value": 1000,
+                        }
+                    ],
+                    "filters": [
+                        {
+                            "value": "1",
+                            "id": "sentry.rules.filters.issue_category.IssueCategoryFilter",
+                        },
+                        {
+                            "value": "2",
+                            "id": "sentry.rules.filters.issue_category.IssueCategoryFilter",
+                        },
+                    ],
+                    "actions": [
+                        {
+                            "targetType": "Team",
+                            "fallthroughType": "ActiveMembers",
+                            "id": "sentry.mail.actions.NotifyEmailAction",
+                            "targetIdentifier": 4367234414355,
+                        }
+                    ],
+                    "actionMatch": "any",
+                    "filterMatch": "any",
+                    "frequency": 60,
+                    "name": "High Number of Issues with Production",
+                    "dateCreated": "2023-01-15T06:45:34.353346Z",
+                    "owner": "team:63562",
+                    "createdBy": {
+                        "id": 2435786,
+                        "name": "John Doe",
+                        "email": "john.doe@example.com",
+                    },
+                    "environment": "prod",
+                    "projects": ["melody"],
+                    "status": "active",
+                    "lastTriggered": "2023-07-15T00:00:00.351236Z",
+                    "snooze": False,
+                },
+            ],
+            status_codes=["200"],
+            response_only=True,
+        )
+    ]
+
+    CREATE_ISSUE_ALERT_RULE = [
+        OpenApiExample(
+            "Issue alert successfully created",
+            value={
+                "id": "1",
+                "conditions": [
+                    {
+                        "id": "sentry.rules.conditions.first_seen_event.FirstSeenEventCondition",
+                    }
+                ],
+                "filters": [
+                    {
+                        "targetType": "Unassigned",
+                        "id": "sentry.rules.filters.assigned_to.AssignedToFilter",
+                        "targetIdentifier": "",
+                    }
+                ],
+                "actions": [
+                    {
+                        "targetType": "Member",
+                        "fallthroughType": "ActiveMembers",
+                        "id": "sentry.mail.actions.NotifyEmailAction",
+                        "targetIdentifier": 1523125,
+                    }
+                ],
+                "actionMatch": "any",
+                "filterMatch": "all",
+                "frequency": 1440,
+                "name": "Owner Alert",
+                "dateCreated": "2023-09-08T20:00:07.244602Z",
+                "owner": "team:74234",
+                "createdBy": {"id": 24601, "name": "Jean Valjean", "email": "jean@example.com"},
+                "environment": None,
+                "projects": ["python"],
+                "status": "active",
+                "snooze": False,
+            },
+            status_codes=["201"],
+            response_only=True,
+        )
+    ]
