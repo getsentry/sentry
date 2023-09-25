1e8e550f4c52b3af33f35e998e1033ac0c28572c
Mark Story <mark@mark-story.com>
2023-09-25 12:30:48 -0400

    feat(hybridcloud) Enable Client to do domain splitting (#56692)
    As part of hybrid cloud we need to start splitting requests from the
    frontend to the region and control silo domains. I'd like to hide this
    logic from most of the application as most environments won't have
    multi-region, and we need additional workarounds for the `yarn dev-ui`
    workflow.
    
    Because handling subdomains in `dev-ui` requires additional host file
    entries, I chose to go with a path prefix `/region/$region` instead.
    This path prefix is handled by webpack proxy and removed when the
    request is forwarded to the appropriate region servers.

--- static/app/api.spec.tsx
+++ static/app/api.spec.tsx
@@ -1,5 +1,8 @@
-import {Request} from 'sentry/api';
+import {Request, resolveHostname} from 'sentry/api';
 import {PROJECT_MOVED} from 'sentry/constants/apiErrorCodes';
+
+import ConfigStore from './stores/configStore';
+import OrganizationStore from './stores/organizationStore';
 
 jest.unmock('sentry/api');
 
@@ -82,5 +85,100 @@ describe('api', function () {
         {}
       )
     ).not.toThrow();
+  });
+});
+
+describe('resolveHostname', function () {
+  let devUi, orgstate, configstate;
+
+  const controlPath = '/api/0/broadcasts/';
+  const regionPath = '/api/0/organizations/slug/issues/';
+
+  beforeEach(function () {
+    orgstate = OrganizationStore.get();
+    configstate = ConfigStore.getState();
+    devUi = window.__SENTRY_DEV_UI;
+
+    OrganizationStore.onUpdate(
+      TestStubs.Organization({features: ['frontend-domainsplit']})
+    );
+    ConfigStore.loadInitialData({
+      ...configstate,
+      links: {
+        organizationUrl: 'https://acme.sentry.io',
+        sentryUrl: 'https://sentry.io',
+        regionUrl: 'https://us.sentry.io',
+      },
+    });
+  });
+
+  afterEach(() => {
+    window.__SENTRY_DEV_UI = devUi;
+    OrganizationStore.onUpdate(orgstate.organization);
+    ConfigStore.loadInitialData(configstate);
+  });
+
+  it('does nothing without feature', function () {
+    // Org does not have the required feature.
+    OrganizationStore.onUpdate(TestStubs.Organization());
+
+    let result = resolveHostname(controlPath);
+    expect(result).toBe(controlPath);
+
+    // Explicit domains still work.
+    result = resolveHostname(controlPath, 'https://sentry.io');
+    expect(result).toBe(`https://sentry.io${controlPath}`);
+
+    result = resolveHostname(regionPath, 'https://de.sentry.io');
+    expect(result).toBe(`https://de.sentry.io${regionPath}`);
+  });
+
+  it('adds domains when feature enabled', function () {
+    let result = resolveHostname(regionPath);
+    expect(result).toBe('https://us.sentry.io/api/0/organizations/slug/issues/');
+
+    result = resolveHostname(controlPath);
+    expect(result).toBe('https://sentry.io/api/0/broadcasts/');
+  });
+
+  it('uses paths for region silo in dev-ui', function () {
+    window.__SENTRY_DEV_UI = true;
+
+    let result = resolveHostname(regionPath);
+    expect(result).toBe('/region/us/api/0/organizations/slug/issues/');
+
+    result = resolveHostname(controlPath);
+    expect(result).toBe('/api/0/broadcasts/');
+  });
+
+  it('removes sentryUrl from dev-ui mode requests', function () {
+    window.__SENTRY_DEV_UI = true;
+
+    let result = resolveHostname(regionPath, 'https://sentry.io');
+    expect(result).toBe('/api/0/organizations/slug/issues/');
+
+    result = resolveHostname(controlPath, 'https://sentry.io');
+    expect(result).toBe('/api/0/broadcasts/');
+  });
+
+  it('removes sentryUrl from dev-ui mode requests when feature is off', function () {
+    window.__SENTRY_DEV_UI = true;
+    // Org does not have the required feature.
+    OrganizationStore.onUpdate(TestStubs.Organization());
+
+    let result = resolveHostname(controlPath);
+    expect(result).toBe(controlPath);
+
+    // control silo shaped URLs don't get a host
+    result = resolveHostname(controlPath, 'https://sentry.io');
+    expect(result).toBe(controlPath);
+
+    result = resolveHostname(regionPath, 'https://de.sentry.io');
+    expect(result).toBe(`/region/de${regionPath}`);
+  });
+
+  it('preserves host parameters', function () {
+    const result = resolveHostname(regionPath, 'https://de.sentry.io');
+    expect(result).toBe('https://de.sentry.io/api/0/organizations/slug/issues/');
   });
 });
