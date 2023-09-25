50f201c048100ae9ef3036d0fedd1ec5ecf531ab
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-09-25 11:59:56 -0700

    feat(replays): add collapsible panel component and allow hiding widgets (#56794)
    closes https://github.com/getsentry/team-replay/issues/191
    
    
    https://github.com/getsentry/sentry/assets/56095982/78976553-37f5-4b0c-a02b-66fa77d6c799
    
    the new `openClosePanel` component is based on the performance
    `Accordion` component, and it's reusable :-)

--- static/app/views/replays/deadRageClick/deadRageSelectorCards.tsx
+++ static/app/views/replays/deadRageClick/deadRageSelectorCards.tsx
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import {Location} from 'history';
 
 import {LinkButton} from 'sentry/components/button';
+import OpenClosePanel from 'sentry/components/openClosePanel';
 import {IconCursorArrow, IconShow} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
@@ -15,11 +16,19 @@ import SelectorTable from 'sentry/views/replays/deadRageClick/selectorTable';
 function DeadRageSelectorCards() {
   const location = useLocation();
 
+  function header(isOpen: boolean) {
+    return isOpen
+      ? t('Hide Actionable Replay Insights')
+      : t('Show Actionable Replay Insights');
+  }
+
   return (
-    <SplitCardContainer>
-      <DeadClickTable location={location} />
-      <RageClickTable location={location} />
-    </SplitCardContainer>
+    <OpenClosePanel header={header} openByDefault>
+      <SplitCardContainer>
+        <DeadClickTable location={location} />
+        <RageClickTable location={location} />
+      </SplitCardContainer>
+    </OpenClosePanel>
   );
 }
 
