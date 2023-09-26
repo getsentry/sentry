2628d54a09f55cedcdd6d8d2af082eea4c316fd8
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-09-26 16:34:52 +0200

    ref(txclusterer): Log Sentry message on missed projects for clustering (#56846)

--- src/sentry/ingest/transaction_clusterer/tasks.py
+++ src/sentry/ingest/transaction_clusterer/tasks.py
@@ -102,6 +102,9 @@ def cluster_projects(projects: Sequence[Project]) -> None:
                 tags={"clustered": False},
                 sample_rate=1.0,
             )
+            sentry_sdk.set_tag("projects.total", len(projects))
+            sentry_sdk.set_tag("projects.unclustered", unclustered)
+            sentry_sdk.capture_message("Transaction clusterer missed projects", level="error")
 
 
 @instrumented_task(
