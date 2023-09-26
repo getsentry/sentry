4997861f8dee59edc0c02e78f31c51a792b44252
Iker Barriocanal <32816711+iker-barriocanal@users.noreply.github.com>
2023-09-26 11:18:01 +0200

    extract data into tags

--- src/sentry/ingest/transaction_clusterer/tasks.py
+++ src/sentry/ingest/transaction_clusterer/tasks.py
@@ -102,9 +102,9 @@ def cluster_projects(projects: Sequence[Project]) -> None:
                 tags={"clustered": False},
                 sample_rate=1.0,
             )
-            sentry_sdk.capture_message(
-                f"Transaction clusterer missed projects: {unclustered}", level="error"
-            )
+            sentry_sdk.set_tag("projs.total", len(projects))
+            sentry_sdk.set_tag("projs.unclustered", unclustered)
+            sentry_sdk.capture_message("Transaction clusterer missed projects", level="error")
 
 
 @instrumented_task(
