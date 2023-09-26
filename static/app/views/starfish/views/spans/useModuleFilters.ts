9e3b4cfd95807f1683778eae3bae873b9b253e09
Abhijeet Prasad <aprasad@sentry.io>
2023-09-26 16:54:27 -0400

    feat(starfish): Use span domain array for domain selector

--- static/app/views/starfish/views/spans/useModuleFilters.ts
+++ static/app/views/starfish/views/spans/useModuleFilters.ts
@@ -6,6 +6,7 @@ import {SpanMetricsField} from 'sentry/views/starfish/types';
 export type ModuleFilters = {
   [SpanMetricsField.SPAN_ACTION]?: string;
   [SpanMetricsField.SPAN_DOMAIN]?: string;
+  [SpanMetricsField.SPAN_DOMAIN_ARRAY]?: string;
   [SpanMetricsField.SPAN_GROUP]?: string;
   [SpanMetricsField.SPAN_OP]?: string;
 };
@@ -16,6 +17,7 @@ export const useModuleFilters = () => {
   return pick(location.query, [
     SpanMetricsField.SPAN_ACTION,
     SpanMetricsField.SPAN_DOMAIN,
+    SpanMetricsField.SPAN_DOMAIN_ARRAY,
     SpanMetricsField.SPAN_OP,
     SpanMetricsField.SPAN_GROUP,
   ]);
