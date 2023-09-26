ca6c7f2889743de6f179df219cff0c9041f36db9
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-09-26 12:30:44 -0500

    fix(notifications): Fix enum types for notifications (#56935)

--- src/sentry/notifications/manager.py
+++ src/sentry/notifications/manager.py
@@ -434,7 +434,11 @@ class NotificationsManager(BaseManager["NotificationSetting"]):  # noqa: F821
 
         if should_use_notifications_v2(organization):  # type: ignore
             # We should replace calls to NotificationSettings.get_notification_recipients at the call site - this code should never be reached
-            setting_type = NotificationSettingEnum(NOTIFICATION_SETTING_TYPES[type])
+            setting_type = (
+                NotificationSettingEnum(NOTIFICATION_SETTING_TYPES[type])
+                if type
+                else NotificationSettingEnum.ISSUE_ALERTS
+            )
             controller = NotificationController(
                 recipients=recipient_actors,
                 project_ids=project_ids,
