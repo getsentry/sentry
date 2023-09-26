7ee6c8df9baf4eddee9c97aadb84dd19eaf2e3b1
Billy Vong <billy@sentry.io>
2023-09-26 13:17:35 -0400

    fix types

--- static/app/components/feedback/widget/feedbackButton.tsx
+++ static/app/components/feedback/widget/feedbackButton.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import styled from '@emotion/styled';
 
 const Button = styled('button')`
--- static/app/components/feedback/widget/sendFeedbackRequest.ts
+++ static/app/components/feedback/widget/sendFeedbackRequest.ts
@@ -1,3 +1,4 @@
+import {getCurrentHub} from '@sentry/react';
 import {DsnComponents} from '@sentry/types';
 
 import {prepareFeedbackEvent} from './prepareFeedbackEvent';
@@ -22,7 +23,7 @@ export async function sendFeedbackRequest({
   replay_id,
   url,
 }): Promise<Response | null> {
-  const hub = window.Sentry?.getCurrentHub();
+  const hub = getCurrentHub();
 
   if (!hub) {
     return null;
@@ -54,49 +55,8 @@ export async function sendFeedbackRequest({
   });
 
   if (!feedbackEvent) {
-    // Taken from baseclient's `_processEvent` method, where this is handled for errors/transactions
-    // client.recordDroppedEvent('event_processor', 'feedback', baseEvent);
     return null;
   }
-
-  //
-  // For reference, the fully built event looks something like this:
-  // {
-  // "data": {
-  //     "dist": "abc123",
-  //     "environment": "production",
-  //     "feedback": {
-  //       "contact_email": "colton.allen@sentry.io",
-  //       "message": "I really like this user-feedback feature!",
-  //       "replay_id": "ec3b4dc8b79f417596f7a1aa4fcca5d2",
-  //       "url": "https://docs.sentry.io/platforms/javascript/"
-  //     },
-  //     "id": "1ffe0775ac0f4417aed9de36d9f6f8dc",
-  //     "platform": "javascript",
-  //     "release": "version@1.3",
-  //     "request": {
-  //       "headers": {
-  //         "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.0.0 Safari/537.36"
-  //       }
-  //     },
-  //     "sdk": {
-  //       "name": "sentry.javascript.react",
-  //       "version": "6.18.1"
-  //     },
-  //     "tags": {
-  //       "key": "value"
-  //     },
-  //     "timestamp": "2023-08-31T14:10:34.954048",
-  //     "user": {
-  //       "email": "username@example.com",
-  //       "id": "123",
-  //       "ip_address": "127.0.0.1",
-  //       "name": "user",
-  //       "username": "user2270129"
-  //     }
-  // }
-  // }
-  //
 
   // Prevent this data (which, if it exists, was used in earlier steps in the processing pipeline) from being sent to
   // sentry. (Note: Our use of this property comes and goes with whatever we might be debugging, whatever hacks we may
