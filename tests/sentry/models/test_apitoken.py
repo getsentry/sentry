cffb51bea7cdd75a0d538534f4d2b32e7310f1c5
Seiji Chew <67301797+schew2381@users.noreply.github.com>
2023-09-26 11:44:11 -0700

    feat(scopes): Enforce scope hierarchy (#54510)
    ### Note
    - User Token UI Changes should be merged beforehand
    https://github.com/getsentry/sentry/pull/54651
    - See[ Notion
    doc](https://www.notion.so/sentry/WIP-Permission-Scope-Hierarchy-67620f99783345bbb8bc2828b1addc64)
    for detailed spec on this work
    
    ### Implementation
    To model the hierarchy I used a simple dict of scope -> all granted
    scopes. I chose this approach b/c it's very simple and easy to enforce
    in the code.
    
    We use a pre-save signal on the `ApiKey` and `ApiToken` models to
    enforce scope hierarchy. The basic scope hierarchy for each resource is:
    - read grants nothing
    - write grants read
    - admin grants read+write
    
    When updating one of these models, there is no way to know if the scopes
    are enforced without fetching it from the DB. To avoid this trip, we
    always iterate through the hierarchy mapping.

--- tests/sentry/models/test_apitoken.py
+++ tests/sentry/models/test_apitoken.py
@@ -2,13 +2,14 @@ from datetime import timedelta
 
 from django.utils import timezone
 
+from sentry.conf.server import SENTRY_SCOPE_HIERARCHY_MAPPING, SENTRY_SCOPES
 from sentry.models import ApiToken
 from sentry.models.integrations.sentry_app_installation import SentryAppInstallation
 from sentry.testutils.cases import TestCase
-from sentry.testutils.silo import region_silo_test
+from sentry.testutils.silo import control_silo_test
 
 
-@region_silo_test(stable=True)
+@control_silo_test(stable=True)
 class ApiTokenTest(TestCase):
     def test_is_expired(self):
         token = ApiToken(expires_at=None)
@@ -29,6 +30,17 @@ class ApiTokenTest(TestCase):
 
         token = ApiToken(scope_list=["project:read"])
         assert token.get_scopes() == ["project:read"]
+
+    def test_enforces_scope_hierarchy(self):
+        user = self.create_user()
+
+        # Ensure hierarchy is enforced for all tokens
+        for scope in SENTRY_SCOPES:
+            token = ApiToken.objects.create(
+                user_id=user.id,
+                scope_list=[scope],
+            )
+            assert set(token.get_scopes()) == SENTRY_SCOPE_HIERARCHY_MAPPING[scope]
 
 
 class ApiTokenInternalIntegrationTest(TestCase):
