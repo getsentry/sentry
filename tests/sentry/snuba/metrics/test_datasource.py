d629c1c98d0672c5ff15b9999cb66e2fa9199548
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-09-26 20:55:05 +0200

    Add tests

--- tests/sentry/snuba/metrics/test_datasource.py
+++ tests/sentry/snuba/metrics/test_datasource.py
@@ -2,6 +2,7 @@ import pytest
 
 from sentry.sentry_metrics.use_case_id_registry import UseCaseID
 from sentry.snuba.metrics import TransactionMRI, get_tag_values
+from sentry.snuba.metrics.naming_layer import TransactionMetricKey
 from sentry.testutils.cases import BaseMetricsLayerTestCase, TestCase
 from sentry.testutils.helpers.datetime import freeze_time
 from sentry.testutils.skips import requires_snuba
@@ -16,19 +17,35 @@ class DatasourceTestCase(BaseMetricsLayerTestCase, TestCase):
     def now(self):
         return BaseMetricsLayerTestCase.MOCK_DATETIME
 
-    def test_get_tag_values(self):
-        self.store_performance_metric(
-            name=TransactionMRI.DURATION.value,
-            tags={"release": "1.0"},
-            value=1,
-        )
-        self.store_performance_metric(
-            name=TransactionMRI.DURATION.value,
-            tags={"release": "2.0"},
-            value=1,
-        )
+    def test_get_tag_values_with_mri(self):
+        for release in ("1.0", "2.0"):
+            self.store_performance_metric(
+                name=TransactionMRI.DURATION.value,
+                tags={"release": release},
+                value=1,
+            )
 
         values = get_tag_values(
             [self.project], "release", [TransactionMRI.DURATION.value], UseCaseID.TRANSACTIONS
         )
         assert values == [{"key": "release", "value": "1.0"}, {"key": "release", "value": "2.0"}]
+
+    def test_get_tag_values_with_public_name(self):
+        for satisfaction in ("satisfied", "tolerable", "miserable"):
+            self.store_performance_metric(
+                name=TransactionMRI.MEASUREMENTS_LCP.value,
+                tags={"satisfaction": satisfaction},
+                value=1,
+            )
+
+        values = get_tag_values(
+            [self.project],
+            "satisfaction",
+            [TransactionMetricKey.MEASUREMENTS_LCP.value],
+            UseCaseID.TRANSACTIONS,
+        )
+        assert values == [
+            {"key": "satisfaction", "value": "miserable"},
+            {"key": "satisfaction", "value": "satisfied"},
+            {"key": "satisfaction", "value": "tolerable"},
+        ]
