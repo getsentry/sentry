f3e155db2d199948b09723bdec611efdf22a0d5e
Billy Vong <billy@sentry.io>
2023-09-26 14:48:08 -0400

    feat(feedback): Use Feedback widget modal for Crons
    This replaces the use of `FeedbackFeature` with a temporary feedbackv2 widget in the Crons view.

--- static/app/views/monitors/components/cronsFeedbackButton.tsx
+++ static/app/views/monitors/components/cronsFeedbackButton.tsx
@@ -1,9 +1,22 @@
-import {FeatureFeedback} from 'sentry/components/featureFeedback';
+import {Button} from 'sentry/components/button';
+import {FeedbackModal} from 'sentry/components/feedback/widget/feedbackModal';
+import {IconMegaphone} from 'sentry/icons/iconMegaphone';
+import {t} from 'sentry/locale';
 
 const CRONS_FEEDBACK_NAME = 'crons';
 
 function CronsFeedbackButton() {
-  return <FeatureFeedback featureName={CRONS_FEEDBACK_NAME} buttonProps={{size: 'sm'}} />;
+  const title = t('Give Feedback');
+
+  return (
+    <FeedbackModal title={title} type={CRONS_FEEDBACK_NAME}>
+      {({showModal}) => (
+        <Button size="sm" icon={<IconMegaphone />} onClick={showModal}>
+          {title}
+        </Button>
+      )}
+    </FeedbackModal>
+  );
 }
 
 export default CronsFeedbackButton;
