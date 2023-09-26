73de6ab7aefe635e7a8da43dfe4814d8aef2f09b
Gabe Villalobos <gabriel.villalobos@sentry.io>
2023-09-26 11:48:18 -0700

    Moves slug reservation replica to hybrid cloud app space, add foreign key to slug replica table

--- src/sentry/models/organizationslugreservationreplica.py
+++ src/sentry/models/organizationslugreservationreplica.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import BoundedBigIntegerField, Model, region_silo_only_model, sane_repr
+from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
 from sentry.models import OrganizationSlugReservationType
 from sentry.services.hybrid_cloud import REGION_NAME_LENGTH
 
@@ -11,6 +12,11 @@ from sentry.services.hybrid_cloud import REGION_NAME_LENGTH
 class OrganizationSlugReservationReplica(Model):
     __relocation_scope__ = RelocationScope.Excluded
 
+    organization_slug_reservation_id = HybridCloudForeignKey(
+        "sentry.organizationslugreservation",
+        on_delete="CASCADE",
+        unique=True,
+    )
     slug = models.SlugField(unique=True, db_index=True)
     organization_id = BoundedBigIntegerField(db_index=True)
     user_id = BoundedBigIntegerField(db_index=True, null=False)
@@ -23,8 +29,8 @@ class OrganizationSlugReservationReplica(Model):
     date_added = models.DateTimeField(null=False, default=timezone.now, editable=False)
 
     class Meta:
-        app_label = "sentry"
-        db_table = "sentry_organizationslugreservationreplica"
+        app_label = "hybridcloud"
+        db_table = "hybridcloud_organizationslugreservationreplica"
         unique_together = (("organization_id", "reservation_type"),)
 
     __repr__ = sane_repr("organization_id", "slug")
