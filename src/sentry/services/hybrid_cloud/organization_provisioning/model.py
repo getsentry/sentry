4c2ee31df4db2fe7491901aad91da7e67c9e3b2c
Gabe Villalobos <GabeVillalobos@users.noreply.github.com>
2023-09-26 16:06:18 -0700

    feat(hybrid-cloud): Adds OrganizationSlugReservation replica and replication impl (#56697)

--- src/sentry/services/hybrid_cloud/organization_provisioning/model.py
+++ src/sentry/services/hybrid_cloud/organization_provisioning/model.py
@@ -22,7 +22,9 @@ class OrganizationProvisioningOptions(pydantic.BaseModel):
 
 
 class RpcOrganizationSlugReservation(pydantic.BaseModel):
+    id: int
     organization_id: int
     user_id: int
     slug: str
     region_name: str
+    reservation_type: int
