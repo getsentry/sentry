dc9764338ef6fb4a64bfbe6e125b2ae8f01267c9
Ash Anand <ash.anand@sentry.io>
2023-09-26 11:29:33 -0400

    Adjust chart props and styling

--- static/app/views/performance/browser/interactionSummary/interactionBreakdownChart.tsx
+++ static/app/views/performance/browser/interactionSummary/interactionBreakdownChart.tsx
@@ -2,15 +2,14 @@ import {useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import {CHART_PALETTE} from 'sentry/constants/chartPalette';
-
+import {space} from 'sentry/styles/space';
+import {getDurationUnit} from 'sentry/utils/discover/charts';
 import {useInteractionBreakdownTimeseriesQuery} from 'sentry/views/performance/browser/interactionSummary/useInteractionBreakdownTimeseriesQuery';
-
 import Chart from 'sentry/views/starfish/components/chart';
 
 type Props = {};
 
 export function InteractionBreakdownChart(props: Props) {
-  const theme = useTheme();
   const {data, isLoading} = useInteractionBreakdownTimeseriesQuery({
     operation: 'ui.action.click',
     element: 'input.app-6fjtrc.e1mw05q50[type="range"][name="replay-timeline"]',
@@ -25,6 +24,8 @@ export function InteractionBreakdownChart(props: Props) {
         loading={isLoading}
         utc={false}
         chartColors={[CHART_PALETTE[0][0]]}
+        durationUnit={getDurationUnit(data)}
+        aggregateOutputFormat={'duration'}
         grid={{
           left: 20,
           right: 50,
@@ -40,4 +41,5 @@ const ChartContainer = styled('div')`
   flex: 1;
   border: 1px solid ${p => p.theme.gray200};
   border-radius: ${p => p.theme.borderRadius};
+  margin-bottom: ${space(4)};
 `;
