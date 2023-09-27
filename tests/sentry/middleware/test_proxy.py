019f0fe3fdf8627a7e188992f94e4023c038756f
Zachary Collins <zachary.collins@sentry.io>
2023-09-26 17:49:53 -0700

    Test fix

--- tests/sentry/middleware/test_proxy.py
+++ tests/sentry/middleware/test_proxy.py
@@ -4,15 +4,16 @@ from dataclasses import asdict
 from functools import cached_property
 
 import pytest
-from django.db import router
 from django.http import HttpRequest
+from django.urls import reverse
 from pytest_django.live_server_helper import LiveServer
+from rest_framework.test import APIClient
 
-from sentry.db.postgres.transactions import in_test_hide_transaction_boundary
 from sentry.middleware.proxy import SetRemoteAddrFromForwardedFor
-from sentry.models import ApiKey, Organization, Team, User
-from sentry.silo import SiloMode, unguarded_write
-from sentry.testutils.cases import APITestCase, TestCase
+from sentry.models import ApiKey, Organization, Team
+from sentry.silo import SiloMode
+from sentry.testutils.asserts import assert_status_code
+from sentry.testutils.cases import APITestCase, TestCase, TransactionTestCase
 from sentry.testutils.factories import Factories
 from sentry.testutils.region import override_regions
 from sentry.testutils.silo import assume_test_silo_mode, control_silo_test, region_silo_test
@@ -52,39 +53,25 @@ test_region = Region(
 
 @region_silo_test(stable=True, regions=[test_region])
 @pytest.mark.usefixtures("live_server")
-class EndToEndAPIProxyTest(APITestCase):
+class EndToEndAPIProxyTest(TransactionTestCase):
     live_server: LiveServer
     endpoint = "sentry-api-0-organization-teams"
     method = "post"
-    user: User
     organization: Organization
     api_key: ApiKey
 
     @pytest.fixture(autouse=True)
     def initialize(self, live_server):
         self.live_server = live_server
+        self.client = APIClient()
 
-    @classmethod
-    def setUpClass(cls) -> None:
-        with in_test_hide_transaction_boundary():
-            cls.user = Factories.create_user()
-            with override_regions([test_region]):
-                cls.organization = Factories.create_organization(owner=cls.user, region="us")
-            cls.api_key = Factories.create_api_key(
-                organization=cls.organization, scope_list=["org:write", "org:admin", "team:write"]
-            )
-            super().setUpClass()
+    def tearDown(self) -> None:
+        self.live_server.stop()
 
-    @classmethod
-    def tearDownClass(cls) -> None:
-        with unguarded_write(router.db_for_write(User)), in_test_hide_transaction_boundary():
-            User.objects.filter(id=cls.user.id).delete()
-            ApiKey.objects.filter(id=cls.api_key.id).delete()
-        with unguarded_write(
-            router.db_for_write(Organization)
-        ), in_test_hide_transaction_boundary():
-            Organization.objects.filter(id=cls.organization.id).delete()
-        super().tearDownClass()
+    def get_response(self, *args, **params):
+        url = reverse(self.endpoint, args=args)
+        headers = params.pop("extra_headers", {})
+        return getattr(self.client, self.method)(url, format="json", data=params, **headers)
 
     def test_through_api_gateway(self):
         if SiloMode.get_current_mode() == SiloMode.MONOLITH:
@@ -93,20 +80,23 @@ class EndToEndAPIProxyTest(APITestCase):
         config = asdict(test_region)
         config["address"] = self.live_server.url
 
-        with override_regions([Region(**config)]), SiloMode.enter_single_process_silo_context(
-            SiloMode.CONTROL
-        ):
-            # self.login_as(user=self.user)
-            resp = self.get_success_response(
-                self.organization.slug,
-                name="hello world",
-                idp_provisioned=True,
-                status_code=201,
-                extra_headers=dict(
-                    HTTP_AUTHORIZATION=self.create_basic_auth_header(self.api_key.key)
-                ),
+        with override_regions([Region(**config)]):
+            self.organization = Factories.create_organization(owner=self.user, region="us")
+            self.api_key = Factories.create_api_key(
+                organization=self.organization, scope_list=["org:write", "org:admin", "team:write"]
             )
 
+            with SiloMode.enter_single_process_silo_context(SiloMode.CONTROL):
+                resp = self.get_response(
+                    self.organization.slug,
+                    name="hello world",
+                    idp_provisioned=True,
+                    extra_headers=dict(
+                        HTTP_AUTHORIZATION=self.create_basic_auth_header(self.api_key.key)
+                    ),
+                )
+
+        assert_status_code(resp, 201)
         result = json.loads(resp.getvalue())
         team = Team.objects.get(id=result["id"])
         assert team.idp_provisioned
