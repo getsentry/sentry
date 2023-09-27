0706460dd4f27edfef7429ba76f4357bc94b6db6
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-09-26 21:45:17 -0700

    test: Import from sentry-fixture inside test files [1] (#56810)
    <!-- Describe your PR here. -->

--- static/app/components/events/interfaces/breadcrumbs/breadcrumb/data/http.spec.tsx
+++ static/app/components/events/interfaces/breadcrumbs/breadcrumb/data/http.spec.tsx
@@ -1,3 +1,5 @@
+import {DataScrubbingRelayPiiConfig} from 'sentry-fixture/dataScrubbingRelayPiiConfig';
+
 import {initializeOrg} from 'sentry-test/initializeOrg';
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 import {textWithMarkupMatcher} from 'sentry-test/utils';
@@ -9,7 +11,7 @@ import {BreadcrumbLevelType, BreadcrumbType} from 'sentry/types/breadcrumbs';
 describe('Breadcrumb Data Http', function () {
   const project = TestStubs.Project({
     id: '0',
-    relayPiiConfig: JSON.stringify(TestStubs.DataScrubbingRelayPiiConfig()),
+    relayPiiConfig: JSON.stringify(DataScrubbingRelayPiiConfig()),
   });
 
   const {organization, router} = initializeOrg({
