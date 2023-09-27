c96e74906187d938b2af1672d8b128719ff67ea8
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-09-27 16:05:24 -0700

    feat(crons): Add get_prev_schedule (#57091)
    This will be used for computing when a missed check-in was expected to
    have happened.

--- tests/sentry/monitors/test_schedule.py
+++ tests/sentry/monitors/test_schedule.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.utils import timezone
 
-from sentry.monitors.schedule import get_next_schedule
+from sentry.monitors.schedule import get_next_schedule, get_prev_schedule
 from sentry.monitors.types import CrontabSchedule, IntervalSchedule
 
 
@@ -25,3 +25,20 @@ def test_get_next_schedule():
     interval_ts = ts.replace(hour=5, minute=42)
     expect = ts.replace(hour=7, minute=42)
     assert get_next_schedule(interval_ts, IntervalSchedule(interval=2, unit="hour")) == expect
+
+
+def test_get_prev_schedule():
+    start_ts = datetime(2019, 1, 1, 3, 30, 0, tzinfo=timezone.utc)
+    ts = datetime(2019, 1, 1, 5, 35, 0, tzinfo=timezone.utc)
+
+    # 00 * * * *: 5:35 -> 5:00
+    expect = ts.replace(hour=5, minute=0)
+    assert get_prev_schedule(start_ts, ts, CrontabSchedule("0 * * * *")) == expect
+
+    # 30 * * * *: 5:35 -> 5:30
+    expect = ts.replace(hour=5, minute=30)
+    assert get_prev_schedule(start_ts, ts, CrontabSchedule("30 * * * *")) == expect
+
+    # 2 hour interval: (start = 3:30) 5:35 -> 5:30
+    expect = ts.replace(hour=5, minute=30)
+    assert get_prev_schedule(start_ts, ts, IntervalSchedule(interval=2, unit="hour")) == expect
