b11fdbf962d3b703643ff1f71614e4093d726437
Abhijeet Prasad <aprasad@sentry.io>
2023-09-27 12:16:53 -0400

    clean up other span.domain usage

--- static/app/views/performance/database/databaseSpanSummaryPage.tsx
+++ static/app/views/performance/database/databaseSpanSummaryPage.tsx
@@ -72,7 +72,6 @@ function SpanSummaryPage({params}: Props) {
       SpanMetricsField.SPAN_OP,
       SpanMetricsField.SPAN_DESCRIPTION,
       SpanMetricsField.SPAN_ACTION,
-      SpanMetricsField.SPAN_DOMAIN,
       SpanMetricsField.SPAN_DOMAIN_ARRAY,
       'count()',
       `${SpanFunction.SPM}()`,
@@ -91,7 +90,7 @@ function SpanSummaryPage({params}: Props) {
     [SpanMetricsField.SPAN_OP]: string;
     [SpanMetricsField.SPAN_DESCRIPTION]: string;
     [SpanMetricsField.SPAN_ACTION]: string;
-    [SpanMetricsField.SPAN_DOMAIN]: string;
+    [SpanMetricsField.SPAN_DOMAIN_ARRAY]: string[];
     [SpanMetricsField.SPAN_GROUP]: string;
   };
 
