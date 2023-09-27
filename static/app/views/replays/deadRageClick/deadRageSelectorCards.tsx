3dcfb921af0243808173d5d7a6b8a3a224092e08
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-09-27 15:46:27 -0700

    feat(replays): new widgets with example replays

--- static/app/views/replays/deadRageClick/deadRageSelectorCards.tsx
+++ static/app/views/replays/deadRageClick/deadRageSelectorCards.tsx
@@ -1,89 +1,266 @@
-import {Fragment} from 'react';
+import {ComponentProps, Fragment, ReactNode, useMemo, useState} from 'react';
 import styled from '@emotion/styled';
 import {Location} from 'history';
 
-import {IconCursorArrow} from 'sentry/icons';
+import {LinkButton} from 'sentry/components/button';
+import EmptyStateWarning from 'sentry/components/emptyStateWarning';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import TextOverflow from 'sentry/components/textOverflow';
+import {IconCursorArrow, IconWarning} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
+import EventView from 'sentry/utils/discover/eventView';
+import getRouteStringFromRoutes from 'sentry/utils/getRouteStringFromRoutes';
 import useDeadRageSelectors from 'sentry/utils/replays/hooks/useDeadRageSelectors';
+import useReplayList from 'sentry/utils/replays/hooks/useReplayList';
 import {useLocation} from 'sentry/utils/useLocation';
-import SelectorTable from 'sentry/views/replays/deadRageClick/selectorTable';
+import useOrganization from 'sentry/utils/useOrganization';
+import {useRoutes} from 'sentry/utils/useRoutes';
+import {normalizeUrl} from 'sentry/utils/withDomainRequired';
+import Accordion from 'sentry/views/performance/landing/widgets/components/accordion';
+import {RightAlignedCell} from 'sentry/views/performance/landing/widgets/components/selectableList';
+import {
+  ContentContainer,
+  HeaderContainer,
+  HeaderTitleLegend,
+  StatusContainer,
+  Subtitle,
+  WidgetContainer,
+} from 'sentry/views/profiling/landing/styles';
+import {ReplayCell} from 'sentry/views/replays/replayTable/tableCell';
+import {ReplayListLocationQuery} from 'sentry/views/replays/types';
+
+function transformSelectorQuery(selector: string) {
+  const escaped = selector.replace(/"/g, '\\"');
+  return escaped.replace(/aria=/g, 'aria-label=');
+}
+
+function UseExampleReplays(selector, location, clickType, deadOrRage) {
+  const organization = useOrganization();
+  const query = transformSelectorQuery(selector);
+  const {project, environment, start, statsPeriod, utc, end} = location.query;
+  const emptyLocation: Location<ReplayListLocationQuery> = useMemo(() => {
+    return {
+      pathname: '',
+      search: '',
+      hash: '',
+      state: '',
+      action: 'PUSH' as const,
+      key: '',
+      query: {project, environment, start, statsPeriod, utc, end},
+    };
+  }, [project, environment, start, statsPeriod, utc, end]);
+
+  const eventView = useMemo(
+    () =>
+      EventView.fromNewQueryWithLocation(
+        {
+          id: '',
+          name: '',
+          version: 2,
+          fields: [
+            'activity',
+            'duration',
+            'id',
+            'project_id',
+            'user',
+            'finished_at',
+            'is_archived',
+            'started_at',
+            'urls',
+          ],
+          projects: [],
+          query: `${deadOrRage}.selector:"${query}"`,
+          orderby: `-${clickType}`,
+        },
+        emptyLocation
+      ),
+    [emptyLocation, query, clickType, deadOrRage]
+  );
+
+  const {replays, isFetching, fetchError} = useReplayList({
+    eventView,
+    location: emptyLocation,
+    organization,
+    perPage: 3,
+  });
+
+  const routes = useRoutes();
+  const referrer = getRouteStringFromRoutes(routes);
+
+  return (
+    <Fragment>
+      {fetchError || (!isFetching && !replays?.length) ? (
+        <EmptyStateWarning withIcon={false} small>
+          {t('No replays found')}
+        </EmptyStateWarning>
+      ) : (
+        replays?.map(r => {
+          return (
+            <ReplayCell
+              key="session"
+              replay={r}
+              eventView={eventView}
+              organization={organization}
+              referrer={referrer}
+              showUrl={false}
+              referrer_table="main"
+            />
+          );
+        })
+      )}
+    </Fragment>
+  );
+}
 
 function DeadRageSelectorCards() {
   const location = useLocation();
 
   return (
     <SplitCardContainer>
-      <DeadClickTable location={location} />
-      <RageClickTable location={location} />
+      <AccordionWidget
+        clickType="count_dead_clicks"
+        widgetTitle={t('Most Dead Clicks')}
+        deadOrRage="dead"
+        location={location}
+      />
+      <AccordionWidget
+        clickType="count_rage_clicks"
+        widgetTitle={t('Most Rage Clicks')}
+        deadOrRage="rage"
+        location={location}
+      />
     </SplitCardContainer>
   );
 }
 
-function DeadClickTable({location}: {location: Location<any>}) {
+function AccordionWidget({
+  location,
+  clickType,
+  deadOrRage,
+  widgetTitle,
+}: {
+  clickType: 'count_dead_clicks' | 'count_rage_clicks';
+  deadOrRage: string;
+  location: Location<any>;
+  widgetTitle: string;
+}) {
+  const [selectedListIndex, setSelectListIndex] = useState(0);
   const {isLoading, isError, data} = useDeadRageSelectors({
-    per_page: 4,
-    sort: '-count_dead_clicks',
+    per_page: 3,
+    sort: `-${clickType}`,
     cursor: undefined,
     prefix: 'selector_',
     isWidgetData: true,
   });
 
+  const filteredData = data.filter(d => (d[clickType] ?? 0) > 0);
+
   return (
-    <SelectorTable
-      data={data.filter(d => (d.count_dead_clicks ?? 0) > 0)}
-      isError={isError}
-      isLoading={isLoading}
-      location={location}
-      clickCountColumns={[{key: 'count_dead_clicks', name: 'dead clicks'}]}
-      title={
-        <Fragment>
-          <IconContainer>
-            <IconCursorArrow size="xs" color="yellow300" />
-          </IconContainer>
-          {t('Most Dead Clicks')}
-        </Fragment>
-      }
-      customHandleResize={() => {}}
-      clickCountSortable={false}
-    />
+    <Fragment>
+      <WidgetContainer>
+        <StyledHeaderContainer>
+          <HeaderTitleLegend>{widgetTitle}</HeaderTitleLegend>
+          <Subtitle>{t('Suggested replays to watch')}</Subtitle>
+        </StyledHeaderContainer>
+        <StyledContainerContainer>
+          {isLoading && (
+            <StatusContainer>
+              <LoadingIndicator />
+            </StatusContainer>
+          )}
+          {isError && (
+            <StatusContainer>
+              <IconWarning data-test-id="error-indicator" color="gray300" size="lg" />
+            </StatusContainer>
+          )}
+          {!isLoading && filteredData.length === 0 ? (
+            <EmptyStateWarning>
+              <p>{t('No results found')}</p>
+            </EmptyStateWarning>
+          ) : (
+            <Accordion
+              expandedIndex={selectedListIndex}
+              setExpandedIndex={setSelectListIndex}
+              items={filteredData.map(d => {
+                return {
+                  header: () => (
+                    <Fragment>
+                      <StyledText>
+                        <code>{d.dom_element}</code>
+                      </StyledText>
+                      <RightAlignedCell>
+                        {clickType === 'count_dead_clicks' ? (
+                          <DeadClickCount>
+                            <IconContainer>
+                              <IconCursorArrow size="xs" />
+                            </IconContainer>
+                            {d[clickType]}
+                          </DeadClickCount>
+                        ) : (
+                          <RageClickCount>
+                            <IconContainer>
+                              <IconCursorArrow size="xs" />
+                            </IconContainer>
+                            {d[clickType]}
+                          </RageClickCount>
+                        )}
+                      </RightAlignedCell>
+                    </Fragment>
+                  ),
+                  content: () =>
+                    UseExampleReplays(d.dom_element, location, clickType, deadOrRage),
+                };
+              })}
+            />
+          )}
+        </StyledContainerContainer>
+        <SearchButton
+          label={t('See all selectors')}
+          path="selectors"
+          sort={`-${clickType}`}
+        />
+      </WidgetContainer>
+    </Fragment>
   );
 }
 
-function RageClickTable({location}: {location: Location<any>}) {
-  const {isLoading, isError, data} = useDeadRageSelectors({
-    per_page: 4,
-    sort: '-count_rage_clicks',
-    cursor: undefined,
-    prefix: 'selector_',
-    isWidgetData: true,
-  });
+function SearchButton({
+  label,
+  sort,
+  path,
+  ...props
+}: {
+  label: ReactNode;
+  path: string;
+  sort: string;
+} & Omit<ComponentProps<typeof LinkButton>, 'size' | 'to'>) {
+  const location = useLocation();
+  const organization = useOrganization();
 
   return (
-    <SelectorTable
-      data={data.filter(d => (d.count_rage_clicks ?? 0) > 0)}
-      isError={isError}
-      isLoading={isLoading}
-      location={location}
-      clickCountColumns={[{key: 'count_rage_clicks', name: 'rage clicks'}]}
-      title={
-        <Fragment>
-          <IconContainer>
-            <IconCursorArrow size="xs" color="red300" />
-          </IconContainer>
-          {t('Most Rage Clicks')}
-        </Fragment>
-      }
-      customHandleResize={() => {}}
-      clickCountSortable={false}
-    />
+    <StyledButton
+      {...props}
+      size="xs"
+      to={{
+        pathname: normalizeUrl(`/organizations/${organization.slug}/replays/${path}/`),
+        query: {
+          ...location.query,
+          sort,
+          query: undefined,
+          cursor: undefined,
+        },
+      }}
+    >
+      {label}
+    </StyledButton>
   );
 }
 
 const SplitCardContainer = styled('div')`
   display: grid;
   grid-template-columns: 1fr 1fr;
-  grid-template-rows: max-content max-content;
+  grid-template-rows: max-content;
   grid-auto-flow: column;
   gap: 0 ${space(2)};
   align-items: stretch;
@@ -92,6 +269,36 @@ const SplitCardContainer = styled('div')`
 
 const IconContainer = styled('span')`
   margin-right: ${space(1)};
+`;
+
+const StyledText = styled('div')`
+  flex: 1;
+`;
+
+const DeadClickCount = styled(TextOverflow)`
+  color: ${p => p.theme.yellow300};
+`;
+
+const RageClickCount = styled(TextOverflow)`
+  color: ${p => p.theme.red300};
+`;
+
+const StyledHeaderContainer = styled(HeaderContainer)`
+  margin-bottom: ${space(1)};
+`;
+
+const StyledContainerContainer = styled(ContentContainer)`
+  justify-content: flex-start;
+`;
+
+const StyledButton = styled(LinkButton)`
+  width: 100%;
+  border-radius: ${p => p.theme.borderRadiusBottom};
+  padding: ${space(3)};
+  border-bottom: none;
+  border-left: none;
+  border-right: none;
+  font-size: ${p => p.theme.fontSizeMedium};
 `;
 
 export default DeadRageSelectorCards;
