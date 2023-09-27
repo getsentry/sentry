d7551018ed3f3426cbe457d920937fea10438e6d
Malachi Willey <malwilley@gmail.com>
2023-09-27 13:21:52 -0700

    ref(recommended-event): Rename from helpful event to recommended event (#56993)
    Some of these variables were still calling this a helpful event, but
    with the new UI we renamed the feature to "recommended" event. This
    changes the variable names and the endpoint URL.

--- static/app/components/groupPreviewTooltip/evidencePreview.spec.tsx
+++ static/app/components/groupPreviewTooltip/evidencePreview.spec.tsx
@@ -50,7 +50,7 @@ describe('EvidencePreview', () => {
     });
 
     MockApiClient.addMockResponse({
-      url: `/issues/group-id/events/helpful/`,
+      url: `/issues/group-id/events/recommended/`,
       body: event,
     });
 
--- static/app/views/issueDetails/groupDetails.spec.tsx
+++ static/app/views/issueDetails/groupDetails.spec.tsx
@@ -101,7 +101,7 @@ describe('groupDetails', () => {
       body: {...group},
     });
     MockApiClient.addMockResponse({
-      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/helpful/`,
+      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/recommended/`,
       statusCode: 200,
       body: {
         ...event,
@@ -170,7 +170,7 @@ describe('groupDetails', () => {
       statusCode: 404,
     });
     MockApiClient.addMockResponse({
-      url: `/organization/${defaultInit.organization.slug}/issues/${group.id}/events/helpful/`,
+      url: `/organization/${defaultInit.organization.slug}/issues/${group.id}/events/recommended/`,
       statusCode: 404,
     });
 
@@ -191,7 +191,7 @@ describe('groupDetails', () => {
       statusCode: 403,
     });
     MockApiClient.addMockResponse({
-      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/helpful/`,
+      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/recommended/`,
       statusCode: 403,
     });
 
@@ -229,7 +229,7 @@ describe('groupDetails', () => {
 
   it('renders issue event error', async function () {
     MockApiClient.addMockResponse({
-      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/helpful/`,
+      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/recommended/`,
       statusCode: 404,
     });
     createWrapper();
@@ -297,16 +297,16 @@ describe('groupDetails', () => {
     ).toBeInTheDocument();
   });
 
-  it('uses /helpful endpoint when feature flag is on and no event is provided', async function () {
-    const helpfulMock = MockApiClient.addMockResponse({
-      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/helpful/`,
+  it('uses /recommended endpoint when feature flag is on and no event is provided', async function () {
+    const recommendedMock = MockApiClient.addMockResponse({
+      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/recommended/`,
       statusCode: 200,
       body: event,
     });
 
     createWrapper();
 
-    await waitFor(() => expect(helpfulMock).toHaveBeenCalledTimes(1));
+    await waitFor(() => expect(recommendedMock).toHaveBeenCalledTimes(1));
   });
 
   it('uses /latest endpoint when default is set to latest', async function () {
@@ -335,10 +335,10 @@ describe('groupDetails', () => {
     await waitFor(() => expect(oldestMock).toHaveBeenCalledTimes(1));
   });
 
-  it('uses /helpful endpoint when default is set to recommended', async function () {
+  it('uses /recommended endpoint when default is set to recommended', async function () {
     ConfigStore.loadInitialData(TestStubs.Config({user: recommendedUser}));
     const recommendedMock = MockApiClient.addMockResponse({
-      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/helpful/`,
+      url: `/organizations/${defaultInit.organization.slug}/issues/${group.id}/events/recommended/`,
       statusCode: 200,
       body: event,
     });
