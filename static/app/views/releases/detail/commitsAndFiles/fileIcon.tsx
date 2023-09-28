4783baaa60057552694ff1a00a52b0b15ff750a1
Scott Cooper <scttcper@gmail.com>
2023-09-28 15:52:55 -0700

    ref(ui): Cleanup files changed styles (#57174)

--- static/app/views/releases/detail/commitsAndFiles/fileIcon.tsx
+++ static/app/views/releases/detail/commitsAndFiles/fileIcon.tsx
@@ -4,22 +4,19 @@ import {IconFile} from 'sentry/icons';
 import {fileExtensionToPlatform, getFileExtension} from 'sentry/utils/fileExtension';
 import theme from 'sentry/utils/theme';
 
-type Props = {
+interface FileIconProps {
   fileName: string;
-  className?: string;
-  size?: string;
-};
+}
 
-function FileIcon({fileName, size: providedSize = 'sm'}: Props) {
+function FileIcon({fileName}: FileIconProps) {
   const fileExtension = getFileExtension(fileName);
   const iconName = fileExtension ? fileExtensionToPlatform(fileExtension) : null;
-  const size = theme.iconSizes[providedSize] ?? providedSize;
 
   if (!iconName) {
-    return <IconFile size={size} />;
+    return <IconFile size="sm" />;
   }
 
-  return <PlatformIcon platform={iconName} size={size} />;
+  return <PlatformIcon platform={iconName} size={theme.iconSizes.sm} />;
 }
 
 export default FileIcon;
