830e797586658d029399550816446ea3e282b665
Scott Cooper <scttcper@gmail.com>
2023-09-28 14:59:22 -0700

    ref(ui): Cleanup files changed styles
    - Styles did not need to be passed down
    - remove from the component directory, these are specific to releases

--- static/app/views/releases/detail/commitsAndFiles/fileChange.tsx
+++ static/app/views/releases/detail/commitsAndFiles/fileChange.tsx
@@ -1,34 +1,28 @@
 import styled from '@emotion/styled';
 
 import AvatarList from 'sentry/components/avatar/avatarList';
-import FileIcon from 'sentry/components/fileIcon';
 import {ListGroupItem} from 'sentry/components/listGroup';
 import TextOverflow from 'sentry/components/textOverflow';
 import {space} from 'sentry/styles/space';
-import {AvatarUser, CommitAuthor} from 'sentry/types';
+import type {AvatarUser, CommitAuthor} from 'sentry/types';
 
-type Props = {
+import FileIcon from './fileIcon';
+
+interface FileChangeProps {
   authors: CommitAuthor[];
   filename: string;
-  className?: string;
-};
+}
 
-function FileChange({filename, authors, className}: Props) {
+function FileChange({filename, authors}: FileChangeProps) {
   return (
-    <FileItem className={className}>
+    <FileItem>
       <Filename>
         <FileIconWrapper>
           <FileIcon fileName={filename} />
         </FileIconWrapper>
         <TextOverflow>{filename}</TextOverflow>
       </Filename>
-      <div>
-        <AvatarList
-          users={authors as AvatarUser[]}
-          avatarSize={25}
-          typeAvatars="authors"
-        />
-      </div>
+      <AvatarList users={authors as AvatarUser[]} avatarSize={25} typeAvatars="authors" />
     </FileItem>
   );
 }
@@ -37,18 +31,29 @@ const FileItem = styled(ListGroupItem)`
   display: flex;
   align-items: center;
   justify-content: space-between;
+  gap: ${space(3)};
+
+  border-radius: 0;
+  border-left: none;
+  border-right: none;
+  border-top: none;
+  :last-child {
+    border: none;
+    border-radius: 0;
+  }
 `;
 
 const Filename = styled('div')`
   font-size: ${p => p.theme.fontSizeMedium};
   display: grid;
-  gap: ${space(1)};
-  margin-right: ${space(3)};
-  align-items: center;
   grid-template-columns: max-content 1fr;
+  gap: ${space(1)};
+  align-items: center;
 `;
 
 const FileIconWrapper = styled('div')`
+  display: flex;
+  align-items: center;
   color: ${p => p.theme.gray200};
   border-radius: 3px;
 `;
