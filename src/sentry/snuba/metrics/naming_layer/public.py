f28f1279449dfe7a3d13af46a5ce03a16df7e956
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2023-09-29 12:41:10 -0400

    feat(stats-detectors): New metric for duration (#56202)
    We want to materialize a new less granular metric that will only include
    transaction name and duration.
    
    The goal is to run timeseries analysis and find regressions in duration
    for specific transaction names. This will be used in Statistical
    Detectors so we'll need to fetch timeseries faster than we currently do.
    We're not interested in any other tags.
    
    Cardinality analysis results were captured
    [here](https://getsentry.atlassian.net/browse/REQ3S-54)
    
    Relay PR: https://github.com/getsentry/relay/pull/2513

--- src/sentry/snuba/metrics/naming_layer/public.py
+++ src/sentry/snuba/metrics/naming_layer/public.py
@@ -96,6 +96,9 @@ class TransactionMetricKey(Enum):
     TEAM_KEY_TRANSACTION = "transactions.team_key_transaction"
     HTTP_ERROR_RATE = "transaction.http_error_rate"
 
+    # Less granular coarse metrics
+    DURATION_LIGHT = "d:transactions/duration_light@millisecond"
+
     # Span metrics.
     # NOTE: These might be moved to their own namespace soon.
     SPAN_USER = "span.user"
