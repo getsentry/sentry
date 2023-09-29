2e65c6c292ac4e5af56bfb699c8931c670a6497c
Arthur Knaus <arthur.knaus@sentry.io>
2023-09-29 13:11:01 +0200

    feat(onboarding): Simplify platform javascript-gatsby

--- static/app/gettingStartedDocs/javascript/gatsby.tsx
+++ static/app/gettingStartedDocs/javascript/gatsby.tsx
@@ -43,19 +43,34 @@ export const steps = ({
 }: Partial<StepProps> = {}): LayoutProps['steps'] => [
   {
     type: StepType.INSTALL,
-    description: t(
-      'Sentry captures data by using an SDK within your application’s runtime.'
+    description: (
+      <p>
+        {tct(
+          'Add the Sentry SDK as a dependency using [codeYarn:yarn] or [codeNpm:npm]:',
+          {
+            codeYarn: <code />,
+            codeNpm: <code />,
+          }
+        )}
+      </p>
     ),
     configurations: [
       {
         language: 'bash',
-        code: `
-# Using yarn
-yarn add @sentry/gatsby
-
-# Using npm
-npm install --save @sentry/gatsby
-        `,
+        code: [
+          {
+            label: 'npm',
+            value: 'npm',
+            language: 'bash',
+            code: 'npm install --save @sentry/gatsby',
+          },
+          {
+            label: 'Yarn',
+            value: 'yarn',
+            language: 'bash',
+            code: 'yarn add @sentry/gatsby',
+          },
+        ],
       },
     ],
   },
