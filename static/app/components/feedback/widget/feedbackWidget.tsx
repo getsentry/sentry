abab58dd999538503d5c54ba68d3bd1f5f630bdc
Billy Vong <billyvg@users.noreply.github.com>
2023-09-29 12:16:39 -0400

    fix(feedback): Fix for dark moders (#57067)
    dark mode--
    
    
    https://github.com/getsentry/sentry/assets/79684/3f423cc3-1da6-4468-9889-cb97c7824efd
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/components/feedback/widget/feedbackWidget.tsx
+++ static/app/components/feedback/widget/feedbackWidget.tsx
@@ -1,5 +1,8 @@
 import {getCurrentHub} from '@sentry/react';
 
+import ConfigStore from 'sentry/stores/configStore';
+import {useLegacyStore} from 'sentry/stores/useLegacyStore';
+
 import {FeedbackButton} from './feedbackButton';
 import {FeedbackModal} from './feedbackModal';
 
@@ -17,14 +20,19 @@ export default function FeedbackWidget({
   title = 'Report a Bug',
   type,
 }: FeedbackWidgetProps) {
+  const config = useLegacyStore(ConfigStore);
+
   // Don't render anything if Sentry SDK is not already loaded
   if (!getCurrentHub()) {
     return null;
   }
 
+  const widgetTheme = config.theme === 'dark' ? 'dark' : 'light';
   return (
-    <FeedbackModal title={title} type={type}>
-      {({open, showModal}) => (open ? null : <FeedbackButton onClick={showModal} />)}
+    <FeedbackModal title={title} type={type} widgetTheme={widgetTheme}>
+      {({open, showModal}) =>
+        open ? null : <FeedbackButton onClick={showModal} widgetTheme={widgetTheme} />
+      }
     </FeedbackModal>
   );
 }
