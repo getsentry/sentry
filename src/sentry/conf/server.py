dc0e85911e01d611b06f9181a0ec495c5df05e19
Gabe Villalobos <GabeVillalobos@users.noreply.github.com>
2023-09-29 13:22:39 -0700

    chore(hybrid-cloud): Adds write logic for org slug reservations (#56975)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -733,7 +733,6 @@ CELERY_IMPORTS = (
     "sentry.tasks.low_priority_symbolication",
     "sentry.tasks.merge",
     "sentry.tasks.options",
-    "sentry.tasks.organization_mapping",
     "sentry.tasks.ping",
     "sentry.tasks.post_process",
     "sentry.tasks.process_buffer",
@@ -942,12 +941,6 @@ CELERYBEAT_SCHEDULE_CONTROL = {
         "task": "sentry.tasks.integrations.kickoff_vsts_subscription_check",
         "schedule": crontab_with_minute_jitter(hour="*/6"),
         "options": {"expires": 60 * 25, "queue": "integrations.control"},
-    },
-    "hybrid-cloud-repair-mappings": {
-        "task": "sentry.tasks.organization_mapping.repair_mappings",
-        # Run every hour
-        "schedule": crontab(minute=0, hour="*/1"),
-        "options": {"expires": 3600},
     },
 }
 
