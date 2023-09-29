adf55673daec3455347dab32913a887ac2fa375d
Josh Ferge <josh.ferge@sentry.io>
2023-09-29 11:07:06 -0700

    ref(feedback): create feedback base endpoint  (#57190)

--- src/sentry/feedback/endpoints/project_feedback_details.py
+++ src/sentry/feedback/endpoints/project_feedback_details.py
@@ -1,53 +1,29 @@
 from __future__ import annotations
 
-from uuid import UUID
-
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry import features
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
-from sentry.api.bases.project import ProjectEndpoint
-from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.serializers.base import serialize
+from sentry.feedback.endpoints.base import ProjectFeedbackEndpoint
 from sentry.feedback.models import Feedback
 from sentry.feedback.serializers import FeedbackSerializer
 from sentry.models.project import Project
 
 
 @region_silo_endpoint
-class ProjectFeedbackDetailsEndpoint(ProjectEndpoint):
+class ProjectFeedbackDetailsEndpoint(ProjectFeedbackEndpoint):
     publish_status = {
         "GET": ApiPublishStatus.EXPERIMENTAL,
         "DELETE": ApiPublishStatus.EXPERIMENTAL,
     }
     owner = ApiOwner.FEEDBACK
 
-    def get(self, request: Request, project: Project, feedback_id: UUID) -> Response:
-        if not features.has(
-            "organizations:user-feedback-ingest", project.organization, actor=request.user
-        ):
-            return Response(status=404)
-
-        try:
-            feedback = Feedback.objects.get(feedback_id=feedback_id, project_id=project.id)
-        except Feedback.DoesNotExist:
-            raise ResourceDoesNotExist
-
+    def get(self, request: Request, project: Project, feedback: Feedback) -> Response:
         return self.respond(serialize(feedback, request.user, FeedbackSerializer()))
 
-    def delete(self, request: Request, project: Project, feedback_id: UUID) -> Response:
-        if not features.has(
-            "organizations:user-feedback-ingest", project.organization, actor=request.user
-        ):
-            return Response(status=404)
-
-        try:
-            feedback = Feedback.objects.get(feedback_id=feedback_id, project_id=project.id)
-        except Feedback.DoesNotExist:
-            raise ResourceDoesNotExist
-
+    def delete(self, request: Request, project: Project, feedback: Feedback) -> Response:
         feedback.delete()
         return Response(status=204)
