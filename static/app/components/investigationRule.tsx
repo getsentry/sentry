1b852b29b859f615d337916d4fb41d05dca37511
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2023-10-02 13:12:00 +0200

    move investigation rule into its own component file

---
+++ static/app/components/investigationRule.tsx
@@ -0,0 +1,216 @@
+import styled from '@emotion/styled';
+import moment from 'moment';
+
+import {addErrorMessage, addSuccessMessage} from 'sentry/actionCreators/indicator';
+import {Button} from 'sentry/components/button';
+import ExternalLink from 'sentry/components/links/externalLink';
+import {Tooltip} from 'sentry/components/tooltip';
+import {IconQuestion, IconTag} from 'sentry/icons';
+import {t, tct} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {OrganizationSummary} from 'sentry/types';
+import EventView from 'sentry/utils/discover/eventView';
+import {
+  ApiQueryKey,
+  useApiQuery,
+  useMutation,
+  useQueryClient,
+} from 'sentry/utils/queryClient';
+import useApi from 'sentry/utils/useApi';
+
+// Number of samples under which we can trigger an investigation rule
+const INVESTIGATION_MAX_SAMPLES_TRIGGER = 5;
+
+type Props = {
+  eventView: EventView;
+  numSamples: number | null | undefined;
+  organization: OrganizationSummary;
+};
+
+type CustomDynamicSamplingRule = {
+  condition: Record<string, any>;
+  dateAdded: string;
+  endDate: string;
+  numSamples: number;
+  orgId: string;
+  projects: number[];
+  ruleId: number;
+  sampleRate: number;
+  startDate: string;
+};
+type CreateCustomRuleVariables = {
+  organization: OrganizationSummary;
+  period: string | null;
+  projects: number[];
+  query: string;
+};
+
+function makeRuleExistsQueryKey(
+  query: string,
+  projects: number[],
+  organization: OrganizationSummary
+): ApiQueryKey {
+  return [
+    `/api/0/organizations/${organization.slug}/dynamic-sampling/custom-rules/`,
+    {
+      query: {
+        project: projects,
+        query,
+      },
+    },
+  ];
+}
+
+function hasTooFewSamples(numSamples: number | null | undefined) {
+  // check if we have got the samples, but they are too few of them
+  return (
+    numSamples !== null &&
+    numSamples !== undefined &&
+    numSamples < INVESTIGATION_MAX_SAMPLES_TRIGGER
+  );
+}
+
+function useGetExistingRule(
+  query: string,
+  projects: number[],
+  organization: OrganizationSummary,
+  numSamples: number | null | undefined
+) {
+  const enabled = hasTooFewSamples(numSamples);
+
+  const result = useApiQuery<CustomDynamicSamplingRule | '' | null>(
+    makeRuleExistsQueryKey(query, projects, organization),
+    {
+      staleTime: 0,
+      enabled,
+    }
+  );
+
+  if (result.data === '') {
+    // cleanup, the endpoint returns a 204 (with no body), change it to null
+    result.data = null;
+  }
+
+  return result;
+}
+
+function useCreateInvestigationRuleMutation(vars: CreateCustomRuleVariables) {
+  const api = useApi();
+  const queryClient = useQueryClient();
+  const {mutate} = useMutation<
+    CustomDynamicSamplingRule,
+    Error,
+    CreateCustomRuleVariables
+  >({
+    mutationFn: (variables: CreateCustomRuleVariables) => {
+      const {organization} = variables;
+      const endpoint = `/api/0/organizations/${organization.slug}/dynamic-sampling/custom-rules/`;
+      return api.requestPromise(endpoint, {
+        method: 'POST',
+        data: variables,
+      });
+    },
+    onSuccess: (_data: CustomDynamicSamplingRule) => {
+      addSuccessMessage(t('Successfully created investigation rule'));
+      // invalidate the rule-exists query
+      queryClient.invalidateQueries(
+        makeRuleExistsQueryKey(vars.query, vars.projects, vars.organization)
+      );
+    },
+    onError: (_error: Error) => {
+      addErrorMessage(t('Unable to create investigation rule'));
+    },
+  });
+  return mutate;
+}
+
+const InvestigationInProgressNotification = styled('span')`
+  margin: ${space(1.5)};
+  font-size: ${p => p.theme.fontSizeMedium};
+  color: ${p => p.theme.subText};
+  font-weight: 600;
+  display: inline-flex;
+  align-items: center;
+  gap: ${space(0.5)};
+`;
+
+export function InvestigationRuleCreation(props: Props) {
+  const projects = [...props.eventView.project];
+  const organization = props.organization;
+  const period = props.eventView.statsPeriod || null;
+  const query = props.eventView.getQuery();
+  const createInvestigationRule = useCreateInvestigationRuleMutation({
+    query,
+    projects,
+    organization,
+    period,
+  });
+  const request = useGetExistingRule(query, projects, organization, props.numSamples);
+
+  if (!hasTooFewSamples(props.numSamples)) {
+    // no results yet (we can't take a decision) or enough results,
+    // we don't need investigation rule UI
+    return null;
+  }
+  if (request.isLoading) {
+    return null;
+  }
+
+  if (request.error !== null) {
+    const errorResponse = t('Unable to fetch custom performance metrics');
+    addErrorMessage(errorResponse);
+    return null;
+  }
+
+  const rule = request.data;
+  const haveInvestigationRuleInProgress = rule !== null;
+
+  if (haveInvestigationRuleInProgress) {
+    // investigation rule in progress just show a message
+    const existingRule = rule as CustomDynamicSamplingRule;
+    const ruleStartDate = new Date(existingRule.startDate);
+    const now = new Date();
+    const interval = moment.duration(now.getTime() - ruleStartDate.getTime()).humanize();
+
+    return (
+      <InvestigationInProgressNotification>
+        {tct('Collecting samples since [interval]  ago.', {interval})}
+
+        <Tooltip
+          isHoverable
+          title={tct(
+            'A user has temporarily adjusted retention priorities, increasing the odds of getting events matching your search query. [link:Learn more.]',
+            // TODO find out where this link is pointing to
+            {
+              link: <ExternalLink href="https://docs.sentry.io" />,
+            }
+          )}
+        >
+          <IconQuestion size="xs" color="subText" />
+        </Tooltip>
+      </InvestigationInProgressNotification>
+    );
+  }
+
+  // no investigation rule in progress show a button to create one
+  return (
+    <Tooltip
+      isHoverable
+      title={tct(
+        'We can find more events that match your search query by adjusting your retention priorities for an hour, increasing the odds of getting matching events. [link:Learn more.]',
+        // TODO find out where this link is pointing to
+        {
+          link: <ExternalLink href="https://docs.sentry.io" />,
+        }
+      )}
+    >
+      <Button
+        size="sm"
+        onClick={() => createInvestigationRule({organization, period, projects, query})}
+        icon={<IconTag size="xs" />}
+      >
+        {t('Get Samples')}
+      </Button>
+    </Tooltip>
+  );
+}
--- static/app/views/discover/table/tableActions.tsx
+++ static/app/views/discover/table/tableActions.tsx
@@ -1,36 +1,21 @@
 import {Fragment} from 'react';
-import styled from '@emotion/styled';
 import {Location} from 'history';
-import moment from 'moment';
 
-import {addErrorMessage, addSuccessMessage} from 'sentry/actionCreators/indicator';
 import Feature from 'sentry/components/acl/feature';
 import FeatureDisabled from 'sentry/components/acl/featureDisabled';
 import GuideAnchor from 'sentry/components/assistant/guideAnchor';
 import {Button} from 'sentry/components/button';
 import DataExport, {ExportQueryType} from 'sentry/components/dataExport';
 import {Hovercard} from 'sentry/components/hovercard';
-import ExternalLink from 'sentry/components/links/externalLink';
-import {Tooltip} from 'sentry/components/tooltip';
-import {IconDownload, IconQuestion, IconStack, IconTag} from 'sentry/icons';
-import {t, tct} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
+import {InvestigationRuleCreation} from 'sentry/components/investigationRule';
+import {IconDownload, IconStack, IconTag} from 'sentry/icons';
+import {t} from 'sentry/locale';
 import {OrganizationSummary} from 'sentry/types';
 import {trackAnalytics} from 'sentry/utils/analytics';
 import {TableData} from 'sentry/utils/discover/discoverQuery';
 import EventView from 'sentry/utils/discover/eventView';
-import {
-  ApiQueryKey,
-  useApiQuery,
-  useMutation,
-  useQueryClient,
-} from 'sentry/utils/queryClient';
-import useApi from 'sentry/utils/useApi';
 
 import {downloadAsCsv} from '../utils';
-
-// Number of samples under which we can trigger an investigation rule
-const INVESTIGATION_MAX_SAMPLES_TRIGGER = 5;
 
 type Props = {
   error: string | null;
@@ -167,10 +152,14 @@ function FeatureWrapper(props: FeatureWrapperProps) {
 }
 
 function TableActions(props: Props) {
+  const investigationRuleProps = {...props, numSamples: props.tableData?.data?.length};
   return (
     <div className="table-actions">
       <Fragment>
-        <InvestigationRuleCreation {...props} key="investigationRuleCreation" />
+        <InvestigationRuleCreation
+          {...investigationRuleProps}
+          key="investigationRuleCreation"
+        />
         <FeatureWrapper {...props} key="edit">
           {renderEditButton}
         </FeatureWrapper>
@@ -180,186 +169,6 @@ function TableActions(props: Props) {
         {renderSummaryButton(props)}
       </Fragment>
     </div>
-  );
-}
-
-type CustomRule = {
-  condition: Record<string, any>;
-  dateAdded: string;
-  endDate: string;
-  numSamples: number;
-  orgId: string;
-  projects: number[];
-  ruleId: number;
-  sampleRate: number;
-  startDate: string;
-};
-
-type CreateCustomRuleVariables = {
-  organization: OrganizationSummary;
-  period: string | null;
-  projects: number[];
-  query: string;
-};
-
-function makeRuleExistsQueryKey(
-  query: string,
-  projects: number[],
-  organization: OrganizationSummary
-): ApiQueryKey {
-  return [
-    `/api/0/organizations/${organization.slug}/dynamic-sampling/custom-rules/`,
-    {
-      query: {
-        project: projects,
-        query,
-      },
-    },
-  ];
-}
-
-function useGetExistingRule(
-  query: string,
-  projects: number[],
-  organization: OrganizationSummary,
-  tableData: TableData | null | undefined
-) {
-  const enabled =
-    tableData?.data && tableData.data.length < INVESTIGATION_MAX_SAMPLES_TRIGGER;
-
-  const result = useApiQuery<CustomRule | '' | null>(
-    makeRuleExistsQueryKey(query, projects, organization),
-    {
-      staleTime: 0,
-      enabled,
-    }
-  );
-
-  if (result.data === '') {
-    // cleanup, the endpoint returns a 204 (with no body), change it to null
-    result.data = null;
-  }
-
-  return result;
-}
-
-function useCreateInvestigationRuleMutation(vars: CreateCustomRuleVariables) {
-  const api = useApi();
-  const queryClient = useQueryClient();
-  const {mutate} = useMutation<CustomRule, Error, CreateCustomRuleVariables>({
-    mutationFn: (variables: CreateCustomRuleVariables) => {
-      const {organization} = variables;
-      const endpoint = `/api/0/organizations/${organization.slug}/dynamic-sampling/custom-rules/`;
-      return api.requestPromise(endpoint, {
-        method: 'POST',
-        data: variables,
-      });
-    },
-    onSuccess: (_data: CustomRule) => {
-      addSuccessMessage(t('Successfully created investigation rule'));
-      // invalidate the rule-exists query
-      queryClient.invalidateQueries(
-        makeRuleExistsQueryKey(vars.query, vars.projects, vars.organization)
-      );
-    },
-    onError: (_error: Error) => {
-      addErrorMessage(t('Unable to create investigation rule'));
-    },
-  });
-  return mutate;
-}
-
-const InvestigationInProgressNotification = styled('span')`
-  margin: ${space(1.5)};
-  font-size: ${p => p.theme.fontSizeMedium};
-  color: ${p => p.theme.subText};
-  font-weight: 600;
-  display: inline-flex;
-  align-items: center;
-  gap: ${space(0.5)};
-`;
-
-function InvestigationRuleCreation(props: Props) {
-  const projects = [...props.eventView.project];
-  const organization = props.organization;
-  const period = props.eventView.statsPeriod || null;
-  const query = props.eventView.getQuery();
-  const createInvestigationRule = useCreateInvestigationRuleMutation({
-    query,
-    projects,
-    organization,
-    period,
-  });
-  const request = useGetExistingRule(query, projects, organization, props.tableData);
-
-  if (
-    !props.tableData?.data ||
-    props.tableData.data.length > INVESTIGATION_MAX_SAMPLES_TRIGGER
-  ) {
-    // no results yet (we can't take a decision) or enough results,
-    // we don't need investigation rule UI
-    return null;
-  }
-  if (request.isLoading) {
-    return null;
-  }
-
-  if (request.error !== null) {
-    const errorResponse = t('Unable to fetch custom performance metrics');
-    addErrorMessage(errorResponse);
-    return null;
-  }
-
-  const rule = request.data;
-  const haveInvestigationRuleInProgress = rule !== null;
-
-  if (haveInvestigationRuleInProgress) {
-    // investigation rule in progress just show a message
-    const existingRule = rule as CustomRule;
-    const ruleStartDate = new Date(existingRule.startDate);
-    const now = new Date();
-    const interval = moment.duration(now.getTime() - ruleStartDate.getTime()).humanize();
-
-    return (
-      <InvestigationInProgressNotification>
-        {tct('Collecting samples since [interval]  ago.', {interval})}
-
-        <Tooltip
-          isHoverable
-          title={tct(
-            'A user has temporarily adjusted retention priorities, increasing the odds of getting events matching your search query. [link:Learn more.]',
-            // TODO find out where this link is pointing to
-            {
-              link: <ExternalLink href="https://docs.sentry.io" />,
-            }
-          )}
-        >
-          <IconQuestion size="xs" color="subText" />
-        </Tooltip>
-      </InvestigationInProgressNotification>
-    );
-  }
-
-  // no investigation rule in progress show a button to create one
-  return (
-    <Tooltip
-      isHoverable
-      title={tct(
-        'We can find more events that match your search query by adjusting your retention priorities for an hour, increasing the odds of getting matching events. [link:Learn more.]',
-        // TODO find out where this link is pointing to
-        {
-          link: <ExternalLink href="https://docs.sentry.io" />,
-        }
-      )}
-    >
-      <Button
-        size="sm"
-        onClick={() => createInvestigationRule({organization, period, projects, query})}
-        icon={<IconTag size="xs" />}
-      >
-        {t('Get Samples')}
-      </Button>
-    </Tooltip>
   );
 }
 
