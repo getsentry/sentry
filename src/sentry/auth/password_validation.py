f9e7d1cc4b7380242a71dc03fa4ceae5320760ae
Alexander Tarasov <alex.tarasov@sentry.io>
2023-10-02 13:33:36 +0200

    remove MinimumLengthValidator: exactly the same code as in upstream django

--- src/sentry/auth/password_validation.py
+++ src/sentry/auth/password_validation.py
@@ -69,34 +69,6 @@ def _password_validators_help_text_html(password_validators=None):
 password_validators_help_text_html = lazy(_password_validators_help_text_html, str)
 
 
-class MinimumLengthValidator:
-    """
-    Validate whether the password is of a minimum length.
-    """
-
-    def __init__(self, min_length=8):
-        self.min_length = min_length
-
-    def validate(self, password, user=None):
-        if len(password) < self.min_length:
-            raise ValidationError(
-                ngettext(
-                    "This password is too short. It must contain at least %(min_length)d character.",
-                    "This password is too short. It must contain at least %(min_length)d characters.",
-                    self.min_length,
-                ),
-                code="password_too_short",
-                params={"min_length": self.min_length},
-            )
-
-    def get_help_text(self):
-        return ngettext(
-            "Your password must contain at least %(min_length)d character.",
-            "Your password must contain at least %(min_length)d characters.",
-            self.min_length,
-        ) % {"min_length": self.min_length}
-
-
 class MaximumLengthValidator:
     """
     Validate whether the password is of a maximum length.
