0fb06fd9e6bc3839a48631a34b504921869d5eee
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-10-02 12:38:46 -0700

    ref(replays): add more cases to transformSelectorQuery

--- static/app/views/replays/deadRageClick/selectorTable.tsx
+++ static/app/views/replays/deadRageClick/selectorTable.tsx
@@ -48,7 +48,9 @@ export function transformSelectorQuery(selector: string) {
   return selector
     .replaceAll('"', `\\"`)
     .replaceAll('aria=', 'aria-label=')
-    .replaceAll('testid=', 'data-test-id=');
+    .replaceAll('testid=', 'data-test-id=')
+    .replaceAll(':', '\\:')
+    .replaceAll('..', '.');
 }
 
 interface Props {
