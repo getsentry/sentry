5abcae7e6e227d34fc835604378cef0846f30034
Cathy Teng <cathy.teng@sentry.io>
2023-10-03 10:13:10 -0700

    make endpoint public

--- src/sentry/api/endpoints/organization_stats_summary.py
+++ src/sentry/api/endpoints/organization_stats_summary.py
@@ -119,7 +119,7 @@ class StatsSummaryApiResponse(TypedDict):
 @extend_schema(tags=["Organizations"])
 @region_silo_endpoint
 class OrganizationStatsSummaryEndpoint(OrganizationEventsEndpointBase):
-    publish_status = {"GET": ApiPublishStatus.EXPERIMENTAL}
+    publish_status = {"GET": ApiPublishStatus.PUBLIC}
     owner = ApiOwner.ENTERPRISE
 
     @extend_schema(
