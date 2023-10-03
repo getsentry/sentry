250c2e8fb4e5bc448c004464734ef28b6ed033ef
Dameli Ushbayeva <d.ushbayeva@gmail.com>
2023-10-03 14:52:24 -0400

    fix types

--- tests/sentry/statistical_detectors/test_issue_platform_adapter.py
+++ tests/sentry/statistical_detectors/test_issue_platform_adapter.py
@@ -8,7 +8,7 @@ from sentry.statistical_detectors.issue_platform_adapter import send_regressions
 
 @mock.patch("sentry.statistical_detectors.issue_platform_adapter.produce_occurrence_to_kafka")
 def test_send_regressions_to_platform(mock_produce_occurrence_to_kafka):
-    project_id = 123
+    project_id = "123"
 
     mock_regressions: List[BreakpointData] = [
         {
@@ -35,7 +35,7 @@ def test_send_regressions_to_platform(mock_produce_occurrence_to_kafka):
     assert dict(
         occurrence,
         **{
-            "project_id": project_id,
+            "project_id": 123,
             "issue_title": "Exp Duration Regression",
             "subtitle": "Increased from 14.0ms to 28.0ms (P95)",
             "resource_id": None,
@@ -57,7 +57,7 @@ def test_send_regressions_to_platform(mock_produce_occurrence_to_kafka):
     assert dict(
         event,
         **{
-            "project_id": project_id,
+            "project_id": 123,
             "transaction": "foo",
             "event_id": occurrence["event_id"],
             "platform": "python",
