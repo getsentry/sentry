06c7955af94537c7bdb131150090fec74a813f12
17hogeju <17hogeju@gmail.com>
2023-10-03 12:54:50 -0700

    fix(discord): Ensure validation of metric alert discord channel ids

--- src/sentry/integrations/discord/utils/channel.py
+++ src/sentry/integrations/discord/utils/channel.py
@@ -8,10 +8,10 @@ from sentry.shared_integrations.exceptions.base import ApiError
 
 from . import logger
 
-NO_CHANNEL_MESSAGE = "We couldn't find a channel with that ID. Make sure you have the correct server selected and are providing a Discord channel ID (not a channel name)."
 
-
-def validate_channel_id(channel_id: str, guild_id: str, integration_id: int | None) -> None:
+def validate_channel_id(
+    channel_id: str, guild_id: str, integration_id: int | None, guild_name: str | None
+) -> None:
     """
     Make sure that for this integration, the channel exists, belongs to this
     integration, and our bot has access to it.
@@ -25,16 +25,18 @@ def validate_channel_id(channel_id: str, guild_id: str, integration_id: int | No
                 "rule.discord.channel_info_failed",
                 extra={
                     "channel_id": channel_id,
+                    "guild_name": guild_name,
                     "reason": "channel ID missing or malformed",
                     "code": e.code,
                 },
             )
-            raise ValidationError("Discord channel id is missing or not formatted correctly")
+            raise ValidationError("Discord channel id is missing or formatted incorrectly")
         elif e.code == 403:
             logger.info(
                 "rule.discord.channel_info_failed",
                 extra={
                     "channel_id": channel_id,
+                    "guild_name": guild_name,
                     "reason": "channel access not allowed",
                     "code": e.code,
                 },
@@ -45,6 +47,7 @@ def validate_channel_id(channel_id: str, guild_id: str, integration_id: int | No
                 "rule.discord.channel_info_failed",
                 extra={
                     "channel_id": channel_id,
+                    "guild_name": guild_name,
                     "reason": "channel not found",
                     "code": e.code,
                 },
@@ -57,6 +60,7 @@ def validate_channel_id(channel_id: str, guild_id: str, integration_id: int | No
                     "guild_id": guild_id,
                     "channel_id": channel_id,
                     "reason": "channel does not belong to indicated guild (server)",
+                    "guild_name": guild_name,
                     "code": e.code,
                 },
             )
@@ -71,6 +75,10 @@ def validate_channel_id(channel_id: str, guild_id: str, integration_id: int | No
         # so we don't expose other guilds' channel IDs.
         logger.info(
             "rule.discord.wrong_guild_for_channel",
-            extra={"guild_id": guild_id, "channel_belongs_to": result["guild_id"]},
+            extra={
+                "guild_id": guild_id,
+                "channel_belongs_to": result["guild_id"],
+                "guild_name": guild_name,
+            },
         )
-        raise ValidationError(NO_CHANNEL_MESSAGE)
+        raise ValidationError(f"Discord channel not in {guild_name}")
