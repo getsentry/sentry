9dedd4f13b27978f0a3bcdd99a24aa4065ad49c6
Scott Cooper <scttcper@gmail.com>
2023-10-03 13:20:58 -0700

    feat(ui): Remove ids option from `useTeams` (#57314)

--- static/app/components/forms/fields/sentryMemberTeamSelectorField.tsx
+++ static/app/components/forms/fields/sentryMemberTeamSelectorField.tsx
@@ -5,6 +5,7 @@ import {t} from 'sentry/locale';
 import {Project} from 'sentry/types';
 import {useMembers} from 'sentry/utils/useMembers';
 import {useTeams} from 'sentry/utils/useTeams';
+import {useTeamsById} from 'sentry/utils/useTeamsById';
 
 import FormContext from '../formContext';
 
@@ -60,7 +61,7 @@ function SentryMemberTeamSelectorField({
       currentItems?.filter(item => item.startsWith('team:')).map(user => user.slice(5)),
     [currentItems]
   );
-  useTeams({ids: ensureTeamIds});
+  useTeamsById({ids: ensureTeamIds});
 
   const {
     teams,
