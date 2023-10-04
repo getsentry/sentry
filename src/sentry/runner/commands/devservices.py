25d51ffc8c462ae9aa66d19a191f2ae682f15cd2
Matt Gaunt-Seo @ Sentry.io <112419115+mattgauntseo-sentry@users.noreply.github.com>
2023-10-04 15:28:13 -0700

    Add a healthcheck for vroom (#57425)

--- src/sentry/runner/commands/devservices.py
+++ src/sentry/runner/commands/devservices.py
@@ -8,6 +8,8 @@ import signal
 import subprocess
 import sys
 import time
+import urllib.error
+import urllib.request
 from concurrent.futures import ThreadPoolExecutor, as_completed
 from typing import TYPE_CHECKING, Any, Callable, Generator, Literal, NamedTuple, overload
 
@@ -588,7 +590,7 @@ def run_with_retries(
     for retry in range(1, retries + 1):
         try:
             cmd()
-        except (subprocess.CalledProcessError):
+        except (subprocess.CalledProcessError, urllib.error.HTTPError):
             if retry == retries:
                 raise
             else:
@@ -651,6 +653,15 @@ def check_redis(containers: dict[str, Any]) -> None:
     )
 
 
+def check_vroom(containers: dict[str, Any]) -> None:
+    options = containers["vroom"]
+    (port,) = options["ports"].values()
+
+    # Vroom is a slim debian based image and does not have curl, wget or
+    # python3. Check health with a simple request on the host machine.
+    urllib.request.urlopen(f"http://{port[0]}:{port[1]}/health", timeout=1)
+
+
 def check_clickhouse(containers: dict[str, Any]) -> None:
     options = containers["clickhouse"]
     port = options["ports"]["8123/tcp"]
@@ -681,4 +692,5 @@ service_healthchecks: dict[str, ServiceHealthcheck] = {
     "rabbitmq": ServiceHealthcheck(check=check_rabbitmq),
     "redis": ServiceHealthcheck(check=check_redis),
     "clickhouse": ServiceHealthcheck(check=check_clickhouse),
+    "vroom": ServiceHealthcheck(check=check_vroom),
 }
