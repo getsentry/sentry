1fe99d443df44cd8b0ec31b933cbde1b8a75b1b1
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-10-04 13:42:05 -0700

    feat(bug reports): Create a infinite scrolling list of feedback items (#57432)
    This sets up an infinite loading list of feedback items along the left
    side of the page, replacing the single-page of feedback items we had
    before. Also improves some rendering issues by telling the measurement
    cache to update whenever new data has arrived.
    
    <img width="1327" alt="SCR-20231003-owss"
    src="https://github.com/getsentry/sentry/assets/187460/2fc603be-2a21-4903-9d00-be2b7762059b">
    
    The network requests, after some scrolling, look like this:
    <img width="733" alt="SCR-20231003-oxec"
    src="https://github.com/getsentry/sentry/assets/187460/ca0e34ef-b595-46d6-911b-8a8673abc3e0">
    
    Each page is a max of 10 records right now, which is not optimal, but
    that's something to change in a followup.
    
    The way this works is:
    - We've got `FeedbackDataContext` which is a simple Provider that allows
    us to hoist the data-fetching hook up to the top of the page, and share
    the data with everything below.
    - The data-fetching hook is `useFetchFeedbackInfiniteListData` which
    uses `class InfiniteListLoader` under the hood.
    - the hook part is basically an adapter that returns callbacks for
    `react-virtualized` to call, and in turn will call into `class
    InfiniteListLoader` to load more data, either at the top or bottom of
    the list.
    
    `class InfiniteListLoader` is kind of simple, but has some features for
    later:
    - It contains an EventDispatcher (event emitter?) so we can easily
    reason about when updates will be flushed, basically controlling when
    react will re-render. I found this easier than trying to carefully
    manage object identity.
    - The class will be able to fetch 'newer' items, but won't render them
    right away, doing that would cause `react-virtualized` to get confused.
    The pattern that we will build will be to: a) fetch data in the
    background, or on demand, whatever, b) show a floating pull on the list
    (like a "jump to bottom" style) that loads new data at the top of the
    list. the user it taken to the top of the list for this to work.
    - This would be useful when the user is deep-linked to a specific
    feedback item. In that case we can a) load the specific feedback item
    first b) get the timestamp of that feedback item c) set the list to
    start from that timestamp, loading items 'older' and older. This means
    that the currently selected item naturally be at the top of the list,
    visible to the user. d) in the background we can load 'newer' feedbacks,
    and when the user is ready they can click to render those 'newer' items
    above the currently select item that was deep-linked.
    
    We've also got a new helper in here called `useLocationQuery()` which is
    something i hope can help to reduce re-renders whenever location
    changes. This can be something to experiment with inside of replays as
    well.
    
    ---------
    
    Co-authored-by: Billy Vong <billyvg@users.noreply.github.com>
    Co-authored-by: Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/components/feedback/list/feedbackListItem.tsx
+++ static/app/components/feedback/list/feedbackListItem.tsx
@@ -1,4 +1,5 @@
-import {CSSProperties} from 'react';
+import {CSSProperties, forwardRef} from 'react';
+import {browserHistory} from 'react-router';
 import styled from '@emotion/styled';
 
 import FeatureBadge from 'sentry/components/featureBadge';
@@ -17,7 +18,7 @@ import {
   HydratedFeedbackItem,
 } from 'sentry/utils/feedback/item/types';
 import {decodeScalar} from 'sentry/utils/queryString';
-import {useLocation} from 'sentry/utils/useLocation';
+import useLocationQuery from 'sentry/utils/url/useLocationQuery';
 import useOrganization from 'sentry/utils/useOrganization';
 import useProjects from 'sentry/utils/useProjects';
 import {normalizeUrl} from 'sentry/utils/withDomainRequired';
@@ -43,57 +44,69 @@ function UnreadBadge() {
   return <FeatureBadge type="new" variant="indicator" />;
 }
 
-export default function FeedbackListItem({className, feedbackItem, style}: Props) {
-  const organization = useOrganization();
-  const {projects} = useProjects();
-  const location = useLocation<FeedbackItemLoaderQueryParams>();
-  const feedbackSlug = decodeScalar(location.query.feedbackSlug);
+function useIsSelectedFeedback({feedbackItem}: {feedbackItem: HydratedFeedbackItem}) {
+  const {feedbackSlug} = useLocationQuery<FeedbackItemLoaderQueryParams>({
+    fields: {feedbackSlug: decodeScalar},
+  });
   const [, feedbackId] = feedbackSlug?.split(':') ?? [];
-
-  const isSelected = feedbackId === feedbackItem.feedback_id;
-
-  const project = projects.find(p => p.id === String(feedbackItem.project_id));
-  if (!project) {
-    // TODO[feedback]: Guard against invalid test data that has no valid project.
-    return null;
-  }
-
-  return (
-    <Wrapper
-      className={className}
-      style={style}
-      data-selected={isSelected}
-      to={{
-        pathname: normalizeUrl(`/organizations/${organization.slug}/feedback/`),
-        query: {
-          referrer: 'feedback_list_page',
-          feedbackSlug: `${project.slug}:${feedbackItem.feedback_id}`,
-        },
-      }}
-      onClick={() => {
-        trackAnalytics('feedback_list.details_link.click', {organization});
-      }}
-    >
-      <InteractionStateLayer />
-      <Flex column style={{gridArea: 'right'}}>
-        <input type="checkbox" />
-        <UnreadBadge />
-      </Flex>
-      <strong style={{gridArea: 'user'}}>
-        <FeedbackItemUsername feedbackItem={feedbackItem} />
-      </strong>
-      <span style={{gridArea: 'time'}}>
-        <TimeSince date={feedbackItem.timestamp} />
-      </span>
-      <div style={{gridArea: 'message'}}>
-        <TextOverflow>{feedbackItem.message}</TextOverflow>
-      </div>
-      <Flex style={{gridArea: 'icons'}}>
-        {feedbackItem.replay_id ? <ReplayBadge /> : null}
-      </Flex>
-    </Wrapper>
-  );
+  return feedbackId === feedbackItem.feedback_id;
 }
+
+const FeedbackListItem = forwardRef<HTMLAnchorElement, Props>(
+  ({className, feedbackItem, style}: Props, ref) => {
+    const organization = useOrganization();
+    const {projects} = useProjects();
+
+    const isSelected = useIsSelectedFeedback({feedbackItem});
+
+    const project = projects.find(p => p.id === String(feedbackItem.project_id));
+    if (!project) {
+      // TODO[feedback]: Guard against invalid test data that has no valid project.
+      return null;
+    }
+
+    return (
+      <Wrapper
+        ref={ref}
+        className={className}
+        style={style}
+        data-selected={isSelected}
+        to={() => {
+          const location = browserHistory.getCurrentLocation();
+          return {
+            pathname: normalizeUrl(`/organizations/${organization.slug}/feedback/`),
+            query: {
+              ...location.query,
+              referrer: 'feedback_list_page',
+              feedbackSlug: `${project.slug}:${feedbackItem.feedback_id}`,
+            },
+          };
+        }}
+        onClick={() => {
+          trackAnalytics('feedback_list.details_link.click', {organization});
+        }}
+      >
+        <InteractionStateLayer />
+        <Flex column style={{gridArea: 'right'}}>
+          <input type="checkbox" />
+          <UnreadBadge />
+        </Flex>
+        <strong style={{gridArea: 'user'}}>
+          <FeedbackItemUsername feedbackItem={feedbackItem} />
+        </strong>
+        <span style={{gridArea: 'time'}}>
+          <TimeSince date={feedbackItem.timestamp} />
+        </span>
+        <div style={{gridArea: 'message'}}>
+          <TextOverflow>{feedbackItem.message}</TextOverflow>
+        </div>
+        <Flex style={{gridArea: 'icons'}}>
+          {feedbackItem.replay_id ? <ReplayBadge /> : null}
+        </Flex>
+      </Wrapper>
+    );
+  }
+);
 
 const Wrapper = styled(Link)`
   border-radius: ${p => p.theme.borderRadius};
@@ -118,3 +131,5 @@ const Wrapper = styled(Link)`
   gap: ${space(1)};
   place-items: stretch;
 `;
+
+export default FeedbackListItem;
