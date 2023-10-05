a619bb8f68b4f3a3ee0b6fe729ffe503c0bcf083
davidenwang <dwang@sentry.io>
2023-10-05 16:14:28 -0700

    ref(crons): Remove onboarding feature flag backend

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -71,7 +71,7 @@ default_manager.add("organizations:alert-crash-free-metrics", OrganizationFeatur
 default_manager.add("organizations:alert-filters", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:api-keys", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:auto-enable-codecov", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
-default_manager.add("organizations:crons-new-onboarding", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
+default_manager.add("organizations:crash-rate-alerts", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:customer-domains", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:dashboards-mep", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:dashboards-rh-widget", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
