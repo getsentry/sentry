3553947911f066cccefa6c6c3411ffe7582b7531
Colton Allen <colton.allen@sentry.io>
2023-10-05 15:13:59 -0500

    fix(replays): Add safer class parsing to click ingest (#57515)

--- src/sentry/replays/usecases/ingest/dom_index.py
+++ src/sentry/replays/usecases/ingest/dom_index.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import logging
 import random
 import time
@@ -275,11 +277,15 @@ def create_click_event(
 
     attributes = node.get("attributes", {})
 
+    # The class attribute can have extra white-space contained within. We need to filter them out
+    # before truncating the list.
+    classes = _parse_classes(attributes.get("class", ""))
+
     return {
         "node_id": node["id"],
         "tag": node["tagName"][:32],
         "id": attributes.get("id", "")[:64],
-        "class": attributes.get("class", "").split(" ")[:10],
+        "class": classes,
         "text": node["textContent"][:1024],
         "role": attributes.get("role", "")[:32],
         "alt": attributes.get("alt", "")[:64],
@@ -293,3 +299,7 @@ def create_click_event(
             "{}{}{}".format(replay_id, str(payload["timestamp"]), str(node["id"]))
         ),
     }
+
+
+def _parse_classes(classes: str) -> list[str]:
+    return list(filter(lambda n: n != "", classes.split(" ")))[:10]
