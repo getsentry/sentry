ca89c2795ad66a3431b0523bc525e4bb0b135737
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-10-05 14:37:22 -0400

    Improve tooltip
    - add documentation link
    - better descriptions

--- static/app/views/starfish/components/tableCells/timeSpentCell.tsx
+++ static/app/views/starfish/components/tableCells/timeSpentCell.tsx
@@ -1,11 +1,11 @@
 import clamp from 'lodash/clamp';
 
+import ExternalLink from 'sentry/components/links/externalLink';
 import {Tooltip} from 'sentry/components/tooltip';
-import {tct} from 'sentry/locale';
+import {t, tct} from 'sentry/locale';
 import {defined} from 'sentry/utils';
 import {NumberContainer} from 'sentry/utils/discover/styles';
 import {formatPercentage, getDuration} from 'sentry/utils/formatters';
-import {getSpanOperationDescription} from 'sentry/views/starfish/views/spanSummaryPage/getSpanOperationDescription';
 
 interface Props {
   containerProps?: React.DetailedHTMLProps<
@@ -21,10 +21,13 @@ export function TimeSpentCell({percentage, total, op, containerProps}: Props) {
   const formattedPercentage = formatPercentage(clamp(percentage ?? 0, 0, 1));
   const formattedTotal = getDuration((total ?? 0) / 1000, 2, true);
   const tooltip = tct(
-    'The application spent [percentage] of its total time on this [span].',
+    'The application spent [percentage] of its total time on this [span]. Read more about Time Spent in our [documentation:documentation].',
     {
       percentage: formattedPercentage,
-      span: getSpanOperationDescription(op ?? ''),
+      span: getSpanOperationDescription(op),
+      documentation: (
+        <ExternalLink href="https://docs.sentry.io/product/performance/queries/#what-is-time-spent" />
+      ),
     }
   );
 
@@ -35,4 +38,32 @@ export function TimeSpentCell({percentage, total, op, containerProps}: Props) {
       </Tooltip>
     </NumberContainer>
   );
+}
+
+// TODO: This should use `getSpanOperationDescription` but it uppercases the
+// names. We should update `getSpanOperationDescription` to not uppercase the
+// descriptions needlessly, and use it here. Also, the names here are a little
+// shorter, which is friendlier
+function getSpanOperationDescription(spanOp?: string) {
+  if (spanOp?.startsWith('http')) {
+    return t('request');
+  }
+
+  if (spanOp?.startsWith('db')) {
+    return t('query');
+  }
+
+  if (spanOp?.startsWith('task')) {
+    return t('task');
+  }
+
+  if (spanOp?.startsWith('serialize')) {
+    return t('serializer');
+  }
+
+  if (spanOp?.startsWith('middleware')) {
+    return t('middleware');
+  }
+
+  return t('span');
 }
