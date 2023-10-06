getsentry/sentry

commit 879326f0abf642637e361ae42a7c56b6792d453b

fix(apidocs): Move Issue Alert Rule APIs to Alerts & Notifications (#57600)
This PR renames the Notifications section to Alerts & Notifications, and
moves the Issue Alert Rule APIs to this section.

<img width="1512" alt="image"
src="https://github.com/getsentry/sentry/assets/45607721/2716783b-af50-4f6a-86e7-d95db55ef08c">

src/sentry/api/endpoints/project_rule_details.py
@@ -94,7 +94,7 @@ class ProjectRuleDetailsPutSerializer(serializers.Serializer):
     )
 
 
-@extend_schema(tags=["Events"])
+@extend_schema(tags=["Alerts"])
 @region_silo_endpoint
 class ProjectRuleDetailsEndpoint(RuleEndpoint):
     publish_status = {

src/sentry/api/endpoints/notifications/notification_actions_details.py
@@ -29,7 +29,7 @@ logger = logging.getLogger(__name__)
 
 
 @region_silo_endpoint
-@extend_schema(tags=["Notifications"])
+@extend_schema(tags=["Alerts"])
 class NotificationActionsDetailsEndpoint(OrganizationEndpoint):
     publish_status = {
         "DELETE": ApiPublishStatus.PUBLIC,

src/sentry/api/endpoints/notifications/notification_actions_index.py
@@ -48,7 +48,7 @@ class NotificationActionsPermission(OrganizationPermission):
 
 
 @region_silo_endpoint
-@extend_schema(tags=["Notifications"])
+@extend_schema(tags=["Alerts"])
 class NotificationActionsIndexEndpoint(OrganizationEndpoint):
     publish_status = {
         "GET": ApiPublishStatus.PUBLIC,

src/sentry/api/endpoints/project_rules.py
@@ -441,7 +441,7 @@ A list of filters that determine if a rule fires after the necessary conditions 
     )
 
 
-@extend_schema(tags=["Events"])
+@extend_schema(tags=["Alerts"])
 @region_silo_endpoint
 class ProjectRulesEndpoint(ProjectEndpoint):
     publish_status = {

src/sentry/apidocs/build.py
@@ -123,9 +123,9 @@ OPENAPI_TAGS = [
         },
     },
     {
-        "name": "Notifications",
-        "x-sidebar-name": "Notifications",
-        "description": "Endpoints for Notifications",
+        "name": "Alerts",
+        "x-sidebar-name": "Alerts & Notifications",
+        "description": "Endpoints for Alerts and Notifications",
         "x-display-description": False,
         "externalDocs": {
             "description": "Found an error? Let us know.",

commit 26d4ec0574507d5cfc880181f77edb3663f7d61e

fix(apidocs): Correct broken links (#57709)
This PR fixes broken links in the API docs that were introduced in
#57600

src/sentry/api/endpoints/project_rule_details.py
@@ -61,11 +61,11 @@ class ProjectRuleDetailsPutSerializer(serializers.Serializer):
     )
     conditions = serializers.ListField(
         child=RuleNodeField(type="condition/event"),
-        help_text="A list of triggers that determine when the rule fires. See [Create an Issue Alert Rule](/api/events/create-an-issue-alert-rule-for-a-project) for valid conditions.",
+        help_text="A list of triggers that determine when the rule fires. See [Create an Issue Alert Rule](/api/alerts/create-an-issue-alert-rule-for-a-project) for valid conditions.",
     )
     actions = serializers.ListField(
         child=RuleNodeField(type="action/event"),
-        help_text="A list of actions that take place when all required conditions and filters for the rule are met. See [Create an Issue Alert Rule](/api/events/create-an-issue-alert-rule-for-a-project) for valid actions.",
+        help_text="A list of actions that take place when all required conditions and filters for the rule are met. See [Create an Issue Alert Rule](/api/alerts/create-an-issue-alert-rule-for-a-project) for valid actions.",
     )
     frequency = serializers.IntegerField(
         min_value=5,
@@ -87,7 +87,7 @@ class ProjectRuleDetailsPutSerializer(serializers.Serializer):
     filters = serializers.ListField(
         child=RuleNodeField(type="filter/event"),
         required=False,
-        help_text="A list of filters that determine if a rule fires after the necessary conditions have been met. See [Create an Issue Alert Rule](/api/events/create-an-issue-alert-rule-for-a-project) for valid filters.",
+        help_text="A list of filters that determine if a rule fires after the necessary conditions have been met. See [Create an Issue Alert Rule](/api/alerts/create-an-issue-alert-rule-for-a-project) for valid filters.",
     )
     owner = ActorField(
         required=False, allow_null=True, help_text="The ID of the team or user that owns the rule."