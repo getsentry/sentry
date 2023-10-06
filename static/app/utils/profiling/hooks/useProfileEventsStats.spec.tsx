90b2b98277e32c3f93167351bb57542ee0f66681
Tony Xiao <txiao@sentry.io>
2023-10-06 11:22:59 -0400

    fix some tests

--- static/app/utils/profiling/hooks/useProfileEventsStats.spec.tsx
+++ static/app/utils/profiling/hooks/useProfileEventsStats.spec.tsx
@@ -43,21 +43,15 @@ describe('useProfileEvents', function () {
     });
 
     await waitFor(() => result.current.isSuccess);
-    expect(result.current.data).toEqual([
-      {
-        data: [],
-        meta: {
-          dataset: 'profiles',
-          end: 0,
-          start: 0,
-        },
-        timestamps: [],
+    expect(result.current.data).toEqual({
+      data: [],
+      meta: {
+        dataset: 'profiles',
+        end: 0,
+        start: 0,
       },
-      expect.anything(),
-      expect.objectContaining({
-        getResponseHeader: expect.anything(),
-      }),
-    ]);
+      timestamps: [],
+    });
   });
 
   it('handles 1 axis', async function () {
@@ -90,21 +84,15 @@ describe('useProfileEvents', function () {
     });
 
     await waitFor(() => result.current.isSuccess);
-    expect(result.current.data).toEqual([
-      {
-        data: [{axis: 'count()', values: [1, 2]}],
-        meta: {
-          dataset: 'profiles',
-          start: 0,
-          end: 10,
-        },
-        timestamps: [0, 5],
+    expect(result.current.data).toEqual({
+      data: [{axis: 'count()', values: [1, 2]}],
+      meta: {
+        dataset: 'profiles',
+        start: 0,
+        end: 10,
       },
-      expect.anything(),
-      expect.objectContaining({
-        getResponseHeader: expect.anything(),
-      }),
-    ]);
+      timestamps: [0, 5],
+    });
   });
 
   it('handles n axes', async function () {
@@ -151,23 +139,17 @@ describe('useProfileEvents', function () {
     });
 
     await waitFor(() => result.current.isSuccess);
-    expect(result.current.data).toEqual([
-      {
-        data: [
-          {axis: 'count()', values: [1, 2]},
-          {axis: 'p99()', values: [3, 4]},
-        ],
-        meta: {
-          dataset: 'profiles',
-          start: 0,
-          end: 10,
-        },
-        timestamps: [0, 5],
+    expect(result.current.data).toEqual({
+      data: [
+        {axis: 'count()', values: [1, 2]},
+        {axis: 'p99()', values: [3, 4]},
+      ],
+      meta: {
+        dataset: 'profiles',
+        start: 0,
+        end: 10,
       },
-      expect.anything(),
-      expect.objectContaining({
-        getResponseHeader: expect.anything(),
-      }),
-    ]);
+      timestamps: [0, 5],
+    });
   });
 });
