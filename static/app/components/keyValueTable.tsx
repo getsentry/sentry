9a74d6ca0085413e477aed4a532c5780cf343eb7
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-10-06 09:06:31 -0700

    feat(replay): Highlight when Network request/resposes have different content-types (#57652)
    Using yellow to highlight the network row and header information:
    <img width="534" alt="SCR-20231005-qtyz"
    src="https://github.com/getsentry/sentry/assets/187460/68729dc3-f6ed-4d38-913c-527f8bd463c1">
    <img width="533" alt="SCR-20231005-qucf"
    src="https://github.com/getsentry/sentry/assets/187460/87572273-75d2-40dc-b2e8-3498d745927a">
    
    
    
    
    Fixes https://github.com/getsentry/sentry/issues/57627

--- static/app/components/keyValueTable.tsx
+++ static/app/components/keyValueTable.tsx
@@ -7,6 +7,7 @@ import {space} from 'sentry/styles/space';
 type Props = {
   keyName: React.ReactNode;
   value: React.ReactNode;
+  type?: undefined | 'error' | 'warning';
 };
 
 export const KeyValueTable = styled('dl')<{noMargin?: boolean}>`
@@ -15,32 +16,40 @@ export const KeyValueTable = styled('dl')<{noMargin?: boolean}>`
   ${p => (p.noMargin ? 'margin-bottom: 0;' : null)}
 `;
 
-export function KeyValueTableRow({keyName, value}: Props) {
+export function KeyValueTableRow({keyName, value, type}: Props) {
   return (
     <Fragment>
-      <Key>{keyName}</Key>
-      <Value>{value}</Value>
+      <Key type={type}>{keyName}</Key>
+      <Value type={type}>{value}</Value>
     </Fragment>
   );
 }
 
-const commonStyles = ({theme}: {theme: Theme}) => `
-font-size: ${theme.fontSizeMedium};
-padding: ${space(0.5)} ${space(1)};
-font-weight: normal;
-line-height: inherit;
-${p => p.theme.overflowEllipsis};
-&:nth-of-type(2n-1) {
-  background-color: ${theme.backgroundSecondary};
-}
+const commonStyles = ({theme, type}: {type: Props['type']} & {theme: Theme}) => `
+  font-size: ${theme.fontSizeMedium};
+  padding: ${space(0.5)} ${space(1)};
+  font-weight: normal;
+  line-height: inherit;
+  ${p => p.theme.overflowEllipsis};
+
+  background-color: ${
+    type === 'error'
+      ? theme.red100 + ' !important'
+      : type === 'warning'
+      ? 'var(--background-warning-default, rgba(245, 176, 0, 0.09)) !important'
+      : 'inherit'
+  };
+  &:nth-of-type(2n-1) {
+    background-color: ${theme.backgroundSecondary};
+  }
 `;
 
-const Key = styled('dt')`
+const Key = styled('dt')<{type: Props['type']}>`
   ${commonStyles};
   color: ${p => p.theme.textColor};
 `;
 
-const Value = styled('dd')`
+const Value = styled('dd')<{type: Props['type']}>`
   ${commonStyles};
   color: ${p => p.theme.subText};
   text-align: right;
