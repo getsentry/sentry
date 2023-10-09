51b334b89b7d6bf665e644c56279d70c5adab438
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-10-09 14:01:30 +0200

    feat(ddm): Add options for disabling allow list of specific metric types (#57762)

--- src/sentry/metrics/minimetrics.py
+++ src/sentry/metrics/minimetrics.py
@@ -113,7 +113,7 @@ class MiniMetricsMetricsBackend(MetricsBackend):
         amount: Union[float, int] = 1,
         sample_rate: float = 1,
     ) -> None:
-        if self._keep_metric(sample_rate):
+        if self._keep_metric(sample_rate) or options.get("delightful_metrics.allow_all_incr"):
             sentry_sdk.metrics.incr(
                 key=self._get_key(key),
                 value=amount,
@@ -128,7 +128,7 @@ class MiniMetricsMetricsBackend(MetricsBackend):
         tags: Optional[Tags] = None,
         sample_rate: float = 1,
     ) -> None:
-        if self._keep_metric(sample_rate):
+        if self._keep_metric(sample_rate) or options.get("delightful_metrics.allow_all_timing"):
             sentry_sdk.metrics.distribution(
                 key=self._get_key(key), value=value, tags=tags, unit="second"
             )
@@ -141,6 +141,6 @@ class MiniMetricsMetricsBackend(MetricsBackend):
         tags: Optional[Tags] = None,
         sample_rate: float = 1,
     ) -> None:
-        if self._keep_metric(sample_rate):
+        if self._keep_metric(sample_rate) or options.get("delightful_metrics.allow_all_gauge"):
             # XXX: make this into a gauge later
             sentry_sdk.metrics.incr(key=self._get_key(key), value=value, tags=tags)
