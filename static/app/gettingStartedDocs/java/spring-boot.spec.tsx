852c2d1fab4bc5af520f64583e43841e234f61da
Arthur Knaus <arthur.knaus@sentry.io>
2023-10-09 14:18:24 +0200

    ref(onboarding-docs): Convert java-spring-boot to new structure

--- static/app/gettingStartedDocs/java/spring-boot.spec.tsx
+++ static/app/gettingStartedDocs/java/spring-boot.spec.tsx
@@ -1,28 +1,107 @@
-import {render, screen} from 'sentry-test/reactTestingLibrary';
+import {screen} from 'sentry-test/reactTestingLibrary';
+import {textWithMarkupMatcher} from 'sentry-test/utils';
 
-import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
+import {renderWithOnboardingLayout} from 'sentry/utils/gettingStartedDocs/tests';
 
-import {
-  GettingStartedWithSpringBoot,
-  PackageManager,
-  SpringBootVersion,
-  steps,
-} from './spring-boot';
+import docs, {PackageManager, SpringBootVersion} from './spring-boot';
 
-describe('GettingStartedWithSpringBoot', function () {
-  it('renders doc correctly', function () {
-    render(<GettingStartedWithSpringBoot dsn="test-dsn" projectSlug="test-project" />);
+describe('java-spring-boot onboarding docs', function () {
+  it('renders gradle docs correctly', async function () {
+    renderWithOnboardingLayout(docs, {
+      releaseRegistry: {
+        'sentry.java.android.gradle-plugin': {
+          version: '1.99.9',
+        },
+      },
+    });
 
-    // Steps
-    for (const step of steps({
-      dsn: 'test-dsn',
-      springBootVersion: SpringBootVersion.V2,
-      packageManager: PackageManager.MAVEN,
-      hasPerformance: true,
-    })) {
-      expect(
-        screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
-      ).toBeInTheDocument();
-    }
+    // Renders main headings
+    expect(screen.getByRole('heading', {name: 'Install'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Configure SDK'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Verify'})).toBeInTheDocument();
+
+    // Renders SDK version from registry
+    expect(
+      await screen.findByText(
+        textWithMarkupMatcher(/id "io\.sentry\.jvm\.gradle" version "1\.99\.9"/)
+      )
+    ).toBeInTheDocument();
+  });
+
+  it('renders maven docs correctly', async function () {
+    renderWithOnboardingLayout(docs, {
+      releaseRegistry: {
+        'sentry.java.spring-boot.jakarta': {
+          version: '2.99.9',
+        },
+        'sentry.java.mavenplugin': {
+          version: '3.99.9',
+        },
+      },
+      selectedOptions: {
+        packageManager: PackageManager.MAVEN,
+      },
+    });
+
+    // Renders main headings
+    expect(screen.getByRole('heading', {name: 'Install'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Configure SDK'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Verify'})).toBeInTheDocument();
+
+    // Renders SDK version from registry
+    expect(
+      await screen.findByText(
+        textWithMarkupMatcher(
+          /<artifactId>sentry-spring-boot-starter-jakarta<\/artifactId>\s*<version>2\.99\.9<\/version>/m
+        )
+      )
+    ).toBeInTheDocument();
+
+    expect(
+      await screen.findByText(
+        textWithMarkupMatcher(
+          /<artifactId>sentry-maven-plugin<\/artifactId>\s*<version>3\.99\.9<\/version>/m
+        )
+      )
+    ).toBeInTheDocument();
+  });
+
+  it('renders maven with spring boot 2 docs correctly', async function () {
+    renderWithOnboardingLayout(docs, {
+      releaseRegistry: {
+        'sentry.java.spring-boot': {
+          version: '2.99.9',
+        },
+        'sentry.java.mavenplugin': {
+          version: '3.99.9',
+        },
+      },
+      selectedOptions: {
+        packageManager: PackageManager.MAVEN,
+        springBootVersion: SpringBootVersion.V2,
+      },
+    });
+
+    // Renders main headings
+    expect(screen.getByRole('heading', {name: 'Install'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Configure SDK'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Verify'})).toBeInTheDocument();
+
+    // Renders SDK version from registry
+    expect(
+      await screen.findByText(
+        textWithMarkupMatcher(
+          /<artifactId>sentry-spring-boot-starter<\/artifactId>\s*<version>2\.99\.9<\/version>/m
+        )
+      )
+    ).toBeInTheDocument();
+
+    expect(
+      await screen.findByText(
+        textWithMarkupMatcher(
+          /<artifactId>sentry-maven-plugin<\/artifactId>\s*<version>3\.99\.9<\/version>/m
+        )
+      )
+    ).toBeInTheDocument();
   });
 });
