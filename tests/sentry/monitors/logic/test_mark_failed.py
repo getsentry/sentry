08138919c89dccdbf0fcbd18e5ee4113061e4791
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-10-10 14:22:42 -0700

    one more check

--- tests/sentry/monitors/logic/test_mark_failed.py
+++ tests/sentry/monitors/logic/test_mark_failed.py
@@ -575,6 +575,9 @@ class MarkFailedTestCase(TestCase):
 
         assert len(mock_produce_occurrence_to_kafka.mock_calls) == 0
 
+        monitor_incidents = MonitorIncident.objects.filter(monitor_environment=monitor_environment)
+        assert len(monitor_incidents) == 0
+
     @patch("sentry.issues.producer.produce_occurrence_to_kafka")
     def test_mark_failed_issue_threshold(self, mock_produce_occurrence_to_kafka):
         failure_issue_threshold = 8
@@ -729,5 +732,6 @@ class MarkFailedTestCase(TestCase):
         assert monitor_environment.status == MonitorStatus.ERROR
 
         assert len(mock_produce_occurrence_to_kafka.mock_calls) == 0
+
         monitor_incidents = MonitorIncident.objects.filter(monitor_environment=monitor_environment)
         assert len(monitor_incidents) == 0
