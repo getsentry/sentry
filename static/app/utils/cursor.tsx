1bd3aa6ab60b6bdea6fbbebcbb5e9310dfef6990
Radu Woinaroski <5281987+RaduW@users.noreply.github.com>
2023-10-10 17:46:22 +0200

    add support for pagination when displaying investigation rule UI

---
+++ static/app/utils/cursor.tsx
@@ -0,0 +1,26 @@
+export type CursorInfo = {
+  isPrev: boolean;
+  offset: number;
+  value: number;
+};
+
+/// Converts a cursor string into a Cursor object.
+export function parseCursor(cursor: string | undefined | null): CursorInfo | undefined {
+  if (!cursor) {
+    return undefined;
+  }
+  const bits = cursor.split(':');
+
+  if (bits.length !== 3) {
+    return undefined;
+  }
+
+  try {
+    const value = parseInt(bits[0], 10);
+    const offset = parseInt(bits[1], 10);
+    const isPrev = bits[2] === '1';
+    return {isPrev, offset, value};
+  } catch (e) {
+    return undefined;
+  }
+}
