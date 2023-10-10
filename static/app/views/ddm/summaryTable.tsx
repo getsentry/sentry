97a21ed7da1e73547a43ec53ff6b8eb7394542ad
Ogi <86684834+obostjancic@users.noreply.github.com>
2023-10-10 10:46:16 +0200

    feat(ddm): summary table sort (#57783)

--- static/app/views/ddm/summaryTable.tsx
+++ static/app/views/ddm/summaryTable.tsx
@@ -1,11 +1,11 @@
-import {Fragment} from 'react';
+import {Fragment, useCallback, useState} from 'react';
 import styled from '@emotion/styled';
 import colorFn from 'color';
 
 import {LinkButton} from 'sentry/components/button';
 import ButtonBar from 'sentry/components/buttonBar';
 import {Tooltip} from 'sentry/components/tooltip';
-import {IconLightning, IconReleases} from 'sentry/icons';
+import {IconArrow, IconLightning, IconReleases} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {getUtcDateString} from 'sentry/utils/dates';
@@ -15,6 +15,16 @@ import usePageFilters from 'sentry/utils/usePageFilters';
 import useRouter from 'sentry/utils/useRouter';
 import {Series} from 'sentry/views/ddm/metricWidget';
 import {transactionSummaryRouteWithQuery} from 'sentry/views/performance/transactionSummary/utils';
+
+type SortState = {
+  name: 'name' | 'avg' | 'min' | 'max' | 'sum';
+  order: 'asc' | 'desc';
+};
+
+const DEFAULT_SORT_STATE: SortState = {
+  name: 'name',
+  order: 'asc',
+};
 
 export function SummaryTable({
   series,
@@ -28,10 +38,39 @@ export function SummaryTable({
   operation?: string;
 }) {
   const {selection} = usePageFilters();
-  const router = useRouter();
   const {slug} = useOrganization();
+  const [sortState, setSortState] = useState<SortState>(DEFAULT_SORT_STATE);
+
   const hasActions = series.some(s => s.release || s.transaction);
+
+  const router = useRouter();
   const {start, end, statsPeriod, project, environment} = router.location.query;
+
+  const sort = useCallback(
+    (name: SortState['name']) => {
+      if (sortState.name === name) {
+        if (sortState.order === 'desc') {
+          setSortState(DEFAULT_SORT_STATE);
+        } else if (sortState.order === 'asc') {
+          setSortState({
+            name,
+            order: 'desc',
+          });
+        } else {
+          setSortState({
+            name,
+            order: 'asc',
+          });
+        }
+      } else {
+        setSortState({
+          name,
+          order: 'asc',
+        });
+      }
+    },
+    [sortState]
+  );
 
   const releaseTo = (release: string) => {
     return {
@@ -64,65 +103,145 @@ export function SummaryTable({
       },
     });
 
+  const rows = series
+    .map(s => {
+      return {
+        ...s,
+        ...getValues(s.data),
+        name: getNameFromMRI(s.seriesName),
+      };
+    })
+    .sort((a, b) => {
+      const {name, order} = sortState;
+
+      if (name === 'name') {
+        return order === 'asc'
+          ? a.name.localeCompare(b.name)
+          : b.name.localeCompare(a.name);
+      }
+      const aValue = a[name] ?? 0;
+      const bValue = b[name] ?? 0;
+
+      return order === 'asc' ? aValue - bValue : bValue - aValue;
+    });
+
   return (
     <SummaryTableWrapper hasActions={hasActions}>
-      <HeaderCell />
-      <HeaderCell>{t('Name')}</HeaderCell>
-      <HeaderCell right>{t('Avg')}</HeaderCell>
-      <HeaderCell right>{t('Min')}</HeaderCell>
-      <HeaderCell right>{t('Max')}</HeaderCell>
-      <HeaderCell right>{t('Sum')}</HeaderCell>
-      {hasActions && <HeaderCell right>{t('Actions')}</HeaderCell>}
+      <HeaderCell disabled />
+      <SortableHeaderCell onClick={sort} sortState={sortState} name="name">
+        {t('Name')}
+      </SortableHeaderCell>
+      <SortableHeaderCell onClick={sort} sortState={sortState} name="avg" right>
+        {t('Avg')}
+      </SortableHeaderCell>
+      <SortableHeaderCell onClick={sort} sortState={sortState} name="min" right>
+        {t('Min')}
+      </SortableHeaderCell>
+      <SortableHeaderCell onClick={sort} sortState={sortState} name="max" right>
+        {t('Max')}
+      </SortableHeaderCell>
+      <SortableHeaderCell onClick={sort} sortState={sortState} name="sum" right>
+        {t('Sum')}
+      </SortableHeaderCell>
+      {hasActions && (
+        <HeaderCell disabled right>
+          {t('Actions')}
+        </HeaderCell>
+      )}
 
-      {series.map(({seriesName, color, hidden, unit, data, transaction, release}) => {
-        const {avg, min, max, sum} = getValues(data);
+      {rows.map(
+        ({
+          name,
+          seriesName,
+          color,
+          hidden,
+          unit,
+          transaction,
+          release,
+          avg,
+          min,
+          max,
+          sum,
+        }) => {
+          return (
+            <Fragment key={seriesName}>
+              <CellWrapper
+                onClick={() => onClick(seriesName)}
+                onMouseEnter={() => setHoveredLegend?.(seriesName)}
+                onMouseLeave={() => setHoveredLegend?.('')}
+              >
+                <Cell>
+                  <ColorDot color={color} isHidden={!!hidden} />
+                </Cell>
+                <TextOverflowCell>{name}</TextOverflowCell>
+                {/* TODO(ddm): Add a tooltip with the full value, don't add on click in case users want to copy the value */}
+                <Cell right>{formatMetricsUsingUnitAndOp(avg, unit, operation)}</Cell>
+                <Cell right>{formatMetricsUsingUnitAndOp(min, unit, operation)}</Cell>
+                <Cell right>{formatMetricsUsingUnitAndOp(max, unit, operation)}</Cell>
+                <Cell right>{formatMetricsUsingUnitAndOp(sum, unit, operation)}</Cell>
+              </CellWrapper>
+              {hasActions && (
+                <Cell right>
+                  <ButtonBar gap={0.5}>
+                    {transaction && (
+                      <div>
+                        <Tooltip title={t('Open Transaction Summary')}>
+                          <LinkButton to={transactionTo(transaction)} size="xs">
+                            <IconLightning size="xs" />
+                          </LinkButton>
+                        </Tooltip>
+                      </div>
+                    )}
 
-        return (
-          <Fragment key={seriesName}>
-            <CellWrapper
-              onClick={() => onClick(seriesName)}
-              onMouseEnter={() => setHoveredLegend?.(seriesName)}
-              onMouseLeave={() => setHoveredLegend?.('')}
-            >
-              <Cell>
-                <ColorDot color={color} isHidden={!!hidden} />
-              </Cell>
-              <TextOverflowCell>{getNameFromMRI(seriesName)}</TextOverflowCell>
-              {/* TODO(ddm): Add a tooltip with the full value, don't add on click in case users want to copy the value */}
-              <Cell right>{formatMetricsUsingUnitAndOp(avg, unit, operation)}</Cell>
-              <Cell right>{formatMetricsUsingUnitAndOp(min, unit, operation)}</Cell>
-              <Cell right>{formatMetricsUsingUnitAndOp(max, unit, operation)}</Cell>
-              <Cell right>{formatMetricsUsingUnitAndOp(sum, unit, operation)}</Cell>
-            </CellWrapper>
-            {hasActions && (
-              <Cell right>
-                <ButtonBar gap={0.5}>
-                  {transaction && (
-                    <div>
-                      <Tooltip title={t('Open Transaction Summary')}>
-                        <LinkButton to={transactionTo(transaction)} size="xs">
-                          <IconLightning size="xs" />
-                        </LinkButton>
-                      </Tooltip>
-                    </div>
-                  )}
-
-                  {release && (
-                    <div>
-                      <Tooltip title={t('Open Release Details')}>
-                        <LinkButton to={releaseTo(release)} size="xs">
-                          <IconReleases size="xs" />
-                        </LinkButton>
-                      </Tooltip>
-                    </div>
-                  )}
-                </ButtonBar>
-              </Cell>
-            )}
-          </Fragment>
-        );
-      })}
+                    {release && (
+                      <div>
+                        <Tooltip title={t('Open Release Details')}>
+                          <LinkButton to={releaseTo(release)} size="xs">
+                            <IconReleases size="xs" />
+                          </LinkButton>
+                        </Tooltip>
+                      </div>
+                    )}
+                  </ButtonBar>
+                </Cell>
+              )}
+            </Fragment>
+          );
+        }
+      )}
     </SummaryTableWrapper>
+  );
+}
+
+function SortableHeaderCell({
+  sortState,
+  name,
+  right,
+  children,
+  onClick,
+}: {
+  children: React.ReactNode;
+  name: SortState['name'];
+  onClick: (name: SortState['name']) => void;
+  sortState: SortState;
+  right?: boolean;
+}) {
+  const sortIcon =
+    sortState.name === name ? (
+      <IconArrow size="xs" direction={sortState.order === 'asc' ? 'up' : 'down'} />
+    ) : (
+      ''
+    );
+
+  return (
+    <HeaderCell
+      onClick={() => {
+        onClick(name);
+      }}
+      right={right}
+    >
+      {sortIcon} {children}
+    </HeaderCell>
   );
 }
 
@@ -161,7 +280,7 @@ const SummaryTableWrapper = styled(`div`)<{hasActions: boolean}>`
 `;
 
 // TODO(ddm): This is a copy of PanelTableHeader, try to figure out how to reuse it
-const HeaderCell = styled('div')<{right?: boolean}>`
+const HeaderCell = styled('div')<{disabled?: boolean; right?: boolean}>`
   color: ${p => p.theme.subText};
   font-size: ${p => p.theme.fontSizeSmall};
   font-weight: 600;
@@ -169,10 +288,16 @@ const HeaderCell = styled('div')<{right?: boolean}>`
   border-radius: ${p => p.theme.borderRadius} ${p => p.theme.borderRadius} 0 0;
   line-height: 1;
   display: flex;
-  flex-direction: column;
-  justify-content: center;
-  text-align: ${p => (p.right ? 'right' : 'left')};
+  flex-direction: row;
+  justify-content: ${p => (p.right ? 'flex-end' : 'flex-start')};
   padding: ${space(0.5)} ${space(1)};
+  gap: ${space(0.5)};
+  user-select: none;
+
+  :hover {
+    cursor: ${p => (p.disabled ? 'auto' : 'pointer')};
+    background-color: ${p => (p.disabled ? p.theme.background : p.theme.bodyBackground)};
+  }
 `;
 
 const Cell = styled('div')<{right?: boolean}>`
