919d14f11a0b2c8695f1890f1ca5c788f1b74380
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-10-10 16:31:09 -0700

    test: Improve useLocationQuery with tests and types (#57823)
    <!-- Describe your PR here. -->

--- static/app/utils/url/useLocationQuery.tsx
+++ static/app/utils/url/useLocationQuery.tsx
@@ -10,30 +10,52 @@ type Decoder = typeof decodeList | typeof decodeScalar | typeof decodeInteger;
  * Select and memoize query params from location.
  * This returns a new object only when one of the specified query fields is
  * updated. The object will remain stable otherwise, avoiding re-renders.
+ *
+ * You shouldn't need to manually set the `InferredRequestShape` or `InferredResponseShape`
+ * generics, instead type the left side of the statement.
+ *
+ * For example:
+ * ```
+ * type QueryFields = {statsPeriod: string};
+ * const query: QueryFields = useLocationQuery({
+ *   fields: {statsPeriod: decodeScalar}
+ * });
+ * ```
  */
 export default function useLocationQuery<
-  Fields extends Record<string, Scalar | Scalar[]>,
->({fields}: {fields: Record<keyof Fields, Fields[string] | Decoder>}): Fields {
+  InferredRequestShape extends Record<string, Scalar | Scalar[] | Decoder>,
+  InferredResponseShape extends {
+    readonly [Property in keyof InferredRequestShape]: InferredRequestShape[Property] extends Decoder
+      ? ReturnType<InferredRequestShape[Property]>
+      : InferredRequestShape[Property];
+  },
+>({fields}: {fields: InferredRequestShape}): InferredResponseShape {
   const location = useLocation();
 
   const locationFields = {};
-  const staticFields = {};
+  const forwardedFields = {};
   Object.entries(fields).forEach(([field, decoderOrValue]) => {
     if (typeof decoderOrValue === 'function') {
-      locationFields[field] = decoderOrValue(location.query[field]);
+      if (decoderOrValue === decodeScalar) {
+        locationFields[field] = decoderOrValue(location.query[field], '');
+      } else if (decoderOrValue === decodeInteger) {
+        locationFields[field] = decoderOrValue(location.query[field], 0);
+      } else {
+        locationFields[field] = decoderOrValue(location.query[field]);
+      }
     } else {
-      staticFields[field] = decoderOrValue;
+      forwardedFields[field] = decoderOrValue;
     }
   }, {});
 
-  const stringyFields = JSON.stringify(locationFields);
-  const objFields = useMemo(() => JSON.parse(stringyFields), [stringyFields]);
+  const stringyForwardedFields = JSON.stringify(forwardedFields);
+  const stringyLocationFields = JSON.stringify(locationFields);
 
   return useMemo(
     () => ({
-      ...objFields,
-      ...staticFields,
+      ...(forwardedFields as any),
+      ...(locationFields as any),
     }),
-    [objFields] // eslint-disable-line react-hooks/exhaustive-deps
+    [stringyForwardedFields, stringyLocationFields] // eslint-disable-line
   );
 }
