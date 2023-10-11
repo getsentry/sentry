f9b32acf2e3f1c382eac405d6cd9768adbe3980e
Colleen O'Rourke <colleen@sentry.io>
2023-10-10 17:41:17 -0700

    update comment

--- src/sentry/api/endpoints/group_notes_details.py
+++ src/sentry/api/endpoints/group_notes_details.py
@@ -90,8 +90,7 @@ class GroupNotesDetailsEndpoint(GroupEndpoint):
 
         if serializer.is_valid():
             payload = serializer.validated_data
-            # TODO adding mentions to a note doesn't do subscriptions
-            # or notifications. Should it?
+            # TODO adding mentions to a note doesn't send notifications. Should it?
             # Remove mentions as they shouldn't go into the database
             payload.pop("mentions", [])
 
