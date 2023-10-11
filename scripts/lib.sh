056c08312e66d1b0f424c97688f9f8f27cc7ae1c
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-10-11 15:40:24 -0400

    ref: use node version from .volta.json (#57930)
    <!-- Describe your PR here. -->

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -134,7 +134,7 @@ setup-git() {
 
 node-version-check() {
     # Checks to see if node's version matches the one specified in package.json for Volta.
-    node -pe "process.exit(Number(!(process.version == 'v' + require('./package.json').volta.node )))" ||
+    node -pe "process.exit(Number(!(process.version == 'v' + require('./.volta.json').volta.node )))" ||
         (
             echo 'Unexpected node version. Recommended to use https://github.com/volta-cli/volta'
             echo 'Run `volta install node` and `volta install yarn` to update your toolchain.'
