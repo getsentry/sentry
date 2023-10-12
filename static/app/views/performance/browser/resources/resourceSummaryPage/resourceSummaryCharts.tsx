1f2531e02473db59e0da748d5691f7fb1455476d
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-10-12 15:01:40 -0400

    revert

--- static/app/views/performance/browser/resources/resourceSummaryPage/resourceSummaryCharts.tsx
+++ static/app/views/performance/browser/resources/resourceSummaryPage/resourceSummaryCharts.tsx
@@ -4,44 +4,26 @@ import ChartPanel from 'sentry/views/starfish/components/chartPanel';
 import {useSpanMetricsSeries} from 'sentry/views/starfish/queries/useSpanMetricsSeries';
 import {SpanMetricsField} from 'sentry/views/starfish/types';
 import {getDurationChartTitle} from 'sentry/views/starfish/views/spans/types';
-import {Block, BlockContainer} from 'sentry/views/starfish/views/spanSummaryPage/block';
+import {Block} from 'sentry/views/starfish/views/spanSummaryPage/block';
 
 function ResourceSummaryCharts(props: {groupId: string}) {
   const {data: spanMetricsSeriesData, isLoading: areSpanMetricsSeriesLoading} =
-    useSpanMetricsSeries(props.groupId, {}, [
-      `avg(${SpanMetricsField.SPAN_SELF_TIME})`,
-      `avg(http.response_content_length)`,
-    ]);
+    useSpanMetricsSeries(props.groupId, {}, [`avg(${SpanMetricsField.SPAN_SELF_TIME})`]);
 
   return (
-    <BlockContainer>
-      <Block>
-        <ChartPanel title={getDurationChartTitle('http')}>
-          <Chart
-            height={160}
-            data={[spanMetricsSeriesData?.[`avg(${SpanMetricsField.SPAN_SELF_TIME})`]]}
-            loading={areSpanMetricsSeriesLoading}
-            utc={false}
-            chartColors={[AVG_COLOR]}
-            isLineChart
-            definedAxisTicks={4}
-          />
-        </ChartPanel>
-      </Block>
-      <Block>
-        <ChartPanel title="Resource Size">
-          <Chart
-            height={160}
-            data={[spanMetricsSeriesData?.[`avg(${SpanMetricsField.SPAN_SELF_TIME})`]]}
-            loading={areSpanMetricsSeriesLoading}
-            utc={false}
-            chartColors={[AVG_COLOR]}
-            isLineChart
-            definedAxisTicks={4}
-          />
-        </ChartPanel>
-      </Block>
-    </BlockContainer>
+    <Block>
+      <ChartPanel title={getDurationChartTitle('http')}>
+        <Chart
+          height={160}
+          data={[spanMetricsSeriesData?.[`avg(${SpanMetricsField.SPAN_SELF_TIME})`]]}
+          loading={areSpanMetricsSeriesLoading}
+          utc={false}
+          chartColors={[AVG_COLOR]}
+          isLineChart
+          definedAxisTicks={4}
+        />
+      </ChartPanel>
+    </Block>
   );
 }
 
