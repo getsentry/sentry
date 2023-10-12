736f3f2ecb9a1012af898e9fa244ca95e56ac767
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-10-12 16:18:39 -0700

    more ref

--- static/app/components/feedback/feedbackItem/useDeleteFeedback.tsx
+++ static/app/components/feedback/feedbackItem/useDeleteFeedback.tsx
@@ -20,6 +20,30 @@ interface Props {
   feedbackItem: HydratedFeedbackItem;
 }
 
+function openDeleteModal({onDelete}: {onDelete: () => void | Promise<void>}) {
+  openModal(({Body, Footer, closeModal}: ModalRenderProps) => (
+    <Fragment>
+      <Body>
+        {t('Deleting this feedback is permanent. Are you sure you wish to continue?')}
+      </Body>
+      <Footer>
+        <Flex gap={space(1)}>
+          <Button onClick={closeModal}>{t('Cancel')}</Button>
+          <Button
+            priority="danger"
+            onClick={() => {
+              closeModal();
+              onDelete();
+            }}
+          >
+            {t('Delete')}
+          </Button>
+        </Flex>
+      </Footer>
+    </Fragment>
+  ));
+}
+
 export default function useDeleteFeedback({feedbackItem}: Props) {
   const feedbackId = feedbackItem.feedback_id;
 
@@ -48,27 +72,6 @@ export default function useDeleteFeedback({feedbackItem}: Props) {
   }, [api, feedbackId, setFeedback, setFeedbackSlug, url]);
 
   return {
-    onDelete: () =>
-      openModal(({Body, Footer, closeModal}: ModalRenderProps) => (
-        <Fragment>
-          <Body>
-            {t('Deleting this feedback is permanent. Are you sure you wish to continue?')}
-          </Body>
-          <Footer>
-            <Flex gap={space(1)}>
-              <Button onClick={closeModal}>{t('Cancel')}</Button>
-              <Button
-                priority="danger"
-                onClick={() => {
-                  closeModal();
-                  handleDelete();
-                }}
-              >
-                {t('Delete')}
-              </Button>
-            </Flex>
-          </Footer>
-        </Fragment>
-      )),
+    onDelete: () => openDeleteModal({onDelete: handleDelete}),
   };
 }
