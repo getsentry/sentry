b25a2facd94eb04f62662d177ca42d2539cd6543
Shruthi <shruthilaya.jaganathan@sentry.io>
2023-10-13 12:22:48 -0400

    feat(starfish): Add a metrics ribbon to screen spans page (#57975)
    ![Screenshot 2023-10-12 at 11 08 23
    AM](https://github.com/getsentry/sentry/assets/63818634/6a5b7745-fd6c-4c93-824d-ceedac259e86)

--- static/app/views/starfish/views/screens/screenLoadSpans/index.tsx
+++ static/app/views/starfish/views/screens/screenLoadSpans/index.tsx
@@ -22,6 +22,7 @@ import {useRoutingContext} from 'sentry/views/starfish/utils/routingContext';
 import {QueryParameterNames} from 'sentry/views/starfish/views/queryParameters';
 import {ScreensView, YAxis} from 'sentry/views/starfish/views/screens';
 import {ScreenLoadSpansTable} from 'sentry/views/starfish/views/screens/screenLoadSpans/table';
+import {ScreenMetricsRibbon} from 'sentry/views/starfish/views/screens/screenMetricsRibbon';
 import {SampleList} from 'sentry/views/starfish/views/spanSummaryPage/sampleList';
 
 type Query = {
@@ -86,6 +87,7 @@ function ScreenLoadSpans() {
                     <DatePageFilter alignDropdown="left" />
                   </PageFilterBar>
                   <ReleaseComparisonSelector />
+                  <ScreenMetricsRibbon />
                 </Container>
               </StarfishPageFiltersContainer>
               <ScreensView
@@ -129,7 +131,6 @@ const Container = styled('div')`
   display: grid;
   grid-template-rows: auto auto auto;
   gap: ${space(2)};
-  margin-bottom: ${space(2)};
 
   @media (min-width: ${p => p.theme.breakpoints.small}) {
     grid-template-rows: auto;
