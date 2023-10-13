8ef34cd76eef8f8a247ab15ac7a5ff5d4887602c
Will Ceolin <wceolin@users.noreply.github.com>
2023-10-13 10:53:59 -0300

    fix(docs): Fix root_source_code_paths option

--- static/app/gettingStartedDocs/elixir/elixir.tsx
+++ static/app/gettingStartedDocs/elixir/elixir.tsx
@@ -59,7 +59,7 @@ config :sentry,
 dsn: "${dsn}",
 environment_name: :prod,
 enable_source_code_context: true,
-root_source_code_path: File.cwd!(),
+root_source_code_paths: [File.cwd!()],
 tags: %{
   env: "production"
 },
