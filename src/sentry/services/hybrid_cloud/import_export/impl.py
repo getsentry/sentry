df24d6121e6f94205c74fb82da415db50159e686
Alex Zaslavsky <azaslavsky@users.noreply.github.com>
2023-10-13 16:25:13 -0700

    feat(backup): Enable export over RPC (#57740)
    Exports are already done on a sequential, per-model basis, going in
    reverse dependency order. This change modifies that scheme to work
    across an RPC boundary: code running on REGION instances (the only kind
    of instance that may perform import/export operations) requests the
    exports using either the local implementation (if the model is REGION as
    well), or by sending an RPC call to the same method on the remote
    CONTROL silo instance. Code executed in MONOLITH mode (aka, self-hosted
    and single tenant instances) does all exporting locally.
    
    The new `import_export_service` is thus a bit different from some of the
    others in `hybrid_cloud`. Whereas existing services like `Access`
    provide a means to get the same information using different logic
    depending on which Silo they are located in (ex: using `Organization` or
    `OrganizationMapping` as the source of truth), this service executes
    identical logic in all silo kinds, and requires users to pick the
    implementation themselves. That is, users must examine the model they
    are about to request be exported, and make their own decision as to
    which implementation, remote or local, to use. This is a bit unergonomic
    compared to other `hybrid_cloud` services, but it is only used by one
    rather unique endpoint (the import/export functions) for a very specific
    purpose, making this API a bit easier to swallow.
    
    Issue: getsentry/team-ospo#196

---
+++ src/sentry/services/hybrid_cloud/import_export/impl.py
@@ -0,0 +1,197 @@
+# Please do not use
+#     from __future__ import annotations
+# in modules such as this one where hybrid cloud data models or service classes are
+# defined, because we want to reflect on type annotations and avoid forward references.
+
+from typing import List, Optional, Set
+
+from django.core.serializers import serialize
+from django.db.models import Q
+
+from sentry.backup.dependencies import (
+    ImportKind,
+    NormalizedModelName,
+    PrimaryKeyMap,
+    dependencies,
+    get_model,
+    get_model_name,
+)
+from sentry.backup.findings import InstanceID
+from sentry.backup.helpers import DatetimeSafeDjangoJSONEncoder, Filter
+from sentry.backup.scopes import ExportScope
+from sentry.models.user import User
+from sentry.models.userpermission import UserPermission
+from sentry.models.userrole import UserRoleUser
+from sentry.services.hybrid_cloud.import_export.model import (
+    RpcExportError,
+    RpcExportErrorKind,
+    RpcExportOk,
+    RpcExportResult,
+    RpcExportScope,
+    RpcFilter,
+    RpcPrimaryKeyMap,
+)
+from sentry.services.hybrid_cloud.import_export.service import ImportExportService
+from sentry.silo.base import SiloMode
+
+
+class UniversalImportExportService(ImportExportService):
+    """
+    This implementation is universal regardless of which mode (CONTROL, REGION, or MONOLITH) it is
+    run in. All import/export codepaths must be executed in REGION or MONOLITH instances only, so
+    the only case in which the caller should use the remote implementation are when trying to
+    import/export a CONTROL model from a REGION instance. In such cases, it is up to the caller to
+    manually select the correct remote/local instance based on the model being being
+    imported/exported with a block of code like:
+
+        if SiloMode.CONTROL in model._meta.silo_limit.modes:
+            import_export_service.export_by_model(...)
+        else:
+            ImportExportService.get_local_implementation().export_by_model(...)
+    """
+
+    def export_by_model(
+        self,
+        *,
+        model_name: str = "",
+        from_pk: int = 0,
+        scope: Optional[RpcExportScope] = None,
+        filter_by: List[RpcFilter],
+        pk_map: RpcPrimaryKeyMap,
+        indent: int = 2,
+    ) -> RpcExportResult:
+        try:
+            from sentry.db.models.base import BaseModel
+
+            deps = dependencies()
+            batch_model_name = NormalizedModelName(model_name)
+            model = get_model(batch_model_name)
+            if model is None or not issubclass(model, BaseModel):
+                return RpcExportError(
+                    kind=RpcExportErrorKind.UnknownModel,
+                    on=InstanceID(model_name),
+                    reason=f"The model `{model_name}` could not be found",
+                )
+
+            silo_mode = SiloMode.get_current_mode()
+            model_modes = model._meta.silo_limit.modes  # type: ignore
+            if silo_mode != SiloMode.MONOLITH and silo_mode not in model_modes:
+                return RpcExportError(
+                    kind=RpcExportErrorKind.IncorrectSiloModeForModel,
+                    on=InstanceID(model_name),
+                    reason=f"The model `{model_name}` was forwarded to the incorrect silo (it cannot be exported from the {silo_mode} silo)",
+                )
+
+            if scope is None:
+                return RpcExportError(
+                    kind=RpcExportErrorKind.UnspecifiedScope,
+                    on=InstanceID(model_name),
+                    reason="The RPC was called incorrectly, please set an `ExportScope` parameter",
+                )
+
+            export_scope = scope.from_rpc()
+            in_pk_map = pk_map.from_rpc()
+            allowed_relocation_scopes = export_scope.value
+            possible_relocation_scopes = model.get_possible_relocation_scopes()
+            includable = possible_relocation_scopes & allowed_relocation_scopes
+            if not includable or model._meta.proxy:
+                return RpcExportError(
+                    kind=RpcExportErrorKind.UnexportableModel,
+                    on=InstanceID(model_name),
+                    reason=f"The model `{str(batch_model_name)}` is not exportable",
+                )
+
+            max_pk = from_pk
+            out_pk_map = PrimaryKeyMap()
+            filters: List[Filter] = []
+            for fb in filter_by:
+                if NormalizedModelName(fb.model_name) == batch_model_name:
+                    filters.append(fb.from_rpc())
+
+            def filter_objects(queryset_iterator):
+                # Intercept each value from the queryset iterator, ensure that it has the correct
+                # relocation scope and that all of its dependencies have already been exported. If
+                # they have, store it in the `pk_map`, and then yield it again. If they have not, we
+                # know that some upstream model was filtered out, so we ignore this one as well.
+                for item in queryset_iterator:
+                    if not item.get_relocation_scope() in allowed_relocation_scopes:
+                        continue
+
+                    model = type(item)
+                    model_name = get_model_name(model)
+
+                    # Make sure this model is not explicitly being filtered.
+                    for f in filters:
+                        if f.model == model and getattr(item, f.field, None) not in f.values:
+                            break
+                    else:
+                        # Now make sure its not transitively filtered either.
+                        for field, foreign_field in deps[model_name].foreign_keys.items():
+                            dependency_model_name = get_model_name(foreign_field.model)
+                            field_id = field if field.endswith("_id") else f"{field}_id"
+                            fk = getattr(item, field_id, None)
+                            if fk is None:
+                                # Null deps are allowed.
+                                continue
+                            if in_pk_map.get_pk(dependency_model_name, fk) is None:
+                                # The foreign key value exists, but not found! An upstream model
+                                # must have been filtered out, so we can filter this one out as
+                                # well.
+                                break
+                        else:
+                            # For models that may have circular references to themselves (unlikely),
+                            # keep track of the new pk in the input map as well.
+                            nonlocal max_pk
+                            max_pk = item.pk
+                            in_pk_map.insert(model_name, item.pk, item.pk, ImportKind.Inserted)
+                            out_pk_map.insert(model_name, item.pk, item.pk, ImportKind.Inserted)
+                            yield item
+
+            def yield_objects():
+                q = Q(pk__gt=from_pk)
+
+                # Only do database query filtering if this is a non-global export. If it is a
+                # global export, we want absolutely every relocatable model, so no need to
+                # filter.
+                if export_scope != ExportScope.Global:
+                    # Create a Django filter from the relevant `filter_by` clauses.
+                    query = dict()
+                    for f in filters:
+                        if f.model == model:
+                            query[f.field + "__in"] = f.values
+                    q &= Q(**query)
+                    q = model.query_for_relocation_export(q, in_pk_map)
+
+                pk_name = model._meta.pk.name  # type: ignore
+                queryset = model._base_manager.filter(q).order_by(pk_name)
+                return filter_objects(queryset.iterator())
+
+            json_data = serialize(
+                "json",
+                yield_objects(),
+                indent=indent,
+                use_natural_foreign_keys=False,
+                cls=DatetimeSafeDjangoJSONEncoder,
+            )
+
+            return RpcExportOk(
+                mapped_pks=RpcPrimaryKeyMap.into_rpc(out_pk_map), max_pk=max_pk, json_data=json_data
+            )
+
+        except Exception as e:
+            return RpcExportError(
+                kind=RpcExportErrorKind.Unknown,
+                on=InstanceID(model_name),
+                reason=f"Unknown internal error occurred: {e}",
+            )
+
+    def get_all_globally_privileged_users(self) -> Set[int]:
+        admin_user_pks: Set[int] = set()
+        admin_user_pks.update(
+            User.objects.filter(Q(is_staff=True) | Q(is_superuser=True)).values_list(
+                "id", flat=True
+            )
+        )
+        admin_user_pks.update(UserPermission.objects.values_list("user_id", flat=True))
+        admin_user_pks.update(UserRoleUser.objects.values_list("user_id", flat=True))
+        return admin_user_pks
