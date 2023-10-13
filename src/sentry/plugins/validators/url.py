0e983ce95290b4fcd0b19589d0a9f17453ce7e6d
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-10-13 11:58:48 -0400

    ref: add some types to a few http-related functions (#58074)
    <!-- Describe your PR here. -->

--- src/sentry/plugins/validators/url.py
+++ src/sentry/plugins/validators/url.py
@@ -2,7 +2,7 @@ from sentry.exceptions import PluginError
 from sentry.http import is_valid_url
 
 
-def URLValidator(value, **kwargs):
+def URLValidator(value: str, **kwargs: object) -> str:
     if not value.startswith(("http://", "https://")):
         raise PluginError("Not a valid URL.")
     if not is_valid_url(value):
