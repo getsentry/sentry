218aa4b06c0f54d0a603582c09185e8129aa595e
nathan hsieh <hsieh.nathan@gmail.com>
2023-10-13 16:00:09 -0700

    replace getExactDuration

--- static/app/utils/formatters.tsx
+++ static/app/utils/formatters.tsx
@@ -187,87 +187,45 @@ const SUFFIX_ABBR = {
   milliseconds: t('ms'),
 };
 /**
- * Returns a human readable exact duration.
+ * Translates seconds into human readable format of seconds, minutes, hours, days, and years
  * 'precision' arg will truncate the results to the specified suffix
  *
- * e.g. 1 hour 25 minutes 15 seconds
+ * @param  {number} seconds The number of seconds to be processed
+ * @return {string}         The phrase describing the amount of time
  */
-export function getExactDuration(
-  seconds: number,
-  abbreviation: boolean = false,
-  precision: keyof typeof SUFFIX_ABBR = 'milliseconds'
-) {
-  const minSuffix = ` ${precision}`;
+export function getExactDuration(seconds: number, abbreviation: boolean = false) {
+  const levels = [
+    [Math.floor(seconds / 31536000), abbreviation ? 'years' : 'yr'],
+    [Math.floor((seconds % 31536000) / 604800), abbreviation ? 'weeks' : 'wk'],
+    [Math.floor(((seconds % 31536000) % 604800) / 86400), abbreviation ? 'days' : 'd'],
+    [
+      Math.floor((((seconds % 31536000) % 604800) % 86400) / 3600),
+      abbreviation ? 'hours' : 'hr',
+    ],
+    [
+      Math.floor(((((seconds % 31536000) % 604800) % 86400) % 3600) / 60),
+      abbreviation ? 'minutes' : 'min',
+    ],
+    [
+      ((((seconds % 31536000) % 604800) % 86400) % 3600) % 60,
+      abbreviation ? 'seconds' : 's',
+    ],
+  ];
+  let returntext = '';
 
-  const convertDuration = (secs: number, abbr: boolean): string => {
-    // value in milliseconds
-    const msValue = round(secs * 1000);
-    const value = round(Math.abs(secs * 1000));
-
-    const divideBy = (time: number) => {
-      return {
-        quotient: msValue < 0 ? Math.ceil(msValue / time) : Math.floor(msValue / time),
-        remainder: msValue % time,
-      };
-    };
-
-    if (value >= WEEK || (value && minSuffix === ' weeks')) {
-      const {quotient, remainder} = divideBy(WEEK);
-      const suffix = abbr ? t('wk') : ` ${tn('week', 'weeks', quotient)}`;
-
-      return `${quotient}${suffix} ${
-        minSuffix === suffix ? '' : convertDuration(remainder / 1000, abbr)
-      }`;
+  for (let i = 0, max = levels.length; i < max; i++) {
+    if (levels[i][0] === 0) {
+      continue;
     }
-    if (value >= DAY || (value && minSuffix === ' days')) {
-      const {quotient, remainder} = divideBy(DAY);
-      const suffix = abbr ? t('d') : ` ${tn('day', 'days', quotient)}`;
-
-      return `${quotient}${suffix} ${
-        minSuffix === suffix ? '' : convertDuration(remainder / 1000, abbr)
-      }`;
-    }
-    if (value >= HOUR || (value && minSuffix === ' hours')) {
-      const {quotient, remainder} = divideBy(HOUR);
-      const suffix = abbr ? t('hr') : ` ${tn('hour', 'hours', quotient)}`;
-
-      return `${quotient}${suffix} ${
-        minSuffix === suffix ? '' : convertDuration(remainder / 1000, abbr)
-      }`;
-    }
-    if (value >= MINUTE || (value && minSuffix === ' minutes')) {
-      const {quotient, remainder} = divideBy(MINUTE);
-      const suffix = abbr ? t('min') : ` ${tn('minute', 'minutes', quotient)}`;
-
-      return `${quotient}${suffix} ${
-        minSuffix === suffix ? '' : convertDuration(remainder / 1000, abbr)
-      }`;
-    }
-    if (value >= SECOND || (value && minSuffix === ' seconds')) {
-      const {quotient, remainder} = divideBy(SECOND);
-      const suffix = abbr ? t('s') : ` ${tn('second', 'seconds', quotient)}`;
-
-      return `${quotient}${suffix} ${
-        minSuffix === suffix ? '' : convertDuration(remainder / 1000, abbr)
-      }`;
-    }
-
-    if (value === 0) {
-      return '';
-    }
-
-    const suffix = abbr ? t('ms') : ` ${tn('millisecond', 'milliseconds', value)}`;
-
-    return `${msValue}${suffix}`;
-  };
-
-  const result = convertDuration(seconds, abbreviation).trim();
-
-  if (result.length) {
-    return result;
+    returntext +=
+      ' ' +
+      levels[i][0] +
+      ' ' +
+      (!abbreviation && levels[i][0] === 1
+        ? (levels[i][1] as string).substring(0, (levels[i][1] as string).length - 1) // strip the 's' from the end if its singular
+        : levels[i][1]);
   }
-
-  return `0${abbreviation ? SUFFIX_ABBR[precision] : minSuffix}`;
+  return returntext.trim();
 }
 
 export const SEC_IN_WK = 604800;
