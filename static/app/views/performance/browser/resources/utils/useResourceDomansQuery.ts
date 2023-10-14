8c9ef6e8cc21dd5a8f7e111d9d6c0e1122e31a7b
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-10-13 20:32:45 -0400

    feat(browser-starfish): fetch span domain in dropdown and allow filtering (#58076)

--- static/app/views/performance/browser/resources/utils/useResourceDomansQuery.ts
+++ static/app/views/performance/browser/resources/utils/useResourceDomansQuery.ts
@@ -1,3 +1,5 @@
+import * as Sentry from '@sentry/react';
+
 import {useDiscoverQuery} from 'sentry/utils/discover/discoverQuery';
 import EventView from 'sentry/utils/discover/eventView';
 import {DiscoverDatasets} from 'sentry/utils/discover/types';
@@ -12,19 +14,19 @@ const {SPAN_DOMAIN, SPAN_OP} = SpanMetricsField;
 /**
  * Gets a list of pages that have a resource.
  */
-export const useResourcePagesQuery = () => {
+export const useResourceDomainsQuery = () => {
   const location = useLocation();
   const pageFilters = usePageFilters();
   const {slug: orgSlug} = useOrganization();
-  const resourceFilters = useResourceModuleFilters();
-  const {[SPAN_DOMAIN]: spanDomain} = resourceFilters;
+  const {transaction} = useResourceModuleFilters();
 
-  const fields = ['transaction', 'count()']; // count() is only here because an aggregation is required for the query to work
+  const fields = [SPAN_DOMAIN, 'count()']; // count() is only here because an aggregation is required for the query to work
 
   const queryConditions = [
-    `${SPAN_OP}:${resourceFilters.type || 'resource.*'}`,
-    ...(spanDomain ? [`${SPAN_DOMAIN}:${spanDomain}`] : []),
-  ]; // TODO: We will need to consider other ops
+    `${SPAN_OP}:resource.*`,
+    `has:${SPAN_DOMAIN}`,
+    ...(transaction ? [`transaction:${transaction}`] : []),
+  ];
 
   const eventView = EventView.fromNewQueryWithPageFilters(
     {
@@ -44,7 +46,16 @@ export const useResourcePagesQuery = () => {
     orgSlug,
     limit: 100,
   });
+  const data: string[] =
+    result?.data?.data
+      .map(row => {
+        const domains = row[SPAN_DOMAIN] as any as string[];
+        if (domains?.length > 1) {
+          Sentry.captureException(new Error('More than one domain found in a resource'));
+        }
+        return domains[0].toString();
+      })
+      .sort() || [];
 
-  const pages = result?.data?.data.map(row => row.transaction.toString()).sort() || [];
-  return {...result, data: pages};
+  return {...result, data};
 };
--- static/app/views/performance/browser/resources/utils/useResourceFilters.ts
+++ static/app/views/performance/browser/resources/utils/useResourceFilters.ts
@@ -5,7 +5,7 @@ import {useLocation} from 'sentry/utils/useLocation';
 export enum BrowserStarfishFields {
   RESOURCE_TYPE = 'type',
   TRANSACTION = 'transaction',
-  DOMAIN = 'domain',
+  SPAN_DOMAIN = 'span.domain',
   GROUP_ID = 'groupId',
   DESCRIPTION = 'description',
   RESOURCE_RENDER_BLOCKING_STATUS = 'resource.render_blocking_status',
@@ -16,16 +16,17 @@ export type ModuleFilters = {
     | ''
     | 'non-blocking'
     | 'blocking';
+  [BrowserStarfishFields.SPAN_DOMAIN]?: string;
   [BrowserStarfishFields.RESOURCE_TYPE]?: 'resource.script' | 'resource.img';
   [BrowserStarfishFields.TRANSACTION]?: string;
-  [BrowserStarfishFields.DOMAIN]?: string;
+  [BrowserStarfishFields.SPAN_DOMAIN]?: string;
 };
 
 export const useResourceModuleFilters = () => {
   const location = useLocation<ModuleFilters>();
 
   return pick(location.query, [
-    BrowserStarfishFields.DOMAIN,
+    BrowserStarfishFields.SPAN_DOMAIN,
     BrowserStarfishFields.RESOURCE_TYPE,
     BrowserStarfishFields.TRANSACTION,
     BrowserStarfishFields.GROUP_ID,
