403ade2273831d0d9cdd06972d327db1d83e2df3
nathan hsieh <hsieh.nathan@gmail.com>
2023-10-17 12:31:57 -0700

    remove be feature check

--- src/sentry/api/endpoints/release_thresholds/release_threshold_index.py
+++ src/sentry/api/endpoints/release_thresholds/release_threshold_index.py
@@ -4,7 +4,6 @@ from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry import features
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
@@ -32,9 +31,6 @@ class ReleaseThresholdIndexEndpoint(OrganizationEndpoint):
     }
 
     def get(self, request: Request, organization: Organization) -> HttpResponse:
-        if not features.has("organizations:release-ui-v2", organization, actor=request.user):
-            return self.respond(status=403)
-
         validator = ReleaseThresholdIndexGETValidator(
             data=request.query_params,
         )
