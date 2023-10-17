a4a12277210614d52d72bdefc4dafb14c6be9a7c
Scott Cooper <scttcper@gmail.com>
2023-10-17 15:28:11 -0700

    revert version change

--- babel.config.ts
+++ babel.config.ts
@@ -15,7 +15,7 @@ const config: TransformOptions = {
       '@babel/preset-env',
       {
         useBuiltIns: 'usage',
-        corejs: '3.33',
+        corejs: '3.27',
       },
     ],
     '@babel/preset-typescript',
