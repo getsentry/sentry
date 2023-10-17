53459db9369688cab389a3064bcbdd38a36f5468
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-10-17 10:51:44 -0400

    code reivew

--- static/app/views/performance/browser/resources/resourceTable.tsx
+++ static/app/views/performance/browser/resources/resourceTable.tsx
@@ -16,10 +16,17 @@ import {useResourcesQuery} from 'sentry/views/performance/browser/resources/util
 import {DurationCell} from 'sentry/views/starfish/components/tableCells/durationCell';
 import {renderHeadCell} from 'sentry/views/starfish/components/tableCells/renderHeadCell';
 import {ThroughputCell} from 'sentry/views/starfish/components/tableCells/throughputCell';
-import {SpanMetricsField} from 'sentry/views/starfish/types';
+import {SpanFunction, SpanMetricsField} from 'sentry/views/starfish/types';
 
-const {SPAN_DESCRIPTION, RESOURCE_RENDER_BLOCKING_STATUS, SPAN_OP, SPAN_SELF_TIME} =
-  SpanMetricsField;
+const {
+  SPAN_DESCRIPTION,
+  RESOURCE_RENDER_BLOCKING_STATUS,
+  SPAN_OP,
+  SPAN_SELF_TIME,
+  HTTP_RESPONSE_CONTENT_LENGTH,
+} = SpanMetricsField;
+
+const {SPM} = SpanFunction;
 
 type Row = {
   'avg(http.response_content_length)': number;
@@ -48,24 +55,24 @@ function ResourceTable({sort}: Props) {
     {key: SPAN_OP, width: COL_WIDTH_UNDEFINED, name: 'Type'},
     {key: `avg(${SPAN_SELF_TIME})`, width: COL_WIDTH_UNDEFINED, name: 'Avg Duration'},
     {
-      key: 'spm()',
+      key: `${SPM}()`,
       width: COL_WIDTH_UNDEFINED,
-      name: 'Throughput',
+      name: t('Throughput'),
     },
     {
-      key: 'avg(http.response_content_length)',
+      key: `avg(${HTTP_RESPONSE_CONTENT_LENGTH})`,
       width: COL_WIDTH_UNDEFINED,
-      name: 'Avg Resource size',
+      name: t('Avg Resource size'),
     },
     {
       key: RESOURCE_RENDER_BLOCKING_STATUS,
       width: COL_WIDTH_UNDEFINED,
-      name: 'Render blocking',
+      name: t('Render blocking'),
     },
     {
       key: 'http.decoded_response_content_length',
       width: COL_WIDTH_UNDEFINED,
-      name: 'Uncompressed',
+      name: t('Uncompressed'),
     },
   ];
   const tableData: Row[] = data.length
