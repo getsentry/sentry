43b4aefa58e908679f24780a87707df9094187b1
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-10-17 15:57:11 -0400

    add stats

--- static/app/views/starfish/types.tsx
+++ static/app/views/starfish/types.tsx
@@ -28,9 +28,16 @@ export enum SpanMetricsField {
   PROJECT_ID = 'project.id',
   RESOURCE_RENDER_BLOCKING_STATUS = 'resource.render_blocking_status',
   HTTP_RESPONSE_CONTENT_LENGTH = 'http.response_content_length',
+  HTTP_DECODED_RESPONSE_BODY_LENGTH = 'http.decoded_response_body_length',
+  HTTP_RESPONSE_TRANSFER_SIZE = 'http.response_transfer_size',
 }
 
-export type SpanNumberFields = 'span.self_time' | 'span.duration';
+export type SpanNumberFields =
+  | 'span.self_time'
+  | 'span.duration'
+  | SpanMetricsField.HTTP_DECODED_RESPONSE_BODY_LENGTH
+  | SpanMetricsField.HTTP_RESPONSE_CONTENT_LENGTH
+  | SpanMetricsField.HTTP_RESPONSE_TRANSFER_SIZE;
 
 export type SpanStringFields =
   | 'span.op'
