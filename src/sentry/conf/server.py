4d61631caf273f27d0d2c808cc7e007c734173ab
Arthur Knaus <arthur.knaus@sentry.io>
2023-10-17 15:06:32 +0200

    chore(alerts): Add migration UI feature flag

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1363,6 +1363,8 @@ SENTRY_FEATURES = {
     "auth:register": True,
     # Enables alert creation on indexed events in UI (use for PoC/testing only)
     "organizations:alert-allow-indexed": False,
+    # Enables transaction to metric dataset migration UI for alert rules
+    "organizations:alert-migration-ui": False,
     # Enables tagging javascript errors from the browser console.
     "organizations:javascript-console-error-tag": False,
     # Enables the cron job to auto-enable codecov integrations.
