e002d648dba68cfb54bc1d9c22456f89d3e35521
Alexander Tarasov <alex.tarasov@sentry.io>
2023-10-18 11:53:17 +0200

    fix(integrations): correct domain in the install link (#57463)
    Fixes this mismatch between an org selected for integration and the
    current org (in the URL):
    <img width="810" alt="image"
    src="https://github.com/getsentry/sentry/assets/1127549/20d130c0-9457-47cf-a9b7-b4fe566e82ee">
    
    The install link should contain `https://sentry.sentry.io` in this case.
    
    ---------
    
    Co-authored-by: Evan Purkhiser <evanpurkhiser@gmail.com>

--- static/app/utils.tsx
+++ static/app/utils.tsx
@@ -339,3 +339,8 @@ export function escapeDoubleQuotes(str: string) {
 export function generateBaseControlSiloUrl() {
   return ConfigStore.get('links').sentryUrl || '';
 }
+
+export function generateOrgSlugUrl(orgSlug) {
+  const sentryDomain = window.__initialData.links.sentryUrl.split('/')[2];
+  return `${window.location.protocol}//${orgSlug}.${sentryDomain}${window.location.pathname}`;
+}
