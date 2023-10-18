9b21236ada8f15a9d62973a837510f1bc3c6ab1c
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2023-10-18 11:19:33 -0700

    draft

--- src/sentry/api/endpoints/organization_group_index.py
+++ src/sentry/api/endpoints/organization_group_index.py
@@ -224,6 +224,8 @@ class OrganizationGroupIndexEndpoint(OrganizationEventsEndpointBase):
         :qparam querystring query: an optional Sentry structured search
                                    query.  If not provided an implied
                                    ``"is:unresolved"`` is assumed.)
+        :qparam bool savedSearch:  if this is set to False, then we are making the request without
+                                   a saved search and will look for the default search from this endpoint.
         :pparam string organization_slug: the slug of the organization the
                                           issues belong to.
         :auth: required
