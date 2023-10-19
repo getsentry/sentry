e4bd723a19b2bd534af4d3b12f9a276868c92bee
Malachi Willey <malwilley@gmail.com>
2023-10-19 13:53:39 -0700

    feat(suspect-commits): Hook up new blame logic to commit_context task (#58307)
    Closes https://github.com/getsentry/sentry/issues/58253
    
    1. Adds a util function `find_commit_context_for_event_all_frames` that
    searches frames for the most recent commit.
    2. Adds the minimal amount of changes necessary in the
    `process_commit_context` task to use this new util function when the
    feature flag is enabled.
    3. Tests for the new code path

--- src/sentry/integrations/utils/commit_context.py
+++ src/sentry/integrations/utils/commit_context.py
@@ -2,19 +2,79 @@ from __future__ import annotations
 
 import logging
 from datetime import datetime, timedelta, timezone
-from typing import Any, List, Mapping, Sequence, Tuple
+from typing import Any, List, Mapping, Optional, Sequence, Tuple
 
 import sentry_sdk
 
 from sentry import analytics
 from sentry.integrations.base import IntegrationInstallation
+from sentry.integrations.mixins.commit_context import (
+    CommitContextMixin,
+    FileBlameInfo,
+    SourceLineInfo,
+)
+from sentry.models.commit import Commit
+from sentry.models.commitauthor import CommitAuthor
 from sentry.models.integrations.repository_project_path_config import RepositoryProjectPathConfig
 from sentry.ownership.grammar import get_source_code_path_from_stacktrace_path
 from sentry.services.hybrid_cloud.integration import integration_service
 from sentry.shared_integrations.exceptions import ApiError
+from sentry.utils import metrics
 from sentry.utils.committers import get_stacktrace_path_from_event_frame
 
 logger = logging.getLogger("sentry.tasks.process_commit_context")
+
+
+def find_commit_context_for_event_all_frames(
+    code_mappings: Sequence[RepositoryProjectPathConfig],
+    frames: Sequence[Mapping[str, Any]],
+    organization_id: int,
+    project_id: int,
+    extra: Mapping[str, Any],
+) -> tuple[Optional[FileBlameInfo], Optional[IntegrationInstallation]]:
+    """
+    Given a list of event frames and code mappings, finds the most recent commit.
+    Will also emit analytics events for success or failure.
+    """
+    (
+        integration_to_files_mapping,
+        num_successfully_mapped_frames,
+    ) = _generate_integration_to_files_mapping(
+        frames=frames, code_mappings=code_mappings, extra=extra
+    )
+
+    file_blames, integration_to_install_mapping = _get_blames_from_all_integrations(
+        integration_to_files_mapping=integration_to_files_mapping,
+        organization_id=organization_id,
+        project_id=project_id,
+        extra=extra,
+    )
+
+    most_recent_blame = max(file_blames, key=lambda blame: blame.commit.committedDate, default=None)
+    # Only return suspect commits that are less than a year old
+    selected_blame = (
+        most_recent_blame
+        if most_recent_blame and is_date_less_than_year(most_recent_blame.commit.committedDate)
+        else None
+    )
+
+    _record_commit_context_all_frames_analytics(
+        selected_blame=selected_blame,
+        most_recent_blame=most_recent_blame,
+        organization_id=organization_id,
+        project_id=project_id,
+        extra=extra,
+        frames=frames,
+        file_blames=file_blames,
+        num_successfully_mapped_frames=num_successfully_mapped_frames,
+    )
+
+    return (
+        selected_blame,
+        integration_to_install_mapping[selected_blame.code_mapping.organization_integration_id]
+        if selected_blame
+        else None,
+    )
 
 
 def find_commit_context_for_event(
@@ -116,5 +176,241 @@ def find_commit_context_for_event(
     return result, installation
 
 
-def is_date_less_than_year(date: datetime):
+def is_date_less_than_year(date: datetime) -> bool:
     return date > datetime.now(tz=timezone.utc) - timedelta(days=365)
+
+
+def get_or_create_commit_from_blame(
+    blame: FileBlameInfo, organization_id: int, extra: Mapping[str, str | int]
+) -> Commit:
+    """
+    From a blame object, see if a matching commit already exists in sentry_commit.
+    If not, create it.
+    """
+    try:
+        commit: Commit = Commit.objects.get(
+            repository_id=blame.repo.id,
+            key=blame.commit.commitId,
+        )
+        if commit.message == "":
+            commit.message = blame.commit.commitMessage
+            commit.save()
+
+        return commit
+    except Commit.DoesNotExist:
+        logger.info(
+            "process_commit_context_all_frames.no_commit_in_sentry",
+            extra={
+                **extra,
+                "sha": blame.commit.commitId,
+                "repository_id": blame.repo.id,
+                "code_mapping_id": blame.code_mapping.id,
+                "reason": "commit_sha_does_not_exist_in_sentry",
+            },
+        )
+
+        # If a commit does not exist in sentry_commit, we will add it
+        commit_author, _ = CommitAuthor.objects.get_or_create(
+            organization_id=organization_id,
+            email=blame.commit.commitAuthorEmail,
+            defaults={"name": blame.commit.commitAuthorName},
+        )
+        commit = Commit.objects.create(
+            organization_id=organization_id,
+            repository_id=blame.repo.id,
+            key=blame.commit.commitId,
+            date_added=blame.commit.committedDate,
+            author=commit_author,
+            message=blame.commit.commitMessage,
+        )
+
+        logger.info(
+            "process_commit_context_all_frames.added_commit_to_sentry_commit",
+            extra={
+                **extra,
+                "sha": blame.commit.commitId,
+                "repository_id": blame.repo.id,
+                "code_mapping_id": blame.code_mapping.id,
+                "reason": "commit_sha_does_not_exist_in_sentry_for_all_code_mappings",
+            },
+        )
+
+        return commit
+
+
+def _generate_integration_to_files_mapping(
+    frames: Sequence[Mapping[str, Any]],
+    code_mappings: Sequence[RepositoryProjectPathConfig],
+    extra: Mapping[str, Any],
+) -> tuple[dict[str, list[SourceLineInfo]], int]:
+    """
+    Because a single stack trace can be mapped to multiple integrations,
+    this function is used to separate files into each integration so that
+    we can later call get_commit_context_all_frames on each integration.
+    """
+    integration_to_files_mapping: dict[str, list[SourceLineInfo]] = {}
+    num_successfully_mapped_frames = 0
+
+    for frame in frames:
+        for code_mapping in code_mappings:
+            stacktrace_path = get_stacktrace_path_from_event_frame(frame)
+
+            if not stacktrace_path:
+                logger.info(
+                    "process_commit_context_all_frames.no_stacktrace_path",
+                    extra={
+                        **extra,
+                        "code_mapping_id": code_mapping.id,
+                    },
+                )
+                continue
+
+            src_path = get_source_code_path_from_stacktrace_path(stacktrace_path, code_mapping)
+
+            # src_path can be none if the stacktrace_path is an invalid filepath
+            if not src_path:
+                logger.info(
+                    "process_commit_context_all_frames.no_src_path",
+                    extra={
+                        **extra,
+                        "code_mapping_id": code_mapping.id,
+                        "stacktrace_path": stacktrace_path,
+                    },
+                )
+                continue
+
+            num_successfully_mapped_frames += 1
+            logger.info(
+                "process_commit_context_all_frames.found_stacktrace_and_src_paths",
+                extra={
+                    **extra,
+                    "code_mapping_id": code_mapping.id,
+                    "stacktrace_path": stacktrace_path,
+                    "src_path": src_path,
+                },
+            )
+
+            files = integration_to_files_mapping.setdefault(
+                code_mapping.organization_integration_id, []
+            )
+            files.append(
+                SourceLineInfo(
+                    lineno=frame["lineno"],
+                    path=src_path,
+                    ref=code_mapping.default_branch or "master",
+                    repo=code_mapping.repository,
+                    code_mapping=code_mapping,
+                )
+            )
+            break
+
+    return integration_to_files_mapping, num_successfully_mapped_frames
+
+
+def _get_blames_from_all_integrations(
+    integration_to_files_mapping: dict[str, list[SourceLineInfo]],
+    organization_id: int,
+    project_id: int,
+    extra: Mapping[str, Any],
+) -> tuple[list[FileBlameInfo], dict[str, IntegrationInstallation]]:
+    """
+    Calls get_commit_context_all_frames for each integration, using the file
+    list provided for the integration ID, and returns a combined list of
+    file blames.
+    """
+    file_blames: list[FileBlameInfo] = []
+    integration_to_install_mapping: dict[str, IntegrationInstallation] = {}
+
+    for integration_organization_id, files in integration_to_files_mapping.items():
+        integration = integration_service.get_integration(
+            organization_integration_id=integration_organization_id
+        )
+        if not integration:
+            continue
+        install = integration.get_installation(organization_id=organization_id)
+        if not isinstance(install, CommitContextMixin):
+            continue
+        integration_to_install_mapping[integration_organization_id] = install
+        try:
+            blames = install.get_commit_context_all_frames(files)
+            file_blames.extend(blames)
+        except ApiError:
+            logger.exception(
+                "process_commit_context_all_frames.api_error",
+                extra={
+                    **extra,
+                    "project_id": project_id,
+                    "provider": integration.provider,
+                    "integration_id": integration.id,
+                },
+            )
+
+    return file_blames, integration_to_install_mapping
+
+
+def _record_commit_context_all_frames_analytics(
+    selected_blame: Optional[FileBlameInfo],
+    most_recent_blame: Optional[FileBlameInfo],
+    organization_id: int,
+    project_id: int,
+    extra: Mapping[str, Any],
+    frames: Sequence[Mapping[str, Any]],
+    file_blames: Sequence[FileBlameInfo],
+    num_successfully_mapped_frames: int,
+):
+    if not selected_blame:
+        reason = "commit_too_old" if most_recent_blame else "no_commit_found"
+        metrics.incr(
+            "sentry.tasks.process_commit_context.aborted",
+            tags={"detail": reason},
+        )
+        logger.info(
+            "process_commit_context_all_frames.find_commit_context_failed",
+            extra={
+                **extra,
+                "project_id": project_id,
+                "reason": reason,
+                "num_frames": len(frames),
+            },
+        )
+        analytics.record(
+            "integrations.failed_to_fetch_commit_context_all_frames",
+            organization_id=organization_id,
+            project_id=project_id,
+            group_id=extra["group"],
+            event_id=extra["event"],
+            num_frames=len(frames),
+            num_successfully_mapped_frames=num_successfully_mapped_frames,
+            reason=reason,
+        )
+        return
+
+    unique_commit_ids = {blame.commit.commitId for blame in file_blames}
+    unique_author_emails = {blame.commit.commitAuthorEmail for blame in file_blames}
+    selected_frame_index = next(
+        (
+            i
+            for i, frame in enumerate(frames)
+            if frame["lineno"] == selected_blame.lineno
+            and get_source_code_path_from_stacktrace_path(
+                get_stacktrace_path_from_event_frame(frame) or "", selected_blame.code_mapping
+            )
+            == selected_blame.path
+        ),
+        None,
+    )
+
+    analytics.record(
+        "integrations.successfully_fetched_commit_context_all_frames",
+        organization_id=organization_id,
+        project_id=project_id,
+        group_id=extra["group"],
+        event_id=extra["event"],
+        num_frames=len(frames),
+        num_unique_commits=len(unique_commit_ids),
+        num_unique_commit_authors=len(unique_author_emails),
+        num_successfully_mapped_frames=num_successfully_mapped_frames,
+        selected_frame_index=selected_frame_index,
+        selected_provider=selected_blame.repo.provider,
+        selected_code_mapping_id=selected_blame.code_mapping.id,
+    )
