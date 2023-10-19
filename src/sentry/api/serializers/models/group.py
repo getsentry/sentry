c636a05bfc5c41d865235bc2278c1fe839828b30
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2023-10-19 10:02:23 -0700

    feat(issue-stream): Combined endpoint for stats + unhandled (#58195)
    this pr created a new combined endpoint for stats + unhandled
    information for groups that will be used on the issue stream. Right now,
    there is no front end request being made to it, but the front end will
    use the endpoint instead of the existing `issues-stats` endpoint.
    
    Closes https://github.com/getsentry/sentry/issues/57846

--- src/sentry/api/serializers/models/group.py
+++ src/sentry/api/serializers/models/group.py
@@ -25,7 +25,7 @@ import sentry_sdk
 from django.conf import settings
 from django.db.models import Min, prefetch_related_objects
 
-from sentry import tagstore
+from sentry import features, tagstore
 from sentry.api.serializers import Serializer, register, serialize
 from sentry.api.serializers.models.actor import ActorSerializer
 from sentry.api.serializers.models.plugin import is_plugin_deprecated
@@ -493,7 +493,13 @@ class GroupSerializerBase(Serializer, ABC):
     def _get_group_snuba_stats(
         self, item_list: Sequence[Group], seen_stats: Optional[Mapping[Group, SeenStats]]
     ):
-        if self._collapse("unhandled"):
+        if (
+            self._collapse("unhandled")
+            and len(item_list) > 0
+            and features.has(
+                "organizations:issue-stream-performance", item_list[0].project.organization
+            )
+        ):
             return None
         start = self._get_start_from_seen_stats(seen_stats)
         unhandled = {}
