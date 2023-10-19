7672a449024449110922dccea6d3b70b6e44a620
John <john.yang@sentry.io>
2023-10-19 11:53:17 -0700

    feat(metrics): Add new option to enable dual writing from new cache schema for indexer (#58413)

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -994,6 +994,13 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
+# An option to enable writing from the new schema for the caching indexer
+register(
+    "sentry-metrics.indexer.write-new-cache-namespace",
+    default=False,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
+
 # Global and per-organization limits on the writes to the string indexer's DB.
 #
 # Format is a list of dictionaries of format {
