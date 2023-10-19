ae57cf88a974b80c4e3d4609dcfdab4f81d8cbda
Arthur Knaus <arthur.knaus@sentry.io>
2023-10-19 11:36:34 +0200

    ref(migration-ui): Add TODO comments

--- static/app/views/alerts/utils/migrationUi.ts
+++ static/app/views/alerts/utils/migrationUi.ts
@@ -4,13 +4,15 @@ import useOrganization from 'sentry/utils/useOrganization';
 import {Dataset} from 'sentry/views/alerts/rules/metric/types';
 import {CombinedMetricIssueAlerts} from 'sentry/views/alerts/types';
 
+// TODO(telemetry-experience): remove when the migration is complete
 export const hasMigrationFeatureFlag = (organization: Organization): boolean =>
   organization.features.includes('alert-migration-ui');
 
+// TODO(telemetry-experience): remove when the migration is complete
 export const ruleNeedsMigration = (rule: CombinedMetricIssueAlerts): boolean => {
   return 'dataset' in rule && rule.dataset === Dataset.TRANSACTIONS;
 };
-
+// TODO(telemetry-experience): remove when the migration is complete
 export function useOrgNeedsMigration(): boolean {
   const organization = useOrganization();
   const {data = []} = useApiQuery<CombinedMetricIssueAlerts[]>(
