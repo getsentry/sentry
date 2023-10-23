932aae9402ed658b1fa63e96a4091e720e7a91b3
Malachi Willey <malwilley@gmail.com>
2023-10-23 13:50:11 -0700

    Fix class name in repr

--- src/sentry/integrations/gitlab/client.py
+++ src/sentry/integrations/gitlab/client.py
@@ -33,7 +33,7 @@ class GitLabRateLimitInfo:
         return datetime.utcfromtimestamp(self.reset).strftime("%H:%M:%S")
 
     def __repr__(self) -> str:
-        return f"GitLabRateLimit(limit={self.limit},rem={self.remaining},reset={self.reset}),used={self.used})"
+        return f"GitLabRateLimitInfo(limit={self.limit},rem={self.remaining},reset={self.reset}),used={self.used})"
 
 
 class GitLabApiClientPath:
