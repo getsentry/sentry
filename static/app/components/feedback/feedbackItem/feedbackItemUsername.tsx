16853f949f9fd6551c6dcf016433c6512f20ef8a
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-10-23 14:59:36 -0700

    ref(bug reports): remove display of name for now

--- static/app/components/feedback/feedbackItem/feedbackItemUsername.tsx
+++ static/app/components/feedback/feedbackItem/feedbackItemUsername.tsx
@@ -1,7 +1,4 @@
-import styled from '@emotion/styled';
-
 import {t} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
 import type {HydratedFeedbackItem} from 'sentry/utils/feedback/item/types';
 
 interface Props {
@@ -10,27 +7,15 @@ interface Props {
 }
 
 export default function FeedbackItemUsername({feedbackItem, detailDisplay}: Props) {
-  const name = ''; // feedbackItem.name;
   const email = feedbackItem.metadata.contact_email;
 
-  if (!email && !name) {
+  if (!email) {
     return <strong>{t('Anonymous User')}</strong>;
   }
 
   if (detailDisplay) {
-    return (
-      <strong>
-        {name ?? t('No Name')}
-        <Purple>•</Purple>
-        {email ?? t('No Email')}
-      </strong>
-    );
+    return <strong>{email ?? t('No Email')}</strong>;
   }
 
   return <strong>{email ?? name}</strong>;
 }
-
-const Purple = styled('span')`
-  color: ${p => p.theme.purple300};
-  padding: ${space(0.5)};
-`;
