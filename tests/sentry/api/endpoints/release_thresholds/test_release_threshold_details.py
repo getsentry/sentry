6f4e0379726d133c4f8cf385bb8df85fafacf69c
Nathan Hsieh <6186377+nhsiehgit@users.noreply.github.com>
2023-10-23 09:49:30 -0700

    implement threshold delete (#58553)
    DELETE api method for managing release thresholds

--- tests/sentry/api/endpoints/release_thresholds/test_release_threshold_details.py
+++ tests/sentry/api/endpoints/release_thresholds/test_release_threshold_details.py
@@ -71,7 +71,68 @@ class ReleaseThresholdDetailsGETTest(APITestCase):
         assert response.data["trigger_type"] == "over"
         assert response.data["value"] == 100
         assert response.data["window_in_seconds"] == 1800
-        assert response.data["environment"]["name"] == "canary"
+
+
+class ReleaseThresholdDetailsDELETETest(APITestCase):
+    def setUp(self):
+        super().setUp()
+        self.user = self.create_user(is_staff=True, is_superuser=True)
+
+        self.canary_environment = Environment.objects.create(
+            organization_id=self.organization.id, name="canary"
+        )
+        self.production_environment = Environment.objects.create(
+            organization_id=self.organization.id, name="production"
+        )
+        self.login_as(user=self.user)
+
+        self.basic_threshold = ReleaseThreshold.objects.create(
+            threshold_type=0,
+            trigger_type=0,
+            value=100,
+            window_in_seconds=1800,
+            project=self.project,
+            environment=self.canary_environment,
+        )
+
+    def test_invalid_threshold_id(self):
+        url = reverse(
+            "sentry-api-0-project-release-thresholds-details",
+            kwargs={
+                "organization_slug": self.organization.slug,
+                "project_slug": self.project.slug,
+                "release_threshold": 123,
+            },
+        )
+        response = self.client.delete(url)
+
+        assert response.status_code == 404
+
+    def test_invalid_project(self):
+        url = reverse(
+            "sentry-api-0-project-release-thresholds-details",
+            kwargs={
+                "organization_slug": self.organization.slug,
+                "project_slug": "kingdom_of_the_crystal_skull",
+                "release_threshold": self.basic_threshold.id,
+            },
+        )
+        response = self.client.delete(url)
+
+        assert response.status_code == 404
+
+    def test_valid(self):
+        url = reverse(
+            "sentry-api-0-project-release-thresholds-details",
+            kwargs={
+                "organization_slug": self.organization.slug,
+                "project_slug": self.project.slug,
+                "release_threshold": self.basic_threshold.id,
+            },
+        )
+        response = self.client.delete(url)
+
+        assert response.status_code == 204
 
 
 class ReleaseThresholdDetailsPUTTest(APITestCase):
