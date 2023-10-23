25a4fa1c3ff727ca8a7a93577769739c4adaba76
Mark Story <mark@mark-story.com>
2023-10-23 18:01:06 -0400

    feat(hybridcloud) Make error embed view use region URLs v2
    Mulligan on #58608 as those changes imported a dev dependency which
    doesn't exist in production builds.
    
    This reverts commit 17daa1f4ef4ef6f0a4abcd3ce567424f7fab550b.

--- src/sentry/web/frontend/error_page_embed.py
+++ src/sentry/web/frontend/error_page_embed.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from django import forms
 from django.db import IntegrityError, router
 from django.http import HttpRequest, HttpResponse
@@ -6,6 +8,7 @@ from django.utils.safestring import mark_safe
 from django.utils.translation import gettext_lazy as _
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
+from django_stubs_ext import StrOrPromise
 
 from sentry import eventstore
 from sentry.models.options.project_option import ProjectOption
@@ -13,10 +16,12 @@ from sentry.models.project import Project
 from sentry.models.projectkey import ProjectKey
 from sentry.models.userreport import UserReport
 from sentry.signals import user_feedback_received
+from sentry.types.region import get_local_region
 from sentry.utils import json
 from sentry.utils.db import atomic_transaction
-from sentry.utils.http import absolute_uri, is_valid_origin, origin_from_request
+from sentry.utils.http import is_valid_origin, origin_from_request
 from sentry.utils.validators import normalize_event_id
+from sentry.web.frontend.base import region_silo_view
 from sentry.web.helpers import render_to_response, render_to_string
 
 GENERIC_ERROR = _("An unknown error occurred while submitting your report. Please try again.")
@@ -34,7 +39,7 @@ DEFAULT_COMMENTS_LABEL = _("What happened?")
 DEFAULT_CLOSE_LABEL = _("Close")
 DEFAULT_SUBMIT_LABEL = _("Submit Crash Report")
 
-DEFAULT_OPTIONS = {
+DEFAULT_OPTIONS: dict[str, StrOrPromise] = {
     "title": DEFAULT_TITLE,
     "subtitle": DEFAULT_SUBTITLE,
     "subtitle2": DEFAULT_SUBTITLE2,
@@ -66,6 +71,7 @@ class UserReportForm(forms.ModelForm):
         fields = ("name", "email", "comments")
 
 
+@region_silo_view
 class ErrorPageEmbedView(View):
     def _get_project_key(self, request: HttpRequest):
         try:
@@ -187,6 +193,8 @@ class ErrorPageEmbedView(View):
         elif request.method == "POST":
             return self._smart_response(request, {"errors": dict(form.errors)}, status=400)
 
+        region = get_local_region()
+        endpoint = region.to_url(request.get_full_path())
         show_branding = (
             ProjectOption.objects.get_value(
                 project=key.project, key="feedback:branding", default="1"
@@ -211,7 +219,7 @@ class ErrorPageEmbedView(View):
         )
 
         context = {
-            "endpoint": mark_safe("*/" + json.dumps(absolute_uri(request.get_full_path())) + ";/*"),
+            "endpoint": mark_safe("*/" + json.dumps(endpoint) + ";/*"),
             "template": mark_safe("*/" + json.dumps(template) + ";/*"),
             "strings": mark_safe(
                 "*/"
