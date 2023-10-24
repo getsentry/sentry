9baabfb66741860599c906aa20c15da7537e9a63
nathan hsieh <hsieh.nathan@gmail.com>
2023-10-24 09:06:02 -0700

    update note

--- static/app/utils/formatters.tsx
+++ static/app/utils/formatters.tsx
@@ -187,7 +187,7 @@ const SUFFIX_ABBR = {
 };
 /**
  * Returns a human readable exact duration.
- * 'minDuration' arg will truncate the results to the specified amount
+ * 'precision' arg will truncate the results to the specified suffix
  *
  * e.g. 1 hour 25 minutes 15 seconds
  */
