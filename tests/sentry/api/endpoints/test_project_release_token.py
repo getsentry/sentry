4fe6d22971a302532f78c1a12480889c163c3198
Mark Story <mark@mark-story.com>
2023-10-24 13:37:18 -0400

    feat(hybridcloud) Update release webhook generation to include region (#58655)
    - New release webhooks will need to use region domains to work.
    - Allow regionless requests to be forwarded to the monolith region to
    maintain compatiblity with existing customers.

--- tests/sentry/api/endpoints/test_project_release_token.py
+++ tests/sentry/api/endpoints/test_project_release_token.py
@@ -2,8 +2,11 @@ from django.urls import reverse
 
 from sentry.models.options.project_option import ProjectOption
 from sentry.testutils.cases import APITestCase
+from sentry.testutils.silo import region_silo_test
+from sentry.types.region import get_local_region
 
 
+@region_silo_test(stable=True)
 class ReleaseTokenGetTest(APITestCase):
     def test_simple(self):
         project = self.create_project(name="foo")
@@ -38,6 +41,22 @@ class ReleaseTokenGetTest(APITestCase):
         assert response.status_code == 200, response.content
         assert response.data["token"] is not None
         assert ProjectOption.objects.get_value(project, "sentry:release-token") is not None
+
+    def test_generate_region_webhookurl(self):
+        project = self.create_project(name="foo")
+
+        url = reverse(
+            "sentry-api-0-project-releases-token",
+            kwargs={"organization_slug": project.organization.slug, "project_slug": project.slug},
+        )
+
+        self.login_as(user=self.user)
+
+        response = self.client.get(url)
+        assert response.status_code == 200, response.content
+
+        region = get_local_region()
+        assert response.data["webhookUrl"].startswith(region.to_url("/"))
 
     def test_regenerates_token(self):
         project = self.create_project(name="foo")
