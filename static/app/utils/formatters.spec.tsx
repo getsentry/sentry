aa584fc3197041f9bfae5901b1c0d58df4896b12
nathan hsieh <hsieh.nathan@gmail.com>
2023-10-24 08:22:51 -0700

    add min suffix to exactduration formatter

--- static/app/utils/formatters.spec.tsx
+++ static/app/utils/formatters.spec.tsx
@@ -329,7 +329,7 @@ describe('getExactDuration', () => {
     expect(getExactDuration(0)).toEqual('0 milliseconds');
   });
 
-  it('should format in the right way', () => {
+  it('should format durations without extra suffixes', () => {
     expect(getExactDuration(2.030043848568126)).toEqual('2 seconds 30 milliseconds');
     expect(getExactDuration(0.2)).toEqual('200 milliseconds');
     expect(getExactDuration(13)).toEqual('13 seconds');
@@ -353,5 +353,17 @@ describe('getExactDuration', () => {
 
   it('should abbreviate label', () => {
     expect(getExactDuration(234235435, true)).toEqual('387wk 2d 1hr 23min 55s');
+  });
+
+  it('should pin/truncate to the min suffix if provided', () => {
+    expect(getExactDuration(0, false, 'seconds')).toEqual('0 seconds');
+    expect(getExactDuration(0.2, false, 'seconds')).toEqual('0 seconds');
+    expect(getExactDuration(2.030043848568126, false, 'seconds')).toEqual('2 seconds');
+    expect(getExactDuration(13, false, 'seconds')).toEqual('13 seconds');
+    expect(getExactDuration(60, false, 'seconds')).toEqual('1 minute');
+    expect(getExactDuration(121, false, 'seconds')).toEqual('2 minutes 1 second');
+    expect(getExactDuration(234235435.2, false, 'seconds')).toEqual(
+      '387 weeks 2 days 1 hour 23 minutes 55 seconds'
+    );
   });
 });
