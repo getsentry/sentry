ee18627106375fa87a2b9a336d9b097a1d1c90ba
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-10-24 10:37:53 -0700

    ref(replays): accessible rage/dead click/error count colors (#58662)
    the click counts are now gray/black rather than red/yellow.
    
    before:
    <img width="505" alt="SCR-20231023-mjvk"
    src="https://github.com/getsentry/sentry/assets/56095982/e4ea5b03-e6e2-45af-ad9a-78308260bd18">
    
    <img width="216" alt="SCR-20231023-mjxu"
    src="https://github.com/getsentry/sentry/assets/56095982/a3e26de4-8edb-4973-bd79-bcf390880d89">
    
    
    
    
    after:
    
    <img width="260" alt="SCR-20231023-nqjm"
    src="https://github.com/getsentry/sentry/assets/56095982/f84b9598-27e9-445e-95e0-52e522ee54a0">
    
    <img width="1162" alt="SCR-20231023-nqci"
    src="https://github.com/getsentry/sentry/assets/56095982/d41cdbf3-2e04-4ee6-ab31-b83baf7c5775">
    
    <img width="323" alt="SCR-20231024-ipps"
    src="https://github.com/getsentry/sentry/assets/56095982/41bd2e1d-a846-46e2-805e-cd0bcafb71bc">
    
    <img width="267" alt="SCR-20231024-iqdc"
    src="https://github.com/getsentry/sentry/assets/56095982/d2880734-5c26-463f-a240-108db89748a7">
    
    
    <!-- Describe your PR here. -->

--- static/app/views/replays/deadRageClick/deadRageSelectorCards.tsx
+++ static/app/views/replays/deadRageClick/deadRageSelectorCards.tsx
@@ -107,9 +107,7 @@ function AccordionWidget({
   return (
     <StyledWidgetContainer>
       <StyledHeaderContainer>
-        <ClickColor color={clickColor}>
-          <IconCursorArrow />
-        </ClickColor>
+        <IconCursorArrow color={clickColor} />
         {header}
       </StyledHeaderContainer>
       {isLoading ? (
@@ -187,10 +185,10 @@ function AccordionItemHeader({
   selectorQuery: string;
 }) {
   const clickCount = (
-    <ClickColor color={clickColor}>
-      <IconCursorArrow size="xs" />
+    <ClickCount>
+      <IconCursorArrow size="xs" color={clickColor} />
       {count}
-    </ClickColor>
+    </ClickCount>
   );
   return (
     <StyledAccordionHeader>
@@ -248,8 +246,8 @@ const SplitCardContainer = styled('div')`
   align-items: stretch;
 `;
 
-const ClickColor = styled(TextOverflow)<{color: ColorOrAlias}>`
-  color: ${p => p.theme[p.color]};
+const ClickCount = styled(TextOverflow)`
+  color: ${p => p.theme.gray400};
   display: grid;
   grid-template-columns: auto auto;
   gap: ${space(0.75)};
