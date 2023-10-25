0f062dc76e0a2c2f73924ba3c14724eb5f044c69
isabellaenriquez <isabella.enriquez@sentry.io>
2023-10-25 14:56:46 -0700

    tests

--- tests/sentry/notifications/notifications/test_organization_request.py
+++ tests/sentry/notifications/notifications/test_organization_request.py
@@ -1,3 +1,5 @@
+import pytest
+
 from sentry.models.organizationmember import OrganizationMember
 from sentry.notifications.notifications.organization_request import OrganizationRequestNotification
 from sentry.notifications.notifications.strategies.role_based_recipient_strategy import (
@@ -7,6 +9,7 @@ from sentry.notifications.types import NotificationSettingOptionValues, Notifica
 from sentry.services.hybrid_cloud.actor import ActorType, RpcActor
 from sentry.services.hybrid_cloud.notifications.service import notifications_service
 from sentry.testutils.cases import TestCase
+from sentry.testutils.helpers.features import with_feature
 from sentry.testutils.silo import region_silo_test
 from sentry.types.integrations import ExternalProviders
 
@@ -57,3 +60,39 @@ class GetParticipantsTest(TestCase):
             ExternalProviders.EMAIL: self.user_actors,
             ExternalProviders.SLACK: self.user_actors,
         }
+
+    def test_build_notification_footer(self):
+        actor = RpcActor(id=self.user.id, actor_type=ActorType.USER)
+        notification = DummyRequestNotification(self.organization, self.user)
+        query_params = f"?referrer={notification.metrics_key}-slack-user&notification_uuid={notification.notification_uuid}"
+        url = str(
+            self.organization.absolute_url(
+                "/settings/account/notifications/approval/", query=query_params
+            )
+        )
+        assert (
+            notification.build_notification_footer(actor, ExternalProviders.SLACK)
+            == f"You are receiving this notification because you're listed as an organization Member | <{url}|Notification Settings>"
+        )
+
+    def test_build_notification_footer_for_team(self):
+        actor = RpcActor(id=self.team.id, actor_type=ActorType.TEAM)
+        notification = DummyRequestNotification(self.organization, self.user)
+        with pytest.raises(NotImplementedError):
+            notification.build_notification_footer(actor, ExternalProviders.SLACK)
+
+    @with_feature("organizations:team-workflow-notifications")
+    def test_team_recipient_gets_team_settings_link(self):
+        actor = RpcActor(id=self.team.id, actor_type=ActorType.TEAM, slug=self.team.slug)
+        notification = DummyRequestNotification(self.organization, self.user)
+        query_params = f"?referrer={notification.metrics_key}-slack-team&notification_uuid={notification.notification_uuid}"
+        url = str(
+            self.organization.absolute_url(
+                f"/settings/{self.organization.slug}/teams/{self.team.slug}/notifications/",
+                query=query_params,
+            )
+        )
+        assert (
+            notification.build_notification_footer(actor, ExternalProviders.SLACK)
+            == f"You are receiving this notification because you're listed as an organization Member | <{url}|Notification Settings>"
+        )
