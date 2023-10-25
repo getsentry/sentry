d84ff9e8b900522925882a96e6d4fd60685cae7f
Joshua Ferge <josh.ferge@sentry.io>
2023-10-25 10:21:11 -0700

    add feedback widget to new perf pages

--- static/app/views/performance/browser/webVitals/pageOverview.tsx
+++ static/app/views/performance/browser/webVitals/pageOverview.tsx
@@ -5,6 +5,7 @@ import ProjectAvatar from 'sentry/components/avatar/projectAvatar';
 import Breadcrumbs from 'sentry/components/breadcrumbs';
 import {LinkButton} from 'sentry/components/button';
 import FeatureBadge from 'sentry/components/featureBadge';
+import FeedbackWidget from 'sentry/components/feedback/widget/feedbackWidget';
 import * as Layout from 'sentry/components/layouts/thirds';
 import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
@@ -120,6 +121,7 @@ export default function PageOverview() {
           </TabList>
         </Layout.Header>
         <Layout.Body>
+          <FeedbackWidget />
           <Layout.Main>
             <TopMenuContainer>
               {transaction && (
