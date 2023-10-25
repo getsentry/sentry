b3c10f4285a783ae41199b1361eb8b72c07513d0
Bartek Ogryczak <bartek.ogryczak@sentry.io>
2023-10-24 17:28:43 -0700

    Issue Platform event type is 'generic_event'

--- src/sentry/issues/json_schemas.py
+++ src/sentry/issues/json_schemas.py
@@ -5,7 +5,7 @@ from sentry_kafka_schemas import SchemaNotFound, sentry_kafka_schemas
 logger = logging.getLogger(__name__)
 
 try:
-    EVENT_PAYLOAD_SCHEMA = sentry_kafka_schemas._get_schema("events")["schema"]
+    EVENT_PAYLOAD_SCHEMA = sentry_kafka_schemas._get_schema("generic-events")["schema"]
 except SchemaNotFound:
     logger.exception("Failed to load Events schema from Kafka schemas")
     raise
