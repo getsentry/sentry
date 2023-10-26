9f9f570bda21b7148a6b2c022a928b78a8d77a3c
LucasZF <lucas-zimerman1@hotmail.com>
2023-10-26 10:36:35 -0300

    Update static/app/components/onboarding/productSelection.tsx

--- static/app/components/onboarding/productSelection.tsx
+++ static/app/components/onboarding/productSelection.tsx
@@ -92,7 +92,10 @@ export const platformProductAvailability = {
     ProductSolution.PERFORMANCE_MONITORING,
     ProductSolution.SESSION_REPLAY,
   ],
-  capacitor: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.SESSION_REPLAY],
+  capacitor: [
+    ProductSolution.PERFORMANCE_MONITORING,
+    ProductSolution.SESSION_REPLAY,
+  ],
   'javascript-ember': [
     ProductSolution.PERFORMANCE_MONITORING,
     ProductSolution.SESSION_REPLAY,
