17d0b42817ecbbc631f112d98e5d3f4eac8d4b91
Edward Gou <edward.gou@sentry.io>
2023-10-26 15:42:51 -0400

    adds feature flag to control pageoverview links to new vs old versions

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -219,6 +219,7 @@ default_manager.add("organizations:starfish-aggregate-span-waterfall", Organizat
 default_manager.add("organizations:starfish-test-endpoint", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:starfish-wsv-chart-dropdown", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:starfish-browser-webvitals", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
+default_manager.add("organizations:starfish-browser-webvitals-pageoverview-v2", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:streamline-targeting-context", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:symbol-sources", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:sentry-pride-logo-footer", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
