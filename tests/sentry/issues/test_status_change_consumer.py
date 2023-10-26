363be4b8b81ad7846d218ded95351cbcca936996
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-10-26 12:23:00 +0200

    feat(issue-platform): Add logic to set status to ignored for issue platform issues (#58301)
    Sets up the issue platform API support the IGNORED (or archived) status.
    We add validation to ensure the provided (status, substatus) pair is
    valid before updating the group status, activity, and group history.
    
    The expectation for the UNTIL_ESCALATING and UNTIL_CONDITION_MET
    substatuses is that the caller will handle any conditional evaluation
    and will call the API with the new status when the conditions are met.
    
    Fixes https://github.com/getsentry/sentry/issues/58036
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- tests/sentry/issues/test_status_change_consumer.py
+++ tests/sentry/issues/test_status_change_consumer.py
@@ -7,8 +7,9 @@ from sentry.issues.occurrence_consumer import _process_message
 from sentry.models.group import GroupStatus
 from sentry.models.grouphash import GroupHash
 from sentry.testutils.helpers.datetime import iso_format
-from sentry.testutils.helpers.features import with_feature
+from sentry.testutils.helpers.features import apply_feature_flag_on_cls
 from sentry.testutils.pytest.fixtures import django_db_all
+from sentry.types.group import GroupSubStatus
 from tests.sentry.issues.test_occurrence_consumer import IssueOccurrenceTestBase
 
 
@@ -29,10 +30,10 @@ def get_test_message(
     return payload
 
 
+@apply_feature_flag_on_cls("organizations:issue-platform-api-crons-sd")
 class StatusChangeProcessMessageTest(IssueOccurrenceTestBase):
-    @django_db_all
-    @with_feature("organizations:issue-platform-api-crons-sd")
-    def test_valid_payload(self) -> None:
+    def setUp(self):
+        super().setUp()
         event = self.store_event(
             data={
                 "event_id": "a" * 32,
@@ -44,9 +45,11 @@ class StatusChangeProcessMessageTest(IssueOccurrenceTestBase):
         )
         group = event.group
         assert group
-        group_hash = GroupHash.objects.filter(group=group, project=self.project).first()
+        self.group_hash = GroupHash.objects.filter(group=group, project=self.project).first()
 
-        message = get_test_message(self.project.id, fingerprint=[group_hash.hash])
+    @django_db_all
+    def test_valid_payloa_resolved(self) -> None:
+        message = get_test_message(self.project.id, fingerprint=[self.group_hash.hash])
         result = _process_message(message)
         assert result is not None
         group_info = result[1]
@@ -56,3 +59,20 @@ class StatusChangeProcessMessageTest(IssueOccurrenceTestBase):
 
         assert group.status == GroupStatus.RESOLVED
         assert group.substatus is None
+
+    def test_valid_payload_archived_forever(self) -> None:
+        message = get_test_message(
+            self.project.id,
+            fingerprint=[self.group_hash.hash],
+            new_status=GroupStatus.IGNORED,
+            new_substatus=GroupSubStatus.FOREVER,
+        )
+        result = _process_message(message)
+        assert result is not None
+        group_info = result[1]
+        assert group_info is not None
+        group = group_info.group
+        group.refresh_from_db()
+
+        assert group.status == GroupStatus.IGNORED
+        assert group.substatus == GroupSubStatus.FOREVER
