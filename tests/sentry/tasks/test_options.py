4e1ce622d6ce560131e231077f39634dbd208012
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-10-26 10:27:35 -0700

    Don't mess with the reset of the state of default_manager

--- tests/sentry/tasks/test_options.py
+++ tests/sentry/tasks/test_options.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from unittest.mock import patch
 
-from sentry.options import default_manager, default_store, load_defaults
+from sentry.options import UnknownOption, default_manager, default_store
 from sentry.tasks.options import sync_options
 from sentry.testutils.cases import TestCase
 from sentry.testutils.silo import all_silo_test
@@ -9,27 +9,22 @@ from sentry.testutils.silo import all_silo_test
 
 @all_silo_test(stable=True)
 class SyncOptionsTest(TestCase):
-    @staticmethod
-    def _reset_options():
-        for key in list(default_manager.registry):
-            default_manager.unregister(key)
-        load_defaults()
-
-    def setUp(self):
-        super().setUp()
-        self._reset_options()
+    _TEST_KEY = "foo"
 
     def tearDown(self):
         super().tearDown()
-        self._reset_options()
+        try:
+            default_manager.unregister(self._TEST_KEY)
+        except UnknownOption:
+            pass
 
     def test_task_persistent_name(self):
         assert sync_options.name == "sentry.tasks.options.sync_options"
 
     @patch.object(default_store, "set_cache")
     def test_simple(self, mock_set_cache):
-        default_manager.register("foo")
-        option = default_store.model.objects.create(key="foo", value="bar")
+        default_manager.register(self._TEST_KEY)
+        option = default_store.model.objects.create(key=self._TEST_KEY, value="bar")
         sync_options(cutoff=60)
 
         assert mock_set_cache.called
