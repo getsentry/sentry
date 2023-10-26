e2798804d82cd5540ba4a72052fde089f64007fe
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-10-26 09:30:04 -0700

    Reset options more carefully

--- tests/sentry/tasks/test_options.py
+++ tests/sentry/tasks/test_options.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 from unittest.mock import patch
 
-from sentry.options import default_manager, default_store
+from sentry.options import default_manager, default_store, load_defaults
 from sentry.tasks.options import sync_options
 from sentry.testutils.cases import TestCase
 from sentry.testutils.silo import all_silo_test
@@ -9,10 +9,19 @@ from sentry.testutils.silo import all_silo_test
 
 @all_silo_test(stable=True)
 class SyncOptionsTest(TestCase):
-    def setUp(self):
-        super().setUp()
+    @staticmethod
+    def _reset_options():
         for key in list(default_manager.registry):
             default_manager.unregister(key)
+        load_defaults()
+
+    def setUp(self):
+        super().setUp()
+        self._reset_options()
+
+    def tearDown(self):
+        super().tearDown()
+        self._reset_options()
 
     def test_task_persistent_name(self):
         assert sync_options.name == "sentry.tasks.options.sync_options"
