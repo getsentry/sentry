9a800c87a3d4d5d477ba53edd1217670efa91f56
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-10-30 14:15:03 +0100

    set default values

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -1670,15 +1670,18 @@ register(
 register("metric_alerts.rate_limit", default=False, flags=FLAG_AUTOMATOR_MODIFIABLE)
 
 # SDK Crash Detection
+#
+# The project ID belongs to the sentry organization: https://sentry.sentry.io/projects/cocoa-sdk-crashes/?project=4505469596663808.
 register(
     "issues.sdk_crash_detection.cocoa.project_id",
     type=int,
+    default=4505469596663808,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
 register(
     "issues.sdk_crash_detection.cocoa.sample_rate",
-    default=0.0,
+    default=1.0,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 # END: SDK Crash Detection
