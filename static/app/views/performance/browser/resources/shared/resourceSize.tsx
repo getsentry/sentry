4745a72173e8f0d7ab6257053761f147bccfa76d
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-10-30 12:46:11 -0400

    use double dash instead of triple

--- static/app/views/performance/browser/resources/shared/resourceSize.tsx
+++ static/app/views/performance/browser/resources/shared/resourceSize.tsx
@@ -10,7 +10,7 @@ function ResourceSize(props: Props) {
   const {bytes} = props;
   const hasNoSize = bytes === 0;
   if (hasNoSize) {
-    return <Fragment>---</Fragment>;
+    return <Fragment>--</Fragment>;
   }
 
   return <FileSize bytes={bytes} />;
