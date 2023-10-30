05ade3fec74a31dd93989d4439aee6d427f570ca
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-10-30 12:41:10 -0400

    handle 0 size case

---
+++ static/app/views/performance/browser/resources/shared/resourceSize.tsx
@@ -0,0 +1,19 @@
+import {Fragment} from 'react';
+
+import FileSize from 'sentry/components/fileSize';
+
+type Props = {
+  bytes: number;
+};
+
+function ResourceSize(props: Props) {
+  const {bytes} = props;
+  const hasNoSize = bytes === 0;
+  if (hasNoSize) {
+    return <Fragment>---</Fragment>;
+  }
+
+  return <FileSize bytes={bytes} />;
+}
+
+export default ResourceSize;
