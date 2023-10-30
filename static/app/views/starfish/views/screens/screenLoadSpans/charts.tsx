32676c7451168fdbff30ea2f8e1802711adf83d0
Shruthi <shruthilaya.jaganathan@sentry.io>
2023-10-30 15:02:56 -0400

    feat(starfish): Update screen load spans charts (#58999)
    A couple updates to the screen load spans page:
    - changes the span ribbon to little cards with TTID/TTFD values for
    selected releases
    - TTID chart is broken down by device class and release and is a bar
    chart
    - adds columns for avg span self time for release 1 & release 2
    
    ![Screenshot 2023-10-30 at 2 30
    05 PM](https://github.com/getsentry/sentry/assets/63818634/f073288a-8715-46db-ae1f-1b77f4ebb0c9)
    
    Chart updates
    - units in yaxis and tooltip for selectable bar charts
    - truncated x-axis value, currently hardcoded to truncate at 14
    characters for simplicity
    
    ![Screenshot 2023-10-30 at 2 27
    27 PM](https://github.com/getsentry/sentry/assets/63818634/222ee284-0ece-4ab0-8c55-356392e67c9c)

--- static/app/views/starfish/views/screens/screenLoadSpans/charts.tsx
+++ static/app/views/starfish/views/screens/screenLoadSpans/charts.tsx
@@ -1,4 +1,5 @@
 import {Fragment} from 'react';
+import {useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
 import Color from 'color';
 
@@ -14,6 +15,7 @@ import {tooltipFormatterUsingAggregateOutputType} from 'sentry/utils/discover/ch
 import EventView from 'sentry/utils/discover/eventView';
 import {DiscoverDatasets} from 'sentry/utils/discover/types';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
+import {useLocation} from 'sentry/utils/useLocation';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import Chart, {useSynchronizeCharts} from 'sentry/views/starfish/components/chart';
 import MiniChartPanel from 'sentry/views/starfish/components/miniChartPanel';
@@ -26,6 +28,8 @@ import {
   OUTPUT_TYPE,
   YAXIS_COLUMNS,
 } from 'sentry/views/starfish/views/screens';
+import {ScreensBarChart} from 'sentry/views/starfish/views/screens/screenBarChart';
+import {useTableQuery} from 'sentry/views/starfish/views/screens/screensTable';
 
 export enum YAxis {
   WARM_START,
@@ -38,14 +42,6 @@ export enum YAxis {
   COUNT,
 }
 
-const DEVICE_CLASS_BREAKDOWN_INDEX = {
-  high: 0,
-  medium: 1,
-  low: 2,
-};
-
-const EMPTY = '';
-const UNKNOWN = 'unknown';
 type Props = {
   yAxes: YAxis[];
   additionalFilters?: string[];
@@ -54,6 +50,8 @@ type Props = {
 
 export function ScreenCharts({yAxes, additionalFilters, chartHeight}: Props) {
   const pageFilter = usePageFilters();
+  const location = useLocation();
+  const theme = useTheme();
 
   const yAxisCols = yAxes.map(val => YAXIS_COLUMNS[val]);
 
@@ -73,16 +71,15 @@ export function ScreenCharts({yAxes, additionalFilters, chartHeight}: Props) {
   useSynchronizeCharts();
   const {
     isLoading: seriesIsLoading,
-    data: releaseSeries,
+    data: releaseCountSeries,
     isError,
   } = useEventsStatsQuery({
     eventView: EventView.fromNewQueryWithPageFilters(
       {
         name: '',
-        fields: ['release', 'device.class', ...yAxisCols],
-        topEvents: '6',
-        orderby: yAxisCols[0],
-        yAxis: yAxisCols,
+        fields: ['release', 'count()'],
+        topEvents: '2',
+        yAxis: ['count()'],
         query: queryString,
         dataset: DiscoverDatasets.METRICS,
         version: 2,
@@ -99,104 +96,138 @@ export function ScreenCharts({yAxes, additionalFilters, chartHeight}: Props) {
     initialData: {},
   });
 
+  const {data: deviceClassEvents, isLoading: isDeviceClassEventsLoading} = useTableQuery({
+    eventView: EventView.fromNewQueryWithLocation(
+      {
+        name: '',
+        fields: ['device.class', 'release', ...yAxisCols],
+        orderby: yAxisCols[0],
+        yAxis: yAxisCols,
+        query: queryString,
+        dataset: DiscoverDatasets.METRICS,
+        version: 2,
+      },
+      location
+    ),
+    enabled: !isReleasesLoading,
+  });
+
   if (isReleasesLoading) {
     return <LoadingContainer />;
   }
 
-  const transformedReleaseSeries: {
+  function getTransformedRelease(series) {
+    return Object.keys(series).map((release, index) => {
+      const data =
+        series[release]?.data.map(datum => {
+          return {
+            name: datum[0] * 1000,
+            value: datum[1][0].count,
+          } as SeriesDataUnit;
+        }) ?? [];
+
+      return {
+        seriesName: release,
+        color: CHART_PALETTE[2][index],
+        data,
+      };
+    });
+  }
+
+  const transformedEvents: {
     [yAxisName: string]: {
-      [releaseVersion: string]: {[deviceClass: string]: Series | undefined};
+      [releaseVersion: string]: Series;
     };
   } = {};
+
   yAxes.forEach(val => {
-    transformedReleaseSeries[YAXIS_COLUMNS[val]] = {};
+    transformedEvents[YAXIS_COLUMNS[val]] = {};
     if (primaryRelease) {
-      transformedReleaseSeries[YAXIS_COLUMNS[val]][primaryRelease] = {};
+      transformedEvents[YAXIS_COLUMNS[val]][primaryRelease] = {
+        seriesName: primaryRelease,
+        data: Array(['high', 'medium', 'low', 'Unknown'].length).fill(0),
+      };
     }
     if (secondaryRelease) {
-      transformedReleaseSeries[YAXIS_COLUMNS[val]][secondaryRelease] = {};
+      transformedEvents[YAXIS_COLUMNS[val]][secondaryRelease] = {
+        seriesName: secondaryRelease,
+        data: Array(['high', 'medium', 'low', 'Unknown'].length).fill(0),
+      };
     }
   });
 
-  function renderCharts() {
-    if (defined(releaseSeries)) {
-      Object.keys(releaseSeries).forEach(seriesName => {
-        const [deviceClass, ...releaseArray] = seriesName.split(',');
-        const index = DEVICE_CLASS_BREAKDOWN_INDEX[deviceClass] ?? 3;
-        const release = releaseArray.join(',');
-        const isPrimary = release === primaryRelease;
+  const deviceClassIndex = Object.fromEntries(
+    ['high', 'medium', 'low', 'Unknown'].map((e, i) => [e, i])
+  );
 
-        if (release !== EMPTY) {
-          Object.keys(releaseSeries[seriesName]).forEach(yAxis => {
-            const label = `${deviceClass === EMPTY ? UNKNOWN : deviceClass}, ${release}`;
-            if (yAxis in transformedReleaseSeries) {
-              const data =
-                releaseSeries[seriesName][yAxis]?.data.map(datum => {
-                  return {
-                    name: datum[0] * 1000,
-                    value: datum[1][0].count,
-                  } as SeriesDataUnit;
-                }) ?? [];
+  if (defined(deviceClassEvents)) {
+    deviceClassEvents.data?.forEach(row => {
+      const deviceClass = row['device.class'];
+      const index = deviceClassIndex[deviceClass];
 
-              transformedReleaseSeries[yAxis][release][
-                deviceClass === EMPTY ? UNKNOWN : deviceClass
-              ] = {
-                seriesName: label,
-                color: isPrimary
-                  ? CHART_PALETTE[5][index]
-                  : Color(CHART_PALETTE[5][index]).lighten(0.5).string(),
-                data,
-              };
-            }
-          });
-        }
+      const release = row.release;
+      const isPrimary = release === primaryRelease;
+      yAxes.forEach(val => {
+        transformedEvents[YAXIS_COLUMNS[val]][release].data[index] = {
+          name: deviceClass,
+          value: row[YAXIS_COLUMNS[val]],
+          itemStyle: {
+            color: isPrimary ? theme.gray300 : Color(theme.gray300).lighten(0.5).string(),
+          },
+        } as SeriesDataUnit;
       });
-    }
+    });
+  }
 
+  function renderCharts() {
     return (
       <Fragment>
-        {yAxes.map((val, index) => {
-          return (
-            <ChartsContainerItem key={val}>
-              <MiniChartPanel title={CHART_TITLES[val]}>
-                <Chart
-                  height={chartHeight ?? 180}
-                  data={
-                    ['high', 'medium', 'low', UNKNOWN]
-                      .flatMap(deviceClass => {
-                        return [primaryRelease, secondaryRelease].map(r => {
-                          if (r) {
-                            return transformedReleaseSeries[yAxisCols[index]][r][
-                              deviceClass
-                            ];
-                          }
-                          return null;
-                        });
-                      })
-                      .filter(v => defined(v)) as Series[]
-                  }
-                  loading={seriesIsLoading}
-                  utc={false}
-                  grid={{
-                    left: '0',
-                    right: '0',
-                    top: '16px',
-                    bottom: '0',
-                  }}
-                  showLegend
-                  definedAxisTicks={2}
-                  isLineChart
-                  aggregateOutputFormat={OUTPUT_TYPE[val]}
-                  tooltipFormatterOptions={{
-                    valueFormatter: value =>
-                      tooltipFormatterUsingAggregateOutputType(value, OUTPUT_TYPE[val]),
-                  }}
-                  errored={isError}
-                />
-              </MiniChartPanel>
-            </ChartsContainerItem>
-          );
-        })}
+        <ChartsContainerItem>
+          <MiniChartPanel title={CHART_TITLES[YAxis.COUNT]}>
+            <Chart
+              height={chartHeight ?? 180}
+              data={
+                defined(releaseCountSeries)
+                  ? getTransformedRelease(releaseCountSeries)
+                  : []
+              }
+              loading={seriesIsLoading}
+              utc={false}
+              grid={{
+                left: '0',
+                right: '0',
+                top: '16px',
+                bottom: '0',
+              }}
+              showLegend
+              definedAxisTicks={2}
+              isLineChart
+              aggregateOutputFormat={OUTPUT_TYPE[YAxis.COUNT]}
+              tooltipFormatterOptions={{
+                valueFormatter: value =>
+                  tooltipFormatterUsingAggregateOutputType(
+                    value,
+                    OUTPUT_TYPE[YAxis.COUNT]
+                  ),
+              }}
+              errored={isError}
+            />
+          </MiniChartPanel>
+        </ChartsContainerItem>
+        <ChartsContainerItem key="deviceClass">
+          <ScreensBarChart
+            chartOptions={yAxes.map(yAxis => {
+              return {
+                title: CHART_TITLES[yAxis],
+                yAxis: YAXIS_COLUMNS[yAxis],
+                series: Object.values(transformedEvents[YAXIS_COLUMNS[yAxis]]),
+                xAxisLabel: ['high', 'medium', 'low', 'Unknown'],
+              };
+            })}
+            chartHeight={chartHeight ?? 180}
+            isLoading={isDeviceClassEventsLoading}
+          />
+        </ChartsContainerItem>
       </Fragment>
     );
   }
