c7884f1e840279f6d2eaba9ea3a7d28b0c907b6e
Nathan Hsieh <6186377+nhsiehgit@users.noreply.github.com>
2023-10-30 11:44:30 -0700

    Implement 'New Threshold' creation (#58966)
    Implements ability to create new thresholds without an existing
    threshold
    
    
    https://github.com/getsentry/sentry/assets/6186377/7f90a1ee-87d7-477f-88fa-6f5349a36d04

--- static/app/views/releases/thresholdsList/index.tsx
+++ static/app/views/releases/thresholdsList/index.tsx
@@ -11,14 +11,19 @@ import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilter';
 import PanelTable from 'sentry/components/panels/panelTable';
+import {ALL_ACCESS_PROJECTS} from 'sentry/constants/pageFilters';
 import {t} from 'sentry/locale';
+import ConfigStore from 'sentry/stores/configStore';
 import {space} from 'sentry/styles/space';
+import {Project} from 'sentry/types';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
+import useProjects from 'sentry/utils/useProjects';
 import useRouter from 'sentry/utils/useRouter';
 
 import Header from '../components/header';
-import {Threshold} from '../utils/types';
+import {NEW_GROUP_PREFIX} from '../utils/constants';
+import {NewThresholdGroup, Threshold} from '../utils/types';
 import useFetchThresholdsListData from '../utils/useFetchThresholdsListData';
 
 import {ThresholdGroupRows} from './thresholdGroupRows';
@@ -29,13 +34,15 @@ function ReleaseThresholdList({}: Props) {
   const [listError, setListError] = useState<string>('');
   const router = useRouter();
   const organization = useOrganization();
+  const [newThresholdGroup, setNewThresholdGroup] = useState<NewThresholdGroup[]>([]);
+  const [newGroupIterator, setNewGroupIterator] = useState<number>(0);
   useEffect(() => {
     const hasV2ReleaseUIEnabled = organization.features.includes('release-ui-v2');
     if (!hasV2ReleaseUIEnabled) {
       router.replace('/releases/');
     }
   }, [router, organization]);
-  // const {projects} = useProjects();
+  const {projects} = useProjects();
   const {selection} = usePageFilters();
   const {
     data: thresholds = [],
@@ -48,51 +55,57 @@ function ReleaseThresholdList({}: Props) {
     selectedEnvs: selection.environments,
   });
 
-  // const _getAllSelectedProjects = (): Project[] => {
-  //   return projects.filter(project =>
-  //     selection.projects.some(id => String(id) === project.id || id === -1)
-  //   );
-  // };
+  const selectedProjects: Project[] = useMemo(() => {
+    return projects.filter(project =>
+      selection.projects.some(id => String(id) === project.id || id === -1)
+    );
+  }, [projects, selection.projects]);
 
-  // const _getAllEnvironments = (): string[] => {
-  //   const selectedProjects = selection.projects.map(id => String(id));
-  //   const {user} = ConfigStore.getState();
-  //   const allEnvSet = new Set(projects.flatMap(project => project.environments));
-  //   // NOTE: mostly taken from environmentSelector.tsx
-  //   const unSortedEnvs = new Set(
-  //     projects.flatMap(project => {
-  //       /**
-  //        * Include environments from:
-  //        * all projects if the user is a superuser
-  //        * the requested projects
-  //        * all member projects if 'my projects' (empty list) is selected.
-  //        * all projects if -1 is the only selected project.
-  //        */
-  //       if (
-  //         (selectedProjects.length === 1 &&
-  //           selectedProjects[0] === ALL_ACCESS_PROJECTS &&
-  //           project.hasAccess) ||
-  //         (selectedProjects.length === 0 && (project.isMember || user.isSuperuser)) ||
-  //         selectedProjects.includes(project.id)
-  //       ) {
-  //         return project.environments;
-  //       }
+  const getAllEnvironments = (): string[] => {
+    const selectedProjectIds = selection.projects.map(id => String(id));
+    const {user} = ConfigStore.getState();
+    const allEnvSet = new Set(projects.flatMap(project => project.environments));
+    // NOTE: mostly taken from environmentSelector.tsx
+    const unSortedEnvs = new Set(
+      projects.flatMap(project => {
+        /**
+         * Include environments from:
+         * all projects if the user is a superuser
+         * the requested projects
+         * all member projects if 'my projects' (empty list) is selected.
+         * all projects if -1 is the only selected project.
+         */
+        if (
+          (selectedProjectIds.length === 1 &&
+            selectedProjectIds[0] === String(ALL_ACCESS_PROJECTS) &&
+            project.hasAccess) ||
+          (selectedProjectIds.length === 0 && (project.isMember || user.isSuperuser)) ||
+          selectedProjectIds.includes(project.id)
+        ) {
+          return project.environments;
+        }
 
-  //       return [];
-  //     })
-  //   );
-  //   const envDiff = new Set([...allEnvSet].filter(x => !unSortedEnvs.has(x)));
+        return [];
+      })
+    );
+    const envDiff = new Set([...allEnvSet].filter(x => !unSortedEnvs.has(x)));
 
-  //   return Array.from(unSortedEnvs)
-  //     .sort()
-  //     .concat([...envDiff].sort());
-  // };
+    // bubble the selected projects envs first, then concat the rest of the envs
+    return Array.from(unSortedEnvs)
+      .sort()
+      .concat([...envDiff].sort());
+  };
 
-  // NOTE: currently no way to filter for 'None' environments
+  /**
+   * Thresholds filtered by environment selection
+   * NOTE: currently no way to filter for 'None' environments
+   */
   const filteredThresholds = selection.environments.length
-    ? thresholds.filter(
-        threshold => selection.environments.indexOf(threshold.environment.name) > -1
-      )
+    ? thresholds.filter(threshold => {
+        return threshold.environment?.name
+          ? selection.environments.indexOf(threshold.environment.name) > -1
+          : !selection.environments.length;
+      })
     : thresholds;
 
   const thresholdGroups: {[key: string]: {[key: string]: Threshold[]}} = useMemo(() => {
@@ -102,7 +115,7 @@ function ReleaseThresholdList({}: Props) {
       if (!byProj[projId]) {
         byProj[projId] = {};
       }
-      const env = threshold.environment.name;
+      const env = threshold.environment ? threshold.environment.name : '';
       if (!byProj[projId][env]) {
         byProj[projId][env] = [];
       }
@@ -123,10 +136,36 @@ function ReleaseThresholdList({}: Props) {
     return <LoadingIndicator />;
   }
 
+  const createNewThresholdGroup = () => {
+    if (selectedProjects.length === 1) {
+      setNewThresholdGroup([
+        ...newThresholdGroup,
+        {
+          id: `${NEW_GROUP_PREFIX}-${newGroupIterator}`,
+          project: selectedProjects[0],
+          environments: getAllEnvironments(),
+        },
+      ]);
+      setNewGroupIterator(newGroupIterator + 1);
+    } else {
+      tempError('Must select a single project in order to create a new threshold');
+    }
+  };
+
+  const onNewGroupFinished = id => {
+    const newGroups = newThresholdGroup.filter(group => group.id !== id);
+    setNewThresholdGroup(newGroups);
+  };
+
   return (
     <PageFiltersContainer>
       <NoProjectMessage organization={organization}>
-        <Header router={router} hasV2ReleaseUIEnabled />
+        <Header
+          router={router}
+          hasV2ReleaseUIEnabled
+          newThresholdAction={createNewThresholdGroup}
+          newThresholdDisabled={selection.projects.length !== 1}
+        />
         <Layout.Body>
           <Layout.Main fullWidth>
             <FilterRow>
@@ -140,7 +179,11 @@ function ReleaseThresholdList({}: Props) {
             </FilterRow>
             <StyledPanelTable
               isLoading={isLoading}
-              isEmpty={filteredThresholds.length === 0 && !isError}
+              isEmpty={
+                filteredThresholds.length === 0 &&
+                newThresholdGroup.length === 0 &&
+                !isError
+              }
               emptyMessage={t('No thresholds found.')}
               headers={[
                 t('Project Name'),
@@ -157,12 +200,25 @@ function ReleaseThresholdList({}: Props) {
                       key={`${projId}-${envName}`}
                       thresholds={thresholdGroup}
                       refetch={refetch}
-                      columns={5}
                       orgSlug={organization.slug}
                       setError={tempError}
                     />
                   ));
                 })}
+              {selectedProjects.length === 1 &&
+                newThresholdGroup[0] &&
+                newThresholdGroup
+                  .filter(group => group.project === selectedProjects[0])
+                  .map(group => (
+                    <ThresholdGroupRows
+                      key={group.id}
+                      newGroup={group}
+                      refetch={refetch}
+                      orgSlug={organization.slug}
+                      setError={tempError}
+                      onFormClose={onNewGroupFinished}
+                    />
+                  ))}
             </StyledPanelTable>
           </Layout.Main>
         </Layout.Body>
--- static/app/views/releases/utils/types.tsx
+++ static/app/views/releases/utils/types.tsx
@@ -1,3 +1,5 @@
+import moment from 'moment';
+
 import {Environment, Project} from 'sentry/types';
 
 export type ThresholdQuery = {
@@ -14,4 +16,24 @@ export type Threshold = {
   value: number;
   window_in_seconds: number;
   date_added?: string;
+};
+
+export type EditingThreshold = {
+  id: string;
+  project: Project;
+  threshold_type: string;
+  trigger_type: string;
+  value: number;
+  windowSuffix: moment.unitOfTime.DurationConstructor;
+  windowValue: number;
+  date_added?: string;
+  environment?: string;
+  hasError?: boolean;
+  window_in_seconds?: number;
+};
+
+export type NewThresholdGroup = {
+  environments: string[];
+  id: string;
+  project: Project;
 };
