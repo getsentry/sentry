ce4887ac3f3a066415b6431e6e0ea2e0a35452bf
Ash Anand <ash.anand@sentry.io>
2023-10-31 18:18:47 -0400

    Do not annotate Fragments

--- babel.config.ts
+++ babel.config.ts
@@ -25,7 +25,7 @@ const config: TransformOptions = {
     '@emotion/babel-plugin',
     '@babel/plugin-transform-runtime',
     '@babel/plugin-transform-class-properties',
-    '@fullstory/babel-plugin-annotate-react',
+    ['@fullstory/babel-plugin-annotate-react', {'annotate-fragments': false}],
   ],
   env: {
     production: {
