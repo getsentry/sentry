217482a751a6b6f4ee0f7f2044f346fa506e73e4
Riccardo Busetti <riccardo.busetti@sentry.io>
2023-10-31 14:23:42 +0100

    fix(on-demand): Remove error message (#59095)

--- src/sentry/snuba/metrics/extraction.py
+++ src/sentry/snuba/metrics/extraction.py
@@ -957,8 +957,10 @@ class OnDemandMetricSpec:
             # Third step is to generate the actual Relay rule that contains all rules nested. We assume that the query
             # being passed here, can be satisfied ONLY by on demand metrics.
             rule_condition = SearchQueryConverter(parsed_query.conditions).convert()
-        except Exception as e:
-            logger.error(f"Error while converting search query: {e}")
+        except Exception as exc:
+            if not parsed_query.is_empty():
+                logger.error(f"Error while converting search query '{self.query}'", exc_info=exc)
+
             return None
 
         # If we don't have to merge the aggregate, we can just return the parsed rules.
