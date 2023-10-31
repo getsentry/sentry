e299d5fbed91b8e0a1075bffad407e0e399d1699
Scott Cooper <scttcper@gmail.com>
2023-10-30 17:11:30 -0700

    fix(notifications): Wait for orgs to be ready (#58859)

--- static/app/views/settings/account/accountNotificationFineTuningController.tsx
+++ static/app/views/settings/account/accountNotificationFineTuningController.tsx
@@ -1,19 +1,27 @@
-import {RouteComponentProps} from 'react-router';
+import type {RouteComponentProps} from 'react-router';
 
-import {Organization} from 'sentry/types';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import type {Organization} from 'sentry/types';
 import withOrganizations from 'sentry/utils/withOrganizations';
 
 import AccountNotificationFineTuning from './accountNotificationFineTuning';
 import AccountNotificationFineTuningV2 from './accountNotificationFineTuningV2';
 
-type Props = RouteComponentProps<{fineTuneType: string}, {}> & {
+interface AccountNotificationFineTuningControllerProps
+  extends RouteComponentProps<{fineTuneType: string}, {}> {
   organizations: Organization[];
-};
+  organizationsLoading?: boolean;
+}
 
 export function AccountNotificationFineTuningController({
   organizations,
+  organizationsLoading,
   ...props
-}: Props) {
+}: AccountNotificationFineTuningControllerProps) {
+  if (organizationsLoading) {
+    return <LoadingIndicator />;
+  }
+
   // check if feature is enabled for any organization
   const hasFeature = organizations.some(org =>
     org.features.includes('notification-settings-v2')
