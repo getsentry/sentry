a7465a2c3247656fa23239c34c99df5ac1516de4
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-10-31 14:50:04 -0700

    feat(feedback): introduce banner with link to set up docs

---
+++ static/app/components/feedback/feedbackSetupBanner.tsx
@@ -0,0 +1,41 @@
+import {CSSProperties} from 'react';
+
+import replaysDeadRageBackground from 'sentry-images/spot/replay-dead-rage-changelog.svg';
+
+import {LinkButton} from 'sentry/components/button';
+import PageBanner from 'sentry/components/replays/pageBanner';
+import {IconBroadcast} from 'sentry/icons';
+import useDismissAlert from 'sentry/utils/useDismissAlert';
+
+interface Props {
+  style?: CSSProperties;
+}
+
+const LOCAL_STORAGE_KEY = 'feedback-set-up-alert-dismissed';
+
+export default function FeedbackSetupBanner({style}: Props) {
+  const {dismiss, isDismissed} = useDismissAlert({key: LOCAL_STORAGE_KEY});
+
+  const docsButton = (
+    <LinkButton
+      external
+      href="https://github.com/getsentry/sentry-javascript/blob/develop/packages/feedback/README.md"
+      priority="primary"
+    >
+      Set Up Now
+    </LinkButton>
+  );
+
+  return isDismissed ? null : (
+    <PageBanner
+      style={style}
+      button={docsButton}
+      description="Set up our feedback widget on your site to receive reports from your users."
+      heading="Introducing the New User Feedback"
+      icon={<IconBroadcast size="sm" />}
+      image={replaysDeadRageBackground}
+      title="User Feedback"
+      onDismiss={dismiss}
+    />
+  );
+}
