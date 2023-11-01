3a6204b9228d761f6db2eaf74fdca3c73f2ca8c7
Nar Saynorath <nar.saynorath@sentry.io>
2023-11-01 16:07:29 -0400

    fix(stat-detectors): Span analysis start accepting query
    We're going to migrate the frontend to pass along an escaped query to be
    consistent with other endpoints

--- src/sentry/api/endpoints/organization_events_root_cause_analysis.py
+++ src/sentry/api/endpoints/organization_events_root_cause_analysis.py
@@ -1,6 +1,5 @@
 from datetime import timedelta
 
-import sentry_sdk
 from rest_framework.response import Response
 from snuba_sdk import Column, Condition, Direction, Function, Op, Or, OrderBy
 
@@ -10,7 +9,6 @@ from sentry.api.base import region_silo_endpoint
 from sentry.api.bases.organization_events import OrganizationEventsEndpointBase
 from sentry.api.endpoints.organization_events_spans_performance import EventID, get_span_description
 from sentry.search.events.builder import QueryBuilder
-from sentry.search.events.constants import METRICS_MAX_LIMIT
 from sentry.search.events.types import QueryBuilderConfig
 from sentry.search.utils import parse_datetime_string
 from sentry.snuba.dataset import Dataset
@@ -20,9 +18,7 @@ from sentry.utils.snuba import raw_snql_query
 DEFAULT_LIMIT = 50
 QUERY_LIMIT = 10000 // 2
 BUFFER = timedelta(hours=6)
-BASE_REFERRER = "api.organization-events-root-cause-analysis"
-SPAN_ANALYSIS = "span"
-GEO_ANALYSIS = "geo"
+REFERRER = "api.organization-events-root-cause-analysis"
 SPAN_ANALYSIS_SCORE_THRESHOLD = 0
 RESPONSE_KEYS = [
     "span_op",
@@ -36,7 +32,7 @@ RESPONSE_KEYS = [
 ]
 
 
-def init_query_builder(params, transaction, regression_breakpoint, limit, span_score_threshold):
+def init_query_builder(params, query, regression_breakpoint, limit, span_score_threshold):
     before_minutes = int((regression_breakpoint - params["start"]).total_seconds() // 60)
     after_minutes = int((params["end"] - regression_breakpoint).total_seconds() // 60)
 
@@ -53,7 +49,7 @@ def init_query_builder(params, transaction, regression_breakpoint, limit, span_s
         params=params,
         selected_columns=selected_columns,
         equations=[],
-        query=f"event.type:transaction transaction:{transaction}",
+        query=query,
         limit=limit,
         config=QueryBuilderConfig(
             auto_aggregations=True,
@@ -141,23 +137,22 @@ def init_query_builder(params, transaction, regression_breakpoint, limit, span_s
     return builder
 
 
-def query_spans(transaction, regression_breakpoint, params, limit, span_score_threshold):
-    referrer = f"{BASE_REFERRER}-{SPAN_ANALYSIS}"
+def query_spans(query, regression_breakpoint, params, limit, span_score_threshold):
 
     snuba_results = raw_snql_query(
         init_query_builder(
-            params, transaction, regression_breakpoint, limit, span_score_threshold
+            params, query, regression_breakpoint, limit, span_score_threshold
         ).get_snql_query(),
-        referrer,
+        REFERRER,
     )
     return snuba_results.get("data")
 
 
 def fetch_span_analysis_results(
-    transaction_name, regression_breakpoint, params, project_id, limit, span_score_threshold
+    query, regression_breakpoint, params, project_id, limit, span_score_threshold
 ):
     span_data = query_spans(
-        transaction=transaction_name,
+        query=query,
         regression_breakpoint=regression_breakpoint,
         params=params,
         limit=limit,
@@ -172,68 +167,6 @@ def fetch_span_analysis_results(
         )
 
     return [{key: row[key] for key in RESPONSE_KEYS} for row in span_data]
-
-
-def fetch_geo_analysis_results(transaction_name, regression_breakpoint, params, limit):
-    def get_geo_data(period):
-        # Copy the params so we aren't modifying the base params each time
-        adjusted_params = {**params}
-
-        if period == "before":
-            adjusted_params["end"] = regression_breakpoint - BUFFER
-        else:
-            adjusted_params["start"] = regression_breakpoint + BUFFER
-
-        geo_code_durations = metrics_query(
-            ["p95(transaction.duration)", "geo.country_code", "tpm()"],
-            f"event.type:transaction transaction:{transaction_name}",
-            adjusted_params,
-            referrer=f"{BASE_REFERRER}-{GEO_ANALYSIS}",
-            limit=METRICS_MAX_LIMIT,
-            # Order by descending TPM to ensure more active countries are prioritized
-            orderby=["-tpm()"],
-        )
-
-        return geo_code_durations
-
-    # For each country code in the second half, compare it to the first half
-    geo_results = [get_geo_data("before"), get_geo_data("after")]
-
-    # Format the data for more efficient comparison
-    for index, result in enumerate(geo_results):
-        geo_results[index] = {
-            f"{data.get('geo.country_code')}": data for data in result.get("data")
-        }
-
-    before_results, after_results = geo_results
-    changed_keys = set(before_results.keys()) & set(after_results.keys())
-    new_keys = set(after_results.keys()) - set(before_results.keys())
-
-    analysis_results = []
-    for key in changed_keys | new_keys:
-        if key == "":
-            continue
-
-        duration_before = (
-            before_results[key]["p95_transaction_duration"] if before_results.get(key) else 0.0
-        )
-        duration_after = after_results[key]["p95_transaction_duration"]
-        if duration_after > duration_before:
-            duration_delta = duration_after - duration_before
-            analysis_results.append(
-                {
-                    "geo.country_code": key,
-                    "duration_before": duration_before,
-                    "duration_after": duration_after,
-                    "duration_delta": duration_delta,
-                    # Multiply duration delta by current TPM to prioritize largest changes
-                    # by most active countries
-                    "score": duration_delta * after_results[key]["tpm"],
-                }
-            )
-
-    analysis_results.sort(key=lambda x: x["score"], reverse=True)
-    return analysis_results[:limit]
 
 
 @region_silo_endpoint
@@ -251,18 +184,21 @@ class OrganizationEventsRootCauseAnalysisEndpoint(OrganizationEventsEndpointBase
             return Response(status=404)
 
         # TODO: Extract this into a custom serializer to handle validation
+        query = request.GET.get("query")
         transaction_name = request.GET.get("transaction")
         project_id = request.GET.get("project")
         regression_breakpoint = request.GET.get("breakpoint")
-        analysis_type = request.GET.get("type", SPAN_ANALYSIS)
         limit = int(request.GET.get("per_page", DEFAULT_LIMIT))
         span_score_threshold = int(
             request.GET.get("span_score_threshold", SPAN_ANALYSIS_SCORE_THRESHOLD)
         )
-        if not transaction_name or not project_id or not regression_breakpoint:
+        if not (query or transaction_name) or not project_id or not regression_breakpoint:
             # Project ID is required to ensure the events we query for are
             # the same transaction
             return Response(status=400)
+
+        if not query and transaction_name:
+            query = f'event.type:transaction transaction:"{transaction_name}"'
 
         regression_breakpoint = parse_datetime_string(regression_breakpoint)
 
@@ -271,28 +207,21 @@ class OrganizationEventsRootCauseAnalysisEndpoint(OrganizationEventsEndpointBase
         with self.handle_query_errors():
             transaction_count_query = metrics_query(
                 ["count()"],
-                f"event.type:transaction transaction:{transaction_name}",
+                query,
                 params,
-                referrer=f"{BASE_REFERRER}-{analysis_type}",
+                referrer=REFERRER,
             )
 
         if transaction_count_query["data"][0]["count"] == 0:
             return Response(status=400, data="Transaction not found")
 
-        sentry_sdk.set_tag("analysis_type", analysis_type)
-        results = []
-        if analysis_type == SPAN_ANALYSIS:
-            results = fetch_span_analysis_results(
-                transaction_name,
-                regression_breakpoint,
-                params,
-                project_id,
-                limit,
-                span_score_threshold,
-            )
-        elif analysis_type == GEO_ANALYSIS:
-            results = fetch_geo_analysis_results(
-                transaction_name, regression_breakpoint, params, limit
-            )
+        results = fetch_span_analysis_results(
+            query,
+            regression_breakpoint,
+            params,
+            project_id,
+            limit,
+            span_score_threshold,
+        )
 
         return Response(results, status=200)
