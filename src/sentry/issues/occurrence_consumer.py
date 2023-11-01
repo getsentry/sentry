20856a1e198db93ad822fd1cad63c965b3da59dc
Snigdha Sharma <snigdha.sharma@sentry.io>
2023-11-01 10:35:23 -0700

    fix(issue-platform): Add default value for payload type (#58941)
    The default value was dropped in a previous PR - this ensures existing
    API calls work as is

--- src/sentry/issues/occurrence_consumer.py
+++ src/sentry/issues/occurrence_consumer.py
@@ -261,11 +261,19 @@ def _process_message(
         sampled=True,
     ) as txn:
         try:
-            if message.get("payload_type") == PayloadType.OCCURRENCE.value:
-                return process_occurrence_message(message, txn)
-            elif message.get("payload_type") == PayloadType.STATUS_CHANGE.value:
+            # Assume messaged without a payload type are of type OCCURRENCE
+            payload_type = message.get("payload_type", PayloadType.OCCURRENCE.value)
+            if payload_type == PayloadType.STATUS_CHANGE.value:
                 group = process_status_change_message(message, txn)
                 return None, GroupInfo(group=group, is_new=False, is_regression=False)
+            elif payload_type == PayloadType.OCCURRENCE.value:
+                return process_occurrence_message(message, txn)
+            else:
+                metrics.incr(
+                    "occurrence_consumer._process_message.dropped_invalid_payload_type",
+                    sample_rate=1.0,
+                    tags={"payload_type": payload_type},
+                )
         except (ValueError, KeyError) as e:
             txn.set_tag("result", "error")
             raise InvalidEventPayloadError(e)
