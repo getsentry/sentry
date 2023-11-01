d447d0c240eb4e0bbf386ac39fc3e71c92413ff2
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-11-01 13:00:22 -0400

    fix(browser-starfish): filtering resources modules inconsistent (#59190)
    1. Remove '*' from the domains filter, this would just search of all
    domains.
    2. Add a 'no domain' filter option, useful when you have relative
    resources ex. '/main.js'
    3. Fix filtering on pageSelector showing pages that have non supported
    resources types.
    4. Add some shared filtering functionality.

--- static/app/views/performance/browser/resources/utils/useResourceDomansQuery.ts
+++ static/app/views/performance/browser/resources/utils/useResourceDomansQuery.ts
@@ -7,7 +7,10 @@ import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import {useResourceModuleFilters} from 'sentry/views/performance/browser/resources/utils/useResourceFilters';
-import {DEFAULT_RESOURCE_FILTERS} from 'sentry/views/performance/browser/resources/utils/useResourcesQuery';
+import {
+  DEFAULT_RESOURCE_FILTERS,
+  getResourceTypeFilter,
+} from 'sentry/views/performance/browser/resources/utils/useResourcesQuery';
 import {SpanMetricsField} from 'sentry/views/starfish/types';
 
 const {SPAN_DOMAIN, SPAN_OP, TRANSACTION} = SpanMetricsField;
@@ -15,7 +18,7 @@ const {SPAN_DOMAIN, SPAN_OP, TRANSACTION} = SpanMetricsField;
 /**
  * Gets a list of pages that have a resource.
  */
-export const useResourceDomainsQuery = () => {
+export const useResourceDomainsQuery = (defaultResourceTypes?: string[]) => {
   const location = useLocation();
   const pageFilters = usePageFilters();
   const {slug: orgSlug} = useOrganization();
@@ -25,8 +28,8 @@ export const useResourceDomainsQuery = () => {
 
   const queryConditions = [
     ...DEFAULT_RESOURCE_FILTERS,
-    `${SPAN_OP}:${resourceFilters[SPAN_OP] || '[resource.script,resource.css]'}`,
     `has:${SPAN_DOMAIN}`,
+    ...getResourceTypeFilter(resourceFilters[SPAN_OP], defaultResourceTypes),
     ...(resourceFilters[TRANSACTION]
       ? [`transaction:${resourceFilters[TRANSACTION]}`]
       : []),
@@ -59,6 +62,7 @@ export const useResourceDomainsQuery = () => {
         }
         return domains[0].toString();
       })
+      .filter(domain => domain !== '*')
       .sort() || [];
 
   return {...result, data};
