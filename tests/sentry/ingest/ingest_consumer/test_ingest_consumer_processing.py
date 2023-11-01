33ed7d6ab179f1dd3e2a102e0d22badd7f140181
Joshua Ferge <josh.ferge@sentry.io>
2023-10-31 18:21:11 -0700

    add flag to test

--- tests/sentry/ingest/ingest_consumer/test_ingest_consumer_processing.py
+++ tests/sentry/ingest/ingest_consumer/test_ingest_consumer_processing.py
@@ -150,11 +150,10 @@ def test_transactions_spawn_save_event_transaction(
 
 @django_db_all
 def test_feedbacks_spawn_save_event_feedback(
-    default_project,
-    task_runner,
-    preprocess_event,
-    save_event_feedback,
+    default_project, task_runner, preprocess_event, save_event_feedback, monkeypatch
 ):
+    monkeypatch.setattr("sentry.features.has", lambda *a, **kw: True)
+
     project_id = default_project.id
     now = datetime.datetime.now()
     event: dict[str, Any] = {
