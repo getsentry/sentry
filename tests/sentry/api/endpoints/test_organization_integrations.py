551b23656cc1dc0b26a62a505089e6c1378c4ab1
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-11-02 10:39:26 -0400

    fix(hybrid-cloud): Prevent cross silo accesses on serializing jira configs (#59221)
    Resolves
    [HC-TEST-CONTROL-E2](https://sentry-st.sentry.io/issues/4593594494/?project=4505211735834624&query=is%3Aunresolved&referrer=issue-stream&statsPeriod=14d&stream_index=1)
    and
    [HC-TEST-CONTROL-E3](https://sentry-st.sentry.io/issues/4593594496/?project=4505211735834624&query=is%3Aunresolved&referrer=issue-stream&statsPeriod=14d&stream_index=2)
    
    Adds a few tests for serializing integrations as well.

--- tests/sentry/api/endpoints/test_organization_integrations.py
+++ tests/sentry/api/endpoints/test_organization_integrations.py
@@ -1,8 +1,8 @@
 from sentry.testutils.cases import APITestCase
-from sentry.testutils.silo import region_silo_test
+from sentry.testutils.silo import control_silo_test
 
 
-@region_silo_test(stable=True)
+@control_silo_test(stable=True)
 class OrganizationIntegrationsListTest(APITestCase):
     endpoint = "sentry-api-0-organization-integrations"
 
@@ -27,3 +27,27 @@ class OrganizationIntegrationsListTest(APITestCase):
         response = self.get_success_response(self.organization.slug, qs_params={"includeConfig": 0})
 
         assert "configOrganization" not in response.data[0]
+
+    def test_feature_filters(self):
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"features": "issue_basic"}
+        )
+        assert response.data[0]["id"] == str(self.integration.id)
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"features": "codeowners"}
+        )
+        assert response.data == []
+
+    def test_provider_key(self):
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"providerKey": "example"}
+        )
+        assert response.data[0]["id"] == str(self.integration.id)
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"provider_key": "example"}
+        )
+        assert response.data[0]["id"] == str(self.integration.id)
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"provider_key": "vercel"}
+        )
+        assert response.data == []
