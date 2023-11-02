a12cd946b8dff6216f0cf367b320888345f1ab38
Philipp Hofmann <philipp.hofmann@sentry.io>
2023-11-02 15:25:48 +0100

    code review

--- src/sentry/utils/sdk_crashes/build_sdk_crash_detection_configs.py
+++ src/sentry/utils/sdk_crashes/build_sdk_crash_detection_configs.py
@@ -7,22 +7,23 @@ from sentry.utils.sdk_crashes.sdk_crash_detection_config import SDKCrashDetectio
 
 
 def build_sdk_crash_detection_configs() -> Sequence[SDKCrashDetectionConfig]:
-    def build_config(sdk_name: SdkName) -> Optional[SDKCrashDetectionConfig]:
-        options_prefix = f"issues.sdk_crash_detection.{sdk_name.value}"
-
-        project_id = options.get(f"{options_prefix}.project_id")
-        if not project_id or project_id == 0:
-            sentry_sdk.capture_message(f"{sdk_name.value} project_id is not set.")
-            return None
-
-        sample_rate = options.get(f"{options_prefix}.sample_rate")
-        if not sample_rate or sample_rate == 0:
-            return None
-
-        return SDKCrashDetectionConfig(
-            sdk_name=sdk_name, project_id=project_id, sample_rate=sample_rate
-        )
-
-    configs = [build_config(SdkName.Cocoa), build_config(SdkName.ReactNative)]
+    configs = [_build_config(SdkName.Cocoa), _build_config(SdkName.ReactNative)]
 
     return [config for config in configs if config is not None]
+
+
+def _build_config(sdk_name: SdkName) -> Optional[SDKCrashDetectionConfig]:
+    options_prefix = f"issues.sdk_crash_detection.{sdk_name.value}"
+
+    project_id = options.get(f"{options_prefix}.project_id")
+    if not project_id:
+        sentry_sdk.capture_message(f"{sdk_name.value} project_id is not set.")
+        return None
+
+    sample_rate = options.get(f"{options_prefix}.sample_rate")
+    if not sample_rate:
+        return None
+
+    return SDKCrashDetectionConfig(
+        sdk_name=sdk_name, project_id=project_id, sample_rate=sample_rate
+    )
