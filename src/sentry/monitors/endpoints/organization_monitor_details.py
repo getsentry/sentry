41571d6a6430b07893d7b0cf36b43dea8e74ba0f
Richard Ortenberg <richard.ortenberg@sentry.io>
2023-11-02 10:45:16 -0700

    meta(crons): Update ownership + publishing for crons (#59253)
    Update ownership rules + published status for crons endpoints

--- src/sentry/monitors/endpoints/organization_monitor_details.py
+++ src/sentry/monitors/endpoints/organization_monitor_details.py
@@ -7,6 +7,7 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from sentry import audit_log
+from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.exceptions import ParameterValidationError
@@ -38,6 +39,7 @@ class OrganizationMonitorDetailsEndpoint(MonitorEndpoint):
         "GET": ApiPublishStatus.PUBLIC,
         "PUT": ApiPublishStatus.PUBLIC,
     }
+    owner = ApiOwner.CRONS
 
     @extend_schema(
         operation_id="Retrieve a Monitor",
--- src/sentry/monitors/endpoints/organization_monitor_index_stats.py
+++ src/sentry/monitors/endpoints/organization_monitor_index_stats.py
@@ -9,6 +9,7 @@ from django.db.models.functions import Extract
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import StatsMixin, region_silo_endpoint
 from sentry.api.bases.organization import OrganizationEndpoint
@@ -31,8 +32,9 @@ def normalize_to_epoch(timestamp: datetime, seconds: int):
 @region_silo_endpoint
 class OrganizationMonitorIndexStatsEndpoint(OrganizationEndpoint, StatsMixin):
     publish_status = {
-        "GET": ApiPublishStatus.UNKNOWN,
+        "GET": ApiPublishStatus.PRIVATE,
     }
+    owner = ApiOwner.CRONS
 
     # TODO(epurkhiser): probably convert to snuba
     def get(self, request: Request, organization) -> Response:
