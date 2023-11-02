9c41012e4f13fb9e1c47a019b45044417e0ba19b
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-11-02 16:39:36 -0400

    ref: upgrade django-stubs to our fork (#59311)
    <!-- Describe your PR here. -->

--- src/sentry/db/models/base.py
+++ src/sentry/db/models/base.py
@@ -99,17 +99,17 @@ class BaseModel(models.Model):
         # In production, you should guard `model.field` with an
         # `if model.is_field_cached`.
         name = self._get_relational_field(field_name).get_cache_name()
-        return self._state.fields_cache.get(name, None)  # type: ignore[attr-defined]  # issue: typeddjango/django-stubs#1743
+        return self._state.fields_cache.get(name, None)
 
     def delete_cached_field_value(self, field_name: str) -> None:
         name = self._get_relational_field(field_name).get_cache_name()
-        if name in self._state.fields_cache:  # type: ignore[operator]  # issue: typeddjango/django-stubs#1743
-            del self._state.fields_cache[name]  # type: ignore[attr-defined]  # issue: typeddjango/django-stubs#1743
+        if name in self._state.fields_cache:
+            del self._state.fields_cache[name]
 
     def is_field_cached(self, field_name: str) -> bool:
         # Ask if a relational field has a cached value.
         name = self._get_relational_field(field_name).get_cache_name()
-        return name in self._state.fields_cache  # type: ignore[operator]  # issue: typeddjango/django-stubs#1743
+        return name in self._state.fields_cache
 
     def get_relocation_scope(self) -> RelocationScope:
         """
