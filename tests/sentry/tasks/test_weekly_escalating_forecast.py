24aeaa1672b79dfefa640facad178b60f031ceaa
17hogeju <17hogeju@gmail.com>
2023-11-02 16:10:28 -0700

    Add some tests

--- tests/sentry/tasks/test_weekly_escalating_forecast.py
+++ tests/sentry/tasks/test_weekly_escalating_forecast.py
@@ -3,19 +3,27 @@ from typing import List
 from unittest.mock import MagicMock, patch
 
 from sentry.issues.escalating_group_forecast import ONE_EVENT_FORECAST, EscalatingGroupForecast
+from sentry.issues.grouptype import (
+    ErrorGroupType,
+    PerformanceDurationRegressionGroupType,
+    PerformanceSlowDBQueryGroupType,
+)
 from sentry.models.group import Group, GroupStatus
 from sentry.tasks.weekly_escalating_forecast import run_escalating_forecast
 from sentry.testutils.cases import APITestCase, SnubaTestCase
+from sentry.testutils.helpers.features import with_feature
 from sentry.types.group import GroupSubStatus
 from tests.sentry.issues.test_utils import get_mock_groups_past_counts_response
 
 
 class TestWeeklyEscalatingForecast(APITestCase, SnubaTestCase):
-    def create_archived_until_escalating_groups(self, num_groups: int) -> List[Group]:
+    def create_archived_until_escalating_groups(
+        self, num_groups: int, group_type: int
+    ) -> List[Group]:
         group_list = []
         project_1 = self.project
         for i in range(num_groups):
-            group = self.create_group(project=project_1)
+            group = self.create_group(project=project_1, type=group_type)
             group.status = GroupStatus.IGNORED
             group.substatus = GroupSubStatus.UNTIL_ESCALATING
             group.save()
@@ -24,17 +32,20 @@ class TestWeeklyEscalatingForecast(APITestCase, SnubaTestCase):
 
     @patch("sentry.issues.forecasts.generate_and_save_missing_forecasts.delay")
     @patch("sentry.issues.escalating.query_groups_past_counts")
-    def test_empty_escalating_forecast(
+    def empty_escalating_forecast(
         self,
         mock_query_groups_past_counts: MagicMock,
         mock_generate_and_save_missing_forecasts: MagicMock,
+        group_type: int = ErrorGroupType.type_id,
     ) -> None:
         """
         Test that when fetch is called and the issue has no forecast, the forecast for one
         event/hr is returned, and the forecast is regenerated.
         """
         with self.tasks():
-            group_list = self.create_archived_until_escalating_groups(num_groups=1)
+            group_list = self.create_archived_until_escalating_groups(
+                num_groups=1, group_type=group_type
+            )
 
             mock_query_groups_past_counts.return_value = {}
 
@@ -44,13 +55,29 @@ class TestWeeklyEscalatingForecast(APITestCase, SnubaTestCase):
             assert fetched_forecast and fetched_forecast.forecast == ONE_EVENT_FORECAST
         assert mock_generate_and_save_missing_forecasts.call_count == 1
 
+    def test_empty_escalating_forecasts(self):
+        self.empty_escalating_forecast()
+        self.empty_escalating_forecast(group_type=PerformanceSlowDBQueryGroupType.type_id)
+        self.empty_escalating_forecast(group_type=PerformanceDurationRegressionGroupType.type_id)
+
+    @with_feature("organizations:issue-platform-api-crons-sd")
+    def test_empty_escalating_forecasts_ff_on(self):
+        self.empty_escalating_forecast()
+        self.empty_escalating_forecast(group_type=PerformanceSlowDBQueryGroupType.type_id)
+        self.empty_escalating_forecast(group_type=PerformanceDurationRegressionGroupType.type_id)
+
     @patch("sentry.analytics.record")
     @patch("sentry.issues.forecasts.query_groups_past_counts")
-    def test_single_group_escalating_forecast(
-        self, mock_query_groups_past_counts: MagicMock, record_mock: MagicMock
+    def single_group_escalating_forecast(
+        self,
+        mock_query_groups_past_counts: MagicMock,
+        record_mock: MagicMock,
+        group_type: int = ErrorGroupType.type_id,
     ) -> None:
         with self.tasks():
-            group_list = self.create_archived_until_escalating_groups(num_groups=1)
+            group_list = self.create_archived_until_escalating_groups(
+                num_groups=1, group_type=group_type
+            )
 
             mock_query_groups_past_counts.return_value = get_mock_groups_past_counts_response(
                 num_days=7, num_hours=1, groups=group_list
@@ -71,13 +98,34 @@ class TestWeeklyEscalatingForecast(APITestCase, SnubaTestCase):
             assert fetched_forecast.date_added < approximate_date_added
             record_mock.assert_called_with("issue_forecasts.saved", num_groups=1)
 
+    def test_single_group_escalating_forecasts(self):
+        self.single_group_escalating_forecast()
+        self.single_group_escalating_forecast(group_type=PerformanceSlowDBQueryGroupType.type_id)
+        self.single_group_escalating_forecast(
+            group_type=PerformanceDurationRegressionGroupType.type_id
+        )
+
+    @with_feature("organizations:issue-platform-api-crons-sd")
+    def test_single_group_escalating_forecasts_ff_on(self):
+        self.single_group_escalating_forecast()
+        self.single_group_escalating_forecast(group_type=PerformanceSlowDBQueryGroupType.type_id)
+        # Not ready yet
+        # self.single_group_escalating_forecast(
+        #     group_type=PerformanceDurationRegressionGroupType.type_id
+        # )
+
     @patch("sentry.analytics.record")
     @patch("sentry.issues.forecasts.query_groups_past_counts")
-    def test_multiple_groups_escalating_forecast(
-        self, mock_query_groups_past_counts: MagicMock, record_mock: MagicMock
+    def multiple_groups_escalating_forecast(
+        self,
+        mock_query_groups_past_counts: MagicMock,
+        record_mock: MagicMock,
+        group_type: int = ErrorGroupType.type_id,
     ) -> None:
         with self.tasks():
-            group_list = self.create_archived_until_escalating_groups(num_groups=3)
+            group_list = self.create_archived_until_escalating_groups(
+                num_groups=3, group_type=group_type
+            )
 
             mock_query_groups_past_counts.return_value = get_mock_groups_past_counts_response(
                 num_days=7, num_hours=23, groups=group_list
@@ -99,13 +147,34 @@ class TestWeeklyEscalatingForecast(APITestCase, SnubaTestCase):
                 assert fetched_forecast.date_added < approximate_date_added
                 record_mock.assert_called_with("issue_forecasts.saved", num_groups=3)
 
+    def test_multiple_groups_escalating_forecasts(self):
+        self.multiple_groups_escalating_forecast()
+        self.multiple_groups_escalating_forecast(group_type=PerformanceSlowDBQueryGroupType.type_id)
+        self.multiple_groups_escalating_forecast(
+            group_type=PerformanceDurationRegressionGroupType.type_id
+        )
+
+    @with_feature("organizations:issue-platform-api-crons-sd")
+    def test_multiple_groups_escalating_forecasts_ff_on(self):
+        self.multiple_groups_escalating_forecast()
+        self.multiple_groups_escalating_forecast(group_type=PerformanceSlowDBQueryGroupType.type_id)
+        # Not ready yet
+        # self.multiple_groups_escalating_forecast(
+        #     group_type=PerformanceDurationRegressionGroupType.type_id
+        # )
+
     @patch("sentry.analytics.record")
     @patch("sentry.issues.forecasts.query_groups_past_counts")
-    def test_update_group_escalating_forecast(
-        self, mock_query_groups_past_counts: MagicMock, record_mock: MagicMock
+    def update_group_escalating_forecast(
+        self,
+        mock_query_groups_past_counts: MagicMock,
+        record_mock: MagicMock,
+        group_type: int = ErrorGroupType.type_id,
     ) -> None:
         with self.tasks():
-            group_list = self.create_archived_until_escalating_groups(num_groups=1)
+            group_list = self.create_archived_until_escalating_groups(
+                num_groups=1, group_type=group_type
+            )
 
             mock_query_groups_past_counts.return_value = get_mock_groups_past_counts_response(
                 num_days=7, num_hours=2, groups=group_list
@@ -125,3 +194,19 @@ class TestWeeklyEscalatingForecast(APITestCase, SnubaTestCase):
             assert second_fetched_forecast is not None
             assert first_fetched_forecast.date_added < second_fetched_forecast.date_added
             record_mock.assert_called_with("issue_forecasts.saved", num_groups=1)
+
+    def test_update_group_escalating_forecasts(self):
+        self.update_group_escalating_forecast()
+        self.update_group_escalating_forecast(group_type=PerformanceSlowDBQueryGroupType.type_id)
+        self.update_group_escalating_forecast(
+            group_type=PerformanceDurationRegressionGroupType.type_id
+        )
+
+    @with_feature("organizations:issue-platform-api-crons-sd")
+    def testupdate_group_escalating_forecasts_ff_on(self):
+        self.update_group_escalating_forecast()
+        self.update_group_escalating_forecast(group_type=PerformanceSlowDBQueryGroupType.type_id)
+        # Not ready yet
+        # self.update_group_escalating_forecast(
+        #     group_type=PerformanceDurationRegressionGroupType.type_id
+        # )
