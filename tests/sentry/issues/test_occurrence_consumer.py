b69dd5ed557371c4d96152fd77cf7bf058afbdb8
Bartek Ogryczak <bartek.ogryczak@sentry.io>
2023-11-02 14:41:52 -0700

    fix(issue-platform): allowing extra fields in StackTrace/Frames (#59313)
    Performance occurrences stack frames are symbolicated add extra fields
    not expected in SDK.
    This change is making validation less strict, allowing these extra
    fields.

--- tests/sentry/issues/test_occurrence_consumer.py
+++ tests/sentry/issues/test_occurrence_consumer.py
@@ -260,6 +260,11 @@ class ParseEventPayloadTest(IssueOccurrenceTestBase):
         message["event"]["timestamp"] = 0000
         self.run_test(message)
 
+    def test_frame_additional_fields_valid_with_new_schema(self) -> None:
+        message = deepcopy(get_test_message(self.project.id))
+        message["event"]["stacktrace"]["frames"][0]["data"] = {"foo": "bar"}
+        self.run_test(message)
+
     def test_tags_not_required_with_new_schema(self) -> None:
         # per https://develop.sentry.dev/sdk/event-payloads/ tags are optional
         message = deepcopy(get_test_message(self.project.id))
