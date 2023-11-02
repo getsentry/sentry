d60671569e1e16371ecf96357ba806d0a59b7772
Evan Hicks <evanh@users.noreply.github.com>
2023-11-02 15:45:45 -0400

    ref(metrics): Add support for gauges to new metrics layer (#59108)
    Very small change. Note that without a gauge MRI this can't be properly
    tested.

--- src/sentry/sentry_metrics/consumers/indexer/batch.py
+++ src/sentry/sentry_metrics/consumers/indexer/batch.py
@@ -420,6 +420,9 @@ class IndexerBatch:
             sentry_received_timestamp = message.value.timestamp.timestamp()
 
             if self.__should_index_tag_values:
+                # Metrics don't support gauges (which use dicts), so assert value type
+                value = old_payload_value["value"]
+                assert isinstance(value, (int, float, list))
                 new_payload_v1: Metric = {
                     "tags": new_tags,
                     # XXX: relay actually sends this value unconditionally
@@ -431,7 +434,7 @@ class IndexerBatch:
                     "timestamp": old_payload_value["timestamp"],
                     "project_id": old_payload_value["project_id"],
                     "type": old_payload_value["type"],
-                    "value": old_payload_value["value"],
+                    "value": value,
                     "sentry_received_timestamp": sentry_received_timestamp,
                 }
 
