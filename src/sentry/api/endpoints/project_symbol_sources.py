29bc1073f8b13461c6ad7c166c5e6c3a57cdc4da
Sebastian Zivota <loewenheim@mailbox.org>
2023-11-02 10:47:54 +0100

    Validate S3 region

--- src/sentry/api/endpoints/project_symbol_sources.py
+++ src/sentry/api/endpoints/project_symbol_sources.py
@@ -143,7 +143,30 @@ class SourceSerializer(serializers.Serializer):
         required=False,
         help_text="The GCS or S3 bucket where the source resides. Required for GCS and S3 sourcse, invalid for HTTP and AppStoreConnect sources.",
     )
-    region = serializers.CharField(
+    region = serializers.ChoiceField(
+        choices=[
+            ("us-east-2", "US East (Ohio)"),
+            ("us-east-1", "US East (N. Virginia)"),
+            ("us-west-1", "US West (N. California)"),
+            ("us-west-2", "US West (Oregon)"),
+            ("ap-east-1", "Asia Pacific (Hong Kong)"),
+            ("ap-south-1", "Asia Pacific (Mumbai)"),
+            ("ap-northeast-2", "Asia Pacific (Seoul)"),
+            ("ap-southeast-1", "Asia Pacific (Singapore)"),
+            ("ap-southeast-2", "Asia Pacific (Sydney)"),
+            ("ap-northeast-1", "Asia Pacific (Tokyo)"),
+            ("ca-central-1", "Canada (Central)"),
+            ("cn-north-1", "China (Beijing)"),
+            ("cn-northwest-1", "China (Ningxia)"),
+            ("eu-central-1", "EU (Frankfurt)"),
+            ("eu-west-1", "EU (Ireland)"),
+            ("eu-west-2", "EU (London)"),
+            ("eu-west-3", "EU (Paris)"),
+            ("eu-north-1", "EU (Stockholm)"),
+            ("sa-east-1", "South America (São Paulo)"),
+            ("us-gov-east-1", "AWS GovCloud (US-East)"),
+            ("us-gov-west-1", "AWS GovCloud (US)"),
+        ],
         required=False,
         help_text="The source's [S3 region](https://docs.aws.amazon.com/general/latest/gr/s3.html). Required for S3 sources, invalid for all others.",
     )
