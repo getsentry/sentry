d4386f29239f7f89bd009b00466578a1d2e06498
Colleen O'Rourke <colleen@sentry.io>
2023-11-03 09:49:00 -0700

    ref(sus commits): Add caching for get_blame_for_files (#58740)
    Add caching for `get_blame_for_files` to help reduce the chance of
    hitting the rate limit.
    
    Closes #57437

--- src/sentry/integrations/gitlab/client.py
+++ src/sentry/integrations/gitlab/client.py
@@ -16,6 +16,7 @@ from sentry.services.hybrid_cloud.util import control_silo_function
 from sentry.shared_integrations.client.proxy import IntegrationProxyClient
 from sentry.shared_integrations.exceptions import ApiError, ApiUnauthorized
 from sentry.silo.base import SiloMode
+from sentry.utils import metrics
 from sentry.utils.http import absolute_uri
 
 if TYPE_CHECKING:
@@ -334,6 +335,7 @@ class GitLabProxyApiClient(IntegrationProxyClient):
         return contents or []
 
     def get_blame_for_files(self, files: Sequence[SourceLineInfo]) -> list[FileBlameInfo]:
+        metrics.incr("sentry.integrations.gitlab.get_blame_for_files")
         return fetch_file_blames(
             self, files, extra={"provider": "gitlab", "org_integration_id": self.org_integration_id}
         )
