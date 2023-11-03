57736b97eb100c760ef7e943f307bed4e1dc97b8
Josh Ferge <josh.ferge@sentry.io>
2023-11-03 13:05:42 -0700

    feat(ddm): add feedback button (#59369)
    - Adds the feedback widget to metrics page
    - Rename "Report a bug" to "Give Feedback", as for these new products we
    want any general feedback, not just bugs

--- static/app/components/feedback/widget/feedbackWidget.tsx
+++ static/app/components/feedback/widget/feedbackWidget.tsx
@@ -20,6 +20,9 @@ export default function FeedbackWidget() {
     const feedback = client?.getIntegration(Feedback);
     const widget = feedback?.createWidget({
       colorScheme: widgetTheme,
+      buttonLabel: 'Give Feedback',
+      submitButtonLabel: 'Send Feedback',
+      messagePlaceholder: 'What did you expect?',
     });
     return () => {
       feedback?.removeWidget(widget);
