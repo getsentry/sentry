0f3e721905f1ba6d4dd1fa577e0e385d74c99c7b
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2023-11-03 09:13:28 -0700

    feat(notifications): Remove team notifications off by default (#58972)
    This PR removes this unnecessary team notification logic (behind a flag)

--- tests/sentry/integrations/slack/test_link_team.py
+++ tests/sentry/integrations/slack/test_link_team.py
@@ -242,6 +242,35 @@ class SlackIntegrationLinkTeamTest(SlackIntegrationLinkTeamTestBase):
             in get_response_text(data)
         )
 
+    @responses.activate
+    @with_feature("organizations:team-workflow-notifications")
+    def test_link_team_v2(self):
+        """Test that we successfully link a team to a Slack channel"""
+        response = self.get_success_response()
+        self.assertTemplateUsed(response, "sentry/integrations/slack/link-team.html")
+
+        response = self.get_success_response(data={"team": self.team.id})
+        self.assertTemplateUsed(response, "sentry/integrations/slack/post-linked-team.html")
+
+        external_actors = self.get_linked_teams()
+        assert len(external_actors) == 1
+        assert external_actors[0].team_id == self.team.id
+
+        assert len(responses.calls) >= 1
+        data = json.loads(str(responses.calls[0].request.body.decode("utf-8")))
+        assert (
+            f"The {self.team.slug} team will now receive issue alert and workflow notifications in the {external_actors[0].external_name} channel."
+            in get_response_text(data)
+        )
+
+        # Test that we didn't make an NotificationSetting object
+        # Instead we will use the default in notificationcontroller.py
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            team_settings = NotificationSetting.objects.filter(
+                scope_type=NotificationScopeType.TEAM.value, team_id=self.team.id
+            )
+            assert len(team_settings) == 0
+
 
 @region_silo_test(stable=True)
 class SlackIntegrationUnlinkTeamTest(SlackIntegrationLinkTeamTestBase):
--- tests/sentry/notifications/test_notificationcontroller.py
+++ tests/sentry/notifications/test_notificationcontroller.py
@@ -651,7 +651,29 @@ class NotificationControllerTest(TestCase):
             type=NotificationSettingEnum.WORKFLOW,
         )
         assert controller.get_participants() == {
-            rpc_user: {ExternalProviders.SLACK: NotificationSettingsOptionEnum.SUBSCRIBE_ONLY}
+            rpc_user: {
+                ExternalProviders.SLACK: NotificationSettingsOptionEnum.SUBSCRIBE_ONLY,
+                ExternalProviders.EMAIL: NotificationSettingsOptionEnum.SUBSCRIBE_ONLY,
+            }
+        }
+
+    @with_feature("organizations:notification-settings-v2")
+    @with_feature("organizations:team-workflow-notifications")
+    def test_get_team_issue_alert_participants(self):
+        rpc_user = RpcActor.from_object(self.team)
+        with assume_test_silo_mode(SiloMode.REGION):
+            link_team(self.team, self.integration, "#team-channel", "team_channel_id")
+        controller = NotificationController(
+            recipients=[self.team],
+            project_ids=[self.project.id],
+            organization_id=self.organization.id,
+            type=NotificationSettingEnum.ISSUE_ALERTS,
+        )
+        assert controller.get_participants() == {
+            rpc_user: {
+                ExternalProviders.SLACK: NotificationSettingsOptionEnum.ALWAYS,
+                ExternalProviders.EMAIL: NotificationSettingsOptionEnum.ALWAYS,
+            }
         }
 
     def test_get_notification_value_for_recipient_and_type(self):
