b5575c087cf345f0be9109d0b6baeb3927b276a6
ArthurKnaus <arthur.knaus@sentry.io>
2023-11-06 12:37:49 +0100

    ref(onboarding): Convert platform capacitor to new structure (#59415)
    - relates to https://github.com/getsentry/sentry/issues/56549

--- static/app/gettingStartedDocs/capacitor/capacitor.spec.tsx
+++ static/app/gettingStartedDocs/capacitor/capacitor.spec.tsx
@@ -1,18 +1,31 @@
-import {render, screen} from 'sentry-test/reactTestingLibrary';
+import {renderWithOnboardingLayout} from 'sentry-test/onboarding/renderWithOnboardingLayout';
+import {screen} from 'sentry-test/reactTestingLibrary';
 
-import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
+import docs, {CapacitorVersion} from './capacitor';
 
-import {GettingStartedWithCapacitor, steps} from './capacitor';
+describe('capacitor onboarding docs', function () {
+  it('renders docs correctly', function () {
+    renderWithOnboardingLayout(docs);
 
-describe('GettingStartedWithCapacitor', function () {
-  it('renders doc correctly', function () {
-    render(<GettingStartedWithCapacitor dsn="test-dsn" projectSlug="test-project" />);
+    // Renders main headings
+    expect(screen.getByRole('heading', {name: 'Install'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Configure SDK'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Verify'})).toBeInTheDocument();
+  });
 
-    // Steps
-    for (const step of steps()) {
-      expect(
-        screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
-      ).toBeInTheDocument();
-    }
+  it('renders capacitor 2 docs correctly', function () {
+    renderWithOnboardingLayout(docs, {
+      selectedOptions: {
+        capacitorVersion: CapacitorVersion.V2,
+      },
+    });
+
+    // Renders main headings
+    expect(screen.getByRole('heading', {name: 'Install'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Configure SDK'})).toBeInTheDocument();
+    expect(
+      screen.getByRole('heading', {name: 'Capacitor 2 - Android'})
+    ).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Verify'})).toBeInTheDocument();
   });
 });
--- static/app/gettingStartedDocs/capacitor/capacitor.tsx
+++ static/app/gettingStartedDocs/capacitor/capacitor.tsx
@@ -1,65 +1,36 @@
-import {Fragment} from 'react';
-
-import {Layout, LayoutProps} from 'sentry/components/onboarding/gettingStartedDoc/layout';
-import {ModuleProps} from 'sentry/components/onboarding/gettingStartedDoc/sdkDocumentation';
 import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/step';
+import type {
+  Docs,
+  DocsParams,
+  OnboardingConfig,
+} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {t, tct} from 'sentry/locale';
 
-// Configuration Start
-export const steps = ({
-  dsn,
-}: Partial<Pick<ModuleProps, 'dsn'>> = {}): LayoutProps['steps'] => [
-  {
-    type: StepType.INSTALL,
-    description: t(
-      'Install the Sentry Capacitor SDK alongside the sibling Sentry Angular SDK:'
-    ),
-    configurations: [
-      {
-        language: 'bash',
-        code: `
-# npm
-npm install --save @sentry/capacitor @sentry/angular-ivy
+export enum CapacitorVersion {
+  V2 = 'v2',
+  V3 = 'v3',
+}
 
-# yarn
-yarn add @sentry/capacitor @sentry/angular @sentry/tracing --exact
-        `,
+const platformOptions = {
+  capacitorVersion: {
+    label: t('Capacitor Version'),
+    items: [
+      {
+        label: t('Capacitor 3'),
+        value: CapacitorVersion.V3,
       },
       {
-        language: 'bash',
-        description: t(
-          "Or install the standalone Sentry Capacitor SDK if you don't use Ionic/Angular:"
-        ),
-        code: `
-# npm
-npm install --save @sentry/capacitor @sentry/tracing
-
-# yarn
-yarn add @sentry/capacitor
-        `,
+        label: t('Capacitor 2'),
+        value: CapacitorVersion.V2,
       },
-      {
-        description: (
-          <Fragment>
-            <h5>{t('Capacitor 2 - Android')}</h5>
-            {t('This step is not needed if you are using Capacitor 3')}
-            <p>
-              {tct(
-                'Then, add the [sentryCapacitorCode:SentryCapacitor] plugin class inside the [onCreateCode:onCreate] method of your [mainActivityCode:MainActivity] file.',
-                {
-                  sentryCapacitorCode: <code />,
-                  onCreateCode: <code />,
-                  mainActivityCode: <code />,
-                }
-              )}
-            </p>
-          </Fragment>
-        ),
-        configurations: [
-          {
-            description: <strong>Java</strong>,
-            language: 'java',
-            code: `
+    ],
+  },
+};
+
+type PlatformOptions = typeof platformOptions;
+type Params = DocsParams<PlatformOptions>;
+
+const getInstallCapacitor2SnippetJava = () => `
 import android.os.Bundle;
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
@@ -75,13 +46,9 @@ public class MainActivity extends BridgeActivity {
       add(SentryCapacitor.class);
     }});
   }
-}
-            `,
-          },
-          {
-            description: <strong>Kotlin</strong>,
-            language: 'kotlin',
-            code: `
+}`;
+
+const getInstallCapacitor2SnippetKotlin = () => `
 import android.os.Bundle
 import com.getcapacitor.BridgeActivity
 import com.getcapacitor.Plugin
@@ -96,134 +63,213 @@ class MainActivity : BridgeActivity() {
       listOf<Class<out Plugin>>(SentryCapacitor::class.java)
     )
   }
-}
-            `,
-          },
-        ],
-      },
-    ],
-    additionalInfo: (
-      <p>
-        {tct(
-          'The version of the sibling SDK must match with the version referred by Sentry Capacitor. To check which version of the sibling SDK is installed, use the following command: [code:npm info @sentry/capacitor peerDependencies]',
-          {code: <code />}
-        )}
-      </p>
-    ),
-  },
+}`;
+
+const getConfigureSnippetAngular = (params: Params) => `
+// app.module.ts
+import * as Sentry from '@sentry/capacitor';
+// Use "@sentry/angular-ivy" for Angular 12+ or "@sentry/angular" for Angular 10 or 11
+import * as SentryAngular from '@sentry/angular-ivy';
+
+
+Sentry.init(
   {
-    type: StepType.CONFIGURE,
-    description: t('You must initialize the Sentry SDK as early as you can:'),
-    configurations: [
-      {
-        description: t('With Ionic/Angular:'),
-        language: 'typescript',
-        code: `
-  // app.module.ts
-  import * as Sentry from '@sentry/capacitor';
-  // Use "@sentry/angular-ivy" for Angular 12+ or "@sentry/angular" for Angular 10 or 11
-  import * as SentryAngular from '@sentry/angular-ivy';
+    dsn: '${params.dsn}',
+    // To set your release and dist versions
+    release: 'my-project-name@' + process.env.npm_package_version,
+    dist: '1',
+    // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
+    // We recommend adjusting this value in production.
+    tracesSampleRate: 1.0,
+    integrations: [
+      new SentryAngular.BrowserTracing({
+        // Set "tracePropagationTargets" to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+        routingInstrumentation: SentryAngular.routingInstrumentation,
+      }),
+    ]
+  },
+  // Forward the init method from @sentry/angular
+  SentryAngular.init
+);
 
-
-  Sentry.init(
+@NgModule({
+  providers: [
     {
-      dsn: '${dsn}',
-      // To set your release and dist versions
-      release: 'my-project-name@' + process.env.npm_package_version,
-      dist: '1',
-      // Set tracesSampleRate to 1.0 to capture 100% of transactions for performance monitoring.
-      // We recommend adjusting this value in production.
-      tracesSampleRate: 1.0,
-      integrations: [
-        new SentryAngular.BrowserTracing({
-          // Set "tracePropagationTargets" to control for which URLs distributed tracing should be enabled
-          tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
-          routingInstrumentation: SentryAngular.routingInstrumentation,
-        }),
-      ]
+      provide: ErrorHandler,
+      // Attach the Sentry ErrorHandler
+      useValue: SentryAngular.createErrorHandler(),
     },
-    // Forward the init method from @sentry/angular
-    SentryAngular.init
-  );
+    {
+      provide: SentryAngular.TraceService,
+      deps: [Router],
+    },
+    {
+      provide: APP_INITIALIZER,
+      useFactory: () => () => {},
+      deps: [SentryAngular.TraceService],
+      multi: true,
+    },
+  ],
+})`;
 
-  @NgModule({
-    providers: [
-      {
-        provide: ErrorHandler,
-        // Attach the Sentry ErrorHandler
-        useValue: SentryAngular.createErrorHandler(),
-      },
-      {
-        provide: SentryAngular.TraceService,
-        deps: [Router],
-      },
-      {
-        provide: APP_INITIALIZER,
-        useFactory: () => () => {},
-        deps: [SentryAngular.TraceService],
-        multi: true,
-      },
-    ],
-  })
-  `,
-      },
-      {
-        description: t('Standalone:'),
-        language: 'javascript',
-        code: `
+const getConfigureSnippetStandalone = (params: Params) => `
 // App.js
 import * as Sentry from "@sentry/capacitor";
 
 Sentry.init({
-  dsn: "${dsn}",
+  dsn: "${params.dsn}",
 
   // Set your release version, such as 'getsentry@1.0.0'
   release: "my-project-name@<release-name>",
   // Set your dist version, such as "1"
   dist: "<dist>",
-});
-        `,
-      },
-    ],
-  },
-  {
-    type: StepType.VERIFY,
-    description: t(
-      'This snippet includes an intentional error, so you can test that everything is working as soon as you set it up:'
-    ),
-    configurations: [
-      {
-        language: 'javascript',
-        code: `
-import * as Sentry from "@sentry/capacitor";
+});`;
 
-Sentry.captureException("Test Captured Exception");
-        `,
-      },
-      {
-        language: 'javascript',
-        description: t('You can also throw an error anywhere in your application:'),
-        code: `
-// Must be thrown after Sentry.init is called to be captured.
-throw new Error("Test Thrown Error");
-        `,
-      },
-      {
-        language: 'javascript',
-        description: t('Or trigger a native crash:'),
-        code: `
-import * as Sentry from "@sentry/capacitor";
+const onboarding: OnboardingConfig<PlatformOptions> = {
+  install: params => [
+    {
+      type: StepType.INSTALL,
+      description: t(
+        'Install the Sentry Capacitor SDK alongside the sibling Sentry Angular SDK:'
+      ),
+      configurations: [
+        {
+          language: 'bash',
+          code: [
+            {
+              label: 'npm',
+              value: 'npm',
+              language: 'bash',
+              code: 'npm install --save @sentry/capacitor @sentry/angular-ivy',
+            },
+            {
+              label: 'yarn',
+              value: 'yarn',
+              language: 'bash',
+              code: 'yarn add @sentry/capacitor @sentry/angular @sentry/tracing --exact',
+            },
+          ],
+        },
+        {
+          language: 'bash',
+          description: t(
+            "Or install the standalone Sentry Capacitor SDK if you don't use Ionic/Angular:"
+          ),
+          code: [
+            {
+              label: 'npm',
+              value: 'npm',
+              language: 'bash',
+              code: 'npm install --save @sentry/capacitor @sentry/tracing',
+            },
+            {
+              label: 'yarn',
+              value: 'yarn',
+              language: 'bash',
+              code: 'yarn add @sentry/capacitor',
+            },
+          ],
+        },
+      ],
+      additionalInfo: tct(
+        'The version of the sibling SDK must match with the version referred by Sentry Capacitor. To check which version of the sibling SDK is installed, use the following command: [code:npm info @sentry/capacitor peerDependencies]',
+        {code: <code />}
+      ),
+    },
+    ...(params.platformOptions.capacitorVersion === CapacitorVersion.V2
+      ? [
+          {
+            title: t('Capacitor 2 - Android'),
+            description: tct(
+              'Add the [sentryCapacitorCode:SentryCapacitor] plugin class inside the [onCreateCode:onCreate] method of your [mainActivityCode:MainActivity] file.',
+              {
+                sentryCapacitorCode: <code />,
+                onCreateCode: <code />,
+                mainActivityCode: <code />,
+              }
+            ),
+            configurations: [
+              {
+                language: 'java',
+                code: [
+                  {
+                    label: 'Java',
+                    value: 'java',
+                    language: 'java',
+                    code: getInstallCapacitor2SnippetJava(),
+                  },
+                  {
+                    label: 'Kotlin',
+                    value: 'kotlin',
+                    language: 'kotlin',
+                    code: getInstallCapacitor2SnippetKotlin(),
+                  },
+                ],
+              },
+            ],
+          },
+        ]
+      : []),
+  ],
+  configure: params => [
+    {
+      type: StepType.CONFIGURE,
+      description: t('You must initialize the Sentry SDK as early as you can:'),
+      configurations: [
+        {
+          description: t('With Ionic/Angular:'),
+          language: 'typescript',
+          code: getConfigureSnippetAngular(params),
+        },
+        {
+          description: t('Standalone:'),
+          language: 'javascript',
+          code: getConfigureSnippetStandalone(params),
+        },
+      ],
+    },
+  ],
+  verify: () => [
+    {
+      type: StepType.VERIFY,
+      description: t(
+        'This snippet includes an intentional error, so you can test that everything is working as soon as you set it up:'
+      ),
+      configurations: [
+        {
+          language: 'javascript',
+          code: `
+  import * as Sentry from "@sentry/capacitor";
 
-Sentry.nativeCrash();
-        `,
-      },
-    ],
-  },
-];
-// Configuration End
+  Sentry.captureException("Test Captured Exception");
+          `,
+        },
+        {
+          language: 'javascript',
+          description: t('You can also throw an error anywhere in your application:'),
+          code: `
+  // Must be thrown after Sentry.init is called to be captured.
+  throw new Error("Test Thrown Error");
+          `,
+        },
+        {
+          language: 'javascript',
+          description: t('Or trigger a native crash:'),
+          code: `
+  import * as Sentry from "@sentry/capacitor";
 
-export function GettingStartedWithCapacitor({dsn, ...props}: ModuleProps) {
-  return <Layout steps={steps({dsn})} {...props} />;
-}
+  Sentry.nativeCrash();
+          `,
+        },
+      ],
+    },
+  ],
+};
 
-export default GettingStartedWithCapacitor;
+const docs: Docs<PlatformOptions> = {
+  onboarding,
+  platformOptions,
+};
+
+export default docs;
