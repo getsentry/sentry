da693921c0e6371b66e8078ce9885f7d6a46e2e5
ArthurKnaus <arthur.knaus@sentry.io>
2023-11-06 13:22:37 +0100

    ref(onboarding): Convert platform electron to new structure (#59419)
    - relates to https://github.com/getsentry/sentry/issues/56549

--- static/app/gettingStartedDocs/electron/electron.tsx
+++ static/app/gettingStartedDocs/electron/electron.tsx
@@ -1,138 +1,105 @@
 import ExternalLink from 'sentry/components/links/externalLink';
-import {Layout, LayoutProps} from 'sentry/components/onboarding/gettingStartedDoc/layout';
-import {ModuleProps} from 'sentry/components/onboarding/gettingStartedDoc/sdkDocumentation';
 import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/step';
+import type {
+  Docs,
+  DocsParams,
+  OnboardingConfig,
+} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {getUploadSourceMapsStep} from 'sentry/components/onboarding/gettingStartedDoc/utils';
 import {t, tct} from 'sentry/locale';
-import type {Organization, PlatformKey} from 'sentry/types';
 
-type StepProps = {
-  newOrg: boolean;
-  organization: Organization;
-  platformKey: PlatformKey;
-  projectId: string;
-  sentryInitContent: string;
+type Params = DocsParams;
+
+const getConfigureSnippet = (params: Params) => `
+import * as Sentry from "@sentry/electron";
+
+Sentry.init({
+  dsn: "${params.dsn}",
+});`;
+
+const onboarding: OnboardingConfig = {
+  install: () => [
+    {
+      type: StepType.INSTALL,
+      description: t('Add the Sentry Electron SDK package as a dependency:'),
+      configurations: [
+        {
+          code: [
+            {
+              label: 'npm',
+              value: 'npm',
+              language: 'bash',
+              code: 'npm install --save @sentry/electron',
+            },
+            {
+              label: 'yarn',
+              value: 'yarn',
+              language: 'bash',
+              code: 'yarn add @sentry/electron',
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  configure: params => [
+    {
+      type: StepType.CONFIGURE,
+      description: tct(
+        `You need to call [codeInit:Sentry.init] in the [codeMain:main] process and in every [codeRenderer:renderer] process you spawn.
+           For more details about configuring the Electron SDK [docsLink:click here].`,
+        {
+          codeInit: <code />,
+          codeMain: <code />,
+          codeRenderer: <code />,
+          docsLink: (
+            <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/electron/" />
+          ),
+        }
+      ),
+      configurations: [
+        {
+          language: 'javascript',
+          code: getConfigureSnippet(params),
+        },
+      ],
+    },
+    getUploadSourceMapsStep({
+      guideLink:
+        'https://docs.sentry.io/platforms/javascript/guides/electron/sourcemaps/',
+      ...params,
+    }),
+  ],
+  verify: () => [
+    {
+      type: StepType.VERIFY,
+      description: t(
+        `One way to verify your setup is by intentionally causing an error that breaks your application.`
+      ),
+      configurations: [
+        {
+          description: t(
+            `Calling an undefined function will throw a JavaScript exception:`
+          ),
+          language: 'javascript',
+          code: 'myUndefinedFunction();',
+        },
+        {
+          description: t(
+            `With Electron you can test native crash reporting by triggering a crash:`
+          ),
+          language: 'javascript',
+          code: 'process.crash();',
+        },
+      ],
+      additionalInfo: t(
+        'You may want to try inserting these code snippets into both your main and any renderer processes to verify Sentry is operational in both.'
+      ),
+    },
+  ],
+};
+const docs: Docs = {
+  onboarding,
 };
 
-export const steps = ({
-  sentryInitContent,
-  ...props
-}: Partial<StepProps> = {}): LayoutProps['steps'] => [
-  {
-    type: StepType.INSTALL,
-    description: t('Add the Sentry Electron SDK package as a dependency:'),
-    configurations: [
-      {
-        code: [
-          {
-            label: 'npm',
-            value: 'npm',
-            language: 'bash',
-            code: 'npm install --save @sentry/electron',
-          },
-          {
-            label: 'yarn',
-            value: 'yarn',
-            language: 'bash',
-            code: 'yarn add @sentry/electron',
-          },
-        ],
-      },
-    ],
-  },
-  {
-    type: StepType.CONFIGURE,
-    description: (
-      <p>
-        {tct(
-          `You need to call [codeInit:Sentry.init] in the [codeMain:main] process and in every [codeRenderer:renderer] process you spawn.
-           For more details about configuring the Electron SDK [docsLink:click here].`,
-          {
-            codeInit: <code />,
-            codeMain: <code />,
-            codeRenderer: <code />,
-            docsLink: (
-              <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/electron/" />
-            ),
-          }
-        )}
-      </p>
-    ),
-    configurations: [
-      {
-        language: 'javascript',
-        code: `
-        import * as Sentry from "@sentry/electron";
-
-        Sentry.init({
-          ${sentryInitContent}
-        });
-        `,
-      },
-    ],
-  },
-  getUploadSourceMapsStep({
-    guideLink: 'https://docs.sentry.io/platforms/javascript/guides/electron/sourcemaps/',
-    ...props,
-  }),
-  {
-    type: StepType.VERIFY,
-    description: t(
-      `One way to verify your setup is by intentionally causing an error that breaks your application.`
-    ),
-    configurations: [
-      {
-        description: t(
-          `Calling an undefined function will throw a JavaScript exception:`
-        ),
-        language: 'javascript',
-        code: `
-        myUndefinedFunction();
-        `,
-      },
-      {
-        description: t(
-          `With Electron you can test native crash reporting by triggering a crash:`
-        ),
-        language: 'javascript',
-        code: `
-        process.crash();
-      `,
-      },
-    ],
-    additionalInfo: t(
-      'You may want to try inserting these code snippets into both your main and any renderer processes to verify Sentry is operational in both.'
-    ),
-  },
-];
-
-// Configuration End
-
-export function GettingStartedWithElectron({
-  dsn,
-  organization,
-  platformKey,
-  projectId,
-  newOrg,
-  ...props
-}: ModuleProps) {
-  const sentryInitContent: string[] = [`dsn: "${dsn}",`];
-
-  return (
-    <Layout
-      steps={steps({
-        sentryInitContent: sentryInitContent.join('\n'),
-        organization,
-        platformKey,
-        projectId,
-        newOrg,
-      })}
-      nextSteps={[]}
-      newOrg={newOrg}
-      platformKey={platformKey}
-      {...props}
-    />
-  );
-}
-
-export default GettingStartedWithElectron;
+export default docs;
