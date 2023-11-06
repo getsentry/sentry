22ed7f38972d466c846d7ba029b8a494ecbf54ce
ArthurKnaus <arthur.knaus@sentry.io>
2023-11-06 12:38:05 +0100

    ref(onboarding): Convert platform cordova to new structure (#59416)
    - relates to https://github.com/getsentry/sentry/issues/56549

--- static/app/gettingStartedDocs/cordova/cordova.tsx
+++ static/app/gettingStartedDocs/cordova/cordova.tsx
@@ -1,75 +1,79 @@
 import {Fragment} from 'react';
 
 import ExternalLink from 'sentry/components/links/externalLink';
-import {Layout, LayoutProps} from 'sentry/components/onboarding/gettingStartedDoc/layout';
-import {ModuleProps} from 'sentry/components/onboarding/gettingStartedDoc/sdkDocumentation';
 import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/step';
+import type {
+  Docs,
+  DocsParams,
+  OnboardingConfig,
+} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {t, tct} from 'sentry/locale';
 
-// Configuration Start
-export const steps = ({
-  dsn,
-}: Partial<Pick<ModuleProps, 'dsn'>> = {}): LayoutProps['steps'] => [
-  {
-    type: StepType.INSTALL,
-    description: t('Install our SDK using the cordova command:'),
-    configurations: [
-      {
-        language: 'bash',
-        code: 'cordova plugin add sentry-cordova',
-      },
-    ],
-  },
-  {
-    type: StepType.CONFIGURE,
-    description: (
-      <p>
-        {tct(
-          'You should [initCode:init] the SDK in the [deviceReadyCode:deviceReady] function, to make sure the native integrations runs. For more details about Cordova [link:click here]',
-          {
-            initCode: <code />,
-            deviceReadyCode: <code />,
-            link: (
-              <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/cordova/" />
-            ),
-          }
-        )}
-      </p>
-    ),
-    configurations: [
-      {
-        language: 'javascript',
-        code: `
+type Params = DocsParams;
+
+const getConfigureSnippet = (params: Params) => `
 onDeviceReady: function() {
   var Sentry = cordova.require('sentry-cordova.Sentry');
-  Sentry.init({ dsn: '${dsn}' });
-}
-        `,
-      },
-    ],
-  },
-  {
-    type: StepType.VERIFY,
-    description: (
-      <Fragment>
-        {t(
-          'One way to verify your setup is by intentionally causing an error that breaks your application.'
-        )}
-        <p>{t('Calling an undefined function will throw an exception:')}</p>
-      </Fragment>
-    ),
-    configurations: [
-      {
-        language: 'javascript',
-        code: 'myUndefinedFunction();',
-      },
-    ],
-  },
-];
-// Configuration End
+  Sentry.init({ dsn: '${params.dsn}' });
+}`;
 
-export function GettingStartedWithCordova({dsn, ...props}: ModuleProps) {
-  return <Layout steps={steps({dsn})} {...props} />;
-}
+const onboarding: OnboardingConfig = {
+  install: () => [
+    {
+      type: StepType.INSTALL,
+      description: t('Install our SDK using the cordova command:'),
+      configurations: [
+        {
+          language: 'bash',
+          code: 'cordova plugin add sentry-cordova',
+        },
+      ],
+    },
+  ],
+  configure: params => [
+    {
+      type: StepType.CONFIGURE,
+      description: tct(
+        'You should [initCode:init] the SDK in the [deviceReadyCode:deviceReady] function, to make sure the native integrations runs. For more details about Cordova [link:click here]',
+        {
+          initCode: <code />,
+          deviceReadyCode: <code />,
+          link: (
+            <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/cordova/" />
+          ),
+        }
+      ),
+      configurations: [
+        {
+          language: 'javascript',
+          code: getConfigureSnippet(params),
+        },
+      ],
+    },
+  ],
+  verify: () => [
+    {
+      type: StepType.VERIFY,
+      description: (
+        <Fragment>
+          {t(
+            'One way to verify your setup is by intentionally causing an error that breaks your application.'
+          )}
+          <p>{t('Calling an undefined function will throw an exception:')}</p>
+        </Fragment>
+      ),
+      configurations: [
+        {
+          language: 'javascript',
+          code: 'myUndefinedFunction();',
+        },
+      ],
+    },
+  ],
+};
 
-export default GettingStartedWithCordova;
+const docs: Docs = {
+  onboarding,
+};
+
+export default docs;
