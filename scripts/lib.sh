59cac01d1d8e15cc0244e5c3f01326fb28d857d2
Buck Evan <buck.evan@sentry.io>
2023-11-06 14:11:15 -0600

    -

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -24,9 +24,9 @@ export XDG_DATA_HOME="${XDG_DATA_HOME:-$HOME/.local/share}"
 export XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 export XDG_STATE_HOME="${XDG_STATE_HOME:-$HOME/.local/state}"
 export XDG_DATA_DIRS="${XDG_DATA_DIRS:-/usr/local/share/:/usr/share/}"
-export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS:/etc/xdg}"
+export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS:-/etc/xdg}"
 export XDG_CACHE_HOME="${XDG_CACHE_HOME:-$HOME/.cache}"
-export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:/var/run}"
+export XDG_RUNTIME_DIR="${XDG_RUNTIME_DIR:-/var/run}"
 
 
 # Check if a command is available
