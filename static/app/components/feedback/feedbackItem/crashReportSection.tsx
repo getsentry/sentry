3c711fe60e29f16adf6416ee6636ee77ce746dc3
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-11-06 21:58:42 -0800

    working

--- static/app/components/feedback/feedbackItem/crashReportSection.tsx
+++ static/app/components/feedback/feedbackItem/crashReportSection.tsx
@@ -7,7 +7,7 @@ import Section from 'sentry/components/feedback/feedbackItem/feedbackItemSection
 import {IconIssues} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
-import {Event, Organization} from 'sentry/types';
+import {Event, Group, Organization} from 'sentry/types';
 import {useApiQuery} from 'sentry/utils/queryClient';
 
 interface Props {
@@ -21,18 +21,23 @@ export default function CrashReportSection({
   organization,
   projSlug,
 }: Props) {
-  const endpoint = `/projects/${organization.slug}/${projSlug}/events/${crashReportId}/`;
-  const {data: crashReportData} = useApiQuery<Event>([endpoint], {staleTime: 0});
-  return crashReportData ? (
+  const eventEndpoint = `/projects/${organization.slug}/${projSlug}/events/${crashReportId}/`;
+  const {data: crashReportData} = useApiQuery<Event>([eventEndpoint], {staleTime: 0});
+  // need to fetch the issue data since the event data is not complete enough for the component
+  const groupId = crashReportData?.groupID;
+  const issueEndpoint = `/organizations/${organization.slug}/issues/${groupId}/`;
+  const {data: groupData} = useApiQuery<Group>([issueEndpoint], {staleTime: 0});
+
+  return groupData ? (
     <Section icon={<IconIssues size="xs" />} title={t('Linked Issue')}>
       <ErrorBoundary mini>
         <IssueDetailsContainer>
           <EventOrGroupHeader
             organization={organization}
-            data={crashReportData}
+            data={groupData}
             size="normal"
           />
-          <EventOrGroupExtraDetails data={crashReportData} showInboxTime />
+          <EventOrGroupExtraDetails data={groupData} showInboxTime />
         </IssueDetailsContainer>
       </ErrorBoundary>
     </Section>
