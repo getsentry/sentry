37def21c9e5b81e4a366b013b41bb1ee8fac048e
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-11-06 17:00:19 -0800

    feat(feedback): add associated crash report to feedback details

--- static/app/components/feedback/feedbackItem/feedbackItem.tsx
+++ static/app/components/feedback/feedbackItem/feedbackItem.tsx
@@ -9,6 +9,8 @@ import {
 import Button from 'sentry/components/actions/button';
 import ProjectAvatar from 'sentry/components/avatar/projectAvatar';
 import ErrorBoundary from 'sentry/components/errorBoundary';
+import EventOrGroupExtraDetails from 'sentry/components/eventOrGroupExtraDetails';
+import EventOrGroupHeader from 'sentry/components/eventOrGroupHeader';
 import FeedbackAssignedTo from 'sentry/components/feedback/feedbackItem/feedbackAssignedTo';
 import Section from 'sentry/components/feedback/feedbackItem/feedbackItemSection';
 import FeedbackItemUsername from 'sentry/components/feedback/feedbackItem/feedbackItemUsername';
@@ -21,12 +23,13 @@ import PanelItem from 'sentry/components/panels/panelItem';
 import {Flex} from 'sentry/components/profiling/flex';
 import TextCopyInput from 'sentry/components/textCopyInput';
 import TextOverflow from 'sentry/components/textOverflow';
-import {IconLink} from 'sentry/icons';
+import {IconIssues, IconLink} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Event} from 'sentry/types';
 import {GroupStatus} from 'sentry/types';
 import type {FeedbackIssue} from 'sentry/utils/feedback/types';
+import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 
 interface Props {
@@ -54,6 +57,11 @@ export default function FeedbackItem({feedbackItem, eventData, tags}: Props) {
       addSuccessMessage(t('Updated feedback'));
     },
   };
+
+  // const crashReportId = eventData?.contexts?.feedback?.associated_event_id;
+  const endpoint = `/organizations/${organization.slug}/issues/${feedbackItem.id}/events/${eventData?.id}/`; // replace with crashReportId
+  const {data: crashReportData} = useApiQuery<Event>([endpoint], {staleTime: 0});
+  // console.log(crashReportData);
 
   return (
     <Fragment>
@@ -119,13 +127,26 @@ export default function FeedbackItem({feedbackItem, eventData, tags}: Props) {
             <pre>{feedbackItem.metadata.message}</pre>
           </Blockquote>
         </Section>
-
         <Section icon={<IconLink size="xs" />} title={t('Url')}>
           <ErrorBoundary mini>
             <TextCopyInput size="sm">{url?.value ?? t('URL not found')}</TextCopyInput>
           </ErrorBoundary>
         </Section>
 
+        {crashReportData && (
+          <Section icon={<IconIssues size="xs" />} title={t('Linked Issue')}>
+            <ErrorBoundary mini>
+              <IssueDetailsContainer>
+                <EventOrGroupHeader
+                  organization={organization}
+                  data={crashReportData}
+                  size="normal"
+                />
+                <EventOrGroupExtraDetails data={crashReportData} showInboxTime />
+              </IssueDetailsContainer>
+            </ErrorBoundary>
+          </Section>
+        )}
         {hasReplayId && replayId ? (
           <ReplaySection
             eventTimestampMs={new Date(feedbackItem.firstSeen).getTime()}
@@ -133,12 +154,18 @@ export default function FeedbackItem({feedbackItem, eventData, tags}: Props) {
             replayId={replayId}
           />
         ) : null}
-
         <TagsSection tags={tags} />
       </OverflowPanelItem>
     </Fragment>
   );
 }
+
+const IssueDetailsContainer = styled('div')`
+  border: 1px solid ${p => p.theme.border};
+  border-radius: ${p => p.theme.borderRadius};
+  position: relative;
+  padding: ${space(1.5)} ${space(1.5)} ${space(1.5)} ${space(2)};
+`;
 
 const HeaderPanelItem = styled(PanelItem)`
   display: grid;
