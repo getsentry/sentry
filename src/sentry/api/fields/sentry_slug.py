1429fa58f2e6593641eb4a359af893ef9afd46be
schew2381 <seiji.chew@sentry.io>
2023-11-07 11:54:42 -0800

    fix comment block

--- src/sentry/api/fields/sentry_slug.py
+++ src/sentry/api/fields/sentry_slug.py
@@ -7,8 +7,8 @@ from rest_framework import serializers
 
 r"""
 Standard slug pattern:
-# (?![0-9]+$) - Negative lookahead to ensure the slug is not entirely numeric
-# [a-z0-9_\-] - Matches lowercase letters, numbers, underscores, and hyphens
+    (?![0-9]+$) - Negative lookahead to ensure the slug is not entirely numeric
+    [a-z0-9_\-] - Matches lowercase letters, numbers, underscores, and hyphens
 """
 MIXED_SLUG_PATTERN = r"^(?![0-9]+$)[a-z0-9_\-]+$"
 DEFAULT_SLUG_ERROR_MESSAGE = _(
@@ -18,10 +18,10 @@ DEFAULT_SLUG_ERROR_MESSAGE = _(
 
 """
 Organization slug pattern:
-(?![0-9]+$)   - Negative lookahead to ensure the slug is not entirely numeric
-[a-zA-Z0-9]   - Must start with a lowercase letter or number
-[a-zA-Z0-9-]* - Matches lowercase letters, numbers, and hyphens
-(?<!-)        - Negative lookbehind to ensure the slug does not end with a hyphen
+    (?![0-9]+$)   - Negative lookahead to ensure the slug is not entirely numeric
+    [a-zA-Z0-9]   - Must start with a lowercase letter or number
+    [a-zA-Z0-9-]* - Matches lowercase letters, numbers, and hyphens
+    (?<!-)        - Negative lookbehind to ensure the slug does not end with a hyphen
 """
 ORG_SLUG_PATTERN = r"^(?![0-9]+$)[a-zA-Z0-9][a-zA-Z0-9-]*(?<!-)$"
 
