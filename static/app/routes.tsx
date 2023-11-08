f66b2d6e5e6d94ac70c6304f2c955f4ae29c5952
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2023-11-08 10:33:26 -0500

    chore(starfish): Nest Screens under Performance in sidebar

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -1698,6 +1698,21 @@ function buildRoutes() {
           />
         </Route>
       </Route>
+      <Route path="mobile/">
+        <Route path="screens/">
+          <IndexRoute
+            component={make(
+              () => import('sentry/views/starfish/modules/mobile/pageload')
+            )}
+          />
+          <Route
+            path="spans/"
+            component={make(
+              () => import('sentry/views/starfish/views/screens/screenLoadSpans')
+            )}
+          />
+        </Route>
+      </Route>
       <Route path="summary/">
         <IndexRoute
           component={make(
