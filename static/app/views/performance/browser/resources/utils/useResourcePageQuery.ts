4b020c9de8c0f3c102b94350ebda3cd284d2e3b0
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-11-08 11:42:18 -0500

    feat(browser-starfish): add render blocking status to table (#59583)
    To match the mocks, we are adding render blocking status in order to
    quickly find if a resource is only blocking certain pages. We are doing
    this despite the duplicate entries, long term we can figure out a way to
    aggregate between the two.
    
    <img width="923" alt="image"
    src="https://github.com/getsentry/sentry/assets/44422760/969ba711-e3a5-49ab-9fca-d866a1f5323d">

--- static/app/views/performance/browser/resources/utils/useResourcePageQuery.ts
+++ static/app/views/performance/browser/resources/utils/useResourcePageQuery.ts
@@ -2,10 +2,11 @@ import {Sort} from 'sentry/utils/discover/fields';
 import {useSpanTransactionMetrics} from 'sentry/views/starfish/queries/useSpanTransactionMetrics';
 import {SpanMetricsField} from 'sentry/views/starfish/types';
 
-const {HTTP_RESPONSE_CONTENT_LENGTH} = SpanMetricsField;
+const {HTTP_RESPONSE_CONTENT_LENGTH, RESOURCE_RENDER_BLOCKING_STATUS} = SpanMetricsField;
 
 export const useResourcePagesQuery = (groupId: string, {sort}: {sort: Sort}) => {
   return useSpanTransactionMetrics({'span.group': groupId}, [sort], undefined, [
     `avg(${HTTP_RESPONSE_CONTENT_LENGTH})`,
+    RESOURCE_RENDER_BLOCKING_STATUS,
   ]);
 };
