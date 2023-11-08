f541fa2eab80c637b186da920be2e4bde6744d2e
Cathy Teng <cathy.teng@sentry.io>
2023-11-08 15:41:00 -0800

    ref members list

--- static/app/views/settings/organizationMembers/organizationMembersList.tsx
+++ static/app/views/settings/organizationMembers/organizationMembersList.tsx
@@ -173,30 +173,6 @@ class OrganizationMembersList extends DeprecatedAsyncView<Props, State> {
     this.setState(state => ({invited: {...state.invited, [id]: 'success'}}));
   };
 
-  handleInviteMissingMember = async (email: string) => {
-    const {organization} = this.props;
-
-    try {
-      await this.api.requestPromise(
-        `/organizations/${organization.slug}/members/?referrer=github_nudge_invite`,
-        {
-          method: 'POST',
-          data: {email},
-        }
-      );
-      addSuccessMessage(tct('Sent invite to [email]', {email}));
-      this.fetchMembersList();
-      this.setState(state => ({
-        missingMembers: state.missingMembers.map(integrationMissingMembers => ({
-          ...integrationMissingMembers,
-          users: integrationMissingMembers.users.filter(member => member.email !== email),
-        })),
-      }));
-    } catch {
-      addErrorMessage(t('Error sending invite'));
-    }
-  };
-
   fetchMembersList = async () => {
     const {organization} = this.props;
 
@@ -300,13 +276,7 @@ class OrganizationMembersList extends DeprecatedAsyncView<Props, State> {
 
   renderBody() {
     const {organization} = this.props;
-    const {
-      membersPageLinks,
-      members,
-      member: currentMember,
-      inviteRequests,
-      missingMembers,
-    } = this.state;
+    const {membersPageLinks, members, member: currentMember, inviteRequests} = this.state;
     const {access} = organization;
 
     const canAddMembers = access.includes('member:write');
@@ -334,15 +304,10 @@ class OrganizationMembersList extends DeprecatedAsyncView<Props, State> {
       </SearchWrapperWithFilter>
     );
 
-    const githubMissingMembers = missingMembers?.filter(
-      integrationMissingMembers => integrationMissingMembers.integration === 'github'
-    )[0];
-
     return (
       <Fragment>
         <InviteBanner
-          missingMembers={githubMissingMembers}
-          onSendInvite={this.handleInviteMissingMember}
+          onSendInvite={this.fetchMembersList}
           onModalClose={this.fetchData}
           allowedRoles={currentMember ? currentMember.roles : ORG_ROLES}
         />
