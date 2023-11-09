db5209a5ab6993fb76184b040cf30baaa1e7f7ee
John <john.yang@sentry.io>
2023-11-09 13:57:42 -0800

    fix(metrics): Enforce total ordering of all messages (#59545)
    ### Overview
    
    Enforce total ordering of all valid and invalid messages, so when
    invalid message is put to the dlq, it is guaranteed to be in the buffer.
    
    Ideally, the validation and filtering of invalid messages should take
    place in another step. It might be worth considering refactoring this
    further in the future

--- tests/sentry/sentry_metrics/test_gen_metrics_multiprocess_steps.py
+++ tests/sentry/sentry_metrics/test_gen_metrics_multiprocess_steps.py
@@ -4,7 +4,6 @@ import logging
 import pickle
 import re
 import time
-from collections import deque
 from copy import deepcopy
 from datetime import datetime, timezone
 from typing import Any, Dict, List, MutableMapping, Sequence, Union
@@ -12,6 +11,7 @@ from unittest.mock import Mock, call
 
 import pytest
 from arroyo.backends.kafka import KafkaPayload
+from arroyo.dlq import InvalidMessage
 from arroyo.processing.strategies import MessageRejected
 from arroyo.types import BrokerValue, Message, Partition, Topic, Value
 
@@ -21,7 +21,6 @@ from sentry.sentry_metrics.configuration import IndexerStorage, UseCaseKey, get_
 from sentry.sentry_metrics.consumers.indexer.batch import valid_metric_name
 from sentry.sentry_metrics.consumers.indexer.common import (
     BatchMessages,
-    BrokerMeta,
     IndexerOutputMessageBatch,
     MetricsBatchBuilder,
 )
@@ -57,6 +56,10 @@ def compare_messages_ignoring_mapping_metadata(actual: Message, expected: Messag
     actual_payload = actual.payload
     expected_payload = expected.payload
 
+    if isinstance(actual_payload, InvalidMessage):
+        assert actual_payload == expected_payload
+        return
+
     assert actual_payload.key == expected_payload.key
 
     actual_headers_without_mapping_sources = [
@@ -74,7 +77,7 @@ def compare_message_batches_ignoring_metadata(
     actual: IndexerOutputMessageBatch, expected: Sequence[Message]
 ) -> None:
     assert len(actual.data) == len(expected)
-    for (a, e) in zip(actual.data, expected):
+    for a, e in zip(actual.data, expected):
         compare_messages_ignoring_mapping_metadata(a, e)
 
 
@@ -367,7 +370,6 @@ def test_process_messages() -> None:
         )
 
     compare_message_batches_ignoring_metadata(new_batch, expected_new_batch)
-    assert not new_batch.invalid_msg_meta
 
 
 @pytest.mark.django_db
@@ -396,7 +398,6 @@ def test_process_messages_default_card_rollout(set_sentry_option) -> None:
         new_batch = MESSAGE_PROCESSOR.process_messages(outer_message=outer_message)
 
     assert len(new_batch.data) == len(message_batch)
-    assert not new_batch.invalid_msg_meta
 
 
 invalid_payloads = [
@@ -502,11 +503,16 @@ def test_process_messages_invalid_messages(
                 ),
                 expected_msg.committable,
             )
-        )
+        ),
+        Message(
+            Value(
+                InvalidMessage(Partition(Topic("topic"), 0), 1),
+                message_batch[1].committable,
+            )
+        ),
     ]
     compare_message_batches_ignoring_metadata(new_batch, expected_new_batch)
     assert error_text in caplog.text
-    assert new_batch.invalid_msg_meta == deque([BrokerMeta(Partition(Topic("topic"), 0), 1)])
 
 
 @pytest.mark.django_db
@@ -578,7 +584,6 @@ def test_process_messages_rate_limited(caplog, settings) -> None:
     ]
     compare_message_batches_ignoring_metadata(new_batch, expected_new_batch)
     assert "dropped_message" in caplog.text
-    assert not new_batch.invalid_msg_meta
 
 
 @pytest.mark.django_db
@@ -630,8 +635,6 @@ def test_process_messages_cardinality_limited(
             new_batch = MESSAGE_PROCESSOR.process_messages(outer_message=outer_message)
 
         compare_message_batches_ignoring_metadata(new_batch, [])
-
-    assert not new_batch.invalid_msg_meta
 
 
 def test_valid_metric_name() -> None:
