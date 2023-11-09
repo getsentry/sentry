caec99cfc52e8fef2c5053b712b69fd78740132f
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2023-11-09 12:23:26 -0500

    fix(browser-starifish): resource sizes dissapear when switching page in summary (#59692)
    Fixes a bug where switches pages in the resource summary would result in
    <img width="583" alt="image"
    src="https://github.com/getsentry/sentry/assets/44422760/ab31e74b-c159-459b-9bd1-96a7b07d32e3">
    On the resource info.
    
    This was because the request to fetch the resource info would read the
    `cursor` query param, and try to fetch the "next page".

--- static/app/views/performance/browser/resources/utils/useResourcePageQuery.ts
+++ static/app/views/performance/browser/resources/utils/useResourcePageQuery.ts
@@ -4,8 +4,11 @@ import {SpanMetricsField} from 'sentry/views/starfish/types';
 
 const {HTTP_RESPONSE_CONTENT_LENGTH, RESOURCE_RENDER_BLOCKING_STATUS} = SpanMetricsField;
 
-export const useResourcePagesQuery = (groupId: string, {sort}: {sort: Sort}) => {
-  return useSpanTransactionMetrics({'span.group': groupId}, [sort], undefined, [
+export const useResourcePagesQuery = (
+  groupId: string,
+  {sort, cursor}: {sort: Sort; cursor?: string}
+) => {
+  return useSpanTransactionMetrics({'span.group': groupId}, [sort], cursor, [
     `avg(${HTTP_RESPONSE_CONTENT_LENGTH})`,
     RESOURCE_RENDER_BLOCKING_STATUS,
   ]);
