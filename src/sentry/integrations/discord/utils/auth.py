a804d7f706a74d2a5825aeb92fb5d078a3a425c9
17hogeju <17hogeju@gmail.com>
2023-11-09 10:29:35 -0800

    Avoid circular import

--- src/sentry/integrations/discord/utils/auth.py
+++ src/sentry/integrations/discord/utils/auth.py
@@ -2,12 +2,12 @@ from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
 from rest_framework import status
 
-from sentry.integrations.discord.requests.base import DiscordRequestError
-
 
 def verify_signature(public_key_string: str, signature: str, timestamp: str, body: str) -> None:
     try:
         public_key = Ed25519PublicKey.from_public_bytes(bytes.fromhex(public_key_string))
         public_key.verify(bytes.fromhex(signature), f"{timestamp}{body}".encode())
     except (InvalidSignature, ValueError):
+        from sentry.integrations.discord.requests.base import DiscordRequestError
+
         raise DiscordRequestError(status=status.HTTP_401_UNAUTHORIZED)
