d24ef6c79149d94366a07e0aaae664c7235220bf
17hogeju <17hogeju@gmail.com>
2023-11-09 10:08:41 -0800

    Fix logging imports

--- src/sentry/integrations/discord/utils/auth.py
+++ src/sentry/integrations/discord/utils/auth.py
@@ -4,15 +4,10 @@ from rest_framework import status
 
 from sentry.integrations.discord.requests.base import DiscordRequestError
 
-from ..utils import logger
-
 
 def verify_signature(public_key_string: str, signature: str, timestamp: str, body: str) -> None:
     try:
         public_key = Ed25519PublicKey.from_public_bytes(bytes.fromhex(public_key_string))
         public_key.verify(bytes.fromhex(signature), f"{timestamp}{body}".encode())
     except (InvalidSignature, ValueError):
-        logger.info(
-            "discord.auth.verify.signature.false", extra={"timestamp": timestamp, "body": body}
-        )
         raise DiscordRequestError(status=status.HTTP_401_UNAUTHORIZED)
