1c36acbd23c274b3a0ada87edea487a8bd85f3d9
17hogeju <17hogeju@gmail.com>
2023-11-09 12:11:45 -0800

    Fix optional

--- src/sentry/integrations/discord/utils/auth.py
+++ src/sentry/integrations/discord/utils/auth.py
@@ -1,10 +1,12 @@
+from typing import Optional
+
 from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
 from rest_framework import status
 
 
 def verify_signature(
-    public_key_string: str, signature: str | None, timestamp: str | None, body: str
+    public_key_string: str, signature: Optional[str], timestamp: Optional[str] | None, body: str
 ) -> None:
     try:
         public_key = Ed25519PublicKey.from_public_bytes(bytes.fromhex(public_key_string))
