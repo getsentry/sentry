ca1e1c98a88fc4fd764aa43a729a599e2ebb6c16
Kev <fisherk.work@gmail.com>
2023-11-10 15:46:41 -0500

    Wrong variable

--- src/sentry/snuba/metrics/extraction.py
+++ src/sentry/snuba/metrics/extraction.py
@@ -933,7 +933,7 @@ class OnDemandMetricSpec:
         str_to_hash = self._query_str_for_hash
         hash = hashlib.shake_128(bytes(str_to_hash, encoding="ascii")).hexdigest(4)
         with sentry_sdk.start_span(op="OnDemandMetricSpec.query_hash", description=hash) as span:
-            span.set_tag("str_to_hash", hash)
+            span.set_tag("str_to_hash", str_to_hash)
         return hash
 
     def _field_for_hash(self) -> Optional[str]:
