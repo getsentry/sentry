d8ea1ffc8a3e6fce8530efcd0b3eb472767aaf28
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2023-11-10 10:58:38 -0500

    excluded tags

--- static/app/components/feedback/feedbackSearch.tsx
+++ static/app/components/feedback/feedbackSearch.tsx
@@ -3,10 +3,23 @@ import {browserHistory} from 'react-router';
 import styled from '@emotion/styled';
 
 import {t} from 'sentry/locale';
+import {FieldKey} from 'sentry/utils/fields';
 import {decodeScalar} from 'sentry/utils/queryString';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import IssueListSearchBar from 'sentry/views/issueList/searchBar';
+
+const excludedTags = [
+  FieldKey.EVENT_TYPE,
+  FieldKey.ISSUE_CATEGORY,
+  FieldKey.ERROR_HANDLED,
+  FieldKey.ERROR_UNHANDLED,
+  FieldKey.ERROR_MAIN_THREAD,
+  FieldKey.ERROR_MECHANISM,
+  FieldKey.ERROR_RECEIVED,
+  FieldKey.ERROR_TYPE,
+  FieldKey.ERROR_VALUE,
+];
 
 interface Props {
   className?: string;
@@ -23,6 +36,7 @@ export default function FeedbackSearch({className, style}: Props) {
         placeholder={t('Search Feedback')}
         organization={organization}
         defaultSearchGroup={undefined}
+        excludedTags={excludedTags}
         defaultQuery=""
         query={decodeScalar(query.query, '')}
         onSearch={searchQuery => {
