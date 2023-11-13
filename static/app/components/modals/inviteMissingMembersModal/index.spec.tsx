719fdbc8b34c55213bcbf34a33a7698909206622
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2023-11-13 14:45:10 -0800

    ref(github-growth): scroll table in modal (#59856)

--- static/app/components/modals/inviteMissingMembersModal/index.spec.tsx
+++ static/app/components/modals/inviteMissingMembersModal/index.spec.tsx
@@ -183,10 +183,14 @@ describe('InviteMissingMembersModal', function () {
     const teamInputs = screen.getAllByRole('textbox', {name: 'Add to Team'});
 
     await userEvent.click(screen.getByLabelText('Select hello@sentry.io'));
-    await selectEvent.select(roleInputs[0], 'Admin');
+    await selectEvent.select(roleInputs[0], 'Admin', {
+      container: document.body,
+    });
 
     await userEvent.click(screen.getByLabelText('Select abcd@sentry.io'));
-    await selectEvent.select(teamInputs[1], '#team-slug');
+    await selectEvent.select(teamInputs[1], '#team-slug', {
+      container: document.body,
+    });
 
     await userEvent.click(screen.getByLabelText('Send Invites'));
 
