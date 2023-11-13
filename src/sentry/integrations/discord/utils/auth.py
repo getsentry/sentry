80c8b2cbb3ad2725ae45ee2b784d4eb1d9f09cb6
Julia Hoge <julia.hoge@sentry.io>
2023-11-13 11:49:41 -0800

    ref(discord): Add logging for discord.request.error (#59474)
    Trying to resolve
    https://sentry.sentry.io/issues/4552017651/?project=1&query=discord.request.error&referrer=issue-stream&statsPeriod=14d&stream_index=0
    
    `discord.request.error` with error code 401 is only called in the
    authorize function in base.py. In order to debug which of the conditions
    is failing in `if signature and timestamp and
    verify_signature(public_key, signature, timestamp + body)`, I'm adding
    logging.
    
    Ensuring we are following the authorization documented in the Discord
    developer portal
    https://discord.com/developers/docs/interactions/receiving-and-responding
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>
    Co-authored-by: Snigdha Sharma <snigdha.sharma@sentry.io>

--- src/sentry/integrations/discord/utils/auth.py
+++ src/sentry/integrations/discord/utils/auth.py
@@ -2,10 +2,9 @@ from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
 
 
-def verify_signature(public_key_string: str, signature: str, message: str) -> bool:
+def verify_signature(public_key_string: str, signature: str, timestamp: str, body: str) -> None:
     try:
         public_key = Ed25519PublicKey.from_public_bytes(bytes.fromhex(public_key_string))
-        public_key.verify(bytes.fromhex(signature), message.encode("utf-8"))
-        return True
-    except (InvalidSignature, ValueError):
-        return False
+        public_key.verify(bytes.fromhex(signature), f"{timestamp}{body}".encode())
+    except (InvalidSignature, ValueError) as e:
+        raise e
