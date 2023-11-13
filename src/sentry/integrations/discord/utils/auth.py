a6c558467304a31b5daff7c6d5f6034e2796d5bb
17hogeju <17hogeju@gmail.com>
2023-11-13 10:04:53 -0800

    Move try/except

--- src/sentry/integrations/discord/utils/auth.py
+++ src/sentry/integrations/discord/utils/auth.py
@@ -1,13 +1,6 @@
-from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.primitives.asymmetric.ed25519 import Ed25519PublicKey
-from rest_framework import status
 
 
 def verify_signature(public_key_string: str, signature: str, timestamp: str, body: str) -> None:
-    try:
-        public_key = Ed25519PublicKey.from_public_bytes(bytes.fromhex(public_key_string))
-        public_key.verify(bytes.fromhex(signature), f"{timestamp}{body}".encode())
-    except (InvalidSignature, ValueError):
-        from sentry.integrations.discord.requests.base import DiscordRequestError
-
-        raise DiscordRequestError(status=status.HTTP_401_UNAUTHORIZED)
+    public_key = Ed25519PublicKey.from_public_bytes(bytes.fromhex(public_key_string))
+    public_key.verify(bytes.fromhex(signature), f"{timestamp}{body}".encode())
