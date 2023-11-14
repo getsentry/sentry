f49869cfc3c2ce3af0b33b136ca0ab3d7cbe8d5c
Nisanthan Nanthakumar <nnutoronto2013@gmail.com>
2023-11-13 20:55:01 -0800

    fix analytic event attribute

--- src/sentry/analytics/events/eventuser_snuba_query.py
+++ src/sentry/analytics/events/eventuser_snuba_query.py
@@ -5,7 +5,7 @@ class EventUserSnubaQuery(analytics.Event):
     type = "eventuser_snuba.query"
 
     attributes = (
-        analytics.Attribute("project_id", type=list),
+        analytics.Attribute("project_ids", type=list),
         analytics.Attribute("query"),
         analytics.Attribute("count_rows_returned", required=True, type=int),
         analytics.Attribute("count_rows_filtered", required=True, type=int),
