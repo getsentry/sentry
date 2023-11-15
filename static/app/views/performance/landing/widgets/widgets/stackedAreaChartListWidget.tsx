8016f5404e17da0889a0d9736b04ee92278653bc
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2023-11-15 12:29:45 -0500

    fix(webvitals): adds more optional checks (#59985)
    adds more optional checks

--- static/app/views/performance/landing/widgets/widgets/stackedAreaChartListWidget.tsx
+++ static/app/views/performance/landing/widgets/widgets/stackedAreaChartListWidget.tsx
@@ -406,7 +406,7 @@ export function StackedAreaChartListWidget(props: PerformanceWidgetProps) {
       listItem =>
         function () {
           const transaction = (listItem.transaction as string | undefined) ?? '';
-          const count = projectData?.data[0]['count()'] as number;
+          const count = projectData?.data?.[0]?.['count()'] as number;
           if (props.chartSetting === PerformanceWidgetSetting.HIGHEST_OPPORTUNITY_PAGES) {
             const projectScore = calculatePerformanceScoreFromTableDataRow(
               projectData?.data?.[0]
@@ -419,7 +419,9 @@ export function StackedAreaChartListWidget(props: PerformanceWidgetProps) {
               fid: listItem['p75(measurements.fid)'] as number,
             });
             const opportunity =
-              projectScore.totalScore !== null && rowScore.totalScore !== null
+              projectScore.totalScore !== null &&
+              rowScore.totalScore !== null &&
+              count !== undefined
                 ? calculateOpportunity(
                     projectScore.totalScore,
                     count,
