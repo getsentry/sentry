c1ec2e62bec0df3ebf733dd6c2e52e09f9c4bf3c
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2023-11-15 15:53:01 -0500

    clean up timeline feature flag

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -212,7 +212,6 @@ default_manager.add("organizations:session-replay-trial-ended-banner", Organizat
 default_manager.add("organizations:session-replay-ui", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:session-replay-new-event-counts", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:session-replay-weekly-email", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
-default_manager.add("organizations:session-replay-new-timeline", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:session-replay-accessibility-issues", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:set-grouping-config", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:slack-overage-notifications", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
