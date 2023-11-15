ab5cba0f02624ae61dfef48fd3a0465cb2f52e4c
Jonas <jonas.badalic@sentry.io>
2023-11-15 08:26:11 -0500

    feat(profiling): add differential flamegraph boilerplate to statistical issue (#59905)
    Add component boilerplate for differential flamegraph

--- static/app/utils/profiling/hooks/useAggregateFlamegraphQuery.ts
+++ static/app/utils/profiling/hooks/useAggregateFlamegraphQuery.ts
@@ -1,15 +1,23 @@
 import {useMemo} from 'react';
 
 import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
+import {PageFilters} from 'sentry/types';
 import {useApiQuery} from 'sentry/utils/queryClient';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import useOrganization from 'sentry/utils/useOrganization';
-import usePageFilters from 'sentry/utils/usePageFilters';
 
-export function useAggregateFlamegraphQuery({transaction}: {transaction: string}) {
+export function useAggregateFlamegraphQuery({
+  projects,
+  datetime,
+  environments,
+  transaction,
+}: {
+  datetime: Partial<PageFilters['datetime']>;
+  environments: string[];
+  projects: number[];
+  transaction: string;
+}) {
   const organization = useOrganization();
-  const {selection} = usePageFilters();
-
   const path = `/organizations/${organization.slug}/profiling/flamegraph/`;
 
   const query = useMemo(() => {
@@ -20,14 +28,13 @@ export function useAggregateFlamegraphQuery({transaction}: {transaction: string}
     return conditions.formatString();
   }, [transaction]);
 
-  const enabled =
-    !!transaction && Array.isArray(selection.projects) && selection.projects.length > 0;
+  const enabled = !!transaction && Array.isArray(projects) && projects.length > 0;
 
   const endpointOptions = {
     query: {
-      project: selection.projects,
-      environment: selection.environments,
-      ...normalizeDateTimeParams(selection.datetime),
+      project: projects,
+      environment: environments,
+      ...normalizeDateTimeParams(datetime),
       query,
     },
   };
