050a54cd136db8094c7a3d44d7d016ffa267110c
Buck Evan <buck.evan@sentry.io>
2023-11-16 11:45:04 -0600

    fix lint: "print not allowed"

--- src/sentry/runner/commands/migrations.py
+++ src/sentry/runner/commands/migrations.py
@@ -3,13 +3,6 @@ import click
 from sentry.runner.decorators import configuration
 
 
-def info(*msg: str, end="\n"):
-    """Send a message to the user and/or logging."""
-    from sys import stderr
-
-    print(*msg, file=stderr, flush=True, end=end)
-
-
 @click.group()
 def migrations():
     "Manage migrations."
@@ -32,30 +25,34 @@ def run(ctx, app_name, migration_name):
     if not getattr(migration, "is_dangerous", None):
         raise click.ClickException(
             f"This is not a post-deployment migration: {migration.name}\n"
-            "Please use `make apply-migrations` to apply."
+            f"To apply this migration, please run: make apply-migrations"
         )
 
-    # state = self.project_state((migration.app_label, migration.name), at_end=false)
-
     project_state = executor.loader.project_state(
-        nodes=(migration.app_label, migration.name),
+        nodes=[(migration.app_label, migration.name)],
         at_end=False,
     )
-    info("Running post-deployment migration:")
-    info(" ", migration.name)
+
+    click.secho("Running post-deployment migration:", fg="cyan")
+    click.secho(f"  {migration.name}", bold=True)
     with connection.schema_editor() as schema_editor:
         for op in migration.operations:
-            info("    *", op.describe(), end="... ")
+            click.echo(f"    * {op.describe()}... ", nl=False)
             new_state = project_state.clone()
 
-            op.state_forwards(migration.app_label, new_state)
-            op.database_forwards(
-                app_label=migration.app_label,
-                schema_editor=schema_editor,
-                from_state=project_state,
-                to_state=new_state,
-            )
-            info("OK")
+            try:
+                op.state_forwards(migration.app_label, new_state)
+                op.database_forwards(
+                    app_label=migration.app_label,
+                    schema_editor=schema_editor,
+                    from_state=project_state,
+                    to_state=new_state,
+                )
+            except BaseException:
+                click.secho("FAIL", fg="red", bold=True)
+                raise
+            else:
+                click.secho("OK", fg="green", bold=True)
 
             project_state = new_state
-    info("Migration complete.")
+    click.secho("Migration complete.", fg="cyan")
