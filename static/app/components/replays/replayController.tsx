6a1ce0ca0ed2fd657566576fc4d29cb77a391453
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-11-16 09:10:34 -0800

    Revert "style(replays): add padding for timeline zoom (#60061)"
    This reverts commit 4a5f193e0539159dae33047192feceffc1bc4ef1.

--- static/app/components/replays/replayController.tsx
+++ static/app/components/replays/replayController.tsx
@@ -161,7 +161,7 @@ function TimelineSizeBar() {
         aria-label={t('Zoom out')}
         disabled={timelineScale === 1}
       />
-      <span style={{padding: `${space(0.5)}`}}>
+      <span>
         {timelineScale}
         {t('x')}
       </span>
