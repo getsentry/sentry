f4de500a05b3123c2f5c94bccfe6b4e8efc233a0
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2023-11-16 14:25:42 -0800

    fix test'

--- static/app/gettingStartedDocs/javascript/nextjs.spec.tsx
+++ static/app/gettingStartedDocs/javascript/nextjs.spec.tsx
@@ -1,3 +1,5 @@
+import {Organization} from 'sentry-fixture/organization';
+
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
@@ -5,11 +7,22 @@ import {StepTitle} from 'sentry/components/onboarding/gettingStartedDoc/step';
 import {GettingStartedWithNextJs, steps} from './nextjs';
 
 describe('GettingStartedWithNextJs', function () {
+  const organization = Organization();
   it('renders doc correctly', function () {
-    render(<GettingStartedWithNextJs dsn="test-dsn" projectSlug="test-project" />);
+    render(
+      <GettingStartedWithNextJs
+        dsn="test-dsn"
+        projectSlug="test-project"
+        organization={organization}
+      />
+    );
 
     // Steps
-    for (const step of steps({dsn: 'test-dsn', projectSlug: 'test-project'})) {
+    for (const step of steps({
+      dsn: 'test-dsn',
+      projectSlug: 'test-project',
+      organization,
+    })) {
       expect(
         screen.getByRole('heading', {name: step.title ?? StepTitle[step.type]})
       ).toBeInTheDocument();
