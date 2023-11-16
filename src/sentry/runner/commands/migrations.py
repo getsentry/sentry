770f119c5b822064aa5f45b081653d12bc65327b
Buck Evan <112430378+bukzor-sentryio@users.noreply.github.com>
2023-11-16 15:24:06 -0600

    fix(cli): allow all kinds of "dangerous" migrations to run post-deployment (#60105)

--- src/sentry/runner/commands/migrations.py
+++ src/sentry/runner/commands/migrations.py
@@ -15,21 +15,44 @@ def migrations():
 @click.pass_context
 def run(ctx, app_name, migration_name):
     "Manually run a single data migration. Will error if migration is not data only."
+    del ctx  # assertion: unused argument
 
-    from django.db import connections
-    from django.db.migrations import RunPython
+    from django.db import connection, connections
     from django.db.migrations.executor import MigrationExecutor
 
     executor = MigrationExecutor(connections["default"])
     migration = executor.loader.get_migration_by_prefix(app_name, migration_name)
-    apps = executor.loader.project_state(nodes=[(app_name, migration.name)]).apps
+    if not getattr(migration, "is_dangerous", None):
+        raise click.ClickException(
+            f"This is not a post-deployment migration: {migration.name}\n"
+            f"To apply this migration, please run: make apply-migrations"
+        )
 
-    for op in migration.operations:
-        if not isinstance(op, RunPython):
-            raise click.ClickException(
-                "Migration must contain only RunPython ops, found: %s" % type(op).__name__
-            )
+    project_state = executor.loader.project_state(
+        nodes=[(migration.app_label, migration.name)],
+        at_end=False,
+    )
 
-    for op in migration.operations:
-        assert isinstance(op, RunPython)
-        op.code(apps, None)
+    click.secho("Running post-deployment migration:", fg="cyan")
+    click.secho(f"  {migration.name}", bold=True)
+    with connection.schema_editor() as schema_editor:
+        for op in migration.operations:
+            click.echo(f"    * {op.describe()}... ", nl=False)
+            new_state = project_state.clone()
+
+            try:
+                op.state_forwards(migration.app_label, new_state)
+                op.database_forwards(
+                    app_label=migration.app_label,
+                    schema_editor=schema_editor,
+                    from_state=project_state,
+                    to_state=new_state,
+                )
+            except BaseException:
+                click.secho("FAIL", fg="red", bold=True)
+                raise
+            else:
+                click.secho("OK", fg="green", bold=True)
+
+            project_state = new_state
+    click.secho("Migration complete.", fg="cyan")
