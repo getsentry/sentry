8886b15c1f3b3da0988188b6dd6e9139328922f6
NisanthanNanthakumar <nisanthan.nanthakumar@sentry.io>
2023-11-16 10:12:38 -0800

    unrevert(feat(eventuser)): Migrate IssuesByTagProcessor away from EventUser (#60071)
    ## Objective:
    Create the for_tags method in the EventUser dataclass. Migrates
    get_eventuser_callback to use the dataclass.
    
    Unreverts https://github.com/getsentry/sentry/pull/59672

--- src/sentry/analytics/events/__init__.py
+++ src/sentry/analytics/events/__init__.py
@@ -12,6 +12,7 @@ from .comment_webhooks import *  # noqa: F401,F403
 from .cron_monitor_created import *  # noqa: F401,F403
 from .eventuser_endpoint_request import *  # noqa: F401,F403
 from .eventuser_equality_check import *  # noqa: F401,F403
+from .eventuser_snuba_query import *  # noqa: F401,F403
 from .first_cron_checkin_sent import *  # noqa: F401,F403
 from .first_event_sent import *  # noqa: F401,F403
 from .first_feedback_sent import *  # noqa: F401,F403
