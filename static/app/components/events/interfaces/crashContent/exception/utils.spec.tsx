fbeff50ad6b0039812e824524ddcae192d3b48ae
17hogeju <17hogeju@gmail.com>
2023-11-20 11:10:52 -0800

    Use screen.getByRole

--- static/app/components/events/interfaces/crashContent/exception/utils.spec.tsx
+++ static/app/components/events/interfaces/crashContent/exception/utils.spec.tsx
@@ -1,13 +1,13 @@
-import {render} from 'sentry-test/reactTestingLibrary';
+import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import {Linkify} from 'sentry/components/events/interfaces/crashContent/exception/utils';
 
 describe('Linkify()', function () {
   it('does not alter text that does not contain urls', function () {
     const text = 'This is not a link';
-    const {container, getByText} = render(<Linkify exceptionText={text} />);
+    const {container} = render(<Linkify exceptionText={text} />);
 
-    expect(getByText(text)).toBeInTheDocument();
+    expect(screen.getByText(text)).toBeInTheDocument();
 
     // check if the text is directly within the container (which implies a fragment)
     expect(container.firstChild).toHaveTextContent(text);
@@ -23,9 +23,9 @@ describe('Linkify()', function () {
   it('applies links to text containing a single url', function () {
     const url = 'https://www.example.com';
     const text = `Go to ${url} to search.`;
-    const {container, getByText} = render(<Linkify exceptionText={text} />);
+    const {container} = render(<Linkify exceptionText={text} />);
 
-    const linkElement = getByText(url);
+    const linkElement = screen.getByRole('link', {name: url});
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute('href', url);
 
@@ -36,13 +36,13 @@ describe('Linkify()', function () {
     const url_1 = 'https://www.example.com';
     const url_2 = 'https://docs.sentry.io';
     const text = `Go to ${url_1} to search and ${url_2} for docs.`;
-    const {container, getByText} = render(<Linkify exceptionText={text} />);
+    const {container} = render(<Linkify exceptionText={text} />);
 
-    const linkElement = getByText(url_1);
+    const linkElement = screen.getByRole('link', {name: url_1});
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute('href', url_1);
 
-    const linkElement_2 = getByText(url_2);
+    const linkElement_2 = screen.getByRole('link', {name: url_2});
     expect(linkElement_2).toBeInTheDocument();
     expect(linkElement_2).toHaveAttribute('href', url_2);
 
@@ -52,9 +52,9 @@ describe('Linkify()', function () {
   it('applies links to text containing complex urls with query parameters and hashes', function () {
     const url = 'https://www.example.com/search?query=linkify&sort=recent#section2';
     const text = `Go to ${url}`;
-    const {container, getByText} = render(<Linkify exceptionText={text} />);
+    const {container} = render(<Linkify exceptionText={text} />);
 
-    const linkElement = getByText(url);
+    const linkElement = screen.getByRole('link', {name: url});
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute('href', url);
 
@@ -64,9 +64,10 @@ describe('Linkify()', function () {
   it('handles text containing uls of non-supported schemes', function () {
     const url = 'ftp://myname:hello@lenny/lucky.png';
     const text = `Go to ${url}`;
-    const {container, getByText} = render(<Linkify exceptionText={text} />);
+    const {container} = render(<Linkify exceptionText={text} />);
 
-    expect(getByText(text)).toBeInTheDocument();
+    const textElement = screen.getByText(text);
+    expect(textElement).toBeInTheDocument();
 
     // check if the text is directly within the container
     expect(container.firstChild).toHaveTextContent(text);
@@ -76,13 +77,13 @@ describe('Linkify()', function () {
     const url_1 = 'https://www.example.com';
     const url_2 = 'https://docs.sentry.io';
     const text = `${url_1} and ${url_2}`;
-    const {getByText} = render(<Linkify exceptionText={text} />);
+    render(<Linkify exceptionText={text} />);
 
-    const linkElement = getByText(url_1);
+    const linkElement = screen.getByRole('link', {name: url_1});
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute('href', url_1);
 
-    const linkElement_2 = getByText(url_2);
+    const linkElement_2 = screen.getByRole('link', {name: url_2});
     expect(linkElement_2).toBeInTheDocument();
     expect(linkElement_2).toHaveAttribute('href', url_2);
   });
@@ -91,9 +92,9 @@ describe('Linkify()', function () {
     const url = 'https://www.example.com';
     const longString = 'a b c d e f g h i j k l m n o p'.repeat(1000);
     const text = `Go to ${url} ${longString}`;
-    const {container, getByText} = render(<Linkify exceptionText={text} />);
+    const {container} = render(<Linkify exceptionText={text} />);
 
-    const linkElement = getByText(url);
+    const linkElement = screen.getByRole('link', {name: url});
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute('href', url);
 
@@ -101,9 +102,16 @@ describe('Linkify()', function () {
   });
 
   it('handles html/special characters in text input', function () {
-    const textWithHtml =
-      'Check out this link: <a href="https://www.example.com">https://www.example.com</a> & don\'t forget to visit us!';
+    const url = 'https://www.example.com';
+    const textWithHtml = `Check out this link: <a href="${url}">${url}</a> & don\'t forget to visit us!`;
     const {container} = render(<Linkify exceptionText={textWithHtml} />);
+
+    const linkElements = screen.getAllByRole('link', {name: url});
+    expect(linkElements).toHaveLength(2);
+
+    linkElements.forEach(linkElement => {
+      expect(linkElement).toHaveAttribute('href', url);
+    });
 
     expect(container).toHaveTextContent(textWithHtml);
   });
@@ -111,8 +119,8 @@ describe('Linkify()', function () {
   it('applies links to text containing urls of mixed casing', function () {
     const url = 'Http://ExAmPlE.com';
     const mixedCaseText = `check this link: ${url}`;
-    const {container, getByText} = render(<Linkify exceptionText={mixedCaseText} />);
-    const linkElement = getByText(url);
+    const {container} = render(<Linkify exceptionText={mixedCaseText} />);
+    const linkElement = screen.getByRole('link', {name: url});
     expect(linkElement).toBeInTheDocument();
     expect(linkElement).toHaveAttribute('href', url);
 
