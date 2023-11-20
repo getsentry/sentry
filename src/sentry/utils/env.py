5b31abaf546e648b6614e7c8a61254332dfb8c58
maxwelllwang <maxwell.wang@sentry.io>
2023-11-20 10:53:29 -0800

    add: additional logging for other auth methods

--- src/sentry/utils/env.py
+++ src/sentry/utils/env.py
@@ -41,28 +41,39 @@ def log_gcp_credentials_details(logger) -> None:
     if in_test_environment():
         return
 
-    credentials, project_id = default()
+    # Checking GOOGLE_APPLICATION_CREDENTIALS environment variable
+    adc_path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", "")
+    if adc_path:
+        with open(adc_path) as fp:
+            adc = json.load(fp)
 
-    if not credentials:
-        logger.error("gcp.credentials.notfound")
-        return
+            logger.info(
+                "gcp.credentials.file_found",
+                extra={
+                    "adc": adc,
+                },
+            )
 
-    logger.info(
-        "gcp.credentials.found",
-        extra={
-            "sanitized_credentials": {
-                "client_id": getattr(credentials, "client_id", None),
-                "expired": getattr(credentials, "expired", None),
-                "expiry": getattr(credentials, "expiry", None),
-                "granted_scopes": getattr(credentials, "granted_scopes", None),
-                "quota_project_id": getattr(credentials, "quota_project_id", None),
-                "scopes": getattr(credentials, "scopes", None),
-                "valid": getattr(credentials, "valid", None),
+    # Checking User credentials set up by using the Google Cloud CLI
+    cli_auth_path = "$HOME/.config/gcloud/application_default_credentials.json"
+    expanded_cli_auth_path = os.path.expandvars(cli_auth_path)
+
+    if os.path.exists(expanded_cli_auth_path):
+        logger.info(
+            "gcp.credentials.user_cli_found",
+            extra={
+                "user_cli": expanded_cli_auth_path,
             },
-            "project_id": project_id,
-        },
-    )
+        )
+    else:
+        logger.info(
+            "gcp.credentials.user_cli_not_found",
+            extra={
+                "user_cli": expanded_cli_auth_path,
+            },
+        )
 
+    # Checking The attached service account, returned by the metadata server
     try:
         # this will only work inside a GCP machine
         service_accounts = requests.get(
@@ -82,6 +93,30 @@ def log_gcp_credentials_details(logger) -> None:
         logger.info(
             "Unable to get service accounts from metadata server, this only works inside gke pod or gcp machine"
         )
+
+    # Checking using google-auth
+    credentials, project_id = default()
+
+    if not credentials:
+        logger.error("gcp.credentials.notfound")
+        return
+
+    logger.info(
+        "gcp.credentials.found",
+        extra={
+            "sanitized_credentials": {
+                "client_id": getattr(credentials, "client_id", None),
+                "expired": getattr(credentials, "expired", None),
+                "expiry": getattr(credentials, "expiry", None),
+                "granted_scopes": getattr(credentials, "granted_scopes", None),
+                "quota_project_id": getattr(credentials, "quota_project_id", None),
+                "scopes": getattr(credentials, "scopes", None),
+                "valid": getattr(credentials, "valid", None),
+                "service_account_email": getattr(credentials, "service_account_email", None),
+            },
+            "project_id": project_id,
+        },
+    )
 
 
 def is_split_db() -> bool:
