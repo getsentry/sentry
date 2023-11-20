07427aee8cd25a69849290ea8aa3e50db3414a55
17hogeju <17hogeju@gmail.com>
2023-11-20 10:26:52 -0800

    Add comments

--- static/app/components/events/interfaces/crashContent/exception/utils.tsx
+++ static/app/components/events/interfaces/crashContent/exception/utils.tsx
@@ -22,7 +22,7 @@ export function isFrameFilenamePathlike(frame: Frame): boolean {
   );
 }
 
-// need to add comments
+// Detects URLs in text and renders them with anchor tags
 export function Linkify({exceptionText}: {exceptionText?: string}): React.ReactElement {
   if (!exceptionText) {
     return <React.Fragment>{''}</React.Fragment>;
@@ -30,6 +30,19 @@ export function Linkify({exceptionText}: {exceptionText?: string}): React.ReactE
 
   const urlRegex =
     /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b(?:[-a-zA-Z0-9@:%_\+.~#?&\/=,\[\]]*)/gi;
+  // https?: Matches both "http" and "https"
+  // :\/\/: This is a literal match for "://"
+  // (?:www\.)?: Matches URLs with or without "www."
+  // [-a-zA-Z0-9@:%._\+~#=]{1,256}: Matches the domain name
+  //    It allows for a range of characters (letters, digits, and special characters)
+  //    The {1,256} specifies that these characters can occur anywhere from 1 to 256 times, which covers the range of typical domain name lengths
+  // \.: Matches the dot before the top-level domain (like ".com")
+  // [a-zA-Z0-9]{1,6}: Matches the top-level domain (like "com" or "org"). It's limited to letters and digits and can be between 1 and 6 characters long
+  // \b: Marks the end of the domain part of the URL
+  // (?:[-a-zA-Z0-9@:%_\+.~#?&\/=,\[\]]*): Matches the path or query parameters that can follow the domain in a URL
+  //    It includes a wide range of characters typically found in paths and query strings
+  // /gi: The regex will match all occurrences in the string, not just the first one
+  //    i makes the regex match both upper and lower case characters
 
   const parts = exceptionText.split(urlRegex);
   const urls = exceptionText.match(urlRegex);
@@ -42,6 +55,8 @@ export function Linkify({exceptionText}: {exceptionText?: string}): React.ReactE
         </a>
       ) : null;
 
+    // Combine the text part and its following URL into a React Fragment
+    // Each part of the text and each link is given a unique key for React's rendering optimization
     return [<React.Fragment key={`text-${index}`}>{part}</React.Fragment>, link];
   });
 
