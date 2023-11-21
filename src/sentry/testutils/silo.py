90ccc2257e5b88f1e613b2bf37505e9920efe5f2
Ryan Skonnord <ryan.skonnord@sentry.io>
2023-11-21 13:02:55 -0800

    test(hc): Change default test behavior to silo modes (#60291)
    Have all test cases tagged with a silo mode decorator
    (`@control_silo_test` or `@region_silo_test`) run only in that mode. The
    `stable` argument is now true by default, and the test is not run in
    monolith mode unless it has `include_monolith_run=True`.

--- src/sentry/testutils/silo.py
+++ src/sentry/testutils/silo.py
@@ -109,13 +109,15 @@ class SiloModeTestDecorator:
     def __call__(
         self,
         decorated_obj: Any = None,
-        stable: bool = False,
+        stable: bool = True,
         regions: Sequence[Region] = (),
+        include_monolith_run: bool = False,
     ) -> Any:
         mod = _SiloModeTestModification(
             silo_modes=self.silo_modes,
             regions=tuple(regions or _DEFAULT_TEST_REGIONS),
             stable=stable,
+            include_monolith_run=include_monolith_run,
         )
 
         return mod.apply if decorated_obj is None else mod.apply(decorated_obj)
@@ -128,11 +130,9 @@ class _SiloModeTestModification:
     silo_modes: frozenset[SiloMode]
     regions: tuple[Region, ...]
     stable: bool
+    include_monolith_run: bool
 
-    # The default values can be treated as switches for desired global behavior,
-    # when we're ready to change.
-    include_monolith_run: bool = True
-    run_original_class_in_silo_mode: bool = False
+    run_original_class_in_silo_mode: bool = True
 
     @contextmanager
     def test_config(self, silo_mode: SiloMode):
@@ -184,7 +184,7 @@ class _SiloModeTestModification:
         """
         if len(self.silo_modes) == 1:
             (silo_mode,) = self.silo_modes
-            if not self.include_monolith_run:
+            if not (self.include_monolith_run or settings.FORCE_SILOED_TESTS):
                 return silo_mode, ()
             if self.run_original_class_in_silo_mode:
                 return silo_mode, (SiloMode.MONOLITH,)
