8c9a864ec83f4837c9c7e6f85d6cf6fb3c1831f9
Mark Story <mark@mark-story.com>
2023-11-22 10:27:14 -0500

    fix(hybridcloud) Add missing silo annotation to view (#60419)
    The project event redirect view needs a silo annotation in order to be
    proxied through the api gateway
    
    Fixes HC-999

--- src/sentry/web/frontend/project_event.py
+++ src/sentry/web/frontend/project_event.py
@@ -1,16 +1,18 @@
 from django.http import Http404, HttpResponseRedirect
 from django.urls import reverse
 from rest_framework.request import Request
-from rest_framework.response import Response
 
 from sentry import eventstore
-from sentry.web.frontend.base import ProjectView
+from sentry.web.frontend.base import ProjectView, region_silo_view
 
 
+@region_silo_view
 class ProjectEventRedirect(ProjectView):
     required_scope = "event:read"
 
-    def handle(self, request: Request, organization, project, client_event_id) -> Response:
+    def handle(
+        self, request: Request, organization, project, client_event_id
+    ) -> HttpResponseRedirect:
         """
         Given a client event id and project, redirects to the event page
         """
@@ -22,9 +24,8 @@ class ProjectEventRedirect(ProjectView):
         if not event.group_id:
             raise Http404
 
-        return HttpResponseRedirect(
-            reverse(
-                "sentry-organization-event-detail",
-                args=[organization.slug, event.group_id, event.event_id],
-            )
+        path = reverse(
+            "sentry-organization-event-detail",
+            args=[organization.slug, event.group_id, event.event_id],
         )
+        return HttpResponseRedirect(organization.absolute_url(path))
