5dd0cd2fb4afd54434b5a8d9cf6e3eec781cf4e9
Ogi <86684834+obostjancic@users.noreply.github.com>
2023-11-23 11:57:29 +0100

    extract to util

--- static/app/views/ddm/widget.tsx
+++ static/app/views/ddm/widget.tsx
@@ -281,37 +281,58 @@ function getChartSeries(
     };
   });
 
-  const isBarChart = displayType === MetricDisplayType.BAR;
-  // copied over from dashboards
-  const palette = theme.charts.getColorPalette(series.length - 2).toReversed();
-  // we need to reverse the palette for bar charts
-  const colors = isBarChart ? palette.toReversed() : palette;
+  const colors = getChartColorPalette(displayType, series.length);
 
-  return (
-    series
-      // we need to sort the series by their values so that the colors in area chart do not overlap
-      .sort((a, b) => {
-        return Number(a.values?.[0]) > Number(b.values?.[0]) ? -1 : 1;
-      })
+  return sortSeries(series, displayType).map((item, i) => ({
+    seriesName: item.name,
+    unit,
+    color: colorFn(colors[i])
+      .alpha(hoveredLegend && hoveredLegend !== item.name ? 0.1 : 1)
+      .string(),
+    hidden: focusedSeries && focusedSeries !== item.name,
+    data: item.values.map((value, index) => ({
+      name: moment(data.intervals[index]).valueOf(),
+      value,
+    })),
+    transaction: item.transaction as string | undefined,
+    release: item.release as string | undefined,
+    emphasis: {
+      focus: 'series',
+    } as LineSeriesOption['emphasis'],
+  })) as Series[];
+}
 
-      .map((item, i) => ({
-        seriesName: item.name,
-        unit,
-        color: colorFn(colors[i])
-          .alpha(hoveredLegend && hoveredLegend !== item.name ? 0.1 : 1)
-          .string(),
-        hidden: focusedSeries && focusedSeries !== item.name,
-        data: item.values.map((value, index) => ({
-          name: moment(data.intervals[index]).valueOf(),
-          value,
-        })),
-        transaction: item.transaction as string | undefined,
-        release: item.release as string | undefined,
-        emphasis: {
-          focus: 'series',
-        } as LineSeriesOption['emphasis'],
-      })) as Series[]
-  );
+function sortSeries(
+  series: {
+    name: string;
+    release: string;
+    transaction: string;
+    values: (number | null)[];
+  }[],
+  displayType: MetricDisplayType
+) {
+  const sorted = series
+    // we need to sort the series by their values so that the colors in area chart do not overlap
+    // for now we are only sorting by the first value, but we might need to sort by the sum of all values
+    .sort((a, b) => {
+      return Number(a.values?.[0]) > Number(b.values?.[0]) ? -1 : 1;
+    });
+
+  if (displayType === MetricDisplayType.BAR) {
+    return sorted.toReversed();
+  }
+
+  return sorted;
+}
+
+function getChartColorPalette(displayType: MetricDisplayType, length: number) {
+  const palette = theme.charts.getColorPalette(length - 2);
+
+  if (displayType === MetricDisplayType.BAR) {
+    return palette;
+  }
+
+  return palette.toReversed();
 }
 
 function normalizeChartTimeParams(data: MetricsApiResponse) {
