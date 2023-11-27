91d472a194ea4b81618e76f9ab874d9252a015e0
ArthurKnaus <arthur.knaus@sentry.io>
2023-11-27 12:00:28 +0100

    fix(alerts): transaction metrics being available (#60584)
    Fix `useMetricsMeta` returning wrong use cases
    Fix `MriField` to reset to `DEFAULT_METRIC_ALERT_AGGREGATE` if no
    metrics are available.

--- static/app/views/alerts/rules/metric/utils/isCustomMetricAlert.tsx
+++ static/app/views/alerts/rules/metric/utils/isCustomMetricAlert.tsx
@@ -1,7 +1,7 @@
 import {fieldToMri} from 'sentry/utils/metrics';
 
 /**
- * Currently we can tell if an alert is a crash free alert by checking the aggregate for a MRI,
+ * Currently we can tell if an alert is a custom metric alert by checking the aggregate for a MRI,
  */
 export function isCustomMetricAlert(aggregate: string): boolean {
   return fieldToMri(aggregate).mri !== undefined;
