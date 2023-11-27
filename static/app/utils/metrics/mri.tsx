18199c83f4189e91a9755acd0317ece9d5714aa3
Ogi <86684834+obostjancic@users.noreply.github.com>
2023-11-27 17:14:27 +0100

    fix(ddm): invalid mri message (#60599)

--- static/app/utils/metrics/mri.tsx
+++ static/app/utils/metrics/mri.tsx
@@ -27,7 +27,7 @@ function _parseMRI(mri: MRI): ParsedMRI {
   const mriArray = mri.split(new RegExp(/[:/@]/));
 
   if (mriArray.length !== 4) {
-    Sentry.captureMessage(`Invalid MRI: ${mri}`);
+    Sentry.captureMessage(`Failed to parse invalid MRI`, {extra: {mri}});
     throw new Error('Invalid MRI');
   }
 
