68d3371099ebee329496169ad0346b98edf0d36d
Stephen Cefali <scefali@sentry.io>
2023-11-27 14:40:12 -0800

    ref(notifications): change how we enable slack settings for linking team (#60623)
    This PR stops using `notifications_service.update_settings` in Slack
    team linking and removes that function since we don't need it anymore.

--- src/sentry/services/hybrid_cloud/notifications/service.py
+++ src/sentry/services/hybrid_cloud/notifications/service.py
@@ -8,9 +8,7 @@ from typing import List, Mapping, MutableMapping, Optional, Set, Tuple
 from sentry.notifications.types import (
     NotificationScopeEnum,
     NotificationSettingEnum,
-    NotificationSettingOptionValues,
     NotificationSettingsOptionEnum,
-    NotificationSettingTypes,
 )
 from sentry.services.hybrid_cloud.actor import ActorType, RpcActor
 from sentry.services.hybrid_cloud.auth.model import AuthenticationContext
@@ -37,27 +35,13 @@ class NotificationsService(RpcService):
 
     @rpc_method
     @abstractmethod
-    def update_settings(
-        self,
-        *,
-        external_provider: ExternalProviders,
-        notification_type: NotificationSettingTypes,
-        setting_option: NotificationSettingOptionValues,
-        actor: RpcActor,
-        project_id: Optional[int] = None,
-        organization_id: Optional[int] = None,
-        skip_provider_updates: bool = False,
-        organization_id_for_team: Optional[int] = None,
-    ) -> None:
-        pass
-
-    @rpc_method
-    @abstractmethod
     def enable_all_settings_for_provider(
         self,
         *,
         external_provider: ExternalProviderEnum,
-        user_id: int,
+        user_id: Optional[int] = None,
+        team_id: Optional[int] = None,
+        types: Optional[List[NotificationSettingEnum]] = None,
     ) -> None:
         pass
 
