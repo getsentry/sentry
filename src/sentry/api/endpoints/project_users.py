065cfa229b2bac09739d86643ae0da6157fc18f1
Nisanthan Nanthakumar <nnutoronto2013@gmail.com>
2023-11-29 01:14:35 -0500

    feat(eventuser): Rm all references to EventUser model

--- src/sentry/api/endpoints/project_users.py
+++ src/sentry/api/endpoints/project_users.py
@@ -1,13 +1,12 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry import analytics, features
+from sentry import analytics
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases.project import ProjectEndpoint
-from sentry.api.paginator import CallbackPaginator, DateTimePaginator
+from sentry.api.paginator import CallbackPaginator
 from sentry.api.serializers import serialize
-from sentry.models.eventuser import EventUser as EventUser_model
 from sentry.types.ratelimit import RateLimit, RateLimitCategory
 from sentry.utils.eventuser import EventUser
 
@@ -51,37 +50,21 @@ class ProjectUsersEndpoint(ProjectEndpoint):
             except (ValueError, KeyError):
                 return Response([])
 
-        if not features.has("organizations:eventuser-from-snuba", project.organization):
-            queryset = EventUser_model.objects.filter(project_id=project.id)
-            if field and identifier:
-                queryset = queryset.filter(
-                    project_id=project.id,
-                    **{EventUser_model.attr_from_keyword(field): identifier},
-                )
+        keyword_filters = {}
+        if field and identifier:
+            keyword_filters[field] = [identifier]
 
-            return self.paginate(
-                request=request,
-                queryset=queryset,
-                order_by="-date_added",
-                paginator_cls=DateTimePaginator,
-                on_results=lambda x: serialize(x, request.user),
+        def callback(limit, offset):
+            return EventUser.for_projects(
+                projects=[project],
+                keyword_filters=keyword_filters,
+                result_limit=limit,
+                result_offset=offset,
             )
-        else:
-            keyword_filters = {}
-            if field and identifier:
-                keyword_filters[field] = [identifier]
 
-            def callback(limit, offset):
-                return EventUser.for_projects(
-                    projects=[project],
-                    keyword_filters=keyword_filters,
-                    result_limit=limit,
-                    result_offset=offset,
-                )
-
-            return self.paginate(
-                request=request,
-                paginator_cls=CallbackPaginator,
-                callback=callback,
-                on_results=lambda x: serialize(x, request.user),
-            )
+        return self.paginate(
+            request=request,
+            paginator_cls=CallbackPaginator,
+            callback=callback,
+            on_results=lambda x: serialize(x, request.user),
+        )
--- src/sentry/utils/eventuser.py
+++ src/sentry/utils/eventuser.py
@@ -21,9 +21,8 @@ from snuba_sdk import (
     Request,
 )
 
-from sentry import analytics, features
+from sentry import analytics
 from sentry.eventstore.models import Event
-from sentry.models.eventuser import EventUser as EventUser_model
 from sentry.models.project import Project
 from sentry.snuba.dataset import Dataset, EntityKey
 from sentry.utils.avatar import get_gravatar_url
@@ -287,9 +286,6 @@ class EventUser:
         Return a dictionary of {tag_value: event_user}.
         """
         projects = Project.objects.filter(id=project_id)
-
-        if not features.has("organizations:eventuser-from-snuba", projects[0].organization):
-            return EventUser_model.for_tags(project_id, values)
 
         result = {}
         keyword_filters: Dict[str, Any] = {}
