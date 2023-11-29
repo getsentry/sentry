2e985e31db69aa9d6fe462a6f97a32af987c8604
Jan Michael Auer <mail@jauer.org>
2023-11-29 16:11:45 +0100

    fix(options): Register delightful_metrics.metrics_summary_sample_rate (#60783)

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -1675,6 +1675,12 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
+register(
+    "delightful_metrics.metrics_summary_sample_rate",
+    default=0.0,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
+
 # SDK Crash Detection
 #
 # The project ID belongs to the sentry organization: https://sentry.sentry.io/projects/cocoa-sdk-crashes/?project=4505469596663808.
