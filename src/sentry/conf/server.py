6fc2a79915fb74a3d37e7fc0fc8b47e0c90630e0
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2023-11-29 16:25:47 -0500

    ref: add TypedDict for settings.LOGGING (#60819)
    <!-- Describe your PR here. -->

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -27,6 +27,7 @@ from urllib.parse import urlparse
 
 import sentry
 from sentry.conf.types.consumer_definition import ConsumerDefinition
+from sentry.conf.types.logging_config import LoggingConfig
 from sentry.conf.types.role_dict import RoleDict
 from sentry.conf.types.sdk_config import ServerSdkConfig
 from sentry.conf.types.topic_definition import TopicDefinition
@@ -1270,7 +1271,7 @@ BGTASKS = {
 # The loggers that it overrides are root and any in LOGGING.overridable.
 # Be very careful with this in a production system, because the celery
 # logger can be extremely verbose when given INFO or DEBUG.
-LOGGING = {
+LOGGING: LoggingConfig = {
     "default_level": "INFO",
     "version": 1,
     "disable_existing_loggers": True,
