efbb1bfe41fe64726511a513bb5be7d1bef6a756
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-11-28 17:21:40 -0800

    ref(replay): Refactor duplicate code between Network and A11y tabs (#60709)

--- static/app/views/replays/detail/network/details/index.tsx
+++ static/app/views/replays/detail/network/details/index.tsx
@@ -1,15 +1,9 @@
-import {Fragment, MouseEvent} from 'react';
-import styled from '@emotion/styled';
+import {Fragment} from 'react';
 
-import {Button} from 'sentry/components/button';
-import Stacked from 'sentry/components/replays/breadcrumbs/stacked';
-import {IconClose} from 'sentry/icons';
-import {t} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
+import DetailsSplitDivider from 'sentry/components/replays/virtualizedGrid/detailsSplitDivider';
 import type {SpanFrame} from 'sentry/utils/replays/types';
 import {useResizableDrawer} from 'sentry/utils/useResizableDrawer';
 import useUrlParams from 'sentry/utils/useUrlParams';
-import SplitDivider from 'sentry/views/replays/detail/layout/splitDivider';
 import NetworkDetailsContent from 'sentry/views/replays/detail/network/details/content';
 import NetworkDetailsTabs, {
   TabKey,
@@ -43,27 +37,14 @@ function NetworkDetails({
 
   return (
     <Fragment>
-      <StyledStacked>
-        <StyledNetworkDetailsTabs underlined={false} />
-        <StyledSplitDivider
-          data-is-held={isHeld}
-          data-slide-direction="updown"
-          onDoubleClick={onDoubleClick}
-          onMouseDown={onMouseDown}
-        />
-        <CloseButtonWrapper>
-          <Button
-            aria-label={t('Hide request details')}
-            borderless
-            icon={<IconClose isCircled size="sm" color="subText" />}
-            onClick={(e: MouseEvent) => {
-              e.preventDefault();
-              onClose();
-            }}
-            size="zero"
-          />
-        </CloseButtonWrapper>
-      </StyledStacked>
+      <DetailsSplitDivider
+        isHeld={isHeld}
+        onClose={onClose}
+        onDoubleClick={onDoubleClick}
+        onMouseDown={onMouseDown}
+      >
+        <NetworkDetailsTabs underlined={false} />
+      </DetailsSplitDivider>
 
       <NetworkDetailsContent
         isSetup={isSetup}
@@ -75,57 +56,5 @@ function NetworkDetails({
     </Fragment>
   );
 }
-
-const StyledStacked = styled(Stacked)`
-  position: relative;
-  border-top: 1px solid ${p => p.theme.border};
-  border-bottom: 1px solid ${p => p.theme.border};
-`;
-
-const StyledNetworkDetailsTabs = styled(NetworkDetailsTabs)`
-  /*
-  Use padding instead of margin so all the <li> will cover the <SplitDivider>
-  without taking 100% width.
-  */
-
-  & > li {
-    margin-right: 0;
-    padding-right: ${space(3)};
-    background: ${p => p.theme.surface400};
-    z-index: ${p => p.theme.zIndex.initial};
-  }
-  & > li:first-child {
-    padding-left: ${space(2)};
-  }
-  & > li:last-child {
-    padding-right: ${space(1)};
-  }
-
-  & > li > a {
-    padding-top: ${space(1)};
-    padding-bottom: ${space(0.5)};
-    height: 100%;
-    border-bottom: ${space(0.5)} solid transparent;
-  }
-`;
-
-const CloseButtonWrapper = styled('div')`
-  position: absolute;
-  right: 0;
-  height: 100%;
-  padding: ${space(1)};
-  z-index: ${p => p.theme.zIndex.initial};
-  display: flex;
-  align-items: center;
-`;
-
-const StyledSplitDivider = styled(SplitDivider)`
-  padding: ${space(0.75)};
-
-  :hover,
-  &[data-is-held='true'] {
-    z-index: ${p => p.theme.zIndex.initial};
-  }
-`;
 
 export default NetworkDetails;
