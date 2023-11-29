a3e0e75af0f76a51ddaa8e94357f26634655da9d
nathan hsieh <hsieh.nathan@gmail.com>
2023-11-29 10:50:50 -0800

    turn release card into a functional component

--- static/app/views/releases/list/releaseCard/index.tsx
+++ static/app/views/releases/list/releaseCard/index.tsx
@@ -1,4 +1,4 @@
-import {Component} from 'react';
+import {useMemo} from 'react';
 import styled from '@emotion/styled';
 import color from 'color';
 import {Location} from 'history';
@@ -56,34 +56,29 @@ type Props = {
   showReleaseAdoptionStages: boolean;
 };
 
-class ReleaseCard extends Component<Props> {
-  shouldComponentUpdate(nextProps: Props) {
-    // we don't want project health rows to reorder/jump while the whole card is loading
-    if (this.props.reloading && nextProps.reloading) {
-      return false;
-    }
+// TODO: change to functional component
+function ReleaseCard({
+  release,
+  organization,
+  activeDisplay,
+  location,
+  reloading,
+  selection,
+  showHealthPlaceholders,
+  isTopRelease,
+  getHealthData,
+  showReleaseAdoptionStages,
+}: Props) {
+  const {version, commitCount, lastDeploy, dateCreated, versionInfo} = release;
 
-    return true;
-  }
+  // TODO: fetch release threshold status
+  // Query for every release card??
+  // Query a full batch and match via card?
 
-  render() {
-    const {
-      release,
-      organization,
-      activeDisplay,
-      location,
-      reloading,
-      selection,
-      showHealthPlaceholders,
-      isTopRelease,
-      getHealthData,
-      showReleaseAdoptionStages,
-    } = this.props;
-    const {version, commitCount, lastDeploy, dateCreated, versionInfo} = release;
-
+  const [projectsToShow, projectsToHide] = useMemo(() => {
     // sort health rows inside release card alphabetically by project name,
     // show only the ones that are selected in global header
-    const [projectsToShow, projectsToHide] = partition(
+    return partition(
       release.projects.sort((a, b) => a.slug.localeCompare(b.slug)),
       p =>
         // do not filter for My Projects & All Projects
@@ -91,126 +86,124 @@ class ReleaseCard extends Component<Props> {
           ? selection.projects.includes(p.id)
           : true
     );
+  }, [release, selection.projects]);
 
-    function getHiddenProjectsTooltip() {
-      const limitedProjects = projectsToHide.map(p => p.slug).slice(0, 5);
-      const remainderLength = projectsToHide.length - limitedProjects.length;
+  const getHiddenProjectsTooltip = () => {
+    const limitedProjects = projectsToHide.map(p => p.slug).slice(0, 5);
+    const remainderLength = projectsToHide.length - limitedProjects.length;
 
-      if (remainderLength) {
-        limitedProjects.push(tn('and %s more', 'and %s more', remainderLength));
-      }
-
-      return limitedProjects.join(', ');
+    if (remainderLength) {
+      limitedProjects.push(tn('and %s more', 'and %s more', remainderLength));
     }
 
-    return (
-      <StyledPanel reloading={reloading ? 1 : 0} data-test-id="release-panel">
-        <ReleaseInfo>
-          <ReleaseInfoHeader>
-            <GlobalSelectionLink
-              to={{
-                pathname: `/organizations/${
-                  organization.slug
-                }/releases/${encodeURIComponent(version)}/`,
-                query: {project: getReleaseProjectId(release, selection)},
-              }}
+    return limitedProjects.join(', ');
+  };
+
+  return (
+    <StyledPanel reloading={reloading ? 1 : 0} data-test-id="release-panel">
+      <ReleaseInfo>
+        <ReleaseInfoHeader>
+          <GlobalSelectionLink
+            to={{
+              pathname: `/organizations/${
+                organization.slug
+              }/releases/${encodeURIComponent(version)}/`,
+              query: {project: getReleaseProjectId(release, selection)},
+            }}
+          >
+            <GuideAnchor
+              disabled={!isTopRelease || projectsToShow.length > 1}
+              target="release_version"
             >
-              <GuideAnchor
-                disabled={!isTopRelease || projectsToShow.length > 1}
-                target="release_version"
-              >
-                <VersionWrapper>
-                  <StyledVersion version={version} tooltipRawVersion anchor={false} />
-                </VersionWrapper>
-              </GuideAnchor>
-            </GlobalSelectionLink>
-            {commitCount > 0 && (
-              <ReleaseCardCommits release={release} withHeading={false} />
-            )}
-          </ReleaseInfoHeader>
-          <ReleaseInfoSubheader>
-            {versionInfo?.package && (
-              <PackageName>
-                <TextOverflow ellipsisDirection="left">
-                  {versionInfo.package}
-                </TextOverflow>
-              </PackageName>
-            )}
-            <TimeSince date={lastDeploy?.dateFinished || dateCreated} />
-            {lastDeploy?.dateFinished && ` \u007C ${lastDeploy.environment}`}
-          </ReleaseInfoSubheader>
-        </ReleaseInfo>
-
-        <ReleaseProjects>
-          <ReleaseProjectsHeader lightText>
-            <ReleaseProjectsLayout showReleaseAdoptionStages={showReleaseAdoptionStages}>
-              <ReleaseProjectColumn>{t('Project Name')}</ReleaseProjectColumn>
-              {showReleaseAdoptionStages && (
-                <AdoptionStageColumn>{t('Adoption Stage')}</AdoptionStageColumn>
-              )}
-              <AdoptionColumn>
-                <span>{t('Adoption')}</span>
-                <ReleaseCardStatsPeriod location={location} />
-              </AdoptionColumn>
-              <CrashFreeRateColumn>{t('Crash Free Rate')}</CrashFreeRateColumn>
-              <CrashesColumn>{t('Crashes')}</CrashesColumn>
-              <NewIssuesColumn>{t('New Issues')}</NewIssuesColumn>
-            </ReleaseProjectsLayout>
-          </ReleaseProjectsHeader>
-
-          <ProjectRows>
-            <Collapsible
-              expandButton={({onExpand, numberOfHiddenItems}) => (
-                <ExpandButtonWrapper>
-                  <Button priority="primary" size="xs" onClick={onExpand}>
-                    {tct('Show [numberOfHiddenItems] More', {numberOfHiddenItems})}
-                  </Button>
-                </ExpandButtonWrapper>
-              )}
-              collapseButton={({onCollapse}) => (
-                <CollapseButtonWrapper>
-                  <Button priority="primary" size="xs" onClick={onCollapse}>
-                    {t('Collapse')}
-                  </Button>
-                </CollapseButtonWrapper>
-              )}
-            >
-              {projectsToShow.map((project, index) => (
-                <ReleaseCardProjectRow
-                  key={`${release.version}-${project.slug}-row`}
-                  index={index}
-                  organization={organization}
-                  project={project}
-                  location={location}
-                  getHealthData={getHealthData}
-                  releaseVersion={release.version}
-                  activeDisplay={activeDisplay}
-                  showPlaceholders={showHealthPlaceholders}
-                  showReleaseAdoptionStages={showReleaseAdoptionStages}
-                  isTopRelease={isTopRelease}
-                  adoptionStages={release.adoptionStages}
-                />
-              ))}
-            </Collapsible>
-          </ProjectRows>
-
-          {projectsToHide.length > 0 && (
-            <HiddenProjectsMessage data-test-id="hidden-projects">
-              <Tooltip title={getHiddenProjectsTooltip()}>
-                <TextOverflow>
-                  {projectsToHide.length === 1
-                    ? tct('[number:1] hidden project', {number: <strong />})
-                    : tct('[number] hidden projects', {
-                        number: <strong>{projectsToHide.length}</strong>,
-                      })}
-                </TextOverflow>
-              </Tooltip>
-            </HiddenProjectsMessage>
+              <VersionWrapper>
+                <StyledVersion version={version} tooltipRawVersion anchor={false} />
+              </VersionWrapper>
+            </GuideAnchor>
+          </GlobalSelectionLink>
+          {commitCount > 0 && (
+            <ReleaseCardCommits release={release} withHeading={false} />
           )}
-        </ReleaseProjects>
-      </StyledPanel>
-    );
-  }
+        </ReleaseInfoHeader>
+        <ReleaseInfoSubheader>
+          {versionInfo?.package && (
+            <PackageName>
+              <TextOverflow ellipsisDirection="left">{versionInfo.package}</TextOverflow>
+            </PackageName>
+          )}
+          <TimeSince date={lastDeploy?.dateFinished || dateCreated} />
+          {lastDeploy?.dateFinished && ` \u007C ${lastDeploy.environment}`}
+        </ReleaseInfoSubheader>
+      </ReleaseInfo>
+
+      <ReleaseProjects>
+        <ReleaseProjectsHeader lightText>
+          <ReleaseProjectsLayout showReleaseAdoptionStages={showReleaseAdoptionStages}>
+            <ReleaseProjectColumn>{t('Project Name')}</ReleaseProjectColumn>
+            {showReleaseAdoptionStages && (
+              <AdoptionStageColumn>{t('Adoption Stage')}</AdoptionStageColumn>
+            )}
+            <AdoptionColumn>
+              <span>{t('Adoption')}</span>
+              <ReleaseCardStatsPeriod location={location} />
+            </AdoptionColumn>
+            <CrashFreeRateColumn>{t('Crash Free Rate')}</CrashFreeRateColumn>
+            <CrashesColumn>{t('Crashes')}</CrashesColumn>
+            <NewIssuesColumn>{t('New Issues')}</NewIssuesColumn>
+          </ReleaseProjectsLayout>
+        </ReleaseProjectsHeader>
+
+        <ProjectRows>
+          <Collapsible
+            expandButton={({onExpand, numberOfHiddenItems}) => (
+              <ExpandButtonWrapper>
+                <Button priority="primary" size="xs" onClick={onExpand}>
+                  {tct('Show [numberOfHiddenItems] More', {numberOfHiddenItems})}
+                </Button>
+              </ExpandButtonWrapper>
+            )}
+            collapseButton={({onCollapse}) => (
+              <CollapseButtonWrapper>
+                <Button priority="primary" size="xs" onClick={onCollapse}>
+                  {t('Collapse')}
+                </Button>
+              </CollapseButtonWrapper>
+            )}
+          >
+            {projectsToShow.map((project, index) => (
+              <ReleaseCardProjectRow
+                key={`${release.version}-${project.slug}-row`}
+                index={index}
+                organization={organization}
+                project={project}
+                location={location}
+                getHealthData={getHealthData}
+                releaseVersion={release.version}
+                activeDisplay={activeDisplay}
+                showPlaceholders={showHealthPlaceholders}
+                showReleaseAdoptionStages={showReleaseAdoptionStages}
+                isTopRelease={isTopRelease}
+                adoptionStages={release.adoptionStages}
+              />
+            ))}
+          </Collapsible>
+        </ProjectRows>
+
+        {projectsToHide.length > 0 && (
+          <HiddenProjectsMessage data-test-id="hidden-projects">
+            <Tooltip title={getHiddenProjectsTooltip()}>
+              <TextOverflow>
+                {projectsToHide.length === 1
+                  ? tct('[number:1] hidden project', {number: <strong />})
+                  : tct('[number] hidden projects', {
+                      number: <strong>{projectsToHide.length}</strong>,
+                    })}
+              </TextOverflow>
+            </Tooltip>
+          </HiddenProjectsMessage>
+        )}
+      </ReleaseProjects>
+    </StyledPanel>
+  );
 }
 
 const VersionWrapper = styled('div')`
