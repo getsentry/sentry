91c07dc70b97799885ec2a455e334bfb8df4cf96
David Herberth <david.herberth@sentry.io>
2023-12-04 09:59:01 +0100

    feat(relay): Introduce options to global config

--- src/sentry/relay/globalconfig.py
+++ src/sentry/relay/globalconfig.py
@@ -1,10 +1,20 @@
+import sentry.options
 from sentry.relay.config.measurements import get_measurements_config
 from sentry.utils import metrics
+
+# List of options to include in the global config.
+RELAY_OPTIONS = []
 
 
 @metrics.wraps("relay.globalconfig.get")
 def get_global_config():
     """Return the global configuration for Relay."""
+
+    options = dict()
+    for option in RELAY_OPTIONS:
+        options[option] = sentry.options.get(option)
+
     return {
         "measurements": get_measurements_config(),
+        "options": options,
     }
