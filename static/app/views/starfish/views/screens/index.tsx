c11469567ac30b78bf5016c322751caa05cea9bb
Nar Saynorath <nar.saynorath@sentry.io>
2023-12-05 14:55:06 -0500

    feat(app-start): Add top screens with cold/warm start data
    Makes queries to Discover to retrieve top screens and their cold/warm
    starts and displays it in a table. I reused the screens table because
    the functionality was largely the same, but instead of hardcoding the
    column names into the component I made that a prop so we can customize
    the behaviour.
    
    We will likely do the same with custom rendering since there is logic
    for linking to the screen details page, but we'll change that when the
    page is developed.

--- static/app/views/starfish/views/screens/index.tsx
+++ static/app/views/starfish/views/screens/index.tsx
@@ -50,6 +50,8 @@ export enum YAxis {
 }
 
 export const TOP_SCREENS = 5;
+const MAX_CHART_RELEASE_CHARS = 12;
+const MAX_TABLE_RELEASE_CHARS = 15;
 
 export const YAXIS_COLUMNS: Readonly<Record<YAxis, string>> = {
   [YAxis.WARM_START]: 'avg(measurements.app_start_warm)',
@@ -269,6 +271,14 @@ export function ScreensView({yAxes, additionalFilters, chartHeight}: Props) {
     });
   }
 
+  const truncatedPrimary = formatVersionAndCenterTruncate(
+    primaryRelease ?? '',
+    MAX_TABLE_RELEASE_CHARS
+  );
+  const truncatedSecondary = formatVersionAndCenterTruncate(
+    secondaryRelease ?? '',
+    MAX_TABLE_RELEASE_CHARS
+  );
   const derivedQuery = getTransactionSearchQuery(location, tableEventView.query);
 
   const tableSearchFilters = new MutableSearch(['transaction.op:ui.load']);
@@ -290,9 +300,15 @@ export function ScreensView({yAxes, additionalFilters, chartHeight}: Props) {
                   subtitle: primaryRelease
                     ? t(
                         '%s v. %s',
-                        formatVersionAndCenterTruncate(primaryRelease, 12),
+                        formatVersionAndCenterTruncate(
+                          primaryRelease,
+                          MAX_CHART_RELEASE_CHARS
+                        ),
                         secondaryRelease
-                          ? formatVersionAndCenterTruncate(secondaryRelease, 12)
+                          ? formatVersionAndCenterTruncate(
+                              secondaryRelease,
+                              MAX_CHART_RELEASE_CHARS
+                            )
                           : ''
                       )
                     : '',
@@ -324,9 +340,15 @@ export function ScreensView({yAxes, additionalFilters, chartHeight}: Props) {
                     subtitle: primaryRelease
                       ? t(
                           '%s v. %s',
-                          formatVersionAndCenterTruncate(primaryRelease, 12),
+                          formatVersionAndCenterTruncate(
+                            primaryRelease,
+                            MAX_CHART_RELEASE_CHARS
+                          ),
                           secondaryRelease
-                            ? formatVersionAndCenterTruncate(secondaryRelease, 12)
+                            ? formatVersionAndCenterTruncate(
+                                secondaryRelease,
+                                MAX_CHART_RELEASE_CHARS
+                              )
                             : ''
                         )
                       : '',
@@ -366,12 +388,32 @@ export function ScreensView({yAxes, additionalFilters, chartHeight}: Props) {
         data={topTransactionsData}
         isLoading={topTransactionsLoading}
         pageLinks={pageLinks}
+        columnNameMap={{
+          transaction: t('Screen'),
+          [`avg_if(measurements.time_to_initial_display,release,${primaryRelease})`]: t(
+            'TTID (%s)',
+            truncatedPrimary
+          ),
+          [`avg_if(measurements.time_to_initial_display,release,${secondaryRelease})`]: t(
+            'TTID (%s)',
+            truncatedSecondary
+          ),
+          [`avg_if(measurements.time_to_full_display,release,${primaryRelease})`]: t(
+            'TTFD (%s)',
+            truncatedPrimary
+          ),
+          [`avg_if(measurements.time_to_full_display,release,${secondaryRelease})`]: t(
+            'TTFD (%s)',
+            truncatedSecondary
+          ),
+          'count()': t('Total Count'),
+        }}
       />
     </div>
   );
 }
 
-function getFreeTextFromQuery(query: string) {
+export function getFreeTextFromQuery(query: string) {
   const conditions = new MutableSearch(query);
   const transactionValues = conditions.getFilterValues('transaction');
   if (transactionValues.length) {
