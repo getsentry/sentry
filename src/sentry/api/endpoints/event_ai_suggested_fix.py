65230f21b4dcb53eda860c68d97ddd3b50ac3bd1
Tillman Elser <trillville@users.noreply.github.com>
2023-12-05 10:15:07 -0800

    bump to 16k model

--- src/sentry/api/endpoints/event_ai_suggested_fix.py
+++ src/sentry/api/endpoints/event_ai_suggested_fix.py
@@ -258,7 +258,7 @@ def describe_event_for_ai(event, model):
     return data
 
 
-def suggest_fix(event_data, model="gpt-3.5-turbo", stream=False):
+def suggest_fix(event_data, model="gpt-3.5-turbo-16k", stream=False):
     """Runs an OpenAI request to suggest a fix."""
     prompt = PROMPT.replace("___FUN_PROMPT___", random.choice(FUN_PROMPT_CHOICES))
     event_info = describe_event_for_ai(event_data, model=model)
