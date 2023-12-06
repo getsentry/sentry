b5128228e1e6b73fa12ecd0358d39a6ee41bd201
Hubert Deng <hubert.deng@sentry.io>
2023-12-05 16:06:39 -0800

    chore(relocation): 23.11.2 Release Tests, now fixed (#61204)
    follow up to https://github.com/getsentry/sentry/pull/60900

--- tests/sentry/backup/test_releases.py
+++ tests/sentry/backup/test_releases.py
@@ -97,6 +97,17 @@ class ReleaseTests(BackupTestCase):
             # Return the export so that we can ensure that all models were seen.
             return exported
 
+    def test_at_23_11_2(self):
+        with tempfile.TemporaryDirectory() as tmp_dir:
+            _, snapshot_refval = read_snapshot_file(self.get_snapshot_path("23.11.2"))
+            snapshot_data = yaml.safe_load(snapshot_refval)
+            tmp_path = Path(tmp_dir).joinpath(f"{self._testMethodName}.json")
+            with open(tmp_path, "w") as f:
+                json.dump(snapshot_data, f)
+
+            with open(tmp_path, "rb") as f:
+                import_in_global_scope(f, printer=NOOP_PRINTER)
+
     def test_at_23_11_1(self):
         with tempfile.TemporaryDirectory() as tmp_dir:
             _, snapshot_refval = read_snapshot_file(self.get_snapshot_path("23.11.1"))
