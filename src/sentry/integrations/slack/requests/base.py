6e441123a248cb9e9b4f03f04b33937be9cf1f11
Leander Rodrigues <leander.rodrigues@sentry.io>
2023-12-06 11:15:54 -0500

    Revert "ref(hybrid-cloud): Add debugging logs around Slack webhook authentication" (#61251)
    Reverts getsentry/sentry#60803 - remove logging as debugging has been
    completed

--- src/sentry/integrations/slack/requests/base.py
+++ src/sentry/integrations/slack/requests/base.py
@@ -11,7 +11,6 @@ from sentry.services.hybrid_cloud.identity import RpcIdentity, identity_service
 from sentry.services.hybrid_cloud.integration import RpcIntegration, integration_service
 from sentry.services.hybrid_cloud.user import RpcUser
 from sentry.services.hybrid_cloud.user.service import user_service
-from sentry.silo.base import SiloMode
 
 from ..utils import check_signing_secret, logger
 
@@ -168,18 +167,6 @@ class SlackRequest:
         elif verification_token and self._check_verification_token(verification_token):
             return
 
-        # HACK(Leander): Dangerous logging, only used for testing hybrid cloud proxying
-        if SiloMode.get_current_mode() == SiloMode.REGION:
-            logger.error(
-                "slack-region-auth-failure",
-                extra={
-                    "signing_secret_opt": signing_secret,
-                    "verification_token_opt": verification_token,
-                    "request_data": self.request.data,
-                    **self.logging_data,
-                },
-            )
-
         # unfortunately, we can't know which auth was supposed to succeed
         self._error("slack.action.auth")
         raise SlackRequestError(status=status_.HTTP_401_UNAUTHORIZED)
@@ -189,9 +176,6 @@ class SlackRequest:
         timestamp = self.request.META.get("HTTP_X_SLACK_REQUEST_TIMESTAMP")
         if not (signature and timestamp):
             return False
-
-        if SiloMode.get_current_mode() == SiloMode.REGION:
-            self._info("slack-region-valid-headers")
 
         return check_signing_secret(signing_secret, self.request.body, timestamp, signature)
 
