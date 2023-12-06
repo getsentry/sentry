a8f496e4ca71ed515d0d93a8ff5c33d619132ba9
Bartek Ogryczak <bartek.ogryczak@sentry.io>
2023-12-05 18:51:36 -0800

    remove exc_info=False and noqa comment

--- src/sentry/integrations/pipeline.py
+++ src/sentry/integrations/pipeline.py
@@ -72,7 +72,7 @@ def is_violating_region_restriction(organization_id: int, integration_id: int):
     try:
         mapping = OrganizationMapping.objects.get(organization_id=organization_id)
     except OrganizationMapping.DoesNotExist:
-        logger.exception("mapping_missing", extra=logger_extra, exc_info=False)  # noqa: LOG007
+        logger.exception("mapping_missing", extra=logger_extra)
         return True
 
     return mapping.region_name not in region_names
