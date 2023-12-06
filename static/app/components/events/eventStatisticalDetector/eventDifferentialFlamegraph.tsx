94aa244cb7b340d3489b4cd3046aebf7823366b3
JonasBa <jonas@badalic.com>
2023-12-06 15:21:28 -0500

    feat(profiling): update copy

--- static/app/components/events/eventStatisticalDetector/eventDifferentialFlamegraph.tsx
+++ static/app/components/events/eventStatisticalDetector/eventDifferentialFlamegraph.tsx
@@ -579,8 +579,8 @@ function DifferentialFlamegraphExplanationBar(
     <DifferentialFlamegraphExplanationBarContainer>
       <div>
         {props.negated
-          ? `Flamegraph is showing how the stack population will change.`
-          : `Flamegraph is showing how the stack population has changed.`}
+          ? `Flamegraph is showing how stack frequency will change.`
+          : `Flamegraph is showing how stack frequency has changed.`}
       </div>
       <DifferentialFlamegraphLegend />
     </DifferentialFlamegraphExplanationBarContainer>
