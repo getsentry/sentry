c4b6f0a0343514b51b87b47eca435641273bc2ac
ArthurKnaus <arthur.knaus@sentry.io>
2023-12-07 10:06:18 +0100

    feat(ddm): Add minimize tray button (#61257)
    Add base `SplitPanel` component.
    Add minimize button to DDM tray. 
    
    - part of https://github.com/getsentry/sentry/issues/60076
    
    ---------
    
    Co-authored-by: Ryan Albrecht <ryan.albrecht@sentry.io>

--- static/app/views/ddm/trayContent.tsx
+++ static/app/views/ddm/trayContent.tsx
@@ -1,31 +1,50 @@
+import {useContext, useState} from 'react';
 import styled from '@emotion/styled';
 
+import {Button} from 'sentry/components/button';
 import EmptyMessage from 'sentry/components/emptyMessage';
+import {SplitPanelContext} from 'sentry/components/splitPanel';
 import {TabList, Tabs} from 'sentry/components/tabs';
-import {IconSearch} from 'sentry/icons';
+import {IconChevron, IconSearch} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
+import {CodeLocations} from 'sentry/views/ddm/codeLocations';
 import {useDDMContext} from 'sentry/views/ddm/context';
 import {TraceTable} from 'sentry/views/ddm/traceTable';
 
+enum Tab {
+  SAMPLES = 'samples',
+  CODE_LOCATIONS = 'codeLocations',
+}
+
 export function TrayContent() {
   const {selectedWidgetIndex, widgets} = useDDMContext();
+  const [selectedTab, setSelectedTab] = useState(Tab.SAMPLES);
+  const {isMaximized, maximiseSize, resetSize} = useContext(SplitPanelContext);
+  // the tray is minimized when the main content is maximized
+  const trayIsMinimized = isMaximized;
   const selectedWidget = widgets[selectedWidgetIndex];
 
   return (
     <TrayWrapper>
       <Header>
         <Title>{selectedWidget?.mri || t('Choose a metric to display data')}</Title>
-        {/* TODO(aknaus): Add collapse toggle */}
+        <ToggleButton
+          size="xs"
+          isMinimized={trayIsMinimized}
+          icon={<IconChevron size="xs" />}
+          onClick={trayIsMinimized ? resetSize : maximiseSize}
+          aria-label={trayIsMinimized ? t('show') : t('hide')}
+        />
       </Header>
-      <Tabs defaultValue="samples">
+      <Tabs value={selectedTab} onChange={setSelectedTab}>
         <StyledTabList>
-          <TabList.Item key="samples">{t('Samples')}</TabList.Item>
-          <TabList.Item key="codeLocations">{t('Code Location')}</TabList.Item>
+          <TabList.Item key={Tab.SAMPLES}>{t('Samples')}</TabList.Item>
+          <TabList.Item key={Tab.CODE_LOCATIONS}>{t('Code Location')}</TabList.Item>
         </StyledTabList>
       </Tabs>
       <ContentWrapper>
-        {!selectedWidget.mri ? (
+        {!selectedWidget?.mri ? (
           <CenterContent>
             <EmptyMessage
               style={{margin: 'auto'}}
@@ -34,12 +53,14 @@ export function TrayContent() {
               description={t('Choose a metric to display data.')}
             />
           </CenterContent>
-        ) : (
+        ) : selectedTab === Tab.SAMPLES ? (
           <TraceTable
             // Force re-render when selectedWidget changes so the mocked data updates
             // TODO: remove this when we have real data
             key={selectedWidget.mri}
           />
+        ) : (
+          <CodeLocations mri={selectedWidget.mri} />
         )}
       </ContentWrapper>
     </TrayWrapper>
@@ -69,13 +90,20 @@ const Title = styled('div')`
   font-weight: bold;
 `;
 
+const ToggleButton = styled(Button)<{isMinimized}>`
+  & svg {
+    transform: rotate(${p => (p.isMinimized ? '0deg' : '180deg')});
+  }
+`;
+
 const StyledTabList = styled(TabList)`
   padding: 0 ${space(4)};
   background-color: ${p => p.theme.backgroundSecondary};
 `;
 
 const ContentWrapper = styled('div')`
-  padding: ${space(0)} ${space(4)};
+  position: relative;
+  padding: ${space(2)} ${space(4)};
   overflow: auto;
 `;
 
