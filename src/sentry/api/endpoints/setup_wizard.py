8e7c5c7c45954392aaf7b7a3eb3b1d54e05c1560
Lukas Stracke <lukas.stracke@sentry.io>
2023-12-07 10:42:12 +0100

    chore(api-docs): Set SetupWizard endpoints to Private

--- src/sentry/api/endpoints/setup_wizard.py
+++ src/sentry/api/endpoints/setup_wizard.py
@@ -22,8 +22,8 @@ SETUP_WIZARD_CACHE_TIMEOUT = 600
 class SetupWizard(Endpoint):
     owner = ApiOwner.WEB_FRONTEND_SDKS
     publish_status = {
-        "DELETE": ApiPublishStatus.UNKNOWN,
-        "GET": ApiPublishStatus.UNKNOWN,
+        "DELETE": ApiPublishStatus.PRIVATE,
+        "GET": ApiPublishStatus.PRIVATE,
     }
     permission_classes = ()
 
