76134f988e86da97cc10e937c83dd39f4acae976
isabellaenriquez <isabella.enriquez@sentry.io>
2023-12-06 16:23:54 -0800

    logger.exception includes exception already

--- src/sentry/issues/issue_velocity.py
+++ src/sentry/issues/issue_velocity.py
@@ -139,10 +139,10 @@ def calculate_threshold(project: Project) -> Optional[float]:
 
     try:
         return result[0][THRESHOLD_QUANTILE["name"]]
-    except Exception as error:
+    except Exception:
         logger.exception(
             "Unexpected shape for threshold query results",
-            extra={"project_id": project.id, "result": json.dumps(result), "error": error},
+            extra={"project_id": project.id, "result": json.dumps(result)},
         )
         return None
 
