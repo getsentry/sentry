ce6f2f23dd89210a18e3c890b1ad6dee69ed3829
Nisanthan Nanthakumar <nnutoronto2013@gmail.com>
2023-12-07 14:41:11 -0800

    feat(eventuser): Drop eventuser-from-snuba feature flag

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1526,8 +1526,6 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     "organizations:escalating-metrics-backend": False,
     # Enable attaching arbitrary files to events.
     "organizations:event-attachments": True,
-    # Enable querying Snuba to get the EventUser
-    "organizations:eventuser-from-snuba": True,
     # Enable the frontend to request from region & control silo domains.
     "organizations:frontend-domainsplit": False,
     # Enable disabling gitlab integrations when broken is detected
