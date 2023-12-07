71ed3a338cca0b33c33da1c30e641535084f8faf
Ryan Albrecht <ryan.albrecht@sentry.io>
2023-12-07 15:09:20 -0800

    extract main feedback logic into a hook and share it between the floating, and button based feedbacks

--- static/app/components/feedback/widget/feedbackWidgetButton.tsx
+++ static/app/components/feedback/widget/feedbackWidgetButton.tsx
@@ -1,36 +1,13 @@
-import {useEffect, useRef} from 'react';
-import {Feedback, getCurrentHub} from '@sentry/react';
+import {useRef} from 'react';
 
 import {Button} from 'sentry/components/button';
+import useFeedbackWidget from 'sentry/components/feedback/widget/useFeedbackWidget';
 import {IconMegaphone} from 'sentry/icons/iconMegaphone';
 import {t} from 'sentry/locale';
-import ConfigStore from 'sentry/stores/configStore';
-import {useLegacyStore} from 'sentry/stores/useLegacyStore';
 
 export default function FeedbackWidgetButton() {
   const buttonRef = useRef<HTMLButtonElement>(null);
-  const config = useLegacyStore(ConfigStore);
-  const hub = getCurrentHub();
-  const feedback = hub.getIntegration(Feedback);
-
-  useEffect(() => {
-    if (!buttonRef.current || !feedback) {
-      return undefined;
-    }
-
-    const widget = feedback.attachTo(buttonRef.current, {
-      colorScheme: config.theme === 'dark' ? 'dark' : 'light',
-      formTitle: t('Give Feedback'),
-      submitButtonLabel: t('Send Feedback'),
-      messagePlaceholder: t('What did you expect?'),
-    });
-
-    return () => {
-      if (widget && feedback) {
-        feedback.removeWidget(widget);
-      }
-    };
-  }, [config.theme, feedback]);
+  const feedback = useFeedbackWidget({buttonRef});
 
   // Do not show button if Feedback integration is not enabled
   if (!feedback) {
--- static/app/components/feedback/widget/floatingFeedbackWidget.tsx
+++ static/app/components/feedback/widget/floatingFeedbackWidget.tsx
@@ -1,38 +1,15 @@
-import {useEffect} from 'react';
 import {css, Global} from '@emotion/react';
-import {Feedback, getCurrentHub} from '@sentry/react';
 
-import ConfigStore from 'sentry/stores/configStore';
-import {useLegacyStore} from 'sentry/stores/useLegacyStore';
+import useFeedbackWidget from 'sentry/components/feedback/widget/useFeedbackWidget';
 import theme from 'sentry/utils/theme';
 
 /**
  * Use this to display the Feedback widget in certain routes/components
  */
 export default function FloatingFeedbackWidget() {
-  const config = useLegacyStore(ConfigStore);
-  const hub = getCurrentHub();
-  const feedback = hub.getIntegration(Feedback);
+  const feedback = useFeedbackWidget({});
 
-  useEffect(() => {
-    if (!feedback) {
-      return undefined;
-    }
-
-    const widget = feedback?.createWidget({
-      colorScheme: config.theme === 'dark' ? 'dark' : 'light',
-      buttonLabel: 'Give Feedback',
-      submitButtonLabel: 'Send Feedback',
-      messagePlaceholder: 'What did you expect?',
-      formTitle: 'Give Feedback',
-    });
-
-    return () => {
-      feedback?.removeWidget(widget);
-    };
-  }, [config.theme, feedback]);
-
-  // Do not do anything if Feedback integration is not enabled
+  // No need for global styles if Feedback integration is not enabled
   if (!feedback) {
     return null;
   }
