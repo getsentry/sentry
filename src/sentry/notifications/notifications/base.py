5f3e41e3fd3c9633d58cc9d87e06c2565c1e280c
Stephen Cefali <scefali@sentry.io>
2023-12-07 14:39:18 -0800

    ref(notifications): remove more notification constants (#61374)
    Removing code we don't need anymore. Had to copy over
    `NotificationScopeType` to a migration because it was importing from
    core Sentry (it shouldn't do that).

--- src/sentry/notifications/notifications/base.py
+++ src/sentry/notifications/notifications/base.py
@@ -10,12 +10,7 @@ import sentry_sdk
 from sentry import analytics
 from sentry.db.models import Model
 from sentry.models.environment import Environment
-from sentry.notifications.types import (
-    NOTIFICATION_SETTING_TYPES,
-    NotificationSettingEnum,
-    NotificationSettingTypes,
-    UnsubscribeContext,
-)
+from sentry.notifications.types import NotificationSettingEnum, UnsubscribeContext
 from sentry.notifications.utils.actions import MessageAction
 from sentry.services.hybrid_cloud.actor import ActorType, RpcActor
 from sentry.types.integrations import EXTERNAL_PROVIDERS, ExternalProviders
@@ -41,16 +36,6 @@ class BaseNotification(abc.ABC):
     def __init__(self, organization: Organization, notification_uuid: str | None = None):
         self.organization = organization
         self.notification_uuid = notification_uuid if notification_uuid else str(uuid.uuid4())
-
-    # TODO(Steve): Remove notification_setting_type
-    @property
-    def notification_setting_type(self) -> NotificationSettingTypes | None:
-        if self.notification_setting_type_enum is not None:
-            # find the matching NotificationSettingTypes
-            for key, value in NOTIFICATION_SETTING_TYPES.items():
-                if value == self.notification_setting_type_enum.value:
-                    return key
-        return None
 
     @property
     def from_email(self) -> str | None:
@@ -254,8 +239,8 @@ class BaseNotification(abc.ABC):
         )
 
     def get_participants(self) -> Mapping[ExternalProviders, Iterable[RpcActor]]:
-        # need a notification_setting_type to call this function
-        if not self.notification_setting_type:
+        # need a notification_setting_type_enum to call this function
+        if not self.notification_setting_type_enum:
             raise NotImplementedError
 
         available_providers = self.get_notification_providers()
