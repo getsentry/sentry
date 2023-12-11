88955f11efb0d725c1b11f802a7734383459edc7
Scott Cooper <scttcper@gmail.com>
2023-12-11 11:05:20 -0800

    feat(issues): Move resources section for hardcoded solutions (#61472)

--- static/app/utils/issueTypeConfig/errorConfig.tsx
+++ static/app/utils/issueTypeConfig/errorConfig.tsx
@@ -1,6 +1,5 @@
 import {t} from 'sentry/locale';
-import {ErrorType} from 'sentry/types';
-import {
+import type {
   IssueCategoryConfigMapping,
   IssueTypeConfig,
 } from 'sentry/utils/issueTypeConfig/types';
@@ -25,12 +24,23 @@ export const errorConfig: IssueCategoryConfigMapping = {
   },
 };
 
-export const errorTypeConfigMap: Record<ErrorType, Partial<IssueTypeConfig>> = {
-  [ErrorType.CHUNK_LOAD_ERROR]: {
+const enum ErrorHelpType {
+  CHUNK_LOAD_ERROR = 'chunk_load_error',
+}
+
+const errorHelpTypeResourceMap: Record<
+  ErrorHelpType,
+  Pick<IssueTypeConfig, 'resources'>
+> = {
+  [ErrorHelpType.CHUNK_LOAD_ERROR]: {
     resources: {
-      description: t(
-        'ChunkLoadErrors occur when the JavaScript chunks (bundles) that an application is trying to load encounter issues during the loading process. Some common causes are dynamic imports, version mismatching, and code splitting issues. To learn more about how to fix ChunkLoadErrors, check out these resources:'
-      ),
+      // Not attempting translation
+      description: `While we hoped to fill this page with tons of useful info...we'll cut to the chase and
+provide some high level context that's likely more helpful for this error type.
+ChunkLoadErrors occur when the JavaScript chunks (bundles) that an application is trying to
+load encounter issues during the loading process. Some common causes are dynamic imports,
+version mismatching, and code splitting issues. To learn more about how to fix ChunkLoadErrors,
+check out the following:`,
       links: [
         {
           text: t('How to fix ChunkLoadErrors'),
@@ -41,3 +51,13 @@ export const errorTypeConfigMap: Record<ErrorType, Partial<IssueTypeConfig>> = {
     },
   },
 };
+
+export function getErrorHelpResource(
+  title: string
+): Pick<IssueTypeConfig, 'resources'> | null {
+  if (title.includes('ChunkLoadError')) {
+    return errorHelpTypeResourceMap[ErrorHelpType.CHUNK_LOAD_ERROR];
+  }
+
+  return null;
+}
