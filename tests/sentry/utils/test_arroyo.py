7ff588540f2fdb69526b42e7c39fe0ca28428743
Lyn <lyn.nagara@gmail.com>
2023-12-11 13:32:30 -0800

    rm test

--- tests/sentry/utils/test_arroyo.py
+++
@@ -1,9 +0,0 @@
-from sentry.utils.arroyo import get_reusable_multiprocessing_pool
-
-
-def test_reusable_multiprocessing_pool() -> None:
-    pool1 = get_reusable_multiprocessing_pool(1, None)
-    pool2 = get_reusable_multiprocessing_pool(1, None)
-    assert id(pool1) == id(pool2)
-    pool3 = get_reusable_multiprocessing_pool(2, None)
-    assert id(pool3) != id(pool1)
