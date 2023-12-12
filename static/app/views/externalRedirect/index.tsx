78139ba68fe1e42b993fb1060141aed91381bbb9
17hogeju <17hogeju@gmail.com>
2023-12-12 10:42:52 -0800

    Style changes

--- static/app/views/externalRedirect/index.tsx
+++ static/app/views/externalRedirect/index.tsx
@@ -1,83 +1,59 @@
-import {Component} from 'react';
+import {useEffect, useState} from 'react';
 import styled from '@emotion/styled';
 
 import Panel from 'sentry/components/panels/panel';
 import {tct} from 'sentry/locale';
 
-interface ExternalRedirectState {
-  count: number;
-  redirectUrl: string | null;
-}
+function ExternalRedirect() {
+  const [count, setCount] = useState<number>(5);
+  const [redirectUrl, setRedirectUrl] = useState<string | null>(null);
 
-class ExternalRedirect extends Component<{}, ExternalRedirectState> {
-  constructor(props: {}) {
-    super(props);
-    this.state = {
-      count: 5,
-      redirectUrl: null,
-    };
-  }
-
-  componentDidMount() {
+  useEffect(() => {
     const queryParams = new URLSearchParams(window.location.search);
-    const redirectUrl = queryParams.get('url');
+    const url = queryParams.get('url');
 
-    if (redirectUrl) {
-      this.setState({redirectUrl});
+    if (!url) {
+      return;
     }
 
-    this.timer = window.setInterval(() => {
-      this.setState(prevState => ({
-        count: prevState.count - 1,
-      }));
+    setRedirectUrl(url);
+
+    const intervalId = setInterval(() => {
+      setCount(prevCount => prevCount - 1);
     }, 1000);
+
+    // eslint-disable-next-line consistent-return
+    return () => clearInterval(intervalId);
+  }, []);
+
+  if (redirectUrl && count <= 0) {
+    window.location.href = redirectUrl;
   }
 
-  componentDidUpdate() {
-    const {count, redirectUrl} = this.state;
-    if (count === 0 && redirectUrl) {
-      if (this.timer !== null) {
-        clearInterval(this.timer);
-        this.timer = null;
-        window.location.href = redirectUrl;
-      }
-    }
-  }
-
-  componentWillUnmount() {
-    if (this.timer !== null) {
-      clearInterval(this.timer);
-      this.timer = null;
-    }
-  }
-
-  timer: number | null = null;
-
-  goBack = () => {
-    window.close();
-  };
-
-  render() {
-    const {count, redirectUrl} = this.state;
-
-    return (
-      <div className="app">
-        <RedirectContainer>
-          <div className="pattern-bg" />
-          <AuthPanel>
-            {tct(
-              'You are being redirected to [redirectUrl] in [count] seconds. Changed your mind? [link:Go back]',
-              {
-                redirectUrl,
-                count,
-                link: <a href="#" onClick={this.goBack} />,
-              }
-            )}
-          </AuthPanel>
-        </RedirectContainer>
-      </div>
-    );
-  }
+  return (
+    <div className="app">
+      <RedirectContainer>
+        <div className="pattern-bg" />
+        <AuthPanel>
+          <div>
+            {tct('In [count] seconds you will be redirected to [redirectUrl].', {
+              count,
+              redirectUrl: <strong>{redirectUrl}</strong>,
+            })}
+          </div>
+          <div>
+            {tct('Changed your mind? [link:Go back to Sentry]', {
+              link: (
+                <a href="#" onClick={() => window.close()}>
+                  Go back to Sentry
+                </a>
+              ),
+            })}
+          </div>
+        </AuthPanel>
+      </RedirectContainer>
+    </div>
+  );
 }
 
 const RedirectContainer = styled('div')`
@@ -89,10 +65,7 @@ const RedirectContainer = styled('div')`
 
 const AuthPanel = styled(Panel)`
   width: 550px;
-  display: inline-grid;
-  grid-template-columns: 1fr;
   text-align: center;
-  align-items: center;
   word-wrap: break-word;
   padding: 2rem;
 `;
