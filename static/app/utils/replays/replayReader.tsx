a9f30708e0cdaeb685c08c5a6459dde11fc4bdca
Scott Cooper <scttcper@gmail.com>
2023-12-12 14:57:38 -0800

    whoops

--- static/app/utils/replays/replayReader.tsx
+++ static/app/utils/replays/replayReader.tsx
@@ -295,8 +295,7 @@ export default class ReplayReader {
       ...this._sortedBreadcrumbFrames.filter(
         frame =>
           ['replay.init', 'replay.mutations'].includes(frame.category) ||
-          (this._options.showHydrationErrors &&
-            frame.category === 'replay.hydrate-error-error')
+          (this._options.showHydrationErrors && frame.category === 'replay.hydrate-error')
       ),
       ...this._errors,
     ].sort(sortFrames)
