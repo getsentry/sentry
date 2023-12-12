76e60d685542f949315119f931382ecf1ee035f3
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-12-11 17:49:17 -0800

    feat(crons): Validate that a monitor may be assigned a seat

--- tests/sentry/monitors/endpoints/test_organization_monitor_details.py
+++ tests/sentry/monitors/endpoints/test_organization_monitor_details.py
@@ -1,4 +1,5 @@
 from datetime import timedelta
+from unittest.mock import patch
 
 import pytest
 
@@ -17,6 +18,7 @@ from sentry.monitors.models import (
     ScheduleType,
 )
 from sentry.monitors.utils import get_timeout_at
+from sentry.quotas.base import SeatAssignmentResult
 from sentry.testutils.cases import MonitorTestCase
 from sentry.testutils.helpers.datetime import freeze_time
 from sentry.testutils.silo import region_silo_test
@@ -566,6 +568,41 @@ class UpdateMonitorTest(MonitorTestCase):
         assert (
             resp.data["detail"]["message"] == "existing monitors may not be moved between projects"
         ), resp.content
+
+    @patch("sentry.quotas.backend.check_assign_monitor_seat")
+    def test_activate_monitor_success(self, check_assign_monitor_seat):
+        check_assign_monitor_seat.return_value = SeatAssignmentResult(assignable=True)
+
+        monitor = self._create_monitor()
+        monitor.update(status=ObjectStatus.DISABLED)
+
+        self.get_success_response(
+            self.organization.slug, monitor.slug, method="PUT", **{"status": "active"}
+        )
+
+        monitor = Monitor.objects.get(id=monitor.id)
+        assert monitor.status == ObjectStatus.ACTIVE
+
+    @patch("sentry.quotas.backend.check_assign_monitor_seat")
+    def test_activate_monitor_invalid(self, check_assign_monitor_seat):
+        result = SeatAssignmentResult(
+            assignable=False,
+            reason="Over quota",
+        )
+        check_assign_monitor_seat.return_value = result
+
+        monitor = self._create_monitor()
+        monitor.update(status=ObjectStatus.DISABLED)
+
+        resp = self.get_error_response(
+            self.organization.slug,
+            monitor.slug,
+            method="PUT",
+            status_code=400,
+            **{"status": "active"},
+        )
+
+        assert resp.data["status"][0] == result.reason
 
 
 @region_silo_test()
