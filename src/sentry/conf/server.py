a513b575e452329b468a9407adbf760766d791ea
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2023-12-12 12:57:31 -0800

    chore(spike protection): feature flag for new heuristic (#61624)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1865,6 +1865,8 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     "organizations:sourcemaps-bundle-flat-file-indexing": False,
     # Upload release bundles as artifact bundles.
     "organizations:sourcemaps-upload-release-as-artifact-bundle": False,
+    # Updated spike protection heuristic
+    "organizations:spike-protection-decay-heuristic": False,
     # Enable basic SSO functionality, providing configurable single sign on
     # using services like GitHub / Google. This is *not* the same as the signup
     # and login with Github / Azure DevOps that sentry.io provides.
