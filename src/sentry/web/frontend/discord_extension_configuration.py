4f09c35762a71713f01cebfb576e0ca0f5325c00
Julia Hoge <julia.hoge@sentry.io>
2023-12-15 10:56:47 -0800

    fix(discord): Fix installation flow from Discord to Sentry (#61723)
    Closes https://github.com/getsentry/getsentry/issues/12287
    
    Flow of adding Sentry to Discord from Discord is broken.
    
    ---------
    
    Co-authored-by: Stephen Cefali <scefali@sentry.io>

--- src/sentry/web/frontend/discord_extension_configuration.py
+++ src/sentry/web/frontend/discord_extension_configuration.py
@@ -4,3 +4,6 @@ from .integration_extension_configuration import IntegrationExtensionConfigurati
 class DiscordExtensionConfigurationView(IntegrationExtensionConfigurationView):
     provider = "discord"
     external_provider_key = "discord"
+
+    def map_params_to_state(self, params):
+        return {"use_configure": "1", **params}
