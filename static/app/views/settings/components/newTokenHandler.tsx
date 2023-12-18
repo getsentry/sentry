04ca439a5e69893423dfefaf2fdf0ca883cbd550
Yash Kamothi <yash.kamothi@gmail.com>
2023-12-18 14:55:24 -0800

    fix(user token): Hide API token after creation (#61939)
    Hide the user API token after it's been created. Currently the API token
    in user settings can always be seen, even after creation. This
    experience also deviates from the org auth tokens. To keep both
    experiences consistent, and increase security around tokens from
    leaking, we are updating the API token contract and UI. The current PR
    focuses on UI/FE, and addressing a safe blue/green change, while the BE
    will change independently in another PR.
    
    - The API contract for /api-tokens will include a new field of
    tokenLastCharacters
    - A general component for showing new tokens has been created and
    utilized to keep token experiences consistent
    
    Video showing experience when **no** BE changes have been made:
    
    
    https://github.com/getsentry/sentry/assets/5581484/0563d32d-1f6b-4971-a4f0-6ad5c14eadc5
    
    
    Video showing overall experience when the BE changes _have_ been made:
    
    
    https://github.com/getsentry/sentry/assets/5581484/e44bd860-f9e1-4ed7-aaea-f753bf01a631

---
+++ static/app/views/settings/components/newTokenHandler.tsx
@@ -0,0 +1,66 @@
+import {MouseEventHandler} from 'react';
+import styled from '@emotion/styled';
+
+import Alert from 'sentry/components/alert';
+import {Button} from 'sentry/components/button';
+import FieldGroup from 'sentry/components/forms/fieldGroup';
+import PanelItem from 'sentry/components/panels/panelItem';
+import TextCopyInput from 'sentry/components/textCopyInput';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+
+function NewTokenHandler({
+  token,
+  handleGoBack,
+}: {
+  handleGoBack: MouseEventHandler;
+  token: string;
+}) {
+  return (
+    <div>
+      <Alert type="warning" showIcon system>
+        {t("Please copy this token to a safe place — it won't be shown again!")}
+      </Alert>
+
+      <PanelItem>
+        <InputWrapper>
+          <FieldGroupNoPadding
+            label={t('Token')}
+            help={t('You can only view this token when it was created.')}
+            inline
+            flexibleControlStateSize
+          >
+            <TextCopyInput aria-label={t('Generated token')}>{token}</TextCopyInput>
+          </FieldGroupNoPadding>
+        </InputWrapper>
+      </PanelItem>
+
+      <PanelItem>
+        <ButtonWrapper>
+          <Button onClick={handleGoBack} priority="primary">
+            {t('Done')}
+          </Button>
+        </ButtonWrapper>
+      </PanelItem>
+    </div>
+  );
+}
+
+const InputWrapper = styled('div')`
+  flex: 1;
+`;
+
+const FieldGroupNoPadding = styled(FieldGroup)`
+  padding: 0;
+`;
+
+const ButtonWrapper = styled('div')`
+  margin-left: auto;
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  font-size: ${p => p.theme.fontSizeSmall};
+  gap: ${space(1)};
+`;
+
+export default NewTokenHandler;
