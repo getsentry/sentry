3b92bad804a79a4f0cad227e158c2684af613dd0
isabellaenriquez <isabella.enriquez@sentry.io>
2023-12-18 15:22:10 -0500

    tests :woozy_face:

--- tests/sentry/integrations/slack/notifications/test_nudge.py
+++ tests/sentry/integrations/slack/notifications/test_nudge.py
@@ -7,7 +7,8 @@ from sentry.notifications.notifications.integration_nudge import (
     IntegrationNudgeNotification,
 )
 from sentry.testutils.cases import SlackActivityNotificationTest
-from sentry.testutils.helpers.slack import get_attachment_no_text
+from sentry.testutils.helpers.features import with_feature
+from sentry.testutils.helpers.slack import get_attachment_no_text, get_blocks_no_text
 from sentry.types.integrations import ExternalProviders
 from sentry.utils import json
 
@@ -38,3 +39,27 @@ class SlackNudgeNotificationTest(SlackActivityNotificationTest):
         request_data = parse_qs(responses.calls[0].request.body)
         request_block_payload = json.loads(request_data["attachments"][0])
         assert request_block_payload[0]["callback_id"]
+
+    @responses.activate
+    @with_feature("organizations:slack-block-kit")
+    def test_nudge_block(self):
+        notification = IntegrationNudgeNotification(
+            self.organization,
+            recipient=self.user,
+            provider=ExternalProviders.SLACK,
+            seed=SEED,
+        )
+
+        with self.tasks():
+            notification.send()
+
+        blocks = get_blocks_no_text()
+        assert blocks[0]["text"]["text"] == MESSAGE_LIBRARY[SEED].format(provider="Slack")
+        assert len(blocks[1]["elements"]) == 1
+        assert blocks[1]["elements"][0]["action_id"] == "enable_notifications"
+        assert blocks[1]["elements"][0]["text"]["text"] == "Turn on personal notifications"
+        assert blocks[1]["elements"][0]["value"] == "all_slack"
+
+        # Slack requires callback_id to handle enablement
+        request_data = parse_qs(responses.calls[0].request.body)
+        assert request_data["callback_id"]
