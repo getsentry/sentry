1dacb9c87a3bdaab5ad7d95e097b022dbb3d5c8e
Cathy Teng <cathy.teng@sentry.io>
2023-12-19 11:48:45 -0800

    nits

--- src/sentry/tasks/integrations/github/open_pr_comment.py
+++ src/sentry/tasks/integrations/github/open_pr_comment.py
@@ -207,6 +207,21 @@ def get_pr_filenames_and_patches(pr_files: JSONData) -> Tuple[List[str], List[st
 
 # currently Python only
 def get_file_functions(patch: str) -> Set[str]:
+    r"""
+    Function header regex pattern
+    ^           - Asserts the start of a line.
+    @@.*@@      - Matches a string that starts with two "@" characters, followed by any characters
+                (except newline), and ends with two "@" characters.
+    \s+         - Matches one or more whitespace characters (spaces, tabs, etc.).
+    def         - Matches the literal characters "def".
+    \\s+         - Matches one or more whitespace characters.
+    (?P<fnc>.*) - This is a named capturing group that captures any characters (except newline)
+                and assigns them to the named group "fnc".
+    \(          - Matches an opening parenthesis "(".
+    .*          - Matches any characters (except newline).
+    $           - Asserts the end of a line.
+    """
+
     python_function_regex = r"^@@.*@@\s+def\s+(?P<fnc>.*)\(.*$"
     return set(re.findall(python_function_regex, patch, flags=re.M))
 
