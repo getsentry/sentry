041f8fac3a0770fb774c2a702f66d7feb146933d
John Yang <john.yang@sentry.io>
2023-12-19 19:22:24 -0800

    add profiling to extract_msg ops

--- src/sentry/sentry_metrics/consumers/indexer/batch.py
+++ src/sentry/sentry_metrics/consumers/indexer/batch.py
@@ -1,5 +1,7 @@
+import cProfile
 import logging
 import random
+import time
 from collections import defaultdict
 from typing import (
     Any,
@@ -98,7 +100,9 @@ class IndexerBatch:
         self.filtered_msg_meta: Set[BrokerMeta] = set()
         self.parsed_payloads_by_meta: MutableMapping[BrokerMeta, ParsedMessage] = {}
 
-        self._extract_messages()
+        with cProfile.Profile() as pr:
+            self._extract_messages()
+            pr.dump_stats(f"{int(time.time())}_{len(outer_message.payload)}_extract_messges.prof")
 
     @metrics.wraps("process_messages.extract_messages")
     def _extract_messages(self) -> None:
