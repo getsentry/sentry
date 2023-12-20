137195c25eb8bfd80603050eaa060af683c29ab1
Katie Byers <katie.byers@sentry.io>
2023-12-20 13:05:20 -0800

    ref(grouping): Move primary/secondary hashing into/out of functions (#61948)
    This is a small refactoring PR which makes the structure of our primary and secondary grouping code match slightly better, both for the purposes of an upcoming metric and in order to make testing easier. Specifically:
    
    - Getting the secondary grouping config now happens in the main flow, just as it does with the primary grouping config.
    - Actually calculating the primary hashes now happens in a helper function, just as it does with the calculation of the secondary hash.

--- src/sentry/event_manager.py
+++ src/sentry/event_manager.py
@@ -532,7 +532,10 @@ class EventManager:
 
         if _should_run_secondary_grouping(project):
             with metrics.timer("event_manager.secondary_grouping", tags=metric_tags):
-                secondary_hashes = _calculate_secondary_hash(project, job)
+                secondary_grouping_config = SecondaryGroupingConfigLoader().get_config_dict(project)
+                secondary_hashes = _calculate_secondary_hash(
+                    project, job, secondary_grouping_config
+                )
 
         with metrics.timer("event_manager.load_grouping_config"):
             # At this point we want to normalize the in_app values in case the
@@ -554,7 +557,7 @@ class EventManager:
             op="event_manager",
             description="event_manager.save.calculate_event_grouping",
         ), metrics.timer("event_manager.calculate_event_grouping", tags=metric_tags):
-            hashes = _calculate_event_grouping(project, job["event"], grouping_config)
+            hashes = _calculate_primary_hash(project, job, grouping_config)
 
         # Track the total number of grouping calculations done overall, so we can divide by the
         # count to get an average number of calculations per event
@@ -744,7 +747,20 @@ def _should_run_secondary_grouping(project: Project) -> bool:
     return result
 
 
-def _calculate_secondary_hash(project: Project, job: Job) -> None | CalculatedHashes:
+def _calculate_primary_hash(
+    project: Project, job: Job, grouping_config: GroupingConfig
+) -> CalculatedHashes:
+    """
+    Get the primary hash for the event.
+
+    This is pulled out into a separate function mostly in order to make testing easier.
+    """
+    return _calculate_event_grouping(project, job["event"], grouping_config)
+
+
+def _calculate_secondary_hash(
+    project: Project, job: Job, secondary_grouping_config: GroupingConfig
+) -> None | CalculatedHashes:
     """Calculate secondary hash for event using a fallback grouping config for a period of time.
     This happens when we upgrade all projects that have not opted-out to automatic upgrades plus
     when the customer changes the grouping config.
@@ -756,11 +772,11 @@ def _calculate_secondary_hash(project: Project, job: Job) -> None | CalculatedHa
             op="event_manager",
             description="event_manager.save.secondary_calculate_event_grouping",
         ):
-            secondary_event = copy.deepcopy(job["event"])
-            loader = SecondaryGroupingConfigLoader()
-            secondary_grouping_config = loader.get_config_dict(project)
+            # create a copy since `_calculate_event_grouping` modifies the event to add all sorts
+            # of grouping info and we don't want the backup grouping data in there
+            event_copy = copy.deepcopy(job["event"])
             secondary_hashes = _calculate_event_grouping(
-                project, secondary_event, secondary_grouping_config
+                project, event_copy, secondary_grouping_config
             )
     except Exception:
         sentry_sdk.capture_exception()
