226bc47137c6df38e9d48ab7edb4e09892f14c59
Billy Vong <billy@sentry.io>
2023-12-20 14:30:34 -0500

    fix PluginsFixture

--- static/app/views/settings/projectPlugins/index.spec.tsx
+++ static/app/views/settings/projectPlugins/index.spec.tsx
@@ -1,4 +1,5 @@
 import {Organization} from 'sentry-fixture/organization';
+import {Plugin as PluginFixture} from 'sentry-fixture/plugin';
 import {Plugins as PluginsFixture} from 'sentry-fixture/plugins';
 import {Project as ProjectFixture} from 'sentry-fixture/project';
 
@@ -24,13 +25,13 @@ describe('ProjectPluginsContainer', function () {
     org = Organization();
     project = ProjectFixture();
     plugins = PluginsFixture([
-      {
+      PluginFixture({
         enabled: true,
         id: 'disableable plugin',
         name: 'Disableable Plugin',
         slug: 'disableable plugin',
         canDisable: true,
-      },
+      }),
     ]);
     params = {
       projectId: project.slug,
