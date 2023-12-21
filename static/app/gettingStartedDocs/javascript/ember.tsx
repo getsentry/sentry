499a77b1017937e34877964dab00292822fe9b3b
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2023-12-21 12:09:56 -0500

    ref(replays): refactor replay onboarding docs to be separate (#62128)
    The replay onboarding content is now fairly separate from the normal
    onboarding docs -- the only big thing we're reusing is the `Step`
    component.
    
    - Gets rid of `SdkDocumentation` usage in our sidebar file
    - Instead, use a new hook `useLoadOnboardingDoc` to load the new
    onboarding content we want
    - No behavior change (I hope)
    - Create our own replay-specific `ReplayOnboardingLayout` component
    instead of re-using the docs one
    - The boolean `isReplayConfigStep` is gone; we don't need it anymore
    - Moved the 'Mask All Text' and 'Block All Media' toggles to right above
    the code snippet
    
    
    
    https://github.com/getsentry/sentry/assets/56095982/fb1a2d80-6d15-440e-97c6-da95c226d29d

--- static/app/gettingStartedDocs/javascript/ember.tsx
+++ static/app/gettingStartedDocs/javascript/ember.tsx
@@ -170,7 +170,6 @@ const replayOnboarding: OnboardingConfig = {
   configure: (params: Params) => [
     {
       type: StepType.CONFIGURE,
-      isReplayConfigStep: true,
       description: getReplayConfigureDescription({
         link: 'https://docs.sentry.io/platforms/javascript/guides/ember/session-replay/',
       }),
