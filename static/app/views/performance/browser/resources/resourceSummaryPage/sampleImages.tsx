afee433c98fe7c0839afcb1f6af1c2abdc01372c
Dominik Buszowiecki <DominikB2014@gmail.com>
2023-12-21 15:36:46 -0500

    add ui to enable image settings

--- static/app/views/performance/browser/resources/resourceSummaryPage/sampleImages.tsx
+++ static/app/views/performance/browser/resources/resourceSummaryPage/sampleImages.tsx
@@ -1,8 +1,14 @@
 import styled from '@emotion/styled';
 
+import {Button} from 'sentry/components/button';
+import Link from 'sentry/components/links/link';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import {formatBytesBase2} from 'sentry/utils';
 import getDynamicText from 'sentry/utils/getDynamicText';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import useProjects from 'sentry/utils/useProjects';
+import {normalizeUrl} from 'sentry/utils/withDomainRequired';
 import {useIndexedResourcesQuery} from 'sentry/views/performance/browser/resources/utils/useIndexedResourceQuery';
 import ChartPanel from 'sentry/views/starfish/components/chartPanel';
 import {SpanIndexedField} from 'sentry/views/starfish/types';
@@ -13,10 +19,13 @@ const {SPAN_GROUP, SPAN_DESCRIPTION, HTTP_RESPONSE_CONTENT_LENGTH} = SpanIndexed
 const imageWidth = '200px';
 
 function SampleImages({groupId}: Props) {
+  const isImagesEnabled = true; // TODO - this is temporary, this will be controlled by a project setting
+
   const imageResources = useIndexedResourcesQuery({
     queryConditions: [`${SPAN_GROUP}:${groupId}`],
     sorts: [{field: `measurements.${HTTP_RESPONSE_CONTENT_LENGTH}`, kind: 'desc'}],
     limit: 100,
+    enabled: isImagesEnabled,
   });
 
   const uniqueResources = new Set();
@@ -34,19 +43,54 @@ function SampleImages({groupId}: Props) {
 
   return (
     <ChartPanel title={t('Example Images')}>
-      <ImageWrapper>
-        {filteredResources.map(resource => {
-          return (
-            <ImageContainer
-              src={resource[SPAN_DESCRIPTION]}
-              fileName={getFileNameFromDescription(resource[SPAN_DESCRIPTION])}
-              size={resource[`measurements.${HTTP_RESPONSE_CONTENT_LENGTH}`]}
-              key={resource[SPAN_DESCRIPTION]}
-            />
-          );
-        })}
-      </ImageWrapper>
+      {isImagesEnabled ? (
+        <ImageWrapper>
+          {filteredResources.map(resource => {
+            return (
+              <ImageContainer
+                src={resource[SPAN_DESCRIPTION]}
+                fileName={getFileNameFromDescription(resource[SPAN_DESCRIPTION])}
+                size={resource[`measurements.${HTTP_RESPONSE_CONTENT_LENGTH}`]}
+                key={resource[SPAN_DESCRIPTION]}
+              />
+            );
+          })}
+        </ImageWrapper>
+      ) : (
+        <DisabledImages />
+      )}
     </ChartPanel>
+  );
+}
+
+function DisabledImages() {
+  const {
+    selection: {projects: selectedProjects},
+  } = usePageFilters();
+  const {projects} = useProjects();
+  const firstProjectSelected = projects.find(
+    project => project.id === selectedProjects[0].toString()
+  );
+
+  return (
+    <div>
+      <DisabledImageTextContainer>
+        <h6>{t('Images not shown')}</h6>
+        {t(
+          'You know, you can see the actual images that are on your site if you opt into this feature.'
+        )}
+      </DisabledImageTextContainer>
+      <ButtonContainer>
+        <Button>Only show links</Button>
+        <Link
+          to={normalizeUrl(
+            `/settings/projects/${firstProjectSelected?.slug}/performance/`
+          )}
+        >
+          <Button priority="primary">Enable in Settings</Button>
+        </Link>
+      </ButtonContainer>
+    </div>
   );
 }
 
@@ -72,12 +116,6 @@ function ImageContainer({
   );
 }
 
-const ImageWrapper = styled('div')`
-  display: grid;
-  grid-template-columns: repeat(auto-fill, ${imageWidth});
-  gap: 30px;
-`;
-
 const getFileNameFromDescription = (description: string) => {
   try {
     const url = new URL(description);
@@ -86,5 +124,26 @@ const getFileNameFromDescription = (description: string) => {
     return description;
   }
 };
+
+const ImageWrapper = styled('div')`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, ${imageWidth});
+  gap: 30px;
+`;
+
+const ButtonContainer = styled('div')`
+  display: grid;
+  grid-template-columns: repeat(2, auto);
+  gap: ${space(1)};
+  justify-content: center;
+  align-items: center;
+  padding-top: ${space(2)};
+`;
+
+const DisabledImageTextContainer = styled('div')`
+  text-align: center;
+  width: 300px;
+  margin: auto;
+`;
 
 export default SampleImages;
