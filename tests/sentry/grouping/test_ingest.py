eec3912867de938065c3765b95607c1d32ee0892
Katie Byers <lobsterkatie@gmail.com>
2023-12-20 18:57:15 -0800

    move tests

---
+++ tests/sentry/grouping/test_ingest.py
@@ -0,0 +1,53 @@
+from __future__ import annotations
+
+from unittest.mock import MagicMock, patch
+
+from sentry.event_manager import EventManager
+from sentry.testutils.cases import TestCase
+from sentry.testutils.silo import region_silo_test
+from sentry.testutils.skips import requires_snuba
+
+pytestmark = [requires_snuba]
+
+
+@region_silo_test
+class IngestGroupingHelpersTest(TestCase):
+    @patch("sentry.grouping.ingest._calculate_background_grouping")
+    def test_applies_background_grouping(self, mock_calc_background_grouping: MagicMock) -> None:
+        manager = EventManager({"message": "foo 123"})
+        manager.normalize()
+        manager.save(self.project.id)
+
+        assert mock_calc_background_grouping.call_count == 0
+
+        with self.options(
+            {
+                "store.background-grouping-config-id": "mobile:2021-02-12",
+                "store.background-grouping-sample-rate": 1.0,
+            }
+        ):
+            manager.save(self.project.id)
+
+        assert mock_calc_background_grouping.call_count == 1
+
+    @patch("sentry.grouping.ingest._calculate_background_grouping")
+    def test_background_grouping_sample_rate(
+        self, mock_calc_background_grouping: MagicMock
+    ) -> None:
+        manager = EventManager({"message": "foo 123"})
+        manager.normalize()
+        manager.save(self.project.id)
+
+        assert mock_calc_background_grouping.call_count == 0
+
+        with self.options(
+            {
+                "store.background-grouping-config-id": "mobile:2021-02-12",
+                "store.background-grouping-sample-rate": 0.0,
+            }
+        ):
+            manager.save(self.project.id)
+
+        manager.save(self.project.id)
+
+        assert mock_calc_background_grouping.call_count == 0
