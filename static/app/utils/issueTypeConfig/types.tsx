f9b28e499a3252d2f37c759ead54b3a6f0663fe1
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2023-12-21 08:09:31 -0800

    feat(issue-details): Socket hang up resource in issue details (#62041)
    this pr adds in resources for socket hang up issues on issue details

--- static/app/utils/issueTypeConfig/types.tsx
+++ static/app/utils/issueTypeConfig/types.tsx
@@ -105,6 +105,7 @@ export const enum ErrorHelpType {
   DOCUMENT_OR_WINDOW_OBJECT_ERROR = 'document_or_window_object_error',
   HANDLE_HARD_NAVIGATE_ERROR = 'handle_hard_navigate_error',
   DYNAMIC_SERVER_USAGE = 'dynamic_server_usage',
+  SOCKET_HANG_UP = 'socket_hang_up',
 }
 
 export interface ErrorInfo {
