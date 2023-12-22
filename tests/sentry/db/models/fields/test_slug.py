944f982ced823a476a51aa1603f6700ee83a8b72
Yash Kamothi <yash.kamothi@gmail.com>
2023-12-22 15:29:48 -0800

    type linting

--- tests/sentry/db/models/fields/test_slug.py
+++ tests/sentry/db/models/fields/test_slug.py
@@ -7,28 +7,28 @@ from sentry.testutils.cases import TestCase
 
 class TestNoNumericValidator(TestCase):
     def test_non_numeric_value(self) -> None:
-        self.assertIsNone(no_numeric_validator("abc"))
+        no_numeric_validator("abc")
 
     def test_numeric_value(self) -> None:
         with pytest.raises(ValidationError):
             no_numeric_validator("123")
 
     def test_combination_of_numeric_and_non_numeric_values(self) -> None:
-        self.assertIsNone(no_numeric_validator("49dk20sk5"))
+        no_numeric_validator("49dk20sk5")
 
     def test_emtpy_value(self) -> None:
-        self.assertIsNone(no_numeric_validator(""))
+        no_numeric_validator("")
 
 
 class TestSentrySlugField(TestCase):
     def setUp(self) -> None:
         self.field = SentrySlugField()
 
-    def test_valid_value(self):
+    def test_valid_value(self) -> None:
         # Valid slug value should not raise a ValidationError
         self.field.run_validators("valid-slug")
 
-    def test_numeric_value(self):
+    def test_numeric_value(self) -> None:
         # Numeric value should raise a ValidationError
         with pytest.raises(ValidationError):
             self.field.run_validators("123")
