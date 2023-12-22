04a4d3631b9002d7c09b0412b1b9ae88c3b9422e
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2023-12-22 16:20:53 +0100

    Refactor options

--- src/sentry/relay/globalconfig.py
+++ src/sentry/relay/globalconfig.py
@@ -6,9 +6,6 @@ from sentry.utils import metrics
 
 # List of options to include in the global config.
 RELAY_OPTIONS: List[str] = [
-    "profiling.profile_metrics.unsampled_profiles.allowed_org_ids",
-    "profiling.profile_metrics.unsampled_profiles.excluded_org_ids",
-    "profiling.profile_metrics.unsampled_profiles.excluded_project_ids",
     "profiling.profile_metrics.unsampled_profiles.platforms",
     "profiling.profile_metrics.unsampled_profiles.sample_rate",
 ]
