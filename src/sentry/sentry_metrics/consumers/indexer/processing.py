cf5c3a19ec4dfd59c767a82632bde61b8727e5c6
John Yang <john.yang@sentry.io>
2023-12-21 17:22:06 -0800

    Remove cardinality limiter

--- src/sentry/sentry_metrics/consumers/indexer/processing.py
+++ src/sentry/sentry_metrics/consumers/indexer/processing.py
@@ -23,7 +23,6 @@ from sentry.sentry_metrics.consumers.indexer.tags_validator import (
     ReleaseHealthTagsValidator,
 )
 from sentry.sentry_metrics.indexer.base import StringIndexer
-from sentry.sentry_metrics.indexer.limiters.cardinality import cardinality_limiter_factory
 from sentry.sentry_metrics.indexer.mock import MockIndexer
 from sentry.sentry_metrics.indexer.postgres.postgres_v2 import PostgresIndexer
 from sentry.utils import metrics, sdk
@@ -130,19 +129,6 @@ class MessageProcessor:
 
         sdk.set_measurement("indexer_batch.payloads.len", len(batch.parsed_payloads_by_meta))
 
-        with metrics.timer("metrics_consumer.check_cardinality_limits"), sentry_sdk.start_span(
-            op="check_cardinality_limits"
-        ):
-            cardinality_limiter = cardinality_limiter_factory.get_ratelimiter(self._config)
-            cardinality_limiter_state = cardinality_limiter.check_cardinality_limits(
-                self._config.use_case_id, batch.parsed_payloads_by_meta
-            )
-
-        sdk.set_measurement(
-            "cardinality_limiter.keys_to_remove.len", len(cardinality_limiter_state.keys_to_remove)
-        )
-        batch.filter_messages(cardinality_limiter_state.keys_to_remove)
-
         extracted_strings = batch.extract_strings()
 
         sdk.set_measurement("org_strings.len", len(extracted_strings))
@@ -156,11 +142,5 @@ class MessageProcessor:
         results = batch.reconstruct_messages(mapping, bulk_record_meta)
 
         sdk.set_measurement("new_messages.len", len(results.data))
-
-        with metrics.timer("metrics_consumer.apply_cardinality_limits"), sentry_sdk.start_span(
-            op="apply_cardinality_limits"
-        ):
-            # TODO: move to separate thread
-            cardinality_limiter.apply_cardinality_limits(cardinality_limiter_state)
 
         return results
