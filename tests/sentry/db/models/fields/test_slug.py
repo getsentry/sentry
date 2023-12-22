e1a4ccf1fa401ef3fbcf4c8415ef9ae3ba74ab3c
Yash Kamothi <yash.kamothi@gmail.com>
2023-12-22 14:57:46 -0800

    address PR comments

---
+++ tests/sentry/db/models/fields/test_slug.py
@@ -0,0 +1,34 @@
+import pytest
+from django.core.exceptions import ValidationError
+
+from sentry.db.models.fields.slug import SentrySlugField, no_numeric_validator
+from sentry.testutils.cases import TestCase
+
+
+class TestNoNumericValidator(TestCase):
+    def test_non_numeric_value(self) -> None:
+        self.assertIsNone(no_numeric_validator("abc"))
+
+    def test_numeric_value(self) -> None:
+        with pytest.raises(ValidationError):
+            no_numeric_validator("123")
+
+    def test_combination_of_numeric_and_non_numeric_values(self) -> None:
+        self.assertIsNone(no_numeric_validator("49dk20sk5"))
+
+    def test_emtpy_value(self) -> None:
+        self.assertIsNone(no_numeric_validator(""))
+
+
+class TestSentrySlugField(TestCase):
+    def setUp(self) -> None:
+        self.field = SentrySlugField()
+
+    def test_valid_value(self):
+        # Valid slug value should not raise a ValidationError
+        self.field.run_validators("valid-slug")
+
+    def test_numeric_value(self):
+        # Numeric value should raise a ValidationError
+        with pytest.raises(ValidationError):
+            self.field.run_validators("123")
