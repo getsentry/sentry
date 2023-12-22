c730df0d9e8af710f6bfa128b903fb4944cf8c89
Yash Kamothi <yash.kamothi@gmail.com>
2023-12-22 15:29:48 -0800

    type linting

--- src/sentry/db/models/fields/slug.py
+++ src/sentry/db/models/fields/slug.py
@@ -6,6 +6,8 @@ def no_numeric_validator(value: str) -> None:
     if value.isnumeric():
         raise ValidationError("Value cannot be entirely numeric.")
 
+    return None
+
 
 class SentrySlugField(SlugField):
     default_validators = list(SlugField.default_validators) + [no_numeric_validator]
