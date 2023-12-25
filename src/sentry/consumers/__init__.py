f21ed7364a19e3abe16366c1cf407e47799ae598
Colton Allen <colton.allen@sentry.io>
2023-12-25 14:27:57 -0600

    Collect up to 2.5 megabytes of data before committing

--- src/sentry/consumers/__init__.py
+++ src/sentry/consumers/__init__.py
@@ -62,12 +62,20 @@ def ingest_replay_recordings_options() -> List[click.Option]:
 def ingest_replay_recordings_buffered_options() -> List[click.Option]:
     """Return a list of ingest-replay-recordings-buffered options."""
     options = [
-        click.Option(["--max-buffer-row-count", "max_buffer_row_count"], type=int, default=100),
         click.Option(
-            ["--max-buffer-size-in-bytes", "max_buffer_size_in_bytes"], type=int, default=0
+            ["--max-buffer-row-count", "max_buffer_row_count"],
+            type=int,
+            default=100,
         ),
         click.Option(
-            ["--max-buffer-time-in-seconds", "max_buffer_time_in_seconds"], type=int, default=1
+            ["--max-buffer-size-in-bytes", "max_buffer_size_in_bytes"],
+            type=int,
+            default=2_500_000,
+        ),
+        click.Option(
+            ["--max-buffer-time-in-seconds", "max_buffer_time_in_seconds"],
+            type=int,
+            default=1,
         ),
     ]
     return options
