fd7605dd81b0754314e3aea7edec0426845c914a
George Gritsouk <989898+gggritso@users.noreply.github.com>
2023-12-28 11:54:43 -0500

    Fix incorrect platform declaration

--- static/app/views/performance/landing/index.spec.tsx
+++ static/app/views/performance/landing/index.spec.tsx
@@ -271,7 +271,7 @@ describe('Performance > Landing > Index', function () {
     expect(screen.getByTestId('frontend-other-view')).toBeInTheDocument();
 
     const updatedData = initializeData({
-      projects: [ProjectFixture({id: '123', platform: 'unknown})],
+      projects: [ProjectFixture({id: '123', platform: undefined})],
       selectedProject: 123,
     });
 
