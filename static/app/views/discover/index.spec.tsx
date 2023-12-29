49057e3745e48fa9472da18cade05bf23926d71a
Evan Purkhiser <evanpurkhiser@gmail.com>
2023-12-29 15:12:05 -0500

    ref(js): Named export for RouteComponentPropsFixture (#62448)

--- static/app/views/discover/index.spec.tsx
+++ static/app/views/discover/index.spec.tsx
@@ -1,7 +1,7 @@
 import selectEvent from 'react-select-event';
 import {Organization} from 'sentry-fixture/organization';
 import {Project as ProjectFixture} from 'sentry-fixture/project';
-import RouteComponentPropsFixture from 'sentry-fixture/routeComponentPropsFixture';
+import {RouteComponentPropsFixture} from 'sentry-fixture/routeComponentPropsFixture';
 import {RouterContextFixture} from 'sentry-fixture/routerContextFixture';
 
 import {render, screen} from 'sentry-test/reactTestingLibrary';
