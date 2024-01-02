7917fe339b5940c80ab0beec107d51b42832fe71
Yash Kamothi <yash.kamothi@gmail.com>
2024-01-02 11:05:51 -0800

    Update comments and tests

--- tests/sentry/db/models/fields/test_slug.py
+++ tests/sentry/db/models/fields/test_slug.py
@@ -9,17 +9,24 @@ class TestSentrySlugField(TestCase):
     def setUp(self) -> None:
         self.field = SentrySlugField()
 
-    def test_combination_of_numeric_and_non_numeric_values(self) -> None:
+    def test_combination_of_numeric_and_non_numeric(self) -> None:
         # Valid slug value should not raise a ValidationError
         self.field.run_validators("49dk20sk5-34fas")
 
-    def test_non_numeric_value(self) -> None:
+    def test_non_numeric(self) -> None:
         self.field.run_validators("abc")
 
-    def test_numeric_value(self) -> None:
+    def test_numeric(self) -> None:
         # Numeric value should raise a ValidationError
         with pytest.raises(ValidationError):
             self.field.run_validators("123")
+
+    def test_capitalized(self) -> None:
+        with pytest.raises(ValidationError):
+            self.field.run_validators("rjkl29FRJF-dh439")
+
+    def test_underscore(self) -> None:
+        self.field.run_validators("sdjkh2390_dhj3290-")
 
 
 class TestSentryOrgSlugField(TestCase):
@@ -29,10 +36,14 @@ class TestSentryOrgSlugField(TestCase):
     def test_valid_value(self) -> None:
         self.field.run_validators("valid-123-slug")
 
-    def test_hyphen_at_end_of_value(self) -> None:
+    def test_hyphen_at_end(self) -> None:
         with pytest.raises(ValidationError):
             self.field.run_validators("not-123-valid-")
 
     def test_start_with_special_character(self) -> None:
         with pytest.raises(ValidationError):
             self.field.run_validators("!1Not-valid")
+
+    def test_with_underscore(self) -> None:
+        with pytest.raises(ValidationError):
+            self.field.run_validators("dfj49_29dFJ")
