86bd328d4803586f3cbca0bcd848de8952f833b5
Scott Cooper <scttcper@gmail.com>
2024-01-02 12:41:40 -0800

    build(ui): Add NO_SPOTLIGHT env variable to webpack (#62495)
    `export NO_SPOTLIGHT=1` will disable spotlight sidecar even in dev mode

--- webpack.config.ts
+++ webpack.config.ts
@@ -78,6 +78,7 @@ const HAS_WEBPACK_DEV_SERVER_CONFIG =
 const NO_DEV_SERVER = !!env.NO_DEV_SERVER; // Do not run webpack dev server
 const SHOULD_FORK_TS = DEV_MODE && !env.NO_TS_FORK; // Do not run fork-ts plugin (or if not dev env)
 const SHOULD_HOT_MODULE_RELOAD = DEV_MODE && !!env.SENTRY_UI_HOT_RELOAD;
+const SHOULD_RUN_SPOTLIGHT = DEV_MODE && !env.NO_SPOTLIGHT; // Do not run spotlight sidecar
 
 // Deploy previews are built using vercel. We can check if we're in vercel's
 // build process by checking the existence of the PULL_REQUEST env var.
@@ -603,7 +604,7 @@ if (
 }
 
 // We want Spotlight only in Dev mode - Local and UI only
-if (DEV_MODE) {
+if (SHOULD_RUN_SPOTLIGHT) {
   appConfig.plugins?.push(
     new WebpackHookPlugin({
       onBuildStart: ['yarn run spotlight-sidecar'],
