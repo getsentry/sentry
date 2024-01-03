e84968c56c40b870e4eeebdf6c7cc6453eec16ec
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2024-01-03 10:55:20 -0800

    test

--- static/app/views/issueList/noGroupsHandler/index.tsx
+++ static/app/views/issueList/noGroupsHandler/index.tsx
@@ -113,7 +113,7 @@ class NoGroupsHandler extends Component<Props, State> {
   }
 
   renderAwaitingEvents(projects: State['firstEventProjects']) {
-    const {organization, groupIds} = this.props;
+        const {organization, groupIds} = this.props;
 
     const project = projects && projects.length > 0 ? projects[0] : undefined;
     const sampleIssueId = groupIds.length > 0 ? groupIds[0] : undefined;
@@ -133,7 +133,7 @@ class NoGroupsHandler extends Component<Props, State> {
       return (
       <EmptyStateWarning>
         <p>{emptyMessage}</p>
-      </EmptyStateWarning>
+</EmptyStateWarning>
     );
     }
     return <NoIssuesMatched />;
