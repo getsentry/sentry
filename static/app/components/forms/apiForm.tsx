5f35e72c5c0df3b92d368265e04e2bcfac690979
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-01-02 16:40:15 -0800

    Rolls back ApiForm changes

--- static/app/components/forms/apiForm.tsx
+++ static/app/components/forms/apiForm.tsx
@@ -1,29 +1,19 @@
-import {useCallback, useMemo} from 'react';
+import {useCallback} from 'react';
 
 import {addLoadingMessage, clearIndicators} from 'sentry/actionCreators/indicator';
 import {RequestOptions} from 'sentry/api';
 import Form, {FormProps} from 'sentry/components/forms/form';
-import FormModel from 'sentry/components/forms/model';
 import {t} from 'sentry/locale';
 import useApi from 'sentry/utils/useApi';
 
 type Props = FormProps & {
   apiEndpoint: string;
   apiMethod: string;
-  hostOverride?: string;
   onSubmit?: (data: Record<string, any>) => any | void;
 };
 
-function ApiForm({
-  onSubmit,
-  apiMethod,
-  apiEndpoint,
-  hostOverride,
-  model,
-  ...otherProps
-}: Props) {
+function ApiForm({onSubmit, apiMethod, apiEndpoint, ...otherProps}: Props) {
   const api = useApi();
-  const formModel = useMemo(() => model ?? new FormModel(), [model]);
 
   const handleSubmit = useCallback(
     (
@@ -31,10 +21,6 @@ function ApiForm({
       onSuccess: (response: Record<string, any>) => void,
       onError: (error: any) => void
     ) => {
-      if (!formModel.validateForm()) {
-        return;
-      }
-
       const transformed = onSubmit?.(data);
       addLoadingMessage(t('Saving changes\u2026'));
 
@@ -51,16 +37,12 @@ function ApiForm({
         },
       };
 
-      if (hostOverride) {
-        requestOptions.host = hostOverride;
-      }
-
       api.request(apiEndpoint, requestOptions);
     },
-    [api, onSubmit, apiMethod, apiEndpoint, hostOverride, formModel]
+    [api, onSubmit, apiMethod, apiEndpoint]
   );
 
-  return <Form onSubmit={handleSubmit} model={formModel} {...otherProps} />;
+  return <Form onSubmit={handleSubmit} {...otherProps} />;
 }
 
 export default ApiForm;
