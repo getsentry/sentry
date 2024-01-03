4777821232e352fc3343ab2dd2dff9eb42126852
ArthurKnaus <arthur.knaus@sentry.io>
2024-01-03 09:15:10 +0100

    fix(ddm): Scratchpad project selection (#62424)
    Select default scratchpad on page load if query matches.
    Persist page filters if no scratchpad is selected.
    
    - closes https://github.com/getsentry/sentry/issues/59703
    - closes https://github.com/getsentry/sentry/issues/62365
    - closes https://github.com/getsentry/sentry/issues/59705

--- static/app/utils/metrics/index.tsx
+++ static/app/utils/metrics/index.tsx
@@ -510,6 +510,16 @@ export function useUpdateQuery() {
   );
 }
 
+export function useClearQuery() {
+  const router = useRouter();
+  // Store the router in a ref so that we can use it in the callback
+  // without needing to generate a new callback every time the location changes
+  const routerRef = useInstantRef(router);
+  return useCallback(() => {
+    clearQuery(routerRef.current);
+  }, [routerRef]);
+}
+
 // TODO(ddm): there has to be a nicer way to do this
 export function getSeriesName(
   group: MetricsGroup,
