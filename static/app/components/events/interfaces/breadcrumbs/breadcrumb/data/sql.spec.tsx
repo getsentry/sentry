92d380c4bc004c6d04661881cb050697c0c5dca5
17hogeju <17hogeju@gmail.com>
2024-01-04 12:55:21 -0800

    Add test

---
+++ static/app/components/events/interfaces/breadcrumbs/breadcrumb/data/sql.spec.tsx
@@ -0,0 +1,36 @@
+import {Project} from 'sentry-fixture/project';
+
+import {initializeOrg} from 'sentry-test/initializeOrg';
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import {Sql} from 'sentry/components/events/interfaces/breadcrumbs/breadcrumb/data/sql';
+
+describe('Breadcrumb Data SQL', function () {
+  const project = Project({id: '0'});
+
+  const {organization, router} = initializeOrg({
+    router: {
+      location: {query: {project: project.id}},
+    },
+    projects: [project],
+  });
+
+  it('displays formatted SQL message', function () {
+    const message = `
+SELECT db.id, db.project_id,
+       db.release_name, db.dist_name,
+       db.date_added
+FROM db
+WHERE (db.dist_name = %s
+       AND db.project_id = %s
+       AND db.release_name = %s)
+ORDER BY db.id ASC
+LIMIT 1
+FOR
+UPDATE NOWAIT`;
+    render(<Sql searchTerm="" message={message} />, {organization, router});
+
+    expect(screen.getByText('SELECT db.id, db.project_id,')).toBeInTheDocument();
+    expect(screen.getByText('ORDER BY db.id ASC')).toBeInTheDocument();
+  });
+});
