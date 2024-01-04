43d0a54985483c4e384a25a6eac2c3e9a8b889b0
Colton Allen <colton.allen@sentry.io>
2024-01-04 10:16:32 -0600

    fix(replays): Cap the number of aggregation targets we'll search for to match the number of aggregations we'll return (#62616)
    Sometimes the client will send more aggregation keys than the endpoint
    is capable of returning. We should handle these situations gracefully.
    The proposed solution is to cap the list and silently drop extra
    aggregation keys which matches current production behavior without the
    wild memory usage.
    
    Addresses: https://sentry.sentry.io/issues/4802777330

--- src/sentry/replays/usecases/replay_counts.py
+++ src/sentry/replays/usecases/replay_counts.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import logging
 from collections import defaultdict
 from collections.abc import Sequence
 from typing import Any
@@ -18,6 +19,9 @@ MAX_VALS_PROVIDED = {
 }
 
 FILTER_HAS_A_REPLAY = " AND !replayId:''"
+
+
+logger = logging.getLogger()
 
 
 def get_replay_counts(snuba_params: SnubaParams, query, return_ids, data_source) -> dict[str, Any]:
@@ -55,6 +59,13 @@ def _get_replay_id_mappings(
         # just return a mapping of replay_id:replay_id instead of hitting discover
         # if we want to validate list of replay_ids existence
         return {v: [v] for v in value}
+
+    # Any number of issues can be specified however we're only going to return at
+    # most 25 of them. Cap the lists length to 25. The other ids are silently
+    # dropped.
+    if len(value) > 25:
+        logger.warning("Received more than 25 ids: %d", len(value))
+        value = value[:25]
 
     builder = QueryBuilder(
         dataset=data_source,
