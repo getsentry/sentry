41771b31ab343452c2befa720c2817413155f670
17hogeju <17hogeju@gmail.com>
2024-01-04 11:25:29 -0800

    Styling fixes

--- static/app/components/events/interfaces/breadcrumbs/breadcrumb/data/sql.tsx
+++ static/app/components/events/interfaces/breadcrumbs/breadcrumb/data/sql.tsx
@@ -1,4 +1,7 @@
+import styled from '@emotion/styled';
+
 import Highlight from 'sentry/components/highlight';
+import {prismStyles} from 'sentry/styles/prism';
 import {usePrismTokens} from 'sentry/utils/usePrismTokens';
 
 import Summary from './summary';
@@ -12,19 +15,35 @@ export function Sql({message, searchTerm}: Props) {
   const tokens = usePrismTokens({code: message, language: 'sql'});
   return (
     <Summary>
-      <pre className="language-sql">
-        {tokens.map((line, i) => (
-          <div key={i}>
-            <div>
-              {line.map((token, j) => (
-                <span key={j} className={token.className}>
-                  <Highlight text={searchTerm}>{token.children}</Highlight>
-                </span>
-              ))}
-            </div>
-          </div>
-        ))}
-      </pre>
+      <Wrapper>
+        <pre className="language-sql">
+          <code>
+            {tokens.map((line, i) => (
+              <div key={i}>
+                <div>
+                  {line.map((token, j) => (
+                    <span key={j} className={token.className}>
+                      <Highlight text={searchTerm}>{token.children}</Highlight>
+                    </span>
+                  ))}
+                </div>
+              </div>
+            ))}
+          </code>
+        </pre>
+      </Wrapper>
     </Summary>
   );
 }
+
+const Wrapper = styled('div')`
+  background: var(--prism-block-background);
+
+  ${p => prismStyles(p.theme)}
+
+  pre {
+    margin: 0;
+    padding: 0;
+    font-size: ${p => p.theme.fontSizeSmall};
+  }
+`;
