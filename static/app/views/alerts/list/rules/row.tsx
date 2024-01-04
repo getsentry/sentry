251f571ff0046bacb83d4009110be16a2056de97
Jodi Jang <jodi.jang@sentry.io>
2024-01-04 12:46:49 -0800

    ref: Make change in css

--- static/app/views/alerts/list/rules/row.tsx
+++ static/app/views/alerts/list/rules/row.tsx
@@ -240,7 +240,7 @@ function RuleListRow({
           header: <h5>{t('Delete Alert Rule?')}</h5>,
           message: t(
             'Are you sure you want to delete "%s"? You won\'t be able to view the history of this alert once it\'s deleted.',
-            rule.name ? rule.name.replace(/(.{64})/g, '$1\n') : rule.name
+            rule.name
           ),
           confirmText: t('Delete Rule'),
           priority: 'danger',
