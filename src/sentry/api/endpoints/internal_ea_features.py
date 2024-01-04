38e87a87d2b43bc33d1018a59d8d90c481c9e72a
Nisanthan Nanthakumar <nnutoronto2013@gmail.com>
2024-01-04 10:26:04 -0800

    feat(feature-flags): Endpoint to return EA'd features

---
+++ src/sentry/api/endpoints/internal_ea_features.py
@@ -0,0 +1,30 @@
+from rest_framework.response import Response
+
+from sentry import features
+from sentry.api.base import Endpoint, region_silo_endpoint
+from sentry.api.permissions import SuperuserPermission
+from sentry.conf.server import SENTRY_EARLY_FEATURES
+from sentry.models.organization import Organization
+
+
+@region_silo_endpoint
+class InternalEAFeaturesEndpoint(Endpoint):
+    permission_classes = (SuperuserPermission,)
+
+    def get(self, request):
+        features_dict = features.all()
+
+        ea_org = Organization()
+        ea_org.flags.early_adopter = True
+
+        ea_features = [
+            feature_name for feature_name in features_dict if features.has(feature_name, ea_org)
+        ]
+
+        missing_from_self_hosted = [
+            feature for feature in ea_features if feature not in SENTRY_EARLY_FEATURES
+        ]
+
+        return Response(
+            {"ea_features": ea_features, "missing_from_self_hosted": missing_from_self_hosted}
+        )
