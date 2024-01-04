558f0dd7368b0d49eb9b66c83204ad365916fb08
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-01-04 08:44:54 -0500

    ref(ts): Remove fixture type aliases (#62588)
    Now that fixtures are all suffixed with `Fixture` this is not needed.
    https://github.com/getsentry/frontend-tsc/issues/49 💅🏻

--- fixtures/js-stubs/repositoryProjectPathConfig.ts
+++ fixtures/js-stubs/repositoryProjectPathConfig.ts
@@ -2,11 +2,10 @@ import type {
   Integration,
   Project,
   Repository,
-  RepositoryProjectPathConfig as RepositoryProjectPathConfigType,
+  RepositoryProjectPathConfig,
 } from 'sentry/types';
 
-interface RepositoryProjectPathConfigArgs
-  extends Partial<RepositoryProjectPathConfigType> {
+interface RepositoryProjectPathConfigArgs extends Partial<RepositoryProjectPathConfig> {
   integration: Pick<Integration, 'id' | 'provider'>;
   project: Pick<Project, 'id' | 'slug'>;
   repo: Pick<Repository, 'id' | 'name'>;
@@ -14,7 +13,7 @@ interface RepositoryProjectPathConfigArgs
 
 export function RepositoryProjectPathConfigFixture(
   params: RepositoryProjectPathConfigArgs
-): RepositoryProjectPathConfigType {
+): RepositoryProjectPathConfig {
   const {project, repo, integration, ...rest} = params;
   return {
     id: '2',
