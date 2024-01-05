07c459042d797e8773cb263821fb06b420cd41ad
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2024-01-05 10:50:39 -0800

    fix test

--- tests/sentry/api/endpoints/test_team_projects.py
+++ tests/sentry/api/endpoints/test_team_projects.py
@@ -182,16 +182,6 @@ class TeamProjectsCreateTest(APITestCase):
         }
 
         assert javascript_filter_states["browser-extensions"]
-        assert set(javascript_filter_states["legacy-browsers"]) == {
-            "ie_pre_9",
-            "ie9",
-            "ie10",
-            "ie11",
-            "safari_pre_6",
-            "opera_pre_15",
-            "opera_mini_pre_8",
-            "android_pre_4",
-            "edge_pre_79",
-        }
+        assert javascript_filter_states["legacy-browsers"]
         assert javascript_filter_states["web-crawlers"]
         assert javascript_filter_states["filtered-transaction"]
