510bb66615294071cbd7795bd7d394e4cd585b98
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-01-05 13:04:23 -0800

    ref(hybrid-cloud): Integration parser throws exceptions for region lookup failures

--- src/sentry/middleware/integrations/parsers/base.py
+++ src/sentry/middleware/integrations/parsers/base.py
@@ -197,10 +197,14 @@ class BaseRequestParser(abc.ABC):
             integration = self.get_integration_from_request()
         if not integration:
             logger.info("%s.no_integration", self.provider, extra={"path": self.request.path})
-            return []
+            raise Integration.DoesNotExist()
         organization_integrations = OrganizationIntegration.objects.filter(
             integration_id=integration.id
         )
+
+        if organization_integrations.count() == 0:
+            logger.info("%s.no_organizations", self.provider, extra={"path": self.request.path})
+            raise OrganizationIntegration.DoesNotExist()
         organization_ids = [oi.organization_id for oi in organization_integrations]
         return organization_mapping_service.get_many(organization_ids=organization_ids)
 
@@ -212,8 +216,5 @@ class BaseRequestParser(abc.ABC):
         """
         if not organizations:
             organizations = self.get_organizations_from_integration()
-        if not organizations:
-            logger.info("%s.no_organizations", self.provider, extra={"path": self.request.path})
-            return []
 
         return [get_region_for_organization(organization.slug) for organization in organizations]
