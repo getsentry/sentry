67a8a064b24537eadc32fb326d011c950ec79a76
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-01-08 09:23:01 -0800

    ref(PageBanner): Move `<PageBanner>` into components/alerts folder (#62725)
    This component is not replay specific, so it shouldn't be in the replays
    folder.
    
    `/components/alerts` seems like a fine place. I'd rather it be in a
    folder than in the great big mess of `/components` directly.

--- static/app/views/releases/components/releaseFeedbackBanner.tsx
+++ static/app/views/releases/components/releaseFeedbackBanner.tsx
@@ -2,8 +2,8 @@ import styled from '@emotion/styled';
 
 import replaysDeadRageBackground from 'sentry-images/spot/replay-dead-rage-changelog.svg';
 
+import PageBanner from 'sentry/components/alerts/pageBanner';
 import {LinkButton} from 'sentry/components/button';
-import PageBanner from 'sentry/components/replays/pageBanner';
 import {IconBroadcast} from 'sentry/icons';
 import {t, tct} from 'sentry/locale';
 import useDismissAlert from 'sentry/utils/useDismissAlert';
