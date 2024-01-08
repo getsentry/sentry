c91e22ef54033be958b9a1c4513a12d168342277
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2024-01-08 17:26:42 -0500

    feat(webvitals): Unrevert perf score chart breakdown tooltip (#62794)
    Unreverts the following pr, with fixes to broken test import:
    https://github.com/getsentry/sentry/pull/62057
    
    Adds better tooltips for the Performance Score breakdown chart
    component.
    Also adds some tests.
    <img width="583" alt="image"
    src="https://github.com/getsentry/sentry/assets/83961295/39d88e1d-7029-4d7c-9bf6-b4d4b0dbe3e4">

---
+++ static/app/views/performance/browser/webVitals/components/performanceScoreBreakdownChart.spec.tsx
@@ -0,0 +1,146 @@
+import {OrganizationFixture} from 'sentry-fixture/organization';
+
+import {render, screen, waitForElementToBeRemoved} from 'sentry-test/reactTestingLibrary';
+
+import {useLocation} from 'sentry/utils/useLocation';
+import useOrganization from 'sentry/utils/useOrganization';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import {PerformanceScoreBreakdownChart} from 'sentry/views/performance/browser/webVitals/components/performanceScoreBreakdownChart';
+
+jest.mock('sentry/utils/useLocation');
+jest.mock('sentry/utils/usePageFilters');
+jest.mock('sentry/utils/useOrganization');
+
+describe('PerformanceScoreBreakdownChart', function () {
+  const organization = OrganizationFixture();
+  let eventsMock, eventsStatsMock;
+
+  beforeEach(function () {
+    jest.mocked(useLocation).mockReturnValue({
+      pathname: '',
+      search: '',
+      query: {},
+      hash: '',
+      state: undefined,
+      action: 'PUSH',
+      key: '',
+    });
+    jest.mocked(usePageFilters).mockReturnValue({
+      isReady: true,
+      desyncedFilters: new Set(),
+      pinnedFilters: new Set(),
+      shouldPersist: true,
+      selection: {
+        datetime: {
+          period: '10d',
+          start: null,
+          end: null,
+          utc: false,
+        },
+        environments: [],
+        projects: [],
+      },
+    });
+    jest.mocked(useOrganization).mockReturnValue(organization);
+
+    eventsMock = MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/events/`,
+      body: {
+        data: [],
+      },
+    });
+    eventsStatsMock = MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/events-stats/`,
+      body: {},
+    });
+  });
+
+  afterEach(function () {
+    jest.resetAllMocks();
+  });
+
+  it('renders using frontend score calculation', async () => {
+    render(<PerformanceScoreBreakdownChart />);
+    await waitForElementToBeRemoved(() => screen.getByTestId('loading-indicator'));
+    expect(eventsStatsMock).toHaveBeenCalledWith(
+      '/organizations/org-slug/events-stats/',
+      expect.objectContaining({
+        method: 'GET',
+        query: expect.objectContaining({
+          yAxis: [
+            'p75(measurements.lcp)',
+            'p75(measurements.fcp)',
+            'p75(measurements.cls)',
+            'p75(measurements.ttfb)',
+            'p75(measurements.fid)',
+            'count()',
+          ],
+        }),
+      })
+    );
+  });
+
+  it('renders using backend scores', async () => {
+    jest.mocked(useLocation).mockReturnValue({
+      pathname: '',
+      search: '',
+      query: {useStoredScores: 'true'},
+      hash: '',
+      state: undefined,
+      action: 'PUSH',
+      key: '',
+    });
+    render(<PerformanceScoreBreakdownChart />);
+    await waitForElementToBeRemoved(() => screen.getByTestId('loading-indicator'));
+    expect(eventsMock).toHaveBeenCalledWith(
+      '/organizations/org-slug/events/',
+      expect.objectContaining({
+        method: 'GET',
+        query: expect.objectContaining({
+          field: [
+            'performance_score(measurements.score.lcp)',
+            'performance_score(measurements.score.fcp)',
+            'performance_score(measurements.score.cls)',
+            'performance_score(measurements.score.fid)',
+            'performance_score(measurements.score.ttfb)',
+            'avg(measurements.score.total)',
+            'avg(measurements.score.weight.lcp)',
+            'avg(measurements.score.weight.fcp)',
+            'avg(measurements.score.weight.cls)',
+            'avg(measurements.score.weight.fid)',
+            'avg(measurements.score.weight.ttfb)',
+            'count()',
+            'count_scores(measurements.score.total)',
+            'count_scores(measurements.score.lcp)',
+            'count_scores(measurements.score.fcp)',
+            'count_scores(measurements.score.cls)',
+            'count_scores(measurements.score.ttfb)',
+            'count_scores(measurements.score.fid)',
+          ],
+        }),
+      })
+    );
+
+    expect(eventsStatsMock).toHaveBeenCalledWith(
+      '/organizations/org-slug/events-stats/',
+      expect.objectContaining({
+        method: 'GET',
+        query: expect.objectContaining({
+          yAxis: [
+            'weighted_performance_score(measurements.score.lcp)',
+            'weighted_performance_score(measurements.score.fcp)',
+            'weighted_performance_score(measurements.score.cls)',
+            'weighted_performance_score(measurements.score.fid)',
+            'weighted_performance_score(measurements.score.ttfb)',
+            'performance_score(measurements.score.lcp)',
+            'performance_score(measurements.score.fcp)',
+            'performance_score(measurements.score.cls)',
+            'performance_score(measurements.score.fid)',
+            'performance_score(measurements.score.ttfb)',
+            'count()',
+          ],
+        }),
+      })
+    );
+  });
+});
