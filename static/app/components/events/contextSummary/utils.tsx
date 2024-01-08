8e0c7821379afaaa56e72ccd61ab5868a637a516
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2024-01-08 12:31:54 -0800

    feat(issue-details): add tag changes to other SSR frameworks (#62704)
    this pr builds upon https://github.com/getsentry/sentry/pull/62278 by
    adding it to other SSR frameworks.

--- static/app/components/events/contextSummary/utils.tsx
+++ static/app/components/events/contextSummary/utils.tsx
@@ -3,6 +3,18 @@ import {defined} from 'sentry/utils';
 
 import {Context} from './types';
 
+const serverSideSdks = [
+  'sentry.javascript.nextjs',
+  'sentry.javascript.gatsby',
+  'sentry.javascript.remix',
+  'sentry.javascript.astro',
+  'sentry.javascript.sveltekit',
+];
+
+function isServerSideRenderedEvent(event: Event) {
+  return event.sdk && serverSideSdks.includes(event.sdk.name);
+}
+
 /**
  * Generates a predicate to filter a list of contexts for an event.
  *
@@ -21,7 +33,7 @@ export const makeContextFilter = (event: Event) =>
       }
     }
 
-    if (event.sdk?.name === 'sentry.javascript.nextjs') {
+    if (isServerSideRenderedEvent(event)) {
       if (context.keys.includes('browser')) {
         const runtime = event.contexts?.runtime;
 
