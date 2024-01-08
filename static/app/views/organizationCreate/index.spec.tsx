3e7e07fdb062c44baebe6a583a1486190abaf14b
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-01-08 13:50:30 -0800

    Fixes mismatched quote types in test

--- static/app/views/organizationCreate/index.spec.tsx
+++ static/app/views/organizationCreate/index.spec.tsx
@@ -168,7 +168,7 @@ describe('OrganizationCreate', function () {
     );
     await userEvent.click(screen.getByText('Create Organization'));
 
-    const expectedHost = "https://us.example.com";
+    const expectedHost = 'https://us.example.com';
     await waitFor(() => {
       expect(orgCreateMock).toHaveBeenCalledWith('/organizations/', {
         success: expect.any(Function),
@@ -220,7 +220,7 @@ describe('OrganizationCreate', function () {
     );
     await userEvent.click(screen.getByText('Create Organization'));
 
-    const expectedHost = "https://de.example.com";
+    const expectedHost = 'https://de.example.com';
     await waitFor(() => {
       expect(orgCreateMock).toHaveBeenCalledWith('/organizations/', {
         success: expect.any(Function),
