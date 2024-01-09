402742cc34cbcf34a099c0aec283d2fc31b8e7ba
JonasBa <jonas@badalic.com>
2024-01-09 14:30:53 -0500

    ref: use slice instead of regexp

--- static/app/utils/url/stripURLOrigin.tsx
+++ static/app/utils/url/stripURLOrigin.tsx
@@ -8,12 +8,13 @@ import {safeURL} from 'sentry/utils/url/safeURL';
 export default function stripURLOrigin(url: string): string {
   const parsedUrl = safeURL(url);
 
-  if (!parsedUrl) {
+  if (parsedUrl === undefined) {
     return url;
   }
 
   // We cannot do something like parsedUrl.pathname + parsedUrl.search + parsedUrl.hash
   // as the parsed representation of the url may not exactly match the string url argument.
   // See more detailed explanation in https://github.com/getsentry/sentry/pull/62839#discussion_r1446450773
-  return url.replace(parsedUrl.origin, '');
+  // take last n characters where n is the length of the url - length of the origin
+  return url.slice(-(url.length - parsedUrl.origin.length));
 }
