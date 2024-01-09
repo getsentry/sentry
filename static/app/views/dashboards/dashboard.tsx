05847eabbbd058153958378fad79ec0dc6de639b
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-01-09 09:45:17 +0100

    remove add

--- static/app/views/dashboards/dashboard.tsx
+++ static/app/views/dashboards/dashboard.tsx
@@ -210,11 +210,6 @@ class Dashboard extends Component<Props, State> {
   handleStartAdd = (dataset?: DataSet) => {
     const {organization, router, location, paramDashboardId} = this.props;
 
-    if (dataset === DataSet.METRICS) {
-      this.handleAddMetricsWidget();
-      return;
-    }
-
     if (paramDashboardId) {
       router.push(
         normalizeUrl({
@@ -242,8 +237,6 @@ class Dashboard extends Component<Props, State> {
 
     return;
   };
-
-  handleAddMetricsWidget() {}
 
   handleUpdateComplete = (prevWidget: Widget) => (nextWidget: Widget) => {
     const {isEditing, onUpdate, handleUpdateWidgetList} = this.props;
