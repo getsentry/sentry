88fb4c84671d8cc9c54a268d50114b72ddb2d952
17hogeju <17hogeju@gmail.com>
2024-01-09 09:49:58 -0800

    Move styling

--- static/app/components/events/interfaces/breadcrumbs/breadcrumb/data/sql.tsx
+++ static/app/components/events/interfaces/breadcrumbs/breadcrumb/data/sql.tsx
@@ -1,7 +1,4 @@
-import styled from '@emotion/styled';
-
 import Highlight from 'sentry/components/highlight';
-import {prismStyles} from 'sentry/styles/prism';
 import {BreadcrumbTypeDefault, BreadcrumbTypeNavigation} from 'sentry/types/breadcrumbs';
 import {usePrismTokens} from 'sentry/utils/usePrismTokens';
 
@@ -18,35 +15,21 @@ export function Sql({breadcrumb, searchTerm}: Props) {
 
   return (
     <Summary kvData={data}>
-      <Wrapper>
-        <pre className="language-sql">
-          <code>
-            {tokens.map((line, i) => (
-              <div key={i}>
-                <div>
-                  {line.map((token, j) => (
-                    <span key={j} className={token.className}>
-                      <Highlight text={searchTerm}>{token.children}</Highlight>
-                    </span>
-                  ))}
-                </div>
+      <pre className="language-sql">
+        <code>
+          {tokens.map((line, i) => (
+            <div key={i}>
+              <div>
+                {line.map((token, j) => (
+                  <span key={j} className={token.className}>
+                    <Highlight text={searchTerm}>{token.children}</Highlight>
+                  </span>
+                ))}
               </div>
-            ))}
-          </code>
-        </pre>
-      </Wrapper>
+            </div>
+          ))}
+        </code>
+      </pre>
     </Summary>
   );
 }
-
-const Wrapper = styled('div')`
-  background: var(--prism-block-background);
-
-  ${p => prismStyles(p.theme)}
-
-  pre {
-    margin: 0;
-    padding: 0;
-    font-size: ${p => p.theme.fontSizeSmall};
-  }
-`;
