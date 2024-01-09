5ff063196d4dea2d10e9c985f380340daab66cfd
Katie Byers <katie.byers@sentry.io>
2024-01-09 11:28:37 -0800

    ref(grouping): Move `_calculate_event_grouping` to `grouping` module (#62203)
    This moves `_calculate_event_grouping` from `event_manager` to `grouping.ingest`, as part of a larger refactor. No code changes made other than to remove the leading underscore, since it's no longer an internal function.

--- tests/sentry/tasks/test_reprocessing2.py
+++ tests/sentry/tasks/test_reprocessing2.py
@@ -506,7 +506,7 @@ def test_apply_new_fingerprinting_rules(
     )
 
     with mock.patch(
-        "sentry.event_manager.get_fingerprinting_config_for_project", return_value=new_rules
+        "sentry.grouping.ingest.get_fingerprinting_config_for_project", return_value=new_rules
     ):
         # Reprocess
         with burst_task_runner() as burst_reprocess:
