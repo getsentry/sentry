460e92262463d6df8344d7923d6920fd160cbad1
Alberto Leal <mail4alberto@gmail.com>
2024-01-09 18:14:16 -0500

    fix

--- src/sentry/types/region.py
+++ src/sentry/types/region.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum
-from typing import Any, Collection, Container, Dict, Iterable, List, Optional, Set
+from typing import Any, Collection, Container, Dict, Iterable, List, Set
 from urllib.parse import urljoin
 
 import sentry_sdk
