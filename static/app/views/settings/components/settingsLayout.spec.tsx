c5f9be9805b837406ce003f93291de768b93dce9
Mark Story <mark@mark-story.com>
2024-01-10 07:08:13 -0800

    fix(ui) Don't show system health banners in sentry.io (#62879)
    Staff users sometimes see warning banners during deploys as celery ping
    jobs are missed. These banners alarm people and disrupt what they are
    doing to report the issue to engineering. We have more robust monitoring
    in place for celery and this banner is not helpful outside of
    self-hosted. By not having it we save customers and ourselves some
    cpu/bandwidth.

--- static/app/views/settings/components/settingsLayout.spec.tsx
+++ static/app/views/settings/components/settingsLayout.spec.tsx
@@ -12,12 +12,6 @@ describe('SettingsLayout', function () {
   beforeEach(function () {
     MockApiClient.clearMockResponses();
     MockApiClient.addMockResponse({
-      url: '/internal/health/',
-      body: {
-        problems: [],
-      },
-    });
-    MockApiClient.addMockResponse({
       url: '/organizations/',
       body: [OrganizationFixture()],
     });
