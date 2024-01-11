a5fff01ddf1639e1bfcff5b8326f323e5f4c304a
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-01-11 11:39:20 -0800

    ref(feedback): allow tags section to be expanded/collapsed (#63030)
    Now there's a button that says "expand tags" or "collapse tags" (it's
    collapsed by default) in the tags section. The "collapse" shows a
    preview of 3 tags.
    
    
    https://github.com/getsentry/sentry/assets/56095982/a5a3ec21-a38c-4f3b-bffc-9314752706b0
    
    Also margin at the bottom of the feedback item is increased, so there's
    less overlap with the "Give Feedback" floating button.
    
    Closes https://github.com/getsentry/team-replay/issues/340
    Closes https://github.com/getsentry/sentry/issues/63027

--- static/app/components/feedback/feedbackItem/tagsSection.tsx
+++ static/app/components/feedback/feedbackItem/tagsSection.tsx
@@ -1,16 +1,25 @@
 import {KeyValueTable, KeyValueTableRow} from 'sentry/components/keyValueTable';
 import TextOverflow from 'sentry/components/textOverflow';
 import {Tooltip} from 'sentry/components/tooltip';
+import {space} from 'sentry/styles/space';
 
 interface Props {
   tags: Record<string, string>;
+  collapsed?: boolean;
 }
 
-export default function TagsSection({tags}: Props) {
+export default function TagsSection({tags, collapsed}: Props) {
   const entries = Object.entries(tags);
 
   return (
-    <KeyValueTable noMargin>
+    <KeyValueTable
+      noMargin
+      style={
+        collapsed
+          ? {maxHeight: '90px', overflow: 'hidden', marginBottom: `${space(1)}`}
+          : undefined
+      }
+    >
       {entries.map(([key, value]) => (
         <KeyValueTableRow
           key={key}
