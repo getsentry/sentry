2814a127a6308a1c3170870ee5cd9ac5bdd367bc
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-01-12 09:57:06 -0500

    ref: replace memcached client with vendored copy (#62755)
    it is removed in django 4.2
    
    <!-- Describe your PR here. -->

--- self-hosted/sentry.conf.py
+++ self-hosted/sentry.conf.py
@@ -115,7 +115,7 @@ if memcached:
     memcached_port = env("SENTRY_MEMCACHED_PORT") or "11211"
     CACHES = {
         "default": {
-            "BACKEND": "django.core.cache.backends.memcached.MemcachedCache",
+            "BACKEND": "sentry.utils.memcached.MemcachedCache",
             "LOCATION": [memcached + ":" + memcached_port],
             "TIMEOUT": 3600,
         }
