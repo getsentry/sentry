4286f6f40c0fa9ae18917ab554f7bd22fa15eb02
Colleen O'Rourke <colleen@sentry.io>
2024-01-12 09:10:04 -0800

    ref(escalating issues): Enable flag and fix acceptance tests (#63049)
    Acceptance tests are failing in
    https://github.com/getsentry/sentry/pull/62967 so this PR enables the
    flag and fixes the acceptance tests so it can unblock the front end PR.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1530,7 +1530,7 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     # Enables data secrecy mode
     "organizations:enterprise-data-secrecy": False,
     # Enable archive/escalating issue workflow
-    "organizations:escalating-issues": False,
+    "organizations:escalating-issues": True,
     # Enable archive/escalating issue workflow in MS Teams
     "organizations:escalating-issues-msteams": False,
     # Enable archive/escalating issue workflow features in v2
