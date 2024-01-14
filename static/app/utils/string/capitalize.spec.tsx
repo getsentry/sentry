3e838b6956b73c67c0be619f35ef5c132b123967
JonasBa <jonas@badalic.com>
2024-01-14 08:20:47 -0500

    lodash: lowercase rest of string

--- static/app/utils/string/capitalize.spec.tsx
+++ static/app/utils/string/capitalize.spec.tsx
@@ -5,6 +5,10 @@ describe('capitalize', () => {
     expect(capitalize('hello')).toEqual('Hello');
   });
 
+  it('lowercases the rest of the string', () => {
+    expect(capitalize('HELLO')).toEqual('Hello');
+  });
+
   it('does not change the string if it is already capitalized', () => {
     expect(capitalize('Hello')).toEqual('Hello');
   });
--- static/app/utils/string/capitalize.tsx
+++ static/app/utils/string/capitalize.tsx
@@ -5,5 +5,5 @@ export function capitalize(string: string): string {
   if (string.length === 0) {
     return string;
   }
-  return string.charAt(0).toUpperCase() + string.slice(1);
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
 }
