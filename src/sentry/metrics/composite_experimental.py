6587d443b239c8bc0e637aff2b98686f39e78b07
Jan Michael Auer <mail@jauer.org>
2024-01-15 11:09:18 +0100

    fix: Use settings instead

--- src/sentry/metrics/composite_experimental.py
+++ src/sentry/metrics/composite_experimental.py
@@ -1,7 +1,6 @@
-import os
 from typing import Any, Dict, Optional, Type, Union
 
-from sentry import options
+from sentry import options, settings
 from sentry.metrics.base import MetricsBackend, Tags
 from sentry.metrics.dummy import DummyMetricsBackend
 from sentry.metrics.minimetrics import MiniMetricsMetricsBackend
@@ -9,9 +8,6 @@ from sentry.options import UnknownOption
 from sentry.utils.imports import import_string
 
 __all__ = ["CompositeExperimentalMetricsBackend"]
-
-
-DISABLED = os.getenv("SENTRY_DDM_DISABLE", "0") in ("1", "true", "True")
 
 
 class CompositeExperimentalMetricsBackend(MetricsBackend):
@@ -35,7 +31,7 @@ class CompositeExperimentalMetricsBackend(MetricsBackend):
         self._minimetrics: MiniMetricsMetricsBackend = MiniMetricsMetricsBackend()
 
     def _is_denied(self, key: str) -> bool:
-        return DISABLED or key.startswith(self._deny_prefixes)
+        return settings.SENTRY_DDM_DISABLE or key.startswith(self._deny_prefixes)
 
     @staticmethod
     def _minimetrics_sample_rate() -> float:
