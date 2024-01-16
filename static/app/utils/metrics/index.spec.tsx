7f25c192bf4c376fa23ceba2865922e6854fab22
Arthur Knaus <arthur.knaus@sentry.io>
2024-01-16 11:12:07 +0100

    ref(ddm): Add tests

--- static/app/utils/metrics/index.spec.tsx
+++ static/app/utils/metrics/index.spec.tsx
@@ -56,6 +56,7 @@ describe('getMetricsApiRequestQuery', () => {
       useCase: 'custom',
       interval: '2h',
       groupBy: ['project'],
+      orderBy: '-sessions',
       allowPrivate: true,
       per_page: 10,
     });
@@ -81,6 +82,7 @@ describe('getMetricsApiRequestQuery', () => {
       useCase: 'custom',
       interval: '30m',
       groupBy: ['project'],
+      orderBy: '-sessions',
       allowPrivate: true,
       per_page: 10,
     });
@@ -107,6 +109,69 @@ describe('getMetricsApiRequestQuery', () => {
       useCase: 'custom',
       interval: '5m',
       groupBy: ['environment'],
+      orderBy: '-sessions',
+      allowPrivate: true,
+      per_page: 10,
+    });
+  });
+
+  it('does not add a default orderBy if one is already present', () => {
+    const metric = {
+      field: 'sessions',
+      query: 'error',
+      groupBy: ['project'],
+      orderBy: 'foo',
+    };
+    const filters = {
+      projects: [1],
+      environments: ['production'],
+      datetime: {start: '2023-01-01', end: '2023-01-02', period: null, utc: true},
+    };
+    const overrides = {};
+
+    const result = getMetricsApiRequestQuery(metric, filters, overrides);
+
+    expect(result).toEqual({
+      start: '2023-01-01T00:00:00.000Z',
+      end: '2023-01-02T00:00:00.000Z',
+      query: 'error',
+      project: [1],
+      environment: ['production'],
+      field: 'sessions',
+      useCase: 'custom',
+      interval: '5m',
+      groupBy: ['project'],
+      orderBy: 'foo',
+      allowPrivate: true,
+      per_page: 10,
+    });
+  });
+
+  it('does not add a default orderBy if there are no groups', () => {
+    const metric = {
+      field: 'sessions',
+      query: 'error',
+      groupBy: [],
+    };
+    const filters = {
+      projects: [1],
+      environments: ['production'],
+      datetime: {start: '2023-01-01', end: '2023-01-02', period: null, utc: true},
+    };
+    const overrides = {};
+
+    const result = getMetricsApiRequestQuery(metric, filters, overrides);
+
+    expect(result).toEqual({
+      start: '2023-01-01T00:00:00.000Z',
+      end: '2023-01-02T00:00:00.000Z',
+      query: 'error',
+      project: [1],
+      environment: ['production'],
+      field: 'sessions',
+      useCase: 'custom',
+      interval: '5m',
+      groupBy: [],
       allowPrivate: true,
       per_page: 10,
     });
