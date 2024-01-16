e077ab9138c76ecca53ee1043b80db332c7ba028
Cathy Teng <cathy.teng@sentry.io>
2024-01-16 13:19:50 -0800

    refactor regex into parser class

---
+++ src/sentry/tasks/integrations/github/patch_parsers.py
@@ -0,0 +1,34 @@
+import re
+from abc import abstractmethod
+from typing import Set
+
+
+class LanguagePatch:
+    @staticmethod
+    @abstractmethod
+    def extract_functions_from_patch(self, patch: str) -> Set[str]:
+        pass
+
+
+class PythonPatch(LanguagePatch):
+    python_function_regex = r"^@@.*@@\s+def\s+(?P<fnc>.*)\(.*$"
+
+    def extract_functions_from_patch(self, patch: str) -> Set[str]:
+        r"""
+        Function header regex pattern
+        ^           - Asserts the start of a line.
+        @@.*@@      - Matches a string that starts with two "@" characters, followed by any characters
+                    (except newline), and ends with two "@" characters.
+        \s+         - Matches one or more whitespace characters (spaces, tabs, etc.).
+        def         - Matches the literal characters "def".
+        \\s+         - Matches one or more whitespace characters.
+        (?P<fnc>.*) - This is a named capturing group that captures any characters (except newline)
+                    and assigns them to the named group "fnc".
+        \(          - Matches an opening parenthesis "(".
+        .*          - Matches any characters (except newline).
+        $           - Asserts the end of a line.
+        """
+        return set(re.findall(self.python_function_regex, patch, flags=re.M))
+
+
+PATCH_PARSERS = {"py": PythonPatch}
