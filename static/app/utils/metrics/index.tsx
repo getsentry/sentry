ee20692f46a71193143a1f7a0966ecbb89eedac3
ArthurKnaus <arthur.knaus@sentry.io>
2024-01-16 11:29:18 +0100

    feat(ddm): Default orderBy (#63230)
    - closes https://github.com/getsentry/sentry/issues/63107

--- static/app/utils/metrics/index.tsx
+++ static/app/utils/metrics/index.tsx
@@ -243,6 +243,8 @@ export function getMetricsApiRequestQuery(
   const useCase = getUseCaseFromMRI(mri) ?? 'custom';
   const interval = getDDMInterval(datetime, useCase, overrides.fidelity);
 
+  const hasGroupBy = groupBy && groupBy.length > 0;
+
   const queryToSend = {
     ...getDateTimeParams(datetime),
     query,
@@ -252,7 +254,7 @@ export function getMetricsApiRequestQuery(
     useCase,
     interval,
     groupBy,
-    orderBy,
+    orderBy: hasGroupBy && !orderBy ? `-${field}` : orderBy,
     allowPrivate: true, // TODO(ddm): reconsider before widening audience
     // Max result groups for compatibility with old metrics layer
     // TODO(telemetry-experience): remove once everyone is on new metrics layer
