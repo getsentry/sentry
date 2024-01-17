9cc92833594f0bf0cc2aabf9c35fa99ccb6b121b
Seiji Chew <67301797+schew2381@users.noreply.github.com>
2024-01-17 15:33:34 -0800

    chore(staff): Rename component (#63352)
    Rename the component, must be merged at the same time as the linked
    getsentry PR
    
    Requires https://github.com/getsentry/getsentry/pull/12587

--- static/app/types/system.tsx
+++ static/app/types/system.tsx
@@ -11,7 +11,7 @@ export enum SentryInitRenderReactComponent {
   SETUP_WIZARD = 'SetupWizard',
   SYSTEM_ALERTS = 'SystemAlerts',
   U2F_SIGN = 'U2fSign',
-  SU_ACCESS_FORM = 'SuperuserAccessForm',
+  SU_STAFF_ACCESS_FORM = 'SuperuserStaffAccessForm',
 }
 
 export type OnSentryInitConfiguration =
