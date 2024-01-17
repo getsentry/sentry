f1b59c8b73677ada72b790dc4935c334af413e49
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-01-17 15:56:28 -0500

    Fix bug

--- src/sentry/relay/config/metric_extraction.py
+++ src/sentry/relay/config/metric_extraction.py
@@ -255,10 +255,10 @@ def _merge_metric_specs(
     alert_specs: List[HashedMetricSpec], widget_specs: List[HashedMetricSpec]
 ) -> List[MetricSpec]:
     # We use a dict so that we can deduplicate metrics with the same hash.
-    _metrics: Dict[str, MetricSpec] = {}
+    specs: dict[str, MetricSpec] = {}
     duplicated_specs = 0
     for query_hash, spec, _ in alert_specs + widget_specs:
-        already_present = _metrics.get(query_hash)
+        already_present = specs.get(query_hash)
         if already_present and already_present != spec:
             logger.warning(
                 "Duplicate metric spec found for hash %s with different specs: %s != %s",
@@ -269,15 +269,13 @@ def _merge_metric_specs(
             duplicated_specs += 1
             continue
 
-        metrics[query_hash] = spec
+        specs[query_hash] = spec
 
     if duplicated_specs > 0:
-        logger.error(
-            "%s metrics are duplicated. Check breadcrumbs for details.", len(duplicated_specs)
-        )
-        metrics.incr("on_demand_metrics.duplicate_specs", amount=len(duplicated_specs))
+        logger.error("%s metrics are duplicated. Check breadcrumbs for details.", duplicated_specs)
+        metrics.incr("on_demand_metrics.duplicate_specs", amount=duplicated_specs)
 
-    return [metric for metric in _metrics.values()]
+    return list(specs.values())
 
 
 def _convert_snuba_query_to_metrics(
