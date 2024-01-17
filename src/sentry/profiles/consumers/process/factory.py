26ed6db6ea283b4db90d1624655d2a664942934f
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-01-17 15:57:35 +0100

    Fix get_headers_dict

--- src/sentry/profiles/consumers/process/factory.py
+++ src/sentry/profiles/consumers/process/factory.py
@@ -42,7 +42,7 @@ class ProcessProfileStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
 
 def get_headers_dict(headers) -> Dict[str, str]:
     h = dict()
-    for k, v in headers.items():
+    for k, v in headers:
         if isinstance(v, "bytes"):
             v = str(v, encoding="utf-8")
         h[k] = v
