466ed6f947bf319b8cdcd194d8fb6dfa1febadb9
Arthur Knaus <arthur.knaus@sentry.io>
2024-01-17 14:27:51 +0100

    feat(ddm): Display spans summary

--- static/app/views/ddm/sampleTable.tsx
+++ static/app/views/ddm/sampleTable.tsx
@@ -1,9 +1,11 @@
+import {Fragment} from 'react';
 import {Link} from 'react-router';
 import styled from '@emotion/styled';
 import {PlatformIcon} from 'platformicons';
 import * as qs from 'query-string';
 
 import {LinkButton} from 'sentry/components/button';
+import Duration from 'sentry/components/duration';
 import GridEditable, {
   COL_WIDTH_UNDEFINED,
   GridColumnHeader,
@@ -12,6 +14,7 @@ import GridEditable, {
 import {extractSelectionParameters} from 'sentry/components/organizations/pageFilters/utils';
 import TextOverflow from 'sentry/components/textOverflow';
 import {Tooltip} from 'sentry/components/tooltip';
+import {CHART_PALETTE} from 'sentry/constants/chartPalette';
 import {IconArrow, IconProfiling} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
@@ -24,8 +27,29 @@ import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import useProjects from 'sentry/utils/useProjects';
 import {normalizeUrl} from 'sentry/utils/withDomainRequired';
+import ColorBar from 'sentry/views/performance/vitalDetail/colorBar';
 
 import {MetricRange, MetricSpan} from '../../utils/metrics/index';
+
+/**
+ * Limits the number of spans to the top n + an "other" entry
+ */
+function sortAndLimitSpans(spans: MetricSpan['spansSummary'], limit: number) {
+  if (!spans) {
+    return [];
+  }
+
+  const sortedSpans = [...spans].sort((a, b) => b.spanDuration - a.spanDuration);
+
+  return sortedSpans.slice(0, limit).concat([
+    {
+      spanDuration: sortedSpans
+        .slice(limit)
+        .reduce((acc, span) => acc + span.spanDuration, 0),
+      spanOp: `+${sortedSpans.length - limit} more`,
+    },
+  ]);
+}
 
 export type SamplesTableProps = MetricRange & {
   mri?: MRI;
@@ -38,6 +62,7 @@ const columnOrder: GridColumnOrder<keyof MetricSpan>[] = [
   {key: 'transactionId', width: COL_WIDTH_UNDEFINED, name: 'Event ID'},
   {key: 'segmentName', width: COL_WIDTH_UNDEFINED, name: 'Transaction'},
   {key: 'spansNumber', width: COL_WIDTH_UNDEFINED, name: 'Number of Spans'},
+  {key: 'spansSummary', width: COL_WIDTH_UNDEFINED, name: 'Spans Summary'},
   {key: 'duration', width: COL_WIDTH_UNDEFINED, name: 'Duration'},
   {key: 'traceId', width: COL_WIDTH_UNDEFINED, name: 'Trace ID'},
   {key: 'profileId', width: COL_WIDTH_UNDEFINED, name: 'Profile'},
@@ -139,6 +164,50 @@ export function SampleTable({mri, ...metricMetaOptions}: SamplesTableProps) {
         </span>
       );
     }
+    if (key === 'spansSummary') {
+      const totalDuration =
+        row.spansSummary?.reduce(
+          (acc, spanSummary) => acc + spanSummary.spanDuration,
+          0
+        ) ?? 0;
+
+      if (totalDuration === 0) {
+        return <ColorBar colorStops={[]} />;
+      }
+
+      const preparedSpans = sortAndLimitSpans(row.spansSummary, 5);
+
+      return (
+        <StyledColorBar
+          colorStops={preparedSpans.map((spanSummary, i) => {
+            return {
+              color: CHART_PALETTE[4][i % CHART_PALETTE.length],
+              percent: (spanSummary.spanDuration / totalDuration) * 100,
+              renderBarStatus: (barStatus, barKey) => (
+                <Tooltip
+                  title={
+                    <Fragment>
+                      <div>{spanSummary.spanOp}</div>
+                      <div>
+                        <Duration
+                          seconds={spanSummary.spanDuration / 1000}
+                          fixedDigits={2}
+                          abbreviation
+                        />
+                      </div>
+                    </Fragment>
+                  }
+                  key={barKey}
+                  skipWrapper
+                >
+                  {barStatus}
+                </Tooltip>
+              ),
+            };
+          })}
+        />
+      );
+    }
     if (key === 'profileId') {
       return (
         <AlignCenter>
@@ -189,4 +258,8 @@ const DurationHeadCell = styled('span')`
 const StyledPlatformIcon = styled(PlatformIcon)`
   margin-right: ${space(1)};
   height: ${space(3)};
+`;
+
+const StyledColorBar = styled(ColorBar)`
+  margin-bottom: 0px;
 `;
