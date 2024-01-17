14483d92f54f0b69c2c5aab24f4417e8ce538737
Colton Allen <colton.allen@sentry.io>
2024-01-17 15:35:53 -0600

    fix(replays): Add handling for upload errors in buffered consumer (#63298)
    Adds handling for errors while uploading to the service-provider.
    Specifically it:
    
    1. Captures and logs errors that occur inside the threadpool.
    2. Raises an exception which resets the buffer and begins processing
    from the previously committed offset.
    
    This was added to address (in a safe and reliable way) the "BadGateway"
    error that GCS throws.

--- tests/sentry/replays/unit/test_recording_buffer.py
+++ tests/sentry/replays/unit/test_recording_buffer.py
@@ -1,8 +1,14 @@
 import datetime
+from unittest.mock import patch
 
+import pytest
 import time_machine
 
-from sentry.replays.consumers.recording_buffered import RecordingBuffer
+from sentry.replays.consumers.recording_buffered import (
+    BufferCommitFailed,
+    RecordingBuffer,
+    commit_uploads,
+)
 
 
 def test_recording_buffer_commit_default():
@@ -45,7 +51,7 @@ def test_recording_buffer_commit_default():
 
 def test_recording_buffer_commit_deadline():
     buffer = RecordingBuffer(
-        max_buffer_row_count=1_000_000,  # Never triggers commit.
+        max_buffer_message_count=1_000_000,  # Never triggers commit.
         max_buffer_size_in_bytes=1_000_000,  # Never triggers commit.
         max_buffer_time_in_seconds=5,
     )
@@ -88,7 +94,7 @@ def test_recording_buffer_commit_next_state():
     traveller = time_machine.travel(now)
     traveller.start()
     buffer = RecordingBuffer(
-        max_buffer_row_count=1_000_000,  # Never triggers commit.
+        max_buffer_message_count=1_000_000,  # Never triggers commit.
         max_buffer_size_in_bytes=1_000_000,  # Never triggers commit.
         max_buffer_time_in_seconds=5,
     )
@@ -114,3 +120,28 @@ def test_recording_buffer_commit_next_state():
     assert second_deadline == int((now + datetime.timedelta(seconds=15)).timestamp())
 
     traveller.stop()
+
+
+@patch("sentry.replays.consumers.recording_buffered._do_upload")
+def test_commit_uploads(_do_upload):
+    """Assert successful batch does not error."""
+
+    def mocked(u):
+        return None
+
+    _do_upload.side_effect = mocked
+
+    commit_uploads([{}])  # type: ignore
+
+
+@patch("sentry.replays.consumers.recording_buffered._do_upload")
+def test_commit_uploads_failure(_do_upload):
+    """Assert _do_upload failure rate limits the consumer process."""
+
+    def mocked(u):
+        raise ValueError("")
+
+    _do_upload.side_effect = mocked
+
+    with pytest.raises(BufferCommitFailed):
+        commit_uploads([{}])  # type: ignore
