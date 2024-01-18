bfdee7c926a53678d4a77cb27d9e01da69eb601c
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-01-18 12:50:18 -0500

    ✨ change webhook provider to google

--- src/sentry/middleware/integrations/parsers/google.py
+++ src/sentry/middleware/integrations/parsers/google.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 class GoogleRequestParser(BaseRequestParser):
     provider = "google"
-    webhook_identifier = WebhookProviderIdentifier.GETSENTRY
+    webhook_identifier = WebhookProviderIdentifier.GOOGLE
 
     def get_response(self):
         return self.get_response_from_control_silo()
