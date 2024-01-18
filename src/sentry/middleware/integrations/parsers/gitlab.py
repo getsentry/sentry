780216125b4074fbfe5d79361ab9ace889a3b14c
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-01-18 12:10:34 -0800

    ref(hybrid-cloud): Changes shard ID for new webhook outboxes to integration ID

--- src/sentry/middleware/integrations/parsers/gitlab.py
+++ src/sentry/middleware/integrations/parsers/gitlab.py
@@ -60,6 +60,7 @@ class GitlabRequestParser(BaseRequestParser, GitlabWebhookMixin):
                 return self._integration
         except Exception:
             pass
+
         return None
 
     def get_response_from_gitlab_webhook(self):
@@ -68,6 +69,10 @@ class GitlabRequestParser(BaseRequestParser, GitlabWebhookMixin):
             return maybe_http_response
 
         try:
+            integration = self.get_integration_from_request()
+            if not integration:
+                return self.get_default_missing_integration_response()
+
             regions = self.get_regions_from_organizations()
         except (Integration.DoesNotExist, OrganizationIntegration.DoesNotExist):
             return self.get_default_missing_integration_response()
