3441b0f26a3977a92d99b4c258b26b141b45ab6a
Josh Ferge <josh.ferge@sentry.io>
2024-01-18 13:24:21 -0800

    feat(replays): add feature flag for rage click issue creation (#63472)
    adds a flag that will be used in
    https://github.com/getsentry/sentry/pull/63376 to control wether or not
    that code block is entered.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1856,6 +1856,8 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     "organizations:session-replay-new-event-counts": False,
     # Enable View Sample Replay button on the Replay-List empty-state page
     "organizations:session-replay-onboarding-cta-button": False,
+    # Enable Rage Click Issue Creation In Recording Consumer
+    "organizations:session-replay-rage-click-issue-creation": False,
     # Enable data scrubbing of replay recording payloads in Relay.
     "organizations:session-replay-recording-scrubbing": False,
     # Enable core Session Replay SDK for recording on sentry.io
