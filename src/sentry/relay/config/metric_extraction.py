1833fbb222d7d2a87d55b0b052a4c1ef81d36b7e
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-01-19 10:15:17 -0500

    Few more tweaks

--- src/sentry/relay/config/metric_extraction.py
+++ src/sentry/relay/config/metric_extraction.py
@@ -527,6 +527,12 @@ def _convert_aggregate_and_query_to_metrics(
         return None
 
     metric_specs_and_hashes = []
+    extra = {
+        "dataset": dataset,
+        "aggregate": aggregate,
+        "query": query,
+        "groupbys": groupbys,
+    }
     # Create as many specs as we support
     for spec_version in OnDemandMetricSpecVersioning.get_spec_versions():
         try:
@@ -544,35 +550,20 @@ def _convert_aggregate_and_query_to_metrics(
                 validate_sampling_condition(json.dumps(condition))
             else:
                 metrics.incr(
-                    "on_demand_metrics.missing_condition_spec",
-                    tags={"prefilling": prefilling},
+                    "on_demand_metrics.missing_condition_spec", tags={"prefilling": prefilling}
                 )
 
             metric_specs_and_hashes.append((on_demand_spec.query_hash, metric_spec, spec_version))
-        except UnicodeEncodeError:
-            # Known issue let's continue
-            continue
         except ValueError:
             # raised by validate_sampling_condition or metric_spec lacking "condition"
             metrics.incr("on_demand_metrics.invalid_metric_spec", tags={"prefilling": prefilling})
-            logger.exception(
-                "Invalid on-demand metric spec",
-                extra={
-                    "dataset": dataset,
-                    "aggregate": aggregate,
-                    "query": query,
-                    "groupbys": groupbys,
-                },
-            )
-
-            return None
-        except Exception as e:
+            logger.exception("Invalid on-demand metric spec", extra=extra)
+        except Exception:
             # Since prefilling might include several non-ondemand-compatible alerts, we want to not trigger errors in the
             # Sentry console.
             if not prefilling:
-                logger.exception(str(e))
+                logger.exception("Failed on-demand metric spec creation.", extra=extra)
 
-            return None
     return metric_specs_and_hashes
 
 
