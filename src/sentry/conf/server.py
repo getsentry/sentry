b024f82290ec5815f6b58b079def83b2abfef26a
Mark Story <mark@mark-story.com>
2024-01-19 09:09:52 -0800

    fix(hybridcloud) Improve ngrok server config (#63502)
    Blank out the region-template-url when using ngrok mode. Ngrok typically
    only proxies a single domain and having a region-template left over from
    other devserver configurations leads to cross-domain errors, or mixed
    protocol errors.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -4035,6 +4035,7 @@ SENTRY_DDM_DISABLE = os.getenv("SENTRY_DDM_DISABLE", "0") in ("1", "true", "True
 ngrok_host = os.environ.get("SENTRY_DEVSERVER_NGROK")
 if ngrok_host and SILO_MODE != "REGION":
     SENTRY_OPTIONS["system.url-prefix"] = f"https://{ngrok_host}"
+    SENTRY_OPTIONS["system.region-api-url-template"] = ""
     CSRF_TRUSTED_ORIGINS = [f".{ngrok_host}"]
     ALLOWED_HOSTS = [f".{ngrok_host}", "localhost", "127.0.0.1", ".docker.internal"]
 
