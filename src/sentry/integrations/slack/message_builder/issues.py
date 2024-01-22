53147b96a37f2b1cf064db0820d7c75ba63d534c
Colleen O'Rourke <colleen@sentry.io>
2024-01-22 13:38:42 -0800

    fix typing and tests

--- src/sentry/integrations/slack/message_builder/issues.py
+++ src/sentry/integrations/slack/message_builder/issues.py
@@ -149,7 +149,7 @@ def build_tag_fields(
 
 def get_tags(
     event_for_tags: Any,
-    has_slack_formatting_update: bool | False,
+    has_slack_formatting_update: bool = False,
     tags: set[str] | None = None,
 ) -> Sequence[Mapping[str, str | bool]]:
     """Get tag keys and values for block kit"""
