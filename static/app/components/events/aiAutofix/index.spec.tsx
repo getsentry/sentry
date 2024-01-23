669a795d99a21e4fa5e5dd9db9acb14d783a9e92
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-01-22 17:01:41 -0800

    refactor tests

--- static/app/components/events/aiAutofix/index.spec.tsx
+++ static/app/components/events/aiAutofix/index.spec.tsx
@@ -1,29 +1,18 @@
+import {GroupFixture} from 'sentry-fixture/group';
+
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import {AiAutofix} from 'sentry/components/events/aiAutofix';
-import {useAiAutofix} from 'sentry/components/events/aiAutofix/useAiAutofix';
-import {Group} from 'sentry/types';
+import {EventMetadataWithAutofix} from 'sentry/components/events/aiAutofix/types';
 
-jest.mock('sentry/components/events/aiAutofix/useAiAutofix', () => ({
-  useAiAutofix: jest.fn(() => ({
-    autofixData: null,
-    triggerAutofix: jest.fn(),
-    additionalContext: '',
-    setAdditionalContext: jest.fn(),
-    error: null,
-    isError: false,
-    isPolling: false,
-  })),
-}));
-
-const group = {
-  /* Mock group data */
-  id: 1,
-} as unknown as Group;
+const group = GroupFixture();
 
 describe('AiAutofix', () => {
-  afterEach(() => {
-    jest.resetAllMocks();
+  beforeAll(() => {
+    MockApiClient.addMockResponse({
+      url: `/issues/${group.id}/ai-autofix/`,
+      body: null,
+    });
   });
 
   it('renders the Banner component when autofixData is null', () => {
@@ -33,22 +22,23 @@ describe('AiAutofix', () => {
   });
 
   it('renders the FixResult component when autofixData is present', () => {
-    (useAiAutofix as jest.Mock).mockReturnValue({
-      autofixData: {
-        status: 'SUCCESS',
-        fix: {
-          title: 'Fixed the bug!',
-        },
-      },
-      triggerAutofix: jest.fn(),
-      additionalContext: '',
-      setAdditionalContext: jest.fn(),
-      error: null,
-      isError: false,
-      isPolling: false,
-    });
-
-    render(<AiAutofix group={group} />);
+    render(
+      <AiAutofix
+        group={{
+          ...group,
+          metadata: {
+            autofix: {
+              status: 'COMPLETED',
+              completedAt: '',
+              createdAt: '',
+              fix: {
+                title: 'Fixed the bug!',
+              },
+            },
+          } as EventMetadataWithAutofix,
+        }}
+      />
+    );
 
     expect(screen.getByText('Fixed the bug!')).toBeInTheDocument();
     expect(screen.getByText('View Pull Request')).toBeInTheDocument();
