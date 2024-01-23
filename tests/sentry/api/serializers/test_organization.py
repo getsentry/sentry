b053ce10ba21eb78c74a681e23c065b6b143a9af
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-01-22 16:19:58 -0800

    chore(issue-platform): Remove flags for crons/sd issues  (#63401)

--- tests/sentry/api/serializers/test_organization.py
+++ tests/sentry/api/serializers/test_organization.py
@@ -90,8 +90,6 @@ class OrganizationSerializerTest(TestCase):
             "performance-tracing-without-performance",
             "invite-members",
             "invite-members-rate-limits",
-            "issue-platform-api-crons-sd",
-            "issue-platform-crons-sd",
             "minute-resolution-sessions",
             "new-page-filter",
             "open-membership",
