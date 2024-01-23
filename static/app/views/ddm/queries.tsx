74a228ade86de976b6b1294bb5649ece5f43a22e
Arthur Knaus <arthur.knaus@sentry.io>
2024-01-23 10:23:07 +0100

    fix(ddm): Quote filter values

--- static/app/views/ddm/queries.tsx
+++ static/app/views/ddm/queries.tsx
@@ -40,6 +40,7 @@ export function Queries() {
               op: widget.op,
               groupBy: widget.groupBy,
               title: widget.title,
+              query: widget.query,
             }}
             displayType={widget.displayType}
             isEdit
