3cbd64ba7387c8d40339adc2da7cd539c4246231
Pierre Massat <pierre.massat@sentry.io>
2024-01-23 15:01:28 -0500

    Add raw INP value

--- src/sentry/sentry_metrics/indexer/strings.py
+++ src/sentry/sentry_metrics/indexer/strings.py
@@ -205,6 +205,7 @@ SPAN_METRICS_NAMES = {
     "d:spans/webvital.score.total@ratio": PREFIX + 414,
     "d:spans/webvital.score.inp@ratio": PREFIX + 415,
     "d:spans/webvital.score.weight.inp@ratio": PREFIX + 416,
+    "d:spans/webvital.inp@millisecond": PREFIX + 417,
 }
 
 # 500-599
