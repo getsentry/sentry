14662b0eaf6ef760c18a6bec8ec103530409e3f2
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-01-23 07:25:05 -0500

    fix(metrics_extraction): Handle Unicode characters in widget queries (#63458)
    Some customer's widgets contain Unicode characters which are making the
    spec generation fail.
    
    This change will create the query hash with the Unicode encoding rather than ASCII's. There's no need for a new spec version since the Unicode encoding is a superset of ASCII's (info provided by @vartec )
    
    Fixes [SENTRY-2D8H](https://sentry.sentry.io/issues/4834560384/).

--- src/sentry/snuba/metrics/extraction.py
+++ src/sentry/snuba/metrics/extraction.py
@@ -1162,7 +1162,7 @@ class OnDemandMetricSpec:
     @cached_property
     def query_hash(self) -> str:
         str_to_hash = self._query_str_for_hash
-        hash = hashlib.shake_128(bytes(str_to_hash, encoding="ascii")).hexdigest(4)
+        hash = hashlib.shake_128(bytes(str_to_hash, encoding="utf-8")).hexdigest(4)
         with sentry_sdk.start_span(op="OnDemandMetricSpec.query_hash", description=hash) as span:
             span.set_tag("str_to_hash", str_to_hash)
         return hash
