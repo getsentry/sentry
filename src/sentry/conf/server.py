7c761ca57b57a3025c3512b7eafc1dd109574fa5
Meredith Heller <meredith@sentry.io>
2024-01-23 15:08:34 -0800

    use image-mirror now

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -2880,7 +2880,7 @@ SENTRY_DEVSERVICES: dict[str, Callable[[Any, Any], dict[str, Any]]] = {
     ),
     "clickhouse": lambda settings, options: (
         {
-            "image": "altinity/clickhouse-server:23.8.8.21.altinitystable",
+            "image": "ghcr.io/getsentry/image-mirror-altinity-clickhouse-server:23.8.8.21.altinitystable",
             "ports": {"9000/tcp": 9000, "9009/tcp": 9009, "8123/tcp": 8123},
             "ulimits": [{"name": "nofile", "soft": 262144, "hard": 262144}],
             # The arm image does not properly load the MAX_MEMORY_USAGE_RATIO
