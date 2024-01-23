6c073833e7110d40e436a7bc94f6f417ef1bfdcc
Scott Cooper <scttcper@gmail.com>
2024-01-22 16:35:38 -0800

    feat(issues): Only show app icons for sentry apps (#63631)

--- static/app/components/events/interfaces/frame/openInContextLine.spec.tsx
+++ static/app/components/events/interfaces/frame/openInContextLine.spec.tsx
@@ -1,9 +1,12 @@
 import {GroupFixture} from 'sentry-fixture/group';
+import {OrganizationFixture} from 'sentry-fixture/organization';
 import {SentryAppInstallationFixture} from 'sentry-fixture/sentryAppInstallation';
+import {UserFixture} from 'sentry-fixture/user';
 
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import {OpenInContextLine} from 'sentry/components/events/interfaces/frame/openInContextLine';
+import ConfigStore from 'sentry/stores/configStore';
 import {SentryAppComponent, SentryAppSchemaStacktraceLink} from 'sentry/types';
 import {addQueryParamsToExistingUrl} from 'sentry/utils/queryString';
 
@@ -66,6 +69,28 @@ describe('OpenInContextLine', function () {
         'href',
         'http://localhost:4000/something?filename=%2Fsentry%2Fapp.py&installationId=25d10adb-7b89-45ac-99b5-edaa714341ba&lineNo=233&projectSlug=internal'
       );
+    });
+
+    it('renders only app icons with issue-details-stacktrace-link-in-frame feature', function () {
+      ConfigStore.set(
+        'user',
+        UserFixture({
+          options: {
+            ...UserFixture().options,
+            issueDetailsNewExperienceQ42023: true,
+          },
+        })
+      );
+      const organization = OrganizationFixture({
+        features: ['issue-details-stacktrace-link-in-frame'],
+      });
+      render(
+        <OpenInContextLine filename={filename} lineNo={lineNo} components={components} />,
+        {organization}
+      );
+
+      expect(screen.getByRole('link', {name: 'Foo'})).toHaveTextContent('');
+      expect(screen.getByRole('link', {name: 'Tesla'})).toHaveTextContent('');
     });
   });
 });
