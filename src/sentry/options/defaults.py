bd4cb47bf9ed6ef4c37f6dd44fae16915c708429
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-01-24 13:12:33 -0800

    chore(issue-priority): Register new option for issue priority flag (#63690)
    Related to https://github.com/getsentry/sentry/issues/63653

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -758,6 +758,13 @@ register(
     flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
 )
 
+register(
+    "issues.priority.projects-allowlist",
+    type=Sequence,
+    default=[],
+    flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
+)
+
 
 # ## sentry.killswitches
 #
