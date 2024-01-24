bdbf8b7066414bcfc96daebe78ab0905b2e694f0
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-01-23 20:15:48 -0800

    Fix typing

--- tests/sentry/migrations/test_0637_backfill_priority_for_groups.py
+++ tests/sentry/migrations/test_0637_backfill_priority_for_groups.py
@@ -1,5 +1,4 @@
 import logging
-from typing import Tuple
 
 from sentry.issues.grouptype import (
     ErrorGroupType,
@@ -9,7 +8,7 @@ from sentry.issues.grouptype import (
     PerformanceP95EndpointRegressionGroupType,
     ReplayDeadClickType,
 )
-from sentry.models.group import Group, GroupStatus
+from sentry.models.group import GroupStatus
 from sentry.models.project import Project
 from sentry.testutils.cases import TestMigrations
 from sentry.types.group import GroupSubStatus, PriorityLevel
@@ -19,9 +18,26 @@ class BackfillGroupPriority(TestMigrations):
     migrate_from = "0636_monitor_incident_env_resolving_index"
     migrate_to = "0637_backfill_priority_for_groups"
 
-    def _create_groups_to_backfill(
-        self, project: Project
-    ) -> Tuple[list[Group], list[Group], list[Group]]:
+    def setup_initial_state(self):
+        self._create_groups_to_backfill(self.project)
+
+    def test(self):
+        for desc, group in self.high_priority_groups:
+            group.refresh_from_db()
+            assert group.priority == PriorityLevel.HIGH, desc
+            # assert group.data["metadata"]["initial_priority"] == PriorityLevel.HIGH
+
+        for desc, group in self.medium_priority_groups:
+            group.refresh_from_db()
+            assert group.priority == PriorityLevel.MEDIUM, desc
+            # assert group.data["metadata"]["initial_priority"] == PriorityLevel.MEDIUM
+
+        for desc, group in self.low_priority_groups:
+            group.refresh_from_db()
+            assert group.priority == PriorityLevel.LOW, desc
+            # assert group.data["metadata"]["initial_priority"] == PriorityLevel.LOW
+
+    def _create_groups_to_backfill(self, project: Project) -> None:
         data = [
             # groups with priority remain unchanged, even if escalating.
             (
@@ -172,42 +188,17 @@ class BackfillGroupPriority(TestMigrations):
             ),
         ]
 
-        low_priority_groups = []
-        medium_priority_groups = []
-        high_priority_groups = []
+        self.low_priority_groups = []
+        self.medium_priority_groups = []
+        self.high_priority_groups = []
 
         for desc, group_data, expected_priority in data:
             group = self.create_group(project=project, **group_data)
             if expected_priority == PriorityLevel.LOW:
-                low_priority_groups.append((desc, group))
+                self.low_priority_groups.append((desc, group))
 
             elif expected_priority == PriorityLevel.MEDIUM:
-                medium_priority_groups.append((desc, group))
+                self.medium_priority_groups.append((desc, group))
 
             elif expected_priority == PriorityLevel.HIGH:
-                high_priority_groups.append((desc, group))
-
-        return low_priority_groups, medium_priority_groups, high_priority_groups
-
-    def setup_initial_state(self):
-        (
-            self.low_priority_groups,
-            self.medium_priority_groups,
-            self.high_priority_groups,
-        ) = self._create_groups_to_backfill(self.project)
-
-    def test(self):
-        for desc, group in self.high_priority_groups:
-            group.refresh_from_db()
-            assert group.priority == PriorityLevel.HIGH, desc
-            # assert group.data["metadata"]["initial_priority"] == PriorityLevel.HIGH
-
-        for desc, group in self.medium_priority_groups:
-            group.refresh_from_db()
-            assert group.priority == PriorityLevel.MEDIUM, desc
-            # assert group.data["metadata"]["initial_priority"] == PriorityLevel.MEDIUM
-
-        for desc, group in self.low_priority_groups:
-            group.refresh_from_db()
-            assert group.priority == PriorityLevel.LOW, desc
-            # assert group.data["metadata"]["initial_priority"] == PriorityLevel.LOW
+                self.high_priority_groups.append((desc, group))
