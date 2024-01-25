a3e44085adb6880f733092c7f03e7a0b54bcfa2c
Alexander Tarasov <alex.tarasov@sentry.io>
2024-01-25 10:05:55 +0100

    fix(security): add feature permission check when adding integration (#63737)
    Another attempt of https://github.com/getsentry/sentry/pull/63501
    
    Prevent enabling paid tier integrations (e.g. Slack) when no feature is
    enabled for the organization.

--- src/sentry/web/frontend/organization_integration_setup.py
+++ src/sentry/web/frontend/organization_integration_setup.py
@@ -1,10 +1,12 @@
 import logging
 
 import sentry_sdk
+from django.conf import settings
 from django.http import Http404
 from django.http.response import HttpResponseBase
 from rest_framework.request import Request
 
+from sentry import features
 from sentry.integrations.pipeline import IntegrationPipeline
 from sentry.web.frontend.base import ControlSiloOrganizationView, control_silo_view
 
@@ -36,6 +38,21 @@ class OrganizationIntegrationSetupView(ControlSiloOrganizationView):
             pipeline = IntegrationPipeline(
                 request=request, organization=organization, provider_key=provider_id
             )
+
+            is_feature_enabled = {}
+            for feature in pipeline.provider.features:
+                feature_flag_name = "organizations:integrations-%s" % feature.value
+                is_feature_enabled[feature_flag_name] = True
+                if feature_flag_name in settings.SENTRY_FEATURES:
+                    is_feature_enabled[feature_flag_name] = features.has(
+                        feature_flag_name, organization
+                    )
+
+            if not any(is_feature_enabled.values()):
+                return pipeline.render_warning(
+                    "At least one feature from this list has to be enabled in order to setup the integration:\n%s"
+                    % "\n".join(is_feature_enabled)
+                )
 
             if not pipeline.provider.can_add:
                 raise Http404
