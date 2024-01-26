faeafdc5c598054141d294e9b41cba0cec6ede3f
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-01-26 12:07:48 +0100

    ref(ddm): Rename the files for metrics metadata (#63931)

--- src/sentry/api/serializers/models/metrics_code_locations.py
+++ src/sentry/api/serializers/models/metrics_code_locations.py
@@ -1,7 +1,7 @@
 from sentry.api.serializers import Serializer
 
 
-class CodeLocationsSerializer(Serializer):
+class MetricCodeLocationsSerializer(Serializer):
     def __init__(self, *args, **kwargs):
         Serializer.__init__(self, *args, **kwargs)
 
--- src/sentry/api/serializers/models/metrics_correlations.py
+++ src/sentry/api/serializers/models/metrics_correlations.py
@@ -1,7 +1,7 @@
 from sentry.api.serializers import Serializer
 
 
-class CorrelationsSerializer(Serializer):
+class MetricCorrelationsSerializer(Serializer):
     def __init__(self, *args, **kwargs):
         Serializer.__init__(self, *args, **kwargs)
 
