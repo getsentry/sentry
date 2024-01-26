aa880e1f06c11686bf9a3ad4311dacda4494e854
Kev <6111995+k-fish@users.noreply.github.com>
2024-01-26 13:56:50 -0500

    feat(metrics-extraction): Add a date_added column to widget on-demand (#63761)
    ### Summary
    This adds a creation column to the on-demand extraction table, which
    will help us determine when specific organizations are ready to migrate,
    as well as help us detect churn on the tables.
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- src/sentry/backup/comparators.py
+++ src/sentry/backup/comparators.py
@@ -785,6 +785,7 @@ def get_default_comparators():
             "sentry.orgauthtoken": [
                 HashObfuscatingComparator("token_hashed", "token_last_characters")
             ],
+            "sentry.dashboardwidgetqueryondemand": [DateUpdatedComparator("date_modified")],
             "sentry.organization": [AutoSuffixComparator("slug")],
             "sentry.organizationintegration": [DateUpdatedComparator("date_updated")],
             "sentry.organizationmember": [
