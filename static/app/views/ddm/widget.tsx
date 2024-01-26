cf40534faca7a9478c8466bc8a69a7f7433fae8c
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-01-26 13:37:39 +0100

    ref(ddm): group focus area props

--- static/app/views/ddm/widget.tsx
+++ static/app/views/ddm/widget.tsx
@@ -35,7 +35,7 @@ import {useCorrelatedSamples} from 'sentry/utils/metrics/useMetricsCodeLocations
 import {useMetricsDataZoom} from 'sentry/utils/metrics/useMetricsData';
 import theme from 'sentry/utils/theme';
 import {MetricChart} from 'sentry/views/ddm/chart';
-import type {FocusArea} from 'sentry/views/ddm/focusArea';
+import type {FocusAreaProps} from 'sentry/views/ddm/context';
 import {QuerySymbol} from 'sentry/views/ddm/querySymbol';
 import {SummaryTable} from 'sentry/views/ddm/summaryTable';
 
@@ -47,15 +47,13 @@ type MetricWidgetProps = {
   onChange: (index: number, data: Partial<MetricWidgetQueryParams>) => void;
   projects: PageFilters['projects'];
   widget: MetricWidgetQueryParams;
-  addFocusArea?: (area: FocusArea) => void;
-  focusArea?: FocusArea | null;
+  focusArea?: FocusAreaProps;
   hasSiblings?: boolean;
   highlightedSampleId?: string;
   index?: number;
   isSelected?: boolean;
   onSampleClick?: (sample: Sample) => void;
   onSelect?: (index: number) => void;
-  removeFocusArea?: () => void;
   showQuerySymbols?: boolean;
 };
 
@@ -76,10 +74,8 @@ export const MetricWidget = memo(
     onSelect,
     onChange,
     hasSiblings = false,
-    addFocusArea,
-    removeFocusArea,
     showQuerySymbols,
-    focusArea = null,
+    focusArea,
     onSampleClick,
     highlightedSampleId,
   }: MetricWidgetProps) => {
@@ -167,9 +163,7 @@ export const MetricWidget = memo(
                 projects={projects}
                 environments={environments}
                 onChange={handleChange}
-                addFocusArea={addFocusArea}
                 focusArea={focusArea}
-                removeFocusArea={removeFocusArea}
                 onSampleClick={onSampleClick}
                 chartHeight={300}
                 highlightedSampleId={highlightedSampleId}
@@ -191,16 +185,14 @@ export const MetricWidget = memo(
   }
 );
 
-interface MetricWidgetBodyProps extends MetricWidgetQueryParams {
-  focusArea: FocusArea | null;
+type MetricWidgetBodyProps = MetricWidgetQueryParams & {
   widgetIndex: number;
-  addFocusArea?: (area: FocusArea) => void;
   chartHeight?: number;
+  focusArea?: FocusAreaProps;
   highlightedSampleId?: string;
   onChange?: (data: Partial<MetricWidgetQueryParams>) => void;
   onSampleClick?: (sample: Sample) => void;
-  removeFocusArea?: () => void;
-}
+};
 
 export const MetricWidgetBody = memo(
   ({
@@ -210,9 +202,7 @@ export const MetricWidgetBody = memo(
     highlightedSampleId,
     sort,
     widgetIndex,
-    addFocusArea,
     focusArea,
-    removeFocusArea,
     chartHeight,
     onSampleClick,
     ...metricsQuery
@@ -237,7 +227,9 @@ export const MetricWidgetBody = memo(
       {fidelity: displayType === MetricDisplayType.BAR ? 'low' : 'high'}
     );
 
-    const {data: samplesData} = useCorrelatedSamples(mri, {...focusArea?.range});
+    const {data: samplesData} = useCorrelatedSamples(mri, {
+      ...focusArea?.selection?.range,
+    });
 
     const chartRef = useRef<ReactEchartsRef>(null);
 
@@ -326,13 +318,11 @@ export const MetricWidgetBody = memo(
           displayType={displayType}
           operation={metricsQuery.op}
           widgetIndex={widgetIndex}
-          addFocusArea={addFocusArea}
-          focusArea={focusArea}
-          removeFocusArea={removeFocusArea}
           height={chartHeight}
           highlightedSampleId={highlightedSampleId}
           correlations={correlations}
           onSampleClick={onSampleClick}
+          focusArea={focusArea}
         />
         {metricsQuery.showSummaryTable && (
           <SummaryTable
