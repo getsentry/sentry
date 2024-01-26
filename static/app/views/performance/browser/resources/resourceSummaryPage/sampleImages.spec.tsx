0b7beaf44e10dc6e0fde9247f89fc795c1ea2c7c
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-01-26 15:27:10 -0500

    fix tests

--- static/app/views/performance/browser/resources/resourceSummaryPage/sampleImages.spec.tsx
+++ static/app/views/performance/browser/resources/resourceSummaryPage/sampleImages.spec.tsx
@@ -9,9 +9,10 @@ import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import SampleImages from 'sentry/views/performance/browser/resources/resourceSummaryPage/sampleImages';
-import {SpanMetricsField} from 'sentry/views/starfish/types';
+import {SpanIndexedField} from 'sentry/views/starfish/types';
 
-const {SPAN_GROUP, HTTP_RESPONSE_CONTENT_LENGTH, SPAN_DESCRIPTION} = SpanMetricsField;
+const {SPAN_GROUP, HTTP_RESPONSE_CONTENT_LENGTH, RAW_DOMAIN, SPAN_DESCRIPTION} =
+  SpanIndexedField;
 
 jest.mock('sentry/utils/useLocation');
 jest.mock('sentry/utils/usePageFilters');
@@ -117,6 +118,7 @@ const setupMockRequests = (
           project: 'javascript',
           [SPAN_DESCRIPTION]: 'https://cdn.com/image.png',
           'any(id)': 'anyId123',
+          [RAW_DOMAIN]: '',
         },
       ],
     },
