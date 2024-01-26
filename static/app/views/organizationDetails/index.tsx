26948f2cb22ec588cb6e5f24d8f1a302ff06b42a
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-01-26 15:57:10 -0800

    ref(js): Move Sidebar component to OrganizationDetails

--- static/app/views/organizationDetails/index.tsx
+++ static/app/views/organizationDetails/index.tsx
@@ -2,6 +2,8 @@ import useRouteAnalyticsHookSetup from 'sentry/utils/routeAnalytics/useRouteAnal
 import OrganizationLayout from 'sentry/views/organizationLayout';
 
 import Body from './body';
+import useOrganization from 'sentry/utils/useOrganization';
+import Sidebar from 'sentry/components/sidebar';
 
 interface Props {
   children: React.ReactNode;
@@ -9,9 +11,11 @@ interface Props {
 
 function OrganizationDetails({children}: Props) {
   useRouteAnalyticsHookSetup();
+  const organization = useOrganization({allowNull: true});
 
   return (
-    <OrganizationLayout includeSidebar>
+    <OrganizationLayout>
+      <Sidebar organization={organization ?? undefined} />
       <Body>{children}</Body>
     </OrganizationLayout>
   );
