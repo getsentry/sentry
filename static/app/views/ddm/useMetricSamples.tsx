0356412ab0dd271a9816810d3edaac48a5625264
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-01-29 13:34:57 +0100

    feat(ddm): solid color samples (#64044)

--- static/app/views/ddm/useMetricSamples.tsx
+++ static/app/views/ddm/useMetricSamples.tsx
@@ -150,7 +150,7 @@ export function useMetricSamples({
         projectId: sample.projectId,
         itemStyle: {
           color: theme.purple400,
-          opacity: isHighlighted ? 0.9 : 0.75,
+          opacity: 1,
         },
         yAxisIndex: 1,
         xAxisIndex: 1,
