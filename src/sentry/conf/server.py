0697ce0f563c94cfddac435edccae812126cd926
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-01-29 05:56:16 -0500

    ref: remove USE_L10N -- default true in django 4, always true in django 5 (#63777)
    going to wait a few days before merging since this solidifies in django
    4.x
    
    <!-- Describe your PR here. -->

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -316,10 +316,6 @@ SITE_ID = 1
 # to load the internationalization machinery.
 USE_I18N = True
 
-# If you set this to False, Django will not format dates, numbers and
-# calendars according to the current locale
-USE_L10N = True
-
 USE_TZ = True
 
 # CAVEAT: If you're adding a middleware that modifies a response's content,
