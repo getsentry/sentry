9abc400e50800976cbbc17b990ebde613a39ba53
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-01-29 08:46:08 +0100

    ref(ddm): Switch correlations endpoint query parsing to use visitors (#63943)

--- src/sentry/sentry_metrics/querying/visitors.py
+++ src/sentry/sentry_metrics/querying/visitors.py
@@ -1,5 +1,5 @@
 from abc import ABC
-from typing import Generic, Optional, Sequence, TypeVar
+from typing import Generic, Mapping, Optional, Sequence, TypeVar
 
 from snuba_sdk import BooleanCondition, BooleanOp, Column, Condition, Formula, Op, Timeseries
 from snuba_sdk.conditions import ConditionGroup
@@ -47,6 +47,9 @@ class QueryConditionVisitor(ABC, Generic[TVisited]):
     """
 
     def visit_group(self, condition_group: ConditionGroup) -> ConditionGroup:
+        if not condition_group:
+            return condition_group
+
         visited_conditions = []
         for condition in condition_group:
             visited_conditions.append(self.visit(condition))
@@ -82,7 +85,6 @@ class EnvironmentsInjectionVisitor(QueryExpressionVisitor[QueryExpression]):
         self._environment_names = [environment.name for environment in environments]
 
     def _visit_timeseries(self, timeseries: Timeseries) -> QueryExpression:
-        # TODO: inject the filter in the formula filters also.
         if self._environment_names:
             current_filters = timeseries.filters if timeseries.filters else []
             current_filters.extend(
@@ -180,4 +182,60 @@ class LatestReleaseTransformationVisitor(QueryConditionVisitor[QueryCondition]):
             lhs=condition.lhs,
             op=Op.IN,
             rhs=[latest_release.version for latest_release in latest_releases],
+        )
+
+
+class TagsTransformationVisitor(QueryConditionVisitor[QueryCondition]):
+    """
+    Visitor that recursively transforms all conditions to work on tags in the form `tags[x]`.
+    """
+
+    def __init__(self, check_sentry_tags: bool):
+        self._check_sentry_tags = check_sentry_tags
+
+    def _visit_condition(self, condition: Condition) -> QueryCondition:
+        if not isinstance(condition.lhs, Column):
+            return condition
+
+        # We assume that all incoming conditions are on tags, since we do not allow filtering by project in the
+        # query filters.
+        tag_column = f"tags[{condition.lhs.name}]"
+        sentry_tag_column = f"sentry_tags[{condition.lhs.name}]"
+
+        if self._check_sentry_tags:
+            tag_column = f"tags[{condition.lhs.name}]"
+            # We might have tags across multiple nested structures such as `tags` and `sentry_tags` for this reason
+            # we want to emit a condition that spans both.
+            return BooleanCondition(
+                op=BooleanOp.OR,
+                conditions=[
+                    Condition(lhs=Column(name=tag_column), op=condition.op, rhs=condition.rhs),
+                    Condition(
+                        lhs=Column(name=sentry_tag_column),
+                        op=condition.op,
+                        rhs=condition.rhs,
+                    ),
+                ],
+            )
+        else:
+            return Condition(lhs=Column(name=tag_column), op=condition.op, rhs=condition.rhs)
+
+
+class MappingTransformationVisitor(QueryConditionVisitor[QueryCondition]):
+    """
+    Visitor that recursively transforms all conditions whose `key` matches one of the supplied mappings. If found,
+    replaces it with the mapped value.
+    """
+
+    def __init__(self, mappings: Mapping[str, str]):
+        self._mappings = mappings
+
+    def _visit_condition(self, condition: Condition) -> QueryCondition:
+        if not isinstance(condition.lhs, Column):
+            return condition
+
+        return Condition(
+            lhs=Column(name=self._mappings.get(condition.lhs.key, condition.lhs.name)),
+            op=condition.op,
+            rhs=condition.rhs,
         )
