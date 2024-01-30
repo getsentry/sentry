a6d392594fd9f3fda3142c969abe5f4bef8cf522
Malachi Willey <malwilley@gmail.com>
2024-01-30 14:02:16 -0800

    feat(issue-details): Customize replay breadcrumbs to show issue and event info (#64178)
    Ref https://github.com/getsentry/sentry/issues/63157
    
    Modifies the breadcrumb items (in tooltips and the sidebar when in
    fullscreen mode) to include a linkable event ID and issue shortcode.
    These links are only clickable when going to a different issue/event,
    otherwise they are just displayed as text. The focused event is shown as
    "This Event".
    
    This introduces a new context `ReplayGroupContext` which provides the
    group and event ID. When available, the replay breadcrumbs will
    highlight the current event/issue. This context is provided by the
    EventReplay component, but could be used in other places as well down
    the line.

--- static/app/components/events/eventReplay/index.spec.tsx
+++ static/app/components/events/eventReplay/index.spec.tsx
@@ -1,12 +1,15 @@
 import {EventFixture} from 'sentry-fixture/event';
+import {GroupFixture} from 'sentry-fixture/group';
 import {OrganizationFixture} from 'sentry-fixture/organization';
 import {ProjectFixture} from 'sentry-fixture/project';
 import {RRWebInitFrameEventsFixture} from 'sentry-fixture/replay/rrweb';
+import {ReplayErrorFixture} from 'sentry-fixture/replayError';
 import {ReplayRecordFixture} from 'sentry-fixture/replayRecord';
 
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import EventReplay from 'sentry/components/events/eventReplay';
+import ConfigStore from 'sentry/stores/configStore';
 import {
   useHasOrganizationSentAnyReplayEvents,
   useReplayOnboardingSidebarPanel,
@@ -14,35 +17,72 @@ import {
 import useReplayReader from 'sentry/utils/replays/hooks/useReplayReader';
 import ReplayReader from 'sentry/utils/replays/replayReader';
 import useProjects from 'sentry/utils/useProjects';
+import type {ReplayError} from 'sentry/views/replays/types';
 
 jest.mock('sentry/utils/replays/hooks/useReplayOnboarding');
 jest.mock('sentry/utils/replays/hooks/useReplayReader');
 jest.mock('sentry/utils/useProjects');
 
-const now = new Date();
+const mockEventTimestamp = new Date('2022-09-22T16:59:41Z');
+const mockReplayId = '761104e184c64d439ee1014b72b4d83b';
+
+const mockErrors: ReplayError[] = [
+  ReplayErrorFixture({
+    id: '1',
+    issue: 'JAVASCRIPT-101',
+    'issue.id': 101,
+    'error.value': ['Something bad happened.'],
+    'error.type': ['error'],
+    'project.name': 'javascript',
+    timestamp: mockEventTimestamp.toISOString(),
+    title: 'Something bad happened.',
+  }),
+  ReplayErrorFixture({
+    id: '2',
+    issue: 'JAVASCRIPT-102',
+    'issue.id': 102,
+    'error.value': ['Something bad happened 2.'],
+    'error.type': ['error'],
+    'project.name': 'javascript',
+    timestamp: mockEventTimestamp.toISOString(),
+    title: 'Something bad happened 2.',
+  }),
+];
+
 const mockReplay = ReplayReader.factory({
-  replayRecord: ReplayRecordFixture({started_at: now}),
-  errors: [],
-  attachments: RRWebInitFrameEventsFixture({timestamp: now}),
+  replayRecord: ReplayRecordFixture({
+    browser: {
+      name: 'Chrome',
+      version: '110.0.0',
+    },
+  }),
+  errors: mockErrors,
+  attachments: RRWebInitFrameEventsFixture({
+    timestamp: new Date('Sep 22, 2022 4:58:39 PM UTC'),
+  }),
 });
 
 jest.mocked(useReplayReader).mockImplementation(() => {
   return {
     attachments: [],
-    errors: [],
+    errors: mockErrors,
     fetchError: undefined,
     fetching: false,
     onRetry: jest.fn(),
     projectSlug: ProjectFixture().slug,
     replay: mockReplay,
-    replayId: ReplayRecordFixture({}).id,
+    replayId: mockReplayId,
     replayRecord: ReplayRecordFixture(),
   };
 });
 
+const mockIsFullscreen = jest.fn();
+
 jest.mock('screenfull', () => ({
   enabled: true,
-  isFullscreen: false,
+  get isFullscreen() {
+    return mockIsFullscreen();
+  },
   request: jest.fn(),
   exit: jest.fn(),
   on: jest.fn(),
@@ -62,12 +102,15 @@ describe('EventReplay', function () {
     features: ['session-replay'],
   });
 
+  const mockEvent = EventFixture({
+    entries: [],
+    tags: [],
+    platform: 'javascript',
+    dateCreated: mockEventTimestamp.getTime(),
+  });
+
   const defaultProps = {
-    event: EventFixture({
-      entries: [],
-      tags: [],
-      platform: 'javascript',
-    }),
+    event: mockEvent,
     projectSlug: 'project-slug',
   };
 
@@ -177,5 +220,65 @@ describe('EventReplay', function () {
     );
 
     expect(await screen.findByTestId('player-container')).toBeInTheDocument();
+  });
+
+  describe('replay clip', function () {
+    const orgWithClipFlag = OrganizationFixture({
+      features: [...organization.features, 'issue-details-inline-replay-viewer'],
+    });
+
+    beforeEach(() => {
+      MockUseHasOrganizationSentAnyReplayEvents.mockReturnValue({
+        hasOrgSentReplays: true,
+        fetching: false,
+      });
+      MockUseReplayOnboardingSidebarPanel.mockReturnValue({
+        activateSidebar: jest.fn(),
+      });
+
+      const user = ConfigStore.get('user');
+
+      ConfigStore.set('user', {
+        ...user,
+        options: {...user.options, issueDetailsNewExperienceQ42023: true},
+      });
+    });
+
+    it('adds event and issue information to breadcrumbs', async () => {
+      mockIsFullscreen.mockReturnValue(true);
+
+      render(
+        <EventReplay
+          {...defaultProps}
+          event={EventFixture({
+            ...mockEvent,
+            id: '1',
+            contexts: {
+              replay: {
+                replay_id: '761104e184c64d439ee1014b72b4d83b',
+              },
+            },
+          })}
+          group={GroupFixture({id: '101'})}
+        />,
+        {
+          organization: orgWithClipFlag,
+        }
+      );
+
+      // Event that matches ID 1 should be shown as "This Event"
+      expect(await screen.findByText('Error: This Event')).toBeInTheDocument();
+      expect(screen.getByText('JAVASCRIPT-101')).toBeInTheDocument();
+
+      // Other events should link to the event and issue
+      expect(screen.getByRole('link', {name: '2'})).toHaveAttribute(
+        'href',
+        '/organizations/org-slug/issues/102/events/2/#replay'
+      );
+      expect(screen.getByRole('link', {name: 'JAVASCRIPT-102'})).toHaveAttribute(
+        'href',
+        '/organizations/org-slug/issues/102/'
+      );
+    });
   });
 });
