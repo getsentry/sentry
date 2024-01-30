8c6d328e6ea5f2590df5baf6fa09fcb7934cd579
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-01-30 13:52:15 +0100

    feat(settings): table style (#64159)

--- static/app/views/settings/projectMetrics/blockButton.tsx
+++ static/app/views/settings/projectMetrics/blockButton.tsx
@@ -1,32 +1,45 @@
 import type {BaseButtonProps} from 'sentry/components/button';
 import {Button} from 'sentry/components/button';
 import Confirm from 'sentry/components/confirm';
+import {Tooltip} from 'sentry/components/tooltip';
 import {IconNot, IconPlay} from 'sentry/icons';
 import {t} from 'sentry/locale';
 
-export interface BlockButtonProps extends BaseButtonProps {
+import type {OpenConfirmOptions} from '../../../components/confirm';
+
+export interface BlockButtonProps
+  extends BaseButtonProps,
+    Pick<OpenConfirmOptions, 'message'> {
+  hasAccess: boolean;
   isBlocked: boolean;
   onConfirm: () => void;
 }
 
-export function BlockMetricButton({isBlocked, onConfirm, ...props}: BlockButtonProps) {
+export function BlockButton({isBlocked, onConfirm, ...props}: BlockButtonProps) {
+  const button = (
+    <Button
+      {...props}
+      icon={isBlocked ? <IconPlay size="xs" /> : <IconNot size="xs" />}
+      disabled={!props.hasAccess || props.disabled}
+    >
+      {isBlocked ? t('Unblock') : t('Block')}
+    </Button>
+  );
+
   return (
     <Confirm
       priority="danger"
       onConfirm={onConfirm}
-      confirmText={isBlocked ? t('Unblock Metric') : t('Block Metric')}
-      message={
-        isBlocked
-          ? t('Are you sure you want to unblock this metric?')
-          : t('Are you sure you want to block this metric?')
-      }
+      message={props.message}
+      confirmText={isBlocked ? t('Unblock') : t('Block')}
     >
-      <Button
-        icon={isBlocked ? <IconPlay size="xs" /> : <IconNot size="xs" />}
-        {...props}
-      >
-        {isBlocked ? t('Unblock') : t('Block')}
-      </Button>
+      {props.hasAccess ? (
+        button
+      ) : (
+        <Tooltip title={t('You do not have permissions to edit metrics.')}>
+          {button}
+        </Tooltip>
+      )}
     </Confirm>
   );
 }
