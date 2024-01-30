a286d2ffaff44de3c3b1de0a5b9fdffd6c8b36b4
Sebastian Zivota <loewenheim@mailbox.org>
2024-01-30 15:37:51 +0100

    Make debug files endpoint private

--- src/sentry/api/endpoints/debug_files.py
+++ src/sentry/api/endpoints/debug_files.py
@@ -180,9 +180,9 @@ class ProguardArtifactReleasesEndpoint(ProjectEndpoint):
 class DebugFilesEndpoint(ProjectEndpoint):
     owner = ApiOwner.OWNERS_NATIVE
     publish_status = {
-        "DELETE": ApiPublishStatus.UNKNOWN,
-        "GET": ApiPublishStatus.UNKNOWN,
-        "POST": ApiPublishStatus.UNKNOWN,
+        "DELETE": ApiPublishStatus.PRIVATE,
+        "GET": ApiPublishStatus.PRIVATE,
+        "POST": ApiPublishStatus.PRIVATE,
     }
     permission_classes = (ProjectReleasePermission,)
 
