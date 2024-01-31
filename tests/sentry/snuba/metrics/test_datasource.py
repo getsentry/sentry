68ab1bfa497d253ce782e03c849a1a399c8639fc
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-01-31 08:42:41 +0100

    Update test

--- tests/sentry/snuba/metrics/test_datasource.py
+++ tests/sentry/snuba/metrics/test_datasource.py
@@ -39,7 +39,7 @@ class DatasourceTestCase(BaseMetricsLayerTestCase, TestCase):
             )
         )
 
-        custom_mri = "d:custom/page_load@millisecond"
+        custom_mri = "d:custom/PageLoad@millisecond"
         self.store_metric(
             self.project.organization.id,
             self.project.id,
