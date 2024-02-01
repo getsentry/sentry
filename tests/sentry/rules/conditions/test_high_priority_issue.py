9eeb7034e85a84ff5877eaf7e218da18326bb6d7
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-02-01 14:22:22 -0800

    Use priority for high priority alerts

--- tests/sentry/rules/conditions/test_high_priority_issue.py
+++ tests/sentry/rules/conditions/test_high_priority_issue.py
@@ -1,3 +1,4 @@
+from sentry.issues.priority import PriorityLevel
 from sentry.rules.conditions.high_priority_issue import HighPriorityIssueCondition
 from sentry.testutils.cases import RuleTestCase
 from sentry.testutils.helpers.features import with_feature
@@ -28,4 +29,27 @@ class HighPriorityIssueConditionTest(RuleTestCase):
         event.group.data["metadata"] = {"severity": "0.0"}
         self.assertPasses(rule, event, is_new=False, has_reappeared=False, has_escalated=True)
         self.assertPasses(rule, event, is_new=False, has_reappeared=False, has_escalated=True)
+        self.assertDoesNotPass(rule, event, is_new=True, has_reappeared=False, has_escalated=False)
+
+    @with_feature("projects:high-priority-alerts")
+    @with_feature("projects:issue-priority")
+    def test_applies_correctly_with_priority(self):
+        rule = self.get_rule()
+        event = self.get_event()
+
+        # This will always pass
+        event.group.update(priority=PriorityLevel.HIGH)
+        self.assertPasses(rule, event, is_new=False, has_reappeared=False, has_escalated=False)
+        self.assertPasses(rule, event, is_new=True, has_reappeared=False, has_escalated=True)
+        self.assertPasses(rule, event, is_new=False, has_reappeared=False, has_escalated=False)
+        self.assertPasses(rule, event, is_new=True, has_reappeared=True, has_escalated=False)
+
+        # This will never pass
+        event.group.update(priority=PriorityLevel.LOW)
+        self.assertDoesNotPass(rule, event, is_new=True, has_reappeared=False, has_escalated=False)
+        self.assertDoesNotPass(rule, event, is_new=False, has_reappeared=False, has_escalated=False)
+
+        # This will never pass
+        event.group.update(priority=PriorityLevel.MEDIUM)
+        self.assertDoesNotPass(rule, event, is_new=False, has_reappeared=False, has_escalated=True)
         self.assertDoesNotPass(rule, event, is_new=True, has_reappeared=False, has_escalated=False)
