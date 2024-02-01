416f858cd75ecf9398b9cce72f20307228b35033
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-01-31 17:12:41 -0800

    feat(ai-autofix): Implement a new step-based autofix  (#64319)
    - Update call to seer autofix with better checks for the issue/event
    before sending (still locked to getsentry/sentry repo)
    - Receive step-based autofix status in real time from seer service
    
    **tested e2e locally with frontend and seer service**
    
    
    Frontend UI for reference of what this looks like:
    <img width="919" alt="Screenshot 2024-01-30 at 1 00 03 PM"
    src="https://github.com/getsentry/sentry/assets/30991498/c49b04fe-4e07-4ae7-8d29-192485b7eadc">

--- src/sentry/tasks/ai_autofix.py
+++ src/sentry/tasks/ai_autofix.py
@@ -16,11 +16,20 @@ def ai_autofix_check_for_timeout(group_id: int, created_at: str):
     autofix_data = metadata.get("autofix", {})
     # created_at is checked to make sure that the correct run is being marked as completed
     if autofix_data.get("status") == "PROCESSING" and autofix_data.get("createdAt") == created_at:
+        steps = autofix_data.get("steps", [])
+
+        for step in steps:
+            if step.get("status") == "PROCESSING":
+                step["status"] = "ERROR"
+            if step.get("status") == "PENDING":
+                step["status"] = "CANCELLED"
+
         metadata["autofix"] = {
             **autofix_data,
             "fix": None,
-            "status": "COMPLETED",
+            "status": "ERROR",
             "completedAt": datetime.now().isoformat(),
+            "steps": steps,
         }
         group.data["metadata"] = metadata
         group.save()
