d7e4b27bc2bdd44b1c15f5c55f1b603ca5260a14
17hogeju <17hogeju@gmail.com>
2024-02-01 15:14:40 -0800

    feat(priority): Add priorityLockedAt to serializer

--- src/sentry/api/serializers/models/group.py
+++ src/sentry/api/serializers/models/group.py
@@ -353,6 +353,7 @@ class GroupSerializerBase(Serializer, ABC):
         if features.has("projects:issue-priority", obj.project, actor=None):
             priority_label = PRIORITY_LEVEL_TO_STR[obj.priority] if obj.priority else None
             group_dict["priority"] = priority_label
+            group_dict["priorityLockedAt"] = obj.priority_locked_at
 
         # This attribute is currently feature gated
         if "is_unhandled" in attrs:
