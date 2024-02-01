e6e9dc223a3beae95c4c0aef4d2657aa8809c8b4
Colleen O'Rourke <colleen@sentry.io>
2024-02-01 15:51:24 -0800

    ref(slack): Don't recreate actions (#64431)
    We noticed during prod testing that performing actions on a notification
    kept timing out because we rebuild the notification after a selection is
    made (e.g. resolving an issue) to include text about what just happened
    (issue resolved by colleen) and it was taking longer than the allotted 3
    seconds. This PR skips re-building the actions as [building the list of
    members](https://github.com/getsentry/sentry/blob/master/src/sentry/integrations/slack/message_builder/issues.py#L199)
    for assignment was taking a long time, and we don't render buttons after
    an action is taken anyway.
    
    Note that this is a quick short term solution - we should [update the
    message](https://api.slack.com/interactivity/handling#updating_message_response)
    instead, but that'll take a little bit more time.

--- src/sentry/integrations/slack/webhooks/action.py
+++ src/sentry/integrations/slack/webhooks/action.py
@@ -523,16 +523,17 @@ class SlackActionEndpoint(Endpoint):
             except client.ApiError as error:
                 return self.api_error(slack_request, group, identity_user, error, "status_dialog")
 
-            attachment = SlackIssuesMessageBuilder(
+            blocks = SlackIssuesMessageBuilder(
                 group,
                 identity=identity,
                 actions=[action],
                 tags=original_tags_from_request,
                 rules=[rule] if rule else None,
+                issue_details=True,
                 skip_fallback=True,
             ).build()
             body = self.construct_reply(
-                attachment, is_message=slack_request.callback_data["is_message"]
+                blocks, is_message=slack_request.callback_data["is_message"]
             )
             # use the original response_url to update the link attachment
             slack_client = SlackClient(integration_id=slack_request.integration.id)
@@ -618,8 +619,6 @@ class SlackActionEndpoint(Endpoint):
 
         # Reload group as it may have been mutated by the action
         group = Group.objects.get(id=group.id)
-
-        use_block_kit = features.has("organizations:slack-block-kit", group.project.organization)
 
         if use_block_kit:
             response = SlackIssuesMessageBuilder(
