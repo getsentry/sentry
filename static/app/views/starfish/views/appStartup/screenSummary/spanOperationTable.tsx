dd6a346027142fb0187e2d7e01ee46c7e7b69e09
Nar Saynorath <nar.saynorath@sentry.io>
2024-02-02 10:21:32 -0500

    feat(mobile-starfish): Replace release labels with R1, R2 (#64408)
    In order to simplify the text being shown in headers, we've decided to
    modify the release selector to prefix the primary release with "R1" and
    the secondary release with "R2" and update usages in the table headers
    and metrics ribbon to fit this abbreviation
    
    Updated both the app start module as well as the screen loads
    module to be consistent since they share the release selector and both
    modules will need this change eventually.

--- static/app/views/starfish/views/appStartup/screenSummary/spanOperationTable.tsx
+++ static/app/views/starfish/views/appStartup/screenSummary/spanOperationTable.tsx
@@ -24,9 +24,12 @@ import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import {normalizeUrl} from 'sentry/utils/withDomainRequired';
+import {
+  PRIMARY_RELEASE_ALIAS,
+  SECONDARY_RELEASE_ALIAS,
+} from 'sentry/views/starfish/components/releaseSelector';
 import {OverflowEllipsisTextContainer} from 'sentry/views/starfish/components/textAlign';
 import {SpanMetricsField} from 'sentry/views/starfish/types';
-import {formatVersionAndCenterTruncate} from 'sentry/views/starfish/utils/centerTruncate';
 import {STARFISH_CHART_INTERVAL_FIDELITY} from 'sentry/views/starfish/utils/constants';
 import {appendReleaseFilters} from 'sentry/views/starfish/utils/releaseComparison';
 import {SpanOpSelector} from 'sentry/views/starfish/views/appStartup/screenSummary/spanOpSelector';
@@ -64,8 +67,6 @@ export function SpanOperationTable({
   const cursor = decodeScalar(location.query?.[MobileCursors.SPANS_TABLE]);
 
   const spanOp = decodeScalar(location.query[SpanMetricsField.SPAN_OP]) ?? '';
-  const truncatedPrimary = formatVersionAndCenterTruncate(primaryRelease ?? '', 15);
-  const truncatedSecondary = formatVersionAndCenterTruncate(secondaryRelease ?? '', 15);
 
   const searchQuery = new MutableSearch([
     'transaction.op:ui.load',
@@ -127,11 +128,11 @@ export function SpanOperationTable({
     'count()': t('Total Count'),
     [`avg_if(${SPAN_SELF_TIME},release,${primaryRelease})`]: t(
       'Duration (%s)',
-      truncatedPrimary
+      PRIMARY_RELEASE_ALIAS
     ),
     [`avg_if(${SPAN_SELF_TIME},release,${secondaryRelease})`]: t(
       'Duration (%s)',
-      truncatedSecondary
+      SECONDARY_RELEASE_ALIAS
     ),
     ['time_spent_percentage()']: t('Total Time Spent'),
   };
