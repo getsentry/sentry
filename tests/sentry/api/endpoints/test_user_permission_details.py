96f048d35c03fc284cd50c41f6748703b7952f64
schew2381 <seiji.chew@sentry.io>
2024-02-01 16:19:13 -0800

    write tests for staff permission changes

--- tests/sentry/api/endpoints/test_user_permission_details.py
+++ tests/sentry/api/endpoints/test_user_permission_details.py
@@ -13,53 +13,39 @@ class UserDetailsTest(APITestCase):
 
     def setUp(self):
         super().setUp()
-        self.user = self.create_user(is_superuser=True)
-        self.login_as(user=self.user, superuser=True)
-        self.add_user_permission(self.user, "users.admin")
+        self.superuser = self.create_user(is_superuser=True)
+        self.add_user_permission(self.superuser, "users.admin")
 
         self.staff_user = self.create_user(is_staff=True)
         self.add_user_permission(self.staff_user, "users.admin")
 
-    def test_fails_without_superuser(self):
-        self.user = self.create_user(is_superuser=False)
-        self.login_as(self.user)
+        self.normal_user = self.create_user(is_superuser=False, is_staff=False)
 
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 403
-
-        self.user.update(is_superuser=True)
-        self.login_as(self.user, superuser=True)
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 403
-
-    def test_fails_without_staff(self):
-        self.user = self.create_user(is_staff=False)
-        self.login_as(self.user)
-
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 403
-
-        self.user.update(is_staff=True)
-        self.login_as(self.user, staff=True)
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 403
+    def test_fails_without_superuser_or_superuser(self):
+        self.login_as(self.normal_user)
+        response = self.get_response("me", "broadcasts.admin")
+        assert response.status_code == 403
 
     def test_fails_without_users_admin_permission(self):
-        self.user = self.create_user(is_superuser=True)
-        self.login_as(self.user, superuser=True)
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 403
+        self.superuser_and_staff = self.create_user(is_superuser=True, is_staff=True)
+        self.login_as(self.superuser_and_staff, superuser=True, staff=True)
+
+        # We are active superuser and staff but lack the users.admin permission
+        response = self.get_response("me", "broadcasts.admin", status_code=403)
+        assert response.status_code == 403
 
 
 class UserPermissionDetailsGetTest(UserDetailsTest):
-    def test_with_permission(self):
-        self.add_user_permission(self.user, "broadcasts.admin")
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 204
+    method = "GET"
 
-    def test_without_permission(self):
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 404
+    def test_superuser_with_permission(self):
+        self.login_as(self.superuser, superuser=True)
+        self.add_user_permission(self.superuser, "broadcasts.admin")
+        self.get_success_response("me", "broadcasts.admin", status_code=204)
+
+    def test_superuser_without_permission(self):
+        self.login_as(self.superuser, superuser=True)
+        self.get_error_response("me", "broadcasts.admin", status_code=404)
 
     @with_feature("auth:enterprise-staff-cookie")
     @patch.object(StaffPermission, "has_permission", wraps=StaffPermission().has_permission)
@@ -67,8 +53,7 @@ class UserPermissionDetailsGetTest(UserDetailsTest):
         self.login_as(self.staff_user, staff=True)
         self.add_user_permission(self.staff_user, "broadcasts.admin")
 
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 204
+        self.get_success_response("me", "broadcasts.admin", status_code=204)
         # ensure we fail the scope check and call is_active_staff
         assert mock_has_permission.call_count == 1
 
@@ -77,8 +62,7 @@ class UserPermissionDetailsGetTest(UserDetailsTest):
     def test_staff_without_permission(self, mock_has_permission):
         self.login_as(self.staff_user, staff=True)
 
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 404
+        self.get_error_response("me", "broadcasts.admin", status_code=404)
         # ensure we fail the scope check and call is_active_staff
         assert mock_has_permission.call_count == 1
 
@@ -86,32 +70,90 @@ class UserPermissionDetailsGetTest(UserDetailsTest):
 class UserPermissionDetailsPostTest(UserDetailsTest):
     method = "POST"
 
-    def test_with_permission(self):
-        self.add_user_permission(self.user, "broadcasts.admin")
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 410
-        assert UserPermission.objects.filter(user=self.user, permission="broadcasts.admin").exists()
+    def test_superuser_with_permission(self):
+        self.login_as(self.superuser, superuser=True)
 
-    def test_without_permission(self):
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 201
-        assert UserPermission.objects.filter(user=self.user, permission="broadcasts.admin").exists()
+        self.get_success_response("me", "broadcasts.admin", status_code=201)
+        assert UserPermission.objects.filter(
+            user=self.superuser, permission="broadcasts.admin"
+        ).exists()
+
+    def test_superuser_duplicate_permission(self):
+        self.login_as(self.superuser, superuser=True)
+        self.add_user_permission(self.superuser, "broadcasts.admin")
+
+        self.get_error_response("me", "broadcasts.admin", status_code=410)
+        assert UserPermission.objects.filter(
+            user=self.superuser, permission="broadcasts.admin"
+        ).exists()
+
+    @with_feature("auth:enterprise-staff-cookie")
+    @patch.object(StaffPermission, "has_permission", wraps=StaffPermission().has_permission)
+    def test_staff_with_permission(self, mock_has_permission):
+        self.login_as(self.staff_user, staff=True)
+
+        self.get_success_response("me", "broadcasts.admin", status_code=201)
+        assert UserPermission.objects.filter(
+            user=self.staff_user, permission="broadcasts.admin"
+        ).exists()
+        # ensure we fail the scope check and call is_active_staff
+        assert mock_has_permission.call_count == 1
+
+    @with_feature("auth:enterprise-staff-cookie")
+    @patch.object(StaffPermission, "has_permission", wraps=StaffPermission().has_permission)
+    def test_staff_duplicate_permission(self, mock_has_permission):
+        self.login_as(self.staff_user, staff=True)
+        self.add_user_permission(self.staff_user, "broadcasts.admin")
+
+        self.get_error_response("me", "broadcasts.admin", status_code=410)
+        assert UserPermission.objects.filter(
+            user=self.staff_user, permission="broadcasts.admin"
+        ).exists()
+        # ensure we fail the scope check and call is_active_staff
+        assert mock_has_permission.call_count == 1
 
 
 class UserPermissionDetailsDeleteTest(UserDetailsTest):
     method = "DELETE"
 
-    def test_with_permission(self):
-        self.add_user_permission(self.user, "broadcasts.admin")
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 204
+    def test_superuser_with_permission(self):
+        self.login_as(self.superuser, superuser=True)
+        self.add_user_permission(self.superuser, "broadcasts.admin")
+
+        self.get_success_response("me", "broadcasts.admin", status_code=204)
         assert not UserPermission.objects.filter(
-            user=self.user, permission="broadcasts.admin"
+            user=self.superuser, permission="broadcasts.admin"
         ).exists()
 
-    def test_without_permission(self):
-        resp = self.get_response("me", "broadcasts.admin")
-        assert resp.status_code == 404
+    def test_superuser_without_permission(self):
+        self.login_as(self.superuser, superuser=True)
+
+        self.get_error_response("me", "broadcasts.admin", status_code=404)
         assert not UserPermission.objects.filter(
-            user=self.user, permission="broadcasts.admin"
+            user=self.superuser, permission="broadcasts.admin"
         ).exists()
+
+    @with_feature("auth:enterprise-staff-cookie")
+    @patch.object(StaffPermission, "has_permission", wraps=StaffPermission().has_permission)
+    def test_staff_with_permission(self, mock_has_permission):
+        self.login_as(self.staff_user, staff=True)
+        self.add_user_permission(self.staff_user, "broadcasts.admin")
+
+        self.get_success_response("me", "broadcasts.admin", status_code=204)
+        assert not UserPermission.objects.filter(
+            user=self.staff_user, permission="broadcasts.admin"
+        ).exists()
+        # ensure we fail the scope check and call is_active_staff
+        assert mock_has_permission.call_count == 1
+
+    @with_feature("auth:enterprise-staff-cookie")
+    @patch.object(StaffPermission, "has_permission", wraps=StaffPermission().has_permission)
+    def test_staff_without_permission(self, mock_has_permission):
+        self.login_as(self.staff_user, staff=True)
+
+        self.get_error_response("me", "broadcasts.admin", status_code=404)
+        assert not UserPermission.objects.filter(
+            user=self.staff_user, permission="broadcasts.admin"
+        ).exists()
+        # ensure we fail the scope check and call is_active_staff
+        assert mock_has_permission.call_count == 1
