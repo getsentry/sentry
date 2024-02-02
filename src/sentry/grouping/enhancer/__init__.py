getsentry/sentry

commit 83bc51fd031c096ea2eb8343c5a3f2ac6fa38c3b

Small improvements to matchers (#63202)

src/sentry/grouping/enhancer/matchers.py
@@ -52,7 +52,6 @@ MATCHERS = {
 
 
 def _get_function_name(frame_data: dict, platform: Optional[str]):
-
     function_name = get_function_name_for_frame(frame_data, platform)
 
     return function_name or "<unknown>"
@@ -115,14 +114,12 @@ InstanceKey = Tuple[str, str, bool]
 
 
 class FrameMatch(Match):
-
     # Global registry of matchers
     instances: Dict[InstanceKey, Match] = {}
     field: Any = None
 
     @classmethod
     def from_key(cls, key: str, pattern: str, negated: bool) -> Match:
-
         instance_key = (key, pattern, negated)
         if instance_key in cls.instances:
             instance = cls.instances[instance_key]
@@ -134,7 +131,6 @@ class FrameMatch(Match):
 
     @classmethod
     def _from_key(cls, key: str, pattern: str, negated: bool) -> Match:
-
         subclass = {
             "package": PackageMatch,
             "path": PathMatch,
@@ -213,12 +209,10 @@ class PathLikeMatch(FrameMatch):
 
 
 class PackageMatch(PathLikeMatch):
-
     field = "package"
 
 
 class PathMatch(PathLikeMatch):
-
     field = "path"
 
 
@@ -237,11 +231,10 @@ class FamilyMatch(FrameMatch):
 class InAppMatch(FrameMatch):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._ref_val = get_rule_bool(self.pattern)
+        self._ref_val = bool(get_rule_bool(self.pattern))
 
     def _positive_frame_match(self, match_frame, platform, exception_data, cache):
-        ref_val = self._ref_val
-        return ref_val is not None and ref_val == match_frame["in_app"]
+        return self._ref_val == match_frame["in_app"]
 
 
 class FrameFieldMatch(FrameMatch):
@@ -256,17 +249,14 @@ class FrameFieldMatch(FrameMatch):
 
 
 class FunctionMatch(FrameFieldMatch):
-
     field = "function"
 
 
 class ModuleMatch(FrameFieldMatch):
-
     field = "module"
 
 
 class CategoryMatch(FrameFieldMatch):
-
     field = "category"
 
 
@@ -286,49 +276,46 @@ class ExceptionFieldMatch(FrameMatch):
 
 
 class ExceptionTypeMatch(ExceptionFieldMatch):
-
     field_path = ["type"]
 
 
 class ExceptionValueMatch(ExceptionFieldMatch):
-
     field_path = ["value"]
 
 
 class ExceptionMechanismMatch(ExceptionFieldMatch):
-
     field_path = ["mechanism", "type"]
 
 
 class CallerMatch(Match):
-    def __init__(self, caller: FrameMatch):
-        self.caller = caller
+    def __init__(self, inner: FrameMatch):
+        self.inner = inner
 
     @property
     def description(self) -> str:
-        return f"[ {self.caller.description} ] |"
+        return f"[ {self.inner.description} ] |"
 
     def _to_config_structure(self, version):
-        return f"[{self.caller._to_config_structure(version)}]|"
+        return f"[{self.inner._to_config_structure(version)}]|"
 
     def matches_frame(self, frames, idx, platform, exception_data, cache):
-        return idx > 0 and self.caller.matches_frame(
+        return idx > 0 and self.inner.matches_frame(
             frames, idx - 1, platform, exception_data, cache
         )
 
 
 class CalleeMatch(Match):
-    def __init__(self, caller: FrameMatch):
-        self.caller = caller
+    def __init__(self, inner: FrameMatch):
+        self.inner = inner
 
     @property
     def description(self) -> str:
-        return f"| [ {self.caller.description} ]"
+        return f"| [ {self.inner.description} ]"
 
     def _to_config_structure(self, version):
-        return f"|[{self.caller._to_config_structure(version)}]"
+        return f"|[{self.inner._to_config_structure(version)}]"
 
     def matches_frame(self, frames, idx, platform, exception_data, cache):
-        return idx < len(frames) - 1 and self.caller.matches_frame(
+        return idx < len(frames) - 1 and self.inner.matches_frame(
             frames, idx + 1, platform, exception_data, cache
         )

commit abc000c44ee1e8fc657085d4cb8dd236bbb3821d

Fix invalid `app` matcher regression
In #63202 I regressed the behavior of invalid `app` matchers.
If an `app` matcher is not parsable as `bool`, it should *never* match, instead of defaulting to `false`.

This also has some more driveby fixes to the Rust Enhancements error, and paving the way to prefer the Rust result later on.

src/sentry/grouping/enhancer/__init__.py
@@ -179,7 +179,7 @@ def parse_rust_enhancements(
 
             metrics.incr("rust_enhancements.parsing_performed", tags={"source": source})
         except Exception:
-            logger.exception("failed parsing Rust Enhancements from `{source}`")
+            logger.exception("failed parsing Rust Enhancements from `%s`", source)
 
     return rust_enhancements
 
@@ -250,6 +250,13 @@ def compare_rust_enhancers(
                 sentry_sdk.capture_message("Rust Enhancements mismatch")
 
 
+def prefer_rust_enhancers():
+    try:
+        return random.random() < options.get("grouping.rust_enhancers.prefer_rust_result")
+    except Exception:
+        return False
+
+
 class Enhancements:
     # NOTE: You must add a version to ``VERSIONS`` any time attributes are added
     # to this class, s.t. no enhancements lacking these attributes are loaded
@@ -293,14 +300,13 @@ class Enhancements:
             self.rust_enhancements, match_frames, exception_data
         )
 
-        # TODO: Once we are happy with the state of Rust provided enhancements,
-        # update the frames and return before the Python code runs.
-        # for frame, (category, in_app) in zip(frames, rust_enhanced_frames):
-        #     if in_app is not None:
-        #         set_in_app(frame, in_app)
-        #     if category is not None:
-        #         set_path(frame, "data", "category", value=category)
-        # return
+        if rust_enhanced_frames and prefer_rust_enhancers():
+            for frame, (category, in_app) in zip(frames, rust_enhanced_frames):
+                if in_app is not None:
+                    set_in_app(frame, in_app)
+                if category is not None:
+                    set_path(frame, "data", "category", value=category)
+            return
 
         in_memory_cache: dict[str, str] = {}
 

src/sentry/grouping/enhancer/matchers.py
@@ -234,10 +234,11 @@ class FamilyMatch(FrameMatch):
 class InAppMatch(FrameMatch):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self._ref_val = bool(get_rule_bool(self.pattern))
+        self._ref_val = get_rule_bool(self.pattern)
 
     def _positive_frame_match(self, match_frame, exception_data, cache):
-        return self._ref_val == bool(match_frame["in_app"])
+        ref_val = self._ref_val
+        return ref_val is not None and ref_val == match_frame["in_app"]
 
 
 class FrameFieldMatch(FrameMatch):

src/sentry/options/defaults.py
@@ -2009,10 +2009,17 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
-# Rate at which to run the Rust implementation of `apply_modifications_to_frames
-# and compare the results`
+# Rate at which to run the Rust implementation of `apply_modifications_to_frames`
+# and compare the results
 register(
     "grouping.rust_enhancers.modify_frames_rate",
+    default=0.0,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
+
+# Rate at which to prefer the `apply_modifications_to_frames` result of the Rust implementation.
+register(
+    "grouping.rust_enhancers.prefer_rust_result",
     default=0.0,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )