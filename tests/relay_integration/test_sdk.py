35f8fa814db1b04b16e812768ddcab4688d0bc2c
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-02-02 19:48:11 -0500

    ✅ revert to no-silo-test, but annotate the fixture

--- tests/relay_integration/test_sdk.py
+++ tests/relay_integration/test_sdk.py
@@ -13,7 +13,7 @@ from sentry.silo.base import SiloMode
 from sentry.testutils.asserts import assert_mock_called_once_with_partial
 from sentry.testutils.pytest.fixtures import django_db_all
 from sentry.testutils.pytest.relay import adjust_settings_for_relay_tests
-from sentry.testutils.silo import assume_test_silo_mode, region_silo_test
+from sentry.testutils.silo import assume_test_silo_mode, no_silo_test
 from sentry.testutils.skips import requires_kafka
 from sentry.utils.sdk import bind_organization_context, configure_sdk
 
@@ -66,7 +66,7 @@ def post_event_with_sdk(settings, relay_server, wait_for_ingest_consumer):
         yield inner
 
 
-@region_silo_test
+@no_silo_test
 @django_db_all
 @override_settings(SENTRY_PROJECT=1)
 def test_simple(settings, post_event_with_sdk):
@@ -77,7 +77,7 @@ def test_simple(settings, post_event_with_sdk):
     assert event.data["logentry"]["formatted"] == "internal client test"
 
 
-@region_silo_test
+@no_silo_test
 @override_settings(SENTRY_PROJECT=1)
 @django_db_all
 def test_recursion_breaker(settings, post_event_with_sdk):
@@ -95,7 +95,7 @@ def test_recursion_breaker(settings, post_event_with_sdk):
     assert_mock_called_once_with_partial(save, settings.SENTRY_PROJECT, cache_key=f"e:{event_id}:1")
 
 
-@region_silo_test
+@no_silo_test
 @django_db_all
 @override_settings(SENTRY_PROJECT=1)
 def test_encoding(settings, post_event_with_sdk):
@@ -111,7 +111,7 @@ def test_encoding(settings, post_event_with_sdk):
     assert "NotJSONSerializable" in event.data["extra"]["request"]
 
 
-@region_silo_test
+@no_silo_test
 @override_settings(SENTRY_PROJECT=1)
 @django_db_all
 def test_bind_organization_context(default_organization):
@@ -127,7 +127,7 @@ def test_bind_organization_context(default_organization):
     }
 
 
-@region_silo_test
+@no_silo_test
 @override_settings(SENTRY_PROJECT=1)
 @django_db_all
 def test_bind_organization_context_with_callback(settings, default_organization):
@@ -143,7 +143,7 @@ def test_bind_organization_context_with_callback(settings, default_organization)
     assert Hub.current.scope._tags["organization.test"] == "1"
 
 
-@region_silo_test
+@no_silo_test
 @override_settings(SENTRY_PROJECT=1)
 @django_db_all
 def test_bind_organization_context_with_callback_error(settings, default_organization):
