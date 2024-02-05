b9aa31dafa2396c1bd11e969505801df8eea7267
Josh Ferge <josh.ferge@sentry.io>
2024-02-05 09:34:42 -0800

    feat(replays): add rage click issue toggle project option (#64245)
    - [x] adds a project option `sentry:replay_rage_click_issues`
    - [x] modifies project details so its modifiable there
    
    
    Will be used in a follow up. required by
    https://github.com/getsentry/sentry/pull/64241/

--- src/sentry/api/serializers/models/project.py
+++ src/sentry/api/serializers/models/project.py
@@ -210,6 +210,8 @@ def format_options(attrs: dict[str, Any]) -> dict[str, Any]:
             options.get(f"sentry:{FilterTypes.ERROR_MESSAGES}", [])
         ),
         "feedback:branding": options.get("feedback:branding", "1") == "1",
+        "sentry:replay_rage_click_issues": options.get("sentry:replay_rage_click_issues", "1")
+        == "1",
         "quotas:spike-protection-disabled": options.get("quotas:spike-protection-disabled"),
     }
 
