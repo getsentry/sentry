60b361fdb2d2552db3ad789de0eedebdf9741e5a
Julia Hoge <julia.hoge@sentry.io>
2024-02-05 08:55:54 -0800

    feat(priority): Update PUT endpoint to support priority (#64405)
    Closes https://github.com/getsentry/sentry/issues/63268
    
    `PUT /issues/<issue_id>/`
    - [x] This existing endpoint should be modified to support `priority`,
    which will be sent as a string (`high`/ `medium` / `low`).
    - [x] This should set `priority_locked_at` to the current timestamp
    which blocks further automatic priority changes.
    - [x] Ensure that an Activity entry is made: “[User X] updated the
    priority value of this issue to be X priority”

--- src/sentry/api/helpers/group_index/validators/group.py
+++ src/sentry/api/helpers/group_index/validators/group.py
@@ -4,6 +4,7 @@ from typing import Any
 from rest_framework import serializers
 
 from sentry.api.fields import ActorField
+from sentry.issues.priority import PRIORITY_UPDATE_CHOICES
 from sentry.models.actor import Actor
 from sentry.models.group import STATUS_UPDATE_CHOICES
 from sentry.models.team import Team
@@ -38,6 +39,9 @@ class GroupValidator(serializers.Serializer):
     # in minutes, max of one week
     ignoreUserWindow = serializers.IntegerField(max_value=7 * 24 * 60)
     assignedTo = ActorField()
+    priority = serializers.ChoiceField(
+        choices=list(zip(PRIORITY_UPDATE_CHOICES.keys(), PRIORITY_UPDATE_CHOICES.keys()))
+    )
 
     # TODO(dcramer): remove in 9.0
     # for the moment, the CLI sends this for any issue update, so allow nulls
