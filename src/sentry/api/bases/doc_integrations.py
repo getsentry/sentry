19aab57b21f21dc7fe7de402e12167983de7f1db
schew2381 <seiji.chew@sentry.io>
2024-02-06 13:32:08 -0800

    chore(staff): Let staff access doc integrations endpoints

--- src/sentry/api/bases/doc_integrations.py
+++ src/sentry/api/bases/doc_integrations.py
@@ -5,7 +5,7 @@ from rest_framework.request import Request
 
 from sentry.api.base import Endpoint
 from sentry.api.bases.integration import PARANOID_GET
-from sentry.api.permissions import SentryPermission
+from sentry.api.permissions import SentryPermission, StaffPermissionMixin
 from sentry.api.validators.doc_integration import METADATA_PROPERTIES
 from sentry.auth.superuser import is_active_superuser
 from sentry.models.integrations.doc_integration import DocIntegration
@@ -17,9 +17,15 @@ class DocIntegrationsPermission(SentryPermission):
     scope_map = {"GET": PARANOID_GET}
 
     def has_permission(self, request: Request, view: object) -> bool:
+        """
+        We only want non-sentry employees to be able to access GET. PUT + DEL
+        should only be accessible through _admin.
+        """
         if not super().has_permission(request, view):
             return False
 
+        # TODO(schew2381): Remove superuser check once staff feature flag is rolled out
+        # We want to limit POST + DEL to staff only through the mixin
         if is_active_superuser(request) or request.method == "GET":
             return True
 
@@ -28,9 +34,15 @@ class DocIntegrationsPermission(SentryPermission):
     def has_object_permission(
         self, request: Request, view: object, doc_integration: DocIntegration
     ) -> bool:
+        """
+        We only want non-sentry employees to be able to access GET, and only for
+        published integrations. PUT + DEL should only be accessible through _admin.
+        """
         if not hasattr(request, "user") or not request.user:
             return False
 
+        # TODO(schew2381): Remove superuser check once staff feature flag is rolled out
+        # We want to limit POST + DEL to staff only through the mixin
         if is_active_superuser(request):
             return True
 
@@ -40,14 +52,26 @@ class DocIntegrationsPermission(SentryPermission):
         return False
 
 
+class DocIntegrationsAndStaffPermission(StaffPermissionMixin, DocIntegrationsPermission):
+    pass
+
+
 class DocIntegrationsBaseEndpoint(Endpoint):
-    permission_classes = (DocIntegrationsPermission,)
+    """
+    Base endpoint used for doc integration collection endpoints.
+    """
+
+    permission_classes = (DocIntegrationsAndStaffPermission,)
 
     def generate_incoming_metadata(self, request: Request) -> JSONData:
         return {k: v for k, v in request.json_body.items() if k in METADATA_PROPERTIES}
 
 
 class DocIntegrationBaseEndpoint(DocIntegrationsBaseEndpoint):
+    """
+    Base endpoint used for doc integration item endpoints.
+    """
+
     def convert_args(self, request: Request, doc_integration_slug: str, *args, **kwargs):
         try:
             doc_integration = DocIntegration.objects.get(slug=doc_integration_slug)
