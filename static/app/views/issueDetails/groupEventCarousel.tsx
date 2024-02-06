a0ae77e37d3fdc2bdd8d19769fccddbad12b43b4
Scott Cooper <scttcper@gmail.com>
2024-02-05 16:02:12 -0800

    feat(issues): Remove trace timeline from behind user flag (#64619)

--- static/app/views/issueDetails/groupEventCarousel.tsx
+++ static/app/views/issueDetails/groupEventCarousel.tsx
@@ -39,7 +39,6 @@ import {useLocation} from 'sentry/utils/useLocation';
 import useMedia from 'sentry/utils/useMedia';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
-import {useUser} from 'sentry/utils/useUser';
 import {normalizeUrl} from 'sentry/utils/withDomainRequired';
 import EventCreatedTooltip from 'sentry/views/issueDetails/eventCreatedTooltip';
 import {TraceLink} from 'sentry/views/issueDetails/traceTimeline/traceLink';
@@ -363,7 +362,6 @@ export function GroupEventActions({event, group, projectSlug}: GroupEventActions
 export function GroupEventCarousel({event, group, projectSlug}: GroupEventCarouselProps) {
   const organization = useOrganization();
   const location = useLocation();
-  const user = useUser();
 
   const latencyThreshold = 30 * 60 * 1000; // 30 minutes
   const isOverLatencyThreshold =
@@ -379,7 +377,7 @@ export function GroupEventCarousel({event, group, projectSlug}: GroupEventCarous
     text: event.id,
   });
 
-  const hasTraceTimeline = hasTraceTimelineFeature(organization, user);
+  const hasTraceTimeline = hasTraceTimelineFeature(organization);
 
   return (
     <CarouselAndButtonsWrapper>
