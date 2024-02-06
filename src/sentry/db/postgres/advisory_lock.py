2f3828ba14ca5b38d829774ce6ddebd50c43f245
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-02-05 22:46:24 -0800

    Adds sleep based advisory lock logic, comments

--- src/sentry/db/postgres/advisory_lock.py
+++ src/sentry/db/postgres/advisory_lock.py
@@ -1,14 +1,10 @@
 import contextlib
-import logging
-import threading
-from typing import Optional
+import time
 
 import sentry_sdk
-from django.db import OperationalError, connections
+from django.db import connections
 
 from sentry.utils import metrics
-
-logger = logging.getLogger("advisory_lock_logger")
 
 
 class AdvisoryLockAcquireTimeoutException(Exception):
@@ -19,27 +15,18 @@ class AdvisoryLockUnlockException(Exception):
     pass
 
 
-def clean_up_advisory_lock(
-    lock_acquired: bool, using: str, lock_id: int, original_lock_timeout: Optional[str]
-):
+def clean_up_advisory_lock(using: str, lock_id: int):
     """
     Only meant to be used by the `advisory_lock` decorator. This function will attempt
     to reset the state of the advisory lock with the given key, and will set the connection
     timeout to the specified time as well.
-    :param lock_acquired:
     :param using:
     :param lock_id:
-    :param original_lock_timeout:
     :return:
     """
     try:
         with connections[using].cursor() as cursor:
-            if lock_acquired:
-                cursor.execute("SELECT pg_advisory_unlock(%s)", [lock_id])
-                # While it's not ideal to keep the lock timeout reset tied to lock
-                # acquisition, it makes testing this functionality possible.
-                # Since this is only used for explicit locking, we should be ok.
-                cursor.execute("SET lock_timeout=%s", [original_lock_timeout])
+            cursor.execute("SELECT pg_advisory_unlock(%s)", [lock_id])
 
     except Exception:
         connections[using].close()
@@ -49,40 +36,43 @@ def clean_up_advisory_lock(
 
 @contextlib.contextmanager
 def advisory_lock(using: str, lock_id: int, lock_timeout_seconds: int, lock_metric_name: str):
+    """
+    Context manager used to acquire an advisory lock on the given lock ID. If
+    the lock is held by another session, the context manager will reattempt to
+    obtain the lock every second until the maximum duration provided in
+    `lock_timeout_seconds` is reached.
+
+
+
+    :param using: The database against which the lock is to be acquired.
+    :param lock_id: An integer value representing a unique lock ID
+    :param lock_timeout_seconds: The maximum amount of time in seconds to
+    attempt acquiring the lock. Must be less than 30s.
+    :param lock_metric_name:
+    :return:
+    """
     assert lock_timeout_seconds >= 1, "lock_timeout_seconds must be at least 1 second"
+    assert lock_timeout_seconds <= 30, "lock_timeout_seconds must be less than 30 seconds"
 
     lock_acquired = False
-    with connections[using].cursor() as cursor:
-        cursor.execute("show lock_timeout")
-        # Obtain the previously set lock timeout, so we can reset it after unlocking.
-        # This is returned as a string with the units included (E.x. "5s")
-        orig_timeout = cursor.fetchone()[0]
-        cursor.execute(f"SET local lock_timeout='{lock_timeout_seconds}s'")
-
     try:
         with metrics.timer(lock_metric_name), connections[using].cursor() as cursor:
-            cursor.execute("SELECT pg_advisory_lock(%s)", [lock_id])
-            result = cursor.fetchone()[0]
+            for i in range(lock_timeout_seconds + 1):
+                cursor.execute("SELECT pg_try_advisory_lock(%s)", [lock_id])
+                lock_acquired = cursor.fetchone()[0]
+                if lock_acquired:
+                    yield
+                    return
 
-            lock_acquired = True
-            logger.info(
-                "lock_acquired",
-                extra={
-                    "lock_id": lock_id,
-                    "thread_id": threading.get_ident(),
-                    "result": result,
-                    "connection_id": connections[using],
-                },
+                # Unable to acquire lock, so spin wait up until the maximum timeout
+                if i < lock_timeout_seconds:
+                    time.sleep(1)
+            raise AdvisoryLockAcquireTimeoutException(
+                f"Failed to acquire lock within {lock_timeout_seconds}s"
             )
-            yield
-    except OperationalError as e:
-        if "LockNotAvailable" in str(e):
-            raise AdvisoryLockAcquireTimeoutException() from e
-        raise
     finally:
-        clean_up_advisory_lock(
-            lock_acquired=lock_acquired,
-            using=using,
-            lock_id=lock_id,
-            original_lock_timeout=orig_timeout,
-        )
+        if lock_acquired:
+            clean_up_advisory_lock(
+                using=using,
+                lock_id=lock_id,
+            )
---
+++ tests/sentry/db/postgres/test_advisory_lock.py
@@ -0,0 +1,192 @@
+import functools
+import threading
+import time
+from typing import Any, Callable, Optional
+
+import pytest
+from django.db import connections, transaction
+
+from sentry.db.postgres.advisory_lock import (
+    AdvisoryLockAcquireTimeoutException,
+    advisory_lock,
+    clean_up_advisory_lock,
+)
+from sentry.testutils.cases import TestCase
+
+advisory_lock_key = 1234
+
+
+def acquire_advisory_lock_with_barrier(
+    lock_key: int,
+    lock_timeout: int,
+    entry_barrier: Optional[threading.Barrier] = None,
+    exit_barrier: Optional[threading.Barrier] = None,
+):
+    with advisory_lock(
+        using="default",
+        lock_id=lock_key,
+        lock_timeout_seconds=lock_timeout,
+        lock_metric_name="advisory_lock_test",
+    ):
+        if entry_barrier is not None:
+            entry_barrier.wait()
+
+        if exit_barrier is not None:
+            exit_barrier.wait()
+
+
+def wait_until_barrier_hit(
+    barrier: threading.Barrier, min_waiters: int, maximum_timeout_seconds: int
+):
+    for i in range(maximum_timeout_seconds):
+        if barrier.n_waiting >= min_waiters:
+            return
+
+        time.sleep(1)
+
+    raise Exception("Maximum wait time exceeded for barrier to be reached by parties")
+
+
+def wrap_with_connection_closure(c: Callable[..., Any]) -> Callable[..., Any]:
+    def wrapper(*args: Any, **kwds: Any) -> Any:
+        try:
+            return c(*args, **kwds)
+        finally:
+            for connection in connections.all():
+                connection.close()
+
+    functools.update_wrapper(wrapper, c)
+    return wrapper
+
+
+def get_advisory_lock_thread(
+    lock_key: int,
+    lock_timeout: int,
+    entry_barrier: threading.Barrier,
+    exit_barrier: threading.Barrier,
+) -> threading.Thread:
+    return threading.Thread(
+        target=wrap_with_connection_closure(
+            lambda: acquire_advisory_lock_with_barrier(
+                lock_key=lock_key,
+                entry_barrier=entry_barrier,
+                exit_barrier=exit_barrier,
+                lock_timeout=lock_timeout,
+            )
+        )
+    )
+
+
+class TestAdvisoryLock(TestCase):
+    def setUp(self):
+        self.original_lock_timeout = "30s"
+        with connections["default"].cursor() as cursor:
+            cursor.execute(f"SET lock_timeout='{self.original_lock_timeout}'")
+
+    def test_lock_and_unlock_happy_path(self):
+        with advisory_lock(
+            using="default",
+            lock_id=advisory_lock_key,
+            lock_timeout_seconds=1,
+            lock_metric_name="advisory_lock",
+        ):
+            pass
+
+    def test_lock_timeout(self):
+        barrier = threading.Barrier(timeout=100, parties=2)
+        lock_thread = get_advisory_lock_thread(
+            lock_key=advisory_lock_key, lock_timeout=1, entry_barrier=barrier, exit_barrier=barrier
+        )
+
+        lock_thread.start()
+        barrier.wait()
+        with pytest.raises(AdvisoryLockAcquireTimeoutException):
+            with transaction.atomic(using="default"):
+                acquire_advisory_lock_with_barrier(lock_key=advisory_lock_key, lock_timeout=1)
+
+        # This doesn't completely test the cleanup code, as the transaction handling will
+        # rollback the timeout we set in the advisory lock code.
+        barrier.wait()
+        lock_thread.join(timeout=10)
+
+        acquire_advisory_lock_with_barrier(lock_key=advisory_lock_key, lock_timeout=1)
+
+    def test_lock_contention(self):
+        # barrier = threading.Barrier(timeout=10, parties=2)
+        entry_barrier = threading.Barrier(parties=2, timeout=5)
+        exit_barrier = threading.Barrier(parties=2, timeout=5)
+        original_lock_thread = get_advisory_lock_thread(
+            lock_key=advisory_lock_key,
+            lock_timeout=10,
+            entry_barrier=entry_barrier,
+            exit_barrier=exit_barrier,
+        )
+        contentious_lock_thread = get_advisory_lock_thread(
+            lock_key=advisory_lock_key,
+            lock_timeout=10,
+            entry_barrier=entry_barrier,
+            exit_barrier=exit_barrier,
+        )
+
+        # Start the initial thread, block on the exit barrier and ensure the thread has reached it
+        original_lock_thread.start()
+        entry_barrier.wait()
+        wait_until_barrier_hit(exit_barrier, 1, 2)
+
+        # Start the contentious thread, ensure the lock is inaccessible given the timeout
+        contentious_lock_thread.start()
+        with pytest.raises(AdvisoryLockAcquireTimeoutException):
+            acquire_advisory_lock_with_barrier(lock_key=advisory_lock_key, lock_timeout=1)
+
+        assert contentious_lock_thread.is_alive(), "Contentious thread has died"
+        assert (
+            entry_barrier.n_waiting == 0
+        ), "Entry barrier should not have any parties awaiting due to lock"
+
+        # Release the initial thread to finish its execution and release the lock
+        exit_barrier.wait()
+
+        # Assert contentious thread has encountered the barrier
+        wait_until_barrier_hit(entry_barrier, 1, 5)
+        entry_barrier.wait()
+        exit_barrier.wait()
+        original_lock_thread.join(timeout=1)
+        contentious_lock_thread.join(timeout=1)
+
+    def test_lock_with_exception(self):
+        # It pops, locks, and crashes. Lock should be unlocked.
+        # The original exception should be thrown.
+        exception_message = "An exception occurred in the locked region"
+        with pytest.raises(Exception) as e:
+            with advisory_lock(
+                using="default",
+                lock_id=advisory_lock_key,
+                lock_timeout_seconds=1,
+                lock_metric_name="advisory_lock",
+            ):
+                raise Exception(exception_message)
+
+        assert exception_message in str(e)
+
+        acquire_advisory_lock_with_barrier(lock_key=advisory_lock_key, lock_timeout=1)
+
+    def test_nested_lock(self):
+        # It locks, it locks again without error, it unlocks the lock in the inner context, it noops on the outer lock.
+        with advisory_lock(
+            using="default",
+            lock_id=advisory_lock_key,
+            lock_timeout_seconds=1,
+            lock_metric_name="advisory_lock",
+        ):
+            with advisory_lock(
+                using="default",
+                lock_id=advisory_lock_key,
+                lock_timeout_seconds=1,
+                lock_metric_name="advisory_lock",
+            ):
+                pass
+
+
+class TestCleanUpAdvisoryLock(TestCase):
+    def test_cleanup_without_lock_held(self):
+        clean_up_advisory_lock(using="default", lock_id=1234)
