1b44e512523442153f2c18a8e68deef944e8e3c6
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-02-06 13:41:30 +0100

    fix(on-demand): Escape glob patterns meta characters (#64651)

--- src/sentry/snuba/metrics/extraction.py
+++ src/sentry/snuba/metrics/extraction.py
@@ -1531,6 +1531,29 @@ def _get_satisfactory_threshold_and_metric(project: Project) -> tuple[int, str]:
     return threshold, metric_field
 
 
+def _escape_wildcard(value: str) -> str:
+    """
+    Escapes all characters in the wildcard which are considered as meta characters in the glob
+    implementation in Relay, which can be found at: https://docs.rs/globset/latest/globset/#syntax.
+
+    The goal of this function is to only preserve the `*` character as it is the only character that Sentry's
+    product offers to users to perform wildcard matching.
+    """
+    i, n = 0, len(value)
+    escaped = ""
+
+    while i < n:
+        c = value[i]
+        i = i + 1
+
+        if c in "[]{}?":
+            escaped += rf"\{c}"
+        else:
+            escaped += c
+
+    return escaped
+
+
 T = TypeVar("T")
 
 
@@ -1633,7 +1656,7 @@ class SearchQueryConverter:
             condition: RuleCondition = {
                 "op": "glob",
                 "name": _map_field_name(key),
-                "value": [value],
+                "value": [_escape_wildcard(value)],
             }
         else:
             # Special case for the `has` and `!has` operators which are parsed as follows:
