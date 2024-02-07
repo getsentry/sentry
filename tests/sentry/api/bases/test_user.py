e2565fe13b9c48a7b4173f01d28284b95867db5e
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-02-07 09:34:31 -0800

    Reverts initial API test to be an all silo test

--- tests/sentry/api/bases/test_user.py
+++ tests/sentry/api/bases/test_user.py
@@ -18,7 +18,7 @@ from sentry.testutils.helpers.features import with_feature
 from sentry.testutils.silo import all_silo_test, control_silo_test, no_silo_test, region_silo_test
 
 
-@region_silo_test
+@all_silo_test
 class UserPermissionTest(DRFPermissionTestCase):
     user_permission = UserPermission()
 
