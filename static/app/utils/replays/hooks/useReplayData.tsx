getsentry/sentry

commit 5f1d60fc9a2610320bce337e65106832a3e90761

ref(replay): Rewrite useReplayData to leverage useApiQuery 1/3 (#63455)
Per the title, this is a rewrite of useReplayData that leverages
useApiQuery and friends to cache data, which will make navigations
back/forward much faster.

This part only addresses the first query:
``/organizations/${orgSlug}/replays/${replayId}/` which grabs the main
replay record data. Navigating back & forth with be faster after this
change, because this is a blocking fetch, but the data will already be
in cache.

This is a slice from https://github.com/getsentry/sentry/pull/62109, to
try to isolate test failures and keep the changes more readable (and not
a big table-flip rewrite).

static/app/utils/replays/hooks/useReplayData.tsx
@@ -5,6 +5,7 @@ import type {Client} from 'sentry/api';
 import {ALL_ACCESS_PROJECTS} from 'sentry/constants/pageFilters';
 import type {ParsedHeader} from 'sentry/utils/parseLinkHeader';
 import parseLinkHeader from 'sentry/utils/parseLinkHeader';
+import {useApiQuery, useQueryClient} from 'sentry/utils/queryClient';
 import {mapResponseToReplayRecord} from 'sentry/utils/replays/replayDataUtils';
 import type RequestError from 'sentry/utils/requestError/requestError';
 import useApi from 'sentry/utils/useApi';
@@ -23,7 +24,6 @@ type State = {
    */
   fetchingAttachments: boolean;
   fetchingErrors: boolean;
-  fetchingReplay: boolean;
 };
 
 type Options = {
@@ -64,7 +64,6 @@ const INITIAL_STATE: State = Object.freeze({
   fetchError: undefined,
   fetchingAttachments: true,
   fetchingErrors: true,
-  fetchingReplay: true,
 });
 
 /**
@@ -100,12 +99,28 @@ function useReplayData({
   const projects = useProjects();
 
   const api = useApi();
+  const queryClient = useQueryClient();
 
   const [state, setState] = useState<State>(INITIAL_STATE);
   const [attachments, setAttachments] = useState<unknown[]>([]);
   const attachmentMap = useRef<Map<string, unknown[]>>(new Map()); // Map keys are always iterated by insertion order
   const [errors, setErrors] = useState<ReplayError[]>([]);
-  const [replayRecord, setReplayRecord] = useState<ReplayRecord>();
+
+  // Fetch every field of the replay. The TS type definition lists every field
+  // that's available. It's easier to ask for them all and not have to deal with
+  // partial types or nullable fields.
+  // We're overfetching for sure.
+  const {
+    data: replayData,
+    isFetching: isFetchingReplay,
+    error: fetchReplayError,
+  } = useApiQuery<{data: unknown}>([`/organizations/${orgSlug}/replays/${replayId}/`], {
+    staleTime: Infinity,
+  });
+  const replayRecord = useMemo(
+    () => (replayData?.data ? mapResponseToReplayRecord(replayData.data) : undefined),
+    [replayData?.data]
+  );
 
   const projectSlug = useMemo(() => {
     if (!replayRecord) {
@@ -113,14 +128,6 @@ function useReplayData({
     }
     return projects.projects.find(p => p.id === replayRecord.project_id)?.slug ?? null;
   }, [replayRecord, projects.projects]);
-
-  // Fetch every field of the replay. We're overfetching, not every field is used
-  const fetchReplay = useCallback(async () => {
-    const response = await api.requestPromise(makeFetchReplayApiUrl(orgSlug, replayId));
-    const mappedRecord = mapResponseToReplayRecord(response.data);
-    setReplayRecord(mappedRecord);
-    setState(prev => ({...prev, fetchingReplay: false}));
-  }, [api, orgSlug, replayId]);
 
   const fetchAttachments = useCallback(async () => {
     if (!replayRecord || !projectSlug) {
@@ -186,19 +193,10 @@ function useReplayData({
     setState(prev => ({...prev, fetchingErrors: false}));
   }, [api, orgSlug, replayRecord, errorsPerPage]);
 
-  const onError = useCallback(error => {
-    Sentry.captureException(error);
-    setState(prev => ({...prev, fetchError: error}));
+  const onError = useCallback(err => {
+    Sentry.captureException(err);
+    setState(prev => ({...prev, fetchError: err}));
   }, []);
-
-  const loadData = useCallback(
-    () => fetchReplay().catch(onError),
-    [fetchReplay, onError]
-  );
-
-  useEffect(() => {
-    loadData();
-  }, [loadData]);
 
   useEffect(() => {
     if (state.fetchError) {
@@ -214,19 +212,33 @@ function useReplayData({
     fetchAttachments().catch(onError);
   }, [state.fetchError, fetchAttachments, onError]);
 
+  const clearQueryCache = useCallback(() => {
+    () => {
+      queryClient.invalidateQueries({
+        queryKey: [`/organizations/${orgSlug}/replays/${replayId}/`],
+      });
+      queryClient.invalidateQueries({
+        queryKey: [
+          `/projects/${orgSlug}/${projectSlug}/replays/${replayId}/recording-segments/`,
+        ],
+      });
+      // The next one isn't optimized
+      // This statement will invalidate the cache of fetched error events for replayIds
+      queryClient.invalidateQueries({
+        queryKey: [`/organizations/${orgSlug}/replays-events-meta/`],
+      });
+    };
+  }, [orgSlug, replayId, projectSlug, queryClient]);
+
   return {
     attachments,
     errors,
-    fetchError: state.fetchError,
-    fetching: state.fetchingAttachments || state.fetchingErrors || state.fetchingReplay,
-    onRetry: loadData,
+    fetchError: fetchReplayError ?? state.fetchError,
+    fetching: state.fetchingAttachments || state.fetchingErrors || isFetchingReplay,
     projectSlug,
+    onRetry: clearQueryCache,
     replayRecord,
   };
-}
-
-function makeFetchReplayApiUrl(orgSlug: string, replayId: string) {
-  return `/organizations/${orgSlug}/replays/${replayId}/`;
 }
 
 async function fetchReplayErrors(

static/app/utils/replays/hooks/useReplayData.spec.tsx
@@ -1,3 +1,4 @@
+import type {ReactNode} from 'react';
 import {duration} from 'moment';
 import {
   ReplayConsoleEventFixture,
@@ -8,13 +9,14 @@ import {ReplayErrorFixture} from 'sentry-fixture/replayError';
 import {ReplayRecordFixture} from 'sentry-fixture/replayRecord';
 
 import {initializeOrg} from 'sentry-test/initializeOrg';
+import {makeTestQueryClient} from 'sentry-test/queryClient';
 import {reactHooks} from 'sentry-test/reactTestingLibrary';
 
+import {QueryClientProvider} from 'sentry/utils/queryClient';
 import useReplayData from 'sentry/utils/replays/hooks/useReplayData';
 import useProjects from 'sentry/utils/useProjects';
 import type {ReplayRecord} from 'sentry/views/replays/types';
 
-jest.useFakeTimers();
 jest.mock('sentry/utils/useProjects');
 
 const {organization, project} = initializeOrg();
@@ -28,6 +30,12 @@ jest.mocked(useProjects).mockReturnValue({
   onSearch: () => Promise.resolve(),
   placeholders: [],
 });
+
+function wrapper({children}: {children?: ReactNode}) {
+  return (
+    <QueryClientProvider client={makeTestQueryClient()}>{children}</QueryClientProvider>
+  );
+}
 
 function getMockReplayRecord(replayRecord?: Partial<ReplayRecord>) {
   const HYDRATED_REPLAY = ReplayRecordFixture({
@@ -59,6 +67,10 @@ describe('useReplayData', () => {
       error_ids: [],
     });
     MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/replays/${mockReplayResponse.id}/`,
+      body: {data: mockReplayResponse},
+    });
+    MockApiClient.addMockResponse({
       url: `/organizations/${organization.slug}/replays-events-meta/`,
       body: {
         data: [],
@@ -77,6 +89,7 @@ describe('useReplayData', () => {
     });
 
     const {result, waitForNextUpdate} = reactHooks.renderHook(useReplayData, {
+      wrapper,
       initialProps: {
         replayId: mockReplayResponse.id,
         orgSlug: organization.slug,
@@ -149,6 +162,7 @@ describe('useReplayData', () => {
     });
 
     const {result, waitForNextUpdate} = reactHooks.renderHook(useReplayData, {
+      wrapper,
       initialProps: {
         replayId: mockReplayResponse.id,
         orgSlug: organization.slug,
@@ -156,7 +170,6 @@ describe('useReplayData', () => {
       },
     });
 
-    jest.runAllTimers();
     await waitForNextUpdate();
 
     expect(mockedSegmentsCall1).toHaveBeenCalledTimes(1);
@@ -237,6 +250,7 @@ describe('useReplayData', () => {
     });
 
     const {result, waitForNextUpdate} = reactHooks.renderHook(useReplayData, {
+      wrapper,
       initialProps: {
         replayId: mockReplayResponse.id,
         orgSlug: organization.slug,
@@ -244,7 +258,6 @@ describe('useReplayData', () => {
       },
     });
 
-    jest.runAllTimers();
     await waitForNextUpdate();
 
     expect(mockedErrorsCall1).toHaveBeenCalledTimes(1);
@@ -302,6 +315,7 @@ describe('useReplayData', () => {
     });
 
     const {result, waitForNextUpdate} = reactHooks.renderHook(useReplayData, {
+      wrapper,
       initialProps: {
         replayId: mockReplayResponse.id,
         orgSlug: organization.slug,
@@ -324,7 +338,6 @@ describe('useReplayData', () => {
     expect(mockedSegmentsCall).not.toHaveBeenCalledTimes(1);
     expect(result.current).toEqual(expectedReplayData);
 
-    jest.advanceTimersByTime(10);
     await waitForNextUpdate();
 
     // Afterwards we see the attachments & errors requests are made
@@ -340,7 +353,6 @@ describe('useReplayData', () => {
       })
     );
 
-    jest.advanceTimersByTime(100);
     await waitForNextUpdate();
 
     // Next we see that some rrweb data has arrived
@@ -352,7 +364,6 @@ describe('useReplayData', () => {
       })
     );
 
-    jest.advanceTimersByTime(250);
     await waitForNextUpdate();
 
     // Finally we see fetching is complete, errors are here too

commit c1039a83bafb90f2e33e0ad78a05376959ffee30

bug(replay): Show the correct NotFound screen after a 404 when loading a replay (#64826)
I think this regressed with
https://github.com/getsentry/sentry/pull/63455, the error type/message
might've changed or become compressed. It seemed like it's still working
in dev, which makes testing the change a bit harder :(

Checking the status code could be more reliable. ~~I'll open the broken
replay
(https://sentry.sentry.io/replays/a7fcc2b504d34db296f2c7293bd47f28/) in
the preview env to check again before landing.~~ Edit: checked, it's
showing Not Found in the preview env.

Fixes
[JAVASCRIPT-2RKD](https://sentry.io/organizations/sentry/issues/?project=11276&query=JAVASCRIPT-2RKD)

static/app/utils/replays/hooks/useReplayData.tsx
@@ -89,6 +89,7 @@ function useReplayData({
     error: fetchReplayError,
   } = useApiQuery<{data: unknown}>([`/organizations/${orgSlug}/replays/${replayId}/`], {
     staleTime: Infinity,
+    retry: false,
   });
   const replayRecord = useMemo(
     () => (replayData?.data ? mapResponseToReplayRecord(replayData.data) : undefined),

static/app/views/replays/details.tsx
@@ -95,7 +95,7 @@ function ReplayDetails({params: {replaySlug}}: Props) {
     );
   }
   if (fetchError) {
-    if (fetchError.statusText === 'Not Found') {
+    if (fetchError.status === 404) {
       return (
         <Page
           orgSlug={orgSlug}