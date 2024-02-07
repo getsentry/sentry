01f1e0d9a1aa2186541392acce8d9739adcec6f9
Malachi Willey <malwilley@gmail.com>
2024-02-07 10:26:59 -0800

    feat(issue-priority): Add priority dropdown to issue details page (#64734)

---
+++ static/app/views/issueDetails/groupPriority.tsx
@@ -0,0 +1,56 @@
+import {bulkUpdate} from 'sentry/actionCreators/group';
+import {addLoadingMessage, clearIndicators} from 'sentry/actionCreators/indicator';
+import {GroupPriorityDropdown} from 'sentry/components/group/groupPriority';
+import {t} from 'sentry/locale';
+import IssueListCacheStore from 'sentry/stores/IssueListCacheStore';
+import {type Group, PriorityLevel} from 'sentry/types';
+import {trackAnalytics} from 'sentry/utils/analytics';
+import {getAnalyticsDataForGroup} from 'sentry/utils/events';
+import useApi from 'sentry/utils/useApi';
+import useOrganization from 'sentry/utils/useOrganization';
+
+type GroupDetailsPriorityProps = {
+  group: Group;
+};
+
+function GroupPriority({group}: GroupDetailsPriorityProps) {
+  const api = useApi({persistInFlight: true});
+  const organization = useOrganization();
+
+  const onChange = (priority: PriorityLevel) => {
+    if (priority === group.priority) {
+      return;
+    }
+
+    trackAnalytics('issue_details.set_priority', {
+      organization,
+      ...getAnalyticsDataForGroup(group),
+      from_priority: group.priority,
+      to_priority: priority,
+    });
+
+    addLoadingMessage(t('Saving changes\u2026'));
+
+    bulkUpdate(
+      api,
+      {
+        orgId: organization.slug,
+        projectId: group.project.slug,
+        itemIds: [group.id],
+        data: {priority},
+      },
+      {complete: clearIndicators}
+    );
+
+    IssueListCacheStore.reset();
+  };
+
+  return (
+    <GroupPriorityDropdown
+      onChange={onChange}
+      value={group.priority ?? PriorityLevel.MEDIUM}
+    />
+  );
+}
+
+export default GroupPriority;
