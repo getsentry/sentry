370bb4da501ea390097a2112e76cae6af821abfd
schew2381 <seiji.chew@sentry.io>
2024-02-07 16:09:50 -0800

    fix comment spacing

--- src/sentry/apidocs/spectacular_ports.py
+++ src/sentry/apidocs/spectacular_ports.py
@@ -146,12 +146,11 @@ def resolve_type_hint(hint) -> Any:
         else:
             schema = resolve_type_hint(type_args[0])
         if type(None) in args:
-            # There's an issue where if 3 types are OR'd together and one of
-            # them is None, validating the schema will fail because "nullable:
-            # true" with "anyOf" raises an error because there is no "type" key
-            # on the schema. This works around it by including a proxy null
-            # object in the "anyOf".
-            # See https://github.com/OAI/OpenAPI-Specification/issues/1368.
+            # There's an issue where if 3 or more types are OR'd together and one of
+            # them is None, validating the schema will fail because "nullable: true"
+            # with "anyOf" raises an error because there is no "type" key on the
+            # schema. This works around it by including a proxy null object in
+            # the "anyOf". See https://github.com/OAI/OpenAPI-Specification/issues/1368.
             if len(args) > 2:
                 schema["oneOf"].append({"type": "object", "nullable": True})
             else:
