6d371b7df87ada598d22edd555a126a8ea4421f6
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-02-09 14:46:04 -0500

    ✅ Update project_ownership test

--- tests/sentry/api/endpoints/test_project_ownership.py
+++ tests/sentry/api/endpoints/test_project_ownership.py
@@ -79,6 +79,7 @@ class ProjectOwnershipEndpointTestCase(APITestCase):
             "dateCreated": None,
             "lastUpdated": None,
             "codeownersAutoSync": True,
+            "schema": None,
         }
 
     def test_update(self):
@@ -90,7 +91,6 @@ class ProjectOwnershipEndpointTestCase(APITestCase):
         assert resp.data["dateCreated"] is not None
         assert resp.data["lastUpdated"] is not None
         assert resp.data["codeownersAutoSync"] is True
-        assert "schema" not in resp.data.keys()
 
         resp = self.client.put(self.path, {"fallthrough": False})
         assert resp.status_code == 200
@@ -242,90 +242,6 @@ class ProjectOwnershipEndpointTestCase(APITestCase):
             "lastUpdated": None,
             "codeownersAutoSync": True,
             "schema": None,
-        }
-
-    def test_get_rule_deleted_owner(self):
-        self.member_user_delete = self.create_user("member_delete@localhost", is_superuser=False)
-        self.create_member(
-            user=self.member_user_delete,
-            organization=self.organization,
-            role="member",
-            teams=[self.team],
-        )
-        self.client.put(self.path, {"raw": "*.js member_delete@localhost #tiger-team"})
-
-        with assume_test_silo_mode(SiloMode.CONTROL):
-            self.member_user_delete.delete()
-
-        resp = self.client.get(self.path)
-        assert resp.data["schema"] == {
-            "$version": 1,
-            "rules": [
-                {
-                    "matcher": {"type": "path", "pattern": "*.js"},
-                    "owners": [{"type": "team", "name": "tiger-team", "id": self.team.id}],
-                }
-            ],
-        }
-
-    def test_get_no_rule_deleted_owner(self):
-        self.member_user_delete = self.create_user("member_delete@localhost", is_superuser=False)
-        self.create_member(
-            user=self.member_user_delete,
-            organization=self.organization,
-            role="member",
-            teams=[self.team],
-        )
-
-        self.client.put(self.path, {"raw": "*.js member_delete@localhost"})
-
-        with assume_test_silo_mode(SiloMode.CONTROL):
-            self.member_user_delete.delete()
-
-        resp = self.client.get(self.path)
-        assert resp.data["schema"] == {"$version": 1, "rules": []}
-
-    def test_get_multiple_rules_deleted_owners_with_streamline_targeting(self):
-        self.member_user_delete = self.create_user("member_delete@localhost", is_superuser=False)
-        self.create_member(
-            user=self.member_user_delete,
-            organization=self.organization,
-            role="member",
-            teams=[self.team],
-        )
-        self.member_user_delete2 = self.create_user("member_delete2@localhost", is_superuser=False)
-        self.create_member(
-            user=self.member_user_delete2,
-            organization=self.organization,
-            role="member",
-            teams=[self.team],
-        )
-        self.client.put(
-            self.path,
-            {
-                "raw": "*.js member_delete@localhost\n*.py #tiger-team\n*.css member_delete2@localhost\n*.rb member@localhost"
-            },
-        )
-
-        with assume_test_silo_mode(SiloMode.CONTROL):
-            self.member_user_delete.delete()
-            self.member_user_delete2.delete()
-
-        resp = self.client.get(self.path)
-        assert resp.data["schema"] == {
-            "$version": 1,
-            "rules": [
-                {
-                    "matcher": {"pattern": "*.py", "type": "path"},
-                    "owners": [{"id": self.team.id, "name": "tiger-team", "type": "team"}],
-                },
-                {
-                    "matcher": {"pattern": "*.rb", "type": "path"},
-                    "owners": [
-                        {"id": self.member_user.id, "name": "member@localhost", "type": "user"}
-                    ],
-                },
-            ],
         }
 
     def test_invalid_email(self):
