203e9da47ba5c19e7ec9441452cad427b60e8611
Katie Byers <katie.byers@sentry.io>
2024-02-12 09:45:24 -0800

    feat(tests): Add utility function to capture return values of mocks (#64856)
    When pytest mocks or spies on a function, it keeps track of the input each time the function is called. It does not, however, keep track of the output. (Why?? Because you shouldn't be testing implementation details? But then why keep track of the input?) In order to test certain side effects, though, sometimes you need to know what your spies return. 
    
    This PR solves that by adding a helper, `capture_return_values`, which adds the return values of all calls of the given function to a given list, so the return values can be accessed later. (It also accepts a dictionary of lists, in case you want to keep track of the return values of more than one function at a time.)

---
+++ tests/sentry/testutils/pytest/mocking/animals/__init__.py
@@ -0,0 +1,14 @@
+def get_dog():
+    return "maisey"
+
+
+def get_cat():
+    return "piper"
+
+
+def a_function_that_calls_get_dog():
+    return f"{get_dog()} is a good dog!"
+
+
+def a_function_that_calls_get_cat():
+    return f"{get_cat()} is a good cat, because she thinks she's a dog!"
