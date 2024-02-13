ee0bc55dac2495bbb2f2adcfea6cccd586f12611
Gabe Villalobos <GabeVillalobos@users.noreply.github.com>
2024-02-12 17:00:28 -0800

    fix(hybrid-cloud): Sets region mode to default test, unless monolith dbs specified (#65021)

--- src/sentry/testutils/pytest/sentry.py
+++ src/sentry/testutils/pytest/sentry.py
@@ -54,7 +54,10 @@ DEFAULT_SILO_MODE_FOR_TEST_CASES = SiloMode.MONOLITH
 
 
 def _configure_test_env_regions() -> None:
-    settings.SILO_MODE = DEFAULT_SILO_MODE_FOR_TEST_CASES
+    SENTRY_USE_MONOLITH_DBS = os.environ.get("SENTRY_USE_MONOLITH_DBS", "0") == "1"
+    settings.SILO_MODE = (
+        DEFAULT_SILO_MODE_FOR_TEST_CASES if not SENTRY_USE_MONOLITH_DBS else SiloMode.MONOLITH
+    )
 
     # Assign a random name on every test run, as a reminder that test setup and
     # assertions should not depend on this value. If you need to test behavior that
