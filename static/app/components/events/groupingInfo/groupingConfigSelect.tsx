490396c5db44c0aade6ac6f8dd47e2bded68c32e
Malachi Willey <malwilley@gmail.com>
2024-02-13 15:15:49 -0800

    ref(tsc): Convert GroupingConfigSelect to FC + useApiQuery (#65124)

--- static/app/components/events/groupingInfo/groupingConfigSelect.tsx
+++ static/app/components/events/groupingInfo/groupingConfigSelect.tsx
@@ -1,69 +1,68 @@
+import {useMemo} from 'react';
 import styled from '@emotion/styled';
 
-import DeprecatedAsyncComponent from 'sentry/components/deprecatedAsyncComponent';
 import DropdownAutoComplete from 'sentry/components/dropdownAutoComplete';
 import DropdownButton from 'sentry/components/dropdownButton';
+import LoadingError from 'sentry/components/loadingError';
 import {Tooltip} from 'sentry/components/tooltip';
 import {t} from 'sentry/locale';
 import type {EventGroupingConfig} from 'sentry/types';
+import {useApiQuery} from 'sentry/utils/queryClient';
+import useOrganization from 'sentry/utils/useOrganization';
 
 import {GroupingConfigItem} from '.';
 
-type Props = DeprecatedAsyncComponent['props'] & {
+type GroupingConfigSelectProps = {
   configId: string;
   eventConfigId: string;
   onSelect: (selection: any) => void;
-  organizationSlug: string;
 };
 
-type State = DeprecatedAsyncComponent['state'] & {
-  configs: EventGroupingConfig[];
-};
+export function GroupingConfigSelect({
+  configId,
+  eventConfigId,
+  onSelect,
+}: GroupingConfigSelectProps) {
+  const organization = useOrganization();
+  const {
+    data: configs,
+    isLoading,
+    isError,
+  } = useApiQuery<EventGroupingConfig[]>(
+    [`/organizations/${organization.slug}/grouping-configs/`],
+    {staleTime: 0, retry: false}
+  );
 
-class GroupingConfigSelect extends DeprecatedAsyncComponent<Props, State> {
-  getDefaultState() {
-    return {
-      ...super.getDefaultState(),
-      configs: [],
-    };
+  const options = useMemo(
+    () =>
+      (configs ?? []).map(({id, hidden}) => ({
+        value: id,
+        label: (
+          <GroupingConfigItem isHidden={hidden} isActive={id === eventConfigId}>
+            {id}
+          </GroupingConfigItem>
+        ),
+      })),
+    [configs, eventConfigId]
+  );
+
+  if (isError) {
+    return <LoadingError message={t('Failed to load config options')} />;
   }
 
-  getEndpoints(): ReturnType<DeprecatedAsyncComponent['getEndpoints']> {
-    const {organizationSlug} = this.props;
-    return [['configs', `/organizations/${organizationSlug}/grouping-configs/`]];
-  }
-
-  renderLoading() {
-    return this.renderBody();
-  }
-
-  renderBody() {
-    const {configId, eventConfigId, onSelect} = this.props;
-    const {configs} = this.state;
-
-    const options = configs.map(({id, hidden}) => ({
-      value: id,
-      label: (
-        <GroupingConfigItem isHidden={hidden} isActive={id === eventConfigId}>
-          {id}
-        </GroupingConfigItem>
-      ),
-    }));
-
-    return (
-      <DropdownAutoComplete onSelect={onSelect} items={options}>
-        {({isOpen}) => (
-          <Tooltip title={t('Click here to experiment with other grouping configs')}>
-            <StyledDropdownButton isOpen={isOpen} size="sm">
-              <GroupingConfigItem isActive={eventConfigId === configId}>
-                {configId}
-              </GroupingConfigItem>
-            </StyledDropdownButton>
-          </Tooltip>
-        )}
-      </DropdownAutoComplete>
-    );
-  }
+  return (
+    <DropdownAutoComplete busy={isLoading} onSelect={onSelect} items={options}>
+      {({isOpen}) => (
+        <Tooltip title={t('Click here to experiment with other grouping configs')}>
+          <StyledDropdownButton isOpen={isOpen} size="sm">
+            <GroupingConfigItem isActive={eventConfigId === configId}>
+              {configId}
+            </GroupingConfigItem>
+          </StyledDropdownButton>
+        </Tooltip>
+      )}
+    </DropdownAutoComplete>
+  );
 }
 
 const StyledDropdownButton = styled(DropdownButton)`
--- static/app/components/events/groupingInfo/index.spec.tsx
+++ static/app/components/events/groupingInfo/index.spec.tsx
@@ -8,29 +8,38 @@ import {EventGroupVariantType, IssueCategory} from 'sentry/types';
 
 describe('EventGroupingInfo', function () {
   const group = GroupFixture();
-  const event = EventFixture();
+  const event = EventFixture({
+    groupingConfig: {
+      id: 'default:XXXX',
+    },
+  });
 
   const defaultProps = {
     event,
     projectSlug: 'project-slug',
-    showGroupingConfig: true,
+    showGroupingConfig: false,
     group,
   };
 
-  it('fetches and renders grouping info for errors', async function () {
-    MockApiClient.addMockResponse({
+  let groupingInfoRequest = jest.fn();
+
+  beforeEach(() => {
+    MockApiClient.clearMockResponses();
+    groupingInfoRequest = MockApiClient.addMockResponse({
       url: `/projects/org-slug/project-slug/events/${event.id}/grouping-info/`,
       body: {
         app: {
           description: 'variant description',
           hash: '123',
-          hasMismatch: false,
+          hashMismatch: false,
           key: 'key',
           type: EventGroupVariantType.CHECKSUM,
         },
       },
     });
+  });
 
+  it('fetches and renders grouping info for errors', async function () {
     render(<EventGroupingInfo {...defaultProps} />);
 
     await screen.findByText('variant description');
@@ -56,5 +65,47 @@ describe('EventGroupingInfo', function () {
     expect(screen.queryByText('123')).not.toBeInTheDocument();
     await userEvent.click(screen.getByRole('button', {name: 'Show Details'}));
     expect(screen.getByText('123')).toBeInTheDocument();
+
+    // Should not make grouping-info request
+    expect(groupingInfoRequest).not.toHaveBeenCalled();
+  });
+
+  it('can switch grouping configs', async function () {
+    MockApiClient.addMockResponse({
+      url: `/organizations/org-slug/grouping-configs/`,
+      body: [
+        {id: 'default:XXXX', hidden: false},
+        {id: 'new:XXXX', hidden: false},
+      ],
+    });
+
+    render(<EventGroupingInfo {...defaultProps} showGroupingConfig />);
+
+    await userEvent.click(screen.getByRole('button', {name: 'Show Details'}));
+
+    // Should show first hash
+    await screen.findByText('123');
+
+    expect(screen.getByText('default:XXXX')).toBeInTheDocument();
+
+    MockApiClient.addMockResponse({
+      url: `/projects/org-slug/project-slug/events/${event.id}/grouping-info/`,
+      query: {config: 'new:XXXX'},
+      body: {
+        app: {
+          description: 'variant description',
+          hash: '789',
+          hashMismatch: false,
+          key: 'key',
+          type: EventGroupVariantType.CHECKSUM,
+        },
+      },
+    });
+
+    await userEvent.click(screen.getAllByRole('button', {name: 'default:XXXX'})[0]);
+    await userEvent.click(screen.getByRole('option', {name: 'new:XXXX'}));
+
+    // Should show new hash
+    await screen.findByText('789');
   });
 });
