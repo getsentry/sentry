87ea3db670ccf4140aca46bc50c496532d8e5bd9
Yash Kamothi <yash.kamothi@gmail.com>
2024-02-14 14:49:39 -0800

    update data model

--- src/sentry/models/notificationmessage.py
+++ src/sentry/models/notificationmessage.py
@@ -1,4 +1,4 @@
-from django.db.models import DateTimeField
+from django.db.models import CheckConstraint, DateTimeField, IntegerField, Q
 from django.utils import timezone
 
 from sentry.backup.scopes import RelocationScope
@@ -16,8 +16,12 @@ from sentry.db.models import (
 class NotificationMessage(Model):
     """
     Data model represents the aggregate for an entire notification message.
-    A notification message must always have the request and response that it took, along with the resulting identifier
-    that was sent back.
+    A notification message must always have the resulting identifier that was sent back if there was no error.
+    When there is an error, there will be no identifier to leverage or store.
+
+    If a notification message has an error, the resulting error code and details will be saved.
+    This information will then be leveraged to show the user which notifications are consistently failing.
+    An example of this would be if a Slack channel no longer exists.
 
     The data model hold the singular gateway for both Metric, and Issue, Alerts.
     Following the specific data relationship, you should be able to find the specific rule, organization, project, and
@@ -29,16 +33,21 @@ class NotificationMessage(Model):
 
     __relocation_scope__ = RelocationScope.Excluded
 
-    request = JSONField(null=False)
-    response = JSONField(null=False)
-    message_identifier = CharField(null=False, blank=False, db_index=True)
+    # Related information regarding failed notifications.
+    # Leveraged to help give the user visibility into notifications that are consistently failing.
+    error_details = JSONField(null=True)
+    error_code = IntegerField(null=True, db_index=True)
+
+    # Resulting identifier from the vendor that can be leveraged for future interaction with the notification.
+    message_identifier = CharField(null=True, db_index=True)
+    # Reference to another notification if we choose to modify the original message or reply to it (like start a thread)
     parent_notification_message = FlexibleForeignKey("self", null=True)
 
-    # related information regarding Alert Rules (Metric Alerts)
+    # Related information regarding Alert Rules (Metric Alerts)
     incident = FlexibleForeignKey("sentry.Incident", null=True)
-    trigger_action = FlexibleForeignKey("sentry.AlertRuleTriggerAction", null=True, db_index=True)
+    trigger_action = FlexibleForeignKey("sentry.AlertRuleTriggerAction", null=True)
 
-    # related information regarding Rules (Issue Alerts)
+    # Related information regarding Rules (Issue Alerts)
     rule_fire_history = FlexibleForeignKey("sentry.RuleFireHistory", null=True)
     rule_action_uuid = CharField(null=True, db_index=True)
 
@@ -47,5 +56,21 @@ class NotificationMessage(Model):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_notificationmessage"
+        # A notification message should exist for either issue or metric alert, but never both
+        constraints = [
+            CheckConstraint(
+                check=(
+                    (
+                        Q(incident__isnull=False, trigger_action__isnull=False)
+                        & Q(rule_fire_history__isnull=True, rule_action_uuid__isnull=True)
+                    )
+                    | (
+                        Q(incident__isnull=True, trigger_action__isnull=True)
+                        & Q(rule_fire_history__isnull=False, rule_action_uuid__isnull=False)
+                    )
+                ),
+                name="notification_for_issue_xor_metric_alert",
+            )
+        ]
 
     __repr__ = sane_repr("release_id", "commit_id", "order")
