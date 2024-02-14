dbc3058b4bf19cd1f17da588e38b4ebf5bf399e7
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2024-02-14 09:45:21 -0800

    feat(slack): feature flag for pre-GA slack block kit changes (#65164)

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1880,6 +1880,8 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     "organizations:spike-protection-decay-heuristic": False,
     # Enable Slack messages using Block Kit
     "organizations:slack-block-kit": False,
+    # Improvements to Slack messages using Block Kit
+    "organizations:slack-block-kit-improvements": False,
     # Enable basic SSO functionality, providing configurable single sign on
     # using services like GitHub / Google. This is *not* the same as the signup
     # and login with Github / Azure DevOps that sentry.io provides.
--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -254,6 +254,7 @@ default_manager.add("organizations:session-replay-weekly-email", OrganizationFea
 default_manager.add("organizations:set-grouping-config", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:settings-legal-tos-ui", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:slack-block-kit", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
+default_manager.add("organizations:slack-block-kit-improvements", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
 default_manager.add("organizations:slack-overage-notifications", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:source-maps-debugger-blue-thunder-edition", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:sourcemaps-bundle-flat-file-indexing", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
