12db73ac5de824b9c5c4d2932b52b52d94a43541
Abdkhan14 <60121741+Abdkhan14@users.noreply.github.com>
2024-02-14 16:50:13 -0500

    feat(new-trace-view): Added page and trace headers. (#65122)
    Adding TraceHeader summarizing the trace view with:
    - user info
    - browser info
    - trace meta
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/performance/newTraceDetails/index.tsx
+++ static/app/views/performance/newTraceDetails/index.tsx
@@ -1,17 +1,38 @@
-import {useMemo, useState} from 'react';
+import {Fragment, useMemo, useState} from 'react';
+import type {Location} from 'history';
 
+import ButtonBar from 'sentry/components/buttonBar';
+import DiscoverButton from 'sentry/components/discoverButton';
 import * as Layout from 'sentry/components/layouts/thirds';
 import NoProjectMessage from 'sentry/components/noProjectMessage';
 import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
+import {ALL_ACCESS_PROJECTS} from 'sentry/constants/pageFilters';
 import {t} from 'sentry/locale';
+import type {EventTransaction, Organization} from 'sentry/types';
+import {trackAnalytics} from 'sentry/utils/analytics';
+import EventView from 'sentry/utils/discover/eventView';
 import {TraceFullDetailedQuery} from 'sentry/utils/performance/quickTrace/traceFullQuery';
+import TraceMetaQuery, {
+  type TraceMetaQueryChildrenProps,
+} from 'sentry/utils/performance/quickTrace/traceMetaQuery';
+import type {
+  TraceFullDetailed,
+  TraceSplitResults,
+} from 'sentry/utils/performance/quickTrace/types';
+import {useApiQuery} from 'sentry/utils/queryClient';
 import {decodeScalar} from 'sentry/utils/queryString';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
+import useProjects from 'sentry/utils/useProjects';
 
-import {Trace} from './trace';
+import Breadcrumb from '../breadcrumb';
+
+import Trace from './trace';
+import TraceHeader from './traceHeader';
+import {TraceTree} from './traceTree';
+import TraceWarnings from './traceWarnings';
 
 const DOCUMENT_TITLE = [t('Trace Details'), t('Performance')].join(' — ');
 
@@ -33,23 +54,22 @@ export function TraceView() {
     return {start, end, statsPeriod};
   }, [location.query]);
 
-  // @TODO pass this to children
-  // const _traceEventView = useMemo(() => {
-  //   const {start, end, statsPeriod} = dateSelection;
+  const _traceEventView = useMemo(() => {
+    const {start, end, statsPeriod} = dateSelection;
 
-  //   return EventView.fromSavedQuery({
-  //     id: undefined,
-  //     name: `Events with Trace ID ${traceSlug}`,
-  //     fields: ['title', 'event.type', 'project', 'timestamp'],
-  //     orderby: '-timestamp',
-  //     query: `trace:${traceSlug}`,
-  //     projects: [ALL_ACCESS_PROJECTS],
-  //     version: 2,
-  //     start,
-  //     end,
-  //     range: statsPeriod,
-  //   });
-  // }, []);
+    return EventView.fromSavedQuery({
+      id: undefined,
+      name: `Events with Trace ID ${traceSlug}`,
+      fields: ['title', 'event.type', 'project', 'timestamp'],
+      orderby: '-timestamp',
+      query: `trace:${traceSlug}`,
+      projects: [ALL_ACCESS_PROJECTS],
+      version: 2,
+      start,
+      end,
+      range: statsPeriod,
+    });
+  }, [dateSelection, traceSlug]);
 
   const [_limit, _setLimit] = useState<number>();
   // const _handleLimithange = useCallback((newLimit: number) => {
@@ -68,10 +88,124 @@ export function TraceView() {
             end={dateSelection.end}
             statsPeriod={dateSelection.statsPeriod}
           >
-            {trace => <Trace trace={trace?.traces} trace_id={traceSlug} />}
+            {trace => (
+              <TraceMetaQuery
+                location={location}
+                orgSlug={organization.slug}
+                traceId={traceSlug}
+                start={dateSelection.start}
+                end={dateSelection.end}
+                statsPeriod={dateSelection.statsPeriod}
+              >
+                {metaResults => (
+                  <TraceViewContent
+                    traceSplitResult={trace?.traces}
+                    traceSlug={traceSlug}
+                    organization={organization}
+                    location={location}
+                    traceEventView={_traceEventView}
+                    metaResults={metaResults}
+                  />
+                )}
+              </TraceMetaQuery>
+            )}
           </TraceFullDetailedQuery>
         </NoProjectMessage>
       </Layout.Page>
     </SentryDocumentTitle>
+  );
+}
+
+type TraceViewContentProps = {
+  location: Location;
+  metaResults: TraceMetaQueryChildrenProps;
+  organization: Organization;
+  traceEventView: EventView;
+  traceSlug: string;
+  traceSplitResult: TraceSplitResults<TraceFullDetailed> | null;
+};
+
+function TraceViewContent(props: TraceViewContentProps) {
+  const rootEvent = props.traceSplitResult?.transactions?.[0];
+  const {projects} = useProjects();
+  const tree = useMemo(() => {
+    if (!props.traceSplitResult) {
+      return TraceTree.Loading({
+        project_slug: projects?.[0]?.slug ?? '',
+        event_id: props.traceSlug,
+      });
+    }
+
+    return TraceTree.FromTrace(props.traceSplitResult);
+  }, [props.traceSlug, props.traceSplitResult, projects]);
+
+  const traceType = useMemo(() => {
+    return TraceTree.GetTraceType(tree.root);
+  }, [tree]);
+
+  const rootEventResults = useApiQuery<EventTransaction>(
+    [
+      `/organizations/${props.organization.slug}/events/${rootEvent?.project_slug}:${rootEvent?.event_id}/`,
+      {
+        query: {
+          referrer: 'trace-details-summary',
+        },
+      },
+    ],
+    {
+      staleTime: 0,
+      enabled: !!(
+        props.traceSplitResult?.transactions &&
+        props.traceSplitResult.transactions.length > 0
+      ),
+    }
+  );
+
+  return (
+    <Fragment>
+      <Layout.Header>
+        <Layout.HeaderContent>
+          <Breadcrumb
+            organization={props.organization}
+            location={props.location}
+            transaction={{
+              project: rootEventResults.data?.projectID ?? '',
+              name: rootEventResults.data?.title ?? '',
+            }}
+            traceSlug={props.traceSlug}
+          />
+          <Layout.Title data-test-id="trace-header">
+            {t('Trace ID: %s', props.traceSlug)}
+          </Layout.Title>
+        </Layout.HeaderContent>
+        <Layout.HeaderActions>
+          <ButtonBar gap={1}>
+            <DiscoverButton
+              size="sm"
+              to={props.traceEventView.getResultsViewUrlTarget(props.organization.slug)}
+              onClick={() => {
+                trackAnalytics('performance_views.trace_view.open_in_discover', {
+                  organization: props.organization,
+                });
+              }}
+            >
+              {t('Open in Discover')}
+            </DiscoverButton>
+          </ButtonBar>
+        </Layout.HeaderActions>
+      </Layout.Header>
+      <Layout.Body>
+        <Layout.Main fullWidth>
+          {traceType ? <TraceWarnings type={traceType} /> : null}
+          <TraceHeader
+            rootEventResults={rootEventResults}
+            metaResults={props.metaResults}
+            organization={props.organization}
+            traces={props.traceSplitResult}
+          />
+          <Trace trace={tree} trace_id={props.traceSlug} />
+        </Layout.Main>
+      </Layout.Body>
+    </Fragment>
   );
 }
