7c63d2c1c9e087f17520bf028a8deb31774adcd4
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-02-14 15:36:26 +0100

    fix

--- static/app/utils/metrics/dashboard.tsx
+++ static/app/utils/metrics/dashboard.tsx
@@ -3,11 +3,8 @@ import {urlEncode} from '@sentry/utils';
 import type {MRI, PageFilters} from 'sentry/types';
 import {emptyWidget} from 'sentry/utils/metrics/constants';
 import {formatMRI, MRIToField, parseField} from 'sentry/utils/metrics/mri';
-import type {
-  MetricDisplayType,
-  MetricsQuery,
-  MetricWidgetQueryParams,
-} from 'sentry/utils/metrics/types';
+import type {MetricsQuery, MetricWidgetQueryParams} from 'sentry/utils/metrics/types';
+import {MetricDisplayType} from 'sentry/utils/metrics/types';
 import type {Widget} from 'sentry/views/dashboards/types';
 import {
   DashboardWidgetSource,
