3fe205f5f9b1fab21f9c17a73c372e85c43873cd
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-02-15 11:45:33 -0500

    ref(similarity-embedding): Move log (#65243)
    Move log before seer api call so it's called if seer api call fails

--- src/sentry/api/endpoints/group_similar_issues_embeddings.py
+++ src/sentry/api/endpoints/group_similar_issues_embeddings.py
@@ -136,11 +136,11 @@ class GroupSimilarIssuesEmbeddingsEndpoint(GroupEndpoint):
         if request.GET.get("threshold"):
             similar_issues_params.update({"threshold": float(request.GET["threshold"])})
 
-        results = get_similar_issues_embeddings(similar_issues_params)
-
         extra: dict[str, Any] = dict(similar_issues_params.copy())
         extra["group_message"] = extra.pop("message")
         logger.info("Similar issues embeddings parameters", extra=extra)
+
+        results = get_similar_issues_embeddings(similar_issues_params)
 
         analytics.record(
             "group_similar_issues_embeddings.count",
