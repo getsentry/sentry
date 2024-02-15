7f6e8ca271e04a06feaedec12ca755caa25eb66f
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-02-15 13:18:02 -0800

    ref(tsc): convert admin packages to FC

--- static/app/views/admin/adminPackages.tsx
+++ static/app/views/admin/adminPackages.tsx
@@ -1,60 +1,53 @@
 import {Fragment} from 'react';
 
 import {t} from 'sentry/locale';
-import DeprecatedAsyncView from 'sentry/views/deprecatedAsyncView';
+import {useApiQuery} from 'sentry/utils/queryClient';
 
 type Data = {
   extensions: [key: string, value: string][];
   modules: [key: string, value: string][];
 };
 
-type State = DeprecatedAsyncView['state'] & {data: Data};
+export default function AdminPackages() {
+  const {data} = useApiQuery<Data>(['/internal/packages/'], {
+    staleTime: 0,
+  });
 
-export default class AdminPackages extends DeprecatedAsyncView<{}, State> {
-  getEndpoints(): ReturnType<DeprecatedAsyncView['getEndpoints']> {
-    return [['data', '/internal/packages/']];
-  }
+  return (
+    <div>
+      <h3>{t('Extensions')}</h3>
 
-  renderBody() {
-    const {data} = this.state;
-    const {extensions, modules} = data;
+      {data?.extensions && data?.extensions.length > 0 ? (
+        <dl className="vars">
+          {data?.extensions.map(([key, value]) => (
+            <Fragment key={key}>
+              <dt>{key}</dt>
+              <dd>
+                <pre className="val">{value}</pre>
+              </dd>
+            </Fragment>
+          ))}
+        </dl>
+      ) : (
+        <p>{t('No extensions registered')}</p>
+      )}
 
-    return (
-      <div>
-        <h3>{t('Extensions')}</h3>
+      <h3>{t('Modules')}</h3>
 
-        {extensions.length > 0 ? (
-          <dl className="vars">
-            {extensions.map(([key, value]) => (
-              <Fragment key={key}>
-                <dt>{key}</dt>
-                <dd>
-                  <pre className="val">{value}</pre>
-                </dd>
-              </Fragment>
-            ))}
-          </dl>
-        ) : (
-          <p>{t('No extensions registered')}</p>
-        )}
-
-        <h3>{t('Modules')}</h3>
-
-        {modules.length > 0 ? (
-          <dl className="vars">
-            {modules.map(([key, value]) => (
-              <Fragment key={key}>
-                <dt>{key}</dt>
-                <dd>
-                  <pre className="val">{value}</pre>
-                </dd>
-              </Fragment>
-            ))}
-          </dl>
-        ) : (
-          <p>{t('No modules registered')}</p>
-        )}
-      </div>
-    );
-  }
+      {data?.modules && data?.modules.length > 0 ? (
+        <dl className="vars">
+          {data?.modules.map(([key, value]) => (
+            <Fragment key={key}>
+              <dt>{key}</dt>
+              <dd>
+                <pre className="val">{value}</pre>
+              </dd>
+            </Fragment>
+          ))}
+        </dl>
+      ) : (
+        <p>{t('No modules registered')}</p>
+      )}
+    </div>
+  );
 }
