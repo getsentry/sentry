fa43eedd63f938696cfd1ada4c083aef324eb41f
Malachi Willey <malwilley@gmail.com>
2024-02-16 08:50:22 -0800

    ref(issue-stream): Refactor issue stream action logic with better typing and flexibility

--- static/app/utils/analytics/issueAnalyticsEvents.tsx
+++ static/app/utils/analytics/issueAnalyticsEvents.tsx
@@ -190,7 +190,7 @@ export type IssueEventParameters = {
   };
   'issues_stream.archived': {
     action_status_details?: string;
-    action_substatus?: string;
+    action_substatus?: string | null;
   };
   'issues_stream.issue_assigned': IssueStream & {
     assigned_type: string;
