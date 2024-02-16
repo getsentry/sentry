d5a15612ed129bc69ae8246dd00d364464abb4d1
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-02-16 09:40:27 +0100

    fix(alerts): metric alert feature flag

--- src/sentry/incidents/serializers/alert_rule.py
+++ src/sentry/incidents/serializers/alert_rule.py
@@ -136,7 +136,7 @@ class AlertRuleSerializer(CamelSnakeModelSerializer):
 
     def validate_aggregate(self, aggregate):
         allow_mri = features.has(
-            "organizations:ddm-experimental",
+            "organizations:custom-metrics",
             self.context["organization"],
             actor=self.context.get("user", None),
         )
