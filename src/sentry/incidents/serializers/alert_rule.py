eac1ab7d08ab8bb54e8e0ac8e75a3b5eeedb6d2e
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-02-16 09:42:26 +0100

    fix

--- src/sentry/incidents/serializers/alert_rule.py
+++ src/sentry/incidents/serializers/alert_rule.py
@@ -256,7 +256,7 @@ class AlertRuleSerializer(CamelSnakeModelSerializer):
             dataset = data["dataset"] = Dataset.Metrics
 
         if features.has(
-            "organizations:ddm-experimental",
+            "organizations:custom-metrics",
             self.context["organization"],
             actor=self.context.get("user", None),
         ):
