ac877a5a4b8f07c188023d829440ce7df5f2725c
Yash Kamothi <yash.kamothi@gmail.com>
2024-02-15 17:07:59 -0800

    play it safe

--- src/sentry/deletions/defaults/alert_rule_trigger_action.py
+++ src/sentry/deletions/defaults/alert_rule_trigger_action.py
@@ -6,12 +6,5 @@ class AlertRuleTriggerActionDeletionTask(ModelDeletionTask):
         from sentry.models.notificationmessage import NotificationMessage
 
         return [
-            ModelRelation(NotificationMessage, {"trigger_action": instance.id}),
+            ModelRelation(NotificationMessage, {"trigger_action_id": instance.id}),
         ]
-
-    def mark_deletion_in_progress(self, instance_list):
-        from sentry.constants import ObjectStatus
-
-        for instance in instance_list:
-            if instance.status != ObjectStatus.PENDING_DELETION:
-                instance.update(status=ObjectStatus.PENDING_DELETION)
