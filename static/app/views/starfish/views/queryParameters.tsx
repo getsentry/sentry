eb4beff4063d939a3790122123723510fd93771c
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-02-16 15:52:57 -0500

    feat(perf): Add barebones HTTP module page (#65188)
    Don't sweat the names of things yet, this is just to get started. Mostly
    just copying the Database landing page for now, but they'll diverge over
    time.
    
    ---------
    
    Co-authored-by: Yagiz Nizipli <yagiz@nizipli.com>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/views/starfish/views/queryParameters.tsx
+++ static/app/views/starfish/views/queryParameters.tsx
@@ -1,6 +1,8 @@
 export enum QueryParameterNames {
   SPANS_CURSOR = 'spansCursor',
   SPANS_SORT = 'spansSort',
+  DOMAINS_CURSOR = 'domainsCursor',
+  DOMAINS_SORT = 'domainsSort',
   ENDPOINTS_CURSOR = 'endpointsCursor',
   ENDPOINTS_SORT = 'endpointsSort',
   PAGES_CURSOR = 'pagesCursor',
