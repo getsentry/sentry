26486356a1b943170de3a7b43d1a282362cfd25d
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-02-16 11:15:39 -0800

    feat(issue-priority): Add support for issue.priority search (#65205)
    Adds support for searching for issue.priority with a single value or an
    array of values.
    
    <img width="1286" alt="image"
    src="https://github.com/getsentry/sentry/assets/16563948/45d97b10-43b4-4f31-96d0-e4902bcb1e9d">
    
    Fixes https://github.com/getsentry/sentry/issues/65097

--- tests/sentry/issues/test_priority.py
+++ tests/sentry/issues/test_priority.py
@@ -1,7 +1,6 @@
 from unittest.mock import MagicMock, patch
 
 from sentry.issues.priority import (
-    PRIORITY_LEVEL_TO_STR,
     PRIORITY_TO_GROUP_HISTORY_STATUS,
     PriorityChangeReason,
     auto_update_priority,
@@ -21,7 +20,7 @@ class TestUpdatesPriority(TestCase):
     def assert_activity_grouphistory_set(self, group, priority, reason) -> None:
         activity = Activity.objects.get(group=group, type=ActivityType.SET_PRIORITY.value)
         assert activity.data == {
-            "priority": PRIORITY_LEVEL_TO_STR[priority],
+            "priority": priority.to_str(),
             "reason": reason.value,
         }
 
