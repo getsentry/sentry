34d7ef7a7e5fc35ed919165889f8e847ad283de4
Yash Kamothi <yash.kamothi@gmail.com>
2024-02-20 11:32:43 -0800

    add comments

--- src/sentry/notifications/repository/__init__.py
+++ src/sentry/notifications/repository/__init__.py
@@ -4,6 +4,7 @@ The classes help separate the query interface with the actual data store for the
 
 If we scale quickly, the current NotificationMessage data model will have to shift from django postgres to
 snuba clickhouse, and these classes will help keep the changes consolidated to here.
+For self-hosted, if the user wants to utilize another datastore, they are able to do so.
 
 What we query from an interface level won't change, simply how we query will change, and these classes should be the
 only thing that need to change after we make the migration.
