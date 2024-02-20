4da6e30a0e88cb776d151d5d740e12a7c49414c0
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-02-20 17:44:07 -0500

    ✨ fix activity text

--- static/app/views/issueDetails/groupActivityItem.tsx
+++ static/app/views/issueDetails/groupActivityItem.tsx
@@ -61,6 +61,7 @@ function AssignedMessage({activity, author, issueType}: AssignedMessageProps) {
     slack: t('Slack'),
     projectOwnership: t('Ownership Rule'),
     codeowners: t('Codeowners Rule'),
+    suspectCommitter: t('Suspect Committer'),
   };
 
   return (
