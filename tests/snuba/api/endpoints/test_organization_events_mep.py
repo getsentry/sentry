e0b05b7f277dc8d5aea9fc1a820969ae0d952854
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-02-20 13:42:25 +0100

    fix(on-demand): Enable user_misery back again (#65383)
    This PR enables `user_misery` back again after a change in Relay has
    been made (https://github.com/getsentry/relay/pull/3122) which allows
    access to the `sentry_user` computed field.

--- tests/snuba/api/endpoints/test_organization_events_mep.py
+++ tests/snuba/api/endpoints/test_organization_events_mep.py
@@ -3221,7 +3221,6 @@ class OrganizationEventsMetricsEnhancedPerformanceEndpointTestWithOnDemandMetric
         response = self._make_on_demand_request(params)
         self._assert_on_demand_response(response)
 
-    @pytest.mark.skip(reason="Re-enable when user misery is supported again.")
     def test_transaction_user_misery(self) -> None:
         user_misery_field = "user_misery(300)"
         apdex_field = "apdex(300)"
