47512a5f532723e94b96ab49c0a0f097044caf26
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2024-02-21 14:58:17 -0500

    clean up

--- src/sentry/tasks/spans.py
+++ src/sentry/tasks/spans.py
@@ -1,5 +1,6 @@
 import logging
-from collections.abc import Mapping
+import random
+from collections.abc import Mapping, Sequence
 from typing import Any
 
 import sentry_sdk
@@ -8,6 +9,7 @@ from sentry_kafka_schemas.codecs import Codec
 from sentry_kafka_schemas.schema_types.snuba_spans_v1 import SpanEvent
 
 from sentry.event_manager import (
+    Job,
     _detect_performance_problems,
     _pull_out_data,
     _send_occurrence_to_platform,
@@ -34,7 +36,7 @@ def transform_spans_to_event_dict(spans):
     deserialized_spans = []
     for span in spans:
         try:
-            deserialized_span = _deserialize_span(span)
+            deserialized_span: SpanEvent = _deserialize_span(span)
         except Exception:
             logger.exception("Failed to process span payload")
             continue
@@ -45,14 +47,13 @@ def transform_spans_to_event_dict(spans):
             event["event_id"] = deserialized_span.get("event_id")
             event["transaction"] = sentry_tags.get("transaction")
             event["contexts"]["trace"] = {
-                "trace_id": deserialized_span.get("trace_id"),
+                "trace_id": deserialized_span["trace_id"],
                 "type": "trace",
                 "op": sentry_tags.get("transaction.op"),
             }
             event["received"] = deserialized_span["received"]
             event["timestamp"] = (
-                deserialized_span.get("start_timestamp_ms", 0)
-                + deserialized_span.get("duration_ms")
+                deserialized_span["start_timestamp_ms"] + deserialized_span["duration_ms"]
             ) / 1000
 
             if (profile_id := deserialized_span.get("profile_id")) is not None:
@@ -62,11 +63,12 @@ def transform_spans_to_event_dict(spans):
             deserialized_span["op"] = op
 
         if (group := sentry_tags.get("group")) is not None:
+            # TODO: Calculate span hash based on raw span description when available.
             deserialized_span["hash"] = group
 
-        deserialized_span["start_timestamp"] = deserialized_span.get("start_timestamp_ms") / 1000
+        deserialized_span["start_timestamp"] = deserialized_span["start_timestamp_ms"] / 1000
         deserialized_span["timestamp"] = (
-            deserialized_span.get("start_timestamp_ms", 0) + deserialized_span.get("duration_ms")
+            deserialized_span["start_timestamp_ms"] + deserialized_span["duration_ms"]
         ) / 1000
 
         deserialized_spans.append(deserialized_span)
@@ -92,28 +94,30 @@ def _process_segment(project_id, segment_id):
     projects = {project.id: project}
 
     data = CanonicalKeyDict(event)
-    jobs: dict[str, Any] = [
+    jobs: Sequence[Job] = [
         {
             "data": data,
             "project_id": project.id,
             "raw": False,
-            "start_time": None,  # TODO: figure out how to get start time
+            "start_time": None,
         }
     ]
 
     _pull_out_data(jobs, projects)
     _detect_performance_problems(jobs, projects)
-    performance_problems = jobs[0].pop("performance_problems")
-    updated_problems = []
 
-    for performance_problem in performance_problems:
-        performance_problem.type = PerformanceStreamedSpansGroupTypeExperimental
-        updated_problems.append(performance_problem)
+    for job in jobs:
+        updated_problems = []
+        performance_problems = job.pop("performance_problems")
+        for performance_problem in performance_problems:
+            performance_problem.type = PerformanceStreamedSpansGroupTypeExperimental
+            updated_problems.append(performance_problem)
 
-    jobs[0]["performance_problems"] = updated_problems
+        job["performance_problems"] = updated_problems
+
     _send_occurrence_to_platform(jobs, projects)
 
-    return jobs[0]
+    return jobs
 
 
 @instrumented_task(
@@ -123,4 +127,8 @@ def _process_segment(project_id, segment_id):
 )
 def process_segment(project_id, segment_id):
     mark_scope_as_unsafe()
-    _process_segment(project_id, segment_id)
+    try:
+        _process_segment(project_id, segment_id)
+    except Exception as err:
+        if random.random() < 0.05:
+            sentry_sdk.capture_exception(err)
