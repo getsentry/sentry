04467a027a5a45f2702ed5a0a0874dc024f29a61
Colton Allen <colton.allen@sentry.io>
2024-02-22 11:00:41 -0600

    Fix imports

--- src/sentry/replays/endpoints/project_replay_recording_segment_details.py
+++ src/sentry/replays/endpoints/project_replay_recording_segment_details.py
@@ -18,7 +18,7 @@ from sentry.apidocs.constants import RESPONSE_BAD_REQUEST, RESPONSE_FORBIDDEN, R
 from sentry.apidocs.examples.replay_examples import ReplayExamples
 from sentry.apidocs.parameters import GlobalParams, ReplayParams
 from sentry.apidocs.utils import inline_sentry_response_serializer
-from sentry.replays.lib.storage import RecordingSegmentStorageMeta
+from sentry.replays.lib.storage.legacy import RecordingSegmentStorageMeta
 from sentry.replays.types import ReplayRecordingSegment
 from sentry.replays.usecases.reader import download_segment, fetch_segment_metadata
 
