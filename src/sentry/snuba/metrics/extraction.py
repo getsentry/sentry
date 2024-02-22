179b7acd2b78def9818ea8018a91fd8f87c592ea
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-02-22 09:47:07 -0500

    test(metrics_extraction): Remove a test and clean up

--- src/sentry/snuba/metrics/extraction.py
+++ src/sentry/snuba/metrics/extraction.py
@@ -1063,12 +1063,10 @@ def user_misery_tag_spec(project: Project, arguments: Sequence[str] | None) -> l
 
 
 # This is used to map a metric to a function which generates a specification
-_DERIVED_METRICS: dict[MetricOperationType, TagsSpecsGenerator | None] = {
+_DERIVED_METRICS: dict[MetricOperationType, TagsSpecsGenerator] = {
     "on_demand_failure_count": failure_tag_spec,
     "on_demand_failure_rate": failure_tag_spec,
     "on_demand_apdex": apdex_tag_spec,
-    "on_demand_epm": None,
-    "on_demand_eps": None,
     "on_demand_count_web_vitals": count_web_vitals_spec,
     "on_demand_user_misery": user_misery_tag_spec,
 }
