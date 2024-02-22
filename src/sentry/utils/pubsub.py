535109c6710a2aa47609bd916f3dcbc6fcd9d6d2
Colton Allen <colton.allen@sentry.io>
2024-02-22 11:46:44 -0600

    Reset pubsub module back to its default state

--- src/sentry/utils/pubsub.py
+++ src/sentry/utils/pubsub.py
@@ -2,24 +2,14 @@ from confluent_kafka import Producer
 
 
 class KafkaPublisher:
-    def __init__(self, connection, asynchronous=True):
+    def __init__(self, connection, asynchronous: bool = True):
         self.producer = Producer(connection or {})
         self.asynchronous = asynchronous
 
-    def flush(self):
-        """Manually flush the messages to Kafka.
-
-        Only useful when asynchronous is set to `True`.
-        """
-        self.producer.flush()
-
-    # CHANGE(cmanallen) - Added `send_async` argument. Publishers are typically initialized as
-    # singletons meaning different uses of the singleton in the same memory region may conflict
-    # with one another. By passing a stateless argument you can guarantee messages are published
-    # asynchronously without considering alternate uses of the producer.
-    def publish(self, channel, value, key=None, send_async: bool = False):
+    def publish(self, channel, value, key=None):
+        """Publish a message to Kafka."""
         self.producer.produce(topic=channel, value=value, key=key)
-        if self.asynchronous or send_async:
+        if self.asynchronous:
             self.producer.poll(0)
         else:
             self.producer.flush()
