3e3608d41af25352a07b1e486aeed83ce621a3fc
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-02-22 14:30:11 -0800

    Remove priority from backend

--- src/sentry/issues/endpoints/organization_group_index.py
+++ src/sentry/issues/endpoints/organization_group_index.py
@@ -185,10 +185,6 @@ class OrganizationGroupIndexEndpoint(OrganizationEndpoint):
                 result = inbox_search(**query_kwargs)
             else:
                 query_kwargs["referrer"] = "search.group_index"
-                # TODO(snigdha): remove this once we've migrated saved searches to replace priority with trends
-                if query_kwargs["sort_by"] == "priority":
-                    query_kwargs["sort_by"] = "trends"
-
                 result = search.query(**query_kwargs)
             return result, query_kwargs
 
