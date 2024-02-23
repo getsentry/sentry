cbc585b2a392e776ec633b6ade475b764ddaf21f
Abdkhan14 <60121741+Abdkhan14@users.noreply.github.com>
2024-02-23 16:57:01 -0500

    feat(new-trace-view): Adding the trace footer with trace level webvitals and tag summary (#65705)
    <img width="1267" alt="Screenshot 2024-02-23 at 10 53 11 AM"
    src="https://github.com/getsentry/sentry/assets/60121741/86e14c79-49ef-434e-a27b-dee2205d2ce9">
    <img width="1275" alt="Screenshot 2024-02-23 at 10 54 27 AM"
    src="https://github.com/getsentry/sentry/assets/60121741/e06cfdb6-1103-4876-a36a-dad15f8e46f0">
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/performance/newTraceDetails/index.tsx
+++ static/app/views/performance/newTraceDetails/index.tsx
@@ -30,6 +30,7 @@ import useProjects from 'sentry/utils/useProjects';
 import Breadcrumb from '../breadcrumb';
 
 import Trace from './trace';
+import {TraceFooter} from './traceFooter';
 import TraceHeader from './traceHeader';
 import {TraceTree} from './traceTree';
 import TraceWarnings from './traceWarnings';
@@ -205,6 +206,13 @@ function TraceViewContent(props: TraceViewContentProps) {
             traces={props.traceSplitResult}
           />
           <Trace trace={tree} trace_id={props.traceSlug} />
+          <TraceFooter
+            rootEventResults={rootEventResults}
+            organization={props.organization}
+            location={props.location}
+            traces={props.traceSplitResult}
+            traceEventView={props.traceEventView}
+          />
         </Layout.Main>
       </Layout.Body>
     </Fragment>
