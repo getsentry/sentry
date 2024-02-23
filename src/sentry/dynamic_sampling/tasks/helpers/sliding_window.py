18d94492f1581a7414f55cf102e66b0906132850
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-02-23 09:07:05 +0100

    ref(dynamic-sampling): Remove sliding window implementation

--- src/sentry/dynamic_sampling/tasks/helpers/sliding_window.py
+++ src/sentry/dynamic_sampling/tasks/helpers/sliding_window.py
@@ -19,65 +19,6 @@ SLIDING_WINDOW_CALCULATION_ERROR = "sliding_window_error"
 EXECUTED_CACHE_KEY_TTL = 60 * 60 * 1000
 
 
-def generate_sliding_window_executed_cache_key() -> str:
-    return "ds::sliding_window_executed"
-
-
-def mark_sliding_window_executed() -> None:
-    redis_client = get_redis_client_for_ds()
-    cache_key = generate_sliding_window_executed_cache_key()
-
-    redis_client.set(cache_key, 1)
-    redis_client.pexpire(cache_key, EXECUTED_CACHE_KEY_TTL)
-
-
-def was_sliding_window_executed() -> bool:
-    redis_client = get_redis_client_for_ds()
-    cache_key = generate_sliding_window_executed_cache_key()
-
-    return bool(redis_client.exists(cache_key))
-
-
-def generate_sliding_window_cache_key(org_id: int) -> str:
-    return f"ds::o:{org_id}:sliding_window"
-
-
-def get_sliding_window_sample_rate(
-    org_id: int, project_id: int, error_sample_rate_fallback: float
-) -> float | None:
-    redis_client = get_redis_client_for_ds()
-    cache_key = generate_sliding_window_cache_key(org_id=org_id)
-
-    try:
-        value = redis_client.hget(cache_key, project_id)
-        # In case we had an explicit error or the sliding window was not run, we want to return the error fallback
-        # sample rate.
-        if value == SLIDING_WINDOW_CALCULATION_ERROR:
-            sentry_sdk.capture_message("Sliding window calculation error stored in cache")
-            return error_sample_rate_fallback
-
-        return float(value)
-    # Thrown if the input is not a string or a float (e.g., None).
-    except TypeError:
-        # In case we couldn't convert the value to float, that is, it is a string or the value is not there, we want
-        # to fall back to 100% in case we know that the sliding window was executed. We track whether the task was
-        # executed and completed successfully under the assumption that, if that is the case, all orgs and projects
-        # with at least 1 metric were considered, thus if they are not in cache, we assume they had 0 metrics.
-        if was_sliding_window_executed():
-            return 1.0
-
-        # In the other case were the sliding window was not run, maybe because of an issue, we will just fallback to
-        # blended sample rate, to avoid oversampling.
-        sentry_sdk.capture_message(
-            "Sliding window value not stored in cache and sliding window not executed"
-        )
-        return error_sample_rate_fallback
-    # Thrown if the input is not a valid float.
-    except ValueError:
-        sentry_sdk.capture_message("Invalid sliding window value stored in cache")
-        return error_sample_rate_fallback
-
-
 def generate_sliding_window_org_executed_cache_key() -> str:
     return "ds::sliding_window_org_executed"
 
