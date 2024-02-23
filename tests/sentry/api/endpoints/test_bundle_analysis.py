fae33d63e61bab1b92119860f9c64ee15a5a2d59
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-02-23 13:15:19 -0500

    update tests

--- tests/sentry/api/endpoints/test_bundle_analysis.py
+++ tests/sentry/api/endpoints/test_bundle_analysis.py
@@ -1,10 +1,22 @@
 from django.urls import reverse
 
+from sentry.models.apitoken import ApiToken
+from sentry.silo import SiloMode
 from sentry.testutils.cases import APITestCase
-from sentry.testutils.silo import region_silo_test
+from sentry.testutils.silo import assume_test_silo_mode, region_silo_test
 from sentry.testutils.skips import requires_snuba
 
 pytestmark = [requires_snuba]
+
+data = {
+    "total_size": 210,
+    "javascript_size": 130,
+    "css_size": 20,
+    "fonts_size": 20,
+    "images_size": 40,
+    "bundle_name": "bundle_1",
+    "environment": "test",
+}
 
 
 @region_silo_test
@@ -16,14 +28,27 @@ class BundleAnalysisEndpoint(APITestCase):
         self.organization.save()
         self.team1 = self.create_team(organization=self.organization)
         self.project1 = self.create_project(teams=[self.team1], organization=self.organization)
-
-    def test_raises_permission_denied_if_missing_feature(self):
-        url = reverse(
+        self.url = reverse(
             "sentry-api-0-organization-bundle-size",
             kwargs={
                 "organization_slug": self.organization.slug,
                 "project_slug": self.project1.slug,
             },
         )
-        response = self.client.post(url)
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            self.token = ApiToken.objects.create(user=self.user, scope_list=["project:write"])
+
+        self.login_as(user=self.user1)
+
+    def test_raises_permission_denied_if_missing_feature(self):
+        response = self.client.post(self.url, data)
         assert response.status_code == 403
+
+    def test_adds_metrics(self):
+        with self.feature({"organizations:starfish-browser-resource-module-bundle-analysis": True}):
+            response = self.client.post(
+                self.url,
+                data,
+                HTTP_AUTHORIZATION=f"Bearer {self.token.token}",
+            )
+            assert response.status_code == 200
