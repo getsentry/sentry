d0d345cf4578565acb20436f291d92471a039730
Colton Allen <colton.allen@sentry.io>
2024-02-26 16:10:25 -0600

    Reset pubsub module

--- src/sentry/utils/pubsub.py
+++ src/sentry/utils/pubsub.py
@@ -2,22 +2,13 @@ from confluent_kafka import Producer
 
 
 class KafkaPublisher:
-    def __init__(self, connection, asynchronous: bool = True):
+    def __init__(self, connection, asynchronous=True):
         self.producer = Producer(connection or {})
         self.asynchronous = asynchronous
 
     def publish(self, channel, value, key=None):
-        """Publish a message to Kafka."""
         self.producer.produce(topic=channel, value=value, key=key)
         if self.asynchronous:
-            self.poll(0)
+            self.producer.poll(0)
         else:
-            self.flush()
-
-    def flush(self) -> None:
-        """Block and flush messages in the client buffer to Kafka."""
-        self.producer.flush()
-
-    def poll(self, timeout: int) -> None:
-        """Poll the producer."""
-        self.producer.poll(timeout)
+            self.producer.flush()
