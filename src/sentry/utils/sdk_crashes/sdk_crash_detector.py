4d384f5baa270ee0782a99a0503434c4d758c32f
Philipp Hofmann <philipp.hofmann@sentry.io>
2024-02-27 08:55:01 +0100

    ref(sdk-crash): Add KeepFieldPathReplacer (#65773)
    Add the KeepFieldPathReplacer, which only looks at the field key and
    keeps the full path if it matches. This is required to add the SDK crash
    detection for Android/Java.

--- src/sentry/utils/sdk_crashes/sdk_crash_detector.py
+++ src/sentry/utils/sdk_crashes/sdk_crash_detector.py
@@ -20,8 +20,8 @@ class SDKCrashDetector:
     def fields_containing_paths(self) -> set[str]:
         return {"package", "module", "abs_path", "filename"}
 
-    def replace_sdk_frame_path(self, path: str) -> str:
-        return self.config.sdk_frame_config.path_replacer.replace_path(path)
+    def replace_sdk_frame_path(self, path_field: str, path_value: str) -> str | None:
+        return self.config.sdk_frame_config.path_replacer.replace_path(path_field, path_value)
 
     def is_sdk_supported(
         self,
