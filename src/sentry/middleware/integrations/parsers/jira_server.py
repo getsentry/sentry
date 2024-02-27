e4b3ee1d51a72d594757f42e6b79bf8f4d23220c
Mark Story <mark@mark-story.com>
2024-02-27 16:34:00 -0500

    fix(hybridcloud) Don't forward no-op webhooks (#65863)
    There are a few high-volume jira-server integrations in production.
    These integrations generate large volumes of webhooks that result in
    no-ops as they don't contain the `changelog` attribute we rely on.
    
    Instead of storing these payloads and forwarding them to the region we
    can acknowledge and drop them in control silo.

--- src/sentry/middleware/integrations/parsers/jira_server.py
+++ src/sentry/middleware/integrations/parsers/jira_server.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 
 import logging
 
+from django.http import HttpResponse
+
 from sentry.integrations.jira_server.webhooks import (
     JiraServerIssueUpdatedWebhook,
     get_integration_from_token,
 )
 from sentry.middleware.integrations.parsers.base import BaseRequestParser
 from sentry.models.outbox import WebhookProviderIdentifier
+from sentry.utils import json
 
 logger = logging.getLogger(__name__)
 
@@ -22,9 +25,21 @@ class JiraServerRequestParser(BaseRequestParser):
             integration = get_integration_from_token(token)
         except ValueError as e:
             logger.info("%s.no_integration", self.provider, extra={"error": str(e)})
-            return self.get_response_from_control_silo()
+            return HttpResponse(status=200)
+
         organizations = self.get_organizations_from_integration(integration=integration)
         regions = self.get_regions_from_organizations(organizations=organizations)
+
+        try:
+            data = json.loads(self.request.body)
+        except ValueError:
+            data = {}
+
+        # We only process webhooks with changelogs
+        if not data.get("changelog"):
+            logger.info("missing-changelog", extra={"integration_id": integration.id})
+            return HttpResponse(status=200)
+
         return self.get_response_from_outbox_creation_for_integration(
             regions=regions, integration=integration
         )
@@ -32,4 +47,5 @@ class JiraServerRequestParser(BaseRequestParser):
     def get_response(self):
         if self.view_class == JiraServerIssueUpdatedWebhook:
             return self.get_response_from_issue_update_webhook()
+
         return self.get_response_from_control_silo()
