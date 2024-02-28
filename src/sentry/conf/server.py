d770727d01aed8b7c8bde48958ec0b7cabfd60e3
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2024-02-28 10:57:38 -0800

    rm feature flag

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1515,8 +1515,6 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     "organizations:ddm-dashboard-import": False,
     # Enable the default alert at project creation to be the high priority alert
     "organizations:default-high-priority-alerts": False,
-    # Enable inbound filters to be turned on by default for new Javascript Projects
-    "organizations:default-inbound-filters": False,
     # Enables automatically deriving of code mappings
     "organizations:derive-code-mappings": True,
     # Enable device.class as a selectable column
