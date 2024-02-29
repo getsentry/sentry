399617de84c25ce10bd8a0bb0f79e990e74458ee
schew2381 <seiji.chew@sentry.io>
2024-02-28 17:25:53 -0800

    update name

--- src/sentry/receivers/staff.py
+++ src/sentry/receivers/staff.py
@@ -2,9 +2,9 @@ from django.contrib.auth.signals import user_logged_out
 
 
 def disable_staff(request, user, **kwargs):
-    su = getattr(request, "staff", None)
-    if su:
-        su.set_logged_out()
+    staff = getattr(request, "staff", None)
+    if staff:
+        staff.set_logged_out()
 
 
 user_logged_out.connect(disable_staff, dispatch_uid="disable_staff", weak=False)
