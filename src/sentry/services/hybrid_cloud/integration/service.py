65321ffd2dd9dcd4d3c41ddd1df2696876651454
Mark Story <mark@mark-story.com>
2024-02-29 18:38:02 -0500

    chore(hybridcloud) Remove unused parameter from sent_incident_alert_notification
    Complete work started in #65736, #65791, and #65883 to remove
    a parameter from the send_incident_alert_notification RPC method.
    
    Fixes HC-1123
    Fixes SENTRY-2QAG

--- src/sentry/services/hybrid_cloud/integration/service.py
+++ src/sentry/services/hybrid_cloud/integration/service.py
@@ -12,7 +12,6 @@ from sentry.services.hybrid_cloud.integration.model import (
     RpcIntegrationExternalProject,
     RpcIntegrationIdentityContext,
 )
-from sentry.services.hybrid_cloud.organization import RpcOrganizationSummary
 from sentry.services.hybrid_cloud.pagination import RpcPaginationArgs, RpcPaginationResult
 from sentry.services.hybrid_cloud.rpc import RpcService, rpc_method
 from sentry.silo import SiloMode
@@ -238,8 +237,7 @@ class IntegrationService(RpcService):
         incident_id: int,
         new_status: int,
         incident_attachment_json: str,
-        organization: RpcOrganizationSummary | None = None,
-        organization_id: int | None = None,
+        organization_id: int,
         metric_value: str | None = None,
         notification_uuid: str | None = None,
     ) -> bool:
