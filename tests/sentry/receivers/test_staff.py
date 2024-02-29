8df9672cc0cd9fb4b154df9b45fe271b46f6811c
schew2381 <seiji.chew@sentry.io>
2024-02-28 17:22:27 -0800

    feat(staff): Remove cookie automatically upon logout

---
+++ tests/sentry/receivers/test_staff.py
@@ -0,0 +1,13 @@
+from sentry.auth.staff import is_active_staff
+from sentry.receivers.staff import disable_staff
+from sentry.testutils.cases import TestCase
+
+
+class StaffReceiverTest(TestCase):
+    def test_disable_superuser_active__superuser(self):
+        staff_user = self.create_user(is_staff=True)
+        staff_request = self.make_request(user=staff_user, is_staff=True)
+
+        assert is_active_staff(staff_request)
+        disable_staff(request=staff_request, user=staff_user)
+        assert not is_active_staff(staff_request)
