ab5fad3533bb9d9a3d07498b57e1bfa33b5378a3
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-02-29 10:47:30 -0800

    ref(replay): Pass in a path to query when loading a list of replays (#65981)
    Depends on https://github.com/getsentry/sentry/pull/66086
    Fixes https://github.com/getsentry/sentry/issues/65888

--- static/app/utils/replays/hooks/useReplayList.tsx
+++ static/app/utils/replays/hooks/useReplayList.tsx
@@ -5,14 +5,17 @@ import type {Organization} from 'sentry/types';
 import type EventView from 'sentry/utils/discover/eventView';
 import fetchReplayList from 'sentry/utils/replays/fetchReplayList';
 import useApi from 'sentry/utils/useApi';
-import type {ReplayListLocationQuery} from 'sentry/views/replays/types';
+import type {
+  ReplayListLocationQuery,
+  ReplayListQueryReferrer,
+} from 'sentry/views/replays/types';
 
 type Options = {
   eventView: EventView;
   location: Location<ReplayListLocationQuery>;
   organization: Organization;
   perPage?: number;
-  queryReferrer?: 'issueReplays';
+  queryReferrer?: ReplayListQueryReferrer;
 };
 
 type State = Awaited<ReturnType<typeof fetchReplayList>> & {isFetching: boolean};
