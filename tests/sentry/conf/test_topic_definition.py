7014e8860169bcc0668914c19e578dc7558efc55
Lyn Nagara <lyn.nagara@gmail.com>
2024-02-29 11:25:44 -0800

    update test

--- tests/sentry/conf/test_topic_definition.py
+++ tests/sentry/conf/test_topic_definition.py
@@ -1,4 +1,3 @@
-# Every topic is mapped to a cluster
 from django.conf import settings
 
 from sentry.conf.types.topic_definition import Topic
@@ -7,5 +6,7 @@ from sentry.conf.types.topic_definition import Topic
 def test_topic_definition() -> None:
     for topic in Topic:
         cluster_name = settings.KAFKA_TOPIC_TO_CLUSTER[topic.value]
-        assert cluster_name in settings.KAFKA_CLUSTERS
+        assert (
+            cluster_name in settings.KAFKA_CLUSTERS
+        ), f"{cluster_name} is not defined in KAFKA_CLUSTERS"
     assert len(Topic) == len(settings.KAFKA_TOPIC_TO_CLUSTER)
