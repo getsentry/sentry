1b219a73cb5d55124b663784ec5c9d44bf30ede0
Lyn Nagara <lyn.nagara@gmail.com>
2024-02-29 09:45:50 -0800

    check cluster name exists

--- tests/sentry/conf/test_topic_definition.py
+++ tests/sentry/conf/test_topic_definition.py
@@ -6,5 +6,6 @@ from sentry.conf.types.topic_definition import Topic
 
 def test_topic_definition() -> None:
     for topic in Topic:
-        assert topic.value in settings.KAFKA_TOPIC_TO_CLUSTER
+        cluster_name = settings.KAFKA_TOPIC_TO_CLUSTER[topic.value]
+        assert cluster_name in settings.KAFKA_CLUSTERS
     assert len(Topic) == len(settings.KAFKA_TOPIC_TO_CLUSTER)
