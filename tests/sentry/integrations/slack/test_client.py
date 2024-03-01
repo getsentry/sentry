86ca9c36722a0160ace178efb6a45b64adc2cbed
Mark Story <mark@mark-story.com>
2024-03-01 12:57:16 -0500

    fix(hybridcloud) Don't use integration proxy for slack (#65861)
    Slack doesn't use refresh tokens and thus doesn't need to go through the
    integration proxy as there are no refresh tokens to coordinate.

--- tests/sentry/integrations/slack/test_client.py
+++ tests/sentry/integrations/slack/test_client.py
@@ -1,15 +1,12 @@
 import re
 
 import responses
-from django.test import override_settings
 from responses import matchers
 
 from sentry.integrations.slack.client import SlackClient
 from sentry.silo.base import SiloMode
-from sentry.silo.util import PROXY_BASE_PATH, PROXY_OI_HEADER, PROXY_PATH, PROXY_SIGNATURE_HEADER
 from sentry.testutils.cases import TestCase
 from sentry.testutils.silo import assume_test_silo_mode, region_silo_test
-from tests.sentry.integrations.test_helpers import add_control_silo_proxy_response
 
 
 @region_silo_test
@@ -39,14 +36,6 @@ class SlackClientTest(TestCase):
                 match=match,
             )
 
-            add_control_silo_proxy_response(
-                method=responses.POST,
-                path="chat.postMessage",
-                status=200,
-                json=json,
-                additional_matchers=match,
-            )
-
         _add_response(
             json=self.mock_user_access_token_response,
             match=[
@@ -63,47 +52,6 @@ class SlackClientTest(TestCase):
             json=self.mock_not_authed_response,
             match=[matchers.header_matcher({})],
         )
-
-    def assert_proxy_request(self, request, is_proxy=True):
-        assert (PROXY_BASE_PATH in request.url) == is_proxy
-        assert (PROXY_OI_HEADER in request.headers) == is_proxy
-        assert (PROXY_SIGNATURE_HEADER in request.headers) == is_proxy
-        if is_proxy:
-            assert request.headers[PROXY_OI_HEADER] is not None
-
-    @responses.activate
-    def test_integration_proxy_is_active(self):
-        class SlackProxyTestClient(SlackClient):
-            _use_proxy_url_for_tests = True
-
-        with override_settings(SILO_MODE=SiloMode.MONOLITH):
-            client = SlackProxyTestClient(integration_id=self.integration.id)
-            client.post("/chat.postMessage", data=self.payload)
-            request = responses.calls[0].request
-
-            assert "/chat.postMessage" in request.url
-            assert client.base_url in request.url
-            self.assert_proxy_request(request, is_proxy=False)
-
-        responses.calls.reset()
-        with override_settings(SILO_MODE=SiloMode.CONTROL):
-            client = SlackProxyTestClient(integration_id=self.integration.id)
-            client.post("/chat.postMessage", data=self.payload)
-            request = responses.calls[0].request
-
-            assert "/chat.postMessage" in request.url
-            assert client.base_url in request.url
-            self.assert_proxy_request(request, is_proxy=False)
-
-        responses.calls.reset()
-        with override_settings(SILO_MODE=SiloMode.REGION):
-            client = SlackProxyTestClient(integration_id=self.integration.id)
-            client.post("/chat.postMessage", data=self.payload)
-            request = responses.calls[0].request
-
-            assert request.headers[PROXY_PATH] == "chat.postMessage"
-            assert client.base_url not in request.url
-            self.assert_proxy_request(request, is_proxy=True)
 
     @responses.activate
     def test_authorize_with_no_id_noop(self):
@@ -123,13 +71,6 @@ class SlackClientTest(TestCase):
 
     @responses.activate
     @assume_test_silo_mode(SiloMode.CONTROL)
-    def test_authorize_with_org_integration_id(self):
-        client = SlackClient(org_integration_id=self.organization_integration.id)
-        response = client.post("/chat.postMessage", data=self.payload)
-        assert response == self.mock_access_token_response
-
-    @responses.activate
-    @assume_test_silo_mode(SiloMode.CONTROL)
     def test_authorize_with_integration_id(self):
         client = SlackClient(integration_id=self.integration.id)
         response = client.post("/chat.postMessage", data=self.payload)
@@ -139,22 +80,18 @@ class SlackClientTest(TestCase):
     @assume_test_silo_mode(SiloMode.CONTROL)
     def test_authorize_user_access_token(self):
         self.integration.update(metadata={"user_access_token": self.user_access_token})
-        client = SlackClient(org_integration_id=self.organization_integration.id)
+        client = SlackClient(integration_id=self.integration.id)
         response = client.post("/chat.postMessage", data=self.payload)
         assert response == self.mock_user_access_token_response
 
     @responses.activate
     def test_no_authorization_in_region_mode(self):
-        client = SlackClient(org_integration_id=self.organization_integration.id)
-        response = client.post("/chat.postMessage", data=self.payload)
-        assert response == self.mock_not_authed_response
-
         client = SlackClient(integration_id=self.integration.id)
         response = client.post("/chat.postMessage", data=self.payload)
-        assert response == self.mock_not_authed_response
+        assert response == self.mock_access_token_response
 
         with assume_test_silo_mode(SiloMode.CONTROL):
             self.integration.update(metadata={"user_access_token": self.user_access_token})
-        client = SlackClient(org_integration_id=self.organization_integration.id)
+        client = SlackClient(integration_id=self.integration.id)
         response = client.post("/chat.postMessage", data=self.payload)
-        assert response == self.mock_not_authed_response
+        assert response == self.mock_user_access_token_response
