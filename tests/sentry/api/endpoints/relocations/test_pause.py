679c12ea793950d4577c2969b246850a6296439b
Seiji Chew <67301797+schew2381@users.noreply.github.com>
2024-03-01 10:46:45 -0800

    Update tests/sentry/api/endpoints/relocations/test_pause.py
    Co-authored-by: Alex Zaslavsky <azaslavsky@users.noreply.github.com>

--- tests/sentry/api/endpoints/relocations/test_pause.py
+++ tests/sentry/api/endpoints/relocations/test_pause.py
@@ -44,7 +44,7 @@ class PauseRelocationTest(APITestCase):
         )
 
     @with_feature("auth:enterprise-staff-cookie")
-    def test_staff_good_pause_asap(self):
+    def test_good_staff_pause_asap(self):
         self.login_as(user=self.staff_user, staff=True)
         response = self.get_success_response(self.relocation.uuid, status_code=200)
 
