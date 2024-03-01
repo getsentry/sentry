0afb6d0b50a6630735c6634ba929a6b20fcfaeec
Arthur Knaus <arthur.knaus@sentry.io>
2024-03-01 13:46:27 +0100

    ref(ddm): Pass blocked metric tags to search bar

--- static/app/utils/metrics/index.tsx
+++ static/app/utils/metrics/index.tsx
@@ -28,7 +28,6 @@ import type {
   MRI,
   UseCase,
 } from 'sentry/types/metrics';
-import {statsPeriodToDays} from 'sentry/utils/dates';
 import {isMeasurement as isMeasurementName} from 'sentry/utils/discover/fields';
 import {generateEventSlug} from 'sentry/utils/discover/urls';
 import {getMeasurements} from 'sentry/utils/measurements/measurements';
@@ -224,7 +223,7 @@ export function useClearQuery() {
   }, [routerRef]);
 }
 
-export function formatMetricsFormula(formula: string) {
+export function unescapeMetricsFormula(formula: string) {
   // Remove the $ from variable names
   return formula.replaceAll('$', '');
 }
@@ -236,7 +235,7 @@ export function getMetricsSeriesName(
 ) {
   let name = '';
   if (isMetricFormula(query)) {
-    name = formatMetricsFormula(query.formula);
+    name = unescapeMetricsFormula(query.formula);
   } else {
     name = formatMRIField(MRIToField(query.mri, query.op));
   }
@@ -420,9 +419,6 @@ export function getMetricsCorrelationSpanUrl(
 
 export function getMetaDateTimeParams(datetime?: PageFilters['datetime']) {
   if (datetime?.period) {
-    if (statsPeriodToDays(datetime.period) < 14) {
-      return {statsPeriod: '14d'};
-    }
     return {statsPeriod: datetime.period};
   }
   if (datetime?.start && datetime?.end) {
