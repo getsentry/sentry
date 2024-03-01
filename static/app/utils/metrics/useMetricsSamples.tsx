946a7686832125e6b0ac9042113be6168b8c5501
Tony Xiao <txiao@sentry.io>
2024-03-01 12:12:35 -0500

    feat(metrics): Render summary for custom metrics
    When rendering samples for custom metrics, show a summary of the metric to make
    it clear why these samples are shown. It should also respect the op chosen. For
    example, when the op is `count`, we should show a metric count within the span.
    One caveat is that when a quantile is chosen, we opt to show the avg because we
    dont not have the quantile available.

--- static/app/utils/metrics/useMetricsSamples.tsx
+++ static/app/utils/metrics/useMetricsSamples.tsx
@@ -1,10 +1,40 @@
 import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
-import type {MRI, PageFilters} from 'sentry/types';
+import type {DateString, MRI, PageFilters} from 'sentry/types';
 import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 
-interface UseMetricSamplesOptions<F extends string> {
+/**
+ * This type is incomplete as there are other fields available.
+ */
+type FieldTypes = {
+  id: string;
+  profile_id: string | undefined;
+  project: string;
+  'span.description': string;
+  'span.duration': number;
+  'span.op': string;
+  'span.self_time': number;
+  timestamp: DateString;
+  trace: string;
+  'transaction.id': string;
+};
+
+export type Summary = {
+  count: number;
+  max: number;
+  min: number;
+  sum: number;
+};
+
+type ResultFieldTypes = FieldTypes & {
+  summary: Summary;
+};
+
+export type Field = keyof FieldTypes;
+export type ResultField = keyof ResultFieldTypes;
+
+interface UseMetricSamplesOptions<F extends Field> {
   fields: F[];
   referrer: string;
   datetime?: PageFilters['datetime'];
@@ -17,14 +47,12 @@ interface UseMetricSamplesOptions<F extends string> {
   sort?: string;
 }
 
-export interface MetricsSamplesResults<F extends string> {
-  data: {
-    [K in F]: string[] | string | number | null;
-  }[];
+export interface MetricsSamplesResults<F extends Field> {
+  data: Pick<ResultFieldTypes, F | 'summary'>[];
   meta: any; // not going to type this yet
 }
 
-export function useMetricsSamples<F extends string>({
+export function useMetricsSamples<F extends Field>({
   datetime,
   enabled,
   fields,
