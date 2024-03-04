7199b039f5ee1b0b4d6a011da0d97794bc62f8b3
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-03-04 12:04:51 -0500

    Replace some metrics ribbons with readouts

--- static/app/views/starfish/views/spanSummaryPage/sampleList/sampleInfo/index.tsx
+++ static/app/views/starfish/views/spanSummaryPage/sampleList/sampleInfo/index.tsx
@@ -1,37 +1,26 @@
-import type {CSSProperties} from 'react';
 import styled from '@emotion/styled';
 
-import {RateUnit} from 'sentry/utils/discover/fields';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {DurationUnit, RateUnit} from 'sentry/utils/discover/fields';
 import {usePageAlert} from 'sentry/utils/performance/contexts/pageAlert';
-import {CountCell} from 'sentry/views/starfish/components/tableCells/countCell';
-import {DurationCell} from 'sentry/views/starfish/components/tableCells/durationCell';
-import {ThroughputCell} from 'sentry/views/starfish/components/tableCells/throughputCell';
-import {TimeSpentCell} from 'sentry/views/starfish/components/tableCells/timeSpentCell';
+import {MetricReadout} from 'sentry/views/performance/metricReadout';
+import {getTimeSpentExplanation} from 'sentry/views/starfish/components/tableCells/timeSpentCell';
 import {useSpanMetrics} from 'sentry/views/starfish/queries/useSpanMetrics';
-import {SpanMetricsField} from 'sentry/views/starfish/types';
-import {DataTitles, getThroughputTitle} from 'sentry/views/starfish/views/spans/types';
-import {Block, BlockContainer} from 'sentry/views/starfish/views/spanSummaryPage/block';
+import {SpanFunction, SpanMetricsField} from 'sentry/views/starfish/types';
+import {getThroughputTitle} from 'sentry/views/starfish/views/spans/types';
 
 const {SPAN_SELF_TIME, SPAN_OP} = SpanMetricsField;
-
-const DEFAULT_DISPLAYED_METRICS = [
-  'spm()',
-  `avg(${SPAN_SELF_TIME})`,
-  'time_spent_percentage()',
-];
 
 type Props = {
   groupId: string;
   transactionName: string;
-  displayedMetrics?: string[];
   transactionMethod?: string;
 };
 
 function SampleInfo(props: Props) {
   const {groupId, transactionName, transactionMethod} = props;
   const {setPageError} = usePageAlert();
-
-  const displayedMetrics = props.displayedMetrics ?? DEFAULT_DISPLAYED_METRICS;
 
   const filters = {
     'span.group': groupId,
@@ -42,7 +31,11 @@ function SampleInfo(props: Props) {
     filters['transaction.method'] = transactionMethod;
   }
 
-  const {data, error} = useSpanMetrics({
+  const {
+    data,
+    error,
+    isLoading: areSpanMetricsLoading,
+  } = useSpanMetrics({
     filters,
     fields: [
       SPAN_OP,
@@ -57,69 +50,50 @@ function SampleInfo(props: Props) {
 
   const spanMetrics = data[0] ?? {};
 
-  const style: CSSProperties = {
-    textAlign: 'left',
-  };
-
   if (error) {
     setPageError(error.message);
   }
 
-  function getDisplayBlock(metric: string) {
-    switch (metric) {
-      case `avg(${SPAN_SELF_TIME})`:
-        return (
-          <Block key={metric} title={DataTitles.avg} alignment="left">
-            <DurationCell
-              containerProps={{style}}
-              milliseconds={spanMetrics?.[`avg(${SPAN_SELF_TIME})`]}
-            />
-          </Block>
-        );
-      case 'count()':
-        return (
-          <Block key={metric} title={DataTitles.count} alignment="left">
-            <CountCell containerProps={{style}} count={spanMetrics?.['count()']} />
-          </Block>
-        );
-      case 'time_spent_percentage()':
-        return (
-          <Block title={DataTitles.timeSpent} alignment="left">
-            <TimeSpentCell
-              containerProps={{style}}
-              percentage={spanMetrics?.[`time_spent_percentage()`]}
-              total={spanMetrics?.[`sum(${SPAN_SELF_TIME})`]}
-              op={spanMetrics?.['span.op']}
-            />
-          </Block>
-        );
-      case 'spm()':
-        return (
-          <Block
-            key={metric}
-            title={getThroughputTitle(spanMetrics?.[SPAN_OP])}
-            alignment="left"
-          >
-            <ThroughputCell
-              containerProps={{style}}
-              rate={spanMetrics?.['spm()']}
-              unit={RateUnit.PER_MINUTE}
-            />
-          </Block>
-        );
-      default:
-        return null;
-    }
-  }
-
   return (
     <SampleInfoContainer>
-      <BlockContainer>
-        {displayedMetrics.map(metric => getDisplayBlock(metric))}
-      </BlockContainer>
+      <MetricsRibbon>
+        <MetricReadout
+          align="left"
+          title={getThroughputTitle(spanMetrics?.[SPAN_OP])}
+          value={spanMetrics?.[`${SpanFunction.SPM}()`]}
+          unit={RateUnit.PER_MINUTE}
+          isLoading={areSpanMetricsLoading}
+        />
+
+        <MetricReadout
+          align="left"
+          title={t('Avg Duration')}
+          value={spanMetrics?.[`avg(${SpanMetricsField.SPAN_SELF_TIME})`]}
+          unit={DurationUnit.MILLISECOND}
+          isLoading={areSpanMetricsLoading}
+        />
+
+        <MetricReadout
+          align="left"
+          title={t('Time Spent')}
+          value={spanMetrics?.['sum(span.self_time)']}
+          unit={DurationUnit.MILLISECOND}
+          tooltip={getTimeSpentExplanation(
+            spanMetrics?.['time_spent_percentage()'],
+            spanMetrics?.['span.op']
+          )}
+          isLoading={areSpanMetricsLoading}
+        />
+      </MetricsRibbon>
     </SampleInfoContainer>
   );
 }
+
+const MetricsRibbon = styled('div')`
+  display: flex;
+  flex-wrap: wrap;
+  gap: ${space(4)};
+`;
 
 const SampleInfoContainer = styled('div')`
   display: flex;
