1155aa8cb769fb6ad862977894bb8b510cdb5fc6
Luca Forstner <luca.forstner@sentry.io>
2024-03-05 20:50:52 +0100

    lint

--- src/sentry/api/endpoints/source_map_debug_blue_thunder_edition.py
+++ src/sentry/api/endpoints/source_map_debug_blue_thunder_edition.py
@@ -9,7 +9,7 @@ from rest_framework.exceptions import NotFound
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry import eventstore, features
+from sentry import eventstore
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
