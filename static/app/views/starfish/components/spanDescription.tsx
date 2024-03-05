2585e198cf4af5a7fbb2ded2ffdd462ba97a559a
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2024-03-05 13:40:59 -0500

    feat(performance): Updates useIndexedSpans to accept an optional fields array (#66275)
    Updates useIndexedSpans to accept an optional fields array to allow
    selecting specific fields.
    Refactors existing call to use new function signature.

--- static/app/views/starfish/components/spanDescription.tsx
+++ static/app/views/starfish/components/spanDescription.tsx
@@ -10,7 +10,8 @@ import {
 } from 'sentry/views/starfish/components/stackTraceMiniFrame';
 import {useFullSpanFromTrace} from 'sentry/views/starfish/queries/useFullSpanFromTrace';
 import {useIndexedSpans} from 'sentry/views/starfish/queries/useIndexedSpans';
-import type {SpanIndexedField, SpanIndexedFieldTypes} from 'sentry/views/starfish/types';
+import type {SpanIndexedFieldTypes} from 'sentry/views/starfish/types';
+import {SpanIndexedField} from 'sentry/views/starfish/types';
 import {SQLishFormatter} from 'sentry/views/starfish/utils/sqlish/SQLishFormatter';
 
 interface Props {
@@ -35,11 +36,17 @@ export function DatabaseSpanDescription({
   groupId,
   preliminaryDescription,
 }: Omit<Props, 'op'>) {
-  const {data: indexedSpans, isFetching: areIndexedSpansLoading} = useIndexedSpans(
-    {'span.group': groupId},
-    [INDEXED_SPAN_SORT],
-    1
-  );
+  // TODO: we're using all SpanIndexedFields here, but maybe we should only use what we need?
+  // Truncate to 20 fields otherwise discover will complain.
+  const fields = Object.values(SpanIndexedField);
+
+  const {data: indexedSpans, isFetching: areIndexedSpansLoading} = useIndexedSpans({
+    filters: {'span.group': groupId},
+    sorts: [INDEXED_SPAN_SORT],
+    limit: 1,
+    fields,
+    referrer: 'api.starfish.span-description',
+  });
   const indexedSpan = indexedSpans?.[0];
 
   // NOTE: We only need this for `span.data`! If this info existed in indexed spans, we could skip it
