e6023706da23f3a14c149332ad2e02d15447d784
Raj Joshi <raj.joshi@sentry.io>
2024-03-06 13:31:22 -0800

    created allowlist

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -17,6 +17,9 @@ from typing import Any, Final, Union, overload
 from urllib.parse import urlparse
 
 import sentry
+from sentry.conf.api_pagination_allowlist_do_not_modify import (
+    SENTRY_API_PAGINATION_ALLOWLIST_DO_NOT_MODIFY,
+)
 from sentry.conf.types.kafka_definition import ConsumerDefinition
 from sentry.conf.types.logging_config import LoggingConfig
 from sentry.conf.types.role_dict import RoleDict
@@ -2405,6 +2408,8 @@ SENTRY_MANAGED_USER_FIELDS = ()
 
 # Secret key for OpenAI
 OPENAI_API_KEY: str | None = None
+
+SENTRY_API_PAGINATION_ALLOWLIST = SENTRY_API_PAGINATION_ALLOWLIST_DO_NOT_MODIFY
 
 SENTRY_SCOPES = {
     "org:read",
