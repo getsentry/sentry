631d7932cc4daecb3d4fe49afba40cf644ff8c08
Raj Joshi <raj.joshi@sentry.io>
2024-03-05 16:52:27 -0800

    wrote test

---
+++ tests/sentry/api/test_api_pagination_check.py
@@ -0,0 +1,36 @@
+from unittest import TestCase
+
+import pytest
+from django.http import HttpRequest
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from sentry.api.base import Endpoint
+from sentry.api.paginator import MissingPaginationError
+
+
+class APIPaginationCheckTestCase(TestCase):
+    def test_if_wrong_api_method_fails(self):
+        class ExampleEndpoint(TestCase, Endpoint):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+                self.access = "read"
+
+            # Required to go through the dispatch method
+            def initialize_request(self, request: HttpRequest, *args, **kwargs) -> HttpRequest:
+                return request
+
+            # Required to go through the dispatch method
+            def check_permissions(self, request: Request) -> None:
+                pass
+
+            def get(self, request, *args, **kwargs):
+                return Response(data=[])
+
+        # Test the endpoint, assert there is a MissingPaginationError
+        with pytest.raises(MissingPaginationError):
+            endpoint = ExampleEndpoint()
+            request = Request(HttpRequest())
+            request.method = "GET"
+            request.access = "read"
+            ExampleEndpoint.dispatch(endpoint, request)
