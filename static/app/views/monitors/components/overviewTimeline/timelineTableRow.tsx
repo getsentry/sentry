925c16a22be62ca861630269e9821b148534023f
David Wang <dwang@sentry.io>
2024-03-06 11:44:20 -0800

    feat(crons): Show project information in details area for crons (#65944)
    As we transition to a state where monitor slugs are unique to the
    project and not the org, we need to have project slug information in the
    crons listing page to differentiate duplicate cron slugs within an org.
    
    <img width="1246" alt="image"
    src="https://github.com/getsentry/sentry/assets/9372512/f31e4f75-cdc0-41d3-820e-a2928562fe73">
    
    If the project + schedule text gets too long, then it wraps to the next
    line. We have a good amount of space in the details area so opted for
    wrapping vs. ellipsizing

--- static/app/views/monitors/components/overviewTimeline/timelineTableRow.tsx
+++ static/app/views/monitors/components/overviewTimeline/timelineTableRow.tsx
@@ -6,6 +6,7 @@ import styled from '@emotion/styled';
 import {Button} from 'sentry/components/button';
 import {openConfirmModal} from 'sentry/components/confirm';
 import {DropdownMenu} from 'sentry/components/dropdownMenu';
+import ProjectBadge from 'sentry/components/idBadge/projectBadge';
 import Tag from 'sentry/components/tag';
 import {Tooltip} from 'sentry/components/tooltip';
 import {IconEllipsis} from 'sentry/icons';
@@ -13,6 +14,7 @@ import {t, tct} from 'sentry/locale';
 import {fadeIn} from 'sentry/styles/animations';
 import {space} from 'sentry/styles/space';
 import type {ObjectStatus} from 'sentry/types';
+import {trimSlug} from 'sentry/utils/trimSlug';
 import useOrganization from 'sentry/utils/useOrganization';
 import {StatusToggleButton} from 'sentry/views/monitors/components/statusToggleButton';
 import type {Monitor} from 'sentry/views/monitors/types';
@@ -68,7 +70,18 @@ export function TimelineTableRow({
           <Name>{monitor.name}</Name>
           {isDisabled && <Tag>{t('Disabled')}</Tag>}
         </DetailsHeadline>
-        <Schedule>{scheduleAsText(monitor.config)}</Schedule>
+        <ProjectScheduleDetails>
+          <DetailsText>{scheduleAsText(monitor.config)}</DetailsText>
+          <ProjectDetails>
+            <ProjectBadge
+              project={monitor.project}
+              avatarSize={12}
+              disableLink
+              hideName
+            />
+            <DetailsText>{trimSlug(monitor.project.slug)}</DetailsText>
+          </ProjectDetails>
+        </ProjectScheduleDetails>
       </DetailsLink>
       <DetailsActions>
         {onToggleStatus && (
@@ -219,13 +232,24 @@ const DetailsHeadline = styled('div')`
   grid-template-columns: 1fr minmax(30px, max-content);
 `;
 
+const ProjectScheduleDetails = styled('div')`
+  display: flex;
+  gap: ${space(1)};
+  flex-wrap: wrap;
+`;
+
+const ProjectDetails = styled('div')`
+  display: flex;
+  gap: ${space(0.5)};
+`;
+
 const Name = styled('h3')`
   font-size: ${p => p.theme.fontSizeLarge};
   margin-bottom: ${space(0.25)};
   word-break: break-word;
 `;
 
-const Schedule = styled('small')`
+const DetailsText = styled('small')`
   color: ${p => p.theme.subText};
   font-size: ${p => p.theme.fontSizeSmall};
 `;
