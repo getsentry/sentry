36de513443b47c29023382b77cf657c94cba237e
Joshua Ferge <josh.ferge@sentry.io>
2024-03-06 10:07:45 -0800

    fix(feedbacks): fix typo in feedback option projects serializer

--- src/sentry/api/serializers/models/project.py
+++ src/sentry/api/serializers/models/project.py
@@ -233,8 +233,8 @@ def format_options(attrs: dict[str, Any]) -> dict[str, Any]:
             options.get(f"sentry:{FilterTypes.ERROR_MESSAGES}", [])
         ),
         "feedback:branding": options.get("feedback:branding", "1") == "1",
-        "sentry:feedback_user_report_notification": bool(
-            options.get("sentry:feedback_user_report_notification")
+        "sentry:feedback_user_report_notifications": bool(
+            options.get("sentry:feedback_user_report_notifications")
         ),
         "sentry:feedback_ai_spam_detection": bool(options.get("sentry:feedback_ai_spam_detection")),
         "sentry:replay_rage_click_issues": options.get("sentry:replay_rage_click_issues"),
