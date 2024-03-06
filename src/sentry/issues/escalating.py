8b892a85daaca9108a96f4d1e1a9bccea53353dc
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2024-03-06 15:10:08 -0800

    spell it correctly

--- src/sentry/issues/escalating.py
+++ src/sentry/issues/escalating.py
@@ -443,7 +443,7 @@ def is_escalating(group: Group) -> tuple[bool, int | None]:
     """
     group_hourly_count = get_group_hourly_count(group)
     forecast_today = EscalatingGroupForecast.fetch_todays_forecast(group.project.id, group.id)
-    # Check if current event occurence is greater than forecast for today's date
+    # Check if current event occurrence is greater than forecast for today's date
     if forecast_today and group_hourly_count > forecast_today:
         return True, forecast_today
     return False, None
