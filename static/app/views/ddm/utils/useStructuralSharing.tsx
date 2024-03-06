2a6dc5c46346d3fe3df12ce5451ec65a1b5d14ef
Arthur Knaus <arthur.knaus@sentry.io>
2024-03-06 11:10:14 +0100

    fix(ddm): Typo

--- static/app/views/ddm/utils/useStructuralSharing.tsx
+++ static/app/views/ddm/utils/useStructuralSharing.tsx
@@ -57,10 +57,10 @@ export function structuralSharing<T>(oldValue: T, newValue: T): T {
 }
 
 export function useStructuralSharing<T>(value: T): T {
-  const previeousValue = useRef<T>(value);
+  const previousValue = useRef<T>(value);
   return useMemo(() => {
-    const newValue = structuralSharing(previeousValue.current, value);
-    previeousValue.current = newValue;
+    const newValue = structuralSharing(previousValue.current, value);
+    previousValue.current = newValue;
     return newValue;
   }, [value]);
 }
