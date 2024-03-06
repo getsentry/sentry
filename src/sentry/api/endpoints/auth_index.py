getsentry/sentry

commit 1d9f49a7337e15bc09f2385c6dc9c73d3d554185

chore(superuser): No longer enable superuser locally upon login (#66043)
We need to use a different setting constant b/c we're setting the
original one to `False` in getsentry in
https://github.com/getsentry/getsentry/pull/13095

src/sentry/auth/superuser.py
@@ -68,7 +68,9 @@ SUPERUSER_ACCESS_CATEGORIES = getattr(settings, "SUPERUSER_ACCESS_CATEGORIES", [
 
 UNSET = object()
 
-ENABLE_SU_UPON_LOGIN_FOR_LOCAL_DEV = getattr(settings, "ENABLE_SU_UPON_LOGIN_FOR_LOCAL_DEV", False)
+DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL = getattr(
+    settings, "DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL", False
+)
 
 SUPERUSER_SCOPES = settings.SENTRY_SCOPES.union({"org:superuser"})
 
@@ -177,7 +179,7 @@ class Superuser(ElevatedMode):
 
     @staticmethod
     def _needs_validation():
-        if is_self_hosted() or ENABLE_SU_UPON_LOGIN_FOR_LOCAL_DEV:
+        if is_self_hosted() or DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL:
             return False
         return settings.VALIDATE_SUPERUSER_ACCESS_CATEGORY_AND_REASON
 

commit 023103e182e2f8dddb3cae61861191aaf3243060

chore(superuser): Add loggers to _needs_validation (#66364)
There's a bug where we're no longer requiring superuser access category
and reason validation, which is caused by
https://github.com/getsentry/sentry/pull/66043/files.

I want to validate what those booleans are in order to find out if this
is the cause of the bug.

src/sentry/api/endpoints/auth_index.py
@@ -1,6 +1,5 @@
 import logging
 
-from django.conf import settings
 from django.contrib.auth import logout
 from django.contrib.auth.models import AnonymousUser
 from django.utils.http import url_has_allowed_host_and_scheme
@@ -32,10 +31,6 @@ logger: logging.Logger = logging.getLogger(__name__)
 getsentry_logger = logging.getLogger("getsentry.staff_auth_index")
 
 PREFILLED_SU_MODAL_KEY = "prefilled_su_modal"
-
-DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL = getattr(
-    settings, "DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL", False
-)
 
 
 @control_silo_endpoint

src/sentry/auth/superuser.py
@@ -179,7 +179,15 @@ class Superuser(ElevatedMode):
 
     @staticmethod
     def _needs_validation():
-        if is_self_hosted() or DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL:
+        self_hosted = is_self_hosted()
+        logger.info(
+            "superuser.needs-validation",
+            extra={
+                "DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL": DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL,
+                "self_hosted": self_hosted,
+            },
+        )
+        if self_hosted or DISABLE_SU_FORM_U2F_CHECK_FOR_LOCAL:
             return False
         return settings.VALIDATE_SUPERUSER_ACCESS_CATEGORY_AND_REASON
 

tests/sentry/auth/test_superuser.py
@@ -191,7 +191,7 @@ class SuperuserTestCase(TestCase):
         superuser = Superuser(request, org_id=None)
         superuser.set_logged_in(request.user)
         assert superuser.is_active is True
-        assert logger.info.call_count == 2
+        assert logger.info.call_count == 3
         logger.info.assert_any_call(
             "superuser.superuser_access",
             extra={
@@ -411,7 +411,7 @@ class SuperuserTestCase(TestCase):
         superuser = Superuser(request, org_id=None)
         superuser.set_logged_in(request.user)
         assert superuser.is_active is True
-        assert logger.info.call_count == 1
+        assert logger.info.call_count == 2
         logger.info.assert_any_call(
             "superuser.logged-in",
             extra={"ip_address": "127.0.0.1", "user_id": user.id},