a2e1f67464865d8790cf629c94a7ae7d4cb5b829
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-03-07 14:25:26 -0500

    pr comments

--- static/app/components/feedback/feedbackItem/screenshotSection.tsx
+++ static/app/components/feedback/feedbackItem/screenshotSection.tsx
@@ -1,28 +1,20 @@
-import {useState} from 'react';
+import {useCallback, useMemo, useState} from 'react';
 
 import {
   useDeleteEventAttachmentOptimistic,
   useFetchEventAttachments,
 } from 'sentry/actionCreators/events';
 import {openModal} from 'sentry/actionCreators/modal';
+import Screenshot from 'sentry/components/events/eventTagsAndScreenshot/screenshot';
+import Modal, {
+  modalCss,
+} from 'sentry/components/events/eventTagsAndScreenshot/screenshot/modal';
 import Section from 'sentry/components/feedback/feedbackItem/feedbackItemSection';
 import {t} from 'sentry/locale';
 import type {Organization} from 'sentry/types';
 import type {Event} from 'sentry/types/event';
 import type {EventAttachment} from 'sentry/types/group';
 import {objectIsEmpty} from 'sentry/utils';
-
-import Screenshot from '../../events/eventTagsAndScreenshot/screenshot';
-import Modal, {modalCss} from '../../events/eventTagsAndScreenshot/screenshot/modal';
-
-const SCREENSHOT_NAMES = [
-  'screenshot.jpg',
-  'screenshot.png',
-  'screenshot-1.jpg',
-  'screenshot-1.png',
-  'screenshot-2.jpg',
-  'screenshot-2.png',
-];
 
 type Props = {
   event: Event;
@@ -31,69 +23,72 @@ type Props = {
   isShare?: boolean;
 };
 
-export function ScreenshotSection({
-  projectSlug,
-  event,
-  organization,
-  isShare = false,
-}: Props) {
+export function ScreenshotSection({projectSlug, event, organization}: Props) {
   const hasContext = !objectIsEmpty(event.user ?? {}) || !objectIsEmpty(event.contexts);
-  const {data: attachments} = useFetchEventAttachments(
-    {
-      orgSlug: organization.slug,
-      projectSlug,
-      eventId: event.id,
-    },
-    {enabled: !isShare}
-  );
+  const {data: attachments} = useFetchEventAttachments({
+    orgSlug: organization.slug,
+    projectSlug,
+    eventId: event.id,
+  });
   const {mutate: deleteAttachment} = useDeleteEventAttachmentOptimistic();
-  const screenshots =
-    attachments?.filter(({name}) => SCREENSHOT_NAMES.includes(name)) ?? [];
+  const screenshots = useMemo(() => {
+    return attachments ?? [];
+  }, [attachments]);
 
   const [screenshotInFocus, setScreenshotInFocus] = useState<number>(0);
 
-  if (!hasContext && (isShare || !screenshots.length)) {
+  const handleDeleteScreenshot = useCallback(
+    (attachmentId: string) => {
+      deleteAttachment({
+        orgSlug: organization.slug,
+        projectSlug,
+        eventId: event.id,
+        attachmentId,
+      });
+    },
+    [deleteAttachment, event.id, organization.slug, projectSlug]
+  );
+
+  const handleOpenVisualizationModal = useCallback(
+    (eventAttachment: EventAttachment, downloadUrl: string) => {
+      function handleDelete() {
+        handleDeleteScreenshot(eventAttachment.id);
+      }
+
+      openModal(
+        modalProps => (
+          <Modal
+            {...modalProps}
+            event={event}
+            orgSlug={organization.slug}
+            projectSlug={projectSlug}
+            eventAttachment={eventAttachment}
+            downloadUrl={downloadUrl}
+            onDelete={handleDelete}
+            onDownload={() => undefined}
+            attachments={screenshots}
+            attachmentIndex={screenshotInFocus}
+          />
+        ),
+        {modalCss}
+      );
+    },
+    [
+      event,
+      handleDeleteScreenshot,
+      organization.slug,
+      projectSlug,
+      screenshotInFocus,
+      screenshots,
+    ]
+  );
+
+  if (!hasContext && !screenshots.length) {
     return null;
   }
 
-  const showScreenshot = !isShare && !!screenshots.length;
+  const showScreenshot = !!screenshots.length;
   const screenshot = screenshots[screenshotInFocus];
-
-  const handleDeleteScreenshot = (attachmentId: string) => {
-    deleteAttachment({
-      orgSlug: organization.slug,
-      projectSlug,
-      eventId: event.id,
-      attachmentId,
-    });
-  };
-
-  function handleOpenVisualizationModal(
-    eventAttachment: EventAttachment,
-    downloadUrl: string
-  ) {
-    function handleDelete() {
-      handleDeleteScreenshot(eventAttachment.id);
-    }
-
-    openModal(
-      modalProps => (
-        <Modal
-          {...modalProps}
-          event={event}
-          orgSlug={organization.slug}
-          projectSlug={projectSlug}
-          eventAttachment={eventAttachment}
-          downloadUrl={downloadUrl}
-          onDelete={handleDelete}
-          onDownload={() => undefined}
-          attachments={screenshots}
-          attachmentIndex={screenshotInFocus}
-        />
-      ),
-      {modalCss}
-    );
-  }
 
   return showScreenshot ? (
     <Section title={t('Screenshot')}>
