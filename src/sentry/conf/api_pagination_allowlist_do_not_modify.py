506b9762fee7b5dc69d3365130ac68623c3da4bb
Raj Joshi <raj.joshi@sentry.io>
2024-03-06 16:06:49 -0800

    more updates to allowlist <tired>

--- src/sentry/conf/api_pagination_allowlist_do_not_modify.py
+++ src/sentry/conf/api_pagination_allowlist_do_not_modify.py
@@ -98,4 +98,5 @@ SENTRY_API_PAGINATION_ALLOWLIST_DO_NOT_MODIFY = {
     "JiraSearchEndpoint",
     "GithubSharedSearchEndpoint",
     "ProjectMonitorStatsEndpoint",
+    "ProjectRuleStatsIndexEndpoint",
 }
