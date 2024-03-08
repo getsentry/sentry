71a149db716664c46aa8db51a057aa6696b52c5f
Lyn Nagara <lyn.nagara@gmail.com>
2024-03-07 16:31:15 -0800

    remove more stuff

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -3444,8 +3444,6 @@ KAFKA_CLUSTERS: dict[str, dict[str, Any]] = {
 }
 
 # START DEPRECATED SECTION
-KAFKA_SNUBA_GENERIC_METRICS = "snuba-generic-metrics"
-KAFKA_INGEST_OCCURRENCES = "ingest-occurrences"
 KAFKA_INGEST_MONITORS = "ingest-monitors"
 KAFKA_GROUP_ATTRIBUTES = "group-attributes"
 
