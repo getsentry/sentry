f12a9d3f5ea74215973f88b9c71531c9b882bc35
Nathan Hsieh <6186377+nhsiehgit@users.noreply.github.com>
2024-03-08 14:10:20 -0800

    Move activation condition model to activations file (#66634)

--- src/sentry/incidents/models/alert_rule.py
+++ src/sentry/incidents/models/alert_rule.py
@@ -411,30 +411,6 @@ class AlertRuleTrigger(Model):
 
 
 @region_silo_only_model
-class AlertRuleActivationCondition(Model):
-    """
-    This model represents the activation condition for an activated AlertRule
-
-    label is an optional identifier for an activation condition
-    condition_type is AlertRuleActivationConditionType
-    TODO: implement extra query params for advanced conditional rules (eg. +10m after event occurs)
-    """
-
-    __relocation_scope__ = RelocationScope.Organization
-
-    alert_rule = FlexibleForeignKey("sentry.AlertRule", related_name="activation_conditions")
-    label = models.TextField()
-    condition_type = models.SmallIntegerField(null=True)
-
-    date_added = models.DateTimeField(default=timezone.now)
-
-    class Meta:
-        app_label = "sentry"
-        db_table = "sentry_alertruleactivationcondition"
-        unique_together = (("alert_rule", "label"),)
-
-
-@region_silo_only_model
 class AlertRuleTriggerExclusion(Model):
     """
     Allows us to define a specific trigger to be excluded from a query subscription
