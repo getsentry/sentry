49a32c078f265b720fa07c8779ea5f24d621c7d9
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-03-08 11:21:40 -0800

    ref: Let functional components call useProjects() & useOrganization() 1/2 (#66572)
    Don't call withProjects on a functional component

--- static/app/views/settings/organizationGeneralSettings/index.tsx
+++ static/app/views/settings/organizationGeneralSettings/index.tsx
@@ -17,11 +17,11 @@ import Panel from 'sentry/components/panels/panel';
 import PanelHeader from 'sentry/components/panels/panelHeader';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {t, tct} from 'sentry/locale';
-import type {Organization, Project} from 'sentry/types';
+import type {Organization} from 'sentry/types';
 import {trackAnalytics} from 'sentry/utils/analytics';
 import useApi from 'sentry/utils/useApi';
 import useOrganization from 'sentry/utils/useOrganization';
-import withProjects from 'sentry/utils/withProjects';
+import useProjects from 'sentry/utils/useProjects';
 import SettingsPageHeader from 'sentry/views/settings/components/settingsPageHeader';
 import TextBlock from 'sentry/views/settings/components/text/textBlock';
 import PermissionAlert from 'sentry/views/settings/organization/permissionAlert';
@@ -29,13 +29,10 @@ import {OrganizationRegionAction} from 'sentry/views/settings/organizationGenera
 
 import OrganizationSettingsForm from './organizationSettingsForm';
 
-type Props = {
-  projects: Project[];
-} & RouteComponentProps<{}, {}>;
-
-function OrganizationGeneralSettings({projects}: Props) {
+export default function OrganizationGeneralSettings({}: RouteComponentProps<{}, {}>) {
   const api = useApi();
   const organization = useOrganization();
+  const {projects} = useProjects();
 
   const removeConfirmMessage = (
     <Fragment>
@@ -144,5 +141,3 @@ function OrganizationGeneralSettings({projects}: Props) {
     </Fragment>
   );
 }
-
-export default withProjects(OrganizationGeneralSettings);
