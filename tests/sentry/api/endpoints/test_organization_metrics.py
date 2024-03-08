16b26fa12dbc3cbe5f08155650ef4e114a63368d
Tony Xiao <txiao@sentry.io>
2024-03-08 08:07:02 -0600

    feat(metrics): Allow additional sorts on metrics samples (#66545)
    This adds 2 additional sorts for some MRIs.
    - For ones from the spans use case, it can now sort by span.self_time
    - For ones from the custom use case, it can now sort by summary but
    requires the operation to be specified

--- tests/sentry/api/endpoints/test_organization_metrics.py
+++ tests/sentry/api/endpoints/test_organization_metrics.py
@@ -233,10 +233,10 @@ class OrganizationMetricsSamplesEndpointTest(BaseSpansTestCase, APITestCase):
 
         query = {
             "mri": "d:spans/duration@millisecond",
-            "field": ["id", "span.duration"],
+            "field": ["id", "span.self_time"],
             "project": [self.project.id],
             "statsPeriod": "14d",
-            "sort": "-span.duration",
+            "sort": "-span.self_time",
         }
         response = self.do_request(query)
         assert response.status_code == 200, response.data
@@ -590,26 +590,28 @@ class OrganizationMetricsSamplesEndpointTest(BaseSpansTestCase, APITestCase):
                     "count": 4,
                 }, operation
 
-        query = {
-            "mri": mri,
-            "field": ["id", "span.duration"],
-            "project": [self.project.id],
-            "statsPeriod": "14d",
-            "sort": "-span.duration",
-        }
-        response = self.do_request(query)
-        assert response.status_code == 200, response.data
-        expected = {int(span_id, 16) for span_id in span_ids}
-        actual = {int(row["id"], 16) for row in response.data["data"]}
-        assert actual == expected
-
-        for i, (val, row) in enumerate(zip(reversed(values), response.data["data"])):
-            assert row["summary"] == {
-                "min": val - 1,
-                "max": val + 1,
-                "sum": val * (len(values) - i) * 2,
-                "count": (len(values) - i) * 2,
+        for operation in ["avg", "min", "max", "count"]:
+            query = {
+                "mri": mri,
+                "field": ["id", "span.duration"],
+                "project": [self.project.id],
+                "statsPeriod": "14d",
+                "sort": "-summary",
+                "operation": operation,
             }
+            response = self.do_request(query)
+            assert response.status_code == 200, response.data
+            expected = {int(span_id, 16) for span_id in span_ids}
+            actual = {int(row["id"], 16) for row in response.data["data"]}
+            assert actual == expected
+
+            for i, (val, row) in enumerate(zip(reversed(values), response.data["data"])):
+                assert row["summary"] == {
+                    "min": val - 1,
+                    "max": val + 1,
+                    "sum": val * (len(values) - i) * 2,
+                    "count": (len(values) - i) * 2,
+                }
 
     def test_multiple_span_sample_per_time_bucket(self):
         custom_mri = "d:custom/value@millisecond"
