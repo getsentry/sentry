64aaf441d68d488a5aaddbf4d6aeae25a546976a
Edward Gou <edward.gou@sentry.io>
2024-03-11 17:04:16 -0400

    update banner to include link to enableInp docs

--- static/app/views/performance/browser/webVitals/webVitalsLandingPage.tsx
+++ static/app/views/performance/browser/webVitals/webVitalsLandingPage.tsx
@@ -128,10 +128,13 @@ export default function WebVitalsLandingPage() {
                       )}
                       <br />
                       {tct(
-                        `Users should update their Sentry SDKs to the [link:latest version (7.104.0+)] and enable the INP option to start receiving updated Performance Scores.`,
+                        `Users should update their Sentry SDKs to the [link:latest version (7.104.0+)] and [enableInp:enable the INP option] to start receiving updated Performance Scores.`,
                         {
                           link: (
                             <ExternalLink href="https://github.com/getsentry/sentry-javascript/releases/tag/7.104.0" />
+                          ),
+                          enableInp: (
+                            <ExternalLink href="https://docs.sentry.io/platforms/javascript/performance/instrumentation/automatic-instrumentation/#enableinp" />
                           ),
                         }
                       )}
