9264f5b67eb52fca6c3cac8758a827c1c1b25ce7
Yash Kamothi <yash.kamothi@gmail.com>
2024-03-11 12:36:26 -0700

    update test

--- static/app/components/modals/inviteMembersModal/index.spec.tsx
+++ static/app/components/modals/inviteMembersModal/index.spec.tsx
@@ -172,7 +172,7 @@ describe('InviteMembersModal', function () {
     expect(close).toHaveBeenCalled();
   });
 
-  it('sends all successful invites', async function () {
+  it('sends all successful invites, and removes team defaults', async function () {
     jest.mocked(useOrganization).mockReturnValue(org);
     const createMemberMock = MockApiClient.addMockResponse({
       url: `/organizations/${org.slug}/members/`,
@@ -196,6 +196,67 @@ describe('InviteMembersModal', function () {
     await userEvent.tab();
     await selectEvent.select(roleInputs[0], 'Admin');
     await selectEvent.select(teamInputs[0], '#team-slug');
+
+    await userEvent.type(emailInputs[1], 'test2@test.com');
+    await selectEvent.select(teamInputs[1], '#team-slug');
+    await userEvent.tab();
+
+    await userEvent.click(screen.getByRole('button', {name: 'Send invites (2)'}));
+
+    // Verify data sent to the backend
+    expect(createMemberMock).toHaveBeenCalledTimes(2);
+
+    expect(createMemberMock).toHaveBeenNthCalledWith(
+      1,
+      `/organizations/${org.slug}/members/`,
+      expect.objectContaining({
+        data: {email: 'test1@test.com', role: 'admin', teams: []},
+      })
+    );
+    expect(createMemberMock).toHaveBeenNthCalledWith(
+      2,
+      `/organizations/${org.slug}/members/`,
+      expect.objectContaining({
+        data: {email: 'test2@test.com', role: 'member', teams: []},
+      })
+    );
+
+    // Wait for them to finish
+    expect(
+      await screen.findByText(textWithMarkupMatcher('Sent 2 invites'))
+    ).toBeInTheDocument();
+
+    expect(screen.getByRole('button', {name: 'Close'})).toBeInTheDocument();
+    expect(screen.getByRole('button', {name: 'Send more invites'})).toBeInTheDocument();
+
+    // Send more reset the modal
+    await userEvent.click(screen.getByRole('button', {name: 'Send more invites'}));
+
+    expect(screen.getByRole('button', {name: 'Send invite'})).toBeDisabled();
+  });
+
+  it('sends all successful invites with team default', async function () {
+    jest.mocked(useOrganization).mockReturnValue(org);
+    const createMemberMock = MockApiClient.addMockResponse({
+      url: `/organizations/${org.slug}/members/`,
+      method: 'POST',
+    });
+
+    render(<InviteMembersModal {...modalProps} />);
+
+    await waitFor(() => {
+      expect(screen.getByRole('button', {name: 'Add another'})).toBeInTheDocument();
+    });
+
+    // Setup two rows, one email each, the first with a admin role.
+    await userEvent.click(screen.getByRole('button', {name: 'Add another'}));
+
+    const emailInputs = screen.getAllByRole('textbox', {name: 'Email Addresses'});
+    const roleInputs = screen.getAllByRole('textbox', {name: 'Role'});
+
+    await userEvent.type(emailInputs[0], 'test1@test.com');
+    await userEvent.tab();
+    await selectEvent.select(roleInputs[0], 'Admin');
 
     await userEvent.type(emailInputs[1], 'test2@test.com');
     await userEvent.tab();
