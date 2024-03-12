6fa4838aed64ca235733c41afa73bb8bb0c930b0
Cathy Teng <cathy.teng@sentry.io>
2024-03-12 14:20:24 -0700

    remove duplicate const

--- src/sentry/integrations/pagerduty/utils.py
+++ src/sentry/integrations/pagerduty/utils.py
@@ -28,14 +28,6 @@ PAGERDUTY_CUSTOM_PRIORITIES = {
 }  # known as severities in pagerduty
 
 
-PAGERDUTY_CUSTOM_PRIORITIES = {
-    "critical",
-    "warning",
-    "error",
-    "info",
-}  # known as severities in pagerduty
-
-
 class PagerDutyServiceDict(TypedDict):
     integration_id: int
     integration_key: str
