c572d4d686fd3dcb0871df62738b881d1b6e65ce
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2024-03-12 14:19:37 -0400

    feat(spans): Add a new recombiner consumer

---
+++ src/sentry/spans/consumers/recombine/factory.py
@@ -0,0 +1,29 @@
+import logging
+from collections.abc import Mapping
+
+from arroyo.backends.kafka.consumer import KafkaPayload
+from arroyo.processing.strategies.abstract import ProcessingStrategy, ProcessingStrategyFactory
+from arroyo.processing.strategies.commit import CommitOffsets
+from arroyo.processing.strategies.run_task import RunTask
+from arroyo.types import BrokerValue, Commit, Message, Partition
+
+from sentry.tasks.spans import process_segment
+
+logger = logging.getLogger(__name__)
+
+
+def process_message(message: Message[KafkaPayload]):
+    assert isinstance(message.value, BrokerValue)
+    process_segment(message.value)
+
+
+class RecombineSegmentStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
+    def create_with_partitions(
+        self,
+        commit: Commit,
+        partitions: Mapping[Partition, int],
+    ) -> ProcessingStrategy[KafkaPayload]:
+        return RunTask(
+            function=process_message,
+            next_step=CommitOffsets(commit),
+        )
