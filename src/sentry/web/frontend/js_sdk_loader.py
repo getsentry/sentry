beae07358a5f3c426074f5c21cc51681e341cd55
Sigrid Huemer <32902192+s1gr1d@users.noreply.github.com>
2024-03-13 11:28:40 +0100

    feat(js-loader): Only use es5/es6 bundles in v7 JS loader (#66303)
    As the coming version of the JS SDK only uses ES2017 bundles, it is no
    longer necessary to differentiate between es5 and es6 bundles.
    
    ref https://github.com/getsentry/sentry-javascript/pull/10911

--- src/sentry/web/frontend/js_sdk_loader.py
+++ src/sentry/web/frontend/js_sdk_loader.py
@@ -67,6 +67,7 @@ class JavaScriptSdkLoader(BaseView):
                 "hasDebug": False,
             }
 
+        is_v7_sdk = sdk_version >= Version("7.0.0") and sdk_version < Version("8.0.0")
         is_greater_or_equal_v7_sdk = sdk_version >= Version("7.0.0")
 
         is_lazy = True
@@ -89,12 +90,12 @@ class JavaScriptSdkLoader(BaseView):
             bundle_kind_modifier += ".replay"
             is_lazy = False
 
-        # From JavaScript SDK version 7 onwards, the default bundle code is ES6, however, in the loader we
+        # In JavaScript SDK version 7, the default bundle code is ES6, however, in the loader we
         # want to provide the ES5 version. This is why we need to modify the requested bundle name here.
         #
         # If we are loading replay, do not add the es5 modifier, as those bundles are
         # ES6 only.
-        if is_greater_or_equal_v7_sdk and not has_replay:
+        if is_v7_sdk and not has_replay:
             bundle_kind_modifier += ".es5"
 
         if has_debug:
