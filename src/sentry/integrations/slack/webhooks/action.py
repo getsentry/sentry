36f6d25bfc379d6b16c5722a8ac1ddc1d4de7b40
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2024-03-13 15:45:28 -0700

    chore(slack): log trigger id for dialog issues (#66923)

--- src/sentry/integrations/slack/webhooks/action.py
+++ src/sentry/integrations/slack/webhooks/action.py
@@ -430,6 +430,8 @@ class SlackActionEndpoint(Endpoint):
                         "error": str(e),
                         "organization_id": org.id,
                         "integration_id": slack_request.integration.id,
+                        "trigger_id": slack_request.data["trigger_id"],
+                        "dialog": "resolve",
                     },
                 )
 
@@ -447,6 +449,8 @@ class SlackActionEndpoint(Endpoint):
                 )
 
     def open_archive_dialog(self, slack_request: SlackActionRequest, group: Group) -> None:
+        org = group.project.organization
+
         callback_id = {
             "issue": group.id,
             "orig_response_url": slack_request.data["response_url"],
@@ -468,7 +472,16 @@ class SlackActionEndpoint(Endpoint):
             headers = {"content-type": "application/json; charset=utf-8"}
             slack_client.post("/views.open", data=json.dumps(payload), headers=headers)
         except ApiError as e:
-            logger.exception("slack.action.response-error", extra={"error": str(e)})
+            logger.exception(
+                "slack.action.response-error",
+                extra={
+                    "error": str(e),
+                    "organization_id": org.id,
+                    "integration_id": slack_request.integration.id,
+                    "trigger_id": slack_request.data["trigger_id"],
+                    "dialog": "archive",
+                },
+            )
 
     def construct_reply(self, attachment: SlackBody, is_message: bool = False) -> SlackBody:
         # XXX(epurkhiser): Slack is inconsistent about it's expected responses
