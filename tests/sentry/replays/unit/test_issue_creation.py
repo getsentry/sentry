4a0fe68c8df4576f127a075d35269a6a3a5142a8
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-03-13 16:39:35 -0700

    update test

--- tests/sentry/replays/unit/test_issue_creation.py
+++ tests/sentry/replays/unit/test_issue_creation.py
@@ -171,26 +171,37 @@ def test_report_rage_click_no_trace(default_project):
     assert Group.objects.get(message__contains="div.xyz > a")
 
 
-@pytest.mark.snuba
 @django_db_all
-def test_report_rage_click_no_component_name(default_project):
-    replay_id = "b58a67446c914f44a4e329763420047b"
+@patch("sentry.replays.usecases.ingest.issue_creation.new_issue_occurrence")
+def test_report_rage_click_no_component_name(mock_new_issue_occurrence, default_project):
     seq1_timestamp = datetime.now() - timedelta(minutes=10, seconds=52)
-    with Feature(
-        {
-            "organizations:replay-click-rage-ingest": True,
-        }
-    ):
-        report_rage_click_issue_with_replay_event(
-            project_id=default_project.id,
-            replay_id=replay_id,
-            selector="div.xyz > a",
-            timestamp=seq1_timestamp.timestamp(),
-            url="https://www.sentry.io",
-            node={"tagName": "a"},
-            component_name=None,
-            replay_event=mock_replay_event(),
-        )
 
-    # test that the Issue gets created with no component name
-    assert Group.objects.get(message__contains="div.xyz > a")
+    replay_id = "b58a67446c914f44a4e329763420047b"
+    report_rage_click_issue_with_replay_event(
+        project_id=default_project.id,
+        replay_id=replay_id,
+        selector="div.xyz > a",
+        timestamp=seq1_timestamp.timestamp(),
+        url="https://www.sentry.io",
+        node={"tagName": "a"},
+        component_name=None,
+        replay_event=mock_replay_event(),
+    )
+    issue_occurence_call = mock_new_issue_occurrence.call_args[1]
+    assert issue_occurence_call["culprit"] == "https://www.sentry.io"
+    assert issue_occurence_call["environment"] == "production"
+    assert issue_occurence_call["fingerprint"] == ["div.xyz > a"]
+
+    assert issue_occurence_call["evidence_data"] == {
+        "node": {"tagName": "a"},
+        "selector": "div.xyz > a",
+    }
+
+    assert (
+        issue_occurence_call["evidence_display"][0].to_dict()
+        == IssueEvidence(name="Clicked Element", value="a", important=False).to_dict()
+    )
+    assert (
+        issue_occurence_call["evidence_display"][1].to_dict()
+        == IssueEvidence(name="Selector Path", value="div.xyz > a", important=True).to_dict()
+    )
