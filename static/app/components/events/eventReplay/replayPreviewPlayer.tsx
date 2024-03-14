4d839f7490a99970d8dba399dac026e4c3d597ce
Stephen Cefali <scefali@sentry.io>
2024-03-14 14:20:53 -0700

    move full replay button

--- static/app/components/events/eventReplay/replayPreviewPlayer.tsx
+++ static/app/components/events/eventReplay/replayPreviewPlayer.tsx
@@ -3,7 +3,6 @@ import {useRef, useState} from 'react';
 import styled from '@emotion/styled';
 
 import {Button, LinkButton} from 'sentry/components/button';
-import ButtonBar from 'sentry/components/buttonBar';
 import ErrorBoundary from 'sentry/components/errorBoundary';
 import Panel from 'sentry/components/panels/panel';
 import {useReplayContext} from 'sentry/components/replays/replayContext';
@@ -82,7 +81,7 @@ function ReplayPreviewPlayer({
 
   return (
     <PlayerPanel>
-      {replayRecord && (
+      <HeaderWrapper>
         <StyledReplayCell
           key="session"
           replay={replayRecord}
@@ -90,7 +89,10 @@ function ReplayPreviewPlayer({
           organization={organization}
           referrer="issue-details-replay-header"
         />
-      )}
+        <LinkButton size="sm" to={fullReplayUrl} {...fullReplayButtonProps}>
+          {t('See Full Replay')}
+        </LinkButton>
+      </HeaderWrapper>
       <PreviewPlayerContainer ref={fullscreenRef} isSidebarOpen={isSidebarOpen}>
         <PlayerBreadcrumbContainer>
           <PlayerContextContainer>
@@ -143,12 +145,7 @@ function ReplayPreviewPlayer({
             <Container>
               <TimeAndScrubberGrid />
             </Container>
-            <ButtonBar gap={1}>
-              <LinkButton size="sm" to={fullReplayUrl} {...fullReplayButtonProps}>
-                {t('See Full Replay')}
-              </LinkButton>
-              <ReplayFullscreenButton toggleFullscreen={toggleFullscreen} />
-            </ButtonBar>
+            <ReplayFullscreenButton toggleFullscreen={toggleFullscreen} />
           </ButtonGrid>
         </ErrorBoundary>
       </PreviewPlayerContainer>
@@ -222,6 +219,14 @@ const ContextContainer = styled('div')`
 
 const StyledReplayCell = styled(ReplayCell)`
   padding: 0 0 ${space(1)};
+`;
+
+const HeaderWrapper = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  gap: ${space(1)};
+  margin-bottom: ${space(1)};
 `;
 
 export default ReplayPreviewPlayer;
