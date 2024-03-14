a6932abd397c5c4f375b3555acebfcff4eceb672
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-03-14 08:18:33 -0700

    feat(replay): add component name to rage click issue data if it exists (#66922)
    - Add the React component name to the rage click issue evidence if it
    exists.
    - The selector is still in the evidence - we'll always display it, in
    case the React component name doesn't exist.
    - Add tests to make sure the issue still gets created if the component
    name doesn't exist.
    - The order of evidence display would be 
    ``` 
    Clicked Element
    Selector Path
    React Component Name
    ```
    
    Relates to https://github.com/getsentry/sentry/issues/64673

--- tests/sentry/replays/unit/test_issue_creation.py
+++ tests/sentry/replays/unit/test_issue_creation.py
@@ -27,6 +27,7 @@ def test_report_rage_click_issue_with_replay_event(mock_new_issue_occurrence, de
         timestamp=seq1_timestamp.timestamp(),
         url="https://www.sentry.io",
         node={"tagName": "a"},
+        component_name="SmartSearchBar",
         replay_event=mock_replay_event(),
     )
     issue_occurence_call = mock_new_issue_occurrence.call_args[1]
@@ -42,15 +43,22 @@ def test_report_rage_click_issue_with_replay_event(mock_new_issue_occurrence, de
     assert issue_occurence_call["evidence_data"] == {
         "node": {"tagName": "a"},
         "selector": "div.xyz > a",
+        "component_name": "SmartSearchBar",
     }
 
     assert (
         issue_occurence_call["evidence_display"][0].to_dict()
-        == IssueEvidence(name="Clicked Element", value="a", important=True).to_dict()
+        == IssueEvidence(name="Clicked Element", value="a", important=False).to_dict()
     )
     assert (
         issue_occurence_call["evidence_display"][1].to_dict()
-        == IssueEvidence(name="Selector Path", value="div.xyz > a", important=True).to_dict()
+        == IssueEvidence(name="Selector Path", value="div.xyz > a", important=False).to_dict()
+    )
+    assert (
+        issue_occurence_call["evidence_display"][2].to_dict()
+        == IssueEvidence(
+            name="React Component Name", value="SmartSearchBar", important=True
+        ).to_dict()
     )
 
     assert issue_occurence_call["extra_event_data"] == {
@@ -103,6 +111,7 @@ def test_report_rage_click_long_url(default_project):
             timestamp=seq1_timestamp.timestamp(),
             url=f"https://www.sentry.io{'a' * 300}",
             node={"tagName": "a"},
+            component_name="SmartSearchBar",
             replay_event=mock_replay_event(),
         )
 
@@ -130,6 +139,7 @@ def test_report_rage_click_no_environment(default_project):
             timestamp=seq1_timestamp.timestamp(),
             url="https://www.sentry.io",
             node={"tagName": "a"},
+            component_name="SmartSearchBar",
             replay_event=mock_replay_event(),
         )
 
@@ -153,8 +163,45 @@ def test_report_rage_click_no_trace(default_project):
             timestamp=seq1_timestamp.timestamp(),
             url="https://www.sentry.io",
             node={"tagName": "a"},
+            component_name="SmartSearchBar",
             replay_event=mock_replay_event(trace_ids=[]),
         )
 
     # test that the Issue gets created
     assert Group.objects.get(message__contains="div.xyz > a")
+
+
+@django_db_all
+@patch("sentry.replays.usecases.ingest.issue_creation.new_issue_occurrence")
+def test_report_rage_click_no_component_name(mock_new_issue_occurrence, default_project):
+    seq1_timestamp = datetime.now() - timedelta(minutes=10, seconds=52)
+
+    replay_id = "b58a67446c914f44a4e329763420047b"
+    report_rage_click_issue_with_replay_event(
+        project_id=default_project.id,
+        replay_id=replay_id,
+        selector="div.xyz > a",
+        timestamp=seq1_timestamp.timestamp(),
+        url="https://www.sentry.io",
+        node={"tagName": "a"},
+        component_name=None,
+        replay_event=mock_replay_event(),
+    )
+    issue_occurence_call = mock_new_issue_occurrence.call_args[1]
+    assert issue_occurence_call["culprit"] == "https://www.sentry.io"
+    assert issue_occurence_call["environment"] == "production"
+    assert issue_occurence_call["fingerprint"] == ["div.xyz > a"]
+
+    assert issue_occurence_call["evidence_data"] == {
+        "node": {"tagName": "a"},
+        "selector": "div.xyz > a",
+    }
+
+    assert (
+        issue_occurence_call["evidence_display"][0].to_dict()
+        == IssueEvidence(name="Clicked Element", value="a", important=False).to_dict()
+    )
+    assert (
+        issue_occurence_call["evidence_display"][1].to_dict()
+        == IssueEvidence(name="Selector Path", value="div.xyz > a", important=True).to_dict()
+    )
