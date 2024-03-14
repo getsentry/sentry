915c9b9c9f28ccb3986eb62820c7e0f530594537
Stephen Cefali <scefali@sentry.io>
2024-03-14 13:28:43 -0700

    feat(replay): adds replay to replay tab (#66282)
    This PR adds the embedded replay viewer to the replay tab on issue
    details under the feature flag `replay-play-from-replay-tab`.
    
    ![Screenshot 2024-03-14 at 11 48
    51 AM](https://github.com/getsentry/sentry/assets/8533851/a748135f-1ae2-4831-9836-817d562e0a3a)
    
    
    In a follow up PR I will move the `See Full Replay` button that impacts
    the issue details replay viewer as well.

---
+++ static/app/components/events/eventReplay/replayClipPreviewPlayer.tsx
@@ -0,0 +1,154 @@
+import type {ComponentProps} from 'react';
+import styled from '@emotion/styled';
+
+import {Alert} from 'sentry/components/alert';
+import type {LinkButton} from 'sentry/components/button';
+import NegativeSpaceContainer from 'sentry/components/container/negativeSpaceContainer';
+import {
+  REPLAY_LOADING_HEIGHT,
+  REPLAY_LOADING_HEIGHT_LARGE,
+} from 'sentry/components/events/eventReplay/constants';
+import ReplayPreviewPlayer from 'sentry/components/events/eventReplay/replayPreviewPlayer';
+import {StaticReplayPreview} from 'sentry/components/events/eventReplay/staticReplayPreview';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {Flex} from 'sentry/components/profiling/flex';
+import MissingReplayAlert from 'sentry/components/replays/alerts/missingReplayAlert';
+import ReplayProcessingError from 'sentry/components/replays/replayProcessingError';
+import {IconDelete} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import type {IssueCategory} from 'sentry/types';
+import type {TabKey} from 'sentry/utils/replays/hooks/useActiveReplayTab';
+import type useReplayReader from 'sentry/utils/replays/hooks/useReplayReader';
+import type RequestError from 'sentry/utils/requestError/requestError';
+import useRouteAnalyticsParams from 'sentry/utils/routeAnalytics/useRouteAnalyticsParams';
+import FluidHeight from 'sentry/views/replays/detail/layout/fluidHeight';
+import type {ReplayRecord} from 'sentry/views/replays/types';
+
+type Props = {
+  analyticsContext: string;
+  orgSlug: string;
+  focusTab?: TabKey;
+  fullReplayButtonProps?: Partial<ComponentProps<typeof LinkButton>>;
+  handleBackClick?: () => void;
+  handleForwardClick?: () => void;
+  isLarge?: boolean;
+  issueCategory?: IssueCategory;
+  onClickNextReplay?: () => void;
+  overlayText?: string;
+  showNextAndPrevious?: boolean;
+} & ReturnType<typeof useReplayReader>;
+
+function getReplayAnalyticsStatus({
+  fetchError,
+  replayRecord,
+}: {
+  fetchError?: RequestError;
+  replayRecord?: ReplayRecord;
+}) {
+  if (fetchError) {
+    return 'error';
+  }
+
+  if (replayRecord?.is_archived) {
+    return 'archived';
+  }
+
+  if (replayRecord) {
+    return 'success';
+  }
+
+  return 'none';
+}
+function ReplayClipPreviewPlayer({
+  analyticsContext,
+  orgSlug,
+  fullReplayButtonProps,
+  issueCategory,
+  isLarge,
+  handleForwardClick,
+  handleBackClick,
+  overlayText,
+  fetching,
+  replay,
+  replayRecord,
+  fetchError,
+  replayId,
+  showNextAndPrevious,
+  onClickNextReplay,
+}: Props) {
+  useRouteAnalyticsParams({
+    event_replay_status: getReplayAnalyticsStatus({fetchError, replayRecord}),
+  });
+
+  if (replayRecord?.is_archived) {
+    return (
+      <Alert type="warning" data-test-id="replay-error">
+        <Flex gap={space(0.5)}>
+          <IconDelete color="gray500" size="sm" />
+          {t('The replay for this event has been deleted.')}
+        </Flex>
+      </Alert>
+    );
+  }
+
+  if (fetchError) {
+    return <MissingReplayAlert orgSlug={orgSlug} />;
+  }
+
+  if (fetching || !replayRecord || !replay) {
+    return (
+      <StyledNegativeSpaceContainer testId="replay-loading-placeholder" isLarge={isLarge}>
+        <LoadingIndicator />
+      </StyledNegativeSpaceContainer>
+    );
+  }
+
+  if (replay.getDurationMs() <= 0) {
+    return (
+      <StaticReplayPreview
+        analyticsContext={analyticsContext}
+        isFetching={false}
+        replay={replay}
+        replayId={replayId}
+        fullReplayButtonProps={fullReplayButtonProps}
+        initialTimeOffsetMs={0}
+      />
+    );
+  }
+
+  return (
+    <PlayerContainer data-test-id="player-container" isLarge={isLarge}>
+      {replay?.hasProcessingErrors() ? (
+        <ReplayProcessingError processingErrors={replay.processingErrors()} />
+      ) : (
+        <ReplayPreviewPlayer
+          replayId={replayId}
+          fullReplayButtonProps={fullReplayButtonProps}
+          replayRecord={replayRecord}
+          issueCategory={issueCategory}
+          handleBackClick={handleBackClick}
+          handleForwardClick={handleForwardClick}
+          overlayText={overlayText}
+          showNextAndPrevious={showNextAndPrevious}
+          onClickNextReplay={onClickNextReplay}
+          // if the player is large, we want to keep the priority as default
+          playPausePriority={isLarge ? 'default' : undefined}
+        />
+      )}
+    </PlayerContainer>
+  );
+}
+
+const PlayerContainer = styled(FluidHeight)<{isLarge?: boolean}>`
+  position: relative;
+  max-height: ${p =>
+    p.isLarge ? REPLAY_LOADING_HEIGHT_LARGE : REPLAY_LOADING_HEIGHT + 16}px;
+`;
+
+const StyledNegativeSpaceContainer = styled(NegativeSpaceContainer)<{isLarge?: boolean}>`
+  height: ${p => (p.isLarge ? REPLAY_LOADING_HEIGHT_LARGE : REPLAY_LOADING_HEIGHT)}px;
+  margin-bottom: ${space(2)};
+`;
+
+export default ReplayClipPreviewPlayer;
--- static/app/views/replays/replayTable/replayTableClipPlayer.tsx
+++ static/app/views/replays/replayTable/replayTableClipPlayer.tsx
@@ -1,42 +1,19 @@
-import {useMemo} from 'react';
-
 import ReplayClipPreviewPlayer from 'sentry/components/events/eventReplay/replayClipPreviewPlayer';
 import {StaticReplayPreferences} from 'sentry/components/replays/preferences/replayPreferences';
 import {Provider as ReplayContextProvider} from 'sentry/components/replays/replayContext';
 import useReplayReader from 'sentry/utils/replays/hooks/useReplayReader';
 
 type Props = {
-  clipOffsets: {
-    durationAfterMs: number;
-    durationBeforeMs: number;
-  };
-  eventTimestampMs: number;
   replaySlug: string;
 } & Omit<
   React.ComponentProps<typeof ReplayClipPreviewPlayer>,
   keyof ReturnType<typeof useReplayReader>
 >;
 
-function ReplayClipPreview({
-  analyticsContext,
-  clipOffsets,
-  eventTimestampMs,
-  orgSlug,
-  replaySlug,
-  ...props
-}: Props) {
-  const clipWindow = useMemo(
-    () => ({
-      startTimestampMs: eventTimestampMs - clipOffsets.durationBeforeMs,
-      endTimestampMs: eventTimestampMs + clipOffsets.durationAfterMs,
-    }),
-    [clipOffsets.durationBeforeMs, clipOffsets.durationAfterMs, eventTimestampMs]
-  );
-
+function ReplayTableClipPlayer({analyticsContext, orgSlug, replaySlug, ...props}: Props) {
   const replayContext = useReplayReader({
     orgSlug,
     replaySlug,
-    clipWindow,
   });
 
   const {fetching, replay} = replayContext;
@@ -57,4 +34,4 @@ function ReplayClipPreview({
     </ReplayContextProvider>
   );
 }
-export default ReplayClipPreview;
+export default ReplayTableClipPlayer;
