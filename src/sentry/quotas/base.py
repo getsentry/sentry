e30075dc355430f266d6aa77324707ef48066ed6
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-03-14 12:08:23 +0100

    Fix

--- src/sentry/quotas/base.py
+++ src/sentry/quotas/base.py
@@ -420,6 +420,13 @@ class Quota(Service):
                 scope=QuotaScope.ORGANIZATION,
             ),
             AbuseQuota(
+                id="oacm",
+                option="organization-abuse-quota.custom-metric-bucket-limit",
+                categories=[DataCategory.METRIC_BUCKET],
+                scope=QuotaScope.ORGANIZATION,
+                namespace="custom",
+            ),
+            AbuseQuota(
                 id="gam",
                 option="global-abuse-quota.metric-bucket-limit",
                 categories=[DataCategory.METRIC_BUCKET],
@@ -451,13 +458,6 @@ class Quota(Service):
                 option="global-abuse-quota.custom-metric-bucket-limit",
                 categories=[DataCategory.METRIC_BUCKET],
                 scope=QuotaScope.GLOBAL,
-                namespace="custom",
-            ),
-            AbuseQuota(
-                id="oacm",
-                option="organization-abuse-quota.custom-metric-bucket-limit",
-                categories=[DataCategory.METRIC_BUCKET],
-                scope=QuotaScope.ORGANIZATION,
                 namespace="custom",
             ),
         ]
@@ -498,9 +498,9 @@ class Quota(Service):
             # Negative limits in config mean a reject-all quota.
             if limit < 0:
                 yield QuotaConfig(
+                    limit=0,
                     scope=quota.scope,
                     categories=quota.categories,
-                    limit=0,
                     reason_code="disabled",
                     namespace=quota.namespace,
                 )
