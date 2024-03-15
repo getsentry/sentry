4e4495f389090b740b1d9a88edc2fa4111df4e5a
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-03-15 16:43:11 -0400

    🐛 ensure ssl setting is accurate

--- src/sentry/monitoring/queues.py
+++ src/sentry/monitoring/queues.py
@@ -51,6 +51,7 @@ class _ConnectionParams(TypedDict):
     userid: str | None
     password: str | None
     virtual_host: str
+    ssl: bool
 
 
 class AmqpBackend:
@@ -59,11 +60,13 @@ class AmqpBackend:
         host, port = dsn.hostname, dsn.port
         if port is None:
             port = 5672
+        has_ssl = dsn.scheme == "amqps"
         self.conn_info: _ConnectionParams = dict(
             host="%s:%d" % (host, port),
             userid=dsn.username,
             password=dsn.password,
             virtual_host=dsn.path[1:] or "/",
+            ssl=has_ssl,
         )
 
     def get_conn(self) -> Connection:
