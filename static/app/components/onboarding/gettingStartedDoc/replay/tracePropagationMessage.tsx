cc4ffd57daeb250701c8c81e387d971bab469432
Bruno Garcia <bruno@brunogarcia.com>
2024-03-17 13:37:03 -0400

    fix(link): leading slash breaks anchor (#66176)
    Clicking on this link took me to the page but not to the anchor. 
    
    Removing the leading slash fixes that

--- static/app/components/onboarding/gettingStartedDoc/replay/tracePropagationMessage.tsx
+++ static/app/components/onboarding/gettingStartedDoc/replay/tracePropagationMessage.tsx
@@ -9,7 +9,7 @@ export default function TracePropagationMessage() {
         `To see replays for backend errors, ensure that you have set up trace propagation. To learn more, [link:read the docs].`,
         {
           link: (
-            <ExternalLink href="https://docs.sentry.io/product/session-replay/getting-started/#replays-for-backend-errors/" />
+            <ExternalLink href="https://docs.sentry.io/product/session-replay/getting-started/#replays-for-backend-errors" />
           ),
         }
       )}
