04209ec95b98b94f54e48ced24f8165abb274a9f
Yash Kamothi <yash.kamothi@gmail.com>
2024-03-18 15:21:51 -0700

    feat(integration): Allow MSTeams tenant in issue (#67196)
    Orgs that have MSTeams in tenant mode cannot use issue alert actions
    because we filter them out. Enable the logic for a specific few orgs to
    test out the integration first before exposing across SaaS.
    
    Related to: https://github.com/getsentry/sentry/issues/66130

--- src/sentry/features/__init__.py
+++ src/sentry/features/__init__.py
@@ -295,6 +295,7 @@ default_manager.add("organizations:user-feedback-onboarding", OrganizationFeatur
 default_manager.add("organizations:view-hierarchies-options-dev", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:widget-viewer-modal-minimap", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 default_manager.add("organizations:replay-play-from-replay-tab", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
+default_manager.add("organizations:integrations-msteams-tenant", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
 # NOTE: Don't add features down here! Add them to their specific group and sort
 #       them alphabetically! The order features are registered is not important.
 
