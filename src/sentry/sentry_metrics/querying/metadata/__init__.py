11ebc196b81d82a8284c6da5b3753b33d2138174
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-03-18 10:00:10 +0100

    ref(ddm): Remove old metrics correlations code (#67115)

--- src/sentry/sentry_metrics/querying/metadata/__init__.py
+++ src/sentry/sentry_metrics/querying/metadata/__init__.py
@@ -1,9 +1,3 @@
 from .metrics_code_locations import MetricCodeLocations, get_metric_code_locations
-from .metrics_correlations import MetricCorrelations, get_metric_correlations
 
-__all__ = [
-    "MetricCodeLocations",
-    "get_metric_code_locations",
-    "MetricCorrelations",
-    "get_metric_correlations",
-]
+__all__ = ["MetricCodeLocations", "get_metric_code_locations"]
