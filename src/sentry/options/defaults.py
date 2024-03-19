8dc088a3a125c4eb42a4362ccf2477a0c82d46a6
David Herberth <david.herberth@sentry.io>
2024-03-19 13:10:17 +0100

    ref(relay): Add metric stats rollout rate to global config (#67221)

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -886,6 +886,9 @@ register(
 register("relay.metric-bucket-set-encodings", default={}, flags=FLAG_AUTOMATOR_MODIFIABLE)
 register("relay.metric-bucket-distribution-encodings", default={}, flags=FLAG_AUTOMATOR_MODIFIABLE)
 
+# Controls the rollout rate in percent (`0.0` to `1.0`) for metric stats.
+register("relay.metric-stats.rollout-rate", default=0.0, flags=FLAG_AUTOMATOR_MODIFIABLE)
+
 # Write new kafka headers in eventstream
 register("eventstream:kafka-headers", default=True, flags=FLAG_AUTOMATOR_MODIFIABLE)
 
