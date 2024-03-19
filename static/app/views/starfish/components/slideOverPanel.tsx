42e94acabdc2016ac7b6f371b3bfe1a40f78b7dc
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-03-19 14:36:36 -0400

    Improve small screen behaviour
    - the panel takes up almost the entire screen
    - docks slightly to the right

--- static/app/views/starfish/components/slideOverPanel.tsx
+++ static/app/views/starfish/components/slideOverPanel.tsx
@@ -1,18 +1,23 @@
 import type {ForwardedRef} from 'react';
 import {forwardRef, Fragment, useEffect} from 'react';
 import isPropValid from '@emotion/is-prop-valid';
+import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 import {motion} from 'framer-motion';
 
 import {Backdrop} from 'sentry/components/globalModal';
+import {space} from 'sentry/styles/space';
 
 const BACKDROP_OPACITY = 0.5;
+
 const PANEL_WIDTH = '50vw';
 const PANEL_HEIGHT = '50vh';
+
 const INITIAL_STYLES = {
   bottom: {opacity: 0, x: 0, y: 0},
   right: {opacity: 0, x: PANEL_WIDTH, y: 0},
 };
+
 const FINAL_STYLES = {
   bottom: {opacity: 0, x: 0, y: PANEL_HEIGHT},
   right: {opacity: 0, x: PANEL_WIDTH},
@@ -71,29 +76,56 @@ const _SlideOverPanel = styled(motion.div, {
   collapsed: boolean;
   slidePosition?: 'right' | 'bottom';
 }>`
-  ${p =>
-    p.slidePosition === 'bottom'
-      ? `
-      width: 100%;
-      height: ${PANEL_HEIGHT};
-      position: sticky;
-    `
-      : `
-      width: ${PANEL_WIDTH};
-      height: 100%;
-      position: fixed;
-      top: 0;
-    `}
-  box-shadow: ${p => p.theme.dropShadowHeavy};
-  bottom: 0;
+  position: fixed;
+
+  top: ${space(2)};
   right: 0;
+  bottom: ${space(2)};
+  left: ${space(2)};
+
+  overflow: auto;
+
+  z-index: ${p => p.theme.zIndex.modal + 1};
+
+  box-shadow: ${p => p.theme.dropShadowHeavy};
   background: ${p => p.theme.background};
   color: ${p => p.theme.textColor};
+
   text-align: left;
-  z-index: ${p => p.theme.zIndex.modal + 1};
+
+  @media (min-width: ${p => p.theme.breakpoints.small}) {
+    ${p =>
+      p.slidePosition === 'bottom'
+        ? css`
+            position: sticky;
+
+            width: 100%;
+            height: ${PANEL_HEIGHT};
+
+            right: 0;
+            bottom: 0;
+            left: 0;
+          `
+        : css`
+            position: fixed;
+
+            width: ${PANEL_WIDTH};
+            height: 100%;
+
+            top: 0;
+            right: 0;
+            bottom: 0;
+            left: auto;
+          `}
+  }
+
   ${p =>
     p.collapsed
-      ? 'overflow: hidden;'
-      : `overflow-x: hidden;
-  overflow-y: scroll;`}
+      ? css`
+          overflow: hidden;
+        `
+      : css`
+          overflow-x: hidden;
+          overflow-y: auto;
+        `}
 `;
