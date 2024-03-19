d16ac9a278725c4a67580353b76a0f0f881b49a1
William Mak <william@wmak.io>
2024-03-19 16:00:46 -0400

    feat(tracing): Add average to event details (#67264)
    - This adds a param to the org event details endpoint `averageColumn`
    that currently accepts 1 param `span.self_time`
    - When this param is passed we'll collect all the group ids for spans in
    the transaction and get their average span self time

--- src/sentry/api/endpoints/organization_event_details.py
+++ src/sentry/api/endpoints/organization_event_details.py
@@ -1,5 +1,9 @@
+from collections import defaultdict
+from datetime import datetime, timedelta
+
 from rest_framework.request import Request
 from rest_framework.response import Response
+from snuba_sdk import Column, Condition, Function, Op
 
 from sentry import eventstore
 from sentry.api.api_publish_status import ApiPublishStatus
@@ -10,6 +14,56 @@ from sentry.api.serializers.models.event import SqlFormatEventSerializer
 from sentry.api.utils import handle_query_errors
 from sentry.constants import ObjectStatus
 from sentry.models.project import Project
+from sentry.search.events.builder import SpansMetricsQueryBuilder
+from sentry.snuba.dataset import Dataset
+
+
+def add_comparison_to_event(event, average_column):
+    if "spans" not in event.data:
+        return
+    group_to_span_map = defaultdict(list)
+    end = datetime.now()
+    start = end - timedelta(hours=24)
+    for span in event.data["spans"]:
+        group = span.get("sentry_tags", {}).get("group")
+        if group is not None:
+            group_to_span_map[group].append(span)
+
+    # Nothing to add comparisons to
+    if len(group_to_span_map) == 0:
+        return
+
+    with handle_query_errors():
+        builder = SpansMetricsQueryBuilder(
+            dataset=Dataset.PerformanceMetrics,
+            params={
+                "start": start,
+                "end": end,
+                "project_objects": [event.project],
+                "organization_id": event.organization.id,
+            },
+            selected_columns=[
+                "group",
+                f"avg({average_column}) as avg",
+            ],
+            # orderby shouldn't matter, just picking something so results are consistent
+            orderby=["group"],
+        )
+        builder.add_conditions(
+            [
+                Condition(
+                    Column(builder.resolve_column_name("group")),
+                    Op.IN,
+                    Function("tuple", list(group_to_span_map.keys())),
+                )
+            ]
+        )
+        result = builder.run_query("Get avg for spans")
+        for result in result["data"]:
+            group = result["group"]
+            avg = result["avg"]
+            for span in group_to_span_map[group]:
+                span["span.average_time"] = avg
 
 
 @region_silo_endpoint
@@ -42,6 +96,10 @@ class OrganizationEventDetailsEndpoint(OrganizationEventsEndpointBase):
 
         if event is None:
             return Response({"detail": "Event not found"}, status=404)
+
+        average_column = request.GET.get("averageColumn")
+        if average_column in ["span.self_time"]:
+            add_comparison_to_event(event, average_column)
 
         # TODO: Remove `for_group` check once performance issues are moved to the issue platform
         if hasattr(event, "for_group") and event.group:
