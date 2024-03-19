a18fe5038ee4fac27dbe25eac9809500c87fd302
Scott Cooper <scttcper@gmail.com>
2024-03-19 13:51:55 -0700

    remove another render function

--- static/app/views/settings/components/teamSelect/utils.tsx
+++ static/app/views/settings/components/teamSelect/utils.tsx
@@ -5,7 +5,7 @@ import debounce from 'lodash/debounce';
 import {openCreateTeamModal} from 'sentry/actionCreators/modal';
 import {hasEveryAccess} from 'sentry/components/acl/access';
 import DropdownAutoComplete from 'sentry/components/dropdownAutoComplete';
-import type {Item} from 'sentry/components/dropdownAutoComplete/types';
+import type {Item, ItemsBeforeFilter} from 'sentry/components/dropdownAutoComplete/types';
 import DropdownButton from 'sentry/components/dropdownButton';
 import {TeamBadge} from 'sentry/components/idBadge/teamBadge';
 import Link from 'sentry/components/links/link';
@@ -118,25 +118,22 @@ function renderDropdownOption({
   isAddingTeamToMember: boolean;
   isAddingTeamToProject: boolean;
   team: Team;
-}) {
+}): ItemsBeforeFilter[number] {
   const isIdpProvisioned = isAddingTeamToMember && team.flags['idp:provisioned'];
   const buttonHelpText = getButtonHelpText(isIdpProvisioned);
+  const label = isIdpProvisioned ? (
+    <Tooltip title={buttonHelpText}>
+      <DropdownTeamBadgeDisabled avatarSize={18} team={team} />
+    </Tooltip>
+  ) : (
+    <DropdownTeamBadge avatarSize={18} team={team} />
+  );
 
   return {
     index,
     value: team.slug,
     searchKey: team.slug,
-    label: () => {
-      if (isIdpProvisioned) {
-        return (
-          <Tooltip title={buttonHelpText}>
-            <DropdownTeamBadgeDisabled avatarSize={18} team={team} />
-          </Tooltip>
-        );
-      }
-
-      return <DropdownTeamBadge avatarSize={18} team={team} />;
-    },
+    label,
     disabled: disabled || isIdpProvisioned,
   };
 }
