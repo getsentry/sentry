4856016992119a3b13c19d755abc4d47b7b4ec5e
Colleen O'Rourke <colleen@sentry.io>
2024-03-19 09:19:56 -0700

    ref(daily summary): Add discover link, add more issue detail (#67098)
    This PR addresses some minor changes to the daily summary. 
    * Restricts the number of escalated and regressed issues per project to
    3 each
    * Adds a Discover link to the error issue count (if the org has
    Discover)
    * Adds issue subtitle text, if available
    
    Take this screenshot with a grain of salt, I hardcoded the event count
    0000 to get this to show up and double check the link worked
    <img width="589" alt="Screenshot 2024-03-15 at 5 00 41 PM"
    src="https://github.com/getsentry/sentry/assets/29959063/c8a5629e-4ed8-4b16-82e7-7db1350a0315">
    
    
    Closes
    https://github.com/getsentry/team-core-product-foundations/issues/173,
    https://github.com/getsentry/team-core-product-foundations/issues/174,
    and
    https://github.com/getsentry/team-core-product-foundations/issues/172

--- src/sentry/integrations/slack/message_builder/notifications/daily_summary.py
+++ src/sentry/integrations/slack/message_builder/notifications/daily_summary.py
@@ -2,10 +2,16 @@ from __future__ import annotations
 
 from collections.abc import Mapping
 from typing import Any
+from urllib.parse import urlencode
 
 from sentry_relay.processing import parse_release
 
-from sentry.integrations.message_builder import build_attachment_title, get_title_link
+from sentry import features
+from sentry.integrations.message_builder import (
+    build_attachment_text,
+    build_attachment_title,
+    get_title_link,
+)
 from sentry.integrations.slack.message_builder import SlackBlock
 from sentry.integrations.slack.utils.escape import escape_slack_text
 from sentry.models.project import Project
@@ -15,6 +21,7 @@ from sentry.services.hybrid_cloud.actor import RpcActor
 from sentry.tasks.summaries.utils import COMPARISON_PERIOD
 from sentry.types.integrations import ExternalProviders
 from sentry.utils import json
+from sentry.utils.http import absolute_uri
 
 from .base import SlackNotificationsMessageBuilder
 
@@ -34,13 +41,38 @@ class SlackDailySummaryMessageBuilder(SlackNotificationsMessageBuilder):
     def linkify_error_title(self, group):
         link = get_title_link(group, None, False, False, self.notification, ExternalProviders.SLACK)
         title = build_attachment_title(group)
-        return f"<{link}|*{escape_slack_text(title)}*>"
+        attachment_text = self.get_attachment_text(group)
+        if not attachment_text:
+            return f"<{link}|*{escape_slack_text(title)}*>"
+        return f"<{link}|*{escape_slack_text(title)}*> {attachment_text}"
 
     def linkify_release(self, release, organization):
         path = f"/releases/{release.version}/"
         url = organization.absolute_url(path)
         release_description = parse_release(release.version).get("description")
         return f":rocket: *<{url}|Release {release_description}>*\n"
+
+    def get_attachment_text(self, group):
+        attachment_text = build_attachment_text(group)
+        if attachment_text and len(attachment_text) > 50:
+            attachment_text = attachment_text[0:49] + "..."
+        return attachment_text
+
+    def build_discover_url(self, project):
+        query_params = {
+            "field": ["title", "event.type", "project", "user.display", "timestamp"],
+            "name": "All Events",
+            "project": project.id,
+            "query": "event.type:error",
+            "sort": "-timestamp",
+            "statsPeriod": "24h",
+            "yAxis": "count()",
+        }
+        query_string = urlencode(query_params, doseq=True)
+        url = absolute_uri(
+            f"/organizations/{project.organization.slug}/discover/homepage/?{query_string}"
+        )
+        return url
 
     def build(self) -> SlackBlock:
         blocks = []
@@ -65,11 +97,17 @@ class SlackDailySummaryMessageBuilder(SlackNotificationsMessageBuilder):
             project_text = f"Here's what happened in the *{project.slug}* project today:"
             blocks.append(self.get_markdown_block(project_text))
 
+            fields = []
+            event_count_text = "*Today’s Event Count*: "
+            if features.has("organizations:discover", project.organization):
+                discover_url = self.build_discover_url(project)
+                event_count_text += f"<{discover_url}|{context.total_today}>"
+            else:
+                event_count_text += str(context.total_today)
+            fields.append(self.make_field(event_count_text))
+
             # Calculate today's event count percentage against 14 day avg
             if context.comparison_period_avg > 0:  # avoid a zerodivisionerror
-                fields = []
-                event_count_text = f"*Today’s Event Count*: {context.total_today}"
-                fields.append(self.make_field(event_count_text))
                 percentage_diff = context.total_today / context.comparison_period_avg
                 if context.total_today > context.comparison_period_avg:
                     percentage_diff_text = (
@@ -82,7 +120,7 @@ class SlackDailySummaryMessageBuilder(SlackNotificationsMessageBuilder):
                     )
                     fields.append(self.make_field(percentage_diff_text))
 
-                blocks.append(self.get_section_fields_block(fields))
+            blocks.append(self.get_section_fields_block(fields))
 
             # Add release info if we have it
             if context.new_in_release:
