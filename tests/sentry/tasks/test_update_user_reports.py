1437fb5c708974e23b8a1ebff3d32ab313d34d12
Bartek Ogryczak <bartek.ogryczak@sentry.io>
2024-03-19 17:34:39 -0700

    chore(issues): update_user_reports backfill  (#67263)
    Adding starte and end date to `update_user_reports` to allow for
    backfill
    I'll add a script using this functionality for backfilling in
    getsentry/getsentry.

--- tests/sentry/tasks/test_update_user_reports.py
+++ tests/sentry/tasks/test_update_user_reports.py
@@ -37,3 +37,44 @@ class UpdateUserReportTest(TestCase):
         assert report2.environment_id == event2.get_environment().id
         assert report3.group_id is None
         assert report3.environment_id is None
+
+    def test_start_end(self):
+        now = timezone.now()
+        project = self.create_project()
+        event1 = self.store_event(data={}, project_id=project.id)
+        UserReport.objects.create(
+            project_id=project.id, event_id=event1.event_id, date_added=now - timedelta(days=4)
+        )
+        event2 = self.store_event(data={}, project_id=project.id)
+        UserReport.objects.create(
+            project_id=project.id, event_id=event2.event_id, date_added=now - timedelta(days=3)
+        )
+        event3 = self.store_event(data={}, project_id=project.id)
+        UserReport.objects.create(
+            project_id=project.id, event_id=event3.event_id, date_added=now - timedelta(days=2)
+        )
+        event4 = self.store_event(data={}, project_id=project.id)
+        UserReport.objects.create(
+            project_id=project.id, event_id=event4.event_id, date_added=now - timedelta(days=1)
+        )
+        event5 = self.store_event(data={}, project_id=project.id)
+        UserReport.objects.create(project_id=project.id, event_id=event5.event_id, date_added=now)
+        with self.tasks():
+            update_user_reports(start=now - timedelta(days=3), end=now - timedelta(days=2))
+
+        report1 = UserReport.objects.get(project_id=project.id, event_id=event1.event_id)
+        report2 = UserReport.objects.get(project_id=project.id, event_id=event2.event_id)
+        report3 = UserReport.objects.get(project_id=project.id, event_id=event3.event_id)
+        report4 = UserReport.objects.get(project_id=project.id, event_id=event4.event_id)
+        report5 = UserReport.objects.get(project_id=project.id, event_id=event5.event_id)
+
+        assert report1.group_id is None
+        assert report1.environment_id is None
+        assert report2.group_id == event2.group_id
+        assert report2.environment_id == event2.get_environment().id
+        assert report3.group_id == event3.group_id
+        assert report3.environment_id == event3.get_environment().id
+        assert report4.group_id is None
+        assert report4.environment_id is None
+        assert report5.group_id is None
+        assert report5.environment_id is None
