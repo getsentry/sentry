8fee0e2fd7be53557bd79607a2b6715694e49c15
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-03-21 13:50:12 -0400

    Update related_issues.py

--- src/sentry/api/endpoints/issues/related_issues.py
+++ src/sentry/api/endpoints/issues/related_issues.py
@@ -8,8 +8,6 @@ from sentry.api.bases.group import GroupEndpoint
 from sentry.issues.related import find_related_issues
 from sentry.models.group import Group
 
-# from sentry import features
-
 
 @region_silo_endpoint
 class RelatedIssuesEndpoint(GroupEndpoint):
@@ -17,8 +15,5 @@ class RelatedIssuesEndpoint(GroupEndpoint):
     publish_status = {"GET": ApiPublishStatus.EXPERIMENTAL}
 
     def get(self, _: Request, group: Group) -> Response:
-        # XXX: Add real feature flag
-        # if not features.has("FOO", organization, actor=request.user):
-        #     return Response({"status": "disabled"}, status=403)
         related_issues = find_related_issues(group)
         return Response({key: [g.id for g in groups] for key, groups in related_issues.items()})
