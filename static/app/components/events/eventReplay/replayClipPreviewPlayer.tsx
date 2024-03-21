df35240dec2ddae9d1bc3b2f106f98dd67eb7ce8
Stephen Cefali <scefali@sentry.io>
2024-03-21 14:39:50 -0700

    add min height to replay player

--- static/app/components/events/eventReplay/replayClipPreviewPlayer.tsx
+++ static/app/components/events/eventReplay/replayClipPreviewPlayer.tsx
@@ -138,6 +138,11 @@ const PlayerContainer = styled(FluidHeight)<{isLarge?: boolean}>`
   position: relative;
   max-height: ${p =>
     p.isLarge ? REPLAY_LOADING_HEIGHT_LARGE : REPLAY_LOADING_HEIGHT + 16}px;
+  @media (min-width: ${p => p.theme.breakpoints.small}) {
+    display: flex;
+    min-height: ${p =>
+      p.isLarge ? REPLAY_LOADING_HEIGHT_LARGE : REPLAY_LOADING_HEIGHT + 16}px;
+  }
 `;
 
 const StyledNegativeSpaceContainer = styled(NegativeSpaceContainer)<{isLarge?: boolean}>`
