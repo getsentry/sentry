c04111562a147762c725f093f8f8d9d036a73035
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-03-22 13:20:30 -0700

    ref(replays): update feedback breadcrumb (#67382)
    - The new feedback breadcrumb is pink & has a megaphone icon in both the
    breadcrumbs & timeline.
    - The errors count in the top right doesn't include feedback anymore
    - Errors tab doesn't include feedback
    - Feedback breadcrumb links to the feedback page rather than issues
    
    ## New breadcrumb color & icon
    
    
    
    <img width="310" alt="SCR-20240320-myzy"
    src="https://github.com/getsentry/sentry/assets/56095982/248d24a9-d271-4395-b8df-177312b78173">
    
    <img width="542" alt="SCR-20240320-myze"
    src="https://github.com/getsentry/sentry/assets/56095982/2051acf1-e1fa-4eed-8df9-b8d4d875162b">
    
    ## No errors in errors tab or the error count in top right:
    
    
    
    https://github.com/getsentry/sentry/assets/56095982/46888899-19ae-4f01-b89e-3e1bf7b53708
    
    
    
    
    ## Links to the feedback page rather than issues:
    
    
    https://github.com/getsentry/sentry/assets/56095982/b767c99b-23d0-4097-a92a-934ca4f3af7d
    
    
    Closes https://github.com/getsentry/team-replay/issues/408

--- static/app/views/replays/detail/breadcrumbs/useBreadcrumbFilters.tsx
+++ static/app/views/replays/detail/breadcrumbs/useBreadcrumbFilters.tsx
@@ -69,6 +69,7 @@ const OPORCATEGORY_TO_TYPE: Record<string, keyof typeof TYPE_TO_LABEL> = {
   'ui.click': 'click',
   'ui.keyDown': 'keydown',
   'ui.input': 'input',
+  feedback: 'feedback',
 };
 
 function typeToLabel(val: string): string {
