697b84dced9cd39ba1b8fd23a37628942a67e746
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-03-25 13:27:19 +0100

    feat(ddm): Add pydocs to the API code (#67580)

--- src/sentry/sentry_metrics/querying/data_v2/preparation/base.py
+++ src/sentry/sentry_metrics/querying/data_v2/preparation/base.py
@@ -9,6 +9,18 @@ from sentry.sentry_metrics.querying.units import MeasurementUnit, UnitFamily
 
 @dataclass(frozen=True)
 class IntermediateQuery:
+    """
+    Represents an intermediate query that can be run through a series of immutable transformations.
+
+    Attributes:
+        metrics_query: The query that needs to be run.
+        order: The order of the groups that are returned.
+        limit: The maximum number of groups to return.
+        unit_family: The UnitFamily of the query.
+        unit: The unit of the query.
+        scaling_factor: The scaling factor that was applied on the query to normalize it to unit.
+    """
+
     metrics_query: MetricsQuery
     order: QueryOrder | None = None
     limit: int | None = None
@@ -18,14 +30,34 @@ class IntermediateQuery:
 
 
 class PreparationStep(ABC):
+    """
+    Represents an abstract step that prepares a collection of IntermediateQuery objects.
+
+    The preparation of these objects might include transforming them or just obtaining some intermediate data that is
+    useful to compute other things before executing the query.
+    """
+
     @abstractmethod
     def run(self, intermediate_queries: list[IntermediateQuery]) -> list[IntermediateQuery]:
+        """
+        Runs the preparation steps on a list of intermediate queries.
+
+        Returns:
+            A list of intermediate queries.
+        """
         raise NotImplementedError
 
 
 def run_preparation_steps(
     intermediate_queries: list[IntermediateQuery], *steps
 ) -> list[IntermediateQuery]:
+    """
+    Takes a series of intermediate queries and steps and runs those intermediate queries on the supplied steps in an
+    accumulating way.
+
+    Returns:
+        A list of intermediate queries after running the preparation steps.
+    """
     for step in steps:
         if isinstance(step, PreparationStep):
             intermediate_queries = step.run(intermediate_queries=intermediate_queries)
