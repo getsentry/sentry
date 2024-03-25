439ff64207cc64dffb82e9ac7ac8d53f5dd2713b
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-03-25 12:56:36 -0700

    remove UI changes from this branch, moved them to jcallender/release_use_activated_alerts_ui

--- static/app/views/releases/utils/useFetchThresholdsListData.tsx
+++ static/app/views/releases/utils/useFetchThresholdsListData.tsx
@@ -1,13 +1,5 @@
-import type {Environment} from 'sentry/types';
-import {
-  type ApiQueryKey,
-  setApiQueryData,
-  useApiQuery,
-  useQueryClient,
-} from 'sentry/utils/queryClient';
+import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
-import type {MetricRule} from 'sentry/views/alerts/rules/metric/types';
-import {TOTAL_ERROR_COUNT_STR} from 'sentry/views/releases/utils/constants';
 
 import type {Threshold, ThresholdQuery} from './types';
 
@@ -19,70 +11,33 @@ export type HookProps = {
 export default function useFetchThresholdsListData({
   selectedEnvs = [],
   selectedProjectIds = [],
-}: HookProps = {}) {
+}: HookProps) {
   const organization = useOrganization();
-  const queryClient = useQueryClient();
-  const query: ThresholdQuery = {};
-  const isActivatedAlert = organization.features?.includes('activated-alert-rules');
 
-  if (selectedEnvs.length) query.environment = selectedEnvs;
+  const query: ThresholdQuery = {};
   if (selectedProjectIds.length) {
     query.project = selectedProjectIds;
   } else {
     query.project = [-1];
   }
+  if (selectedEnvs.length) {
+    query.environment = selectedEnvs;
+  }
 
-  let onSuccess: ((data: unknown[]) => Threshold[]) | undefined = undefined;
-  let queryKey: ApiQueryKey = [
-    `/organizations/${organization.slug}/release-thresholds/`,
-    {
-      query,
-    },
-  ];
-
-  if (isActivatedAlert) {
-    query.monitor_type = 1;
-    queryKey = [
-      `/organizations/${organization.slug}/alert-rules/`,
+  return useApiQuery<Threshold[]>(
+    [
+      `/organizations/${organization.slug}/release-thresholds/`,
       {
         query,
       },
-    ];
-
-    onSuccess = (data: unknown[]) => {
-      const alerts = data[0] as MetricRule[];
-
-      const thresholdData = alerts.map(
-        (rule: MetricRule) =>
-          ({
-            date_added: rule.dateCreated,
-            environment: {
-              name: rule.environment,
-              displayName: rule.environment,
-            } as Environment,
-            id: rule.id,
-            project: {id: rule.projects[0], slug: rule.projects[0]},
-            threshold_type: TOTAL_ERROR_COUNT_STR,
-            trigger_type: 'over',
-            value: rule.triggers[0]?.alertThreshold,
-            window_in_seconds: rule.timeWindow,
-          }) as Threshold
-      );
-
-      // override the result with the formatted alert rule data
-      setApiQueryData(queryClient, queryKey, thresholdData);
-      return thresholdData;
-    };
-  }
-
-  const hasReleaseV2Feature =
-    organization.features?.includes('releases-v2-internal') ||
-    organization.features?.includes('releases-v2') ||
-    organization.features?.includes('releases-v2-st');
-
-  return useApiQuery<Threshold[]>(queryKey, {
-    onSuccess,
-    staleTime: 0,
-    enabled: hasReleaseV2Feature || isActivatedAlert,
-  });
+    ],
+    {
+      staleTime: 0,
+      enabled:
+        (organization.features?.includes('releases-v2-internal') ||
+          organization.features?.includes('releases-v2') ||
+          organization.features?.includes('releases-v2-st')) ??
+        false,
+    }
+  );
 }
