572decf310af9f279384f2e74068d877fd013d22
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-03-25 12:02:05 -0400

    feat(perf): Add project avatars to HTTP module (#67560)
    Hostnames are ambiguous, it can be important which project they belong
    to. Right now this is lost! Show project avatar next to domains where
    appropriate.
    
    - Use `useLocationQuery`
    - Show project next to domain in table
    - Show project next to hostname in summary

--- static/app/views/performance/http/domainCell.tsx
+++ static/app/views/performance/http/domainCell.tsx
@@ -1,23 +1,31 @@
 import {Link} from 'react-router';
+import styled from '@emotion/styled';
 import * as qs from 'query-string';
 
+import ProjectAvatar from 'sentry/components/avatar/projectAvatar';
+import {space} from 'sentry/styles/space';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
+import useProjects from 'sentry/utils/useProjects';
 import {normalizeUrl} from 'sentry/utils/withDomainRequired';
 import {OverflowEllipsisTextContainer} from 'sentry/views/starfish/components/textAlign';
 
 interface Props {
   domain?: string;
+  projectId?: string;
 }
 
-export function DomainCell({domain}: Props) {
+export function DomainCell({projectId, domain}: Props) {
   const location = useLocation();
   const organization = useOrganization();
+  const {projects} = useProjects();
 
   // NOTE: This is for safety only, the product should not fetch or render rows with missing domains or project IDs
   if (!domain) {
     return NULL_DESCRIPTION;
   }
+
+  const project = projects.find(p => projectId === p.id);
 
   const queryString = {
     ...location.query,
@@ -25,16 +33,27 @@ export function DomainCell({domain}: Props) {
   };
 
   return (
-    <OverflowEllipsisTextContainer>
-      <Link
-        to={normalizeUrl(
-          `/organizations/${organization.slug}/performance/http/domains/?${qs.stringify(queryString)}`
-        )}
-      >
-        {domain}
-      </Link>
-    </OverflowEllipsisTextContainer>
+    <DomainDescription>
+      {project && <ProjectAvatar project={project} />}
+
+      <OverflowEllipsisTextContainer>
+        <Link
+          to={normalizeUrl(
+            `/organizations/${organization.slug}/performance/http/domains/?${qs.stringify(queryString)}`
+          )}
+        >
+          {domain}
+        </Link>
+      </OverflowEllipsisTextContainer>
+    </DomainDescription>
   );
 }
+
+const DomainDescription = styled('div')`
+  display: flex;
+  flex-wrap: nowrap;
+  gap: ${space(1)};
+  align-items: center;
+`;
 
 const NULL_DESCRIPTION = <span>&lt;null&gt;</span>;
