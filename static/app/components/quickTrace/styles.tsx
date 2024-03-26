c0f0509452a78aed193c77039624d7cb1f4b562d
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-03-26 16:56:54 -0400

    ref(js): Remove a couple more default exports of Tag

--- static/app/components/quickTrace/styles.tsx
+++ static/app/components/quickTrace/styles.tsx
@@ -5,7 +5,7 @@ import type {LocationDescriptor} from 'history';
 import MenuHeader from 'sentry/components/actions/menuHeader';
 import ExternalLink from 'sentry/components/links/externalLink';
 import MenuItem from 'sentry/components/menuItem';
-import Tag, {Background} from 'sentry/components/tag';
+import {Background,Tag} from 'sentry/components/tag';
 import Truncate from 'sentry/components/truncate';
 import {space} from 'sentry/styles/space';
 import {getDuration} from 'sentry/utils/formatters';
