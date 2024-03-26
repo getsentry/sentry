bb87d82405dab3bd503e67fc5809e2d8c0f6ebd6
Nipunn Koorapati <nipunn1313@gmail.com>
2024-03-25 17:16:13 -0700

    Add a test

---
+++ static/app/gettingStartedDocs/convex/convex.spec.tsx
@@ -0,0 +1,14 @@
+import {renderWithOnboardingLayout} from 'sentry-test/onboarding/renderWithOnboardingLayout';
+import {screen} from 'sentry-test/reactTestingLibrary';
+
+import docs from './convex';
+
+describe('convex onboarding docs', function () {
+  it('renders docs correctly', function () {
+    renderWithOnboardingLayout(docs);
+
+    // Renders main headings
+    expect(screen.getByRole('heading', {name: 'Install'})).toBeInTheDocument();
+    expect(screen.getByRole('heading', {name: 'Verify'})).toBeInTheDocument();
+  });
+});
