cb305c105291e2dc79ce26e5ba06039999caa8a4
schew2381 <seiji.chew@sentry.io>
2024-03-26 15:08:03 -0700

    put ticket behind option

--- src/sentry/integrations/github_enterprise/__init__.py
+++ src/sentry/integrations/github_enterprise/__init__.py
@@ -1,3 +1,4 @@
+from sentry import options
 from sentry.rules import rules
 
 from .actions.create_ticket import *  # noqa: F401,F403
@@ -8,4 +9,5 @@ from .repository import *  # noqa: F401,F403
 from .urls import *  # noqa: F401,F403
 from .webhook import *  # noqa: F401,F403
 
-rules.add(GitHubEnterpriseCreateTicketAction)
+if options.get("github-enterprise-app.alert-rule-action"):
+    rules.add(GitHubEnterpriseCreateTicketAction)
