6a8f958329c4593ffcd2e31ed6c2ad7251aa2d0c
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2024-03-27 09:25:13 -0700

    ref(code-mappings): Unify code-mapping logic (#67665)
    The goal of this PR is to unify all logic to derive the stack trace root
    and source code root, since this logic has been materially different
    across automatic and manual code-mapping derivations.
    
    This PR makes `find_roots` the single source of truth for deriving these
    roots across the manual and automatic derivations. As an added benefit,
    `find_roots` is more refactorable and resilient to future platforms that
    made be added, and it reduces the footprint and complexity of
    code-mappings in general.

--- tests/sentry/api/endpoints/test_project_repo_path_parsing.py
+++ tests/sentry/api/endpoints/test_project_repo_path_parsing.py
@@ -196,8 +196,8 @@ class ProjectStacktraceLinkGithubTest(BaseStacktraceLinkTest):
             "integrationId": self.integration.id,
             "repositoryId": self.repo.id,
             "provider": "github",
-            "stackRoot": "",
-            "sourceRoot": "src/",
+            "stackRoot": "sentry/",
+            "sourceRoot": "src/sentry/",
             "defaultBranch": "master",
         }
 
@@ -224,8 +224,8 @@ class ProjectStacktraceLinkGithubTest(BaseStacktraceLinkTest):
             "integrationId": self.integration.id,
             "repositoryId": self.repo.id,
             "provider": "github",
-            "stackRoot": "stuff/hey/here/",
-            "sourceRoot": "src/",
+            "stackRoot": "stuff/hey/here/sentry/",
+            "sourceRoot": "src/sentry/",
             "defaultBranch": "master",
         }
 
@@ -260,8 +260,8 @@ class ProjectStacktraceLinkGithubTest(BaseStacktraceLinkTest):
             "integrationId": self.integration.id,
             "repositoryId": self.repo.id,
             "provider": "github",
-            "stackRoot": "C:\\potatos\\and\\prs\\",
-            "sourceRoot": "src/",
+            "stackRoot": "C:\\potatos\\and\\prs\\sentry\\",
+            "sourceRoot": "src/sentry/",
             "defaultBranch": "main",
         }
 
@@ -298,8 +298,8 @@ class ProjectStacktraceLinkGitlabTest(BaseStacktraceLinkTest):
             "integrationId": self.integration.id,
             "repositoryId": self.repo.id,
             "provider": "gitlab",
-            "stackRoot": "",
-            "sourceRoot": "src/",
+            "stackRoot": "sentry/",
+            "sourceRoot": "src/sentry/",
             "defaultBranch": "master",
         }
 
