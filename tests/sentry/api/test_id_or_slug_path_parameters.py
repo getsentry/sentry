7d78cd53336372a9237331da01fd34fa4c7d1b0f
Raj Joshi <raj.joshi@sentry.io>
2024-03-27 15:07:57 -0700

    more comments & refactoring

--- tests/sentry/api/test_id_or_slug_path_parameters.py
+++ tests/sentry/api/test_id_or_slug_path_parameters.py
@@ -15,13 +15,68 @@ from sentry.testutils.helpers.options import override_options
 from sentry.testutils.silo import no_silo_test
 
 
+def extract_slug_path_params(path: str) -> list[str]:
+    return re.findall(r"<(\w+_slug)>", path)
+
+
+def extract_other_path_params(path: str) -> list[str]:
+    return re.findall(r"<(\w+?)(?<!_slug)>", path)
+
+
+def extract_all_url_patterns(
+    urlpatterns, base: str = ""
+) -> Generator[Any | tuple[str, Any], Any, None]:
+    for pattern in urlpatterns:
+        if isinstance(pattern, URLResolver):
+            yield from extract_all_url_patterns(pattern.url_patterns, base + str(pattern.pattern))
+        elif isinstance(pattern, URLPattern):
+            url_pattern = base + str(pattern.pattern).replace("^", "").replace(
+                "$", ""
+            )  # Clean up pattern string
+            callback = pattern.callback
+            if hasattr(callback, "view_class"):
+                callback = callback.view_class
+                yield (url_pattern, callback)
+
+
 @no_silo_test
 class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
+    """
+    This test class is used to test if all endpoints work with both slugs and ids as path parameters.
+    Ex: /api/0/organizations/{organization_slug}/integrations/{doc_integration_slug}/ &
+        /api/0/organizations/{organization_id}/integrations/{doc_integration_id}/
+
+    The test works by recursively searching through all Django URL patterns and checking if the endpoint has a `convert_args` method.
+    If the endpoint has a `convert_args` method, the test will call the method with both slugs and ids as path parameters and compare the results.
+
+    If the endpoint that is created uses an existing `convert_args` method thats in the test, DO NOTHING.
+
+    To add a new endpoint that creates a new `convert_args`  to the test, complete the following steps:
+        1. If the endpoint creates a new `convert_args` method, you will need to creatre a new entry in the `convert_args_setup_registry` dictionary.
+            1a. The key should be the new `convert_args` method.
+            1b. The value should be the test method that will be called with the endpoint. You can either reuse an existing test method or create a new one.
+        2. If you are introducing a new slug parameter, add the slug to the `slug_mappings` dictionary & `reverse_slug_mappings` dictionary.
+        3. Some of our endpoints don't properly handle all slugs in kwargs, and pass them to the base endpoint without converting them. If the endpoint is one of these, add the endpoint's `convert_args` method to the `no_slugs_in_kwargs_allowlist` list.
+        4. Each test method should have the following signature: `def test_method(self, endpoint_class, slug_params, other_params, *args):`
+            4a. `endpoint_class` is the endpoint class that is being tested.
+            4b. `slug_params` is a list of path parameters that end with `_slug`.
+            4c. `other_params` is a list of path parameters that do not end with `_slug`.
+            4d. `*args` is a list of additional arguments that can be passed to the test method, including mock objects.
+        5. If the endpoint requires additional parameters to be passed to the `convert_args` method, add the parameters to the `other_mappings` & `reverse_other_mappings` dictionaries.
+        6. For assertations, use the `assert_conversion` method to compare the results of the `convert_args` method. This is for endpoints that use RPC objects instead of the actual objects or generally don't return the same object.
+            6a. Pass reverse_other_mappings if the endpoint.convert_args has additional parameters that need to be compared.
+            6b. Pass use_id=True if you want to compare the ids of the objects instead of the objects themselves.
+
+
+    Take a look at `sentry_app_test` for an example of how to add a new endpoint to the test.
+    """
+
     databases: set[str] | str = "__all__"
 
     def setUp(self):
         super().setUp()
 
+        # Step 1: Add new endpoints to the registry
         # If a new `convert_args` method is created for an endpoint, add the endpoint to the registry and create/assign a test method
         self.convert_args_setup_registry: dict[Any, Callable] = {
             DocIntegrationBaseEndpoint.convert_args: self.doc_integration_test,
@@ -33,6 +88,7 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
         self.doc_integration = self.create_doc_integration()
         self.sentry_app = self.create_sentry_app(organization=self.organization)
 
+        # Step 2: Add new slugs to the mappings
         # Add slug mappings for the test methods
         self.slug_mappings = {
             "doc_integration_slug": self.doc_integration,
@@ -45,61 +101,12 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
             "sentry_app": self.sentry_app,
         }
 
+        # Step 3: Add endpoints to the allowlist
         # Some endpoints don't properly handle all slugs in kwargs, and pass them to the base class without converting them
         # Add the endpoint's `convert_args` method to this list to skip the check for slugs in kwargs
         self.no_slugs_in_kwargs_allowlist = {}
 
-    def extract_slug_path_params(self, path: str) -> list[str]:
-        """
-        Extracts path parameters from a path string which end with _slug.
-        """
-        return re.findall(r"<(\w+_slug)>", path)
-
-    def extract_other_path_params(self, path: str) -> list[str]:
-        """
-        Extracts path parameters from a path string which do not end with _slug.
-        """
-        return re.findall(r"<(\w+?)(?<!_slug)>", path)
-
-    def extract_all_url_patterns(
-        self, urlpatterns, base: str = ""
-    ) -> Generator[Any | tuple[str, Any], Any, None]:
-        """
-        Recursively search through Django URL patterns and yield all URL patterns.
-
-        Args:
-        - urlpatterns: The list of urlpatterns to search through.
-        - base: The base URL path for recursive accumulation.
-
-        Yields:
-        - All URL patterns as strings, including their full recursive path.
-        """
-        for pattern in urlpatterns:
-            if isinstance(pattern, URLResolver):
-                # For URLResolver objects, recursively yield from its url_patterns
-                yield from self.extract_all_url_patterns(
-                    pattern.url_patterns, base + str(pattern.pattern)
-                )
-            elif isinstance(pattern, URLPattern):
-                # For URLPattern objects, yield the combined base + pattern string and its callback
-                url_pattern = base + str(pattern.pattern).replace("^", "").replace(
-                    "$", ""
-                )  # Clean up pattern string
-                callback = pattern.callback
-                if hasattr(callback, "view_class"):
-                    callback = callback.view_class
-                    yield (url_pattern, callback)
-
-    def assert_objects(
-        self, endpoint_class, converted_slugs, converted_ids, reverse_other_mappings=None
-    ) -> None:
-        check_no_slugs_in_kwargs = (
-            endpoint_class.convert_args not in self.no_slugs_in_kwargs_allowlist
-        )
-        if check_no_slugs_in_kwargs:
-            assert not any(str.endswith(param, "_slug") for param in converted_ids)
-
-        assert converted_slugs == converted_ids
+    def assert_objects(self, converted_slugs, converted_ids, reverse_other_mappings=None) -> None:
         if reverse_other_mappings:
             assert all(
                 converted_slugs[key] == self.reverse_slug_mappings.get(key)
@@ -120,16 +127,7 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
                 converted_ids[key] == self.reverse_slug_mappings.get(key) for key in converted_ids
             )
 
-    def assert_ids(
-        self, endpoint_class, converted_slugs, converted_ids, reverse_other_mappings=None
-    ) -> None:
-        check_no_slugs_in_kwargs = (
-            endpoint_class.convert_args not in self.no_slugs_in_kwargs_allowlist
-        )
-        if check_no_slugs_in_kwargs:
-            assert not any(str.endswith(param, "_slug") for param in converted_ids)
-
-        assert converted_slugs == converted_ids
+    def assert_ids(self, converted_slugs, converted_ids, reverse_other_mappings=None) -> None:
         if reverse_other_mappings:
             for key, value in converted_slugs.items():
                 correct_mapping = self.reverse_slug_mappings.get(key, reverse_other_mappings[key])
@@ -148,6 +146,27 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
                 converted_ids[key].id == self.reverse_slug_mappings[key].id for key in converted_ids
             )
 
+    def assert_conversion(
+        self,
+        endpoint_class,
+        converted_slugs,
+        converted_ids,
+        reverse_other_mappings=None,
+        use_id=False,
+    ) -> None:
+        check_no_slugs_in_kwargs = (
+            endpoint_class.convert_args not in self.no_slugs_in_kwargs_allowlist
+        )
+        if check_no_slugs_in_kwargs:
+            assert not any(str.endswith(param, "_slug") for param in converted_ids)
+
+        assert converted_slugs == converted_ids
+
+        if use_id:
+            self.assert_ids(converted_slugs, converted_ids, reverse_other_mappings)
+        else:
+            self.assert_objects(converted_slugs, converted_ids, reverse_other_mappings)
+
     @patch("sentry.api.bases.doc_integrations.DocIntegrationBaseEndpoint.check_object_permissions")
     @override_options({"api.id-or-slug-enabled": True})
     def doc_integration_test(self, endpoint_class, slug_params, other_params, *args):
@@ -158,7 +177,7 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
         _, converted_slugs = endpoint_class().convert_args(request=None, **slug_kwargs)
         _, converted_ids = endpoint_class().convert_args(request=None, **id_kwargs)
 
-        self.assert_objects(endpoint_class, converted_slugs, converted_ids)
+        self.assert_conversion(endpoint_class, converted_slugs, converted_ids)
 
     @patch("sentry.api.bases.sentryapps.SentryAppBaseEndpoint.check_object_permissions")
     @patch("sentry.api.bases.sentryapps.RegionSentryAppBaseEndpoint.check_object_permissions")
@@ -170,6 +189,7 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
 
         api_token = self.create_user_auth_token(user=self.user)
 
+        # Step 5: Add extra params to the mappings
         # mapping extra params required for the endpoint
         other_mappings: dict[str, Any] = {
             "api_token_id": api_token.id,
@@ -188,7 +208,10 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
         _, converted_slugs = endpoint_class().convert_args(request=None, **slug_kwargs)
         _, converted_ids = endpoint_class().convert_args(request=None, **id_kwargs)
 
-        self.assert_objects(endpoint_class, converted_slugs, converted_ids, reverse_other_mappings)
+        # Step 6: Assert the conversion
+        self.assert_conversion(
+            endpoint_class, converted_slugs, converted_ids, reverse_other_mappings, use_id=False
+        )
 
     @patch("sentry.api.bases.sentryapps.RegionSentryAppBaseEndpoint.check_object_permissions")
     @override_options({"api.id-or-slug-enabled": True})
@@ -203,19 +226,19 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
 
         # Need to check if a RPC Sentry App is created instead of a Sentry App
         # Because we can't check for equality between the two, we check the ids
-        self.assert_ids(endpoint_class, converted_slugs, converted_ids)
+        self.assert_conversion(endpoint_class, converted_slugs, converted_ids, use_id=True)
 
     def test_if_endpoints_work_with_id_or_slug(self):
         root_resolver = get_resolver()
-        all_url_patterns = self.extract_all_url_patterns(root_resolver.url_patterns)
+        all_url_patterns = extract_all_url_patterns(root_resolver.url_patterns)
 
         for pattern, callback in all_url_patterns:
-            slug_path_params = self.extract_slug_path_params(pattern)
+            slug_path_params = extract_slug_path_params(pattern)
             if slug_path_params:
                 if slug_path_params == ["doc_integration_slug"] or slug_path_params == [
                     "sentry_app_slug"
                 ]:
-                    other_params = self.extract_other_path_params(pattern)
+                    other_params = extract_other_path_params(pattern)
                     self.convert_args_setup_registry[callback.convert_args](
                         callback, slug_path_params, other_params
                     )
