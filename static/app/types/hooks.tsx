a97b1ecb4facb20ae8c20592f397f1cd7c07fc8c
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2024-03-27 14:27:00 -0700

    feat(data-consent): Add in hooks for feature specific entrypoint (#67790)
    this pr adds in a hook for the feature specific entrypoint for data
    consent that will be shown in the priority dropdown.
    
    closes https://github.com/getsentry/getsentry/issues/13266

--- static/app/types/hooks.tsx
+++ static/app/types/hooks.tsx
@@ -185,6 +185,7 @@ export type ComponentHooks = {
   'component:crons-list-page-header': () => React.ComponentType<CronsBillingBannerProps>;
   'component:dashboards-header': () => React.ComponentType<DashboardHeadersProps>;
   'component:data-consent-banner': () => React.ComponentType<{source: string}> | null;
+  'component:data-consent-priority-learn-more': () => React.ComponentType<{}> | null;
   'component:ddm-metrics-samples-list': () => React.ComponentType<DDMMetricsSamplesListProps>;
   'component:disabled-app-store-connect-multiple': () => React.ComponentType<DisabledAppStoreConnectMultiple>;
   'component:disabled-custom-symbol-sources': () => React.ComponentType<DisabledCustomSymbolSources>;
