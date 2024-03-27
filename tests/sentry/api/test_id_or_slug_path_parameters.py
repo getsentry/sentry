3b1cda6105ef0a40e2a909693115b41fcb11278f
Raj Joshi <raj.joshi@sentry.io>
2024-03-27 12:02:59 -0700

    refactor

--- tests/sentry/api/test_id_or_slug_path_parameters.py
+++ tests/sentry/api/test_id_or_slug_path_parameters.py
@@ -19,20 +19,29 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
 
     def setUp(self):
         super().setUp()
-        self.doc_integration = self.create_doc_integration()
 
+        # If a new `convert_args` method is created for an endpoint, add the endpoint to the registry and create/assign a test method
         self.convert_args_setup_registry: dict[Any, Callable] = {
             DocIntegrationBaseEndpoint.convert_args: self.doc_integration_test,
         }
 
+        self.doc_integration = self.create_doc_integration()
+        self.sentry_app = self.create_sentry_app(organization=self.organization)
+
+        # Add slug mappings for the test methods
         self.slug_mappings = {
             "doc_integration_slug": self.doc_integration,
+            "sentry_app_slug": self.sentry_app,
         }
 
+        # Map values in kwargs to the actual objects
         self.reverse_slug_mappings = {
             "doc_integration": self.doc_integration,
+            "sentry_app": self.sentry_app,
         }
 
+        # Some endpoints don't properly handle all slugs in kwargs, and pass them to the base class without converting them
+        # Add the endpoint's `convert_args` method to this list to skip the check for slugs in kwargs
         self.no_slugs_in_kwargs_allowlist = {}
 
     def extract_slug_path_params(self, path: str) -> list[str]:
@@ -76,45 +85,88 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
                     callback = callback.view_class
                     yield (url_pattern, callback)
 
-    @patch("sentry.api.bases.doc_integrations.DocIntegrationBaseEndpoint.check_object_permissions")
-    @override_options({"api.id-or-slug-enabled": True})
-    def doc_integration_test(
-        self, endpoint_class, slug_params, other_params, check_no_slugs_in_kwargs, *args
-    ):
-
-        slug_kwargs = {param: self.slug_mappings[param].slug for param in slug_params}
-        id_kwargs = {param: self.slug_mappings[param].id for param in slug_params}
-
-        other_mappings: dict[str, Any] = {}
-
-        if other_params:
-            slug_kwargs.update({param: other_mappings[param] for param in other_params})
-            id_kwargs.update({param: other_mappings[param] for param in other_params})
-
-        _, converted_slugs = endpoint_class().convert_args(request=None, **slug_kwargs)
-        _, converted_ids = endpoint_class().convert_args(request=None, **id_kwargs)
-
+    def assert_objects(
+        self, endpoint_class, converted_slugs, converted_ids, reverse_other_mappings=None
+    ) -> None:
+        check_no_slugs_in_kwargs = (
+            endpoint_class.convert_args not in self.no_slugs_in_kwargs_allowlist
+        )
         if check_no_slugs_in_kwargs:
             assert not any(str.endswith(param, "_slug") for param in converted_ids)
 
         assert converted_slugs == converted_ids
-        assert all(
-            converted_slugs[key] == self.reverse_slug_mappings[key] for key in converted_slugs
+        if reverse_other_mappings:
+            assert all(
+                converted_slugs[key] == self.reverse_slug_mappings.get(key)
+                or converted_slugs[key] == reverse_other_mappings[key]
+                for key in converted_slugs
+            )
+            assert all(
+                converted_ids[key] == self.reverse_slug_mappings.get(key)
+                or converted_slugs[key] == reverse_other_mappings[key]
+                for key in converted_ids
+            )
+        else:
+            assert all(
+                converted_slugs[key] == self.reverse_slug_mappings.get(key)
+                for key in converted_slugs
+            )
+            assert all(
+                converted_ids[key] == self.reverse_slug_mappings.get(key) for key in converted_ids
+            )
+
+    def assert_ids(
+        self, endpoint_class, converted_slugs, converted_ids, reverse_other_mappings=None
+    ) -> None:
+        check_no_slugs_in_kwargs = (
+            endpoint_class.convert_args not in self.no_slugs_in_kwargs_allowlist
         )
-        assert all(converted_ids[key] == self.reverse_slug_mappings[key] for key in converted_ids)
+        if check_no_slugs_in_kwargs:
+            assert not any(str.endswith(param, "_slug") for param in converted_ids)
+
+        assert converted_slugs == converted_ids
+        if reverse_other_mappings:
+            assert all(
+                converted_slugs[key].id == self.reverse_slug_mappings.get(key).id
+                or converted_slugs[key].id == reverse_other_mappings[key].id
+                for key in converted_slugs
+            )
+            assert all(
+                converted_ids[key].id == self.reverse_slug_mappings.get(key).id
+                or converted_slugs[key].id == reverse_other_mappings[key].id
+                for key in converted_ids
+            )
+        else:
+            assert all(
+                converted_slugs[key].id == self.reverse_slug_mappings.get(key).id
+                for key in converted_slugs
+            )
+            assert all(
+                converted_ids[key].id == self.reverse_slug_mappings.get(key).id
+                for key in converted_ids
+            )
+
+    @patch("sentry.api.bases.doc_integrations.DocIntegrationBaseEndpoint.check_object_permissions")
+    @override_options({"api.id-or-slug-enabled": True})
+    def doc_integration_test(self, endpoint_class, slug_params, other_params, *args):
+
+        slug_kwargs = {param: self.slug_mappings[param].slug for param in slug_params}
+        id_kwargs = {param: self.slug_mappings[param].id for param in slug_params}
+
+        _, converted_slugs = endpoint_class().convert_args(request=None, **slug_kwargs)
+        _, converted_ids = endpoint_class().convert_args(request=None, **id_kwargs)
+
+        self.assert_objects(endpoint_class, converted_slugs, converted_ids)
 
     def test_if_endpoints_work_with_id_or_slug(self):
         root_resolver = get_resolver()
-        all_urlpatterns = self.extract_all_url_patterns(root_resolver.url_patterns)
+        all_url_patterns = self.extract_all_url_patterns(root_resolver.url_patterns)
 
-        for pattern, callback in all_urlpatterns:
-            path_params = self.extract_slug_path_params(pattern)
-            if path_params:
-                other_params = self.extract_other_path_params(pattern)
-                if path_params == ["doc_integration_slug"]:
-                    check_no_slugs_in_kwargs = (
-                        callback.convert_args not in self.no_slugs_in_kwargs_allowlist
-                    )
+        for pattern, callback in all_url_patterns:
+            slug_path_params = self.extract_slug_path_params(pattern)
+            if slug_path_params:
+                if slug_path_params == ["doc_integration_slug"]:
+                    other_params = self.extract_other_path_params(pattern)
                     self.convert_args_setup_registry[callback.convert_args](
-                        callback, path_params, other_params, check_no_slugs_in_kwargs
+                        callback, slug_path_params, other_params
                     )
