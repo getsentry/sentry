37a6cc36b7519df3e92080144616e071a8883721
Raj Joshi <raj.joshi@sentry.io>
2024-03-27 13:01:20 -0700

    sentry_app_slug stuff

--- tests/sentry/api/test_id_or_slug_path_parameters.py
+++ tests/sentry/api/test_id_or_slug_path_parameters.py
@@ -8,6 +8,8 @@ from django.urls import URLPattern, URLResolver
 from django.urls.resolvers import get_resolver
 
 from sentry.api.bases.doc_integrations import DocIntegrationBaseEndpoint
+from sentry.api.bases.sentryapps import RegionSentryAppBaseEndpoint, SentryAppBaseEndpoint
+from sentry.api.endpoints.integrations.sentry_apps import SentryInternalAppTokenDetailsEndpoint
 from sentry.testutils.cases import BaseTestCase
 from sentry.testutils.helpers.options import override_options
 from sentry.testutils.silo import no_silo_test
@@ -23,6 +25,9 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
         # If a new `convert_args` method is created for an endpoint, add the endpoint to the registry and create/assign a test method
         self.convert_args_setup_registry: dict[Any, Callable] = {
             DocIntegrationBaseEndpoint.convert_args: self.doc_integration_test,
+            SentryAppBaseEndpoint.convert_args: self.sentry_app_test,
+            SentryInternalAppTokenDetailsEndpoint.convert_args: self.sentry_app_test,
+            RegionSentryAppBaseEndpoint.convert_args: self.region_sentry_app_test,
         }
 
         self.doc_integration = self.create_doc_integration()
@@ -158,6 +163,51 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
 
         self.assert_objects(endpoint_class, converted_slugs, converted_ids)
 
+    @patch("sentry.api.bases.sentryapps.SentryAppBaseEndpoint.check_object_permissions")
+    @patch("sentry.api.bases.sentryapps.RegionSentryAppBaseEndpoint.check_object_permissions")
+    @override_options({"api.id-or-slug-enabled": True})
+    def sentry_app_test(self, endpoint_class, slug_params, other_params, *args):
+
+        slug_kwargs = {param: self.slug_mappings[param].slug for param in slug_params}
+        id_kwargs = {param: self.slug_mappings[param].id for param in slug_params}
+
+        api_token = self.create_user_auth_token(user=self.user)
+
+        # mapping extra params required for the endpoint
+        other_mappings: dict[str, Any] = {
+            "api_token_id": api_token.id,
+        }
+
+        # mapping kwargs to the actual objects
+        reverse_other_mappings: dict[str, Any] = {
+            "api_token": api_token,
+        }
+
+        if other_params:
+            slug_kwargs.update({param: other_mappings[param] for param in other_params})
+            id_kwargs.update({param: other_mappings[param] for param in other_params})
+
+        # convert both slugs and ids
+        _, converted_slugs = endpoint_class().convert_args(request=None, **slug_kwargs)
+        _, converted_ids = endpoint_class().convert_args(request=None, **id_kwargs)
+
+        self.assert_objects(endpoint_class, converted_slugs, converted_ids, reverse_other_mappings)
+
+    @patch("sentry.api.bases.sentryapps.RegionSentryAppBaseEndpoint.check_object_permissions")
+    @override_options({"api.id-or-slug-enabled": True})
+    def region_sentry_app_test(self, endpoint_class, slug_params, other_params, *args):
+        slug_kwargs = {param: self.slug_mappings[param].slug for param in slug_params}
+        id_kwargs = {param: self.slug_mappings[param].id for param in slug_params}
+
+        _, converted_slugs = endpoint_class().convert_args(request=None, **slug_kwargs)
+        _, converted_ids = endpoint_class().convert_args(request=None, **id_kwargs)
+
+        assert converted_slugs == converted_ids
+
+        # Need to check if a RPC Sentry App is created instead of a Sentry App
+        # Because we can't check for equality between the two, we check the ids
+        self.assert_ids(endpoint_class, converted_slugs, converted_ids)
+
     def test_if_endpoints_work_with_id_or_slug(self):
         root_resolver = get_resolver()
         all_url_patterns = self.extract_all_url_patterns(root_resolver.url_patterns)
@@ -165,7 +215,9 @@ class APIIdOrSlugPathParamTest(BaseTestCase, TestCase):
         for pattern, callback in all_url_patterns:
             slug_path_params = self.extract_slug_path_params(pattern)
             if slug_path_params:
-                if slug_path_params == ["doc_integration_slug"]:
+                if slug_path_params == ["doc_integration_slug"] or slug_path_params == [
+                    "sentry_app_slug"
+                ]:
                     other_params = self.extract_other_path_params(pattern)
                     self.convert_args_setup_registry[callback.convert_args](
                         callback, slug_path_params, other_params
