d7ce9299046ce0f6a8c7abff5abb2cd604e6a8c7
David Wang <dwang@sentry.io>
2024-03-28 07:45:11 -0700

    ref(crons): Shorten auto-mute broken monitors for testing (#67818)
    Shortens the period we wait before marking monitors as broken, to be
    used for testing of the internal orgs that we have this task enabled to
    run on.
    
    Also clarifies the comment for this constant
    
    Will return it to normal: https://github.com/getsentry/sentry/pull/67819

--- src/sentry/monitors/tasks/detect_broken_monitor_envs.py
+++ src/sentry/monitors/tasks/detect_broken_monitor_envs.py
@@ -34,8 +34,8 @@ NUM_CONSECUTIVE_BROKEN_CHECKINS = 4
 # The number of days a monitor env has to be failing to qualify as broken
 NUM_DAYS_BROKEN_PERIOD = 14
 
-# The number of days until a monitor env is auto-muted AFTER it has been marked broken
-NUM_DAYS_MUTED_PERIOD = 14
+# The number of days until a monitor env is auto-muted AFTER user has been notified it was broken
+NUM_DAYS_MUTED_PERIOD = 1
 
 # Max number of environments to have in the broken monitor email link
 MAX_ENVIRONMENTS_IN_MONITOR_LINK = 10
