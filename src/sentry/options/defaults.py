7e3ded789d75a0bf20cc7e537619709de6782c0f
Andrew Liu <159852527+aliu3ntry@users.noreply.github.com>
2024-03-28 10:30:42 -0700

    feat(feedback): add option for ingest feedback topic (#67839)
    relates to https://github.com/getsentry/sentry/issues/66100

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -426,6 +426,12 @@ register(
     default=[],
     flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
 )
+# Produce feedback to the new ingest-feedback-events topic, rather than ingest-events
+register(
+    "feedback.ingest-topic.rollout-rate",
+    default=0.0,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
 
 # Analytics
 register("analytics.backend", default="noop", flags=FLAG_NOSTORE)
