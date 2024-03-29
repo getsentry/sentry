b988375074a4b4a02d6d4eb91cb70275eae3e3db
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-03-29 17:07:02 -0400

    feat(crons): Add zooming to crons overview (#67948)
    Adds the ability to zoom on monitor timelines
    
    Looks like this
    
    
    https://github.com/getsentry/sentry/assets/1421724/f4014c35-a6a0-49ff-ada3-72f2730037bc

--- static/app/views/monitors/components/overviewTimeline/timelineCursor.tsx
+++ static/app/views/monitors/components/overviewTimeline/timelineCursor.tsx
@@ -11,10 +11,10 @@ const TOOLTIP_OFFSET = 10;
 
 interface Options {
   /**
-   * Function used to compute the text of the cursor tooltip. Recieves the %
-   * value the cursor is within the container.
+   * Function used to compute the text of the cursor tooltip. Receives the
+   * offset value within the container.
    */
-  labelText: (percentPosition: number) => string;
+  labelText: (positionX: number) => string;
   /**
    * May be set to false to disable rendering the timeline cursor
    */
@@ -71,7 +71,7 @@ function useTimelineCursor<E extends HTMLElement>({
         const offset = e.clientX - containerRect.left;
         const tooltipWidth = labelRef.current.offsetWidth;
 
-        labelRef.current.innerText = labelText(offset / containerRect.width);
+        labelRef.current.innerText = labelText(offset);
 
         containerRef.current.style.setProperty('--cursorOffset', `${offset}px`);
         containerRef.current.style.setProperty('--cursorMax', `${containerRect.width}px`);
