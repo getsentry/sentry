64d437927b64b45d7555a69995b50bc445ce5e4d
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-03-29 11:43:25 -0700

    ref(feedback): update screenshot display in feedback details

---
+++ static/app/components/feedback/feedbackItem/feedbackScreenshot.tsx
@@ -0,0 +1,169 @@
+import type {ReactEventHandler} from 'react';
+import {Fragment, useState} from 'react';
+import styled from '@emotion/styled';
+
+import {Role} from 'sentry/components/acl/role';
+import {Button} from 'sentry/components/button';
+import ImageVisualization from 'sentry/components/events/eventTagsAndScreenshot/screenshot/imageVisualization';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import Panel from 'sentry/components/panels/panel';
+import PanelBody from 'sentry/components/panels/panelBody';
+import PanelHeader from 'sentry/components/panels/panelHeader';
+import {IconChevron} from 'sentry/icons';
+import {t, tct} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import type {Event, EventAttachment, Organization, Project} from 'sentry/types';
+
+type Props = {
+  eventId: Event['id'];
+  openVisualizationModal: (eventAttachment: EventAttachment) => void;
+  organization: Organization;
+  projectSlug: Project['slug'];
+  screenshot: EventAttachment;
+  screenshotInFocus: number;
+  totalScreenshots: number;
+  onNext?: ReactEventHandler;
+  onPrevious?: ReactEventHandler;
+};
+
+function FeedbackScreenshot({
+  eventId,
+  organization,
+  screenshot,
+  screenshotInFocus,
+  onNext,
+  onPrevious,
+  totalScreenshots,
+  projectSlug,
+  openVisualizationModal,
+}: Props) {
+  const orgSlug = organization.slug;
+  const [loadingImage, setLoadingImage] = useState(true);
+
+  function renderContent(screenshotAttachment: EventAttachment) {
+    return (
+      <Fragment>
+        {totalScreenshots > 1 && (
+          <StyledPanelHeader lightText>
+            <Button
+              disabled={screenshotInFocus === 0}
+              aria-label={t('Previous Screenshot')}
+              onClick={onPrevious}
+              icon={<IconChevron direction="left" />}
+              size="xs"
+            />
+            {tct('[currentScreenshot] of [totalScreenshots]', {
+              currentScreenshot: screenshotInFocus + 1,
+              totalScreenshots,
+            })}
+            <Button
+              disabled={screenshotInFocus + 1 === totalScreenshots}
+              aria-label={t('Next Screenshot')}
+              onClick={onNext}
+              icon={<IconChevron direction="right" />}
+              size="xs"
+            />
+          </StyledPanelHeader>
+        )}
+        <StyledPanelBody hasHeader={totalScreenshots > 1}>
+          {loadingImage && (
+            <StyledLoadingIndicator>
+              <LoadingIndicator mini />
+            </StyledLoadingIndicator>
+          )}
+          <StyledImageWrapper
+            onClick={() => openVisualizationModal(screenshotAttachment)}
+          >
+            <StyledImageVisualization
+              attachment={screenshotAttachment}
+              orgId={orgSlug}
+              projectSlug={projectSlug}
+              eventId={eventId}
+              onLoad={() => setLoadingImage(false)}
+              onError={() => setLoadingImage(false)}
+            />
+          </StyledImageWrapper>
+        </StyledPanelBody>
+      </Fragment>
+    );
+  }
+
+  return (
+    <Role organization={organization} role={organization.attachmentsRole}>
+      {({hasRole}) => {
+        if (!hasRole) {
+          return null;
+        }
+        return <StyledPanel>{renderContent(screenshot)}</StyledPanel>;
+      }}
+    </Role>
+  );
+}
+
+export default FeedbackScreenshot;
+
+const StyledPanel = styled(Panel)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 0;
+  max-width: 360px;
+  max-height: 360px;
+  border: 0;
+`;
+
+const StyledPanelHeader = styled(PanelHeader)`
+  padding: ${space(1)};
+  width: 100%;
+  border: 1px solid ${p => p.theme.border};
+  border-bottom: 0;
+  border-top-left-radius: ${p => p.theme.borderRadius};
+  border-top-right-radius: ${p => p.theme.borderRadius};
+  display: flex;
+  justify-content: space-between;
+  text-transform: none;
+  background: ${p => p.theme.background};
+`;
+
+const StyledPanelBody = styled(PanelBody)<{hasHeader: boolean}>`
+  border: 1px solid ${p => p.theme.border};
+  min-height: 48px;
+  overflow: hidden;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  flex: 1;
+
+  ${p =>
+    !p.hasHeader &&
+    `
+  border-top-left-radius: ${p.theme.borderRadius};
+  border-top-right-radius: ${p.theme.borderRadius};
+  `}
+`;
+
+const StyledLoadingIndicator = styled('div')`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+`;
+
+const StyledImageWrapper = styled('div')`
+  :hover {
+    cursor: zoom-in;
+  }
+`;
+
+const StyledImageVisualization = styled(ImageVisualization)`
+  z-index: 1;
+  border: 0;
+  img {
+    width: auto;
+    height: auto;
+  }
+`;
--- static/app/components/feedback/feedbackItem/screenshotSection.tsx
+++ static/app/components/feedback/feedbackItem/screenshotSection.tsx
@@ -1,14 +1,18 @@
 import {useCallback, useState} from 'react';
+import styled from '@emotion/styled';
 
 import {useDeleteEventAttachmentOptimistic} from 'sentry/actionCreators/events';
 import {openModal} from 'sentry/actionCreators/modal';
-import Screenshot from 'sentry/components/events/eventTagsAndScreenshot/screenshot';
-import Modal, {
+import {Button} from 'sentry/components/button';
+import {openConfirmModal} from 'sentry/components/confirm';
+import FeedbackScreenshot from 'sentry/components/feedback/feedbackItem/feedbackScreenshot';
+import OpenScreenshotModal, {
   modalCss,
-} from 'sentry/components/events/eventTagsAndScreenshot/screenshot/modal';
-import Section from 'sentry/components/feedback/feedbackItem/feedbackItemSection';
+} from 'sentry/components/feedback/feedbackItem/openScreenshotModal';
 import useFeedbackScreenshot from 'sentry/components/feedback/feedbackItem/useFeedbackHasScreenshot';
+import {IconDelete} from 'sentry/icons';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import type {Organization} from 'sentry/types';
 import type {Event} from 'sentry/types/event';
 import type {EventAttachment} from 'sentry/types/group';
@@ -39,22 +43,15 @@ export function ScreenshotSection({projectSlug, event, organization}: Props) {
   );
 
   const handleOpenVisualizationModal = useCallback(
-    (eventAttachment: EventAttachment, downloadUrl: string) => {
-      function handleDelete() {
-        handleDeleteScreenshot(eventAttachment.id);
-      }
-
+    (eventAttachment: EventAttachment) => {
       openModal(
         modalProps => (
-          <Modal
+          <OpenScreenshotModal
             {...modalProps}
             event={event}
             orgSlug={organization.slug}
             projectSlug={projectSlug}
             eventAttachment={eventAttachment}
-            downloadUrl={downloadUrl}
-            onDelete={handleDelete}
-            onDownload={() => undefined}
             attachments={screenshots}
             attachmentIndex={screenshotInFocus}
           />
@@ -62,14 +59,7 @@ export function ScreenshotSection({projectSlug, event, organization}: Props) {
         {modalCss}
       );
     },
-    [
-      event,
-      handleDeleteScreenshot,
-      organization.slug,
-      projectSlug,
-      screenshotInFocus,
-      screenshots,
-    ]
+    [event, organization.slug, projectSlug, screenshotInFocus, screenshots]
   );
 
   if (!hasContext && !screenshots.length) {
@@ -80,19 +70,38 @@ export function ScreenshotSection({projectSlug, event, organization}: Props) {
   const screenshot = screenshots[screenshotInFocus];
 
   return showScreenshot ? (
-    <Section title={t('Screenshot')}>
-      <Screenshot
+    <ScreenshotWrapper>
+      <FeedbackScreenshot
         organization={organization}
         eventId={event.id}
         projectSlug={projectSlug}
         screenshot={screenshot}
-        onDelete={handleDeleteScreenshot}
         onNext={() => setScreenshotInFocus(screenshotInFocus + 1)}
         onPrevious={() => setScreenshotInFocus(screenshotInFocus - 1)}
         screenshotInFocus={screenshotInFocus}
         totalScreenshots={screenshots.length}
         openVisualizationModal={handleOpenVisualizationModal}
       />
-    </Section>
+      <Button
+        icon={<IconDelete />}
+        borderless
+        size="xs"
+        onClick={() => {
+          openConfirmModal({
+            header: t('Delete screenshot?'),
+            message: t('This action cannot be undone.'),
+            confirmText: t('Delete screenshot'),
+            onConfirm: () => handleDeleteScreenshot(screenshot.id),
+            priority: 'danger',
+          });
+        }}
+        aria-label="delete screenshot"
+      />
+    </ScreenshotWrapper>
   ) : null;
 }
+
+const ScreenshotWrapper = styled('div')`
+  display: flex;
+  gap: ${space(1)};
+`;
