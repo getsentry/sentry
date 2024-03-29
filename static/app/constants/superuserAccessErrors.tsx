ca8e7679d742db17aa785aea915c51eba42082cd
Seiji Chew <67301797+schew2381@users.noreply.github.com>
2024-03-29 10:41:38 -0700

    nit(ui): Specify account type in error message (#67927)

--- static/app/constants/superuserAccessErrors.tsx
+++ static/app/constants/superuserAccessErrors.tsx
@@ -5,6 +5,6 @@ export enum ErrorCodes {
   INVALID_SSO_SESSION = 'Your SSO Session has expired, please reauthenticate',
   INVALID_ACCESS_CATEGORY = 'Please fill out the access category and reason correctly',
   MISSING_PASSWORD_OR_U2F = 'Password or U2F challenge/response was not sent',
-  NO_AUTHENTICATOR = 'Please add a U2F authenticator to your account',
+  NO_AUTHENTICATOR = 'Please add a U2F authenticator to your Sentry account',
   UNKNOWN_ERROR = 'An error occurred, please try again',
 }
