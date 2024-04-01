f934db7364a1de0cfc4a074089e17c2974bb5917
nathan hsieh <hsieh.nathan@gmail.com>
2024-04-01 12:04:03 -0700

    reorder

--- src/sentry/incidents/logic.py
+++ src/sentry/incidents/logic.py
@@ -594,15 +594,15 @@ def create_alert_rule(
             ]
             AlertRuleExcludedProjects.objects.bulk_create(exclusions)
 
-        # initialize projects join table for activated alert rules
-        arps = [AlertRuleProjects(alert_rule=alert_rule, project=project) for project in projects]
-        AlertRuleProjects.objects.bulk_create(arps)
-
         if monitor_type == AlertRuleMonitorType.ACTIVATED and activation_condition:
             # NOTE: if monitor_type is activated, activation_condition is required
             AlertRuleActivationCondition.objects.create(
                 alert_rule=alert_rule, condition_type=activation_condition.value
             )
+
+        # initialize projects join table for activated alert rules
+        arps = [AlertRuleProjects(alert_rule=alert_rule, project=project) for project in projects]
+        AlertRuleProjects.objects.bulk_create(arps)
 
         # NOTE: This constructs the query in snuba
         # NOTE: Will only subscribe if AlertRule.monitor_type === 'CONTINUOUS'
