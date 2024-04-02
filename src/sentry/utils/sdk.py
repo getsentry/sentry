063b3c1c970368f7de912fd0e97928370329ebb9
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-04-02 10:53:26 -0400

    enable cache spans

--- src/sentry/utils/sdk.py
+++ src/sentry/utils/sdk.py
@@ -460,7 +460,7 @@ def configure_sdk():
         transport=MultiplexingTransport(),
         integrations=[
             DjangoAtomicIntegration(),
-            DjangoIntegration(signals_spans=False),
+            DjangoIntegration(signals_spans=False, cache_spans=True),
             CeleryIntegration(monitor_beat_tasks=True, exclude_beat_tasks=exclude_beat_tasks),
             # This makes it so all levels of logging are recorded as breadcrumbs,
             # but none are captured as events (that's handled by the `internal`
