d04414280cf9b93eac121d02d7a12960294294f3
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2024-04-03 12:21:00 -0400

    Update temporary.py

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -239,7 +239,7 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:starfish-browser-webvitals-pageoverview-v2", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
     manager.add("organizations:starfish-browser-webvitals-replace-fid-with-inp", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
     manager.add("organizations:starfish-browser-webvitals-score-computed-total", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
-    manager.add("organizations:performance-cache-module-view", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
+    manager.add("organizations:performance-cache-view", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
     manager.add("organizations:starfish-browser-webvitals-use-backend-scores", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
     manager.add("organizations:starfish-mobile-appstart", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
     manager.add("organizations:starfish-mobile-ui-module", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
