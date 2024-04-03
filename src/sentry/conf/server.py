e204cf68ec49011fa732cb4c9deb29a04d574f62
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2024-04-03 12:49:56 -0400

    feat(starfish-cache): register cache module ui flag (#68184)
    register flag to control cache module ui

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1915,6 +1915,8 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     "organizations:starfish-browser-webvitals-replace-fid-with-inp": False,
     # Uses a computed total count to calculate the score in the browser starfish webvitals module, instead of measurements.score.total
     "organizations:starfish-browser-webvitals-score-computed-total": False,
+    # Enable browser starfish cache module ui
+    "organizations:performance-cache-view": False,
     # Enable mobile starfish app start module view
     "organizations:starfish-mobile-appstart": False,
     # Enable mobile starfish ui module view
