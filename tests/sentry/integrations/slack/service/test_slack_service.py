a3615351f446f8853a5ef83085c36ce7c54fbda8
Yash Kamothi <yash.kamothi@gmail.com>
2024-04-03 15:12:32 -0700

    tests

--- tests/sentry/integrations/slack/service/test_slack_service.py
+++ tests/sentry/integrations/slack/service/test_slack_service.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from unittest import mock
 from uuid import uuid4
 
@@ -41,7 +42,75 @@ class TestGetNotificationMessageToSend(TestCase):
 
 class TestHandleParentNotification(TestCase):
     def setUp(self) -> None:
+        """
+        Setup default and reusable testing data or objects
+        """
         self.service = SlackService.default()
+
+        self.rule_action_uuid = str(uuid4())
+        self.notify_issue_owners_action = [
+            {
+                "targetType": "IssueOwners",
+                "fallthroughType": "ActiveMembers",
+                "id": "sentry.mail.actions.NotifyEmailAction",
+                "targetIdentifier": "",
+                "uuid": self.rule_action_uuid,
+            }
+        ]
+        self.rule = self.create_project_rule(
+            project=self.project, action_match=self.notify_issue_owners_action
+        )
+        self.rule_fire_history = RuleFireHistory.objects.create(
+            project=self.project,
+            rule=self.rule,
+            group=self.group,
+            event_id=456,
+            notification_uuid=str(uuid4()),
+        )
+
+    def test_raises_exception_when_parent_notification_does_not_have_rule_fire_history_data(
+        self,
+    ) -> None:
+        """
+        Purposefully create a domain object that does not have rule_fire_history
+        """
+        parent_notification = IssueAlertNotificationMessage(
+            id=123,
+            date_added=datetime.now(),
+            message_identifier="1a2s3d",
+            rule_action_uuid=str(uuid4()),
+        )
+        with pytest.raises(RuleDataError) as err:
+            self.service._handle_parent_notification(
+                parent_notification=parent_notification,
+                notification_to_send="",
+                client=mock.MagicMock(),
+            )
+            assert (
+                err.value
+                == f"parent notification {parent_notification.id} does not have a rule_fire_history"
+            )
+
+    def test_raises_exception_when_parent_notification_does_not_have_rule_action_uuid(self) -> None:
+        """
+        Purposefully create a domain object that does not have the rule_action_uuid
+        """
+        parent_notification = IssueAlertNotificationMessage(
+            id=123,
+            date_added=datetime.now(),
+            message_identifier="1a2s3d",
+            rule_fire_history=self.rule_fire_history,
+        )
+        with pytest.raises(RuleDataError) as err:
+            self.service._handle_parent_notification(
+                parent_notification=parent_notification,
+                notification_to_send="",
+                client=mock.MagicMock(),
+            )
+            assert (
+                err.value
+                == f"parent notification {parent_notification.id} does not have a rule_action_uuid"
+            )
 
     def test_raises_exception_when_rule_action_does_not_exist(self) -> None:
         """
@@ -84,29 +153,9 @@ class TestHandleParentNotification(TestCase):
             )
 
     def test_raises_exception_when_rule_action_does_not_have_channel_id(self) -> None:
-        rule_action_uuid = str(uuid4())
-        notify_issue_owners_action = [
-            {
-                "targetType": "IssueOwners",
-                "fallthroughType": "ActiveMembers",
-                "id": "sentry.mail.actions.NotifyEmailAction",
-                "targetIdentifier": "",
-                "uuid": rule_action_uuid,
-            }
-        ]
-        rule = self.create_project_rule(
-            project=self.project, action_match=notify_issue_owners_action
-        )
-        rule_fire_history = RuleFireHistory.objects.create(
-            project=self.project,
-            rule=rule,
-            group=self.group,
-            event_id=456,
-            notification_uuid=str(uuid4()),
-        )
         parent_notification_message = NotificationMessage.objects.create(
-            rule_fire_history=rule_fire_history,
-            rule_action_uuid=rule_action_uuid,
+            rule_fire_history=self.rule_fire_history,
+            rule_action_uuid=self.rule_action_uuid,
             message_identifier="123abc",
         )
         parent_notification = IssueAlertNotificationMessage.from_model(parent_notification_message)
@@ -118,5 +167,5 @@ class TestHandleParentNotification(TestCase):
             )
             assert (
                 err.value
-                == f"failed to get channel_id for rule {rule.id} and rule action {parent_notification.rule_action_uuid}"
+                == f"failed to get channel_id for rule {self.rule.id} and rule action {parent_notification.rule_action_uuid}"
             )
