a514e7774a2200d9a341aabd3f9c9f09a826537c
Edward Gou <edward.gou@sentry.io>
2024-04-04 14:48:48 -0400

    Fix tests

--- static/app/views/performance/browser/webVitals/components/webVitalMeters.spec.tsx
+++ static/app/views/performance/browser/webVitals/components/webVitalMeters.spec.tsx
@@ -56,18 +56,7 @@ describe('WebVitalMeters', function () {
     jest.mocked(useOrganization).mockReturnValue(organization);
   });
 
-  it('renders web vital meters with first input delay', async () => {
-    render(<WebVitalMeters projectData={projectData} projectScore={projectScore} />);
-    await screen.findByText('Largest Contentful Paint');
-    screen.getByText('First Contentful Paint');
-    screen.getByText('Cumulative Layout Shift');
-    screen.getByText('Time To First Byte');
-    screen.getByText('First Input Delay');
-  });
-
   it('renders web vital meters with interaction to next paint', async () => {
-    const organizationWithInp = OrganizationFixture();
-    jest.mocked(useOrganization).mockReturnValue(organizationWithInp);
     render(<WebVitalMeters projectData={projectData} projectScore={projectScore} />);
     await screen.findByText('Largest Contentful Paint');
     screen.getByText('First Contentful Paint');
