8c36b1e9297bb30ee8a8a95a95ad329db8476d9f
Sebastian Zivota <loewenheim@mailbox.org>
2024-04-04 18:20:03 +0200

    fix: Enable automator for proguard options

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -625,10 +625,17 @@ register(
 )
 
 # Enable use of Symbolicator proguard processing for specific projects.
-register("symbolicator.proguard-processing-projects", type=Sequence, default=[])
+register(
+    "symbolicator.proguard-processing-projects",
+    type=Sequence,
+    default=[],
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
 # Enable use of Symbolicator proguard processing for fraction of projects.
-register("symbolicator.proguard-processing-sample-rate", default=0.0)
-register("symbolicator.proguard-processing-ab-test", default=0.0)
+register(
+    "symbolicator.proguard-processing-sample-rate", default=0.0, flags=FLAG_AUTOMATOR_MODIFIABLE
+)
+register("symbolicator.proguard-processing-ab-test", default=0.0, flags=FLAG_AUTOMATOR_MODIFIABLE)
 
 # Post Process Error Hook Sampling
 register(
