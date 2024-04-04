644f701658bc6c8472c519ed8c6b9046a71914e6
joshuarli <joshuarli98@gmail.com>
2024-04-04 11:56:33 -0700

    devenv: remove legacy sentry-cli bash reporting (#68217)

--- scripts/do.sh
+++ scripts/do.sh
@@ -9,10 +9,6 @@ HERE="$(
 # shellcheck disable=SC1090
 source "${HERE}/lib.sh"
 
-# This block is to enable reporting issues to Sentry.io
-# SENTRY_DSN already defined in .envrc
-configure-sentry-cli
-
 # This guarantees that we're within a venv. A caller that is not within
 # a venv can avoid enabling this by setting SENTRY_NO_VENV_CHECK
 [ -z "${SENTRY_NO_VENV_CHECK+x}" ] && eval "${HERE}/ensure-venv.sh"
