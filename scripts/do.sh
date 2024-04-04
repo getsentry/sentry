5b6ca50b64798c0a57be1bcf3575feb20414f390
Joshua Li <joshuarli98@gmail.com>
2024-04-04 11:58:21 -0700

    feedback

--- scripts/do.sh
+++ scripts/do.sh
@@ -3,25 +3,25 @@
 # Call this script as "do.sh method_from_lib" to execute any function from that library
 set -eu
 
-command -v devenv >/dev/null 2>&1 || {
-    echo '
-Please install the devenv tool:
-https://github.com/getsentry/devenv#install
-'
-    exit 1
-}
-
 HERE="$(
     cd "$(dirname "${BASH_SOURCE[0]}")"
     pwd -P
 )"
 
-[[ "$VIRTUAL_ENV" -ef "${HERE}/../.venv" ]] || {
+if ! command -v devenv >/dev/null 2>&1; then
+    echo '
+Please install the devenv tool:
+https://github.com/getsentry/devenv#install
+'
+    exit 1
+fi
+
+if ! [[ "$VIRTUAL_ENV" -ef "${HERE}/../.venv" ]]; then
     echo "
 Your virtualenv isn't activated. You need to successfully run 'direnv allow'.
 "
     exit 1
-}
+fi
 
 # shellcheck disable=SC1090
 source "${HERE}/lib.sh"
