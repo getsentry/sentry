76456b1a1110e96cb2d5aff5f14d1b669d57f7e5
colin-sentry <161344340+colin-sentry@users.noreply.github.com>
2024-04-04 12:00:16 -0400

    Initial AI analytics page (#68266)
    A new page for AI analytics workloads like monitoring LLM error rates

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -1490,6 +1490,16 @@ function buildRoutes() {
     </Route>
   );
 
+  const aiAnalyticsRoutes = (
+    <Route
+      path="/ai-analytics/"
+      component={make(() => import('sentry/views/ai-analytics'))}
+      withOrgPath
+    >
+      <IndexRoute component={make(() => import('sentry/views/ai-analytics/landing'))} />
+    </Route>
+  );
+
   const performanceRoutes = (
     <Route
       path="/performance/"
@@ -2151,6 +2161,7 @@ function buildRoutes() {
       {statsRoutes}
       {discoverRoutes}
       {performanceRoutes}
+      {aiAnalyticsRoutes}
       {starfishRoutes}
       {profilingRoutes}
       {metricsRoutes}
