679479bd9876962b57771dc1f40cafd309f1452d
Shruthi <shruthilaya.jaganathan@sentry.io>
2024-04-05 12:38:00 -0400

    chore: Add some observability to perf issue detection pipeline (#68285)
    Adding some observability to the pipeline to help narrow down what's
    causing the funkiness we're seeing.
    
    1. We have fields missing from spans, so we're capturing transactions
    and dumping some of the span payloads into the tags so we can see where
    the fields get dropped.
    2. Don't receive all the spans we expect to receive in a segment, so I'm
    adding a couple metrics to see if we're significantly dropping spans at
    a certain step: `process_spans.spans.received.count`: how many we
    receive -> `process_spans.spans.write.count`: how many we write to
    redis, `process_spans.spans.read.count`: how many we read from redis and
    `detect_performance_issues.spans.count`: how many we run through perf
    issue detector.
    3. We could be processing the wrong bucket, so tagging some timestamps
    and sample keys to help debug.
    4. Explicitly capture exceptions in sentry
    
    Note: there are no functional changes to the code, this PR just adds
    logging/observability. I'd like to merge this first, get some data to
    see if it supports our theories and then follow up with simplifications
    to the code that might solve the issues so we can pinpoint a root cause.

--- src/sentry/spans/consumers/process/factory.py
+++ src/sentry/spans/consumers/process/factory.py
@@ -1,8 +1,10 @@
 import dataclasses
 import logging
+import random
 from collections.abc import Mapping
 from typing import Any
 
+import sentry_sdk
 from arroyo.backends.kafka.consumer import Headers, KafkaPayload
 from arroyo.processing.strategies import RunTask
 from arroyo.processing.strategies.abstract import ProcessingStrategy, ProcessingStrategyFactory
@@ -15,6 +17,7 @@ from sentry_kafka_schemas.schema_types.snuba_spans_v1 import SpanEvent
 from sentry import options
 from sentry.spans.buffer.redis import RedisSpansBuffer
 from sentry.spans.produce_segment import produce_segment_to_kafka
+from sentry.utils import metrics
 from sentry.utils.arroyo import MultiprocessingPool, RunTaskWithMultiprocessing
 
 logger = logging.getLogger(__name__)
@@ -42,7 +45,7 @@ def _deserialize_span(value: bytes) -> Mapping[str, Any]:
     return SPAN_SCHEMA.decode(value)
 
 
-def process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | None:
+def _process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | None:
     if not options.get("standalone-spans.process-spans-consumer.enable"):
         return None
 
@@ -58,45 +61,95 @@ def process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | N
         return None
 
     assert isinstance(message.value, BrokerValue)
-    try:
-        span = _deserialize_span(message.payload.value)
+    metrics.incr("process_spans.spans.received.count")
+
+    with sentry_sdk.start_transaction(op="process", name="spans.process.process_message") as txn:
+        payload_value = message.payload.value
+        timestamp = int(message.value.timestamp.timestamp())
+        partition = message.value.partition.index
+
+        if random.random() < 0.005:
+            sentry_sdk.set_context("payload", {"value": str(payload_value)})
+            txn.set_tag("has_payload", True)
+
+        span = _deserialize_span(payload_value)
         segment_id = span["segment_id"]
-    except Exception:
-        logger.exception("Failed to process span payload")
-        return None
+        trace_id = span["trace_id"]
 
-    timestamp = int(message.value.timestamp.timestamp())
-    partition = message.value.partition.index
+        txn.set_tag("trace.id", trace_id)
+        txn.set_tag("segment.id", segment_id)
+        sentry_sdk.set_measurement("num_keys", len(span))
 
-    client = RedisSpansBuffer()
+        client = RedisSpansBuffer()
 
-    should_process_segments = client.write_span_and_check_processing(
-        project_id, segment_id, timestamp, partition, message.payload.value
-    )
+        should_process_segments = client.write_span_and_check_processing(
+            project_id, segment_id, timestamp, partition, payload_value
+        )
+
+        metrics.incr("process_spans.spans.write.count")
 
     return ProduceSegmentContext(
         should_process_segments=should_process_segments, timestamp=timestamp, partition=partition
     )
 
 
-def produce_segment(message: Message[ProduceSegmentContext | None]):
+def process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | None:
+    try:
+        return _process_message(message)
+    except Exception:
+        sentry_sdk.capture_exception()
+        return None
+
+
+def _produce_segment(message: Message[ProduceSegmentContext | None]):
     if message.payload is None:
         return
 
     context: ProduceSegmentContext = message.payload
+
     if context.should_process_segments:
-        client = RedisSpansBuffer()
+        with sentry_sdk.start_transaction(
+            op="process", name="spans.process.produce_segment"
+        ) as txn:
+            client = RedisSpansBuffer()
+            payload_context = {}
 
-        keys = client.get_unprocessed_segments_and_prune_bucket(
-            context.timestamp, context.partition
-        )
-        # With pipelining, redis server is forced to queue replies using
-        # up memory, so batching the keys we fetch.
-        for i in range(0, len(keys), BATCH_SIZE):
-            segments = client.read_and_expire_many_segments(keys[i : i + BATCH_SIZE])
+            with txn.start_child(op="process", description="fetch_unprocessed_segments"):
+                keys = client.get_unprocessed_segments_and_prune_bucket(
+                    context.timestamp, context.partition
+                )
 
-            for segment in segments:
-                produce_segment_to_kafka(segment)
+            sentry_sdk.set_measurement("segments.count", len(keys))
+            if len(keys) > 0:
+                payload_context["sample_key"] = keys[0]
+
+            sample_span = None
+            total_spans_read = 0
+            # With pipelining, redis server is forced to queue replies using
+            # up memory, so batching the keys we fetch.
+            with txn.start_child(op="process", description="produce_fetched_segments"):
+                for i in range(0, len(keys), BATCH_SIZE):
+                    segments = client.read_and_expire_many_segments(keys[i : i + BATCH_SIZE])
+
+                    for segment in segments:
+                        total_spans_read += len(segment)
+                        if len(segment) > 0:
+                            sample_span = segment[0]
+                        produce_segment_to_kafka(segment)
+
+            metrics.incr("process_spans.spans.read.count", total_spans_read)
+            if sample_span and random.random() < 0.01:
+                txn.set_tag("has_payload", True)
+                payload_context["sample_span"] = str(sample_span)
+
+            sentry_sdk.set_context("payload", payload_context)
+
+
+def produce_segment(message: Message[ProduceSegmentContext | None]):
+    try:
+        _produce_segment(message)
+    except Exception:
+        sentry_sdk.capture_exception()
 
 
 class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
