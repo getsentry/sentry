3334aa41120e8ccb6a2004be6d79e424663f34ea
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-04-05 10:11:15 -0400

    feat(related_issues): Change endpoint return shape
    This add the new shape of the related-issues endpoint. It is backward compatible until we change the UI.

--- src/sentry/issues/related/__init__.py
+++ src/sentry/issues/related/__init__.py
@@ -11,9 +11,9 @@ RELATED_ISSUES_ALGORITHMS = {
 }
 
 
-def find_related_issues(group: Group) -> dict[str, list[Group]]:
-    related_issues = {}
+def find_related_issues(group: Group) -> list[dict[str, str | list[Group]]]:
+    related_issues = []
     for key, func in RELATED_ISSUES_ALGORITHMS.items():
-        related_issues[key] = func(group)
+        related_issues.append({"type": key, "data": func(group)})
 
-    return related_issues or {}
+    return related_issues
