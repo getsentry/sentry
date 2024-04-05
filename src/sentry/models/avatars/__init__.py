a20386a5d2b74aac62134da024c714f450d04d3d
Mark Story <mark@mark-story.com>
2024-04-05 15:06:41 -0400

    Remove foreign keys first

--- src/sentry/models/avatars/__init__.py
+++ src/sentry/models/avatars/__init__.py
@@ -2,7 +2,9 @@ from .base import AvatarBase
 from .control_base import ControlAvatarBase
 from .doc_integration_avatar import DocIntegrationAvatar
 from .organization_avatar import OrganizationAvatar
+from .project_avatar import ProjectAvatar
 from .sentry_app_avatar import SentryAppAvatar
+from .team_avatar import TeamAvatar
 from .user_avatar import UserAvatar
 
 __all__ = (
@@ -10,6 +12,8 @@ __all__ = (
     "ControlAvatarBase",
     "DocIntegrationAvatar",
     "OrganizationAvatar",
+    "ProjectAvatar",
     "SentryAppAvatar",
+    "TeamAvatar",
     "UserAvatar",
 )
