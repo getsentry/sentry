7426655491e2443945287b460d3021bddc8e9130
alwayshang <zhanghonghao@outlook.com>
2024-04-07 13:58:20 +0800

    chore: remove repetitive words
    Signed-off-by: alwayshang <zhanghonghao@outlook.com>

--- static/app/utils/url/stripURLOrigin.spec.tsx
+++ static/app/utils/url/stripURLOrigin.spec.tsx
@@ -13,7 +13,7 @@ describe('stripURLOrigin', () => {
     '/[a-z]@[a-z].com/',
     'nulltext/html;base64,PHNjcmlwdD4KICAgICAgb25tZXNzYWdlID0gKGV2ZW50KSA9PiB7CiAgICAgICAgY29uc29sZS5sb2coJ2hlbGxvIHdvcmxkJyk7CiAgICAgIH0KICA8L3NjcmlwdD4=',
   ])(
-    'should return the the same string, when something is not parseable as a url',
+    'should return the same string, when something is not parseable as a url',
     str => {
       expect(stripURLOrigin(str)).toEqual(str);
     }
