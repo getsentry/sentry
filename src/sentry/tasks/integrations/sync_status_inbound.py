ba98348810a608ee7006554bc7bb9ae3d7b0a2ec
Stephen Cefali <scefali@sentry.io>
2024-04-08 14:49:50 -0700

    adds the ability for Jira to configure resolving in next release, current release, or immediately

--- src/sentry/tasks/integrations/sync_status_inbound.py
+++ src/sentry/tasks/integrations/sync_status_inbound.py
@@ -1,10 +1,15 @@
 from collections.abc import Mapping
 from typing import Any
 
+from django.utils import timezone as django_timezone
+
 from sentry import analytics
+from sentry.api.helpers.group_index.update import get_current_release_version_of_group
 from sentry.models.group import Group, GroupStatus
+from sentry.models.groupresolution import GroupResolution
 from sentry.models.integrations.integration import Integration
 from sentry.models.organization import Organization
+from sentry.models.release import Release, ReleaseStatus, follows_semver_versioning_scheme
 from sentry.services.hybrid_cloud.integration import integration_service
 from sentry.silo import SiloMode
 from sentry.tasks.base import instrumented_task, retry, track_group_async_operation
@@ -38,6 +43,7 @@ def sync_status_inbound(
         return
 
     installation = integration.get_installation(organization_id=organization_id)
+    config = installation.get_config_data()
 
     try:
         # This makes an API call.
@@ -51,17 +57,78 @@ def sync_status_inbound(
         "provider_key": provider.key,
         "integration_id": integration_id,
     }
-
     if action == ResolveSyncAction.RESOLVE:
+        activity_type = ActivityType.SET_RESOLVED
+        # If the resolution strategy is set to resolve in the next release or current release
+        if config.get("resolution_strategy") in [
+            "resolve_next_release",
+            "resolve_current_release",
+        ]:
+            # only allow setting resolved in release if there is a release for each project
+            all_project_ids = list({group.project_id for group in affected_groups})
+            has_releases_for_each_project = all(
+                Release.objects.filter(
+                    projects=project_id, organization_id=organization_id, status=ReleaseStatus.OPEN
+                ).exists()
+                for project_id in all_project_ids
+            )
+            if has_releases_for_each_project:
+                if config.get("resolution_strategy") == "resolve_next_release":
+                    activity_type = ActivityType.SET_RESOLVED_IN_RELEASE
+                    activity_data["inNextRelease"] = True
+                elif config.get("resolution_strategy") == "resolve_current_release":
+                    activity_type = ActivityType.SET_RESOLVED_IN_RELEASE
+
         Group.objects.update_group_status(
             groups=affected_groups,
             status=GroupStatus.RESOLVED,
             substatus=None,
-            activity_type=ActivityType.SET_RESOLVED,
+            activity_type=activity_type,
             activity_data=activity_data,
         )
-
         for group in affected_groups:
+            # update the resolutions
+            # probably should be done within a single transaction with the status but this is fine for now
+            # note this logic is ported from src/sentry/api/helpers/group_index/update.py
+            if activity_type == ActivityType.SET_RESOLVED_IN_RELEASE:
+                # find the latest release by date for the project
+                last_release_by_date = (
+                    Release.objects.filter(
+                        projects=group.project,
+                        organization_id=organization_id,
+                        status=ReleaseStatus.OPEN,
+                    )
+                    .extra(select={"sort": "COALESCE(date_released, date_added)"})
+                    .order_by("-sort")
+                    .first()
+                )
+                # Check if semver versioning scheme is followed
+                follows_semver = follows_semver_versioning_scheme(
+                    org_id=organization_id,
+                    project_id=group.project.id,
+                    release_version=last_release_by_date.version,
+                )
+
+                resolution_params = {
+                    "status": GroupStatus.RESOLVED,
+                    "release": last_release_by_date,  # Is this the right release?
+                }
+                if config.get("resolution_strategy") == "resolve_next_release":
+                    # get the current release version of the group if we are resolving in the next release
+                    current_release_version = get_current_release_version_of_group(
+                        group=group, follows_semver=follows_semver
+                    )
+                    resolution_params["current_release_version"] = current_release_version
+                    resolution_params["type"] = GroupResolution.Type.in_next_release
+                else:
+                    resolution_params["type"] = GroupResolution.Type.in_release
+
+                resolution, created = GroupResolution.objects.get_or_create(
+                    group=group, defaults=resolution_params
+                )
+                if not created:
+                    resolution.update(datetime=django_timezone.now(), **resolution_params)
+
             analytics.record(
                 "issue.resolved",
                 project_id=group.project.id,
