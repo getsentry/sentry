38c22878c2663c4defb646b9f76f2901340152a1
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-04-08 09:11:04 -0400

    Add refetch button

--- static/app/views/performance/http/httpSamplesPanel.tsx
+++ static/app/views/performance/http/httpSamplesPanel.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 import * as qs from 'query-string';
 
 import ProjectAvatar from 'sentry/components/avatar/projectAvatar';
+import {Button} from 'sentry/components/button';
 import {SegmentedControl} from 'sentry/components/segmentedControl';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
@@ -130,6 +131,7 @@ export function HTTPSamplesPanel() {
     data: samplesData,
     isFetching: isSamplesDataFetching,
     error: samplesDataError,
+    refetch: refetchSpanSamples,
   } = useSpanSamples({
     search: MutableSearch.fromQueryObject(filters),
     fields: [
@@ -321,6 +323,12 @@ export function HTTPSamplesPanel() {
               />
             </ModuleLayout.Full>
           )}
+
+          <ModuleLayout.Full>
+            <Button onClick={() => refetchSpanSamples()}>
+              {t('Try Different Samples')}
+            </Button>
+          </ModuleLayout.Full>
         </ModuleLayout.Layout>
       </DetailPanel>
     </PageAlertProvider>
