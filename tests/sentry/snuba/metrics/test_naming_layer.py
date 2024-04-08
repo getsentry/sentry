691b0f6252f0d3405e477b8867971c5c2735f48b
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-04-08 14:51:14 +0200

    feat(ddm): Implement lenient MRI parsing (#68175)

--- tests/sentry/snuba/metrics/test_naming_layer.py
+++ tests/sentry/snuba/metrics/test_naming_layer.py
@@ -2,12 +2,7 @@ import re
 
 import pytest
 
-from sentry.snuba.metrics.naming_layer.mri import (
-    MRI_SCHEMA_REGEX,
-    ParsedMRI,
-    is_custom_measurement,
-    parse_mri,
-)
+from sentry.snuba.metrics.naming_layer.mri import ParsedMRI, is_custom_measurement, parse_mri
 from sentry.snuba.metrics.naming_layer.public import PUBLIC_NAME_REGEX
 
 
@@ -46,44 +41,6 @@ def test_invalid_public_name_regex(name):
 
 
 @pytest.mark.parametrize(
-    "name",
-    [
-        "e:sessions/error.preaggr@none",
-        "e:sessions/crashed_abnormal@none",
-        "e:sessions/user.crashed_abnormal@none",
-        "e:sessions/healthy@",
-        "e:sessions/healthy.crashed@",
-        "e:sessions/healthy.crashed.crashed@",
-        "e:sessions/healthy_crashed.crashed@",
-        "e:sessions/healthy.crashed_crashed_sessions@",
-        "d:transactions/measurements.frames_slow_rate@ratio",
-        "c:sessions/session@none",
-        "s:sessions/error@none",
-        "g:sessions/error@none",
-        "g:custom/error@none",
-        "c:custom/MyMetric@user",
-        "c:custom/MyMetric.MyService@User",
-        "c:custom/MyMetric.MyService.2@User",
-    ],
-)
-def test_valid_mri_schema_regex(name):
-    matches = MRI_SCHEMA_REGEX.match(name)
-    assert matches
-    assert matches[0] == name
-
-
-@pytest.mark.parametrize(
-    "name",
-    [
-        "t:sessions/error.preaggr@none",
-        "e:foo/error.preaggr@none" "foo.bar",
-    ],
-)
-def test_invalid_mri_schema_regex(name):
-    assert MRI_SCHEMA_REGEX.match(name) is None
-
-
-@pytest.mark.parametrize(
     "name, expected",
     [
         (
@@ -102,12 +59,45 @@ def test_invalid_mri_schema_regex(name):
             "s:sessions/error@none",
             ParsedMRI("s", "sessions", "error", "none"),
         ),
+        (
+            "dist:my_namespace/organizations/v1/my endpoint@{none}",
+            ParsedMRI("dist", "my_namespace", "organizations/v1/my endpoint", "{none}"),
+        ),
+        (
+            "d:transactions/measurements.disk_io@byte/second",
+            ParsedMRI("d", "transactions", "measurements.disk_io", "byte/second"),
+        ),
+        (
+            "c:custom/http.client.open_connections@{connection}",
+            ParsedMRI("c", "custom", "http.client.open_connections", "{connection}"),
+        ),
+        (
+            "c:custom/http.client.active_requests@{request}",
+            ParsedMRI("c", "custom", "http.client.active_requests", "{request}"),
+        ),
     ],
 )
-def test_parse_mri(name, expected):
+def test_parse_mri_with_valid_mri(name, expected):
     parsed_mri = parse_mri(name)
     assert parsed_mri == expected
     assert parsed_mri.mri_string == name
+
+
+@pytest.mark.parametrize(
+    "name",
+    [
+        "d@transactions/measurements.stall_longest_time",
+        "d:transactions/breakdowns.span_ops.ops.http",
+        "d/transactions@breakdowns.span_ops.ops.http:millisecond",
+        "d/transactions",
+        "transactions",
+        ":transactions/breakdowns.span_ops.ops.http@none",
+        ":/@",
+    ],
+)
+def test_parse_mri_with_invalid_mri(name):
+    parsed_mri = parse_mri(name)
+    assert parsed_mri is None
 
 
 @pytest.mark.parametrize(
