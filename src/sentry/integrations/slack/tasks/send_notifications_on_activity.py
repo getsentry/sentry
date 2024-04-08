5bcfb53f5e1d436d17f7db503fd30887767e8c68
Yash Kamothi <yash.kamothi@gmail.com>
2024-04-08 15:09:13 -0700

    add tests and address pr comments

--- src/sentry/integrations/slack/tasks/send_notifications_on_activity.py
+++ src/sentry/integrations/slack/tasks/send_notifications_on_activity.py
@@ -1,5 +1,6 @@
 import logging
 
+from django.db import router, transaction
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -9,6 +10,7 @@ from sentry.models.activity import Activity
 from sentry.models.organization import Organization
 from sentry.silo import SiloMode
 from sentry.tasks.base import instrumented_task
+from sentry.utils import metrics
 
 _default_logger = logging.getLogger(__name__)
 
@@ -25,19 +27,28 @@ def send_activity_notifications(activity_id):
         _default_logger.info("Activity does not exist", extra={"activity_id": activity_id})
         return
 
-    _default_logger.info("Sending notifications for activity", extra={"activity_id": activity_id})
-
     organization = Organization.objects.get_from_cache(pk=activity.project.organization_id)
     if features.has("organizations:slack-thread-issue-alert", organization):
         slack_service = SlackService.default()
-        slack_service.notify_all_threads_for_activity(activity=activity)
+        try:
+            slack_service.notify_all_threads_for_activity(activity=activity)
+            metrics.incr(
+                "sentry.integrations.slack.tasks.send_notifications_on_activity.send_activity_notifications.success",
+                sample_rate=1.0,
+            )
+        except Exception as err:
+            _default_logger.info(
+                "Failed to send notifications for activity",
+                exc_info=err,
+                extra={"activity_id": activity_id},
+            )
+            metrics.incr(
+                "sentry.integrations.slack.tasks.send_notifications_on_activity.send_activity_notifications.failure",
+                sample_rate=1.0,
+            )
 
-    _default_logger.info(
-        "Finished sending notifications for activity", extra={"activity_id": activity_id}
-    )
 
-
-@receiver(post_save, sender=Activity, weak=False)
+@receiver(post_save, sender=Activity)
 def activity_created_receiver(instance, created, **kwargs):
     """
     If an activity is created for an issue, this will trigger, and we can kick off an async process
@@ -45,4 +56,7 @@ def activity_created_receiver(instance, created, **kwargs):
     if not created:
         return
 
-    send_activity_notifications.apply_async(kwargs={"activity_id": instance.id})
+    transaction.on_commit(
+        lambda: send_activity_notifications.apply_async(kwargs={"activity_id": instance.id}),
+        using=router.db_for_read(Activity),
+    )
---
+++ tests/sentry/integrations/slack/tasks/test_send_notifications_on_activity.py
@@ -0,0 +1,64 @@
+from unittest import mock
+
+from sentry.integrations.slack.tasks.send_notifications_on_activity import (
+    activity_created_receiver,
+    send_activity_notifications,
+)
+from sentry.testutils.cases import TestCase
+from sentry.testutils.helpers import with_feature
+
+
+class TestActivityCreatedReceiver(TestCase):
+    def test_ignores_uncreated_events(self) -> None:
+        mock_send_activity_notifications = mock.MagicMock()
+        mock_method = mock.MagicMock()
+        mock_send_activity_notifications.apply_async = mock_method
+        with mock.patch(
+            "sentry.integrations.slack.tasks.send_notifications_on_activity.send_activity_notifications",
+            mock_send_activity_notifications,
+        ):
+            activity_created_receiver({}, False)
+            mock_send_activity_notifications.apply_async.assert_not_called()
+
+    def test_calls_async_function(self) -> None:
+        mock_send_activity_notifications = mock.MagicMock()
+        mock_method = mock.MagicMock()
+        mock_send_activity_notifications.apply_async = mock_method
+        with mock.patch(
+            "sentry.integrations.slack.tasks.send_notifications_on_activity.send_activity_notifications",
+            mock_send_activity_notifications,
+        ):
+            mock_activity = mock.MagicMock()
+            mock_activity.id = 123
+            activity_created_receiver(mock_activity, True)
+            mock_send_activity_notifications.apply_async.assert_called_with(
+                kwargs={"activity_id": mock_activity.id}
+            )
+
+
+class TestSendActivityNotifications(TestCase):
+    def setUp(self) -> None:
+        mock_slack_service = mock.MagicMock()
+        mock_default_method = mock.MagicMock(return_value=mock_slack_service)
+        mock_notify_all_threads_for_activity = mock.MagicMock()
+        mock_slack_service.default = mock_default_method
+        mock_slack_service.notify_all_threads_for_activity = mock_notify_all_threads_for_activity
+
+        self.mock_slack_service = mock_slack_service
+
+    def test_returns_early_when_no_activity_found(self) -> None:
+        with mock.patch(
+            "sentry.integrations.slack.tasks.send_notifications_on_activity.SlackService",
+            self.mock_slack_service,
+        ):
+            send_activity_notifications(activity_id=123)
+            self.mock_slack_service.notify_all_threads_for_activity.assert_not_called()
+
+    @with_feature("organizations:slack-thread-issue-alert")
+    def test_calls_notify_all_threads_for_activity(self) -> None:
+        with mock.patch(
+            "sentry.integrations.slack.tasks.send_notifications_on_activity.SlackService",
+            self.mock_slack_service,
+        ):
+            send_activity_notifications(activity_id=self.activity.id)
+            self.mock_slack_service.notify_all_threads_for_activity.assert_called()
