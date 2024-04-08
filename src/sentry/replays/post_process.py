f3c7e426ba74f5a8870533c5bf995c8aed9b3caf
Andrew Liu <159852527+aliu3ntry@users.noreply.github.com>
2024-04-08 13:42:05 -0700

    feat(replay): add has_viewed to details and index (#68391)
    Relates to https://github.com/getsentry/sentry/issues/64924. Splitting
    https://github.com/getsentry/sentry/pull/67972 into smaller PRs.
    
    ---------
    
    Co-authored-by: Colton Allen <colton.allen@sentry.io>

--- src/sentry/replays/post_process.py
+++ src/sentry/replays/post_process.py
@@ -73,6 +73,7 @@ class ReplayDetailsResponse(TypedDict, total=False):
     info_ids: list[str] | None
     count_warnings: int | None
     count_infos: int | None
+    has_viewed: bool
 
 
 def process_raw_response(
@@ -181,6 +182,8 @@ def generate_normalized_output(
         ret_item["info_ids"] = item.pop("info_ids", None)
         ret_item["count_infos"] = item.pop("count_infos", None)
         ret_item["count_warnings"] = item.pop("count_warnings", None)
+        # Returns a UInt8 of either 0 or 1. We coerce to a bool.
+        ret_item["has_viewed"] = bool(item.get("has_viewed", 0))
         yield ret_item
 
 
