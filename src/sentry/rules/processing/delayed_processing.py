b17c4ef4f9f9286442b6efc12082a2918367f951
schew2381 <seiji.chew@sentry.io>
2024-04-08 10:54:22 -0700

    initial fixes

--- src/sentry/rules/processing/delayed_processing.py
+++ src/sentry/rules/processing/delayed_processing.py
@@ -1,25 +1,24 @@
 import logging
 from collections.abc import Mapping
 
-from sentry.buffer.redis import RedisBuffer
+from sentry.buffer.redis import BufferHookEvent, RedisBuffer, redis_buffer_registry
 from sentry.models.project import Project
 from sentry.utils.safe import safe_execute
 
 logger = logging.getLogger("sentry.rules.delayed_processing")
 
 
-# TODO: Import once available
 PROJECT_ID_BUFFER_LIST_KEY = "project_id_buffer_list"
 
 
-# TODO: Add redis buffer registry decorator
+@redis_buffer_registry.add_handler(BufferHookEvent.FLUSH)
 def process_delayed_alert_conditions(buffer: RedisBuffer) -> None:
-    project_ids = buffer.get_list(PROJECT_ID_BUFFER_LIST_KEY)  # type: ignore[attr-defined]
+    project_ids = buffer.get_set(PROJECT_ID_BUFFER_LIST_KEY)
 
     project_mapping = Project.objects.in_bulk(set(project_ids))
 
     for project_id, project in project_mapping.items():
-        rulegroup_event_mapping: dict[str, int] = buffer.get_queue(Project, {id: project_id})  # type: ignore[attr-defined]
+        rulegroup_event_mapping: dict[str, int] = buffer.get_hash(Project, {id: project_id})
         safe_execute(
             apply_delayed,
             project,
