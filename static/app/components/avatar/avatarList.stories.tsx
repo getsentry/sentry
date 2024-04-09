b95df426d4620ce20d62b5660464a54902443070
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-04-09 12:08:30 -0700

    better stories

--- static/app/components/avatar/avatarList.stories.tsx
+++ static/app/components/avatar/avatarList.stories.tsx
@@ -10,7 +10,7 @@ import {useMembers} from 'sentry/utils/useMembers';
 import {useUserTeams} from 'sentry/utils/useUserTeams';
 
 function useLoadedMembers() {
-  const {members, loadMore, ...rest} = useMembers();
+  const {members, loadMore, ...rest} = useMembers({limit: 50});
 
   useEffect(() => {
     // `loadMore` is not referentially stable, so we cannot include it in the dependencies array
@@ -22,6 +22,19 @@ function useLoadedMembers() {
 
 export default storyBook(AvatarList, story => {
   story('Default', () => {
+    const {members, fetching} = useLoadedMembers();
+
+    return (
+      <Fragment>
+        <p>Your Org Members</p>
+        <SizingWindow display="block" style={{width: '50%'}}>
+          {fetching ? <Placeholder /> : <AvatarList users={members} />}
+        </SizingWindow>
+      </Fragment>
+    );
+  });
+
+  story('Combine users & members', () => {
     const {teams, isLoading} = useUserTeams();
     const {members, fetching} = useLoadedMembers();
 
@@ -30,18 +43,40 @@ export default storyBook(AvatarList, story => {
         <p>
           Your Teams
           <SizingWindow display="block" style={{width: '50%'}}>
+            {isLoading ? <Placeholder /> : <AvatarList teams={teams} />}
+          </SizingWindow>
+        </p>
+
+        <p>
+          Teams and Members together
+          <SizingWindow display="block" style={{width: '50%'}}>
+            {isLoading || fetching ? (
+              <Placeholder />
+            ) : (
+              <AvatarList teams={teams} users={members} maxVisibleAvatars={10} />
+            )}
+          </SizingWindow>
+        </p>
+      </Fragment>
+    );
+  });
+
+  story('typeAvatars', () => {
+    const {teams, isLoading} = useUserTeams();
+    const {members, fetching} = useLoadedMembers();
+
+    return (
+      <Fragment>
+        <p>
+          You can set <JSXProperty name="typeAvatars" value={String} /> at any time, but
+          it's especially important when passing in teams or teams + users together. This
+          is rendered as the suffix to the tooltip on the summary avatar "10 other users"
+          <SizingWindow display="block" style={{width: '50%'}}>
             {isLoading ? (
               <Placeholder />
             ) : (
               <AvatarList teams={teams} typeAvatars="teams" />
             )}
-          </SizingWindow>
-        </p>
-
-        <p>
-          Your Org Members
-          <SizingWindow display="block" style={{width: '50%'}}>
-            {fetching ? <Placeholder /> : <AvatarList users={members} />}
           </SizingWindow>
         </p>
 
