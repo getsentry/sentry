0995ce838043b71736f4d36ff068f3e544fb86cb
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2024-04-09 15:33:49 -0400

    feat(spans): Add DLQ topic for buffered spans

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -3501,6 +3501,7 @@ KAFKA_TOPIC_TO_CLUSTER: Mapping[str, str] = {
     "snuba-spans": "default",
     "shared-resources-usage": "default",
     "buffered-segments": "default",
+    "buffered-segments-dlq": "default",
 }
 
 
