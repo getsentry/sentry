95799844a585d4be5aa2ede8fe333473e40eb3fa
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-04-10 16:38:21 +0200

    ref(profiling): add test to check symbolicated field for javascript frames (#68591)

--- tests/sentry/profiles/test_task.py
+++ tests/sentry/profiles/test_task.py
@@ -11,7 +11,10 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.urls import reverse
 
 from sentry.lang.javascript.processing import _handles_frame as is_valid_javascript_frame
+from sentry.models.files.file import File
 from sentry.models.project import Project
+from sentry.models.release import Release
+from sentry.models.releasefile import ReleaseFile
 from sentry.profiles.task import (
     _calculate_profile_duration_ms,
     _deobfuscate,
@@ -19,6 +22,7 @@ from sentry.profiles.task import (
     _deobfuscate_using_symbolicator,
     _normalize,
     _process_symbolicator_results_for_sample,
+    _symbolicate_profile,
 )
 from sentry.testutils.cases import TransactionTestCase
 from sentry.testutils.factories import Factories, get_fixture_path
@@ -824,3 +828,40 @@ class DeobfuscationViaSymbolicator(TransactionTestCase):
                 "source_line": 40,
             },
         ]
+
+    @requires_symbolicator
+    @pytest.mark.symbolicator
+    def test_js_symbolication_set_symbolicated_field(self):
+        release = Release.objects.create(
+            organization_id=self.project.organization_id, version="nodeprof123"
+        )
+        release.add_project(self.project)
+
+        for file in ["embedded.js", "embedded.js.map"]:
+            with open(get_fixture_path(f"profiles/{file}"), "rb") as f:
+                f1 = File.objects.create(
+                    name=file,
+                    type="release.file",
+                    headers={},
+                )
+                f1.putfile(f)
+
+            ReleaseFile.objects.create(
+                name=f"http://example.com/{f1.name}",
+                release_id=release.id,
+                organization_id=self.project.organization_id,
+                file=f1,
+            )
+
+        js_profile = load_profile("valid_js_profile.json")
+        js_profile.update(
+            {
+                "project_id": self.project.id,
+                "event_id": js_profile["profile_id"],
+                "release": release.version,
+                "debug_meta": {"images": []},
+            }
+        )
+
+        _symbolicate_profile(js_profile, self.project)
+        assert js_profile["profile"]["frames"][0].get("data", {}).get("symbolicated", False)
