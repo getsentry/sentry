9923fb675db6b53ed61476b2dfcac6cd57b5dbaf
Mark Story <mark@mark-story.com>
2024-04-10 11:15:05 -0400

    chore(hybridcloud) Remove TeamAvatar & ProjectAvatar from django state (#68542)
    These models were never used and not having them will reduce complexity
    and junk code. Not having these models also makes changes to other
    Avatars easier.
    
    Refs #68364
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- src/sentry/models/avatars/__init__.py
+++ src/sentry/models/avatars/__init__.py
@@ -2,9 +2,7 @@ from .base import AvatarBase
 from .control_base import ControlAvatarBase
 from .doc_integration_avatar import DocIntegrationAvatar
 from .organization_avatar import OrganizationAvatar
-from .project_avatar import ProjectAvatar
 from .sentry_app_avatar import SentryAppAvatar
-from .team_avatar import TeamAvatar
 from .user_avatar import UserAvatar
 
 __all__ = (
@@ -12,8 +10,6 @@ __all__ = (
     "ControlAvatarBase",
     "DocIntegrationAvatar",
     "OrganizationAvatar",
-    "ProjectAvatar",
     "SentryAppAvatar",
-    "TeamAvatar",
     "UserAvatar",
 )
