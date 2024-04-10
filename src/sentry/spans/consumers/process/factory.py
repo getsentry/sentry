449d476c893202d54063b41edcdc11744bcbe368
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2024-04-10 14:26:53 -0400

    return filtered payload instead of a filter step

--- src/sentry/spans/consumers/process/factory.py
+++ src/sentry/spans/consumers/process/factory.py
@@ -5,11 +5,10 @@ from typing import Any
 
 import sentry_sdk
 from arroyo.backends.kafka.consumer import Headers, KafkaPayload
-from arroyo.commit import ONCE_PER_SECOND
-from arroyo.processing.strategies import FilterStep, RunTask
+from arroyo.processing.strategies import RunTask
 from arroyo.processing.strategies.abstract import ProcessingStrategy, ProcessingStrategyFactory
 from arroyo.processing.strategies.commit import CommitOffsets
-from arroyo.types import BrokerValue, Commit, Message, Partition
+from arroyo.types import FILTERED_PAYLOAD, BrokerValue, Commit, FilteredPayload, Message, Partition
 from sentry_kafka_schemas import get_codec
 from sentry_kafka_schemas.codecs import Codec
 from sentry_kafka_schemas.schema_types.snuba_spans_v1 import SpanEvent
@@ -17,7 +16,6 @@ from sentry_kafka_schemas.schema_types.snuba_spans_v1 import SpanEvent
 from sentry import options
 from sentry.spans.buffer.redis import RedisSpansBuffer
 from sentry.spans.produce_segment import produce_segment_to_kafka
-from sentry.utils import metrics
 from sentry.utils.arroyo import MultiprocessingPool, RunTaskWithMultiprocessing
 
 logger = logging.getLogger(__name__)
@@ -45,23 +43,22 @@ def _deserialize_span(value: bytes) -> Mapping[str, Any]:
     return SPAN_SCHEMA.decode(value)
 
 
-def _process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | None:
+def _process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | FilteredPayload:
     if not options.get("standalone-spans.process-spans-consumer.enable"):
-        return None
+        return FILTERED_PAYLOAD
 
     try:
         project_id = get_project_id(message.payload.headers)
     except Exception:
         logger.exception("Failed to parse span message header")
-        return None
+        return FILTERED_PAYLOAD
 
     if project_id is None or project_id not in options.get(
         "standalone-spans.process-spans-consumer.project-allowlist"
     ):
-        return None
+        return FILTERED_PAYLOAD
 
     assert isinstance(message.value, BrokerValue)
-    metrics.incr("process_spans.spans.received.count")
 
     with sentry_sdk.start_transaction(op="process", name="spans.process.process_message") as txn:
         payload_value = message.payload.value
@@ -73,7 +70,7 @@ def _process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | 
 
         segment_id = span.get("segment_id", None)
         if segment_id is None:
-            return None
+            return FILTERED_PAYLOAD
 
         trace_id = span["trace_id"]
 
@@ -87,19 +84,17 @@ def _process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | 
             project_id, segment_id, timestamp, partition, payload_value
         )
 
-        metrics.incr("process_spans.spans.write.count")
-
     return ProduceSegmentContext(
         should_process_segments=should_process_segments, timestamp=timestamp, partition=partition
     )
 
 
-def process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | None:
+def process_message(message: Message[KafkaPayload]) -> ProduceSegmentContext | FilteredPayload:
     try:
         return _process_message(message)
     except Exception:
         sentry_sdk.capture_exception()
-        return None
+        return FILTERED_PAYLOAD
 
 
 def _produce_segment(message: Message[ProduceSegmentContext]):
@@ -132,7 +127,6 @@ def _produce_segment(message: Message[ProduceSegmentContext]):
                         total_spans_read += len(segment)
                         produce_segment_to_kafka(segment)
 
-            metrics.incr("process_spans.spans.read.count", total_spans_read)
             sentry_sdk.set_context("payload", payload_context)
 
 
@@ -157,7 +151,7 @@ class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
         self.max_batch_time = max_batch_time
         self.input_block_size = input_block_size
         self.output_block_size = output_block_size
-        self.pool = MultiprocessingPool(num_processes)
+        self.__pool = MultiprocessingPool(num_processes)
 
     def create_with_partitions(
         self,
@@ -166,18 +160,16 @@ class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
     ) -> ProcessingStrategy[KafkaPayload]:
 
         next_step = RunTask(function=produce_segment, next_step=CommitOffsets(commit))
-        filter_step = FilterStep(
-            function=lambda msg: bool(msg.payload),
-            next_step=next_step,
-            commit_policy=ONCE_PER_SECOND,
-        )
 
         return RunTaskWithMultiprocessing(
             function=process_message,
-            next_step=filter_step,
+            next_step=next_step,
             max_batch_size=self.max_batch_size,
             max_batch_time=self.max_batch_time,
-            pool=self.pool,
+            pool=self.__pool,
             input_block_size=self.input_block_size,
             output_block_size=self.output_block_size,
         )
+
+    def shutdown(self) -> None:
+        self.__pool.close()
