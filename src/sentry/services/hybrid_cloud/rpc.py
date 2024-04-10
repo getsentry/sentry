a8e473067650b11393c58f126559a376708a66be
Ryan Skonnord <ryan.skonnord@sentry.io>
2024-04-10 14:30:43 -0700

    Convert import in list_all_service_method_signatures to absolute

--- src/sentry/services/hybrid_cloud/rpc.py
+++ src/sentry/services/hybrid_cloud/rpc.py
@@ -376,7 +376,7 @@ class RpcService(abc.ABC):
 def list_all_service_method_signatures() -> Iterable[RpcMethodSignature]:
     """List signatures of all RPC methods in the global registry."""
 
-    from .. import hybrid_cloud as hybrid_cloud_service_pkg
+    from sentry.services import hybrid_cloud as hybrid_cloud_service_pkg
 
     # Forcibly import all service packages to ensure the global registry is fully populated
     for (importer, name, is_pkg) in pkgutil.iter_modules(hybrid_cloud_service_pkg.__path__):
