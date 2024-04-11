63ef99cc2b34d67c0d8b7f94d875421d332622bc
Bartek Ogryczak <bartek.ogryczak@sentry.io>
2024-04-11 11:16:39 -0700

    fixed utils.py

--- src/sentry/grouping/utils.py
+++ src/sentry/grouping/utils.py
@@ -1,41 +1,49 @@
+from __future__ import annotations
+
 import re
 from hashlib import md5
+from typing import TYPE_CHECKING
 
 from django.utils.encoding import force_bytes
 
 from sentry.stacktraces.processing import get_crash_frame_from_event_data
 from sentry.utils.safe import get_path
 
+if TYPE_CHECKING:
+    from sentry.db.models import NodeData
+
 _fingerprint_var_re = re.compile(r"\{\{\s*(\S+)\s*\}\}")
 
 
-def parse_fingerprint_var(value):
+def parse_fingerprint_var(value: str) -> str | None:
     match = _fingerprint_var_re.match(value)
     if match is not None and match.end() == len(value):
         return match.group(1)
+    return None
 
 
-def is_default_fingerprint_var(value):
+def is_default_fingerprint_var(value: str) -> bool:
     return parse_fingerprint_var(value) == "default"
 
 
-def hash_from_values(values):
+def hash_from_values(values: list[str]) -> str:
     result = md5()
     for value in values:
         result.update(force_bytes(value, errors="replace"))
     return result.hexdigest()
 
 
-def get_rule_bool(value):
+def get_rule_bool(value: str) -> bool:
     if value:
         value = value.lower()
         if value in ("1", "yes", "true"):
             return True
         elif value in ("0", "no", "false"):
             return False
+    return False  # explicit default
 
 
-def get_fingerprint_value(var, data):
+def get_fingerprint_value(var: str, data: NodeData) -> str | None:
     if var == "transaction":
         return data.get("transaction") or "<no-transaction>"
     elif var == "message":
@@ -83,10 +91,11 @@ def get_fingerprint_value(var, data):
             if t == tag:
                 return value
         return "<no-value-for-tag-%s>" % tag
+    return None
 
 
-def resolve_fingerprint_values(values, event_data):
-    def _get_fingerprint_value(value):
+def resolve_fingerprint_values(values: list[str], event_data: NodeData) -> list[str | None]:
+    def _get_fingerprint_value(value: str) -> str | None:
         var = parse_fingerprint_var(value)
         if var is None:
             return value
@@ -98,8 +107,8 @@ def resolve_fingerprint_values(values, event_data):
     return [_get_fingerprint_value(x) for x in values]
 
 
-def expand_title_template(template, event_data):
-    def _handle_match(match):
+def expand_title_template(template: str, event_data: NodeData) -> str:
+    def _handle_match(match: re.Match[str]) -> str:
         var = match.group(1)
         rv = get_fingerprint_value(var, event_data)
         if rv is not None:
