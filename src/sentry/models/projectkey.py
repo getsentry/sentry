getsentry/sentry

commit 5b6c4ec63ec1f2ff528feaa21ffcc9b5b536bbf6

feat(project_keys): Expose crons relay API url on key (#68440)
Refs #68237

src/sentry/models/projectkey.py
@@ -259,6 +259,10 @@ class ProjectKey(Model):
         return f"{self.get_endpoint()}/api/{self.project_id}/unreal/{self.public_key}/"
 
     @property
+    def crons_endpoint(self):
+        return f"{self.get_endpoint()}/api/{self.project_id}/crons/___MONITOR_SLUG___/{self.public_key}/"
+
+    @property
     def js_sdk_loader_cdn_url(self) -> str:
         if settings.JS_SDK_LOADER_CDN_URL:
             return f"{settings.JS_SDK_LOADER_CDN_URL}{self.public_key}.min.js"

tests/sentry/models/test_projectkey.py
@@ -103,6 +103,10 @@ class ProjectKeyTest(TestCase):
                 == f"http://testserver/api/{self.project.id}/minidump/?sentry_key=abc"
             )
             assert key.unreal_endpoint == f"http://testserver/api/{self.project.id}/unreal/abc/"
+            assert (
+                key.crons_endpoint
+                == f"http://testserver/api/{self.project.id}/crons/___MONITOR_SLUG___/abc/"
+            )
             assert key.js_sdk_loader_cdn_url == "http://testserver/js-sdk-loader/abc.min.js"
 
     def test_get_dsn_org_subdomain(self):
@@ -123,6 +127,10 @@ class ProjectKeyTest(TestCase):
                 == f"http://{host}/api/{self.project.id}/minidump/?sentry_key=abc"
             )
             assert key.unreal_endpoint == f"http://{host}/api/{self.project.id}/unreal/abc/"
+            assert (
+                key.crons_endpoint
+                == f"http://{host}/api/{self.project.id}/crons/___MONITOR_SLUG___/abc/"
+            )
 
     @override_settings(SENTRY_REGION="us")
     def test_get_dsn_multiregion(self):
@@ -136,6 +144,10 @@ class ProjectKeyTest(TestCase):
             key.minidump_endpoint == f"http://{host}/api/{self.project.id}/minidump/?sentry_key=abc"
         )
         assert key.unreal_endpoint == f"http://{host}/api/{self.project.id}/unreal/abc/"
+        assert (
+            key.crons_endpoint
+            == f"http://{host}/api/{self.project.id}/crons/___MONITOR_SLUG___/abc/"
+        )
 
     @override_settings(SENTRY_REGION="us")
     def test_get_dsn_org_subdomain_and_multiregion(self):
@@ -156,6 +168,10 @@ class ProjectKeyTest(TestCase):
                 == f"http://{host}/api/{self.project.id}/minidump/?sentry_key=abc"
             )
             assert key.unreal_endpoint == f"http://{host}/api/{self.project.id}/unreal/abc/"
+            assert (
+                key.crons_endpoint
+                == f"http://{host}/api/{self.project.id}/crons/___MONITOR_SLUG___/abc/"
+            )
 
 
 @mock.patch("sentry.models.projectkey.schedule_invalidate_project_config")

src/sentry/api/serializers/models/project_key.py
@@ -25,6 +25,7 @@ class DSN(TypedDict):
     minidump: str
     nel: str
     unreal: str
+    crons: str
     cdn: str
 
 
@@ -91,6 +92,7 @@ class ProjectKeySerializer(Serializer):
                 "minidump": obj.minidump_endpoint,
                 "nel": obj.nel_endpoint,
                 "unreal": obj.unreal_endpoint,
+                "crons": obj.crons_endpoint,
                 "cdn": obj.js_sdk_loader_cdn_url,
             },
             "browserSdkVersion": get_selected_browser_sdk_version(obj),

src/sentry/apidocs/examples/project_examples.py
@@ -18,6 +18,7 @@ KEY_RATE_LIMIT = {
         "nel": "https://o4504765715316736.ingest.sentry.io/api/4505281256090153/nel/?sentry_key=a785682ddda719b7a8a4011110d75598",
         "unreal": "https://o4504765715316736.ingest.sentry.io/api/4505281256090153/unreal/a785682ddda719b7a8a4011110d75598/",
         "cdn": "https://js.sentry-cdn.com/a785682ddda719b7a8a4011110d75598.min.js",
+        "crons": "https://o4504765715316736.ingest.sentry.io/api/4505281256090153/crons/___MONITOR_SLUG___/a785682ddda719b7a8a4011110d75598/",
     },
     "browserSdkVersion": "7.x",
     "browserSdk": {"choices": [["latest", "latest"], ["7.x", "7.x"]]},

commit 761a0ae8497b1bf302111f6756231142fe854f94

ref(crons): Correct crons_endpoint URL (#68702)
Fixes: GH-68622

This was regressed in 5b6c4ec63ec1f2ff528feaa21ffcc9b5b536bbf6

src/sentry/models/projectkey.py
@@ -260,7 +260,7 @@ class ProjectKey(Model):
 
     @property
     def crons_endpoint(self):
-        return f"{self.get_endpoint()}/api/{self.project_id}/crons/___MONITOR_SLUG___/{self.public_key}/"
+        return f"{self.get_endpoint()}/api/{self.project_id}/cron/___MONITOR_SLUG___/{self.public_key}/"
 
     @property
     def js_sdk_loader_cdn_url(self) -> str:

tests/sentry/models/test_projectkey.py
@@ -105,7 +105,7 @@ class ProjectKeyTest(TestCase):
             assert key.unreal_endpoint == f"http://testserver/api/{self.project.id}/unreal/abc/"
             assert (
                 key.crons_endpoint
-                == f"http://testserver/api/{self.project.id}/crons/___MONITOR_SLUG___/abc/"
+                == f"http://testserver/api/{self.project.id}/cron/___MONITOR_SLUG___/abc/"
             )
             assert key.js_sdk_loader_cdn_url == "http://testserver/js-sdk-loader/abc.min.js"
 
@@ -129,7 +129,7 @@ class ProjectKeyTest(TestCase):
             assert key.unreal_endpoint == f"http://{host}/api/{self.project.id}/unreal/abc/"
             assert (
                 key.crons_endpoint
-                == f"http://{host}/api/{self.project.id}/crons/___MONITOR_SLUG___/abc/"
+                == f"http://{host}/api/{self.project.id}/cron/___MONITOR_SLUG___/abc/"
             )
 
     @override_settings(SENTRY_REGION="us")
@@ -146,7 +146,7 @@ class ProjectKeyTest(TestCase):
         assert key.unreal_endpoint == f"http://{host}/api/{self.project.id}/unreal/abc/"
         assert (
             key.crons_endpoint
-            == f"http://{host}/api/{self.project.id}/crons/___MONITOR_SLUG___/abc/"
+            == f"http://{host}/api/{self.project.id}/cron/___MONITOR_SLUG___/abc/"
         )
 
     @override_settings(SENTRY_REGION="us")
@@ -170,7 +170,7 @@ class ProjectKeyTest(TestCase):
             assert key.unreal_endpoint == f"http://{host}/api/{self.project.id}/unreal/abc/"
             assert (
                 key.crons_endpoint
-                == f"http://{host}/api/{self.project.id}/crons/___MONITOR_SLUG___/abc/"
+                == f"http://{host}/api/{self.project.id}/cron/___MONITOR_SLUG___/abc/"
             )
 
 