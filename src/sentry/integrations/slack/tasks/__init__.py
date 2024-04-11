b947ada1064ab878bc5681ad9b178aae874325e1
Yash Kamothi <yash.kamothi@gmail.com>
2024-04-11 10:16:19 -0700

    fix(celery): Expose task correctly so it gets picked up (#68667)
    We were not correctly exposing the celery task, which led to a key error
    
    Resolves:
    https://sentry.sentry.io/issues/4271076192/?project=1&query=is%3Aunresolved+issue.priority%3A%5Bhigh%2C+medium%5D+send_activity_notifications_to_slack_threads&referrer=issue-stream&statsPeriod=14d&stream_index=0

--- src/sentry/integrations/slack/tasks/__init__.py
+++ src/sentry/integrations/slack/tasks/__init__.py
@@ -1,3 +1,3 @@
-from sentry.integrations.slack.tasks.send_notifications_on_activity import (  # noqa
-    send_activity_notifications_to_slack_threads,
-)
+from .send_notifications_on_activity import send_activity_notifications_to_slack_threads
+
+__all__ = ("send_activity_notifications_to_slack_threads",)
