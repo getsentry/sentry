fcfe7456567c42d76348f600b57102a5620a2d0d
Jonas <jonas.badalic@sentry.io>
2024-04-11 14:31:07 -0400

    feat(trace): bring back profiling visualization for missing instrumentation spans (#68697)
    ![CleanShot 2024-04-11 at 10 45
    22@2x](https://github.com/getsentry/sentry/assets/9317857/45ae5a9c-d08a-4451-8b2e-8dc3313328f5)

--- static/app/views/performance/newTraceDetails/traceDrawer/details/span.tsx
+++ static/app/views/performance/newTraceDetails/traceDrawer/details/span.tsx
@@ -1,3 +1,5 @@
+import {useMemo} from 'react';
+
 import {Button} from 'sentry/components/button';
 import NewTraceDetailsSpanDetail from 'sentry/components/events/interfaces/spans/newTraceDetailsSpanDetails';
 import {
@@ -26,9 +28,13 @@ export function SpanNodeDetails({
 }: TraceTreeNodeDetailsProps<TraceTreeNode<TraceTree.Span>>) {
   const {projects} = useProjects();
   const span = node.value;
-  const {event, childTransaction} = node.value;
+  const {event} = node.value;
   const project = projects.find(proj => proj.slug === event?.projectSlug);
   const profileId = event?.contexts?.profile?.profile_id ?? null;
+
+  const childTransactions = useMemo(() => {
+    return node.value.childTransaction ? [node.value.childTransaction] : [];
+  }, [node.value.childTransaction]);
 
   return (
     <TraceDrawerComponents.DetailContainer>
@@ -45,7 +51,7 @@ export function SpanNodeDetails({
             <div>{t('span')}</div>
             <TraceDrawerComponents.TitleOp>
               {' '}
-              {getSpanOperation(span) + ' - ' + span.description ?? span.span_id}
+              {getSpanOperation(span) + ' - ' + (span.description ?? span.span_id)}
             </TraceDrawerComponents.TitleOp>
           </TraceDrawerComponents.TitleText>
         </TraceDrawerComponents.Title>
@@ -70,12 +76,12 @@ export function SpanNodeDetails({
                 traceID={profileId || ''}
               >
                 <NewTraceDetailsSpanDetail
+                  span={span}
                   node={node}
-                  childTransactions={childTransaction ? [childTransaction] : []}
                   event={event}
                   openPanel="open"
                   organization={organization}
-                  span={span}
+                  childTransactions={childTransactions}
                   trace={parseTrace(event)}
                   onParentClick={onParentClick}
                 />
