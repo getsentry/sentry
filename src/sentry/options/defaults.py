cdbd524304f1384aed550e3c999e59189b9cf924
Colton Allen <colton.allen@sentry.io>
2024-04-12 10:10:05 -0500

    Rename mobile to video

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -451,7 +451,7 @@ register(
     flags=FLAG_ALLOW_EMPTY | FLAG_PRIORITIZE_DISK | FLAG_AUTOMATOR_MODIFIABLE,
 )
 register(
-    "replay.organizations.mobile-slug-denylist",
+    "replay.organizations.video-slug-denylist",
     type=Sequence,
     default=[],
     flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
