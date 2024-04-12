4fee91202086daf02aa61862ea9c24a64081b6a8
Colton Allen <colton.allen@sentry.io>
2024-04-12 11:23:01 -0500

    feat(replays): Add killswitch for mobile replays (#68567)
    Allows all orgs unless they're explicitly blocked.
    
    Related: https://github.com/getsentry/sentry/issues/63255

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -450,6 +450,12 @@ register(
     default=True,
     flags=FLAG_ALLOW_EMPTY | FLAG_PRIORITIZE_DISK | FLAG_AUTOMATOR_MODIFIABLE,
 )
+register(
+    "replay.organizations.video-slug-denylist",
+    type=Sequence,
+    default=[],
+    flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
+)
 
 # User Feedback Options
 register(
