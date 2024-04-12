8ccc13c861110aaf83cda27863e78dc0d3fa9db3
Simon Hellmayr <simon.hellmayr@sentry.io>
2024-04-12 15:19:33 +0200

    wip: implement first running version of modulator-visitor based on flexibly defined modulators

--- src/sentry/sentry_metrics/querying/visitors/modulator.py
+++ src/sentry/sentry_metrics/querying/visitors/modulator.py
@@ -1,17 +1,22 @@
-import abc
 from collections.abc import Sequence
 
 from snuba_sdk import Formula
 
 from sentry.models.project import Project
-from sentry.sentry_metrics.querying.visitors.query_modulator import ModulationMetadata
 
 
-class Modulator(abc.ABC):
-    def __init__(self, from_key: str, to_key: str, from_value: str):
+class ModulationMetadata:
+    def __init__(self, from_key: str, to_key: str, from_value: str, to_value: str):
         self.from_key = from_key
         self.to_key = to_key
         self.from_value = from_value
+        self.to_value = to_value
+
+
+class Modulator:
+    def __init__(self, from_key: str, to_key: str):
+        self.from_key = from_key
+        self.to_key = to_key
 
     def modulate(
         self, formula: Formula, projects: Sequence[Project]
