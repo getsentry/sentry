6d65e306c093cd1caf6e95c8b117759ad932bd77
Riccardo Busetti <riccardo.busetti@sentry.io>
2024-04-15 16:34:29 +0200

    Fix

--- src/sentry/sentry_metrics/querying/data/query.py
+++ src/sentry/sentry_metrics/querying/data/query.py
@@ -1,4 +1,3 @@
-import re
 from collections.abc import Mapping
 from dataclasses import dataclass
 from typing import Any, cast
@@ -61,10 +60,8 @@ class MQLQuery:
         # We sort query names by length and content with the goal of trying to always match the longest queries first.
         sorted_query_names = sorted(sub_queries.keys(), key=lambda q: (len(q), q), reverse=True)
         for query_name in sorted_query_names:
-            # We pass a lambda to the sub, because of how escaping is handled by `re.sub`. More information
-            # can be found at https://stackoverflow.com/questions/280435/escaping-regex-string.
-            replaced_mql_formula = re.sub(
-                rf"\${query_name}", lambda _: sub_queries[query_name].mql, replaced_mql_formula
+            replaced_mql_formula = replaced_mql_formula.replace(
+                f"${query_name}", sub_queries[query_name].mql
             )
 
         return MQLQuery(mql=replaced_mql_formula, order=self.order, limit=self.limit)
