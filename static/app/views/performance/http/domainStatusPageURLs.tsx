bd3a24b5cc55c8009955bd2a969278830f5f4150
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-04-15 17:06:00 -0400

    feat(perf): Use `@sentry/status-page-list` for domain status link (#68899)
    Use
    [`@sentry/domain-status-list`](https://github.com/getsentry/status-page-list),
    which is where we're going to keep a domain --> status page lookup. Load
    the library async when the component is rendered so we don't increase
    everyone's bundle. Right now it's about 14KB.
    
    - Add package
    - Use `status-page-list` library

--- static/app/views/performance/http/domainStatusPageURLs.tsx
+++
@@ -1,8 +0,0 @@
-export const DOMAIN_STATUS_PAGE_URLS = {
-  '*.github.com': 'https://www.githubstatus.com',
-  '*.slack.com': 'https://slack-status.com',
-  '*.atlassian.net': 'https://status.atlassian.com/api',
-  '*.googleapis.com': 'https://status.cloud.google.com',
-  '*.codecov.io': 'https://status.codecov.com',
-  '*.sentry.io': 'https://status.sentry.io',
-};
