1c087cd2ccbd4c2d1bd09b67cffef2934c7a76a4
Malachi Willey <malwilley@gmail.com>
2024-04-15 12:40:32 -0700

    feat(query-builder): Add new basic search component

---
+++ static/app/components/searchQueryBuilder/useQueryBuilderState.tsx
@@ -0,0 +1,56 @@
+import {type Reducer, useCallback, useMemo, useReducer} from 'react';
+
+import type {
+  ParseResultToken,
+  Token,
+  TokenResult,
+} from 'sentry/components/searchSyntax/parser';
+
+type QueryBuilderState = {
+  focus: null; // TODO(malwilley): Implement focus state
+  query: string;
+};
+
+type DeleteTokenAction = {
+  token: ParseResultToken;
+  type: 'DELETE_TOKEN';
+};
+
+export type QueryBuilderActions = DeleteTokenAction;
+
+function removeQueryToken(query: string, token: TokenResult<Token>): string {
+  return (
+    query.substring(0, token.location.start.offset) +
+    query.substring(token.location.end.offset)
+  );
+}
+
+export function useQueryBuilderState({initialQuery}: {initialQuery: string}) {
+  const initialState: QueryBuilderState = {query: initialQuery, focus: null};
+
+  const reducer: Reducer<QueryBuilderState, QueryBuilderActions> = useCallback(
+    (state, action): QueryBuilderState => {
+      switch (action.type) {
+        case 'DELETE_TOKEN':
+          return {
+            ...state,
+            query: removeQueryToken(state.query, action.token),
+            focus: null,
+          };
+        default:
+          return state;
+      }
+    },
+    []
+  );
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return useMemo(
+    () => ({
+      state,
+      dispatch,
+    }),
+    [state, dispatch]
+  );
+}
