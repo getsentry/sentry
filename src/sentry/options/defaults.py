bd852908a884fc0f472f2c8d6f3414fecf568ffd
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-04-16 09:53:32 -0700

    fix(hc): Adds fix for evaluation context builder error handler, adds feature option

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -1780,6 +1780,7 @@ register(
 register("hybrid_cloud.multi-region-selector", default=False, flags=FLAG_AUTOMATOR_MODIFIABLE)
 register("hybrid_cloud.region-domain-allow-list", default=[], flags=FLAG_AUTOMATOR_MODIFIABLE)
 register("hybrid_cloud.region-user-allow-list", default=[], flags=FLAG_AUTOMATOR_MODIFIABLE)
+register("features", default={}, flags=FLAG_AUTOMATOR_MODIFIABLE)
 
 register(
     "hybrid_cloud.use_region_specific_upload_url", default=True, flags=FLAG_AUTOMATOR_MODIFIABLE
