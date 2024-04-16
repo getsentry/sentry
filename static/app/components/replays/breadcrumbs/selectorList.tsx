4320bc14ff96e92b0dd6c73d0623a61d349df887
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-04-16 15:04:37 -0700

    fix(replay): guard against experimental frames in breadcrumbs

--- static/app/components/replays/breadcrumbs/selectorList.tsx
+++ static/app/components/replays/breadcrumbs/selectorList.tsx
@@ -13,8 +13,16 @@ export default function SelectorList({frame}: {frame: ClickFrame}) {
   const organization = useOrganization();
 
   const componentName = frame.data.node?.attributes['data-sentry-component'];
+
+  // Guard against experimental frames for mobile replays
+  // TODO: can probably remove this check when mobile replay frames are
+  // more complete
   const lastComponentIndex =
-    frame.message.lastIndexOf('>') === -1 ? 0 : frame.message.lastIndexOf('>') + 2;
+    'message' in frame
+      ? frame.message?.lastIndexOf('>') === -1
+        ? 0
+        : frame.message?.lastIndexOf('>') + 2
+      : 0;
 
   return componentName ? (
     <Fragment>
