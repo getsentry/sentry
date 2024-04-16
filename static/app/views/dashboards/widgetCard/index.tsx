5bbf1350dbd8e48f1b053ba598951b0c6c7a8efa
Scott Cooper <scttcper@gmail.com>
2024-04-16 09:21:06 -0700

    Revert "fix(dashboards): dashboard widget height (#68966)"
    This reverts commit be24d6746e5560f900acd6a2932bd1babe922dd6.

--- static/app/views/dashboards/widgetCard/index.tsx
+++ static/app/views/dashboards/widgetCard/index.tsx
@@ -1,5 +1,5 @@
 import {Component, Fragment} from 'react';
-// import LazyLoad from 'react-lazyload';
+import LazyLoad from 'react-lazyload';
 import type {WithRouterProps} from 'react-router';
 import type {useSortable} from '@dnd-kit/sortable';
 import styled from '@emotion/styled';
@@ -226,7 +226,7 @@ class WidgetCard extends Component<Props, State> {
       renderErrorMessage,
       tableItemLimit,
       windowWidth,
-      // noLazyLoad,
+      noLazyLoad,
       showStoredAlert,
       noDashboardsMEPProvider,
       dashboardFilters,
@@ -349,8 +349,7 @@ class WidgetCard extends Component<Props, State> {
                       <IconWarning color="gray500" size="lg" />
                     </StyledErrorPanel>
                   </Fragment>
-                ) : (
-                  // noLazyLoad ?
+                ) : noLazyLoad ? (
                   <WidgetCardChartContainer
                     location={location}
                     api={api}
@@ -365,28 +364,23 @@ class WidgetCard extends Component<Props, State> {
                     dashboardFilters={dashboardFilters}
                     chartGroup={DASHBOARD_CHART_GROUP}
                   />
-                  // )
-                  // : (
-                  //   <LazyLoad
-                  //     once
-                  //     resize
-                  //     height={200}
-                  //   >
-                  //     <WidgetCardChartContainer
-                  //       location={location}
-                  //       api={api}
-                  //       organization={organization}
-                  //       selection={selection}
-                  //       widget={widget}
-                  //       isMobile={isMobile}
-                  //       renderErrorMessage={renderErrorMessage}
-                  //       tableItemLimit={tableItemLimit}
-                  //       windowWidth={windowWidth}
-                  //       onDataFetched={this.setData}
-                  //       dashboardFilters={dashboardFilters}
-                  //       chartGroup={DASHBOARD_CHART_GROUP}
-                  //     />
-                  //   </LazyLoad>
+                ) : (
+                  <LazyLoad once resize height={200}>
+                    <WidgetCardChartContainer
+                      location={location}
+                      api={api}
+                      organization={organization}
+                      selection={selection}
+                      widget={widget}
+                      isMobile={isMobile}
+                      renderErrorMessage={renderErrorMessage}
+                      tableItemLimit={tableItemLimit}
+                      windowWidth={windowWidth}
+                      onDataFetched={this.setData}
+                      dashboardFilters={dashboardFilters}
+                      chartGroup={DASHBOARD_CHART_GROUP}
+                    />
+                  </LazyLoad>
                 )}
                 {this.renderToolbar()}
               </WidgetCardPanel>
