2e1429f02cb0be8056505ff31a6ea2fc8dfa7b01
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-04-16 14:14:42 -0700

    ref(badges): Remove shim files for badges that were not in components/badges/* (#68923)
    In #68843 we moved a bunch of files around, and then
    getsentry/getsentry#13646 needed to follow suit.
    This cleans up the leftover stubs which we needed so build don't break
    along the way.
    
    Follows: https://github.com/getsentry/sentry/pull/68843
    Depends on https://github.com/getsentry/getsentry/pull/13646

--- static/app/components/tag.tsx
+++
@@ -1,8 +0,0 @@
-import Tag from 'sentry/components/badge/tag';
-
-/**
- * @deprecated Import from `sentry/components/badge/tag` instead
- *
- * TODO(ryan953): remove this shim once getsentry imports are updated
- */
-export {Tag};
