696c889c65c7b292d1be09edfa491ab25639a4f8
David Herberth <david.herberth@sentry.io>
2024-04-16 12:45:58 +0200

    feat(metric-stats): Add cardinality.{limit,scope} tags to strings indexer (#68891)
    Adds `cardinality.scope` and `cardinality.limit` tags to strings
    indexer, to allow manual queries via cardinality analyzer spanning
    multiple/all orgs with a fixed metric id/tag id.
    
    The tag is unused in all indexer tables.

--- src/sentry/sentry_metrics/indexer/strings.py
+++ src/sentry/sentry_metrics/indexer/strings.py
@@ -198,6 +198,8 @@ SHARED_TAG_STRINGS = {
     "outcome.id": PREFIX + 276,
     "outcome.reason": PREFIX + 277,
     "cardinality.window": PREFIX + 278,
+    "cardinality.limit": PREFIX + 279,
+    "cardinality.scope": PREFIX + 280,
     # GENERAL/MISC (don't have a category)
     "": PREFIX + 1000,
 }
