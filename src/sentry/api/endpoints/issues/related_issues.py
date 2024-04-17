c52fc6b1101df687eafff7486c92a8a46a1b434e
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-04-17 16:41:33 -0400

    Add limit and window

--- src/sentry/api/endpoints/issues/related_issues.py
+++ src/sentry/api/endpoints/issues/related_issues.py
@@ -17,9 +17,9 @@ class RelatedIssuesEndpoint(GroupEndpoint):
     enforce_rate_limit = True
     rate_limits = {
         "GET": {
-            RateLimitCategory.IP: RateLimit(15, 1),
-            RateLimitCategory.USER: RateLimit(15, 1),
-            RateLimitCategory.ORGANIZATION: RateLimit(15, 1),
+            RateLimitCategory.IP: RateLimit(limit=15, window=5),
+            RateLimitCategory.USER: RateLimit(limit=15, window=5),
+            RateLimitCategory.ORGANIZATION: RateLimit(limit=15, window=1),
         }
     }
 
