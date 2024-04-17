266b98bac6684fac49dc0a36de4bc6a19d663395
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2024-04-17 16:41:50 +0200

    enhance(profiling): add a deny list for projects we want to stop ingesting profiles function metrics into the generic metric platform (#68978)

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -2368,6 +2368,15 @@ register(
     flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
 )
 
+# list of project IDs we want to deny ingesting profiles
+# function metrics into the generic metrics platform
+register(
+    "profiling.generic_metrics.functions_ingestion.denied_proj_ids",
+    type=Sequence,
+    default=[],
+    flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
+)
+
 # Standalone spans
 register(
     "standalone-spans.process-spans-consumer.enable",
