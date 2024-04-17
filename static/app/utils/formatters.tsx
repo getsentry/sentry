d6d72f719857a0bfe8eed076bad3bb6ef2e4defe
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-04-17 17:34:27 -0400

    `formatPercentage` `minimumValue` option
    Just like `formatRate`!

--- static/app/utils/formatters.tsx
+++ static/app/utils/formatters.tsx
@@ -373,10 +373,23 @@ export function formatFloat(number: number, places: number) {
 /**
  * Format a value between 0 and 1 as a percentage
  */
-export function formatPercentage(value: number, places: number = 2) {
+export function formatPercentage(
+  value: number,
+  places: number = 2,
+  options: {
+    minimumValue?: number;
+  } = {}
+) {
   if (value === 0) {
     return '0%';
   }
+
+  const minimumValue = options.minimumValue ?? 0;
+
+  if (value <= minimumValue) {
+    return `<${minimumValue}%`;
+  }
+
   return (
     round(value * 100, places).toLocaleString(undefined, {
       maximumFractionDigits: places,
