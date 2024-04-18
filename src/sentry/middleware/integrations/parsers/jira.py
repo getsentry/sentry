fe7cd940d369573b46b751339de3ee3506728d43
Mark Story <mark@mark-story.com>
2024-04-18 16:23:07 -0400

    chore(hybridcloud) Cleanup logging and option usage in webhooks (#69192)
    With multi-org webhook handling fixed, I don't need all this logging.
    The parallel delivery can be used more frequently to help deliver small
    backlogs faster.
    
    I've also simplified the parser interface as we had two very similar
    methods and no way to fully remove usage of the non-integration focused
    one. I think having fewer methods makes the design simpler.

--- src/sentry/middleware/integrations/parsers/jira.py
+++ src/sentry/middleware/integrations/parsers/jira.py
@@ -81,8 +81,8 @@ class JiraRequestParser(BaseRequestParser):
                 return self.get_response_from_control_silo()
 
         if self.view_class in self.outbox_response_region_classes:
-            return self.get_response_from_webhookpayload_for_integration(
-                regions=regions, integration=integration
+            return self.get_response_from_webhookpayload(
+                regions=regions, identifier=integration.id, integration_id=integration.id
             )
 
         return self.get_response_from_control_silo()
