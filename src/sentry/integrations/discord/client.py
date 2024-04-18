77023c878e02a510630c0c145d4e3d7d4de68106
Yash Kamothi <yash.kamothi@gmail.com>
2024-04-18 16:22:14 -0700

    feat(slack): Update and add some metrics

--- src/sentry/integrations/discord/client.py
+++ src/sentry/integrations/discord/client.py
@@ -47,6 +47,7 @@ class DiscordClient(ApiClient):
     _METRICS_FAILURE_KEY: str = "sentry.integrations.discord.failure"
     _METRICS_SUCCESS_KEY: str = "sentry.integrations.discord.success"
     _METRICS_USER_ERROR_KEY: str = "sentry.integrations.discord.failure.user_error"
+    _METRICS_RATE_LIMIT_KEY: str = "sentry.integrations.discord.failure.rate_limit"
 
     def __init__(self):
         super().__init__()
@@ -147,12 +148,13 @@ class DiscordClient(ApiClient):
         }
 
         if not is_ok or error:
-            self._handle_failure(log_params=log_params, resp=resp)
+            self._handle_failure(code=int(code), log_params=log_params, resp=resp)
         else:
             self._handle_success(log_params=log_params)
 
     def _handle_failure(
         self,
+        code: int,
         log_params: dict[str, Any],
         resp: Response | None = None,
     ) -> None:
@@ -187,7 +189,15 @@ class DiscordClient(ApiClient):
         is_user_error = discord_error_code in DISCORD_USER_ERRORS
         log_params["is_user_error"] = is_user_error
 
-        metrics_key = self._METRICS_USER_ERROR_KEY if is_user_error else self._METRICS_FAILURE_KEY
+        if is_user_error:
+            metrics_key = self._METRICS_USER_ERROR_KEY
+        else:
+            metrics_key = (
+                self._METRICS_RATE_LIMIT_KEY
+                if code == status.HTTP_429_TOO_MANY_REQUESTS
+                else self._METRICS_FAILURE_KEY
+            )
+
         metrics.incr(
             metrics_key,
             sample_rate=1.0,
