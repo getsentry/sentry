da847358c5e70b5ad9178c428693da9dd355f1b2
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-04-22 14:13:56 +0200

    fix(metrics): fix UTC date format

--- static/app/views/metrics/chart/chart.tsx
+++ static/app/views/metrics/chart/chart.tsx
@@ -155,6 +155,7 @@ export const MetricChart = memo(
           showTimeInTooltip: true,
           addSecondsToTimeFormat: isSubMinuteBucket,
           limit: 10,
+          utc: !!dateTimeOptions.utc,
           filter: (_, seriesParam) => {
             return seriesParam?.axisId === MAIN_X_AXIS_ID;
           },
@@ -221,10 +222,12 @@ export const MetricChart = memo(
                   return true;
                 });
 
-                const date = defaultFormatAxisLabel(
+                const date = params[0].value[0];
+
+                defaultFormatAxisLabel(
                   params[0].value[0] as number,
                   timeseriesFormatters.isGroupedByDate,
-                  false,
+                  timeseriesFormatters.utc,
                   timeseriesFormatters.showTimeInTooltip,
                   timeseriesFormatters.addSecondsToTimeFormat,
                   timeseriesFormatters.bucketSize
