cb8df946b7c57ce0e49c18715d8e77e48d04db27
Sebastian Zivota <loewenheim@mailbox.org>
2024-04-22 16:10:03 +0200

    options_load

--- src/sentry/integrations/slack/requests/options_load.py
+++ src/sentry/integrations/slack/requests/options_load.py
@@ -2,13 +2,26 @@ from __future__ import annotations
 
 from typing import Any
 
+import orjson
+import sentry_sdk
 from rest_framework import status
 
+from sentry.features.rollout import in_random_rollout
 from sentry.integrations.slack.requests.base import SlackRequest, SlackRequestError
 from sentry.models.group import Group
 from sentry.utils import json
+from sentry.utils.json import JSONData
 
 VALID_PAYLOAD_TYPES = ["block_suggestion"]
+
+
+# TODO: remove this once we're confident that orjson is working as expected
+def load_json(data: Any) -> JSONData:
+    if in_random_rollout("integrations.slack.enable-orjson"):
+        # Span is required because `json.loads` calls it by default
+        with sentry_sdk.start_span(op="sentry.utils.json.loads"):
+            return orjson.loads(data)
+    return json.loads(data)
 
 
 class SlackOptionsLoadRequest(SlackRequest):
@@ -19,8 +32,8 @@ class SlackOptionsLoadRequest(SlackRequest):
     @property
     def group_id(self) -> int:
         if self.data.get("container", {}).get("is_app_unfurl"):
-            return int(json.loads(self.data["app_unfurl"]["blocks"][0]["block_id"])["issue"])
-        return int(json.loads(self.data["message"]["blocks"][0]["block_id"])["issue"])
+            return int(load_json(self.data["app_unfurl"]["blocks"][0]["block_id"])["issue"])
+        return int(load_json(self.data["message"]["blocks"][0]["block_id"])["issue"])
 
     @property
     def substring(self) -> str:
@@ -33,7 +46,7 @@ class SlackOptionsLoadRequest(SlackRequest):
             raise SlackRequestError(status=status.HTTP_400_BAD_REQUEST)
 
         try:
-            self._data = json.loads(self.data["payload"])
+            self._data = load_json(self.data["payload"])
         except (KeyError, IndexError, TypeError, ValueError):
             raise SlackRequestError(status=status.HTTP_400_BAD_REQUEST)
 
