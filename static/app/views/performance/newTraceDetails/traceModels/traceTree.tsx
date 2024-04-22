c4f9e44fbccd1de58bc753784152403cae803a9a
Abdkhan14 <60121741+Abdkhan14@users.noreply.github.com>
2024-04-22 16:58:57 -0400

    fix(new-trace): Adding span self time and total time duration comparisons to span node details. (#69339)
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/performance/newTraceDetails/traceModels/traceTree.tsx
+++ static/app/views/performance/newTraceDetails/traceModels/traceTree.tsx
@@ -188,7 +188,7 @@ function fetchTransactionSpans(
   event_id: string
 ): Promise<EventTransaction> {
   return api.requestPromise(
-    `/organizations/${organization.slug}/events/${project_slug}:${event_id}/?averageColumn=span.self_time`
+    `/organizations/${organization.slug}/events/${project_slug}:${event_id}/?averageColumn=span.self_time&averageColumn=span.duration`
   );
 }
 
