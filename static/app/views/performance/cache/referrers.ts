299972394b77d7816b00cf5b102af1932ceb2f46
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-04-22 16:07:38 -0400

    add cache miss rate chart

---
+++ static/app/views/performance/cache/referrers.ts
@@ -0,0 +1,11 @@
+const referrerPrefix = 'api.performance.cache';
+
+enum ReferrerSuffix {
+  LANDING_CACHE_HIT_MISS_CHART = `landing-cache-hit-miss-chart`,
+}
+
+type PrefixedReferrer = {
+  [key in keyof typeof ReferrerSuffix]: `${typeof referrerPrefix}.${(typeof ReferrerSuffix)[key]}`;
+};
+
+export const Referrer: PrefixedReferrer = ReferrerSuffix as unknown as PrefixedReferrer;
