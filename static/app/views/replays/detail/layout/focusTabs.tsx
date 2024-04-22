80fff41006e75eccde00c32bd51933d4c6b9c65b
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-04-22 10:09:11 -0700

    fix

--- static/app/views/replays/detail/layout/focusTabs.tsx
+++ static/app/views/replays/detail/layout/focusTabs.tsx
@@ -23,13 +23,15 @@ function getReplayTabs({
   organization: Organization;
 }): Record<TabKey, ReactNode> {
   // For video replays, we hide the console, a11y, trace, and memory tabs
-  // The console tab isn't useful for video replays; most of the useful logging
-  // context will come from breadcrumbs
+  // The console tab isn't useful for video replays (for now); most of the
+  // useful logging context will come from breadcrumbs
   // A11y, trace, and memory aren't applicable for mobile
 
-  const showConsoleTab = organization.features.includes(
-    'session-replay-mobile-network-tab'
-  );
+  // Show the console tab if 1) it's a video replay and we have the FF enabled
+  // or 2) it's not a video replay
+  const showConsoleTab = isVideoReplay
+    ? organization.features.includes('session-replay-mobile-network-tab')
+    : true;
 
   return {
     [TabKey.BREADCRUMBS]: t('Breadcrumbs'),
