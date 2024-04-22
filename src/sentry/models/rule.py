5b0f4d7e9504a96c5f3bd52d9def9fe854f51e4b
Mark Story <mark@mark-story.com>
2024-04-22 10:08:53 -0400

    Rename columns and add check constraint.

--- src/sentry/models/rule.py
+++ src/sentry/models/rule.py
@@ -58,8 +58,8 @@ class Rule(Model):
         choices=RuleSource.as_choices(),
     )
     owner = FlexibleForeignKey("sentry.Actor", null=True, on_delete=models.SET_NULL)
-    user_id = HybridCloudForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete="SET_NULL")
-    team = FlexibleForeignKey("sentry.Team", null=True, on_delete=models.SET_NULL)
+    owner_user_id = HybridCloudForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete="SET_NULL")
+    owner_team = FlexibleForeignKey("sentry.Team", null=True, on_delete=models.SET_NULL)
 
     date_added = models.DateTimeField(default=timezone.now)
 
@@ -69,6 +69,16 @@ class Rule(Model):
         db_table = "sentry_rule"
         app_label = "sentry"
         indexes = (models.Index(fields=("project", "status", "owner")),)
+        constraints = (
+            models.CheckConstraint(
+                check=(
+                    models.Q(owner_user_id__isnull=True, owner_team__isnull=False)
+                    | models.Q(owner_user_id__isnull=False, owner_team__isnull=True)
+                    | models.Q(owner_user_id__isnull=True, owner_team__isnull=True)
+                ),
+                name="rule_owner_user_or_team_check",
+            ),
+        )
 
     __repr__ = sane_repr("project_id", "label")
 
