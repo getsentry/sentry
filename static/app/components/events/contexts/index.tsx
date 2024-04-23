496d5a6e63338a21346f4098ab069783e0dd90df
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-04-23 14:50:54 -0400

    ref(event-tags): Refactor work for Issue Details changes (#69426)
    No significant changes here, but a lot of the util/refactors/new props
    are necessary for enabling rendering custom highlights.
    
    List of changes:
    - Pulls out the list of context items from the `EventContexts` component
    - Pulls the context content out of the `ContextCard` component (with a
    config to edit how its rendered)
    - Adds a config to `EventTagsTreeRow` component to edit how its rendered
    - Remove a div from the `EventTagsTree`
    - Creates the `ContextCard` components from the `EventContexts`
    component into the `ContextDataSection`
    - Implement a hook to get a detailed project from a slug (since
    `useProjects` does not render with the `DetailedProjectSerializer`)
    - Correct the flag name on some frontend tests
    - Rename param for `useIssueDetailsColumnCount` hook
    - Adds `highlightContext` and `highlightTags` to the Project type

--- static/app/components/events/contexts/index.tsx
+++ static/app/components/events/contexts/index.tsx
@@ -1,10 +1,9 @@
 import {Fragment, useCallback, useEffect} from 'react';
 import * as Sentry from '@sentry/react';
 
-import ContextCard from 'sentry/components/events/contexts/contextCard';
 import ContextDataSection from 'sentry/components/events/contexts/contextDataSection';
 import {useHasNewTagsUI} from 'sentry/components/events/eventTags/util';
-import type {Event} from 'sentry/types/event';
+import type {Event, EventContexts as EventContextValues} from 'sentry/types/event';
 import type {Group} from 'sentry/types/group';
 import {objectIsEmpty} from 'sentry/utils';
 import useProjects from 'sentry/utils/useProjects';
@@ -15,6 +14,55 @@ type Props = {
   event: Event;
   group?: Group;
 };
+
+interface UnknownContextValue {
+  [key: string]: any;
+  type: 'default';
+}
+
+/**
+ * Catch-all for context values, known and unknown
+ */
+export type ContextValue =
+  | EventContextValues[keyof EventContextValues]
+  | UnknownContextValue;
+
+export interface ContextItem {
+  alias: string;
+  type: string;
+  value: ContextValue;
+}
+
+export function getOrderedContextItems(event): ContextItem[] {
+  const {user, contexts} = event;
+
+  const {feedback, response, ...otherContexts} = contexts ?? {};
+  const orderedContext: [ContextItem['alias'], ContextValue][] = [
+    ['response', response],
+    ['feedback', feedback],
+    ['user', user],
+    ...Object.entries(otherContexts),
+  ];
+  // For these context keys, use 'key' as 'type' rather than 'value.type'
+  const overrideTypes = new Set(['response', 'feedback', 'user']);
+  const items = orderedContext
+    .filter(([_k, ctxValue]) => {
+      const contextKeys = Object.keys(ctxValue ?? {});
+      const isInvalid =
+        // Empty context
+        contextKeys.length === 0 ||
+        // Empty aside from 'type' key
+        (contextKeys.length === 1 && contextKeys[0] === 'type');
+      return !isInvalid;
+    })
+    .map<ContextItem>(([alias, ctx]) => ({
+      alias,
+      type: overrideTypes.has(ctx.type) ? ctx : ctx?.type,
+      value: ctx,
+    }));
+
+  return items;
+}
 
 export function EventContexts({event, group}: Props) {
   const hasNewTagsUI = useHasNewTagsUI();
@@ -39,37 +87,7 @@ export function EventContexts({event, group}: Props) {
   }, [usingOtel, sdk]);
 
   if (hasNewTagsUI) {
-    const orderedContext: [string, any][] = [
-      ['response', response],
-      ['feedback', feedback],
-      ['user', user],
-      ...Object.entries(otherContexts),
-    ];
-    // For these context keys, use 'key' as 'type' rather than 'value.type'
-    const overrideTypes = new Set(['response', 'feedback', 'user']);
-    const cards = orderedContext
-      .filter(([_k, v]) => {
-        const contextKeys = Object.keys(v ?? {});
-        const isInvalid =
-          // Empty context
-          contextKeys.length === 0 ||
-          // Empty aside from 'type' key
-          (contextKeys.length === 1 && contextKeys[0] === 'type');
-        return !isInvalid;
-      })
-      .map(([k, v]) => (
-        <ContextCard
-          key={k}
-          type={overrideTypes.has(k) ? k : v?.type ?? ''}
-          alias={k}
-          value={v}
-          event={event}
-          group={group}
-          project={project}
-        />
-      ));
-
-    return <ContextDataSection cards={cards} />;
+    return <ContextDataSection event={event} group={group} project={project} />;
   }
 
   return (
