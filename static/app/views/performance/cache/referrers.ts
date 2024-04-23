f567529a5d4be38ab2d36a1c32c06b44ad416bb7
Yagiz Nizipli <yagiz@nizipli.com>
2024-04-23 16:44:32 -0400

    feat: enable typescript biome rules (#69534)
    We weren't leveraging enough Typescript rules. Adding these caught
    some...
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/views/performance/cache/referrers.ts
+++ static/app/views/performance/cache/referrers.ts
@@ -1,6 +1,4 @@
-const PREFIX = `api.performance.cache`;
-
 export enum Referrer {
-  LANDING_CACHE_HIT_MISS_CHART = `${PREFIX}.landing-cache-hit-miss-chart`,
-  LANDING_CACHE_THROUGHPUT_CHART = `${PREFIX}.landing-cache-throughput-chart`,
+  LANDING_CACHE_HIT_MISS_CHART = `api.performance.cache.landing-cache-hit-miss-chart`,
+  LANDING_CACHE_THROUGHPUT_CHART = `api.performance.cache.landing-cache-throughput-chart`,
 }
