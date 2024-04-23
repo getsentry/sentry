aa523b07741b672cfe1c0b71dcc86c08ffbce143
Edward Gou <edward.gou@sentry.io>
2024-04-23 13:33:56 -0400

    Adds queues landing page content, summary page content, and samples slideout content

--- static/app/views/performance/queues/queuesLandingPage.tsx
+++ static/app/views/performance/queues/queuesLandingPage.tsx
@@ -1,4 +1,5 @@
 import {Fragment} from 'react';
+import styled from '@emotion/styled';
 
 import FeatureBadge from 'sentry/components/badge/featureBadge';
 import {Breadcrumbs} from 'sentry/components/breadcrumbs';
@@ -8,15 +9,23 @@ import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import {EnvironmentPageFilter} from 'sentry/components/organizations/environmentPageFilter';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilter';
+import SmartSearchBar from 'sentry/components/smartSearchBar';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import useOrganization from 'sentry/utils/useOrganization';
 import {normalizeUrl} from 'sentry/utils/withDomainRequired';
+import {useOnboardingProject} from 'sentry/views/performance/browser/webVitals/utils/useOnboardingProject';
 import * as ModuleLayout from 'sentry/views/performance/moduleLayout';
 import {ModulePageProviders} from 'sentry/views/performance/modulePageProviders';
+import Onboarding from 'sentry/views/performance/onboarding';
+import {LatencyChart} from 'sentry/views/performance/queues/charts/latencyChart';
+import {ThroughputChart} from 'sentry/views/performance/queues/charts/throughputChart';
+import {QueuesTable} from 'sentry/views/performance/queues/queuesTable';
 import {MODULE_TITLE, RELEASE_LEVEL} from 'sentry/views/performance/queues/settings';
 
 function QueuesLandingPage() {
   const organization = useOrganization();
+  const onboardingProject = useOnboardingProject();
 
   return (
     <Fragment>
@@ -54,6 +63,30 @@ function QueuesLandingPage() {
                 <DatePageFilter />
               </PageFilterBar>
             </ModuleLayout.Full>
+
+            {onboardingProject && (
+              <Onboarding organization={organization} project={onboardingProject} />
+            )}
+
+            {!onboardingProject && (
+              <Fragment>
+                <ModuleLayout.Half>
+                  <LatencyChart />
+                </ModuleLayout.Half>
+
+                <ModuleLayout.Half>
+                  <ThroughputChart />
+                </ModuleLayout.Half>
+
+                <ModuleLayout.Full>
+                  <Flex>
+                    {/* TODO: Make search bar work */}
+                    <SmartSearchBar />
+                    <QueuesTable />
+                  </Flex>
+                </ModuleLayout.Full>
+              </Fragment>
+            )}
           </ModuleLayout.Layout>
         </Layout.Main>
       </Layout.Body>
@@ -73,3 +106,9 @@ function LandingPageWithProviders() {
   );
 }
 export default LandingPageWithProviders;
+
+const Flex = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: ${space(2)};
+`;
