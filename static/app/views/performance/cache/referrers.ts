ae6a471aba01d4a6abe3d10b36aed38a74f5d111
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-04-23 11:06:50 -0400

    update enum

--- static/app/views/performance/cache/referrers.ts
+++ static/app/views/performance/cache/referrers.ts
@@ -1,12 +1,6 @@
-const referrerPrefix = 'api.performance.cache';
+const PREFIX = `api.performance.cache`;
 
-enum ReferrerSuffix {
-  LANDING_CACHE_HIT_MISS_CHART = `landing-cache-hit-miss-chart`,
-  LANDING_CACHE_THROUGHPUT_CHART = `landing-cache-throughput-chart`,
+export enum Referrer {
+  LANDING_CACHE_HIT_MISS_CHART = `${PREFIX}.landing-cache-hit-miss-chart`,
+  LANDING_CACHE_THROUGHPUT_CHART = `${PREFIX}.landing-cache-throughput-chart`,
 }
-
-type PrefixedReferrer = {
-  [key in keyof typeof ReferrerSuffix]: `${typeof referrerPrefix}.${(typeof ReferrerSuffix)[key]}`;
-};
-
-export const Referrer: PrefixedReferrer = ReferrerSuffix as unknown as PrefixedReferrer;
