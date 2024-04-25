fe96cbdedff8bf5bfb34d444c587900c0a5e5570
ArthurKnaus <arthur.knaus@sentry.io>
2024-04-25 09:22:25 +0200

    feat(metrics): Add interactive elements to metric details (#69561)
    https://github.com/getsentry/sentry/assets/7033940/cdb95fe9-2639-4524-be22-e454626b5379
    
    - closes https://github.com/getsentry/sentry/issues/68888

--- static/app/views/metrics/metricListItemDetails.tsx
+++ static/app/views/metrics/metricListItemDetails.tsx
@@ -1,12 +1,13 @@
 import {Fragment, startTransition, useEffect, useMemo, useState} from 'react';
 import styled from '@emotion/styled';
 
+import {Button, LinkButton} from 'sentry/components/button';
 import ProjectBadge from 'sentry/components/idBadge/projectBadge';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
-import {IconWarning} from 'sentry/icons';
+import {IconSettings, IconWarning} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
-import type {MetricMeta, Project} from 'sentry/types';
+import type {MetricMeta, MRI, Project} from 'sentry/types';
 import {getReadableMetricType} from 'sentry/utils/metrics/formatters';
 import {formatMRI, parseMRI} from 'sentry/utils/metrics/mri';
 import {
@@ -19,18 +20,22 @@ import useOrganization from 'sentry/utils/useOrganization';
 const MAX_PROJECTS_TO_SHOW = 3;
 const MAX_TAGS_TO_SHOW = 5;
 
-const STANDARD_TAGS = ['release', 'environment', 'transaction'];
+const STANDARD_TAGS = ['release', 'environment', 'transaction', 'project'];
 
 export function MetricListItemDetails({
   metric,
   selectedProjects,
+  onTagClick,
 }: {
   metric: MetricMeta;
+  onTagClick: (mri: MRI, tag: string) => void;
   selectedProjects: Project[];
 }) {
   const organization = useOrganization();
   const queryClient = useQueryClient();
   const isCustomMetric = parseMRI(metric.mri)?.useCase === 'custom';
+
+  const [showAllTags, setShowAllTags] = useState(false);
 
   const projectIds = useMemo(
     () => selectedProjects.map(project => parseInt(project.id, 10)),
@@ -86,30 +91,36 @@ export function MetricListItemDetails({
       }),
     [tagsData]
   );
-  const truncatedTags = sortedTags.slice(0, MAX_TAGS_TO_SHOW);
+  const truncatedTags = showAllTags ? sortedTags : sortedTags.slice(0, MAX_TAGS_TO_SHOW);
 
   return (
     <DetailsWrapper>
-      <MetricName>
-        {/* Add zero width spaces at delimiter characters for nice word breaks */}
-        {formatMRI(metric.mri).replaceAll(/([\.\/-_])/g, '\u200b$1')}
-        {!isCustomMetric && (
-          <SamplingWarning>
-            <IconWarning color="yellow400" size="xs" />
-            {t('Prone to client-side sampling')}
-          </SamplingWarning>
+      <Header>
+        <MetricName>
+          {/* Add zero width spaces at delimiter characters for nice word breaks */}
+          {formatMRI(metric.mri).replaceAll(/([\.\/-_])/g, '\u200b$1')}
+          {!isCustomMetric && (
+            <SamplingWarning>
+              <IconWarning color="yellow400" size="xs" />
+              {t('Prone to client-side sampling')}
+            </SamplingWarning>
+          )}
+        </MetricName>
+        {isCustomMetric && (
+          <LinkButton
+            size="xs"
+            to={`/settings/projects/${metricProjects[0].slug}/metrics/${encodeURIComponent(metric.mri)}`}
+            aria-label={t('Open metric settings')}
+            icon={<IconSettings />}
+            borderless
+          />
         )}
-      </MetricName>
+      </Header>
       <DetailsGrid>
         <DetailsLabel>{t('Project')}</DetailsLabel>
         <DetailsValue>
           {truncatedProjects.map(project => (
-            <ProjectBadge
-              project={project}
-              key={project.slug}
-              avatarSize={12}
-              disableLink
-            />
+            <ProjectBadge project={project} key={project.slug} avatarSize={12} />
           ))}
           {metricProjects.length > MAX_PROJECTS_TO_SHOW && (
             <span>{t('+%d more', metricProjects.length - MAX_PROJECTS_TO_SHOW)}</span>
@@ -127,9 +138,29 @@ export function MetricListItemDetails({
             t('(None)')
           ) : (
             <Fragment>
-              {truncatedTags.map(tag => tag.key).join(', ')}
-              {tagsData.length > MAX_TAGS_TO_SHOW && (
-                <div>{t('+%d more', tagsData.length - MAX_TAGS_TO_SHOW)}</div>
+              {truncatedTags.map((tag, index) => {
+                const shouldAddDelimiter = index < truncatedTags.length - 1;
+                return (
+                  <Fragment key={tag.key}>
+                    <TagWrapper>
+                      <Button
+                        priority="link"
+                        onClick={() => onTagClick(metric.mri, tag.key)}
+                      >
+                        {tag.key}
+                      </Button>
+                      {/* Make the comma stick to the Button when the text wraps to the next line */}
+                      {shouldAddDelimiter ? ',' : null}
+                    </TagWrapper>
+                    {shouldAddDelimiter ? ' ' : null}
+                  </Fragment>
+                );
+              })}
+              <br />
+              {tagsData.length > MAX_TAGS_TO_SHOW && !showAllTags && (
+                <Button priority="link" onClick={() => setShowAllTags(true)}>
+                  {t('+%d more', tagsData.length - MAX_TAGS_TO_SHOW)}
+                </Button>
               )}
             </Fragment>
           )}
@@ -144,8 +175,15 @@ const DetailsWrapper = styled('div')`
   line-height: 1.4;
 `;
 
+const Header = styled('div')`
+  display: grid;
+  grid-template-columns: 1fr max-content;
+  align-items: center;
+  gap: ${space(0.5)};
+  padding: ${space(0.75)} ${space(0.25)} ${space(0.75)} ${space(1.5)};
+`;
+
 const MetricName = styled('div')`
-  padding: ${space(0.75)} ${space(1.5)};
   word-break: break-word;
 `;
 
@@ -180,6 +218,10 @@ const DetailsValue = styled('div')`
   border-top-right-radius: ${p => p.theme.borderRadius};
   border-bottom-right-radius: ${p => p.theme.borderRadius};
   min-width: 0;
+`;
+
+const TagWrapper = styled('span')`
+  white-space: nowrap;
 `;
 
 const SamplingWarning = styled('div')`
