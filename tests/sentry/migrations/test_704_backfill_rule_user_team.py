281be4b747dc3f6d36a1ba34efc74c1d12f6ec6c
Mark Story <mark@mark-story.com>
2024-04-25 16:22:27 -0400

    chore(actor) Backfill user_id & team_id on GroupHistory
    Part of the actor cleanup. Backfill the user_id and team_id columns on
    GroupHistory so that we can start reading from them. New writes were
    taken care of in #69667
    
    Refs HC-1174

--- tests/sentry/migrations/test_704_backfill_rule_user_team.py
+++ tests/sentry/migrations/test_704_backfill_rule_user_team.py
@@ -1,56 +1,46 @@
 from sentry.models.actor import ACTOR_TYPES, Actor
-from sentry.models.rule import Rule
+from sentry.models.grouphistory import GroupHistory, GroupHistoryStatus, record_group_history
 from sentry.testutils.cases import TestMigrations
 
 
-class BackfillRuleUserTeamTest(TestMigrations):
-    migrate_from = "0703_add_team_user_to_rule"
-    migrate_to = "0704_backfill_rule_user_team"
+class BackfillGroupHistoryUserTeamTest(TestMigrations):
+    migrate_from = "0705_grouphistory_add_userteam"
+    migrate_to = "0706_grouphistory_userteam_backfill"
 
     def setup_initial_state(self):
         self.org = self.create_organization(owner=self.user)
         self.team = self.create_team(organization=self.org, members=[self.user])
         self.project = self.create_project(organization=self.org)
+        self.group = self.create_group(project=self.project, title="oh no")
+        other_user = self.create_user()
 
         self.user_actor = Actor.objects.create(type=ACTOR_TYPES["user"], user_id=self.user.id)
         self.team_actor = Actor.objects.get(type=ACTOR_TYPES["team"], team_id=self.team.id)
-        self.team_rule = Rule.objects.create(
-            project=self.project,
-            label="team rule",
-            owner=self.team_actor,
-            owner_team=self.team,
+        self.team_history = record_group_history(
+            group=self.group, actor=self.team, status=GroupHistoryStatus.RESOLVED
         )
-        self.user_rule = Rule.objects.create(
-            project=self.project,
-            label="user rule",
-            owner=self.user_actor,
-            owner_user_id=self.user.id,
+        self.user_history = record_group_history(
+            group=self.group, actor=self.user, status=GroupHistoryStatus.ESCALATING
+        )
+        self.valid = record_group_history(
+            group=self.group, actor=other_user, status=GroupHistoryStatus.ONGOING
         )
 
-        other_user = self.create_user()
-        other_actor = Actor.objects.create(type=ACTOR_TYPES["user"], user_id=other_user.id)
-        self.valid = Rule.objects.create(
-            project=self.project,
-            label="valid",
-            owner=other_actor,
-            owner_user_id=other_user.id,
-        )
-
-        # Use QuerySet.update() to avoid validation in AlertRule
-        Rule.objects.filter(id__in=[self.team_rule.id, self.user_rule.id]).update(
-            owner_team_id=None, owner_user_id=None
+        # Use QuerySet.update() to avoid validation in GroupHistory
+        GroupHistory.objects.filter(id__in=[self.team_history.id, self.user_history.id]).update(
+            team_id=None, user_id=None
         )
 
     def test(self):
-        self.user_rule.refresh_from_db()
-        self.team_rule.refresh_from_db()
+        self.user_history.refresh_from_db()
+        self.team_history.refresh_from_db()
         self.valid.refresh_from_db()
 
-        assert self.user_rule.owner_user_id == self.user.id
-        assert self.user_rule.owner_team_id is None
+        assert self.user_history.user_id == self.user.id
+        assert self.user_history.team_id is None
 
-        assert self.team_rule.owner_team_id == self.team.id
-        assert self.team_rule.owner_user_id is None
+        assert self.team_history.team_id == self.team.id
+        assert self.team_history.user_id is None
 
-        assert self.valid.owner_team_id is None
-        assert self.valid.owner_user_id
+        assert self.valid.team_id is None
+        assert self.valid.user_id
