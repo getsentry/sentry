3b6e0fe2434b4f57eb63a121dce51f4ff6c0de89
Simon Hellmayr <simon.hellmayr@sentry.io>
2024-04-25 10:55:47 +0200

    Fix wrong import

--- src/sentry/sentry_metrics/querying/data/api.py
+++ src/sentry/sentry_metrics/querying/data/api.py
@@ -9,9 +9,7 @@ from sentry.models.organization import Organization
 from sentry.models.project import Project
 from sentry.sentry_metrics.querying.data.execution import QueryExecutor
 from sentry.sentry_metrics.querying.data.mapping.base import MapperConfig
-from sentry.sentry_metrics.querying.data.mapping.project_to_project_id_mapper import (
-    Project2ProjectIDMapper,
-)
+from sentry.sentry_metrics.querying.data.mapping.mapper import Project2ProjectIDMapper
 from sentry.sentry_metrics.querying.data.parsing import QueryParser
 from sentry.sentry_metrics.querying.data.postprocessing.base import run_post_processing_steps
 from sentry.sentry_metrics.querying.data.postprocessing.remapping import QueryRemappingStep
