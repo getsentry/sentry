5a7d90cd20b08d57c2095a3f3c5800adb470fd58
Arthur Knaus <arthur.knaus@sentry.io>
2024-04-25 10:16:01 +0200

    fix(metrics): Disallow wildcard in search

--- static/app/views/metrics/metricSearchBar.tsx
+++ static/app/views/metrics/metricSearchBar.tsx
@@ -181,6 +181,7 @@ export function MetricSearchBar({
       placeholder={t('Filter by tags')}
       query={query}
       savedSearchType={SavedSearchType.METRIC}
+      disallowWildcard
       {...searchConfig}
       {...props}
     />
