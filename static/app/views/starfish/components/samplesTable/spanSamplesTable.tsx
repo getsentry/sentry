5db522a0134ba069f8a2bceac774e5c9824025c0
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-04-29 14:05:31 -0400

    ref(js): Use Link component in spanSamplesTable (#69895)

--- static/app/views/starfish/components/samplesTable/spanSamplesTable.tsx
+++ static/app/views/starfish/components/samplesTable/spanSamplesTable.tsx
@@ -1,9 +1,9 @@
-import {Link} from 'react-router';
 import styled from '@emotion/styled';
 
 import {LinkButton} from 'sentry/components/button';
 import type {GridColumnHeader} from 'sentry/components/gridEditable';
 import GridEditable, {COL_WIDTH_UNDEFINED} from 'sentry/components/gridEditable';
+import Link from 'sentry/components/links/link';
 import {Tooltip} from 'sentry/components/tooltip';
 import {IconProfiling} from 'sentry/icons/iconProfiling';
 import {t} from 'sentry/locale';
