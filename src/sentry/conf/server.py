ee1796917bff043223c0149e6e403f984efb33ed
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-04-29 08:46:38 -0400

    Remove feature flag usage

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1748,8 +1748,6 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     "organizations:performance-screens-view": False,
     # Enable platform selector for screens flow
     "organizations:performance-screens-platform-selector": False,
-    # Enable API aka HTTP aka Network Performance module
-    "organizations:performance-http-view": False,
     # Enable column that shows ttid ttfd contributing spans
     "organizations:mobile-ttid-ttfd-contribution": False,
     # Enable histogram view in span details
