385c95e11601c5f1c9651f95aa1323bd042a5ecf
Scott Cooper <scttcper@gmail.com>
2024-04-29 11:17:14 -0700

    access safely in test

--- static/app/bootstrap/initializeSdk.tsx
+++ static/app/bootstrap/initializeSdk.tsx
@@ -225,7 +225,7 @@ export function initializeSdk(config: Config, {routes}: {routes?: Function} = {}
     Sentry.setTag('sentry_version', window.__SENTRY__VERSION);
   }
 
-  const {customerDomain} = window.__initialData;
+  const {customerDomain, features} = window.__initialData;
 
   if (customerDomain) {
     Sentry.setTag('isCustomerDomain', 'yes');
@@ -237,9 +237,9 @@ export function initializeSdk(config: Config, {routes}: {routes?: Function} = {}
   // TODO: Remove once we've finished rolling out the new renderer
   Sentry.setTag(
     'isConcurrentRenderer',
-    (window.__initialData.features as unknown as string[]).includes(
+    (features as unknown as string[])?.includes(
       'organizations:react-concurrent-renderer-enabled'
-    )
+    ) ?? false
   );
 }
 
