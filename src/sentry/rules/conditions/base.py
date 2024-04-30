3d8f85a14f5a355f847f8d01e0afe89f0827af04
schew2381 <seiji.chew@sentry.io>
2024-05-01 06:46:07 +0900

    chore(typing): Add condition typeddict to processing

--- src/sentry/rules/conditions/base.py
+++ src/sentry/rules/conditions/base.py
@@ -1,10 +1,17 @@
 import abc
 from collections.abc import Sequence
 from datetime import datetime
+from typing import TypedDict
 
 from sentry.eventstore.models import GroupEvent
 from sentry.rules.base import EventState, RuleBase
 from sentry.types.condition_activity import ConditionActivity
+
+
+class GenericCondition(TypedDict):
+    # the ID in the rules registry that maps to a condition class
+    # e.g. "sentry.rules.conditions.every_event.EveryEventCondition"
+    id: str
 
 
 class EventCondition(RuleBase, abc.ABC):
