0c2b4c2f08abfa544863802aefd399cc030ffa56
Cathy Teng <cathy.teng@sentry.io>
2024-05-01 14:03:42 -0700

    put back ff i'm removing in a different PR

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1455,6 +1455,8 @@ SENTRY_EARLY_FEATURES = {
 
 # NOTE: Please maintain alphabetical order when adding new feature flags
 SENTRY_FEATURES: dict[str, bool | None] = {
+    # Enables superuser read vs. write separation
+    "auth:enterprise-superuser-read-write": False,
     # Enables user registration.
     "auth:register": True,
     # Enables activated alert rules
