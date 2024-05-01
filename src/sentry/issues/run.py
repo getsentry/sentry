57ef9475d48b586af09aecca6c95335be52ab066
Stephen Cefali <scefali@sentry.io>
2024-05-01 09:20:33 -0600

    feat(issue-platform): issue platform batched consumer (#69998)
    Had to revert this
    [PR](https://github.com/getsentry/sentry/pull/69466/files) because of
    this
    [error](https://sentry-st.sentry.io/issues/5284414231/?project=1513938&query=release%3Abackend%40bc99edc32bcc9011d86e9e78f0fca26758353464&referrer=release-issue-stream).
    This PR fixes it by adding a type annotation to the return value of
    `multiprocessing_options` ensures we append a single option instead of
    trying to concat lists of different types.
    
    Original description:
    This PR makes a batched parallel option for the occurrence consumer for
    issue platform. The reason for this is because right now we are
    processing events out of order when we have multi-processed consumer in
    parallel. By using batching, we can ensure the order. In order to
    prevent processing the same occurrence multiple times, we utilize
    Memcached to store the ids of the messages we've processed within the
    past 300 seconds.

--- src/sentry/issues/run.py
+++ src/sentry/issues/run.py
@@ -1,5 +1,8 @@
+import functools
 import logging
 from collections.abc import Mapping
+from concurrent.futures import ThreadPoolExecutor
+from typing import Literal
 
 from arroyo.backends.kafka import KafkaPayload
 from arroyo.processing.strategies import (
@@ -7,6 +10,8 @@ from arroyo.processing.strategies import (
     ProcessingStrategy,
     ProcessingStrategyFactory,
 )
+from arroyo.processing.strategies.batching import BatchStep, ValuesBatch
+from arroyo.processing.strategies.run_task import RunTask
 from arroyo.types import Commit, Message, Partition
 
 from sentry.utils.arroyo import MultiprocessingPool, RunTaskWithMultiprocessing
@@ -19,22 +24,34 @@ class OccurrenceStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
         self,
         max_batch_size: int,
         max_batch_time: int,
-        num_processes: int,
-        input_block_size: int | None,
-        output_block_size: int | None,
+        # not needed in batched-parallel mode
+        num_processes: int | None = None,
+        input_block_size: int | None = None,
+        output_block_size: int | None = None,
+        mode: Literal["batched-parallel", "parallel"] | None = None,
     ):
         super().__init__()
         self.max_batch_size = max_batch_size
         self.max_batch_time = max_batch_time
         self.input_block_size = input_block_size
         self.output_block_size = output_block_size
-        self.pool = MultiprocessingPool(num_processes)
 
-    def create_with_partitions(
+        self.batched = mode == "batched-parallel"
+        # either use multi-process pool or a thread pool
+        if self.batched:
+            self.worker: ThreadPoolExecutor | None = ThreadPoolExecutor()
+            self.pool: MultiprocessingPool | None = None
+        else:
+            # make sure num_processes is not None
+            assert num_processes is not None
+            self.pool = MultiprocessingPool(num_processes)
+            self.worker = None
+
+    def crate_parallel_worker(
         self,
         commit: Commit,
-        partitions: Mapping[Partition, int],
     ) -> ProcessingStrategy[KafkaPayload]:
+        assert self.pool is not None
         return RunTaskWithMultiprocessing(
             function=process_message,
             next_step=CommitOffsets(commit),
@@ -45,8 +62,33 @@ class OccurrenceStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
             output_block_size=self.output_block_size,
         )
 
+    def creat_batched_parallel_worker(self, commit: Commit) -> ProcessingStrategy[KafkaPayload]:
+        assert self.worker is not None
+        batch_processor = RunTask(
+            function=functools.partial(process_batch, self.worker),
+            next_step=CommitOffsets(commit),
+        )
+        return BatchStep(
+            max_batch_size=self.max_batch_size,
+            max_batch_time=self.max_batch_time,
+            next_step=batch_processor,
+        )
+
+    def create_with_partitions(
+        self,
+        commit: Commit,
+        partitions: Mapping[Partition, int],
+    ) -> ProcessingStrategy[KafkaPayload]:
+        if self.batched:
+            return self.creat_batched_parallel_worker(commit)
+        else:
+            return self.crate_parallel_worker(commit)
+
     def shutdown(self) -> None:
-        self.pool.close()
+        if self.pool:
+            self.pool.close()
+        if self.worker:
+            self.worker.shutdown()
 
 
 def process_message(message: Message[KafkaPayload]) -> None:
@@ -59,3 +101,14 @@ def process_message(message: Message[KafkaPayload]) -> None:
             _process_message(payload)
     except Exception:
         logger.exception("failed to process message payload")
+
+
+def process_batch(worker: ThreadPoolExecutor, messages: Message[ValuesBatch[KafkaPayload]]) -> None:
+    from sentry.issues.occurrence_consumer import _process_batch
+    from sentry.utils import metrics
+
+    try:
+        with metrics.timer("occurrence_consumer.process_batch"):
+            _process_batch(worker, messages)
+    except Exception:
+        logger.exception("failed to process batch payload")
