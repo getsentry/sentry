71c1d46415e4a47f75e02c288e91d6e1ef6e0c07
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-05-01 13:23:23 -0400

    ✨ Add feedback button

--- static/app/components/events/highlights/highlightsDataSection.tsx
+++ static/app/components/events/highlights/highlightsDataSection.tsx
@@ -23,8 +23,9 @@ import {
   getHighlightContextData,
   getHighlightTagData,
 } from 'sentry/components/events/highlights/util';
+import useFeedbackWidget from 'sentry/components/feedback/widget/useFeedbackWidget';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
-import {IconEdit} from 'sentry/icons';
+import {IconEdit, IconMegaphone} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Event, Group, Project} from 'sentry/types';
@@ -158,6 +159,7 @@ export default function HighlightsDataSection({
       type="event-highlights"
       actions={
         <ButtonBar gap={1}>
+          <HighlightsFeedback />
           {viewAllButton}
           <Button
             size="xs"
@@ -193,6 +195,31 @@ export default function HighlightsDataSection({
         )}
       </HighlightContainer>
     </EventDataSection>
+  );
+}
+
+function HighlightsFeedback() {
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const feedback = useFeedbackWidget({
+    buttonRef,
+    messagePlaceholder: t(
+      'How can we make tags, context or highlights more useful to you?'
+    ),
+  });
+
+  if (!feedback) {
+    return null;
+  }
+
+  return (
+    <Button
+      ref={buttonRef}
+      aria-label={t('Give Feedback')}
+      icon={<IconMegaphone />}
+      size={'xs'}
+    >
+      {t('Feedback')}
+    </Button>
   );
 }
 
