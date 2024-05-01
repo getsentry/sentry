64d9af17fb2b31ba9da53ff7a485bbc3a79bb324
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2024-05-01 14:39:41 -0700

    chore(superuser): replace read-write FF entirely with option (#70095)

--- src/sentry/auth/superuser.py
+++ src/sentry/auth/superuser.py
@@ -24,7 +24,7 @@ from django.utils.crypto import constant_time_compare, get_random_string
 from rest_framework import serializers, status
 from rest_framework.request import Request
 
-from sentry import features, options
+from sentry import options
 from sentry.api.exceptions import SentryAPIException
 from sentry.auth.elevated_mode import ElevatedMode, InactiveReason
 from sentry.auth.system import is_system_auth
@@ -81,10 +81,7 @@ def get_superuser_scopes(auth_state: RpcAuthState, user: Any) -> set[str]:
     superuser_scopes = SUPERUSER_SCOPES
     if (
         not is_self_hosted()
-        and (
-            options.get("superuser.read-write.ga-rollout")
-            or features.has("auth:enterprise-superuser-read-write", actor=user)
-        )
+        and options.get("superuser.read-write.ga-rollout")
         and "superuser.write" not in auth_state.permissions
     ):
         superuser_scopes = SUPERUSER_READONLY_SCOPES
@@ -111,10 +108,7 @@ def superuser_has_permission(
         return True
 
     # if we aren't enforcing superuser read-write, then superuser always has access
-    if not (
-        features.has("auth:enterprise-superuser-read-write", actor=request.user)
-        or options.get("superuser.read-write.ga-rollout")
-    ):
+    if not options.get("superuser.read-write.ga-rollout"):
         return True
 
     # either request.access or permissions must exist
