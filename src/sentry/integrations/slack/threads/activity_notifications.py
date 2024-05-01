2cdb16e1b6ab8cb1da96c22485815dc05f3c00d8
Yash Kamothi <yash.kamothi@gmail.com>
2024-05-01 13:47:39 -0700

    add doc string and comments

--- src/sentry/integrations/slack/threads/activity_notifications.py
+++ src/sentry/integrations/slack/threads/activity_notifications.py
@@ -7,5 +7,11 @@ from sentry.notifications.notifications.activity.assigned import (
 
 
 class AssignedActivityNotification(BaseAssignedActivityNotification):
+    """
+    This notification overrides the base AssignedActivityNotification text template to remove the explicit issue name,
+    and instead leverages "this issue" since this notification is already attached to an existing notification where
+    the issue name exists.
+    """
+
     def get_description(self) -> tuple[str, str | None, Mapping[str, Any]]:
         return "{author} assigned this issue to {assignee}", None, {"assignee": self.get_assignee()}
