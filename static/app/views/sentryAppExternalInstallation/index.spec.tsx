937b4b035d6600add6e7b92d9742fa83236fb70b
Mark Story <mark@mark-story.com>
2024-05-02 17:47:11 -0400

    Add test for de org loading

--- static/app/views/sentryAppExternalInstallation/index.spec.tsx
+++ static/app/views/sentryAppExternalInstallation/index.spec.tsx
@@ -206,6 +206,38 @@ describe('SentryAppExternalInstallation', () => {
       await waitFor(() => expect(screen.getByTestId('install')).toBeEnabled());
     });
 
+    it('loads orgs from multiple regions', async () => {
+      window.__initialData = {
+        ...window.__initialData,
+        memberRegions: [
+          {name: 'us', url: 'https://us.example.org'},
+          {name: 'de', url: 'https://de.example.org'},
+        ],
+      };
+      ConfigStore.loadInitialData(window.__initialData);
+
+      const deorg = OrganizationFixture({slug: 'de-org'});
+      const getDeOrgs = MockApiClient.addMockResponse({
+        url: '/organizations/',
+        body: [deorg],
+        match: [
+          function (_url: string, options: Record<string, any>) {
+            return options.host === 'https://de.example.org';
+          },
+        ],
+      });
+
+      render(
+        <SentryAppExternalInstallation
+          {...RouteComponentPropsFixture()}
+          params={{sentryAppSlug: sentryApp.slug}}
+        />
+      );
+      await waitFor(() => expect(getInstallationsMock).toHaveBeenCalled());
+
+      expect(getDeOrgs).toHaveBeenCalled();
+    });
+
     it('selecting org changes the url', async () => {
       const preselectedOrg = OrganizationFixture();
       const {routerProps} = initializeOrg({organization: preselectedOrg});
