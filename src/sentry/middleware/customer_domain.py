f54ddf69c45e9669c56359d4e25233087f11cf49
Raj Joshi <raj.joshi@sentry.io>
2024-05-02 12:51:41 -0700

    fix logic

--- src/sentry/middleware/customer_domain.py
+++ src/sentry/middleware/customer_domain.py
@@ -52,11 +52,8 @@ def _resolve_redirect_url(request, activeorg):
     if redirect_subdomain:
         redirect_url = generate_organization_url(activeorg)
     result = resolve(request.path)
-    org_slug_path_mismatch = (
-        result.kwargs
-        and (
-            "organization_slug" in result.kwargs and result.kwargs["organization_slug"] != activeorg
-        )
+    org_slug_path_mismatch = result.kwargs and (
+        ("organization_slug" in result.kwargs and result.kwargs["organization_slug"] != activeorg)
         or (
             "organization_id_or_slug" in result.kwargs
             and result.kwargs["organization_id_or_slug"] != activeorg
