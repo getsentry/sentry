getsentry/sentry

commit 25fda6523e6d808d91dc90a68076edac9ad5d6fb

feat(related-issues): Add tab showing issues with the same root cause (#67079)
This PR depends on #66992 which adds the API to related issues.

---------

Co-authored-by: Scott Cooper <scttcper@gmail.com>
Co-authored-by: Malachi Willey <malwilley@gmail.com>

static/app/routes.tsx
@@ -1816,6 +1816,12 @@ function buildRoutes() {
           )}
         />
         <Route
+          path={TabPaths[Tab.RELATED_ISSUES]}
+          component={hoc(
+            make(() => import('sentry/views/issueDetails/groupRelatedIssues'))
+          )}
+        />
+        <Route
           path={TabPaths[Tab.MERGED]}
           component={hoc(make(() => import('sentry/views/issueDetails/groupMerged')))}
         />

static/app/views/issueDetails/groupRelatedIssues/index.tsx
@@ -1,0 +1,106 @@
+// XXX: A lot of the UI for this file will be changed once we use IssueListActions
+// We're using GroupList to help us iterate quickly
+import type {RouteComponentProps} from 'react-router';
+import styled from '@emotion/styled';
+
+import Feature from 'sentry/components/acl/feature';
+import GroupList from 'sentry/components/issues/groupList';
+import * as Layout from 'sentry/components/layouts/thirds';
+import LoadingError from 'sentry/components/loadingError';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {useApiQuery} from 'sentry/utils/queryClient';
+import useOrganization from 'sentry/utils/useOrganization';
+
+type RouteParams = {
+  groupId: string;
+};
+
+type Props = RouteComponentProps<RouteParams, {}>;
+
+type RelatedIssuesResponse = {
+  same_root_cause: number[];
+};
+
+function GroupRelatedIssues({params}: Props) {
+  const {groupId} = params;
+
+  const organization = useOrganization();
+  const orgSlug = organization.slug;
+
+  // Fetch the list of related issues
+  const {
+    isLoading,
+    isError,
+    data: relatedIssues,
+    refetch,
+  } = useApiQuery<RelatedIssuesResponse>([`/issues/${groupId}/related-issues/`], {
+    staleTime: 0,
+  });
+
+  // If the group we're looking related issues for shows up in the table,
+  // it will trigger a bug in getGroupReprocessingStatus because activites would be empty
+  const groups = relatedIssues?.same_root_cause
+    ?.filter(id => id.toString() !== groupId)
+    ?.join(',');
+
+  return (
+    <Layout.Body>
+      <Layout.Main fullWidth>
+        <HeaderWrapper>
+          <Title>{t('Related Issues')}</Title>
+          <small>
+            {t(
+              'Related Issues are issues that may have the same root cause and can be acted on together.'
+            )}
+          </small>
+        </HeaderWrapper>
+        {isLoading ? (
+          <LoadingIndicator />
+        ) : isError ? (
+          <LoadingError
+            message={t('Unable to load related issues, please try again later')}
+            onRetry={refetch}
+          />
+        ) : groups ? (
+          <GroupList
+            endpointPath={`/organizations/${orgSlug}/issues/`}
+            orgSlug={orgSlug}
+            queryParams={{query: `issue.id:[${groups}]`}}
+            query=""
+            source="related-issues-tab"
+            renderEmptyMessage={() => <Title>No related issues</Title>}
+            renderErrorMessage={() => <Title>Error loading related issues</Title>}
+          />
+        ) : (
+          <b>No related issues found!</b>
+        )}
+      </Layout.Main>
+    </Layout.Body>
+  );
+}
+
+function GroupRelatedIssuesWrapper(props: Props) {
+  return (
+    <Feature features={['related-issues']}>
+      <GroupRelatedIssues {...props} />
+    </Feature>
+  );
+}
+
+// Export the component without feature flag controls
+export {GroupRelatedIssues};
+export default GroupRelatedIssuesWrapper;
+
+const Title = styled('h4')`
+  margin-bottom: ${space(0.75)};
+`;
+
+const HeaderWrapper = styled('div')`
+  margin-bottom: ${space(2)};
+
+  small {
+    color: ${p => p.theme.subText};
+  }
+`;

static/app/views/issueDetails/types.tsx
@@ -7,6 +7,7 @@ export enum Tab {
   EVENTS = 'events',
   MERGED = 'merged',
   SIMILAR_ISSUES = 'similar-issues',
+  RELATED_ISSUES = 'related-issues',
   REPLAYS = 'Replays',
 }
 
@@ -19,5 +20,6 @@ export const TabPaths: Record<Tab, string> = {
   [Tab.EVENTS]: 'events/',
   [Tab.MERGED]: 'merged/',
   [Tab.SIMILAR_ISSUES]: 'similar/',
+  [Tab.RELATED_ISSUES]: 'related/',
   [Tab.REPLAYS]: 'replays/',
 };

static/app/views/issueDetails/groupRelatedIssues/index.spec.tsx
@@ -1,0 +1,87 @@
+import {OrganizationFixture} from 'sentry-fixture/organization';
+import {RouterFixture} from 'sentry-fixture/routerFixture';
+
+import {render, screen} from 'sentry-test/reactTestingLibrary';
+
+import {GroupRelatedIssues} from 'sentry/views/issueDetails/groupRelatedIssues';
+
+describe('Related Issues View', function () {
+  let relatedIssuesMock: jest.Mock;
+  let issuesInfoMock: jest.Mock;
+  const router = RouterFixture();
+
+  const organization = OrganizationFixture();
+  const orgSlug = organization.slug;
+  const groupId = '12345678';
+  const group1 = '15';
+  const group2 = '20';
+  // query=issue.id:[15,20] -> query=issue.id%3A%5B15%2C20%5D
+  const orgIssuesEndpoint = `/organizations/${orgSlug}/issues/?query=issue.id%3A%5B${group1}%2C${group2}%5D`;
+
+  const params = {groupId: groupId};
+  const errorType = 'RuntimeError';
+
+  beforeEach(function () {
+    // GroupList calls this but we don't need it for this test
+    MockApiClient.addMockResponse({
+      url: `/organizations/${orgSlug}/users/`,
+      body: {},
+    });
+    relatedIssuesMock = MockApiClient.addMockResponse({
+      url: `/issues/${groupId}/related-issues/`,
+      body: {same_root_cause: [group1, group2]},
+    });
+    issuesInfoMock = MockApiClient.addMockResponse({
+      url: orgIssuesEndpoint,
+      body: [
+        {
+          id: group1,
+          shortId: `EARTH-${group1}`,
+          project: {id: '3', name: 'Earth', slug: 'earth', platform: null},
+          type: 'error',
+          metadata: {
+            type: errorType,
+          },
+          issueCategory: 'error',
+          lastSeen: '2024-03-15T20:15:30Z',
+        },
+        {
+          id: group2,
+          shortId: `EARTH-${group2}`,
+          project: {id: '3', name: 'Earth', slug: 'earth', platform: null},
+          type: 'error',
+          metadata: {
+            type: errorType,
+          },
+          issueCategory: 'error',
+          lastSeen: '2024-03-16T20:15:30Z',
+        },
+      ],
+    });
+  });
+
+  afterEach(() => {
+    MockApiClient.clearMockResponses();
+    jest.clearAllMocks();
+  });
+
+  it('renders with mocked data', async function () {
+    render(
+      <GroupRelatedIssues
+        params={params}
+        location={router.location}
+        router={router}
+        routeParams={router.params}
+        routes={router.routes}
+        route={{}}
+      />
+    );
+
+    // Wait for the issues showing up on the table
+    expect(await screen.findByText(`EARTH-${group1}`)).toBeInTheDocument();
+    expect(await screen.findByText(`EARTH-${group2}`)).toBeInTheDocument();
+
+    expect(relatedIssuesMock).toHaveBeenCalled();
+    expect(issuesInfoMock).toHaveBeenCalled();
+  });
+});

commit 40f4374d8027bca97ca6b4d84d768e8f2d1140c4

feat(related_issues): Integrate related issues into Similar Issues tab
This change integrated the trace-connected errors and same-root-cause issues into the Similar Issues tab. It also drops the Related Issues tab (introduced in #67079).

static/app/routes.tsx
@@ -1816,12 +1816,6 @@ function buildRoutes() {
           )}
         />
         <Route
-          path={TabPaths[Tab.RELATED_ISSUES]}
-          component={hoc(
-            make(() => import('sentry/views/issueDetails/groupRelatedIssues'))
-          )}
-        />
-        <Route
           path={TabPaths[Tab.MERGED]}
           component={hoc(make(() => import('sentry/views/issueDetails/groupMerged')))}
         />

static/app/views/issueDetails/groupRelatedIssues/index.tsx
@@ -1,12 +1,9 @@
-// XXX: A lot of the UI for this file will be changed once we use IssueListActions
-// We're using GroupList to help us iterate quickly
+import {Fragment} from 'react';
 import type {RouteComponentProps} from 'react-router';
 import styled from '@emotion/styled';
 
-import Feature from 'sentry/components/acl/feature';
 import {LinkButton} from 'sentry/components/button';
 import GroupList from 'sentry/components/issues/groupList';
-import * as Layout from 'sentry/components/layouts/thirds';
 import Link from 'sentry/components/links/link';
 import LoadingError from 'sentry/components/loadingError';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
@@ -70,107 +67,90 @@ function GroupRelatedIssues({params}: Props) {
   );
 
   return (
-    <Layout.Body>
-      <Layout.Main fullWidth>
-        <HeaderWrapper>
-          <small>
-            {t(
-              'Related Issues are issues that are related in some way and can be acted on together.'
-            )}
-          </small>
-        </HeaderWrapper>
-        {isLoading ? (
-          <LoadingIndicator />
-        ) : isError ? (
-          <LoadingError
-            message={t('Unable to load related issues, please try again later')}
-            onRetry={refetch}
-          />
-        ) : (
+    <Fragment>
+      {isLoading ? (
+        <LoadingIndicator />
+      ) : isError ? (
+        <LoadingError
+          message={t('Unable to load related issues, please try again later')}
+          onRetry={refetch}
+        />
+      ) : (
+        <Fragment>
           <div>
-            <div>
-              <HeaderWrapper>
-                <Title>{t('Issues caused by the same root cause')}</Title>
-                {sameRootCauseIssues.length > 0 ? (
-                  <div>
-                    <TextButtonWrapper>
-                      <div />
-                      <LinkButton
-                        to={`/organizations/${orgSlug}/issues/?query=issue.id:[${groupId},${sameRootCauseIssues}]`}
-                        size="xs"
-                      >
-                        {t('Open in Issues')}
-                      </LinkButton>
-                    </TextButtonWrapper>
-                    <GroupList
-                      endpointPath={`/organizations/${orgSlug}/issues/`}
-                      orgSlug={orgSlug}
-                      queryParams={{query: `issue.id:[${sameRootCauseIssues}]`}}
-                      query=""
-                      source="related-issues-tab"
-                      canSelectGroups={false}
-                      withChart={false}
-                    />
-                  </div>
-                ) : (
-                  <small>{t('No same-root-cause related issues were found.')}</small>
-                )}
-              </HeaderWrapper>
-            </div>
-            <div>
-              <HeaderWrapper>
-                <Title>{t('Trace connected issues')}</Title>
-                {traceConnectedIssues.length > 0 ? (
-                  <div>
-                    <TextButtonWrapper>
-                      <small>
-                        {t('These are the issues belonging to ')}
-                        <Link
-                          to={`/organizations/${orgSlug}/performance/trace/${traceMeta.trace_id}/?node=error-${traceMeta.event_id}`}
-                        >
-                          {t('this trace')}
-                        </Link>
-                      </small>
-                      <LinkButton
-                        to={`/organizations/${orgSlug}/issues/?query=trace:${traceMeta.trace_id}`}
-                        size="xs"
-                      >
-                        {t('Open in Issues')}
-                      </LinkButton>
-                    </TextButtonWrapper>
-                    <GroupList
-                      endpointPath={`/organizations/${orgSlug}/issues/`}
-                      orgSlug={orgSlug}
-                      queryParams={{query: `issue.id:[${traceConnectedIssues}]`}}
-                      query=""
-                      source="related-issues-tab"
-                      canSelectGroups={false}
-                      withChart={false}
-                    />
-                  </div>
-                ) : (
-                  <small>{t('No trace-connected related issues were found.')}</small>
-                )}
-              </HeaderWrapper>
-            </div>
+            <HeaderWrapper>
+              <Title>{t('Issues caused by the same root cause')}</Title>
+              {sameRootCauseIssues.length > 0 ? (
+                <div>
+                  <TextButtonWrapper>
+                    <div />
+                    <LinkButton
+                      to={`/organizations/${orgSlug}/issues/?query=issue.id:[${groupId},${sameRootCauseIssues}]`}
+                      size="xs"
+                    >
+                      {t('Open in Issues')}
+                    </LinkButton>
+                  </TextButtonWrapper>
+                  <GroupList
+                    endpointPath={`/organizations/${orgSlug}/issues/`}
+                    orgSlug={orgSlug}
+                    queryParams={{query: `issue.id:[${sameRootCauseIssues}]`}}
+                    query=""
+                    source="related-issues-tab"
+                    canSelectGroups={false}
+                    withChart={false}
+                  />
+                </div>
+              ) : (
+                <small>{t('No same-root-cause related issues were found.')}</small>
+              )}
+            </HeaderWrapper>
           </div>
-        )}
-      </Layout.Main>
-    </Layout.Body>
-  );
-}
-
-function GroupRelatedIssuesWrapper(props: Props) {
-  return (
-    <Feature features={['related-issues']}>
-      <GroupRelatedIssues {...props} />
-    </Feature>
+          <div>
+            <HeaderWrapper>
+              <Title>{t('Issues in the same trace')}</Title>
+              {traceConnectedIssues.length > 0 ? (
+                <div>
+                  <TextButtonWrapper>
+                    <small>
+                      {t('These issues were all found within ')}
+                      <Link
+                        to={`/organizations/${orgSlug}/performance/trace/${traceMeta.trace_id}/?node=error-${traceMeta.event_id}`}
+                      >
+                        {t('this trace')}
+                      </Link>
+                      .
+                    </small>
+                    <LinkButton
+                      to={`/organizations/${orgSlug}/issues/?query=trace:${traceMeta.trace_id}`}
+                      size="xs"
+                    >
+                      {t('Open in Issues')}
+                    </LinkButton>
+                  </TextButtonWrapper>
+                  <GroupList
+                    endpointPath={`/organizations/${orgSlug}/issues/`}
+                    orgSlug={orgSlug}
+                    queryParams={{query: `issue.id:[${traceConnectedIssues}]`}}
+                    query=""
+                    source="related-issues-tab"
+                    canSelectGroups={false}
+                    withChart={false}
+                  />
+                </div>
+              ) : (
+                <small>{t('No trace-connected related issues were found.')}</small>
+              )}
+            </HeaderWrapper>
+          </div>
+        </Fragment>
+      )}
+    </Fragment>
   );
 }
 
 // Export the component without feature flag controls
 export {GroupRelatedIssues};
-export default GroupRelatedIssuesWrapper;
 
 const Title = styled('h4')`
   margin-bottom: ${space(0.75)};

static/app/views/issueDetails/groupSimilarIssues/index.tsx
@@ -1,4 +1,6 @@
 import Feature from 'sentry/components/acl/feature';
+import * as Layout from 'sentry/components/layouts/thirds';
+import {GroupRelatedIssues} from 'sentry/views/issueDetails/groupRelatedIssues';
 
 import SimilarStackTrace from './similarStackTrace';
 
@@ -6,9 +8,16 @@ type Props = React.ComponentProps<typeof SimilarStackTrace>;
 
 function GroupSimilarIssues({project, ...props}: Props) {
   return (
-    <Feature features="similarity-view" project={project}>
-      <SimilarStackTrace project={project} {...props} />
-    </Feature>
+    <Layout.Body>
+      <Layout.Main fullWidth>
+        <Feature features="related-issues">
+          <GroupRelatedIssues {...props} />
+        </Feature>
+        <Feature features="similarity-view" project={project}>
+          <SimilarStackTrace project={project} {...props} />
+        </Feature>
+      </Layout.Main>
+    </Layout.Body>
   );
 }
 

static/app/views/issueDetails/groupSimilarIssues/similarStackTrace/index.tsx
@@ -7,7 +7,6 @@ import * as qs from 'query-string';
 import Alert from 'sentry/components/alert';
 import EmptyStateWarning from 'sentry/components/emptyStateWarning';
 import HookOrDefault from 'sentry/components/hookOrDefault';
-import * as Layout from 'sentry/components/layouts/thirds';
 import LoadingError from 'sentry/components/loadingError';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
 import Panel from 'sentry/components/panels/panel';
@@ -176,66 +175,62 @@ function SimilarStackTrace({params, location, project}: Props) {
           which refers to whether or not we'd group the similar issue into the main issue.
         </Alert>
       )}
-      <Layout.Body>
-        <Layout.Main fullWidth>
-          <HeaderWrapper>
-            <Title>{t('Issues with a similar stack trace')}</Title>
-            <small>
+      <HeaderWrapper>
+        <Title>{t('Issues with a similar stack trace')}</Title>
+        <small>
+          {t(
+            'This is an experimental feature. Data may not be immediately available while we process merges.'
+          )}
+        </small>
+      </HeaderWrapper>
+      {status === 'loading' && <LoadingIndicator />}
+      {status === 'error' && (
+        <LoadingError
+          message={t('Unable to load similar issues, please try again later')}
+          onRetry={fetchData}
+        />
+      )}
+      {status === 'ready' && !hasSimilarItems && !hasSimilarityEmbeddingsFeature && (
+        <Panel>
+          <EmptyStateWarning>
+            <p>{t("There don't seem to be any similar issues.")}</p>
+          </EmptyStateWarning>
+        </Panel>
+      )}
+      {status === 'ready' && !hasSimilarItems && hasSimilarityEmbeddingsFeature && (
+        <Panel>
+          <EmptyStateWarning>
+            <p>
               {t(
-                'This is an experimental feature. Data may not be immediately available while we process merges.'
+                "There don't seem to be any similar issues. This can occur when the issue has no stacktrace or in-app frames."
               )}
-            </small>
-          </HeaderWrapper>
-          {status === 'loading' && <LoadingIndicator />}
-          {status === 'error' && (
-            <LoadingError
-              message={t('Unable to load similar issues, please try again later')}
-              onRetry={fetchData}
-            />
-          )}
-          {status === 'ready' && !hasSimilarItems && !hasSimilarityEmbeddingsFeature && (
-            <Panel>
-              <EmptyStateWarning>
-                <p>{t("There don't seem to be any similar issues.")}</p>
-              </EmptyStateWarning>
-            </Panel>
-          )}
-          {status === 'ready' && !hasSimilarItems && hasSimilarityEmbeddingsFeature && (
-            <Panel>
-              <EmptyStateWarning>
-                <p>
-                  {t(
-                    "There don't seem to be any similar issues. This can occur when the issue has no stacktrace or in-app frames."
-                  )}
-                </p>
-              </EmptyStateWarning>
-            </Panel>
-          )}
-          {status === 'ready' && hasSimilarItems && !hasSimilarityEmbeddingsFeature && (
-            <List
-              items={items.similar}
-              filteredItems={items.filtered}
-              onMerge={handleMerge}
-              orgId={orgId}
-              project={project}
-              groupId={groupId}
-              pageLinks={items.pageLinks}
-            />
-          )}
-          {status === 'ready' && hasSimilarItems && hasSimilarityEmbeddingsFeature && (
-            <List
-              items={items.similar.concat(items.filtered)}
-              filteredItems={[]}
-              onMerge={handleMerge}
-              orgId={orgId}
-              project={project}
-              groupId={groupId}
-              pageLinks={items.pageLinks}
-            />
-          )}
-          <DataConsentBanner source="grouping" />
-        </Layout.Main>
-      </Layout.Body>
+            </p>
+          </EmptyStateWarning>
+        </Panel>
+      )}
+      {status === 'ready' && hasSimilarItems && !hasSimilarityEmbeddingsFeature && (
+        <List
+          items={items.similar}
+          filteredItems={items.filtered}
+          onMerge={handleMerge}
+          orgId={orgId}
+          project={project}
+          groupId={groupId}
+          pageLinks={items.pageLinks}
+        />
+      )}
+      {status === 'ready' && hasSimilarItems && hasSimilarityEmbeddingsFeature && (
+        <List
+          items={items.similar.concat(items.filtered)}
+          filteredItems={[]}
+          onMerge={handleMerge}
+          orgId={orgId}
+          project={project}
+          groupId={groupId}
+          pageLinks={items.pageLinks}
+        />
+      )}
+      <DataConsentBanner source="grouping" />
     </Fragment>
   );
 }

static/app/views/issueDetails/header.tsx
@@ -142,13 +142,6 @@ function GroupHeaderTabs({
         {t('Merged Issues')}
       </TabList.Item>
       <TabList.Item
-        key={Tab.RELATED_ISSUES}
-        hidden={!organizationFeatures.has('related-issues')}
-        to={`${baseUrl}related/${location.search}`}
-      >
-        {t('Related Issues')}
-      </TabList.Item>
-      <TabList.Item
         key={Tab.SIMILAR_ISSUES}
         hidden={!hasSimilarView || !issueTypeConfig.similarIssues.enabled}
         disabled={disabledTabs.includes(Tab.SIMILAR_ISSUES)}

static/app/views/issueDetails/types.tsx
@@ -7,7 +7,6 @@ export enum Tab {
   EVENTS = 'events',
   MERGED = 'merged',
   SIMILAR_ISSUES = 'similar-issues',
-  RELATED_ISSUES = 'related-issues',
   REPLAYS = 'Replays',
 }
 
@@ -20,6 +19,5 @@ export const TabPaths: Record<Tab, string> = {
   [Tab.EVENTS]: 'events/',
   [Tab.MERGED]: 'merged/',
   [Tab.SIMILAR_ISSUES]: 'similar/',
-  [Tab.RELATED_ISSUES]: 'related/',
   [Tab.REPLAYS]: 'replays/',
 };