ea83df62ca2d4abc155c365c236f87a4ee816010
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-05-02 15:11:26 -0700

    add tab

--- static/app/views/replays/detail/breadcrumbs/useBreadcrumbFilters.tsx
+++ static/app/views/replays/detail/breadcrumbs/useBreadcrumbFilters.tsx
@@ -49,6 +49,7 @@ const TYPE_TO_LABEL: Record<string, string> = {
   click: 'User Click',
   keydown: 'KeyDown',
   input: 'Input',
+  tap: 'Tap',
 };
 
 const OPORCATEGORY_TO_TYPE: Record<string, keyof typeof TYPE_TO_LABEL> = {
