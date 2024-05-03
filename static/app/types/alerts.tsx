381f9cfcbf6b39f67a7e14251da201aa5d279670
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-05-03 10:16:30 -0700

    chore(priority-alerts): Remove high priority alert frontend condition  (#70106)
    FE counterpart to https://github.com/getsentry/sentry/pull/70105

--- static/app/types/alerts.tsx
+++ static/app/types/alerts.tsx
@@ -39,7 +39,6 @@ export const enum IssueAlertConditionType {
   EVENT_FREQUENCY = 'sentry.rules.conditions.event_frequency.EventFrequencyCondition',
   EVENT_UNIQUE_USER_FREQUENCY = 'sentry.rules.conditions.event_frequency.EventUniqueUserFrequencyCondition',
   EVENT_FREQUENCY_PERCENT = 'sentry.rules.conditions.event_frequency.EventFrequencyPercentCondition',
-  HIGH_PRIORITY_ISSUE = 'sentry.rules.conditions.high_priority_issue.HighPriorityIssueCondition',
   NEW_HIGH_PRIORITY_ISSUE = 'sentry.rules.conditions.high_priority_issue.NewIssueCondition',
   EXISTING_HIGH_PRIORITY_ISSUE = 'sentry.rules.conditions.high_priority_issue.ExistingIssueCondition',
 }
