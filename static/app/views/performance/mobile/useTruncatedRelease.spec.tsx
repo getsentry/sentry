3814a0bab4faebee9796a0cb3dcab36fa40275e4
Nar Saynorath <nar.saynorath@sentry.io>
2024-05-06 13:06:28 -0400

    Small hook tests

--- static/app/views/performance/mobile/useTruncatedRelease.spec.tsx
+++ static/app/views/performance/mobile/useTruncatedRelease.spec.tsx
@@ -1,5 +1,6 @@
 import {renderHook} from 'sentry-test/reactTestingLibrary';
 
+import {ELLIPSIS} from 'sentry/utils/profiling/gl/utils';
 import useTruncatedReleaseNames from 'sentry/views/performance/mobile/useTruncatedRelease';
 import {useReleaseSelection} from 'sentry/views/starfish/queries/useReleases';
 
@@ -14,13 +15,25 @@ jest.mocked(useReleaseSelection).mockReturnValue({
 describe('useTruncatedRelease', () => {
   it('truncates long release names to 12 characters by default', () => {
     const {result} = renderHook(useTruncatedReleaseNames);
+
+    expect(
+      [...result.current.truncatedPrimaryRelease].filter(char => char !== ELLIPSIS)
+    ).toHaveLength(12);
+
     expect(result.current.truncatedPrimaryRelease).toBe('2.10.5…1 (42)');
     expect(result.current.truncatedSecondaryRelease).toBe('2.10.3 (42)');
   });
 
   it('truncates long release names to provided length limit', () => {
-    const {result} = renderHook(useTruncatedReleaseNames, {truncation: 5});
-    expect(result.current.truncatedPrimaryRelease).toBe('2.10.5…1 (42)');
-    expect(result.current.truncatedSecondaryRelease).toBe('2.10.3 (42)');
+    const {result} = renderHook(useTruncatedReleaseNames, {
+      initialProps: 5,
+    });
+
+    expect(
+      [...result.current.truncatedPrimaryRelease].filter(char => char !== ELLIPSIS).length
+    ).toBeLessThanOrEqual(5);
+
+    expect(result.current.truncatedPrimaryRelease).toBe('2.…2)');
+    expect(result.current.truncatedSecondaryRelease).toBe('2.…2)');
   });
 });
