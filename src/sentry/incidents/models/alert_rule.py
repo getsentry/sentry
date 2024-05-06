cdaa01bdab31baeb6e38f1dfbc208bb1ba666547
nathan hsieh <hsieh.nathan@gmail.com>
2024-05-06 12:59:16 -0700

    remove Q

--- src/sentry/incidents/models/alert_rule.py
+++ src/sentry/incidents/models/alert_rule.py
@@ -10,7 +10,7 @@ from typing import Any, ClassVar, Protocol, Self
 from django.conf import settings
 from django.core.cache import cache
 from django.db import models
-from django.db.models import Q, QuerySet
+from django.db.models import QuerySet
 from django.db.models.signals import post_delete, post_save
 from django.utils import timezone
 
@@ -89,7 +89,7 @@ class AlertRuleManager(BaseManager["AlertRule"]):
     def fetch_for_organization(self, organization, projects=None):
         queryset = self.filter(organization=organization)
         if projects is not None:
-            queryset = queryset.filter(Q(projects__in=projects)).distinct()
+            queryset = queryset.filter(projects__in=projects).distinct()
 
         return queryset
 
