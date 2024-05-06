112fb097579c009f133e63a2918f3409850c81c3
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-05-06 11:54:49 -0400

    chore(replay): show mark as spam buttons for everyone

--- static/app/components/feedback/feedbackItem/feedbackActions.tsx
+++ static/app/components/feedback/feedbackItem/feedbackActions.tsx
@@ -47,28 +47,17 @@ export default function FeedbackActions({
 }
 
 function LargeWidth({feedbackItem}: {feedbackItem: FeedbackIssue}) {
-  const {
-    isResolved,
-    onResolveClick,
-    hasSpamFeature,
-    isSpam,
-    onSpamClick,
-    hasSeen,
-    onMarkAsReadClick,
-  } = useFeedbackActions({feedbackItem});
+  const {isResolved, onResolveClick, isSpam, onSpamClick, hasSeen, onMarkAsReadClick} =
+    useFeedbackActions({feedbackItem});
 
   return (
     <Fragment>
       <Button priority={isResolved ? 'danger' : 'primary'} onClick={onResolveClick}>
         {isResolved ? t('Unresolve') : t('Resolve')}
       </Button>
-
-      {hasSpamFeature && (
-        <Button priority="default" onClick={onSpamClick}>
-          {isSpam ? t('Move to Inbox') : t('Mark as Spam')}
-        </Button>
-      )}
-
+      <Button priority="default" onClick={onSpamClick}>
+        {isSpam ? t('Move to Inbox') : t('Mark as Spam')}
+      </Button>
       <Button onClick={onMarkAsReadClick}>
         {hasSeen ? t('Mark Unread') : t('Mark Read')}
       </Button>
@@ -77,15 +66,8 @@ function LargeWidth({feedbackItem}: {feedbackItem: FeedbackIssue}) {
 }
 
 function MediumWidth({feedbackItem}: {feedbackItem: FeedbackIssue}) {
-  const {
-    isResolved,
-    onResolveClick,
-    hasSpamFeature,
-    isSpam,
-    onSpamClick,
-    hasSeen,
-    onMarkAsReadClick,
-  } = useFeedbackActions({feedbackItem});
+  const {isResolved, onResolveClick, isSpam, onSpamClick, hasSeen, onMarkAsReadClick} =
+    useFeedbackActions({feedbackItem});
 
   return (
     <Fragment>
@@ -102,13 +84,11 @@ function MediumWidth({feedbackItem}: {feedbackItem: FeedbackIssue}) {
           size: 'xs',
         }}
         items={[
-          hasSpamFeature
-            ? {
-                key: 'spam',
-                label: isSpam ? t('Move to Inbox') : t('Mark as Spam'),
-                onAction: onSpamClick,
-              }
-            : null,
+          {
+            key: 'spam',
+            label: isSpam ? t('Move to Inbox') : t('Mark as Spam'),
+            onAction: onSpamClick,
+          },
           {
             key: 'read',
             label: hasSeen ? t('Mark Unread') : t('Mark Read'),
