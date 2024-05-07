6d53a19d918afe076eee8d0eccb926a28753abc2
Gabe Villalobos <GabeVillalobos@users.noreply.github.com>
2024-05-07 10:55:20 -0700

    fix(hc): Adds fix for evaluation context builder error handler, adds feature option (#69001)

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -1790,6 +1790,10 @@ register(
 register("hybrid_cloud.allow_cross_db_tombstones", default=False, flags=FLAG_AUTOMATOR_MODIFIABLE)
 register("hybrid_cloud.disable_tombstone_cleanup", default=False, flags=FLAG_AUTOMATOR_MODIFIABLE)
 
+# Flagpole Rollout
+register("features", default={}, flags=FLAG_AUTOMATOR_MODIFIABLE)
+register("hybrid_cloud.flagpole_rollout_phase", default=0, flags=FLAG_AUTOMATOR_MODIFIABLE)
+
 # Retry controls
 register("hybridcloud.regionsiloclient.retries", default=5, flags=FLAG_AUTOMATOR_MODIFIABLE)
 register("hybridcloud.rpc.retries", default=5, flags=FLAG_AUTOMATOR_MODIFIABLE)
