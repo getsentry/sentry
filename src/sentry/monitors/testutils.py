6c5bb0a68203a9bbb2440a57c9998998a21b8757
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-05-07 12:21:12 -0400

    ref(crons): Remove unused make_ref_time

--- src/sentry/monitors/testutils.py
+++ src/sentry/monitors/testutils.py
@@ -1,7 +1,5 @@
-import zoneinfo
-from datetime import UTC, datetime
+from datetime import datetime
 
-from django.utils import timezone
 from sentry_kafka_schemas.schema_types.ingest_monitors_v1 import CheckIn
 
 from sentry.monitors.processing_errors import (
@@ -10,35 +8,6 @@ from sentry.monitors.processing_errors import (
     ProcessingErrorType,
 )
 from sentry.monitors.types import CheckinItem, CheckinPayload
-
-
-def make_ref_time(**kwargs):
-    """
-    To accurately reflect the real usage of our check tasks, we want the ref
-    time to be truncated down to a minute for our tests.
-    """
-    tz_name = kwargs.pop("timezone", "UTC")
-
-    ts = timezone.now().replace(**kwargs, tzinfo=zoneinfo.ZoneInfo(tz_name))
-
-    # Typically the task will not run exactly on the minute, but it will
-    # run very close, let's say for our test that it runs 12 seconds after
-    # the minute.
-    #
-    # This is testing that the task correctly clamps its reference time
-    # down to the minute.
-    #
-    # Task timestamps are in UTC, convert our reference time to UTC for this
-    task_run_ts = ts.astimezone(UTC).replace(second=12, microsecond=0)
-
-    # Fan-out tasks recieve a floored version of the timestamp
-    sub_task_run_ts = task_run_ts.replace(second=0)
-
-    # We truncate down to the minute when we mark the next_checkin, do the
-    # same here.
-    trimmed_ts = ts.replace(second=0, microsecond=0)
-
-    return task_run_ts, sub_task_run_ts, trimmed_ts
 
 
 def build_checkin_item(ts=None, partition=0, message_overrides=None, payload_overrides=None):
