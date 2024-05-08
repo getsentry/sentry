8e38d34405c57559608580fdd28fabf541157d47
Raj Joshi <raj.joshi@sentry.io>
2024-05-08 16:22:14 -0700

    revert changes

--- static/app/chartcuterie/performance.tsx
+++ static/app/chartcuterie/performance.tsx
@@ -1,4 +1,3 @@
-import type {LineChartProps} from 'sentry/components/charts/lineChart';
 import {transformToLineSeries} from 'sentry/components/charts/lineChart';
 import getBreakpointChartOptionsFromData, {
   type EventBreakpointChartData,
@@ -11,33 +10,19 @@ import {ChartType} from './types';
 
 export const performanceCharts: RenderDescriptor<ChartType>[] = [];
 
-function modifyOptionsForSlack(options: Omit<LineChartProps, 'series'>) {
-  options.legend = options.legend || {};
-  options.legend.icon = 'none';
-  options.legend.left = '25';
-  options.legend.top = '20';
-
-  return {
-    ...options,
-    grid: slackChartDefaults.grid,
-    visualMap: options.options?.visualMap,
-  };
-}
-
 performanceCharts.push({
   key: ChartType.SLACK_PERFORMANCE_ENDPOINT_REGRESSION,
   getOption: (data: EventBreakpointChartData) => {
     const {chartOptions, series} = getBreakpointChartOptionsFromData(data, theme);
     const transformedSeries = transformToLineSeries({series});
-    const modifiedOptions = modifyOptionsForSlack(chartOptions);
 
     return {
-      ...modifiedOptions,
+      ...chartOptions,
 
       backgroundColor: theme.background,
       series: transformedSeries,
       grid: slackChartDefaults.grid,
-      visualMap: modifiedOptions.options?.visualMap,
+      visualMap: chartOptions.options?.visualMap,
     };
   },
   ...slackChartSize,
