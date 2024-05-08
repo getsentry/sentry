5a78eedf0876dc5480246b1ff3ea946fc96e287b
MichaelSun48 <michael.sun@sentry.io>
2024-05-08 15:24:26 -0700

    Tooltip working

--- static/app/components/assigneeBadge.tsx
+++ static/app/components/assigneeBadge.tsx
@@ -5,8 +5,10 @@ import styled from '@emotion/styled';
 import ActorAvatar from 'sentry/components/avatar/actorAvatar';
 import Tag from 'sentry/components/badge/tag';
 import {Chevron} from 'sentry/components/chevron';
+import ExternalLink from 'sentry/components/links/externalLink';
 import Placeholder from 'sentry/components/placeholder';
-import {tct} from 'sentry/locale';
+import {Tooltip} from 'sentry/components/tooltip';
+import {t, tct} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Actor} from 'sentry/types';
 
@@ -38,14 +40,7 @@ export function AssigneeBadge({
           actor={actor}
           className="avatar"
           size={AVATAR_SIZE}
-          tooltip={
-            <TooltipWrapper>
-              {tct('Assigned to [name]', {
-                name: actor.type === 'team' ? `#${actor.name}` : actor.name,
-              })}
-              {assignmentReason && <TooltipSubtext>{assignmentReason}</TooltipSubtext>}
-            </TooltipWrapper>
-          }
+          hasTooltip={false}
         />
         {showLabel && <Fragment>{actor.name}</Fragment>}
         <Chevron direction="down" size="small" />
@@ -67,10 +62,42 @@ export function AssigneeBadge({
     );
   };
 
+  const makeAssignedTooltipText = (actor: Actor) => {
+    // Cant use StyledTag's tooltipText prop because
+    // it screws with the nested div style ()
+    return (
+      <TooltipWrapper>
+        {tct('Assigned to [name]', {
+          name: actor.type === 'team' ? `#${actor.name}` : actor.name,
+        })}
+        {assignmentReason && <TooltipSubtext>{assignmentReason}</TooltipSubtext>}
+      </TooltipWrapper>
+    );
+  };
+
+  const makeUnAssignedTooltipText = () => {
+    return (
+      <TooltipWrapper>
+        <div>{t('Unassigned')}</div>
+        <TooltipSubtext>
+          {tct('You can auto-assign issues by adding [issueOwners:Issue Owner rules].', {
+            issueOwners: (
+              <TooltipSubExternalLink href="https://docs.sentry.io/product/error-monitoring/issue-owners/" />
+            ),
+          })}
+        </TooltipSubtext>
+      </TooltipWrapper>
+    );
+  };
+
   return assignedTo ? (
-    <StyledTag icon={makeAssignedIcon(assignedTo)} />
+    <Tooltip title={makeAssignedTooltipText(assignedTo)}>
+      <StyledTag icon={makeAssignedIcon(assignedTo)} />
+    </Tooltip>
   ) : (
-    <StyledTag icon={makeUnassignedIcon()} borderStyle="dashed" />
+    <Tooltip title={makeUnAssignedTooltipText()}>
+      <StyledTag icon={makeUnassignedIcon()} borderStyle="dashed" />
+    </Tooltip>
   );
 }
 
@@ -82,15 +109,6 @@ export function AssigneeBadge({
 const TooltipWrapper = styled('div')`
   text-align: left;
 `;
-
-// const TooltipSubExternalLink = styled(ExternalLink)`
-//   color: ${p => p.theme.subText};
-//   text-decoration: underline;
-
-//   :hover {
-//     color: ${p => p.theme.subText};
-//   }
-// `;
 
 const StyledTag = styled(Tag)`
   span {
@@ -108,11 +126,11 @@ const TooltipSubtext = styled('div')`
   color: ${p => p.theme.subText};
 `;
 
-// const PillOutline = styled('div')<{assigneeExists: boolean}>`
-//   display: inline-flex;
-//   padding: 2px 4px 4px 4px;
-//   border: 1px ${p => (p.assigneeExists ? 'solid' : 'dashed')} ${p => p.theme.border};
-//   border-radius: 16px;
-//   align-items: center;
-//   align-self: flex-start;
-// `;
+const TooltipSubExternalLink = styled(ExternalLink)`
+  color: ${p => p.theme.subText};
+  text-decoration: underline;
+
+  :hover {
+    color: ${p => p.theme.subText};
+  }
+`;
