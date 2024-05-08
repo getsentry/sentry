afa014a922678b467faa10d5bd4e4dd80319c775
Kev <6111995+k-fish@users.noreply.github.com>
2024-05-08 13:03:26 -0400

    feat(traces): More improvements, use sdk names now (#70514)
    ### Summary
    This uses sdk names as well as project to differentiate when it comes to
    multi-service single project traces. This also adds some small UI
    tweaks.

--- static/app/views/performance/traces/utils.tsx
+++ static/app/views/performance/traces/utils.tsx
@@ -1,4 +1,5 @@
-import type {TraceResult} from './content';
+import type {SpanResult, TraceResult} from './content';
+import type {Field} from './data';
 
 export function normalizeTraces(traces: TraceResult<string>[] | undefined) {
   if (!traces) {
@@ -8,4 +9,20 @@ export function normalizeTraces(traces: TraceResult<string>[] | undefined) {
     // Only sort name == null to the end, the rest leave in the original order.
     (t1, t2) => (t1.name ? '0' : '1').localeCompare(t2.name ? '0' : '1')
   );
+}
+
+export function getStylingSliceName(
+  sliceName: string | null,
+  sliceSecondaryName: string | null
+) {
+  if (sliceSecondaryName) {
+    // Our color picking relies on the first 4 letters. Since we want to differentiate sdknames and project names we have to include part of the sdk name.
+    return sliceSecondaryName.slice(-2) + (sliceName ?? '');
+  }
+
+  return sliceName;
+}
+
+export function getSecondaryNameFromSpan(span: SpanResult<Field>) {
+  return span['sdk.name'];
 }
