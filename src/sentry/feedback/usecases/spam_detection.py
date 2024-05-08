b987a4552729706f236f8a0670c62c622bcd6991
Josh Ferge <josh.ferge@sentry.io>
2024-05-08 14:31:55 -0700

    feat(feedback): update spam detection prompt (#70544)
    from my testing ([colab
    notebook](https://colab.research.google.com/drive/1d_FPW27gghcCvX0Bw40xx7k5GGAcRb6n?usp=sharing))
    this prompt does a lot better. also sets temperature to 0, which should
    have been done before.

--- src/sentry/feedback/usecases/spam_detection.py
+++ src/sentry/feedback/usecases/spam_detection.py
@@ -5,15 +5,33 @@ from sentry.utils import metrics
 
 logger = logging.getLogger(__name__)
 
-PROMPT = """Classify the text into one of the following two classes: [Junk, Not Junk]. Choose Junk only if you are confident. Text: """
+PROMPT = """
+Please analyze the following input and output `spam` if the input is not coherent, and `not spam` if it is coherent.
+Some example responses:
+  asdfasdf,spam
+  It doesn't work,not spam
+  es funktioniert nicht, not spam
+  لا يعمل,not spam,
+  Nothing,spam
+  ..,spam
+  hey,spam
+Complete the following:
+"""
 
 
 @metrics.wraps("feedback.spam_detection", sample_rate=1.0)
 def is_spam(message):
     is_spam = False
-    response = complete_prompt(usecase=LLMUseCase.SPAM_DETECTION, prompt=PROMPT, message=message)
-    if response and response.strip().lower() in ("junk", "[junk]"):
-        is_spam = True
+    trimmed_response = ""
+    response = complete_prompt(
+        usecase=LLMUseCase.SPAM_DETECTION,
+        prompt=PROMPT,
+        message=message + ",",  # add a comma so it knows to complete the csv
+        temperature=0,
+        max_output_tokens=20,
+    )
+    if response:
+        is_spam, trimmed_response = trim_response(response)
 
     logger.info(
         "Spam detection",
@@ -21,7 +39,23 @@ def is_spam(message):
             "feedback_message": message,
             "is_spam": is_spam,
             "response": response,
+            "trimmed_response": trimmed_response,
         },
     )
     metrics.incr("spam-detection", tags={"is_spam": is_spam}, sample_rate=1.0)
     return is_spam
+
+
+def trim_response(text):
+    trimmed_text = text.strip().lower()
+
+    trimmed_text.replace("`", "")
+
+    import re
+
+    trimmed_text = re.sub(r"\W+", "", trimmed_text)
+
+    if trimmed_text in ("spam", "[spam]"):
+        return True, trimmed_text
+    else:
+        return False, trimmed_text
