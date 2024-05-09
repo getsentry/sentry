61ae1ef9de33d2724ab3caa98a5bd265df897d89
Tillman Elser <trillville@users.noreply.github.com>
2024-05-09 16:14:33 -0700

    ref(seer): separate out all seer connections (#69961)
    there are now 4 different URLs corresponding to the 4 different seer
    deployments/services
    
    - breakpoint detection
    - severity
    - grouping
    - autofix
    
    i have left anomaly detection for now as the endpoint file is still
    there, but in a separate PR we can clean that up too
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- src/sentry/seer/utils.py
+++ src/sentry/seer/utils.py
@@ -47,27 +47,27 @@ class BreakpointResponse(TypedDict):
     data: list[BreakpointData]
 
 
-seer_connection_pool = connection_from_url(
-    settings.ANOMALY_DETECTION_URL,
+seer_grouping_connection_pool = connection_from_url(
+    settings.SEER_GROUPING_URL,
     retries=Retry(
         total=5,
         status_forcelist=[408, 429, 502, 503, 504],
     ),
-    timeout=settings.ANOMALY_DETECTION_TIMEOUT,
+    timeout=settings.SEER_GROUPING_TIMEOUT,
 )
 
-seer_staging_connection_pool = connection_from_url(
-    settings.SEER_AUTOFIX_URL,
+seer_breakpoint_connection_pool = connection_from_url(
+    settings.SEER_BREAKPOINT_DETECTION_URL,
     retries=Retry(
         total=5,
         status_forcelist=[408, 429, 502, 503, 504],
     ),
-    timeout=settings.ANOMALY_DETECTION_TIMEOUT,
+    timeout=settings.SEER_BREAKPOINT_DETECTION_TIMEOUT,
 )
 
 
 def detect_breakpoints(breakpoint_request) -> BreakpointResponse:
-    response = seer_connection_pool.urlopen(
+    response = seer_breakpoint_connection_pool.urlopen(
         "POST",
         "/trends/breakpoint-detector",
         body=json.dumps(breakpoint_request),
@@ -178,7 +178,7 @@ def get_similar_issues_embeddings(
     similar_issues_request: SimilarIssuesEmbeddingsRequest,
 ) -> list[SeerSimilarIssueData]:
     """Request similar issues data from seer and normalize the results."""
-    response = seer_staging_connection_pool.urlopen(
+    response = seer_grouping_connection_pool.urlopen(
         "POST",
         SEER_SIMILAR_ISSUES_URL,
         body=json.dumps(similar_issues_request),
