a82c9ccb3435f2f71565765f469d4262b95fab06
William Mak <william@wmak.io>
2024-05-09 15:06:48 -0400

    fix: Move quick trace query to only happen for ANR
    - Don't hit the lite response anymore either cause we don't need it
    - Add an option to pass spans at the top level

--- static/app/utils/performance/quickTrace/quickTraceQuery.tsx
+++ static/app/utils/performance/quickTrace/quickTraceQuery.tsx
@@ -18,9 +18,16 @@ import {
 type QueryProps = Omit<DiscoverQueryProps, 'api' | 'eventView'> & {
   children: (props: QuickTraceQueryChildrenProps) => React.ReactNode;
   event: Event | undefined;
+  skipLight: boolean;
+  type?: 'detailed' | 'spans';
 };
 
-export default function QuickTraceQuery({children, event, ...props}: QueryProps) {
+export default function QuickTraceQuery({
+  children,
+  event,
+  skipLight,
+  ...props
+}: QueryProps) {
   const renderEmpty = () => (
     <Fragment>
       {children({
@@ -44,6 +51,83 @@ export default function QuickTraceQuery({children, event, ...props}: QueryProps)
   }
 
   const {start, end} = getTraceTimeRangeFromEvent(event);
+  if (skipLight) {
+    return (
+      <TraceFullQuery
+        eventId={event.id}
+        traceId={traceId}
+        start={start}
+        end={end}
+        {...props}
+      >
+        {traceFullResults => {
+          const scope = new Sentry.Scope();
+          const traceErrorMsg =
+            'Updated: Trace endpoints returning non-array in response';
+          scope.setFingerprint([traceErrorMsg]);
+
+          if (
+            !traceFullResults.isLoading &&
+            traceFullResults.error === null &&
+            traceFullResults.traces !== null
+          ) {
+            const orphanError = traceFullResults.traces.orphan_errors?.find(
+              e => e.event_id === event.id
+            );
+            if (orphanError) {
+              return children({
+                ...traceFullResults,
+                trace: [],
+                orphanErrors: [orphanError],
+                currentEvent: orphanError,
+              });
+            }
+
+            const traceTransactions = traceFullResults.traces.transactions;
+
+            try {
+              for (const subtrace of traceTransactions) {
+                try {
+                  const trace = flattenRelevantPaths(event, subtrace);
+                  return children({
+                    ...traceFullResults,
+                    trace,
+                    currentEvent: trace.find(e => isCurrentEvent(e, event)) ?? null,
+                  });
+                } catch {
+                  // let this fall through and check the next subtrace
+                  // or use the trace lite results
+                }
+              }
+            } catch {
+              // capture exception and let this fall through to
+              // use the /events-trace-lite/ response below
+              scope.setExtras({
+                traceTransactions,
+                traceFullResults,
+              });
+              Sentry.captureException(new Error(traceErrorMsg), scope);
+            }
+          }
+
+          return children({
+            // only use the light results loading state if it didn't error
+            // if it did, we should rely on the full results
+            isLoading: traceFullResults.isLoading,
+            // swallow any errors from the light results because we
+            // should rely on the full results in this situations
+            error: traceFullResults.error,
+            trace: [],
+            // if we reach this point but there were some traces in the full results,
+            // that means there were other transactions in the trace, but the current
+            // event could not be found
+            type: traceFullResults.traces?.transactions?.length ? 'missing' : 'empty',
+            currentEvent: null,
+          });
+        }}
+      </TraceFullQuery>
+    );
+  }
 
   return (
     <TraceLiteQuery
--- static/app/views/issueDetails/groupEventDetails/groupEventDetailsContent.tsx
+++ static/app/views/issueDetails/groupEventDetails/groupEventDetailsContent.tsx
@@ -51,7 +51,10 @@ import {
 import type {EventTransaction} from 'sentry/types/event';
 import {EntryType} from 'sentry/types/event';
 import {shouldShowCustomErrorResourceConfig} from 'sentry/utils/issueTypeConfig';
+import {QuickTraceContext} from 'sentry/utils/performance/quickTrace/quickTraceContext';
+import QuickTraceQuery from 'sentry/utils/performance/quickTrace/quickTraceQuery';
 import {getReplayIdFromEvent} from 'sentry/utils/replays/getReplayIdFromEvent';
+import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import {ResourcesAndMaybeSolutions} from 'sentry/views/issueDetails/resourcesAndMaybeSolutions';
 
@@ -97,6 +100,7 @@ function DefaultGroupEventDetailsContent({
 }: Required<GroupEventDetailsContentProps>) {
   const organization = useOrganization();
   const hasNewTagsUI = useHasNewTagsUI();
+  const location = useLocation();
   const tagsRef = useRef<HTMLDivElement>(null);
 
   const projectSlug = project.slug;
@@ -187,7 +191,21 @@ function DefaultGroupEventDetailsContent({
       <GroupEventEntry entryType={EntryType.STACKTRACE} {...eventEntryProps} />
       <GroupEventEntry entryType={EntryType.THREADS} {...eventEntryProps} />
       {hasAnrImprovementsFeature && isANR && (
-        <AnrRootCause event={event} organization={organization} />
+        <QuickTraceQuery
+          event={event}
+          location={location}
+          orgSlug={organization.slug}
+          type={'spans'}
+          skipLight
+        >
+          {results => {
+            return (
+              <QuickTraceContext.Provider value={results}>
+                <AnrRootCause event={event} organization={organization} />
+              </QuickTraceContext.Provider>
+            );
+          }}
+        </QuickTraceQuery>
       )}
       {group.issueCategory === IssueCategory.PERFORMANCE && (
         <SpanEvidenceSection
