f567c88d4fce6095d14d1d6f3665a3cbc0ba14ba
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2024-05-09 11:51:29 -0400

    ref(starifish): require referrer in discover hooks (#70540)
    Every discover query should have a referrer

--- static/app/views/performance/transactionSummary/transactionSpans/spanSummary/spanSummaryCharts.tsx
+++ static/app/views/performance/transactionSummary/transactionSpans/spanSummary/spanSummaryCharts.tsx
@@ -44,24 +44,28 @@ function SpanSummaryCharts() {
     isLoading: isThroughputDataLoading,
     data: throughputData,
     error: throughputError,
-  } = useSpanMetricsSeries({
-    search: MutableSearch.fromQueryObject(filters),
-    yAxis: ['spm()'],
-    enabled: Boolean(groupId),
-    referrer: SpanSummaryReferrer.SPAN_SUMMARY_THROUGHPUT_CHART,
-  });
+  } = useSpanMetricsSeries(
+    {
+      search: MutableSearch.fromQueryObject(filters),
+      yAxis: ['spm()'],
+      enabled: Boolean(groupId),
+    },
+    SpanSummaryReferrer.SPAN_SUMMARY_THROUGHPUT_CHART
+  );
 
   const {
     isLoading: isAvgDurationDataLoading,
     data: avgDurationData,
     error: avgDurationError,
-  } = useSpanMetricsSeries({
-    search: MutableSearch.fromQueryObject(filters),
-    // TODO: Switch this to SPAN_DURATION before release
-    yAxis: [`avg(${SpanMetricsField.SPAN_SELF_TIME})`],
-    enabled: Boolean(groupId),
-    referrer: SpanSummaryReferrer.SPAN_SUMMARY_DURATION_CHART,
-  });
+  } = useSpanMetricsSeries(
+    {
+      search: MutableSearch.fromQueryObject(filters),
+      // TODO: Switch this to SPAN_DURATION before release
+      yAxis: [`avg(${SpanMetricsField.SPAN_SELF_TIME})`],
+      enabled: Boolean(groupId),
+    },
+    SpanSummaryReferrer.SPAN_SUMMARY_DURATION_CHART
+  );
 
   const eventView = EventView.fromNewQueryWithLocation(
     {
