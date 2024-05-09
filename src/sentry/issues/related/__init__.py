f662031d0919685a8c98a1d05ea4035f32cef8aa
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-05-09 15:25:42 -0400

    feat(related_issues): Decouple endpoint (#70504)
    The API tries to find issues for both approaches sequentially (slower),
    making it harder to iterate on each approach independently.
    
    This change allows using a `type` parameter with the endpoint to execute
    one approach or the other.
    
    The UI will be changed to make two independent requests, thus, improving
    load time.
    
    This is required to empower trace issues from the issues details page.
    
    ---------
    
    Co-authored-by: Matt Duncan <14761+mrduncan@users.noreply.github.com>

--- src/sentry/issues/related/__init__.py
+++ src/sentry/issues/related/__init__.py
@@ -5,7 +5,7 @@ from sentry.models.group import Group
 from .same_root_cause import same_root_cause_analysis
 from .trace_connected import trace_connected_analysis
 
-__all__ = ["find_related_issues"]
+__all__ = ["find_related_issues", "same_root_cause_analysis", "trace_connected_analysis"]
 
 RELATED_ISSUES_ALGORITHMS = {
     "same_root_cause": same_root_cause_analysis,
