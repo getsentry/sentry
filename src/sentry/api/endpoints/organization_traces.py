59a5cafc40564997ada131dcc533128ef2fbee6f
Tony Xiao <txiao@sentry.io>
2024-05-10 16:02:14 -0400

    fix(trace-explorer): Merge parallel spans
    Parallel spans can create a deeply nested timeline. This tries to merge them
    together for a cleaner flatten visualization. We're explicitly not merging root
    spans as that can create strange visualizations.

--- src/sentry/api/endpoints/organization_traces.py
+++ src/sentry/api/endpoints/organization_traces.py
@@ -42,6 +42,7 @@ class TraceInterval(TypedDict):
     opCategory: str | None
     duration: int
     components: NotRequired[list[tuple[int, int]]]
+    isRoot: bool
 
 
 class TraceResult(TypedDict):
@@ -760,6 +761,7 @@ class TraceSamplesExecutor:
                 "transaction",
                 "span.category",
                 "sdk.name",
+                "parent_span",
                 "precise.start_ts",
                 "precise.finish_ts",
             ],
@@ -1000,7 +1002,12 @@ def process_breakdowns(data, traces_range):
 
     def should_merge(interval_a, interval_b):
         return (
-            interval_a["end"] >= interval_b["start"]
+            # only merge intervals that have parent spans, i.e. those that aren't the trace root
+            not interval_a["isRoot"]
+            and not interval_b["isRoot"]
+            # only merge intervals that overlap
+            and interval_a["end"] >= interval_b["start"]
+            # only merge intervals that are part of the same service
             and interval_a["project"] == interval_b["project"]
             and interval_a["sdkName"] == interval_b["sdkName"]
             and interval_a["opCategory"] == interval_b["opCategory"]
@@ -1032,14 +1039,16 @@ def process_breakdowns(data, traces_range):
                     "components": [
                         (last_interval["components"][-1][1], interval["components"][0][0]),
                     ],
+                    "isRoot": False,
                 }
             )
 
         breakdown.append(interval)
 
     def stack_push(trace, interval):
-        last_interval = stack_peek(trace)
-        if last_interval and should_merge(last_interval, interval):
+        for last_interval in reversed(stacks[trace]):
+            if not should_merge(last_interval, interval):
+                continue
             # update the end of this interval and it will
             # be updated in the breakdown as well
             last_interval["end"] = max(interval["end"], last_interval["end"])
@@ -1131,6 +1140,7 @@ def process_breakdowns(data, traces_range):
             "end": row["quantized.finish_ts"],
             "duration": 0,
             "components": [(row["precise.start_ts"], row["precise.finish_ts"])],
+            "isRoot": not bool(row.get("parent_span")),
         }
 
         # Clear the stack of any intervals that end before the current interval
@@ -1138,11 +1148,6 @@ def process_breakdowns(data, traces_range):
         stack_clear(trace, until=cur["start"])
 
         stack_push(trace, cur)
-
-        # Clear the stack of any intervals that end before the current interval
-        # ends. Here we do not need to push them to the breakdowns because
-        # that time has already be attributed to the most recent interval.
-        stack_clear(trace, until=cur["end"])
 
     for trace, trace_range in traces_range.items():
         # Check to see if there is still a gap before the trace ends and fill it
@@ -1158,6 +1163,7 @@ def process_breakdowns(data, traces_range):
             "start": other_start,
             "end": other_end,
             "duration": 0,
+            "isRoot": False,
         }
 
         # Clear the remaining intervals on the stack to find the latest end time
