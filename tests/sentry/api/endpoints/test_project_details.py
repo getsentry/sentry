3aade54c7bf83614f22132e6b56191848d38d210
Raj Joshi <raj.joshi@sentry.io>
2024-05-10 09:38:19 -0700

    feat(api-idorslug): Updated Subset of Endpoints to use `organization_id_or_slug` (#70636)
    A subset of changes from https://github.com/getsentry/sentry/pull/70081!

--- tests/sentry/api/endpoints/test_project_details.py
+++ tests/sentry/api/endpoints/test_project_details.py
@@ -269,7 +269,7 @@ class ProjectUpdateTestTokenAuthenticated(APITestCase):
         self.url = reverse(
             "sentry-api-0-project-details",
             kwargs={
-                "organization_slug": self.project.organization.slug,
+                "organization_id_or_slug": self.project.organization.slug,
                 "project_id_or_slug": self.project.slug,
             },
         )
@@ -1597,7 +1597,7 @@ class TestProjectDetailsDynamicSamplingBiases(TestProjectDetailsDynamicSamplingB
         self.url = reverse(
             "sentry-api-0-project-details",
             kwargs={
-                "organization_slug": self.project.organization.slug,
+                "organization_id_or_slug": self.project.organization.slug,
                 "project_id_or_slug": self.project.slug,
             },
         )
@@ -1698,7 +1698,7 @@ class TestProjectDetailsDynamicSamplingBiases(TestProjectDetailsDynamicSamplingB
         url = reverse(
             "sentry-api-0-project-details",
             kwargs={
-                "organization_slug": self.project.organization.slug,
+                "organization_id_or_slug": self.project.organization.slug,
                 "project_id_or_slug": self.project.slug,
             },
         )
@@ -1742,7 +1742,7 @@ class TestProjectDetailsDynamicSamplingBiases(TestProjectDetailsDynamicSamplingB
         url = reverse(
             "sentry-api-0-project-details",
             kwargs={
-                "organization_slug": self.project.organization.slug,
+                "organization_id_or_slug": self.project.organization.slug,
                 "project_id_or_slug": self.project.slug,
             },
         )
