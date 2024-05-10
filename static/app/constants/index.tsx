576aa81310daeebefb16788848b5a775bf18053d
Scott Cooper <scttcper@gmail.com>
2024-05-10 14:13:48 -0700

    profileSecond api name

--- static/app/constants/index.tsx
+++ static/app/constants/index.tsx
@@ -328,7 +328,7 @@ export const DATA_CATEGORY_INFO = {
   },
   [DataCategoryExact.PROFILE_DURATION]: {
     name: DataCategoryExact.PROFILE_DURATION,
-    apiName: 'profileSeconds',
+    apiName: 'profileSecond',
     plural: 'profileSeconds',
     displayName: 'profile seconds',
     titleName: t('Profile Seconds'),
