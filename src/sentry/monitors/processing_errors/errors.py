8445bf95e64ee9014d4b3b424fbd86ce24ee3299
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-05-13 18:12:00 -0400

    feat(crons): Better typing on processing errors

--- src/sentry/monitors/processing_errors/errors.py
+++ src/sentry/monitors/processing_errors/errors.py
@@ -1,108 +1,260 @@
 from __future__ import annotations
 
-import dataclasses
-import logging
 import uuid
-from enum import Enum
-from typing import Any, TypedDict
+from dataclasses import dataclass, field
+from enum import IntEnum
+from typing import Literal, Mapping, Sequence, TypedDict, Union
 
 from sentry.monitors.models import Monitor
 from sentry.monitors.types import CheckinItem, CheckinItemData
 
-logger = logging.getLogger(__name__)
 
+class ProcessingErrorType(IntEnum):
+    """
+    Enum used as a discriminate tag for each type of processing error
+    """
 
-class ProcessingErrorType(Enum):
     CHECKIN_ENVIRONMENT_MISMATCH = 0
-    """The environment sent with the checkin update doesn't match the environment already associated with the checkin"""
     CHECKIN_FINISHED = 1
-    """The checkin was already completed and we attempted to modify it"""
     CHECKIN_GUID_PROJECT_MISMATCH = 2
-    """The guid for the checkin matched a checkin that was related to a different project than the one provided in the DSN"""
     CHECKIN_INVALID_DURATION = 3
-    """We dropped a checkin due to invalid duration"""
     CHECKIN_INVALID_GUID = 4
-    """GUID passed with checkin is invalid"""
     CHECKIN_VALIDATION_FAILED = 5
-    """Checkin format was invalid"""
     MONITOR_DISABLED = 6
-    """Monitor was disabled for a non-billing related reason"""
     MONITOR_DISABLED_NO_QUOTA = 7
-    """Monitor was disabled and we couldn't assign a seat"""
     MONITOR_INVALID_CONFIG = 8
-    """A monitor wasn't found, and we failed to upsert due to invalid config"""
     MONITOR_INVALID_ENVIRONMENT = 9
-    """The environment information passed with the checkin was invalid"""
     MONITOR_LIMIT_EXCEEDED = 10
-    """The maximum number of monitors allowed per project has been exceeded"""
     MONITOR_NOT_FOUND = 11
-    """Monitor with the provided slug doesn't exist, and either no or invalid upsert data provided"""
     MONITOR_OVER_QUOTA = 12
-    """This monitor can't accept checkins and is over quota"""
     MONITOR_ENVIRONMENT_LIMIT_EXCEEDED = 13
-    """The monitor has too many environments associated with it already, can't add another"""
     MONITOR_ENVIRONMENT_RATELIMITED = 14
-    """This monitor environment is sending checkins too frequently"""
     ORGANIZATION_KILLSWITCH_ENABLED = 15
-    """We have disabled checkin ingestion for this org. Contact support for details"""
 
 
-class CheckinValidationError(Exception):
-    def __init__(self, processing_errors: list[ProcessingError], monitor: Monitor | None = None):
-        # Monitor is optional, since we don't always have the monitor related to the checkin available
+class CheckinEnvironmentMismatch(TypedDict):
+    """
+    The environment sent with the checkin update doesn't match the environment
+    already associated with the checkin
+    """
+
+    type: Literal[ProcessingErrorType.CHECKIN_ENVIRONMENT_MISMATCH]
+
+    existing_environment: str
+    """
+    Name of the environment that is already associated with the check-in
+    """
+
+
+class CheckinFinished(TypedDict):
+    """
+    The checkin was already completed and we attempted to modify it
+    """
+
+    type: Literal[ProcessingErrorType.CHECKIN_FINISHED]
+
+
+class CheckinGuidProjectMismatch(TypedDict):
+    """
+    The guid for the checkin matched a checkin that was related to a different
+    project than the one provided in the DSN
+    """
+
+    type: Literal[ProcessingErrorType.CHECKIN_GUID_PROJECT_MISMATCH]
+
+    guid: str
+    """
+    The guid which is assoicated to a different project
+    """
+
+
+class CheckinInvalidDuration(TypedDict):
+    """
+    We dropped a checkin due to invalid duration
+    """
+
+    type: Literal[ProcessingErrorType.CHECKIN_INVALID_DURATION]
+
+    duration: str
+    """
+    The user provided duration
+    """
+
+
+class CheckinInvalidGuid(TypedDict):
+    """
+    GUID passed with checkin is invalid
+    """
+
+    type: Literal[ProcessingErrorType.CHECKIN_INVALID_GUID]
+
+
+class CheckinValidationFailed(TypedDict):
+    """
+    Checkin format was invalid
+    """
+
+    type: Literal[ProcessingErrorType.CHECKIN_VALIDATION_FAILED]
+
+    errors: Mapping[str, Sequence[str]]
+    """
+    Mapping of check-in field name to the problems with that field
+    """
+
+
+class MonitorDisabled(TypedDict):
+    """
+    Monitor was disabled for a non-billing related reason
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_DISABLED]
+
+
+class MonitorDisabledNoQuota(TypedDict):
+    """
+    Monitor was disabled and we couldn't assign a seat
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_DISABLED_NO_QUOTA]
+
+
+class MonitorInvalidConfig(TypedDict):
+    """
+    A monitor wasn't found, and we failed to upsert due to invalid config
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_INVALID_CONFIG]
+
+    errors: Mapping[str, Sequence[str]]
+    """
+    Mapping of monitor config field name to the problems with that field
+    """
+
+
+class MonitorInvalidEnvironment(TypedDict):
+    """
+    The environment information passed with the checkin was invalid
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_INVALID_ENVIRONMENT]
+    reason: str
+
+
+class MonitorLimitExceeded(TypedDict):
+    """
+    The maximum number of monitors allowed per project has been exceeded
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_LIMIT_EXCEEDED]
+    reason: str
+
+
+class MonitorNotFound(TypedDict):
+    """
+    Monitor with the provided slug doesn't exist, and either no or invalid
+    upsert data provided
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_NOT_FOUND]
+
+
+class MonitorOverQuota(TypedDict):
+    """
+    This monitor can't accept checkins and is over quota
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_OVER_QUOTA]
+
+
+class MonitorEnvironmentLimitExceeded(TypedDict):
+    """
+    The monitor has too many environments associated with it already, can't add
+    another
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_ENVIRONMENT_LIMIT_EXCEEDED]
+    reason: str
+
+
+class MonitorEnviromentRateLimited(TypedDict):
+    """
+    This monitor environment is sending checkins too frequently
+    """
+
+    type: Literal[ProcessingErrorType.MONITOR_ENVIRONMENT_RATELIMITED]
+
+
+class OrganizationKillswitchEnabled(TypedDict):
+    """
+    We have disabled checkin ingestion for this org. Contact support for details
+    """
+
+    type: Literal[ProcessingErrorType.ORGANIZATION_KILLSWITCH_ENABLED]
+
+
+ProcessingError = Union[
+    CheckinEnvironmentMismatch,
+    CheckinFinished,
+    CheckinGuidProjectMismatch,
+    CheckinInvalidDuration,
+    CheckinInvalidGuid,
+    CheckinValidationFailed,
+    MonitorDisabled,
+    MonitorDisabledNoQuota,
+    MonitorInvalidConfig,
+    MonitorInvalidEnvironment,
+    MonitorLimitExceeded,
+    MonitorNotFound,
+    MonitorOverQuota,
+    MonitorEnvironmentLimitExceeded,
+    MonitorEnviromentRateLimited,
+    OrganizationKillswitchEnabled,
+]
+
+
+class ProcessingErrorsException(Exception):
+    """
+    This exception should be raised wtih a list of ProcessingError representing
+    the problems which occured while processing a monitor check-in.
+    """
+
+    def __init__(
+        self,
+        processing_errors: Sequence[ProcessingError],
+        monitor: Monitor | None = None,
+    ):
         self.processing_errors = processing_errors
+
+        # Monitor is optional, since we don't always have the monitor related
+        # to the checkin available
         self.monitor = monitor
 
 
-class ProcessingErrorData(TypedDict):
-    type: str
-    data: dict[str, Any]
-
-
-@dataclasses.dataclass(frozen=True)
-class ProcessingError:
-    type: ProcessingErrorType
-    data: dict[str, Any] = dataclasses.field(default_factory=dict)
-
-    def to_dict(self) -> ProcessingErrorData:
-        return {
-            "type": self.type.name,
-            "data": self.data,
-        }
-
-    @classmethod
-    def from_dict(cls, processing_error_data: ProcessingErrorData) -> ProcessingError:
-        return cls(
-            ProcessingErrorType[processing_error_data["type"]],
-            processing_error_data["data"],
-        )
-
-
 class CheckinProcessingErrorData(TypedDict):
-    errors: list[ProcessingErrorData]
-    checkin: CheckinItemData
     id: str
+    checkin: CheckinItemData
+    errors: Sequence[ProcessingError]
 
 
-@dataclasses.dataclass(frozen=True)
+@dataclass(frozen=True)
 class CheckinProcessingError:
-    errors: list[ProcessingError]
+    errors: Sequence[ProcessingError]
     checkin: CheckinItem
-    id: uuid.UUID = dataclasses.field(default_factory=uuid.uuid4)
+    id: uuid.UUID = field(default_factory=uuid.uuid4)
 
     def to_dict(self) -> CheckinProcessingErrorData:
         return {
-            "errors": [error.to_dict() for error in self.errors],
-            "checkin": self.checkin.to_dict(),
             "id": self.id.hex,
+            "checkin": self.checkin.to_dict(),
+            "errors": self.errors,
         }
 
     @classmethod
     def from_dict(cls, data: CheckinProcessingErrorData) -> CheckinProcessingError:
         return cls(
-            errors=[ProcessingError.from_dict(error) for error in data["errors"]],
-            checkin=CheckinItem.from_dict(data["checkin"]),
             id=uuid.UUID(data["id"]),
+            checkin=CheckinItem.from_dict(data["checkin"]),
+            errors=data["errors"],
         )
 
     def __hash__(self):
--- src/sentry/monitors/testutils.py
+++ src/sentry/monitors/testutils.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Sequence
 
 from sentry_kafka_schemas.schema_types.ingest_monitors_v1 import CheckIn
 
@@ -41,10 +42,13 @@ def build_checkin_item(
 
 
 def build_checkin_processing_error(
-    processing_errors: list[ProcessingError] | None = None, **checkin_item_params
+    processing_errors: Sequence[ProcessingError] | None = None, **checkin_item_params
 ):
     if processing_errors is None:
         processing_errors = [
-            ProcessingError(ProcessingErrorType.MONITOR_DISABLED, {"some": "data"})
+            {
+                "type": ProcessingErrorType.CHECKIN_ENVIRONMENT_MISMATCH,
+                "existing_environment": "some-env",
+            },
         ]
     return CheckinProcessingError(processing_errors, build_checkin_item(**checkin_item_params))
