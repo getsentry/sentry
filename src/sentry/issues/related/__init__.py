de075a48eb4609cfa934670606ec8e40b9c8e9ea
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-05-13 08:12:50 -0400

    deprecate(related_issues): Drop support for doing all types of related issues (#70606)
    This is a follow-up to #70504. Once the UI stops using this approach we
    will merge this change.

--- src/sentry/issues/related/__init__.py
+++ src/sentry/issues/related/__init__.py
@@ -1,7 +1,5 @@
 """This module exports a function to find related issues. It groups them by type."""
 
-from sentry.models.group import Group
-
 from .same_root_cause import same_root_cause_analysis
 from .trace_connected import trace_connected_analysis
 
@@ -11,12 +9,3 @@ RELATED_ISSUES_ALGORITHMS = {
     "same_root_cause": same_root_cause_analysis,
     "trace_connected": trace_connected_analysis,
 }
-
-
-def find_related_issues(group: Group) -> list[dict[str, str | list[int] | dict[str, str]]]:
-    related_issues: list[dict[str, str | list[int] | dict[str, str]]] = []
-    for key, func in RELATED_ISSUES_ALGORITHMS.items():
-        data, meta = func(group)
-        related_issues.append({"type": key, "data": data, "meta": meta})
-
-    return related_issues
