229408e5efd7cded39765f75df53869a3936f075
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-05-13 11:00:14 -0400

    ref(perf): Improve `SpanDescriptionLink` (#70755)
    ## Changes
    
    - Remove `extractRoute` helper
        
    It _always_ returns `null` because the Starfish routes are gone, so it's
        pointless.
    
    - `SpanDescriptionLink` --> `SpanGroupDetailsLink`
        
        More descriptive! The component is, specifically, a link to a span's
    group details page. This is different from other description links, like
        when span descriptions link to a domain page.
    
    - Remove unused parameters
        
    The `endpoint` and `endpointMethod` parameters are left over from early
        Starfish days. Nothing provides or checks those parameters anymore.

--- static/app/views/starfish/components/spanGroupDetailsLink.tsx
+++ static/app/views/starfish/components/spanGroupDetailsLink.tsx
@@ -6,7 +6,6 @@ import useOrganization from 'sentry/utils/useOrganization';
 import {normalizeUrl} from 'sentry/utils/withDomainRequired';
 import {OverflowEllipsisTextContainer} from 'sentry/views/starfish/components/textAlign';
 import {SpanMetricsField} from 'sentry/views/starfish/types';
-import {extractRoute} from 'sentry/views/starfish/utils/extractRoute';
 import {useRoutingContext} from 'sentry/views/starfish/utils/routingContext';
 
 const {SPAN_OP} = SpanMetricsField;
@@ -14,20 +13,11 @@ const {SPAN_OP} = SpanMetricsField;
 interface Props {
   description: React.ReactNode;
   projectId: number;
-  endpoint?: string;
-  endpointMethod?: string;
   group?: string;
   spanOp?: string;
 }
 
-export function SpanDescriptionLink({
-  group,
-  projectId,
-  endpoint,
-  endpointMethod,
-  spanOp,
-  description,
-}: Props) {
+export function SpanGroupDetailsLink({group, projectId, spanOp, description}: Props) {
   const location = useLocation();
   const organization = useOrganization();
   const routingContext = useRoutingContext();
@@ -35,8 +25,6 @@ export function SpanDescriptionLink({
   const queryString = {
     ...location.query,
     project: projectId,
-    endpoint,
-    endpointMethod,
     ...(spanOp ? {[SPAN_OP]: spanOp} : {}),
   };
 
@@ -45,9 +33,7 @@ export function SpanDescriptionLink({
       {group ? (
         <Link
           to={normalizeUrl(
-            `/organizations/${organization.slug}${routingContext.baseURL}/${
-              extractRoute(location) ?? 'spans'
-            }/span/${group}/?${qs.stringify(queryString)}`
+            `/organizations/${organization.slug}${routingContext.baseURL}/spans/span/${group}/?${qs.stringify(queryString)}`
           )}
         >
           {description}
