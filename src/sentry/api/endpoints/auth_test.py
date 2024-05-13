aacd5d4e70c8832a40ab15ae745c444f45c101be
mdtro <20070360+mdtro@users.noreply.github.com>
2024-05-13 18:23:41 -0500

    reduce rate limit

---
+++ src/sentry/api/endpoints/auth_test.py
@@ -0,0 +1,35 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK, HTTP_403_FORBIDDEN
+
+from sentry.api.api_owners import ApiOwner
+from sentry.api.api_publish_status import ApiPublishStatus
+from sentry.api.authentication import (
+    OrgAuthTokenAuthentication,
+    SessionAuthentication,
+    UserAuthTokenAuthentication,
+)
+from sentry.api.base import Endpoint, control_silo_endpoint
+from sentry.types.ratelimit import RateLimit, RateLimitCategory
+
+
+@control_silo_endpoint
+class AuthTestEndpoint(Endpoint):
+    """Simple API endpoint to validate successful authentication"""
+
+    publish_status = {"GET": ApiPublishStatus.PUBLIC}
+    owner = ApiOwner.SECURITY
+    authentication_classes = (
+        SessionAuthentication,
+        UserAuthTokenAuthentication,
+        OrgAuthTokenAuthentication,
+    )
+    permission_classes = ()
+
+    enforce_rate_limit = True
+    rate_limits = {"GET": {RateLimitCategory.IP: RateLimit(limit=1250, window=1)}}
+
+    def get(self, request: Request) -> Response:
+        if request.auth or request.user.is_authenticated:
+            return Response(status=HTTP_200_OK)
+        return Response(status=HTTP_403_FORBIDDEN)
