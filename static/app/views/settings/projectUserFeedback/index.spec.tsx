5d34949e9fc747aa775cf5ca68d8576e14e1af78
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-05-14 09:37:37 -0700

    ref(feedback): consolidate all settings into one heading under project (#70679)
    closes https://github.com/getsentry/sentry/issues/70663
    as a fun bonus, this is also a deprecated async conversion so ref
    https://github.com/getsentry/frontend-tsc/issues/2
    
    
    ### Before, we had two User Feedback headings in project settings, one
    under Processing (for spam detection) and one under SDK Setup (for crash
    report alerting options):
    
    <img width="1154" alt="SCR-20240510-nqlf"
    src="https://github.com/getsentry/sentry/assets/56095982/05031369-22da-48bd-8686-a66f8c20f18c">
    
    ### This PR moves all 3 settings into one User Feedback header under the
    main Project heading:
    <img width="684" alt="SCR-20240510-nsig-2"
    src="https://github.com/getsentry/sentry/assets/56095982/d58b8937-9dfc-4eff-9ec4-e809161cbc9f">

--- static/app/views/settings/projectUserFeedback/index.spec.tsx
+++ static/app/views/settings/projectUserFeedback/index.spec.tsx
@@ -3,7 +3,58 @@ import {ProjectFixture} from 'sentry-fixture/project';
 import {initializeOrg} from 'sentry-test/initializeOrg';
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
-import ProjectUserFeedbackProcessing from 'sentry/views/settings/project/projectUserFeedbackProcessing';
+import ProjectUserFeedback from 'sentry/views/settings/projectUserFeedback';
+
+describe('ProjectUserFeedback', function () {
+  const {routerProps, organization, project, routerContext} = initializeOrg();
+  const url = `/projects/${organization.slug}/${project.slug}/`;
+
+  beforeEach(function () {
+    MockApiClient.clearMockResponses();
+    MockApiClient.addMockResponse({
+      url,
+      method: 'GET',
+      body: ProjectFixture(),
+    });
+    MockApiClient.addMockResponse({
+      url: `${url}keys/`,
+      method: 'GET',
+      body: [],
+    });
+  });
+
+  it('can toggle sentry branding option', async function () {
+    render(
+      <ProjectUserFeedback
+        {...routerProps}
+        organization={organization}
+        project={project}
+      />,
+      {
+        context: routerContext,
+      }
+    );
+
+    const mock = MockApiClient.addMockResponse({
+      url,
+      method: 'PUT',
+    });
+
+    await userEvent.click(
+      screen.getByRole('checkbox', {name: 'Show Sentry Branding in Crash Report Modal'})
+    );
+
+    expect(mock).toHaveBeenCalledWith(
+      url,
+      expect.objectContaining({
+        method: 'PUT',
+        data: {
+          options: {'feedback:branding': true},
+        },
+      })
+    );
+  });
+});
 
 describe('ProjectUserFeedbackProcessing', function () {
   const {routerProps, organization, project, routerContext} = initializeOrg();
@@ -25,7 +76,7 @@ describe('ProjectUserFeedbackProcessing', function () {
 
   it('can toggle spam detection', async function () {
     render(
-      <ProjectUserFeedbackProcessing
+      <ProjectUserFeedback
         {...routerProps}
         organization={organization}
         project={project}
