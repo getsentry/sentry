b6b4d614f3f17f178d036cd770fabe5745c83462
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-05-14 16:13:35 -0700

    feat(similarity-embedding): Add endpoint to call backfill script (#68751)
    Create endpoint to call the backfill script
    [here](https://github.com/getsentry/sentry/pull/68466)
    
    ---------
    
    Co-authored-by: Josh Ferge <josh.ferge@sentry.io>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>
    Co-authored-by: Katie Byers <katie.byers@sentry.io>

---
+++ src/sentry/api/endpoints/project_backfill_similar_issues_embeddings_records.py
@@ -0,0 +1,31 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from sentry import features
+from sentry.api.api_owners import ApiOwner
+from sentry.api.api_publish_status import ApiPublishStatus
+from sentry.api.base import region_silo_endpoint
+from sentry.api.bases.project import ProjectEndpoint
+from sentry.auth.superuser import is_active_superuser
+from sentry.tasks.backfill_seer_grouping_records import backfill_seer_grouping_records
+
+
+@region_silo_endpoint
+class ProjectBackfillSimilarIssuesEmbeddingsRecords(ProjectEndpoint):
+    owner = ApiOwner.ISSUES
+    publish_status = {
+        "POST": ApiPublishStatus.PRIVATE,
+    }
+
+    def post(self, request: Request, project) -> Response:
+        if not features.has(
+            "projects:similarity-embeddings-backfill", project
+        ) or not is_active_superuser(request):
+            return Response(status=404)
+
+        last_processed_id = None
+        if request.data.get("last_processed_id"):
+            last_processed_id = int(request.data["last_processed_id"])
+
+        backfill_seer_grouping_records.delay(project.id, last_processed_id)
+        return Response(status=204)
