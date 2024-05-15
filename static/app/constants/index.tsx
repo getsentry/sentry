0884d786f373065cdbe8eb2fde26a3a9c17004de
Scott Cooper <scttcper@gmail.com>
2024-05-14 17:14:32 -0700

    fix(billing): Fix plural of profileDuration
    Change constant, add comment explaining when plural vs DataCategory would be used

--- static/app/constants/index.tsx
+++ static/app/constants/index.tsx
@@ -329,7 +329,7 @@ export const DATA_CATEGORY_INFO = {
   [DataCategoryExact.PROFILE_DURATION]: {
     name: DataCategoryExact.PROFILE_DURATION,
     apiName: 'profileSeconds',
-    plural: 'profileSeconds',
+    plural: 'profileDuration',
     displayName: 'profile seconds',
     titleName: t('Profile Seconds'),
     uid: 17,
