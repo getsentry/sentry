2b2a5ee74e50ccf1bb373ced1b1f9dd9eed6572f
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-05-15 13:40:25 -0700

    feat(flagpole): Merges conditions and operators

--- src/flagpole/conditions.py
+++ src/flagpole/conditions.py
@@ -1,25 +1,205 @@
-from pydantic import BaseModel, constr
+from abc import abstractmethod
+from enum import Enum
+from typing import Annotated, Any, Literal, TypeVar
+
+from pydantic import BaseModel, Field, StrictBool, StrictFloat, StrictInt, StrictStr, constr
 
 from flagpole.evaluation_context import EvaluationContext
-from flagpole.operators import AvailableOperators
 
 
-class Condition(BaseModel):
-    property: str | None
-    operator: AvailableOperators
+class ConditionOperatorKind(str, Enum):
+    IN = "in"
+    """Provided a list of values, check if the property value is in the list ov values"""
+
+    NOT_IN = "not_in"
+
+    CONTAINS = "contains"
+    """Provided a single value, check if the property (a list) is included"""
+
+    NOT_CONTAINS = "not_contains"
+    """Provided a single value, check if the property (a list) is not included"""
+
+    EQUALS = "equals"
+    """Comprare a value to another. Values are compared with types"""
+
+    NOT_EQUALS = "not_equals"
+    """Comprare a value to not be equal to another. Values are compared with types"""
+
+
+class ConditionTypeMismatchException(Exception):
+    pass
+
+
+def get_type_name(value: Any):
+    return type(value).__name__
+
+
+T = TypeVar("T", str, int, float)
+
+
+def create_case_insensitive_set_from_list(values: list[T]) -> set[T]:
+    case_insensitive_set = set()
+    for value in values:
+        if isinstance(value, str):
+            case_insensitive_set.add(value.lower())
+        else:
+            case_insensitive_set.add(value)
+
+    return case_insensitive_set
+
+
+class ConditionBase(BaseModel):
+    property: str
+    operator: ConditionOperatorKind
+    value: Any
 
     def match(self, context: EvaluationContext, segment_name: str) -> bool:
         if self.property is None:
             return False
 
-        return self.operator.match(
+        return self._operator_match(
             condition_property=context.get(self.property), segment_name=segment_name
         )
+
+    @abstractmethod
+    def _operator_match(self, condition_property: Any, segment_name: str) -> bool:
+        raise NotImplementedError("Each Condition needs to implement this method")
+
+    def _evaluate_in(self, condition_property: any, segment_name: str) -> bool:
+        if not isinstance(self.value, list):
+            raise ConditionTypeMismatchException(
+                f"'In' condition value must be a list, but was provided a '{get_type_name(self.value)}'"
+                + f" of segment {segment_name}"
+            )
+        if isinstance(condition_property, (list, dict)):
+            raise ConditionTypeMismatchException(
+                "'In' condition property value must be str | int | float | bool | None, but was provided a"
+                + f"'{get_type_name(self.value)}' of segment {segment_name}"
+            )
+        if isinstance(condition_property, str):
+            condition_property = condition_property.lower()
+
+        return condition_property in create_case_insensitive_set_from_list(self.value)
+
+    def _evaluate_contains(self, condition_property: Any, segment_name: str) -> bool:
+        if not isinstance(condition_property, list):
+            raise ConditionTypeMismatchException(
+                f"'Contains' can only be checked against a list, but was given a {get_type_name(condition_property)}"
+                + f" context property '{condition_property}' of segment '{segment_name}'"
+            )
+        value = self.value
+        if isinstance(value, str):
+            value = value.lower()
+
+        return value in create_case_insensitive_set_from_list(condition_property)
+
+    def _evaluate_equals(self, condition_property: Any, segment_name: str) -> bool:
+        if not isinstance(condition_property, type(self.value)):
+            value_type = get_type_name(self.value)
+            property_value = get_type_name(condition_property)
+            raise ConditionTypeMismatchException(
+                "'Equals' operator cannot be applied to values of mismatching types"
+                + f"({value_type} and {property_value}) for segment {segment_name}"
+            )
+
+        if isinstance(condition_property, str):
+            return condition_property.lower() == self.value.lower()
+
+        return condition_property == self.value
+
+
+InOperatorValueTypes = list[StrictInt] | list[StrictFloat] | list[StrictStr]
+
+
+class InCondition(ConditionBase):
+    operator: Literal[ConditionOperatorKind.IN] = ConditionOperatorKind.IN
+    value: InOperatorValueTypes
+
+    def _operator_match(self, condition_property: Any, segment_name: str):
+        return self._evaluate_in(condition_property=condition_property, segment_name=segment_name)
+
+
+class NotInCondition(ConditionBase):
+    operator: Literal[ConditionOperatorKind.NOT_IN] = ConditionOperatorKind.NOT_IN
+    value: InOperatorValueTypes
+
+    def _operator_match(self, condition_property: Any, segment_name: str):
+        return not self._evaluate_in(
+            condition_property=condition_property, segment_name=segment_name
+        )
+
+
+ContainsOperatorValueTypes = StrictInt | StrictStr | StrictFloat
+
+
+class ContainsCondition(ConditionBase):
+    operator: Literal[ConditionOperatorKind.CONTAINS] = ConditionOperatorKind.CONTAINS
+    value: StrictInt | StrictStr | StrictFloat
+
+    def _operator_match(self, condition_property: Any, segment_name: str):
+        return self._evaluate_contains(
+            condition_property=condition_property, segment_name=segment_name
+        )
+
+
+class NotContainsCondition(ConditionBase):
+    operator: Literal[ConditionOperatorKind.NOT_CONTAINS] = ConditionOperatorKind.NOT_CONTAINS
+    value: ContainsOperatorValueTypes
+
+    def _operator_match(self, condition_property: Any, segment_name: str):
+        return not self._evaluate_contains(
+            condition_property=condition_property, segment_name=segment_name
+        )
+
+
+EqualsOperatorValueTypes = (
+    StrictInt
+    | StrictFloat
+    | StrictStr
+    | StrictBool
+    | list[StrictInt]
+    | list[StrictFloat]
+    | list[StrictStr]
+)
+
+
+class EqualsCondition(ConditionBase):
+    operator: Literal[ConditionOperatorKind.EQUALS] = ConditionOperatorKind.EQUALS
+    value: EqualsOperatorValueTypes
+
+    def _operator_match(self, condition_property: Any, segment_name: str):
+        return self._evaluate_equals(
+            condition_property=condition_property, segment_name=segment_name
+        )
+
+
+class NotEqualsCondition(ConditionBase):
+    operator: Literal[ConditionOperatorKind.NOT_EQUALS] = ConditionOperatorKind.NOT_EQUALS
+    value: EqualsOperatorValueTypes
+
+    def _operator_match(self, condition_property: Any, segment_name: str):
+        return not self._evaluate_equals(
+            condition_property=condition_property, segment_name=segment_name
+        )
+
+
+# We have to group and annotate all the different subclasses of Operator
+# in order for Pydantic to be able to discern between the different types
+# when parsing a dict or JSON.
+AvailableConditions = Annotated[
+    InCondition
+    | NotInCondition
+    | ContainsCondition
+    | NotContainsCondition
+    | EqualsCondition
+    | NotEqualsCondition,
+    Field(discriminator="operator"),
+]
 
 
 class Segment(BaseModel):
     name: constr(min_length=1)  # type:ignore[valid-type]
-    conditions: list[Condition]
+    conditions: list[AvailableConditions]
     rollout: int | None = 0
 
     def match(self, context: EvaluationContext) -> bool:
--- tests/flagpole/test_operators.py
+++ tests/flagpole/test_operators.py
@@ -3,16 +3,17 @@ from typing import Any
 import pytest
 from pydantic import ValidationError
 
-from flagpole.operators import (
+from flagpole import EvaluationContext
+from flagpole.conditions import (
+    ConditionBase,
+    ConditionOperatorKind,
     ConditionTypeMismatchException,
-    ContainsOperator,
-    EqualsOperator,
-    InOperator,
-    NotContainsOperator,
-    NotEqualsOperator,
-    NotInOperator,
-    Operator,
-    OperatorKind,
+    ContainsCondition,
+    EqualsCondition,
+    InCondition,
+    NotContainsCondition,
+    NotEqualsCondition,
+    NotInCondition,
     create_case_insensitive_set_from_list,
 )
 from sentry.testutils.cases import TestCase
@@ -33,136 +34,172 @@ class TestCreateCaseInsensitiveSetFromList(TestCase):
         assert create_case_insensitive_set_from_list(["AbC", "DEF"]) == {"abc", "def"}
 
 
-def assert_valid_types(operator: type[Operator], expected_types: list[Any]):
+def assert_valid_types(condition: type[ConditionBase], expected_types: list[Any]):
     for value in expected_types:
-        operator_dict = dict(value=value)
+        operator_dict = dict(property="test", value=value)
         json_operator = json.dumps(operator_dict)
         try:
-            parsed_operator = operator.parse_raw(json_operator)
+            parsed_operator = condition.parse_raw(json_operator)
         except ValidationError as exc:
             raise AssertionError(
-                f"Expected value `{value}` to be a valid value for operator '{operator}'"
+                f"Expected value `{value}` to be a valid value for operator '{condition}'"
             ) from exc
         assert parsed_operator.value == value
 
 
-def assert_invalid_types(operator: type[Operator], invalid_types: list[Any]):
+def assert_invalid_types(condition: type[ConditionBase], invalid_types: list[Any]):
     for value in invalid_types:
         json_dict = dict(value=value)
         operator_json = json.dumps(json_dict)
         try:
-            operator.parse_raw(operator_json)
+            condition.parse_raw(operator_json)
         except ValidationError:
             continue
 
         raise AssertionError(
-            f"Expected validation error for value: `{value}` for operator `{operator}`"
+            f"Expected validation error for value: `{value}` for operator `{condition}`"
         )
 
 
-class TestInOperators(TestCase):
+class TestInConditions(TestCase):
     def test_invalid_values(self):
         with pytest.raises(ValidationError):
-            InOperator(value="bar")
+            InCondition(value="bar")
 
         with pytest.raises(ValidationError):
-            InOperator(value=1234)
+            InCondition(value=1234)
 
     def test_is_in(self):
         values = ["bar", "baz"]
-        operator = InOperator(kind=OperatorKind.IN, value=values)
-        assert operator.match(condition_property="bar", segment_name="test")
+        condition = InCondition(property="foo", kind=ConditionOperatorKind.IN, value=values)
+        assert condition.match(context=EvaluationContext({"foo": "bar"}), segment_name="test")
 
-        not_operator = NotInOperator(kind=OperatorKind.NOT_IN, value=values)
-        assert not not_operator.match(condition_property="bar", segment_name="test")
+        not_condition = NotInCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_IN, value=values
+        )
+        assert not not_condition.match(
+            context=EvaluationContext({"foo": "bar"}), segment_name="test"
+        )
 
         int_values = [1, 2]
-        operator = InOperator(kind=OperatorKind.IN, value=int_values)
+        condition = InCondition(property="foo", kind=ConditionOperatorKind.IN, value=int_values)
         # Validation check to ensure no type coercion occurs
-        assert operator.value == int_values
-        assert operator.match(condition_property=2, segment_name="test")
-        assert not operator.match(condition_property=3, segment_name="test")
+        assert condition.value == int_values
+        assert condition.match(context=EvaluationContext({"foo": 2}), segment_name="test")
+        assert not condition.match(context=EvaluationContext({"foo": 3}), segment_name="test")
 
     def test_is_in_numeric_string(self):
         values = ["123", "456"]
-        operator = InOperator(kind=OperatorKind.IN, value=values)
-        assert operator.value == values
-        assert not operator.match(condition_property=123, segment_name="test")
-        assert operator.match(condition_property="123", segment_name="test")
+        condition = InCondition(property="foo", kind=ConditionOperatorKind.IN, value=values)
+        assert condition.value == values
+        assert not condition.match(context=EvaluationContext({"foo": 123}), segment_name="test")
+        assert condition.match(context=EvaluationContext({"foo": "123"}), segment_name="test")
 
     def test_is_not_in(self):
         values = ["bar", "baz"]
-        operator = InOperator(kind=OperatorKind.IN, value=values)
-        assert not operator.match(condition_property="foo", segment_name="test")
+        condition = InCondition(property="foo", kind=ConditionOperatorKind.IN, value=values)
+        assert not condition.match(context=EvaluationContext({"foo": "foo"}), segment_name="test")
 
-        not_operator = NotInOperator(kind=OperatorKind.NOT_IN, value=values)
-        assert not_operator.match(condition_property="foo", segment_name="test")
+        not_condition = NotInCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_IN, value=values
+        )
+        assert not_condition.match(context=EvaluationContext({"foo": "foo"}), segment_name="test")
 
     def test_is_in_case_insensitivity(self):
         values = ["bAr", "baz"]
-        operator = InOperator(kind=OperatorKind.IN, value=values)
-        assert operator.match(condition_property="BaR", segment_name="test")
+        condition = InCondition(property="foo", kind=ConditionOperatorKind.IN, value=values)
+        assert condition.match(context=EvaluationContext({"foo": "BaR"}), segment_name="test")
 
-        not_operator = NotInOperator(kind=OperatorKind.NOT_IN, value=values)
-        assert not not_operator.match(condition_property="BaR", segment_name="test")
+        not_condition = NotInCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_IN, value=values
+        )
+        assert not not_condition.match(
+            context=EvaluationContext({"foo": "BaR"}), segment_name="test"
+        )
 
     def test_invalid_property_value(self):
         values = ["bar", "baz"]
-        operator = InOperator(kind=OperatorKind.IN, value=values)
+        condition = InCondition(property="foo", kind=ConditionOperatorKind.IN, value=values)
 
         with pytest.raises(ConditionTypeMismatchException):
-            operator.match(condition_property=[], segment_name="test")
+            condition.match(context=EvaluationContext({"foo": []}), segment_name="test")
 
-        not_operator = NotInOperator(kind=OperatorKind.NOT_IN, value=values)
+        not_condition = NotInCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_IN, value=values
+        )
         with pytest.raises(ConditionTypeMismatchException):
-            not_operator.match(condition_property=[], segment_name="test")
+            not_condition.match(context=EvaluationContext({"foo": []}), segment_name="test")
 
     def test_valid_json_and_reparse(self):
         values = [["foo", "bar"], [1, 2], [1.1, 2.2], []]
-        assert_valid_types(operator=InOperator, expected_types=values)
-        assert_valid_types(operator=NotInOperator, expected_types=values)
+        assert_valid_types(condition=InCondition, expected_types=values)
+        assert_valid_types(condition=NotInCondition, expected_types=values)
 
     def test_invalid_value_type_parsing(self):
         values = ["abc", 1, 2.2, True, None, ["a", 1], [True], [[]], [1, 2.2], [1.1, "2.2"]]
-        assert_invalid_types(operator=InOperator, invalid_types=values)
-        assert_invalid_types(operator=NotInOperator, invalid_types=values)
+        assert_invalid_types(condition=InCondition, invalid_types=values)
+        assert_invalid_types(condition=NotInCondition, invalid_types=values)
 
 
-class TestContainsOperators(TestCase):
+class TestContainsConditions(TestCase):
     def test_does_contain(self):
-        operator = ContainsOperator(kind=OperatorKind.CONTAINS, value="bar")
-        assert operator.match(condition_property=["foo", "bar"], segment_name="test")
+        operator = ContainsCondition(
+            property="foo", kind=ConditionOperatorKind.CONTAINS, value="bar"
+        )
+        assert operator.match(
+            context=EvaluationContext({"foo": ["foo", "bar"]}), segment_name="test"
+        )
 
-        not_operator = NotContainsOperator(kind=OperatorKind.NOT_CONTAINS, value="bar")
-        assert not not_operator.match(condition_property=["foo", "bar"], segment_name="test")
+        not_operator = NotContainsCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_CONTAINS, value="bar"
+        )
+        assert not not_operator.match(
+            context=EvaluationContext({"foo": ["foo", "bar"]}), segment_name="test"
+        )
 
-        operator = ContainsOperator(kind=OperatorKind.CONTAINS, value=1)
-        assert operator.match(condition_property=[1, 2], segment_name="test")
-        assert not operator.match(condition_property=[3, 4], segment_name="test")
+        operator = ContainsCondition(property="foo", kind=ConditionOperatorKind.CONTAINS, value=1)
+        assert operator.match(context=EvaluationContext({"foo": [1, 2]}), segment_name="test")
+        assert not operator.match(context=EvaluationContext({"foo": [3, 4]}), segment_name="test")
 
     def test_does_not_contain(self):
         values = "baz"
-        operator = ContainsOperator(kind=OperatorKind.CONTAINS, value=values)
-        assert not operator.match(condition_property=["foo", "bar"], segment_name="test")
+        operator = ContainsCondition(
+            property="foo", kind=ConditionOperatorKind.CONTAINS, value=values
+        )
+        assert not operator.match(
+            context=EvaluationContext({"foo": ["foo", "bar"]}), segment_name="test"
+        )
 
-        not_operator = NotContainsOperator(kind=OperatorKind.NOT_CONTAINS, value=values)
-        assert not_operator.match(condition_property=["foo", "bar"], segment_name="test")
+        not_operator = NotContainsCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_CONTAINS, value=values
+        )
+        assert not_operator.match(
+            context=EvaluationContext({"foo": ["foo", "bar"]}), segment_name="test"
+        )
 
     def test_invalid_property_provided(self):
         values = "baz"
 
         with pytest.raises(ConditionTypeMismatchException):
-            operator = ContainsOperator(kind=OperatorKind.CONTAINS, value=values)
-            assert not operator.match(condition_property="oops", segment_name="test")
+            operator = ContainsCondition(
+                property="foo", kind=ConditionOperatorKind.CONTAINS, value=values
+            )
+            assert not operator.match(
+                context=EvaluationContext({"foo": "oops"}), segment_name="test"
+            )
 
         with pytest.raises(ConditionTypeMismatchException):
-            not_operator = NotContainsOperator(kind=OperatorKind.NOT_CONTAINS, value=values)
-            assert not_operator.match(condition_property="oops", segment_name="test")
+            not_operator = NotContainsCondition(
+                property="foo", kind=ConditionOperatorKind.NOT_CONTAINS, value=values
+            )
+            assert not_operator.match(
+                context=EvaluationContext({"foo": "oops"}), segment_name="test"
+            )
 
     def test_valid_json_parsing_with_types(self):
         values = [1, 2.2, "abc"]
-        assert_valid_types(operator=ContainsOperator, expected_types=values)
-        assert_valid_types(operator=NotContainsOperator, expected_types=values)
+        assert_valid_types(condition=ContainsCondition, expected_types=values)
+        assert_valid_types(condition=NotContainsCondition, expected_types=values)
 
     def test_invalid_value_type_parsing(self):
         values: list[Any] = [
@@ -171,52 +208,64 @@ class TestContainsOperators(TestCase):
             dict(foo="bar"),
             [[]],
         ]
-        assert_invalid_types(operator=ContainsOperator, invalid_types=values)
-        assert_invalid_types(operator=NotContainsOperator, invalid_types=values)
+        assert_invalid_types(condition=ContainsCondition, invalid_types=values)
+        assert_invalid_types(condition=NotContainsCondition, invalid_types=values)
 
 
-class TestEqualsOperators(TestCase):
+class TestEqualsConditions(TestCase):
     def test_is_equal_string(self):
         value = "foo"
-        operator = EqualsOperator(kind=OperatorKind.EQUALS, value=value)
-        assert operator.match(condition_property="foo", segment_name="test")
+        operator = EqualsCondition(property="foo", kind=ConditionOperatorKind.EQUALS, value=value)
+        assert operator.match(context=EvaluationContext({"foo": "foo"}), segment_name="test")
 
-        not_operator = NotEqualsOperator(kind=OperatorKind.NOT_EQUALS, value=value)
-        assert not not_operator.match(condition_property="foo", segment_name="test")
+        not_operator = NotEqualsCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_EQUALS, value=value
+        )
+        assert not not_operator.match(
+            context=EvaluationContext({"foo": "foo"}), segment_name="test"
+        )
 
     def test_is_not_equals(self):
         values = "bar"
-        operator = EqualsOperator(kind=OperatorKind.EQUALS, value=values)
-        assert not operator.match(condition_property="foo", segment_name="test")
+        operator = EqualsCondition(property="foo", kind=ConditionOperatorKind.EQUALS, value=values)
+        assert not operator.match(context=EvaluationContext({"foo": "foo"}), segment_name="test")
 
-        not_operator = NotEqualsOperator(kind=OperatorKind.NOT_EQUALS, value=values)
-        assert not_operator.match(condition_property="foo", segment_name="test")
+        not_operator = NotEqualsCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_EQUALS, value=values
+        )
+        assert not_operator.match(context=EvaluationContext({"foo": "foo"}), segment_name="test")
 
     def test_is_equal_case_insensitive(self):
         values = "bAr"
-        operator = EqualsOperator(kind=OperatorKind.EQUALS, value=values)
-        assert operator.match(condition_property="BaR", segment_name="test")
+        operator = EqualsCondition(property="foo", kind=ConditionOperatorKind.EQUALS, value=values)
+        assert operator.match(context=EvaluationContext({"foo": "BaR"}), segment_name="test")
 
-        not_operator = NotEqualsOperator(kind=OperatorKind.NOT_EQUALS, value=values)
-        assert not not_operator.match(condition_property="BaR", segment_name="test")
+        not_operator = NotEqualsCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_EQUALS, value=values
+        )
+        assert not not_operator.match(
+            context=EvaluationContext({"foo": "BaR"}), segment_name="test"
+        )
 
     def test_equality_type_mismatch_strings(self):
         values = ["foo", "bar"]
-        operator = EqualsOperator(kind=OperatorKind.EQUALS, value=values)
+        operator = EqualsCondition(property="foo", kind=ConditionOperatorKind.EQUALS, value=values)
 
         with pytest.raises(ConditionTypeMismatchException):
-            operator.match(condition_property="foo", segment_name="test")
+            operator.match(context=EvaluationContext({"foo": "foo"}), segment_name="test")
 
-        not_operator = NotEqualsOperator(kind=OperatorKind.NOT_EQUALS, value=values)
+        not_operator = NotEqualsCondition(
+            property="foo", kind=ConditionOperatorKind.NOT_EQUALS, value=values
+        )
         with pytest.raises(ConditionTypeMismatchException):
-            not_operator.match(condition_property="foo", segment_name="test")
+            not_operator.match(context=EvaluationContext({"foo": "foo"}), segment_name="test")
 
     def test_valid_json_parsing_with_types(self):
         values = [1, 2.2, "abc", True, False, [], ["foo"], [1], [1.1]]
-        assert_valid_types(operator=EqualsOperator, expected_types=values)
-        assert_valid_types(operator=NotEqualsOperator, expected_types=values)
+        assert_valid_types(condition=EqualsCondition, expected_types=values)
+        assert_valid_types(condition=NotEqualsCondition, expected_types=values)
 
     def test_invalid_value_type_parsing(self):
         values = [None, dict(foo="bar")]
-        assert_invalid_types(operator=EqualsOperator, invalid_types=values)
-        assert_invalid_types(operator=NotEqualsOperator, invalid_types=values)
+        assert_invalid_types(condition=EqualsCondition, invalid_types=values)
+        assert_invalid_types(condition=NotEqualsCondition, invalid_types=values)
