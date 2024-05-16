fd381cd541e6fa0a5103b0ca92b9ffbb3cd617be
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-05-16 10:22:11 -0400

    Register feature flag

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -1893,6 +1893,8 @@ SENTRY_FEATURES: dict[str, bool | None] = {
     "organizations:starfish-browser-webvitals-replace-fid-with-inp": False,
     # Uses a computed total count to calculate the score in the browser starfish webvitals module, instead of measurements.score.total
     "organizations:starfish-browser-webvitals-score-computed-total": False,
+    # Rename current Performance modules to "Insights"
+    "organizations:performance-insights": False,
     # Enable queues module ui
     "organizations:performance-queues-view": False,
     # Enable browser starfish cache module ui
