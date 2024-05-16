834274b96d4aba35512052743fe4f766da988548
Joris Bayer <joris.bayer@sentry.io>
2024-05-16 13:56:45 +0200

    test: add snapshot

--- tests/sentry/api/endpoints/test_relay_globalconfig_v3.py
+++ tests/sentry/api/endpoints/test_relay_globalconfig_v3.py
@@ -122,3 +122,9 @@ def test_return_global_config_on_right_version(
 def test_global_config_valid_with_generic_filters():
     config = get_global_config()
     assert config == normalize_global_config(config)
+
+
+@django_db_all
+def test_global_config_histogram_outliers(insta_snapshot):
+    config = get_global_config()
+    insta_snapshot(config["metricExtraction"])
