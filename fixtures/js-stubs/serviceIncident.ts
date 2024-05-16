dc392a31d843d384f02bca0ea02da13bebaa51bd
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-05-16 11:42:30 -0400

    ref(ui): Add useServiceIncidents (#70993)
    I'll be using this on the crons timeline to show incidents affecting the
    crons status component as an overlay on the timeline.
    
    This is a quick refactor to use useQuery and as a hook

--- fixtures/js-stubs/serviceIncident.ts
+++ fixtures/js-stubs/serviceIncident.ts
@@ -1,38 +1,59 @@
-import type {SentryServiceIncident} from 'sentry/types/system';
+import {StatusPageComponent, type StatuspageIncident} from 'sentry/types/system';
 
 export function ServiceIncidentFixture(
-  params: Partial<SentryServiceIncident> = {}
-): SentryServiceIncident {
+  params: Partial<StatuspageIncident> = {}
+): StatuspageIncident {
   return {
     id: '1',
-    status: '',
+    page_id: '',
+    status: 'unresolved',
     name: 'Test Incident',
-    createdAt: '2022-05-23T13:33:38.737-07:00',
-    updates: [
+    impact: 'major',
+    monitoring_at: undefined,
+    created_at: '2022-05-23T13:33:38.737-07:00',
+    started_at: undefined,
+    resolved_at: undefined,
+    updated_at: undefined,
+    incident_updates: [
       {
-        status: '',
-        updatedAt: '2022-05-23T13:33:38.737-07:00',
+        id: '1',
+        incident_id: '1',
+        status: 'unresolved',
         body: 'Things look bad',
+        affected_components: [],
+        created_at: '2022-05-23T13:33:38.737-07:00',
+        updated_at: '2022-05-23T13:33:38.737-07:00',
+        display_at: '2022-05-23T13:33:38.737-07:00',
       },
       {
-        status: '',
-        updatedAt: '2022-05-23T13:45:38.737-07:00',
+        id: '2',
+        incident_id: '1',
+        status: 'unresolved',
         body: 'Investigating',
+        affected_components: [],
+        updated_at: '2022-05-23T13:45:38.737-07:00',
+        created_at: '2022-05-23T13:45:38.737-07:00',
+        display_at: '2022-05-23T13:45:38.737-07:00',
       },
     ],
-    affectedComponents: [
+    components: [
       {
+        id: StatusPageComponent.US_ERRORS,
+        page_id: '',
+        position: 1,
+        showcase: false,
+        description: 'Errors ingestion',
+        group: false,
+        group_id: '1',
+        only_show_if_degraded: false,
         name: '',
         status: 'major_outage',
-        updatedAt: '2022-05-23T13:33:38.737-07:00',
-      },
-      {
-        name: 'Second Update',
-        status: 'operational',
-        updatedAt: '2022-05-23T13:45:38.737-07:00',
+        start_date: '2022-05-23',
+        updated_at: '2022-05-23T13:33:38.737-07:00',
+        created_at: '2022-05-23T13:33:38.737-07:00',
       },
     ],
-    url: 'https://status.sentry.io',
+    shortlink: 'https://status.sentry.io',
     ...params,
   };
 }
