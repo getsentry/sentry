getsentry/sentry

commit 202883969b2d7b773374dfb94c2fd89e6f07265a

ref(ui): Extract ServiceIncidentDetails component (#71060)
We will be using this in the crons timeline to display incidents

<img alt="clipboard.png" width="665"
src="https://i.imgur.com/X5sxxi7.png" />

static/app/components/sidebar/serviceIncidents.tsx
@@ -1,28 +1,10 @@
 import {Fragment} from 'react';
-import styled from '@emotion/styled';
-import color from 'color';
-import sortBy from 'lodash/sortBy';
-import startCase from 'lodash/startCase';
 
-import {LinkButton} from 'sentry/components/button';
-import List from 'sentry/components/list';
-import ListItem from 'sentry/components/list/listItem';
-import Text from 'sentry/components/text';
-import TimeSince from 'sentry/components/timeSince';
-import {Tooltip} from 'sentry/components/tooltip';
-import {
-  IconCheckmark,
-  IconFatal,
-  IconFire,
-  IconInfo,
-  IconOpen,
-  IconWarning,
-} from 'sentry/icons';
-import {t, tct} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
-import type {StatusPageServiceStatus} from 'sentry/types';
-import marked from 'sentry/utils/marked';
+import {IconWarning} from 'sentry/icons';
+import {t} from 'sentry/locale';
 import {useServiceIncidents} from 'sentry/utils/useServiceIncidents';
+
+import {ServiceIncidentDetails} from '../serviceIncidentDetails';
 
 import SidebarItem from './sidebarItem';
 import SidebarPanel from './sidebarPanel';
@@ -33,13 +15,6 @@ import {SidebarPanelKey} from './types';
 
 type Props = CommonSidebarProps;
 
-const COMPONENT_STATUS_SORT: StatusPageServiceStatus[] = [
-  'operational',
-  'degraded_performance',
-  'partial_outage',
-  'major_outage',
-];
-
 function ServiceIncidents({
   currentPanel,
   onShowPanel,
@@ -47,7 +22,7 @@ function ServiceIncidents({
   collapsed,
   orientation,
 }: Props) {
-  const {data: incidents} = useServiceIncidents({statusFilter: 'unresolved'});
+  const {data: incidents} = useServiceIncidents({statusFilter: 'resolved'});
 
   if (!incidents) {
     return null;
@@ -82,55 +57,8 @@ function ServiceIncidents({
             <SidebarPanelEmpty>{t('There are no incidents to report')}</SidebarPanelEmpty>
           )}
           {incidents.map(incident => (
-            <SidebarPanelItem title={incident.name} key={incident.id}>
-              <LinkButton
-                size="xs"
-                icon={<IconOpen />}
-                priority="link"
-                href={incident.shortlink}
-                external
-              >
-                {t('Full Incident Details')}
-              </LinkButton>
-              <AffectedServices>
-                {tct(
-                  "This incident started [timeAgo]. We're experiencing the following problems with our services",
-                  {
-                    timeAgo: (
-                      <strong>
-                        <TimeSince date={incident.created_at} />
-                      </strong>
-                    ),
-                  }
-                )}
-                <ComponentList>
-                  {sortBy(incident.components, i =>
-                    COMPONENT_STATUS_SORT.indexOf(i.status)
-                  ).map(({name, status}, key) => (
-                    <ComponentStatus
-                      key={key}
-                      padding="24px"
-                      symbol={getStatusSymbol(status)}
-                    >
-                      {name}
-                    </ComponentStatus>
-                  ))}
-                </ComponentList>
-              </AffectedServices>
-
-              <UpdatesList>
-                {incident.incident_updates.map(({status, body, updated_at}, key) => (
-                  <ListItem key={key}>
-                    <UpdateHeading>
-                      <StatusTitle>{startCase(status)}</StatusTitle>
-                      <StatusDate>
-                        {tct('([time])', {time: <TimeSince date={updated_at} />})}
-                      </StatusDate>
-                    </UpdateHeading>
-                    <Text dangerouslySetInnerHTML={{__html: marked(body)}} />
-                  </ListItem>
-                ))}
-              </UpdatesList>
+            <SidebarPanelItem key={incident.id}>
+              <ServiceIncidentDetails incident={incident} />
             </SidebarPanelItem>
           ))}
         </SidebarPanel>
@@ -138,98 +66,5 @@ function ServiceIncidents({
     </Fragment>
   );
 }
-
-function getStatusSymbol(status: StatusPageServiceStatus) {
-  return (
-    <Tooltip skipWrapper title={startCase(status)}>
-      {status === 'operational' ? (
-        <IconCheckmark size="sm" isCircled color="successText" />
-      ) : status === 'major_outage' ? (
-        <IconFatal size="sm" color="errorText" />
-      ) : status === 'degraded_performance' ? (
-        <IconWarning size="sm" color="warningText" />
-      ) : status === 'partial_outage' ? (
-        <IconFire size="sm" color="warningText" />
-      ) : (
-        <IconInfo size="sm" color="subText" />
-      )}
-    </Tooltip>
-  );
-}
-
-const AffectedServices = styled('div')`
-  margin: ${space(2)} 0;
-`;
-
-const UpdatesList = styled(List)`
-  gap: ${space(3)};
-  margin-left: ${space(1.5)};
-  position: relative;
-
-  &::before {
-    content: '';
-    display: block;
-    position: absolute;
-    height: 100%;
-    width: 2px;
-    margin: ${space(1)} 0 ${space(1)} -${space(1.5)};
-    background: ${p => p.theme.gray100};
-  }
-
-  &::after {
-    content: '';
-    display: block;
-    position: absolute;
-    bottom: -${space(1)};
-    margin-left: -${space(1.5)};
-    height: 30px;
-    width: 2px;
-    background: linear-gradient(
-      0deg,
-      ${p => p.theme.background},
-      ${p => color(p.theme.background).alpha(0).string()}
-    );
-  }
-`;
-
-const UpdateHeading = styled('div')`
-  margin-bottom: ${space(0.5)};
-  display: flex;
-  align-items: center;
-  gap: ${space(1)};
-  position: relative;
-
-  &::before {
-    content: '';
-    display: block;
-    position: absolute;
-    height: 8px;
-    width: 8px;
-    margin-left: -15px;
-    border-radius: 50%;
-    background: ${p => p.theme.purple300};
-  }
-`;
-
-const StatusTitle = styled('div')`
-  color: ${p => p.theme.headingColor};
-  font-weight: bold;
-`;
-
-const StatusDate = styled('div')`
-  color: ${p => p.theme.subText};
-  font-size: ${p => p.theme.fontSizeRelativeSmall};
-`;
-
-const ComponentList = styled(List)`
-  margin-top: ${space(1)};
-  display: block;
-  column-count: 2;
-`;
-
-const ComponentStatus = styled(ListItem)`
-  font-size: ${p => p.theme.fontSizeSmall};
-  line-height: 2;
-`;
 
 export default ServiceIncidents;

static/app/components/serviceIncidentDetails.tsx
@@ -1,0 +1,185 @@
+import {Fragment} from 'react';
+import styled from '@emotion/styled';
+import color from 'color';
+import sortBy from 'lodash/sortBy';
+import startCase from 'lodash/startCase';
+
+import {LinkButton} from 'sentry/components/button';
+import List from 'sentry/components/list';
+import ListItem from 'sentry/components/list/listItem';
+import Text from 'sentry/components/text';
+import TimeSince from 'sentry/components/timeSince';
+import {Tooltip} from 'sentry/components/tooltip';
+import {
+  IconCheckmark,
+  IconFatal,
+  IconFire,
+  IconInfo,
+  IconOpen,
+  IconWarning,
+} from 'sentry/icons';
+import {t, tct} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import type {StatuspageIncident, StatusPageServiceStatus} from 'sentry/types';
+import marked from 'sentry/utils/marked';
+
+interface Props {
+  incident: StatuspageIncident;
+}
+
+const COMPONENT_STATUS_SORT: StatusPageServiceStatus[] = [
+  'operational',
+  'degraded_performance',
+  'partial_outage',
+  'major_outage',
+];
+
+export function ServiceIncidentDetails({incident}: Props) {
+  return (
+    <Fragment>
+      <Title>{incident.name}</Title>
+      <LinkButton
+        size="xs"
+        icon={<IconOpen />}
+        priority="link"
+        href={incident.shortlink}
+        external
+      >
+        {t('Full Incident Details')}
+      </LinkButton>
+      <AffectedServices>
+        {tct(
+          "This incident started [timeAgo]. We're experiencing the following problems with our services",
+          {
+            timeAgo: (
+              <strong>
+                <TimeSince date={incident.created_at} />
+              </strong>
+            ),
+          }
+        )}
+        <ComponentList>
+          {sortBy(incident.components, i => COMPONENT_STATUS_SORT.indexOf(i.status)).map(
+            ({name, status}, key) => (
+              <ComponentStatus key={key} padding="24px" symbol={getStatusSymbol(status)}>
+                {name}
+              </ComponentStatus>
+            )
+          )}
+        </ComponentList>
+      </AffectedServices>
+
+      <UpdatesList>
+        {incident.incident_updates.map(({status, body, updated_at}, key) => (
+          <ListItem key={key}>
+            <UpdateHeading>
+              <StatusTitle>{startCase(status)}</StatusTitle>
+              <StatusDate>
+                {tct('([time])', {time: <TimeSince date={updated_at} />})}
+              </StatusDate>
+            </UpdateHeading>
+            <Text dangerouslySetInnerHTML={{__html: marked(body)}} />
+          </ListItem>
+        ))}
+      </UpdatesList>
+    </Fragment>
+  );
+}
+
+function getStatusSymbol(status: StatusPageServiceStatus) {
+  return (
+    <Tooltip skipWrapper title={startCase(status)}>
+      {status === 'operational' ? (
+        <IconCheckmark size="sm" isCircled color="successText" />
+      ) : status === 'major_outage' ? (
+        <IconFatal size="sm" color="errorText" />
+      ) : status === 'degraded_performance' ? (
+        <IconWarning size="sm" color="warningText" />
+      ) : status === 'partial_outage' ? (
+        <IconFire size="sm" color="warningText" />
+      ) : (
+        <IconInfo size="sm" color="subText" />
+      )}
+    </Tooltip>
+  );
+}
+
+const Title = styled('h2')`
+  font-size: ${p => p.theme.fontSizeLarge};
+  margin-bottom: ${space(1)};
+`;
+
+const AffectedServices = styled('div')`
+  margin: ${space(2)} 0;
+`;
+
+const UpdatesList = styled(List)`
+  gap: ${space(3)};
+  margin-left: ${space(1.5)};
+  position: relative;
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    height: 100%;
+    width: 2px;
+    margin: ${space(1)} 0 ${space(1)} -${space(1.5)};
+    background: ${p => p.theme.gray100};
+  }
+
+  &::after {
+    content: '';
+    display: block;
+    position: absolute;
+    bottom: -${space(1)};
+    margin-left: -${space(1.5)};
+    height: 30px;
+    width: 2px;
+    background: linear-gradient(
+      0deg,
+      ${p => p.theme.background},
+      ${p => color(p.theme.background).alpha(0).string()}
+    );
+  }
+`;
+
+const UpdateHeading = styled('div')`
+  margin-bottom: ${space(0.5)};
+  display: flex;
+  align-items: center;
+  gap: ${space(1)};
+  position: relative;
+
+  &::before {
+    content: '';
+    display: block;
+    position: absolute;
+    height: 8px;
+    width: 8px;
+    margin-left: -15px;
+    border-radius: 50%;
+    background: ${p => p.theme.purple300};
+  }
+`;
+
+const StatusTitle = styled('div')`
+  color: ${p => p.theme.headingColor};
+  font-weight: bold;
+`;
+
+const StatusDate = styled('div')`
+  color: ${p => p.theme.subText};
+  font-size: ${p => p.theme.fontSizeRelativeSmall};
+`;
+
+const ComponentList = styled(List)`
+  margin-top: ${space(1)};
+  display: block;
+  column-count: 2;
+`;
+
+const ComponentStatus = styled(ListItem)`
+  font-size: ${p => p.theme.fontSizeSmall};
+  line-height: 2;
+`;

commit 1454584b86e0db15bad1669c0cb0b2d83062dc77

fix(ui): Only show unresolved service incidents in sidebar (#71070)
This regressed in https://github.com/getsentry/sentry/pull/71060

static/app/components/sidebar/serviceIncidents.tsx
@@ -22,7 +22,7 @@ function ServiceIncidents({
   collapsed,
   orientation,
 }: Props) {
-  const {data: incidents} = useServiceIncidents({statusFilter: 'resolved'});
+  const {data: incidents} = useServiceIncidents({statusFilter: 'unresolved'});
 
   if (!incidents) {
     return null;