fc44b6ff461f4731660d66a1d744503cb95e3aef
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-05-17 04:38:40 -0400

    ref: kill unused to_unicode and pprint template filter (#71062)
    leftovers from python 2
    
    <!-- Describe your PR here. -->

--- src/sentry/utils/strings.py
+++ src/sentry/utils/strings.py
@@ -7,9 +7,9 @@ import re
 import string
 import zlib
 from collections.abc import Callable
-from typing import Any, overload
+from typing import overload
 
-from django.utils.encoding import force_str, smart_str
+from django.utils.encoding import smart_str
 
 _sprintf_placeholder_re = re.compile(
     r"%(?:\d+\$)?[+-]?(?:[ 0]|\'.{1})?-?\d*(?:\.\d+)?[bcdeEufFgGosxX]"
@@ -114,19 +114,6 @@ def soft_break(value: str, length: int, process: Callable[[str], str] = lambda c
         return "".join(results).rstrip("\u200b")
 
     return re.sub(rf"\S{{{length},}}", soft_break_delimiter, value)
-
-
-def to_unicode(value: Any) -> str:
-    try:
-        value = str(force_str(value))
-    except (UnicodeEncodeError, UnicodeDecodeError):
-        value = "(Error decoding value)"
-    except Exception:  # in some cases we get a different exception
-        try:
-            value = str(repr(type(value)))
-        except Exception:
-            value = "(Error decoding value)"
-    return value
 
 
 valid_dot_atom_characters = frozenset(string.ascii_letters + string.digits + ".!#$%&'*+-/=?^_`{|}~")
