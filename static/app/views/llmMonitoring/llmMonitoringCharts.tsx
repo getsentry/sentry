430df9167137118d1438e751121d2e0966a2c0ce
Yagiz Nizipli <yagiz@nizipli.com>
2024-05-20 10:55:04 -0400

    perf: reduce barrel file usages (#71158)
    Reduces the barrel file usages across the repository. Instead of
    `sentry/types`, this PR replaces ~100 usages with `sentry/types/XXXX`
    
    Remaining usages:
    
    ```
    Checked 5505 files in 834ms. No fixes needed.
    Found 776 errors.
    ```

--- static/app/views/llmMonitoring/llmMonitoringCharts.tsx
+++ static/app/views/llmMonitoring/llmMonitoringCharts.tsx
@@ -2,7 +2,7 @@ import styled from '@emotion/styled';
 
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
-import type {MetricsQueryApiResponseLastMeta} from 'sentry/types';
+import type {MetricsQueryApiResponseLastMeta} from 'sentry/types/metrics';
 import {MetricDisplayType} from 'sentry/utils/metrics/types';
 import {useMetricsQuery} from 'sentry/utils/metrics/useMetricsQuery';
 import usePageFilters from 'sentry/utils/usePageFilters';
