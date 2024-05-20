cde3c1612e96d4ad792a90e5fbcf3b8775d3776c
David Wang <dwang@sentry.io>
2024-05-20 15:25:35 -0400

    Fill in placeholder descriptions from gaprl
    Co-authored-by: Gabriel Lopes <lopes@hey.com>

--- static/app/views/monitors/components/processingErrors/processingErrorTitle.tsx
+++ static/app/views/monitors/components/processingErrors/processingErrorTitle.tsx
@@ -4,38 +4,38 @@ import {ProcessingErrorType} from 'sentry/views/monitors/types';
 export function ProcessingErrorTitle({type}: {type: ProcessingErrorType}) {
   switch (type) {
     case ProcessingErrorType.CHECKIN_ENVIRONMENT_MISMATCH:
-      return t('<placeholder description>');
+      return t('Environment mismatch');
     case ProcessingErrorType.CHECKIN_FINISHED:
-      return t('Check-In has already succeeded or failed.');
+      return t('Check-in already completed');
     case ProcessingErrorType.CHECKIN_GUID_PROJECT_MISMATCH:
-      return t('<placeholder description>');
+      return t('DSN project mismatch');
     case ProcessingErrorType.CHECKIN_INVALID_DURATION:
-      return t('<placeholder description>');
+      return t('Invalid duration');
     case ProcessingErrorType.CHECKIN_INVALID_GUID:
-      return t('<placeholder description>');
+      return t('Invalid GUID');
     case ProcessingErrorType.CHECKIN_VALIDATION_FAILED:
-      return t('<placeholder description>');
+      return t('Invalid check-in payload');
     case ProcessingErrorType.MONITOR_DISABLED:
-      return t('<placeholder description>');
+      return t('Monitor disabled');
     case ProcessingErrorType.MONITOR_DISABLED_NO_QUOTA:
-      return t('<placeholder description>');
+      return t('Insufficient quota');
     case ProcessingErrorType.MONITOR_INVALID_CONFIG:
-      return t('<placeholder description>');
+      return t('Invalid monitor payload');
     case ProcessingErrorType.MONITOR_INVALID_ENVIRONMENT:
-      return t('<placeholder description>');
+      return t('Invalid environment');
     case ProcessingErrorType.MONITOR_LIMIT_EXCEEDED:
-      return t('<placeholder description>');
+      return t('Maximum monitor limit exceeded');
     case ProcessingErrorType.MONITOR_NOT_FOUND:
-      return t('<placeholder description>');
+      return t('Monitor not found');
     case ProcessingErrorType.MONITOR_OVER_QUOTA:
-      return t('<placeholder description>');
+      return t('Monitor not created due to insufficient quota');
     case ProcessingErrorType.MONITOR_ENVIRONMENT_LIMIT_EXCEEDED:
-      return t('<placeholder description>');
+      return t('Environment limit exceeded');
     case ProcessingErrorType.MONITOR_ENVIRONMENT_RATELIMITED:
-      return t('<placeholder description>');
+      return t('Rate limited');
     case ProcessingErrorType.ORGANIZATION_KILLSWITCH_ENABLED:
-      return t('<placeholder description>');
+      return t('Disabled check-in ingestion');
     default:
-      return t('Unknown error');
+      return t('Unknown processing error');
   }
 }
