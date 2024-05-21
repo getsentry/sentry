ab7552a590c219b6ca9d749c204cbe248aec01e0
Sebastian Zivota <loewenheim@mailbox.org>
2024-05-21 11:42:31 +0200

    Fix tests

--- tests/sentry/processing/realtime_metrics/test_pb.py
+++ tests/sentry/processing/realtime_metrics/test_pb.py
@@ -7,14 +7,14 @@ from sentry.processing.realtime_metrics.pb import PbRealtimeMetricsStore
 def test_invalid_target():
     # there is no grpc service at that addr
     store = PbRealtimeMetricsStore(target="http://localhost:12345")
-    store.record_project_duration(1, 123456789)
-    assert not store.is_lpq_project(1)
+    store.record_project_duration("native", 1, 123456789)
+    assert not store.is_lpq_project("native", 1)
 
 
 def test_pb_works():
     store = PbRealtimeMetricsStore(target="http://localhost:4433")
 
     project_id = floor(random() * (1 << 32))
-    assert not store.is_lpq_project(project_id)
-    store.record_project_duration(project_id, 123456789)
-    assert store.is_lpq_project(project_id)
+    assert not store.is_lpq_project("native", project_id)
+    store.record_project_duration("native", project_id, 123456789)
+    assert store.is_lpq_project("native", project_id)
