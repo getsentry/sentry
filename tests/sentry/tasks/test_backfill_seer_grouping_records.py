c81c3481e83239e808eb6bb2cd138445890d3549
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-05-21 16:37:03 -0700

    ref: Change grouping record backfill batch size (#71301)
    Change record backfill batch size from 20 to 10
    Fix flaky tests by setting event timestamp in the past

--- tests/sentry/tasks/test_backfill_seer_grouping_records.py
+++ tests/sentry/tasks/test_backfill_seer_grouping_records.py
@@ -26,6 +26,7 @@ from sentry.tasks.backfill_seer_grouping_records import (
     make_backfill_redis_key,
 )
 from sentry.testutils.cases import SnubaTestCase, TestCase
+from sentry.testutils.helpers.datetime import before_now, iso_format
 from sentry.testutils.helpers.features import with_feature
 from sentry.testutils.helpers.task_runner import TaskRunner
 from sentry.testutils.pytest.fixtures import django_db_all
@@ -93,7 +94,8 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
             data = {
                 "exception": self.create_exception_values(
                     function_names[i], type_names[i], value_names[i]
-                )
+                ),
+                "timestamp": iso_format(before_now(seconds=3)),
             }
             event = self.store_event(data=data, project_id=self.project.id, assert_no_errors=False)
             events.append(event)
@@ -112,21 +114,27 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
                 group_id=event.group.id,
                 hash="".join(choice(ascii_uppercase) for i in range(32)),
             )
+
         return {"rows": rows, "events": events, "messages": messages}
 
     def setUp(self):
         super().setUp()
-        bulk_data = self.create_group_event_rows(10)
+        bulk_data = self.create_group_event_rows(5)
         self.bulk_rows, self.bulk_events, self.bulk_messages = (
             bulk_data["rows"],
             bulk_data["events"],
             bulk_data["messages"],
         )
         self.event = self.store_event(
-            data={"exception": EXCEPTION}, project_id=self.project.id, assert_no_errors=False
+            data={"exception": EXCEPTION, "timestamp": iso_format(before_now(seconds=3))},
+            project_id=self.project.id,
+            assert_no_errors=False,
         )
         self.event.group.times_seen = 5
         self.event.group.save()
+
+        self.wait_for_event_count(self.project.id, 6)
+
         group_hashes = GroupHash.objects.all().distinct("group_id")
         self.group_hashes = {group_hash.group_id: group_hash.hash for group_hash in group_hashes}
 
@@ -238,7 +246,7 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         ]
         expected_stacktraces = [
             f'Error{i}: error with value\n  File "function_{i}.py", function function_{i}'
-            for i in range(10)
+            for i in range(5)
         ]
         assert bulk_event_ids == expected_event_ids
         assert invalid_event_ids == set()
@@ -431,7 +439,7 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         ]
         expected_stacktraces = [
             f'Error{i}: error with value\n  File "function_{i}.py", function function_{i}'
-            for i in range(10)
+            for i in range(5)
         ]
         assert bulk_group_data_stacktraces["data"] == expected_group_data
         assert bulk_group_data_stacktraces["stacktrace_list"] == expected_stacktraces
@@ -482,7 +490,7 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         ]
         expected_stacktraces = [
             f'Error{i}: error with value\n  File "function_{i}.py", function function_{i}'
-            for i in range(10)
+            for i in range(5)
         ]
         assert bulk_group_data_stacktraces["data"] == expected_group_data
         assert bulk_group_data_stacktraces["stacktrace_list"] == expected_stacktraces
@@ -538,7 +546,7 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         ]
         expected_stacktraces = [
             f'Error{i}: error with value\n  File "function_{i}.py", function function_{i}'
-            for i in range(9)
+            for i in range(4)
         ]
         assert bulk_group_data_stacktraces["data"] == expected_group_data
         assert bulk_group_data_stacktraces["stacktrace_list"] == expected_stacktraces
@@ -583,7 +591,7 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         ]
         expected_stacktraces = [
             f'Error{i}: error with value\n  File "function_{i}.py", function function_{i}'
-            for i in range(9)
+            for i in range(4)
         ]
         assert bulk_group_data_stacktraces["data"] == expected_group_data
         assert bulk_group_data_stacktraces["stacktrace_list"] == expected_stacktraces
@@ -597,7 +605,6 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
             },
         )
 
-    @django_db_all
     @with_feature("projects:similarity-embeddings-backfill")
     @patch("sentry.tasks.backfill_seer_grouping_records.post_bulk_grouping_records")
     def test_backfill_seer_grouping_records_success_simple(self, mock_post_bulk_grouping_records):
@@ -619,7 +626,6 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         last_processed_index = int(redis_client.get(make_backfill_redis_key(self.project.id)) or 0)
         assert last_processed_index == len(groups)
 
-    @django_db_all
     @patch("time.sleep", return_value=None)
     @patch("sentry.nodestore.backend.get_multi")
     @patch("sentry.tasks.backfill_seer_grouping_records.lookup_event")
@@ -642,7 +648,6 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         for group in Group.objects.filter(project_id=self.project.id):
             assert not group.data["metadata"].get("seer_similarity")
 
-    @django_db_all
     def test_backfill_seer_grouping_records_no_feature(self):
         """
         Test that the function does not create records when there is no feature flag
@@ -655,7 +660,6 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         for group in Group.objects.filter(project_id=self.project.id):
             assert not group.data["metadata"].get("seer_similarity")
 
-    @django_db_all
     @with_feature("projects:similarity-embeddings-backfill")
     @patch("sentry.tasks.backfill_seer_grouping_records.post_bulk_grouping_records")
     @patch("sentry.tasks.backfill_seer_grouping_records.delete_grouping_records")
@@ -698,7 +702,8 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
             data = {
                 "exception": self.create_exception_values(
                     function_names[i], type_names[i], value_names[i]
-                )
+                ),
+                "timestamp": iso_format(before_now(seconds=3)),
             }
             event = self.store_event(data=data, project_id=self.project.id, assert_no_errors=False)
             groups_seen_once.append(event.group)
@@ -742,7 +747,8 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
             data = {
                 "exception": self.create_exception_values(
                     function_names[i], type_names[i], value_names[i]
-                )
+                ),
+                "timestamp": iso_format(before_now(seconds=3)),
             }
             event = self.store_event(data=data, project_id=self.project.id, assert_no_errors=False)
             event.group.times_seen = 2
@@ -808,7 +814,8 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
         data = {
             "exception": self.create_exception_values(
                 "another_function!", "AnotherError!", "error with value"
-            )
+            ),
+            "timestamp": iso_format(before_now(seconds=3)),
         }
         event = self.store_event(data=data, project_id=self.project.id, assert_no_errors=False)
         event.group.times_seen = 2
@@ -854,7 +861,6 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
     @pytest.mark.skip(
         "this test is flakey in production; trying to replicate locally and skipping it for now"
     )
-    @django_db_all
     @with_feature("projects:similarity-embeddings-backfill")
     @patch("sentry.tasks.backfill_seer_grouping_records.post_bulk_grouping_records")
     def test_backfill_seer_grouping_records_multiple_batches(self, mock_post_bulk_grouping_records):
@@ -869,7 +875,8 @@ class TestBackfillSeerGroupingRecords(SnubaTestCase, TestCase):
             data = {
                 "exception": self.create_exception_values(
                     function_names[i], type_names[i], value_names[i]
-                )
+                ),
+                "timestamp": iso_format(before_now(seconds=3)),
             }
             event = self.store_event(data=data, project_id=self.project.id, assert_no_errors=False)
             event.group.times_seen = 2
