5a125936deaa9930a226c9148963c729de1517fc
Kevin Liu <kevinsliu0@gmail.com>
2024-05-21 15:09:28 -0400

    feat(insights): add status and retry filtering to queue sample panel (#71136)
    Adds status and retry filtering to the queue sample panel. Retry filters
    are only available for consumers.
    
    <img width="949" alt="image"
    src="https://github.com/getsentry/sentry/assets/58920989/74708859-9357-4f43-b84f-6c85cdd32e13">

--- static/app/views/performance/queues/settings.ts
+++ static/app/views/performance/queues/settings.ts
@@ -18,6 +18,28 @@ export const DEFAULT_QUERY_FILTER = 'span.op:[queue.process,queue.publish]';
 export const CONSUMER_QUERY_FILTER = 'span.op:queue.process';
 export const PRODUCER_QUERY_FILTER = 'span.op:queue.publish';
 
+export const TRACE_STATUS_OPTIONS = [
+  'ok',
+  'cancelled',
+  'unknown',
+  'unknown_error',
+  'invalid_argument',
+  'deadline_exceeded',
+  'not_found',
+  'already_exists',
+  'permission_denied',
+  'resource_exhausted',
+  'failed_precondition',
+  'aborted',
+  'out_of_range',
+  'unimplemented',
+  'internal_error',
+  'unavailable',
+  'data_loss',
+  'unauthenticated',
+];
+export const RETRY_COUNT_OPTIONS = ['0', '1-3', '4+'];
+
 export enum MessageActorType {
   PRODUCER = 'producer',
   CONSUMER = 'consumer',
