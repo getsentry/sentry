87d67b6e9be57020c6d3f74fb79331491ea01827
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-05-21 15:37:01 -0400

    ♻️ Replace context card component with generic one

---
+++ static/app/components/keyValueData/card.tsx
@@ -0,0 +1,188 @@
+import {useState} from 'react';
+import styled from '@emotion/styled';
+
+import {AnnotatedTextErrors} from 'sentry/components/events/meta/annotatedText/annotatedTextErrors';
+import Link from 'sentry/components/links/link';
+import Panel from 'sentry/components/panels/panel';
+import {StructuredData} from 'sentry/components/structuredEventData';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import type {KeyValueListDataItem, MetaError} from 'sentry/types';
+import {defined} from 'sentry/utils';
+
+export interface ContentProps {
+  /**
+   * Item to display
+   */
+  item: KeyValueListDataItem;
+  /**
+   * Displays tag value as plain text, rather than a hyperlink if applicable.
+   */
+  disableRichValue?: boolean;
+  /**
+   * Errors pertaining to content item
+   */
+  errors?: MetaError[];
+  /**
+   * Metadata pertaining to content item
+   */
+  meta?: Record<string, any>;
+}
+
+export function Content({
+  item,
+  meta,
+  errors = [],
+  disableRichValue = false,
+  ...props
+}: ContentProps) {
+  const {subject, value: contextValue, action = {}} = item;
+
+  const hasErrors = errors.length > 0;
+
+  const dataComponent = (
+    <StructuredData
+      value={contextValue}
+      depth={0}
+      maxDefaultDepth={0}
+      meta={meta}
+      withAnnotatedText
+      withOnlyFormattedText
+    />
+  );
+
+  return (
+    <ContentWrapper hasErrors={hasErrors} {...props}>
+      <Subject>{subject}</Subject>
+      <ValueSection hasErrors={hasErrors}>
+        <ValueWrapper>
+          {!disableRichValue && defined(action?.link) ? (
+            <Link to={action.link}>{dataComponent}</Link>
+          ) : (
+            dataComponent
+          )}
+        </ValueWrapper>
+        {hasErrors && (
+          <div>
+            <AnnotatedTextErrors errors={errors} />
+          </div>
+        )}
+      </ValueSection>
+    </ContentWrapper>
+  );
+}
+
+export interface CardProps {
+  /**
+   * ContentProps items to be rendered in this card.
+   */
+  contentItems: ContentProps[];
+  /**
+   *  Flag to disable alphabetical sorting by item subject. Uses given item ordering.
+   */
+  skipSorting?: boolean;
+  /**
+   * Title of the key value data grouping
+   */
+  title?: string;
+  /**
+   * Content item length which, when exceeded, displays a 'Show more' option
+   */
+  truncateLength?: number;
+}
+
+export function Card({
+  contentItems,
+  title,
+  truncateLength = Infinity,
+  skipSorting = false,
+}: CardProps) {
+  const [isTruncated, setIsTruncated] = useState(contentItems.length > truncateLength);
+
+  if (contentItems.length === 0) {
+    return null;
+  }
+
+  const truncatedItems = isTruncated
+    ? contentItems.slice(0, truncateLength)
+    : [...contentItems];
+
+  const orderedItems = skipSorting
+    ? truncatedItems
+    : truncatedItems.sort((a, b) => a.item.subject.localeCompare(b.item.subject));
+
+  const componentItems = orderedItems.map((itemProps, i) => (
+    <Content key={`content-card-${title}-${i}`} {...itemProps} />
+  ));
+
+  return (
+    <CardPanel>
+      {title && <Title>{title}</Title>}
+      {componentItems}
+      {contentItems.length > truncateLength && (
+        <TruncateWrapper onClick={() => setIsTruncated(!isTruncated)}>
+          {isTruncated ? t('Show more...') : t('Show less')}
+        </TruncateWrapper>
+      )}
+    </CardPanel>
+  );
+}
+
+const CardPanel = styled(Panel)`
+  padding: ${space(0.75)};
+  display: grid;
+  column-gap: ${space(1.5)};
+  grid-template-columns: minmax(100px, auto) 1fr;
+  font-size: ${p => p.theme.fontSizeSmall};
+`;
+
+const Title = styled('p')`
+  grid-column: span 2;
+  padding: ${space(0.25)} ${space(0.75)};
+  margin: 0;
+  color: ${p => p.theme.headingColor};
+  font-weight: bold;
+`;
+
+const ContentWrapper = styled('div')<{hasErrors: boolean}>`
+  display: grid;
+  grid-template-columns: subgrid;
+  grid-column: span 2;
+  column-gap: ${space(1.5)};
+  padding: ${space(0.25)} ${space(0.75)};
+  border-radius: 4px;
+  color: ${p => (p.hasErrors ? p.theme.alert.error.color : p.theme.subText)};
+  border: 1px solid ${p => (p.hasErrors ? p.theme.alert.error.border : 'transparent')};
+  background-color: ${p =>
+    p.hasErrors ? p.theme.alert.error.backgroundLight : p.theme.background};
+  &:nth-child(odd) {
+    background-color: ${p =>
+      p.hasErrors ? p.theme.alert.error.backgroundLight : p.theme.backgroundSecondary};
+  }
+`;
+
+const Subject = styled('div')`
+  grid-column: span 1;
+  font-family: ${p => p.theme.text.familyMono};
+  word-break: break-word;
+`;
+
+const ValueSection = styled(Subject)<{hasErrors: boolean}>`
+  color: ${p => (p.hasErrors ? 'inherit' : p.theme.textColor)};
+  grid-column: span 1;
+  display: grid;
+  grid-template-columns: 1fr auto;
+  grid-column-gap: ${space(0.5)};
+`;
+
+const ValueWrapper = styled('div')`
+  word-break: break-word;
+`;
+
+const TruncateWrapper = styled('a')`
+  display: flex;
+  grid-column: 1 / -1;
+  margin: ${space(0.5)} 0;
+  justify-content: center;
+  font-family: ${p => p.theme.text.family};
+`;
