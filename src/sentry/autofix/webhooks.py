7a7d122389f72be97cc1786ab8ccaaf6facd6899
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-05-21 15:33:14 -0700

    Update src/sentry/autofix/webhooks.py
    Co-authored-by: Cathy Teng <70817427+cathteng@users.noreply.github.com>

--- src/sentry/autofix/webhooks.py
+++ src/sentry/autofix/webhooks.py
@@ -19,37 +19,20 @@ def get_webhook_analytics_fields(autofix_state: dict[str, Any]) -> dict[str, Any
 
 def handle_github_pr_webhook_for_autofix(
     org: Organization, action: str, pull_request: dict, github_user: dict
-):
-    if github_user["id"] == AUTOFIX_GITHUB_APP_USER_ID:
-        if action == "opened":
-            autofix_state = get_autofix_state_from_pr_id("integrations:github", pull_request["id"])
-            if autofix_state:
-                analytics.record(
-                    "ai.autofix.pr.opened",
-                    organization_id=org.id,
-                    integration="github",
-                    **get_webhook_analytics_fields(autofix_state),
-                )
-                metrics.incr("ai.autofix.pr.opened")
-        if action == "closed":
-            autofix_state = get_autofix_state_from_pr_id("integrations:github", pull_request["id"])
-            if autofix_state:
-                if pull_request["merged"]:
-                    # PR Merged
-                    analytics.record(
-                        "ai.autofix.pr.merged",
-                        organization_id=org.id,
-                        integration="github",
-                        **get_webhook_analytics_fields(autofix_state),
-                    )
-                    metrics.incr("ai.autofix.pr.merged")
+) -> None:
+    if github_user["id"] != AUTOFIX_GITHUB_APP_USER_ID:
+        return
 
-                elif pull_request["merged"] is False:
-                    # PR Closed
-                    analytics.record(
-                        "ai.autofix.pr.closed",
-                        organization_id=org.id,
-                        integration="github",
-                        **get_webhook_analytics_fields(autofix_state),
-                    )
-                    metrics.incr("ai.autofix.pr.closed")
+    if action in ["opened", "closed"]:
+        autofix_state = get_autofix_state_from_pr_id("integrations:github", pull_request["id"])
+        if autofix_state:
+            analytic_action = "opened" if action == "opened" else "closed"
+            if pull_request["merged"]:
+                analytic_action = "merged"
+            analytics.record(
+                f"ai.autofix.pr.{analytic_action}",
+                organization_id=org.id,
+                integration="github",
+                **get_webhook_analytics_fields(autofix_state),
+            )
+            metrics.incr(f"ai.autofix.pr.{analytic_action}")
