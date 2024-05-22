e0547ac24f182b09efc3a215bb36ba2e613c4695
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2024-05-22 12:27:17 -0400

    fix(performance): fix link to webvital docs (#71351)
    Fixes webvitals docs link pointing to the wrong page

--- static/app/views/performance/browser/webVitals/settings.ts
+++ static/app/views/performance/browser/webVitals/settings.ts
@@ -9,5 +9,4 @@ export const REPLACE_FID_WITH_INP = false;
 export const MODULE_DESCRIPTION = t(
   'Measure the quality of real user experience in your web applications using industry standard quality signals.'
 );
-export const MODULE_DOC_LINK =
-  'https://docs.sentry.io/product/performance/cache-monitoring/';
+export const MODULE_DOC_LINK = 'https://docs.sentry.io/product/performance/web-vitals/';
