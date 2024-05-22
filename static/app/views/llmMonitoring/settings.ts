fb192cc089aa325d2b2bc89d37b5583f7255df6a
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-05-22 14:01:55 -0400

    Apply to LLM docs

--- static/app/views/llmMonitoring/settings.ts
+++ static/app/views/llmMonitoring/settings.ts
@@ -2,3 +2,5 @@ import {t} from 'sentry/locale';
 
 export const MODULE_TITLE = t('LLM Monitoring');
 export const BASE_URL = 'llm-monitoring';
+
+export const MODULE_DOC_LINK = 'https://docs.sentry.io/product/llm-monitoring/';
