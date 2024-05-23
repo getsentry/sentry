f85801271fcfb537da36c58175394ebdee7f4f77
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-05-23 14:27:46 -0400

    Account for missing organization

--- static/app/views/performance/utils/useInsightsURL.tsx
+++ static/app/views/performance/utils/useInsightsURL.tsx
@@ -12,7 +12,12 @@ export function useInsightsURL(moduleName: RoutableModuleNames) {
 type URLBuilder = (moduleName: RoutableModuleNames) => string;
 
 export function useInsightsURLBuilder(): URLBuilder {
-  const organization = useOrganization();
+  const organization = useOrganization({allowNull: true}); // Some parts of the app, like the main sidebar, render even if the organization isn't available (during loading, or at all).
+
+  if (!organization) {
+    // If there isn't an organization, items that link to modules won't be visible, so this is a fallback just-in-case, and isn't trying too hard to be useful
+    return () => '';
+  }
 
   return function (moduleName: RoutableModuleNames) {
     // If `insights` flag is present, all Insights modules are routed from `/insights`. If the flag is absent, LLM is routed from `/` and other insights modules are routed of `/performance`
