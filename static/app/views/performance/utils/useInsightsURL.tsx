87e2059525093fae1286cc7469a48f59929f07ee
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-05-23 10:32:56 -0400

    Move Insights modules to `/insights`

---
+++ static/app/views/performance/utils/useInsightsURL.tsx
@@ -0,0 +1,25 @@
+import useOrganization from 'sentry/utils/useOrganization';
+import {ModuleName} from 'sentry/views/starfish/types';
+
+type ModuleNameStrings = `${ModuleName}`;
+type RoutableModuleNames = Exclude<ModuleNameStrings, '' | 'other'>;
+
+export function useInsightsURL(moduleName: RoutableModuleName) {
+  const builder = useInsightsURLBuilder();
+  return builder(moduleName);
+}
+
+type URLBuilder = (moduleName: RoutableModuleNames) => string;
+
+export function useInsightsURLBuilder(): URLBuilder {
+  const organization = useOrganization();
+
+  return function (moduleName: RoutableModuleNames) {
+    // If `insights` flag is present, all Insights modules are routed from `/insights`. If the flag is absent, LLM is routed from `/` and other insights modules are routed of `/performance`
+    return organization?.features?.includes('performance-insights')
+      ? 'insights'
+      : moduleName === ModuleName.AI
+        ? ''
+        : 'performance';
+  };
+}
