d72e8bc81276f9a3a06de0fce2932b4a71961e4d
Nar Saynorath <nar.saynorath@sentry.io>
2024-05-24 16:47:16 -0400

    wip: tests

---
+++ static/app/views/performance/mobile/usePlatformSelector.spec.tsx
@@ -0,0 +1,7 @@
+describe('usePlatformSelector', () => {
+  it('returns null for project if >1 project is selected', () => {});
+
+  it('returns the corresponding project data if a single project is selected', () => {});
+
+  it('returns the selected platform', () => {});
+});
