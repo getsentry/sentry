8ec67d00c49775624dad4de2ea6c8c5edea8e27a
Nar Saynorath <nar.saynorath@sentry.io>
2024-05-27 09:14:05 -0400

    add tests

--- static/app/views/performance/mobile/usePlatformSelector.spec.tsx
+++ static/app/views/performance/mobile/usePlatformSelector.spec.tsx
@@ -1,7 +1,98 @@
+import {LocationFixture} from 'sentry-fixture/locationFixture';
+import {ProjectFixture} from 'sentry-fixture/project';
+
+import {renderHook} from 'sentry-test/reactTestingLibrary';
+
+import type {Project} from 'sentry/types/project';
+import {useLocation} from 'sentry/utils/useLocation';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import useProjects from 'sentry/utils/useProjects';
+import {PlatformSelector as PlatformSelectorComponent} from 'sentry/views/performance/mobile/screenload/screens/platformSelector';
+import usePlatformSelector from 'sentry/views/performance/mobile/usePlatformSelector';
+
+jest.mock('sentry/utils/usePageFilters');
+jest.mock('sentry/utils/useLocation');
+jest.mock('sentry/utils/useProjects');
+
+function mockPageFilters(projects: number[]) {
+  jest.mocked(usePageFilters).mockReturnValue({
+    isReady: true,
+    desyncedFilters: new Set(),
+    pinnedFilters: new Set(),
+    shouldPersist: true,
+    selection: {
+      datetime: {
+        period: '10d',
+        start: null,
+        end: null,
+        utc: false,
+      },
+      environments: [],
+      projects,
+    },
+  });
+}
+
+function mockProjects(projects: Project[]) {
+  jest.mocked(useProjects).mockReturnValue({
+    fetchError: null,
+    fetching: false,
+    hasMore: false,
+    initiallyLoaded: false,
+    onSearch: jest.fn(),
+    placeholders: [],
+    projects,
+  });
+}
+
 describe('usePlatformSelector', () => {
-  it('returns null for project if >1 project is selected', () => {});
+  let mockProject: Project;
+  beforeEach(() => {
+    jest.clearAllMocks();
 
-  it('returns the corresponding project data if a single project is selected', () => {});
+    mockProject = ProjectFixture({platform: 'flutter'});
+    jest.mocked(useLocation).mockReturnValue(LocationFixture());
+    mockProjects([mockProject]);
+  });
 
-  it('returns the selected platform', () => {});
+  it('returns null for project if >1 project is selected', () => {
+    mockPageFilters([1, 2, 3]);
+
+    const {result} = renderHook(usePlatformSelector);
+
+    const {project, isProjectCrossPlatform} = result.current;
+
+    expect(project).toBeNull();
+    expect(isProjectCrossPlatform).toBe(false);
+  });
+
+  it('returns the corresponding project data if a single project is selected', () => {
+    mockPageFilters([parseInt(mockProject.id, 10)]);
+
+    const {result} = renderHook(usePlatformSelector);
+
+    const {project, isProjectCrossPlatform, PlatformSelector, selectedPlatform} =
+      result.current;
+
+    expect(project).toBe(mockProject);
+    expect(isProjectCrossPlatform).toBe(true);
+
+    // The default selection result is "Android"
+    expect(selectedPlatform).toBe('Android');
+    expect(PlatformSelector).toBe(PlatformSelectorComponent);
+  });
+
+  it('returns false for isProjectCrossPlatform if project is not cross platform', () => {
+    const testProject = ProjectFixture({platform: 'python'});
+
+    mockProjects([testProject]);
+    mockPageFilters([parseInt(testProject.id, 10)]);
+
+    const {result} = renderHook(usePlatformSelector);
+
+    const {project, isProjectCrossPlatform} = result.current;
+
+    expect(project).toBe(testProject);
+    expect(isProjectCrossPlatform).toBe(false);
+  });
 });
