efa4029a04e001acadfb5fcebed678ddbc9acb8d
Nar Saynorath <nar.saynorath@sentry.io>
2024-05-27 13:50:02 -0400

    ref(insights): Add platform selector hook to screenloads (#71502)
    There's a lot of code around that's specific for determining if a single
    project is selected and what platform that project is. I'm adding the
    platform selector to app starts and before doing that I wanted to
    extract some of that code to simplify the components more. This is in
    preparation to add the platform selector to App Starts.
    
    I also removed any cases of
    `organization.features.includes('spans-first-ui');` because these
    modules should already be gated by that feature flag.

--- static/app/views/performance/mobile/screenload/screenLoadSpans/table.tsx
+++ static/app/views/performance/mobile/screenload/screenLoadSpans/table.tsx
@@ -13,7 +13,6 @@ import Pagination from 'sentry/components/pagination';
 import {Tooltip} from 'sentry/components/tooltip';
 import {t, tct} from 'sentry/locale';
 import type {NewQuery} from 'sentry/types/organization';
-import type {Project} from 'sentry/types/project';
 import {browserHistory} from 'sentry/utils/browserHistory';
 import type {TableDataRow} from 'sentry/utils/discover/discoverQuery';
 import type {MetaType} from 'sentry/utils/discover/eventView';
@@ -32,14 +31,9 @@ import {
   TTID_CONTRIBUTING_SPAN_OPS,
 } from 'sentry/views/performance/mobile/screenload/screenLoadSpans/spanOpSelector';
 import {MobileCursors} from 'sentry/views/performance/mobile/screenload/screens/constants';
-import {
-  DEFAULT_PLATFORM,
-  PLATFORM_LOCAL_STORAGE_KEY,
-  PLATFORM_QUERY_PARAM,
-} from 'sentry/views/performance/mobile/screenload/screens/platformSelector';
 import {useTableQuery} from 'sentry/views/performance/mobile/screenload/screens/screensTable';
-import {isCrossPlatform} from 'sentry/views/performance/mobile/screenload/screens/utils';
 import {MODULE_DOC_LINK} from 'sentry/views/performance/mobile/screenload/settings';
+import useCrossPlatformProject from 'sentry/views/performance/mobile/useCrossPlatformProject';
 import {useModuleURL} from 'sentry/views/performance/utils/useModuleURL';
 import {
   PRIMARY_RELEASE_ALIAS,
@@ -57,7 +51,6 @@ const {SPAN_SELF_TIME, SPAN_DESCRIPTION, SPAN_GROUP, SPAN_OP, PROJECT_ID} =
 
 type Props = {
   primaryRelease?: string;
-  project?: Project | null;
   secondaryRelease?: string;
   transaction?: string;
 };
@@ -66,24 +59,18 @@ export function ScreenLoadSpansTable({
   transaction,
   primaryRelease,
   secondaryRelease,
-  project,
 }: Props) {
   const moduleURL = useModuleURL('screen_load');
   const location = useLocation();
   const {selection} = usePageFilters();
   const organization = useOrganization();
   const cursor = decodeScalar(location.query?.[MobileCursors.SPANS_TABLE]);
+  const {isProjectCrossPlatform, selectedPlatform} = useCrossPlatformProject();
 
   const spanOp = decodeScalar(location.query[SpanMetricsField.SPAN_OP]) ?? '';
   const {hasTTFD, isLoading: hasTTFDLoading} = useTTFDConfigured([
     `transaction:"${transaction}"`,
   ]);
-
-  const hasPlatformSelectFeature = organization.features.includes('spans-first-ui');
-  const platform =
-    decodeScalar(location.query[PLATFORM_QUERY_PARAM]) ??
-    localStorage.getItem(PLATFORM_LOCAL_STORAGE_KEY) ??
-    DEFAULT_PLATFORM;
 
   const queryStringPrimary = useMemo(() => {
     const searchQuery = new MutableSearch([
@@ -95,17 +82,16 @@ export function ScreenLoadSpansTable({
         : [`span.op:[${TTID_CONTRIBUTING_SPAN_OPS.join(',')}]`]),
     ]);
 
-    if (project && isCrossPlatform(project) && hasPlatformSelectFeature) {
-      searchQuery.addFilterValue('os.name', platform);
+    if (isProjectCrossPlatform) {
+      searchQuery.addFilterValue('os.name', selectedPlatform);
     }
 
     return appendReleaseFilters(searchQuery, primaryRelease, secondaryRelease);
   }, [
-    hasPlatformSelectFeature,
-    platform,
+    isProjectCrossPlatform,
     primaryRelease,
-    project,
     secondaryRelease,
+    selectedPlatform,
     spanOp,
     transaction,
   ]);
