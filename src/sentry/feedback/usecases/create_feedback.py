5c3860ea444d44bf85a186b8ac91ff899f617d5e
Andrew Liu <andrew.liu@sentry.io>
2024-05-28 16:24:30 -0700

    Remove unused creation src

--- src/sentry/feedback/usecases/create_feedback.py
+++ src/sentry/feedback/usecases/create_feedback.py
@@ -32,7 +32,6 @@ UNREAL_FEEDBACK_UNATTENDED_MESSAGE = "Sent in the unattended mode"
 
 class FeedbackCreationSource(Enum):
     NEW_FEEDBACK_ENVELOPE = "new_feedback_envelope"
-    NEW_FEEDBACK_DJANGO_ENDPOINT = "new_feedback_sentry_django_endpoint"  # TODO: delete this once feedback_ingest API deprecated
     USER_REPORT_DJANGO_ENDPOINT = "user_report_sentry_django_endpoint"
     USER_REPORT_ENVELOPE = "user_report_envelope"
     CRASH_REPORT_EMBED_FORM = "crash_report_embed_form"
@@ -44,7 +43,6 @@ class FeedbackCreationSource(Enum):
             c.value
             for c in [
                 cls.NEW_FEEDBACK_ENVELOPE,
-                cls.NEW_FEEDBACK_DJANGO_ENDPOINT,
             ]
         }
 
