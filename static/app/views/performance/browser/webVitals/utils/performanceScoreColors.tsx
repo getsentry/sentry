f1153381999ed88eabef73808ab2034580064006
KevinL10 <kevinsliu0@gmail.com>
2024-05-28 17:44:46 -0400

    add border property

--- static/app/views/performance/browser/webVitals/utils/performanceScoreColors.tsx
+++ static/app/views/performance/browser/webVitals/utils/performanceScoreColors.tsx
@@ -1,18 +1,22 @@
 export const PERFORMANCE_SCORE_COLORS = {
   good: {
     light: 'green100',
+    border: 'green300',
     normal: 'green300',
   },
   needsImprovement: {
     light: 'yellow100',
+    border: 'yellow300',
     normal: 'yellow400',
   },
   bad: {
     light: 'red100',
+    border: 'red300',
     normal: 'red300',
   },
   none: {
     light: 'gray100',
+    border: 'gray300',
     normal: 'gray300',
   },
 };
