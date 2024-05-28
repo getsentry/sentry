3a394e394c347f868f6a6d61d05c17544f8cea1e
Matej Minar <matej.minar@sentry.io>
2024-05-28 08:45:16 +0200

    feat(metrics): Duplicate metrics with different units warning (#71529)
    ![CleanShot 2024-05-27 at 18 30
    56](https://github.com/getsentry/sentry/assets/9060071/d5ec8a9a-8632-481e-9502-5a2228be965c)
    
    
    
    Closes https://github.com/getsentry/sentry/issues/71513

--- static/app/views/metrics/metricListItemDetails.tsx
+++ static/app/views/metrics/metricListItemDetails.tsx
@@ -12,7 +12,7 @@ import {navigateTo} from 'sentry/actionCreators/navigation';
 import {Button, LinkButton} from 'sentry/components/button';
 import ProjectBadge from 'sentry/components/idBadge/projectBadge';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
-import {IconSettings, IconWarning} from 'sentry/icons';
+import {IconInfo, IconSettings, IconWarning} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {MetricMeta, MRI} from 'sentry/types/metrics';
@@ -41,7 +41,9 @@ export function MetricListItemDetails({
   metric,
   selectedProjects,
   onTagClick,
+  isDuplicateWithDifferentUnit,
 }: {
+  isDuplicateWithDifferentUnit: boolean;
   metric: MetricMeta;
   onTagClick: (mri: MRI, tag: string) => void;
   selectedProjects: Project[];
@@ -122,11 +124,23 @@ export function MetricListItemDetails({
         <MetricName>
           {/* Add zero width spaces at delimiter characters for nice word breaks */}
           {formatMRI(metric.mri).replaceAll(/([\.\/-_])/g, '\u200b$1')}
+          {isDuplicateWithDifferentUnit && (
+            <Disclaimer textColor="yellow400">
+              <IconWrapper>
+                <IconWarning color="yellow400" size="xs" />
+              </IconWrapper>
+              {t(
+                'Metrics with the same name and different units were detected. Unwanted metrics can be disabled in settings.'
+              )}
+            </Disclaimer>
+          )}
           {!isCustomMetric && (
-            <SamplingWarning>
-              <IconWarning color="yellow400" size="xs" />
+            <Disclaimer>
+              <IconWrapper>
+                <IconInfo size="xs" />
+              </IconWrapper>
               {t('Prone to client-side sampling')}
-            </SamplingWarning>
+            </Disclaimer>
           )}
         </MetricName>
         {isCustomMetric &&
@@ -264,11 +278,14 @@ const TagWrapper = styled('span')`
   white-space: nowrap;
 `;
 
-const SamplingWarning = styled('div')`
+const Disclaimer = styled('div')<{textColor?: string}>`
   display: grid;
-  gap: ${space(0.25)};
+  gap: ${space(0.5)};
   grid-template-columns: max-content 1fr;
   font-size: ${p => p.theme.fontSizeSmall};
-  color: ${p => p.theme.yellow400};
-  align-items: center;
+  color: ${p => (p.textColor ? p.theme[p.textColor] ?? null : null)};
+`;
+
+const IconWrapper = styled('div')`
+  margin-top: 1px;
 `;
