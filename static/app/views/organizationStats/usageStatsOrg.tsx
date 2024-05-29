32521f430796027846f6c6a1dd285ad5af66ef04
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-05-29 09:12:48 +0200

    fix(stats): metric stats

--- static/app/views/organizationStats/usageStatsOrg.tsx
+++ static/app/views/organizationStats/usageStatsOrg.tsx
@@ -439,7 +439,10 @@ class UsageStatsOrganization<
       orgStats.groups.forEach(group => {
         const {outcome, category} = group.by;
         // HACK: The backend enum are singular, but the frontend enums are plural
-        if (!dataCategory.includes(`${category}`)) {
+        if (
+          dataCategory !== DATA_CATEGORY_INFO.metrics.plural &&
+          !dataCategory.includes(`${category}`)
+        ) {
           return;
         }
 
@@ -483,7 +486,6 @@ class UsageStatsOrganization<
         } as any);
         (chartStats.filtered as any[])?.push({value: [stat.date, stat.filtered]});
       });
-
       return {
         cardStats: {
           total: formatUsageWithUnits(
