2edb494d3190d461a4d1ff1808a0c18c33553cc6
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-05-29 14:05:52 -0400

    update

--- static/app/views/performance/browser/resources/settings.ts
+++ static/app/views/performance/browser/resources/settings.ts
@@ -1,6 +1,7 @@
 import {t} from 'sentry/locale';
 
 export const MODULE_TITLE = t('Assets');
+export const DATA_TYPE = t('Asset');
 export const BASE_URL = 'browser/assets'; // Name of the data shown (singular)
 export const MODULE_DESCRIPTION = t(
   'Find large and slow-to-load resources used by your application and understand their impact on page performance.'
