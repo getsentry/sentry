37ecafc8fa51dfda7c70878a20009befc87f19b0
mdtro <20070360+mdtro@users.noreply.github.com>
2024-05-31 17:39:40 -0500

    explicit outbox control in migration

--- tests/sentry/migrations/test_0726_apitoken_backfill_hashes.py
+++ tests/sentry/migrations/test_0726_apitoken_backfill_hashes.py
@@ -1,6 +1,6 @@
+from sentry.models.outbox import ControlOutbox, OutboxCategory, OutboxScope
 from sentry.testutils.cases import TestMigrations
 from sentry.testutils.helpers import override_options
-from sentry.testutils.outbox import outbox_runner
 from sentry.testutils.silo import control_silo_test
 
 
@@ -12,12 +12,20 @@ class TestBackfillApiTokenHashesMigration(TestMigrations):
 
     @override_options({"apitoken.save-hash-on-create": False})
     def setup_initial_state(self):
-        with outbox_runner():
-            self.user_1 = self.create_user()
-            self.user_1_auth_token = self.create_user_auth_token(user=self.user_1)
+        self.user_1 = self.create_user()
+        self.user_1_auth_token = self.create_user_auth_token(user=self.user_1)
 
-            assert self.user_1_auth_token.hashed_token is None
+        # Put the user in an org so we have membership
+        self.create_organization(owner=self.user_1)
+
+        assert self.user_1_auth_token.hashed_token is None
 
     def test_for_hashed_value(self):
         self.user_1_auth_token.refresh_from_db()
         assert self.user_1_auth_token.hashed_token is not None
+        assert ControlOutbox.objects.get(
+            shard_scope=OutboxScope.USER_SCOPE,
+            category=OutboxCategory.API_TOKEN_UPDATE,
+            object_identifier=self.user_1_auth_token.id,
+            shard_identifier=self.user_1_auth_token.user_id,
+        )
