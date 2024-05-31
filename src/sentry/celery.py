ace8208bac5da5ab687b4a467cdc3d04bfaea9fc
Yash Kamothi <yash.kamothi@gmail.com>
2024-05-31 10:37:08 -0700

    add extra metadata

--- src/sentry/celery.py
+++ src/sentry/celery.py
@@ -1,6 +1,7 @@
 import gc
 from datetime import datetime
 from itertools import chain
+from typing import Any
 
 from celery import Celery, Task, signals
 from celery.worker.request import Request
@@ -85,11 +86,20 @@ def celery_prefork_freeze_gc(**kwargs: object) -> None:
 class SentryTask(Task):
     Request = "sentry.celery:SentryRequest"
 
-    def delay(self, *args, **kwargs):
+    @classmethod
+    def _add_metadata(cls, kwargs: dict[str, Any]) -> None:
+        """
+        Helper method that adds relevant metadata
+        """
+        # Add the start time when the task was kicked off for async processing by the calling code
         kwargs["__start_time"] = datetime.now().timestamp()
+
+    def delay(self, *args, **kwargs):
+        self._add_metadata(kwargs)
         return super().delay(*args, **kwargs)
 
     def apply_async(self, *args, **kwargs):
+        self._add_metadata(kwargs)
         # If intended detect bad uses of pickle and make the tasks fail in tests.  This should
         # in theory pick up a lot of bad uses without accidentally failing tasks in prod.
         if (
