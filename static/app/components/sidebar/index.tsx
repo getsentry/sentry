f36d66b6330f74d5a56ce2002e8edea10eeeb259
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-05-31 10:37:00 -0400

    Gate Insights module access

--- static/app/components/sidebar/index.tsx
+++ static/app/components/sidebar/index.tsx
@@ -249,7 +249,7 @@ function Sidebar() {
   const moduleURLBuilder = useModuleURLBuilder(true);
 
   const queries = hasOrganization && (
-    <Feature key="db" features="spans-first-ui" organization={organization}>
+    <Feature key="db" features="insights-entry-points" organization={organization}>
       <SidebarItem
         {...sidebarItemProps}
         label={
@@ -263,7 +263,7 @@ function Sidebar() {
   );
 
   const requests = hasOrganization && (
-    <Feature key="http" features="spans-first-ui" organization={organization}>
+    <Feature key="http" features="insights-entry-points" organization={organization}>
       <SidebarItem
         {...sidebarItemProps}
         label={<GuideAnchor target="performance-http">{MODULE_TITLES.http}</GuideAnchor>}
@@ -275,7 +275,11 @@ function Sidebar() {
   );
 
   const caches = hasOrganization && (
-    <Feature key="cache" features="performance-cache-view" organization={organization}>
+    <Feature
+      key="cache"
+      features={['insights-entry-points', 'performance-cache-view']}
+      organization={organization}
+    >
       <SidebarItem
         {...sidebarItemProps}
         label={
@@ -290,7 +294,7 @@ function Sidebar() {
   );
 
   const webVitals = hasOrganization && (
-    <Feature key="vital" features="spans-first-ui" organization={organization}>
+    <Feature key="vital" features="insights-entry-points" organization={organization}>
       <SidebarItem
         {...sidebarItemProps}
         label={
@@ -304,7 +308,11 @@ function Sidebar() {
   );
 
   const queues = hasOrganization && (
-    <Feature key="queue" features="performance-queues-view" organization={organization}>
+    <Feature
+      key="queue"
+      features={['insights-entry-points', 'performance-queues-view']}
+      organization={organization}
+    >
       <SidebarItem
         {...sidebarItemProps}
         label={
@@ -319,7 +327,11 @@ function Sidebar() {
   );
 
   const screenLoads = hasOrganization && (
-    <Feature key="screen_load" features="spans-first-ui" organization={organization}>
+    <Feature
+      key="screen_load"
+      features="insights-entry-points"
+      organization={organization}
+    >
       <SidebarItem
         {...sidebarItemProps}
         label={MODULE_TITLES.screen_load}
@@ -331,7 +343,7 @@ function Sidebar() {
   );
 
   const appStarts = hasOrganization && (
-    <Feature key="app_start" features="spans-first-ui" organization={organization}>
+    <Feature key="app_start" features="insights-entry-points" organization={organization}>
       <SidebarItem
         {...sidebarItemProps}
         label={MODULE_TITLES.app_start}
@@ -345,7 +357,7 @@ function Sidebar() {
   const mobileUI = hasOrganization && (
     <Feature
       key="mobile-ui"
-      features={['spans-first-ui', 'starfish-mobile-ui-module']}
+      features={['insights-entry-points', 'starfish-mobile-ui-module']}
       organization={organization}
     >
       <SidebarItem
@@ -360,7 +372,7 @@ function Sidebar() {
   );
 
   const resources = hasOrganization && (
-    <Feature key="resource" features="spans-first-ui">
+    <Feature key="resource" features="insights-entry-points">
       <SidebarItem
         {...sidebarItemProps}
         label={<GuideAnchor target="starfish">{resourceModuleTitle}</GuideAnchor>}
@@ -385,7 +397,10 @@ function Sidebar() {
   );
 
   const llmMonitoring = hasOrganization && (
-    <Feature features="ai-analytics" organization={organization}>
+    <Feature
+      features={['insights-entry-points', 'ai-analytics']}
+      organization={organization}
+    >
       <SidebarItem
         {...sidebarItemProps}
         icon={hasNewSidebarHierarchy ? <SubitemDot collapsed /> : <IconRobot />}
