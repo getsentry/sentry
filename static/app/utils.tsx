9f2150ea2351189c8d1f684d063377561191b50e
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-05-31 10:33:09 -0700

    cleanup: Remove unused isFunction (#71838)
    Related to https://github.com/getsentry/frontend-tsc/issues/13

--- static/app/utils.tsx
+++ static/app/utils.tsx
@@ -290,8 +290,6 @@ export function generateQueryWithTag(prevQuery: Query, tag: EventTag): Query {
   return query;
 }
 
-export const isFunction = (value: any): value is Function => typeof value === 'function';
-
 // NOTE: only escapes a " if it's not already escaped
 export function escapeDoubleQuotes(str: string) {
   return str.replace(/\\([\s\S])|(")/g, '\\$1$2');
