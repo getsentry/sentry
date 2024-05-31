95c5acc958c8db519625e31bb2455547b025574b
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-05-31 12:20:42 -0400

    Typo

--- static/app/views/llmMonitoring/settings.ts
+++ static/app/views/llmMonitoring/settings.ts
@@ -4,7 +4,7 @@ import {t} from 'sentry/locale';
 export const MODULE_TITLE = t('LLM Monitoring');
 export const BASE_URL = 'llm-monitoring';
 
-export const RELEASE_LEVEL: BadgeType = '';
+export const RELEASE_LEVEL: BadgeType = 'alpha';
 
 // NOTE: Awkward typing, but without it `RELEASE_LEVEL` is narrowed and the comparison is not allowed
 export const releaseLevelAsBadgeProps = {
