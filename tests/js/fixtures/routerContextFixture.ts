3be7b36b8a468af5f247b947fa68ebdf6c1a5d83
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-05-31 01:13:00 -0400

    test(js): Remove organization from RouterContextFixture (#71815)
    organization is no longer ever in the legacy context

--- tests/js/fixtures/routerContextFixture.ts
+++ tests/js/fixtures/routerContextFixture.ts
@@ -1,5 +1,4 @@
 import {LocationFixture} from 'sentry-fixture/locationFixture';
-import {OrganizationFixture} from 'sentry-fixture/organization';
 import {ProjectFixture} from 'sentry-fixture/project';
 import {RouterFixture} from 'sentry-fixture/routerFixture';
 
@@ -10,14 +9,12 @@ export function RouterContextFixture([context, childContextTypes] = []) {
     context: {
       location: LocationFixture(),
       router: RouterFixture(),
-      organization: OrganizationFixture(),
       project: ProjectFixture(),
       ...context,
     },
     childContextTypes: {
       router: SentryPropTypeValidators.isObject,
       location: SentryPropTypeValidators.isObject,
-      organization: SentryPropTypeValidators.isObject,
       project: SentryPropTypeValidators.isObject,
       ...childContextTypes,
     },
