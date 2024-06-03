ec3b9a09c6f316e077f19e51816afeee1b116aab
MichaelSun48 <michael.sun@sentry.io>
2024-06-03 15:07:16 -0700

    Add org to model relation, test to ensure only members views are deleted and not user

--- src/sentry/deletions/defaults/organizationmember.py
+++ src/sentry/deletions/defaults/organizationmember.py
@@ -5,6 +5,11 @@ from ..base import ModelDeletionTask, ModelRelation
 
 class OrganizationMemberDeletionTask(ModelDeletionTask):
     def get_child_relations(self, instance):
-        relations = [ModelRelation(GroupSearchView, {"user_id": instance.user_id})]
+        relations = [
+            ModelRelation(
+                GroupSearchView,
+                {"user_id": instance.user_id, "organization": instance.organization},
+            )
+        ]
 
         return relations
