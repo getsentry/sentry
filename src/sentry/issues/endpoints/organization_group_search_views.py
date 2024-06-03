c1d1fd87d1c618e2bd055c1b432ef2e5436cf6d0
MichaelSun48 <michael.sun@sentry.io>
2024-06-03 15:00:40 -0700

    Remove references to Put/post endpoint

--- src/sentry/issues/endpoints/organization_group_search_views.py
+++ src/sentry/issues/endpoints/organization_group_search_views.py
@@ -33,7 +33,6 @@ DEFAULT_VIEWS: list[GroupSearchViewSerializerResponse] = [
 class MemberPermission(OrganizationPermission):
     scope_map = {
         "GET": ["member:read", "member:write"],
-        "PUT": ["member:read", "member:write"],
     }
 
 
@@ -41,7 +40,6 @@ class MemberPermission(OrganizationPermission):
 class OrganizationGroupSearchViewsEndpoint(OrganizationEndpoint):
     publish_status = {
         "GET": ApiPublishStatus.EXPERIMENTAL,
-        "POST": ApiPublishStatus.EXPERIMENTAL,
     }
     owner = ApiOwner.ISSUES
     permission_classes = (MemberPermission,)
