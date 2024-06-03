9315db6f2d5b5c7b6d6e2e234bd76f5fd378c26b
MichaelSun48 <michael.sun@sentry.io>
2024-06-03 10:50:41 -0700

    Update old migration tests with new logic

--- tests/sentry/migrations/test_0726_backfill_groupsearchviews_with_pinned_searches.py
+++ tests/sentry/migrations/test_0726_backfill_groupsearchviews_with_pinned_searches.py
@@ -4,11 +4,13 @@ from sentry.testutils.cases import TestMigrations
 
 
 class BackfillIssueViewsWithPinnedSearchesTest(TestMigrations):
-    migrate_from = "0724_discover_saved_query_dataset"
-    migrate_to = "0725_backfill_issue_views_with_pinned_searches"
+    migrate_from = "0725_create_sentry_groupsearchview_table"
+    migrate_to = "0726_backfill_groupsearchviews_with_pinned_searches"
 
     def setup_initial_state(self):
         self.user = self.create_user()
+        self.org = self.create_organization(owner=self.user)
+
         self.saved_search = SavedSearch.objects.create(
             organization=self.org,
             owner_id=self.user.id,
@@ -16,7 +18,6 @@ class BackfillIssueViewsWithPinnedSearchesTest(TestMigrations):
             query="assigned:me",
             query_sort="date",
         )
-        self.org = self.create_organization(owner=self.user)
 
     def test(self):
         custom_view = GroupSearchView.objects.get(org_member_id=self.user.id)
