6e534db634da31a78d0e08f58d66562832bb881c
MichaelSun48 <michael.sun@sentry.io>
2024-06-03 10:40:46 -0700

    rebase and fix merge conflicts. Update logic to reflect new table

---
+++ tests/sentry/migrations/test_0726_backfill_issue_views_with_pinned_searches.py
@@ -0,0 +1,29 @@
+from sentry.models.groupsearchview import GroupSearchView
+from sentry.models.savedsearch import SavedSearch
+from sentry.testutils.cases import TestMigrations
+
+
+class BackfillIssueViewsWithPinnedSearchesTest(TestMigrations):
+    migrate_from = "0724_discover_saved_query_dataset"
+    migrate_to = "0725_backfill_issue_views_with_pinned_searches"
+
+    def setup_initial_state(self):
+        self.user = self.create_user()
+        self.saved_search = SavedSearch.objects.create(
+            organization=self.org,
+            owner_id=self.user.id,
+            visibility="owner_pinned",
+            query="assigned:me",
+            query_sort="date",
+        )
+        self.org = self.create_organization(owner=self.user)
+
+    def test(self):
+        custom_view = GroupSearchView.objects.get(org_member_id=self.user.id)
+
+        assert custom_view
+        assert custom_view.organizaiton == self.org
+        assert custom_view.user_id == self.user.id
+        assert custom_view.name == "Default View"
+        assert custom_view.query == "assigned:me"
+        assert custom_view.query_sort == "date"
