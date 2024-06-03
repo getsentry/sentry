cb074bbe233bc173bcc24f162cf7377b0508bfc9
Simon Hellmayr <simon.hellmayr@sentry.io>
2024-06-03 08:47:22 +0200

    chore(metrics): remove old metrics options from options automator

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -2644,21 +2644,3 @@ register(
     default=100,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
-
-# Enable percentile operations in the metrics/meta endpoint in the Metrics API for the orgs in the list. This is used to
-# also hide those expensive operations from view in the Metrics UI for everyone except the whitelist.
-register(
-    "sentry-metrics.metrics-api.enable-percentile-operations-for-orgs",
-    type=Sequence,
-    default=[],
-    flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
-)
-
-# Enable the "last" operation in the metrics/meta endpoint in the Metrics API for the orgs in the list. This is used to
-# also hide those expensive operations from view in the Metrics UI for everyone except the whitelist.
-register(
-    "sentry-metrics.metrics-api.enable-gauge-last-for-orgs",
-    type=Sequence,
-    default=[],
-    flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
-)
