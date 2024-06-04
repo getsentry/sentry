5e2e817e3f6a1add70fad21ee786e2666de2ca56
Scott Cooper <scttcper@gmail.com>
2024-06-04 10:02:14 -0700

    ref(pageFilters): Return stable reference from getState (#71817)

--- static/app/stores/pageFiltersStore.tsx
+++ static/app/stores/pageFiltersStore.tsx
@@ -4,7 +4,7 @@ import {getDefaultSelection} from 'sentry/components/organizations/pageFilters/u
 import type {PageFilters, PinnedPageFilter} from 'sentry/types/core';
 import {valueIsEqual} from 'sentry/utils/object/valueIsEqual';
 
-import type {CommonStoreDefinition} from './types';
+import type {StrictStoreDefinition} from './types';
 
 function datetimeHasSameValue(
   a: PageFilters['datetime'],
@@ -36,12 +36,16 @@ function datetimeHasSameValue(
   return true;
 }
 
-interface CommonState {
+interface PageFiltersState {
   /**
    * The set of page filters which have been pinned but do not match the current
    * URL state.
    */
   desyncedFilters: Set<PinnedPageFilter>;
+  /**
+   * Are page filters ready?
+   */
+  isReady: boolean;
   /**
    * The set of page filters which are currently pinned
    */
@@ -58,27 +62,7 @@ interface CommonState {
   shouldPersist: boolean;
 }
 
-/**
- * External state
- */
-interface State extends CommonState {
-  /**
-   * Are page filters ready?
-   */
-  isReady: boolean;
-}
-
-interface InternalDefinition extends CommonState {
-  /**
-   * Have we initalized page filters?
-   */
-  hasInitialState: boolean;
-}
-
-interface PageFiltersStoreDefinition
-  extends InternalDefinition,
-    CommonStoreDefinition<State> {
-  init(): void;
+interface PageFiltersStoreDefinition extends StrictStoreDefinition<PageFiltersState> {
   onInitializeUrlState(
     newSelection: PageFilters,
     pinned: Set<PinnedPageFilter>,
@@ -95,45 +79,46 @@ interface PageFiltersStoreDefinition
 }
 
 const storeConfig: PageFiltersStoreDefinition = {
-  selection: getDefaultSelection(),
-  pinnedFilters: new Set(),
-  desyncedFilters: new Set(),
-  shouldPersist: true,
-  hasInitialState: false,
+  state: {
+    isReady: false,
+    selection: getDefaultSelection(),
+    pinnedFilters: new Set(),
+    desyncedFilters: new Set(),
+    shouldPersist: true,
+  },
 
   init() {
     // XXX: Do not use `this.listenTo` in this store. We avoid usage of reflux
     // listeners due to their leaky nature in tests.
 
-    this.reset(this.selection);
+    this.reset(this.state.selection);
   },
 
   reset(selection) {
-    this._isReady = false;
-    this.selection = selection || getDefaultSelection();
-    this.pinnedFilters = new Set();
+    this.state = {
+      ...this.state,
+      isReady: false,
+      selection: selection || getDefaultSelection(),
+      pinnedFilters: new Set(),
+    };
   },
 
   /**
    * Initializes the page filters store data
    */
   onInitializeUrlState(newSelection, pinned, persist = true) {
-    this._isReady = true;
-
-    this.selection = newSelection;
-    this.pinnedFilters = pinned;
-    this.shouldPersist = persist;
+    this.state = {
+      ...this.state,
+      isReady: true,
+      selection: newSelection,
+      pinnedFilters: pinned,
+      shouldPersist: persist,
+    };
     this.trigger(this.getState());
   },
 
   getState() {
-    return {
-      selection: this.selection,
-      pinnedFilters: this.pinnedFilters,
-      desyncedFilters: this.desyncedFilters,
-      shouldPersist: this.shouldPersist,
-      isReady: this._isReady,
-    };
+    return this.state;
   },
 
   onReset() {
@@ -142,78 +127,87 @@ const storeConfig: PageFiltersStoreDefinition = {
   },
 
   updatePersistence(shouldPersist: boolean) {
-    this.shouldPersist = shouldPersist;
+    this.state = {...this.state, shouldPersist};
     this.trigger(this.getState());
   },
 
   updateDesyncedFilters(filters: Set<PinnedPageFilter>) {
-    this.desyncedFilters = filters;
+    this.state = {...this.state, desyncedFilters: filters};
     this.trigger(this.getState());
   },
 
   updateProjects(projects = [], environments = null) {
-    if (valueIsEqual(this.selection.projects, projects)) {
+    if (valueIsEqual(this.state.selection.projects, projects)) {
       return;
     }
 
-    if (this.desyncedFilters.has('projects')) {
-      const newDesyncedFilters = new Set(this.desyncedFilters);
+    const newDesyncedFilters = new Set(this.state.desyncedFilters);
+    if (this.state.desyncedFilters.has('projects')) {
       newDesyncedFilters.delete('projects');
-      this.desyncedFilters = newDesyncedFilters;
     }
 
-    this.selection = {
-      ...this.selection,
+    const selection = {
+      ...this.state.selection,
       projects,
-      environments: environments === null ? this.selection.environments : environments,
+      environments:
+        environments === null ? this.state.selection.environments : environments,
     };
+    this.state = {...this.state, selection, desyncedFilters: newDesyncedFilters};
     this.trigger(this.getState());
   },
 
   updateDateTime(newDateTime) {
-    if (datetimeHasSameValue(this.selection.datetime, newDateTime)) {
+    if (datetimeHasSameValue(this.state.selection.datetime, newDateTime)) {
       return;
     }
 
-    if (this.desyncedFilters.has('datetime')) {
-      const newDesyncedFilters = new Set(this.desyncedFilters);
+    const newDesyncedFilters = new Set(this.state.desyncedFilters);
+    if (this.state.desyncedFilters.has('datetime')) {
       newDesyncedFilters.delete('datetime');
-      this.desyncedFilters = newDesyncedFilters;
     }
 
-    this.selection = {
-      ...this.selection,
-      datetime: newDateTime,
+    this.state = {
+      ...this.state,
+      selection: {
+        ...this.state.selection,
+        datetime: newDateTime,
+      },
+      desyncedFilters: newDesyncedFilters,
     };
     this.trigger(this.getState());
   },
 
   updateEnvironments(environments) {
-    if (valueIsEqual(this.selection.environments, environments)) {
+    if (valueIsEqual(this.state.selection.environments, environments)) {
       return;
     }
 
-    if (this.desyncedFilters.has('environments')) {
-      const newDesyncedFilters = new Set(this.desyncedFilters);
+    const newDesyncedFilters = new Set(this.state.desyncedFilters);
+    if (this.state.desyncedFilters.has('environments')) {
       newDesyncedFilters.delete('environments');
-      this.desyncedFilters = newDesyncedFilters;
     }
 
-    this.selection = {
-      ...this.selection,
-      environments: environments ?? [],
+    this.state = {
+      ...this.state,
+      desyncedFilters: newDesyncedFilters,
+      selection: {
+        ...this.state.selection,
+        environments: environments ?? [],
+      },
     };
 
     this.trigger(this.getState());
   },
 
   pin(filter, pin) {
+    const newPinnedFilters = new Set(this.state.pinnedFilters);
     if (pin) {
-      this.pinnedFilters.add(filter);
+      newPinnedFilters.add(filter);
     } else {
-      this.pinnedFilters.delete(filter);
+      newPinnedFilters.delete(filter);
     }
 
+    this.state = {...this.state, pinnedFilters: newPinnedFilters};
     this.trigger(this.getState());
   },
 };
