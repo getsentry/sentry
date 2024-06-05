65197c9e739684990be64af0d16023f3643cb2a7
anthony sottile <anthony.sottile@sentry.io>
2024-06-05 09:24:56 -0400

    Reapply "ref: upgrade sentry to python 3.12 (#65075)"
    This reverts commit 889bf3e4e05a8a0f9346d931375ed4b91e59c7c0.

--- tests/sentry/test_devimports.py
+++ tests/sentry/test_devimports.py
@@ -94,7 +94,7 @@ for xfail in {xfail!r}:
         raise SystemExit(f'unexpected success importing {{xfail}}')
 """
 
-    env = {"SENTRY_ENVIRONMENT": "production", "SETUPTOOLS_USE_DISTUTILS": "stdlib"}
+    env = {"SENTRY_ENVIRONMENT": "production"}
     ret = subprocess.run(
         (sys.executable, "-c", script),
         env=env,
