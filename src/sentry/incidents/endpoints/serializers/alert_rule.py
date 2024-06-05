d174c04c4b1038a9890d4f5fe10e2a38f0d0d280
Mark Story <mark@mark-story.com>
2024-06-05 12:24:19 -0400

    chore(hybridcloud) Move hacky AlertRule sentryapp logic to serializer
    These changes echo the changes made in #71956 to AlertRule
    serialization. Getting this logic out of the endpoint and into the
    serializer will be helpful for reducing complexity here in the future.
    
    Refs HC-1024

--- src/sentry/incidents/endpoints/serializers/alert_rule.py
+++ src/sentry/incidents/endpoints/serializers/alert_rule.py
@@ -13,6 +13,7 @@ from drf_spectacular.utils import extend_schema_serializer
 from sentry import features
 from sentry.api.serializers import Serializer, register, serialize
 from sentry.api.serializers.models.rule import RuleSerializer
+from sentry.constants import SentryAppStatus
 from sentry.incidents.models.alert_rule import (
     AlertRule,
     AlertRuleActivity,
@@ -23,10 +24,15 @@ from sentry.incidents.models.alert_rule import (
 )
 from sentry.incidents.models.alert_rule_activations import AlertRuleActivations
 from sentry.incidents.models.incident import Incident
+from sentry.models.apiapplication import ApiApplication
+from sentry.models.integrations.sentry_app import SentryApp
+from sentry.models.integrations.sentry_app_component import SentryAppComponent
+from sentry.models.integrations.sentry_app_installation import SentryAppInstallation
 from sentry.models.rule import Rule
 from sentry.models.rulesnooze import RuleSnooze
 from sentry.models.user import User
 from sentry.services.hybrid_cloud.app import app_service
+from sentry.services.hybrid_cloud.app.model import RpcSentryApp
 from sentry.services.hybrid_cloud.user import RpcUser
 from sentry.services.hybrid_cloud.user.service import user_service
 from sentry.snuba.models import SnubaQueryEventType
@@ -85,6 +91,7 @@ class AlertRuleSerializerResponse(AlertRuleSerializerResponseOptional):
     createdBy: dict
     monitorType: int
     activations: list[dict]
+    errors: list[str]
 
 
 @register(AlertRule)
@@ -93,8 +100,9 @@ class AlertRuleSerializer(Serializer):
     Serializer for returning an alert rule to the client
     """
 
-    def __init__(self, expand: list[str] | None = None):
+    def __init__(self, expand: list[str] | None = None, prepare_component_fields: bool = False):
         self.expand = expand or []
+        self.prepare_component_fields = prepare_component_fields
 
     def get_attrs(
         self, item_list: Sequence[Any], user: User | RpcUser, **kwargs: Any
@@ -110,30 +118,93 @@ class AlertRuleSerializer(Serializer):
             alert_rule_trigger__alert_rule_id__in=alert_rules.keys()
         ).exclude(Q(sentry_app_config__isnull=True) | Q(sentry_app_id__isnull=True))
 
-        sentry_app_installations_by_sentry_app_id = app_service.get_related_sentry_app_components(
-            organization_ids=[alert_rule.organization_id for alert_rule in alert_rules.values()],
-            sentry_app_ids=list(trigger_actions.values_list("sentry_app_id", flat=True)),
-            type="alert-rule-action",
-        )
+        sentry_app_map: dict[int, RpcSentryApp] = {}
+        sentry_app_installations_by_sentry_app_id: Mapping[str, Any] = {}
+        organization_ids = list({alert_rule.organization_id for alert_rule in alert_rules.values()})
+        if self.prepare_component_fields:
+            sentry_app_ids = list(trigger_actions.values_list("sentry_app_id", flat=True))
+
+            sentry_app_installations_by_sentry_app_id = (
+                app_service.get_related_sentry_app_components(
+                    organization_ids=organization_ids,
+                    sentry_app_ids=sentry_app_ids,
+                    type="alert-rule-action",
+                )
+            )
+            if sentry_app_ids:
+                fetched_rpc_installations = app_service.get_many(
+                    filter=dict(app_ids=sentry_app_ids, organization_id=organization_ids[0])
+                )
+                sentry_app_map = {
+                    install.sentry_app.id: install.sentry_app
+                    for install in fetched_rpc_installations
+                }
 
         for trigger, serialized in zip(triggers, serialized_triggers):
-            alert_rule_triggers = result[alert_rules[trigger.alert_rule_id]].setdefault(
-                "triggers", []
-            )
+            errors = []
+            alert_rule = alert_rules[trigger.alert_rule_id]
+            alert_rule_triggers = result[alert_rule].setdefault("triggers", [])
             for action in serialized.get("actions", []):
                 if action is None:
                     continue
 
-                sentry_app_id = str(action.get("sentryAppId"))
+                # Prepare AlertRuleTriggerActions that are SentryApp components
                 install = None
+                sentry_app_id = str(action.get("sentryAppId"))
                 if sentry_app_id:
                     install = sentry_app_installations_by_sentry_app_id.get(sentry_app_id)
                 if install:
-                    action["_sentry_app_component"] = install.get("sentry_app_component")
-                    action["_sentry_app_installation"] = install.get("sentry_app_installation")
-                    action["sentryAppInstallationUuid"] = install.get(
-                        "sentry_app_installation"
-                    ).get("uuid")
+                    sentry_app_component = install.get("sentry_app_component")
+                    sentry_app_installation = install.get("sentry_app_installation")
+                    action["sentryAppInstallationUuid"] = sentry_app_installation.get("uuid")
+
+                    # TODO(hybridcloud) This is nasty and should be fixed.
+                    # Because all of the prepare_* functions currently operate on ORM
+                    # records we need to convert our RpcSentryApp and dict data into detached
+                    # ORM models and stitch together relations used in preparing UI components.
+                    installation = SentryAppInstallation(**sentry_app_installation)
+                    rpc_app = sentry_app_map.get(installation.sentry_app_id)
+                    installation.sentry_app = SentryApp(
+                        id=rpc_app.id,
+                        scope_list=rpc_app.scope_list,
+                        application_id=rpc_app.application_id,
+                        application=ApiApplication(
+                            id=rpc_app.application.id,
+                            client_id=rpc_app.application.client_id,
+                            client_secret=rpc_app.application.client_secret,
+                        ),
+                        proxy_user_id=rpc_app.proxy_user_id,
+                        owner_id=rpc_app.owner_id,
+                        name=rpc_app.name,
+                        slug=rpc_app.slug,
+                        uuid=rpc_app.uuid,
+                        events=rpc_app.events,
+                        webhook_url=rpc_app.webhook_url,
+                        status=SentryAppStatus.as_int(rpc_app.status),
+                        metadata=rpc_app.metadata,
+                    )
+                    # The api_token_id field is nulled out to prevent relation traversal as these
+                    # ORM objects are turned back into RPC objects.
+                    installation.api_token_id = None
+
+                    component = installation.prepare_ui_component(
+                        SentryAppComponent(**sentry_app_component),
+                        None,
+                        action.get("settings"),
+                    )
+                    if component is None:
+                        errors.append(
+                            {
+                                "detail": f"Could not fetch details from {installation.sentry_app.name}"
+                            }
+                        )
+                        action["disabled"] = True
+                        continue
+
+                    action["formFields"] = component.schema.get("settings", {})
+
+            if errors:
+                result[alert_rule]["errors"] = errors
             alert_rule_triggers.append(serialized)
 
         alert_activations_ranked = AlertRuleActivations.objects.annotate(
@@ -278,6 +349,8 @@ class AlertRuleSerializer(Serializer):
 
         if "latestIncident" in self.expand:
             data["latestIncident"] = attrs.get("latestIncident", None)
+        if "errors" in attrs:
+            data["errors"] = attrs["errors"]
 
         return data
 
