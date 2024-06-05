022488e58c7b4f13d5d54f8ce9ee3c206f66a225
Arthur Knaus <arthur.knaus@sentry.io>
2024-06-05 17:03:35 +0200

    ref(metrics): Extract MRISelect as separate component

--- static/app/views/metrics/mriSelect.spec.tsx
+++ static/app/views/metrics/mriSelect.spec.tsx
@@ -1,5 +1,5 @@
 import type {MetricMeta} from 'sentry/types/metrics';
-import {getMetricsWithDuplicateNames} from 'sentry/views/metrics/queryBuilder';
+import {getMetricsWithDuplicateNames} from 'sentry/views/metrics/mriSelect';
 
 function createMetricMeta(name: string, unit: string): MetricMeta {
   return {
