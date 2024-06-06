3365963e1b3b46f444aa084c1edb98d623327f8a
Andrew Liu <159852527+aliu3ntry@users.noreply.github.com>
2024-06-06 16:37:53 -0400

    metrics(replay): add a counter for hydrate error breadcrumbs (#72271)
    https://github.com/getsentry/sentry/issues/72266
    Can refine with tags, extras, later. Org/project is too high cardinality

--- src/sentry/replays/usecases/ingest/dom_index.py
+++ src/sentry/replays/usecases/ingest/dom_index.py
@@ -450,6 +450,7 @@ def _handle_breadcrumb(
             return click
 
     elif category == "replay.hydrate-error":
+        metrics.incr("replay.hydration_error_breadcrumb")
         if replay_event is not None and _should_report_hydration_error_issue(project_id):
             report_hydration_error_issue_with_replay_event(
                 project_id,
