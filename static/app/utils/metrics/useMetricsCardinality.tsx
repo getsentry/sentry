0cae3604ec267390da49e967dfc3deab487e8712
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-06-06 13:43:51 +0200

    feat(metrics): settings cardinality indicator

---
+++ static/app/utils/metrics/useMetricsCardinality.tsx
@@ -0,0 +1,52 @@
+import type {MRI} from 'sentry/types';
+import type {Project} from 'sentry/types/project';
+import {useMetricsQuery} from 'sentry/utils/metrics/useMetricsQuery';
+
+type Props = {
+  project: Project;
+};
+
+const CARDINALITY_QUERIES = [
+  {
+    name: 'a',
+    mri: 'g:metric_stats/cardinality@none' as MRI,
+    op: 'max',
+    groupBy: ['mri'],
+    query: '!mri:"" cardinality.window:3600',
+    orderBy: 'desc' as 'desc' | 'asc',
+  },
+];
+
+const CARDINALITY_DATE_TIME = {
+  period: '1h',
+  start: null,
+  end: null,
+  utc: null,
+};
+
+const CARDINALITY_INTERVAL = '1h';
+
+export function useMetricsCardinality({project}: Props) {
+  const cardinalityQuery = useMetricsQuery(
+    CARDINALITY_QUERIES,
+    {
+      environments: [],
+      datetime: CARDINALITY_DATE_TIME,
+      projects: [parseInt(project.id, 10)],
+    },
+    {interval: CARDINALITY_INTERVAL, includeSeries: false}
+  );
+
+  if (cardinalityQuery.data?.data[0]) {
+    const data = cardinalityQuery.data.data[0].reduce(
+      (acc, group) => {
+        acc[group.by.mri] = group.totals;
+        return acc;
+      },
+      {} as Record<string, number>
+    );
+    return {...cardinalityQuery, data};
+  }
+
+  return {...cardinalityQuery, data: undefined};
+}
