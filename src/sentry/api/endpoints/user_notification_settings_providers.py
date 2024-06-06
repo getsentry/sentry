19ced1d5c20622e6840b62c90bd6c670e60002f2
Athena Moghaddam <132939361+sentaur-athena@users.noreply.github.com>
2024-06-06 11:25:37 -0700

    chore(apis): Update ownership from issues to alerts & notifications (#71190)
    After re-orgs some endpoints are better owned by alerts & notifications
    vs. issues. I'm not sure if the ownership I'm assigning in this PR is
    correct so will get help from both teams.

--- src/sentry/api/endpoints/user_notification_settings_providers.py
+++ src/sentry/api/endpoints/user_notification_settings_providers.py
@@ -24,7 +24,7 @@ class UserNotificationSettingsProvidersEndpoint(UserEndpoint):
         "GET": ApiPublishStatus.PRIVATE,
         "PUT": ApiPublishStatus.PRIVATE,
     }
-    owner = ApiOwner.ISSUES
+    owner = ApiOwner.ALERTS_NOTIFICATIONS
     # TODO(Steve): Make not private when we launch new system
     private = True
 
