d638fdb81f53bcf701979091e7ec07dc2186e95d
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2024-06-06 13:37:34 -0400

    ref(error_eventtype): Easier to read
    This changes includes:
    
    * Include the module for stronger typing
    * Create more functions to reduce main function complexity

--- src/sentry/eventtypes/error.py
+++ src/sentry/eventtypes/error.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 from collections.abc import MutableMapping
 from typing import Any
 
@@ -10,54 +8,42 @@ from .base import BaseEvent, compute_title_with_tree_label
 
 Metadata = dict[str, Any]
 
-
-def get_crash_location(data):
-    from sentry.stacktraces.processing import get_crash_frame_from_event_data
-
-    frame = get_crash_frame_from_event_data(
-        data, frame_filter=lambda x: x.get("function") not in (None, "<redacted>", "<unknown>")
-    )
-    if frame is not None:
-        from sentry.stacktraces.functions import get_function_name_for_frame
-
-        func = get_function_name_for_frame(frame, data.get("platform"))
-        return frame.get("filename") or frame.get("abs_path"), func
+# These platforms are considered non-native/mobile platforms.
+NON_NATIVE_MOBILE_PLATFORMS = (
+    "cocoa",
+    "objc",
+    "native",
+    "swift",
+    "c",
+    "android",
+    "apple-ios",
+    "cordova",
+    "capacitor",
+    "javascript-cordova",
+    "javascript-capacitor",
+    "react-native",
+    "flutter",
+    "dart-flutter",
+    "unity",
+    "dotnet-xamarin",
+)
 
 
 class ErrorEvent(BaseEvent):
     key = "error"
 
     def extract_metadata(self, data: MutableMapping[str, Any]) -> Metadata:
-
+        """Extracts the metadata from the event data."""
         exceptions = get_path(data, "exception", "values")
         if not exceptions:
             return {}
 
-        # When there are multiple exceptions, we need to pick one to extract the metadata from.
-        # If the event data has been marked with a main_exception_id, then we should be able to
-        # find the exception with the matching metadata.exception_id and use that one.
-        # This can be the case for some exception groups.
-
-        # Otherwise, the default behavior is to use the last one in the list.
-
+        # With chained exceptions, the SDK could set the main_exception_id to
+        # indicate which exception to use for title & subtitle
         main_exception_id = get_path(data, "main_exception_id")
-        exception = (
-            next(
-                exception
-                for exception in exceptions
-                if get_path(exception, "mechanism", "exception_id") == main_exception_id
-            )
-            if main_exception_id
-            else None
-        )
-
-        if not exception:
-            exception = get_path(exceptions, -1)
-
+        exception = get_exception(exceptions, main_exception_id)
         if not exception:
             return {}
-
-        loc = get_crash_location(data)
         rv = {"value": trim(get_path(exception, "value", default=""), 1024)}
 
         # If the exception mechanism indicates a synthetic exception we do not
@@ -66,6 +52,7 @@ class ErrorEvent(BaseEvent):
             rv["type"] = trim(get_path(exception, "type", default="Error"), 128)
 
         # Attach crash location if available
+        loc = get_crash_location(data)
         if loc is not None:
             fn, func = loc
             if fn:
@@ -73,32 +60,15 @@ class ErrorEvent(BaseEvent):
             if func:
                 rv["function"] = func
 
-        rv["display_title_with_tree_label"] = data.get("platform") in (
-            # For now we disable rendering of tree labels for non-native/mobile
-            # platform in issuestream and everywhere else but the grouping
-            # breakdown. The grouping breakdown overrides this flag to force
-            # tree labels to show.
-            #
-            # In the frontend we look at the event platform directly when
-            # rendering the title to apply this logic to old data that doesn't
-            # have this flag materialized.
-            "cocoa",
-            "objc",
-            "native",
-            "swift",
-            "c",
-            "android",
-            "apple-ios",
-            "cordova",
-            "capacitor",
-            "javascript-cordova",
-            "javascript-capacitor",
-            "react-native",
-            "flutter",
-            "dart-flutter",
-            "unity",
-            "dotnet-xamarin",
-        )
+        # For now we disable rendering of tree labels for non-native/mobile
+        # platform in issuestream and everywhere else but the grouping
+        # breakdown. The grouping breakdown overrides this flag to force
+        # tree labels to show.
+        #
+        # In the frontend we look at the event platform directly when
+        # rendering the title to apply this logic to old data that doesn't
+        # have this flag materialized.
+        rv["display_title_with_tree_label"] = data.get("platform") in NON_NATIVE_MOBILE_PLATFORMS
 
         return rv
 
@@ -119,3 +89,42 @@ class ErrorEvent(BaseEvent):
 
     def get_location(self, metadata: Metadata) -> str | None:
         return metadata.get("filename")
+
+
+def get_crash_location(data: MutableMapping[str, Any]) -> tuple[str | None, str | None] | None:
+    """If available, return the frame and function of the crash location."""
+    from sentry.stacktraces.processing import get_crash_frame_from_event_data
+
+    frame = get_crash_frame_from_event_data(
+        data, frame_filter=lambda x: x.get("function") not in (None, "<redacted>", "<unknown>")
+    )
+    if frame is not None:
+        from sentry.stacktraces.functions import get_function_name_for_frame
+
+        func = get_function_name_for_frame(frame, data.get("platform"))
+        return frame.get("filename") or frame.get("abs_path"), func
+    return None
+
+
+def get_exception(exceptions: MutableMapping[str, Any], main_exception_id: str) -> str | None:
+    """Returns the exception from the event data."""
+    # When there are multiple exceptions, we need to pick one to extract the metadata from.
+    # If the event data has been marked with a main_exception_id, then we should be able to
+    # find the exception with the matching metadata.exception_id and use that one.
+    # This can be the case for some exception groups.
+
+    # Otherwise, the default behavior is to use the last one in the list.
+    exception = (
+        next(
+            exception
+            for exception in exceptions
+            if get_path(exception, "mechanism", "exception_id") == main_exception_id
+        )
+        if main_exception_id
+        else None
+    )
+
+    if not exception:
+        exception = get_path(exceptions, -1)
+
+    return exception
