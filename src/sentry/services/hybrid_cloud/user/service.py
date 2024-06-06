b07a6525860b6bc327ae6e42cd803606980e8313
Mark Story <mark@mark-story.com>
2024-06-06 09:39:45 -0400

    feat(hybridcloud) Add caching decorator for many records (#71719)
    The user_service.get_many() call is the highest volume RPC call we have,
    and it could also benefit from caching. Currently the get_user() method
    has a hit rate ~70%, and I think get_many can have a high hit rate as
    well.
    
    These changes only modify the existing single record cache read and add
    the plumbing for multiple record cache reads.
    
    Refs HC-1185

--- src/sentry/services/hybrid_cloud/user/service.py
+++ src/sentry/services/hybrid_cloud/user/service.py
@@ -25,7 +25,6 @@ from sentry.services.hybrid_cloud.user.model import (
     UserIdEmailArgs,
 )
 from sentry.silo.base import SiloMode
-from sentry.utils import metrics
 
 
 class UserService(RpcService):
@@ -193,7 +192,6 @@ class UserService(RpcService):
 
         :param user_id: The user to fetch
         """
-        metrics.incr("user_service.get_user.call")
         return get_user(user_id)
 
     @rpc_method
@@ -313,7 +311,6 @@ class UserService(RpcService):
 
 @back_with_silo_cache("user_service.get_user", SiloMode.REGION, RpcUser)
 def get_user(user_id: int) -> RpcUser | None:
-    metrics.incr("user_service.get_user.rpc_call")
     users = user_service.get_many(filter=dict(user_ids=[user_id]))
     if len(users) > 0:
         return users[0]
