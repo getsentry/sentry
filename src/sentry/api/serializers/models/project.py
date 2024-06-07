51209fced2ced6fc271e7f1f0202ffdd6eeadb03
Sebastian Zivota <loewenheim@mailbox.org>
2024-06-07 16:36:16 +0200

    Use any

--- src/sentry/api/serializers/models/project.py
+++ src/sentry/api/serializers/models/project.py
@@ -687,19 +687,16 @@ class ProjectSummarySerializer(ProjectWithTeamSerializer):
             deploys_by_project = self.get_deploys_by_project(item_list)
 
         for item in item_list:
-            # check if the project is in LPQ for any platform
-            symbolication_degraded = False
-            for platform in ["native", "js", "jvm"]:
-                if should_demote_symbolication(platform, project_id=item.id):
-                    symbolication_degraded = True
-                    break
-
             attrs[item]["latest_release"] = latest_release_versions.get(item.id)
             attrs[item]["environments"] = environments_by_project.get(item.id, [])
             attrs[item]["has_user_reports"] = item.id in projects_with_user_reports
             if not self._collapse(LATEST_DEPLOYS_KEY):
                 attrs[item]["deploys"] = deploys_by_project.get(item.id)
-            attrs[item]["symbolication_degraded"] = symbolication_degraded
+            # check if the project is in LPQ for any platform
+            attrs[item]["symbolication_degraded"] = any(
+                should_demote_symbolication(platform, project_id=item.id)
+                for platform in ["native", "js", "jvm"]
+            )
 
         return attrs
 
