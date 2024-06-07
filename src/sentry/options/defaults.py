0b69a2bce0b0ae5a0f22323e2827d2a644c06612
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2024-06-07 12:22:12 -0700

    ref(slack): use sdk client in send_confirmation_notification (#72260)

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -499,12 +499,6 @@ register("slack.client-secret", flags=FLAG_CREDENTIAL | FLAG_PRIORITIZE_DISK)
 # signing-secret is preferred, but need to keep verification-token for apps that use it
 register("slack.verification-token", flags=FLAG_CREDENTIAL | FLAG_PRIORITIZE_DISK)
 register("slack.signing-secret", flags=FLAG_CREDENTIAL | FLAG_PRIORITIZE_DISK)
-register(
-    "slack.sdk-web-client",
-    type=Sequence,
-    default=[],
-    flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
-)
 
 # Codecov Integration
 register("codecov.client-secret", flags=FLAG_CREDENTIAL | FLAG_PRIORITIZE_DISK)
