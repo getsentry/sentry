d349d893dc45950136a4d04fe02d04737d33d2fe
Athena Moghaddam <132939361+sentaur-athena@users.noreply.github.com>
2024-06-07 10:22:01 -0700

    feat(charcuterie): Add image to cache (#72134)
    Adding image to cache so the expensive computation of querying events
    happen only once for all kinds of notifications (slack, email, etc).
    
    [Tech
    spec](https://www.notion.so/sentry/Chartcuterie-Improvements-Technical-Design-Doc-3dd20eac5c9c494ab0c6bebd0d0a6dad?pvs=4#884676f39698490db944b04bfa39746b)

--- src/sentry/integrations/issue_alert_image_builder.py
+++ src/sentry/integrations/issue_alert_image_builder.py
@@ -1,6 +1,8 @@
 import logging
 
 import sentry_sdk
+from django.conf import settings
+from django.core.cache import cache
 
 from sentry.api import client
 from sentry.charts import backend as charts
@@ -18,9 +20,16 @@ from sentry.models.apikey import ApiKey
 from sentry.models.group import Group
 from sentry.snuba.referrer import Referrer
 from sentry.utils import metrics
-from sentry.utils.performance_issues.detectors.utils import escape_transaction
+from sentry.utils.locking import UnableToAcquireLock
+from sentry.utils.locking.backends import LockBackend
+from sentry.utils.locking.manager import LockManager
+from sentry.utils.performance_issues.detectors import utils
+from sentry.utils.services import build_instance_from_options_of_type
 
 logger = logging.getLogger("sentry.chartcuterie")
+locks = LockManager(
+    build_instance_from_options_of_type(LockBackend, settings.SENTRY_DEFAULT_LOCKS_BACKEND_OPTIONS)
+)
 
 
 class IssueAlertImageBuilder:
@@ -32,25 +41,46 @@ class IssueAlertImageBuilder:
             "provider": self.provider,
             "issue_category": self.group.issue_category,
         }
+        self.lock = locks.get(key=f"lock_{self.cache_key}", duration=10, name="issue_alert_image")
 
     def get_image_url(self) -> str | None:
         try:
-            image_url = None
             metrics.incr("chartcuterie.issue_alert.attempt", tags=self.tags)
-            if self.group.issue_type == PerformanceP95EndpointRegressionGroupType:
-                image_url = self._get_endpoint_regression_image_url()
-            elif self.group.issue_type == ProfileFunctionRegressionType:
-                image_url = self._get_function_regression_image_url()
-
-            if image_url:
-                metrics.incr("chartcuterie.issue_alert.success", tags=self.tags)
-                return image_url
+            image_url = cache.get(self.cache_key)
+            if image_url is None:
+                self.lock.blocking_acquire(initial_delay=1, timeout=30)
+                # Checking again in case another thread generated the image while
+                # this thread was acquiring the lock
+                image_url = cache.get(self.cache_key)
+                if image_url is None:
+                    if self.group.issue_type == PerformanceP95EndpointRegressionGroupType:
+                        image_url = self._get_endpoint_regression_image_url()
+                    elif self.group.issue_type == ProfileFunctionRegressionType:
+                        image_url = self._get_function_regression_image_url()
+                self.lock.release()
+        except UnableToAcquireLock:
+            # There is a chance that another thread generated the image
+            image_url = cache.get(self.cache_key)
+            if not image_url:
+                logger.warning(
+                    "issue_alert_chartcuterie_image.lock.failed",
+                    extra={"group_id": self.group.id},
+                )
         except Exception as e:
             logger.exception(
                 "issue_alert_chartcuterie_image.failed",
                 extra={"exception": e, "group_id": self.group.id},
             )
             sentry_sdk.capture_exception()
+            if self.lock.locked():
+                self.lock.release()
+
+        if image_url:
+            metrics.incr("chartcuterie.issue_alert.success", tags=self.tags)
+            # We don't want to regenerate the image if another type of notification is sending the same one
+            # For example slack notification and email notification for the same issue
+            cache.set(self.cache_key, image_url, timeout=60 * 5)
+            return image_url
         return None
 
     def _get_endpoint_regression_image_url(self) -> str | None:
@@ -58,7 +88,7 @@ class IssueAlertImageBuilder:
         event = self.group.get_latest_event_for_environments()
         if event is None or event.transaction is None or event.occurrence is None:
             return None
-        transaction_name = escape_transaction(event.transaction)
+        transaction_name = utils.escape_transaction(event.transaction)
         period = get_relative_time(anchor=get_approx_start_time(self.group), relative_days=14)
         resp = client.get(
             auth=ApiKey(organization_id=organization.id, scope_list=["org:read"]),
