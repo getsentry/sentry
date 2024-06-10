08cee0ed4e4037b8e150aad2578f64d71b24615d
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-06-10 09:37:39 -0400

    feat(insights): Remove "Beta" badges from Caches and Queues (#72316)
    We're ready for GA very soon, so the "Beta" badges are not needed.

--- static/app/views/performance/queues/settings.ts
+++ static/app/views/performance/queues/settings.ts
@@ -1,12 +1,9 @@
-import type {BadgeType} from 'sentry/components/badge/featureBadge';
 import {t} from 'sentry/locale';
 
 export const MODULE_TITLE = t('Queues');
 export const BASE_URL = 'queues';
 
 export const DESTINATION_TITLE = t('Destination Summary');
-
-export const RELEASE_LEVEL: BadgeType = 'beta';
 
 export const releaseLevelAsBadgeProps = {
   isNew: true,
