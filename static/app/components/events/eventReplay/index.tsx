8bac8b947b67857b7ed05e845f9384f3680e66d2
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-06-10 12:31:51 -0700

    ref(replay): Extract useEventCanShowReplayUpsell to re-use (#72442)
    Extracting useEventCanShowReplayUpsell, which also replaces
    `useHasOrganizationSentAnyReplayEvents`, so it can be re-used for the
    new (coming next) hydration-error-diff section inside the issue details
    page.
    
    Pulling this logic out makes
    `static/app/components/events/eventReplay/index.tsx` much simpler and
    easier to copy+paste in order to make a new component.
    
    Relates to https://github.com/getsentry/sentry/issues/70199

--- static/app/components/events/eventReplay/index.tsx
+++ static/app/components/events/eventReplay/index.tsx
@@ -3,14 +3,10 @@ import {lazy} from 'react';
 import ErrorBoundary from 'sentry/components/errorBoundary';
 import {ReplayClipSection} from 'sentry/components/events/eventReplay/replayClipSection';
 import LazyLoad from 'sentry/components/lazyLoad';
-import {replayBackendPlatforms} from 'sentry/data/platformCategories';
 import type {Event} from 'sentry/types/event';
 import type {Group} from 'sentry/types/group';
+import useEventCanShowReplayUpsell from 'sentry/utils/event/useEventCanShowReplayUpsell';
 import {getReplayIdFromEvent} from 'sentry/utils/replays/getReplayIdFromEvent';
-import {useHasOrganizationSentAnyReplayEvents} from 'sentry/utils/replays/hooks/useReplayOnboarding';
-import {projectCanUpsellReplay} from 'sentry/utils/replays/projectSupportsReplay';
-import useOrganization from 'sentry/utils/useOrganization';
-import useProjectFromSlug from 'sentry/utils/useProjectFromSlug';
 
 interface Props {
   event: Event;
@@ -21,36 +17,24 @@ interface Props {
 const ReplayOnboardingPanel = lazy(() => import('./replayInlineOnboardingPanel'));
 
 export default function EventReplay({event, group, projectSlug}: Props) {
-  const organization = useOrganization();
-  const hasReplaysFeature = organization.features.includes('session-replay');
-
-  const {hasOrgSentReplays, fetching: fetchingHasSentReplays} =
-    useHasOrganizationSentAnyReplayEvents();
-  const project = useProjectFromSlug({organization, projectSlug});
-
   const replayId = getReplayIdFromEvent(event);
-
-  if (!hasReplaysFeature || fetchingHasSentReplays) {
-    return null;
-  }
-
-  const platform = group?.project.platform ?? group?.platform ?? 'other';
-  const projectId = group?.project.id ?? event.projectID ?? '';
+  const {canShowUpsell, upsellPlatform, upsellProjectId} = useEventCanShowReplayUpsell({
+    event,
+    group,
+    projectSlug,
+  });
 
   if (replayId) {
     return <ReplayClipSection event={event} replayId={replayId} group={group} />;
   }
 
-  if (
-    projectCanUpsellReplay(project) &&
-    (!hasOrgSentReplays || replayBackendPlatforms.includes(platform))
-  ) {
+  if (canShowUpsell) {
     return (
       <ErrorBoundary mini>
         <LazyLoad
           LazyComponent={ReplayOnboardingPanel}
-          platform={platform}
-          projectId={projectId}
+          platform={upsellPlatform}
+          projectId={upsellProjectId}
         />
       </ErrorBoundary>
     );
