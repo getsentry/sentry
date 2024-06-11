831c057817349094e6ee94c6b6cb3977463892cf
Mark Story <mark@mark-story.com>
2024-06-11 18:17:11 -0400

    chore(hybridcloud) Remove unused option (#72249)
    This option was used to rollout a new cached rpc method. That rollout is
    complete and we don't need this option anymore.
    
    Refs #72040, #72334

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -1830,10 +1830,6 @@ register(
 
 # Break glass controls
 register("hybrid_cloud.rpc.disabled-service-methods", default=[], flags=FLAG_AUTOMATOR_MODIFIABLE)
-
-# RPC optimization
-register("sentryapps.get_by_application_id_cached", default=0.0, flags=FLAG_AUTOMATOR_MODIFIABLE)
-register("sentryapps.get_installation_cached", default=0.0, flags=FLAG_AUTOMATOR_MODIFIABLE)
 # == End hybrid cloud subsystem
 
 # Decides whether an incoming transaction triggers an update of the clustering rule applied to it.
