8f8a096b76c33731f51682fc60a56f9277f69c0b
Matt Duncan <matt.duncan@sentry.io>
2024-06-11 14:27:39 -0700

    chore(issues): maybe -> possible
    This aligns the section title phrasing better with the rest of the
    phrasing we use on the issue details page.

--- static/app/views/issueDetails/groupEventDetails/groupEventDetailsContent.tsx
+++ static/app/views/issueDetails/groupEventDetails/groupEventDetailsContent.tsx
@@ -55,7 +55,7 @@ import QuickTraceQuery from 'sentry/utils/performance/quickTrace/quickTraceQuery
 import {getReplayIdFromEvent} from 'sentry/utils/replays/getReplayIdFromEvent';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
-import {ResourcesAndMaybeSolutions} from 'sentry/views/issueDetails/resourcesAndMaybeSolutions';
+import {ResourcesAndPossibleSolutions} from 'sentry/views/issueDetails/resourcesAndPossibleSolutions';
 
 const LLMMonitoringSection = lazy(
   () => import('sentry/components/events/interfaces/llm-monitoring/llmMonitoringSection')
@@ -106,7 +106,7 @@ function DefaultGroupEventDetailsContent({
   const mechanism = event.tags?.find(({key}) => key === 'mechanism')?.value;
   const isANR = mechanism === 'ANR' || mechanism === 'AppExitInfo';
   const hasAnrImprovementsFeature = organization.features.includes('anr-improvements');
-  const showMaybeSolutionsHigher = shouldShowCustomErrorResourceConfig(group, project);
+  const showPossibleSolutionsHigher = shouldShowCustomErrorResourceConfig(group, project);
 
   const eventEntryProps = {group, event, project};
 
@@ -174,8 +174,8 @@ function DefaultGroupEventDetailsContent({
         project={project}
         viewAllRef={tagsRef}
       />
-      {showMaybeSolutionsHigher && (
-        <ResourcesAndMaybeSolutionsIssueDetailsContent
+      {showPossibleSolutionsHigher && (
+        <ResourcesAndPossibleSolutionsIssueDetailsContent
           event={event}
           project={project}
           group={group}
@@ -217,8 +217,8 @@ function DefaultGroupEventDetailsContent({
       <GroupEventEntry entryType={EntryType.EXPECTSTAPLE} {...eventEntryProps} />
       <GroupEventEntry entryType={EntryType.TEMPLATE} {...eventEntryProps} />
       <GroupEventEntry entryType={EntryType.BREADCRUMBS} {...eventEntryProps} />
-      {!showMaybeSolutionsHigher && (
-        <ResourcesAndMaybeSolutionsIssueDetailsContent
+      {!showPossibleSolutionsHigher && (
+        <ResourcesAndPossibleSolutionsIssueDetailsContent
           event={event}
           project={project}
           group={group}
@@ -259,14 +259,14 @@ function DefaultGroupEventDetailsContent({
   );
 }
 
-function ResourcesAndMaybeSolutionsIssueDetailsContent({
+function ResourcesAndPossibleSolutionsIssueDetailsContent({
   event,
   project,
   group,
 }: Required<GroupEventDetailsContentProps>) {
   return (
     <ErrorBoundary mini>
-      <ResourcesAndMaybeSolutions event={event} project={project} group={group} />
+      <ResourcesAndPossibleSolutions event={event} project={project} group={group} />
     </ErrorBoundary>
   );
 }
