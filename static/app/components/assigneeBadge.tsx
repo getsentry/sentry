0343c83afbe6ce0d9b651f4bb71daab89607c927
MichaelSun48 <michael.sun@sentry.io>
2024-06-11 14:39:34 -0700

    Change data-test-id on assignee badge

--- static/app/components/assigneeBadge.tsx
+++ static/app/components/assigneeBadge.tsx
@@ -81,6 +81,7 @@ export function AssigneeBadge({
     <Fragment>
       <Placeholder
         shape="circle"
+        data-test-id="unassigned-avatar"
         width={`${AVATAR_SIZE}px`}
         height={`${AVATAR_SIZE}px`}
       />
