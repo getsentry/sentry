f7f2e43b6c535872e814b603ecbaba2541452e10
Mark Story <mark@mark-story.com>
2024-06-11 15:41:45 -0400

    feat(hybridcloud) Add a system feature to combine hybridcloud features
    We have a few feature flags that all need to move in unison to make
    multi-region sentry work. I'd like to collapse these flags into a single
    system feature flag.
    
    The feature flags that will be combined are:
    
    - organizations:customer-domains
    - organizations:frontend-domainsplit
    - organizations:multi-region-selector
    
    The `system:multi-region` feature will be disabled by default, but
    enabled in saas, and getsentry dev.
    
    Refs HC-1096

--- src/sentry/features/permanent.py
+++ src/sentry/features/permanent.py
@@ -1,4 +1,9 @@
-from sentry.features.base import FeatureHandlerStrategy, OrganizationFeature, ProjectFeature
+from sentry.features.base import (
+    FeatureHandlerStrategy,
+    OrganizationFeature,
+    ProjectFeature,
+    SystemFeature,
+)
 from sentry.features.manager import FeatureManager
 
 # XXX: See `features/__init__.py` for documentation on how to use feature flags
@@ -127,3 +132,6 @@ def register_permanent_features(manager: FeatureManager):
         manager.add(
             project_feature, ProjectFeature, FeatureHandlerStrategy.INTERNAL, default=default
         )
+
+    # Enable support for multiple regions, and org slug subdomains.
+    manager.add("system:multi-region", SystemFeature, default=False)
