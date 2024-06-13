0e0949694d7277903e2634855546e627eea921db
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2024-06-13 10:49:39 -0400

    chore(insights): removes fid deprecation alert banner from webvitals module (#72685)
    Removes fid deprecation alert banner from webvitals module, since this
    banner has been active for 90+ days and no longer necessary.

--- static/app/views/performance/browser/webVitals/pageOverview.tsx
+++ static/app/views/performance/browser/webVitals/pageOverview.tsx
@@ -1,7 +1,6 @@
 import React, {useMemo, useState} from 'react';
 import styled from '@emotion/styled';
 import omit from 'lodash/omit';
-import moment from 'moment';
 
 import ProjectAvatar from 'sentry/components/avatar/projectAvatar';
 import {Breadcrumbs} from 'sentry/components/breadcrumbs';
@@ -10,30 +9,24 @@ import ButtonBar from 'sentry/components/buttonBar';
 import {AggregateSpans} from 'sentry/components/events/interfaces/spans/aggregateSpans';
 import FeedbackWidgetButton from 'sentry/components/feedback/widget/feedbackWidgetButton';
 import * as Layout from 'sentry/components/layouts/thirds';
-import ExternalLink from 'sentry/components/links/externalLink';
 import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import {EnvironmentPageFilter} from 'sentry/components/organizations/environmentPageFilter';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilter';
 import {TabList, Tabs} from 'sentry/components/tabs';
-import {IconChevron, IconClose} from 'sentry/icons';
-import {t, tct} from 'sentry/locale';
-import ConfigStore from 'sentry/stores/configStore';
+import {IconChevron} from 'sentry/icons';
+import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {defined} from 'sentry/utils';
 import {trackAnalytics} from 'sentry/utils/analytics';
 import {browserHistory} from 'sentry/utils/browserHistory';
 import {decodeScalar} from 'sentry/utils/queryString';
-import useDismissAlert from 'sentry/utils/useDismissAlert';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import useProjects from 'sentry/utils/useProjects';
 import useRouter from 'sentry/utils/useRouter';
 import {PageOverviewSidebar} from 'sentry/views/performance/browser/webVitals/components/pageOverviewSidebar';
-import {
-  FID_DEPRECATION_DATE,
-  PerformanceScoreBreakdownChart,
-} from 'sentry/views/performance/browser/webVitals/components/performanceScoreBreakdownChart';
+import {PerformanceScoreBreakdownChart} from 'sentry/views/performance/browser/webVitals/components/performanceScoreBreakdownChart';
 import WebVitalMeters from 'sentry/views/performance/browser/webVitals/components/webVitalMeters';
 import {PageOverviewWebVitalsDetailPanel} from 'sentry/views/performance/browser/webVitals/pageOverviewWebVitalsDetailPanel';
 import {PageSamplePerformanceTable} from 'sentry/views/performance/browser/webVitals/pageSamplePerformanceTable';
@@ -41,11 +34,6 @@ import {useProjectRawWebVitalsQuery} from 'sentry/views/performance/browser/webV
 import {calculatePerformanceScoreFromStoredTableDataRow} from 'sentry/views/performance/browser/webVitals/utils/queries/storedScoreQueries/calculatePerformanceScoreFromStored';
 import {useProjectWebVitalsScoresQuery} from 'sentry/views/performance/browser/webVitals/utils/queries/storedScoreQueries/useProjectWebVitalsScoresQuery';
 import type {WebVitals} from 'sentry/views/performance/browser/webVitals/utils/types';
-import {
-  AlertContent,
-  DismissButton,
-  StyledAlert,
-} from 'sentry/views/performance/browser/webVitals/webVitalsLandingPage';
 import {ModulePageProviders} from 'sentry/views/performance/modulePageProviders';
 import {useModuleBreadcrumbs} from 'sentry/views/performance/utils/useModuleBreadcrumbs';
 import {useModuleURL} from 'sentry/views/performance/utils/useModuleURL';
@@ -101,12 +89,6 @@ export function PageOverview() {
     webVital: (location.query.webVital as WebVitals) ?? null,
   });
 
-  const user = ConfigStore.get('user');
-
-  const {dismiss, isDismissed} = useDismissAlert({
-    key: `${organization.slug}-${user.id}:fid-deprecation-message-dismissed`,
-  });
-
   const crumbs = useModuleBreadcrumbs('vital');
 
   const query = decodeScalar(location.query.query);
@@ -136,9 +118,6 @@ export function PageOverview() {
     isProjectScoresLoading || isLoading
       ? undefined
       : calculatePerformanceScoreFromStoredTableDataRow(projectScores?.data?.[0]);
-
-  const fidDeprecationTimestampString =
-    moment(FID_DEPRECATION_DATE).format('DD MMMM YYYY');
 
   return (
     <React.Fragment>
@@ -219,41 +198,6 @@ export function PageOverview() {
                   <DatePageFilter />
                 </PageFilterBar>
               </TopMenuContainer>
-              {!isDismissed && (
-                <StyledAlert type="info" showIcon>
-                  <AlertContent>
-                    <span>
-                      {tct(
-                        `Starting on [fidDeprecationTimestampString], [inpStrong:INP] (Interaction to Next Paint) will replace [fidStrong:FID] (First Input Delay) in our performance score calculation.`,
-                        {
-                          fidDeprecationTimestampString,
-                          inpStrong: <strong />,
-                          fidStrong: <strong />,
-                        }
-                      )}
-                      <br />
-                      {tct(
-                        `Users should update their Sentry SDKs to the [link:latest version (7.104.0+)] and [enableInp:enable the INP option] to start receiving updated Performance Scores.`,
-                        {
-                          link: (
-                            <ExternalLink href="https://github.com/getsentry/sentry-javascript/releases/tag/7.104.0" />
-                          ),
-                          enableInp: (
-                            <ExternalLink href="https://docs.sentry.io/platforms/javascript/performance/instrumentation/automatic-instrumentation/#enableinp" />
-                          ),
-                        }
-                      )}
-                    </span>
-                    <DismissButton
-                      priority="link"
-                      icon={<IconClose />}
-                      onClick={dismiss}
-                      aria-label={t('Dismiss Alert')}
-                      title={t('Dismiss Alert')}
-                    />
-                  </AlertContent>
-                </StyledAlert>
-              )}
               <Flex>
                 <PerformanceScoreBreakdownChart transaction={transaction} />
               </Flex>
