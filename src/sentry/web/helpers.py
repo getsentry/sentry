fb56d0e2ecf9ea7cf982e7f6922bf72957173032
schew2381 <seiji.chew@sentry.io>
2024-06-13 15:38:50 -0700

    split type fix into other PR

--- src/sentry/web/helpers.py
+++ src/sentry/web/helpers.py
@@ -33,7 +33,7 @@ def render_to_string(
 
 
 def render_to_response(
-    template: Sequence[str] | str,
+    template: str,
     context: Mapping[str, Any] | None = None,
     request: HttpRequest | None = None,
     status: int = 200,
