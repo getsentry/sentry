f5c1a7c1777843cb2f5fd70cbe78a80509f4dad6
Raj Joshi <raj.joshi@sentry.io>
2024-06-14 10:37:38 -0700

    refactored link identity

--- src/sentry/integrations/slack/views/link_identity.py
+++ src/sentry/integrations/slack/views/link_identity.py
@@ -1,7 +1,10 @@
+import logging
+
 from django.core.signing import BadSignature, SignatureExpired
-from django.http import HttpResponse
+from django.db import IntegrityError
+from django.http import Http404, HttpRequest, HttpResponse
+from django.http.response import HttpResponseBase
 from django.utils.decorators import method_decorator
-from rest_framework.request import Request
 
 from sentry.integrations.types import ExternalProviderEnum, ExternalProviders
 from sentry.integrations.utils import get_identity_or_404
@@ -9,6 +12,7 @@ from sentry.models.identity import Identity
 from sentry.notifications.notificationcontroller import NotificationController
 from sentry.notifications.notifications.integration_nudge import IntegrationNudgeNotification
 from sentry.services.hybrid_cloud.integration.model import RpcIntegration
+from sentry.utils import metrics
 from sentry.utils.signing import unsign
 from sentry.web.frontend.base import BaseView, control_silo_view
 from sentry.web.helpers import render_to_response
@@ -16,6 +20,8 @@ from sentry.web.helpers import render_to_response
 from ..utils import send_slack_response
 from . import build_linking_url as base_build_linking_url
 from . import never_cache
+
+_logger = logging.getLogger(__name__)
 
 SUCCESS_LINKED_MESSAGE = (
     "Your Slack identity has been linked to your Sentry account. You're good to go!"
@@ -40,33 +46,72 @@ class SlackLinkIdentityView(BaseView):
     Django view for linking user to slack account. Creates an entry on Identity table.
     """
 
+    _METRICS_SUCCESS_KEY = "sentry.integrations.slack.link_identity_view.success"
+    _METRICS_FAILURE_KEY = "sentry.integrations.slack.link_identity_view.failure"
+
     @method_decorator(never_cache)
-    def handle(self, request: Request, signed_params: str) -> HttpResponse:
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponseBase:
         try:
+            signed_params = kwargs.pop("signed_params")
             params = unsign(signed_params)
-        except (SignatureExpired, BadSignature):
+        except (SignatureExpired, BadSignature) as e:
+            _logger.warning("dispatch.signature_error", exc_info=e)
+            metrics.incr(self._METRICS_FAILURE_KEY, tags={"error": str(e)}, sample_rate=1.0)
             return render_to_response(
                 "sentry/integrations/slack/expired-link.html",
                 request=request,
             )
 
-        organization, integration, idp = get_identity_or_404(
-            ExternalProviders.SLACK,
-            request.user,
-            integration_id=params["integration_id"],
+        try:
+            organization, integration, idp = get_identity_or_404(
+                ExternalProviders.SLACK,
+                request.user,
+                integration_id=params["integration_id"],
+            )
+        except Http404:
+            _logger.exception(
+                "get_identity_error", extra={"integration_id": params["integration_id"]}
+            )
+            metrics.incr(self._METRICS_FAILURE_KEY + ".get_identity", sample_rate=1.0)
+            raise
+
+        _logger.info("get_identity_success", extra={"integration_id": params["integration_id"]})
+        metrics.incr(self._METRICS_SUCCESS_KEY + ".get_identity", sample_rate=1.0)
+        params.update({"organization": organization, "integration": integration, "idp": idp})
+        return super().dispatch(request, params=params)
+
+    def get(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        params = kwargs["params"]
+        organization, integration = params["organization"], params["integration"]
+
+        return render_to_response(
+            "sentry/auth-link-identity.html",
+            request=request,
+            context={"organization": organization, "provider": integration.get_provider()},
         )
 
-        if request.method != "POST":
-            return render_to_response(
-                "sentry/auth-link-identity.html",
-                request=request,
-                context={"organization": organization, "provider": integration.get_provider()},
-            )
+    def post(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
+        params = kwargs["params"]
+        organization, integration, idp = (
+            params["organization"],
+            params["integration"],
+            params["idp"],
+        )
 
-        Identity.objects.link_identity(user=request.user, idp=idp, external_id=params["slack_id"])
+        try:
+            Identity.objects.link_identity(
+                user=request.user, idp=idp, external_id=params["slack_id"]
+            )
+        except IntegrityError:
+            _logger.exception("slack.link.integrity_error")
+            metrics.incr(
+                self._METRICS_FAILURE_KEYKEY + ".post.identity.integrity_error",
+                sample_rate=1.0,
+            )
+            raise Http404
 
         send_slack_response(integration, SUCCESS_LINKED_MESSAGE, params, command="link")
-        has_slack_settings = None
+
         controller = NotificationController(
             recipients=[request.user],
             organization_id=organization.id,
@@ -77,8 +122,9 @@ class SlackLinkIdentityView(BaseView):
         if not has_slack_settings:
             IntegrationNudgeNotification(organization, request.user, ExternalProviders.SLACK).send()
 
-        # TODO(epurkhiser): We could do some fancy slack querying here to
-        #  render a nice linking page with info about the user their linking.
+        _logger.info("link_identity_success", extra={"slack_id": params["slack_id"]})
+        metrics.incr(self._METRICS_SUCCESS_KEY + ".post.link_identity", sample_rate=1.0)
+
         return render_to_response(
             "sentry/integrations/slack/linked.html",
             request=request,
