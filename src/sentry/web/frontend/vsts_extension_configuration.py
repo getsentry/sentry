a5182b799d0bbead309220a7b795454ff0b3d576
mdtro <20070360+mdtro@users.noreply.github.com>
2024-06-17 10:18:41 -0500

    fix(vsts): verify targetName matches expected pattern

--- src/sentry/web/frontend/vsts_extension_configuration.py
+++ src/sentry/web/frontend/vsts_extension_configuration.py
@@ -1,3 +1,7 @@
+import re
+
+from django.core.exceptions import BadRequest
+
 from sentry.web.frontend.base import control_silo_view
 
 from .integration_extension_configuration import IntegrationExtensionConfigurationView
@@ -8,5 +12,25 @@ class VstsExtensionConfigurationView(IntegrationExtensionConfigurationView):
     provider = "vsts"
     external_provider_key = "vsts-extension"
 
+    def __is_validate_account_name(self, account_name: str) -> bool:
+        """Validates the Azure DevOps account name
+
+        https://learn.microsoft.com/en-us/azure/devops/organizations/accounts/rename-organization?view=azure-devops#rename-your-organization
+
+        > Adhere to the following guidelines when you create an organization name.
+        >
+        > Use only letters from the English alphabet
+        > Start your organization name with a letter or number
+        > Use letters, numbers, or hyphens after the initial character
+        > Ensure that your organization doesn't exceed 50 Unicode characters
+        > End with a letter or number
+        """
+        pattern = r"^[A-Za-z0-9][A-Za-z0-9-]{0,48}[A-Za-z0-9]$"
+
+        return bool(re.match(pattern, account_name))
+
     def map_params_to_state(self, params):
+        if not self.__is_validate_account_name(account_name=params["targetName"]):
+            raise BadRequest("invalid targetName")
+
         return {"accountId": params["targetId"], "accountName": params["targetName"]}
