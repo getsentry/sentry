50c8b6e87f5a498bda0100c671117a20a8d863d3
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-06-17 12:17:29 -0700

    fix(replay): fix some ui jumping between mobile/web (#72825)
    this PR fixes some of the UI 'jumping' when we realize a mobile replay
    is a mobile replay (and not a web replay).
    
    namely, this adds two loading placeholders which hides the following
    discrepancies while we're waiting for our `isVideoReplay` check to
    evaluate:
    - web "contact us" button vs. mobile "give feedback" button in top right
    corner
    - no rage/dead clicks in header for mobile replays
    - configure replay button in top right goes away for mobile replays
    
    also updated the header placeholder style so they're consistent, and
    less jarring white.
    
    before: 
    
    
    https://github.com/getsentry/sentry/assets/56095982/094bfea3-0d01-46f3-87d2-c93395c25dbb
    
    
    after (mobile) - testing on `dev-ui` so the 'give feedback' button
    doesn't show up but it will on prod:
    
    
    
    
    https://github.com/getsentry/sentry/assets/56095982/1d4e7c70-42f1-4189-bffd-583848d6c43e
    
    
    
    
    
    
    after (web):
    
    
    
    
    https://github.com/getsentry/sentry/assets/56095982/50c8184c-fe15-4899-af98-a464e41fb553
    
    
    
    it's not perfect and still looks slightly janky (probably because the
    dimensions aren't exact - they were chosen by me arbitrarily and can
    change; open to suggestions). also because the 'seen by' avatar bit
    loads in at a different time 🫠
    
    the logic i'm using to check for the loading state is based on whether
    the rrweb events have all loaded in, because for normal replays (both
    mobile/web) there are at least 2 rrweb events - otherwise there was some
    sort of processing error.
    
    will follow up with changes to fix the rest of the discrepancies; wanted
    to get feedback on this new logic/loading state first.
    
    relates to https://github.com/getsentry/sentry/issues/68109

--- static/app/components/replays/header/headerPlaceholder.tsx
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled';
-
-import Placeholder from 'sentry/components/placeholder';
-
-const HeaderPlaceholder = styled(Placeholder)`
-  background-color: ${p => p.theme.background};
-`;
-
-export default HeaderPlaceholder;
--- static/app/components/replays/header/replayMetaData.tsx
+++ static/app/components/replays/header/replayMetaData.tsx
@@ -2,8 +2,8 @@ import {Fragment} from 'react';
 import styled from '@emotion/styled';
 
 import Link from 'sentry/components/links/link';
+import Placeholder from 'sentry/components/placeholder';
 import ErrorCounts from 'sentry/components/replays/header/errorCounts';
-import HeaderPlaceholder from 'sentry/components/replays/header/headerPlaceholder';
 import ReplayViewers from 'sentry/components/replays/header/replayViewers';
 import {IconCursorArrow} from 'sentry/icons';
 import {t} from 'sentry/locale';
@@ -18,10 +18,16 @@ import type {ReplayError, ReplayRecord} from 'sentry/views/replays/types';
 type Props = {
   replayErrors: ReplayError[];
   replayRecord: ReplayRecord | undefined;
+  isLoading?: boolean;
   showDeadRageClicks?: boolean;
 };
 
-function ReplayMetaData({replayErrors, replayRecord, showDeadRageClicks = true}: Props) {
+function ReplayMetaData({
+  replayErrors,
+  replayRecord,
+  showDeadRageClicks = true,
+  isLoading,
+}: Props) {
   const location = useLocation();
   const routes = useRoutes();
   const referrer = getRouteStringFromRoutes(routes);
@@ -37,7 +43,9 @@ function ReplayMetaData({replayErrors, replayRecord, showDeadRageClicks = true}:
     },
   };
 
-  return (
+  return isLoading ? (
+    <Placeholder height="47px" width="203px" />
+  ) : (
     <KeyMetrics>
       {showDeadRageClicks && (
         <Fragment>
@@ -80,7 +88,7 @@ function ReplayMetaData({replayErrors, replayRecord, showDeadRageClicks = true}:
         {replayRecord ? (
           <ErrorCounts replayErrors={replayErrors} replayRecord={replayRecord} />
         ) : (
-          <HeaderPlaceholder width="20px" height="16px" />
+          <Placeholder width="20px" height="16px" />
         )}
       </KeyMetricData>
       <KeyMetricLabel>{t('Seen By')}</KeyMetricLabel>
@@ -88,7 +96,7 @@ function ReplayMetaData({replayErrors, replayRecord, showDeadRageClicks = true}:
         {replayRecord ? (
           <ReplayViewers projectId={replayRecord.project_id} replayId={replayRecord.id} />
         ) : (
-          <HeaderPlaceholder width="55px" height="27px" />
+          <Placeholder width="55px" height="27px" />
         )}
       </KeyMetricData>
     </KeyMetrics>
