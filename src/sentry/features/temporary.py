3bd3826ef3e84dd03e5660d4f3bf3f369f39e6a4
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-06-16 21:02:31 -0700

    chore(replay): option backed ff for materialized view queries (#72796)
    To be used in https://github.com/getsentry/sentry/pull/68658

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -358,6 +358,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:session-replay-hydration-error-issue-creation", OrganizationFeature, FeatureHandlerStrategy.OPTIONS)
     # Enable linking from 'new issue' email notifs to the issue replay list
     manager.add("organizations:session-replay-issue-emails", OrganizationFeature, FeatureHandlerStrategy.INTERNAL)
+    # Enable queries to materialized view from replay index endpoint
+    manager.add("organizations:session-replay-materialized-view", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, default=False)
     # Enable mobile replay player
     manager.add("organizations:session-replay-mobile-player", OrganizationFeature, FeatureHandlerStrategy.REMOTE)
     # Enable mobile replay player network tab
