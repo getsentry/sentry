f825093f5f24332ac607fedf9e144be76fad57ec
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-06-17 15:39:40 -0400

    Breadcrumbs

--- static/app/views/performance/utils/useInsightsTitle.tsx
+++ static/app/views/performance/utils/useInsightsTitle.tsx
@@ -1,10 +1,5 @@
 import {t} from 'sentry/locale';
-import useOrganization from 'sentry/utils/useOrganization';
 
 export function useInsightsTitle() {
-  const organization = useOrganization();
-
-  return organization?.features?.includes('performance-insights')
-    ? t('Insights')
-    : t('Performance');
+  return t('Insights');
 }
