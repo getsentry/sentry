fabe1eb05189b5e04bf6c36e914fd47fd25eeaf9
Dan Fuller <dfuller@sentry.io>
2024-06-18 15:15:15 -0700

    This model doesn't need to be tied to a specific project, that can be controlled by the system that creates the subscription
    Fix unique

--- src/sentry/remote_subscriptions/models.py
+++ src/sentry/remote_subscriptions/models.py
@@ -5,7 +5,7 @@ from typing import ClassVar, Self
 from django.db import models
 
 from sentry.backup.scopes import RelocationScope
-from sentry.db.models import BaseManager, DefaultFieldsModel, FlexibleForeignKey, region_silo_model
+from sentry.db.models import BaseManager, DefaultFieldsModel, region_silo_model
 
 
 @region_silo_model
@@ -19,17 +19,21 @@ class RemoteSubscription(DefaultFieldsModel):
         DELETING = 3
         DISABLED = 4
 
-    project = FlexibleForeignKey("sentry.Project")
     # Text identifier for the subscription type this is. Used to identify the registered callback associated with this
     # subscription.
     type = models.TextField()
     status = models.SmallIntegerField(default=Status.ACTIVE.value, db_index=True)
-    subscription_id = models.TextField(unique=True, null=True)
+    subscription_id = models.TextField(null=True)
 
     objects: ClassVar[BaseManager[Self]] = BaseManager(
-        cache_fields=("pk", "subscription_id"), cache_ttl=int(timedelta(hours=1).total_seconds())
+        cache_fields=["pk"], cache_ttl=int(timedelta(hours=1).total_seconds())
     )
 
     class Meta:
         app_label = "sentry"
         db_table = "sentry_remotesubscription"
+        constraints = [
+            models.UniqueConstraint(
+                fields=["subscription_id", "type"], name="unique_type_subscription_id"
+            )
+        ]
