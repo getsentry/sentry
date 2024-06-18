131d1d8a1c034f3cf11362a380ea457ed4e04a87
Benjamin E. Coe <benjamin.coe@sentry.io>
2024-06-18 15:29:15 -0400

    fix(insights): updated empty-state copy for caches / queues (#72905)
    Better describe the benefits of using the cache / queue insights.

--- static/app/views/performance/cache/cacheLandingPage.spec.tsx
+++ static/app/views/performance/cache/cacheLandingPage.spec.tsx
@@ -239,7 +239,7 @@ describe('CacheLandingPage', function () {
     await waitForElementToBeRemoved(() => screen.queryAllByTestId('loading-indicator'));
 
     expect(
-      screen.getByText('Start collecting Insights about your Caches!')
+      screen.getByText('Make sure your application’s caching is behaving properly')
     ).toBeInTheDocument();
   });
 });
