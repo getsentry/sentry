cf227ba69b1036b5a5b6ff2de1dac5bfa3f7ff76
Mark Story <mark@mark-story.com>
2024-06-18 14:53:03 -0400

    chore(hybridcloud) Remove the customer-domains feature
    With all checks no combined with multi-region the customer-domains
    feature flag is redundant.
    
    Refs HC-1096

--- tests/sentry/web/frontend/test_react_page.py
+++ tests/sentry/web/frontend/test_react_page.py
@@ -1,6 +1,5 @@
 from fnmatch import fnmatch
 
-from django.test import override_settings
 from django.urls import URLResolver, get_resolver, reverse
 
 from sentry.models.organization import OrganizationStatus
@@ -266,10 +265,7 @@ class ReactPageViewTest(TestCase):
         other_org = self.create_organization()
 
         self.login_as(self.user)
-        with (
-            override_settings(SENTRY_USE_CUSTOMER_DOMAINS=True),
-            self.feature({"system:multi-region": True}),
-        ):
+        with self.feature({"system:multi-region": True}):
             # Should not be able to induce activeorg
             assert "activeorg" not in self.client.session
             response = self.client.get(
@@ -287,10 +283,7 @@ class ReactPageViewTest(TestCase):
         other_org = self.create_organization()
 
         self.login_as(user, superuser=is_superuser, staff=is_staff)
-        with (
-            override_settings(SENTRY_USE_CUSTOMER_DOMAINS=True),
-            self.feature({"system:multi-region": True}),
-        ):
+        with self.feature({"system:multi-region": True}):
             # Induce activeorg
             assert "activeorg" not in self.client.session
             response = self.client.get(
