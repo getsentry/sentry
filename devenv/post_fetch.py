7afc919e2c84a2c16d14d8a98dd1af85a6171518
Joshua Li <joshuarli98@gmail.com>
2024-06-19 12:48:53 -0700

    rename

--- devenv/post_fetch.py
+++ devenv/post_fetch.py
@@ -4,7 +4,7 @@ import configparser
 
 
 def main(context: dict[str, str]) -> int:
-    # post_bootstrap is meant for recommended but not required defaults
+    # post_fetch is meant for recommended but not required defaults
     reporoot = context["reporoot"]
 
     git_config = configparser.ConfigParser()
