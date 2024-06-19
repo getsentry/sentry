b827f7686a24de8cfc44a30608d4a69fcfb3a870
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-06-19 14:45:14 +0200

    test ref

--- tests/sentry/snuba/metrics/test_span_attribute_extraction.py
+++ tests/sentry/snuba/metrics/test_span_attribute_extraction.py
@@ -1,42 +1,41 @@
 from sentry.sentry_metrics.extraction_rules import MetricsExtractionRule
 from sentry.snuba.metrics.span_attribute_extraction import convert_to_spec
 
-expected_spec = {
-    "category": "span",
-    "mri": "d:custom/span.duration@millisecond",
-    "field": "span.duration",
-    "tags": [
-        {"key": "region", "field": "span.data.region"},
-        {"key": "http.status_code", "field": "span.data.http\\.status_code"},
-        {"key": "foo", "field": "span.data.foo"},
-        {"key": "user_segment", "field": "span.data.user_segment"},
-    ],
-    "condition": {
-        "op": "or",
-        "inner": [
-            {
-                "op": "and",
-                "inner": [
-                    {"op": "eq", "name": "span.data.region", "value": ["us", "de"]},
-                    {"op": "eq", "name": "span.data.user_segment", "value": "vip"},
-                ],
-            },
-            {"op": "eq", "name": "span.data.foo", "value": "bar"},
-        ],
-    },
-}
-
-_input = MetricsExtractionRule(
-    span_attribute="span.duration",
-    type="d",
-    unit="millisecond",
-    tags={"region", "http.status_code"},
-    conditions=["region:[us, de] user_segment:vip", "foo:bar"],
-)
-
 
 def test_convert_to_spec():
-    metric_spec = convert_to_spec(_input)
+    rule = MetricsExtractionRule(
+        span_attribute="span.duration",
+        type="d",
+        unit="millisecond",
+        tags={"region", "http.status_code"},
+        conditions=["region:[us, de] user_segment:vip", "foo:bar"],
+    )
+    metric_spec = convert_to_spec(rule)
+
+    expected_spec = {
+        "category": "span",
+        "mri": "d:custom/span.duration@millisecond",
+        "field": "span.duration",
+        "tags": [
+            {"key": "region", "field": "span.data.region"},
+            {"key": "http.status_code", "field": "span.data.http\\.status_code"},
+            {"key": "foo", "field": "span.data.foo"},
+            {"key": "user_segment", "field": "span.data.user_segment"},
+        ],
+        "condition": {
+            "op": "or",
+            "inner": [
+                {
+                    "op": "and",
+                    "inner": [
+                        {"op": "eq", "name": "span.data.region", "value": ["us", "de"]},
+                        {"op": "eq", "name": "span.data.user_segment", "value": "vip"},
+                    ],
+                },
+                {"op": "eq", "name": "span.data.foo", "value": "bar"},
+            ],
+        },
+    }
 
     assert metric_spec["category"] == expected_spec["category"]
     assert metric_spec["mri"] == expected_spec["mri"]
