ccedeed33b3075084953b2eb89b1e222229c44e2
Dan Fuller <dfuller@sentry.io>
2024-06-20 13:39:57 -0700

    fix migrations

--- src/sentry/uptime/models.py
+++ src/sentry/uptime/models.py
@@ -14,31 +14,6 @@ from sentry.db.models.manager.base import BaseManager
 
 
 @region_silo_model
-class ProjectUptimeSubscription(DefaultFieldsModel):
-    # TODO: This should be included in export/import, but right now it has no relation to
-    # any projects/orgs. Will fix this in a later pr
-    __relocation_scope__ = RelocationScope.Excluded
-
-    project_id = BoundedBigIntegerField(db_index=True)
-    uptime_subscription = FlexibleForeignKey("uptime.UptimeSubscription")
-
-    objects: ClassVar[BaseManager[Self]] = BaseManager(
-        cache_fields=["pk"], cache_ttl=int(timedelta(hours=1).total_seconds())
-    )
-
-    class Meta:
-        app_label = "uptime"
-        db_table = "uptime_projectuptimesubscription"
-
-        constraints = [
-            models.UniqueConstraint(
-                fields=["project_id", "uptime_subscription"],
-                name="uptime_projectuptimesubscription_unique_project_subscription",
-            ),
-        ]
-
-
-@region_silo_model
 class UptimeSubscription(DefaultFieldsModel):
     # TODO: This should be included in export/import, but right now it has no relation to
     # any projects/orgs. Will fix this in a later pr
@@ -65,5 +40,30 @@ class UptimeSubscription(DefaultFieldsModel):
             models.UniqueConstraint(
                 fields=["url", "interval_seconds"],
                 name="uptime_uptimesubscription_unique_url_check",
+            ),
+        ]
+
+
+@region_silo_model
+class ProjectUptimeSubscription(DefaultFieldsModel):
+    # TODO: This should be included in export/import, but right now it has no relation to
+    # any projects/orgs. Will fix this in a later pr
+    __relocation_scope__ = RelocationScope.Excluded
+
+    project_id = BoundedBigIntegerField(db_index=True)
+    uptime_subscription = FlexibleForeignKey("uptime.UptimeSubscription")
+
+    objects: ClassVar[BaseManager[Self]] = BaseManager(
+        cache_fields=["pk"], cache_ttl=int(timedelta(hours=1).total_seconds())
+    )
+
+    class Meta:
+        app_label = "uptime"
+        db_table = "uptime_projectuptimesubscription"
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["project_id", "uptime_subscription"],
+                name="uptime_projectuptimesubscription_unique_project_subscription",
             ),
         ]
