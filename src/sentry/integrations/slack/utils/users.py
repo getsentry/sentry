3a5113628fc2124b8b9c3df70a01e0bbc38edd4e
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2024-06-20 09:46:43 -0700

    ref(slack): use sdk client to get users with pagination (#72871)

--- src/sentry/integrations/slack/utils/users.py
+++ src/sentry/integrations/slack/utils/users.py
@@ -1,9 +1,13 @@
 from __future__ import annotations
 
-from collections.abc import Mapping, MutableMapping, Sequence
+from collections.abc import Iterable, Mapping, MutableMapping, Sequence
+from dataclasses import dataclass
 from typing import Any
 
+from slack_sdk.errors import SlackApiError
+
 from sentry.integrations.slack.client import SlackClient
+from sentry.integrations.slack.sdk_client import SlackSdkClient
 from sentry.models.integrations.integration import Integration
 from sentry.models.organization import Organization
 from sentry.models.user import User
@@ -17,12 +21,20 @@ SLACK_GET_USERS_PAGE_LIMIT = 100
 SLACK_GET_USERS_PAGE_SIZE = 200
 
 
-def get_users(integration: Integration, organization: Organization) -> Sequence[Mapping[str, Any]]:
-    client = SlackClient(integration_id=integration.id)
+@dataclass(frozen=True)
+class SlackUserData:
+    email: str
+    team_id: str
+    slack_id: str
 
+
+def get_users(integration: Integration, organization: Organization) -> Sequence[Mapping[str, Any]]:
     user_list = []
     next_cursor = None
-    for i in range(SLACK_GET_USERS_PAGE_LIMIT):
+
+    client = SlackClient(integration_id=integration.id)
+
+    for _ in range(SLACK_GET_USERS_PAGE_LIMIT):
         try:
             next_users = client.get(
                 "/users.list",
@@ -49,13 +61,11 @@ def get_users(integration: Integration, organization: Organization) -> Sequence[
 
 def get_slack_info_by_email(
     integration: Integration, organization: Organization
-) -> Mapping[str, Mapping[str, str]]:
+) -> Mapping[str, SlackUserData]:
     return {
-        member["profile"]["email"]: {
-            "email": member["profile"]["email"],
-            "team_id": member["team_id"],
-            "slack_id": member["id"],
-        }
+        member["profile"]["email"]: SlackUserData(
+            email=member["profile"]["email"], team_id=member["team_id"], slack_id=member["id"]
+        )
         for member in get_users(integration, organization)
         if not member["deleted"] and member["profile"].get("email")
     }
@@ -65,12 +75,63 @@ def get_slack_data_by_user(
     integration: Integration | RpcIntegration,
     organization: Organization | RpcOrganization,
     emails_by_user: Mapping[User, Sequence[str]],
-) -> Mapping[User, Mapping[str, str]]:
+) -> Mapping[User, SlackUserData]:
     slack_info_by_email = get_slack_info_by_email(integration, organization)
-    slack_data_by_user: MutableMapping[User, Mapping[str, str]] = {}
+    slack_data_by_user: MutableMapping[User, SlackUserData] = {}
     for user, emails in emails_by_user.items():
         for email in emails:
             if email in slack_info_by_email:
                 slack_data_by_user[user] = slack_info_by_email[email]
                 break
     return slack_data_by_user
+
+
+# USING SDK CLIENT
+
+
+def format_slack_data_by_user(
+    emails_by_user: Mapping[User, Sequence[str]], slack_info_by_email: dict[str, SlackUserData]
+) -> Mapping[User, SlackUserData]:
+    slack_data_by_user: MutableMapping[User, SlackUserData] = {}
+    for user, emails in emails_by_user.items():
+        # get overlap between user emails and emails in slack
+        user_slack_emails = set(emails) & set(slack_info_by_email.keys())
+        if user_slack_emails:
+            slack_data_by_user[user] = slack_info_by_email[list(user_slack_emails)[0]]
+    return slack_data_by_user
+
+
+def format_slack_info_by_email(users: dict[str, Any]) -> dict[str, SlackUserData]:
+    return {
+        member["profile"]["email"]: SlackUserData(
+            email=member["profile"]["email"], team_id=member["team_id"], slack_id=member["id"]
+        )
+        for member in users
+        if not member["deleted"] and member["profile"].get("email")
+    }
+
+
+def get_slack_data_by_user_via_sdk(
+    integration: Integration | RpcIntegration,
+    organization: Organization | RpcOrganization,
+    emails_by_user: Mapping[User, Sequence[str]],
+) -> Iterable[Mapping[User, SlackUserData]]:
+    sdk_client = SlackSdkClient(integration_id=integration.id)
+    try:
+        users_list = sdk_client.users_list(limit=SLACK_GET_USERS_PAGE_SIZE)
+
+        for page in users_list:
+            users: dict[str, Any] = page.get("members")
+            slack_info_by_email = format_slack_info_by_email(users)
+            slack_data_by_user = format_slack_data_by_user(emails_by_user, slack_info_by_email)
+
+            yield slack_data_by_user
+    except SlackApiError as e:
+        logger.info(
+            "slack.post_install.get_users.error",
+            extra={
+                "error": str(e),
+                "organization": organization.slug,
+                "integration_id": integration.id,
+            },
+        )
--- src/sentry/tasks/integrations/slack/link_slack_user_identities.py
+++ src/sentry/tasks/integrations/slack/link_slack_user_identities.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 import logging
+from collections.abc import Mapping
 
 from django.utils import timezone
 
+from sentry import features
 from sentry.integrations.slack.utils import get_slack_data_by_user
+from sentry.integrations.slack.utils.users import SlackUserData, get_slack_data_by_user_via_sdk
 from sentry.integrations.utils import get_identities_by_user
 from sentry.models.identity import Identity, IdentityProvider, IdentityStatus
+from sentry.models.user import User
 from sentry.models.useremail import UserEmail
 from sentry.services.hybrid_cloud.integration import integration_service
 from sentry.services.hybrid_cloud.organization import organization_service
@@ -32,26 +36,45 @@ def link_slack_user_identities(
     assert organization is not None and integration is not None
 
     emails_by_user = UserEmail.objects.get_emails_by_user(organization=organization)
-    slack_data_by_user = get_slack_data_by_user(integration, organization, emails_by_user)
-
     idp = IdentityProvider.objects.get(
         type=integration.provider,
         external_id=integration.external_id,
     )
+
+    if not features.has("organizations:slack-sdk-get-users", organization):
+        slack_data_by_user = get_slack_data_by_user(integration, organization, emails_by_user)
+        update_identities(slack_data_by_user, idp)
+        return None
+
+    logger.info(
+        "slack.post_install.link_identities.start",
+        extra={
+            "organization": organization.slug,
+            "integration_id": integration.id,
+        },
+    )
+    slack_data_by_user = get_slack_data_by_user_via_sdk(integration, organization, emails_by_user)
+    for data in slack_data_by_user:
+        update_identities(data, idp)
+
+
+# has different typing for slack_data_by_user, need to grab slack_id from a dataclass
+def update_identities(slack_data_by_user: Mapping[User, SlackUserData], idp: IdentityProvider):
     date_verified = timezone.now()
     identities_by_user = get_identities_by_user(idp, slack_data_by_user.keys())
 
     for user, data in slack_data_by_user.items():
+        slack_id = data.slack_id
         # Identity already exists, the emails match, AND the external ID has changed
         if user in identities_by_user.keys():
-            if data["slack_id"] != identities_by_user[user].external_id:
+            if slack_id != identities_by_user[user].external_id:
                 # replace the Identity's external_id with the new one we just got from Slack
-                identities_by_user[user].update(external_id=data["slack_id"])
+                identities_by_user[user].update(external_id=data.slack_id)
             continue
         # the user doesn't already have an Identity and one of their Sentry emails matches their Slack email
         matched_identity, created = Identity.objects.get_or_create(
             idp=idp,
-            external_id=data["slack_id"],
+            external_id=slack_id,
             defaults={"user": user, "status": IdentityStatus.VALID, "date_verified": date_verified},
         )
         # the Identity matching that idp/external_id combo is linked to a different user
@@ -60,7 +83,7 @@ def link_slack_user_identities(
                 "post_install.identity_linked_different_user",
                 extra={
                     "idp_id": idp.id,
-                    "external_id": data["slack_id"],
+                    "external_id": slack_id,
                     "object_id": matched_identity.id,
                     "user_id": user.id,
                     "type": idp.type,
