42e00948bf59a0653192895f35e8653bc1be7dbb
Kevin Liu <kevinsliu0@gmail.com>
2024-06-20 17:03:24 -0400

    feat(analytics): add project_platforms back to analytics (#73081)
    update analytics

--- static/app/views/traces/content.tsx
+++ static/app/views/traces/content.tsx
@@ -37,6 +37,7 @@ import {decodeInteger, decodeList, decodeScalar} from 'sentry/utils/queryString'
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
+import useProjects from 'sentry/utils/useProjects';
 import * as ModuleLayout from 'sentry/views/insights/common/components/moduleLayout';
 
 import {type Field, FIELDS, SORTS} from './data';
@@ -615,6 +616,7 @@ function useTraces<F extends string>({
   sort,
 }: UseTracesOptions<F>) {
   const organization = useOrganization();
+  const {projects} = useProjects();
   const {selection} = usePageFilters();
 
   const path = `/organizations/${organization.slug}/traces/`;
@@ -665,6 +667,11 @@ function useTraces<F extends string>({
 
   useEffect(() => {
     if (result.status === 'success') {
+      const project_slugs = [...new Set(result.data.data.map(trace => trace.project))];
+      const project_platforms = projects
+        .filter(p => project_slugs.includes(p.slug))
+        .map(p => p.platform ?? '');
+
       trackAnalytics('trace_explorer.search_success', {
         organization,
         queries,
@@ -672,6 +679,7 @@ function useTraces<F extends string>({
         num_traces: result.data.data.length,
         num_missing_trace_root: result.data.data.filter(trace => trace.name === null)
           .length,
+        project_platforms,
       });
     } else if (result.status === 'error') {
       const response = result.error.responseJSON;
