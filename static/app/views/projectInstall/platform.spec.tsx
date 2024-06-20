4a0f509c60287cd16e4c9efbc77ad1333ad1433c
Hubert Deng <hubert.deng@sentry.io>
2024-06-20 13:48:14 -0700

    feat(self-hosted): Gate features on project onboarding page for errors-only self-hosted (#72176)
    Ensure that the project onboarding page disables all non-errors
    features.
    1. Checkbox for performance, session replay, and profiling are disabled
    for configure SDK code
    2. Button to navigate to performance and session replay pages are
    disabled
    
    ![Screenshot 2024-06-05 at 2 44
    38 PM](https://github.com/getsentry/sentry/assets/25517925/438e2b76-0fc1-4181-a622-869b0ae51a09)

--- static/app/views/projectInstall/platform.spec.tsx
+++ static/app/views/projectInstall/platform.spec.tsx
@@ -4,6 +4,7 @@ import {ProjectKeysFixture} from 'sentry-fixture/projectKeys';
 import {initializeOrg} from 'sentry-test/initializeOrg';
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
+import ConfigStore from 'sentry/stores/configStore';
 import ProjectsStore from 'sentry/stores/projectsStore';
 import type {Project} from 'sentry/types/project';
 import {ProjectInstallPlatform} from 'sentry/views/projectInstall/platform';
@@ -53,6 +54,7 @@ function mockProjectApiResponses(projects: Array<Project | ProjectWithBadPlatfor
 describe('ProjectInstallPlatform', function () {
   beforeEach(function () {
     MockApiClient.clearMockResponses();
+    ConfigStore.init();
   });
 
   it('should render NotFound if no matching integration/platform', async function () {
@@ -136,5 +138,46 @@ describe('ProjectInstallPlatform', function () {
         name: 'Configure Browser JavaScript SDK',
       })
     ).toBeInTheDocument();
+
+    expect(await screen.getByText('Take me to Issues')).toBeInTheDocument();
+    expect(await screen.getByText('Take me to Performance')).toBeInTheDocument();
+    expect(await screen.getByText('Take me to Session Replay')).toBeInTheDocument();
+  });
+
+  it('should not render performance/session replay buttons for errors only self-hosted', async function () {
+    const project = ProjectFixture({platform: 'javascript'});
+
+    const routeParams = {
+      projectId: project.slug,
+      platform: 'python',
+    };
+
+    const {routerProps, router} = initializeOrg({
+      router: {
+        location: {
+          query: {},
+        },
+        params: routeParams,
+      },
+    });
+
+    ProjectsStore.loadInitialData([project]);
+
+    mockProjectApiResponses([project]);
+    ConfigStore.set('isSelfHostedErrorsOnly', true);
+
+    render(<ProjectInstallPlatform {...routerProps} />, {
+      router,
+    });
+
+    expect(
+      await screen.findByRole('heading', {
+        name: 'Configure Browser JavaScript SDK',
+      })
+    ).toBeInTheDocument();
+
+    expect(screen.getByText('Take me to Issues')).toBeInTheDocument();
+    expect(() => screen.getByText('Take me to Performance')).toThrow();
+    expect(() => screen.getByText('Take me to Session Replay')).toThrow();
   });
 });
