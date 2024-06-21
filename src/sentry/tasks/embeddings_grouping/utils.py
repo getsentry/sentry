9d03adef66f63e29a5d95189447d02ba0b68c2af
Josh Ferge <josh.ferge@sentry.io>
2024-06-21 16:28:25 -0700

    fix(similarity): cohort backfill pr adjustments (#73126)
    follow ups from https://github.com/getsentry/sentry/pull/73075

--- src/sentry/tasks/embeddings_grouping/utils.py
+++ src/sentry/tasks/embeddings_grouping/utils.py
@@ -514,10 +514,10 @@ def delete_seer_grouping_records(
         Group.objects.bulk_update(groups_with_seer_metadata, ["data"])
 
 
-def get_project_for_batch(last_processed_project_index, cohort_list, cohort_name):
-    next_cohort_index = last_processed_project_index + 1
-    if next_cohort_index >= len(cohort_list):
+def get_project_for_batch(last_processed_project_index, cohort_projects):
+    next_project_index = last_processed_project_index + 1
+    if next_project_index >= len(cohort_projects):
         return None, None
-    project_id = cohort_list[next_cohort_index]
-    last_processed_project_index = next_cohort_index
+    project_id = cohort_projects[next_project_index]
+    last_processed_project_index = next_project_index
     return project_id, last_processed_project_index
