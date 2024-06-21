9362f8e9c466524d73386dd3e1257a9e8a91df14
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-06-21 12:54:16 -0400

    ✅ Fix test mocks

--- static/app/components/replays/breadcrumbs/breadcrumbItem.spec.tsx
+++ static/app/components/replays/breadcrumbs/breadcrumbItem.spec.tsx
@@ -1,36 +1,21 @@
 import {OrganizationFixture} from 'sentry-fixture/organization';
+import {ReplayClickFrameFixture} from 'sentry-fixture/replay/replayBreadcrumbFrameData';
+import {ReplayRecordFixture} from 'sentry-fixture/replayRecord';
 
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import BreadcrumbItem from 'sentry/components/replays/breadcrumbs/breadcrumbItem';
+import hydrateBreadcrumbs from 'sentry/utils/replays/hydrateBreadcrumbs';
 
-const MOCK_FRAME = {
-  timestamp: new Date(1718818835397),
-  type: 'default',
-  category: 'ui.click',
-  message:
-    'div.app-pv36ag.e151korg0 > IntegrationRow > FlexContainer > TitleContainer > IntegrationName',
-  data: {
-    nodeId: 6889,
-    node: {
-      id: 6889,
-      tagName: 'a',
-      textContent: '',
-      attributes: {
-        class: 'app-3jkjlw ek2xunm6',
-        'data-sentry-component': 'IntegrationName',
-      },
-    },
-  },
-  offsetMs: 208397,
-  timestampMs: 1718818835397,
-};
-
-const MOCK_EXTRACTION = {
-  frame: MOCK_FRAME,
-  html: '<a data-sentry-element="IntegrationName" data-sentry-source-file="integrationRow.tsx" class="app-3jkjlw ek2xunm6" href="https://pelpr-org.sentry.io/settings/integrations/msteams/">********* *****</a>',
-  timestamp: 1718818835397,
-};
+const [MOCK_FRAME] = hydrateBreadcrumbs(
+  ReplayRecordFixture(),
+  [
+    ReplayClickFrameFixture({
+      timestamp: new Date('2024/06/21'),
+    }),
+  ],
+  []
+);
 
 describe('BreadcrumbItem', function () {
   const organization = OrganizationFixture({features: ['new-timeline-ui']});
@@ -41,7 +26,6 @@ describe('BreadcrumbItem', function () {
     const mockMouseLeave = jest.fn();
     render(
       <BreadcrumbItem
-        extraction={MOCK_EXTRACTION}
         frame={MOCK_FRAME}
         onMouseEnter={mockMouseEnter}
         onMouseLeave={mockMouseLeave}
@@ -55,7 +39,6 @@ describe('BreadcrumbItem', function () {
     const title = screen.getByText('User Click');
     expect(title).toBeInTheDocument();
     expect(screen.getByText('00:00')).toBeInTheDocument();
-    expect(screen.getByText('IntegrationName')).toBeInTheDocument();
 
     await userEvent.hover(title);
     expect(mockMouseEnter).toHaveBeenCalled();
