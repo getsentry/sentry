2b1c89dd2721f43934792fadf7122c6650a2f05f
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-06-21 08:58:18 +0200

    chore(metrics): remove disabled featured flag (#73101)

--- static/app/utils/metrics/features.tsx
+++ static/app/utils/metrics/features.tsx
@@ -5,15 +5,8 @@ export function hasMetricsExperimentalFeature(organization: Organization) {
   return organization.features.includes('custom-metrics-experimental');
 }
 
-export function hasMetricsSidebarItem(organization: Organization) {
-  return !organization.features.includes('ddm-sidebar-item-hidden');
-}
-
 export function hasCustomMetrics(organization: Organization) {
-  return (
-    organization.features.includes('custom-metrics') &&
-    hasMetricsSidebarItem(organization)
-  );
+  return organization.features.includes('custom-metrics');
 }
 
 export function hasMetricAlertFeature(organization: Organization) {
