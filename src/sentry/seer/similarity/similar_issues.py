39afae796db310be632602c39ba640d966ef5102
Katie Byers <katie.byers@sentry.io>
2024-06-24 15:13:33 -0700

    feat(seer grouping): Add `seer.similar_issues_request` metric (#73142)
    This adds a new metric, `seer.similar_issues_request`, to track the results of our calls to the seer similarity endpoint. In addition to counting the calls themselves, the new metric tracks the response status code, the result of the call (matching group, similar groups, no similar groups, error), and in the erroring case, what type of error occurred. 
    
    Notes:
    
    - For now the sample rate is set to 100%, so that we can get some decent signal even while the feature is only rolled out to a few customers. Once we GA, we'll probably want to change it to a lower rate.
    
    - In keeping with our handling of non-200 responses, redirects are considered errors, because they imply that a change has been made on the Seer side and we haven't correctly updated the Sentry code to match. They are however tracked separately from other request errors.

--- src/sentry/seer/similarity/similar_issues.py
+++ src/sentry/seer/similarity/similar_issues.py
@@ -11,10 +11,15 @@ from sentry.seer.similarity.types import (
     SimilarGroupNotFoundError,
     SimilarIssuesEmbeddingsRequest,
 )
-from sentry.utils import json
+from sentry.utils import json, metrics
 from sentry.utils.json import JSONDecodeError
 
 logger = logging.getLogger(__name__)
+
+# TODO: Keeping this at a 100% sample rate for now in order to get good signal as we're rolling out
+# and calls are still comparatively rare. Once traffic gets heavy enough, we should probably ramp
+# this down.
+SIMILARITY_REQUEST_METRIC_SAMPLE_RATE = 1.0
 
 
 seer_grouping_connection_pool = connection_from_url(
@@ -39,6 +44,8 @@ def get_similarity_data_from_seer(
         ),
     )
 
+    metric_tags: dict[str, str | int] = {"response_status": response.status}
+
     if response.status > 200:
         redirect = response.get_redirect_location()
         if redirect:
@@ -50,6 +57,17 @@ def get_similarity_data_from_seer(
                 f"Received {response.status} when calling Seer endpoint {SEER_SIMILAR_ISSUES_URL}.",  # noqa
                 extra={"response_data": response.data},
             )
+
+        metrics.incr(
+            "seer.similar_issues_request",
+            sample_rate=SIMILARITY_REQUEST_METRIC_SAMPLE_RATE,
+            tags={
+                **metric_tags,
+                "outcome": "error",
+                "error": "Redirect" if redirect else "RequestError",
+            },
+        )
+
         return []
 
     try:
@@ -58,7 +76,7 @@ def get_similarity_data_from_seer(
         AttributeError,  # caused by a response with no data and therefore no `.decode` method
         UnicodeError,
         JSONDecodeError,
-    ):
+    ) as e:
         logger.exception(
             "Failed to parse seer similar issues response",
             extra={
@@ -67,10 +85,24 @@ def get_similarity_data_from_seer(
                 "response_code": response.status,
             },
         )
+        metrics.incr(
+            "seer.similar_issues_request",
+            sample_rate=SIMILARITY_REQUEST_METRIC_SAMPLE_RATE,
+            tags={**metric_tags, "outcome": "error", "error": type(e).__name__},
+        )
         return []
 
     if not response_data:
+        metrics.incr(
+            "seer.similar_issues_request",
+            sample_rate=SIMILARITY_REQUEST_METRIC_SAMPLE_RATE,
+            tags={**metric_tags, "outcome": "no_similar_groups"},
+        )
         return []
+
+    # This may get overwritten as we process the results, but by this point we know that Seer at
+    # least found *something*
+    metric_tags["outcome"] = "similar_groups_found"
 
     normalized_results = []
 
@@ -79,8 +111,21 @@ def get_similarity_data_from_seer(
             normalized = SeerSimilarIssueData.from_raw(
                 similar_issues_request["project_id"], raw_similar_issue_data
             )
+
+            if (
+                normalized.should_group
+                # If an earlier entry in the results list caused an error, we don't want to
+                # overwrite that information
+                and metric_tags["outcome"] != "error"
+            ):
+                metric_tags["outcome"] = "matching_group_found"
+
             normalized_results.append(normalized)
         except IncompleteSeerDataError as err:
+            # This will tag the entire request as errored even if not all of the results are
+            # incomplete, but that's okay, because even one being incomplete means that Seer is
+            # broken in some way
+            metric_tags.update({"outcome": "error", "error": "IncompleteSeerDataError"})
             logger.exception(
                 str(err),
                 extra={
@@ -89,6 +134,11 @@ def get_similarity_data_from_seer(
                 },
             )
         except SimilarGroupNotFoundError:
+            # This will similarly mark the entire request as errored even if it's only one group
+            # that we can't find, but again, we're okay with that because a group being missing
+            # implies there's something wrong with our deletion logic, which is a problem to which
+            # we want to pay attention.
+            metric_tags.update({"outcome": "error", "error": "SimilarGroupNotFoundError"})
             logger.warning(
                 "get_similarity_data_from_seer.parent_group_not_found",
                 extra={
@@ -98,6 +148,11 @@ def get_similarity_data_from_seer(
                 },
             )
 
+    metrics.incr(
+        "seer.similar_issues_request",
+        sample_rate=SIMILARITY_REQUEST_METRIC_SAMPLE_RATE,
+        tags=metric_tags,
+    )
     return sorted(
         normalized_results,
         key=lambda issue_data: issue_data.stacktrace_distance,
