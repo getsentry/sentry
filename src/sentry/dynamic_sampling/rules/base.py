201191247ae4189f3c91d1433d402e55c3be5ec6
Vjeran Grozdanic <vjerangrozdanic@sentry.io>
2024-06-26 15:38:34 +0200

    feedback fix
    Signed-off-by: Vjeran Grozdanic <vjerangrozdanic@sentry.io>

--- src/sentry/dynamic_sampling/rules/base.py
+++ src/sentry/dynamic_sampling/rules/base.py
@@ -49,9 +49,8 @@ def get_guarded_blended_sample_rate(organization: Organization, project: Project
     )
 
     # get_blended_sample_rate returns None if the organization doesn't have dynamic sampling
-    # see: https://github.com/getsentry/sentry/blob/5499277c21f2dc997da79343087565b6676a6f50/src/sentry/quotas/base.py#L585
     if sample_rate is None:
-        sample_rate = 1.0
+        return 1.0
 
     # If the sample rate is 100%, we don't want to use any special dynamic sample rate, we will just sample at 100%.
     if sample_rate == 1.0:
