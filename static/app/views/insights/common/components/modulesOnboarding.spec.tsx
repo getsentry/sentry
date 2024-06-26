688c0e23de53579c01bacf1c0d13d62e657b5164
KevinL10 <kevinsliu0@gmail.com>
2024-06-26 14:57:53 -0400

    update moduleOnboarding test for new project flags

--- static/app/views/insights/common/components/modulesOnboarding.spec.tsx
+++ static/app/views/insights/common/components/modulesOnboarding.spec.tsx
@@ -1,59 +1,105 @@
-import {OrganizationFixture} from 'sentry-fixture/organization';
+import {ProjectFixture} from 'sentry-fixture/project';
 
-import {render, screen, waitForElementToBeRemoved} from 'sentry-test/reactTestingLibrary';
+import {render, screen} from 'sentry-test/reactTestingLibrary';
 
-import {MutableSearch} from 'sentry/utils/tokenizeSearch';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import useProjects from 'sentry/utils/useProjects';
+import {ModuleName} from 'sentry/views/insights/types';
 
 import {ModulesOnboarding} from './modulesOnboarding';
 
+jest.mock('sentry/utils/useProjects');
+jest.mock('sentry/utils/usePageFilters');
+
 describe('ModulesOnboarding', () => {
-  const organization = OrganizationFixture();
+  beforeEach(function () {});
 
   afterEach(() => {
     jest.resetAllMocks();
   });
 
   it('renders children correctly', async () => {
-    const eventsMock = MockApiClient.addMockResponse({
-      url: `/organizations/${organization.slug}/events/`,
-      method: 'GET',
-      body: {data: [{'count()': 1}]},
+    const project = ProjectFixture();
+    project.firstTransactionEvent = true;
+    project.hasInsightsCaches = true;
+
+    jest.mocked(useProjects).mockReturnValue({
+      projects: [project],
+      onSearch: jest.fn(),
+      placeholders: [],
+      fetching: false,
+      hasMore: null,
+      fetchError: null,
+      initiallyLoaded: false,
+    });
+
+    jest.mocked(usePageFilters).mockReturnValue({
+      isReady: true,
+      desyncedFilters: new Set(),
+      pinnedFilters: new Set(),
+      shouldPersist: true,
+      selection: {
+        datetime: {
+          period: '10d',
+          start: null,
+          end: null,
+          utc: false,
+        },
+        environments: [],
+        projects: [2],
+      },
     });
 
     render(
       <ModulesOnboarding
-        moduleQueryFilter={new MutableSearch('')}
+        moduleName={ModuleName.CACHE}
         onboardingContent={<div>Start collecting Insights!</div>}
-        referrer=""
       >
         <div>Module Content</div>
       </ModulesOnboarding>
     );
 
-    expect(eventsMock).toHaveBeenCalled();
-    await waitForElementToBeRemoved(() => screen.queryAllByTestId('loading-indicator'));
     await screen.getByText('Module Content');
   });
 
   it('renders onboarding content correctly', async () => {
-    const eventsMock = MockApiClient.addMockResponse({
-      url: `/organizations/${organization.slug}/events/`,
-      method: 'GET',
-      body: {data: []},
+    const project = ProjectFixture();
+    jest.mocked(useProjects).mockReturnValue({
+      projects: [project],
+      onSearch: jest.fn(),
+      placeholders: [],
+      fetching: false,
+      hasMore: null,
+      fetchError: null,
+      initiallyLoaded: false,
+    });
+
+    jest.mocked(usePageFilters).mockReturnValue({
+      isReady: true,
+      desyncedFilters: new Set(),
+      pinnedFilters: new Set(),
+      shouldPersist: true,
+      selection: {
+        datetime: {
+          period: '10d',
+          start: null,
+          end: null,
+          utc: false,
+        },
+        environments: [],
+        projects: [2],
+      },
     });
 
     render(
       <ModulesOnboarding
-        moduleQueryFilter={new MutableSearch('')}
+        moduleName={ModuleName.CACHE}
         onboardingContent={<div>Start collecting Insights!</div>}
-        referrer=""
       >
         <div>Module Content</div>
       </ModulesOnboarding>
     );
 
-    expect(eventsMock).toHaveBeenCalled();
-    await waitForElementToBeRemoved(() => screen.queryAllByTestId('loading-indicator'));
     await screen.findByText('Start collecting Insights!');
   });
 });
