5132a7dfd0ada1121bb217a8c390f58784ac71bf
Raj Joshi <raj.joshi@sentry.io>
2024-06-27 11:19:52 -0700

    fix

--- tests/sentry/integrations/slack/webhooks/events/test_link_shared.py
+++ tests/sentry/integrations/slack/webhooks/events/test_link_shared.py
@@ -8,6 +8,7 @@ import responses
 from slack_sdk.web import SlackResponse
 
 from sentry.integrations.slack.unfurl import Handler, make_type_coercer
+from sentry.testutils.helpers.features import with_feature
 
 from . import LINK_SHARED_EVENT, BaseEventTest, build_test_block
 
@@ -66,42 +67,6 @@ class LinkSharedEventTest(BaseEventTest):
         assert unfurls["link2"] == "unfurl"
 
     @responses.activate
-    @patch(
-        "sentry.integrations.slack.webhooks.event.match_link",
-        # match_link will be called twice, for each our links. Resolve into
-        # two unique links and one duplicate.
-        side_effect=[
-            ("mock_link", {"arg1": "value1"}),
-            ("mock_link", {"arg1", "value2"}),
-            ("mock_link", {"arg1": "value1"}),
-        ],
-    )
-    @patch(
-        "sentry.integrations.slack.webhooks.event.link_handlers",
-        {
-            "mock_link": Handler(
-                matcher=[re.compile(r"test")],
-                arg_mapper=make_type_coercer({}),
-                fn=Mock(return_value={"link1": "unfurl", "link2": "unfurl"}),
-            )
-        },
-    )
-    @with_feature("organizations:slack-sdk-slack-event-endpoint")
-    def test_share_links_sdk(self, mock_match_link):
-        resp = self.post_webhook(event_data=orjson.loads(LINK_SHARED_EVENT))
-        assert resp.status_code == 200, resp.content
-        assert len(mock_match_link.mock_calls) == 3
-
-        data = self.mock_unfurl.call_args[1]
-        unfurls = orjson.loads(data["unfurls"])
-
-        # We only have two unfurls since one link was duplicated
-        assert len(unfurls) == 2
-        assert unfurls["link1"] == "unfurl"
-        assert unfurls["link2"] == "unfurl"
-
-    @responses.activate
-    @with_feature("organizations:slack-block-kit")
     @patch(
         "sentry.integrations.slack.webhooks.event.match_link",
         # match_link will be called twice, for each our links. Resolve into
