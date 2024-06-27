b083a2bac93a18e5d31f7051f90d388d1fb320ec
Mark Story <mark@mark-story.com>
2024-06-27 14:22:49 -0400

    Add shim libs for getsentry

---
+++ src/sentry/services/hybrid_cloud/auth/__init__.py
@@ -0,0 +1,4 @@
+# TODO(hybridcloud) Remove once getsentry usage is updated
+from sentry.auth.services.auth.service import auth_service
+
+__all__ = ("auth_service",)
---
+++ src/sentry/services/hybrid_cloud/auth/impl.py
@@ -0,0 +1,4 @@
+# TODO(hybridcloud) Remove once getsentry usage is updated
+from sentry.auth.services.auth.impl import promote_request_rpc_user
+
+__all__ = ("promote_request_rpc_user",)
