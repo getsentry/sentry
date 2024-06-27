26f7d247df9ff7c5302d08e1467f92f0888bc5e6
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-06-27 10:30:31 -0700

    feat(replay): add info alert about quotas, to replay settings page (#73412)
    Ref https://github.com/getsentry/sentry/issues/72123 and
    https://github.com/getsentry/getsentry/pull/14453
    
    Info alert shows up as blue on `dev-ui` from local `getsentry` repo.
    Note screenshot is outdated, see the getsentry PR for the most up to
    date message
    ![Screenshot 2024-06-26 at 4 34
    49 PM](https://github.com/getsentry/sentry/assets/159852527/12bcc504-8db2-4a1b-81d8-79a23a2c418c)
    
    I also checked the page doesn't error when `dev-ui` is ran from just
    `sentry`

---
+++ static/app/components/replays/alerts/replaySettingsAlert.tsx
@@ -0,0 +1,8 @@
+import HookOrDefault from 'sentry/components/hookOrDefault';
+
+const ReplaySettingsAlert = HookOrDefault({
+  hookName: 'component:replay-settings-alert',
+  defaultComponent: null,
+});
+
+export default ReplaySettingsAlert;
