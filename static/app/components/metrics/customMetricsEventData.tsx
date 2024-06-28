82461ad4fbe4f134f08a93e5c51105fa71a8f5c2
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-06-28 10:09:47 +0200

    ref(metrics): renames operation to aggregation (#73147)

--- static/app/components/metrics/customMetricsEventData.tsx
+++ static/app/components/metrics/customMetricsEventData.tsx
@@ -22,7 +22,7 @@ import type {
   MRI,
 } from 'sentry/types/metrics';
 import {defined} from 'sentry/utils';
-import {getDefaultAggregate, getMetricsUrl} from 'sentry/utils/metrics';
+import {getDefaultAggregation, getMetricsUrl} from 'sentry/utils/metrics';
 import {hasCustomMetrics} from 'sentry/utils/metrics/features';
 import {formatMetricUsingUnit} from 'sentry/utils/metrics/formatters';
 import {formatMRI, parseMRI} from 'sentry/utils/metrics/mri';
@@ -93,7 +93,7 @@ export function CustomMetricsEventData({
       metricsSummaryEntries.map((entry, index) => ({
         mri: entry.mri,
         name: index.toString(),
-        op: getDefaultAggregate(entry.mri),
+        aggregation: getDefaultAggregation(entry.mri),
         query: Object.entries(entry.item.tags ?? {})
           .map(([tagKey, tagValue]) => tagToQuery(tagKey, tagValue))
           .join(' '),
@@ -210,7 +210,7 @@ export function CustomMetricsEventData({
               {
                 mri: mri,
                 displayType: MetricDisplayType.LINE,
-                op: getDefaultAggregate(mri),
+                aggregation: getDefaultAggregation(mri),
                 query: Object.entries(summaryItem.tags ?? {})
                   .map(([tagKey, tagValue]) => tagToQuery(tagKey, tagValue))
                   .join(' '),
