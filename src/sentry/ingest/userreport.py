b0162c212c28df3bfa9d6cc30940eb2f8e52feb2
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-06-28 18:15:22 -0700

    log(feedback): log user reports filtered from the denylist

--- src/sentry/ingest/userreport.py
+++ src/sentry/ingest/userreport.py
@@ -32,7 +32,17 @@ def save_userreport(
     start_time=None,
 ):
     with metrics.timer("sentry.ingest.userreport.save_userreport"):
-        if is_org_in_denylist(project.organization):
+        if project.organization.slug in options.get("feedback.organizations.slug-denylist"):
+            metrics.incr("user_report.create_user_report.filtered", tags={"reason": "org.denylist"})
+            logger.info(
+                "Filtered an org in the deny list",
+                extra={
+                    "organization_id": project.organization.id,
+                    "organization_slug": project.organization.slug,
+                    "project_id": project.id,
+                    "project_slug": project.slug,
+                },
+            )
             return
         if should_filter_user_report(report["comments"]):
             return
@@ -143,11 +153,4 @@ def should_filter_user_report(comments: str):
         )
         return True
 
-    return False
-
-
-def is_org_in_denylist(organization):
-    if organization.slug in options.get("feedback.organizations.slug-denylist"):
-        metrics.incr("user_report.create_user_report.filtered", tags={"reason": "org.denylist"})
-        return True
     return False
