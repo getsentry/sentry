f54ea178b8d2190ac70421e7daeecc28d7b90f73
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-07-01 15:10:42 -0700

    leave hydrateBreadcrumbs alone

--- static/app/utils/replays/hydrateBreadcrumbs.tsx
+++ static/app/utils/replays/hydrateBreadcrumbs.tsx
@@ -3,13 +3,31 @@ import invariant from 'invariant';
 import {t} from 'sentry/locale';
 import {BreadcrumbType} from 'sentry/types/breadcrumbs';
 import isValidDate from 'sentry/utils/date/isValidDate';
-import type {BreadcrumbFrame, RawBreadcrumbFrame} from 'sentry/utils/replays/types';
-import {isBreadcrumbFrame} from 'sentry/utils/replays/types';
+import type {
+  BreadcrumbFrame,
+  RawBreadcrumbFrame,
+  RecordingFrame,
+} from 'sentry/utils/replays/types';
+import {EventType, isBreadcrumbFrame} from 'sentry/utils/replays/types';
 import type {ReplayRecord} from 'sentry/views/replays/types';
+
+function findCloseMutations(date: Date, rrwebFrames: RecordingFrame[]) {
+  const timeMS = date.getTime();
+  const incrementalFrames = rrwebFrames.filter(
+    frame => frame.type === EventType.IncrementalSnapshot
+  );
+  const framesBefore = incrementalFrames.filter(frame => frame.timestamp <= timeMS);
+  const framesAfter = incrementalFrames.filter(frame => frame.timestamp > timeMS);
+  return {
+    prev: framesBefore.at(-1) ?? null,
+    next: framesAfter.at(0) ?? null,
+  };
+}
 
 export default function hydrateBreadcrumbs(
   replayRecord: ReplayRecord,
-  breadcrumbFrames: RawBreadcrumbFrame[]
+  breadcrumbFrames: RawBreadcrumbFrame[],
+  rrwebFrames: RecordingFrame[]
 ): BreadcrumbFrame[] {
   const startTimestampMs = replayRecord.started_at.getTime();
 
@@ -22,6 +40,7 @@ export default function hydrateBreadcrumbs(
         if (frame.category === 'replay.hydrate-error') {
           frame.data = {
             description: t('Encountered an error while hydrating'),
+            mutations: findCloseMutations(time, rrwebFrames),
           };
         }
         return {
