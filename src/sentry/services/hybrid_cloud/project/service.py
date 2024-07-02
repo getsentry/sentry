e894a7f381d466fae47edc83ac087d311f353b47
Mark Story <mark@mark-story.com>
2024-07-02 12:18:19 -0400

    Add shim modules for getsentry

---
+++ src/sentry/services/hybrid_cloud/project/service.py
@@ -0,0 +1,4 @@
+# TODO(hybridcloud) Remove once getsentry usage is updated
+from sentry.projects.services.project.service import project_service
+
+__all__ = ("project_service",)
---
+++ src/sentry/services/hybrid_cloud/project_key/service.py
@@ -0,0 +1,4 @@
+# TODO(hybridcloud) Remove once getsentry usage is updated
+from sentry.projects.services.project_key.service import project_key_service
+
+__all__ = ("project_key_service",)
