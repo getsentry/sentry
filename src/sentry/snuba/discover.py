d76a61d2aa3a1a1ceb13152aa09ef56b93170732
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2024-07-03 16:29:44 -0400

    remove discoverSplitDecision from meta for now

--- src/sentry/snuba/discover.py
+++ src/sentry/snuba/discover.py
@@ -14,12 +14,12 @@ from snuba_sdk import Condition, Function, Op
 from sentry.discover.arithmetic import categorize_columns
 from sentry.exceptions import InvalidSearchQuery
 from sentry.models.group import Group
-from sentry.search.events.builder.discover import (
-    DiscoverQueryBuilder,
+from sentry.search.events.builder import (
     HistogramQueryBuilder,
     TimeseriesQueryBuilder,
     TopEventsQueryBuilder,
 )
+from sentry.search.events.builder.discover import DiscoverQueryBuilder
 from sentry.search.events.fields import (
     FIELD_ALIASES,
     get_function_alias,
@@ -70,7 +70,6 @@ class EventsMeta(TypedDict):
     datasetReason: NotRequired[str]
     isMetricsData: NotRequired[bool]
     isMetricsExtractedData: NotRequired[bool]
-    discoverSplitDecision: NotRequired[bool]
 
 
 # When calling make build-spectacular-docs we hit this issue
