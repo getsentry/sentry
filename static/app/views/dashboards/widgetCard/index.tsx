92e4c5b3bd61278850fc3e0b502de9228281ca9b
Arthur Knaus <arthur.knaus@sentry.io>
2024-07-04 13:37:05 +0200

    fix(metrics-extraction): Dashboard without feature flag

--- static/app/views/dashboards/widgetCard/index.tsx
+++ static/app/views/dashboards/widgetCard/index.tsx
@@ -25,7 +25,10 @@ import {getFormattedDate} from 'sentry/utils/dates';
 import type {TableDataWithTitle} from 'sentry/utils/discover/discoverQuery';
 import type {AggregationOutputType} from 'sentry/utils/discover/fields';
 import {parseFunction} from 'sentry/utils/discover/fields';
-import {hasCustomMetrics} from 'sentry/utils/metrics/features';
+import {
+  hasCustomMetrics,
+  hasCustomMetricsExtractionRules,
+} from 'sentry/utils/metrics/features';
 import {VirtualMetricsContextProvider} from 'sentry/utils/metrics/virtualMetricsContext';
 import {hasOnDemandMetricWidgetFeature} from 'sentry/utils/onDemandMetrics/features';
 import {ExtractedMetricsTag} from 'sentry/utils/performance/contexts/metricsEnhancedPerformanceDataContext';
@@ -274,7 +277,7 @@ class WidgetCard extends Component<Props, State> {
 
     if (widget.widgetType === WidgetType.METRICS) {
       if (hasCustomMetrics(organization)) {
-        return (
+        return hasCustomMetricsExtractionRules(organization) ? (
           <VirtualMetricsContextProvider>
             <MetricWidgetCard
               index={this.props.index}
@@ -292,6 +295,22 @@ class WidgetCard extends Component<Props, State> {
               showContextMenu={this.props.showContextMenu}
             />
           </VirtualMetricsContextProvider>
+        ) : (
+          <MetricWidgetCard
+            index={this.props.index}
+            isEditingDashboard={this.props.isEditingDashboard}
+            onEdit={this.props.onEdit}
+            onDelete={this.props.onDelete}
+            onDuplicate={this.props.onDuplicate}
+            router={this.props.router}
+            location={this.props.location}
+            organization={organization}
+            selection={selection}
+            widget={widget}
+            dashboardFilters={dashboardFilters}
+            renderErrorMessage={renderErrorMessage}
+            showContextMenu={this.props.showContextMenu}
+          />
         );
       }
     }
