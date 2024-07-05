856f27f2cf840c5409bfa51ba0ce9d9164ab939d
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-07-05 16:28:11 -0400

    add check

--- static/app/components/replays/videoReplayer.tsx
+++ static/app/components/replays/videoReplayer.tsx
@@ -447,7 +447,7 @@ export class VideoReplayer {
   protected setVideoTime(video: HTMLVideoElement, timeMs: number) {
     // If 'ended' is true, the current time will be overwritten to 0 after hitting play.
     // This ensures that ended is reset to false so current time doesn't get overwritten
-    if (timeMs > 0) {
+    if (video.ended && timeMs > 0) {
       video.currentTime = (timeMs - 1) / 1000;
     }
 
