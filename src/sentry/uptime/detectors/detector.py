6742cf6264b84da87a700bd67ff66929f37ad888
davidenwang <dwang@sentry.io>
2024-07-08 14:36:56 -0700

    feat(uptime): Add additional logs for detection

--- src/sentry/uptime/detectors/detector.py
+++ src/sentry/uptime/detectors/detector.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 from sentry import features
 from sentry.uptime.detectors.ranking import add_base_url_to_rank
 from sentry.uptime.detectors.url_extraction import extract_base_url
+from sentry.utils import metrics
 
 if TYPE_CHECKING:
     from sentry.models.project import Project
@@ -19,3 +20,4 @@ def detect_base_url_for_project(project: Project, url: str) -> None:
         return
 
     add_base_url_to_rank(project, base_url)
+    metrics.incr("uptime.url_added_to_rank")
