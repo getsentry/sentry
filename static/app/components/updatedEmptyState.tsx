getsentry/sentry

commit 87293f8d8fedea22a3eeb90f86e4550a3179c9f4

feat(empty-states): Add more platforms (#73771)
this pr adds more platforms to the issue stream empty state, along with
a few changes to support them.

static/app/components/updatedEmptyState.tsx
@@ -95,10 +95,13 @@ export default function UpdatedEmptyState({project}: {project?: Project}) {
   const installConfigurations = install.configurations ?? [];
 
   const {configurations, description: configureDescription} = configure[0] ?? {};
-  const {configurations: sourceMapConfigurations, description: sourcemapDescription} =
-    configure[1] ?? {};
+  const {
+    configurations: extraConfigurations,
+    description: extraConfigDescription,
+    title: extraConfigTitle,
+  } = configure[1] ?? {};
 
-  const {description: verifyDescription, configurations: verifyConfigutations} =
+  const {description: verifyDescription, configurations: verifyConfigurations} =
     verify[0] ?? {};
 
   return (
@@ -135,23 +138,25 @@ export default function UpdatedEmptyState({project}: {project?: Project}) {
                       </CodeSnippetWrapper>
                     </div>
                   ))}
-                  {verify.length === 0 && (
-                    <FirstEventIndicator
-                      organization={organization}
-                      project={project}
-                      eventType="error"
-                    >
-                      {({indicator, firstEventButton}) => (
-                        <div>
-                          <IndicatorWrapper>{indicator}</IndicatorWrapper>
-                          <StyledButtonBar gap={1}>
-                            <GuidedSteps.BackButton size="md" />
-                            {firstEventButton}
-                          </StyledButtonBar>
-                        </div>
-                      )}
-                    </FirstEventIndicator>
-                  )}
+                  {!configurations &&
+                    !extraConfigDescription &&
+                    !verifyConfigurations && (
+                      <FirstEventIndicator
+                        organization={organization}
+                        project={project}
+                        eventType="error"
+                      >
+                        {({indicator, firstEventButton}) => (
+                          <div>
+                            <IndicatorWrapper>{indicator}</IndicatorWrapper>
+                            <StyledButtonBar gap={1}>
+                              <GuidedSteps.BackButton size="md" />
+                              {firstEventButton}
+                            </StyledButtonBar>
+                          </div>
+                        )}
+                      </FirstEventIndicator>
+                    )}
                 </div>
                 <GuidedSteps.ButtonWrapper>
                   <GuidedSteps.BackButton size="md" />
@@ -180,6 +185,9 @@ export default function UpdatedEmptyState({project}: {project?: Project}) {
                             )
                           ) : null}
                         </CodeSnippetWrapper>
+                        <DescriptionWrapper>
+                          {configuration.additionalInfo}
+                        </DescriptionWrapper>
                       </div>
                     ))}
                   </div>
@@ -192,43 +200,66 @@ export default function UpdatedEmptyState({project}: {project?: Project}) {
             ) : (
               <Fragment />
             )}
-            {sourcemapDescription ? (
+            {extraConfigDescription ? (
               <GuidedSteps.Step
-                stepKey="sourcemaps-sentry"
-                title={t('Upload Sourcemaps')}
+                stepKey="extra-configuration-sentry"
+                title={extraConfigTitle || t('Upload Source Maps')}
               >
                 <div>
-                  <DescriptionWrapper>{sourcemapDescription}</DescriptionWrapper>
-                  {sourceMapConfigurations?.map((configuration, index) => (
-                    <div key={index}>
-                      <DescriptionWrapper>{configuration.description}</DescriptionWrapper>
-                      <CodeSnippetWrapper>
-                        {configuration.code ? (
-                          Array.isArray(configuration.code) ? (
-                            <TabbedCodeSnippet tabs={configuration.code} />
-                          ) : (
-                            <OnboardingCodeSnippet language={configuration.language}>
-                              {configuration.code}
-                            </OnboardingCodeSnippet>
-                          )
-                        ) : null}
-                      </CodeSnippetWrapper>
-                    </div>
-                  ))}
-                  <GuidedSteps.ButtonWrapper>
-                    <GuidedSteps.BackButton size="md" />
-                    <GuidedSteps.NextButton size="md" />
-                  </GuidedSteps.ButtonWrapper>
+                  <div>
+                    <DescriptionWrapper>{extraConfigDescription}</DescriptionWrapper>
+                    {extraConfigurations?.map((configuration, index) => (
+                      <div key={index}>
+                        <DescriptionWrapper>
+                          {configuration.description}
+                        </DescriptionWrapper>
+                        <CodeSnippetWrapper>
+                          {configuration.code ? (
+                            Array.isArray(configuration.code) ? (
+                              <TabbedCodeSnippet tabs={configuration.code} />
+                            ) : (
+                              <OnboardingCodeSnippet language={configuration.language}>
+                                {configuration.code}
+                              </OnboardingCodeSnippet>
+                            )
+                          ) : null}
+                        </CodeSnippetWrapper>
+                      </div>
+                    ))}
+                    {!verifyConfigurations && !verifyDescription && (
+                      <FirstEventIndicator
+                        organization={organization}
+                        project={project}
+                        eventType="error"
+                      >
+                        {({indicator, firstEventButton}) => (
+                          <div>
+                            <IndicatorWrapper>{indicator}</IndicatorWrapper>
+                            <StyledButtonBar gap={1}>
+                              <GuidedSteps.BackButton size="md" />
+                              {firstEventButton}
+                            </StyledButtonBar>
+                          </div>
+                        )}
+                      </FirstEventIndicator>
+                    )}
+                  </div>
+                  {(verifyConfigurations || verifyDescription) && (
+                    <GuidedSteps.ButtonWrapper>
+                      <GuidedSteps.BackButton size="md" />
+                      <GuidedSteps.NextButton size="md" />
+                    </GuidedSteps.ButtonWrapper>
+                  )}
                 </div>
               </GuidedSteps.Step>
             ) : (
               <Fragment />
             )}
-            {verifyConfigutations ? (
+            {verifyConfigurations || verifyDescription ? (
               <GuidedSteps.Step stepKey="verify-sentry" title={t('Verify')}>
                 <div>
                   <DescriptionWrapper>{verifyDescription}</DescriptionWrapper>
-                  {verifyConfigutations.map((configuration, index) => (
+                  {verifyConfigurations?.map((configuration, index) => (
                     <div key={index}>
                       <DescriptionWrapper>{configuration.description}</DescriptionWrapper>
                       <CodeSnippetWrapper>
@@ -378,4 +409,8 @@ const CodeSnippetWrapper = styled('div')`
 
 const DescriptionWrapper = styled('div')`
   margin-bottom: ${space(1)};
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: ${space(0.5)};
 `;

static/app/views/issueList/noGroupsHandler/index.tsx
@@ -137,8 +137,13 @@ class NoGroupsHandler extends Component<Props, State> {
       'android',
       ...(organization.features.includes('issue-stream-empty-state-additional-platforms')
         ? [
+            'apple-ios',
+            'dotnet',
+            'dotnet-aspnetcore',
+            'flutter',
             'go',
             'javascript',
+            'javascript-angular',
             'javascript-react',
             'javascript-vue',
             'node-express',
@@ -148,7 +153,10 @@ class NoGroupsHandler extends Component<Props, State> {
             'python',
             'python-fastapi',
             'python-flask',
+            'react-native',
             'ruby',
+            'ruby-rails',
+            'unity',
           ]
         : []),
     ];

commit 38c15efc182b2aee53e3fe97708b0558f665efe7

fix(empty-states): Fix empty state style (#74051)
this pr fixes a styling bug introduced in
https://github.com/getsentry/sentry/pull/73771 with regards to flex box.

static/app/components/updatedEmptyState.tsx
@@ -409,8 +409,4 @@ const CodeSnippetWrapper = styled('div')`
 
 const DescriptionWrapper = styled('div')`
   margin-bottom: ${space(1)};
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  gap: ${space(0.5)};
 `;