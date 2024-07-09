d5c7de52f4b51825ebde15eedc4ec11d5a497d73
Ryan Skonnord <ryan.skonnord@sentry.io>
2024-07-09 15:52:07 -0700

    Add attr for whether we want to check the entire body

--- src/sentry/integrations/slack/utils/errors.py
+++ src/sentry/integrations/slack/utils/errors.py
@@ -9,12 +9,13 @@ _logger = logging.getLogger(__name__)
 @dataclass(frozen=True, eq=True)
 class SlackSdkErrorCategory:
     message: str
+    check_body: bool = False
 
 
 SLACK_SDK_ERROR_CATEGORIES = (
     RATE_LIMITED := SlackSdkErrorCategory("ratelimited"),
     CHANNEL_NOT_FOUND := SlackSdkErrorCategory("channel_not_found"),
-    EXPIRED_URL := SlackSdkErrorCategory("Expired url"),
+    EXPIRED_URL := SlackSdkErrorCategory("Expired url", check_body=True),
 )
 
 _CATEGORIES_BY_MESSAGE = {c.message: c for c in SLACK_SDK_ERROR_CATEGORIES}
@@ -32,7 +33,7 @@ def unpack_slack_api_error(exc: SlackApiError | SlackRequestError) -> SlackSdkEr
         return category
 
     for category in SLACK_SDK_ERROR_CATEGORIES:
-        if category.message in dump:
+        if category.check_body and category.message in dump:
             return category
 
     # Indicate that a new value needs to be added to SLACK_SDK_ERROR_CATEGORIES
