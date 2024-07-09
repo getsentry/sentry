f629aafc2cd1e77dfd89542002edf90ba8084b76
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-07-09 09:37:52 -0700

    Updates type on default_args

--- tests/sentry/runner/commands/test_createflag.py
+++ tests/sentry/runner/commands/test_createflag.py
@@ -6,7 +6,7 @@ from sentry.testutils.cases import CliTestCase
 
 class TestCreateFlag(CliTestCase):
     command = createflag
-    default_args = []
+    default_args: list[str] = []
 
     def convert_output_to_feature(self, output: str) -> Feature:
         split_output = output.split("=== GENERATED YAML ===\n")
