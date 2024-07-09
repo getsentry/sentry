3b1bf6f4096af650942e92658810a2e8b0d184cd
Ryan Skonnord <ryan.skonnord@sentry.io>
2024-07-09 15:47:06 -0700

    Fall back to inspecting the entire string dump

--- src/sentry/integrations/slack/utils/errors.py
+++ src/sentry/integrations/slack/utils/errors.py
@@ -21,9 +21,20 @@ _CATEGORIES_BY_MESSAGE = {c.message: c for c in SLACK_SDK_ERROR_CATEGORIES}
 
 
 def unpack_slack_api_error(exc: SlackApiError | SlackRequestError) -> SlackSdkErrorCategory | None:
-    message = (isinstance(exc, SlackApiError) and exc.response["error"]) or str(exc).split("\n")[0]
-    category = _CATEGORIES_BY_MESSAGE.get(message)
-    if category is None:
-        # Indicate that a new value needs to be added to SLACK_SDK_ERROR_CATEGORIES
-        _logger.warning("unrecognized_slack_api_message", extra={"slack_api_message": message})
-    return category
+    if isinstance(exc, SlackApiError):
+        category = _CATEGORIES_BY_MESSAGE.get(exc.response["error"])
+        if category:
+            return category
+
+    dump = str(exc)
+    category = _CATEGORIES_BY_MESSAGE.get(dump.split("\n")[0])
+    if category:
+        return category
+
+    for category in SLACK_SDK_ERROR_CATEGORIES:
+        if category.message in dump:
+            return category
+
+    # Indicate that a new value needs to be added to SLACK_SDK_ERROR_CATEGORIES
+    _logger.warning("unrecognized_slack_api_message", extra={"slack_api_message": dump})
+    return None
