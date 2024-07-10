88e589cc613cb9e915962716e5f629140c70bfe5
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-07-09 22:08:00 -0700

    figured out the serializer and get_attrs. works out pretty nicely in the end.

--- src/sentry/api/endpoints/project_templates.py
+++ src/sentry/api/endpoints/project_templates.py
@@ -4,6 +4,9 @@ from rest_framework.response import Response
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases.organization import OrganizationEndpoint
+from sentry.api.paginator import OffsetPaginator
+from sentry.api.serializers import serialize
+from sentry.api.serializers.models.project_template import ProjectTemplateSerializer
 from sentry.models.organization import Organization
 from sentry.models.projecttemplate import ProjectTemplate
 
@@ -16,31 +19,19 @@ class OrganizationProjectTemplatesIndexEndpoint(OrganizationEndpoint):
 
     def get(self, request: Request, organization: Organization) -> Response:
         """
-        List your Project Templates
-        ``````````````````
-        Return a list of project templates available to the authenticated
-        session in a region.
+        List of Project Templates, does not include the options for the project template.
 
-        :auth: required
+        Return a list of project templates available to the authenticated user.
         """
+
+        # TODO verify that this will autenticate the user to the organization,
+        # otherwise, add a filter to ensure the user is in the organization or is a superuser.
         queryset = ProjectTemplate.objects.filter(organization=organization)
 
-        # TODO - Use self.paginate() to handle pagination
-        # TODO - Create a serializer for the ProjectTemplate model
-        # TODO - Create a serializer for the ProjectTemplateOption model
-
-        return Response(
-            {
-                "id": project_template.id,
-                "name": project_template.name,
-                "organization": project_template.organization.slug,
-            }
-            for project_template in queryset
+        return self.paginate(
+            request=request,
+            queryset=queryset,
+            order_by="name",
+            on_results=lambda x: serialize(x, request.user, ProjectTemplateSerializer()),
+            paginator_cls=OffsetPaginator,
         )
-
-        # return self.paginate(
-        #     request=request,
-        #     queryset=queryset,
-        #     order_by="name",
-        #     on_results=lambda x: serialize(x, request.user),
-        # )
---
+++ src/sentry/api/serializers/models/project_template.py
@@ -0,0 +1,59 @@
+from collections import defaultdict
+from collections.abc import Iterable, Mapping, Sequence
+from datetime import datetime
+from typing import Any, TypedDict
+
+from sentry.api.serializers import Serializer, register
+from sentry.models.options.project_template_option import TProjectOptions
+from sentry.models.projecttemplate import ProjectTemplate
+from sentry.models.user import User
+
+
+class SerializedProjectTemplate(TypedDict, total=False):
+    id: int
+    name: str
+    createdAt: datetime | None
+    updatedAt: datetime | None
+    options: TProjectOptions | None
+
+
+@register(ProjectTemplate)
+class ProjectTemplateSerializer(Serializer):
+    """
+    This is used to serialize the project template model
+    """
+
+    def __init__(self, expand: Iterable[str] | None = None) -> None:
+        self.expand = expand
+
+    def _expand(self, key: str) -> bool:
+        return self.expand is not None and key in self.expand
+
+    def get_attrs(self, item_list: Sequence[ProjectTemplate], user: User, **kwargs: Any):
+        attrs = super().get_attrs(item_list, user, **kwargs)
+        all_attrs: dict[ProjectTemplate, dict[str, Any]] = defaultdict(dict)
+
+        if self._expand("options"):
+            for template in item_list:
+                all_attrs[template]["options"] = template.get_options()
+
+                # Merge the attrs from the parent with the options
+                for key in attrs.get(template, {}).keys():
+                    all_attrs[template][key] = attrs.get(template, {}).get(key)
+
+        return all_attrs
+
+    def serialize(
+        self, obj: ProjectTemplate, attrs: Mapping[str, Any], user: User, **kwargs: Any
+    ) -> SerializedProjectTemplate:
+        response: SerializedProjectTemplate = {
+            "id": obj.id,
+            "name": obj.name,
+            "createdAt": obj.date_added,
+            "updatedAt": obj.date_updated,
+        }
+
+        if (options := attrs.get("options")) is not None:
+            response["options"] = options
+
+        return response
