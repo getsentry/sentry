6d3e34d87b793fb507b4e3b886f028045c171f3e
JonasBa <jonas@badalic.com>
2024-07-10 12:07:10 -0400

    ref: remove queue metadata and empty type

--- static/app/utils/profiling/profile/continuousProfile.tsx
+++ static/app/utils/profiling/profile/continuousProfile.tsx
@@ -157,30 +157,17 @@ export class ContinuousProfile extends Profile {
   }
 }
 
-const COCOA_MAIN_THREAD = 'com.apple.main-thread';
 function getThreadData(profile: Profiling.SentryContinousProfileChunk): {
   threadId: number;
   threadName: string;
 } {
-  const {samples, queue_metadata = {}, thread_metadata = {}} = profile.profile;
+  const {samples, thread_metadata = {}} = profile.profile;
   const sample = samples[0];
   const threadId = parseInt(sample.thread_id, 10);
   const threadName = thread_metadata?.[threadId]?.name;
 
   if (threadName) {
     return {threadId, threadName};
-  }
-
-  // cocoa has a queue address that we fall back to to try to get a thread name
-  // is this the only platform string to check for?
-  if (profile.platform === 'cocoa') {
-    const queueName =
-      sample.queue_address && queue_metadata?.[sample.queue_address]?.label;
-
-    // if a queue has the main thread name, we discard it
-    if (queueName && queueName !== COCOA_MAIN_THREAD) {
-      return {threadId, threadName: queueName};
-    }
   }
 
   return {threadId, threadName: ''};
