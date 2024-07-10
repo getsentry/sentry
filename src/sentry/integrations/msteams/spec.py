fb8aa21d56e386a20133aaf6d66d6f41a8cba67e
Ryan Skonnord <ryan.skonnord@sentry.io>
2024-07-10 04:23:15 -0700

    Fix imports

--- src/sentry/integrations/msteams/spec.py
+++ src/sentry/integrations/msteams/spec.py
@@ -1,21 +1,21 @@
 from sentry import analytics
 from sentry.integrations.base import IntegrationProvider
 from sentry.integrations.messaging import MessagingIdentityLinkViewSet, MessagingIntegrationSpec
-from sentry.integrations.msteams import MsTeamsIntegrationProvider
-from sentry.integrations.msteams.actions.notification import MsTeamsNotifyServiceAction
-from sentry.integrations.msteams.analytics import MSTeamsIntegrationNotificationSent
-from sentry.integrations.msteams.link_identity import MsTeamsLinkIdentityView
-from sentry.integrations.msteams.unlink_identity import MsTeamsUnlinkIdentityView
 from sentry.rules.actions import IntegrationEventAction
 
 
 class MsTeamsMessagingSpec(MessagingIntegrationSpec):
     @property
     def integration_provider(self) -> type[IntegrationProvider]:
+        from sentry.integrations.msteams import MsTeamsIntegrationProvider
+
         return MsTeamsIntegrationProvider
 
     @property
     def identity_view_set(self) -> MessagingIdentityLinkViewSet:
+        from sentry.integrations.msteams.link_identity import MsTeamsLinkIdentityView
+        from sentry.integrations.msteams.unlink_identity import MsTeamsUnlinkIdentityView
+
         return MessagingIdentityLinkViewSet(
             link_personal_identity=MsTeamsLinkIdentityView,
             unlink_personal_identity=MsTeamsUnlinkIdentityView,
@@ -23,8 +23,12 @@ class MsTeamsMessagingSpec(MessagingIntegrationSpec):
 
     @property
     def notify_service_action(self) -> type[IntegrationEventAction] | None:
+        from sentry.integrations.msteams.actions.notification import MsTeamsNotifyServiceAction
+
         return MsTeamsNotifyServiceAction
 
     @property
     def notification_sent(self) -> type[analytics.Event] | None:
+        from sentry.integrations.msteams.analytics import MSTeamsIntegrationNotificationSent
+
         return MSTeamsIntegrationNotificationSent
--- src/sentry/integrations/slack/spec.py
+++ src/sentry/integrations/slack/spec.py
@@ -1,23 +1,23 @@
 from sentry import analytics
 from sentry.integrations.base import IntegrationProvider
 from sentry.integrations.messaging import MessagingIdentityLinkViewSet, MessagingIntegrationSpec
-from sentry.integrations.slack.actions.notification import SlackNotifyServiceAction
-from sentry.integrations.slack.analytics import SlackIntegrationNotificationSent
-from sentry.integrations.slack.integration import SlackIntegrationProvider
-from sentry.integrations.slack.views.link_identity import SlackLinkIdentityView
-from sentry.integrations.slack.views.link_team import SlackLinkTeamView
-from sentry.integrations.slack.views.unlink_identity import SlackUnlinkIdentityView
-from sentry.integrations.slack.views.unlink_team import SlackUnlinkTeamView
 from sentry.rules.actions import IntegrationEventAction
 
 
 class SlackMessagingSpec(MessagingIntegrationSpec):
     @property
     def integration_provider(self) -> type[IntegrationProvider]:
+        from sentry.integrations.slack.integration import SlackIntegrationProvider
+
         return SlackIntegrationProvider
 
     @property
     def identity_view_set(self) -> MessagingIdentityLinkViewSet:
+        from sentry.integrations.slack.views.link_identity import SlackLinkIdentityView
+        from sentry.integrations.slack.views.link_team import SlackLinkTeamView
+        from sentry.integrations.slack.views.unlink_identity import SlackUnlinkIdentityView
+        from sentry.integrations.slack.views.unlink_team import SlackUnlinkTeamView
+
         return MessagingIdentityLinkViewSet(
             link_personal_identity=SlackLinkIdentityView,
             unlink_personal_identity=SlackUnlinkIdentityView,
@@ -27,8 +27,12 @@ class SlackMessagingSpec(MessagingIntegrationSpec):
 
     @property
     def notify_service_action(self) -> type[IntegrationEventAction] | None:
+        from sentry.integrations.slack.actions.notification import SlackNotifyServiceAction
+
         return SlackNotifyServiceAction
 
     @property
     def notification_sent(self) -> type[analytics.Event] | None:
+        from sentry.integrations.slack.analytics import SlackIntegrationNotificationSent
+
         return SlackIntegrationNotificationSent
