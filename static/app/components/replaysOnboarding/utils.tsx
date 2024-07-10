7e778b246d28c59f22cabaabbfe464aebac14fd4
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-07-10 10:32:40 -0700

    ref(replay): clean up references to old onboarding docs (#74025)
    we've migrated all replay docs over to the new structure for about half
    a year now. let's get rid of references to the old docs in the replay
    onboarding.

--- static/app/components/replaysOnboarding/utils.tsx
+++ static/app/components/replaysOnboarding/utils.tsx
@@ -1,19 +1,6 @@
-import {replayFrontendPlatforms, replayPlatforms} from 'sentry/data/platformCategories';
+import {replayFrontendPlatforms} from 'sentry/data/platformCategories';
 import platforms from 'sentry/data/platforms';
-import type {PlatformIntegration, PlatformKey} from 'sentry/types/project';
-
-export function generateDocKeys(platform: PlatformKey): string[] {
-  const platformKey = platform.startsWith('javascript')
-    ? platform
-    : 'javascript-' + platform;
-  return ['1-install', '2-configure'].map(
-    key => `${platformKey}-replay-onboarding-${key}`
-  );
-}
-
-export function isPlatformSupported(platform: undefined | PlatformIntegration) {
-  return platform?.id ? replayPlatforms.includes(platform?.id) : false;
-}
+import type {PlatformIntegration} from 'sentry/types/project';
 
 export const replayJsFrameworkOptions: PlatformIntegration[] = platforms.filter(p =>
   replayFrontendPlatforms.includes(p.id)
