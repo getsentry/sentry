7a8b80823e1c55c32df11eca7df7bc0125fefebb
Mia Hsu <ameliahsu@gmail.com>
2024-07-10 16:21:20 -0700

    import class instead of method L

--- src/sentry/api/endpoints/api_tokens.py
+++ src/sentry/api/endpoints/api_tokens.py
@@ -24,26 +24,6 @@ from sentry.types.token import AuthTokenType
 ALLOWED_FIELDS = ["name", "tokenId"]
 
 
-def _get_appropriate_user_id(request: Request) -> int:
-    """
-    Gets the user id to use for the request, based on what the current state of the request is.
-    If the request is made by a superuser, then they are allowed to act on behalf of other user's data.
-    Therefore, when GET or DELETE endpoints are invoked by the superuser, we may utilize a provided user_id.
-
-    The user_id to use comes from the GET or BODY parameter based on the request type.
-    For GET endpoints, the GET dict is used.
-    For all others, the DATA dict is used.
-    """
-    # Get the user id for the user that made the current request as a baseline default
-    user_id = request.user.id
-    if has_elevated_mode(request):
-        datastore = request.GET if request.GET else request.data
-        # If a userId override is not found, use the id for the user who made the request
-        user_id = int(datastore.get("userId", user_id))
-
-    return user_id
-
-
 class ApiTokenSerializer(serializers.Serializer):
     name = CharField(max_length=255, allow_blank=True, required=False)
     scopes = MultipleChoiceField(required=True, choices=settings.SENTRY_SCOPES)
