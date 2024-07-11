2891dc576d1a3b9c1b0840dd5a95c826b583f1d5
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-07-11 16:27:36 -0700

    Regenerate migration

--- tests/sentry/migrations/test_0738_backfill_group_info_to_group_attributes.py
+++ tests/sentry/migrations/test_0738_backfill_group_info_to_group_attributes.py
@@ -39,8 +39,8 @@ def run_test(expected_groups):
 
 
 class TestBackfillGroupAttributes(SnubaTestCase, TestMigrations):
-    migrate_from = "0735_sunset_appstore_connect_integration"
-    migrate_to = "0736_backfill_group_info_to_group_attributes"
+    migrate_from = "0737_add_discover_saved_query_dataset_source"
+    migrate_to = "0738_backfill_group_info_to_group_attributes"
 
     def setup_initial_state(self):
         self.group = self.create_group(priority=PriorityLevel.HIGH)
@@ -54,8 +54,8 @@ class TestBackfillGroupAttributes(SnubaTestCase, TestMigrations):
 
 
 class TestBackfillGroupAttributesRetry(SnubaTestCase, TestMigrations):
-    migrate_from = "0735_sunset_appstore_connect_integration"
-    migrate_to = "0736_backfill_group_info_to_group_attributes"
+    migrate_from = "0737_add_discover_saved_query_dataset_source"
+    migrate_to = "0738_backfill_group_info_to_group_attributes"
 
     def setup_initial_state(self):
         self.group = self.create_group(priority=PriorityLevel.HIGH)
