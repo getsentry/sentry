fb065995fa17e6db2cde97d433ca9aaa15931a80
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-07-11 08:38:06 +0200

    fix(alerts): Fix display of invalid search key (#74059)

--- static/app/views/insights/common/views/spanSummaryPage/sampleList/index.tsx
+++ static/app/views/insights/common/views/spanSummaryPage/sampleList/index.tsx
@@ -226,7 +226,6 @@ export function SampleList({
             onSearch={handleSearch}
             placeholder={t('Search for span attributes')}
             organization={organization}
-            metricAlert={false}
             supportedTags={supportedTags}
             dataset={DiscoverDatasets.SPANS_INDEXED}
             projectIds={selection.projects}
