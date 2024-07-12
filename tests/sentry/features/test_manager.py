feb78afc535c9110f0bb93b3eb627e26106c7947
Mark Story <mark@mark-story.com>
2024-07-12 17:47:40 -0400

    feat(features) Allow features to be excluded from API results (#74200)
    We have many features that are not used in the UI code. Those features
    could be omitted from API responses to save time checking the features,
    and save response bloat.

--- tests/sentry/features/test_manager.py
+++ tests/sentry/features/test_manager.py
@@ -74,6 +74,27 @@ class FeatureManagerTest(TestCase):
             "projects:feature3",
         }
 
+    def test_feature_registry_api_expose(self):
+        manager = features.FeatureManager()
+        assert manager.all() == {}
+
+        manager.add("organizations:feature1", OrganizationFeature)
+        manager.add("organizations:feature2", OrganizationFeature, api_expose=True)
+        manager.add("organizations:feature3", OrganizationFeature, api_expose=False)
+        exposed = {"organizations:feature1", "organizations:feature2"}
+        hidden = {
+            "organizations:feature3",
+        }
+        assert set(manager.all(OrganizationFeature).keys()) == exposed | hidden
+        assert (
+            set(manager.all(feature_type=OrganizationFeature, api_expose_only=True).keys())
+            == exposed
+        )
+        assert (
+            set(manager.all(feature_type=OrganizationFeature, api_expose_only=False).keys())
+            == exposed | hidden
+        )
+
     def test_feature_register_default(self):
         manager = features.FeatureManager()
         manager.add("organizations:red-paint", OrganizationFeature, default=False)
