63ff5aeb6929c81b161fc0408e4d254f95b02a6f
Sebastian Zivota <loewenheim@mailbox.org>
2024-07-15 09:53:14 +0200

    Delete unused functions

--- src/sentry/lang/java/plugin.py
+++ src/sentry/lang/java/plugin.py
@@ -4,7 +4,7 @@ from collections.abc import Mapping, Sequence
 from typing import Any
 
 from sentry.lang.java.processing import deobfuscate_exception_value
-from sentry.lang.java.utils import deobfuscate_view_hierarchy, has_proguard_file
+from sentry.lang.java.utils import has_proguard_file
 from sentry.plugins.base.v2 import EventPreprocessor, Plugin2
 
 
