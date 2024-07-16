ece4377001dfb9583247df0e20f38c27e338b3a4
Mia Hsu <ameliahsu@gmail.com>
2024-07-16 14:13:52 -0700

    oops

--- src/sentry/api/endpoints/api_tokens.py
+++ src/sentry/api/endpoints/api_tokens.py
@@ -43,11 +43,8 @@ def get_appropriate_user_id(request: Request) -> int:
     user_id = request.user.id
     if has_elevated_mode(request):
         datastore = request.GET if request.GET else request.data
-        try:
-            # If a userId override is not found, use the id for the user who made the request
-            user_id = int(datastore.get("userId", user_id))
-        except ValueError:
-            return Response("Invalid User ID", status=400)
+        # If a userId override is not found, use the id for the user who made the request
+        user_id = int(datastore.get("userId", user_id))
 
     return user_id
 
