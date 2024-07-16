308d3422c2b4523b50e1825712f88368284b73fc
Colleen O'Rourke <colleen@sentry.io>
2024-07-16 10:42:31 -0700

    make it run post deploy

--- src/sentry/migrations/0742_backfill_alertrule_detection_type.py
+++ src/sentry/migrations/0742_backfill_alertrule_detection_type.py
@@ -42,7 +42,7 @@ class Migration(CheckedMigration):
     #   is a schema change, it's completely safe to run the operation after the code has deployed.
     # Once deployed, run these manually via: https://develop.sentry.dev/database-migrations/#migration-deployment
 
-    is_post_deployment = False
+    is_post_deployment = True
 
     dependencies = [
         ("sentry", "0741_metric_alert_anomaly_detection"),
