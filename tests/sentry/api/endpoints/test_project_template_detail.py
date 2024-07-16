952f523f8097fa4b6874eeaed6d64dfbfe7ddb6c
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-07-15 21:32:10 -0700

    Create the delete endpoint

--- tests/sentry/api/endpoints/test_project_template_detail.py
+++ tests/sentry/api/endpoints/test_project_template_detail.py
@@ -1,6 +1,9 @@
+import pytest
 from rest_framework.exceptions import ErrorDetail
 
 from sentry.api.endpoints.project_templates_index import PROJECT_TEMPLATE_FEATURE_FLAG
+from sentry.models.options.project_template_option import ProjectTemplateOption
+from sentry.models.projecttemplate import ProjectTemplate
 from sentry.testutils.cases import APITestCase
 from sentry.testutils.helpers import with_feature
 
@@ -63,3 +66,53 @@ class ProjectTemplateDetailTest(APITestCase):
         # Ensure this errors with 403, as the user does not have access to the organization
         response = self.get_error_response(org_two.id, project_template.id, status_code=403)
         assert response.status_code == 403
+
+
+class ProjectTemplateDetailDeleteTest(APITestCase):
+    endpoint = "sentry-api-0-organization-project-template-detail"
+    method = "delete"
+
+    def setUp(self):
+        super().setUp()
+        self.user = self.create_user()
+        self.organization = self.create_organization(owner=self.user)
+        self.team = self.create_team()
+
+        self.login_as(self.user)
+        self.project_template = self.create_project_template(organization=self.organization)
+
+    def test_delete__no_feature(self):
+        response = self.get_error_response(
+            self.organization.id, self.project_template.id, status_code=404
+        )
+        assert response.status_code == 404
+
+    @with_feature(PROJECT_TEMPLATE_FEATURE_FLAG)
+    def test_delete(self):
+        template_id = self.project_template.id
+        response = self.get_success_response(self.organization.id, template_id, status_code=204)
+        assert response.status_code == 204
+
+        with pytest.raises(ProjectTemplate.DoesNotExist):
+            ProjectTemplate.objects.get(id=template_id)
+
+    @with_feature(PROJECT_TEMPLATE_FEATURE_FLAG)
+    def test_delete__with_options(self):
+        template_id = self.project_template.id
+        self.project_template.options.create(
+            project_template=self.project_template, key="sentry:release_track", value="test"
+        )
+        self.project_template.options.create(
+            project_template=self.project_template, key="sentry:another_example", value="test"
+        )
+
+        # Ensure the options are created
+        assert ProjectTemplateOption.objects.filter(project_template_id=template_id).count() == 2
+
+        response = self.get_success_response(self.organization.id, template_id, status_code=204)
+        assert response.status_code == 204
+
+        # Ensure data is deleted
+        with pytest.raises(ProjectTemplate.DoesNotExist):
+            ProjectTemplate.objects.get(id=template_id)
+        assert ProjectTemplateOption.objects.filter(project_template_id=template_id).count() == 0
