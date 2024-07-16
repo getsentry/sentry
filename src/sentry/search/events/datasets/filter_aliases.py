0de9c0f87b6cd3a5cade3fb139c01f0cb3b8d7eb
Simon Hellmayr <simon.hellmayr@sentry.io>
2024-07-16 10:49:15 +0200

    fix(metrics): enable MetricsSummaries search by project

--- src/sentry/search/events/datasets/filter_aliases.py
+++ src/sentry/search/events/datasets/filter_aliases.py
@@ -102,7 +102,7 @@ def project_slug_converter(
         # Create a new search filter with the correct values
         converted_filter = builder.convert_search_filter_to_condition(
             SearchFilter(
-                SearchKey("project.id"),
+                SearchKey("project_id"),
                 search_filter.operator,
                 SearchValue(project_ids if search_filter.is_in_filter else project_ids[0]),
             )
