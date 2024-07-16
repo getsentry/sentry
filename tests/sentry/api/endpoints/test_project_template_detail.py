d90032e1b7c41641ec483662527fd4685a17e21b
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-07-16 10:54:05 -0700

    Add DELETE endpoint for project templates (#74331)
    # Description
    [Tech
    Spec](https://www.notion.so/sentry/Service-Settings-Templates-ca6fdb38b15f4a6db61d319bc342b86c?pvs=4#10e53faf34b649bfb393448e997cc1bf)
    
    Adds ability to remove project templates by id through the api. `DELETE`
    to `/api/0/organizations/:slug_or_id/project_templates/:id/`
    - Will remove all options as well because of the cascade on delete,
    wrote a test to ensure it's working as expected.
    - [ ] TODO update the tests with the base once the POST PR is merged

--- tests/sentry/api/endpoints/test_project_template_detail.py
+++ tests/sentry/api/endpoints/test_project_template_detail.py
@@ -1,24 +1,21 @@
+import pytest
 from rest_framework.exceptions import ErrorDetail
 
 from sentry.api.endpoints.project_templates_index import PROJECT_TEMPLATE_FEATURE_FLAG
-from sentry.testutils.cases import APITestCase
+from sentry.models.options.project_template_option import ProjectTemplateOption
+from sentry.models.projecttemplate import ProjectTemplate
 from sentry.testutils.helpers import with_feature
 
+from .test_project_templates_index import ProjectTemplateAPIBase
 
-class ProjectTemplateDetailTest(APITestCase):
+
+class ProjectTemplateDetailTest(ProjectTemplateAPIBase):
     endpoint = "sentry-api-0-organization-project-template-detail"
 
-    def setUp(self):
-        super().setUp()
-        self.user = self.create_user()
-        self.org = self.create_organization()
-        self.team = self.create_team(organization=self.org, members=[self.user])
-
-        self.login_as(self.user)
-        self.project_template = self.create_project_template(organization=self.org)
-
     def test_get__no_feature(self):
-        response = self.get_error_response(self.org.id, self.project_template.id, status_code=404)
+        response = self.get_error_response(
+            self.organization.id, self.project_template.id, status_code=404
+        )
         assert response.status_code == 404
 
     @with_feature(PROJECT_TEMPLATE_FEATURE_FLAG)
@@ -26,14 +23,14 @@ class ProjectTemplateDetailTest(APITestCase):
         self.project_template.options.create(
             project_template=self.project_template, key="sentry:release_track", value="test"
         )
-        response = self.get_success_response(self.org.id, self.project_template.id)
+        response = self.get_success_response(self.organization.id, self.project_template.id)
 
         assert response.data["name"] == self.project_template.name
         assert response.data["options"] == {"sentry:release_track": "test"}
 
     @with_feature(PROJECT_TEMPLATE_FEATURE_FLAG)
     def test_get__not_found(self):
-        response = self.get_error_response(self.org.id, 100, status_code=404)
+        response = self.get_error_response(self.organization.id, 100, status_code=404)
         assert response.status_code == 404
         assert response.data == {
             "detail": ErrorDetail(
@@ -46,7 +43,9 @@ class ProjectTemplateDetailTest(APITestCase):
         org_two = self.create_organization()
         project_template = self.create_project_template(organization=org_two)
 
-        response = self.get_error_response(self.org.id, project_template.id, status_code=404)
+        response = self.get_error_response(
+            self.organization.id, project_template.id, status_code=404
+        )
         assert response.status_code == 404
         assert response.data == {
             "detail": ErrorDetail(
@@ -62,4 +61,56 @@ class ProjectTemplateDetailTest(APITestCase):
 
         # Ensure this errors with 403, as the user does not have access to the organization
         response = self.get_error_response(org_two.id, project_template.id, status_code=403)
+        assert response.status_code == 403
+
+
+class ProjectTemplateDetailDeleteTest(ProjectTemplateAPIBase):
+    endpoint = "sentry-api-0-organization-project-template-detail"
+    method = "delete"
+
+    def test_delete__no_feature(self):
+        response = self.get_error_response(
+            self.organization.id, self.project_template.id, status_code=404
+        )
+        assert response.status_code == 404
+
+    @with_feature(PROJECT_TEMPLATE_FEATURE_FLAG)
+    def test_delete(self):
+        template_id = self.project_template.id
+        response = self.get_success_response(self.organization.id, template_id, status_code=204)
+        assert response.status_code == 204
+
+        with pytest.raises(ProjectTemplate.DoesNotExist):
+            ProjectTemplate.objects.get(id=template_id)
+
+    @with_feature(PROJECT_TEMPLATE_FEATURE_FLAG)
+    def test_delete__with_options(self):
+        template_id = self.project_template.id
+        self.project_template.options.create(
+            project_template=self.project_template, key="sentry:release_track", value="test"
+        )
+        self.project_template.options.create(
+            project_template=self.project_template, key="sentry:another_example", value="test"
+        )
+
+        # Ensure the options are created
+        assert ProjectTemplateOption.objects.filter(project_template_id=template_id).count() == 2
+
+        response = self.get_success_response(self.organization.id, template_id, status_code=204)
+        assert response.status_code == 204
+
+        # Ensure data is deleted
+        with pytest.raises(ProjectTemplate.DoesNotExist):
+            ProjectTemplate.objects.get(id=template_id)
+        assert ProjectTemplateOption.objects.filter(project_template_id=template_id).count() == 0
+
+    @with_feature(PROJECT_TEMPLATE_FEATURE_FLAG)
+    def test_delete__as_member_without_permission(self):
+        user = self.create_user()
+        self.create_member(user=user, organization=self.organization, role="member")
+        self.login_as(user)
+
+        response = self.get_error_response(
+            self.organization.id, self.project_template.id, status_code=403
+        )
         assert response.status_code == 403
