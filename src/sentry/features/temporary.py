5dec71c362abdff41ece90688b9f820a07a4b6a0
Vjeran Grozdanic <vjerangrozdanic@sentry.io>
2024-07-16 12:00:21 +0200

    remove feature flag definition
    Signed-off-by: Vjeran Grozdanic <vjerangrozdanic@sentry.io>

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -92,8 +92,6 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:custom-metrics-extraction-rule", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE)
     # Enables UI for creation of custom metrics extraction rules
     manager.add("organizations:custom-metrics-extraction-rule-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE)
-    # Enables low cardinality limits on custom metrics for projects
-    manager.add("organizations:low-cardinality-limits-on-project", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE)
     # Enables metrics extrapolation feature
     manager.add("organizations:metrics-extrapolation", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE)
     # Enable the default alert at project creation to be the high priority alert
