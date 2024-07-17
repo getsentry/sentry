da92406a4f2bba6195c6b7def058b6d13eb30093
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-07-17 16:00:30 -0700

    ref(devtoolbar): move code from getsentry

---
+++ static/app/components/devtoolbar/hooks/useConfiguration.tsx
@@ -0,0 +1,26 @@
+import {createContext, useContext} from 'react';
+
+import type {Configuration} from '../types';
+
+const context = createContext<Configuration>({
+  apiPrefix: '',
+  environment: ['production'],
+  organizationSlug: '',
+  placement: 'right-edge',
+  projectId: 0,
+  projectSlug: '',
+});
+
+export function ConfigurationContextProvider({
+  children,
+  config,
+}: {
+  children: React.ReactNode;
+  config: Configuration;
+}) {
+  return <context.Provider value={config}>{children}</context.Provider>;
+}
+
+export default function useConfiguration() {
+  return useContext(context);
+}
