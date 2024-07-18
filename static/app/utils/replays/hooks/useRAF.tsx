d22912bbe333ea989fdab82d1db64a3f3346b14d
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-07-18 15:54:09 -0700

    ref(useRAF): add an enabled option to useRAF so it can be turned off sometimes

--- static/app/utils/replays/hooks/useRAF.tsx
+++ static/app/utils/replays/hooks/useRAF.tsx
@@ -1,9 +1,16 @@
 import {useEffect} from 'react';
 
 // TODO: move into app/utils/*
-export default function useRAF(callback: () => unknown) {
+export default function useRAF(
+  callback: () => unknown,
+  opts?: {enabled: undefined | null | boolean}
+) {
+  const {enabled = true} = opts ?? {};
   useEffect(() => {
-    const timer = window.requestAnimationFrame(callback);
-    return () => window.cancelAnimationFrame(timer);
-  }, [callback]);
+    if (enabled) {
+      const timer = window.requestAnimationFrame(callback);
+      return () => window.cancelAnimationFrame(timer);
+    }
+    return () => {};
+  }, [callback, enabled]);
 }
