187f3b5620c899b9cfcc10e3595d25b9df0ed805
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2024-07-18 15:50:50 -0400

    widget query fixture

--- tests/js/fixtures/widgetQuery.ts
+++ tests/js/fixtures/widgetQuery.ts
@@ -3,9 +3,9 @@ import type {WidgetQuery} from 'sentry/views/dashboards/types';
 export function WidgetQueryFixture(params: Partial<WidgetQuery> = {}): WidgetQuery {
   return {
     name: '',
-    fields: ['title', 'count()', 'count_unique(user)', 'epm()', 'count()'],
+    fields: ['title', 'count()', 'count_unique(user)', 'epm()'],
     columns: ['title'],
-    aggregates: ['count()', 'count_unique(user)', 'epm()', 'count()'],
+    aggregates: ['count()', 'count_unique(user)', 'epm()'],
     conditions: 'tag:value',
     orderby: '',
     ...params,
