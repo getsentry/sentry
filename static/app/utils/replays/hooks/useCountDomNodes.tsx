af73b5ade6e5d0898091e1f1a043233548b79776
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-07-18 23:49:12 -0700

    :recycle: some types

--- static/app/utils/replays/hooks/useCountDomNodes.tsx
+++ static/app/utils/replays/hooks/useCountDomNodes.tsx
@@ -1,6 +1,15 @@
 import {useQuery} from 'sentry/utils/queryClient';
 import type ReplayReader from 'sentry/utils/replays/replayReader';
 
+export type DomNodeChartDatapoint = {
+  added: number;
+  count: number;
+  endTimestampMs: number;
+  removed: number;
+  startTimestampMs: number;
+  timestampMs: number;
+};
+
 function countDomNodes({replay}: {replay: null | ReplayReader}) {
   return replay?.getCountDomNodes();
 }
