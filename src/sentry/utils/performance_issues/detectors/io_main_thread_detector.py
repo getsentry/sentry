0044525b4f6c99e48a5855150714629c9c1c83e2
Dhiogo Brustolin <dhiogorb@gmail.com>
2024-07-22 17:09:43 +0200

    using glob

--- src/sentry/utils/performance_issues/detectors/io_main_thread_detector.py
+++ src/sentry/utils/performance_issues/detectors/io_main_thread_detector.py
@@ -18,6 +18,7 @@ from sentry.lang.java.proguard import open_proguard_mapper
 from sentry.models.debugfile import ProjectDebugFile
 from sentry.models.organization import Organization
 from sentry.models.project import Project
+from sentry.utils.glob import glob_match
 
 from ..base import (
     DetectorType,
@@ -116,7 +117,7 @@ class FileIOMainThreadDetector(BaseIOMainThreadDetector):
 
     __slots__ = ("stored_problems",)
 
-    IGNORED_LIST = {".nib", ".plist", "kblayout_iphone.dat"}
+    IGNORED_LIST = {"*.nib", "*.plist", .dat"}
     SPAN_PREFIX = "file"
     type = DetectorType.FILE_IO_MAIN_THREAD
     settings_key = DetectorType.FILE_IO_MAIN_THREAD
@@ -192,8 +193,8 @@ class FileIOMainThreadDetector(BaseIOMainThreadDetector):
         data = span.get("data", {})
         if data is None:
             return False
-        file_path = data.get("file.path", "").lower()
-        if any(file_path.endswith(ext) for ext in self.IGNORED_LIST):
+        file_path = data.get("file.path", "")
+        if any(glob_match(file_path, ignored_pattern, ignore_case=True) for ignored_pattern in self.IGNORED_LIST):
             return False
         # doing is True since the value can be any type
         return data.get("blocked_main_thread", False) is True
