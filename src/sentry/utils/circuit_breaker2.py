186b0189b3597ccf3608df4dff3d68fdf5a4e298
Katie Byers <lobsterkatie@gmail.com>
2024-07-22 23:11:54 -0700

    add config class

--- src/sentry/utils/circuit_breaker2.py
+++ src/sentry/utils/circuit_breaker2.py
@@ -6,9 +6,31 @@ and get rid of the old one, at which point this can lose the `2`.
 """
 
 from enum import Enum
+from typing import NotRequired, TypedDict
 
 
 class CircuitBreakerState(Enum):
     OK = "circuit_okay"
     BROKEN = "circuit_broken"
     RECOVERY = "recovery"
+
+
+class CircuitBreakerConfig(TypedDict):
+    # The number of errors within the given time period necessary to trip the breaker
+    error_limit: int
+    # The time period, in seconds, over which we're tracking errors
+    error_limit_window: int
+    # How long, in seconds, to stay in the BROKEN state (blocking all requests) before entering the
+    # RECOVERY phase
+    broken_state_duration: int
+    # The number of errors within the given time period necessary to trip the breaker while in
+    # RECOVERY. Will be set automatically to 10% of `error_limit` if not provided.
+    recovery_error_limit: NotRequired[int]
+    # The length, in seconds, of each time bucket ("granule") used by the underlying rate limiter -
+    # effectively the resolution of the time window. Will be set automatically based on
+    # `error_limit_window` if not provided.
+    error_limit_window_granularity: NotRequired[int]
+    # How long, in seconds, to stay in the RECOVERY state (allowing requests but with a stricter
+    # error limit) before returning to normal operation. Will be set to twice `error_limit_window`
+    # if not provided.
+    recovery_duration: NotRequired[int]
