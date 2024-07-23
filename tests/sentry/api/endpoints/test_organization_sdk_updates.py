2e3bba2194baae42c9b38aec3e27b6a12de346a0
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-07-23 15:13:18 -0700

    Temp fix for deprecation warning dependent test

--- tests/sentry/api/endpoints/test_organization_sdk_updates.py
+++ tests/sentry/api/endpoints/test_organization_sdk_updates.py
@@ -2,6 +2,7 @@ from unittest import mock
 
 import pytest
 from django.urls import reverse
+from pydantic import PydanticDeprecatedSince20
 
 from sentry.sdk_updates import SdkIndexState
 from sentry.testutils.cases import APITestCase, SnubaTestCase
@@ -187,6 +188,12 @@ class OrganizationSdkUpdates(APITestCase, SnubaTestCase):
 
         update_suggestions = response.data
         assert len(update_suggestions) == 0
+
+        # TODO(Gabe): Temporary kludge to allow this to pass while pydantic
+        # deprecation warnings are active.
+        warninfo = [
+            info for info in warninfo if not isinstance(info.message, PydanticDeprecatedSince20)
+        ]
 
         # until it is turned into an error, we'll get a warning about parsing an invalid version
         (warning,) = warninfo
