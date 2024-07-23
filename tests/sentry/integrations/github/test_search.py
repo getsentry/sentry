e764d5b01e1474fa6647a0db50b656b219ad0abe
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-07-23 19:32:37 -0400

    ref: prevent clobbering create_integration in base class (#74809)
    fixes an error in mypy 1.11
    
    <!-- Describe your PR here. -->

--- tests/sentry/integrations/github/test_search.py
+++ tests/sentry/integrations/github/test_search.py
@@ -16,7 +16,7 @@ class GithubSearchTest(APITestCase):
     provider = "github"
     base_url = "https://api.github.com"
 
-    def create_integration(self):
+    def _create_integration(self):
         future = datetime.now() + timedelta(hours=1)
         return self.create_provider_integration(
             provider=self.provider,
@@ -32,7 +32,7 @@ class GithubSearchTest(APITestCase):
 
     def setUp(self):
         super().setUp()
-        self.integration = self.create_integration()
+        self.integration = self._create_integration()
         identity = Identity.objects.create(
             idp=self.create_identity_provider(type=self.provider),
             user=self.user,
