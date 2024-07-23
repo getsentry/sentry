d2bacc2245ad8579385e7516ae98b133fd7bd377
Vjeran Grozdanic <vjerangrozdanic@sentry.io>
2024-07-23 17:55:24 +0200

    typo fix
    Signed-off-by: Vjeran Grozdanic <vjerangrozdanic@sentry.io>

--- tests/sentry/db/postgres/lookups/test_array_element_contains.py
+++ tests/sentry/db/postgres/lookups/test_array_element_contains.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class ArrayElementContainsLookupTestModel(models.Model):
     id = models.AutoField(primary_key=True)
-    array_filed = DjangoArrayField(models.TextField(), null=True)
+    array_field = DjangoArrayField(models.TextField(), null=True)
 
     class Meta:
         app_label = "fixtures"
@@ -15,10 +15,10 @@ class ArrayElementContainsLookupTestModel(models.Model):
 def array_element_contains_db():
     ArrayElementContainsLookupTestModel.objects.bulk_create(
         [
-            ArrayElementContainsLookupTestModel(array_filed=["foo", "bar", "baz"]),
-            ArrayElementContainsLookupTestModel(array_filed=["foo", "bar"]),
-            ArrayElementContainsLookupTestModel(array_filed=[]),
-            ArrayElementContainsLookupTestModel(array_filed=None),
+            ArrayElementContainsLookupTestModel(array_field=["foo", "bar", "baz"]),
+            ArrayElementContainsLookupTestModel(array_field=["foo", "bar"]),
+            ArrayElementContainsLookupTestModel(array_field=[]),
+            ArrayElementContainsLookupTestModel(array_field=None),
         ]
     )
     yield
@@ -29,23 +29,23 @@ def array_element_contains_db():
 def test_basic_usage_for_array_field(array_element_contains_db):
     assert (
         ArrayElementContainsLookupTestModel.objects.filter(
-            array_filed__element_contains="foo"
+            array_field__element_contains="foo"
         ).count()
         == 2
     )
 
-    result = ArrayElementContainsLookupTestModel.objects.filter(array_filed__element_contains="baz")
+    result = ArrayElementContainsLookupTestModel.objects.filter(array_field__element_contains="baz")
     assert len(result) == 1
-    assert result[0].array_filed == ["foo", "bar", "baz"]
+    assert result[0].array_field == ["foo", "bar", "baz"]
 
     assert (
         ArrayElementContainsLookupTestModel.objects.filter(
-            array_filed__element_contains="qux"
+            array_field__element_contains="qux"
         ).count()
         == 0
     )
 
     assert (
-        ArrayElementContainsLookupTestModel.objects.filter(array_filed__element_contains="").count()
+        ArrayElementContainsLookupTestModel.objects.filter(array_field__element_contains="").count()
         == 2
     )  # only non empty arrays are considered, and it's elements are checked if they contain ''
