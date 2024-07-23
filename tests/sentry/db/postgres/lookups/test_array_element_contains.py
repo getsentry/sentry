ae8d65908ccc1b9053ded95a17b49ff10e49aad6
Vjeran Grozdanic <vjerangrozdanic@sentry.io>
2024-07-23 17:29:12 +0200

    test basic usage
    Signed-off-by: Vjeran Grozdanic <vjerangrozdanic@sentry.io>

--- tests/sentry/db/postgres/lookups/test_array_element_contains.py
+++ tests/sentry/db/postgres/lookups/test_array_element_contains.py
@@ -1,30 +1,51 @@
-from unittest.mock import Mock, patch
+import pytest
+from django.contrib.postgres.fields import ArrayField as DjangoArrayField
+from django.db import models
 
-from sentry.db.postgres.lookups.array_element_contains import ArrayElementContainsLookup
+
+class ArrayElementContainsLookupTestModel(models.Model):
+    id = models.AutoField(primary_key=True)
+    array_filed = DjangoArrayField(models.TextField(), null=True)
+
+    class Meta:
+        app_label = "fixtures"
 
 
-def test_as_sql_basic_usage():
-    with (
-        patch(
-            "sentry.db.postgres.lookups.array_element_contains.ArrayElementContainsLookup.process_lhs"
-        ) as mock_process_lhs,
-        patch(
-            "sentry.db.postgres.lookups.array_element_contains.ArrayElementContainsLookup.process_rhs"
-        ) as mock_process_rhs,
-    ):
-        lhs = "column_name"
-        rhs = "%s"
-        mock_process_lhs.return_value = (lhs, [])
-        mock_process_rhs.return_value = (rhs, ["value"])
+@pytest.fixture
+def array_element_contains_db():
+    ArrayElementContainsLookupTestModel.objects.bulk_create(
+        [
+            ArrayElementContainsLookupTestModel(array_filed=["foo", "bar", "baz"]),
+            ArrayElementContainsLookupTestModel(array_filed=["foo", "bar"]),
+            ArrayElementContainsLookupTestModel(array_filed=[]),
+            ArrayElementContainsLookupTestModel(array_filed=None),
+        ]
+    )
+    yield
+    ArrayElementContainsLookupTestModel.objects.all().delete()
 
-        lookup = ArrayElementContainsLookup(lhs, rhs)
-        sql, params = lookup.as_sql(Mock(), Mock())
 
-        assert (
-            sql
-            == """EXISTS (
-            SELECT * FROM UNNEST(column_name) AS elem
-            WHERE elem LIKE '%%' || %s || '%%'
-        )"""
-        )
-        assert params == ["value"]
+@pytest.mark.django_db
+def test_basic_usage_for_array_field(array_element_contains_db):
+    assert (
+        ArrayElementContainsLookupTestModel.objects.filter(
+            array_filed__element_contains="foo"
+        ).count()
+        == 2
+    )
+
+    result = ArrayElementContainsLookupTestModel.objects.filter(array_filed__element_contains="baz")
+    assert len(result) == 1
+    assert result[0].array_filed == ["foo", "bar", "baz"]
+
+    assert (
+        ArrayElementContainsLookupTestModel.objects.filter(
+            array_filed__element_contains="qux"
+        ).count()
+        == 0
+    )
+
+    assert (
+        ArrayElementContainsLookupTestModel.objects.filter(array_filed__element_contains="").count()
+        == 2
+    )  # only non empty arrays are considered, and it's elements are checked if they contain ''
