88655c24ec7cb71955542df3a35b4c31d2c1a3cd
Gabe Villalobos <GabeVillalobos@users.noreply.github.com>
2024-07-24 13:17:04 -0700

    fix(hybrid-cloud): Resubmission: adds defaults to provisioning model fields, lost_password_hash model (#74766) (#74883)

--- src/sentry/users/services/lost_password_hash/model.py
+++ src/sentry/users/services/lost_password_hash/model.py
@@ -3,7 +3,10 @@
 # in modules such as this one where hybrid cloud data models or service classes are
 # defined, because we want to reflect on type annotations and avoid forward references.
 
-import datetime
+from datetime import datetime
+
+from django.utils import timezone
+from pydantic import Field
 
 from sentry.hybridcloud.rpc import RpcModel
 from sentry.models.lostpasswordhash import LostPasswordHash
@@ -13,7 +16,7 @@ class RpcLostPasswordHash(RpcModel):
     id: int = -1
     user_id: int = -1
     hash: str = ""
-    date_added = datetime.datetime
+    date_added: datetime = Field(default_factory=timezone.now)
 
     def get_absolute_url(self, mode: str = "recover") -> str:
         return LostPasswordHash.get_lostpassword_url(self.user_id, self.hash, mode)
