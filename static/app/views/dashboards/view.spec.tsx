653b16d37c1a85f7d3654adba900c18a81906507
Nar Saynorath <nar.saynorath@sentry.io>
2024-07-24 13:35:40 -0400

    Finish test

--- static/app/views/dashboards/view.spec.tsx
+++ static/app/views/dashboards/view.spec.tsx
@@ -4,7 +4,7 @@ import {LocationFixture} from 'sentry-fixture/locationFixture';
 import {WidgetFixture} from 'sentry-fixture/widget';
 
 import {initializeOrg} from 'sentry-test/initializeOrg';
-import {render, screen, waitFor} from 'sentry-test/reactTestingLibrary';
+import {render, screen, userEvent, waitFor} from 'sentry-test/reactTestingLibrary';
 
 import {browserHistory} from 'sentry/utils/browserHistory';
 import usePageFilters from 'sentry/utils/usePageFilters';
@@ -14,6 +14,7 @@ import ViewEditDashboard from 'sentry/views/dashboards/view';
 jest.mock('sentry/utils/usePageFilters');
 
 describe('Dashboards > ViewEditDashboard', function () {
+  let location;
   let initialData = initializeOrg({
     organization: {
       features: ['dashboards-basic', 'dashboards-edit'],
@@ -80,7 +81,7 @@ describe('Dashboards > ViewEditDashboard', function () {
   });
 
   it('removes widget params from url and preserves selection params', async function () {
-    const location = {
+    location = {
       pathname: '/',
       query: {
         environment: 'canary',
@@ -135,77 +136,98 @@ describe('Dashboards > ViewEditDashboard', function () {
     );
   });
 
-  it('updates the table widget with the discover split decision', async function () {
-    initialData = initializeOrg({
-      organization: {
-        features: [
-          'dashboards-basic',
-          'dashboards-edit',
-          'performance-discover-dataset-selector',
-        ],
-      },
-    });
-    MockApiClient.addMockResponse({
-      url: `/organizations/${initialData.organization.slug}/dashboards/`,
-      statusCode: 200,
-      body: [DashboardFixture([], {id: '1', title: 'Custom Errors'})],
-    });
+  it.each(['error-events', 'transaction-like'])(
+    'updates the table widget with the discover split decision',
+    async function (splitDecision) {
+      initialData = initializeOrg({
+        organization: {
+          features: [
+            'dashboards-basic',
+            'dashboards-edit',
+            'performance-discover-dataset-selector',
+          ],
+        },
+      });
+      MockApiClient.addMockResponse({
+        url: `/organizations/${initialData.organization.slug}/dashboards/`,
+        statusCode: 200,
+        body: [DashboardFixture([], {id: '1', title: 'Custom Errors'})],
+      });
 
-    const widgets = [
-      WidgetFixture({
-        widgetType: WidgetType.DISCOVER,
-        id: '1',
-        title: 'Split to Errors Widget',
-        displayType: DisplayType.TABLE,
-      }),
-    ];
-    MockApiClient.addMockResponse({
-      url: `/organizations/${initialData.organization.slug}/dashboards/1/`,
-      statusCode: 200,
-      body: DashboardFixture(widgets, {
-        id: '1',
-        title: 'Custom Errors',
-        filters: {},
-      }),
-    });
-    const eventsMock = MockApiClient.addMockResponse({
-      url: `/organizations/${initialData.organization.slug}/events/`,
-      method: 'GET',
-      body: {
-        data: [],
-        meta: {discoverWidgetSplit: 'error-events'},
-      },
-    });
+      const widgets = [
+        WidgetFixture({
+          widgetType: WidgetType.DISCOVER,
+          id: '1',
+          title: 'Split Widget',
+          displayType: DisplayType.TABLE,
+        }),
+      ];
+      MockApiClient.addMockResponse({
+        url: `/organizations/${initialData.organization.slug}/dashboards/1/`,
+        statusCode: 200,
+        body: DashboardFixture(widgets, {
+          id: '1',
+          filters: {},
+        }),
+      });
+      const eventsMock = MockApiClient.addMockResponse({
+        url: `/organizations/${initialData.organization.slug}/events/`,
+        method: 'GET',
+        body: {
+          data: [],
+          meta: {discoverSplitDecision: splitDecision},
+        },
+      });
 
-    // Clear the query so we don't go into the edit state
-    const location = LocationFixture();
-    location.query = {};
+      const eventsMockPut = MockApiClient.addMockResponse({
+        url: `/organizations/${initialData.organization.slug}/dashboards/1/`,
+        statusCode: 200,
+        method: 'PUT',
+      });
 
-    // render the dashboard with it set up with a discover widget
-    render(
-      <ViewEditDashboard
-        location={location}
-        organization={initialData.organization}
-        router={initialData.router}
-        params={{
-          orgId: initialData.organization.slug,
-          dashboardId: '1',
-        }}
-        route={{}}
-        routes={[]}
-        routeParams={{}}
-      >
-        <Fragment />
-      </ViewEditDashboard>
-    );
+      // Clear the query so we don't go into the edit state
+      location = {...LocationFixture(), query: {}};
 
-    await waitFor(() => {
-      expect(eventsMock).toHaveBeenCalledTimes(1);
-    });
+      // render the dashboard with it set up with a discover widget
+      render(
+        <ViewEditDashboard
+          location={location}
+          organization={initialData.organization}
+          router={initialData.router}
+          params={{
+            orgId: initialData.organization.slug,
+            dashboardId: '1',
+          }}
+          route={{}}
+          routes={[]}
+          routeParams={{}}
+        >
+          <Fragment />
+        </ViewEditDashboard>,
+        {organization: initialData.organization}
+      );
 
-    // Check the state of the widget
-    // await userEvent.click(screen.getByRole('button', {name: 'Edit Dashboard'}));
-    // expect(await screen.findByText('Save and Finish')).toBeInTheDocument();
-    // await userEvent.click(screen.getByRole('button', {name: /duplicate widget/}));
-  });
+      await waitFor(() => {
+        expect(eventsMock).toHaveBeenCalledTimes(1);
+      });
+
+      // This is a hack to verify that the widget state inside dashboards was updated
+      // to the split decision because there is no user-facing way to verify this
+      await userEvent.click(screen.getByRole('button', {name: 'Edit Dashboard'}));
+      await userEvent.click(screen.getByRole('button', {name: 'Duplicate Widget'}));
+      await userEvent.click(screen.getByRole('button', {name: 'Save and Finish'}));
+
+      expect(eventsMockPut).toHaveBeenCalledWith(
+        '/organizations/org-slug/dashboards/1/',
+        expect.objectContaining({
+          data: expect.objectContaining({
+            widgets: [
+              expect.objectContaining({widgetType: splitDecision}),
+              expect.objectContaining({widgetType: splitDecision}),
+            ],
+          }),
+        })
+      );
+    }
+  );
 });
