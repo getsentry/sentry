c84a3e099e4dc75cb224d087f516e30ba5d96d2d
KevinL10 <kevinsliu0@gmail.com>
2024-07-25 15:57:12 -0400

    feat(insights): add release markings to fullscreen insight charts

--- static/app/views/insights/http/components/charts/durationChart.tsx
+++ static/app/views/insights/http/components/charts/durationChart.tsx
@@ -29,6 +29,8 @@ export function DurationChart({
 }: Props) {
   // TODO: This is duplicated from `DurationChart` in `SampleList`. Resolve the duplication
   const handleChartHighlight: EChartHighlightHandler = function (event) {
+    if (!event.batch) return;
+
     // TODO: Gross hack. Even though `scatterPlot` is a separate prop, it's just an array of `Series` that gets appended to the main series. To find the point that was hovered, we re-construct the correct series order. It would have been cleaner to just pass the scatter plot as its own, single series
     const allSeries = [...series, ...(scatterPlot ?? [])];
 
