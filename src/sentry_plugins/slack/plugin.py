2af25d2ad1a739968e9dcd2a2ce4efde66231096
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-07-25 09:45:47 -0400

    ref: unify signature of notify(...) (#74903)
    mypy 1.11 noticed these were incompatible
    
    <!-- Describe your PR here. -->

--- src/sentry_plugins/slack/plugin.py
+++ src/sentry_plugins/slack/plugin.py
@@ -3,7 +3,7 @@ from rest_framework import status
 from sentry import tagstore
 from sentry.integrations.base import FeatureDescription, IntegrationFeatures
 from sentry.integrations.slack.message_builder import LEVEL_TO_COLOR
-from sentry.plugins.base import Notification
+from sentry.plugins.base.structs import Notification
 from sentry.plugins.bases import notify
 from sentry.shared_integrations.exceptions import ApiError
 from sentry.utils import json
