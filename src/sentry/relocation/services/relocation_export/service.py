132fe23f7aafa337da60c0bce30d6c39b96256b3
Alex Zaslavsky <azaslavsky@users.noreply.github.com>
2024-07-25 16:05:42 -0700

    fix(relocation): Change relocation_export RPC type (#75017)
    Previously, we tried to send a raw `bytes` object. While pydantic allows
    this from a type checking perspective, it fails when we try to encode
    the data as a JSON payload for transport over the wire, which requires
    UTF-8 encoding.
    
    To get around this, we change the type of `encrypted_contents` to
    `list[int]`, then cast the `bytes` to/from a `list[int]` when
    alternating between sending over the wire and saving to a filestore.
    
    While a change to an RPC schema like this is generally unsafe, because
    this endpoint is behind an admin barrier and only being tested
    internally, I feel like it is safe to do so in this case.

--- src/sentry/relocation/services/relocation_export/service.py
+++ src/sentry/relocation/services/relocation_export/service.py
@@ -73,7 +73,9 @@ class RegionRelocationExportService(RpcService):
         requesting_region_name: str,
         replying_region_name: str,
         org_slug: str,
-        encrypted_contents: bytes,
+        # TODO(azaslavsky): finish transfer from `encrypted_contents` -> `encrypted_bytes`.
+        encrypted_contents: bytes | None,
+        encrypted_bytes: list[int] | None = None,
     ) -> None:
         """
         This method is responsible for asynchronously sending an already generated and locally-saved
@@ -130,7 +132,9 @@ class ControlRelocationExportService(RpcService):
         requesting_region_name: str,
         replying_region_name: str,
         org_slug: str,
-        encrypted_contents: bytes,
+        # TODO(azaslavsky): finish transfer from `encrypted_contents` -> `encrypted_bytes`.
+        encrypted_contents: bytes | None,
+        encrypted_bytes: list[int] | None = None,
     ) -> None:
         """
         This helper method is a proxy handler for the `reply_with_export` method, durably forwarding
