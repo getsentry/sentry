b5a2449b6c50a445b4a98bbdb6f72c3b59f45e6f
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-07-25 11:31:21 -0700

    fix(apidocs): fix typo in release API docs

--- src/sentry/apidocs/parameters.py
+++ src/sentry/apidocs/parameters.py
@@ -547,12 +547,9 @@ class SessionsParams:
         required=True,
         type=str,
         many=True,
-        description="""The list of fields to query.\n\nThe available fields are\n  - `sum(session)`\n  - `count_unique("
-                    "user)`\n  - `avg`, `p50`, `p75`, `p90`, `p95`, `p99`, `max` applied to `session.duration`. For "
-                    "example, `p99(session.duration)`. Session duration is [no longer being recorded]("
-                    "https://github.com/getsentry/sentry/discussions/42716) as of on Jan 12, 2023. Returned data may "
-                    "be incomplete.\n  - `crash_rate`, `crash_free_rate` applied to `user` or `session`. For example, "
-                    "`crash_free_rate(user)`""",
+        description="""The list of fields to query.\n\nThe available fields are\n  - `sum(session)`\n  - `count_unique(user)`\n  - `avg`, `p50`, `p75`, `p90`, `p95`, `p99`, `max` applied to `session.duration`. For
+                    example, `p99(session.duration)`. Session duration is [no longer being recorded](https://github.com/getsentry/sentry/discussions/42716) as of on Jan 12, 2023. Returned data may be incomplete.\n  - `crash_rate`, `crash_free_rate` applied to `user` or `session`. For example,
+                    `crash_free_rate(user)`""",
     )
     INTERVAL = OpenApiParameter(
         name="interval",
