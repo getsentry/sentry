6a7f32581437a711fa1f00c74be6010513ffad29
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2024-07-26 15:21:41 -0700

    feat(draggableTabs): Add new draggable tabs component  (#75025)
    This PR creates the `<DraggableTabs/>` component along with many
    supporting components. It utilizes the new tab variant [recently
    introduced](https://github.com/getsentry/sentry/pull/74867), and adds
    support for reordering them via dragging (implemented w/ Framer motion).
    
    This PR contains a subset of changes from a previous
    [PR](https://github.com/getsentry/sentry/pull/73543) that contained a
    lot of new changes to the draggable tabs component, and it will be the
    first in a series of PRs that break up that PR into smaller chunks. This
    PR _should_ be the largest of them all, since it creates all of the
    draggable tabs components. I have tried to pare the old PR down to
    _only_ contain the changes necessary to get the draggable tabs to be
    actually draggable (using Framer motion), but please feel free to
    comment on anything that looks like it may be an artifact of these
    changes.
    
    Known issues in this current implementation that will be fixed in
    subsequent PRs:
    
    - There are no tests for this component 
    - Overflow tabs interact with the tab dividers very weirdly 
    - Tab names wrap very weirdly 
    
    
    https://github.com/user-attachments/assets/329fe197-b6af-47af-8faa-4d34ef65181f

---
+++ static/app/components/draggableTabs/draggableTab.tsx
@@ -0,0 +1,68 @@
+import type React from 'react';
+import {forwardRef} from 'react';
+import styled from '@emotion/styled';
+import type {AriaTabProps} from '@react-aria/tabs';
+import {useTab} from '@react-aria/tabs';
+import {useObjectRef} from '@react-aria/utils';
+import type {TabListState} from '@react-stately/tabs';
+import type {Node, Orientation} from '@react-types/shared';
+
+import {BaseTab} from 'sentry/components/tabs/tab';
+
+interface DraggableTabProps extends AriaTabProps {
+  item: Node<any>;
+  orientation: Orientation;
+  /**
+   * Whether this tab is overflowing the TabList container. If so, the tab
+   * needs to be visually hidden. Users can instead select it via an overflow
+   * menu.
+   */
+  overflowing: boolean;
+  state: TabListState<any>;
+}
+
+export const DraggableTab = forwardRef(
+  (
+    {item, state, orientation, overflowing}: DraggableTabProps,
+    forwardedRef: React.ForwardedRef<HTMLLIElement>
+  ) => {
+    const ref = useObjectRef(forwardedRef);
+
+    const {
+      key,
+      rendered,
+      props: {to, hidden},
+    } = item;
+    const {tabProps, isSelected} = useTab({key, isDisabled: hidden}, state, ref);
+
+    return (
+      <StyledBaseTab
+        tabProps={tabProps}
+        isSelected={isSelected}
+        to={to}
+        hidden={hidden}
+        orientation={orientation}
+        overflowing={overflowing}
+        ref={ref}
+        variant="filled"
+      >
+        <TabContentWrap>{rendered}</TabContentWrap>
+      </StyledBaseTab>
+    );
+  }
+);
+
+const StyledBaseTab = styled(BaseTab)`
+  padding: 2px 12px 2px 12px;
+  gap: 8px;
+  border-radius: 6px 6px 0px 0px;
+  border: 1px solid ${p => p.theme.gray200};
+  opacity: 0px;
+`;
+
+const TabContentWrap = styled('span')`
+  display: flex;
+  align-items: center;
+  flex-direction: row;
+  gap: 6px;
+`;
