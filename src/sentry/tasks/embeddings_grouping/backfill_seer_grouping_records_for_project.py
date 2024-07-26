7cf3fe09450de45db2622860f9f98c1a4aa6df8c
Jodi Jang <jodi.jang@sentry.io>
2024-07-26 10:45:25 -0700

    fix: Typing

--- src/sentry/tasks/embeddings_grouping/backfill_seer_grouping_records_for_project.py
+++ src/sentry/tasks/embeddings_grouping/backfill_seer_grouping_records_for_project.py
@@ -115,6 +115,7 @@ def backfill_seer_grouping_records_for_project(
                 "project_manually_skipped": is_project_skipped,
             },
         )
+        assert last_processed_project_index_input is not None
         call_next_backfill(
             last_processed_group_id=None,
             project_id=current_project_id,
