fd53a3141090f408f89acb867227b1c3ca26de5d
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2024-07-26 12:51:53 -0700

    ref(control_silo): Consolidate integrations api serializers and tests (#74815)
    Moves all integrations related serializers and their tests to sentry/integrations/api/serializers. Additionally fixes any related mypy typing errors.
    
    ref(#73859)
    
    ---------
    
    Co-authored-by: Mark Story <mark@mark-story.com>

--- src/sentry/api/endpoints/avatar/doc_integration.py
+++ src/sentry/api/endpoints/avatar/doc_integration.py
@@ -3,7 +3,9 @@ from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import control_silo_endpoint
 from sentry.api.bases.avatar import AvatarMixin
 from sentry.api.bases.doc_integrations import DocIntegrationBaseEndpoint
-from sentry.api.serializers.rest_framework.doc_integration import DocIntegrationAvatarSerializer
+from sentry.integrations.api.serializers.rest_framework.doc_integration import (
+    DocIntegrationAvatarSerializer,
+)
 from sentry.integrations.models.doc_integration_avatar import DocIntegrationAvatar
 
 
