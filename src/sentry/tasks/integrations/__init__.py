d6eff6202c53d66e9b23c1460ad7fcc9b4b73f07
Christinarlong <christina.long@sentry.io>
2024-07-26 19:20:11 -0700

    remove unnecessary changes

--- src/sentry/tasks/integrations/__init__.py
+++ src/sentry/tasks/integrations/__init__.py
@@ -1,7 +1,5 @@
 import logging
 
-from django.conf import settings
-
 from sentry import features
 from sentry.integrations.base import IntegrationInstallation
 from sentry.integrations.models.external_issue import ExternalIssue
@@ -35,30 +33,14 @@ __all__ = (
     "vsts_subscription_check",
 )
 
-settings.CELERY_IMPORTS += (
-    "sentry.tasks.integrations.create_comment",
-    "sentry.tasks.integrations.github.pr_comment",
-    "sentry.tasks.integrations.kick_off_status_syncs_impl",
-    "sentry.tasks.integrations.link_all_repos",
-    "sentry.tasks.integrations.migrate_opsgenie_plugins",
-    "sentry.tasks.integrations.migrate_issues",
-    "sentry.tasks.integrations.migrate_repo",
-    "sentry.tasks.integrations.sync_assignee_outbound_impl",
-    "sentry.tasks.integrations.sync_metadata",
-    "sentry.tasks.integrations.sync_status_inbound",
-    "sentry.tasks.integrations.sync_status_outbound",
-    "sentry.tasks.integrations.update_comment",
-    "sentry.tasks.integrations.vsts.kickoff_subscription_check",
-    "sentry.tasks.integrations.vsts.subscription_check",
-)
 
-from .create_comment import create_comment
-from .kick_off_status_syncs_impl import kick_off_status_syncs
-from .migrate_opsgenie_plugins import migrate_opsgenie_plugin
-from .migrate_repo import migrate_repo
-from .sync_assignee_outbound_impl import sync_assignee_outbound
-from .sync_metadata import sync_metadata
-from .sync_status_inbound import sync_status_inbound
-from .sync_status_outbound import sync_status_outbound
-from .update_comment import update_comment
-from .vsts import kickoff_vsts_subscription_check, vsts_subscription_check
+from sentry.integrations.tasks.create_comment import create_comment
+from sentry.integrations.tasks.kick_off_status_syncs_impl import kick_off_status_syncs
+from sentry.integrations.tasks.migrate_opsgenie_plugins import migrate_opsgenie_plugin
+from sentry.integrations.tasks.migrate_repo import migrate_repo
+from sentry.integrations.tasks.sync_assignee_outbound_impl import sync_assignee_outbound
+from sentry.integrations.tasks.sync_metadata import sync_metadata
+from sentry.integrations.tasks.sync_status_inbound import sync_status_inbound
+from sentry.integrations.tasks.sync_status_outbound import sync_status_outbound
+from sentry.integrations.tasks.update_comment import update_comment
+from sentry.integrations.tasks.vsts import kickoff_vsts_subscription_check, vsts_subscription_check
