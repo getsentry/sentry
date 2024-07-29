5510b9b07cefa7798e56f797d9ca1c539a4bd5ed
Mark Story <mark@mark-story.com>
2024-07-29 12:26:15 -0400

    chore(features) Remove FeatureHandlerStrategy.REMOTE
    We don't need two aliases for dynamic features and we can use more
    concrete terms now as flagpole is part of sentry.

--- src/sentry/features/base.py
+++ src/sentry/features/base.py
@@ -92,12 +92,12 @@ class FeatureHandlerStrategy(Enum):
     """
 
     INTERNAL = 1
-    """Handle the feature using a constant or logic within python"""
-    REMOTE = 2
-    """Handle the feature using a remote flag management service"""
+    """Handle the feature using a logic within a FeatureHandler subclass"""
     OPTIONS = 3
     """Handle the feature using options. see https://develop.sentry.dev/feature-flags/#building-your-options-based-feature
     for more information.
     """
     FLAGPOLE = 4
-    """Handle the feature using the Flagpole management service"""
+    """Handle the feature using Flagpole and option backed rules based features.
+    Features will automatically have options registered for them.
+    """
