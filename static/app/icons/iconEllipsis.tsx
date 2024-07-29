71dbd4e391b0cd56cf05c69fe0a89bd6fed7d4cb
MichaelSun48 <michael.sun@sentry.io>
2024-07-29 10:54:51 -0700

    Set default val for ellipsis compact prop

--- static/app/icons/iconEllipsis.tsx
+++ static/app/icons/iconEllipsis.tsx
@@ -7,17 +7,19 @@ interface IconEllipsisProps extends SVGIconProps {
   compact?: boolean;
 }
 
-const IconEllipsis = forwardRef<SVGSVGElement, IconEllipsisProps>((props, ref) => {
-  const circleRadius = props.compact ? 1.11 : 1.31;
-  const circleSpacing = props.compact ? 5.5 : 6.69;
-  return (
-    <SvgIcon {...props} ref={ref}>
-      <circle cx="8" cy="8" r={circleRadius} />
-      <circle cx={8 - circleSpacing} cy="8" r={circleRadius} />
-      <circle cx={8 + circleSpacing} cy="8" r={circleRadius} />
-    </SvgIcon>
-  );
-});
+const IconEllipsis = forwardRef<SVGSVGElement, IconEllipsisProps>(
+  ({compact = false, ...props}: IconEllipsisProps, ref) => {
+    const circleRadius = compact ? 1.11 : 1.31;
+    const circleSpacing = compact ? 5.5 : 6.69;
+    return (
+      <SvgIcon {...props} ref={ref}>
+        <circle cx="8" cy="8" r={circleRadius} />
+        <circle cx={8 - circleSpacing} cy="8" r={circleRadius} />
+        <circle cx={8 + circleSpacing} cy="8" r={circleRadius} />
+      </SvgIcon>
+    );
+  }
+);
 
 IconEllipsis.displayName = 'IconEllipsis';
 
