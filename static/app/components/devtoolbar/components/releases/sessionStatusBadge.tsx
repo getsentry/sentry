d7f4634fe13731440555ebd5d3174442f6e4b773
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-07-29 15:59:07 -0700

    feat(toolbar): add red indicator badge if crashes >> healthy sessions

---
+++ static/app/components/devtoolbar/components/releases/sessionStatusBadge.tsx
@@ -0,0 +1,24 @@
+import IndicatorBadge from 'sentry/components/devtoolbar/components/indicatorBadge';
+import useSessionStatus from 'sentry/components/devtoolbar/components/releases/useSessionStatus';
+import {IconFire} from 'sentry/icons';
+
+export default function CrashCountBadge() {
+  const {data} = useSessionStatus();
+  const healthySessions = data?.json.groups.find(
+    g => g.by['session.status'] === 'healthy'
+  )?.totals['sum(session)'];
+  const crashSessions = data?.json.groups.find(g => g.by['session.status'] === 'crashed')
+    ?.totals['sum(session)'];
+
+  if (!crashSessions || !healthySessions) {
+    return null;
+  }
+
+  const crashPercent = (crashSessions / healthySessions) * 100;
+
+  // over 10% of sessions were crashes
+  if (crashPercent > 10) {
+    return <IndicatorBadge icon={<IconFire size="xs" />} variant="red" />;
+  }
+  return null;
+}
