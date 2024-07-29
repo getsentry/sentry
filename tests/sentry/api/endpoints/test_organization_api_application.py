f9ee8b1037897d98cfb2aa3b04c470f1ddaf4e5d
Colton Allen <colton.allen@sentry.io>
2024-07-29 10:56:29 -0500

    Add coverage

--- tests/sentry/api/endpoints/test_organization_api_application.py
+++ tests/sentry/api/endpoints/test_organization_api_application.py
@@ -3,6 +3,8 @@ from django.urls import reverse
 from sentry.testutils.cases import APITestCase
 from sentry.testutils.silo import control_silo_test
 
+FEATURES = {"organizations:oauth2-public-api-applications": True}
+
 
 @control_silo_test
 class OrganizationApiApplicationTest(APITestCase):
@@ -15,35 +17,38 @@ class OrganizationApiApplicationTest(APITestCase):
 
     def test_get_organization_api_application(self):
         self._create_api_application([], [])
-        response = self.client.get(self.url)
-        assert response.status_code == 200
-
-        response_json = response.json()
-        assert len(response_json) == 3
-        assert "id" in response_json
-        assert response_json["allowedOrigins"] == []
-        assert response_json["redirectUris"] == []
+        with self.feature(FEATURES):
+            response = self.client.get(self.url)
+            assert response.status_code == 200
+            response_json = response.json()
+            assert len(response_json) == 3
+            assert "id" in response_json
+            assert response_json["allowedOrigins"] == []
+            assert response_json["redirectUris"] == []
 
     def test_get_organization_api_application_not_found(self):
-        response = self.client.get(self.url)
-        assert response.status_code == 404
+        with self.feature(FEATURES):
+            response = self.client.get(self.url)
+            assert response.status_code == 404
 
     def test_post_organization_api_application(self):
         self._create_api_application([], [])
 
         # Read our writes.
-        response = self.client.get(self.url)
-        assert response.status_code == 200
-        response_json = response.json()
-        assert len(response_json) == 3
-        assert "id" in response_json
-        assert response_json["allowedOrigins"] == []
-        assert response_json["redirectUris"] == []
+        with self.feature(FEATURES):
+            response = self.client.get(self.url)
+            assert response.status_code == 200
+            response_json = response.json()
+            assert len(response_json) == 3
+            assert "id" in response_json
+            assert response_json["allowedOrigins"] == []
+            assert response_json["redirectUris"] == []
 
     def test_post_organization_api_application_duplicate(self):
         self._create_api_application([], [])
-        response = self.client.post(self.url, data={"allowedOrigins": [], "redirectUris": []})
-        assert response.status_code == 200
+        with self.feature(FEATURES):
+            response = self.client.post(self.url, data={"allowedOrigins": [], "redirectUris": []})
+            assert response.status_code == 200
 
     def test_put_organization_api_application(self):
         self._create_api_application([], [])
@@ -52,44 +57,72 @@ class OrganizationApiApplicationTest(APITestCase):
             "allowedOrigins": ["https://www.sentry.io"],
             "redirectUris": ["https://www.sentry.io/authorized/"],
         }
-        response = self.client.put(self.url, data=data)
-        assert response.status_code == 202
+        with self.feature(FEATURES):
+            response = self.client.put(self.url, data=data)
+            assert response.status_code == 202
 
-        # Read our writes.
-        response = self.client.get(self.url)
-        assert response.status_code == 200
-        response_json = response.json()
-        assert len(response_json) == 3
-        assert "id" in response_json
-        assert response_json["allowedOrigins"] == ["https://www.sentry.io"]
-        assert response_json["redirectUris"] == ["https://www.sentry.io/authorized/"]
+            # Read our writes.
+            response = self.client.get(self.url)
+            assert response.status_code == 200
+            response_json = response.json()
+            assert len(response_json) == 3
+            assert "id" in response_json
+            assert response_json["allowedOrigins"] == ["https://www.sentry.io"]
+            assert response_json["redirectUris"] == ["https://www.sentry.io/authorized/"]
 
     def test_put_organization_api_application_not_found(self):
         data = {
             "allowedOrigins": ["https://www.sentry.io"],
             "redirectUris": ["https://www.sentry.io/authorized/"],
         }
-        response = self.client.put(self.url, data=data)
-        assert response.status_code == 404
+        with self.feature(FEATURES):
+            response = self.client.put(self.url, data=data)
+            assert response.status_code == 404
 
     def test_delete_organization_api_application(self):
         self._create_api_application([], [])
-        response = self.client.delete(self.url)
-        assert response.status_code == 204
+        with self.feature(FEATURES):
+            response = self.client.delete(self.url)
+            assert response.status_code == 204
 
-        # Read our writes.
-        response = self.client.get(self.url)
-        assert response.status_code == 404
+            # Read our writes.
+            response = self.client.get(self.url)
+            assert response.status_code == 404
 
     def test_delete_organization_api_application_not_found(self):
-        response = self.client.delete(self.url)
-        assert response.status_code == 404
+        with self.feature(FEATURES):
+            response = self.client.delete(self.url)
+            assert response.status_code == 404
 
     def _create_api_application(self, origins, redirects):
         data = {
             "allowedOrigins": origins,
             "redirectUris": redirects,
         }
+        with self.feature(FEATURES):
+            response = self.client.post(self.url, data=data)
+            assert response.status_code == 201
+            return response
+
+    # Feature flag disabled coverage.
+
+    def test_get_organization_api_application_flag_disabled(self):
+        self._create_api_application([], [])
+        response = self.client.get(self.url)
+        assert response.status_code == 404
+
+    def test_post_organization_api_application_flag_disabled(self):
+        data = {"allowedOrigins": [], "redirectUris": []}
         response = self.client.post(self.url, data=data)
-        assert response.status_code == 201
-        return response
+        assert response.status_code == 404
+
+    def test_put_organization_api_application_flag_disabled(self):
+        self._create_api_application([], [])
+        data = {"allowedOrigins": [], "redirectUris": []}
+        response = self.client.put(self.url, data=data)
+        assert response.status_code == 404
+
+    def test_delete_organization_api_application_flag_disabled(self):
+        self._create_api_application([], [])
+        response = self.client.delete(self.url)
+        assert response.status_code == 404
