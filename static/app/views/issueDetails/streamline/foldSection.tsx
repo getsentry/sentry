ad6f75cc81d07406b751ffbfef896c386f078963
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-07-29 13:33:51 -0400

    ✨ Apply new fixes from attempting implementation

--- static/app/views/issueDetails/streamline/foldSection.tsx
+++ static/app/views/issueDetails/streamline/foldSection.tsx
@@ -1,31 +1,89 @@
-import {useCallback} from 'react';
+import {useCallback, useState} from 'react';
 import styled from '@emotion/styled';
 
+import ErrorBoundary from 'sentry/components/errorBoundary';
 import InteractionStateLayer from 'sentry/components/interactionStateLayer';
 import {IconChevron} from 'sentry/icons';
 import {space} from 'sentry/styles/space';
 import {useLocalStorageState} from 'sentry/utils/useLocalStorageState';
 
-const LOCAL_STORAGE_PREFIX = 'issue-details-collapse-';
-export const enum SectionKey {
-  HIGHLIGHTS = 'highlights',
-  STACK_TRACE = 'stack-trace',
-  BREADCRUMBS = 'breadcrumbs',
-  TAGS = 'tags',
-  CONTEXTS = 'contexts',
+const LOCAL_STORAGE_PREFIX = 'fold-section-collapse-';
+export const enum FoldSectionKey {
+  // Suspect Commits & Traces
+  USER_FEEDBACK = 'issue-details-user-feedback', // In development
+  LLM_MONITORING = 'issue-details-llm-monitoring',
+
+  UPTIME = 'issue-details-uptime', // Only Uptime issues
+  CRON = 'issue-details-cron-timeline', // Only Cron issues
+
+  HIGHLIGHTS = 'issue-details-highlights',
+  RESOURCES = 'issue-details-resources', // Position controlled by flag
+
+  EVIDENCE = 'issue-details-evidence',
+  MESSAGE = 'issue-details-message',
+  STACK_TRACE = 'issue-details-stack-trace',
+
+  THREADS = 'issue-details-threads',
+  THREAD_STATE = 'issue-details-thread-state',
+  THREAD_TAGS = 'issue-details-thread-tags',
+
+  // QuickTraceQuery -> todo
+
+  SPAN_EVIDENCE = 'issue-details-span-evidence',
+  HYDRATION_DIFF = 'issue-details-hydration-diff',
+  REPLAY = 'issue-details-replay',
+
+  HPKP = 'issue-details-hpkp',
+  CSP = 'issue-details-csp',
+  EXPECTCT = 'issue-details-expectct',
+  TEMPLATE = 'issue-details-template',
+
+  BREADCRUMBS = 'issue-details-breadcrumbs',
+  DEBUGMETA = 'issue-details-debugmeta',
+  REQUEST = 'issue-details-request',
+
+  TAGS = 'issue-details-tags',
+  SCREENSHOT = 'issue-details-screenshot',
+
+  CONTEXTS = 'issue-details-contexts',
+  EXTRA = 'issue-details-extra',
+  PACKAGE = 'issue-details-package',
+  DEVICE = 'issue-details-device',
+  VIEW_HIERARCHY = 'issue-details-view-hierarchy',
+  ATTACHMENTS = 'issue-details-attachments',
+  SDK = 'issue-details-sdk',
+  GROUPING_INFO = 'issue-details-grouping-info',
+  RRWEB = 'issue-details-rrweb',
 }
 
 interface FoldSectionProps {
   children: React.ReactNode;
-  sectionKey: SectionKey;
+  /**
+   * Unique key to persist user preferences for initalizing the section to open/closed
+   */
+  sectionKey: FoldSectionKey;
+  /**
+   * Title of the section, always visible
+   */
   title: React.ReactNode;
+  /**
+   * Actions associated with the section, only visible when open
+   */
+  actions?: React.ReactNode;
+  /**
+   * Should this section be initially open, gets overridden by user preferences
+   */
   initialCollapse?: boolean;
+  /**
+   * Disable the ability for the user to collapse the section
+   */
   preventCollapse?: boolean;
 }
 
 export function FoldSection({
   children,
   title,
+  actions,
   sectionKey,
   initialCollapse = false,
   preventCollapse = false,
@@ -35,6 +93,7 @@ export function FoldSection({
     `${LOCAL_STORAGE_PREFIX}${sectionKey}`,
     initialCollapse
   );
+  const [isLayerEnabled, setIsLayerEnabled] = useState(true);
 
   const toggleCollapse = useCallback(
     (e: React.MouseEvent) => {
@@ -46,35 +105,49 @@ export function FoldSection({
 
   return (
     <section {...props}>
-      <Details open={!isCollapsed || preventCollapse}>
+      <details open={!isCollapsed || preventCollapse}>
         <Summary
           preventCollapse={preventCollapse}
           onClick={preventCollapse ? e => e.preventDefault() : toggleCollapse}
         >
-          <InteractionStateLayer hidden={preventCollapse} />
-          <div>{title}</div>
+          <InteractionStateLayer
+            hidden={preventCollapse ? preventCollapse : !isLayerEnabled}
+          />
+          <TitleWithActions>
+            {title}
+            {!preventCollapse && !isCollapsed && (
+              <div
+                onClick={e => e.stopPropagation()}
+                onMouseEnter={() => setIsLayerEnabled(false)}
+                onMouseLeave={() => setIsLayerEnabled(true)}
+              >
+                {actions}
+              </div>
+            )}
+          </TitleWithActions>
           <IconWrapper preventCollapse={preventCollapse}>
-            <IconChevron direction={isCollapsed ? 'up' : 'down'} size="xs" />
+            <IconChevron direction={isCollapsed ? 'down' : 'up'} size="xs" />
           </IconWrapper>
         </Summary>
-        {children}
-      </Details>
+        <ErrorBoundary mini>
+          <Content>{children}</Content>
+        </ErrorBoundary>
+      </details>
     </section>
   );
 }
 
-const Details = styled('details')`
-  margin: ${space(1)} ${space(0.75)};
+const Content = styled('div')`
+  padding: ${space(0.5)} ${space(0.75)};
 `;
 
 const Summary = styled('summary')<{preventCollapse: boolean}>`
   display: grid;
   grid-template-columns: 1fr auto;
   align-items: center;
-  margin-bottom: ${space(0.5)};
   font-size: ${p => p.theme.fontSizeMedium};
   font-weight: ${p => p.theme.fontWeightBold};
-  padding: ${space(0.5)} ${space(1)};
+  padding: ${space(0.5)} ${space(0.75)};
   border-radius: ${p => p.theme.borderRadius};
   cursor: ${p => (p.preventCollapse ? 'initial' : 'pointer')};
   position: relative;
@@ -84,4 +157,14 @@ const IconWrapper = styled('div')<{preventCollapse: boolean}>`
   color: ${p => p.theme.subText};
   line-height: 0;
   visibility: ${p => (p.preventCollapse ? 'hidden' : 'initial')};
+`;
+
+const TitleWithActions = styled('div')`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  margin-right: 8px;
+  align-items: center;
+  /* Usually the actions are buttons, this height allows actions appearing after opening the
+  details section to not expand the summary */
+  min-height: 26px;
 `;
