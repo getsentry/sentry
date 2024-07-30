3d6585b735b9ca17237b74001120ae0d7c114dad
Jonas <jonas.badalic@sentry.io>
2024-07-30 13:22:37 -0400

    Update static/app/gettingStartedDocs/javascript/gatsby.spec.tsx
    Co-authored-by: Tony Xiao <txiao@sentry.io>

--- static/app/gettingStartedDocs/javascript/gatsby.spec.tsx
+++ static/app/gettingStartedDocs/javascript/gatsby.spec.tsx
@@ -73,7 +73,7 @@ describe('javascript-gatsby onboarding docs', function () {
     ).toBeInTheDocument();
   });
 
-  it('enables profiling by setting profiling samplerates', () => {
+  it('enables profiling by setting profiling sample rates', () => {
     renderWithOnboardingLayout(docs, {
       selectedProducts: [ProductSolution.ERROR_MONITORING, ProductSolution.PROFILING],
     });
