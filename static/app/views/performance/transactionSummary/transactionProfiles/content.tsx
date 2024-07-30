4388bf765eb839f6da9702580728561d5bfa809a
Tony Xiao <txiao@sentry.io>
2024-07-30 14:33:34 -0400

    ref(profiling): Split transaction summary profile into new component (#75274)
    This component is getting quite large and we'd like to add some more
    functionality to it. So split it up into new components first.

---
+++ static/app/views/performance/transactionSummary/transactionProfiles/content.tsx
@@ -0,0 +1,255 @@
+import {useCallback, useMemo} from 'react';
+import styled from '@emotion/styled';
+
+import {Button} from 'sentry/components/button';
+import {CompactSelect} from 'sentry/components/compactSelect';
+import type {SelectOption} from 'sentry/components/compactSelect/types';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {AggregateFlamegraph} from 'sentry/components/profiling/flamegraph/aggregateFlamegraph';
+import {AggregateFlamegraphTreeTable} from 'sentry/components/profiling/flamegraph/aggregateFlamegraphTreeTable';
+import {FlamegraphSearch} from 'sentry/components/profiling/flamegraph/flamegraphToolbar/flamegraphSearch';
+import {SegmentedControl} from 'sentry/components/segmentedControl';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import type {DeepPartial} from 'sentry/types/utils';
+import type {CanvasScheduler} from 'sentry/utils/profiling/canvasScheduler';
+import {
+  CanvasPoolManager,
+  useCanvasScheduler,
+} from 'sentry/utils/profiling/canvasScheduler';
+import type {FlamegraphState} from 'sentry/utils/profiling/flamegraph/flamegraphStateProvider/flamegraphContext';
+import {FlamegraphStateProvider} from 'sentry/utils/profiling/flamegraph/flamegraphStateProvider/flamegraphContextProvider';
+import {FlamegraphThemeProvider} from 'sentry/utils/profiling/flamegraph/flamegraphThemeProvider';
+import type {Frame} from 'sentry/utils/profiling/frame';
+import {useAggregateFlamegraphQuery} from 'sentry/utils/profiling/hooks/useAggregateFlamegraphQuery';
+import {useLocalStorageState} from 'sentry/utils/useLocalStorageState';
+import {
+  FlamegraphProvider,
+  useFlamegraph,
+} from 'sentry/views/profiling/flamegraphProvider';
+import {ProfileGroupProvider} from 'sentry/views/profiling/profileGroupProvider';
+
+const DEFAULT_FLAMEGRAPH_PREFERENCES: DeepPartial<FlamegraphState> = {
+  preferences: {
+    sorting: 'alphabetical' satisfies FlamegraphState['preferences']['sorting'],
+  },
+};
+
+const noop = () => void 0;
+
+interface TransactionProfilesContentProps {
+  query: string;
+}
+
+export function TransactionProfilesContent({query}: TransactionProfilesContentProps) {
+  const {data, isLoading, isError} = useAggregateFlamegraphQuery({
+    query,
+  });
+
+  const [frameFilter, setFrameFilter] = useLocalStorageState<
+    'system' | 'application' | 'all'
+  >('flamegraph-frame-filter', 'application');
+
+  const onFrameFilterChange = useCallback(
+    (value: 'system' | 'application' | 'all') => {
+      setFrameFilter(value);
+    },
+    [setFrameFilter]
+  );
+
+  const flamegraphFrameFilter: ((frame: Frame) => boolean) | undefined = useMemo(() => {
+    if (frameFilter === 'all') {
+      return () => true;
+    }
+    if (frameFilter === 'application') {
+      return frame => frame.is_application;
+    }
+    return frame => !frame.is_application;
+  }, [frameFilter]);
+
+  const [visualization, setVisualization] = useLocalStorageState<
+    'flamegraph' | 'call tree'
+  >('flamegraph-visualization', 'flamegraph');
+
+  const onVisualizationChange = useCallback(
+    (value: 'flamegraph' | 'call tree') => {
+      setVisualization(value);
+    },
+    [setVisualization]
+  );
+
+  const canvasPoolManager = useMemo(() => new CanvasPoolManager(), []);
+  const scheduler = useCanvasScheduler(canvasPoolManager);
+
+  return (
+    <ProfileVisualization>
+      <ProfileGroupProvider
+        traceID=""
+        type="flamegraph"
+        input={data ?? null}
+        frameFilter={flamegraphFrameFilter}
+      >
+        <FlamegraphStateProvider initialState={DEFAULT_FLAMEGRAPH_PREFERENCES}>
+          <FlamegraphThemeProvider>
+            <FlamegraphProvider>
+              <AggregateFlamegraphToolbar
+                scheduler={scheduler}
+                canvasPoolManager={canvasPoolManager}
+                visualization={visualization}
+                onVisualizationChange={onVisualizationChange}
+                frameFilter={frameFilter}
+                onFrameFilterChange={onFrameFilterChange}
+                hideSystemFrames={false}
+                setHideSystemFrames={noop}
+              />
+              <FlamegraphContainer>
+                {visualization === 'flamegraph' ? (
+                  <AggregateFlamegraph
+                    canvasPoolManager={canvasPoolManager}
+                    scheduler={scheduler}
+                  />
+                ) : (
+                  <AggregateFlamegraphTreeTable
+                    recursion={null}
+                    expanded={false}
+                    frameFilter={frameFilter}
+                    canvasPoolManager={canvasPoolManager}
+                    withoutBorders
+                  />
+                )}
+              </FlamegraphContainer>
+              {isLoading ? (
+                <RequestStateMessageContainer>
+                  <LoadingIndicator />
+                </RequestStateMessageContainer>
+              ) : isError ? (
+                <RequestStateMessageContainer>
+                  {t('There was an error loading the flamegraph.')}
+                </RequestStateMessageContainer>
+              ) : null}
+            </FlamegraphProvider>
+          </FlamegraphThemeProvider>
+        </FlamegraphStateProvider>
+      </ProfileGroupProvider>
+    </ProfileVisualization>
+  );
+}
+
+interface AggregateFlamegraphToolbarProps {
+  canvasPoolManager: CanvasPoolManager;
+  frameFilter: 'system' | 'application' | 'all';
+  hideSystemFrames: boolean;
+  onFrameFilterChange: (value: 'system' | 'application' | 'all') => void;
+  onVisualizationChange: (value: 'flamegraph' | 'call tree') => void;
+  scheduler: CanvasScheduler;
+  setHideSystemFrames: (value: boolean) => void;
+  visualization: 'flamegraph' | 'call tree';
+}
+
+function AggregateFlamegraphToolbar(props: AggregateFlamegraphToolbarProps) {
+  const flamegraph = useFlamegraph();
+  const flamegraphs = useMemo(() => [flamegraph], [flamegraph]);
+  const spans = useMemo(() => [], []);
+
+  const frameSelectOptions: SelectOption<'system' | 'application' | 'all'>[] =
+    useMemo(() => {
+      return [
+        {value: 'system', label: t('System Frames')},
+        {value: 'application', label: t('Application Frames')},
+        {value: 'all', label: t('All Frames')},
+      ];
+    }, []);
+
+  const onResetZoom = useCallback(() => {
+    props.scheduler.dispatch('reset zoom');
+  }, [props.scheduler]);
+
+  const onFrameFilterChange = useCallback(
+    (value: {value: 'application' | 'system' | 'all'}) => {
+      props.onFrameFilterChange(value.value);
+    },
+    [props]
+  );
+
+  return (
+    <AggregateFlamegraphToolbarContainer>
+      <ViewSelectContainer>
+        <SegmentedControl
+          aria-label={t('View')}
+          size="xs"
+          value={props.visualization}
+          onChange={props.onVisualizationChange}
+        >
+          <SegmentedControl.Item key="flamegraph">
+            {t('Flamegraph')}
+          </SegmentedControl.Item>
+          <SegmentedControl.Item key="call tree">{t('Call Tree')}</SegmentedControl.Item>
+        </SegmentedControl>
+      </ViewSelectContainer>
+      <AggregateFlamegraphSearch
+        spans={spans}
+        canvasPoolManager={props.canvasPoolManager}
+        flamegraphs={flamegraphs}
+      />
+      <Button size="xs" onClick={onResetZoom}>
+        {t('Reset Zoom')}
+      </Button>
+      <CompactSelect
+        onChange={onFrameFilterChange}
+        value={props.frameFilter}
+        size="xs"
+        options={frameSelectOptions}
+      />
+    </AggregateFlamegraphToolbarContainer>
+  );
+}
+
+const ProfileVisualization = styled('div')`
+  display: grid;
+  grid-template-rows: min-content 1fr;
+  height: 100%;
+  flex: 1;
+  border: 1px solid ${p => p.theme.border};
+  border-radius: ${p => p.theme.borderRadius};
+  overflow: hidden;
+`;
+
+const FlamegraphContainer = styled('div')`
+  overflow: hidden;
+  display: flex;
+`;
+
+const RequestStateMessageContainer = styled('div')`
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  color: ${p => p.theme.subText};
+  pointer-events: none;
+`;
+
+const AggregateFlamegraphToolbarContainer = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  gap: ${space(1)};
+  padding: ${space(1)};
+  background-color: ${p => p.theme.background};
+  /*
+    force height to be the same as profile digest header,
+    but subtract 1px for the border that doesnt exist on the header
+   */
+  height: 41px;
+  border-bottom: 1px solid ${p => p.theme.border};
+`;
+
+const ViewSelectContainer = styled('div')`
+  min-width: 160px;
+`;
+
+const AggregateFlamegraphSearch = styled(FlamegraphSearch)`
+  max-width: 300px;
+`;
