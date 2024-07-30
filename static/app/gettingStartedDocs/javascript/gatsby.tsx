5da238f661ac69c21a5f52af4cfc3814e912a41a
JonasBa <jonas@badalic.com>
2024-07-30 13:52:46 -0400

    onboardin: update comment

--- static/app/gettingStartedDocs/javascript/gatsby.tsx
+++ static/app/gettingStartedDocs/javascript/gatsby.tsx
@@ -73,12 +73,8 @@ ${getFeedbackConfigOptions(params.feedbackOptions)}}),`
 }${
   params.isProfilingSelected
     ? `
-        // Set profilesSampleRate to 1.0 to profile every transaction.
-        // Since profilesSampleRate is relative to tracesSampleRate,
-        // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
-        // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
-        // results in 25% of transactions being profiled (0.5*0.5=0.25)
-        profilesSampleRate: 1.0,`
+      // Profiling
+      profilesSampleRate: 1.0, // Profile 100% of the transactions. This value is relative to tracesSampleRate`
     : ''
 }
 });
