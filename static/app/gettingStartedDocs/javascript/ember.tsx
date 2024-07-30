8d1409d068a4ef559fd93d4ddad536d84bedb8e7
Jonas <jonas.badalic@sentry.io>
2024-07-30 13:35:24 -0400

    feat(onboarding) add profiling onboarding (#75252)
    Adds profiling to JS onboarding setup. I will followup with a separate
    PR to add the document-policy header instructions
    
    ---------
    
    Co-authored-by: Tony Xiao <txiao@sentry.io>

--- static/app/gettingStartedDocs/javascript/ember.tsx
+++ static/app/gettingStartedDocs/javascript/ember.tsx
@@ -40,6 +40,11 @@ Sentry.init({
         Sentry.replayIntegration(${getReplayConfigOptions(params.replayOptions)}),`
       : ''
   }${
+    params.isProfilingSelected
+      ? `
+          Sentry.browserProfilingIntegration(),`
+      : ''
+  }${
     params.isFeedbackSelected
       ? `
         Sentry.feedbackIntegration({
@@ -62,6 +67,16 @@ ${getFeedbackConfigOptions(params.feedbackOptions)}}),`
       // Session Replay
       replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
       replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.`
+    : ''
+}${
+  params.isProfilingSelected
+    ? `
+        // Set profilesSampleRate to 1.0 to profile every transaction.
+        // Since profilesSampleRate is relative to tracesSampleRate,
+        // the final profiling rate can be computed as tracesSampleRate * profilesSampleRate
+        // For example, a tracesSampleRate of 0.5 and profilesSampleRate of 0.5 would
+        // results in 25% of transactions being profiled (0.5*0.5=0.25)
+        profilesSampleRate: 1.0,`
     : ''
 }
 });
