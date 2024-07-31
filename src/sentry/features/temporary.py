4d2cfc67dbec6758a3f50bd50fe9f3e60a5a0aca
Edward Gou <edward.gou@sentry.io>
2024-07-31 11:31:36 -0400

    adds new flag for insights alerts

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -422,6 +422,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:insights-initial-modules", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=True)
     # Enable access to newer Insights modules (Caches, Queues, LLMs, Mobile UI)
     manager.add("organizations:insights-addon-modules", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=True)
+    # Enable access to insights metrics alerts
+    manager.add("organizations:insights-alerts", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=True)
     # Enable standalone span ingestion
     manager.add("organizations:standalone-span-ingestion", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enable the aggregate span waterfall view
