463c9fa56a65e10eb7f36b536de42c8bb314a0c8
Christinarlong <christina.long@sentry.io>
2024-07-31 15:15:32 -0700

    ref(control_silo): Create forwards compatible slack integration task shims
    Adds new task shims in the places where integrations tasks will be moved (in the future) for slack tasks.
    
    ref(issue # here)
    Split off of #74925

---
+++ src/sentry/integrations/slack/tasks/find_channel_id_for_rule.py
@@ -0,0 +1,28 @@
+from collections.abc import Sequence
+from typing import Any
+
+from sentry.incidents.models.alert_rule import AlertRuleTriggerAction
+from sentry.models.project import Project
+from sentry.silo.base import SiloMode
+from sentry.tasks.base import instrumented_task
+from sentry.tasks.integrations.slack.find_channel_id_for_rule import (
+    find_channel_id_for_rule as old_find_channel_id_for_rule,
+)
+
+
+@instrumented_task(
+    name="sentry.integrations.slack.tasks.search_channel_id",
+    queue="integrations",
+    silo_mode=SiloMode.REGION,
+)
+def find_channel_id_for_rule(
+    project: Project,
+    actions: Sequence[AlertRuleTriggerAction],
+    uuid: str,
+    rule_id: int | None = None,
+    user_id: int | None = None,
+    **kwargs: Any,
+) -> None:
+    old_find_channel_id_for_rule(
+        project=project, actions=actions, uuid=uuid, rule_id=rule_id, user_id=user_id, **kwargs
+    )
