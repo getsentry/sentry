43bbfcb8bce17115f37c462bb782d8982d6b3756
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-07-31 13:08:32 -0400

    ref: fill out TypeVars for ForeignKey (#75228)
    this fixes a class of problem when upgrading to django-stubs 4.0.3 where
    `_ST` becomes unbound
    
    <!-- Describe your PR here. -->

--- src/sentry/db/models/fields/foreignkey.py
+++ src/sentry/db/models/fields/foreignkey.py
@@ -5,10 +5,12 @@ from typing import Any
 from django.db import models
 from django.db.models import ForeignKey
 
+from sentry.db.models.fields.types import FieldGetType, FieldSetType
+
 __all__ = ("FlexibleForeignKey",)
 
 
-class FlexibleForeignKey(ForeignKey):
+class FlexibleForeignKey(ForeignKey[FieldSetType, FieldGetType]):
     def __init__(self, *args: Any, **kwargs: Any):
         kwargs.setdefault("on_delete", models.CASCADE)
         super().__init__(*args, **kwargs)
