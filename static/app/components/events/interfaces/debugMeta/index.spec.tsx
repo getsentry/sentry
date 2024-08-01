9424ac9f6990af0877a955ba69494b5b6a6694de
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-08-01 16:28:39 -0400

    ✅ Add other test

--- static/app/components/events/interfaces/debugMeta/index.spec.tsx
+++ static/app/components/events/interfaces/debugMeta/index.spec.tsx
@@ -1,42 +1,83 @@
-import {Fragment} from 'react';
 import {EventFixture} from 'sentry-fixture/event';
 import {EntryDebugMetaFixture} from 'sentry-fixture/eventEntry';
 
 import {initializeOrg} from 'sentry-test/initializeOrg';
-import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
+import {
+  render,
+  renderGlobalModal,
+  screen,
+  userEvent,
+} from 'sentry-test/reactTestingLibrary';
 
 import {DebugMeta} from 'sentry/components/events/interfaces/debugMeta';
-import {getFileName} from 'sentry/components/events/interfaces/debugMeta/utils';
-import GlobalModal from 'sentry/components/globalModal';
 
 describe('DebugMeta', function () {
   it('opens details modal', async function () {
     const eventEntryDebugMeta = EntryDebugMetaFixture();
     const event = EventFixture({entries: [eventEntryDebugMeta]});
-    const {organization, project, router} = initializeOrg();
-    const routerProps = {router, location: router.location};
+    const {organization, project} = initializeOrg();
+    const image = eventEntryDebugMeta.data.images[0];
+    const mockGetDebug = MockApiClient.addMockResponse({
+      url: `/projects/${organization.slug}/${project.slug}/files/dsyms/?debug_id=${image?.debug_id}`,
+      method: 'GET',
+      body: [],
+    });
 
     render(
-      <Fragment>
-        <GlobalModal />
-        <DebugMeta
-          organization={organization}
-          projectSlug={project.slug}
-          event={event}
-          data={eventEntryDebugMeta.data}
-          {...routerProps}
-        />
-      </Fragment>
+      <DebugMeta
+        projectSlug={project.slug}
+        event={event}
+        data={eventEntryDebugMeta.data}
+      />,
+      {organization}
     );
+    renderGlobalModal();
 
-    await screen.findByRole('heading', {name: 'Images Loaded'});
+    screen.getByRole('heading', {name: 'Images Loaded'});
+    const imageName = image?.debug_file as string;
+    expect(screen.queryByText(imageName)).not.toBeInTheDocument();
 
     await userEvent.click(screen.getByRole('button', {name: 'Show Details'}));
+    expect(screen.getByText('Ok')).toBeInTheDocument();
+    expect(screen.getByText(imageName)).toBeInTheDocument();
+    expect(screen.getByText('Symbolication')).toBeInTheDocument();
+    expect(mockGetDebug).not.toHaveBeenCalled();
 
+    const codeFile = image?.code_file as string;
+    expect(screen.queryByText(codeFile)).not.toBeInTheDocument();
     await userEvent.click(screen.getByRole('button', {name: 'View'}));
+    expect(screen.getByText(codeFile)).toBeInTheDocument();
+    expect(mockGetDebug).toHaveBeenCalled();
+  });
 
-    expect(
-      await screen.findByText(getFileName(eventEntryDebugMeta.data.images[0]?.code_file)!)
-    ).toBeInTheDocument();
+  it('searches image contents', async function () {
+    const eventEntryDebugMeta = EntryDebugMetaFixture();
+    const event = EventFixture({entries: [eventEntryDebugMeta]});
+    const {organization, project} = initializeOrg();
+    const image = eventEntryDebugMeta.data.images[0];
+
+    render(
+      <DebugMeta
+        projectSlug={project.slug}
+        event={event}
+        data={eventEntryDebugMeta.data}
+      />,
+      {organization}
+    );
+    const imageName = image?.debug_file as string;
+    const codeFile = image?.code_file as string;
+
+    screen.getByRole('heading', {name: 'Images Loaded'});
+    await userEvent.click(screen.getByRole('button', {name: 'Show Details'}));
+    const imageNode = screen.getByText(imageName);
+    expect(imageNode).toBeInTheDocument();
+
+    const searchBar = screen.getByRole('textbox');
+    await userEvent.type(searchBar, 'some jibberish');
+    expect(screen.queryByText(imageName)).not.toBeInTheDocument();
+    await userEvent.clear(searchBar);
+    expect(screen.getByText(imageName)).toBeInTheDocument();
+    await userEvent.type(searchBar, codeFile);
+    expect(screen.getByText(imageName)).toBeInTheDocument();
   });
 });
