97bd9c723dfbb273005b8d53d885a62566b93b9c
Katie Byers <katie.byers@sentry.io>
2024-08-01 11:33:50 -0700

    ref(seer grouping): Prepare for calling Seer in optimized grouping flow (#75257)
    This PR makes two changes to prepare for adding a Seer call to the optimized grouping flow in ingest.
    
    - Pull the check for whether to call Seer and the call itself into a new function, `maybe_check_seer_for_matching_grouphash`, so that the same logic can be used in both the current and optimized flows.
    - Refactor `get_seer_similar_issues` so that it returns a `GroupHash` object rather than a group, to make it compatible with `handle_existing_grouphash` (which is used in the new flow).

--- src/sentry/event_manager.py
+++ src/sentry/event_manager.py
@@ -71,7 +71,7 @@ from sentry.grouping.ingest.metrics import (
     record_hash_calculation_metrics,
     record_new_group_metrics,
 )
-from sentry.grouping.ingest.seer import get_seer_similar_issues, should_call_seer_for_grouping
+from sentry.grouping.ingest.seer import maybe_check_seer_for_matching_grouphash
 from sentry.grouping.ingest.utils import (
     add_group_id_to_grouphashes,
     check_for_category_mismatch,
@@ -1533,29 +1533,14 @@ def _save_aggregate(
             # If we still haven't found a matching grouphash, we're now safe to go ahead and talk to
             # seer and/or create the group.
             if existing_grouphash is None:
-                seer_matched_group = None
-
-                if should_call_seer_for_grouping(event, primary_hashes):
-                    metrics.incr(
-                        "grouping.similarity.did_call_seer",
-                        # TODO: Consider lowering this (in all the spots this metric is
-                        # collected) once we roll Seer grouping out more widely
-                        sample_rate=1.0,
-                        tags={"call_made": True, "blocker": "none"},
-                    )
-                    try:
-                        # If no matching group is found in Seer, we'll still get back result
-                        # metadata, but `seer_matched_group` will be None
-                        seer_response_data, seer_matched_group = get_seer_similar_issues(
-                            event, primary_hashes
-                        )
-                        event.data["seer_similarity"] = seer_response_data
-
-                    # Insurance - in theory we shouldn't ever land here
-                    except Exception as e:
-                        sentry_sdk.capture_exception(
-                            e, tags={"event": event.event_id, "project": project.id}
-                        )
+                seer_matched_grouphash = maybe_check_seer_for_matching_grouphash(
+                    event, primary_hashes
+                )
+                seer_matched_group = (
+                    Group.objects.filter(id=seer_matched_grouphash.group_id).first()
+                    if seer_matched_grouphash
+                    else None
+                )
 
                 group = seer_matched_group or _create_group(project, event, **group_creation_kwargs)
 
