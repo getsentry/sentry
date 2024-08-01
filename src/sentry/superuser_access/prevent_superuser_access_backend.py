d6c8b8e50d6b204f27692987e22600a3e96a4372
Raj Joshi <raj.joshi@sentry.io>
2024-08-01 11:49:20 -0700

    add doc string

--- src/sentry/superuser_access/prevent_superuser_access_backend.py
+++ src/sentry/superuser_access/prevent_superuser_access_backend.py
@@ -2,6 +2,13 @@ from sentry.models.organization import Organization
 from sentry.organizations.services.organization import RpcUserOrganizationContext
 from sentry.utils.services import Service
 
+"""
+This class defines the interface for a backend that determines whether a superuser should be allowed access to an organization.
+The backend should implement the `should_allow_superuser_access` method, which takes an organization context and returns a boolean.
+This is a SAAS-only feature, so the default implementation should always return True.
+getsentry contains buisness logic.
+"""
+
 
 class PreventSuperuserAccessBackend(Service):
     def should_allow_superuser_access(
@@ -11,7 +18,7 @@ class PreventSuperuserAccessBackend(Service):
         raise NotImplementedError
 
 
-class PreventSuperuserAccess(Service):
+class PreventSuperuserAccess(PreventSuperuserAccessBackend):
     def should_allow_superuser_access(
         self,
         organization_context: Organization | RpcUserOrganizationContext,
