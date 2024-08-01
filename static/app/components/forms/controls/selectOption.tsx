91f96e88298b66053ceaf2f446d8f329406f2deb
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-08-01 14:47:05 +0200

    ref(selectoption): Tweak list items alignment

--- static/app/components/forms/controls/selectOption.tsx
+++ static/app/components/forms/controls/selectOption.tsx
@@ -4,7 +4,7 @@ import {ClassNames} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import MenuListItem from 'sentry/components/menuListItem';
-import {IconCheckmark} from 'sentry/icons';
+import {IconAdd, IconCheckmark} from 'sentry/icons';
 import {defined} from 'sentry/utils';
 
 type Props = React.ComponentProps<typeof selectComponents.Option>;
@@ -56,17 +56,28 @@ function SelectOption(props: Props) {
           innerWrapProps={{'data-test-id': value}}
           labelProps={{as: typeof label === 'string' ? 'p' : 'div'}}
           leadingItems={
-            <Fragment>
-              <CheckWrap isMultiple={isMultiple} isSelected={isSelected}>
-                {isSelected && (
-                  <IconCheckmark
-                    size={isMultiple ? 'xs' : 'sm'}
-                    color={isMultiple ? 'white' : undefined}
-                  />
-                )}
-              </CheckWrap>
-              {data.leadingItems}
-            </Fragment>
+            itemProps.__isNew__ ? (
+              <Fragment>
+                <IconAdd size="sm" />
+                {data.leadingItems}
+              </Fragment>
+            ) : isMultiple ? (
+              <Fragment>
+                <CheckWrap isMultiple isSelected={isSelected}>
+                  {isSelected && <IconCheckmark size="xs" color="white" />}
+                </CheckWrap>
+                {data.leadingItems}
+              </Fragment>
+            ) : (
+              isSelected && (
+                <Fragment>
+                  <CheckWrap isMultiple={false} isSelected>
+                    <IconCheckmark size="sm" />
+                  </CheckWrap>
+                  {data.leadingItems}
+                </Fragment>
+              )
+            )
           }
         />
       )}
