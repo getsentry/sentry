04063c5670dfe317504ce273a460f98ed331d1b6
Ryan Skonnord <ryan.skonnord@sentry.io>
2024-08-02 11:33:54 -0700

    Extract functions used only by single endpoints

--- src/sentry/incidents/endpoints/organization_incident_seen.py
+++ src/sentry/incidents/endpoints/organization_incident_seen.py
@@ -1,3 +1,4 @@
+from django.utils import timezone as django_timezone
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -5,7 +6,8 @@ from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases.incident import IncidentEndpoint, IncidentPermission
-from sentry.incidents.logic import set_incident_seen
+from sentry.incidents.models.incident import Incident, IncidentProject, IncidentSeen
+from sentry.users.services.user import RpcUser
 from sentry.users.services.user.serial import serialize_generic_user
 
 
@@ -27,3 +29,32 @@ class OrganizationIncidentSeenEndpoint(IncidentEndpoint):
 
         set_incident_seen(incident=incident, user=serialize_generic_user(request.user))
         return Response({}, status=201)
+
+
+def set_incident_seen(incident: Incident, user: RpcUser | None = None) -> bool:
+    """
+    Updates the incident to be seen
+    """
+
+    is_org_member = incident.organization.has_access(user)
+
+    if is_org_member:
+        is_project_member = False
+        for incident_project in IncidentProject.objects.filter(incident=incident).select_related(
+            "project"
+        ):
+            if incident_project.project.member_set.filter(
+                user_id=user.id if user else None
+            ).exists():
+                is_project_member = True
+                break
+
+        if is_project_member:
+            incident_seen, created = IncidentSeen.objects.create_or_update(
+                incident=incident,
+                user_id=user.id if user else None,
+                values={"last_seen": django_timezone.now()},
+            )
+            return incident_seen
+
+    return False
--- src/sentry/incidents/logic.py
+++ src/sentry/incidents/logic.py
@@ -48,7 +48,6 @@ from sentry.incidents.models.incident import (
     IncidentActivity,
     IncidentActivityType,
     IncidentProject,
-    IncidentSeen,
     IncidentStatus,
     IncidentStatusMethod,
     IncidentSubscription,
@@ -247,35 +246,6 @@ def update_incident_status(
         return incident
 
 
-def set_incident_seen(incident: Incident, user: RpcUser | None = None) -> bool:
-    """
-    Updates the incident to be seen
-    """
-
-    is_org_member = incident.organization.has_access(user)
-
-    if is_org_member:
-        is_project_member = False
-        for incident_project in IncidentProject.objects.filter(incident=incident).select_related(
-            "project"
-        ):
-            if incident_project.project.member_set.filter(
-                user_id=user.id if user else None
-            ).exists():
-                is_project_member = True
-                break
-
-        if is_project_member:
-            incident_seen, created = IncidentSeen.objects.create_or_update(
-                incident=incident,
-                user_id=user.id if user else None,
-                values={"last_seen": django_timezone.now()},
-            )
-            return incident_seen
-
-    return False
-
-
 @transaction.atomic(router.db_for_write(Incident))
 def create_incident_activity(
     incident: Incident,
@@ -335,22 +305,6 @@ def create_incident_activity(
         )
 
     return activity
-
-
-def update_comment(activity: IncidentActivity, comment: str) -> int:
-    """
-    Specifically updates an IncidentActivity with type IncidentActivityType.COMMENT
-    """
-
-    return activity.update(comment=comment)
-
-
-def delete_comment(activity: IncidentActivity) -> tuple[int, dict[str, int]]:
-    """
-    Specifically deletes an IncidentActivity with type IncidentActivityType.COMMENT
-    """
-
-    return activity.delete()
 
 
 def _build_incident_query_builder(
