8db61dd99d8e16d01f2618088ccdea218429bd9b
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-08-02 15:39:41 -0700

    Remove priority feature flags

--- tests/sentry/api/endpoints/test_project_rules_configuration.py
+++ tests/sentry/api/endpoints/test_project_rules_configuration.py
@@ -191,21 +191,8 @@ class ProjectRuleConfigurationTest(APITestCase):
         filter_ids = {f["id"] for f in response.data["filters"]}
         assert IssueCategoryFilter.id in filter_ids
 
-    def test_high_priority_issue_condition_feature(self):
-        # Hide the high priority issue condition when high-priority-alerts is off
-        with self.feature({"projects:high-priority-alerts": False}):
-            response = self.get_success_response(self.organization.slug, self.project.slug)
-            assert (
-                "sentry.rules.conditions.high_priority_issue.NewHighPriorityIssueCondition"
-                not in [filter["id"] for filter in response.data["conditions"]]
-            )
-            assert (
-                "sentry.rules.conditions.high_priority_issue.ExistingHighPriorityIssueCondition"
-                not in [filter["id"] for filter in response.data["conditions"]]
-            )
-
-        # Show the high priority issue condition when high-priority-alerts is on
-        with self.feature({"projects:high-priority-alerts": True}):
+    def test_high_priority_issue_condition(self):
+        with self.feature({"projects:priority-ga-features": True}):
             response = self.get_success_response(self.organization.slug, self.project.slug)
             assert "sentry.rules.conditions.high_priority_issue.NewHighPriorityIssueCondition" in [
                 filter["id"] for filter in response.data["conditions"]
