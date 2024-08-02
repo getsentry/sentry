9246bacbccf66defcc94acb93fb3bdd480a77527
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-08-01 21:25:02 -0700

    Add tooltip + some comments and cleanup

--- static/app/components/devtoolbar/components/replay/replayPanel.tsx
+++ static/app/components/devtoolbar/components/replay/replayPanel.tsx
@@ -1,4 +1,4 @@
-import {Fragment, useEffect, useState} from 'react';
+import {useEffect, useState} from 'react';
 import {css} from '@emotion/react';
 import type {ReplayRecordingMode} from '@sentry/types';
 
@@ -7,7 +7,6 @@ import SentryAppLink from 'sentry/components/devtoolbar/components/sentryAppLink
 import {resetFlexRowCss} from 'sentry/components/devtoolbar/styles/reset';
 import ProjectBadge from 'sentry/components/idBadge/projectBadge';
 import {IconPause, IconPlay} from 'sentry/icons';
-import {t} from 'sentry/locale';
 import type {PlatformKey} from 'sentry/types/project';
 
 import useConfiguration from '../../hooks/useConfiguration';
@@ -61,74 +60,77 @@ export default function ReplayPanel() {
 
   return (
     <PanelLayout title="Session Replay">
-      <Fragment>
-        <Button
-          size="sm"
-          icon={isRecording ? <IconPause /> : <IconPlay />}
-          disabled={!replay} // TODO: tooltip on why this is disabled,
-          onClick={() => {
-            if (replay) {
-              if (isRecording) {
-                replay.stop(); // stop should call flush
-              } else {
-                // If we're sampling errors, the we might be in buffering mode.
-                // In this case start() will do nothing. flush() will switch to session mode + start recording.
-                replay.flush();
-              }
-              setSessionId(replay.getReplayId()); // this will set isRecording
+      <Button
+        size="sm"
+        icon={isRecording ? <IconPause /> : <IconPlay />}
+        disabled={!replay}
+        title={
+          replay ? undefined : 'Your app is missing the session replay integration.'
+          // TODO: should we also do FF checks?
+        }
+        onClick={() => {
+          if (replay) {
+            if (isRecording) {
+              replay.stop(); // stop should call flush
+            } else {
+              // If we're sampling errors, then we might be in buffer mode, and
+              // start() will do nothing. In either mode, flush() will set the
+              // mode to session and start recording.
+              replay.flush();
             }
-          }}
-        >
-          {isRecording
-            ? t('In progress. Click to stop recording')
-            : t('Start recording the current session')}
-        </Button>
-        <div css={[smallCss, panelSectionCss, panelInsetContentCss]}>
-          {lastReplayId ? (
-            <span css={[resetFlexRowCss, {gap: 'var(--space50)'}]}>
-              {isRecording ? t('Current replay: ') : t('Last recorded replay: ')}
-              <SentryAppLink
-                to={{
-                  url: `/replays/${lastReplayId}`,
-                  query: {project: projectId},
-                }}
-                onClick={() => {
-                  trackAnalytics?.({
-                    eventKey: `devtoolbar.current-replay-link.click`,
-                    eventName: `devtoolbar: Current replay link clicked`,
-                  });
-                }}
+            setSessionId(replay.getReplayId()); // this will set isRecording
+          }
+        }}
+      >
+        {isRecording
+          ? 'In progress. Click to stop recording'
+          : 'Start recording the current session'}
+      </Button>
+      <div css={[smallCss, panelSectionCss, panelInsetContentCss]}>
+        {lastReplayId ? (
+          <span css={[resetFlexRowCss, {gap: 'var(--space50)'}]}>
+            {isRecording ? 'Current replay: ' : 'Last recorded replay: '}
+            <SentryAppLink
+              to={{
+                url: `/replays/${lastReplayId}`,
+                query: {project: projectId},
+              }}
+              onClick={() => {
+                trackAnalytics?.({
+                  eventKey: `devtoolbar.current-replay-link.click`,
+                  eventName: `devtoolbar: Current replay link clicked`,
+                });
+              }}
+            >
+              <div
+                css={[
+                  resetFlexRowCss,
+                  {
+                    display: 'inline-flex',
+                    gap: 'var(--space50)',
+                    alignItems: 'center',
+                  },
+                ]}
               >
-                <div
-                  css={[
-                    resetFlexRowCss,
-                    {
-                      display: 'inline-flex',
-                      gap: 'var(--space50)',
-                      alignItems: 'center',
-                    },
-                  ]}
-                >
-                  <ProjectBadge
-                    css={css({'&& img': {boxShadow: 'none'}})}
-                    project={{
-                      slug: projectSlug,
-                      id: projectId,
-                      platform: projectPlatform as PlatformKey,
-                    }}
-                    avatarSize={16}
-                    hideName
-                    avatarProps={{hasTooltip: false}}
-                  />
-                  {lastReplayId.slice(0, TRUNC_ID_LENGTH)}
-                </div>
-              </SentryAppLink>
-            </span>
-          ) : (
-            'No replay'
-          )}
-        </div>
-      </Fragment>
+                <ProjectBadge
+                  css={css({'&& img': {boxShadow: 'none'}})}
+                  project={{
+                    slug: projectSlug,
+                    id: projectId,
+                    platform: projectPlatform as PlatformKey,
+                  }}
+                  avatarSize={16}
+                  hideName
+                  avatarProps={{hasTooltip: false}}
+                />
+                {lastReplayId.slice(0, TRUNC_ID_LENGTH)}
+              </div>
+            </SentryAppLink>
+          </span>
+        ) : (
+          'No replay is recording this session.'
+        )}
+      </div>
     </PanelLayout>
   );
 }
