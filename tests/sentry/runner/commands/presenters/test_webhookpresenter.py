a697db59ec33180b3cd672b04dca6d64d64c5771
Ian Woodard <17186604+IanWoodard@users.noreply.github.com>
2024-08-02 09:50:22 -0700

    Updating tests

--- tests/sentry/runner/commands/presenters/test_webhookpresenter.py
+++ tests/sentry/runner/commands/presenters/test_webhookpresenter.py
@@ -10,7 +10,7 @@ from sentry.utils import json
 @responses.activate
 @override_settings(
     OPTIONS_AUTOMATOR_SLACK_WEBHOOK_URL="https://test/",
-    OPTIONS_AUTOMATOR_HMAC_SECRET_KEY="test-secret-key",
+    OPTIONS_AUTOMATOR_HMAC_SECRET="test-secret-key",
     SENTRY_REGION="test_region",
 )
 def test_is_slack_enabled():
@@ -81,13 +81,94 @@ def test_is_slack_enabled():
 
     assert responses.calls[0].response.status_code == 200
     assert expected_json_data == json.loads(responses.calls[0].request.body)
+    assert (
+        responses.calls[0].request.headers["x-sentry-options-signature"]
+        == "6d984a54d1ecb82014f162c5cb620205310fb6f6343b21a80c19563cf48f5e95"
+    )
 
 
 @pytest.mark.django_db
 @responses.activate
 @override_settings(
     OPTIONS_AUTOMATOR_SLACK_WEBHOOK_URL="https://test/",
-    OPTIONS_AUTOMATOR_HMAC_SECRET_KEY="test-secret-key",
+    SENTRY_REGION="test_region",
+)
+def test_is_slack_enabled_without_secret_key():
+    responses.add(responses.POST, "https://test/", status=200)
+
+    presenter = WebhookPresenter("options-automator")
+    assert presenter.is_webhook_enabled()
+    presenter.set("option1", "value1")
+    presenter.set("option2", "value2")
+
+    presenter.unset("option3")
+    presenter.unset("option4")
+
+    presenter.update("option5", "db_value5", "value5")
+    presenter.update("option6", "db_value6", "value6")
+
+    presenter.channel_update("option7")
+    presenter.channel_update("option8")
+
+    presenter.drift("option9", "db_value9")
+    presenter.drift("option10", "db_value10")
+
+    presenter.not_writable("option11", "error_reason11")
+    presenter.not_writable("option12", "error_reason12")
+
+    presenter.unregistered("option13")
+    presenter.unregistered("option14")
+
+    presenter.invalid_type("option15", str, int)
+    presenter.invalid_type("option16", float, int)
+
+    presenter.flush()
+
+    expected_json_data = {
+        "region": "test_region",
+        "source": "options-automator",
+        "drifted_options": [
+            {"option_name": "option9", "option_value": "db_value9"},
+            {"option_name": "option10", "option_value": "db_value10"},
+        ],
+        "updated_options": [
+            {"option_name": "option5", "db_value": "db_value5", "value": "value5"},
+            {"option_name": "option6", "db_value": "db_value6", "value": "value6"},
+        ],
+        "set_options": [
+            {"option_name": "option1", "option_value": "value1"},
+            {"option_name": "option2", "option_value": "value2"},
+        ],
+        "unset_options": ["option3", "option4"],
+        "not_writable_options": [
+            {"option_name": "option11", "error_msg": "error_reason11"},
+            {"option_name": "option12", "error_msg": "error_reason12"},
+        ],
+        "unregistered_options": ["option13", "option14"],
+        "invalid_type_options": [
+            {
+                "option_name": "option15",
+                "got_type": "<class 'str'>",
+                "expected_type": "<class 'int'>",
+            },
+            {
+                "option_name": "option16",
+                "got_type": "<class 'float'>",
+                "expected_type": "<class 'int'>",
+            },
+        ],
+    }
+
+    assert responses.calls[0].response.status_code == 200
+    assert expected_json_data == json.loads(responses.calls[0].request.body)
+    assert "x-sentry-options-signature" not in responses.calls[0].request.headers
+
+
+@pytest.mark.django_db
+@responses.activate
+@override_settings(
+    OPTIONS_AUTOMATOR_SLACK_WEBHOOK_URL="https://test/",
+    OPTIONS_AUTOMATOR_HMAC_SECRET="test-secret-key",
     SENTRY_REGION="test_region",
 )
 def test_slack_presenter_empty():
@@ -102,7 +183,7 @@ def test_slack_presenter_empty():
 @responses.activate
 @override_settings(
     OPTIONS_AUTOMATOR_SLACK_WEBHOOK_URL="https://test/",
-    OPTIONS_AUTOMATOR_HMAC_SECRET_KEY="test-secret-key",
+    OPTIONS_AUTOMATOR_HMAC_SECRET="test-secret-key",
     SENTRY_REGION="test_region",
 )
 def test_slack_presenter_methods_with_different_types():
@@ -148,3 +229,7 @@ def test_slack_presenter_methods_with_different_types():
 
     assert responses.calls[0].response.status_code == 200
     assert expected_json_data == json.loads(responses.calls[0].request.body)
+    assert (
+        responses.calls[0].request.headers["x-sentry-options-signature"]
+        == "8b7827d99b4373d1a969e38d2813be1ee8de3f4a75c46223af548c04a053f98a"
+    )
