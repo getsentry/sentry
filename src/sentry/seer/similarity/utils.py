c434ff1d1a85fdc9f09ed50b22b4b346d9b358e9
Katie Byers <katie.byers@sentry.io>
2024-08-05 12:04:49 -0700

    chore(seer grouping): Remove redundant metrics (#75565)
    We have a bunch of metrics which were added before the `did_call_seer` metric was added, and which are now duplicated by instances of that metric with tags corresponding to the metrics in question. (For example, where we've been collecting a `grouping.similarity.seer_global_killswitch_enabled` metric, we've also been collecting the `did_call_seer` metric with the tag `blocker: global-killswitch`.) This removes all of the redundant metrics (which, it should be noted, we also haven't even been using).

--- src/sentry/seer/similarity/utils.py
+++ src/sentry/seer/similarity/utils.py
@@ -171,7 +171,6 @@ def killswitch_enabled(project_id: int, event: Event | None = None) -> bool:
             "should_call_seer_for_grouping.seer_global_killswitch_enabled",
             extra=logger_extra,
         )
-        metrics.incr("grouping.similarity.seer_global_killswitch_enabled")
         metrics.incr(
             "grouping.similarity.did_call_seer",
             sample_rate=1.0,
@@ -184,7 +183,6 @@ def killswitch_enabled(project_id: int, event: Event | None = None) -> bool:
             "should_call_seer_for_grouping.seer_similarity_killswitch_enabled",
             extra=logger_extra,
         )
-        metrics.incr("grouping.similarity.seer_similarity_killswitch_enabled")
         metrics.incr(
             "grouping.similarity.did_call_seer",
             sample_rate=1.0,
