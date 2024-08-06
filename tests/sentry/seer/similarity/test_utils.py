af3288f56b1bbd4aca55bc11e6535eebbf6b6481
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-08-06 11:09:05 -0700

    ref(similarity): Limit stacktrace to 30 chained exceptions (#75605)
    Limit stacktrace to 30 chained exceptions in similarity stacktrace
    parsing
    Add distribution metric for number of exceptions

--- tests/sentry/seer/similarity/test_utils.py
+++ tests/sentry/seer/similarity/test_utils.py
@@ -634,6 +634,22 @@ class GetStacktraceStringTest(TestCase):
                 == expected_frame_count
             )
 
+    def test_chained_too_many_exceptions(self):
+        """Test that we restrict number of chained exceptions to 30."""
+        data_chained_exception = copy.deepcopy(self.CHAINED_APP_DATA)
+        data_chained_exception["app"]["component"]["values"][0]["values"] = [
+            self.create_exception(
+                exception_type_str="Exception",
+                exception_value=f"exception {i} message!",
+                frames=self.create_frames(num_frames=1, context_line_factory=lambda i: f"line {i}"),
+            )
+            for i in range(1, 32)
+        ]
+        stacktrace_str = get_stacktrace_string(data_chained_exception)
+        for i in range(2, 32):
+            assert f"exception {i} message!" in stacktrace_str
+        assert "exception 1 message!" not in stacktrace_str
+
     def test_thread(self):
         stacktrace_str = get_stacktrace_string(self.MOBILE_THREAD_DATA)
         assert stacktrace_str == 'File "", function TestHandler'
