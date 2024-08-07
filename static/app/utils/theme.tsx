1cd5337289f91b1f94c1ddf614354ef50b54be21
JonasBa <jonas@badalic.com>
2024-08-07 19:30:54 -0400

    ref(styles) deprecate space export and add space to theme

--- static/app/utils/theme.tsx
+++ static/app/utils/theme.tsx
@@ -3,6 +3,7 @@ import color from 'color';
 
 import {DATA_CATEGORY_INFO} from 'sentry/constants';
 import {CHART_PALETTE} from 'sentry/constants/chartPalette';
+import {themeSpace} from 'sentry/styles/space';
 import {Outcome} from 'sentry/types';
 
 const lightColors = {
@@ -951,6 +952,7 @@ export const lightTheme = {
     ...darkColors,
     ...darkAliases,
   },
+  space: themeSpace,
   ...generateUtils(lightColors, lightAliases),
   alert: generateAlertTheme(lightColors, lightAliases),
   badge: generateBadgeTheme(lightColors),
@@ -979,6 +981,7 @@ export const darkTheme: Theme = {
     ...lightColors,
     ...lightAliases,
   },
+  space: themeSpace,
   ...generateUtils(darkColors, lightAliases),
   alert: generateAlertTheme(darkColors, darkAliases),
   badge: generateBadgeTheme(darkColors),
