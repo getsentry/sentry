c692343c3b637c4ec1a5525f8e24585fc57c8407
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-08-07 13:55:56 +0200

    revert

--- static/app/views/metrics/pageHeaderActions.tsx
+++ static/app/views/metrics/pageHeaderActions.tsx
@@ -18,6 +18,7 @@ import {
 } from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {trackAnalytics} from 'sentry/utils/analytics';
+import {isCustomMeasurement} from 'sentry/utils/metrics';
 import {
   hasCustomMetricsExtractionRules,
   hasMetricsNewInputs,
