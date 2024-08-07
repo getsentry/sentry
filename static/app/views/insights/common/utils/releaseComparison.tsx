efc4f00e9617d23439d0a28c508d9cb319e9a554
Shruthi <shruthilaya.jaganathan@sentry.io>
2024-08-07 15:40:02 -0400

    fix(insights): Escape release names (#75760)
    Use addDisjunctionFilterValues to add release conditions,
    since it escapes release names correctly

--- static/app/views/insights/common/utils/releaseComparison.tsx
+++ static/app/views/insights/common/utils/releaseComparison.tsx
@@ -14,7 +14,7 @@ export function appendReleaseFilters(
   ) {
     queryString = query
       .copy()
-      .addStringFilter(`release:[${primaryRelease},${secondaryRelease}]`)
+      .addDisjunctionFilterValues('release', [primaryRelease, secondaryRelease])
       .formatString();
   } else if (defined(primaryRelease)) {
     queryString = query
