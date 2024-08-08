0dd4f58005048783ac7e2133b1bfebcee3c8696f
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-08-08 11:06:09 -0400

    chore(dashboards): Remove `dashboards-rh-widget` feature flag declaration (#75767)
    Code removed in https://github.com/getsentry/sentry/pull/75669

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -76,8 +76,6 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:dashboards-import", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable metrics enhanced performance in dashboards
     manager.add("organizations:dashboards-mep", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable release health widget in dashboards
-    manager.add("organizations:dashboards-rh-widget", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     manager.add("organizations:dashboards-span-metrics", OrganizationFeature, FeatureHandlerStrategy.OPTIONS, api_expose=False)
     # Enable the dev toolbar PoC code for employees
     manager.add("organizations:devtoolbar", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, default=False, api_expose=True)
