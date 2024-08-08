9b55c023b926afdbef3dda116d2ce2033be7c9d6
Tony Xiao <txiao@sentry.io>
2024-08-08 12:20:16 -0400

    Update src/sentry/features/temporary.py
    Co-authored-by: William Mak <william@wmak.io>

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -307,7 +307,7 @@ def register_temporary_features(manager: FeatureManager):
     # Enable showing INP web vital in default views
     manager.add("organizations:performance-vitals-inp", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable the new explore page
-    manager.add("organizations:visibility-explore", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    manager.add("organizations:visibility-explore-view", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable profiling
     manager.add("organizations:profiling", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=True)
     # Enabled for those orgs who participated in the profiling Beta program
