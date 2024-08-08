41dbc2ddb393e19f8c3d4307c6253a0edc4adbcb
Mia Hsu <ameliahsu@gmail.com>
2024-08-08 10:47:48 -0700

    add test

--- static/app/views/alerts/rules/issue/setupMessagingIntegrationButton.spec.tsx
+++ static/app/views/alerts/rules/issue/setupMessagingIntegrationButton.spec.tsx
@@ -5,6 +5,7 @@ import {ProjectFixture} from 'sentry-fixture/project';
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import {openModal} from 'sentry/actionCreators/modal';
+import HookStore from 'sentry/stores/hookStore';
 import SetupMessagingIntegrationButton from 'sentry/views/alerts/rules/issue/setupMessagingIntegrationButton';
 
 jest.mock('sentry/actionCreators/modal');
@@ -73,7 +74,7 @@ describe('SetupAlertIntegrationButton', function () {
     expect(openModal).toHaveBeenCalled();
   });
 
-  it('returns null if project is loading', function () {
+  it('does not render button if project is loading', function () {
     MockApiClient.addMockResponse({
       url: `/projects/${organization.slug}/${project.slug}/`,
       statusCode: 400,
@@ -81,5 +82,32 @@ describe('SetupAlertIntegrationButton', function () {
     });
     render(getComponent(), {organization: organization});
     expect(screen.queryByRole('button')).not.toBeInTheDocument();
+  });
+
+  it('disables button if user does not have integration feature', async function () {
+    MockApiClient.addMockResponse({
+      url: `/projects/${organization.slug}/${project.slug}/`,
+      body: {
+        ...project,
+        hasAlertIntegrationInstalled: false,
+      },
+    });
+
+    HookStore.add('integrations:feature-gates', () => {
+      return {
+        IntegrationFeatures: p =>
+          p.children({
+            disabled: true,
+            disabledReason: 'some reason',
+            ungatedFeatures: p.features,
+            gatedFeatureGroups: [],
+          }),
+        FeatureList: p => (p.provider = providers[0]),
+      };
+    });
+
+    render(getComponent(), {organization: organization});
+    await screen.findByRole('button', {name: /connect to messaging/i});
+    expect(screen.getByRole('button')).toBeDisabled();
   });
 });
