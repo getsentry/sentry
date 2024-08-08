ab018027a67dbf58398303ae6c5e7f2f511f56b3
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-08-08 14:15:33 -0400

    fix(ui): Center integration install footer (#75847)

--- static/app/components/modals/sentryAppDetailsModal.tsx
+++ static/app/components/modals/sentryAppDetailsModal.tsx
@@ -263,6 +263,7 @@ const Permission = styled('div')`
 
 const Footer = styled('div')`
   display: flex;
+  align-items: center;
   padding: 20px 30px;
   border-top: 1px solid #e2dee6;
   margin: 20px -30px -30px;
