1e4fe3db8f2bb6ec63e1aa1610786bfc93af6f04
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-08-08 12:33:12 -0400

    ref: improve typing of get_digest_as_context (#75823)
    <!-- Describe your PR here. -->

--- src/sentry/digests/utils.py
+++ src/sentry/digests/utils.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from collections import Counter, defaultdict
 from collections.abc import Iterable, Mapping, Sequence
 from datetime import datetime
-from typing import Any
+from typing import TypedDict
 
 from django.db.models import Q
 
@@ -56,7 +56,15 @@ def should_get_personalized_digests(target_type: ActionTargetType, project_id: i
     )
 
 
-def get_digest_as_context(digest: Digest) -> Mapping[str, Any]:
+class _DigestContext(TypedDict):
+    counts: Counter[Group]
+    digest: Digest
+    group: Group
+    end: datetime | None
+    start: datetime | None
+
+
+def get_digest_as_context(digest: Digest) -> _DigestContext:
     start, end, counts = get_digest_metadata(digest)
     group = next(iter(counts))
 
