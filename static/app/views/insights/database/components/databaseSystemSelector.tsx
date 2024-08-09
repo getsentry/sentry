110e21cd45bc279c2ed0eb03fc763f276c9f0b36
Ash Anand <ash.anand@sentry.io>
2024-08-09 17:41:44 -0400

    map possible system values to proper label text

--- static/app/views/insights/database/components/databaseSystemSelector.tsx
+++ static/app/views/insights/database/components/databaseSystemSelector.tsx
@@ -4,6 +4,32 @@ import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import {useSpanMetrics} from 'sentry/views/insights/common/queries/useDiscover';
 import {SpanMetricsField} from 'sentry/views/insights/types';
 
+/**
+ * The supported relational database system values are based on what is
+ * set in the Sentry Python SDK. The only supported NoSQL DBMS is MongoDB.
+ *
+ * https://github.com/getsentry/sentry-python/blob/master/sentry_sdk/integrations/sqlalchemy.py#L125
+ */
+enum SupportedDatabaseSystems {
+  // SQL
+  SQLITE = 'sqlite',
+  POSTGRESQL = 'postgresql',
+  MARIADB = 'mariadb',
+  MYSQL = 'mysql',
+  ORACLE = 'oracle',
+  // NoSQL
+  MONGODB = 'mongodb',
+}
+
+const DATABASE_SYSTEM_TO_LABEL: Record<SupportedDatabaseSystems, string> = {
+  [SupportedDatabaseSystems.SQLITE]: 'SQLite',
+  [SupportedDatabaseSystems.POSTGRESQL]: 'PostgreSQL',
+  [SupportedDatabaseSystems.MARIADB]: 'MariaDB',
+  [SupportedDatabaseSystems.MYSQL]: 'MySQL',
+  [SupportedDatabaseSystems.ORACLE]: 'Oracle',
+  [SupportedDatabaseSystems.MONGODB]: 'MongoDB',
+};
+
 export function DatabaseSystemSelector() {
   const handleChange = () => {};
 
@@ -22,10 +48,17 @@ export function DatabaseSystemSelector() {
     return null;
   }
 
-  const options = [
-    {value: 'PostgreSQL', label: 'PostgreSQL'},
-    {value: 'MongoDB', label: 'MongoDB'},
-  ];
+  const options = data.map(entry => {
+    const system = entry['span.system'];
+    const label =
+      system in DATABASE_SYSTEM_TO_LABEL
+        ? DATABASE_SYSTEM_TO_LABEL[system]
+        : system === ''
+          ? t('Unknown')
+          : system;
+
+    return {value: system, label};
+  });
 
   const getDefaultValue = () => {
     if (isLoading || isError) {
