53796e4cc9af6cb86348cf66e1e4b59a2865a537
Ash Anand <ash.anand@sentry.io>
2024-08-09 16:41:12 -0400

    query for db system metrics data

--- static/app/views/insights/database/components/databaseSystemSelector.tsx
+++ static/app/views/insights/database/components/databaseSystemSelector.tsx
@@ -1,8 +1,22 @@
 import {CompactSelect} from 'sentry/components/compactSelect';
 import {t} from 'sentry/locale';
+import {MutableSearch} from 'sentry/utils/tokenizeSearch';
+import {useSpanMetrics} from 'sentry/views/insights/common/queries/useDiscover';
 
 export function DatabaseSystemSelector() {
   const handleChange = () => {};
+
+  const {data, isLoading, isError} = useSpanMetrics(
+    {
+      search: MutableSearch.fromQueryObject({'span.op': 'db'}),
+
+      fields: ['span.system', 'count()'],
+      sorts: [{field: 'count()', kind: 'desc'}],
+    },
+    'referrer'
+  );
+
+  console.dir(data);
 
   const options = [
     {value: 'PostgreSQL', label: 'PostgreSQL'},
@@ -13,7 +27,6 @@ export function DatabaseSystemSelector() {
     <CompactSelect
       onChange={handleChange}
       options={options}
-      title="ffdfd"
       triggerProps={{prefix: t('DB System')}}
       defaultValue={options[0].value}
     />
