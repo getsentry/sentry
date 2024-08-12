863161051de850c368b1006ac4895c368627ec32
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-08-12 15:28:41 -0700

    :art: make sure event is not none

--- src/sentry/integrations/mixins/issues.py
+++ src/sentry/integrations/mixins/issues.py
@@ -121,7 +121,7 @@ class IssueBasicMixin:
 
     def get_group_description(self, group, event, **kwargs):
         output = self.get_group_link(group, **kwargs)
-        if isinstance(event, GroupEvent) and event.occurrence is not None:
+        if event and isinstance(event, GroupEvent) and event.occurrence is not None:
             body = ""
             if group.issue_category == GroupCategory.FEEDBACK:
                 body = self.get_feedback_issue_body(event)
