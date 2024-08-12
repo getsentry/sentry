e7b892ef8dc09ce18b3614d75ccfdc793b022e4c
Alberto Leal <mail4alberto@gmail.com>
2024-08-12 19:57:04 -0400

    add metrics

--- src/sentry/buffer/redis.py
+++ src/sentry/buffer/redis.py
@@ -515,10 +515,14 @@ class RedisBuffer(Buffer):
             # model associated with the model_key.
             process_incr_kwargs: dict[str, Any] = dict()
             if model_key is None:
+                metrics.incr("buffer.process-incr.model-key-missing")
                 return process_incr_kwargs
             queue = pending_buffers_router.queue(model_key=model_key)
             if queue is not None:
                 process_incr_kwargs["queue"] = queue
+                metrics.incr("buffer.process-incr-queue", tags={"queue": queue})
+            else:
+                metrics.incr("buffer.process-incr-default-queue")
             return process_incr_kwargs
 
         try:
