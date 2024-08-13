37b8d9be25d38d47e1004724167d22db4d29dbca
Malachi Willey <malwilley@gmail.com>
2024-08-13 09:34:03 -0700

    feat(query-builder): Display uncategorized items when 'all' is selected

--- static/app/components/searchQueryBuilder/tokens/filterKeyListBox/utils.tsx
+++ static/app/components/searchQueryBuilder/tokens/filterKeyListBox/utils.tsx
@@ -1,7 +1,66 @@
 import {getEscapedKey} from 'sentry/components/compactSelect/utils';
+import {KeyDescription} from 'sentry/components/searchQueryBuilder/tokens/filterKeyListBox/keyDescription';
+import type {
+  KeyItem,
+  KeySectionItem,
+} from 'sentry/components/searchQueryBuilder/tokens/filterKeyListBox/types';
+import type {
+  FieldDefinitionGetter,
+  FilterKeySection,
+} from 'sentry/components/searchQueryBuilder/types';
+import type {Tag, TagCollection} from 'sentry/types/group';
+import {type FieldDefinition, FieldKind} from 'sentry/utils/fields';
 
 const RECENT_FILTER_KEY_PREFIX = '__recent_filter_key__';
 
 export function createRecentFilterOptionKey(filter: string) {
   return getEscapedKey(`${RECENT_FILTER_KEY_PREFIX}${filter}`);
+}
+
+export function getKeyLabel(
+  tag: Tag,
+  fieldDefinition: FieldDefinition | null,
+  {includeAggregateArgs = false} = {}
+) {
+  if (fieldDefinition?.kind === FieldKind.FUNCTION) {
+    if (fieldDefinition.parameters?.length) {
+      if (includeAggregateArgs) {
+        return `${tag.key}(${fieldDefinition.parameters.map(p => p.name).join(', ')})`;
+      }
+      return `${tag.key}(...)`;
+    }
+    return `${tag.key}()`;
+  }
+
+  return tag.key;
+}
+
+export function createSection(
+  section: FilterKeySection,
+  keys: TagCollection,
+  getFieldDefinition: FieldDefinitionGetter
+): KeySectionItem {
+  return {
+    key: section.value,
+    value: section.value,
+    label: section.label,
+    options: section.children.map(key => createItem(keys[key], getFieldDefinition(key))),
+    type: 'section',
+  };
+}
+
+export function createItem(tag: Tag, fieldDefinition: FieldDefinition | null): KeyItem {
+  const description = fieldDefinition?.desc;
+
+  return {
+    key: getEscapedKey(tag.key),
+    label: getKeyLabel(tag, fieldDefinition),
+    description: description ?? '',
+    value: tag.key,
+    textValue: tag.key,
+    hideCheck: true,
+    showDetailsInOverlay: true,
+    details: <KeyDescription tag={tag} />,
+    type: 'item',
+  };
 }
