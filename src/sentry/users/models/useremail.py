5c101d75d3a66ce3dc181f98259855be7901de2a
Christinarlong <christina.long@sentry.io>
2024-08-13 15:42:43 -0700

    added empty string check for validation hash

--- src/sentry/users/models/useremail.py
+++ src/sentry/users/models/useremail.py
@@ -93,6 +93,7 @@ class UserEmail(ControlOutboxProducingModel):
         self.validation_hash = get_secure_token()
 
     def hash_is_valid(self) -> bool:
+        assert self.validation_hash, "Validation hash can't be empty"
         return self.validation_hash and self.date_hash_added > timezone.now() - timedelta(hours=48)
 
     def is_primary(self) -> bool:
