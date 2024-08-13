45ec5d3432bbf6549da82cd16eb3798f6d65659b
Ash Anand <ash.anand@sentry.io>
2024-08-13 13:11:19 -0400

    create hook to separate logic for fetching and saving selected db system

--- static/app/views/insights/database/components/databaseSystemSelector.tsx
+++ static/app/views/insights/database/components/databaseSystemSelector.tsx
@@ -1,80 +1,21 @@
 import {CompactSelect} from 'sentry/components/compactSelect';
 import {t} from 'sentry/locale';
-import {MutableSearch} from 'sentry/utils/tokenizeSearch';
-import {useSpanMetrics} from 'sentry/views/insights/common/queries/useDiscover';
-import {SpanMetricsField} from 'sentry/views/insights/types';
-
-/**
- * The supported relational database system values are based on what is
- * set in the Sentry Python SDK. The only supported NoSQL DBMS is MongoDB.
- *
- * https://github.com/getsentry/sentry-python/blob/master/sentry_sdk/integrations/sqlalchemy.py#L125
- */
-enum SupportedDatabaseSystems {
-  // SQL
-  SQLITE = 'sqlite',
-  POSTGRESQL = 'postgresql',
-  MARIADB = 'mariadb',
-  MYSQL = 'mysql',
-  ORACLE = 'oracle',
-  // NoSQL
-  MONGODB = 'mongodb',
-}
-
-const DATABASE_SYSTEM_TO_LABEL: Record<SupportedDatabaseSystems, string> = {
-  [SupportedDatabaseSystems.SQLITE]: 'SQLite',
-  [SupportedDatabaseSystems.POSTGRESQL]: 'PostgreSQL',
-  [SupportedDatabaseSystems.MARIADB]: 'MariaDB',
-  [SupportedDatabaseSystems.MYSQL]: 'MySQL',
-  [SupportedDatabaseSystems.ORACLE]: 'Oracle',
-  [SupportedDatabaseSystems.MONGODB]: 'MongoDB',
-};
+import {useSystemSelectorOptions} from 'sentry/views/insights/database/components/useSystemSelectorOptions';
 
 export function DatabaseSystemSelector() {
-  const handleChange = () => {};
-
-  const {data, isLoading, isError} = useSpanMetrics(
-    {
-      search: MutableSearch.fromQueryObject({'span.op': 'db'}),
-
-      fields: [SpanMetricsField.SPAN_SYSTEM, 'count()'],
-      sorts: [{field: 'count()', kind: 'desc'}],
-    },
-    'api.starfish.database-system-selector'
-  );
-
-  // No point in displaying the dropdown if there is only one option
-  if (data.length <= 1) {
-    return null;
-  }
-
-  const options = data.map(entry => {
-    const system = entry['span.system'];
-    const label =
-      system in DATABASE_SYSTEM_TO_LABEL
-        ? DATABASE_SYSTEM_TO_LABEL[system]
-        : system === ''
-          ? t('Unknown')
-          : system;
-
-    return {value: system, label};
-  });
-
-  const getDefaultValue = () => {
-    if (isLoading || isError) {
-      return t('—');
-    }
-
-    return options[0].value;
-  };
+  const {selectedOption, setSelectedOption, options, isLoading, isError} =
+    useSystemSelectorOptions();
 
   return (
     <CompactSelect
-      onChange={handleChange}
+      onChange={option => {
+        setSelectedOption(option.value);
+      }}
       options={options}
       triggerProps={{prefix: t('DB System')}}
-      defaultValue={getDefaultValue()}
-      disabled={isLoading || isError}
+      loading={isLoading}
+      disabled={isError || isLoading || options.length <= 1}
+      value={selectedOption}
     />
   );
 }
---
+++ static/app/views/insights/database/components/useSystemSelectorOptions.tsx
@@ -0,0 +1,69 @@
+import {MutableSearch} from 'sentry/utils/tokenizeSearch';
+import {useLocalStorageState} from 'sentry/utils/useLocalStorageState';
+import {useSpanMetrics} from 'sentry/views/insights/common/queries/useDiscover';
+import {SpanMetricsField} from 'sentry/views/insights/types';
+
+/**
+ * The supported relational database system values are based on what is
+ * set in the Sentry Python SDK. The only supported NoSQL DBMS is MongoDB.
+ *
+ * https://github.com/getsentry/sentry-python/blob/master/sentry_sdk/integrations/sqlalchemy.py#L125
+ */
+enum SupportedDatabaseSystems {
+  // SQL
+  SQLITE = 'sqlite',
+  POSTGRESQL = 'postgresql',
+  MARIADB = 'mariadb',
+  MYSQL = 'mysql',
+  ORACLE = 'oracle',
+  // NoSQL
+  MONGODB = 'mongodb',
+}
+
+const DATABASE_SYSTEM_TO_LABEL: Record<SupportedDatabaseSystems, string> = {
+  [SupportedDatabaseSystems.SQLITE]: 'SQLite',
+  [SupportedDatabaseSystems.POSTGRESQL]: 'PostgreSQL',
+  [SupportedDatabaseSystems.MARIADB]: 'MariaDB',
+  [SupportedDatabaseSystems.MYSQL]: 'MySQL',
+  [SupportedDatabaseSystems.ORACLE]: 'Oracle',
+  [SupportedDatabaseSystems.MONGODB]: 'MongoDB',
+};
+
+type Option = {
+  label: string;
+  value: string;
+};
+
+export function useSystemSelectorOptions() {
+  const [selectedOption, setSelectedOption] = useLocalStorageState<string>(
+    'insights-db-system-selector',
+    ''
+  );
+
+  const {data, isLoading, isError} = useSpanMetrics(
+    {
+      search: MutableSearch.fromQueryObject({'span.op': 'db'}),
+
+      fields: [SpanMetricsField.SPAN_SYSTEM, 'count()'],
+      sorts: [{field: 'count()', kind: 'desc'}],
+    },
+    'api.starfish.database-system-selector'
+  );
+
+  const options: Option[] = [];
+  data.forEach(entry => {
+    const system = entry['span.system'];
+    if (system) {
+      const label =
+        system in DATABASE_SYSTEM_TO_LABEL ? DATABASE_SYSTEM_TO_LABEL[system] : system;
+
+      options.push({value: system, label});
+    }
+  });
+
+  if (selectedOption === '' && options.length > 0) {
+    setSelectedOption(options[0].value);
+  }
+
+  return {selectedOption, setSelectedOption, options, isLoading, isError};
+}
