28e6a0d4f4264355e05c54a5d5b1c7d5c0c519f0
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2024-08-14 04:11:40 +0700

    feat(ai-issue-summary): Issue summary card in sidebar (#76095)
    Introduces the issue summary card and trigger on the issue details page
    sidebar guarded by the `organizations:ai-summary` feature flag
    
    <img width="395" alt="Screenshot 2024-08-13 at 9 58 17 AM"
    src="https://github.com/user-attachments/assets/fb7e1bcf-9ef7-4941-b326-a88bcd05654b">
    
    
    https://github.com/user-attachments/assets/6057959d-cc20-4582-95f5-cb47df3767e8
    
    ---------
    
    Co-authored-by: Scott Cooper <scttcper@gmail.com>

---
+++ static/app/components/group/groupSummary.tsx
@@ -0,0 +1,135 @@
+import styled from '@emotion/styled';
+
+import FeatureBadge from 'sentry/components/badge/featureBadge';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import Panel from 'sentry/components/panels/panel';
+import * as SidebarSection from 'sentry/components/sidebarSection';
+import {t, tct} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import marked from 'sentry/utils/marked';
+import {type ApiQueryKey, useApiQuery} from 'sentry/utils/queryClient';
+import useOrganization from 'sentry/utils/useOrganization';
+
+interface GroupSummaryProps {
+  groupId: string;
+}
+
+interface GroupSummaryData {
+  groupId: string;
+  impact: string;
+  summary: string;
+}
+
+const makeGroupSummaryQueryKey = (
+  organizationSlug: string,
+  groupId: string
+): ApiQueryKey => [`/organizations/${organizationSlug}/issues/${groupId}/summarize/`];
+
+export function GroupSummary({groupId}: GroupSummaryProps) {
+  const organization = useOrganization();
+  const {data, isLoading, isError} = useApiQuery<GroupSummaryData>(
+    makeGroupSummaryQueryKey(organization.slug, groupId),
+    {
+      staleTime: Infinity, // Cache the result indefinitely as it's unlikely to change if it's already computed
+    }
+  );
+
+  return (
+    <SidebarSection.Wrap>
+      <Wrapper>
+        <StyledTitleRow>
+          <StyledTitle>
+            <span>{t('Issue Summary')}</span>
+            <StyledFeatureBadge
+              type="internal"
+              title={tct(
+                'This feature is currently only testing internally. Please let us know your feedback at [channel:#proj-issue-summary].',
+                {
+                  channel: <a href="https://sentry.slack.com/archives/C07GPS55GUC" />,
+                }
+              )}
+              tooltipProps={{isHoverable: true}}
+            />
+          </StyledTitle>
+          {isLoading && <StyledLoadingIndicator size={16} mini />}
+        </StyledTitleRow>
+        <div>
+          {isError ? <div>{t('Error loading summary')}</div> : null}
+          {data && (
+            <Content>
+              <SummaryContent
+                dangerouslySetInnerHTML={{
+                  __html: marked(data.summary),
+                }}
+              />
+              <ImpactContent>
+                <StyledTitle>{t('Potential Impact')}</StyledTitle>
+                <SummaryContent
+                  dangerouslySetInnerHTML={{
+                    __html: marked(data.impact),
+                  }}
+                />
+              </ImpactContent>
+            </Content>
+          )}
+        </div>
+      </Wrapper>
+    </SidebarSection.Wrap>
+  );
+}
+
+const Wrapper = styled(Panel)`
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 0;
+  background: linear-gradient(
+    269.35deg,
+    ${p => p.theme.backgroundTertiary} 0.32%,
+    rgba(245, 243, 247, 0) 99.69%
+  );
+  padding: ${space(1.5)} ${space(2)};
+`;
+
+const StyledTitleRow = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const StyledTitle = styled('div')`
+  margin: 0;
+  color: ${p => p.theme.text};
+  font-size: ${p => p.theme.fontSizeMedium};
+  font-weight: 600;
+  align-items: center;
+  display: flex;
+`;
+
+const StyledFeatureBadge = styled(FeatureBadge)`
+  margin-top: -1px;
+`;
+
+const SummaryContent = styled('div')`
+  p {
+    margin: 0;
+  }
+`;
+
+const StyledLoadingIndicator = styled(LoadingIndicator)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 16px;
+  max-height: 16px;
+`;
+
+const ImpactContent = styled('div')`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Content = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: ${space(1)};
+`;
