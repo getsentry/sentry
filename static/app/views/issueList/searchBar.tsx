dda1ef2259e491f70520e8f02c01a34ebfa8cd76
Malachi Willey <malwilley@gmail.com>
2024-08-13 11:41:31 -0700

    fix(issue-stream): Remove environment from filter keys

--- static/app/views/issueList/searchBar.tsx
+++ static/app/views/issueList/searchBar.tsx
@@ -222,6 +222,8 @@ function IssueListSearchBar({organization, tags, onClose, ...props}: Props) {
     [onClose]
   );
 
+  console.log({organization, issueTags, tags, filterKeySections, onClose, props});
+
   if (organization.features.includes('issue-stream-search-query-builder')) {
     return (
       <SearchQueryBuilder
