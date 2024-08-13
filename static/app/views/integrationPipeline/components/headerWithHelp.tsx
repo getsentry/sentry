cf17aa80937755511112a5e94ed273dc7160e477
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-08-13 09:11:11 -0600

    ref(js): Remove ActionButton
    This was intended to just be used within `components/actions` but
    likely because of the filename it was imported and used in a number of
    places as just `Button`.
    
    It only set `size="xs"` so we can just replace all usages with regular
    button and set size="xs" when appropriate (most usages were already
    setting size anyway).

--- static/app/views/integrationPipeline/components/headerWithHelp.tsx
+++ static/app/views/integrationPipeline/components/headerWithHelp.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-import Button from 'sentry/components/actions/button';
+import {Button} from 'sentry/components/button';
 import LogoSentry from 'sentry/components/logoSentry';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
