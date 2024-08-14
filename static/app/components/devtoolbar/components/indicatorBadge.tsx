c4ee10bd9dc6fa971ce124d72534bdf750b00a80
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-08-14 10:51:39 -0700

    style(toolbar): adjust alert indicator css

--- static/app/components/devtoolbar/components/indicatorBadge.tsx
+++ static/app/components/devtoolbar/components/indicatorBadge.tsx
@@ -11,12 +11,11 @@ const variants = {
 };
 
 interface Props {
-  icon: React.ReactNode;
   variant: keyof typeof variants;
 }
 
-export default function IndicatorBadge({icon, variant}: Props) {
-  return <div css={[smallCss, counterCss, variants[variant]]}>{icon}</div>;
+export default function IndicatorBadge({variant}: Props) {
+  return <div css={[smallCss, counterCss, variants[variant]]} />;
 }
 
 const counterCss = css`
@@ -25,12 +24,12 @@ const counterCss = css`
   border: 1px solid transparent;
   box-sizing: content-box;
   color: var(--gray100);
-  height: 1rem;
+  height: 0.55rem;
   line-height: 1rem;
   position: absolute;
-  right: -6px;
-  top: -6px;
-  width: 1rem;
+  right: 2px;
+  top: 18px;
+  width: 0.55rem;
   display: flex;
   align-items: center;
   justify-content: center;
