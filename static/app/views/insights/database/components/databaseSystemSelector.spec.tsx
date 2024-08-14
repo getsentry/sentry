7b32a54ad1ca8599789ae5ef1aae4f4b033d6c34
Ash Anand <ash.anand@sentry.io>
2024-08-14 12:02:25 -0400

    reorder tests

--- static/app/views/insights/database/components/databaseSystemSelector.spec.tsx
+++ static/app/views/insights/database/components/databaseSystemSelector.spec.tsx
@@ -18,6 +18,25 @@ describe('DatabaseSystemSelector', function () {
     jest.clearAllMocks();
   });
 
+  it('is disabled when only one database system is present and shows that system as selected', async function () {
+    mockUseSpanMetrics.mockReturnValue({
+      data: [
+        {
+          'span.system': 'postgresql',
+          'count()': 1000,
+        },
+      ],
+      isLoading: false,
+      isError: false,
+    } as any);
+
+    render(<DatabaseSystemSelector />, {organization});
+
+    const dropdownSelector = await screen.findByRole('button');
+    expect(dropdownSelector).toBeDisabled();
+    expect(dropdownSelector).toHaveTextContent('DB SystemPostgreSQL');
+  });
+
   it('renders all database system options correctly', async function () {
     mockUseSpanMetrics.mockReturnValue({
       data: [
@@ -54,24 +73,5 @@ describe('DatabaseSystemSelector', function () {
     expect(dropdownOptionLabels[1]).toHaveTextContent('MongoDB');
     // chungusdb does not exist, so we do not expect this option to have casing
     expect(dropdownOptionLabels[2]).toHaveTextContent('chungusdb');
-  });
-
-  it('is disabled when only one database system is present and shows that system as selected', async function () {
-    mockUseSpanMetrics.mockReturnValue({
-      data: [
-        {
-          'span.system': 'postgresql',
-          'count()': 1000,
-        },
-      ],
-      isLoading: false,
-      isError: false,
-    } as any);
-
-    render(<DatabaseSystemSelector />, {organization});
-
-    const dropdownSelector = await screen.findByRole('button');
-    expect(dropdownSelector).toBeDisabled();
-    expect(dropdownSelector).toHaveTextContent('DB SystemPostgreSQL');
   });
 });
