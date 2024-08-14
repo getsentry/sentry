3915c1cc4a033f2526fee36c130c56ee980ca372
Ash Anand <ash.anand@sentry.io>
2024-08-14 12:43:23 -0400

    add test to check for localStorage case

--- static/app/views/insights/database/components/databaseSystemSelector.spec.tsx
+++ static/app/views/insights/database/components/databaseSystemSelector.spec.tsx
@@ -2,6 +2,7 @@ import {OrganizationFixture} from 'sentry-fixture/organization';
 
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
+import {useLocalStorageState} from 'sentry/utils/useLocalStorageState';
 import {useSpanMetrics} from 'sentry/views/insights/common/queries/useDiscover';
 import {DatabaseSystemSelector} from 'sentry/views/insights/database/components/databaseSystemSelector';
 
@@ -9,10 +10,19 @@ jest.mock('sentry/views/insights/common/queries/useDiscover', () => ({
   useSpanMetrics: jest.fn(),
 }));
 
+jest.mock('sentry/utils/useLocalStorageState', () => ({
+  useLocalStorageState: jest.fn(),
+}));
+
+const mockUseLocalStorageState = jest.mocked(useLocalStorageState);
 const mockUseSpanMetrics = jest.mocked(useSpanMetrics);
 
 describe('DatabaseSystemSelector', function () {
   const organization = OrganizationFixture();
+
+  beforeEach(() => {
+    mockUseLocalStorageState.mockReturnValue(['', () => {}]);
+  });
 
   afterAll(() => {
     jest.clearAllMocks();
@@ -73,5 +83,32 @@ describe('DatabaseSystemSelector', function () {
     expect(dropdownOptionLabels[1]).toHaveTextContent('MongoDB');
     // chungusdb does not exist, so we do not expect this option to have casing
     expect(dropdownOptionLabels[2]).toHaveTextContent('chungusdb');
+  });
+
+  it('chooses the currently selected system from localStorage', async function () {
+    mockUseLocalStorageState.mockReturnValue(['mongodb', () => {}]);
+    mockUseSpanMetrics.mockReturnValue({
+      data: [
+        {
+          'span.system': 'postgresql',
+          'count()': 1000,
+        },
+        {
+          'span.system': 'mongodb',
+          'count()': 500,
+        },
+        {
+          'span.system': 'chungusdb',
+          'count()': 200,
+        },
+      ],
+      isLoading: false,
+      isError: false,
+    } as any);
+
+    render(<DatabaseSystemSelector />, {organization});
+
+    expect(await screen.findByText('MongoDB')).toBeInTheDocument();
+    screen.debug();
   });
 });
