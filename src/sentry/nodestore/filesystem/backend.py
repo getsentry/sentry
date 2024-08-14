b3c2f92a24705510cde059d4ae167a4174732071
kl <chenkailing@xd.com>
2024-08-14 20:19:46 +0800

    feat(nodestore): file system-based nodestore backend, with added support for S3 and GCS storage.

--- src/sentry/nodestore/filesystem/backend.py
+++ src/sentry/nodestore/filesystem/backend.py
@@ -1,54 +1,58 @@
 import datetime
-import os
+from io import BytesIO
 
 from django.conf import settings
-from django.utils import timezone
 
+from sentry import options as options_store
+from sentry.models.file import get_storage
 from sentry.nodestore.base import NodeStorage
 
 
 class FileSystemNodeStorage(NodeStorage):
     """
-    A simple backend that saves each node as a file. Only appropriate for
-    debugging and development!
+    A file system-based backend that saves each node as a file, Local fileSystem only appropriate for
+    debugging and development! For production environments, please use S3 or GCS. config.yml Configuration reference:
+    >>> filestore.backend: "s3"
+    ... filestore.options:
+    ...   access_key: "xxx"
+    ...   secret_key: "xxx"
+    ...   endpoint_url: "https://s3.us-east-1.amazonaws.com"
+    ...   bucket_name: "sentry"
+    ...   location: "/sentry"
     """
 
-    def __init__(self, path=None):
-        self.path: str = ""
-
-        if not settings.DEBUG:
-            raise ValueError("FileSystemNodeStorage should only be used in development!")
-        if path:
-            self.path = os.path.abspath(os.path.expanduser(path))
-        else:
-            self.path = os.path.abspath(os.path.join(os.path.dirname(__file__), "./nodes"))
+    def __init__(self, prefix_path=None):
+        self.prefix_path: str = "nodestore/"
+        if not settings.DEBUG and options_store.get("filestore.backend") == "filesystem":
+            raise ValueError("Local fileSystem should only be used in development!")
+        if prefix_path:
+            self.prefix_path = prefix_path
 
     def _get_bytes(self, id: str):
-        with open(self.node_path(id), "rb") as file:
-            return file.read()
+        storage = get_storage()
+        path = self.node_path(id)
+        return storage.open(path).read()
 
     def _set_bytes(self, id: str, data: bytes, ttl=0):
-        with open(self.node_path(id), "wb") as file:
-            file.write(data)
+        storage = get_storage()
+        path = self.node_path(id)
+        storage.save(path, BytesIO(data))
 
     def delete(self, id):
-        os.remove(self.node_path(id))
+        storage = get_storage()
+        path = self.node_path(id)
+        storage.delete(path)
 
     def cleanup(self, cutoff: datetime.datetime):
-        for filename in os.listdir(self.path):
-            path = os.path.join(self.path, filename)
-            creation_datetime = datetime.datetime.fromtimestamp(os.path.getctime(path)).replace(
-                tzinfo=timezone.utc
-            )
-
-            if creation_datetime > cutoff:
-                os.remove(path)
+        """
+        This driver does not have managed TTLs.  To enable TTLs you will need to enable it on your
+        bucket.
+        """
+        raise NotImplementedError
 
     def bootstrap(self):
-        try:
-            os.mkdir(self.path)
-        except FileExistsError:
-            pass
+        # Nothing for filesystem backend to do during bootstrap
+        pass
 
     def node_path(self, id: str):
-        return os.path.join(self.path, f"{id}.json")
+        return f"{self.prefix_path}{id}.json"
