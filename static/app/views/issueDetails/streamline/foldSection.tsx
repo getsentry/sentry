e5811f809b88f7991afa290fa0012130d6b12ee2
Scott Cooper <scttcper@gmail.com>
2024-08-14 14:53:21 -0700

    fix(issues): Prevent selection on expand section
    Prevents text selection when expanding sections

--- static/app/views/issueDetails/streamline/foldSection.tsx
+++ static/app/views/issueDetails/streamline/foldSection.tsx
@@ -110,6 +110,7 @@ export const FoldSection = forwardRef<HTMLElement, FoldSectionProps>(function Fo
   const toggleCollapse = useCallback(
     (e: React.MouseEvent) => {
       e.preventDefault(); // Prevent browser summary/details behaviour
+      window.getSelection()?.removeAllRanges(); // Prevent text selection on expand
       trackAnalytics('issue_details.section_fold', {
         sectionKey,
         organization,
