61708bc569dc1a5ec2a7c12bb1bf96583eccecb0
Malachi Willey <malwilley@gmail.com>
2024-08-15 10:27:40 -0700

    feat(query-builder): Display full-width menu when there is an empty query (#76226)
    If the search bar is empty, display a full-width menu with descriptions
    rendered in a pane instead of a tooltip. After a filter has been added,
    we go back to an inline menu.
    
    This should provide a nice starting point for users and will give us
    more space to suggest recent searches as well.
    
    Uses a portal to render this inside the top-level wrapper element.

--- static/app/components/compactSelect/listBox/section.tsx
+++ static/app/components/compactSelect/listBox/section.tsx
@@ -26,6 +26,7 @@ interface ListBoxSectionProps extends AriaListBoxSectionProps {
   showSectionHeaders: boolean;
   size: FormSize;
   onToggle?: (section: SelectSection<SelectKey>, type: 'select' | 'unselect') => void;
+  showDetails?: boolean;
 }
 
 /**
@@ -39,6 +40,7 @@ export function ListBoxSection({
   size,
   hiddenOptions,
   showSectionHeaders,
+  showDetails = true,
 }: ListBoxSectionProps) {
   const {itemProps, headingProps, groupProps} = useListBoxSection({
     heading: item.rendered,
@@ -77,6 +79,7 @@ export function ListBoxSection({
               item={child}
               listState={listState}
               size={size}
+              showDetails={showDetails}
             />
           ))}
         </SectionGroup>
