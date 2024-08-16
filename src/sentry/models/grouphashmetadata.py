937587bf65dc9bd9181dd8b83680a3b69627404e
Katie Byers <katie.byers@sentry.io>
2024-08-16 12:28:12 -0700

    ref(grouping): Add data to `GroupHash` and `GroupHashMetadata` reprs (#76310)
    To make debugging easier, this adds group id and hash to the `GroupHash` repr and group id, hash, and grouphash id to the `GroupHashMetadata` repr.

--- src/sentry/models/grouphashmetadata.py
+++ src/sentry/models/grouphashmetadata.py
@@ -3,6 +3,7 @@ from django.utils import timezone
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import Model, region_silo_model
+from sentry.db.models.base import sane_repr
 
 
 @region_silo_model
@@ -18,3 +19,13 @@ class GroupHashMetadata(Model):
     class Meta:
         app_label = "sentry"
         db_table = "sentry_grouphashmetadata"
+
+    @property
+    def group_id(self) -> int | None:
+        return self.grouphash.group_id
+
+    @property
+    def hash(self) -> str:
+        return self.grouphash.hash
+
+    __repr__ = sane_repr("grouphash_id", "group_id", "hash")
