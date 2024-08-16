c657e36b84666176632a35ac76badc0472ac2a29
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-08-16 11:15:35 -0700

    chore(similarity): EA similarity record deletion task (#76331)
    Remove random percentage rollout option usage in similarity record
    deletion task

--- src/sentry/tasks/delete_seer_grouping_records.py
+++ src/sentry/tasks/delete_seer_grouping_records.py
@@ -1,5 +1,4 @@
 import logging
-from random import randint
 from typing import Any
 
 from sentry import features, options
@@ -59,10 +58,7 @@ def call_delete_seer_grouping_records_by_hash(
         project
         and (
             features.has("projects:similarity-embeddings-grouping", project)
-            or (
-                project.get_option("sentry:similarity_backfill_completed")
-                and randint(1, 100) <= options.get("similarity.delete_task_EA_rollout_percentage")
-            )
+            or project.get_option("sentry:similarity_backfill_completed")
         )
         and not killswitch_enabled(project.id)
         and not options.get("seer.similarity-embeddings-delete-by-hash-killswitch.enabled")
