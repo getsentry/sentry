843beafdb7b88c1746ce7003afe84c83d96ed1c6
Colleen O'Rourke <colleen@sentry.io>
2024-08-16 09:35:39 -0700

    feat(alerts): Update metric alert rule template for dynamic alerts (#76146)
    Follow up to https://github.com/getsentry/sentry/pull/76030/ which moves
    the code into a shared location that updates Slack, MSTeams, OpsGenie,
    and Pagerduty. I had it the other way before to control the order of the
    content in the Slack notification, but it's easier this way. This
    updates all the aforementioned metric alert notifications to include the
    threshold text.

--- tests/sentry/incidents/action_handlers/test_pagerduty.py
+++ tests/sentry/incidents/action_handlers/test_pagerduty.py
@@ -4,14 +4,21 @@ from unittest.mock import patch
 import pytest
 import responses
 from django.http import Http404
+from urllib3.response import HTTPResponse
 
 from sentry.incidents.action_handlers import PagerDutyActionHandler
 from sentry.incidents.logic import update_incident_status
-from sentry.incidents.models.alert_rule import AlertRuleTriggerAction
+from sentry.incidents.models.alert_rule import (
+    AlertRuleDetectionType,
+    AlertRuleSeasonality,
+    AlertRuleSensitivity,
+    AlertRuleTriggerAction,
+)
 from sentry.incidents.models.incident import IncidentStatus, IncidentStatusMethod
 from sentry.integrations.pagerduty.utils import add_service
 from sentry.silo.base import SiloMode
 from sentry.testutils.helpers.datetime import freeze_time
+from sentry.testutils.helpers.features import with_feature
 from sentry.testutils.silo import assume_test_silo_mode
 from sentry.utils import json
 
@@ -89,7 +96,59 @@ class PagerDutyActionHandlerTest(FireTest):
         assert data["links"][0]["text"] == f"Critical: {alert_rule.name}"
         assert (
             data["links"][0]["href"]
-            == f"http://testserver/organizations/baz/alerts/rules/details/{alert_rule.id}/?alert={incident.identifier}&referrer=metric_alert_pagerduty&detection_type=static&notification_uuid={notification_uuid}"
+            == f"http://testserver/organizations/baz/alerts/rules/details/{alert_rule.id}/?alert={incident.identifier}&referrer=metric_alert_pagerduty&detection_type={alert_rule.detection_type}&notification_uuid={notification_uuid}"
+        )
+
+    @with_feature("organizations:anomaly-detection-alerts")
+    @patch(
+        "sentry.seer.anomaly_detection.store_data.seer_anomaly_detection_connection_pool.urlopen"
+    )
+    def test_build_incident_attachment_dynamic_alert(self, mock_seer_request):
+        from sentry.integrations.pagerduty.utils import build_incident_attachment
+
+        mock_seer_request.return_value = HTTPResponse(status=200)
+        alert_rule = self.create_alert_rule(
+            detection_type=AlertRuleDetectionType.DYNAMIC,
+            time_window=30,
+            sensitivity=AlertRuleSensitivity.LOW,
+            seasonality=AlertRuleSeasonality.AUTO,
+        )
+        incident = self.create_incident(alert_rule=alert_rule, status=IncidentStatus.CRITICAL.value)
+        trigger = self.create_alert_rule_trigger(alert_rule=alert_rule, alert_threshold=0)
+        update_incident_status(
+            incident, IncidentStatus.CRITICAL, status_method=IncidentStatusMethod.RULE_TRIGGERED
+        )
+        self.create_alert_rule_trigger_action(
+            target_identifier=self.service["id"],
+            type=AlertRuleTriggerAction.Type.PAGERDUTY,
+            target_type=AlertRuleTriggerAction.TargetType.SPECIFIC,
+            integration=self.integration,
+            alert_rule_trigger=trigger,
+            triggered_for_incident=incident,
+        )
+        metric_value = 1000
+        notification_uuid = str(uuid.uuid4())
+        data = build_incident_attachment(
+            incident,
+            self.integration_key,
+            IncidentStatus(incident.status),
+            metric_value,
+            notification_uuid,
+        )
+
+        assert data["routing_key"] == self.integration_key
+        assert data["event_action"] == "trigger"
+        assert data["dedup_key"] == f"incident_{incident.organization_id}_{incident.identifier}"
+        assert data["payload"]["summary"] == alert_rule.name
+        assert data["payload"]["severity"] == "critical"
+        assert data["payload"]["source"] == str(incident.identifier)
+        assert data["payload"]["custom_details"] == {
+            "details": f"1000 events in the last 30 minutes\nThreshold: {alert_rule.detection_type.title()}"
+        }
+        assert data["links"][0]["text"] == f"Critical: {alert_rule.name}"
+        assert (
+            data["links"][0]["href"]
+            == f"http://testserver/organizations/baz/alerts/rules/details/{alert_rule.id}/?alert={incident.identifier}&referrer=metric_alert_pagerduty&detection_type={alert_rule.detection_type}&notification_uuid={notification_uuid}"
         )
 
     @responses.activate
