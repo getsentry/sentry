28a4dd2e2cc9b7bc422ba2f896d386c3614d08d2
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2024-08-16 09:37:59 -0700

    feat(custom-views): Add custom views to issue stream behind feature flag  (#75883)
    This PR releases the custom views feature into the issue stream under a
    feature flag. Flagged users will also see the "Custom Search" button has
    been removed from the issue stream in favor of custom views. The bulk of
    this new component exists in `<CustomViewsIssueListHeader/>`, whose
    parent component is in overview.tsx.
    
    Several changes have also been made to the base draggable tab components
    to iron out any compatibility issues that did not appear during sandbox
    development.
    
    Things that are still broken that I am actively working on: 
    * ~Tab Renaming is both ugly and broken (can't type in spaces)~ (#76247)
    * Tab overflow logic does not exist at all 
    * QueryCounts do not work for any tabs with queries that do not match
    queries of previous default tabs
    * Some queries that are functionally the same but off by a space
    register as different, thus triggering the "unsaved changes" state.
    * Tabs do not animate in when added, nor do they animate out when
    deleted
    * No loading or failure state for tabs request 
    * ~Stories is busted as a result of all of the changes made to base tabs
    components~ (deleted stories, this component is not, and was not meant
    to be reusable)

--- static/app/views/issueList/overview.tsx
+++ static/app/views/issueList/overview.tsx
@@ -61,6 +61,7 @@ import withIssueTags from 'sentry/utils/withIssueTags';
 import withOrganization from 'sentry/utils/withOrganization';
 import withPageFilters from 'sentry/utils/withPageFilters';
 import withSavedSearches from 'sentry/utils/withSavedSearches';
+import CustomViewsIssueListHeader from 'sentry/views/issueList/customViewsHeader';
 import SavedIssueSearches from 'sentry/views/issueList/savedIssueSearches';
 import type {IssueUpdateData} from 'sentry/views/issueList/types';
 import {parseIssuePrioritySearch} from 'sentry/views/issueList/utils/parseIssuePrioritySearch';
@@ -321,7 +322,10 @@ class IssueListOverview extends Component<Props, State> {
     savedSearch,
     location,
   }: Pick<Props, 'savedSearch' | 'location'>): string {
-    if (savedSearch) {
+    if (
+      !this.props.organization.features.includes('issue-stream-custom-views') &&
+      savedSearch
+    ) {
       return savedSearch.query;
     }
 
@@ -1221,18 +1225,28 @@ class IssueListOverview extends Component<Props, State> {
 
     return (
       <Layout.Page>
-        <IssueListHeader
-          organization={organization}
-          query={query}
-          sort={this.getSort()}
-          queryCount={queryCount}
-          queryCounts={queryCounts}
-          realtimeActive={realtimeActive}
-          onRealtimeChange={this.onRealtimeChange}
-          router={router}
-          displayReprocessingTab={showReprocessingTab}
-          selectedProjectIds={selection.projects}
-        />
+        {organization.features.includes('issue-stream-custom-views') ? (
+          <CustomViewsIssueListHeader
+            organization={organization}
+            queryCounts={queryCounts}
+            router={router}
+            selectedProjectIds={selection.projects}
+          />
+        ) : (
+          <IssueListHeader
+            organization={organization}
+            query={query}
+            sort={this.getSort()}
+            queryCount={queryCount}
+            queryCounts={queryCounts}
+            realtimeActive={realtimeActive}
+            onRealtimeChange={this.onRealtimeChange}
+            router={router}
+            displayReprocessingTab={showReprocessingTab}
+            selectedProjectIds={selection.projects}
+          />
+        )}
+
         <StyledBody>
           <StyledMain>
             <DataConsentBanner source="issues" />
