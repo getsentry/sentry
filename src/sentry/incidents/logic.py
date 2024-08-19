f0faadfb5d6bd6fe5042163147aa5a492b432ca8
Ryan Skonnord <ryan.skonnord@sentry.io>
2024-08-19 15:41:21 -0700

    Revert get_alert_rule_trigger_action_slack_channel_id

--- src/sentry/incidents/logic.py
+++ src/sentry/incidents/logic.py
@@ -1519,7 +1519,7 @@ def get_target_identifier_display_for_integration(type, target_value, *args, **k
 
 
 def get_alert_rule_trigger_action_slack_channel_id(
-    name, integration_id, use_async_lookup, integrations
+    name, organization, integration_id, use_async_lookup, integrations
 ):
     from sentry.integrations.slack.utils.channel import get_channel_id
 
