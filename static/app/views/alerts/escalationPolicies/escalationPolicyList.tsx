f1b06f41a710ccbae62f21b0d70cead557a98db3
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2024-08-20 14:05:38 -0700

    feat(escalation_policies): Add preliminary design for escalation policy card (#76418)
    This PR adds an unfinished component for the escalation policy card,
    `<EscalationPolicyTimeline/>`. This component is not at all reusable or
    configurable currently. I will work on making it so in the future when I
    have time.
    
    
    ![image](https://github.com/user-attachments/assets/7e6c5d34-d01b-41d7-8030-add2153b8e32)
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/views/alerts/escalationPolicies/escalationPolicyList.tsx
+++ static/app/views/alerts/escalationPolicies/escalationPolicyList.tsx
@@ -1,13 +1,122 @@
 import {Fragment} from 'react';
+import styled from '@emotion/styled';
 
+import ParticipantList from 'sentry/components/group/streamlinedParticipantList';
 import * as Layout from 'sentry/components/layouts/thirds';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
+import Timeline from 'sentry/components/timeline';
+import {IconClock, IconExclamation, IconMegaphone, IconRefresh} from 'sentry/icons';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import type {User} from 'sentry/types';
 import useOrganization from 'sentry/utils/useOrganization';
 import useRouter from 'sentry/utils/useRouter';
 import AlertHeader from 'sentry/views/alerts/list/header';
 
+function NotifyItem({users}: {users?: User[]}) {
+  return (
+    <Timeline.Item
+      title={'Notify:'}
+      icon={<IconMegaphone size="xs" />}
+      colorConfig={{
+        title: 'purple400',
+        icon: 'purple400',
+        iconBorder: 'purple200',
+      }}
+    >
+      {users && <ParticipantList users={users} />}
+    </Timeline.Item>
+  );
+}
+
+function EscalateAfterItem({minutes}: {minutes: number}) {
+  return (
+    <EscalateAfterTimelineItem
+      title={
+        <Fragment>
+          Escalate after{' '}
+          <text style={{textDecoration: 'underline'}}>{minutes} minutes</text> if not
+          acknowledged
+        </Fragment>
+      }
+      icon={<IconClock size="xs" />}
+      colorConfig={{
+        title: 'blue400',
+        icon: 'blue400',
+        iconBorder: 'blue200',
+      }}
+      showLastLine
+    />
+  );
+}
+
+function IncidentCreatedItem() {
+  return (
+    <IncidentCreatedTimelineItem
+      title={'Immediately after an incident is created...'}
+      icon={<IconExclamation size="xs" />}
+      colorConfig={{
+        title: 'red400',
+        icon: 'red400',
+        iconBorder: 'red200',
+      }}
+      isActive
+    />
+  );
+}
+
+function RepeatItem() {
+  return (
+    <Timeline.Item
+      title={'Repeat:'}
+      icon={<IconRefresh size="xs" />}
+      colorConfig={{
+        title: 'purple400',
+        icon: 'purple400',
+        iconBorder: 'purple200',
+      }}
+    />
+  );
+}
+
+function SideBarSection({children, title}: {children: React.ReactNode; title: string}) {
+  return (
+    <div>
+      <SideBarTitle>{title}</SideBarTitle>
+      {children}
+    </div>
+  );
+}
+
+interface EscalationPolicyTimelineProps {
+  title?: string;
+}
+
+function EscalationPolicyTimeline({title}: EscalationPolicyTimelineProps) {
+  return (
+    <EscalationPolicyContainer>
+      <h3>{title ?? 'Example Escalation Policy'}</h3>
+      <EscalationPolicyContent>
+        <Timeline.Container>
+          <IncidentCreatedItem />
+          <NotifyItem />
+          <EscalateAfterItem minutes={10} />
+          <NotifyItem />
+          <EscalateAfterItem minutes={15} />
+          <RepeatItem />
+        </Timeline.Container>
+        <SideBarContainer>
+          <SideBarSection title={'Used by 1 service'}>Some content here</SideBarSection>
+          <SideBarSection title={'Used by 2 teams'}>Some content here</SideBarSection>
+          <SideBarSection title={'Send On-Call Handoff Notifications'}>
+            when in use by a service
+          </SideBarSection>
+        </SideBarContainer>
+      </EscalationPolicyContent>
+    </EscalationPolicyContainer>
+  );
+}
 function EscalationPolicyList() {
   const router = useRouter();
   const organization = useOrganization();
@@ -19,11 +128,66 @@ function EscalationPolicyList() {
       <PageFiltersContainer>
         <AlertHeader router={router} activeTab="policies" />
         <Layout.Body>
-          <Layout.Main fullWidth>WIP by Michael</Layout.Main>
+          <Layout.Main fullWidth>
+            <EscalationPolicyTimeline />
+          </Layout.Main>
         </Layout.Body>
       </PageFiltersContainer>
     </Fragment>
   );
 }
+
+const IncidentCreatedTimelineItem = styled(Timeline.Item)`
+  border-bottom: 1px solid transparent;
+  &:not(:last-child) {
+    border-image: linear-gradient(
+        to right,
+        transparent 20px,
+        ${p => p.theme.translucentInnerBorder} 20px
+      )
+      100% 1;
+  }
+`;
+
+const EscalateAfterTimelineItem = styled(Timeline.Item)`
+  border-bottom: 1px solid transparent;
+  &:not(:last-child) {
+    border-image: linear-gradient(
+        to right,
+        transparent 20px,
+        ${p => p.theme.translucentInnerBorder} 20px
+      )
+      100% 1;
+  }
+`;
+
+const EscalationPolicyContainer = styled('div')`
+  border: 1px solid ${p => p.theme.innerBorder};
+  border-radius: 5px;
+  padding: 15px;
+  display: flex;
+  flex-direction: column;
+`;
+
+const EscalationPolicyContent = styled('div')`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  gap: 15px;
+`;
+
+const SideBarContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: 5px;
+`;
+
+const SideBarTitle = styled('h6')`
+  color: ${p => p.theme.subText};
+  display: flex;
+  align-items: center;
+  gap: ${space(0.5)};
+  font-size: ${p => p.theme.fontSizeMedium};
+  margin: ${space(1)} 0 0;
+`;
 
 export default EscalationPolicyList;
