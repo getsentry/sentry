69ecc4c604e015b80a7c8e14b245db2a817f1dc1
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2024-08-20 09:56:16 -0700

    feat(escalation_policies): Add FE hooks for Rotation Schedule and Escalation policy endpoints  (#76393)
    Adds FE hooks for the following endpoints: 
    
    * GET `organization/<org>/escalation-policies/`
    * PUT `organization/<org>/escalation-policies/`
    * GET `organization/<org>/escalation-policies/<policy_id>`
    * DELETE `organization/<org>/escalation-policies/<policy_id>`
    * GET `organization/<org>/rotation-schedules/`
    * PUT `organization/<org>/rotation-schedules/`
    * GET `organization/<org>/rotation-schedules/<policy_id>`
    * DELETE `organization/<org>/rotation-schedules/<policy_id>`

---
+++ static/app/views/escalationPolicies/queries/useFetchEscalationPolicyDetails.ts
@@ -0,0 +1,38 @@
+import {
+  type ApiQueryKey,
+  useApiQuery,
+  type UseApiQueryOptions,
+} from 'sentry/utils/queryClient';
+import type {EscalationPolicy} from 'sentry/views/escalationPolicies/queries/useFetchEscalationPolicies';
+
+interface FetchEscalationPolicyDetailsParams {
+  escalationPolicyId: string;
+  orgSlug: string;
+}
+
+interface FetchEscalationPolicyDetailsResponse {
+  escalationPolicy: EscalationPolicy;
+}
+
+export const makeFetchEscalationPoliciesKey = ({
+  orgSlug,
+  escalationPolicyId,
+}: FetchEscalationPolicyDetailsParams): ApiQueryKey => [
+  `/organizations/${orgSlug}/escalation-policies/${escalationPolicyId}`,
+  {
+    query: {},
+  },
+];
+
+export const useFetchEscalationPolicies = (
+  params: FetchEscalationPolicyDetailsParams,
+  options: Partial<UseApiQueryOptions<FetchEscalationPolicyDetailsResponse>> = {}
+) => {
+  return useApiQuery<FetchEscalationPolicyDetailsResponse>(
+    makeFetchEscalationPoliciesKey(params),
+    {
+      staleTime: 0,
+      ...options,
+    }
+  );
+};
