52c3691d40faa266460900c4c85d0cf2267055a5
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-08-21 09:38:08 -0700

    Rename cli var

--- src/sentry/runner/commands/task_worker.py
+++ src/sentry/runner/commands/task_worker.py
@@ -5,16 +5,16 @@ from sentry.runner.decorators import configuration
 
 @click.command()
 @click.option(
-    "--generate-empty-task",
+    "--generate-tasks",
     default=False,
     is_flag=True,
     help="If true, will generate a mini task to spawn with logs",
 )
 @configuration
-def task_worker(generate_empty_task) -> None:
+def task_worker(generate_tasks) -> None:
     from sentry.taskdemo import broken, demotasks, say_hello
 
-    if generate_empty_task:
+    if generate_tasks:
         say_hello.delay("it works!")
         broken.delay("safeboom")
     from sentry.taskworker.worker import Worker
