00693f88a4e2e70b4bd947806078935479d59cfe
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-08-26 15:05:45 +0200

    fix(onboarding): Fix integration issues

--- static/app/views/projectInstall/platformOrIntegration.tsx
+++ static/app/views/projectInstall/platformOrIntegration.tsx
@@ -1,28 +1,72 @@
-import * as qs from 'query-string';
+import {useState} from 'react';
+import type {RouteComponentProps} from 'react-router';
 
 import {OnboardingContextProvider} from 'sentry/components/onboarding/onboardingContext';
+import platforms from 'sentry/data/platforms';
+import {t} from 'sentry/locale';
+import type {PlatformIntegration} from 'sentry/types/project';
 import {platformToIntegrationMap} from 'sentry/utils/integrationUtil';
+import useOrganization from 'sentry/utils/useOrganization';
+import useProjects from 'sentry/utils/useProjects';
 
 import GettingStarted from './gettingStarted';
 import {ProjectInstallPlatform} from './platform';
 import PlatformIntegrationSetup from './platformIntegrationSetup';
 
-type Props = React.ComponentProps<typeof ProjectInstallPlatform> &
-  Omit<React.ComponentProps<typeof PlatformIntegrationSetup>, 'integrationSlug'>;
+const allPlatforms: PlatformIntegration[] = [
+  ...platforms,
+  {
+    id: 'other',
+    name: t('Other'),
+    link: 'https://docs.sentry.io/platforms/',
+    type: 'language',
+    language: 'other',
+  },
+];
 
-function PlatformOrIntegration(props: Props) {
-  const parsed = qs.parse(window.location.search);
-  const {platform} = props.params;
-  const integrationSlug = platform && platformToIntegrationMap[platform];
-  // check for manual override query param
-  // TODO(priscila): check this case
-  if (integrationSlug && parsed.manual !== '1') {
-    return <PlatformIntegrationSetup integrationSlug={integrationSlug} {...props} />;
+type Props = RouteComponentProps<{projectId: string}, {}>;
+
+function PlatformOrIntegration({params}: Props) {
+  const organization = useOrganization();
+  const [integrationUseManualSetup, setIntegrationUseManualSetup] = useState(false);
+
+  const {projects, initiallyLoaded} = useProjects({
+    slugs: [params.projectId],
+    orgId: organization.slug,
+  });
+
+  const loadingProjects = !initiallyLoaded;
+  const project = !loadingProjects
+    ? projects.find(proj => proj.slug === params.projectId)
+    : undefined;
+
+  const currentPlatformKey = project?.platform ?? 'other';
+  const currentPlatform = allPlatforms.find(p => p.id === currentPlatformKey);
+
+  const integrationSlug: string | undefined =
+    platformToIntegrationMap[currentPlatformKey];
+  const showIntegrationOnboarding = integrationSlug && !integrationUseManualSetup;
+
+  if (showIntegrationOnboarding) {
+    return (
+      <PlatformIntegrationSetup
+        integrationSlug={integrationSlug}
+        onClickManualSetup={() => setIntegrationUseManualSetup(true)}
+        project={project}
+        platform={currentPlatform}
+      />
+    );
   }
+
   return (
     <OnboardingContextProvider>
       <GettingStarted>
-        <ProjectInstallPlatform {...props} />
+        <ProjectInstallPlatform
+          project={project}
+          loading={loadingProjects}
+          platform={currentPlatform}
+          currentPlatformKey={currentPlatformKey}
+        />
       </GettingStarted>
     </OnboardingContextProvider>
   );
