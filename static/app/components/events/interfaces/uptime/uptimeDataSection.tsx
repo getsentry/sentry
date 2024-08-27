5d44f03842be9fce96a3b0a1f939a5931b62d3a8
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-08-27 16:18:25 -0400

    feat(uptime): Add link to "Uptime Detection RUle"

--- static/app/components/events/interfaces/uptime/uptimeDataSection.tsx
+++ static/app/components/events/interfaces/uptime/uptimeDataSection.tsx
@@ -1,16 +1,24 @@
 import {useRef} from 'react';
 import styled from '@emotion/styled';
 
+import {LinkButton} from 'sentry/components/button';
 import DateTime from 'sentry/components/dateTime';
 import Duration from 'sentry/components/duration';
 import {EventDataSection} from 'sentry/components/events/eventDataSection';
 import {Tooltip} from 'sentry/components/tooltip';
+import {IconSettings} from 'sentry/icons';
 import {t, tct} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
+import type {Event} from 'sentry/types/event';
 import {type Group, GroupActivityType, GroupStatus} from 'sentry/types/group';
+import type {Project} from 'sentry/types/project';
+import normalizeUrl from 'sentry/utils/url/normalizeUrl';
+import useOrganization from 'sentry/utils/useOrganization';
 
 interface Props {
+  event: Event;
   group: Group;
+  project: Project;
 }
 
 const DOWNTIME_START_TYPES = [
@@ -20,7 +28,8 @@ const DOWNTIME_START_TYPES = [
 
 const DOWNTIME_TERMINAL_TYPES = [GroupActivityType.SET_RESOLVED];
 
-export function UptimeDataSection({group}: Props) {
+export function UptimeDataSection({group, event, project}: Props) {
+  const organization = useOrganization();
   const nowRef = useRef(new Date());
   const downtimeStartActivity = group.activity.findLast(activity =>
     DOWNTIME_START_TYPES.includes(activity.type)
@@ -53,11 +62,26 @@ export function UptimeDataSection({group}: Props) {
     </Tooltip>
   );
 
+  const alertRuleId = event.tags.find(tag => tag.key === 'uptime_rule')?.value;
+
   return (
     <EventDataSection
       title={t('Downtime Information')}
       type="downtime"
       help={t('Information about the detected downtime')}
+      actions={
+        alertRuleId !== undefined && (
+          <LinkButton
+            icon={<IconSettings />}
+            size="xs"
+            to={normalizeUrl(
+              `/organizations/${organization.slug}/alerts/rules/uptime/${project.slug}/${alertRuleId}/details/`
+            )}
+          >
+            {t('Uptime Alert Rule')}
+          </LinkButton>
+        )
+      }
     >
       {isResolved
         ? tct('Domain was down for [duration]', {
