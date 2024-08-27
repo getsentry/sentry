9a7de8d5742a5adcf2b16500e44d0c474ca95cba
Katie Byers <lobsterkatie@gmail.com>
2024-08-27 14:51:41 -0700

    change order of checks in `should_call_seer_for_grouping`

--- src/sentry/grouping/ingest/seer.py
+++ src/sentry/grouping/ingest/seer.py
@@ -36,13 +36,14 @@ def should_call_seer_for_grouping(event: Event, primary_hashes: CalculatedHashes
 
     project = event.project
 
-    if not _project_has_similarity_grouping_enabled(project):
+    # Check both of these before returning based on either so we can gather metrics on their results
+    content_is_eligible = event_content_is_seer_eligible(event)
+    seer_enabled_for_project = _project_has_similarity_grouping_enabled(project)
+
+    if not (content_is_eligible and seer_enabled_for_project):
         return False
 
     if _has_customized_fingerprint(event, primary_hashes):
-        return False
-
-    if not event_content_is_seer_eligible(event):
         return False
 
     # **Do not add any new checks after this.** The rate limit check MUST remain the last of all the
