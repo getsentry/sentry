b6cee82966582e835b1671357f7e8e652600efcb
Scott Cooper <scttcper@gmail.com>
2024-08-28 13:20:59 -0700

    fix(issues): Prevent passing isVisible style prop (#76694)

--- static/app/components/draggableTabs/draggableTabList.tsx
+++ static/app/components/draggableTabs/draggableTabList.tsx
@@ -209,7 +209,9 @@ DraggableTabList.Item = Item;
  * TabDividers are only visible around NON-selected tabs. They are not visible around the selected tab,
  * but they still create some space and act as a gap between tabs.
  */
-const TabDivider = styled(motion.div)<{isVisible: boolean}>`
+const TabDivider = styled(motion.div, {
+  shouldForwardProp: prop => prop !== 'isVisible',
+})<{isVisible: boolean}>`
   ${p =>
     p.isVisible &&
     `
