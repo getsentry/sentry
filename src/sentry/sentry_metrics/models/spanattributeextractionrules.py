3ddf9119ddad9b3ef58713fda055daa52ff3eeec
Simon Hellmayr <simon.hellmayr@sentry.io>
2024-08-28 15:40:31 +0200

    ref(metrics): remove foreign key constraints on span attribute extraction models

--- src/sentry/sentry_metrics/models/spanattributeextractionrules.py
+++ src/sentry/sentry_metrics/models/spanattributeextractionrules.py
@@ -24,6 +24,7 @@ class SpanAttributeExtractionRuleCondition(DefaultFieldsModel):
         "sentry.SpanAttributeExtractionRuleConfig",
         related_name="conditions",
         on_delete=CASCADE,
+        db_constraint=False,
     )
 
     class Meta:
