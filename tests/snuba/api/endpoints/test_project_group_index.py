d607f43b2c0b52ee04689e2591dbf52da5ec33c6
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2024-08-28 16:09:24 -0700

    github review fixes

--- tests/snuba/api/endpoints/test_project_group_index.py
+++ tests/snuba/api/endpoints/test_project_group_index.py
@@ -352,7 +352,7 @@ class GroupListTest(APITestCase, SnubaTestCase):
         assert response.status_code == 200
         assert [int(r["id"]) for r in response.data] == [event.group.id]
 
-    def test_group_by_hash(self):
+    def test_single_group_by_hash(self):
         event = self.store_event(
             data={"timestamp": iso_format(before_now(seconds=500)), "fingerprint": ["group-1"]},
             project_id=self.project.id,
@@ -363,8 +363,9 @@ class GroupListTest(APITestCase, SnubaTestCase):
         response = self.client.get(f"{self.path}?hashes={event.get_primary_hash()}")
         assert response.status_code == 200
         assert len(response.data) == 1
+        assert int(response.data[0]["id"]) == event.group.id
 
-    def test_groups_by_hashes(self):
+    def test_multiple_groups_by_hashes(self):
         event = self.store_event(
             data={"timestamp": iso_format(before_now(seconds=500)), "fingerprint": ["group-1"]},
             project_id=self.project.id,
@@ -381,6 +382,10 @@ class GroupListTest(APITestCase, SnubaTestCase):
         )
         assert response.status_code == 200
         assert len(response.data) == 2
+
+        response_group_ids = [int(group["id"]) for group in response.data]
+        assert event.group.id in response_group_ids
+        assert event2.group.id in response_group_ids
 
 
 class GroupUpdateTest(APITestCase, SnubaTestCase):
