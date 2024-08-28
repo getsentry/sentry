31ed81bac01917c8afc6432312be67b6d088db0a
MichaelSun48 <michael.sun@sentry.io>
2024-08-28 09:26:12 -0700

    Remove unncessary dependencies from useEffect

--- static/app/views/issueList/customViewsHeader.tsx
+++ static/app/views/issueList/customViewsHeader.tsx
@@ -241,16 +241,8 @@ function CustomViewsIssueListHeaderTabsContent({
       tabListState?.setSelectedKey('temporary-tab');
       return;
     }
-  }, [
-    tabListState,
-    draggableTabs,
-    navigate,
-    organization.slug,
-    query,
-    queryParams,
-    sort,
-    viewId,
-  ]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [tabListState, navigate, organization.slug, query, sort, viewId]);
 
   // Update local tabs when new views are received from mutation request
   useEffect(() => {
