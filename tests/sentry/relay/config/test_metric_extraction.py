5791cdce2c0813c6cac51465af9a6cd855480f13
Simon Hellmayr <shellmayr@users.noreply.github.com>
2024-09-02 12:42:42 +0200

    chore(metrics): remove span metrics relay config & serializers (#76779)

--- tests/sentry/relay/config/test_metric_extraction.py
+++ tests/sentry/relay/config/test_metric_extraction.py
@@ -6,14 +6,12 @@ from unittest import mock
 
 import pytest
 from django.utils import timezone
-from sentry_relay.processing import normalize_project_config
 
 from sentry.incidents.models.alert_rule import AlertRule, AlertRuleProjects
 from sentry.models.dashboard_widget import DashboardWidgetQuery, DashboardWidgetQueryOnDemand
 from sentry.models.environment import Environment
 from sentry.models.project import Project
 from sentry.models.transaction_threshold import ProjectTransactionThreshold, TransactionMetric
-from sentry.relay.config.experimental import TimeoutException
 from sentry.relay.config.metric_extraction import (
     _set_bulk_cached_query_chunk,
     get_current_widget_specs,
@@ -21,7 +19,6 @@ from sentry.relay.config.metric_extraction import (
 )
 from sentry.relay.types import RuleCondition
 from sentry.search.events.constants import VITAL_THRESHOLDS
-from sentry.sentry_metrics.models import SpanAttributeExtractionRuleConfig
 from sentry.snuba.dataset import Dataset
 from sentry.snuba.metrics.extraction import (
     MetricSpec,
@@ -2108,198 +2105,3 @@ def test_get_current_widget_specs(
     ):
         specs = get_current_widget_specs(default_project.organization)
     assert specs == expected
-
-
-@django_db_all
-def test_get_span_attribute_metrics(default_project: Project) -> None:
-    extraction_configs = [
-        {
-            "spanAttribute": "span.duration",
-            "aggregates": ["count", "p50", "p75", "p90", "p95", "p99"],
-            "unit": "millisecond",
-            "tags": ["foo"],
-            "conditions": [
-                {"id": 1, "value": "bar:baz"},
-                {"id": 2, "value": "abc:xyz"},
-            ],
-        },
-        {
-            "spanAttribute": "other_attribute",
-            "aggregates": ["count"],
-            "unit": "none",
-            "tags": ["mytag"],
-            "conditions": [{"id": 3, "value": ""}],
-        },
-    ]
-    for extraction_config in extraction_configs:
-        SpanAttributeExtractionRuleConfig.from_dict(extraction_config, 1, default_project)
-
-    config = get_metric_extraction_config(default_project)
-    assert not config
-
-    with Feature("organizations:custom-metrics-extraction-rule"):
-        config = get_metric_extraction_config(default_project)
-        assert config
-        assert sorted(config["metrics"], key=lambda x: x["mri"]) == [
-            {
-                "category": "span",
-                "condition": {"name": "span.data.bar", "op": "eq", "value": "baz"},
-                "field": None,
-                "mri": "c:custom/span_attribute_1@none",
-                "tags": [
-                    {"field": "span.data.bar", "key": "bar"},
-                    {"field": "span.data.foo", "key": "foo"},
-                ],
-            },
-            {
-                "category": "span",
-                "condition": {"name": "span.data.abc", "op": "eq", "value": "xyz"},
-                "field": None,
-                "mri": "c:custom/span_attribute_2@none",
-                "tags": [
-                    {"field": "span.data.abc", "key": "abc"},
-                    {"field": "span.data.foo", "key": "foo"},
-                ],
-            },
-            {
-                "category": "span",
-                "condition": {
-                    "inner": {"name": "span.data.other_attribute", "op": "eq", "value": None},
-                    "op": "not",
-                },
-                "field": None,
-                "mri": "c:custom/span_attribute_3@none",
-                "tags": [{"field": "span.data.mytag", "key": "mytag"}],
-            },
-            {
-                "category": "span",
-                "condition": {"name": "span.data.bar", "op": "eq", "value": "baz"},
-                "field": "span.duration",
-                "mri": "d:custom/span_attribute_1@none",
-                "tags": [
-                    {"field": "span.data.bar", "key": "bar"},
-                    {"field": "span.data.foo", "key": "foo"},
-                ],
-            },
-            {
-                "category": "span",
-                "condition": {"name": "span.data.abc", "op": "eq", "value": "xyz"},
-                "field": "span.duration",
-                "mri": "d:custom/span_attribute_2@none",
-                "tags": [
-                    {"field": "span.data.abc", "key": "abc"},
-                    {"field": "span.data.foo", "key": "foo"},
-                ],
-            },
-        ]
-
-
-@django_db_all
-def test_get_span_attribute_metrics_timeout_exception(default_project: Project) -> None:
-    with Feature("organizations:custom-metrics-extraction-rule"):
-        with mock.patch(
-            "sentry.relay.config.metric_extraction._generate_span_attribute_specs",
-            side_effect=TimeoutException,
-        ) as mock_get_span_attribute_specs:
-            mock_get_span_attribute_specs.__name__ = "_generate_span_attribute_specs"
-
-            config = get_metric_extraction_config(default_project)
-            assert config is None
-
-
-@django_db_all
-@override_options({"metric_extraction.max_span_attribute_specs": 1})
-def test_get_metric_extraction_config_span_attributes_above_max_limit(
-    default_project: Project,
-) -> None:
-
-    extraction_configs = [
-        {
-            "spanAttribute": "span.duration",
-            "aggregates": ["p50", "p75", "p90", "p95", "p99"],
-            "unit": "millisecond",
-            "tags": ["foo"],
-            "conditions": [
-                {"id": 1, "value": "bar:baz"},
-                {"id": 2, "value": "abc:xyz"},
-            ],
-        },
-        {
-            "spanAttribute": "other_attribute",
-            "aggregates": ["count"],
-            "unit": "none",
-            "tags": [],
-            "conditions": [{"id": 3, "value": ""}],
-        },
-    ]
-    for extraction_config in extraction_configs:
-        SpanAttributeExtractionRuleConfig.from_dict(extraction_config, 1, default_project)
-
-    with Feature("organizations:custom-metrics-extraction-rule"):
-        config = get_metric_extraction_config(default_project)
-
-        assert config
-        assert len(config["metrics"]) == 1
-
-
-@django_db_all
-@override_options(
-    {
-        "sentry-metrics.extrapolation.enable_transactions": True,
-        "sentry-metrics.extrapolation.enable_spans": True,
-    }
-)
-def test_get_metric_extrapolation_config(default_project: Project) -> None:
-    default_project.update_option("sentry:extrapolate_metrics", True)
-
-    # Create a dummy extraction rule to ensure there is at least one
-    # metric. Otherwise, the spec will be empty.
-    attr_config = {
-        "spanAttribute": "span.duration",
-        "aggregates": ["count"],
-        "unit": "none",
-        "tags": [],
-        "conditions": [{"id": 1, "value": "bar:baz"}],
-    }
-    SpanAttributeExtractionRuleConfig.from_dict(attr_config, 1, default_project)
-
-    with Feature(
-        ["organizations:metrics-extrapolation", "organizations:custom-metrics-extraction-rule"]
-    ):
-        config = get_metric_extraction_config(default_project)
-
-    assert config and config["extrapolate"]
-
-    # Generate boilerplate around minimal project config:
-    project_config = {
-        "allowedDomains": ["*"],
-        "piiConfig": None,
-        "trustedRelays": [],
-        "metricExtraction": config,
-    }
-
-    normalized = normalize_project_config(project_config)["metricExtraction"]["extrapolate"]
-    assert normalized == config["extrapolate"]
-
-
-@django_db_all
-def test_get_metric_extraction_config_when_on_demand_metrics_specs_timeout_exception(
-    default_project: Project, default_environment: Environment
-) -> None:
-    with Feature(ON_DEMAND_METRICS):
-        create_alert(
-            "count()",
-            "device.platform:android OR device.platform:ios",
-            default_project,
-            environment=default_environment,
-        )
-
-        with mock.patch(
-            "sentry.relay.config.metric_extraction.get_on_demand_metric_specs",
-            side_effect=TimeoutException,
-        ) as mock_get_on_demand_metric_specs:
-            mock_get_on_demand_metric_specs.__name__ = (
-                "get_on_demand_metric_specs"  # needs to be set for the mock to work
-            )
-            config = get_metric_extraction_config(default_project)
-            assert config is None
