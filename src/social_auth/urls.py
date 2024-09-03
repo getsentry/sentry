2114f225a7865595049b9dc4ec9a94a489223f74
Valery Brobbey <valery.brobbey@sentry.io>
2024-09-03 15:24:44 -0700

    fix(auth): add /auth/sso to social auth urls

--- src/social_auth/urls.py
+++ src/social_auth/urls.py
@@ -8,6 +8,11 @@ urlpatterns = [
         r"^associate/complete/(?P<backend>[^/]+)/$", complete, name="socialauth_associate_complete"
     ),
     re_path(
+        r"^associate/complete/(?P<backend>[^/]+)/auth/sso/$",
+        complete,
+        name="socialauth_associate_complete_auth_sso",
+    ),
+    re_path(
         r"^associate/(?P<backend>[^/]+)/$",
         auth,
         name="socialauth_associate",
