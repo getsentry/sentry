006d3a2258d5f41b9e92d415a04327f708f58346
LinardsLaugalis <Linards.Laugalis@gmail.com>
2024-09-03 19:28:39 +0300

    fix tests

--- tests/sentry/ownership/test_section_line.py
+++ tests/sentry/ownership/test_section_line.py
@@ -18,32 +18,32 @@ def test_get_owners_both_owners_returns_path_owners():
 
 def test_is_preserved_comment_empty_line_returns_true():
     line = SectionLine("", "", [], [])
-    assert line.is_preserved_comment == True
+    assert line.is_preserved_comment is True
 
 
 def test_is_preserved_valid_comment_returns_true():
     line = SectionLine("# some comment", "", [], [])
-    assert line.is_preserved_comment == True
+    assert line.is_preserved_comment is True
 
 
 def test_should_skip_valid_comment_returns_false():
     line = SectionLine("# some comment", "", [], [])
-    assert line.should_skip() == False
+    assert line.should_skip() is False
 
 
 def test_should_skip_line_with_spaces_returns_true():
     line = SectionLine(" ", " ", [], [])
-    assert line.should_skip() == True
+    assert line.should_skip() is True
 
 
 def test_should_skip_line_valid_path_returns_false():
     line = SectionLine("/fileA.txt", "/fileA.txt", [], [])
-    assert line.should_skip() == False
+    assert line.should_skip() is False
 
 
 def test_should_skip_line_invalid_path_returns_true():
     line = SectionLine(" cde", " cde", [], [])
-    assert line.should_skip() == True
+    assert line.should_skip() is True
 
 
 def test_get_dict_key_invalid_path_returns_original_line():
