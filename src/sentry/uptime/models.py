b52a2610499890ac5d52efb46d88624a7730152e
Dan Fuller <dfuller@sentry.io>
2024-09-03 11:37:49 -0700

    feat(uptime): Add url_domain and url_suffix columns to the uptime subscription model (#76366)
    This adds `url_domain` and `url_suffix` to the uptime subscription
    model. The plan is to store the domainof each url here for use in
    limiting total number of checks per domain
    
    We'll use `url_suffix` to understand how many customers are using a
    specific host, which can be useful for analytics and potentially for
    large downtime detection.
    
    <!-- Describe your PR here. -->

--- src/sentry/uptime/models.py
+++ src/sentry/uptime/models.py
@@ -24,6 +24,15 @@ class UptimeSubscription(BaseRemoteSubscription, DefaultFieldsModel):
 
     # The url to check
     url = models.CharField(max_length=255)
+    # The domain of the url, extracted via TLDExtract
+    url_domain = models.CharField(max_length=255, db_index=True, default="")
+    # The suffix of the url, extracted via TLDExtract. This can be a public suffix, such as .com, .gov.uk, .com.au, or
+    # a private suffix, such as vercel.dev
+    url_domain_suffix = models.CharField(max_length=255, db_index=True, default="")
+    # Org name of the host of the url
+    host_whois_orgname = models.CharField(max_length=255, db_index=True, default="")
+    # Org id of the host of the url
+    host_whois_orgid = models.CharField(max_length=255, db_index=True, default="")
     # How frequently to run the check in seconds
     interval_seconds = models.IntegerField()
     # How long to wait for a response from the url before we assume a timeout
