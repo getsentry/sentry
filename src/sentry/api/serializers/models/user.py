763ac0c7d66e4491c49f1b4b3095344e55a50f35
Christinarlong <christina.long@sentry.io>
2024-09-03 11:02:30 -0700

    im trolling

--- src/sentry/api/serializers/models/user.py
+++ src/sentry/api/serializers/models/user.py
@@ -1,3 +1,3 @@
 from sentry.users.api.serializers.user import DetailedSelfUserSerializer, OrganizationMapping
 
-__all__ = (DetailedSelfUserSerializer, OrganizationMapping)
+__all__ = ("DetailedSelfUserSerializer", "OrganizationMapping")
