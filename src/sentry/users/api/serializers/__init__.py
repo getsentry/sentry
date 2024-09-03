803f453c05b5cfafb029f7fcbd4e538dcd338834
Christinarlong <christina.long@sentry.io>
2024-09-02 17:57:42 -0700

    fix typing on serializers

--- src/sentry/users/api/serializers/__init__.py
+++
@@ -1,3 +0,0 @@
-from sentry.users.api.serializers.authenticator import *  # noqa: F401,F403
-from sentry.users.api.serializers.identity import *  # noqa: F401,F403
-from sentry.users.api.serializers.identityprovider import *  # noqa: F401,F403
