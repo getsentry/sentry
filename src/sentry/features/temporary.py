ce598ca284d3aebdf75c85d9a6320a0a75409ec9
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-03 11:33:13 -0700

    feat(feedback): add a FF for feedback details trace section (#76867)
    Used for https://github.com/getsentry/sentry/pull/76858

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -498,6 +498,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:user-feedback-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # User Feedback Error Link Ingestion Changes
     manager.add("organizations:user-feedback-event-link-ingestion-changes", OrganizationFeature, FeatureHandlerStrategy.OPTIONS, api_expose=False)
+    # Enable display of a trace data section in feedback details
+    manager.add("organizations:user-feedback-trace-section", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable view hierarchies options
     manager.add("organizations:view-hierarchies-options-dev", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable the new explore page
