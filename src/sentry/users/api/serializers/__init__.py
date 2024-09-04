a6fb581335e1a99ba7aa65cd68729f01a2ba0ff3
Christinarlong <christina.long@sentry.io>
2024-09-04 13:50:20 -0700

    move all other user serializers and add typing to email

---
+++ src/sentry/users/api/serializers/__init__.py
@@ -0,0 +1,5 @@
+from .identity import *  # noqa: F401,F403
+from .identityprovider import *  # noqa: F401,F403
+from .useremail import *  # noqa: F401,F403
+from .userip import *  # noqa: F401,F403
+from .userrole import *  # noqa: F401,F403
