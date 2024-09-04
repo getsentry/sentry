76e2574adc89e22f250b787e128ae4d1808d3bbf
Malachi Willey <malwilley@gmail.com>
2024-09-04 14:50:49 -0700

    Fix typing issue

--- src/sentry/testutils/factories.py
+++ src/sentry/testutils/factories.py
@@ -124,7 +124,7 @@ from sentry.models.project import Project
 from sentry.models.projectbookmark import ProjectBookmark
 from sentry.models.projectcodeowners import ProjectCodeOwners
 from sentry.models.projecttemplate import ProjectTemplate
-from sentry.models.release import Release, ReleaseStatus
+from sentry.models.release import Release
 from sentry.models.releasecommit import ReleaseCommit
 from sentry.models.releaseenvironment import ReleaseEnvironment
 from sentry.models.releasefile import ReleaseFile, update_artifact_index
@@ -605,7 +605,7 @@ class Factories:
         date_released: datetime | None = None,
         adopted: datetime | None = None,
         unadopted: datetime | None = None,
-        status: ReleaseStatus | None = None,
+        status: int | None = None,
     ):
         if version is None:
             version = force_str(hexlify(os.urandom(20)))
