5450c40721aa9255a45728ac1fff4b6e0ab8a30e
Christinarlong <christina.long@sentry.io>
2024-09-04 11:58:16 -0700

    revert useremail changes

--- src/sentry/api/serializers/models/useremail.py
+++ src/sentry/api/serializers/models/useremail.py
@@ -1,22 +1,10 @@
-from collections.abc import Mapping
-from typing import Any, TypedDict
-
 from sentry.api.serializers import Serializer, register
-from sentry.users.models.user import User
 from sentry.users.models.useremail import UserEmail
-
-
-class UserEmailSerializerResponse(TypedDict):
-    email: str
-    isPrimary: bool
-    isVerified: bool
 
 
 @register(UserEmail)
 class UserEmailSerializer(Serializer):
-    def serialize(
-        self, obj: UserEmail, attrs: Mapping[str, Any], user: User, **kwargs: Any
-    ) -> UserEmailSerializerResponse:
+    def serialize(self, obj, attrs, user, **kwargs):
         primary_email = UserEmail.objects.get_primary_email(user)
         return {
             "email": obj.email,
