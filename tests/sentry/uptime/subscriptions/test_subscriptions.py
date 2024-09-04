fb772e7ddce0c8ccbbc145e2e20d002231d42f94
Dan Fuller <dfuller@sentry.io>
2024-09-04 11:19:51 -0700

    feat(uptime): Extract domain and suffix from urls when creating uptime monitors (#76367)
    This uses `tldextract` to extract the domain and suffix from urls, for
    use in limiting number of monitors per domain, analytics and detecting
    anomalous failures across monitors.

--- tests/sentry/uptime/subscriptions/test_subscriptions.py
+++ tests/sentry/uptime/subscriptions/test_subscriptions.py
@@ -35,6 +35,24 @@ class CreateUptimeSubscriptionTest(TestCase):
         assert uptime_sub.status == UptimeSubscription.Status.ACTIVE.value
         assert uptime_sub.type == UPTIME_SUBSCRIPTION_TYPE
         assert uptime_sub.url == url
+        assert uptime_sub.url_domain == "sentry"
+        assert uptime_sub.url_domain_suffix == "io"
+        assert uptime_sub.interval_seconds == uptime_sub.interval_seconds
+        assert uptime_sub.timeout_ms == timeout_ms
+
+    def test_private_domain_suffix(self):
+        url = "https://test.vercel.app"
+        interval_seconds = 300
+        timeout_ms = 500
+        uptime_sub = create_uptime_subscription(url, interval_seconds, timeout_ms)
+        # Subscription.subscription_id ends up set in the task, so refresh
+        uptime_sub.refresh_from_db()
+        assert uptime_sub.subscription_id is None
+        assert uptime_sub.status == UptimeSubscription.Status.CREATING.value
+        assert uptime_sub.type == UPTIME_SUBSCRIPTION_TYPE
+        assert uptime_sub.url == url
+        assert uptime_sub.url_domain == "test"
+        assert uptime_sub.url_domain_suffix == "vercel.app"
         assert uptime_sub.interval_seconds == uptime_sub.interval_seconds
         assert uptime_sub.timeout_ms == timeout_ms
 
@@ -81,6 +99,8 @@ class CreateUptimeSubscriptionTest(TestCase):
         assert uptime_sub.status == UptimeSubscription.Status.CREATING.value
         assert uptime_sub.type == UPTIME_SUBSCRIPTION_TYPE
         assert uptime_sub.url == url
+        assert uptime_sub.url_domain == "sentry"
+        assert uptime_sub.url_domain_suffix == "io"
         assert uptime_sub.interval_seconds == uptime_sub.interval_seconds
         assert uptime_sub.timeout_ms == timeout_ms
 
