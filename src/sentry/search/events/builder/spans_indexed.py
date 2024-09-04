bd1d80c3395a2be94f6b6282ebe02c8494f5153d
William Mak <william@wmak.io>
2024-09-04 14:32:12 -0400

    fix(spans): Add duration to eap spans (#76937)
    - This enables the span.duration field for the spans dataset
    - For now this is going to continue inheriting the entirety of the
    dataset config until we do more testing

--- src/sentry/search/events/builder/spans_indexed.py
+++ src/sentry/search/events/builder/spans_indexed.py
@@ -4,7 +4,10 @@ from snuba_sdk import Column, Function
 from sentry.search.events import constants
 from sentry.search.events.builder.base import BaseQueryBuilder
 from sentry.search.events.builder.discover import TimeseriesQueryBuilder, TopEventsQueryBuilder
-from sentry.search.events.datasets.spans_indexed import SpansIndexedDatasetConfig
+from sentry.search.events.datasets.spans_indexed import (
+    SpansEAPDatasetConfig,
+    SpansIndexedDatasetConfig,
+)
 from sentry.search.events.fields import custom_time_processor
 from sentry.search.events.types import SelectType
 from sentry.snuba.dataset import Dataset
@@ -60,7 +63,7 @@ class SpansEAPQueryBuilder(SpansIndexedQueryBuilderMixin, BaseQueryBuilder):
     requires_organization_condition = True
     uuid_fields = SPAN_UUID_FIELDS
     span_id_fields = SPAN_ID_FIELDS
-    config_class = SpansIndexedDatasetConfig
+    config_class = SpansEAPDatasetConfig
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
