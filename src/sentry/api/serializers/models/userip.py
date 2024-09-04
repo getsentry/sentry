fd588bb07a2b83d439b5bda93355421ba1348951
Christinarlong <christina.long@sentry.io>
2024-09-04 11:54:28 -0700

    revert userip changes

--- src/sentry/api/serializers/models/userip.py
+++ src/sentry/api/serializers/models/userip.py
@@ -1,26 +1,10 @@
-from collections.abc import Mapping
-from datetime import datetime
-from typing import Any, TypedDict
-
 from sentry.api.serializers import Serializer, register
-from sentry.users.models.user import User
 from sentry.users.models.userip import UserIP
-
-
-class UserIPSerializerResponse(TypedDict):
-    id: str
-    ipAddress: str
-    countryCode: str | None
-    regionCode: str | None
-    lastSeen: datetime
-    firstSeen: datetime
 
 
 @register(UserIP)
 class UserIPSerializer(Serializer):
-    def serialize(
-        self, obj: UserIP, attrs: Mapping[str, Any], user: User, **kwargs: Any
-    ) -> UserIPSerializerResponse:
+    def serialize(self, obj, attrs, user, **kwargs):
         return {
             "id": str(obj.id),
             "ipAddress": obj.ip_address,
