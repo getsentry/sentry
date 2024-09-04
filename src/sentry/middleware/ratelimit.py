864e639114088759509b828bc8242521e0d7dff5
Josh Ferge <josh.ferge@sentry.io>
2024-09-04 16:45:25 -0700

    perf(ratelimits): remove timing metric (#76954)
    seeing super bizarre perf characteristics of this function in hybrid
    cloud calls. the function has no async calls, and spans confirm it
    doesn't really do anything. One idea is that its this timing metric
    itself that causes problems. perhaps it runs too often. will remove this
    metric as a sanity check.
    
    [search to
    monitor](https://sentry.sentry.io/traces/?project=1&query=span.description%3Asentry.middleware.ratelimit.RatelimitMiddleware.process_view%20transaction%3A%2Fapi%2F0%2Finternal%2Frpc%2F%7Bservice_name%7D%2F%7Bmethod_name%7D%2F%20span.duration%3A%3E1s&statsPeriod=24h)

--- src/sentry/middleware/ratelimit.py
+++ src/sentry/middleware/ratelimit.py
@@ -49,86 +49,84 @@ class RatelimitMiddleware:
     ) -> HttpResponseBase | None:
         """Check if the endpoint call will violate."""
 
-        with metrics.timer("middleware.ratelimit.process_view"):
-            try:
-                with sentry_sdk.start_span(op="ratelimit.early_return"):
-                    # TODO: put these fields into their own object
-                    request.will_be_rate_limited = False
-                    if settings.SENTRY_SELF_HOSTED:
-                        return None
-                    request.rate_limit_category = None
-                    request.rate_limit_uid = uuid.uuid4().hex
-                    view_class = getattr(view_func, "view_class", None)
-                    if not view_class:
-                        return None
+        try:
+            with sentry_sdk.start_span(op="ratelimit.early_return"):
+                # TODO: put these fields into their own object
+                request.will_be_rate_limited = False
+                if settings.SENTRY_SELF_HOSTED:
+                    return None
+                request.rate_limit_category = None
+                request.rate_limit_uid = uuid.uuid4().hex
+                view_class = getattr(view_func, "view_class", None)
+                if not view_class:
+                    return None
 
-                    enforce_rate_limit = getattr(view_class, "enforce_rate_limit", False)
-                    if enforce_rate_limit is False:
-                        return None
+                enforce_rate_limit = getattr(view_class, "enforce_rate_limit", False)
+                if enforce_rate_limit is False:
+                    return None
 
-                with sentry_sdk.start_span(op="ratelimit.determine_limit_config"):
-                    rate_limit_config = get_rate_limit_config(
-                        view_class, view_args, {**view_kwargs, "request": request}
-                    )
-                    rate_limit_group = (
-                        rate_limit_config.group if rate_limit_config else RateLimitConfig().group
-                    )
-                    request.rate_limit_key = get_rate_limit_key(
-                        view_func, request, rate_limit_group, rate_limit_config
-                    )
-                    if request.rate_limit_key is None:
-                        return None
-
-                    category_str = request.rate_limit_key.split(":", 1)[0]
-                    request.rate_limit_category = category_str
-
-                    rate_limit = get_rate_limit_value(
-                        http_method=request.method,
-                        category=RateLimitCategory(category_str),
-                        rate_limit_config=rate_limit_config,
-                    )
-                    if rate_limit is None:
-                        return None
-
-                with sentry_sdk.start_span(op="ratelimit.above_rate_limit_check"):
-                    request.rate_limit_metadata = above_rate_limit_check(
-                        request.rate_limit_key, rate_limit, request.rate_limit_uid, rate_limit_group
-                    )
-
-                # TODO: also limit by concurrent window once we have the data
-                rate_limit_cond = (
-                    request.rate_limit_metadata.rate_limit_type != RateLimitType.NOT_LIMITED
-                    if settings.ENFORCE_CONCURRENT_RATE_LIMITS
-                    else request.rate_limit_metadata.rate_limit_type == RateLimitType.FIXED_WINDOW
+            with sentry_sdk.start_span(op="ratelimit.determine_limit_config"):
+                rate_limit_config = get_rate_limit_config(
+                    view_class, view_args, {**view_kwargs, "request": request}
                 )
-                if rate_limit_cond:
-                    request.will_be_rate_limited = True
-                    logger.info(
-                        "sentry.api.rate-limit.exceeded",
-                        extra={
-                            "key": request.rate_limit_key,
-                            "url": request.build_absolute_uri(),
-                            "limit": request.rate_limit_metadata.limit,
-                            "window": request.rate_limit_metadata.window,
-                        },
-                    )
-                    response = HttpResponse(
-                        orjson.dumps(
-                            DEFAULT_ERROR_MESSAGE.format(
-                                limit=request.rate_limit_metadata.limit,
-                                window=request.rate_limit_metadata.window,
-                            )
-                        ),
-                        status=429,
-                    )
-                    assert request.method is not None
-                    return apply_cors_headers(
-                        request=request, response=response, allowed_methods=[request.method]
-                    )
-            except Exception:
-                logging.exception(
-                    "Error during rate limiting, failing open. THIS SHOULD NOT HAPPEN"
+                rate_limit_group = (
+                    rate_limit_config.group if rate_limit_config else RateLimitConfig().group
                 )
+                request.rate_limit_key = get_rate_limit_key(
+                    view_func, request, rate_limit_group, rate_limit_config
+                )
+                if request.rate_limit_key is None:
+                    return None
+
+                category_str = request.rate_limit_key.split(":", 1)[0]
+                request.rate_limit_category = category_str
+
+                rate_limit = get_rate_limit_value(
+                    http_method=request.method,
+                    category=RateLimitCategory(category_str),
+                    rate_limit_config=rate_limit_config,
+                )
+                if rate_limit is None:
+                    return None
+
+            with sentry_sdk.start_span(op="ratelimit.above_rate_limit_check"):
+                request.rate_limit_metadata = above_rate_limit_check(
+                    request.rate_limit_key, rate_limit, request.rate_limit_uid, rate_limit_group
+                )
+
+            # TODO: also limit by concurrent window once we have the data
+            rate_limit_cond = (
+                request.rate_limit_metadata.rate_limit_type != RateLimitType.NOT_LIMITED
+                if settings.ENFORCE_CONCURRENT_RATE_LIMITS
+                else request.rate_limit_metadata.rate_limit_type == RateLimitType.FIXED_WINDOW
+            )
+            if rate_limit_cond:
+                request.will_be_rate_limited = True
+                logger.info(
+                    "sentry.api.rate-limit.exceeded",
+                    extra={
+                        "key": request.rate_limit_key,
+                        "url": request.build_absolute_uri(),
+                        "limit": request.rate_limit_metadata.limit,
+                        "window": request.rate_limit_metadata.window,
+                    },
+                )
+                response = HttpResponse(
+                    orjson.dumps(
+                        DEFAULT_ERROR_MESSAGE.format(
+                            limit=request.rate_limit_metadata.limit,
+                            window=request.rate_limit_metadata.window,
+                        )
+                    ),
+                    status=429,
+                )
+                assert request.method is not None
+                return apply_cors_headers(
+                    request=request, response=response, allowed_methods=[request.method]
+                )
+        except Exception:
+            logging.exception("Error during rate limiting, failing open. THIS SHOULD NOT HAPPEN")
+
         return None
 
     def process_response(
