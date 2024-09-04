2851b9233a37f11ca7ed0a9cf7fc75deb704813e
Tony Xiao <txiao@sentry.io>
2024-09-04 09:32:59 -0400

    feat(explore): Support multiple visualizes to explore toolbar (#76891)
    This adds support for having multiple visualizes in the explore toolbar
    to generate multiple graphs and aggregates.

--- static/app/views/explore/toolbar/toolbarVisualize.tsx
+++ static/app/views/explore/toolbar/toolbarVisualize.tsx
@@ -1,27 +1,34 @@
-import {useMemo} from 'react';
+import {useCallback, useMemo} from 'react';
 import styled from '@emotion/styled';
 
 import {CompactSelect, type SelectOption} from 'sentry/components/compactSelect';
 import {t} from 'sentry/locale';
+import {defined} from 'sentry/utils';
+import type {ParsedFunction} from 'sentry/utils/discover/fields';
 import {parseFunction} from 'sentry/utils/discover/fields';
-import type {SpanIndexedField} from 'sentry/views/insights/types';
-
 import {
   ALLOWED_VISUALIZE_AGGREGATES,
   ALLOWED_VISUALIZE_FIELDS,
-} from '../hooks/useVisualize';
+  DEFAULT_VISUALIZATION,
+  useVisualizes,
+} from 'sentry/views/explore/hooks/useVisualizes';
+import type {SpanIndexedField} from 'sentry/views/insights/types';
 
-import {ToolbarHeader, ToolbarHeading, ToolbarSection} from './styles';
+import {
+  ToolbarHeader,
+  ToolbarHeaderButton,
+  ToolbarHeading,
+  ToolbarSection,
+} from './styles';
 
-interface ToolbarVisualizeProps {
-  setVisualize: (visualize: string) => void;
-  visualize: string;
-}
+interface ToolbarVisualizeProps {}
 
-export function ToolbarVisualize({visualize, setVisualize}: ToolbarVisualizeProps) {
-  const parsedVisualize = useMemo(() => {
-    return parseFunction(visualize);
-  }, [visualize]);
+export function ToolbarVisualize({}: ToolbarVisualizeProps) {
+  const [visualizes, setVisualizes] = useVisualizes();
+
+  const parsedVisualizes: ParsedFunction[] = useMemo(() => {
+    return visualizes.map(parseFunction).filter(defined);
+  }, [visualizes]);
 
   const fieldOptions: SelectOption<SpanIndexedField>[] = ALLOWED_VISUALIZE_FIELDS.map(
     field => {
@@ -41,33 +48,56 @@ export function ToolbarVisualize({visualize, setVisualize}: ToolbarVisualizeProp
     }
   );
 
+  const addChart = useCallback(() => {
+    setVisualizes([...visualizes, DEFAULT_VISUALIZATION]);
+  }, [setVisualizes, visualizes]);
+
+  const setChartField = useCallback(
+    (index: number, {value}: SelectOption<string>) => {
+      const newVisualizes = [...visualizes];
+      newVisualizes[index] = `${parsedVisualizes[index].name}(${value})`;
+      setVisualizes(newVisualizes);
+    },
+    [parsedVisualizes, setVisualizes, visualizes]
+  );
+
+  const setChartAggregate = useCallback(
+    (index: number, {value}: SelectOption<string>) => {
+      const newVisualizes = [...visualizes];
+      newVisualizes[index] = `${value}(${parsedVisualizes[index].arguments[0]})`;
+      setVisualizes(newVisualizes);
+    },
+    [parsedVisualizes, setVisualizes, visualizes]
+  );
+
   return (
-    <ToolbarSection data-test-id="section-visualize">
+    <ToolbarSection data-test-id="section-visualizes">
       <ToolbarHeader>
         <ToolbarHeading>{t('Visualize')}</ToolbarHeading>
+        <ToolbarHeaderButton size="xs" onClick={addChart} borderless>
+          {t('+Add Chart')}
+        </ToolbarHeaderButton>
       </ToolbarHeader>
-      <ToolbarContent>
-        <CompactSelect
-          size="md"
-          options={fieldOptions}
-          value={parsedVisualize?.arguments[0]}
-          onChange={newField =>
-            setVisualize(`${parsedVisualize?.name}(${newField.value})`)
-          }
-        />
-        <CompactSelect
-          size="md"
-          options={aggregateOptions}
-          value={parsedVisualize?.name}
-          onChange={newAggregate =>
-            setVisualize(`${newAggregate.value}(${parsedVisualize?.arguments[0]})`)
-          }
-        />
-      </ToolbarContent>
+      {parsedVisualizes.map((parsedVisualize, index) => (
+        <VisualizeOption key={index}>
+          <CompactSelect
+            size="md"
+            options={fieldOptions}
+            value={parsedVisualize.arguments[0]}
+            onChange={newField => setChartField(index, newField)}
+          />
+          <CompactSelect
+            size="md"
+            options={aggregateOptions}
+            value={parsedVisualize?.name}
+            onChange={newAggregate => setChartAggregate(index, newAggregate)}
+          />
+        </VisualizeOption>
+      ))}
     </ToolbarSection>
   );
 }
 
-const ToolbarContent = styled('div')`
+const VisualizeOption = styled('div')`
   display: flex;
 `;
