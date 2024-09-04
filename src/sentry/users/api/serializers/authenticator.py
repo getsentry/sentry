6b49f500433e87c526f48b2d3a0cc9057e0c6dad
Christinarlong <christina.long@sentry.io>
2024-09-03 18:28:33 -0700

    add typing for authenticator

--- src/sentry/users/api/serializers/authenticator.py
+++ src/sentry/users/api/serializers/authenticator.py
@@ -1,3 +1,9 @@
+from __future__ import annotations
+
+from collections.abc import Mapping
+from datetime import datetime
+from typing import TYPE_CHECKING, Any, TypedDict, cast
+
 from sentry.api.serializers import Serializer, register
 from sentry.auth.authenticators.base import AuthenticatorInterface
 from sentry.auth.authenticators.recovery_code import RecoveryCodeInterface
@@ -5,11 +11,45 @@ from sentry.auth.authenticators.sms import SmsInterface
 from sentry.auth.authenticators.totp import TotpInterface
 from sentry.auth.authenticators.u2f import U2fInterface
 
+if TYPE_CHECKING:
+    from django.utils.functional import _StrOrPromise
+
+    from sentry.users.models.user import User
+
+
+class EnrolledAuthenticatorInterfaceSerializerResponse(TypedDict, total=False):
+    authId: str
+    createdAt: datetime
+    lastUsedAt: datetime | None
+
+
+class AuthenticatorInterfaceSerializerResponse(EnrolledAuthenticatorInterfaceSerializerResponse):
+    id: str
+    name: _StrOrPromise
+    description: _StrOrPromise
+    rotationWarning: _StrOrPromise | None
+    enrollButton: _StrOrPromise
+    configureButton: _StrOrPromise
+    removeButton: _StrOrPromise | None
+    isBackupInterface: bool
+    isEnrolled: bool
+    disallowNewEnrollment: bool
+    status: str
+    canValidateOtp: bool
+    allowMultiEnrollment: bool
+    allowRotationInPlace: bool
+
 
 @register(AuthenticatorInterface)
 class AuthenticatorInterfaceSerializer(Serializer):
-    def serialize(self, obj, attrs, user, **kwargs):
-        data = {
+    def serialize(
+        self,
+        obj: AuthenticatorInterface,
+        attrs: Mapping[str, Any],
+        user: User,
+        **kwargs: Any,
+    ) -> AuthenticatorInterfaceSerializerResponse:
+        data: AuthenticatorInterfaceSerializerResponse = {
             "id": str(obj.interface_id),
             "name": obj.name,
             "description": obj.description,
@@ -35,10 +75,23 @@ class AuthenticatorInterfaceSerializer(Serializer):
         return data
 
 
+class SmsInterfaceSerializerResponse(AuthenticatorInterfaceSerializerResponse):
+    phone: str
+
+
 @register(SmsInterface)
 class SmsInterfaceSerializer(AuthenticatorInterfaceSerializer):
-    def serialize(self, obj, attrs, user, **kwargs):
-        data = super().serialize(obj, attrs, user)
+    def serialize(
+        self,
+        obj: AuthenticatorInterface,
+        attrs: Mapping[str, Any],
+        user: User,
+        **kwargs: Any,
+    ) -> SmsInterfaceSerializerResponse:
+        data = cast(SmsInterfaceSerializerResponse, super().serialize(obj, attrs, user))
+        assert isinstance(
+            obj, SmsInterface
+        ), "Interface must be SmsInterface to serialize phone number"
         data["phone"] = obj.phone_number
         return data
 
