2e04be1fc284f27f59762c24340ef217ffa8f575
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-09-05 12:17:13 -0400

    feat(dashboards): Events `SearchQueryBuilder` (#76958)
    Follow-up to #76944
    
    I'm using `ResultsSearchQueryBuilder` here because it does everything
    I'd need to do and more. I had to tweak it a little (accept an
    `onChange`, allow an optional `onSearch`, remove a bottom margin) but
    otherwise I'm using it straight as-is.
    
    IMO it probably deserves a more generic name, but to be honest once the
    dataset splits in the UI are complete this might look pretty different.
    
    Here as in the other PR, the flag that controls the query builder is
    `'search-query-builder-discover'` so it's in sync with what Discover is
    doing.

--- static/app/views/discover/resultsSearchQueryBuilder.tsx
+++ static/app/views/discover/resultsSearchQueryBuilder.tsx
@@ -1,5 +1,4 @@
 import {useCallback, useMemo} from 'react';
-import styled from '@emotion/styled';
 import omit from 'lodash/omit';
 
 import {fetchSpanFieldValues, fetchTagValues} from 'sentry/actionCreators/tags';
@@ -14,9 +13,11 @@ import {
 } from 'sentry/components/events/searchBarFieldConstants';
 import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
 import {SearchQueryBuilder} from 'sentry/components/searchQueryBuilder';
-import type {FilterKeySection} from 'sentry/components/searchQueryBuilder/types';
+import type {
+  CallbackSearchState,
+  FilterKeySection,
+} from 'sentry/components/searchQueryBuilder/types';
 import {t} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
 import {SavedSearchType, type TagCollection} from 'sentry/types/group';
 import {defined} from 'sentry/utils';
 import type {CustomMeasurementCollection} from 'sentry/utils/customMeasurements/customMeasurements';
@@ -112,16 +113,18 @@ export const getHasTag = (tags: TagCollection) => ({
 });
 
 type Props = {
-  onSearch: (query: string) => void;
   customMeasurements?: CustomMeasurementCollection;
   dataset?: DiscoverDatasets;
   fields?: Readonly<Field[]>;
   includeSessionTagsValues?: boolean;
   includeTransactions?: boolean;
   omitTags?: string[];
+  onChange?: (query: string, state: CallbackSearchState) => void;
+  onSearch?: (query: string) => void;
   placeholder?: string;
   projectIds?: number[] | Readonly<number[]>;
   query?: string;
+  searchSource?: string;
   supportedTags?: TagCollection | undefined;
 };
 
@@ -277,21 +280,18 @@ function ResultsSearchQueryBuilder(props: Props) {
   }, [filteredTags, dataset]);
 
   return (
-    <StyledResultsSearchQueryBuilder
+    <SearchQueryBuilder
       placeholder={placeholderText}
       filterKeys={getTagList}
       initialQuery={props.query ?? ''}
       onSearch={props.onSearch}
-      searchSource={'eventsv2'}
+      onChange={props.onChange}
+      searchSource={props.searchSource || 'eventsv2'}
       filterKeySections={filterKeySections}
       getTagValues={getEventFieldValues}
       recentSearches={SavedSearchType.EVENT}
     />
   );
 }
-
-const StyledResultsSearchQueryBuilder = styled(SearchQueryBuilder)`
-  margin-bottom: ${space(2)};
-`;
 
 export default ResultsSearchQueryBuilder;
