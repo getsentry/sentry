b7a3d08f37effb5ed5906acd5cff4eee6a04bd78
Christinarlong <christina.long@sentry.io>
2024-09-05 14:08:52 -0700

    remove barrel file

--- src/sentry/users/api/serializers/__init__.py
+++
@@ -1,5 +0,0 @@
-from .identity import *  # noqa: F401,F403
-from .identityprovider import *  # noqa: F401,F403
-from .useremail import *  # noqa: F401,F403
-from .userip import *  # noqa: F401,F403
-from .userrole import *  # noqa: F401,F403
