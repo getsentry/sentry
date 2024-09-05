5290623891dd8b74e962411270ba40adc5b94f7e
Edward Gou <edward.gou@sentry.io>
2024-09-05 14:05:38 -0400

    locale

--- static/app/views/insights/queues/alerts.ts
+++ static/app/views/insights/queues/alerts.ts
@@ -1,22 +1,24 @@
+import {t} from 'sentry/locale';
+
 export const ALERTS = {
   latency: {
     aggregate: 'avg(g:spans/messaging.message.receive.latency@millisecond)',
     query: 'span.op:queue.process',
-    name: 'Create Time in Queue Alert',
+    name: t('Create Time in Queue Alert'),
   },
   duration: {
     aggregate: 'avg(d:spans/duration@millisecond)',
     query: 'span.op:queue.process',
-    name: 'Create Processing Time Alert',
+    name: t('Create Processing Time Alert'),
   },
   processed: {
     aggregate: 'spm()',
     query: 'span.op:queue.process',
-    name: 'Create Processed Alert',
+    name: t('Create Processed Alert'),
   },
   published: {
     aggregate: 'spm()',
     query: 'span.op:queue.publish',
-    name: 'Create Published Alert',
+    name: t('Create Published Alert'),
   },
 };
