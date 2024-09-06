e445931baa5c6ddc7339258742932e8e3fcc8a8a
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-09-06 13:13:12 +0200

    ref(whats-new): Revamp "Whats New" (#76818)

--- static/app/components/sidebar/broadcasts.tsx
+++ static/app/components/sidebar/broadcasts.tsx
@@ -4,6 +4,7 @@ import {getAllBroadcasts, markBroadcastsAsSeen} from 'sentry/actionCreators/broa
 import type {Client} from 'sentry/api';
 import DemoModeGate from 'sentry/components/acl/demoModeGate';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {BroadcastPanelItem} from 'sentry/components/sidebar/broadcastPanelItem';
 import SidebarItem from 'sentry/components/sidebar/sidebarItem';
 import SidebarPanel from 'sentry/components/sidebar/sidebarPanel';
 import SidebarPanelEmpty from 'sentry/components/sidebar/sidebarPanelEmpty';
@@ -115,10 +116,11 @@ class Broadcasts extends Component<Props, State> {
   }
 
   render() {
-    const {orientation, collapsed, currentPanel, hidePanel} = this.props;
+    const {orientation, collapsed, currentPanel, hidePanel, organization} = this.props;
     const {broadcasts, loading} = this.state;
 
     const unseenPosts = this.unseenIds;
+    const whatIsNewRevampFeature = organization.features.includes('what-is-new-revamp');
 
     return (
       <DemoModeGate>
@@ -149,6 +151,18 @@ class Broadcasts extends Component<Props, State> {
                 <SidebarPanelEmpty>
                   {t('No recent updates from the Sentry team.')}
                 </SidebarPanelEmpty>
+              ) : whatIsNewRevampFeature ? (
+                broadcasts.map(item => (
+                  <BroadcastPanelItem
+                    key={item.id}
+                    hasSeen={item.hasSeen}
+                    title={item.title}
+                    message={item.message}
+                    link={item.link}
+                    mediaUrl={item.mediaUrl}
+                    category={item.category}
+                  />
+                ))
               ) : (
                 broadcasts.map(item => (
                   <SidebarPanelItem
