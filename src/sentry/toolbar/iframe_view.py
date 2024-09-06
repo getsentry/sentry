e42a32d937bd9ab76d0e957b847189ba640af491
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-06 12:49:44 -0700

    X-Frame-Options, determine_active_org, and login_success_popup

--- src/sentry/toolbar/iframe_view.py
+++ src/sentry/toolbar/iframe_view.py
@@ -1,21 +1,25 @@
+from django.http import HttpRequest, HttpResponse
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
-from rest_framework.response import Response
 
-from sentry.web.frontend.base import region_silo_view
+from sentry.types.region import subdomain_is_region
+from sentry.utils.http import is_using_customer_domain
+from sentry.web.frontend.base import OrganizationMixin
 from sentry.web.helpers import render_to_response
 
 
-@region_silo_view
-class IframeView(View):
-    """
-    (description)
-    """
-
+class IframeView(View, OrganizationMixin):
     @method_decorator(csrf_exempt)
-    def dispatch(self, request, *args, **kwargs):
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         if request.method != "GET":
-            return Response(status=405)
+            return HttpResponse(status=405)
 
-        return render_to_response("sentry/toolbar/iframe.html", status=200, request=request)
+        organization_slug = kwargs.get("organization_slug", None)
+        if request and is_using_customer_domain(request) and not subdomain_is_region(request):
+            organization_slug = request.subdomain
+        self.determine_active_organization(request, organization_slug)
+
+        response = render_to_response("sentry/toolbar/iframe.html")
+        response["X-Frame-Options"] = "ALLOWALL"
+        return response
