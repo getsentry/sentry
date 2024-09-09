378f1eb21c8c71ff2f2f0b1473bf4789a12bbe62
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2024-09-09 14:49:16 -0400

    feat(insights): Adds create alert buttons to resource module charts (#77007)
    Adds Create Alert buttons to resource module charts

--- static/app/views/insights/database/alerts.ts
+++ static/app/views/insights/database/alerts.ts
@@ -1,4 +1,6 @@
-export const ALERTS = {
+import type {AlertConfig} from 'sentry/views/insights/common/components/chartPanel';
+
+export const ALERTS: Record<string, AlertConfig> = {
   duration: {
     aggregate: 'avg(d:spans/exclusive_time@millisecond)',
     query: 'span.module:db has:span.description',
