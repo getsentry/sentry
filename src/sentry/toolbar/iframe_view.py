40cb29969ec16bcf7c7f2258465295d472b458cc
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-09 14:50:45 -0700

    Handle iframe no auth case by wrapping BaseView.dispatch

--- src/sentry/toolbar/iframe_view.py
+++ src/sentry/toolbar/iframe_view.py
@@ -1,7 +1,21 @@
+from django.utils.decorators import method_decorator
+from django.views.decorators.csrf import csrf_exempt
+
 from sentry.web.frontend.base import BaseView
 
 
 class IframeView(BaseView):
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        if self.is_auth_required(request, *args, **kwargs):
+            self.default_context = self.get_context_data(request, *args, **kwargs)
+            # TODO: might want to check the method or org here
+            response = self.respond("sentry/toolbar/iframe-no-auth.html", status=401)
+            response["X-Frame-Options"] = "ALLOWALL"
+            return response
+
+        return super().dispatch(request, *args, **kwargs)
+
     def get(self, request, organization_slug, project_slug):
         response = self.respond("sentry/toolbar/iframe.html")
         response["X-Frame-Options"] = "ALLOWALL"
--- src/sentry/toolbar/login_success_view.py
+++ src/sentry/toolbar/login_success_view.py
@@ -3,4 +3,4 @@ from sentry.web.frontend.base import BaseView
 
 class LoginSuccessView(BaseView):
     def get(self, request, organization_slug, project_slug):
-        return self.respond("sentry/toolbar/login_success.html")
+        return self.respond("sentry/toolbar/login-success.html")
