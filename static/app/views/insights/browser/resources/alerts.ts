cca81a39204df2fb68d3d88cc3b412d1a27984a7
Edward Gou <edward.gou@sentry.io>
2024-09-09 14:35:13 -0400

    types and use existing constants

--- static/app/views/insights/browser/resources/alerts.ts
+++ static/app/views/insights/browser/resources/alerts.ts
@@ -1,6 +1,8 @@
 import {t} from 'sentry/locale';
+import type {AlertConfig} from 'sentry/views/insights/common/components/chartPanel';
+import {SpanMetricsField} from 'sentry/views/insights/types';
 
-export const ALERTS = {
+export const ALERTS: Record<string, AlertConfig> = {
   spm: {
     aggregate: 'spm()',
   },
@@ -8,15 +10,15 @@ export const ALERTS = {
     aggregate: 'avg(d:spans/duration@millisecond)',
   },
   decodedSize: {
-    aggregate: 'avg(d:spans/http.decoded_response_content_length@byte)',
+    aggregate: `avg(d:spans/${SpanMetricsField.HTTP_DECODED_RESPONSE_CONTENT_LENGTH}@byte)`,
     name: t('Create Decoded Size Alert'),
   },
   transferSize: {
-    aggregate: 'avg(d:spans/http.response_transfer_size@byte)',
+    aggregate: `avg(d:spans/${SpanMetricsField.HTTP_RESPONSE_TRANSFER_SIZE}@byte)`,
     name: t('Create Transfer Size Alert'),
   },
   encodedSize: {
-    aggregate: 'avg(d:spans/http.response_content_length@byte)',
+    aggregate: `avg(d:spans/${SpanMetricsField.HTTP_RESPONSE_CONTENT_LENGTH}@byte)`,
     name: t('Create Encoded Size Alert'),
   },
 };
