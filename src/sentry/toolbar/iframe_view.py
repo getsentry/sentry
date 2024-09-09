3f5d102347477750327eb661da7e6ce224bb1303
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-09 13:56:14 -0700

    Auto-close popup and rm csrf_exempt

--- src/sentry/toolbar/iframe_view.py
+++ src/sentry/toolbar/iframe_view.py
@@ -1,11 +1,7 @@
-from django.utils.decorators import method_decorator
-from django.views.decorators.csrf import csrf_exempt
-
 from sentry.web.frontend.base import BaseView
 
 
 class IframeView(BaseView):
-    @method_decorator(csrf_exempt)
     def get(self, request, organization_slug):
         response = self.respond("sentry/toolbar/iframe.html")
         response["X-Frame-Options"] = "ALLOWALL"
