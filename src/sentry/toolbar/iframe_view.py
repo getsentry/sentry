6e42ccb6af10e343c47fd95956b6404b7b7def4b
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-09 15:32:34 -0700

    Handle reload iframe on msg from popup->main

--- src/sentry/toolbar/iframe_view.py
+++ src/sentry/toolbar/iframe_view.py
@@ -5,18 +5,26 @@ from sentry.web.frontend.base import BaseView
 
 
 class IframeView(BaseView):
+    security_headers = {
+        "X-Frame-Options": "ALLOWALL",
+        # TODO: # Not working, seems to get overridden by a BaseView wrapper
+        # "Content-Security-Policy": "frame-ancestors http://dev.getsentry.net/ http: https:",
+    }
+
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         if self.is_auth_required(request, *args, **kwargs):
             self.default_context = self.get_context_data(request, *args, **kwargs)
             # TODO: might want to check the method or org here
             response = self.respond("sentry/toolbar/iframe-no-auth.html", status=401)
-            response["X-Frame-Options"] = "ALLOWALL"
+            for header, val in IframeView.security_headers.items():
+                response[header] = val
             return response
 
         return super().dispatch(request, *args, **kwargs)
 
     def get(self, request, organization_slug, project_slug):
         response = self.respond("sentry/toolbar/iframe.html")
-        response["X-Frame-Options"] = "ALLOWALL"
+        for header, val in IframeView.security_headers.items():
+            response[header] = val
         return response
