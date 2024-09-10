b5a189292849461c665d3a00f1c48e69ee2768a2
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-09-10 18:27:30 -0400

    feat(replay): Support absolute time (#77251)
    Adds support for absolute time.
    
    All timestamps on the replay details page and breadcrumbs list support
    absolute time
    <img width="1506" alt="image"
    src="https://github.com/user-attachments/assets/d8e39002-8b61-460d-88ea-efc22edbd5f0">
    
    The timestamp tooltip has changes to show the absolute time and the time
    within replay in milliseconds
    <img width="784" alt="image"
    src="https://github.com/user-attachments/assets/10bd42e8-605f-480b-8e2a-d976b890da98">
    
    
    
    Closes: https://github.com/getsentry/sentry/issues/52682

--- static/app/components/replays/preferences/replayPreferenceDropdown.tsx
+++ static/app/components/replays/preferences/replayPreferenceDropdown.tsx
@@ -3,6 +3,9 @@ import {CompositeSelect} from 'sentry/components/compactSelect/composite';
 import {IconSettings} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {useReplayPrefs} from 'sentry/utils/replays/playback/providers/replayPreferencesContext';
+import {toTitleCase} from 'sentry/utils/string/toTitleCase';
+
+const timestampOptions: ('relative' | 'absolute')[] = ['relative', 'absolute'];
 
 export default function ReplayPreferenceDropdown({
   speedOptions,
@@ -33,6 +36,15 @@ export default function ReplayPreferenceDropdown({
         onChange={opt => setPrefs({playbackSpeed: opt.value})}
         options={speedOptions.map(option => ({
           label: `${option}x`,
+          value: option,
+        }))}
+      />
+      <CompositeSelect.Region
+        label={t('Timestamps')}
+        value={prefs.timestampType}
+        onChange={opt => setPrefs({timestampType: opt.value})}
+        options={timestampOptions.map(option => ({
+          label: `${toTitleCase(option)}`,
           value: option,
         }))}
       />
