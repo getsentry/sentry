27f7c7bc85ad410ceafc158167629529dd665e00
Mark Story <mark@mark-story.com>
2024-09-10 18:03:22 -0400

    chore(deletions) Move RuleFireHistory to model deletions
    Ordering NotificationMessage by id when doing a bulk delete doesn't work
    because a message could be past retention, but also be a 'parent' for
    a message that is within retention.
    
    Similarily rulefirehistory records cannot be deleted by date alone as
    there are foreign keys in notification message. By moving
    rulefirehistory deletion to model deletes, we'll be able to traverse
    relations and remove all notification messages related to
    a rulefirehistory at once.
    
    This does create the opportunity for the parent message to be removed
    before the thread is 'complete'. Based on my interpretation of rule
    fire history a new parent message will be created on the next trigger.
    
    Refs SENTRY-3BW5
    Refs SENTRY-3E5X

--- src/sentry/deletions/__init__.py
+++ src/sentry/deletions/__init__.py
@@ -176,6 +176,7 @@ def load_defaults():
     default_manager.register(models.UserReport, BulkModelDeletionTask)
     default_manager.register(models.ArtifactBundle, ArtifactBundleDeletionTask)
     default_manager.register(models.Rule, defaults.RuleDeletionTask)
+    default_manager.register(models.RuleFireHistory, defaults.RuleFireHistoryDeletionTask)
 
 
 load_defaults()
