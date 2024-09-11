27c6a57c3e2205b07ebc57e9208e0cf1d2782282
Dan Fuller <dfuller@sentry.io>
2024-09-11 12:12:43 -0700

    add name unique constraints

--- src/sentry/workflow_engine/models/detector.py
+++ src/sentry/workflow_engine/models/detector.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import UniqueConstraint
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import DefaultFieldsModel, FlexibleForeignKey, region_silo_model
@@ -13,4 +14,9 @@ class Detector(DefaultFieldsModel, OwnerModel):
     name = models.CharField(max_length=200)
 
     class Meta(OwnerModel.Meta):
-        pass
+        constraints = OwnerModel.Meta.constraints + [
+            UniqueConstraint(
+                fields=["organization", "name"],
+                name="workflow_engine_detector_org_name",
+            )
+        ]
