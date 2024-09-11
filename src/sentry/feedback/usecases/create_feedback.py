c0da5e37544fd4be6d730636be5eccb461a6cd2e
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-09-11 16:36:37 -0700

    :art: rm another one

--- src/sentry/feedback/usecases/create_feedback.py
+++ src/sentry/feedback/usecases/create_feedback.py
@@ -367,7 +367,6 @@ def shim_to_feedback(
                 feedback_event["contexts"]["trace"] = event.data["contexts"]["trace"]
 
             feedback_event["timestamp"] = event.datetime.timestamp()
-            feedback_event["level"] = event.data["level"]
             feedback_event["platform"] = event.platform
             feedback_event["level"] = event.data["level"]
             feedback_event["environment"] = event.get_environment().name
@@ -384,7 +383,6 @@ def shim_to_feedback(
             )
 
             feedback_event["timestamp"] = datetime.utcnow().timestamp()
-            feedback_event["platform"] = "other"
 
             if report.get("event_id"):
                 feedback_event["contexts"]["feedback"]["associated_event_id"] = report["event_id"]
