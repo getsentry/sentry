3c2f9e7c2507c295e642302d2c5a28fae45ca3d1
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2024-09-11 14:33:42 -0400

    feat(insights): Adds insights alerts for http module (#77267)
    Adds create alert buttons to http module charts.
    Includes changes to `insightsMetricField` to support functions with non
    MRI args.
    Also organizes some constants better.

--- static/app/views/alerts/rules/metric/utils/isInsightsMetricAlert.tsx
+++ static/app/views/alerts/rules/metric/utils/isInsightsMetricAlert.tsx
@@ -1,14 +1,47 @@
 import {parseField} from 'sentry/utils/metrics/mri';
 
-export const INSIGHTS_METRICS_OPERATIONS = [
+export const INSIGHTS_METRICS_OPERATIONS_WITHOUT_ARGS = [
   {
     label: 'spm',
     value: 'spm',
+    mri: 'd:spans/duration@millisecond',
   },
   {
     label: 'cache_miss_rate',
     value: 'cache_miss_rate',
+    mri: 'd:spans/duration@millisecond',
   },
+];
+
+export const INSIGHTS_METRICS_OPERATIONS_WITH_CUSTOM_ARGS = [
+  {
+    label: 'http_response_rate',
+    value: 'http_response_rate',
+    options: [
+      {label: '3', value: '3'},
+      {label: '4', value: '4'},
+      {label: '5', value: '5'},
+    ],
+    mri: 'd:spans/duration@millisecond',
+  },
+  {
+    label: 'performance_score',
+    value: 'performance_score',
+    options: [
+      {label: 'measurements.score.lcp', value: 'measurements.score.lcp'},
+      {label: 'measurements.score.fcp', value: 'measurements.score.fcp'},
+      {label: 'measurements.score.inp', value: 'measurements.score.inp'},
+      {label: 'measurements.score.cls', value: 'measurements.score.cls'},
+      {label: 'measurements.score.ttfb', value: 'measurements.score.ttfb'},
+      {label: 'measurements.score.total', value: 'measurements.score.total'},
+    ],
+    mri: 'd:transactions/measurements.score.total@ratio',
+  },
+];
+
+export const INSIGHTS_METRICS_OPERATIONS = [
+  ...INSIGHTS_METRICS_OPERATIONS_WITH_CUSTOM_ARGS,
+  ...INSIGHTS_METRICS_OPERATIONS_WITHOUT_ARGS,
 ];
 
 export const INSIGHTS_METRICS = [
