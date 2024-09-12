60ce280b25df5b4e93b8269f0fc06c027d09235d
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-09-12 10:58:55 -0700

    Alerts Create Issues: Workflow Actions (#77361)
    ## Description
    Spec:
    https://www.notion.so/sentry/Alerts-Create-Issues-9114a498098143178839d584c293fe75?pvs=4#8675cde09f734e9cb86f8b1a45e459fc
    
    This is a little different from the spec, breaking the `type` and the
    `data` apart. From here, we can use a factory pattern to execute the
    different workflow actions.
    
    Each action will have a schema it defines for the data properties as
    well, this will allow us to enforce a light schema in the future.
    Support will be added for the json schema definitions when we start to
    fill in the base abstraction for the workflow actions.

--- src/sentry/testutils/helpers/backups.py
+++ src/sentry/testutils/helpers/backups.py
@@ -608,7 +608,8 @@ class ExhaustiveFixtures(Fixtures):
             access_end=timezone.now() + timedelta(days=1),
         )
 
-        self.create_workflow(organization=org)
+        workflow = self.create_workflow(organization=org)
+        self.create_workflowaction(workflow=workflow)
         self.create_datasource(organization=org)
         self.create_detector(organization=org)
 
--- src/sentry/workflow_engine/models/__init__.py
+++ src/sentry/workflow_engine/models/__init__.py
@@ -1,5 +1,6 @@
-__all__ = ["DataSource", "Detector", "Workflow"]
+__all__ = ["DataSource", "Detector", "Workflow", "WorkflowAction"]
 
 from .data_source import DataSource
 from .detector import Detector
 from .workflow import Workflow
+from .workflow_action import WorkflowAction
