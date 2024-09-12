5117fe891cef1693c8833dd58d542bafb5551e1e
Ryan Skonnord <ryan.skonnord@sentry.io>
2024-09-12 11:06:28 -0700

    Temporary getsentry fix

--- src/sentry/integrations/slack/__init__.py
+++ src/sentry/integrations/slack/__init__.py
@@ -1,3 +1,6 @@
 from sentry.integrations.slack.spec import SlackMessagingSpec
 
+# TODO: Delete this after removing getsentry dependency
+from .actions.notification import SlackNotifyServiceAction  # noqa: F401,F403
+
 SlackMessagingSpec().initialize()
