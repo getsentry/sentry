435d7d20b4b28ddba17e664b7c26f717e19fac97
Ash <0Calories@users.noreply.github.com>
2024-09-12 17:12:02 -0400

    fix(insights): Queries module page filters did not include `span.system` (#77349)
    Fixes an issue where the action (command) and domain (table) selectors
    in the DB module were not including the currently selected database
    system. With this change, the options for these page filters will only
    show DB commands and tables that are valid for the currently selected
    database system.
    
    ---------
    
    Co-authored-by: Matt Quinn <matt.quinn@sentry.io>

--- static/app/views/insights/database/components/databaseSystemSelector.tsx
+++ static/app/views/insights/database/components/databaseSystemSelector.tsx
@@ -6,7 +6,7 @@ import {useNavigate} from 'sentry/utils/useNavigate';
 import {useSystemSelectorOptions} from 'sentry/views/insights/database/components/useSystemSelectorOptions';
 import {SpanMetricsField} from 'sentry/views/insights/types';
 
-const {SPAN_SYSTEM} = SpanMetricsField;
+const {SPAN_SYSTEM, SPAN_DOMAIN, SPAN_ACTION} = SpanMetricsField;
 
 export function DatabaseSystemSelector() {
   const location = useLocation();
@@ -21,7 +21,16 @@ export function DatabaseSystemSelector() {
     <CompactSelect
       onChange={option => {
         setSelectedSystem(option.value);
-        navigate({...location, query: {...location.query, [SPAN_SYSTEM]: option.value}});
+        navigate({
+          ...location,
+          query: {
+            ...location.query,
+            [SPAN_SYSTEM]: option.value,
+            // Reset the table and command since they won't be valid if the DB system changes
+            [SPAN_DOMAIN]: undefined,
+            [SPAN_ACTION]: undefined,
+          },
+        });
       }}
       options={options}
       triggerProps={{prefix: t('DB System')}}
