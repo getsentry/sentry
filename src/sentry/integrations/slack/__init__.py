af42cd081733d14aa2eaf893ce1d9e184490dda1
Ryan Skonnord <ryan.skonnord@sentry.io>
2024-09-12 13:27:42 -0700

    Revert "Temporary getsentry fix"
    This reverts commit 5117fe891cef1693c8833dd58d542bafb5551e1e.

--- src/sentry/integrations/slack/__init__.py
+++ src/sentry/integrations/slack/__init__.py
@@ -1,6 +1,3 @@
 from sentry.integrations.slack.spec import SlackMessagingSpec
 
-# TODO: Delete this after removing getsentry dependency
-from .actions.notification import SlackNotifyServiceAction  # noqa: F401,F403
-
 SlackMessagingSpec().initialize()
