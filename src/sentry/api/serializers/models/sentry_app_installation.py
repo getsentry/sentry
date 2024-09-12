getsentry/sentry

commit 3afbcb9df0921931be223cf75a39cdddf2c1a0bd

fix(security): hide integrations code from members, attempt 2 (#71551)
Another attempt of https://github.com/getsentry/sentry/pull/70128
This also adds the missing access parameter to the serializer call in
`post()` method.

src/sentry/api/serializers/models/sentry_app_installation.py
@@ -36,7 +36,7 @@ class SentryAppInstallationSerializer(Serializer):
             }
         return result
 
-    def serialize(self, install, attrs, user):
+    def serialize(self, install, attrs, user, access=None):
         data = {
             "app": {"uuid": attrs["sentry_app"].uuid, "slug": attrs["sentry_app"].slug},
             "organization": {"slug": attrs["organization"].slug},
@@ -44,7 +44,7 @@ class SentryAppInstallationSerializer(Serializer):
             "status": SentryAppInstallationStatus.as_str(install.status),
         }
 
-        if install.api_grant:
+        if install.api_grant and access and access.has_scope("org:integrations"):
             data["code"] = install.api_grant.code
 
         return data

tests/sentry/mediators/sentry_app_installations/test_installation_notifier.py
@@ -56,7 +56,6 @@ class TestInstallationNotifier(TestCase):
                     "app": {"uuid": self.sentry_app.uuid, "slug": self.sentry_app.slug},
                     "organization": {"slug": self.organization.slug},
                     "uuid": self.install.uuid,
-                    "code": self.install.api_grant.code,
                     "status": "installed",
                 }
             },
@@ -85,7 +84,6 @@ class TestInstallationNotifier(TestCase):
                     "app": {"uuid": self.sentry_app.uuid, "slug": self.sentry_app.slug},
                     "organization": {"slug": self.organization.slug},
                     "uuid": self.install.uuid,
-                    "code": self.install.api_grant.code,
                     "status": "installed",
                 }
             },

src/sentry/api/endpoints/integrations/sentry_apps/installation/details.py
@@ -27,7 +27,9 @@ class SentryAppInstallationDetailsEndpoint(SentryAppInstallationBaseEndpoint):
     }
 
     def get(self, request: Request, installation) -> Response:
-        return Response(serialize(SentryAppInstallation.objects.get(id=installation.id)))
+        return Response(
+            serialize(SentryAppInstallation.objects.get(id=installation.id), access=request.access)
+        )
 
     def delete(self, request: Request, installation) -> Response:
         installation = SentryAppInstallation.objects.get(id=installation.id)

src/sentry/api/endpoints/integrations/sentry_apps/installation/index.py
@@ -39,7 +39,7 @@ class SentryAppInstallationsEndpoint(SentryAppInstallationsBaseEndpoint):
             queryset=queryset,
             order_by="-date_added",
             paginator_cls=OffsetPaginator,
-            on_results=lambda x: serialize(x, request.user),
+            on_results=lambda x: serialize(x, request.user, access=request.access),
         )
 
     def post(self, request: Request, organization) -> Response:
@@ -94,4 +94,4 @@ class SentryAppInstallationsEndpoint(SentryAppInstallationsBaseEndpoint):
                 organization_id=organization.id, slug=slug, notify=True
             ).run(user=request.user, request=request)
 
-        return Response(serialize(install))
+        return Response(serialize(install, access=request.access))

tests/sentry/api/endpoints/test_organization_sentry_app_installation_details.py
@@ -58,8 +58,26 @@ class SentryAppInstallationDetailsTest(APITestCase):
 
 @control_silo_test
 class GetSentryAppInstallationDetailsTest(SentryAppInstallationDetailsTest):
-    def test_access_within_installs_organization(self):
-        self.login_as(user=self.user)
+    def test_access_within_installs_organization_by_member(self):
+        member_user = self.create_user("member@example.com")
+        self.create_member(organization=self.org, user=member_user, role="member")
+        self.login_as(member_user)
+
+        response = self.client.get(self.url, format="json")
+
+        assert response.status_code == 200, response.content
+        assert response.data == {
+            "app": {"uuid": self.unpublished_app.uuid, "slug": self.unpublished_app.slug},
+            "organization": {"slug": self.org.slug},
+            "uuid": self.installation2.uuid,
+            "status": "pending",
+        }
+
+    def test_access_within_installs_organization_by_manager(self):
+        manager_user = self.create_user("manager@example.com")
+        self.create_member(organization=self.org, user=manager_user, role="manager")
+        self.login_as(manager_user)
+
         response = self.client.get(self.url, format="json")
 
         assert response.status_code == 200, response.content

commit 2c168453e1c2356cf525c73a543e378d660a3087

fix(integrations): restore `code` for sentryapp installation webhooks (#77403)
Fixes https://github.com/getsentry/sentry/issues/76903
The issue was introduced in
https://github.com/getsentry/sentry/pull/71551

src/sentry/api/serializers/models/sentry_app_installation.py
@@ -45,7 +45,9 @@ class SentryAppInstallationSerializer(Serializer):
             "status": SentryAppInstallationStatus.as_str(obj.status),
         }
 
-        if obj.api_grant and access and access.has_scope("org:integrations"):
+        is_webhook = "is_webhook" in kwargs and kwargs["is_webhook"]
+
+        if obj.api_grant and ((access and access.has_scope("org:integrations")) or is_webhook):
             data["code"] = obj.api_grant.code
 
         return data

src/sentry/mediators/sentry_app_installations/installation_notifier.py
@@ -32,9 +32,11 @@ class InstallationNotifier(Mediator):
     @property
     def request(self) -> AppPlatformEvent:
         data = serialize(
-            [self.install], user=self.user, serializer=SentryAppInstallationSerializer()
+            [self.install],
+            user=self.user,
+            serializer=SentryAppInstallationSerializer(),
+            is_webhook=True,
         )[0]
-
         return AppPlatformEvent(
             resource="installation",
             action=self.action,

tests/sentry/mediators/sentry_app_installations/test_installation_notifier.py
@@ -56,6 +56,7 @@ class TestInstallationNotifier(TestCase):
                     "app": {"uuid": self.sentry_app.uuid, "slug": self.sentry_app.slug},
                     "organization": {"slug": self.organization.slug},
                     "uuid": self.install.uuid,
+                    "code": self.install.api_grant.code,
                     "status": "installed",
                 }
             },
@@ -84,6 +85,7 @@ class TestInstallationNotifier(TestCase):
                     "app": {"uuid": self.sentry_app.uuid, "slug": self.sentry_app.slug},
                     "organization": {"slug": self.organization.slug},
                     "uuid": self.install.uuid,
+                    "code": self.install.api_grant.code,
                     "status": "installed",
                 }
             },