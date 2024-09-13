3f0994ffe208e6b4094a88e475006e1652dd9e8b
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-09-13 15:13:18 +0200

    feat(link-utils): Add validate link exists function

---
+++ static/app/utils/url/validateLinkExists.ts
@@ -0,0 +1,23 @@
+import {addErrorMessage} from 'sentry/actionCreators/indicator';
+import {t} from 'sentry/locale';
+import {handleXhrErrorResponse} from 'sentry/utils/handleXhrErrorResponse';
+
+/**
+ * Checks if a URL exists by sending a HEAD request.
+ * @param url - The URL to check.
+ * @returns A promise that resolves to `true` if the URL exists, or `false` otherwise.
+ */
+export async function validateLinkExists(url: string): Promise<boolean> {
+  try {
+    const response = await fetch(url, {method: 'HEAD'}); // Using HEAD method to just check if the resource exists
+    if (response.ok) {
+      return true;
+    }
+    return false;
+  } catch (error) {
+    const message = t('Unable to validate ig the link exists');
+    handleXhrErrorResponse(message, error);
+    addErrorMessage(message);
+    return false;
+  }
+}
