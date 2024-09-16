8ac870ade5769c56df1a16777382f21587744460
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2024-09-16 11:02:07 -0400

    ref(grouping): Remove variants from CalculatedHashes (#77422)
    We can calculate the variants directly from an Event model.
    
    Originally added in #68360.

--- src/sentry/grouping/result.py
+++ src/sentry/grouping/result.py
@@ -1,17 +1,12 @@
-from dataclasses import dataclass, field
+from dataclasses import dataclass
 from typing import Optional
 
 from sentry.db.models import NodeData
-from sentry.grouping.variants import BaseVariant
 
 
 @dataclass(frozen=True)
 class CalculatedHashes:
     hashes: list[str]
-    # `variants` will never be `None` when the `CalculatedHashes` instance is created as part of
-    # event grouping, but it has to be typed including `None` because we use the `CalculatedHashes`
-    # container in other places where we don't have the variants data
-    variants: dict[str, BaseVariant] = field(default_factory=dict)
 
     def write_to_event(self, event_data: NodeData) -> None:
         event_data["hashes"] = self.hashes
