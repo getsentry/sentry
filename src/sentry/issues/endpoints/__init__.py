f1ce571e14ec0123cd04e7107e325093b0304bdc
Matt Duncan <14761+mrduncan@users.noreply.github.com>
2024-09-16 10:25:10 -0700

    chore(issues): Move issues endpoints to issues (#77527)
    These endpoints are all very specific to issues, let's keep them all
    together. These have likely evaded some of our broader monitoring until
    now.

--- src/sentry/issues/endpoints/__init__.py
+++ src/sentry/issues/endpoints/__init__.py
@@ -1,9 +1,12 @@
 from .actionable_items import ActionableItemsEndpoint
 from .group_events import GroupEventsEndpoint
 from .organization_group_index import OrganizationGroupIndexEndpoint
+from .organization_group_index_stats import OrganizationGroupIndexStatsEndpoint
 from .organization_group_search_views import OrganizationGroupSearchViewsEndpoint
 from .organization_release_previous_commits import OrganizationReleasePreviousCommitsEndpoint
 from .organization_searches import OrganizationSearchesEndpoint
+from .project_group_index import ProjectGroupIndexEndpoint
+from .project_group_stats import ProjectGroupStatsEndpoint
 from .project_stacktrace_link import ProjectStacktraceLinkEndpoint
 from .source_map_debug import SourceMapDebugEndpoint
 
@@ -11,9 +14,12 @@ __all__ = (
     "ActionableItemsEndpoint",
     "GroupEventsEndpoint",
     "OrganizationGroupIndexEndpoint",
+    "OrganizationGroupIndexStatsEndpoint",
     "OrganizationGroupSearchViewsEndpoint",
     "OrganizationReleasePreviousCommitsEndpoint",
     "OrganizationSearchesEndpoint",
+    "ProjectGroupIndexEndpoint",
+    "ProjectGroupStatsEndpoint",
     "ProjectStacktraceLinkEndpoint",
     "SourceMapDebugEndpoint",
 )
