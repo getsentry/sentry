5c4b2d931f28c1edb04691dd6cec45824deffa59
Matt Duncan <14761+mrduncan@users.noreply.github.com>
2024-09-16 15:46:40 -0700

    chore(issues): Move a few more issues endpoints (#77594)
    These are issues specific endpoints so let's move them into
    sentry.issues.endpoints from the broad sentry.api.endpoints.

--- src/sentry/issues/endpoints/__init__.py
+++ src/sentry/issues/endpoints/__init__.py
@@ -1,5 +1,9 @@
 from .actionable_items import ActionableItemsEndpoint
+from .group_event_details import GroupEventDetailsEndpoint
 from .group_events import GroupEventsEndpoint
+from .group_hashes import GroupHashesEndpoint
+from .group_similar_issues import GroupSimilarIssuesEndpoint
+from .group_similar_issues_embeddings import GroupSimilarIssuesEmbeddingsEndpoint
 from .organization_group_index import OrganizationGroupIndexEndpoint
 from .organization_group_index_stats import OrganizationGroupIndexStatsEndpoint
 from .organization_group_search_views import OrganizationGroupSearchViewsEndpoint
@@ -8,11 +12,16 @@ from .organization_searches import OrganizationSearchesEndpoint
 from .project_group_index import ProjectGroupIndexEndpoint
 from .project_group_stats import ProjectGroupStatsEndpoint
 from .project_stacktrace_link import ProjectStacktraceLinkEndpoint
+from .shared_group_details import SharedGroupDetailsEndpoint
 from .source_map_debug import SourceMapDebugEndpoint
 
 __all__ = (
     "ActionableItemsEndpoint",
     "GroupEventsEndpoint",
+    "GroupEventDetailsEndpoint",
+    "GroupHashesEndpoint",
+    "GroupSimilarIssuesEndpoint",
+    "GroupSimilarIssuesEmbeddingsEndpoint",
     "OrganizationGroupIndexEndpoint",
     "OrganizationGroupIndexStatsEndpoint",
     "OrganizationGroupSearchViewsEndpoint",
@@ -21,5 +30,6 @@ __all__ = (
     "ProjectGroupIndexEndpoint",
     "ProjectGroupStatsEndpoint",
     "ProjectStacktraceLinkEndpoint",
+    "SharedGroupDetailsEndpoint",
     "SourceMapDebugEndpoint",
 )
