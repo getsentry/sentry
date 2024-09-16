1d1d6efd266cf1b4d234eb8fa8f24bbc86e637c5
Ash <0Calories@users.noreply.github.com>
2024-09-16 18:53:41 -0400

    fix(insights): Bug when sometimes parsing MongoDB queries in summary (#77596)
    Fixes a couple of things:
    
    - Sometimes queries would fail to parse in the span summary of mongoDB
    queries because there wasn't an exhaustive check being applied
    - The fully unparameterized query would get used in the tooltip hover
    due to ordering of the if statements, but we need the collection to be
    unparameterized

--- static/app/views/insights/common/components/fullSpanDescription.tsx
+++ static/app/views/insights/common/components/fullSpanDescription.tsx
@@ -6,7 +6,10 @@ import LoadingIndicator from 'sentry/components/loadingIndicator';
 import {space} from 'sentry/styles/space';
 import {SQLishFormatter} from 'sentry/utils/sqlish/SQLishFormatter';
 import {useFullSpanFromTrace} from 'sentry/views/insights/common/queries/useFullSpanFromTrace';
-import {isValidJson} from 'sentry/views/insights/database/utils/isValidJson';
+import {
+  isValidJson,
+  prettyPrintJsonString,
+} from 'sentry/views/insights/database/utils/jsonUtils';
 import {ModuleName} from 'sentry/views/insights/types';
 
 const formatter = new SQLishFormatter();
@@ -54,13 +57,10 @@ export function FullSpanDescription({
     if (system === 'mongodb') {
       let stringifiedQuery = '';
 
-      if (isValidJson(description)) {
-        stringifiedQuery = prettyPrintJsonString(description);
-      } else if (
-        fullSpan?.sentry_tags &&
-        isValidJson(fullSpan?.sentry_tags?.description)
-      ) {
+      if (fullSpan?.sentry_tags && isValidJson(fullSpan?.sentry_tags?.description)) {
         stringifiedQuery = prettyPrintJsonString(fullSpan?.sentry_tags?.description);
+      } else if (isValidJson(description)) {
+        stringifiedQuery = prettyPrintJsonString(description);
       } else {
         stringifiedQuery = description || fullSpan?.sentry_tags?.description || 'N/A';
       }
@@ -83,10 +83,6 @@ export function FullSpanDescription({
 }
 
 const LINE_LENGTH = 60;
-
-function prettyPrintJsonString(json: string) {
-  return JSON.stringify(JSON.parse(json), null, 4);
-}
 
 const PaddedSpinner = styled('div')`
   padding: 0 ${space(0.5)};
