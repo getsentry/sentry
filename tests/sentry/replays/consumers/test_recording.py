b6843c7fa1e3e8d3cd44224080b315195448a5a1
Colton Allen <colton.allen@sentry.io>
2024-09-17 12:49:38 -0500

    Add coverage for option

--- tests/sentry/replays/consumers/test_recording.py
+++ tests/sentry/replays/consumers/test_recording.py
@@ -12,13 +12,14 @@ from arroyo.backends.kafka import KafkaPayload
 from arroyo.types import BrokerValue, Message, Partition, Topic
 from sentry_kafka_schemas.schema_types.ingest_replay_recordings_v1 import ReplayRecording
 
+from sentry import options
 from sentry.models.organizationonboardingtask import OnboardingTask, OnboardingTaskStatus
 from sentry.replays.consumers.recording import ProcessReplayRecordingStrategyFactory
 from sentry.replays.consumers.recording_buffered import (
     RecordingBufferedStrategyFactory,
     cast_payload_from_bytes,
 )
-from sentry.replays.lib.storage import _make_recording_filename, storage_kv
+from sentry.replays.lib.storage import _make_recording_filename, _make_video_filename, storage_kv
 from sentry.replays.models import ReplayRecordingSegment
 from sentry.replays.usecases.pack import unpack
 from sentry.testutils.cases import TransactionTestCase
@@ -79,16 +80,28 @@ class RecordingTestCase(TransactionTestCase):
             return unpack(zlib.decompress(result))[1]
 
     def get_video_data(self, segment_id):
-        result = storage_kv.get(
-            _make_recording_filename(
-                project_id=self.project.id,
-                replay_id=self.replay_id,
-                segment_id=segment_id,
-                retention_days=30,
+        if self.organization.id in options.get("replay.replay-video.organization-file-packing"):
+            result = storage_kv.get(
+                _make_recording_filename(
+                    project_id=self.project.id,
+                    replay_id=self.replay_id,
+                    segment_id=segment_id,
+                    retention_days=30,
+                )
             )
-        )
-        if result:
-            return unpack(zlib.decompress(result))[0]
+            if result:
+                return unpack(zlib.decompress(result))[0]
+        else:
+            result = storage_kv.get(
+                _make_video_filename(
+                    project_id=self.project.id,
+                    replay_id=self.replay_id,
+                    segment_id=segment_id,
+                    retention_days=30,
+                )
+            )
+            if result:
+                return result
 
     def processing_factory(self):
         return ProcessReplayRecordingStrategyFactory(
@@ -209,6 +222,45 @@ class RecordingTestCase(TransactionTestCase):
         )
 
         assert track_outcome.called
+
+    @patch("sentry.models.OrganizationOnboardingTask.objects.record")
+    @patch("sentry.analytics.record")
+    @patch("sentry.replays.usecases.ingest.track_outcome")
+    def test_event_with_replay_video_packed(self, track_outcome, mock_record, mock_onboarding_task):
+        segment_id = 0
+
+        with self.options(
+            {"replay.replay-video.organization-file-packing": [self.organization.id]}
+        ):
+            self.submit(
+                self.nonchunked_messages(
+                    segment_id=segment_id,
+                    compressed=True,
+                    replay_video=b"hello, world!",
+                )
+            )
+            self.assert_replay_recording_segment(segment_id, compressed=True)
+            assert self.get_video_data(segment_id) == b"hello, world!"
+
+            self.project.refresh_from_db()
+            assert self.project.flags.has_replays
+
+            mock_onboarding_task.assert_called_with(
+                organization_id=self.project.organization_id,
+                task=OnboardingTask.SESSION_REPLAY,
+                status=OnboardingTaskStatus.COMPLETE,
+                date_completed=ANY,
+            )
+
+            mock_record.assert_called_with(
+                "first_replay.sent",
+                organization_id=self.organization.id,
+                project_id=self.project.id,
+                platform=self.project.platform,
+                user_id=self.organization.default_owner_id,
+            )
+
+            assert track_outcome.called
 
     @patch("sentry.models.OrganizationOnboardingTask.objects.record")
     @patch("sentry.analytics.record")
