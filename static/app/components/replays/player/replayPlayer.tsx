getsentry/sentry

commit 2827cdb22dab0eba6212ec47456fe88deb7763d6

fix(replay): New replay foundation for hydration errors (#76927)
Two problems I wanted to fix were:

1. Too many re-renders. With the old system all our replayer state was
in one ginat context, so whenever anything changed (like the currentTime
react would re-calculate everything on the page and see if it needed to
be re-rendered. No longer! This new system splits state into multiple
parts based on stability.
2. The previous <ReplayPlayer> component was doing too much.
Specifically the measuring/scaling code in there was having trouble (the
extra rendering from #1 above didn’t help). This was a root cause for
why hydration errors rendered unreliably. Now we have a new, slimmer
<ReplayPlayerContainment> component to do the measurements and scale the
replay into the webpage. All the other responsibilities will be easily
handled in some future PR.


This PR adds a bunch of react-hooks & providers to split up the state
and make managing the `new Replayer` instances cleaner. This will be
something to continue iterating on over time. For now using it to do
hydration error stuff is a good stepping stone to get the code in and
working in a scenario where the replay doesn't need to play, and doesn't
need to deal with mobile replays.

This rendering fix should solve these:
Fixes https://github.com/getsentry/sentry/issues/76079
Fixes https://github.com/getsentry/sentry/issues/75689
Fixes https://github.com/getsentry/sentry/issues/75073

static/app/components/replays/player/replayPlayer.tsx
@@ -1,0 +1,103 @@
+import {type HTMLAttributes, useEffect, useLayoutEffect, useRef} from 'react';
+import {type Interpolation, type Theme, useTheme} from '@emotion/react';
+import {Replayer} from '@sentry-internal/rrweb';
+
+import {
+  baseReplayerCss,
+  sentryReplayerCss,
+} from 'sentry/components/replays/player/styles';
+import {useReplayPlayerEvents} from 'sentry/utils/replays/playback/providers/replayPlayerEventsContext';
+import {useReplayPlayerPlugins} from 'sentry/utils/replays/playback/providers/replayPlayerPluginsContext';
+import {
+  useReplayPlayerStateDispatch,
+  useReplayUserAction,
+} from 'sentry/utils/replays/playback/providers/replayPlayerStateContext';
+import {useReplayPrefs} from 'sentry/utils/replays/playback/providers/replayPreferencesContext';
+
+function useReplayerInstance() {
+  // The div that is emitted from react, where we will attach the replayer to
+  const mountPointRef = useRef<HTMLDivElement>(null);
+
+  // The single Replayer instance, that is mounted into mountPointRef
+  const replayerRef = useRef<Replayer | null>(null);
+
+  // Collect the info Replayer depends on:
+  const theme = useTheme();
+  const [prefs] = useReplayPrefs();
+  const initialPrefsRef = useRef(prefs); // don't re-mount the player when prefs change, instead there's a useEffect
+  const getPlugins = useReplayPlayerPlugins();
+  const events = useReplayPlayerEvents();
+
+  // Hooks to sync this Replayer state up and out of this component
+  const dispatch = useReplayPlayerStateDispatch();
+  const userAction = useReplayUserAction();
+
+  // useLayoutEffect in order to wait for `mountPointRef.current`
+  useLayoutEffect(() => {
+    const root = mountPointRef.current;
+    if (!root || replayerRef.current) {
+      return () => {};
+    }
+
+    const replayer = new Replayer(events, {
+      root,
+      blockClass: 'sentry-block',
+      mouseTail: {
+        duration: 0.75 * 1000,
+        lineCap: 'round',
+        lineWidth: 2,
+        strokeStyle: theme.purple200,
+      },
+      plugins: getPlugins(events),
+      skipInactive: initialPrefsRef.current.isSkippingInactive,
+      speed: initialPrefsRef.current.playbackSpeed,
+    });
+
+    replayerRef.current = replayer;
+    dispatch({type: 'didMountPlayer', replayer, dispatch});
+    return () => dispatch({type: 'didUnmountPlayer', replayer});
+  }, [dispatch, events, getPlugins, theme]);
+
+  useEffect(() => {
+    if (!replayerRef.current) {
+      return;
+    }
+
+    if (replayerRef.current.config.speed !== prefs.playbackSpeed) {
+      userAction({type: 'setConfigPlaybackSpeed', playbackSpeed: prefs.playbackSpeed});
+    }
+    if (replayerRef.current.config.skipInactive !== prefs.isSkippingInactive) {
+      userAction({
+        type: 'setConfigIsSkippingInactive',
+        isSkippingInactive: prefs.isSkippingInactive,
+      });
+    }
+  }, [prefs, userAction]);
+
+  return mountPointRef;
+}
+
+interface Props extends HTMLAttributes<HTMLDivElement> {
+  css?: Interpolation<Theme>;
+  offsetMs?: undefined | number;
+}
+
+export default function ReplayPlayer({offsetMs, ...props}: Props) {
+  const mountPointRef = useReplayerInstance();
+  const userAction = useReplayUserAction();
+
+  useEffect(() => {
+    if (offsetMs !== undefined) {
+      userAction({type: 'jumpToOffset', offsetMs});
+    }
+  }, [offsetMs, userAction]);
+
+  return (
+    <div
+      {...props}
+      data-inspectable={props['data-inspectable']}
+      css={[baseReplayerCss, sentryReplayerCss, props.css]}
+      ref={mountPointRef}
+    />
+  );
+}

static/app/components/replays/player/styles.tsx
@@ -18,7 +18,9 @@ export const baseReplayerCss = css`
   .replayer-wrapper > iframe {
     border: none;
     background: white;
+  }
 
+  &[data-inspectable='true'] .replayer-wrapper > iframe {
     /* Set pointer-events to make it easier to right-click & inspect */
     pointer-events: initial !important;
   }

static/app/components/replays/diff/replaySliderDiff.tsx
@@ -2,13 +2,16 @@ import {Fragment, useCallback, useRef} from 'react';
 import styled from '@emotion/styled';
 
 import NegativeSpaceContainer from 'sentry/components/container/negativeSpaceContainer';
-import ReplayIFrameRoot from 'sentry/components/replays/player/replayIFrameRoot';
-import {Provider as ReplayContextProvider} from 'sentry/components/replays/replayContext';
+import ReplayPlayer from 'sentry/components/replays/player/replayPlayer';
+import ReplayPlayerMeasurer from 'sentry/components/replays/player/replayPlayerMeasurer';
 import {Tooltip} from 'sentry/components/tooltip';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {trackAnalytics} from 'sentry/utils/analytics';
 import toPixels from 'sentry/utils/number/toPixels';
+import {ReplayPlayerEventsContextProvider} from 'sentry/utils/replays/playback/providers/replayPlayerEventsContext';
+import {ReplayPlayerPluginsContextProvider} from 'sentry/utils/replays/playback/providers/replayPlayerPluginsContext';
+import {ReplayPlayerStateContextProvider} from 'sentry/utils/replays/playback/providers/replayPlayerStateContext';
 import type ReplayReader from 'sentry/utils/replays/replayReader';
 import {useDimensions} from 'sentry/utils/useDimensions';
 import useOrganization from 'sentry/utils/useOrganization';
@@ -99,30 +102,32 @@ function DiffSides({leftOffsetMs, replay, rightOffsetMs, viewDimensions, width})
 
   return (
     <Fragment>
-      <Cover style={{width}}>
-        <Placement style={{width}}>
-          <ReplayContextProvider
-            analyticsContext="replay_comparison_modal_left"
-            initialTimeOffsetMs={{offsetMs: leftOffsetMs}}
-            isFetching={false}
-            replay={replay}
-          >
-            <ReplayIFrameRoot viewDimensions={viewDimensions} />
-          </ReplayContextProvider>
-        </Placement>
-      </Cover>
-      <Cover ref={rightSideElem} style={{width: 0}}>
-        <Placement style={{width}}>
-          <ReplayContextProvider
-            analyticsContext="replay_comparison_modal_right"
-            initialTimeOffsetMs={{offsetMs: rightOffsetMs}}
-            isFetching={false}
-            replay={replay}
-          >
-            <ReplayIFrameRoot viewDimensions={viewDimensions} />
-          </ReplayContextProvider>
-        </Placement>
-      </Cover>
+      <ReplayPlayerPluginsContextProvider>
+        <ReplayPlayerEventsContextProvider replay={replay}>
+          <Cover style={{width}}>
+            <Placement style={{width}}>
+              <ReplayPlayerStateContextProvider>
+                <NegativeSpaceContainer>
+                  <ReplayPlayerMeasurer measure="width">
+                    {style => <ReplayPlayer style={style} offsetMs={leftOffsetMs} />}
+                  </ReplayPlayerMeasurer>
+                </NegativeSpaceContainer>
+              </ReplayPlayerStateContextProvider>
+            </Placement>
+          </Cover>
+          <Cover ref={rightSideElem} style={{width: 0}}>
+            <Placement style={{width}}>
+              <ReplayPlayerStateContextProvider>
+                <NegativeSpaceContainer>
+                  <ReplayPlayerMeasurer measure="width">
+                    {style => <ReplayPlayer style={style} offsetMs={rightOffsetMs} />}
+                  </ReplayPlayerMeasurer>
+                </NegativeSpaceContainer>
+              </ReplayPlayerStateContextProvider>
+            </Placement>
+          </Cover>
+        </ReplayPlayerEventsContextProvider>
+      </ReplayPlayerPluginsContextProvider>
       <Divider ref={dividerElem} onMouseDown={onDividerMouseDownWithAnalytics} />
     </Fragment>
   );

static/app/components/replays/player/__stories__/jumpToOffsetButtonBar.tsx
@@ -1,0 +1,34 @@
+import {Button} from 'sentry/components/button';
+import ButtonBar from 'sentry/components/buttonBar';
+import formatDuration from 'sentry/utils/duration/formatDuration';
+import {intervalToMilliseconds} from 'sentry/utils/duration/intervalToMilliseconds';
+import {useReplayUserAction} from 'sentry/utils/replays/playback/providers/replayPlayerStateContext';
+
+interface Props {
+  intervals: string[];
+}
+
+export default function JumpToOffsetButtonBar({intervals}: Props) {
+  const userAction = useReplayUserAction();
+
+  return (
+    <ButtonBar merged>
+      {intervals.map(interval => {
+        const intervalMs = intervalToMilliseconds(interval);
+        return (
+          <Button
+            key={interval}
+            onClick={() => userAction({type: 'jumpToOffset', offsetMs: intervalMs})}
+            size="sm"
+          >
+            {formatDuration({
+              duration: [intervalMs, 'ms'],
+              style: 'h:mm:ss.sss',
+              precision: 'ms',
+            })}
+          </Button>
+        );
+      })}
+    </ButtonBar>
+  );
+}

static/app/components/replays/player/__stories__/providers.tsx
@@ -1,0 +1,26 @@
+import type {ReactNode} from 'react';
+
+import {StaticNoSkipReplayPreferences} from 'sentry/components/replays/preferences/replayPreferences';
+import {ReplayPlayerEventsContextProvider} from 'sentry/utils/replays/playback/providers/replayPlayerEventsContext';
+import {ReplayPlayerPluginsContextProvider} from 'sentry/utils/replays/playback/providers/replayPlayerPluginsContext';
+import {ReplayPlayerStateContextProvider} from 'sentry/utils/replays/playback/providers/replayPlayerStateContext';
+import {ReplayPreferencesContextProvider} from 'sentry/utils/replays/playback/providers/replayPreferencesContext';
+import type ReplayReader from 'sentry/utils/replays/replayReader';
+
+export default function Providers({
+  children,
+  replay,
+}: {
+  children: ReactNode;
+  replay: ReplayReader;
+}) {
+  return (
+    <ReplayPreferencesContextProvider prefsStrategy={StaticNoSkipReplayPreferences}>
+      <ReplayPlayerPluginsContextProvider>
+        <ReplayPlayerEventsContextProvider replay={replay}>
+          <ReplayPlayerStateContextProvider>{children}</ReplayPlayerStateContextProvider>
+        </ReplayPlayerEventsContextProvider>
+      </ReplayPlayerPluginsContextProvider>
+    </ReplayPreferencesContextProvider>
+  );
+}

static/app/components/replays/player/__stories__/replaySlugChooser.tsx
@@ -1,0 +1,45 @@
+import {Fragment, type ReactNode} from 'react';
+import {css} from '@emotion/react';
+
+import Providers from 'sentry/components/replays/player/__stories__/providers';
+import useReplayReader from 'sentry/utils/replays/hooks/useReplayReader';
+import useOrganization from 'sentry/utils/useOrganization';
+import {useSessionStorage} from 'sentry/utils/useSessionStorage';
+
+export default function ReplaySlugChooser({children}: {children: ReactNode}) {
+  const [replaySlug, setReplaySlug] = useSessionStorage('stories:replaySlug', '');
+
+  return (
+    <Fragment>
+      <input
+        defaultValue={replaySlug}
+        onChange={event => {
+          setReplaySlug(event.target.value);
+        }}
+        placeholder="Paste a replaySlug"
+        css={css`
+          font-variant-numeric: tabular-nums;
+        `}
+        size={34}
+      />
+      {replaySlug ? <LoadReplay replaySlug={replaySlug}>{children}</LoadReplay> : null}
+    </Fragment>
+  );
+}
+
+function LoadReplay({children, replaySlug}: {children: ReactNode; replaySlug: string}) {
+  const organization = useOrganization();
+  const {fetchError, fetching, replay} = useReplayReader({
+    orgSlug: organization.slug,
+    replaySlug,
+  });
+
+  if (fetchError) {
+    return fetchError.message;
+  }
+  if (!replay || fetching) {
+    return 'Loading...';
+  }
+
+  return <Providers replay={replay}>{children}</Providers>;
+}

static/app/components/replays/player/replayCurrentTime.stories.tsx
@@ -1,0 +1,55 @@
+import NegativeSpaceContainer from 'sentry/components/container/negativeSpaceContainer';
+import JumpToOffsetButtonBar from 'sentry/components/replays/player/__stories__/jumpToOffsetButtonBar';
+import ReplaySlugChooser from 'sentry/components/replays/player/__stories__/replaySlugChooser';
+import ReplayCurrentTime from 'sentry/components/replays/player/replayCurrentTime';
+import ReplayPlayer from 'sentry/components/replays/player/replayPlayer';
+import ReplayPlayerMeasurer from 'sentry/components/replays/player/replayPlayerMeasurer';
+import ReplayPlayPauseButton from 'sentry/components/replays/player/replayPlayPauseButton';
+import SideBySide from 'sentry/components/stories/sideBySide';
+import storyBook from 'sentry/stories/storyBook';
+
+export default storyBook(ReplayCurrentTime, story => {
+  story('Default', () => {
+    function Example() {
+      return (
+        <SideBySide>
+          <ReplayPlayPauseButton />
+          <ReplayCurrentTime />
+          <NegativeSpaceContainer style={{height: 300}}>
+            <ReplayPlayerMeasurer measure="both">
+              {style => <ReplayPlayer style={style} />}
+            </ReplayPlayerMeasurer>
+          </NegativeSpaceContainer>
+        </SideBySide>
+      );
+    }
+    return (
+      <ReplaySlugChooser>
+        <Example />
+      </ReplaySlugChooser>
+    );
+  });
+
+  story('Jumping to different times', () => {
+    function Example() {
+      return (
+        <SideBySide>
+          <ReplayPlayPauseButton />
+          <ReplayCurrentTime />
+          <JumpToOffsetButtonBar intervals={['0m', '1m', '12m']} />
+
+          <NegativeSpaceContainer style={{height: 300}}>
+            <ReplayPlayerMeasurer measure="both">
+              {style => <ReplayPlayer style={style} />}
+            </ReplayPlayerMeasurer>
+          </NegativeSpaceContainer>
+        </SideBySide>
+      );
+    }
+    return (
+      <ReplaySlugChooser>
+        <Example />
+      </ReplaySlugChooser>
+    );
+  });
+});

static/app/components/replays/player/replayCurrentTime.tsx
@@ -1,0 +1,12 @@
+import {useState} from 'react';
+
+import Duration from 'sentry/components/duration/duration';
+import useReplayCurrentTime from 'sentry/utils/replays/playback/hooks/useReplayCurrentTime';
+
+export default function ReplayCurrentTime() {
+  const [currentTime, setCurrentTime] = useState({timeMs: 0});
+
+  useReplayCurrentTime({callback: setCurrentTime});
+
+  return <Duration duration={[currentTime.timeMs, 'ms']} precision="sec" />;
+}

commit 9213fd5ce1c001bca7c9ffb42a42d8bbc05a6c92

fix(replay): Allow replay player instances to be interactable/scrollable again (#77647)
Broke in
https://github.com/getsentry/sentry/pull/76927/files#diff-e10692ee093e56669e267837b07b16ed1d800a6dc996049aea27186f5fe0b8bbR21
because that code is used by:
https://github.com/getsentry/sentry/blame/8b1891e/static/app/components/replays/replayPlayer.tsx#L185-L193

Fixes https://github.com/getsentry/sentry/issues/77608#issue-2529675927


1. What I've done is make all the replay views "inspectable" by allowing
pointer-events. So links can be clicked inside the replay... but also
doing "Right-Click -> Inspect Element" works great again.

2. The attribute `scrolling="no"` is set, and from what I can tell was
always set on the iframe. So i'm not sure how or when that behavior
changed.

static/app/components/replays/player/replayPlayer.tsx
@@ -95,7 +95,6 @@ export default function ReplayPlayer({offsetMs, ...props}: Props) {
   return (
     <div
       {...props}
-      data-inspectable={props['data-inspectable']}
       css={[baseReplayerCss, sentryReplayerCss, props.css]}
       ref={mountPointRef}
     />

static/app/components/replays/player/styles.tsx
@@ -18,9 +18,7 @@ export const baseReplayerCss = css`
   .replayer-wrapper > iframe {
     border: none;
     background: white;
-  }
 
-  &[data-inspectable='true'] .replayer-wrapper > iframe {
     /* Set pointer-events to make it easier to right-click & inspect */
     pointer-events: initial !important;
   }

static/app/components/replays/replayPlayer.tsx
@@ -182,13 +182,10 @@ const PositionedLoadingIndicator = styled(LoadingIndicator)`
   position: absolute;
 `;
 
-// Base styles, to make the Replayer instance work
-const PlayerRoot = styled(BasePlayerRoot)`
+const SentryPlayerRoot = styled(BasePlayerRoot)`
+  /* Base styles, to make the Replayer instance work */
   ${baseReplayerCss}
-`;
-
-// Sentry-specific styles for the player.
-const SentryPlayerRoot = styled(PlayerRoot)`
+  /* Sentry-specific styles for the player */
   ${p => sentryReplayerCss(p.theme)}
 `;
 