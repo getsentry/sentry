d9706f44f2f1dee6e4a7f445da59ac558b1deeea
Christinarlong <christina.long@sentry.io>
2024-09-18 16:09:47 -0700

    add to barrel file

--- src/sentry/sentry_apps/models/__init__.py
+++ src/sentry/sentry_apps/models/__init__.py
@@ -1,4 +1,5 @@
 from .sentry_app import SentryApp
+from .sentry_app_component import SentryAppComponent
 from .sentry_app_installation import SentryAppInstallation
 
-__all__ = ("SentryApp", "SentryAppInstallation")
+__all__ = ("SentryApp", "SentryAppInstallation", "SentryAppComponent")
