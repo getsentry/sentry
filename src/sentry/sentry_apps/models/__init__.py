7f64faac4954d33f2c9dfe4ac6cc70396087459f
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2024-09-19 08:50:11 -0700

    ref(sentry_apps): Move SentryAppInstallationToken to sentry_apps (#77734)
    issue ref(#73857)

--- src/sentry/sentry_apps/models/__init__.py
+++ src/sentry/sentry_apps/models/__init__.py
@@ -1,4 +1,5 @@
 from .sentry_app import SentryApp
 from .sentry_app_installation import SentryAppInstallation
+from .sentry_app_installation_token import SentryAppInstallationToken
 
-__all__ = ("SentryApp", "SentryAppInstallation")
+__all__ = ("SentryApp", "SentryAppInstallationToken", "SentryAppInstallation")
