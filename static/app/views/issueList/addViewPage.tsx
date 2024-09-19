d69395b81b9af8e84caa85e7c5883e136ae70943
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2024-09-19 08:52:03 -0700

    feat(issue-views): Add better recommended searches (#77758)
    Makes the recommended searches not garbage.

--- static/app/views/issueList/addViewPage.tsx
+++ static/app/views/issueList/addViewPage.tsx
@@ -26,13 +26,16 @@ interface SearchSuggestionListProps {
 }
 
 const RECOMMENDED_SEARCHES: SearchSuggestion[] = [
-  {label: 'Assigned to Me', query: 'assigned:me'},
-  {label: 'My Bookmarks', query: 'bookmarks:me'},
-  {label: 'Errors Only', query: 'status:unresolved level:error'},
+  {label: 'Prioritized', query: 'is:unresolved issue.priority:[high, medium]'},
+  {label: 'Assigned to Me', query: 'is:unresolved assigned_or_suggested:me'},
   {
-    label: 'Unhandled',
-    query: 'status:unresolved error.unhandled:True',
+    label: 'For Review',
+    query: 'is:unresolved is:for_review assigned_or_suggested:[me, my_teams, none]',
   },
+  {label: 'Request Errors', query: 'is:unresolved http.status_code:5*'},
+  {label: 'High Volume Issues', query: 'is:unresolved timesSeen:>100'},
+  {label: 'Recent Errors', query: 'is:unresolved issue.category:error firstSeen:-24h'},
+  {label: 'Function Regressions', query: 'issue.type:profile_function_regression'},
 ];
 
 function AddViewPage({savedSearches}: {savedSearches: SavedSearch[]}) {
