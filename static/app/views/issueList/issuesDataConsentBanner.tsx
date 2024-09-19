54f98f86e7e69a64158ad5e7270ebfc451403fed
MichaelSun48 <michael.sun@sentry.io>
2024-09-19 13:23:56 -0700

    Remove dataconsentbanner outside of component

--- static/app/views/issueList/issuesDataConsentBanner.tsx
+++ static/app/views/issueList/issuesDataConsentBanner.tsx
@@ -3,12 +3,12 @@ import {useContext} from 'react';
 import HookOrDefault from 'sentry/components/hookOrDefault';
 import {NewTabContext} from 'sentry/views/issueList/utils/newTabContext';
 
-export function IssuesDataConsentBanner({source}: {source: string}) {
-  const DataConsentBanner = HookOrDefault({
-    hookName: 'component:data-consent-banner',
-    defaultComponent: null,
-  });
+const DataConsentBanner = HookOrDefault({
+  hookName: 'component:data-consent-banner',
+  defaultComponent: null,
+});
 
+export function IssuesDataConsentBanner({source}: {source: string}) {
   const {newViewActive} = useContext(NewTabContext);
 
   return newViewActive ? null : <DataConsentBanner source={source} />;
