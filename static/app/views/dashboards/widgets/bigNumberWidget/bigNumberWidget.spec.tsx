33ac24f5e3f6cdfaa6c809d646a7ab34ec41e9ae
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-09-19 15:51:55 -0400

    Split up the spec by feature

--- static/app/views/dashboards/widgets/bigNumberWidget/bigNumberWidget.spec.tsx
+++ static/app/views/dashboards/widgets/bigNumberWidget/bigNumberWidget.spec.tsx
@@ -3,29 +3,55 @@ import {render, screen} from 'sentry-test/reactTestingLibrary';
 import {BigNumberWidget} from 'sentry/views/dashboards/widgets/bigNumberWidget/bigNumberWidget';
 
 describe('BigNumberWidget', () => {
-  it('Renders formatted data', () => {
-    render(
-      <BigNumberWidget
-        title="EPS"
-        description="Number of events per second"
-        data={[
-          {
-            'eps()': 0.01087819860850493,
-          },
-        ]}
-        meta={{
-          fields: {
-            'eps()': 'rate',
-          },
-          units: {
-            'eps()': '1/second',
-          },
-        }}
-      />
-    );
+  describe('Layout', () => {
+    it('Renders', () => {
+      render(
+        <BigNumberWidget
+          title="EPS"
+          description="Number of events per second"
+          data={[
+            {
+              'eps()': 0.01087819860850493,
+            },
+          ]}
+          meta={{
+            fields: {
+              'eps()': 'rate',
+            },
+            units: {
+              'eps()': '1/second',
+            },
+          }}
+        />
+      );
 
-    expect(screen.getByText('EPS')).toBeInTheDocument();
-    expect(screen.getByText('Number of events per second')).toBeInTheDocument();
-    expect(screen.getByText('0.0109/s')).toBeInTheDocument();
+      expect(screen.getByText('EPS')).toBeInTheDocument();
+      expect(screen.getByText('Number of events per second')).toBeInTheDocument();
+      expect(screen.getByText('0.0109/s')).toBeInTheDocument();
+    });
+  });
+
+  describe('Visualization', () => {
+    it('Formats duration data', () => {
+      render(
+        <BigNumberWidget
+          data={[
+            {
+              'p95(span.duration)': 17.28,
+            },
+          ]}
+          meta={{
+            fields: {
+              'p95(span.duration)': 'duration',
+            },
+            units: {
+              'p95(spa.duration)': 'milliseconds',
+            },
+          }}
+        />
+      );
+
+      expect(screen.getByText('17.28ms')).toBeInTheDocument();
+    });
   });
 });
