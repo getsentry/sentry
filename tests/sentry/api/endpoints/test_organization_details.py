getsentry/sentry

commit 375ca007e6ddecb197be7ad42e2f749e1fbfaa8c

ref(hybrid-cloud): Add authenticators to API response for users.  (#44219)
This allows us to avoid additional cross silo requests to check if
authenticators exist, which happens on nigh every request for users in
organizations that require 2fa.

---------

Co-authored-by: Mike Ihbe <mike.ihbe@sentry.io>
Co-authored-by: Zach Collins <recursive.cookie.jar@gmail.com>
Co-authored-by: Zachary Collins <zachary.collins@sentry.io>
Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

tests/sentry/api/endpoints/test_organization_details.py
@@ -336,7 +336,7 @@ class OrganizationUpdateTest(OrganizationDetailsTestBase):
         # needed to set require2FA
         interface = TotpInterface()
         interface.enroll(self.user)
-        assert Authenticator.objects.user_has_2fa(self.user)
+        assert self.user.has_2fa()
 
         self.get_success_response(self.organization.slug, **data)
 
@@ -873,14 +873,14 @@ class OrganizationSettings2FATest(TwoFactorAPITestCase):
         self.has_2fa = self.create_user()
         TotpInterface().enroll(self.has_2fa)
         self.create_member(organization=self.organization, user=self.has_2fa, role="manager")
-        assert Authenticator.objects.user_has_2fa(self.has_2fa)
+        assert self.has_2fa.has_2fa()
 
     def assert_2fa_email_equal(self, outbox, expected):
         assert len(outbox) == len(expected)
         assert sorted(email.to[0] for email in outbox) == sorted(expected)
 
     def test_cannot_enforce_2fa_without_2fa_enabled(self):
-        assert not Authenticator.objects.user_has_2fa(self.owner)
+        assert not self.owner.has_2fa()
         self.assert_cannot_enable_org_2fa(self.organization, self.owner, 400, ERR_NO_2FA)
 
     def test_cannot_enforce_2fa_with_sso_enabled(self):

src/sentry/api/bases/organization.py
@@ -19,14 +19,7 @@ from sentry.api.utils import (
 )
 from sentry.auth.superuser import is_active_superuser
 from sentry.constants import ALL_ACCESS_PROJECTS, ALL_ACCESS_PROJECTS_SLUG
-from sentry.models import (
-    ApiKey,
-    Authenticator,
-    Organization,
-    Project,
-    ProjectStatus,
-    ReleaseProject,
-)
+from sentry.models import ApiKey, Organization, Project, ProjectStatus, ReleaseProject
 from sentry.models.environment import Environment
 from sentry.models.release import Release
 from sentry.utils import auth
@@ -50,7 +43,7 @@ class OrganizationPermission(SentryPermission):
     def is_not_2fa_compliant(self, request: Request, organization: Organization) -> bool:
         return (
             organization.flags.require_2fa
-            and not Authenticator.objects.user_has_2fa(request.user)
+            and not request.user.has_2fa()
             and not is_active_superuser(request)
         )
 

src/sentry/api/endpoints/auth_index.py
@@ -160,7 +160,7 @@ class AuthIndexEndpoint(Endpoint):
 
         # If 2fa login is enabled then we cannot sign in with username and
         # password through this api endpoint.
-        if Authenticator.objects.user_has_2fa(request.user):
+        if request.user.has_2fa():
             return Response(
                 {
                     "2fa_required": True,

src/sentry/api/endpoints/organization_details.py
@@ -31,7 +31,6 @@ from sentry.lang.native.utils import (
     convert_crashreport_count,
 )
 from sentry.models import (
-    Authenticator,
     AuthProvider,
     Organization,
     OrganizationAvatar,
@@ -210,7 +209,7 @@ class OrganizationSerializer(BaseOrganizationSerializer):
 
     def validate_require2FA(self, value):
         user = self.context["user"]
-        has_2fa = Authenticator.objects.user_has_2fa(user)
+        has_2fa = user.has_2fa()
         if value and not has_2fa:
             raise serializers.ValidationError(ERR_NO_2FA)
 

src/sentry/api/invite_helper.py
@@ -8,7 +8,6 @@ from rest_framework.request import Request
 
 from sentry import audit_log, features
 from sentry.models import (
-    Authenticator,
     AuthIdentity,
     AuthProvider,
     Organization,
@@ -225,8 +224,9 @@ class ApiInviteHelper:
 
     def _needs_2fa(self) -> bool:
         org_requires_2fa = self.organization.flags.require_2fa.is_set
-        user_has_2fa = Authenticator.objects.user_has_2fa(self.request.user.id)
-        return org_requires_2fa and not user_has_2fa
+        return org_requires_2fa and (
+            not self.request.user.is_authenticated or not self.request.user.has_2fa()
+        )
 
     def _needs_email_verification(self) -> bool:
         organization = self.organization

src/sentry/models/authenticator.py
@@ -98,12 +98,6 @@ class AuthenticatorManager(BaseManager):
         except Authenticator.DoesNotExist:
             return interface.generate(EnrollmentStatus.NEW)
 
-    def user_has_2fa(self, user_id):
-        """Checks if the user has any 2FA configured."""
-        return Authenticator.objects.filter(
-            user_id=user_id, type__in=[a.type for a in available_authenticators(ignore_backup=True)]
-        ).exists()
-
     def bulk_users_have_2fa(self, user_ids):
         """Checks if a list of user ids have 2FA configured.
         Returns a dict of {<id>: <has_2fa>}

src/sentry/models/user.py
@@ -14,6 +14,7 @@ from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
 from bitfield import BitField
+from sentry.auth.authenticators import available_authenticators
 from sentry.db.models import (
     BaseManager,
     BaseModel,
@@ -24,6 +25,7 @@ from sentry.db.models import (
 )
 from sentry.db.postgres.roles import test_psql_role_override
 from sentry.models import LostPasswordHash
+from sentry.models.authenticator import Authenticator
 from sentry.models.outbox import ControlOutbox, OutboxCategory, OutboxScope, find_regions_for_user
 from sentry.services.hybrid_cloud.user import RpcUser
 from sentry.types.integrations import EXTERNAL_PROVIDERS, ExternalProviders
@@ -218,6 +220,11 @@ class User(BaseModel, AbstractBaseUser):
     def has_module_perms(self, app_label):
         warnings.warn("User.has_module_perms is deprecated", DeprecationWarning)
         return self.is_superuser
+
+    def has_2fa(self):
+        return Authenticator.objects.filter(
+            user_id=self.id, type__in=[a.type for a in available_authenticators(ignore_backup=True)]
+        ).exists()
 
     def get_unverified_emails(self):
         return self.emails.filter(is_verified=False)

src/sentry/services/hybrid_cloud/user/__init__.py
@@ -4,7 +4,7 @@ import datetime
 from abc import abstractmethod
 from dataclasses import dataclass
 from enum import IntEnum
-from typing import TYPE_CHECKING, FrozenSet, List, Optional, TypedDict
+from typing import TYPE_CHECKING, Any, FrozenSet, List, Optional, TypedDict
 
 from sentry.services.hybrid_cloud import InterfaceWithLifecycle, silo_mode_delegation, stubbed
 from sentry.services.hybrid_cloud.filter_query import FilterQueryInterface
@@ -40,6 +40,7 @@ class RpcUser:
     permissions: FrozenSet[str] = frozenset()
     avatar: Optional[RpcAvatar] = None
     useremails: FrozenSet[RpcUserEmail] = frozenset()
+    authenticators: FrozenSet[RpcAuthenticator] = frozenset()
 
     def has_usable_password(self) -> bool:
         return self.password_usable
@@ -64,6 +65,9 @@ class RpcUser:
     def class_name(self) -> str:
         return "User"
 
+    def has_2fa(self) -> bool:
+        return len(self.authenticators) > 0
+
 
 @dataclass(frozen=True, eq=True)
 class RpcAvatar:
@@ -78,6 +82,16 @@ class RpcUserEmail:
     id: int = 0
     email: str = ""
     is_verified: bool = False
+
+
+@dataclass(frozen=True, eq=True)
+class RpcAuthenticator:
+    id: int = 0
+    user_id: int = -1
+    created_at: datetime.datetime = datetime.datetime(2000, 1, 1)
+    last_used_at: datetime.datetime = datetime.datetime(2000, 1, 1)
+    type: int = -1
+    config: Any = None
 
 
 class UserSerializeType(IntEnum):  # annoying

commit 2800dcea8e2c674443b770e7b48256d49b250799

fix(2fa): do not allow to enforce 2FA when having recovery codes only (#77798)
Currently, when an owner has only recovery codes (backup interface),
this still allows them to enforce 2FA for the organization what is
wrong.

The requirement of having a proper 2FA method was implemented in
https://github.com/getsentry/sentry/pull/6812 but it was broken during
HC changes in https://github.com/getsentry/sentry/pull/44219.

The `has_2fa()` method has a different semantic in the regional silo:


https://github.com/getsentry/sentry/blob/054228300533f6b700fe6f4018ef6796d887ea58/src/sentry/users/services/user/model.py#L124-L125

vs control silo:

https://github.com/getsentry/sentry/blob/054228300533f6b700fe6f4018ef6796d887ea58/src/sentry/users/models/user.py#L239-L242

src/sentry/users/services/user/model.py
@@ -122,7 +122,7 @@ class RpcUser(RpcUserProfile):
         return "User"
 
     def has_2fa(self) -> bool:
-        return len(self.authenticators) > 0
+        return any(a.type != 0 for a in self.authenticators)
 
 
 class UserCreateResult(RpcModel):

tests/sentry/api/endpoints/test_organization_details.py
@@ -20,6 +20,7 @@ from sentry import options as sentry_options
 from sentry.api.endpoints.organization_details import ERR_NO_2FA, ERR_SSO_ENABLED
 from sentry.api.serializers.models.organization import TrustedRelaySerializer
 from sentry.api.utils import generate_region_url
+from sentry.auth.authenticators.recovery_code import RecoveryCodeInterface
 from sentry.auth.authenticators.totp import TotpInterface
 from sentry.constants import RESERVED_ORGANIZATION_SLUGS, ObjectStatus
 from sentry.models.auditlogentry import AuditLogEntry
@@ -1114,6 +1115,12 @@ class OrganizationSettings2FATest(TwoFactorAPITestCase):
 
     def test_cannot_enforce_2fa_without_2fa_enabled(self):
         with assume_test_silo_mode_of(Authenticator):
+            assert not self.owner.has_2fa()
+        self.assert_cannot_enable_org_2fa(self.organization, self.owner, 400, ERR_NO_2FA)
+
+        # having recovery codes only (backup method) should not allow to enforce org 2FA
+        with assume_test_silo_mode_of(Authenticator):
+            RecoveryCodeInterface().enroll(self.owner)
             assert not self.owner.has_2fa()
         self.assert_cannot_enable_org_2fa(self.organization, self.owner, 400, ERR_NO_2FA)
 