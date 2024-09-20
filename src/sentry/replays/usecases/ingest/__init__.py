adeebadee66a63bdb612e47f1be8e1b3b358ac0f
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-20 13:32:00 -0700

    Rm select related

--- src/sentry/replays/usecases/ingest/__init__.py
+++ src/sentry/replays/usecases/ingest/__init__.py
@@ -262,7 +262,7 @@ def recording_post_processor(
             op="replays.usecases.ingest.parse_and_emit_replay_actions",
             description="parse_and_emit_replay_actions",
         ):
-            project = Project.objects.select_related("organization").get_from_cache(
+            project = Project.objects.get_from_cache(
                 id=message.project_id
             )
             parse_and_emit_replay_actions(
