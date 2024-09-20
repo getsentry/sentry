1e566cb8cd3cb7af4cfe1283697393cc44bb266a
Michelle Fu <michelle.fu@sentry.io>
2024-09-20 10:36:24 -0700

    replace config with typed config dict

--- tests/sentry/api/endpoints/test_organization_historical_anomalies.py
+++ tests/sentry/api/endpoints/test_organization_historical_anomalies.py
@@ -5,6 +5,7 @@ from sentry.incidents.models.alert_rule import (
     AlertRuleSensitivity,
     AlertRuleThresholdType,
 )
+from sentry.seer.anomaly_detection.types import AnomalyDetectionConfig
 from sentry.testutils.cases import APITestCase
 from sentry.testutils.helpers.features import with_feature
 from sentry.testutils.outbox import outbox_runner
@@ -20,15 +21,15 @@ class OrganizationEventsAnomaliesEndpointTest(APITestCase):
     def test_simple(self):
         self.create_team(organization=self.organization, members=[self.user])
         self.login_as(self.user)
-
+        config = AnomalyDetectionConfig(
+            time_period=60,
+            sensitivity=AlertRuleSensitivity.LOW,
+            direction=AlertRuleThresholdType.ABOVE,
+            expected_seasonality=AlertRuleSeasonality.AUTO,
+        )
         data = {
             "project_id": 1,
-            "config": {
-                "time_period": 60,
-                "sensitivity": AlertRuleSensitivity.LOW,
-                "direction": AlertRuleThresholdType.ABOVE,
-                "expected_seasonality": AlertRuleSeasonality.AUTO,
-            },
+            "config": config,
             "current_data": [[1, {"count": 0.077881957}], [2, {"count": 0.075652768}]],
             "historical_data": [[169, {"count": 0.048480431}], [170, {"count": 0.047910238}]],
         }
