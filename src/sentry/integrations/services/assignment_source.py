94578b82e531471def0b19292ca919519abdb147
Gabe Villalobos <gabriel.villalobos@sentry.io>
2024-09-20 16:48:56 -0700

    removes SourceType enum

--- src/sentry/integrations/services/assignment_source.py
+++ src/sentry/integrations/services/assignment_source.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from dataclasses import asdict, dataclass
 from datetime import datetime
-from enum import Enum
 from typing import TYPE_CHECKING
 
 from django.utils import timezone
@@ -14,13 +13,8 @@ if TYPE_CHECKING:
     from sentry.integrations.services.integration import RpcIntegration
 
 
-class AssignmentSourceType(str, Enum):
-    integration = "integration"
-
-
 @dataclass(frozen=True)
 class AssignmentSource:
-    source_type: AssignmentSourceType
     source_name: str
     integration_id: int
     queued: datetime = timezone.now()
@@ -28,7 +22,6 @@ class AssignmentSource:
     @classmethod
     def from_integration(cls, integration: Integration | RpcIntegration) -> AssignmentSource:
         return AssignmentSource(
-            source_type=AssignmentSourceType.integration,
             source_name=integration.name,
             integration_id=integration.id,
         )
