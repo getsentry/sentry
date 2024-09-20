c01006b6a59734450e83ae9de789fa4206784053
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-20 12:49:50 -0700

    Tweak basic test

--- tests/sentry/toolbar/utils/test_url.py
+++ tests/sentry/toolbar/utils/test_url.py
@@ -3,7 +3,6 @@ from sentry.toolbar.utils.url import url_matches
 """
 TODO:
 Hostname
-- no match
 - wildcard (y/n, n for startswith case)
 
 Port
@@ -17,15 +16,15 @@ Scheme
 
 
 def test_url_matches_basic():
-    cases = [
+    test_cases = [
         ("http://abc.net", "http://abc.net", True),
-        ("http://pocketpair.com", "http://sentry.io", False),
+        ("http://pocketpair.io", "http://sentry.io", False),
         ("http://sentry.io/issues", "http://sentry.io", True),
         ("http://sentry.io", "http://sentry.io/issues", True),
         ("https://cmu.edu", "https://cmu.com", False),
         ("https://youtube.com:443/watch?v=3xhb", "https://youtube.com/profile", True),
     ]
-    for url, target_url, expected in cases:
+    for url, target_url, expected in test_cases:
         assert url_matches(url, target_url) == expected
 
 
