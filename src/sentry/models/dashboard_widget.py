99dce1905900b0edce3d08ba99b050fd62e0e0f1
harshithadurai <harshi.durai@esentry.io>
2024-09-23 14:14:06 -0400

    add selected aggregate field

--- src/sentry/models/dashboard_widget.py
+++ src/sentry/models/dashboard_widget.py
@@ -161,6 +161,7 @@ class DashboardWidgetQuery(Model):
     date_modified = models.DateTimeField(default=timezone.now)
     # Whether this query is hidden from the UI, used by metric widgets
     is_hidden = models.BooleanField(default=False)
+    selected_aggregate = models.IntegerField(null=True)
 
     class Meta:
         app_label = "sentry"
