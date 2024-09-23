92b893090b48beb01c63a08df04f5dbc488f50cf
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-09-23 13:58:34 -0400

    fix(uptime): Use correct feature flag for wizard (#77959)

--- static/app/views/alerts/wizard/options.tsx
+++ static/app/views/alerts/wizard/options.tsx
@@ -133,7 +133,8 @@ export const getAlertWizardCategories = (org: Organization) => {
         options: ['llm_tokens', 'llm_cost'],
       });
     }
-    if (org.features.includes('organizations:uptime-display-wizard-create')) {
+
+    if (org.features.includes('uptime-display-wizard-create')) {
       result.push({
         categoryHeading: t('Uptime'),
         options: ['uptime_monitor'],
