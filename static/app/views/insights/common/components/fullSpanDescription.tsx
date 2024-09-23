03154bd3bfc3a9907dded314d997727d18526508
Ash <0Calories@users.noreply.github.com>
2024-09-23 11:19:31 -0400

    feat(insights): Occlude very large queries in the hover tooltip (#77800)
    Extremely long queries can be overwhelming and ugly when they appear in
    the tooltip preview after hovering over a query description. This PR
    uses the `ClippedBox` component to add a max height to queries, and
    provides a "View full query" button to encourage the user to inspect the
    query in its entirety. Clicking on this button will just jump to the
    summary page for that query
    
    Applies to both SQL and MongoDB queries.
    
    
    
    ![image](https://github.com/user-attachments/assets/3cad00b1-21e5-4999-8ddd-f690e82136a4)
    
    ---------
    
    Co-authored-by: Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>

--- static/app/views/insights/common/components/fullSpanDescription.tsx
+++ static/app/views/insights/common/components/fullSpanDescription.tsx
@@ -1,13 +1,20 @@
-import {Fragment} from 'react';
+import {Fragment, type ReactNode} from 'react';
 import styled from '@emotion/styled';
 
+import ClippedBox from 'sentry/components/clippedBox';
 import {CodeSnippet} from 'sentry/components/codeSnippet';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {IconOpen} from 'sentry/icons';
+import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {SQLishFormatter} from 'sentry/utils/sqlish/SQLishFormatter';
+import {MutableSearch} from 'sentry/utils/tokenizeSearch';
+import {useLocation} from 'sentry/utils/useLocation';
+import {useNavigate} from 'sentry/utils/useNavigate';
+import {useSpansIndexed} from 'sentry/views/insights/common/queries/useDiscover';
 import {useFullSpanFromTrace} from 'sentry/views/insights/common/queries/useFullSpanFromTrace';
 import {prettyPrintJsonString} from 'sentry/views/insights/database/utils/jsonUtils';
-import {ModuleName} from 'sentry/views/insights/types';
+import {ModuleName, SpanIndexedField} from 'sentry/views/insights/types';
 
 const formatter = new SQLishFormatter();
 
@@ -29,16 +36,32 @@ export function FullSpanDescription({
   filters,
   moduleName,
 }: Props) {
+  const {data: indexedSpans, isFetching: areIndexedSpansLoading} = useSpansIndexed(
+    {
+      search: MutableSearch.fromQueryObject({'span.group': group}),
+      limit: 1,
+      fields: [
+        SpanIndexedField.PROJECT_ID,
+        SpanIndexedField.TRANSACTION_ID,
+        SpanIndexedField.SPAN_DESCRIPTION,
+      ],
+    },
+    'api.starfish.span-description'
+  );
+  const indexedSpan = indexedSpans?.[0];
+
+  // This is used as backup in case we don't have the necessary data available in the indexed span
   const {
     data: fullSpan,
     isLoading,
     isFetching,
-  } = useFullSpanFromTrace(group, [INDEXED_SPAN_SORT], Boolean(group), filters);
+  } = useFullSpanFromTrace(group, [INDEXED_SPAN_SORT], Boolean(indexedSpan), filters);
 
-  const description = fullSpan?.description ?? shortDescription;
+  const description =
+    indexedSpan?.['span.description'] ?? fullSpan?.description ?? shortDescription;
   const system = fullSpan?.data?.['db.system'];
 
-  if (isLoading && isFetching) {
+  if (areIndexedSpansLoading || (isLoading && isFetching)) {
     return (
       <PaddedSpinner>
         <LoadingIndicator mini hideMessage relative />
@@ -53,24 +76,37 @@ export function FullSpanDescription({
   if (moduleName === ModuleName.DB) {
     if (system === 'mongodb') {
       let stringifiedQuery = '';
+      let result: ReturnType<typeof prettyPrintJsonString> | undefined = undefined;
 
-      if (fullSpan?.sentry_tags) {
-        stringifiedQuery = prettyPrintJsonString(fullSpan?.sentry_tags?.description);
+      if (indexedSpan?.['span.description']) {
+        result = prettyPrintJsonString(indexedSpan?.['span.description']);
       } else if (description) {
-        stringifiedQuery = prettyPrintJsonString(description);
+        result = prettyPrintJsonString(description);
       } else if (fullSpan?.sentry_tags?.description) {
-        stringifiedQuery = prettyPrintJsonString(fullSpan?.sentry_tags?.description);
+        result = prettyPrintJsonString(fullSpan?.sentry_tags.description);
+      } else if (fullSpan?.sentry_tags?.description) {
+        result = prettyPrintJsonString(fullSpan?.sentry_tags?.description);
       } else {
         stringifiedQuery = description || fullSpan?.sentry_tags?.description || 'N/A';
       }
 
-      return <CodeSnippet language="json">{stringifiedQuery}</CodeSnippet>;
+      if (result) {
+        stringifiedQuery = result.prettifiedQuery;
+      }
+
+      return (
+        <QueryClippedBox group={group}>
+          <CodeSnippet language="json">{stringifiedQuery}</CodeSnippet>
+        </QueryClippedBox>
+      );
     }
 
     return (
-      <CodeSnippet language="sql">
-        {formatter.toString(description, {maxLineLength: LINE_LENGTH})}
-      </CodeSnippet>
+      <QueryClippedBox group={group}>
+        <CodeSnippet language="sql">
+          {formatter.toString(description, {maxLineLength: LINE_LENGTH})}
+        </CodeSnippet>
+      </QueryClippedBox>
     );
   }
 
@@ -81,8 +117,40 @@ export function FullSpanDescription({
   return <Fragment>{description}</Fragment>;
 }
 
+type TruncatedQueryClipBoxProps = {
+  children: ReactNode;
+  group: string | undefined;
+};
+
+function QueryClippedBox({group, children}: TruncatedQueryClipBoxProps) {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  return (
+    <StyledClippedBox
+      btnText={t('View full query')}
+      clipHeight={500}
+      buttonProps={{
+        icon: <IconOpen />,
+        onClick: () =>
+          navigate({
+            pathname: `${location.pathname}spans/span/${group}/`,
+          }),
+      }}
+    >
+      {children}
+    </StyledClippedBox>
+  );
+}
+
 const LINE_LENGTH = 60;
 
 const PaddedSpinner = styled('div')`
   padding: 0 ${space(0.5)};
+`;
+
+const StyledClippedBox = styled(ClippedBox)`
+  > div > div {
+    z-index: 1;
+  }
 `;
