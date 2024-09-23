1b80b9b6609fd808639a5ac2a6811e163e9bc328
David Wang <dwang@sentry.io>
2024-09-23 15:11:21 -0700

    feat(uptime): Add validator fields for method, body, header (#77723)
    Adds new fields to the validator matching up with
    https://github.com/getsentry/sentry/pull/77611
    
    ---------
    
    Co-authored-by: Dan Fuller <dfuller@sentry.io>

--- tests/sentry/uptime/endpoints/test_project_uptime_alert_index.py
+++ tests/sentry/uptime/endpoints/test_project_uptime_alert_index.py
@@ -1,6 +1,7 @@
 from rest_framework.exceptions import ErrorDetail
 
 from sentry.testutils.helpers import with_feature
+from sentry.uptime.endpoints.validators import MAX_REQUEST_SIZE_BYTES
 from sentry.uptime.models import ProjectUptimeSubscription, ProjectUptimeSubscriptionMode
 from sentry.uptime.subscriptions.subscriptions import DEFAULT_SUBSCRIPTION_TIMEOUT_MS
 from tests.sentry.uptime.endpoints import UptimeAlertBaseEndpointTest
@@ -101,3 +102,95 @@ class ProjectUptimeAlertIndexPostEndpointTest(ProjectUptimeAlertIndexBaseEndpoin
         assert uptime_subscription.url == "http://sentry.io"
         assert uptime_subscription.interval_seconds == 60
         assert uptime_subscription.timeout_ms == DEFAULT_SUBSCRIPTION_TIMEOUT_MS
+
+    @with_feature("organizations:uptime-api-create-update")
+    def test_headers_body_method(self):
+        resp = self.get_success_response(
+            self.organization.slug,
+            self.project.slug,
+            name="test",
+            owner=f"user:{self.user.id}",
+            url="http://sentry.io",
+            interval_seconds=60,
+            method="POST",
+            body='{"key": "value"}',
+            headers={"header": "value"},
+        )
+        uptime_monitor = ProjectUptimeSubscription.objects.get(id=resp.data["id"])
+        uptime_subscription = uptime_monitor.uptime_subscription
+        assert uptime_monitor.name == "test"
+        assert uptime_monitor.owner_user_id == self.user.id
+        assert uptime_monitor.owner_team_id is None
+        assert uptime_monitor.mode == ProjectUptimeSubscriptionMode.MANUAL
+        assert uptime_subscription.url == "http://sentry.io"
+        assert uptime_subscription.interval_seconds == 60
+        assert uptime_subscription.timeout_ms == DEFAULT_SUBSCRIPTION_TIMEOUT_MS
+        assert uptime_subscription.body == '{"key": "value"}'
+        assert uptime_subscription.headers == {"header": "value"}
+
+    @with_feature("organizations:uptime-api-create-update")
+    def test_headers_body_method_already_exists(self):
+        resp = self.get_success_response(
+            self.organization.slug,
+            self.project.slug,
+            name="test",
+            owner=f"user:{self.user.id}",
+            url="http://sentry.io",
+            interval_seconds=60,
+            method="POST",
+            body='{"key": "value"}',
+            headers={"header": "value"},
+        )
+        uptime_monitor = ProjectUptimeSubscription.objects.get(id=resp.data["id"])
+        new_proj = self.create_project()
+        resp = self.get_success_response(
+            self.organization.slug,
+            new_proj.slug,
+            name="test",
+            owner=f"user:{self.user.id}",
+            url="http://sentry.io",
+            interval_seconds=60,
+            method="POST",
+            body='{"key": "value"}',
+            headers={"header": "value"},
+        )
+        new_uptime_monitor = ProjectUptimeSubscription.objects.get(id=resp.data["id"])
+        assert uptime_monitor.uptime_subscription_id == new_uptime_monitor.uptime_subscription_id
+        assert new_uptime_monitor.project_id != uptime_monitor.project_id
+        resp = self.get_success_response(
+            self.organization.slug,
+            new_proj.slug,
+            name="test",
+            owner=f"user:{self.user.id}",
+            url="http://sentry.io",
+            interval_seconds=60,
+            method="POST",
+            body='{"key": "value"}',
+            headers={"header": "valu"},
+        )
+        newer_uptime_monitor = ProjectUptimeSubscription.objects.get(id=resp.data["id"])
+        assert (
+            newer_uptime_monitor.uptime_subscription_id != new_uptime_monitor.uptime_subscription_id
+        )
+
+    @with_feature("organizations:uptime-api-create-update")
+    def test_size_too_big(self):
+        resp = self.get_error_response(
+            self.organization.slug,
+            self.project.slug,
+            name="test",
+            owner=f"user:{self.user.id}",
+            url="http://sentry.io",
+            interval_seconds=60,
+            method="POST",
+            body="body" * 250,
+            headers={"header": "value"},
+        )
+        assert resp.data == {
+            "nonFieldErrors": [
+                ErrorDetail(
+                    string=f"Request is too large, max size is {MAX_REQUEST_SIZE_BYTES} bytes",
+                    code="invalid",
+                )
+            ]
+        }
