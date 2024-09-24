d45164830a5e221bdf556424b8e7ba943a06d94b
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-09-24 13:02:52 -0700

    feat(stories): add story for `usePrevious` (#78042)
    turns out we can stack `usePrevious` to get even older states.
    
    added a story for `usePrevious` to demonstrate this behavior:
    
    
    https://github.com/user-attachments/assets/5992e309-e0e5-4622-8be9-f70ba767c01f

---
+++ static/app/utils/usePrevious.stories.tsx
@@ -0,0 +1,39 @@
+import {Fragment, useState} from 'react';
+
+import StructuredEventData from 'sentry/components/structuredEventData';
+import storyBook from 'sentry/stories/storyBook';
+import usePrevious from 'sentry/utils/usePrevious';
+
+export default storyBook('usePrevious', story => {
+  story('Default', () => {
+    const [count, setCount] = useState(0);
+    const prevCount = usePrevious(count);
+
+    return (
+      <Fragment>
+        <p>
+          Use <code>usePrevious</code> to keep track of the previous state.
+        </p>
+        <button onClick={() => setCount(prev => prev + 1)}>Add 1</button>
+        <StructuredEventData data={{count, prevCount}} />;
+      </Fragment>
+    );
+  });
+
+  story('Stacked', () => {
+    const [count, setCount] = useState(0);
+    const prevCount = usePrevious(count);
+    const penultimateCount = usePrevious(prevCount);
+
+    return (
+      <Fragment>
+        <p>
+          You can even stack <code>usePrevious</code> to keep track of the penultimate
+          states.
+        </p>
+        <button onClick={() => setCount(prev => prev + 1)}>Add 1</button>
+        <StructuredEventData data={{count, prevCount, penultimateCount}} />;
+      </Fragment>
+    );
+  });
+});
