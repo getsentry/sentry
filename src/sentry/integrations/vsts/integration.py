c08119f3cdb433765f805446feed3d80c2ef97a8
Raj Joshi <raj.joshi@sentry.io>
2024-09-24 14:50:13 -0700

    :mag: nit: fix tests, update logger to warning, and error metrics

--- src/sentry/integrations/vsts/integration.py
+++ src/sentry/integrations/vsts/integration.py
@@ -42,6 +42,7 @@ from sentry.shared_integrations.exceptions import (
     IntegrationProviderError,
 )
 from sentry.silo.base import SiloMode
+from sentry.utils import metrics
 from sentry.utils.http import absolute_uri
 from sentry.web.helpers import render_to_response
 
@@ -527,11 +528,20 @@ class VstsIntegrationProvider(IntegrationProvider):
                 status=ObjectStatus.ACTIVE,
             ).exists()
 
+            metrics.incr(
+                "integrations.migration.vsts_integration_migration",
+                sample_rate=1.0,
+            )
+
         except (IntegrationModel.DoesNotExist, AssertionError, KeyError):
-            logger.info(
+            logger.warning(
                 "vsts.build_integration.error",
                 extra={
-                    "organization_id": self.pipeline.organization.id,
+                    "organization_id": (
+                        self.pipeline.organization.id
+                        if self.pipeline and self.pipeline.organization
+                        else None
+                    ),
                     "user_id": user["id"],
                     "account": account,
                 },
