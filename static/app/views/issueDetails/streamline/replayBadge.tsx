e6fcf93decab83359d36b854f4b86f0fed139009
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-09-24 15:22:31 -0700

    ✨ Implement Replay drawer

---
+++ static/app/views/issueDetails/streamline/replayBadge.tsx
@@ -0,0 +1,54 @@
+import {Fragment, useRef} from 'react';
+
+import {ReplayDrawer} from 'sentry/components/events/eventReplay/replayDrawer';
+import useDrawer, {type DrawerOptions} from 'sentry/components/globalDrawer';
+import {IconPlay} from 'sentry/icons';
+import {tn} from 'sentry/locale';
+import type {Group} from 'sentry/types/group';
+import type {Project} from 'sentry/types/project';
+import {getConfigForIssueType} from 'sentry/utils/issueTypeConfig';
+import useReplayCountForIssues from 'sentry/utils/replayCount/useReplayCountForIssues';
+import {Divider} from 'sentry/views/issueDetails/divider';
+import {TextButton} from 'sentry/views/issueDetails/streamline/attachmentsBadge';
+
+export function ReplayBadge({group, project}: {group: Group; project: Project}) {
+  const {openDrawer} = useDrawer();
+  const openButtonRef = useRef<HTMLButtonElement>(null);
+  const drawerOptions: DrawerOptions = {
+    ariaLabel: 'replay drawer',
+    shouldCloseOnInteractOutside: el => {
+      if (openButtonRef.current?.contains(el)) {
+        return false;
+      }
+      return true;
+    },
+  };
+  const issueTypeConfig = getConfigForIssueType(group, project);
+  const {getReplayCountForIssue} = useReplayCountForIssues();
+  const replaysCount = getReplayCountForIssue(group.id, group.issueCategory) ?? 0;
+
+  if (!issueTypeConfig.replays.enabled || replaysCount <= 0) {
+    return null;
+  }
+
+  return (
+    <Fragment>
+      <Divider />
+      <TextButton
+        ref={openButtonRef}
+        type="button"
+        priority="link"
+        size="zero"
+        icon={<IconPlay size="xs" />}
+        onClick={() => {
+          openDrawer(
+            () => <ReplayDrawer group={group} project={project} />,
+            drawerOptions
+          );
+        }}
+      >
+        {tn('%s Replay', '%s Replays', replaysCount)}
+      </TextButton>
+    </Fragment>
+  );
+}
