f55a2a9bf8dca4c189a37ebacc5d487c8d047ced
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-25 15:42:58 -0700

    Add basic login_success test

--- src/sentry/toolbar/views/login_success_view.py
+++ src/sentry/toolbar/views/login_success_view.py
@@ -3,8 +3,10 @@ from django.http import HttpRequest
 from sentry.toolbar.views.base import ToolbarView
 from sentry.web.frontend.base import region_silo_view
 
+SUCCESS_TEMPLATE = "sentry/toolbar/login-success.html"
+
 
 @region_silo_view
 class LoginSuccessView(ToolbarView):
     def get(self, request: HttpRequest, organization, project):
-        return self.respond("sentry/toolbar/login-success.html", status=200)
+        return self.respond(SUCCESS_TEMPLATE, status=200)
--- tests/sentry/toolbar/views/test_login_success_view.py
+++ tests/sentry/toolbar/views/test_login_success_view.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 
 from sentry.testutils.cases import APITestCase
+from sentry.toolbar.views.login_success_view import SUCCESS_TEMPLATE
 from tests.sentry.toolbar.utils.test_http import get_directives
 
 
@@ -12,6 +13,11 @@ class LoginSuccessViewTest(APITestCase):
         self.login_as(user=self.user)
         self.url = reverse(self.view_name, args=(self.organization.slug, self.project.slug))
 
+    def test_basic(self):
+        res = self.client.get(self.url)
+        assert res.status_code == 200
+        assert [t.name for t in res.templates] == [SUCCESS_TEMPLATE]
+
     def test_csp(self):
         res = self.client.get(self.url)
         csp = res.headers.get("Content-Security-Policy")
@@ -22,5 +28,3 @@ class LoginSuccessViewTest(APITestCase):
         for src in ["sentry.io", "*.sentry.io"]:
             assert src in script_src
         assert any([src == "'unsafe-inline'" or src.startswith("'nonce-") for src in script_src])
-
-    # TODO: success template test
