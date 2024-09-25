c20ee78fbccbb9985f5d7b8918cbda5e339d2752
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-09-25 10:39:27 -0400

    feat(dashboards): `BigNumberWidget` Layout, UI, Data, State (#77877)
    Implements
    [`BigNumberWidget`](https://www.notion.so/sentry/Widget-Components-Spec-c8aec2bc216a446b9616e31085204ce0?pvs=4)
    component with the Layout, Data, State, and Visualization features. This
    is the bare-bones implementation.
    
    The code is almost all extracted straight out of the existing code.
    
    N.B. Some of the rounding and formatting isn't quite working yet, I have
    other PRs in flight that'll fix that.
    
    - `BigNumberWidgetVisualization` is basically just
    `bigNumberComponent()` from `WidgetCardCart`
    - `WidgetFrame` is basically the outer content of `WidgetCard` and so on
    
    This is the first widget component, and I chose a code structure that I
    think makes sense and is as simple as possible. I'm assuming this'll
    change when more widget components show up and more functionality is
    added, but if you have concerns about this setup, do tell!
    
    Setting up specs and stories right away, too.
    
    Closes https://github.com/getsentry/sentry/issues/77777

---
+++ static/app/views/dashboards/widgets/common/widgetFrame.tsx
@@ -0,0 +1,88 @@
+import styled from '@emotion/styled';
+
+import {HeaderTitle} from 'sentry/components/charts/styles';
+import {Tooltip} from 'sentry/components/tooltip';
+import {space} from 'sentry/styles/space';
+
+export interface Props {
+  children: React.ReactNode;
+  description?: string;
+  title?: string;
+}
+
+export function WidgetFrame(props: Props) {
+  const {title, description, children} = props;
+
+  return (
+    <Frame>
+      <Header>
+        <Title>
+          <Tooltip title={title} containerDisplayMode="grid" showOnlyOnOverflow>
+            <TitleText>{title}</TitleText>
+          </Tooltip>
+        </Title>
+
+        {description && (
+          <Tooltip
+            title={description}
+            containerDisplayMode="grid"
+            showOnlyOnOverflow
+            isHoverable
+          >
+            <Description>{description}</Description>
+          </Tooltip>
+        )}
+      </Header>
+
+      <VisualizationWrapper>{children}</VisualizationWrapper>
+    </Frame>
+  );
+}
+
+const Frame = styled('div')`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+
+  height: 100%;
+  width: 100%;
+
+  border-radius: ${p => p.theme.panelBorderRadius};
+  border: ${p => p.theme.border};
+  border: 1px ${p => 'solid ' + p.theme.border};
+
+  background: ${p => p.theme.background};
+`;
+
+const Header = styled('div')`
+  display: flex;
+  flex-direction: column;
+
+  width: 100%;
+  min-height: 36px;
+
+  padding: ${space(2)} ${space(1)} 0 ${space(3)};
+`;
+
+const Title = styled('div')`
+  display: inline-flex;
+  align-items: center;
+  gap: ${space(0.75)};
+`;
+
+const Description = styled('small')`
+  ${p => p.theme.overflowEllipsis}
+
+  color: ${p => p.theme.gray300};
+`;
+
+const TitleText = styled(HeaderTitle)`
+  ${p => p.theme.overflowEllipsis};
+  font-weight: ${p => p.theme.fontWeightBold};
+`;
+
+const VisualizationWrapper = styled('div')`
+  display: flex;
+  flex-grow: 1;
+  position: relative;
+`;
