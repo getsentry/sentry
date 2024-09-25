f2d954d32a55af2a3898d995d19f6cca9bfe7ea4
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-09-25 11:32:05 -0700

    remove threshold, change comparison to json

--- src/sentry/workflow_engine/models/data_condition.py
+++ src/sentry/workflow_engine/models/data_condition.py
@@ -6,11 +6,6 @@ from sentry.db.models import DefaultFieldsModel, region_silo_model, sane_repr
 from .data_condition_group import DataConditionGroup
 
 
-def validate_primitive_value(value):
-    if not isinstance(value, (bool, int, float, str)):
-        raise ValueError("This value must be a primitive type.")
-
-
 @region_silo_model
 class DataCondition(DefaultFieldsModel):
     """
@@ -23,14 +18,11 @@ class DataCondition(DefaultFieldsModel):
     # The condition is the logic condition that needs to be met, gt, lt, eq, etc.
     condition = models.CharField(max_length=200)
 
-    # The threshold is the value that the condition is compared to for numeric expressions
-    threshold = models.FloatField(blank=True, null=True)
-
-    # The comparison is the value that the condition is compared to for string expressions
-    comparison = models.CharField(max_length=200, blank=True, null=True)
+    # The comparison is the value that the condition is compared to for the evaluation, this must be a primitive value
+    comparison = models.JSONField()
 
     # The condition_result is the value that is returned if the condition is met, this must be a primitive value
-    condition_result = models.JSONField(validators=[validate_primitive_value])
+    condition_result = models.JSONField()
 
     # The type of condition, this is used to initialize the condition classes
     type = models.CharField(max_length=200)
