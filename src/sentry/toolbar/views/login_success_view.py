dd11d3c4f172a1eb8357bc5146dd7f3a3c2f4bd5
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-09-26 13:23:54 -0700

    Add basic login_success test

--- src/sentry/toolbar/views/login_success_view.py
+++ src/sentry/toolbar/views/login_success_view.py
@@ -4,6 +4,8 @@ from django.http import HttpRequest
 from sentry.toolbar.views import TOOLBAR_CSP_SCRIPT_SRC
 from sentry.web.frontend.base import OrganizationView, region_silo_view
 
+SUCCESS_TEMPLATE = "sentry/toolbar/login-success.html"
+
 
 @region_silo_view
 class LoginSuccessView(OrganizationView):
@@ -11,4 +13,4 @@ class LoginSuccessView(OrganizationView):
         SCRIPT_SRC=TOOLBAR_CSP_SCRIPT_SRC
     )  # Appends to the CSP enforced in prod. Allows running the inline scripts in the response templates.
     def get(self, request: HttpRequest, organization, project_id_or_slug):
-        return self.respond("sentry/toolbar/login-success.html", status=200)
+        return self.respond(SUCCESS_TEMPLATE, status=200)
--- tests/sentry/toolbar/views/test_login_success_view.py
+++ tests/sentry/toolbar/views/test_login_success_view.py
@@ -3,6 +3,7 @@ from django.urls import reverse
 
 from sentry.testutils.cases import APITestCase
 from sentry.toolbar.views import has_valid_csp
+from sentry.toolbar.views.login_success_view import SUCCESS_TEMPLATE
 
 
 class LoginSuccessViewTest(APITestCase):
@@ -12,6 +13,11 @@ class LoginSuccessViewTest(APITestCase):
         super().setUp()
         self.login_as(user=self.user)
         self.url = reverse(self.view_name, args=(self.organization.slug, self.project.slug))
+
+    def test_get(self):
+        response = self.client.get(self.url)
+        assert response.status_code == 200
+        self.assertTemplateUsed(response, SUCCESS_TEMPLATE)
 
     @override_settings(CSP_REPORT_ONLY=False)
     def test_csp_enforce(self):
