299d586bd4af28a7b7b8dbaaaf14a5fee6b803a6
Filippo Pacifici <fpacifici@sentry.io>
2024-09-26 17:34:10 -0700

    Add safe append

--- src/sentry/utils/celery.py
+++ src/sentry/utils/celery.py
@@ -47,3 +47,28 @@ def make_split_task_queues(config: Mapping[str, SplitQueueTaskRoute]) -> Sequenc
         ret.extend(_build_queues(conf["default_queue"], conf["queues_config"]["total"]))
 
     return ret
+
+
+def safe_append(queues: MutableSequence[Queue], queue: Queue) -> None:
+    """
+    We define queues as lists in the configuration and we allow override
+    of the config per environment.
+    Unfortunately if you add twice a queue with the same name to the celery
+    config. Celery just creates the queue twice. This can be an undesired behavior
+    depending on the Celery backend. So this method allows to add queues to
+    a list without duplications.
+    """
+    existing_queue_names = {q.name for q in queues}
+    if queue.name not in existing_queue_names:
+        queues.append(queue)
+
+
+def safe_extend(queues: MutableSequence[Queue], to_add: Sequence[Queue]) -> None:
+    """
+    Like `safe_append` but it works like extend adding multiple queues
+    to the config.
+    """
+    existing_queue_names = {q.name for q in queues}
+    for q in to_add:
+        if q.name not in existing_queue_names:
+            queues.append(q)
--- tests/sentry/utils/test_celery.py
+++ tests/sentry/utils/test_celery.py
@@ -1,6 +1,12 @@
 from kombu import Queue
 
-from sentry.utils.celery import build_queue_names, make_split_queues, make_split_task_queues
+from sentry.utils.celery import (
+    build_queue_names,
+    make_split_queues,
+    make_split_task_queues,
+    safe_append,
+    safe_extend,
+)
 
 
 def test_split_queue() -> None:
@@ -39,3 +45,38 @@ def test_split_task_queue() -> None:
 
 def test_build_names() -> None:
     assert build_queue_names("my_queue", 3) == ["my_queue_1", "my_queue_2", "my_queue_3"]
+
+
+def test_safe_append() -> None:
+    queues = [
+        Queue(name="my_queue_1", routing_key="my_queue_1"),
+        Queue(name="my_queue_2", routing_key="my_queue_2"),
+    ]
+
+    safe_append(queues, Queue(name="my_queue_1", routing_key="my_queue_1"))
+    safe_append(queues, Queue(name="my_queue_3", routing_key="my_queue_3"))
+
+    assert queues == [
+        Queue(name="my_queue_1", routing_key="my_queue_1"),
+        Queue(name="my_queue_2", routing_key="my_queue_2"),
+        Queue(name="my_queue_3", routing_key="my_queue_3"),
+    ]
+
+
+def test_safe_extend() -> None:
+    queues = [
+        Queue(name="my_queue_1", routing_key="my_queue_1"),
+    ]
+
+    safe_extend(
+        queues,
+        [
+            Queue(name="my_queue_1", routing_key="my_queue_1"),
+            Queue(name="my_queue_2", routing_key="my_queue_2"),
+        ],
+    )
+
+    assert queues == [
+        Queue(name="my_queue_1", routing_key="my_queue_1"),
+        Queue(name="my_queue_2", routing_key="my_queue_2"),
+    ]
