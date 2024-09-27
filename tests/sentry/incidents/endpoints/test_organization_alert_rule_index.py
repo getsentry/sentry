85569b3706c576f100c86df96a0f182e1397b4cd
Colleen O'Rourke <colleen@sentry.io>
2024-09-27 11:53:53 -0700

    ref(alerts): Move dynamic rule code into their own functions for create/update alert (#78241)
    `create_alert_rule` and `update_alert_rule` are long functions already,
    so this PR moves the dynamic rule specific code out of it.
    
    Closes https://getsentry.atlassian.net/browse/ALRT-306

--- tests/sentry/incidents/endpoints/test_organization_alert_rule_index.py
+++ tests/sentry/incidents/endpoints/test_organization_alert_rule_index.py
@@ -10,6 +10,7 @@ from django.db import router, transaction
 from django.test.utils import override_settings
 from httpx import HTTPError
 from rest_framework import status
+from rest_framework.exceptions import ErrorDetail
 from urllib3.exceptions import MaxRetryError, TimeoutError
 from urllib3.response import HTTPResponse
 
@@ -400,7 +401,10 @@ class AlertRuleCreateEndpointTest(AlertRuleIndexBase, SnubaTestCase):
                 **data,
             )
         assert not AlertRule.objects.filter(detection_type=AlertRuleDetectionType.DYNAMIC).exists()
-        assert resp.data["detail"]["message"] == "Invalid request"
+        assert resp.data[0] == ErrorDetail(
+            string="Failed to send data to Seer - cannot create alert rule.", code="invalid"
+        )
+
         assert mock_seer_request.call_count == 1
 
     @with_feature("organizations:anomaly-detection-alerts")
