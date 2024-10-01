9899cdbbb779a9f9e0262ef891d678bd4e2ad83c
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-09-30 17:06:38 -0700

    WIP

---
+++ src/sentry/workflow_engine/processors/data_sources.py
@@ -0,0 +1,8 @@
+def process_data_sources(data_sources: list):
+    # get the tuple id/type from the data_sources list
+
+    # query the database for the data source, and get the corresponding detectors
+
+    # format the data for process_detectors, a tuple of the detector + the data, we don't need the datasource anymore
+    # return the formatted data
+    return None
