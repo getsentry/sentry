185cec2116f397debf0badf74873d75e68fc16ad
Malachi Willey <malwilley@gmail.com>
2024-10-01 12:12:35 -0700

    chore(query-builder): Remove flag check for project details search (#78414)

--- static/app/views/projectDetail/projectFilters.tsx
+++ static/app/views/projectDetail/projectFilters.tsx
@@ -5,12 +5,10 @@ import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import {EnvironmentPageFilter} from 'sentry/components/organizations/environmentPageFilter';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import {SearchQueryBuilder} from 'sentry/components/searchQueryBuilder';
-import SmartSearchBar from 'sentry/components/smartSearchBar';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Tag} from 'sentry/types/group';
 import {SEMVER_TAGS} from 'sentry/utils/discover/fields';
-import useOrganization from 'sentry/utils/useOrganization';
 
 import type {TagValueLoader} from '../issueList/types';
 
@@ -30,8 +28,6 @@ const SUPPORTED_TAGS = {
 };
 
 function ProjectFilters({query, relativeDateOptions, tagValueLoader, onSearch}: Props) {
-  const organization = useOrganization();
-
   const getTagValues = useCallback(
     async (tag: Tag, currentQuery: string): Promise<string[]> => {
       const values = await tagValueLoader(tag.key, currentQuery);
@@ -46,28 +42,15 @@ function ProjectFilters({query, relativeDateOptions, tagValueLoader, onSearch}: 
         <EnvironmentPageFilter />
         <DatePageFilter relativeOptions={relativeDateOptions} />
       </PageFilterBar>
-      {organization.features.includes('search-query-builder-project-details') ? (
-        <SearchQueryBuilder
-          searchSource="project_filters"
-          initialQuery={query ?? ''}
-          placeholder={t('Search by release version, build, package, or stage')}
-          filterKeys={SUPPORTED_TAGS}
-          onSearch={onSearch}
-          getTagValues={getTagValues}
-          showUnsubmittedIndicator
-        />
-      ) : (
-        <SmartSearchBar
-          searchSource="project_filters"
-          query={query}
-          placeholder={t('Search by release version, build, package, or stage')}
-          hasRecentSearches={false}
-          supportedTags={SUPPORTED_TAGS}
-          maxMenuHeight={500}
-          onSearch={onSearch}
-          onGetTagValues={getTagValues}
-        />
-      )}
+      <SearchQueryBuilder
+        searchSource="project_filters"
+        initialQuery={query ?? ''}
+        placeholder={t('Search by release version, build, package, or stage')}
+        filterKeys={SUPPORTED_TAGS}
+        onSearch={onSearch}
+        getTagValues={getTagValues}
+        showUnsubmittedIndicator
+      />
     </FiltersWrapper>
   );
 }
