6d37d2e8314fc41a4f5766dccf53256148895a69
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-10-01 08:59:39 -0400

    ref(tsc): Convert `ProjectApdexScoreCard` to FC (#78294)
    Part of https://github.com/getsentry/frontend-tsc/issues/2

--- static/app/views/projectDetail/projectScoreCards/projectApdexScoreCard.spec.tsx
+++ static/app/views/projectDetail/projectScoreCards/projectApdexScoreCard.spec.tsx
@@ -1,11 +1,12 @@
 import {OrganizationFixture} from 'sentry-fixture/organization';
 
-import {render} from 'sentry-test/reactTestingLibrary';
+import {render, screen} from 'sentry-test/reactTestingLibrary';
 
 import ProjectApdexScoreCard from 'sentry/views/projectDetail/projectScoreCards/projectApdexScoreCard';
 
 describe('ProjectDetail > ProjectApdex', function () {
-  let endpointMock: jest.Mock;
+  let currentDataEndpointMock: jest.Mock;
+  let previousDataEndpointMock: jest.Mock;
   const organization = OrganizationFixture();
 
   const selection = {
@@ -19,21 +20,36 @@ describe('ProjectDetail > ProjectApdex', function () {
     },
   };
 
-  beforeEach(function () {
-    endpointMock = MockApiClient.addMockResponse({
-      url: `/organizations/${organization.slug}/events/`,
-      body: {
-        data: [],
-      },
-      status: 200,
-    });
-  });
-
   afterEach(function () {
     MockApiClient.clearMockResponses();
   });
 
-  it('calls api with apdex', function () {
+  it('renders apdex', async function () {
+    previousDataEndpointMock = MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/events/`,
+      body: {
+        data: [
+          {
+            'apdex()': 0.678,
+          },
+        ],
+      },
+      status: 200,
+    });
+
+    currentDataEndpointMock = MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/events/`,
+      body: {
+        data: [
+          {
+            'apdex()': 0.781,
+          },
+        ],
+      },
+      status: 200,
+      match: [MockApiClient.matchQuery({statsPeriod: '14d'})],
+    });
+
     render(
       <ProjectApdexScoreCard
         organization={{...organization, features: ['discover-basic', 'performance-view']}}
@@ -43,7 +59,11 @@ describe('ProjectDetail > ProjectApdex', function () {
       />
     );
 
-    expect(endpointMock).toHaveBeenNthCalledWith(
+    expect(await screen.findByText('Apdex')).toBeInTheDocument();
+    expect(await screen.findByText('0.781')).toBeInTheDocument();
+    expect(await screen.findByText('0.103')).toBeInTheDocument();
+
+    expect(currentDataEndpointMock).toHaveBeenNthCalledWith(
       1,
       `/organizations/${organization.slug}/events/`,
       expect.objectContaining({
@@ -56,5 +76,46 @@ describe('ProjectDetail > ProjectApdex', function () {
         },
       })
     );
+
+    expect(previousDataEndpointMock).toHaveBeenNthCalledWith(
+      1,
+      `/organizations/${organization.slug}/events/`,
+      expect.objectContaining({
+        query: {
+          environment: [],
+          field: ['apdex()'],
+          project: ['1'],
+          query: 'event.type:transaction count():>0',
+          start: '2017-09-19T02:41:20',
+          end: '2017-10-03T02:41:20',
+        },
+      })
+    );
+  });
+
+  it('renders without performance', async function () {
+    const endpointMock = MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/events/`,
+      body: {
+        detail: 'test error',
+      },
+      status: 404,
+    });
+
+    render(
+      <ProjectApdexScoreCard
+        organization={{...organization, features: ['performance-view']}}
+        hasTransactions={false}
+        selection={selection}
+        isProjectStabilized
+        query="test-query"
+      />
+    );
+
+    expect(await screen.findByText('Apdex')).toBeInTheDocument();
+    expect(await screen.findByRole('button', {name: 'Start Setup'})).toBeInTheDocument();
+    expect(await screen.findByRole('button', {name: 'Get Tour'})).toBeInTheDocument();
+
+    expect(endpointMock).not.toHaveBeenCalled();
   });
 });
