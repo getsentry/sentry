9cb8a526df7bca0a069a9b95be910310bf8d2d8e
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-10-02 16:13:44 -0400

    💄 Initial redesign

--- static/app/views/issueDetails/groupTags/groupTagsDrawer.tsx
+++ static/app/views/issueDetails/groupTags/groupTagsDrawer.tsx
@@ -1,13 +1,10 @@
 import {useRef} from 'react';
 import styled from '@emotion/styled';
 
-import Alert from 'sentry/components/alert';
 import ProjectAvatar from 'sentry/components/avatar/projectAvatar';
 import {LinkButton} from 'sentry/components/button';
 import ButtonBar from 'sentry/components/buttonBar';
-import Count from 'sentry/components/count';
 import DataExport, {ExportQueryType} from 'sentry/components/dataExport';
-import {DeviceName} from 'sentry/components/deviceName';
 import {
   CrumbContainer,
   EventDrawerBody,
@@ -18,23 +15,17 @@ import {
   NavigationCrumbs,
   ShortId,
 } from 'sentry/components/events/eventDrawer';
-import {TAGS_DOCS_LINK} from 'sentry/components/events/eventTags/util';
-import ExternalLink from 'sentry/components/links/externalLink';
-import Link from 'sentry/components/links/link';
 import LoadingError from 'sentry/components/loadingError';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
-import Panel from 'sentry/components/panels/panel';
-import PanelBody from 'sentry/components/panels/panelBody';
-import Version from 'sentry/components/version';
-import {t, tct} from 'sentry/locale';
+import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Project} from 'sentry/types/project';
-import {percent} from 'sentry/utils';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
 import useProjects from 'sentry/utils/useProjects';
 import {TagDetailsDrawerContent} from 'sentry/views/issueDetails/groupTags/tagDetailsDrawerContent';
+import {TagDistribution} from 'sentry/views/issueDetails/groupTags/tagDistribution';
 import {useGroupTags} from 'sentry/views/issueDetails/groupTags/useGroupTags';
 import {Tab, TabPaths} from 'sentry/views/issueDetails/types';
 import {useGroupDetailsRoute} from 'sentry/views/issueDetails/useGroupDetailsRoute';
@@ -137,68 +128,9 @@ export function GroupTagsDrawer({projectSlug, groupId}: GroupTagsDrawerProps) {
           />
         ) : (
           <Wrapper>
-            <MarginlessAlert type="info">
-              {tct(
-                'Tags are automatically indexed for searching and breakdown charts. Learn how to [link: add custom tags to issues]',
-                {
-                  link: <ExternalLink href={TAGS_DOCS_LINK} />,
-                }
-              )}
-            </MarginlessAlert>
             <Container>
               {alphabeticalTags.map((tag, tagIdx) => (
-                <TagItem key={tagIdx}>
-                  <StyledPanel>
-                    <PanelBody withPadding>
-                      <TagHeading>
-                        <LinkButton
-                          priority="link"
-                          size="zero"
-                          to={{
-                            pathname: `${location.pathname}${tag.key}/`,
-                            query: location.query,
-                            replace: true,
-                          }}
-                        >
-                          <span data-test-id="tag-title">{tag.key}</span>
-                        </LinkButton>
-                      </TagHeading>
-                      <UnstyledUnorderedList>
-                        {tag.topValues.map((tagValue, tagValueIdx) => (
-                          <li key={tagValueIdx} data-test-id={tag.key}>
-                            <TagProgressBarLink
-                              // All events with the tag as the query
-                              to={{
-                                pathname: `${baseUrl}${TabPaths[Tab.EVENTS]}`,
-                                query: {
-                                  ...location.query,
-                                  query:
-                                    tagValue.query || `${tag.key}:"${tagValue.value}"`,
-                                },
-                              }}
-                            >
-                              <TagBarBackground
-                                widthPercent={
-                                  percent(tagValue.count, tag.totalValues) + '%'
-                                }
-                              />
-                              <TagBarLabel>
-                                {tag.key === 'release' ? (
-                                  <Version version={tagValue.name} anchor={false} />
-                                ) : (
-                                  <DeviceName value={tagValue.name} />
-                                )}
-                              </TagBarLabel>
-                              <TagBarCount>
-                                <Count value={tagValue.count} />
-                              </TagBarCount>
-                            </TagProgressBarLink>
-                          </li>
-                        ))}
-                      </UnstyledUnorderedList>
-                    </PanelBody>
-                  </StyledPanel>
-                </TagItem>
+                <TagDistribution tag={tag} key={tagIdx} />
               ))}
             </Container>
           </Wrapper>
@@ -213,81 +145,9 @@ const Wrapper = styled('div')`
   flex-direction: column;
   gap: ${space(2)};
 `;
-
-const MarginlessAlert = styled(Alert)`
-  margin: 0;
-`;
-
 const Container = styled('div')`
   display: grid;
   grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
   gap: ${space(2)};
   margin-bottom: ${space(2)};
-`;
-
-const StyledPanel = styled(Panel)`
-  height: 100%;
-`;
-
-const TagHeading = styled('h5')`
-  font-size: ${p => p.theme.fontSizeLarge};
-  margin-bottom: 0;
-  color: ${p => p.theme.linkColor};
-`;
-
-const UnstyledUnorderedList = styled('ul')`
-  list-style: none;
-  padding-left: 0;
-  margin-bottom: 0;
-`;
-
-const TagItem = styled('div')`
-  padding: 0;
-`;
-
-const TagBarBackground = styled('div')<{widthPercent: string}>`
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  background: ${p => p.theme.tagBar};
-  border-radius: ${p => p.theme.borderRadius};
-  width: ${p => p.widthPercent};
-`;
-
-const TagProgressBarLink = styled(Link)`
-  position: relative;
-  display: flex;
-  line-height: 2.2;
-  color: ${p => p.theme.textColor};
-  margin-bottom: ${space(0.5)};
-  padding: 0 ${space(1)};
-  background: ${p => p.theme.backgroundSecondary};
-  border-radius: ${p => p.theme.borderRadius};
-  overflow: hidden;
-
-  &:hover {
-    color: ${p => p.theme.textColor};
-    text-decoration: underline;
-    ${TagBarBackground} {
-      background: ${p => p.theme.tagBarHover};
-    }
-  }
-`;
-
-const TagBarLabel = styled('div')`
-  display: flex;
-  align-items: center;
-  font-size: ${p => p.theme.fontSizeMedium};
-  position: relative;
-  flex-grow: 1;
-  ${p => p.theme.overflowEllipsis}
-`;
-
-const TagBarCount = styled('div')`
-  font-size: ${p => p.theme.fontSizeMedium};
-  position: relative;
-  padding-left: ${space(2)};
-  padding-right: ${space(1)};
-  font-variant-numeric: tabular-nums;
 `;
---
+++ static/app/views/issueDetails/groupTags/tagDistribution.tsx
@@ -0,0 +1,142 @@
+import {useState} from 'react';
+import styled from '@emotion/styled';
+
+import {LinkButton} from 'sentry/components/button';
+import {DeviceName} from 'sentry/components/deviceName';
+import Link from 'sentry/components/links/link';
+import {Tooltip} from 'sentry/components/tooltip';
+import Version from 'sentry/components/version';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {percent} from 'sentry/utils';
+import {useLocation} from 'sentry/utils/useLocation';
+import type {GroupTag} from 'sentry/views/issueDetails/groupTags/useGroupTags';
+import {Tab, TabPaths} from 'sentry/views/issueDetails/types';
+
+export function TagDistribution({tag}: {tag: GroupTag}) {
+  const location = useLocation();
+  const [isHovered, setIsHovered] = useState(false);
+
+  return (
+    <div onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+      <TagHeader>
+        <TagTitle>{tag.key}</TagTitle>
+        <TagDetailsButton
+          borderless
+          size="zero"
+          to={{
+            pathname: `${location.pathname}${tag.key}/`,
+            query: location.query,
+            replace: true,
+          }}
+          isVisible={isHovered}
+        >
+          {t('Details')}
+        </TagDetailsButton>
+      </TagHeader>
+      <TagValueContent>
+        {tag.topValues.map((tagValue, tagValueIdx) => {
+          const percentage = percent(tagValue.count, tag.totalValues);
+          const displayPercentage = percentage < 1 ? '<1%' : `${Math.round(percentage)}%`;
+          return (
+            <TagValueRow key={tagValueIdx}>
+              <Tooltip delay={300} title={tagValue.name} skipWrapper>
+                <TagValue
+                  to={{
+                    pathname: `${location.pathname}${TabPaths[Tab.EVENTS]}`,
+                    query: {
+                      ...location.query,
+                      query: tagValue.query || `${tag.key}:"${tagValue.value}"`,
+                    },
+                  }}
+                >
+                  {tag.key === 'release' ? (
+                    <Version version={tagValue.name} anchor={false} />
+                  ) : (
+                    <DeviceName value={tagValue.name} />
+                  )}
+                </TagValue>
+              </Tooltip>
+              <Tooltip
+                delay={300}
+                title={`${tagValue.count} / ${tag.totalValues} `}
+                skipWrapper
+              >
+                <TagBar widthPercent={percentage} displayPercentage={displayPercentage} />
+              </Tooltip>
+            </TagValueRow>
+          );
+        })}
+      </TagValueContent>
+    </div>
+  );
+}
+
+const TagHeader = styled('div')`
+  grid-area: header;
+  display: flex;
+  justify-content: space-between;
+  border-bottom: 1px solid ${p => p.theme.translucentBorder};
+  margin-bottom: ${space(1)};
+`;
+
+const TagTitle = styled('div')`
+  font-size: ${p => p.theme.fontSizeMedium};
+  font-weight: ${p => p.theme.fontWeightBold};
+`;
+
+const TagDetailsButton = styled(LinkButton)<{isVisible: boolean}>`
+  font-size: ${p => p.theme.fontSizeSmall};
+  font-weight: ${p => p.theme.fontWeightNormal};
+  color: ${p => (p.isVisible ? p.theme.subText : 'transparent')};
+`;
+
+// The 40px is a buffer to prevent percentages from overflowing
+const TagValueContent = styled('div')`
+  display: grid;
+  grid-template-columns: 50% 1fr 40px;
+  color: ${p => p.theme.subText};
+`;
+
+const TagValueRow = styled('div')`
+  display: grid;
+  grid-column: 1 / -1;
+  grid-template-columns: subgrid;
+  align-items: center;
+`;
+
+const TagValue = styled(Link)`
+  display: block;
+  text-align: right;
+  color: ${p => p.theme.subText};
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+  margin-right: ${space(1)};
+  justify-self: end;
+`;
+
+const TagBar = styled('div')<{displayPercentage: string; widthPercent: number}>`
+  height: ${space(1)};
+  position: relative;
+  flex: 1;
+  width: calc(${p => p.widthPercent}%);
+  min-width: ${space(1)};
+  &:before {
+    position: absolute;
+    inset: 0;
+    content: '';
+    border-radius: 1000px;
+    background: ${p => p.theme.border};
+    border: 1px solid ${p => p.theme.translucentBorder};
+    width: 100%;
+  }
+  &:after {
+    position: absolute;
+    left: 100%;
+    top: 50%;
+    content: '${p => p.displayPercentage}';
+    line-height: 0;
+    margin-left: ${space(0.5)};
+  }
+`;
