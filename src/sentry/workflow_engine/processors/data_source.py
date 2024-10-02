ef280d1ea0922242016f3901468c6ab8ab2eaedc
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-10-02 13:59:43 -0700

    add telemetry

--- src/sentry/workflow_engine/processors/data_source.py
+++ src/sentry/workflow_engine/processors/data_source.py
@@ -1,17 +1,22 @@
+import sentry_sdk
 from django.db.models import Prefetch
 
+from sentry.utils import metrics
 from sentry.workflow_engine.models import DataPacket, DataSource, Detector
 
 
 def process_data_sources(
     data_packets: list[DataPacket], query_type: DataSource.Type = DataSource.Type.SNUBA_QUERY
 ) -> list[tuple[DataPacket, list[Detector]]]:
+    metrics.incr("sentry.workflow_engine.process_data_sources", tags={"query_type": query_type})
+
     data_packet_ids = [packet.id for packet in data_packets]
 
     # Fetch all data sources and associated detectors for the given data packets
-    data_sources = DataSource.objects.filter(
-        query_id__in=data_packet_ids, type=query_type
-    ).prefetch_related(Prefetch("datasourcedetector_set__detector", to_attr="detectors"))
+    with sentry_sdk.start_span(op="sentry.workflow_engine.process_data_sources.fetch_data_sources"):
+        data_sources = DataSource.objects.filter(
+            query_id__in=data_packet_ids, type=query_type
+        ).prefetch_related(Prefetch("datasourcedetector_set__detector", to_attr="detectors"))
 
     # Build a lookup dict for query_id to detectors
     query_id_to_detectors = {ds.query_id: ds.detectors for ds in data_sources}
