f56fb95c90aa1705f808faec7c5fd418ecde4f67
Katie Byers <lobsterkatie@gmail.com>
2024-10-02 11:05:46 -0700

    try fix

--- tests/sentry/event_manager/grouping/test_group_creation_lock.py
+++ tests/sentry/event_manager/grouping/test_group_creation_lock.py
@@ -66,38 +66,48 @@ def test_group_creation_race_new(monkeypatch, default_project, is_race_free):
         "metric_tags": {},
     }
 
+    errors = []
+
     def save_event():
         try:
-            with patch(
-                "sentry.grouping.ingest.hashing._calculate_event_grouping",
-                return_value=hashes,
-            ):
-                with patch(
-                    "sentry.event_manager._get_group_processing_kwargs",
-                    return_value=group_processing_kwargs,
-                ):
-                    with patch("sentry.event_manager._materialize_metadata_many"):
-                        group_info = _save_aggregate_new(**save_aggregate_kwargs)
 
-                        assert group_info is not None
-                        return_values.append(group_info)
+            group_info = _save_aggregate_new(**save_aggregate_kwargs)
+
+            assert group_info is not None
+            return_values.append(group_info)
+        except Exception as err:
+            errors.append(err)
         finally:
             transaction.get_connection(router.db_for_write(GroupHash)).close()
 
-    threads = []
-    for _ in range(CONCURRENCY):
-        thread = Thread(target=save_event)
-        thread.start()
-        threads.append(thread)
+    with patch(
+        "sentry.grouping.ingest.hashing._calculate_event_grouping",
+        return_value=hashes,
+    ):
+        with patch(
+            "sentry.event_manager._get_group_processing_kwargs",
+            return_value=group_processing_kwargs,
+        ):
+            with patch("sentry.event_manager._materialize_metadata_many"):
 
-    for thread in threads:
-        thread.join()
+                threads = []
+                for _ in range(CONCURRENCY):
+                    thread = Thread(target=save_event)
+                    thread.start()
+                    threads.append(thread)
 
-    if is_race_free:
-        # assert only one new group was created
-        assert len({group_info.group.id for group_info in return_values}) == 1
-        assert sum(group_info.is_new for group_info in return_values) == 1
-    else:
-        # assert multiple new groups were created
-        assert 1 < len({group_info.group.id for group_info in return_values}) <= CONCURRENCY
-        assert 1 < sum(group_info.is_new for group_info in return_values) <= CONCURRENCY
+                for thread in threads:
+                    thread.join()
+
+                if is_race_free:
+                    # assert only one new group was created
+                    assert len({group_info.group.id for group_info in return_values}) == 1
+                    assert sum(group_info.is_new for group_info in return_values) == 1
+                else:
+                    # assert multiple new groups were created
+                    assert (
+                        1
+                        < len({group_info.group.id for group_info in return_values})
+                        <= CONCURRENCY
+                    )
+                    assert 1 < sum(group_info.is_new for group_info in return_values) <= CONCURRENCY
