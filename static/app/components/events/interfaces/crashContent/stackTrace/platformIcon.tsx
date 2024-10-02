3537bce8b689b23500d9b5a731bdb775aebc8d48
Scott Cooper <scttcper@gmail.com>
2024-10-02 11:50:16 -0700

    fix(issues): Shrink stack trace platform icon (#78490)

--- static/app/components/events/interfaces/crashContent/stackTrace/platformIcon.tsx
+++ static/app/components/events/interfaces/crashContent/stackTrace/platformIcon.tsx
@@ -1,15 +1,18 @@
 import styled from '@emotion/styled';
 import {PlatformIcon} from 'platformicons';
 
+import {useHasStreamlinedUI} from 'sentry/views/issueDetails/utils';
+
 type Props = {
   platform: string;
 };
 
 function StacktracePlatformIcon({platform}: Props) {
+  const hasStreamlineUi = useHasStreamlinedUI();
   return (
     <StyledPlatformIcon
       platform={platform}
-      size="20px"
+      size={hasStreamlineUi ? '16px' : '20px'}
       radius={null}
       data-test-id={`platform-icon-${platform}`}
     />
@@ -19,7 +22,7 @@ function StacktracePlatformIcon({platform}: Props) {
 const StyledPlatformIcon = styled(PlatformIcon)`
   position: absolute;
   top: 0;
-  left: -20px;
+  left: -${p => p.size};
   border-radius: 3px 0 0 3px;
 
   @media (max-width: ${p => p.theme.breakpoints.medium}) {
