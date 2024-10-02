22a244ba00f8b1955ab2e7d04adff9385ab14b66
Scott Cooper <scttcper@gmail.com>
2024-10-02 11:50:36 -0700

    fix(issues): Allow the parent to align annotated text (#78484)

--- static/app/components/events/meta/annotatedText/redaction.tsx
+++ static/app/components/events/meta/annotatedText/redaction.tsx
@@ -2,6 +2,5 @@ import styled from '@emotion/styled';
 
 export const Redaction = styled('span')<{withoutBackground?: boolean}>`
   cursor: default;
-  vertical-align: middle;
   ${p => !p.withoutBackground && `background: rgba(255, 0, 0, 0.05);`}
 `;
