d134fbb103c6deba64f1c1ad747db5d47db39872
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-10-02 13:50:23 -0700

    Add processors/data_source and unit tests

--- src/sentry/workflow_engine/processors/data_source.py
+++ src/sentry/workflow_engine/processors/data_source.py
@@ -1,8 +1,31 @@
-def process_data_sources(data_sources: list):
-    # get the tuple id/type from the data_sources list
+from django.db.models import Prefetch
 
-    # query the database for the data source, and get the corresponding detectors
+from sentry.workflow_engine.models import DataPacket, DataSource, Detector
 
-    # format the data for process_detectors, a tuple of the detector + the data, we don't need the datasource anymore
-    # return the formatted data
-    return None
+
+def process_data_sources(
+    data_packets: list[DataPacket], query_type: DataSource.Type = DataSource.Type.SNUBA_QUERY
+) -> list[tuple[DataPacket, list[Detector]]]:
+    data_packet_ids = [packet.id for packet in data_packets]
+
+    # Fetch all data sources and associated detectors for the given data packets
+    data_sources = DataSource.objects.filter(
+        query_id__in=data_packet_ids, type=query_type
+    ).prefetch_related(Prefetch("datasourcedetector_set__detector", to_attr="detectors"))
+
+    # Build a lookup dict for query_id to detectors
+    query_id_to_detectors = {ds.query_id: ds.detectors for ds in data_sources}
+
+    # Create the result tuples
+    result = []
+    for packet in data_packets:
+        detectors = query_id_to_detectors.get(packet.id)
+
+        if detectors:
+            data_packet_tuple = (packet, list(detectors.all()))
+        else:
+            data_packet_tuple = (packet, [])
+
+        result.append(data_packet_tuple)
+
+    return result
