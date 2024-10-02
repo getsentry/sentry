f2c46a950193b46bae812b38a6da10972a21ecaf
Arthur Knaus <arthur.knaus@sentry.io>
2024-10-02 14:18:52 +0200

    fix(endpoint): Revert accidental code changes

--- src/sentry/web/frontend/setup_wizard.py
+++ src/sentry/web/frontend/setup_wizard.py
@@ -10,7 +10,6 @@ from django.http import Http404, HttpRequest, HttpResponse, HttpResponseBadReque
 from django.http.response import HttpResponseBase
 from django.shortcuts import get_object_or_404
 
-from sentry import features
 from sentry.api.endpoints.setup_wizard import SETUP_WIZARD_CACHE_KEY, SETUP_WIZARD_CACHE_TIMEOUT
 from sentry.api.serializers import serialize
 from sentry.api.serializers.models.project import STATUS_LABELS
@@ -122,12 +121,6 @@ class SetupWizardView(BaseView):
                 )
                 filled_projects.append(enriched_project)
 
-        if features.has("users:new-setup-wizard-ui", user=request.user, actor=request.user):
-            context["allowSelection"] = True
-            context["projects"] = filled_projects
-            context["organizations"] = list(org_mappings_map.values())
-            return render_to_response("sentry/setup-wizard.html", context, request)
-
         # Fetching or creating a token
         serialized_token = get_token(org_mappings, request.user)
 
@@ -136,7 +129,6 @@ class SetupWizardView(BaseView):
         key = f"{SETUP_WIZARD_CACHE_KEY}{wizard_hash}"
         default_cache.set(key, result, SETUP_WIZARD_CACHE_TIMEOUT)
 
-        context["projects"] = []
         context["organizations"] = list(org_mappings_map.values())
         return render_to_response("sentry/setup-wizard.html", context, request)
 
