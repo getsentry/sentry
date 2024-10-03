118c76c336553ff5d9f328b80b3a07eebde87bd8
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-10-03 14:17:39 -0400

    `ProjectApdexScoreCard`

--- static/app/views/projectDetail/projectScoreCards/projectApdexScoreCard.tsx
+++ static/app/views/projectDetail/projectScoreCards/projectApdexScoreCard.tsx
@@ -1,13 +1,8 @@
-import {Fragment} from 'react';
 import round from 'lodash/round';
 
 import {shouldFetchPreviousPeriod} from 'sentry/components/charts/utils';
-import Count from 'sentry/components/count';
-import LoadingError from 'sentry/components/loadingError';
 import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
-import ScoreCard from 'sentry/components/scoreCard';
 import {parseStatsPeriod} from 'sentry/components/timeRangeSelector/utils';
-import {IconArrow} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import type {PageFilters} from 'sentry/types/core';
 import type {Organization} from 'sentry/types/organization';
@@ -15,6 +10,8 @@ import {defined} from 'sentry/utils';
 import type {TableData} from 'sentry/utils/discover/discoverQuery';
 import {getPeriod} from 'sentry/utils/duration/getPeriod';
 import {useApiQuery} from 'sentry/utils/queryClient';
+import {BigNumberWidget} from 'sentry/views/dashboards/widgets/bigNumberWidget/bigNumberWidget';
+import {WidgetFrame} from 'sentry/views/dashboards/widgets/common/widgetFrame';
 import {getTermHelp, PerformanceTerm} from 'sentry/views/performance/data';
 
 import MissingPerformanceButtons from '../missingFeatureButtons/missingPerformanceButtons';
@@ -118,8 +115,6 @@ function ProjectApdexScoreCard(props: Props) {
       ? round(apdex - previousApdex, 3)
       : undefined;
 
-  const shouldRenderTrend = !isLoading && defined(apdex) && defined(trend);
-
   const cardTitle = t('Apdex');
 
   let cardHelp = getTermHelp(organization, PerformanceTerm.APDEX);
@@ -130,44 +125,35 @@ function ProjectApdexScoreCard(props: Props) {
 
   if (!hasTransactions || !organization.features.includes('performance-view')) {
     return (
-      <ScoreCard
-        title={cardTitle}
-        help={cardHelp}
-        score={<MissingPerformanceButtons organization={organization} />}
-      />
-    );
-  }
-
-  if (error) {
-    return (
-      <LoadingError
-        message={
-          (error.responseJSON?.detail as React.ReactNode) ||
-          t('There was an error loading data.')
-        }
-        onRetry={refetch}
-      />
+      <WidgetFrame title={cardTitle} description={cardHelp}>
+        <MissingPerformanceButtons organization={organization} />
+      </WidgetFrame>
     );
   }
 
   return (
-    <ScoreCard
+    <BigNumberWidget
       title={cardTitle}
-      help={cardHelp}
-      score={isLoading || !defined(apdex) ? '\u2014' : <Count value={apdex} />}
-      trend={
-        shouldRenderTrend ? (
-          <Fragment>
-            {trend >= 0 ? (
-              <IconArrow direction="up" size="xs" />
-            ) : (
-              <IconArrow direction="down" size="xs" />
-            )}
-            <Count value={Math.abs(trend)} />
-          </Fragment>
-        ) : null
-      }
-      trendStatus={!trend ? undefined : trend > 0 ? 'good' : 'bad'}
+      description={cardHelp}
+      data={[
+        {
+          'apdex()': apdex,
+        },
+      ]}
+      previousPeriodData={[
+        {
+          'apdex()': previousApdex,
+        },
+      ]}
+      meta={{
+        fields: {
+          'apdex()': 'number',
+        },
+      }}
+      preferredPolarity="+"
+      isLoading={isLoading}
+      error={error ?? undefined}
+      onRetry={refetch}
     />
   );
 }
