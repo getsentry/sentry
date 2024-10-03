d20c9e5c9ed59556fee6aa6aeb16b55b0f66deb3
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-10-03 11:18:03 -0400

    add view trends button

--- static/app/views/insights/database/views/databaseSpanSummaryPage.tsx
+++ static/app/views/insights/database/views/databaseSpanSummaryPage.tsx
@@ -207,11 +207,7 @@ export function DatabaseSpanSummaryPage({params}: Props) {
         </Layout.Header>
       )}
 
-      {isInDomainView && (
-        <Layout.Header>
-          <BackendHeader module={ModuleName.DB} />
-        </Layout.Header>
-      )}
+      {isInDomainView && <BackendHeader module={ModuleName.DB} />}
 
       <Layout.Body>
         <Layout.Main fullWidth>
