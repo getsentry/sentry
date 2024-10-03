215b6fca0a53757646e89cf381e63fa2190d013f
Rohit <rohit.vinnakota@sentry.io>
2024-10-03 10:58:12 -0400

    Update regex

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -3317,7 +3317,7 @@ urlpatterns = [
         name="sentry-api-catchall",
     ),
     re_path(
-        r"^organizations/(?P<organization_id_or_slug>[^/]+)/repo/(?P<repo_name>[^/]+)/pr/(?P<pull_request_number>\d+)/externalid/(?P<external_id>[^/]+)/generate-unit-tests/$",
+        r"^organizations/(?P<organization_id_or_slug>[^/]+)/repo/(?P<repo_name>[^/]+)/pr/(?P<pull_request_number>[^/]+)/externalid/(?P<external_id>[^/]+)/generate-unit-tests/$",
         AIUnitTestGenerationEndpoint.as_view(),
         name="sentry-api-0-generate-unit-tests",
     ),
