c114f1ed15c78bf72d325aea14033ed38f721ba8
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2024-10-03 14:10:56 -0700

    feat(invite-members): new invite modal design (#77528)
    changes to note:
    - added ability to copy/paste lists of emails
    - removed the "Add another" button
    - new submission flow (successfully sent emails are removed and the
    inputs are no longer disabled after submission, so users can continue to
    input emails without needing to select "Send more invites")
    - slight change to the success/error messages
    - refactored invite modal code
    
    next change will be to stop overflow of email address input (I want to
    do that in a separate PR since the `SelectControl` component is widely
    used in other places)
    
    **before:**
    
    
    https://github.com/user-attachments/assets/daf4a7bb-1e5d-4d0b-bfb7-3fbf3469cf3a
    
    **after:**
    
    
    https://github.com/user-attachments/assets/0d037342-9d11-45a8-abd8-4146599269ca

--- static/app/components/modals/inviteMembersModal/index.tsx
+++ static/app/components/modals/inviteMembersModal/index.tsx
@@ -1,14 +1,24 @@
 import {css} from '@emotion/react';
+import styled from '@emotion/styled';
 
 import type {ModalRenderProps} from 'sentry/actionCreators/modal';
 import ErrorBoundary from 'sentry/components/errorBoundary';
 import LoadingError from 'sentry/components/loadingError';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {
+  ErrorAlert,
+  InviteMessage,
+} from 'sentry/components/modals/inviteMembersModal/inviteHeaderMessages';
+import {InviteMembersContext} from 'sentry/components/modals/inviteMembersModal/inviteMembersContext';
+import InviteMembersFooter from 'sentry/components/modals/inviteMembersModal/inviteMembersFooter';
 import InviteMembersModalView from 'sentry/components/modals/inviteMembersModal/inviteMembersModalview';
+import InviteRowControl from 'sentry/components/modals/inviteMembersModal/inviteRowControlNew';
 import type {InviteRow} from 'sentry/components/modals/inviteMembersModal/types';
 import useInviteModal from 'sentry/components/modals/inviteMembersModal/useInviteModal';
 import {InviteModalHook} from 'sentry/components/modals/memberInviteModalCustomization';
+import {ORG_ROLES} from 'sentry/constants';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import {trackAnalytics} from 'sentry/utils/analytics';
 import {isActiveSuperuser} from 'sentry/utils/isActiveSuperuser';
 import useOrganization from 'sentry/utils/useOrganization';
@@ -19,6 +29,8 @@ interface InviteMembersModalProps extends ModalRenderProps {
 }
 
 function InviteMembersModal({
+  Header,
+  Body,
   closeModal,
   initialData,
   source,
@@ -37,6 +49,7 @@ function InviteMembersModal({
     setEmails,
     setRole,
     setTeams,
+    setInviteStatus,
     willInvite,
     complete,
     inviteStatus,
@@ -70,7 +83,41 @@ function InviteMembersModal({
         onSendInvites={sendInvites}
       >
         {({sendInvites: _sendInvites, canSend, headerInfo}) => {
-          return (
+          return organization.features.includes('invite-members-new-modal') ? (
+            <InviteMembersContext.Provider
+              value={{
+                willInvite,
+                invites,
+                setEmails,
+                setRole,
+                setTeams,
+                setInviteStatus,
+                sendInvites,
+                reset,
+                inviteStatus,
+                pendingInvites: pendingInvites[0],
+                sendingInvites,
+                complete,
+                error,
+              }}
+            >
+              <Header closeButton>
+                <ErrorAlert />
+                <Heading>{t('Invite New Members')}</Heading>
+              </Header>
+              <Body>
+                <InviteMessage />
+                {headerInfo}
+                <StyledInviteRow
+                  roleOptions={memberResult.data?.orgRoleList ?? ORG_ROLES}
+                  roleDisabledUnallowed={willInvite}
+                />
+              </Body>
+              <Footer>
+                <InviteMembersFooter canSend />
+              </Footer>
+            </InviteMembersContext.Provider>
+          ) : (
             <InviteMembersModalView
               addInviteRow={addInviteRow}
               canSend={canSend}
@@ -109,6 +156,17 @@ export const modalCss = css`
   width: 100%;
   max-width: 900px;
   margin: 50px auto;
+`;
+
+const Heading = styled('h1')`
+  font-weight: ${p => p.theme.fontWeightNormal};
+  font-size: ${p => p.theme.headerFontSize};
+  margin-top: 0;
+  margin-bottom: ${space(0.75)};
+`;
+
+const StyledInviteRow = styled(InviteRowControl)`
+  margin-bottom: ${space(1.5)};
 `;
 
 export default InviteMembersModal;
