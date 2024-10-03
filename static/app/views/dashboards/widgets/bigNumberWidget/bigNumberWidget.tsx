299fd67660b2d554aa7fea77208ee265f63b0d02
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-10-03 15:02:41 -0400

    feat(dashboards): Big Number Widget error state improvements (#78556)
    This is a small feature and a refactor that enables it. In short, all
    the code that handled value errors, loading states, and error states is
    lifted out of `BigNumberWidgetVisualization` into `BigNumberWidget`.
    This is a lot simpler because this way we don't have lower components
    detect errors that might affect higher components. Now the visualization
    component just visualizes good data. Everything else is higher, and/or
    moved into `WidgetFrame` so it can be shared between different widgets.
    
    Once that's done, I improved the layout of the error panel somewhat, and
    adds support for "Retry" buttons. There's also a bit of cleanup to go
    with this.

--- static/app/views/dashboards/widgets/bigNumberWidget/bigNumberWidget.tsx
+++ static/app/views/dashboards/widgets/bigNumberWidget/bigNumberWidget.tsx
@@ -1,6 +1,7 @@
 import styled from '@emotion/styled';
 
 import {space} from 'sentry/styles/space';
+import {defined} from 'sentry/utils';
 import {
   BigNumberWidgetVisualization,
   type Props as BigNumberWidgetVisualizationProps,
@@ -10,26 +11,60 @@ import {
   WidgetFrame,
 } from 'sentry/views/dashboards/widgets/common/widgetFrame';
 
+import {MISSING_DATA_MESSAGE, NON_FINITE_NUMBER_MESSAGE} from '../common/settings';
+import type {DataProps, StateProps} from '../common/types';
+
+import {DEEMPHASIS_COLOR_NAME, LOADING_PLACEHOLDER} from './settings';
+
 interface Props
-  extends Omit<WidgetFrameProps, 'children'>,
-    BigNumberWidgetVisualizationProps {}
+  extends DataProps,
+    StateProps,
+    Omit<WidgetFrameProps, 'children'>,
+    Omit<BigNumberWidgetVisualizationProps, 'value' | 'previousPeriodValue'> {}
 
 export function BigNumberWidget(props: Props) {
+  const {data, previousPeriodData} = props;
+
+  // TODO: Instrument getting more than one data key back as an error
+  // e.g., with data that looks like `[{'apdex()': 0.8}] this pulls out `"apdex()"` or `undefined`
+  const field = Object.keys(data?.[0] ?? {})[0];
+  const value = data?.[0]?.[field];
+  const previousPeriodValue = previousPeriodData?.[0]?.[field];
+
+  if (props.isLoading) {
+    return (
+      <WidgetFrame title={props.title} description={props.description}>
+        <LoadingPlaceholder>{LOADING_PLACEHOLDER}</LoadingPlaceholder>
+      </WidgetFrame>
+    );
+  }
+
+  let parsingError: string | undefined = undefined;
+
+  if (!defined(value)) {
+    parsingError = MISSING_DATA_MESSAGE;
+  } else if (!Number.isFinite(value) || Number.isNaN(value)) {
+    parsingError = NON_FINITE_NUMBER_MESSAGE;
+  }
+
+  const error = props.error ?? parsingError;
+
   return (
     <WidgetFrame
       title={props.title}
       description={props.description}
       actions={props.actions}
+      error={error}
+      onRetry={props.onRetry}
     >
       <BigNumberResizeWrapper>
         <BigNumberWidgetVisualization
-          data={props.data}
-          previousPeriodData={props.previousPeriodData}
+          value={Number(value)}
+          previousPeriodValue={Number(previousPeriodValue)}
+          field={field}
           maximumValue={props.maximumValue}
           preferredPolarity={props.preferredPolarity}
           meta={props.meta}
-          isLoading={props.isLoading}
-          error={props.error}
         />
       </BigNumberResizeWrapper>
     </WidgetFrame>
@@ -40,4 +75,9 @@ const BigNumberResizeWrapper = styled('div')`
   position: relative;
   flex-grow: 1;
   margin-top: ${space(1)};
+`;
+
+const LoadingPlaceholder = styled('span')`
+  color: ${p => p.theme[DEEMPHASIS_COLOR_NAME]};
+  font-size: ${p => p.theme.fontSizeLarge};
 `;
