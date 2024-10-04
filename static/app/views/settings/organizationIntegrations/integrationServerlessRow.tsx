3d57d4e78e483318070cc8b494b22993832bed3e
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-10-04 16:29:23 -0400

    ref(serverless): Convert IntegrationServerlessRow to FC (#78643)
    Requires: https://github.com/getsentry/sentry/pull/78641
    
    Wasn't using async component but still could use some love.

--- static/app/views/settings/organizationIntegrations/integrationServerlessRow.tsx
+++ static/app/views/settings/organizationIntegrations/integrationServerlessRow.tsx
@@ -1,4 +1,4 @@
-import {Component, Fragment} from 'react';
+import {Fragment, useCallback, useMemo, useState} from 'react';
 import styled from '@emotion/styled';
 
 import {
@@ -6,7 +6,6 @@ import {
   addLoadingMessage,
   addSuccessMessage,
 } from 'sentry/actionCreators/indicator';
-import type {Client} from 'sentry/api';
 import {Button} from 'sentry/components/button';
 import Switch from 'sentry/components/switchButton';
 import {t} from 'sentry/locale';
@@ -15,136 +14,130 @@ import type {
   OrganizationIntegration,
   ServerlessFunction,
 } from 'sentry/types/integrations';
-import type {Organization} from 'sentry/types/organization';
-import {trackIntegrationAnalytics} from 'sentry/utils/integrationUtil';
-import withApi from 'sentry/utils/withApi';
+import {trackAnalytics} from 'sentry/utils/analytics';
+import useApi from 'sentry/utils/useApi';
+import useOrganization from 'sentry/utils/useOrganization';
 
-type Props = {
-  api: Client;
+interface IntegrationServerlessRowProps {
   integration: OrganizationIntegration;
-  onUpdateFunction: (serverlessFunctionUpdate: Partial<ServerlessFunction>) => void;
-  organization: Organization;
+  onUpdate: (serverlessFunctionUpdate: Partial<ServerlessFunction>) => void;
   serverlessFunction: ServerlessFunction;
-};
+}
 
-type State = {
-  submitting: boolean;
-};
+export function IntegrationServerlessRow({
+  integration,
+  onUpdate,
+  serverlessFunction,
+}: IntegrationServerlessRowProps) {
+  const api = useApi();
+  const organization = useOrganization();
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const endpoint = `/organizations/${organization.slug}/integrations/${integration.id}/serverless-functions/`;
 
-class IntegrationServerlessRow extends Component<Props, State> {
-  state: State = {
-    submitting: false,
-  };
-  get enabled() {
-    return this.props.serverlessFunction.enabled;
-  }
-  get endpoint() {
-    const orgSlug = this.props.organization.slug;
-    return `/organizations/${orgSlug}/integrations/${this.props.integration.id}/serverless-functions/`;
-  }
+  const {version} = serverlessFunction;
+  // during optimistic update, we might be enabled without a version
+  const versionText =
+    serverlessFunction.enabled && version > 0 ? (
+      <Fragment>&nbsp;|&nbsp;v{version}</Fragment>
+    ) : null;
 
-  recordAction = (action: 'enable' | 'disable' | 'updateVersion') => {
-    trackIntegrationAnalytics('integrations.serverless_function_action', {
-      integration: this.props.integration.provider.key,
-      integration_type: 'first_party',
-      action,
-      organization: this.props.organization,
-    });
-  };
-  toggleEnable = async () => {
-    const {serverlessFunction} = this.props;
-    const action = this.enabled ? 'disable' : 'enable';
-    const data = {
-      action,
-      target: serverlessFunction.name,
-    };
-    try {
-      addLoadingMessage();
-      this.setState({submitting: true});
-      // optimistically update enable state
-      this.props.onUpdateFunction({enabled: !this.enabled});
-      this.recordAction(action);
-      const resp = await this.props.api.requestPromise(this.endpoint, {
-        method: 'POST',
-        data,
+  const recordAction = useCallback(
+    (action: 'enable' | 'disable' | 'updateVersion') => {
+      trackAnalytics('integrations.serverless_function_action', {
+        integration: integration.provider.key,
+        integration_type: 'first_party',
+        action,
+        organization,
       });
-      // update remaining after response
-      this.props.onUpdateFunction(resp);
-      addSuccessMessage(t('Success'));
-    } catch (err) {
-      // restore original on failure
-      this.props.onUpdateFunction(serverlessFunction);
-      addErrorMessage(err.responseJSON?.detail ?? t('Error occurred'));
-    }
-    this.setState({submitting: false});
-  };
-  updateVersion = async () => {
-    const {serverlessFunction} = this.props;
+    },
+    [integration.provider.key, organization]
+  );
+
+  const handleUpdate = useCallback(async () => {
     const data = {
       action: 'updateVersion',
       target: serverlessFunction.name,
     };
     try {
-      this.setState({submitting: true});
+      setIsSubmitting(true);
       // don't know the latest version but at least optimistically remove the update button
-      this.props.onUpdateFunction({outOfDate: false});
+      onUpdate({outOfDate: false});
       addLoadingMessage();
-      this.recordAction('updateVersion');
-      const resp = await this.props.api.requestPromise(this.endpoint, {
+      recordAction('updateVersion');
+      const resp = await api.requestPromise(endpoint, {
         method: 'POST',
         data,
       });
       // update remaining after response
-      this.props.onUpdateFunction(resp);
+      onUpdate(resp);
       addSuccessMessage(t('Success'));
     } catch (err) {
       // restore original on failure
-      this.props.onUpdateFunction(serverlessFunction);
+      onUpdate(serverlessFunction);
       addErrorMessage(err.responseJSON?.detail ?? t('Error occurred'));
     }
-    this.setState({submitting: false});
-  };
-  renderLayerStatus() {
-    const {serverlessFunction} = this.props;
+    setIsSubmitting(false);
+  }, [api, endpoint, onUpdate, recordAction, serverlessFunction]);
+
+  const handleToggle = useCallback(async () => {
+    const action = serverlessFunction.enabled ? 'disable' : 'enable';
+    const data = {
+      action,
+      target: serverlessFunction.name,
+    };
+    try {
+      addLoadingMessage();
+      setIsSubmitting(true);
+      // optimistically update enable state
+      onUpdate({enabled: !serverlessFunction.enabled});
+      recordAction(action);
+      const resp = await api.requestPromise(endpoint, {
+        method: 'POST',
+        data,
+      });
+      // update remaining after response
+      onUpdate(resp);
+      addSuccessMessage(t('Success'));
+    } catch (err) {
+      // restore original on failure
+      onUpdate(serverlessFunction);
+      addErrorMessage(err.responseJSON?.detail ?? t('Error occurred'));
+    }
+    setIsSubmitting(false);
+  }, [api, endpoint, onUpdate, recordAction, serverlessFunction]);
+
+  const layerStatus = useMemo(() => {
     if (!serverlessFunction.outOfDate) {
-      return this.enabled ? t('Latest') : t('Disabled');
+      return serverlessFunction.enabled ? t('Latest') : t('Disabled');
     }
     return (
-      <UpdateButton size="sm" priority="primary" onClick={this.updateVersion}>
+      <UpdateButton size="sm" priority="primary" onClick={handleUpdate}>
         {t('Update')}
       </UpdateButton>
     );
-  }
-  render() {
-    const {serverlessFunction} = this.props;
-    const {version} = serverlessFunction;
-    // during optimistic update, we might be enabled without a version
-    const versionText =
-      this.enabled && version > 0 ? <Fragment>&nbsp;|&nbsp;v{version}</Fragment> : null;
-    return (
-      <Item>
-        <NameWrapper>
-          <NameRuntimeVersionWrapper>
-            <Name>{serverlessFunction.name}</Name>
-            <RuntimeAndVersion>
-              <DetailWrapper>{serverlessFunction.runtime}</DetailWrapper>
-              <DetailWrapper>{versionText}</DetailWrapper>
-            </RuntimeAndVersion>
-          </NameRuntimeVersionWrapper>
-        </NameWrapper>
-        <LayerStatusWrapper>{this.renderLayerStatus()}</LayerStatusWrapper>
-        <StyledSwitch
-          isActive={this.enabled}
-          isDisabled={this.state.submitting}
-          size="sm"
-          toggle={this.toggleEnable}
-        />
-      </Item>
-    );
-  }
-}
+  }, [serverlessFunction.outOfDate, serverlessFunction.enabled, handleUpdate]);
 
-export default withApi(IntegrationServerlessRow);
+  return (
+    <Item>
+      <NameWrapper>
+        <NameRuntimeVersionWrapper>
+          <Name>{serverlessFunction.name}</Name>
+          <RuntimeAndVersion>
+            <DetailWrapper>{serverlessFunction.runtime}</DetailWrapper>
+            <DetailWrapper>{versionText}</DetailWrapper>
+          </RuntimeAndVersion>
+        </NameRuntimeVersionWrapper>
+      </NameWrapper>
+      <LayerStatusWrapper>{layerStatus}</LayerStatusWrapper>
+      <StyledSwitch
+        isActive={serverlessFunction.enabled}
+        isDisabled={isSubmitting}
+        size="sm"
+        toggle={handleToggle}
+      />
+    </Item>
+  );
+}
 
 const Item = styled('div')`
   padding: ${space(2)};
