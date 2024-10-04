76398cee9159fa35b4b2a1bc865c7ea76a48be4d
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-10-04 09:46:41 -0400

    ref(dashboards): Replace Project Details score cards with `BigNumberWidget` (#78566)
    Replaces usage of `ProjectCard` in project details pages with
    [`BigNumberWidget`](https://sentry.sentry.io/stories/?name=app/views/dashboards/widgets/bigNumberWidget/bigNumberWidget.stories.tsx).
    A small refactor, to clean stuff up and try that component out. A few
    immediate wins:
    
    - consistent rounding and truncation
    - a little less code duplication
    - uncovered at least one bug
    - nicer loading and error states
    - full value on hover
    
    Closes https://github.com/getsentry/sentry/issues/77780

--- static/app/views/projectDetail/projectScoreCards/projectApdexScoreCard.tsx
+++ static/app/views/projectDetail/projectScoreCards/projectApdexScoreCard.tsx
@@ -1,20 +1,14 @@
-import {Fragment} from 'react';
-import round from 'lodash/round';
-
 import {shouldFetchPreviousPeriod} from 'sentry/components/charts/utils';
-import Count from 'sentry/components/count';
-import LoadingError from 'sentry/components/loadingError';
 import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
-import ScoreCard from 'sentry/components/scoreCard';
 import {parseStatsPeriod} from 'sentry/components/timeRangeSelector/utils';
-import {IconArrow} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import type {PageFilters} from 'sentry/types/core';
 import type {Organization} from 'sentry/types/organization';
-import {defined} from 'sentry/utils';
 import type {TableData} from 'sentry/utils/discover/discoverQuery';
 import {getPeriod} from 'sentry/utils/duration/getPeriod';
 import {useApiQuery} from 'sentry/utils/queryClient';
+import {BigNumberWidget} from 'sentry/views/dashboards/widgets/bigNumberWidget/bigNumberWidget';
+import {WidgetFrame} from 'sentry/views/dashboards/widgets/common/widgetFrame';
 import {getTermHelp, PerformanceTerm} from 'sentry/views/performance/data';
 
 import MissingPerformanceButtons from '../missingFeatureButtons/missingPerformanceButtons';
@@ -113,61 +107,41 @@ function ProjectApdexScoreCard(props: Props) {
 
   const previousApdex = Number(previousData?.data?.[0]?.['apdex()']) || undefined;
 
-  const trend =
-    defined(apdex) && defined(previousApdex)
-      ? round(apdex - previousApdex, 3)
-      : undefined;
-
-  const shouldRenderTrend = !isLoading && defined(apdex) && defined(trend);
-
   const cardTitle = t('Apdex');
 
-  let cardHelp = getTermHelp(organization, PerformanceTerm.APDEX);
-
-  if (trend) {
-    cardHelp += t(' This shows how it has changed since the last period.');
-  }
+  const cardHelp = getTermHelp(organization, PerformanceTerm.APDEX);
 
   if (!hasTransactions || !organization.features.includes('performance-view')) {
     return (
-      <ScoreCard
-        title={cardTitle}
-        help={cardHelp}
-        score={<MissingPerformanceButtons organization={organization} />}
-      />
-    );
-  }
-
-  if (error) {
-    return (
-      <LoadingError
-        message={
-          (error.responseJSON?.detail as React.ReactNode) ||
-          t('There was an error loading data.')
-        }
-        onRetry={refetch}
-      />
+      <WidgetFrame title={cardTitle} description={cardHelp}>
+        <MissingPerformanceButtons organization={organization} />
+      </WidgetFrame>
     );
   }
 
   return (
-    <ScoreCard
+    <BigNumberWidget
       title={cardTitle}
-      help={cardHelp}
-      score={isLoading || !defined(apdex) ? '\u2014' : <Count value={apdex} />}
-      trend={
-        shouldRenderTrend ? (
-          <Fragment>
-            {trend >= 0 ? (
-              <IconArrow direction="up" size="xs" />
-            ) : (
-              <IconArrow direction="down" size="xs" />
-            )}
-            <Count value={Math.abs(trend)} />
-          </Fragment>
-        ) : null
-      }
-      trendStatus={!trend ? undefined : trend > 0 ? 'good' : 'bad'}
+      description={cardHelp}
+      data={[
+        {
+          'apdex()': apdex,
+        },
+      ]}
+      previousPeriodData={[
+        {
+          'apdex()': previousApdex,
+        },
+      ]}
+      meta={{
+        fields: {
+          'apdex()': 'number',
+        },
+      }}
+      preferredPolarity="+"
+      isLoading={isLoading}
+      error={error ?? undefined}
+      onRetry={refetch}
     />
   );
 }
