ba18fc10f531596cd55d4e4dde713b49bd87fabb
Colton Allen <colton.allen@sentry.io>
2024-10-04 12:20:06 -0500

    Register audit log presenter

--- src/sentry/runner/commands/presenters/presenterdelegator.py
+++ src/sentry/runner/commands/presenters/presenterdelegator.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from sentry.runner.commands.presenters.audit_log_presenter import AuditLogPresenter
 from sentry.runner.commands.presenters.consolepresenter import ConsolePresenter
 from sentry.runner.commands.presenters.webhookpresenter import WebhookPresenter
 
@@ -11,11 +12,15 @@ class PresenterDelegator:
         self._slackpresenter = None
         if WebhookPresenter.is_webhook_enabled():
             self._slackpresenter = WebhookPresenter(source)
+        if AuditLogPresenter.is_webhook_enabled():
+            self._auditlogpresenter = AuditLogPresenter(source)
 
     def __getattr__(self, attr: str) -> Any:
         def wrapper(*args: Any, **kwargs: Any) -> None:
             getattr(self._consolepresenter, attr)(*args, **kwargs)
             if self._slackpresenter:
                 getattr(self._slackpresenter, attr)(*args, **kwargs)
+            if self._auditlogpresenter:
+                getattr(self._auditlogpresenter, attr)(*args, **kwargs)
 
         return wrapper
