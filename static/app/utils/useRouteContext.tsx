3ac0af0a8b72ef322c79a537ddef5bf5b4877fbe
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-10-07 16:19:46 -0400

    feat(rr6): Add an option to disableRouterMocks (#78715)
    Allows us to write tests that actually use the routers functionality

--- static/app/utils/useRouteContext.tsx
+++ static/app/utils/useRouteContext.tsx
@@ -3,9 +3,5 @@ import {useContext} from 'react';
 import {RouteContext} from 'sentry/views/routeContext';
 
 export function useRouteContext() {
-  const route = useContext(RouteContext);
-  if (route === null) {
-    throw new Error(`useRouteContext called outside of routes provider`);
-  }
-  return route;
+  return useContext(RouteContext);
 }
