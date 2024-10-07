7fe68af0b40800e0a8b81b98063934b802a4f640
Colton Allen <colton.allen@sentry.io>
2024-10-07 11:12:22 -0500

    fix(replays): Un-revert "Store options changes in the audit log" (#78678)

--- src/sentry/runner/commands/presenters/presenterdelegator.py
+++ src/sentry/runner/commands/presenters/presenterdelegator.py
@@ -6,16 +6,22 @@ from sentry.runner.commands.presenters.webhookpresenter import WebhookPresenter
 
 class PresenterDelegator:
     def __init__(self, source: str) -> None:
+        from sentry.runner.commands.presenters.audit_log_presenter import AuditLogPresenter
+
         self._consolepresenter = ConsolePresenter()
 
         self._slackpresenter = None
         if WebhookPresenter.is_webhook_enabled():
             self._slackpresenter = WebhookPresenter(source)
+        if AuditLogPresenter.is_webhook_enabled():
+            self._auditlogpresenter = AuditLogPresenter(source)
 
     def __getattr__(self, attr: str) -> Any:
         def wrapper(*args: Any, **kwargs: Any) -> None:
             getattr(self._consolepresenter, attr)(*args, **kwargs)
             if self._slackpresenter:
                 getattr(self._slackpresenter, attr)(*args, **kwargs)
+            if self._auditlogpresenter:
+                getattr(self._auditlogpresenter, attr)(*args, **kwargs)
 
         return wrapper
