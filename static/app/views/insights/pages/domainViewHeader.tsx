308b96be17d344025ad341b4ec3f90db04111a98
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2024-10-07 14:18:43 -0400

    ref(insights): create and use shared domain view header (#78640)

---
+++ static/app/views/insights/pages/domainViewHeader.tsx
@@ -0,0 +1,121 @@
+import {Fragment} from 'react';
+
+import {Breadcrumbs, type Crumb} from 'sentry/components/breadcrumbs';
+import ButtonBar from 'sentry/components/buttonBar';
+import FeedbackWidgetButton from 'sentry/components/feedback/widget/feedbackWidgetButton';
+import * as Layout from 'sentry/components/layouts/thirds';
+import {TabList, Tabs} from 'sentry/components/tabs';
+import {t} from 'sentry/locale';
+import {useNavigate} from 'sentry/utils/useNavigate';
+import {
+  type RoutableModuleNames,
+  useModuleURLBuilder,
+} from 'sentry/views/insights/common/utils/useModuleURL';
+import {OVERVIEW_PAGE_TITLE} from 'sentry/views/insights/pages/settings';
+import {MODULE_TITLES} from 'sentry/views/insights/settings';
+import type {ModuleName} from 'sentry/views/insights/types';
+
+type Props = {
+  domainBaseUrl: string;
+  headerTitle: React.ReactNode;
+  modules: ModuleName[];
+  selectedModule: ModuleName | undefined;
+  additionalBreadCrumbs?: Crumb[];
+  additonalHeaderActions?: React.ReactNode;
+  hideDefaultTabs?: boolean;
+  tabs?: {onTabChange: (key: string) => void; tabList: React.ReactNode; value: string};
+};
+
+type Tab = {
+  key: string;
+  label: string;
+};
+
+export function DomainViewHeader({
+  modules,
+  headerTitle,
+  selectedModule,
+  hideDefaultTabs,
+  additonalHeaderActions,
+  additionalBreadCrumbs = [],
+  domainBaseUrl,
+  tabs,
+}: Props) {
+  const navigate = useNavigate();
+  const moduleURLBuilder = useModuleURLBuilder();
+
+  const baseCrumbs: Crumb[] = [
+    {
+      label: t('Performance'),
+      to: undefined, // There is no base /performance/ page
+      preservePageFilters: true,
+    },
+    {
+      label: headerTitle,
+      to: domainBaseUrl,
+      preservePageFilters: true,
+    },
+    {
+      label: selectedModule ? MODULE_TITLES[selectedModule] : OVERVIEW_PAGE_TITLE,
+      to: `${moduleURLBuilder(selectedModule as RoutableModuleNames)}/`,
+      preservePageFilters: true,
+    },
+    ...additionalBreadCrumbs,
+  ];
+
+  const defaultHandleTabChange = (key: ModuleName | typeof OVERVIEW_PAGE_TITLE) => {
+    if (key === selectedModule || (key === OVERVIEW_PAGE_TITLE && !module)) {
+      return;
+    }
+    if (!key) {
+      return;
+    }
+    if (key === OVERVIEW_PAGE_TITLE) {
+      navigate(domainBaseUrl);
+      return;
+    }
+    navigate(`${moduleURLBuilder(key as RoutableModuleNames)}/`);
+  };
+
+  const tabValue =
+    hideDefaultTabs && tabs?.value ? tabs.value : selectedModule ?? OVERVIEW_PAGE_TITLE;
+
+  const handleTabChange =
+    hideDefaultTabs && tabs ? tabs.onTabChange : defaultHandleTabChange;
+
+  const tabList: Tab[] = [
+    {
+      key: OVERVIEW_PAGE_TITLE,
+      label: OVERVIEW_PAGE_TITLE,
+    },
+    ...modules.map(moduleName => ({key: moduleName, label: MODULE_TITLES[moduleName]})),
+  ];
+
+  return (
+    <Fragment>
+      <Tabs value={tabValue} onChange={handleTabChange}>
+        <Layout.Header>
+          <Layout.HeaderContent>
+            <Breadcrumbs crumbs={baseCrumbs} />
+
+            <Layout.Title>{headerTitle}</Layout.Title>
+          </Layout.HeaderContent>
+          <Layout.HeaderActions>
+            <ButtonBar gap={1}>
+              {additonalHeaderActions}
+              <FeedbackWidgetButton />
+            </ButtonBar>
+          </Layout.HeaderActions>
+          {!hideDefaultTabs && (
+            <TabList hideBorder>
+              {tabList.map(tab => (
+                <TabList.Item key={tab.key}>{tab.label}</TabList.Item>
+              ))}
+            </TabList>
+          )}
+          {hideDefaultTabs && tabs && tabs.tabList}
+        </Layout.Header>
+      </Tabs>
+    </Fragment>
+  );
+}
--- static/app/views/insights/pages/mobile/mobilePageHeader.tsx
+++ static/app/views/insights/pages/mobile/mobilePageHeader.tsx
@@ -1,27 +1,11 @@
-import {Fragment} from 'react';
-
-import {Breadcrumbs, type Crumb} from 'sentry/components/breadcrumbs';
-import ButtonBar from 'sentry/components/buttonBar';
-import FeedbackWidgetButton from 'sentry/components/feedback/widget/feedbackWidgetButton';
-import * as Layout from 'sentry/components/layouts/thirds';
-import {TabList, Tabs} from 'sentry/components/tabs';
-import {t} from 'sentry/locale';
 import normalizeUrl from 'sentry/utils/url/normalizeUrl';
-import {useNavigate} from 'sentry/utils/useNavigate';
 import useOrganization from 'sentry/utils/useOrganization';
-import {
-  type RoutableModuleNames,
-  useModuleURLBuilder,
-} from 'sentry/views/insights/common/utils/useModuleURL';
+import {DomainViewHeader} from 'sentry/views/insights/pages/domainViewHeader';
 import {
   MOBILE_LANDING_SUB_PATH,
   MOBILE_LANDING_TITLE,
 } from 'sentry/views/insights/pages/mobile/settings';
-import {
-  DOMAIN_VIEW_BASE_URL,
-  OVERVIEW_PAGE_TITLE,
-} from 'sentry/views/insights/pages/settings';
-import {MODULE_TITLES} from 'sentry/views/insights/settings';
+import {DOMAIN_VIEW_BASE_URL} from 'sentry/views/insights/pages/settings';
 import {ModuleName} from 'sentry/views/insights/types';
 
 type Props = {
@@ -33,78 +17,23 @@ type Props = {
 
 // TODO - add props to append to breadcrumbs and change title
 export function MobileHeader({module, hideDefaultTabs, headerActions, tabs}: Props) {
-  const navigate = useNavigate();
   const {slug} = useOrganization();
-  const moduleURLBuilder = useModuleURLBuilder();
 
   const mobileBaseUrl = normalizeUrl(
     `/organizations/${slug}/${DOMAIN_VIEW_BASE_URL}/${MOBILE_LANDING_SUB_PATH}/`
   );
 
-  const crumbs: Crumb[] = [
-    {
-      label: t('Performance'),
-      to: '/performance', // There is no page at `/insights/` so there is nothing to link to
-      preservePageFilters: true,
-    },
-    {
-      label: MOBILE_LANDING_TITLE,
-      to: mobileBaseUrl,
-      preservePageFilters: true,
-    },
-    {
-      label: module ? MODULE_TITLES[module] : OVERVIEW_PAGE_TITLE,
-      to: undefined,
-      preservePageFilters: true,
-    },
-  ];
-
-  const defaultHandleTabChange = (key: ModuleName | typeof OVERVIEW_PAGE_TITLE) => {
-    if (key === module || (key === OVERVIEW_PAGE_TITLE && !module)) {
-      return;
-    }
-    if (!key) {
-      return;
-    }
-    if (key === OVERVIEW_PAGE_TITLE) {
-      navigate(mobileBaseUrl);
-      return;
-    }
-    navigate(`${moduleURLBuilder(key as RoutableModuleNames)}/`);
-  };
-
-  const tabValue =
-    hideDefaultTabs && tabs?.value ? tabs.value : module ?? OVERVIEW_PAGE_TITLE;
-
-  const handleTabChange =
-    hideDefaultTabs && tabs ? tabs.onTabChange : defaultHandleTabChange;
+  const modules = [ModuleName.MOBILE_SCREENS];
 
   return (
-    <Fragment>
-      <Tabs value={tabValue} onChange={handleTabChange}>
-        <Layout.Header>
-          <Layout.HeaderContent>
-            <Breadcrumbs crumbs={crumbs} />
-
-            <Layout.Title>{MOBILE_LANDING_TITLE}</Layout.Title>
-          </Layout.HeaderContent>
-          <Layout.HeaderActions>
-            <ButtonBar gap={1}>
-              {headerActions}
-              <FeedbackWidgetButton />
-            </ButtonBar>
-          </Layout.HeaderActions>
-          {!hideDefaultTabs && (
-            <TabList hideBorder>
-              <TabList.Item key={OVERVIEW_PAGE_TITLE}>{OVERVIEW_PAGE_TITLE}</TabList.Item>
-              <TabList.Item key={ModuleName.MOBILE_SCREENS}>
-                {MODULE_TITLES[ModuleName.MOBILE_SCREENS]}
-              </TabList.Item>
-            </TabList>
-          )}
-          {hideDefaultTabs && tabs && tabs.tabList}
-        </Layout.Header>
-      </Tabs>
-    </Fragment>
+    <DomainViewHeader
+      domainBaseUrl={mobileBaseUrl}
+      headerTitle={MOBILE_LANDING_TITLE}
+      modules={modules}
+      selectedModule={module}
+      tabs={tabs}
+      hideDefaultTabs={hideDefaultTabs}
+      additonalHeaderActions={headerActions}
+    />
   );
 }
