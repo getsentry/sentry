f59224e9bd0ac99fd1728c0eb681b01605a7d5aa
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-10-07 10:35:47 -0700

    fix(toolbar): Fix incorrect headers, and auth passing templates

--- src/sentry/toolbar/views/iframe_view.py
+++ src/sentry/toolbar/views/iframe_view.py
@@ -1,74 +1,91 @@
 from typing import Any
 
-from django.http import HttpRequest, HttpResponse
+from django.http import HttpRequest
 
 from sentry.models.organization import Organization
 from sentry.models.project import Project
-from sentry.toolbar.utils.url import check_origin
-from sentry.web.frontend.base import OrganizationView, region_silo_view
+from sentry.toolbar.utils.url import is_origin_allowed
+from sentry.web.frontend.base import ProjectView, region_silo_view
 
-REFERRER_HEADER = "HTTP_REFERER"  # 1 R is the spelling used here: https://docs.djangoproject.com/en/5.1/ref/request-response/
-SUCCESS_TEMPLATE = "sentry/toolbar/iframe.html"
-INVALID_TEMPLATE = "sentry/toolbar/iframe-invalid.html"
+TEMPLATE = "sentry/toolbar/iframe.html"
+
+
+def _get_referrer(request) -> str | None:
+    # 1 R is because of legacy http reasons: https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referer
+    return request.META.get("HTTP_REFERER")
 
 
 @region_silo_view
-class IframeView(OrganizationView):
-    def respond(self, template: str, context: dict[str, Any] | None = None, status: int = 200):
+class IframeView(ProjectView):
+    def respond(self, template: str, context: dict[str, Any], status: int = 200):
+        self.default_context = {}  # self.get_context_data(request, *args, **kwargs)
+
+        referrer = context.get("referrer") or "'none'"
         response = super().respond(template, context=context, status=status)
-        # These HTTP headers allow response to be embedded in an iframe.
-        response["X-Frame-Options"] = "ALLOWALL"
-        response._csp_replace = {  # type: ignore[attr-defined]
-            # This is an alternative to @csp_replace - we need to use this pattern to access the referrer.
-            "frame-ancestors": [self.request.META.get(REFERRER_HEADER, "'none'")]
-        }
+
+        # This is an alternative to @csp_replace - we need to use this pattern to access the referrer.
+        response._csp_replace = {"frame-ancestors": [referrer.strip("/")]}  # type: ignore[attr-defined]
+        response["X-Frame-Options"] = "DENY" if referrer == "'none'" else "ALLOWALL"
+
         return response
 
+    def handle_disabled_member(self, request: HttpRequest, *args, **kwargs):
+        return self._handle_logged_out(request, *args, **kwargs)
+
+    def handle_not_2fa_compliant(self, request: HttpRequest, *args, **kwargs):
+        return self._handle_logged_out(request, *args, **kwargs)
+
+    def handle_sudo_required(self, request: HttpRequest, *args, **kwargs):
+        return self._handle_logged_out(request, *args, **kwargs)
+
     def handle_auth_required(self, request: HttpRequest, *args, **kwargs):
-        # Override redirects to /auth/login
-        return HttpResponse(status=401)
+        return self._handle_logged_out(request, *args, **kwargs)
+
+    def _handle_logged_out(self, request: HttpRequest, *args, **kwargs):
+        return self.respond(
+            TEMPLATE,
+            status=200,  # 401,
+            context={
+                "referrer": _get_referrer(request),
+                "state": "logged-out",
+                "logging": request.GET.get("logging", ""),
+            },
+        )
 
     def handle_permission_required(self, request: HttpRequest, *args, **kwargs):
-        # Override redirects to /auth/login
-        return HttpResponse(status=403)
-
-    def convert_args(self, request: HttpRequest, organization_slug: str, project_id_or_slug: int | str, *args: Any, **kwargs: Any) -> tuple[tuple[Any, ...], dict[str, Any]]:  # type: ignore[override]
-        args, kwargs = super().convert_args(request, organization_slug, *args, **kwargs)
-        organization: Organization | None = kwargs["organization"]
-        active_project: Project | None = (
-            self.get_active_project(
-                request=request,
-                organization=organization,  # type: ignore[arg-type]
-                project_id_or_slug=project_id_or_slug,
-            )
-            if organization
-            else None
+        return self.respond(
+            TEMPLATE,
+            status=200,  # 404,
+            context={
+                "referrer": _get_referrer(request),
+                "state": "missing-project",
+                "logging": request.GET.get("logging", ""),
+            },
         )
-        kwargs["project"] = active_project
-        return args, kwargs
 
     def get(
         self, request: HttpRequest, organization: Organization, project: Project, *args, **kwargs
     ):
-        referrer = request.META.get(REFERRER_HEADER)
-        if not project:
-            return self.respond(
-                INVALID_TEMPLATE,
-                status=404,
-                context={"referrer": referrer, "has_project": False},
-            )
-
+        referrer = _get_referrer(request)
         allowed_origins: list[str] = project.get_option("sentry:toolbar_allowed_origins")
-        origin_allowed, info_msg = check_origin(referrer, allowed_origins)
-        if not origin_allowed:
+
+        if referrer and is_origin_allowed(referrer, allowed_origins):
             return self.respond(
-                INVALID_TEMPLATE,
-                status=403,
+                TEMPLATE,
+                status=200,
                 context={
                     "referrer": referrer,
-                    "has_project": True,
-                    "allow_origin": False,
+                    "state": "success",
+                    "logging": request.GET.get("logging", ""),
                 },
             )
 
-        return self.respond(SUCCESS_TEMPLATE, status=200, context={"referrer": referrer})
+        return self.respond(
+            TEMPLATE,
+            status=200,  # 403,
+            context={
+                "referrer": referrer,
+                "state": "invalid-domain",
+                "logging": request.GET.get("logging", ""),
+            },
+        )
--- tests/sentry/toolbar/utils/test_url.py
+++ tests/sentry/toolbar/utils/test_url.py
@@ -1,29 +1,53 @@
-from sentry.toolbar.utils.url import url_matches
+from urllib.parse import urlparse
+
+from sentry.toolbar.utils.url import is_origin_allowed, url_matches
 
 
 def test_url_matches_basic():
-    assert url_matches("http://abc.net", "http://abc.net")
-    assert not url_matches("http://pocketpair.io", "http://sentry.io")
-    assert url_matches("http://sentry.io/issues/", "http://sentry.io")
-    assert url_matches("http://sentry.io", "http://sentry.io/issues/")
-    assert not url_matches("https://cmu.edu", "https://cmu.com")
-    assert url_matches("https://youtube.com:443/watch?v=3xhb", "https://youtube.com/profile")
+    assert url_matches(urlparse("http://abc.net"), "http://abc.net")
+    assert not url_matches(urlparse("http://pocketpair.io"), "http://sentry.io")
+    assert url_matches(urlparse("http://sentry.io/issues/"), "http://sentry.io")
+    assert url_matches(urlparse("http://sentry.io"), "http://sentry.io/issues/")
+    assert not url_matches(urlparse("https://cmu.edu"), "https://cmu.com")
+    assert url_matches(
+        urlparse("https://youtube.com:443/watch?v=3xhb"), "https://youtube.com/profile"
+    )
 
 
 def test_url_matches_wildcard():
-    assert url_matches("https://nugettrends.sentry.io", "https://*.sentry.io")
-    assert not url_matches("https://nugettrends.sentry.com", "https://*.sentry.io")
-    assert not url_matches("https://nugettrends.sentry.io", "https://*.io")
-    assert not url_matches("https://sentry.io", "https://*.sentry.io")
+    assert url_matches(urlparse("https://nugettrends.sentry.io"), "https://*.sentry.io")
+    assert not url_matches(urlparse("https://nugettrends.sentry.com"), "https://*.sentry.io")
+    assert not url_matches(urlparse("https://nugettrends.sentry.io"), "https://*.io")
+    assert not url_matches(urlparse("https://sentry.io"), "https://*.sentry.io")
 
 
 def test_url_matches_port():
-    assert url_matches("https://sentry.io:42", "https://sentry.io:42")
-    assert not url_matches("https://sentry.io", "https://sentry.io:42")
-    assert url_matches("https://sentry.io:42", "https://sentry.io")
+    assert url_matches(urlparse("https://sentry.io:42"), "https://sentry.io:42")
+    assert not url_matches(urlparse("https://sentry.io"), "https://sentry.io:42")
+    assert url_matches(urlparse("https://sentry.io:42"), "https://sentry.io")
 
 
 def test_url_matches_scheme():
-    assert url_matches("https://sentry.io", "sentry.io")
-    assert url_matches("http://sentry.io", "sentry.io")
-    assert not url_matches("http://sentry.io", "https://sentry.io")
+    assert url_matches(urlparse("https://sentry.io"), "sentry.io")
+    assert url_matches(urlparse("http://sentry.io"), "sentry.io")
+    assert not url_matches(urlparse("http://sentry.io"), "https://sentry.io")
+
+
+def test_url_is_empty():
+    assert not url_matches(urlparse(""), "sentry.io")
+
+
+def test_is_origin_allowed_allows_some():
+    assert is_origin_allowed("http://sentry.io", ["http://abc.net", "http://sentry.io"])
+
+
+def test_is_origin_allowed_rejects_all():
+    assert not is_origin_allowed("http://sentry.io", ["http://abc.net", "http://xyz.net"])
+
+
+def test_is_origin_allowed_rejects_empty():
+    assert not is_origin_allowed("", ["http://abc.net", "http://xyz.net"])
+
+
+def test_is_origin_allowed_prepends_http():
+    assert is_origin_allowed("sentry.io", ["http://abc.net", "http://sentry.io"])
