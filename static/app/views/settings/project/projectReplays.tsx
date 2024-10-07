1dcde10dd5288e44f8d538fa651cec39acf38c04
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-10-07 14:30:56 -0700

    fix(toolbar): fix allowed origins form saving (#78718)
    Closes https://github.com/getsentry/sentry/issues/78689
    
    Also adds a jest test and renames file/class to match other project
    settings (`project` prefix)

--- static/app/views/settings/project/projectReplays.tsx
+++ static/app/views/settings/project/projectReplays.tsx
@@ -7,6 +7,7 @@ import Link from 'sentry/components/links/link';
 import ReplaySettingsAlert from 'sentry/components/replays/alerts/replaySettingsAlert';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {t, tct} from 'sentry/locale';
+import ProjectsStore from 'sentry/stores/projectsStore';
 import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import type {Organization} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
@@ -80,6 +81,9 @@ function ProjectReplaySettings({organization, project, params: {projectId}}: Pro
         apiMethod="PUT"
         apiEndpoint={`/projects/${organization.slug}/${projectId}/`}
         initialData={project.options}
+        onSubmitSuccess={(
+          response // This will update our project context
+        ) => ProjectsStore.onUpdateSuccess(response)}
       >
         <Access access={['project:write']} project={project}>
           {({hasAccess}) => (
