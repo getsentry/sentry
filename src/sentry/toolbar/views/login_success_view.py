0cf8b3909ee8637f16147f8d1bf53e1a0e6d6f09
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-10-08 09:09:35 -0700

    fix(toolbar): Set headers in `handle_*` responses, and iterate on templates (#78695)

--- src/sentry/toolbar/views/login_success_view.py
+++ src/sentry/toolbar/views/login_success_view.py
@@ -1,11 +1,21 @@
+from django.conf import settings
 from django.http import HttpRequest
 
 from sentry.web.frontend.base import OrganizationView, region_silo_view
 
-SUCCESS_TEMPLATE = "sentry/toolbar/login-success.html"
+TEMPLATE = "sentry/toolbar/login-success.html"
+
+session_cookie_name = settings.SESSION_COOKIE_NAME
 
 
 @region_silo_view
 class LoginSuccessView(OrganizationView):
     def get(self, request: HttpRequest, organization, project_id_or_slug):
-        return self.respond(SUCCESS_TEMPLATE, status=200)
+        return self.respond(
+            TEMPLATE,
+            status=200,
+            context={
+                "delay": request.GET.get("delay", 3000),
+                "cookie": f"{session_cookie_name}={request.COOKIES.get(session_cookie_name)}",
+            },
+        )
