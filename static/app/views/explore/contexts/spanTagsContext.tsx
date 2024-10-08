b52e147bb3975eddbe5fe7f8af89bcf9af22382d
Tony Xiao <txiao@sentry.io>
2024-10-08 15:27:02 -0400

    feat(explore): Load tags from eap spans (#78737)
    This begins loading tags from eap spans for the columns and group bys.
    Search will be updated next.

--- static/app/views/explore/contexts/spanTagsContext.tsx
+++ static/app/views/explore/contexts/spanTagsContext.tsx
@@ -1,33 +1,136 @@
 import type React from 'react';
-import {createContext, useContext} from 'react';
+import {createContext, useContext, useMemo} from 'react';
 
-import type {TagCollection} from 'sentry/types/group';
-import type {UseApiQueryResult} from 'sentry/utils/queryClient';
-import type RequestError from 'sentry/utils/requestError/requestError';
+import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
+import type {Tag, TagCollection} from 'sentry/types/group';
+import {DiscoverDatasets} from 'sentry/utils/discover/types';
+import {useApiQuery} from 'sentry/utils/queryClient';
+import useOrganization from 'sentry/utils/useOrganization';
+import usePageFilters from 'sentry/utils/usePageFilters';
 import {
-  type SpanFieldsResponse,
+  useSpanFieldStaticTags,
   useSpanFieldSupportedTags,
 } from 'sentry/views/performance/utils/useSpanFieldSupportedTags';
 
-const SpanTagsContext = createContext<
-  | (Omit<UseApiQueryResult<SpanFieldsResponse, RequestError>, 'data'> & {
-      data: TagCollection;
-    })
-  | undefined
->(undefined);
+type TypedSpanTags = {
+  number: TagCollection;
+  string: TagCollection;
+};
 
-export function SpanTagsProvider({children}: {children: React.ReactNode}) {
-  const results = useSpanFieldSupportedTags();
+const SpanTagsContext = createContext<TypedSpanTags | undefined>(undefined);
 
-  return <SpanTagsContext.Provider value={results}>{children}</SpanTagsContext.Provider>;
+interface SpanTagsProviderProps {
+  children: React.ReactNode;
+  dataset: DiscoverDatasets;
 }
 
-export const useSpanTags = () => {
-  const context = useContext(SpanTagsContext);
+export function SpanTagsProvider({children, dataset}: SpanTagsProviderProps) {
+  const supportedTags = useSpanFieldSupportedTags();
 
-  if (context === undefined) {
+  const numberTags: TagCollection = useTypedSpanTags({
+    enabled: dataset === DiscoverDatasets.SPANS_EAP,
+    type: 'number',
+  });
+
+  const stringTags: TagCollection = useTypedSpanTags({
+    enabled: dataset === DiscoverDatasets.SPANS_EAP,
+    type: 'string',
+  });
+
+  const staticTags = useSpanFieldStaticTags();
+
+  const allNumberTags = useMemo(() => {
+    if (dataset === DiscoverDatasets.SPANS_INDEXED) {
+      return {};
+    }
+
+    return numberTags;
+  }, [dataset, numberTags]);
+
+  const allStringTags = useMemo(() => {
+    if (dataset === DiscoverDatasets.SPANS_INDEXED) {
+      return supportedTags.data;
+    }
+
+    return {
+      ...stringTags,
+      ...staticTags,
+    };
+  }, [dataset, supportedTags, stringTags, staticTags]);
+
+  const tags = {
+    number: allNumberTags,
+    string: allStringTags,
+  };
+
+  return <SpanTagsContext.Provider value={tags}>{children}</SpanTagsContext.Provider>;
+}
+
+export const useSpanTags = (type?: 'number' | 'string') => {
+  const typedTags = useContext(SpanTagsContext);
+
+  if (typedTags === undefined) {
     throw new Error('useSpanTags must be used within a SpanTagsProvider');
   }
 
-  return context;
+  if (type === 'number') {
+    return typedTags.number;
+  }
+  return typedTags.string;
 };
+
+function useTypedSpanTags({
+  enabled,
+  type,
+}: {
+  type: 'number' | 'string';
+  enabled?: boolean;
+}) {
+  const organization = useOrganization();
+  const {selection} = usePageFilters();
+
+  const path = `/organizations/${organization.slug}/spans/fields/`;
+  const endpointOptions = {
+    query: {
+      project: selection.projects,
+      environment: selection.environments,
+      ...normalizeDateTimeParams(selection.datetime),
+      dataset: 'spans',
+      type,
+    },
+  };
+
+  const result = useApiQuery<Tag[]>([path, endpointOptions], {
+    enabled,
+    staleTime: 0,
+    refetchOnWindowFocus: false,
+    retry: false,
+  });
+
+  const tags: TagCollection = useMemo(() => {
+    const allTags = {};
+
+    for (const tag of result.data ?? []) {
+      // For now, skip all the sentry. prefixed tags as they
+      // should be covered by the static tags that will be
+      // merged with these results.
+      if (tag.key.startsWith('sentry.')) {
+        continue;
+      }
+
+      // EAP spans contain tags with illegal characters
+      if (!/^[a-zA-Z0-9_.:-]+$/.test(tag.key)) {
+        continue;
+      }
+
+      allTags[tag.key] = {
+        key: tag.key,
+        name: tag.name,
+      };
+    }
+
+    return allTags;
+  }, [result]);
+
+  return tags;
+}
