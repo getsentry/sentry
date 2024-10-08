5eb18790c7c7a7a4cb6192d71ef1bc1b1a8e26f5
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2024-10-08 13:16:00 -0700

    feat(integrations): add option to filter by integration type (#78656)
    add query param to filter by `integrationType`:
    - `messaging`
    - `project_management`
    - `source_code_management`
    - `on_call_scheduling`

--- tests/sentry/integrations/api/endpoints/test_organization_integrations.py
+++ tests/sentry/integrations/api/endpoints/test_organization_integrations.py
@@ -15,11 +15,29 @@ class OrganizationIntegrationsListTest(APITestCase):
             name="Example",
             external_id="example:1",
         )
+        self.msteams_integration = self.create_integration(
+            organization=self.organization,
+            provider="msteams",
+            name="MS Teams",
+            external_id="msteams:1",
+        )
+        self.opsgenie = self.create_integration(
+            organization=self.organization,
+            provider="opsgenie",
+            name="Opsgenie",
+            external_id="opsgenie:1",
+        )
+        self.slack_integration = self.create_integration(
+            organization=self.organization,
+            provider="slack",
+            name="Slack",
+            external_id="slack:1",
+        )
 
     def test_simple(self):
         response = self.get_success_response(self.organization.slug)
 
-        assert len(response.data) == 1
+        assert len(response.data) == 4
         assert response.data[0]["id"] == str(self.integration.id)
         assert "configOrganization" in response.data[0]
 
@@ -51,3 +69,40 @@ class OrganizationIntegrationsListTest(APITestCase):
             self.organization.slug, qs_params={"provider_key": "vercel"}
         )
         assert response.data == []
+
+    def test_integration_type(self):
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"integrationType": "messaging"}
+        )
+        assert len(response.data) == 2
+        assert response.data[0]["id"] == str(self.msteams_integration.id)
+        assert response.data[1]["id"] == str(self.slack_integration.id)
+        response = self.get_success_response(
+            self.organization.slug, qs_params={"integrationType": "on_call_scheduling"}
+        )
+        assert len(response.data) == 1
+        assert response.data[0]["id"] == str(self.opsgenie.id)
+        response = self.get_error_response(
+            self.organization.slug, qs_params={"integrationType": "third_party"}
+        )
+        assert response.data == {"detail": "Invalid integration type"}
+        assert response.status_code == 400
+
+    def test_provider_key_and_integration_type(self):
+        response = self.get_success_response(
+            self.organization.slug,
+            qs_params={"providerKey": "slack", "integrationType": "messaging"},
+        )
+        assert len(response.data) == 1
+        assert response.data[0]["id"] == str(self.slack_integration.id)
+        response = self.get_success_response(
+            self.organization.slug,
+            qs_params={"providerKey": "vercel", "integrationType": "messaging"},
+        )
+        assert response.data == []
+        response = self.get_error_response(
+            self.organization.slug,
+            qs_params={"providerKey": "slack", "integrationType": "third_party"},
+        )
+        assert response.data == {"detail": "Invalid integration type"}
+        assert response.status_code == 400
