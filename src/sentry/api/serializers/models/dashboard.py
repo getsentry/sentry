c14af569876cec74ee3677b768244f8afee5a155
Nar Saynorath <nar.saynorath@sentry.io>
2024-10-08 09:32:31 -0400

    fix(dashboard): Use correct enum when surfacing split (#78763)
    Auto-imported the wrong enum when this was updated. Use the correct one
    from dashboard widgets
    
    Fixes SENTRY-3G1K

--- src/sentry/api/serializers/models/dashboard.py
+++ src/sentry/api/serializers/models/dashboard.py
@@ -6,7 +6,6 @@ import orjson
 from sentry import features
 from sentry.api.serializers import Serializer, register, serialize
 from sentry.constants import ALL_ACCESS_PROJECTS
-from sentry.discover.models import DatasetSourcesTypes
 from sentry.models.dashboard import Dashboard
 from sentry.models.dashboard_widget import (
     DashboardWidget,
@@ -14,6 +13,7 @@ from sentry.models.dashboard_widget import (
     DashboardWidgetQuery,
     DashboardWidgetQueryOnDemand,
     DashboardWidgetTypes,
+    DatasetSourcesTypes,
 )
 from sentry.snuba.metrics.extraction import OnDemandMetricSpecVersioning
 from sentry.users.api.serializers.user import UserSerializerResponse
