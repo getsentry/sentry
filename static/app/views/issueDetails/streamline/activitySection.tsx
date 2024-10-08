cc00d8656073af8b850ab86a61ba9c3a908d289e
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2024-10-08 09:06:37 -0700

    feat(issue-details): Add ability to delete comment in streamlined activity (#78126)
    this pr adds the ability to delete comments in the streamlined activity.
    ability to edit will come in future pr
    <img width="326" alt="Screenshot 2024-09-25 at 10 32 21 AM"
    src="https://github.com/user-attachments/assets/57c3502c-6b9b-4d66-aba0-2fc668ed1d26">

--- static/app/views/issueDetails/streamline/activitySection.tsx
+++ static/app/views/issueDetails/streamline/activitySection.tsx
@@ -1,4 +1,4 @@
-import {Fragment, useCallback, useMemo, useState} from 'react';
+import {Fragment, useCallback, useState} from 'react';
 import styled from '@emotion/styled';
 
 import {addErrorMessage, addSuccessMessage} from 'sentry/actionCreators/indicator';
@@ -8,17 +8,19 @@ import useMutateActivity from 'sentry/components/feedback/useMutateActivity';
 import Timeline from 'sentry/components/timeline';
 import TimeSince from 'sentry/components/timeSince';
 import {t} from 'sentry/locale';
-import ConfigStore from 'sentry/stores/configStore';
 import GroupStore from 'sentry/stores/groupStore';
+import {space} from 'sentry/styles/space';
 import type {NoteType} from 'sentry/types/alerts';
-import type {Group} from 'sentry/types/group';
+import type {Group, GroupActivity} from 'sentry/types/group';
+import {GroupActivityType} from 'sentry/types/group';
 import type {User} from 'sentry/types/user';
 import {uniqueId} from 'sentry/utils/guid';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useTeamsById} from 'sentry/utils/useTeamsById';
-import type {MutateActivityOptions} from 'sentry/views/issueDetails/groupActivity';
+import {useUser} from 'sentry/utils/useUser';
 import {groupActivityTypeIconMapping} from 'sentry/views/issueDetails/streamline/groupActivityIcons';
 import getGroupActivityItem from 'sentry/views/issueDetails/streamline/groupActivityItem';
+import {NoteDropdown} from 'sentry/views/issueDetails/streamline/noteDropdown';
 
 function StreamlinedActivitySection({group}: {group: Group}) {
   const organization = useOrganization();
@@ -26,7 +28,7 @@ function StreamlinedActivitySection({group}: {group: Group}) {
 
   const [inputId, setInputId] = useState(uniqueId());
 
-  const me = ConfigStore.get('user');
+  const activeUser = useUser();
   const projectSlugs = group?.project ? [group.project.slug] : [];
   const noteProps = {
     minHeight: 140,
@@ -40,23 +42,44 @@ function StreamlinedActivitySection({group}: {group: Group}) {
     group,
   });
 
-  const createOptions: MutateActivityOptions = useMemo(() => {
-    return {
-      onError: () => {
-        addErrorMessage(t('Unable to post comment'));
-      },
-      onSuccess: data => {
-        GroupStore.addActivity(group.id, data);
-        addSuccessMessage(t('Comment posted'));
-      },
-    };
-  }, [group.id]);
+  const handleDelete = useCallback(
+    (item: GroupActivity) => {
+      const restore = group.activity.find(activity => activity.id === item.id);
+      const index = GroupStore.removeActivity(group.id, item.id);
 
-  const onCreate = useCallback(
-    (n: NoteType, _me: User) => {
-      mutators.handleCreate(n, group.activity, createOptions);
+      if (index === -1 || restore === undefined) {
+        addErrorMessage(t('Failed to delete comment'));
+        return;
+      }
+      mutators.handleDelete(
+        item.id,
+        group.activity.filter(a => a.id !== item.id),
+        {
+          onError: () => {
+            addErrorMessage(t('Failed to delete comment'));
+          },
+          onSuccess: () => {
+            addSuccessMessage(t('Comment removed'));
+          },
+        }
+      );
     },
-    [createOptions, group.activity, mutators]
+    [group.activity, mutators, group.id]
+  );
+
+  const handleCreate = useCallback(
+    (n: NoteType, _me: User) => {
+      mutators.handleCreate(n, group.activity, {
+        onError: () => {
+          addErrorMessage(t('Unable to post comment'));
+        },
+        onSuccess: data => {
+          GroupStore.addActivity(group.id, data);
+          addSuccessMessage(t('Comment posted'));
+        },
+      });
+    },
+    [group.activity, mutators, group.id]
   );
 
   return (
@@ -67,7 +90,7 @@ function StreamlinedActivitySection({group}: {group: Group}) {
           storageKey="groupinput:latest"
           itemKey={group.id}
           onCreate={n => {
-            onCreate(n, me);
+            handleCreate(n, activeUser);
             setInputId(uniqueId());
           }}
           source="issue-details"
@@ -87,7 +110,14 @@ function StreamlinedActivitySection({group}: {group: Group}) {
 
           return (
             <ActivityTimelineItem
-              title={title}
+              title={
+                <TitleWrapper>
+                  {title}
+                  {item.type === GroupActivityType.NOTE && (
+                    <NoteDropdown onDelete={() => handleDelete(item)} user={item.user} />
+                  )}
+                </TitleWrapper>
+              }
               timestamp={<SmallTimestamp date={item.dateCreated} />}
               icon={
                 Icon && (
@@ -110,6 +140,13 @@ function StreamlinedActivitySection({group}: {group: Group}) {
 
 const Author = styled('span')`
   font-weight: ${p => p.theme.fontWeightBold};
+`;
+
+const TitleWrapper = styled('div')`
+  display: flex;
+  align-items: center;
+  font-weight: normal;
+  gap: ${space(0.5)};
 `;
 
 const ActivityTimelineItem = styled(Timeline.Item)`
