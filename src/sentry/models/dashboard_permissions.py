df01b743bf1162d34c5de6ccc66a204c473ea2da
harshithadurai <harshi.durai@esentry.io>
2024-10-09 11:06:52 -0400

    fix lint

--- src/sentry/models/dashboard_permissions.py
+++ src/sentry/models/dashboard_permissions.py
@@ -5,7 +5,6 @@ from django.db import models
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import Model, region_silo_model
 from sentry.db.models.base import sane_repr
-from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
 
 
 @region_silo_model
