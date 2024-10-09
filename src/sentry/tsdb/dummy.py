2523b7d28f5694b37a8b49a0cb77131e0739c9a1
Josh Ferge <josh.ferge@sentry.io>
2024-10-09 16:56:36 -0700

    feat(alerts): add new tsdb functions for new condition (#78866)
    new TSDB function that allows for conditions to be passed to the uniq
    count function. will be used in the coming PR
    https://github.com/getsentry/team-issues/issues/40. This will only be
    used in that case, so my tests which cover the feature will cover this
    function, which is just a simple passthrough to the snuba `get_data`
    function.

--- src/sentry/tsdb/dummy.py
+++ src/sentry/tsdb/dummy.py
@@ -76,6 +76,23 @@ class DummyTSDB(BaseTSDB):
         self.validate_arguments([model], [environment_id])
         return {k: 0 for k in keys}
 
+    def get_distinct_counts_totals_with_conditions(
+        self,
+        model,
+        keys: Sequence[int],
+        start,
+        end=None,
+        rollup=None,
+        environment_id=None,
+        use_cache=False,
+        jitter_value=None,
+        tenant_ids=None,
+        referrer_suffix=None,
+        conditions=None,
+    ):
+        self.validate_arguments([model], [environment_id])
+        return 0
+
     def get_distinct_counts_union(
         self,
         model: TSDBModel,
