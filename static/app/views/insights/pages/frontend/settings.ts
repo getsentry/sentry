67e5c99f764a35fb968d6f91aeb0e2d810979e55
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2024-10-09 14:01:08 -0400

    feat(insights): filter frontend by relevant transaction ops (#78848)

--- static/app/views/insights/pages/frontend/settings.ts
+++ static/app/views/insights/pages/frontend/settings.ts
@@ -2,3 +2,10 @@ import {t} from 'sentry/locale';
 
 export const FRONTEND_LANDING_SUB_PATH = 'frontend';
 export const FRONTEND_LANDING_TITLE = t('Frontend');
+
+export const OVERVIEW_PAGE_ALLOWED_OPS = [
+  'pageload',
+  'navigation',
+  'ui.render',
+  'interaction',
+];
