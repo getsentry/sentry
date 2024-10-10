535a86b78870f479752bfd281cae17ca5ae75077
Abdullah Khan <60121741+Abdkhan14@users.noreply.github.com>
2024-10-10 15:04:37 -0400

    feat(new-trace): Decoupling traceTree instantiation from trace view waterfall.  (#78890)
    This will help use info from `TraceTree`, outside the waterfall. For
    example in the header for the new designs:
    <img width="1041" alt="Screenshot 2024-10-09 at 10 40 34 PM"
    src="https://github.com/user-attachments/assets/988051c5-c5b8-4080-a30a-c70f59a759df">
    
    Added tests.
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

---
+++ static/app/views/performance/newTraceDetails/traceApi/useTraceTree.tsx
@@ -0,0 +1,121 @@
+import {useEffect, useMemo, useReducer, useRef, useState} from 'react';
+
+import type {TraceSplitResults} from 'sentry/utils/performance/quickTrace/types';
+import type {QueryStatus, UseApiQueryResult} from 'sentry/utils/queryClient';
+import useApi from 'sentry/utils/useApi';
+import useOrganization from 'sentry/utils/useOrganization';
+import useProjects from 'sentry/utils/useProjects';
+import type {ReplayRecord} from 'sentry/views/replays/types';
+
+import {TraceTree} from '../traceModels/traceTree';
+
+import type {TraceMetaQueryResults} from './useTraceMeta';
+
+type UseTraceTreeParams = {
+  metaResults: TraceMetaQueryResults;
+  replayRecord: ReplayRecord | null;
+  traceResults: UseApiQueryResult<
+    TraceSplitResults<TraceTree.Transaction> | undefined,
+    any
+  >;
+  traceSlug?: string;
+};
+
+function getTraceViewQueryStatus(
+  traceQueryStatus: QueryStatus,
+  traceMetaQueryStatus: QueryStatus
+): QueryStatus {
+  if (traceQueryStatus === 'error' || traceMetaQueryStatus === 'error') {
+    return 'error';
+  }
+
+  if (traceQueryStatus === 'pending' || traceMetaQueryStatus === 'pending') {
+    return 'pending';
+  }
+
+  return 'success';
+}
+
+export function useTraceTree({
+  traceResults,
+  metaResults,
+  traceSlug,
+  replayRecord,
+}: UseTraceTreeParams): TraceTree {
+  const api = useApi();
+  const {projects} = useProjects();
+  const organization = useOrganization();
+
+  const [tree, setTree] = useState<TraceTree>(TraceTree.Empty());
+  const loadingTraceRef = useRef<TraceTree | null>(null);
+  const [_, rerender] = useReducer(x => (x + 1) % Number.MAX_SAFE_INTEGER, 0);
+
+  const status = useMemo(() => {
+    return getTraceViewQueryStatus(traceResults.status, metaResults.status);
+  }, [traceResults.status, metaResults.status]);
+
+  useEffect(() => {
+    if (status === 'error') {
+      const errorTree = TraceTree.Error({
+        project_slug: projects?.[0]?.slug ?? '',
+        event_id: traceSlug,
+      });
+      setTree(errorTree);
+      return;
+    }
+
+    if (
+      traceResults?.data?.transactions.length === 0 &&
+      traceResults?.data?.orphan_errors.length === 0
+    ) {
+      setTree(TraceTree.Empty());
+      return;
+    }
+
+    if (status === 'pending') {
+      const loadingTrace =
+        loadingTraceRef.current ??
+        TraceTree.Loading({
+          project_slug: projects?.[0]?.slug ?? '',
+          event_id: traceSlug,
+        });
+
+      loadingTraceRef.current = loadingTrace;
+      setTree(loadingTrace);
+      return;
+    }
+
+    if (traceResults.data && metaResults.data) {
+      const trace = TraceTree.FromTrace(traceResults.data, {
+        meta: metaResults,
+        replayRecord: replayRecord,
+      });
+
+      // Root frame + 2 nodes
+      const promises: Promise<void>[] = [];
+      if (trace.list.length <= 3) {
+        for (const c of trace.list) {
+          if (c.canFetch) {
+            promises.push(trace.zoom(c, true, {api, organization}).then(rerender));
+          }
+        }
+      }
+
+      Promise.allSettled(promises).finally(() => {
+        setTree(trace);
+      });
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [
+    api,
+    organization,
+    projects,
+    replayRecord,
+    status,
+    metaResults.data,
+    traceResults.data,
+    traceSlug,
+  ]);
+
+  return tree;
+}
