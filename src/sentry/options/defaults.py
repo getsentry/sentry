29efa6b18f9d67facb6cc9f61344eb40d6a316ff
Mark Story <mark@mark-story.com>
2024-10-11 10:41:57 -0400

    fix(sentryapps) Stop passing event to celery task (#79013)
    Passing the event greatly inflates the size of task messages which can
    contribute to backlogs and memory pressure in rabbit. Instead we can
    only pass the event_id and refetch the event in the task.

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -2785,3 +2785,10 @@ register(
     default=True,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
+
+register(
+    "sentryapps.process-resource-change.use-eventid",
+    type=Bool,
+    default=False,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
