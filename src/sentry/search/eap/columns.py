c65da90b1da76b30ca453ee1762e1c806c2c868e
William Mak <william@wmak.io>
2024-10-11 11:14:36 -0400

    feat(spans): Add most of the span columns (#78863)
    - Adds a bunch of the columns from utils/snuba.py
    - Adds a way to return virtual contexts with columns
    - Handling both string and numeric tags

--- src/sentry/search/eap/columns.py
+++ src/sentry/search/eap/columns.py
@@ -1,10 +1,17 @@
 from collections.abc import Callable
 from dataclasses import dataclass
-from typing import Any, Literal
+from typing import Any
 
-from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeAggregation, AttributeKey
+from sentry_protos.snuba.v1.trace_item_attribute_pb2 import (
+    AttributeAggregation,
+    AttributeKey,
+    VirtualColumnContext,
+)
 
+from sentry.exceptions import InvalidSearchQuery
 from sentry.search.eap import constants
+from sentry.search.events.types import SnubaParams
+from sentry.utils.validators import is_event_id, is_span_id
 
 
 @dataclass(frozen=True)
@@ -14,7 +21,9 @@ class ResolvedColumn:
     # The internal rpc alias for this column
     internal_name: str
     # The public type for this column
-    search_type: Literal["string", "number", "duration"]
+    search_type: str
+    # The internal rpc type for this column, optional as it can mostly be inferred from search_type
+    internal_type: AttributeKey.Type.ValueType | None = None
     # Processor is the function run in the post process step to transform a row into the final result
     processor: Callable[[Any], Any] | None = None
     # Validator to check if the value in a query is correct
@@ -26,41 +35,152 @@ class ResolvedColumn:
 
     def validate(self, value: Any) -> None:
         if self.validator is not None:
-            self.validator(value)
+            if not self.validator(value):
+                raise InvalidSearchQuery(f"{value} is an invalid value for {self.public_alias}")
 
     @property
     def proto_definition(self) -> AttributeAggregation | AttributeKey:
         """The definition of this function as needed by the RPC"""
         # Placeholder to implement search for now
-        return AttributeKey(name=self.internal_name, type=constants.TYPE_MAP[self.search_type])
+        return AttributeKey(
+            name=self.internal_name,
+            type=self.internal_type
+            if self.internal_type is not None
+            else constants.TYPE_MAP[self.search_type],
+        )
 
 
-# Temporary, just doing enough of these for now so I can write some tests for resolve_query
 SPAN_COLUMN_DEFINITIONS = {
-    "id": ResolvedColumn(
-        public_alias="id",
-        internal_name="span_id",
-        search_type="string",
-    ),
-    "organization.id": ResolvedColumn(
-        public_alias="organization.id", internal_name="organization_id", search_type="string"
-    ),
-    "span.action": ResolvedColumn(
-        public_alias="span.action",
-        internal_name="action",
-        search_type="string",
-    ),
-    "span.description": ResolvedColumn(
-        public_alias="span.description",
-        internal_name="name",
-        search_type="string",
-    ),
-    "span.op": ResolvedColumn(public_alias="span.op", internal_name="op", search_type="string"),
-    "ai.total_tokens.used": ResolvedColumn(
-        public_alias="ai.total_tokens.used",
-        internal_name="ai_total_tokens_used",
-        search_type="number",
-    ),
+    column.public_alias: column
+    for column in [
+        ResolvedColumn(
+            public_alias="id",
+            internal_name="span_id",
+            search_type="string",
+            validator=is_span_id,
+        ),
+        ResolvedColumn(
+            public_alias="organization.id", internal_name="organization_id", search_type="string"
+        ),
+        ResolvedColumn(
+            public_alias="span.action",
+            internal_name="action",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="span.description",
+            internal_name="name",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="description",
+            internal_name="name",
+            search_type="string",
+        ),
+        # Message maps to description, this is to allow wildcard searching
+        ResolvedColumn(
+            public_alias="message",
+            internal_name="name",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="span.domain", internal_name="attr_str[domain]", search_type="string"
+        ),
+        ResolvedColumn(
+            public_alias="span.group", internal_name="attr_str[group]", search_type="string"
+        ),
+        ResolvedColumn(public_alias="span.op", internal_name="attr_str[op]", search_type="string"),
+        ResolvedColumn(
+            public_alias="span.category", internal_name="attr_str[category]", search_type="string"
+        ),
+        ResolvedColumn(
+            public_alias="span.self_time", internal_name="exclusive_time_ms", search_type="duration"
+        ),
+        ResolvedColumn(
+            public_alias="span.status", internal_name="attr_str[status]", search_type="string"
+        ),
+        ResolvedColumn(
+            public_alias="trace",
+            internal_name="trace_id",
+            search_type="string",
+            validator=is_event_id,
+        ),
+        ResolvedColumn(
+            public_alias="messaging.destination.name",
+            internal_name="attr_str[messaging.destination.name]",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="messaging.message.id",
+            internal_name="attr_str[messaging.message.id]",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="span.status_code",
+            internal_name="attr_str[status_code]",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="replay.id", internal_name="attr_str[replay_id]", search_type="string"
+        ),
+        ResolvedColumn(
+            public_alias="span.ai.pipeline.group",
+            internal_name="attr_str[ai_pipeline_group]",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="trace.status",
+            internal_name="attr_str[trace.status]",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="browser.name",
+            internal_name="attr_str[browser.name]",
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="ai.total_cost",
+            internal_name="attr_num[ai.total_cost]",
+            search_type="number",
+        ),
+        ResolvedColumn(
+            public_alias="ai.total_tokens.used",
+            internal_name="attr_num[ai_total_tokens_used]",
+            search_type="number",
+        ),
+        ResolvedColumn(
+            public_alias="project",
+            internal_name="project_id",
+            internal_type=constants.INT,
+            search_type="string",
+        ),
+        ResolvedColumn(
+            public_alias="project.slug",
+            internal_name="project_id",
+            search_type="string",
+            internal_type=constants.INT,
+        ),
+    ]
+}
+
+
+def project_context_constructor(column_name: str) -> Callable[[SnubaParams], VirtualColumnContext]:
+    def context_constructor(params: SnubaParams) -> VirtualColumnContext:
+        return VirtualColumnContext(
+            from_column_name="project_id",
+            to_column_name=column_name,
+            value_map={
+                str(project_id): project_name
+                for project_id, project_name in params.project_id_map.items()
+            },
+        )
+
+    return context_constructor
+
+
+VIRTUAL_CONTEXTS = {
+    "project": project_context_constructor("project"),
+    "project.slug": project_context_constructor("project.slug"),
 }
 
 
--- tests/sentry/search/eap/test_spans.py
+++ tests/sentry/search/eap/test_spans.py
@@ -1,8 +1,10 @@
+import pytest
 from sentry_protos.snuba.v1.trace_item_attribute_pb2 import (
     AttributeKey,
     AttributeValue,
     IntArray,
     StrArray,
+    VirtualColumnContext,
 )
 from sentry_protos.snuba.v1.trace_item_filter_pb2 import (
     AndFilter,
@@ -11,6 +13,7 @@ from sentry_protos.snuba.v1.trace_item_filter_pb2 import (
     TraceItemFilter,
 )
 
+from sentry.exceptions import InvalidSearchQuery
 from sentry.search.eap.spans import SearchResolver
 from sentry.search.eap.types import SearchResolverConfig
 from sentry.search.events.types import SnubaParams
@@ -45,7 +48,9 @@ class SearchResolverQueryTest(TestCase):
         query = self.resolver.resolve_query("ai.total_tokens.used:123")
         assert query == TraceItemFilter(
             comparison_filter=ComparisonFilter(
-                key=AttributeKey(name="ai_total_tokens_used", type=AttributeKey.Type.TYPE_INT),
+                key=AttributeKey(
+                    name="attr_num[ai_total_tokens_used]", type=AttributeKey.Type.TYPE_INT
+                ),
                 op=ComparisonFilter.OP_EQUALS,
                 value=AttributeValue(val_int=123),
             )
@@ -61,6 +66,20 @@ class SearchResolverQueryTest(TestCase):
             )
         )
 
+    def test_uuid_validation(self):
+        query = self.resolver.resolve_query(f"id:{'f'*16}")
+        assert query == TraceItemFilter(
+            comparison_filter=ComparisonFilter(
+                key=AttributeKey(name="span_id", type=AttributeKey.Type.TYPE_STRING),
+                op=ComparisonFilter.OP_EQUALS,
+                value=AttributeValue(val_str="f" * 16),
+            )
+        )
+
+    def test_invalid_uuid_validation(self):
+        with pytest.raises(InvalidSearchQuery):
+            self.resolver.resolve_query("id:hello")
+
     def test_not_in_filter(self):
         query = self.resolver.resolve_query("!span.description:[foo,bar,baz]")
         assert query == TraceItemFilter(
@@ -75,7 +94,9 @@ class SearchResolverQueryTest(TestCase):
         query = self.resolver.resolve_query("ai.total_tokens.used:[123,456,789]")
         assert query == TraceItemFilter(
             comparison_filter=ComparisonFilter(
-                key=AttributeKey(name="ai_total_tokens_used", type=AttributeKey.Type.TYPE_INT),
+                key=AttributeKey(
+                    name="attr_num[ai_total_tokens_used]", type=AttributeKey.Type.TYPE_INT
+                ),
                 op=ComparisonFilter.OP_IN,
                 value=AttributeValue(val_int_array=IntArray(values=[123, 456, 789])),
             )
@@ -85,7 +106,9 @@ class SearchResolverQueryTest(TestCase):
         query = self.resolver.resolve_query("ai.total_tokens.used:>123")
         assert query == TraceItemFilter(
             comparison_filter=ComparisonFilter(
-                key=AttributeKey(name="ai_total_tokens_used", type=AttributeKey.Type.TYPE_INT),
+                key=AttributeKey(
+                    name="attr_num[ai_total_tokens_used]", type=AttributeKey.Type.TYPE_INT
+                ),
                 op=ComparisonFilter.OP_GREATER_THAN,
                 value=AttributeValue(val_int=123),
             )
@@ -105,7 +128,9 @@ class SearchResolverQueryTest(TestCase):
                     ),
                     TraceItemFilter(
                         comparison_filter=ComparisonFilter(
-                            key=AttributeKey(name="op", type=AttributeKey.Type.TYPE_STRING),
+                            key=AttributeKey(
+                                name="attr_str[op]", type=AttributeKey.Type.TYPE_STRING
+                            ),
                             op=ComparisonFilter.OP_EQUALS,
                             value=AttributeValue(val_str="bar"),
                         )
@@ -128,7 +153,9 @@ class SearchResolverQueryTest(TestCase):
                     ),
                     TraceItemFilter(
                         comparison_filter=ComparisonFilter(
-                            key=AttributeKey(name="op", type=AttributeKey.Type.TYPE_STRING),
+                            key=AttributeKey(
+                                name="attr_str[op]", type=AttributeKey.Type.TYPE_STRING
+                            ),
                             op=ComparisonFilter.OP_EQUALS,
                             value=AttributeValue(val_str="bar"),
                         )
@@ -159,7 +186,7 @@ class SearchResolverQueryTest(TestCase):
                                 TraceItemFilter(
                                     comparison_filter=ComparisonFilter(
                                         key=AttributeKey(
-                                            name="op", type=AttributeKey.Type.TYPE_STRING
+                                            name="attr_str[op]", type=AttributeKey.Type.TYPE_STRING
                                         ),
                                         op=ComparisonFilter.OP_EQUALS,
                                         value=AttributeValue(val_str="345"),
@@ -183,7 +210,7 @@ class SearchResolverQueryTest(TestCase):
                                 TraceItemFilter(
                                     comparison_filter=ComparisonFilter(
                                         key=AttributeKey(
-                                            name="op", type=AttributeKey.Type.TYPE_STRING
+                                            name="attr_str[op]", type=AttributeKey.Type.TYPE_STRING
                                         ),
                                         op=ComparisonFilter.OP_EQUALS,
                                         value=AttributeValue(val_str="bar"),
@@ -199,3 +226,62 @@ class SearchResolverQueryTest(TestCase):
     def test_empty_query(self):
         query = self.resolver.resolve_query("")
         assert query is None
+
+
+class SearchResolverColumnTest(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.project = self.create_project(name="test")
+        self.resolver = SearchResolver(
+            params=SnubaParams(projects=[self.project]), config=SearchResolverConfig()
+        )
+
+    def test_simple_op_field(self):
+        resolved_column, virtual_context = self.resolver.resolve_column("span.op")
+        assert resolved_column.proto_definition == AttributeKey(
+            name="attr_str[op]", type=AttributeKey.Type.TYPE_STRING
+        )
+        assert virtual_context is None
+
+    def test_project_field(self):
+        resolved_column, virtual_context = self.resolver.resolve_column("project")
+        assert resolved_column.proto_definition == AttributeKey(
+            name="project_id", type=AttributeKey.Type.TYPE_INT
+        )
+        assert virtual_context == VirtualColumnContext(
+            from_column_name="project_id",
+            to_column_name="project",
+            value_map={str(self.project.id): self.project.slug},
+        )
+
+    def test_project_slug_field(self):
+        resolved_column, virtual_context = self.resolver.resolve_column("project.slug")
+        assert resolved_column.proto_definition == AttributeKey(
+            name="project_id", type=AttributeKey.Type.TYPE_INT
+        )
+        assert virtual_context == VirtualColumnContext(
+            from_column_name="project_id",
+            to_column_name="project.slug",
+            value_map={str(self.project.id): self.project.slug},
+        )
+
+    def test_simple_tag(self):
+        resolved_column, virtual_context = self.resolver.resolve_column("tags[foo]")
+        assert resolved_column.proto_definition == AttributeKey(
+            name="attr_str[foo]", type=AttributeKey.Type.TYPE_STRING
+        )
+        assert virtual_context is None
+
+    def test_simple_string_tag(self):
+        resolved_column, virtual_context = self.resolver.resolve_column("tags[foo, string]")
+        assert resolved_column.proto_definition == AttributeKey(
+            name="attr_str[foo]", type=AttributeKey.Type.TYPE_STRING
+        )
+        assert virtual_context is None
+
+    def test_simple_number_tag(self):
+        resolved_column, virtual_context = self.resolver.resolve_column("tags[foo, number]")
+        assert resolved_column.proto_definition == AttributeKey(
+            name="attr_num[foo]", type=AttributeKey.Type.TYPE_INT
+        )
+        assert virtual_context is None
