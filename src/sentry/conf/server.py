e152608c9c0057b7acda0d884c2e219a5078837a
Dan Fuller <dfuller@sentry.io>
2024-10-11 12:09:22 -0700

    fix(migrations): Add in a feature to force a lock timeout to be set on all migration operations
    We had an incident where a migration couldn't acquire a lock to modify `sentry_groupedmessage` and took down production. This shouldn't have happened, since we rely on django-zero-downtime-migrations to set a lock acquisition timeout on all operations.
    
    Unfortunately, it doesn't set it on `RunSQL` or `RunPython` ops. There are other operations that it considers safe that it also doesn't set it on, but those aren't an issue.
    
    This pr adds `ZERO_DOWNTIME_MIGRATIONS_LOCK_TIMEOUT_FORCE`, which when set to True will guarantee that we always set the lock timeout for all operations. In cases where we would have previously set both a lock and statement timeout, the result is unchanged.
    
    We specifically don't want to set a statement timeout on all operations. Some operations can take a long time (adding an index) and so we don't want to force this in all cases.
    
    This pr vendors a bunch of code from `dzdm`. Ideally we could attempt to submit it upstream, but I think we'd need to tidy things up a little to do so.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -3150,6 +3150,7 @@ PG_VERSION: str = os.getenv("PG_VERSION") or "14"
 ZERO_DOWNTIME_MIGRATIONS_RAISE_FOR_UNSAFE = True
 ZERO_DOWNTIME_MIGRATIONS_LOCK_TIMEOUT = None
 ZERO_DOWNTIME_MIGRATIONS_STATEMENT_TIMEOUT = None
+ZERO_DOWNTIME_MIGRATIONS_LOCK_TIMEOUT_FORCE = False
 
 if int(PG_VERSION.split(".", maxsplit=1)[0]) < 12:
     # In v0.6 of django-pg-zero-downtime-migrations this settings is deprecated for PostreSQLv12+
