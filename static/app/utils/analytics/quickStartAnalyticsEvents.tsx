b41a28e20c9e385e6d27a3e41787c7a55c3b0db7
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-10-15 12:10:41 +0200

    feat(quick-start): Add analytics code to the frontend to track quick start completion (#79092)

--- static/app/utils/analytics/quickStartAnalyticsEvents.tsx
+++ static/app/utils/analytics/quickStartAnalyticsEvents.tsx
@@ -1,4 +1,7 @@
 export type QuickStartEventParameters = {
+  'quick_start.completed': {
+    referrer: string;
+  };
   'quick_start.opened': {};
   'quick_start.task_card_clicked': {
     action: string;
@@ -10,4 +13,5 @@ export type QuickStartEventParameters = {
 export const quickStartEventMap: Record<keyof QuickStartEventParameters, string> = {
   'quick_start.opened': 'Quick Start: Opened',
   'quick_start.task_card_clicked': 'Quick Start: Task Card Clicked',
+  'quick_start.completed': 'Quick Start: Completed',
 };
