e270652704ff2a613c20afb8e228b70c51e5d3ab
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-10-15 18:13:46 -0300

    feat(quick-start): Add logic to skeleton - Part 1

--- static/app/components/onboardingWizard/newSidebar.tsx
+++ static/app/components/onboardingWizard/newSidebar.tsx
@@ -1,18 +1,73 @@
-import {Fragment, useState} from 'react';
+import {
+  Fragment,
+  useCallback,
+  useContext,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from 'react';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
+import partition from 'lodash/partition';
 
+import {navigateTo} from 'sentry/actionCreators/navigation';
+import {updateOnboardingTask} from 'sentry/actionCreators/onboardingTasks';
 import {Chevron} from 'sentry/components/chevron';
 import InteractionStateLayer from 'sentry/components/interactionStateLayer';
+import {
+  OnboardingContext,
+  type OnboardingContextProps,
+} from 'sentry/components/onboarding/onboardingContext';
+import {findCompleteTasks, taskIsDone} from 'sentry/components/onboardingWizard/utils';
 import ProgressRing from 'sentry/components/progressRing';
 import SidebarPanel from 'sentry/components/sidebar/sidebarPanel';
 import type {CommonSidebarProps} from 'sentry/components/sidebar/types';
 import {Tooltip} from 'sentry/components/tooltip';
 import {IconCheckmark} from 'sentry/icons';
 import {t, tct} from 'sentry/locale';
+import DemoWalkthroughStore from 'sentry/stores/demoWalkthroughStore';
 import pulsingIndicatorStyles from 'sentry/styles/pulsingIndicator';
 import {space} from 'sentry/styles/space';
+import {type OnboardingTask, OnboardingTaskGroup} from 'sentry/types/onboarding';
+import type {Organization} from 'sentry/types/organization';
+import type {Project} from 'sentry/types/project';
+import {trackAnalytics} from 'sentry/utils/analytics';
 import {isDemoWalkthrough} from 'sentry/utils/demoMode';
+import useApi from 'sentry/utils/useApi';
+import useOrganization from 'sentry/utils/useOrganization';
+import useProjects from 'sentry/utils/useProjects';
+import useRouter from 'sentry/utils/useRouter';
+
+import {getMergedTasks} from './taskConfig';
+
+/**
+ * How long (in ms) to delay before beginning to mark tasks complete
+ */
+const INITIAL_MARK_COMPLETE_TIMEOUT = 600;
+
+/**
+ * How long (in ms) to delay between marking each unseen task as complete.
+ */
+const COMPLETION_SEEN_TIMEOUT = 800;
+
+function useOnboardingTasks(
+  organization: Organization,
+  projects: Project[],
+  onboardingContext: OnboardingContextProps
+) {
+  return useMemo(() => {
+    const all = getMergedTasks({
+      organization,
+      projects,
+      onboardingContext,
+    }).filter(task => task.display);
+    return {
+      allTasks: all,
+      basicTasks: all.filter(task => task.group === OnboardingTaskGroup.BASIC),
+    };
+  }, [organization, projects, onboardingContext]);
+}
 
 function getPanelDescription(walkthrough: boolean) {
   if (walkthrough) {
@@ -28,16 +83,56 @@ function getPanelDescription(walkthrough: boolean) {
 }
 
 interface TaskProps {
-  description: string;
-  title: string;
+  hidePanel: () => void;
+  task: OnboardingTask;
   status?: 'waiting' | 'completed';
 }
 
-function Task({title, description, status}: TaskProps) {
+function Task({task, status, hidePanel}: TaskProps) {
+  const organization = useOrganization();
+  const router = useRouter();
+
+  const handleClick = useCallback(
+    (e: React.MouseEvent) => {
+      trackAnalytics('quick_start.task_card_clicked', {
+        organization,
+        todo_id: task.task,
+        todo_title: task.title,
+        action: 'clickthrough',
+      });
+
+      e.stopPropagation();
+
+      if (isDemoWalkthrough()) {
+        DemoWalkthroughStore.activateGuideAnchor(task.task);
+      }
+
+      if (task.actionType === 'external') {
+        window.open(task.location, '_blank');
+      }
+
+      if (task.actionType === 'action') {
+        task.action(router);
+      }
+
+      if (task.actionType === 'app') {
+        // Convert all paths to a location object
+        let to =
+          typeof task.location === 'string' ? {pathname: task.location} : task.location;
+        // Add referrer to all links
+        to = {...to, query: {...to.query, referrer: 'onboarding_task'}};
+
+        navigateTo(to, router);
+      }
+      hidePanel();
+    },
+    [task, organization, router, hidePanel]
+  );
+
   if (status === 'completed') {
     return (
       <TaskWrapper completed>
-        <strong>{title}</strong>
+        <strong>{task.title}</strong>
         <IconCheckmark color="green300" isCircled />
       </TaskWrapper>
     );
@@ -45,11 +140,11 @@ function Task({title, description, status}: TaskProps) {
 
   if (status === 'waiting') {
     return (
-      <TaskWrapper>
+      <TaskWrapper onClick={handleClick}>
         <InteractionStateLayer />
         <div>
-          <strong>{title}</strong>
-          <p>{description}</p>
+          <strong>{task.title}</strong>
+          <p>{task.description}</p>
         </div>
         <Tooltip title={t('Waiting for event')}>
           <PulsingIndicator />
@@ -59,11 +154,11 @@ function Task({title, description, status}: TaskProps) {
   }
 
   return (
-    <TaskWrapper>
+    <TaskWrapper onClick={handleClick}>
       <InteractionStateLayer />
       <div>
-        <strong>{title}</strong>
-        <p>{description}</p>
+        <strong>{task.title}</strong>
+        <p>{task.description}</p>
       </div>
     </TaskWrapper>
   );
@@ -71,20 +166,18 @@ function Task({title, description, status}: TaskProps) {
 
 interface TaskGroupProps {
   description: string;
+  hidePanel: () => void;
+  tasks: OnboardingTask[];
   title: string;
-  totalCompletedTasks: number;
-  totalTasks: number;
   expanded?: boolean;
 }
 
-function TaskGroup({
-  title,
-  description,
-  totalCompletedTasks,
-  totalTasks,
-  expanded,
-}: TaskGroupProps) {
+function TaskGroup({title, description, tasks, expanded, hidePanel}: TaskGroupProps) {
   const [isExpanded, setIsExpanded] = useState(expanded);
+  const [completedTasks, incompletedTasks] = partition(tasks, task =>
+    findCompleteTasks(task)
+  );
+
   return (
     <TaskGroupWrapper>
       <TaskGroupHeader onClick={() => setIsExpanded(!isExpanded)}>
@@ -105,22 +198,32 @@ function TaskGroup({
           <TaskGroupBody>
             <TaskGroupProgress>
               {tct('[totalCompletedTasks] out of [totalTasks] tasks completed', {
-                totalCompletedTasks,
-                totalTasks,
+                totalCompletedTasks: completedTasks.length,
+                totalTasks: tasks.length,
               })}
               <ProgressRing
-                value={(totalCompletedTasks / totalTasks) * 100}
+                value={(completedTasks.length / tasks.length) * 100}
                 progressEndcaps="round"
                 size={16}
                 barWidth={2}
               />
             </TaskGroupProgress>
-            <Task title="Title" description="Description" status="waiting" />
-            <Task title="Title" description="Description" />
-            <Task title="Title" description="Description" />
-            <TaskGroupProgress completed>{t('Completed')}</TaskGroupProgress>
-            <Task title="Title" description="Description" status="completed" />
-            <Task title="Title" description="Description" status="completed" />
+            {incompletedTasks.map(task => (
+              <Task key={task.task} task={task} hidePanel={hidePanel} />
+            ))}
+            {completedTasks.length > 0 && (
+              <Fragment>
+                <TaskGroupProgress completed>{t('Completed')}</TaskGroupProgress>
+                {completedTasks.map(task => (
+                  <Task
+                    key={task.task}
+                    task={task}
+                    hidePanel={hidePanel}
+                    status="completed"
+                  />
+                ))}
+              </Fragment>
+            )}
           </TaskGroupBody>
         </Fragment>
       )}
@@ -133,8 +236,69 @@ interface NewSidebarProps extends Pick<CommonSidebarProps, 'orientation' | 'coll
 }
 
 export function NewOnboardingSidebar({onClose, orientation, collapsed}: NewSidebarProps) {
+  const api = useApi();
+  const organization = useOrganization();
+  const onboardingContext = useContext(OnboardingContext);
+  const {projects} = useProjects();
   const walkthrough = isDemoWalkthrough();
   const {title, description} = getPanelDescription(walkthrough);
+  const {allTasks, basicTasks} = useOnboardingTasks(
+    organization,
+    projects,
+    onboardingContext
+  );
+
+  const markCompletionTimeout = useRef<number | undefined>();
+  const markCompletionSeenTimeout = useRef<number | undefined>();
+
+  function completionTimeout(time: number): Promise<void> {
+    window.clearTimeout(markCompletionTimeout.current);
+    return new Promise(resolve => {
+      markCompletionTimeout.current = window.setTimeout(resolve, time);
+    });
+  }
+
+  function seenTimeout(time: number): Promise<void> {
+    window.clearTimeout(markCompletionSeenTimeout.current);
+    return new Promise(resolve => {
+      markCompletionSeenTimeout.current = window.setTimeout(resolve, time);
+    });
+  }
+
+  const markTasksAsSeen = useCallback(
+    async function () {
+      const unseenTasks = allTasks
+        .filter(task => taskIsDone(task) && !task.completionSeen)
+        .map(task => task.task);
+
+      // Incrementally mark tasks as seen. This gives the card completion
+      // animations time before we move each task into the completed section.
+      for (const task of unseenTasks) {
+        await seenTimeout(COMPLETION_SEEN_TIMEOUT);
+        updateOnboardingTask(api, organization, {task, completionSeen: true});
+      }
+    },
+    [api, organization, allTasks]
+  );
+
+  const markSeenOnOpen = useCallback(
+    async function () {
+      // Add a minor delay to marking tasks complete to account for the animation
+      // opening of the sidebar panel
+      await completionTimeout(INITIAL_MARK_COMPLETE_TIMEOUT);
+      markTasksAsSeen();
+    },
+    [markTasksAsSeen]
+  );
+
+  useEffect(() => {
+    markSeenOnOpen();
+
+    return () => {
+      window.clearTimeout(markCompletionTimeout.current);
+      window.clearTimeout(markCompletionSeenTimeout.current);
+    };
+  }, [markSeenOnOpen]);
 
   return (
     <Wrapper
@@ -145,25 +309,17 @@ export function NewOnboardingSidebar({onClose, orientation, collapsed}: NewSideb
     >
       <Content>
         <p>{description}</p>
-        <TaskGroup
-          title="Title"
-          description="Description"
-          totalCompletedTasks={3}
-          totalTasks={8}
-          expanded
-        />
-        <TaskGroup
-          title="Title"
-          description="Description"
-          totalCompletedTasks={3}
-          totalTasks={8}
-        />
-        <TaskGroup
-          title="Title"
-          description="Description"
-          totalCompletedTasks={3}
-          totalTasks={8}
-        />
+        {basicTasks.length && (
+          <TaskGroup
+            title={t('The basics')}
+            description={t(
+              'Learn the essentials to set up monitoring, capture errors, and track releases.'
+            )}
+            tasks={basicTasks}
+            hidePanel={onClose}
+            expanded
+          />
+        )}
       </Content>
     </Wrapper>
   );
