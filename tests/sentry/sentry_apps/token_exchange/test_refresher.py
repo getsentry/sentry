114e66de56651e4e7e77516d1f6ddfa4eb66c495
Christinarlong <christina.long@sentry.io>
2024-10-15 15:47:37 -0700

    forgot to remove patch variable

--- tests/sentry/sentry_apps/token_exchange/test_refresher.py
+++ tests/sentry/sentry_apps/token_exchange/test_refresher.py
@@ -42,7 +42,7 @@ class TestRefresher(TestCase):
         self.refresher.run()
         assert not ApiToken.objects.filter(id=self.token.id).exists()
 
-    def test_validates_generic_token_exchange_requirements(self, validator):
+    def test_validates_generic_token_exchange_requirements(self):
         assert self.refresher._validate()
 
     def test_validates_token_belongs_to_sentry_app(self):
