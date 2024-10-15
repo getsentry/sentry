66dcc0e93c6fb68e6cf067a86fdbaa8affd97d4f
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-10-15 10:47:49 -0400

    chore(feedback): Analytics for error rendering feedback item (#78978)
    Adds analytics for error when rendering feedback item. Also fixes
    loading screen when the feedback does not exist

--- static/app/components/feedback/feedbackItem/feedbackItemLoader.tsx
+++ static/app/components/feedback/feedbackItem/feedbackItemLoader.tsx
@@ -22,10 +22,16 @@ export default function FeedbackItemLoader() {
   useSentryAppComponentsData({projectId: projectSlug});
 
   useEffect(() => {
+    if (issueResult.isError) {
+      trackAnalytics('feedback.feedback-item-not-found', {organization, feedbackId});
+    }
+  }, [organization, issueResult.isError, feedbackId]);
+
+  useEffect(() => {
     if (issueData) {
       trackAnalytics('feedback.feedback-item-rendered', {organization});
     }
-  }, [organization, issueData]);
+  }, [issueData, organization]);
 
   // There is a case where we are done loading, but we're fetching updates
   // This happens when the user has seen a feedback, clicks around a bit, then
