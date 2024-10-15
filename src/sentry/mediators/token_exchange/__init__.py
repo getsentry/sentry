d4d8507e01815b55988bbe0b1590577214014a70
Christinarlong <christina.long@sentry.io>
2024-10-15 10:50:54 -0700

    missed some things

--- src/sentry/mediators/token_exchange/__init__.py
+++ src/sentry/mediators/token_exchange/__init__.py
@@ -1,8 +1,1 @@
-from ...sentry_apps.token_exchange.util import (  # NOQA
-    AUTHORIZATION,
-    REFRESH,
-    GrantTypes,
-    token_expiration,
-)
-from ...sentry_apps.token_exchange.validator import Validator  # NOQA
 from .refresher import Refresher  # NOQA
--- tests/sentry/mediators/token_exchange/test_refresher.py
+++ tests/sentry/mediators/token_exchange/test_refresher.py
@@ -43,7 +43,7 @@ class TestRefresher(TestCase):
         assert not ApiToken.objects.filter(id=self.token.id).exists()
 
     def test_validates_generic_token_exchange_requirements(self):
-        self.refresher._validate()
+        assert self.refresher._validate()
 
     def test_validates_token_belongs_to_sentry_app(self):
         refresh_token = ApiToken.objects.create(
