2013ee2895029902a386fc927ee8ddccae6276b0
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2024-10-15 13:59:11 -0700

    ref(mediators): Make refresher mediator to dataclass (#79112)

--- src/sentry/sentry_apps/token_exchange/refresher.py
+++ src/sentry/sentry_apps/token_exchange/refresher.py
@@ -1,10 +1,10 @@
-from django.db import router
+from dataclasses import dataclass
+
+from django.db import router, transaction
 from django.utils.functional import cached_property
 
 from sentry import analytics
 from sentry.coreapi import APIUnauthorized
-from sentry.mediators.mediator import Mediator
-from sentry.mediators.param import Param
 from sentry.mediators.token_exchange.util import token_expiration
 from sentry.mediators.token_exchange.validator import Validator
 from sentry.models.apiapplication import ApiApplication
@@ -15,42 +15,39 @@ from sentry.sentry_apps.services.app import RpcSentryAppInstallation
 from sentry.users.models.user import User
 
 
-class Refresher(Mediator):
+@dataclass
+class Refresher:
     """
     Exchanges a Refresh Token for a new Access Token
     """
 
-    install = Param(RpcSentryAppInstallation)
-    refresh_token = Param(str)
-    client_id = Param(str)
-    user = Param(User)
-    using = router.db_for_write(User)
+    install: RpcSentryAppInstallation
+    refresh_token: str
+    client_id: str
+    user: User
 
-    def call(self):
-        self._validate()
-        self._delete_token()
+    def run(self) -> ApiToken:
+        with transaction.atomic(router.db_for_write(ApiToken)):
+            self._validate()
+            self.token.delete()
+
+        self.record_analytics()
         return self._create_new_token()
 
-    def record_analytics(self):
+    def record_analytics(self) -> None:
         analytics.record(
             "sentry_app.token_exchanged",
             sentry_app_installation_id=self.install.id,
             exchange_type="refresh",
         )
 
-    def _validate(self):
+    def _validate(self) -> None:
         Validator.run(install=self.install, client_id=self.client_id, user=self.user)
 
-        self._validate_token_belongs_to_app()
-
-    def _validate_token_belongs_to_app(self):
         if self.token.application != self.application:
-            raise APIUnauthorized
+            raise APIUnauthorized("Token does not belong to the application")
 
-    def _delete_token(self):
-        self.token.delete()
-
-    def _create_new_token(self):
+    def _create_new_token(self) -> ApiToken:
         token = ApiToken.objects.create(
             user=self.user,
             application=self.application,
@@ -64,22 +61,22 @@ class Refresher(Mediator):
         return token
 
     @cached_property
-    def token(self):
+    def token(self) -> ApiToken:
         try:
             return ApiToken.objects.get(refresh_token=self.refresh_token)
         except ApiToken.DoesNotExist:
-            raise APIUnauthorized
+            raise APIUnauthorized("Token does not exist")
 
     @cached_property
-    def application(self):
+    def application(self) -> ApiApplication:
         try:
             return ApiApplication.objects.get(client_id=self.client_id)
         except ApiApplication.DoesNotExist:
-            raise APIUnauthorized
+            raise APIUnauthorized("Application does not exist")
 
     @property
-    def sentry_app(self):
+    def sentry_app(self) -> SentryApp:
         try:
             return self.application.sentry_app
         except SentryApp.DoesNotExist:
-            raise APIUnauthorized
+            raise APIUnauthorized("Sentry App does not exist")
