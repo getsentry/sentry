4ead57583c2fe2b7a5a3617388e7992cf94b722d
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-10-15 15:20:31 +0200

    feat(quick-start): Add new feature flag for the new updates (#79094)

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -351,6 +351,8 @@ def register_temporary_features(manager: FeatureManager):
     # Limit project events endpoint to only query back a certain number of days
     manager.add("organizations:project-event-date-limit", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     manager.add("organizations:project-templates", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
+    # Enable the new quick start guide
+    manager.add("organizations:quick-start-updates", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable the new Related Events feature
     manager.add("organizations:related-events", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enable related issues feature
