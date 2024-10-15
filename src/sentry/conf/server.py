c682515a4d8c9eba29d4ac49f35f97b81a5da2b7
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-10-15 11:05:26 -0700

    analytics(toolbar): record api requests in custom middleware (#78778)
    Closes https://github.com/getsentry/sentry-toolbar/issues/33
    
    We're ready to release a developer toolbar for installation on 3rd party
    sites. Once installed, toolbars will query sentry backend for data - see
    the [repo](https://github.com/getsentry/sentry-toolbar) or /devtoolbar
    folder of sentry for examples. This helps us get analytics on every
    endpoint, whenever the `querryReferrer=devtoolbar` header is included.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -346,6 +346,7 @@ MIDDLEWARE: tuple[str, ...] = (
     "sentry.middleware.locale.SentryLocaleMiddleware",
     "sentry.middleware.ratelimit.RatelimitMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
+    "sentry.middleware.devtoolbar.DevToolbarAnalyticsMiddleware",
 )
 
 ROOT_URLCONF = "sentry.conf.urls"
