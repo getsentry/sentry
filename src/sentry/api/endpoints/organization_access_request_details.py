06cb5af8ff94372bcea7b7054c36a91b6ad757e9
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-10-16 12:15:58 -0400

    feat(teams): Prune outdated team invites if they no longer apply (#79123)
    Resolves https://github.com/getsentry/sentry/issues/77945. Not sure
    about this approach, but the code works. I think this makes more sense
    to implement as a celery-beat task, so I may investigate that in a
    follow up.
    
    For now though, querying the list access requests endpoint will first
    remove any outdated team requests (from users already having joined
    those teams, before returning a list to the frontend. While I considered
    trying to ensure everywhere a user joins a team we could query and
    remove any team requests, there would be no way to ensure that logic
    stays linked, and ultimately doesn't help the existing organizations
    with these requests that can be pruned.
    
    **todo**
    - [x] Add tests to verify this new behavior
    
    ---------
    
    Co-authored-by: Ryan Skonnord <ryan.skonnord@sentry.io>

--- src/sentry/api/endpoints/organization_access_request_details.py
+++ src/sentry/api/endpoints/organization_access_request_details.py
@@ -1,3 +1,5 @@
+import logging
+
 from django.db import IntegrityError, router, transaction
 from rest_framework import serializers
 from rest_framework.request import Request
@@ -11,7 +13,10 @@ from sentry.api.bases.organization import OrganizationEndpoint, OrganizationPerm
 from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.serializers import serialize
 from sentry.models.organizationaccessrequest import OrganizationAccessRequest
+from sentry.models.organizationmember import OrganizationMember
 from sentry.models.organizationmemberteam import OrganizationMemberTeam
+
+logger = logging.getLogger(__name__)
 
 
 class AccessRequestPermission(OrganizationPermission):
@@ -71,8 +76,8 @@ class OrganizationAccessRequestDetailsEndpoint(OrganizationEndpoint):
 
     def get(self, request: Request, organization) -> Response:
         """
-        Get list of requests to join org/team
-
+        Get a list of requests to join org/team.
+        If any requests are redundant (user already joined the team), they are not returned.
         """
         if request.access.has_scope("org:write"):
             access_requests = list(
@@ -80,7 +85,7 @@ class OrganizationAccessRequestDetailsEndpoint(OrganizationEndpoint):
                     team__organization=organization,
                     member__user_is_active=True,
                     member__user_id__isnull=False,
-                ).select_related("team")
+                ).select_related("team", "member")
             )
 
         elif request.access.has_scope("team:write") and request.access.team_ids_with_membership:
@@ -89,19 +94,27 @@ class OrganizationAccessRequestDetailsEndpoint(OrganizationEndpoint):
                     member__user_is_active=True,
                     member__user_id__isnull=False,
                     team__id__in=request.access.team_ids_with_membership,
-                ).select_related("team")
+                ).select_related("team", "member")
             )
         else:
             # Return empty response if user does not have access
             return Response([])
 
-        return Response(serialize(access_requests, request.user))
+        teams_by_user = OrganizationMember.objects.get_teams_by_user(organization=organization)
+
+        # We omit any requests which are now redundant (i.e. the user joined that team some other way)
+        valid_access_requests = [
+            access_request
+            for access_request in access_requests
+            if access_request.member.user_id is not None
+            and access_request.team_id not in teams_by_user[access_request.member.user_id]
+        ]
+
+        return Response(serialize(valid_access_requests, request.user))
 
     def put(self, request: Request, organization, request_id) -> Response:
         """
         Approve or deny a request
-
-        Approve or deny a request.
 
             {method} {path}
 
