1b4c02687a3709fb15a42bbf83c0ae9fd818b1cf
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-10-16 14:12:10 -0400

    ref: remove/replace calls to iso_format(...) in relay / symbolicator tests (#79060)
    automated using
    https://github.com/getsentry/sentry/pull/78931#issue-2578827921
    
    <!-- Describe your PR here. -->

--- tests/relay_integration/test_integration.py
+++ tests/relay_integration/test_integration.py
@@ -7,7 +7,7 @@ import pytest
 from sentry.models.eventattachment import EventAttachment
 from sentry.tasks.relay import invalidate_project_config
 from sentry.testutils.cases import TransactionTestCase
-from sentry.testutils.helpers.datetime import before_now, iso_format, timestamp_format
+from sentry.testutils.helpers.datetime import before_now, timestamp_format
 from sentry.testutils.relay import RelayStoreHelper
 from sentry.testutils.skips import requires_kafka
 
@@ -17,7 +17,7 @@ pytestmark = [requires_kafka]
 class SentryRemoteTest(RelayStoreHelper, TransactionTestCase):
     # used to be test_ungzipped_data
     def test_simple_data(self):
-        event_data = {"message": "hello", "timestamp": iso_format(before_now(seconds=1))}
+        event_data = {"message": "hello", "timestamp": before_now(seconds=1).isoformat()}
         event = self.post_and_retrieve_event(event_data)
         assert event.message == "hello"
 
@@ -143,8 +143,8 @@ class SentryRemoteTest(RelayStoreHelper, TransactionTestCase):
             "event_id": "d2132d31b39445f1938d7e21b6bf0ec4",
             "type": "transaction",
             "transaction": "/organizations/:orgId/performance/:eventSlug/",
-            "start_timestamp": iso_format(before_now(minutes=1, milliseconds=500)),
-            "timestamp": iso_format(before_now(minutes=1)),
+            "start_timestamp": before_now(minutes=1, milliseconds=500).isoformat(),
+            "timestamp": before_now(minutes=1).isoformat(),
             "contexts": {
                 "trace": {
                     "trace_id": "ff62a8b040f340bda5d830223def1d81",
@@ -234,6 +234,6 @@ class SentryRemoteTest(RelayStoreHelper, TransactionTestCase):
         with mock.patch(
             "sentry.api.endpoints.relay.project_configs.RelayProjectConfigsEndpoint.post"
         ):
-            event_data = {"message": "hello", "timestamp": iso_format(before_now(seconds=1))}
+            event_data = {"message": "hello", "timestamp": before_now(seconds=1)}
             event = self.post_and_retrieve_event(event_data)
             assert event.message == "hello"
