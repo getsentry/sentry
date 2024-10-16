36adb0d6c0a5e065681171c2db64ad336369c698
Lyn Nagara <1779792+lynnagara@users.noreply.github.com>
2024-10-16 11:48:15 -0700

    ref: cleanup option used for rollout (#79202)
    this has now been successfully rolled out in prod, we can clean up the
    option

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -2793,11 +2793,3 @@ register(
     default=True,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
-
-# TODO: Temporary, to be removed
-register(
-    "split_queue_task_router.enable",
-    type=Bool,
-    default=False,
-    flags=FLAG_AUTOMATOR_MODIFIABLE,
-)
