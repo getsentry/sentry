f339080281a1f79b4d6c10a95e16e59dfc1350ea
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-10-16 17:17:52 -0700

    :white_check_mark: add test with real data

--- tests/sentry/flags/endpoints/test_hooks.py
+++ tests/sentry/flags/endpoints/test_hooks.py
@@ -25,40 +25,141 @@ class OrganizationFlagsHooksEndpointTestCase(APITestCase):
 
         self.client.credentials(HTTP_AUTHORIZATION=f"Bearer {token}")
 
-    def test_launchdarkly_post(self):
+    def test_launchdarkly_post_create(self):
         request_data = {
             "_links": {
-                "property1": {"href": "string", "type": "string"},
-                "property2": {"href": "string", "type": "string"},
-            },
-            "_id": "12345",
-            "_accountId": "67890",
-            "date": 1728680051000,
-            "accesses": [{"action": "deleteFlag", "resource": "string"}],
-            "kind": "string",
-            "name": "Example Flag Name",
-            "description": "Example, turning on the flag for testing",
-            "shortDescription": "Example, turning on the flag",
-            "comment": "This is an automated test",
-            "subject": {
-                "_links": {
-                    "property1": {"href": "string", "type": "string"},
-                    "property2": {"href": "string", "type": "string"},
+                "canonical": {
+                    "href": "/api/v2/flags/default/test-flag",
+                    "type": "application/json",
                 },
-                "name": "string",
-                "avatarUrl": "string",
+                "parent": {"href": "/api/v2/auditlog", "type": "application/json"},
+                "self": {
+                    "href": "/api/v2/auditlog/1234",
+                    "type": "application/json",
+                },
+                "site": {"href": "/default/~/features/test-flag", "type": "text/html"},
             },
+            "_id": "1234",
+            "_accountId": "1234",
+            "date": 1729123465221,
+            "accesses": [
+                {"action": "createFlag", "resource": "proj/default:env/test:flag/test-flag"},
+                {"action": "createFlag", "resource": "proj/default:env/production:flag/test-flag"},
+            ],
+            "kind": "flag",
+            "name": "test flag",
+            "description": "",
+            "shortDescription": "",
             "member": {
                 "_links": {
-                    "property1": {"href": "string", "type": "string"},
-                    "property2": {"href": "string", "type": "string"},
+                    "parent": {"href": "/api/v2/members", "type": "application/json"},
+                    "self": {
+                        "href": "/api/v2/members/1234",
+                        "type": "application/json",
+                    },
                 },
-                "_id": "507f1f77bcf86cd799439011",
-                "email": "jd@test.com",
-                "firstName": "John",
+                "_id": "1234",
+                "email": "michelle@example.com",
+                "firstName": "Michelle",
                 "lastName": "Doe",
             },
-            "titleVerb": "turned on the flag",
+            "titleVerb": "created the flag",
+            "title": "Michelle created the flag [test flag](https://app.launchdarkly.com/default/~/features/test-flag)",
+            "target": {
+                "_links": {
+                    "canonical": {
+                        "href": "/api/v2/flags/default/test-flag",
+                        "type": "application/json",
+                    },
+                    "site": {"href": "/default/~/features/test-flag", "type": "text/html"},
+                },
+                "name": "test flag",
+                "resources": [
+                    "proj/default:env/test:flag/test-flag",
+                    "proj/default:env/production:flag/test-flag",
+                ],
+            },
+            "currentVersion": {
+                "name": "test flag",
+                "kind": "boolean",
+                "description": "testing a feature flag",
+                "key": "test-flag",
+                "_version": 1,
+                "creationDate": 1729123465176,
+                "includeInSnippet": False,
+                "clientSideAvailability": {"usingMobileKey": False, "usingEnvironmentId": False},
+                "variations": [
+                    {"_id": "d883033e-fa8b-41d4-a4be-112d9a59278e", "value": True, "name": "on"},
+                    {"_id": "73aaa33f-c9ca-4bdc-8c97-01a20567aa3f", "value": False, "name": "off"},
+                ],
+                "temporary": False,
+                "tags": [],
+                "_links": {
+                    "parent": {"href": "/api/v2/flags/default", "type": "application/json"},
+                    "self": {"href": "/api/v2/flags/default/test-flag", "type": "application/json"},
+                },
+                "maintainerId": "1234",
+                "_maintainer": {
+                    "_links": {
+                        "self": {
+                            "href": "/api/v2/members/1234",
+                            "type": "application/json",
+                        }
+                    },
+                    "_id": "1234",
+                    "firstName": "Michelle",
+                    "lastName": "Doe",
+                    "role": "owner",
+                    "email": "michelle@example.com",
+                },
+                "goalIds": [],
+                "experiments": {"baselineIdx": 0, "items": []},
+                "customProperties": {},
+                "archived": False,
+                "deprecated": False,
+                "defaults": {"onVariation": 0, "offVariation": 1},
+                "environments": {
+                    "production": {
+                        "on": False,
+                        "archived": False,
+                        "salt": "1234",
+                        "sel": "1234",
+                        "lastModified": 1729123465190,
+                        "version": 1,
+                        "targets": [],
+                        "contextTargets": [],
+                        "rules": [],
+                        "fallthrough": {"variation": 0},
+                        "offVariation": 1,
+                        "prerequisites": [],
+                        "_site": {
+                            "href": "/default/production/features/test-flag",
+                            "type": "text/html",
+                        },
+                        "_environmentName": "Production",
+                        "trackEvents": False,
+                        "trackEventsFallthrough": False,
+                    },
+                    "test": {
+                        "on": False,
+                        "archived": False,
+                        "salt": "7495d3dcf72f43aaa075012fad947d0d",
+                        "sel": "61b4861e6ed54135bc244bb120e9e2da",
+                        "lastModified": 1729123465190,
+                        "version": 1,
+                        "targets": [],
+                        "contextTargets": [],
+                        "rules": [],
+                        "fallthrough": {"variation": 0},
+                        "offVariation": 1,
+                        "prerequisites": [],
+                        "_site": {"href": "/default/test/features/test-flag", "type": "text/html"},
+                        "_environmentName": "Test",
+                        "trackEvents": False,
+                        "trackEventsFallthrough": False,
+                    },
+                },
+            },
         }
 
         token = "sntrys_abc123_xyz"
@@ -83,9 +184,265 @@ class OrganizationFlagsHooksEndpointTestCase(APITestCase):
         assert FlagAuditLogModel.objects.count() == 1
         flag = FlagAuditLogModel.objects.first()
         assert flag is not None
-        assert flag.action == ACTION_MAP["deleted"]
-        assert flag.flag == "Example Flag Name"
-        assert flag.created_by == "jd@test.com"
+        assert flag.action == ACTION_MAP["created"]
+        assert flag.flag == "test flag"
+        assert flag.created_by == "michelle@example.com"
+        assert flag.created_by_type == CREATED_BY_TYPE_MAP["email"]
+        assert flag.organization_id == self.organization.id
+        assert flag.tags is not None
+
+    def test_launchdarkly_post_update(self):
+        request_data = {
+            "_links": {
+                "canonical": {
+                    "href": "/api/v2/flags/default/test-flag",
+                    "type": "application/json",
+                },
+                "parent": {"href": "/api/v2/auditlog", "type": "application/json"},
+                "self": {
+                    "href": "/api/v2/auditlog/1234",
+                    "type": "application/json",
+                },
+                "site": {"href": "/default/test/features/test-flag", "type": "text/html"},
+            },
+            "_id": "1234",
+            "_accountId": "1234",
+            "date": 1729123867537,
+            "accesses": [
+                {"action": "updateOn", "resource": "proj/default:env/test:flag/test-flag"},
+                {"action": "updateFallthrough", "resource": "proj/default:env/test:flag/test-flag"},
+            ],
+            "kind": "flag",
+            "name": "test flag",
+            "description": "* Turned the flag on\n* Changed the default variation from ~~`on`~~ to `off`\n",
+            "shortDescription": "",
+            "comment": "",
+            "member": {
+                "_links": {
+                    "parent": {"href": "/api/v2/members", "type": "application/json"},
+                    "self": {
+                        "href": "/api/v2/members/1234",
+                        "type": "application/json",
+                    },
+                },
+                "_id": "1234",
+                "email": "michelle@example.io",
+                "firstName": "Michelle",
+                "lastName": "Doe",
+            },
+            "titleVerb": "updated the flag",
+            "title": "Michelle updated the flag [test flag](https://app.launchdarkly.com/default/test/features/test-flag) in `Test`",
+            "target": {
+                "_links": {
+                    "canonical": {
+                        "href": "/api/v2/flags/default/test-flag",
+                        "type": "application/json",
+                    },
+                    "site": {"href": "/default/test/features/test-flag", "type": "text/html"},
+                },
+                "name": "test flag",
+                "resources": ["proj/default:env/test:flag/test-flag"],
+            },
+            "parent": {
+                "_links": {
+                    "canonical": {
+                        "href": "/api/v2/projects/default/environments/test",
+                        "type": "application/json",
+                    },
+                    "site": {"href": "/settings/projects", "type": "text/html"},
+                },
+                "name": "Test",
+                "resource": "proj/default:env/test",
+            },
+            "previousVersion": {
+                "name": "test flag",
+                "kind": "boolean",
+                "description": "testing a feature flag",
+                "key": "test-flag",
+                "_version": 1,
+                "creationDate": 1729123465176,
+                "includeInSnippet": False,
+                "clientSideAvailability": {"usingMobileKey": False, "usingEnvironmentId": False},
+                "variations": [
+                    {"_id": "1234", "value": True, "name": "on"},
+                    {"_id": "1234", "value": False, "name": "off"},
+                ],
+                "temporary": True,
+                "tags": [],
+                "_links": {
+                    "parent": {"href": "/api/v2/flags/default", "type": "application/json"},
+                    "self": {"href": "/api/v2/flags/default/test-flag", "type": "application/json"},
+                },
+                "maintainerId": "1234",
+                "_maintainer": {
+                    "_links": {
+                        "self": {
+                            "href": "/api/v2/members/1234",
+                            "type": "application/json",
+                        }
+                    },
+                    "_id": "1234",
+                    "firstName": "Michelle",
+                    "lastName": "Doe",
+                    "role": "owner",
+                    "email": "michelle@example.io",
+                },
+                "goalIds": [],
+                "experiments": {"baselineIdx": 0, "items": []},
+                "customProperties": {},
+                "archived": False,
+                "deprecated": False,
+                "defaults": {"onVariation": 0, "offVariation": 1},
+                "environments": {
+                    "production": {
+                        "on": False,
+                        "archived": False,
+                        "salt": "ee006f821c8443538c49541256188073",
+                        "sel": "d928ffd4789949d3b8956a35e3ded70f",
+                        "lastModified": 1729123465190,
+                        "version": 1,
+                        "targets": [],
+                        "contextTargets": [],
+                        "rules": [],
+                        "fallthrough": {"variation": 0},
+                        "offVariation": 1,
+                        "prerequisites": [],
+                        "_site": {
+                            "href": "/default/production/features/test-flag",
+                            "type": "text/html",
+                        },
+                        "_environmentName": "Production",
+                        "trackEvents": False,
+                        "trackEventsFallthrough": False,
+                    },
+                    "test": {
+                        "on": False,
+                        "archived": False,
+                        "salt": "7495d3dcf72f43aaa075012fad947d0d",
+                        "sel": "61b4861e6ed54135bc244bb120e9e2da",
+                        "lastModified": 1729123465190,
+                        "version": 1,
+                        "targets": [],
+                        "contextTargets": [],
+                        "rules": [],
+                        "fallthrough": {"variation": 0},
+                        "offVariation": 1,
+                        "prerequisites": [],
+                        "_site": {"href": "/default/test/features/test-flag", "type": "text/html"},
+                        "_environmentName": "Test",
+                        "trackEvents": False,
+                        "trackEventsFallthrough": False,
+                    },
+                },
+            },
+            "currentVersion": {
+                "name": "test flag",
+                "kind": "boolean",
+                "description": "testing a feature flag",
+                "key": "test-flag",
+                "_version": 1,
+                "creationDate": 1729123465176,
+                "includeInSnippet": False,
+                "clientSideAvailability": {"usingMobileKey": False, "usingEnvironmentId": False},
+                "variations": [
+                    {"_id": "1234", "value": True, "name": "on"},
+                    {"_id": "1234", "value": False, "name": "off"},
+                ],
+                "temporary": True,
+                "tags": [],
+                "_links": {
+                    "parent": {"href": "/api/v2/flags/default", "type": "application/json"},
+                    "self": {"href": "/api/v2/flags/default/test-flag", "type": "application/json"},
+                },
+                "maintainerId": "1234",
+                "_maintainer": {
+                    "_links": {
+                        "self": {
+                            "href": "/api/v2/members/1234",
+                            "type": "application/json",
+                        }
+                    },
+                    "_id": "1234",
+                    "firstName": "Michelle",
+                    "lastName": "Doe",
+                    "role": "owner",
+                    "email": "michelle@example.io",
+                },
+                "goalIds": [],
+                "experiments": {"baselineIdx": 0, "items": []},
+                "customProperties": {},
+                "archived": False,
+                "deprecated": False,
+                "defaults": {"onVariation": 0, "offVariation": 1},
+                "environments": {
+                    "production": {
+                        "on": False,
+                        "archived": False,
+                        "salt": "ee006f821c8443538c49541256188073",
+                        "sel": "d928ffd4789949d3b8956a35e3ded70f",
+                        "lastModified": 1729123465190,
+                        "version": 1,
+                        "targets": [],
+                        "contextTargets": [],
+                        "rules": [],
+                        "fallthrough": {"variation": 0},
+                        "offVariation": 1,
+                        "prerequisites": [],
+                        "_site": {
+                            "href": "/default/production/features/test-flag",
+                            "type": "text/html",
+                        },
+                        "_environmentName": "Production",
+                        "trackEvents": False,
+                        "trackEventsFallthrough": False,
+                    },
+                    "test": {
+                        "on": False,
+                        "archived": False,
+                        "salt": "7495d3dcf72f43aaa075012fad947d0d",
+                        "sel": "61b4861e6ed54135bc244bb120e9e2da",
+                        "lastModified": 1729123867478,
+                        "version": 2,
+                        "targets": [],
+                        "contextTargets": [],
+                        "rules": [],
+                        "fallthrough": {"variation": 1},
+                        "offVariation": 1,
+                        "prerequisites": [],
+                        "_site": {"href": "/default/test/features/test-flag", "type": "text/html"},
+                        "_environmentName": "Test",
+                        "trackEvents": False,
+                        "trackEventsFallthrough": False,
+                    },
+                },
+            },
+        }
+
+        token = "sntrys_abc123_xyz"
+        self.create_org_auth_token(
+            name="Test Token 1",
+            token_hashed=hash_token(token),
+            organization_id=self.organization.id,
+            token_last_characters="xyz",
+            scope_list=["org:ci"],
+            date_last_used=None,
+        )
+
+        url = reverse(self.endpoint, args=(self.organization.slug, "launchdarkly"))
+
+        response = self.client.post(
+            url,
+            request_data,
+            HTTP_AUTHORIZATION=f"Bearer {token}",
+        )
+
+        assert response.status_code == 200
+        assert FlagAuditLogModel.objects.count() == 1
+        flag = FlagAuditLogModel.objects.first()
+        assert flag is not None
+        assert flag.action == ACTION_MAP["updated"]
+        assert flag.flag == "test flag"
+        assert flag.created_by == "michelle@example.io"
         assert flag.created_by_type == CREATED_BY_TYPE_MAP["email"]
         assert flag.organization_id == self.organization.id
         assert flag.tags is not None
