ab32abab3c7a87f8595a11d9248b1fa2d0e5d736
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-10-17 16:08:40 -0700

    Add save_userreport and crash report form coverage

--- tests/sentry/ingest/test_userreport.py
+++ tests/sentry/ingest/test_userreport.py
@@ -1,4 +1,7 @@
-from sentry.feedback.usecases.create_feedback import UNREAL_FEEDBACK_UNATTENDED_MESSAGE
+from sentry.feedback.usecases.create_feedback import (
+    UNREAL_FEEDBACK_UNATTENDED_MESSAGE,
+    FeedbackCreationSource,
+)
 from sentry.ingest.userreport import save_userreport, should_filter_user_report
 from sentry.models.userreport import UserReport
 from sentry.testutils.pytest.fixtures import django_db_all
@@ -44,7 +47,7 @@ def test_save_user_report_returns_instance(set_sentry_option, default_project, m
     }
 
     # Call the function
-    result = save_userreport(default_project, report, "api")
+    result = save_userreport(default_project, report, FeedbackCreationSource.USER_REPORT_ENVELOPE)
 
     # Assert the result is an instance of UserReport
     assert isinstance(result, UserReport)
@@ -61,6 +64,33 @@ def test_save_user_report_denylist(set_sentry_option, default_project, monkeypat
         "project_id": default_project.id,
     }
 
-    result = save_userreport(default_project, report, "api")
+    result = save_userreport(default_project, report, FeedbackCreationSource.USER_REPORT_ENVELOPE)
 
     assert result is None
+
+
+@django_db_all
+def test_save_user_report_large_message_truncated(default_project, monkeypatch):
+    # Mocking dependencies and setting up test data
+    monkeypatch.setattr("sentry.ingest.userreport.is_in_feedback_denylist", lambda org: False)
+    monkeypatch.setattr("sentry.ingest.userreport.should_filter_user_report", lambda message: False)
+    monkeypatch.setattr(
+        "sentry.eventstore.backend.get_event_by_id", lambda project_id, event_id: None
+    )
+
+    max_length = UserReport._meta.get_field("comments").max_length
+    report = {
+        "event_id": "123456",
+        "name": "Test User",
+        "email": "test@example.com",
+        "comments": "a" * (max_length + 3001),
+        "project_id": default_project.id,
+    }
+
+    result = save_userreport(default_project, report, FeedbackCreationSource.USER_REPORT_ENVELOPE)
+    saved_result = UserReport.objects.get()
+    assert isinstance(result, UserReport)
+    assert result.id == saved_result.id
+
+    assert len(result.comments) == max_length
+    assert len(saved_result.comments) == max_length
