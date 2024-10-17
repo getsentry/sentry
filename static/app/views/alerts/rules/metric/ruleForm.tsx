68603477adb4e1f09d4a90b3b5c2b719d7c004b3
Nate Moore <natemoo-re@users.noreply.github.com>
2024-10-17 13:23:54 -0700

    fix(anomaly detection): handle multiple requests to endpoint (#79291)
    Closes ALRT-348

--- static/app/views/alerts/rules/metric/ruleForm.tsx
+++ static/app/views/alerts/rules/metric/ruleForm.tsx
@@ -1057,21 +1057,20 @@ class RuleFormContainer extends DeprecatedAsyncComponent<Props, State> {
   }
 
   async fetchAnomalies() {
-    if (this.state.comparisonType !== AlertRuleComparisonType.DYNAMIC) {
+    const {comparisonType, historicalData, currentData} = this.state;
+
+    if (
+      comparisonType !== AlertRuleComparisonType.DYNAMIC ||
+      !(Array.isArray(currentData) && Array.isArray(historicalData)) ||
+      currentData.length === 0 ||
+      historicalData.length === 0
+    ) {
       return;
     }
+    this.setState({chartError: false, chartErrorMessage: ''});
+
     const {organization, project} = this.props;
-    const {
-      timeWindow,
-      sensitivity,
-      seasonality,
-      thresholdType,
-      historicalData,
-      currentData,
-    } = this.state;
-    if (!(Array.isArray(currentData) && Array.isArray(historicalData))) {
-      return;
-    }
+    const {timeWindow, sensitivity, seasonality, thresholdType} = this.state;
     const direction =
       thresholdType === AlertRuleThresholdType.ABOVE
         ? 'up'
