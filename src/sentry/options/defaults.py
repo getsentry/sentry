476232a9bed01c5f3325d4eaa8a289e7f3343a41
Joshua Ferge <josh.ferge@sentry.io>
2024-10-18 16:44:44 -0400

    add rollout option

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -2781,3 +2781,10 @@ register(
     default=True,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
+
+register(
+    "eventstore.adjacent_event_ids_use_snql",
+    type=Bool,
+    default=False,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
