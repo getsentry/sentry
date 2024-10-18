6cbb256706a446fc2cc111a018ab5185f473771f
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2024-10-18 12:54:03 -0700

    feat(onboarding): add messaging integration onboarding to project creation (#79194)
    adding an option on the project creation page to send alerts to a
    messaging integration (Slack, Discord, MS Teams, etc.)
    
    email alert rules are still created as well (the checkbox to "notify via
    email" is selected by default, and can not be deselected)
    
    if org has no integrations installed:
    
    
    https://github.com/user-attachments/assets/5e015fa5-f98e-4806-b39c-113bd181481f
    
    *notice how the dropdowns to select an integration provider (slack,
    discord, ms teams) and workspace are disabled, because there is only 1
    choice for each
    
    if org has multiple integrations installed:
    
    
    https://github.com/user-attachments/assets/9de014eb-e89a-4532-b56f-e757fd495c0a
    
    if the org is not on a team plan or above, the button is disabled:
    
    <img width="785" alt="Screenshot 2024-10-16 at 9 53 32 AM"
    src="https://github.com/user-attachments/assets/8d2807f9-68c2-4abe-947d-285242779323">
    
    if the user has selected "notify via integration" but they have not
    input a channel ID, the create project button is disabled:
    
    <img width="934" alt="Screenshot 2024-10-17 at 11 42 04 AM"
    src="https://github.com/user-attachments/assets/ed62f9d7-6170-45ec-98ad-387309fcfb00">
    
    error messages are displayed at the bottom (existing behavior):
    
    <img width="923" alt="Screenshot 2024-10-17 at 11 41 43 AM"
    src="https://github.com/user-attachments/assets/a238a984-d056-48f0-9b16-ef028e43f91f">

--- static/app/views/projectInstall/newProject.spec.tsx
+++ static/app/views/projectInstall/newProject.spec.tsx
@@ -1,3 +1,5 @@
+import {OrganizationFixture} from 'sentry-fixture/organization';
+import {OrganizationIntegrationsFixture} from 'sentry-fixture/organizationIntegrations';
 import {MOCK_RESP_VERBOSE} from 'sentry-fixture/ruleConditions';
 
 import {render} from 'sentry-test/reactTestingLibrary';
@@ -5,10 +7,22 @@ import {render} from 'sentry-test/reactTestingLibrary';
 import NewProject from 'sentry/views/projectInstall/newProject';
 
 describe('NewProjectPlatform', function () {
+  const organization = OrganizationFixture();
+  const integrations = [
+    OrganizationIntegrationsFixture({
+      name: "Moo Deng's Workspace",
+      status: 'active',
+    }),
+  ];
+
   beforeEach(() => {
     MockApiClient.addMockResponse({
       url: `/projects/org-slug/rule-conditions/`,
       body: MOCK_RESP_VERBOSE,
+    });
+    MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/integrations/?integrationType=messaging`,
+      body: integrations,
     });
   });
 
