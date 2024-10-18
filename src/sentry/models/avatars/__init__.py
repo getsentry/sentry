ab0c75ba51f14fd0bda273c71d0ae7c243d02f49
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2024-10-18 10:29:15 -0700

    chore(sentry apps): move sentry app avatar to sentry_apps/ (#79324)

--- src/sentry/models/avatars/__init__.py
+++ src/sentry/models/avatars/__init__.py
@@ -1,11 +1,9 @@
 from .base import AvatarBase
 from .control_base import ControlAvatarBase
 from .organization_avatar import OrganizationAvatar
-from .sentry_app_avatar import SentryAppAvatar
 
 __all__ = (
     "AvatarBase",
     "ControlAvatarBase",
     "OrganizationAvatar",
-    "SentryAppAvatar",
 )
