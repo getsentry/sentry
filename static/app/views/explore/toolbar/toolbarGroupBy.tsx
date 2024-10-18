9954d75e1afdbde489893ce3159a6e227fc5265f
Abdullah Khan <60121741+Abdkhan14@users.noreply.github.com>
2024-10-18 10:59:39 -0400

    feat(explore): Making group by rows draggable  (#79213)
    <img width="1277" alt="Screenshot 2024-10-17 at 4 29 25 AM"
    src="https://github.com/user-attachments/assets/c2fe566e-71f6-4405-a427-42a39094b005">
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/explore/toolbar/toolbarGroupBy.tsx
+++ static/app/views/explore/toolbar/toolbarGroupBy.tsx
@@ -1,24 +1,26 @@
-import {useCallback, useMemo} from 'react';
+import {useMemo} from 'react';
+import {useSortable} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
+import styled from '@emotion/styled';
 
 import {Button} from 'sentry/components/button';
-import type {SelectOption} from 'sentry/components/compactSelect';
+import type {SelectKey, SelectOption} from 'sentry/components/compactSelect';
 import {CompactSelect} from 'sentry/components/compactSelect';
 import {Tooltip} from 'sentry/components/tooltip';
 import {IconAdd} from 'sentry/icons/iconAdd';
 import {IconDelete} from 'sentry/icons/iconDelete';
+import {IconGrabbable} from 'sentry/icons/iconGrabbable';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {defined} from 'sentry/utils';
 import {useGroupBys} from 'sentry/views/explore/hooks/useGroupBys';
-import type {Field} from 'sentry/views/explore/hooks/useSampleFields';
 
+import {DragNDropContext} from '../contexts/dragNDropContext';
 import {useSpanTags} from '../contexts/spanTagsContext';
+import type {Column} from '../hooks/useDragNDropColumns';
+import {useResultMode} from '../hooks/useResultsMode';
 
-import {
-  ToolbarHeader,
-  ToolbarHeaderButton,
-  ToolbarLabel,
-  ToolbarRow,
-  ToolbarSection,
-} from './styles';
+import {ToolbarHeader, ToolbarHeaderButton, ToolbarLabel, ToolbarSection} from './styles';
 
 interface ToolbarGroupByProps {
   disabled?: boolean;
@@ -26,10 +28,11 @@ interface ToolbarGroupByProps {
 
 export function ToolbarGroupBy({disabled}: ToolbarGroupByProps) {
   const tags = useSpanTags();
+  const [resultMode] = useResultMode();
 
   const {groupBys, setGroupBys} = useGroupBys();
 
-  const options: SelectOption<Field>[] = useMemo(() => {
+  const options: SelectOption<string>[] = useMemo(() => {
     // These options aren't known to exist on this project but it was inserted into
     // the group bys somehow so it should be a valid options in the group bys.
     //
@@ -61,68 +64,146 @@ export function ToolbarGroupBy({disabled}: ToolbarGroupByProps) {
     ];
   }, [groupBys, tags]);
 
-  const addGroupBy = useCallback(() => {
-    setGroupBys([...groupBys, '']);
-  }, [setGroupBys, groupBys]);
-
-  const setGroupBy = useCallback(
-    (i: number, {value}: SelectOption<Field>) => {
-      const newGroupBys = groupBys.slice();
-      newGroupBys[i] = value;
-      setGroupBys(newGroupBys);
-    },
-    [setGroupBys, groupBys]
-  );
-
-  const deleteGroupBy = useCallback(
-    (index: number) => {
-      const newGroupBys = groupBys.filter((_, orgIndex) => index !== orgIndex);
-      setGroupBys?.(newGroupBys);
-    },
-    [setGroupBys, groupBys]
-  );
-
   return (
-    <ToolbarSection data-test-id="section-group-by">
-      <ToolbarHeader>
-        <Tooltip
-          position="right"
-          title={t(
-            'Aggregated data by a key attribute to calculate averages, percentiles, count and more'
-          )}
-        >
-          <ToolbarLabel disabled={disabled}>{t('Group By')}</ToolbarLabel>
-        </Tooltip>
-        <ToolbarHeaderButton
-          disabled={disabled}
-          size="zero"
-          onClick={addGroupBy}
-          borderless
-          aria-label={t('Add Group')}
-          icon={<IconAdd />}
-        />
-      </ToolbarHeader>
-      <div>
-        {groupBys.map((groupBy, index) => (
-          <ToolbarRow key={index}>
-            <CompactSelect
-              searchable
+    <DragNDropContext columns={groupBys} setColumns={setGroupBys}>
+      {({editableColumns, insertColumn, updateColumnAtIndex, deleteColumnAtIndex}) => (
+        <ToolbarSection data-test-id="section-group-by">
+          <StyledToolbarHeader>
+            <Tooltip
+              position="right"
+              title={t(
+                'Aggregated data by a key attribute to calculate averages, percentiles, count and more'
+              )}
+            >
+              <ToolbarLabel disabled={disabled}>{t('Group By')}</ToolbarLabel>
+            </Tooltip>
+            <ToolbarHeaderButton
               disabled={disabled}
-              options={options}
-              value={groupBy}
-              onChange={newGroupBy => setGroupBy(index, newGroupBy)}
-            />
-            <Button
-              borderless
-              icon={<IconDelete />}
               size="zero"
-              disabled={disabled || (groupBys.length <= 1 && groupBy === '')}
-              onClick={() => deleteGroupBy(index)}
-              aria-label={t('Remove Group')}
+              onClick={insertColumn}
+              borderless
+              aria-label={t('Add Group')}
+              icon={<IconAdd />}
             />
-          </ToolbarRow>
-        ))}
-      </div>
-    </ToolbarSection>
+          </StyledToolbarHeader>
+          {editableColumns.map((column, i) => (
+            <ColumnEditorRow
+              disabled={resultMode === 'samples'}
+              key={column.id}
+              canDelete={
+                editableColumns.length > 1 || !['', undefined].includes(column.column)
+              }
+              column={column}
+              options={options}
+              onColumnChange={c => updateColumnAtIndex(i, c)}
+              onColumnDelete={() => deleteColumnAtIndex(i)}
+            />
+          ))}
+        </ToolbarSection>
+      )}
+    </DragNDropContext>
   );
 }
+
+const StyledToolbarHeader = styled(ToolbarHeader)`
+  margin-bottom: ${space(1)};
+`;
+
+interface ColumnEditorRowProps {
+  canDelete: boolean;
+  column: Column;
+  onColumnChange: (column: string) => void;
+  onColumnDelete: () => void;
+  options: SelectOption<string>[];
+  disabled?: boolean;
+}
+
+function ColumnEditorRow({
+  canDelete,
+  column,
+  options,
+  onColumnChange,
+  onColumnDelete,
+  disabled = false,
+}: ColumnEditorRowProps) {
+  const {attributes, listeners, setNodeRef, transform, transition} = useSortable({
+    id: column.id,
+  });
+
+  function handleColumnChange(option: SelectOption<SelectKey>) {
+    if (defined(option) && typeof option.value === 'string') {
+      onColumnChange(option.value);
+    }
+  }
+
+  const label = useMemo(() => {
+    const tag = options.find(option => option.value === column.column);
+    return <TriggerLabel>{tag?.label ?? t('None')}</TriggerLabel>;
+  }, [column.column, options]);
+
+  return (
+    <RowContainer
+      key={column.id}
+      ref={setNodeRef}
+      style={{
+        transform: CSS.Transform.toString(transform),
+        transition,
+      }}
+      {...attributes}
+    >
+      <Button
+        aria-label={t('Drag to reorder')}
+        borderless
+        size="zero"
+        disabled={disabled}
+        icon={<IconGrabbable size="sm" />}
+        {...listeners}
+      />
+      <StyledCompactSelect
+        data-test-id="editor-column"
+        options={options}
+        triggerLabel={label}
+        disabled={disabled}
+        value={column.column ?? ''}
+        onChange={handleColumnChange}
+        searchable
+        triggerProps={{
+          style: {
+            width: '100%',
+          },
+        }}
+      />
+      <Button
+        aria-label={t('Remove Column')}
+        borderless
+        disabled={!canDelete || disabled}
+        size="zero"
+        icon={<IconDelete size="sm" />}
+        onClick={() => onColumnDelete()}
+      />
+    </RowContainer>
+  );
+}
+
+const RowContainer = styled('div')`
+  display: flex;
+  flex-direction: row;
+  gap: ${space(0.5)};
+
+  :not(:first-child) {
+    margin-top: ${space(1)};
+  }
+`;
+
+const StyledCompactSelect = styled(CompactSelect)`
+  flex-grow: 1;
+  min-width: 0;
+`;
+
+const TriggerLabel = styled('span')`
+  ${p => p.theme.overflowEllipsis}
+  text-align: left;
+  line-height: normal;
+  position: relative;
+  font-weight: normal;
+`;
