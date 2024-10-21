98048b44e8a4ff8e48f268f55b988040368ea9b0
mdtro <20070360+mdtro@users.noreply.github.com>
2024-10-21 17:14:46 -0500

    fix typing

--- tests/sentry/users/api/endpoints/test_user_authenticator_details.py
+++ tests/sentry/users/api/endpoints/test_user_authenticator_details.py
@@ -294,11 +294,13 @@ class UserAuthenticatorDetailsTest(UserAuthenticatorDetailsTestBase):
         # Enroll the user in TOTP
         totp_interface = TotpInterface()
         totp_interface.enroll(self.user)
+        assert totp_interface.authenticator is not None
         totp_auth = totp_interface.authenticator
 
         # Enroll the user in recovery codes
         recovery_interface = RecoveryCodeInterface()
         recovery_interface.enroll(self.user)
+        assert recovery_interface.authenticator is not None
         recovery_auth = recovery_interface.authenticator
 
         # Confirm both authenticators exist
