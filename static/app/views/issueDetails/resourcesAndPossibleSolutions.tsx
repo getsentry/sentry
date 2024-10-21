9559fa889a7bcdbe1fc845e4073a0daf7a315ac1
Rohan Agarwal <47861399+roaga@users.noreply.github.com>
2024-10-21 13:04:18 -0700

    fix(autofix): Check for gen AI consent without API request (#79446)
    Before we were using the useAutofix hook to make an API request to check
    for gen AI consent. Now we can just read the Organization object and cut
    that out.
    
    It should stop a lot of errors we're seeing in Sentry and Seer where
    gitlab users are getting an error when hitting this endpoint just by
    opening the issue details page (not that any user or functionality is
    actually affected, but it'll stop the events from piling up)

--- static/app/views/issueDetails/resourcesAndPossibleSolutions.tsx
+++ static/app/views/issueDetails/resourcesAndPossibleSolutions.tsx
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 
 import {AiSuggestedSolution} from 'sentry/components/events/aiSuggestedSolution';
 import {Autofix} from 'sentry/components/events/autofix';
-import {useAutofixSetup} from 'sentry/components/events/autofix/useAutofixSetup';
 import {Resources} from 'sentry/components/events/interfaces/performance/resources';
 import {t} from 'sentry/locale';
 import ConfigStore from 'sentry/stores/configStore';
@@ -52,14 +51,11 @@ export function ResourcesAndPossibleSolutions({event, project, group}: Props) {
   const config = getConfigForIssueType(group, project);
   const isSelfHostedErrorsOnly = ConfigStore.get('isSelfHostedErrorsOnly');
   const isSampleError = useIsSampleEvent();
-  const {genAIConsent} = useAutofixSetup({
-    groupId: group.id,
-  });
 
   const displayAiAutofix =
     ((organization.features.includes('autofix') &&
       organization.features.includes('issue-details-autofix-ui')) ||
-      genAIConsent) &&
+      organization.genAIConsent) &&
     !shouldShowCustomErrorResourceConfig(group, project) &&
     config.autofix &&
     hasStacktraceWithFrames(event) &&
