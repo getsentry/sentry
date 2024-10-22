cd63cc34dbb01a3e0732962ff6a9fca738757f9a
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-10-22 14:42:47 -0400

    add alpha badge

--- static/app/views/insights/pages/settings.ts
+++ static/app/views/insights/pages/settings.ts
@@ -1,4 +1,6 @@
+import type {BadgeType} from 'sentry/components/badge/featureBadge';
 import {t} from 'sentry/locale';
 
 export const OVERVIEW_PAGE_TITLE = t('Overview');
 export const DOMAIN_VIEW_BASE_URL = 'performance';
+export const DOMAIN_VIEW_RELEAE_LEVEL: BadgeType = 'alpha';
