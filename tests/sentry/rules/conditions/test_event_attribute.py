4720b7f313c91e3c8e26c94d9ab8ddd29d3f1226
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2024-10-22 10:03:04 -0700

    ref(alerts): use registry pattern for EventAttributeCondition attempt 2 (#79456)

--- tests/sentry/rules/conditions/test_event_attribute.py
+++ tests/sentry/rules/conditions/test_event_attribute.py
@@ -1,3 +1,5 @@
+from unittest.mock import patch
+
 from sentry.rules.conditions.event_attribute import EventAttributeCondition
 from sentry.rules.match import MatchType
 from sentry.testutils.cases import RuleTestCase
@@ -336,6 +338,14 @@ class EventAttributeConditionTest(RuleTestCase):
         )
         self.assertDoesNotPass(rule, event)
 
+    @patch("sentry.eventstore.models.get_interfaces", return_value={})
+    def test_exception_type_keyerror(self, mock_interface):
+        event = self.get_event()
+        rule = self.get_rule(
+            data={"match": MatchType.EQUAL, "attribute": "exception.type", "value": "SyntaxError"}
+        )
+        self.assertDoesNotPass(rule, event)
+
     def test_error_handled(self):
         event = self.get_event(
             exception={
@@ -362,6 +372,24 @@ class EventAttributeConditionTest(RuleTestCase):
         event = self.get_event()
         rule = self.get_rule(
             data={"match": MatchType.EQUAL, "attribute": "error.handled", "value": "True"}
+        )
+        self.assertDoesNotPass(rule, event)
+
+    @patch("sentry.eventstore.models.get_interfaces", return_value={})
+    def test_error_handled_keyerror(self, mock_interface):
+        event = self.get_event(
+            exception={
+                "values": [
+                    {
+                        "type": "Generic",
+                        "value": "hello world",
+                        "mechanism": {"type": "UncaughtExceptionHandler", "handled": False},
+                    }
+                ]
+            }
+        )
+        rule = self.get_rule(
+            data={"match": MatchType.EQUAL, "attribute": "error.handled", "value": "False"}
         )
         self.assertDoesNotPass(rule, event)
 
