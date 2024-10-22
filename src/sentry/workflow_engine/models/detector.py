7ca4942941491e45ae6c8643600970950ec3a3c9
Dan Fuller <dfuller@sentry.io>
2024-10-22 15:20:29 -0700

    feat(workflow-engine): Add basic `StatefulDetectorHandler` that can commit changes from a workflow
    This adds part of `StatefulDetectorHandler`. The main method here is `commit_state_update_data`, which is intended to be used once related workflows have been completed and we want to commit all the changes to the detector state.

--- src/sentry/workflow_engine/models/detector.py
+++ src/sentry/workflow_engine/models/detector.py
@@ -3,22 +3,35 @@ from __future__ import annotations
 import abc
 import dataclasses
 import logging
+from datetime import timedelta
 from typing import TYPE_CHECKING, Any, Generic, TypeVar
 
+from django.conf import settings
 from django.db import models
-from django.db.models import UniqueConstraint
+from django.db.models import Q, UniqueConstraint
+from sentry_redis_tools.retrying_cluster import RetryingRedisCluster
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import DefaultFieldsModel, FlexibleForeignKey, region_silo_model
 from sentry.issues import grouptype
 from sentry.models.owner_base import OwnerModel
 from sentry.types.group import PriorityLevel
+from sentry.utils import redis
 from sentry.workflow_engine.models import DataPacket
+from sentry.workflow_engine.models.detector_state import DetectorState
 
 if TYPE_CHECKING:
     from sentry.workflow_engine.models.detector_state import DetectorStatus
 
 logger = logging.getLogger(__name__)
+
+
+REDIS_TTL = int(timedelta(days=7).total_seconds())
+
+
+def get_redis_client() -> RetryingRedisCluster:
+    cluster_key = settings.SENTRY_WORKFLOW_ENGINE_REDIS_CLUSTER
+    return redis.redis_clusters.get(cluster_key)  # type: ignore[return-value]
 
 
 @region_silo_model
@@ -89,8 +102,10 @@ class DetectorStateData:
     # reprocessing
     dedupe_value: int
     # Stateful detectors allow various counts to be tracked. We need to update these after we process workflows, so
-    # include the updates in the state
-    counter_updates: dict[str, int]
+    # include the updates in the state.
+    # This dictionary is in the format {counter_name: counter_value, ...}
+    # If a counter value is `None` it means to unset the value
+    counter_updates: dict[str, int | None]
 
 
 @dataclasses.dataclass(frozen=True)
@@ -111,3 +126,80 @@ class DetectorHandler(abc.ABC, Generic[T]):
     @abc.abstractmethod
     def evaluate(self, data_packet: DataPacket[T]) -> list[DetectorEvaluationResult]:
         pass
+
+
+class StatefulDetectorHandler(DetectorHandler[T], abc.ABC):
+    def build_dedupe_value_key(self, group_key: str | None) -> str:
+        if group_key is None:
+            group_key = ""
+        return f"{self.detector.id}:{group_key}:dedupe_value"
+
+    def build_counter_value_key(self, group_key: str | None, counter_name: str) -> str:
+        if group_key is None:
+            group_key = ""
+        return f"{self.detector.id}:{group_key}:{counter_name}"
+
+    def bulk_get_detector_state(
+        self, state_updates: list[DetectorStateData]
+    ) -> dict[str | None, DetectorState]:
+        group_keys = {update.group_key for update in state_updates}
+        query_filter = Q(
+            detector_group_key__in=[group_key for group_key in group_keys if group_key is not None]
+        )
+        if None in group_keys:
+            query_filter |= Q(detector_group_key__isnull=True)
+
+        return {
+            detector_state.detector_group_key: detector_state
+            for detector_state in self.detector.detectorstate_set.filter(query_filter)
+        }
+
+    def commit_state_update_data(self, state_updates: list[DetectorStateData]):
+        detector_state_lookup = self.bulk_get_detector_state(state_updates)
+        created_detector_states = []
+        updated_detector_states = []
+        dedupe_values = []
+        group_counter_updates = {}
+        for state_update in state_updates:
+            detector_state = detector_state_lookup.get(state_update.group_key)
+            if not detector_state:
+                created_detector_states.append(
+                    DetectorState(
+                        detector_group_key=state_update.group_key,
+                        detector=self.detector,
+                        active=state_update.active,
+                        state=state_update.status,
+                    )
+                )
+            elif (
+                state_update.active != detector_state.active
+                or state_update.status != detector_state.state
+            ):
+                detector_state.active = state_update.active
+                detector_state.state = state_update.status
+                updated_detector_states.append(detector_state)
+
+            dedupe_values.append((state_update.group_key, state_update.dedupe_value))
+            group_counter_updates[state_update.group_key] = state_update.counter_updates
+
+        if created_detector_states:
+            DetectorState.objects.bulk_create(created_detector_states)
+
+        if updated_detector_states:
+            DetectorState.objects.bulk_update(updated_detector_states, ["active", "state"])
+
+        pipeline = get_redis_client().pipeline()
+        if dedupe_values:
+            for group_key, dedupe_value in dedupe_values:
+                pipeline.set(self.build_dedupe_value_key(group_key), dedupe_value, ex=REDIS_TTL)
+
+        if group_counter_updates:
+            for group_key, counter_updates in group_counter_updates.items():
+                for counter_name, counter_value in counter_updates.items():
+                    key_name = self.build_counter_value_key(group_key, counter_name)
+                    if counter_value is None:
+                        pipeline.delete(key_name)
+                    else:
+                        pipeline.set(key_name, counter_value, ex=REDIS_TTL)
+
+        pipeline.execute()
