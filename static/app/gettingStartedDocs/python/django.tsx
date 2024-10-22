7805e56c6a27bc065afbd38eaba3898651207762
Tony Xiao <txiao@sentry.io>
2024-10-22 11:59:41 -0400

    feat(profiling): Add continuous profiling onboarding docs for python (#79464)
    This adds continuous profiling onboarding docs for all python platforms.

--- static/app/gettingStartedDocs/python/django.tsx
+++ static/app/gettingStartedDocs/python/django.tsx
@@ -8,7 +8,10 @@ import {
 } from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {getPythonMetricsOnboarding} from 'sentry/components/onboarding/gettingStartedDoc/utils/metricsOnboarding';
 import replayOnboardingJsLoader from 'sentry/gettingStartedDocs/javascript/jsLoader/jsLoader';
-import {crashReportOnboardingPython} from 'sentry/gettingStartedDocs/python/python';
+import {
+  AlternativeConfiguration,
+  crashReportOnboardingPython,
+} from 'sentry/gettingStartedDocs/python/python';
 import {t, tct} from 'sentry/locale';
 
 type Params = DocsParams;
@@ -27,13 +30,23 @@ sentry_sdk.init(
     traces_sample_rate=1.0,`
         : ''
     }${
-      params.isProfilingSelected
+      params.isProfilingSelected &&
+      params.profilingOptions?.defaultProfilingMode === 'transaction'
         ? `
     # Set profiles_sample_rate to 1.0 to profile 100%
     # of sampled transactions.
     # We recommend adjusting this value in production.
     profiles_sample_rate=1.0,`
-        : ''
+        : params.isProfilingSelected &&
+            params.profilingOptions?.defaultProfilingMode === 'continuous'
+          ? `
+    _experiments={
+        # Set continuous_profiling_auto_start to True
+        # to automatically start the profiler on when
+        # possible.
+        "continuous_profiling_auto_start": True,
+    },`
+          : ''
     }
 )
 `;
@@ -86,6 +99,7 @@ const onboarding: OnboardingConfig = {
           ],
         },
       ],
+      additionalInfo: <AlternativeConfiguration />,
     },
   ],
   verify: () => [
