8297bfb063823c2ec8d8a5761a7066696feb0270
Neo Huang <126607112+kneeyo1@users.noreply.github.com>
2024-10-23 15:18:06 -0700

    Splitting RC-Processing  (#79598)
    repeat of #79200  
    This splits up that PR so that we change `save_event` first before the
    kafka consumers.
    
    ---------
    
    Co-authored-by: Mark Story <mark@mark-story.com>

--- src/sentry/tasks/store.py
+++ src/sentry/tasks/store.py
@@ -17,6 +17,7 @@ from sentry.constants import DEFAULT_STORE_NORMALIZER_ARGS
 from sentry.datascrubbing import scrub_data
 from sentry.eventstore import processing
 from sentry.feedback.usecases.create_feedback import FeedbackCreationSource, create_feedback_issue
+from sentry.ingest.types import ConsumerType
 from sentry.killswitches import killswitch_matches_context
 from sentry.lang.native.symbolicator import SymbolicatorTaskKind
 from sentry.models.organization import Organization
@@ -492,6 +493,7 @@ def _do_save_event(
     event_id: str | None = None,
     project_id: int | None = None,
     has_attachments: bool = False,
+    consumer_type: str | None = None,
     **kwargs: Any,
 ) -> None:
     """
@@ -505,8 +507,13 @@ def _do_save_event(
 
     event_type = "none"
 
+    if consumer_type and consumer_type == ConsumerType.Transactions:
+        processing_store = processing.transaction_processing_store
+    else:
+        processing_store = processing.event_processing_store
+
     if cache_key and data is None:
-        data = processing.event_processing_store.get(cache_key)
+        data = processing_store.get(cache_key)
         if data is not None:
             event_type = data.get("type") or "none"
 
@@ -562,11 +569,11 @@ def _do_save_event(
             data = manager.get_data()
             if not isinstance(data, dict):
                 data = dict(data.items())
-            processing.event_processing_store.store(data)
+            processing_store.store(data)
         except HashDiscarded:
             # Delete the event payload from cache since it won't show up in post-processing.
             if cache_key:
-                processing.event_processing_store.delete_by_key(cache_key)
+                processing_store.delete_by_key(cache_key)
         except Exception:
             metrics.incr("events.save_event.exception", tags={"event_type": event_type})
             raise
@@ -604,7 +611,15 @@ def save_event(
     project_id: int | None = None,
     **kwargs: Any,
 ) -> None:
-    _do_save_event(cache_key, data, start_time, event_id, project_id, **kwargs)
+    _do_save_event(
+        cache_key,
+        data,
+        start_time,
+        event_id,
+        project_id,
+        consumer_type=ConsumerType.Events,
+        **kwargs,
+    )
 
 
 @instrumented_task(
@@ -621,7 +636,15 @@ def save_event_transaction(
     project_id: int | None = None,
     **kwargs: Any,
 ) -> None:
-    _do_save_event(cache_key, data, start_time, event_id, project_id, **kwargs)
+    _do_save_event(
+        cache_key,
+        data,
+        start_time,
+        event_id,
+        project_id,
+        consumer_type=ConsumerType.Transactions,
+        **kwargs,
+    )
 
 
 @instrumented_task(
@@ -658,23 +681,12 @@ def save_event_attachments(
     **kwargs: Any,
 ) -> None:
     _do_save_event(
-        cache_key, data, start_time, event_id, project_id, has_attachments=True, **kwargs
+        cache_key,
+        data,
+        start_time,
+        event_id,
+        project_id,
+        consumer_type=ConsumerType.Attachments,
+        has_attachments=True,
+        **kwargs,
     )
-
-
-# TODO(swatinem): remove this (and related queue) once the backing worker deployment is gone
-@instrumented_task(
-    name="sentry.tasks.store.save_event_highcpu",
-    queue="events.save_event_highcpu",
-    time_limit=65,
-    soft_time_limit=60,
-)
-def save_event_highcpu(
-    cache_key: str | None = None,
-    data: MutableMapping[str, Any] | None = None,
-    start_time: float | None = None,
-    event_id: str | None = None,
-    project_id: int | None = None,
-    **kwargs: Any,
-) -> None:
-    _do_save_event(cache_key, data, start_time, event_id, project_id, **kwargs)
