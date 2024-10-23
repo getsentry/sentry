c546bff142e89898a858d2ac8b1ed2ac2916eedb
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-10-23 15:54:34 -0400

    ref(issue-details): Clean up event package module (#79615)
    This module is ugly and long, and clipped box doesn't make sense for the
    streamlined UI since the sections are foldable. Did a quick column
    layout, doing tags +1 for the count. Also setting it to collapse by
    default.
    
    **Before:**
    <img width="1234" alt="image"
    src="https://github.com/user-attachments/assets/01ca5075-c820-4c74-94da-8d828a950d97">
    
    **After**
    <img width="1299" alt="image"
    src="https://github.com/user-attachments/assets/ab9b7c6f-db2c-41bd-a8ad-78622222ac69">

--- static/app/components/events/packageData.spec.tsx
+++ static/app/components/events/packageData.spec.tsx
@@ -1,5 +1,8 @@
 import {DataScrubbingRelayPiiConfigFixture} from 'sentry-fixture/dataScrubbingRelayPiiConfig';
 import {EventFixture} from 'sentry-fixture/event';
+import {LocationFixture} from 'sentry-fixture/locationFixture';
+import {OrganizationFixture} from 'sentry-fixture/organization';
+import {RouterFixture} from 'sentry-fixture/routerFixture';
 
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 import {textWithMarkupMatcher} from 'sentry-test/utils';
@@ -7,30 +10,78 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 import {EventPackageData} from 'sentry/components/events/packageData';
 
 describe('EventPackageData', function () {
-  it('display redacted data', async function () {
-    const event = EventFixture({
+  const event = EventFixture({
+    packages: {
+      certifi: '',
+      pip: '18.0',
+      python: '2.7.15',
+      'sentry-sdk': '0.3.1',
+      setuptools: '40.0.0',
+      urllib3: '1.23',
+      wheel: '0.31.1',
+      wsgiref: '0.1.2',
+    },
+    _meta: {
       packages: {
-        certifi: '',
-        pip: '18.0',
-        python: '2.7.15',
-        'sentry-sdk': '0.3.1',
-        setuptools: '40.0.0',
-        urllib3: '1.23',
-        wheel: '0.31.1',
-        wsgiref: '0.1.2',
+        certifi: {'': {rem: [['organization:1', 'x']]}},
       },
-      _meta: {
-        packages: {
-          certifi: {'': {rem: [['organization:1', 'x']]}},
-        },
-      },
-    });
+    },
+  });
+  const organization = OrganizationFixture({
+    relayPiiConfig: JSON.stringify(DataScrubbingRelayPiiConfigFixture()),
+  });
 
+  it('changes section title depending on the platform', function () {
     render(<EventPackageData event={event} />, {
-      organization: {
-        relayPiiConfig: JSON.stringify(DataScrubbingRelayPiiConfigFixture()),
-      },
+      organization,
+      router: RouterFixture({
+        location: LocationFixture({query: {streamline: '1'}}),
+      }),
     });
+    expect(screen.getByText('Packages')).toBeInTheDocument();
+    render(<EventPackageData event={{...event, platform: 'csharp'}} />, {
+      organization,
+      router: RouterFixture({
+        location: LocationFixture({query: {streamline: '1'}}),
+      }),
+    });
+    expect(screen.getByText('Assemblies')).toBeInTheDocument();
+    render(<EventPackageData event={{...event, platform: 'java'}} />, {
+      organization,
+      router: RouterFixture({
+        location: LocationFixture({query: {streamline: '1'}}),
+      }),
+    });
+    expect(screen.getByText('Dependencies')).toBeInTheDocument();
+  });
+
+  it('displays all the data in column format', async function () {
+    render(<EventPackageData event={event} />, {
+      organization,
+      router: RouterFixture({
+        location: LocationFixture({query: {streamline: '1'}}),
+      }),
+    });
+    // Should be collapsed by default
+    expect(screen.queryByText(/python/)).not.toBeInTheDocument();
+    // Displays when open
+    await userEvent.click(screen.getByText('Packages'));
+    expect(screen.getByText(/python/)).toBeInTheDocument();
+    expect(screen.getByText(event?.packages?.python as string)).toBeInTheDocument();
+    // Respects _meta annotations
+    expect(screen.getByText(/redacted/)).toBeInTheDocument();
+    await userEvent.hover(screen.getByText(/redacted/));
+    expect(
+      await screen.findByText(
+        textWithMarkupMatcher(
+          "Removed because of the data scrubbing rule [Mask] [Credit card numbers] from [$message] in your organization's settings"
+        )
+      )
+    ).toBeInTheDocument();
+  });
+
+  it('display redacted data', async function () {
+    render(<EventPackageData event={event} />, {organization});
 
     expect(screen.getByText(/redacted/)).toBeInTheDocument();
 
