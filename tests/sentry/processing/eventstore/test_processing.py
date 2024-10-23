26e8ecc6620ada7fe1759e131fb5ac0487f8bb3e
Neo Huang <126607112+kneeyo1@users.noreply.github.com>
2024-10-23 14:44:12 -0700

    Update tests/sentry/processing/eventstore/test_processing.py
    Co-authored-by: Mark Story <mark@mark-story.com>

--- tests/sentry/processing/eventstore/test_processing.py
+++ tests/sentry/processing/eventstore/test_processing.py
@@ -7,7 +7,7 @@ from sentry.utils.services import LazyServiceWrapper
 
 @django_db_all
 @override_settings(
-    SENTRY_TRANSACTION_PROCESSING_STORE=None, SENTRY_TRANSACTION_PROCESSING_STORE_OPTIONS=None
+    SENTRY_TRANSACTION_PROCESSING_STORE=None, SENTRY_TRANSACTION_PROCESSING_STORE_OPTIONS={}
 )
 def test_transaction_datastore_defaults_to_event_store():
     assert isinstance(transaction_processing_store, LazyServiceWrapper)
