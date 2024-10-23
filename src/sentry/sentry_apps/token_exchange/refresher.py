a7b67f189ae28fcbfb2a7b8d09da879cf3159641
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2024-10-23 10:19:19 -0700

    ref(mediators): Revert the revert of turning refresher into dataclass (#79569)
    prev ( #79112)

--- src/sentry/sentry_apps/token_exchange/refresher.py
+++ src/sentry/sentry_apps/token_exchange/refresher.py
@@ -1,12 +1,11 @@
 import logging
+from dataclasses import dataclass
 
-from django.db import router
+from django.db import router, transaction
 from django.utils.functional import cached_property
 
 from sentry import analytics
 from sentry.coreapi import APIUnauthorized
-from sentry.mediators.mediator import Mediator
-from sentry.mediators.param import Param
 from sentry.mediators.token_exchange.util import token_expiration
 from sentry.mediators.token_exchange.validator import Validator
 from sentry.models.apiapplication import ApiApplication
@@ -19,52 +18,49 @@ from sentry.users.models.user import User
 logger = logging.getLogger("sentry.token-exchange")
 
 
-class Refresher(Mediator):
+@dataclass
+class Refresher:
     """
     Exchanges a Refresh Token for a new Access Token
     """
 
-    install = Param(RpcSentryAppInstallation)
-    refresh_token = Param(str)
-    client_id = Param(str)
-    user = Param(User)
-    using = router.db_for_write(User)
+    install: RpcSentryAppInstallation
+    refresh_token: str
+    client_id: str
+    user: User
 
-    def call(self):
-        try:
-            self._validate()
-            self._delete_token()
-            return self._create_new_token()
-        except APIUnauthorized:
-            logger.info(
-                "refresher.context",
-                extra={
-                    "application_id": self.application.id,
-                    "refresh_token": self.refresh_token[-4:],
-                },
-            )
-            raise
+    def run(self) -> ApiToken:
+        with transaction.atomic(router.db_for_write(ApiToken)):
+            try:
+                self._validate()
+                self.token.delete()
 
-    def record_analytics(self):
+                self._record_analytics()
+                return self._create_new_token()
+            except APIUnauthorized:
+                logger.info(
+                    "refresher.context",
+                    extra={
+                        "application_id": self.application.id,
+                        "refresh_token": self.refresh_token[-4:],
+                    },
+                )
+                raise
+
+    def _record_analytics(self) -> None:
         analytics.record(
             "sentry_app.token_exchanged",
             sentry_app_installation_id=self.install.id,
             exchange_type="refresh",
         )
 
-    def _validate(self):
+    def _validate(self) -> None:
         Validator.run(install=self.install, client_id=self.client_id, user=self.user)
 
-        self._validate_token_belongs_to_app()
-
-    def _validate_token_belongs_to_app(self):
         if self.token.application != self.application:
             raise APIUnauthorized("Token does not belong to the application")
 
-    def _delete_token(self):
-        self.token.delete()
-
-    def _create_new_token(self):
+    def _create_new_token(self) -> ApiToken:
         token = ApiToken.objects.create(
             user=self.user,
             application=self.application,
@@ -78,21 +74,21 @@ class Refresher(Mediator):
         return token
 
     @cached_property
-    def token(self):
+    def token(self) -> ApiToken:
         try:
             return ApiToken.objects.get(refresh_token=self.refresh_token)
         except ApiToken.DoesNotExist:
             raise APIUnauthorized("Token does not exist")
 
     @cached_property
-    def application(self):
+    def application(self) -> ApiApplication:
         try:
             return ApiApplication.objects.get(client_id=self.client_id)
         except ApiApplication.DoesNotExist:
             raise APIUnauthorized("Application does not exist")
 
     @property
-    def sentry_app(self):
+    def sentry_app(self) -> SentryApp:
         try:
             return self.application.sentry_app
         except SentryApp.DoesNotExist:
