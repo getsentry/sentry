d6dbccac72d46294bb591270ecee2c57bd7e9e93
Nate Moore <nate.moore@sentry.io>
2024-10-24 15:20:34 -0500

    feat(nav): add help menu

--- static/app/components/nav/overlay/help.tsx
+++ static/app/components/nav/overlay/help.tsx
@@ -1,13 +1,35 @@
-import {Fragment} from 'react';
+import {Fragment, type MouseEventHandler, useCallback} from 'react';
 
-import Link from 'sentry/components/links/link';
+import {openHelpSearchModal} from 'sentry/actionCreators/modal';
+import {OverlayMenuLink} from 'sentry/components/nav/overlay';
 import {t} from 'sentry/locale';
+import useOrganization from 'sentry/utils/useOrganization';
 
 export default function Help() {
+  const organization = useOrganization();
+
+  const handleClick: MouseEventHandler<HTMLAnchorElement> = useCallback(
+    event => {
+      event.preventDefault();
+      openHelpSearchModal({organization});
+    },
+    [organization]
+  );
+
   return (
     <Fragment>
-      <Link to="https://sentry.zendesk.com/hc/en-us">{t('Visit Help Center')}</Link>
-      <Link to="https://discord.com/invite/sentry">{t('Join our Discord')}</Link>
+      <OverlayMenuLink to={{}} onClick={handleClick}>
+        {t('Search Support, Docs and More')}
+      </OverlayMenuLink>
+      <OverlayMenuLink to="https://sentry.zendesk.com/hc/en-us">
+        {t('Visit Help Center')}
+      </OverlayMenuLink>
+      <OverlayMenuLink to="https://discord.com/invite/sentry">
+        {t('Join our Discord')}
+      </OverlayMenuLink>
+      <OverlayMenuLink to="mailto:support@sentry.io">
+        {t('Contact Support')}
+      </OverlayMenuLink>
     </Fragment>
   );
 }
