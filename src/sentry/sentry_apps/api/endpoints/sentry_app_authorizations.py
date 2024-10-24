998c937a29abcf550af5db1978e6c0078b500b24
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2024-10-24 10:11:55 -0700

    ref(mediators): Round 2, turn validator into dataclass (#79571)

--- src/sentry/sentry_apps/api/endpoints/sentry_app_authorizations.py
+++ src/sentry/sentry_apps/api/endpoints/sentry_app_authorizations.py
@@ -10,10 +10,10 @@ from sentry.api.base import control_silo_endpoint
 from sentry.api.serializers.models.apitoken import ApiTokenSerializer
 from sentry.auth.services.auth.impl import promote_request_api_user
 from sentry.coreapi import APIUnauthorized
-from sentry.mediators.token_exchange.util import GrantTypes
 from sentry.sentry_apps.api.bases.sentryapps import SentryAppAuthorizationsBaseEndpoint
 from sentry.sentry_apps.token_exchange.grant_exchanger import GrantExchanger
 from sentry.sentry_apps.token_exchange.refresher import Refresher
+from sentry.sentry_apps.token_exchange.util import GrantTypes
 
 logger = logging.getLogger(__name__)
 
--- src/sentry/sentry_apps/token_exchange/validator.py
+++ src/sentry/sentry_apps/token_exchange/validator.py
@@ -1,54 +1,53 @@
-from django.db import router
+from dataclasses import dataclass
+
 from django.utils.functional import cached_property
 
 from sentry.coreapi import APIUnauthorized
-from sentry.mediators.mediator import Mediator
-from sentry.mediators.param import Param
 from sentry.models.apiapplication import ApiApplication
 from sentry.sentry_apps.models.sentry_app import SentryApp
 from sentry.sentry_apps.services.app import RpcSentryAppInstallation
 from sentry.users.models.user import User
 
 
-class Validator(Mediator):
+@dataclass
+class Validator:
     """
     Validates general authorization params for all types of token exchanges.
     """
 
-    install = Param(RpcSentryAppInstallation)
-    client_id = Param(str)
-    user = Param(User)
-    using = router.db_for_write(User)
+    install: RpcSentryAppInstallation
+    client_id: str
+    user: User
 
-    def call(self):
+    def run(self) -> bool:
         self._validate_is_sentry_app_making_request()
         self._validate_app_is_owned_by_user()
         self._validate_installation()
         return True
 
-    def _validate_is_sentry_app_making_request(self):
+    def _validate_is_sentry_app_making_request(self) -> None:
         if not self.user.is_sentry_app:
             raise APIUnauthorized("User is not a Sentry App")
 
-    def _validate_app_is_owned_by_user(self):
+    def _validate_app_is_owned_by_user(self) -> None:
         if self.sentry_app.proxy_user != self.user:
             raise APIUnauthorized("Sentry App does not belong to given user")
 
-    def _validate_installation(self):
+    def _validate_installation(self) -> None:
         if self.install.sentry_app.id != self.sentry_app.id:
             raise APIUnauthorized(
                 f"Sentry App Installation is not for Sentry App: {self.sentry_app.slug}"
             )
 
     @cached_property
-    def sentry_app(self):
+    def sentry_app(self) -> SentryApp:
         try:
             return self.application.sentry_app
         except SentryApp.DoesNotExist:
             raise APIUnauthorized("Sentry App does not exist")
 
     @cached_property
-    def application(self):
+    def application(self) -> ApiApplication:
         try:
             return ApiApplication.objects.get(client_id=self.client_id)
         except ApiApplication.DoesNotExist:
