1dd7d7ca7485092775822202fc5dfa50a9ac3694
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2024-10-24 09:29:31 -0700

    chore(alerts): remove notification-all-recipients flag (#79532)
    This feature is disabled in production. Remove all references to the
    flag as part of cleanup for ACI.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -209,8 +209,6 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:more-slow-alerts", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     manager.add("organizations:navigation-sidebar-v2", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     manager.add("organizations:new-page-filter", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, default=True, api_expose=True)
-    # Notify all project members when fallthrough is disabled, instead of just the auto-assignee
-    manager.add("organizations:notification-all-recipients", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Drop obsoleted status changes in occurence consumer
     manager.add("organizations:occurence-consumer-prune-status-changes", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable User Feedback v1
