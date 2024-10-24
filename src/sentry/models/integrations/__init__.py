8b6522cf14567f20cdbc1bf8f102d673a7db3504
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2024-10-24 13:53:38 -0700

    chore(sentry apps): remove shims (#79321)

--- src/sentry/models/integrations/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ("SentryApp",)
-
-
-# REQUIRED for migrations to run.
-from sentry.integrations.types import ExternalProviders  # NOQA
-from sentry.sentry_apps.models.sentry_app import SentryApp
