1586f977c86f21c3886619f5eb1b06b60f9c70f9
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2024-10-24 12:48:29 -0700

    fix(issues): Don't show break if people section doesn't exist (#79701)
    this pr updates the sidebar so that it doesn't show a break if the
    people section isn't there

--- static/app/views/issueDetails/streamline/peopleSection.spec.tsx
+++ static/app/views/issueDetails/streamline/peopleSection.spec.tsx
@@ -1,4 +1,3 @@
-import {GroupFixture} from 'sentry-fixture/group';
 import {TeamFixture} from 'sentry-fixture/team';
 import {UserFixture} from 'sentry-fixture/user';
 
@@ -8,45 +7,38 @@ import type {TeamParticipant, UserParticipant} from 'sentry/types/group';
 import PeopleSection from 'sentry/views/issueDetails/streamline/peopleSection';
 
 describe('PeopleSection', () => {
-  const group = GroupFixture();
+  const teams: TeamParticipant[] = [{...TeamFixture(), type: 'team'}];
+  const users: UserParticipant[] = [
+    {
+      ...UserFixture({
+        id: '2',
+        name: 'John Smith',
+        email: 'johnsmith@example.com',
+      }),
+      type: 'user',
+    },
+    {
+      ...UserFixture({
+        id: '3',
+        name: 'Sohn Jmith',
+        email: 'sohnjmith@example.com',
+      }),
+      type: 'user',
+    },
+  ];
 
   it('displays participants and viewers', async () => {
-    const teams: TeamParticipant[] = [{...TeamFixture(), type: 'team'}];
-    const users: UserParticipant[] = [
-      {
-        ...UserFixture({
-          id: '2',
-          name: 'John Smith',
-          email: 'johnsmith@example.com',
-        }),
-        type: 'user',
-      },
-      {
-        ...UserFixture({
-          id: '3',
-          name: 'Sohn Jmith',
-          email: 'sohnjmith@example.com',
-        }),
-        type: 'user',
-      },
-    ];
-
-    const participantGroup = {
-      ...group,
-      participants: [...teams, ...users],
-      seenBy: users,
-    };
-
-    render(<PeopleSection group={participantGroup} />);
+    render(
+      <PeopleSection teamParticipants={teams} userParticipants={users} viewers={users} />
+    );
 
     expect(await screen.findByText('participating')).toBeInTheDocument();
     expect(await screen.findByText('viewed')).toBeInTheDocument();
   });
 
-  it('does not display anything if there are no participants or viewers', () => {
-    render(<PeopleSection group={group} />);
+  it('does not display particiapnts if there are none', () => {
+    render(<PeopleSection teamParticipants={[]} userParticipants={[]} viewers={users} />);
 
     expect(screen.queryByText('participating')).not.toBeInTheDocument();
-    expect(screen.queryByText('viewed')).not.toBeInTheDocument();
   });
 });
