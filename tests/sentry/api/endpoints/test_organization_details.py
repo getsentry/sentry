ba3f351080c7c8771e56ac126103f50825602121
Fabian Schindler <fabian.schindler@sentry.io>
2024-10-24 18:11:25 +0200

    fix(dynamic-sampling): add missing serialization for sampling mode

--- tests/sentry/api/endpoints/test_organization_details.py
+++ tests/sentry/api/endpoints/test_organization_details.py
@@ -332,6 +332,15 @@ class OrganizationDetailsTest(OrganizationDetailsTestBase):
             response = self.get_success_response(self.organization.slug)
             assert "targetSampleRate" not in response.data
 
+    def test_dynamic_sampling_custom_sampling_mode(self):
+        with self.feature({"organizations:dynamic-sampling-custom": True}):
+            response = self.get_success_response(self.organization.slug)
+            assert response.data["samplingMode"] == "organization"
+
+        with self.feature({"organizations:dynamic-sampling-custom": False}):
+            response = self.get_success_response(self.organization.slug)
+            assert "samplingMode" not in response.data
+
     def test_sensitive_fields_too_long(self):
         value = 1000 * ["0123456789"] + ["1"]
         resp = self.get_response(self.organization.slug, method="put", sensitiveFields=value)
