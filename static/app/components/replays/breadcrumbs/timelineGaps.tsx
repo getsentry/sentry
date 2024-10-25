1ceafb5b16404fafc82cad7df248522ac960dda9
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-10-25 16:21:19 -0400

    ref(replay): Only show timeline gaps larger than 1.1s (#79733)
    Only gaps larger than 1.1s will be shown. Also adds analytics to see how
    many timeline gaps there are within a replay
    
    Before:
    <img width="1501" alt="image"
    src="https://github.com/user-attachments/assets/2995cd5b-9c78-4604-8a12-a0a1a546fe72">
    
    After:
    <img width="1501" alt="image"
    src="https://github.com/user-attachments/assets/33d36944-0911-402f-af03-0b3a51923c5b">

--- static/app/components/replays/breadcrumbs/timelineGaps.tsx
+++ static/app/components/replays/breadcrumbs/timelineGaps.tsx
@@ -1,10 +1,12 @@
-import {Fragment} from 'react';
+import {Fragment, useEffect, useMemo} from 'react';
 import styled from '@emotion/styled';
 
 import {Tooltip} from 'sentry/components/tooltip';
 import {t} from 'sentry/locale';
+import {trackAnalytics} from 'sentry/utils/analytics';
 import toPercent from 'sentry/utils/number/toPercent';
 import type {VideoEvent} from 'sentry/utils/replays/types';
+import useOrganization from 'sentry/utils/useOrganization';
 
 interface Props {
   durationMs: number;
@@ -13,33 +15,47 @@ interface Props {
 }
 
 export default function TimelineGaps({durationMs, startTimestampMs, videoEvents}: Props) {
-  const ranges: Array<{left: string; width: string}> = [];
+  const organization = useOrganization();
 
-  let start = startTimestampMs;
+  const gaps = useMemo(() => {
+    const ranges: Array<{left: string; width: string}> = [];
+    let previousVideoEnd = startTimestampMs;
 
-  // create gap in timeline when there is a gap between video events
-  for (const video of videoEvents) {
-    if (start < video.timestamp) {
+    // create gap in timeline when there is a gap between video events larger than 1.1s
+    for (const video of videoEvents) {
+      if (video.timestamp - previousVideoEnd > 1100) {
+        ranges.push({
+          left: toPercent((previousVideoEnd - startTimestampMs) / durationMs),
+          width: toPercent((video.timestamp - previousVideoEnd) / durationMs),
+        });
+      }
+      previousVideoEnd = video.timestamp + video.duration;
+    }
+
+    // add gap at the end if the last video segment ends before the replay ends
+    if (videoEvents.length && previousVideoEnd < startTimestampMs + durationMs) {
       ranges.push({
-        left: toPercent((start - startTimestampMs) / durationMs),
-        width: toPercent((video.timestamp - start) / durationMs),
+        left: toPercent((previousVideoEnd - startTimestampMs) / durationMs),
+        width: toPercent(durationMs / durationMs),
       });
     }
-    start = video.timestamp + video.duration;
-  }
 
-  // add gap at the end if the last video segment ends before the replay ends
-  if (videoEvents.length && start < startTimestampMs + durationMs) {
-    ranges.push({
-      left: toPercent((start - startTimestampMs) / durationMs),
-      width: toPercent(durationMs / durationMs),
+    return ranges;
+  }, [durationMs, startTimestampMs, videoEvents]);
+
+  useEffect(() => {
+    trackAnalytics('replay.gaps_detected', {
+      gaps: gaps.length,
+      max_gap: Math.max(...gaps.map(obj => parseFloat(obj.width))),
+      replay_duration: durationMs,
+      organization: organization,
     });
-  }
+  }, [durationMs, organization, gaps]);
 
   // TODO: Fix tooltip position to follow mouse (it currently goes off the timeline when zoomed too much)
   return (
     <Fragment>
-      {ranges.map(rangeCss => {
+      {gaps.map(rangeCss => {
         return (
           <Range key={`${rangeCss.left}-${rangeCss.width}`} style={rangeCss}>
             <Tooltip
