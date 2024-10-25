ea12c168703a4e14df67853f4ea3f14aca124eae
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2024-10-25 16:21:19 -0400

    ref(replay): Only show timeline gaps larger than 1.1s (#79733)
    Only gaps larger than 1.1s will be shown. Also adds analytics to see how
    many timeline gaps there are within a replay
    
    Before:
    <img width="1501" alt="image"
    src="https://github.com/user-attachments/assets/2995cd5b-9c78-4604-8a12-a0a1a546fe72">
    
    After:
    <img width="1501" alt="image"
    src="https://github.com/user-attachments/assets/33d36944-0911-402f-af03-0b3a51923c5b">

--- static/app/utils/analytics/replayAnalyticsEvents.tsx
+++ static/app/utils/analytics/replayAnalyticsEvents.tsx
@@ -60,6 +60,11 @@ export type ReplayEventParameters = {
     frame: string;
   };
 
+  'replay.gaps_detected': {
+    gaps: number;
+    max_gap: number;
+    replay_duration: number;
+  };
   'replay.hydration-error.issue-details-opened': {};
   'replay.hydration-modal.slider-interaction': {};
   'replay.hydration-modal.tab-change': {
@@ -148,6 +153,7 @@ export const replayEventMap: Record<ReplayEventKey, string | null> = {
   'replay.list-sorted': 'Sorted Replay List',
   'replay.list-time-spent': 'Time Spent Viewing Replay List',
   'replay.list-view-setup-sidebar': 'Views Set Up Replays Sidebar',
+  'replay.gaps_detected': 'Number of Gaps in Replay Timeline',
   'replay.play-pause': 'Played/Paused Replay',
   'replay.rage-click-sdk-banner.dismissed': 'Replay Rage Click SDK Banner Dismissed',
   'replay.rage-click-sdk-banner.rendered': 'Replay Rage Click SDK Banner Rendered',
