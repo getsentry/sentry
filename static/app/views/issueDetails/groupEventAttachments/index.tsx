293529eab39f4747036e3e6285bd1447b0e15bd3
Scott Cooper <scttcper@gmail.com>
2024-10-25 15:36:39 -0700

    feat(issues): Display attachments as view again (#79796)

--- static/app/views/issueDetails/groupEventAttachments/index.tsx
+++ static/app/views/issueDetails/groupEventAttachments/index.tsx
@@ -1,10 +1,15 @@
+import {css} from '@emotion/react';
+import styled from '@emotion/styled';
+
 import Feature from 'sentry/components/acl/feature';
 import FeatureDisabled from 'sentry/components/acl/featureDisabled';
 import * as Layout from 'sentry/components/layouts/thirds';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import type {Group} from 'sentry/types/group';
 import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import useOrganization from 'sentry/utils/useOrganization';
+import {useHasStreamlinedUI} from 'sentry/views/issueDetails/utils';
 
 import GroupEventAttachments from './groupEventAttachments';
 
@@ -14,6 +19,8 @@ type Props = RouteComponentProps<{groupId: string}, {}> & {
 
 function GroupEventAttachmentsContainer({group}: Props) {
   const organization = useOrganization();
+  const hasStreamlinedUI = useHasStreamlinedUI();
+
   return (
     <Feature
       features="event-attachments"
@@ -22,13 +29,27 @@ function GroupEventAttachmentsContainer({group}: Props) {
         <FeatureDisabled {...props} featureName={t('Event Attachments')} />
       )}
     >
-      <Layout.Body>
+      <StyledLayoutBody hasStreamlinedUI={hasStreamlinedUI}>
         <Layout.Main fullWidth>
           <GroupEventAttachments project={group.project} groupId={group.id} />
         </Layout.Main>
-      </Layout.Body>
+      </StyledLayoutBody>
     </Feature>
   );
 }
+
+const StyledLayoutBody = styled(Layout.Body)<{hasStreamlinedUI?: boolean}>`
+  ${p =>
+    p.hasStreamlinedUI &&
+    css`
+      border: 1px solid ${p.theme.border};
+      border-radius: ${p.theme.borderRadius};
+      padding: ${space(2)} 0;
+
+      @media (min-width: ${p.theme.breakpoints.medium}) {
+        padding: ${space(2)} ${space(2)};
+      }
+    `}
+`;
 
 export default GroupEventAttachmentsContainer;
