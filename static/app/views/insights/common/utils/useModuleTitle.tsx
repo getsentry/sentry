d6e7a54da956292e00c6cc31556e38c432b12b29
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-10-28 15:39:52 -0400

    use memo

--- static/app/views/insights/common/utils/useModuleTitle.tsx
+++ static/app/views/insights/common/utils/useModuleTitle.tsx
@@ -1,3 +1,5 @@
+import {useMemo} from 'react';
+
 import {DOMAIN_VIEW_MODULE_TITLES} from 'sentry/views/insights/common/utils/moduleTitles';
 import {useDomainViewFilters} from 'sentry/views/insights/pages/useFilters';
 import {MODULE_TITLES} from 'sentry/views/insights/settings';
@@ -5,10 +7,14 @@ import type {ModuleName} from 'sentry/views/insights/types';
 
 export const useModuleTitles = (): Record<ModuleName, string> => {
   const {isInDomainView, view} = useDomainViewFilters();
-  let moduleTitles = {...MODULE_TITLES};
-  if (isInDomainView && view) {
-    moduleTitles = {...MODULE_TITLES, ...DOMAIN_VIEW_MODULE_TITLES[view]};
-  }
+  const moduleTitles = useMemo(() => {
+    let titles = {...MODULE_TITLES};
+    if (isInDomainView && view) {
+      titles = {...MODULE_TITLES, ...DOMAIN_VIEW_MODULE_TITLES[view]};
+    }
+    return titles;
+  }, [isInDomainView, view]);
+
   return moduleTitles;
 };
 
