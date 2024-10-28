52771b52d4aaf1b843722ef79aab011a467cb12a
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-10-28 08:45:59 -0700

    ref(unmerge): Remove delete function (#79129)
    Remove the DELETE function now that it is replaced by PUT

--- src/sentry/apidocs/api_publish_status_allowlist_dont_modify.py
+++ src/sentry/apidocs/api_publish_status_allowlist_dont_modify.py
@@ -18,7 +18,7 @@ API_PUBLISH_STATUS_ALLOWLIST_DONT_MODIFY = {
     "/api/0/{var}/{issue_id}/events/{event_id}/": {"GET"},
     "/api/0/{var}/{issue_id}/{var}/": {"GET", "POST"},
     "/api/0/{var}/{issue_id}/{var}/{note_id}/": {"DELETE", "PUT"},
-    "/api/0/{var}/{issue_id}/hashes/": {"GET", "DELETE", "PUT"},
+    "/api/0/{var}/{issue_id}/hashes/": {"GET", "PUT"},
     "/api/0/{var}/{issue_id}/reprocessing/": {"POST"},
     "/api/0/{var}/{issue_id}/stats/": {"GET"},
     "/api/0/{var}/{issue_id}/tags/": {"GET"},
@@ -80,7 +80,6 @@ API_PUBLISH_STATUS_ALLOWLIST_DONT_MODIFY = {
     },
     "/api/0/organizations/{organization_id_or_slug}/{var}/{issue_id}/hashes/": {
         "GET",
-        "DELETE",
         "PUT",
     },
     "/api/0/organizations/{organization_id_or_slug}/{var}/{issue_id}/reprocessing/": {"POST"},
