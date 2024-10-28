20c324c06ef4046230ca905b621bf483b4d20b09
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-10-28 13:40:31 -0600

    feat(flags): add suspect flag analytics (#79470)
    create a hook `useSuspectFlags` which tracks the number of suspect flags
    on a given event, based on the naive heuristic specified
    [here](https://www.notion.so/sentry/Suspect-Flags-Heuristic-1198b10e4b5d803e9125f3382315b002).
    also broken down below:
    
    ## heuristic:
    - For a given error event, collect all of the flags stored on it (these
    are the flags specified in `event.contexts.flags`.)
    - Look up those flags in the log which changed prior to the first error
    event. (this is done in the `useSuspectFlags` endpoint call by taking
    the current issue's `firstSeen` time and going as far back as 1 year).
    - Display the flag audit-log events in order of proximity to the start
    date. The closer to the start date the more suspect the change. Cap the
    maximum number of suspect flags to 3. (this step is not done yet; this
    PR just collects data for analysis)
    
    right now we're doing nothing with the suspect flag data -- only logging
    it so i can make an amplitude chart to assess how often we get suspect
    flags.
    
    suspect flag funnel example:
    <img width="573" alt="SCR-20241028-krcq"
    src="https://github.com/user-attachments/assets/564a66ff-5764-4e25-8bcb-0751a528f65b">

--- static/app/views/issueDetails/streamline/flagSeries.tsx
+++ static/app/views/issueDetails/streamline/flagSeries.tsx
@@ -4,35 +4,21 @@ import moment from 'moment-timezone';
 import MarkLine from 'sentry/components/charts/components/markLine';
 import {t} from 'sentry/locale';
 import type {Event} from 'sentry/types/event';
+import type {Group} from 'sentry/types/group';
 import type {Organization} from 'sentry/types/organization';
 import {getFormattedDate} from 'sentry/utils/dates';
 import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
-
-type RawFlag = {
-  action: string;
-  created_at: string;
-  created_by: string;
-  created_by_type: string;
-  flag: string;
-  id: number;
-  tags: Record<string, any>;
-};
-
-export type RawFlagData = {data: RawFlag[]};
-
-type FlagSeriesDatapoint = {
-  // flag action
-  label: {formatter: () => string};
-  // flag name
-  name: string;
-  // unix timestamp
-  xAxis: number;
-};
+import {
+  hydrateToFlagSeries,
+  type RawFlagData,
+} from 'sentry/views/issueDetails/streamline/featureFlagUtils';
+import useSuspectFlags from 'sentry/views/issueDetails/streamline/useSuspectFlags';
 
 interface FlagSeriesProps {
   event: Event | undefined;
+  group: Group;
   query: Record<string, any>;
 }
 
@@ -43,34 +29,16 @@ function useOrganizationFlagLog({
   organization: Organization;
   query: Record<string, any>;
 }) {
-  const {data, isError, isPending} = useApiQuery<RawFlagData>(
+  return useApiQuery<RawFlagData>(
     [`/organizations/${organization.slug}/flags/logs/`, {query}],
     {
       staleTime: 0,
       enabled: organization.features?.includes('feature-flag-ui'),
     }
   );
-  return {data, isError, isPending};
 }
 
-function hydrateFlagData({
-  rawFlagData,
-}: {
-  rawFlagData: RawFlagData;
-}): FlagSeriesDatapoint[] {
-  // transform raw flag data into series data
-  // each data point needs to be type FlagSeriesDatapoint
-  const flagData = rawFlagData.data.map(f => {
-    return {
-      xAxis: Date.parse(f.created_at),
-      label: {formatter: () => f.action},
-      name: `${f.flag}`,
-    };
-  });
-  return flagData;
-}
-
-export default function useFlagSeries({query = {}, event}: FlagSeriesProps) {
+export default function useFlagSeries({query = {}, event, group}: FlagSeriesProps) {
   const theme = useTheme();
   const organization = useOrganization();
   const {
@@ -80,6 +48,14 @@ export default function useFlagSeries({query = {}, event}: FlagSeriesProps) {
   } = useOrganizationFlagLog({organization, query});
   const {selection} = usePageFilters();
 
+  useSuspectFlags({
+    organization,
+    query,
+    firstSeen: group.firstSeen,
+    rawFlagData,
+    event,
+  });
+
   if (!rawFlagData || isError || isPending) {
     return {
       seriesName: t('Feature Flags'),
@@ -88,7 +64,7 @@ export default function useFlagSeries({query = {}, event}: FlagSeriesProps) {
     };
   }
 
-  const hydratedFlagData: FlagSeriesDatapoint[] = hydrateFlagData({rawFlagData});
+  const hydratedFlagData = hydrateToFlagSeries(rawFlagData);
 
   // create a markline series using hydrated flag data
   const markLine = MarkLine({
