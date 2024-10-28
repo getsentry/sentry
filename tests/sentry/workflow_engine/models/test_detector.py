7e35954b7fad4cc16548d17fa84e9465acd1781e
Dan Fuller <dfuller@sentry.io>
2024-10-28 14:45:31 -0700

    feat(workflow-engine): Add function to fetch state data for group keys (#79657)
    This implements the ability to get state data for group keys that is
    associated with the detector. This will be used when we process results
    and need to check the existing state.

--- tests/sentry/workflow_engine/models/test_detector.py
+++ tests/sentry/workflow_engine/models/test_detector.py
@@ -1,5 +1,6 @@
 import unittest
 
+from sentry.testutils.abstract import Abstract
 from sentry.testutils.cases import TestCase
 from sentry.workflow_engine.models import DataPacket, DetectorEvaluationResult
 from sentry.workflow_engine.models.detector import (
@@ -13,8 +14,16 @@ from sentry.workflow_engine.types import DetectorPriorityLevel
 
 
 class MockDetectorStateHandler(StatefulDetectorHandler[dict]):
+    counter_names = ["test1", "test2"]
+
     def evaluate(self, data_packet: DataPacket[dict]) -> list[DetectorEvaluationResult]:
         return []
+
+    def get_dedupe_value(self, data_packet: DataPacket[dict]) -> int:
+        return 0
+
+    def get_group_key_values(self, data_packet: DataPacket[dict]) -> dict[str, int]:
+        return {}
 
 
 class TestKeyBuilders(unittest.TestCase):
@@ -55,10 +64,57 @@ class TestKeyBuilders(unittest.TestCase):
         )
 
 
-class TestCommitStateUpdateData(TestCase):
-    def setUp(self):
-        super().setUp()
+class StatefulDetectorHandlerTestMixin(TestCase):
+    __test__ = Abstract(__module__, __qualname__)
 
+    def build_handler(self, detector: Detector | None = None) -> MockDetectorStateHandler:
+        if detector is None:
+            detector = self.create_detector()
+        return MockDetectorStateHandler(detector)
+
+
+class TestGetStateData(StatefulDetectorHandlerTestMixin):
+    def test_new(self):
+        handler = self.build_handler()
+        key = "test_key"
+        assert handler.get_state_data([key]) == {
+            key: DetectorStateData(
+                key, False, DetectorPriorityLevel.OK, 0, {"test1": None, "test2": None}
+            )
+        }
+
+    def test_existing(self):
+        handler = self.build_handler()
+        key = "test_key"
+        state_data = DetectorStateData(
+            key, True, DetectorPriorityLevel.OK, 10, {"test1": 5, "test2": 200}
+        )
+        handler.commit_state_update_data([state_data])
+        assert handler.get_state_data([key]) == {key: state_data}
+
+    def test_multi(self):
+        handler = self.build_handler()
+        key_1 = "test_key_1"
+        state_data_1 = DetectorStateData(
+            key_1, True, DetectorPriorityLevel.OK, 100, {"test1": 50, "test2": 300}
+        )
+        key_2 = "test_key_2"
+        state_data_2 = DetectorStateData(
+            key_2, True, DetectorPriorityLevel.OK, 10, {"test1": 55, "test2": 12}
+        )
+        key_uncommitted = "test_key_uncommitted"
+        state_data_uncommitted = DetectorStateData(
+            key_uncommitted, False, DetectorPriorityLevel.OK, 0, {"test1": None, "test2": None}
+        )
+        handler.commit_state_update_data([state_data_1, state_data_2])
+        assert handler.get_state_data([key_1, key_2, key_uncommitted]) == {
+            key_1: state_data_1,
+            key_2: state_data_2,
+            key_uncommitted: state_data_uncommitted,
+        }
+
+
+class TestCommitStateUpdateData(StatefulDetectorHandlerTestMixin):
     def build_handler(self, detector: Detector | None = None) -> MockDetectorStateHandler:
         if detector is None:
             detector = self.create_detector()
