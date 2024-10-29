249a5dbe7323148e16dc5408272295060b2cbedd
Jonas <jonas.badalic@sentry.io>
2024-10-29 11:49:39 -0400

    ref(trace) make autogrouping opt in and opt out (#78840)
    This changes allows for the trace tree autogrouping and missing
    instrumentations to be configurable.
    
    Todo: 
    - ~ensure links to missing instrumentation work even if users dont have
    missing instrumentation enabled~
    - ~ensure links to autogroups and their children work if users dont have
    autogrouping enabled~
    - ~add select toggle~

--- static/app/views/performance/newTraceDetails/traceState/tracePreferences.tsx
+++ static/app/views/performance/newTraceDetails/traceState/tracePreferences.tsx
@@ -14,6 +14,7 @@ type TracePreferencesAction =
     }
   | {payload: number; type: 'set list width'}
   | {payload: boolean; type: 'minimize drawer'}
+  | {payload: boolean; type: 'set missing instrumentation'}
   | {payload: boolean; type: 'set autogrouping'};
 
 type TraceDrawerPreferences = {
@@ -34,6 +35,7 @@ export type TracePreferencesState = {
   list: {
     width: number;
   };
+  missing_instrumentation: boolean;
 };
 
 export const TRACE_DRAWER_DEFAULT_SIZES: TraceDrawerPreferences['sizes'] = {
@@ -56,6 +58,7 @@ export const DEFAULT_TRACE_VIEW_PREFERENCES: TracePreferencesState = {
     parent: true,
     sibling: true,
   },
+  missing_instrumentation: true,
   layout: 'drawer right',
   list: {
     width: 0.5,
@@ -118,6 +121,15 @@ function isValidAutogrouping(
   return true;
 }
 
+function isValidMissingInstrumentation(
+  state: TracePreferencesState
+): state is TracePreferencesState & {missing_instrumentation: undefined} {
+  if (typeof state.missing_instrumentation !== 'boolean') {
+    return false;
+  }
+  return true;
+}
+
 export function loadTraceViewPreferences(key: string): TracePreferencesState | null {
   const stored = localStorage.getItem(key);
 
@@ -132,6 +144,10 @@ export function loadTraceViewPreferences(key: string): TracePreferencesState | n
         // Correct old preferences that are missing autogrouping
         if (!isValidAutogrouping(parsed)) {
           parsed.autogroup = {...DEFAULT_TRACE_VIEW_PREFERENCES.autogroup};
+        }
+        if (!isValidMissingInstrumentation(parsed)) {
+          parsed.missing_instrumentation =
+            DEFAULT_TRACE_VIEW_PREFERENCES.missing_instrumentation;
         }
         return parsed;
       }
@@ -173,6 +189,11 @@ export function tracePreferencesReducer(
         autogroup: {sibling: action.payload, parent: action.payload},
       };
     }
+    case 'set missing instrumentation':
+      return {
+        ...state,
+        missing_instrumentation: action.payload,
+      };
     case 'set list width':
       return {
         ...state,
