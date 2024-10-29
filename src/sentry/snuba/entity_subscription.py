2e3ae6e827fde11972aedfa21598509ce1f79108
Ogi <86684834+obostjancic@users.noreply.github.com>
2024-10-29 09:19:11 +0100

    fix

--- src/sentry/snuba/entity_subscription.py
+++ src/sentry/snuba/entity_subscription.py
@@ -268,17 +268,15 @@ class BaseMetricsEntitySubscription(BaseEntitySubscription, ABC):
         self.org_id = extra_fields["org_id"]
         self.time_window = time_window
         self.use_metrics_layer = (
-            (
-                features.has(
-                    "organizations:custom-metrics",
-                    Organization.objects.get_from_cache(id=self.org_id),
-                )
-                # required in order to correctly clean up the custom metric alert subscriptions
-                or features.has(
-                    "custom-metrics-alerts-widgets-removal-info",
-                    Organization.objects.get_from_cache(id=self.org_id),
-                )
-            ),
+            features.has(
+                "organizations:custom-metrics",
+                Organization.objects.get_from_cache(id=self.org_id),
+            )
+            # required in order to correctly clean up the custom metric alert subscriptions
+            or features.has(
+                "custom-metrics-alerts-widgets-removal-info",
+                Organization.objects.get_from_cache(id=self.org_id),
+            )
         )
 
         self.on_demand_metrics_enabled = features.has(
