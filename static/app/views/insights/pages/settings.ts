1d05a1621d58b07f40660b402698dc3c32ae152b
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-10-29 15:30:46 -0400

    update links

--- static/app/views/insights/pages/settings.ts
+++ static/app/views/insights/pages/settings.ts
@@ -2,3 +2,4 @@ import {t} from 'sentry/locale';
 
 export const OVERVIEW_PAGE_TITLE = t('Overview');
 export const DOMAIN_VIEW_BASE_URL = 'performance';
+export const DOMAIN_VIEW_BASE_TITLE = t('Performance');
