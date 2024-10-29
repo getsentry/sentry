ced74e6c838e2a2b6537ba2bf589a918ed43ff0a
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2024-10-29 15:35:52 -0400

    feat(insights): update `useModuleUrl` to always take you to a domain view (#79866)
    Work for #77572 
    
    Now that we are further along in diving performance by domains, we
    should ensure all url's to modules end up within a domain view (meaning
    under `/performance/frontend`, `/performance/backend`). This PR makes
    the necessary changes to do so
    
    **useModuleUrlBuilder changes**
    - The props `forceDomainView` is gone, all module urls should go to
    domain views now if the feature flag is on
    - A prop `detectDomainView` is introduced, which controls whether the
    domain view should be determined from the url (in cases like the trace
    view where they might end up at a backend module from a frontend trace,
    we wouldn't want any auto detection)
    - getModuleView is used to determine which domain applies to which
    module, as all but HTTP module only has one domain view, we can directly
    determine this. For this PR, http will always go to backend, in the
    future, we should determine wether we should go to backend or frontend
    based on the project.

--- static/app/views/insights/pages/backend/settings.ts
+++ static/app/views/insights/pages/backend/settings.ts
@@ -1,4 +1,12 @@
 import {t} from 'sentry/locale';
+import {ModuleName} from 'sentry/views/insights/types';
 
 export const BACKEND_LANDING_SUB_PATH = 'backend';
 export const BACKEND_LANDING_TITLE = t('Backend');
+
+export const MODULES = [
+  ModuleName.DB,
+  ModuleName.HTTP,
+  ModuleName.CACHE,
+  ModuleName.QUEUE,
+];
