425162f5db6b8d559e9ff5029dca80507dc5c6d2
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2024-10-29 13:43:59 -0400

    feat(new-trace): Skipping flaky test

--- static/app/views/performance/newTraceDetails/trace.spec.tsx
+++ static/app/views/performance/newTraceDetails/trace.spec.tsx
@@ -1062,6 +1062,7 @@ describe('trace view', () => {
       });
     });
 
+    // This test is flaky and needs to be fixed
     // eslint-disable-next-line jest/no-disabled-tests
     it.skip('triggers search on load but does not steal focus from node param', async () => {
       mockQueryString('?search=transaction-op-999&node=txn-0');
