af162c30b8ae60bc0f5b560bf3c336dd0e268d45
MichaelSun48 <michael.sun@sentry.io>
2024-10-29 11:06:44 -0700

    update model to use uuidfield, update snapshots

--- src/sentry/models/rollbackidentifier.py
+++ src/sentry/models/rollbackidentifier.py
@@ -1,4 +1,4 @@
-import uuid
+from uuid import uuid4
 
 from django.db import models
 from django.db.models import UniqueConstraint
@@ -9,24 +9,18 @@ from sentry.db.models.fields.foreignkey import FlexibleForeignKey
 from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
 
 
-def default_uuid():
-    return str(uuid.uuid4())
-
-
 @region_silo_model
 class RollbackIdentifier(DefaultFieldsModelExisting):
     __relocation_scope__ = RelocationScope.Organization
 
     user_id = HybridCloudForeignKey("sentry.User", on_delete="CASCADE")
     organization = FlexibleForeignKey("sentry.Organization")
-    uuid = models.CharField(max_length=64, default=default_uuid)
-    share_uuid = models.CharField(max_length=64, default=default_uuid)
+    uuid = models.UUIDField(default=uuid4, unique=True)
+    share_uuid = models.UUIDField(default=uuid4, unique=True)
 
     class Meta:
         app_label = "sentry"
         db_table = "sentry_rollbackidentifier"
         constraints = [
             UniqueConstraint(fields=["user_id", "organization_id"], name="unique_user_org"),
-            UniqueConstraint(fields=["uuid"], name="unique_uuid"),
-            UniqueConstraint(fields=["share_uuid"], name="unique_share_uuid"),
         ]
