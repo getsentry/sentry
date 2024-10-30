c2aac822819c50c87c30de75f4155daf34d7b514
ArthurKnaus <arthur.knaus@sentry.io>
2024-10-30 12:15:16 +0100

    feat(dynamic-sampling): Add unsaved changes warning (#79960)
    Add an `unsafed changes` warning to the dynamic sampling settings form.
    Remove unused react-router v3 props from `OnRouteLeave `.

--- static/app/utils/reactRouter6Compat/onRouteLeave.tsx
+++ static/app/utils/reactRouter6Compat/onRouteLeave.tsx
@@ -1,18 +1,12 @@
 import {type Location, unstable_usePrompt} from 'react-router-dom';
 
-import type {InjectedRouter, PlainRoute} from 'sentry/types/legacyReactRouter';
-
 type ReactRouterV6RouteLeaveCallback = (state: {
   currentLocation: Location;
   nextLocation: Location;
 }) => boolean;
-type ReactRouterV3RouteLeaveCallback = () => string | undefined;
 
 interface OnRouteLeaveProps {
-  legacyWhen: ReactRouterV3RouteLeaveCallback;
   message: string;
-  route: PlainRoute;
-  router: InjectedRouter<any, any>;
   when: ReactRouterV6RouteLeaveCallback;
 }
 
