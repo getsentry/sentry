cd0befcac750910a636386c6317625f8a67b22af
Jan Michael Auer <mail@jauer.org>
2024-10-30 12:12:37 +0100

    feat(ds): Support pagination and proper validation

--- src/sentry/api/endpoints/organization_sampling_project_rates.py
+++ src/sentry/api/endpoints/organization_sampling_project_rates.py
@@ -1,3 +1,8 @@
+from collections.abc import Mapping
+from typing import Any
+
+from django.db import transaction
+from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -5,14 +10,38 @@ from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases import OrganizationEndpoint, OrganizationPermission
+from sentry.api.paginator import OffsetPaginator
+from sentry.api.serializers import Serializer, serialize
 from sentry.models.options.project_option import ProjectOption
 from sentry.models.project import Project
 
 OPTION_KEY = "sentry:target_sample_rate"
 
 
+class GetSerializer(Serializer):
+    """TODO"""
+
+    def get_attrs(self, item_list, user, **kwargs) -> Mapping[Project, float]:
+        return ProjectOption.objects.get_value_bulk(item_list, OPTION_KEY)
+
+    def serialize(self, obj: Project, attrs: float, user, **kwargs) -> Mapping[str, Any]:
+        return {"id": obj.id, "sampleRate": attrs}
+
+
+class PutSerializer(serializers.Serializer):
+    """TODO"""
+
+    id = serializers.IntegerField(required=True)
+    sampleRate = serializers.FloatField(required=True, min_value=0, max_value=1)
+
+    def validate(self, data, **kwargs) -> Mapping[str, Any]:
+        return data
+
+
 @region_silo_endpoint
 class OrganizationSamplingProjectRatesEndpoint(OrganizationEndpoint):
+    """TODO"""
+
     owner = ApiOwner.TELEMETRY_EXPERIENCE
 
     publish_status = {
@@ -23,25 +52,40 @@ class OrganizationSamplingProjectRatesEndpoint(OrganizationEndpoint):
     permission_classes = (OrganizationPermission,)
 
     def get(self, request: Request, organization) -> Response:
-        # TODO: Implement pagination
-        projects = organization.projects
-        options = ProjectOption.objects.get_value_bulk(projects, OPTION_KEY)
-        result = {str(project.id): value for project, value in options.items()}
-        return Response({"projects": result})
+        """TODO"""
+
+        # NOTE: This fetches all projects in the organization. We do not filter
+        # to projects the org member has access to as the sample rate and
+        # project ID do not constitute sensitive information.
+        queryset = Project.objects.filter(
+            organization=organization,
+            status=Project.ACTIVE,
+        )
+
+        return self.paginate(
+            request=request,
+            queryset=queryset,
+            order_by="id",
+            paginator_cls=OffsetPaginator,
+            on_results=lambda x: serialize(x, request.user, GetSerializer()),
+        )
 
     def put(self, request: Request, organization) -> Response:
-        options = request.data.get("projects")
+        """TODO"""
 
-        # TODO: Validate payload against schema
-        if not isinstance(options, dict):
+        if not isinstance(request.DATA, list):
             raise ValueError("projects must be a dictionary")
-        for sample_rate in options.values():
-            if not isinstance(sample_rate, (int, float)) or sample_rate < 0 or sample_rate > 1:
-                raise ValueError("all sample_rates must be a number between 0 and 1")
 
-        projects = Project.objects.get_many_from_cache(list(options.keys()))
+        serializer = PutSerializer(request.DATA, many=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
 
-        for project in projects:
-            project.set_option(OPTION_KEY, options[str(project.id)])
+        project_ids = [d["id"] for d in serializer.data]
+        projects = self.get_projects(request, organization, project_ids=project_ids)
+
+        rate_by_project = {d["id"]: d["sampleRate"] for d in serializer.data}
+        with transaction.atomic():
+            for project in projects:
+                project.set_option(OPTION_KEY, rate_by_project[project.id])
 
         return Response(status=204)
