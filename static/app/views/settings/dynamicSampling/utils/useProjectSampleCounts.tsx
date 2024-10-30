696fabbf21a663c61b56976bdee8717609422e36
ArthurKnaus <arthur.knaus@sentry.io>
2024-10-30 14:32:29 +0100

    feat(dynamic-sampling): Use spans root count metric (#79962)
    Use `c:spans/count_per_root_project@none` metric.
    Remove mock data.

--- static/app/views/settings/dynamicSampling/utils/useProjectSampleCounts.tsx
+++ static/app/views/settings/dynamicSampling/utils/useProjectSampleCounts.tsx
@@ -1,25 +1,23 @@
 import {useMemo} from 'react';
 
 import type {MRI} from 'sentry/types/metrics';
+import type {Project} from 'sentry/types/project';
 import {
   type MetricsQueryApiQueryParams,
   useMetricsQuery,
 } from 'sentry/utils/metrics/useMetricsQuery';
 import useProjects from 'sentry/utils/useProjects';
 
-// TODO(aknaus): Switch to c:spans/count_per_root_project@none once available
-const SPANS_COUNT_METRIC: MRI = `c:transactions/count_per_root_project@none`;
+const SPANS_COUNT_METRIC: MRI = `c:spans/count_per_root_project@none`;
 const metricsQuery: MetricsQueryApiQueryParams[] = [
   {
     mri: SPANS_COUNT_METRIC,
-    aggregation: 'count',
+    aggregation: 'sum',
     name: 'spans',
-    groupBy: ['project'],
+    groupBy: ['project', 'target_project_id'],
     orderBy: 'desc',
   },
 ];
-
-const fakeSubProjects = ['angular', 'sentry', 'snuba', 'relay', 'email-service'];
 
 export function useProjectSampleCounts({period}: {period: '24h' | '30d'}) {
   const {projects, fetching} = useProjects();
@@ -42,49 +40,96 @@ export function useProjectSampleCounts({period}: {period: '24h' | '30d'}) {
     }
   );
 
+  const queryResult = data?.data?.[0];
+
   const projectBySlug = useMemo(
     () =>
-      projects.reduce((acc, project) => {
-        acc[project.slug] = project;
-        return acc;
-      }, {}),
+      projects.reduce(
+        (acc, project) => {
+          acc[project.slug] = project;
+          return acc;
+        },
+        {} as Record<string, Project>
+      ),
     [projects]
   );
 
-  const groupedCounts = useMemo(
+  const projectById = useMemo(
     () =>
-      (data?.data[0] ?? [])
-        .map(item => {
-          // TODO(aknaus): Remove mock data once real data is available
-          // Create random sub-projects for testing UI
-          const hasSubProjects = Math.random() > 0.3;
-          const countMagnitude = Math.floor(Math.log10(item.totals));
-          const subProjects = hasSubProjects
-            ? fakeSubProjects.map(slug => ({
-                slug: slug,
-                count: Math.floor(Math.random() * Math.pow(10, countMagnitude + 1)),
-              }))
-            : [];
+      projects.reduce(
+        (acc, project) => {
+          acc[project.id] = project;
+          return acc;
+        },
+        {} as Record<string, Project>
+      ),
+    [projects]
+  );
 
-          const total =
-            item.totals +
-            subProjects.reduce((acc, subProject) => acc + subProject.count, 0);
+  const projectEntries = useMemo(() => {
+    const map = new Map<
+      string,
+      {
+        count: number;
+        ownCount: number;
+        slug: string;
+        subProjects: Array<{count: number; slug: string}>;
+      }
+    >();
 
+    for (const row of queryResult ?? []) {
+      const project = row.by.project && projectBySlug[row.by.project];
+      const subProject =
+        row.by.target_project_id && projectById[row.by.target_project_id];
+      const rowValue = row.totals;
+
+      if (!project || !subProject) {
+        continue;
+      }
+
+      const existingEntry = map.get(project.slug) ?? {
+        count: 0,
+        ownCount: 0,
+        slug: project.slug,
+        subProjects: [],
+      };
+
+      existingEntry.count += rowValue;
+
+      if (subProject && subProject.id === project.id) {
+        existingEntry.ownCount = rowValue;
+      } else {
+        existingEntry.subProjects.push({
+          count: rowValue,
+          slug: subProject.slug,
+        });
+      }
+
+      map.set(project.slug, existingEntry);
+    }
+
+    return map;
+  }, [projectById, projectBySlug, queryResult]);
+
+  const items = useMemo(
+    () =>
+      projectEntries
+        .entries()
+        .map(([key, value]) => {
           return {
-            id: item.by.project,
-            project: projectBySlug[item.by.project],
-            count: total,
-            ownCount: item.totals,
+            id: key,
+            project: projectBySlug[key],
+            count: value.count,
+            ownCount: value.ownCount,
             // This is a placeholder value to satisfy typing
             // the actual value is calculated in the balanceSampleRate function
             sampleRate: 1,
-            subProjects: subProjects.toSorted((a, b) => b.count - a.count),
+            subProjects: value.subProjects.toSorted((a, b) => b.count - a.count),
           };
         })
-        // Remove items where we cannot match the project
-        .filter(item => item.project),
-    [data?.data, projectBySlug]
+        .toArray(),
+    [projectBySlug, projectEntries]
   );
 
-  return {data: groupedCounts, isPending: fetching || isPending, isError, refetch};
+  return {data: items, isPending: fetching || isPending, isError, refetch};
 }
