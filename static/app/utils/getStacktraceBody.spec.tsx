386fa68e3af4e5b42774881a1cec0fcdd08d2333
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-10-31 06:11:41 -0700

    fix(similarity): Show threads in similarity diff (#80029)
    Show threads (if it exists) instead of message, when exception does not
    exist in similarity diff ui
    
    ---------
    
    Co-authored-by: Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
    Co-authored-by: Katie Byers <katie.byers@sentry.io>

--- static/app/utils/getStacktraceBody.spec.tsx
+++ static/app/utils/getStacktraceBody.spec.tsx
@@ -2,6 +2,7 @@ import {EventFixture} from 'sentry-fixture/event';
 import {
   EventStacktraceExceptionFixture,
   EventStacktraceMessageFixture,
+  EventStacktraceThreadsFixture,
 } from 'sentry-fixture/eventStacktraceException';
 
 import getStacktraceBody from 'sentry/utils/getStacktraceBody';
@@ -9,6 +10,7 @@ import getStacktraceBody from 'sentry/utils/getStacktraceBody';
 describe('getStacktraceBody', function () {
   const eventException = EventStacktraceExceptionFixture({platform: 'python'});
   const eventMessage = EventStacktraceMessageFixture({platform: 'python'});
+  const eventThreads = EventStacktraceThreadsFixture({platform: 'python'});
 
   it('formats with an exception', function () {
     const result = getStacktraceBody(eventException);
@@ -22,6 +24,15 @@ describe('getStacktraceBody', function () {
   it('formats with a message', function () {
     const result = getStacktraceBody(eventMessage);
     expect(result).toEqual(['Something is broken']);
+  });
+
+  it('formats with a thread', function () {
+    const result = getStacktraceBody(eventThreads);
+    expect(result).toEqual([
+      `Error: an error occurred
+  File "application", line 1, in main
+  File "application", line 2, in doThing`,
+    ]);
   });
 
   it('returns empty array for empty event entries', function () {
