bb059ad5873329d63aee7ec73fd284d51e12cd4c
Michelle Fu <michelle.fu@sentry.io>
2024-10-31 16:07:07 -0700

    looks ugly :(

--- static/app/views/alerts/rules/metric/details/body.tsx
+++ static/app/views/alerts/rules/metric/details/body.tsx
@@ -1,11 +1,14 @@
-import {Fragment} from 'react';
+import {Fragment, useCallback} from 'react';
 import styled from '@emotion/styled';
 import type {Location} from 'history';
 import moment from 'moment-timezone';
 
 import type {Client} from 'sentry/api';
 import {Alert} from 'sentry/components/alert';
+import {useDismissable} from 'sentry/components/banner';
+import {Button} from 'sentry/components/button';
 import {getInterval} from 'sentry/components/charts/utils';
+import {feedbackClient} from 'sentry/components/featureFeedback/feedbackModal';
 import * as Layout from 'sentry/components/layouts/thirds';
 import Link from 'sentry/components/links/link';
 import Panel from 'sentry/components/panels/panel';
@@ -14,12 +17,15 @@ import Placeholder from 'sentry/components/placeholder';
 import type {ChangeData} from 'sentry/components/timeRangeSelector';
 import {TimeRangeSelector} from 'sentry/components/timeRangeSelector';
 import {Tooltip} from 'sentry/components/tooltip';
+import {IconClose} from 'sentry/icons';
 import {t, tct} from 'sentry/locale';
+import ConfigStore from 'sentry/stores/configStore';
 import {space} from 'sentry/styles/space';
 import {RuleActionsCategories} from 'sentry/types/alerts';
 import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import type {Organization} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
+import {trackAnalytics} from 'sentry/utils/analytics';
 import {formatMRIField} from 'sentry/utils/metrics/mri';
 import {shouldShowOnDemandMetricAlertUI} from 'sentry/utils/onDemandMetrics/features';
 import {ErrorMigrationWarning} from 'sentry/views/alerts/rules/metric/details/errorMigrationWarning';
@@ -63,6 +69,65 @@ interface MetricDetailsBodyProps extends RouteComponentProps<{}, {}> {
   project?: Project;
   rule?: MetricRule;
   selectedIncident?: Incident | null;
+}
+
+interface AnomalyDetectionFeedbackProps {
+  id: string;
+  organization: Organization;
+  selectedIncident: Incident;
+}
+
+function AnomalyDetectionFeedbackBanner({
+  id,
+  organization,
+  selectedIncident,
+}: AnomalyDetectionFeedbackProps) {
+  const [isSubmitted, submit] = useDismissable(id);
+
+  const handleClick = useCallback(
+    (isAnomaly: boolean) => {
+      trackAnalytics('anomaly_detection.incident_banner_feedback_received', {
+        is_anomaly: isAnomaly,
+        organization,
+        incident_id: id,
+      });
+      feedbackClient.captureEvent({
+        request: {
+          url: window.location.href, // gives the full url (origin + pathname)
+        },
+        tags: {
+          featureName: 'anomaly-detection-alerts-feedback',
+          project: selectedIncident.projects[0], // each metric alert is associated with precisely 1 project
+          incident_identifier: selectedIncident.identifier,
+          isAnomaly,
+        },
+        user: ConfigStore.get('user'),
+        level: 'info',
+        message: 'Escalating Issues Banner Feedback',
+      });
+      submit();
+    },
+    [id, organization, selectedIncident.identifier, selectedIncident.projects, submit]
+  );
+
+  if (isSubmitted) {
+    return null;
+  }
+  return (
+    <StyledAlert
+      type="info"
+      trailingItems={
+        <Fragment>
+          <Button onClick={() => handleClick(true)}>Yes</Button>
+          <Button onClick={() => handleClick(false)}>No</Button>
+        </Fragment>
+      }
+      showIcon
+    >
+      <CloseButton onClick={submit} aria-label={t('Close')} />
+      {'Friends, Romans, countrymen, lend me your ears.' + selectedIncident.identifier}
+    </StyledAlert>
+  );
 }
 
 export default function MetricDetailsBody({
@@ -238,9 +303,11 @@ export default function MetricDetailsBody({
 
           {selectedIncident?.alertRule.detectionType ===
             AlertRuleComparisonType.DYNAMIC && (
-            <StyledAlert type="info" showIcon>
-              {t('Friends, Romans, countrymen, lend me your ears.')}
-            </StyledAlert>
+            <AnomalyDetectionFeedbackBanner
+              id={selectedIncident.identifier}
+              organization={organization}
+              selectedIncident={selectedIncident}
+            />
           )}
 
           <ErrorMigrationWarning project={project} rule={rule} />
@@ -249,7 +316,6 @@ export default function MetricDetailsBody({
           <MetricChart
             api={api}
             rule={rule}
-            incidents={incidents}
             anomalies={anomalies}
             timePeriod={timePeriod}
             selectedIncident={selectedIncident}
@@ -345,3 +411,20 @@ const StyledSubHeader = styled('div')`
 const StyledTimeRangeSelector = styled(TimeRangeSelector)`
   margin-right: ${space(1)};
 `;
+
+const CloseButton = styled(Button)`
+  position: absolute;
+  display: block;
+  top: ${space(2)};
+  right: ${space(2)};
+  cursor: pointer;
+  z-index: 1;
+`;
+
+CloseButton.defaultProps = {
+  icon: <IconClose />,
+  ['aria-label']: t('Close'),
+  priority: 'link',
+  borderless: true,
+  size: 'xs',
+};
