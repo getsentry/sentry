e4ac67b367f7836b9b311425de4da87e8c46d00e
Nar Saynorath <nar.saynorath@sentry.io>
2024-10-31 15:26:18 -0400

    feat(dashboards): Add custom search bar for EAP

---
+++ static/app/views/dashboards/widgetBuilder/buildSteps/filterResultsStep/spansSearchBar.tsx
@@ -0,0 +1,28 @@
+import {EAPSpanSearchQueryBuilder} from 'sentry/components/performance/spanSearchQueryBuilder';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import type {WidgetBuilderSearchBarProps} from 'sentry/views/dashboards/datasetConfig/base';
+import {useSpanTags} from 'sentry/views/explore/contexts/spanTagsContext';
+
+/**
+ * A search bar for exploring tags and values for spans in Dashboards.
+ * This assumes that the dataset used is the EAP spans dataset.
+ */
+function SpansSearchBar({widgetQuery, onSearch}: WidgetBuilderSearchBarProps) {
+  const {
+    selection: {projects},
+  } = usePageFilters();
+  const numberTags = useSpanTags('number');
+  const stringTags = useSpanTags('string');
+  return (
+    <EAPSpanSearchQueryBuilder
+      initialQuery={widgetQuery.conditions}
+      onSearch={onSearch}
+      numberTags={numberTags}
+      stringTags={stringTags}
+      searchSource="explore"
+      projects={projects}
+    />
+  );
+}
+
+export default SpansSearchBar;
