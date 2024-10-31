17c09c7f2c41aabf5b967801ccb6b7dd9d25a75e
Colleen O'Rourke <colleen@sentry.io>
2024-10-31 12:13:04 -0700

    fix(alerts): Fix alert rule deletion task (#80082)
    Delete incidents when running the delete alert rules task
    
    Fixes SENTRY-3DRY

--- tests/sentry/deletions/test_alert_rule.py
+++ tests/sentry/deletions/test_alert_rule.py
@@ -1,6 +1,7 @@
 from unittest.mock import patch
 
 import orjson
+from django.utils import timezone
 from urllib3 import HTTPResponse
 
 from sentry.deletions.tasks.scheduled import run_scheduled_deletions
@@ -11,6 +12,7 @@ from sentry.incidents.models.alert_rule import (
     AlertRuleSensitivity,
     AlertRuleTrigger,
 )
+from sentry.incidents.models.incident import Incident
 from sentry.models.organization import Organization
 from sentry.testutils.cases import TestCase
 from sentry.testutils.helpers.features import with_feature
@@ -22,6 +24,14 @@ class DeleteAlertRuleTest(TestCase, HybridCloudTestMixin):
         organization = self.create_organization()
         alert_rule = self.create_alert_rule(organization=organization)
         alert_rule_trigger = self.create_alert_rule_trigger(alert_rule=alert_rule)
+        incident = self.create_incident(
+            organization,
+            title="Incident #1",
+            date_started=timezone.now(),
+            date_detected=timezone.now(),
+            projects=[self.project],
+            alert_rule=alert_rule,
+        )
 
         self.ScheduledDeletion.schedule(instance=alert_rule, days=0)
 
@@ -31,6 +41,7 @@ class DeleteAlertRuleTest(TestCase, HybridCloudTestMixin):
         assert Organization.objects.filter(id=organization.id).exists()
         assert not AlertRule.objects.filter(id=alert_rule.id).exists()
         assert not AlertRuleTrigger.objects.filter(id=alert_rule_trigger.id).exists()
+        assert not Incident.objects.filter(id=incident.id).exists()
 
     @with_feature("organizations:anomaly-detection-alerts")
     @with_feature("organizations:anomaly-detection-rollout")
@@ -42,7 +53,6 @@ class DeleteAlertRuleTest(TestCase, HybridCloudTestMixin):
     )
     def test_dynamic_alert_rule(self, mock_store_request, mock_delete_request):
         organization = self.create_organization()
-        alert_rule = self.create_alert_rule(organization=organization)
 
         seer_return_value = {"success": True}
         mock_store_request.return_value = HTTPResponse(orjson.dumps(seer_return_value), status=200)
@@ -55,6 +65,17 @@ class DeleteAlertRuleTest(TestCase, HybridCloudTestMixin):
             detection_type=AlertRuleDetectionType.DYNAMIC,
             organization=organization,
         )
+        alert_rule_trigger = self.create_alert_rule_trigger(
+            alert_rule=alert_rule, alert_threshold=0
+        )
+        incident = self.create_incident(
+            organization,
+            title="Incident #1",
+            date_started=timezone.now(),
+            date_detected=timezone.now(),
+            projects=[self.project],
+            alert_rule=alert_rule,
+        )
 
         self.ScheduledDeletion.schedule(instance=alert_rule, days=0)
 
@@ -63,5 +84,7 @@ class DeleteAlertRuleTest(TestCase, HybridCloudTestMixin):
 
         assert Organization.objects.filter(id=organization.id).exists()
         assert not AlertRule.objects.filter(id=alert_rule.id).exists()
+        assert not AlertRuleTrigger.objects.filter(id=alert_rule_trigger.id).exists()
+        assert not Incident.objects.filter(id=incident.id).exists()
 
         assert mock_delete_request.call_count == 1
