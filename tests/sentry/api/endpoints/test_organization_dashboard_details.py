3a3b0b3f6fbac64b0dfe0a76429717acfba1d9f8
Harshitha Durai <76853136+harshithadurai@users.noreply.github.com>
2024-11-01 14:02:46 -0400

    ref(dashboards): Remove feature flag for Big Number Equations (backend) (#79860)
    remove feature flags added here:
    https://github.com/getsentry/sentry/pull/77713
    
    Co-authored-by: harshithadurai <harshi.durai@esentry.io>

--- tests/sentry/api/endpoints/test_organization_dashboard_details.py
+++ tests/sentry/api/endpoints/test_organization_dashboard_details.py
@@ -339,11 +339,10 @@ class OrganizationDashboardDetailsGetTest(OrganizationDashboardDetailsTestCase):
             selected_aggregate=1,
             order=0,
         )
-        with self.feature({"organizations:dashboards-bignumber-equations": True}):
-            response = self.do_request(
-                "get",
-                self.url(self.dashboard.id),
-            )
+        response = self.do_request(
+            "get",
+            self.url(self.dashboard.id),
+        )
         assert response.status_code == 200, response.content
 
         assert response.data["widgets"][0]["queries"][0]["selectedAggregate"] is None
@@ -754,8 +753,7 @@ class OrganizationDashboardDetailsPutTest(OrganizationDashboardDetailsTestCase):
                 },
             ],
         }
-        with self.feature({"organizations:dashboards-bignumber-equations": True}):
-            response = self.do_request("put", self.url(self.dashboard.id), data=data)
+        response = self.do_request("put", self.url(self.dashboard.id), data=data)
         assert response.status_code == 200, response.data
 
         widgets = self.get_widgets(self.dashboard.id)
