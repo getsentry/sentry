c50a543e3e6dc4fa01ee4984d4d068b03f073405
Nar Saynorath <nar.saynorath@sentry.io>
2024-11-01 16:08:37 -0400

    fix(dashboard): Only update permission if not none (#80142)
    The dashboard request body can contain `permissions: null` which means
    that creating/updating fails.
    Adds a feature flag, but also ignore when the permission is None.
    
    Also adds a feature flag check.
    
    Closes SENTRY-3HCC

--- tests/sentry/api/endpoints/test_organization_dashboard_details.py
+++ tests/sentry/api/endpoints/test_organization_dashboard_details.py
@@ -1869,9 +1869,10 @@ class OrganizationDashboardDetailsPutTest(OrganizationDashboardDetailsTestCase):
             "title": "Dashboard",
             "permissions": {"isCreatorOnlyEditable": "False"},
         }
-        response = self.do_request(
-            "put", f"{self.url(self.dashboard.id)}?environment=mock_env", data=data
-        )
+        with self.feature({"organizations:dashboards-edit-access": True}):
+            response = self.do_request(
+                "put", f"{self.url(self.dashboard.id)}?environment=mock_env", data=data
+            )
         assert response.status_code == 200, response.data
         assert response.data["permissions"]["isCreatorOnlyEditable"] is False
 
@@ -1882,9 +1883,10 @@ class OrganizationDashboardDetailsPutTest(OrganizationDashboardDetailsTestCase):
             "title": "Dashboard",
             "permissions": {"isCreatorOnlyEditable": "true"},
         }
-        response = self.do_request(
-            "put", f"{self.url(self.dashboard.id)}?environment=mock_env", data=data
-        )
+        with self.feature({"organizations:dashboards-edit-access": True}):
+            response = self.do_request(
+                "put", f"{self.url(self.dashboard.id)}?environment=mock_env", data=data
+            )
         assert response.status_code == 200, response.data
         assert response.data["permissions"]["isCreatorOnlyEditable"] is True
 
@@ -1900,9 +1902,10 @@ class OrganizationDashboardDetailsPutTest(OrganizationDashboardDetailsTestCase):
         }
 
         assert permission.is_creator_only_editable is False
-        response = self.do_request(
-            "put", f"{self.url(self.dashboard.id)}?environment=mock_env", data=data
-        )
+        with self.feature({"organizations:dashboards-edit-access": True}):
+            response = self.do_request(
+                "put", f"{self.url(self.dashboard.id)}?environment=mock_env", data=data
+            )
         assert response.status_code == 200, response.data
         assert response.data["permissions"]["isCreatorOnlyEditable"] is True
 
@@ -1916,9 +1919,10 @@ class OrganizationDashboardDetailsPutTest(OrganizationDashboardDetailsTestCase):
             "title": "Dashboard",
             "permissions": {"isCreatorOnlyEditable": "something-invalid"},
         }
-        response = self.do_request(
-            "put", f"{self.url(self.dashboard.id)}?environment=mock_env", data=data
-        )
+        with self.feature({"organizations:dashboards-edit-access": True}):
+            response = self.do_request(
+                "put", f"{self.url(self.dashboard.id)}?environment=mock_env", data=data
+            )
         assert response.status_code == 400, response.data
         assert "isCreatorOnlyEditable" in response.data["permissions"]
 
@@ -1969,6 +1973,17 @@ class OrganizationDashboardDetailsPutTest(OrganizationDashboardDetailsTestCase):
         with self.feature({"organizations:dashboards-edit-access": True}):
             response = self.do_request("put", self.url(self.dashboard.id))
         assert response.status_code == 200, response.content
+
+    def test_update_dashboard_permissions_with_none_does_not_create_permissions_object(self):
+        data = {
+            "title": "Dashboard",
+            "permissions": None,
+        }
+        with self.feature({"organizations:dashboards-edit-access": True}):
+            response = self.do_request("put", self.url(self.dashboard.id), data=data)
+        assert response.status_code == 200, response.data
+        assert response.data["permissions"] is None
+        assert not DashboardPermissions.objects.filter(dashboard=self.dashboard).exists()
 
 
 class OrganizationDashboardDetailsOnDemandTest(OrganizationDashboardDetailsTestCase):
