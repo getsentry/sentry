1bc99f4584e4c277dd1358e590b447df4f43139d
Athena Moghaddam <132939361+sentaur-athena@users.noreply.github.com>
2024-11-01 09:34:00 -0700

    feat(oauth): send feature flag to frontend (#80120)
    There's no org [where this feature flag is being
    checked](https://github.com/getsentry/getsentry/blob/master/static/getsentry/gsAdmin/views/instanceLevelOAuth/components/newInstanceLevelOAuthClient.tsx#L24)
    so enabling it through the config. This basically makes it available in
    the Config object on the frontend

--- src/sentry/web/client_config.py
+++ src/sentry/web/client_config.py
@@ -224,6 +224,13 @@ class _ClientConfig:
             yield "relocation:enabled"
         if features.has("system:multi-region"):
             yield "system:multi-region"
+        # TODO @athena: remove this feature flag after development is done
+        # this is a temporary hack to be able to used flagpole in a case where there's no organization
+        # availble on the frontend
+        if self.last_org and features.has(
+            "organizations:scoped-partner-oauth", self.last_org, actor=self.user
+        ):
+            yield "system:scoped-partner-oauth"
 
     @property
     def needs_upgrade(self) -> bool:
