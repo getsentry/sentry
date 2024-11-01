92482023eeeb403ad25a69874e0b4b392c89e36d
harshithadurai <harshi.durai@esentry.io>
2024-11-01 12:20:31 -0400

    new migration

--- src/sentry/models/dashboard_permissions.py
+++ src/sentry/models/dashboard_permissions.py
@@ -16,7 +16,7 @@ class DashboardPermissions(Model):
     __relocation_scope__ = RelocationScope.Organization
 
     is_editable_by_everyone = models.BooleanField(default=True)
-    teams_with_edit_access = models.ManyToManyField("sentry.Team", null=True)
+    teams_with_edit_access = models.ManyToManyField("sentry.Team", blank=True)
 
     dashboard = models.OneToOneField(
         "sentry.Dashboard", on_delete=models.CASCADE, related_name="permissions"
