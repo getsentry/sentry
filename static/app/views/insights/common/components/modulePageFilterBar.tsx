a54cd4323a5ba218a3a0d5de8a0b15120c7fcbd2
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-11-01 10:43:39 -0400

    grey out and disable options that aren't selected

--- static/app/views/insights/common/components/modulePageFilterBar.tsx
+++ static/app/views/insights/common/components/modulePageFilterBar.tsx
@@ -1,4 +1,6 @@
 import {type ComponentProps, Fragment, useEffect, useState} from 'react';
+import styled from '@emotion/styled';
+import moment from 'moment-timezone';
 
 import {
   DatePageFilter,
@@ -7,10 +9,12 @@ import {
 import {EnvironmentPageFilter} from 'sentry/components/organizations/environmentPageFilter';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilter';
+import {parseStatsPeriod} from 'sentry/components/timeRangeSelector/utils';
 import {Tooltip} from 'sentry/components/tooltip';
+import {IconBusiness} from 'sentry/icons';
 import {t} from 'sentry/locale';
-import {SECOND} from 'sentry/utils/formatters';
-// import useOrganization from 'sentry/utils/useOrganization';
+import {DAY, SECOND} from 'sentry/utils/formatters';
+import useOrganization from 'sentry/utils/useOrganization';
 import useProjects from 'sentry/utils/useProjects';
 import {useHasFirstSpan} from 'sentry/views/insights/common/queries/useHasFirstSpan';
 import type {ModuleName} from 'sentry/views/insights/types';
@@ -22,16 +26,22 @@ type Props = {
 };
 
 const CHANGE_PROJECT_TEXT = t('Make sure you have the correct project selected.');
+const QUERY_DATE_RANGE_LIMIT = 7;
+const QUERY_DATE_RANGE_LIMIT_MS = 7 * DAY;
 
 export function ModulePageFilterBar({moduleName, onProjectChange, extraFilters}: Props) {
   const {projects: allProjects} = useProjects();
-  // const organization = useOrganization();
+  const organization = useOrganization();
 
   const hasDataWithSelectedProjects = useHasFirstSpan(moduleName);
   const hasDataWithAllProjects = useHasFirstSpan(moduleName, allProjects);
   const [showTooltip, setShowTooltip] = useState(false);
 
-  // const hasDateRangeQueryLimit = organization.features.includes('insights-query-date-range-limit');
+  const getDateRange = memoizedDateDifference();
+
+  const hasDateRangeQueryLimit = organization.features.includes(
+    'insights-query-date-range-limit'
+  );
 
   const handleClickAnywhereOnPage = () => {
     setShowTooltip(false);
@@ -58,8 +68,23 @@ export function ModulePageFilterBar({moduleName, onProjectChange, extraFilters}:
   }, []);
 
   const dateFilterProps: DatePageFilterProps = {};
-  if (true) {
-    dateFilterProps.maxPickableDays = 7;
+  if (hasDateRangeQueryLimit) {
+    dateFilterProps.relativeOptions = {
+      '1h': t('Last 1 hour'),
+      '24h': t('Last 24 hours'),
+      '7d': t('Last 7 days'),
+      '14d': <DisabledDateOption value={t('Last 14 days')} />,
+      '30d': <DisabledDateOption value={t('Last 30 days')} />,
+      '90d': <DisabledDateOption value={t('Last 90 days')} />,
+    };
+    dateFilterProps.maxPickableDays = QUERY_DATE_RANGE_LIMIT;
+    dateFilterProps.isOptionDisabled = ({value}) => {
+      if (value === 'absolute') {
+        return false;
+      }
+      const dateRange = getDateRange(value);
+      return dateRange > QUERY_DATE_RANGE_LIMIT_MS;
+    };
   }
 
   return (
@@ -83,3 +108,35 @@ export function ModulePageFilterBar({moduleName, onProjectChange, extraFilters}:
     </Fragment>
   );
 }
+
+function DisabledDateOption({value}: {value: string}) {
+  return (
+    <DisabledDateOptionContainer>
+      {value}
+      <StyledIconBuisness />
+    </DisabledDateOptionContainer>
+  );
+}
+
+const DisabledDateOptionContainer = styled('div')`
+  display: flex;
+  align-items: center;
+`;
+
+const StyledIconBuisness = styled(IconBusiness)`
+  margin-left: auto;
+`;
+
+// There's only a couple options, so might as well memoize this and not repeat it many times
+const memoizedDateDifference = () => {
+  const cache = {};
+  return (value: string) => {
+    if (value in cache) {
+      return cache[value];
+    }
+    const {start, end} = parseStatsPeriod(value);
+    const difference = moment(end).diff(moment(start));
+    cache[value] = difference;
+    return difference;
+  };
+};
