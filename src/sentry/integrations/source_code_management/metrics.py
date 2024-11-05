2560f27ecfeb62d88cb56cadc17adcd07576f19d
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2024-11-05 11:03:30 -0800

    chore(integrations): RepositoryIntegration metrics (#79999)
    Add metrics to the following `RepositoryIntegration` methods:
    `get_stacktrace_link`, `check_file`, and `get_codeowner_file`.

---
+++ src/sentry/integrations/source_code_management/metrics.py
@@ -0,0 +1,54 @@
+from collections.abc import Mapping
+from enum import Enum
+from typing import Any
+
+from attr import dataclass
+
+from sentry.integrations.base import IntegrationDomain
+from sentry.integrations.models.organization_integration import OrganizationIntegration
+from sentry.integrations.services.integration import RpcOrganizationIntegration
+from sentry.integrations.utils.metrics import IntegrationEventLifecycleMetric
+from sentry.models.organization import Organization
+from sentry.organizations.services.organization import RpcOrganization
+
+
+class RepositoryIntegrationInteractionType(Enum):
+    """
+    A RepositoryIntegration feature.
+    """
+
+    GET_STACKTRACE_LINK = "GET_STACKTRACE_LINK"
+    GET_CODEOWNER_FILE = "GET_CODEOWNER_FILE"
+    CHECK_FILE = "CHECK_FILE"
+
+    def __str__(self) -> str:
+        return self.value.lower()
+
+
+@dataclass
+class RepositoryIntegrationInteractionEvent(IntegrationEventLifecycleMetric):
+    """
+    An instance to be recorded of a RepositoryIntegration feature call.
+    """
+
+    interaction_type: RepositoryIntegrationInteractionType
+    provider_key: str
+
+    # Optional attributes to populate extras
+    organization: Organization | RpcOrganization | None = None
+    org_integration: OrganizationIntegration | RpcOrganizationIntegration | None = None
+
+    def get_integration_domain(self) -> IntegrationDomain:
+        return IntegrationDomain.SOURCE_CODE_MANAGEMENT
+
+    def get_integration_name(self) -> str:
+        return self.provider_key
+
+    def get_interaction_type(self) -> str:
+        return str(self.interaction_type)
+
+    def get_extras(self) -> Mapping[str, Any]:
+        return {
+            "organization_id": (self.organization.id if self.organization else None),
+            "org_integration_id": (self.org_integration.id if self.org_integration else None),
+        }
