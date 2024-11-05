42a8f360554a2437742f92959769f6ca8070f60c
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-11-05 16:41:32 -0500

    feat(crons): Add cronsim behind an option (#80271)

--- src/sentry/monitors/validators.py
+++ src/sentry/monitors/validators.py
@@ -2,7 +2,8 @@ import re
 from typing import Literal
 
 import sentry_sdk
-from croniter import CroniterBadDateError, croniter
+from croniter import CroniterBadCronError, CroniterBadDateError
+from cronsim import CronSimError
 from django.core.exceptions import ValidationError
 from django.utils import timezone
 from drf_spectacular.types import OpenApiTypes
@@ -222,9 +223,6 @@ class ConfigValidator(serializers.Serializer):
                     schedule = NONSTANDARD_CRONTAB_SCHEDULES[schedule]
                 except KeyError:
                     raise ValidationError({"schedule": "Schedule was not parseable"})
-            # crontab schedule must be valid
-            if not croniter.is_valid(schedule):
-                raise ValidationError({"schedule": "Schedule was not parseable"})
 
             # XXX(epurkhiser): Make sure we can traverse forward and back in
             # the schedule. croniter is good, but there are some very edge case
@@ -233,7 +231,7 @@ class ConfigValidator(serializers.Serializer):
             try:
                 get_next_schedule(now, CrontabSchedule(schedule))
                 get_prev_schedule(now, now, CrontabSchedule(schedule))
-            except CroniterBadDateError:
+            except (CroniterBadCronError, CroniterBadDateError, CronSimError):
                 raise ValidationError({"schedule": "Schedule is invalid"})
 
             # Do not support 6 or 7 field crontabs
