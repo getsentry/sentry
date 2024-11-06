555df7d7d7fe7004b6badecb1761b0ed1294b2ad
viglia <francesco.vigliaturo@sentry.io>
2024-11-06 11:28:55 +0100

    feat(profiling): add organizations:continuous-profiling to the list of exposable features (#80236)
    FF for org that will allow to choose whether only beta orgs or all orgs can send profiles

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -76,6 +76,10 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:codecov-commit-sha-from-git-blame", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable continuous profiling
     manager.add("organizations:continuous-profiling", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    # Enabled for beta orgs
+    manager.add("organizations:continuous-profiling-beta", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    # Enable stopping the ingestion of continuous profile for non-beta orgs
+    manager.add("organizations:continuous-profiling-beta-ingest", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable continuous profiling ui
     manager.add("organizations:continuous-profiling-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Display profile durations on the stats page
