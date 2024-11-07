c7d2d4afd08a9123b9bc10870b8b5a36a2d7053d
Enoch Tang <enoch.tang@sentry.io>
2024-11-07 13:55:12 -0500

    feat(task-worker): Add taskworker pull (#80161)
    This PR is responsible for adding the taskworker pull which is
    responsible for fetching tasks from a task buffer (inflight actiivation
    store) and executing them in a multiprocess pool. After tasks are
    executed, the worker will update the task status appropriately in the
    task queue.
    
    Review notes / Changes:
    * Default `expires` and `processing_deadline_duration` are defined on
    the namespace level. If these fields are not provided in the task, the
    namespace defaults will be used.
    * The `process_deadline_duration` is defined on the task activation.
    When the arroyo consumer stores an inflight activation into the buffer,
    it will use this value to set the `processing_deadline`.
    * The `TaskClient` is a dummy class created for testing. Once we unblock
    GRPC, we can properly implement this.
    
    ---------
    
    Co-authored-by: Mark Story <mark@mark-story.com>

--- src/sentry/taskworker/registry.py
+++ src/sentry/taskworker/registry.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+import datetime
 import logging
 from collections.abc import Callable
 from functools import cached_property
@@ -11,6 +12,7 @@ from django.conf import settings
 from sentry_protos.sentry.v1.taskworker_pb2 import TaskActivation
 
 from sentry.conf.types.kafka_definition import Topic
+from sentry.taskworker.constants import DEFAULT_PROCESSING_DEADLINE
 from sentry.taskworker.retry import Retry
 from sentry.taskworker.router import TaskRouter
 from sentry.taskworker.task import P, R, Task
@@ -27,11 +29,19 @@ class TaskNamespace:
     worker pool.
     """
 
-    def __init__(self, name: str, topic: Topic, retry: Retry | None):
-        # TODO(taskworker) implement default deadlines for tasks
+    def __init__(
+        self,
+        name: str,
+        topic: Topic,
+        retry: Retry | None,
+        expires: int | datetime.timedelta | None = None,
+        processing_deadline_duration: int = DEFAULT_PROCESSING_DEADLINE,
+    ):
         self.name = name
         self.topic = topic
         self.default_retry = retry
+        self.default_expires = expires  # seconds
+        self.default_processing_deadline_duration = processing_deadline_duration  # seconds
         self._registered_tasks: dict[str, Task[Any, Any]] = {}
         self._producer: KafkaProducer | None = None
 
@@ -57,19 +67,22 @@ class TaskNamespace:
         self,
         *,
         name: str,
-        idempotent: bool = False,
         retry: Retry | None = None,
+        expires: int | datetime.timedelta | None = None,
+        processing_deadline_duration: int | datetime.timedelta | None = None,
     ) -> Callable[[Callable[P, R]], Task[P, R]]:
         """register a task, used as a decorator"""
 
         def wrapped(func: Callable[P, R]) -> Task[P, R]:
-            # TODO(taskworker) Implement task deadlines
             task = Task(
                 name=name,
                 func=func,
                 namespace=self,
-                idempotent=idempotent,
                 retry=retry or self.default_retry,
+                expires=expires or self.default_expires,
+                processing_deadline_duration=(
+                    processing_deadline_duration or self.default_processing_deadline_duration
+                ),
             )
             # TODO(taskworker) tasks should be registered into the registry
             # so that we can ensure task names are globally unique
@@ -106,6 +119,9 @@ class TaskRegistry:
 
         return router
 
+    def contains(self, name: str) -> bool:
+        return name in self._namespaces
+
     def get(self, name: str) -> TaskNamespace:
         """Fetch a namespace by name."""
         if name not in self._namespaces:
@@ -116,23 +132,30 @@ class TaskRegistry:
         """Fetch a task by namespace and name."""
         return self.get(namespace).get(task)
 
-    def import_tasks(self) -> None:
-        """Import all the modules listed in settings.TASKWORKER_IMPORTS"""
-        imports = settings.TASKWORKER_IMPORTS
-        for module in imports:
-            __import__(module)
-
-    def create_namespace(self, name: str, retry: Retry | None = None) -> TaskNamespace:
+    def create_namespace(
+        self,
+        name: str,
+        *,
+        retry: Retry | None = None,
+        expires: int | datetime.timedelta | None = None,
+        processing_deadline_duration: int = DEFAULT_PROCESSING_DEADLINE,
+    ) -> TaskNamespace:
         """
         Create a namespaces.
 
-        Namespaces can define default retry policies, deadlines.
-
-        Namespaces are mapped onto topics with a router allowing
+        Namespaces are mapped onto topics through the configured router allowing
         infrastructure to be scaled based on a region's requirements.
+
+        Namespaces can define default behavior for tasks defined within a namespace.
         """
         topic = self._router.route_namespace(name)
-        namespace = TaskNamespace(name=name, topic=topic, retry=retry)
+        namespace = TaskNamespace(
+            name=name,
+            topic=topic,
+            retry=retry,
+            expires=expires,
+            processing_deadline_duration=processing_deadline_duration,
+        )
         self._namespaces[name] = namespace
 
         return namespace
