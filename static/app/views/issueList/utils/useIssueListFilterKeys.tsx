e3873504bad7a3d6fc903776688a7790dc3b84c0
Malachi Willey <malwilley@gmail.com>
2024-11-07 09:22:22 -0800

    fix(issue-views): Display typed filters correctly in new issue view page (#80346)
    Typed filters, such as `firstSeen` (date), weren't rendering correctly
    because we weren't providing the set of filter keys to the
    FormattedQuery. This extracts the hook that calculates these filter keys
    and uses it in both the search bar and the new view page so it's using
    the same set in both places.

---
+++ static/app/views/issueList/utils/useIssueListFilterKeys.tsx
@@ -0,0 +1,23 @@
+import {getUtcDateString} from 'sentry/utils/dates';
+import useOrganization from 'sentry/utils/useOrganization';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import {useFetchIssueTags} from 'sentry/views/issueList/utils/useFetchIssueTags';
+
+export function useIssueListFilterKeys() {
+  const organization = useOrganization();
+  const {selection: pageFilters} = usePageFilters();
+  const {tags: issueTags} = useFetchIssueTags({
+    org: organization,
+    projectIds: pageFilters.projects.map(id => id.toString()),
+    keepPreviousData: true,
+    start: pageFilters.datetime.start
+      ? getUtcDateString(pageFilters.datetime.start)
+      : undefined,
+    end: pageFilters.datetime.end
+      ? getUtcDateString(pageFilters.datetime.end)
+      : undefined,
+    statsPeriod: pageFilters.datetime.period,
+  });
+
+  return issueTags;
+}
