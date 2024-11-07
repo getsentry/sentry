8c31a58f4b24b0e824a2947b07a42f290de6d450
William Mak <william@wmak.io>
2024-11-07 14:47:37 -0500

    feat(rpc): Introduce a function just for table calls (#80395)
    - Instead of doing a map for every class name, add a function call so
    its more explicit which endpoint is being hit and what the usecase is

--- src/sentry/utils/snuba_rpc.py
+++ src/sentry/utils/snuba_rpc.py
@@ -7,14 +7,16 @@ import sentry_protos.snuba.v1alpha.request_common_pb2
 import sentry_sdk
 import sentry_sdk.scope
 from google.protobuf.message import Message as ProtobufMessage
+from sentry_protos.snuba.v1.endpoint_trace_item_table_pb2 import (
+    TraceItemTableRequest,
+    TraceItemTableResponse,
+)
 from sentry_protos.snuba.v1.error_pb2 import Error as ErrorProto
+from urllib3.response import BaseHTTPResponse
 
 from sentry.utils.snuba import SnubaError, _snuba_pool
 
 RPCResponseType = TypeVar("RPCResponseType", bound=ProtobufMessage)
-ENDPOINT_NAMES = {
-    "TraceItemTableRequest": "EndpointTraceItemTable",
-}
 
 # Show the snuba query params and the corresponding sql or errors in the server logs
 SNUBA_INFO_FILE = os.environ.get("SENTRY_SNUBA_INFO_FILE", "")
@@ -46,6 +48,13 @@ class SnubaRPCRequest(Protocol):
         sentry_protos.snuba.v1alpha.request_common_pb2.RequestMeta
         | sentry_protos.snuba.v1.request_common_pb2.RequestMeta
     ): ...
+
+
+def table_rpc(req: TraceItemTableRequest) -> TraceItemTableResponse:
+    resp = _make_rpc_request("EndpointTraceItemTable", "v1", req)
+    response = TraceItemTableResponse()
+    response.ParseFromString(resp.data)
+    return response
 
 
 def rpc(req: SnubaRPCRequest, resp_type: type[RPCResponseType]) -> RPCResponseType:
@@ -80,6 +89,18 @@ def rpc(req: SnubaRPCRequest, resp_type: type[RPCResponseType]) -> RPCResponseTy
     )
     aggregate_resp = snuba.rpc(aggregate_req, AggregateBucketResponse)
     """
+    cls = req.__class__
+    endpoint_name = cls.__name__
+    class_version = cls.__module__.split(".", 3)[2]
+    http_resp = _make_rpc_request(endpoint_name, class_version, req)
+    resp = resp_type()
+    resp.ParseFromString(http_resp.data)
+    return resp
+
+
+def _make_rpc_request(
+    endpoint_name: str, class_version: str, req: SnubaRPCRequest
+) -> BaseHTTPResponse:
     referrer = req.meta.referrer
     if SNUBA_INFO:
         from google.protobuf.json_format import MessageToJson
@@ -87,12 +108,6 @@ def rpc(req: SnubaRPCRequest, resp_type: type[RPCResponseType]) -> RPCResponseTy
         log_snuba_info(f"{referrer}.body:\n{MessageToJson(req)}")  # type: ignore[arg-type]
     with sentry_sdk.start_span(op="snuba_rpc.run", name=req.__class__.__name__) as span:
         span.set_tag("snuba.referrer", referrer)
-
-        cls = req.__class__
-        class_name = cls.__name__
-        endpoint_name = ENDPOINT_NAMES.get(class_name, class_name)
-        class_version = cls.__module__.split(".", 3)[2]
-
         http_resp = _snuba_pool.urlopen(
             "POST",
             f"/rpc/{endpoint_name}/{class_version}",
@@ -107,7 +122,4 @@ def rpc(req: SnubaRPCRequest, resp_type: type[RPCResponseType]) -> RPCResponseTy
             if SNUBA_INFO:
                 log_snuba_info(f"{referrer}.error:\n{error}")
             raise SnubaRPCError(error)
-
-        resp = resp_type()
-        resp.ParseFromString(http_resp.data)
-        return resp
+        return http_resp
