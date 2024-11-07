12f13f04d83c6f2f3b51c29f88d93f0313d7f378
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-11-07 13:54:18 -0500

    Add comment

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -2856,6 +2856,7 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
+# option for sample size when fetching project tag keys
 register(
     "visibility.tag-key-sample-size",
     default=1_000_000,
