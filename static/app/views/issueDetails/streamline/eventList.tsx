7f3e14f0a901704e5006778e48903fa0c93865dc
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2024-11-07 14:27:03 -0800

    feat(issue-details): Sort all events table (#80414)
    Updated version of https://github.com/getsentry/sentry/pull/80398.
    https://github.com/getsentry/sentry/pull/80404 meant that the sort was
    getting wiped if there was no sort query.

--- static/app/views/issueDetails/streamline/eventList.tsx
+++ static/app/views/issueDetails/streamline/eventList.tsx
@@ -44,6 +44,7 @@ export function EventList({group}: EventListProps) {
     group,
     queryProps: {
       fields,
+      orderby: ['-timestamp'],
       widths: fields.map(field => {
         switch (field) {
           case 'id':
@@ -68,9 +69,11 @@ export function EventList({group}: EventListProps) {
     },
   });
 
-  eventView.sorts = decodeSorts(location.query.sort).filter(sort =>
-    fields.includes(sort.field)
-  );
+  if (location.query.sort) {
+    eventView.sorts = decodeSorts(location.query.sort).filter(sort =>
+      fields.includes(sort.field)
+    );
+  }
 
   const grayText = css`
     color: ${theme.subText};
