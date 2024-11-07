4e97eb1224b0f7f94c422d4568d3ea43703dc1c0
Arthur Knaus <arthur.knaus@sentry.io>
2024-11-07 07:22:19 +0100

    fix(dynamic-sampling): Use toggle for mode switch

--- static/app/views/settings/dynamicSampling/organizationSampling.tsx
+++ static/app/views/settings/dynamicSampling/organizationSampling.tsx
@@ -1,15 +1,11 @@
 import {useState} from 'react';
-import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import {addErrorMessage, addSuccessMessage} from 'sentry/actionCreators/indicator';
 import {Button} from 'sentry/components/button';
-import FieldGroup from 'sentry/components/forms/fieldGroup';
-import ExternalLink from 'sentry/components/links/externalLink';
 import Panel from 'sentry/components/panels/panel';
 import PanelBody from 'sentry/components/panels/panelBody';
 import PanelHeader from 'sentry/components/panels/panelHeader';
-import QuestionTooltip from 'sentry/components/questionTooltip';
 import {SegmentedControl} from 'sentry/components/segmentedControl';
 import {Tooltip} from 'sentry/components/tooltip';
 import {t, tct} from 'sentry/locale';
@@ -68,32 +64,6 @@ export function OrganizationSampling() {
         <Panel>
           <PanelHeader>{t('Automatic Sampling')}</PanelHeader>
           <PanelBody>
-            <FieldGroup
-              label={t('Sampling Mode')}
-              help={t('The current configuration mode for dynamic sampling.')}
-            >
-              <div
-                css={css`
-                  display: flex;
-                  align-items: center;
-                  gap: ${space(1)};
-                `}
-              >
-                {t('Automatic')}{' '}
-                <QuestionTooltip
-                  size="sm"
-                  isHoverable
-                  title={tct(
-                    'Automatic mode allows you to set a target sample rate for your organization. Sentry automatically adjusts individual project rates to boost small projects and ensure equal visibility. [link:Learn more]',
-                    {
-                      link: (
-                        <ExternalLink href="https://docs.sentry.io/product/performance/retention-priorities/" />
-                      ),
-                    }
-                  )}
-                />
-              </div>
-            </FieldGroup>
             <SamplingModeField />
             <OrganizationSampleRateField />
           </PanelBody>
