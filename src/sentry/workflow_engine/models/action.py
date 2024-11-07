c2a8f8fbd0b4051b592b959b3fda5b634e189fd2
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-11-06 16:11:07 -0800

    Update the Action Model (#79923)
    ## Description
    Chatted with Colleen about `AlertRuleTriggerActions` => `Actions` and
    found a few fields that are missing in the model, that shouldn't be part
    of the `data` blob. Adding these to the Action table for now.
    
    Given the Notification Platform work, we will probably want to move
    these to a new table as part of that project -- so these are all marked
    as "LEGACY" to denote the fields that I feel like we'll want to
    eventually remove.
    
    [More info the spec](
    https://www.notion.so/Alerts-Create-Issues-Metric-Alert-Migration-1198b10e4b5d80e29cdffc191de502c7?d=12d8b10e4b5d80cc93af001c5d42edac&pvs=4#11c8b10e4b5d80658f82c4f8a231fceb)

--- src/sentry/workflow_engine/models/action.py
+++ src/sentry/workflow_engine/models/action.py
@@ -2,6 +2,8 @@ from django.db import models
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import DefaultFieldsModel, region_silo_model, sane_repr
+from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
+from sentry.models.notificationaction import ActionTarget
 
 
 @region_silo_model
@@ -9,12 +11,15 @@ class Action(DefaultFieldsModel):
     """
     Actions are actions that can be taken if the conditions of a DataConditionGroup are satisfied.
     Examples include: detectors emitting events, sending notifications, creating an issue in the Issue Platform, etc.
+
+    Any fields denoted with LEGACY are fields that will likely be migrated / removed in the future, and should likely
+    live in a separate model related to the notifications.
     """
 
     __relocation_scope__ = RelocationScope.Excluded
     __repr__ = sane_repr("workflow_id", "type")
 
-    # TODO (@saponifi3d): Don't hardcode these values
+    # TODO (@saponifi3d): Don't hardcode these values, and these are incomplete values
     class Type(models.TextChoices):
         Notification = "SendNotificationAction"
         TriggerWorkflow = "TriggerWorkflowAction"
@@ -26,5 +31,21 @@ class Action(DefaultFieldsModel):
     """
     required = models.BooleanField(default=False)
 
+    # The type field is used to denote the type of action we want to trigger
     type = models.TextField(choices=Type.choices)
     data = models.JSONField(default=dict)
+
+    # LEGACY: The integration_id is used to map the integration_id found in the AlertRuleTriggerAction
+    # This allows us to map the way we're saving the notification channels to the action.
+    integration_id = HybridCloudForeignKey(
+        "sentry.Integration", blank=True, null=True, on_delete="CASCADE"
+    )
+
+    # LEGACY: The target_display is used to display the target's name in notifications
+    target_display = models.TextField(null=True)
+
+    # LEGACY: The target_identifier is used to target the user / team / org that notifications are being sent to
+    target_identifier = models.TextField(null=True)
+
+    # LEGACY: This is used to denote if the Notification is going to a user, team, sentry app, etc
+    target_type = models.SmallIntegerField(choices=ActionTarget.as_choices(), null=True)
