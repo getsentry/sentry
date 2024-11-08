34a516224d6ab26d4389d147470bae85c16eb6da
Isabella Enriquez <isabella.enriquez@sentry.io>
2024-11-08 13:57:35 -0800

    chore(api): Publish ProjectEnvironmentDetails (#79603)
    Closes
    https://www.notion.so/sentry/Publish-ProjectEnvironmentDetailsEndpoint-badd944e32b147b1a8fb63b9a6dae54f?pvs=4

--- src/sentry/api/endpoints/project_environment_details.py
+++ src/sentry/api/endpoints/project_environment_details.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from drf_spectacular.utils import extend_schema
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -7,21 +7,46 @@ from sentry.api.base import region_silo_endpoint
 from sentry.api.bases.project import ProjectEndpoint
 from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.serializers import serialize
+from sentry.api.serializers.models.environment import EnvironmentProjectSerializer
+from sentry.api.serializers.rest_framework.environment import EnvironmentSerializer
+from sentry.apidocs.constants import (
+    RESPONSE_BAD_REQUEST,
+    RESPONSE_FORBIDDEN,
+    RESPONSE_NOT_FOUND,
+    RESPONSE_UNAUTHORIZED,
+)
+from sentry.apidocs.examples.environment_examples import EnvironmentExamples
+from sentry.apidocs.parameters import EnvironmentParams, GlobalParams
 from sentry.models.environment import Environment, EnvironmentProject
 
 
-class ProjectEnvironmentSerializer(serializers.Serializer):
-    isHidden = serializers.BooleanField()
-
-
+@extend_schema(tags=["Environments"])
 @region_silo_endpoint
 class ProjectEnvironmentDetailsEndpoint(ProjectEndpoint):
     publish_status = {
-        "GET": ApiPublishStatus.UNKNOWN,
-        "PUT": ApiPublishStatus.UNKNOWN,
+        "GET": ApiPublishStatus.PUBLIC,
+        "PUT": ApiPublishStatus.PUBLIC,
     }
 
+    @extend_schema(
+        operation_id="Retrieve a Project Environment",
+        parameters=[
+            GlobalParams.ORG_ID_OR_SLUG,
+            GlobalParams.PROJECT_ID_OR_SLUG,
+            EnvironmentParams.ENVIRONMENT,
+        ],
+        responses={
+            200: EnvironmentProjectSerializer,
+            401: RESPONSE_UNAUTHORIZED,
+            403: RESPONSE_FORBIDDEN,
+            404: RESPONSE_NOT_FOUND,
+        },
+        examples=EnvironmentExamples.RETRIEVE_PROJECT_ENVIRONMENT,
+    )
     def get(self, request: Request, project, environment) -> Response:
+        """
+        Return details on a project environment.
+        """
         try:
             instance = EnvironmentProject.objects.select_related("environment").get(
                 project=project,
@@ -32,7 +57,27 @@ class ProjectEnvironmentDetailsEndpoint(ProjectEndpoint):
 
         return Response(serialize(instance, request.user))
 
+    @extend_schema(
+        operation_id="Update a Project Environment",
+        parameters=[
+            GlobalParams.ORG_ID_OR_SLUG,
+            GlobalParams.PROJECT_ID_OR_SLUG,
+            EnvironmentParams.ENVIRONMENT,
+        ],
+        request=EnvironmentSerializer,
+        responses={
+            200: EnvironmentProjectSerializer,
+            400: RESPONSE_BAD_REQUEST,
+            401: RESPONSE_UNAUTHORIZED,
+            403: RESPONSE_FORBIDDEN,
+            404: RESPONSE_NOT_FOUND,
+        },
+        examples=EnvironmentExamples.RETRIEVE_PROJECT_ENVIRONMENT,
+    )
     def put(self, request: Request, project, environment) -> Response:
+        """
+        Update the visibility for a project environment.
+        """
         try:
             instance = EnvironmentProject.objects.select_related("environment").get(
                 project=project,
@@ -41,7 +86,7 @@ class ProjectEnvironmentDetailsEndpoint(ProjectEndpoint):
         except EnvironmentProject.DoesNotExist:
             raise ResourceDoesNotExist
 
-        serializer = ProjectEnvironmentSerializer(data=request.data, partial=True)
+        serializer = EnvironmentSerializer(data=request.data, partial=True)
         if not serializer.is_valid():
             return Response(serializer.errors, status=400)
 
