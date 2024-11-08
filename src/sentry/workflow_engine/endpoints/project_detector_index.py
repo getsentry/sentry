01312455f8febd5edbb644bb274b113b03c3181b
Dan Fuller <dfuller@sentry.io>
2024-11-08 13:42:10 -0800

    remove junk file

--- src/sentry/workflow_engine/endpoints/project_detector_index.py
+++
@@ -1,9 +0,0 @@
-# def validate_group_type(self, value: str) -> GroupType:
-#     detector_type = grouptype.registry.get_by_slug(value)
-#     if detector_type is None:
-#         raise serializers.ValidationError("Unknown group type")
-#     if detector_type.detector_validator is None:
-#         raise serializers.ValidationError("Group type not compatible with detectors")
-#     # TODO: Probably need to check a feature flag to decide if a given
-#     # org/user is allowed to add a detector
-#     return detector_type
