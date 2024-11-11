192a0a5b1b85aeb694cbedc0533ce811e0c5fbc7
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-11-11 14:21:56 -0800

    fix(replay): handle time units for duration search from the new search bar (#80036)
    Closes https://github.com/getsentry/sentry/issues/79870
    
    After the new search bar was GA'd, including a unit (s, m(in), h(r)) is
    now the norm for `duration`. This PR changes our config for
    parse_search_filters (a shared util) to handle them properly. Values
    without a unit are now interpreted as ms.
    
    The current solution for sub-second precisions is to fail and return an
    error msg to the user. Due to the return type of parse_duration we have
    to use ms for our Snuba queries now.
    
    This still does't handle "in" searches like `duration in [16s, 17s]`,
    but I suggest merging this first and following up.

--- src/sentry/replays/lib/new_query/parsers.py
+++ src/sentry/replays/lib/new_query/parsers.py
@@ -22,6 +22,21 @@ def parse_int(value: str) -> int:
     return int(parse_float(value))
 
 
+def parse_duration(value: str) -> int:
+    """
+    Assert that second resolution is given. The input and output of this fx is still in milliseconds, to match the
+    output of api.event_search.parse_search_query
+    """
+    milliseconds = parse_int(value)
+    if milliseconds % 1000:
+        # TODO: remove once we support milliseconds.
+        # TODO: this error isn't actually returned to the frontend, it's caught and then we raise a ParseError
+        raise CouldNotParseValue(
+            f"Replays only supports second-resolution timestamps at this time. Try '{milliseconds // 1000}s' instead."
+        )
+    return milliseconds
+
+
 def parse_str(value: str) -> str:
     """Coerce to str or fail."""
     return value
