ef2c40f7633e831a3d7dbf8b0caa7f7adcdc4cdd
Arthur Knaus <arthur.knaus@sentry.io>
2024-11-12 15:26:04 +0100

    feat(dynamic-sampling): Org-assist logic

---
+++ static/app/views/settings/dynamicSampling/utils/scaleSampleRates.tsx
@@ -0,0 +1,75 @@
+interface ScalingItem {
+  count: number;
+  sampleRate: number;
+}
+
+/**
+ * Scales the sample rates of items proportionally to their current sample rate.
+ */
+export function scaleSampleRates<T extends ScalingItem>({
+  items,
+  sampleRate,
+}: {
+  items: T[];
+  sampleRate: number;
+}): {
+  scaledItems: T[];
+} {
+  const totalSpans = items.reduce((acc, item) => acc + item.count, 0);
+  const oldSampleRate = items.reduce(
+    (acc, item) => acc + item.sampleRate * (item.count / totalSpans),
+    0
+  );
+
+  if (sampleRate === oldSampleRate) {
+    return {scaledItems: items};
+  }
+
+  if (
+    oldSampleRate === 0 ||
+    oldSampleRate === 1 ||
+    sampleRate === 0 ||
+    sampleRate === 1
+  ) {
+    return {
+      scaledItems: items.map(item => ({
+        ...item,
+        sampleRate,
+      })),
+    };
+  }
+
+  const newSampled = totalSpans * sampleRate;
+
+  let factor = sampleRate / oldSampleRate;
+  let remainingTotal = totalSpans;
+  let remainingSampled = newSampled;
+  let remainingOldSampled = totalSpans * oldSampleRate;
+
+  const sortedItems = items.toSorted((a, b) => a.count - b.count);
+  const remainingProjects = sortedItems.toReversed();
+
+  const scaledItems: T[] = [];
+  for (const item of sortedItems) {
+    remainingProjects.pop();
+    const newProjectRate = Math.min(1, Math.max(0, item.sampleRate * factor));
+    const newProjectSampled = item.count * newProjectRate;
+
+    remainingTotal -= item.count;
+    remainingSampled -= newProjectSampled;
+    remainingOldSampled -= item.count * item.sampleRate;
+
+    const newTargetRate = remainingSampled / remainingTotal;
+
+    const remainingTotalRef = remainingTotal;
+    const remainingOldSampleRate = remainingOldSampled / remainingTotalRef;
+
+    factor = newTargetRate / remainingOldSampleRate;
+
+    scaledItems.push({
+      ...item,
+      sampleRate: newProjectRate,
+    });
+  }
+  return {scaledItems};
+}
