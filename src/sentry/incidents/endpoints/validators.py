73c166e847020a7e95584f395ab388c520b3aa0c
Dan Fuller <dfuller@sentry.io>
2024-11-12 15:23:24 -0800

    feat(workflow_engine): Implement validator for Data sources (#80511)
    This adds data source validators to the workflow engine. This allows us
    to fully customize validation for each group type.
    
    <!-- Describe your PR here. -->

--- src/sentry/incidents/endpoints/validators.py
+++ src/sentry/incidents/endpoints/validators.py
@@ -1,12 +1,76 @@
+from collections.abc import Sequence
+from datetime import timedelta
+
 from rest_framework import serializers
 
+from sentry.api.serializers.rest_framework import EnvironmentField
+from sentry.incidents.utils.constants import INCIDENTS_SNUBA_SUBSCRIPTION_TYPE
+from sentry.snuba.dataset import Dataset
+from sentry.snuba.models import QuerySubscription, SnubaQuery, SnubaQueryEventType
+from sentry.snuba.subscriptions import create_snuba_query, create_snuba_subscription
 from sentry.workflow_engine.endpoints.validators import (
     BaseDataSourceValidator,
     BaseGroupTypeDetectorValidator,
     NumericComparisonConditionValidator,
 )
+from sentry.workflow_engine.models import DataSource
 from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.types import DetectorPriorityLevel
+
+
+class SnubaQueryDataSourceValidator(BaseDataSourceValidator[QuerySubscription]):
+    query_type = serializers.IntegerField(required=True)
+    dataset = serializers.CharField(required=True)
+    query = serializers.CharField(required=True)
+    aggregate = serializers.CharField(required=True)
+    time_window = serializers.IntegerField(required=True)
+    environment = EnvironmentField(required=True, allow_null=True)
+    event_types = serializers.ListField(
+        child=serializers.IntegerField(),
+    )
+
+    data_source_type = DataSource.Type.SNUBA_QUERY_SUBSCRIPTION
+
+    def validate_query_type(self, value: int) -> SnubaQuery.Type:
+        try:
+            return SnubaQuery.Type(value)
+        except ValueError:
+            raise serializers.ValidationError(f"Invalid query type {value}")
+
+    def validate_dataset(self, value: str) -> Dataset:
+        try:
+            return Dataset(value)
+        except ValueError:
+            raise serializers.ValidationError(
+                f"Invalid dataset {value}. Must be one of: {', '.join(Dataset.__members__)}"
+            )
+
+    def validate_event_types(self, value: Sequence[int]) -> list[SnubaQueryEventType.EventType]:
+        try:
+            return [SnubaQueryEventType.EventType(t) for t in value]
+        except ValueError:
+            raise serializers.ValidationError(f"Invalid event type: {value}")
+
+    def validate_time_window(self, value: int) -> timedelta:
+        return timedelta(minutes=value)
+
+    def create_source(self, validated_data) -> QuerySubscription:
+        snuba_query = create_snuba_query(
+            query_type=validated_data["query_type"],
+            dataset=validated_data["dataset"],
+            query=validated_data["query"],
+            aggregate=validated_data["aggregate"],
+            time_window=validated_data["time_window"],
+            # TODO: Feed the usual metric alerts logic in here based on time window
+            resolution=timedelta(minutes=1),
+            environment=validated_data["environment"],
+            event_types=validated_data["event_types"],
+        )
+        return create_snuba_subscription(
+            project=self.context["project"],
+            subscription_type=INCIDENTS_SNUBA_SUBSCRIPTION_TYPE,
+            snuba_query=snuba_query,
+        )
 
 
 class MetricAlertComparisonConditionValidator(NumericComparisonConditionValidator):
@@ -25,7 +89,7 @@ class MetricAlertComparisonConditionValidator(NumericComparisonConditionValidato
 
 
 class MetricAlertsDetectorValidator(BaseGroupTypeDetectorValidator):
-    data_source = BaseDataSourceValidator()
+    data_source = SnubaQueryDataSourceValidator(required=True)
     data_conditions = MetricAlertComparisonConditionValidator(many=True)
 
     def validate(self, attrs):
