797572e5c10cea73ef4297dc5bb01223d45b6bc5
Scott Cooper <scttcper@gmail.com>
2024-11-12 10:54:36 -0800

    feat(issues): Hide event details headers for cron issues (#80568)

--- static/app/utils/issueTypeConfig/index.tsx
+++ static/app/utils/issueTypeConfig/index.tsx
@@ -39,6 +39,7 @@ const BASE_CONFIG: IssueTypeConfig = {
   aiSuggestedSolution: false,
   events: {enabled: true},
   mergedIssues: {enabled: false},
+  filterAndSearchHeader: {enabled: true},
   performanceDurationRegression: {enabled: false},
   profilingDurationRegression: {enabled: false},
   regression: {enabled: false},
@@ -109,7 +110,7 @@ export const getIssueCategoryAndTypeFromOccurrenceType = (
 export const getConfigForIssueType = (
   params: GetConfigForIssueTypeParams,
   project: Project
-) => {
+): IssueTypeConfig => {
   const {issueCategory, issueType, title} =
     'eventOccurrenceType' in params
       ? getIssueCategoryAndTypeFromOccurrenceType(params.eventOccurrenceType as number)
