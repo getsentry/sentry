ee7a34aa67c371e24ec47d6d6b4851e7660d2d8f
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-11-13 12:56:20 -0800

    fix(flags): fix sort analytic (#80685)
    new flag analytic since we changed how sorting works.
    
    options for `sortMethod` are now
    - `eval`
    - `alphabetical`
    - `newest`
    - `oldest`
    - `a-z`
    - `z-a`
    
    
    <img width="220" alt="SCR-20241113-limo"
    src="https://github.com/user-attachments/assets/453e196b-420c-4f61-8e01-dba50f4c7ec5">

--- static/app/utils/analytics/featureFlagAnalyticsEvents.tsx
+++ static/app/utils/analytics/featureFlagAnalyticsEvents.tsx
@@ -5,7 +5,7 @@ export type FeatureFlagEventParameters = {
     numTotalFlags: number;
   };
   'flags.setup_modal_opened': {};
-  'flags.sort-flags': {sortMethod: string};
+  'flags.sort_flags': {sortMethod: string};
   'flags.table_rendered': {
     numFlags: number;
   };
@@ -17,7 +17,7 @@ export type FeatureFlagEventKey = keyof FeatureFlagEventParameters;
 
 export const featureFlagEventMap: Record<FeatureFlagEventKey, string | null> = {
   'flags.view-all-clicked': 'Clicked View All Flags',
-  'flags.sort-flags': 'Sorted Flags',
+  'flags.sort_flags': 'Sorted Flags',
   'flags.event_and_suspect_flags_found': 'Number of Event and Suspect Flags',
   'flags.setup_modal_opened': 'Flag Setup Integration Modal Opened',
   'flags.webhook_url_generated': 'Flag Webhook URL Generated in Setup Integration Modal',
