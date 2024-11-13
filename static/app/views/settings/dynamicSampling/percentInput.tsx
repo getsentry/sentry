53ee776e06666539a3fc361ca3624273def69880
ArthurKnaus <arthur.knaus@sentry.io>
2024-11-13 12:00:50 +0100

    feat(dynamic-sampling): Org assist UX (#80651)
    Render the org rate input as disabled initially.
    Add a `edit` button with a tooltip to explain the functionality.
    
    Closes https://github.com/getsentry/projects/issues/191

--- static/app/views/settings/dynamicSampling/percentInput.tsx
+++ static/app/views/settings/dynamicSampling/percentInput.tsx
@@ -1,4 +1,5 @@
 import type React from 'react';
+import {forwardRef} from 'react';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
@@ -7,20 +8,22 @@ import {space} from 'sentry/styles/space';
 
 interface Props extends React.ComponentProps<typeof InputGroup.Input> {}
 
-export function PercentInput(props: Props) {
-  return (
-    <InputGroup
-      css={css`
-        width: 160px;
-      `}
-    >
-      <InputGroup.Input type="number" min={0} max={100} {...props} />
-      <InputGroup.TrailingItems>
-        <TrailingPercent>%</TrailingPercent>
-      </InputGroup.TrailingItems>
-    </InputGroup>
-  );
-}
+export const PercentInput = forwardRef<HTMLInputElement, Props>(
+  function PercentInput(props, ref) {
+    return (
+      <InputGroup
+        css={css`
+          width: 160px;
+        `}
+      >
+        <InputGroup.Input ref={ref} type="number" min={0} max={100} {...props} />
+        <InputGroup.TrailingItems>
+          <TrailingPercent>%</TrailingPercent>
+        </InputGroup.TrailingItems>
+      </InputGroup>
+    );
+  }
+);
 
 const TrailingPercent = styled('strong')`
   padding: 0 ${space(0.25)};
