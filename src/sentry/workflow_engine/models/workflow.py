61456628c31ade537d226048d98c1ecbfe93f395
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-11-13 14:27:53 -0800

    Add the ownermodel to the workflow, add 'enabled' flag for snooze

--- src/sentry/workflow_engine/models/workflow.py
+++ src/sentry/workflow_engine/models/workflow.py
@@ -2,12 +2,11 @@ from django.db import models
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import DefaultFieldsModel, FlexibleForeignKey, region_silo_model, sane_repr
-
-from .data_condition_group import DataConditionGroup
+from sentry.models.owner_base import OwnerModel
 
 
 @region_silo_model
-class Workflow(DefaultFieldsModel):
+class Workflow(DefaultFieldsModel, OwnerModel):
     """
     A workflow is a way to execute actions in a specified order.
     Workflows are initiated after detectors have been processed, driven by changes to their state.
@@ -17,8 +16,13 @@ class Workflow(DefaultFieldsModel):
     name = models.CharField(max_length=200)
     organization = FlexibleForeignKey("sentry.Organization")
 
+    # If the workflow is not enabled, it will not be evaluated / invoke actions. This is how we "snooze" a workflow
+    enabled = models.BooleanField(default=True)
+
     # Required as the 'when' condition for the workflow, this evalutes states emitted from the detectors
-    when_condition_group = FlexibleForeignKey(DataConditionGroup, blank=True, null=True)
+    when_condition_group = FlexibleForeignKey(
+        "workflow_engine.DataConditionGroup", blank=True, null=True
+    )
 
     __repr__ = sane_repr("name", "organization_id")
 
