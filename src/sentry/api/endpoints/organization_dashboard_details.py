8bb5f84829ec51e34347752357d0c2582cd7a7af
harshithadurai <harshi.durai@esentry.io>
2024-11-13 15:02:51 -0500

    modify dashboard perm class

--- src/sentry/api/endpoints/organization_dashboard_details.py
+++ src/sentry/api/endpoints/organization_dashboard_details.py
@@ -44,19 +44,10 @@ class DashboardPermissions(BasePermission):
         "DELETE": ["org:read", "org:write", "org:admin"],
     }
 
-    def has_object_permission(self, request: Request, view, obj):
-        if isinstance(obj, Dashboard) and features.has(
-            "organizations:dashboards-edit-access", obj.organization, actor=request.user
-        ):
-            # Check if user has permissions to edit dashboard
-            if hasattr(obj, "permissions"):
-                return obj.permissions.has_edit_permissions(request.user.id)
-        return True
-
 
 class OrganizationDashboardBase(OrganizationEndpoint):
     owner = ApiOwner.PERFORMANCE
-    permission_classes = (OrganizationDashboardsPermission, DashboardPermissions)
+    permission_classes = (OrganizationDashboardsPermission,)
 
     def convert_args(
         self, request: Request, organization_id_or_slug, dashboard_id, *args, **kwargs
