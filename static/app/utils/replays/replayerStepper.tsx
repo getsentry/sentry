getsentry/sentry

commit 37f9785a90c834f9c2e3631ff2d8e7cbffbafd4c

feat(replay): Skip media interaction frames for replay stepper (#80681)
When generating page HTML using replay data, we step through the entire
replay as quickly as possible and take a snapshot of the DOM at specific
timestamps. This was causing issues with media elements as the replayer
will try to play them. Skip these frames as these interactions are not
important when trying to extract page html.

Fixes JAVASCRIPT-2NXM

static/app/utils/replays/replayerStepper.tsx
@@ -1,4 +1,4 @@
-import type {Replayer} from '@sentry-internal/rrweb';
+import {EventType, IncrementalSource, type Replayer} from '@sentry-internal/rrweb';
 
 import type {RecordingFrame, ReplayFrame} from 'sentry/utils/replays/types';
 
@@ -38,7 +38,19 @@ export default function replayerStepper<
       return;
     }
 
-    const {replayer, cleanupReplayer} = createHiddenPlayer(rrwebEvents);
+    // Skip media interaction events as they are unnecessary to the
+    // stepper. Prevents errors with `play()`
+    // (https://developer.chrome.com/blog/play-request-was-interrupted)
+    // as well.
+    const rrwebEventsWithoutMediaInteractions = rrwebEvents.filter(
+      ({type, data}) =>
+        type === EventType.IncrementalSnapshot &&
+        data.source !== IncrementalSource.MediaInteraction
+    );
+
+    const {replayer, cleanupReplayer} = createHiddenPlayer(
+      rrwebEventsWithoutMediaInteractions
+    );
 
     const nextFrame = (function () {
       let i = 0;

commit 7e919c333d78bd106f274bd7fce4d5dd00344025

fix(replay): Fix replay playback showing duped elements...
... and elements that do not disappear. Fixes regression introduced in https://github.com/getsentry/sentry/pull/80681

The filter statement is incorrect, but beyond that, there is a larger problem in that the replayStepper is somehow affecting the replayer playback instance.

static/app/utils/replays/replayerStepper.tsx
@@ -44,7 +44,7 @@ export default function replayerStepper<
     // as well.
     const rrwebEventsWithoutMediaInteractions = rrwebEvents.filter(
       ({type, data}) =>
-        type === EventType.IncrementalSnapshot &&
+        type !== EventType.IncrementalSnapshot ||
         data.source !== IncrementalSource.MediaInteraction
     );
 