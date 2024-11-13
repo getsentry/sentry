4a43dfab47846c4b5bd108831aa0e68003f244a6
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-11-13 13:02:17 -0800

    chore(flags): rm webhook analytic (#80686)
    we won't be generating webhook URLs anymore.

--- static/app/utils/analytics/featureFlagAnalyticsEvents.tsx
+++ static/app/utils/analytics/featureFlagAnalyticsEvents.tsx
@@ -10,7 +10,6 @@ export type FeatureFlagEventParameters = {
     numFlags: number;
   };
   'flags.view-all-clicked': {};
-  'flags.webhook_url_generated': {};
 };
 
 export type FeatureFlagEventKey = keyof FeatureFlagEventParameters;
@@ -20,6 +19,5 @@ export const featureFlagEventMap: Record<FeatureFlagEventKey, string | null> = {
   'flags.sort_flags': 'Sorted Flags',
   'flags.event_and_suspect_flags_found': 'Number of Event and Suspect Flags',
   'flags.setup_modal_opened': 'Flag Setup Integration Modal Opened',
-  'flags.webhook_url_generated': 'Flag Webhook URL Generated in Setup Integration Modal',
   'flags.table_rendered': 'Flag Table Rendered',
 };
