a33b0b39ca44b146228917539d1838550e1cfbe6
Gabe Villalobos <GabeVillalobos@users.noreply.github.com>
2024-11-14 12:43:32 -0800

    ref(ecosystem): Allows strings for integration SLO failure and halt metrics (#80756)

--- tests/sentry/integrations/github/test_integration.py
+++ tests/sentry/integrations/github/test_integration.py
@@ -116,7 +116,7 @@ class GitHubIntegrationTest(IntegrationTestCase):
         (event_failures,) = (
             call for call in mock_record.mock_calls if call.args[0] == EventLifecycleOutcome.FAILURE
         )
-        assert event_failures.args[1]["failure_reason"] == error_msg
+        assert event_failures.args[1] == error_msg
 
     @pytest.fixture(autouse=True)
     def stub_get_jwt(self):
