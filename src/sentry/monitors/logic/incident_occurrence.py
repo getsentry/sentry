c799b15eaf2dbb94c32645417ea0bc5ca5193e05
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-11-14 14:38:04 -0500

    ref(crons): Move resolution logic to incidents module (#80748)
    We already moved the incident creation logic into the incidents and
    incident_occurrence modules, move the recovery logic into these modules
    for consistency as well.

--- src/sentry/monitors/logic/incident_occurrence.py
+++ src/sentry/monitors/logic/incident_occurrence.py
@@ -11,6 +11,10 @@ from django.utils.text import get_text_list
 from django.utils.translation import gettext_lazy as _
 
 from sentry.issues.grouptype import MonitorIncidentType
+from sentry.issues.issue_occurrence import IssueEvidence, IssueOccurrence
+from sentry.issues.producer import PayloadType, produce_occurrence_to_kafka
+from sentry.issues.status_change_message import StatusChangeMessage
+from sentry.models.group import GroupStatus
 from sentry.monitors.models import (
     CheckInStatus,
     MonitorCheckIn,
@@ -31,9 +35,6 @@ def create_incident_occurrence(
     incident: MonitorIncident,
     received: datetime | None,
 ) -> None:
-    from sentry.issues.issue_occurrence import IssueEvidence, IssueOccurrence
-    from sentry.issues.producer import PayloadType, produce_occurrence_to_kafka
-
     monitor_env = failed_checkin.monitor_environment
 
     if monitor_env is None:
@@ -169,3 +170,16 @@ def get_monitor_environment_context(monitor_environment: MonitorEnvironment):
         "status": monitor_environment.get_status_display(),
         "type": monitor_environment.monitor.get_type_display(),
     }
+
+
+def resolve_incident_group(incident: MonitorIncident, project_id: int):
+    status_change = StatusChangeMessage(
+        fingerprint=[incident.grouphash],
+        project_id=project_id,
+        new_status=GroupStatus.RESOLVED,
+        new_substatus=None,
+    )
+    produce_occurrence_to_kafka(
+        payload_type=PayloadType.STATUS_CHANGE,
+        status_change=status_change,
+    )
