3a67dbe194d42f3eeec1d87ea5d43359c474f3de
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-11-15 16:06:08 -0500

    feat(crons): Respect tick decisions in incident occurrence consumer (#80845)
    This adds the logic to delay dispatching an incident occurrence based on
    if we're in an abnormal or recovering state.
    
    Part of GH-79328

--- src/sentry/monitors/consumers/incident_occurrences_consumer.py
+++ src/sentry/monitors/consumers/incident_occurrences_consumer.py
@@ -6,22 +6,36 @@ from datetime import UTC, datetime
 from typing import TypeGuard
 
 from arroyo.backends.kafka.consumer import KafkaPayload
+from arroyo.processing.strategies import MessageRejected
 from arroyo.processing.strategies.abstract import ProcessingStrategy, ProcessingStrategyFactory
 from arroyo.processing.strategies.commit import CommitOffsets
 from arroyo.processing.strategies.run_task import RunTask
 from arroyo.types import BrokerValue, Commit, FilteredPayload, Message, Partition
+from cachetools.func import ttl_cache
 from sentry_kafka_schemas.codecs import Codec
 from sentry_kafka_schemas.schema_types.monitors_incident_occurrences_v1 import IncidentOccurrence
 
+from sentry import options
 from sentry.conf.types.kafka_definition import Topic, get_topic_codec
 from sentry.monitors.logic.incident_occurrence import send_incident_occurrence
 from sentry.monitors.models import MonitorCheckIn, MonitorIncident
+from sentry.monitors.system_incidents import TickAnomalyDecision, get_clock_tick_decision
 
 logger = logging.getLogger(__name__)
 
 MONITORS_INCIDENT_OCCURRENCES: Codec[IncidentOccurrence] = get_topic_codec(
     Topic.MONITORS_INCIDENT_OCCURRENCES
 )
+
+
+@ttl_cache(ttl=5)
+def memoized_tick_decision(tick: datetime) -> TickAnomalyDecision | None:
+    """
+    Memoized version of get_clock_tick_decision. Used in
+    process_incident_occurrence to avoid stampeding calls when waiting for a
+    tick decision to resolve.
+    """
+    return get_clock_tick_decision(tick)
 
 
 def process_incident_occurrence(message: Message[KafkaPayload | FilteredPayload]):
@@ -33,6 +47,22 @@ def process_incident_occurrence(message: Message[KafkaPayload | FilteredPayload]
     assert isinstance(message.value, BrokerValue)
 
     wrapper: IncidentOccurrence = MONITORS_INCIDENT_OCCURRENCES.decode(message.payload.value)
+    clock_tick = datetime.fromtimestamp(wrapper["clock_tick_ts"], UTC)
+
+    # May be used as a killswitch if system incident decisions become incorrect
+    # for any reason
+    use_decision = options.get("crons.system_incidents.use_decisions")
+    tick_decision = memoized_tick_decision(clock_tick)
+
+    if use_decision and tick_decision and tick_decision.is_pending():
+        # The decision is pending resolution. We need to stop consuming until
+        # the tick decision is resolved so we can know if it's OK to dispatch the
+        # incident occurrence, or if we should drop the occurrence and mark the
+        # associated check-ins as UNKNOWN due to a system incident.
+
+        # XXX(epurkhiser): MessageRejected tells arroyo that we can't process
+        # this message right now and it should try again
+        raise MessageRejected()
 
     try:
         incident = MonitorIncident.objects.get(id=int(wrapper["incident_id"]))
@@ -56,6 +86,15 @@ def process_incident_occurrence(message: Message[KafkaPayload | FilteredPayload]
         return
 
     received = datetime.fromtimestamp(wrapper["received_ts"], UTC)
+
+    if use_decision and tick_decision and tick_decision.is_incident():
+        # TODO(epurkhiser): We are in a system incident. Mark the check-in
+        # which triggerd this incident occurrence as unknown
+        #
+        # We may need some additional logic do determine if one of the
+        # previous_checkins was part of the incident to decide if we can mark that
+        # as unknown.
+        pass
 
     send_incident_occurrence(failed_checkin, previous_checkins, incident, received)
 
