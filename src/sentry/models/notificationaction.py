3d8069504411ca848027665ce5c3d4ffbe5d7e9f
Christinarlong <christina.long@sentry.io>
2024-11-15 09:18:00 -0800

    shim

---
+++ src/sentry/models/notificationaction.py
@@ -0,0 +1,17 @@
+from sentry.notifications.models.notificationaction import (
+    ActionService,
+    ActionTarget,
+    ActionTrigger,
+    NotificationAction,
+    NotificationActionInputData,
+    NotificationActionProject,
+)
+
+__all__ = (
+    "NotificationActionProject",
+    "NotificationAction",
+    "NotificationActionInputData",
+    "ActionService",
+    "ActionTrigger",
+    "ActionTarget",
+)
