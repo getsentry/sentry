86773638208b39b6e9a7a90fa6ef87e33f6ee86e
Christinarlong <christina.long@sentry.io>
2024-11-15 15:40:27 -0800

    add barrel file for django discovery

---
+++ src/sentry/notifications/models/__init__.py
@@ -0,0 +1,4 @@
+from sentry.notifications.models.notificationmessage import NotificationMessage
+from sentry.notifications.models.notificationsettingbase import NotificationSettingBase
+
+__all__ = ("NotificationSettingBase", "NotificationMessage")
