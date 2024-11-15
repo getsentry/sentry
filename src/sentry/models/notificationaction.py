6cc79a8172d3ecd9fc00e1dc50a9a2f87d44c3ba
Christinarlong <christina.long@sentry.io>
2024-11-15 09:36:01 -0800

    shim forgot to add another class

--- src/sentry/models/notificationaction.py
+++ src/sentry/models/notificationaction.py
@@ -1,4 +1,5 @@
 from sentry.notifications.models.notificationaction import (
+    ActionRegistration,
     ActionService,
     ActionTarget,
     ActionTrigger,
@@ -12,4 +13,5 @@ __all__ = (
     "ActionService",
     "ActionTrigger",
     "ActionTarget",
+    "ActionRegistration",
 )
