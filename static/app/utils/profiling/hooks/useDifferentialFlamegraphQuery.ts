07c88ec1a55597a0da67ac346fe00e2712fb548e
Tony Xiao <txiao@sentry.io>
2024-11-15 10:25:34 -0500

    chore(flamegraph): Remove transactions from differential flamegraphs (#80807)
    Due to continuous profiling, we can no longer associate functions to
    transactions. Only transactions to functions. This removes any reference
    to transactions in differential flamegraphs so we are only working with
    functions.

--- static/app/utils/profiling/hooks/useDifferentialFlamegraphQuery.ts
+++ static/app/utils/profiling/hooks/useDifferentialFlamegraphQuery.ts
@@ -2,7 +2,6 @@ import {useMemo} from 'react';
 
 import {RELATIVE_DAYS_WINDOW} from 'sentry/components/events/eventStatisticalDetector/consts';
 import {useRelativeDateTime} from 'sentry/utils/profiling/hooks/useRelativeDateTime';
-import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 
 import type {AggregateFlamegraphQueryParameters} from './useAggregateFlamegraphQuery';
 import {useAggregateFlamegraphQuery} from './useAggregateFlamegraphQuery';
@@ -12,7 +11,6 @@ interface DifferentialFlamegraphQueryParameters {
   environments: AggregateFlamegraphQueryParameters['environments'];
   fingerprint: string | undefined;
   projectID: number | null;
-  transaction: string;
 }
 
 export interface DifferentialFlamegraphQueryResult {
@@ -23,32 +21,17 @@ export interface DifferentialFlamegraphQueryResult {
 export function useDifferentialFlamegraphQuery(
   params: DifferentialFlamegraphQueryParameters
 ): DifferentialFlamegraphQueryResult {
-  const query = useMemo(() => {
-    // TODO: this should contain the user query
-    // wait util we fully switch over to the transactions dataset
-    const conditions = new MutableSearch('');
-    conditions.setFilterValues('transaction', [params.transaction]);
-    return conditions.formatString();
-  }, [params.transaction]);
-
   const sharedAggregateQueryParams: AggregateFlamegraphQueryParameters = useMemo(() => {
     const p: Exclude<AggregateFlamegraphQueryParameters, 'datetime'> = {
-      query,
+      query: '',
       environments: params.environments,
       fingerprint: params.fingerprint,
       projects:
         params.projectID === null || isNaN(params.projectID) ? [] : [params.projectID],
-      enabled: !!params.transaction,
     };
 
     return p;
-  }, [
-    query,
-    params.environments,
-    params.projectID,
-    params.fingerprint,
-    params.transaction,
-  ]);
+  }, [params.environments, params.projectID, params.fingerprint]);
 
   const regressionDateRange = useRelativeDateTime({
     anchor: params.breakpoint,
