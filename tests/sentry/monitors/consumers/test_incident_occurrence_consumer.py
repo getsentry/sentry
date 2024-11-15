ea09d01b41a1aa150af9df5bfa84922759e68717
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-11-15 12:18:49 -0500

    feat(crons): Implement simple incident occurence consumer (#80773)
    Simply deserializes the IncidentOccurrence messages and dispatches the
    incident occurrence.
    
    Does not yet determine if the clock tick should cause the dispatch to be
    delayed.

--- tests/sentry/monitors/consumers/test_incident_occurrence_consumer.py
+++ tests/sentry/monitors/consumers/test_incident_occurrence_consumer.py
@@ -1,3 +1,4 @@
+import uuid
 from datetime import datetime
 from unittest import mock
 
@@ -11,6 +12,14 @@ from sentry.monitors.consumers.incident_occurrences_consumer import (
     MONITORS_INCIDENT_OCCURRENCES,
     MonitorIncidentOccurenceStrategyFactory,
 )
+from sentry.monitors.models import (
+    CheckInStatus,
+    MonitorCheckIn,
+    MonitorEnvironment,
+    MonitorIncident,
+    MonitorStatus,
+)
+from sentry.testutils.cases import TestCase
 
 partition = Partition(Topic("test"), 0)
 
@@ -35,19 +44,128 @@ def sned_incident_occurrence(
     consumer.submit(Message(value))
 
 
-def test_simple():
-    # XXX(epurkhiser): Doesn't really test anything yet
-    ts = timezone.now().replace(second=0, microsecond=0)
-
-    consumer = create_consumer()
-    sned_incident_occurrence(
-        consumer,
-        ts,
-        {
-            "clock_tick_ts": 1617895645,
-            "received_ts": 1617895650,
-            "failed_checkin_id": 123456,
-            "incident_id": 987654,
-            "previous_checkin_ids": [111222, 333444, 55666],
-        },
+class MonitorsIncidentOccurrenceConsumerTestCase(TestCase):
+    @mock.patch(
+        "sentry.monitors.consumers.incident_occurrences_consumer.create_incident_occurrence"
     )
+    def test_simple(self, mock_create_incident_occurrence):
+        ts = timezone.now().replace(second=0, microsecond=0)
+
+        monitor = self.create_monitor()
+        monitor_environment = MonitorEnvironment.objects.create(
+            monitor=monitor,
+            environment_id=self.environment.id,
+            status=MonitorStatus.ERROR,
+        )
+
+        last_checkin = timezone.now()
+        trace_id = uuid.uuid4()
+
+        failed_checkin = MonitorCheckIn.objects.create(
+            monitor=monitor,
+            monitor_environment=monitor_environment,
+            project_id=self.project.id,
+            status=CheckInStatus.ERROR,
+            trace_id=trace_id,
+            date_added=last_checkin,
+        )
+        incident = MonitorIncident.objects.create(
+            monitor=monitor,
+            monitor_environment=monitor_environment,
+            starting_checkin=failed_checkin,
+            starting_timestamp=last_checkin,
+        )
+
+        consumer = create_consumer()
+        sned_incident_occurrence(
+            consumer,
+            ts,
+            {
+                "clock_tick_ts": int(ts.timestamp()),
+                "received_ts": int(last_checkin.timestamp()),
+                "incident_id": incident.id,
+                "failed_checkin_id": failed_checkin.id,
+                "previous_checkin_ids": [failed_checkin.id],
+            },
+        )
+
+        assert mock_create_incident_occurrence.call_count == 1
+        assert mock_create_incident_occurrence.mock_calls[0] == mock.call(
+            failed_checkin,
+            [failed_checkin],
+            incident,
+            last_checkin.replace(microsecond=0),
+        )
+
+    @mock.patch("sentry.monitors.consumers.incident_occurrences_consumer.logger")
+    def test_missing_data(self, mock_logger):
+        ts = timezone.now().replace(second=0, microsecond=0)
+
+        monitor = self.create_monitor()
+        monitor_environment = MonitorEnvironment.objects.create(
+            monitor=monitor,
+            environment_id=self.environment.id,
+            status=MonitorStatus.ERROR,
+        )
+
+        last_checkin = timezone.now()
+        trace_id = uuid.uuid4()
+
+        failed_checkin = MonitorCheckIn.objects.create(
+            monitor=monitor,
+            monitor_environment=monitor_environment,
+            project_id=self.project.id,
+            status=CheckInStatus.ERROR,
+            trace_id=trace_id,
+            date_added=last_checkin,
+        )
+        incident = MonitorIncident.objects.create(
+            monitor=monitor,
+            monitor_environment=monitor_environment,
+            starting_checkin=failed_checkin,
+            starting_timestamp=last_checkin,
+        )
+
+        consumer = create_consumer()
+
+        # Send with bad incident id
+        sned_incident_occurrence(
+            consumer,
+            ts,
+            {
+                "clock_tick_ts": int(ts.timestamp()),
+                "received_ts": int(last_checkin.timestamp()),
+                "incident_id": 1234,
+                "failed_checkin_id": failed_checkin.id,
+                "previous_checkin_ids": [failed_checkin.id],
+            },
+        )
+        mock_logger.exception.assert_called_with("missing_incident")
+
+        # Send with bad failed_checkin_id
+        sned_incident_occurrence(
+            consumer,
+            ts,
+            {
+                "clock_tick_ts": int(ts.timestamp()),
+                "received_ts": int(last_checkin.timestamp()),
+                "incident_id": incident.id,
+                "failed_checkin_id": 1234,
+                "previous_checkin_ids": [failed_checkin.id],
+            },
+        )
+        mock_logger.error.assert_called_with("missing_check_ins")
+
+        # Send with bad previous_checkin_ids
+        sned_incident_occurrence(
+            consumer,
+            ts,
+            {
+                "clock_tick_ts": int(ts.timestamp()),
+                "received_ts": int(last_checkin.timestamp()),
+                "incident_id": incident.id,
+                "failed_checkin_id": failed_checkin.id,
+                "previous_checkin_ids": [123],
+            },
+        )
+        mock_logger.error.assert_called_with("missing_check_ins")
