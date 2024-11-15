2217bc6416944ad00a8836cfca2337aa107337d5
ArthurKnaus <arthur.knaus@sentry.io>
2024-11-15 14:51:21 +0100

    feat(dynamic-sampling): Copy changes and doc links (#80818)
    part of https://github.com/getsentry/projects/issues/390

--- static/app/views/settings/dynamicSampling/index.tsx
+++ static/app/views/settings/dynamicSampling/index.tsx
@@ -1,6 +1,7 @@
 import {Fragment} from 'react';
 
 import {Alert} from 'sentry/components/alert';
+import {LinkButton} from 'sentry/components/button';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {t} from 'sentry/locale';
 import {hasDynamicSamplingCustomFeature} from 'sentry/utils/dynamicSampling/features';
@@ -20,7 +21,17 @@ export default function DynamicSamplingSettings() {
     <Fragment>
       <SentryDocumentTitle title={t('Dynamic Sampling')} orgSlug={organization.slug} />
       <div>
-        <SettingsPageHeader title={t('Dynamic Sampling')} />
+        <SettingsPageHeader
+          title={t('Dynamic Sampling')}
+          action={
+            <LinkButton
+              external
+              href="https://docs.sentry.io/product/performance/retention-priorities/"
+            >
+              {t('Read the docs')}
+            </LinkButton>
+          }
+        />
         <p>
           {t(
             'Dynamic sampling adaptively reduces the number of spans stored in Sentry without changing SDK sample rates. It allows you to keep the most relevant samples and obtain accurate high-level insights while limiting redundancy and stored span volume. You can customize sample rates and priorities in these settings to control which data is stored.'
