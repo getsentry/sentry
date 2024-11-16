99a4f43c822c1df6fdcd412ad686922fc1200a92
Christinarlong <christina.long@sentry.io>
2024-11-15 16:03:06 -0800

    add barrel file for django

---
+++ src/sentry/notifications/models/__init__.py
@@ -0,0 +1,17 @@
+from sentry.notifications.models.notificationaction import (
+    ActionRegistration,
+    ActionService,
+    ActionTarget,
+    ActionTrigger,
+    NotificationAction,
+    NotificationActionProject,
+)
+
+__all__ = (
+    "NotificationActionProject",
+    "NotificationAction",
+    "ActionService",
+    "ActionTrigger",
+    "ActionTarget",
+    "ActionRegistration",
+)
