0ebee2b107ea9f41a9cb156ed6403c6324b27f85
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-11-19 12:45:59 +0800

    ref(eslint): Copy in eslint-config-sentry-react/* rules
    note that we still have `settings.react.version: 17.0.2`. It was wrong
    before but i'm leaving it. Enabling it will cause passwordStrength.tsx
    to error with `ReactDOM.render is deprecated`

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,336 @@
 
 const detectDeprecations = !!process.env.SENTRY_DETECT_DEPRECATIONS;
 
+const reactReactRules = {
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/display-name.md
+  'react/display-name': ['off'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-multi-comp.md
+  'react/no-multi-comp': [
+    'off',
+    {
+      ignoreStateless: true,
+    },
+  ],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-fragments.md
+  'react/jsx-fragments': ['error', 'element'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-handler-names.md
+  // Ensures that any component or prop methods used to handle events are correctly prefixed.
+  'react/jsx-handler-names': [
+    'off',
+    {
+      eventHandlerPrefix: 'handle',
+      eventHandlerPropPrefix: 'on',
+    },
+  ],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
+  'react/jsx-key': ['error'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-undef.md
+  'react/jsx-no-undef': ['error'],
+
+  // Disabled as we use the newer JSX transform babel plugin.
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-react.md
+  'react/jsx-uses-react': ['off'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-vars.md
+  'react/jsx-uses-vars': ['error'],
+
+  /**
+   * Deprecation related rules
+   */
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-deprecated.md
+  'react/no-deprecated': ['error'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-is-mounted.md
+  'react/no-is-mounted': ['warn'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-find-dom-node.md
+  // Recommended to use callback refs instead
+  // TODO: Upgrade sentry to use callback refs
+  'react/no-find-dom-node': ['warn'],
+
+  // Prevent usage of the return value of React.render
+  // deprecation: https://facebook.github.io/react/docs/react-dom.html#render
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-render-return-value.md
+  'react/no-render-return-value': ['error'],
+
+  // Children should always be actual children, not passed in as a prop.
+  // When using JSX, the children should be nested between the opening and closing tags. When not using JSX, the children should be passed as additional arguments to React.createElement.
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-children-prop.md
+  'react/no-children-prop': ['error'],
+
+  // This rule helps prevent problems caused by using children and the dangerouslySetInnerHTML prop at the same time.
+  // React will throw a warning if this rule is ignored.
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger-with-children.md
+  'react/no-danger-with-children': ['error'],
+
+  // Prevent direct mutation of this.state
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
+  'react/no-direct-mutation-state': ['error'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-mount-set-state.md
+  'react/no-did-mount-set-state': ['error'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-update-set-state.md"
+  'react/no-did-update-set-state': ['error'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-redundant-should-component-update.md
+  'react/no-redundant-should-component-update': ['error'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-typos.md
+  'react/no-typos': ['error'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-string-refs.md
+  // This is now considered legacy, callback refs preferred
+  'react/no-string-refs': ['warn'],
+
+  // Prevent invalid characters from appearing in markup
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unescaped-entities.md
+  'react/no-unescaped-entities': ['off'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
+  'react/no-unknown-property': ['error', {ignore: ['css']}],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unused-prop-types.md
+  // Disabled since this currently fails to correctly detect a lot of
+  // typescript prop type usage.
+  'react/no-unused-prop-types': ['off'],
+
+  // We do not need proptypes since we're using typescript
+  'react/prop-types': ['off'],
+
+  // When writing the render method in a component it is easy to forget to return the JSX content.
+  // This rule will warn if the return statement is missing.
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-render-return.md
+  'react/require-render-return': ['error'],
+
+  // Disabled as we are using the newer JSX transform babel plugin.
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
+  'react/react-in-jsx-scope': ['off'],
+
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/self-closing-comp.md
+  'react/self-closing-comp': ['error'],
+
+  // This also causes issues with typescript
+  // See: https://github.com/yannickcr/eslint-plugin-react/issues/2066
+  //
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/sort-comp.md
+  'react/sort-comp': ['warn'],
+
+  // Disabled because of prettier
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/wrap-multilines.md
+  'react/jsx-wrap-multilines': ['off'],
+
+  // Consistent <Component booleanProp /> (never add ={true})
+  // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-boolean-value.md
+  'react/jsx-boolean-value': ['error', 'never'],
+
+  // Consistent function component declaration styles
+  // https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/function-component-definition.md
+  'react/function-component-definition': [
+    'error',
+    {namedComponents: 'function-declaration'},
+  ],
+};
+
+const reactImportRules = {
+  // Not recommended to be enabled with typescript-eslint
+  // https://typescript-eslint.io/linting/troubleshooting/performance-troubleshooting/#eslint-plugin-import
+  'import/no-unresolved': ['off'],
+  'import/named': ['off'],
+  'import/default': ['off'],
+  'import/export': ['off'],
+  'import/no-named-as-default-member': ['off'],
+
+  // Redflags
+  // do not allow a default import name to match a named export (airbnb: error)
+  // Issue with `DefaultIssuePlugin` and `app/plugins/index`
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-as-default.md
+  'import/no-named-as-default': ['off'],
+
+  // disallow use of jsdoc-marked-deprecated imports
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-deprecated.md
+  'import/no-deprecated': ['off'],
+
+  // Forbid mutable exports (airbnb: error)
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-mutable-exports.md
+  // TODO: enable?
+  'import/no-mutable-exports': ['off'],
+
+  // disallow require()
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-commonjs.md
+  'import/no-commonjs': ['off'],
+
+  // disallow AMD require/define
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-amd.md
+  'import/no-amd': ['error'],
+
+  // disallow duplicate imports
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-duplicates.md
+  'import/no-duplicates': ['error'],
+
+  // disallow namespace imports
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-namespace.md
+  'import/no-namespace': ['off'],
+
+  // Ensure consistent use of file extension within the import path
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/extensions.md
+  // TODO this fucks up getsentry
+  'import/extensions': [
+    'off',
+    'always',
+    {
+      js: 'never',
+      jsx: 'never',
+    },
+  ],
+
+  // Enforce a convention in module import order
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/order.md
+  'import/order': [
+    'error',
+    {
+      groups: ['builtin', 'external', 'internal', ['parent', 'sibling', 'index']],
+      'newlines-between': 'always',
+    },
+  ],
+
+  // Require a newline after the last import/require in a group
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/newline-after-import.md
+  'import/newline-after-import': ['error'],
+
+  // Require modules with a single export to use a default export (airbnb: error)
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/prefer-default-export.md
+  'import/prefer-default-export': ['off'],
+
+  // Restrict which files can be imported in a given folder
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-restricted-paths.md
+  'import/no-restricted-paths': ['off'],
+
+  // Forbid modules to have too many dependencies
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/max-dependencies.md
+  'import/max-dependencies': ['off', {max: 10}],
+
+  // Forbid import of modules using absolute paths
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-absolute-path.md
+  'import/no-absolute-path': ['error'],
+
+  // Forbid require() calls with expressions (airbnb: error)
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-dynamic-require.md
+  'import/no-dynamic-require': ['off'],
+
+  // Use webpack default chunk names
+  'import/dynamic-import-chunkname': ['off'],
+
+  // prevent importing the submodules of other modules
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-internal-modules.md
+  'import/no-internal-modules': [
+    'off',
+    {
+      allow: [],
+    },
+  ],
+
+  // Warn if a module could be mistakenly parsed as a script by a consumer
+  // leveraging Unambiguous JavaScript Grammar
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/unambiguous.md
+  // this should not be enabled until this proposal has at least been *presented* to TC39.
+  // At the moment, it"s not a thing.
+  'import/unambiguous': ['off'],
+
+  // Forbid Webpack loader syntax in imports
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-webpack-loader-syntax.md
+  'import/no-webpack-loader-syntax': ['error'],
+
+  // Prevent unassigned imports
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-unassigned-import.md
+  // importing for side effects is perfectly acceptable, if you need side effects.
+  'import/no-unassigned-import': ['off'],
+
+  // Prevent importing the default as if it were named
+  // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-named-default.md
+  'import/no-named-default': ['error'],
+
+  // Reports if a module"s default export is unnamed
+  // https://github.com/benmosher/eslint-plugin-import/blob/d9b712ac7fd1fddc391f7b234827925c160d956f/docs/rules/no-anonymous-default-export.md
+  'import/no-anonymous-default-export': [
+    'error',
+    {
+      allowArray: false,
+      allowArrowFunction: false,
+      allowAnonymousClass: false,
+      allowAnonymousFunction: false,
+      allowCallExpression: true,
+      allowLiteral: false,
+      allowObject: false,
+    },
+  ],
+};
+
+const reactJestRules = {
+  'jest/no-large-snapshots': ['warn', {maxSize: 2000}],
+  'jest/no-disabled-tests': 'error',
+};
+
+const reactRules = {
+  ...reactReactRules,
+  ...reactImportRules,
+  ...reactJestRules,
+  /**
+   * React hooks
+   */
+  'react-hooks/exhaustive-deps': 'error',
+  // Biome not yet enforcing all parts of this rule https://github.com/biomejs/biome/issues/1984
+  'react-hooks/rules-of-hooks': 'error',
+
+  /**
+   * Custom
+   */
+  // highlights literals in JSX components w/o translation tags
+  'getsentry/jsx-needs-il8n': ['off'],
+  'testing-library/render-result-naming-convention': 'off',
+  'testing-library/no-unnecessary-act': 'off',
+
+  // Disabled as we have many tests which render as simple validations
+  'jest/expect-expect': 'off',
+
+  // Disabled as we have some comment out tests that cannot be
+  // uncommented due to typescript errors.
+  'jest/no-commented-out-tests': 'off',
+
+  // Disabled as we do sometimes have conditional expects
+  'jest/no-conditional-expect': 'off',
+
+  // Useful for exporting some test utilities
+  'jest/no-export': 'off',
+
+  'typescript-sort-keys/interface': [
+    'error',
+    'asc',
+    {caseSensitive: true, natural: false, requiredFirst: true},
+  ],
+
+  // Disallow importing `import React from 'react'`. This is not needed since
+  // React 17. We prefer the named imports for potential tree-shaking gains
+  // in the future.
+  'no-restricted-imports': [
+    'error',
+    {
+      paths: [
+        {
+          name: 'react',
+          importNames: ['default'],
+          message: 'Prefer named React imports (React types DO NOT need imported!)',
+        },
+      ],
+    },
+  ],
+};
+
 const appRules = {
   /**
    * emotion rules for v10
@@ -249,7 +579,12 @@ const strictRules = {
   'sentry/no-styled-shortcut': ['error'],
 };
 
-const extendsList = ['sentry-react', 'plugin:import/typescript'];
+const extendsList = [
+  'sentry',
+  'plugin:jest/recommended',
+  'plugin:jest-dom/recommended',
+  'plugin:import/typescript',
+];
 if (detectDeprecations) {
   extendsList.push('plugin:deprecation/recommended');
 }
@@ -259,6 +594,10 @@ module.exports = {
   extends: extendsList,
 
   plugins: [
+    'jest-dom',
+    'testing-library',
+    'typescript-sort-keys',
+    'react-hooks',
     '@typescript-eslint',
     '@emotion',
     'import',
@@ -309,6 +648,9 @@ module.exports = {
   },
 
   settings: {
+    react: {
+      version: '17.0.2', // React version, can not `detect` because of getsentry
+    },
     'import/parsers': {
       '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
@@ -319,6 +661,7 @@ module.exports = {
   },
 
   rules: {
+    ...reactRules,
     ...appRules,
     ...strictRules,
     'react-hooks/rules-of-hooks': 'error',
@@ -439,6 +782,7 @@ module.exports = {
       files: ['static/**/*.spec.{ts,js}', 'tests/js/**/*.{ts,js}'],
       extends: ['plugin:testing-library/react', ...extendsList],
       rules: {
+        ...reactRules,
         ...appRules,
         ...strictRules,
         // TODO(@anonrig): Remove this from eslint-sentry-config
