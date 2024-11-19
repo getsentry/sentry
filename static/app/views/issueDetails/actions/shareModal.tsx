929fb6825afe508780aa63ff209d6ac2e579140e
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-11-19 14:15:33 -0500

    Update static/app/views/issueDetails/actions/shareModal.tsx
    Co-authored-by: Scott Cooper <scttcper@gmail.com>

--- static/app/views/issueDetails/actions/shareModal.tsx
+++ static/app/views/issueDetails/actions/shareModal.tsx
@@ -29,7 +29,7 @@ interface ShareIssueModalProps extends ModalRenderProps {
 type UrlRef = React.ElementRef<typeof AutoSelectText>;
 
 export function getShareUrl(group: Group) {
-  const path = `/share/issue/${group!.shareId}/`;
+  const path = `/share/issue/${group.shareId}/`;
   const {host, protocol} = window.location;
   return `${protocol}//${host}${path}`;
 }
