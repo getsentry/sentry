f8566e00e51976d98a1bb047458eed97f7084631
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2024-11-19 11:02:40 -0800

    chore(notifications): move notificationaction to notifications (#80797)

--- src/sentry/notifications/models/__init__.py
+++ src/sentry/notifications/models/__init__.py
@@ -1,3 +1,19 @@
+from sentry.notifications.models.notificationaction import (
+    ActionRegistration,
+    ActionService,
+    ActionTarget,
+    ActionTrigger,
+    NotificationAction,
+    NotificationActionProject,
+)
 from sentry.notifications.models.notificationsettingoption import NotificationSettingOption
 
-__all__ = ("NotificationSettingOption",)
+__all__ = (
+    "NotificationActionProject",
+    "NotificationAction",
+    "ActionService",
+    "ActionTrigger",
+    "ActionTarget",
+    "ActionRegistration",
+    "NotificationSettingOption",
+)
