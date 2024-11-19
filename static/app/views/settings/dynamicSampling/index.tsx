35fba1e7f6ebaaeff7dfa3d60fd802c6aa28423a
ArthurKnaus <arthur.knaus@sentry.io>
2024-11-19 18:48:03 +0100

    fix(dynamic-sampling): Bug bash papercuts (#80991)
    - Display banner for insufficient permissions
    - Add tooltips to all disabled inputs
    - Ensure estimated org rate stays in boundaries (0-100)
    - Fix Safari bug where `.map()` is not available on a `MapIterator`
    instance
    - Properly disable project rate inputs when permissions are missing

--- static/app/views/settings/dynamicSampling/index.tsx
+++ static/app/views/settings/dynamicSampling/index.tsx
@@ -9,9 +9,11 @@ import useOrganization from 'sentry/utils/useOrganization';
 import SettingsPageHeader from 'sentry/views/settings/components/settingsPageHeader';
 import {OrganizationSampling} from 'sentry/views/settings/dynamicSampling/organizationSampling';
 import {ProjectSampling} from 'sentry/views/settings/dynamicSampling/projectSampling';
+import {useHasDynamicSamplingWriteAccess} from 'sentry/views/settings/dynamicSampling/utils/access';
 
 export default function DynamicSamplingSettings() {
   const organization = useOrganization();
+  const hasWriteAccess = useHasDynamicSamplingWriteAccess();
 
   if (!hasDynamicSamplingCustomFeature(organization)) {
     return <Alert type="warning">{t("You don't have access to this feature")}</Alert>;
@@ -20,29 +22,34 @@ export default function DynamicSamplingSettings() {
   return (
     <Fragment>
       <SentryDocumentTitle title={t('Dynamic Sampling')} orgSlug={organization.slug} />
-      <div>
-        <SettingsPageHeader
-          title={t('Dynamic Sampling')}
-          action={
-            <LinkButton
-              external
-              href="https://docs.sentry.io/product/performance/retention-priorities/"
-            >
-              {t('Read the docs')}
-            </LinkButton>
-          }
-        />
-        <p>
+      <SettingsPageHeader
+        title={t('Dynamic Sampling')}
+        action={
+          <LinkButton
+            external
+            href="https://docs.sentry.io/product/performance/retention-priorities/"
+          >
+            {t('Read the docs')}
+          </LinkButton>
+        }
+      />
+      {!hasWriteAccess && (
+        <Alert type="warning">
           {t(
-            'Dynamic sampling adaptively reduces the number of spans stored in Sentry without changing SDK sample rates. It allows you to keep the most relevant samples and obtain accurate high-level insights while limiting redundancy and stored span volume. You can customize sample rates and priorities in these settings to control which data is stored.'
+            'These settings can only be edited by users with the organization owner or manager role.'
           )}
-        </p>
-        {organization.samplingMode === 'organization' ? (
-          <OrganizationSampling />
-        ) : (
-          <ProjectSampling />
+        </Alert>
+      )}
+      <p>
+        {t(
+          'Dynamic sampling adaptively reduces the number of spans stored in Sentry without changing SDK sample rates. It allows you to keep the most relevant samples and obtain accurate high-level insights while limiting redundancy and stored span volume. You can customize sample rates and priorities in these settings to control which data is stored.'
         )}
-      </div>
+      </p>
+      {organization.samplingMode === 'organization' ? (
+        <OrganizationSampling />
+      ) : (
+        <ProjectSampling />
+      )}
     </Fragment>
   );
 }
