da9c427eda0597bc5c9b62eba5337d495c02cb06
victoria-yining-huang <victoria.huang777@gmail.com>
2024-11-19 13:02:49 -0500

    fixed tests

--- src/sentry/ingest/consumer/processors.py
+++ src/sentry/ingest/consumer/processors.py
@@ -203,9 +203,10 @@ def process_event(
         else:
             with metrics.timer("ingest_consumer._store_event"):
                 cache_key = processing_store.store(data)
-            track_sampled_event(
-                data["event_id"], data.get("type"), TransactionStageStatus.REDIS_PUT
-            )
+            if data.get("type") == "transaction":
+                track_sampled_event(
+                    data["event_id"], "transaction", TransactionStageStatus.REDIS_PUT
+                )
 
             save_attachments(attachments, cache_key)
 
