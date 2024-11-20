b19f5b7deb6f77f29a71df2c65fcb3ebeed0fa27
Nikki Kapadia <72356613+nikkikapadia@users.noreply.github.com>
2024-11-20 14:13:37 -0500

    feat(dashboards): Create table for landing page table view (#80945)
    As part of creating table view for the dashboards landing page this PR
    creates the actual table with most of the fields that are present in the
    mockup (only column left to add is permissions/access column). Switching
    the toggle to list view will bring you to this page view. I've defaulted
    to 25 items per page (i'm not sure if there's a different amount we want
    to use). It's still under the feature flag `dashboards-table-view` which
    I only have access to atm.
    
    <img width="1512" alt="image"
    src="https://github.com/user-attachments/assets/89530817-cd94-4128-ad3a-fa1ea05851d6">

---
+++ static/app/views/dashboards/manage/dashboardGrid.spec.tsx
@@ -0,0 +1,284 @@
+import {DashboardListItemFixture} from 'sentry-fixture/dashboard';
+import {LocationFixture} from 'sentry-fixture/locationFixture';
+import {OrganizationFixture} from 'sentry-fixture/organization';
+import {UserFixture} from 'sentry-fixture/user';
+
+import {initializeOrg} from 'sentry-test/initializeOrg';
+import {
+  render,
+  renderGlobalModal,
+  screen,
+  userEvent,
+  waitFor,
+  within,
+} from 'sentry-test/reactTestingLibrary';
+
+import DashboardGrid from 'sentry/views/dashboards/manage/dashboardGrid';
+import {type DashboardListItem, DisplayType} from 'sentry/views/dashboards/types';
+
+describe('Dashboards - DashboardGrid', function () {
+  let dashboards: DashboardListItem[];
+  let deleteMock: jest.Mock;
+  let dashboardUpdateMock: jest.Mock;
+  let createMock: jest.Mock;
+  const organization = OrganizationFixture({
+    features: ['global-views', 'dashboards-basic', 'dashboards-edit', 'discover-query'],
+  });
+
+  const {router} = initializeOrg();
+
+  beforeEach(function () {
+    MockApiClient.clearMockResponses();
+
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/projects/',
+      body: [],
+    });
+    dashboards = [
+      DashboardListItemFixture({
+        id: '1',
+        title: 'Dashboard 1',
+        dateCreated: '2021-04-19T13:13:23.962105Z',
+        createdBy: UserFixture({id: '1'}),
+      }),
+      DashboardListItemFixture({
+        id: '2',
+        title: 'Dashboard 2',
+        dateCreated: '2021-04-19T13:13:23.962105Z',
+        createdBy: UserFixture({id: '1'}),
+        widgetPreview: [
+          {
+            displayType: DisplayType.LINE,
+            layout: null,
+          },
+          {
+            displayType: DisplayType.TABLE,
+            layout: null,
+          },
+        ],
+      }),
+    ];
+    deleteMock = MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/dashboards/2/',
+      method: 'DELETE',
+      statusCode: 200,
+    });
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/dashboards/2/',
+      method: 'GET',
+      statusCode: 200,
+      body: {
+        id: '2',
+        title: 'Dashboard Demo',
+        widgets: [
+          {
+            id: '1',
+            title: 'Errors',
+            displayType: 'big_number',
+            interval: '5m',
+          },
+          {
+            id: '2',
+            title: 'Transactions',
+            displayType: 'big_number',
+            interval: '5m',
+          },
+          {
+            id: '3',
+            title: 'p50 of /api/cat',
+            displayType: 'big_number',
+            interval: '5m',
+          },
+        ],
+      },
+    });
+    createMock = MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/dashboards/',
+      method: 'POST',
+      statusCode: 200,
+    });
+    dashboardUpdateMock = jest.fn();
+  });
+
+  it('renders an empty list', async function () {
+    render(
+      <DashboardGrid
+        onDashboardsChange={jest.fn()}
+        organization={organization}
+        dashboards={[]}
+        location={router.location}
+        columnCount={3}
+        rowCount={3}
+      />
+    );
+
+    expect(screen.getByTestId('empty-state')).toBeInTheDocument();
+    expect(
+      await screen.findByText('Sorry, no Dashboards match your filters.')
+    ).toBeInTheDocument();
+  });
+
+  it('renders dashboard list', function () {
+    render(
+      <DashboardGrid
+        onDashboardsChange={jest.fn()}
+        organization={organization}
+        dashboards={dashboards}
+        location={router.location}
+        columnCount={3}
+        rowCount={3}
+      />
+    );
+
+    expect(screen.getByText('Dashboard 1')).toBeInTheDocument();
+    expect(screen.getByText('Dashboard 2')).toBeInTheDocument();
+  });
+
+  it('returns landing page url for dashboards', function () {
+    render(
+      <DashboardGrid
+        onDashboardsChange={jest.fn()}
+        organization={organization}
+        dashboards={dashboards}
+        location={router.location}
+        columnCount={3}
+        rowCount={3}
+      />,
+      {router}
+    );
+
+    expect(screen.getByRole('link', {name: 'Dashboard 1'})).toHaveAttribute(
+      'href',
+      '/organizations/org-slug/dashboard/1/'
+    );
+    expect(screen.getByRole('link', {name: 'Dashboard 2'})).toHaveAttribute(
+      'href',
+      '/organizations/org-slug/dashboard/2/'
+    );
+  });
+
+  it('persists global selection headers', function () {
+    render(
+      <DashboardGrid
+        onDashboardsChange={jest.fn()}
+        organization={organization}
+        dashboards={dashboards}
+        location={{...LocationFixture(), query: {statsPeriod: '7d'}}}
+        columnCount={3}
+        rowCount={3}
+      />,
+      {router}
+    );
+
+    expect(screen.getByRole('link', {name: 'Dashboard 1'})).toHaveAttribute(
+      'href',
+      '/organizations/org-slug/dashboard/1/?statsPeriod=7d'
+    );
+  });
+
+  it('can delete dashboards', async function () {
+    render(
+      <DashboardGrid
+        organization={organization}
+        dashboards={dashboards}
+        location={{...LocationFixture(), query: {}}}
+        onDashboardsChange={dashboardUpdateMock}
+        columnCount={3}
+        rowCount={3}
+      />,
+      {router}
+    );
+    renderGlobalModal();
+
+    await userEvent.click(screen.getAllByRole('button', {name: /dashboard actions/i})[1]);
+    await userEvent.click(screen.getByTestId('dashboard-delete'));
+
+    expect(deleteMock).not.toHaveBeenCalled();
+
+    await userEvent.click(
+      within(screen.getByRole('dialog')).getByRole('button', {name: /confirm/i})
+    );
+
+    await waitFor(() => {
+      expect(deleteMock).toHaveBeenCalled();
+      expect(dashboardUpdateMock).toHaveBeenCalled();
+    });
+  });
+
+  it('cannot delete last dashboard', async function () {
+    const singleDashboard = [
+      DashboardListItemFixture({
+        id: '1',
+        title: 'Dashboard 1',
+        dateCreated: '2021-04-19T13:13:23.962105Z',
+        createdBy: UserFixture({id: '1'}),
+        widgetPreview: [],
+      }),
+    ];
+    render(
+      <DashboardGrid
+        organization={organization}
+        dashboards={singleDashboard}
+        location={LocationFixture()}
+        onDashboardsChange={dashboardUpdateMock}
+        columnCount={3}
+        rowCount={3}
+      />
+    );
+
+    await userEvent.click(screen.getByRole('button', {name: /dashboard actions/i}));
+    expect(screen.getByTestId('dashboard-delete')).toHaveAttribute(
+      'aria-disabled',
+      'true'
+    );
+  });
+
+  it('can duplicate dashboards', async function () {
+    render(
+      <DashboardGrid
+        organization={organization}
+        dashboards={dashboards}
+        location={{...LocationFixture(), query: {}}}
+        onDashboardsChange={dashboardUpdateMock}
+        columnCount={3}
+        rowCount={3}
+      />
+    );
+
+    await userEvent.click(screen.getAllByRole('button', {name: /dashboard actions/i})[1]);
+    await userEvent.click(screen.getByTestId('dashboard-duplicate'));
+
+    await waitFor(() => {
+      expect(createMock).toHaveBeenCalled();
+      expect(dashboardUpdateMock).toHaveBeenCalled();
+    });
+  });
+
+  it('does not throw an error if the POST fails during duplication', async function () {
+    const postMock = MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/dashboards/',
+      method: 'POST',
+      statusCode: 404,
+    });
+
+    render(
+      <DashboardGrid
+        organization={organization}
+        dashboards={dashboards}
+        location={{...LocationFixture(), query: {}}}
+        onDashboardsChange={dashboardUpdateMock}
+        columnCount={3}
+        rowCount={3}
+      />
+    );
+
+    await userEvent.click(screen.getAllByRole('button', {name: /dashboard actions/i})[1]);
+    await userEvent.click(screen.getByTestId('dashboard-duplicate'));
+
+    await waitFor(() => {
+      expect(postMock).toHaveBeenCalled();
+      // Should not update, and not throw error
+      expect(dashboardUpdateMock).not.toHaveBeenCalled();
+    });
+  });
+});
