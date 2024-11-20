d135502cecf55b6bb4e6de03c776ea00cd766ece
Leander Rodrigues <leander.rodrigues@sentry.io>
2024-11-20 12:26:15 -0500

    chore(streamline): Allow new flag to enforce the UI (#81049)
    This PR allows the `issue-details-streamline-enforce` flag to force
    users of an organization into the streamline UI without the ability to
    opt out. [Only my test
    org](https://github.com/getsentry/sentry-options-automator/pull/2730)
    will receive this change, but eventually it'll apply to more people.

--- static/app/views/issueDetails/utils.spec.tsx
+++ static/app/views/issueDetails/utils.spec.tsx
@@ -1,16 +1,21 @@
 import {LocationFixture} from 'sentry-fixture/locationFixture';
+import {OrganizationFixture} from 'sentry-fixture/organization';
 import {UserFixture} from 'sentry-fixture/user';
 
 import {act, renderHook} from 'sentry-test/reactTestingLibrary';
 
 import ConfigStore from 'sentry/stores/configStore';
 import {useLocation} from 'sentry/utils/useLocation';
+import useOrganization from 'sentry/utils/useOrganization';
 import {useHasStreamlinedUI} from 'sentry/views/issueDetails/utils';
 
 jest.mock('sentry/utils/useLocation');
+jest.mock('sentry/utils/useOrganization');
 
 describe('useHasStreamlinedUI', () => {
   it("respects the 'streamline' query param", () => {
+    jest.mocked(useOrganization).mockReturnValue(OrganizationFixture());
+
     const location = LocationFixture({query: {streamline: '1'}});
     jest.mocked(useLocation).mockReturnValue(location);
     const {result: queryParamEnabled} = renderHook(useHasStreamlinedUI);
@@ -28,8 +33,7 @@ describe('useHasStreamlinedUI', () => {
     user.options.prefersIssueDetailsStreamlinedUI = true;
     act(() => ConfigStore.set('user', user));
 
-    const location = LocationFixture();
-    jest.mocked(useLocation).mockReturnValue(location);
+    jest.mocked(useLocation).mockReturnValue(LocationFixture());
     const {result: userPrefersStreamline} = renderHook(useHasStreamlinedUI);
     expect(userPrefersStreamline.current).toBe(true);
 
@@ -39,7 +43,12 @@ describe('useHasStreamlinedUI', () => {
     expect(userPrefersLegacy.current).toBe(false);
   });
 
-  it('values query param above user preferences', () => {
+  it('values query param above user preferences and organization flags', () => {
+    const enforceOrg = OrganizationFixture({
+      features: ['issue-details-streamline-enforce'],
+    });
+    jest.mocked(useOrganization).mockReturnValue(enforceOrg);
+
     ConfigStore.init();
     const user = UserFixture();
     user.options.prefersIssueDetailsStreamlinedUI = false;
@@ -56,5 +65,21 @@ describe('useHasStreamlinedUI', () => {
     const {result: prefersStreamlineButQueryParamDisabled} =
       renderHook(useHasStreamlinedUI);
     expect(prefersStreamlineButQueryParamDisabled.current).toBe(false);
+  });
+
+  it('ignores preferences if enforce flag is set', () => {
+    const enforceOrg = OrganizationFixture({
+      features: ['issue-details-streamline-enforce'],
+    });
+    jest.mocked(useOrganization).mockReturnValue(enforceOrg);
+
+    ConfigStore.init();
+    const user = UserFixture();
+    user.options.prefersIssueDetailsStreamlinedUI = false;
+    act(() => ConfigStore.set('user', user));
+
+    jest.mocked(useLocation).mockReturnValue(LocationFixture());
+    const {result} = renderHook(useHasStreamlinedUI);
+    expect(result.current).toBe(true);
   });
 });
