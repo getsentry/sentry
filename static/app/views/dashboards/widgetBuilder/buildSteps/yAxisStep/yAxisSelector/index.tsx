9fe290deddbbfc34106644bac9ce00e68e100fcd
Nar Saynorath <nar.saynorath@sentry.io>
2024-11-21 15:45:28 -0500

    feat(dashboards): Add to Dashboard from Explore (#81151)
    Same as #81064 but with tests fixed.

--- static/app/views/dashboards/widgetBuilder/buildSteps/yAxisStep/yAxisSelector/index.tsx
+++ static/app/views/dashboards/widgetBuilder/buildSteps/yAxisStep/yAxisSelector/index.tsx
@@ -21,6 +21,8 @@ import {FieldValueKind} from 'sentry/views/discover/table/types';
 import {AddButton} from './addButton';
 import {DeleteButton} from './deleteButton';
 
+export const MAX_NUM_Y_AXES = 3;
+
 interface Props {
   aggregates: QueryFieldValue[];
   displayType: DisplayType;
@@ -107,7 +109,7 @@ export function YAxisSelector({
 
   const hideAddYAxisButtons =
     ([DisplayType.LINE, DisplayType.AREA, DisplayType.BAR].includes(displayType) &&
-      aggregates.length === 3) ||
+      aggregates.length === MAX_NUM_Y_AXES) ||
     (displayType === DisplayType.BIG_NUMBER && widgetType === WidgetType.RELEASE);
 
   let injectedFunctions: Set<string> = new Set();
