b03dff09ce02f7c99b721cc471533a521007f2bf
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2024-11-21 07:24:36 -0500

    ref(releases): Function to handle resolved in release (#80983)
    This is the largest code block in update_groups. This moves all related
    logic into a single function.

--- src/sentry/issues/status_change.py
+++ src/sentry/issues/status_change.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 import logging
-from collections import defaultdict, namedtuple
+from collections import defaultdict
 from collections.abc import Sequence
 from datetime import datetime, timedelta, timezone
 from typing import Any
@@ -25,7 +25,6 @@ from sentry.users.models.user import User
 from sentry.utils import json
 
 logger = logging.getLogger(__name__)
-ActivityInfo = namedtuple("ActivityInfo", ("activity_type", "activity_data"))
 
 
 def infer_substatus(
@@ -74,12 +73,10 @@ def handle_status_update(
     status_details: dict[str, Any],
     acting_user: User | None,
     sender: Any,
-) -> ActivityInfo:
+) -> None:
     """
     Update the status for a list of groups and create entries for Activity and GroupHistory.
     This currently handles unresolving or ignoring groups.
-
-    Returns a tuple of (activity_type, activity_data) for the activity that was created.
     """
     activity_data = {}
     activity_type = (
@@ -173,5 +170,3 @@ def handle_status_update(
                 created=False,
                 update_fields=["status", "substatus"],
             )
-
-    return ActivityInfo(activity_type, activity_data)
