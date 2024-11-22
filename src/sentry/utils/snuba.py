59e9308713a0f375c322cb0a09da945e90735ac1
Pierre Massat <pierre.massat@sentry.io>
2024-11-22 13:10:36 -0500

    chore(metrics-summaries): Remove metrics summaries queries and endpoints (#81133)

--- src/sentry/utils/snuba.py
+++ src/sentry/utils/snuba.py
@@ -236,22 +236,6 @@ SPAN_EAP_COLUMN_MAP = {
     "user.geo.country_code": "attr_str[sentry.user.geo.country_code]",
 }
 
-METRICS_SUMMARIES_COLUMN_MAP = {
-    "project": "project_id",
-    "project.id": "project_id",
-    "id": "span_id",
-    "trace": "trace_id",
-    "metric": "metric_mri",
-    "timestamp": "end_timestamp",
-    "segment.id": "segment_id",
-    "span.duration": "duration_ms",
-    "span.group": "group",
-    "min_metric": "min",
-    "max_metric": "max",
-    "sum_metric": "sum",
-    "count_metric": "count",
-}
-
 SPAN_COLUMN_MAP.update(
     {col.value.alias: col.value.spans_name for col in Columns if col.value.spans_name is not None}
 )
@@ -302,7 +286,6 @@ DATASETS: dict[Dataset, dict[str, str]] = {
     Dataset.Discover: DISCOVER_COLUMN_MAP,
     Dataset.Sessions: SESSIONS_SNUBA_MAP,
     Dataset.Metrics: METRICS_COLUMN_MAP,
-    Dataset.MetricsSummaries: METRICS_SUMMARIES_COLUMN_MAP,
     Dataset.PerformanceMetrics: METRICS_COLUMN_MAP,
     Dataset.SpansIndexed: SPAN_COLUMN_MAP,
     Dataset.EventsAnalyticsPlatform: SPAN_EAP_COLUMN_MAP,
@@ -321,7 +304,6 @@ DATASET_FIELDS = {
     Dataset.IssuePlatform: list(ISSUE_PLATFORM_MAP.values()),
     Dataset.SpansIndexed: list(SPAN_COLUMN_MAP.values()),
     Dataset.EventsAnalyticsPlatform: list(SPAN_EAP_COLUMN_MAP.values()),
-    Dataset.MetricsSummaries: list(METRICS_SUMMARIES_COLUMN_MAP.values()),
 }
 
 SNUBA_OR = "or"
@@ -501,7 +483,13 @@ class RetrySkipTimeout(urllib3.Retry):
     """
 
     def increment(
-        self, method=None, url=None, response=None, error=None, _pool=None, _stacktrace=None
+        self,
+        method=None,
+        url=None,
+        response=None,
+        error=None,
+        _pool=None,
+        _stacktrace=None,
     ):
         """
         Just rely on the parent class unless we have a read timeout. In that case
@@ -640,7 +628,9 @@ def get_organization_id_from_project_ids(project_ids: Sequence[int]) -> int:
     return organization_id
 
 
-def infer_project_ids_from_related_models(filter_keys: Mapping[str, Sequence[int]]) -> list[int]:
+def infer_project_ids_from_related_models(
+    filter_keys: Mapping[str, Sequence[int]],
+) -> list[int]:
     ids = [set(get_related_project_ids(k, filter_keys[k])) for k in filter_keys]
     return list(set.union(*ids))
 
@@ -960,7 +950,10 @@ def raw_snql_query(
     # other functions do here. It does not add any automatic conditions, format
     # results, nothing. Use at your own risk.
     return bulk_snuba_queries(
-        requests=[request], referrer=referrer, use_cache=use_cache, query_source=query_source
+        requests=[request],
+        referrer=referrer,
+        use_cache=use_cache,
+        query_source=query_source,
     )[0]
 
 
@@ -1099,7 +1092,9 @@ def _apply_cache_and_build_results(
         for result, (query_pos, _, opt_cache_key) in zip(query_results, to_query):
             if opt_cache_key:
                 cache.set(
-                    opt_cache_key, json.dumps(result), settings.SENTRY_SNUBA_CACHE_TTL_SECONDS
+                    opt_cache_key,
+                    json.dumps(result),
+                    settings.SENTRY_SNUBA_CACHE_TTL_SECONDS,
                 )
             results.append((query_pos, result))
 
@@ -1168,7 +1163,8 @@ def _bulk_snuba_query(snuba_requests: Sequence[SnubaRequest]) -> ResultSet:
             except ValueError:
                 if response.status != 200:
                     logger.exception(
-                        "snuba.query.invalid-json", extra={"response.data": response.data}
+                        "snuba.query.invalid-json",
+                        extra={"response.data": response.data},
                     )
                     raise SnubaError("Failed to parse snuba error response")
                 raise UnexpectedResponseError(f"Could not decode JSON response: {response.data!r}")
@@ -1445,7 +1441,6 @@ def resolve_column(dataset) -> Callable:
 
         # Some dataset specific logic:
         if dataset == Dataset.Discover:
-
             if isinstance(col, (list, tuple)) or col in ("project_id", "group_id"):
                 return col
         elif dataset == Dataset.EventsAnalyticsPlatform:
@@ -1828,7 +1823,11 @@ def get_snuba_translators(filter_keys, is_grouprelease=False):
     reverse = compose(
         reverse,
         lambda row: (
-            replace(row, "bucketed_end", int(parse_datetime(row["bucketed_end"]).timestamp()))
+            replace(
+                row,
+                "bucketed_end",
+                int(parse_datetime(row["bucketed_end"]).timestamp()),
+            )
             if "bucketed_end" in row
             else row
         ),
