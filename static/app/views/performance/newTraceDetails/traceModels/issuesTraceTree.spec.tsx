d3d0d21821c7315b218333fd2ae7b059247ea33c
JonasBa <jonas@badalic.com>
2024-11-22 11:23:55 -0500

    feat(trace) collapse span tree
    ref(trace) implement span sub tree collapsing

--- static/app/views/performance/newTraceDetails/traceModels/issuesTraceTree.spec.tsx
+++ static/app/views/performance/newTraceDetails/traceModels/issuesTraceTree.spec.tsx
@@ -1,5 +1,18 @@
+import {OrganizationFixture} from 'sentry-fixture/organization';
+
+import {EntryType} from 'sentry/types/event';
+import {isTransactionNode} from 'sentry/views/performance/newTraceDetails/traceGuards';
+import {TraceTree} from 'sentry/views/performance/newTraceDetails/traceModels/traceTree';
+import {DEFAULT_TRACE_VIEW_PREFERENCES} from 'sentry/views/performance/newTraceDetails/traceState/tracePreferences';
+
 import {IssuesTraceTree} from './issuesTraceTree';
-import {makeTrace, makeTraceError, makeTransaction} from './traceTreeTestUtils';
+import {
+  makeEventTransaction,
+  makeSpan,
+  makeTrace,
+  makeTraceError,
+  makeTransaction,
+} from './traceTreeTestUtils';
 
 const traceWithErrorInMiddle = makeTrace({
   transactions: [
@@ -34,7 +47,10 @@ const traceWithSiblingCollapsedNodes = makeTrace({
       children: [
         makeTransaction({}),
         makeTransaction({}),
-        makeTransaction({transaction: 'transaction 2', errors: [makeTraceError({})]}),
+        makeTransaction({
+          transaction: 'transaction 2',
+          errors: [makeTraceError({})],
+        }),
         makeTransaction({}),
       ],
     }),
@@ -42,6 +58,20 @@ const traceWithSiblingCollapsedNodes = makeTrace({
     makeTransaction({transaction: 'transaction 4'}),
   ],
 });
+
+function mockSpansResponse(
+  spans: TraceTree.Span[],
+  project_slug: string,
+  event_id: string
+): jest.Mock<any, any> {
+  return MockApiClient.addMockResponse({
+    url: `/organizations/org-slug/events/${project_slug}:${event_id}/?averageColumn=span.self_time&averageColumn=span.duration`,
+    method: 'GET',
+    body: makeEventTransaction({
+      entries: [{type: EntryType.SPANS, data: spans}],
+    }),
+  });
+}
 
 describe('IssuesTraceTree', () => {
   it('collapsed nodes without errors', () => {
@@ -79,5 +109,62 @@ describe('IssuesTraceTree', () => {
     });
 
     expect(tree.build().serialize()).toMatchSnapshot();
+  });
+
+  describe('FromSpans', () => {
+    const traceWithSpans = makeTrace({
+      transactions: [
+        makeTransaction({transaction: 'transaction 0'}),
+        makeTransaction({transaction: 'transaction 0'}),
+        makeTransaction({
+          transaction: 'transaction 1',
+          children: [
+            makeTransaction({
+              transaction: 'transaction 2',
+              event_id: 'event-id',
+              project_slug: 'project',
+              errors: [
+                makeTraceError({
+                  span: 'error-span-id',
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    });
+
+    it('collapses spans', async () => {
+      const tree = IssuesTraceTree.FromTrace(traceWithSpans, {
+        meta: null,
+        replay: null,
+      });
+
+      mockSpansResponse(
+        [
+          makeSpan({op: 'cache', description: 'GET'}),
+          makeSpan({op: 'http', description: 'GET /'}),
+          makeSpan({op: 'db', description: 'SELECT'}),
+          makeSpan({op: 'cache', description: 'GET'}),
+          makeSpan({op: 'http', description: 'GET /', span_id: 'error-span-id'}),
+          makeSpan({op: 'db', description: 'SELECT'}),
+        ],
+        'project',
+        'event-id'
+      );
+
+      const txn = TraceTree.Find(
+        tree.root,
+        node => isTransactionNode(node) && node.value.transaction === 'transaction 2'
+      )!;
+
+      await tree.zoom(txn, true, {
+        api: new MockApiClient(),
+        organization: OrganizationFixture(),
+        preferences: DEFAULT_TRACE_VIEW_PREFERENCES,
+      });
+
+      expect(tree.build().serialize()).toMatchSnapshot();
+    });
   });
 });
