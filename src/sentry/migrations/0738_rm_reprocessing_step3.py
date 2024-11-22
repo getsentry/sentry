3c8f78afbf85e1e951a167e0dda26ee57fc6ed28
Dan Fuller <dfuller@sentry.io>
2024-11-22 13:03:00 -0800

    feat(migrations): Prevent new migrations from using `RunSQL` (#81003)
    This adds in `allow_run_sql` to our `CheckedMigration` baseclass. We
    want to prevent the use of `RunSQL` in most cases going forward, since
    our migration safety framework doesn't know how to parse sql to ensure
    safety.
    
    Our main uses for this in the past were:
    - Adding new columns with a default. We now can do this within the
    framework by using `db_default` instead of `default`.
    - Deleting columns/tables. Since we have to remove from state, deploy,
    then delete we have to use raw sql to remove them from Postgres. I'll
    follow this up with a pr to provide ways to handle these deletions
    within our framework.

--- src/sentry/migrations/0738_rm_reprocessing_step3.py
+++ src/sentry/migrations/0738_rm_reprocessing_step3.py
@@ -20,6 +20,8 @@ class Migration(CheckedMigration):
 
     is_post_deployment = False
 
+    allow_run_sql = True
+
     dependencies = [
         ("sentry", "0737_add_discover_saved_query_dataset_source"),
     ]
