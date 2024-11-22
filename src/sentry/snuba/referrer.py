b753e8df64bcf5949301e9562c03ea90c55db5ef
Pierre Massat <pierre.massat@sentry.io>
2024-11-22 13:10:36 -0500

    chore(metrics-summaries): Remove metrics summaries queries and endpoints (#81133)

--- src/sentry/snuba/referrer.py
+++ src/sentry/snuba/referrer.py
@@ -165,12 +165,8 @@ class Referrer(Enum):
     API_ORGANIZATION_METRICS_DATA = "api.organization.metrics-data"
     API_ORGANIZATION_METRICS_ESTIMATION_STATS = "api.organization-metrics-estimation-stats"
     API_ORGANIZATION_METRICS_METADATA_FETCH_SPANS = "api.organization.metrics-metadata.fetch-spans"
-    API_ORGANIZATION_METRICS_METADATA_FETCH_METRICS_SUMMARIES = (
-        "api.organization.metrics-metadata.fetch-metrics-summaries"
-    )
     API_ORGANIZATION_METRICS_QUERY = "api.organization.metrics-query"
     API_ORGANIZATION_METRICS_EAP_QUERY = "api.organization.metrics-eap-query"
-    API_ORGANIZATION_METRICS_SAMPLES = "api.organization.metrics-samples"
     API_ORGANIZATION_ISSUE_REPLAY_COUNT = "api.organization-issue-replay-count"
     API_ORGANIZATION_SDK_UPDATES = "api.organization-sdk-updates"
     API_ORGANIZATION_SPANS_HISTOGRAM_MIN_MAX = "api.organization-spans-histogram-min-max"
