96e21077cb804b823a1764ac1d5745f67b35d2b9
Mia Hsu <ameliahsu@gmail.com>
2024-11-25 12:47:23 -0800

    clean up test file

--- tests/sentry/sentry_apps/services/test_region_app.py
+++ tests/sentry/sentry_apps/services/test_region_app.py
@@ -9,13 +9,13 @@ from sentry.utils.sentry_apps import SentryAppWebhookRequestsBuffer
 @django_db_all(transaction=True)
 @control_silo_test(regions=create_test_regions("us"))
 class TestRegionApp(TestCase):
-    def test_get_buffer_requests_for_region(self):
-        user = Factories.create_user()
-        org = Factories.create_organization(owner=user, region="us")
-        app = Factories.create_sentry_app(
+    def setUp(self):
+        self.user = Factories.create_user()
+        self.org = Factories.create_organization(owner=self.user, region="us")
+        self.app = Factories.create_sentry_app(
             name="demo-app",
-            user=user,
-            organization=org,
+            user=self.user,
+            organization=self.org,
             published=True,
             schema={
                 "elements": [
@@ -31,95 +31,54 @@ class TestRegionApp(TestCase):
             },
         )
 
-        buffer = SentryAppWebhookRequestsBuffer(app)
+    def test_get_buffer_requests_for_region(self):
+        buffer = SentryAppWebhookRequestsBuffer(self.app)
         buffer.add_request(
             response_code=200,
-            org_id=org.id,
+            org_id=self.org.id,
             event="issue.assigned",
-            url=app.webhook_url,
+            url=self.app.webhook_url,
         )
         requests = region_app_service.get_buffer_requests_for_region(
-            sentry_app=app, region_name="us"
+            sentry_app=self.app, region_name="us"
         )
         assert len(requests) == 1
-        assert requests[0].organization_id == org.id
+        assert requests[0].organization_id == self.org.id
 
-    def test_get_buffer_requests_for_region_error(self):
-        user = Factories.create_user()
-        org = Factories.create_organization(owner=user, region="us")
-        app = Factories.create_sentry_app(
-            name="demo-app",
-            user=user,
-            organization=org,
-            published=True,
-            schema={
-                "elements": [
-                    {
-                        "type": "alert-rule-trigger",
-                        "title": "go beep",
-                        "settings": {
-                            "type": "alert-rule-settings",
-                            "uri": "https://example.com/search/",
-                        },
-                    },
-                ]
-            },
-        )
-
-        buffer = SentryAppWebhookRequestsBuffer(app)
+    def test_get_buffer_requests_for_region_with_error_request(self):
+        buffer = SentryAppWebhookRequestsBuffer(self.app)
         buffer.add_request(
             response_code=200,
-            org_id=org.id,
+            org_id=self.org.id,
             event="issue.assigned",
             url="https://example.com/hook",
             error_id="d5111da2c28645c5889d072017e3445d",
             project_id=1,
         )
         requests = region_app_service.get_buffer_requests_for_region(
-            sentry_app=app, region_name="us"
+            sentry_app=self.app, region_name="us"
         )
         assert len(requests) == 1
         assert requests[0].error_id == "d5111da2c28645c5889d072017e3445d"
         assert requests[0].project_id == 1
 
-    def test_filtered_get_buffer_requests_for_region(self):
-        user = Factories.create_user()
-        org = Factories.create_organization(owner=user, region="us")
-        app = Factories.create_sentry_app(
-            name="demo-app",
-            user=user,
-            organization=org,
-            published=True,
-            schema={
-                "elements": [
-                    {
-                        "type": "alert-rule-trigger",
-                        "title": "go beep",
-                        "settings": {
-                            "type": "alert-rule-settings",
-                            "uri": "https://example.com/search/",
-                        },
-                    },
-                ]
-            },
-        )
-
-        buffer = SentryAppWebhookRequestsBuffer(app)
+    def test_get_filtered_buffer_requests_for_region(self):
+        buffer = SentryAppWebhookRequestsBuffer(self.app)
         buffer.add_request(
             response_code=200,
-            org_id=org.id,
+            org_id=self.org.id,
             event="issue.assigned",
-            url=app.webhook_url,
+            url=self.app.webhook_url,
         )
         buffer.add_request(
             response_code=200,
-            org_id=org.id,
+            org_id=self.org.id,
             event="issue.resolved",
-            url=app.webhook_url,
+            url=self.app.webhook_url,
         )
         filter: SentryAppRequestFilterArgs = {"event": "issue.assigned"}
         requests = region_app_service.get_buffer_requests_for_region(
-            sentry_app=app, region_name="us", filter=filter
+            sentry_app=self.app, region_name="us", filter=filter
         )
         assert len(requests) == 1
-        assert requests[0].organization_id == org.id
+        assert requests[0].organization_id == self.org.id
