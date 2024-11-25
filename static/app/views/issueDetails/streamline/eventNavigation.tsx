9674edd00a1055dfe894af7f40bba6622e843497
Scott Cooper <scttcper@gmail.com>
2024-11-25 13:00:45 -0800

    fix(issues): Animate dropdown chevrons, button sizes (#81262)

--- static/app/views/issueDetails/streamline/eventNavigation.tsx
+++ static/app/views/issueDetails/streamline/eventNavigation.tsx
@@ -286,7 +286,7 @@ export function IssueEventNavigation({event, group, query}: IssueEventNavigation
             },
           ]}
           offset={[-2, 1]}
-          trigger={triggerProps =>
+          trigger={(triggerProps, isOpen) =>
             hideDropdownButton ? (
               <NavigationLabel>
                 {TabName[currentTab] ?? TabName[Tab.DETAILS]}
@@ -294,6 +294,7 @@ export function IssueEventNavigation({event, group, query}: IssueEventNavigation
             ) : (
               <NavigationDropdownButton
                 {...triggerProps}
+                isOpen={isOpen}
                 borderless
                 size="sm"
                 disabled={hideDropdownButton}
