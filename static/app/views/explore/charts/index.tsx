e338b6aa19922c29a1c9296ea265e8d268b70961
Nar Saynorath <nar.saynorath@sentry.io>
2024-11-25 11:46:46 -0500

    feat(explore): Convert buttons into context menu (#81219)
    Convert the buttons for alerts and add to dashboards into a context
    menu. Shifts over a bunch of the code for both the alerts and dashboards
    buttons into a context menu component that's specific for explore
    charts.
    
    I needed to extract the logic for getting the sort option to a helper
    function I could use in my callback. This is because visualizes is a
    list of items and we map over it for charts, meaning I can't use a hook
    to reference the correct chart at the point of instantiation, it needs
    to be used in a callback that's defined during that mapping.

--- static/app/views/explore/charts/index.tsx
+++ static/app/views/explore/charts/index.tsx
@@ -2,13 +2,11 @@ import type {Dispatch, SetStateAction} from 'react';
 import {Fragment, useCallback, useEffect, useMemo} from 'react';
 import styled from '@emotion/styled';
 
-import Feature from 'sentry/components/acl/feature';
 import {getInterval} from 'sentry/components/charts/utils';
 import {CompactSelect} from 'sentry/components/compactSelect';
-import {DropdownMenu} from 'sentry/components/dropdownMenu';
 import {Tooltip} from 'sentry/components/tooltip';
 import {CHART_PALETTE} from 'sentry/constants/chartPalette';
-import {IconClock, IconGraph, IconSubscribed} from 'sentry/icons';
+import {IconClock, IconGraph} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {dedupeArray} from 'sentry/utils/dedupeArray';
@@ -18,12 +16,9 @@ import {
   prettifyParsedFunction,
 } from 'sentry/utils/discover/fields';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
-import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
-import useProjects from 'sentry/utils/useProjects';
 import {formatVersion} from 'sentry/utils/versions/formatVersion';
-import {Dataset} from 'sentry/views/alerts/rules/metric/types';
-import {AddToDashboardButton} from 'sentry/views/explore/components/addToDashboardButton';
+import ChartContextMenu from 'sentry/views/explore/components/chartContextMenu';
 import {useChartInterval} from 'sentry/views/explore/hooks/useChartInterval';
 import {useDataset} from 'sentry/views/explore/hooks/useDataset';
 import {useVisualizes} from 'sentry/views/explore/hooks/useVisualizes';
@@ -33,7 +28,6 @@ import Chart, {
 } from 'sentry/views/insights/common/components/chart';
 import ChartPanel from 'sentry/views/insights/common/components/chartPanel';
 import {useSortedTimeSeries} from 'sentry/views/insights/common/queries/useSortedTimeSeries';
-import {getAlertsUrl} from 'sentry/views/insights/common/utils/getAlertsUrl';
 import {CHART_HEIGHT} from 'sentry/views/insights/database/settings';
 
 import {useGroupBys} from '../hooks/useGroupBys';
@@ -67,9 +61,6 @@ export const EXPLORE_CHART_GROUP = 'explore-charts_group';
 // TODO: Update to support aggregate mode and multiple queries / visualizations
 export function ExploreCharts({query, setError}: ExploreChartsProps) {
   const pageFilters = usePageFilters();
-  const organization = useOrganization();
-  const {projects} = useProjects();
-
   const [dataset] = useDataset();
   const [visualizes, setVisualizes] = useVisualizes();
   const [interval, setInterval, intervalOptions] = useChartInterval();
@@ -184,29 +175,6 @@ export function ExploreCharts({query, setError}: ExploreChartsProps) {
               ? 'area'
               : 'bar';
 
-        const project =
-          projects.length === 1
-            ? projects[0]
-            : projects.find(p => p.id === `${pageFilters.selection.projects[0]}`);
-        const singleProject =
-          (pageFilters.selection.projects.length === 1 || projects.length === 1) &&
-          project;
-        const alertsUrls = singleProject
-          ? visualizeYAxes.map(yAxis => ({
-              key: yAxis,
-              label: yAxis,
-              to: getAlertsUrl({
-                project,
-                query,
-                pageFilters: pageFilters.selection,
-                aggregate: yAxis,
-                orgSlug: organization.slug,
-                dataset: Dataset.EVENTS_ANALYTICS_PLATFORM,
-                interval,
-              }),
-            }))
-          : undefined;
-
         const data = getSeries(dedupedYAxes, formattedYAxes);
 
         const outputTypes = new Set(
@@ -251,32 +219,12 @@ export function ExploreCharts({query, setError}: ExploreChartsProps) {
                     options={intervalOptions}
                   />
                 </Tooltip>
-                <Feature features="organizations:alerts-eap">
-                  <Tooltip
-                    title={
-                      singleProject
-                        ? t('Create an alert for this chart')
-                        : t('Cannot create an alert when multiple projects are selected')
-                    }
-                  >
-                    <DropdownMenu
-                      triggerProps={{
-                        'aria-label': t('Create Alert'),
-                        size: 'sm',
-                        borderless: true,
-                        showChevron: false,
-                        icon: <IconSubscribed />,
-                      }}
-                      position="bottom-end"
-                      items={alertsUrls ?? []}
-                      menuTitle={t('Create an alert for')}
-                      isDisabled={!alertsUrls || alertsUrls.length === 0}
-                    />
-                  </Tooltip>
-                </Feature>
-                <Feature features="organizations:dashboards-eap">
-                  <AddToDashboardButton visualizeIndex={index} />
-                </Feature>
+                <ChartContextMenu
+                  visualizeYAxes={visualizeYAxes}
+                  query={query}
+                  interval={interval}
+                  visualizeIndex={index}
+                />
               </ChartHeader>
               <Chart
                 height={CHART_HEIGHT}
