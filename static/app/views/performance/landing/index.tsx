b6addf5b48cc9a36fb5a4b2c2677ad0ab43d2927
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-11-25 11:34:58 -0500

    remove insight domain view flag 1

--- static/app/views/performance/landing/index.tsx
+++ static/app/views/performance/landing/index.tsx
@@ -95,7 +95,6 @@ export function PerformanceLanding(props: Props) {
   const {setPageInfo, pageAlert} = usePageAlert();
   const {teams, initiallyLoaded} = useTeams({provideUserTeams: true});
   const {slug} = organization;
-  const hasDomainViews = organization.features.includes('insights-domain-view');
 
   const performanceMovingAlert = useMemo(() => {
     if (!slug) {
@@ -127,11 +126,7 @@ export function PerformanceLanding(props: Props) {
   const landingDisplay = paramLandingDisplay ?? defaultLandingDisplayForProjects;
   const showOnboarding = onboardingProject !== undefined;
 
-  if (
-    hasDomainViews &&
-    performanceMovingAlert &&
-    pageAlert?.message !== performanceMovingAlert
-  ) {
+  if (performanceMovingAlert && pageAlert?.message !== performanceMovingAlert) {
     setPageInfo(performanceMovingAlert);
   }
 
