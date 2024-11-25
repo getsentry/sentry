a6c45ebf0259215b55ae8bd022bfb273306d2995
Nar Saynorath <nar.saynorath@sentry.io>
2024-11-25 11:46:46 -0500

    feat(explore): Convert buttons into context menu (#81219)
    Convert the buttons for alerts and add to dashboards into a context
    menu. Shifts over a bunch of the code for both the alerts and dashboards
    buttons into a context menu component that's specific for explore
    charts.
    
    I needed to extract the logic for getting the sort option to a helper
    function I could use in my callback. This is because visualizes is a
    list of items and we map over it for charts, meaning I can't use a hook
    to reference the correct chart at the point of instantiation, it needs
    to be used in a callback that's defined during that mapping.

--- static/app/views/explore/hooks/useAddToDashboard.spec.tsx
+++ static/app/views/explore/hooks/useAddToDashboard.spec.tsx
@@ -2,7 +2,7 @@ import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import {openAddToDashboardModal} from 'sentry/actionCreators/modal';
 import {DisplayType, WidgetType} from 'sentry/views/dashboards/types';
-import {AddToDashboardButton} from 'sentry/views/explore/components/addToDashboardButton';
+import {useAddToDashboard} from 'sentry/views/explore/hooks/useAddToDashboard';
 import {useResultMode} from 'sentry/views/explore/hooks/useResultsMode';
 import {useVisualizes} from 'sentry/views/explore/hooks/useVisualizes';
 import {ChartType} from 'sentry/views/insights/common/components/chart';
@@ -10,6 +10,11 @@ import {ChartType} from 'sentry/views/insights/common/components/chart';
 jest.mock('sentry/actionCreators/modal');
 jest.mock('sentry/views/explore/hooks/useVisualizes');
 jest.mock('sentry/views/explore/hooks/useResultsMode');
+
+function TestPage({visualizeIndex}: {visualizeIndex: number}) {
+  const {addToDashboard} = useAddToDashboard();
+  return <button onClick={() => addToDashboard(visualizeIndex)}>Add to Dashboard</button>;
+}
 
 describe('AddToDashboardButton', () => {
   beforeEach(() => {
@@ -29,15 +34,10 @@ describe('AddToDashboardButton', () => {
     jest.mocked(useResultMode).mockReturnValue(['samples', jest.fn()]);
   });
 
-  it('renders', async () => {
-    render(<AddToDashboardButton visualizeIndex={0} />);
-    await userEvent.hover(screen.getByLabelText('Add to Dashboard'));
-    expect(await screen.findByText('Add to Dashboard')).toBeInTheDocument();
-  });
-
   it('opens the dashboard modal with the correct query for samples mode', async () => {
-    render(<AddToDashboardButton visualizeIndex={0} />);
-    await userEvent.click(screen.getByLabelText('Add to Dashboard'));
+    render(<TestPage visualizeIndex={0} />);
+
+    await userEvent.click(screen.getByText('Add to Dashboard'));
 
     // The table columns are encoded as the fields for the defaultWidgetQuery
     expect(openAddToDashboardModal).toHaveBeenCalledWith(
@@ -107,8 +107,8 @@ describe('AddToDashboardButton', () => {
       jest.fn(),
     ]);
 
-    render(<AddToDashboardButton visualizeIndex={1} />);
-    await userEvent.click(screen.getByLabelText('Add to Dashboard'));
+    render(<TestPage visualizeIndex={1} />);
+    await userEvent.click(screen.getByText('Add to Dashboard'));
 
     // The group by and the yAxes are encoded as the fields for the defaultTableQuery
     expect(openAddToDashboardModal).toHaveBeenCalledWith(
@@ -163,8 +163,8 @@ describe('AddToDashboardButton', () => {
   it('uses the yAxes for the aggregate mode', async () => {
     jest.mocked(useResultMode).mockReturnValue(['aggregate', jest.fn()]);
 
-    render(<AddToDashboardButton visualizeIndex={0} />);
-    await userEvent.click(screen.getByLabelText('Add to Dashboard'));
+    render(<TestPage visualizeIndex={0} />);
+    await userEvent.click(screen.getByText('Add to Dashboard'));
 
     expect(openAddToDashboardModal).toHaveBeenCalledWith(
       expect.objectContaining({
@@ -219,8 +219,8 @@ describe('AddToDashboardButton', () => {
       jest.fn(),
     ]);
 
-    render(<AddToDashboardButton visualizeIndex={0} />);
-    await userEvent.click(screen.getByLabelText('Add to Dashboard'));
+    render(<TestPage visualizeIndex={0} />);
+    await userEvent.click(screen.getByText('Add to Dashboard'));
 
     expect(openAddToDashboardModal).toHaveBeenCalledWith(
       expect.objectContaining({
