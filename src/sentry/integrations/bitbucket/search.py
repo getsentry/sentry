ae56714dc63dce6983d8eed0173fd0175cc62e0c
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2024-11-25 09:37:14 -0800

    chore(integrations): SourceCodeSearchEndpoint metrics (#80956)
    Add metrics to `handle_search_issues`, `handle_search_repositories`, and
    `get`.

--- src/sentry/integrations/bitbucket/search.py
+++ src/sentry/integrations/bitbucket/search.py
@@ -9,6 +9,10 @@ from sentry.api.base import control_silo_endpoint
 from sentry.integrations.bitbucket.integration import BitbucketIntegration
 from sentry.integrations.models.integration import Integration
 from sentry.integrations.source_code_management.issues import SourceCodeIssueIntegration
+from sentry.integrations.source_code_management.metrics import (
+    SCMIntegrationInteractionType,
+    SourceCodeSearchEndpointHaltReason,
+)
 from sentry.integrations.source_code_management.search import SourceCodeSearchEndpoint
 from sentry.shared_integrations.exceptions import ApiError
 
@@ -37,32 +41,37 @@ class BitbucketSearchEndpoint(SourceCodeSearchEndpoint):
         return BitbucketIntegration
 
     def handle_search_issues(self, installation: T, query: str, repo: str | None) -> Response:
-        assert repo
+        with self.record_event(
+            SCMIntegrationInteractionType.HANDLE_SEARCH_ISSUES
+        ).capture() as lifecycle:
+            assert repo
 
-        full_query = f'title~"{query}"'
-        try:
-            response = installation.search_issues(query=full_query, repo=repo)
-        except ApiError as e:
-            if "no issue tracker" in str(e):
-                logger.info(
-                    "bitbucket.issue-search-no-issue-tracker",
-                    extra={"installation_id": installation.model.id, "repo": repo},
-                )
-                return Response(
-                    {"detail": "Bitbucket Repository has no issue tracker."}, status=400
-                )
-            raise
+            full_query = f'title~"{query}"'
+            try:
+                response = installation.search_issues(query=full_query, repo=repo)
+            except ApiError as e:
+                if "no issue tracker" in str(e):
+                    lifecycle.record_halt(str(SourceCodeSearchEndpointHaltReason.NO_ISSUE_TRACKER))
+                    logger.info(
+                        "bitbucket.issue-search-no-issue-tracker",
+                        extra={"installation_id": installation.model.id, "repo": repo},
+                    )
+                    return Response(
+                        {"detail": "Bitbucket Repository has no issue tracker."}, status=400
+                    )
+                raise
 
-        assert isinstance(response, dict)
-        return Response(
-            [
-                {"label": "#{} {}".format(i["id"], i["title"]), "value": i["id"]}
-                for i in response.get("values", [])
-            ]
-        )
+            assert isinstance(response, dict)
+            return Response(
+                [
+                    {"label": "#{} {}".format(i["id"], i["title"]), "value": i["id"]}
+                    for i in response.get("values", [])
+                ]
+            )
 
     def handle_search_repositories(
         self, integration: Integration, installation: T, query: str
     ) -> Response:
-        result = installation.get_repositories(query)
-        return Response([{"label": i["name"], "value": i["name"]} for i in result])
+        with self.record_event(SCMIntegrationInteractionType.HANDLE_SEARCH_REPOSITORIES).capture():
+            result = installation.get_repositories(query)
+            return Response([{"label": i["name"], "value": i["name"]} for i in result])
--- src/sentry/integrations/vsts/search.py
+++ src/sentry/integrations/vsts/search.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 
 from sentry.api.base import control_silo_endpoint
 from sentry.integrations.source_code_management.issues import SourceCodeIssueIntegration
+from sentry.integrations.source_code_management.metrics import SCMIntegrationInteractionType
 from sentry.integrations.source_code_management.search import SourceCodeSearchEndpoint
 from sentry.integrations.vsts.integration import VstsIntegration
 
@@ -21,17 +22,18 @@ class VstsSearchEndpoint(SourceCodeSearchEndpoint):
         return VstsIntegration
 
     def handle_search_issues(self, installation: T, query: str, repo: str | None) -> Response:
-        if not query:
-            return Response([])
+        with self.record_event(SCMIntegrationInteractionType.HANDLE_SEARCH_ISSUES).capture():
+            if not query:
+                return Response([])
 
-        assert isinstance(installation, self.installation_class)
-        resp = installation.search_issues(query=query)
-        return Response(
-            [
-                {
-                    "label": f'({i["fields"]["system.id"]}) {i["fields"]["system.title"]}',
-                    "value": i["fields"]["system.id"],
-                }
-                for i in resp.get("results", [])
-            ]
-        )
+            assert isinstance(installation, self.installation_class)
+            resp = installation.search_issues(query=query)
+            return Response(
+                [
+                    {
+                        "label": f'({i["fields"]["system.id"]}) {i["fields"]["system.title"]}',
+                        "value": i["fields"]["system.id"],
+                    }
+                    for i in resp.get("results", [])
+                ]
+            )
