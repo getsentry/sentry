74fc24560fbb598cd26b79a38ba99fd9b5fc0375
jenn.muengtaweepongsa <jenn.muengtaweepongsa@sentry.io>
2024-11-26 15:52:30 -0800

    more ts

--- static/app/components/events/autofix/types.ts
+++ static/app/components/events/autofix/types.ts
@@ -73,7 +73,7 @@ export type AutofixData = {
 export type AutofixProgressItem = {
   message: string;
   timestamp: string;
-  type: AutofixStatus;
+  type: 'INFO' | 'WARNING' | 'ERROR' | 'NEED_MORE_INFORMATION';
   data?: any;
 };
 
