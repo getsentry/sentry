8deb7d77a4b5c9ea001d0d317de0f9f6f7376d2b
Scott Cooper <scttcper@gmail.com>
2024-11-26 10:49:55 -0800

    feat(releases): Remove withRepositories, withReleaseRepos (#81222)

--- static/app/views/releases/detail/commitsAndFiles/emptyState.tsx
+++ static/app/views/releases/detail/commitsAndFiles/emptyState.tsx
@@ -1,12 +1,17 @@
+import {LinkButton} from 'sentry/components/button';
+import EmptyMessage from 'sentry/components/emptyMessage';
 import EmptyStateWarning from 'sentry/components/emptyStateWarning';
+import {Body, Main} from 'sentry/components/layouts/thirds';
 import Panel from 'sentry/components/panels/panel';
 import PanelBody from 'sentry/components/panels/panelBody';
+import {IconCommit} from 'sentry/icons';
+import {t} from 'sentry/locale';
 
 type Props = {
   children: React.ReactNode;
 };
 
-function EmptyState({children}: Props) {
+export function EmptyState({children}: Props) {
   return (
     <Panel>
       <PanelBody>
@@ -18,4 +23,41 @@ function EmptyState({children}: Props) {
   );
 }
 
-export default EmptyState;
+export function NoReleaseRepos() {
+  return (
+    <Body>
+      <Main fullWidth>
+        <Panel dashedBorder>
+          <EmptyMessage
+            icon={<IconCommit size="xl" />}
+            title={t('Releases are better with commit data!')}
+            description={t('No commits associated with this release have been found.')}
+          />
+        </Panel>
+      </Main>
+    </Body>
+  );
+}
+
+export function NoRepositories({orgSlug}: {orgSlug: string}) {
+  return (
+    <Body>
+      <Main fullWidth>
+        <Panel dashedBorder>
+          <EmptyMessage
+            icon={<IconCommit size="xl" />}
+            title={t('Releases are better with commit data!')}
+            description={t(
+              'Connect a repository to see commit info, files changed, and authors involved in future releases.'
+            )}
+            action={
+              <LinkButton priority="primary" to={`/settings/${orgSlug}/repos/`}>
+                {t('Connect a repository')}
+              </LinkButton>
+            }
+          />
+        </Panel>
+      </Main>
+    </Body>
+  );
+}
