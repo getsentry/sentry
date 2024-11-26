8cf7b593e1a528db9389354abb4e65d9b7f6e9cd
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-11-26 15:36:11 -0800

    remove testutils folder, didn't add much

--- tests/sentry/workflow_engine/processors/test_workflow.py
+++ tests/sentry/workflow_engine/processors/test_workflow.py
@@ -1,6 +1,6 @@
 from sentry.workflow_engine.processors.workflow import process_workflows
 from sentry.workflow_engine.types import DetectorType
-from tests.sentry.workflow_engine.testutils.test_base import BaseWorkflowTest
+from tests.sentry.workflow_engine.test_base import BaseWorkflowTest
 
 
 class TestProcessWorkflows(BaseWorkflowTest):
