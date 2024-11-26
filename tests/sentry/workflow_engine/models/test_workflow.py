790f15590781e9052a608fe90e48069588ec2bc6
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-11-26 15:30:42 -0800

    make a base test class that can create all the different models a setup situations for us. upate the model / processor tests to use this base test

--- tests/sentry/workflow_engine/models/test_workflow.py
+++ tests/sentry/workflow_engine/models/test_workflow.py
@@ -1,51 +1,29 @@
-from unittest import mock
-
-from sentry.testutils.cases import TestCase
-from sentry.workflow_engine.models import Workflow
-from sentry.workflow_engine.types import DetectorPriorityLevel
+from sentry.workflow_engine.types import DetectorType
+from tests.sentry.workflow_engine.testutils.test_base import BaseWorkflowTest
 
 
-class WorkflowTest(TestCase):
+class WorkflowTest(BaseWorkflowTest):
     def setUp(self):
-        self.org = self.create_organization()
-        self.project = self.create_project(organization=self.org)
-        self.detector = self.create_detector(project=self.project)
-        self.workflow = Workflow.objects.create(name="test_workflow", organization=self.org)
+        self.workflow, self.detector, self.detector_workflow, self.data_condition_group = (
+            self.create_detector_and_workflow(detector_type=DetectorType.ERROR)
+        )
+        self.data_condition = self.data_condition_group.conditions.first()
+        self.group, self.event, self.group_event = self.create_group_event()
 
-    def tearDown(self):
-        self.org.delete()
-
-    def test_evaluate_trigger_conditions__passes(self):
-        evaluation = self.workflow.evaluate_trigger_conditions(DetectorPriorityLevel.HIGH)
+    def test_evaluate_trigger_conditions__condition_new_event__True(self):
+        evaluation = self.workflow.evaluate_trigger_conditions(self.group_event)
         assert evaluation is True
 
-    def test_evaluate_trigger_conditions__fails(self):
-        evaluation = self.workflow.evaluate_trigger_conditions(DetectorPriorityLevel.OK)
+    def test_evaluate_trigger_conditions__condition_new_event__False(self):
+        # Update event to have been seen before
+        self.group_event.group.times_seen = 5
+
+        evaluation = self.workflow.evaluate_trigger_conditions(self.group_event)
         assert evaluation is False
 
     def test_evaluate_trigger_conditions__no_conditions(self):
         self.workflow.when_condition_group = None
         self.workflow.save()
 
-        evaluation = self.workflow.evaluate_trigger_conditions(DetectorPriorityLevel.LOW)
-        assert evaluation is True
-
-    def test_evaluate_trigger_conditions__invalid_condition(self):
-        self.data_condition.condition = "invalid"
-        self.data_condition.save()
-
-        with mock.patch("sentry.workflow_engine.models.data_condition.logger") as mock_logger:
-            evaluation = self.workflow.evaluate_trigger_conditions(2)
-            assert evaluation is False
-            assert mock_logger.exception.call_args[0][0] == "Invalid condition"
-
-    def test_evaluate_trigger_conditions__many_conditions(self):
-        self.create_data_condition(
-            condition_group=self.data_condition_group,
-            condition="gte",
-            comparison=DetectorPriorityLevel.LOW,
-            condition_result=DetectorPriorityLevel.LOW,
-        )
-
-        evaluation = self.workflow.evaluate_trigger_conditions(DetectorPriorityLevel.HIGH)
+        evaluation = self.workflow.evaluate_trigger_conditions(self.group_event)
         assert evaluation is True
