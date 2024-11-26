9a4077bc93ea4c185911ae52aad39e9684c85920
harshithadurai <harshi.durai@esentry.io>
2024-11-26 09:13:00 -0500

    remove changes

--- static/app/views/dashboards/editAccessSelector.tsx
+++ static/app/views/dashboards/editAccessSelector.tsx
@@ -1,8 +1,8 @@
 import {useState} from 'react';
 import styled from '@emotion/styled';
+import debounce from 'lodash/debounce';
 import isEqual from 'lodash/isEqual';
 
-import {hasEveryAccess} from 'sentry/components/acl/access';
 import AvatarList from 'sentry/components/avatar/avatarList';
 import TeamAvatar from 'sentry/components/avatar/teamAvatar';
 import Badge from 'sentry/components/badge/badge';
@@ -14,12 +14,13 @@ import {CheckWrap} from 'sentry/components/compactSelect/styles';
 import UserBadge from 'sentry/components/idBadge/userBadge';
 import {InnerWrap, LeadingItems} from 'sentry/components/menuListItem';
 import {Tooltip} from 'sentry/components/tooltip';
+import {DEFAULT_DEBOUNCE_DURATION} from 'sentry/constants';
 import {t, tct} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Team} from 'sentry/types/organization';
 import type {User} from 'sentry/types/user';
 import {defined} from 'sentry/utils';
-import useOrganization from 'sentry/utils/useOrganization';
+import {useTeams} from 'sentry/utils/useTeams';
 import {useTeamsById} from 'sentry/utils/useTeamsById';
 import {useUser} from 'sentry/utils/useUser';
 import type {DashboardDetails, DashboardPermissions} from 'sentry/views/dashboards/types';
@@ -36,14 +37,16 @@ interface EditAccessSelectorProps {
 function EditAccessSelector({dashboard, onChangeEditAccess}: EditAccessSelectorProps) {
   const currentUser: User = useUser();
   const dashboardCreator: User | undefined = dashboard.createdBy;
-  const organization = useOrganization();
-  const userCanEditDashboardPermissions =
-    dashboardCreator?.id === currentUser.id ||
-    hasEveryAccess(['org:write'], {organization});
-  const {teams} = useTeamsById();
-  const teamIds: string[] = Object.values(teams).map(team => team.id);
+  const isCurrentUserDashboardOwner = dashboardCreator?.id === currentUser.id;
+
+  // Retrieves teams from the team store, which may contain only a subset of all teams
+  const {teams: teamsToRender} = useTeamsById();
+  const {onSearch} = useTeams();
+  const teamIds: string[] = Object.values(teamsToRender).map(team => team.id);
+
   const [selectedOptions, setSelectedOptions] = useState<string[]>(getSelectedOptions());
   const [isMenuOpen, setMenuOpen] = useState<boolean>(false);
+  const {teams: selectedTeam} = useTeamsById({ids: [selectedOptions[1]]});
 
   // Handles state change when dropdown options are selected
   const onSelectOptions = newSelectedOptions => {
@@ -136,10 +139,11 @@ function EditAccessSelector({dashboard, onChangeEditAccess}: EditAccessSelectorP
         key="avatar-list-2-badges"
         typeAvatars="users"
         users={[dashboardCreator]}
-        teams={[teams.find(team => team.id === selectedOptions[1])!]}
+        teams={selectedTeam ? selectedTeam : []}
         maxVisibleAvatars={1}
         avatarSize={25}
         renderUsersFirst
+        tooltipOptions={{disabled: !isCurrentUserDashboardOwner}}
       />
     ) : (
       // Case where we display 1 Creator Avatar + a Badge with no. of teams selected
@@ -149,6 +153,7 @@ function EditAccessSelector({dashboard, onChangeEditAccess}: EditAccessSelectorP
         users={Array(selectedOptions.length).fill(dashboardCreator)}
         maxVisibleAvatars={1}
         avatarSize={25}
+        tooltipOptions={{disabled: !isCurrentUserDashboardOwner}}
       />
     );
 
@@ -160,16 +165,16 @@ function EditAccessSelector({dashboard, onChangeEditAccess}: EditAccessSelectorP
         {
           value: '_allUsers',
           label: t('All users'),
-          disabled: !userCanEditDashboardPermissions,
+          disabled: !isCurrentUserDashboardOwner,
         },
       ],
     },
     {
       value: '_teams',
       label: t('Teams'),
-      options: teams.map(makeTeamOption),
-      showToggleAllButton: userCanEditDashboardPermissions,
-      disabled: !userCanEditDashboardPermissions,
+      options: teamsToRender.map(makeTeamOption),
+      showToggleAllButton: isCurrentUserDashboardOwner,
+      disabled: !isCurrentUserDashboardOwner,
     },
   ];
 
@@ -184,7 +189,7 @@ function EditAccessSelector({dashboard, onChangeEditAccess}: EditAccessSelectorP
             setSelectedOptions(getSelectedOptions());
           }
         }}
-        disabled={!userCanEditDashboardPermissions}
+        disabled={!isCurrentUserDashboardOwner}
       >
         {t('Cancel')}
       </Button>
@@ -204,7 +209,7 @@ function EditAccessSelector({dashboard, onChangeEditAccess}: EditAccessSelectorP
         }}
         priority="primary"
         disabled={
-          !userCanEditDashboardPermissions ||
+          !isCurrentUserDashboardOwner ||
           isEqual(getDashboardPermissions(), dashboard.permissions)
         }
       >
@@ -224,13 +229,14 @@ function EditAccessSelector({dashboard, onChangeEditAccess}: EditAccessSelectorP
       options={allDropdownOptions}
       value={selectedOptions}
       triggerLabel={[
-        t('Edit Access:'),
-        triggerAvatars,
-        <FeatureBadge
+        <StyledFeatureBadge
           key="beta-badge"
           type="beta"
           title={t('This feature is available for early adopters and may change')}
+          tooltipProps={{position: 'left', delay: 1000, isHoverable: true}}
         />,
+        t('Edit Access:'),
+        triggerAvatars,
       ]}
       searchPlaceholder={t('Search Teams')}
       isOpen={isMenuOpen}
@@ -241,13 +247,15 @@ function EditAccessSelector({dashboard, onChangeEditAccess}: EditAccessSelectorP
         }
       }}
       menuFooter={dropdownFooterButtons}
+      onSearch={debounce(val => void onSearch(val), DEFAULT_DEBOUNCE_DURATION)}
     />
   );
 
-  return userCanEditDashboardPermissions ? (
-    dropdownMenu
-  ) : (
-    <Tooltip title={t('Only the creator of the dashboard can edit permissions')}>
+  return (
+    <Tooltip
+      title={t('Only the creator of the dashboard can edit permissions')}
+      disabled={isCurrentUserDashboardOwner || isMenuOpen}
+    >
       {dropdownMenu}
     </Tooltip>
   );
@@ -276,6 +284,11 @@ const StyledDisplayName = styled('div')`
 const StyledAvatarList = styled(AvatarList)`
   margin-left: 10px;
   margin-right: -3px;
+`;
+
+const StyledFeatureBadge = styled(FeatureBadge)`
+  margin-left: 0px;
+  margin-right: 6px;
 `;
 
 const StyledBadge = styled(Badge)`
