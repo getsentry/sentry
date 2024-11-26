1b8ce5705dc8971fafa9f51aca105df449f1f864
Nar Saynorath <nar.saynorath@sentry.io>
2024-11-26 14:52:57 -0500

    fix(dashboards): Remove fullscreen from Add to Dashboard (#81330)
    The add to dashboard modal shouldn't have the fullscreen option. Also
    disable it in the widget builder.

--- static/app/views/dashboards/widgetBuilder/buildSteps/visualizationStep.tsx
+++ static/app/views/dashboards/widgetBuilder/buildSteps/visualizationStep.tsx
@@ -146,6 +146,7 @@ export function VisualizationStep({
               : undefined
           }
           widgetLegendState={widgetLegendState}
+          disableFullscreen
         />
 
         <IndexedEventsSelectionAlert widget={widget} />
