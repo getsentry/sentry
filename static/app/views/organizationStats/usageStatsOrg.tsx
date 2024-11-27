6256e690fc0a4deca1f799f28d96ae9cf0fcbe6e
Fabian Schindler <fabian.schindler@sentry.io>
2024-11-27 10:28:34 +0100

    feat(dynamic-sampling): add link from accepted span card to dynamic sampling settings if applicable (#81311)
    In Stats add a link from the "accepted spans" card to the dynamic
    sampling settings if dynamic sampling was applied
    
    
    Closes https://github.com/getsentry/projects/issues/140

--- static/app/views/organizationStats/usageStatsOrg.tsx
+++ static/app/views/organizationStats/usageStatsOrg.tsx
@@ -5,6 +5,7 @@ import isEqual from 'lodash/isEqual';
 import moment from 'moment-timezone';
 
 import {navigateTo} from 'sentry/actionCreators/navigation';
+import {LinkButton} from 'sentry/components/button';
 import type {TooltipSubLabel} from 'sentry/components/charts/components/tooltip';
 import OptionSelector from 'sentry/components/charts/optionSelector';
 import {InlineContainer, SectionHeading} from 'sentry/components/charts/styles';
@@ -20,6 +21,7 @@ import type {ScoreCardProps} from 'sentry/components/scoreCard';
 import ScoreCard from 'sentry/components/scoreCard';
 import SwitchButton from 'sentry/components/switchButton';
 import {DEFAULT_STATS_PERIOD} from 'sentry/constants';
+import {IconSettings} from 'sentry/icons';
 import {t, tct} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {DataCategoryInfo, IntervalPeriod} from 'sentry/types/core';
@@ -389,7 +391,7 @@ class UsageStatsOrganization<
         score: accepted,
         trend:
           dataCategoryApiName === 'span' && accepted_stored ? (
-            t('%s stored', accepted_stored)
+            <SpansStored organization={organization} acceptedStored={accepted_stored} />
           ) : (
             <UsageStatsPerMin
               dataCategoryApiName={dataCategoryApiName}
@@ -608,3 +610,35 @@ const FooterDate = styled('div')`
     font-size: ${p => p.theme.fontSizeMedium};
   }
 `;
+
+type SpansStoredProps = {
+  acceptedStored: string;
+  organization: Organization;
+};
+
+const StyledSettingsButton = styled(LinkButton)`
+  top: 2px;
+`;
+
+const StyledTextWrapper = styled('div')`
+  min-height: 22px;
+`;
+
+function SpansStored({organization, acceptedStored}: SpansStoredProps) {
+  return (
+    <StyledTextWrapper>
+      {t('%s stored', acceptedStored)}{' '}
+      {organization.access.includes('org:read') &&
+        hasDynamicSamplingCustomFeature(organization) && (
+          <StyledSettingsButton
+            borderless
+            size="zero"
+            icon={<IconSettings color="subText" />}
+            title={t('Dynamic Sampling Settings')}
+            aria-label={t('Dynamic Sampling Settings')}
+            to={`/settings/${organization.slug}/dynamic-sampling/`}
+          />
+        )}
+    </StyledTextWrapper>
+  );
+}
