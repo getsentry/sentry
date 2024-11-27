eaed1d0e95b29caa5e35232ba88077a486b1ef75
Nar Saynorath <nar.saynorath@sentry.io>
2024-11-27 14:12:00 -0500

    feat(widget-builder): Add feature flag for redesign (#81377)

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -110,6 +110,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:dashboards-share", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable favouriting dashboards
     manager.add("organizations:dashboards-favourite", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    # Enable the dashboard widget builder redesign UI
+    manager.add("organizations:dashboards-widget-builder-redesign", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable the dev toolbar PoC code for employees
     # Data Secrecy
     manager.add("organizations:data-secrecy", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
