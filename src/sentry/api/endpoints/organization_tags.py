f65dd3f9e465c670a7c846326f8e1bd080b19066
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-11-27 14:04:45 -0500

    feat(visibility): Clamp date range for `TagStore` queries (#81363)
    Follow-up and complement to
    https://github.com/getsentry/sentry/pull/80332. In short, `SAMPLE`
    wasn't enough. For longer ranges (i.e., >30d) fetching a list of project
    tags still times out.
    
    In this PR, we're adding time range clamping for fetching tag keys. We
    will only ever query a maximum of N days (14 for now, but it's
    configurable). If someone chooses to get tags for the last 90 days, they
    will get tags from the last 14 days. If they need tags from November 5th
    - November 10th, we will fetch the range they asked for.
    
    We think this is a reasonable compromise. When tags don't load at all,
    autocomplete stops working, which is very bad UX. Limiting the date
    range to 14 days is a tradeoff. The tags will succeed more often, but
    some tags might be missing.
    
    Also, limiting the range should improve the cache hit ratio a bit, but
    we'll see.
    
    ---------
    
    Co-authored-by: Tony Xiao <txiao@sentry.io>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- src/sentry/api/endpoints/organization_tags.py
+++ src/sentry/api/endpoints/organization_tags.py
@@ -1,16 +1,18 @@
+import datetime
+
 import sentry_sdk
 from rest_framework.exceptions import ParseError
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry import tagstore
+from sentry import features, options, tagstore
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases import NoProjects
 from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.api.serializers import serialize
-from sentry.api.utils import handle_query_errors
+from sentry.api.utils import clamp_date_range, handle_query_errors
 from sentry.snuba.dataset import Dataset
 from sentry.utils.numbers import format_grouped_length
 from sentry.utils.sdk import set_measurement
@@ -39,11 +41,23 @@ class OrganizationTagsEndpoint(OrganizationEndpoint):
 
         with sentry_sdk.start_span(op="tagstore", name="get_tag_keys_for_projects"):
             with handle_query_errors():
+                start = filter_params["start"]
+                end = filter_params["end"]
+
+                if features.has("organizations:tag-key-sample-n", organization) and start and end:
+                    # Tag queries longer than 14 days tend to time out for large customers. For getting a list of tags, clamping to 14 days is a reasonable compromise of speed vs. completeness
+                    (start, end) = clamp_date_range(
+                        (start, end),
+                        datetime.timedelta(
+                            days=options.get("visibility.tag-key-max-date-range.days")
+                        ),
+                    )
+
                 results = tagstore.backend.get_tag_keys_for_projects(
                     filter_params["project_id"],
                     filter_params.get("environment"),
-                    filter_params["start"],
-                    filter_params["end"],
+                    start,
+                    end,
                     use_cache=request.GET.get("use_cache", "0") == "1",
                     dataset=dataset,
                     tenant_ids={"organization_id": organization.id},
