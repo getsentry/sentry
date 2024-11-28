b42ddcc4d127a8ae822b9a4c5e2e4484c888c3dc
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-11-28 15:28:57 -0500

    Add y-axis label

--- static/app/views/dashboards/widgets/lineChartWidget/lineChartWidgetVisualization.tsx
+++ static/app/views/dashboards/widgets/lineChartWidget/lineChartWidgetVisualization.tsx
@@ -142,6 +142,9 @@ export function LineChartWidgetVisualization(props: LineChartWidgetVisualization
         left: 0,
       }}
       tooltip={{
+        axisPointer: {
+          type: 'cross',
+        },
         formatter,
         valueFormatter: value => {
           return formatChartValue(value, type, unit);
@@ -151,6 +154,17 @@ export function LineChartWidgetVisualization(props: LineChartWidgetVisualization
         axisLabel: {
           formatter(value: number) {
             return formatChartValue(value, type, unit);
+          },
+        },
+        axisPointer: {
+          type: 'line',
+          snap: false,
+          lineStyle: {
+            type: 'solid',
+            width: 0.5,
+          },
+          label: {
+            show: false,
           },
         },
       }}
