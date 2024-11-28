c01aec7b68dd74098b14c71190a421e78d734e9a
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2024-11-28 10:26:55 +0100

    fix: flatten searchable os distribution fields (#81297)
    <!-- Describe your PR here. -->
    update to https://github.com/getsentry/sentry/pull/69865
    To search the distribution fields, we cannot have them in a hierarchy
    `os.distribution.name`, we instead need them flattened to
    `os.distribution_name`
    
    This is part of reworking the following native-SDK PR:
    https://github.com/getsentry/sentry-native/pull/963
    
    The update is also tracked on the docs side:
    https://github.com/getsentry/sentry-docs/pull/11936
    And on Relay: https://github.com/getsentry/relay/pull/4292

--- src/sentry/rules/conditions/event_attribute.py
+++ src/sentry/rules/conditions/event_attribute.py
@@ -63,8 +63,8 @@ ATTR_CHOICES = {
     "stacktrace.package": Columns.STACK_PACKAGE,
     "unreal.crashtype": Columns.UNREAL_CRASH_TYPE,
     "app.in_foreground": Columns.APP_IN_FOREGROUND,
-    "os.distribution.name": Columns.OS_DISTRIBUTION_NAME,
-    "os.distribution.version": Columns.OS_DISTRIBUTION_VERSION,
+    "os.distribution_name": Columns.OS_DISTRIBUTION_NAME,
+    "os.distribution_version": Columns.OS_DISTRIBUTION_VERSION,
 }
 
 
@@ -418,21 +418,14 @@ class AppAttributeHandler(AttributeHandler):
 
 @attribute_registry.register("os")
 class OsAttributeHandler(AttributeHandler):
-    minimum_path_length = 3
+    minimum_path_length = 2
 
     @classmethod
     def _handle(cls, path: list[str], event: GroupEvent) -> list[str]:
-        if path[1] in ("distribution"):
-            if path[2] in ("name", "version"):
-                contexts = event.data.get("contexts", {})
-                os_context = contexts.get("os")
-                if os_context is None:
-                    os_context = {}
-
-                distribution = os_context.get(path[1])
-                if distribution is None:
-                    distribution = {}
-
-                return [distribution.get(path[2])]
-            return []
+        if path[1] in ("distribution_name", "distribution_version"):
+            contexts = event.data.get("contexts", {})
+            os_context = contexts.get("os")
+            if os_context is None:
+                os_context = {}
+            return [os_context.get(path[1])]
         return []
