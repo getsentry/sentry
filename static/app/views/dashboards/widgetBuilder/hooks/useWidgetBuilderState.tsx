7c824a208d9a67104a779ecd00bf1093a47a31dc
Nar Saynorath <nar.saynorath@sentry.io>
2024-11-28 10:28:35 -0500

    wip

--- static/app/views/dashboards/widgetBuilder/hooks/useWidgetBuilderState.tsx
+++ static/app/views/dashboards/widgetBuilder/hooks/useWidgetBuilderState.tsx
@@ -1,15 +1,18 @@
 import {useCallback, useMemo} from 'react';
+import {DisplayType} from 'sentry/views/dashboards/types';
 
 import {useQueryParamState} from 'sentry/views/dashboards/widgetBuilder/hooks/useQueryParamState';
 
 export const BuilderStateAction = {
   SET_TITLE: 'SET_TITLE',
   SET_DESCRIPTION: 'SET_DESCRIPTION',
+  SET_DISPLAY_TYPE: 'SET_DISPLAY_TYPE',
 } as const;
 
 type WidgetAction =
   | {payload: string; type: typeof BuilderStateAction.SET_TITLE}
-  | {payload: string; type: typeof BuilderStateAction.SET_DESCRIPTION};
+  | {payload: string; type: typeof BuilderStateAction.SET_DESCRIPTION}
+  | {payload: DisplayType; type: typeof BuilderStateAction.SET_DISPLAY_TYPE};
 
 interface WidgetBuilderState {
   description?: string;
@@ -22,8 +25,12 @@ function useWidgetBuilderState(): {
 } {
   const [title, setTitle] = useQueryParamState('title');
   const [description, setDescription] = useQueryParamState('description');
+  const [displayType, setDisplayType] = useQueryParamState('displayType');
 
-  const state = useMemo(() => ({title, description}), [title, description]);
+  const state = useMemo(
+    () => ({title, description, displayType}),
+    [title, description, displayType]
+  );
 
   const dispatch = useCallback(
     (action: WidgetAction) => {
@@ -34,11 +41,14 @@ function useWidgetBuilderState(): {
         case BuilderStateAction.SET_DESCRIPTION:
           setDescription(action.payload);
           break;
+        case BuilderStateAction.SET_DISPLAY_TYPE:
+          setDisplayType(action.payload);
+          break;
         default:
           break;
       }
     },
-    [setTitle, setDescription]
+    [setTitle, setDescription, setDisplayType]
   );
 
   return {
