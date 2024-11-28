647e5de0d39d82efe84dabfbe0498a05935bc9e6
Dominik Buszowiecki <DominikB2014@gmail.com>
2024-11-28 13:56:44 -0500

    fix tests

--- static/app/views/insights/common/components/moduleUpsellHookWrapper.tsx
+++ static/app/views/insights/common/components/moduleUpsellHookWrapper.tsx
@@ -1,9 +1,5 @@
-import Feature from 'sentry/components/acl/feature';
 import HookOrDefault from 'sentry/components/hookOrDefault';
-import {NoAccess} from 'sentry/components/noAccess';
-import useOrganization from 'sentry/utils/useOrganization';
 import type {TitleableModuleNames} from 'sentry/views/insights/common/components/modulePageProviders';
-import {MODULE_FEATURE_MAP} from 'sentry/views/insights/settings';
 
 // TODO - remove, This is only necessary for domain views, where we don't want to show the full upsell page.
 export function ModuleBodyUpsellHook({
@@ -13,17 +9,9 @@ export function ModuleBodyUpsellHook({
   children: React.ReactNode;
   moduleName: TitleableModuleNames;
 }) {
-  const organization = useOrganization();
-
   return (
     <UpsellPageHook moduleName={moduleName} fullPage={false}>
-      <Feature
-        features={MODULE_FEATURE_MAP[moduleName]}
-        organization={organization}
-        renderDisabled={NoAccess}
-      >
-        {children}
-      </Feature>
+      {children}
     </UpsellPageHook>
   );
 }
