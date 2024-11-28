cb04737b2339030dd4befaf8996946dd5e27296f
Nar Saynorath <nar.saynorath@sentry.io>
2024-11-28 09:41:40 -0500

    feat(widget-builder): Add useWidgetBuilderState hook with title (#81394)
    Exposes a title field in the widget builder. Also adds a feature flagged
    dev page that will be used as a quick sandbox to test this hook more
    broadly with simple UI. The hidden page can be accessed on any dashboard
    page with the feature flag and adding `devBuilder=true` to the URL.

--- static/app/views/dashboards/detail.tsx
+++ static/app/views/dashboards/detail.tsx
@@ -40,6 +40,7 @@ import {MetricsCardinalityProvider} from 'sentry/utils/performance/contexts/metr
 import {MetricsResultsMetaProvider} from 'sentry/utils/performance/contexts/metricsEnhancedPerformanceDataContext';
 import {MEPSettingProvider} from 'sentry/utils/performance/contexts/metricsEnhancedSetting';
 import {OnDemandControlProvider} from 'sentry/utils/performance/contexts/onDemandControl';
+import {decodeScalar} from 'sentry/utils/queryString';
 import normalizeUrl from 'sentry/utils/url/normalizeUrl';
 import withApi from 'sentry/utils/withApi';
 import withOrganization from 'sentry/utils/withOrganization';
@@ -54,6 +55,7 @@ import {
   isWidgetUsingTransactionName,
   resetPageFilters,
 } from 'sentry/views/dashboards/utils';
+import DevBuilder from 'sentry/views/dashboards/widgetBuilder/components/devBuilder';
 import {DataSet} from 'sentry/views/dashboards/widgetBuilder/utils';
 import WidgetLegendNameEncoderDecoder from 'sentry/views/dashboards/widgetLegendNameEncoderDecoder';
 import {MetricsDataSwitcherAlert} from 'sentry/views/performance/landing/metricsDataSwitcherAlert';
@@ -1135,8 +1137,22 @@ class DashboardDetail extends Component<Props, State> {
     );
   }
 
+  /**
+   * This is a temporary component to test the new widget builder hook during development.
+   */
+  renderDevWidgetBuilder() {
+    return <DevBuilder />;
+  }
+
   render() {
-    const {organization} = this.props;
+    const {organization, location} = this.props;
+
+    if (
+      organization.features.includes('dashboards-widget-builder-redesign') &&
+      decodeScalar(location.query?.devBuilder) === 'true'
+    ) {
+      return this.renderDevWidgetBuilder();
+    }
 
     if (this.isWidgetBuilderRouter) {
       return this.renderWidgetBuilder();
