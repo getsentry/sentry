d84965d3f883728eedaaa64549b4c752dc5e76ac
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2024-11-29 11:14:08 -0500

    failign test

--- static/app/components/charts/eventsRequest.spec.tsx
+++ static/app/components/charts/eventsRequest.spec.tsx
@@ -72,13 +72,13 @@ describe('EventsRequest', function () {
       expect(doEventsRequest).toHaveBeenCalled();
     });
 
-    it('sets use RPC param', function () {
+    it('sets use RPC param', async function () {
       render(
         <EventsRequest {...DEFAULTS} useRpc>
           {mock}
         </EventsRequest>
       );
-
+      await waitFor(() => expect(doEventsRequest).toHaveBeenCalledTimes(1));
       expect(doEventsRequest).toHaveBeenCalledWith(
         expect.anything(),
         expect.objectContaining({
