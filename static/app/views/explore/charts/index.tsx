e177910f3d64bcf7bfcd5326a0af3d61569aa5ad
Tony Xiao <txiao@sentry.io>
2024-11-29 12:13:05 -0500

    feat(explore): Support rpc calls for timeseries (#81462)
    This enables RPC support for the timeseries API.

--- static/app/views/explore/charts/index.tsx
+++ static/app/views/explore/charts/index.tsx
@@ -61,7 +61,7 @@ export const EXPLORE_CHART_GROUP = 'explore-charts_group';
 // TODO: Update to support aggregate mode and multiple queries / visualizations
 export function ExploreCharts({query, setError}: ExploreChartsProps) {
   const pageFilters = usePageFilters();
-  const [dataset] = useDataset();
+  const [dataset] = useDataset({allowRPC: true});
   const [visualizes, setVisualizes] = useVisualizes();
   const [interval, setInterval, intervalOptions] = useChartInterval();
   const {groupBys} = useGroupBys();
