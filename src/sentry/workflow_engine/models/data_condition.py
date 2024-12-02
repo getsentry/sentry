11ac48fed336a331a237ad6ce8090d6724ac9876
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-12-02 14:35:21 -0800

    fix type check issue

--- src/sentry/workflow_engine/models/data_condition.py
+++ src/sentry/workflow_engine/models/data_condition.py
@@ -84,7 +84,7 @@ class DataCondition(DefaultFieldsModel):
 
         return None
 
-    def get_condition_handler(self, type_: type[T]) -> DataConditionHandler[T] | None:
+    def get_condition_handler(self, type_: T) -> DataConditionHandler[T] | None:
         try:
             condition_type = Condition(self.type)
         except ValueError:
