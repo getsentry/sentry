52c6d4d5da75e325cbfe78c5de8c981156562704
Nikki Kapadia <72356613+nikkikapadia@users.noreply.github.com>
2024-12-02 12:17:18 -0500

    feat(widget-builder): Add title and description fields (#81497)
    Added the title and description fields to the slideout. I've also
    integrated them in with the hooks. It looks like this so far and we can
    see the url params update 🎉
    
    <img width="1512" alt="image"
    src="https://github.com/user-attachments/assets/541c39dd-8df3-4871-ad34-2e28ec29d8e6">
    
    <img width="1505" alt="image"
    src="https://github.com/user-attachments/assets/b52cbc9d-1594-4481-a7fd-7d4a7c4cae70">
    
    
    closes https://github.com/getsentry/sentry/issues/81502 and closes
    https://github.com/getsentry/sentry/issues/81503

---
+++ static/app/views/dashboards/widgetBuilder/components/nameAndDescFields.tsx
@@ -0,0 +1,76 @@
+import {Fragment, useState} from 'react';
+import styled from '@emotion/styled';
+
+import {Button} from 'sentry/components/button';
+import TextArea from 'sentry/components/forms/controls/textarea';
+import Input from 'sentry/components/input';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import useWidgetBuilderState, {
+  BuilderStateAction,
+} from 'sentry/views/dashboards/widgetBuilder/hooks/useWidgetBuilderState';
+
+function WidgetBuilderNameAndDescription() {
+  const {state, dispatch} = useWidgetBuilderState();
+  const [isDescSelected, setIsDescSelected] = useState(state.description ? true : false);
+
+  return (
+    <Fragment>
+      <Header>{t('Widget Name & Description')}</Header>
+      <StyledInput
+        size="md"
+        placeholder={t('Name')}
+        title={t('Widget Name')}
+        type="text"
+        aria-label={t('Widget Name')}
+        value={state.title}
+        onChange={e => {
+          dispatch({type: BuilderStateAction.SET_TITLE, payload: e.target.value});
+        }}
+      />
+      {!isDescSelected && (
+        <AddDescriptionButton
+          priority="link"
+          aria-label={t('Add Widget Description')}
+          onClick={() => {
+            setIsDescSelected(true);
+          }}
+          data-test-id={'add-description'}
+        >
+          {t('+ Add Widget Description')}
+        </AddDescriptionButton>
+      )}
+      {isDescSelected && (
+        <DescriptionTextArea
+          placeholder={t('Description')}
+          aria-label={t('Widget Description')}
+          autosize
+          rows={4}
+          value={state.description}
+          onChange={e => {
+            dispatch({type: BuilderStateAction.SET_DESCRIPTION, payload: e.target.value});
+          }}
+        />
+      )}
+    </Fragment>
+  );
+}
+
+export default WidgetBuilderNameAndDescription;
+
+const Header = styled('h6')`
+  font-size: ${p => p.theme.fontSizeLarge};
+  margin-bottom: ${space(1)};
+`;
+
+const StyledInput = styled(Input)`
+  margin-bottom: ${space(1)};
+`;
+
+const AddDescriptionButton = styled(Button)`
+  margin-bottom: ${space(1)};
+`;
+
+const DescriptionTextArea = styled(TextArea)`
+  margin: ${space(2)} 0;
+`;
