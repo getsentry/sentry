7f881574c2cd2b0cd89d7a0c6612d32e20bd0e7f
Simon Hellmayr <simon.hellmayr@sentry.io>
2024-12-02 15:59:47 +0100

    move fix to put method

--- tests/sentry/api/endpoints/test_organization_sampling_project_rates.py
+++ tests/sentry/api/endpoints/test_organization_sampling_project_rates.py
@@ -22,9 +22,7 @@ class OrganizationSamplingProjectRatesTest(APITestCase):
     def test_get(self):
         project1 = self.create_project(teams=[self.team])
         project2 = self.create_project(teams=[self.team])
-        project3 = self.create_project(teams=[self.team])
         project2.update_option("sentry:target_sample_rate", 0.2)
-        project3.update_option("sentry:target_sample_rate", 0.123456789)
 
         with self.feature(self.features):
             response = self.get_success_response(self.organization.slug)
@@ -32,7 +30,6 @@ class OrganizationSamplingProjectRatesTest(APITestCase):
         assert response.data == [
             {"id": project1.id, "sampleRate": 1.0},
             {"id": project2.id, "sampleRate": 0.2},
-            {"id": project3.id, "sampleRate": 0.1235},
         ]
 
     def test_put(self):
@@ -40,10 +37,13 @@ class OrganizationSamplingProjectRatesTest(APITestCase):
         project1.update_option("sentry:target_sample_rate", 0.2)
         project2 = self.create_project(teams=[self.team])
         project2.update_option("sentry:target_sample_rate", 0.2)
+        project3 = self.create_project(teams=[self.team])
+        project3.update_option("sentry:target_sample_rate", 0.2)
 
         data = [
             # we leave project 1 unchanged
             {"id": project2.id, "sampleRate": 0.5},
+            {"id": project3.id, "sampleRate": 0.123456789},
         ]
 
         with self.feature(self.features):
@@ -53,10 +53,12 @@ class OrganizationSamplingProjectRatesTest(APITestCase):
 
         assert response.data == [
             {"id": project2.id, "sampleRate": 0.5},
+            {"id": project3.id, "sampleRate": 0.1235},
         ]
 
         assert project1.get_option("sentry:target_sample_rate") == 0.2
         assert project2.get_option("sentry:target_sample_rate") == 0.5
+        assert project3.get_option("sentry:target_sample_rate") == 0.1235
 
     def test_put_automatic_mode(self):
         self.organization.update_option(
