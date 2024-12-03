5a5374ee1850c7cba038d08689635794ecfc97a5
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2024-12-03 07:22:05 -0800

    fix types in unfold

--- src/sentry/spans/consumers/process/factory.py
+++ src/sentry/spans/consumers/process/factory.py
@@ -15,7 +15,15 @@ from arroyo.processing.strategies.batching import BatchStep, ValuesBatch
 from arroyo.processing.strategies.produce import Produce
 from arroyo.processing.strategies.run_task import RunTask
 from arroyo.processing.strategies.unfold import Unfold
-from arroyo.types import FILTERED_PAYLOAD, BrokerValue, Commit, FilteredPayload, Message, Partition
+from arroyo.types import (
+    FILTERED_PAYLOAD,
+    BrokerValue,
+    Commit,
+    FilteredPayload,
+    Message,
+    Partition,
+    Value,
+)
 from sentry_kafka_schemas.codecs import Codec
 from sentry_kafka_schemas.schema_types.snuba_spans_v1 import SpanEvent
 
@@ -196,7 +204,7 @@ def batch_write_to_redis(
 
 def _expand_segments(should_process_segments: list[ProcessSegmentsContext]):
     with sentry_sdk.start_transaction(op="process", name="spans.process.expand_segments") as txn:
-        buffered_segments: list[KafkaPayload | FilteredPayload] = []
+        buffered_segments: list[Value] = []
 
         for result in should_process_segments:
             timestamp = result.timestamp
@@ -235,7 +243,9 @@ def _expand_segments(should_process_segments: list[ProcessSegmentsContext]):
                             metrics.incr("performance.buffered_segments.max_payload_size_exceeded")
                             continue
 
-                        buffered_segments.append(KafkaPayload(None, payload_data, []))
+                        buffered_segments.append(
+                            Value(KafkaPayload(None, payload_data, []), {}, timestamp)
+                        )
 
     return buffered_segments
 
