f064062e85a3c60524f643a5b6ec7581c18932c3
Sebastian Zivota <loewenheim@users.noreply.github.com>
2024-12-03 18:37:47 +0100

    ref: Improve symbolication platform logic (#81577)
    There are a number of discrepancies between native, JS, and JVM
    regarding how events and frames that should be symbolicated are
    detected. For example, native and JVM check the `"platform"` field of
    frames, but JS doesn't. Conversely, JVM doesn't check it for events.
    
    This PR aims to align the logic between the platforms so that platforms
    are checked everywhere. We have metrics in Symbolicator showing that we
    don't get any frames where at least one of the platforms isn't set, but
    we _do_ rarely get frames with nonsensical platforms (Java, Lua, …) in
    the JS case, likely because the check is too lenient.
    
    One open question is whether it is possible for an event to contain
    JVM/Java images, but not have a platform of `"java"`. We now emit a
    counter for cases like this.

--- tests/relay_integration/lang/java/test_plugin.py
+++ tests/relay_integration/lang/java/test_plugin.py
@@ -8,6 +8,7 @@ from django.urls import reverse
 
 from sentry.models.debugfile import ProjectDebugFile
 from sentry.models.files.file import File
+from sentry.stacktraces.processing import find_stacktraces_in_data
 from sentry.testutils.cases import TransactionTestCase
 from sentry.testutils.helpers.datetime import before_now
 from sentry.testutils.relay import RelayStoreHelper
@@ -1585,7 +1586,9 @@ class BasicResolvingIntegrationTest(RelayStoreHelper, TransactionTestCase):
             },
             "timestamp": before_now(seconds=1),
         }
-        assert is_jvm_event(event)
+
+        stacktraces = find_stacktraces_in_data(event)
+        assert is_jvm_event(event, stacktraces)
 
         event = {
             "user": {"ip_address": "31.172.207.97"},
@@ -1615,7 +1618,8 @@ class BasicResolvingIntegrationTest(RelayStoreHelper, TransactionTestCase):
             "timestamp": before_now(seconds=1),
         }
         # has no platform
-        assert is_jvm_event(event)
+        stacktraces = find_stacktraces_in_data(event)
+        assert is_jvm_event(event, stacktraces)
 
         event = {
             "user": {"ip_address": "31.172.207.97"},
@@ -1646,4 +1650,68 @@ class BasicResolvingIntegrationTest(RelayStoreHelper, TransactionTestCase):
             "timestamp": before_now(seconds=1),
         }
         # has no modules
-        assert not is_jvm_event(event)
+        stacktraces = find_stacktraces_in_data(event)
+        assert is_jvm_event(event, stacktraces)
+
+        event = {
+            "user": {"ip_address": "31.172.207.97"},
+            "extra": {},
+            "project": self.project.id,
+            "debug_meta": {},
+            "exception": {
+                "values": [
+                    {
+                        "stacktrace": {
+                            "frames": [
+                                {
+                                    "platform": "java",
+                                    "function": "whoops4",
+                                    "abs_path": "SourceFile",
+                                    "module": "io.sentry.samples.MainActivity$OneMoreInnerClass",
+                                    "filename": "SourceFile",
+                                    "lineno": 38,
+                                },
+                            ]
+                        },
+                        "module": "io.sentry.samples",
+                        "type": "RuntimeException",
+                        "value": "whoops",
+                    }
+                ]
+            },
+            "timestamp": before_now(seconds=1),
+        }
+        # has a Java frame
+        stacktraces = find_stacktraces_in_data(event)
+        assert is_jvm_event(event, stacktraces)
+
+        event = {
+            "user": {"ip_address": "31.172.207.97"},
+            "extra": {},
+            "project": self.project.id,
+            "debug_meta": {},
+            "exception": {
+                "values": [
+                    {
+                        "stacktrace": {
+                            "frames": [
+                                {
+                                    "function": "whoops4",
+                                    "abs_path": "SourceFile",
+                                    "module": "io.sentry.samples.MainActivity$OneMoreInnerClass",
+                                    "filename": "SourceFile",
+                                    "lineno": 38,
+                                },
+                            ]
+                        },
+                        "module": "io.sentry.samples",
+                        "type": "RuntimeException",
+                        "value": "whoops",
+                    }
+                ]
+            },
+            "timestamp": before_now(seconds=1),
+        }
+        # has no platform, frame, or modules
+        stacktraces = find_stacktraces_in_data(event)
+        assert not is_jvm_event(event, stacktraces)
