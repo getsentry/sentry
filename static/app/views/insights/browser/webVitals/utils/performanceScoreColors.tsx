4eb51c54a82a2f814b556866725cd19375f811e7
Ash <0Calories@users.noreply.github.com>
2024-12-03 11:50:23 -0500

    fix(insights): Styling of Web Vitals cards (#81585)
    Touches up the styling of the web vitals cards so they are consistent
    with what is found in the designs
    
    ## Before
    
    ![image](https://github.com/user-attachments/assets/17afcea7-4ea5-4f07-a75b-ea962e1d53aa)
    
    ## After
    
    ![image](https://github.com/user-attachments/assets/f1743288-bd82-4f1e-bb85-32b0c3712092)

--- static/app/views/insights/browser/webVitals/utils/performanceScoreColors.tsx
+++ static/app/views/insights/browser/webVitals/utils/performanceScoreColors.tsx
@@ -2,17 +2,21 @@ export const PERFORMANCE_SCORE_COLORS = {
   good: {
     light: 'green100',
     normal: 'green300',
+    border: 'green200',
   },
   needsImprovement: {
     light: 'yellow100',
     normal: 'yellow400',
+    border: 'yellow200',
   },
   bad: {
     light: 'red100',
     normal: 'red300',
+    border: 'red200',
   },
   none: {
     light: 'gray100',
     normal: 'gray300',
+    border: 'gray200',
   },
 };
