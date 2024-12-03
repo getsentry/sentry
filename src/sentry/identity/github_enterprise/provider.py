c0f07cbb51e42dd2fc73111cb7ee8efb1ba00ed7
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-12-03 13:01:33 -0500

    ref: remove non-functional github enterprise identity provider (#79753)
    this would always crash if used, has never worked
    
    <!-- Describe your PR here. -->

--- src/sentry/identity/github_enterprise/provider.py
+++ src/sentry/identity/github_enterprise/provider.py
@@ -1,21 +1,6 @@
-from django.core.exceptions import PermissionDenied
+from typing import NoReturn
 
-from sentry import http
 from sentry.identity.oauth2 import OAuth2Provider
-
-
-def get_user_info(url, access_token):
-    with http.build_session() as session:
-        resp = session.get(
-            f"https://{url}/api/v3/user",
-            headers={
-                "Accept": "application/vnd.github.machine-man-preview+json",
-                "Authorization": f"token {access_token}",
-            },
-            verify=False,
-        )
-        resp.raise_for_status()
-    return resp.json()
 
 
 class GitHubEnterpriseIdentityProvider(OAuth2Provider):
@@ -24,19 +9,5 @@ class GitHubEnterpriseIdentityProvider(OAuth2Provider):
 
     oauth_scopes = ()
 
-    def build_identity(self, data):
-        data = data["data"]
-        access_token = data.get("access_token")
-        if not access_token:
-            raise PermissionDenied()
-
-        # todo(meredith): this doesn't work yet, need to pass in the base url
-        user = get_user_info(access_token)
-
-        return {
-            "type": "github_enterprise",
-            "id": user["id"],
-            "email": user["email"],
-            "scopes": [],  # GitHub apps do not have user scopes
-            "data": self.get_oauth_data(data),
-        }
+    def build_identity(self, data: object) -> NoReturn:
+        raise NotImplementedError
