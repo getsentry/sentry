41bc80de869bea47b7bed8f175ca69605678441c
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2024-12-04 13:17:00 -0500

    ref(grouping): Minor refactor for utils (#81657)
    This helps with readability.

--- src/sentry/seer/similarity/utils.py
+++ src/sentry/seer/similarity/utils.py
@@ -1,4 +1,5 @@
 import logging
+from collections.abc import Mapping, Sequence
 from enum import StrEnum
 from typing import Any, TypeVar
 
@@ -172,7 +173,7 @@ class NoFilenameOrModuleException(Exception):
     pass
 
 
-def _get_value_if_exists(exception_value: dict[str, Any]) -> str:
+def _get_value_if_exists(exception_value: Mapping[str, Any]) -> str:
     return exception_value["values"][0] if exception_value.get("values") else ""
 
 
@@ -224,18 +225,14 @@ def get_stacktrace_string(data: dict[str, Any], platform: str | None = None) -> 
         frame_count += len(contributing_frames)
 
         for frame in contributing_frames:
-            frame_dict = {"filename": "", "function": "", "context-line": "", "module": ""}
-            for frame_values in frame.get("values", []):
-                if frame_values.get("id") in frame_dict:
-                    frame_dict[frame_values["id"]] = _get_value_if_exists(frame_values)
+            frame_dict = extract_values_from_frame_values(frame.get("values", []))
+            filename = extract_filename(frame_dict)
 
             if not _is_snipped_context_line(frame_dict["context-line"]):
                 found_non_snipped_context_line = True
 
-            if frame_dict["filename"] == "" and frame_dict["module"] == "":
+            if not filename:
                 has_no_filename_or_module = True
-            elif frame_dict["filename"] == "":
-                frame_dict["filename"] = frame_dict["module"]
 
             # Not an exhaustive list of tests we could run to detect HTML, but this is only
             # meant to be a temporary, quick-and-dirty metric
@@ -245,18 +242,11 @@ def get_stacktrace_string(data: dict[str, Any], platform: str | None = None) -> 
             if frame_dict["filename"].endswith("html") or "<html>" in frame_dict["context-line"]:
                 html_frame_count += 1
 
-            # We want to skip frames with base64 encoded filenames since they can be large
-            # and not contain any usable information
-            base64_encoded = False
-            for base64_prefix in BASE64_ENCODED_PREFIXES:
-                if frame_dict["filename"].startswith(base64_prefix):
-                    base64_encoded = True
-                    break
-            if base64_encoded:
+            if is_base64_encoded_frame(frame_dict):
                 continue
 
             frame_strings.append(
-                f'  File "{frame_dict["filename"]}", function {frame_dict["function"]}\n    {frame_dict["context-line"]}\n'
+                f'  File "{filename}", function {frame_dict["function"]}\n    {frame_dict["context-line"]}\n'
             )
 
         return frame_strings
@@ -330,44 +320,65 @@ def get_stacktrace_string(data: dict[str, Any], platform: str | None = None) -> 
     return stacktrace_str.strip()
 
 
+def extract_values_from_frame_values(values: Sequence[Mapping[str, Any]]) -> dict[str, Any]:
+    frame_dict = {"filename": "", "function": "", "context-line": "", "module": ""}
+    for frame_values in values:
+        if frame_values.get("id") in frame_dict:
+            frame_dict[frame_values["id"]] = _get_value_if_exists(frame_values)
+    return frame_dict
+
+
+def extract_filename(frame_dict: Mapping[str, Any]) -> str:
+    """
+    Extract the filename from the frame dictionary. Fallback to module if filename is not present.
+    """
+    filename = frame_dict["filename"]
+    if filename == "" and frame_dict["module"] != "":
+        filename = frame_dict["module"]
+    return filename
+
+
+def is_base64_encoded_frame(frame_dict: Mapping[str, Any]) -> bool:
+    # We want to skip frames with base64 encoded filenames since they can be large
+    # and not contain any usable information
+    base64_encoded = False
+    for base64_prefix in BASE64_ENCODED_PREFIXES:
+        if frame_dict["filename"].startswith(base64_prefix):
+            base64_encoded = True
+            break
+    return base64_encoded
+
+
 def get_stacktrace_string_with_metrics(
     data: dict[str, Any], platform: str | None, referrer: ReferrerOptions
 ) -> str | None:
+    stacktrace_string = None
+    key = "grouping.similarity.did_call_seer"
+    sample_rate = options.get("seer.similarity.metrics_sample_rate")
     try:
         stacktrace_string = get_stacktrace_string(data, platform)
     except TooManyOnlySystemFramesException:
         platform = platform if platform else "unknown"
         metrics.incr(
             "grouping.similarity.over_threshold_only_system_frames",
-            sample_rate=options.get("seer.similarity.metrics_sample_rate"),
+            sample_rate=sample_rate,
             tags={"platform": platform, "referrer": referrer},
         )
         if referrer == ReferrerOptions.INGEST:
             metrics.incr(
-                "grouping.similarity.did_call_seer",
-                sample_rate=options.get("seer.similarity.metrics_sample_rate"),
-                tags={
-                    "call_made": False,
-                    "blocker": "over-threshold-only-system-frames",
-                },
+                key,
+                sample_rate=sample_rate,
+                tags={"call_made": False, "blocker": "over-threshold-only-system-frames"},
             )
-        stacktrace_string = None
     except NoFilenameOrModuleException:
         if referrer == ReferrerOptions.INGEST:
             metrics.incr(
-                "grouping.similarity.did_call_seer",
-                sample_rate=options.get("seer.similarity.metrics_sample_rate"),
-                tags={
-                    "call_made": False,
-                    "blocker": "no-module-or-filename",
-                },
+                key,
+                sample_rate=sample_rate,
+                tags={"call_made": False, "blocker": "no-module-or-filename"},
             )
-        stacktrace_string = None
-    except Exception as e:
-        logger.exception(
-            "Unexpected exception in stacktrace string formatting", extra={"exception": str(e)}
-        )
-        stacktrace_string = None
+    except Exception:
+        logger.exception("Unexpected exception in stacktrace string formatting")
 
     return stacktrace_string
 
