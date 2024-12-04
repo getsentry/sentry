16e365f74ac8177df80a436359c82648e1058900
Raj Joshi <raj.joshi@sentry.io>
2024-12-03 17:46:37 -0800

    :mag: nit: refactor based on josh's branch

---
+++ src/sentry/workflow_engine/actions/notification_action.py
@@ -0,0 +1,165 @@
+# import uuid
+# from typing import Any
+
+# import sentry_sdk
+
+# from sentry.constants import ObjectStatus
+# from sentry.models.group import GroupEvent
+# from sentry.models.rule import Rule, RuleSource
+# from sentry.models.rulefirehistory import RuleFireHistory
+# from sentry.rules.actions.base import instantiate_action
+# from sentry.types.rules import RuleFuture
+# from sentry.utils.safe import safe_execute
+# from sentry.workflow_engine.models.action import Action
+# from sentry.workflow_engine.models.detector import Detector
+# from sentry.workflow_engine.typings.notification_action import (
+#     ACTION_TYPE_2_INTEGRATION_ID_KEY,
+#     ACTION_TYPE_2_RULE_REGISTRY_ID,
+#     ACTION_TYPE_2_TARGET_DISPLAY_KEY,
+#     ACTION_TYPE_2_TARGET_IDENTIFIER_KEY,
+# )
+
+
+# def build_rule_data_blob(action: Action) -> list[dict[str, Any]]:
+#     """
+#     Builds the Rule.data.actions json blob from the Action model.
+
+#     :param action: Action model instance
+#     :return: list of dicts with the rule data with length 1
+#     """
+
+#     # Copy the action data to the rule data
+#     rule_data = dict(action.data)
+
+#     # Add the rule registry id to the rule data
+#     rule_data["id"] = ACTION_TYPE_2_RULE_REGISTRY_ID[action.type]
+
+#     # Add the action uuid to the rule data
+#     rule_data["uuid"] = action.id
+
+#     # Add the integration id to the rule data
+#     integration_id_key = ACTION_TYPE_2_INTEGRATION_ID_KEY[action.type]
+#     rule_data[integration_id_key] = action.integration_id
+
+#     # Add the target identifier to the rule data if it exists
+#     target_identifier_key = ACTION_TYPE_2_TARGET_IDENTIFIER_KEY.get(action.type)
+#     if target_identifier_key:
+#         rule_data[target_identifier_key] = action.target_identifier
+
+#     # Add the target display to the rule data if it exists
+#     target_display_key = ACTION_TYPE_2_TARGET_DISPLAY_KEY.get(action.type)
+#     if target_display_key:
+#         rule_data[target_display_key] = action.target_display
+
+#     # Because the rule expects "actions" to be a list, we need to return a list with a single dict
+#     return [rule_data]
+
+
+# def create_rule_from_action(action: Action, detector: Detector) -> Rule:
+#     """
+#     Creates a Rule model from the Action model.
+
+#     :param action: Action model instance
+#     :param detector: Detector model instance
+#     :return: Rule model instance
+#     """
+#     rule = Rule(
+#         id=detector.id,
+#         project=detector.project,
+#         label=detector.name,
+#         data={"actions": build_rule_data_blob(action)},
+#         status=ObjectStatus.ACTIVE,
+#         source=RuleSource.ISSUE,
+#     )
+
+#     rule.save()
+
+#     return rule
+
+
+# def deduce_provider_from_action(action: Action) -> str:
+#     """
+#     Deduces the provider from the action.
+
+#     :param action: Action model instance
+#     :return: str
+#     """
+
+
+# def create_rule_fire_history_from_action(
+#     action: Action, detector: Detector, group_event: GroupEvent, rule: Rule, notification_uuid: str
+# ) -> RuleFireHistory:
+#     """
+#     Creates a RuleFireHistory model from the Action model.
+
+#     :param action: Action model instance
+#     :param detector: Detector model instance
+#     :param group_event: GroupEvent model instance
+#     :param rule: Rule model instance
+#     :param notification_uuid: str
+#     :return: RuleFireHistory model instanceq
+#     """
+
+#     rule_fire_history = RuleFireHistory(
+#         project=detector.project,
+#         rule=rule,
+#         event_id=group_event.event_id,
+#         group_id=group_event.group_id,
+#         notification_uuid=notification_uuid,
+#     )
+
+#     rule_fire_history.save()
+
+#     return rule_fire_history
+
+
+# def send_notification_using_rule_registry(
+#     action: Action, detector: Detector, group_event: GroupEvent
+# ):
+#     """
+#     Invokes the issue alert registry (rule registry) and sends a notification.
+
+#     :param action: Action model instance
+#     :param detector: Detector model instance
+#     :param group_event: GroupEvent model instance
+#     """
+
+#     with sentry_sdk.start_span(
+#         op="sentry.workflow_engine.actions.notification_action.create_legacy_rule_registry_models"
+#     ):
+#         # Create a notification uuid
+#         notification_uuid = str(uuid.uuid4())
+
+#         # TODO(iamrajjoshi): Change to a check
+#         assert detector.project == group_event.project
+
+#         # Create a rule
+#         rule = create_rule_from_action(action, detector)
+
+#         # Create a rule fire history
+#         rule_fire_history = create_rule_fire_history_from_action(
+#             action, detector, group_event, rule, notification_uuid
+#         )
+
+#         # TODO(iamrajjoshi): Add a check to see if the rule has only one action
+#         assert len(rule.data.get("actions", [])) == 1
+
+#     # This should only have one action
+#     for action_data in rule.data.get("actions", []):
+#         action_inst = instantiate_action(rule, action_data, rule_fire_history)
+#         if not action_inst:
+#             continue
+
+#         results = safe_execute(
+#             action_inst.after,
+#             event=group_event,
+#             notification_uuid=notification_uuid,
+#         )
+#         if results is None:
+#             # TODO(iamrajjoshi): Log an error
+#             continue
+
+#         for future in results:
+#             rule_future = RuleFuture(rule=rule, kwargs=future.kwargs)
+#             # Send the notification
+#             safe_execute(future.callback, group_event, [rule_future])
--- src/sentry/workflow_engine/handlers/action/notification.py
+++ src/sentry/workflow_engine/handlers/action/notification.py
@@ -1,5 +1,6 @@
 from sentry.eventstore.models import GroupEvent
-from sentry.workflow_engine.models import Action, Detector
+from sentry.workflow_engine.actions.notification_action import send_notification_using_rule_registry
+from sentry.workflow_engine.models import Action, DataSource, DataSourceDetector, Detector
 from sentry.workflow_engine.registry import action_handler_registry
 from sentry.workflow_engine.types import ActionHandler
 
@@ -8,9 +9,39 @@ from sentry.workflow_engine.types import ActionHandler
 class NotificationActionHandler(ActionHandler):
     @staticmethod
     def execute(
-        evt: GroupEvent,
+        self,
+        group_event: GroupEvent,
         action: Action,
         detector: Detector,
     ) -> None:
+        """
+        Sends a notification to the specified integration.
+
+        :param action: Action model instance
+        :param group_event: GroupEvent model instance
+        """
         # TODO: Implment this in milestone 2
-        pass
+
+        # Get the detector_id from the group_event's evidence data
+        # TODO(iamrajjoshi): Add a check to see if the detector_id is valid
+        detector_id = group_event.occurrence.evidence_data.get("detector_id")
+
+        # TODO(iamrajjoshi): Add a check to see if the detector exists
+        detector = Detector.objects.get(id=detector_id)
+        assert isinstance(detector, Detector)
+
+        # TODO(iamrajjoshi): Add a check to see if the detector belongs to the same project as the group_event
+        assert detector.project == group_event.project
+
+        # Determine what registry to use, based on the data source type
+        data_source_detector = DataSourceDetector.objects.get(detector=detector)
+        assert isinstance(data_source_detector, DataSourceDetector)
+
+        data_source = data_source_detector.data_source
+        assert isinstance(data_source, DataSource)
+
+        if data_source.type == "IssueOccurrence":
+            send_notification_using_rule_registry(action, detector, group_event)
+        else:
+            # TODO(iamrajjoshi): Implement the logic to invoke Metric Alert Registry
+            pass
