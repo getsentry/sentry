3b642322c1deec16cfc724b6fcc87b45390af09b
Tony Xiao <txiao@sentry.io>
2024-12-04 15:27:07 -0500

    feat(explore): Compute overall confidence for explore results
    The overall confidence for the explore results is calculated in 2 steps
    1. compute the confidence per timeseries. 25% or more of data points must not be low confidence
    2. compute the overall confidence. 50% or more of the time series must not be low confidence
    
    This is because we assume a few things
    1. the table results are more likely to be high confidence since it's using all data points instead of bucketed by time
    2. 25% roughly corresponds to the 2 out of 7 days for the weekends
    3. all aggregations will have similar confidence because they're using the same data

--- static/app/views/alerts/rules/metric/utils/isLowConfidenceTimeSeries.tsx
+++ static/app/views/alerts/rules/metric/utils/isLowConfidenceTimeSeries.tsx
@@ -1,4 +1,9 @@
-import type {EventsStats, MultiSeriesEventsStats} from 'sentry/types/organization';
+import type {
+  Confidence,
+  EventsStats,
+  MultiSeriesEventsStats,
+} from 'sentry/types/organization';
+import {defined} from 'sentry/utils';
 import {isEventsStats} from 'sentry/views/insights/common/queries/useSortedTimeSeries';
 
 // Returns true if any of the time series data has a low confidence interval
@@ -7,11 +12,71 @@ export function isLowConfidenceTimeSeries(
 ) {
   if (data) {
     if (isEventsStats(data)) {
-      return data.data.some(series => series[1][0]?.confidence === 'LOW');
+      return data.data.some(series => series[1][0]?.confidence === 'low');
     }
     return Object.values(data).some(eventsStats =>
-      eventsStats.data.some(series => series[1][0]?.confidence === 'LOW')
+      eventsStats.data.some(series => series[1][0]?.confidence === 'low')
     );
   }
   return false;
+}
+
+const LOW_CONFIDENCE_THRESHOLD = 0.25;
+
+export function determineSeriesConfidence(
+  data: EventsStats,
+  threshold = LOW_CONFIDENCE_THRESHOLD
+): Confidence {
+  if (!defined(data.confidence) || data.confidence.length < 1) {
+    return null;
+  }
+
+  const perDataUnitConfidence: Confidence[] = data.confidence.map(unit => {
+    return unit[1].reduce(
+      (acc, entry) => combineConfidence(acc, entry.count),
+      null as Confidence
+    );
+  });
+
+  const {lowConfidence, nullConfidence} = perDataUnitConfidence.reduce(
+    (acc, confidence) => {
+      if (confidence === 'low') {
+        acc.lowConfidence += 1;
+      } else if (confidence === 'high') {
+        acc.highConfidence += 1;
+      } else {
+        acc.nullConfidence += 1;
+      }
+      return acc;
+    },
+    {lowConfidence: 0, highConfidence: 0, nullConfidence: 0}
+  );
+
+  const totalEntries = perDataUnitConfidence.length;
+
+  if (nullConfidence === totalEntries) {
+    return null;
+  }
+
+  if (lowConfidence / perDataUnitConfidence.length < threshold) {
+    return 'low';
+  }
+
+  return 'high';
+}
+
+export function combineConfidence(a: Confidence, b: Confidence): Confidence {
+  if (!defined(a)) {
+    return b;
+  }
+
+  if (!defined(b)) {
+    return a;
+  }
+
+  if (a === 'low' || b === 'low') {
+    return 'high';
+  }
+
+  return 'high';
 }
--- static/app/views/explore/charts/index.tsx
+++ static/app/views/explore/charts/index.tsx
@@ -9,12 +9,14 @@ import {CHART_PALETTE} from 'sentry/constants/chartPalette';
 import {IconClock, IconGraph} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
+import type {Confidence} from 'sentry/types/organization';
 import {dedupeArray} from 'sentry/utils/dedupeArray';
 import {
   aggregateOutputType,
   parseFunction,
   prettifyParsedFunction,
 } from 'sentry/utils/discover/fields';
+import {DiscoverDatasets} from 'sentry/utils/discover/types';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import usePrevious from 'sentry/utils/usePrevious';
 import {formatVersion} from 'sentry/utils/versions/formatVersion';
@@ -38,6 +40,7 @@ import {formatSort} from '../tables/aggregatesTable';
 
 interface ExploreChartsProps {
   query: string;
+  setConfidence: Dispatch<SetStateAction<Confidence>>;
   setError: Dispatch<SetStateAction<string>>;
 }
 
@@ -58,8 +61,7 @@ const exploreChartTypeOptions = [
 
 export const EXPLORE_CHART_GROUP = 'explore-charts_group';
 
-// TODO: Update to support aggregate mode and multiple queries / visualizations
-export function ExploreCharts({query, setError}: ExploreChartsProps) {
+export function ExploreCharts({query, setConfidence, setError}: ExploreChartsProps) {
   const [dataset] = useDataset({allowRPC: true});
   const [visualizes, setVisualizes] = useVisualizes();
   const [interval, setInterval, intervalOptions] = useChartInterval();
@@ -138,11 +140,52 @@ export function ExploreCharts({query, setError}: ExploreChartsProps) {
 
   const timeSeriesResult = useSortedTimeSeries(options, 'api.explorer.stats', dataset);
 
+  const resultConfidence = useMemo(() => {
+    if (
+      dataset !== DiscoverDatasets.SPANS_EAP_RPC ||
+      timeSeriesResult.isPending ||
+      timeSeriesResult.error
+    ) {
+      return null;
+    }
+
+    const {lowConfidence, highConfidence, nullConfidence} = Object.values(
+      timeSeriesResult.data
+    ).reduce(
+      (acc, series) => {
+        for (const s of series) {
+          if (s.confidence === 'low') {
+            acc.lowConfidence += 1;
+          } else if (s.confidence === 'high') {
+            acc.highConfidence += 1;
+          } else {
+            acc.nullConfidence += 1;
+          }
+        }
+        return acc;
+      },
+      {lowConfidence: 0, highConfidence: 0, nullConfidence: 0}
+    );
+
+    const total = lowConfidence + highConfidence + nullConfidence;
+    if (lowConfidence / total > 0.5) {
+      return 'low';
+    }
+
+    return 'high';
+  }, [
+    dataset,
+    timeSeriesResult.data,
+    timeSeriesResult.isPending,
+    timeSeriesResult.error,
+  ]);
+
   const previousTimeSeriesResult = usePrevious(timeSeriesResult);
 
   useEffect(() => {
+    setConfidence(resultConfidence);
     setError(timeSeriesResult.error?.message ?? '');
-  }, [setError, timeSeriesResult.error?.message]);
+  }, [setConfidence, setError, resultConfidence, timeSeriesResult.error?.message]);
 
   const getSeries = useCallback(
     (dedupedYAxes: string[], formattedYAxes: (string | undefined)[]) => {
