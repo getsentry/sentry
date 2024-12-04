a29f5fc517a64dbc4a29947f74bfe830a0e7dbf4
Colton Allen <colton.allen@sentry.io>
2024-12-04 11:27:07 -0600

    Catch exceptions

--- src/sentry/utils/flag.py
+++ src/sentry/utils/flag.py
@@ -1,6 +1,9 @@
+import logging
+
 import sentry_sdk
-from sentry_sdk.flag_utils import flag_error_processor
 from sentry_sdk.integrations import Integration
+
+logger = logging.getLogger()
 
 
 class FlagPoleIntegration(Integration):
@@ -12,6 +15,18 @@ class FlagPoleIntegration(Integration):
         scope.add_error_processor(flag_error_processor)
 
 
+def flag_error_processor(event, exc_info):
+    try:
+        scope = sentry_sdk.get_current_scope()
+        event["contexts"]["flags"] = {"values": scope.flags.get()}
+        return event
+    except Exception:
+        logger.exception("Flag serialization failed")
+
+
 def flag_pole_hook(flag: str, result: bool):
-    flags = sentry_sdk.get_current_scope().flags
-    flags.set(flag, result)
+    try:
+        flags = sentry_sdk.get_current_scope().flags
+        flags.set(flag, result)
+    except Exception:
+        logger.exception("Set flag failed")
