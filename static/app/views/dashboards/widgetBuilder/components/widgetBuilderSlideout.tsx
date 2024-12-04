e5279bb4dca6310bf52cc4ff8e51a705c63a126a
nikkikapadia <nikki.kapadia@sentry.io>
2024-12-04 10:31:54 -0500

    add filter search field(s)

--- static/app/views/dashboards/widgetBuilder/components/widgetBuilderSlideout.tsx
+++ static/app/views/dashboards/widgetBuilder/components/widgetBuilderSlideout.tsx
@@ -9,6 +9,7 @@ import WidgetBuilderDatasetSelector from 'sentry/views/dashboards/widgetBuilder/
 import DevBuilder from 'sentry/views/dashboards/widgetBuilder/components/devBuilder';
 import WidgetBuilderFilterBar from 'sentry/views/dashboards/widgetBuilder/components/filtersBar';
 import WidgetBuilderNameAndDescription from 'sentry/views/dashboards/widgetBuilder/components/nameAndDescFields';
+import WidgetBuilderQueryFilterBuilder from 'sentry/views/dashboards/widgetBuilder/components/queryFilterBuilder';
 
 type WidgetBuilderSlideoutProps = {
   isOpen: boolean;
@@ -37,6 +38,9 @@ function WidgetBuilderSlideout({isOpen, onClose}: WidgetBuilderSlideoutProps) {
         </Section>
         <Section>
           <WidgetBuilderDatasetSelector />
+        </Section>
+        <Section>
+          <WidgetBuilderQueryFilterBuilder />
         </Section>
         <Section>
           <WidgetBuilderNameAndDescription />
