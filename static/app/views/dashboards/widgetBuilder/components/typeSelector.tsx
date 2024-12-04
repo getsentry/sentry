38b60c03c8c6553fa201a280bca448c3e70c031b
Nikki Kapadia <72356613+nikkikapadia@users.noreply.github.com>
2024-12-04 11:01:50 -0500

    feat(widget-builder): Add display type dropdown (#81589)
    Added the Display Type dropdown to the slideout. Changing the type also
    updates the URL and is integrated with the widget builder context. It
    looks like this:
    
    
    ![image](https://github.com/user-attachments/assets/c1314247-7b19-4099-aa29-f0efd8a31be2)
    
    Closes https://github.com/getsentry/sentry/issues/81505

---
+++ static/app/views/dashboards/widgetBuilder/components/typeSelector.tsx
@@ -0,0 +1,86 @@
+import {Fragment} from 'react';
+import {components} from 'react-select';
+import styled from '@emotion/styled';
+
+import SelectControl from 'sentry/components/forms/controls/selectControl';
+import {Tooltip} from 'sentry/components/tooltip';
+import {IconGraph, IconNumber, IconTable} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {DisplayType} from 'sentry/views/dashboards/types';
+import {useWidgetBuilderContext} from 'sentry/views/dashboards/widgetBuilder/contexts/widgetBuilderContext';
+import {BuilderStateAction} from 'sentry/views/dashboards/widgetBuilder/hooks/useWidgetBuilderState';
+
+const typeIcons = {
+  [DisplayType.AREA]: <IconGraph key="area" type="area" />,
+  [DisplayType.BAR]: <IconGraph key="bar" type="bar" />,
+  [DisplayType.LINE]: <IconGraph key="line" type="line" />,
+  [DisplayType.TABLE]: <IconTable key="table" />,
+  [DisplayType.BIG_NUMBER]: <IconNumber key="number" />,
+};
+
+const displayTypes = {
+  [DisplayType.AREA]: t('Area'),
+  [DisplayType.BAR]: t('Bar'),
+  [DisplayType.LINE]: t('Line'),
+  [DisplayType.TABLE]: t('Table'),
+  [DisplayType.BIG_NUMBER]: t('Big Number'),
+};
+
+function WidgetBuilderTypeSelector() {
+  const {state, dispatch} = useWidgetBuilderContext();
+
+  return (
+    <Fragment>
+      <Tooltip
+        title={t('This is the type of visualization (ex. line chart)')}
+        position="right-end"
+        delay={200}
+        isHoverable
+        showUnderline
+      >
+        <Header>{t('Type')}</Header>
+      </Tooltip>
+      <SelectControl
+        name="displayType"
+        value={state.displayType}
+        options={Object.keys(displayTypes).map(value => ({
+          leadingItems: typeIcons[value],
+          label: displayTypes[value],
+          value,
+        }))}
+        clearable={false}
+        onChange={newValue => {
+          dispatch({
+            type: BuilderStateAction.SET_DISPLAY_TYPE,
+            payload: newValue.value,
+          });
+        }}
+        components={{
+          SingleValue: containerProps => {
+            return (
+              <components.SingleValue {...containerProps}>
+                <SelectionWrapper>
+                  {containerProps.data.leadingItems}
+                  {containerProps.children}
+                </SelectionWrapper>
+              </components.SingleValue>
+            );
+          },
+        }}
+      />
+    </Fragment>
+  );
+}
+
+export default WidgetBuilderTypeSelector;
+
+const SelectionWrapper = styled('div')`
+  display: flex;
+  gap: ${space(1)};
+`;
+
+const Header = styled('h6')`
+  font-size: ${p => p.theme.fontSizeLarge};
+  margin-bottom: ${space(1)};
+`;
