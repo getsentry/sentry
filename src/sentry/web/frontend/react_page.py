a48b7f17c91a9ce4ed86b19e8eb67450d9d43e70
Burak Yigit Kaya <byk@sentry.io>
2024-12-05 20:31:38 +0000

    fix those pesky tests

--- src/sentry/web/frontend/react_page.py
+++ src/sentry/web/frontend/react_page.py
@@ -87,6 +87,7 @@ class ReactMixin:
         return domains
 
     def handle_react(self, request: Request, **kwargs) -> HttpResponse:
+        org_context = getattr(self, "active_organization", None)
         context = {
             "CSRF_COOKIE_NAME": settings.CSRF_COOKIE_NAME,
             "meta_tags": [
@@ -98,8 +99,8 @@ class ReactMixin:
             # Rendering the layout requires serializing the active organization.
             # Since we already have it here from the OrganizationMixin, we can
             # save some work and render it faster.
-            "org_context": getattr(self, "active_organization", None),
-            "react_config": get_client_config(request, getattr(self, "active_organization", None)),
+            "org_context": org_context,
+            "react_config": get_client_config(request, org_context),
         }
 
         # Force a new CSRF token to be generated and set in user's
