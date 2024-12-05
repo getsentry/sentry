16d400bd972bf7110b8a72338185e5b7cb5b713e
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-12-04 17:02:44 -0800

    handle exception and add a test

--- src/sentry/workflow_engine/processors/workflow.py
+++ src/sentry/workflow_engine/processors/workflow.py
@@ -1,7 +1,12 @@
+import logging
+
 from sentry.eventstore.models import GroupEvent
-from sentry.workflow_engine.models import Workflow
+from sentry.utils import metrics
+from sentry.workflow_engine.models import Detector, Workflow
 from sentry.workflow_engine.processors.action import evaluate_workflow_action_filters
 from sentry.workflow_engine.processors.detector import get_detector_by_event
+
+logger = logging.getLogger(__name__)
 
 
 def evaluate_workflow_triggers(workflows: set[Workflow], evt: GroupEvent) -> set[Workflow]:
@@ -23,7 +28,12 @@ def process_workflows(evt: GroupEvent) -> set[Workflow]:
     Finally, each of the triggered workflows will have their actions evaluated and executed.
     """
     # Check to see if the GroupEvent has an issue occurrence
-    detector = get_detector_by_event(evt)
+    try:
+        detector = get_detector_by_event(evt)
+    except Detector.DoesNotExist:
+        metrics.incr("workflow_engine.process_workflows.error")
+        logger.exception("Detector not found for event", extra={"event_id": evt.event_id})
+        return set()
 
     workflows = set(Workflow.objects.filter(detectorworkflow__detector_id=detector.id).distinct())
     triggered_workflows = evaluate_workflow_triggers(workflows, evt)
