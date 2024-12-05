b5ffc9e240e98eddec9bdf96b0afa433a98831da
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2024-12-05 14:51:19 -0800

    chore(aci milestone 3): create feature flag for metric alert migration (#81773)
    Create a temporary flag to gate the new metric issue flows.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -539,6 +539,10 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:widget-viewer-modal-minimap", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enabled unresolved issue webhook for organization
     manager.add("organizations:webhooks-unresolved", OrganizationFeature, FeatureHandlerStrategy.OPTIONS, api_expose=True)
+    # Enable dual writing for metric alert issues (see: alerts create issues)
+    manager.add("organizations:workflow-engine-m3-dual-write", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
+    # Enable reading from new ACI tables for metric alert issues (see: alerts create issues)
+    manager.add("organizations:workflow-engine-m3-read", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable new workflow_engine UI (see: alerts create issues)
     manager.add("organizations:workflow-engine-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable EventUniqueUserFrequencyConditionWithConditions special alert condition
