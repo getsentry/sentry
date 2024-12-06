0f1d272bcd1baca6f0e382a21b2f5a2a115ce0da
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-12-05 16:08:22 -0800

    fix(flags): don't show cta if some recent event has flags (#81782)
    relates to https://github.com/getsentry/team-replay/issues/508.
    
    sometimes the recommended event is not necessarily the latest event, so
    the recommended event may not have `event.contexts.flags` set (but the
    latest one does). we don't want to show the flag cta if the issue
    already has flags present in at least 1 recent event.

--- static/app/views/issueDetails/groupEventDetails/groupEventDetails.spec.tsx
+++ static/app/views/issueDetails/groupEventDetails/groupEventDetails.spec.tsx
@@ -160,6 +160,10 @@ const mockGroupApis = (
   trace?: QuickTraceEvent
 ) => {
   MockApiClient.addMockResponse({
+    url: '/organizations/org-slug/issues/1/events/',
+    body: [],
+  });
+  MockApiClient.addMockResponse({
     url: '/organizations/org-slug/flags/logs/',
     body: {data: []},
   });
