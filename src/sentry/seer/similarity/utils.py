754232d80eec3ffecf4c2ab1b0c1b7619224b591
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-12-09 13:30:56 -0800

    chore(similarity): Add more platforms to similarity eligible (#81871)
    Add cordova, ionic, and perl to eligible seer grouping platforms

--- src/sentry/seer/similarity/utils.py
+++ src/sentry/seer/similarity/utils.py
@@ -33,6 +33,7 @@ SEER_ELIGIBLE_PLATFORMS_EVENTS = frozenset(
 SYSTEM_FRAME_CHECK_BLACKLIST_PLATFORMS = frozenset(
     [
         "bun",
+        "cordova",
         "deno",
         "django",
         "go",
@@ -44,6 +45,7 @@ SYSTEM_FRAME_CHECK_BLACKLIST_PLATFORMS = frozenset(
         "go-iris",
         "go-martini",
         "go-negroni",
+        "ionic",
         "javascript",
         "javascript-angular",
         "javascript-angularjs",
@@ -154,6 +156,7 @@ SEER_ELIGIBLE_PLATFORMS = SYSTEM_FRAME_CHECK_BLACKLIST_PLATFORMS | frozenset(
         "java-logback",
         "java-spring",
         "java-spring-boot",
+        "perl",
     ]
 )
 BASE64_ENCODED_PREFIXES = [
