60066355f722dd84cf14f4ce9a95e3c6e4a554ef
Raj Joshi <raj.joshi@sentry.io>
2024-12-09 14:19:33 -0800

    :recycle: ref: revert to using data source type

--- src/sentry/workflow_engine/handlers/action/notification.py
+++ src/sentry/workflow_engine/handlers/action/notification.py
@@ -1,8 +1,8 @@
 from sentry.eventstore.models import GroupEvent
 from sentry.workflow_engine.actions.notification_action import send_notification_using_rule_registry
-from sentry.workflow_engine.models import Action, Detector
+from sentry.workflow_engine.models import Action, DataSource, Detector
 from sentry.workflow_engine.registry import action_handler_registry
-from sentry.workflow_engine.types import ActionHandler, DetectorType
+from sentry.workflow_engine.types import ActionHandler
 
 
 @action_handler_registry.register(Action.Type.NOTIFICATION)
@@ -24,7 +24,9 @@ class NotificationActionHandler(ActionHandler):
         # TODO(iamrajjoshi): Add a check to see if the detector belongs to the same project as the group_event
         assert detector.project == group_event.project
 
-        if detector.type == DetectorType.ERROR:
+        data_source = DataSource.objects.get(id=action.data_source_id)
+
+        if data_source.type == "IssueOccurrence":
             # We should use the legacy issue alert notification logic
             send_notification_using_rule_registry(action, detector, group_event)
         else:
