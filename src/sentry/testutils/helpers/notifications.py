b06fcb8606eefa28f9167557ad50f415aaa0971a
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-12-09 16:02:59 -0500

    ref: fix typing for sentry.testutils.helpers.notifications (#81840)
    <!-- Describe your PR here. -->

--- src/sentry/testutils/helpers/notifications.py
+++ src/sentry/testutils/helpers/notifications.py
@@ -16,12 +16,9 @@ from sentry.issues.grouptype import (
 )
 from sentry.issues.issue_occurrence import IssueEvidence, IssueOccurrence
 from sentry.models.group import Group
-from sentry.models.team import Team
 from sentry.notifications.notifications.base import BaseNotification
 from sentry.notifications.utils.actions import MessageAction
 from sentry.types.actor import Actor
-from sentry.users.models.user import User
-from sentry.users.services.user import RpcUser
 
 
 class DummyNotification(BaseNotification):
@@ -34,7 +31,7 @@ class DummyNotification(BaseNotification):
     def get_subject(self, context: Mapping[str, Any] | None = None) -> str:
         return "My Subject"
 
-    def determine_recipients(self) -> list[Team | RpcUser]:
+    def determine_recipients(self) -> list[Actor]:
         return []
 
     def build_attachment_title(self, *args):
@@ -86,13 +83,14 @@ class DummyNotificationWithMoreFields(DummyNotification):
     def get_notification_title(
         self, provider: ExternalProviders, context: Mapping[str, Any] | None = None
     ) -> str:
+        assert context is not None
         some_value = context["some_field"]
         return f"Notification Title with {some_value}"
 
     def build_notification_footer(self, *args):
         return "Notification Footer"
 
-    def get_message_description(self, recipient: User | Team, provider: ExternalProviders):
+    def get_message_description(self, recipient: Actor, provider: ExternalProviders):
         return "Message Description"
 
     def get_title_link(self, *args):
