725f2f0c839f4befe085349fea4753f24026f18d
Neo Huang <126607112+kneeyo1@users.noreply.github.com>
2024-12-10 13:21:13 -0800

    Better logging for backpressure (#81648)
    This just adds host/port of the service to the logs.

--- tests/sentry/processing/backpressure/test_redis.py
+++ tests/sentry/processing/backpressure/test_redis.py
@@ -21,6 +21,8 @@ def test_rb_cluster_returns_some_usage() -> None:
     assert memory.used > 0
     assert memory.available > 0
     assert 0.0 < memory.percentage < 1.0
+    assert memory.host == "localhost"
+    assert memory.port == 6379
 
 
 @use_redis_cluster()
@@ -35,6 +37,8 @@ def test_redis_cluster_cluster_returns_some_usage() -> None:
     assert memory.used > 0
     assert memory.available > 0
     assert 0.0 < memory.percentage < 1.0
+    assert memory.host == "127.0.0.1"
+    assert memory.port in {7000, 7001, 7002, 7003, 7004, 7005}
 
 
 @use_redis_cluster(high_watermark=100)
@@ -47,6 +51,14 @@ def test_redis_health():
     assert isinstance(redis_services, list)
     assert len(redis_services) == 0
 
+    usage = list(iter_cluster_memory_usage(services["redis"].cluster))
+    for memory in usage:
+        assert memory.used >= 0
+        assert memory.available > 0
+        assert 0.0 < memory.percentage <= 1.0
+        assert memory.host == "127.0.0.1"
+        assert memory.port in {7000, 7001, 7002, 7003, 7004, 7005}
+
 
 @use_redis_cluster(high_watermark=0)
 def test_redis_unhealthy_state():
@@ -57,3 +69,15 @@ def test_redis_unhealthy_state():
     redis_services = unhealthy_services.get("redis")
     assert isinstance(redis_services, list)
     assert len(redis_services) == 6
+
+    usage = list(iter_cluster_memory_usage(services["redis"].cluster))
+    for memory in usage:
+        assert memory.used >= 0
+        assert memory.available > 0
+        assert 0.0 < memory.percentage <= 1.0
+        assert memory.host == "127.0.0.1"
+        assert memory.port in {7000, 7001, 7002, 7003, 7004, 7005}
+
+    for memory in redis_services:
+        assert memory.host == "127.0.0.1"
+        assert memory.port in {7000, 7001, 7002, 7003, 7004, 7005}
