6f3934f4fbe3663e9aef6a36ac4634e360e77c95
Nar Saynorath <nar.saynorath@sentry.io>
2024-12-10 17:33:05 -0500

    ref(widget-builder): Split out tests (#81949)
    Splits up some of these tests so they don't rely on the whole widget
    builder to test

---
+++ static/app/views/dashboards/widgetBuilder/components/typeSelector.spec.tsx
@@ -0,0 +1,50 @@
+import {OrganizationFixture} from 'sentry-fixture/organization';
+import {RouterFixture} from 'sentry-fixture/routerFixture';
+
+import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
+
+import {useNavigate} from 'sentry/utils/useNavigate';
+import TypeSelector from 'sentry/views/dashboards/widgetBuilder/components/typeSelector';
+import {WidgetBuilderProvider} from 'sentry/views/dashboards/widgetBuilder/contexts/widgetBuilderContext';
+
+jest.mock('sentry/utils/useNavigate', () => ({
+  useNavigate: jest.fn(),
+}));
+
+const mockUseNavigate = jest.mocked(useNavigate);
+
+describe('TypeSelector', () => {
+  let router;
+  let organization;
+  beforeEach(function () {
+    router = RouterFixture();
+    organization = OrganizationFixture({});
+  });
+
+  it('changes the visualization type', async function () {
+    const mockNavigate = jest.fn();
+    mockUseNavigate.mockReturnValue(mockNavigate);
+
+    render(
+      <WidgetBuilderProvider>
+        <TypeSelector />
+      </WidgetBuilderProvider>,
+      {
+        router,
+        organization,
+      }
+    );
+
+    // click dropdown
+    await userEvent.click(await screen.findByText('Table'));
+    // select new option
+    await userEvent.click(await screen.findByText('Bar'));
+
+    expect(mockNavigate).toHaveBeenCalledWith(
+      expect.objectContaining({
+        ...router.location,
+        query: expect.objectContaining({displayType: 'bar'}),
+      })
+    );
+  });
+});
