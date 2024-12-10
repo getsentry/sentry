0a4e1655cce42668736410b3adf482edffcc5bcf
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-12-10 09:51:51 +0100

    ref(quick-start): Mark 'Configure an Issue Alert' as complete by creating a project  (#81797)

--- src/sentry/receivers/rules.py
+++ src/sentry/receivers/rules.py
@@ -1,7 +1,13 @@
+import logging
+
+from sentry import features
 from sentry.models.project import Project
 from sentry.models.rule import Rule
 from sentry.notifications.types import FallthroughChoiceType
-from sentry.signals import project_created
+from sentry.signals import alert_rule_created, project_created
+from sentry.users.services.user.model import RpcUser
+
+logger = logging.getLogger("sentry")
 
 DEFAULT_RULE_LABEL = "Send a notification for high priority issues"
 DEFAULT_RULE_ACTIONS = [
@@ -31,7 +37,29 @@ def create_default_rules(project: Project, default_rules=True, RuleModel=Rule, *
         return
 
     rule_data = DEFAULT_RULE_DATA
-    RuleModel.objects.create(project=project, label=DEFAULT_RULE_LABEL, data=rule_data)
+    rule = RuleModel.objects.create(project=project, label=DEFAULT_RULE_LABEL, data=rule_data)
+
+    try:
+        user: RpcUser = project.organization.get_default_owner()
+    except IndexError:
+        logger.warning(
+            "Cannot record default rule created for organization (%s) due to missing owners",
+            project.organization_id,
+        )
+        return
+
+    if features.has("organizations:quick-start-updates", project.organization, actor=user):
+        # When a user creates a new project and opts to set up an issue alert within it,
+        # the corresponding task in the quick start sidebar is automatically marked as complete.
+        alert_rule_created.send(
+            user=user,
+            project=project,
+            rule_id=rule.id,
+            # The default rule created within a new project is always of type 'issue'
+            rule_type="issue",
+            sender=type(project),
+            is_api_token=False,
+        )
 
 
 project_created.connect(create_default_rules, dispatch_uid="create_default_rules", weak=False)
