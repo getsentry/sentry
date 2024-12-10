cca97305f2333add28943670ba7f96ab769c30da
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-12-10 09:51:51 +0100

    ref(quick-start): Mark 'Configure an Issue Alert' as complete by creating a project  (#81797)

--- src/sentry/receivers/onboarding.py
+++ src/sentry/receivers/onboarding.py
@@ -641,8 +641,6 @@ def record_plugin_enabled(plugin, project, user, **kwargs):
 
 @alert_rule_created.connect(weak=False)
 def record_alert_rule_created(user, project: Project, rule_type: str, **kwargs):
-    # NOTE: This intentionally does not fire for the default issue alert rule
-    # that gets created on new project creation.
     task = OnboardingTask.METRIC_ALERT if rule_type == "metric" else OnboardingTask.ALERT_RULE
     rows_affected, created = OrganizationOnboardingTask.objects.create_or_update(
         organization_id=project.organization_id,
