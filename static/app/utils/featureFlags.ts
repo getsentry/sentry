d45bf81659c4b1288b16eebe99aaeaad508ba2ad
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-12-10 17:26:45 -0800

    ref(flags): instrument sdk featureFlagsIntegration to track FE flag evals

---
+++ static/app/utils/featureFlags.ts
@@ -0,0 +1,73 @@
+import {logger} from '@sentry/core';
+import * as Sentry from '@sentry/react';
+
+import type {Organization} from 'sentry/types/organization';
+import type {Project} from 'sentry/types/project';
+
+/**
+ * Returns a callback that can be used to track sentry flag evaluations through
+ * the Sentry SDK, in the event context. If the FeatureFlagsIntegration is not
+ * installed, the callback is a no-op.
+ */
+export function getSentryFeaturesHook(): (name: string, value: unknown) => void {
+  const featureFlagsIntegration =
+    Sentry.getClient()?.getIntegrationByName<Sentry.FeatureFlagsIntegration>(
+      'FeatureFlags'
+    );
+  if (!featureFlagsIntegration || !('addFeatureFlag' in featureFlagsIntegration)) {
+    logger.error(
+      'Unable to track flag evaluations because FeatureFlagsIntegration is not installed correctly.'
+    );
+    return (_name, _value) => {};
+  }
+  return (name: string, value: unknown) => {
+    // Append `feature.organizations:` in front to match the Sentry options automator format
+    featureFlagsIntegration?.addFeatureFlag('feature.organizations:' + name, value);
+  };
+}
+
+/**
+ * Registers a hook that processes feature names and values on each call to
+ * organization.features.includes().
+ */
+export function addOrganizationFeaturesHook({
+  organization,
+  hook,
+}: {
+  hook: (name: string, value: unknown) => void;
+  organization: Organization;
+}) {
+  const handler = {
+    apply: (target: any, orgFeatures: string[], flagName: string[]) => {
+      // Evaluate the result of .includes()
+      const flagResult = target.apply(orgFeatures, flagName);
+      hook(flagName[0], flagResult);
+      return flagResult;
+    },
+  };
+  const proxy = new Proxy(organization.features.includes, handler);
+  organization.features.includes = proxy;
+}
+
+/**
+ * Registers a handler to track names and values of features passed into the
+ * project.features.includes() function.
+ */
+export function addProjectFeaturesHook({
+  project,
+  hook,
+}: {
+  hook: (name: string, value: unknown) => void;
+  project: Project;
+}) {
+  const handler = {
+    apply: (target: any, projFeatures: string[], flagName: string[]) => {
+      // Evaluate the result of .includes()
+      const flagResult = target.apply(projFeatures, flagName);
+      hook(flagName[0], flagResult);
+      return flagResult;
+    },
+  };
+  const proxy = new Proxy(project.features.includes, handler);
+  project.features.includes = proxy;
+}
