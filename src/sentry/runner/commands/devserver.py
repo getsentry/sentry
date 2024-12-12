7c1d449c903f01a2cf957154a6dc235c43bb8819
Hubert Deng <hubert.deng@sentry.io>
2024-12-12 10:00:53 -0800

    feat(devservices): More descriptive error message when kafka not running for devserver (#81978)
    Folks are running into issues when kafka is not running for devserver
    with the new revamped devservices. Turns out, it's because they're not
    properly setting the environment variable. This message is more
    descriptive, allowing people to debug the errors much easier.
    
    Changed the logic to also check for ALL valid kafka container names, not
    just one or the other

--- src/sentry/runner/commands/devserver.py
+++ src/sentry/runner/commands/devserver.py
@@ -354,10 +354,19 @@ def devserver(
 
     # Create all topics if the Kafka eventstream is selected
     if kafka_consumers:
-        kafka_container_name = (
-            "kafka-kafka-1" if os.environ.get("USE_NEW_DEVSERVICES") == "1" else "sentry_kafka"
+        use_new_devservices = os.environ.get("USE_NEW_DEVSERVICES") == "1"
+        valid_kafka_container_names = ["kafka-kafka-1", "sentry_kafka"]
+        kafka_container_name = "kafka-kafka-1" if use_new_devservices else "sentry_kafka"
+        kafka_container_warning_message = (
+            f"""
+Devserver is configured to work with the revamped devservices. Looks like the `{kafka_container_name}` container is not running.
+Please run `devservices up` to start it. If you would like to use devserver with `sentry devservices`, set `USE_NEW_DEVSERVICES=0` in your environment."""
+            if use_new_devservices
+            else f"""
+Devserver is configured to work with `sentry devservices`. Looks like the `{kafka_container_name}` container is not running.
+Please run `sentry devservices up kafka` to start it. If you would like to use devserver with the revamped devservices, set `USE_NEW_DEVSERVICES=1` in your environment."""
         )
-        if kafka_container_name not in containers:
+        if not any(name in containers for name in valid_kafka_container_names):
             raise click.ClickException(
                 f"""
 Devserver is configured to start some kafka consumers, but Kafka
@@ -373,7 +382,7 @@ or:
 
     SENTRY_EVENTSTREAM = "sentry.eventstream.kafka.KafkaEventStream"
 
-and run `sentry devservices up kafka`.
+{kafka_container_warning_message}
 
 Alternatively, run without --workers.
         """
