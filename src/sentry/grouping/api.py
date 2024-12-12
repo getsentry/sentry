1c7c862d8755d07a4a8ff3c779dde72b4eaa90a9
Katie Byers <lobsterkatie@gmail.com>
2024-12-12 13:20:15 -0800

    add comment re: order of strategy iteration

--- src/sentry/grouping/api.py
+++ src/sentry/grouping/api.py
@@ -285,8 +285,10 @@ def _get_component_trees_for_variants(
     precedence_hint: str | None = None
     all_strategies_components_by_variant: dict[str, list[BaseGroupingComponent]] = {}
 
+    # `iter_strategies` presents strategies in priority order, which allows us to go with the first
+    # one which produces a result. (See `src/sentry/grouping/strategies/configurations.py` for the
+    # strategies used by each config.)
     for strategy in context.config.iter_strategies():
-        # Defined in src/sentry/grouping/strategies/base.py
         current_strategy_components_by_variant = strategy.get_grouping_components(
             event, context=context
         )
