e23fefc031811feb79107e1b827a182f2245967e
Joshua Ferge <josh.ferge@sentry.io>
2024-12-13 16:34:52 -0700

    simplify

--- src/sentry/models/projectteam.py
+++ src/sentry/models/projectteam.py
@@ -2,7 +2,7 @@ from collections.abc import Sequence
 from typing import TYPE_CHECKING, ClassVar
 
 from django.db import router, transaction
-from django.db.models import QuerySet, prefetch_related_objects
+from django.db.models import QuerySet
 from django.db.models.signals import post_delete, post_save
 
 from sentry.backup.scopes import RelocationScope
@@ -16,14 +16,12 @@ if TYPE_CHECKING:
 
 class ProjectTeamManager(BaseManager["ProjectTeam"]):
     def get_for_teams_with_org_cache(self, teams: Sequence["Team"]) -> QuerySet["ProjectTeam"]:
-        project_teams = (
+        return (
             self.filter(team__in=teams, project__status=ObjectStatus.ACTIVE)
             .order_by("project__name", "project__slug")
             .select_related("project")
+            .prefetch_related("project__organization")
         )
-        prefetch_related_objects(project_teams, "project__organization")
-
-        return project_teams
 
 
 @region_silo_model
