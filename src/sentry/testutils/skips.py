967282d36f9b16ff72e76de700582f51d0b32db0
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-12-13 17:24:01 -0500

    ref: remove skip_for_relay_store (#82106)
    never referenced in code -- introduced in
    ad3d1d58ca1e5eb5e06f8adf6bc408fb9711ca09
    
    <!-- Describe your PR here. -->

--- src/sentry/testutils/skips.py
+++ src/sentry/testutils/skips.py
@@ -1,30 +1,10 @@
 from __future__ import annotations
 
 import socket
-from collections.abc import Callable
-from typing import Any, TypeVar
 from urllib.parse import urlparse
 
 import pytest
 from django.conf import settings
-
-T = TypeVar("T", bound=Callable[..., Any])
-
-
-def skip_for_relay_store(reason: str) -> Callable[[T], T]:
-    """
-    Decorator factory will skip marked tests if Relay is enabled.
-    A test decorated with @skip_for_relay_store("this test has been moved in relay")
-    Will not be executed when the settings SENTRY_USE_RELAY = True
-    :param reason: the reason the test should be skipped
-
-    Note: Eventually, when Relay becomes compulsory, tests marked with this decorator will be deleted.
-    """
-
-    def decorator(function: T) -> T:
-        return pytest.mark.skipif(settings.SENTRY_USE_RELAY, reason=reason)(function)
-
-    return decorator
 
 
 def _service_available(host: str, port: int) -> bool:
