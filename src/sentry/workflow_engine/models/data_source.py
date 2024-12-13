6ae1cd72fbfee11c322342b0e22cd03af6488d22
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-12-13 14:59:52 -0800

    Add a pre save reciever to ensure the data_source.type is a valid key in the registry.

--- src/sentry/workflow_engine/models/data_source.py
+++ src/sentry/workflow_engine/models/data_source.py
@@ -3,6 +3,8 @@ import dataclasses
 from typing import Generic, TypeVar
 
 from django.db import models
+from django.db.models.signals import pre_save
+from django.dispatch import receiver
 
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import (
@@ -11,6 +13,7 @@ from sentry.db.models import (
     FlexibleForeignKey,
     region_silo_model,
 )
+from sentry.utils.registry import NoRegistrationExistsError
 from sentry.workflow_engine.models.data_source_detector import DataSourceDetector
 from sentry.workflow_engine.registry import data_source_type_registry
 from sentry.workflow_engine.types import DataSourceTypeHandler
@@ -30,6 +33,8 @@ class DataSource(DefaultFieldsModel):
 
     organization = FlexibleForeignKey("sentry.Organization")
     query_id = BoundedBigIntegerField()
+
+    # This is a dynamic field, depending on the type in the data_source_type_registry
     type = models.TextField()
 
     detectors = models.ManyToManyField("workflow_engine.Detector", through=DataSourceDetector)
@@ -45,3 +50,19 @@ class DataSource(DefaultFieldsModel):
         if not handler:
             raise ValueError(f"Unknown data source type: {self.type}")
         return handler
+
+
+@receiver(pre_save, sender=DataSource)
+def ensure_valid_type(sender, instance: DataSource, **kwargs):
+    """
+    Ensure that the type of the data source is valid and registered in the data_source_type_registry
+    """
+    data_source_type = instance.type
+
+    if not data_source_type:
+        raise ValueError(f"No group type found with type {instance.type}")
+
+    try:
+        data_source_type_registry.get(data_source_type)
+    except NoRegistrationExistsError:
+        raise ValueError(f"No data source type found with type {data_source_type}")
