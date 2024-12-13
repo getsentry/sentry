18772e75a4c73c38e6eb007c8e45792142b29f43
Jodi Jang <116035587+jangjodi@users.noreply.github.com>
2024-12-13 09:56:12 -0800

    fix(issues): Show empty key in request body (#82066)
    Show empty keys in issue details request body dictionary

--- static/app/components/structuredEventData/recursiveStructuredData.tsx
+++ static/app/components/structuredEventData/recursiveStructuredData.tsx
@@ -42,15 +42,15 @@ export function RecursiveStructuredData({
   withOnlyFormattedText = false,
 }: Props) {
   let i = 0;
-
-  const formattedObjectKey = objectKey ? (
-    <Fragment>
-      <ValueObjectKey>
-        {config?.renderObjectKeys?.(objectKey) ?? objectKey}
-      </ValueObjectKey>
-      <span>{': '}</span>
-    </Fragment>
-  ) : null;
+  const formattedObjectKey =
+    objectKey !== undefined ? (
+      <Fragment>
+        <ValueObjectKey>
+          {config?.renderObjectKeys?.(objectKey) ?? objectKey}
+        </ValueObjectKey>
+        <span>{': '}</span>
+      </Fragment>
+    ) : null;
 
   function Wrapper({children}: {children: React.ReactNode}) {
     return (
