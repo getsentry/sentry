dc5f106d810ab1219987047707968c40288159da
Snigdha Sharma <snigdha.sharma@sentry.io>
2024-12-13 13:38:04 -0800

    feat(metric-issues): Configure workflow notifications by group type (#81609)
    Metric issues, and other detector-controlled issues, need the ability to
    disable workflow notifications for status changes (i.e. issue
    resolved/regressed/unresolved) while still sending workflow
    notifications for other changes to the issue such as assignments and
    comments.
    
    To support this cleanly, this PR adds a field to the GroupType dataclass
    to allow issue types to disable the status change workflow
    notifications. Issue activity notifications will continue to be sent for
    all issue types. For issue types that do not use the existing status
    change workflow notifications, the issue type will be responsible for
    sending these notifications.
    
    We use the new `enable_status_change_workflow_notifications` field to
    disable notifications for status-based activity types when
    activity.send_notification is called. Added a new test to confirm
    disables notifications for the MetricIssuePOC type and existing tests
    remain unaffected for error/other issue platform types.

--- src/sentry/testutils/factories.py
+++ src/sentry/testutils/factories.py
@@ -1003,6 +1003,9 @@ class Factories:
     @staticmethod
     @assume_test_silo_mode(SiloMode.REGION)
     def create_group(project, **kwargs):
+        from sentry.models.group import GroupStatus
+        from sentry.types.group import GroupSubStatus
+
         kwargs.setdefault("message", "Hello world")
         kwargs.setdefault("data", {})
         if "type" not in kwargs["data"]:
@@ -1012,6 +1015,10 @@ class Factories:
         if "metadata" in kwargs:
             metadata = kwargs.pop("metadata")
             kwargs["data"].setdefault("metadata", {}).update(metadata)
+        if "status" not in kwargs:
+            kwargs["status"] = GroupStatus.UNRESOLVED
+            kwargs["substatus"] = GroupSubStatus.NEW
+
         return Group.objects.create(project=project, **kwargs)
 
     @staticmethod
