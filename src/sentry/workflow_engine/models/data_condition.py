4edf09d5b1a265424f1425ff9a2e4a12df78922c
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2024-12-16 15:48:57 -0800

    Work-in-progress - just want to get the basic overall mapping down in a diff to share and ask for feedback

--- src/sentry/workflow_engine/models/data_condition.py
+++ src/sentry/workflow_engine/models/data_condition.py
@@ -1,7 +1,6 @@
 import logging
 import operator
 from collections.abc import Callable
-from enum import StrEnum
 from typing import Any, TypeVar, cast
 
 from django.db import models
@@ -19,8 +18,7 @@ from sentry.workflow_engine.types import (
 logger = logging.getLogger(__name__)
 
 
-# TODO should this be in types?
-class Condition(StrEnum):
+class Condition(models.TextChoices):
     EQUAL = "eq"
     GREATER_OR_EQUAL = "gte"
     GREATER = "gt"
@@ -51,23 +49,28 @@ class DataCondition(DefaultFieldsModel):
     __relocation_scope__ = RelocationScope.Organization
     __repr__ = sane_repr("type", "condition", "condition_group")
 
-    # The condition is the logic condition that needs to be met, gt, lt, eq, etc.
-    condition = models.CharField(max_length=200)
-
     # The comparison is the value that the condition is compared to for the evaluation, this must be a primitive value
-    comparison = models.JSONField()
+    comparison_value = models.JSONField()
 
     # The condition_result is the value that is returned if the condition is met, this must be a primitive value
     condition_result = models.JSONField()
 
     # The type of condition, this is used to initialize the condition classes
-    type = models.CharField(max_length=200)
+    type = models.CharField(max_length=200, choices=Condition.choices)
+
+    # The operator used on the condition
+    operator = models.CharField(
+        max_length=200, null=True, choices=[(key, key) for key in condition_ops.keys()]
+    )
 
     condition_group = models.ForeignKey(
         "workflow_engine.DataConditionGroup",
         related_name="conditions",
         on_delete=models.CASCADE,
     )
+
+    # input_data_filters can be used to get data out of dictionaries or classes
+    input_data_filter = models.CharField(max_length=200, null=True)
 
     def get_condition_result(self) -> DataConditionResult:
         match self.condition_result:
@@ -104,11 +107,13 @@ class DataCondition(DefaultFieldsModel):
             condition_handler = self.get_condition_handler()
         except NoRegistrationExistsError:
             # If it's not a custom handler, use the default operators
-            condition = Condition(self.condition)
-            op = condition_ops.get(condition, None)
+            if self.operator is not None:
+                condition = Condition(self.operator)
+                op = condition_ops.get(condition, None)
 
         if condition_handler is not None:
-            result = condition_handler.evaluate_value(value, self.comparison, self.condition)
+            data_filter = self.input_data_filter or ""
+            result = condition_handler.evaluate_value(value, self.comparison_value, data_filter)
         elif op is not None:
             result = op(cast(Any, value), self.comparison)
         else:
@@ -117,7 +122,7 @@ class DataCondition(DefaultFieldsModel):
                 extra={
                     "id": self.id,
                     "type": self.type,
-                    "condition": self.condition,
+                    "operator": self.operator,
                 },
             )
 
