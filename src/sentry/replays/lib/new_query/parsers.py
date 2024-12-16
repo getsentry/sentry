ac8e29e7899c023159eba900b9a59684dea93ad4
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2024-12-16 13:11:23 -0800

    ref(replay): rm value from CouldNotParseValue error msgs (#82181)
    The parser reformats the searchbar input, switches/strips units, etc, so
    the value of search_filter.value.value could be confusing to the user.
    IMO we don't need to include the value, just the key. Ex:
    ![Screenshot 2024-12-16 at 12 45
    13 PM](https://github.com/user-attachments/assets/d8b2e959-bdbf-4531-b5a3-008bca9e4118)
    
    
    Also adds a "Detail: " prefix and a handler for `parse_int`

--- src/sentry/replays/lib/new_query/parsers.py
+++ src/sentry/replays/lib/new_query/parsers.py
@@ -19,7 +19,10 @@ def parse_float(value: str) -> float:
 
 def parse_int(value: str) -> int:
     """Coerce to int or fail."""
-    return int(parse_float(value))
+    try:
+        return int(parse_float(value))
+    except (ValueError, CouldNotParseValue):
+        raise CouldNotParseValue("Failed to parse int.")
 
 
 def parse_duration(value: str) -> int:
