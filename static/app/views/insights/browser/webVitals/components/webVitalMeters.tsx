41349ab4832015df6a03c819af97273ce26ba5f9
Ash <0Calories@users.noreply.github.com>
2024-12-16 15:59:48 -0500

    fix(trace-view): Update Web Vital tooltip text (#82175)
    Updates the web vitals tooltips with the newer copy 
    
    ![image](https://github.com/user-attachments/assets/8b752e33-7534-4480-a436-fed4ddd9081c)

--- static/app/views/insights/browser/webVitals/components/webVitalMeters.tsx
+++ static/app/views/insights/browser/webVitals/components/webVitalMeters.tsx
@@ -21,7 +21,6 @@ import {
   scoreToStatus,
   STATUS_TEXT,
 } from 'sentry/views/insights/browser/webVitals/utils/scoreToStatus';
-import {vitalDescription} from 'sentry/views/performance/vitalDetail/utils';
 
 type Props = {
   onClick?: (webVital: WebVitals) => void;
@@ -333,11 +332,11 @@ export function VitalPill({webVital, score, meterValue}: VitalPillProps) {
       <NoValue />
     );
 
-  const tooltipText = vitalDescription[`measurements.${webVital}`];
+  const tooltipText = VITAL_DESCRIPTIONS[`measurements.${webVital}`];
 
   return (
     <VitalPillContainer>
-      <Tooltip title={tooltipText}>
+      <Tooltip title={tooltipText?.shortDescription}>
         <VitalPillName status={status}>
           {`${webVital ? webVital.toUpperCase() : ''} (${STATUS_TEXT[status] ?? 'N/A'})`}
         </VitalPillName>
