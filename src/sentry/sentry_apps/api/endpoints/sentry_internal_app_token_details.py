9983c2023511956b52cb0cc9343ff5a0bb868af7
Christinarlong <christina.long@sentry.io>
2024-12-16 04:01:01 -0800

    update inner errors for siatd

--- src/sentry/sentry_apps/api/endpoints/sentry_internal_app_token_details.py
+++ src/sentry/sentry_apps/api/endpoints/sentry_internal_app_token_details.py
@@ -1,5 +1,4 @@
 from django.db import router, transaction
-from django.http import Http404
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -8,6 +7,7 @@ from sentry import analytics, deletions
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import control_silo_endpoint
+from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.coreapi import APIUnauthorized
 from sentry.models.apitoken import ApiToken
 from sentry.sentry_apps.api.bases.sentryapps import (
@@ -37,7 +37,9 @@ class SentryInternalAppTokenDetailsEndpoint(SentryAppBaseEndpoint):
         try:
             kwargs["api_token"] = ApiToken.objects.get(id=api_token_id)
         except (ApiToken.DoesNotExist, ValueError):
-            raise SentryAppIntegratorError(APIUnauthorized("Couldn't find the given api token"))
+            raise SentryAppIntegratorError(
+                ResourceDoesNotExist("Couldn't find the given api token"), status_code=404
+            )
 
         return (args, kwargs)
 
@@ -46,7 +48,7 @@ class SentryInternalAppTokenDetailsEndpoint(SentryAppBaseEndpoint):
         # Validate the token is associated with the application
         if api_token.application_id != sentry_app.application_id:
             raise SentryAppIntegratorError(
-                APIUnauthorized("Given token is not owned by this sentry app")
+                APIUnauthorized("Given token is not owned by this sentry app"), status_code=403
             )
 
         if not sentry_app.is_internal:
@@ -66,7 +68,9 @@ class SentryInternalAppTokenDetailsEndpoint(SentryAppBaseEndpoint):
                 install_token = SentryAppInstallationToken.objects.get(api_token=api_token)
                 sentry_app_installation = install_token.sentry_app_installation
             except SentryAppInstallationToken.DoesNotExist:
-                raise SentryAppIntegratorError(Http404("Could not find given token"))
+                raise SentryAppIntegratorError(
+                    ResourceDoesNotExist("Could not find given token"), status_code=404
+                )
 
             deletions.exec_sync(install_token)
 
