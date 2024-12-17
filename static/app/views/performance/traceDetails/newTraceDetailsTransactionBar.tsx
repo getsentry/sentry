966b7b8936a1de0f254e0cebbe34a6690b1023b9
Scott Cooper <scttcper@gmail.com>
2024-12-17 11:50:03 -0800

    imports

--- static/app/views/performance/traceDetails/newTraceDetailsTransactionBar.tsx
+++ static/app/views/performance/traceDetails/newTraceDetailsTransactionBar.tsx
@@ -1,4 +1,4 @@
-import {Fragment, useCallback, useEffect, useMemo, useState} from 'react';
+import {Fragment, useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import styled from '@emotion/styled';
 import type {Location} from 'history';
 import {Observer} from 'mobx-react';
