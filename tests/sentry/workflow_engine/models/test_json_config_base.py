30a8864d679cf424202493f858ed429e133c0afe
Colleen O'Rourke <colleen@sentry.io>
2024-12-16 18:09:45 -0800

    update tes

--- tests/sentry/workflow_engine/models/test_json_config_base.py
+++ tests/sentry/workflow_engine/models/test_json_config_base.py
@@ -134,7 +134,7 @@ class TestMetricAlertFireDetectorConfig(TestJsonConfigBase, APITestCase):
                     "threshold_period": self.metric_alert.threshold_period,
                     "comparison_delta": self.metric_alert.comparison_delta,
                     "sensitivity": self.metric_alert.sensitivity,
-                    "seasonality": 42,
+                    "seasonality": self.metric_alert.seasonality,
                 },
             )
 
