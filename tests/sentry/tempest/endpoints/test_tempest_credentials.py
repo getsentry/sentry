24a213665b6a8db7cc57b6588e59c7c348233278
Vjeran Grozdanić <vjeran.grozdanic@sentry.io>
2024-12-17 14:34:20 +0100

    feat(tempest): Endpoint for creating tempest credentials (#82224)
    Endpoint for creating tempest credentials.
    
    There will be another PR which handles deletion of credentials.
    
    Part of https://github.com/getsentry/team-gdx/issues/52
    
    ---------
    
    Signed-off-by: Vjeran Grozdanic <vjeran.grozdanic@sentry.io>

--- tests/sentry/tempest/endpoints/test_tempest_credentials.py
+++ tests/sentry/tempest/endpoints/test_tempest_credentials.py
@@ -1,3 +1,4 @@
+from sentry.tempest.models import TempestCredentials
 from sentry.testutils.cases import APITestCase
 from sentry.testutils.helpers.features import Feature
 
@@ -5,7 +6,9 @@ from sentry.testutils.helpers.features import Feature
 class TestTempestCredentials(APITestCase):
     endpoint = "sentry-api-0-project-tempest-credentials"
 
-    def test_create_tempest_credentials(self):
+    valid_credentials_data = {"clientId": "test", "clientSecret": "test"}
+
+    def test_get_tempest_credentials(self):
         with Feature({"organizations:tempest-access": True}):
             credentials = [self.create_tempest_credentials(self.project) for _ in range(5)]
 
@@ -34,3 +37,88 @@ class TestTempestCredentials(APITestCase):
 
     def test_unauthenticated_user_cant_access_endpoint(self):
         self.get_error_response(self.project.organization.slug, self.project.slug)
+
+    def test_create_tempest_credentials(self):
+        with Feature({"organizations:tempest-access": True}):
+            self.login_as(self.user)
+            response = self.get_success_response(
+                self.project.organization.slug,
+                self.project.slug,
+                method="POST",
+                **self.valid_credentials_data,
+            )
+            assert response.status_code == 201
+            creds_obj = TempestCredentials.objects.get(project=self.project)
+            assert creds_obj.client_id == self.valid_credentials_data["clientId"]
+            assert creds_obj.client_secret == self.valid_credentials_data["clientSecret"]
+            assert creds_obj.project == self.project
+            assert creds_obj.created_by_id == self.user.id
+
+    def test_create_tempest_credentials_without_feature_flag(self):
+        self.login_as(self.user)
+        response = self.get_error_response(
+            self.project.organization.slug,
+            self.project.slug,
+            method="POST",
+            **self.valid_credentials_data,
+        )
+        assert response.status_code == 404
+
+    def test_create_tempest_credentials_as_unauthenticated_user(self):
+        response = self.get_error_response(
+            self.project.organization.slug,
+            self.project.slug,
+            method="POST",
+            **self.valid_credentials_data,
+        )
+        assert response.status_code == 401
+
+    def test_non_admin_cant_create_tempest_credentials(self):
+        non_admin_user = self.create_user()
+        self.create_member(
+            user=non_admin_user, organization=self.project.organization, role="member"
+        )
+        with Feature({"organizations:tempest-access": True}):
+            self.login_as(non_admin_user)
+            response = self.get_error_response(
+                self.project.organization.slug,
+                self.project.slug,
+                method="POST",
+                **self.valid_credentials_data,
+            )
+            assert response.status_code == 403
+
+    def test_create_tempest_credentials_with_invalid_data(self):
+        with Feature({"organizations:tempest-access": True}):
+            self.login_as(self.user)
+            response = self.get_error_response(
+                self.project.organization.slug,
+                self.project.slug,
+                method="POST",
+                **{"clientId": "test"},
+            )
+            assert response.status_code == 400
+
+            response2 = self.get_error_response(
+                self.project.organization.slug,
+                self.project.slug,
+                method="POST",
+                **{"clientSecret": "test"},
+            )
+            assert response2.status_code == 400
+
+    def test_cant_create_tempest_credentials_with_duplicate_client_id(self):
+        with Feature({"organizations:tempest-access": True}):
+            self.login_as(self.user)
+            self.create_tempest_credentials(
+                self.project, client_id=self.valid_credentials_data["clientId"]
+            )
+            response = self.get_error_response(
+                self.project.organization.slug,
+                self.project.slug,
+                method="POST",
+                **self.valid_credentials_data,
+            )
+            # database constraint violation
+            assert response.status_code == 400
+            assert response.data["detail"] == "A credential with this client ID already exists."
