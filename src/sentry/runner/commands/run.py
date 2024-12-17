f5ed494af9ffca78f794a153bb05ea62fa7e617a
Mark Story <mark@mark-story.com>
2024-12-17 16:26:16 -0500

    feat(taskworker) Add autoreload to taskworker (#82013)
    Enable code reloading for `sentry run taskworker` to improve DX when
    working locally.
    
    Reloading was interacting poorly with our fork strategy, so I switched
    back to spawn. Going to spawn required moving the test tasks into a
    'real' module and ensuring those tasks are loaded by worker processess
    in the initializer hook.
    
    Refs #81243

--- src/sentry/runner/commands/run.py
+++ src/sentry/runner/commands/run.py
@@ -7,6 +7,7 @@ from multiprocessing import cpu_count
 from typing import Any
 
 import click
+from django.utils import autoreload
 
 from sentry.bgtasks.api import managed_bgtasks
 from sentry.runner.decorators import configuration, log_options
@@ -230,8 +231,6 @@ def worker(ignore_unknown_queues: bool, **options: Any) -> None:
                 raise click.ClickException(message)
 
     if options["autoreload"]:
-        from django.utils import autoreload
-
         autoreload.run_with_reloader(run_worker, **options)
     else:
         run_worker(**options)
@@ -248,7 +247,22 @@ def worker(ignore_unknown_queues: bool, **options: Any) -> None:
 )
 @log_options()
 @configuration
-def taskworker(rpc_host: str, max_task_count: int, namespace: str | None, **options: Any) -> None:
+def taskworker(**options: Any) -> None:
+    """
+    Run a taskworker worker
+    """
+    if options["autoreload"]:
+        autoreload.run_with_reloader(run_taskworker, **options)
+    else:
+        run_taskworker(**options)
+
+
+def run_taskworker(
+    rpc_host: str, max_task_count: int, namespace: str | None, **options: Any
+) -> None:
+    """
+    taskworker factory that can be reloaded
+    """
     from sentry.taskworker.worker import TaskWorker
 
     with managed_bgtasks(role="taskworker"):
