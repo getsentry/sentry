7068fcd182dc08f54463498411899b9e8f8b8e85
Raj Joshi <raj.joshi@sentry.io>
2024-12-18 09:09:48 -0800

    :wrench: chore(integrations): remove Customer Priority FF Manager (#82246)
    followup of
    https://github.com/getsentry/sentry-options-automator/pull/2908

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -145,8 +145,6 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:increased-issue-owners-rate-limit", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Starfish: extract metrics from the spans
     manager.add("organizations:indexed-spans-extraction", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
-    # Enable custom alert priorities for Pagerduty and Opsgenie
-    manager.add("organizations:integrations-custom-alert-priorities", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True, default=True)
     # Enable integration functionality to work deployment integrations like Vercel
     manager.add("organizations:integrations-deployment", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, default=True, api_expose=True)
     manager.add("organizations:integrations-feature-flag-integration", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
