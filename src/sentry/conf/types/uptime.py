bd078340921de601166e544c81e9ef0831776642
Dan Fuller <dfuller@sentry.io>
2024-12-19 09:12:11 +1100

    Update src/sentry/conf/types/uptime.py
    Co-authored-by: Evan Purkhiser <evanpurkhiser@gmail.com>

--- src/sentry/conf/types/uptime.py
+++ src/sentry/conf/types/uptime.py
@@ -5,6 +5,9 @@ from sentry.conf.types.kafka_definition import Topic
 
 @dataclasses.dataclass
 class UptimeRegionConfig:
+    """
+    Defines an available region which an uptime-checker is run in.
+    """
     slug: str
     name: str
     config_topic: Topic
