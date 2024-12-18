9ce039898801cb2e812ff1c682da3248390d071e
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2024-12-18 16:46:27 -0500

    ref: remove NonNone helper (#82330)
    silently discarding nullability is an antipattern, most of these weren't
    needed anyway
    
    <!-- Describe your PR here. -->

--- tests/sentry/grouping/ingest/test_seer.py
+++ tests/sentry/grouping/ingest/test_seer.py
@@ -16,7 +16,6 @@ from sentry.seer.similarity.utils import MAX_FRAME_COUNT
 from sentry.testutils.cases import TestCase
 from sentry.testutils.helpers.eventprocessing import save_new_event
 from sentry.testutils.helpers.options import override_options
-from sentry.utils.types import NonNone
 
 
 class ShouldCallSeerTest(TestCase):
@@ -220,8 +219,9 @@ class GetSeerSimilarIssuesTest(TestCase):
         assert self.existing_event.get_primary_hash() == "04e89719410791836f0a0bbf03bf0d2e"
         # In real life just filtering on group id wouldn't be enough to guarantee us a single,
         # specific GroupHash record, but since the database resets before each test, here it's okay
+        assert self.existing_event.group_id is not None
         self.existing_event_grouphash = GroupHash.objects.filter(
-            group_id=NonNone(self.existing_event.group_id)
+            group_id=self.existing_event.group_id
         ).first()
         self.new_event = Event(
             project_id=self.project.id,
@@ -277,9 +277,10 @@ class GetSeerSimilarIssuesTest(TestCase):
         )
 
     def test_returns_metadata_and_grouphash_if_sufficiently_close_group_found(self) -> None:
+        assert self.existing_event.group_id
         seer_result_data = SeerSimilarIssueData(
-            parent_hash=NonNone(self.existing_event.get_primary_hash()),
-            parent_group_id=NonNone(self.existing_event.group_id),
+            parent_hash=self.existing_event.get_primary_hash(),
+            parent_group_id=self.existing_event.group_id,
             stacktrace_distance=0.01,
             should_group=True,
         )
