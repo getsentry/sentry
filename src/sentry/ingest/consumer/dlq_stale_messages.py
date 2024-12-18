ec067a4f21bc40a25cc2086e33113031b8b849b7
kneeyo1 <neo.huang@sentry.io>
2024-12-17 16:08:47 -0800

    updating how we configure timestamps'

--- src/sentry/ingest/consumer/dlq_stale_messages.py
+++ src/sentry/ingest/consumer/dlq_stale_messages.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta, timezone
 
 from arroyo.dlq import InvalidMessage
 from arroyo.processing.strategies import ProcessingStrategy, RunTask
@@ -9,10 +9,11 @@ TPayload = TypeVar("TPayload")
 
 def create_dlq_stale_messages_step(next_step: ProcessingStrategy[TPayload], threshold_sec: int):
     def dlq_delayed_messages(message: Message[TPayload]) -> TPayload:
-        min_accepted_timestamp = datetime.utcnow() - threshold_sec
+        min_accepted_timestamp = datetime.now(timezone.utc) - timedelta(seconds=threshold_sec)
 
         if isinstance(message.value, BrokerValue):
-            if message.timestamp and message.timestamp < min_accepted_timestamp:
+            message_timestamp = message_timestamp = message.timestamp.astimezone(timezone.utc)
+            if message_timestamp < min_accepted_timestamp:
                 raise InvalidMessage(message.value.partition, message.value.offset)
 
         return message.payload
