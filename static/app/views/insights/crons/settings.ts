56e9b8f24580609b86b7d7b4ff0818d9034c5b37
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-12-19 14:01:22 -0500

    feat(insights): Add crons / uptime (#82376)

---
+++ static/app/views/insights/crons/settings.ts
@@ -0,0 +1,14 @@
+import {t} from 'sentry/locale';
+
+export const MODULE_TITLE = t('Cron Monitors');
+export const MODULE_SIDEBAR_TITLE = t('Crons');
+export const DATA_TYPE = t('Cron Check-In');
+export const DATA_TYPE_PLURAL = t('Cron Check-Ins');
+export const BASE_URL = 'crons';
+
+export const MODULE_DESCRIPTION = t(
+  'Monitor cron jobs for failures, timeouts, and missed runs.'
+);
+export const MODULE_DOC_LINK = 'https://docs.sentry.io/product/crons/';
+
+export const MODULE_FEATURES = ['insights-crons'];
