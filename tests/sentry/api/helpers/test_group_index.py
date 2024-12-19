8923393b8d6fe321d2db42078b1cf086d27c5b2a
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2024-12-19 08:29:08 -0500

    ref(update_groups): Simplify signature (#82245)
    `update_groups` will now require a sequence of groups rather than group
    IDs, project and an org id.

--- tests/sentry/api/helpers/test_group_index.py
+++ tests/sentry/api/helpers/test_group_index.py
@@ -8,6 +8,7 @@ from django.http import QueryDict
 from sentry.api.helpers.group_index import update_groups, validate_search_filter_permissions
 from sentry.api.helpers.group_index.delete import delete_groups
 from sentry.api.helpers.group_index.update import (
+    get_group_list,
     handle_assigned_to,
     handle_has_seen,
     handle_is_bookmarked,
@@ -106,7 +107,8 @@ class UpdateGroupsTest(TestCase):
         request.data = {"status": "unresolved", "substatus": "ongoing"}
         request.GET = QueryDict(query_string=f"id={resolved_group.id}")
 
-        update_groups(request, request.GET.getlist("id"), [self.project], self.organization.id)
+        group_list = get_group_list(self.organization.id, [self.project], request.GET.getlist("id"))
+        update_groups(request, group_list)
 
         resolved_group.refresh_from_db()
 
@@ -126,7 +128,8 @@ class UpdateGroupsTest(TestCase):
         request.data = {"status": "resolved", "substatus": None}
         request.GET = QueryDict(query_string=f"id={unresolved_group.id}")
 
-        update_groups(request, request.GET.getlist("id"), [self.project], self.organization.id)
+        group_list = get_group_list(self.organization.id, [self.project], request.GET.getlist("id"))
+        update_groups(request, group_list)
 
         unresolved_group.refresh_from_db()
 
@@ -145,7 +148,8 @@ class UpdateGroupsTest(TestCase):
         request.data = {"status": "ignored", "substatus": "archived_forever"}
         request.GET = QueryDict(query_string=f"id={group.id}")
 
-        update_groups(request, request.GET.getlist("id"), [self.project], self.organization.id)
+        group_list = get_group_list(self.organization.id, [self.project], request.GET.getlist("id"))
+        update_groups(request, group_list)
 
         group.refresh_from_db()
 
@@ -174,7 +178,8 @@ class UpdateGroupsTest(TestCase):
         }
         request.GET = QueryDict(query_string=f"id={group.id}")
 
-        update_groups(request, request.GET.getlist("id"), [self.project], self.organization.id)
+        group_list = get_group_list(self.organization.id, [self.project], request.GET.getlist("id"))
+        update_groups(request, group_list)
 
         group.refresh_from_db()
 
@@ -215,7 +220,10 @@ class UpdateGroupsTest(TestCase):
             request.data = data["request_data"]
             request.GET = QueryDict(query_string=f"id={group.id}")
 
-            update_groups(request, request.GET.getlist("id"), [self.project], self.organization.id)
+            group_list = get_group_list(
+                self.organization.id, [self.project], request.GET.getlist("id")
+            )
+            update_groups(request, group_list)
 
             group.refresh_from_db()
 
@@ -233,7 +241,8 @@ class UpdateGroupsTest(TestCase):
         request.data = {"inbox": False}
         request.GET = QueryDict(query_string=f"id={group.id}")
 
-        update_groups(request, request.GET.getlist("id"), [self.project], self.organization.id)
+        group_list = get_group_list(self.organization.id, [self.project], request.GET.getlist("id"))
+        update_groups(request, group_list)
 
         group.refresh_from_db()
 
@@ -250,7 +259,8 @@ class UpdateGroupsTest(TestCase):
         request.data = {"status": "ignored", "substatus": "archived_until_escalating"}
         request.GET = QueryDict(query_string=f"id={group.id}")
 
-        update_groups(request, request.GET.getlist("id"), [self.project], self.organization.id)
+        group_list = get_group_list(self.organization.id, [self.project], request.GET.getlist("id"))
+        update_groups(request, group_list)
 
         group.refresh_from_db()
 
@@ -271,7 +281,8 @@ class MergeGroupsTest(TestCase):
         request.data = {"merge": 1}
         request.GET = {"id": group_ids, "project": [project.id]}
 
-        update_groups(request, group_ids, [project], self.organization.id)
+        group_list = get_group_list(self.organization.id, [project], group_ids)
+        update_groups(request, group_list)
 
         call_args = mock_handle_merge.call_args.args
 
@@ -298,7 +309,8 @@ class MergeGroupsTest(TestCase):
         request.data = {"merge": 1}
         request.GET = {"id": group_ids, "project": project_ids}
 
-        response = update_groups(request, group_ids, projects, self.organization.id)
+        group_list = get_group_list(self.organization.id, projects, group_ids)
+        response = update_groups(request, group_list)
 
         assert response.data == {"detail": "Merging across multiple projects is not supported"}
         assert response.status_code == 400
@@ -320,7 +332,8 @@ class MergeGroupsTest(TestCase):
         # we're passing multiple project ids
         request.GET = {"id": group_ids, "project": project_ids}
 
-        update_groups(request, group_ids, projects, self.organization.id)
+        group_list = get_group_list(self.organization.id, projects, group_ids)
+        update_groups(request, group_list)
 
         call_args = mock_handle_merge.call_args.args
 
@@ -342,7 +355,8 @@ class MergeGroupsTest(TestCase):
         request.data = {"merge": 1}
         request.GET = {"id": group_ids}
 
-        update_groups(request, group_ids, [project], self.organization.id)
+        group_list = get_group_list(self.organization.id, [project], group_ids)
+        update_groups(request, group_list)
 
         call_args = mock_handle_merge.call_args.args
 
@@ -393,7 +407,8 @@ class MergeGroupsTest(TestCase):
             request.META = {"HTTP_REFERER": referer}
 
             with patch("sentry.api.helpers.group_index.update.metrics.incr") as mock_metrics_incr:
-                update_groups(request, group_ids, [project], self.organization.id)
+                group_list = get_group_list(self.organization.id, [project], group_ids)
+                update_groups(request, group_list)
 
                 mock_metrics_incr.assert_any_call(
                     "grouping.merge_issues",
