0d5fbc70a9630cb4ca127519434bc5f01728b7b7
Dan Fuller <dfuller@sentry.io>
2024-12-19 12:21:38 +1100

    feat(uptime): Add region configuration and basic functions to interact with regions (#82287)
    This adds types and configs for interacting with regions. The idea is
    that when regions are configured, uptime monitors will look at the
    regions they currently have configured and then automatically add them
    in - that will come in a follow up pr.

---
+++ src/sentry/conf/types/uptime.py
@@ -0,0 +1,15 @@
+import dataclasses
+
+from sentry.conf.types.kafka_definition import Topic
+
+
+@dataclasses.dataclass
+class UptimeRegionConfig:
+    """
+    Defines a region which uptime checks can be run in.
+    """
+
+    slug: str
+    name: str
+    config_topic: Topic
+    enabled: bool
