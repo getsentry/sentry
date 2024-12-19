b1152f9b66c9008be0b586ea4f6b8bb315ef1b82
Ash Anand <ash.anand@sentry.io>
2024-12-19 15:12:10 -0500

    update copy and links

--- static/app/views/performance/newTraceDetails/traceDrawer/details/traceIntroductionView.tsx
+++ static/app/views/performance/newTraceDetails/traceDrawer/details/traceIntroductionView.tsx
@@ -8,8 +8,8 @@ import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {
   KEYBOARD_SHORTCUTS,
-  ShortcutsLayout,
   Shortcuts,
+  ShortcutsLayout,
   TIMELINE_SHORTCUTS,
 } from 'sentry/views/performance/newTraceDetails/traceShortcutsModal';
 
@@ -26,11 +26,31 @@ export function TraceIntroductionView() {
 
       <ContentWrapper>
         <InfoText>
-          {t(
-            `Welcome to the world of distributed tracing! Chances are, you’ve got multiple services connected and acting like dominos.
-            Traces help you understand what span operations cascade into another, debugging errors, slowdowns, etc.
-            Click any of the bars to get the nitty gritty details of what happened there or learn about some of the most popular workflows from here.`
-          )}
+          <p>
+            {t(
+              `If you’re seeing this, it means you’ve successfully set up Tracing in your SDK. Nice!`
+            )}
+          </p>
+
+          <p>
+            <strong>{t('What is this? ')}</strong>
+            {t(`
+              A trace is a map of everything that happens when a request moves through your stack—browser sessions, function calls, db queries, and third-party api requests.
+              Each recorded step is a “span” with timestamps and metadata. Put these spans together, and you get a “trace” showing how your system handled the request from start to finish.
+            `)}
+          </p>
+
+          <p>
+            <strong>{t('Why is this useful? ')}</strong>
+            {t(`
+             When something slows down or breaks, you won’t need to guess where the problem is hidden in your code.
+             You get a precise view into the entire chain of events so you can see context that you might not get in an exception handler.
+            `)}
+          </p>
+
+          <p>
+            <strong>{t('Learn how to use Tracing:')}</strong>
+          </p>
         </InfoText>
 
         <LinksWithImage>
@@ -69,49 +89,51 @@ function LinkSection() {
       <li>
         <ExternalLink
           openInNewTab
-          href="https://docs.sentry.io/workflows/traces/workflows/"
+          href="https://blog.sentry.io/everyone-needs-to-know-how-to-trace/"
         >
-          {t('How to debug an error')}
+          {t('What is Tracing, and how do I use it to debug?')}
         </ExternalLink>
       </li>
       <li>
         <ExternalLink
           openInNewTab
-          href="https://docs.sentry.io/workflows/traces/workflows/"
+          href="https://docs.sentry.io/product/tracing/#how-to-use-tracing-in-sentry"
         >
-          {t('How to find dependencies')}
+          {t('Product Walkthrough: Tracing')}
         </ExternalLink>
       </li>
       <li>
         <ExternalLink
           openInNewTab
-          href="https://docs.sentry.io/workflows/traces/workflows/"
+          href="https://docs.sentry.io/concepts/key-terms/tracing/distributed-tracing/#what-is-distributed-tracing"
         >
-          {t('What the heck is LCP?')}
+          {t('What is Distributed Tracing?')}
         </ExternalLink>
       </li>
       <li>
         <ExternalLink
           openInNewTab
-          href="https://docs.sentry.io/workflows/traces/workflows/"
+          href="https://blog.sentry.io/my-errors-are-gone-with-a-trace/"
         >
-          {t('What the heck is FCP?')}
+          {t('How does tracing show helpful context not found in errors?')}
+        </ExternalLink>
+      </li>
+      <li>
+        <ExternalLink openInNewTab href="https://docs.sentry.io/product/explore/traces/">
+          {t('Learn how to find trends with the Trace Explorer')}
         </ExternalLink>
       </li>
       <li>
         <ExternalLink
           openInNewTab
-          href="https://docs.sentry.io/workflows/traces/workflows/"
+          href="https://blog.sentry.io/backend-insights-with-caches-queues-requests-queries/"
         >
-          {t('What the heck is TTFB?')}
+          {t('Identifying backend issues with Performance Insights')}
         </ExternalLink>
       </li>
       <li>
-        <ExternalLink
-          openInNewTab
-          href="https://docs.sentry.io/workflows/traces/workflows/"
-        >
-          {t('How to yada yada yada')}
+        <ExternalLink openInNewTab href="https://docs.sentry.io/product/performance/">
+          {t('How are Performance Monitoring & Traces related?')}
         </ExternalLink>
       </li>
     </StyledList>
@@ -154,9 +176,7 @@ const ContentWrapper = styled('div')`
   font-size: ${p => p.theme.fontSizeMedium};
 `;
 
-const InfoText = styled('div')`
-  margin-bottom: ${space(3)};
-`;
+const InfoText = styled('div')``;
 
 const LinksWithImage = styled('div')`
   display: flex;
