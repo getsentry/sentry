b72cf4016eaaf0a968d0f7f59fd54c7f1d313b3e
Scott Cooper <scttcper@gmail.com>
2024-12-19 14:53:25 -0800

    fix(issues): Add gradient to tag bar (#82424)

--- static/app/views/issueDetails/groupTags/tagDetailsDrawerContent.tsx
+++ static/app/views/issueDetails/groupTags/tagDetailsDrawerContent.tsx
@@ -1,5 +1,6 @@
 import {Fragment, useState} from 'react';
 import styled from '@emotion/styled';
+import Color from 'color';
 import type {LocationDescriptor} from 'history';
 
 import {useFetchIssueTag, useFetchIssueTagValues} from 'sentry/actionCreators/group';
@@ -26,7 +27,6 @@ import {useNavigate} from 'sentry/utils/useNavigate';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
 import {hasDatasetSelector} from 'sentry/views/dashboards/utils';
-import {TagBar} from 'sentry/views/issueDetails/groupTags/tagDistribution';
 import {useIssueDetailsEventView} from 'sentry/views/issueDetails/streamline/hooks/useIssueDetailsDiscoverQuery';
 
 type TagSort = 'date' | 'count';
@@ -176,7 +176,7 @@ function TagDetailsRow({
       <RightAlignedValue>{tagValue.count.toLocaleString()}</RightAlignedValue>
       <RightAlignedValue>{displayPercentage}</RightAlignedValue>
       {tag.totalValues ? (
-        <TagBar style={{height: space(1.5)}} percentage={percentage} />
+        <TagBarContainer style={{width: `${percentage}%`, height: space(1.5)}} />
       ) : (
         '--'
       )}
@@ -384,4 +384,23 @@ const OverflowTimeSince = styled(TimeSince)`
 
 const ExternalLinkbutton = styled(Button)`
   color: ${p => p.theme.subText};
+`;
+
+const TagBarContainer = styled('div')`
+  height: ${space(1)};
+  position: relative;
+  flex: 1;
+  min-width: ${space(1)};
+  display: flex;
+  align-items: center;
+  &:before {
+    position: absolute;
+    inset: 0;
+    content: '';
+    border-radius: 3px;
+    background: ${p =>
+      `linear-gradient(to right, ${Color(p.theme.gray300).alpha(0.5).toString()} 20px, ${Color(p.theme.gray300).alpha(0.7).toString()} 100%)`};
+    box-shadow: inset 0 0 0 1px ${p => p.theme.translucentInnerBorder};
+    width: 100%;
+  }
 `;
