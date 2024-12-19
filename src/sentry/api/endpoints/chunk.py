c8558c0cd200f43212e8183358cb1ea444970d4a
Daniel Szoke <7881302+szokeasaurusrex@users.noreply.github.com>
2024-12-19 04:27:03 -0500

    feat(chunks): Declare Proguard chunk upload support (#82304)
    Update the `accept` field of the payload returned by the chunk upload
    endpoint to indicate that the Sentry server supports receiving
    chunk-uploaded Proguard mappings.
    
    Support for receiving chunk-uploaded Proguard mappings was added in
    https://github.com/getsentry/sentry/pull/81131.

--- src/sentry/api/endpoints/chunk.py
+++ src/sentry/api/endpoints/chunk.py
@@ -36,6 +36,7 @@ CHUNK_UPLOAD_ACCEPT = (
     "portablepdbs",  # Portable PDB debug file
     "artifact_bundles",  # Artifact Bundles for JavaScript Source Maps
     "artifact_bundles_v2",  # The `assemble` endpoint will check for missing chunks
+    "proguard",  # Chunk-uploaded proguard mappings
 )
 
 
