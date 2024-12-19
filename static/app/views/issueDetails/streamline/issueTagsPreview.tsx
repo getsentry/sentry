5ae9a0f18275fd7ffce04dc408cc50e033e8938e
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2024-12-18 16:26:01 -0800

    make preview better

--- static/app/views/issueDetails/streamline/issueTagsPreview.tsx
+++ static/app/views/issueDetails/streamline/issueTagsPreview.tsx
@@ -22,34 +22,33 @@ export default function IssueTagsPreview({
     environment: environments,
     limit: 3,
   });
-  const tagsToPreview = useMemo(() => {
+  const tagToPreview = useMemo(() => {
     const priorityTags = ['browser.name', 'os.name', 'runtime.name', 'environment'];
     // Sort tags based on priority order defined in priorityTags array
     const sortedTags = tags
       ?.filter(tag => priorityTags.includes(tag.key))
       .sort((a, b) => priorityTags.indexOf(a.key) - priorityTags.indexOf(b.key));
 
-    return sortedTags?.slice(0, 1) ?? [];
+    return sortedTags?.[0] ?? null;
   }, [tags]);
 
   if (isPending) {
     return (
-      <div style={{paddingTop: space(1)}}>
-        <Placeholder width="365px" height="95px" />
-      </div>
+      <LoadingContainer style={{paddingTop: space(1)}}>
+        <Placeholder width="155px" height="95px" />
+        <SectionDivider />
+      </LoadingContainer>
     );
   }
 
-  if (isError || tagsToPreview.length === 0) {
+  if (isError || !tagToPreview) {
     return null;
   }
 
   return (
     <Fragment>
       <TagsPreview>
-        {tagsToPreview.map(tag => (
-          <TagPreviewDistribution key={tag.key} tag={tag} />
-        ))}
+        <TagPreviewDistribution tag={tagToPreview} />
       </TagsPreview>
       <SectionDivider />
     </Fragment>
@@ -59,7 +58,16 @@ export default function IssueTagsPreview({
 const TagsPreview = styled('div')`
   padding-top: ${space(1)};
   max-width: 25%;
-  width: 145px;
+  width: 155px;
+
+  @media (max-width: ${p => p.theme.breakpoints.small}) {
+    display: none;
+  }
+`;
+
+const LoadingContainer = styled('div')`
+  padding-top: ${space(1)};
+  display: flex;
 `;
 
 const SectionDivider = styled('div')`
@@ -67,4 +75,8 @@ const SectionDivider = styled('div')`
   display: flex;
   align-items: center;
   margin: ${space(1)};
+
+  @media (max-width: ${p => p.theme.breakpoints.small}) {
+    display: none;
+  }
 `;
