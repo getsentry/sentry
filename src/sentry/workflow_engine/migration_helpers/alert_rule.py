f710abf9515df59489986c6247ec65640941c6aa
Michelle Fu <michelle.fu@sentry.io>
2024-12-20 12:45:14 -0800

    rebase dual delete helpers

--- src/sentry/workflow_engine/migration_helpers/alert_rule.py
+++ src/sentry/workflow_engine/migration_helpers/alert_rule.py
@@ -1,6 +1,9 @@
+from sentry.deletions.models.scheduleddeletion import RegionScheduledDeletion
 from sentry.incidents.grouptype import MetricAlertFire
 from sentry.incidents.models.alert_rule import AlertRule
+from sentry.incidents.utils.types import DATA_SOURCE_SNUBA_QUERY_SUBSCRIPTION
 from sentry.snuba.models import QuerySubscription, SnubaQuery
+from sentry.snuba.subscriptions import bulk_delete_snuba_subscriptions
 from sentry.users.services.user import RpcUser
 from sentry.workflow_engine.models import (
     AlertRuleDetector,
@@ -35,12 +38,10 @@ def create_data_source(
 ) -> DataSource | None:
     if not snuba_query:
         return None
-
     try:
         query_subscription = QuerySubscription.objects.get(snuba_query=snuba_query.id)
     except QuerySubscription.DoesNotExist:
         return None
-
     return DataSource.objects.create(
         organization_id=organization_id,
         query_id=query_subscription.id,
@@ -143,3 +144,55 @@ def migrate_alert_rule(
         alert_rule_workflow,
         detector_workflow,
     )
+
+
+def get_data_source(alert_rule: AlertRule) -> DataSource | None:
+    # TODO: if dual deleting, then we should delete the subscriptions here and not in logic.py
+    snuba_query = alert_rule.snuba_query
+    organization = alert_rule.organization
+    if not snuba_query or not organization:
+        # This shouldn't be possible, but just in case.
+        return None
+    try:
+        query_subscription = QuerySubscription.objects.get(snuba_query=snuba_query.id)
+    except QuerySubscription.DoesNotExist:
+        return None
+    try:
+        data_source = DataSource.objects.get(
+            organization=organization,
+            query_id=query_subscription.id,
+            type=DATA_SOURCE_SNUBA_QUERY_SUBSCRIPTION,
+        )
+    except DataSource.DoesNotExist:
+        return None
+    bulk_delete_snuba_subscriptions([QuerySubscription])
+    return data_source
+
+
+def dual_delete_migrated_alert_rule(
+    alert_rule: AlertRule,
+    user: RpcUser | None = None,
+) -> None:
+    try:
+        alert_rule_detector = AlertRuleDetector.objects.get(alert_rule=alert_rule)
+    except AlertRuleDetector.DoesNotExist:
+        # TODO: log failure
+        return
+
+    detector: Detector = alert_rule_detector.detector
+    data_condition_group: DataConditionGroup | None = detector.workflow_condition_group
+
+    data_source = get_data_source(alert_rule=alert_rule)
+    if data_source is None:
+        # TODO: log failure
+        return
+
+    # deleting the alert_rule also deletes alert_rule_workflow (in main delete logic)
+    # also deletes alert_rule_detector, detector_workflow, detector_state
+    RegionScheduledDeletion.schedule(instance=detector, days=0, actor=user)
+    # also deletes workflow_data_condition_group
+    if data_condition_group:
+        RegionScheduledDeletion.schedule(instance=data_condition_group, days=0, actor=user)
+    RegionScheduledDeletion.schedule(instance=data_source, days=0, actor=user)
+
+    return
