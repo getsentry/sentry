8f92c83f07189305eb004312d98f8cb645337c65
George Gritsouk <989898+gggritso@users.noreply.github.com>
2024-12-20 15:39:09 -0500

    ref(insights): Split out `getAxisMaxForPercentageSeries` (#82493)
    Moves this helper into its own file.

--- static/app/views/insights/http/components/charts/responseRateChart.tsx
+++ static/app/views/insights/http/components/charts/responseRateChart.tsx
@@ -7,6 +7,7 @@ import {
 } from 'sentry/views/insights/colors';
 import Chart, {ChartType} from 'sentry/views/insights/common/components/chart';
 import ChartPanel from 'sentry/views/insights/common/components/chartPanel';
+import {getAxisMaxForPercentageSeries} from 'sentry/views/insights/common/utils/getAxisMaxForPercentageSeries';
 import {DataTitles} from 'sentry/views/insights/common/views/spans/types';
 import {CHART_HEIGHT} from 'sentry/views/insights/http/settings';
 
@@ -45,19 +46,4 @@ export function ResponseRateChart({series, isLoading, error}: Props) {
       />
     </ChartPanel>
   );
-}
-
-/**
- * Given a set of `Series` objects that contain percentage data (i.e., every item in `data` has a `value` between 0 and 1) return an appropriate max value.
- *
- * e.g., for series with very low values (like 5xx rates), it rounds to the nearest significant digit. For other cases, it limits it to 100
- */
-export function getAxisMaxForPercentageSeries(series: Series[]): number {
-  const maxValue = Math.max(
-    ...series.map(serie => Math.max(...serie.data.map(datum => datum.value)))
-  );
-
-  const maxNumberOfDecimalPlaces = Math.ceil(Math.min(0, Math.log10(maxValue)));
-
-  return Math.pow(10, maxNumberOfDecimalPlaces);
 }
