8a305a5811d36c96114073000771674d616bcd7f
Alexander Tarasov <alex.tarasov@sentry.io>
2024-12-20 19:12:28 +0100

    fix(flags): separate permission class (#82463)

--- src/sentry/api/bases/organization.py
+++ src/sentry/api/bases/organization.py
@@ -230,6 +230,14 @@ class OrganizationMetricsPermission(OrganizationPermission):
     }
 
 
+class OrganizationFlagWebHookSigningSecretPermission(OrganizationPermission):
+    scope_map = {
+        "GET": ["org:read", "org:write", "org:admin"],
+        "POST": ["org:read", "org:write", "org:admin"],
+        "DELETE": ["org:write", "org:admin"],
+    }
+
+
 class ControlSiloOrganizationEndpoint(Endpoint):
     """
     A base class for endpoints that use an organization scoping but lives in the control silo
