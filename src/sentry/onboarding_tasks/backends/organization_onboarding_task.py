48454d4f977bd86d5597db763280e6655049b11b
Priscila Oliveira <priscila.oliveira@sentry.io>
2024-12-20 10:48:37 +0100

    fix(quick-start): Conditionally have source maps as required task (#82444)

--- src/sentry/onboarding_tasks/backends/organization_onboarding_task.py
+++ src/sentry/onboarding_tasks/backends/organization_onboarding_task.py
@@ -10,10 +10,12 @@ from sentry.models.organizationonboardingtask import (
     OnboardingTaskStatus,
     OrganizationOnboardingTask,
 )
+from sentry.models.project import Project
 from sentry.onboarding_tasks.base import OnboardingTaskBackend
 from sentry.users.models.user import User
 from sentry.users.services.user.model import RpcUser
 from sentry.utils import json
+from sentry.utils.platform_categories import SOURCE_MAPS
 
 
 class OrganizationOnboardingTaskBackend(OnboardingTaskBackend[OrganizationOnboardingTask]):
@@ -47,7 +49,21 @@ class OrganizationOnboardingTaskBackend(OnboardingTaskBackend[OrganizationOnboar
 
         organization = Organization.objects.get_from_cache(id=organization_id)
         if features.has("organizations:quick-start-updates", organization, actor=user):
-            required_tasks = OrganizationOnboardingTask.NEW_REQUIRED_ONBOARDING_TASKS
+
+            projects = Project.objects.filter(organization=organization)
+            project_with_source_maps = next(
+                (p for p in projects if p.platform in SOURCE_MAPS), None
+            )
+
+            # If a project supports source maps, we require them to complete the quick start.
+            # It's possible that the first project doesn't have source maps,
+            # but the second project (which users are guided to create in the "Add Sentry to other parts of the app" step) may have source maps.
+            required_tasks = (
+                OrganizationOnboardingTask.NEW_REQUIRED_ONBOARDING_TASKS_WITH_SOURCE_MAPS
+                if project_with_source_maps
+                else OrganizationOnboardingTask.NEW_REQUIRED_ONBOARDING_TASKS
+            )
+
         else:
             required_tasks = OrganizationOnboardingTask.REQUIRED_ONBOARDING_TASKS
 
