b5c79a21f6508ae31b47f4e1cf36a3ed1d70b38d
Nar Saynorath <nar.saynorath@sentry.io>
2024-12-23 11:51:07 -0500

    feat(widget-builder): Add RPC toggle for spans dataset (#82531)
    This uses a few tricks to set the `useRpc` flag in events/events-stats
    requests. We use local storage to store the key, but update the query
    params when the dataset is toggled so we can pick it up on the preview
    and re-query the dataset.
    
    I'm storing the state in local storage because I don't want to update
    all of the config interfaces just to support this temporary flag.

--- static/app/views/dashboards/widgetBuilder/components/newWidgetBuilder.tsx
+++ static/app/views/dashboards/widgetBuilder/components/newWidgetBuilder.tsx
@@ -11,6 +11,8 @@ import EventView from 'sentry/utils/discover/eventView';
 import {DiscoverDatasets} from 'sentry/utils/discover/types';
 import {MetricsCardinalityProvider} from 'sentry/utils/performance/contexts/metricsCardinality';
 import {MEPSettingProvider} from 'sentry/utils/performance/contexts/metricsEnhancedSetting';
+import {decodeBoolean} from 'sentry/utils/queryString';
+import useLocationQuery from 'sentry/utils/url/useLocationQuery';
 import useKeyPress from 'sentry/utils/useKeyPress';
 import {useLocation} from 'sentry/utils/useLocation';
 import useMedia from 'sentry/utils/useMedia';
@@ -21,6 +23,7 @@ import {
   type DashboardFilters,
   DisplayType,
   type Widget,
+  WidgetType,
 } from 'sentry/views/dashboards/types';
 import {
   DEFAULT_WIDGET_DRAG_POSITIONING,
@@ -166,6 +169,11 @@ export function WidgetPreviewContainer({
   const organization = useOrganization();
   const location = useLocation();
   const theme = useTheme();
+  const {useRpc} = useLocationQuery({
+    fields: {
+      useRpc: decodeBoolean,
+    },
+  });
 
   const isSmallScreen = useMedia(`(max-width: ${theme.breakpoints.small})`);
   // if small screen and draggable, enable dragging
@@ -247,6 +255,8 @@ export function WidgetPreviewContainer({
                   }}
                 >
                   <WidgetPreview
+                    // While we test out RPC for spans, force a re-render if the spans toggle changes
+                    key={state.dataset === WidgetType.SPANS && useRpc ? 'spans' : 'other'}
                     dashboardFilters={dashboardFilters}
                     dashboard={dashboard}
                     isWidgetInvalid={isWidgetInvalid}
