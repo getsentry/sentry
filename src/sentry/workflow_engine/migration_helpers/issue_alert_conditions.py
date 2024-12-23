febd643ad74856c375ce4ba3214b8d541501fd67
Cathy Teng <cathy.teng@sentry.io>
2024-12-23 15:46:31 -0800

    event frequency handler

--- src/sentry/workflow_engine/migration_helpers/issue_alert_conditions.py
+++ src/sentry/workflow_engine/migration_helpers/issue_alert_conditions.py
@@ -1,8 +1,10 @@
 from collections.abc import Callable
 from typing import Any
 
+from sentry.rules.conditions.event_frequency import EventFrequencyCondition
 from sentry.rules.conditions.reappeared_event import ReappearedEventCondition
 from sentry.rules.conditions.regression_event import RegressionEventCondition
+from sentry.utils import json
 from sentry.utils.registry import Registry
 from sentry.workflow_engine.models.data_condition import Condition, DataCondition
 from sentry.workflow_engine.models.data_condition_group import DataConditionGroup
@@ -30,12 +32,24 @@ def create_reappeared_event_data_condition(
 
 
 @data_condition_translator_registry.register(RegressionEventCondition.id)
-def create_regressed_event_data_condition(
+def create_regression_event_data_condition(
     data: dict[str, Any], dcg: DataConditionGroup
 ) -> DataCondition:
     return DataCondition.objects.create(
         type=Condition.REGRESSION_EVENT,
         comparison=True,
+        condition_result=True,
+        condition_group=dcg,
+    )
+
+
+@data_condition_translator_registry.register(EventFrequencyCondition.id)
+def create_event_frequency_data_condition(
+    data: dict[str, Any], dcg: DataConditionGroup
+) -> DataCondition:
+    return DataCondition.objects.create(
+        type=Condition.EVENT_FREQUENCY,
+        comparison=json.dumps({"interval": data["interval"], "value": data["value"]}),
         condition_result=True,
         condition_group=dcg,
     )
