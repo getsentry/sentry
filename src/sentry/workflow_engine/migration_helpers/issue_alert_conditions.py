5c676b1b685c803067fa0bf443f64396588ff4de
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2024-12-23 07:44:52 -0800

    feat(aci): registry for dual writing issue alert conditions (#82511)

---
+++ src/sentry/workflow_engine/migration_helpers/issue_alert_conditions.py
@@ -0,0 +1,41 @@
+from collections.abc import Callable
+from typing import Any
+
+from sentry.rules.conditions.reappeared_event import ReappearedEventCondition
+from sentry.rules.conditions.regression_event import RegressionEventCondition
+from sentry.utils.registry import Registry
+from sentry.workflow_engine.models.data_condition import Condition, DataCondition
+from sentry.workflow_engine.models.data_condition_group import DataConditionGroup
+
+data_condition_translator_registry = Registry[
+    Callable[[dict[str, Any], DataConditionGroup], DataCondition]
+]()
+
+
+def translate_to_data_condition(data: dict[str, Any], dcg: DataConditionGroup):
+    translator = data_condition_translator_registry.get(data["id"])
+    return translator(data, dcg)
+
+
+@data_condition_translator_registry.register(ReappearedEventCondition.id)
+def create_reappeared_event_data_condition(
+    data: dict[str, Any], dcg: DataConditionGroup
+) -> DataCondition:
+    return DataCondition.objects.create(
+        type=Condition.REAPPEARED_EVENT,
+        comparison=True,
+        condition_result=True,
+        condition_group=dcg,
+    )
+
+
+@data_condition_translator_registry.register(RegressionEventCondition.id)
+def create_regressed_event_data_condition(
+    data: dict[str, Any], dcg: DataConditionGroup
+) -> DataCondition:
+    return DataCondition.objects.create(
+        type=Condition.REGRESSION_EVENT,
+        comparison=True,
+        condition_result=True,
+        condition_group=dcg,
+    )
---
+++ tests/sentry/workflow_engine/handlers/condition/test_group_state_handlers.py
@@ -0,0 +1,79 @@
+from sentry.eventstream.base import GroupState
+from sentry.rules.conditions.reappeared_event import ReappearedEventCondition
+from sentry.rules.conditions.regression_event import RegressionEventCondition
+from sentry.workflow_engine.models.data_condition import Condition
+from sentry.workflow_engine.types import WorkflowJob
+from tests.sentry.workflow_engine.handlers.condition.test_base import ConditionTestCase
+
+
+class TestReappearedEventCondition(ConditionTestCase):
+    condition = Condition.REAPPEARED_EVENT
+    rule_cls = ReappearedEventCondition
+    payload = {"id": ReappearedEventCondition.id}
+
+    def test(self):
+        job = WorkflowJob(
+            {
+                "event": self.group_event,
+                "has_reappeared": True,
+            }
+        )
+        dc = self.create_data_condition(
+            type=self.condition,
+            comparison=True,
+            condition_result=True,
+        )
+
+        self.assert_passes(dc, job)
+
+        job["has_reappeared"] = False
+        self.assert_does_not_pass(dc, job)
+
+    def test_dual_write(self):
+        dcg = self.create_data_condition_group()
+        dc = self.translate_to_data_condition(self.payload, dcg)
+
+        assert dc.type == self.condition
+        assert dc.comparison is True
+        assert dc.condition_result is True
+        assert dc.condition_group == dcg
+
+
+class TestRegressionEventCondition(ConditionTestCase):
+    condition = Condition.REGRESSION_EVENT
+    rule_cls = RegressionEventCondition
+    payload = {"id": RegressionEventCondition.id}
+
+    def test(self):
+        job = WorkflowJob(
+            {
+                "event": self.group_event,
+                "group_state": GroupState(
+                    {
+                        "id": 1,
+                        "is_regression": True,
+                        "is_new": False,
+                        "is_new_group_environment": False,
+                    }
+                ),
+            }
+        )
+        dc = self.create_data_condition(
+            type=self.condition,
+            comparison=True,
+            condition_result=True,
+        )
+
+        self.assert_passes(dc, job)
+
+        job["group_state"]["is_regression"] = False
+        self.assert_does_not_pass(dc, job)
+
+    def test_dual_write(self):
+        dcg = self.create_data_condition_group()
+        dc = self.translate_to_data_condition(self.payload, dcg)
+
+        assert dc.type == self.condition
+        assert dc.comparison is True
+        assert dc.condition_result is True
+        assert dc.condition_group == dcg
