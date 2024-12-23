d6a355edda644a3faf1756fd50ddc095b5ec5bfd
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-12-23 11:36:20 -0500

    ref: rm browserHistory from useFiltersInLocationQuery and tests (#82530)

--- static/app/views/replays/detail/tagPanel/useTagFilters.spec.tsx
+++ static/app/views/replays/detail/tagPanel/useTagFilters.spec.tsx
@@ -3,22 +3,18 @@ import {ReplayRecordFixture} from 'sentry-fixture/replayRecord';
 
 import {renderHook} from 'sentry-test/reactTestingLibrary';
 
-import {browserHistory} from 'sentry/utils/browserHistory';
 import {useLocation} from 'sentry/utils/useLocation';
 import type {FilterFields} from 'sentry/views/replays/detail/tagPanel/useTagFilters';
 import useTagFilters from 'sentry/views/replays/detail/tagPanel/useTagFilters';
 
 jest.mock('sentry/utils/useLocation');
+jest.mock('sentry/utils/useNavigate');
 
 const mockUseLocation = jest.mocked(useLocation);
 
 const tags = ReplayRecordFixture().tags;
 
 describe('useTagsFilters', () => {
-  beforeEach(() => {
-    jest.mocked(browserHistory.push).mockReset();
-  });
-
   it('should not filter anything when no values are set', () => {
     mockUseLocation.mockReturnValue({
       pathname: '/',
