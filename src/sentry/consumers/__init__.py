e04bf96e25acb4790181a280345b94e37b0ac6c8
Lyn Nagara <1779792+lynnagara@users.noreply.github.com>
2024-12-23 15:20:01 -0800

    support stale topic for ingest-events (#82549)
    similarly to transactions, will get a dedicated lowpri topic later, for
    now testing by sending stale events to ingest-events-dlq

--- src/sentry/consumers/__init__.py
+++ src/sentry/consumers/__init__.py
@@ -347,6 +347,7 @@ KAFKA_CONSUMERS: Mapping[str, ConsumerDefinition] = {
             "consumer_type": ConsumerType.Events,
         },
         "dlq_topic": Topic.INGEST_EVENTS_DLQ,
+        "stale_topic": Topic.INGEST_EVENTS_DLQ,
     },
     "ingest-feedback-events": {
         "topic": Topic.INGEST_FEEDBACK_EVENTS,
