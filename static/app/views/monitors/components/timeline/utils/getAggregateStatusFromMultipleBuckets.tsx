f3d1348431e26b7cd01e232b8f5eba3075435687
Mia Hsu <ameliahsu@gmail.com>
2024-12-26 11:27:34 -0800

    getAggregateStatus

--- static/app/views/monitors/components/timeline/utils/getAggregateStatusFromMultipleBuckets.tsx
+++ static/app/views/monitors/components/timeline/utils/getAggregateStatusFromMultipleBuckets.tsx
@@ -1,7 +1,10 @@
-import type {MonitorBucketEnvMapping} from '../types';
+import type {MonitorBucketEnvMapping, StatsBucket} from '../types';
 
 import {CHECKIN_STATUS_PRECEDENT} from './constants';
-import {getAggregateStatus} from './getAggregateStatus';
+import {
+  getAggregateStatus,
+  getAggregateStatusFromStatsBucket,
+} from './getAggregateStatus';
 
 /**
  * Given multiple env buckets [{prod: {ok: 1, ...}, {prod: {ok: 0, ...}}]
@@ -12,6 +15,23 @@ export function getAggregateStatusFromMultipleBuckets(
 ) {
   return envDataArr
     .map(getAggregateStatus)
+    .reduce(
+      (aggregateStatus, currentStatus) =>
+        CHECKIN_STATUS_PRECEDENT.indexOf(currentStatus) >
+        CHECKIN_STATUS_PRECEDENT.indexOf(aggregateStatus)
+          ? currentStatus
+          : aggregateStatus,
+      CHECKIN_STATUS_PRECEDENT[0]
+    );
+}
+
+/**
+ * Given multiple stats buckets [{ok: 1, ...}, {ok: 0, ...}]
+ * returns the aggregate status across all buckets
+ */
+export function getAggregateStatusFromMultipleStatsBuckets(statsArr: StatsBucket[]) {
+  return statsArr
+    .map(getAggregateStatusFromStatsBucket)
     .reduce(
       (aggregateStatus, currentStatus) =>
         CHECKIN_STATUS_PRECEDENT.indexOf(currentStatus) >
