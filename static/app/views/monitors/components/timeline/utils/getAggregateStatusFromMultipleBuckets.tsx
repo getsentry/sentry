18def23d16b135d73dffc35da3c5cfd33f2c6fd3
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2024-12-26 13:46:40 -0800

    ref(crons): new merge/aggregate status functions without `MonitorBucketEnvMapping` (#82566)
    introducing new functions to replace:
    - `getAggregateStatus`
    - `getAggregateStatusFromMultipleBuckets`
    - `mergeEnvMappings`
    
    since `checkinTimeline` doesn't need to know anything about environment,
    we want to change these functions to take in `StatsBucket` instead of
    `MonitorBucketEnvMapping` types
    
    replacing usage + deleting old functions will come in following PRs

--- static/app/views/monitors/components/timeline/utils/getAggregateStatusFromMultipleBuckets.tsx
+++ static/app/views/monitors/components/timeline/utils/getAggregateStatusFromMultipleBuckets.tsx
@@ -1,7 +1,10 @@
-import type {MonitorBucketEnvMapping} from '../types';
+import type {MonitorBucketEnvMapping, StatsBucket} from '../types';
 
 import {CHECKIN_STATUS_PRECEDENT} from './constants';
-import {getAggregateStatus} from './getAggregateStatus';
+import {
+  getAggregateStatus,
+  getAggregateStatusFromStatsBucket,
+} from './getAggregateStatus';
 
 /**
  * Given multiple env buckets [{prod: {ok: 1, ...}, {prod: {ok: 0, ...}}]
@@ -12,6 +15,23 @@ export function getAggregateStatusFromMultipleBuckets(
 ) {
   return envDataArr
     .map(getAggregateStatus)
+    .reduce(
+      (aggregateStatus, currentStatus) =>
+        CHECKIN_STATUS_PRECEDENT.indexOf(currentStatus) >
+        CHECKIN_STATUS_PRECEDENT.indexOf(aggregateStatus)
+          ? currentStatus
+          : aggregateStatus,
+      CHECKIN_STATUS_PRECEDENT[0]
+    );
+}
+
+/**
+ * Given multiple stats buckets [{..., error: 1, unknown: 0}, {..., error: 0, unknown: 4}]
+ * returns the aggregate status across all buckets (unknown)
+ */
+export function getAggregateStatusFromMultipleStatsBuckets(statsArr: StatsBucket[]) {
+  return statsArr
+    .map(getAggregateStatusFromStatsBucket)
     .reduce(
       (aggregateStatus, currentStatus) =>
         CHECKIN_STATUS_PRECEDENT.indexOf(currentStatus) >
