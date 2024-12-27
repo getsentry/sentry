ab8e34b00ce06957ef2d653d16c76836a5155580
Mia Hsu <ameliahsu@gmail.com>
2024-12-27 13:13:59 -0800

    ExistingHighPriorityIssueCondition

--- src/sentry/workflow_engine/handlers/condition/group_state_handlers.py
+++ src/sentry/workflow_engine/handlers/condition/group_state_handlers.py
@@ -1,5 +1,6 @@
 from typing import Any
 
+from sentry.types.group import PriorityLevel
 from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.registry import condition_handler_registry
 from sentry.workflow_engine.types import DataConditionHandler, WorkflowJob
@@ -25,3 +26,17 @@ class ReappearedEventConditionHandler(DataConditionHandler[WorkflowJob]):
             return False
 
         return has_reappeared == comparison
+
+
+@condition_handler_registry.register(Condition.EXISTING_HIGH_PRIORITY_ISSUE)
+class ExistingHighPriorityIssueConditionHandler(DataConditionHandler[WorkflowJob]):
+    @staticmethod
+    def evaluate_value(job: WorkflowJob, comparison: Any) -> bool:
+        state = job.get("group_state", None)
+        if state is None or state["is_new"]:
+            return False
+
+        has_reappeared = job.get("has_reappeared", None)
+        has_escalated = job.get("has_escalated", None)
+        is_escalating = has_reappeared or has_escalated
+        return is_escalating and job["event"].group.priority == PriorityLevel.HIGH
