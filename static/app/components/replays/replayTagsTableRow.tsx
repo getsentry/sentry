26b15d8100f61222183880471b9c9a9d29fb030f
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-12-30 18:38:26 -0500

    ref(replay): add collapsible row to mobile tags

--- static/app/components/replays/replayTagsTableRow.tsx
+++ static/app/components/replays/replayTagsTableRow.tsx
@@ -8,11 +8,34 @@ import {KeyValueTableRow} from 'sentry/components/keyValueTable';
 import Link from 'sentry/components/links/link';
 import {Tooltip} from 'sentry/components/tooltip';
 import Version from 'sentry/components/version';
+import {CollapsibleValue} from 'sentry/components/structuredEventData/collapsibleValue';
+import {space} from 'sentry/styles/space';
 
 interface Props {
   name: string;
   values: ReactNode[];
   generateUrl?: (name: string, value: ReactNode) => LocationDescriptor;
+}
+
+const expandedViewKeys = [
+  'sdk.replay.maskedViewClasses',
+  'sdk.replay.unmaskedViewClasses',
+];
+
+function renderValueList(values: ReactNode[]) {
+  const valueItems = values[0] as Array<string>;
+  if (!valueItems.length) {
+    return undefined;
+  }
+
+  return valueItems.map(value => {
+    return (
+      <Fragment>
+        <AnnotatedText value={value} />
+        <br />
+      </Fragment>
+    );
+  });
 }
 
 function ReplayTagsTableRow({name, values, generateUrl}: Props) {
@@ -24,6 +47,17 @@ function ReplayTagsTableRow({name, values, generateUrl}: Props) {
           <Version key={index} version={String(value)} anchor={false} withPackage />
         </Fragment>
       ));
+    }
+    if (expandedViewKeys.includes(name)) {
+      if (!renderValueList(values)) {
+        return '[ ]';
+      }
+
+      return (
+        <CollapsibleValue openTag="[" closeTag="]" path="$" noBasePadding>
+          {renderValueList(values)}
+        </CollapsibleValue>
+      );
     }
 
     return values.map((value, index) => {
@@ -46,15 +80,35 @@ function ReplayTagsTableRow({name, values, generateUrl}: Props) {
         </StyledTooltip>
       }
       value={
-        <StyledTooltip title={renderTagValue} isHoverable showOnlyOnOverflow>
-          {renderTagValue}
-        </StyledTooltip>
+        <ValueContainer>
+          <StyledTooltip
+            overlayStyle={
+              expandedViewKeys.includes(name) ? {textAlign: 'left'} : undefined
+            }
+            title={
+              expandedViewKeys.includes(name) ? renderValueList(values) : renderTagValue
+            }
+            isHoverable
+            showOnlyOnOverflow
+          >
+            {renderTagValue}
+          </StyledTooltip>
+        </ValueContainer>
       }
     />
   );
 }
 
 export default ReplayTagsTableRow;
+
+const ValueContainer = styled('div')`
+  span {
+    font-size: ${p => p.theme.fontSizeMedium};
+  }
+  display: flex;
+  padding: ${space(0.25)};
+  justify-content: flex-end;
+`;
 
 const StyledTooltip = styled(Tooltip)`
   ${p => p.theme.overflowEllipsis};
