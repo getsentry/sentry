241b222c344f08ee2b58649596ea9e746ba91587
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2024-12-30 13:50:17 -0500

    chore(api-docs): Update documentation for group_tagkey_values (#79801)
    Updates documentation for the tags/{tag}/values endpoint 
    
    Before:
    
    <img width="1479" alt="image"
    src="https://github.com/user-attachments/assets/7056f9a2-6dfb-4de5-9d22-6f34e887840e">
    
    
    After:
    
    <img width="1483" alt="image"
    src="https://github.com/user-attachments/assets/d0fef18e-fda4-4de2-aabb-b45febefaf6f">

--- src/sentry/apidocs/examples/tags_examples.py
+++ src/sentry/apidocs/examples/tags_examples.py
@@ -7,17 +7,17 @@ SIMPLE_TAG_DETAILS = {
     "totalValues": 3,
     "topValues": [
         {
-            "key": "chunkymonkey",
-            "name": "Chunky Monkey",
-            "value": "chunkymonkey",
+            "key": "strawberry",
+            "name": "Strawberry",
+            "value": "strawberry",
             "count": 2,
             "lastSeen": "2024-01-01T00:00:00Z",
             "firstSeen": "2024-01-01T00:00:00Z",
         },
         {
-            "key": "halfbaked",
-            "name": "Half Baked",
-            "value": "halfbaked",
+            "key": "vanilla",
+            "name": "Vanilla",
+            "value": "vanilla",
             "count": 1,
             "lastSeen": "2024-01-01T00:00:00Z",
             "firstSeen": "2024-01-01T00:00:00Z",
@@ -25,11 +25,53 @@ SIMPLE_TAG_DETAILS = {
     ],
 }
 
+SIMPLE_TAG_VALUES = [
+    {
+        "key": "strawberry",
+        "name": "Strawberry",
+        "value": "strawberry",
+        "count": 2,
+        "lastSeen": "2024-01-01T00:00:00Z",
+        "firstSeen": "2024-01-01T00:00:00Z",
+    },
+    {
+        "key": "vanilla",
+        "name": "Vanilla",
+        "value": "vanilla",
+        "count": 1,
+        "lastSeen": "2024-01-01T00:00:00Z",
+        "firstSeen": "2024-01-01T00:00:00Z",
+    },
+    {
+        "key": "chocolate",
+        "name": "Chocolate",
+        "value": "chocolate",
+        "count": 1,
+        "lastSeen": "2024-01-01T00:00:00Z",
+        "firstSeen": "2024-01-01T00:00:00Z",
+    },
+    {
+        "key": "Neopolitan",
+        "name": "Neopolitan",
+        "value": "neopolitan",
+        "count": 1,
+        "lastSeen": "2024-01-01T00:00:00Z",
+        "firstSeen": "2024-01-01T00:00:00Z",
+    },
+]
+
 
 class TagsExamples:
     GROUP_TAGKEY_DETAILS = OpenApiExample(
         "Return a specific tag's details",
         value=SIMPLE_TAG_DETAILS,
+        response_only=True,
+        status_codes=["200"],
+    )
+
+    GROUP_TAGKEY_VALUES = OpenApiExample(
+        "Return all tag values for a specific tag",
+        value=SIMPLE_TAG_VALUES,
         response_only=True,
         status_codes=["200"],
     )
