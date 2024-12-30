dee90c87f145c8206d6361412b6b1c06d4a4941a
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2024-12-30 10:39:49 -0500

    :recycle: change to rn

--- static/app/components/replays/configureMobileReplayCard.tsx
+++ static/app/components/replays/configureMobileReplayCard.tsx
@@ -13,17 +13,17 @@ function ResourceButtons() {
       <Resource
         title={t('General')}
         subtitle={t('Configure sampling rates and recording thresholds')}
-        link="https://docs.sentry.io/platforms/android/session-replay/#sampling"
+        link="https://docs.sentry.io/platforms/react-native/session-replay/#sampling"
       />
       <Resource
         title={t('Element Masking/Blocking')}
         subtitle={t('Unmask text (****) and unblock media (img, svg, video, etc.)')}
-        link="https://docs.sentry.io/platforms/android/session-replay/#privacy"
+        link="https://docs.sentry.io/platforms/react-native/session-replay/#privacy"
       />
       <Resource
         title={t('Identify Users')}
         subtitle={t('Identify your users through a specific attribute, such as email.')}
-        link="https://docs.sentry.io/platforms/android/enriching-events/identify-user/"
+        link="https://docs.sentry.io/platforms/react-native/enriching-events/identify-user/"
       />
     </ButtonContainer>
   );
