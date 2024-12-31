6c437f180c04348f21075d17b6361011af1dec84
Ryan Albrecht <ryan.albrecht@sentry.io>
2024-12-31 11:36:43 -0800

    no need to define rules that are already included in recommended set, or disabled by default

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -756,40 +756,9 @@ export default typescript.config([
         {eventHandlerPrefix: 'handle', eventHandlerPropPrefix: 'on'},
       ],
 
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-key.md
-      'react/jsx-key': 'error',
-
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-no-undef.md
-      'react/jsx-no-undef': 'error',
-
       // Disabled as we use the newer JSX transform babel plugin.
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-react.md
       'react/jsx-uses-react': 'off',
-
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/jsx-uses-vars.md
-      'react/jsx-uses-vars': 'error',
-
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-deprecated.md
-      'react/no-deprecated': 'error',
-
-      // Prevent usage of the return value of React.render
-      // deprecation: https://facebook.github.io/react/docs/react-dom.html#render
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-render-return-value.md
-      'react/no-render-return-value': 'error',
-
-      // Children should always be actual children, not passed in as a prop.
-      // When using JSX, the children should be nested between the opening and closing tags. When not using JSX, the children should be passed as additional arguments to React.createElement.
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-children-prop.md
-      'react/no-children-prop': 'error',
-
-      // This rule helps prevent problems caused by using children and the dangerouslySetInnerHTML prop at the same time.
-      // React will throw a warning if this rule is ignored.
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-danger-with-children.md
-      'react/no-danger-with-children': 'error',
-
-      // Prevent direct mutation of this.state
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-direct-mutation-state.md
-      'react/no-direct-mutation-state': 'error',
 
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-did-mount-set-state.md
       'react/no-did-mount-set-state': 'error',
@@ -810,18 +779,8 @@ export default typescript.config([
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unknown-property.md
       'react/no-unknown-property': ['error', {ignore: ['css']}],
 
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-unused-prop-types.md
-      // Disabled since this currently fails to correctly detect a lot of
-      // typescript prop type usage.
-      'react/no-unused-prop-types': 'off',
-
       // We do not need proptypes since we're using typescript
       'react/prop-types': 'off',
-
-      // When writing the render method in a component it is easy to forget to return the JSX content.
-      // This rule will warn if the return statement is missing.
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/require-render-return.md
-      'react/require-render-return': 'error',
 
       // Disabled as we are using the newer JSX transform babel plugin.
       // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
@@ -846,17 +805,6 @@ export default typescript.config([
         'error',
         {namedComponents: 'function-declaration'},
       ],
-
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-is-mounted.md
-      'react/no-is-mounted': 'error',
-
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-find-dom-node.md
-      // Recommended to use callback refs instead
-      'react/no-find-dom-node': 'error',
-
-      // https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/no-string-refs.md
-      // This is now considered legacy, callback refs preferred
-      'react/no-string-refs': 'error',
     },
   },
   {
