48e08d08745787c55bf055846d82846255c8953b
JonasBa <jonas@badalic.com>
2024-12-31 17:52:25 -0500

    fix

--- static/app/components/metrics/chart/chart.tsx
+++ static/app/components/metrics/chart/chart.tsx
@@ -142,9 +142,9 @@ export const MetricChart = memo(
         }
       });
 
-      const bucketSize = series[0]!.data[1]!.name - series[0]!.data[0]!.name;
+      const bucketSize = series[0]!?.data[1]!?.name - series[0]!?.data[0]!?.name;
       const isSubMinuteBucket = bucketSize < 60_000;
-      const lastBucketTimestamp = series[0]!.data?.[series[0]!.data?.length - 1]!.name;
+      const lastBucketTimestamp = series[0]!?.data?.[series[0]!?.data?.length - 1]!?.name;
       const ingestionBuckets = useMemo(
         () => getIngestionDelayBucketCount(bucketSize, lastBucketTimestamp),
         [bucketSize, lastBucketTimestamp]
