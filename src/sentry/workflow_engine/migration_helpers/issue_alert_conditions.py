b30111a66b4463fb2b0b6b10478b2c5c375be1f9
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-01-02 12:08:27 -0800

    feat(workflow-engine): add `EventAttributeConditionHandler` (#82741)
    add `EventAttributeConditionHandler`

--- src/sentry/workflow_engine/migration_helpers/issue_alert_conditions.py
+++ src/sentry/workflow_engine/migration_helpers/issue_alert_conditions.py
@@ -1,10 +1,12 @@
 from collections.abc import Callable
 from typing import Any
 
+from sentry.rules.conditions.event_attribute import EventAttributeCondition
 from sentry.rules.conditions.every_event import EveryEventCondition
 from sentry.rules.conditions.existing_high_priority_issue import ExistingHighPriorityIssueCondition
 from sentry.rules.conditions.reappeared_event import ReappearedEventCondition
 from sentry.rules.conditions.regression_event import RegressionEventCondition
+from sentry.utils import json
 from sentry.utils.registry import Registry
 from sentry.workflow_engine.models.data_condition import Condition, DataCondition
 from sentry.workflow_engine.models.data_condition_group import DataConditionGroup
@@ -62,6 +64,19 @@ def create_existing_high_priority_issue_data_condition(
     return DataCondition.objects.create(
         type=Condition.EXISTING_HIGH_PRIORITY_ISSUE,
         comparison=True,
+        condition_result=True,
+        condition_group=dcg,
+    )
+
+
+@data_condition_translator_registry.register(EventAttributeCondition.id)
+def create_event_attribute_data_condition(
+    data: dict[str, Any], dcg: DataConditionGroup
+) -> DataCondition:
+    comparison = {"match": data["match"], "value": data["value"], "attribute": data["attribute"]}
+    return DataCondition.objects.create(
+        type=Condition.EVENT_ATTRIBUTE,
+        comparison=json.dumps(comparison),
         condition_result=True,
         condition_group=dcg,
     )
