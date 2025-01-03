416b060ed882d2018cf0382b4f9f09668b530794
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-01-03 13:57:52 -0800

    feat(workflow-engine): add `LevelConditionHandler` (#82874)
    add `LevelConditionHandler`

--- src/sentry/workflow_engine/handlers/condition/group_event_handlers.py
+++ src/sentry/workflow_engine/handlers/condition/group_event_handlers.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import sentry_sdk
 
+from sentry.constants import LOG_LEVELS_MAP
 from sentry.eventstore.models import GroupEvent
 from sentry.rules import MatchType, match_values
 from sentry.rules.conditions.event_attribute import attribute_registry
@@ -83,3 +84,35 @@ class EventAttributeConditionHandler(DataConditionHandler[WorkflowJob]):
         return match_values(
             group_values=attribute_values, match_value=desired_value, match_type=match
         )
+
+
+@condition_handler_registry.register(Condition.LEVEL)
+class LevelConditionHandler(DataConditionHandler[WorkflowJob]):
+    @staticmethod
+    def evaluate_value(job: WorkflowJob, comparison: Any) -> bool:
+        event = job["event"]
+        level_name = event.get_tag("level")
+        if level_name is None:
+            return False
+
+        desired_level_raw = comparison.get("level")
+        desired_match = comparison.get("match")
+
+        if not desired_level_raw or not desired_match:
+            return False
+
+        desired_level = int(desired_level_raw)
+        # Fetch the event level from the tags since event.level is
+        # event.group.level which may have changed
+        try:
+            level: int = LOG_LEVELS_MAP[level_name]
+        except KeyError:
+            return False
+
+        if desired_match == MatchType.EQUAL:
+            return level == desired_level
+        elif desired_match == MatchType.GREATER_OR_EQUAL:
+            return level >= desired_level
+        elif desired_match == MatchType.LESS_OR_EQUAL:
+            return level <= desired_level
+        return False
