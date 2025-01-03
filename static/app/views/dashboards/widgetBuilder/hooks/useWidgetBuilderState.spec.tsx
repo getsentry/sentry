75415d00e044d413efeb679023d565a12245244f
Nar Saynorath <nar.saynorath@sentry.io>
2025-01-03 15:45:05 -0500

    fix(widget-builder): Field duplicating when toggling datasets (#82883)
    If you set the big number display type, then switch to another dataset
    and back, then y-axis gets populated because of the `else` statement
    checking for `DisplayType.TABLE`. Then when you change the type to
    `DisplayType.TABLE`, you'll see an extra `count()` being added.
    
    I found the same behaviour occurs when switching datasets with a
    line chart displayed, this is because we were setting `fields` and then
    setting the y-axis, but when it's a timeseries chart we should be
    clearing the `fields` state.

--- static/app/views/dashboards/widgetBuilder/hooks/useWidgetBuilderState.spec.tsx
+++ static/app/views/dashboards/widgetBuilder/hooks/useWidgetBuilderState.spec.tsx
@@ -321,6 +321,61 @@ describe('useWidgetBuilderState', () => {
       ]);
     });
 
+    it('does not duplicate fields when switching dataset in line chart then display type to table', () => {
+      mockedUsedLocation.mockReturnValue(
+        LocationFixture({
+          query: {
+            displayType: DisplayType.LINE,
+            dataset: WidgetType.ERRORS,
+            yAxis: ['count()'],
+          },
+        })
+      );
+
+      const {result} = renderHook(() => useWidgetBuilderState(), {
+        wrapper: WidgetBuilderProvider,
+      });
+
+      expect(result.current.state.yAxis).toEqual([
+        {
+          function: ['count', '', undefined, undefined],
+          alias: undefined,
+          kind: 'function',
+        },
+      ]);
+
+      act(() => {
+        result.current.dispatch({
+          type: BuilderStateAction.SET_DATASET,
+          payload: WidgetType.TRANSACTIONS,
+        });
+      });
+
+      expect(result.current.state.yAxis).toEqual([
+        {
+          function: ['count', '', undefined, undefined],
+          alias: undefined,
+          kind: 'function',
+        },
+      ]);
+      expect(result.current.state.fields).toEqual([]);
+
+      act(() => {
+        result.current.dispatch({
+          type: BuilderStateAction.SET_DISPLAY_TYPE,
+          payload: DisplayType.TABLE,
+        });
+      });
+
+      expect(result.current.state.fields).toEqual([
+        {
+          function: ['count', '', undefined, undefined],
+          alias: undefined,
+          kind: 'function',
+        },
+      ]);
+    });
+
     it('does not duplicate fields when changing display from table to chart', () => {
       mockedUsedLocation.mockReturnValue(
         LocationFixture({
@@ -370,6 +425,61 @@ describe('useWidgetBuilderState', () => {
       expect(result.current.state.yAxis).toEqual([
         {
           function: ['count', 'span.duration', undefined, undefined],
+          alias: undefined,
+          kind: 'function',
+        },
+      ]);
+    });
+
+    it('does not duplicate fields when switching dataset in big number then display type to table', () => {
+      mockedUsedLocation.mockReturnValue(
+        LocationFixture({
+          query: {
+            displayType: DisplayType.BIG_NUMBER,
+            dataset: WidgetType.ERRORS,
+            field: ['count()'],
+          },
+        })
+      );
+
+      const {result} = renderHook(() => useWidgetBuilderState(), {
+        wrapper: WidgetBuilderProvider,
+      });
+
+      expect(result.current.state.fields).toEqual([
+        {
+          function: ['count', '', undefined, undefined],
+          alias: undefined,
+          kind: 'function',
+        },
+      ]);
+
+      act(() => {
+        result.current.dispatch({
+          type: BuilderStateAction.SET_DATASET,
+          payload: WidgetType.TRANSACTIONS,
+        });
+      });
+
+      expect(result.current.state.fields).toEqual([
+        {
+          function: ['count', '', undefined, undefined],
+          alias: undefined,
+          kind: 'function',
+        },
+      ]);
+      expect(result.current.state.yAxis).toEqual([]);
+
+      act(() => {
+        result.current.dispatch({
+          type: BuilderStateAction.SET_DISPLAY_TYPE,
+          payload: DisplayType.TABLE,
+        });
+      });
+
+      expect(result.current.state.fields).toEqual([
+        {
+          function: ['count', '', undefined, undefined],
           alias: undefined,
           kind: 'function',
         },
