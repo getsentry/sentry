38fbdc51e6c7048ccb83494b3822173196dc4e6c
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-01-06 15:13:21 -0800

    ref(crons): new `mergeBuckets` timeline function without env (#82569)
    new `mergeBuckets` function that merges new `monitorBucketWithStats`
    type
    
    since `checkinTimeline` doesn't need to know anything about environment,
    we want to replace `MonitorBucketEnvMapping` fields with `StatsBucket`

--- static/app/views/monitors/components/timeline/utils/isEnvMappingEmpty.tsx
+++ static/app/views/monitors/components/timeline/utils/isEnvMappingEmpty.tsx
@@ -1,8 +1,12 @@
-import type {MonitorBucketEnvMapping} from '../types';
+import type {MonitorBucketEnvMapping, StatsBucket} from '../types';
 
 /**
  * Determines if an environment mapping includes any job run data
  */
 export function isEnvMappingEmpty(envMapping: MonitorBucketEnvMapping) {
   return Object.keys(envMapping).length === 0;
+}
+
+export function isStatsBucketEmpty(stats: StatsBucket): boolean {
+  return Object.values(stats).every(value => value === 0);
 }
