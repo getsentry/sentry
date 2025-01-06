80acdf8086b4b826ad762da6687b9e58fb34556d
Nar Saynorath <nar.saynorath@sentry.io>
2025-01-06 16:41:38 -0500

    fix(widget-builder): Deep clone new field so updates don't alias (#82957)
    When adding new fields, we were just adding the default query each time
    which would always point to the same memory address. We need to clone it
    so each additional field is treated individually and modifying the
    fields array doesn't trickle changes to the other added fields because
    they reference the same object.
    
    Closes #82945

--- static/app/views/dashboards/widgetBuilder/components/visualize.spec.tsx
+++ static/app/views/dashboards/widgetBuilder/components/visualize.spec.tsx
@@ -606,6 +606,48 @@ describe('Visualize', () => {
     expect(screen.queryByRole('option', {name: 'None'})).not.toBeInTheDocument();
   });
 
+  it('updates only the selected field', async () => {
+    render(
+      <WidgetBuilderProvider>
+        <Visualize />
+      </WidgetBuilderProvider>,
+      {
+        organization,
+        router: RouterFixture({
+          location: LocationFixture({
+            query: {
+              dataset: WidgetType.TRANSACTIONS,
+              displayType: DisplayType.TABLE,
+              field: ['count_unique(user)'],
+            },
+          }),
+        }),
+      }
+    );
+
+    expect(await screen.findByLabelText('Aggregate Selection')).toHaveTextContent(
+      'count_unique'
+    );
+    expect(screen.getByLabelText('Column Selection')).toHaveTextContent('user');
+
+    // Add 3 fields
+    await userEvent.click(screen.getByRole('button', {name: 'Add Field'}));
+    await userEvent.click(screen.getByRole('button', {name: 'Add Field'}));
+    await userEvent.click(screen.getByRole('button', {name: 'Add Field'}));
+
+    // count() is the default aggregate when adding a field
+    expect(screen.getAllByText('count')).toHaveLength(3);
+
+    // Change the last field
+    await userEvent.click(screen.getAllByText('count')[2]!);
+    await userEvent.click(screen.getByRole('option', {name: 'epm'}));
+
+    // The other fields should not be affected
+    expect(screen.getByText('count_unique')).toBeInTheDocument();
+    expect(screen.getAllByText('count')).toHaveLength(2);
+    expect(screen.getAllByText('epm')).toHaveLength(1);
+  });
+
   describe('spans', () => {
     beforeEach(() => {
       jest.mocked(useSpanTags).mockImplementation((type?: 'string' | 'number') => {
