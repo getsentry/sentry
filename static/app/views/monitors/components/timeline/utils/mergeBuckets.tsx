54be21255166e7b472ca0795edfe2a1631bc7fcf
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-01-06 15:13:21 -0800

    ref(crons): new `mergeBuckets` timeline function without env (#82569)
    new `mergeBuckets` function that merges new `monitorBucketWithStats`
    type
    
    since `checkinTimeline` doesn't need to know anything about environment,
    we want to replace `MonitorBucketEnvMapping` fields with `StatsBucket`

--- static/app/views/monitors/components/timeline/utils/mergeBuckets.tsx
+++ static/app/views/monitors/components/timeline/utils/mergeBuckets.tsx
@@ -1,10 +1,22 @@
-import type {JobTickData, MonitorBucket} from '../types';
+import {filterMonitorStatsBucketByEnv} from 'sentry/views/monitors/components/timeline/utils/filterMonitorStatsBucketByEnv';
 
-import {filterMonitorStatsBucketByEnv} from './filterMonitorStatsBucketByEnv';
-import {getAggregateStatus} from './getAggregateStatus';
-import {getAggregateStatusFromMultipleBuckets} from './getAggregateStatusFromMultipleBuckets';
-import {isEnvMappingEmpty} from './isEnvMappingEmpty';
-import {mergeEnvMappings} from './mergeEnvMappings';
+import type {
+  JobTickData,
+  JobTickDataWithStats,
+  MonitorBucket,
+  MonitorBucketWithStats,
+} from '../types';
+
+import {
+  getAggregateStatus,
+  getAggregateStatusFromStatsBucket,
+} from './getAggregateStatus';
+import {
+  getAggregateStatusFromMultipleBuckets,
+  getAggregateStatusFromMultipleStatsBuckets,
+} from './getAggregateStatusFromMultipleBuckets';
+import {isEnvMappingEmpty, isStatsBucketEmpty} from './isEnvMappingEmpty';
+import {mergeEnvMappings, mergeStats} from './mergeEnvMappings';
 
 function generateJobTickFromBucket(
   bucket: MonitorBucket,
@@ -16,6 +28,22 @@ function generateJobTickFromBucket(
     startTs: timestamp,
     width: 1,
     envMapping,
+    roundedLeft: false,
+    roundedRight: false,
+    ...options,
+  };
+}
+
+function generateJobTickFromBucketWithStats(
+  bucket: MonitorBucketWithStats,
+  options?: Partial<JobTickDataWithStats>
+) {
+  const [timestamp, stats] = bucket;
+  return {
+    endTs: timestamp,
+    startTs: timestamp,
+    width: 1,
+    stats,
     roundedLeft: false,
     roundedRight: false,
     ...options,
@@ -78,6 +106,70 @@ export function mergeBuckets(data: MonitorBucket[], environment: string) {
       return currentJobTick;
     },
     null as JobTickData | null
+  );
+
+  return jobTicks;
+}
+
+export function mergeBucketsWithStats(
+  data: MonitorBucketWithStats[]
+): JobTickDataWithStats[] {
+  const minTickWidth = 4;
+  const jobTicks: JobTickDataWithStats[] = [];
+
+  data.reduce(
+    (currentJobTick: JobTickDataWithStats | null, [timestamp, stats], i) => {
+      const statsEmpty = isStatsBucketEmpty(stats);
+
+      // If no current job tick, we start the first one
+      if (!currentJobTick) {
+        return statsEmpty
+          ? currentJobTick
+          : generateJobTickFromBucketWithStats([timestamp, stats], {roundedLeft: true});
+      }
+
+      const bucketStatus = getAggregateStatusFromStatsBucket(stats);
+      const currJobTickStatus = getAggregateStatusFromStatsBucket(currentJobTick.stats);
+
+      // If the current stats are empty and our job tick has reached the min width, finalize the tick
+      if (statsEmpty && currentJobTick.width >= minTickWidth) {
+        currentJobTick.roundedRight = true;
+        jobTicks.push(currentJobTick);
+        return null;
+      }
+
+      // Calculate the aggregate status for the next minTickWidth buckets
+      const nextTickAggregateStatus = getAggregateStatusFromMultipleStatsBuckets(
+        data.slice(i, i + minTickWidth).map(([_, sliceStats]) => sliceStats)
+      );
+
+      // If the status changes or we reach the min width, push the current tick and start a new one
+      if (
+        bucketStatus !== currJobTickStatus &&
+        nextTickAggregateStatus !== currJobTickStatus &&
+        currentJobTick.width >= minTickWidth
+      ) {
+        jobTicks.push(currentJobTick);
+        return generateJobTickFromBucketWithStats([timestamp, stats]);
+      }
+
+      // Otherwise, continue merging data into the current job tick
+      currentJobTick = {
+        ...currentJobTick,
+        endTs: timestamp,
+        stats: mergeStats(currentJobTick.stats, stats),
+        width: currentJobTick.width + 1,
+      };
+
+      // Ensure we render the last tick if it's the final bucket
+      if (i === data.length - 1) {
+        currentJobTick.roundedRight = true;
+        jobTicks.push(currentJobTick);
+      }
+
+      return currentJobTick;
+    },
+    null as JobTickDataWithStats | null
   );
 
   return jobTicks;
