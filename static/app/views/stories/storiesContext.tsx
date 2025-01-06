69dc0a032ebb298a15e5918e4c8ed2537d8013d9
Jonas <jonas.badalic@sentry.io>
2025-01-06 12:54:45 -0500

    ref(storybook) use react query for module loading (#82931)
    Use react query for story loading

--- static/app/views/stories/storiesContext.tsx
+++ static/app/views/stories/storiesContext.tsx
@@ -1,5 +1,6 @@
 export default function storiesContext() {
   const context = require.context('sentry', true, /\.stories.tsx$/, 'lazy');
+
   return {
     files: () => context.keys().map(file => file.replace(/^\.\//, 'app/')),
     importStory: (filename: string) => context(filename.replace(/^app\//, './')),
