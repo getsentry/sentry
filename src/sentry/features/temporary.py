bfc59a6704bccd5225b9805a8872c65c69e3cd0b
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-01-06 11:15:35 -0800

    fix(replay): flag option needs api expose

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -415,7 +415,7 @@ def register_temporary_features(manager: FeatureManager):
     # Enable replay web vital breadcrumbs
     manager.add("organizations:session-replay-web-vitals", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, default=False, api_expose=True)
     # Enable GA banner for mobile replay beta orgs about the grace period that will last 2 months. Flag can be removed after March 7th 2024.
-    manager.add("organizations:mobile-replay-beta-orgs", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, default=False, api_expose=False)
+    manager.add("organizations:mobile-replay-beta-orgs", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, default=False, api_expose=True)
     # Enable Dev Toolbar frontend features (ex project settings page)
     manager.add("organizations:dev-toolbar-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, default=False, api_expose=True)
     # Lets organizations manage grouping configs
