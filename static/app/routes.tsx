7c678ff11fe4803fa0b46ae381f4e5ee722646d5
Malachi Willey <malwilley@gmail.com>
2025-01-06 09:19:59 -0800

    ref(issue-list): Create copy of overview.tsx as a functional component (#82784)
    Adds a copy of overview.tsx (and tests) that is a functional component.
    I mostly copied it 1 for 1, so the resulting component is not much
    better than the original. We need a starting place first to start making
    more improvements.
    
    This new component is only rendered if the
    `issue-stream-functional-refactor` flag is enabled. We will test this
    internally before rolling out more widely and deleting the original to
    reduce risk.
    
    The most significant changes were around data fetching and caching,
    since converting things to useEffects resulted in some bugs and test
    failures that the original lifecycle methods managed to avoid somehow.
    For example, we are now using the `requestParams` object to determine if
    we should refetch instead of checking against previous props for
    everything individually.
    
    I have also removed `issue-stream-performance` checks in this version of
    the component since that complicated things significantly.

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -25,7 +25,7 @@ import {ModuleName} from 'sentry/views/insights/types';
 import {GroupEventDetailsLoading} from 'sentry/views/issueDetails/groupEventDetails/groupEventDetailsLoading';
 import {Tab, TabPaths} from 'sentry/views/issueDetails/types';
 import IssueListContainer from 'sentry/views/issueList';
-import IssueListOverview from 'sentry/views/issueList/overview';
+import {OverviewWrapper} from 'sentry/views/issueList/overviewWrapper';
 import OrganizationContainer from 'sentry/views/organizationContainer';
 import OrganizationLayout from 'sentry/views/organizationLayout';
 import OrganizationRoot from 'sentry/views/organizationRoot';
@@ -1903,8 +1903,8 @@ function buildRoutes() {
 
   const issueListRoutes = (
     <Route path="/issues" component={errorHandler(IssueListContainer)} withOrgPath>
-      <IndexRoute component={errorHandler(IssueListOverview)} />
-      <Route path="searches/:searchId/" component={errorHandler(IssueListOverview)} />
+      <IndexRoute component={errorHandler(OverviewWrapper)} />
+      <Route path="searches/:searchId/" component={errorHandler(OverviewWrapper)} />
     </Route>
   );
 
