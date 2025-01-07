cfbf518a3caa6e38af759ca74bdf51b29e167b4e
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2025-01-07 08:14:37 -0500

    Minor adjustment

--- src/sentry/issues/ignored.py
+++ src/sentry/issues/ignored.py
@@ -6,6 +6,7 @@ from collections.abc import Sequence
 from datetime import datetime, timedelta
 from typing import Any, TypedDict
 
+from django.contrib.auth.models import AnonymousUser
 from django.utils import timezone
 
 from sentry.db.postgres.transactions import in_test_hide_transaction_boundary
@@ -89,7 +90,7 @@ def handle_ignored(
     group_list: Sequence[Group],
     status_details: dict[str, Any],
     acting_user: User | RpcUser,
-    user: User | RpcUser,
+    user: User | RpcUser | AnonymousUser,
 ) -> IgnoredStatusDetails:
     """
     Handle issues that are ignored and create a snooze for them.
