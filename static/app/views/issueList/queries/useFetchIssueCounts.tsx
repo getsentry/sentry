128840c8614d443be7443a829b99639148b32cd7
MichaelSun48 <michael.sun@sentry.io>
2025-01-06 17:24:03 -0800

    Add hook to fetch issue counts

---
+++ static/app/views/issueList/queries/useFetchIssueCounts.tsx
@@ -0,0 +1,38 @@
+import type {PageFilters} from 'sentry/types/core';
+import type {ApiQueryKey, UseApiQueryOptions} from 'sentry/utils/queryClient';
+import {useApiQuery} from 'sentry/utils/queryClient';
+import type {QueryCounts} from 'sentry/views/issueList/utils';
+
+// Copied from CountEndpointParams in overview.tsx
+interface FetchIssueCountsParameters extends Partial<PageFilters['datetime']> {
+  environment: string[];
+  orgSlug: string;
+  project: number[];
+  query: string[];
+  groupStatsPeriod?: string | null;
+  sort?: string;
+  statsPeriod?: string | null;
+  useGroupSnubaDataset?: boolean;
+}
+
+export const makeFetchIssueCounts = ({
+  orgSlug,
+  ...requestParams
+}: FetchIssueCountsParameters): ApiQueryKey => [
+  `/organizations/${orgSlug}/issues-count/`,
+  {
+    query: {
+      ...requestParams,
+    },
+  },
+];
+
+export const useFetchIssueCounts = (
+  params: FetchIssueCountsParameters,
+  options: Partial<UseApiQueryOptions<QueryCounts>> = {}
+) => {
+  return useApiQuery<QueryCounts>(makeFetchIssueCounts(params), {
+    staleTime: 0,
+    ...options,
+  });
+};
