933023d59da8d161301208071fa50a8f62cf6871
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2025-01-08 13:31:33 -0800

    fix(alerts): frontend for sentry app alerts permissions (#83110)
    Set the default to be no access, like the other permissions.

--- static/app/utils/consolidatedScopes.tsx
+++ static/app/utils/consolidatedScopes.tsx
@@ -18,6 +18,7 @@ const HUMAN_RESOURCE_NAMES = {
   event: 'Event',
   org: 'Organization',
   member: 'Member',
+  alerts: 'Alerts',
 };
 
 const DEFAULT_RESOURCE_PERMISSIONS: Permissions = {
@@ -27,6 +28,7 @@ const DEFAULT_RESOURCE_PERMISSIONS: Permissions = {
   Event: 'no-access',
   Organization: 'no-access',
   Member: 'no-access',
+  Alerts: 'no-access',
 };
 
 const PROJECT_RELEASES = 'project:releases';
