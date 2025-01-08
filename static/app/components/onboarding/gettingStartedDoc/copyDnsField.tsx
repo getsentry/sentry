1c5536500ce35a69e9c30c48c409050004f9a412
Arthur Knaus <arthur.knaus@sentry.io>
2025-01-08 12:55:11 +0100

    feat(onboarding): Generic copy DNS

---
+++ static/app/components/onboarding/gettingStartedDoc/copyDnsField.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+import type {DocsParams} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import TextCopyInput from 'sentry/components/textCopyInput';
+import {tct} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {trackAnalytics} from 'sentry/utils/analytics';
+
+export function CopyDnsField({params}: {params: DocsParams<any>}) {
+  return (
+    <Wrapper>
+      <p>
+        {tct(
+          "If you already have the configuration for Sentry in your application, and just need this project's ([projectSlug]) DSN, you can find it below:",
+          {
+            projectSlug: <code>{params.projectSlug}</code>,
+          }
+        )}
+      </p>
+      <TextCopyInput
+        onCopy={() =>
+          trackAnalytics('onboarding.nextjs-dsn-copied', {
+            organization: params.organization,
+          })
+        }
+      >
+        {params.dsn.public}
+      </TextCopyInput>
+    </Wrapper>
+  );
+}
+
+const Wrapper = styled('div')`
+  padding-top: ${space(2)};
+  border-top: 1px solid ${p => p.theme.border};
+  display: flex;
+  flex-direction: column;
+  gap: ${space(1)};
+`;
