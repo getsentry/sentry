7237ecb7aff37e5385f5c2fdd525b5f67b439280
Christinarlong <christina.long@sentry.io>
2025-01-07 16:38:29 -0800

    raise errors in external issue actions endpt

--- src/sentry/sentry_apps/api/endpoints/installation_external_issue_actions.py
+++ src/sentry/sentry_apps/api/endpoints/installation_external_issue_actions.py
@@ -2,13 +2,11 @@ from django.utils.functional import empty
 from jsonschema import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
-from sentry_sdk import capture_exception
 
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.serializers import serialize
-from sentry.coreapi import APIError, APIUnauthorized
 from sentry.models.group import Group
 from sentry.models.project import Project
 from sentry.sentry_apps.api.bases.sentryapps import SentryAppInstallationBaseEndpoint
@@ -16,6 +14,7 @@ from sentry.sentry_apps.api.serializers.platform_external_issue import (
     PlatformExternalIssueSerializer,
 )
 from sentry.sentry_apps.external_issues.issue_link_creator import IssueLinkCreator
+from sentry.sentry_apps.utils.errors import SentryAppError
 from sentry.users.models.user import User
 from sentry.users.services.user.serial import serialize_rpc_user
 
@@ -45,7 +44,10 @@ class SentryAppInstallationExternalIssueActionsEndpoint(SentryAppInstallationBas
         data = request.data.copy()
 
         if not {"groupId", "action", "uri"}.issubset(data.keys()):
-            return Response(status=400)
+            raise SentryAppError(
+                ValidationError('"groupId", "action", and "uri" must be present in request'),
+                status_code=404,
+            )
 
         group_id = data.get("groupId")
         del data["groupId"]
@@ -56,34 +58,25 @@ class SentryAppInstallationExternalIssueActionsEndpoint(SentryAppInstallationBas
                 project_id__in=Project.objects.filter(organization_id=installation.organization_id),
             )
         except Group.DoesNotExist:
-            return Response(status=404)
+            raise SentryAppError(
+                ValidationError("Could not find issue from given groupId"), status_code=404
+            )
 
         action = data.pop("action")
         uri = data.pop("uri")
 
-        try:
-            user = _extract_lazy_object(request.user)
-            if isinstance(user, User):
-                user = serialize_rpc_user(user)
+        user = _extract_lazy_object(request.user)
+        if isinstance(user, User):
+            user = serialize_rpc_user(user)
 
-            external_issue = IssueLinkCreator(
-                install=installation,
-                group=group,
-                action=action,
-                fields=data,
-                uri=uri,
-                user=user,
-            ).run()
-        except (APIError, ValidationError, APIUnauthorized) as e:
-            return Response({"error": str(e)}, status=400)
-        except Exception as e:
-            error_id = capture_exception(e)
-            return Response(
-                {
-                    "error": f"Something went wrong while trying to link issue. Sentry error ID: {error_id}"
-                },
-                status=500,
-            )
+        external_issue = IssueLinkCreator(
+            install=installation,
+            group=group,
+            action=action,
+            fields=data,
+            uri=uri,
+            user=user,
+        ).run()
 
         return Response(
             serialize(objects=external_issue, serializer=PlatformExternalIssueSerializer())
