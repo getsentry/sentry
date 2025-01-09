c79bcbce5b604c9ee32809877667be4d51bf3398
Nate Moore <natemoo-re@users.noreply.github.com>
2025-01-09 15:15:56 -0600

    feat(aci): add automation + monitor views (#83130)
    Adds directories + files for new ACI views (both Automations and
    Monitors). Uses components added in #82973.

---
+++ static/app/views/detectors/detail.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable no-alert */
+import {Fragment} from 'react';
+
+import {Button, LinkButton} from 'sentry/components/button';
+import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
+import {ActionsProvider} from 'sentry/components/workflowEngine/layout/actions';
+import {BreadcrumbsProvider} from 'sentry/components/workflowEngine/layout/breadcrumbs';
+import DetailLayout from 'sentry/components/workflowEngine/layout/detail';
+import {IconEdit} from 'sentry/icons';
+import {t} from 'sentry/locale';
+
+export default function DetectorDetail() {
+  return (
+    <SentryDocumentTitle title={t('Edit Monitor')} noSuffix>
+      <BreadcrumbsProvider crumb={{label: t('Monitors'), to: '/monitors'}}>
+        <ActionsProvider actions={<Actions />}>
+          <DetailLayout>
+            <DetailLayout.Main>main</DetailLayout.Main>
+            <DetailLayout.Sidebar>sidebar</DetailLayout.Sidebar>
+          </DetailLayout>
+        </ActionsProvider>
+      </BreadcrumbsProvider>
+    </SentryDocumentTitle>
+  );
+}
+
+function Actions() {
+  const disable = () => {
+    window.alert('disable');
+  };
+  return (
+    <Fragment>
+      <Button onClick={disable}>{t('Disable')}</Button>
+      <LinkButton to="/monitors/edit" priority="primary" icon={<IconEdit />}>
+        {t('Edit')}
+      </LinkButton>
+    </Fragment>
+  );
+}
