56614eb00c2c20bada2d8f7780c4c7d595b02be1
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2025-01-09 10:56:44 -0800

    feat(aci milestone 3): ACI dual delete helper (#82280)
    Add helper method to delete the corresponding rows in the ACI tables
    after deleting an `AlertRule`.

--- src/sentry/incidents/logic.py
+++ src/sentry/incidents/logic.py
@@ -1024,6 +1024,8 @@ def delete_alert_rule(
     Marks an alert rule as deleted and fires off a task to actually delete it.
     :param alert_rule:
     """
+    from sentry.workflow_engine.migration_helpers.alert_rule import dual_delete_migrated_alert_rule
+
     if alert_rule.status == AlertRuleStatus.SNAPSHOT.value:
         raise AlreadyDeletedError()
 
@@ -1037,11 +1039,7 @@ def delete_alert_rule(
                 data=alert_rule.get_audit_log_data(),
                 event=audit_log.get_event_id("ALERT_RULE_REMOVE"),
             )
-
         subscriptions = _unpack_snuba_query(alert_rule).subscriptions.all()
-        bulk_delete_snuba_subscriptions(subscriptions)
-
-        schedule_update_project_config(alert_rule, [sub.project for sub in subscriptions])
 
         incidents = Incident.objects.filter(alert_rule=alert_rule)
         if incidents.exists():
@@ -1064,7 +1062,13 @@ def delete_alert_rule(
             )
         else:
             RegionScheduledDeletion.schedule(instance=alert_rule, days=0, actor=user)
+        # NOTE: we want to run the dual delete regardless of whether the user is flagged into dual writes:
+        # the user could be removed from the dual write flag for whatever reason, and we need to make sure
+        # that the extra table data is deleted. If the rows don't exist, we'll exit early.
+        dual_delete_migrated_alert_rule(alert_rule=alert_rule, user=user)
 
+        bulk_delete_snuba_subscriptions(subscriptions)
+        schedule_update_project_config(alert_rule, [sub.project for sub in subscriptions])
         alert_rule.update(status=AlertRuleStatus.SNAPSHOT.value)
 
     if alert_rule.id:
