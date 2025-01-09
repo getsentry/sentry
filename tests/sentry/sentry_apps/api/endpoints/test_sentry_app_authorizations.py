500302c2562159a5548db7721df404e08d69de38
Christinarlong <christina.long@sentry.io>
2025-01-09 10:34:14 -0800

    add status code enums to endpoint test for readbility

--- tests/sentry/sentry_apps/api/endpoints/test_sentry_app_authorizations.py
+++ tests/sentry/sentry_apps/api/endpoints/test_sentry_app_authorizations.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 
 from django.urls import reverse
 from django.utils import timezone
+from rest_framework import status
 
 from sentry.models.apiapplication import ApiApplication
 from sentry.models.apitoken import ApiToken
@@ -72,15 +73,15 @@ class TestSentryAppAuthorizations(APITestCase):
     def test_exchange_for_token_missing_data(self):
         response = self.get_error_response(code=None)
 
-        assert response.status_code == 400
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
 
         # This is rejected by the base `SentryAppAuthorizationBaseEndpoint`
         # class's authentication, so expect an unauthorized error.
         response = self.get_error_response(client_id=None)
-        assert response.status_code == 401
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_incorrect_grant_type(self):
-        self.get_error_response(grant_type="notit", status_code=403)
+        self.get_error_response(grant_type="notit", status_code=status.HTTP_403_FORBIDDEN)
 
     def test_invalid_installation(self):
         self.install = self.create_sentry_app_installation(
@@ -91,20 +92,22 @@ class TestSentryAppAuthorizations(APITestCase):
         )
 
         # URL with this new Install's uuid in it
-        self.get_error_response(self.install.uuid, status_code=403)
+        self.get_error_response(self.install.uuid, status_code=status.HTTP_403_FORBIDDEN)
 
     def test_non_sentry_app_user(self):
         app = ApiApplication.objects.create(owner=self.create_user())
         self.get_error_response(
-            client_id=app.client_id, client_secret=app.client_secret, status_code=401
+            client_id=app.client_id,
+            client_secret=app.client_secret,
+            status_code=status.HTTP_401_UNAUTHORIZED,
         )
 
     def test_invalid_grant(self):
-        self.get_error_response(code="123", status_code=403)
+        self.get_error_response(code="123", status_code=status.HTTP_403_FORBIDDEN)
 
     def test_expired_grant(self):
         self.install.api_grant.update(expires_at=timezone.now() - timedelta(minutes=2))
-        response = self.get_error_response(status_code=403)
+        response = self.get_error_response(status_code=status.HTTP_403_FORBIDDEN)
         assert response.data["detail"] == "Grant has already expired"
 
     def test_request_with_exchanged_access_token(self):
@@ -116,7 +119,7 @@ class TestSentryAppAuthorizations(APITestCase):
         with assume_test_silo_mode(SiloMode.REGION):
             response = self.client.get(url, HTTP_AUTHORIZATION=f"Bearer {token}")
 
-        assert response.status_code == 200
+        assert response.status_code == status.HTTP_200_OK
         assert response.data["id"] == str(self.organization.id)
 
     def test_state(self):
@@ -158,7 +161,7 @@ class TestSentryAppAuthorizations(APITestCase):
             code=None, refresh_token=None, grant_type="refresh_token"
         )
 
-        assert response.status_code == 400
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data == {"detail": "{'refresh_token': ['This field may not be null.']}"}
 
         # This is rejected by the base `SentryAppAuthorizationBaseEndpoint`
@@ -166,7 +169,7 @@ class TestSentryAppAuthorizations(APITestCase):
         response = self.get_error_response(
             code=None, refresh_token=refresh_token, grant_type="refresh_token", client_id=None
         )
-        assert response.status_code == 401
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_authorization_serializer(self):
         response = self.get_error_response(
@@ -174,5 +177,5 @@ class TestSentryAppAuthorizations(APITestCase):
             code=None,
             grant_type="authorization_code",
         )
-        assert response.status_code == 400
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
         assert response.data == {"detail": "{'code': ['This field may not be null.']}"}
