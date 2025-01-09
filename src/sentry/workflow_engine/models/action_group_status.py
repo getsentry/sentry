93062cdd12f63ab4932e89507a34cf6c47dbba4f
Cathy Teng <cathy.teng@sentry.io>
2025-01-08 17:38:49 -0800

    remove unused status field

--- src/sentry/workflow_engine/models/action_group_status.py
+++ src/sentry/workflow_engine/models/action_group_status.py
@@ -7,17 +7,13 @@ from sentry.db.models import DefaultFieldsModel, FlexibleForeignKey, region_silo
 @region_silo_model
 class ActionGroupStatus(DefaultFieldsModel):
     """
-    Stores when a workflow last fired for a Group.
+    Stores when an action last fired for a Group.
     """
-
-    ACTIVE = 0
-    INACTIVE = 1
 
     __relocation_scope__ = RelocationScope.Excluded
 
     action = FlexibleForeignKey("workflow_engine.Action", on_delete=models.CASCADE)
     group = FlexibleForeignKey("sentry.Group")
-    status = models.PositiveSmallIntegerField(default=ACTIVE)
 
     class Meta:
         constraints = [
