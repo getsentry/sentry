142f3414aef8f268391d8c4cb8a2ed470065a1c9
Scott Cooper <scttcper@gmail.com>
2025-01-09 11:28:53 -0800

    comment

--- static/app/utils/provideAriaRouter.tsx
+++ static/app/utils/provideAriaRouter.tsx
@@ -3,7 +3,7 @@ import {RouterProvider as AriaRouterProvider} from '@react-aria/utils';
 
 import {useNavigate} from 'sentry/utils/useNavigate';
 /**
- * React aira has its own context for internal hooks/components. Links, Menu, Tabs, etc.
+ * React aira has its own context for useTabs, useMenuItem, useLink, etc.
  * We need to provide the router instance so it knows how to navigate.
  * @link https://react-spectrum.adobe.com/react-aria/routing.html#react-router
  */
