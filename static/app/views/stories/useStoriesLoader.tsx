d46df210dc39e57dbe76f5fe934e29c3c8c387b4
JonasBa <jonas@badalic.com>
2025-01-10 15:22:41 -0500

    ref(storybook): add landing page

--- static/app/views/stories/useStoriesLoader.tsx
+++ static/app/views/stories/useStoriesLoader.tsx
@@ -1,13 +1,9 @@
 import type React from 'react';
 import {useMemo} from 'react';
 
-import {useQuery} from 'sentry/utils/queryClient';
+import {useQuery, type UseQueryResult} from 'sentry/utils/queryClient';
 
 const context = require.context('sentry', true, /\.stories.tsx$/, 'lazy');
-
-interface UseStoriesLoaderOptions {
-  filename: string;
-}
 
 export interface StoryDescriptor {
   exports: Record<string, React.ComponentType | any>;
@@ -27,9 +23,26 @@ async function importStory(filename: string): Promise<StoryDescriptor> {
   };
 }
 
-export default function useStoriesLoader({filename}: UseStoriesLoaderOptions) {
+type StoriesResult<T> = T extends string ? StoryDescriptor : StoryDescriptor[];
+
+interface UseStoriesLoaderOptions<T extends string | string[]> {
+  filename: T;
+}
+
+export default function useStoriesLoader<T extends string | string[]>(
+  options: UseStoriesLoaderOptions<T>
+): UseQueryResult<StoriesResult<T>, Error> {
   return useQuery({
-    queryKey: [filename],
-    queryFn: () => importStory(filename),
+    queryKey: [options.filename],
+    queryFn: (): Promise<StoriesResult<T>> => {
+      if (Array.isArray(options.filename)) {
+        return Promise.all(options.filename.map(importStory)) as Promise<
+          StoriesResult<T>
+        >;
+      }
+
+      return importStory(options.filename) as Promise<StoriesResult<T>>;
+    },
+    enabled: !!options.filename,
   });
 }
