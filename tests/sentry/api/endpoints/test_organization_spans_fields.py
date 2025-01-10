325a144d4a96d06c9c53e2f497d1c79ad5e1047d
William Mak <william@wmak.io>
2025-01-10 11:32:32 -0500

    fix(spans): Handle invalid spans fields query (#83111)
    - Add a handle_query_errors around when we execute the tag_values query
    so we return an error message instead of erroring
    - fixes SENTRY-3G6J

--- tests/sentry/api/endpoints/test_organization_spans_fields.py
+++ tests/sentry/api/endpoints/test_organization_spans_fields.py
@@ -3,6 +3,7 @@ from uuid import uuid4
 
 from django.urls import reverse
 
+from sentry.exceptions import InvalidSearchQuery
 from sentry.testutils.cases import APITestCase, BaseSpansTestCase
 from sentry.testutils.helpers.datetime import before_now
 
@@ -839,6 +840,34 @@ class OrganizationSpansTagKeyValuesEndpointTest(BaseSpansTestCase, APITestCase):
             response = self.do_request(key)
             assert response.status_code == 200, response.data
             assert response.data == []
+
+    @mock.patch(
+        "sentry.api.endpoints.organization_spans_fields.EAPSpanFieldValuesAutocompletionExecutor.execute",
+        side_effect=InvalidSearchQuery,
+    )
+    @mock.patch(
+        "sentry.api.endpoints.organization_spans_fields.SpanFieldValuesAutocompletionExecutor.execute",
+        side_effect=InvalidSearchQuery,
+    )
+    def test_invalid_query(self, mock_executor_1, mock_executor_2):
+        timestamp = before_now(days=0, minutes=10).replace(microsecond=0)
+        self.store_segment(
+            self.project.id,
+            uuid4().hex,
+            uuid4().hex,
+            span_id=uuid4().hex[:16],
+            organization_id=self.organization.id,
+            parent_span_id=None,
+            timestamp=timestamp,
+            transaction="foo",
+            duration=100,
+            exclusive_time=100,
+            tags={"tag": "foo"},
+            is_eap=self.is_eap,
+        )
+
+        response = self.do_request("tag")
+        assert response.status_code == 400, response.data
 
 
 class OrganizationEAPSpansTagKeyValuesEndpointTest(OrganizationSpansTagKeyValuesEndpointTest):
