5254c4069d3d036d8c3c7739c930cf5cf08d819c
Raj Joshi <raj.joshi@sentry.io>
2025-01-10 13:28:48 -0800

    :wrench: chore: create temp flag for data secrecy

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -123,6 +123,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:dynamic-sampling-custom", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enables data secrecy mode
     manager.add("organizations:enterprise-data-secrecy", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
+    # This is a temporary flag that will help us migrate to the new data secrecy logic
+    manager.add("organizations:enterprise-data-secrecy-legacy", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable issue platform deletion
     manager.add("organizations:issue-platform-deletion", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable archive/escalating issue workflow features in v2
