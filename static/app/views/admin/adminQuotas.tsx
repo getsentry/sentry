6f5b8ebe268cb470c59cfd9778dc2a74710637f8
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-01-10 13:25:32 -0800

    ref(tsc): convert adminQuotas to FC

--- static/app/views/admin/adminQuotas.tsx
+++ static/app/views/admin/adminQuotas.tsx
@@ -1,89 +1,88 @@
 import {Fragment} from 'react';
 
-import DeprecatedAsyncComponent from 'sentry/components/deprecatedAsyncComponent';
 import TextField from 'sentry/components/forms/fields/textField';
 import InternalStatChart from 'sentry/components/internalStatChart';
+import LoadingError from 'sentry/components/loadingError';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
 import Panel from 'sentry/components/panels/panel';
 import PanelBody from 'sentry/components/panels/panelBody';
 import PanelHeader from 'sentry/components/panels/panelHeader';
 import {t} from 'sentry/locale';
+import {useApiQuery} from 'sentry/utils/queryClient';
 
 type Config = {
   backend: string;
   options: Record<string, string>;
 };
 
-type State = DeprecatedAsyncComponent['state'] & {
-  config: Config;
-  resolution: string;
-  since: number;
-};
+export default function AdminQuotas() {
+  const {
+    data: config,
+    isPending,
+    isError,
+  } = useApiQuery<Config>(['/internal/quotas/'], {
+    staleTime: 0,
+  });
+  const since = new Date().getTime() / 1000 - 3600 * 24 * 7;
+  const resolution = '1h';
 
-export default class AdminQuotas extends DeprecatedAsyncComponent<{}, State> {
-  getDefaultState() {
-    return {
-      ...super.getDefaultState(),
-      since: new Date().getTime() / 1000 - 3600 * 24 * 7,
-      resolution: '1h',
-    };
+  if (isPending) {
+    return <LoadingIndicator />;
   }
 
-  getEndpoints(): ReturnType<DeprecatedAsyncComponent['getEndpoints']> {
-    return [['config', '/internal/quotas/']];
+  if (isError) {
+    return <LoadingError />;
   }
 
-  renderBody() {
-    const {config} = this.state;
-    return (
-      <Fragment>
-        <h3>Quotas</h3>
+  return (
+    <Fragment>
+      <h3>Quotas</h3>
 
-        <Panel>
-          <PanelHeader>{t('Config')}</PanelHeader>
-          <PanelBody withPadding>
-            <TextField
-              name="backend"
-              value={config.backend}
-              label="Backend"
-              disabled
-              inline={false}
-              stacked
-            />
-            <TextField
-              name="rateLimit"
-              value={config.options['system.rate-limit']}
-              label="Rate Limit"
-              disabled
-              inline={false}
-              stacked
-            />
-          </PanelBody>
-        </Panel>
+      <Panel>
+        <PanelHeader>{t('Config')}</PanelHeader>
+        <PanelBody withPadding>
+          <TextField
+            name="backend"
+            value={config.backend}
+            label="Backend"
+            disabled
+            inline={false}
+            stacked
+          />
+          <TextField
+            name="rateLimit"
+            value={config.options['system.rate-limit']}
+            label="Rate Limit"
+            disabled
+            inline={false}
+            stacked
+          />
+        </PanelBody>
+      </Panel>
 
-        <Panel>
-          <PanelHeader>{t('Total Events')}</PanelHeader>
-          <PanelBody withPadding>
-            <InternalStatChart
-              since={this.state.since}
-              resolution={this.state.resolution}
-              stat="events.total"
-              label="Events"
-            />
-          </PanelBody>
-        </Panel>
+      <Panel>
+        <PanelHeader>{t('Total Events')}</PanelHeader>
+        <PanelBody withPadding>
+          <InternalStatChart
+            since={since}
+            resolution={resolution}
+            stat="events.total"
+            label="Events"
+          />
+        </PanelBody>
+      </Panel>
 
-        <Panel>
-          <PanelHeader>{t('Dropped Events')}</PanelHeader>
-          <PanelBody withPadding>
-            <InternalStatChart
-              since={this.state.since}
-              resolution={this.state.resolution}
-              stat="events.dropped"
-              label="Events"
-            />
-          </PanelBody>
-        </Panel>
-      </Fragment>
-    );
-  }
+      <Panel>
+        <PanelHeader>{t('Dropped Events')}</PanelHeader>
+        <PanelBody withPadding>
+          <InternalStatChart
+            since={since}
+            resolution={resolution}
+            stat="events.dropped"
+            label="Events"
+          />
+        </PanelBody>
+      </Panel>
+    </Fragment>
+  );
 }
