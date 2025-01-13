ea23c0bbba77f9a0c27a89dfa1cf3f0c53e6af10
Jonas <jonas.badalic@sentry.io>
2025-01-13 15:16:16 -0500

    ref(storybook) add useUser story (#83263)
    Render the serialized user JSON value
    
    ---------
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/utils/useOrganization.stories.tsx
+++ static/app/utils/useOrganization.stories.tsx
@@ -1,10 +1,32 @@
+import {Fragment} from 'react';
+
 import StructuredEventData from 'sentry/components/structuredEventData';
 import storyBook from 'sentry/stories/storyBook';
 import useOrganization from 'sentry/utils/useOrganization';
 
 export default storyBook('useOrganization', story => {
-  story('useOrganization - via StructuredEventData', () => {
+  story('Default', () => {
     const org = useOrganization();
-    return <StructuredEventData data={org} />;
+
+    // Sort features and access alphabetically for the story use case
+    org.features.sort((a, b) => a.localeCompare(b));
+    org.access.sort((a, b) => a.localeCompare(b));
+
+    return (
+      <Fragment>
+        <p>
+          <code>useOrganization</code> returns the currently selected organization.
+        </p>
+        In the context of the Sentry dashboard, only a single organization can be selected
+        at a time.
+        <p>
+          A common need to use <code>useOrganization</code> is to check for feature flags
+          via <code>organization.features.includes(feature)</code>. Note that our feature
+          names are currently not typed, so you need to make sure you pass in the correct
+          feature name.
+        </p>
+        <StructuredEventData data={org} />
+      </Fragment>
+    );
   });
 });
