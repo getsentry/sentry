94da3ea0c0d42714fe0ccf03e5a9ccb4aaf5ce2e
Tony Xiao <txiao@sentry.io>
2025-01-13 11:51:43 -0500

    ref(profiling): First pass to consolidate profile providers (#83274)
    This makes the 2 profile provider components more similar in preparation
    to merge them into 1 soon.

--- static/app/views/profiling/continuousProfileProvider.tsx
+++ static/app/views/profiling/continuousProfileProvider.tsx
@@ -1,6 +1,5 @@
 import {createContext, useContext, useLayoutEffect, useMemo, useState} from 'react';
 import * as Sentry from '@sentry/react';
-import * as qs from 'query-string';
 
 import type {Client} from 'sentry/api';
 import {ContinuousProfileHeader} from 'sentry/components/profiling/continuousProfileHeader';
@@ -9,7 +8,9 @@ import type {EventTransaction} from 'sentry/types/event';
 import type {Organization} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
 import {useSentryEvent} from 'sentry/utils/profiling/hooks/useSentryEvent';
+import {decodeScalar} from 'sentry/utils/queryString';
 import useApi from 'sentry/utils/useApi';
+import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
 import useProjects from 'sentry/utils/useProjects';
@@ -68,59 +69,93 @@ function isValidDate(date: string): boolean {
   return !isNaN(Date.parse(date));
 }
 
-function getContinuousChunkQueryParams(
-  query: string
-): ContinuousProfileQueryParams | null {
-  const queryString = new URLSearchParams(query);
-  const start = queryString.get('start');
-  const end = queryString.get('end');
-  const profiler_id = queryString.get('profilerId');
-
-  if (!start || !end || !profiler_id) {
-    return null;
-  }
-
-  if (!isValidDate(start) || !isValidDate(end)) {
-    return null;
-  }
-
-  return {
-    start,
-    end,
-    profiler_id,
-  };
-}
-
-interface ContinuousFlamegraphViewProps {
+interface FlamegraphViewProps {
   children: React.ReactNode;
 }
 
-function ContinuousProfileProvider(
-  props: ContinuousFlamegraphViewProps
-): React.ReactElement {
-  const api = useApi();
-  const params = useParams();
+function ProfilesAndTransactionProvider(props: FlamegraphViewProps): React.ReactElement {
   const organization = useOrganization();
-  const projects = useProjects();
+  const params = useParams();
+  const location = useLocation();
+
+  const projectSlug = params.projectId!;
+
+  const profileMeta = useMemo(() => {
+    const start = decodeScalar(location.query.start);
+    const end = decodeScalar(location.query.end);
+    const profilerId = decodeScalar(location.query.profilerId);
+
+    if (!start || !end || !profilerId) {
+      return null;
+    }
+
+    if (!isValidDate(start) || !isValidDate(end)) {
+      return null;
+    }
+
+    return {
+      start,
+      end,
+      profiler_id: profilerId,
+    };
+  }, [location.query.start, location.query.end, location.query.profilerId]);
+
+  const profileTransaction = useSentryEvent<EventTransaction>(
+    organization.slug,
+    projectSlug!,
+    decodeScalar(location.query.eventId) || null
+  );
+
+  return (
+    <ProfilesProvider
+      orgSlug={organization.slug}
+      profileMeta={profileMeta}
+      projectSlug={projectSlug}
+    >
+      <ContinuousProfileSegmentContext.Provider value={profileTransaction}>
+        <ContinuousProfileHeader
+          projectId={projectSlug}
+          transaction={
+            profileTransaction.type === 'resolved' ? profileTransaction.data : null
+          }
+        />
+        {props.children}
+      </ContinuousProfileSegmentContext.Provider>
+    </ProfilesProvider>
+  );
+}
+
+interface ProfilesProviderProps {
+  children: React.ReactNode;
+  orgSlug: Organization['slug'];
+  profileMeta: ContinuousProfileQueryParams | null;
+  projectSlug: Project['slug'];
+  onUpdateProfiles?: (profiles: RequestState<Profiling.ProfileInput>) => void;
+}
+
+export function ProfilesProvider({
+  children,
+  onUpdateProfiles,
+  orgSlug,
+  profileMeta,
+  projectSlug,
+}: ProfilesProviderProps) {
+  const api = useApi();
+  const {projects} = useProjects();
 
   const [profiles, setProfiles] = useState<RequestState<Profiling.ProfileInput>>({
     type: 'initial',
   });
 
   useLayoutEffect(() => {
-    if (!params.projectId) {
-      return undefined;
-    }
-
-    const chunkParams = getContinuousChunkQueryParams(window.location.search);
-    const project = projects.projects.find(p => p.slug === params.projectId);
-
-    if (!chunkParams) {
+    if (!profileMeta) {
       Sentry.captureMessage(
-        'Failed to fetch continuous profile - invalid query parameters.'
+        'Failed to fetch continuous profile - invalid chunk parameters.'
       );
       return undefined;
     }
+
+    const project = projects.find(p => p.slug === projectSlug);
     if (!project) {
       Sentry.captureMessage('Failed to fetch continuous profile - project not found.');
       return undefined;
@@ -128,41 +163,25 @@ function ContinuousProfileProvider(
 
     setProfiles({type: 'loading'});
 
-    fetchContinuousProfileFlamegraph(api, chunkParams, project.id, organization.slug)
+    fetchContinuousProfileFlamegraph(api, profileMeta, project.id, orgSlug)
       .then(p => {
         setProfiles({type: 'resolved', data: p});
+        onUpdateProfiles?.({type: 'resolved', data: p});
       })
       .catch(err => {
         setProfiles({type: 'errored', error: 'Failed to fetch profiles'});
+        onUpdateProfiles?.({type: 'errored', error: 'Failed to fetch profiles'});
         Sentry.captureException(err);
       });
 
     return () => api.clear();
-  }, [api, organization.slug, projects.projects, params.projectId]);
-
-  const eventPayload = useMemo(() => {
-    const query = qs.parse(window.location.search);
-
-    return {
-      project: projects.projects.find(p => p.slug === params.projectId),
-      eventId: query.eventId as string,
-    };
-  }, [projects, params.projectId]);
-
-  const profileTransaction = useSentryEvent<EventTransaction>(
-    organization.slug,
-    eventPayload.project?.id ?? '',
-    eventPayload.eventId
-  );
+  }, [api, onUpdateProfiles, profileMeta, orgSlug, projectSlug, projects]);
 
   return (
     <ContinuousProfileContext.Provider value={profiles}>
-      <ContinuousProfileSegmentContext.Provider value={profileTransaction}>
-        <ContinuousProfileHeader />
-        {props.children}
-      </ContinuousProfileSegmentContext.Provider>
+      {children}
     </ContinuousProfileContext.Provider>
   );
 }
 
-export default ContinuousProfileProvider;
+export default ProfilesAndTransactionProvider;
