41ba4728270286a91ae7cc647490c96f651922c5
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-01-13 09:38:09 -0800

    fix: Fix actionCreator/organization promises and enable `no-async-promise-executor` eslint rule (#83270)
    https://eslint.org/docs/latest/rules/no-async-promise-executor#rule-details
    
    This rule uncovered a case where an error inside of `loadOrganization()`
    could result in a stalled sudo auth page, and where
    `loadTeamsAndProjects()` would swallow errors.
    
    **loadOrganization**
    Before: if a `401` or `403` response was returned and
    `getErrorMessage()` couldn't read `.detail` or `.detail.message` then
    we'd arrive at the lonely `return;` statement on line 178 without
    resolving or rejecting the parent promise. This would stall the promise
    forever.
    
    Now: the promise always resolves or rejects, one way or another.
    
    This was only a problem inside of:
    -
    https://github.com/getsentry/sentry/blob/0944815865e35621323e0b40793648e29094b008/static/app/views/organizationContext.tsx#L182
    Called by:
    -
    https://github.com/getsentry/sentry/blob/0944815865e35621323e0b40793648e29094b008/static/app/components/modals/sudoModal.tsx#L87
    
    All other callsites do no await the promise. 
    
    
    **loadTeamsAndProjects**
    
    The promise executor inside `loadTeamsAndProjects` had no try/catch
    blocks before. So any errors thrown from inside any of the function
    calls (including fetchProjectsAndTeams) would not be caught or re-thown
    by the `new Promise` class instance. They're just gone, and the promise
    returned by loadTeamsAndProjects() would never reject.
    This caused the same issue as `loadOrganization()` above and impacts the
    same callsites. Now the method always resolves or rejects.

--- static/app/actionCreators/organization.tsx
+++ static/app/actionCreators/organization.tsx
@@ -131,12 +131,12 @@ async function fetchProjectsAndTeams(
  *               current organization in the store)
  * @param usePreload Should the preloaded data be used if available?
  */
-export function fetchOrganizationDetails(
+export async function fetchOrganizationDetails(
   api: Client,
   slug: string,
   silent: boolean,
   usePreload?: boolean
-) {
+): Promise<void> {
   if (!silent) {
     OrganizationStore.reset();
     ProjectsStore.reset();
@@ -154,53 +154,48 @@ export function fetchOrganizationDetails(
     return null;
   };
 
-  const loadOrganization = () => {
-    return new Promise(async (resolve, reject) => {
-      let org: Organization | undefined = undefined;
-      try {
-        org = await fetchOrg(api, slug, usePreload);
-      } catch (err) {
-        if (!err) {
-          reject(err);
-          return;
+  const loadOrganization = async () => {
+    let org: Organization | undefined = undefined;
+    try {
+      org = await fetchOrg(api, slug, usePreload);
+    } catch (err) {
+      if (!err) {
+        throw err;
+      }
+
+      OrganizationStore.onFetchOrgError(err);
+
+      if (err.status === 403 || err.status === 401) {
+        const errMessage = getErrorMessage(err);
+
+        if (errMessage) {
+          addErrorMessage(errMessage);
+          throw errMessage;
         }
 
-        OrganizationStore.onFetchOrgError(err);
-
-        if (err.status === 403 || err.status === 401) {
-          const errMessage = getErrorMessage(err);
-
-          if (errMessage) {
-            addErrorMessage(errMessage);
-            reject(errMessage);
-          }
-
-          return;
-        }
-        Sentry.captureException(err);
+        return undefined;
       }
-      resolve(org);
-    });
+      Sentry.captureException(err);
+    }
+    return org;
   };
 
-  const loadTeamsAndProjects = () => {
-    return new Promise(async resolve => {
-      const [[projects], [teams, , resp]] = await fetchProjectsAndTeams(slug, usePreload);
+  const loadTeamsAndProjects = async () => {
+    const [[projects], [teams, , resp]] = await fetchProjectsAndTeams(slug, usePreload);
 
-      ProjectsStore.loadInitialData(projects ?? []);
+    ProjectsStore.loadInitialData(projects ?? []);
 
-      const teamPageLinks = resp?.getResponseHeader('Link');
-      if (teamPageLinks) {
-        const paginationObject = parseLinkHeader(teamPageLinks);
-        const hasMore = paginationObject?.next?.results ?? false;
-        const cursor = paginationObject.next?.cursor;
-        TeamStore.loadInitialData(teams, hasMore, cursor);
-      } else {
-        TeamStore.loadInitialData(teams);
-      }
-      resolve([projects, teams]);
-    });
+    const teamPageLinks = resp?.getResponseHeader('Link');
+    if (teamPageLinks) {
+      const paginationObject = parseLinkHeader(teamPageLinks);
+      const hasMore = paginationObject?.next?.results ?? false;
+      const cursor = paginationObject.next?.cursor;
+      TeamStore.loadInitialData(teams, hasMore, cursor);
+    } else {
+      TeamStore.loadInitialData(teams);
+    }
+    return [projects, teams];
   };
 
-  return Promise.all([loadOrganization(), loadTeamsAndProjects()]);
+  await Promise.all([loadOrganization(), loadTeamsAndProjects()]);
 }
