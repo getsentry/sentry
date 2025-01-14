52bcb45379982a787f61b4038f4d2270ba0158d7
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2025-01-14 17:29:02 -0500

    ref: fix argument errors in dispatch signatures (#83447)
    <!-- Describe your PR here. -->

--- src/sentry/api/endpoints/auth_config.py
+++ src/sentry/api/endpoints/auth_config.py
@@ -1,5 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.http.request import HttpRequest
+from django.http.response import HttpResponseBase
 from django.urls import reverse
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -30,7 +32,7 @@ class AuthConfigEndpoint(Endpoint, OrganizationMixin):
     # Disable authentication and permission requirements.
     permission_classes = ()
 
-    def dispatch(self, request: Request, *args, **kwargs) -> Response:
+    def dispatch(self, request: HttpRequest, *args, **kwargs) -> HttpResponseBase:
         self.determine_active_organization(request)
         return super().dispatch(request, *args, **kwargs)
 
