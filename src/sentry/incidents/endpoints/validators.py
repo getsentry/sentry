636ce9291d99c9883228a2fbfcbd1a761b7710c4
Colleen O'Rourke <colleen@sentry.io>
2025-01-14 14:49:01 -0800

    rm config

--- src/sentry/incidents/endpoints/validators.py
+++ src/sentry/incidents/endpoints/validators.py
@@ -94,7 +94,6 @@ class MetricAlertComparisonConditionValidator(NumericComparisonConditionValidato
 class MetricAlertsDetectorValidator(BaseGroupTypeDetectorValidator):
     data_source = SnubaQueryDataSourceValidator(required=True)
     data_conditions = MetricAlertComparisonConditionValidator(many=True)
-    config = serializers.JSONField(required=False)
 
     def validate(self, attrs):
         """
