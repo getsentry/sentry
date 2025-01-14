20d8758a9c00254f3bca3387004996d65e281b50
Colleen O'Rourke <colleen@sentry.io>
2025-01-14 15:26:43 -0800

    feat(alerts): Add schema for MetricAlertFire (#82216)
    Based on https://github.com/getsentry/sentry/pull/81979/ define a schema
    for `MetricRuleFire` group type.
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- tests/sentry/workflow_engine/endpoints/test_project_detector_index.py
+++ tests/sentry/workflow_engine/endpoints/test_project_detector_index.py
@@ -32,15 +32,11 @@ class ProjectDetectorIndexBaseTest(APITestCase):
 @region_silo_test
 class ProjectDetectorIndexGetTest(ProjectDetectorIndexBaseTest):
     def test_simple(self):
-        detector = Detector.objects.create(
-            organization_id=self.organization.id,
-            name="Test Detector",
-            type=MetricAlertFire.slug,
+        detector = self.create_detector(
+            organization_id=self.organization.id, name="Test Detector", type=MetricAlertFire.slug
         )
-        detector_2 = Detector.objects.create(
-            organization_id=self.organization.id,
-            name="Test Detector 2",
-            type=MetricAlertFire.slug,
+        detector_2 = self.create_detector(
+            organization_id=self.organization.id, name="Test Detector 2", type=MetricAlertFire.slug
         )
         response = self.get_success_response(self.organization.slug, self.project.slug)
         assert response.data == serialize([detector, detector_2])
