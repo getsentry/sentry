a62ed5e5df817896b19ed6bca5f7659f4ba28fe0
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2025-01-14 10:25:28 -0500

    chore(derived_code_mappings): Remove feature flag
    The flag is no longer needed.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -107,8 +107,6 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:data-secrecy", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     manager.add("organizations:devtoolbar", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, default=False, api_expose=True)
     manager.add("organizations:email-performance-regression-image", OrganizationFeature, FeatureHandlerStrategy.OPTIONS, api_expose=False)
-    # Enables automatically deriving of code mappings
-    manager.add("organizations:derive-code-mappings", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, default=True)
     # Enables synthesis of device.class in ingest
     manager.add("organizations:device-class-synthesis", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enable device.class as a selectable column
