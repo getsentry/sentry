f87cf9118f5bcc32b54bdb97bfc891caed40ed49
MichaelSun48 <michael.sun@sentry.io>
2025-01-15 10:07:13 -0800

    Fix date comparators AGAIN

--- src/sentry/backup/comparators.py
+++ src/sentry/backup/comparators.py
@@ -806,8 +806,14 @@ def get_default_comparators() -> dict[str, list[JSONScrubbingComparator]]:
                 DateUpdatedComparator("date_added", "date_updated"),
             ],
             "sentry.groupsearchview": [DateUpdatedComparator("date_updated")],
-            "sentry.groupsearchviewproject": [DateUpdatedComparator("date_updated")],
-            "sentry.groupsearchviewenvironment": [DateUpdatedComparator("date_updated")],
+            "sentry.groupsearchviewproject": [
+                DateUpdatedComparator("date_updated"),
+                DateUpdatedComparator("date_added"),
+            ],
+            "sentry.groupsearchviewenvironment": [
+                DateUpdatedComparator("date_updated"),
+                DateUpdatedComparator("date_added"),
+            ],
             "sentry.incident": [UUID4Comparator("detection_uuid")],
             "sentry.incidentactivity": [UUID4Comparator("notification_uuid")],
             "sentry.incidenttrigger": [DateUpdatedComparator("date_modified")],
