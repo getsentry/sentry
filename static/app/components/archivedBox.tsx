9262f8b2e42ba9231c521b99f7abedb5bd1cba79
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-01-16 15:52:42 -0500

    feat(issue-details): Display the resolution/archival reason on new UI (#83555)
    Though the activity was still present, we stopped showing a banner for
    reasons associated with a resolution or archival. Those are brought back
    now alongside the new banner.
    
    <img width="640" alt="image"
    src="https://github.com/user-attachments/assets/6840e069-c048-4e18-9e17-56ac7c235e14"
    />
    
    <img width="535" alt="image"
    src="https://github.com/user-attachments/assets/6dc57b2b-8175-4942-9105-9608a774f323"
    />

--- static/app/components/archivedBox.tsx
+++ static/app/components/archivedBox.tsx
@@ -12,76 +12,79 @@ interface ArchivedBoxProps {
   organization: Organization;
   statusDetails: IgnoredStatusDetails;
   substatus: Group['substatus'];
+  hasStreamlinedUI?: boolean;
 }
 
+export function renderArchiveReason({
+  substatus,
+  statusDetails,
+  organization,
+  hasStreamlinedUI = false,
+}: ArchivedBoxProps) {
+  const {ignoreUntil, ignoreCount, ignoreWindow, ignoreUserCount, ignoreUserWindow} =
+    statusDetails;
+
+  if (substatus === GroupSubstatus.ARCHIVED_UNTIL_ESCALATING) {
+    return hasStreamlinedUI
+      ? t('This issue has been archived until it escalates.')
+      : t(
+          "This issue has been archived. It'll return to your inbox if it escalates. To learn more, %s",
+          <ExternalLink
+            href="https://docs.sentry.io/product/issues/states-triage/#archive"
+            onClick={() =>
+              trackAnalytics('issue_details.issue_status_docs_clicked', {organization})
+            }
+          >
+            {t('read the docs')}
+          </ExternalLink>
+        );
+  }
+  if (ignoreUntil) {
+    return t(
+      'This issue has been archived until %s.',
+      <strong>
+        <DateTime date={ignoreUntil} />
+      </strong>
+    );
+  }
+  if (ignoreCount && ignoreWindow) {
+    return t(
+      'This issue has been archived until it occurs %s time(s) in %s.',
+      <strong>{ignoreCount.toLocaleString()}</strong>,
+      <strong>
+        <Duration seconds={ignoreWindow * 60} />
+      </strong>
+    );
+  }
+  if (ignoreCount) {
+    return t(
+      'This issue has been archived until it occurs %s more time(s).',
+      <strong>{ignoreCount.toLocaleString()}</strong>
+    );
+  }
+  if (ignoreUserCount && ignoreUserWindow) {
+    return t(
+      'This issue has been archived until it affects %s user(s) in %s.',
+      <strong>{ignoreUserCount.toLocaleString()}</strong>,
+      <strong>
+        <Duration seconds={ignoreUserWindow * 60} />
+      </strong>
+    );
+  }
+  if (ignoreUserCount) {
+    return t(
+      'This issue has been archived until it affects %s more user(s).',
+      <strong>{ignoreUserCount.toLocaleString()}</strong>
+    );
+  }
+
+  return t('This issue has been archived forever.');
+}
 function ArchivedBox({substatus, statusDetails, organization}: ArchivedBoxProps) {
-  function trackDocsClick() {
-    trackAnalytics('issue_details.issue_status_docs_clicked', {
-      organization,
-    });
-  }
-
-  function renderReason() {
-    const {ignoreUntil, ignoreCount, ignoreWindow, ignoreUserCount, ignoreUserWindow} =
-      statusDetails;
-
-    if (substatus === GroupSubstatus.ARCHIVED_UNTIL_ESCALATING) {
-      return t(
-        "This issue has been archived. It'll return to your inbox if it escalates. To learn more, %s",
-        <ExternalLink
-          href="https://docs.sentry.io/product/issues/states-triage/#archive"
-          onClick={trackDocsClick}
-        >
-          {t('read the docs')}
-        </ExternalLink>
-      );
-    }
-    if (ignoreUntil) {
-      return t(
-        'This issue has been archived until %s.',
-        <strong>
-          <DateTime date={ignoreUntil} />
-        </strong>
-      );
-    }
-    if (ignoreCount && ignoreWindow) {
-      return t(
-        'This issue has been archived until it occurs %s time(s) in %s.',
-        <strong>{ignoreCount.toLocaleString()}</strong>,
-        <strong>
-          <Duration seconds={ignoreWindow * 60} />
-        </strong>
-      );
-    }
-    if (ignoreCount) {
-      return t(
-        'This issue has been archived until it occurs %s more time(s).',
-        <strong>{ignoreCount.toLocaleString()}</strong>
-      );
-    }
-    if (ignoreUserCount && ignoreUserWindow) {
-      return t(
-        'This issue has been archived until it affects %s user(s) in %s.',
-        <strong>{ignoreUserCount.toLocaleString()}</strong>,
-        <strong>
-          <Duration seconds={ignoreUserWindow * 60} />
-        </strong>
-      );
-    }
-    if (ignoreUserCount) {
-      return t(
-        'This issue has been archived until it affects %s more user(s).',
-        <strong>{ignoreUserCount.toLocaleString()}</strong>
-      );
-    }
-
-    return t('This issue has been archived forever.');
-  }
-
   return (
     <BannerContainer priority="default">
       <BannerSummary>
-        <span>{renderReason()}</span>
+        <span>{renderArchiveReason({substatus, statusDetails, organization})}</span>
       </BannerSummary>
     </BannerContainer>
   );
