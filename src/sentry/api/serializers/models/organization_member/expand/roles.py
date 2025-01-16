a1dc065f14bc5f1c956cec3f1207dc501b95b5d6
mdtro <20070360+mdtro@users.noreply.github.com>
2025-01-16 10:54:33 -0600

    use pop instead of del

--- src/sentry/api/serializers/models/organization_member/expand/roles.py
+++ src/sentry/api/serializers/models/organization_member/expand/roles.py
@@ -59,8 +59,7 @@ class OrganizationMemberWithRolesSerializer(OrganizationMemberWithTeamsSerialize
 
         # Filter out emails from the serialized user data
         for user_data in users_by_id.values():
-            if "emails" in user_data:
-                del user_data["emails"]
+            user_data.pop("emails", None)
 
         for item in item_list:
             result.setdefault(item, {})["serializedUser"] = users_by_id.get(str(item.user_id), {})
