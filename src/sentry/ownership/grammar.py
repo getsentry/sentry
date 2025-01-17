cebacd54644329fb1096a52cfbf088144f7d6f48
Joshua Ferge <josh.ferge@sentry.io>
2025-01-17 14:21:17 -0500

    fix re-export

--- src/sentry/ownership/grammar.py
+++ src/sentry/ownership/grammar.py
@@ -1,1 +1,7 @@
-from sentry.issues.ownership.grammar import *  # noqa: F401, F403
+from sentry.issues.ownership.grammar import (  # noqa: F401, F403
+    Matcher,
+    Owner,
+    Rule,
+    dump_schema,
+    load_schema,
+)
