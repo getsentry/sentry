88d18f5aa25df6bf228f614d0647cdcd8a93a9be
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-01-17 13:26:23 -0500

    🧪 Fix experiment

--- src/sentry/issues/streamline.py
+++ src/sentry/issues/streamline.py
@@ -1,18 +1,22 @@
+from random import random
+
+from sentry import options
 from sentry.models.organization import Organization
-from sentry.utils.options import sample_modulo
 
 
 def apply_streamline_rollout_group(organization: Organization):
-    # If the rollout_result is false, the organization is not in the experiment.
-    rollout_result = sample_modulo(
-        "issues.details.streamline-experiment-rollout-rate", organization.id
-    )
+    # This shouldn't run multiple times, but if it does, don't resort the organization.
+    if organization.get_option("sentry:streamline_ui_only") is not None:
+        return
 
-    if rollout_result:
-        # If split_result is true, they only receive the Streamline UI.
-        # If split_result is false, they only receive the Legacy UI.
+    rollout_rate = options.get("issues.details.streamline-experiment-rollout-rate")
+
+    # If the rollout_rate is greater than a random number, the organization is in the experiment.
+    if rollout_rate > random():
+        split_rate = options.get("issues.details.streamline-experiment-split-rate")
+        # If the split_rate is greater than a random number, the split_result is true..
+        split_result = split_rate > random()
+        # When split_result is true, they only receive the Streamline UI.
+        # When split_result is false, they only receive the Legacy UI.
         # This behaviour is controlled on the frontend.
-        split_result = sample_modulo(
-            "issues.details.streamline-experiment-split-rate", organization.id
-        )
         organization.update_option("sentry:streamline_ui_only", split_result)
