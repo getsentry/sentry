03d052e623fd5db999017f55cff4214fc35b1ae8
Raj Joshi <raj.joshi@sentry.io>
2025-01-21 12:10:15 -0500

    🔧  chore(pagerduty): Support "default" for custom severity config (#83564)

--- tests/sentry/incidents/action_handlers/__init__.py
+++ tests/sentry/incidents/action_handlers/__init__.py
@@ -13,15 +13,13 @@ class FireTest(TestCase, abc.ABC):
     def run_test(self, incident: Incident, method: str, **kwargs):
         pass
 
-    def run_fire_test(self, method="fire", chart_url=None):
+    def run_fire_test(self, method="fire", chart_url=None, status=IncidentStatus.CLOSED):
         kwargs = {}
         if chart_url:
             kwargs = {"chart_url": chart_url}
 
         self.alert_rule = self.create_alert_rule()
-        incident = self.create_incident(
-            alert_rule=self.alert_rule, status=IncidentStatus.CLOSED.value
-        )
+        incident = self.create_incident(alert_rule=self.alert_rule, status=status.value)
         if method == "resolve":
             update_incident_status(
                 incident, IncidentStatus.CLOSED, status_method=IncidentStatusMethod.MANUAL
