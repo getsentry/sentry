5bc96bb87d9cc53b2ca83acd9cefa37b5e7a05bf
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-01-21 15:46:39 +0100

    fix(project-settings): If storeCrashReports is null remove '"sentry:store_crash_reports"' value from project options (#83728)

--- src/sentry/api/endpoints/project_details.py
+++ src/sentry/api/endpoints/project_details.py
@@ -697,7 +697,7 @@ class ProjectDetailsEndpoint(ProjectEndpoint):
         if result.get("highlightTags") is not None:
             if project.update_option("sentry:highlight_tags", result["highlightTags"]):
                 changed_proj_settings["sentry:highlight_tags"] = result["highlightTags"]
-        if result.get("storeCrashReports") is not None:
+        if "storeCrashReports" in result:
             if project.get_option("sentry:store_crash_reports") != result["storeCrashReports"]:
                 changed_proj_settings["sentry:store_crash_reports"] = result["storeCrashReports"]
                 if result["storeCrashReports"] is None:
