1510f164a6047751ea4549ed29a52f09745f8b6b
Raj Joshi <raj.joshi@sentry.io>
2025-01-21 14:24:01 -0800

    :sparkles: feat: update uptime threading behavior

--- src/sentry/integrations/slack/actions/notification.py
+++ src/sentry/integrations/slack/actions/notification.py
@@ -1,12 +1,14 @@
 from __future__ import annotations
 
 from collections.abc import Generator, Sequence
+from datetime import datetime
 from logging import Logger, getLogger
 from typing import Any
 
 import orjson
 from slack_sdk.errors import SlackApiError
 
+from sentry import features
 from sentry.api.serializers.rest_framework.rule import ACTION_UUID_KEY
 from sentry.constants import ISSUE_ALERTS_THREAD_DEFAULT
 from sentry.eventstore.models import GroupEvent
@@ -39,15 +41,42 @@ from sentry.integrations.slack.utils.errors import (
     unpack_slack_api_error,
 )
 from sentry.integrations.utils.metrics import EventLifecycle
+from sentry.issues.grouptype import GroupCategory
+from sentry.models.activity import Activity
+from sentry.models.group import Group
 from sentry.models.options.organization_option import OrganizationOption
 from sentry.models.rule import Rule
 from sentry.notifications.additional_attachment_manager import get_additional_attachment
 from sentry.rules.actions import IntegrationEventAction
 from sentry.rules.base import CallbackFuture
+from sentry.types.activity import ActivityType
 from sentry.types.rules import RuleFuture
 from sentry.utils import metrics
 
 _default_logger: Logger = getLogger(__name__)
+
+
+def open_period_start_for_group(group: Group) -> datetime | None:
+    """
+    Get the start of the open period for a group.
+    This is the last activity of the group that is not a resolution or the first_seen of the group.
+    We need to check the first seen since we don't create an activity when the group is created.
+    """
+
+    # Get the last activity of the group
+    latest_unresolved_activity: Activity | None = (
+        Activity.objects.filter(
+            group=group,
+            type=ActivityType.SET_UNRESOLVED.value,
+        )
+        .order_by("-datetime")
+        .first()
+    )
+
+    if latest_unresolved_activity:
+        return latest_unresolved_activity.datetime
+
+    return group.first_seen
 
 
 class SlackNotifyServiceAction(IntegrationEventAction):
@@ -129,6 +158,16 @@ class SlackNotifyServiceAction(IntegrationEventAction):
                 rule_action_uuid=rule_action_uuid,
             )
 
+            open_period_start: datetime | None = None
+            if (
+                features.has(
+                    "organizations:slack-threads-refactor-uptime", self.project.organization
+                )
+                and event.group.issue_category == GroupCategory.UPTIME
+            ):
+                open_period_start = open_period_start_for_group(event.group)
+                new_notification_message_object.open_period_start = open_period_start
+
             def get_thread_ts(lifecycle: EventLifecycle) -> str | None:
                 """Find the thread in which to post this notification as a reply.
 
@@ -152,6 +191,7 @@ class SlackNotifyServiceAction(IntegrationEventAction):
                         rule_id=rule_id,
                         group_id=event.group.id,
                         rule_action_uuid=rule_action_uuid,
+                        open_period_start=open_period_start,
                     )
                 except Exception as e:
                     lifecycle.record_halt(e)
