ddec465e39394e4d136f3af1473631e51b123f95
Christinarlong <christina.long@sentry.io>
2025-01-22 14:57:43 -0800

    improve logging in installation pipeline to debug cust issue

--- src/sentry/integrations/pipeline.py
+++ src/sentry/integrations/pipeline.py
@@ -252,7 +252,10 @@ class IntegrationPipeline(Pipeline):
         ):
             self.get_logger().info(
                 "finish_pipeline.multi_region_install_error",
-                extra={"organization_id": self.organization_id, "provider_key": self.provider.key},
+                extra={
+                    "organization_id": self.organization_id,
+                    "provider_key": self.provider.key,
+                },
             )
             return self._dialog_response(
                 {
