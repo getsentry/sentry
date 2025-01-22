c49c5c0ca60c0307c3b30b8bc188b0dab915ed83
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-01-22 13:13:34 -0500

    fix(dashboards): Double padding in widgets (#83839)
    This is effectively a revert of
    https://github.com/getsentry/sentry/pull/83744 which ended up causing
    padding issues with tables. They're the only visualization type that
    ignores padding.

--- static/app/views/projectDetail/projectScoreCards/actionWrapper.tsx
+++ static/app/views/projectDetail/projectScoreCards/actionWrapper.tsx
@@ -3,5 +3,5 @@ import styled from '@emotion/styled';
 import {space} from 'sentry/styles/space';
 
 export const ActionWrapper = styled('div')`
-  padding: ${space(1)} 0 0 0;
+  padding: ${space(2)};
 `;
