de157374b1fd8d4dccd269fadd776b60e77f791e
ArthurKnaus <arthur.knaus@sentry.io>
2025-01-22 09:43:25 +0100

    feat(setup-wizard): Allow project creation (#83811)
    Allow creating Sentry projects in the setup wizard workflow.
    It is only enabled if the wizard passes the `project_platform` URL param
    which is used to define the projects platform.
    
    - part of https://github.com/getsentry/sentry-wizard/issues/549

---
+++ static/app/views/setupWizard/utils/useOrganizationTeams.tsx
@@ -0,0 +1,25 @@
+import type {Team} from 'sentry/types/organization';
+import {useQuery} from 'sentry/utils/queryClient';
+import type RequestError from 'sentry/utils/requestError/requestError';
+import useApi from 'sentry/utils/useApi';
+import type {OrganizationWithRegion} from 'sentry/views/setupWizard/types';
+
+export function useOrganizationTeams({
+  organization,
+}: {
+  organization?: OrganizationWithRegion;
+}) {
+  const api = useApi();
+
+  return useQuery<Team[], RequestError>({
+    queryKey: [`/organizations/${organization?.slug}/teams/`],
+    queryFn: () => {
+      return api.requestPromise(`/organizations/${organization?.slug}/teams/`, {
+        host: organization?.region.url,
+      });
+    },
+    enabled: !!organization,
+    refetchOnWindowFocus: true,
+    retry: false,
+  });
+}
