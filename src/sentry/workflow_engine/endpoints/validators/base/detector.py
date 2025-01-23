4d7801404312a71973fc3895c67e1ed0cf5bf463
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-01-23 13:25:22 -0800

    ref(workflow_engine): Refactor the Validators to be broken a part a bit more granularly (#83952)
    ## Description
    Break apart the `validators/base.py` into many files for each of the
    bases (data source, data condition, detector) to prepare for adding more
    (actions and workflows).
    
    This isn't changing any logic, just organizing the code.

--- src/sentry/workflow_engine/endpoints/validators/base/detector.py
+++ src/sentry/workflow_engine/endpoints/validators/base/detector.py
@@ -1,116 +1,22 @@
-from collections.abc import Callable
-from typing import Generic, TypeVar
-
 from django.db import router, transaction
 from rest_framework import serializers
-from rest_framework.fields import Field
 
 from sentry import audit_log
 from sentry.api.serializers.rest_framework import CamelSnakeSerializer
-from sentry.db.models import Model
 from sentry.issues import grouptype
 from sentry.issues.grouptype import GroupType
 from sentry.utils.audit import create_audit_entry
+from sentry.workflow_engine.endpoints.validators.base import (
+    BaseDataConditionValidator,
+    BaseDataSourceValidator,
+)
 from sentry.workflow_engine.models import (
     DataConditionGroup,
     DataSource,
     DataSourceDetector,
     Detector,
 )
-from sentry.workflow_engine.models.data_condition import Condition, DataCondition
-from sentry.workflow_engine.registry import data_source_type_registry
-from sentry.workflow_engine.types import DataSourceTypeHandler, DetectorPriorityLevel
-
-T = TypeVar("T", bound=Model)
-
-
-class DataSourceCreator(Generic[T]):
-    def __init__(self, create_fn: Callable[[], T]):
-        self._create_fn = create_fn
-        self._instance: T | None = None
-
-    def create(self) -> T:
-        if self._instance is None:
-            self._instance = self._create_fn()
-        return self._instance
-
-
-class BaseDataConditionValidator(CamelSnakeSerializer):
-
-    type = serializers.CharField(
-        required=True,
-        max_length=200,
-        help_text="Condition used to compare data value to the stored comparison value",
-    )
-
-    @property
-    def comparison(self) -> Field:
-        raise NotImplementedError
-
-    @property
-    def result(self) -> Field:
-        raise NotImplementedError
-
-    def validate(self, attrs):
-        attrs = super().validate(attrs)
-        return attrs
-
-
-class NumericComparisonConditionValidator(BaseDataConditionValidator):
-
-    comparison = serializers.FloatField(
-        required=True,
-        help_text="Comparison value to be compared against value from data.",
-    )
-    result = serializers.ChoiceField(
-        choices=[
-            (DetectorPriorityLevel.HIGH, "High"),
-            (DetectorPriorityLevel.MEDIUM, "Medium"),
-            (DetectorPriorityLevel.LOW, "Low"),
-        ]
-    )
-
-    @property
-    def supported_conditions(self) -> frozenset[Condition]:
-        raise NotImplementedError
-
-    @property
-    def supported_results(self) -> frozenset[DetectorPriorityLevel]:
-        raise NotImplementedError
-
-    def validate_type(self, value: str) -> Condition:
-        try:
-            type = Condition(value)
-        except ValueError:
-            type = None
-
-        if type not in self.supported_conditions:
-            raise serializers.ValidationError(f"Unsupported type {value}")
-        return type
-
-    def validate_result(self, value: str) -> DetectorPriorityLevel:
-        try:
-            result = DetectorPriorityLevel(int(value))
-        except ValueError:
-            result = None
-        if result not in self.supported_results:
-            raise serializers.ValidationError("Unsupported condition result")
-        return result
-
-
-class BaseDataSourceValidator(CamelSnakeSerializer, Generic[T]):
-    @property
-    def data_source_type_handler(self) -> type[DataSourceTypeHandler]:
-        raise NotImplementedError
-
-    def validate(self, attrs):
-        attrs = super().validate(attrs)
-        attrs["_creator"] = DataSourceCreator[T](lambda: self.create_source(attrs))
-        attrs["data_source_type"] = data_source_type_registry.get_key(self.data_source_type_handler)
-        return attrs
-
-    def create_source(self, validated_data) -> T:
-        raise NotImplementedError
+from sentry.workflow_engine.models.data_condition import DataCondition
 
 
 class BaseGroupTypeDetectorValidator(CamelSnakeSerializer):
