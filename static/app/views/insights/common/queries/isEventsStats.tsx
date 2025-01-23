1d1fefd5ba07658ef281f41d16bae7d435bba505
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-01-23 16:14:28 -0500

    More concrete behaviour

--- static/app/views/insights/common/queries/isEventsStats.tsx
+++ static/app/views/insights/common/queries/isEventsStats.tsx
@@ -4,18 +4,46 @@ import type {
   MultiSeriesEventsStats,
 } from 'sentry/types/organization';
 
-export function isEventsStats(
-  obj: EventsStats | MultiSeriesEventsStats | GroupedMultiSeriesEventsStats
-): obj is EventsStats {
-  return typeof obj === 'object' && obj !== null && typeof obj.data === 'object';
-}
-
-export function isMultiSeriesEventsStats(
-  obj: EventsStats | MultiSeriesEventsStats | GroupedMultiSeriesEventsStats
-): obj is MultiSeriesEventsStats {
+export function isEventsStats(obj: unknown): obj is EventsStats {
   if (typeof obj !== 'object' || obj === null) {
     return false;
   }
 
-  return Object.values(obj).every(series => isEventsStats(series));
+  return 'data' in obj && Array.isArray(obj.data);
+}
+
+export function isMultiSeriesEventsStats(obj: unknown): obj is MultiSeriesEventsStats {
+  if (typeof obj !== 'object' || obj === null) {
+    return false;
+  }
+
+  return (
+    getValues(obj).every(subObject => isEventsStats(subObject)) &&
+    !obj.hasOwnProperty('data')
+  );
+}
+
+export function isGroupedMultiSeriesEventsStats(
+  obj: unknown
+): obj is GroupedMultiSeriesEventsStats {
+  if (obj === null || obj === undefined) {
+    return false;
+  }
+
+  return (
+    getValues(obj).every(subObject => isMultiSeriesEventsStats(subObject)) &&
+    !obj.hasOwnProperty('data')
+  );
+}
+
+function getValues(obj: unknown): unknown[] {
+  if (obj === null || obj === undefined) {
+    return [];
+  }
+
+  return Object.entries(obj)
+    .filter(([key, _value]) => key !== 'order')
+    .map(([_key, value]) => {
+      return value as unknown;
+    });
 }
