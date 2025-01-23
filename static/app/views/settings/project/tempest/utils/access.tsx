f8982ccaed862f943b3c1d1ada4e2ae67254d795
Vjeran Grozdanić <vjeran.grozdanic@sentry.io>
2025-01-23 13:49:56 +0100

    feat(tempest): relax permissions UI needed to add/delete tempest credentials (#83909)
    This will be deployed after
    https://github.com/getsentry/sentry/pull/83908
    
    The changes follow relaxation of permissions also in UI.

--- static/app/views/settings/project/tempest/utils/access.tsx
+++ static/app/views/settings/project/tempest/utils/access.tsx
@@ -4,7 +4,7 @@ import {hasEveryAccess} from 'sentry/components/acl/access';
 import type {Scope} from 'sentry/types/core';
 import useOrganization from 'sentry/utils/useOrganization';
 
-const TEMPEST_WRITE_ACCESS: Scope[] = ['org:admin'];
+const TEMPEST_WRITE_ACCESS: Scope[] = ['org:admin', 'project:admin', 'project:write'];
 
 export function useHasTempestWriteAccess() {
   const organization = useOrganization();
