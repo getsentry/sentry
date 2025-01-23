e17dca0322e012b89cb30e2caddab4f310d7bbd9
Tony Xiao <txiao@sentry.io>
2025-01-23 17:52:53 -0500

    chore(explore): Better samples mode default columns (#83946)
    Change the columns available in the samples mode by default.

--- static/app/views/explore/tables/spansTable.tsx
+++ static/app/views/explore/tables/spansTable.tsx
@@ -76,7 +76,13 @@ export function SpansTable({confidences, spansTableResult}: SpansTableProps) {
   });
 
   const tableRef = useRef<HTMLTableElement>(null);
-  const {initialTableStyles, onResizeMouseDown} = useTableStyles(visibleFields, tableRef);
+  const {initialTableStyles, onResizeMouseDown} = useTableStyles(
+    visibleFields,
+    tableRef,
+    {
+      minimumColumnWidth: 50,
+    }
+  );
 
   const meta = result.meta ?? {};
 
