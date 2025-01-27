7402331312866cbd184c097799dba7ea9170ce54
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2025-01-27 10:25:41 -0500

    chore(github): Remove debugging code (#83986)
    This code was added to investigate an edge case.
    
    An organization with fewer repositories than the maximum page number
    would not add a `Link` header. In other words, we only see when a GitHub
    org has less than 100 repositories.

--- src/sentry/integrations/github/client.py
+++ src/sentry/integrations/github/client.py
@@ -503,8 +503,6 @@ class GitHubBaseClient(GithubProxyClient, RepositoryClient, CommitContextClient)
         It uses page_size from the base class to specify how many items per page.
         The upper bound of requests is controlled with self.page_number_limit to prevent infinite requests.
         """
-        # XXX: In order to speed up this function we could use ThreadPoolExecutor
-        # to fetch repositories in parallel. See src/sentry/utils/snuba.py
         repos = self.get_with_pagination(
             "/installation/repositories",
             response_key="repositories",
@@ -550,31 +548,19 @@ class GitHubBaseClient(GithubProxyClient, RepositoryClient, CommitContextClient)
             output = []
 
             page_number = 1
-            logger.info("Page %s: %s?per_page=%s", page_number, path, self.page_size)
             resp = self.get(path, params={"per_page": self.page_size})
             output.extend(resp) if not response_key else output.extend(resp[response_key])
             next_link = get_next_link(resp)
 
-            # XXX: Debugging code; remove afterward
-            if (
-                response_key
-                and response_key == "repositories"
-                and resp["total_count"] > 0
-                and not output
-            ):
-                logger.info("headers: %s", resp.headers)
-                logger.info("output: %s", output)
-                logger.info("next_link: %s", next_link)
-                logger.error("No list of repos even when there's some. Investigate.")
-
             # XXX: In order to speed up this function we will need to parallelize this
             # Use ThreadPoolExecutor; see src/sentry/utils/snuba.py#L358
             while next_link and page_number < page_number_limit:
+                # If a per_page is specified, GitHub preserves the per_page value
+                # in the response headers.
                 resp = self.get(next_link)
                 output.extend(resp) if not response_key else output.extend(resp[response_key])
 
                 next_link = get_next_link(resp)
-                logger.info("Page %s: %s", page_number, next_link)
                 page_number += 1
             return output
 
