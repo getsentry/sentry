d6c577267b2e8d56bc29417038492a85a521b810
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-01-27 15:54:34 +0100

    ref(onboarding): Order popular platforms by 'popularity' (#84066)

--- static/app/components/platformPicker.tsx
+++ static/app/components/platformPicker.tsx
@@ -104,9 +104,19 @@ class PlatformPicker extends Component<PlatformPickerProps, State> {
       }
     }
 
-    const filtered = tempSelectablePlatforms
-      .filter(this.state.filter ? subsetMatch : categoryMatch)
-      .sort((a, b) => {
+    // 'other' is not part of the createablePlatforms list, therefore it won't be included in the filtered list
+    const filtered = tempSelectablePlatforms.filter(
+      this.state.filter ? subsetMatch : categoryMatch
+    );
+
+    if (this.props.showOther && this.state.filter.toLocaleLowerCase() === 'other') {
+      // We only show 'Other' if users click on the 'Other' suggestion rendered in the not found state or type this word in the search bar
+      return [otherPlatform];
+    }
+
+    if (category !== 'popular') {
+      // We only want to sort the platforms alphabetically if users are not viewing the 'popular' tab category
+      return filtered.sort((a, b) => {
         if (startsWithPunctuation(a.name) && !startsWithPunctuation(b.name)) {
           return 1;
         }
@@ -115,13 +125,8 @@ class PlatformPicker extends Component<PlatformPickerProps, State> {
         }
         return a.name.localeCompare(b.name);
       });
-
-    if (this.props.showOther && this.state.filter.toLocaleLowerCase() === 'other') {
-      // We only show 'Other' if users click on the 'Other' suggestion rendered in the not found state or type this word in the search bar
-      return [otherPlatform];
     }
 
-    // 'other' is not part of the createablePlatforms list, therefore it won't be included in the filtered list
     return filtered;
   }
 
