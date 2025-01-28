da2781abb7f15419aab3776507ae1f9b6af0e140
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2025-01-28 10:30:05 -0500

    ref: fix typing for incidents.subscription_processor (#84087)
    <!-- Describe your PR here. -->

--- tests/sentry/incidents/test_subscription_processor.py
+++ tests/sentry/incidents/test_subscription_processor.py
@@ -2966,7 +2966,7 @@ class ProcessUpdateTest(ProcessUpdateBaseClass):
             == DATA_SOURCE_SNUBA_QUERY_SUBSCRIPTION
         )
         data_packet_list = mock_process_data_packets.call_args_list[0][0][0]
-        assert data_packet_list[0].query_id == self.sub.id
+        assert data_packet_list[0].query_id == str(self.sub.id)
         assert data_packet_list[0].packet["values"] == {"data": [{"some_col_name": 10}]}
 
 
