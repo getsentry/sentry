90ec16a1e24ad43f290f480c1ca1b6d28b865ecf
Catherine Lee <55311782+c298lee@users.noreply.github.com>
2025-01-28 11:49:58 -0800

    replace @sentry/utils with @sentry/core

--- static/app/utils/performanceForSentry/index.tsx
+++ static/app/utils/performanceForSentry/index.tsx
@@ -1,12 +1,12 @@
 import type {ProfilerOnRenderCallback, ReactNode} from 'react';
 import {Fragment, Profiler, useEffect, useRef} from 'react';
 import type {MeasurementUnit, Span, TransactionEvent} from '@sentry/core';
-import * as Sentry from '@sentry/react';
 import {
   _browserPerformanceTimeOriginMode,
   browserPerformanceTimeOrigin,
   timestampInSeconds,
-} from '@sentry/utils';
+} from '@sentry/core';
+import * as Sentry from '@sentry/react';
 
 import {useLocation} from 'sentry/utils/useLocation';
 import usePrevious from 'sentry/utils/usePrevious';
