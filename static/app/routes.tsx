b31acd13ef6d21de797101c79acdfbd9e1b44086
Malachi Willey <malwilley@gmail.com>
2025-01-27 16:36:18 -0800

    feat(nav): Implement new secondary nav in explore

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -1902,10 +1902,12 @@ function buildRoutes() {
 
   const exploreRoutes = (
     <Route
-      path="/explore/logs"
-      component={make(() => import('sentry/views/explore/logs'))}
+      path="/explore/"
+      component={make(() => import('sentry/views/explore/navigation'))}
       withOrgPath
-    />
+    >
+      <Route path="logs" component={make(() => import('sentry/views/explore/logs'))} />
+    </Route>
   );
 
   const userFeedbackRoutes = (
