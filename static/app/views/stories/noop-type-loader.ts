6a6371968b09139cce14cfe1a4e8ea407d6c8dd6
JonasBa <jonas@badalic.com>
2025-01-28 18:01:59 -0500

    build: ignore async without await in noop loader

--- static/app/views/stories/noop-type-loader.ts
+++ static/app/views/stories/noop-type-loader.ts
@@ -1,8 +1,6 @@
 import type {LoaderContext} from 'webpack';
 
-// Use async to mimick the type-loader behavior
-// eslint-disable-next-line require-await
-export default async function noopTypeLoader(this: LoaderContext<any>, _source: string) {
+export default function noopTypeLoader(this: LoaderContext<any>, _source: string) {
   const callback = this.async();
   return callback(null, 'export default {}');
 }
