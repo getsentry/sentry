5ab070d4a6b2270f9b5c59b7c92066ae96847e2b
Raj Joshi <raj.joshi@sentry.io>
2025-01-29 11:00:11 -0800

    :bug: fix: make no issue tracker error to halt

--- src/sentry/integrations/bitbucket/issues.py
+++ src/sentry/integrations/bitbucket/issues.py
@@ -8,9 +8,18 @@ from django.urls import reverse
 from sentry.integrations.source_code_management.issues import SourceCodeIssueIntegration
 from sentry.models.group import Group
 from sentry.organizations.services.organization.service import organization_service
-from sentry.shared_integrations.exceptions import ApiError, IntegrationFormError
+from sentry.shared_integrations.exceptions import (
+    ApiError,
+    IntegrationFormError,
+    IntegrationInstallationConfigurationError,
+)
 from sentry.silo.base import all_silo_function
 from sentry.users.models.user import User
+
+# Generated based on the response from the Bitbucket API
+# Example: {"type": "error", "error": {"message": "Repository has no issue tracker."}}
+BITBUCKET_HALT_ERROR_CODES = ["Repository has no issue tracker."]
+
 
 ISSUE_TYPES = (
     ("bug", "Bug"),
@@ -140,6 +149,11 @@ class BitbucketIssuesSpec(SourceCodeIssueIntegration):
         try:
             issue = client.create_issue(data.get("repo"), data)
         except ApiError as e:
+            if e.json:
+                if (
+                    message := e.json.get("error", {}).get("message")
+                ) in BITBUCKET_HALT_ERROR_CODES:
+                    raise IntegrationInstallationConfigurationError(message)
             self.raise_error(e)
 
         return {
