be9aa5b63e00b036794ac1164dd6f8e8ae6861a6
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-01-29 13:55:25 -0500

    feat(new-trace): Adding flag check for new explore search option in tag tree

--- static/app/components/events/eventTags/eventTagsTreeRow.tsx
+++ static/app/components/events/eventTags/eventTagsTreeRow.tsx
@@ -4,7 +4,7 @@ import * as qs from 'query-string';
 
 import {openNavigateToExternalLinkModal} from 'sentry/actionCreators/modal';
 import {hasEveryAccess} from 'sentry/components/acl/access';
-import {DropdownMenu} from 'sentry/components/dropdownMenu';
+import {DropdownMenu, type MenuItemProps} from 'sentry/components/dropdownMenu';
 import type {TagTreeContent} from 'sentry/components/events/eventTags/eventTagsTree';
 import EventTagsValue from 'sentry/components/events/eventTags/eventTagsValue';
 import {AnnotatedTextErrors} from 'sentry/components/events/meta/annotatedText/annotatedTextErrors';
@@ -29,6 +29,7 @@ import {
   getSearchInExploreTarget,
   TraceDrawerActionKind,
 } from 'sentry/views/performance/newTraceDetails/traceDrawer/details/utils';
+import {useHasTraceNewUi} from 'sentry/views/performance/newTraceDetails/useHasTraceNewUi';
 import {getTransactionSummaryBaseUrl} from 'sentry/views/performance/transactionSummary/utils';
 
 interface EventTagTreeRowConfig {
@@ -125,6 +126,7 @@ function EventTagsTreeRowDropdown({
   project,
 }: Pick<EventTagsTreeRowProps, 'content' | 'event' | 'project'>) {
   const location = useLocation();
+  const hasNewTraceUi = useHasTraceNewUi();
   const organization = useOrganization();
   const {onClick: handleCopy} = useCopyToClipboard({
     text: content.value,
@@ -151,7 +153,122 @@ function EventTagsTreeRowDropdown({
   });
   const isIssueDetailsRoute = location.pathname.includes(`issues/${event.groupID}/`);
 
-  // ADD CHECK FOR FEATURE FLAG!!!!!!!
+  const items: MenuItemProps[] = [
+    ...(isIssueDetailsRoute
+      ? [
+          {
+            key: 'tag-details',
+            label: t('Tag breakdown'),
+            to: {
+              pathname: `/organizations/${organization.slug}/issues/${event.groupID}/tags/${encodeURIComponent(originalTag.key)}/`,
+              query: location.query,
+            },
+          },
+        ]
+      : []),
+    {
+      key: 'view-events',
+      label: t('View other events with this tag value'),
+      hidden: !event.groupID,
+      to: {
+        pathname: `/organizations/${organization.slug}/issues/${event.groupID}/events/`,
+        query,
+      },
+    },
+    {
+      key: 'view-issues',
+      label: t('Search issues with this tag value'),
+      to: {
+        pathname: `/organizations/${organization.slug}/issues/`,
+        query,
+      },
+    },
+  ];
+
+  if (hasNewTraceUi) {
+    items.push({
+      key: 'view-traces',
+      label: t('Search explore with this tag value'),
+      to: getSearchInExploreTarget(
+        organization,
+        location,
+        originalTag.key,
+        originalTag.value,
+        TraceDrawerActionKind.INCLUDE
+      ),
+      onAction: () => {
+        traceAnalytics.trackExploreSearch(
+          organization,
+          originalTag.key,
+          originalTag.value,
+          TraceDrawerActionKind.INCLUDE
+        );
+      },
+    });
+  }
+
+  items.push(
+    {
+      key: 'copy-value',
+      label: t('Copy tag value to clipboard'),
+      onAction: handleCopy,
+    },
+    {
+      key: 'add-to-highlights',
+      label: t('Add to event highlights'),
+      hidden: hideAddHighlightsOption || !isProjectAdmin,
+      onAction: () => {
+        saveTag({
+          highlightTags: [...(project?.highlightTags ?? []), originalTag.key],
+        });
+      },
+    },
+    {
+      key: 'release',
+      label: t('View this release'),
+      hidden: originalTag.key !== 'release',
+      to:
+        originalTag.key === 'release'
+          ? `/organizations/${organization.slug}/releases/${encodeURIComponent(content.value)}/`
+          : undefined,
+    },
+    {
+      key: 'transaction',
+      label: t('View this transaction'),
+      hidden: originalTag.key !== 'transaction',
+      to:
+        originalTag.key === 'transaction'
+          ? {
+              pathname: `${getTransactionSummaryBaseUrl(organization)}/`,
+              query: {
+                project: event.projectID,
+                transaction: content.value,
+                referrer,
+              },
+            }
+          : undefined,
+    },
+    {
+      key: 'replay',
+      label: t('View this replay'),
+      hidden: originalTag.key !== 'replay_id' && originalTag.key !== 'replayId',
+      to:
+        originalTag.key === 'replay_id' || originalTag.key === 'replayId'
+          ? {
+              pathname: `/organizations/${organization.slug}/replays/${encodeURIComponent(content.value)}/`,
+              query: {referrer},
+            }
+          : undefined,
+    },
+    {
+      key: 'external-link',
+      label: t('Visit this external link'),
+      hidden: !isUrl(content.value),
+      onAction: () => {
+        openNavigateToExternalLinkModal({linkText: content.value});
+      },
+    }
+  );
 
   return (
     <TreeValueDropdown
@@ -166,116 +283,7 @@ function EventTagsTreeRowDropdown({
         showChevron: false,
         className: 'tag-button',
       }}
-      items={[
-        ...(isIssueDetailsRoute
-          ? [
-              {
-                key: 'tag-details',
-                label: t('Tag breakdown'),
-                to: {
-                  pathname: `/organizations/${organization.slug}/issues/${event.groupID}/tags/${encodeURIComponent(originalTag.key)}/`,
-                  query: location.query,
-                },
-              },
-            ]
-          : []),
-        {
-          key: 'view-events',
-          label: t('View other events with this tag value'),
-          hidden: !event.groupID,
-          to: {
-            pathname: `/organizations/${organization.slug}/issues/${event.groupID}/events/`,
-            query,
-          },
-        },
-        {
-          key: 'view-issues',
-          label: t('Search issues with this tag value'),
-          to: {
-            pathname: `/organizations/${organization.slug}/issues/`,
-            query,
-          },
-        },
-        {
-          key: 'view-traces',
-          label: t('Search explore with this tag value'),
-          to: getSearchInExploreTarget(
-            organization,
-            location,
-            originalTag.key,
-            originalTag.value,
-            TraceDrawerActionKind.INCLUDE
-          ),
-          onAction: () => {
-            traceAnalytics.trackExploreSearch(
-              organization,
-              originalTag.key,
-              originalTag.value,
-              TraceDrawerActionKind.INCLUDE
-            );
-          },
-        },
-        {
-          key: 'copy-value',
-          label: t('Copy tag value to clipboard'),
-          onAction: handleCopy,
-        },
-        {
-          key: 'add-to-highlights',
-          label: t('Add to event highlights'),
-          hidden: hideAddHighlightsOption || !isProjectAdmin,
-          onAction: () => {
-            saveTag({
-              highlightTags: [...(project?.highlightTags ?? []), originalTag.key],
-            });
-          },
-        },
-        {
-          key: 'release',
-          label: t('View this release'),
-          hidden: originalTag.key !== 'release',
-          to:
-            originalTag.key === 'release'
-              ? `/organizations/${organization.slug}/releases/${encodeURIComponent(content.value)}/`
-              : undefined,
-        },
-        {
-          key: 'transaction',
-          label: t('View this transaction'),
-          hidden: originalTag.key !== 'transaction',
-          to:
-            originalTag.key === 'transaction'
-              ? {
-                  pathname: `${getTransactionSummaryBaseUrl(organization)}/`,
-                  query: {
-                    project: event.projectID,
-                    transaction: content.value,
-                    referrer,
-                  },
-                }
-              : undefined,
-        },
-        {
-          key: 'replay',
-          label: t('View this replay'),
-          hidden: originalTag.key !== 'replay_id' && originalTag.key !== 'replayId',
-          to:
-            originalTag.key === 'replay_id' || originalTag.key === 'replayId'
-              ? {
-                  pathname: `/organizations/${organization.slug}/replays/${encodeURIComponent(content.value)}/`,
-                  query: {referrer},
-                }
-              : undefined,
-        },
-        {
-          key: 'external-link',
-          label: t('Visit this external link'),
-          hidden: !isUrl(content.value),
-          onAction: () => {
-            openNavigateToExternalLinkModal({linkText: content.value});
-          },
-        },
-      ]}
+      items={items}
     />
   );
 }
