3984ce2f03f9714bc6367f2bec5469fffc9c22fc
MichaelSun48 <michael.sun@sentry.io>
2025-01-29 15:48:32 -0800

    Fix tests without cheating

--- static/app/views/issueList/issueViewsHeader.tsx
+++ static/app/views/issueList/issueViewsHeader.tsx
@@ -202,12 +202,10 @@ function IssueViewsIssueListHeaderTabsContent({
   const debounceUpdateViews = useMemo(
     () =>
       debounce((groupSearchViews: UpdateGroupSearchViewPayload[]) => {
-        if (groupSearchViews) {
-          updateViews({
-            orgSlug: organization.slug,
-            groupSearchViews,
-          });
-        }
+        updateViews({
+          orgSlug: organization.slug,
+          groupSearchViews,
+        });
       }, 10000),
     [updateViews, organization.slug]
   );
@@ -215,33 +213,31 @@ function IssueViewsIssueListHeaderTabsContent({
   // This useEffect is here temporarily to start saving user's most recently used page filters
   // to their views. This will be removed once the frontend is updated to use a view's page filters
   useEffect(() => {
-    if (process.env.NODE_ENV !== 'test') {
-      const isAllProjects =
-        pageFilters.selection.projects.length === 1 &&
-        pageFilters.selection.projects[0] === -1;
+    const isAllProjects =
+      pageFilters.selection.projects.length === 1 &&
+      pageFilters.selection.projects[0] === -1;
 
-      const projects = isAllProjects ? [] : pageFilters.selection.projects;
+    const projects = isAllProjects ? [] : pageFilters.selection.projects;
 
-      debounceUpdateViews(
-        views
-          .filter(view => view.isCommitted)
-          .map(view => ({
-            id: view.id,
-            name: view.label,
-            query: view.query,
-            querySort: view.querySort,
-            projects,
-            isAllProjects,
-            environments: pageFilters.selection.environments,
-            timeFilters: pageFilters.selection.datetime,
-          }))
-      );
+    debounceUpdateViews(
+      views
+        .filter(view => view.isCommitted)
+        .map(view => ({
+          id: view.id,
+          name: view.label,
+          query: view.query,
+          querySort: view.querySort,
+          projects,
+          isAllProjects,
+          environments: pageFilters.selection.environments,
+          timeFilters: pageFilters.selection.datetime,
+        }))
+    );
 
-      trackAnalytics('issue_views.page_filters_logged', {
-        user_id: user.id,
-        organization,
-      });
-    }
+    trackAnalytics('issue_views.page_filters_logged', {
+      user_id: user.id,
+      organization,
+    });
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [pageFilters.selection]);
 
