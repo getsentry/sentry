2427f0f50b1c666135c2353a9b911a852d72867a
nikkikapadia <nikki.kapadia@sentry.io>
2025-01-30 16:33:49 -0500

    make large columns smaller with minmax

--- static/app/views/dashboards/widgetBuilder/components/widgetPreview.tsx
+++ static/app/views/dashboards/widgetBuilder/components/widgetPreview.tsx
@@ -24,6 +24,8 @@ interface WidgetPreviewProps {
   shouldForceDescriptionTooltip?: boolean;
 }
 
+const MIN_TABLE_COLUMN_WIDTH = '125px';
+
 function WidgetPreview({
   dashboard,
   dashboardFilters,
@@ -88,6 +90,8 @@ function WidgetPreview({
       // onWidgetSplitDecision={onWidgetSplitDecision}
 
       showConfidenceWarning={widget.widgetType === WidgetType.SPANS}
+      // ensure table columns are at least a certain width (helps with lack of truncation on large fields)
+      minTableColumnWidth={MIN_TABLE_COLUMN_WIDTH}
     />
   );
 }
