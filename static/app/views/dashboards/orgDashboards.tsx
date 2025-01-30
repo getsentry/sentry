e3069cab577b777cdd8f955c0d3ee531342d1afa
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-01-30 15:39:41 -0800

    ref(tsc): convert orgDashboards to FC

--- static/app/views/dashboards/orgDashboards.tsx
+++ static/app/views/dashboards/orgDashboards.tsx
@@ -1,18 +1,19 @@
-import type {Location} from 'history';
-import isEqual from 'lodash/isEqual';
+import {useState} from 'react';
 
-import type {Client} from 'sentry/api';
-import DeprecatedAsyncComponent from 'sentry/components/deprecatedAsyncComponent';
 import NotFound from 'sentry/components/errors/notFound';
 import * as Layout from 'sentry/components/layouts/thirds';
+import LoadingError from 'sentry/components/loadingError';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {t} from 'sentry/locale';
-import type {Organization} from 'sentry/types/organization';
-import {browserHistory} from 'sentry/utils/browserHistory';
+import {useApiQuery} from 'sentry/utils/queryClient';
 import type {WithRouteAnalyticsProps} from 'sentry/utils/routeAnalytics/withRouteAnalytics';
 import withRouteAnalytics from 'sentry/utils/routeAnalytics/withRouteAnalytics';
 import normalizeUrl from 'sentry/utils/url/normalizeUrl';
+import {useLocation} from 'sentry/utils/useLocation';
+import {useNavigate} from 'sentry/utils/useNavigate';
+import useOrganization from 'sentry/utils/useOrganization';
+import {useParams} from 'sentry/utils/useParams';
 
 import {assignTempId} from './layoutUtils';
 import type {DashboardDetails, DashboardListItem} from './types';
@@ -26,116 +27,46 @@ type OrgDashboardsChildrenProps = {
 };
 
 type Props = WithRouteAnalyticsProps & {
-  api: Client;
   children: (props: OrgDashboardsChildrenProps) => React.ReactNode;
-  location: Location;
-  organization: Organization;
-  params: {orgId: string; dashboardId?: string};
 };
 
-type State = {
-  // endpoint response
-  dashboards: DashboardListItem[] | null;
-  /**
-   * The currently selected dashboard.
-   */
-  selectedDashboard: DashboardDetails | null;
-} & DeprecatedAsyncComponent['state'];
+function OrgDashboards(props: Props) {
+  const {children} = props;
+  const location = useLocation();
+  const organization = useOrganization();
+  const navigate = useNavigate();
+  const {dashboardId} = useParams<{dashboardId: string}>();
 
-class OrgDashboards extends DeprecatedAsyncComponent<Props, State> {
-  state: State = {
-    // AsyncComponent state
-    loading: true,
-    reloading: false,
-    error: false,
-    errors: {},
+  const ENDPOINT = `/organizations/${organization.slug}/dashboards/`;
 
-    dashboards: [],
-    selectedDashboard: null,
-  };
+  // The currently selected dashboard
+  const [selectedDashboardState, setSelectedDashboardState] =
+    useState<DashboardDetails | null>(null);
 
-  componentDidUpdate(prevProps: Props) {
-    if (!isEqual(prevProps.params.dashboardId, this.props.params.dashboardId)) {
-      this.remountComponent();
-    }
-  }
+  // componentDidUpdate(prevProps: Props) {
+  //   if (!isEqual(prevProps.params.dashboardId, this.props.params.dashboardId)) {
+  //     this.remountComponent();
+  //   }
+  // }
 
-  getEndpoints(): ReturnType<DeprecatedAsyncComponent['getEndpoints']> {
-    const {organization, params} = this.props;
-    const url = `/organizations/${organization.slug}/dashboards/`;
-    const endpoints: ReturnType<DeprecatedAsyncComponent['getEndpoints']> = [
-      ['dashboards', url],
-    ];
+  const {
+    data: dashboards,
+    isPending: isDashboardsPending,
+    isError: isDashboardsError,
+    error: dashboardsError,
+  } = useApiQuery<DashboardListItem[]>([ENDPOINT], {staleTime: 0});
 
-    if (params.dashboardId) {
-      endpoints.push(['selectedDashboard', `${url}${params.dashboardId}/`]);
-    }
+  const {
+    data: fetchedSelectedDashboard,
+    isPending: isSelectedDashboardPending,
+    isError: isSelectedDashboardError,
+    error: selectedDashboardError,
+  } = useApiQuery<DashboardDetails>([`${ENDPOINT}${dashboardId}/`], {
+    staleTime: 0,
+    enabled: !!dashboardId,
+  });
 
-    return endpoints;
-  }
-
-  onDashboardUpdate(updatedDashboard: DashboardDetails) {
-    this.setState({selectedDashboard: updatedDashboard});
-  }
-
-  getDashboards(): DashboardListItem[] {
-    const {dashboards} = this.state;
-
-    return Array.isArray(dashboards) ? dashboards : [];
-  }
-
-  onRequestSuccess({stateKey, data}: any) {
-    const {params, organization, location} = this.props;
-
-    if (params.dashboardId || stateKey === 'selectedDashboard') {
-      const queryParamFilters = new Set([
-        'project',
-        'environment',
-        'statsPeriod',
-        'start',
-        'end',
-        'utc',
-        'release',
-      ]);
-      if (
-        stateKey === 'selectedDashboard' &&
-        // Only redirect if there are saved filters and none of the filters
-        // appear in the query params
-        hasSavedPageFilters(data) &&
-        Object.keys(location.query).filter(unsavedQueryParam =>
-          queryParamFilters.has(unsavedQueryParam)
-        ).length === 0
-      ) {
-        browserHistory.replace({
-          ...location,
-          query: {
-            ...location.query,
-            project: data.projects,
-            environment: data.environment,
-            statsPeriod: data.period,
-            start: data.start,
-            end: data.end,
-            utc: data.utc,
-          },
-        });
-      }
-      return;
-    }
-
-    // If we don't have a selected dashboard, and one isn't going to arrive
-    // we can redirect to the first dashboard in the list.
-    const dashboardId = data.length ? data[0].id : 'default-overview';
-    browserHistory.replace(
-      normalizeUrl({
-        pathname: `/organizations/${organization.slug}/dashboard/${dashboardId}/`,
-        query: {
-          ...location.query,
-        },
-      })
-    );
-  }
-
-  renderLoading() {
+  if (isDashboardsPending || isSelectedDashboardPending) {
     return (
       <Layout.Page withPadding>
         <LoadingIndicator />
@@ -143,16 +74,83 @@ class OrgDashboards extends DeprecatedAsyncComponent<Props, State> {
     );
   }
 
-  renderBody() {
-    const {children} = this.props;
-    const {selectedDashboard, error} = this.state;
-    let dashboard = selectedDashboard;
+  if (isDashboardsError || isSelectedDashboardError) {
+    const notFound =
+      dashboardsError?.status === 404 || selectedDashboardError?.status === 404;
 
+    if (notFound) {
+      return <NotFound />;
+    }
+
+    return <LoadingError />;
+  }
+
+  const selectedDashboard = selectedDashboardState ?? fetchedSelectedDashboard;
+
+  const onDashboardUpdate = (updatedDashboard: DashboardDetails) => {
+    setSelectedDashboardState(updatedDashboard);
+  };
+
+  const getDashboards = (): DashboardListItem[] => {
+    return Array.isArray(dashboards) ? dashboards : [];
+  };
+
+  if (dashboardId || fetchedSelectedDashboard) {
+    const queryParamFilters = new Set([
+      'project',
+      'environment',
+      'statsPeriod',
+      'start',
+      'end',
+      'utc',
+      'release',
+    ]);
+    if (
+      fetchedSelectedDashboard &&
+      // Only redirect if there are saved filters and none of the filters
+      // appear in the query params
+      hasSavedPageFilters(fetchedSelectedDashboard) &&
+      Object.keys(location.query).filter(unsavedQueryParam =>
+        queryParamFilters.has(unsavedQueryParam)
+      ).length === 0
+    ) {
+      navigate(
+        {
+          ...location,
+          query: {
+            ...location.query,
+            project: fetchedSelectedDashboard.projects,
+            environment: fetchedSelectedDashboard.environment,
+            statsPeriod: fetchedSelectedDashboard.period,
+            start: fetchedSelectedDashboard.start,
+            end: fetchedSelectedDashboard.end,
+            utc: fetchedSelectedDashboard.utc,
+          },
+        },
+        {replace: true}
+      );
+    }
+  }
+
+  // If we don't have a selected dashboard, and one isn't going to arrive
+  // we can redirect to the first dashboard in the list.
+  const firstDashboardId = dashboards.length ? dashboards[0]?.id : 'default-overview';
+  navigate(
+    normalizeUrl({
+      pathname: `/organizations/${organization.slug}/dashboard/${firstDashboardId}/`,
+      query: {
+        ...location.query,
+      },
+    }),
+    {replace: true}
+  );
+
+  const renderContent = () => {
     // Ensure there are always tempIds for grid layout
     // This is needed because there are cases where the dashboard
     // renders before the onRequestSuccess setState is processed
     // and will caused stacked widgets because of missing tempIds
-    dashboard = selectedDashboard
+    const dashboard = selectedDashboard
       ? {
           ...selectedDashboard,
           widgets: selectedDashboard.widgets.map(assignTempId),
@@ -160,61 +158,49 @@ class OrgDashboards extends DeprecatedAsyncComponent<Props, State> {
       : null;
 
     return children({
-      error,
+      error: Boolean(dashboardsError || selectedDashboardError),
       dashboard,
-      dashboards: this.getDashboards(),
+      dashboards: getDashboards(),
       onDashboardUpdate: (updatedDashboard: DashboardDetails) =>
-        this.onDashboardUpdate(updatedDashboard),
+        onDashboardUpdate(updatedDashboard),
     });
-  }
+  };
 
-  renderError(error: Error) {
-    const notFound = Object.values(this.state.errors).find(
-      resp => resp && resp.status === 404
+  if (!organization.features.includes('dashboards-basic')) {
+    // Redirect to Dashboards v1
+    navigate(
+      normalizeUrl({
+        pathname: `/organizations/${organization.slug}/dashboards/`,
+        query: {
+          ...location.query,
+        },
+      }),
+      {replace: true}
     );
-
-    if (notFound) {
-      return <NotFound />;
-    }
-
-    return super.renderError(error, true);
+    return null;
   }
 
-  renderComponent() {
-    const {organization, location} = this.props;
-    const {loading, selectedDashboard} = this.state;
-
-    if (!organization.features.includes('dashboards-basic')) {
-      // Redirect to Dashboards v1
-      browserHistory.replace(
-        normalizeUrl({
-          pathname: `/organizations/${organization.slug}/dashboards/`,
-          query: {
-            ...location.query,
-          },
-        })
-      );
-      return null;
-    }
-
-    if (
-      loading &&
-      selectedDashboard &&
-      hasSavedPageFilters(selectedDashboard) &&
-      Object.keys(location.query).length === 0
-    ) {
-      // Block dashboard from rendering if the dashboard has filters and
-      // the URL does not contain filters yet. The filters can either match the
-      // saved filters, or can be different (i.e. sharing an unsaved state)
-      return this.renderLoading();
-    }
-
+  if (
+    (isDashboardsPending || isSelectedDashboardPending) &&
+    selectedDashboard &&
+    hasSavedPageFilters(selectedDashboard) &&
+    Object.keys(location.query).length === 0
+  ) {
+    // Block dashboard from rendering if the dashboard has filters and
+    // the URL does not contain filters yet. The filters can either match the
+    // saved filters, or can be different (i.e. sharing an unsaved state)
     return (
-      <SentryDocumentTitle title={t('Dashboards')} orgSlug={organization.slug}>
-        {super.renderComponent() as React.ReactChild}
-      </SentryDocumentTitle>
+      <Layout.Page withPadding>
+        <LoadingIndicator />
+      </Layout.Page>
     );
   }
+
+  return (
+    <SentryDocumentTitle title={t('Dashboards')} orgSlug={organization.slug}>
+      {renderContent()}
+    </SentryDocumentTitle>
+  );
 }
 
 export default withRouteAnalytics(OrgDashboards);
