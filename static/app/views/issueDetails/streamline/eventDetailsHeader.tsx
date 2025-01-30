73cac722ea447624c1a98b87f36456d2600875df
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-01-30 12:30:14 -0500

    ♻️ refactor for more graph and bar options

--- static/app/views/issueDetails/streamline/eventDetailsHeader.tsx
+++ static/app/views/issueDetails/streamline/eventDetailsHeader.tsx
@@ -1,5 +1,8 @@
+import {Fragment} from 'react';
+import {css, useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
 
+import {Button} from 'sentry/components/button';
 import {Flex} from 'sentry/components/container/flex';
 import ErrorBoundary from 'sentry/components/errorBoundary';
 import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
@@ -18,28 +21,22 @@ import {
   useEventQuery,
 } from 'sentry/views/issueDetails/streamline/eventSearch';
 import IssueTagsPreview from 'sentry/views/issueDetails/streamline/issueTagsPreview';
+import {OccurrenceSummary} from 'sentry/views/issueDetails/streamline/occurrenceSummary';
 import {ToggleSidebar} from 'sentry/views/issueDetails/streamline/sidebar/toggleSidebar';
-import {TimelineSummary} from 'sentry/views/issueDetails/streamline/timelineSummary';
 import {useEnvironmentsFromUrl} from 'sentry/views/issueDetails/utils';
 
-export function EventDetailsHeader({
-  group,
-  event,
-  project,
-}: {
-  event: Event | undefined;
+interface EventDetailsHeaderProps {
   group: Group;
   project: Project;
-}) {
+  event?: Event;
+}
+
+export function EventDetailsHeader({group, event, project}: EventDetailsHeaderProps) {
   const navigate = useNavigate();
   const location = useLocation();
   const environments = useEnvironmentsFromUrl();
   const searchQuery = useEventQuery({groupId: group.id});
   const issueTypeConfig = getConfigForIssueType(group, project);
-
-  if (!issueTypeConfig.header.filterAndSearch.enabled) {
-    return null;
-  }
 
   const searchText = t(
     'Filter %s\u2026',
@@ -49,52 +46,99 @@ export function EventDetailsHeader({
   return (
     <PageErrorBoundary mini message={t('There was an error loading the event filters')}>
       <FilterContainer role="group" aria-description={t('Event filtering controls')}>
-        <EnvironmentFilter
-          triggerProps={{
-            borderless: true,
-            style: {
-              borderRadius: 0,
-            },
-          }}
-        />
-        <DateFilter
-          triggerProps={{
-            borderless: true,
-            style: {
-              borderRadius: 0,
-            },
-          }}
-        />
-        <Flex style={{gridArea: 'search'}}>
-          <SearchFilter
-            group={group}
-            handleSearch={query => {
-              navigate({...location, query: {...location.query, query}}, {replace: true});
-            }}
-            environments={environments}
-            query={searchQuery}
-            queryBuilderProps={{
-              disallowFreeText: true,
-              placeholder: searchText,
-              label: searchText,
-            }}
-          />
-          <ToggleSidebar />
-        </Flex>
+        {issueTypeConfig.header.filterBar.enabled && (
+          <Fragment>
+            <EnvironmentSelector group={group} event={event} project={project} />
+            <DateFilter
+              triggerProps={{
+                borderless: true,
+                style: {
+                  borderRadius: 0,
+                },
+              }}
+            />
+            <Flex style={{gridArea: 'search'}}>
+              <SearchFilter
+                group={group}
+                handleSearch={query => {
+                  navigate(
+                    {...location, query: {...location.query, query}},
+                    {replace: true}
+                  );
+                }}
+                environments={environments}
+                query={searchQuery}
+                queryBuilderProps={{
+                  disallowFreeText: true,
+                  placeholder: searchText,
+                  label: searchText,
+                }}
+              />
+              <ToggleSidebar />
+            </Flex>
+          </Fragment>
+        )}
         <GraphSection>
-          <EventGraph event={event} group={group} style={{flex: 1}} />
-          <SectionDivider />
-          <IssueTagsPreview
-            groupId={group.id}
-            environments={environments}
-            project={project}
-          />
+          {issueTypeConfig.header.graph.enabled && (
+            <EventGraph event={event} group={group} style={{flex: 1}} />
+          )}
+          {issueTypeConfig.header.tagDistribution.enabled && (
+            <Fragment>
+              <SectionDivider />
+              <IssueTagsPreview
+                groupId={group.id}
+                environments={environments}
+                project={project}
+              />
+            </Fragment>
+          )}
         </GraphSection>
-        {issueTypeConfig.header.timelineSummary.enabled && (
-          <TimelineSection group={group} />
+        {issueTypeConfig.header.occurrenceSummary.enabled && (
+          <OccurrenceSummarySection group={group} />
         )}
       </FilterContainer>
     </PageErrorBoundary>
+  );
+}
+
+function EnvironmentSelector({group, event, project}: EventDetailsHeaderProps) {
+  const theme = useTheme();
+  const issueTypeConfig = getConfigForIssueType(group, project);
+  const isFixedEnvironment = issueTypeConfig.header.filterBar.fixedEnvironment;
+  const eventEnvironment = event?.tags?.find(tag => tag.key === 'environment')?.value;
+
+  const environmentCss = css`
+    grid-area: env;
+    &:before {
+      right: 0;
+      top: ${space(1)};
+      bottom: ${space(1)};
+      width: 1px;
+      content: '';
+      position: absolute;
+      background: ${theme.translucentInnerBorder};
+    }
+  `;
+
+  return isFixedEnvironment ? (
+    <Button
+      disabled
+      borderless
+      title={t('This issue only occurs in a single environment')}
+      css={environmentCss}
+    >
+      {eventEnvironment}
+    </Button>
+  ) : (
+    <EnvironmentPageFilter
+      css={environmentCss}
+      triggerProps={{
+        borderless: true,
+        style: {
+          borderRadius: 0,
+        },
+      }}
+    />
   );
 }
 
@@ -109,19 +153,6 @@ const FilterContainer = styled('div')`
     'timeline timeline  timeline  timeline';
   border: 0px solid ${p => p.theme.translucentBorder};
   border-width: 0 1px 1px 0;
-`;
-
-const EnvironmentFilter = styled(EnvironmentPageFilter)`
-  grid-area: env;
-  &:before {
-    right: 0;
-    top: ${space(1)};
-    bottom: ${space(1)};
-    width: 1px;
-    content: '';
-    position: absolute;
-    background: ${p => p.theme.translucentInnerBorder};
-  }
 `;
 
 const SearchFilter = styled(EventSearch)`
@@ -149,7 +180,7 @@ const GraphSection = styled('div')`
   border-top: 1px solid ${p => p.theme.translucentBorder};
 `;
 
-const TimelineSection = styled(TimelineSummary)`
+const OccurrenceSummarySection = styled(OccurrenceSummary)`
   grid-area: timeline;
   padding: ${space(2)};
   padding-right: 0;
