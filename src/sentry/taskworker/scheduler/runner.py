56839021e8e58666d5da2a5734892512eee4c5e2
Mark Story <mark@mark-story.com>
2025-01-31 17:03:20 -0500

    feat(taskworker) Add scheduler run command and reorganize (#84370)
    Break up the large scheduler module into two smaller modules. This was
    partly done to address feedback about the number of classes in the
    scheduler module and to also fix import loops for the `crontab` config
    struct.
    
    Refs https://github.com/getsentry/sentry/issues/81458

--- src/sentry/taskworker/scheduler/runner.py
+++ src/sentry/taskworker/scheduler/runner.py
@@ -1,41 +1,29 @@
 from __future__ import annotations
 
-import abc
-import dataclasses
 import heapq
 import logging
 from collections.abc import Mapping
 from datetime import datetime, timedelta
-from typing import Any, TypedDict
+from typing import Any
 
-from cronsim import CronSim, CronSimError
 from django.utils import timezone
 from redis.client import StrictRedis
 from rediscluster import RedisCluster
 from sentry_sdk import capture_exception
 
+from sentry.conf.types.taskworker import ScheduleConfig, crontab
 from sentry.taskworker.registry import TaskRegistry
+from sentry.taskworker.scheduler.schedules import CrontabSchedule, Schedule, TimedeltaSchedule
 from sentry.taskworker.task import Task
 from sentry.utils import metrics
 
-logger = logging.getLogger(__name__)
-
-
-class ScheduleConfig(TypedDict):
-    """The schedule definition for an individual task."""
-
-    task: str
-    schedule: timedelta | crontab
-
-
-ScheduleConfigMap = Mapping[str, ScheduleConfig]
-"""A collection of schedule configuration, usually defined in application configuration"""
+logger = logging.getLogger("taskworker.scheduler")
 
 
 class RunStorage:
     """
     Storage interface for tracking the last run time of tasks.
-    This is split out from `ScheduleSet` to allow us to change storage
+    This is split out from `ScheduleRunner` to allow us to change storage
     in the future, or adapt taskworkers for other applications should we need to.
     """
 
@@ -85,177 +73,8 @@ class RunStorage:
         self._redis.delete(self._make_key(taskname))
 
 
-class Schedule(metaclass=abc.ABCMeta):
-    """Interface for scheduling tasks to run at specific times."""
-
-    @abc.abstractmethod
-    def is_due(self, last_run: datetime | None = None) -> bool:
-        """
-        Check if the schedule is due to run again based on last_run.
-        """
-
-    @abc.abstractmethod
-    def remaining_seconds(self, last_run: datetime | None = None) -> int:
-        """
-        Get the remaining seconds until the schedule should run again.
-        """
-
-    @abc.abstractmethod
-    def runtime_after(self, start: datetime) -> datetime:
-        """
-        Get the next scheduled time after `start`
-        """
-
-
-class TimedeltaSchedule(Schedule):
-    """Task schedules defined as `datetime.timedelta` intervals"""
-
-    def __init__(self, delta: timedelta) -> None:
-        self._delta = delta
-        if delta.microseconds:
-            raise ValueError("microseconds are not supported")
-        if delta.total_seconds() < 0:
-            raise ValueError("interval must be at least one second")
-
-    def is_due(self, last_run: datetime | None = None) -> bool:
-        """Check if the schedule is due to run again based on last_run."""
-        if last_run is None:
-            return True
-        remaining = self.remaining_seconds(last_run)
-        return remaining <= 0
-
-    def remaining_seconds(self, last_run: datetime | None = None) -> int:
-        """The number of seconds remaining until the next task should spawn"""
-        if last_run is None:
-            return 0
-        # floor to timestamp as microseconds are not relevant
-        now = int(timezone.now().timestamp())
-        last_run_ts = int(last_run.timestamp())
-
-        seconds_remaining = self._delta.total_seconds() - (now - last_run_ts)
-        return max(int(seconds_remaining), 0)
-
-    def runtime_after(self, start: datetime) -> datetime:
-        """Get the next time a task should run after start"""
-        return start + self._delta
-
-
-@dataclasses.dataclass
-class crontab:
-    """
-    crontab schedule value object
-
-    Used in configuration to define a task schedule.
-    """
-
-    minute: str = "*"
-    hour: str = "*"
-    day_of_week: str = "*"
-    day_of_month: str = "*"
-    month_of_year: str = "*"
-
-    def __str__(self) -> str:
-        return (
-            f"{self.minute} {self.hour} {self.day_of_month} {self.month_of_year} {self.day_of_week}"
-        )
-
-
-class CrontabSchedule(Schedule):
-    """
-    Task schedules defined as crontab expressions.
-
-    Last run state is used to determine the next scheduled time.
-
-    If last run is in the future, the future value will be used to
-    calculate the next scheduled time.
-
-    If runs are missed, the schedule will align to the next interval
-    that would be scheduled. Lost runs are not recovered.
-    """
-
-    def __init__(self, name: str, crontab: crontab) -> None:
-        self._crontab = crontab
-        self._name = name
-        try:
-            self._cronsim = CronSim(str(crontab), timezone.now())
-        except CronSimError as e:
-            raise ValueError(f"crontab expression {self._crontab} is invalid") from e
-
-    def is_due(self, last_run: datetime | None = None) -> bool:
-        """Check if the schedule is due to run again based on last_run."""
-        if last_run is None:
-            return True
-        remaining = self.remaining_seconds(last_run)
-        return remaining <= 0
-
-    def remaining_seconds(self, last_run: datetime | None = None) -> int:
-        """
-        Get the number of seconds until this schedule is due again
-
-        Use the current time to find the next schedule time
-        """
-        if last_run is None:
-            return 0
-
-        # This could result in missed beats, or increased load on redis.
-        last_run = last_run.replace(second=0, microsecond=0)
-        now = timezone.now().replace(second=0, microsecond=0)
-
-        # A future last_run means we should wait until the
-        # next scheduled time, and then we can try again.
-        # we could be competing with another scheduler, or
-        # missing beats.
-        if last_run > now:
-            logger.warning(
-                "taskworker.scheduler.future_value",
-                extra={
-                    "task": self._name,
-                    "last_run": last_run,
-                    "now": now,
-                },
-            )
-            next_run = self._advance(last_run + timedelta(minutes=1))
-            return int(next_run.timestamp() - now.timestamp())
-
-        # If last run is in the past, see if the next runtime
-        # is in the future.
-        if last_run < now:
-            next_run = self._advance(last_run)
-            # Our next runtime is in the future, or now
-            if next_run >= now:
-                return int(next_run.timestamp() - now.timestamp())
-
-            # still in the past, we missed an interval :(
-            next_run = self._advance(now)
-            logger.warning(
-                "taskworker.scheduler.missed_interval",
-                extra={
-                    "task": self._name,
-                    "last_run": last_run,
-                    "now": now,
-                    "next_run": next_run,
-                },
-            )
-            return int(next_run.timestamp() - now.timestamp())
-
-        # last_run == now, we are on the beat, find the next interval
-        next_run = self._advance(now + timedelta(minutes=1))
-
-        return int(next_run.timestamp() - now.timestamp())
-
-    def _advance(self, dt: datetime) -> datetime:
-        self._cronsim.dt = dt
-        self._cronsim.advance()
-        return self._cronsim.dt
-
-    def runtime_after(self, start: datetime) -> datetime:
-        """Get the next time a task should be spawned after `start`"""
-        start = start.replace(second=0, microsecond=0) + timedelta(minutes=1)
-        return self._advance(start)
-
-
 class ScheduleEntry:
-    """Metadata about a task that can be scheduled"""
+    """An individual task that can be scheduled to be run."""
 
     def __init__(self, *, task: Task[Any, Any], schedule: timedelta | crontab) -> None:
         self._task = task
@@ -292,13 +111,13 @@ class ScheduleEntry:
         self._task.delay()
 
 
-class ScheduleSet:
+class ScheduleRunner:
     """
     A task scheduler that a command run process can use to spawn tasks
     based on their schedules.
 
     Contains a collection of ScheduleEntry objects which are composed
-    using `ScheduleSet.add()`. Once the scheduler is built, `tick()`
+    using `ScheduleRunner.add()`. Once the scheduler is built, `tick()`
     is used in a while loop to spawn tasks and sleep.
     """
 
@@ -309,7 +128,7 @@ class ScheduleSet:
         self._heap: list[tuple[int, ScheduleEntry]] = []
 
     def add(self, task_config: ScheduleConfig) -> None:
-        """Add a task to the scheduleset."""
+        """Add a task to the runner."""
         try:
             (namespace, taskname) = task_config["task"].split(":")
         except ValueError:
