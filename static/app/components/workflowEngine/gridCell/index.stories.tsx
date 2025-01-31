e241b916c6d2b0609e1e7607cadd32e39f2db8d3
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-01-31 09:58:44 -0800

    feat(aci): grid cell components for monitors (#84281)
    # `TitleCell`
    refactored + renamed `AutomationTitleCell` so that it can be used for
    monitors and automations
    
    <img width="502" alt="Screenshot 2025-01-29 at 3 33 04 PM"
    src="https://github.com/user-attachments/assets/42dd80c5-6047-45cd-abe3-c1b48bd1856a"
    />
    
    # `ConnectionCell`
    refactored + renamed `MonitorsCell` so that it can be used for connected
    monitors and automations
    
    <img width="278" alt="Screenshot 2025-01-29 at 3 32 49 PM"
    src="https://github.com/user-attachments/assets/e2146d8c-283f-4398-a944-fd06fb70aa37"
    />
    
    # `IssueCell`
    
    
    https://github.com/user-attachments/assets/a17dabb0-e60f-4fe7-992e-c36bd16fb5b7
    
    # `NumberCell`
    <img width="170" alt="Screenshot 2025-01-29 at 3 15 00 PM"
    src="https://github.com/user-attachments/assets/4fa44018-f2e6-402b-99af-aff4613138b5"
    />
    
    ---------
    
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

--- static/app/components/workflowEngine/gridCell/index.stories.tsx
+++ static/app/components/workflowEngine/gridCell/index.stories.tsx
@@ -7,103 +7,130 @@ import {
   ActionCell,
 } from 'sentry/components/workflowEngine/gridCell/actionCell';
 import {
-  AutomationTitleCell,
-  type AutomationTitleCellProps,
-} from 'sentry/components/workflowEngine/gridCell/automationTitleCell';
-import type {Monitor} from 'sentry/components/workflowEngine/gridCell/monitorsCell';
-import {MonitorsCell} from 'sentry/components/workflowEngine/gridCell/monitorsCell';
+  ConnectionCell,
+  type ConnectionCellProps,
+} from 'sentry/components/workflowEngine/gridCell/connectionCell';
+import {NumberCell} from 'sentry/components/workflowEngine/gridCell/numberCell';
 import {TimeAgoCell} from 'sentry/components/workflowEngine/gridCell/timeAgoCell';
+import {
+  TitleCell,
+  type TitleCellProps,
+} from 'sentry/components/workflowEngine/gridCell/titleCell';
+import {tn} from 'sentry/locale';
 import storyBook from 'sentry/stories/storyBook';
 
 type ExampleAutomation = {
   action: Action[];
-  automation: AutomationTitleCellProps;
-  monitors: Monitor[];
+  linkedItems: ConnectionCellProps;
+  openIssues: number;
   timeAgo: Date | null;
+  title: TitleCellProps;
 };
 
 export default storyBook('Grid Cell Components', story => {
   const data: ExampleAutomation[] = [
     {
-      automation: {
-        id: '1',
+      title: {
         name: 'Slack suggested assignees',
         project: {slug: 'sentry', platform: 'python'},
+        link: '/monitors/1',
       },
       action: ['slack'],
       timeAgo: new Date(),
-      monitors: [
-        {
-          name: 'my monitor',
-          id: 'abc123',
-          project: {slug: 'ngrok-luver', platform: 'ruby'},
-        },
-      ],
+      linkedItems: {
+        items: [
+          {
+            name: 'my monitor',
+            project: {slug: 'ngrok-luver', platform: 'ruby'},
+            link: 'monitors/abc123',
+          },
+        ],
+        renderText: count => tn('%s monitor', '%s monitors', count),
+      },
+      openIssues: 3,
     },
     {
-      automation: {
-        id: '2',
+      title: {
         name: 'Send Discord notification',
-        project: {slug: 'javascript', platform: 'javascript'},
+        project: {
+          slug: 'javascript',
+          platform: 'javascript',
+        },
+        details: ['transaction.duration', '2s warn, 2.5s critical threshold'],
+        link: '/monitors/2',
       },
       action: ['discord'],
       timeAgo: new Date(Date.now() - 2 * 60 * 60 * 1000),
-      monitors: [
-        {
-          name: '/endpoint',
-          id: 'def456',
-          project: {slug: 'javascript', platform: 'javascript'},
-          description: 'transaction.duration',
-        },
-        {
-          name: '/checkout',
-          id: 'ghi789',
-          project: {slug: 'javascript', platform: 'javascript'},
-          description: 'transaction.duration',
-        },
-      ],
+      linkedItems: {
+        items: [
+          {
+            name: '/endpoint',
+            project: {slug: 'javascript', platform: 'javascript'},
+            description: 'transaction.duration',
+            link: 'monitors/def456',
+          },
+          {
+            name: '/checkout',
+            project: {slug: 'javascript', platform: 'javascript'},
+            description: 'transaction.duration',
+            link: 'monitors/ghi789',
+          },
+        ],
+        renderText: count => tn('%s monitor', '%s monitors', count),
+      },
+      openIssues: 1,
     },
     {
-      automation: {
-        id: '3',
+      title: {
         name: 'Email suggested assignees',
         project: {slug: 'javascript', platform: 'javascript'},
+        details: ['Every hour'],
+        link: '/monitors/3',
       },
       action: ['email'],
       timeAgo: new Date(Date.now() - 25 * 60 * 60 * 1000),
-      monitors: [
-        {
-          name: 'test monitor',
-          id: 'jkl012',
-          project: {slug: 'bruh', platform: 'android'},
-          description: 'transaction.duration',
-        },
-        {
-          name: 'test python monitor',
-          id: 'mno345',
-          project: {slug: 'bruh.py', platform: 'python'},
-        },
-        {
-          name: 'test swift monitor',
-          id: 'pqr678',
-          project: {slug: 'bruh.swift', platform: 'swift'},
-        },
-      ],
+      linkedItems: {
+        items: [
+          {
+            name: 'test automation',
+            project: {slug: 'bruh', platform: 'android'},
+            description: 'transaction.duration',
+            link: 'automations/jkl012',
+          },
+          {
+            name: 'test python automation',
+            project: {slug: 'bruh.py', platform: 'python'},
+            link: 'automations/mno345',
+          },
+          {
+            name: 'test swift automation',
+            project: {slug: 'bruh.swift', platform: 'swift'},
+            link: 'automations/pqr678',
+          },
+        ],
+        renderText: count => tn('%s automation', '%s automations', count),
+      },
+      openIssues: 0,
     },
     {
-      automation: {
-        id: '4',
+      title: {
         name: 'Send notification',
         project: {slug: 'android', platform: 'android'},
+        link: '/monitors/4',
+        disabled: true,
       },
       action: ['slack', 'discord', 'email'],
       timeAgo: null,
-      monitors: [],
+      linkedItems: {
+        items: [],
+        renderText: count => tn('%s automation', '%s automations', count),
+      },
+      openIssues: 0,
     },
   ];
 
-  const automationTitleTable: Array<GridColumnOrder<keyof ExampleAutomation>> = [
-    {key: 'automation', name: 'Name', width: 200},
+  const TitleTable: Array<GridColumnOrder<keyof ExampleAutomation>> = [
+    {key: 'title', name: 'Name', width: 200},
   ];
 
   const actionTable: Array<GridColumnOrder<keyof ExampleAutomation>> = [
@@ -114,8 +141,12 @@ export default storyBook('Grid Cell Components', story => {
     {key: 'timeAgo', name: 'Last Triggered', width: 200},
   ];
 
-  const monitorsTable: Array<GridColumnOrder<keyof ExampleAutomation>> = [
-    {key: 'monitors', name: 'Connected Monitors', width: 200},
+  const linkedGroupsTable: Array<GridColumnOrder<keyof ExampleAutomation>> = [
+    {key: 'linkedItems', name: 'Connected Monitors/Automations', width: 200},
+  ];
+
+  const openIssuesTable: Array<GridColumnOrder<keyof ExampleAutomation>> = [
+    {key: 'openIssues', name: 'Open Issues', width: 200},
   ];
 
   const renderHeadCell = (column: GridColumnOrder) => column.name;
@@ -125,30 +156,39 @@ export default storyBook('Grid Cell Components', story => {
     dataRow: ExampleAutomation
   ) => {
     switch (column.key) {
-      case 'automation':
+      case 'title':
         return (
-          <AutomationTitleCell
-            id={dataRow.automation.id}
-            name={dataRow.automation.name}
-            project={dataRow.automation.project}
+          <TitleCell
+            link={dataRow.title.link}
+            name={dataRow.title.name}
+            project={dataRow.title.project}
+            details={dataRow.title.details}
+            disabled={dataRow.title.disabled}
           />
         );
       case 'action':
         return <ActionCell actions={dataRow.action} />;
       case 'timeAgo':
         return <TimeAgoCell date={dataRow.timeAgo ?? undefined} />;
-      case 'monitors':
-        return <MonitorsCell monitors={dataRow.monitors} />;
+      case 'linkedItems':
+        return (
+          <ConnectionCell
+            items={dataRow.linkedItems.items}
+            renderText={dataRow.linkedItems.renderText}
+          />
+        );
+      case 'openIssues':
+        return <NumberCell number={dataRow.openIssues} />;
       default:
         return null;
     }
   };
 
-  story('AutomationTitleCell', () => (
+  story('TitleCell', () => (
     <Fragment>
       <GridEditable
         data={data}
-        columnOrder={automationTitleTable}
+        columnOrder={TitleTable}
         columnSortBy={[]}
         grid={{
           renderHeadCell,
@@ -186,11 +226,25 @@ export default storyBook('Grid Cell Components', story => {
     </Fragment>
   ));
 
-  story('MonitorsCell', () => (
+  story('ConnectionCell', () => (
     <Fragment>
       <GridEditable
         data={data}
-        columnOrder={monitorsTable}
+        columnOrder={linkedGroupsTable}
+        columnSortBy={[]}
+        grid={{
+          renderHeadCell,
+          renderBodyCell,
+        }}
+      />
+    </Fragment>
+  ));
+
+  story('NumberCell', () => (
+    <Fragment>
+      <GridEditable
+        data={data}
+        columnOrder={openIssuesTable}
         columnSortBy={[]}
         grid={{
           renderHeadCell,
