d98fc1eeae7ffe005d63f2b189783bebbfd1101d
Colton Allen <colton.allen@sentry.io>
2025-01-30 16:23:23 -0800

    Add exemplars

--- tests/sentry/issues/endpoints/test_organization_group_index.py
+++ tests/sentry/issues/endpoints/test_organization_group_index.py
@@ -4050,15 +4050,15 @@ class GroupListTest(APITestCase, SnubaTestCase, SearchIssueTestMixin):
             }
         ):
             response = self.get_success_response(query="abc:true")
-            assert len(json.loads(response.content)) == 1
+            assert len(json.loads(response.content)) == 1, "abc:true on"
 
         with self.feature({"organizations:feature-flag-autocomplete": False}):
             response = self.get_success_response(query="abc:true")
-            assert len(json.loads(response.content)) == 0
+            assert len(json.loads(response.content)) == 0, "abc:true off"
 
         with self.feature({"organizations:feature-flag-autocomplete": True}):
             response = self.get_success_response(query="abc:false")
-            assert len(json.loads(response.content)) == 0
+            assert len(json.loads(response.content)) == 0, "abc:false on"
 
 
 class GroupUpdateTest(APITestCase, SnubaTestCase):
