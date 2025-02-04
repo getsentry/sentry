4900fef0d54f83d5b924661cff5cdc24243ba544
Mitan <dmitrykharkovsky@gmail.com>
2025-02-04 11:14:27 -0500

    comment hardcoded responses.

--- src/sentry/api/endpoints/organization_spans_frequency_stats.py
+++ src/sentry/api/endpoints/organization_spans_frequency_stats.py
@@ -2,13 +2,13 @@ from google.protobuf.json_format import MessageToDict
 from rest_framework.exceptions import ParseError
 from rest_framework.request import Request
 from rest_framework.response import Response
+
+# from sentry_protos.snuba.v1.endpoint_find_traces_pb2 import AndTraceFilter
 from sentry_protos.snuba.v1.endpoint_trace_item_stats_pb2 import (
     AttributeDistributionsRequest,
     StatsType,
     TraceItemStatsRequest,
 )
-from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeKey, AttributeValue
-from sentry_protos.snuba.v1.trace_item_filter_pb2 import ComparisonFilter, TraceItemFilter
 
 from sentry import features
 from sentry.api.base import region_silo_endpoint
@@ -20,6 +20,9 @@ from sentry.search.eap.span_columns import SPAN_DEFINITIONS
 from sentry.search.eap.types import SearchResolverConfig
 from sentry.snuba.referrer import Referrer
 from sentry.utils import snuba_rpc
+
+# from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeKey, AttributeValue
+# from sentry_protos.snuba.v1.trace_item_filter_pb2 import ComparisonFilter, TraceItemFilter
 
 
 @region_silo_endpoint
@@ -49,24 +52,45 @@ class OrganizationSpansFrequencyStatsEndpoint(OrganizationEventsV2EndpointBase):
         if serialized["dataset"] != "spans":
             raise ParseError(detail='only using dataset="spans" is supported for this endpoint')
 
+        # keep start and end as is, we need them for the filter
+        snuba_params.start = snuba_params.start_date
+        snuba_params.end = snuba_params.end_date
+
         resolver = SearchResolver(
             params=snuba_params, config=SearchResolverConfig(), definitions=SPAN_DEFINITIONS
         )
 
         meta = resolver.resolve_meta(referrer=Referrer.API_SPANS_TAG_KEYS_RPC.value)
 
-        # sample request from the tests in the sentry-protos repo
         # todo do we need this or it should be done in upstream?
-        filter = TraceItemFilter(
-            comparison_filter=ComparisonFilter(
-                key=AttributeKey(
-                    type=AttributeKey.TYPE_STRING,
-                    name="eap.measurement",
-                ),
-                op=ComparisonFilter.OP_GREATER_THAN,
-                value=AttributeValue(val_double=999),
-            ),
-        )
+        # filter = TraceItemFilter(
+        #     and_filter=TraceItemFilter(
+        #         and_filter=AndTraceFilter(
+        #             filters=[
+        #                 TraceItemFilter(
+        #                     comparison_filter=ComparisonFilter(
+        #                         key=AttributeKey(
+        #                             type=AttributeKey.TYPE_STRING,
+        #                             name="timestamp",
+        #                         ),
+        #                         op=ComparisonFilter.OP_GREATER_THAN,
+        #                         value=AttributeValue(val_str=snuba_params.start_date),
+        #                     ),
+        #                 ),
+        #                 TraceItemFilter(
+        #                     comparison_filter=ComparisonFilter(
+        #                         key=AttributeKey(
+        #                             type=AttributeKey.TYPE_STRING,
+        #                             name="timestamp",
+        #                         ),
+        #                         op=ComparisonFilter.OP_LESS_THAN,
+        #                         value=AttributeValue(val_str=snuba_params.end_date),
+        #                     ),
+        #                 ),
+        #             ]
+        #         )
+        #     )
+        # )
         stats_type = StatsType(
             attribute_distributions=AttributeDistributionsRequest(
                 max_buckets=10, max_attributes=100
@@ -74,7 +98,7 @@ class OrganizationSpansFrequencyStatsEndpoint(OrganizationEventsV2EndpointBase):
         )
 
         rpc_request = TraceItemStatsRequest(
-            filter=filter,
+            filter=None,
             meta=meta,
             stats_types=[stats_type],
         )
