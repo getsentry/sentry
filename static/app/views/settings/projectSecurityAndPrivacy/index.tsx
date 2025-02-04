13854146128eceb25b03b4607072725365be6fb1
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-02-04 11:08:59 +0100

    typo

--- static/app/views/settings/projectSecurityAndPrivacy/index.tsx
+++ static/app/views/settings/projectSecurityAndPrivacy/index.tsx
@@ -63,7 +63,7 @@ export default function ProjectSecurityAndPrivacy({organization, project}: Props
         additionalContext={
           <span>
             {tct(
-              'Advanced data scrubbing rules can configured for each project. These rules will be applied in addition to any organization-level rules in in [linkToOrganizationSecurityAndPrivacy].',
+              'Advanced data scrubbing rules can configured for each project. These rules will be applied in addition to any organization-level rules in [linkToOrganizationSecurityAndPrivacy].',
               {
                 linkToOrganizationSecurityAndPrivacy: (
                   <Link to={`/settings/${organization.slug}/security-and-privacy/`}>
