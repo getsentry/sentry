8819fe4d44ff11ddfd040fbc3365d862497ffe76
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-02-04 14:54:31 -0800

    chore(replay): create a project blocklist for viewed by API (inc-1024) (#84544)
    Ref
    - https://getsentry.atlassian.net/browse/INC-1024
    - All/most of problematic queries involve the viewed_by column:
    https://cloudlogging.app.goo.gl/qjisuZ6EqPDHF8Uh7
    - Project list from slack thread:
    https://sentry.slack.com/archives/C08BB46SJQ7/p1738698577582169?thread_ts=1738698342.127569&cid=C08BB46SJQ7

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -490,6 +490,13 @@ register(
     default=5000,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
+# Disables viewed by queries for a list of project ids.
+register(
+    "replay.viewed-by.project-denylist",
+    type=Sequence,
+    default=[],
+    flags=FLAG_ALLOW_EMPTY | FLAG_PRIORITIZE_DISK | FLAG_AUTOMATOR_MODIFIABLE,
+)
 
 # User Feedback Options
 register(
