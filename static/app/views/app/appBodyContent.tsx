c7455fa94a5c0a799cc9b34cad99c3c62b5d505b
Malachi Willey <malwilley@gmail.com>
2025-02-04 10:50:12 -0800

    Add comment to AppBodyContent

--- static/app/views/app/appBodyContent.tsx
+++ static/app/views/app/appBodyContent.tsx
@@ -6,6 +6,13 @@ interface AppContentProps {
   children: React.ReactNode;
 }
 
+/**
+ * Wraps the body content of the app. For organization routes,
+ * this is the main content which excludes the sidebar.
+ *
+ * For now, this just provides system alerts, which should be visible
+ * on all pages.
+ */
 export function AppBodyContent({children}: AppContentProps) {
   return (
     <Fragment>
