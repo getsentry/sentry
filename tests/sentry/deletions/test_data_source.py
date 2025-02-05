1fcb401a58dbda0da7ffba99b9cd90e984e51f5a
Colleen O'Rourke <colleen@sentry.io>
2025-02-05 11:01:43 -0800

    remove unnecessary stuff

--- tests/sentry/deletions/test_data_source.py
+++ tests/sentry/deletions/test_data_source.py
@@ -1,5 +1,5 @@
 from sentry.deletions.tasks.scheduled import run_scheduled_deletions
-from sentry.snuba.models import QuerySubscription, SnubaQuery
+from sentry.snuba.models import QuerySubscription
 from sentry.testutils.hybrid_cloud import HybridCloudTestMixin
 from sentry.workflow_engine.models import DataSource
 from tests.sentry.workflow_engine.test_base import BaseWorkflowTest
@@ -22,23 +22,3 @@ class DeleteDataSourceTest(BaseWorkflowTest, HybridCloudTestMixin):
 
         assert not DataSource.objects.filter(id=self.data_source.id).exists()
         assert not QuerySubscription.objects.filter(id=self.subscription.id).exists()
-        assert not SnubaQuery.objects.filter(id=self.snuba_query.id).exists()
-
-    def test_snuba_query_shared(self):
-        """
-        Test that if a SnubaQuery is shared amongst multiple QuerySubscriptions we do not delete it
-        """
-        alert_rule_2 = self.create_alert_rule()
-        sub_2 = QuerySubscription.objects.get(snuba_query_id=alert_rule_2.snuba_query.id)
-        sub_2.update(snuba_query=self.snuba_query)
-        sub_2.save()
-        sub_2.refresh_from_db()
-
-        self.ScheduledDeletion.schedule(instance=self.data_source, days=0)
-
-        with self.tasks():
-            run_scheduled_deletions()
-
-        assert not DataSource.objects.filter(id=self.data_source.id).exists()
-        assert not QuerySubscription.objects.filter(id=self.subscription.id).exists()
-        assert SnubaQuery.objects.filter(id=self.snuba_query.id).exists()
