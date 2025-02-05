9f44f7282220176f6ed64eb2f75c091c66e41dc3
JonasBa <jonas@badalic.com>
2025-02-05 10:39:33 -0500

    rever profiling

--- jest.config.ts
+++ jest.config.ts
@@ -284,8 +284,8 @@ const config: Config.InitialOptions = {
           : false,
         // Use production env to reduce sampling of commits on master
         environment: CI ? (IS_MASTER_BRANCH ? 'ci:master' : 'ci:pull_request') : 'local',
-        tracesSampleRate: 1,
-        profilesSampleRate: 1,
+        tracesSampleRate: CI ? 0.75 : 0,
+        profilesSampleRate: 0,
         transportOptions: {keepAlive: true},
       },
       transactionOptions: {
