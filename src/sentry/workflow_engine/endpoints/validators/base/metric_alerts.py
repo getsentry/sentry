c03742416dfc923c6e0c2439e7092e723deff86f
Colleen O'Rourke <colleen@sentry.io>
2025-02-06 11:50:21 -0800

    chore(ACI): Move metric alert detector validator (#84720)
    Just moving this file to be in the ACI directory before I start fleshing
    it out

--- src/sentry/workflow_engine/endpoints/validators/base/metric_alerts.py
+++ src/sentry/workflow_engine/endpoints/validators/base/metric_alerts.py
@@ -78,15 +78,6 @@ class SnubaQueryDataSourceValidator(BaseDataSourceValidator[QuerySubscription]):
 
 
 class MetricAlertComparisonConditionValidator(NumericComparisonConditionValidator):
-    """
-    Implementation note:
-    This is just a reference for how to use these validators and a basic implementation for the
-    metric alert conditions. Note that these shouldn't exist in this file long term - these are
-    implementations, and so should be implemented in `incidents` project (or wherever we decide
-    metric alerts live in the future).
-    Only generic workflow code should live here
-    """
-
     supported_conditions = frozenset((Condition.GREATER, Condition.LESS))
     supported_results = frozenset((DetectorPriorityLevel.HIGH, DetectorPriorityLevel.MEDIUM))
 
@@ -100,7 +91,6 @@ class MetricAlertsDetectorValidator(BaseGroupTypeDetectorValidator):
         This is just a sample implementation. We should have all the same logic here that
         we have for conditions, query validation, etc in
         https://github.com/getsentry/sentry/blob/837d5c1e13a8dc71b622aafec5191d84d0e827c7/src/sentry/incidents/serializers/alert_rule.py#L65
-        And this should be moved to a metric alert specific app, probably `incidents/`
         """
         attrs = super().validate(attrs)
         conditions = attrs["data_conditions"]
