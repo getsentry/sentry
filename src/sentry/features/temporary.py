270941e3c2b328ba3041b5944e482e5425a3d226
Raj Joshi <raj.joshi@sentry.io>
2025-02-06 13:27:17 -0500

    :wrench: chore(aci): create NOA FF (#84662)
    need it to ff some of my slack refactors

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -463,6 +463,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:workflow-engine-metric-alert-processing", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable dual writing for issue alert issues (see: alerts create issues)
     manager.add("organizations:workflow-engine-issue-alert-dual-write", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
+    # Enable notification action for workflow_engine (see: alerts create issues)
+    manager.add("organizations:workflow-engine-notification-action", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable new workflow_engine UI (see: alerts create issues)
     manager.add("organizations:workflow-engine-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable EventUniqueUserFrequencyConditionWithConditions special alert condition
