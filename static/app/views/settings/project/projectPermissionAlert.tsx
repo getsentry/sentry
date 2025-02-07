63b46fa4e25f7e17c0c2230e9dbc0755783f57b1
JonasBa <jonas@badalic.com>
2025-02-07 16:19:25 -0500

    permissionalert: use named export

--- static/app/views/settings/project/projectPermissionAlert.tsx
+++ static/app/views/settings/project/projectPermissionAlert.tsx
@@ -1,32 +1,33 @@
 import Access from 'sentry/components/acl/access';
-import {Alert} from 'sentry/components/alert';
+import {Alert, type AlertProps} from 'sentry/components/alert';
 import {t} from 'sentry/locale';
 import type {Scope} from 'sentry/types/core';
 import type {Team} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
 
-interface Props extends React.ComponentPropsWithoutRef<typeof Alert> {
+interface ProjectPermissionAlertProps extends Omit<AlertProps, 'type'> {
   access?: Scope[];
   project?: Project;
   team?: Team;
 }
 
-export const permissionAlertText = t(
-  'These settings can only be edited by users with the organization-level owner, manager, or team-level admin roles.'
-);
-
-function PermissionAlert({access = ['project:write'], project, team, ...props}: Props) {
+export function ProjectPermissionAlert({
+  access = ['project:write'],
+  project,
+  team,
+  ...props
+}: ProjectPermissionAlertProps) {
   return (
     <Access access={access} project={project} team={team}>
       {({hasAccess}) =>
         !hasAccess && (
           <Alert data-test-id="project-permission-alert" type="warning" {...props}>
-            {permissionAlertText}
+            {t(
+              'These settings can only be edited by users with the organization-level owner, manager, or team-level admin roles.'
+            )}
           </Alert>
         )
       }
     </Access>
   );
 }
-
-export default PermissionAlert;
