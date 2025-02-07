cd1f2e15ab8506138ef4549d050ac05b0d075cda
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-02-07 15:47:17 -0500

    fix(uptime): Further prevent unloading table/graph for uptime (#84805)
    Adds a new hook that will allow for a fallback event to be loaded in a
    90d window which hopefully has the tag for an `uptime_rule`. That ID
    will allow us to query properly for checks and stats for the graph.
    
    This is a workaround which should be removed once we structure the
    uptime rule onto the group itself, since we could avoid making these
    complicated queries and searches.

--- static/app/views/issueDetails/streamline/eventDetailsHeader.tsx
+++ static/app/views/issueDetails/streamline/eventDetailsHeader.tsx
@@ -114,7 +114,7 @@ export function EventDetailsHeader({group, event, project}: EventDetailsHeaderPr
               <MetricIssueChart group={group} project={project} event={event} />
             )}
             {issueTypeConfig.header.graph.type === 'checkin-timeline' && (
-              <IssueCheckInTimeline />
+              <IssueCheckInTimeline group={group} />
             )}
             {issueTypeConfig.header.tagDistribution.enabled && (
               <IssueTagsPreview
