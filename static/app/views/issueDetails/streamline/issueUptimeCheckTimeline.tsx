fa6e74c1f2ecb0b242d5f1ba61b678b356179957
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-02-07 16:38:16 -0500

    ✨ Fix merge conflicts

--- static/app/views/issueDetails/streamline/issueUptimeCheckTimeline.tsx
+++ static/app/views/issueDetails/streamline/issueUptimeCheckTimeline.tsx
@@ -10,8 +10,13 @@ import {
 import {useTimeWindowConfig} from 'sentry/components/checkInTimeline/hooks/useTimeWindowConfig';
 import {Flex} from 'sentry/components/container/flex';
 import {space} from 'sentry/styles/space';
+import type {Event} from 'sentry/types/event';
+import type {Group} from 'sentry/types/group';
+import {useApiQuery} from 'sentry/utils/queryClient';
 import {useDebouncedValue} from 'sentry/utils/useDebouncedValue';
 import {useDimensions} from 'sentry/utils/useDimensions';
+import useOrganization from 'sentry/utils/useOrganization';
+import {useUser} from 'sentry/utils/useUser';
 import {CheckIndicator} from 'sentry/views/alerts/rules/uptime/checkIndicator';
 import {CheckStatus} from 'sentry/views/alerts/rules/uptime/types';
 import {
@@ -21,30 +26,63 @@ import {
 } from 'sentry/views/insights/uptime/timelineConfig';
 import {useUptimeMonitorStats} from 'sentry/views/insights/uptime/utils/useUptimeMonitorStats';
 import {useIssueDetails} from 'sentry/views/issueDetails/streamline/context';
+import {getGroupEventQueryKey} from 'sentry/views/issueDetails/utils';
 
-export function IssueUptimeCheckTimeline() {
+export function useUptimeIssueAlertId({groupId}: {groupId: string}): string | undefined {
+  /**
+   * This should be removed once the uptime rule value is set on the issue.
+   * This will fetch an event from the max range if the detector details
+   * are not available (e.g. time range has changed and page refreshed)
+   */
+  const user = useUser();
+  const organization = useOrganization();
   const {detectorDetails} = useIssueDetails();
   const {detectorId, detectorType} = detectorDetails;
+
+  const hasUptimeDetector = detectorId && detectorType === 'uptime_monitor';
+
+  const {data: event} = useApiQuery<Event>(
+    getGroupEventQueryKey({
+      orgSlug: organization.slug,
+      groupId,
+      eventId: user.options.defaultIssueEvent,
+      environments: [],
+    }),
+    {
+      staleTime: Infinity,
+      enabled: !hasUptimeDetector,
+      retry: false,
+    }
+  );
+
+  // Fall back to the fetched event since the legacy UI isn't nested within the provider the provider
+  return hasUptimeDetector
+    ? detectorId
+    : event?.tags?.find(tag => tag.key === 'uptime_rule')?.value;
+}
+
+export function IssueUptimeCheckTimeline({group}: {group: Group}) {
+  const uptimeAlertId = useUptimeIssueAlertId({groupId: group.id});
   const elementRef = useRef<HTMLDivElement>(null);
   const {width: containerWidth} = useDimensions<HTMLDivElement>({elementRef});
   const timelineWidth = useDebouncedValue(containerWidth, 500);
   const timeWindowConfig = useTimeWindowConfig({timelineWidth});
 
   const {data: uptimeStats, isPending} = useUptimeMonitorStats({
-    ruleIds: detectorType === 'uptime_monitor' && detectorId ? [detectorId] : [],
+    ruleIds: uptimeAlertId ? [uptimeAlertId] : [],
     timeWindowConfig,
   });
 
   const legendStatuses = useMemo(() => {
     const hasUnknownStatus =
-      detectorId &&
-      uptimeStats?.[detectorId]?.some(
+      uptimeAlertId &&
+      uptimeStats?.[uptimeAlertId]?.some(
         ([_, stats]) => stats[CheckStatus.MISSED_WINDOW] > 0
       );
     return [CheckStatus.SUCCESS, CheckStatus.MISSED_WINDOW, CheckStatus.FAILURE].filter(
       status => (hasUnknownStatus ? true : status !== CheckStatus.MISSED_WINDOW)
     );
-  }, [detectorId, uptimeStats]);
+  }, [uptimeAlertId, uptimeStats]);
 
   return (
     <ChartContainer>
@@ -69,7 +107,7 @@ export function IssueUptimeCheckTimeline() {
           <CheckInPlaceholder />
         ) : (
           <CheckInTimeline
-            bucketedData={detectorId ? uptimeStats?.[detectorId] ?? [] : []}
+            bucketedData={uptimeAlertId ? uptimeStats?.[uptimeAlertId] ?? [] : []}
             statusLabel={statusToText}
             statusStyle={tickStyle}
             statusPrecedent={checkStatusPrecedent}
