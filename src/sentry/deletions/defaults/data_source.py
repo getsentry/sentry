dc4b43fd69c02ee0da01155185148a2a1899322e
Colleen O'Rourke <colleen@sentry.io>
2025-02-07 13:27:58 -0800

    typing

--- src/sentry/deletions/defaults/data_source.py
+++ src/sentry/deletions/defaults/data_source.py
@@ -1,7 +1,9 @@
+from typing import cast
+
 from sentry.deletions.base import BaseRelation, ModelDeletionTask
 from sentry.workflow_engine.models.data_source import DataSource
 
 
 class DataSourceDeletionTask(ModelDeletionTask[DataSource]):
     def get_child_relations(self, instance: DataSource) -> list[BaseRelation]:
-        return instance.type_handler.related_model(instance)
+        return cast(list[BaseRelation], instance.type_handler.related_model(instance))
