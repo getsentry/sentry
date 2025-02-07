303ec8a2dec0efa50ade21528fb548862969de17
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-02-07 12:48:41 +0100

    revert remove has_custom_metrics

--- src/sentry/projects/services/project/model.py
+++ src/sentry/projects/services/project/model.py
@@ -37,6 +37,7 @@ class RpcProjectFlags(RpcModel):
     has_cron_monitors: bool
     has_cron_checkins: bool
     has_sourcemaps: bool
+    has_custom_metrics: bool
     has_high_priority_alerts: bool
     has_insights_http: bool
     has_insights_db: bool
