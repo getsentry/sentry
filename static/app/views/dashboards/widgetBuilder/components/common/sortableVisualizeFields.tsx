ccdb6de1cdafd753a694305f4dde058728d0b3b4
nikkikapadia <nikki.kapadia@sentry.io>
2025-02-07 14:32:16 -0500

    make fields draggable and sortable

---
+++ static/app/views/dashboards/widgetBuilder/components/common/sortableVisualizeFields.tsx
@@ -0,0 +1,67 @@
+import {useSortable} from '@dnd-kit/sortable';
+import {CSS} from '@dnd-kit/utilities';
+import {useTheme} from '@emotion/react';
+import styled from '@emotion/styled';
+
+import {Button} from 'sentry/components/button';
+import {IconGrabbable} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+
+export function SortableVisualizeFields({
+  dragId,
+  canDrag,
+  children,
+}: {
+  canDrag: boolean;
+  children: React.ReactNode;
+  dragId: string;
+}) {
+  const theme = useTheme();
+  const {listeners, setNodeRef, transform, transition, attributes, isDragging} =
+    useSortable({
+      id: dragId,
+      transition: null,
+      disabled: !canDrag,
+    });
+
+  let style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+    zIndex: 'auto',
+    display: 'flex',
+    // alignItems: 'center',
+    gap: space(1),
+    width: '100%',
+  } as React.CSSProperties;
+
+  if (isDragging) {
+    style = {
+      ...style,
+      zIndex: 100,
+      height: theme.form.md.height,
+      border: `2px dashed ${theme.border}`,
+      borderRadius: theme.borderRadius,
+    };
+  }
+
+  return (
+    <div ref={setNodeRef} style={style}>
+      {canDrag && (
+        <DragAndReorderButton
+          {...listeners}
+          {...attributes}
+          aria-label={t('Drag to reorder')}
+          icon={<IconGrabbable size="xs" />}
+          size="zero"
+          borderless
+        />
+      )}
+      {children}
+    </div>
+  );
+}
+
+const DragAndReorderButton = styled(Button)`
+  height: ${p => p.theme.form.md.height}px;
+`;
