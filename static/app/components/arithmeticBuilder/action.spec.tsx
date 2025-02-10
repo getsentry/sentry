4c9476bb76ba3a533a3ddf49b88376093a64b071
Tony Xiao <txiao@sentry.io>
2025-02-10 14:57:11 -0500

    feat(explore): Add modifier actions for arithmetic builder
    These are the actions that can be dispatched in order to modify the equations.

---
+++ static/app/components/arithmeticBuilder/action.spec.tsx
@@ -0,0 +1,186 @@
+import {act, renderHook} from 'sentry-test/reactTestingLibrary';
+
+import {useArithmeticBuilderAction} from 'sentry/components/arithmeticBuilder/action';
+import {isTokenFreeText} from 'sentry/components/arithmeticBuilder/token';
+import {tokenizeExpression} from 'sentry/components/arithmeticBuilder/tokenizer';
+
+describe('useArithmeticBuilderAction', function () {
+  it('returns initial state', function () {
+    const {result} = renderHook(
+      ({initialQuery}) =>
+        useArithmeticBuilderAction({
+          initialQuery,
+        }),
+      {
+        initialProps: {
+          initialQuery: 'initial query',
+        },
+      }
+    );
+    expect(result.current).toEqual({
+      dispatch: expect.any(Function),
+      state: {
+        query: 'initial query',
+        focusOverride: null,
+      },
+    });
+  });
+
+  it('resets focus override', function () {
+    const expression = '( avg(span.duration) )';
+
+    const tokens = tokenizeExpression(expression);
+
+    const {result} = renderHook(
+      ({initialQuery}) =>
+        useArithmeticBuilderAction({
+          initialQuery,
+        }),
+      {
+        initialProps: {
+          initialQuery: expression,
+        },
+      }
+    );
+
+    act(() =>
+      result.current.dispatch({
+        type: 'REPLACE_TOKEN',
+        token: tokens[3]!,
+        text: 'sum(span.duration)',
+        focusOverride: {itemKey: 'foo'},
+      })
+    );
+
+    expect(result.current).toEqual({
+      dispatch: expect.any(Function),
+      state: {
+        query: '( sum(span.duration) )',
+        focusOverride: {
+          itemKey: 'foo',
+        },
+      },
+    });
+
+    act(() =>
+      result.current.dispatch({
+        type: 'RESET_FOCUS_OVERRIDE',
+      })
+    );
+
+    expect(result.current).toEqual({
+      dispatch: expect.any(Function),
+      state: {
+        query: '( sum(span.duration) )',
+        focusOverride: null,
+      },
+    });
+  });
+
+  it('deletes token', function () {
+    const expression = '( avg(span.duration) )';
+
+    const tokens = tokenizeExpression(expression);
+
+    const {result} = renderHook(
+      ({initialQuery}) =>
+        useArithmeticBuilderAction({
+          initialQuery,
+        }),
+      {
+        initialProps: {
+          initialQuery: expression,
+        },
+      }
+    );
+
+    act(() =>
+      result.current.dispatch({
+        type: 'DELETE_TOKEN',
+        token: tokens[3]!,
+      })
+    );
+
+    expect(result.current).toEqual({
+      dispatch: expect.any(Function),
+      state: {
+        query: '( )',
+        focusOverride: null,
+      },
+    });
+  });
+
+  it('updates free text token', function () {
+    const expression = '( foobar )';
+
+    const tokens = tokenizeExpression(expression);
+
+    const {result} = renderHook(
+      ({initialQuery}) =>
+        useArithmeticBuilderAction({
+          initialQuery,
+        }),
+      {
+        initialProps: {
+          initialQuery: expression,
+        },
+      }
+    );
+
+    const token = tokens[2];
+
+    if (!isTokenFreeText(token)) {
+      throw new Error('this should be a free text token');
+    }
+
+    act(() =>
+      result.current.dispatch({
+        type: 'UPDATE_FREE_TEXT',
+        token,
+        text: 'sum(span.duration)',
+      })
+    );
+
+    expect(result.current).toEqual({
+      dispatch: expect.any(Function),
+      state: {
+        query: '( sum(span.duration) )',
+        focusOverride: null,
+      },
+    });
+  });
+
+  it('replaces token', function () {
+    const expression = '( avg(span.duration) )';
+
+    const tokens = tokenizeExpression(expression);
+
+    const {result} = renderHook(
+      ({initialQuery}) =>
+        useArithmeticBuilderAction({
+          initialQuery,
+        }),
+      {
+        initialProps: {
+          initialQuery: expression,
+        },
+      }
+    );
+
+    act(() =>
+      result.current.dispatch({
+        type: 'REPLACE_TOKEN',
+        token: tokens[3]!,
+        text: 'sum(span.duration)',
+      })
+    );
+
+    expect(result.current).toEqual({
+      dispatch: expect.any(Function),
+      state: {
+        query: '( sum(span.duration) )',
+        focusOverride: null,
+      },
+    });
+  });
+});
