61b5f415fded97bedcaa1a2f8ceee5157768fb8a
Dan Fuller <dfuller@sentry.io>
2025-02-10 14:43:21 -0800

    add utility function with docstring

--- src/sentry/utils/not_set.py
+++ src/sentry/utils/not_set.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum, auto
+from typing import TypeVar
 
 
 class NotSet(Enum):
@@ -8,3 +9,19 @@ class NotSet(Enum):
 
 
 NOT_SET = NotSet.TOKEN
+
+T = TypeVar("T")
+
+
+def get_current_value_if_not_set(current_value: T, new_value: T | NotSet) -> T:
+    """
+    Used for optionally passing parameters to a function, and defaulting to some value if not passed.
+    This is useful for updating fields on a model, since we can't set those defaults on the function level.
+    Example usage:
+    def my_updater(my_model: SomeModel, val_a: str | NotSet = NOT_SET):
+        my_model.some_field = get_current_value_if_not_set(my_model.some_field, val_a)
+        my_model.save()
+    """
+    if new_value is NOT_SET:
+        return current_value
+    return new_value
