b06d6ed15e951a617625abaf89df74e6360b6858
Shruthi <shruthilaya.jaganathan@sentry.io>
2025-02-10 11:57:52 -0500

    feat(multi-query): Hook up query constructors to location (#84556)
    ![Screenshot 2025-02-07 at 3 06
    24 PM](https://github.com/user-attachments/assets/219865b1-04b1-45d6-a3e2-63be5b610fe7)
    
    Clicking buttons pushes state to URL. Going to add charts and tables in
    a follow up to keep this PR contained.

--- static/app/views/explore/multiQueryMode/queryConstructors/search.tsx
+++ static/app/views/explore/multiQueryMode/queryConstructors/search.tsx
@@ -3,25 +3,36 @@ import {t} from 'sentry/locale';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import {useSpanTags} from 'sentry/views/explore/contexts/spanTagsContext';
 import {
+  type ReadableExploreQueryParts,
+  useUpdateQueryAtIndex,
+} from 'sentry/views/explore/multiQueryMode/locationUtils';
+import {
   Section,
   SectionHeader,
   SectionLabel,
 } from 'sentry/views/explore/multiQueryMode/queryConstructors/styles';
 
-export function SearchBarSection() {
+type Props = {
+  index: number;
+  query: ReadableExploreQueryParts;
+};
+
+export function SearchBarSection({query, index}: Props) {
   const {selection} = usePageFilters();
   const numberTags = useSpanTags('number');
   const stringTags = useSpanTags('string');
 
+  const updateQuerySearch = useUpdateQueryAtIndex(index);
+
   return (
-    <Section data-test-id="section-filter">
+    <Section data-test-id={`section-filter-${index}`}>
       <SectionHeader>
         <SectionLabel underlined={false}>{t('Filter')}</SectionLabel>
       </SectionHeader>
       <EAPSpanSearchQueryBuilder
         projects={selection.projects}
-        initialQuery={''}
-        onSearch={() => {}}
+        initialQuery={query.query ?? ''}
+        onSearch={value => updateQuerySearch({query: value})}
         searchSource="explore"
         numberTags={numberTags}
         stringTags={stringTags}
