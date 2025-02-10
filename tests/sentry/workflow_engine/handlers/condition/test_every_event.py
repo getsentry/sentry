8c4b39ec2dc57aee9c594681ad5cd89c1bfb5f0e
Cathy Teng <cathy.teng@sentry.io>
2025-02-10 09:35:43 -0800

    fix test

--- tests/sentry/workflow_engine/handlers/condition/test_every_event.py
+++ tests/sentry/workflow_engine/handlers/condition/test_every_event.py
@@ -2,13 +2,12 @@ from sentry.rules.conditions.every_event import EveryEventCondition
 from tests.sentry.workflow_engine.handlers.condition.test_base import ConditionTestCase
 
 
-class TestFirstSeenEventCondition(ConditionTestCase):
+class TestEveryEventCondition(ConditionTestCase):
     payload = {"id": EveryEventCondition.id}
 
     def test_dual_write(self):
         # skip translating every event condition
         dcg = self.create_data_condition_group()
         dc = self.translate_to_data_condition(self.payload, dcg)
-        assert dc
 
         assert dc is None
