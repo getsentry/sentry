8c5a021d4d0e555625271747cbcad04c40acdb0c
Malachi Willey <malwilley@gmail.com>
2025-02-10 15:17:02 -0800

    feat(nav): Modify link paths in replays to match new structure (#84909)
    We need to modify all the existing links and `navigate`s to use the new
    `/explore/replays/` structure when the feature flag for the new nav is
    enabled. To accomplish this, I've added a `makeReplaysPathname` util to
    help generate those links.

---
+++ static/app/views/replays/pathnames.tsx
@@ -0,0 +1,19 @@
+import type {Organization} from 'sentry/types/organization';
+import normalizeUrl from 'sentry/utils/url/normalizeUrl';
+
+const LEGACY_REPLAYS_BASE_PATHNAME = 'replays';
+const REPLAYS_BASE_PATHNAME = 'explore/replays';
+
+export function makeReplaysPathname({
+  path,
+  organization,
+}: {
+  organization: Organization;
+  path: '/' | `/${string}/`;
+}) {
+  return normalizeUrl(
+    organization.features.includes('navigation-sidebar-v2')
+      ? `/organizations/${organization.slug}/${REPLAYS_BASE_PATHNAME}${path}`
+      : `/organizations/${organization.slug}/${LEGACY_REPLAYS_BASE_PATHNAME}${path}`
+  );
+}
