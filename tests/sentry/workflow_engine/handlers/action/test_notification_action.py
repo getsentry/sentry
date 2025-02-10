da60e501cddf564bd83079be646daceed617ac2e
Raj Joshi <raj.joshi@sentry.io>
2025-02-10 17:21:10 -0500

    :sparkles: feat(aci): Invoke Rule Registry from Notification Action (#84524)
    This PR introduces a new ABC which we will inherit and create new
    handlers to invoke the legacy issue alert registry.
    
    Basically after the notification action determines that it needs to
    invoke the issue alert registry (rule registry), it will call
    `BaseIssueAlertHandler.invoke_legacy_registry` which will shape the data
    into a rule instance and then `safe_execute` the callback returned by
    the `RuleFuture`.
    
    the method mentioned above is based on
    
    https://github.com/getsentry/sentry/blob/aa95456c77e51b21e920107af33d3e79f05d4f79/src/sentry/rules/processing/processor.py#L147-L184
    
    
    [spec](https://www.notion.so/sentry/Alerts-Create-Issues-Notification-Action-NOA-1268b10e4b5d805b967ed2655c962db6#1268b10e4b5d812aaa59e13c96a04abd)
    
    closes https://getsentry.atlassian.net/browse/ACI-150

--- tests/sentry/workflow_engine/handlers/action/test_notification_action.py
+++ tests/sentry/workflow_engine/handlers/action/test_notification_action.py
@@ -1,9 +1,15 @@
 from unittest import mock
 
+import pytest
+
 from sentry.grouping.grouptype import ErrorGroupType
 from sentry.issues.grouptype import MetricIssuePOC
 from sentry.utils.registry import NoRegistrationExistsError
-from sentry.workflow_engine.handlers.action.notification import NotificationActionHandler
+from sentry.workflow_engine.handlers.action.notification.handler import (
+    IssueAlertRegistryInvoker,
+    NotificationActionHandler,
+    NotificationHandlerException,
+)
 from sentry.workflow_engine.models import Action
 from sentry.workflow_engine.types import WorkflowJob
 from tests.sentry.workflow_engine.test_base import BaseWorkflowTest
@@ -25,7 +31,7 @@ class TestNotificationActionHandler(BaseWorkflowTest):
         # Test passes if no exception is raised
 
     @mock.patch(
-        "sentry.workflow_engine.handlers.action.notification.group_type_notification_registry.get"
+        "sentry.workflow_engine.handlers.action.notification.handler.group_type_notification_registry.get"
     )
     def test_execute_error_group_type(self, mock_registry_get):
         """Test that execute calls correct handler for ErrorGroupType"""
@@ -38,10 +44,12 @@ class TestNotificationActionHandler(BaseWorkflowTest):
         NotificationActionHandler.execute(self.job, self.action, self.detector)
 
         mock_registry_get.assert_called_once_with(ErrorGroupType.slug)
-        mock_handler.assert_called_once_with(self.job, self.action, self.detector)
+        mock_handler.handle_workflow_action.assert_called_once_with(
+            self.job, self.action, self.detector
+        )
 
     @mock.patch(
-        "sentry.workflow_engine.handlers.action.notification.group_type_notification_registry.get"
+        "sentry.workflow_engine.handlers.action.notification.handler.group_type_notification_registry.get"
     )
     def test_execute_metric_alert_type(self, mock_registry_get):
         """Test that execute calls correct handler for MetricIssuePOC"""
@@ -54,13 +62,15 @@ class TestNotificationActionHandler(BaseWorkflowTest):
         NotificationActionHandler.execute(self.job, self.action, self.detector)
 
         mock_registry_get.assert_called_once_with(MetricIssuePOC.slug)
-        mock_handler.assert_called_once_with(self.job, self.action, self.detector)
+        mock_handler.handle_workflow_action.assert_called_once_with(
+            self.job, self.action, self.detector
+        )
 
     @mock.patch(
-        "sentry.workflow_engine.handlers.action.notification.group_type_notification_registry.get",
+        "sentry.workflow_engine.handlers.action.notification.handler.group_type_notification_registry.get",
         side_effect=NoRegistrationExistsError,
     )
-    @mock.patch("sentry.workflow_engine.handlers.action.notification.logger")
+    @mock.patch("sentry.workflow_engine.handlers.action.notification.handler.logger")
     def test_execute_unknown_group_type(self, mock_logger, mock_registry_get):
         """Test that execute does nothing when detector has no group_type"""
         NotificationActionHandler.execute(self.job, self.action, self.detector)
@@ -69,4 +79,42 @@ class TestNotificationActionHandler(BaseWorkflowTest):
             "No notification handler found for detector type: %s",
             self.detector.type,
             extra={"detector_id": self.detector.id, "action_id": self.action.id},
+        )
+
+
+class TestIssueAlertRegistryInvoker(BaseWorkflowTest):
+    def setUp(self):
+        super().setUp()
+        self.project = self.create_project()
+        self.detector = self.create_detector(project=self.project)
+        self.action = Action(type=Action.Type.DISCORD)
+        self.group, self.event, self.group_event = self.create_group_event()
+        self.job = WorkflowJob({"event": self.group_event})
+
+    @mock.patch(
+        "sentry.workflow_engine.handlers.action.notification.handler.issue_alert_handler_registry.get"
+    )
+    def test_handle_workflow_action_no_handler(self, mock_registry_get):
+        """Test that handle_workflow_action raises NoRegistrationExistsError when no handler exists"""
+        mock_registry_get.side_effect = NoRegistrationExistsError()
+
+        with pytest.raises(NoRegistrationExistsError):
+            IssueAlertRegistryInvoker.handle_workflow_action(self.job, self.action, self.detector)
+
+    @mock.patch(
+        "sentry.workflow_engine.handlers.action.notification.handler.issue_alert_handler_registry.get"
+    )
+    @mock.patch("sentry.workflow_engine.handlers.action.notification.handler.logger")
+    def test_handle_workflow_action_general_exception(self, mock_logger, mock_registry_get):
+        """Test that handle_workflow_action wraps general exceptions in NotificationHandlerException"""
+        mock_handler = mock.Mock()
+        mock_handler.invoke_legacy_registry.side_effect = Exception("Something went wrong")
+        mock_registry_get.return_value = mock_handler
+
+        with pytest.raises(NotificationHandlerException):
+            IssueAlertRegistryInvoker.handle_workflow_action(self.job, self.action, self.detector)
+
+        mock_logger.exception.assert_called_once_with(
+            "Error invoking issue alert handler",
+            extra={"action_id": self.action.id},
         )
