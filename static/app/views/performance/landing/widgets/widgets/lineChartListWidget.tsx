getsentry/sentry

commit 0b98f2a2949db782545863758977f767da416d5d

fix(insights): view all button broken on self hosted (#84538)
closes https://github.com/getsentry/self-hosted/issues/3452

Fixes a self hosted issue where the `view all` on insight overview
widgets take you a url without a org and org slug in the path.

This is fixed by removing the `bare=true` parameters from the
`moduleUrlBuilder`. When `bare= true` we bypass `normalizeUrl` and don't
add the customer domain. For example if `bare=true`,
`moduleUrlBuilder('db') === /insights/backend/database/`.

I think the expectation here was that `LinkButton` would run
`normalizeUrl` for us, which might handles adding the org and slug to
the path, however, the `normalizeUrl` function works by conditionally
replacing `/organizations/slug` with an empty string IF the user is not
on a customer domain url.

What this means is that anytime `normalizeUrl` is called, we should
always include `/organizations/org-slug`.

Technically we are running `normalizeUrl` twice with this solution, but
there's no serious performance effects of this, and I'd rather run
`moduleUrlBuilder` without the `bare` parameter, that way we never have
to worry about when we need to use it, and we never run into a similar
situation as this.

static/app/views/performance/landing/widgets/widgets/lineChartListWidget.tsx
@@ -804,7 +804,7 @@ export function LineChartListWidget(props: PerformanceWidgetProps) {
           },
         ];
 
-  const moduleURLBuilder = useModuleURLBuilder(true);
+  const moduleURLBuilder = useModuleURLBuilder();
 
   const getContainerActions = (provided: ComponentData) => {
     const route: string =
@@ -828,7 +828,7 @@ export function LineChartListWidget(props: PerformanceWidgetProps) {
     ].includes(props.chartSetting) ? (
       <Fragment>
         <div>
-          <LinkButton to={`/${route}/`} size="sm">
+          <LinkButton to={`${route}/`} size="sm">
             {t('View All')}
           </LinkButton>
         </div>

static/app/views/performance/landing/widgets/components/widgetContainer.spec.tsx
@@ -2,6 +2,7 @@ import type {InitializeDataSettings} from 'sentry-test/performance/initializePer
 import {initializeData as _initializeData} from 'sentry-test/performance/initializePerformanceData';
 import {render, screen, userEvent, waitFor} from 'sentry-test/reactTestingLibrary';
 
+import ConfigStore from 'sentry/stores/configStore';
 import {MetricsCardinalityProvider} from 'sentry/utils/performance/contexts/metricsCardinality';
 import {MEPSettingProvider} from 'sentry/utils/performance/contexts/metricsEnhancedSetting';
 import {PageAlert, PageAlertProvider} from 'sentry/utils/performance/contexts/pageAlert';
@@ -69,6 +70,7 @@ describe('Performance > Widgets > WidgetContainer', function () {
   let issuesListMock: jest.Mock;
 
   beforeEach(function () {
+    ConfigStore.init();
     eventStatsMock = MockApiClient.addMockResponse({
       method: 'GET',
       url: `/organizations/org-slug/events-stats/`,
@@ -869,7 +871,7 @@ describe('Performance > Widgets > WidgetContainer', function () {
     );
     expect(await screen.findByRole('button', {name: 'View All'})).toHaveAttribute(
       'href',
-      '/insights/backend/database/'
+      '/organizations/org-slug/insights/backend/database/'
     );
     expect(eventsMock).toHaveBeenCalledTimes(1);
     expect(eventsMock).toHaveBeenNthCalledWith(
@@ -915,7 +917,7 @@ describe('Performance > Widgets > WidgetContainer', function () {
     );
     expect(await screen.findByRole('button', {name: 'View All'})).toHaveAttribute(
       'href',
-      '/insights/backend/http/'
+      '/organizations/org-slug/insights/backend/http/'
     );
     expect(eventsMock).toHaveBeenCalledTimes(1);
     expect(eventsMock).toHaveBeenNthCalledWith(
@@ -959,7 +961,7 @@ describe('Performance > Widgets > WidgetContainer', function () {
     );
     expect(await screen.findByRole('button', {name: 'View All'})).toHaveAttribute(
       'href',
-      '/insights/frontend/assets/'
+      '/organizations/org-slug/insights/frontend/assets/'
     );
     expect(eventsMock).toHaveBeenCalledTimes(1);
     expect(eventsMock).toHaveBeenNthCalledWith(
@@ -989,6 +991,27 @@ describe('Performance > Widgets > WidgetContainer', function () {
     );
   });
 
+  it('should not contain org in link with customer domain', async function () {
+    const data = initializeData();
+    ConfigStore.set('customerDomain', {
+      subdomain: 'sentry',
+      organizationUrl: 'https://sentry.sentry.io',
+      sentryUrl: 'https://sentry.io',
+    });
+
+    wrapper = render(
+      <WrappedComponent
+        data={data}
+        defaultChartSetting={PerformanceWidgetSetting.MOST_TIME_CONSUMING_RESOURCES}
+      />
+    );
+
+    expect(await screen.findByRole('button', {name: 'View All'})).toHaveAttribute(
+      'href',
+      '/insights/frontend/assets/'
+    );
+  });
+
   it('Highest cache miss rate transactions widget', async function () {
     const data = initializeData();
 
@@ -1008,7 +1031,7 @@ describe('Performance > Widgets > WidgetContainer', function () {
     );
     expect(await screen.findByRole('button', {name: 'View All'})).toHaveAttribute(
       'href',
-      '/insights/backend/caches/'
+      '/organizations/org-slug/insights/backend/caches/'
     );
     expect(eventsMock).toHaveBeenCalledTimes(1);
     expect(eventsMock).toHaveBeenNthCalledWith(

commit 7c6842c4b9f06bddb4216b2e553a44cd21fde71b

fix(insights): link from cache module widget broken (#84863)
Fixes a bug where clicking a transaction from the cache module widget
resulted in an invalid url. (double slash `//insights/backend/caches`
instead of `/insights/backend/caches`)

This was a regression from
https://github.com/getsentry/sentry/pull/84538
In that PR we removed bare=true, which means that the url returned from
`moduleUrlBuilder` is now prepended with a `/`, in this PR we remove the
extra slash.

static/app/views/performance/landing/widgets/widgets/lineChartListWidget.tsx
@@ -692,7 +692,7 @@ export function LineChartListWidget(props: PerformanceWidgetProps) {
           const cacheMissRate = listItem[fieldString] as any;
 
           const target = normalizeUrl(
-            `/${moduleRoute}/?${qs.stringify({transaction, project: listItem['project.id']})}`
+            `${moduleRoute}/?${qs.stringify({transaction, project: listItem['project.id']})}`
           );
 
           return (