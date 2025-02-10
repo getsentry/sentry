16a34f62fe7ec4ed63c5df1a6f1e69cdf9b6fcc9
Raj Joshi <raj.joshi@sentry.io>
2025-02-10 17:21:10 -0500

    :sparkles: feat(aci): Invoke Rule Registry from Notification Action (#84524)
    This PR introduces a new ABC which we will inherit and create new
    handlers to invoke the legacy issue alert registry.
    
    Basically after the notification action determines that it needs to
    invoke the issue alert registry (rule registry), it will call
    `BaseIssueAlertHandler.invoke_legacy_registry` which will shape the data
    into a rule instance and then `safe_execute` the callback returned by
    the `RuleFuture`.
    
    the method mentioned above is based on
    
    https://github.com/getsentry/sentry/blob/aa95456c77e51b21e920107af33d3e79f05d4f79/src/sentry/rules/processing/processor.py#L147-L184
    
    
    [spec](https://www.notion.so/sentry/Alerts-Create-Issues-Notification-Action-NOA-1268b10e4b5d805b967ed2655c962db6#1268b10e4b5d812aaa59e13c96a04abd)
    
    closes https://getsentry.atlassian.net/browse/ACI-150

--- src/sentry/workflow_engine/handlers/action/notification/handler.py
+++ src/sentry/workflow_engine/handlers/action/notification/handler.py
@@ -4,6 +4,9 @@ from abc import ABC, abstractmethod
 from sentry.grouping.grouptype import ErrorGroupType
 from sentry.issues.grouptype import MetricIssuePOC
 from sentry.utils.registry import NoRegistrationExistsError, Registry
+from sentry.workflow_engine.handlers.action.notification.issue_alert import (
+    issue_alert_handler_registry,
+)
 from sentry.workflow_engine.models import Action, Detector
 from sentry.workflow_engine.registry import action_handler_registry
 from sentry.workflow_engine.types import ActionHandler, WorkflowJob
@@ -11,21 +14,22 @@ from sentry.workflow_engine.types import ActionHandler, WorkflowJob
 logger = logging.getLogger(__name__)
 
 
+class NotificationHandlerException(Exception):
+    pass
+
+
 class LegacyRegistryInvoker(ABC):
     """
     Abstract base class that defines the interface for notification handlers.
     """
 
-    def __call__(self, job: WorkflowJob, action: Action, detector: Detector) -> None:
-        # Here we could add metrics collection or other common functionality
-        self.handle_workflow_action(job, action, detector)
-
+    @staticmethod
     @abstractmethod
-    def handle_workflow_action(self, job: WorkflowJob, action: Action, detector: Detector) -> None:
+    def handle_workflow_action(job: WorkflowJob, action: Action, detector: Detector) -> None:
         """
         Implement this method to handle the specific notification logic for your handler.
         """
-        pass
+        raise NotImplementedError
 
 
 group_type_notification_registry = Registry[LegacyRegistryInvoker]()
@@ -41,25 +45,40 @@ class NotificationActionHandler(ActionHandler):
     ) -> None:
         try:
             handler = group_type_notification_registry.get(detector.type)
-            handler(job, action, detector)
+            handler.handle_workflow_action(job, action, detector)
         except NoRegistrationExistsError:
             logger.exception(
                 "No notification handler found for detector type: %s",
                 detector.type,
                 extra={"detector_id": detector.id, "action_id": action.id},
             )
-            # Maybe metrics here?
 
 
 @group_type_notification_registry.register(ErrorGroupType.slug)
 class IssueAlertRegistryInvoker(LegacyRegistryInvoker):
-    def handle_workflow_action(self, job: WorkflowJob, action: Action, detector: Detector) -> None:
-        # TODO(iamrajjoshi): Implement this
-        pass
+    @staticmethod
+    def handle_workflow_action(job: WorkflowJob, action: Action, detector: Detector) -> None:
+        try:
+            handler = issue_alert_handler_registry.get(action.type)
+            handler.invoke_legacy_registry(job, action, detector)
+        except NoRegistrationExistsError:
+            logger.exception(
+                "No issue alert handler found for action type: %s",
+                action.type,
+                extra={"action_id": action.id},
+            )
+            raise
+        except Exception as e:
+            logger.exception(
+                "Error invoking issue alert handler",
+                extra={"action_id": action.id},
+            )
+            raise NotificationHandlerException(e)
 
 
 @group_type_notification_registry.register(MetricIssuePOC.slug)
 class MetricAlertRegistryInvoker(LegacyRegistryInvoker):
-    def handle_workflow_action(self, job: WorkflowJob, action: Action, detector: Detector) -> None:
+    @staticmethod
+    def handle_workflow_action(job: WorkflowJob, action: Action, detector: Detector) -> None:
         # TODO(iamrajjoshi): Implement this
         pass
---
+++ tests/sentry/workflow_engine/handlers/action/notification/test_issue_alert.py
@@ -0,0 +1,153 @@
+import uuid
+from unittest import mock
+
+from sentry.constants import ObjectStatus
+from sentry.models.rule import Rule, RuleSource
+from sentry.workflow_engine.handlers.action.notification.issue_alert import (
+    BaseIssueAlertHandler,
+    DiscordIssueAlertHandler,
+)
+from sentry.workflow_engine.models import Action
+from sentry.workflow_engine.types import WorkflowJob
+from tests.sentry.workflow_engine.test_base import BaseWorkflowTest
+
+
+class TestBaseIssueAlertHandler(BaseWorkflowTest):
+    def setUp(self):
+        super().setUp()
+        self.project = self.create_project()
+        self.detector = self.create_detector(project=self.project)
+        self.workflow = self.create_workflow(environment=self.environment)
+        self.action = self.create_action(
+            type=Action.Type.DISCORD,
+            integration_id="1234567890",
+            target_identifier="channel456",
+            data={"tags": "environment,user,my_tag"},
+        )
+        self.group, self.event, self.group_event = self.create_group_event()
+        self.job = WorkflowJob(event=self.group_event, workflow=self.workflow)
+
+        class TestHandler(BaseIssueAlertHandler):
+            @staticmethod
+            def build_rule_action_blob(action: Action) -> dict:
+                return {
+                    "id": "sentry.integrations.discord.notify_action.DiscordNotifyServiceAction",
+                    "server": "1234567890",
+                    "channel_id": "channel456",
+                    "tags": "environment,user,my_tag",
+                }
+
+        self.handler = TestHandler()
+
+    def test_create_rule_instance_from_action(self):
+        """Test that create_rule_instance_from_action creates a Rule with correct attributes"""
+        rule = self.handler.create_rule_instance_from_action(self.action, self.detector, self.job)
+
+        assert isinstance(rule, Rule)
+        assert rule.id == self.action.id
+        assert rule.project == self.detector.project
+        assert rule.environment_id is not None
+        assert self.workflow.environment is not None
+        assert rule.environment_id == self.workflow.environment.id
+        assert rule.label == self.detector.name
+        assert rule.data == {
+            "actions": [
+                {
+                    "id": "sentry.integrations.discord.notify_action.DiscordNotifyServiceAction",
+                    "server": "1234567890",
+                    "channel_id": "channel456",
+                    "tags": "environment,user,my_tag",
+                }
+            ]
+        }
+        assert rule.status == ObjectStatus.ACTIVE
+        assert rule.source == RuleSource.ISSUE
+
+    def test_create_rule_instance_from_action_no_environment(self):
+        """Test that create_rule_instance_from_action creates a Rule with correct attributes"""
+        workflow = self.create_workflow()
+        job = WorkflowJob(event=self.group_event, workflow=workflow)
+        rule = self.handler.create_rule_instance_from_action(self.action, self.detector, job)
+
+        assert isinstance(rule, Rule)
+        assert rule.id == self.action.id
+        assert rule.project == self.detector.project
+        assert rule.environment_id is None
+        assert rule.label == self.detector.name
+        assert rule.data == {
+            "actions": [
+                {
+                    "id": "sentry.integrations.discord.notify_action.DiscordNotifyServiceAction",
+                    "server": "1234567890",
+                    "channel_id": "channel456",
+                    "tags": "environment,user,my_tag",
+                }
+            ]
+        }
+        assert rule.status == ObjectStatus.ACTIVE
+        assert rule.source == RuleSource.ISSUE
+
+    @mock.patch("sentry.workflow_engine.handlers.action.notification.issue_alert.safe_execute")
+    @mock.patch(
+        "sentry.workflow_engine.handlers.action.notification.issue_alert.activate_downstream_actions"
+    )
+    @mock.patch("uuid.uuid4")
+    def test_invoke_legacy_registry(
+        self, mock_uuid, mock_activate_downstream_actions, mock_safe_execute
+    ):
+        # Test that invoke_legacy_registry correctly processes the action
+        mock_uuid.return_value = uuid.UUID("12345678-1234-5678-1234-567812345678")
+
+        # Mock callback and futures
+        mock_callback = mock.Mock()
+        mock_futures = [mock.Mock()]
+        mock_activate_downstream_actions.return_value = {"some_key": (mock_callback, mock_futures)}
+
+        self.handler.invoke_legacy_registry(self.job, self.action, self.detector)
+
+        # Verify activate_downstream_actions called with correct args
+        mock_activate_downstream_actions.assert_called_once_with(
+            mock.ANY, self.job["event"], "12345678-1234-5678-1234-567812345678"  # Rule instance
+        )
+
+        # Verify callback execution
+        mock_safe_execute.assert_called_once_with(mock_callback, self.job["event"], mock_futures)
+
+
+class TestDiscordIssueAlertHandler(BaseWorkflowTest):
+    def setUp(self):
+        super().setUp()
+        self.handler = DiscordIssueAlertHandler()
+        self.action = self.create_action(
+            type=Action.Type.DISCORD,
+            integration_id="1234567890",
+            target_identifier="channel456",
+            data={"tags": "environment,user,my_tag"},
+        )
+        # self.project = self.create_project()
+        # self.detector = self.create_detector(project=self.project)
+        # self.group, self.event, self.group_event = self.create_group_event()
+        # self.job = WorkflowJob(event=self.group_event)
+
+    def test_build_rule_action_blob(self):
+        """Test that build_rule_action_blob creates correct Discord action data"""
+        blob = self.handler.build_rule_action_blob(self.action)
+
+        assert blob == {
+            "id": "sentry.integrations.discord.notify_action.DiscordNotifyServiceAction",
+            "server": "1234567890",
+            "channel_id": "channel456",
+            "tags": "environment,user,my_tag",
+        }
+
+    def test_build_rule_action_blob_no_tags(self):
+        """Test that build_rule_action_blob handles missing tags"""
+        self.action.data = {}
+        blob = self.handler.build_rule_action_blob(self.action)
+
+        assert blob == {
+            "id": "sentry.integrations.discord.notify_action.DiscordNotifyServiceAction",
+            "server": "1234567890",
+            "channel_id": "channel456",
+            "tags": "",
+        }
