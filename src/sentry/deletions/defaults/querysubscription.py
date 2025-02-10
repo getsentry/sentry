545ba8a81e55c4396bb85b497f40b58b3d596529
Colleen O'Rourke <colleen@sentry.io>
2025-02-10 14:21:39 -0800

    feat(ACI): Delete endpoint for a detector (#84279)
    Create a DELETE endpoint for a given detector. This deletes the related
    `DataSource`, `QuerySubscription`, and `SnubaQuery`.
    
    It assumes that a data source is only connected to a single detector.
    The db model supports multiple detectors but the UI does not.

--- src/sentry/deletions/defaults/querysubscription.py
+++ src/sentry/deletions/defaults/querysubscription.py
@@ -1,4 +1,4 @@
-from sentry.deletions.base import ModelDeletionTask
+from sentry.deletions.base import BaseRelation, ModelDeletionTask, ModelRelation
 from sentry.snuba.models import QuerySubscription
 
 
@@ -9,3 +9,16 @@ class QuerySubscriptionDeletionTask(ModelDeletionTask[QuerySubscription]):
         # Clear the foreign key as the schema was created without a cascade clause
         Incident.objects.filter(subscription_id=instance.id).update(subscription_id=None)
         super().delete_instance(instance)
+
+    def get_child_relations(self, instance: QuerySubscription) -> list[BaseRelation]:
+        from sentry.incidents.models.alert_rule import AlertRule
+        from sentry.snuba.models import SnubaQuery
+
+        if not AlertRule.objects.filter(snuba_query_id=instance.snuba_query_id).exists():
+            if (
+                QuerySubscription.objects.filter(snuba_query_id=instance.snuba_query_id).count()
+                == 1
+            ):
+                return [ModelRelation(SnubaQuery, {"id": instance.snuba_query_id})]
+
+        return []
