d804db73466788e515cf2053cfaf81166aa27d37
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-02-10 13:01:22 -0500

    test(detectors): Add some test for the detector section (#84856)
    Add a test for the detector section with all the current detector types:
    cron, uptime, metrics. Adds some IssueTypes.

--- static/app/types/group.tsx
+++ static/app/types/group.tsx
@@ -103,8 +103,14 @@ export enum IssueType {
   REPLAY_RAGE_CLICK = 'replay_click_rage',
   REPLAY_HYDRATION_ERROR = 'replay_hydration_error',
 
+  // Monitors
+  MONITOR_CHECK_IN_FAILURE = 'monitor_check_in_failure',
+
   // Uptime
   UPTIME_DOMAIN_FAILURE = 'uptime_domain_failure',
+
+  // Metric Issues
+  METRIC_ISSUE_POC = 'metric_issue_poc', // To be removed
 }
 
 // Update this if adding an issue type that you don't want to show up in search!
