6f66dbf44513432d5873d8daaaf6e0477eeef2a6
Jonas <jonas.badalic@sentry.io>
2025-02-11 08:50:07 -0500

    theme: sidebar cleanup (#84774)
    Doesnt need to exist on the main theme. There are a couple fields left
    that are trickier to move, will take care of those in a followup PR

--- static/app/views/replays/detail/layout/utils.tsx
+++ static/app/views/replays/detail/layout/utils.tsx
@@ -1,3 +1,4 @@
+import {SIDEBAR_COLLAPSED_WIDTH, SIDEBAR_EXPANDED_WIDTH} from 'sentry/components/sidebar';
 import {LayoutKey} from 'sentry/utils/replays/hooks/useReplayLayout';
 import theme from 'sentry/utils/theme';
 
@@ -5,7 +6,7 @@ export const getDefaultLayout = (collapsed: boolean): LayoutKey => {
   const {innerWidth, innerHeight} = window;
 
   const sidebarWidth = parseInt(
-    collapsed ? theme.sidebar.collapsedWidth : theme.sidebar.expandedWidth,
+    collapsed ? SIDEBAR_COLLAPSED_WIDTH : SIDEBAR_EXPANDED_WIDTH,
     10
   );
 
