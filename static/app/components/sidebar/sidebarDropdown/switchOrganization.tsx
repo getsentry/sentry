3b3929132b3e36315f3703daab33ed97a83ee8b1
Jonas <jonas.badalic@sentry.io>
2025-02-11 08:50:07 -0500

    theme: sidebar cleanup (#84774)
    Doesnt need to exist on the main theme. There are a couple fields left
    that are trickier to move, will take care of those in a followup PR

--- static/app/components/sidebar/sidebarDropdown/switchOrganization.tsx
+++ static/app/components/sidebar/sidebarDropdown/switchOrganization.tsx
@@ -167,8 +167,9 @@ const SwitchOrganizationMenuActor = styled('span')`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  margin: 0 -${p => p.theme.sidebar.menuSpacing};
-  padding: 0 ${p => p.theme.sidebar.menuSpacing};
+  /* @TODO(jonasbadalic): the 15px is non standard spacing. Should it be space(2) which is 16px? */
+  margin: 0 -15px;
+  padding: 0 15px;
 `;
 
 const SwitchOrganizationMenu = styled('div')`
