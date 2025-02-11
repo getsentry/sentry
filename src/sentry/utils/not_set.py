b51d659f844b4115d3fd044ef3b53aeb81a57002
Dan Fuller <dfuller@sentry.io>
2025-02-11 10:35:11 -0800

    chore(api): Generalize `NOT_SET` constant so that we can use it elsewhere (#84915)
    This is useful when providing partial updates to a function
    
    <!-- Describe your PR here. -->

---
+++ src/sentry/utils/not_set.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+from enum import Enum, auto
+from typing import TypeVar
+
+
+class NotSet(Enum):
+    TOKEN = auto()
+
+
+NOT_SET = NotSet.TOKEN
+
+T = TypeVar("T")
+
+
+def default_if_not_set(current_value: T, new_value: T | NotSet) -> T:
+    """
+    Used for optionally passing parameters to a function, and defaulting to some value if not passed.
+    This is useful for updating fields on a model, since we can't set those defaults on the function level.
+    Example usage:
+    def my_updater(my_model: SomeModel, val_a: str | NotSet = NOT_SET):
+        my_model.some_field = default_if_not_set(my_model.some_field, val_a)
+        my_model.save()
+    """
+    if new_value is NOT_SET:
+        return current_value
+    return new_value
