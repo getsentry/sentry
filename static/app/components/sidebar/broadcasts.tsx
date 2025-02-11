65880acbfba11dae47840230f2259de56f3a5bd0
Scott Cooper <scttcper@gmail.com>
2025-02-11 11:57:54 -0800

    fix(broadcasts): Increase stale time of /broadcasts/ endpoint (#84982)

--- static/app/components/sidebar/broadcasts.tsx
+++ static/app/components/sidebar/broadcasts.tsx
@@ -47,7 +47,8 @@ export function Broadcasts({
   const {isPending, data: broadcasts = []} = useApiQuery<Broadcast[]>(
     [`/organizations/${organization.slug}/broadcasts/`],
     {
-      staleTime: 0,
+      // Five minute stale time prevents window focus frequent refetches
+      staleTime: 1000 * 60 * 5,
       refetchInterval: POLLER_DELAY,
       refetchOnWindowFocus: true,
     }
