2221c600f9d9fe943bcb7532e5ca4af2f598b9ab
Vjeran Grozdanic <vjeran.grozdanic@sentry.io>
2025-02-11 09:39:53 +0100

    make it post deployment

--- src/sentry/migrations/0830_add_index_external_id_organization_to_project.py
+++ src/sentry/migrations/0830_add_index_external_id_organization_to_project.py
@@ -18,7 +18,7 @@ class Migration(CheckedMigration):
     #   is a schema change, it's completely safe to run the operation after the code has deployed.
     # Once deployed, run these manually via: https://develop.sentry.dev/database-migrations/#migration-deployment
 
-    is_post_deployment = False
+    is_post_deployment = True
 
     dependencies = [
         ("sentry", "0829_add_external_id_to_project"),
