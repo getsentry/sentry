d82419ce84a9f390e3e6a3fee9b574cc8843c6ae
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2025-02-11 16:03:40 -0500

    fix(insights): double clicking module tab removes global filters (#84994)

--- static/app/views/insights/pages/domainViewHeader.spec.tsx
+++ static/app/views/insights/pages/domainViewHeader.spec.tsx
@@ -1,4 +1,4 @@
-import type {Location} from 'history';
+import type {Location, Query} from 'history';
 import {OrganizationFixture} from 'sentry-fixture/organization';
 
 import {render, screen} from 'sentry-test/reactTestingLibrary';
@@ -11,6 +11,8 @@ import {ModuleName} from 'sentry/views/insights/types';
 jest.mock('sentry/utils/useLocation');
 jest.mock('sentry/utils/useOrganization');
 
+const useLocationMock = jest.mocked(useLocation);
+
 describe('DomainViewHeader', function () {
   const organization = OrganizationFixture({
     features: ['insights-entry-points'],
@@ -18,7 +20,7 @@ describe('DomainViewHeader', function () {
 
   beforeEach(() => {
     jest.resetAllMocks();
-    jest.mocked(useLocation).mockReturnValue({
+    useLocationMock.mockReturnValue({
       pathname: '/organizations/org-slug/insights/frontend/',
     } as Location);
     jest.mocked(useOrganization).mockReturnValue(organization);
@@ -37,6 +39,39 @@ describe('DomainViewHeader', function () {
     expect(screen.getByText('domainTitle')).toBeInTheDocument();
     expect(screen.getByRole('tab', {name: 'Overview'})).toBeInTheDocument();
     expect(screen.getByRole('tab', {name: 'Network Requests'})).toBeInTheDocument();
+  });
+
+  it('renders link with page filters', () => {
+    useLocationMock.mockClear();
+    useLocationMock.mockReturnValue({
+      pathname: '/organizations/org-slug/insights/frontend/',
+      query: {
+        project: ['1'],
+        environment: ['prod'],
+        statsPeriod: '14d',
+        transaction: '123',
+      } as Query,
+    } as Location);
+
+    render(
+      <DomainViewHeader
+        domainBaseUrl="domainBaseUrl"
+        domainTitle="domainTitle"
+        modules={[ModuleName.HTTP]}
+        selectedModule={undefined}
+      />
+    );
+
+    const overviewTab = screen.getByRole('tab', {name: 'Overview'});
+    const networkRequestsTab = screen.getByRole('tab', {name: 'Network Requests'});
+    expect(overviewTab.firstChild).toHaveAttribute(
+      'href',
+      '/domainBaseUrl?environment=prod&project=1&statsPeriod=14d'
+    );
+    expect(networkRequestsTab.firstChild).toHaveAttribute(
+      'href',
+      '/organizations/org-slug/insights/frontend/http/?environment=prod&project=1&statsPeriod=14d'
+    );
   });
 
   it('does not show network requests without features', () => {
