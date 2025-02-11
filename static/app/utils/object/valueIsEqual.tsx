fe4b542a4ecfedee38472d6d5150ac388e512af6
TkDodo <dominik.dorfmeister@sentry.io>
2025-02-11 15:59:57 +0100

    ref: remove now unnecessary ts-expect-error

--- static/app/utils/object/valueIsEqual.tsx
+++ static/app/utils/object/valueIsEqual.tsx
@@ -52,7 +52,6 @@ function objectMatchesSubset(
 
   if (deep !== true) {
     for (k in other) {
-      // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
       if (obj[k] !== other[k]) {
         return false;
       }
@@ -61,7 +60,6 @@ function objectMatchesSubset(
   }
 
   for (k in other) {
-    // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     if (!valueIsEqual(obj[k], other[k], deep)) {
       return false;
     }
--- static/app/utils/useCleanQueryParamsOnRouteLeave.tsx
+++ static/app/utils/useCleanQueryParamsOnRouteLeave.tsx
@@ -19,7 +19,6 @@ export function handleRouteLeave<Q extends Record<PropertyKey, unknown>>({
   oldPathname: string;
 }) {
   const hasSomeValues = fieldsToClean.some(
-    // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     field => newLocation.query[field] !== undefined
   );
 
