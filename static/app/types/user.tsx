12bcd292e23421711150897c1fcb68a24aca71a7
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-02-11 10:25:45 +0100

    ref(quick-start): Add logic to display quick start based on user visit

--- static/app/types/user.tsx
+++ static/app/types/user.tsx
@@ -22,6 +22,12 @@ export type AvatarUser = {
   };
 };
 
+// This object tracks the status of the quick start display for each organization.
+// The key is the organization ID, and the value represents the display status:
+// 0 = Hidden on the first visit
+// 1 = Shown once (on the second visit)
+// 2 = Hidden automatically after the second visit
+type QuickStartDisplay = Record<string, number>;
 export interface User extends Omit<AvatarUser, 'options'> {
   canReset2fa: boolean;
   dateJoined: string;
@@ -48,6 +54,7 @@ export interface User extends Omit<AvatarUser, 'options'> {
     defaultIssueEvent: 'recommended' | 'latest' | 'oldest';
     language: string;
     prefersIssueDetailsStreamlinedUI: boolean;
+    quickStartDisplay: QuickStartDisplay;
     stacktraceOrder: number;
     theme: 'system' | 'light' | 'dark';
     timezone: string;
