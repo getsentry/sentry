56e5cd77d37eb73ba35248b2fec2a7cfe541e2cf
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2025-02-11 19:25:40 -0500

    ref: fix types for test_event_search (#85030)
    <!-- Describe your PR here. -->

--- tests/sentry/api/test_event_search.py
+++ tests/sentry/api/test_event_search.py
@@ -536,7 +536,7 @@ class ParseSearchQueryBackendTest(SimpleTestCase):
         ]
 
     def test_allowed_keys(self):
-        config = SearchConfig(allowed_keys=["good_key"])
+        config = SearchConfig(allowed_keys={"good_key"})
 
         assert parse_search_query("good_key:123 bad_key:123 text") == [
             SearchFilter(key=SearchKey(name="good_key"), operator="=", value=SearchValue("123")),
@@ -553,7 +553,7 @@ class ParseSearchQueryBackendTest(SimpleTestCase):
         ]
 
     def test_blocked_keys(self):
-        config = SearchConfig(blocked_keys=["bad_key"])
+        config = SearchConfig(blocked_keys={"bad_key"})
 
         assert parse_search_query("some_key:123 bad_key:123 text") == [
             SearchFilter(key=SearchKey(name="some_key"), operator="=", value=SearchValue("123")),
