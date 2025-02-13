6727421a3b09cec8f11caa80a74996c6b7d52939
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-02-13 14:20:32 -0800

    feat(aci): add monitors index page (#85043)
    preview: https://sentry-kmmnu365b.sentry.dev/monitors
    
    
    https://github.com/user-attachments/assets/a927ed1b-9174-4f5a-a465-c60982bbefd1
    
    ---------
    
    Co-authored-by: Nate Moore <nate.moore@sentry.io>

--- static/app/views/detectors/list.tsx
+++ static/app/views/detectors/list.tsx
@@ -1,12 +1,17 @@
 import {Fragment} from 'react';
 
 import {LinkButton} from 'sentry/components/button';
+import {Flex} from 'sentry/components/container/flex';
+import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilter';
+import SearchBar from 'sentry/components/searchBar';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {ActionsProvider} from 'sentry/components/workflowEngine/layout/actions';
 import ListLayout from 'sentry/components/workflowEngine/layout/list';
 import {useWorkflowEngineFeatureGate} from 'sentry/components/workflowEngine/useWorkflowEngineFeatureGate';
 import {IconAdd} from 'sentry/icons';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import DetectorListTable from 'sentry/views/detectors/components/detectorListTable';
 
 export default function DetectorsList() {
   useWorkflowEngineFeatureGate({redirect: true});
@@ -15,10 +20,22 @@ export default function DetectorsList() {
     <SentryDocumentTitle title={t('Monitors')} noSuffix>
       <ActionsProvider actions={<Actions />}>
         <ListLayout>
-          <h2>Monitors</h2>
+          <TableHeader />
+          <DetectorListTable detectors={[]} />
         </ListLayout>
       </ActionsProvider>
     </SentryDocumentTitle>
+  );
+}
+
+function TableHeader() {
+  return (
+    <Flex gap={space(2)}>
+      <ProjectPageFilter />
+      <div style={{flexGrow: 1}}>
+        <SearchBar placeholder={t('Search by name')} />
+      </div>
+    </Flex>
   );
 }
 
