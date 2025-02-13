9d9be9ee38edcba95d1d7f25b147b86149b0b0ac
Scott Cooper <scttcper@gmail.com>
2025-02-13 10:20:46 -0800

    feat(ui): Upgrade to react 18.3 (#83334)

--- static/app/utils/silence-react-unsafe-warnings.ts
+++ static/app/utils/silence-react-unsafe-warnings.ts
@@ -1,5 +1,7 @@
 // eslint-disable-next-line no-console
 export const originalConsoleWarn = console.warn;
+// eslint-disable-next-line no-console
+export const originalConsoleError = console.error;
 
 const ignoredWarnings = [
   // React unsafe warnings.
@@ -10,6 +12,16 @@ const ignoredWarnings = [
   /moment construction falls back/,
   // Locale not set during tests
   /Locale not set, defaulting to English/,
+];
+
+const ignoredErrors = [
+  // TODO(react19): We should no longer have these errors before upgrading to React 19 all from AsyncComponent.
+  // <ComponentName> uses the legacy contextTypes API which is no longer supported and will be removed in the next major release.
+  // Use React.createContext() with static contextType instead.
+  /uses the legacy contextTypes API/,
+  // TODO(react19): Another error from AsyncComponent.
+  // <ComponentName> declares both contextTypes and contextType static properties. The legacy contextTypes property will be ignored.
+  /declares both contextTypes and contextType static properties/,
 ];
 
 window.console.warn = (message: any, ...args: any[]) => {
@@ -23,4 +35,13 @@ window.console.warn = (message: any, ...args: any[]) => {
   originalConsoleWarn(message, ...args);
 };
 
+window.console.error = (message: any, ...args: any[]) => {
+  if (typeof message === 'string' && ignoredErrors.some(error => error.test(message))) {
+    return;
+  }
+
+  originalConsoleError(message, ...args);
+};
+
 export const silencedWarn = window.console.warn;
+export const silencedError = window.console.error;
