e0d2edb9271d89a5507ecf89f1bfbe4e423444fd
Fabian Schindler <fabian.schindler@sentry.io>
2025-02-13 16:23:20 +0100

    feat(project): add `__hash__` to `RpcProject` to allow serialization (#85143)

--- src/sentry/projects/services/project/model.py
+++ src/sentry/projects/services/project/model.py
@@ -60,6 +60,11 @@ class RpcProject(RpcModel):
     platform: str | None = None
     external_id: str | None = None
 
+    def __hash__(self) -> int:
+        # Mimic the behavior of hashing a Django ORM entity, for compatibility with
+        # serializers, as this project object is often used for that.
+        return hash((self.id, self.organization_id, self.slug))
+
     def get_option(
         self,
         key: str,
