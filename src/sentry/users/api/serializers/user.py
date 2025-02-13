4b485d5972c5f08c5f793338ed444e4862a24cc3
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-02-13 09:26:28 +0100

    ref(quick-start): Add 'quickStartDisplay' user's option (#84939)

--- src/sentry/users/api/serializers/user.py
+++ src/sentry/users/api/serializers/user.py
@@ -66,6 +66,7 @@ class _UserOptions(TypedDict):
     timezone: str
     clock24Hours: bool
     prefersIssueDetailsStreamlinedUI: bool
+    quickStartDisplay: dict[str, int]
 
 
 class UserSerializerResponseOptional(TypedDict, total=False):
@@ -198,6 +199,7 @@ class UserSerializer(Serializer):
                 "prefersIssueDetailsStreamlinedUI": options.get(
                     "prefers_issue_details_streamlined_ui", False
                 ),
+                "quickStartDisplay": options.get("quick_start_display") or {},
             }
 
             d["flags"] = {"newsletter_consent_prompt": bool(obj.flags.newsletter_consent_prompt)}
