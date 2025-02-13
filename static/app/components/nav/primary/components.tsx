0b8af75cf59ddb5198061b0a5b42adf0d22bcf8e
Malachi Willey <malwilley@gmail.com>
2025-02-13 10:22:25 -0800

    feat(nav): Add what's new (#85172)
    Adds "What's new" (broadcasts) to the new nav. 
    
    While adding this, I reorganized the nav files a bit so it could reuse
    some of the existing components. Instead of everything residing in
    `primary.tsx`, I've created a `primary` folder and broke out the current
    file into `components.tsx` and `index.tsx`.
    
    One big difference is that this now uses the drawer instead of the old
    panel.

---
+++ static/app/components/nav/primary/components.tsx
@@ -0,0 +1,197 @@
+import {type MouseEventHandler, useCallback} from 'react';
+import {css} from '@emotion/react';
+import styled from '@emotion/styled';
+
+import {DropdownMenu, type MenuItemProps} from 'sentry/components/dropdownMenu';
+import InteractionStateLayer from 'sentry/components/interactionStateLayer';
+import Link from 'sentry/components/links/link';
+import {linkStyles} from 'sentry/components/links/styles';
+import {PRIMARY_SIDEBAR_WIDTH} from 'sentry/components/nav/constants';
+import {useNavContext} from 'sentry/components/nav/context';
+import {NavLayout} from 'sentry/components/nav/types';
+import {isLinkActive, makeLinkPropsFromTo} from 'sentry/components/nav/utils';
+import {space} from 'sentry/styles/space';
+import {trackAnalytics} from 'sentry/utils/analytics';
+import normalizeUrl from 'sentry/utils/url/normalizeUrl';
+import {useLocation} from 'sentry/utils/useLocation';
+import useOrganization from 'sentry/utils/useOrganization';
+
+interface SidebarItemLinkProps {
+  analyticsKey: string;
+  label: string;
+  to: string;
+  activeTo?: string;
+  children?: React.ReactNode;
+  forceLabel?: boolean;
+  onClick?: MouseEventHandler<HTMLElement>;
+}
+
+interface SidebarItemDropdownProps {
+  analyticsKey: string;
+  items: MenuItemProps[];
+  label: string;
+  children?: React.ReactNode;
+  forceLabel?: boolean;
+}
+
+export function SidebarItem({children}: {children: React.ReactNode}) {
+  const {layout} = useNavContext();
+  return (
+    <SidebarItemWrapper isMobile={layout === NavLayout.MOBILE}>
+      {children}
+    </SidebarItemWrapper>
+  );
+}
+
+export function SidebarMenu({
+  items,
+  children,
+  analyticsKey,
+  label,
+  forceLabel,
+}: SidebarItemDropdownProps) {
+  const organization = useOrganization();
+  const recordAnalytics = useCallback(
+    () => trackAnalytics('growth.clicked_sidebar', {item: analyticsKey, organization}),
+    [organization, analyticsKey]
+  );
+  const {layout} = useNavContext();
+
+  const showLabel = forceLabel || layout === NavLayout.MOBILE;
+
+  return (
+    <SidebarItem>
+      <DropdownMenu
+        position="right-end"
+        trigger={(props, isOpen) => {
+          return (
+            <NavButton
+              {...props}
+              aria-label={!showLabel ? label : undefined}
+              onClick={event => {
+                recordAnalytics();
+                props.onClick?.(event);
+              }}
+            >
+              <InteractionStateLayer hasSelectedBackground={isOpen} />
+              {children}
+              {showLabel ? label : null}
+            </NavButton>
+          );
+        }}
+        items={items}
+      />
+    </SidebarItem>
+  );
+}
+
+export function SidebarLink({
+  children,
+  to,
+  activeTo = to,
+  analyticsKey,
+  label,
+  forceLabel = false,
+}: SidebarItemLinkProps) {
+  const organization = useOrganization();
+  const location = useLocation();
+  const isActive = isLinkActive(normalizeUrl(activeTo, location), location.pathname);
+  const linkProps = makeLinkPropsFromTo(to);
+
+  const {layout} = useNavContext();
+  const showLabel = forceLabel || layout === NavLayout.MOBILE;
+
+  const recordAnalytics = useCallback(
+    () => trackAnalytics('growth.clicked_sidebar', {item: analyticsKey, organization}),
+    [organization, analyticsKey]
+  );
+
+  return (
+    <SidebarItem>
+      <NavLink
+        {...linkProps}
+        onClick={recordAnalytics}
+        aria-selected={isActive}
+        aria-current={isActive ? 'page' : undefined}
+        aria-label={!showLabel ? label : undefined}
+      >
+        <InteractionStateLayer hasSelectedBackground={isActive} />
+        {children}
+        {showLabel ? label : null}
+      </NavLink>
+    </SidebarItem>
+  );
+}
+
+const SidebarItemWrapper = styled('li')<{isMobile: boolean}>`
+  svg {
+    --size: 14px;
+    width: var(--size);
+    height: var(--size);
+
+    ${p =>
+      !p.isMobile &&
+      css`
+        --size: 16px;
+      `}
+  }
+  > a,
+  button {
+    display: flex;
+    flex-direction: row;
+    gap: ${space(1.5)};
+    align-items: center;
+    padding: ${space(1.5)} ${space(3)};
+    color: var(--color, currentColor);
+    font-size: ${p => p.theme.fontSizeMedium};
+    font-weight: ${p => p.theme.fontWeightNormal};
+    line-height: 1;
+    width: 100%;
+
+    & > * {
+      pointer-events: none;
+    }
+
+    ${p =>
+      !p.isMobile &&
+      css`
+        flex-direction: column;
+        justify-content: center;
+        border-radius: ${p.theme.borderRadius};
+        margin-inline: 0 auto;
+        gap: ${space(0.75)};
+        padding: ${space(1.5)} 0;
+        min-height: 44px;
+        width: ${PRIMARY_SIDEBAR_WIDTH - 10}px;
+        letter-spacing: -0.02em;
+        font-size: 10px;
+      `}
+  }
+`;
+
+export const NavLink = styled(Link)`
+  position: relative;
+`;
+
+export const NavButton = styled('button')`
+  border: none;
+  position: relative;
+  background: transparent;
+
+  ${linkStyles}
+`;
+
+export const SidebarItemBadge = styled('span')`
+  position: absolute;
+  top: ${space(0.5)};
+  right: ${space(1)};
+  display: block;
+  text-align: center;
+  color: ${p => p.theme.white};
+  font-size: ${p => p.theme.fontSizeExtraSmall};
+  background: ${p => p.theme.red300};
+  width: 16px;
+  height: 16px;
+  border-radius: 16px;
+  line-height: 16px;
+`;
---
+++ static/app/components/nav/primary/whatsNew.tsx
@@ -0,0 +1,155 @@
+import {Fragment, useEffect, useMemo, useRef} from 'react';
+
+import useDrawer from 'sentry/components/globalDrawer';
+import InteractionStateLayer from 'sentry/components/interactionStateLayer';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {useNavContext} from 'sentry/components/nav/context';
+import {
+  NavButton,
+  SidebarItem,
+  SidebarItemBadge,
+} from 'sentry/components/nav/primary/components';
+import {NavLayout} from 'sentry/components/nav/types';
+import {BroadcastPanelItem} from 'sentry/components/sidebar/broadcastPanelItem';
+import SidebarPanelEmpty from 'sentry/components/sidebar/sidebarPanelEmpty';
+import {IconBroadcast} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import type {Organization} from 'sentry/types/organization';
+import type {Broadcast} from 'sentry/types/system';
+import {
+  type ApiQueryKey,
+  setApiQueryData,
+  useApiQuery,
+  useMutation,
+  useQueryClient,
+} from 'sentry/utils/queryClient';
+import useApi from 'sentry/utils/useApi';
+import useOrganization from 'sentry/utils/useOrganization';
+
+const MARK_SEEN_DELAY = 1000;
+
+function makeBroadcastsQueryKey({
+  organization,
+}: {
+  organization: Organization;
+}): ApiQueryKey {
+  return [`/organizations/${organization.slug}/broadcasts/`];
+}
+
+function useFetchBroadcasts() {
+  const organization = useOrganization();
+
+  return useApiQuery<Broadcast[]>(makeBroadcastsQueryKey({organization}), {
+    // Five minute stale time prevents window focus frequent refetches
+    staleTime: 1000 * 60 * 5,
+    // 10 minutes poll
+    refetchInterval: 1000 * 60 * 10,
+    refetchOnWindowFocus: true,
+  });
+}
+
+function WhatsNewContent({unseenPostIds}: {unseenPostIds: string[]}) {
+  const api = useApi();
+  const organization = useOrganization();
+  const queryClient = useQueryClient();
+
+  const {mutate: markBroadcastsAsSeen} = useMutation({
+    mutationFn: (newUnseenPostIds: string[]) => {
+      return api.requestPromise('/broadcasts/', {
+        method: 'PUT',
+        query: {id: newUnseenPostIds},
+        data: {hasSeen: '1'},
+      });
+    },
+    onSuccess: () => {
+      setApiQueryData<Broadcast[]>(
+        queryClient,
+        makeBroadcastsQueryKey({organization}),
+        data => (data ? data.map(item => ({...item, hasSeen: true})) : [])
+      );
+    },
+  });
+
+  const {isPending, data: broadcasts = []} = useFetchBroadcasts();
+
+  useEffect(() => {
+    const markSeenTimeout = window.setTimeout(() => {
+      markBroadcastsAsSeen(unseenPostIds);
+    }, MARK_SEEN_DELAY);
+
+    return () => {
+      if (markSeenTimeout) {
+        window.clearTimeout(markSeenTimeout);
+      }
+    };
+  }, [unseenPostIds, markBroadcastsAsSeen]);
+
+  if (isPending) {
+    return <LoadingIndicator />;
+  }
+
+  if (broadcasts.length === 0) {
+    return (
+      <SidebarPanelEmpty>
+        {t('No recent updates from the Sentry team.')}
+      </SidebarPanelEmpty>
+    );
+  }
+
+  return (
+    <Fragment>
+      {broadcasts.map(item => (
+        <BroadcastPanelItem
+          key={item.id}
+          hasSeen={item.hasSeen}
+          title={item.title}
+          message={item.message}
+          link={item.link}
+          mediaUrl={item.mediaUrl}
+          category={item.category}
+        />
+      ))}
+    </Fragment>
+  );
+}
+
+export function WhatsNew() {
+  const ref = useRef<HTMLButtonElement>(null);
+  const {openDrawer, isDrawerOpen, closeDrawer} = useDrawer();
+  const {data: broadcasts = []} = useFetchBroadcasts();
+  const unseenPostIds = useMemo(
+    () => broadcasts.filter(item => !item.hasSeen).map(item => item.id),
+    [broadcasts]
+  );
+
+  const {layout} = useNavContext();
+  const showLabel = layout === NavLayout.MOBILE;
+
+  return (
+    <SidebarItem>
+      <NavButton
+        ref={ref}
+        onClick={() => {
+          if (isDrawerOpen) {
+            closeDrawer();
+          } else {
+            openDrawer(() => <WhatsNewContent unseenPostIds={unseenPostIds} />, {
+              ariaLabel: t("What's New"),
+              shouldCloseOnInteractOutside: el => !ref.current?.contains(el),
+            });
+          }
+        }}
+        aria-label={showLabel ? undefined : t("What's New")}
+      >
+        <InteractionStateLayer />
+        <IconBroadcast />
+        {showLabel && <span>{t("What's New")}</span>}
+        {unseenPostIds.length > 0 && (
+          <SidebarItemBadge data-test-id="whats-new-badge">
+            {unseenPostIds.length}
+          </SidebarItemBadge>
+        )}
+      </NavButton>
+    </SidebarItem>
+  );
+}
