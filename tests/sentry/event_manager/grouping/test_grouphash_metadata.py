ef5d5ded958feed4a1296acf86e5da395e22c23d
Katie Byers <lobsterkatie@gmail.com>
2025-02-14 13:33:52 -0800

    test more stored data

--- tests/sentry/event_manager/grouping/test_grouphash_metadata.py
+++ tests/sentry/event_manager/grouping/test_grouphash_metadata.py
@@ -1,9 +1,10 @@
 from __future__ import annotations
 
 from typing import Any
+from unittest.mock import ANY
 
 from sentry.models.grouphash import GroupHash
-from sentry.models.grouphashmetadata import GroupHashMetadata
+from sentry.models.grouphashmetadata import GroupHashMetadata, HashBasis
 from sentry.projectoptions.defaults import DEFAULT_GROUPING_CONFIG, LEGACY_GROUPING_CONFIG
 from sentry.testutils.cases import TestCase
 from sentry.testutils.helpers import Feature
@@ -60,13 +61,21 @@ class GroupHashMetadataTest(TestCase):
             assert grouphash and grouphash.metadata is None
 
     @with_feature("organizations:grouphash-metadata-creation")
-    def test_stores_grouping_config(self):
-        event = save_new_event({"message": "Dogs are great!"}, self.project)
+    def test_stores_expected_properties(self):
+        event = save_new_event({"message": "Dogs are great!", "platform": "python"}, self.project)
         grouphash = GroupHash.objects.filter(
             project=self.project, hash=event.get_primary_hash()
         ).first()
 
-        self.assert_metadata_values(grouphash, {"latest_grouping_config": DEFAULT_GROUPING_CONFIG})
+        self.assert_metadata_values(
+            grouphash,
+            {
+                "latest_grouping_config": DEFAULT_GROUPING_CONFIG,
+                "hash_basis": HashBasis.MESSAGE,
+                "hashing_metadata": ANY,  # Tested extensively with snapshots
+                "platform": "python",
+            },
+        )
 
     @with_feature("organizations:grouphash-metadata-creation")
     @override_options({"grouping.grouphash_metadata.backfill_sample_rate": 1.0})
