ddf45a9d33efc99a082790578d0efd04edfa2b01
Neel Shah <neel.shah@sentry.io>
2025-02-14 13:09:32 +0100

    chore(ruby): Add send_default_pii to onboarding snippets (#85148)
    part of https://github.com/getsentry/team-sdks/issues/121

--- static/app/gettingStartedDocs/ruby/rack.tsx
+++ static/app/gettingStartedDocs/ruby/rack.tsx
@@ -17,7 +17,11 @@ const getConfigureSnippet = (params: Params) => `
 require 'sentry-ruby'
 
 Sentry.init do |config|
-  config.dsn = '${params.dsn.public}'${
+  config.dsn = '${params.dsn.public}'
+
+  # Add data like request headers and IP for users,
+  # see https://docs.sentry.io/platforms/ruby/data-management/data-collected/ for more info
+  config.send_default_pii = true${
     params.isPerformanceSelected
       ? `
 
