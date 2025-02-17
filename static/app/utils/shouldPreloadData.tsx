cef6d35aab36a561872ef7001d4526b151f68228
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-02-17 09:40:30 +0100

    add comment

--- static/app/utils/shouldPreloadData.tsx
+++ static/app/utils/shouldPreloadData.tsx
@@ -34,6 +34,9 @@ function getPathParams(path: string):
   return undefined;
 }
 
+// Skips data preload if the user is on an invitation acceptance page as they might not have access to the data yet, leading to a 403 error.
+// This frontend check is needed along with the backend check (see: https://github.com/getsentry/sentry/blob/cac47187ae98f105b39edf80a0fd3105c95e1cb5/src/sentry/web/client_config.py#L402-L414).
+// It's necessary because when using `yarn dev-ui`, the app is Single Page Application (SPA), and routing happens client-side without a full page reload.
 export function shouldPreloadData(config: Config): boolean {
   const path = window.location.pathname;
 
