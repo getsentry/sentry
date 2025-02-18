e9170545fee7080ce4fced9d8ce81d99a77ae448
Christinarlong <christina.long@sentry.io>
2025-02-18 10:27:02 -0800

    add on hover tests

--- static/app/views/settings/organizationDeveloperSettings/sentryApplicationRow/sentryApplicationRowButtons.spec.tsx
+++ static/app/views/settings/organizationDeveloperSettings/sentryApplicationRow/sentryApplicationRowButtons.spec.tsx
@@ -68,8 +68,22 @@ describe('Sentry App Row Buttons', function () {
     const publishButton = await screen.findByRole('button', {name: 'Publish'});
     expect(publishButton).toBeDisabled();
 
+    await userEvent.hover(publishButton, {delay: 100});
+
+    expect(
+      screen.getByText('Published integrations cannot be re-published.')
+    ).toBeInTheDocument();
+    expect(publishButton).toBeDisabled();
+
     const deleteButton = await screen.findByRole('button', {name: 'Delete'});
     expect(deleteButton).toBeDisabled();
+
+    await userEvent.hover(deleteButton, {delay: 100});
+
+    expect(
+      screen.getByText('Published integrations cannot be removed.')
+    ).toBeInTheDocument();
+    expect(publishButton).toBeDisabled();
 
     const dashboardButton = await screen.findByRole('button', {name: 'Dashboard'});
     expect(dashboardButton).toBeEnabled();
@@ -138,6 +152,12 @@ describe('Sentry App Row Buttons', function () {
     );
 
     const publishButton = await screen.findByRole('button', {name: 'Publish'});
+
+    await userEvent.hover(publishButton, {delay: 100});
+
+    expect(
+      screen.getByText('Integrations with a UI component must have an icon')
+    ).toBeInTheDocument();
     expect(publishButton).toBeDisabled();
 
     const deleteButton = await screen.findByRole('button', {name: 'Delete'});
@@ -164,6 +184,74 @@ describe('Sentry App Row Buttons', function () {
 
     const publishButton = await screen.findByRole('button', {name: 'Publish'});
     expect(publishButton).toBeDisabled();
+
+    await userEvent.hover(publishButton, {delay: 100});
+
+    expect(
+      screen.getByText('This integration has already been submitted for publishing')
+    ).toBeInTheDocument();
+
+    const deleteButton = await screen.findByRole('button', {name: 'Delete'});
+    expect(deleteButton).toBeEnabled();
+
+    const dashboardButton = await screen.findByRole('button', {name: 'Dashboard'});
+    expect(dashboardButton).toBeEnabled();
+  });
+
+  it('disables the publish button if the app is in progress of deletion', async () => {
+    const organization = OrganizationFixture({
+      features: [`streamlined-publishing-flow`],
+    });
+    const internalSentryApp = SentryAppFixture({status: 'deletion_in_progress'});
+
+    render(
+      <SentryApplicationRowButtons
+        organization={organization}
+        app={internalSentryApp}
+        onClickRemove={removeApp}
+        onClickPublish={publishApp}
+      />
+    );
+
+    const publishButton = await screen.findByRole('button', {name: 'Publish'});
+    expect(publishButton).toBeDisabled();
+
+    await userEvent.hover(publishButton, {delay: 100});
+
+    expect(
+      screen.getByText('Only unpublished integrations can be published')
+    ).toBeInTheDocument();
+
+    const deleteButton = await screen.findByRole('button', {name: 'Delete'});
+    expect(deleteButton).toBeEnabled();
+
+    const dashboardButton = await screen.findByRole('button', {name: 'Dashboard'});
+    expect(dashboardButton).toBeEnabled();
+  });
+
+  it('disables the publish button if the app doesnt have a logo', async () => {
+    const organization = OrganizationFixture({
+      features: [`streamlined-publishing-flow`],
+    });
+    const internalSentryApp = SentryAppFixture({status: 'unpublished'});
+
+    render(
+      <SentryApplicationRowButtons
+        organization={organization}
+        app={internalSentryApp}
+        onClickRemove={removeApp}
+        onClickPublish={publishApp}
+      />
+    );
+
+    const publishButton = await screen.findByRole('button', {name: 'Publish'});
+    expect(publishButton).toBeDisabled();
+
+    await userEvent.hover(publishButton, {delay: 100});
+
+    expect(
+      screen.getByText('A logo is required to publish an integration')
+    ).toBeInTheDocument();
 
     const deleteButton = await screen.findByRole('button', {name: 'Delete'});
     expect(deleteButton).toBeEnabled();
