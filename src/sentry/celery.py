e008902e01e9adfb68fc36b9234081c55162ad9c
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2025-02-18 15:23:55 -0800

    chore(sentry apps): remove send alert event flag (#85381)
    related: https://github.com/getsentry/sentry-options-automator/pull/3171

--- src/sentry/celery.py
+++ src/sentry/celery.py
@@ -14,7 +14,6 @@ LEGACY_PICKLE_TASKS = frozenset(
     [
         # basic tasks that must be passed models still
         "sentry.tasks.process_buffer.process_incr",
-        "sentry.sentry_apps.tasks.sentry_apps.send_alert_event",
         "sentry.tasks.unmerge",
         "src.sentry.notifications.utils.async_send_notification",
         # basic tasks that can already deal with primary keys passed
