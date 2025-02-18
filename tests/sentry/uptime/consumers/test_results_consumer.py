eaca45955541d20b2db0c9c165257ae3f1943a59
Dan Fuller <dfuller@sentry.io>
2025-02-18 15:11:51 -0800

    feat(uptime): Allow failure and recovery thresholds to be configured via options (#85391)
    This gives us more options when we see a high failure rate

--- tests/sentry/uptime/consumers/test_results_consumer.py
+++ tests/sentry/uptime/consumers/test_results_consumer.py
@@ -95,8 +95,8 @@ class ProcessResultTest(ConfigPusherTestMixin, metaclass=abc.ABCMeta):
             mock.patch("sentry.uptime.consumers.results_consumer.metrics") as metrics,
             self.feature(["organizations:uptime", "organizations:uptime-create-issues"]),
             mock.patch(
-                "sentry.uptime.consumers.results_consumer.ACTIVE_FAILURE_THRESHOLD",
-                new=2,
+                "sentry.uptime.consumers.results_consumer.get_active_failure_threshold",
+                return_value=2,
             ),
         ):
             self.send_result(result)
@@ -165,8 +165,8 @@ class ProcessResultTest(ConfigPusherTestMixin, metaclass=abc.ABCMeta):
             mock.patch("sentry.uptime.consumers.results_consumer.metrics") as metrics,
             self.feature(["organizations:uptime", "organizations:uptime-create-issues"]),
             mock.patch(
-                "sentry.uptime.consumers.results_consumer.ACTIVE_FAILURE_THRESHOLD",
-                new=2,
+                "sentry.uptime.consumers.results_consumer.get_active_failure_threshold",
+                return_value=2,
             ),
         ):
             self.send_result(result)
@@ -209,8 +209,8 @@ class ProcessResultTest(ConfigPusherTestMixin, metaclass=abc.ABCMeta):
             mock.patch("sentry.uptime.consumers.results_consumer.metrics") as metrics,
             self.feature(["organizations:uptime", "organizations:uptime-create-issues"]),
             mock.patch(
-                "sentry.uptime.consumers.results_consumer.ACTIVE_FAILURE_THRESHOLD",
-                new=1,
+                "sentry.uptime.consumers.results_consumer.get_active_failure_threshold",
+                return_value=1,
             ),
             override_options({"uptime.restrict-issue-creation-by-hosting-provider-id": ["TEST"]}),
         ):
@@ -341,8 +341,8 @@ class ProcessResultTest(ConfigPusherTestMixin, metaclass=abc.ABCMeta):
         with (
             mock.patch("sentry.uptime.consumers.results_consumer.metrics") as metrics,
             mock.patch(
-                "sentry.uptime.consumers.results_consumer.ACTIVE_FAILURE_THRESHOLD",
-                new=1,
+                "sentry.uptime.consumers.results_consumer.get_active_failure_threshold",
+                return_value=1,
             ),
         ):
             self.send_result(result)
@@ -373,8 +373,8 @@ class ProcessResultTest(ConfigPusherTestMixin, metaclass=abc.ABCMeta):
             mock.patch("sentry.uptime.consumers.results_consumer.metrics") as metrics,
             self.feature(["organizations:uptime", "organizations:uptime-create-issues"]),
             mock.patch(
-                "sentry.uptime.consumers.results_consumer.ACTIVE_FAILURE_THRESHOLD",
-                new=2,
+                "sentry.uptime.consumers.results_consumer.get_active_failure_threshold",
+                return_value=2,
             ),
         ):
             self.send_result(
@@ -906,8 +906,8 @@ class ProcessResultTest(ConfigPusherTestMixin, metaclass=abc.ABCMeta):
             mock.patch("sentry.uptime.consumers.results_consumer.metrics") as metrics,
             self.feature(["organizations:uptime", "organizations:uptime-create-issues"]),
             mock.patch(
-                "sentry.uptime.consumers.results_consumer.ACTIVE_FAILURE_THRESHOLD",
-                new=2,
+                "sentry.uptime.consumers.results_consumer.get_active_failure_threshold",
+                return_value=2,
             ),
             mock.patch(
                 "sentry.uptime.consumers.results_consumer.TOTAL_PROVIDERS_TO_INCLUDE_AS_TAGS",
@@ -986,11 +986,11 @@ class ProcessResultTest(ConfigPusherTestMixin, metaclass=abc.ABCMeta):
 
     @mock.patch("sentry.uptime.consumers.results_consumer._snuba_uptime_checks_producer.produce")
     @mock.patch(
-        "sentry.uptime.consumers.results_consumer.ACTIVE_FAILURE_THRESHOLD",
-        new=1,
+        "sentry.uptime.consumers.results_consumer.get_active_failure_threshold",
+        return_value=1,
     )
     @override_options({"uptime.snuba_uptime_results.enabled": True})
-    def test_produces_snuba_uptime_results_in_incident(self, mock_produce) -> None:
+    def test_produces_snuba_uptime_results_in_incident(self, _, mock_produce) -> None:
         """
         Validates that the consumer produces a message to Snuba's Kafka topic for uptime check results
         """
