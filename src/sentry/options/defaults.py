d41bb2a2ffccb71fd2f12dff557e62af4b6200cb
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2025-02-18 15:23:55 -0800

    chore(sentry apps): remove send alert event flag (#85381)
    related: https://github.com/getsentry/sentry-options-automator/pull/3171

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3020,14 +3020,6 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
-# migrating send_alert_event task to not pass Event
-register(
-    "sentryapps.send_alert_event.use-eventid",
-    type=Float,
-    default=0.0,
-    flags=FLAG_AUTOMATOR_MODIFIABLE,
-)
-
 # allows us to disable indexing during maintenance events
 register(
     "sentry.similarity.indexing.enabled",
