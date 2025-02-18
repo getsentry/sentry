2f8141b795a66b098b69114821eedcf1132d6e05
Rohan Agarwal <47861399+roaga@users.noreply.github.com>
2025-02-18 18:30:17 -0500

    feat(autofix): Add status indicator to button (#85306)
    Redoing this PR, added two missing checks, stricter polling, and a test.
    
    ![image](https://github.com/user-attachments/assets/c4f88dae-410c-4940-8ebc-18368a7d6290)

--- static/app/views/issueDetails/groupSidebar.spec.tsx
+++ static/app/views/issueDetails/groupSidebar.spec.tsx
@@ -84,6 +84,14 @@ describe('GroupSidebar', function () {
       url: `/organizations/${organization.slug}/issues/${group.id}/first-last-release/`,
       method: 'GET',
     });
+    MockApiClient.addMockResponse({
+      url: `/issues/${group.id}/autofix/setup/`,
+      body: {
+        genAIConsent: {ok: true},
+        integration: {ok: true},
+        githubWriteIntegration: {ok: true},
+      },
+    });
   });
 
   afterEach(function () {
