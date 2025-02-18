5cb491894c745e5102c259a435dccc6ba960ab30
JonasBa <jonas@badalic.com>
2025-02-18 13:57:28 -0500

    alerts: remove deprecated export

--- static/app/components/alert.tsx
+++
@@ -1,5 +0,0 @@
-/**
- * @deprecated Import from `sentry/components/core/alert` instead.
- */
-// biome-ignore lint/performance/noBarrelFile: Remove this once imports are fixed
-export * from 'sentry/components/core/alert';
