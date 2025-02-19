ed15a8645ceb2cec786770fe6918a80009536566
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2025-02-19 10:07:13 -0800

    chore(notification settings): yoink over the notifications validator (#85422)

--- src/sentry/api/validators/__init__.py
+++ src/sentry/api/validators/__init__.py
@@ -4,7 +4,6 @@ from .doc_integration import *  # noqa: F401,F403
 from .email import *  # noqa: F401,F403
 from .external_actor import *  # noqa: F401,F403
 from .integrations import *  # noqa: F401,F403
-from .notifications import *  # noqa: F401,F403
 from .project import *  # noqa: F401,F403
 from .project_codeowners import *  # noqa: F401,F403
 from .userrole import *  # noqa: F401,F403
