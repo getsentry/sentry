e1604818459bd76d661e8c61e944064a04f107cb
Mark Story <mark@mark-story.com>
2025-02-19 10:56:13 -0500

    feat(relocation) Add relocation transfer models
    We're running into transaction timeout issues when trying to process
    relocation outbox messages as network transfers are taking longer than
    we have transaction duration.
    
    By moving relocation state tracking away from outboxes we can accomodate
    the longer latency for moving relocation exports around without
    incurring risk from changing outbox locking.
    
    [design doc](https://www.notion.so/sentry/Relocation-outbox-rework-19e8b10e4b5d80268741cc5b513a89ae)

---
+++ src/sentry/relocation/models/relocationtransfer.py
@@ -0,0 +1,57 @@
+from __future__ import annotations
+
+from django.db import models
+from django.utils import timezone
+
+from sentry.backup.scopes import RelocationScope
+from sentry.db.models import control_silo_model, region_silo_model
+from sentry.db.models.base import DefaultFieldsModel
+from sentry.db.models.fields.uuid import UUIDField
+
+
+class RelocationTransferState(models.TextChoices):
+    Request = "request"
+    Reply = "reply"
+
+
+class BaseRelocationTransfer(DefaultFieldsModel):
+    """
+    Base class for control + region relocation transfer models
+
+    Relocation transfers are used to record a retriable
+    state of a regional transfer for relocation data.
+    These models replace outbox based transfers.
+    """
+
+    __relocation_scope__ = RelocationScope.Excluded
+
+    relocation_uuid = UUIDField()
+    org_slug = models.CharField(null=False)
+    requesting_region = models.CharField(null=False)
+    exporting_region = models.CharField(null=False)
+    state = models.CharField(
+        choices=RelocationTransferState, default=RelocationTransferState.Request
+    )
+    scheduled_for = models.DateTimeField(null=True, default=timezone.now)
+    public_key = models.BinaryField(null=True)
+
+    class Meta:
+        abstract = True
+
+
+@control_silo_model
+class ControlRelocationTransfer(BaseRelocationTransfer):
+    __relocation_scope__ = RelocationScope.Excluded
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_controlrelocationtransfer"
+
+
+@region_silo_model
+class RegionRelocationTransfer(BaseRelocationTransfer):
+    __relocation_scope__ = RelocationScope.Excluded
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_regionrelocationtransfer"
