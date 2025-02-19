32d0ae24863528cc64d9a0fb4e2879b49d1e0547
Riccardo Busetti <riccardob36@gmail.com>
2025-02-19 14:34:24 +0100

    Add more spans

--- src/sentry/debug_files/upload.py
+++ src/sentry/debug_files/upload.py
@@ -1,6 +1,7 @@
 from collections.abc import Sequence
 from datetime import timedelta
 
+import sentry_sdk
 from django.utils import timezone
 
 from sentry.models.files import FileBlob, FileBlobOwner
@@ -9,15 +10,23 @@ from sentry.models.files import FileBlob, FileBlobOwner
 def find_missing_chunks(organization_id: int, chunks: Sequence[str]):
     """Returns a list of chunks which are missing for an org."""
     # Refresh the timestamp of all files we are interested in, so they don't disappear from under us.
-    now = timezone.now()
-    threshold = now - timedelta(hours=12)
-    FileBlob.objects.filter(checksum__in=chunks, timestamp__lte=threshold).update(timestamp=now)
+    with sentry_sdk.start_span(op="find_missing_chunks") as span:
+        span.set_data("chunks_size", len(chunks))
 
-    # Compute the set of all existing chunks.
-    owned = set(
-        FileBlobOwner.objects.filter(
-            blob__checksum__in=chunks, organization_id=organization_id
-        ).values_list("blob__checksum", flat=True)
-    )
+        now = timezone.now()
+        threshold = now - timedelta(hours=12)
+
+        with sentry_sdk.start_span(op="find_missing_chunks.update_timestamp"):
+            FileBlob.objects.filter(checksum__in=chunks, timestamp__lte=threshold).update(
+                timestamp=now
+            )
+
+        # Compute the set of all existing chunks.
+        with sentry_sdk.start_span(op="find_missing_chunks.get_owned_chunks"):
+            owned = set(
+                FileBlobOwner.objects.filter(
+                    blob__checksum__in=chunks, organization_id=organization_id
+                ).values_list("blob__checksum", flat=True)
+            )
 
     return list(set(chunks) - owned)
