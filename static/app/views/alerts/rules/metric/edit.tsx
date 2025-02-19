77a0a1e67e56c2dd70da6286ea81ac0a4619e31d
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-02-19 10:31:59 -0500

    chore(metrics): Use a hook across the app for metric rules (#85377)
    Adds a shared hook for metric rules and uses it across the app. Will be
    used in some of my upcoming metric charts changes.

--- static/app/views/alerts/rules/metric/edit.tsx
+++ static/app/views/alerts/rules/metric/edit.tsx
@@ -9,12 +9,11 @@ import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import type {Organization} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
 import {metric} from 'sentry/utils/analytics';
-import {useApiQuery} from 'sentry/utils/queryClient';
 import normalizeUrl from 'sentry/utils/url/normalizeUrl';
 import {useNavigate} from 'sentry/utils/useNavigate';
 import {makeAlertsPathname} from 'sentry/views/alerts/pathnames';
 import RuleForm from 'sentry/views/alerts/rules/metric/ruleForm';
-import type {MetricRule} from 'sentry/views/alerts/rules/metric/types';
+import {useMetricRule} from 'sentry/views/alerts/rules/metric/utils/useMetricRule';
 
 type RouteParams = {
   projectId: string;
@@ -43,13 +42,12 @@ export function MetricRulesEdit({
     isError,
     data: rule,
     error,
-  } = useApiQuery<MetricRule>(
-    [`/organizations/${organization.slug}/alert-rules/${params.ruleId}/`],
+  } = useMetricRule(
     {
-      staleTime: 0,
-      retry: false,
-      refetchOnMount: true,
-    }
+      orgSlug: organization.slug,
+      ruleId: params.ruleId,
+    },
+    {refetchOnMount: true}
   );
 
   useEffect(() => {
