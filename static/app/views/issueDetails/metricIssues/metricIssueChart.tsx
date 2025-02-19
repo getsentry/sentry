f6cb084e7d1645ca3b93af6622f806c9c3907f77
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-02-19 11:10:18 -0500

    ref(metrics): Convert more shared code for metric issue details to hooks (#85385)
    Requires https://github.com/getsentry/sentry/pull/85377
    
    Adds a util file to a new directory to pull out some shared logic for
    the correlated section and the metrics graph. Will also make it a bit
    easier to read through both files.
    
    For the new `useMetricAlertId` hook, it mimics a pattern for cron/uptime
    ([example](https://github.com/getsentry/sentry/blob/cdd59af574fedd32005429827aefc06cdceab811/static/app/views/issueDetails/streamline/issueCronCheckTimeline.tsx#L36))
    where by we fetch any event in 90d to have the best shot of finding a
    `alert_rule_id` ctx key since they are identical across all events. That
    way, even if a user picks small date range, or bad query, we don't
    unload the graph when the event isn't found.
    
    All this refactoring is to make it more reviewable as I take a look at
    changing the graph design

--- static/app/views/issueDetails/metricIssues/metricIssueChart.tsx
+++ static/app/views/issueDetails/metricIssues/metricIssueChart.tsx
@@ -1,46 +1,36 @@
-import {Fragment, lazy, useMemo} from 'react';
+import {lazy} from 'react';
 import {useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
-import moment from 'moment-timezone';
 
-import {DateTime} from 'sentry/components/dateTime';
 import LazyLoad from 'sentry/components/lazyLoad';
-import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
-import type {Event} from 'sentry/types/event';
 import type {Group} from 'sentry/types/group';
 import type {Project} from 'sentry/types/project';
 import useApi from 'sentry/utils/useApi';
 import useOrganization from 'sentry/utils/useOrganization';
-import type {TimePeriodType} from 'sentry/views/alerts/rules/metric/details/constants';
 import {
   getFilter,
   getPeriodInterval,
 } from 'sentry/views/alerts/rules/metric/details/utils';
-import {Dataset, TimePeriod} from 'sentry/views/alerts/rules/metric/types';
+import {Dataset} from 'sentry/views/alerts/rules/metric/types';
 import {extractEventTypeFilterFromRule} from 'sentry/views/alerts/rules/metric/utils/getEventTypeFilter';
 import {isCrashFreeAlert} from 'sentry/views/alerts/rules/metric/utils/isCrashFreeAlert';
 import {useMetricRule} from 'sentry/views/alerts/rules/metric/utils/useMetricRule';
+import {
+  useMetricIssueAlertId,
+  useMetricTimePeriod,
+} from 'sentry/views/issueDetails/metricIssues/utils';
 
 const MetricChart = lazy(
   () => import('sentry/views/alerts/rules/metric/details/metricChart')
 );
 
-export function MetricIssueChart({
-  event,
-  group,
-  project,
-}: {
-  event: Event | undefined;
-  group: Group;
-  project: Project;
-}) {
+export function MetricIssuesChart({group, project}: {group: Group; project: Project}) {
   const theme = useTheme();
   const api = useApi();
   const organization = useOrganization();
 
-  const ruleId = event?.contexts?.metric_alert?.alert_rule_id;
-
+  const ruleId = useMetricIssueAlertId({groupId: group.id});
   const {data: rule} = useMetricRule(
     {
       orgSlug: organization.slug,
@@ -55,33 +45,7 @@ export function MetricIssueChart({
       enabled: !!ruleId,
     }
   );
-
-  const openPeriod = group.openPeriods?.[0];
-  const timePeriod = useMemo((): TimePeriodType | null => {
-    if (!openPeriod) {
-      return null;
-    }
-    const start = openPeriod.start;
-    let end = openPeriod.end;
-    if (!end) {
-      end = new Date().toISOString();
-    }
-    return {
-      start,
-      end,
-      period: TimePeriod.SEVEN_DAYS,
-      usingPeriod: false,
-      label: t('Custom time'),
-      display: (
-        <Fragment>
-          <DateTime date={moment.utc(start)} />
-          {' — '}
-          <DateTime date={moment.utc(end)} />
-        </Fragment>
-      ),
-      custom: true,
-    };
-  }, [openPeriod]);
+  const timePeriod = useMetricTimePeriod({openPeriod: group.openPeriods?.[0]});
 
   if (!rule || !timePeriod) {
     return null;
