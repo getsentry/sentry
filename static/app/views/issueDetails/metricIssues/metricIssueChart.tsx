707890d58d353d1d40a176d8a79702debda0a8d5
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-02-20 14:52:29 -0500

    ✨ New graph for metric issues

--- static/app/views/issueDetails/metricIssues/metricIssueChart.tsx
+++ static/app/views/issueDetails/metricIssues/metricIssueChart.tsx
@@ -1,34 +1,35 @@
-import {lazy} from 'react';
-import {useTheme} from '@emotion/react';
+import {useMemo} from 'react';
 import styled from '@emotion/styled';
 
-import LazyLoad from 'sentry/components/lazyLoad';
+import {AreaChart} from 'sentry/components/charts/areaChart';
+import {defaultFormatAxisLabel} from 'sentry/components/charts/components/tooltip';
+import {useChartZoom} from 'sentry/components/charts/useChartZoom';
 import {space} from 'sentry/styles/space';
 import type {Group} from 'sentry/types/group';
 import type {Project} from 'sentry/types/project';
+import {formatAbbreviatedNumber} from 'sentry/utils/formatters';
 import useOrganization from 'sentry/utils/useOrganization';
-import {
-  getFilter,
-  getPeriodInterval,
-} from 'sentry/views/alerts/rules/metric/details/utils';
-import {Dataset} from 'sentry/views/alerts/rules/metric/types';
-import {extractEventTypeFilterFromRule} from 'sentry/views/alerts/rules/metric/utils/getEventTypeFilter';
-import {isCrashFreeAlert} from 'sentry/views/alerts/rules/metric/utils/isCrashFreeAlert';
+import type {TimePeriodType} from 'sentry/views/alerts/rules/metric/details/constants';
+import type {MetricRule} from 'sentry/views/alerts/rules/metric/types';
 import {useMetricRule} from 'sentry/views/alerts/rules/metric/utils/useMetricRule';
+import type {Anomaly, Incident} from 'sentry/views/alerts/types';
+import {useMetricAnomalies} from 'sentry/views/issueDetails/metricIssues/useMetricAnomalies';
+import {useMetricIncidents} from 'sentry/views/issueDetails/metricIssues/useMetricIncidents';
+import {useMetricStatsChart} from 'sentry/views/issueDetails/metricIssues/useMetricStatsChart';
 import {
   useMetricIssueAlertId,
   useMetricTimePeriod,
 } from 'sentry/views/issueDetails/metricIssues/utils';
 
-const MetricChart = lazy(
-  () => import('sentry/views/alerts/rules/metric/details/metricChart')
-);
+interface MetricIssueChartProps {
+  group: Group;
+  project: Project;
+}
 
-export function MetricIssuesChart({group, project}: {group: Group; project: Project}) {
-  const theme = useTheme();
+export function MetricIssueChart({group, project}: MetricIssueChartProps) {
   const organization = useOrganization();
-
   const ruleId = useMetricIssueAlertId({groupId: group.id});
+  const timePeriod = useMetricTimePeriod();
   const {data: rule} = useMetricRule(
     {
       orgSlug: organization.slug,
@@ -43,34 +44,123 @@ export function MetricIssuesChart({group, project}: {group: Group; project: Proj
       enabled: !!ruleId,
     }
   );
-  const timePeriod = useMetricTimePeriod({openPeriod: group.openPeriods?.[0]});
+  const {data: anomalies = []} = useMetricAnomalies(
+    {
+      orgSlug: organization.slug,
+      ruleId: ruleId ?? '',
+      query: {
+        start: timePeriod.start,
+        end: timePeriod.end,
+      },
+    },
+    {
+      enabled:
+        !!ruleId && organization.features.includes('anomaly-detection-alerts-charts'),
+    }
+  );
+  const {data: rawIncidents = []} = useMetricIncidents(
+    {
+      orgSlug: organization.slug,
+      query: {
+        alertRule: ruleId ?? '',
+        start: timePeriod.start,
+        end: timePeriod.end,
+      },
+    },
+    {
+      enabled: !!ruleId,
+    }
+  );
+  const incidents = useMemo(() => {
+    return rawIncidents.map(incident => ({
+      ...incident,
+      // We do this to omit the label from the graph
+      identifier: '',
+    }));
+  }, [rawIncidents]);
 
-  if (!rule || !timePeriod) {
+  if (!rule) {
     return null;
   }
 
-  const {dataset, aggregate, query} = rule;
-  const formattedAggregate = aggregate;
-  const eventType = extractEventTypeFilterFromRule(rule);
-  const queryWithTypeFilter =
-    dataset === Dataset.EVENTS_ANALYTICS_PLATFORM
-      ? query
-      : (query ? `(${query}) AND (${eventType})` : eventType).trim();
-
   return (
     <MetricChartSection>
-      <LazyLoad
-        theme={theme}
-        LazyComponent={MetricChart}
+      <MetricIssueChartContent
         rule={rule}
         timePeriod={timePeriod}
         project={project}
-        interval={getPeriodInterval(timePeriod, rule)}
-        query={isCrashFreeAlert(dataset) ? query : queryWithTypeFilter}
-        filter={getFilter(rule)}
-        formattedAggregate={formattedAggregate}
+        anomalies={anomalies}
+        incidents={incidents}
       />
     </MetricChartSection>
+  );
+}
+
+/**
+ * This component is nested to avoid trying to fetch data without a rule or time period.
+ */
+function MetricIssueChartContent({
+  rule,
+  timePeriod,
+  project,
+  anomalies,
+  incidents,
+}: {
+  project: Project;
+  rule: MetricRule;
+  timePeriod: TimePeriodType;
+  anomalies?: Anomaly[];
+  incidents?: Incident[];
+}) {
+  const chartZoomProps = useChartZoom({saveOnZoom: true});
+  const {chartProps} = useMetricStatsChart({
+    project,
+    rule,
+    timePeriod,
+    anomalies,
+    incidents,
+    referrer: 'metric-issue-chart',
+  });
+
+  return (
+    <ChartContainer role="figure">
+      <AreaChart
+        series={[]}
+        {...chartProps}
+        height={100}
+        tooltip={{
+          formatAxisLabel: (
+            value,
+            isTimestamp,
+            utc,
+            showTimeInTooltip,
+            addSecondsToTimeFormat,
+            bucketSize,
+            _seriesParamsOrParam
+          ) =>
+            String(
+              defaultFormatAxisLabel(
+                value,
+                isTimestamp,
+                utc,
+                showTimeInTooltip,
+                addSecondsToTimeFormat,
+                bucketSize
+              )
+            ),
+        }}
+        yAxis={{
+          splitNumber: 2,
+          minInterval: 1,
+          axisLabel: {
+            formatter: (value: number) => {
+              return formatAbbreviatedNumber(value);
+            },
+          },
+        }}
+        {...chartZoomProps}
+      />
+    </ChartContainer>
   );
 }
 
@@ -78,4 +168,9 @@ const MetricChartSection = styled('div')`
   display: block;
   padding-right: ${space(1.5)};
   width: 100%;
+`;
+
+const ChartContainer = styled('div')`
+  position: relative;
+  padding: ${space(0.75)} ${space(1)} ${space(0.75)} 0;
 `;
---
+++ static/app/views/issueDetails/metricIssues/useMetricStatsChart.tsx
@@ -0,0 +1,100 @@
+import type {AreaChartProps} from 'sentry/components/charts/areaChart';
+import {transformTimeseriesData} from 'sentry/components/charts/eventsRequest';
+import type {Series} from 'sentry/types/echarts';
+import type {SessionApiResponse} from 'sentry/types/organization';
+import type {EventsStats} from 'sentry/types/organization';
+import type {Project} from 'sentry/types/project';
+import type {UseApiQueryOptions, UseApiQueryResult} from 'sentry/utils/queryClient';
+import type RequestError from 'sentry/utils/requestError/requestError';
+import {MINUTES_THRESHOLD_TO_DISPLAY_SECONDS} from 'sentry/utils/sessions';
+import type {TimePeriodType} from 'sentry/views/alerts/rules/metric/details/constants';
+import {
+  getMetricAlertChartOption,
+  transformSessionResponseToSeries,
+} from 'sentry/views/alerts/rules/metric/details/metricChartOption';
+import type {MetricRule} from 'sentry/views/alerts/rules/metric/types';
+import {isCrashFreeAlert} from 'sentry/views/alerts/rules/metric/utils/isCrashFreeAlert';
+import type {Anomaly, Incident} from 'sentry/views/alerts/types';
+import {useMetricEventStats} from 'sentry/views/issueDetails/metricIssues/useMetricEventStats';
+import {useMetricSessionStats} from 'sentry/views/issueDetails/metricIssues/useMetricSessionStats';
+
+interface MetricStatsParams {
+  project: Project;
+  referrer: string;
+  rule: MetricRule;
+  timePeriod: TimePeriodType;
+  anomalies?: Anomaly[];
+  incidents?: Incident[];
+}
+type MetricStatsResponse = SessionApiResponse | EventsStats | undefined;
+
+interface MetricStatsResult {
+  chartProps: Partial<AreaChartProps>;
+  data: UseApiQueryResult<MetricStatsResponse, RequestError>['data'];
+  resultType: 'sessions' | 'events';
+  queryResult?: Partial<UseApiQueryResult<MetricStatsResponse, RequestError>>;
+}
+
+/**
+ * Helper hook to coerce any rule into returning a series data response, whether it is a session or event rule.
+ * Returns a similar response to a useApiQuery hook,
+ */
+export function useMetricStatsChart(
+  {
+    project,
+    rule,
+    timePeriod,
+    referrer,
+    anomalies = [],
+    incidents = [],
+  }: MetricStatsParams,
+  options: Partial<UseApiQueryOptions<MetricStatsResponse>> = {}
+): MetricStatsResult {
+  const shouldUseSessionsStats = isCrashFreeAlert(rule.dataset);
+  const {data: sessionStats, ...sessionStatsResults} = useMetricSessionStats(
+    {project, rule, timePeriod},
+    {
+      enabled: shouldUseSessionsStats,
+      ...(options as Partial<UseApiQueryOptions<SessionApiResponse>>),
+    }
+  );
+  const {data: eventStats, ...eventStatsResults} = useMetricEventStats(
+    {project, rule, timePeriod, referrer},
+    {
+      enabled: !shouldUseSessionsStats,
+      ...(options as Partial<UseApiQueryOptions<EventsStats>>),
+    }
+  );
+
+  let stats: Series[] = [];
+
+  if (shouldUseSessionsStats && sessionStats) {
+    stats = transformSessionResponseToSeries(sessionStats, rule);
+  } else if (eventStats) {
+    stats = transformTimeseriesData(eventStats.data, eventStats?.meta);
+  }
+
+  let chartProps: Partial<AreaChartProps> = {};
+  if (stats.length > 0) {
+    const {chartOption} = getMetricAlertChartOption({
+      timeseriesData: stats,
+      rule,
+      anomalies,
+      incidents,
+      seriesName: rule.aggregate,
+    });
+    chartProps = {...chartOption};
+  }
+
+  return {
+    chartProps: {
+      minutesThresholdToDisplaySeconds: shouldUseSessionsStats
+        ? MINUTES_THRESHOLD_TO_DISPLAY_SECONDS
+        : undefined,
+      ...chartProps,
+    },
+    resultType: shouldUseSessionsStats ? 'sessions' : 'events',
+    data: shouldUseSessionsStats ? sessionStats : eventStats,
+    queryResult: shouldUseSessionsStats ? sessionStatsResults : eventStatsResults,
+  };
+}
