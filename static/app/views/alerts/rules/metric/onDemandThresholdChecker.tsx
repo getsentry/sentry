25b211a517a61fc3bd69a2684f04fdbbc4ea55a8
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-02-20 11:07:31 +0100

    update endpoint

--- static/app/views/alerts/rules/metric/onDemandThresholdChecker.tsx
+++ static/app/views/alerts/rules/metric/onDemandThresholdChecker.tsx
@@ -1,19 +1,11 @@
 import {Fragment} from 'react';
 
 import type {Project} from 'sentry/types/project';
-import {defined} from 'sentry/utils';
 import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
-import {isOnDemandMetricAlert} from 'sentry/views/alerts/rules/metric/utils/onDemandMetricAlert';
-import {CombinedAlertType, type MetricAlert} from 'sentry/views/alerts/types';
-
-// The maximum number of on-demand metric alerts that can be created per project.
-// This value is defined in Sentry's options in the following file:
-// https://github.com/getsentry/sentry/blob/cac47187ae98f105b39edf80a0fd3105c95e1cb5/src/sentry/options/defaults.py#L2215-L2219
-const MAX_ON_DEMAND_METRIC_ALERTS = 50;
 
 interface OnDemandThresholdCheckerProps {
-  children: (props: {isOnDemandLimitReached: boolean}) => React.ReactNode;
+  children: (props: {isOnDemandLimitReached: boolean | undefined}) => React.ReactNode;
   isExtrapolatedChartData: boolean;
   projectId: Project['id'];
 }
@@ -25,13 +17,12 @@ export function OnDemandThresholdChecker({
 }: OnDemandThresholdCheckerProps) {
   const organization = useOrganization();
 
-  const {data} = useApiQuery<Array<MetricAlert | null>>(
+  const {data} = useApiQuery<{max_allowed: number; total_on_demand_alert_specs: number}>(
     [
-      `/organizations/${organization.slug}/combined-rules/`,
+      `/organizations/${organization.slug}/ondemand-rules/`,
       {
         query: {
           project: projectId,
-          alert_type: CombinedAlertType.METRIC,
         },
       },
     ],
@@ -41,12 +32,8 @@ export function OnDemandThresholdChecker({
     }
   );
 
-  const onDemandMetricAlerts = (data ?? [])
-    .filter(defined)
-    .filter(alert => isOnDemandMetricAlert(alert.dataset, alert.aggregate, alert.query));
-
   const isOnDemandLimitReached =
-    onDemandMetricAlerts.length >= MAX_ON_DEMAND_METRIC_ALERTS;
+    data === undefined ? undefined : data.total_on_demand_alert_specs >= data.max_allowed;
 
   return <Fragment>{children({isOnDemandLimitReached})}</Fragment>;
 }
