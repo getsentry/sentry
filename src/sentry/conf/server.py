2a4c500bc176746534cfb735b449eac909d62eb7
Sebastian Zivota <loewenheim@users.noreply.github.com>
2025-02-20 16:21:08 +0100

    fix(symbolication): Temporarily disable Intel symbol source (#85526)
    Right now Symbolicator is not able to successfully download from the
    Intel source because the source doesn't accept custom user agents. Until
    we are confident we can spoof Symbolicator's user agent without abusing
    the source, we are disabling it. See
    https://github.com/getsentry/team-ingest/issues/642.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -2747,15 +2747,21 @@ SENTRY_BUILTIN_SOURCES = {
         "url": "http://ctxsym.citrix.com/symbols/",
         "is_public": True,
     },
-    "intel": {
-        "type": "http",
-        "id": "sentry:intel",
-        "name": "Intel",
-        "layout": {"type": "symstore"},
-        "filters": {"filetypes": ["pe", "pdb"]},
-        "url": "https://software.intel.com/sites/downloads/symbols/",
-        "is_public": True,
-    },
+    # Right now Symbolicator is not able to successfully download from
+    # the Intel source because the source doesn't accept custom user agents.
+    # Until we are confident we can spoof Symbolicator's user agent without
+    # abusing the source, we are disabling it. See
+    # https://github.com/getsentry/team-ingest/issues/642.
+    #
+    # "intel": {
+    #     "type": "http",
+    #     "id": "sentry:intel",
+    #     "name": "Intel",
+    #     "layout": {"type": "symstore"},
+    #     "filters": {"filetypes": ["pe", "pdb"]},
+    #     "url": "https://software.intel.com/sites/downloads/symbols/",
+    #     "is_public": True,
+    # },
     "amd": {
         "type": "http",
         "id": "sentry:amd",
