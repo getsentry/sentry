34fab6bc7ce9039fa1451f178d64d579dfdd22e1
Riccardo Busetti <riccardo.busetti@sentry.io>
2025-02-20 14:48:51 +0100

    feat(assemble): Implement new experimental missing chunks algorithm (#85528)

--- tests/sentry/api/endpoints/test_organization_artifactbundle_assemble.py
+++ tests/sentry/api/endpoints/test_organization_artifactbundle_assemble.py
@@ -12,6 +12,7 @@ from sentry.models.orgauthtoken import OrgAuthToken
 from sentry.silo.base import SiloMode
 from sentry.tasks.assemble import ChunkFileState, assemble_artifacts
 from sentry.testutils.cases import APITestCase
+from sentry.testutils.helpers import with_feature
 from sentry.testutils.outbox import outbox_runner
 from sentry.testutils.silo import assume_test_silo_mode
 from sentry.utils.security.orgauthtoken_token import generate_token, hash_token
@@ -28,6 +29,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
             args=[self.organization.slug],
         )
 
+    @with_feature("organizations:find-missing-chunks-new")
     def test_assemble_json_schema(self):
         response = self.client.post(
             self.url, data={"lol": "test"}, HTTP_AUTHORIZATION=f"Bearer {self.token.token}"
@@ -116,6 +118,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
         assert response.status_code == 200, response.content
         assert response.data["state"] == ChunkFileState.NOT_FOUND
 
+    @with_feature("organizations:find-missing-chunks-new")
     def test_assemble_with_invalid_projects(self):
         bundle_file = self.create_artifact_bundle_zip(
             org=self.organization.slug, release=self.release.version
@@ -141,6 +144,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
         assert response.status_code == 400, response.content
         assert response.data["error"] == "One or more projects are invalid"
 
+    @with_feature("organizations:find-missing-chunks-new")
     def test_assemble_with_valid_project_slugs(self):
         # Test with all valid project slugs
         valid_project = self.create_project()
@@ -166,6 +170,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
 
         self.assertEqual(response.status_code, 200)
 
+    @with_feature("organizations:find-missing-chunks-new")
     def test_assemble_with_valid_project_ids(self):
         # Test with all valid project IDs
         valid_project = self.create_project()
@@ -191,6 +196,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
 
         self.assertEqual(response.status_code, 200)
 
+    @with_feature("organizations:find-missing-chunks-new")
     def test_assemble_with_mix_of_slugs_and_ids(self):
         # Test with a mix of valid project slugs and IDs
         valid_project = self.create_project()
@@ -221,6 +227,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
 
         self.assertEqual(response.status_code, 200)
 
+    @with_feature("organizations:find-missing-chunks-new")
     @patch("sentry.tasks.assemble.assemble_artifacts")
     def test_assemble_without_version_and_dist(self, mock_assemble_artifacts):
         bundle_file = self.create_artifact_bundle_zip(
@@ -257,6 +264,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
             }
         )
 
+    @with_feature("organizations:find-missing-chunks-new")
     @patch("sentry.tasks.assemble.assemble_artifacts")
     def test_assemble_with_version_and_no_dist(self, mock_assemble_artifacts):
         bundle_file = self.create_artifact_bundle_zip(
@@ -294,6 +302,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
             }
         )
 
+    @with_feature("organizations:find-missing-chunks-new")
     @patch("sentry.tasks.assemble.assemble_artifacts")
     def test_assemble_with_version_and_dist(self, mock_assemble_artifacts):
         dist = "android"
@@ -333,6 +342,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
             }
         )
 
+    @with_feature("organizations:find-missing-chunks-new")
     def test_assemble_with_missing_chunks(self):
         dist = "android"
         bundle_file = self.create_artifact_bundle_zip(
@@ -377,6 +387,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
         assert response.status_code == 200, response.content
         assert response.data["state"] == ChunkFileState.CREATED
 
+    @with_feature("organizations:find-missing-chunks-new")
     def test_assemble_response(self):
         bundle_file = self.create_artifact_bundle_zip(
             org=self.organization.slug, release=self.release.version
@@ -406,6 +417,7 @@ class OrganizationArtifactBundleAssembleTest(APITestCase):
         assert response.status_code == 200, response.content
         assert response.data["state"] == ChunkFileState.CREATED
 
+    @with_feature("organizations:find-missing-chunks-new")
     def test_assemble_org_auth_token(self):
         org2 = self.create_organization(owner=self.user)
 
