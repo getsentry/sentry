d01375470944b9abc844594cdb9a651d9022c259
Colton Allen <colton.allen@sentry.io>
2025-02-20 11:25:10 -0600

    feat(flags): Upgrade SDK and collect options (#85146)
    A previous iteration of this change failed because we weren't on a
    recent enough version of the sentry-sdk. With 2.21 released this
    shouldn't have any issues.

--- src/sentry/utils/flag.py
+++ src/sentry/utils/flag.py
@@ -1,26 +1,13 @@
-import logging
+from typing import Any
 
 import sentry_sdk
-from sentry_sdk.integrations import Integration
-
-logger = logging.getLogger()
 
 
-class FlagPoleIntegration(Integration):
-    identifier = "flag_pole"
-
-    @staticmethod
-    def setup_once():
-        scope = sentry_sdk.get_current_scope()
-        scope.add_error_processor(flag_error_processor)
+def record_feature_flag(name: str, result: Any) -> None:
+    if isinstance(result, bool):
+        sentry_sdk.feature_flags.add_feature_flag("feature." + name, result)
 
 
-def flag_error_processor(event, exc_info):
-    scope = sentry_sdk.get_current_scope()
-    event["contexts"]["flags"] = {"values": scope.flags.get()}
-    return event
-
-
-def flag_pole_hook(flag: str, result: bool):
-    flags = sentry_sdk.get_current_scope().flags
-    flags.set(flag, result)
+def record_option(name: str, result: Any) -> None:
+    if isinstance(result, bool):
+        sentry_sdk.feature_flags.add_feature_flag("option." + name, result)
