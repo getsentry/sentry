f1c8242aaffdb2dfb2a6272e93808c9ca638879e
Katie Byers <lobsterkatie@gmail.com>
2025-02-20 09:42:59 -0800

    add docstring to `parse_fingerprint_entry_as_variable`

--- src/sentry/grouping/utils.py
+++ src/sentry/grouping/utils.py
@@ -22,6 +22,11 @@ DEFAULT_FINGERPRINT_VARIABLE = "{{ default }}"
 
 
 def parse_fingerprint_entry_as_variable(value: str) -> str | None:
+    """
+    Determine if the given fingerprint entry is a variable, and if it is, return its key (that is,
+    extract the variable name from a variable string of the form "{{ var_name }}"). If the given
+    entry isn't the correct form to be a variable, return None.
+    """
     match = _fingerprint_var_re.match(value)
     if match is not None and match.end() == len(value):
         return match.group(1)
