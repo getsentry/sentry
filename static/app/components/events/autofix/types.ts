d56debf7e362f6f8e6905422f78a574223a64070
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2025-02-20 12:04:49 -0800

    ref(autofix): Solution timeline event alignment (#85579)
    Aligns the frontend solution type with what we're gonna be sending from
    the backend, and makes some boolean fields optional for good practice.
    
    This will make bolding not backwards compatible but I guess it makes the
    code much cleaner without needing the array map anymore

--- static/app/components/events/autofix/types.ts
+++ static/app/components/events/autofix/types.ts
@@ -171,18 +171,18 @@ export type AutofixRelevantCodeFile = {
 
 export type AutofixTimelineEvent = {
   code_snippet_and_analysis: string;
-  is_most_important_event: boolean;
   relevant_code_file: AutofixRelevantCodeFile;
   timeline_item_type: 'internal_code' | 'external_system' | 'human_action';
   title: string;
+  is_most_important_event?: boolean;
 };
 
 export type AutofixSolutionTimelineEvent = {
   code_snippet_and_analysis: string;
-  is_new_event: boolean;
   relevant_code_file: AutofixRelevantCodeFile;
-  timeline_item_type: 'internal_code' | 'external_system' | 'human_action';
+  timeline_item_type: 'internal_code';
   title: string;
+  is_most_important_event?: boolean;
 };
 
 export type AutofixRootCauseData = {
