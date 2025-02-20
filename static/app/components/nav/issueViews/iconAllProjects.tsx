13d6e3705684722495ead5451a92d5ca0c517811
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2025-02-20 09:29:32 -0800

    fix(issue-views): Fix all projects dark mode (#85549)
    before: 
    <img width="89" alt="image"
    src="https://github.com/user-attachments/assets/1908140d-b18c-4972-8ca5-d9ec56ea1f31"
    />
    
    after: 
    <img width="77" alt="image"
    src="https://github.com/user-attachments/assets/064b2148-ddbd-4f44-bc9f-1da7b1f8edc5"
    />

--- static/app/components/nav/issueViews/iconAllProjects.tsx
+++ static/app/components/nav/issueViews/iconAllProjects.tsx
@@ -1,4 +1,8 @@
+import {useTheme} from '@emotion/react';
+
 export function IconAllProjects() {
+  const theme = useTheme();
+
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -7,24 +11,24 @@ export function IconAllProjects() {
       viewBox="0 0 18 18"
       fill="none"
     >
-      <rect width="13" height="13" rx="2" fill="#E0DCE5" />
+      <rect width="13" height="13" rx="2" fill={theme.gray200} />
       <rect
         x="0.5"
         y="0.5"
         width="12"
         height="12"
         rx="1.5"
-        stroke="#3A115F"
+        stroke={theme.gray400}
         strokeOpacity="0.14"
       />
-      <rect x="4" y="4" width="13" height="13" rx="2" fill="#E0DCE5" />
+      <rect x="4" y="4" width="13" height="13" rx="2" fill={theme.gray200} />
       <rect
         x="4.5"
         y="4.5"
         width="12"
         height="12"
         rx="1.5"
-        stroke="#3A115F"
+        stroke={theme.gray400}
         strokeOpacity="0.14"
       />
     </svg>
