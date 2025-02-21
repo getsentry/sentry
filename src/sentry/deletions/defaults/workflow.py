4bb18a888740793c666168202b32961048d0142c
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-02-21 15:28:38 -0800

    only do 1 query to get the data condition group

--- src/sentry/deletions/defaults/workflow.py
+++ src/sentry/deletions/defaults/workflow.py
@@ -10,14 +10,13 @@ class WorkflowDeletionTask(ModelDeletionTask[Workflow]):
             workflowdataconditiongroup__workflow_id=instance.id
         ).values_list("id", flat=True)
 
-        action_ids = Action.objects.filter(
-            dataconditiongroupaction__condition_group_id__in=action_filter_ids
-        ).values_list("id", flat=True)
-
-        if action_ids:
-            model_relations.append(ModelRelation(Action, {"id__in": action_ids}))
-
         if action_filter_ids:
+            model_relations.append(
+                ModelRelation(
+                    Action, {"dataconditiongroupaction__condition_group_id__in": action_filter_ids}
+                )
+            )
+
             model_relations.append(ModelRelation(DataConditionGroup, {"id__in": action_filter_ids}))
 
         if instance.when_condition_group:
