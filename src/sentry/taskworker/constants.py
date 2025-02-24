10a0dea41e438ecaba3e5dd8b902b18bf3e73b37
Enoch Tang <enoch.tang@sentry.io>
2025-02-24 14:41:20 -0500

    increase default processing deadline duration

--- src/sentry/taskworker/constants.py
+++ src/sentry/taskworker/constants.py
@@ -1,4 +1,4 @@
-DEFAULT_PROCESSING_DEADLINE = 5
+DEFAULT_PROCESSING_DEADLINE = 30
 """
 The fallback/default processing_deadline that tasks
 will use if neither the TaskNamespace or Task define a deadline
