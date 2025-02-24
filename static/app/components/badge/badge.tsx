ea32c34886802c3cc0b8d9533c70e2e45289104a
Jonas <jonas.badalic@sentry.io>
2025-02-24 15:12:32 -0500

    components: remove deprecated badge export (#85745)
    Following https://github.com/getsentry/sentry/pull/85744, drop the
    deprecated export

--- static/app/components/badge/badge.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @deprecated use `import Badge from 'sentry/components/core/badge'` instead
- */
-// biome-ignore lint/performance/noBarrelFile: deprecated
-export * from 'sentry/components/core/badge';
-/**
- * @deprecated use `import Badge from 'sentry/components/core/badge'` instead
- */
-export {default} from 'sentry/components/core/badge';
