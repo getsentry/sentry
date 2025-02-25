0dd7ba360e72c0fdcabd419ff885e4af68ce57df
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2025-02-25 09:35:12 -0800

    feat(issue-details): Filter activity drawer for comments  (#85491)
    this pr adds the option to filter activity for comments. in the drawer,
    you can click the chat icon to filter to only show comments. if you
    click the icon from the sidebar, then it will show the comments by
    default (with the option to turn the filter off)

--- static/app/views/issueDetails/streamline/hooks/useIssueActivityDrawer.tsx
+++ static/app/views/issueDetails/streamline/hooks/useIssueActivityDrawer.tsx
@@ -28,12 +28,16 @@ export function useIssueActivityDrawer({group, project}: UseIssueActivityDrawerP
         navigate(
           {
             pathname: baseUrl,
-            query: location.query,
+            query: {
+              ...location.query,
+              filter: undefined,
+            },
           },
           {replace: true}
         );
       },
       transitionProps: {stiffness: 1000},
+      shouldCloseOnLocationChange: () => false,
     });
   }, [openDrawer, baseUrl, navigate, location.query, group, project]);
 
