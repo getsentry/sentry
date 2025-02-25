da67311d9d704d2a95f336920f866f816003e5f8
TkDodo <dominik.dorfmeister@sentry.io>
2025-02-25 11:42:44 +0100

    ref: restore ol exports for getsentry

---
+++ static/app/components/input.tsx
@@ -0,0 +1,5 @@
+/**
+ * @deprecated Import from `sentry/components/core/input` instead.
+ */
+// biome-ignore lint/performance/noBarrelFile: Remove this once imports are fixed
+export * from 'sentry/components/core/input';
