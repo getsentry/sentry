c4048bd3edd35906550c9651a36bb26c1b0865af
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-02-25 20:08:18 +0100

    revert

--- src/sentry/spans/buffer_v2.py
+++ src/sentry/spans/buffer_v2.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-from collections import defaultdict
 from collections.abc import Collection, Sequence
 from functools import partial
 from typing import NamedTuple
@@ -46,82 +45,45 @@ class RedisSpansBufferV2:
     def _segment_id(project_id: int, trace_id: str, span_id: str) -> SegmentId:
         return f"span-buf:s:{{{project_id}:{trace_id}}}:{span_id}".encode("ascii")
 
-    def _queue_key(self, trace_id: str) -> str:
-        shard = int(trace_id, 16) % self.sharding_factor
-        return f"span-buf:q:{shard}"
-
     def _is_root_span(self, span: Span) -> bool:
         return span.parent_span_id is None or span.is_segment_span
 
     def process_spans(self, spans: Sequence[Span], now: int):
-        pipelines_to_execute = []
-
-        if isinstance(self.client, RedisCluster):
-            # we're hacking around the fact that certain multi-key commands
-            # like sunionstore do not work on clustered pipelines
-            # (self.client.pipeline()). even though we do talk to nodes
-            # directly, this code was written with the intent that cluster
-            # resizing still works as expected, and that there are no keys that
-            # overlap in name across nodes (unlike some of our redis-blaster
-            # based services that also talk to nodes directly)
-            pipelines_by_key = {}  # mapping from redis key to pipeline obj
-            pipelines_by_node = (
-                {}
-            )  # mapping from node to pipeline obj, to reuse pipelines across trace_ids if they go to the same node
-
+        with self.client.pipeline(transaction=False) as p:
             for span in spans:
-                for key in [
-                    self._segment_id(span.project_id, span.trace_id, span.span_id),
-                    self._queue_key(span.trace_id),
-                ]:
-                    node = self.client.get_node_from_key(key)
-                    if node not in pipelines_by_node:
-                        pipelines_by_node[node] = p = node.pipeline(transaction=False)
-                        pipelines_to_execute.append(p)
+                # (parent_span_id) -> [Span]
+                shard = int(span.trace_id, 16) % self.sharding_factor
+                queue_key = f"span-buf:q:{shard}"
+                _key = partial(self._segment_id, span.project_id, span.trace_id)
+                parent_span_id = span.parent_span_id or span.span_id
+                parent_key = _key(parent_span_id)
+                span_key = _key(span.span_id)
 
-                    pipelines_by_key[key] = pipelines_by_node[node]
-        else:
-            p = self.client.pipeline(transaction=False)
-            pipelines_by_key = defaultdict(lambda: p)
-            pipelines_to_execute.append(p)
+                is_root_span = self._is_root_span(span)
 
-        for span in spans:
-            # (parent_span_id) -> [Span]
-            queue_key = self._queue_key(span.trace_id)
-            _key = partial(self._segment_id, span.project_id, span.trace_id)
-            parent_span_id = span.parent_span_id or span.span_id
-            parent_key = _key(parent_span_id)
-            span_key = _key(span.span_id)
+                if not is_root_span:
+                    p.sunionstore(
+                        parent_key,
+                        parent_key,
+                        span_key,
+                    )
+                    p.delete(span_key)
+                    p.zrem(queue_key, span_key)
 
-            p = pipelines_by_key[span_key]
-            queue_p = pipelines_by_key[queue_key]
+                parent_span_id = span.parent_span_id or span.span_id
+                # TODO: shard this set?
+                # TODO: do we actually need set operations, maybe array is faster?
+                p.sadd(parent_key, span.payload)
+                p.expire(parent_key, self.max_timeout)
 
-            is_root_span = self._is_root_span(span)
+                if is_root_span:
+                    pq_timestamp = now + self.span_buffer_root_timeout
+                else:
+                    pq_timestamp = now + self.span_buffer_timeout
 
-            if not is_root_span:
-                p.sunionstore(
-                    parent_key,
-                    parent_key,
-                    span_key,
-                )
-                p.delete(span_key)
-                queue_p.zrem(queue_key, span_key)
+                p.zadd(queue_key, {parent_key: pq_timestamp})
+                p.expire(queue_key, self.max_timeout)
 
-            parent_span_id = span.parent_span_id or span.span_id
-            # TODO: shard this set?
-            # TODO: do we actually need set operations, maybe array is faster?
-            p.sadd(parent_key, span.payload)
-            p.expire(parent_key, self.max_timeout)
-
-            if is_root_span:
-                pq_timestamp = now + self.span_buffer_root_timeout
-            else:
-                pq_timestamp = now + self.span_buffer_timeout
-
-            queue_p.zadd(queue_key, {parent_key: pq_timestamp})
-            queue_p.expire(queue_key, self.max_timeout)
-
-        for p in pipelines_to_execute:
             p.execute()
 
     def flush_segments(self, now: int) -> dict[SegmentId, set[bytes]]:
