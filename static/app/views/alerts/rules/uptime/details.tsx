08f876bca8d872329210b65475c8c2406681c119
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-02-25 14:30:09 -0500

    ref(uptime): Move details sidebar to it's own component (#85885)

--- static/app/views/alerts/rules/uptime/details.tsx
+++ static/app/views/alerts/rules/uptime/details.tsx
@@ -2,29 +2,21 @@ import {useCallback, useState} from 'react';
 import styled from '@emotion/styled';
 
 import {updateUptimeRule} from 'sentry/actionCreators/uptime';
-import ActorAvatar from 'sentry/components/avatar/actorAvatar';
 import Breadcrumbs from 'sentry/components/breadcrumbs';
 import {LinkButton} from 'sentry/components/button';
 import ButtonBar from 'sentry/components/buttonBar';
-import {SectionHeading} from 'sentry/components/charts/styles';
-import {CodeSnippet} from 'sentry/components/codeSnippet';
 import {Alert} from 'sentry/components/core/alert';
 import IdBadge from 'sentry/components/idBadge';
-import {KeyValueTable, KeyValueTableRow} from 'sentry/components/keyValueTable';
 import * as Layout from 'sentry/components/layouts/thirds';
-import ExternalLink from 'sentry/components/links/externalLink';
 import LoadingError from 'sentry/components/loadingError';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
 import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
-import QuestionTooltip from 'sentry/components/questionTooltip';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
-import Text from 'sentry/components/text';
 import {IconEdit} from 'sentry/icons';
-import {t, tct} from 'sentry/locale';
+import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
-import getDuration from 'sentry/utils/duration/getDuration';
 import {
   type ApiQueryKey,
   setApiQueryData,
@@ -35,14 +27,13 @@ import useApi from 'sentry/utils/useApi';
 import useOrganization from 'sentry/utils/useOrganization';
 import useProjects from 'sentry/utils/useProjects';
 import {makeAlertsPathname} from 'sentry/views/alerts/pathnames';
-import {CheckIndicator} from 'sentry/views/alerts/rules/uptime/checkIndicator';
 import {
   CheckStatus,
   type CheckStatusBucket,
   type UptimeRule,
 } from 'sentry/views/alerts/rules/uptime/types';
-import {statusToText} from 'sentry/views/insights/uptime/timelineConfig';
 
+import {UptimeDetailsSidebar} from './detailsSidebar';
 import {DetailsTimeline} from './detailsTimeline';
 import {StatusToggleButton} from './statusToggleButton';
 import {UptimeChecksTable} from './uptimeChecksTable';
@@ -191,111 +182,10 @@ export default function UptimeAlertDetails({params}: UptimeAlertDetailsProps) {
           <UptimeChecksTable uptimeRule={uptimeRule} />
         </Layout.Main>
         <Layout.Side>
-          <MonitorUrlContainer>
-            <SectionHeading>{t('Checked URL')}</SectionHeading>
-            <CodeSnippet
-              hideCopyButton
-            >{`${uptimeRule.method} ${uptimeRule.url}`}</CodeSnippet>
-          </MonitorUrlContainer>
-          <SectionHeading>{t('Legend')}</SectionHeading>
-          <CheckLegend>
-            <CheckLegendItem>
-              <CheckIndicator status={CheckStatus.SUCCESS} />
-              <LegendText>
-                {statusToText[CheckStatus.SUCCESS]}
-                <QuestionTooltip
-                  isHoverable
-                  size="sm"
-                  title={tct(
-                    'A check status is considered uptime when it meets the uptime check criteria. [link:Learn more].',
-                    {
-                      link: (
-                        <ExternalLink href="https://docs.sentry.io/product/alerts/uptime-monitoring/#uptime-check-criteria" />
-                      ),
-                    }
-                  )}
-                />
-              </LegendText>
-            </CheckLegendItem>
-            <CheckLegendItem>
-              <CheckIndicator status={CheckStatus.FAILURE} />
-              <LegendText>
-                {statusToText[CheckStatus.FAILURE]}
-                <QuestionTooltip
-                  isHoverable
-                  size="sm"
-                  title={tct(
-                    'A check status is marked as intermittent when it fails but has not yet met the threshold to be considered downtime. [link:Learn more].',
-                    {
-                      link: (
-                        <ExternalLink href="https://docs.sentry.io/product/alerts/uptime-monitoring/#uptime-check-failures" />
-                      ),
-                    }
-                  )}
-                />
-              </LegendText>
-            </CheckLegendItem>
-            <CheckLegendItem>
-              <CheckIndicator status={CheckStatus.FAILURE_INCIDENT} />
-              <LegendText>
-                {statusToText[CheckStatus.FAILURE_INCIDENT]}
-                <QuestionTooltip
-                  isHoverable
-                  size="sm"
-                  title={tct(
-                    'A check status is considered downtime when it fails 3 consecutive times, meeting the downtime threshold. [link:Learn more].',
-                    {
-                      link: (
-                        <ExternalLink href="https://docs.sentry.io/product/alerts/uptime-monitoring/#uptime-check-failures" />
-                      ),
-                    }
-                  )}
-                />
-              </LegendText>
-            </CheckLegendItem>
-            {showMissedLegend && (
-              <CheckLegendItem>
-                <CheckIndicator status={CheckStatus.MISSED_WINDOW} />
-                <LegendText>
-                  {statusToText[CheckStatus.MISSED_WINDOW]}
-                  <QuestionTooltip
-                    isHoverable
-                    size="sm"
-                    title={tct(
-                      'A check status is unknown when Sentry is unable to execute an uptime check at the scheduled time. [link:Learn more].',
-                      {
-                        link: (
-                          <ExternalLink href="https://docs.sentry.io/product/alerts/uptime-monitoring/#uptime-check-failures" />
-                        ),
-                      }
-                    )}
-                  />
-                </LegendText>
-              </CheckLegendItem>
-            )}
-          </CheckLegend>
-          <SectionHeading>{t('Configuration')}</SectionHeading>
-          <KeyValueTable>
-            <KeyValueTableRow
-              keyName={t('Check Interval')}
-              value={t('Every %s', getDuration(uptimeRule.intervalSeconds))}
-            />
-            <KeyValueTableRow
-              keyName={t('Timeout')}
-              value={t('After %s', getDuration(uptimeRule.timeoutMs / 1000, 2))}
-            />
-            <KeyValueTableRow keyName={t('Environment')} value={uptimeRule.environment} />
-            <KeyValueTableRow
-              keyName={t('Owner')}
-              value={
-                uptimeRule.owner ? (
-                  <ActorAvatar actor={uptimeRule.owner} />
-                ) : (
-                  t('Unassigned')
-                )
-              }
-            />
-          </KeyValueTable>
+          <UptimeDetailsSidebar
+            uptimeRule={uptimeRule}
+            showMissedLegend={showMissedLegend}
+          />
         </Layout.Side>
       </Layout.Body>
     </Layout.Page>
@@ -304,33 +194,4 @@ export default function UptimeAlertDetails({params}: UptimeAlertDetailsProps) {
 
 const StyledPageFilterBar = styled(PageFilterBar)`
   margin-bottom: ${space(2)};
-`;
-
-const CheckLegend = styled('ul')`
-  display: grid;
-  grid-template-columns: max-content 1fr;
-  margin-bottom: ${space(2)};
-  padding: 0;
-  gap: ${space(1)};
-`;
-
-const CheckLegendItem = styled('li')`
-  display: grid;
-  grid-template-columns: subgrid;
-  align-items: center;
-  grid-column: 1 / -1;
-`;
-
-const LegendText = styled(Text)`
-  display: flex;
-  gap: ${space(1)};
-  align-items: center;
-`;
-
-const MonitorUrlContainer = styled('div')`
-  margin-bottom: ${space(2)};
-
-  h4 {
-    margin-top: 0;
-  }
 `;
