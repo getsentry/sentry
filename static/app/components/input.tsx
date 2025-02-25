5acfc8bfd2a9a6bfe3c9711753c4f79560b57898
TkDodo <dominik.dorfmeister@sentry.io>
2025-02-25 13:02:20 +0100

    fix: restore default export for getsentry

--- static/app/components/input.tsx
+++ static/app/components/input.tsx
@@ -3,3 +3,4 @@
  */
 // biome-ignore lint/performance/noBarrelFile: Remove this once imports are fixed
 export * from 'sentry/components/core/input';
+export {Input as default} from 'sentry/components/core/input';
