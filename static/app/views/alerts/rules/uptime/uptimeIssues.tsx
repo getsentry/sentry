63397af730b5ffbbfa0b30cf17b1f36183be2eab
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-02-25 15:18:31 -0500

    ref(crons): Remove time restriction on uptime issues (#85761)
    There will only ever be one issue, so until we have a better component
    to use here let's just remove the time-range filter so we always show
    that one issue in the table

--- static/app/views/alerts/rules/uptime/uptimeIssues.tsx
+++ static/app/views/alerts/rules/uptime/uptimeIssues.tsx
@@ -5,9 +5,7 @@ import PanelBody from 'sentry/components/panels/panelBody';
 import {t} from 'sentry/locale';
 import {IssueCategory} from 'sentry/types/group';
 import type {Project} from 'sentry/types/project';
-import {getUtcDateString} from 'sentry/utils/dates';
 import useOrganization from 'sentry/utils/useOrganization';
-import usePageFilters from 'sentry/utils/usePageFilters';
 
 interface Props {
   project: Project;
@@ -16,17 +14,6 @@ interface Props {
 
 export function UptimeIssues({project, ruleId}: Props) {
   const organization = useOrganization();
-  const {selection} = usePageFilters();
-  const {start, end, period} = selection.datetime;
-  const timeProps =
-    start && end
-      ? {
-          start: getUtcDateString(start),
-          end: getUtcDateString(end),
-        }
-      : {
-          statsPeriod: period,
-        };
 
   // TODO(davidenwang): Replace this with an actual query for the specific uptime alert rule
   const query = `issue.category:${IssueCategory.UPTIME} tags[uptime_rule]:${ruleId}`;
@@ -50,7 +37,6 @@ export function UptimeIssues({project, ruleId}: Props) {
         query,
         project: project.id,
         limit: 20,
-        ...timeProps,
       }}
       renderEmptyMessage={emptyMessage}
     />
