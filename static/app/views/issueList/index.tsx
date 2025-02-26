d7478a926527d82822ca8ffd197a465873cd2aea
MichaelSun48 <michael.sun@sentry.io>
2025-02-26 15:17:17 -0800

    Use global page filters on All Issues

--- static/app/views/issueList/index.tsx
+++ static/app/views/issueList/index.tsx
@@ -1,9 +1,12 @@
+import {usePrefersStackedNav} from 'sentry/components/nav/prefersStackedNav';
 import NoProjectMessage from 'sentry/components/noProjectMessage';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {t} from 'sentry/locale';
 import useRouteAnalyticsHookSetup from 'sentry/utils/routeAnalytics/useRouteAnalyticsHookSetup';
+import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
+import {useParams} from 'sentry/utils/useParams';
 
 type Props = {
   children: React.ReactNode;
@@ -12,12 +15,21 @@ type Props = {
 function IssueListContainer({children}: Props) {
   const organization = useOrganization();
   useRouteAnalyticsHookSetup();
+  const prefersStackedNav = usePrefersStackedNav();
+  const location = useLocation();
+  const {viewId} = useParams<{orgId?: string; viewId?: string}>();
+
+  const onAllIssues = location.pathname === '/issues/' && !viewId;
+
+  const useGlobalPageFilters =
+    !organization.features.includes('issue-stream-custom-views') ||
+    (prefersStackedNav && onAllIssues);
 
   return (
     <SentryDocumentTitle title={t('Issues')} orgSlug={organization.slug}>
       <PageFiltersContainer
-        skipLoadLastUsed={organization.features.includes('issue-stream-custom-views')}
-        disablePersistence={organization.features.includes('issue-stream-custom-views')}
+        skipLoadLastUsed={!useGlobalPageFilters}
+        disablePersistence={!useGlobalPageFilters}
       >
         <NoProjectMessage organization={organization}>{children}</NoProjectMessage>
       </PageFiltersContainer>
