fbb5043f15942e2f89078c56229c237d2ba22388
JonasBa <jonas@badalic.com>
2025-02-25 20:08:43 -0500

    feature: add backcompat export

---
+++ static/app/components/badge/featureBadge.tsx
@@ -0,0 +1,10 @@
+/**
+ * @deprecated Use `sentry/components/core/badge/featureBadge` instead.
+ */
+// biome-ignore lint/performance/noBarrelFile: deprecated
+export * from 'sentry/components/core/badge/featureBadge';
+/**
+ * @deprecated Use `sentry/components/core/badge/featureBadge` instead.
+ */
+// biome-ignore lint/performance/noBarrelFile: deprecated
+export {default} from 'sentry/components/core/badge/featureBadge';
