429b1f10bc9e82a6157d9d1bc87c5e9a7bdad93a
Dan Fuller <dfuller@sentry.io>
2025-02-26 17:18:42 -0800

    perf(migrations): Improve performance of migration tests a little (#86007)
    It takes ~5s to create `ProjectState`, which is used 3 times when
    rolling migrations backwards and forwards. Caching it should make
    running multiple migrations a little faster.
    
    Unfortunately, rolling back doesn't use the passed `state` param, so
    we're always going to be doing extra work there.
    
    <!-- Describe your PR here. -->

--- src/sentry/testutils/cases.py
+++ src/sentry/testutils/cases.py
@@ -29,6 +29,7 @@ from django.core import signing
 from django.core.cache import cache
 from django.db import connection, connections
 from django.db.migrations.executor import MigrationExecutor
+from django.db.migrations.state import ProjectState
 from django.http import HttpRequest
 from django.test import RequestFactory
 from django.test import TestCase as DjangoTestCase
@@ -2672,6 +2673,20 @@ class OrganizationDashboardWidgetTestCase(APITestCase):
         self.login_as(self.user)
 
 
+_project_state_cache: dict[str, ProjectState] = {}
+
+
+def get_project_state(connection: str) -> ProjectState:
+    global _project_state_cache
+
+    if connection not in _project_state_cache:
+        executor = MigrationExecutor(connections[connection])
+        _project_state_cache[connection] = executor._create_project_state(
+            with_applied_migrations=True
+        )
+    return _project_state_cache[connection].clone()
+
+
 @pytest.mark.migrations
 class TestMigrations(TransactionTestCase):
     """
@@ -2712,14 +2727,14 @@ class TestMigrations(TransactionTestCase):
         old_apps = executor.loader.project_state(migrate_from).apps
 
         # Reverse to the original migration
-        executor.migrate(migrate_from)
+        executor.migrate(migrate_from, state=get_project_state(self.connection))
 
         self.setup_before_migration(old_apps)
 
         # Run the migration to test
         executor = MigrationExecutor(connection)
         executor.loader.build_graph()  # reload.
-        executor.migrate(migrate_to)
+        executor.migrate(migrate_to, state=get_project_state(self.connection))
 
         self.apps = executor.loader.project_state(migrate_to).apps
 
@@ -2727,7 +2742,7 @@ class TestMigrations(TransactionTestCase):
         super().tearDown()
         executor = MigrationExecutor(connection)
         executor.loader.build_graph()  # reload.
-        executor.migrate(self.current_migration)
+        executor.migrate(self.current_migration, state=get_project_state(self.connection))
 
     def setup_initial_state(self):
         # Add code here that will run before we roll back the database to the `migrate_from`
