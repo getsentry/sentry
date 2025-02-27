d4375825d4c231c6088607bc18476f36a3541369
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-02-27 11:52:40 -0800

    Update unleash param

--- static/app/gettingStartedDocs/javascript/javascript.tsx
+++ static/app/gettingStartedDocs/javascript/javascript.tsx
@@ -171,7 +171,7 @@ import { UnleashClient } from "unleash-proxy-client";
 Sentry.init({
   dsn: "${dsn}",
   integrations: [
-    Sentry.unleashIntegration({ unleashClientClass: UnleashClient }),
+    Sentry.unleashIntegration({ featureFlagClientClass: UnleashClient }),
   ],
 });
 
