f5a9af516d0621c5ff77b309c4e5e03a86d1cedb
JonasBa <jonas@badalic.com>
2025-02-27 17:31:48 -0500

    leave back deprecated import

---
+++ static/app/components/badge/tag.tsx
@@ -0,0 +1,9 @@
+/**
+ * @deprecated Use `export * from 'sentry/components/core/badge/tag'` instead.
+ */
+// biome-ignore lint/performance/noBarrelFile: deprecated
+export * from 'sentry/components/core/badge/tag';
+/**
+ * @deprecated Use `export * from 'sentry/components/core/badge/tag'` instead.
+ */
+export {default} from 'sentry/components/core/badge/tag';
