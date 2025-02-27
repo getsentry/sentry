406aff78a470992c54e8df9c97103221599b615b
Raj Joshi <raj.joshi@sentry.io>
2025-02-27 12:54:35 -0500

    :recycle: ref(aci): refactor get_incident_status_text to hoist FK Lookups (#85883)

--- tests/sentry/integrations/test_metric_alerts.py
+++ tests/sentry/integrations/test_metric_alerts.py
@@ -5,7 +5,7 @@ import pytest
 from django.utils import timezone
 
 from sentry.incidents.logic import CRITICAL_TRIGGER_LABEL
-from sentry.incidents.models.alert_rule import AlertRuleDetectionType
+from sentry.incidents.models.alert_rule import AlertRuleDetectionType, AlertRuleThresholdType
 from sentry.incidents.models.incident import IncidentStatus, IncidentTrigger
 from sentry.integrations.metric_alerts import incident_attachment_info
 from sentry.snuba.dataset import Dataset
@@ -134,7 +134,9 @@ class IncidentAttachmentInfoTest(TestCase, BaseIncidentsTest):
     def test_percent_change_alert(self):
         # 1 hour comparison_delta
         alert_rule = self.create_alert_rule(
-            comparison_delta=60, detection_type=AlertRuleDetectionType.PERCENT
+            comparison_delta=60,
+            detection_type=AlertRuleDetectionType.PERCENT,
+            threshold_type=AlertRuleThresholdType.ABOVE,
         )
         date_started = self.now
         incident = self.create_incident(
@@ -161,6 +163,7 @@ class IncidentAttachmentInfoTest(TestCase, BaseIncidentsTest):
         alert_rule = self.create_alert_rule(
             comparison_delta=60,
             detection_type=AlertRuleDetectionType.PERCENT,
+            threshold_type=AlertRuleThresholdType.ABOVE,
             dataset=Dataset.EventsAnalyticsPlatform,
             query_type=SnubaQuery.Type.PERFORMANCE,
         )
@@ -185,9 +188,11 @@ class IncidentAttachmentInfoTest(TestCase, BaseIncidentsTest):
         )
 
     def test_percent_change_alert_custom_comparison_delta(self):
-        # 12 hour comparison_delta
         alert_rule = self.create_alert_rule(
-            comparison_delta=60 * 12, detection_type=AlertRuleDetectionType.PERCENT
+            # 1 month comparison_delta
+            comparison_delta=720,
+            threshold_type=AlertRuleThresholdType.ABOVE,
+            detection_type=AlertRuleDetectionType.PERCENT,
         )
         date_started = self.now
         incident = self.create_incident(
