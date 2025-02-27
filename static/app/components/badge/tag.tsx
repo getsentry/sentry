b6975b000cb0f644aa9a1d13114d669bbc19650b
JonasBa <jonas@badalic.com>
2025-02-27 15:58:45 -0500

    ref(tag): getsentry is home

--- static/app/components/badge/tag.tsx
+++
@@ -1,9 +0,0 @@
-/**
- * @deprecated Use `sentry/components/core/badge/tag` instead.
- */
-// biome-ignore lint/performance/noBarrelFile: deprecated
-export * from 'sentry/components/core/badge/tag';
-/**
- * @deprecated Use `sentry/components/core/badge/tag` instead.
- */
-export {default} from 'sentry/components/core/badge/tag';
