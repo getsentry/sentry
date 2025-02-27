809e7aadfc8c1be9aa0705179a218eef5d023ed3
Ash Anand <ash.anand@sentry.io>
2025-02-27 15:45:17 -0500

    create dropdown options to change chart

--- static/app/views/performance/transactionSummary/transactionOverview/eapChartsWidget.tsx
+++ static/app/views/performance/transactionSummary/transactionOverview/eapChartsWidget.tsx
@@ -1,22 +1,89 @@
+import {useMemo, useState} from 'react';
+import styled from '@emotion/styled';
+
+import {CompactSelect} from 'sentry/components/compactSelect';
 import {t} from 'sentry/locale';
 import {Widget} from 'sentry/views/dashboards/widgets/widget/widget';
 
+enum EAPWidgetType {
+  DURATION_BREAKDOWN = 'duration_breakdown',
+  DURATION_PERCENTILES = 'duration_percentiles',
+  DURATION_DISTRIBUTION = 'duration_distribution',
+  TRENDS = 'trends',
+  WEB_VITALS = 'web_vitals',
+  // USER_MISERY = 5
+}
+
+const WIDGET_OPTIONS: Record<EAPWidgetType, {description: string; title: string}> = {
+  [EAPWidgetType.DURATION_BREAKDOWN]: {
+    title: t('Duration Breakdown'),
+    description: t(
+      'Duration Breakdown reflects transaction durations by percentile over time.'
+    ),
+  },
+  [EAPWidgetType.DURATION_PERCENTILES]: {
+    title: t('Duration Percentiles'),
+    description: t(
+      `Compare the duration at each percentile. Compare with Latency Histogram to see transaction volume at duration intervals.`
+    ),
+  },
+  [EAPWidgetType.DURATION_DISTRIBUTION]: {
+    title: t('Duration Distribution'),
+    description: t(
+      'Duration Distribution reflects the volume of transactions per median duration.'
+    ),
+  },
+  [EAPWidgetType.TRENDS]: {
+    title: t('Trends'),
+    description: t('Trends shows the smoothed value of an aggregate over time.'),
+  },
+  [EAPWidgetType.WEB_VITALS]: {
+    title: t('Web Vitals'),
+    description: t(
+      'Web Vitals Breakdown reflects the 75th percentile of web vitals over time.'
+    ),
+  },
+};
+
 export function EAPChartsWidget() {
+  const [selectedWidget, setSelectedWidget] = useState<EAPWidgetType>(
+    EAPWidgetType.DURATION_BREAKDOWN
+  );
+
+  const options = useMemo(() => {
+    return Object.entries(WIDGET_OPTIONS).map(([key, value]) => ({
+      label: value.title,
+      value: key,
+    }));
+  }, []);
+
   return (
     <Widget
-      Title={<Widget.WidgetTitle title={t('Duration Breakdown')} />}
+      Title={<Widget.WidgetTitle title={WIDGET_OPTIONS[selectedWidget].title} />}
       Actions={
         <Widget.WidgetToolbar>
           <Widget.WidgetDescription
-            title={t('Duration Breakdown')}
-            description={t(
-              'Duration Breakdown reflects transaction durations by percentile over time.'
-            )}
+            title={WIDGET_OPTIONS[selectedWidget].title}
+            description={WIDGET_OPTIONS[selectedWidget].description}
           />
         </Widget.WidgetToolbar>
       }
       Visualization={null}
-      Footer={null}
+      Footer={
+        <FooterContainer>
+          <CompactSelect
+            options={options}
+            value={selectedWidget}
+            onChange={option => setSelectedWidget(option.value as EAPWidgetType)}
+          />
+        </FooterContainer>
+      }
     />
   );
 }
+
+const FooterContainer = styled('div')`
+  display: flex;
+  align-items: right;
+  justify-content: right;
+`;
