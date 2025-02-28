64dfcd8d27a6c4d6a1d73dc2933f4334b171169b
Joshua Ferge <josh.ferge@sentry.io>
2025-02-28 17:59:00 -0500

    fix(api): better error message return to the client

--- src/sentry/api/handlers.py
+++ src/sentry/api/handlers.py
@@ -1,3 +1,4 @@
+import sentry_sdk
 from rest_framework.exceptions import Throttled
 from rest_framework.views import exception_handler
 
@@ -6,8 +7,13 @@ from sentry.utils.snuba import RateLimitExceeded
 
 def custom_exception_handler(exc, context):
     if isinstance(exc, RateLimitExceeded):
-        # If Snuba throws a RateLimitExceeded then it'll likely be available
-        # after another second.
-        exc = Throttled(wait=1)
+        # capture the rate limited exception so we can see it in Sentry
+        sentry_sdk.capture_exception(
+            exc,
+            fingerprint=["snuba-api-rate-limit-exceeded"],
+            level="warning",
+        )
+        # let the client know that they've been rate limited with details
+        exc = Throttled(detail=str(exc))
 
     return exception_handler(exc, context)
