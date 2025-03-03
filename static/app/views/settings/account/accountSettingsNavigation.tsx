0365f8812a99c9b2a7c7bbd61a5b86c6dc0cb69a
Malachi Willey <malwilley@gmail.com>
2025-03-03 15:03:30 -0800

    fix(nav): Show same nav items when user is in org or user settings. (#86161)
    Fixes the issue where Stats & Billing section would disappear when you
    were in account details. The other code route did not append the
    getsentry hooks.

--- static/app/views/settings/account/accountSettingsNavigation.tsx
+++ static/app/views/settings/account/accountSettingsNavigation.tsx
@@ -1,12 +1,18 @@
+import {prefersStackedNav} from 'sentry/components/nav/prefersStackedNav';
 import type {Organization} from 'sentry/types/organization';
 import getConfiguration from 'sentry/views/settings/account/navigationConfiguration';
 import SettingsNavigation from 'sentry/views/settings/components/settingsNavigation';
+import OrganizationSettingsNavigation from 'sentry/views/settings/organization/organizationSettingsNavigation';
 
 type Props = {
   organization?: Organization;
 };
 
 function AccountSettingsNavigation({organization}: Props) {
+  if (organization && prefersStackedNav()) {
+    return <OrganizationSettingsNavigation organization={organization} />;
+  }
+
   return (
     <SettingsNavigation
       organization={organization}
