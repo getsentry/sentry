cd246814a1aad197eaa8b5f97a156c0fa2673944
William Mak <william@wmak.io>
2025-03-03 14:24:23 -0500

    fix(rpc): Handle wildcard free text search (#86050)
    - Discover was Automatically treating any search on its free_text_key as
    a wildcard, but that doesn't really make sense to me and I expect its
    not the intended behaviour
    - This will treat all free_text_key searches as wildcard searches

--- src/sentry/search/eap/resolver.py
+++ src/sentry/search/eap/resolver.py
@@ -32,6 +32,7 @@ from sentry_protos.snuba.v1.trace_item_filter_pb2 import (
 )
 
 from sentry.api import event_search
+from sentry.api.event_search import SearchConfig
 from sentry.exceptions import InvalidSearchQuery
 from sentry.search.eap import constants
 from sentry.search.eap.columns import (
@@ -154,6 +155,7 @@ class SearchResolver:
         try:
             parsed_terms = event_search.parse_search_query(
                 querystring,
+                config=SearchConfig(wildcard_free_text=True),
                 params=self.params.filter_params,
                 get_field_type=self.get_field_type,
                 get_function_result_type=self.get_field_type,
