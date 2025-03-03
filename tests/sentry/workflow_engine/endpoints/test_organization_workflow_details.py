00e750264c76b1fe275b0b3a73332aa982c4b465
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-03-03 12:55:57 -0800

    Setup a simple test case and the audit entry test case

--- tests/sentry/workflow_engine/endpoints/test_organization_workflow_details.py
+++ tests/sentry/workflow_engine/endpoints/test_organization_workflow_details.py
@@ -1,6 +1,11 @@
+from sentry import audit_log
 from sentry.api.serializers import serialize
+from sentry.deletions.models.scheduleddeletion import RegionScheduledDeletion
+from sentry.models.auditlogentry import AuditLogEntry
+from sentry.silo.base import SiloMode
 from sentry.testutils.cases import APITestCase
-from sentry.testutils.silo import region_silo_test
+from sentry.testutils.outbox import outbox_runner
+from sentry.testutils.silo import assume_test_silo_mode, region_silo_test
 from tests.sentry.workflow_engine.test_base import BaseWorkflowTest
 
 
@@ -25,13 +30,31 @@ class OrganizationWorkflowIndexGetTest(OrganizationWorkflowDetailsBaseTest):
 
 @region_silo_test
 class OrganizationDeleteWorkflowTest(OrganizationWorkflowDetailsBaseTest, BaseWorkflowTest):
+    method = "DELETE"
+
     def setUp(self):
         super().setUp()
         self.workflow = self.create_workflow(organization_id=self.organization.id)
 
     def test_simple(self):
-        # delete the workflow
-        pass
+        with outbox_runner():
+            self.get_success_response(self.organization.slug, self.workflow.id)
+
+        assert RegionScheduledDeletion.objects.filter(
+            model_name="Workflow",
+            object_id=self.workflow.id,
+        ).exists()
+
+    def test_audit_entry(self):
+        with outbox_runner():
+            self.get_success_response(self.organization.slug, self.workflow.id)
+
+        with assume_test_silo_mode(SiloMode.CONTROL):
+            assert AuditLogEntry.objects.filter(
+                target_object=self.workflow.id,
+                event=audit_log.get_event_id("WORKFLOW_REMOVE"),
+                actor=self.user,
+            ).exists()
 
     def test_does_not_exist(self):
         # delete a workflow that does not exist, -1 id
@@ -40,10 +63,6 @@ class OrganizationDeleteWorkflowTest(OrganizationWorkflowDetailsBaseTest, BaseWo
     def test_delete_configured_workflow(self):
         # add data conditions
         # add actions
-        pass
-
-    def test_audit_entry(self):
-        # ensure there is an audit entry for the deleted workflow
         pass
 
     def test_without_permissions(self):
