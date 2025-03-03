7d9a7269ecd5061b98afb3dbcc54327feb29b053
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-03-03 15:21:23 -0800

    Add more complex test conditions, ensure that nested models are removed and test permissions

--- tests/sentry/workflow_engine/endpoints/test_organization_workflow_details.py
+++ tests/sentry/workflow_engine/endpoints/test_organization_workflow_details.py
@@ -1,11 +1,14 @@
 from sentry import audit_log
 from sentry.api.serializers import serialize
 from sentry.deletions.models.scheduleddeletion import RegionScheduledDeletion
+from sentry.deletions.tasks.scheduled import run_scheduled_deletions
 from sentry.models.auditlogentry import AuditLogEntry
 from sentry.silo.base import SiloMode
 from sentry.testutils.cases import APITestCase
+from sentry.testutils.helpers import TaskRunner
 from sentry.testutils.outbox import outbox_runner
 from sentry.testutils.silo import assume_test_silo_mode, region_silo_test
+from sentry.workflow_engine.models import Action, DataConditionGroup
 from tests.sentry.workflow_engine.test_base import BaseWorkflowTest
 
 
@@ -32,6 +35,9 @@ class OrganizationWorkflowIndexGetTest(OrganizationWorkflowDetailsBaseTest):
 class OrganizationDeleteWorkflowTest(OrganizationWorkflowDetailsBaseTest, BaseWorkflowTest):
     method = "DELETE"
 
+    def tasks(self):
+        return TaskRunner()
+
     def setUp(self):
         super().setUp()
         self.workflow = self.create_workflow(organization_id=self.organization.id)
@@ -57,14 +63,58 @@ class OrganizationDeleteWorkflowTest(OrganizationWorkflowDetailsBaseTest, BaseWo
             ).exists()
 
     def test_does_not_exist(self):
-        # delete a workflow that does not exist, -1 id
-        pass
+        with outbox_runner():
+            response = self.get_error_response(self.organization.slug, -1)
+            assert response.status_code == 404
 
-    def test_delete_configured_workflow(self):
-        # add data conditions
-        # add actions
-        pass
+        # Ensure it wasn't deleted
+        assert not RegionScheduledDeletion.objects.filter(
+            model_name="Workflow",
+            object_id=self.workflow.id,
+        ).exists()
+
+    def test_delete_configured_workflow__action(self):
+        action_condition_group, action = self.create_workflow_action(workflow=self.workflow)
+
+        with outbox_runner():
+            self.get_success_response(self.organization.slug, self.workflow.id)
+
+        # Ensure the workflow is scheduled for deletion
+        assert RegionScheduledDeletion.objects.filter(
+            model_name="Workflow",
+            object_id=self.workflow.id,
+        ).exists()
+
+        # Delete the workflow
+        with self.tasks():
+            run_scheduled_deletions()
+
+        # Ensure action is removed
+        assert not Action.objects.filter(id=action.id).exists()
+
+    def test_delete_configured_workflow__action_condition(self):
+        action_condition_group, action = self.create_workflow_action(workflow=self.workflow)
+
+        with outbox_runner():
+            self.get_success_response(self.organization.slug, self.workflow.id)
+
+        # Ensure the workflow is scheduled for deletion
+        assert RegionScheduledDeletion.objects.filter(
+            model_name="Workflow",
+            object_id=self.workflow.id,
+        ).exists()
+
+        # Actually delete the workflow
+        with self.tasks():
+            run_scheduled_deletions()
+
+        assert not DataConditionGroup.objects.filter(id=action_condition_group.id).exists()
 
     def test_without_permissions(self):
-        # delete a workflow that does not belong to this organization
-        pass
+        # Create a workflow with a different organization
+        new_org = self.create_organization()
+        workflow = self.create_workflow(organization_id=new_org.id)
+
+        with outbox_runner():
+            response = self.get_error_response(self.organization.slug, workflow.id)
+            assert response.status_code == 404
