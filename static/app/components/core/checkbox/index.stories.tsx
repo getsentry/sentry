c406ae3ee6b145f26a4c4312ab75a6f68aed3819
JonasBa <jonas@badalic.com>
2025-03-03 13:51:04 -0500

    checkbox: add storis for checked and disabled checked state

--- static/app/components/core/checkbox/index.stories.tsx
+++ static/app/components/core/checkbox/index.stories.tsx
@@ -1,4 +1,5 @@
 import {Checkbox} from 'sentry/components/core/checkbox';
+import SideBySide from 'sentry/components/stories/sideBySide';
 import storyBook from 'sentry/stories/storyBook';
 
 // eslint-disable-next-line import/no-webpack-loader-syntax
@@ -8,6 +9,21 @@ export default storyBook('Checkbox', (story, APIReference) => {
   APIReference(types.Checkbox);
 
   story('Default', () => {
-    return <Checkbox />;
+    return (
+      <SideBySide>
+        <label>
+          Default <Checkbox />
+        </label>
+        <label>
+          Checked <Checkbox checked />
+        </label>
+        <label>
+          Disabled <Checkbox disabled />
+        </label>
+        <label>
+          Disabled Checked <Checkbox disabled checked />
+        </label>
+      </SideBySide>
+    );
   });
 });
