84d768a8cceb76a8e95d160c8895d3cb2b246d2a
Scott Cooper <scttcper@gmail.com>
2025-03-04 15:56:41 -0800

    adjust props, move query to query param

--- static/app/components/events/interfaces/crashContent/exception/rawContent.tsx
+++ static/app/components/events/interfaces/crashContent/exception/rawContent.tsx
@@ -5,17 +5,16 @@ import rawStacktraceContent from 'sentry/components/events/interfaces/crashConte
 import LoadingError from 'sentry/components/loadingError';
 import Placeholder from 'sentry/components/placeholder';
 import type {Event, ExceptionType} from 'sentry/types/event';
-import type {Organization} from 'sentry/types/organization';
 import type {PlatformKey, Project} from 'sentry/types/project';
 import {defined} from 'sentry/utils';
 import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 
-interface Props extends Pick<ExceptionType, 'values'> {
+interface Props {
   eventId: Event['id'];
   projectSlug: Project['slug'];
   type: 'original' | 'minified';
-  organization?: Organization;
+  values: ExceptionType['values'];
   platform?: PlatformKey;
 }
 
@@ -31,17 +30,26 @@ export default function RawContent({
   const isNative =
     platform === 'native' || platform === 'cocoa' || platform === 'nintendo-switch';
 
-  const endpointUrl = `/projects/${organization.slug}/${projectSlug}/events/${eventId}/apple-crash-report?minified=${type === 'minified'}`;
   const hasCrashReport = isNative && defined(organization);
 
   const {
     data: crashReport,
     isPending,
     isError,
-  } = useApiQuery<string>([endpointUrl, {headers: {Accept: '*/*; charset=utf-8'}}], {
-    enabled: hasCrashReport,
-    staleTime: Infinity,
-  });
+  } = useApiQuery<string>(
+    [
+      // Note that this endpoint does not have a trailing slash for some reason
+      `/projects/${organization.slug}/${projectSlug}/events/${eventId}/apple-crash-report`,
+      {
+        query: {minified: String(type === 'minified')},
+        headers: {Accept: '*/*; charset=utf-8'},
+      },
+    ],
+    {
+      enabled: hasCrashReport,
+      staleTime: Infinity,
+    }
+  );
 
   if (isPending && hasCrashReport) {
     return <Placeholder height="270px" />;
