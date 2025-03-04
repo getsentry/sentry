fd9c3e94a47da2c893cdf0ac2464ac012f99aa02
Mark Story <mark@mark-story.com>
2025-03-04 13:44:33 -0500

    feat(relocation) Implement relocation transfer task (#85953)
    Implement the processing tasks for relocation transfers. This logic is
    mostly a lift/shift from the outbox receivers
    
    ### control receivers
    
    https://github.com/getsentry/sentry/blob/00176e9b1ea65735c52883632b42df0c1e48a564/src/sentry/receivers/outbox/control.py#L110-L165
    
    ### region receiver
    
    https://github.com/getsentry/sentry/blob/00176e9b1ea65735c52883632b42df0c1e48a564/src/sentry/receivers/outbox/region.py#L84-L120
    
    [design
    doc](https://www.notion.so/sentry/Relocation-outbox-rework-19e8b10e4b5d80268741cc5b513a89ae)

--- src/sentry/relocation/tasks/transfer.py
+++ src/sentry/relocation/tasks/transfer.py
@@ -3,16 +3,24 @@ import logging
 from celery import Task
 from django.db.models import Subquery
 from django.utils import timezone
+from sentry_sdk import capture_exception
 
+from sentry.models.files.utils import get_relocation_storage
 from sentry.relocation.models.relocationtransfer import (
     MAX_AGE,
     RETRY_BACKOFF,
     BaseRelocationTransfer,
     ControlRelocationTransfer,
     RegionRelocationTransfer,
+    RelocationTransferState,
+)
+from sentry.relocation.services.relocation_export.service import (
+    control_relocation_export_service,
+    region_relocation_export_service,
 )
 from sentry.silo.base import SiloMode
 from sentry.tasks.base import instrumented_task
+from sentry.types.region import get_local_region
 
 logger = logging.getLogger("sentry.relocation.tasks")
 
@@ -82,7 +90,86 @@ def _find_relocation_transfer(
     silo_mode=SiloMode.CONTROL,
 )
 def process_relocation_transfer_control(transfer_id: int) -> None:
-    pass
+    log_context = {"id": transfer_id, "silo": "control"}
+    try:
+        transfer = ControlRelocationTransfer.objects.get(id=transfer_id)
+    except ControlRelocationTransfer.DoesNotExist:
+        logging.warning("relocation.transfer_missing", extra=log_context)
+        return
+    log_context["state"] = transfer.state
+    log_context["relocation_uuid"] = str(transfer.relocation_uuid)
+
+    if transfer.state == RelocationTransferState.Request:
+        public_key = transfer.public_key or b""
+        if public_key:
+            public_key = bytes(public_key)
+
+        # Forward the export request to the exporting region.
+        try:
+            region_relocation_export_service.request_new_export(
+                relocation_uuid=str(transfer.relocation_uuid),
+                requesting_region_name=transfer.requesting_region,
+                replying_region_name=transfer.exporting_region,
+                org_slug=transfer.org_slug,
+                encrypt_with_public_key=public_key,
+            )
+            # Once the RPC is successful, we're done with this transfer.
+            transfer.delete()
+        except Exception as err:
+            logging.warning(
+                "relocation.transfer_failed",
+                extra={
+                    **log_context,
+                    "error": str(err),
+                },
+            )
+            capture_exception(err)
+    elif transfer.state == RelocationTransferState.Reply:
+        # We expect the `ProxyRelocationExportService::reply_with_export` implementation to have
+        # written the export data to the control silo's local relocation-specific GCS bucket. Here,
+        # we just read it into memory and attempt the RPC back to the requesting region.
+        uuid = transfer.relocation_uuid
+        slug = transfer.org_slug
+
+        relocation_storage = get_relocation_storage()
+        path = f"runs/{uuid}/saas_to_saas_export/{slug}.tar"
+        try:
+            encrypted_bytes = relocation_storage.open(path)
+        except Exception as err:
+            logger.warning(
+                "relocation.failed_open_reply",
+                extra={
+                    **log_context,
+                    "error": str(err),
+                },
+            )
+            capture_exception(err)
+            return
+
+        try:
+            with encrypted_bytes:
+                # Move encrypted bytes to the requesting region.
+                region_relocation_export_service.reply_with_export(
+                    relocation_uuid=str(transfer.relocation_uuid),
+                    requesting_region_name=transfer.requesting_region,
+                    replying_region_name=transfer.exporting_region,
+                    org_slug=slug,
+                    # TODO(mark): finish transfer from `encrypted_contents` -> `encrypted_bytes`.
+                    encrypted_contents=None,
+                    encrypted_bytes=[int(byte) for byte in encrypted_bytes.read()],
+                )
+                # We are done with this stage of the transfer
+                transfer.delete()
+        except Exception as err:
+            logger.warning(
+                "relocation.failed_rpc_reply",
+                extra={
+                    **log_context,
+                    "error": str(err),
+                },
+            )
+            capture_exception(err)
+            return
 
 
 @instrumented_task(
@@ -91,4 +178,48 @@ def process_relocation_transfer_control(transfer_id: int) -> None:
     silo_mode=SiloMode.REGION,
 )
 def process_relocation_transfer_region(transfer_id: int) -> None:
-    pass
+    log_context = {"id": transfer_id, "silo": "region", "region": get_local_region().name}
+
+    try:
+        transfer = RegionRelocationTransfer.objects.get(id=transfer_id)
+    except RegionRelocationTransfer.DoesNotExist:
+        logging.warning("relocation.transfer_missing", extra=log_context)
+        return
+
+    uuid = str(transfer.relocation_uuid)
+    slug = transfer.org_slug
+
+    log_context["state"] = transfer.state
+    log_context["relocation_uuid"] = uuid
+
+    if transfer.state == RelocationTransferState.Reply:
+        relocation_storage = get_relocation_storage()
+        path = f"runs/{uuid}/saas_to_saas_export/{slug}.tar"
+        try:
+            encrypted_bytes = relocation_storage.open(path)
+        except Exception as err:
+            logger.warning(
+                "relocation.failed_open.export",
+                extra={
+                    **log_context,
+                    "error": str(err),
+                },
+            )
+            capture_exception(err)
+            return
+
+        with encrypted_bytes:
+            control_relocation_export_service.reply_with_export(
+                relocation_uuid=uuid,
+                requesting_region_name=transfer.requesting_region,
+                replying_region_name=transfer.exporting_region,
+                org_slug=slug,
+                # TODO(mark): finish transfer from `encrypted_contents` -> `encrypted_bytes`.
+                encrypted_contents=None,
+                encrypted_bytes=[int(byte) for byte in encrypted_bytes.read()],
+            )
+        # Remove the transfer once the reply is sent.
+        transfer.delete()
+    else:
+        logger.warning("relocation.transfer_invalid_state", extra=log_context)
+        transfer.delete()
