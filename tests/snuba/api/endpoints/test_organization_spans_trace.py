208d0074f7e90aa3bb17b0376971150205d77a74
William Mak <william@wmak.io>
2025-03-04 13:58:14 -0500

    fix(trace): Ignore project param (#86234)
    - This ignores the project param on the trace endpoint since we always
    want to return the entire trace

--- tests/snuba/api/endpoints/test_organization_spans_trace.py
+++ tests/snuba/api/endpoints/test_organization_spans_trace.py
@@ -93,7 +93,19 @@ class OrganizationEventsTraceEndpointTest(OrganizationEventsTraceEndpointBase):
         self.load_trace(is_eap=True)
         with self.feature(self.FEATURES):
             response = self.client_get(
-                data={"project": -1},
+                data={},
+            )
+        assert response.status_code == 200, response.content
+        data = response.data
+        assert len(data) == 1
+        self.assert_trace_data(data[0])
+
+    def test_ignore_project_param(self):
+        self.load_trace(is_eap=True)
+        with self.feature(self.FEATURES):
+            # The trace endpoint should ignore the project param
+            response = self.client_get(
+                data={"project": self.project.id},
             )
         assert response.status_code == 200, response.content
         data = response.data
