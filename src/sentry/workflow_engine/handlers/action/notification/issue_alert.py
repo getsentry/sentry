dadb5ca4e9ad66ba286a0d8f5b1f58ced4fc9917
Raj Joshi <raj.joshi@sentry.io>
2025-03-03 16:54:44 -0800

    :wrench: chore: fix sentry app notification action

--- src/sentry/workflow_engine/handlers/action/notification/issue_alert.py
+++ src/sentry/workflow_engine/handlers/action/notification/issue_alert.py
@@ -2,6 +2,7 @@ import logging
 import uuid
 from abc import ABC
 from collections.abc import Callable, Collection, Sequence
+from dataclasses import asdict
 from typing import Any
 
 import sentry_sdk
@@ -27,6 +28,7 @@ from sentry.workflow_engine.typings.notification_action import (
     EmailFieldMappingKeys,
     OnCallDataBlob,
     SentryAppDataBlob,
+    SentryAppFormConfigDataBlob,
     SlackDataBlob,
     TicketFieldMappingKeys,
     TicketingActionDataBlobHelper,
@@ -402,10 +404,19 @@ class SentryAppIssueAlertHandler(BaseIssueAlertHandler):
         raise ValueError(f"No target identifier key found for action type: {action.type}")
 
     @classmethod
+    def process_settings(cls, settings: list[SentryAppFormConfigDataBlob]) -> list[dict[str, Any]]:
+        # Process each setting, removing None labels
+        return [
+            {k: v for k, v in asdict(setting).items() if not (k == "label" and v is None)}
+            for setting in settings
+        ]
+
+    @classmethod
     def get_additional_fields(cls, action: Action, mapping: ActionFieldMapping) -> dict[str, Any]:
         # Need to check for the settings key, if it exists, then we need to return the settings
         # It won't exist for legacy webhook actions, but will exist for sentry app actions
         if action.data.get("settings"):
             blob = SentryAppDataBlob(**action.data)
-            return {"settings": blob.settings}
+            settings = cls.process_settings(blob.settings)
+            return {"settings": settings}
         return {}
