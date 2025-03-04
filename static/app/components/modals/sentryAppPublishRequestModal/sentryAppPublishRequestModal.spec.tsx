6d0adfb73f42a50aa955d04117160d63d90c9371
Christinarlong <christina.long@sentry.io>
2025-03-04 11:25:53 -0800

    update tests and remove FF for frontend publish flow

--- static/app/components/modals/sentryAppPublishRequestModal/sentryAppPublishRequestModal.spec.tsx
+++ static/app/components/modals/sentryAppPublishRequestModal/sentryAppPublishRequestModal.spec.tsx
@@ -6,10 +6,7 @@ import {SentryAppFixture} from 'sentry-fixture/sentryApp';
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import {makeCloseButton} from 'sentry/components/globalModal/components';
-import {
-  getPermissionSelectionsFromScopes,
-  SentryAppPublishRequestModal,
-} from 'sentry/components/modals/sentryAppPublishRequestModal/sentryAppPublishRequestModal';
+import {SentryAppPublishRequestModal} from 'sentry/components/modals/sentryAppPublishRequestModal/sentryAppPublishRequestModal';
 
 describe('SentryAppDetailsModal', function () {
   const styledWrapper = styled((c: PropsWithChildren) => c.children);
@@ -19,7 +16,7 @@ describe('SentryAppDetailsModal', function () {
     MockApiClient.clearMockResponses();
   });
 
-  it('renders the modal', function () {
+  it('renders the modal', async function () {
     render(
       <SentryAppPublishRequestModal
         closeModal={jest.fn()}
@@ -33,14 +30,55 @@ describe('SentryAppDetailsModal', function () {
       />
     );
 
-    expect(screen.getByText('Publish Request Questionnaire')).toBeInTheDocument();
-    expect(screen.getByText('Questions to answer')).toBeInTheDocument();
-    expect(screen.getByText('What value does it offer customers?')).toBeInTheDocument();
     expect(
-      screen.queryByText(
-        'By submitting your integration, you acknowledge and agree that Sentry reserves the right to remove it at any time in its sole discretion.'
+      screen.getByRole('textbox', {
+        name: 'Provide a description about your integration, how this benefits developers using Sentry along with what’s needed to set up this integration.',
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('textbox', {
+        name: 'Provide a one-liner describing your integration. Subject to approval, we’ll use this to describe your integration on Sentry Integrations .',
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('textbox', {
+        name: 'Select what category best describes your integration. Documentation for reference.',
+      })
+    ).toBeInTheDocument();
+
+    await userEvent.click(
+      screen.getByRole('textbox', {
+        name: 'Select what category best describes your integration. Documentation for reference.',
+      })
+    );
+
+    expect(screen.getByText('Deployment')).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('textbox', {
+        name: 'Link to your documentation page.',
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('textbox', {
+        name: 'Link to a video showing installation, setup and user flow for your submission.',
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByRole('textbox', {
+        name: 'Email address for user support.',
+      })
+    ).toBeInTheDocument();
+
+    expect(
+      screen.getByText(
+        'By submitting your integration, you acknowledge and agree that Sentry reserves the right to remove your integration at any time in its sole discretion.'
       )
-    ).not.toBeInTheDocument();
+    ).toBeInTheDocument();
     expect(screen.getByRole('button', {name: 'Request Publication'})).toBeInTheDocument();
   });
 
@@ -76,7 +114,7 @@ describe('SentryAppDetailsModal', function () {
 
     expect(screen.getByRole('button', {name: 'Request Publication'})).toBeInTheDocument();
   });
-  it('sends correctly formatted JSON for the new modal flow', async () => {
+  it('sends correctly formatted JSON for the modal flow', async () => {
     const mockRequest = MockApiClient.addMockResponse({
       url: `/sentry-apps/${sentryApp.slug}/publish-request/`,
       method: 'POST',
@@ -187,91 +225,6 @@ describe('SentryAppDetailsModal', function () {
       ]),
     });
   });
-  it('sends correctly formatted JSON for the old modal flow', async () => {
-    const mockRequest = MockApiClient.addMockResponse({
-      url: `/sentry-apps/${sentryApp.slug}/publish-request/`,
-      method: 'POST',
-    });
-
-    render(
-      <SentryAppPublishRequestModal
-        closeModal={jest.fn()}
-        Header={p => <span>{p.children}</span>}
-        Footer={styledWrapper()}
-        Body={styledWrapper()}
-        CloseButton={makeCloseButton(() => {})}
-        organization={OrganizationFixture()}
-        app={sentryApp}
-        onPublishSubmission={onPublishSubmission}
-      />
-    );
-
-    // Fill out the form fields
-    await userEvent.type(
-      screen.getByRole('textbox', {
-        name: 'What does your integration do? Please be as detailed as possible.',
-      }),
-      'ok'
-    );
-
-    await userEvent.type(
-      screen.getByRole('textbox', {
-        name: 'What value does it offer customers?',
-      }),
-      'everything'
-    );
-
-    await userEvent.type(
-      screen.getByRole('textbox', {
-        name: 'Do you operate the web service your integration communicates with?',
-      }),
-      'maybe'
-    );
-
-    const permissionInput = screen.getByRole('textbox', {
-      name: /Please justify why you are requesting each of the following permissions/,
-    });
-
-    await userEvent.type(permissionInput, 'monka');
-
-    const submitButton = screen.getByRole('button', {name: 'Request Publication'});
-    expect(submitButton).toBeEnabled();
-    await userEvent.click(submitButton);
-
-    // Get the exact text that was sent to the API
-    const permissions = getPermissionSelectionsFromScopes(sentryApp.scopes);
-    const permissionQuestionBaseText =
-      'Please justify why you are requesting each of the following permissions: ';
-    const permissionQuestionPlainText = `${permissionQuestionBaseText}${permissions.join(
-      ', '
-    )}.`;
-
-    // Verify the API was called with the correct payload
-    expect(mockRequest).toHaveBeenCalledTimes(1);
-    const [url, {method, data}] = mockRequest.mock.calls[0];
-    expect(url).toBe(`/sentry-apps/${sentryApp.slug}/publish-request/`);
-    expect(method).toBe('POST');
-    expect(data).toEqual({
-      questionnaire: expect.arrayContaining([
-        {
-          question: 'What does your integration do? Please be as detailed as possible.',
-          answer: 'ok',
-        },
-        {
-          question: 'What value does it offer customers?',
-          answer: 'everything',
-        },
-        {
-          question: 'Do you operate the web service your integration communicates with?',
-          answer: 'maybe',
-        },
-        {
-          question: permissionQuestionPlainText,
-          answer: 'monka',
-        },
-      ]),
-    });
-  });
 
   it('allows users to select a list of categories', async () => {
     const organization = OrganizationFixture({
@@ -330,30 +283,47 @@ describe('SentryAppDetailsModal', function () {
     // Fill out the form fields
     await userEvent.type(
       screen.getByRole('textbox', {
-        name: 'What does your integration do? Please be as detailed as possible.',
+        name: 'Provide a description about your integration, how this benefits developers using Sentry along with what’s needed to set up this integration.',
       }),
       'ok'
     );
 
     await userEvent.type(
       screen.getByRole('textbox', {
-        name: 'What value does it offer customers?',
+        name: 'Provide a one-liner describing your integration. Subject to approval, we’ll use this to describe your integration on Sentry Integrations .',
       }),
-      'everything'
+      'the coolest integration ever'
+    );
+
+    await userEvent.click(
+      screen.getByRole('textbox', {
+        name: 'Select what category best describes your integration. Documentation for reference.',
+      })
+    );
+
+    expect(screen.getByText('Deployment')).toBeInTheDocument();
+    await userEvent.click(screen.getByText('Deployment'));
+
+    await userEvent.type(
+      screen.getByRole('textbox', {
+        name: 'Link to your documentation page.',
+      }),
+      'http://example.com'
     );
 
     await userEvent.type(
       screen.getByRole('textbox', {
-        name: 'Do you operate the web service your integration communicates with?',
+        name: 'Link to a video showing installation, setup and user flow for your submission.',
       }),
-      'maybe'
+      'https://example.com'
     );
 
-    const permissionInput = screen.getByRole('textbox', {
-      name: /Please justify why you are requesting each of the following permissions/,
-    });
-
-    await userEvent.type(permissionInput, 'monka');
+    await userEvent.type(
+      screen.getByRole('textbox', {
+        name: 'Email address for user support.',
+      }),
+      'example@sentry.io'
+    );
 
     const submitButton = screen.getByRole('button', {name: 'Request Publication'});
     expect(submitButton).toBeEnabled();
