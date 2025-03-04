c1dba542f800183b9a11df8755dd7086da44045a
Michelle Fu <michelle.fu@sentry.io>
2025-03-04 13:29:51 -0800

    wip

--- src/sentry/workflow_engine/handlers/condition/anomaly_detection_handler.py
+++ src/sentry/workflow_engine/handlers/condition/anomaly_detection_handler.py
@@ -1,16 +1,43 @@
 from typing import Any
 
+from sentry.incidents.models.alert_rule import (
+    AlertRuleSeasonality,
+    AlertRuleSensitivity,
+    AlertRuleThresholdType,
+)
+from sentry.workflow_engine.models import DataPacket
 from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.registry import condition_handler_registry
 from sentry.workflow_engine.types import DataConditionHandler, WorkflowEventData
 
 
 @condition_handler_registry.register(Condition.ANOMALY_DETECTION)
-class AnomalyDetectionHandler(DataConditionHandler[WorkflowEventData]):
-    group = DataConditionHandler.Group.DETECTOR_TRIGGER
-    comparison_json_schema = {"type": "boolean"}
+class AnomalyDetectionHandler(DataConditionHandler[DataPacket]):
+    type = [DataConditionHandler.Type.DETECTOR_TRIGGER]
+    # MOVE SENSITIVITY AND STUFF HERE
+    comparison_json_schema = {
+        "type": "object",
+        "properties": {
+            "sensitivity": {
+                "type": "string",
+                "enum": [sensitivity.value for sensitivity in AlertRuleSensitivity],
+            },
+            "seasonality": {
+                "type": "string",
+                "enum": [seasonality.value for seasonality in AlertRuleSeasonality],
+            },
+            "threshold_type": {
+                "type": "integer",
+                "enum": [threshold_type.value for threshold_type in AlertRuleThresholdType],
+            },
+        },
+    }
+
+    # import has_anomaly, compare to comparison
 
     @staticmethod
-    def evaluate_value(event_data: WorkflowEventData, comparison: Any) -> bool:
+    # this gets a subscription update
+    # comparison should be bool ?
+    def evaluate_value(update: DataPacket, comparison: Any) -> bool:
         # this is a placeholder
         return False
