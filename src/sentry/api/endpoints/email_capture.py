327194fbac4094816680712a59ba26005a1c9036
Ogi <ognjen.bostjancic@gmail.com>
2025-03-04 12:35:14 +0100

    fix(demo-mode): remove remaining usages of `IsAuthenticated` (#86270)

--- src/sentry/api/endpoints/email_capture.py
+++ src/sentry/api/endpoints/email_capture.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated  # noqa S012
 from rest_framework.request import Request
 from rest_framework.response import Response
 
