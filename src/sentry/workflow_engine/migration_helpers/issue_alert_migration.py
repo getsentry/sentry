8850ad6adb5f305b5c17d6285db38c3fc631fdd8
Cathy Teng <cathy.teng@sentry.io>
2025-03-04 14:09:06 -0800

    fix pre commit oops

--- src/sentry/workflow_engine/migration_helpers/issue_alert_migration.py
+++ src/sentry/workflow_engine/migration_helpers/issue_alert_migration.py
@@ -189,10 +189,11 @@ class IssueAlertMigrator:
         # the only time the data_conditions list will be empty is if somebody only has EveryEventCondition in their conditions list.
         # if it's empty and this is not the case, we should not migrate
         no_conditions = len(data_conditions) == 0
-        only_has_every_event_cond = len(conditions) == 1 and conditions[0]["id"] == EveryEventCondition.id
+        only_has_every_event_cond = (
+            len(conditions) == 1 and conditions[0]["id"] == EveryEventCondition.id
+        )
 
         if no_conditions and not only_has_every_event_cond:
-        ):
             raise Exception("No valid conditions, skipping migration")
 
         enabled = True
