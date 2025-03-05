538fc1df2d58eea8faed1e4181663250a6df97d5
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2025-03-05 15:48:11 +0100

    ref(ui): cleanup deprecated input component (#86379)

--- static/app/components/input.tsx
+++
@@ -1,6 +0,0 @@
-/**
- * @deprecated Import from `sentry/components/core/input` instead.
- */
-// biome-ignore lint/performance/noBarrelFile: Remove this once imports are fixed
-export * from 'sentry/components/core/input';
-export {Input as default} from 'sentry/components/core/input';
