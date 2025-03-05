5ed76a7e3114e2053fe8e4ead0aeed5f0ba9dfbc
Scott Cooper <scttcper@gmail.com>
2025-03-04 17:28:11 -0800

    fix(issues): Cleanup expect-errors in issue details components
    Some cleanup from when we turned on no implicit any

--- static/app/views/issueDetails/groupDetails.tsx
+++ static/app/views/issueDetails/groupDetails.tsx
@@ -581,8 +581,7 @@ const trackTabChanged = ({
   const analyticsData = event
     ? event.tags
         .filter(({key}) => ['device', 'os', 'browser'].includes(key))
-        .reduce((acc, {key, value}) => {
-          // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+        .reduce<Record<string, string>>((acc, {key, value}) => {
           acc[key] = value;
           return acc;
         }, {})
