4d585de79cab6df01d7621a3743fee3cdd8db6eb
Jonas <jonas.badalic@sentry.io>
2025-03-05 09:35:40 -0500

    checkbox: add chonk style (#86263)
    Implement chonk styling for checkbox component
    
    **Light mode**
    ![CleanShot 2025-03-03 at 21 09
    42@2x](https://github.com/user-attachments/assets/ee6324cc-b0e1-4dfb-8747-9eb6bb3cfe6e)
    
    **Dark mode**
    ![CleanShot 2025-03-03 at 21 09
    39@2x](https://github.com/user-attachments/assets/918cdbed-0170-40e0-acca-9ca9e4bfaa6c)

---
+++ static/app/components/core/checkbox/index.chonk.tsx
@@ -0,0 +1,48 @@
+import {chonkStyled} from 'sentry/utils/theme/theme.chonk';
+
+export const ChonkNativeHiddenCheckbox = chonkStyled('input')`
+  position: absolute;
+  opacity: 0;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+
+  & + * {
+    color: ${p => p.theme.colors.static.white};
+    border: 1px solid ${p => p.theme.colors.dynamic.surface100};
+
+    svg {
+      stroke: ${p => p.theme.colors.static.white};
+    }
+  }
+
+  &:focus-visible + * {
+    outline: none;
+    box-shadow: 0 0 0 2px ${p => p.theme.background},
+      0 0 0 4px ${p => p.theme.focusBorder};
+  }
+
+  &:disabled + *,
+  &[aria-disabled='true'] + * {
+    background-color: ${p => p.theme.colors.dynamic.surface500};
+    border: 1px solid ${p => p.theme.colors.dynamic.surface100};
+    cursor: not-allowed;
+  }
+
+  &:checked + *,
+  &:indeterminate + * {
+    background-color: ${p => p.theme.colors.static.blue400};
+    color: ${p => p.theme.white};
+  }
+
+  &:disabled:checked + *,
+  &:disabled:indeterminate + * {
+    background-color: ${p => p.theme.colors.static.blue400};
+    border: 1px solid ${p => p.theme.colors.static.blue400};
+    opacity: 0.6;
+  }
+`;
