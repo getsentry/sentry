88ef3014abeb4f81843349d312655b4bcd5e70ed
Kev <kevan.fisher@sentry.io>
2025-03-05 12:12:29 -0500

    fix(ourlogs): Add pagination test for logs
    This should pass once the backend respects the passed cursor

--- tests/snuba/api/endpoints/test_organization_events_ourlogs.py
+++ tests/snuba/api/endpoints/test_organization_events_ourlogs.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 
 import pytest
 
+from sentry.utils.cursors import Cursor
 from tests.snuba.api.endpoints.test_organization_events import OrganizationEventsEndpointTestBase
 
 
@@ -116,3 +117,37 @@ class OrganizationEventsOurLogsEndpointTest(OrganizationEventsEndpointTestBase):
         assert data[0]["log.body"] == "foo"
 
         assert meta["dataset"] == self.dataset
+
+    def test_pagination(self):
+        logs = [
+            self.create_ourlog(
+                {"body": "foo"},
+                timestamp=self.eleven_mins_ago,
+            ),
+            self.create_ourlog(
+                {"body": "bar"},
+                timestamp=self.ten_mins_ago,
+            ),
+            self.create_ourlog(
+                {"body": "baz"},
+                timestamp=self.nine_mins_ago,
+            ),
+        ]
+        self.store_ourlogs(logs)
+        response = self.do_request(
+            {
+                "field": ["log.body"],
+                "query": "",
+                "cursor": Cursor(0, 2, False, False),
+                "per_page": 2,
+                "orderby": "log.body",
+                "project": self.project.id,
+                "dataset": self.dataset,
+            }
+        )
+        assert response.status_code == 200, response.content
+        data = response.data["data"]
+        assert len(data) == 1
+        assert data == [
+            {"log.body": "baz"},
+        ]
