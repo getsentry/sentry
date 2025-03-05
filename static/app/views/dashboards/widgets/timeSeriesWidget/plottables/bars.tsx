1466c4fbea1c7a134939e9bd492a21c9de9c1936
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-03-05 11:51:50 -0500

    ref(dashboards): Move some widget files around (#86352)
    - Move `WidgetFrame` into `WidgetCard`, since this is now a
    Dashboards-only component
    - More formatters into a folder together, otherwise that folder has too
    many loose items
    - Move `TimeSeries` helpers into `utils`, or no one will ever find them

--- static/app/views/dashboards/widgets/timeSeriesWidget/plottables/bars.tsx
+++ static/app/views/dashboards/widgets/timeSeriesWidget/plottables/bars.tsx
@@ -2,9 +2,8 @@ import Color from 'color';
 import type {BarSeriesOption, LineSeriesOption} from 'echarts';
 
 import BarSeries from 'sentry/components/charts/series/barSeries';
-
-import {markDelayedData} from '../markDelayedData';
-import {timeSeriesItemToEChartsDataPoint} from '../timeSeriesItemToEChartsDataPoint';
+import {markDelayedData} from 'sentry/utils/timeSeries/markDelayedData';
+import {timeSeriesItemToEChartsDataPoint} from 'sentry/utils/timeSeries/timeSeriesItemToEChartsDataPoint';
 
 import {
   ContinuousTimeSeries,
