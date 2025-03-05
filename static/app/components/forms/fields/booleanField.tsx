2d5e75c92f9458298165f818ab6c1693e711b44a
JonasBa <jonas@badalic.com>
2025-03-05 09:21:30 -0500

    boolean: remove satisfies and explicit type

--- static/app/components/forms/fields/booleanField.tsx
+++ static/app/components/forms/fields/booleanField.tsx
@@ -61,7 +61,7 @@ export default class BooleanField extends Component<BooleanFieldProps> {
           const {type: _, ...propsWithoutType} = props;
           const switchProps: SwitchProps = {
             ...propsWithoutType,
-            size: 'lg' satisfies SwitchProps['size'],
+            size: 'lg',
             checked: !!value,
             disabled,
             onClick: handleChange,
@@ -79,7 +79,7 @@ export default class BooleanField extends Component<BooleanFieldProps> {
                   <Tooltip title={disabledReason} skipWrapper disabled={!disabled}>
                     <Switch
                       {...switchProps}
-                      onClick={(e: React.MouseEvent) => {
+                      onClick={e => {
                         // If we have a `confirm` prop and enabling switch
                         // Then show confirm dialog, otherwise propagate change as normal
                         // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
