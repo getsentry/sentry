41ce2da25a73f035558d9e8736f0e163c0302614
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-03-07 14:55:22 +0100

    debounce flush_segments

--- src/sentry/spans/consumers/process/factory.py
+++ src/sentry/spans/consumers/process/factory.py
@@ -2,9 +2,8 @@ import dataclasses
 import logging
 import time
 from collections import defaultdict
-from collections.abc import Callable, Mapping
+from collections.abc import Mapping
 from datetime import datetime
-from functools import partial
 from typing import Any
 
 import orjson
@@ -288,7 +287,10 @@ class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
         output_block_size: int | None,
     ):
         super().__init__()
+
+        # config
         self.buffer_v2 = buffer_v2
+        self.flush_interval = 1
         self.max_batch_size = max_batch_size
         self.max_batch_time = max_batch_time
         self.input_block_size = input_block_size
@@ -302,6 +304,7 @@ class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
         self.output_topic = ArroyoTopic(
             get_topic_definition(Topic.BUFFERED_SEGMENTS)["real_topic_name"]
         )
+        self.last_flush = 0
 
     def create_with_partitions(
         self,
@@ -317,12 +320,8 @@ class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
 
             unbatch = UnbatchStep(next_step=produce)
 
-            flush_fn: Callable[[Message[int]], ValuesBatch[FilteredPayload | KafkaPayload]] = (
-                partial(flush_segments_v2, max_segments=self.max_batch_size)
-            )
-
             flusher = RunTask(
-                function=flush_fn,
+                function=self.flush_segments_v2,
                 next_step=unbatch,
             )
 
@@ -380,6 +379,61 @@ class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
         self.producer.close()
         self.__pool.close()
 
+    def flush_segments_v2(
+        self, message: Message[int]
+    ) -> ValuesBatch[KafkaPayload | FilteredPayload]:
+        # TODO config
+        buffer = RedisSpansBufferV2()
+
+        now = message.payload
+        if now < self.last_flush + self.flush_interval:
+            return []
+
+        self.last_flush = now
+
+        # TODO: make max segments during flushing configurable
+        flushed_segments = buffer.flush_segments(max_segments=self.max_batch_size, now=now)
+
+        segment_messages: list[BaseValue[KafkaPayload | FilteredPayload]] = []
+
+        for segment_id, spans_set in flushed_segments.items():
+            # TODO: Check if this is correctly placed
+            segment_span_id = segment_to_span_id(segment_id)
+            if not spans_set:
+                # TODO: Fix a bug where we flush empty segments
+                logger.warning(
+                    "skipping segment without spans", extra={"segment_id": segment_span_id}
+                )
+                continue
+
+            segment_spans = []
+            for payload in spans_set:
+                val = rapidjson.loads(payload)
+                val["segment_id"] = segment_span_id
+                val["is_segment"] = segment_span_id == val["span_id"]
+                segment_spans.append(val)
+
+            value = Value(
+                payload=KafkaPayload(
+                    None, rapidjson.dumps({"spans": segment_spans}).encode("utf8"), []
+                ),
+                committable={},
+            )
+
+            segment_messages.append(value)
+
+        if segment_messages:
+            # TODO: remove once https://github.com/getsentry/arroyo/pull/427 lands
+            segment_messages[-1] = Value(
+                payload=segment_messages[-1].payload, committable=message.committable
+            )
+
+        # TODO: call done_flush_segments after commit, not here
+        # figure out how to flush when consumer is inactive
+        buffer.done_flush_segments(flushed_segments)
+
+        return segment_messages
+
 
 def process_batch_v2(values: Message[ValuesBatch[KafkaPayload]]) -> int:
     # TODO config
@@ -401,53 +455,3 @@ def process_batch_v2(values: Message[ValuesBatch[KafkaPayload]]) -> int:
     now = int(time.time())
     buffer.process_spans(spans, now=now)
     return now
-
-
-def flush_segments_v2(
-    message: Message[int], max_segments: int
-) -> ValuesBatch[KafkaPayload | FilteredPayload]:
-    # TODO config
-    buffer = RedisSpansBufferV2()
-
-    now = message.payload
-
-    # TODO: make max segments during flushing configurable
-    flushed_segments = buffer.flush_segments(max_segments=max_segments, now=now)
-
-    segment_messages: list[BaseValue[KafkaPayload | FilteredPayload]] = []
-
-    for segment_id, spans_set in flushed_segments.items():
-        # TODO: Check if this is correctly placed
-        segment_span_id = segment_to_span_id(segment_id)
-        if not spans_set:
-            # TODO: Fix a bug where we flush empty segments
-            logger.warning("skipping segment without spans", extra={"segment_id": segment_span_id})
-            continue
-
-        segment_spans = []
-        for payload in spans_set:
-            val = rapidjson.loads(payload)
-            val["segment_id"] = segment_span_id
-            val["is_segment"] = segment_span_id == val["span_id"]
-            segment_spans.append(val)
-
-        value = Value(
-            payload=KafkaPayload(
-                None, rapidjson.dumps({"spans": segment_spans}).encode("utf8"), []
-            ),
-            committable={},
-        )
-
-        segment_messages.append(value)
-
-    if segment_messages:
-        # TODO: remove once https://github.com/getsentry/arroyo/pull/427 lands
-        segment_messages[-1] = Value(
-            payload=segment_messages[-1].payload, committable=message.committable
-        )
-
-    # TODO: call done_flush_segments after commit, not here
-    # figure out how to flush when consumer is inactive
-    buffer.done_flush_segments(flushed_segments)
-
-    return segment_messages
