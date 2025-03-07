b68725d0bbf29aaa30227d942199006108ebc5d6
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2025-03-06 16:26:16 -0800

    chore(tasks): Add ignore and capture exception to retry decorator (#86566)

--- tests/sentry/tasks/test_base.py
+++ tests/sentry/tasks/test_base.py
@@ -1,8 +1,10 @@
+from unittest.mock import patch
+
 import pytest
 from django.test import override_settings
 
 from sentry.silo.base import SiloLimit, SiloMode
-from sentry.tasks.base import instrumented_task
+from sentry.tasks.base import instrumented_task, retry
 
 
 @instrumented_task(name="test.tasks.test_base.region_task", silo_mode=SiloMode.REGION)
@@ -13,6 +15,30 @@ def region_task(param):
 @instrumented_task(name="test.tasks.test_base.control_task", silo_mode=SiloMode.CONTROL)
 def control_task(param):
     return f"Control task {param}"
+
+
+@retry(ignore_and_capture=(Exception,))
+@instrumented_task(name="test.tasks.test_base.ignore_and_capture_task", silo_mode=SiloMode.CONTROL)
+def ignore_and_capture_retry_task(param):
+    raise Exception(param)
+
+
+@retry(on=(Exception,))
+@instrumented_task(name="test.tasks.test_base.retry_task", silo_mode=SiloMode.CONTROL)
+def retry_on_task(param):
+    raise Exception(param)
+
+
+@retry(ignore=(Exception,))
+@instrumented_task(name="test.tasks.test_base.ignore_task", silo_mode=SiloMode.CONTROL)
+def ignore_on_exception_task(param):
+    raise Exception(param)
+
+
+@retry(exclude=(Exception,))
+@instrumented_task(name="test.tasks.test_base.exclude_task", silo_mode=SiloMode.CONTROL)
+def exclude_on_exception_task(param):
+    raise Exception(param)
 
 
 @override_settings(SILO_MODE=SiloMode.REGION)
@@ -36,6 +62,39 @@ def test_task_silo_limit_call_control():
 def test_task_silo_limit_call_monolith():
     assert "Region task hi" == region_task("hi")
     assert "Control task hi" == control_task("hi")
+
+
+@patch("sentry.tasks.base.capture_exception")
+def test_ignore_and_retry(capture_exception):
+    ignore_and_capture_retry_task("bruh")
+
+    assert capture_exception.call_count == 1
+
+
+@patch("sentry.tasks.base.capture_exception")
+def test_ignore_exception_retry(capture_exception):
+    ignore_on_exception_task("bruh")
+
+    assert capture_exception.call_count == 0
+
+
+@patch("sentry.tasks.base.capture_exception")
+def test_exclude_exception_retry(capture_exception):
+    with pytest.raises(Exception):
+        exclude_on_exception_task("bruh")
+
+    assert capture_exception.call_count == 0
+
+
+@patch("sentry.tasks.base.current_task")
+@patch("sentry.tasks.base.capture_exception")
+def test_retry_on(capture_exception, current_task):
+
+    # In reality current_task.retry will cause the given exception to be re-raised but we patch it here so no need to .raises :bufo-shrug:
+    retry_on_task("bruh")
+
+    assert capture_exception.call_count == 1
+    assert current_task.retry.call_count == 1
 
 
 @pytest.mark.parametrize(
