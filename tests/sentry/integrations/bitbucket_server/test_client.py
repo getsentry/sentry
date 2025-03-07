0cc3a49374802c81f3e124cfc05f8b438b5e34b9
Jian Yuan Lee <jianyuan@gmail.com>
2025-03-07 23:07:02 +0000

    feat(bitbucket-server): CODEOWNERS support and stacktrace linking

--- tests/sentry/integrations/bitbucket_server/test_client.py
+++ tests/sentry/integrations/bitbucket_server/test_client.py
@@ -1,19 +1,29 @@
 import orjson
+import pytest
 import responses
 from django.test import override_settings
 from requests import Request
 
 from fixtures.bitbucket_server import REPO
-from sentry.integrations.bitbucket_server.client import (
-    BitbucketServerAPIPath,
-    BitbucketServerClient,
-)
+from sentry.integrations.bitbucket_server.client import BitbucketServerAPIPath
+from sentry.integrations.bitbucket_server.integration import BitbucketServerIntegration
+from sentry.models.repository import Repository
+from sentry.shared_integrations.exceptions import ApiError
+from sentry.shared_integrations.response.base import BaseApiResponse
+from sentry.silo.base import SiloMode
 from sentry.testutils.cases import BaseTestCase, TestCase
-from sentry.testutils.silo import control_silo_test
+from sentry.testutils.helpers.integrations import get_installation_of_type
+from sentry.testutils.silo import assume_test_silo_mode, control_silo_test
 from tests.sentry.integrations.jira_server import EXAMPLE_PRIVATE_KEY
 
 control_address = "http://controlserver"
 secret = "hush-hush-im-invisible"
+
+BITBUCKET_SERVER_CODEOWNERS = {
+    "filepath": ".bitbucket/CODEOWNERS",
+    "html_url": "https://bitbucket.example.com/projects/PROJ/repos/repository-name/browse/.bitbucket/CODEOWNERS?at=master",
+    "raw": "docs/* @jianyuan @getsentry/ecosystem\n* @jianyuan\n",
+}
 
 
 @override_settings(
@@ -44,8 +54,23 @@ class BitbucketServerClientTest(TestCase, BaseTestCase):
         self.integration.add_organization(
             self.organization, self.user, default_auth_id=self.identity.id
         )
-        self.install = self.integration.get_installation(self.organization.id)
-        self.bb_server_client: BitbucketServerClient = self.install.get_client()
+        self.install = get_installation_of_type(
+            BitbucketServerIntegration, self.integration, self.organization.id
+        )
+        self.bb_server_client = self.install.get_client()
+
+        with assume_test_silo_mode(SiloMode.REGION):
+            self.repo = Repository.objects.create(
+                provider=self.integration.provider,
+                name="PROJ/repository-name",
+                organization_id=self.organization.id,
+                config={
+                    "name": "TEST/repository-name",
+                    "project": "PROJ",
+                    "repo": "repository-name",
+                },
+                integration_id=self.integration.id,
+            )
 
     def test_authorize_request(self):
         method = "GET"
@@ -81,3 +106,126 @@ class BitbucketServerClientTest(TestCase, BaseTestCase):
 
         assert len(responses.calls) == 1
         assert "oauth_consumer_key" in responses.calls[0].request.headers["Authorization"]
+
+    @responses.activate
+    def test_check_file(self):
+        path = "src/sentry/integrations/bitbucket_server/client.py"
+        version = "master"
+        url = self.bb_server_client.base_url + BitbucketServerAPIPath.source.format(
+            project=self.repo.config["project"],
+            repo=self.repo.config["repo"],
+            path=path,
+            sha=version,
+        )
+
+        responses.add(
+            responses.HEAD,
+            url=url,
+            status=200,
+        )
+
+        resp = self.bb_server_client.check_file(self.repo, path, version)
+        assert isinstance(resp, BaseApiResponse)
+        assert resp.status_code == 200
+
+    @responses.activate
+    def test_check_no_file(self):
+        path = "src/santry/integrations/bitbucket_server/client.py"
+        version = "master"
+        url = self.bb_server_client.base_url + BitbucketServerAPIPath.source.format(
+            project=self.repo.config["project"],
+            repo=self.repo.config["repo"],
+            path=path,
+            sha=version,
+        )
+
+        responses.add(
+            responses.HEAD,
+            url=url,
+            status=404,
+        )
+
+        with pytest.raises(ApiError):
+            self.bb_server_client.check_file(self.repo, path, version)
+
+    @responses.activate
+    def test_get_file(self):
+        path = "src/sentry/integrations/bitbucket_server/client.py"
+        version = "master"
+        url = self.bb_server_client.base_url + BitbucketServerAPIPath.raw.format(
+            project=self.repo.config["project"],
+            repo=self.repo.config["repo"],
+            path=path,
+            sha=version,
+        )
+
+        responses.add(
+            responses.GET,
+            url=url,
+            body="Hello, world!",
+            status=200,
+        )
+
+        resp = self.bb_server_client.get_file(self.repo, path, version)
+        assert resp == "Hello, world!"
+
+    @responses.activate
+    def test_get_stacktrace_link(self):
+        path = "src/sentry/integrations/bitbucket/client.py"
+        version = "master"
+        url = self.bb_server_client.base_url + BitbucketServerAPIPath.source.format(
+            project=self.repo.config["project"],
+            repo=self.repo.config["repo"],
+            path=path,
+            sha=version,
+        )
+
+        responses.add(
+            method=responses.HEAD,
+            url=url,
+            status=200,
+        )
+
+        source_url = self.install.get_stacktrace_link(self.repo, path, "master", version)
+        assert (
+            source_url
+            == "https://bitbucket.example.com/projects/PROJ/repos/repository-name/browse/src/sentry/integrations/bitbucket/client.py?at=master"
+        )
+
+    @responses.activate
+    def test_get_codeowner_file(self):
+        self.config = self.create_code_mapping(
+            repo=self.repo,
+            project=self.project,
+        )
+
+        path = ".bitbucket/CODEOWNERS"
+        source_url = self.bb_server_client.base_url + BitbucketServerAPIPath.source.format(
+            project=self.repo.config["project"],
+            repo=self.repo.config["repo"],
+            path=path,
+            sha=self.config.default_branch,
+        )
+        raw_url = self.bb_server_client.base_url + BitbucketServerAPIPath.raw.format(
+            project=self.repo.config["project"],
+            repo=self.repo.config["repo"],
+            path=path,
+            sha=self.config.default_branch,
+        )
+
+        responses.add(
+            method=responses.HEAD,
+            url=source_url,
+            status=200,
+        )
+        responses.add(
+            method=responses.GET,
+            url=raw_url,
+            content_type="text/plain",
+            body=BITBUCKET_SERVER_CODEOWNERS["raw"],
+        )
+
+        result = self.install.get_codeowner_file(
+            self.config.repository, ref=self.config.default_branch
+        )
+        assert result == BITBUCKET_SERVER_CODEOWNERS
