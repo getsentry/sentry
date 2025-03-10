f5832e55d78a139785e7cd860164aa4331f51484
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2025-03-10 09:48:09 -0700

    feat(autofix): Get state checks repo access every minute (#86638)
    Utilizing memcache, we run the repo access check at most every minute,
    this way when users come back from setting up/fixing their GitHub
    integration the warning will be updated.
    
    Tested locally between sentry and seer

--- src/sentry/autofix/utils.py
+++ src/sentry/autofix/utils.py
@@ -85,13 +85,14 @@ def get_autofix_repos_from_project_code_mappings(project: Project) -> list[dict]
 
 
 def get_autofix_state(
-    *, group_id: int | None = None, run_id: int | None = None
+    *, group_id: int | None = None, run_id: int | None = None, check_repo_access: bool = False
 ) -> AutofixState | None:
     path = "/v1/automation/autofix/state"
     body = orjson.dumps(
         {
             "group_id": group_id,
             "run_id": run_id,
+            "check_repo_access": check_repo_access,
         }
     )
 
