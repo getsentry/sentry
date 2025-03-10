dec8feb59d0dbe730e9985691b06422be685dedf
Nar Saynorath <nar.saynorath@sentry.io>
2025-03-10 16:11:14 -0400

    fix(dashboards): Ignore release health widget limit (#86724)
    Re-add this because it was mistakenly removed when resolving
    a merge conflict

--- static/app/views/dashboards/widgetCard/widgetCardDataLoader.tsx
+++ static/app/views/dashboards/widgetCard/widgetCardDataLoader.tsx
@@ -87,7 +87,7 @@ export function WidgetCardDataLoader({
         organization={organization}
         widget={widget}
         selection={selection}
-        limit={widget.limit ?? tableItemLimit}
+        limit={tableItemLimit}
         onDataFetched={onDataFetched}
         dashboardFilters={dashboardFilters}
       >
