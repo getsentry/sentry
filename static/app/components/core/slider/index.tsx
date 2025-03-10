824ddd06ab6119fa6e0d43b8f3bd4a507d5d6908
JonasBa <jonas@badalic.com>
2025-03-10 14:52:16 -0400

    revert style change as emotion doesnt support it?

--- static/app/components/core/slider/index.tsx
+++ static/app/components/core/slider/index.tsx
@@ -22,7 +22,15 @@ function SliderStyles(p: {theme: Theme}) {
     width: 100%;
     background: transparent;
 
-    &::-webkit-slider-runnable-track,
+    &::-webkit-slider-runnable-track {
+      width: 100%;
+      height: 3px;
+      cursor: pointer;
+      background: ${p.theme.border};
+      border-radius: 3px;
+      border: 0;
+    }
+
     &::-moz-range-track {
       width: 100%;
       height: 3px;
@@ -32,7 +40,23 @@ function SliderStyles(p: {theme: Theme}) {
       border: 0;
     }
 
-    &::-webkit-slider-thumb,
+    &::-webkit-slider-thumb {
+      box-shadow: 0 0 0 3px ${p.theme.background};
+      height: 17px;
+      width: 17px;
+      border-radius: 50%;
+      background: ${p.theme.active};
+      cursor: pointer;
+      /* stylelint-disable-next-line property-no-vendor-prefix */
+      -webkit-appearance: none;
+      appearance: none;
+      margin-top: -7px;
+      border: 0;
+      transition:
+        background 0.1s,
+        box-shadow 0.1s;
+    }
+
     &::-moz-range-thumb {
       box-shadow: 0 0 0 3px ${p.theme.background};
       height: 17px;
@@ -58,30 +82,45 @@ function SliderStyles(p: {theme: Theme}) {
     }
 
     &[disabled] {
-      &::-webkit-slider-thumb,
+      &::-webkit-slider-thumb {
+        background: ${p.theme.border};
+        cursor: default;
+      }
       &::-moz-range-thumb {
         background: ${p.theme.border};
         cursor: default;
       }
 
-      &::-webkit-slider-runnable-track,
+      &::-webkit-slider-runnable-track {
+        cursor: default;
+      }
       &::-moz-range-track {
         cursor: default;
       }
     }
 
-    &:not([disabled])::-webkit-slider-runnable-track:hover,
+    &:not([disabled])::-webkit-slider-runnable-track:hover {
+      background: ${p.theme.activeHover};
+    }
     &:not([disabled])::-moz-range-track:hover {
       background: ${p.theme.activeHover};
     }
 
-    &:focus::-webkit-slider-thumb,
+    &:focus::-webkit-slider-thumb {
+      box-shadow:
+        ${p.theme.background} 0 0 0 3px,
+        ${p.theme.focus} 0 0 0 6px;
+    }
     &:focus-visible::-webkit-slider-thumb {
       box-shadow:
         ${p.theme.background} 0 0 0 3px,
         ${p.theme.focus} 0 0 0 6px;
     }
-    &:focus::-moz-range-thumb,
+    &:focus::-moz-range-thumb {
+      box-shadow:
+        ${p.theme.background} 0 0 0 3px,
+        ${p.theme.focus} 0 0 0 6px;
+    }
     &:focus-visible::-moz-range-thumb {
       box-shadow:
         ${p.theme.background} 0 0 0 3px,
