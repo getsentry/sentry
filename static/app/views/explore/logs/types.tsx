3fbf32b34043ee69b0b5491c74b25ebe19858945
Kev <6111995+k-fish@users.noreply.github.com>
2025-03-10 12:28:04 -0400

    feat(ourlogs): Allow column editing, resizing etc. (#86627)
    ### Summary
    This allows columns in the logs table to be edited, resized, and set via
    url.
    
    #### Screenshot
    
    |![Screenshot 2025-03-07 at 3 59
    49 PM](https://github.com/user-attachments/assets/17926e36-17b1-4775-a144-aba0e8e7438f)|![Screenshot
    2025-03-07 at 4 05
    01 PM](https://github.com/user-attachments/assets/bc356ea1-c613-47be-9517-733ce93b1c71)|
    |-|-|

--- static/app/views/explore/logs/types.tsx
+++ static/app/views/explore/logs/types.tsx
@@ -1,20 +1,27 @@
+import type {
+  ColumnValueType,
+  CountUnit,
+  CurrencyUnit,
+  DurationUnit,
+  PercentageUnit,
+  PercentChangeUnit,
+} from 'sentry/utils/discover/fields';
+
 type OurLogCustomFieldKey = string; // We could brand this for nominal types.
 
 // This enum is used to represent known fields or attributes in the logs response.
 export enum OurLogKnownFieldKey {
+  TRACE_ID = 'sentry.trace_id',
   ID = 'sentry.item_id',
-  BODY = 'log.body',
-  SEVERITY_NUMBER = 'log.severity_number',
-  SEVERITY_TEXT = 'log.severity_text',
-  SENTRY_SEVERITY_NUMBER = 'sentry.severity_number',
-  SENTRY_SEVERITY_TEXT = 'sentry.severity_text',
-  ORGANIZATION_ID = 'organization_id',
-  SENTRY_ORGANIZATION_ID = 'sentry.organization_id',
+  BODY = 'sentry.body',
+  SEVERITY_NUMBER = 'sentry.severity_number',
+  SEVERITY_TEXT = 'sentry.severity_text',
+  ORGANIZATION_ID = 'sentry.organization_id',
   PROJECT_ID = 'project_id',
   SENTRY_PROJECT_ID = 'sentry.project_id',
   SPAN_ID = 'sentry.span_id',
   TIMESTAMP = 'timestamp',
-  SENTRY_ITEM_TYPE = 'sentry.item_type',
+  ITEM_TYPE = 'sentry.item_type',
 }
 
 export type OurLogFieldKey = OurLogCustomFieldKey | OurLogKnownFieldKey;
@@ -33,3 +40,23 @@ type OurLogsCustomFieldResponseMap = Record<OurLogCustomFieldKey, string | numbe
 
 export type OurLogsResponseItem = OurLogsKnownFieldResponseMap &
   OurLogsCustomFieldResponseMap;
+
+export type LogAttributeUnits =
+  | null
+  | DurationUnit
+  | CurrencyUnit
+  | PercentageUnit
+  | PercentChangeUnit
+  | CountUnit;
+
+export interface LogRowItem {
+  fieldKey: OurLogFieldKey;
+  metaFieldType: ColumnValueType;
+  unit: LogAttributeUnits;
+  value: OurLogsResponseItem[OurLogFieldKey];
+}
+
+export interface LogAttributeItem {
+  fieldKey: OurLogFieldKey;
+  value: OurLogsResponseItem[OurLogFieldKey] | null;
+}
