83dcb0eae44cea93f9ebd3ce6f4fdb0731cae0e1
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2025-03-11 13:43:24 -0700

    feat(sentry apps): Update sentry app components to return errors (#84187)

---
+++ tests/sentry/sentry_apps/utils/test_errors.py
@@ -0,0 +1,25 @@
+from sentry.sentry_apps.utils.errors import SentryAppError, SentryAppIntegratorError
+from sentry.testutils.cases import TestCase
+
+
+class TestSentryAppBaseError(TestCase):
+
+    def test_to_public_dict(self):
+        error = SentryAppError(message="brooo", public_context={"omg": "omgggg"})
+        body = error.to_public_dict()
+
+        assert body == {"detail": "brooo", "context": {"omg": "omgggg"}}
+
+    def test_to_public_dict_no_webhook_context(self):
+        error = SentryAppIntegratorError(
+            message="brooo", public_context={"omg": "omgggg"}, webhook_context={"bruh": "bruhh"}
+        )
+        body = error.to_public_dict()
+
+        assert body == {"detail": "brooo", "context": {"omg": "omgggg"}}
+
+    def test_to_public_dict_no_context(self):
+        error = SentryAppIntegratorError(message="brooo")
+        body = error.to_public_dict()
+
+        assert body == {"detail": "brooo"}
