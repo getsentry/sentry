d750b8d73711f73425ed19e375b43a81b97d6b1d
Michelle Fu <michelle.fu@sentry.io>
2025-03-11 14:31:56 -0700

    log

--- src/sentry/incidents/subscription_processor.py
+++ src/sentry/incidents/subscription_processor.py
@@ -444,6 +444,12 @@ class SubscriptionProcessor:
                     last_update=self.last_update.timestamp(),
                     aggregation_value=aggregation_value,
                 )
+            # XXX (mifu67): log problematic rule, to be deleted later
+            if self.alert_rule.id == 319175:
+                logger.info(
+                    "Received this response from Seer",
+                    extra={"potential_anomalies": potential_anomalies},
+                )
             if potential_anomalies is None:
                 logger.info(
                     "No potential anomalies found",
