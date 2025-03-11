c1ee5837ab9dea278b0ba1a2b714af4bc9a71c69
Buck Evan <buck.evan@sentry.io>
2025-03-11 16:28:29 -0500

    DEVINFRA-630: record "rerun" test results
    blocked by: getsentry/devinfra-metrics#77

--- src/sentry/testutils/pytest/json_report_reruns.py
+++ src/sentry/testutils/pytest/json_report_reruns.py
@@ -1,34 +1,15 @@
-"""Extend the pytest-json-report plugin with a new "reruns" attribute.
-
-Each test will (if it's run more than once) store its prior results in a
-`reruns` array attribute, where each item may have `setup` `call` and
-`teardown` sections.
-
-See also:
-    * https://github.com/pytest-dev/pytest-rerunfailures
-    * https://github.com/numirias/pytest-json-report
-    * https://getsentry.atlassian.net/browse/DEVINFRA-630
-"""
-
-import pytest
 import pytest_jsonreport.plugin
-
-JSONTestItem = dict[str, list[object]]
-JSONTestItems = dict[str, JSONTestItem]
-
-
-def pytest_plugin_registered(plugin: object, manager: pytest.PytestPluginManager) -> None:
-    """Only register if/when the plugin we're extending is registered."""
-    if isinstance(plugin, pytest_jsonreport.plugin.JSONReport):
-        json_tests = getattr(plugin, "_json_tests")
-        manager.register(PytestRerunJSONReporter(json_tests))
 
 
 class PytestRerunJSONReporter:
-    def __init__(self, json_tests: JSONTestItems):
-        self.json_tests = json_tests
+    json_tests: None | dict[str, object] = None
 
-    def pytest_json_runtest_stage(self, report: pytest.TestReport) -> None:
+    def pytest_plugin_registered(self, plugin, manager):
+        del manager
+        if isinstance(plugin, pytest_jsonreport.plugin.JSONReport):
+            self.json_tests = plugin._json_tests
+
+    def pytest_json_runtest_stage(self, report):
         assert self.json_tests is not None
 
         nodeid = report.nodeid
@@ -44,3 +25,7 @@ class PytestRerunJSONReporter:
                     if key in json_testitem
                 }
             )
+
+
+def pytest_configure(config):
+    config.pluginmanager.register(PytestRerunJSONReporter())
