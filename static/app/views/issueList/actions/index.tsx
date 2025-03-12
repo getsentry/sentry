1a61c3fb7091986919d3151c5ae018574df0113b
Malachi Willey <malwilley@gmail.com>
2025-03-12 09:24:42 -0700

    fix(issue-stream): Various issue stream style tweaks (#86859)
    - Improve how dividers are rendered to avoid having a trailing divider
    at small widths
    - Right align time/number columns

--- static/app/views/issueList/actions/index.tsx
+++ static/app/views/issueList/actions/index.tsx
@@ -26,7 +26,6 @@ import {trackAnalytics} from 'sentry/utils/analytics';
 import {uniq} from 'sentry/utils/array/uniq';
 import {useQueryClient} from 'sentry/utils/queryClient';
 import useApi from 'sentry/utils/useApi';
-import {useBreakpoints} from 'sentry/utils/useBreakpoints';
 import useMedia from 'sentry/utils/useMedia';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useSyncedLocalStorageState} from 'sentry/utils/useSyncedLocalStorageState';
@@ -103,7 +102,6 @@ function ActionsBarPriority({
 }) {
   const organization = useOrganization();
   const shouldDisplayActions = anySelected && !narrowViewport;
-  const screen = useBreakpoints();
 
   return (
     <ActionsBarContainer
@@ -141,9 +139,7 @@ function ActionsBarPriority({
             </HeaderButtonsWrapper>
           ) : organization.features.includes('issue-stream-table-layout') ? (
             <NarrowHeaderButtonsWrapper>
-              <IssueStreamHeaderLabel>{t('Issue')}</IssueStreamHeaderLabel>
-              {/* Ideally we could use a smaller option, xxsmall */}
-              {screen.xsmall && <HeaderDivider />}
+              <IssueStreamHeaderLabel hideDivider>{t('Issue')}</IssueStreamHeaderLabel>
             </NarrowHeaderButtonsWrapper>
           ) : (
             <HeaderButtonsWrapper key="sort" {...animationProps}>
