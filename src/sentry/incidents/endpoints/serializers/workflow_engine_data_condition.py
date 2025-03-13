14ea319d5feb30134fd1fb8ba8d992b320954a19
Colleen O'Rourke <colleen@sentry.io>
2025-03-13 15:13:16 -0700

    dont use old tables for the action, update tests

--- src/sentry/incidents/endpoints/serializers/workflow_engine_data_condition.py
+++ src/sentry/incidents/endpoints/serializers/workflow_engine_data_condition.py
@@ -1,5 +1,6 @@
 from collections import defaultdict
-from typing import DefaultDict
+from collections.abc import Mapping, MutableMapping, Sequence
+from typing import Any, DefaultDict
 
 from sentry.api.serializers import Serializer, serialize
 from sentry.incidents.endpoints.serializers.workflow_engine_action import (
@@ -11,6 +12,7 @@ from sentry.incidents.models.alert_rule import (
     AlertRuleTrigger,
     AlertRuleTriggerAction,
 )
+from sentry.users.models.user import User
 from sentry.workflow_engine.models import (
     Action,
     ActionAlertRuleTriggerAction,
@@ -24,7 +26,9 @@ from sentry.workflow_engine.types import DetectorPriorityLevel
 
 
 class WorkflowEngineDataConditionSerializer(Serializer):
-    def get_attrs(self, item_list, user, **kwargs):
+    def get_attrs(
+        self, item_list: Sequence[DataCondition], user: User, **kwargs: Any
+    ) -> MutableMapping[Action, MutableMapping[str, Any]]:
         data_conditions = {item.id: item for item in item_list}
         data_condition_groups = [
             data_condition.condition_group for data_condition in data_conditions.values()
@@ -42,7 +46,6 @@ class WorkflowEngineDataConditionSerializer(Serializer):
         actions = Action.objects.filter(
             id__in=[dcga.action_id for dcga in data_condition_group_actions]
         ).order_by("id")
-        kwargs = {}  # maybe pass something to help lookup the ActionAlertRuleTriggerAction?
         serialized_actions = serialize(
             list(actions), user, WorkflowEngineActionSerializer(), **kwargs
         )
@@ -57,7 +60,9 @@ class WorkflowEngineDataConditionSerializer(Serializer):
             triggers_actions.append(serialized)
         return result
 
-    def serialize(self, obj, attrs, user, **kwargs):
+    def serialize(
+        self, obj: DataCondition, attrs: Mapping[str, Any], user: User, **kwargs: Any
+    ) -> dict[str, Any]:
         # XXX: we are assuming that the obj/DataCondition is a detector trigger
         detector = Detector.objects.get(workflow_condition_group=obj.condition_group)
         alert_rule_detector = AlertRuleDetector.objects.get(detector=detector)
