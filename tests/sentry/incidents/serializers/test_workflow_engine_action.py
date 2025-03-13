43f6baaf03c706098d1f2a6b5e869a3f142eb888
Colleen O'Rourke <colleen@sentry.io>
2025-03-13 15:13:16 -0700

    dont use old tables for the action, update tests

--- tests/sentry/incidents/serializers/test_workflow_engine_action.py
+++ tests/sentry/incidents/serializers/test_workflow_engine_action.py
@@ -1,19 +1,22 @@
+
 from sentry.api.serializers import serialize
-from sentry.incidents.endpoints.serializers.workflow_engine_action import (
-    WorkflowEngineActionSerializer,
+from sentry.incidents.endpoints.serializers.workflow_engine_data_condition import (
+    WorkflowEngineDataConditionSerializer,
 )
-from sentry.incidents.models.alert_rule import AlertRuleTriggerAction
+from sentry.incidents.endpoints.utils import translate_data_condition_type
+from sentry.incidents.models.alert_rule import AlertRuleThresholdType
 from sentry.testutils.cases import TestCase
 from sentry.testutils.helpers.datetime import freeze_time
 from sentry.workflow_engine.migration_helpers.alert_rule import (
     migrate_alert_rule,
     migrate_metric_action,
     migrate_metric_data_conditions,
+    migrate_resolve_threshold_data_conditions,
 )
 
 
 @freeze_time("2018-12-11 03:21:34")
-class TestActionSerializer(TestCase):
+class TestDataConditionSerializer(TestCase):
     def setUp(self) -> None:
         self.alert_rule = self.create_alert_rule()
         self.critical_trigger = self.create_alert_rule_trigger(
@@ -22,11 +25,22 @@ class TestActionSerializer(TestCase):
         self.critical_trigger_action = self.create_alert_rule_trigger_action(
             alert_rule_trigger=self.critical_trigger
         )
-        migrate_alert_rule(self.alert_rule)
-        migrate_metric_data_conditions(self.critical_trigger)
-        self.action, _, _ = migrate_metric_action(self.critical_trigger_action)
+        self.warning_trigger = self.create_alert_rule_trigger(
+            alert_rule=self.alert_rule, label="warning"
+        )
+        self.warning_trigger_action = self.create_alert_rule_trigger_action(
+            alert_rule_trigger=self.warning_trigger
+        )
+        _, _, _, self.detector, _, _, _, _ = migrate_alert_rule(self.alert_rule)
+        self.critical_detector_trigger, _ = migrate_metric_data_conditions(self.critical_trigger)
+        self.warning_detector_trigger, _ = migrate_metric_data_conditions(self.warning_trigger)
+        self.critical_action, _, _ = migrate_metric_action(self.critical_trigger_action)
+        self.warning_action, _, _ = migrate_metric_action(self.warning_trigger_action)
+        self.resolve_trigger_data_condition, _ = migrate_resolve_threshold_data_conditions(
+            self.alert_rule
+        )
 
-        self.expected = {
+        self.expected_actions = {
             "id": str(self.critical_trigger_action.id),
             "alertRuleTriggerId": str(self.critical_trigger.id),
             "type": "email",
@@ -35,117 +49,83 @@ class TestActionSerializer(TestCase):
             "inputChannelId": None,
             "integrationId": None,
             "sentryAppId": None,
-            "dateCreated": self.action.date_added,
+            "dateCreated": self.critical_trigger_action.date_added,
             "desc": f"Send a notification to {self.user.email}",
-            "priority": self.action.data.get("priority"),
+            "priority": self.critical_action.data.get("priority"),
+        }
+        self.expected_trigger = {
+            "id": str(self.critical_trigger.id),
+            "alertRuleId": str(self.alert_rule.id),
+            "label": "critical",
+            "thresholdType": AlertRuleThresholdType.ABOVE.value,
+            "alertThreshold": self.critical_detector_trigger.comparison,
+            "resolveThreshold": AlertRuleThresholdType.BELOW,
+            "dateCreated": self.critical_trigger.date_added,
+            # "actions": self.expected_actions, # use this after action serializer pr is merged
+            "actions": [],
         }
 
     def test_simple(self) -> None:
-        serialized_action = serialize(self.action, self.user, WorkflowEngineActionSerializer())
-        assert serialized_action == self.expected
+        serialized_data_condition = serialize(
+            self.critical_detector_trigger,
+            self.user,
+            WorkflowEngineDataConditionSerializer(),
+        )
+        assert serialized_data_condition == self.expected_trigger
+
+    def test_comparison_delta(self) -> None:
+        comparison_delta_rule = self.create_alert_rule(comparison_delta=60)
+        comparison_delta_trigger = self.create_alert_rule_trigger(
+            alert_rule=comparison_delta_rule, label="critical"
+        )
+        comparison_delta_trigger_action = self.create_alert_rule_trigger_action(
+            alert_rule_trigger=comparison_delta_trigger
+        )
+        _, _, _, detector, _, _, _, _ = migrate_alert_rule(comparison_delta_rule)
+        comparison_detector_trigger, _ = migrate_metric_data_conditions(comparison_delta_trigger)
+        resolve_trigger_data_condition, _ = migrate_resolve_threshold_data_conditions(
+            comparison_delta_rule
+        )
+        action, _, _ = migrate_metric_action(comparison_delta_trigger_action)
+
+        serialized_data_condition = serialize(
+            comparison_detector_trigger,
+            self.user,
+            WorkflowEngineDataConditionSerializer(),
+        )
+        expected_actions = self.expected_actions.copy()
+        expected_actions["id"] = str(comparison_delta_trigger_action.id)
+        expected_actions["alertRuleTriggerId"] = str(comparison_delta_trigger.id)
+
+        expected_trigger = self.expected_trigger.copy()
+        # expected_trigger["actions"] = expected_actions  # use this after action serializer pr is merged
+        expected_trigger["alertThreshold"] = translate_data_condition_type(
+            detector.config.get("comparison_delta"),
+            comparison_detector_trigger.type,
+            comparison_detector_trigger.comparison,
+        )
+        expected_trigger["id"] = str(comparison_delta_trigger.id)
+        expected_trigger["alertRuleId"] = str(comparison_delta_rule.id)
+        assert serialized_data_condition == expected_trigger
 
     def test_warning_trigger(self) -> None:
-        """
-        Test that we can differentiate between critical and warning triggers
-        """
-        self.og_team = self.create_team(organization=self.organization)
-        self.warning_trigger = self.create_alert_rule_trigger(
-            alert_rule=self.alert_rule, label="warning"
+        serialized_data_condition = serialize(
+            self.warning_detector_trigger,
+            self.user,
+            WorkflowEngineDataConditionSerializer(),
         )
-        self.warning_trigger_action = self.create_alert_rule_trigger_action(
-            alert_rule_trigger=self.warning_trigger,
-            target_type=AlertRuleTriggerAction.TargetType.TEAM,
-            target_identifier=str(self.og_team.id),
-        )
-        migrate_metric_data_conditions(self.warning_trigger)
-        self.warning_action, _, _ = migrate_metric_action(self.warning_trigger_action)
+        expected_actions = self.expected_actions.copy()
+        expected_actions["id"] = str(self.warning_trigger_action.id)
+        expected_actions["alertRuleTriggerId"] = str(self.warning_trigger.id)
 
-        serialized_action = serialize(
-            self.warning_action, self.user, WorkflowEngineActionSerializer()
+        expected_trigger = self.expected_trigger.copy()
+        # expected_trigger["actions"] = expected_actions  # use this after action serializer pr is merged
+        expected_trigger["alertThreshold"] = translate_data_condition_type(
+            self.detector.config.get("comparison_delta"),
+            self.resolve_trigger_data_condition.type,
+            self.warning_detector_trigger.comparison,
         )
-        warning_expected = self.expected.copy()
-        warning_expected["id"] = str(self.warning_trigger_action.id)
-        warning_expected["alertRuleTriggerId"] = str(self.warning_trigger.id)
-        warning_expected["targetType"] = "team"
-        warning_expected["targetIdentifier"] = str(self.og_team.id)
-        warning_expected["dateCreated"] = self.warning_action.date_added
-        warning_expected["desc"] = f"Send an email to members of #{self.og_team.slug}"
-        warning_expected["priority"] = self.warning_action.data.get("priority")
-        assert serialized_action == warning_expected
-
-    def test_sentry_app_action(self) -> None:
-        sentry_app = self.create_sentry_app(
-            organization=self.organization,
-            published=True,
-            verify_install=False,
-            name="Super Awesome App",
-            schema={"elements": [self.create_alert_rule_action_schema()]},
-        )
-        self.sentry_app_trigger = self.create_alert_rule_trigger(
-            alert_rule=self.alert_rule, label="warning"
-        )
-        self.create_sentry_app_installation(
-            slug=sentry_app.slug, organization=self.organization, user=self.user
-        )
-        self.sentry_app_trigger_action = self.create_alert_rule_trigger_action(
-            alert_rule_trigger=self.sentry_app_trigger,
-            type=AlertRuleTriggerAction.Type.SENTRY_APP,
-            target_identifier=sentry_app.id,
-            target_type=AlertRuleTriggerAction.TargetType.SENTRY_APP,
-            sentry_app=sentry_app,
-            sentry_app_config=[
-                {"name": "title", "value": "An alert"},
-            ],
-        )
-        migrate_metric_data_conditions(self.sentry_app_trigger)
-        self.sentry_app_action, _, _ = migrate_metric_action(self.sentry_app_trigger_action)
-
-        serialized_action = serialize(
-            self.sentry_app_action, self.user, WorkflowEngineActionSerializer()
-        )
-        sentry_app_expected = self.expected.copy()
-        sentry_app_expected["type"] = "sentry_app"
-        sentry_app_expected["id"] = str(self.sentry_app_trigger_action.id)
-        sentry_app_expected["alertRuleTriggerId"] = str(self.sentry_app_trigger.id)
-        sentry_app_expected["targetType"] = "sentry_app"
-        sentry_app_expected["targetIdentifier"] = sentry_app.id
-        sentry_app_expected["sentryAppId"] = sentry_app.id
-        sentry_app_expected["settings"] = self.sentry_app_action.data["settings"]
-        sentry_app_expected["desc"] = f"Send a notification via {sentry_app.name}"
-        assert serialized_action == sentry_app_expected
-
-    def test_slack_action(self) -> None:
-        self.integration = self.create_slack_integration(
-            self.organization,
-            external_id="TXXXXXXX1",
-            user=self.user,
-        )
-        self.slack_trigger = self.create_alert_rule_trigger(
-            alert_rule=self.alert_rule, label="warning"
-        )
-        self.slack_trigger_action = AlertRuleTriggerAction.objects.create(
-            alert_rule_trigger=self.slack_trigger,
-            target_identifier="123",
-            target_display="myChannel",
-            type=AlertRuleTriggerAction.Type.SLACK,
-            target_type=AlertRuleTriggerAction.TargetType.SPECIFIC,
-            integration_id=self.integration.id,
-        )
-        migrate_metric_data_conditions(self.slack_trigger)
-        self.slack_action, _, _ = migrate_metric_action(self.slack_trigger_action)
-
-        serialized_action = serialize(
-            self.slack_action, self.user, WorkflowEngineActionSerializer()
-        )
-        slack_expected = self.expected.copy()
-        slack_expected["type"] = self.integration.provider
-        slack_expected["id"] = str(self.slack_trigger_action.id)
-        slack_expected["alertRuleTriggerId"] = str(self.slack_trigger.id)
-        slack_expected["targetType"] = "specific"
-        slack_expected["targetIdentifier"] = self.slack_trigger_action.target_display
-        slack_expected["inputChannelId"] = "123"
-        slack_expected["integrationId"] = self.integration.id
-        slack_expected["desc"] = (
-            f"Send a Slack notification to {self.slack_trigger_action.target_display}"
-        )
-        assert serialized_action == slack_expected
+        expected_trigger["id"] = str(self.warning_trigger.id)
+        expected_trigger["alertRuleId"] = str(self.alert_rule.id)
+        expected_trigger["label"] = "warning"
+        assert serialized_data_condition == expected_trigger
