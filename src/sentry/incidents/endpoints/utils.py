478dd8f4a6729ee861229c412085c8b3ebfb2dc0
Colleen O'Rourke <colleen@sentry.io>
2025-03-13 16:38:46 -0700

    fix typing

--- src/sentry/incidents/endpoints/utils.py
+++ src/sentry/incidents/endpoints/utils.py
@@ -1,5 +1,6 @@
 from sentry.api.helpers.teams import get_teams
 from sentry.incidents.models.alert_rule import AlertRuleThresholdType
+from sentry.workflow_engine.models.data_condition import Condition
 
 
 def parse_team_params(request, organization, teams):
@@ -20,6 +21,11 @@ threshold_translators = {
     AlertRuleThresholdType.BELOW: lambda threshold: 100 - threshold,
 }
 
+data_condition_type_translators = {
+    Condition.GREATER_OR_EQUAL: lambda threshold: threshold - 100,
+    Condition.LESS_OR_EQUAL: lambda threshold: 100 - threshold,
+}
+
 
 def translate_threshold(
     comparison_delta: int, threshold_type: AlertRuleThresholdType, threshold: float | None
@@ -34,3 +40,17 @@ def translate_threshold(
 
     threshold_type = AlertRuleThresholdType(threshold_type)
     return threshold_translators[threshold_type](threshold)
+
+
+def translate_data_condition_type(
+    comparison_delta: int, condition_type: str, threshold: float | None
+):
+    """
+    Translates our internal percent representation into a delta percentage.
+    For ABOVE: A percentage like 170% would become 70% increase
+    For BELOW: A percentage like 40% would become 60% decrease.
+    """
+    if comparison_delta is None or threshold is None:
+        return threshold
+
+    return data_condition_type_translators[condition_type](threshold)
