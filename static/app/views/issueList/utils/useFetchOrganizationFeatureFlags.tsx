4eb64ad7a51cc637b13f0d1ddd24eb718532e2e2
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-03-13 14:41:52 -0700

    Update params type

--- static/app/views/issueList/utils/useFetchOrganizationFeatureFlags.tsx
+++ static/app/views/issueList/utils/useFetchOrganizationFeatureFlags.tsx
@@ -1,38 +1,29 @@
-import {useFetchOrganizationTags} from 'sentry/actionCreators/tags';
+import {
+  type FetchOrganizationTagsParams,
+  useFetchOrganizationTags,
+} from 'sentry/actionCreators/tags';
 import type {Tag} from 'sentry/types/group';
 import type {UseApiQueryOptions} from 'sentry/utils/queryClient';
 import {Dataset} from 'sentry/views/alerts/rules/metric/types';
 
+/**
+ * Queries `flags` column of errors dataset. Response format is the same as `useFetchOrganizationTags`. response.data: Tag[]
+ */
 export default function useFetchOrganizationFeatureFlags(
   {
-    orgSlug,
-    projectIds,
-    enabled,
     keepPreviousData = false,
     useCache = true,
-    ...statsPeriodParams
-  }: {
-    orgSlug: string;
-    projectIds: string[];
-    enabled?: boolean;
-    end?: string;
-    keepPreviousData?: boolean;
-    start?: string;
-    statsPeriod?: string | null;
-    useCache?: boolean;
-  },
+    ...params
+  }: Omit<FetchOrganizationTagsParams, 'dataset' | 'useFlagsBackend'>,
   options: Partial<UseApiQueryOptions<Tag[]>>
 ) {
   return useFetchOrganizationTags(
     {
-      orgSlug,
-      projectIds,
+      ...params,
       dataset: Dataset.ERRORS,
       useCache,
-      useFlagsBackend: true, // Queries `flags` column instead of tags. Response format is the same as `useFetchOrganizationTags`.
-      enabled,
+      useFlagsBackend: true,
       keepPreviousData,
-      ...statsPeriodParams,
     },
     options
   );
