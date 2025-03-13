53f0342efbc96f013e3109aefd93f75b1466601c
Nar Saynorath <nar.saynorath@sentry.io>
2025-03-13 14:27:37 -0400

    fix(dashboards-eap): Ensure groupBy is still sent when empty array
    qs.stringify doesn't support empty arrays in URLs. Any keys that map to
    an empty array will be ignored, but the Explore UI will add a default if
    the key isn't present in the URL. Enforce that the groupBy is added
    to make sure the chart is rendered properly when there is no grouping
    in dashboards by making the only value an empty string.

--- static/app/views/dashboards/utils/getWidgetExploreUrl.tsx
+++ static/app/views/dashboards/utils/getWidgetExploreUrl.tsx
@@ -86,6 +86,15 @@ export function getWidgetExploreUrl(
     utc: decodeBoolean(locationQueryParams.utc) ?? null,
   };
 
+  let groupBy = fields?.filter(field => !isAggregateFieldOrEquation(field));
+  if (groupBy && groupBy.length === 0) {
+    // Force the groupBy to be an array with a single empty string
+    // so that qs.stringify appends the key to the URL. If the key
+    // is not present, the Explore UI will assign a default groupBy
+    // which we do not want if the user has not specified a groupBy.
+    groupBy = [''];
+  }
+
   const queryParams = {
     // Page filters should propagate
     selection: {
@@ -100,7 +109,7 @@ export function getWidgetExploreUrl(
         yAxes: locationQueryParams.yAxes,
       },
     ],
-    groupBy: fields?.filter(field => !isAggregateFieldOrEquation(field)),
+    groupBy,
     field: decodeList(locationQueryParams.field),
     query: decodeScalar(locationQueryParams.query),
     sort:
