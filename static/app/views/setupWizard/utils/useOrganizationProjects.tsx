f944fc0f12856593f0d30b02024f1873a905a017
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2025-03-13 10:28:46 +0100

    ref(lint): add the eslint-plugin-query (#86278)
    This PR adds the
    [eslint-plugin-query](https://tanstack.com/query/v4/docs/eslint/eslint-plugin-query)
    recommended set of lint rules for working with React Query. It also
    tries to fix most violations we have accrued.

--- static/app/views/setupWizard/utils/useOrganizationProjects.tsx
+++ static/app/views/setupWizard/utils/useOrganizationProjects.tsx
@@ -1,7 +1,5 @@
 import type {Project} from 'sentry/types/project';
-import {useQuery} from 'sentry/utils/queryClient';
-import type RequestError from 'sentry/utils/requestError/requestError';
-import useApi from 'sentry/utils/useApi';
+import {type ApiQueryKey, fetchDataQuery, useQuery} from 'sentry/utils/queryClient';
 import type {OrganizationWithRegion} from 'sentry/views/setupWizard/types';
 
 export function useOrganizationProjects({
@@ -11,17 +9,18 @@ export function useOrganizationProjects({
   organization?: OrganizationWithRegion;
   query?: string;
 }) {
-  const api = useApi();
-
-  return useQuery<Project[], RequestError>({
-    queryKey: [`/organizations/${organization?.slug}/projects/`, {query}],
-    queryFn: () => {
-      return api.requestPromise(`/organizations/${organization?.slug}/projects/`, {
+  return useQuery({
+    queryKey: [
+      `/organizations/${organization?.slug}/projects/`,
+      {
         host: organization?.region.url,
         query: {
           query,
         },
-      });
+      },
+    ] satisfies ApiQueryKey,
+    queryFn: context => {
+      return fetchDataQuery<Project[]>(context).then(result => result[0]);
     },
     enabled: !!organization,
     refetchOnWindowFocus: true,
