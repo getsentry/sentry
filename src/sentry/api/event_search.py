2522b3ca2e20295096e3b5c3fc15a9179b0eaa36
anthony sottile <anthony.sottile@sentry.io>
2025-03-13 16:05:26 -0400

    ref: remove config_overrides= argument to parse_search_query
    only used in one place -- difficult to type properly, and the caller can just do the right thing instead

--- src/sentry/api/event_search.py
+++ src/sentry/api/event_search.py
@@ -1332,7 +1332,6 @@ def parse_search_query(
     *,
     config: SearchConfig | None = None,
     params=None,
-    config_overrides=None,
     get_field_type: Callable[[str], str | None] | None = None,
     get_function_result_type: Callable[[str], str | None] | None = None,
 ) -> list[
@@ -1353,9 +1352,6 @@ def parse_search_query(
                 "This is commonly caused by unmatched parentheses. Enclose any text in double quotes.",
             )
         )
-
-    if config_overrides:
-        config = SearchConfig.create_from(config, **config_overrides)
 
     return SearchVisitor(
         config,
