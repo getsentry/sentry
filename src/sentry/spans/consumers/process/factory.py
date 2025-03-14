dfe0e729811374cfef751f2714b7e2f333b29a2c
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-03-14 14:35:22 +0100

    remove more dead code

--- src/sentry/spans/consumers/process/factory.py
+++ src/sentry/spans/consumers/process/factory.py
@@ -1,140 +1,26 @@
-import dataclasses
 import logging
 import time
 from collections.abc import Mapping
 from functools import partial
-from typing import Any
 
-import orjson
 import rapidjson
-import sentry_sdk
 from arroyo import Topic as ArroyoTopic
 from arroyo.backends.kafka import KafkaProducer, build_kafka_configuration
-from arroyo.backends.kafka.consumer import Headers, KafkaPayload
+from arroyo.backends.kafka.consumer import KafkaPayload
 from arroyo.processing.strategies.abstract import ProcessingStrategy, ProcessingStrategyFactory
 from arroyo.processing.strategies.batching import BatchStep, ValuesBatch
 from arroyo.processing.strategies.commit import CommitOffsets
 from arroyo.processing.strategies.run_task import RunTask
-from arroyo.types import FILTERED_PAYLOAD, BrokerValue, Commit, FilteredPayload, Message, Partition
-from sentry_kafka_schemas.codecs import Codec
-from sentry_kafka_schemas.schema_types.snuba_spans_v1 import SpanEvent
+from arroyo.types import Commit, Message, Partition
 
-from sentry import options
-from sentry.conf.types.kafka_definition import Topic, get_topic_codec
+from sentry.conf.types.kafka_definition import Topic
 from sentry.spans.buffer import Span, SpansBuffer
 from sentry.spans.consumers.process.flusher import SpanFlusher
-from sentry.utils import metrics
 from sentry.utils.arroyo import MultiprocessingPool, run_task_with_multiprocessing
 from sentry.utils.kafka_config import get_kafka_producer_cluster_options, get_topic_definition
 from sentry.utils.safe import get_path
 
 logger = logging.getLogger(__name__)
-
-SPANS_CODEC: Codec[SpanEvent] = get_topic_codec(Topic.INGEST_SPANS)
-MAX_PAYLOAD_SIZE = 10 * 1000 * 1000  # 10 MB
-
-BATCH_SIZE = 100
-
-
-def in_process_spans_rollout_group(project_id: int | None) -> bool:
-    if project_id and project_id in options.get(
-        "standalone-spans.process-spans-consumer.project-allowlist"
-    ):
-        return True
-
-    if project_id and (project_id % 100000) / 100000 < options.get(
-        "standalone-spans.process-spans-consumer.project-rollout"
-    ):
-        return True
-    return False
-
-
-@dataclasses.dataclass
-class SpanMessageWithMetadata:
-    segment_id: str
-    project_id: int
-    timestamp: int
-    partition: int
-    span: bytes
-
-
-def get_project_id(headers: Headers) -> int | None:
-    for k, v in headers:
-        if k == "project_id":
-            return int(v.decode("utf-8"))
-
-    return None
-
-
-def prepare_buffered_segment_payload(segments) -> bytes:
-    segment_str = b",".join(segments)
-    return b'{"spans": [' + segment_str + b"]}"
-
-
-@metrics.wraps("spans.consumers.process.deserialize_span")
-def _deserialize_span(value: bytes, use_orjson=False, use_rapidjson=False) -> Mapping[str, Any]:
-    if use_orjson:
-        sentry_sdk.set_tag("json_lib", "orjson")
-        return orjson.loads(value)
-    if use_rapidjson:
-        sentry_sdk.set_tag("json_lib", "rapidjson")
-        return rapidjson.loads(value)
-
-    return SPANS_CODEC.decode(value)
-
-
-def _process_message(message: Message[KafkaPayload]) -> SpanMessageWithMetadata | FilteredPayload:
-    """
-    Deserializes span to get segment_id. Returns `SpanMessageWithMetadata` which contains the
-    original span payload value in bytes along with other segment_id, message timestamp and
-    partition data to ensure correct bucketing in redis.
-    """
-    if not options.get("standalone-spans.process-spans-consumer.enable"):
-        return FILTERED_PAYLOAD
-
-    try:
-        project_id = get_project_id(message.payload.headers)
-    except Exception:
-        logger.exception("Failed to parse span message header")
-        return FILTERED_PAYLOAD
-
-    if not project_id or not in_process_spans_rollout_group(project_id=project_id):
-        return FILTERED_PAYLOAD
-
-    assert isinstance(message.value, BrokerValue)
-
-    with sentry_sdk.start_transaction(op="process", name="spans.process.process_message") as txn:
-        payload_value = message.payload.value
-        timestamp = int(message.value.timestamp.timestamp())
-        partition = message.value.partition.index
-
-        use_orjson = options.get("standalone-spans.deserialize-spans-orjson.enable")
-        use_rapidjson = options.get("standalone-spans.deserialize-spans-rapidjson.enable")
-
-        with txn.start_child(op="deserialize"):
-            span = _deserialize_span(
-                payload_value, use_orjson=use_orjson, use_rapidjson=use_rapidjson
-            )
-
-        segment_id: str | None = span.get("segment_id", None)
-        if segment_id is None:
-            return FILTERED_PAYLOAD
-
-    return SpanMessageWithMetadata(
-        segment_id=segment_id,
-        project_id=project_id,
-        timestamp=timestamp,
-        partition=partition,
-        span=payload_value,
-    )
-
-
-def process_message(message: Message[KafkaPayload]) -> SpanMessageWithMetadata | FilteredPayload:
-    try:
-        return _process_message(message)
-    except Exception:
-        sentry_sdk.capture_exception()
-        return FILTERED_PAYLOAD
 
 
 class ProcessSpansStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
