d9e6945dd45ede604953a16b819d359b8950d6e0
Philipp Hofmann <philipp.hofmann@sentry.io>
2025-03-14 14:34:07 +0100

    fix(releases): Apple-iOS uses mobile session terms (#86971)
    Apple-iOS used desktop TermDescriptions, but instead, it should use the
    ones from mobile to align with Android, Cordova, React-Native, and
    Flutter. Still, we keep the different session term for crashed, cause
    for Apple platforms you usually have errors and not exceptions.
    
    This came up while working on app hang rate for Apple projects
    https://github.com/getsentry/sentry/issues/86106.

--- static/app/views/releases/utils/sessionTerm.tsx
+++ static/app/views/releases/utils/sessionTerm.tsx
@@ -87,7 +87,6 @@ function getTermDescriptions(platform: PlatformKey | null) {
   const technology =
     platform === 'react-native' ||
     platform === 'java-spring' ||
-    platform === 'apple-ios' ||
     platform === 'dotnet-aspnetcore'
       ? platform
       : platform?.split('-')[0];
@@ -119,7 +118,6 @@ function getTermDescriptions(platform: PlatformKey | null) {
           'An unhandled exception that resulted in the application crashing'
         ),
       };
-
     case 'apple': {
       return {
         ...commonTermsDescription,
@@ -142,7 +140,6 @@ function getTermDescriptions(platform: PlatformKey | null) {
         [SessionTerm.UNHANDLED]:
           "An error was captured by the global 'onerror' or 'onunhandledrejection' handler.",
       };
-    case 'apple-ios':
     case 'minidump':
     case 'native':
     case 'nintendo-switch':
