df9b14d94cbe860956f095cdd0414b020b4bdd1f
Jan Michael Auer <mail@jauer.org>
2025-03-14 21:45:45 +0100

    fix(event_manager): Resolve TypeError while recording first insight span (#87123)
    Fixes a regression introduced in #86604 that resulted in a captured
    `TypeError` when recording the first insight span for a project.
    
    We switched from using signals, which are prohibited in
    `EventManager.save`, to directly calling the onboarding receiver
    function. In this specific instance, we missed that the function was
    decorated, and the import resulted in `None`. For an unknown reason, the
    resulting `TypeError` did not show up in Sentry.
    
    Projects that have not recorded these flags since the regression was
    rolled out will be flagged when they ingest the next span of the
    corresponding insights module.

--- tests/sentry/event_manager/test_event_manager.py
+++ tests/sentry/event_manager/test_event_manager.py
@@ -1662,6 +1662,49 @@ class EventManagerTest(TestCase, SnubaTestCase, EventManagerTestMixin, Performan
             mock.call(ClustererNamespace.TRANSACTIONS, self.project, "wait")
         ]
 
+    def test_first_insight_span(self) -> None:
+        event_data = make_event(
+            transaction="test_transaction",
+            contexts={
+                "trace": {
+                    "parent_span_id": "bce14471e0e9654d",
+                    "op": "foobar",
+                    "trace_id": "a0fa8803753e40fd8124b21eeb2986b5",
+                    "span_id": "bf5be759039ede9a",
+                }
+            },
+            spans=[
+                {
+                    "trace_id": "a0fa8803753e40fd8124b21eeb2986b5",
+                    "parent_span_id": "bf5be759039ede9a",
+                    "span_id": "a" * 16,
+                    "start_timestamp": 0,
+                    "timestamp": 1,
+                    "same_process_as_parent": True,
+                    "op": "db.redis",
+                    "description": "EXEC *",
+                    "sentry_tags": {
+                        "description": "EXEC *",
+                        "category": "db",
+                        "op": "db.redis",
+                        "transaction": "/app/index",
+                    },
+                }
+            ],
+            timestamp="2019-06-14T14:01:40Z",
+            start_timestamp="2019-06-14T14:01:40Z",
+            type="transaction",
+        )
+
+        assert not self.project.flags.has_insights_db
+
+        manager = EventManager(event_data)
+        manager.normalize()
+        manager.save(self.project.id)
+
+        self.project.refresh_from_db()
+        assert self.project.flags.has_insights_db
+
     def test_sdk(self) -> None:
         manager = EventManager(make_event(**{"sdk": {"name": "sentry-unity", "version": "1.0"}}))
         manager.normalize()
