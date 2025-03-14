71a55f4aaf1f348cfae314722a8f4ea255a57055
Malachi Willey <malwilley@gmail.com>
2025-03-14 10:42:47 -0700

    Fix test

--- static/app/components/nav/index.spec.tsx
+++ static/app/components/nav/index.spec.tsx
@@ -223,7 +223,9 @@ describe('Nav', function () {
       renderNav();
 
       // Should have a top-level header element with a home link and menu button
-      expect(screen.getByRole('link', {name: 'Sentry Home'})).toBeInTheDocument();
+      expect(
+        screen.getByRole('button', {name: 'Toggle organization menu'})
+      ).toBeInTheDocument();
       expect(screen.getByRole('button', {name: 'Open main menu'})).toBeInTheDocument();
 
       await userEvent.click(screen.getByRole('button', {name: 'Open main menu'}));
