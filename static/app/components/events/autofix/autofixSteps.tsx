ed3f3cf7eac923790be4ee9f0c2f2b63e198bc16
Jenn Mueng <jenn.muengtaweepongsa@sentry.io>
2025-03-14 17:37:28 -0700

    feat(autofix): Thumbs up/down on root cause

--- static/app/components/events/autofix/autofixSteps.tsx
+++ static/app/components/events/autofix/autofixSteps.tsx
@@ -12,6 +12,7 @@ import {
 import {AutofixSolution} from 'sentry/components/events/autofix/autofixSolution';
 import {
   type AutofixData,
+  type AutofixFeedback,
   type AutofixProgressItem,
   type AutofixRepository,
   AutofixStatus,
@@ -37,6 +38,7 @@ interface StepProps {
   repos: AutofixRepository[];
   runId: string;
   step: AutofixStep;
+  feedback?: AutofixFeedback;
   previousDefaultStepIndex?: number;
   previousInsightCount?: number;
   shouldCollapseByDefault?: boolean;
@@ -65,6 +67,7 @@ export function Step({
   shouldCollapseByDefault,
   previousDefaultStepIndex,
   previousInsightCount,
+  feedback,
 }: StepProps) {
   return (
     <StepCard>
@@ -99,6 +102,7 @@ export function Step({
                   repos={repos}
                   previousDefaultStepIndex={previousDefaultStepIndex}
                   previousInsightCount={previousInsightCount}
+                  feedback={feedback}
                 />
               )}
               {step.type === AutofixStepType.SOLUTION && (
@@ -222,6 +226,7 @@ export function AutofixSteps({data, groupId, runId}: AutofixStepsProps) {
                 previousDefaultStepIndex >= 0 ? previousDefaultStepIndex : undefined
               }
               previousInsightCount={previousInsightCount}
+              feedback={data.feedback}
             />
           </div>
         );
