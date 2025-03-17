78e293a949983762660b640c2dbdbcbc0b7a1e89
Nate Moore <nate.moore@sentry.io>
2025-03-17 15:51:05 -0500

    feat(ui2): add slider label

--- static/app/components/core/slider/index.chonk.tsx
+++ static/app/components/core/slider/index.chonk.tsx
@@ -1,81 +1,80 @@
-import {forwardRef, useCallback, useEffect, useRef, useState} from 'react';
+import {
+  type CSSProperties,
+  forwardRef,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react';
 import type {DO_NOT_USE_ChonkTheme} from '@emotion/react';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
+import {space} from 'sentry/styles/space';
 import mergeRefs from 'sentry/utils/mergeRefs';
 
 import type {SliderProps} from './index';
 
-export const Slider = forwardRef<HTMLInputElement, SliderProps>((props, ref) => {
-  const inputRef = useRef<HTMLInputElement>();
-  const refs = mergeRefs([ref, inputRef]);
-  const [label, setLabel] = useState(props.value ?? '50');
+const passthrough = (n: number) => n;
 
-  const handleChange = useCallback(
-    (event: Event) => {
-      const input = event.target as HTMLInputElement;
-      const {value, valueAsNumber, min, max} = input;
-      setLabel(value);
-      const progress =
-        ((valueAsNumber - Number(min || '0')) / Number(max || '100')) * 100;
-      input.parentElement?.style.setProperty('--p', `${progress.toFixed(2)}`);
-      input.style.setProperty('--p', `${progress.toFixed(2)}%`);
-    },
-    [setLabel]
-  );
+export const Slider = forwardRef<HTMLInputElement, SliderProps>(
+  ({formatLabel = passthrough, ...props}, ref) => {
+    const inputRef = useRef<HTMLInputElement>();
+    const refs = mergeRefs([ref, inputRef]);
+    const [label, setLabel] = useState(props.value ?? props.defaultValue ?? 50);
+    const initialProgress = getProgress(
+      props.value ?? props.defaultValue ?? 50,
+      props.min ?? 0,
+      props.max ?? 100
+    );
 
-  useEffect(() => {
-    const input: HTMLInputElement | undefined = inputRef.current;
-    if (!input) {
-      return;
-    }
-    input.addEventListener('input', handleChange);
-    // eslint-disable-next-line consistent-return
-    return () => input.removeEventListener('input', handleChange);
-  });
+    const handleChange = useCallback(
+      (event: Event) => {
+        const input = event.target as HTMLInputElement;
+        const {valueAsNumber, min, max} = input;
+        setLabel(valueAsNumber);
+        const progress = getProgress(valueAsNumber, min || 0, max || 100);
+        input.parentElement?.style.setProperty('--p', `${progress.toFixed(0)}%`);
+      },
+      [setLabel]
+    );
 
-  return (
-    <SliderContainer>
-      <SliderLabel>
-        <SliderLabelInner>{label}</SliderLabelInner>
-      </SliderLabel>
-      <StyledSlider ref={refs} type="range" {...props} />
-    </SliderContainer>
-  );
-});
+    useEffect(() => {
+      const input: HTMLInputElement | undefined = inputRef.current;
+      if (!input) {
+        return;
+      }
+      input.addEventListener('input', handleChange);
+      // eslint-disable-next-line consistent-return
+      return () => input.removeEventListener('input', handleChange);
+    });
+
+    return (
+      <SliderContainer style={{'--p': `${initialProgress.toFixed(0)}%`} as CSSProperties}>
+        {props.disabled ? null : (
+          <SliderOutput htmlFor={props.id}>
+            <SliderLabel>{formatLabel(label)}</SliderLabel>
+          </SliderOutput>
+        )}
+        <StyledSlider ref={refs} type="range" {...props} />
+      </SliderContainer>
+    );
+  }
+);
+
+function toNumber(value: number | string) {
+  if (typeof value === 'number') {
+    return value;
+  }
+  return Number(value);
+}
+function getProgress(n: number, min: number | string, max: number | string) {
+  return ((n - toNumber(min)) / toNumber(max)) * 100;
+}
 
 const StyledSlider = styled('input')`
   ${p => chonkSliderStyles(p as any)}
 `;
-const SliderLabel = styled('span')`
-  ${p => chonkLabel(p as any)}
-`;
-const SliderLabelInner = styled('span')`
-  ${p => chonkLabelInner(p as any)}
-`;
-const SliderContainer = styled('span')`
-  position: relative;
-`;
-
-export function chonkLabel(p: {theme: DO_NOT_USE_ChonkTheme}) {
-  return css`
-    display: flex;
-    color: ${p.theme.colors.chonk.blue400};
-    font-size: ${p.theme.fontSizeMedium};
-    position: absolute;
-    height: 24px;
-    bottom: 12px;
-    padding-left: calc(var(--p) * 1%);
-  `;
-}
-export function chonkLabelInner(_p: {theme: DO_NOT_USE_ChonkTheme}) {
-  return css`
-    display: block;
-    width: 48px;
-    text-align: center;
-  `;
-}
 export function chonkSliderStyles(p: {theme: DO_NOT_USE_ChonkTheme}) {
   return css`
     --track-color: ${p.theme.colors.surface300};
@@ -94,7 +93,7 @@ export function chonkSliderStyles(p: {theme: DO_NOT_USE_ChonkTheme}) {
       0 0 0 8px transparent,
       0 0 0 10px transparent;
 
-    &:focus {
+    &:focus-visible {
       outline: none;
       box-shadow:
         0 0 0 8px ${p.theme.background},
@@ -117,12 +116,13 @@ export function chonkSliderStyles(p: {theme: DO_NOT_USE_ChonkTheme}) {
     &::before {
       content: '';
       position: absolute;
+      min-width: calc(${p.theme.radius.micro} * 6);
       width: var(--p, 50%);
       height: 12px;
       background: var(--track-active-color);
       box-shadow: 0px 3px 0px 0px var(--track-active-color-dark) inset;
       border: 1px solid var(--track-active-color-dark);
-      border-radius: ${p.theme.radius.micro} 0 0 ${p.theme.radius.micro};
+      border-radius: ${p.theme.radius.micro};
     }
 
     /* Chrome styling */
@@ -170,3 +170,63 @@ export function chonkSliderStyles(p: {theme: DO_NOT_USE_ChonkTheme}) {
     }
   `;
 }
+
+const SliderOutput = styled('output')`
+  ${p => chonkOutput(p as any)}
+`;
+export function chonkOutput(p: {theme: DO_NOT_USE_ChonkTheme}) {
+  return css`
+    --tx: clamp(-50%, calc(-50% + var(--p, 0)), 50%);
+    --ty: var(--label-ty);
+    --o: var(--label-opacity);
+
+    /* disable interactions */
+    pointer-events: none;
+    user-select: none;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: ${p.theme.fontSizeSmall};
+    position: absolute;
+    height: 24px;
+    width: calc(100% - 24px);
+    margin-inline: auto;
+    left: 0;
+    right: 0;
+    bottom: 24px;
+    text-align: center;
+
+    opacity: var(--o);
+    transform: translate(var(--tx), var(--ty));
+    transition:
+      100ms opacity cubic-bezier(0.23, 1, 0.32, 1),
+      100ms transform cubic-bezier(0.39, 0.575, 0.565, 1);
+  `;
+}
+const SliderLabel = styled('span')<{theme?: DO_NOT_USE_ChonkTheme}>`
+  font-size: inherit;
+  display: block;
+  min-width: calc(3ch + ${space(0.5)});
+  padding-inline: ${space(0.5)};
+  width: min-content;
+  text-align: center;
+  background: ${p => p.theme.colors.black};
+  color: ${p => p.theme.white};
+  border-radius: ${p => p.theme.radius.micro};
+`;
+
+const SliderContainer = styled('div')`
+  position: relative;
+  width: 100%;
+  flex-grow: 1;
+  --label-opacity: 0;
+  --label-ty: 100%;
+
+  &:focus-within,
+  &:hover,
+  &:active {
+    --label-opacity: 1;
+    --label-ty: 0;
+  }
+`;
