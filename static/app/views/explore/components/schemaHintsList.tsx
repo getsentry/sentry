05937cb4d6234b6052a726a14e0237ab9fec99ed
Nikki Kapadia <72356613+nikkikapadia@users.noreply.github.com>
2025-03-17 10:00:36 -0400

    feat(schema-hints): Add default ordering to displayed hints (#87087)
    Dhrumil and Dora gave me a [list of
    hints](https://www.notion.so/sentry/Schema-Hints-Onboarding-Guide-18a8b10e4b5d8016a64ec3e763001abe?pvs=4#18a8b10e4b5d80af98b1e1325cbd6bfe)
    that should display first (if they exist in the list of tags) so i've
    adjusted the sorting to have these show up at the beginning of the list.
    Here's what it looks like (it varies from project to project):
    
    <img width="1283" alt="image"
    src="https://github.com/user-attachments/assets/8b90e8b4-597b-4502-bb97-b8a359caf382"
    />
    
    Closes https://github.com/getsentry/sentry/issues/86480

--- static/app/views/explore/components/schemaHintsList.tsx
+++ static/app/views/explore/components/schemaHintsList.tsx
@@ -13,10 +13,16 @@ import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Tag, TagCollection} from 'sentry/types/group';
 import {prettifyTagKey} from 'sentry/utils/discover/fields';
-import {type AggregationKey, FieldKind} from 'sentry/utils/fields';
+import {
+  type AggregationKey,
+  FieldKind,
+  FieldValueType,
+  getFieldDefinition,
+} from 'sentry/utils/fields';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import {useLocation} from 'sentry/utils/useLocation';
 import SchemaHintsDrawer from 'sentry/views/explore/components/schemaHintsDrawer';
+import {SCHEMA_HINTS_LIST_ORDER_KEYS} from 'sentry/views/explore/components/schemaHintsUtils/schemaHintsListOrder';
 import {
   PageParamsProvider,
   useExploreQuery,
@@ -36,6 +42,10 @@ const seeFullListTag: Tag = {
   name: t('See full list'),
   kind: undefined,
 };
+
+function getTagsFromKeys(keys: string[], tags: TagCollection): Tag[] {
+  return keys.map(key => tags[key]).filter(tag => !!tag);
+}
 
 function SchemaHintsList({
   supportedAggregates,
@@ -58,13 +68,22 @@ function SchemaHintsList({
     const filterTags: TagCollection = {...functionTags, ...numberTags, ...stringTags};
     filterTags.has = getHasTag({...stringTags});
 
-    const sectionKeys = SPANS_FILTER_KEY_SECTIONS.flatMap(section => section.children);
-    const sectionSortedTags = sectionKeys
-      .map(key => filterTags[key])
-      .filter(tag => !!tag);
-    const otherKeys = Object.keys(filterTags).filter(key => !sectionKeys.includes(key));
-    const otherTags = otherKeys.map(key => filterTags[key]).filter(tag => !!tag);
-    return [...sectionSortedTags, ...otherTags];
+    const schemaHintsPresetTags = getTagsFromKeys(
+      SCHEMA_HINTS_LIST_ORDER_KEYS,
+      filterTags
+    );
+
+    const sectionKeys = SPANS_FILTER_KEY_SECTIONS.flatMap(
+      section => section.children
+    ).filter(key => !SCHEMA_HINTS_LIST_ORDER_KEYS.includes(key));
+    const sectionSortedTags = getTagsFromKeys(sectionKeys, filterTags);
+
+    const otherKeys = Object.keys(filterTags).filter(
+      key => !sectionKeys.includes(key) && !SCHEMA_HINTS_LIST_ORDER_KEYS.includes(key)
+    );
+    const otherTags = getTagsFromKeys(otherKeys, filterTags);
+
+    return [...schemaHintsPresetTags, ...sectionSortedTags, ...otherTags];
   }, [numberTags, stringTags, functionTags]);
 
   const [visibleHints, setVisibleHints] = useState([seeFullListTag]);
@@ -162,9 +181,12 @@ function SchemaHintsList({
       }
 
       const newSearchQuery = new MutableSearch(exploreQuery);
+      const isBoolean =
+        getFieldDefinition(hint.key, 'span', hint.kind)?.valueType ===
+        FieldValueType.BOOLEAN;
       newSearchQuery.addFilterValue(
         hint.key,
-        hint.kind === FieldKind.MEASUREMENT ? '>0' : ''
+        isBoolean ? 'True' : hint.kind === FieldKind.MEASUREMENT ? '>0' : ''
       );
       setExploreQuery(newSearchQuery.formatString());
     },
