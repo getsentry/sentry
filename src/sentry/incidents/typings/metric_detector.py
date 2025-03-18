1b82f4a5f38341a4229a1db7ed01bf7c76af3f63
Raj Joshi <raj.joshi@sentry.io>
2025-03-17 21:26:38 -0700

    :recycle: ref: refactor slack metric alert logic

--- src/sentry/incidents/typings/metric_detector.py
+++ src/sentry/incidents/typings/metric_detector.py
@@ -14,7 +14,7 @@ from sentry.incidents.models.alert_rule import (
 from sentry.incidents.models.incident import Incident, IncidentStatus
 from sentry.issues.issue_occurrence import IssueOccurrence
 from sentry.models.group import Group, GroupStatus
-from sentry.snuba.models import SnubaQuery
+from sentry.snuba.models import QuerySubscription, SnubaQuery
 from sentry.types.group import PriorityLevel
 from sentry.workflow_engine.models import Action, Detector
 
@@ -60,6 +60,7 @@ class NotificationContext:
     NotificationContext is a dataclass that represents the context required send a notification.
     """
 
+    id: int
     integration_id: int | None = None
     target_identifier: str | None = None
     target_display: str | None = None
@@ -69,6 +70,7 @@ class NotificationContext:
     @classmethod
     def from_alert_rule_trigger_action(cls, action: AlertRuleTriggerAction) -> NotificationContext:
         return cls(
+            id=action.id,
             integration_id=action.integration_id,
             target_identifier=action.target_identifier,
             target_display=action.target_display,
@@ -79,6 +81,7 @@ class NotificationContext:
     def from_action_model(cls, action: Action) -> NotificationContext:
         if action.type == Action.Type.SENTRY_APP:
             return cls(
+                id=action.id,
                 integration_id=action.integration_id,
                 target_display=action.config.get("target_display"),
                 sentry_app_config=action.data.get("settings"),
@@ -87,6 +90,7 @@ class NotificationContext:
             )
         elif action.type == Action.Type.OPSGENIE or action.type == Action.Type.PAGERDUTY:
             return cls(
+                id=action.id,
                 integration_id=action.integration_id,
                 target_identifier=action.config.get("target_identifier"),
                 target_display=action.config.get("target_display"),
@@ -95,6 +99,7 @@ class NotificationContext:
         # TODO(iamrajjoshi): Add support for email here
 
         return cls(
+            id=action.id,
             integration_id=action.integration_id,
             target_identifier=action.config.get("target_identifier"),
             target_display=action.config.get("target_display"),
@@ -103,8 +108,10 @@ class NotificationContext:
 
 @dataclass
 class MetricIssueContext:
-    open_period_identifier: int
+    id: int
+    open_period_identifier: int  # Used for link building
     snuba_query: SnubaQuery
+    subscription: QuerySubscription
     new_status: IncidentStatus
     metric_value: float | None
 
@@ -129,6 +136,17 @@ class MetricIssueContext:
         return query
 
     @classmethod
+    def _get_subscription(cls, occurrence: IssueOccurrence) -> QuerySubscription:
+        subscription_id = occurrence.evidence_data.get("subscription_id")
+        if not subscription_id:
+            raise ValueError("Subscription ID is required for alert context")
+        try:
+            subscription = QuerySubscription.objects.get(id=subscription_id)
+        except QuerySubscription.DoesNotExist as e:
+            raise ValueError("Subscription does not exist") from e
+        return subscription
+
+    @classmethod
     def _get_metric_value(cls, occurrence: IssueOccurrence) -> float:
         if (metric_value := occurrence.evidence_data.get("metric_value")) is None:
             raise ValueError("Metric value is required for alert context")
@@ -144,8 +162,11 @@ class MetricIssueContext:
             # TODO(iamrajjoshi): Replace with something once we know how we want to build the link
             # If we store open periods in the database, we can use the id from that
             # Otherwise, we can use the issue id
+            id=group.id,
+            # TODO(iamrajjoshi): This should probably be the id of the latest open period
             open_period_identifier=group.id,
             snuba_query=cls._get_snuba_query(occurrence),
+            subscription=cls._get_subscription(occurrence),
             new_status=cls._get_new_status(group, occurrence),
             metric_value=cls._get_metric_value(occurrence),
         )
@@ -158,8 +179,10 @@ class MetricIssueContext:
         metric_value: float | None = None,
     ) -> MetricIssueContext:
         return cls(
+            id=incident.id,
             open_period_identifier=incident.identifier,
             snuba_query=incident.alert_rule.snuba_query,
+            subscription=incident.subscription,
             new_status=new_status,
             metric_value=metric_value,
         )
