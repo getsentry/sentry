d738d323b7d67b7c9eb5fe245e8ab31d744209cf
Scott Cooper <scttcper@gmail.com>
2025-03-18 10:06:04 -0700

    feat(issues): Replace trace view on performance issues (#87143)

---
+++ static/app/components/events/interfaces/performance/spanEvidenceTraceView.tsx
@@ -0,0 +1,161 @@
+import {lazy, Suspense, useMemo} from 'react';
+import styled from '@emotion/styled';
+import type {LocationDescriptor} from 'history';
+
+import Link from 'sentry/components/links/link';
+import {generateTraceTarget} from 'sentry/components/quickTrace/utils';
+import type {Event} from 'sentry/types/event';
+import type {Organization} from 'sentry/types/organization';
+import {defined} from 'sentry/utils';
+import {trackAnalytics} from 'sentry/utils/analytics';
+import {useLocation} from 'sentry/utils/useLocation';
+import {useIssuesTraceTree} from 'sentry/views/performance/newTraceDetails/traceApi/useIssuesTraceTree';
+import {useTrace} from 'sentry/views/performance/newTraceDetails/traceApi/useTrace';
+import {useTraceMeta} from 'sentry/views/performance/newTraceDetails/traceApi/useTraceMeta';
+import {useTraceRootEvent} from 'sentry/views/performance/newTraceDetails/traceApi/useTraceRootEvent';
+import {TraceViewSources} from 'sentry/views/performance/newTraceDetails/traceHeader/breadcrumbs';
+import {
+  loadTraceViewPreferences,
+  type TracePreferencesState,
+} from 'sentry/views/performance/newTraceDetails/traceState/tracePreferences';
+import {TraceStateProvider} from 'sentry/views/performance/newTraceDetails/traceState/traceStateProvider';
+import {useTraceEventView} from 'sentry/views/performance/newTraceDetails/useTraceEventView';
+import {useTraceQueryParams} from 'sentry/views/performance/newTraceDetails/useTraceQueryParams';
+
+const LazyIssuesTraceWaterfall = lazy(() =>
+  import('sentry/views/performance/newTraceDetails/issuesTraceWaterfall').then(
+    module => ({default: module.IssuesTraceWaterfall})
+  )
+);
+
+function getHrefFromTraceTarget(traceTarget: LocationDescriptor) {
+  if (typeof traceTarget === 'string') {
+    return traceTarget;
+  }
+
+  const searchParams = new URLSearchParams();
+  for (const key in traceTarget.query) {
+    if (defined(traceTarget.query[key])) {
+      searchParams.append(key, traceTarget.query[key]);
+    }
+  }
+
+  return `${traceTarget.pathname}?${searchParams.toString()}`;
+}
+
+const DEFAULT_ISSUE_DETAILS_TRACE_VIEW_PREFERENCES: TracePreferencesState = {
+  drawer: {
+    minimized: true,
+    sizes: {
+      'drawer left': 0.33,
+      'drawer right': 0.33,
+      'drawer bottom': 0.4,
+      'trace context height': 150,
+    },
+    layoutOptions: [],
+  },
+  missing_instrumentation: true,
+  autogroup: {
+    parent: true,
+    sibling: true,
+  },
+  layout: 'drawer bottom',
+  list: {
+    width: 0.5,
+  },
+};
+
+interface SpanEvidenceTraceViewProps {
+  event: Event;
+  organization: Organization;
+  traceId: string;
+}
+
+export function SpanEvidenceTraceView({
+  event,
+  organization,
+  traceId,
+}: SpanEvidenceTraceViewProps) {
+  const timestamp = new Date(event.dateReceived).getTime() / 1e3;
+
+  const location = useLocation();
+  const trace = useTrace({
+    timestamp,
+    traceSlug: traceId,
+    limit: 10000,
+  });
+  const meta = useTraceMeta([{traceSlug: traceId, timestamp}]);
+  const tree = useIssuesTraceTree({trace, meta, replay: null});
+
+  const shouldLoadTraceRoot = !trace.isPending && trace.data;
+
+  const rootEvent = useTraceRootEvent(shouldLoadTraceRoot ? trace.data : null);
+  const preferences = useMemo(
+    () =>
+      loadTraceViewPreferences('issue-details-trace-view-preferences') ||
+      DEFAULT_ISSUE_DETAILS_TRACE_VIEW_PREFERENCES,
+    []
+  );
+
+  const params = useTraceQueryParams({timestamp});
+  const traceEventView = useTraceEventView(traceId, params);
+
+  if (!traceId) {
+    return null;
+  }
+
+  const traceTarget = generateTraceTarget(
+    event,
+    organization,
+    {
+      ...location,
+      query: {
+        ...location.query,
+        groupId: event.groupID,
+      },
+    },
+    TraceViewSources.ISSUE_DETAILS
+  );
+
+  return (
+    <TraceStateProvider
+      initialPreferences={preferences}
+      preferencesStorageKey="issue-details-view-preferences"
+    >
+      <IssuesTraceContainer>
+        <Suspense fallback={null}>
+          <LazyIssuesTraceWaterfall
+            tree={tree}
+            trace={trace}
+            traceSlug={traceId}
+            rootEvent={rootEvent}
+            organization={organization}
+            traceEventView={traceEventView}
+            meta={meta}
+            source="issues"
+            replay={null}
+            event={event}
+          />
+        </Suspense>
+        <IssuesTraceOverlayContainer
+          to={getHrefFromTraceTarget(traceTarget)}
+          onClick={() => {
+            trackAnalytics('issue_details.view_full_trace_waterfall_clicked', {
+              organization,
+            });
+          }}
+        />
+      </IssuesTraceContainer>
+    </TraceStateProvider>
+  );
+}
+
+const IssuesTraceContainer = styled('div')`
+  position: relative;
+`;
+
+const IssuesTraceOverlayContainer = styled(Link)`
+  position: absolute;
+  inset: 0;
+  z-index: 10;
+`;
