f8d97785b1cf60341526226b5fb91c859f2e2987
Malachi Willey <malwilley@gmail.com>
2025-03-19 11:48:21 -0700

    fix(nav): Remove tooltip when menus are opened (#87424)
    Prevents the current issue where tooltips stay open when interacting
    with the menu, by just wrapping the button itself.

--- static/app/components/nav/primary/onboarding.tsx
+++ static/app/components/nav/primary/onboarding.tsx
@@ -114,12 +114,12 @@ function OnboardingItem({
             <SidebarItemUnreadIndicator data-test-id="pending-seen-indicator" />
           )}
         </NavButton>
-        {isOpen && (
-          <PrimaryButtonOverlay overlayProps={overlayProps}>
-            <OnboardingSidebarContent onClose={() => SidebarPanelStore.hidePanel()} />
-          </PrimaryButtonOverlay>
-        )}
       </SidebarItem>
+      {isOpen && (
+        <PrimaryButtonOverlay overlayProps={overlayProps}>
+          <OnboardingSidebarContent onClose={() => SidebarPanelStore.hidePanel()} />
+        </PrimaryButtonOverlay>
+      )}
     </GuideAnchor>
   );
 }
