b42ca5952ea553c3cf008a06fe8fae5f24640311
Malachi Willey <malwilley@gmail.com>
2025-03-20 12:24:49 -0700

    chore(search): Add flag for searching on token completion (#87522)

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -313,6 +313,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:replay-play-from-replay-tab", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable version 2 of reprocessing (completely distinct from v1)
     manager.add("organizations:reprocessing-v2", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
+    # When enabled, searches will be performed on token completion rather than on enter
+    manager.add("organizations:search-on-token-completion", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable Sentry's 2024 Rollback feature
     manager.add("organizations:sentry-rollback-2024", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable Sentry's 2024 Rollback toggle within organization settings
