35662034c8b53738e21aa1bd5df6b15348fd9b42
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2025-03-20 12:56:54 -0700

    chore(issue-details): Better analytics on enforced streamline views  (#87510)
    Add better analytics for how the user is seeing the streamlined ui

--- static/app/views/issueDetails/groupDetails.tsx
+++ static/app/views/issueDetails/groupDetails.tsx
@@ -518,6 +518,7 @@ function useTrackView({
     location.query;
   const groupEventType = useLoadedEventType();
   const user = useUser();
+  const hasStreamlinedUI = useHasStreamlinedUI();
 
   useRouteAnalyticsEventNames('issue_details.viewed', 'Issue Details: Viewed');
   useRouteAnalyticsParams({
@@ -535,7 +536,11 @@ function useTrackView({
     ref_fallback,
     group_event_type: groupEventType,
     prefers_streamlined_ui: user?.options?.prefersIssueDetailsStreamlinedUI ?? false,
+    enforced_streamlined_ui:
+      organization.features.includes('issue-details-streamline-enforce') &&
+      user?.options?.prefersIssueDetailsStreamlinedUI === null,
     org_streamline_only: organization.streamlineOnly ?? undefined,
+    has_streamlined_ui: hasStreamlinedUI,
   });
   // Set default values for properties that may be updated in subcomponents.
   // Must be separate from the above values, otherwise the actual values filled in
