7be695f5ce30c730dfde682c18996db5010d002d
Billy Vong <billyvg@users.noreply.github.com>
2025-03-20 12:11:08 -0400

    rename createReleaseBubbleHighlighter

--- static/app/views/issueDetails/streamline/eventGraph.tsx
+++ static/app/views/issueDetails/streamline/eventGraph.tsx
@@ -255,7 +255,7 @@ export const EventGraph = forwardRef<ReactEchartsRef, EventGraphProps>(
     });
 
     const {
-      createReleaseBubbleHighlighter,
+      connectReleaseBubbleChartRef,
       releaseBubbleEventHandlers,
       releaseBubbleSeries,
       releaseBubbleXAxis,
@@ -282,14 +282,14 @@ export const EventGraph = forwardRef<ReactEchartsRef, EventGraphProps>(
 
     const handleConnectRef = useCallback(
       (e: ReactEchartsRef | null) => {
-        createReleaseBubbleHighlighter(e);
+        connectReleaseBubbleChartRef(e);
         if (typeof ref === 'function') {
           ref(e);
         } else if (ref) {
           ref.current = e;
         }
       },
-      [createReleaseBubbleHighlighter, ref]
+      [connectReleaseBubbleChartRef, ref]
     );
     const flagSeries = useFlagSeries({
       query: {
