1394ee51b19e1b09a3478dafa8cf1ac25436446d
Jonas <jonas.badalic@sentry.io>
2025-03-21 07:15:35 -0400

    buttonbar: remove barid and active management (#87490)
    The priority management is trivial for callers to manage, so it makes
    sense to defer this to the caller, which also makes this component less
    opinionated and more flexible as callers can now toggle different button
    priorities and or manage multiple "toggled" states.

--- static/app/components/buttonBar.tsx
+++ static/app/components/buttonBar.tsx
@@ -1,59 +1,35 @@
-import {Children, cloneElement, isValidElement} from 'react';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
-import classNames from 'classnames';
 
-import type {ButtonProps} from 'sentry/components/core/button';
 import {StyledButton} from 'sentry/components/core/button';
 import type {ValidSize} from 'sentry/styles/space';
 import {space} from 'sentry/styles/space';
 
-type ButtonBarProps = {
+export interface ButtonBarProps
+  extends Omit<React.HTMLAttributes<HTMLDivElement>, 'className'> {
   children: React.ReactNode;
-  active?: ButtonProps['barId'];
-  className?: string;
   gap?: ValidSize | 0;
   merged?: boolean;
-};
+}
 
-function ButtonBar({
-  children,
-  className,
-  active,
-  merged = false,
-  gap = 0,
-}: ButtonBarProps) {
-  const shouldCheckActive = typeof active !== 'undefined';
+function ButtonBar({children, merged = false, gap = 0, ...props}: ButtonBarProps) {
   return (
-    <ButtonGrid merged={merged} gap={gap} className={className}>
-      {shouldCheckActive
-        ? Children.map(children, child => {
-            if (!isValidElement(child)) {
-              return child;
-            }
-
-            const {props: childProps, ...childWithoutProps} = child;
-
-            // We do not want to pass `barId` to <Button>`
-            const {barId, ...props} = childProps;
-            const isActive = active === barId;
-
-            // This ["primary"] could be customizable with a prop,
-            // but let's just enforce one "active" type for now
-            const priority = isActive ? 'primary' : childProps.priority || 'default';
-
-            return cloneElement(childWithoutProps as React.ReactElement, {
-              ...props,
-              className: classNames(className, {active: isActive}),
-              priority,
-            });
-          })
-        : children}
-    </ButtonGrid>
+    <StyledButtonBar merged={merged} gap={gap} {...props}>
+      {children}
+    </StyledButtonBar>
   );
 }
 
-const MergedStyles = () => css`
+const StyledButtonBar = styled('div')<{gap: ValidSize | 0; merged: boolean}>`
+  display: grid;
+  grid-auto-flow: column;
+  grid-column-gap: ${p => (p.gap === 0 ? '0' : space(p.gap))};
+  align-items: center;
+
+  ${p => p.merged && MergedButtonBarStyles}
+`;
+
+const MergedButtonBarStyles = () => css`
   /* Raised buttons show borders on both sides. Useful to create pill bars */
   & > .active {
     z-index: 2;
@@ -119,15 +95,6 @@ const MergedStyles = () => css`
       }
     }
   }
-`;
-
-const ButtonGrid = styled('div')<{gap: ValidSize | 0; merged: boolean}>`
-  display: grid;
-  grid-auto-flow: column;
-  grid-column-gap: ${p => (p.gap === 0 ? '0' : space(p.gap))};
-  align-items: center;
-
-  ${p => p.merged && MergedStyles}
 `;
 
 export default ButtonBar;
