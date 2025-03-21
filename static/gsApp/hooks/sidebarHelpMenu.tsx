b421fca2761df735539299400cac33cc97a18031
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-03-21 18:30:34 -0400

    ref(js): Remove withOrganization from SidebarHelpMenu (#87646)

--- static/gsApp/hooks/sidebarHelpMenu.tsx
+++ static/gsApp/hooks/sidebarHelpMenu.tsx
@@ -1,15 +1,11 @@
 import SidebarMenuItem from 'sentry/components/sidebar/sidebarMenuItem';
 import {t} from 'sentry/locale';
-import type {Organization} from 'sentry/types/organization';
-import withOrganization from 'sentry/utils/withOrganization';
+import useOrganization from 'sentry/utils/useOrganization';
 
 import ZendeskLink from 'getsentry/components/zendeskLink';
 
-type Props = {
-  organization: Organization;
-};
-
-function SupportLink({organization}: Props) {
+function SidebarHelpMenu() {
+  const organization = useOrganization();
   return (
     <ZendeskLink
       source="help_sidebar"
@@ -23,9 +19,8 @@ function SupportLink({organization}: Props) {
   );
 }
 
-const SidebarHelpMenu = withOrganization(SupportLink);
-const hookSidebarHelpMenu = <T extends Record<PropertyKey, unknown>>(props: T) => (
-  <SidebarHelpMenu key="sidebar:help-menu" {...props} />
+const hookSidebarHelpMenu = <T extends Record<PropertyKey, unknown>>(_props: T) => (
+  <SidebarHelpMenu key="sidebar:help-menu" />
 );
 
 export default hookSidebarHelpMenu;
