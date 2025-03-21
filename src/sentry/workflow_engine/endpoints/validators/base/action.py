44d902c16d279a3e5c6a77112ff52effb364500c
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-03-21 11:25:05 -0700

    move validate_json_schema to a utils folder so it can be shared for all the configs we need to validate

--- src/sentry/workflow_engine/endpoints/validators/base/action.py
+++ src/sentry/workflow_engine/endpoints/validators/base/action.py
@@ -1,12 +1,11 @@
 from typing import Any, Generic, TypeVar
 
 from django.forms import ValidationError
-from jsonschema import ValidationError as JsonValidationError
-from jsonschema import validate
 from rest_framework import serializers
 
 from sentry.api.serializers.rest_framework import CamelSnakeModelSerializer
 from sentry.db.models import Model
+from sentry.workflow_engine.endpoints.validators.utils import validate_json_schema
 from sentry.workflow_engine.models import Action
 from sentry.workflow_engine.registry import action_handler_registry
 from sentry.workflow_engine.types import ActionHandler
@@ -14,15 +13,6 @@ from sentry.workflow_engine.types import ActionHandler
 T = TypeVar("T", bound=Model)
 ActionData = dict[str, Any]
 ActionConfig = dict[str, Any]
-
-
-def validate_json_schema(value, schema):
-    try:
-        validate(value, schema)
-    except JsonValidationError as e:
-        raise ValidationError(str(e))
-
-    return value
 
 
 class BaseActionValidator(CamelSnakeModelSerializer[T], Generic[T]):
