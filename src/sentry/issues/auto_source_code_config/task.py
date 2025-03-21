getsentry/sentry

commit 0b751a240872935f12fedced4209fb7300f456f1

ref(auto_source_config): Multiple calls does not create new repository (#87437)
This depends on #87498 merging first.

In #86597 I introduced a bug causing many
[IntegrityErrors](https://sentry.sentry.io/explore/discover/results/?dataset=errors&end=2025-03-12T03%3A59%3A59.000&field=issue&field=count%28%29&interval=2h&name=IntegrityError%3A%20UniqueViolation%28%27duplicate%20key%20value%20violates%20unique%20constraint%20%22sentry_repositoryproject_project_id_stack_root_e376b891_uniq%22%5CnDETAIL%3A%20%20Key%20%28project_id%2C%20stack_root%29%3D%284507969920565328%2C%20kamino_platform%2F%29%20already%20exists.%5Cn%27%29&project=1&query=error.type%3AIntegrityError%20transaction%3Asentry.tasks.auto_source_code_config&queryDataset=error-events&sort=-count&start=2025-03-09T05%3A00%3A00.000&yAxis=count%28%29&yAxis=count_unique%28user%29)
because we called `create` when the repository already existed.

This change is to have a test to prevent trying to create the repository
more than once.

src/sentry/issues/auto_source_code_config/task.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+from collections.abc import MutableMapping
 from enum import StrEnum
 from typing import Any
 
@@ -176,9 +177,8 @@ def create_repos_and_code_mappings(
         raise InstallationNotFoundError
 
     organization_id = organization_integration.organization_id
-    with metrics.timer(
-        f"{METRIC_PREFIX}.create_configurations.duration", tags={"platform": platform}
-    ):
+    tags: MutableMapping[str, str | bool] = {"platform": platform, "dry_run": dry_run}
+    with metrics.timer(f"{METRIC_PREFIX}.create_configurations.duration", tags=tags):
         for code_mapping in code_mappings:
             repository = (
                 Repository.objects.filter(
@@ -187,39 +187,22 @@ def create_repos_and_code_mappings(
                 .order_by("-date_added")
                 .first()
             )
-
             if not repository:
+                created = False
                 if not dry_run:
-                    repository = Repository.objects.create(
+                    repository, created = Repository.objects.get_or_create(
                         name=code_mapping.repo.name,
                         organization_id=organization_id,
                         integration_id=organization_integration.integration_id,
                     )
-                metrics.incr(
-                    key=f"{METRIC_PREFIX}.repository.created",
-                    tags={"platform": platform, "dry_run": dry_run},
-                    sample_rate=1.0,
-                )
+                if created or dry_run:
+                    metrics.incr(
+                        key=f"{METRIC_PREFIX}.repository.created", tags=tags, sample_rate=1.0
+                    )
 
-            extra = {
-                "project_id": project.id,
-                "stack_root": code_mapping.stacktrace_root,
-                "repository_name": code_mapping.repo.name,
-            }
-            # The project and stack_root are unique together
-            existing_code_mappings = RepositoryProjectPathConfig.objects.filter(
-                project=project, stack_root=code_mapping.stacktrace_root
-            )
-            if existing_code_mappings.exists():
-                logger.warning("Investigate.", extra=extra)
-                continue
-
+            created = False
             if not dry_run:
-                if repository is None:  # This is mostly to appease the type checker
-                    logger.warning("Investigate.", extra=extra)
-                    continue
-
-                RepositoryProjectPathConfig.objects.create(
+                _, created = RepositoryProjectPathConfig.objects.get_or_create(
                     project=project,
                     stack_root=code_mapping.stacktrace_root,
                     repository=repository,
@@ -230,9 +213,7 @@ def create_repos_and_code_mappings(
                     default_branch=code_mapping.repo.branch,
                     automatically_generated=True,
                 )
-
-            metrics.incr(
-                key=f"{METRIC_PREFIX}.code_mapping.created",
-                tags={"platform": platform, "dry_run": dry_run},
-                sample_rate=1.0,
-            )
+            if created or dry_run:
+                metrics.incr(
+                    key=f"{METRIC_PREFIX}.code_mapping.created", tags=tags, sample_rate=1.0
+                )

tests/sentry/issues/auto_source_code_config/test_process_event.py
@@ -91,8 +91,11 @@ class BaseDeriveCodeMappings(TestCase):
             ),
             patch("sentry.utils.metrics.incr") as mock_incr,
         ):
+            repositories_count = Repository.objects.all().count()
+            code_mappings_count = RepositoryProjectPathConfig.objects.all().count()
             event = self.create_event(frames, platform)
             process_event(self.project.id, event.group_id, event.event_id)
+
             code_mappings = RepositoryProjectPathConfig.objects.all()
             assert len(code_mappings) == expected_num_code_mappings
             code_mapping = code_mappings.filter(
@@ -101,19 +104,20 @@ class BaseDeriveCodeMappings(TestCase):
             ).first()
             assert code_mapping is not None
             assert code_mapping.repository.name == expected_repo_name
+
             platform_config = PlatformConfig(platform)
             dry_run = platform_config.is_dry_run_platform()
-            # Check that both metrics were called with any order
-            mock_incr.assert_any_call(
-                key=f"{METRIC_PREFIX}.code_mapping.created",
-                tags={"dry_run": dry_run, "platform": event.platform},
-                sample_rate=1.0,
-            )
-            mock_incr.assert_any_call(
-                key=f"{METRIC_PREFIX}.repository.created",
-                tags={"dry_run": dry_run, "platform": event.platform},
-                sample_rate=1.0,
-            )
+            tags = {"dry_run": dry_run, "platform": platform}
+
+            if Repository.objects.all().count() > repositories_count:
+                mock_incr.assert_any_call(
+                    key=f"{METRIC_PREFIX}.repository.created", tags=tags, sample_rate=1.0
+                )
+
+            if RepositoryProjectPathConfig.objects.all().count() > code_mappings_count:
+                mock_incr.assert_any_call(
+                    key=f"{METRIC_PREFIX}.code_mapping.created", tags=tags, sample_rate=1.0
+                )
 
     def _process_and_assert_no_code_mapping(
         self,
@@ -250,7 +254,7 @@ class TestGenericBehaviour(BaseDeriveCodeMappings):
     def test_extension_is_not_included(self) -> None:
         frame_filename = "foo/bar.tbd"
         file_in_repo = "src/foo/bar.tbd"
-        platform = "foo"
+        platform = "other"
         self.event = self.create_event([{"filename": frame_filename, "in_app": True}], platform)
 
         with (
@@ -273,10 +277,11 @@ class TestGenericBehaviour(BaseDeriveCodeMappings):
                     expected_source_root="src/foo/",
                 )
 
-    def test_multiple_repositories(self) -> None:
-        platform = "foo"
+    def test_multiple_calls(self) -> None:
+        platform = "other"
+        # XXX: We need a test for when repo_files changes over time
         repo_trees = {
-            REPO1: ["src/foo/bar.py"],
+            REPO1: ["src/foo/bar.py", "src/app/main.py"],
             REPO2: ["app/baz/qux.py"],
         }
         with (
@@ -292,6 +297,27 @@ class TestGenericBehaviour(BaseDeriveCodeMappings):
                 expected_repo_name=REPO1,
                 expected_num_code_mappings=1,
             )
+            # Processing the same stacktrace again should not create anything new
+            self._process_and_assert_code_mapping(
+                repo_trees=repo_trees,
+                frames=[self.frame("foo/bar.py", True)],
+                platform=platform,
+                expected_stack_root="foo/",
+                expected_source_root="src/foo/",
+                expected_repo_name=REPO1,
+                expected_num_code_mappings=1,
+            )
+            # New code mapping in the same repository
+            self._process_and_assert_code_mapping(
+                repo_trees=repo_trees,
+                frames=[self.frame("app/main.py", True)],
+                platform=platform,
+                expected_stack_root="app/",
+                expected_source_root="src/app/",
+                expected_repo_name=REPO1,  # Same repository as before
+                expected_num_code_mappings=2,  # New code mapping
+            )
+            # New code mapping in a different repository
             self._process_and_assert_code_mapping(
                 repo_trees=repo_trees,
                 frames=[self.frame("baz/qux.py", True)],
@@ -299,7 +325,7 @@ class TestGenericBehaviour(BaseDeriveCodeMappings):
                 expected_stack_root="baz/",
                 expected_source_root="app/baz/",
                 expected_repo_name=REPO2,
-                expected_num_code_mappings=2,
+                expected_num_code_mappings=3,
             )
 
 

commit 1cd32060a5912035a64982203df1f496b6432d6e

fix(auto_source_config): Use `get_or_create` properly (#87581)
This fixes a bunch of integrity errors introduced in #87437.

From the
[docs](https://docs.djangoproject.com/en/5.1/ref/models/querysets/#get-or-create):

> Any keyword arguments passed to get_or_create() — except an optional
one called defaults — will be used in a get() call. If an object is
found, get_or_create() returns a tuple of that object and False.

src/sentry/issues/auto_source_code_config/task.py
@@ -205,13 +205,15 @@ def create_repos_and_code_mappings(
                 _, created = RepositoryProjectPathConfig.objects.get_or_create(
                     project=project,
                     stack_root=code_mapping.stacktrace_root,
-                    repository=repository,
-                    organization_integration_id=organization_integration.id,
-                    integration_id=organization_integration.integration_id,
-                    organization_id=organization_integration.organization_id,
-                    source_root=code_mapping.source_path,
-                    default_branch=code_mapping.repo.branch,
-                    automatically_generated=True,
+                    defaults={
+                        "repository": repository,
+                        "organization_integration_id": organization_integration.id,
+                        "integration_id": organization_integration.integration_id,
+                        "organization_id": organization_integration.organization_id,
+                        "source_root": code_mapping.source_path,
+                        "default_branch": code_mapping.repo.branch,
+                        "automatically_generated": True,
+                    },
                 )
             if created or dry_run:
                 metrics.incr(