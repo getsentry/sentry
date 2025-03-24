73fd6c2ee266eda4115794d1dd7b8d800e38ac6f
Snigdha Sharma <snigdha.sharma@sentry.io>
2025-03-24 15:13:22 -0700

    Add new GroupOpenPeriod table

---
+++ src/sentry/models/groupopenperiod.py
@@ -0,0 +1,41 @@
+from typing import Any
+
+from django.conf import settings
+from django.db import models
+from django.utils import timezone
+
+from sentry.backup.scopes import RelocationScope
+from sentry.db.models import FlexibleForeignKey, GzippedDictField, Model, sane_repr
+from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
+
+
+class GroupOpenPeriod(Model):
+    """
+    A GroupOpenPeriod is a period of time where a group is considered "open",
+    i.e. having a status that is not resolved. This is primarily used for
+    detector-based issues to track the period of time that an issue is open for.
+    """
+
+    __relocation_scope__ = RelocationScope.Excluded
+
+    project = FlexibleForeignKey("sentry.Project")
+    group = FlexibleForeignKey("sentry.Group")
+    resolution_activity = FlexibleForeignKey(
+        "sentry.Activity", null=True, on_delete=models.SET_NULL
+    )
+    data_condition = FlexibleForeignKey(
+        "workflow_engine.DataCondition", null=True, on_delete=models.SET_NULL
+    )
+
+    # if the user is not set, it's assumed to be the system
+    user_id = HybridCloudForeignKey(settings.AUTH_USER_MODEL, null=True, on_delete="SET_NULL")
+    date_started = models.DateTimeField(default=timezone.now)
+    date_ended = models.DateTimeField(null=True)
+
+    data: models.Field[dict[str, Any] | None, dict[str, Any]] = GzippedDictField(null=True)
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_groupopenperiod"
+
+    __repr__ = sane_repr("project_id", "group_id", "date_started", "date_ended", "user_id")
