d4b629b8c9728268738099d96a35614b93195611
JonasBa <jonas@badalic.com>
2025-03-24 18:41:52 -0400

    components: add styled import

--- static/app/components/externalIssues/externalIssueForm.tsx
+++ static/app/components/externalIssues/externalIssueForm.tsx
@@ -1,4 +1,5 @@
 import {useCallback, useEffect, useMemo, useState} from 'react';
+import styled from '@emotion/styled';
 import type {Span} from '@sentry/core';
 import * as Sentry from '@sentry/react';
 
