114222e6f36630399d3f378ad76e0ecff585853e
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-03-24 12:13:07 -0700

    ref(flags): fix analytic for setup sidebar (#87742)

--- static/app/utils/analytics/featureFlagAnalyticsEvents.tsx
+++ static/app/utils/analytics/featureFlagAnalyticsEvents.tsx
@@ -1,6 +1,7 @@
+import type {FeatureFlagOnboardingSurface} from 'sentry/components/events/featureFlags/useFeatureFlagOnboarding';
+
 export type FeatureFlagEventParameters = {
   'flags.cta_dismissed': {type: string};
-  'flags.cta_setup_button_clicked': Record<string, unknown>;
   'flags.event_and_suspect_flags_found': {
     numEventFlags: number;
     numSuspectFlags: number;
@@ -17,7 +18,9 @@ export type FeatureFlagEventParameters = {
     projectSlug: string;
   };
   'flags.view-all-clicked': Record<string, unknown>;
-  'flags.view-setup-sidebar': Record<string, unknown>;
+  'flags.view-setup-sidebar': {
+    surface: FeatureFlagOnboardingSurface;
+  };
 };
 
 export type FeatureFlagEventKey = keyof FeatureFlagEventParameters;
@@ -27,7 +30,6 @@ export const featureFlagEventMap: Record<FeatureFlagEventKey, string | null> = {
   'flags.sort_flags': 'Sorted Flags',
   'flags.event_and_suspect_flags_found': 'Number of Event and Suspect Flags',
   'flags.table_rendered': 'Flag Table Rendered',
-  'flags.cta_setup_button_clicked': 'Flag CTA Setup Button Clicked',
   'flags.cta_dismissed': 'Flag CTA Dismissed',
   'flags.logs-paginated': 'Feature Flag Logs Paginated',
   'flags.view-setup-sidebar': 'Viewed Feature Flag Onboarding Sidebar',
