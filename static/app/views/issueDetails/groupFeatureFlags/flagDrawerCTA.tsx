f1883e5bda0052e80d81942971e093032b72c81d
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-03-24 13:10:45 -0700

    Set analytics surface and rm unused analytics

--- static/app/views/issueDetails/groupFeatureFlags/flagDrawerCTA.tsx
+++ static/app/views/issueDetails/groupFeatureFlags/flagDrawerCTA.tsx
@@ -4,20 +4,14 @@ import {
 } from 'sentry/components/events/featureFlags/featureFlagInlineCTA';
 import {useFeatureFlagOnboarding} from 'sentry/components/events/featureFlags/useFeatureFlagOnboarding';
 import {useDrawerContentContext} from 'sentry/components/globalDrawer/components';
-import {trackAnalytics} from 'sentry/utils/analytics';
-import useOrganization from 'sentry/utils/useOrganization';
 
 export default function FlagDrawerCTA() {
-  const organization = useOrganization();
-  const {activateSidebar} = useFeatureFlagOnboarding();
+  const {activateSidebar} = useFeatureFlagOnboarding({
+    analyticsSurface: 'issue_details.flags_drawer',
+  });
   const {onClose: closeDrawer} = useDrawerContentContext();
 
   function handleSetupButtonClick(e: any) {
-    trackAnalytics('flags.setup_sidebar_opened', {
-      organization,
-      surface: 'issue_details.flags_drawer',
-    });
-    trackAnalytics('flags.cta_setup_button_clicked', {organization});
     closeDrawer?.();
     setTimeout(() => {
       // Wait for global drawer state to update
