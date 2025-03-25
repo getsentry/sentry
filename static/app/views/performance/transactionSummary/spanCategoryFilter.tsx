3cf0f32761e7ae1837cbe5cca332dddc1cbd7cea
Ash Anand <ash.anand@sentry.io>
2025-03-25 11:36:30 -0400

    functionality for fetching options using EAP and selecting

--- static/app/views/performance/transactionSummary/spanCategoryFilter.tsx
+++ static/app/views/performance/transactionSummary/spanCategoryFilter.tsx
@@ -1,3 +1,11 @@
+import {useMemo, useState} from 'react';
+import styled from '@emotion/styled';
+
+import {CompactSelect, type SelectOption} from 'sentry/components/compactSelect';
+import {pickBarColor} from 'sentry/components/performance/waterfall/utils';
+import {IconFilter} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import {useEAPSpans} from 'sentry/views/insights/common/queries/useDiscover';
 import {SpanIndexedField} from 'sentry/views/insights/types';
@@ -10,9 +18,17 @@ type Props = {
 const LIMIT = 10;
 
 export function SpanCategoryFilter({search, serviceEntrySpanName}: Props) {
-  const searchQuery = new MutableSearch(search);
-  // TODO: this will change to span.name eventually
-  searchQuery.addFilterValue('span.description', serviceEntrySpanName);
+  const [category, setCategory] = useState<string | undefined>(undefined);
+
+  const searchQuery = useMemo(() => {
+    const query = new MutableSearch(search);
+    query.addFilterValue('transaction', serviceEntrySpanName);
+
+    if (category) {
+      query.addFilterValue('span.category', category);
+    }
+    return query;
+  }, [search, serviceEntrySpanName, category]);
 
   const {data, isPending, error} = useEAPSpans(
     {
@@ -20,11 +36,53 @@ export function SpanCategoryFilter({search, serviceEntrySpanName}: Props) {
       fields: [SpanIndexedField.SPAN_CATEGORY, 'count()'],
       search: searchQuery,
       sorts: [{field: 'count()', kind: 'desc'}],
+      enabled: !!category,
     },
     'api.transaction-summary.span-category-filter'
   );
 
-  console.dir(data);
+  const options = useMemo(() => {
+    if (isPending || error) {
+      return [];
+    }
 
-  return <div>SpanCategoryFilter</div>;
+    return data
+      .filter(d => !!d[SpanIndexedField.SPAN_CATEGORY])
+      .map(d => ({
+        label: d[SpanIndexedField.SPAN_CATEGORY],
+        value: d[SpanIndexedField.SPAN_CATEGORY],
+        leadingItems: (
+          <OperationDot
+            backgroundColor={pickBarColor(d[SpanIndexedField.SPAN_CATEGORY])}
+          />
+        ),
+      }));
+  }, [data, isPending, error]);
+
+  const onChange = (selectedOption: SelectOption<string> | null) => {
+    console.log('selectedOption', selectedOption);
+    setCategory(selectedOption?.value ?? undefined);
+  };
+
+  return (
+    <CompactSelect
+      clearable
+      // disallowEmptySelection={false}
+      menuTitle={t('Filter by category')}
+      onClear={() => setCategory(undefined)}
+      options={options}
+      value={category ?? undefined}
+      onChange={onChange}
+      triggerLabel={t('Filter')}
+      triggerProps={{icon: <IconFilter />, 'aria-label': t('Filter by category')}}
+    />
+  );
 }
+
+const OperationDot = styled('div')<{backgroundColor: string}>`
+  display: block;
+  width: ${space(1)};
+  height: ${space(1)};
+  border-radius: 100%;
+  background-color: ${p => p.backgroundColor};
+`;
