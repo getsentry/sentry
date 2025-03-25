81820a3acf346c217cd34893445af31e2b045f24
Enoch Tang <enoch.tang@sentry.io>
2025-03-25 10:01:52 -0400

    feat(taskworker): Create shim to migrate celery tasks to taskbroker (#87722)
    ### Overview
    This PR is responsible for adding a shim into `@instrumented_task` to
    either return a `celery.Task` or a `taskworker.Task` depending on a
    rollout percentage defined in sentry options. To accomplish this, a
    `TaskworkerConfig` class was created as a container to hold everything
    required to create a `taskworker.Task`. This object is passed as an
    optional parameter to `@instrumented_task`. Finally, depending on the
    rollout percentage, the task object's `.delay()` or `.apply_async`
    methods are overwritten with taskworker methods.
    
    ### Migration
    To migrate a set of tasks:
    1. Create a new namespace according to the Taskbroker Namespace
    Assignments spreadsheet
    2. Pass the `TaskworkerConfig` object as a parameter of
    `instrumented_task()`
    3. Register the sentry option: 
    ```
    register(
        "taskworker.{namespace}.rollout",
        default={},
        flags=FLAG_AUTOMATOR_MODIFIABLE,
    )
    ```
    4. Deploy sentry
    5. Modify the sentry option by adding the task_name as a key and rollout
    percentage (0 -> 1) as the value:
    ```
    "taskworker.{namespace}.rollout": {
        example.task: 0.1
    }
    ```
    The configuration above will send 10% of tasks to taskworker and 90% of
    task to celery
    
    ### Testing
    * Unit testing to ensure shimming task to taskworker is successful
    * Local testing: Selected a random celery task, followed migration
    instructions above, successfully sent task into taskbroker sqlite.

---
+++ tests/sentry/tasks/test_taskworker_rollout.py
@@ -0,0 +1,147 @@
+from unittest import mock
+
+from sentry import options
+from sentry.tasks.base import instrumented_task
+from sentry.taskworker.config import TaskworkerConfig
+from sentry.taskworker.registry import TaskRegistry
+from sentry.testutils.cases import TestCase
+from sentry.testutils.helpers.options import override_options
+
+registry = TaskRegistry()
+
+
+class TestTaskworkerRollout(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.namespace = registry.create_namespace(name="test_namespace")
+        self.config = TaskworkerConfig(
+            namespace=self.namespace,
+            retry=None,
+            expires=None,
+            processing_deadline_duration=None,
+            at_most_once=False,
+            wait_for_delivery=False,
+        )
+        options.register("taskworker.test_namespace.rollout", default={})
+
+    def tearDown(self):
+        super().tearDown()
+        options.unregister("taskworker.test_namespace.rollout")
+
+    @mock.patch("sentry.tasks.base.random.random")
+    @mock.patch("sentry.taskworker.registry.TaskNamespace.send_task")
+    @override_options(
+        {"taskworker.test_namespace.rollout": {"test.test_with_taskworker_rollout": 0.5}}
+    )
+    def test_with_taskworker_rollout(self, mock_send_task, mock_random):
+        mock_random.return_value = 0.3
+
+        @instrumented_task(
+            name="test.test_with_taskworker_rollout",
+            taskworker_config=self.config,
+        )
+        def test_task():
+            return "done"
+
+        assert test_task.name == "test.test_with_taskworker_rollout"
+        task = self.namespace.get("test.test_with_taskworker_rollout")
+        assert task is not None
+        assert task.name == "test.test_with_taskworker_rollout"
+        test_task.delay()
+        test_task.apply_async()
+        assert mock_send_task.call_count == 2
+
+    @mock.patch("sentry.tasks.base.random.random")
+    @mock.patch("sentry.taskworker.registry.TaskNamespace.send_task")
+    @override_options(
+        {"taskworker.test_namespace.rollout": {"test.test_with_taskworker_rollout": 0.5}}
+    )
+    def test_with_taskworker_rollout_with_args(self, mock_send_task, mock_random):
+        mock_random.return_value = 0.3
+
+        @instrumented_task(
+            name="test.test_with_taskworker_rollout",
+            taskworker_config=self.config,
+        )
+        def test_task(msg):
+            return f"hello {msg}"
+
+        assert test_task.name == "test.test_with_taskworker_rollout"
+        task = self.namespace.get("test.test_with_taskworker_rollout")
+        assert task is not None
+        assert task.name == "test.test_with_taskworker_rollout"
+        test_task.delay("world")
+        test_task.apply_async("world")
+        assert mock_send_task.call_count == 2
+
+    @mock.patch("sentry.tasks.base.random.random")
+    @mock.patch("sentry.celery.Task.apply_async")
+    @override_options(
+        {"taskworker.test_namespace.rollout": {"test.test_without_taskworker_rollout": 0.3}}
+    )
+    def test_without_taskworker_rollout(self, mock_celery_apply_async, mock_random):
+        mock_random.return_value = 0.5
+
+        @instrumented_task(
+            name="test.test_without_taskworker_rollout",
+            taskworker_config=self.config,
+        )
+        def test_task(msg):
+            return f"hello {msg}"
+
+        assert test_task.name == "test.test_without_taskworker_rollout"
+        assert self.namespace.contains("test.test_without_taskworker_rollout") is True
+        test_task.delay()
+        test_task.apply_async()
+        assert mock_celery_apply_async.call_count == 2
+
+    @mock.patch("sentry.tasks.base.random.random")
+    @mock.patch("sentry.celery.Task.apply_async")
+    @override_options(
+        {"taskworker.test_namespace.rollout": {"test.test_without_taskworker_rollout": 0.3}}
+    )
+    def test_without_taskworker_rollout_with_args(self, mock_celery_apply_async, mock_random):
+        mock_random.return_value = 0.5
+
+        @instrumented_task(
+            name="test.test_without_taskworker_rollout",
+            taskworker_config=self.config,
+        )
+        def test_task(a, b):
+            return a + b
+
+        assert test_task.name == "test.test_without_taskworker_rollout"
+        assert self.namespace.contains("test.test_without_taskworker_rollout") is True
+        test_task.delay(1, 2)
+        test_task.apply_async(1, 2)
+        assert mock_celery_apply_async.call_count == 2
+
+    @mock.patch("sentry.celery.Task.apply_async")
+    def test_taskworker_no_rollout_configured(self, mock_celery_apply_async):
+        @instrumented_task(
+            name="test.test_taskworker_no_rollout_configured",
+            taskworker_config=self.config,
+        )
+        def test_task():
+            return "done"
+
+        assert test_task.name == "test.test_taskworker_no_rollout_configured"
+        assert self.namespace.contains("test.test_without_taskworker_rollout") is False
+        test_task.delay()
+        test_task.apply_async()
+        assert mock_celery_apply_async.call_count == 2
+
+    @mock.patch("sentry.celery.Task.apply_async")
+    def test_taskworker_no_rollout_configured_with_args(self, mock_celery_apply_async):
+        @instrumented_task(
+            name="test.test_taskworker_no_rollout_configured",
+            taskworker_config=self.config,
+        )
+        def test_task(msg):
+            return f"hello {msg}"
+
+        assert test_task.name == "test.test_taskworker_no_rollout_configured"
+        assert self.namespace.contains("test.test_without_taskworker_rollout") is False
+        test_task.delay("world")
+        test_task.apply_async("world")
+        assert mock_celery_apply_async.call_count == 2
