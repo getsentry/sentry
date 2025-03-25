53cae99d89a674e1d9ce223781494025bf05161d
JonasBa <jonas@badalic.com>
2025-03-25 10:07:26 -0400

    tabs: externalissueform

--- static/app/components/externalIssues/externalIssueForm.tsx
+++ static/app/components/externalIssues/externalIssueForm.tsx
@@ -1,4 +1,5 @@
 import {useCallback, useEffect, useMemo, useState} from 'react';
+import styled from '@emotion/styled';
 import type {Span} from '@sentry/core';
 import * as Sentry from '@sentry/react';
 
@@ -20,8 +21,9 @@ import type {FieldValue} from 'sentry/components/forms/model';
 import FormModel from 'sentry/components/forms/model';
 import LoadingError from 'sentry/components/loadingError';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
-import NavTabs from 'sentry/components/navTabs';
+import {TabList, Tabs} from 'sentry/components/tabs';
 import {t, tct} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import type {Group} from 'sentry/types/group';
 import type {
   Integration,
@@ -333,17 +335,21 @@ export default function ExternalIssueForm({
       }}
       title={title}
       navTabs={
-        <NavTabs underlined>
-          <li className={action === 'create' ? 'active' : ''}>
-            <a onClick={() => handleClick('create')}>{t('Create')}</a>
-          </li>
-          <li className={action === 'link' ? 'active' : ''}>
-            <a onClick={() => handleClick('link')}>{t('Link')}</a>
-          </li>
-        </NavTabs>
+        <TabsContainer>
+          <Tabs value={action} onChange={tab => handleClick(tab)}>
+            <TabList>
+              <TabList.Item key="create">{t('Create')}</TabList.Item>
+              <TabList.Item key="link">{t('Link')}</TabList.Item>
+            </TabList>
+          </Tabs>
+        </TabsContainer>
       }
       bodyText={null}
       getFieldProps={getExternalIssueFieldProps}
     />
   );
 }
+
+const TabsContainer = styled('div')`
+  margin-bottom: ${space(2)};
+`;
