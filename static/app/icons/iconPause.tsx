b78a0bf8fcab896cfe2951854df4a08bff4853fd
JonasBa <jonas@badalic.com>
2025-03-26 10:30:27 -0400

    icons: almost there

--- static/app/icons/iconPause.tsx
+++ static/app/icons/iconPause.tsx
@@ -1,4 +1,4 @@
-import {forwardRef} from 'react';
+import {forwardRef, Fragment} from 'react';
 import {useTheme} from '@emotion/react';
 
 import type {SVGIconProps} from './svgIcon';
@@ -8,8 +8,17 @@ const IconPause = forwardRef<SVGSVGElement, SVGIconProps>((props, ref) => {
   const theme = useTheme();
   return (
     <SvgIcon {...props} ref={ref}>
-      <path d="M5.83,15.28H1.47a.76.76,0,0,1-.75-.75V1.47A.76.76,0,0,1,1.47.72H5.83a.76.76,0,0,1,.75.75V14.53A.76.76,0,0,1,5.83,15.28Zm-3.61-1.5H5.08V2.22H2.22Z" />
-      <path d="M14.53,15.28H10.17a.76.76,0,0,1-.75-.75V1.47a.76.76,0,0,1,.75-.75h4.36a.76.76,0,0,1,.75.75V14.53A.76.76,0,0,1,14.53,15.28Zm-3.61-1.5h2.86V2.22H10.92Z" />
+      {theme.isChonk ? (
+        <Fragment>
+          <rect x="2.75" y="2.75" width="3.5" height="10.5" rx="1" ry="1" />
+          <rect x="9.75" y="2.75" width="3.5" height="10.5" rx="1" ry="1" />
+        </Fragment>
+      ) : (
+        <Fragment>
+          <path d="M5.83,15.28H1.47a.76.76,0,0,1-.75-.75V1.47A.76.76,0,0,1,1.47.72H5.83a.76.76,0,0,1,.75.75V14.53A.76.76,0,0,1,5.83,15.28Zm-3.61-1.5H5.08V2.22H2.22Z" />
+          <path d="M14.53,15.28H10.17a.76.76,0,0,1-.75-.75V1.47a.76.76,0,0,1,.75-.75h4.36a.76.76,0,0,1,.75.75V14.53A.76.76,0,0,1,14.53,15.28Zm-3.61-1.5h2.86V2.22H10.92Z" />
+        </Fragment>
+      )}
     </SvgIcon>
   );
 });
--- static/app/icons/iconSubtract.tsx
+++ static/app/icons/iconSubtract.tsx
@@ -13,7 +13,9 @@ const IconSubtract = forwardRef<SVGSVGElement, Props>(
     const theme = useTheme();
     return (
       <SvgIcon {...props} data-test-id="icon-subtract" ref={ref}>
-        {isCircled ? (
+        {theme.isChonk ? (
+          <line x1="2.75" y1="8" x2="13.25" y2="8" />
+        ) : isCircled ? (
           <Fragment>
             <path d="M8,16a8,8,0,1,1,8-8A8,8,0,0,1,8,16ZM8,1.53A6.47,6.47,0,1,0,14.47,8,6.47,6.47,0,0,0,8,1.53Z" />
             <path d="M11.28,8.75H4.72a.75.75,0,1,1,0-1.5h6.56a.75.75,0,1,1,0,1.5Z" />
