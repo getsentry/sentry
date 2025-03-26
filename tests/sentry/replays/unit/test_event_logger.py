f4a24fc8796ada9294d55c9704b3fa92dea55cb9
Colton Allen <colton.allen@sentry.io>
2025-03-26 11:02:19 -0500

    Fix typing

--- tests/sentry/replays/unit/test_event_logger.py
+++ tests/sentry/replays/unit/test_event_logger.py
@@ -5,24 +5,24 @@ from sentry.replays.usecases.ingest.event_parser import ClickEvent, ParsedEventM
 def test_gen_rage_clicks():
     # No clicks.
     meta = ParsedEventMeta([], [], [], [], [], [])
-    assert len(list(gen_rage_clicks(meta, 1, "1", {"a", "b"}))) == 0
+    assert len(list(gen_rage_clicks(meta, 1, "1", {"a": "b"}))) == 0
 
     # Not a rage click and not URL.
     meta.click_events.append(
         ClickEvent("", "", [], "", "", 0, 0, 0, "", "", "", "", "", 0, "", url="")
     )
-    assert len(list(gen_rage_clicks(meta, 1, "1", {"a", "b"}))) == 0
+    assert len(list(gen_rage_clicks(meta, 1, "1", {"a": "b"}))) == 0
 
     # Rage click but not url.
     meta.click_events.append(
         ClickEvent("", "", [], "", "", 0, 1, 0, "", "", "", "", "", 0, "", url="")
     )
-    assert len(list(gen_rage_clicks(meta, 1, "1", {"a", "b"}))) == 0
+    assert len(list(gen_rage_clicks(meta, 1, "1", {"a": "b"}))) == 0
 
     # Rage click and url specified.
     meta.click_events.append(
         ClickEvent("", "", [], "", "", 0, 1, 0, "", "", "", "", "", 0, "", url="t")
     )
-    assert len(list(gen_rage_clicks(meta, 1, "1", {"a", "b"}))) == 1
+    assert len(list(gen_rage_clicks(meta, 1, "1", {"a": "b"}))) == 1
     assert len(list(gen_rage_clicks(meta, 1, "1", {}))) == 0
     assert len(list(gen_rage_clicks(meta, 1, "1", None))) == 0
