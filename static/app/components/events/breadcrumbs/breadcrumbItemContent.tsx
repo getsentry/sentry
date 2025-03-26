9f5298cb3bef98c69b13f66e2e88f98bb2da0a33
Luca Forstner <luca.forstner@sentry.io>
2025-03-26 15:38:32 +0100

    feat(breadcrumbs): Don't show `__span` property in breadcrumbs (#87971)

--- static/app/components/events/breadcrumbs/breadcrumbItemContent.tsx
+++ static/app/components/events/breadcrumbs/breadcrumbItemContent.tsx
@@ -67,9 +67,16 @@ export default function BreadcrumbItemContent({
       )}
     </BreadcrumbText>
   ) : null;
-  const defaultData = defined(bc.data) ? (
+
+  const cleanedBreadcrumbData = cleanBreadcrumbData(bc.data);
+
+  const defaultData = defined(cleanedBreadcrumbData) ? (
     <Timeline.Data>
-      <StructuredData value={bc.data} meta={meta?.data} {...structuredDataProps} />
+      <StructuredData
+        value={cleanedBreadcrumbData}
+        meta={meta?.data}
+        {...structuredDataProps}
+      />
     </Timeline.Data>
   ) : null;
 
@@ -124,7 +131,12 @@ function HTTPCrumbContent({
   structuredDataProps: typeof DEFAULT_STRUCTURED_DATA_PROPS;
   meta?: Record<string, any>;
 }) {
-  const {method, url, status_code: statusCode, ...otherData} = breadcrumb?.data ?? {};
+  const {
+    method,
+    url,
+    status_code: statusCode,
+    ...otherData
+  } = cleanBreadcrumbData(breadcrumb?.data) ?? {};
   const isValidUrl = !meta && defined(url) && isUrl(url);
   return (
     <Fragment>
@@ -206,6 +218,26 @@ function ExceptionCrumbContent({
       ) : null}
     </Fragment>
   );
+}
+
+function cleanBreadcrumbData<B extends Record<string, any> | undefined | null>(
+  breadcrumbData: B
+): B {
+  if (!breadcrumbData) {
+    return breadcrumbData;
+  }
+
+  const cleanedBreadcrumbData = {...breadcrumbData};
+
+  // The JS SDK emits the __span property since forever (3+ years).
+  // Originally it was introduced to potentially be able to go from the breadcrumb to a particular span within a trace.
+  // Up until now, this link wasn't built. Showing the span property is extremely noisy within the interface so we hide it.
+  // We can at any point in time use this data again to link to a trace, however, to be able to link to a trace we need to be sure that the trace actually exists.
+  if ('__span' in cleanedBreadcrumbData) {
+    delete cleanedBreadcrumbData.__span;
+  }
+
+  return cleanedBreadcrumbData;
 }
 
 const Link = styled('a')`
