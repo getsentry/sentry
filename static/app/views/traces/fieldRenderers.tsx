08d061eefa70fb3c7de70513b9d7866eefae5041
Malachi Willey <malwilley@gmail.com>
2025-03-26 13:39:50 -0700

    feat(ui): Make ProjectList a common component (#88017)

--- static/app/views/traces/fieldRenderers.tsx
+++ static/app/views/traces/fieldRenderers.tsx
@@ -10,6 +10,7 @@ import Link from 'sentry/components/links/link';
 import {RowRectangle} from 'sentry/components/performance/waterfall/rowBar';
 import {pickBarColor} from 'sentry/components/performance/waterfall/utils';
 import PerformanceDuration from 'sentry/components/performanceDuration';
+import {ProjectList} from 'sentry/components/projectList';
 import TimeSince from 'sentry/components/timeSince';
 import {Tooltip} from 'sentry/components/tooltip';
 import {IconIssues} from 'sentry/icons';
@@ -66,116 +67,31 @@ export function ProjectsRenderer({
   projectSlugs,
   maxVisibleProjects = 2,
 }: ProjectsRendererProps) {
-  const organization = useOrganization();
-
   return (
-    <Projects orgId={organization.slug} slugs={projectSlugs}>
-      {({projects}) => {
-        // ensure that projectAvatars is in the same order as the projectSlugs prop
-        const projectAvatars = projectSlugs.map(slug => {
-          return projects.find(project => project.slug === slug) ?? {slug};
-        });
-        const numProjects = projectAvatars.length;
-        const numVisibleProjects =
-          maxVisibleProjects - numProjects >= 0 ? numProjects : maxVisibleProjects - 1;
-        const visibleProjectAvatars = projectAvatars
-          .slice(0, numVisibleProjects)
-          .reverse();
-        const collapsedProjectAvatars = projectAvatars.slice(numVisibleProjects);
-        const numCollapsedProjects = collapsedProjectAvatars.length;
-
-        return (
-          <ProjectList>
-            {numCollapsedProjects > 0 && (
-              <Tooltip
-                skipWrapper
-                title={
-                  <CollapsedProjects>
-                    {tn(
-                      'This trace contains %s more project.',
-                      'This trace contains %s more projects.',
-                      numCollapsedProjects
-                    )}
-                    {collapsedProjectAvatars.map(project => (
-                      <ProjectBadge
-                        key={project.slug}
-                        project={project}
-                        avatarSize={16}
-                      />
-                    ))}
-                  </CollapsedProjects>
-                }
-              >
-                <CollapsedBadge
-                  size={20}
-                  fontSize={10}
-                  data-test-id="collapsed-projects-badge"
-                >
-                  +{numCollapsedProjects}
-                </CollapsedBadge>
-              </Tooltip>
-            )}
-            {visibleProjectAvatars.map(project => (
-              <StyledProjectBadge
-                key={project.slug}
-                hideName
-                project={project}
-                avatarSize={16}
-                avatarProps={{hasTooltip: true, tooltip: project.slug}}
-              />
-            ))}
-          </ProjectList>
-        );
-      }}
-    </Projects>
+    <ProjectList
+      maxVisibleProjects={maxVisibleProjects}
+      projectSlugs={projectSlugs}
+      collapsedProjectsTooltip={projects => (
+        <CollapsedProjects>
+          {tn(
+            'This trace contains %s more project.',
+            'This trace contains %s more projects.',
+            projects.length
+          )}
+          {projects.map(project => (
+            <ProjectBadge key={project.slug} project={project} avatarSize={16} />
+          ))}
+        </CollapsedProjects>
+      )}
+    />
   );
 }
-
-const ProjectList = styled('div')`
-  display: flex;
-  align-items: center;
-  flex-direction: row-reverse;
-  justify-content: flex-end;
-  padding-right: 8px;
-`;
 
 const CollapsedProjects = styled('div')`
   width: 200px;
   display: flex;
   flex-direction: column;
   gap: ${space(0.5)};
-`;
-
-const AvatarStyle = (p: any) => css`
-  border: 2px solid ${p.theme.background};
-  margin-right: -8px;
-  cursor: default;
-
-  &:hover {
-    z-index: 1;
-  }
-`;
-
-const StyledProjectBadge = styled(ProjectBadge)`
-  overflow: hidden;
-  z-index: 0;
-  ${AvatarStyle}
-`;
-
-const CollapsedBadge = styled('div')<{fontSize: number; size: number}>`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  text-align: center;
-  font-weight: ${p => p.theme.fontWeightBold};
-  background-color: ${p => p.theme.gray200};
-  color: ${p => p.theme.subText};
-  font-size: ${p => p.fontSize}px;
-  width: ${p => p.size}px;
-  height: ${p => p.size}px;
-  border-radius: ${p => p.theme.borderRadius};
-  ${AvatarStyle}
 `;
 
 interface ProjectRendererProps {
