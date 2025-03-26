bb39fae178d62e91a18d2c6a221750939c31711c
Raj Joshi <raj.joshi@sentry.io>
2025-03-26 16:31:43 -0700

    :wrench: chore: cleanup init

--- src/sentry/workflow_engine/handlers/__init__.py
+++ src/sentry/workflow_engine/handlers/__init__.py
@@ -1,9 +1,7 @@
 # Export any handlers we want to include into the registry
 __all__ = [
-    "ActionHandler",
     "EventCreatedByDetectorConditionHandler",
     "EventSeenCountConditionHandler",
 ]
 
-from .action import ActionHandler
 from .condition import EventCreatedByDetectorConditionHandler, EventSeenCountConditionHandler
--- src/sentry/workflow_engine/handlers/action/__init__.py
+++
@@ -1,5 +0,0 @@
-__all__ = [
-    "ActionHandler",
-]
-
-from .notification.handler import ActionHandler
