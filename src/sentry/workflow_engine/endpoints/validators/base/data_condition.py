2ddc31c3482a042c0c63414ecd3f7c67ab9e7fb5
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-03-26 14:10:03 -0700

    Add validate_condition_result and a basic validate_comparison method.

--- src/sentry/workflow_engine/endpoints/validators/base/data_condition.py
+++ src/sentry/workflow_engine/endpoints/validators/base/data_condition.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from rest_framework.fields import Field
 
 from sentry.api.serializers.rest_framework import CamelSnakeSerializer
 from sentry.workflow_engine.models import Condition
@@ -11,18 +10,34 @@ class BaseDataConditionValidator(CamelSnakeSerializer):
         max_length=200,
     )
 
+    comparison = serializers.JSONField(required=True)
+    condition_result = serializers.JSONField(required=True)
+    # condition_group
+
     def validate_type(self, value):
         try:
             vaildated_type = Condition(value)
         except ValueError:
-            raise serializers.ValidationError(f"Invalid Condition.type: {value}")
+            raise serializers.ValidationError(f"Invalid DataCondition.type: {value}")
 
         return vaildated_type
 
-    @property
-    def comparison(self) -> Field:
-        raise NotImplementedError
+    def validate_comparison(self, value):
+        # TODO - validate against schema
+        # handler = self._get_handler_handler()
+        # schema = handler.comparison_json_schmea
+        # validate_json(value, schema)
+        return value
 
-    @property
-    def result(self) -> Field:
-        raise NotImplementedError
+    def validate_condition_result(self, value):
+        if isinstance(value, (dict, list)):
+            raise serializers.ValidationError(
+                f"Invalid DataCondition.condition_result, {value}, must be a primitive value"
+            )
+
+        return value
+
+    def validate_condition_group(self, value):
+        # TODO - validate that the condition group exists
+        # TODO - validate they have permissions to access the group?
+        return value
--- tests/sentry/workflow_engine/endpoints/validators/test_base_data_condition.py
+++ tests/sentry/workflow_engine/endpoints/validators/test_base_data_condition.py
@@ -17,12 +17,60 @@ class TestBaseDataConditionValidator(TestCase):
         validator = BaseDataConditionValidator(data=self.valid_data)
         assert validator.is_valid() is True
 
-    def test_conditions__invalid_condition(self):
+    def test_conditions__no_type(self):
         invalid_data = {"comparison": 0}
         validator = BaseDataConditionValidator(data=invalid_data)
         assert validator.is_valid() is False
 
     def test_conditions__invalid_condition_type(self):
-        invalid_data = {"type": "invalid-type"}
+        invalid_data = {**self.valid_data, "type": "invalid-type"}
+        validator = BaseDataConditionValidator(data=invalid_data)
+        assert validator.is_valid() is False
+
+    def test_comparison__no_comparison(self):
+        invalid_data = {"type": Condition.EQUAL}
+        validator = BaseDataConditionValidator(data=invalid_data)
+        assert validator.is_valid() is False
+
+    # TODO - do we need to compare to null ever? probably?
+    # def test_comparison__null_works(self):
+    #     invalid_data = {**self.valid_data, "comparison": None}
+    #     validator = BaseDataConditionValidator(data=invalid_data)
+    #     assert validator.is_valid() is True
+
+    def test_comparison__primitive_value(self):
+        valid_data = {**self.valid_data, "comparison": 1}
+        validator = BaseDataConditionValidator(data=valid_data)
+        assert validator.is_valid() is True
+
+    def test_comparison__complex_value(self):
+        # TODO - add validation for the json field
+        # -- this requires a lookup to the dictionary via the type, then getting the schema
+        valid_data = {**self.valid_data, "comparison": {"key": "value"}}
+        validator = BaseDataConditionValidator(data=valid_data)
+        assert validator.is_valid() is True
+
+    def test_condition_result__primitive_value__bool(self):
+        valid_data = {**self.valid_data, "condition_result": True}
+        validator = BaseDataConditionValidator(data=valid_data)
+        assert validator.is_valid() is True
+
+    def test_condition_result__primitive_value__int(self):
+        valid_data = {**self.valid_data, "condition_result": 1}
+        validator = BaseDataConditionValidator(data=valid_data)
+        assert validator.is_valid() is True
+
+    def test_condition_result__primitive_value__string(self):
+        valid_data = {**self.valid_data, "condition_result": "foo"}
+        validator = BaseDataConditionValidator(data=valid_data)
+        assert validator.is_valid() is True
+
+    def test_condition_result__complex_value__dict(self):
+        invalid_data = {**self.valid_data, "condition_result": {"key": "value"}}
+        validator = BaseDataConditionValidator(data=invalid_data)
+        assert validator.is_valid() is False
+
+    def test_condition_result__complex_value__array(self):
+        invalid_data = {**self.valid_data, "condition_result": ["foo"]}
         validator = BaseDataConditionValidator(data=invalid_data)
         assert validator.is_valid() is False
