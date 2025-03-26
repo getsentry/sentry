4457cce1af736a2ef3b3be8846adca06b31c88fe
Jonas <jonas.badalic@sentry.io>
2025-03-26 09:59:11 -0400

    tabs: use new tabs on project filters (#87860)
    Remove deprecated NavTabs

--- static/app/views/settings/project/projectFilters/index.tsx
+++ static/app/views/settings/project/projectFilters/index.tsx
@@ -1,9 +1,10 @@
 import {Fragment} from 'react';
+import styled from '@emotion/styled';
 
-import Link from 'sentry/components/links/link';
-import NavTabs from 'sentry/components/navTabs';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
+import {TabList, Tabs} from 'sentry/components/tabs';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import type {Organization} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
@@ -45,18 +46,24 @@ function ProjectFilters(props: Props) {
         <ProjectFiltersChart project={project} />
 
         {features.has('discard-groups') && (
-          <NavTabs underlined style={{paddingTop: '30px'}}>
-            <li className={filterType === 'data-filters' ? 'active' : ''}>
-              <Link to={recreateRoute('data-filters/', {...props, stepBack: -1})}>
-                {t('Data Filters')}
-              </Link>
-            </li>
-            <li className={filterType === 'discarded-groups' ? 'active' : ''}>
-              <Link to={recreateRoute('discarded-groups/', {...props, stepBack: -1})}>
-                {t('Discarded Issues')}
-              </Link>
-            </li>
-          </NavTabs>
+          <TabsContainer>
+            <Tabs value={filterType}>
+              <TabList>
+                <TabList.Item
+                  key="data-filters"
+                  to={recreateRoute('data-filters/', {...props, stepBack: -1})}
+                >
+                  {t('Data Filters')}
+                </TabList.Item>
+                <TabList.Item
+                  key="discarded-groups"
+                  to={recreateRoute('discarded-groups/', {...props, stepBack: -1})}
+                >
+                  {t('Discarded Issues')}
+                </TabList.Item>
+              </TabList>
+            </Tabs>
+          </TabsContainer>
         )}
 
         {filterType === 'discarded-groups' ? (
@@ -68,5 +75,9 @@ function ProjectFilters(props: Props) {
     </Fragment>
   );
 }
+
+const TabsContainer = styled('div')`
+  margin-bottom: ${space(2)};
+`;
 
 export default ProjectFilters;
