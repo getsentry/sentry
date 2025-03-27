90dea6a41f71552db868f8f7bcedb2e804720d8b
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-03-26 22:53:43 -0700

    Update comment

--- static/app/views/issueDetails/types.tsx
+++ static/app/views/issueDetails/types.tsx
@@ -3,7 +3,7 @@ export enum Tab {
   ACTIVITY = 'activity',
   USER_FEEDBACK = 'user-feedback',
   ATTACHMENTS = 'attachments',
-  DISTRIBUTIONS = 'distributions', // Renamed from 'tags' to include feature flags.
+  DISTRIBUTIONS = 'distributions', // Renamed from TAGS so the route may include feature flags.
   EVENTS = 'events',
   MERGED = 'merged',
   SIMILAR_ISSUES = 'similar-issues',
