cc6bbe6b2c0b2effab4ad95c3daa602dce3c0ee9
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-03-27 16:33:00 -0700

    Use a choice field to enforce something in the enum

--- src/sentry/workflow_engine/endpoints/validators/base/data_condition_group.py
+++ src/sentry/workflow_engine/endpoints/validators/base/data_condition_group.py
@@ -2,10 +2,11 @@ from rest_framework import serializers
 
 from sentry.api.serializers.rest_framework import CamelSnakeSerializer
 from sentry.workflow_engine.endpoints.validators.base import BaseDataConditionValidator
+from sentry.workflow_engine.models import DataConditionGroup
 
 
 class BaseDataConditionGroupValidator(CamelSnakeSerializer):
-    logic_type = serializers.CharField(required=True)
+    logic_type = serializers.ChoiceField([(t.value, t.value) for t in DataConditionGroup.Type])
     organization_id = serializers.IntegerField(required=True)
     conditions = serializers.ListField(required=False)
 
