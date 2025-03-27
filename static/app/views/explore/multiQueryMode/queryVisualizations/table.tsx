899d2e373c988a889914538c1629dd978b622213
JonasBa <jonas@badalic.com>
2025-03-27 17:01:21 -0400

    theme: remove direct import of chartPalette

--- static/app/views/explore/multiQueryMode/queryVisualizations/table.tsx
+++ static/app/views/explore/multiQueryMode/queryVisualizations/table.tsx
@@ -1,4 +1,5 @@
 import {Fragment, useMemo, useRef} from 'react';
+import {useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import EmptyStateWarning from 'sentry/components/emptyStateWarning';
