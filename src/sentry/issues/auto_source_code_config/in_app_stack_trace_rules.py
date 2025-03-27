d3afa10f195dadee888ecb36fec067ea271acd34
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2025-03-27 08:50:26 -0400

    feat(auto_source_config): Derive in-app stack trace rules (#87842)
    This is the logic to mark frames as in-app when the files are found in
    the developers' GitHub repositories.
    
    This is very important for languages where the SDK or our internal rules
    cannot determine which frames are in-app and which are not.
    
    The initial language to get support for this is Java.
    
    This will run in dry run mode to discover errors or scaling problems.
    
    ---------
    
    Co-authored-by: Katie Byers <katie.byers@sentry.io>

---
+++ src/sentry/issues/auto_source_code_config/in_app_stack_trace_rules.py
@@ -0,0 +1,64 @@
+import logging
+from collections.abc import Sequence
+
+from sentry.issues.auto_source_code_config.code_mapping import CodeMapping
+from sentry.issues.auto_source_code_config.utils import PlatformConfig
+from sentry.models.project import Project
+from sentry.utils import metrics
+
+from .constants import DERIVED_ENHANCEMENTS_OPTION_KEY, METRIC_PREFIX
+
+logger = logging.getLogger(__name__)
+
+
+def save_in_app_stack_trace_rules(
+    project: Project, code_mappings: Sequence[CodeMapping], platform_config: PlatformConfig
+) -> list[str]:
+    """Save in app stack trace rules for a given project"""
+    rules_from_code_mappings = set()
+    for code_mapping in code_mappings:
+        try:
+            rules_from_code_mappings.add(generate_rule_for_code_mapping(code_mapping))
+        except ValueError:
+            pass
+
+    current_enhancements = project.get_option(DERIVED_ENHANCEMENTS_OPTION_KEY)
+    current_rules = set(current_enhancements.split("\n")) if current_enhancements else set()
+
+    united_rules = rules_from_code_mappings.union(current_rules)
+    if not platform_config.is_dry_run_platform() and united_rules != current_rules:
+        project.update_option(DERIVED_ENHANCEMENTS_OPTION_KEY, "\n".join(sorted(united_rules)))
+
+    new_rules_added = united_rules - current_rules
+    metrics.incr(
+        key=f"{METRIC_PREFIX}.in_app_stack_trace_rules.created",
+        amount=len(new_rules_added),
+        tags={
+            "platform": platform_config.platform,
+            "dry_run": platform_config.is_dry_run_platform(),
+        },
+        sample_rate=1.0,
+    )
+    return list(new_rules_added)
+
+
+# XXX: This is very Java specific. If we want to support other languages, we need to
+# come up with a better way to generate the rule.
+def generate_rule_for_code_mapping(code_mapping: CodeMapping) -> str:
+    """Generate an in-app rule for a given code mapping"""
+    stacktrace_root = code_mapping.stacktrace_root
+    if stacktrace_root == "":
+        raise ValueError("Stacktrace root is empty")
+
+    parts = stacktrace_root.rstrip("/").split("/", 2)
+    # We only want the first two parts
+    module = ".".join(parts[:2])
+
+    if module == "":
+        raise ValueError("Module is empty")
+
+    # a/ -> a.**
+    # x/y/ -> x.y.**
+    # com/example/foo/bar/ -> com.example.**
+    # uk/co/example/foo/bar/ -> uk.co.**
+    return f"stack.module:{module}.** +app"
