047a477508bd200795b263b284d4122ce1abe133
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-03-27 11:54:28 -0400

    wip

--- static/app/views/insights/common/components/insightsTimeSeriesWidget.tsx
+++ static/app/views/insights/common/components/insightsTimeSeriesWidget.tsx
@@ -158,6 +158,7 @@ export function InsightsTimeSeriesWidget(props: InsightsTimeSeriesWidgetProps) {
 
 const COMMON_COLORS: Record<string, string> = {
   'spm()': THROUGHPUT_COLOR,
+  'epm()': THROUGHPUT_COLOR,
   'count()': COUNT_COLOR,
   'avg(span.self_time)': AVG_COLOR,
   'http_response_rate(3)': HTTP_RESPONSE_3XX_COLOR,
