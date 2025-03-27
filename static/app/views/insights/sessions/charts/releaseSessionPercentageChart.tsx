15700f5d6d9e50b15216eb6cf5df139a4017375e
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-03-27 15:36:51 -0700

    ref(releases): add status to release table

--- static/app/views/insights/sessions/charts/releaseSessionPercentageChart.tsx
+++ static/app/views/insights/sessions/charts/releaseSessionPercentageChart.tsx
@@ -13,7 +13,7 @@ export default function ReleaseSessionPercentageChart() {
 
   return (
     <InsightsAreaChartWidget
-      title={t('Release Adoption')}
+      title={t('Percent Sessions by Release')}
       description={t(
         'The percentage of total sessions that each release accounted for. The 5 most recent releases are shown.'
       )}
