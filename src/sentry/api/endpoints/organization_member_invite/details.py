36a6b5bd27ba62e60700ea6d47bcbb99f4779555
Michelle Fu <michelle.fu@sentry.io>
2025-03-27 16:31:04 -0700

    convert args

--- src/sentry/api/endpoints/organization_member_invite/details.py
+++ src/sentry/api/endpoints/organization_member_invite/details.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -5,6 +7,7 @@ from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.bases.organization import OrganizationEndpoint
 from sentry.api.endpoints.organization_member.utils import RelaxedMemberPermission
+from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.serializers import serialize
 from sentry.models.organization import Organization
 from sentry.models.organizationmemberinvite import OrganizationMemberInvite
@@ -18,6 +21,24 @@ class OrganizationMemberIndexDetailsEndpoint(OrganizationEndpoint):
     }
     owner = ApiOwner.ENTERPRISE
     permission_classes = (RelaxedMemberPermission,)
+
+    def convert_args(
+        self,
+        request: Request,
+        member_invite_id: str,
+        organization_id_or_slug: str | int | None = None,
+        *args: Any,
+        **kwargs: Any,
+    ) -> tuple[tuple[Any, ...], dict[str, Any]]:
+        args, kwargs = super().convert_args(request, organization_id_or_slug, *args, **kwargs)
+
+        try:
+            kwargs["invited_member"] = OrganizationMemberInvite.objects.filter(
+                id=int(member_invite_id)
+            )
+        except OrganizationMemberInvite.DoesNotExist:
+            raise ResourceDoesNotExist
+        return args, kwargs
 
     def get(
         self,
