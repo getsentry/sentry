2e263f0a6a24514ccfeea75e3f3d729ca595e8d2
Erik Withak <erikaanestad@gmail.com>
2025-03-28 13:07:14 -0700

    adds missing `traceViewRoute` to route `/organizations/:orgId/dashboards/`

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -1129,6 +1129,7 @@ function buildRoutes() {
           key="org-dashboards"
         >
           <IndexRoute component={make(() => import('sentry/views/dashboards/manage'))} />
+          {traceViewRoute}
         </Route>
       </Fragment>
       <Fragment>
