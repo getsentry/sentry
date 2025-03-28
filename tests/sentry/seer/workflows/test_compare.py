948d2f31e673dd009a36609e12233b589b1ba8a3
Colton Allen <colton.allen@sentry.io>
2025-03-28 14:13:37 -0500

    Fix more types

--- tests/sentry/seer/workflows/test_compare.py
+++ tests/sentry/seer/workflows/test_compare.py
@@ -4,20 +4,20 @@ from sentry.seer.workflows.compare import kl_compare_sets, rrf_compare_sets
 
 
 def test_kl_comparse_sets():
-    baseline = {"true": 10, "false": 200}
-    outliers = {"true": 10}
+    baseline = {"true": 10.0, "false": 200.0}
+    outliers = {"true": 10.0}
     assert math.isclose(kl_compare_sets(baseline, outliers), 8.58, rel_tol=1e-3)
 
-    baseline = {"true": 100, "false": 200}
-    outliers = {"true": 10, "false": 20}
+    baseline = {"true": 100.0, "false": 200.0}
+    outliers = {"true": 10.0, "false": 20.0}
     assert 0.00000001 > kl_compare_sets(baseline, outliers)  # Essentially 0.
 
 
 def test_rrf_compare_sets():
-    baseline = {"true": 10, "false": 200}
-    outliers = {"true": 10}
+    baseline = {"true": 10.0, "false": 200.0}
+    outliers = {"true": 10.0}
     assert math.isclose(rrf_compare_sets(baseline, outliers), 0.025, rel_tol=1e-4)
 
-    baseline = {"true": 100, "false": 200}
-    outliers = {"true": 10, "false": 20}
+    baseline = {"true": 100.0, "false": 200.0}
+    outliers = {"true": 10.0, "false": 20.0}
     assert math.isclose(rrf_compare_sets(baseline, outliers), 0.026526, rel_tol=1e-4)
