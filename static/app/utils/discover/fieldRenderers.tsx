6138477245b4366f1a46404cbff06377f70e082b
Kev <6111995+k-fish@users.noreply.github.com>
2025-03-28 11:11:32 -0400

    feat(ourlogs): UI changes for log attr aliasing (#88135)
    ### Summary
    This allows both sides (object and array) so it's forward compatible and
    doesn't break when #88129 merges.
    
    
    #### Other
    - Fix a bug where `.split` is called on a custom user attribute that
    isn't splittable.

--- static/app/utils/discover/fieldRenderers.tsx
+++ static/app/utils/discover/fieldRenderers.tsx
@@ -650,7 +650,7 @@ const SPECIAL_FIELDS: SpecialFields = {
   user: {
     sortField: 'user',
     renderFunc: data => {
-      if (data.user) {
+      if (data.user?.split) {
         const [key, value] = data.user.split(':');
         const userObj = {
           id: '',
