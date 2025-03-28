1e48f54fbc7b199e938d7e6914949a948ad29de5
Arthur Knaus <arthur.knaus@sentry.io>
2025-03-28 09:31:41 +0100

    feat(profiling): New explore empty state

--- static/app/views/profiling/content.tsx
+++ static/app/views/profiling/content.tsx
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 import type {Location} from 'history';
 
 import {Alert} from 'sentry/components/core/alert';
-import {Button, LinkButton} from 'sentry/components/core/button';
 import type {SmartSearchBarProps} from 'sentry/components/deprecatedSmartSearchBar';
 import FeedbackWidgetButton from 'sentry/components/feedback/widget/feedbackWidgetButton';
 import * as Layout from 'sentry/components/layouts/thirds';
@@ -16,19 +15,13 @@ import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilt
 import {PageHeadingQuestionTooltip} from 'sentry/components/pageHeadingQuestionTooltip';
 import Pagination from 'sentry/components/pagination';
 import {TransactionSearchQueryBuilder} from 'sentry/components/performance/transactionSearchQueryBuilder';
-import {
-  ProfilingAM1OrMMXUpgrade,
-  ProfilingBetaAlertBanner,
-  ProfilingUpgradeButton,
-} from 'sentry/components/profiling/billing/alerts';
+import {ProfilingBetaAlertBanner} from 'sentry/components/profiling/billing/alerts';
 import {ProfileEventsTable} from 'sentry/components/profiling/profileEventsTable';
 import QuestionTooltip from 'sentry/components/questionTooltip';
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
-import {SidebarPanelKey} from 'sentry/components/sidebar/types';
 import {TabList, Tabs} from 'sentry/components/tabs';
 import {ALL_ACCESS_PROJECTS} from 'sentry/constants/pageFilters';
 import {t} from 'sentry/locale';
-import SidebarPanelStore from 'sentry/stores/sidebarPanelStore';
 import {space} from 'sentry/styles/space';
 import type {PageFilters} from 'sentry/types/core';
 import type {Project} from 'sentry/types/project';
@@ -41,10 +34,10 @@ import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import useProjects from 'sentry/utils/useProjects';
 import {LandingAggregateFlamegraph} from 'sentry/views/profiling/landingAggregateFlamegraph';
+import {Onboarding} from 'sentry/views/profiling/onboarding';
 import {DEFAULT_PROFILING_DATETIME_SELECTION} from 'sentry/views/profiling/utils';
 
 import {LandingWidgetSelector} from './landing/landingWidgetSelector';
-import {ProfilingOnboardingPanel} from './profilingOnboardingPanel';
 
 const LEFT_WIDGET_CURSOR = 'leftCursor';
 const RIGHT_WIDGET_CURSOR = 'rightCursor';
@@ -112,7 +105,7 @@ export default function ProfilingContent({location}: ProfilingContentProps) {
                 </PageFilterBar>
               </ActionBar>
               {showOnboardingPanel ? (
-                <ProfilingOnboardingCTA />
+                <Onboarding />
               ) : (
                 <Fragment>
                   {organization.features.includes(
@@ -275,57 +268,6 @@ function shouldShowProfilingOnboardingPanel(selection: PageFilters, projects: Pr
     projects.filter(project => project.hasProfiles).map(project => project.id)
   );
   return selection.projects.every(project => !projectsWithProfiles.has(String(project)));
-}
-
-function ProfilingOnboardingCTA() {
-  const organization = useOrganization();
-  // Open the modal on demand
-  const onSetupProfilingClick = useCallback(() => {
-    trackAnalytics('profiling_views.onboarding', {
-      organization,
-    });
-    SidebarPanelStore.activatePanel(SidebarPanelKey.PROFILING_ONBOARDING);
-  }, [organization]);
-
-  return (
-    <Fragment>
-      <ProfilingOnboardingPanel
-        content={
-          // If user is on m2, show default
-          <ProfilingAM1OrMMXUpgrade
-            organization={organization}
-            fallback={
-              <Fragment>
-                <h3>{t('Function level insights')}</h3>
-                <p>
-                  {t(
-                    'Discover slow-to-execute or resource intensive functions within your application'
-                  )}
-                </p>
-              </Fragment>
-            }
-          />
-        }
-      >
-        <ProfilingUpgradeButton
-          data-test-id="profiling-upgrade"
-          organization={organization}
-          priority="primary"
-          onClick={onSetupProfilingClick}
-          fallback={
-            <Button onClick={onSetupProfilingClick} priority="primary">
-              {t('Set Up Profiling')}
-            </Button>
-          }
-        >
-          {t('Set Up Profiling')}
-        </ProfilingUpgradeButton>
-        <LinkButton href="https://docs.sentry.io/product/profiling/" external>
-          {t('Read Docs')}
-        </LinkButton>
-      </ProfilingOnboardingPanel>
-    </Fragment>
-  );
 }
 
 function ProfilingContentPageHeader() {
---
+++ static/app/views/profiling/onboarding.tsx
@@ -0,0 +1,450 @@
+import styled from '@emotion/styled';
+
+import emptyTraceImg from 'sentry-images/spot/profiling-empty-state.svg';
+
+import {LinkButton} from 'sentry/components/core/button';
+import {GuidedSteps} from 'sentry/components/guidedSteps/guidedSteps';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {AuthTokenGeneratorProvider} from 'sentry/components/onboarding/gettingStartedDoc/authTokenGenerator';
+import {OnboardingCodeSnippet} from 'sentry/components/onboarding/gettingStartedDoc/onboardingCodeSnippet';
+import {
+  type Configuration,
+  type StepProps,
+  StepTitles,
+  TabbedCodeSnippet,
+} from 'sentry/components/onboarding/gettingStartedDoc/step';
+import {
+  DocsPageLocation,
+  type DocsParams,
+  ProductSolution,
+} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {useLoadGettingStarted} from 'sentry/components/onboarding/gettingStartedDoc/utils/useLoadGettingStarted';
+import Panel from 'sentry/components/panels/panel';
+import PanelBody from 'sentry/components/panels/panelBody';
+import {profiling as profilingPlatforms} from 'sentry/data/platformCategories';
+import platforms, {otherPlatform} from 'sentry/data/platforms';
+import {t, tct} from 'sentry/locale';
+import ConfigStore from 'sentry/stores/configStore';
+import {useLegacyStore} from 'sentry/stores/useLegacyStore';
+import pulsingIndicatorStyles from 'sentry/styles/pulsingIndicator';
+import {space} from 'sentry/styles/space';
+import type {Project} from 'sentry/types/project';
+import EventWaiter from 'sentry/utils/eventWaiter';
+import {getSelectedProjectList} from 'sentry/utils/project/useSelectedProjectsHaveField';
+import useApi from 'sentry/utils/useApi';
+import useOrganization from 'sentry/utils/useOrganization';
+import usePageFilters from 'sentry/utils/usePageFilters';
+import useProjects from 'sentry/utils/useProjects';
+
+function useOnboardingProject() {
+  const {projects} = useProjects();
+  const pageFilters = usePageFilters();
+  const selectedProject = getSelectedProjectList(
+    pageFilters.selection.projects,
+    projects
+  );
+  return selectedProject[0];
+}
+
+function WaitingIndicator({project}: {project: Project}) {
+  const api = useApi();
+  const organization = useOrganization();
+
+  return (
+    <EventWaiter
+      api={api}
+      organization={organization}
+      project={project}
+      eventType="profile"
+    >
+      {({firstIssue}) =>
+        firstIssue ? <EventReceivedIndicator /> : <EventWaitingIndicator />
+      }
+    </EventWaiter>
+  );
+}
+
+function ConfigurationRenderer({configuration}: {configuration: Configuration}) {
+  const subConfigurations = configuration.configurations ?? [];
+  return (
+    <ConfigurationWrapper>
+      {configuration.description && (
+        <DescriptionWrapper>{configuration.description}</DescriptionWrapper>
+      )}
+      {configuration.code ? (
+        Array.isArray(configuration.code) ? (
+          <TabbedCodeSnippet tabs={configuration.code} />
+        ) : (
+          <OnboardingCodeSnippet language={configuration.language}>
+            {configuration.code}
+          </OnboardingCodeSnippet>
+        )
+      ) : null}
+      {subConfigurations.map((subConfiguration, index) => (
+        <ConfigurationRenderer key={index} configuration={subConfiguration} />
+      ))}
+      {configuration.additionalInfo && (
+        <AdditionalInfo>{configuration.additionalInfo}</AdditionalInfo>
+      )}
+    </ConfigurationWrapper>
+  );
+}
+
+function StepRenderer({
+  project,
+  step,
+  isLastStep,
+}: {
+  isLastStep: boolean;
+  project: Project;
+  step: StepProps;
+}) {
+  const {type, title} = step;
+  return (
+    <GuidedSteps.Step stepKey={type || title} title={title || (type && StepTitles[type])}>
+      <ConfigurationRenderer configuration={step} />
+      {isLastStep && <WaitingIndicator project={project} />}
+      <GuidedSteps.ButtonWrapper>
+        <GuidedSteps.BackButton size="md" />
+        <GuidedSteps.NextButton size="md" />
+      </GuidedSteps.ButtonWrapper>
+    </GuidedSteps.Step>
+  );
+}
+
+function OnboardingPanel({
+  project,
+  children,
+}: {
+  children: React.ReactNode;
+  project: Project;
+}) {
+  return (
+    <Panel>
+      <PanelBody>
+        <AuthTokenGeneratorProvider projectSlug={project?.slug}>
+          <div>
+            <HeaderWrapper>
+              <HeaderText>
+                <Title>{t('Find Slow Code')}</Title>
+                <SubTitle>
+                  {t(
+                    'Use aggregated profiling data to find the slowest code paths in your app and to identify functions that have regressed in performance.'
+                  )}
+                </SubTitle>
+                <BulletList>
+                  <li>
+                    {t(
+                      'Find and optimize resource-intensive code paths that cause excessive infrastructure cost for running your backend services'
+                    )}
+                  </li>
+                  <li>
+                    {t(
+                      'Debug unresponsive interactions and janky scrolling in your mobile and browser apps'
+                    )}
+                  </li>
+                  <li>
+                    {t(
+                      'Augment traces & spans with function-level visibility into the code that is causing increased latency'
+                    )}
+                  </li>
+                </BulletList>
+              </HeaderText>
+              <Image src={emptyTraceImg} />
+            </HeaderWrapper>
+            <Divider />
+            <Body>
+              <Setup>{children}</Setup>
+              <Preview>
+                <BodyTitle>{t('Preview a Sentry Profile')}</BodyTitle>
+                <Arcade
+                  src="https://demo.arcade.software/eAXR59QVjx1gtFBdkUDw?embed"
+                  loading="lazy"
+                  allowFullScreen
+                />
+              </Preview>
+            </Body>
+          </div>
+        </AuthTokenGeneratorProvider>
+      </PanelBody>
+    </Panel>
+  );
+}
+
+export function Onboarding() {
+  const api = useApi();
+  const {isSelfHosted, urlPrefix} = useLegacyStore(ConfigStore);
+  const project = useOnboardingProject();
+  const organization = useOrganization();
+
+  const currentPlatform = project?.platform
+    ? platforms.find(p => p.id === project.platform)
+    : undefined;
+
+  const {isLoading, docs, dsn, projectKeyId} = useLoadGettingStarted({
+    platform: currentPlatform || otherPlatform,
+    orgSlug: organization.slug,
+    projSlug: project?.slug,
+  });
+
+  const doesSupportProfiling = project?.platform
+    ? profilingPlatforms.includes(project.platform)
+    : false;
+
+  const profilingDocs = docs?.profilingOnboarding || docs?.onboarding;
+
+  if (!project) {
+    return <div>{t('No project found')}</div>;
+  }
+
+  if (isLoading) {
+    return <LoadingIndicator />;
+  }
+
+  if (!doesSupportProfiling) {
+    return (
+      <OnboardingPanel project={project}>
+        {tct(
+          'Fiddlesticks. Profiling isn’t available for your [platform] project yet but we’re definitely still working on it. Stay tuned.',
+          {platform: currentPlatform?.name || project.slug}
+        )}
+        <br />
+        <LinkButton size="sm" href="https://docs.sentry.io/product/profiling/" external>
+          {t('Go to Documentation')}
+        </LinkButton>
+      </OnboardingPanel>
+    );
+  }
+
+  if (!currentPlatform || !profilingDocs || !dsn || !projectKeyId) {
+    return (
+      <OnboardingPanel project={project}>
+        {tct(
+          'Fiddlesticks. The profiling onboarding checklist isn’t available for your [project] project yet, but for now, go to Sentry docs for installation details.',
+          {project: project.slug}
+        )}
+        <br />
+        <LinkButton
+          size="sm"
+          href="https://docs.sentry.io/product/profiling/getting-started/"
+          external
+        >
+          {t('Go to Documentation')}
+        </LinkButton>
+      </OnboardingPanel>
+    );
+  }
+
+  const docParams: DocsParams<any> = {
+    api,
+    projectKeyId,
+    dsn,
+    organization,
+    platformKey: project.platform || 'other',
+    projectId: project.id,
+    projectSlug: project.slug,
+    isFeedbackSelected: false,
+    isPerformanceSelected: true,
+    isProfilingSelected: false,
+    isReplaySelected: false,
+    sourcePackageRegistries: {
+      isLoading: false,
+      data: undefined,
+    },
+    platformOptions: [ProductSolution.PERFORMANCE_MONITORING, ProductSolution.PROFILING],
+    docsLocation: DocsPageLocation.PROFILING_PAGE,
+    newOrg: false,
+    urlPrefix,
+    isSelfHosted,
+  };
+
+  const steps = [
+    ...profilingDocs.install(docParams),
+    ...profilingDocs.configure(docParams),
+  ];
+
+  return (
+    <OnboardingPanel project={project}>
+      <BodyTitle>{t('Set up the Sentry SDK')}</BodyTitle>
+      <GuidedSteps>
+        {steps.map((step, index) => (
+          <StepRenderer
+            key={index}
+            project={project}
+            step={step}
+            isLastStep={index === steps.length - 1}
+          />
+        ))}
+      </GuidedSteps>
+    </OnboardingPanel>
+  );
+}
+
+const EventWaitingIndicator = styled((p: React.HTMLAttributes<HTMLDivElement>) => (
+  <div {...p}>
+    {t("Waiting for this project's first profile")}
+    <PulsingIndicator />
+  </div>
+))`
+  display: flex;
+  align-items: center;
+  position: relative;
+  padding: ${space(1.5)} 0;
+  z-index: 10;
+  flex-grow: 1;
+  font-size: ${p => p.theme.fontSizeMedium};
+  color: ${p => p.theme.pink400};
+`;
+
+const PulsingIndicator = styled('div')`
+  ${pulsingIndicatorStyles};
+  margin-left: ${space(1)};
+`;
+
+const EventReceivedIndicator = styled((p: React.HTMLAttributes<HTMLDivElement>) => (
+  <div {...p}>
+    {'🎉 '}
+    {t(
+      "We've received this project's first profile! Refresh the page to see it in action."
+    )}
+  </div>
+))`
+  display: flex;
+  align-items: center;
+  padding: ${space(1.5)} 0;
+  flex-grow: 1;
+  font-size: ${p => p.theme.fontSizeMedium};
+  color: ${p => p.theme.successText};
+`;
+
+const SubTitle = styled('div')`
+  margin-bottom: ${space(1)};
+`;
+
+const Title = styled('div')`
+  font-size: 26px;
+  font-weight: ${p => p.theme.fontWeightBold};
+`;
+
+const BulletList = styled('ul')`
+  list-style-type: disc;
+  padding-left: 20px;
+  margin-bottom: ${space(2)};
+
+  li {
+    margin-bottom: ${space(1)};
+  }
+`;
+
+const HeaderWrapper = styled('div')`
+  display: flex;
+  justify-content: space-between;
+  gap: ${space(3)};
+  border-radius: ${p => p.theme.borderRadius};
+  padding: ${space(4)};
+`;
+
+const HeaderText = styled('div')`
+  flex: 0.65;
+
+  @media (max-width: ${p => p.theme.breakpoints.small}) {
+    flex: 1;
+  }
+`;
+
+const BodyTitle = styled('div')`
+  font-size: ${p => p.theme.fontSizeExtraLarge};
+  font-weight: ${p => p.theme.fontWeightBold};
+  margin-bottom: ${space(1)};
+`;
+
+const Setup = styled('div')`
+  padding: ${space(4)};
+
+  &:after {
+    content: '';
+    position: absolute;
+    right: 50%;
+    top: 2.5%;
+    height: 95%;
+    border-right: 1px ${p => p.theme.border} solid;
+  }
+`;
+
+const Preview = styled('div')`
+  padding: ${space(4)};
+`;
+
+const Body = styled('div')`
+  display: grid;
+  position: relative;
+  grid-auto-columns: minmax(0, 1fr);
+  grid-auto-flow: column;
+
+  h4 {
+    margin-bottom: 0;
+  }
+`;
+
+const Image = styled('img')`
+  display: block;
+  pointer-events: none;
+  height: 120px;
+  overflow: hidden;
+
+  @media (max-width: ${p => p.theme.breakpoints.small}) {
+    display: none;
+  }
+`;
+
+const Divider = styled('hr')`
+  height: 1px;
+  width: 95%;
+  background: ${p => p.theme.border};
+  border: none;
+  margin-top: 0;
+  margin-bottom: 0;
+`;
+
+const Arcade = styled('iframe')`
+  width: 750px;
+  max-width: 100%;
+  margin-top: ${space(3)};
+  height: 522px;
+  border: 0;
+`;
+
+const CONTENT_SPACING = space(1);
+
+const ConfigurationWrapper = styled('div')`
+  margin-bottom: ${CONTENT_SPACING};
+`;
+
+const DescriptionWrapper = styled('div')`
+  code {
+    color: ${p => p.theme.pink400};
+  }
+
+  :not(:last-child) {
+    margin-bottom: ${CONTENT_SPACING};
+  }
+
+  && > h4,
+  && > h5,
+  && > h6 {
+    font-size: ${p => p.theme.fontSizeExtraLarge};
+    font-weight: ${p => p.theme.fontWeightBold};
+    line-height: 34px;
+  }
+
+  && > * {
+    margin: 0;
+    &:not(:last-child) {
+      margin-bottom: ${CONTENT_SPACING};
+    }
+  }
+`;
+
+const AdditionalInfo = styled(DescriptionWrapper)`
+  margin-top: ${CONTENT_SPACING};
+`;
