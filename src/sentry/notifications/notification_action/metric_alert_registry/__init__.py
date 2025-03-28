124e4ad8916943a65e266e06e40ef2080f464170
Raj Joshi <raj.joshi@sentry.io>
2025-03-28 11:37:13 -0400

    ✨ feat(aci): invoke slack metric alert handler from noa (#88161)

--- src/sentry/notifications/notification_action/metric_alert_registry/__init__.py
+++ src/sentry/notifications/notification_action/metric_alert_registry/__init__.py
@@ -3,9 +3,11 @@ __all__ = [
     "PagerDutyMetricAlertHandler",
     "MSTeamsMetricAlertHandler",
     "DiscordMetricAlertHandler",
+    "SlackMetricAlertHandler",
 ]
 
 from .handlers.discord_metric_alert_handler import DiscordMetricAlertHandler
 from .handlers.msteams_metric_alert_handler import MSTeamsMetricAlertHandler
 from .handlers.opsgenie_metric_alert_handler import OpsgenieMetricAlertHandler
 from .handlers.pagerduty_metric_alert_handler import PagerDutyMetricAlertHandler
+from .handlers.slack_metric_alert_handler import SlackMetricAlertHandler
