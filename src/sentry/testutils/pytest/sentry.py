d8045b34441115b6e15f89e2cb54f033865c9533
anthony sottile <anthony.sottile@sentry.io>
2025-03-28 11:11:33 -0400

    ref: make per-test shard deterministic

--- src/sentry/testutils/pytest/sentry.py
+++ src/sentry/testutils/pytest/sentry.py
@@ -412,11 +412,12 @@ def pytest_collection_modifyitems(config: pytest.Config, items: list[pytest.Item
     for index, item in enumerate(items):
         # In the case where we group by round robin (e.g. TEST_GROUP_STRATEGY is not `file`),
         # we want to only include items in `accepted` list
-        item_to_group = (
-            int(md5(item.nodeid.rsplit("::", 1)[0].encode()).hexdigest(), 16)
+        to_hash = (
+            item.nodeid.rsplit("::", 1)[0].encode()
             if grouping_strategy == "scope"
-            else index
+            else item.nodeid.encode()
         )
+        item_to_group = int(md5(to_hash).hexdigest(), 16)
 
         # Split tests in different groups
         group_num = item_to_group % total_groups
