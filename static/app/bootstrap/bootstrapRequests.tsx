67fd710d06d01662180a8fc814d7d44e9d92117c
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-03-31 13:36:57 -0400

    ref(js): Remove LatestContext{Store} (#88261)

--- static/app/bootstrap/bootstrapRequests.tsx
+++ static/app/bootstrap/bootstrapRequests.tsx
@@ -1,7 +1,3 @@
-// XXX(epurkhiser): Ensure the LatestContextStore is initialized before we set
-// the active org. Otherwise we will trigger an action that does nothing
-import 'sentry/stores/latestContextStore';
-
 import {useLayoutEffect} from 'react';
 import * as Sentry from '@sentry/react';
 
