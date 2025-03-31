041e2e60e4576c6890dfdb4b549104f502f97191
nicholas-codecov <nicholas.deschenes@sentry.io>
2025-03-31 10:06:30 -0300

    feat(pipeline) Add pipeline page (#88190)
    This PR sets up the wrapping page that will be rendered on the upcoming
    `/pipeline` route.

---
+++ static/app/views/pipeline/index.tsx
@@ -0,0 +1,25 @@
+import Feature from 'sentry/components/acl/feature';
+import {NoAccess} from 'sentry/components/noAccess';
+import NoProjectMessage from 'sentry/components/noProjectMessage';
+import useOrganization from 'sentry/utils/useOrganization';
+import PipelineSecondaryNav from 'sentry/views/pipeline/navigation';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function PipelinePage({children}: Props) {
+  const organization = useOrganization();
+
+  return (
+    <Feature
+      features={['codecov-ui']}
+      organization={organization}
+      renderDisabled={NoAccess}
+    >
+      <NoProjectMessage organization={organization}>
+        <PipelineSecondaryNav>{children}</PipelineSecondaryNav>
+      </NoProjectMessage>
+    </Feature>
+  );
+}
