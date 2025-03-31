525801391099e48bf67ffbfae2bc6fdf5c0b09c7
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-03-31 10:45:07 -0400

    Replace chart with `InsightsTimeSeriesWidget`

---
+++ static/app/views/insights/browser/webVitals/components/charts/webVitalWeightList.tsx
@@ -0,0 +1,52 @@
+import {useTheme} from '@emotion/react';
+import styled from '@emotion/styled';
+
+import ExternalLink from 'sentry/components/links/externalLink';
+import {t} from 'sentry/locale';
+
+import {MODULE_DOC_LINK} from '../../settings';
+import type {WebVitals} from '../../types';
+import {Dot} from '../webVitalMeters';
+
+import {ORDER} from './performanceScoreChart';
+
+interface WebVitalsWeightListProps {
+  weights: Record<WebVitals, number>;
+}
+export function WebVitalsWeightList({weights}: WebVitalsWeightListProps) {
+  const theme = useTheme();
+  const segmentColors = theme.chart.getColorPalette(3);
+
+  return (
+    <Content>
+      <p>
+        Each Web Vital score contributes a different amount to the total score.
+        <ExternalLink href={`${MODULE_DOC_LINK}#performance-score`}>
+          {' '}
+          {t('How is this calculated?')}
+        </ExternalLink>
+      </p>
+
+      <List>
+        {ORDER.map((webVital, index) => (
+          <ListItem key={webVital}>
+            <Dot color={segmentColors[index]!} /> {webVital.toUpperCase()} contributes{' '}
+            {weights[webVital]}%
+          </ListItem>
+        ))}
+      </List>
+    </Content>
+  );
+}
+
+const Content = styled('div')`
+  font-size: ${p => p.theme.fontSizeSmall};
+`;
+
+const List = styled('ul')`
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+`;
+
+const ListItem = styled('li')``;
