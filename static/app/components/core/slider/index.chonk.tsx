0f6e0d754010c0023220123153e3ecb4383d8bd3
Nate Moore <nate.moore@sentry.io>
2025-03-31 12:09:00 -0500

    feat(ui2): chonkify sliders

--- static/app/components/core/slider/index.chonk.tsx
+++ static/app/components/core/slider/index.chonk.tsx
@@ -1,74 +1,90 @@
 import {
   type CSSProperties,
-  forwardRef,
   useCallback,
   useEffect,
+  useMemo,
   useRef,
   useState,
 } from 'react';
 import type {DO_NOT_USE_ChonkTheme} from '@emotion/react';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
+import {mergeRefs} from '@react-aria/utils';
 
 import {space} from 'sentry/styles/space';
-import mergeRefs from 'sentry/utils/mergeRefs';
 
 import type {SliderProps} from './index';
 
 const passthrough = (n: number | '') => n;
 
-export const Slider = forwardRef<HTMLInputElement, SliderProps>(
-  ({formatLabel = passthrough, ...props}, ref) => {
-    const inputRef = useRef<HTMLInputElement>();
-    const refs = mergeRefs([ref, inputRef]);
-    const [label, setLabel] = useState((props.value ?? props.defaultValue) || 50);
-    const initialProgress = getProgress(
-      (props.value ?? props.defaultValue) || 50,
-      props.min ?? 0,
-      props.max ?? 100
-    );
+export function Slider({
+  formatLabel = passthrough,
+  ref,
+  ...props
+}: SliderProps & {ref: React.Ref<HTMLInputElement>}) {
+  const inputRef = useRef<HTMLInputElement>(null);
+  const refs = mergeRefs(ref, inputRef);
+  const step =
+    (typeof props.step === 'string' ? Number.parseInt(props.step, 10) : props.step) ?? -1;
+  const [label, setLabel] = useState((props.value ?? props.defaultValue) || 50);
+  const initialProgress = getProgress(
+    (props.value ?? props.defaultValue) || 50,
+    props.min ?? 0,
+    props.max ?? 100
+  );
+  const filledSteps = useMemo(() => (step === -1 ? -1 : label / step), [step, label]);
 
-    const handleChange = useCallback(
-      (event: Event) => {
-        const input = event.target as HTMLInputElement;
-        const {valueAsNumber, min, max} = input;
-        setLabel(valueAsNumber);
-        const progress = getProgress(valueAsNumber, min || 0, max || 100);
-        input.parentElement?.style.setProperty('--p', `${progress.toFixed(0)}%`);
-      },
-      [setLabel]
-    );
+  const handleChange = useCallback(
+    (event: Event) => {
+      const input = event.target as HTMLInputElement;
+      const {valueAsNumber, min, max} = input;
+      setLabel(valueAsNumber);
+      const progress = getProgress(valueAsNumber, min || 0, max || 100);
+      input.parentElement?.style.setProperty('--p', `${progress.toFixed(0)}%`);
+    },
+    [setLabel]
+  );
 
-    useEffect(() => {
-      const input: HTMLInputElement | undefined = inputRef.current;
-      if (!input) {
-        return;
+  useEffect(() => {
+    const input: HTMLInputElement | null = inputRef.current;
+    if (!input) {
+      return;
+    }
+    input.addEventListener('input', handleChange);
+    // eslint-disable-next-line consistent-return
+    return () => input.removeEventListener('input', handleChange);
+  });
+
+  return (
+    <SliderContainer
+      style={
+        {'--p': `${initialProgress.toFixed(0)}%`, '--steps': `${label}`} as CSSProperties
       }
-      input.addEventListener('input', handleChange);
-      // eslint-disable-next-line consistent-return
-      return () => input.removeEventListener('input', handleChange);
-    });
+    >
+      {props.step ? (
+        <SliderTicks
+          filledSteps={filledSteps}
+          n={
+            typeof props.step === 'string' ? Number.parseInt(props.step, 10) : props.step
+          }
+        />
+      ) : null}
+      <StyledSlider ref={refs} type="range" {...props} />
+      {props.disabled ? null : (
+        <SliderOutput htmlFor={props.id}>
+          <SliderLabel>{formatLabel(label)}</SliderLabel>
+        </SliderOutput>
+      )}
+    </SliderContainer>
+  );
+}
 
-    return (
-      <SliderContainer style={{'--p': `${initialProgress.toFixed(0)}%`} as CSSProperties}>
-        {props.disabled ? null : (
-          <SliderOutput htmlFor={props.id}>
-            <SliderLabel>{formatLabel(label)}</SliderLabel>
-          </SliderOutput>
-        )}
-        {props.step ? <SliderTicks n={props.step} /> : null}
-        <StyledSlider ref={refs} type="range" {...props} />
-      </SliderContainer>
-    );
-  }
-);
-
-function SliderTicks({n}: {n: number}) {
+function SliderTicks({n, filledSteps}: {filledSteps: number; n: number}) {
   return (
     <StepsContainer role="presentation">
-      {Array.from({length: n}, (_, i) => (
-        <StepMark key={i} />
-      ))}
+      {Array.from({length: n}, (_, i) => {
+        return <StepMark key={i} filled={i <= filledSteps} />;
+      })}
     </StepsContainer>
   );
 }
@@ -81,13 +97,12 @@ const StepsContainer = styled('div')`
   right: 0;
   display: flex;
   flex-direction: row;
-  height: 12px;
-  width: calc(100% - 24px);
+  height: 14px;
+  width: calc(100% - 16px);
   margin-inline: auto;
-  z-index: 1;
 `;
 
-const StepMark = styled('span')<{theme?: DO_NOT_USE_ChonkTheme}>`
+const StepMark = styled('span')<{filled?: boolean; theme?: DO_NOT_USE_ChonkTheme}>`
   box-sizing: border-box;
   position: relative;
   flex-grow: 1;
@@ -98,9 +113,9 @@ const StepMark = styled('span')<{theme?: DO_NOT_USE_ChonkTheme}>`
     position: absolute;
     height: 12px;
     width: 2px;
-    background: linear-gradient(90deg, white, black);
-    opacity: 0.5;
-    mix-blend-mode: multiply;
+    border-radius: ${p => p.theme.radius.xl};
+    background: ${p =>
+      p.filled ? p.theme.colors.chonk.blue300 : p.theme.colors.surface100};
   }
 `;
 
@@ -118,18 +133,19 @@ const StyledSlider = styled('input')`
   ${p => chonkSliderStyles(p as any)}
 `;
 export function chonkSliderStyles(p: {theme: DO_NOT_USE_ChonkTheme}) {
+  const tokens = {
+    trackColor: p.theme.colors.surface100,
+    trackActiveColor: p.theme.colors.chonk.blue300,
+  };
   return css`
-    --track-color: ${p.theme.colors.surface300};
-    --track-color-dark: ${p.theme.colors.surface100};
-    --track-active-color: ${p.theme.colors.chonk.blue400};
-    --track-active-color-dark: ${p.theme.colors.chonk.blue100};
     /* stylelint-disable-next-line property-no-vendor-prefix */
     -webkit-appearance: none;
     appearance: none;
     position: relative;
     width: 100%;
-    height: 12px;
-    border-radius: ${p.theme.radius.lg};
+    height: 16px;
+    background: transparent;
+    border-radius: ${p.theme.radius.nano};
     transition: box-shadow 0.1s;
     box-shadow:
       0 0 0 8px transparent,
@@ -152,61 +168,61 @@ export function chonkSliderStyles(p: {theme: DO_NOT_USE_ChonkTheme}) {
 
       &::-webkit-slider-thumb {
         cursor: not-allowed;
+        border-bottom-width: 1px;
       }
     }
 
     &::before {
       content: '';
       position: absolute;
+      inset: 0;
+      margin: auto 0;
       min-width: calc(${p.theme.radius.micro} * 6);
       width: var(--p, 50%);
-      height: 12px;
-      background: var(--track-active-color);
-      box-shadow: 0px 3px 0px 0px var(--track-active-color-dark) inset;
-      border: 1px solid var(--track-active-color-dark);
+      height: 4px;
+      background: ${tokens.trackActiveColor};
+      border: 1px solid ${tokens.trackActiveColor};
       border-radius: ${p.theme.radius.micro};
     }
 
     /* Chrome styling */
     &::-webkit-slider-runnable-track {
       width: 100%;
-      height: 12px;
-      background: var(--track-color);
-      box-shadow: 0px 3px 0px 0px var(--track-color-dark) inset;
-      border: 1px solid var(--track-color-dark);
+      height: 4px;
+      background: ${tokens.trackColor};
+      border: 1px solid ${tokens.trackColor};
       border-radius: ${p.theme.radius.micro};
     }
 
     &::-webkit-slider-thumb {
       appearance: none;
-      width: 24px;
-      height: 24px;
-      background: ${p.theme.colors.surface300};
+      width: 16px;
+      height: 16px;
+      background: ${p.theme.colors.white};
       border: 1px solid ${p.theme.colors.chonk.blue100};
       border-bottom: 2px solid ${p.theme.colors.chonk.blue100};
-      border-radius: ${p.theme.radius.lg};
+      border-radius: ${p.theme.radius.sm};
       transform: translateY(-7px);
-      z-index: 1;
+      z-index: 10;
     }
 
     /* Firefox styling */
     &::-moz-range-track {
       width: 100%;
-      height: 12px;
-      background: var(--track-color);
-      box-shadow: 0px 3px 0px 0px var(--track-color-dark) inset;
-      border: 1px solid var(--track-color-dark);
+      height: 4px;
+      background: ${tokens.trackColor};
+      border: 1px solid ${tokens.trackColor};
       border-radius: ${p.theme.radius.micro};
     }
 
     &::-moz-range-thumb {
       appearance: none;
-      width: 24px;
-      height: 24px;
-      background: ${p.theme.colors.surface300};
+      width: 16px;
+      height: 16px;
+      background: ${p.theme.colors.white};
       border: 1px solid ${p.theme.colors.chonk.blue100};
       border-bottom: 2px solid ${p.theme.colors.chonk.blue100};
-      border-radius: ${p.theme.radius.lg};
+      border-radius: ${p.theme.radius.sm};
       transform: translateY(-7px);
       z-index: 1;
     }
@@ -232,18 +248,18 @@ export function chonkOutput(p: {theme: DO_NOT_USE_ChonkTheme}) {
     font-size: ${p.theme.fontSizeSmall};
     position: absolute;
     height: 24px;
-    width: calc(100% - 24px);
+    width: calc(100% - 16px);
     margin-inline: auto;
     left: 0;
     right: 0;
-    bottom: 24px;
+    bottom: 20px;
     text-align: center;
 
     opacity: var(--o);
     transform: translate(var(--tx), var(--ty));
     transition:
       100ms opacity cubic-bezier(0.23, 1, 0.32, 1),
-      100ms transform cubic-bezier(0.39, 0.575, 0.565, 1);
+      50ms transform cubic-bezier(0.39, 0.575, 0.565, 1);
   `;
 }
 const SliderLabel = styled('span')<{theme?: DO_NOT_USE_ChonkTheme}>`
@@ -253,9 +269,11 @@ const SliderLabel = styled('span')<{theme?: DO_NOT_USE_ChonkTheme}>`
   padding-inline: ${space(0.5)};
   width: min-content;
   text-align: center;
-  background: ${p => p.theme.colors.black};
+  background: ${p => p.theme.colors.chonk.blue300};
+  border: 1px solid ${p => p.theme.colors.chonk.blue100};
   color: ${p => p.theme.white};
   border-radius: ${p => p.theme.radius.micro};
+  z-index: ${p => p.theme.zIndex.tooltip};
 `;
 
 const SliderContainer = styled('div')`
