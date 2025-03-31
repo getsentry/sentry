71ffade08a0eea4f336fbf1a821438e07eabe900
Fabian Schindler <fabian.schindler@sentry.io>
2025-03-31 15:37:23 +0200

    ref(getting-started): fix code block style definition

--- static/app/gettingStartedDocs/python/celery.tsx
+++ static/app/gettingStartedDocs/python/celery.tsx
@@ -211,9 +211,8 @@ const docs: Docs = {
 
 export default docs;
 
-const CONTENT_SPACING = space(2);
 const SpacedOnboardingCodeSnippet = styled(OnboardingCodeSnippet)`
   pre {
-    margin-bottom: ${CONTENT_SPACING};
+    margin-bottom: ${space(2)};
   }
 `;
