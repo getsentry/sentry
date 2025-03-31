2e717b4c2f8f36f5c76bbfc570321af843bde581
JonasBa <jonas@badalic.com>
2025-03-31 10:43:01 -0400

    theme: add back theme import

--- static/app/utils/theme/index.tsx
+++ static/app/utils/theme/index.tsx
@@ -13,3 +13,5 @@ export type {
 import {darkTheme, lightTheme} from './theme';
 
 export {lightTheme, darkTheme};
+// @deprecated use useTheme hook instead of directly importing the theme. If you require a theme for your tests, use ThemeFixture.
+export default lightTheme;
