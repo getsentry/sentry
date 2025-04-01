c5cd70d8d75a0e6ff9aae22d7b6e863c400c384b
Tony Xiao <txiao@sentry.io>
2025-04-01 16:59:51 -0400

    chore(profiling): Make profiling-sdks a flagpole flag

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -290,7 +290,7 @@ def register_temporary_features(manager: FeatureManager):
     # Enabled for those orgs who participated in the profiling Beta program
     manager.add("organizations:profiling-beta", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=True)
     # Enables monitoring for latest profiling sdk used
-    manager.add("organizations:profiling-sdks", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=True)
+    manager.add("organizations:profiling-sdks", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enables production profiling in sentry browser application
     manager.add("organizations:profiling-browser", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enables separate differential flamegraph page
