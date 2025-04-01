8611069ced502b89326f6b5606a8bb74c985b103
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-04-01 12:18:58 -0400

    feat(dashboards): Add support for "score" value type (#88329)
    A new value type! A "score" is a special kind of integer. It only ranges
    from 0 to 100, and it has no units. Soon, the backend will return this
    type for functions like `performance_score`, but for now just frontend
    support is helpful. The "score" type:
    
    - always rounded to nearest integer in the tooltip formatter
    - always 0-100 on the Y axis
    
    Contributes to [DAIN-111: Replace Web Vitals "Score Breakdown" chart
    with
    `TimeSeriesWidgetVisualization`](https://linear.app/getsentry/issue/DAIN-111/replace-web-vitals-score-breakdown-chart-with)

---
+++ static/app/views/dashboards/widgets/timeSeriesWidget/fixtures/sampleScoreTimeSeries.ts
@@ -0,0 +1,207 @@
+import {TimeSeries} from '../../common/types';
+
+export const sampleScoreTimeSeries: TimeSeries = {
+  field: 'performance_score(measurements.score.lcp)',
+  meta: {
+    type: 'score',
+    unit: null,
+  },
+  data: [
+    {
+      value: 16,
+      timestamp: '2024-10-24T15:00:00-04:00',
+    },
+    {
+      value: 16,
+      timestamp: '2024-10-24T15:30:00-04:00',
+    },
+    {
+      value: 16,
+      timestamp: '2024-10-24T16:00:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-24T16:30:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-24T17:00:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-24T17:30:00-04:00',
+    },
+    {
+      value: 15,
+      timestamp: '2024-10-24T18:00:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-24T18:30:00-04:00',
+    },
+    {
+      value: 16,
+      timestamp: '2024-10-24T19:00:00-04:00',
+    },
+    {
+      value: 16,
+      timestamp: '2024-10-24T19:30:00-04:00',
+    },
+    {
+      value: 15,
+      timestamp: '2024-10-24T20:00:00-04:00',
+    },
+    {
+      value: 16,
+      timestamp: '2024-10-24T20:30:00-04:00',
+    },
+    {
+      value: 15,
+      timestamp: '2024-10-24T21:00:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-24T21:30:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-24T22:00:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-24T22:30:00-04:00',
+    },
+    {
+      value: 16,
+      timestamp: '2024-10-24T23:00:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-24T23:30:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T00:00:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T00:30:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-25T01:00:00-04:00',
+    },
+    {
+      value: 20,
+      timestamp: '2024-10-25T01:30:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T02:00:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T02:30:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T03:00:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T03:30:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T04:00:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T04:30:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-25T05:00:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T05:30:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T06:00:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T06:30:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T07:00:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T07:30:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T08:00:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T08:30:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T09:00:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T09:30:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T10:00:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T10:30:00-04:00',
+    },
+    {
+      value: 19,
+      timestamp: '2024-10-25T11:00:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-25T11:30:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-25T12:00:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T12:30:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-25T13:00:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-25T13:30:00-04:00',
+    },
+    {
+      value: 16,
+      timestamp: '2024-10-25T14:00:00-04:00',
+    },
+    {
+      value: 17,
+      timestamp: '2024-10-25T14:30:00-04:00',
+    },
+    {
+      value: 18,
+      timestamp: '2024-10-25T15:00:00-04:00',
+    },
+  ],
+};
