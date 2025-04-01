87bdd624059dced75e6536b312c203156ab0053b
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-04-01 10:02:24 +0200

    fix(demo-mode): amplitude event

--- static/app/components/demo/demoHeader.tsx
+++ static/app/components/demo/demoHeader.tsx
@@ -62,14 +62,14 @@ export default function DemoHeader() {
             extraQueryParameterWithEmail()
           );
 
-          // Using window.open instead of href={} because we need to read `email`
-          // from localStorage when the user clicks the button.
-          window.open(url, '_blank');
-
           trackAnalytics('growth.demo_click_get_started', {
             cta: undefined,
             organization: null,
           });
+
+          // Using window.open instead of href={} because we need to read `email`
+          // from localStorage when the user clicks the button.
+          window.open(url, '_blank');
         }}
       >
         <FreeTrialTextLong>{t('Start Free Trial')}</FreeTrialTextLong>
