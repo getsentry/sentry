fb663562bd2b577135565929277040a2763afb1d
Rohan Agarwal <rohan.agarwal@sentry.io>
2025-04-01 11:59:55 -0700

    Remove tags drawer

--- static/app/views/issueDetails/groupTags/useGroupTagsDrawer.tsx
+++ static/app/views/issueDetails/groupTags/useGroupTagsDrawer.tsx
@@ -28,7 +28,6 @@ export function useGroupTagsDrawer({
       ),
       {
         ariaLabel: t('Tags Drawer'),
-        drawerKey: 'tags-drawer',
         onClose: () => {
           navigate(
             {
