dd76fcf97b0b682107dda9fe05211bd3f4c3598a
Raj Joshi <raj.joshi@sentry.io>
2025-04-01 14:38:53 -0400

    :wrench: chore: finish up sentry app config and cleanup (#88457)
    i realized we were using the generic schema in a couple places, so
    removed it and made it specific to the actions that were still using it.
    
    i also moved email action stuff from common to its own file

--- src/sentry/workflow_engine/handlers/action/notification/sentry_app_handler.py
+++ src/sentry/workflow_engine/handlers/action/notification/sentry_app_handler.py
@@ -1,16 +1,42 @@
+from sentry.notifications.models.notificationaction import ActionTarget
 from sentry.notifications.notification_action.utils import execute_via_group_type_registry
-from sentry.workflow_engine.handlers.action.notification.common import GENERIC_ACTION_CONFIG_SCHEMA
 from sentry.workflow_engine.models import Action, Detector
 from sentry.workflow_engine.registry import action_handler_registry
 from sentry.workflow_engine.types import ActionHandler, WorkflowEventData
+from sentry.workflow_engine.typings.notification_action import SentryAppIdentifier
 
 
 @action_handler_registry.register(Action.Type.SENTRY_APP)
 class SentryAppActionHandler(ActionHandler):
     group = ActionHandler.Group.OTHER
 
-    config_schema = GENERIC_ACTION_CONFIG_SCHEMA
-    data_schema = {}
+    config_schema = {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "description": "The configuration schema for a Sentry App Action",
+        "type": "object",
+        "properties": {
+            "target_identifier": {"type": ["string"]},
+            "target_display": {"type": ["string", "null"]},
+            "target_type": {
+                "type": ["integer"],
+                "enum": [ActionTarget.SENTRY_APP.value],
+            },
+            "sentry_app_identifier": {
+                "type": ["string"],
+                "enum": [*SentryAppIdentifier],
+            },
+        },
+        "required": ["target_type", "target_identifier", "sentry_app_identifier"],
+        "additionalProperties": False,
+    }
+    data_schema = {
+        "$schema": "https://json-schema.org/draft/2020-12/schema",
+        "type": "object",
+        "properties": {
+            "settings": {"type": ["array", "object"]},
+        },
+        "additionalProperties": False,
+    }
 
     @staticmethod
     def execute(
