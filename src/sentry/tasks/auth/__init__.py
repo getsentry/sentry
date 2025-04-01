08a058eb1a6690e222c462e73f9dbd9417071275
Enoch Tang <enoch.tang@sentry.io>
2025-04-01 10:15:25 -0400

    clean up

--- src/sentry/tasks/auth/__init__.py
+++ src/sentry/tasks/auth/__init__.py
@@ -2,22 +2,3 @@ from sentry.taskworker.registry import TaskNamespace, taskregistry
 
 auth_tasks: TaskNamespace = taskregistry.create_namespace("auth")
 auth_control_tasks: TaskNamespace = taskregistry.create_namespace("auth.control")
-
-
-__all__ = (
-    "email_missing_links",
-    "email_missing_links_control",
-    "email_unlink_notifications",
-    "remove_2fa_non_compliant_members",
-    "check_auth",
-    "check_auth_identities",
-    "check_auth_identity",
-)
-
-from .auth import (
-    email_missing_links,
-    email_missing_links_control,
-    email_unlink_notifications,
-    remove_2fa_non_compliant_members,
-)
-from .check_auth import check_auth, check_auth_identities, check_auth_identity
