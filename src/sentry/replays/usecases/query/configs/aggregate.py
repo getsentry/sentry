6d33b535490122324b36a5fab8b76a0f7ef8caac
Colton Allen <colton.allen@sentry.io>
2025-04-01 11:29:52 -0500

    Add screen alias to query configuration

--- src/sentry/replays/usecases/query/configs/aggregate.py
+++ src/sentry/replays/usecases/query/configs/aggregate.py
@@ -158,7 +158,8 @@ search_config["trace_id"] = search_config["trace_ids"]
 search_config["trace"] = search_config["trace_ids"]
 search_config["url"] = search_config["urls"]
 search_config["user.ip"] = search_config["user.ip_address"]
-
+search_config["screens"] = search_config["urls"]
+search_config["count_screens"] = search_config["count_urls"]
 
 # Field-names which could not be found in the set are tag-keys and will, by default, look for
 # the `*` key to find their search instructions. If this is not defined an error is returned.
