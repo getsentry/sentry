879a37084b14dcb976b6b2e0b03d1936b5adf845
Cathy Teng <cathy.teng@sentry.io>
2025-04-02 08:37:59 -0700

    update migration

--- src/sentry/workflow_engine/models/workflow_fire_history.py
+++ src/sentry/workflow_engine/models/workflow_fire_history.py
@@ -1,5 +1,3 @@
-from django.db.models import Index
-
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import CharField, FlexibleForeignKey, region_silo_model, sane_repr
 from sentry.db.models.base import DefaultFieldsModel
@@ -17,6 +15,5 @@ class WorkflowFireHistory(DefaultFieldsModel):
     class Meta:
         db_table = "workflow_engine_workflowfirehistory"
         app_label = "workflow_engine"
-        indexes = [Index(fields=["workflow", "date_added"])]
 
     __repr__ = sane_repr("workflow_id", "group_id", "event_id", "date_added")
