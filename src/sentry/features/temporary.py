cd1fccc8b94faddb93fb2bbe203cfed78206b46c
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-04-02 10:37:37 -0700

    chore(flags): unregister autcomplete flag (#88344)
    it's been rolled out to everyone for a while, so let's clean up this
    flag
    
    - options removal PR:
    https://github.com/getsentry/sentry-options-automator/pull/3427
    - frontend removal PR: https://github.com/getsentry/sentry/pull/88354

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -345,8 +345,6 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:feature-flag-audit-log", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable feature flag CTA on issue details page
     manager.add("organizations:feature-flag-cta", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable feature flag search autocomplete
-    manager.add("organizations:feature-flag-autocomplete", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable feature flag distribution flyout in issue details
     manager.add("organizations:feature-flag-distribution-flyout", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable new release insights charts
