d5d41faeedddd7cc035a8c3c5e8563b3225750e0
Nar Saynorath <nar.saynorath@sentry.io>
2025-04-02 15:53:55 -0400

    feat(dashboards-eap): Add progressive loading to dashboard timeseries widgets (#88570)
    Support two requests inside spanWidgetQueries. Two genericWidgetQueries
    components are composed such that one is conditionally rendered after
    the first one completes its preflight request. We bias towards the best
    effort data if it's loaded.
    
    I've had to update the dataset configs to accept a sampling mode, which
    then gets passed in the requests's query params. The progressive loading
    indicator is currently unhandled because it causes an error in the console
    that fails our tests. I'll follow up in another PR.
    
    There is currently a bug I need to address where updates to the widget
    (i.e. dashboard filters or widget builder updates) will trigger both
    requests at the same time. This is a state bug and I will fix it, but
    let's review this for now since it's feature flagged and will help us
    evaluate sampled storage for dashboards.

--- static/app/views/dashboards/widgetCard/spansWidgetQueries.tsx
+++ static/app/views/dashboards/widgetCard/spansWidgetQueries.tsx
@@ -1,4 +1,4 @@
-import {useState} from 'react';
+import {Fragment, useCallback, useState} from 'react';
 
 import type {Client} from 'sentry/api';
 import type {PageFilters} from 'sentry/types/core';
@@ -7,22 +7,23 @@ import type {
   EventsStats,
   GroupedMultiSeriesEventsStats,
   MultiSeriesEventsStats,
-  Organization,
 } from 'sentry/types/organization';
 import {defined} from 'sentry/utils';
 import {dedupeArray} from 'sentry/utils/dedupeArray';
 import type {EventsTableData, TableData} from 'sentry/utils/discover/discoverQuery';
 import getDynamicText from 'sentry/utils/getDynamicText';
+import useOrganization from 'sentry/utils/useOrganization';
 import {determineSeriesConfidence} from 'sentry/views/alerts/rules/metric/utils/determineSeriesConfidence';
 import {determineSeriesSampleCountAndIsSampled} from 'sentry/views/alerts/rules/metric/utils/determineSeriesSampleCount';
 import {SpansConfig} from 'sentry/views/dashboards/datasetConfig/spans';
+import {SAMPLING_MODE} from 'sentry/views/explore/hooks/useProgressiveQuery';
 import {combineConfidenceForSeries} from 'sentry/views/explore/utils';
 import {
   convertEventsStatsToTimeSeriesData,
   transformToSeriesMap,
 } from 'sentry/views/insights/common/queries/useSortedTimeSeries';
 
-import type {DashboardFilters, Widget} from '../types';
+import {type DashboardFilters, DisplayType, type Widget} from '../types';
 import {isEventsStats} from '../utils/isEventsStats';
 
 import type {
@@ -34,10 +35,9 @@ import GenericWidgetQueries from './genericWidgetQueries';
 type SeriesResult = EventsStats | MultiSeriesEventsStats | GroupedMultiSeriesEventsStats;
 type TableResult = TableData | EventsTableData;
 
-type Props = {
+type SpansWidgetQueriesProps = {
   api: Client;
   children: (props: GenericWidgetQueriesChildrenProps) => React.JSX.Element;
-  organization: Organization;
   selection: PageFilters;
   widget: Widget;
   cursor?: string;
@@ -46,56 +46,193 @@ type Props = {
   onDataFetched?: (results: OnDataFetchedProps) => void;
 };
 
-function SpansWidgetQueries({
+type SpansWidgetQueriesImplProps = SpansWidgetQueriesProps & {
+  getConfidenceInformation: (result: SeriesResult) => {
+    seriesConfidence: Confidence | null;
+    seriesIsSampled: boolean | null;
+    seriesSampleCount: number | undefined;
+  };
+};
+
+function SpansWidgetQueries(props: SpansWidgetQueriesProps) {
+  const organization = useOrganization();
+
+  const getConfidenceInformation = useCallback(
+    (result: SeriesResult) => {
+      let seriesConfidence;
+      let seriesSampleCount;
+      let seriesIsSampled;
+
+      if (isEventsStats(result)) {
+        seriesConfidence = determineSeriesConfidence(result);
+        const {sampleCount: calculatedSampleCount, isSampled: calculatedIsSampled} =
+          determineSeriesSampleCountAndIsSampled(
+            [
+              convertEventsStatsToTimeSeriesData(
+                props.widget.queries[0]?.aggregates[0] ?? '',
+                result
+              )[1],
+            ],
+            false
+          );
+        seriesSampleCount = calculatedSampleCount;
+        seriesIsSampled = calculatedIsSampled;
+      } else {
+        const dedupedYAxes = dedupeArray(props.widget.queries[0]?.aggregates ?? []);
+        const seriesMap = transformToSeriesMap(result, dedupedYAxes);
+        const series = dedupedYAxes.flatMap(yAxis => seriesMap[yAxis]).filter(defined);
+        const {sampleCount: calculatedSampleCount, isSampled: calculatedIsSampled} =
+          determineSeriesSampleCountAndIsSampled(
+            series,
+            Object.keys(result).filter(seriesName => seriesName.toLowerCase() !== 'other')
+              .length > 0
+          );
+        seriesSampleCount = calculatedSampleCount;
+        seriesConfidence = combineConfidenceForSeries(series);
+        seriesIsSampled = calculatedIsSampled;
+      }
+      return {
+        seriesConfidence,
+        seriesSampleCount,
+        seriesIsSampled,
+      };
+    },
+    [props.widget.queries]
+  );
+
+  // TODO: Remove the check for the display type when we support progressive loading
+  // for the table request as well.
+  if (
+    organization.features.includes('visibility-explore-progressive-loading') &&
+    ![DisplayType.TABLE, DisplayType.BIG_NUMBER].includes(props.widget.displayType)
+  ) {
+    return (
+      <SpansWidgetQueriesProgressiveLoadingImpl
+        {...props}
+        getConfidenceInformation={getConfidenceInformation}
+      />
+    );
+  }
+
+  return (
+    <SpansWidgetQueriesSingleRequestImpl
+      {...props}
+      getConfidenceInformation={getConfidenceInformation}
+    />
+  );
+}
+
+function SpansWidgetQueriesProgressiveLoadingImpl({
   children,
   api,
-  organization,
   selection,
   widget,
   cursor,
   limit,
   dashboardFilters,
   onDataFetched,
-}: Props) {
+  getConfidenceInformation,
+}: SpansWidgetQueriesImplProps) {
+  const config = SpansConfig;
+  const organization = useOrganization();
+
+  const afterFetchSeriesData = (result: SeriesResult) => {
+    const {seriesConfidence, seriesSampleCount, seriesIsSampled} =
+      getConfidenceInformation(result);
+
+    onDataFetched?.({
+      confidence: seriesConfidence,
+      sampleCount: seriesSampleCount,
+      isSampled: seriesIsSampled,
+    });
+  };
+
+  return getDynamicText({
+    value: (
+      <GenericWidgetQueries<SeriesResult, TableResult>
+        config={config}
+        api={api}
+        organization={organization}
+        selection={selection}
+        widget={widget}
+        cursor={cursor}
+        limit={limit}
+        dashboardFilters={dashboardFilters}
+        afterFetchSeriesData={afterFetchSeriesData}
+        samplingMode={SAMPLING_MODE.PREFLIGHT}
+      >
+        {lowFidelityProps => (
+          <Fragment>
+            {/** TODO(nar): There is currently a bug where subsequent rerenders (i.e. changes in the widget
+             * params or dashboard filters) will cause this to refetch both the preflight and best effort data
+             * at the same time
+            ) */}
+            {lowFidelityProps.loading ? (
+              children({
+                ...lowFidelityProps,
+                isProgressivelyLoading: lowFidelityProps.loading,
+              })
+            ) : (
+              <GenericWidgetQueries<SeriesResult, TableResult>
+                config={config}
+                api={api}
+                organization={organization}
+                selection={selection}
+                widget={widget}
+                cursor={cursor}
+                limit={limit}
+                dashboardFilters={dashboardFilters}
+                afterFetchSeriesData={afterFetchSeriesData}
+                samplingMode={SAMPLING_MODE.BEST_EFFORT}
+              >
+                {highFidelityProps =>
+                  children({
+                    ...highFidelityProps,
+                    ...(highFidelityProps.loading
+                      ? {
+                          ...lowFidelityProps,
+                          loading: true,
+                        }
+                      : {
+                          ...highFidelityProps,
+                        }),
+                    isProgressivelyLoading:
+                      highFidelityProps.loading &&
+                      !lowFidelityProps.errorMessage &&
+                      !highFidelityProps.errorMessage,
+                  })
+                }
+              </GenericWidgetQueries>
+            )}
+          </Fragment>
+        )}
+      </GenericWidgetQueries>
+    ),
+    fixed: <div />,
+  });
+}
+
+function SpansWidgetQueriesSingleRequestImpl({
+  children,
+  api,
+  selection,
+  widget,
+  cursor,
+  limit,
+  dashboardFilters,
+  onDataFetched,
+  getConfidenceInformation,
+}: SpansWidgetQueriesImplProps) {
   const config = SpansConfig;
 
+  const organization = useOrganization();
   const [confidence, setConfidence] = useState<Confidence | null>(null);
   const [sampleCount, setSampleCount] = useState<number | undefined>(undefined);
   const [isSampled, setIsSampled] = useState<boolean | null>(null);
 
   const afterFetchSeriesData = (result: SeriesResult) => {
-    let seriesConfidence;
-    let seriesSampleCount;
-    let seriesIsSampled;
-
-    if (isEventsStats(result)) {
-      seriesConfidence = determineSeriesConfidence(result);
-      const {sampleCount: calculatedSampleCount, isSampled: calculatedIsSampled} =
-        determineSeriesSampleCountAndIsSampled(
-          [
-            convertEventsStatsToTimeSeriesData(
-              widget.queries[0]?.aggregates[0] ?? '',
-              result
-            )[1],
-          ],
-          false
-        );
-      seriesSampleCount = calculatedSampleCount;
-      seriesIsSampled = calculatedIsSampled;
-    } else {
-      const dedupedYAxes = dedupeArray(widget.queries[0]?.aggregates ?? []);
-      const seriesMap = transformToSeriesMap(result, dedupedYAxes);
-      const series = dedupedYAxes.flatMap(yAxis => seriesMap[yAxis]).filter(defined);
-      const {sampleCount: calculatedSampleCount, isSampled: calculatedIsSampled} =
-        determineSeriesSampleCountAndIsSampled(
-          series,
-          Object.keys(result).filter(seriesName => seriesName.toLowerCase() !== 'other')
-            .length > 0
-        );
-      seriesSampleCount = calculatedSampleCount;
-      seriesConfidence = combineConfidenceForSeries(series);
-      seriesIsSampled = calculatedIsSampled;
-    }
+    const {seriesConfidence, seriesSampleCount, seriesIsSampled} =
+      getConfidenceInformation(result);
 
     setConfidence(seriesConfidence);
     setSampleCount(seriesSampleCount);
