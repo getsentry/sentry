aced6036833a333daafca491e16866617d456e02
Colleen O'Rourke <colleen@sentry.io>
2025-04-02 13:29:34 -0700

    feat(ACI): Break lookup table foreign keys (#88407)
    Change the lookup tables' foreign keys to store IDs and rename so it
    reflects what it holds now.
    
    See
    https://www.notion.so/sentry/Old-Endpoints-New-Models-1bd8b10e4b5d803d80a9d9d05b6b88b4?pvs=4#1c28b10e4b5d80c5a254dbb442f6859d
    for a lot more context on this change, but the tl;dr is we don't want to
    have to rely on old models that we will be deleting for IDs we only need
    for a short period of time.

--- src/sentry/workflow_engine/models/alertrule_workflow.py
+++ src/sentry/workflow_engine/models/alertrule_workflow.py
@@ -1,7 +1,12 @@
 from django.db.models import CheckConstraint, Q
 
 from sentry.backup.scopes import RelocationScope
-from sentry.db.models import DefaultFieldsModel, FlexibleForeignKey, region_silo_model
+from sentry.db.models import (
+    BoundedBigIntegerField,
+    DefaultFieldsModel,
+    FlexibleForeignKey,
+    region_silo_model,
+)
 
 
 @region_silo_model
@@ -12,21 +17,21 @@ class AlertRuleWorkflow(DefaultFieldsModel):
 
     __relocation_scope__ = RelocationScope.Organization
 
-    alert_rule = FlexibleForeignKey("sentry.AlertRule", null=True)
-    rule = FlexibleForeignKey("sentry.Rule", null=True)
+    alert_rule_id = BoundedBigIntegerField(null=True)
+    rule_id = BoundedBigIntegerField(null=True)
     workflow = FlexibleForeignKey("workflow_engine.Workflow")
 
     class Meta:
         db_table = "workflow_engine_alertruleworkflow"
         app_label = "workflow_engine"
         unique_together = (
-            ("workflow", "rule"),
-            ("workflow", "alert_rule"),
+            ("workflow", "rule_id"),
+            ("workflow", "alert_rule_id"),
         )
         constraints = [
             CheckConstraint(
-                condition=Q(rule__isnull=False, alert_rule__isnull=True)
-                | Q(rule__isnull=True, alert_rule__isnull=False),
+                condition=Q(rule_id__isnull=False, alert_rule_id__isnull=True)
+                | Q(rule_id__isnull=True, alert_rule_id__isnull=False),
                 name="rule_or_alert_rule_workflow",
             ),
         ]
