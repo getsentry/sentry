getsentry/sentry

commit 1828fcc31d6e8fe477f07d4719bd7db0b798f173

ref(dashboards): Set aliases per plottable in `TimeSeriesWidgetVisualization` (#87023)
We allow plottables to have aliases. e.g.,

```jsx
const timeSeries = {
  field: 'failure_rate_5()',
  ...

<TimeSeriesWidgetVisualization
  plottables={[
    new Line(timeSeries)
  ]}
  aliases={{'failure_rate_5()': '5XX'}}
/>
```

That makes it easy to set nice aliases for long series names in charts.
This is not a great API for a few reasons:

1. Aliases have to be constructed ahead of time. Sometimes this is easy,
sometime it's annoying, if you're calculating aliases as the same time
as constructing the plottables
2. This won't work well in the near future. Right now we're passing
around objects with properties like `field: "/insights,prod : eps()"`
which is impractical. We're going to break that stuff up
3. This works like crap for charts that have different series with the
same `field`, like if you're showing different `avg(span.duration)` from
different queries, for example

This PR changes the API, and now every plottable can specify its alias,
which is much more explicit!

e.g.,

```jsx
const timeSeries = {
  field: 'failure_rate_5()',
  ...

<TimeSeriesWidgetVisualization
  plottables={[
    new Line(timeSeries, {alias: '5XX'})
  ]}
/>
```

Each plottable has a `label` property that either returns the alias, or
computes a default value if needed. It sets the label directly on the
output `Series` so we don't have to do any additional processing at
tooltip time 👍🏻

N.B. `InsightsTimeSeriesWidget` still accepts `aliases` and then passes
them along to the right plottable. That's just for convenience in that
case, I'll be gradually thinning that component out.

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/area.tsx
@@ -15,7 +15,7 @@ export class Area extends ContinuousTimeSeries implements Plottable {
     return new Area(this.constrainTimeSeries(boundaryStart, boundaryEnd), this.config);
   }
   toSeries(plottingOptions: ContinuousTimeSeriesPlottingOptions): LineSeriesOption[] {
-    const {timeSeries, config = {}} = this;
+    const {config = {}} = this;
 
     const color = plottingOptions.color ?? config.color ?? undefined;
     const scaledSeries = this.scaleToUnit(plottingOptions.unit);
@@ -26,7 +26,7 @@ export class Area extends ContinuousTimeSeries implements Plottable {
     const plottableSeries: LineSeriesOption[] = [];
 
     const commonOptions = {
-      name: timeSeries.field,
+      name: this.label,
       color,
       animation: false,
       yAxisIndex: plottingOptions.yAxisPosition === 'left' ? 0 : 1,

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/bars.tsx
@@ -26,7 +26,7 @@ export class Bars extends ContinuousTimeSeries<BarsConfig> implements Plottable 
   toSeries(
     plottingOptions: ContinuousTimeSeriesPlottingOptions
   ): Array<BarSeriesOption | LineSeriesOption> {
-    const {timeSeries, config = {}} = this;
+    const {config = {}} = this;
 
     const color = plottingOptions.color ?? config.color ?? undefined;
     const scaledTimeSeries = this.scaleToUnit(plottingOptions.unit);
@@ -35,7 +35,7 @@ export class Bars extends ContinuousTimeSeries<BarsConfig> implements Plottable 
 
     return [
       BarSeries({
-        name: timeSeries.field,
+        name: this.label,
         stack: config.stack,
         yAxisIndex: plottingOptions.yAxisPosition === 'left' ? 0 : 1,
         color,

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/continuousTimeSeries.tsx
@@ -4,8 +4,13 @@ import type {AggregationOutputType, DataUnit} from 'sentry/utils/discover/fields
 import {scaleTimeSeriesData} from 'sentry/utils/timeSeries/scaleTimeSeriesData';
 
 import type {TimeSeries} from '../../common/types';
+import {formatSeriesName} from '../formatters/formatSeriesName';
 
 export type ContinuousTimeSeriesConfig = {
+  /**
+   * Optional alias. If not provided, the series name from the legend will be computed from the `TimeSeries`.
+   */
+  alias?: string;
   /**
    * Optional color. If not provided, a backfill from a common palette will be provided to `toSeries`
    */
@@ -46,6 +51,10 @@ export abstract class ContinuousTimeSeries<
     this.timeSeries = timeSeries;
     this.#timestamps = timeSeries.data.map(datum => datum.timestamp).toSorted();
     this.config = config;
+  }
+
+  get label(): string {
+    return this.config?.alias ?? formatSeriesName(this.timeSeries.field);
   }
 
   get isEmpty(): boolean {

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/line.tsx
@@ -15,7 +15,7 @@ export class Line extends ContinuousTimeSeries implements Plottable {
     return new Line(this.constrainTimeSeries(boundaryStart, boundaryEnd), this.config);
   }
   toSeries(plottingOptions: ContinuousTimeSeriesPlottingOptions) {
-    const {timeSeries, config = {}} = this;
+    const {config = {}} = this;
 
     const color = plottingOptions.color ?? config.color ?? undefined;
     const scaledSeries = this.scaleToUnit(plottingOptions.unit);
@@ -26,7 +26,7 @@ export class Line extends ContinuousTimeSeries implements Plottable {
     const plottableSeries: LineSeriesOption[] = [];
 
     const commonOptions = {
-      name: timeSeries.field,
+      name: this.label,
       color,
       animation: false,
       yAxisIndex: plottingOptions.yAxisPosition === 'left' ? 0 : 1,

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/plottable.tsx
@@ -35,10 +35,13 @@ export interface Plottable {
    * Start timestamp of the plottable, if applicable
    */
   start: string | null;
-
   /**
    *
    * @param plottingOptions Plotting options depend on the specific implementation of the interface.
    */
   toSeries(plottingOptions: unknown): SeriesOption[];
+  /**
+   * Optional label for this plottable, if it appears in the legend and in tooltips.
+   */
+  label?: string;
 }

static/app/views/dashboards/widgets/timeSeriesWidget/timeSeriesWidgetVisualization.stories.tsx
@@ -490,21 +490,18 @@ export default storyBook('TimeSeriesWidgetVisualization', (story, APIReference) 
           the user changes the legend selection by clicking on legend labels.
         </p>
         <p>
-          You can also provide aliases for legends to give them a friendlier name. In this
-          example, verbose names like "p99(span.duration)" are truncated, and the p99
-          series is hidden by default.
+          You can also provide aliases for plottables like <code>Line</code> This will
+          give the legends and tooltips a friendlier name. In this example, verbose names
+          like "p99(span.duration)" are truncated, and the p99 series is hidden by
+          default.
         </p>
 
         <MediumWidget>
           <TimeSeriesWidgetVisualization
             plottables={[
-              new Area(sampleDurationTimeSeries, {}),
-              new Area(sampleDurationTimeSeries2, {}),
+              new Area(sampleDurationTimeSeries, {alias: 'p50'}),
+              new Area(sampleDurationTimeSeries2, {alias: 'p99'}),
             ]}
-            aliases={{
-              'p99(span.duration)': 'p99',
-              'p50(span.duration)': 'p50',
-            }}
             legendSelection={legendSelection}
             onLegendSelectionChange={setLegendSelection}
           />

static/app/views/dashboards/widgets/timeSeriesWidget/timeSeriesWidgetVisualization.tsx
@@ -31,9 +31,8 @@ import {makeReleasesPathname} from 'sentry/views/releases/utils/pathnames';
 
 import {useWidgetSyncContext} from '../../contexts/widgetSyncContext';
 import {NO_PLOTTABLE_VALUES, X_GUTTER, Y_GUTTER} from '../common/settings';
-import type {Aliases, LegendSelection, Release} from '../common/types';
+import type {LegendSelection, Release} from '../common/types';
 
-import {formatSeriesName} from './formatters/formatSeriesName';
 import {formatTooltipValue} from './formatters/formatTooltipValue';
 import {formatXAxisTimestamp} from './formatters/formatXAxisTimestamp';
 import {formatYAxisValue} from './formatters/formatYAxisValue';
@@ -50,9 +49,6 @@ export interface TimeSeriesWidgetVisualizationProps {
    */
   plottables: Plottable[];
   /**
-   * A mapping of time series fields to their user-friendly labels, if needed
-   */
-  aliases?: Aliases;
   /**
    * Disables navigating to release details when clicked
    * TODO(billy): temporary until we implement route based nav
@@ -350,9 +346,6 @@ export function TimeSeriesWidgetVisualization(props: TimeSeriesWidgetVisualizati
 
         return formatTooltipValue(value, fieldType, unitForType[fieldType] ?? undefined);
       },
-      nameFormatter: seriesName => {
-        return props.aliases?.[seriesName] ?? formatSeriesName(seriesName);
-      },
       truncate: true,
       utc: utc ?? false,
     })(deDupedParams, asyncTicket);
@@ -459,7 +452,7 @@ export function TimeSeriesWidgetVisualization(props: TimeSeriesWidgetVisualizati
               left: 0,
               formatter(seriesName: string) {
                 return truncationFormatter(
-                  props.aliases?.[seriesName] ?? formatSeriesName(seriesName),
+                  seriesName,
                   true,
                   // Escaping the legend string will cause some special
                   // characters to render as their HTML equivalents.

static/app/views/dashboards/widgets/common/types.tsx
@@ -42,6 +42,4 @@ export type Release = {
   version: string;
 };
 
-export type Aliases = Record<string, string>;
-
 export type LegendSelection = {[key: string]: boolean};

commit 6026fbc7321b8063decd2ccfca4073f8ec77e931

fix(dashboards): Improve series alias resolution (#88556)
Fixes DAIN-143. There's a subtle bug (introduced in
https://github.com/getsentry/sentry/pull/87023/) in which if multiple
plottables had the same alias, ECharts would merge them in the legend.
e.g., imagine plotting `span.duration` for two really long queries. The
truncation series name formatter would clip the names, and if the first
parts of the queries match they'd have the same name, and the same
legend entry. Confusing! This PR resolves that problem, and ensures that
every plottable has a legend entry.

## Changes
- add `Plottable.name`, a string property that's passed directly to
ECharts. This is a steady under-the-hood value that ECharts uses when
passing stuff around
- resolve all aliases ahead-of-time, so they're fast to look up
- defer to aliases when rendering tooltips and legends

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/area.tsx
@@ -26,7 +26,7 @@ export class Area extends ContinuousTimeSeries implements Plottable {
     const plottableSeries: LineSeriesOption[] = [];
 
     const commonOptions = {
-      name: this.label,
+      name: this.name,
       color,
       animation: false,
       yAxisIndex: plottingOptions.yAxisPosition === 'left' ? 0 : 1,

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/bars.tsx
@@ -36,7 +36,7 @@ export class Bars extends ContinuousTimeSeries<BarsConfig> implements Plottable 
 
     return [
       BarSeries({
-        name: this.label,
+        name: this.name,
         stack: config.stack,
         yAxisIndex: plottingOptions.yAxisPosition === 'left' ? 0 : 1,
         color,

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/continuousTimeSeries.tsx
@@ -56,6 +56,10 @@ export abstract class ContinuousTimeSeries<
     this.config = config;
   }
 
+  get name(): string {
+    return this.timeSeries.field;
+  }
+
   get label(): string {
     return this.config?.alias ?? formatSeriesName(this.timeSeries.field);
   }

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/line.tsx
@@ -26,7 +26,7 @@ export class Line extends ContinuousTimeSeries implements Plottable {
     const plottableSeries: LineSeriesOption[] = [];
 
     const commonOptions: LineSeriesOption = {
-      name: this.label,
+      name: this.name,
       color,
       animation: false,
       yAxisIndex: plottingOptions.yAxisPosition === 'left' ? 0 : 1,

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/plottable.tsx
@@ -32,6 +32,10 @@ export interface Plottable {
    */
   isEmpty: boolean;
   /**
+   * Name of the series. This is used under-the-hood in ECharts.
+   */
+  name: string;
+  /**
    * Whether this plottable needs a color from a shared palette. For example, data series plottables share a palette which is created based on how many series will be plotted.
    */
   needsColor: boolean;

static/app/views/dashboards/widgets/timeSeriesWidget/plottables/samples.tsx
@@ -104,6 +104,10 @@ export class Samples implements Plottable {
     return this.#timestamps.at(-1) ?? null;
   }
 
+  get name(): string {
+    return `${this.config.attributeName} samples`;
+  }
+
   get label(): string {
     return this.config?.alias ?? t('%s Samples', this.config.attributeName);
   }
@@ -150,7 +154,7 @@ export class Samples implements Plottable {
 
     const series: ScatterSeriesOption = {
       type: 'scatter',
-      name: this.label,
+      name: this.name,
       data: samples.data.filter(isValidSampleRow).map(sample => {
         const value = sample[config.attributeName];
         return [sample.timestamp, value ?? ECHARTS_MISSING_DATA_VALUE, sample.id];

static/app/views/dashboards/widgets/timeSeriesWidget/timeSeriesWidgetVisualization.stories.tsx
@@ -671,23 +671,34 @@ export default storyBook('TimeSeriesWidgetVisualization', (story, APIReference) 
         </p>
         <p>
           You can also provide aliases for plottables like <code>Line</code> This will
-          give the legends and tooltips a friendlier name. In this example, verbose names
-          like "p99(span.duration)" are truncated, and the p99 series is hidden by
-          default.
+          give the legends and tooltips a friendlier name. In the first example, verbose
+          names like "p99(span.duration)" are truncated, and the p99 series is hidden by
+          default. The legend will always include an entry for every plottable, even if
+          some plottables have the same alias, as you can see in the second example.
         </p>
 
         <code>{JSON.stringify(legendSelection)}</code>
 
-        <MediumWidget>
-          <TimeSeriesWidgetVisualization
-            plottables={[
-              new Area(sampleDurationTimeSeries, {alias: 'p50'}),
-              new Area(sampleDurationTimeSeries2, {alias: 'p99'}),
-            ]}
-            legendSelection={legendSelection}
-            onLegendSelectionChange={setLegendSelection}
-          />
-        </MediumWidget>
+        <SideBySide>
+          <MediumWidget>
+            <TimeSeriesWidgetVisualization
+              plottables={[
+                new Area(sampleDurationTimeSeries, {alias: 'p50'}),
+                new Area(sampleDurationTimeSeries2, {alias: 'p99'}),
+              ]}
+              legendSelection={legendSelection}
+              onLegendSelectionChange={setLegendSelection}
+            />
+          </MediumWidget>
+          <MediumWidget>
+            <TimeSeriesWidgetVisualization
+              plottables={[
+                new Area(sampleDurationTimeSeries, {alias: 'Duration'}),
+                new Area(sampleDurationTimeSeries2, {alias: 'Duration'}),
+              ]}
+            />
+          </MediumWidget>
+        </SideBySide>
       </Fragment>
     );
   });

static/app/views/dashboards/widgets/timeSeriesWidget/timeSeriesWidgetVisualization.tsx
@@ -313,6 +313,13 @@ export function TimeSeriesWidgetVisualization(props: TimeSeriesWidgetVisualizati
       ? theme.chart.getColorPalette(paletteSize - 2) // -2 because getColorPalette artificially adds 1, I'm not sure why
       : [];
 
+  // Create a lookup of series names (given to ECharts) to labels (from
+  // Plottable). This makes it easier to look up alises when rendering tooltips
+  // and legends
+  const aliases = Object.fromEntries(
+    props.plottables.map(plottable => [plottable.name, plottable.label])
+  );
+
   // Create tooltip formatter
   const formatTooltip: TooltipFormatterCallback<TopLevelFormatterParams> = (
     params,
@@ -354,9 +361,9 @@ export function TimeSeriesWidgetVisualization(props: TimeSeriesWidgetVisualizati
     return getFormatter({
       isGroupedByDate: true,
       showTimeInTooltip: true,
-      nameFormatter: function (name, nameFormatterParams) {
+      nameFormatter: function (seriesName, nameFormatterParams) {
         if (!nameFormatterParams) {
-          return name;
+          return seriesName;
         }
 
         if (
@@ -365,10 +372,10 @@ export function TimeSeriesWidgetVisualization(props: TimeSeriesWidgetVisualizati
         ) {
           // For scatter series, the third point in the `data` array should be the sample's ID
           const sampleId = nameFormatterParams.data.at(2);
-          return defined(sampleId) ? sampleId.toString() : name;
+          return defined(sampleId) ? sampleId.toString() : seriesName;
         }
 
-        return name;
+        return aliases[seriesName] ?? seriesName;
       },
       valueFormatter: function (value, _field, valueFormatterParams) {
         // Use the series to figure out the corresponding `Plottable`, and get the field type. From that, use whichever unit we chose for that field type.
@@ -502,7 +509,7 @@ export function TimeSeriesWidgetVisualization(props: TimeSeriesWidgetVisualizati
               left: 0,
               formatter(seriesName: string) {
                 return truncationFormatter(
-                  seriesName,
+                  aliases[seriesName] ?? seriesName,
                   true,
                   // Escaping the legend string will cause some special
                   // characters to render as their HTML equivalents.