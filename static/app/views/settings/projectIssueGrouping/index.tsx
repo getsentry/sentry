f10c5e70938597ad54c30ffdc77625d80a766b6a
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2025-04-02 08:05:35 -0400

    feat(grouping-settings): Show super user the derived grouping enhancements information (#88475)
    This will help if we need to debug automatically derived in-app stack
    trace rules.
    This will only show when the user is signed in as a superuser.
    
    <img width="473" alt="image"
    src="https://github.com/user-attachments/assets/eca8e2ea-4f7c-4fe6-9217-93bbc9259e3b"
    />
    
    ---------
    
    Co-authored-by: Scott Cooper <scttcper@gmail.com>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/views/settings/projectIssueGrouping/index.tsx
+++ static/app/views/settings/projectIssueGrouping/index.tsx
@@ -12,6 +12,7 @@ import type {EventGroupingConfig} from 'sentry/types/event';
 import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import type {Organization} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
+import {isActiveSuperuser} from 'sentry/utils/isActiveSuperuser';
 import {useApiQuery} from 'sentry/utils/queryClient';
 import routeTitleGen from 'sentry/utils/routeTitle';
 import SettingsPageHeader from 'sentry/views/settings/components/settingsPageHeader';
@@ -50,6 +51,7 @@ export default function ProjectIssueGrouping({organization, project, params}: Pr
   const endpoint = `/projects/${organization.slug}/${project.slug}/`;
 
   const access = new Set(organization.access.concat(project.access));
+  const activeSuperUser = isActiveSuperuser();
   const hasAccess = hasEveryAccess(['project:write'], {organization, project});
 
   const jsonFormProps = {
@@ -100,6 +102,14 @@ export default function ProjectIssueGrouping({organization, project, params}: Pr
           title={t('Stack Trace Rules')}
           fields={[fields.groupingEnhancements]}
         />
+
+        {activeSuperUser && (
+          <JsonForm
+            {...jsonFormProps}
+            title={t('Derived Grouping Enhancements')}
+            fields={[fields.derivedGroupingEnhancements!]}
+          />
+        )}
       </Form>
     </SentryDocumentTitle>
   );
