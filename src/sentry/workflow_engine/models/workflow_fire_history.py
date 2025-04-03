7089139ebbdfab905053669da3e24e07d225d971
Cathy Teng <cathy.teng@sentry.io>
2025-04-03 11:21:55 -0700

    drop notification_uuid from WorkflowFireHistory

--- src/sentry/workflow_engine/models/workflow_fire_history.py
+++ src/sentry/workflow_engine/models/workflow_fire_history.py
@@ -1,7 +1,6 @@
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import CharField, FlexibleForeignKey, region_silo_model, sane_repr
 from sentry.db.models.base import DefaultFieldsModel
-from sentry.db.models.fields.uuid import UUIDField
 
 
 @region_silo_model
@@ -11,7 +10,6 @@ class WorkflowFireHistory(DefaultFieldsModel):
     workflow = FlexibleForeignKey("workflow_engine.Workflow")
     group = FlexibleForeignKey("sentry.Group")
     event_id = CharField(max_length=32)
-    notification_uuid = UUIDField(auto_add=True, unique=True)
 
     class Meta:
         db_table = "workflow_engine_workflowfirehistory"
