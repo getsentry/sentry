ec2503ccdf3b398eac7024e76a841b1168b76752
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2025-04-03 10:52:06 -0400

    fix(derive_code_mapping): Grab absPath instead of abs_path (#88673)
    This is necessary for using the "Set up code mapping" modal for Java
    projects.
    
    Fixes [SENTRY-3RDY](https://sentry.sentry.io/issues/6505158896/)

--- src/sentry/issues/auto_source_code_config/derived_code_mappings_endpoint.py
+++ src/sentry/issues/auto_source_code_config/derived_code_mappings_endpoint.py
@@ -26,7 +26,7 @@ def get_file_and_repo_matches(request: Request, organization: Organization) -> l
 
 def get_frame_info_from_request(request: Request) -> FrameInfo:
     frame = {
-        "absPath": request.GET.get("absPath"),
+        "abs_path": request.GET.get("absPath"),
         # Currently, the only required parameter, thus, avoiding the `get` method
         "filename": request.GET["stacktraceFilename"],
         "module": request.GET.get("module"),
