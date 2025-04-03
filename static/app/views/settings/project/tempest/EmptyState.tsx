b64a90c92c187a93da99a2b7f30c4781e31c39b1
Vjeran Grozdanic <vjeran.grozdanic@sentry.io>
2025-04-03 13:53:23 +0200

    update filter

--- static/app/views/settings/project/tempest/EmptyState.tsx
+++ static/app/views/settings/project/tempest/EmptyState.tsx
@@ -104,7 +104,7 @@ export default function EmptyState() {
                     navigate({
                       pathname: '/issues/',
                       query: {
-                        query: 'sdk.name:minidump.tempest',
+                        query: 'os.name:PlayStation',
                       },
                     });
                   }}
