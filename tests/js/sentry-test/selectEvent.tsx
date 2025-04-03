bbbd35d47697249f170f6bca2d7eedd527ac812f
Scott Cooper <scttcper@gmail.com>
2025-04-03 14:26:01 -0700

    revert

--- tests/js/sentry-test/selectEvent.tsx
+++ tests/js/sentry-test/selectEvent.tsx
@@ -46,7 +46,7 @@ const openMenu = async (
   input: HTMLElement,
   {user = userEvent}: UserEventOptions = {}
 ) => {
-  await user.click(input, {skipHover: true, delay: null});
+  await user.click(input, {skipHover: true});
   // Arrow down may be required?
   // await user.type(input, '{ArrowDown}');
 };
@@ -66,7 +66,7 @@ const type = async (
  * Press the "clear" button, and reset various states
  */
 const clear = async (clearButton: Element, {user}: Required<UserEventOptions>) => {
-  await user.click(clearButton, {skipHover: true, delay: null});
+  await user.click(clearButton, {skipHover: true});
 };
 
 interface Config extends UserEventOptions {
@@ -115,7 +115,7 @@ const select = async (
     // will also match the selector. In this case, the actual dropdown element is
     // positioned last in the DOM tree.
     const optionElement = matchingElements[matchingElements.length - 1]!;
-    await user.click(optionElement, {skipHover: true, delay: null});
+    await user.click(optionElement, {skipHover: true});
   }
 };
 
