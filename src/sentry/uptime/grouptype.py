316d95752a6d7b02e38727801b3f5fa47096d98a
Dan Fuller <dfuller@sentry.io>
2025-04-04 12:20:27 -0700

    chore(uptime): Move UptimeDomainCheckFailure to uptime/grouptype.py take 2 (#88821)
    We want to keep grouptypes in the module that is related to them, so
    just moving this out.
    
    This caused pickling problems last time, so importing the model in its
    old location to prevent issues.

---
+++ src/sentry/uptime/grouptype.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+
+from sentry.issues import grouptype
+from sentry.issues.grouptype import GroupCategory, GroupType
+from sentry.ratelimits.sliding_windows import Quota
+from sentry.types.group import PriorityLevel
+
+
+@dataclass(frozen=True)
+class UptimeDomainCheckFailure(GroupType):
+    type_id = 7001
+    slug = "uptime_domain_failure"
+    description = "Uptime Domain Monitor Failure"
+    category = GroupCategory.UPTIME.value
+    creation_quota = Quota(3600, 60, 1000)  # 1000 per hour, sliding window of 60 seconds
+    default_priority = PriorityLevel.HIGH
+    enable_auto_resolve = False
+    enable_escalation_detection = False
+
+
+# XXX: Temporary hack to work around pickling issues
+grouptype.UptimeDomainCheckFailure = UptimeDomainCheckFailure  # type: ignore[attr-defined]
