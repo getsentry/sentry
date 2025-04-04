ef1d9ef0a6ad092444d982ace31aea5a5170b3b9
Colleen O'Rourke <colleen@sentry.io>
2025-04-04 14:14:55 -0700

    feat(ACI): Add IncidentGroupOpenPeriod lookup table

---
+++ src/sentry/deletions/defaults/incident.py
@@ -0,0 +1,9 @@
+from sentry.deletions.base import BaseRelation, ModelDeletionTask, ModelRelation
+from sentry.incidents.models.incident import Incident
+
+
+class IncidentDeletionTask(ModelDeletionTask[Incident]):
+    def get_child_relations(self, instance: Incident) -> list[BaseRelation]:
+        from sentry.workflow_engine.models import IncidentGroupOpenPeriod
+
+        return [ModelRelation(IncidentGroupOpenPeriod, {"incident_id": instance.id})]
---
+++ src/sentry/workflow_engine/models/incident_groupopenperiod.py
@@ -0,0 +1,32 @@
+from django.db.models import UniqueConstraint
+
+from sentry.backup.scopes import RelocationScope
+from sentry.db.models import (
+    BoundedBigIntegerField,
+    DefaultFieldsModel,
+    FlexibleForeignKey,
+    region_silo_model,
+)
+
+
+@region_silo_model
+class IncidentGroupOpenPeriod(DefaultFieldsModel):
+    """
+    A lookup model for incidents and group open periods.
+    """
+
+    __relocation_scope__ = RelocationScope.Organization
+
+    incident_id = BoundedBigIntegerField(null=True)
+    group_open_period = FlexibleForeignKey("sentry.GroupOpenPeriod")
+
+    class Meta:
+        db_table = "workflow_engine_incidentgroupopenperiod"
+        app_label = "workflow_engine"
+
+        constraints = [
+            UniqueConstraint(
+                fields=["incident_id", "group_open_period"],
+                name="workflow_engine_uniq_incident_groupopenperiod",
+            )
+        ]
