7ee9720139777c788a9621a02c05b8fb181b0632
TkDodo <dominik.dorfmeister@sentry.io>
2025-04-04 12:55:02 +0200

    chore: revert eslint changes

--- static/app/utils/theme/theme.chonk.tsx
+++ static/app/utils/theme/theme.chonk.tsx
@@ -1138,7 +1138,7 @@ export const DO_NOT_USE_darkChonkTheme: ChonkTheme = {
 
 declare module '@emotion/react' {
   // @TODO(jonasbadalic): interface extending a type might be prone to some issues.
-  // eslint-disable-next-line @typescript-eslint/no-shadow
+  // eslint-disable-next-line @typescript-eslint/naming-convention, @typescript-eslint/no-shadow
   export interface DO_NOT_USE_ChonkTheme extends ChonkTheme {
     isChonk: true;
   }
@@ -1155,7 +1155,7 @@ declare module '@emotion/react' {
  * Chonk utilities and overrrides to assert correct theme type
  * inside chonk components without having to check for theme.isChonk everywhere
  */
-
+// eslint-disable-next-line @typescript-eslint/naming-convention
 interface DO_NOT_USE_ChonkTheme extends ChonkTheme {
   isChonk: true;
 }
