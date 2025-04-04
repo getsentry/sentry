7c5ce7735ef3f6abbd17f73a8c6789d0cf1bded6
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-04-04 15:06:23 +0200

    clean up test

--- tests/sentry/api/endpoints/test_organization_insights_tree.py
+++ tests/sentry/api/endpoints/test_organization_insights_tree.py
@@ -6,7 +6,6 @@ from django.urls import reverse
 from sentry.testutils.cases import SnubaTestCase, SpanTestCase
 from sentry.testutils.helpers.datetime import before_now
 from sentry.testutils.skips import requires_snuba
-from sentry.utils.samples import load_data
 from tests.snuba.api.endpoints.test_organization_events import OrganizationEventsEndpointTestBase
 
 
@@ -20,17 +19,8 @@ class OrganizationInsightsTreeEndpointTest(
 
     def setUp(self):
         super().setUp()
-        self.nine_mins_ago = before_now(minutes=9)
         self.ten_mins_ago = before_now(minutes=10)
-        self.ten_mins_ago_iso = self.ten_mins_ago.replace(microsecond=0).isoformat()
-        self.eleven_mins_ago = before_now(minutes=11)
-        self.eleven_mins_ago_iso = self.eleven_mins_ago.isoformat()
-        self.transaction_data = load_data("transaction", timestamp=self.ten_mins_ago)
         self.features = {}
-
-        self.ten_mins_ago = before_now(minutes=10)
-        self.ten_mins_ago_iso = self.ten_mins_ago.replace(microsecond=0).isoformat()
-        self.login_as(user=self.user)
         self.url = reverse(
             self.url_name,
             kwargs={
@@ -38,13 +28,13 @@ class OrganizationInsightsTreeEndpointTest(
             },
         )
 
-    @patch("sentry.snuba.spans_rpc.run_table_query")
-    def test_get_nextjs_function_data(self, mock_run_table_query):
-
         self.create_environment(self.project, name="production")
         self._store_nextjs_function_spans()
         self._store_unrelated_spans()
 
+    @patch("sentry.snuba.spans_rpc.run_table_query")
+    def test_get_nextjs_function_data(self, mock_run_table_query):
+        self.login_as(user=self.user)
         with self.feature(self.FEATURES):
             response = self.client.get(
                 self.url,
