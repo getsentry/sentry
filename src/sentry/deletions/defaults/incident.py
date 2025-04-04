a37281c14810699660b79e26cc8b28717c001cc6
Colleen O'Rourke <colleen@sentry.io>
2025-04-04 15:18:56 -0700

    deal with deletions later

--- src/sentry/deletions/defaults/incident.py
+++
@@ -1,9 +0,0 @@
-from sentry.deletions.base import BaseRelation, ModelDeletionTask, ModelRelation
-from sentry.incidents.models.incident import Incident
-
-
-class IncidentDeletionTask(ModelDeletionTask[Incident]):
-    def get_child_relations(self, instance: Incident) -> list[BaseRelation]:
-        from sentry.workflow_engine.models import IncidentGroupOpenPeriod
-
-        return [ModelRelation(IncidentGroupOpenPeriod, {"incident_id": instance.id})]
