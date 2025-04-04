7d0935f0666a023eda3e4445ed9c67390509a044
anthony sottile <anthony.sottile@sentry.io>
2025-04-04 17:57:18 -0400

    ref: fix sentry.middleware.env to actually tear down request
    request_finished is not always called!

--- src/sentry/middleware/env.py
+++ src/sentry/middleware/env.py
@@ -1,27 +1,16 @@
-from django.core.signals import request_finished
-from django.utils.deprecation import MiddlewareMixin
-from rest_framework.request import Request
+from collections.abc import Callable
+
+from django.http.request import HttpRequest
+from django.http.response import HttpResponseBase
 
 from sentry.app import env
 
 
-# In test environments, we sometimes run requests inline with other requests, so we're forced to maintain the full
-# stack of requests happening inside of a process.  This does not normally happen in production however.
-class SentryEnvMiddleware(MiddlewareMixin):
-    def process_request(self, request: Request):
-        # bind request to env
-        if env.request_stack is None:
-            env.request_stack = []
-        if env.request:
-            env.request_stack.append(env.request)
-        env.request = request
+def SentryEnvMiddleware(
+    get_response: Callable[[HttpRequest], HttpResponseBase]
+) -> Callable[[HttpRequest], HttpResponseBase]:
+    def SentryEnvMiddleware_impl(request: HttpRequest) -> HttpResponseBase:
+        with env.active_request(request):
+            return get_response(request)
 
-
-def clear_request(**kwargs):
-    if env.request_stack:
-        env.request = env.request_stack.pop()
-    else:
-        env.request = None
-
-
-request_finished.connect(clear_request)
+    return SentryEnvMiddleware_impl
