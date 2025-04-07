ee042795ecb642a2eaa38db53ef2bba284652ce5
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-04-07 16:14:22 -0700

    fix(insights): surface error response json detail if it exists (#88982)
    when an error occurs rendering the widget, rather than surface the
    400/bad request error string, surface the response JSON if it exists.
    
    before:
    
    <img width="1190" alt="SCR-20250407-oaax"
    src="https://github.com/user-attachments/assets/c857a895-53e6-48f2-8939-81778cd427a6"
    />
    
    after:
    
    
    
    
    <img width="1189" alt="SCR-20250407-oaqj"
    src="https://github.com/user-attachments/assets/85e5b316-4b98-41a8-8e11-954ca8655f6b"
    />

--- static/app/views/dashboards/widgets/widget/widgetError.tsx
+++ static/app/views/dashboards/widgets/widget/widgetError.tsx
@@ -1,9 +1,13 @@
 import styled from '@emotion/styled';
 
 import {IconWarning} from 'sentry/icons';
+import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {DEEMPHASIS_COLOR_NAME} from 'sentry/views/dashboards/widgets/bigNumberWidget/settings';
-import type {StateProps} from 'sentry/views/dashboards/widgets/common/types';
+import type {
+  ErrorPropWithResponseJSON,
+  StateProps,
+} from 'sentry/views/dashboards/widgets/common/types';
 
 import {X_GUTTER, Y_GUTTER} from '../common/settings';
 
@@ -15,7 +19,13 @@ export function WidgetError({error}: WidgetErrorProps) {
   return (
     <Panel>
       <NonShrinkingWarningIcon color={DEEMPHASIS_COLOR_NAME} size="md" />
-      <ErrorText>{error?.toString()}</ErrorText>
+      <ErrorText>
+        {typeof error === 'string'
+          ? error
+          : ((error as ErrorPropWithResponseJSON)?.responseJSON?.detail.toString() ??
+            error?.message ??
+            t('Error loading data.'))}
+      </ErrorText>
     </Panel>
   );
 }
