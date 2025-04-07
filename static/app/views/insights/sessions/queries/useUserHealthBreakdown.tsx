24cd960d8eab9658ba15f64c0ca40dc142fdb551
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-04-07 16:20:46 -0700

    fix(insights): adjust session interval in query

--- static/app/views/insights/sessions/queries/useUserHealthBreakdown.tsx
+++ static/app/views/insights/sessions/queries/useUserHealthBreakdown.tsx
@@ -1,12 +1,17 @@
 import type {SessionApiResponse} from 'sentry/types/organization';
 import {useApiQuery} from 'sentry/utils/queryClient';
+import {getSessionsInterval} from 'sentry/utils/sessions';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
+import usePageFilters from 'sentry/utils/usePageFilters';
 import {getCountStatusSeries} from 'sentry/views/insights/sessions/utils/sessions';
 
 export default function useUserHealthBreakdown({type}: {type: 'count' | 'rate'}) {
   const location = useLocation();
   const organization = useOrganization();
+  const {
+    selection: {datetime},
+  } = usePageFilters();
 
   const locationQuery = {
     ...location,
@@ -28,6 +33,7 @@ export default function useUserHealthBreakdown({type}: {type: 'count' | 'rate'})
       {
         query: {
           ...locationQuery.query,
+          interval: getSessionsInterval(datetime),
           field: ['count_unique(user)'],
           groupBy: ['session.status'],
         },
