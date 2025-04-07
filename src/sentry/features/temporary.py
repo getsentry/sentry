29dabc7b0a073947032c912ed07863d498866486
Colton Allen <colton.allen@sentry.io>
2025-04-07 09:18:20 -0500

    feat(issues): Add /suspect/tags/ endpoint (#88737)
    This is not expected to be the final version of the endpoint. This is a
    starting point for the issues team to work off of. For now it uses the
    feature flag logic which may or may not be applicable to tags.
    
    The endpoint and the suspect_tags module are copies of the suspect-flags
    endpoint and module (with context appropriate changes). The two units of
    work should expect to diverge over time. There is no need to abstract
    these modules for re-use. It will only introduce dependencies between
    our teams.
    
    ---------
    
    Co-authored-by: Andrew Liu <159852527+aliu39@users.noreply.github.com>

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -349,6 +349,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:feature-flag-distribution-flyout", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable suspect feature flags endpoint.
     manager.add("organizations:feature-flag-suspect-flags", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    # Enable suspect feature tags endpoint.
+    manager.add("organizations:issues-suspect-tags", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable new release insights charts
     manager.add("organizations:insights-session-health-tab-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Lets organizations manage grouping configs
