668cbc7657378609f74454e17600edc65a0afff2
Malachi Willey <malwilley@gmail.com>
2025-04-07 14:29:45 -0700

    feat(issue-views): Allow saving and discarding of unsaved changes

---
+++ static/app/views/issueList/issueViews/useIssueViewUnsavedChanges.tsx
@@ -0,0 +1,37 @@
+import isEqual from 'lodash/isEqual';
+
+import {useLocation} from 'sentry/utils/useLocation';
+import {useNavigate} from 'sentry/utils/useNavigate';
+import {createIssueViewFromUrl} from 'sentry/views/issueList/issueViews/createIssueViewFromUrl';
+import {getIssueViewQueryParams} from 'sentry/views/issueList/issueViews/getIssueViewQueryParams';
+import {useSelectedGroupSearchView} from 'sentry/views/issueList/issueViews/useSelectedGroupSeachView';
+
+export function useIssueViewUnsavedChanges() {
+  const {data: view} = useSelectedGroupSearchView();
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  if (!view) {
+    return {
+      hasUnsavedChanges: false,
+      discardUnsavedChanges: () => {},
+    };
+  }
+
+  const queryParams = getIssueViewQueryParams({view});
+
+  const currentViewData = createIssueViewFromUrl({query: queryParams});
+  const viewFromUrl = createIssueViewFromUrl({query: location.query});
+
+  const hasUnsavedChanges = !isEqual(currentViewData, viewFromUrl);
+
+  return {
+    hasUnsavedChanges,
+    discardUnsavedChanges: () => {
+      navigate({
+        pathname: location.pathname,
+        query: getIssueViewQueryParams({view}),
+      });
+    },
+  };
+}
