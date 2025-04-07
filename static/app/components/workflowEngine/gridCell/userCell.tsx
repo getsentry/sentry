926e7dc8730b87c69af00b013dfaa74796dd933d
Nate Moore <nate.moore@sentry.io>
2025-04-07 17:41:21 -0500

    fix(aci): update icon display

--- static/app/components/workflowEngine/gridCell/userCell.tsx
+++ static/app/components/workflowEngine/gridCell/userCell.tsx
@@ -1,9 +1,8 @@
 import styled from '@emotion/styled';
 
 import UserBadge from 'sentry/components/idBadge/userBadge';
-import LogoSentry from 'sentry/components/logoSentry';
+import {IconSentry} from 'sentry/icons';
 import type {AvatarUser} from 'sentry/types/user';
-import {useUser} from 'sentry/utils/useUser';
 
 export type UserCellProps = {
   user: 'sentry' | AvatarUser;
@@ -12,18 +11,12 @@ export type UserCellProps = {
 };
 
 export function UserCell({user, disabled = false, className}: UserCellProps) {
-  const currentUser = useUser();
-  const isCurrentUser = user === 'sentry' ? false : currentUser.email === user.email;
   return (
     <Wrapper disabled={disabled} className={className}>
       {user === 'sentry' ? (
-        <LogoSentry />
+        <IconSentry size="lg" />
       ) : (
-        <UserBadge
-          hideEmail
-          displayName={`${user.name}${isCurrentUser ? ' (You)' : ''}`}
-          user={user}
-        />
+        <UserBadge hideEmail hideName avatarSize={24} user={user} />
       )}
     </Wrapper>
   );
