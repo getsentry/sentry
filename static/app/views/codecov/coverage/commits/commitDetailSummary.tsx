69ca0123dfba000ec455686e3f1315a6b4e3e941
nicholas-codecov <nicholas.deschenes@sentry.io>
2025-04-08 14:32:02 -0300

    :sparkles: Use translation utils

--- static/app/views/codecov/coverage/commits/commitDetailSummary.tsx
+++ static/app/views/codecov/coverage/commits/commitDetailSummary.tsx
@@ -12,6 +12,7 @@ import Link from 'sentry/components/links/link';
 import Panel from 'sentry/components/panels/panel';
 import PanelBody from 'sentry/components/panels/panelBody';
 import PanelHeader from 'sentry/components/panels/panelHeader';
+import {t} from 'sentry/locale';
 
 const headCommit = {
   sha: '31b72ff64bd75326ea5e43bf8e93b415db56cb62',
@@ -27,51 +28,63 @@ export function CommitDetailSummary() {
   return (
     <SummaryContainer columns={12}>
       <SelectedCommitPanel>
-        <PanelHeader>Coverage On Selected Commit</PanelHeader>
+        <PanelHeader>{t('Coverage On Selected Commit')}</PanelHeader>
         <PanelBody>
           <SummaryEntries largeColumnSpan={6} smallColumnSpan={2}>
             <SummaryEntry>
-              <SummaryEntryLabel showUnderline body={<p>Repository coverage tooltip</p>}>
-                Repository coverage
+              <SummaryEntryLabel
+                showUnderline
+                body={<p>{t('Repository coverage tooltip')}</p>}
+              >
+                {t('Repository coverage')}
               </SummaryEntryLabel>
               <SummaryEntryValue>98.98%</SummaryEntryValue>
               <StyledSubText>
-                Head commit{' '}
+                {t('Head commit')}{' '}
                 <Link to={`/codecov/coverage/commits/${headCommit.sha}`}>
                   {headCommit.shortSha}
                 </Link>
               </StyledSubText>
             </SummaryEntry>
             <SummaryEntry>
-              <SummaryEntryLabel showUnderline body={<p>Patch coverage tooltip</p>}>
-                Patch coverage
+              <SummaryEntryLabel
+                showUnderline
+                body={<p>{t('Patch coverage tooltip')}</p>}
+              >
+                {t('Patch coverage')}
               </SummaryEntryLabel>
               <SummaryEntryValue>100%</SummaryEntryValue>
             </SummaryEntry>
             <SummaryEntry>
-              <SummaryEntryLabel showUnderline body={<p>Uncovered lines tooltip</p>}>
-                Uncovered lines
+              <SummaryEntryLabel
+                showUnderline
+                body={<p>{t('Uncovered lines tooltip')}</p>}
+              >
+                {t('Uncovered lines')}
               </SummaryEntryLabel>
               <SummaryEntryValueLink filterBy="uncovered_lines">5</SummaryEntryValueLink>
             </SummaryEntry>
             <SummaryEntry>
-              <SummaryEntryLabel showUnderline body={<p>Files changed tooltip</p>}>
-                Files changed
+              <SummaryEntryLabel showUnderline body={<p>{t('Files changed tooltip')}</p>}>
+                {t('Files changed')}
               </SummaryEntryLabel>
               <SummaryEntryValueLink filterBy="files_changed">4</SummaryEntryValueLink>
             </SummaryEntry>
             <SummaryEntry>
-              <SummaryEntryLabel showUnderline body={<p>Indirect changes tooltip</p>}>
-                Indirect changes
+              <SummaryEntryLabel
+                showUnderline
+                body={<p>{t('Indirect changes tooltip')}</p>}
+              >
+                {t('Indirect changes')}
               </SummaryEntryLabel>
               <SummaryEntryValueLink filterBy="indirect_changes">1</SummaryEntryValueLink>
             </SummaryEntry>
             <SourceEntry>
-              <SummaryEntryLabel showUnderline body={<p>Source tooltip</p>}>
-                Source
+              <SummaryEntryLabel showUnderline body={<p>{t('Source tooltip')}</p>}>
+                {t('Source')}
               </SummaryEntryLabel>
               <SourceText>
-                This commit {headCommit.shortSha} compared to{' '}
+                {t('This commit %s compared to', headCommit.shortSha)}{' '}
                 <Link to={`/codecov/coverage/commits/${baseCommit.sha}`}>
                   {baseCommit.shortSha}
                 </Link>
@@ -81,15 +94,15 @@ export function CommitDetailSummary() {
         </PanelBody>
       </SelectedCommitPanel>
       <UploadsPanel>
-        <PanelHeader>Coverage Uploads - {headCommit.shortSha}</PanelHeader>
+        <PanelHeader>{t('Coverage Uploads - %s', headCommit.shortSha)}</PanelHeader>
         <PanelBody>
           <SummaryEntries largeColumnSpan={1} smallColumnSpan={1}>
             <SummaryEntry>
-              <SummaryEntryLabel showUnderline body={<p>Uploads count tooltip</p>}>
-                Uploads count
+              <SummaryEntryLabel showUnderline body={<p>{t('Uploads count tooltip')}</p>}>
+                {t('Uploads count')}
               </SummaryEntryLabel>
               <SummaryEntryValueLink filterBy="uploads_count">65</SummaryEntryValueLink>
-              <StyledSubText>(65 processed, 15 pending)</StyledSubText>
+              <StyledSubText>{t('(%s processed, %s pending)', 65, 15)}</StyledSubText>
             </SummaryEntry>
           </SummaryEntries>
         </PanelBody>
