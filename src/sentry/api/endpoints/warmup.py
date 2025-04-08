43f5a80096734c9e554a5feebf76817d526ff9c0
Josh Ferge <josh.ferge@sentry.io>
2025-04-08 16:57:04 -0400

    perf(warmup): add another module to be imported (#89098)
    found another module not currently imported during warmup.
    
    https://sentry.sentry.io/explore/profiling/profile/sentry/7182e5a68d6246a2bbcc079535e1fc03/flamegraph/?colorCoding=by%20system%20vs%20application%20frame&fov=0%2C81%2C87449072%2C14&query=&sorting=call%20order&tid=137599830554304&view=top%20down

--- src/sentry/api/endpoints/warmup.py
+++ src/sentry/api/endpoints/warmup.py
@@ -4,10 +4,11 @@ from django.utils.translation import override
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+import sentry.middleware.integrations.parsers.plugin  # NOQA
+import sentry.sentry_apps.services.app.impl  # NOQA
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import Endpoint, all_silo_endpoint
-from sentry.middleware.integrations.parsers.plugin import PluginRequestParser  # noqa
 from sentry.ratelimits.config import RateLimitConfig
 
 
