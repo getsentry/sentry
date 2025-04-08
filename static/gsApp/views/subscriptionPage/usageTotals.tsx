d80a70ce74754884dd5f365346d4e892e9891b53
Brendan Hy <brendan.hy@sentry.io>
2025-04-08 13:25:18 -0700

    resolve typing

--- static/gsApp/views/subscriptionPage/usageTotals.tsx
+++ static/gsApp/views/subscriptionPage/usageTotals.tsx
@@ -24,6 +24,7 @@ import {
 import {
   type BillingMetricHistory,
   type BillingStatTotal,
+  type DataCategories,
   type EventBucket,
   PlanTier,
   type ProductTrial,
@@ -184,8 +185,8 @@ export function calculateCategoryPrepaidUsage(
   prepaidSpend: number;
   prepaidUsage: number;
 } {
-  // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-  const categoryInfo: BillingMetricHistory = subscription.categories[category];
+  const categoryInfo: BillingMetricHistory | undefined =
+    subscription.categories[category as DataCategories];
   const usage = accepted ?? categoryInfo?.usage ?? 0;
 
   // Calculate the prepaid total
@@ -439,8 +440,9 @@ function UsageTotals({
     onDemandCategoryMax,
   } = calculateCategoryOnDemandUsage(category, subscription);
   const unusedOnDemandWidth = 100 - ondemandPercentUsed;
-  // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-  const categoryInfo: BillingMetricHistory = subscription.categories[category];
+  const categoryInfo: BillingMetricHistory | undefined =
+    subscription.categories[category as DataCategories];
+  const usage = categoryInfo?.usage ?? 0;
   const {prepaidPrice, prepaidPercentUsed, prepaidUsage, onDemandUsage} =
     calculateCategoryPrepaidUsage(
       category,
@@ -496,11 +498,7 @@ function UsageTotals({
     return t('usage this period');
   }
 
-  const formattedUnitsUsed = formatUsageWithUnits(
-    categoryInfo.usage,
-    category,
-    usageOptions
-  );
+  const formattedUnitsUsed = formatUsageWithUnits(usage, category, usageOptions);
 
   // use dropped profile chunks to estimate dropped continuous profiling
   // for AM3 plans, include profiles category to estimate dropped continuous profile hours
@@ -512,9 +510,9 @@ function UsageTotals({
             ? (eventTotals[DataCategory.PROFILES] ?? EMPTY_STAT_TOTAL)
             : EMPTY_STAT_TOTAL,
         ]),
-        accepted: categoryInfo.usage,
+        accepted: usage,
       }
-    : {...totals, accepted: categoryInfo.usage};
+    : {...totals, accepted: usage};
 
   const hasReservedQuota: boolean =
     reserved !== null && (reserved === UNLIMITED_RESERVED || reserved > 0);
