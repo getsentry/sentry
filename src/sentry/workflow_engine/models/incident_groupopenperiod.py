3aff3cdfaa7c88056fda7807ac259a68f9deb9fe
Colleen O'Rourke <colleen@sentry.io>
2025-04-08 14:47:16 -0700

    update migration to remove unique constraints

--- src/sentry/workflow_engine/models/incident_groupopenperiod.py
+++ src/sentry/workflow_engine/models/incident_groupopenperiod.py
@@ -1,5 +1,3 @@
-from django.db.models import UniqueConstraint
-
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import (
     BoundedBigIntegerField,
@@ -23,10 +21,3 @@ class IncidentGroupOpenPeriod(DefaultFieldsModel):
     class Meta:
         db_table = "workflow_engine_incidentgroupopenperiod"
         app_label = "workflow_engine"
-
-        constraints = [
-            UniqueConstraint(
-                fields=["incident_id", "group_open_period"],
-                name="workflow_engine_uniq_incident_groupopenperiod",
-            )
-        ]
