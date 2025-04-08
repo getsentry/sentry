a14c7f99e3a01bf5666904fab9b058367b42de46
Raj Joshi <raj.joshi@sentry.io>
2025-04-08 14:27:40 -0400

    🔧 chore: log all exceptions and invoke issue alert registry correctly (#88999)
    capturing all exception types and actually using the issue alert
    registry when calling `execute_via_issue_alert_handler`

--- src/sentry/notifications/notification_action/utils.py
+++ src/sentry/notifications/notification_action/utils.py
@@ -1,7 +1,9 @@
 import logging
 
-from sentry.notifications.notification_action.registry import group_type_notification_registry
-from sentry.notifications.notification_action.types import BaseIssueAlertHandler
+from sentry.notifications.notification_action.registry import (
+    group_type_notification_registry,
+    issue_alert_handler_registry,
+)
 from sentry.utils.registry import NoRegistrationExistsError
 from sentry.workflow_engine.models import Action, Detector
 from sentry.workflow_engine.types import WorkflowEventData
@@ -21,6 +23,13 @@ def execute_via_group_type_registry(
             detector.type,
             extra={"detector_id": detector.id, "action_id": action.id},
         )
+        raise
+    except Exception:
+        logger.exception(
+            "Error executing via group type registry",
+            extra={"detector_id": detector.id, "action_id": action.id},
+        )
+        raise
 
 
 def execute_via_issue_alert_handler(
@@ -30,4 +39,19 @@ def execute_via_issue_alert_handler(
     This exists so that all ticketing actions can use the same handler as issue alerts since that's the only way we can
     ensure that the same thread is used for the notification action.
     """
-    BaseIssueAlertHandler.invoke_legacy_registry(job, action, detector)
+    try:
+        handler = issue_alert_handler_registry.get(action.type)
+        handler.invoke_legacy_registry(job, action, detector)
+    except NoRegistrationExistsError:
+        logger.exception(
+            "No notification handler found for action type: %s",
+            action.type,
+            extra={"action_id": action.id, "detector_id": detector.id},
+        )
+        raise
+    except Exception:
+        logger.exception(
+            "Error executing via issue alert handler",
+            extra={"action_id": action.id, "detector_id": detector.id},
+        )
+        raise
