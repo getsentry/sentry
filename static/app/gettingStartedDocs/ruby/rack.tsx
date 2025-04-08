b518cc2b8da72625d09293245b5432d2a1fb7cb9
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-04-08 11:02:17 +0200

    feat(onboarding): Add profiling onboarding for ruby & co (#88888)

--- static/app/gettingStartedDocs/ruby/rack.tsx
+++ static/app/gettingStartedDocs/ruby/rack.tsx
@@ -6,6 +6,7 @@ import type {
   OnboardingConfig,
 } from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {CrashReportWebApiOnboarding} from 'sentry/components/onboarding/gettingStartedDoc/utils/feedbackOnboarding';
+import {profilingOnboarding} from 'sentry/gettingStartedDocs/ruby/ruby';
 import {t, tct} from 'sentry/locale';
 
 type Params = DocsParams;
@@ -132,8 +133,8 @@ const onboarding: OnboardingConfig = {
 
 const docs: Docs = {
   onboarding,
-  profilingOnboarding: onboarding,
   crashReportOnboarding: CrashReportWebApiOnboarding,
+  profilingOnboarding,
 };
 
 export default docs;
