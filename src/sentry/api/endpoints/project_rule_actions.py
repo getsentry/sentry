8e51b3e4a949e7145b041683de9ee27910b23f10
Raj Joshi <raj.joshi@sentry.io>
2025-04-07 21:27:32 -0700

    :wrench: chore: update send test notification to use noa

--- src/sentry/api/endpoints/project_rule_actions.py
+++ src/sentry/api/endpoints/project_rule_actions.py
@@ -5,16 +5,25 @@ from rest_framework.exceptions import ValidationError
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from sentry import features
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases import ProjectAlertRulePermission, ProjectEndpoint
 from sentry.api.serializers.rest_framework import DummyRuleSerializer
 from sentry.eventstore.models import GroupEvent
+from sentry.grouping.grouptype import ErrorGroupType
 from sentry.models.rule import Rule
 from sentry.rules.processing.processor import activate_downstream_actions
 from sentry.shared_integrations.exceptions import IntegrationFormError
 from sentry.utils.samples import create_sample_event
+from sentry.workflow_engine.migration_helpers.rule_action import (
+    translate_rule_data_actions_to_notification_actions,
+)
+from sentry.workflow_engine.models import Detector, Workflow
+from sentry.workflow_engine.types import WorkflowEventData
+
+logger = logging.getLogger(__name__)
 
 
 @region_silo_endpoint
@@ -65,6 +74,13 @@ class ProjectRuleActionsEndpoint(ProjectEndpoint):
             project, platform=project.platform, default="javascript", tagged=True
         )
 
+        if features.has(
+            "organizations:workflow-engine-test-notifications", project.organization
+        ) and features.has(
+            "organizations:workflow-engine-issue-alert-dual-write", project.organization
+        ):
+            return self.execute_future_on_test_event_workflow_engine(test_event, rule)
+
         return self.execute_future_on_test_event(test_event, rule)
 
     def execute_future_on_test_event(
@@ -112,6 +128,80 @@ class ProjectRuleActionsEndpoint(ProjectEndpoint):
         status = None
         data = None
         # Presence of "actions" here means we have exceptions to surface to the user
+        if len(action_exceptions) > 0:
+            status = 400
+            data = {"actions": action_exceptions}
+
+        return Response(status=status, data=data)
+
+    def execute_future_on_test_event_workflow_engine(
+        self,
+        test_event: GroupEvent,
+        rule: Rule,
+    ) -> Response:
+        """
+        Invoke the workflow_engine to send a test notification.
+        This method will lookup the corresponding workflow for a given rule then invoke the notification action.
+        """
+        action_exceptions = []
+        actions = rule.data.get("actions", [])
+
+        workflow = Workflow(
+            id=-1,
+            name="Test Workflow",
+            organization=rule.project.organization,
+        )
+
+        detector = Detector(
+            id=-1,
+            project=rule.project,
+            name=rule.label,
+            enabled=True,
+            type=ErrorGroupType.slug,
+        )
+
+        event_data = WorkflowEventData(
+            event=test_event,
+            workflow_id=workflow.id,
+        )
+
+        for action_blob in actions:
+            try:
+                action = translate_rule_data_actions_to_notification_actions(
+                    [action_blob], skip_failures=False
+                )[0]
+                action.id = -1
+            except Exception as e:
+                action_exceptions.append(str(e))
+                sentry_sdk.capture_exception(e)
+                continue
+
+            try:
+                action.trigger(
+                    event_data=event_data,
+                    detector=detector,
+                )
+            except Exception as exc:
+                if isinstance(exc, IntegrationFormError):
+                    logger.warning(
+                        "%s.test_alert.integration_error", action.type, extra={"exc": exc}
+                    )
+
+                    # IntegrationFormErrors should be safe to propagate via the API
+                    action_exceptions.append(str(exc))
+                else:
+                    # If we encounter some unexpected exception, we probably
+                    # don't want to continue executing more callbacks.
+                    logger.warning("%s.test_alert.unexpected_exception", action.type, exc_info=True)
+                    error_id = sentry_sdk.capture_exception(exc)
+                    action_exceptions.append(
+                        f"An unexpected error occurred. Error ID: '{error_id}'"
+                    )
+
+                break
+
+        status = None
+        data = None
         if len(action_exceptions) > 0:
             status = 400
             data = {"actions": action_exceptions}
