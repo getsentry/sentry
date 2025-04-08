3f963839a310311d208ee204103720a34279661e
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-04-08 15:19:30 -0700

    Update tests 2

--- tests/sentry/seer/workflows/test_compare.py
+++ tests/sentry/seer/workflows/test_compare.py
@@ -12,30 +12,27 @@ def test_keyed_kl_score():
     ]
     outliers = [("key", "true", 10), ("other", "true", 100), ("other", "false", 500)]
 
-    scores = keyed_kl_score(
+    score_dict = keyed_kl_score(
         baseline,
         outliers,
         total_a=sum(i[2] for i in baseline),
         total_b=sum(i[2] for i in outliers),
     )
-    assert scores[0][0] == "key"
-    assert math.isclose(scores[0][1], 0.297377, rel_tol=1e-3)
-    assert scores[1][0] == "other"
-    assert math.isclose(scores[1][1], 0.007, abs_tol=1e-3)
+    assert math.isclose(score_dict["key"], 0.297377, rel_tol=1e-3)
+    assert math.isclose(score_dict["other"], 0.007, abs_tol=1e-3)
 
     # Assert that without a large unseen value (missing the other key) the suspicion probability
     # is higher.
     baseline = [("key", "true", 10), ("key", "false", 200)]
     outliers = [("key", "true", 10)]
 
-    scores = keyed_kl_score(
+    score_dict = keyed_kl_score(
         baseline,
         outliers,
         total_a=sum(i[2] for i in baseline),
         total_b=sum(i[2] for i in outliers),
     )
-    assert scores[0][0] == "key"
-    assert math.isclose(scores[0][1], 8.58, rel_tol=1e-3)
+    assert math.isclose(score_dict["key"], 8.58, rel_tol=1e-3)
 
 
 def test_kl_score():
