87da43d0f057d558f4096e5a757b9a6616fc0664
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-04-09 12:31:42 -0700

    now with one line to evaluate the DataCondition

--- src/sentry/workflow_engine/endpoints/validators/base/data_condition_group.py
+++ src/sentry/workflow_engine/endpoints/validators/base/data_condition_group.py
@@ -15,10 +15,9 @@ class BaseDataConditionGroupValidator(CamelSnakeSerializer):
         conditions: list[DataCondition] = []
 
         for condition in value:
-            condition_validator = BaseDataConditionValidator(data=condition)
-
-            if not condition_validator.is_valid():
-                raise serializers.ValidationError(condition_validator.errors)
+            condition_validator = BaseDataConditionValidator(data=condition).is_valid(
+                raise_exception=True
+            )
 
             condition = DataCondition(condition_validator.validated_data)
             conditions.append(condition)
