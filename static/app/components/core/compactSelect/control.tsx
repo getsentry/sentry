e051d4a694609964ef687af8ceee54e20abd2526
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2025-04-09 15:13:58 +0200

    fix(ui): adjust loading indicator position (#89152)
    before:
    ![Screenshot 2025-04-09 at 14 39
    51](https://github.com/user-attachments/assets/24ccaf92-5bd5-431c-94c6-9db8c2849dd1)
    
    after:
    ![Screenshot 2025-04-09 at 14 39
    33](https://github.com/user-attachments/assets/19ab709d-5ff4-43af-8b16-de7ca43f2ec9)

--- static/app/components/core/compactSelect/control.tsx
+++ static/app/components/core/compactSelect/control.tsx
@@ -541,7 +541,7 @@ export function Control({
                 <MenuHeader size={size}>
                   <MenuTitle>{menuTitle}</MenuTitle>
                   <MenuHeaderTrailingItems>
-                    {loading && <StyledLoadingIndicator size={12} mini />}
+                    {loading && <StyledLoadingIndicator size={12} />}
                     {typeof menuHeaderTrailingItems === 'function'
                       ? menuHeaderTrailingItems({closeOverlay: overlayState.close})
                       : menuHeaderTrailingItems}
@@ -641,7 +641,7 @@ const MenuTitle = styled('span')`
 
 const StyledLoadingIndicator = styled(LoadingIndicator)`
   && {
-    margin: 0 ${space(0.5)} 0 ${space(1)};
+    margin: 0;
   }
 `;
 
