ed4005b12bb83adeb580bb8d1dd126a75441cf80
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-04-09 09:21:46 -0700

    feat(replay): Clarify in search typeahead that viewed_by_me and seen_by_me are aliases

--- static/app/utils/fields/index.ts
+++ static/app/utils/fields/index.ts
@@ -2256,7 +2256,9 @@ const REPLAY_FIELD_DEFINITIONS: Record<ReplayFieldKey, FieldDefinition> = {
     valueType: FieldValueType.STRING,
   },
   [ReplayFieldKey.SEEN_BY_ME]: {
-    desc: t('Whether you have seen this replay before (true/false)'),
+    desc: t(
+      'Whether you have seen this replay before. Alias of viewed_by_me. (true/false)'
+    ),
     kind: FieldKind.FIELD,
     valueType: FieldValueType.BOOLEAN,
   },
@@ -2271,7 +2273,7 @@ const REPLAY_FIELD_DEFINITIONS: Record<ReplayFieldKey, FieldDefinition> = {
     valueType: FieldValueType.STRING,
   },
   [ReplayFieldKey.VIEWED_BY_ME]: {
-    desc: t('Whether you have seen this replay before (true/false)'),
+    desc: t('Whether you have seen this replay before. Alias of seen_by_me (true/false)'),
     kind: FieldKind.FIELD,
     valueType: FieldValueType.BOOLEAN,
   },
