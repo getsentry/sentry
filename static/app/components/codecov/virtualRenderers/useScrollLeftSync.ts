b0392098ee872e5f081ee7010cf4705c626d04d6
nicholas-codecov <nicholas.deschenes@sentry.io>
2025-04-09 09:37:27 -0300

    :sparkles: Add in useScrollLeftSync hook

---
+++ static/app/components/codecov/virtualRenderers/useScrollLeftSync.ts
@@ -0,0 +1,42 @@
+import {useLayoutEffect} from 'react';
+
+interface UseScrollLeftSyncArgs {
+  refsToSync: Array<React.RefObject<HTMLElement | null>>;
+  scrollingRef: React.RefObject<HTMLElement | null>;
+}
+
+// this hook syncs the scroll position of the scrollingRef with the refsToSync
+export const useScrollLeftSync = ({scrollingRef, refsToSync}: UseScrollLeftSyncArgs) => {
+  // this effect syncs the scroll position of the scrollingRef with the refsToSync
+  useLayoutEffect(() => {
+    // if the scrollingRef is not available, return
+    if (!scrollingRef.current) {
+      return undefined;
+    }
+
+    // clone the scrollingRef so we can use it in the cleanup function
+    const clonedScrollingRef = scrollingRef.current;
+
+    const onScroll = () => {
+      // if there are no refs to sync, return
+      if (refsToSync.length === 0) {
+        return;
+      }
+
+      // sync the scroll position of the scrollingRef with the refsToSync
+      refsToSync.forEach(ref => {
+        if (clonedScrollingRef && ref.current) {
+          ref.current.scrollLeft = clonedScrollingRef.scrollLeft;
+        }
+      });
+    };
+
+    // add the scroll event listener
+    clonedScrollingRef.addEventListener('scroll', onScroll, {passive: true});
+
+    return () => {
+      // remove the scroll event listener
+      clonedScrollingRef.removeEventListener('scroll', onScroll);
+    };
+  }, [scrollingRef, refsToSync]);
+};
