d8676d8821ab992bbb109b08486eca8d386f99c3
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2025-04-10 12:04:16 -0400

    feat(insights): create table to store user based starred transactions (#89203)
    We're creating a new feature in insights overview, which allows a user
    to favourite transactions (also known as segments).
    We already have the ability to favourite things at a team level, but we
    will be replacing the team level favouriting to be user level.
    
    This Pr
    1. Creates a new model for starred segment
    2. Generates appropriate migration
    
    After this PR we will create an endpoint to toggle starred transaction
    1. When toggled on, we add a new entry to the table
    2. When toggled off, we delete the entry in the table
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- src/sentry/backup/comparators.py
+++ src/sentry/backup/comparators.py
@@ -928,6 +928,9 @@ def get_default_comparators() -> dict[str, list[JSONScrubbingComparator]]:
             "explore.exploresavedquerystarred": [
                 DateUpdatedComparator("date_updated", "date_added")
             ],
+            "insights.insightsstarredsegment": [
+                DateUpdatedComparator("date_updated", "date_added")
+            ],
             "monitors.monitor": [UUID4Comparator("guid")],
         },
     )
