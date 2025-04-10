8914f78b4770417fd8596de2256670717836f568
Adrian <adrian@codecov.io>
2025-04-10 12:07:36 -0600

    feat(codecov): Add DateSelector and DatePicker Codecov components (#89117)
    Adding components that help choose dates for Codecov components. This is
    a component inspired from the `DatePageFilter` and `TimeRangeSelector`
    components. It takes the functionality it needs from those classes and
    creates custom classes for Codecov purposes. These are capable of saving
    data to local storage as well as adding URL query params based on date
    selection. These also only support 1d, 7d and 30d parameters, as per
    Codecov timeseries backend data.
    
    Check it out [here](https://sentry-49loqwkbu.sentry.dev/codecov/tests/)
    
    ### Legal Boilerplate
    
    Look, I get it. The entity doing business as "Sentry" was incorporated
    in the State of Delaware in 2015 as Functional Software, Inc. and is
    gonna need some rights from me in order to utilize my contributions in
    this here PR. So here's the deal: I retain all rights, title and
    interest in and to my contributions, and by keeping this boilerplate
    intact I confirm that Sentry can use, modify, copy, and redistribute my
    contributions, under Sentry's choice of terms.
    
    ---------
    
    Co-authored-by: nicholas-codecov <nicholas.deschenes@sentry.io>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/views/codecov/tests/tests.tsx
+++ static/app/views/codecov/tests/tests.tsx
@@ -1,11 +1,28 @@
 import styled from '@emotion/styled';
 
+import {DatePicker} from 'sentry/components/codecov/datePicker';
+import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
+import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import {space} from 'sentry/styles/space';
+
+export const DEFAULT_CODECOV_DATETIME_SELECTION = {
+  start: null,
+  end: null,
+  utc: false,
+  period: '24h',
+};
 
 export default function TestsPage() {
   return (
     <LayoutGap>
       <p>Test Analytics</p>
+      <PageFiltersContainer
+        defaultSelection={{datetime: DEFAULT_CODECOV_DATETIME_SELECTION}}
+      >
+        <PageFilterBar condensed>
+          <DatePicker />
+        </PageFilterBar>
+      </PageFiltersContainer>
     </LayoutGap>
   );
 }
