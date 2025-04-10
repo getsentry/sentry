dd19754c7723ffb457e6fb8bd5ff7c8efe2a09af
nicholas-codecov <nicholas.deschenes@sentry.io>
2025-04-10 13:16:29 -0300

    :bulb: Clean up comments

--- static/app/components/codecov/virtualRenderers/useIsOverflowing.ts
+++ static/app/components/codecov/virtualRenderers/useIsOverflowing.ts
@@ -7,33 +7,26 @@ import {useEffect, useState} from 'react';
  * @returns Whether the element is overflowing
  */
 export const useIsOverflowing = (ref: React.RefObject<HTMLDivElement | null>) => {
-  // keep track of whether the element is overflowing
   const [isOverflowing, setIsOverflowing] = useState(false);
 
   useEffect(() => {
-    // if the ref is not available, return
     if (!ref.current) {
       return undefined;
     }
 
-    // create a resize observer to watch for changes in the element's size
     const resizeObserver = new ResizeObserver(entries => {
       const entry = entries?.[0];
       if (entry) {
-        // if the element is overflowing, set the state
         setIsOverflowing(entry.target.scrollWidth > entry.target.clientWidth);
       }
     });
 
-    // observe the element
     resizeObserver.observe(ref.current);
 
     return () => {
-      // disconnect the resize observer
       resizeObserver.disconnect();
     };
   }, [ref]);
 
-  // return whether the element is overflowing
   return isOverflowing;
 };
