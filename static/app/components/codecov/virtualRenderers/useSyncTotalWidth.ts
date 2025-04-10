bcaff7710f34d88bc89a081f505bc5ed67f45e35
nicholas-codecov <nicholas.deschenes@sentry.io>
2025-04-10 11:18:12 -0300

    :necktie: Update useSyncTotalWidth logic so we loop over all resize observer entries

--- static/app/components/codecov/virtualRenderers/useSyncTotalWidth.ts
+++ static/app/components/codecov/virtualRenderers/useSyncTotalWidth.ts
@@ -1,32 +1,36 @@
 import {useLayoutEffect} from 'react';
 
-interface UseSyncTotalWidthArgs {
-  textAreaRef: React.RefObject<HTMLTextAreaElement | null>;
-  widthDivRef: React.RefObject<HTMLDivElement | null>;
-}
+type HtmlElementRef = React.RefObject<HTMLElement | null>;
 
-export const useSyncTotalWidth = ({textAreaRef, widthDivRef}: UseSyncTotalWidthArgs) => {
-  // this effect sets an empty div to the width of the text area so the code highlight div can scroll horizontally
+/**
+ * This hook syncs the total width of the base element with the width of the target
+ * element.
+ * @param baseRef - The ref of the base element.
+ * @param targetRef - The ref of the target element.
+ * @returns The total width of the base element.
+ */
+export const useSyncTotalWidth = (baseRef: HtmlElementRef, targetRef: HtmlElementRef) => {
   useLayoutEffect(() => {
     // if the text area or width div ref is not available, return
-    if (!textAreaRef.current || !widthDivRef.current) {
+    if (!baseRef.current || !targetRef.current) {
       return undefined;
     }
 
     // create a resize observer to watch the text area for changes in width so once the text area is resized, the width div can be updated
     const resize = new ResizeObserver(entries => {
-      const entry = entries?.[0];
-      if (widthDivRef.current && entry) {
-        widthDivRef.current.style.width = `${entry.target.scrollWidth}px`;
-      }
+      entries.forEach(entry => {
+        if (targetRef.current && entry) {
+          targetRef.current.style.width = `${entry.target.scrollWidth}px`;
+        }
+      });
     });
 
     // observe the text area for changes
-    resize.observe(textAreaRef.current);
+    resize.observe(baseRef.current);
 
     return () => {
       // disconnect the resize observer
       resize.disconnect();
     };
-  }, [textAreaRef, widthDivRef]);
+  }, [baseRef, targetRef]);
 };
