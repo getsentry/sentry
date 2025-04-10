ff8b3c5f291d0c68ee9623eb0bf06c9ef03e456a
Nate Moore <nate.moore@sentry.io>
2025-04-09 22:18:46 -0500

    feat(aci): add form field hooks

---
+++ static/app/components/workflowEngine/form/hooks.tsx
@@ -0,0 +1,47 @@
+import {useContext, useEffect, useMemo, useState} from 'react';
+
+import FormContext from 'sentry/components/forms/formContext';
+import type FormModel from 'sentry/components/forms/model';
+import {MockModel} from 'sentry/components/forms/model';
+import type {FieldValue} from 'sentry/components/forms/types';
+
+export function useFormFields() {
+  const context = useContext(FormContext);
+  const [model] = useState<FormModel>(() => context.form ?? (new MockModel({}) as any));
+  const [state, forceUpdate] = useState({});
+  useEffect(() => {
+    const prevHook = model.options.onFieldChange;
+    model.setFormOptions({
+      onFieldChange: (id, value) => {
+        forceUpdate({});
+        prevHook?.(id, value);
+      },
+    });
+  }, [model]);
+
+  return useMemo(() => {
+    noop(state);
+    return model.getData();
+  }, [state, model]);
+}
+
+const noop = (_: any) => {};
+
+export function useFormField<Value extends FieldValue = FieldValue>(field: string) {
+  const context = useContext(FormContext);
+  const [model] = useState<FormModel>(() => context.form ?? (new MockModel({}) as any));
+  const [value, setValue] = useState<Value | undefined>(() => model.getValue(field));
+  useEffect(() => {
+    const prevHook = model.options.onFieldChange;
+    model.setFormOptions({
+      onFieldChange: (id, newValue) => {
+        if (id === field) {
+          setValue(newValue as Value);
+        }
+        prevHook?.(id, newValue);
+      },
+    });
+  }, [model, field]);
+
+  return value;
+}
