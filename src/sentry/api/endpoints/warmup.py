2b2fa03baa2fb55b861552ae29c6df503f9e17c1
Joshua Ferge <josh.ferge@sentry.io>
2025-04-10 12:54:12 -0400

    ref(warmup): use __import__ inside the endpoint

--- src/sentry/api/endpoints/warmup.py
+++ src/sentry/api/endpoints/warmup.py
@@ -1,17 +1,9 @@
-import django.db.models.sql.compiler  # NOQA
 from django.conf import settings
 from django.urls import reverse
 from django.utils import translation
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-import sentry.identity.services.identity.impl  # NOQA
-import sentry.integrations.services.integration.impl  # NOQA
-import sentry.middleware.integrations.parsers.plugin  # NOQA
-import sentry.notifications.services.impl  # NOQA
-import sentry.sentry_apps.services.app.impl  # NOQA
-import sentry.users.services.user.impl  # NOQA
-import sentry.users.services.user_option.impl  # NOQA
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import Endpoint, all_silo_endpoint
@@ -28,6 +20,16 @@ class WarmupEndpoint(Endpoint):
     rate_limits = RateLimitConfig(group="INTERNAL")
 
     def get(self, request: Request) -> Response:
+        # Warm up imports
+        __import__("django.db.models.sql.compiler")
+        __import__("sentry.identity.services.identity.impl")
+        __import__("sentry.integrations.services.integration.impl")
+        __import__("sentry.middleware.integrations.parsers.plugin")
+        __import__("sentry.notifications.services.impl")
+        __import__("sentry.sentry_apps.services.app.impl")
+        __import__("sentry.users.services.user.impl")
+        __import__("sentry.users.services.user_option.impl")
+
         languages = [lang for lang, _ in settings.LANGUAGES]
         languages.append(settings.LANGUAGE_CODE)
 
