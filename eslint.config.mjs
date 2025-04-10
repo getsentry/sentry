338db13de3a20311b6a309a8e99d5feaea2af2b9
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-04-10 12:45:51 -0700

    set the rule to error

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -372,7 +372,7 @@ export default typescript.config([
     plugins: {'no-relative-import-paths': noRelativeImportPaths},
     rules: {
       'no-relative-import-paths/no-relative-import-paths': [
-        'warn', // TODO(ryan953): Fix violations and enable this rule,
+        'error',
         {
           prefix: 'sentry',
           rootDir: 'static/app',
@@ -856,7 +856,7 @@ export default typescript.config([
     files: ['static/gsApp/**/*.{js,mjs,ts,jsx,tsx}'],
     rules: {
       'no-relative-import-paths/no-relative-import-paths': [
-        'warn', // TODO(ryan953): Fix violations and enable this rule,
+        'error',
         {
           prefix: 'getsentry',
           rootDir: 'static/gsApp',
@@ -892,7 +892,7 @@ export default typescript.config([
     files: ['static/gsAdmin/**/*.{js,mjs,ts,jsx,tsx}'],
     rules: {
       'no-relative-import-paths/no-relative-import-paths': [
-        'warn', // TODO(ryan953): Fix violations and enable this rule,
+        'error',
         {
           prefix: 'admin',
           rootDir: 'static/gsAdmin',
