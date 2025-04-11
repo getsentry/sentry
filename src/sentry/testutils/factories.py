14d920fb7dfd2ece464dbe9aafd52ef3af7b6875
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-04-11 12:36:26 -0700

    feat(webhooks): only dispatch webhooks for a (configurable) subset of projects (#85502)
    This enables limiting integration's webhooks to fire only for a subset
    of projects, which would solve
    https://github.com/getsentry/sentry/issues/83688
    
    We make sure to skip early on before payload is serialized.
    
    No UI planned at this stage
    
    # Discussion
    
    https://sentry.slack.com/archives/CD4NYFD34/p1739558637148289?thread_ts=1738876805.029239&cid=CD4NYFD34
    
    # Testing
    
    re-tested after rebase
    
    ```
    export P2_DSN=http://0f915a5c0d2b46cec210c3b0f9713e3e@127.0.0.1:8000/2
    export P3_DSN=http://c02b3df68ba1c97abf62aa7d8cea6417@127.0.0.1:8000/3
    export P4_DSN=http://5dc655350751803e0ec57860e83245ac@127.0.0.1:8000/4
    
    python3 http_server.py | grep -o '"project":[[:space:]]*[0-9]\+'
    ngrok http 8077
    
    open http://127.0.0.1:8000/settings/testorg/developer-settings/actual-test-integration-548f00/
    # > plug in ngrok URL as webhook url
    # > check 'error:created'
    # > click SAVE
    
    scurl 'http://127.0.0.1:8000/api/0/sentry-app-installations/bde8f035-832e-4e09-a0c7-e0980d499cdd/service-hook-projects/' -X DELETE
    scurl 'http://127.0.0.1:8000/api/0/sentry-app-installations/bde8f035-832e-4e09-a0c7-e0980d499cdd/service-hook-projects/'
    # > verify ^ returns []
    
    SENTRY_DSN=$P4_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    SENTRY_DSN=$P3_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    SENTRY_DSN=$P2_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    
    # > Verify http server outputs:
    # "project":4
    # "project":3
    # "project":2
    
    # if needed check events are ingested at http://127.0.0.1:8000/organizations/testorg/issues/?project=2&query=&referrer=issue-list&statsPeriod=4h
    # if ngrok/http_server having issues can also check http://127.0.0.1:8000/settings/testorg/developer-settings/actual-test-integration-548f00/dashboard/
    
    scurl 'http://127.0.0.1:8000/api/0/sentry-app-installations/bde8f035-832e-4e09-a0c7-e0980d499cdd/service-hook-projects/' -X POST -H "Content-Type: application/json" -d '{"projects": ["p2", "p4"]}'
    scurl 'http://127.0.0.1:8000/api/0/sentry-app-installations/bde8f035-832e-4e09-a0c7-e0980d499cdd/service-hook-projects/'
    # > verify ^ returns [{..., "project_id": 2}, {..., "project_id": 4}]
    
    SENTRY_DSN=$P2_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    SENTRY_DSN=$P3_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    SENTRY_DSN=$P4_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    
    # > Verify http server outputs:
    # "project":2
    # "project":4
    
    scurl 'http://127.0.0.1:8000/api/0/sentry-app-installations/bde8f035-832e-4e09-a0c7-e0980d499cdd/service-hook-projects/' -X POST -H "Content-Type: application/json" -d '{"projects": ["p4"]}'
    
    SENTRY_DSN=$P2_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    SENTRY_DSN=$P3_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    SENTRY_DSN=$P4_DSN sentry-cli send-envelope --raw ~/home/empower-mocks-public/3/5
    
    # > Verify http server outputs:
    # "project":4
    ```
    
    ---------
    
    Co-authored-by: Christinarlong <60594860+Christinarlong@users.noreply.github.com>
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>
    Co-authored-by: Christinarlong <christina.long@sentry.io>

--- src/sentry/testutils/factories.py
+++ src/sentry/testutils/factories.py
@@ -1387,7 +1387,13 @@ class Factories:
 
     @staticmethod
     @assume_test_silo_mode(SiloMode.REGION)
-    def create_service_hook(actor=None, org=None, project=None, events=None, url=None, **kwargs):
+    def create_service_hook(
+        actor=None, org=None, project=None, events=None, url=None, project_ids=None, **kwargs
+    ):
+        if project:
+            if project_ids is not None:
+                raise ValueError("Cannot provide both project and project_ids")
+            project_ids = [project.id]
         if not actor:
             actor = Factories.create_user()
         if not org:
@@ -1395,8 +1401,8 @@ class Factories:
                 org = project.organization
             else:
                 org = Factories.create_organization(owner=actor)
-        if not project:
-            project = Factories.create_project(organization=org)
+        if project_ids is None:  # empty list for project_ids is valid and means no project filter
+            project_ids = [Factories.create_project(organization=org).id]
         if events is None:
             events = ["event.created"]
         if not url:
@@ -1413,7 +1419,7 @@ class Factories:
             actor_id=actor.id,
             installation_id=installation_id,
             organization_id=org.id,
-            project_ids=[project.id],
+            project_ids=project_ids,
             events=events,
             url=url,
         ).id
