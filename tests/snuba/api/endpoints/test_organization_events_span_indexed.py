f01aea7be9bd6d754adb4883a691aae3325bb8ee
Shruthi <shruthilaya.jaganathan@sentry.io>
2025-04-11 14:12:39 -0400

    feat(explore): add public aliasing for newly exposed searchable fields (#89392)

--- tests/snuba/api/endpoints/test_organization_events_span_indexed.py
+++ tests/snuba/api/endpoints/test_organization_events_span_indexed.py
@@ -2851,6 +2851,38 @@ class OrganizationEventsEAPRPCSpanEndpointTest(OrganizationEventsEAPSpanEndpoint
         assert data[1]["cache.hit"] is True
         assert meta["dataset"] == self.dataset
 
+    def test_searchable_contexts(self):
+        self.store_spans(
+            [
+                self.create_span(
+                    {"sentry_tags": {"device.model": "Apple"}},
+                    start_ts=self.ten_mins_ago,
+                ),
+                self.create_span(
+                    {"sentry_tags": {"request.url": "https://sentry/api/0/foo"}},
+                    start_ts=self.ten_mins_ago,
+                ),
+            ],
+            is_eap=self.is_eap,
+        )
+
+        response = self.do_request(
+            {
+                "field": ["device.model", "request.url"],
+                "project": self.project.id,
+                "dataset": self.dataset,
+                "orderby": "device.model",
+            }
+        )
+
+        assert response.status_code == 200, response.content
+        data = response.data["data"]
+        meta = response.data["meta"]
+        assert len(data) == 2
+        assert data[0]["device.model"] == "Apple"
+        assert data[1]["request.url"] == "https://sentry/api/0/foo"
+        assert meta["dataset"] == self.dataset
+
     def test_trace_status_rate(self):
         statuses = ["unknown", "internal_error", "unauthenticated", "ok", "ok"]
         self.store_spans(
