137f43440047ed4057e245d58df4db641edb81e4
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-04-10 17:11:30 -0700

    more __init__ file improvements

--- src/sentry/workflow_engine/__init__.py
+++ src/sentry/workflow_engine/__init__.py
@@ -1,9 +1,13 @@
 __all__ = [
+    # Data Models
+    "Action",
     "DataCondition",
     "DataPacket",
     "DataSource",
     "Detector",
+    "StatefulDetectorHandler",
     "Workflow",
+    # Processing Methods
     "process_data_packets",
     "process_data_sources",
     "process_detectors",
@@ -16,7 +20,9 @@ __all__ = [
     "DataConditionAlertRuleTrigger",
 ]
 
+from sentry.workflow_engine.handlers import StatefulDetectorHandler
 from sentry.workflow_engine.models import (
+    Action,
     ActionAlertRuleTriggerAction,
     AlertRuleDetector,
     AlertRuleWorkflow,
