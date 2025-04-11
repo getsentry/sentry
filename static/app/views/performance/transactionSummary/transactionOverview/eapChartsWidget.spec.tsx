43821285f831597f47e3af8eca2f9846c7ce15a6
Ash <0Calories@users.noreply.github.com>
2025-04-11 15:18:14 -0400

    feat(perf): Persist selected chart in query param (#89311)
    Allows the currently selected chart in the new transaction summary to
    persist by storing it as a URL parameter. This will allow the chart to
    remain across refreshes and also when clicking into a link with the
    query param added.

---
+++ static/app/views/performance/transactionSummary/transactionOverview/eapChartsWidget.spec.tsx
@@ -0,0 +1,56 @@
+import {LocationFixture} from 'sentry-fixture/locationFixture';
+
+import {render, screen, waitFor} from 'sentry-test/reactTestingLibrary';
+
+import {useLocation} from 'sentry/utils/useLocation';
+
+import {EAPChartsWidget} from './eapChartsWidget';
+
+jest.mock('sentry/utils/useLocation');
+
+const mockUseLocation = useLocation as jest.MockedFunction<typeof useLocation>;
+
+describe('EAPChartsWidget', function () {
+  const transactionName = 'test-transaction';
+
+  beforeEach(() => {
+    MockApiClient.clearMockResponses();
+    mockUseLocation.mockImplementation(() => LocationFixture());
+
+    MockApiClient.addMockResponse({
+      url: '/organizations/org-slug/releases/stats/',
+      body: [],
+      status: 200,
+    });
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+    MockApiClient.clearMockResponses();
+  });
+
+  it('shows default widget type when no query param is provided', async function () {
+    render(<EAPChartsWidget transactionName={transactionName} />);
+
+    await waitFor(() => {
+      expect(screen.getByText('Duration Breakdown')).toBeInTheDocument();
+    });
+  });
+
+  it('shows default widget when invalid query param is provided', async function () {
+    mockUseLocation.mockImplementation(() =>
+      LocationFixture({
+        query: {
+          chartDisplay: 'some_widget_that_does_not_exist',
+        },
+        pathname: '/test',
+      })
+    );
+
+    render(<EAPChartsWidget transactionName={transactionName} />);
+
+    await waitFor(() => {
+      expect(screen.getByText('Duration Breakdown')).toBeInTheDocument();
+    });
+  });
+});
