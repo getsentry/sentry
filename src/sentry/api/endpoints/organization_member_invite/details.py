f32825d6746e1236592b623bc5e582bdfb4f53f3
Michelle Fu <michelle.fu@sentry.io>
2025-04-11 11:32:34 -0700

    rebase

--- src/sentry/api/endpoints/organization_member_invite/details.py
+++ src/sentry/api/endpoints/organization_member_invite/details.py
@@ -8,9 +8,9 @@ from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases.organization import OrganizationEndpoint
-from sentry.api.endpoints.organization_member import get_allowed_org_roless
-from sentry.api.endpoints.organization_member_invite.utils import MemberInviteDetailsPermission
+from sentry.api.endpoints.organization_member import get_allowed_org_roles
 from sentry.api.endpoints.organization_member_invite import MISSING_FEATURE_MESSAGE
+from sentry.api.endpoints.organization_member_invite.utils import MemberInviteDetailsPermission
 from sentry.api.exceptions import ResourceDoesNotExist
 from sentry.api.serializers import serialize
 from sentry.api.serializers.rest_framework.organizationmemberinvite import (
