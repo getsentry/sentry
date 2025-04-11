adce220de2fb266ffbd24d005c9a496174368d14
Vjeran Grozdanić <vjeran.grozdanic@sentry.io>
2025-04-11 10:40:09 +0200

    chore(rollbacks): Remove options for GroupRelease model (#89371)
    The code that was using this option is now refactored, this can now
    safely be removed

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -2803,14 +2803,6 @@ register(
     flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
 )
 
-# Killswitch for GroupRelease.get_or_create refactor
-register(
-    "grouprelease.new_get_or_create.rollout",
-    default=0.0,
-    type=Float,
-    flags=FLAG_AUTOMATOR_MODIFIABLE,
-)
-
 # Killswitch for Postgres query timeout error handling
 register(
     "api.postgres-query-timeout-error-handling.enabled",
