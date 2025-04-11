a237bff93d5bac45834a6af52196b2eb00dab267
Billy Vong <billyvg@users.noreply.github.com>
2025-04-11 13:05:26 -0400

    ref(insights): Combine data loading + widget rendering for Insights -> Http pages (#89325)
    As part of an effort to be able to [deeplink to charts via the Releases
    Drawer](https://github.com/getsentry/sentry/issues/88560), we need to
    move data fetching + chart rendering into a self-contained component
    (with a unique id).
    
    This refactors the widgets on `httpLandingPage` and
    `httpDomainSummaryPage` so that we get an idea of what this will look
    like before continuing on to do others.

---
+++ static/app/views/insights/common/components/widgets/httpDomainSummaryThroughputChartWidget.tsx
@@ -0,0 +1,32 @@
+import {MutableSearch} from 'sentry/utils/tokenizeSearch';
+import {InsightsLineChartWidget} from 'sentry/views/insights/common/components/insightsLineChartWidget';
+import {useHttpDomainSummaryChartFilter} from 'sentry/views/insights/common/components/widgets/hooks/useHttpDomainSummaryChartFilter';
+import {useSpanMetricsSeries} from 'sentry/views/insights/common/queries/useDiscoverSeries';
+import {getThroughputChartTitle} from 'sentry/views/insights/common/views/spans/types';
+import {Referrer} from 'sentry/views/insights/http/referrers';
+
+export default function HttpDomainSummaryThroughputChartWidget() {
+  const chartFilters = useHttpDomainSummaryChartFilter();
+  const {
+    isPending: isThroughputDataLoading,
+    data: throughputData,
+    error: throughputError,
+  } = useSpanMetricsSeries(
+    {
+      search: MutableSearch.fromQueryObject(chartFilters),
+      yAxis: ['epm()'],
+      transformAliasToInputFormat: true,
+    },
+    Referrer.DOMAIN_SUMMARY_THROUGHPUT_CHART
+  );
+
+  return (
+    <InsightsLineChartWidget
+      id="httpDomainSummaryThroughputChartWidget"
+      title={getThroughputChartTitle('http')}
+      series={[throughputData['epm()']]}
+      isLoading={isThroughputDataLoading}
+      error={throughputError}
+    />
+  );
+}
