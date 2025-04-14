bf30e4fb7d900d665319105f5e40fc2c7b5168b7
Kosty Maleyev <kosty.maleyev@sentry.io>
2025-04-14 12:05:26 -0700

    Unrevert webhooks only dispatch for subset of projects (#89521)
    un-reverting https://github.com/getsentry/sentry/pull/85502 which was
    reverted out of abundance of caution
    + switch `logger.error` -> `logger.info` when `actor_id` and
    `installation_id` don't match
    
    # Context
    
    **christina**
    Looked into the installation_id & actor_id mismatch and all the affected
    hooks had the installation_id under the actor_id , and the hook's
    installation_id property would be null causing the mismatch.
    That's probably why the original code also looked up by actor_id
    :bufo-melting: . Imo, I think we're safe then, and downgrade the log
    level info. I have a ticket on my backlog to backfill servicehooks with
    similar issues so it would be nice if we kept track of what hooks are in
    a weird state. (edited)
    **Kosty**
     yeah this should have been a warn/info
    we are still using actor_id like before, that didn’t change, so should
    be safe
    :bufo-thumbsup:
    1
    
    **dan**
    cool, we can revert the revert and just change the log and merge early
    monday then

---
+++ src/sentry/sentry_apps/api/serializers/servicehookproject.py
@@ -0,0 +1,11 @@
+from sentry.api.serializers import Serializer, register
+from sentry.sentry_apps.models.servicehook import ServiceHookProject
+
+
+@register(ServiceHookProject)
+class ServiceHookProjectSerializer(Serializer):
+    def serialize(self, obj, attrs, user, **kwargs):
+        return {
+            "id": str(obj.id),
+            "project_id": str(obj.project_id),
+        }
