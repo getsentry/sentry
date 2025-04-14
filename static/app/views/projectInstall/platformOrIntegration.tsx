9bd5dbdfe38cf6fec90962724b5f24016417e6da
Ogi <ognjen.bostjancic@gmail.com>
2025-04-14 13:46:50 +0200

    feat(getting-started): awslambda (#89377)

--- static/app/views/projectInstall/platformOrIntegration.tsx
+++ static/app/views/projectInstall/platformOrIntegration.tsx
@@ -1,21 +1,16 @@
-import {useState} from 'react';
-
 import {OnboardingContextProvider} from 'sentry/components/onboarding/onboardingContext';
 import allPlatforms from 'sentry/data/platforms';
 import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
-import {platformToIntegrationMap} from 'sentry/utils/integrationUtil';
 import useOrganization from 'sentry/utils/useOrganization';
 import useProjects from 'sentry/utils/useProjects';
 
 import GettingStarted from './gettingStarted';
 import {ProjectInstallPlatform} from './platform';
-import {PlatformIntegrationSetup} from './platformIntegrationSetup';
 
 type Props = RouteComponentProps<{projectId: string}>;
 
 function PlatformOrIntegration({params}: Props) {
   const organization = useOrganization();
-  const [integrationUseManualSetup, setIntegrationUseManualSetup] = useState(false);
 
   const {projects, initiallyLoaded} = useProjects({
     slugs: [params.projectId],
@@ -29,23 +24,6 @@ function PlatformOrIntegration({params}: Props) {
 
   const currentPlatformKey = project?.platform ?? 'other';
   const currentPlatform = allPlatforms.find(p => p.id === currentPlatformKey);
-
-  const integrationSlug: string | undefined =
-    // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-    platformToIntegrationMap[currentPlatformKey];
-  const showIntegrationOnboarding = integrationSlug && !integrationUseManualSetup;
-
-  if (showIntegrationOnboarding) {
-    return (
-      <PlatformIntegrationSetup
-        integrationSlug={integrationSlug}
-        onClickManualSetup={() => setIntegrationUseManualSetup(true)}
-        project={project}
-        platform={currentPlatform}
-        loading={loadingProjects}
-      />
-    );
-  }
 
   return (
     <OnboardingContextProvider>
