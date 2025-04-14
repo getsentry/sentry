03be00c5ae1944aea109feb4cad0b86a8c5776de
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2025-04-14 14:50:06 -0400

    feat(insights): add table cell for starred transaction (#89384)
    Adds a new table cell for a starred transaction, this is a v0 version of
    this feature. This is to support to user based starred transactions in
    EAP. This mainly scaffolds the component, test, styling update and usage
    of this cell will come in future PRs.
    
    This feature is dependant on this API change
    https://github.com/getsentry/sentry/pull/89330

---
+++ static/app/views/insights/common/utils/useStarredSegment.tsx
@@ -0,0 +1,80 @@
+import {useState} from 'react';
+
+import {
+  addErrorMessage,
+  addLoadingMessage,
+  addSuccessMessage,
+} from 'sentry/actionCreators/indicator';
+import {t} from 'sentry/locale';
+import {useMutation} from 'sentry/utils/queryClient';
+import useApi from 'sentry/utils/useApi';
+import useOrganization from 'sentry/utils/useOrganization';
+
+type StarTransactionParams = {
+  project_id: string;
+  segment_name: string;
+};
+
+const URL_PREFIX = '/insights/starred-segments/';
+
+interface Props {
+  initialIsStarred: boolean;
+  projectSlug: string;
+  segmentName: string;
+}
+
+export function useStarredSegment({initialIsStarred, projectSlug, segmentName}: Props) {
+  const [isStarred, setIsStarred] = useState(initialIsStarred);
+  const organization = useOrganization();
+  const api = useApi();
+
+  const url = `/organizations/${organization.slug}${URL_PREFIX}`;
+  const data: StarTransactionParams = {
+    project_id: projectSlug,
+    segment_name: segmentName,
+  };
+
+  const onError = () => {
+    addErrorMessage(t('Failed to star transaction'));
+    setIsStarred(!isStarred);
+  };
+
+  const onSuccess = () => {
+    addSuccessMessage(t('Transaction starred'));
+  };
+
+  const {mutate: starTransaction, ...starTransactionResult} = useMutation({
+    mutationFn: () => api.requestPromise(url, {method: 'POST', data}),
+    onSuccess,
+    onError,
+  });
+
+  const {mutate: unstarTransaction, ...unstarTransactionResult} = useMutation({
+    mutationFn: () => api.requestPromise(url, {method: 'DELETE', data}),
+    onSuccess,
+    onError,
+  });
+
+  const isPending = starTransactionResult.isPending || unstarTransactionResult.isPending;
+
+  const toggleStarredTransaction = () => {
+    if (isPending) {
+      return;
+    }
+
+    addLoadingMessage();
+
+    if (isStarred) {
+      unstarTransaction();
+    } else {
+      starTransaction();
+    }
+    setIsStarred(!isStarred);
+  };
+
+  return {
+    toggleStarredTransaction,
+    isPending,
+    isStarred,
+  };
+}
