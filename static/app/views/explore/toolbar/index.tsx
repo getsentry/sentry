72965034a40a4fdd3e482aab9ce1cfb83868b2ed
Tony Xiao <txiao@sentry.io>
2025-04-14 11:27:43 -0400

    chore(explore): Remove dataset toolbar from explore (#89490)
    The dataset toolbar is no longer needed so removing it here.

--- static/app/views/explore/toolbar/index.tsx
+++ static/app/views/explore/toolbar/index.tsx
@@ -2,18 +2,15 @@ import styled from '@emotion/styled';
 
 import {defined} from 'sentry/utils';
 import {
-  useExploreDataset,
   useExploreFields,
   useExploreGroupBys,
   useExploreMode,
   useExploreSortBys,
   useExploreVisualizes,
-  useSetExploreDataset,
   useSetExploreMode,
   useSetExploreSortBys,
 } from 'sentry/views/explore/contexts/pageParamsContext';
 import {Mode} from 'sentry/views/explore/contexts/pageParamsContext/mode';
-import {ToolbarDataset} from 'sentry/views/explore/toolbar/toolbarDataset';
 import {ToolbarGroupBy} from 'sentry/views/explore/toolbar/toolbarGroupBy';
 import {ToolbarMode} from 'sentry/views/explore/toolbar/toolbarMode';
 import {ToolbarSaveAs} from 'sentry/views/explore/toolbar/toolbarSaveAs';
@@ -21,7 +18,7 @@ import {ToolbarSortBy} from 'sentry/views/explore/toolbar/toolbarSortBy';
 import {ToolbarSuggestedQueries} from 'sentry/views/explore/toolbar/toolbarSuggestedQueries';
 import {ToolbarVisualize} from 'sentry/views/explore/toolbar/toolbarVisualize';
 
-type Extras = 'dataset toggle' | 'equations' | 'tabs';
+type Extras = 'equations' | 'tabs';
 
 interface ExploreToolbarProps {
   extras?: Extras[];
@@ -29,8 +26,6 @@ interface ExploreToolbarProps {
 }
 
 export function ExploreToolbar({extras, width}: ExploreToolbarProps) {
-  const dataset = useExploreDataset();
-  const setDataset = useSetExploreDataset();
   const mode = useExploreMode();
   const setMode = useSetExploreMode();
   const fields = useExploreFields();
@@ -41,9 +36,6 @@ export function ExploreToolbar({extras, width}: ExploreToolbarProps) {
 
   return (
     <Container width={width}>
-      {extras?.includes('dataset toggle') && (
-        <ToolbarDataset dataset={dataset} setDataset={setDataset} />
-      )}
       {!extras?.includes('tabs') && <ToolbarMode mode={mode} setMode={setMode} />}
       <ToolbarVisualize equationSupport={extras?.includes('equations')} />
       {(extras?.includes('tabs') || mode === Mode.AGGREGATE) && <ToolbarGroupBy />}
