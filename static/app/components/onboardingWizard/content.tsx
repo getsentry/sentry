aaea03ef537563722e1da44088611799ca7a163c
Ogi <ognjen.bostjancic@gmail.com>
2025-04-14 14:46:19 +0200

    feat(demo-mode): releases tour (#89271)

--- static/app/components/onboardingWizard/content.tsx
+++ static/app/components/onboardingWizard/content.tsx
@@ -277,6 +277,8 @@ function Task({task, hidePanel, showWaitingIndicator}: TaskProps) {
         // Performance guide is updated to use the new tour
         if (task.task === OnboardingTaskKey.PERFORMANCE_GUIDE) {
           tours?.[DemoTour.PERFORMANCE]?.startTour(DemoTourStep.PERFORMANCE_TABLE);
+        } else if (task.task === OnboardingTaskKey.RELEASE_GUIDE) {
+          tours?.[DemoTour.RELEASES]?.startTour();
         } else {
           DemoWalkthroughStore.activateGuideAnchor(task.task);
         }
