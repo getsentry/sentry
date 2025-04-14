b6a0e84a3011f382feb679a9499e57709c5bb2ee
Vjeran Grozdanić <vjeran.grozdanic@sentry.io>
2025-04-14 13:55:27 +0200

    feat(nextjs): Add prefersNextjsInsightsOverview user option (#89473)
    Adds new option that will be used for NextJs insight page

--- src/sentry/users/api/endpoints/user_details.py
+++ src/sentry/users/api/endpoints/user_details.py
@@ -79,6 +79,7 @@ class UserOptionsSerializer(serializers.Serializer[UserOption]):
         required=False,
     )
     prefersIssueDetailsStreamlinedUI = serializers.BooleanField(required=False)
+    prefersNextjsInsightsOverview = serializers.BooleanField(required=False)
     prefersStackedNavigation = serializers.BooleanField(required=False)
     prefersChonkUI = serializers.BooleanField(required=False)
 
@@ -257,6 +258,7 @@ class UserDetailsEndpoint(UserEndpoint):
             "clock24Hours": "clock_24_hours",
             "prefersIssueDetailsStreamlinedUI": "prefers_issue_details_streamlined_ui",
             "prefersStackedNavigation": "prefers_stacked_navigation",
+            "prefersNextjsInsightsOverview": "prefers_nextjs_insights_overview",
             "prefersChonkUI": "prefers_chonk_ui",
             "quickStartDisplay": "quick_start_display",
         }
