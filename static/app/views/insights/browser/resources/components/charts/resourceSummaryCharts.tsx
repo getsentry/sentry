ecd50f8a02435b69460beff38b3c719c07b7bcd8
Billy Vong <billyvg@users.noreply.github.com>
2025-04-15 11:11:09 -0500

    typo

--- static/app/views/insights/browser/resources/components/charts/resourceSummaryCharts.tsx
+++ static/app/views/insights/browser/resources/components/charts/resourceSummaryCharts.tsx
@@ -3,7 +3,7 @@ import {Fragment} from 'react';
 import * as ModuleLayout from 'sentry/views/insights/common/components/moduleLayout';
 import {ResourceSummaryAverageSizeChartWidget} from 'sentry/views/insights/common/components/widgets/resourceSummaryAverageSizeChartWidget';
 import {ResourceSummaryDurationChartWidget} from 'sentry/views/insights/common/components/widgets/resourceSummaryDurationChartWidget';
-import {ResourceSummaryThroughputChartWidget} from 'sentry/views/insights/common/components/widgets/resourceSummaryThroughtputChartWidget';
+import {ResourceSummaryThroughputChartWidget} from 'sentry/views/insights/common/components/widgets/resourceSummaryThroughputChartWidget';
 
 function ResourceSummaryCharts() {
   return (
