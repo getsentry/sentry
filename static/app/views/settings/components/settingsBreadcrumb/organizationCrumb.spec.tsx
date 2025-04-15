41f329dff7cc7515198734baa471bb7df501c0d5
TkDodo <dominik.dorfmeister@sentry.io>
2025-04-15 13:38:42 +0200

    fix: tests without mocks

--- static/app/views/settings/components/settingsBreadcrumb/organizationCrumb.spec.tsx
+++ static/app/views/settings/components/settingsBreadcrumb/organizationCrumb.spec.tsx
@@ -6,19 +6,15 @@ import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 import ConfigStore from 'sentry/stores/configStore';
 import OrganizationsStore from 'sentry/stores/organizationsStore';
 import type {Config} from 'sentry/types/system';
-import {useParams} from 'sentry/utils/useParams';
 
 import {OrganizationCrumb} from './organizationCrumb';
 import type {RouteWithName, SettingsBreadcrumbProps} from './types';
 
 jest.unmock('sentry/utils/recreateRoute');
-jest.mock('sentry/utils/useParams', () => ({
-  useParams: jest.fn(),
-}));
 
 describe('OrganizationCrumb', function () {
   let initialData: Config;
-  const {organization, project, router, routerProps} = initializeOrg();
+  const {organization, project, routerProps} = initializeOrg();
   const organizations = [
     organization,
     OrganizationFixture({
@@ -31,8 +27,6 @@ describe('OrganizationCrumb', function () {
     OrganizationsStore.init();
     OrganizationsStore.load(organizations);
 
-    jest.mocked(useParams).mockReturnValue({});
-
     initialData = ConfigStore.getState();
   });
 
@@ -43,8 +37,8 @@ describe('OrganizationCrumb', function () {
 
   const renderComponent = (props: Omit<SettingsBreadcrumbProps, 'isLast'>) =>
     render(<OrganizationCrumb {...routerProps} {...props} isLast={false} />, {
-      router,
       organization,
+      enableRouterMocks: false,
     });
 
   afterEach(function () {
@@ -63,10 +57,12 @@ describe('OrganizationCrumb', function () {
       route,
     ];
 
-    renderComponent({routes, route});
+    const result = renderComponent({routes, route});
     await switchOrganization();
 
-    expect(router.push).toHaveBeenCalledWith({pathname: '/settings/org-slug2/'});
+    expect(result.router.location).toMatchObject({
+      pathname: '/settings/org-slug2/',
+    });
   });
 
   it('switches organizations while on API Keys Details route', async function () {
@@ -84,10 +80,13 @@ describe('OrganizationCrumb', function () {
       {path: ':apiKey/', name: 'API Key Details'},
     ];
 
-    renderComponent({routes, route});
+    const result = renderComponent({routes, route});
+
     await switchOrganization();
 
-    expect(router.push).toHaveBeenCalledWith({pathname: '/settings/org-slug2/api-keys/'});
+    expect(result.router.location).toMatchObject({
+      pathname: '/settings/org-slug2/api-keys/',
+    });
   });
 
   it('switches organizations while on API Keys List route', async function () {
@@ -104,10 +103,12 @@ describe('OrganizationCrumb', function () {
       {path: 'api-keys/', name: 'API Key'},
     ];
 
-    renderComponent({routes, route});
+    const result = renderComponent({routes, route});
     await switchOrganization();
 
-    expect(router.push).toHaveBeenCalledWith({pathname: '/settings/org-slug2/api-keys/'});
+    expect(result.router.location).toMatchObject({
+      pathname: '/settings/org-slug2/api-keys/',
+    });
   });
 
   it('switches organizations while in Project Client Keys Details route', async function () {
@@ -121,15 +122,22 @@ describe('OrganizationCrumb', function () {
       {path: ':keyId/', name: 'Details'},
     ];
 
-    jest.mocked(useParams).mockReturnValue({projectId: project.slug});
-
-    renderComponent({
-      routes,
-      route,
-    });
+    const result = render(
+      <OrganizationCrumb {...routerProps} route={route} routes={routes} isLast={false} />,
+      {
+        organization,
+        enableRouterMocks: false,
+        initialRouterConfig: {
+          location: {pathname: `/projects/${project.slug}/`},
+          route: '/projects/:projectId/',
+        },
+      }
+    );
     await switchOrganization();
 
-    expect(router.push).toHaveBeenCalledWith({pathname: '/settings/org-slug2/'});
+    expect(result.router.location).toMatchObject({
+      pathname: '/settings/org-slug2/',
+    });
   });
 
   it('switches organizations for child route with customer domains', async function () {
