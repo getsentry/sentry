ff2539a70893aae5a9ea1ea09a0d19ba5facf403
Scott Cooper <scttcper@gmail.com>
2025-04-15 10:29:42 -0700

    feat(bootstrap): Cache projects in IndexedDB (#89139)
    Implements the caching part of
    https://www.notion.so/sentry/Tech-Spec-Frontend-Bootstrap-Cache-Project-Loading-Performance-1748b10e4b5d80759127c15d3772e063
    
    Went with IndexedDB because some organizations have multiple megabytes
    of projects to cache.
    
    This helps bandaid the entire app waiting for projects to load by
    storing them in the cache and revalidating.

--- static/app/main.tsx
+++ static/app/main.tsx
@@ -3,20 +3,15 @@ import {createBrowserRouter, RouterProvider} from 'react-router-dom';
 import {wrapCreateBrowserRouterV6} from '@sentry/react';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
+import {appQueryClient} from 'sentry/appQueryClient';
 import {OnboardingContextProvider} from 'sentry/components/onboarding/onboardingContext';
 import {ThemeAndStyleProvider} from 'sentry/components/themeAndStyleProvider';
 import {USE_REACT_QUERY_DEVTOOL} from 'sentry/constants';
 import {routes} from 'sentry/routes';
 import {DANGEROUS_SET_REACT_ROUTER_6_HISTORY} from 'sentry/utils/browserHistory';
-import {
-  DEFAULT_QUERY_CLIENT_CONFIG,
-  QueryClient,
-  QueryClientProvider,
-} from 'sentry/utils/queryClient';
+import {QueryClientProvider} from 'sentry/utils/queryClient';
 
 import {buildReactRouter6Routes} from './utils/reactRouter6Compat/router';
-
-const queryClient = new QueryClient(DEFAULT_QUERY_CLIENT_CONFIG);
 
 function buildRouter() {
   const sentryCreateBrowserRouter = wrapCreateBrowserRouterV6(createBrowserRouter);
@@ -30,7 +25,7 @@ function Main() {
   const [router] = useState(buildRouter);
 
   return (
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={appQueryClient}>
       <ThemeAndStyleProvider>
         <OnboardingContextProvider>
           <RouterProvider router={router} />
