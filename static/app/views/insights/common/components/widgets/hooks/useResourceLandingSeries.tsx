5da6791a96f2361cf215268d36724419baad1ada
Billy Vong <billyvg@users.noreply.github.com>
2025-04-15 10:16:12 -0500

    ref(insights): Refactor `ResourceSummaryCharts` into separate modules
    Part of https://github.com/getsentry/sentry/issues/88560

---
+++ static/app/views/insights/common/components/widgets/hooks/useResourceLandingSeries.tsx
@@ -0,0 +1,67 @@
+import {EMPTY_OPTION_VALUE, MutableSearch} from 'sentry/utils/tokenizeSearch';
+import {getResourceTypeFilter} from 'sentry/views/insights/browser/common/queries/useResourcesQuery';
+import {DEFAULT_RESOURCE_TYPES} from 'sentry/views/insights/browser/resources/settings';
+import {
+  BrowserStarfishFields,
+  useResourceModuleFilters,
+} from 'sentry/views/insights/browser/resources/utils/useResourceFilters';
+import {useSpanMetricsSeries} from 'sentry/views/insights/common/queries/useDiscoverSeries';
+import type {ModuleFilters} from 'sentry/views/insights/common/views/spans/types';
+import {SpanMetricsField} from 'sentry/views/insights/types';
+
+const {SPAN_SELF_TIME, NORMALIZED_DESCRIPTION, SPAN_DOMAIN} = SpanMetricsField;
+const {SPAN_DOMAIN: STARFISH_SPAN_DOMAIN, USER_GEO_SUBREGION} = BrowserStarfishFields;
+
+const SPAN_FILTER_KEYS = ['span_operation', SPAN_DOMAIN, 'action'];
+
+const buildDiscoverQueryConditions = (appliedFilters: ModuleFilters) => {
+  const result = Object.keys(appliedFilters)
+    .filter(key => SPAN_FILTER_KEYS.includes(key))
+    // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+    .filter(key => Boolean(appliedFilters[key]))
+    .map(key => {
+      // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+      const value = appliedFilters[key];
+      if (key === SPAN_DOMAIN && value === EMPTY_OPTION_VALUE) {
+        return [`!has:${SPAN_DOMAIN}`];
+      }
+      return `${key}:${value}`;
+    });
+
+  result.push(`has:${NORMALIZED_DESCRIPTION}`);
+
+  return result.join(' ');
+};
+
+export function useResourceLandingSeries() {
+  const filters = useResourceModuleFilters();
+
+  const spanTimeChartsFilters: ModuleFilters = {
+    'span.op': `[${DEFAULT_RESOURCE_TYPES.join(',')}]`,
+    ...(filters[STARFISH_SPAN_DOMAIN]
+      ? {[STARFISH_SPAN_DOMAIN]: filters[STARFISH_SPAN_DOMAIN]}
+      : {}),
+  };
+
+  const extraQuery = [
+    ...getResourceTypeFilter(undefined, DEFAULT_RESOURCE_TYPES),
+    ...(filters[USER_GEO_SUBREGION]
+      ? [`user.geo.subregion:[${filters[USER_GEO_SUBREGION].join(',')}]`]
+      : []),
+  ];
+
+  let query: string = buildDiscoverQueryConditions(spanTimeChartsFilters);
+
+  if (extraQuery) {
+    query += ` ${extraQuery.join(' ')}`;
+  }
+
+  return useSpanMetricsSeries(
+    {
+      search: new MutableSearch(query),
+      yAxis: ['epm()', `avg(${SPAN_SELF_TIME})`],
+      transformAliasToInputFormat: true,
+    },
+    'api.starfish.span-time-charts'
+  );
+}
---
+++ static/app/views/insights/common/components/widgets/hooks/useResourceSummarySeries.tsx
@@ -0,0 +1,47 @@
+import {MutableSearch} from 'sentry/utils/tokenizeSearch';
+import {Referrer} from 'sentry/views/insights/browser/resources/referrer';
+import {useResourceModuleFilters} from 'sentry/views/insights/browser/resources/utils/useResourceFilters';
+import {useSpanMetricsSeries} from 'sentry/views/insights/common/queries/useDiscoverSeries';
+import {SpanMetricsField} from 'sentry/views/insights/types';
+
+const {
+  SPAN_SELF_TIME,
+  HTTP_RESPONSE_CONTENT_LENGTH,
+  HTTP_DECODED_RESPONSE_CONTENT_LENGTH,
+  HTTP_RESPONSE_TRANSFER_SIZE,
+  RESOURCE_RENDER_BLOCKING_STATUS,
+} = SpanMetricsField;
+
+export function useResourceSummarySeries({groupId}: {groupId?: string}) {
+  const filters = useResourceModuleFilters();
+
+  const mutableSearch = MutableSearch.fromQueryObject({
+    'span.group': groupId,
+    ...(filters[RESOURCE_RENDER_BLOCKING_STATUS]
+      ? {
+          [RESOURCE_RENDER_BLOCKING_STATUS]: filters[RESOURCE_RENDER_BLOCKING_STATUS],
+        }
+      : {}),
+    ...(filters[SpanMetricsField.USER_GEO_SUBREGION]
+      ? {
+          [SpanMetricsField.USER_GEO_SUBREGION]: `[${filters[SpanMetricsField.USER_GEO_SUBREGION].join(',')}]`,
+        }
+      : {}),
+  });
+
+  return useSpanMetricsSeries(
+    {
+      search: mutableSearch,
+      yAxis: [
+        `epm()`,
+        `avg(${SPAN_SELF_TIME})`,
+        `avg(${HTTP_RESPONSE_CONTENT_LENGTH})`,
+        `avg(${HTTP_DECODED_RESPONSE_CONTENT_LENGTH})`,
+        `avg(${HTTP_RESPONSE_TRANSFER_SIZE})`,
+      ],
+      enabled: Boolean(groupId),
+      transformAliasToInputFormat: true,
+    },
+    Referrer.RESOURCE_SUMMARY_CHARTS
+  );
+}
