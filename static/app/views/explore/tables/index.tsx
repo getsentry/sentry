7e5253be66285c1ab6d9976d47fadd7aefe84d5e
Tony Xiao <txiao@sentry.io>
2025-04-15 13:50:14 -0400

    chore(explore): Add tooltip to explore edit columns button (#89630)

--- static/app/views/explore/tables/index.tsx
+++ static/app/views/explore/tables/index.tsx
@@ -4,6 +4,7 @@ import styled from '@emotion/styled';
 import {openModal} from 'sentry/actionCreators/modal';
 import {Button} from 'sentry/components/core/button';
 import {TabList, Tabs} from 'sentry/components/tabs';
+import {Tooltip} from 'sentry/components/tooltip';
 import {IconTable} from 'sentry/icons/iconTable';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
@@ -98,13 +99,23 @@ function ExploreTablesTabbed(props: ExploreTablesProps) {
             <TabList.Item key={Mode.AGGREGATE}>{t('Aggregates')}</TabList.Item>
           </TabList>
         </Tabs>
-        <Button
-          disabled={tab !== Tab.SPAN}
-          onClick={openColumnEditor}
-          icon={<IconTable />}
-        >
-          {t('Edit Table')}
-        </Button>
+        {tab === Tab.SPAN ? (
+          <Button onClick={openColumnEditor} icon={<IconTable />}>
+            {t('Edit Table')}
+          </Button>
+        ) : (
+          <Tooltip
+            title={
+              tab === Tab.TRACE
+                ? t('Editing columns is available for span samples only')
+                : t('Use the Group By and Visualize controls to change table columns')
+            }
+          >
+            <Button disabled onClick={openColumnEditor} icon={<IconTable />}>
+              {t('Edit Table')}
+            </Button>
+          </Tooltip>
+        )}
       </SamplesTableHeader>
       {tab === Tab.SPAN && <SpansTable {...props} />}
       {tab === Tab.TRACE && <TracesTable {...props} />}
