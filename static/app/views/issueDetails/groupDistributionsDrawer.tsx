4e5c6fb58f4d7899df0d7fbb7919c450e40fb61b
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-04-16 09:59:49 -0700

    ref(feature flags): Extract GroupDistributionCrumbs from GroupDistributionsDrawer

--- static/app/views/issueDetails/groupDistributionsDrawer.tsx
+++ static/app/views/issueDetails/groupDistributionsDrawer.tsx
@@ -2,7 +2,6 @@ import {useRef, useState} from 'react';
 import styled from '@emotion/styled';
 
 import AnalyticsArea from 'sentry/components/analyticsArea';
-import {ProjectAvatar} from 'sentry/components/core/avatar/projectAvatar';
 import {Button} from 'sentry/components/core/button';
 import {ButtonBar} from 'sentry/components/core/button/buttonBar';
 import {InputGroup} from 'sentry/components/core/input/inputGroup';
@@ -10,37 +9,27 @@ import {SegmentedControl} from 'sentry/components/core/segmentedControl';
 import {ExportQueryType, useDataExport} from 'sentry/components/dataExport';
 import {DropdownMenu} from 'sentry/components/dropdownMenu';
 import {
-  CrumbContainer,
   EventDrawerBody,
   EventDrawerContainer,
   EventDrawerHeader,
   EventNavigator,
-  NavigationCrumbs,
   SearchInput,
-  ShortId,
 } from 'sentry/components/events/eventDrawer';
 import {IconDownload, IconSearch} from 'sentry/icons';
 import {t, tct} from 'sentry/locale';
 import type {Group} from 'sentry/types/group';
 import {trackAnalytics} from 'sentry/utils/analytics';
-import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
 import useProjects from 'sentry/utils/useProjects';
 import useUrlParams from 'sentry/utils/useUrlParams';
+import GroupDistributionCrumbs from 'sentry/views/issueDetails/groupDistributions/groupDistributionCrumbs';
+import {DrawerTab} from 'sentry/views/issueDetails/groupDistributions/types';
 import {FlagDetailsDrawerContent} from 'sentry/views/issueDetails/groupFeatureFlags/flagDetailsDrawerContent';
 import FlagDrawerContent from 'sentry/views/issueDetails/groupFeatureFlags/flagDrawerContent';
 import {TagDetailsDrawerContent} from 'sentry/views/issueDetails/groupTags/tagDetailsDrawerContent';
 import TagDrawerContent from 'sentry/views/issueDetails/groupTags/tagDrawerContent';
-import {Tab, TabPaths} from 'sentry/views/issueDetails/types';
-import {useGroupDetailsRoute} from 'sentry/views/issueDetails/useGroupDetailsRoute';
 import {useEnvironmentsFromUrl} from 'sentry/views/issueDetails/utils';
-
-// Used for `tab` state and URL param.
-export enum DrawerTab {
-  TAGS = 'tags',
-  FEATURE_FLAGS = 'featureFlags',
-}
 
 function useDrawerTab({enabled}: {enabled: boolean}) {
   const {getParamValue: getTabParam, setParamValue: setTabParam} = useUrlParams(
@@ -90,7 +79,6 @@ function BaseGroupDistributionsDrawer({
   group,
   includeFeatureFlagsTab,
 }: GroupDistributionsDrawerProps) {
-  const location = useLocation();
   const organization = useOrganization();
   const environments = useEnvironmentsFromUrl();
   // XXX: tagKey param is re-used for feature flag details drawer
@@ -100,7 +88,6 @@ function BaseGroupDistributionsDrawer({
   const project = projects.find(p => p.slug === group.project.slug)!;
 
   const [isExportDisabled, setIsExportDisabled] = useState(false);
-  const {baseUrl} = useGroupDetailsRoute();
   const handleDataExport = useDataExport({
     payload: {
       queryType: ExportQueryType.ISSUES_BY_TAG,
@@ -196,38 +183,7 @@ function BaseGroupDistributionsDrawer({
   return (
     <EventDrawerContainer ref={drawerRef}>
       <EventDrawerHeader>
-        <NavigationCrumbs
-          crumbs={[
-            {
-              label: (
-                <CrumbContainer>
-                  <ProjectAvatar project={project} />
-                  <ShortId>{group.shortId}</ShortId>
-                </CrumbContainer>
-              ),
-            },
-            tab === DrawerTab.TAGS
-              ? {
-                  label: t('All Tags'),
-                  to: tagKey
-                    ? {
-                        pathname: `${baseUrl}${TabPaths[Tab.DISTRIBUTIONS]}`,
-                        query: {...location.query, tab: DrawerTab.TAGS},
-                      }
-                    : undefined,
-                }
-              : {
-                  label: t('All Feature Flags'),
-                  to: tagKey
-                    ? {
-                        pathname: `${baseUrl}${TabPaths[Tab.DISTRIBUTIONS]}`,
-                        query: {...location.query, tab: DrawerTab.FEATURE_FLAGS},
-                      }
-                    : undefined,
-                },
-            ...(tagKey ? [{label: tagKey}] : []),
-          ]}
-        />
+        <GroupDistributionCrumbs group={group} project={project} tab={tab} />
       </EventDrawerHeader>
       <EventNavigator>
         <Header>{getHeaderTitle(tagKey, tab, includeFeatureFlagsTab)}</Header>
