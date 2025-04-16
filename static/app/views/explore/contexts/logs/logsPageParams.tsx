101edb3fe34ad371a85010610bf687cd448f32c3
Nate Moore <nate.moore@sentry.io>
2025-04-16 09:33:46 -0500

    chore(eslint): autofix

--- static/app/views/explore/contexts/logs/logsPageParams.tsx
+++ static/app/views/explore/contexts/logs/logsPageParams.tsx
@@ -303,7 +303,7 @@ export function useLogsAnalyticsPageSource() {
 }
 
 function getLogCursorFromLocation(location: Location): string {
-  if (!location.query || !location.query[LOGS_CURSOR_KEY]) {
+  if (!location.query?.[LOGS_CURSOR_KEY]) {
     return '';
   }
 
