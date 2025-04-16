5f00109623dab376250c7f73bb79d177870fda34
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-04-16 11:20:22 -0700

    ref(feature flags): Extract GroupDistributionCrumbs from GroupDistributionsDrawer (#89770)

---
+++ static/app/views/issueDetails/groupDistributions/groupDistributionCrumbs.tsx
@@ -0,0 +1,64 @@
+import type {Crumb} from 'sentry/components/breadcrumbs';
+import {ProjectAvatar} from 'sentry/components/core/avatar/projectAvatar';
+import {
+  CrumbContainer,
+  NavigationCrumbs,
+  ShortId,
+} from 'sentry/components/events/eventDrawer';
+import {t} from 'sentry/locale';
+import type {Group} from 'sentry/types/group';
+import type {Project} from 'sentry/types/project';
+import {useLocation} from 'sentry/utils/useLocation';
+import {useParams} from 'sentry/utils/useParams';
+import {DrawerTab} from 'sentry/views/issueDetails/groupDistributions/types';
+import {Tab, TabPaths} from 'sentry/views/issueDetails/types';
+import {useGroupDetailsRoute} from 'sentry/views/issueDetails/useGroupDetailsRoute';
+
+interface Props {
+  group: Group;
+  project: Project;
+  tab: DrawerTab;
+}
+
+export default function GroupDistributionCrumbs({group, project, tab}: Props) {
+  const location = useLocation();
+  const {tagKey} = useParams<{tagKey: string}>();
+
+  const {baseUrl} = useGroupDetailsRoute();
+  const crumbs: Crumb[] = [
+    {
+      label: (
+        <CrumbContainer>
+          <ProjectAvatar project={project} />
+          <ShortId>{group.shortId}</ShortId>
+        </CrumbContainer>
+      ),
+    },
+  ];
+  if (tab === DrawerTab.TAGS) {
+    crumbs.push({
+      label: t('All Tags'),
+      to: tagKey
+        ? {
+            pathname: `${baseUrl}${TabPaths[Tab.DISTRIBUTIONS]}`,
+            query: {...location.query, tab: DrawerTab.TAGS},
+          }
+        : undefined,
+    });
+  } else if (tab === DrawerTab.FEATURE_FLAGS) {
+    crumbs.push({
+      label: t('All Feature Flags'),
+      to: tagKey
+        ? {
+            pathname: `${baseUrl}${TabPaths[Tab.DISTRIBUTIONS]}`,
+            query: {...location.query, tab: DrawerTab.FEATURE_FLAGS},
+          }
+        : undefined,
+    });
+  }
+  if (tagKey) {
+    crumbs.push({label: tagKey});
+  }
+
+  return <NavigationCrumbs crumbs={crumbs} />;
+}
