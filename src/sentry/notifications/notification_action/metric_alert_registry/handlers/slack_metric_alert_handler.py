46a3ced6ef92aba9897f0bf1d407c4fcee76289d
Raj Joshi <raj.joshi@sentry.io>
2025-04-16 12:47:44 -0700

    ✨ WIP(aci): use new workflow engine serializers for messaging handlers (#89697)

--- src/sentry/notifications/notification_action/metric_alert_registry/handlers/slack_metric_alert_handler.py
+++ src/sentry/notifications/notification_action/metric_alert_registry/handlers/slack_metric_alert_handler.py
@@ -5,10 +5,15 @@ from sentry.incidents.typings.metric_detector import (
     OpenPeriodContext,
 )
 from sentry.integrations.slack.utils.notifications import send_incident_alert_notification
+from sentry.models.groupopenperiod import GroupOpenPeriod
 from sentry.models.organization import Organization
+from sentry.notifications.notification_action.metric_alert_registry.handlers.utils import (
+    get_alert_rule_serializer,
+    get_incident_serializer,
+)
 from sentry.notifications.notification_action.registry import metric_alert_handler_registry
 from sentry.notifications.notification_action.types import BaseMetricAlertHandler
-from sentry.workflow_engine.models import Action
+from sentry.workflow_engine.models import Action, Detector
 
 
 @metric_alert_handler_registry.register(Action.Type.SLACK)
@@ -24,6 +29,17 @@ class SlackMetricAlertHandler(BaseMetricAlertHandler):
         notification_uuid: str,
     ) -> None:
 
+        detector = Detector.objects.get(id=alert_context.action_identifier_id)
+        if not detector:
+            raise ValueError("Detector not found")
+
+        open_period = GroupOpenPeriod.objects.get(id=open_period_context.id)
+        if not open_period:
+            raise ValueError("Open period not found")
+
+        alert_rule_serialized_response = get_alert_rule_serializer(detector)
+        incident_serialized_response = get_incident_serializer(open_period)
+
         send_incident_alert_notification(
             notification_context=notification_context,
             alert_context=alert_context,
@@ -31,7 +47,6 @@ class SlackMetricAlertHandler(BaseMetricAlertHandler):
             open_period_context=open_period_context,
             organization=organization,
             notification_uuid=notification_uuid,
-            # TODO(iamrajjoshi): Add responses here once we make a decision on how to handle them
-            alert_rule_serialized_response=None,
-            incident_serialized_response=None,
+            alert_rule_serialized_response=alert_rule_serialized_response,
+            incident_serialized_response=incident_serialized_response,
         )
