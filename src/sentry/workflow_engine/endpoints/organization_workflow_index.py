4837f31a2f85b2e9ee87ea2373418787315bccac
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-04-16 15:45:13 -0700

    feat(workflow_engine): POST API for Workflow (#89823)
    ## Description
    Now that we have lots of validators under the hood, this is a pretty
    simple endpoint. This PR adds a handler for the incoming data, creates
    the new objects, then returns the results.

--- src/sentry/workflow_engine/endpoints/organization_workflow_index.py
+++ src/sentry/workflow_engine/endpoints/organization_workflow_index.py
@@ -1,4 +1,6 @@
 from drf_spectacular.utils import extend_schema
+from rest_framework import status
+from rest_framework.response import Response
 
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
@@ -13,6 +15,7 @@ from sentry.apidocs.constants import (
 )
 from sentry.apidocs.parameters import GlobalParams
 from sentry.workflow_engine.endpoints.serializers import WorkflowSerializer
+from sentry.workflow_engine.endpoints.validators.base.workflow import WorkflowValidator
 from sentry.workflow_engine.models import Workflow
 
 
@@ -51,8 +54,34 @@ class OrganizationWorkflowIndexEndpoint(OrganizationEndpoint):
             on_results=lambda x: serialize(x, request.user),
         )
 
+    @extend_schema(
+        operation_id="Create a Workflow",
+        parameters=[
+            GlobalParams.ORG_ID_OR_SLUG,
+        ],
+        responses={
+            201: WorkflowSerializer,
+            400: RESPONSE_BAD_REQUEST,
+            401: RESPONSE_UNAUTHORIZED,
+            403: RESPONSE_FORBIDDEN,
+            404: RESPONSE_NOT_FOUND,
+        },
+    )
     def post(self, request, organization):
         """
         Creates a workflow for an organization
+        `````````````````````````````````````
+        :param string name: The name of the workflow
+        :param bool enabled: Whether the workflow is enabled or not
+        :param object config: The configuration of the workflow
+        :param object triggers: The Data Condition and DataConditionGroup for the when condition of a workflow
+        :param object action_filters: The Data Conditions, Data Condition Group, and Actions to invoke when a workflow is triggered
         """
-        pass
+        validator = WorkflowValidator(
+            data=request.data,
+            context={"organization": organization, "request": request},
+        )
+
+        validator.is_valid(raise_exception=True)
+        workflow = validator.create(validator.validated_data)
+        return Response(serialize(workflow, request.user), status=status.HTTP_201_CREATED)
--- tests/sentry/workflow_engine/endpoints/test_organization_workflow_index.py
+++ tests/sentry/workflow_engine/endpoints/test_organization_workflow_index.py
@@ -1,6 +1,7 @@
 from sentry.api.serializers import serialize
 from sentry.testutils.cases import APITestCase
 from sentry.testutils.silo import region_silo_test
+from sentry.workflow_engine.models import Action, Workflow, WorkflowDataConditionGroup
 
 
 class OrganizationWorkflowAPITestCase(APITestCase):
@@ -26,3 +27,156 @@ class OrganizationWorkflowIndexBaseTest(OrganizationWorkflowAPITestCase):
     def test_empty_result(self):
         response = self.get_success_response(self.organization.slug)
         assert response.data == []
+
+
+@region_silo_test
+class OrganizationWorkflowCreateTest(OrganizationWorkflowAPITestCase):
+    method = "POST"
+
+    def setUp(self):
+        super().setUp()
+        self.valid_workflow = {
+            "name": "Test Workflow",
+            "enabled": True,
+            "config": {},
+            "triggers": {"logicType": "any", "conditions": []},
+            "action_filters": [],
+        }
+
+    def test_create_workflow__basic(self):
+        response = self.get_success_response(
+            self.organization.slug,
+            raw_data=self.valid_workflow,
+        )
+
+        assert response.status_code == 201
+        new_workflow = Workflow.objects.get(id=response.data["id"])
+        assert new_workflow.name == self.valid_workflow["name"]
+
+    def test_create_workflow__with_config(self):
+        self.valid_workflow["config"] = {"frequency": 100}
+        response = self.get_success_response(
+            self.organization.slug,
+            raw_data=self.valid_workflow,
+        )
+
+        assert response.status_code == 201
+        new_workflow = Workflow.objects.get(id=response.data["id"])
+        assert new_workflow.config == self.valid_workflow["config"]
+
+    def test_create_workflow__with_triggers(self):
+        self.valid_workflow["triggers"] = {
+            "logicType": "any",
+            "conditions": [
+                {
+                    "type": "eq",
+                    "comparison": 1,
+                    "condition_result": True,
+                }
+            ],
+        }
+
+        response = self.get_success_response(
+            self.organization.slug,
+            raw_data=self.valid_workflow,
+        )
+
+        assert response.status_code == 201
+        new_workflow = Workflow.objects.get(id=response.data["id"])
+        assert new_workflow.when_condition_group is not None
+        assert str(new_workflow.when_condition_group.id) == response.data.get("triggers", {}).get(
+            "id"
+        )
+
+    def test_create_workflow__with_actions(self):
+        self.valid_workflow["actionFilters"] = [
+            {
+                "logicType": "any",
+                "conditions": [
+                    {
+                        "type": "eq",
+                        "comparison": 1,
+                        "condition_result": True,
+                    }
+                ],
+                "actions": [
+                    {
+                        "type": Action.Type.SLACK,
+                        "config": {
+                            "target_identifier": "test",
+                            "target_display": "Test",
+                            "target_type": 0,
+                        },
+                        "data": {},
+                        "integrationId": 1,
+                    },
+                ],
+            }
+        ]
+
+        response = self.get_success_response(
+            self.organization.slug,
+            raw_data=self.valid_workflow,
+        )
+
+        assert response.status_code == 201
+        new_workflow = Workflow.objects.get(id=response.data["id"])
+        new_action_filters = WorkflowDataConditionGroup.objects.filter(workflow=new_workflow)
+        assert len(new_action_filters) == len(response.data.get("actionFilters", []))
+        assert str(new_action_filters[0].condition_group.id) == response.data.get(
+            "actionFilters", []
+        )[0].get("id")
+
+    def test_create_invalid_workflow(self):
+        self.valid_workflow["name"] = ""
+        response = self.get_response(
+            self.organization.slug,
+            raw_data=self.valid_workflow,
+        )
+
+        assert response.status_code == 400
+
+    def test_create_workflow__invalid_triggers(self):
+        self.valid_workflow["triggers"] = {
+            "logicType": "some",
+            "conditions": [
+                {
+                    "comparison": 1,
+                    "condition_result": True,
+                }
+            ],
+        }
+
+        response = self.get_response(
+            self.organization.slug,
+            raw_data=self.valid_workflow,
+        )
+
+        assert response.status_code == 400
+
+    def test_create_workflow__invalid_actions(self):
+        self.valid_workflow["actionFilters"] = [
+            {
+                "logicType": "some",
+                "conditions": [
+                    {
+                        "comparison": 1,
+                        "condition_result": True,
+                    }
+                ],
+                "actions": [
+                    {
+                        "type": Action.Type.SLACK,
+                        "config": {},
+                        "data": {},
+                    },
+                ],
+            }
+        ]
+
+        response = self.get_response(
+            self.organization.slug,
+            raw_data=self.valid_workflow,
+        )
+
+        assert response.status_code == 400
