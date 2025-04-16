b7d194e0aa180d3ef5510ce1c87804e1d3cac77e
Tony Xiao <txiao@sentry.io>
2025-04-16 08:56:45 -0400

    feat(explore): Add tour for explore spans
    Attempt 2 of #89248
    
    This adds a tour that triggers the first time when visiting explore spans.
    
    Closes EXP-47
    Closes EXP-61

--- static/app/views/explore/spans/spansTab.tsx
+++ static/app/views/explore/spans/spansTab.tsx
@@ -15,6 +15,7 @@ import {
   useEAPSpanSearchQueryBuilderProps,
 } from 'sentry/components/performance/spanSearchQueryBuilder';
 import {SearchQueryBuilderProvider} from 'sentry/components/searchQueryBuilder/context';
+import {TourElement} from 'sentry/components/tours/components';
 import {IconChevron} from 'sentry/icons/iconChevron';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
@@ -59,6 +60,7 @@ import {useExploreTracesTable} from 'sentry/views/explore/hooks/useExploreTraces
 import {SAMPLING_MODE} from 'sentry/views/explore/hooks/useProgressiveQuery';
 import {Tab, useTab} from 'sentry/views/explore/hooks/useTab';
 import {useVisitQuery} from 'sentry/views/explore/hooks/useVisitQuery';
+import {ExploreSpansTour, ExploreSpansTourContext} from 'sentry/views/explore/spans/tour';
 import {ExploreTables} from 'sentry/views/explore/tables';
 import {ExploreToolbar} from 'sentry/views/explore/toolbar';
 import {
@@ -240,72 +242,105 @@ export function SpansTabContentImpl({
     <SearchQueryBuilderProvider {...eapSpanSearchQueryProviderProps}>
       <Body withToolbar={expanded}>
         <TopSection>
-          <FilterSection>
-            <StyledPageFilterBar condensed>
-              <ProjectPageFilter />
-              <EnvironmentPageFilter />
-              <DatePageFilter
-                defaultPeriod={defaultPeriod}
-                maxPickableDays={maxPickableDays}
-                relativeOptions={({arbitraryOptions}) => ({
-                  ...arbitraryOptions,
-                  ...relativeOptions,
-                })}
-              />
-            </StyledPageFilterBar>
-            <EAPSpanSearchQueryBuilder {...eapSpanSearchQueryBuilderProps} />
-          </FilterSection>
-          <Feature features="organizations:traces-schema-hints">
-            <SchemaHintsSection>
-              <SchemaHintsList
-                supportedAggregates={
-                  mode === Mode.SAMPLES ? [] : ALLOWED_EXPLORE_VISUALIZE_AGGREGATES
-                }
-                numberTags={numberTags}
-                stringTags={stringTags}
-                isLoading={numberTagsLoading || stringTagsLoading}
-                exploreQuery={query}
-                source={SchemaHintsSources.EXPLORE}
-                tableColumns={fields}
-                setPageParams={setExplorePageParams}
-              />
-            </SchemaHintsSection>
-          </Feature>
+          <TourElement<ExploreSpansTour>
+            tourContext={ExploreSpansTourContext}
+            id={ExploreSpansTour.SEARCH_BAR}
+            title={t('Start Your Search')}
+            description={t(
+              'Specify the keys you’d like to narrow your search down to (ex. span.operation) and then any values (ex. db, res, http, etc.).'
+            )}
+            position="bottom"
+            margin={-8}
+          >
+            <FilterSection>
+              <StyledPageFilterBar condensed>
+                <ProjectPageFilter />
+                <EnvironmentPageFilter />
+                <DatePageFilter
+                  defaultPeriod={defaultPeriod}
+                  maxPickableDays={maxPickableDays}
+                  relativeOptions={({arbitraryOptions}) => ({
+                    ...arbitraryOptions,
+                    ...relativeOptions,
+                  })}
+                />
+              </StyledPageFilterBar>
+              <EAPSpanSearchQueryBuilder {...eapSpanSearchQueryBuilderProps} />
+            </FilterSection>
+            <Feature features="organizations:traces-schema-hints">
+              <StyledSchemaHintsSection>
+                <SchemaHintsList
+                  supportedAggregates={
+                    mode === Mode.SAMPLES ? [] : ALLOWED_EXPLORE_VISUALIZE_AGGREGATES
+                  }
+                  numberTags={numberTags}
+                  stringTags={stringTags}
+                  isLoading={numberTagsLoading || stringTagsLoading}
+                  exploreQuery={query}
+                  source={SchemaHintsSources.EXPLORE}
+                  tableColumns={fields}
+                  setPageParams={setExplorePageParams}
+                />
+              </StyledSchemaHintsSection>
+            </Feature>
+          </TourElement>
         </TopSection>
         <SideSection withToolbar={expanded}>
-          <ExploreToolbar width={300} extras={toolbarExtras} />
+          <TourElement<ExploreSpansTour>
+            tourContext={ExploreSpansTourContext}
+            id={ExploreSpansTour.TOOLBAR}
+            title={t('Configure Your Query')}
+            description={t(
+              'Based on your search, you can determine how you want your results shown. Choose your metric visualization, group (optional) and sort.'
+            )}
+            position="right"
+            margin={-8}
+          >
+            <ExploreToolbar width={300} extras={toolbarExtras} />
+          </TourElement>
         </SideSection>
         <section>
           {!resultsLoading && !hasResults && <QuotaExceededAlert referrer="explore" />}
-          <MainContent>
-            <ExploreCharts
-              canUsePreviousResults={canUsePreviousResults}
-              confidences={confidences}
-              query={query}
-              timeseriesResult={timeseriesResult}
-              isProgressivelyLoading={timeseriesIsProgressivelyLoading}
-              visualizes={visualizes}
-              setVisualizes={setVisualizes}
-            />
-            <ExploreTables
-              aggregatesTableResult={aggregatesTableResult}
-              spansTableResult={spansTableResult}
-              tracesTableResult={tracesTableResult}
-              confidences={confidences}
-              samplesTab={samplesTab}
-              setSamplesTab={setSamplesTab}
-              isProgressivelyLoading={tableIsProgressivelyLoading}
-              useTabs={organization.features.includes('visibility-explore-tabs')}
-            />
-            <Toggle>
-              <StyledButton
-                aria-label={expanded ? t('Collapse sidebar') : t('Expande sidebar')}
-                size="xs"
-                icon={<IconDoubleChevron direction={expanded ? 'left' : 'right'} />}
-                onClick={() => setExpanded(!expanded)}
+          <TourElement<ExploreSpansTour>
+            tourContext={ExploreSpansTourContext}
+            id={ExploreSpansTour.RESULTS}
+            title={t('Get Results')}
+            description={t(
+              'See a chart and list of samples or aggregates. IDs will link you to a waterfall for debugging while aggregates will let you scope it down further.'
+            )}
+            position="top"
+            margin={-8}
+          >
+            <MainContent>
+              <ExploreCharts
+                canUsePreviousResults={canUsePreviousResults}
+                confidences={confidences}
+                query={query}
+                timeseriesResult={timeseriesResult}
+                isProgressivelyLoading={timeseriesIsProgressivelyLoading}
+                visualizes={visualizes}
+                setVisualizes={setVisualizes}
               />
-            </Toggle>
-          </MainContent>
+              <ExploreTables
+                aggregatesTableResult={aggregatesTableResult}
+                spansTableResult={spansTableResult}
+                tracesTableResult={tracesTableResult}
+                confidences={confidences}
+                samplesTab={samplesTab}
+                setSamplesTab={setSamplesTab}
+                isProgressivelyLoading={tableIsProgressivelyLoading}
+                useTabs={organization.features.includes('visibility-explore-tabs')}
+              />
+              <Toggle>
+                <StyledButton
+                  aria-label={expanded ? t('Collapse sidebar') : t('Expande sidebar')}
+                  size="xs"
+                  icon={<IconDoubleChevron direction={expanded ? 'left' : 'right'} />}
+                  onClick={() => setExpanded(!expanded)}
+                />
+              </Toggle>
+            </MainContent>
+          </TourElement>
         </section>
       </Body>
     </SearchQueryBuilderProvider>
@@ -404,9 +439,6 @@ const Body = styled(Layout.Body)<{withToolbar: boolean}>`
 
 const TopSection = styled('div')`
   grid-column: 1/3;
-  display: flex;
-  flex-direction: column;
-  gap: ${space(1)};
 `;
 
 const FilterSection = styled('div')`
@@ -458,4 +490,12 @@ const StyledButton = styled(Button)`
 
 const DoubleChevronWrapper = styled('div')`
   display: flex;
+`;
+
+const StyledSchemaHintsSection = styled(SchemaHintsSection)`
+  margin-top: ${space(1)};
+
+  @media (min-width: ${p => p.theme.breakpoints.medium}) {
+    margin-top: ${space(1)};
+  }
 `;
