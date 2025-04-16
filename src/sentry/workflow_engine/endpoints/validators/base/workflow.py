15481b9eb99e0eb8a6543793ae323dc2c9b4c953
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-04-15 21:56:56 -0700

    Add create method to workflow

--- src/sentry/workflow_engine/endpoints/validators/base/workflow.py
+++ src/sentry/workflow_engine/endpoints/validators/base/workflow.py
@@ -1,9 +1,22 @@
+from typing import Any
+
+from django.db import router, transaction
 from rest_framework import serializers
 
 from sentry.api.serializers.rest_framework import CamelSnakeSerializer
-from sentry.workflow_engine.endpoints.validators.base import BaseDataConditionGroupValidator
+from sentry.workflow_engine.endpoints.validators.base import (
+    BaseActionValidator,
+    BaseDataConditionGroupValidator,
+)
 from sentry.workflow_engine.endpoints.validators.utils import validate_json_schema
-from sentry.workflow_engine.models import Workflow
+from sentry.workflow_engine.models import (
+    DataConditionGroupAction,
+    Workflow,
+    WorkflowDataConditionGroup,
+)
+
+DataConditionGroupData = dict[str, Any]
+ActionData = dict[str, Any]
 
 
 class WorkflowValidator(CamelSnakeSerializer):
@@ -11,12 +24,67 @@ class WorkflowValidator(CamelSnakeSerializer):
     enabled = serializers.BooleanField(required=False, default=True)
     config = serializers.JSONField(required=False)
     triggers = BaseDataConditionGroupValidator(required=False)
-    action_filters = serializers.ListField(child=BaseDataConditionGroupValidator(), required=False)
+    action_filters = serializers.ListField(required=False)
 
     # TODO - Need to improve the following fields (validate them in db)
     organization_id = serializers.IntegerField(required=True)
     environment_id = serializers.IntegerField(required=False)
 
+    def _split_action_and_condition_group(
+        self, action_filter: dict[str, Any]
+    ) -> tuple[ActionData, DataConditionGroupData]:
+        action = action_filter.get("action", None)
+
+        if not action:
+            raise serializers.ValidationError("An action is required in the actionFilter")
+
+        condition_group = action_filter - action
+        return action, condition_group
+
     def validate_config(self, value):
         schema = Workflow.config_schema
         return validate_json_schema(value, schema)
+
+    def validate_action_filters(self, value):
+        for action_filter in value:
+            action, condition_group = self._split_action_and_condition_group(action_filter)
+            BaseDataConditionGroupValidator(data=condition_group).is_valid(raise_exception=True)
+            BaseActionValidator(data=action).is_valid(raise_exception=True)
+
+        return value
+
+    def create(self, validated_value: dict[str, Any]) -> Workflow:
+        condition_group_validator = BaseDataConditionGroupValidator()
+        action_validator = BaseActionValidator()
+
+        with transaction.atomic(router.db_for_write(Workflow)):
+            # create triggers
+            when_condition_group = condition_group_validator.create(validated_value["triggers"])
+
+            workflow = Workflow.objects.create(
+                name=validated_value["name"],
+                enabled=validated_value["enabled"],
+                config=validated_value["config"],
+                organization_id=validated_value["organization_id"],
+                environment_id=validated_value.get("environment_id"),
+                when_condition_group=when_condition_group,
+            )
+
+            for action_filter in validated_value["action_filters"]:
+                action, condition_group = self._split_action_and_condition_group(action_filter)
+                new_condition_group = condition_group_validator.create(condition_group)
+                new_action = action_validator.create(action)
+
+                # Connect the aciton to the condition group
+                DataConditionGroupAction.objects.create(
+                    action=new_action,
+                    condition_group=new_condition_group,
+                )
+
+                # Connect the condition group to the workflow
+                WorkflowDataConditionGroup.objects.create(
+                    condition_group=new_condition_group,
+                    workflow=workflow,
+                )
+
+            return workflow
