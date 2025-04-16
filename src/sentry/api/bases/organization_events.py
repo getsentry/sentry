fa3d892851764873a472ab57c8d0aca8496b9dbe
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-04-16 14:37:20 -0400

    cast

--- src/sentry/api/bases/organization_events.py
+++ src/sentry/api/bases/organization_events.py
@@ -137,7 +137,7 @@ class OrganizationEventsEndpointBase(OrganizationEndpoint):
             if sampling_mode is not None:
                 if sampling_mode.upper() not in SAMPLING_MODE_MAP:
                     raise InvalidSearchQuery(f"sampling mode: {sampling_mode} is not supported")
-                sampling_mode = cast(SAMPLING_MODES, sampling_mode)
+                sampling_mode = cast(SAMPLING_MODES, sampling_mode.upper())
 
             if quantize_date_params:
                 filter_params = self.quantize_date_params(request, filter_params)
