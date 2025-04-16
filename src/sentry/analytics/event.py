5d50cf9346ade054f035581bb13c1a8f706dc9d0
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2025-04-16 08:56:00 -0400

    ref: analytics event type is abstract and str (#89658)
    <!-- Describe your PR here. -->

--- src/sentry/analytics/event.py
+++ src/sentry/analytics/event.py
@@ -13,34 +13,16 @@ from sentry.analytics.utils import get_data
 
 
 class Event:
-    __slots__ = ["uuid", "data", "datetime"]
-
-    # This MUST be overridden by child classes.
-    type = None
+    __slots__ = ("uuid", "data", "datetime")
 
     # These should be overridden by child classes.
+    type: str  # abstract
     attributes: Sequence[Attribute] = ()
 
-    def __init__(
-        self, type: Any | None = None, datetime: dt.datetime | None = None, **items: Any
-    ) -> None:
+    def __init__(self, datetime: dt.datetime | None = None, **items: Any) -> None:
         self.uuid = uuid1()
         self.datetime = datetime or timezone.now()
-        self.type = self._get_type(type)  # type: ignore[misc]
         self.data = get_data(self.attributes, items)
-
-    def _get_type(self, _type: Any | None = None) -> Any:
-        """
-        The Event's `type` can either be passed in as a parameter or set as a
-        property on a child class.
-        """
-        if _type is not None:
-            return _type
-
-        if self.type is None:
-            raise ValueError("Event is missing type")
-
-        return self.type
 
     def serialize(self) -> Mapping[str, Any]:
         return {
