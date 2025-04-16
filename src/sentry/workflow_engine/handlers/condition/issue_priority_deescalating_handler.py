de3743f68b4f6a38f2aecd4de329d087c3a4bf2f
Michelle Fu <michelle.fu@sentry.io>
2025-04-16 16:32:34 -0700

    first pass

---
+++ src/sentry/workflow_engine/handlers/condition/issue_priority_deescalating_handler.py
@@ -0,0 +1,19 @@
+from typing import Any
+
+from sentry.workflow_engine.models.data_condition import Condition
+from sentry.workflow_engine.registry import condition_handler_registry
+from sentry.workflow_engine.types import DataConditionHandler, WorkflowEventData
+
+
+@condition_handler_registry.register(Condition.ISSUE_PRIORITY_DEESCALATING)
+class IssuePriorityDeescalatingConditionHandler(DataConditionHandler[WorkflowEventData]):
+    group = DataConditionHandler.Group.ACTION_FILTER
+    subgroup = DataConditionHandler.Subgroup.ISSUE_ATTRIBUTES
+
+    @staticmethod
+    def evaluate_value(event_data: WorkflowEventData, comparison: Any) -> bool:
+        previous_status = event_data.event.occurrence.evidence_data["previous_status"]
+
+        group = event_data.event.group
+        current_status = group.priority
+        return current_status < previous_status
