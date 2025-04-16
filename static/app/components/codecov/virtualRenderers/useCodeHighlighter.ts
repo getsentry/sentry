d76d875eaa1c9c2a8fd8a0ffd9e3a4bafe0acac2
nicholas-codecov <nicholas.deschenes@sentry.io>
2025-04-16 13:54:12 -0400

    add docstring to useCodeHighlighting hook

--- static/app/components/codecov/virtualRenderers/useCodeHighlighter.ts
+++ static/app/components/codecov/virtualRenderers/useCodeHighlighter.ts
@@ -2,6 +2,13 @@ import {useMemo} from 'react';
 
 import {usePrismTokens} from 'sentry/utils/usePrismTokens';
 
+/**
+ * This hook determines the language of the code, and then uses that language to tokenize
+ * the code using the `usePrismTokens` hook.
+ * @param content - The content of the code.
+ * @param fileName - The full name (file.tsx) of the file.
+ * @returns The language/file extension of the code and the tokens.
+ */
 export function useCodeHighlighting(content: string, fileName: string) {
   const language = useMemo(() => {
     if (fileName === '') {
