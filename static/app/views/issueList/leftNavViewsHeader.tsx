20441ee48073ff86aaaa68d60f1800b67ff01bcd
Malachi Willey <malwilley@gmail.com>
2025-04-16 09:36:19 -0700

    feat(issue-views): Add delete menu to issue view details page (#89753)

--- static/app/views/issueList/leftNavViewsHeader.tsx
+++ static/app/views/issueList/leftNavViewsHeader.tsx
@@ -1,16 +1,23 @@
 import styled from '@emotion/styled';
 
+import {openConfirmModal} from 'sentry/components/confirm';
 import {Button} from 'sentry/components/core/button';
+import {DropdownMenu} from 'sentry/components/dropdownMenu';
 import GlobalEventProcessingAlert from 'sentry/components/globalEventProcessingAlert';
 import * as Layout from 'sentry/components/layouts/thirds';
-import {IconStar} from 'sentry/icons';
+import {IconEllipsis, IconStar} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import {setApiQueryData, useQueryClient} from 'sentry/utils/queryClient';
+import normalizeUrl from 'sentry/utils/url/normalizeUrl';
+import {useNavigate} from 'sentry/utils/useNavigate';
 import useOrganization from 'sentry/utils/useOrganization';
 import useProjects from 'sentry/utils/useProjects';
+import {useUser} from 'sentry/utils/useUser';
 import {EditableIssueViewHeader} from 'sentry/views/issueList/editableIssueViewHeader';
 import {useSelectedGroupSearchView} from 'sentry/views/issueList/issueViews/useSelectedGroupSeachView';
+import {canEditIssueView} from 'sentry/views/issueList/issueViews/utils';
+import {useDeleteGroupSearchView} from 'sentry/views/issueList/mutations/useDeleteGroupSearchView';
 import {useUpdateGroupSearchViewStarred} from 'sentry/views/issueList/mutations/useUpdateGroupSearchViewStarred';
 import {makeFetchGroupSearchViewKey} from 'sentry/views/issueList/queries/useFetchGroupSearchView';
 import type {GroupSearchView} from 'sentry/views/issueList/types';
@@ -89,6 +96,72 @@ function IssueViewStarButton() {
           color={groupSearchView?.starred ? 'yellow300' : 'subText'}
         />
       }
+      size="sm"
+    />
+  );
+}
+
+function IssueViewEditMenu() {
+  const organization = useOrganization();
+  const {data: groupSearchView} = useSelectedGroupSearchView();
+  const user = useUser();
+  const {mutate: deleteIssueView} = useDeleteGroupSearchView();
+  const navigate = useNavigate();
+
+  if (!organization.features.includes('issue-view-sharing') || !groupSearchView) {
+    return null;
+  }
+
+  const canDeleteView = canEditIssueView({groupSearchView, user});
+
+  return (
+    <DropdownMenu
+      items={[
+        {
+          key: 'delete',
+          label: t('Delete View'),
+          priority: 'danger',
+          disabled: !canDeleteView,
+          details: canDeleteView
+            ? undefined
+            : t('You do not have permission to delete this view.'),
+          onAction: () => {
+            openConfirmModal({
+              message: t(
+                'Are you sure you want to delete the view "%s"?',
+                groupSearchView.name
+              ),
+              isDangerous: true,
+              confirmText: t('Delete View'),
+              priority: 'danger',
+              onConfirm: () => {
+                deleteIssueView(
+                  {
+                    id: groupSearchView.id,
+                  },
+                  {
+                    onSuccess: () => {
+                      navigate(
+                        normalizeUrl(`/organizations/${organization.slug}/issues/`)
+                      );
+                    },
+                  }
+                );
+              },
+            });
+          },
+        },
+      ]}
+      trigger={props => (
+        <Button
+          size="sm"
+          {...props}
+          icon={<IconEllipsis />}
+          aria-label={t('More issue view options')}
+        />
+      )}
+      position="bottom-end"
+      minMenuWidth={160}
     />
   );
 }
@@ -105,7 +178,10 @@ function LeftNavViewsHeader({selectedProjectIds}: LeftNavViewsHeaderProps) {
       <Layout.HeaderContent unified={prefersStackedNav}>
         <StyledLayoutTitle>
           <PageTitle />
-          <IssueViewStarButton />
+          <Actions>
+            <IssueViewStarButton />
+            <IssueViewEditMenu />
+          </Actions>
         </StyledLayoutTitle>
       </Layout.HeaderContent>
       <Layout.HeaderActions />
@@ -127,7 +203,12 @@ const StyledGlobalEventProcessingAlert = styled(GlobalEventProcessingAlert)`
   }
 `;
 
-const StyledLayoutTitle = styled(Layout.Title)`
+const StyledLayoutTitle = styled('div')`
   display: flex;
   justify-content: space-between;
+`;
+
+const Actions = styled('div')`
+  display: flex;
+  gap: ${space(1)};
 `;
