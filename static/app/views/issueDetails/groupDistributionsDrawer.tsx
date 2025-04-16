13a7b9aa03f2116a3f9337e3363817f41f3a7cb2
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-04-16 09:30:19 -0700

    ref(feature flags): Extract TagExportDropdown from GroupDistributionsDrawer

--- static/app/views/issueDetails/groupDistributionsDrawer.tsx
+++ static/app/views/issueDetails/groupDistributionsDrawer.tsx
@@ -3,12 +3,9 @@ import styled from '@emotion/styled';
 
 import AnalyticsArea from 'sentry/components/analyticsArea';
 import {ProjectAvatar} from 'sentry/components/core/avatar/projectAvatar';
-import {Button} from 'sentry/components/core/button';
 import {ButtonBar} from 'sentry/components/core/button/buttonBar';
 import {InputGroup} from 'sentry/components/core/input/inputGroup';
 import {SegmentedControl} from 'sentry/components/core/segmentedControl';
-import {ExportQueryType, useDataExport} from 'sentry/components/dataExport';
-import {DropdownMenu} from 'sentry/components/dropdownMenu';
 import {
   CrumbContainer,
   EventDrawerBody,
@@ -19,7 +16,7 @@ import {
   SearchInput,
   ShortId,
 } from 'sentry/components/events/eventDrawer';
-import {IconDownload, IconSearch} from 'sentry/icons';
+import {IconSearch} from 'sentry/icons';
 import {t, tct} from 'sentry/locale';
 import type {Group} from 'sentry/types/group';
 import {trackAnalytics} from 'sentry/utils/analytics';
@@ -28,6 +25,7 @@ import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
 import useProjects from 'sentry/utils/useProjects';
 import useUrlParams from 'sentry/utils/useUrlParams';
+import TagExportDropdown from 'sentry/views/issueDetails/groupDistributions/tagExportDropdown';
 import {FlagDetailsDrawerContent} from 'sentry/views/issueDetails/groupFeatureFlags/flagDetailsDrawerContent';
 import FlagDrawerContent from 'sentry/views/issueDetails/groupFeatureFlags/flagDrawerContent';
 import {TagDetailsDrawerContent} from 'sentry/views/issueDetails/groupTags/tagDetailsDrawerContent';
@@ -99,56 +97,18 @@ function BaseGroupDistributionsDrawer({
   const {projects} = useProjects();
   const project = projects.find(p => p.slug === group.project.slug)!;
 
-  const [isExportDisabled, setIsExportDisabled] = useState(false);
   const {baseUrl} = useGroupDetailsRoute();
-  const handleDataExport = useDataExport({
-    payload: {
-      queryType: ExportQueryType.ISSUES_BY_TAG,
-      queryInfo: {
-        project: project.id,
-        group: group.id,
-        key: tagKey,
-      },
-    },
-  });
+
   const [search, setSearch] = useState('');
   const {tab, setTab} = useDrawerTab({enabled: includeFeatureFlagsTab});
 
   const headerActions =
     tagKey && tab === DrawerTab.TAGS ? (
-      <DropdownMenu
-        size="xs"
-        trigger={triggerProps => (
-          <Button
-            {...triggerProps}
-            borderless
-            size="xs"
-            aria-label={t('Export options')}
-            icon={<IconDownload />}
-          />
-        )}
-        items={[
-          {
-            key: 'export-page',
-            label: t('Export Page to CSV'),
-            // TODO(issues): Dropdown menu doesn't support hrefs yet
-            onAction: () => {
-              window.open(
-                `/${organization.slug}/${project.slug}/issues/${group.id}/tags/${tagKey}/export/`,
-                '_blank'
-              );
-            },
-          },
-          {
-            key: 'export-all',
-            label: isExportDisabled ? t('Export in progress...') : t('Export All to CSV'),
-            onAction: () => {
-              handleDataExport();
-              setIsExportDisabled(true);
-            },
-            disabled: isExportDisabled,
-          },
-        ]}
+      <TagExportDropdown
+        tagKey={tagKey}
+        group={group}
+        organization={organization}
+        project={project}
       />
     ) : tagKey && tab === DrawerTab.FEATURE_FLAGS ? null : (
       <ButtonBar gap={1}>
