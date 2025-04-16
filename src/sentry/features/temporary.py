a172aae826926573dc195e379de198db36d93e7c
viglia <francesco.vigliaturo@sentry.io>
2025-04-16 11:09:00 +0200

    feat(profiling): add project feature flag for enabling or disabling profile chunks processing with vroomrs (#89586)

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -84,6 +84,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:continuous-profiling-beta-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Display profile durations on the stats page
     manager.add("organizations:continuous-profiling-stats", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=True)
+    # Enable profile chunks processing with vroomrs
+    manager.add("organizations:continuous-profiling-vroomrs-processing", ProjectFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Restrict the crons data-time picker to only 30 days of history
     manager.add("organizations:crons-30-days-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable daily summary
