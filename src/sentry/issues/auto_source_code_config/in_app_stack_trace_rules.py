c880039e0925c7d953a5868aea13cd358eeea0c3
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2025-04-17 08:52:17 -0400

    fix(auto_source): Prevent creating rule if developer has it already defined
    This prevents creating in-app rules for rules the developer has already defined.

--- src/sentry/issues/auto_source_code_config/in_app_stack_trace_rules.py
+++ src/sentry/issues/auto_source_code_config/in_app_stack_trace_rules.py
@@ -25,7 +25,12 @@ def save_in_app_stack_trace_rules(
     current_enhancements = project.get_option(DERIVED_ENHANCEMENTS_OPTION_KEY)
     current_rules = set(current_enhancements.split("\n")) if current_enhancements else set()
 
-    united_rules = rules_from_code_mappings.union(current_rules)
+    developer_enhancements = project.get_option("sentry:grouping_enhancements")
+    developer_rules = set(developer_enhancements.split("\n")) if developer_enhancements else set()
+
+    # We do not want to duplicate rules from the developer enhancements
+    united_rules = rules_from_code_mappings.union(current_rules).difference(developer_rules)
+
     dry_run = platform_config.is_dry_run_platform(project.organization)
     if not dry_run and united_rules != current_rules:
         project.update_option(DERIVED_ENHANCEMENTS_OPTION_KEY, "\n".join(sorted(united_rules)))
