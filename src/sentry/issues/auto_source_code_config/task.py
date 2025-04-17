d5af89e835a9e32f2891a3cda81037daf55bf736
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2025-04-17 06:39:08 -0400

    chore(auto_config): Remove logic for unintended rules (#89732)
    This has run long enough, and it's run its course.
    
    This was added in #88952 as temporary cleanup code.

--- src/sentry/issues/auto_source_code_config/task.py
+++ src/sentry/issues/auto_source_code_config/task.py
@@ -16,7 +16,6 @@ from sentry.integrations.source_code_management.metrics import (
     SCMIntegrationInteractionType,
 )
 from sentry.issues.auto_source_code_config.code_mapping import CodeMapping, CodeMappingTreesHelper
-from sentry.issues.auto_source_code_config.constants import UNINTENDED_RULES
 from sentry.locks import locks
 from sentry.models.organization import Organization
 from sentry.models.project import Project
@@ -74,14 +73,11 @@ def process_event(
 
     platform = event.platform
     assert platform is not None
+    set_tag("platform", platform)
 
     platform_config = PlatformConfig(platform)
     if not platform_config.is_supported():
         return [], []
-
-    # This is a temporary solution to remove unintended rules across the board
-    if platform_config.creates_in_app_stack_trace_rules():
-        remove_unintended_rules(project)
 
     frames_to_process = get_frames_to_process(event.data, platform)
     if not frames_to_process:
@@ -206,23 +202,6 @@ def create_configurations(
     # We return this to allow tests running in dry-run mode to assert
     # what would have been created.
     return code_mappings, in_app_stack_trace_rules
-
-
-def remove_unintended_rules(project: Project) -> None:
-    """
-    Remove unintended rules from the project's automatic grouping enhancements.
-    """
-    key = "sentry:automatic_grouping_enhancements"
-    in_app_stack_trace_rules = project.get_option(key, default="").split("\n")
-    if not in_app_stack_trace_rules:
-        return
-
-    # Remove rules that are not in the code mappings
-    for rule in in_app_stack_trace_rules:
-        if rule in UNINTENDED_RULES:
-            in_app_stack_trace_rules.remove(rule)
-
-    project.update_option(key, "\n".join(in_app_stack_trace_rules))
 
 
 def create_code_mapping(
