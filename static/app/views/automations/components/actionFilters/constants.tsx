a6fc58430504cd4ada9b97b9b1634bb25efc0bde
Mia Hsu <ameliahsu@gmail.com>
2025-04-17 17:00:33 -0400

    add constants oops

--- static/app/views/automations/components/actionFilters/constants.tsx
+++ static/app/views/automations/components/actionFilters/constants.tsx
@@ -6,3 +6,19 @@ export const FILTER_MATCH_OPTIONS = [
   {value: DataConditionGroupLogicType.ANY_SHORT_CIRCUIT, label: t('any')},
   {value: DataConditionGroupLogicType.NONE, label: t('none')},
 ];
+
+export enum AgeComparison {
+  OLDER = 'older',
+  NEWER = 'newer',
+}
+
+export const AGE_COMPARISON_CHOICES = [
+  {
+    value: AgeComparison.OLDER,
+    label: 'older than',
+  },
+  {
+    value: AgeComparison.NEWER,
+    label: 'newer than',
+  },
+];
