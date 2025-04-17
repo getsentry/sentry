ad20c0ec86f7a562c9477bf232f4337ea9a3f5f7
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-04-17 12:00:15 -0400

    Remove unused files

--- static/app/views/insights/common/utils/findDataPoint.ts
+++
@@ -1,9 +0,0 @@
-export function findSampleFromDataPoint<T extends {timestamp: string}>(
-  dataPoint: {name: string | number; value: number},
-  data: T[],
-  matchKey: keyof T
-) {
-  return data?.find(
-    s => s.timestamp === dataPoint.name && s[matchKey] === dataPoint.value
-  );
-}
