57a97008e39ba7113992a861d8cc8e8f1691ce4c
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-04-17 17:53:58 -0400

    feat(aci): automation creation scaffolding (#89233)
    scaffolding for step 2 of automation creation at `/new-settings`
    
    this introduces a new npm dependency,
    [`flattie`](https://github.com/lukeed/flattie), in order to easily sync
    the automation form data to the automation builder state.
    - **the problem:** the automation builder includes lots of nesting (data
    condition groups >> data conditions (which can also have nested data
    condition groups!) >> comparison input fields). see types
    [here](https://github.com/getsentry/sentry/blob/c980de65aed1c79be7f3d08b81f1eeef2d89382d/static/app/types/workflowEngine/dataConditions.tsx#L65-L79)
    - **alternative solution without these packages:** we would have to
    manually iterate through the automation details in order to populate the
    form model's data.
    - **preferred solution with these packages:** `flattie` is used to take
    an automation details result from the API and flatten it for the form
    model to understand. this utility is very small (`203B`), and there
    would be very little benefit in maintaining a version ourselves.
    
    view preview
    [here](https://sentry-4xprfca9b.sentry.dev/issues/automations/new/settings)
    
    
    https://github.com/user-attachments/assets/b1cbcea9-8e4e-4865-a404-1812954dd1b8

---
+++ static/app/views/automations/components/actionFilters/constants.tsx
@@ -0,0 +1,24 @@
+import {t} from 'sentry/locale';
+import {DataConditionGroupLogicType} from 'sentry/types/workflowEngine/dataConditions';
+
+export const FILTER_MATCH_OPTIONS = [
+  {value: DataConditionGroupLogicType.ALL, label: t('all')},
+  {value: DataConditionGroupLogicType.ANY_SHORT_CIRCUIT, label: t('any')},
+  {value: DataConditionGroupLogicType.NONE, label: t('none')},
+];
+
+export enum AgeComparison {
+  OLDER = 'older',
+  NEWER = 'newer',
+}
+
+export const AGE_COMPARISON_CHOICES = [
+  {
+    value: AgeComparison.OLDER,
+    label: t('older than'),
+  },
+  {
+    value: AgeComparison.NEWER,
+    label: t('newer than'),
+  },
+];
