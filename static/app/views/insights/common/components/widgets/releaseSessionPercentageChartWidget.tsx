a767ac12edf604e34449913d4fa221dfdba2783b
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-04-17 13:07:24 -0700

    ref(insights): refactor session health page widgets (#89671)
    refactor all of the charts to
    
    - take in `props: LoadableChartWidgetProps` only (delete the `project`
    prop)
    - pass `id` and `{...props}` to  the `<InsightsXXXChartWidget>` 
    - for the case of the issues charts, which are custom `Widget`
    components, i pass the props down into the `ChartWithIssues` component,
    then into the visualizations there (both normal view & full screen)
    - rename each chart to `-ChartWidget`
    
    also refactor the data fetching hooks to take in `props.pageFilters`,
    which our drawer will make use of.

--- static/app/views/insights/common/components/widgets/releaseSessionPercentageChartWidget.tsx
+++ static/app/views/insights/common/components/widgets/releaseSessionPercentageChartWidget.tsx
@@ -1,13 +1,18 @@
 import {t} from 'sentry/locale';
 import {formatSeriesName} from 'sentry/views/dashboards/widgets/timeSeriesWidget/formatters/formatSeriesName';
 import {InsightsAreaChartWidget} from 'sentry/views/insights/common/components/insightsAreaChartWidget';
+import type {LoadableChartWidgetProps} from 'sentry/views/insights/common/components/widgets/types';
 import ChartSelectionTitle from 'sentry/views/insights/sessions/components/chartSelectionTitle';
 import useReleaseSessionPercentage from 'sentry/views/insights/sessions/queries/useReleaseSessionPercentage';
 import {CHART_TITLES} from 'sentry/views/insights/sessions/settings';
 import {SESSION_HEALTH_CHART_HEIGHT} from 'sentry/views/insights/sessions/utils/sessions';
 
-export default function ReleaseSessionPercentageChart() {
-  const {series, releases, isPending, error} = useReleaseSessionPercentage();
+export default function ReleaseSessionPercentageChartWidget(
+  props: LoadableChartWidgetProps
+) {
+  const {series, releases, isPending, error} = useReleaseSessionPercentage({
+    pageFilters: props.pageFilters,
+  });
 
   const aliases = Object.fromEntries(
     releases?.map(release => [`${release}_session_percent`, formatSeriesName(release)]) ??
@@ -16,9 +21,11 @@ export default function ReleaseSessionPercentageChart() {
 
   return (
     <InsightsAreaChartWidget
-      title={CHART_TITLES.ReleaseSessionPercentageChart}
+      {...props}
+      id="releaseSessionPercentageChartWidget"
+      title={CHART_TITLES.ReleaseSessionPercentageChartWidget}
       interactiveTitle={() => (
-        <ChartSelectionTitle title={CHART_TITLES.ReleaseSessionPercentageChart} />
+        <ChartSelectionTitle title={CHART_TITLES.ReleaseSessionPercentageChartWidget} />
       )}
       height={SESSION_HEALTH_CHART_HEIGHT}
       description={t(
