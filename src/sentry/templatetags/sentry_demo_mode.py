27dbae7c4afafdb8d3582689aebfceb91170f01a
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-04-17 14:21:00 +0200

    domain equality check

--- src/sentry/templatetags/sentry_demo_mode.py
+++ src/sentry/templatetags/sentry_demo_mode.py
@@ -7,16 +7,20 @@ register = template.Library()
 
 
 @register.simple_tag
-def init_demo_analytics():
-    org = get_demo_org()
+def init_demo_analytics(domain):
+    demo_org = get_demo_org()
 
-    if not org:
+    if not demo_org:
         return ""
 
-    domain = f"{org.slug}.sentry.io"
+    demo_domain = f"{demo_org.slug}.sentry.io"
+
+    if domain != demo_domain:
+        return ""
 
     html = f"""
     <script defer data-domain="{domain}" src="https://plausible.io/js/script.pageview-props.tagged-events.js"></script>
     <script>window.plausible = window.plausible || function() {{ (window.plausible.q = window.plausible.q || []).push(arguments) }}</script>
     """
+
     return mark_safe(html)
