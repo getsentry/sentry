d633fb86f54a0ed7287cbfb7d86b0e5a44fe5a91
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-04-17 08:17:54 -0700

    ref(replay): Collapse Replay diff Before/After labels into components with tooltips (#89836)
    I've also update the label to say "Server" and "Client" instead of
    "Before" and "After" in the case of replay diffs. Per internal
    [feedback](https://sentry.slack.com/archives/CQDHVRS2W/p1744839501049869).
    
    This also fixes an alignment issue inside the HTML Diff tab in the
    modal:
    | Before | After |
    | --- | --- |
    | <img width="166" alt="SCR-20250417-htob"
    src="https://github.com/user-attachments/assets/4b4a4206-69bb-4934-ab28-edb4041bfa30"
    /> | <img width="196" alt="SCR-20250417-htqy"
    src="https://github.com/user-attachments/assets/b58f1873-a7a8-4fd6-9d32-553fb73d83e9"
    />
    
    
    Fixes TRI-763
    
    ---------
    
    Co-authored-by: Priscila Oliveira <priscila.oliveira@sentry.io>

--- static/app/components/contentSliderDiff/index.spec.tsx
+++ static/app/components/contentSliderDiff/index.spec.tsx
@@ -1,57 +1,22 @@
-import {Fragment} from 'react';
-
 import {render, screen, userEvent} from 'sentry-test/reactTestingLibrary';
 
 import * as useDimensions from 'sentry/utils/useDimensions';
 
-import {ContentSliderDiff, type ContentSliderDiffBodyProps} from '.';
-
-const beforeHelpText = 'This is before help text';
-const afterHelpText = 'This is after help text';
-
-function MockComponent({
-  onDragHandleMouseDown,
-}: Pick<ContentSliderDiffBodyProps, 'onDragHandleMouseDown'>) {
-  return (
-    <Fragment>
-      <ContentSliderDiff.Header>
-        <ContentSliderDiff.BeforeLabel help={beforeHelpText} />
-        <ContentSliderDiff.AfterLabel help={afterHelpText} />
-      </ContentSliderDiff.Header>
-      <ContentSliderDiff.Body
-        before={<div>Before Content</div>}
-        after={<div>After Content</div>}
-        onDragHandleMouseDown={onDragHandleMouseDown}
-      />
-    </Fragment>
-  );
-}
+import {ContentSliderDiff} from '.';
 
 describe('ContentSliderDiff', function () {
-  it('renders tooltip when help is provided', async function () {
-    jest.spyOn(useDimensions, 'useDimensions').mockReturnValue({width: 300, height: 300});
-
-    render(<MockComponent />);
-
-    // Before Tooltip
-    await userEvent.hover(screen.getAllByTestId('more-information')[0]!);
-    expect(await screen.findByText(beforeHelpText)).toBeInTheDocument();
-
-    // After Tooltip
-    await userEvent.hover(screen.getAllByTestId('more-information')[1]!);
-    expect(await screen.findByText(afterHelpText)).toBeInTheDocument();
-  });
-
   it('divider can be dragged', async function () {
     jest.spyOn(useDimensions, 'useDimensions').mockReturnValue({width: 300, height: 300});
 
     const mockDragHandleMouseDown = jest.fn();
 
-    render(<MockComponent onDragHandleMouseDown={mockDragHandleMouseDown} />);
-
-    // Ensure that 'Before' and 'After' labels are rendered correctly
-    expect(screen.getByText('Before')).toBeInTheDocument();
-    expect(screen.getByText('After')).toBeInTheDocument();
+    render(
+      <ContentSliderDiff.Body
+        before={<div>Before Content</div>}
+        after={<div>After Content</div>}
+        onDragHandleMouseDown={mockDragHandleMouseDown}
+      />
+    );
 
     const dragHandle = screen.getByTestId('drag-handle');
 
@@ -67,7 +32,12 @@ describe('ContentSliderDiff', function () {
   it('does not render content when dimensions are zero', function () {
     jest.spyOn(useDimensions, 'useDimensions').mockReturnValue({width: 0, height: 0});
 
-    render(<MockComponent />);
+    render(
+      <ContentSliderDiff.Body
+        before={<div>Before Content</div>}
+        after={<div>After Content</div>}
+      />
+    );
 
     expect(screen.queryByTestId('before-content')).not.toBeInTheDocument();
   });
