4c054fea076c7d6d64658c361420ddff1c1d60d1
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-04-18 15:22:09 -0700

    project_id optional

--- src/sentry/feedback/lib/types.py
+++ src/sentry/feedback/lib/types.py
@@ -1,12 +1,14 @@
 from typing import NotRequired, TypedDict
 
 
+# Use for weak type checking of user report data. Keys correspond to fields of the UserReport model.
 class UserReportDict(TypedDict):
     event_id: str
-    project_id: int
     comments: str
+    # required for the model, but functions usually infer this from an explicit Project argument.
+    project_id: NotRequired[int]
     name: NotRequired[str]
     email: NotRequired[str]
-    environment_id: NotRequired[int]
+    environment_id: NotRequired[int]  # defaults to "production".
     group_id: NotRequired[int]
-    level: NotRequired[str]
+    level: NotRequired[str]  # defaults to "info".
