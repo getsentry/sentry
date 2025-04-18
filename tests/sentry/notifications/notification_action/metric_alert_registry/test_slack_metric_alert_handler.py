4391d33957888718c65a732c83adf87488e77603
Raj Joshi <raj.joshi@sentry.io>
2025-04-18 09:46:35 -0700

    :recycle: ref(aci): make `email_users` function a pure function (#89907)

--- tests/sentry/notifications/notification_action/metric_alert_registry/test_slack_metric_alert_handler.py
+++ tests/sentry/notifications/notification_action/metric_alert_registry/test_slack_metric_alert_handler.py
@@ -103,6 +103,7 @@ class TestSlackMetricAlertHandler(MetricAlertHandlerBase):
             threshold_type=None,
             detection_type=None,
             comparison_delta=None,
+            alert_threshold=None,
         )
 
         self.assert_metric_issue_context(
