4fe5f0660592dc85f1fde2656017dafe10ac2286
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-04-18 15:43:41 -0700

    fix(feedback): fix typing for shims from v2 to v1 (#89949)
    Follow-up on https://github.com/getsentry/sentry/pull/89685.
    
    Fixes
    [SENTRY-3SEX](https://sentry.sentry.io/issues/6551468609/events/9a88f59af64d4f86bddf42060cdff0d9/)
    Fixes
    [SENTRY-3SEZ](https://sentry.sentry.io/issues/6551532041/events/bd020dada1a74f1ba96faa07c33df30c/)
    Fixes
    [SENTRY-3SEY](https://sentry.sentry.io/issues/6551484362/events/42136852328a4acdb6f21922bd6425af/)
    
    Only the message/comments field is required, so we need to handle
    missing name or email. Also event timestamp is a ISO str.
    
    Also adds better typing to `save_userreport` and cleans up the event
    formatting code in `create_feedback` a bit. Functionality is the same,
    except now we'll always include the `user.email` context/tag, defaulting
    to empty str `""`

--- tests/sentry/feedback/usecases/test_save_feedback_event.py
+++ tests/sentry/feedback/usecases/test_save_feedback_event.py
@@ -37,11 +37,19 @@ def test_save_feedback_event_no_associated_error(
 
 
 @django_db_all
+@pytest.mark.parametrize(
+    "timestamp_format",
+    ["number", "iso"],
+)
 def test_save_feedback_event_with_associated_error(
-    default_project, mock_create_feedback_issue, mock_save_userreport
+    default_project, mock_create_feedback_issue, mock_save_userreport, timestamp_format
 ):
     event_data = mock_feedback_event(default_project.id)
     event_data["contexts"]["feedback"]["associated_event_id"] = "abcd" * 8
+    start_time = datetime.now(UTC)
+    event_data["timestamp"] = (
+        start_time.timestamp() if timestamp_format == "number" else start_time.isoformat()
+    )
     mock_create_feedback_issue.return_value = event_data
 
     save_feedback_event(event_data, default_project.id)
@@ -62,5 +70,43 @@ def test_save_feedback_event_with_associated_error(
             "comments": feedback_context["message"],
         },
         FeedbackCreationSource.NEW_FEEDBACK_ENVELOPE,
-        start_time=datetime.fromtimestamp(event_data["timestamp"], UTC),
+        start_time=start_time,
+    )
+
+
+@django_db_all
+def test_save_feedback_event_with_associated_error_and_missing_fields(
+    default_project, mock_create_feedback_issue, mock_save_userreport
+):
+    event_data = mock_feedback_event(default_project.id)
+    event_data["contexts"]["feedback"]["associated_event_id"] = "abcd" * 8
+    start_time = datetime.now(UTC)
+    event_data["timestamp"] = start_time.isoformat()
+
+    # Remove name, email, environment
+    del event_data["contexts"]["feedback"]["name"]
+    del event_data["contexts"]["feedback"]["contact_email"]
+    del event_data["environment"]
+
+    mock_create_feedback_issue.return_value = event_data
+
+    save_feedback_event(event_data, default_project.id)
+
+    mock_create_feedback_issue.assert_called_once_with(
+        event_data, default_project.id, FeedbackCreationSource.NEW_FEEDBACK_ENVELOPE
+    )
+
+    feedback_context = event_data["contexts"]["feedback"]
+    mock_save_userreport.assert_called_once_with(
+        default_project,
+        {
+            "event_id": "abcd" * 8,
+            "project_id": default_project.id,
+            "environment_id": "production",
+            "name": "",
+            "email": "",
+            "comments": feedback_context["message"],
+        },
+        FeedbackCreationSource.NEW_FEEDBACK_ENVELOPE,
+        start_time=start_time,
     )
