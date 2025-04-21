80fb01ae02a3a96c57f293648c7c345a255a0799
JonasBa <jonas@badalic.com>
2025-04-21 10:24:36 -0400

    chonk: add nav prompts

---
+++ static/app/utils/theme/useChonkPrompt.tsx
@@ -0,0 +1,40 @@
+import {usePrompt} from 'sentry/actionCreators/prompts';
+import useOrganization from 'sentry/utils/useOrganization';
+
+function noop() {}
+
+export function useChonkPrompt() {
+  const organization = useOrganization();
+  const hasChonkUI = organization?.features.includes('chonk-ui');
+
+  const tooltipPrompt = usePrompt({
+    organization,
+    feature: 'chonk-ui-tooltip',
+    options: {enabled: hasChonkUI},
+  });
+
+  const dotIndicatorPrompt = usePrompt({
+    organization,
+    feature: 'chonk-ui-dot-indicator',
+    options: {enabled: hasChonkUI},
+  });
+
+  if (!hasChonkUI) {
+    return {
+      showTooltipPrompt: false,
+      showDotIndicatorPrompt: false,
+      dismissTooltipPrompt: noop,
+      dismissDotIndicatorPrompt: noop,
+    };
+  }
+
+  return {
+    showTooltipPrompt: !tooltipPrompt.isPromptDismissed,
+    // The dot indicator is visible after the tooltip is dismissed
+    showDotIndicatorPrompt: Boolean(
+      tooltipPrompt.isPromptDismissed && !dotIndicatorPrompt.isPromptDismissed
+    ),
+    dismissTooltipPrompt: tooltipPrompt.dismissPrompt,
+    dismissDotIndicatorPrompt: dotIndicatorPrompt.dismissPrompt,
+  };
+}
