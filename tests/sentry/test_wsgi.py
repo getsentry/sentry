107b49ff73624d8d82425f02ed42e9a9f532fbd4
Tony Xiao <txiao@sentry.io>
2025-04-23 18:42:56 -0400

    fix tests

--- tests/sentry/test_wsgi.py
+++ tests/sentry/test_wsgi.py
@@ -2,7 +2,7 @@ import subprocess
 import sys
 
 modules = [
-    "django.contrib.messages.storage.fallback.FallbackStorage",
+    "django.contrib.messages.storage.fallback",
     "django.contrib.sessions.serializers",
     "django.db.models.sql.compiler",
     "sentry.identity.services.identity.impl",
