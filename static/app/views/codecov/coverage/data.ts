068aeee1a4287326cc73a9342978a33ca6ae4601
nsdeschenes <nicholas.deschenes@sentry.io>
2025-04-24 14:03:14 -0300

    :construction: Temp adding in example with example data to play around with in preview deploy

---
+++ static/app/views/codecov/coverage/data.ts
@@ -0,0 +1,288 @@
+export const data = {
+  name: 'gazebo',
+  fullPath: 'gazebo',
+  dir: true,
+  children: [
+    {
+      name: 'pages',
+      fullPath: 'gazebo/pages',
+      dir: true,
+      children: [
+        {
+          name: 'file.tsx',
+          value: 1,
+          fullPath: 'gazebo/pages/file.tsx',
+          coverage: 100,
+        },
+        {
+          name: 'file1.tsx',
+          value: 1,
+          fullPath: 'gazebo/pages/file1.tsx',
+          coverage: 50,
+        },
+        {
+          name: 'file2.tsx',
+          value: 1,
+          fullPath: 'gazebo/pages/file2.tsx',
+          coverage: 75,
+        },
+        {
+          name: 'file3.tsx',
+          value: 1,
+          fullPath: 'gazebo/pages/file3.tsx',
+          coverage: 75,
+        },
+        {
+          name: 'file4.tsx',
+          value: 1,
+          fullPath: 'gazebo/pages/file4.tsx',
+          coverage: 75,
+        },
+        {
+          name: 'temp1',
+          fullPath: 'gazebo/pages/temp1',
+          dir: true,
+          children: [
+            {
+              name: 'file5.tsx',
+              value: 1,
+              coverage: 100,
+              fullPath: 'gazebo/pages/temp1/file5.tsx',
+            },
+            {
+              name: 'file6.tsx',
+              value: 1,
+              coverage: 16,
+              fullPath: 'gazebo/pages/temp1/file6.tsx',
+            },
+            {
+              name: 'file7.tsx',
+              value: 1,
+              coverage: 25,
+              fullPath: 'gazebo/pages/temp1/file7.tsx',
+            },
+            {
+              name: 'file8.tsx',
+              value: 1,
+              coverage: 100,
+              fullPath: 'gazebo/pages/temp1/file8.tsx',
+            },
+            {
+              name: 'file9.tsx',
+              value: 1,
+              coverage: 100,
+              fullPath: 'gazebo/pages/temp1/file9.tsx',
+            },
+            {
+              name: 'temp2',
+              fullPath: 'gazebo/pages/temp1/temp2',
+              dir: true,
+              children: [
+                {
+                  name: 'file10.tsx',
+                  value: 1,
+                  coverage: 40,
+                  fullPath: 'gazebo/pages/temp1/temp2/file10.tsx',
+                },
+                {
+                  name: 'file11.tsx',
+                  value: 1,
+                  coverage: 90,
+                  fullPath: 'gazebo/pages/temp1/temp2/file11.tsx',
+                },
+                {
+                  name: 'file12.tsx',
+                  value: 1,
+                  coverage: 92,
+                  fullPath: 'gazebo/pages/temp1/temp2/file12.tsx',
+                },
+                {
+                  name: 'file13.tsx',
+                  value: 1,
+                  coverage: 95,
+                  fullPath: 'gazebo/pages/temp1/temp2/file13.tsx',
+                },
+                {
+                  name: 'file14.tsx',
+                  value: 1,
+                  coverage: 100,
+                  fullPath: 'gazebo/pages/temp1/temp2/file14.tsx',
+                },
+                {
+                  name: 'temp3',
+                  fullPath: 'gazebo/pages/temp1/temp2/temp3',
+                  dir: true,
+                  children: [
+                    {
+                      name: 'file15.tsx',
+                      value: 1,
+                      coverage: 95,
+                      fullPath: 'gazebo/pages/temp1/temp2/temp3/file15.tsx',
+                    },
+                    {
+                      name: 'file16.tsx',
+                      value: 1,
+                      coverage: 100,
+                      fullPath: 'gazebo/pages/temp1/temp2/temp3/file16.tsx',
+                    },
+                    {
+                      name: 'file17.tsx',
+                      value: 1,
+                      coverage: 80,
+                      fullPath: 'gazebo/pages/temp1/temp2/temp3/file17.tsx',
+                    },
+                    {
+                      name: 'file18.tsx',
+                      value: 1,
+                      coverage: 15,
+                      fullPath: 'gazebo/pages/temp1/temp2/temp3/file18.tsx',
+                    },
+                    {
+                      name: 'file19.tsx',
+                      value: 1,
+                      coverage: 10,
+                      fullPath: 'gazebo/pages/temp1/temp2/temp3/file19.tsx',
+                    },
+                    {
+                      name: 'temp4',
+                      fullPath: 'gazebo/pages/temp1/temp2/temp3/temp4',
+                      dir: true,
+                      children: [
+                        {
+                          name: 'file20.tsx',
+                          value: 1,
+                          coverage: 100,
+                          fullPath: 'gazebo/pages/temp1/temp2/temp3/temp4/file20.tsx',
+                        },
+                        {
+                          name: 'file21.tsx',
+                          value: 1,
+                          coverage: 100,
+                          fullPath: 'gazebo/pages/temp1/temp2/temp3/temp4/file21.tsx',
+                        },
+                        {
+                          name: 'file22.tsx',
+                          value: 1,
+                          coverage: 100,
+                          fullPath: 'gazebo/pages/temp1/temp2/temp3/temp4/file22.tsx',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+            {
+              name: 'randomTemp',
+              fullPath: 'gazebo/pages/temp1/randomTemp',
+              dir: true,
+              children: [
+                {
+                  name: 'file10.tsx',
+                  value: 1,
+                  coverage: 40,
+                  fullPath: 'gazebo/pages/temp1/randomTemp/file10.tsx',
+                },
+                {
+                  name: 'file11.tsx',
+                  value: 1,
+                  coverage: 90,
+                  fullPath: 'gazebo/pages/temp1/randomTemp/file11.tsx',
+                },
+                {
+                  name: 'file12.tsx',
+                  value: 1,
+                  coverage: 92,
+                  fullPath: 'gazebo/pages/temp1/randomTemp/file12.tsx',
+                },
+                {
+                  name: 'file13.tsx',
+                  value: 1,
+                  coverage: 95,
+                  fullPath: 'gazebo/pages/temp1/randomTemp/file13.tsx',
+                },
+                {
+                  name: 'file14.tsx',
+                  value: 1,
+                  coverage: 100,
+                  fullPath: 'gazebo/pages/temp1/randomTemp/file14.tsx',
+                },
+                {
+                  name: 'temp3',
+                  fullPath: 'gazebo/pages/temp1/randomTemp/temp3',
+                  dir: true,
+                  children: [
+                    {
+                      name: 'file15.tsx',
+                      value: 1,
+                      coverage: 95,
+                      fullPath: 'gazebo/pages/temp1/randomTemp/temp3/file15.tsx',
+                    },
+                    {
+                      name: 'file16.tsx',
+                      value: 1,
+                      coverage: 100,
+                      fullPath: 'gazebo/pages/temp1/randomTemp/temp3/file16.tsx',
+                    },
+                    {
+                      name: 'file17.tsx',
+                      value: 1,
+                      coverage: 80,
+                      fullPath: 'gazebo/pages/temp1/randomTemp/temp3/file17.tsx',
+                    },
+                    {
+                      name: 'file18.tsx',
+                      value: 1,
+                      coverage: 15,
+                      fullPath: 'gazebo/pages/temp1/randomTemp/temp3/file18.tsx',
+                    },
+                    {
+                      name: 'file19.tsx',
+                      value: 1,
+                      coverage: 10,
+                      fullPath: 'gazebo/pages/temp1/randomTemp/temp3/file19.tsx',
+                    },
+                    {
+                      name: 'temp4',
+                      fullPath: 'gazebo/pages/temp1/randomTemp/temp3/temp4',
+                      dir: true,
+                      children: [
+                        {
+                          name: 'file20.tsx',
+                          value: 1,
+                          coverage: 100,
+                          fullPath:
+                            'gazebo/pages/temp1/randomTemp/temp3/temp4/file20.tsx',
+                        },
+                        {
+                          name: 'file21.tsx',
+                          value: 1,
+                          coverage: 100,
+                          fullPath:
+                            'gazebo/pages/temp1/randomTemp/temp3/temp4/file21.tsx',
+                        },
+                        {
+                          name: 'file22.tsx',
+                          value: 1,
+                          coverage: 100,
+                          fullPath:
+                            'gazebo/pages/temp1/randomTemp/temp3/temp4/file22.tsx',
+                        },
+                      ],
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'file23.tsx',
+      value: 1,
+      coverage: 50,
+      fullPath: 'gazebo/file23.tsx',
+    },
+  ],
+};
