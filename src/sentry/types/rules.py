6a6b82a2217c0746eb73e7cfd443c83fde710998
Raj Joshi <raj.joshi@sentry.io>
2025-04-24 09:30:43 -0700

    :wrench: chore(notifs): move notif utils out of __init__.py to prevent circular deps (#90270)

--- src/sentry/types/rules.py
+++ src/sentry/types/rules.py
@@ -1,3 +1,12 @@
 from collections import namedtuple
+from dataclasses import dataclass
 
 RuleFuture = namedtuple("RuleFuture", ["rule", "kwargs"])
+
+
+@dataclass
+class NotificationRuleDetails:
+    id: int
+    label: str
+    url: str
+    status_url: str
