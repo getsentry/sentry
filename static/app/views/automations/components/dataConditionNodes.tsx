3b67770feffebd1780a8695f939397323b0e184d
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-04-24 18:00:14 -0400

    ref(aci): create shared `RuleRow` component for data conditions and actions (#90238)
    made a shared `RuleRow` component that can be used for both data
    condition nodes and action nodes in the automation builder, and moved
    inline form fields to `components/workflowEngine/form`
    
    also renamed the `RuleNode` component to be `DataConditionNode` to be
    more clear, since we'll be adding an `ActionNode` soon for the actions
    dropdown

--- static/app/views/automations/components/dataConditionNodes.tsx
+++ static/app/views/automations/components/dataConditionNodes.tsx
@@ -1,9 +1,5 @@
 import {createContext, useContext} from 'react';
-import styled from '@emotion/styled';
 
-import InputField from 'sentry/components/forms/fields/inputField';
-import NumberField from 'sentry/components/forms/fields/numberField';
-import SelectField from 'sentry/components/forms/fields/selectField';
 import {t} from 'sentry/locale';
 import {
   DataConditionType,
@@ -102,40 +98,3 @@ export const dataConditionNodesMap = new Map<DataConditionType, DataConditionNod
     },
   ],
 ]);
-
-export const InlineInputField = styled(InputField)`
-  padding: 0;
-  width: 180px;
-  height: 28px;
-  min-height: 28px;
-  > div {
-    padding-left: 0;
-  }
-`;
-
-export const InlineNumberInput = styled(NumberField)`
-  padding: 0;
-  width: 90px;
-  height: 28px;
-  min-height: 28px;
-  > div {
-    padding-left: 0;
-  }
-`;
-
-export const selectControlStyles = {
-  control: (provided: any) => ({
-    ...provided,
-    minHeight: '28px',
-    height: '28px',
-    padding: 0,
-  }),
-};
-
-export const InlineSelectControl = styled(SelectField)`
-  width: 180px;
-  padding: 0;
-  > div {
-    padding-left: 0;
-  }
-`;
