d0162bd7db1facbe33900c35a3b3d4f9ddbe824d
Brendan Hy <brendan.hy@sentry.io>
2025-04-24 14:47:32 -0700

    feat(ftc): add disclaimer to invoices fe (#90306)
    closes
    https://linear.app/getsentry/issue/BIL-503/add-disclaimer-to-invoice-view
    
    annual payg
    ![Screenshot 2025-04-24 at 11 47
    02 AM](https://github.com/user-attachments/assets/e7782de1-4b2d-4397-960f-901dfd0bc3d5)
    
    monthly on-demand
    ![Screenshot 2025-04-24 at 11 46
    32 AM](https://github.com/user-attachments/assets/d179db9f-6730-44e5-9839-3eb2b3b6ed4b)

--- static/gsApp/views/invoiceDetails/index.spec.tsx
+++ static/gsApp/views/invoiceDetails/index.spec.tsx
@@ -10,6 +10,7 @@ import {
   waitFor,
 } from 'sentry-test/reactTestingLibrary';
 
+import {PlanFixture} from 'getsentry/__fixtures__/plan';
 import SubscriptionStore from 'getsentry/stores/subscriptionStore';
 import {InvoiceItemType} from 'getsentry/types';
 import InvoiceDetails from 'getsentry/views/invoiceDetails';
@@ -86,6 +87,37 @@ describe('InvoiceDetails', function () {
     expect(screen.getByText('Oct 21, 2021')).toBeInTheDocument();
     expect(screen.getByText('Sep 20, 2021')).toBeInTheDocument();
     expect(screen.getByText('$89.00 USD')).toBeInTheDocument();
+    expect(
+      screen.getByText(
+        /Your subscription will automatically renew on or about the same day each month and your credit card on file will be charged the recurring subscription fees set forth above. In addition to recurring subscription fees, you may also be charged for monthly on-demand fees. You may cancel your subscription at any time /
+      )
+    ).toBeInTheDocument();
+  });
+
+  it('renders disclaimer with annual billing', async function () {
+    const annualInvoice = InvoiceFixture(
+      {
+        customer: SubscriptionFixture({
+          organization,
+          billingInterval: 'annual',
+          planDetails: PlanFixture({budgetTerm: 'pay-as-you-go'}),
+        }),
+      },
+      organization
+    );
+    const mockapi = MockApiClient.addMockResponse({
+      url: `/customers/${organization.slug}/invoices/${annualInvoice.id}/`,
+      method: 'GET',
+      body: annualInvoice,
+    });
+    render(<InvoiceDetails {...routerProps} params={params} />);
+    await waitFor(() => expect(mockapi).toHaveBeenCalled());
+
+    expect(
+      screen.getByText(
+        /Your subscription will automatically renew on or about the same day each year and your credit card on file will be charged the recurring subscription fees set forth above. In addition to recurring subscription fees, you may also be charged for monthly pay-as-you-go fees. You may cancel your subscription at any time /
+      )
+    ).toBeInTheDocument();
   });
 
   it('renders credit applied', async function () {
