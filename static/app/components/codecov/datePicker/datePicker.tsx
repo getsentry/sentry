1cebab205e7a4158fbbcf10550204e628390e49b
Adrian <adrian.bonilla@sentry.io>
2025-04-25 12:25:09 -0700

    test

--- static/app/components/codecov/datePicker/datePicker.tsx
+++ static/app/components/codecov/datePicker/datePicker.tsx
@@ -16,6 +16,7 @@ export const CODECOV_DEFAULT_RELATIVE_PERIODS = {
   '30d': t('Last 30 days'),
 };
 
+// a
 interface DatePickerProps
   extends Partial<Partial<Omit<DateSelectorProps, 'relative' | 'menuBody'>>> {}
 
