79993f893510b69bbdc63ea97d069a85100b30cc
Raj Joshi <raj.joshi@sentry.io>
2025-04-27 15:14:06 -0700

    :bug: fix: typing

--- src/sentry/digests/notifications.py
+++ src/sentry/digests/notifications.py
@@ -11,8 +11,8 @@ from sentry.eventstore.models import Event
 from sentry.models.group import Group, GroupStatus
 from sentry.models.project import Project
 from sentry.models.rule import Rule
-from sentry.notifications.notifications.rules import get_key_from_rule_data
 from sentry.notifications.types import ActionTargetType, FallthroughChoiceType
+from sentry.notifications.utils.rules import get_key_from_rule_data
 from sentry.tsdb.base import TSDBModel
 
 logger = logging.getLogger("sentry.digests")
