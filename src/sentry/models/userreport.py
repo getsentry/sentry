a74b1e111dfea08ba34757d1594f2d908959fbd2
Enoch Tang <enoch.tang@sentry.io>
2025-04-28 15:38:15 -0400

    convert datetimes to timestamps before sending task

--- src/sentry/models/userreport.py
+++ src/sentry/models/userreport.py
@@ -37,7 +37,17 @@ class UserReport(Model):
         from sentry.api.serializers import UserReportWithGroupSerializer, serialize
         from sentry.tasks.user_report import user_report
 
+        def convert_datetime_in_dict_to_timestamp(obj):
+            if isinstance(obj, dict):
+                return {k: convert_datetime_in_dict_to_timestamp(v) for k, v in obj.items()}
+            elif isinstance(obj, timezone.datetime):
+                return obj.timestamp()
+            return obj
+
+        report = serialize(self, AnonymousUser(), UserReportWithGroupSerializer())
+        report = convert_datetime_in_dict_to_timestamp(report)
+
         user_report.delay(
             project_id=self.project_id,
-            report=serialize(self, AnonymousUser(), UserReportWithGroupSerializer()),
+            report=report,
         )
