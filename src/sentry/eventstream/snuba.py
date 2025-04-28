2bcd7577815dac8cfab943057aaca144fcaf5313
Evan Hicks <evanh@users.noreply.github.com>
2025-04-28 13:37:11 -0400

    fix(Taskworker): Send strings to the delete_groups task (#90493)
    Instead of implicitly encoding datetimes to str, pass an encoded string.
    Also modify the tests to
    stop sending unencoded hex strings.
    
    This also exposes a toggle TASK_TYPE_CHECKING to enforce stricter task
    typing when running tests.

--- src/sentry/eventstream/snuba.py
+++ src/sentry/eventstream/snuba.py
@@ -211,7 +211,7 @@ class SnubaProtocolEventStream(EventStream):
             "transaction_id": uuid4().hex,
             "project_id": project_id,
             "group_ids": list(group_ids),
-            "datetime": datetime.now(tz=timezone.utc),
+            "datetime": json.datetime_to_str(datetime.now(tz=timezone.utc)),
         }
 
         self._send(project_id, "start_delete_groups", extra_data=(state,), asynchronous=False)
