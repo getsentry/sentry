612e7fab2596dabd2a825537c1dc1b35d5ab2061
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-04-29 12:18:47 -0400

    fix

--- tests/snuba/api/endpoints/test_organization_events_span_indexed.py
+++ tests/snuba/api/endpoints/test_organization_events_span_indexed.py
@@ -3105,7 +3105,7 @@ class OrganizationEventsEAPRPCSpanEndpointTest(OrganizationEventsSpanIndexedEndp
         meta = response.data["meta"]
         assert len(data) == 1
         assert data[0]["division(mobile.slow_frames,mobile.total_frames)"] == 10 / 100
-        assert data[0]["division(mobile.frozen_frames,mobile.total_frames))"] == 20 / 100
+        assert data[0]["division(mobile.frozen_frames,mobile.total_frames)"] == 20 / 100
         assert meta["dataset"] == self.dataset
 
     def test_opportunity_score_zero_scores(self):
