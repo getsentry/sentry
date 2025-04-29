6763f534a7f7276918aae66305fb46ff57ccd8c5
Calvin Yau <calvin.yau@sentry.io>
2025-04-28 17:07:07 -0700

    feat(codecov): Add TA preonboarding

---
+++ static/app/views/codecov/tests/preOnboarding.tsx
@@ -0,0 +1,206 @@
+import {css} from '@emotion/react';
+import styled from '@emotion/styled';
+
+import testsAnalyticsSummary from 'sentry-images/features/test-analytics-summary.svg';
+
+import {LinkButton} from 'sentry/components/core/button';
+import Link from 'sentry/components/links/link';
+import {IconGithub} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+
+const INSTRUCTIONS_TEXT = {
+  noOrgs: {
+    header: t('Add your GitHub Organization to Sentry'),
+    subtext: t(
+      "You need to install the Sentry app on your GitHub organization as an admin. If you're not an admin, you will need to make sure your GitHub organization admins approve the installation of the Sentry app on GitHub."
+    ),
+    mainCTA: t('Add installation'),
+    mainCTALink: 'https://github.com/apps/sentry-io',
+  },
+  hasOrgs: {
+    header: t('Get Started with Test Analytics'),
+    subtext: t(
+      'Test Analytics offers data on test run times, failure rates, and identifies flaky tests to help decrease the risk of deployment failures and make it easier to ship new features quickly. '
+    ),
+    mainCTA: t('Review permissions'),
+    // TODO: can we get the xxx installation id? it is unique to each signed in GH user
+    mainCTALink: '/settings/installations/xxxx/permissions/update',
+  },
+};
+
+// TODO: change this
+const HAS_INTEGRATED_ORGS = false;
+
+// TODO: Add missing links
+export default function TestPreOnboardingPage() {
+  const instructionSet = HAS_INTEGRATED_ORGS
+    ? INSTRUCTIONS_TEXT.hasOrgs
+    : INSTRUCTIONS_TEXT.noOrgs;
+  return (
+    <LayoutGap>
+      <IntroSection>
+        <img src={testsAnalyticsSummary} />
+        <div>
+          <h2>{t('Say Goodbye to Flaky Tests')}</h2>
+          <StyledP
+            css={css`
+              margin-bottom: ${space(1)};
+            `}
+          >
+            {t(
+              'Test Analytics offers data on test run times, failure rates, and identifies flaky tests to help decrease the risk of deployment failures and make it easier to ship new features quickly. '
+            )}
+            <Link to="https://docs.codecov.com/docs/test-analytics">
+              {t('Learn more')}
+            </Link>
+          </StyledP>
+          <ItalicP>
+            {t(
+              'No more staring at failing tests wondering, “Is it me, or is it just flaky?”'
+            )}
+          </ItalicP>
+        </div>
+      </IntroSection>
+      <InstructionsSection>
+        <h2>{instructionSet.header}</h2>
+        <p>{instructionSet.subtext}</p>
+        <ButtonBar>
+          <LinkButton priority="primary" href={instructionSet.mainCTA}>
+            {instructionSet.mainCTA}
+          </LinkButton>
+          <LinkButton priority="default" href="/settings/integrations/github">
+            Learn more
+          </LinkButton>
+        </ButtonBar>
+        <PrerequisitesSection>
+          <PrerequisitesTitle>
+            {t('Prerequisites to connect your GitHub organization:')}
+          </PrerequisitesTitle>
+          <Prerequisites>
+            <Prereq>
+              <PrereqMainText>{t('Enable GitHub as an Auth Provider')}</PrereqMainText>
+              <PrereqSubText>
+                {t(
+                  "Sentry Prevent analyzes your code through your Git provider. You'll need to authenticate to access data from your organizations."
+                )}
+              </PrereqSubText>
+            </Prereq>
+            <Prereq>
+              <LinkButton
+                priority="default"
+                icon={<IconGithub />}
+                href="https://github.com"
+              >
+                {t('Sign in with GitHub')}
+              </LinkButton>
+            </Prereq>
+            <Prereq>
+              <PrereqMainText>{t('Install the GitHub Sentry App')}</PrereqMainText>
+              <PrereqSubText>
+                <Link to="">{t('Install the app')}</Link>
+                {t(
+                  " on your GitHub org in your Sentry org. You will need to be an Owner of your GitHub organization to fully configure the integration. Note: Once linked, a GitHub org/account can't be connected to another Sentry org."
+                )}
+              </PrereqSubText>
+            </Prereq>
+            <Prereq>
+              <PrereqMainText>
+                {t('Connect your GitHub identities in Sentry')}
+              </PrereqMainText>
+              <PrereqSubText>
+                {t('In your Sentry ')}
+                <Link to="https://sentry.io/settings/account/identities">
+                  {t('identities')}
+                </Link>
+                {t(
+                  " settings, link your GitHub account to your profile. If you're having trouble adding the integration, "
+                )}
+                <Link to="https://sentry.io/settings/account/identities">
+                  {t('disconnect')}
+                </Link>
+                {t(' disconnect then ')}
+                <Link to="">{t('reconnect')}</Link>
+                {t(' your GitHub identity.')}
+              </PrereqSubText>
+            </Prereq>
+          </Prerequisites>
+        </PrerequisitesSection>
+      </InstructionsSection>
+    </LayoutGap>
+  );
+}
+
+const LayoutGap = styled('div')`
+  display: grid;
+  gap: ${space(2)};
+`;
+
+const StyledP = styled('p')`
+  margin-bottom: ${space(1)};
+`;
+
+const ItalicP = styled('p')`
+  font-style: italic;
+  color: ${p => p.theme.gray500};
+`;
+
+const IntroSection = styled('div')`
+  display: flex;
+  border: 1px solid rgba(224, 220, 229, 1);
+  border-radius: 10px;
+  justify-content: space-around;
+  gap: 90px;
+  padding: 44px;
+
+  @media (max-width: ${p => p.theme.breakpoints.medium}) {
+    flex-direction: column;
+    align-items: center;
+    gap: ${space(3)};
+  }
+`;
+
+const InstructionsSection = styled('div')`
+  display: grid;
+  border: 1px solid rgba(224, 220, 229, 1);
+  border-radius: 10px;
+  padding: 24px 40px;
+`;
+
+const ButtonBar = styled('div')`
+  display: flex;
+  gap: ${space(2)};
+`;
+
+const PrerequisitesSection = styled('div')`
+  border-top: 1px solid ${p => p.theme.border};
+  margin-top: 24px;
+  padding-top: ${space(3)};
+`;
+
+const Prerequisites = styled('div')`
+  background-color: ${p => p.theme.backgroundSecondary};
+  padding: 24px;
+  border: 1px solid ${p => p.theme.border};
+  border-radius: 10px;
+  margin-bottom: ${space(1.5)};
+  gap: ${space(1.5)};
+`;
+
+const Prereq = styled('div')`
+  margin-bottom: ${space(1.5)};
+`;
+
+const PrerequisitesTitle = styled('p')`
+  font-size: 16px;
+`;
+
+const PrereqMainText = styled('p')`
+  font-weight: 600;
+  margin: 0;
+`;
+
+const PrereqSubText = styled('p')`
+  font-weight: 400;
+  margin: 0;
+`;
