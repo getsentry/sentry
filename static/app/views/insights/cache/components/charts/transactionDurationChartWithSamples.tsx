43d6ca46ec6c0f5032490339e4802fcbf208d0a5
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2025-04-30 12:46:35 -0400

    fix(insights): cache module sample panel error on eap (#90625)

--- static/app/views/insights/cache/components/charts/transactionDurationChartWithSamples.tsx
+++ static/app/views/insights/cache/components/charts/transactionDurationChartWithSamples.tsx
@@ -5,8 +5,18 @@ import useLocationQuery from 'sentry/utils/url/useLocationQuery';
 import type {Samples} from 'sentry/views/dashboards/widgets/timeSeriesWidget/plottables/samples';
 import {Referrer} from 'sentry/views/insights/cache/referrers';
 import {InsightsLineChartWidget} from 'sentry/views/insights/common/components/insightsLineChartWidget';
-import {useMetricsSeries} from 'sentry/views/insights/common/queries/useDiscoverSeries';
-import type {MetricsQueryFilters} from 'sentry/views/insights/types';
+import type {DiscoverSeries} from 'sentry/views/insights/common/queries/useDiscoverSeries';
+import {
+  useEAPSeries,
+  useMetricsSeries,
+} from 'sentry/views/insights/common/queries/useDiscoverSeries';
+import {useInsightsEap} from 'sentry/views/insights/common/utils/useEap';
+import {
+  MetricsFields,
+  type MetricsQueryFilters,
+  SpanFields,
+  type SpanQueryFilters,
+} from 'sentry/views/insights/types';
 
 type Props = {
   samples: Samples;
@@ -20,18 +30,7 @@ export function TransactionDurationChartWithSamples({samples}: Props) {
     },
   });
 
-  const search: MetricsQueryFilters = {
-    transaction,
-  };
-
-  const {data, isPending, error} = useMetricsSeries(
-    {
-      yAxis: ['avg(transaction.duration)'],
-      search: MutableSearch.fromQueryObject(search),
-      transformAliasToInputFormat: true,
-    },
-    Referrer.SAMPLES_CACHE_TRANSACTION_DURATION_CHART
-  );
+  const {data, isPending, error} = useTransactionDurationSeries({transaction});
 
   return (
     <InsightsLineChartWidget
@@ -39,8 +38,44 @@ export function TransactionDurationChartWithSamples({samples}: Props) {
       title={t('Average Transaction Duration')}
       isLoading={isPending}
       error={error}
-      series={[data['avg(transaction.duration)']]}
+      series={[data['avg(span.duration)']]}
       samples={samples}
     />
   );
 }
+
+export const useTransactionDurationSeries = ({transaction}: {transaction: string}) => {
+  const useEap = useInsightsEap();
+
+  const metricsResult = useMetricsSeries(
+    {
+      yAxis: [`avg(${MetricsFields.TRANSACTION_DURATION})`],
+      search: MutableSearch.fromQueryObject({
+        transaction,
+      } satisfies MetricsQueryFilters),
+      transformAliasToInputFormat: true,
+    },
+    Referrer.SAMPLES_CACHE_TRANSACTION_DURATION_CHART
+  );
+
+  const eapResult = useEAPSeries(
+    {
+      search: MutableSearch.fromQueryObject({
+        transaction,
+        is_transaction: 'true',
+      } satisfies SpanQueryFilters),
+      yAxis: [`avg(${SpanFields.SPAN_DURATION})`],
+    },
+    Referrer.SAMPLES_CACHE_TRANSACTION_DURATION
+  );
+
+  const result = useEap ? eapResult : metricsResult;
+  const finalData: {['avg(span.duration)']: DiscoverSeries} = useEap
+    ? eapResult.data
+    : {
+        [`avg(${SpanFields.SPAN_DURATION})`]:
+          metricsResult.data['avg(transaction.duration)'],
+      };
+
+  return {...result, data: finalData};
+};
