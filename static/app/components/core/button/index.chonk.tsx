f3901b9e40dde462c478875d7e322bc76ac9ecc8
Jonas <jonas.badalic@sentry.io>
2025-04-30 07:50:32 -0700

    button: fix chonk label sizing (#90617)
    This used to be full width via display: flex, so we need to put it back,
    otherwise the text ends up being centered in some buttons due to
    justify-content: center

--- static/app/components/core/button/index.chonk.tsx
+++ static/app/components/core/button/index.chonk.tsx
@@ -106,7 +106,7 @@ export function getChonkButtonStyles(
       display: 'inherit',
       alignItems: 'inherit',
       justifyContent: 'inherit',
-      flex: 'inherit',
+      flex: '1',
       gap: 'inherit',
 
       whiteSpace: 'nowrap',
