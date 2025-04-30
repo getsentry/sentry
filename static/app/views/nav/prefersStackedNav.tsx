7dcb3a722edcb2465facf482363ad29957bdc1be
MichaelSun48 <michael.sun@sentry.io>
2025-04-30 10:34:08 -0700

    Correct logic and user option type

--- static/app/views/nav/prefersStackedNav.tsx
+++ static/app/views/nav/prefersStackedNav.tsx
@@ -3,21 +3,33 @@ import type {Organization} from 'sentry/types/organization';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useUser} from 'sentry/utils/useUser';
 
+// IMPORTANT:
+// These two functions NEED to have the same logic. Make sure to update both if you are changing this logic.
+
 export function prefersStackedNav(organization: Organization) {
-  return (
-    organization.features.includes('enforce-stacked-sidenav') ??
-    ConfigStore.get('user')?.options?.prefersStackedNavigation ??
-    false
-  );
+  const userStackedNavOption = ConfigStore.get('user')?.options?.prefersStackedNavigation;
+
+  if (
+    userStackedNavOption !== false &&
+    organization.features.includes('enforce-stacked-navigation')
+  ) {
+    return true;
+  }
+
+  return userStackedNavOption ?? false;
 }
 
 export function usePrefersStackedNav() {
   const user = useUser();
   const organization = useOrganization();
+  const userStackedNavOption = user?.options?.prefersStackedNavigation;
 
-  return (
-    organization.features.includes('enforce-stacked-sidenav') ??
-    user?.options?.prefersStackedNavigation ??
-    false
-  );
+  if (
+    userStackedNavOption !== false &&
+    organization.features.includes('enforce-stacked-navigation')
+  ) {
+    return true;
+  }
+
+  return userStackedNavOption ?? false;
 }
