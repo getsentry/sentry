30cd154c10f866a567bdf010dcfce5c62930028e
Nikki Kapadia <72356613+nikkikapadia@users.noreply.github.com>
2025-04-30 14:16:19 -0400

    fix(schema-hints): Add aggregates hints functionality (#90442)
    I didn't realize before but the aggregate hints in the drawer were not
    being added to the search bar properly. They were being added like
    regular hints but were then showing errors because they're not valid
    filters and needed parameters. I've added a check to the code that
    checks for aggregate tags and adds the default params to it. I've also
    added text indication in the drawer for functions. Here's what it looks
    like (only applicable to aggregate view):
    
    | Before | After |
    |--------|--------|
    | <img width="383" alt="image"
    src="https://github.com/user-attachments/assets/75d59c20-6873-4d9a-b575-4a462ca44252"
    /> | <img width="475" alt="image"
    src="https://github.com/user-attachments/assets/f555ec6c-8c90-4b7c-8e6e-65d82e8609be"
    /> |

--- static/app/views/explore/components/schemaHints/schemaHintsList.spec.tsx
+++ static/app/views/explore/components/schemaHints/schemaHintsList.spec.tsx
@@ -1,7 +1,7 @@
 import {render, screen, userEvent, within} from 'sentry-test/reactTestingLibrary';
 
 import type {TagCollection} from 'sentry/types/group';
-import {FieldKind} from 'sentry/utils/fields';
+import {AggregationKey, FieldKind} from 'sentry/utils/fields';
 import SchemaHintsList from 'sentry/views/explore/components/schemaHints/schemaHintsList';
 import {
   PageParamsProvider,
@@ -89,23 +89,19 @@ describe('SchemaHintsList', () => {
       <Subject
         stringTags={mockStringTags}
         numberTags={mockNumberTags}
-        supportedAggregates={[]}
-      />,
-      {
-        deprecatedRouterMocks: true,
-      }
+        supportedAggregates={[AggregationKey.COUNT]}
+      />
     );
 
     const container = screen.getByLabelText('Schema Hints List');
     const withinContainer = within(container);
     expect(withinContainer.getByText('stringTag1')).toBeInTheDocument();
     expect(withinContainer.getByText('stringTag2')).toBeInTheDocument();
-    // counting the has tag
-    expect(withinContainer.getAllByText('is')).toHaveLength(3);
+    expect(withinContainer.getAllByText('is')).toHaveLength(2);
     expect(withinContainer.getByText('numberTag1')).toBeInTheDocument();
     expect(withinContainer.getByText('numberTag2')).toBeInTheDocument();
-    expect(withinContainer.getAllByText('>')).toHaveLength(2);
-    // counting the has tag
+    expect(withinContainer.getByText('count(...)')).toBeInTheDocument();
+    expect(withinContainer.getAllByText('>')).toHaveLength(3);
     expect(withinContainer.getAllByText('...')).toHaveLength(5);
     expect(withinContainer.getByText('See full list')).toBeInTheDocument();
   });
@@ -197,7 +193,56 @@ describe('SchemaHintsList', () => {
     });
   });
 
+  it('should properly add aggregate hint to query', async () => {
+    render(
+      <Subject
+        stringTags={mockStringTags}
+        numberTags={mockNumberTags}
+        supportedAggregates={[AggregationKey.COUNT_UNIQUE]}
+      />
+    );
+    const countUniquePill = screen.getByText('count_unique(...)');
+    await userEvent.click(countUniquePill);
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: 'UPDATE_QUERY',
+      query: 'count_unique(user):>0',
+      focusOverride: {
+        itemKey: 'filter:0',
+        part: 'value',
+      },
+    });
+
+    const seeFullList = screen.getByText('See full list');
+    await userEvent.click(seeFullList);
+
+    const withinDrawer = within(screen.getByLabelText('Schema Hints Drawer'));
+    const countUniqueCheckbox = withinDrawer.getByText('count_unique(...)');
+    await userEvent.click(countUniqueCheckbox);
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: 'UPDATE_QUERY',
+      query: 'count_unique(user):>0',
+      focusOverride: {
+        itemKey: 'filter:0',
+        part: 'value',
+      },
+    });
+  });
+
   it('should remove hint from query when checkbox is unchecked on drawer', async () => {
+    const mockUseSearchQueryBuilder = jest
+      .spyOn(
+        require('sentry/components/searchQueryBuilder/context'),
+        'useSearchQueryBuilder'
+      )
+      .mockImplementation(() => ({
+        query: '!stringTag1:"" numberTag1:>0',
+        getTagValues: () => Promise.resolve(['tagValue1', 'tagValue2']),
+        dispatch: mockDispatch,
+        wrapperRef: {current: null},
+      }));
+
     render(
       <Subject
         stringTags={mockStringTags}
@@ -215,12 +260,54 @@ describe('SchemaHintsList', () => {
 
     expect(mockDispatch).toHaveBeenCalledWith({
       type: 'UPDATE_QUERY',
-      query: 'stringTag1:""',
+      query: 'numberTag1:>0',
       focusOverride: {
-        itemKey: 'filter:0',
+        itemKey: 'filter:-1',
         part: 'value',
       },
     });
+
+    mockUseSearchQueryBuilder.mockRestore();
+  });
+
+  it('should remove aggregate hint from query when checkbox is unchecked on drawer', async () => {
+    const mockUseSearchQueryBuilder = jest
+      .spyOn(
+        require('sentry/components/searchQueryBuilder/context'),
+        'useSearchQueryBuilder'
+      )
+      .mockImplementation(() => ({
+        query: 'stringTag1:"" numberTag1:>0 count_unique(user):>0',
+        getTagValues: () => Promise.resolve(['tagValue1', 'tagValue2']),
+        dispatch: mockDispatch,
+        wrapperRef: {current: null},
+      }));
+
+    render(
+      <Subject
+        stringTags={mockStringTags}
+        numberTags={mockNumberTags}
+        supportedAggregates={[AggregationKey.COUNT_UNIQUE]}
+      />
+    );
+
+    const seeFullList = screen.getByText('See full list');
+    await userEvent.click(seeFullList);
+
+    const withinDrawer = within(screen.getByLabelText('Schema Hints Drawer'));
+    const countUniqueCheckbox = withinDrawer.getByText('count_unique(...)');
+    await userEvent.click(countUniqueCheckbox);
+
+    expect(mockDispatch).toHaveBeenCalledWith({
+      type: 'UPDATE_QUERY',
+      query: 'stringTag1:"" numberTag1:>0',
+      focusOverride: {
+        itemKey: 'filter:-1',
+        part: 'value',
+      },
+    });
+
+    mockUseSearchQueryBuilder.mockRestore();
   });
 
   it('should keep drawer open when query is updated', async () => {
