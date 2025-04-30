7ea8151d0631e90262fe868b0971f30690462cba
Nick <nicholas.deschenes@sentry.io>
2025-04-30 08:42:16 -0300

    feat(codecov): Add TA CI efficiency panel (#90597)
    This PR adds in some helpful formatter utils for our summary panels that
    we'll be adding in to the various Codecov pages. As well, this PR adds
    in the CI efficiency panel for the test analytics page.

---
+++ static/app/views/codecov/tests/summaries/ciEfficiency.tsx
@@ -0,0 +1,127 @@
+import {Fragment} from 'react';
+import styled from '@emotion/styled';
+
+import {
+  SummaryEntries,
+  SummaryEntry,
+  SummaryEntryLabel,
+  SummaryEntryValue,
+  SummaryEntryValueLink,
+} from 'sentry/components/codecov/summary';
+import {Tag} from 'sentry/components/core/badge/tag';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import Panel from 'sentry/components/panels/panel';
+import PanelBody from 'sentry/components/panels/panelBody';
+import PanelHeader from 'sentry/components/panels/panelHeader';
+import {t} from 'sentry/locale';
+import {formatPercentRate, formatTimeDuration} from 'sentry/utils/formatters';
+
+function TotalTestsRunTimeTooltip() {
+  return (
+    <Fragment>
+      <p>
+        <ToolTipTitle>Impact:</ToolTipTitle>
+        The cumulative CI time spent running tests over the last <strong>30 days</strong>.
+      </p>
+      <p>
+        <ToolTipTitle>What is it:</ToolTipTitle>
+        The total time it takes to run all your tests.
+      </p>
+    </Fragment>
+  );
+}
+
+interface SlowestTestsTooltipProps {
+  slowestTestsDuration: number;
+}
+
+function SlowestTestsTooltip({slowestTestsDuration}: SlowestTestsTooltipProps) {
+  return (
+    <Fragment>
+      <p>
+        <ToolTipTitle>Impact:</ToolTipTitle>
+        The slowest 100 tests take{' '}
+        <strong>{formatTimeDuration(slowestTestsDuration)}</strong> to run.
+      </p>
+      <p>
+        <ToolTipTitle>What is it:</ToolTipTitle>
+        Lists the tests that take more than the 95th percentile run time to complete.
+        Showing a max of 100 tests.
+      </p>
+    </Fragment>
+  );
+}
+
+const ToolTipTitle = styled('strong')`
+  display: block;
+`;
+
+interface CIEfficiencyBodyProps {
+  slowestTests: number;
+  slowestTestsDuration: number;
+  totalTestsRunTime: number;
+  totalTestsRunTimeChange: number;
+}
+
+function CIEfficiencyBody({
+  totalTestsRunTime,
+  totalTestsRunTimeChange,
+  slowestTestsDuration,
+  slowestTests,
+}: CIEfficiencyBodyProps) {
+  return (
+    <SummaryEntries largeColumnSpan={2} smallColumnSpan={1}>
+      <SummaryEntry>
+        <SummaryEntryLabel showUnderline body={<TotalTestsRunTimeTooltip />}>
+          {t('Total Tests Run Time')}
+        </SummaryEntryLabel>
+        <div>
+          <SummaryEntryValue>
+            {formatTimeDuration(totalTestsRunTime)}
+            <Tag type={totalTestsRunTimeChange > 0 ? 'error' : 'success'}>
+              {formatPercentRate(totalTestsRunTimeChange)}
+            </Tag>
+          </SummaryEntryValue>
+        </div>
+      </SummaryEntry>
+      <SummaryEntry>
+        <SummaryEntryLabel
+          showUnderline
+          body={<SlowestTestsTooltip slowestTestsDuration={slowestTestsDuration} />}
+        >
+          {t('Slowest Tests')}
+        </SummaryEntryLabel>
+        <SummaryEntryValueLink filterBy="slowest_tests">
+          {slowestTests}
+        </SummaryEntryValueLink>
+      </SummaryEntry>
+    </SummaryEntries>
+  );
+}
+
+interface CIEfficiencyProps {
+  isLoading: boolean;
+  slowestTests: number;
+  slowestTestsDuration: number;
+  totalTestsRunTime: number;
+  totalTestsRunTimeChange: number;
+}
+
+export function CIEfficiency({isLoading, ...bodyProps}: CIEfficiencyProps) {
+  return (
+    <CIEfficiencyPanel>
+      <PanelHeader>{t('CI Efficiency')}</PanelHeader>
+      <PanelBody>
+        {isLoading ? <LoadingIndicator /> : <CIEfficiencyBody {...bodyProps} />}
+      </PanelBody>
+    </CIEfficiencyPanel>
+  );
+}
+
+const CIEfficiencyPanel = styled(Panel)`
+  grid-column: span 24;
+
+  @media (min-width: ${p => p.theme.breakpoints.medium}) {
+    grid-column: span 9;
+  }
+`;
