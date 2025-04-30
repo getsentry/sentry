getsentry/sentry

commit 5ed520af5f4d2a00b49d10fe6ceb61c35e42b80d

feat(issues): Replace Java munging logic (#90518)
The new code works better since it is based on the symbolicator logic.

src/sentry/utils/event_frames.py
@@ -1,12 +1,16 @@
 from __future__ import annotations
 
 import inspect
+import logging
 from collections.abc import Callable, Mapping, MutableMapping, Sequence
 from copy import deepcopy
 from dataclasses import dataclass, field
 from typing import Any, Protocol, cast
 
+from sentry.utils import metrics
 from sentry.utils.safe import PathSearchable, get_path
+
+logger = logging.getLogger(__name__)
 
 
 @dataclass(frozen=True)
@@ -56,6 +60,21 @@ def java_frame_munger(frame: EventFrame) -> str | None:
         module[-1] = frame.filename
         return "/".join(module)
     return None
+
+
+def java_new_logic_frame_munger(frame: EventFrame) -> str | None:
+    if not frame.module or not frame.abs_path:
+        return None
+
+    from sentry.issues.auto_source_code_config.code_mapping import get_path_from_module
+
+    try:
+        _, stacktrace_path = get_path_from_module(frame.module, frame.abs_path)
+    except Exception:
+        # Report but continue
+        logger.exception("Investigate. Error munging java frame")
+        return None
+    return stacktrace_path
 
 
 def cocoa_frame_munger(frame: EventFrame) -> str | None:
@@ -108,6 +127,7 @@ def package_relative_path(abs_path: str | None, package: str | None) -> str | No
 
 PLATFORM_FRAME_MUNGER: dict[str, SdkFrameMunger] = {
     "java": SdkFrameMunger(java_frame_munger),
+    "java-new-logic": SdkFrameMunger(java_new_logic_frame_munger),
     "cocoa": SdkFrameMunger(cocoa_frame_munger),
     "other": SdkFrameMunger(flutter_frame_munger, True, {"sentry.dart.flutter"}),
 }
@@ -131,7 +151,12 @@ def try_munge_frame_path(
     if not munger or (munger.requires_sdk and sdk_name not in munger.supported_sdks):
         return None
 
-    return munger.frame_munger(frame)
+    munged_filename = munger.frame_munger(frame)
+    metrics.incr(
+        "sentry.issues.frame_munging",
+        tags={"platform": platform, "outcome": "success" if munged_filename else "failure"},
+    )
+    return munged_filename
 
 
 def munged_filename_and_frames(
@@ -166,7 +191,7 @@ def munged_filename_and_frames(
 
 
 def get_crashing_thread(
-    thread_frames: Sequence[Mapping[str, Any]] | None
+    thread_frames: Sequence[Mapping[str, Any]] | None,
 ) -> Mapping[str, Any] | None:
     if not thread_frames:
         return None

src/sentry/issues/auto_source_code_config/code_mapping.py
@@ -397,6 +397,15 @@ def convert_stacktrace_frame_path_to_source_path(
 
     stack_root = code_mapping.stack_root
 
+    has_new_logic = (
+        features.has("organizations:java-frame-munging-new-logic", organization, actor=None)
+        if organization
+        else False
+    )
+    if has_new_logic and platform == "java":
+        # This will cause the new munging logic to be applied
+        platform = "java-new-logic"
+
     # In most cases, code mappings get applied to frame.filename, but some platforms such as Java
     # contain folder info in other parts of the frame (e.g. frame.module="com.example.app.MainActivity"
     # gets transformed to "com/example/app/MainActivity.java"), so in those cases we use the
@@ -404,17 +413,6 @@ def convert_stacktrace_frame_path_to_source_path(
     stacktrace_path = (
         try_munge_frame_path(frame=frame, platform=platform, sdk_name=sdk_name) or frame.filename
     )
-
-    has_new_logic = (
-        features.has("organizations:java-frame-munging-new-logic", organization, actor=None)
-        if organization
-        else False
-    )
-    if has_new_logic and platform == "java" and frame.module and frame.abs_path:
-        try:
-            _, stacktrace_path = get_path_from_module(frame.module, frame.abs_path)
-        except Exception:
-            logger.warning("Investigate. Falling back to old logic.", exc_info=True)
 
     if stacktrace_path and stacktrace_path.startswith(code_mapping.stack_root):
         return (

commit 0636006c9c58fa2610d7433b8bb4ea0d69792e3f

fix(munging): Handle does not follow naming convention
This regression has been introduced in #90518.

Fixes [SENTRY-3T8M](https://sentry.sentry.io/issues/6577846344/)

src/sentry/utils/event_frames.py
@@ -63,17 +63,23 @@ def java_frame_munger(frame: EventFrame) -> str | None:
 
 
 def java_new_logic_frame_munger(frame: EventFrame) -> str | None:
+    stacktrace_path = None
     if not frame.module or not frame.abs_path:
         return None
 
-    from sentry.issues.auto_source_code_config.code_mapping import get_path_from_module
+    from sentry.issues.auto_source_code_config.code_mapping import (
+        DoesNotFollowJavaPackageNamingConvention,
+        get_path_from_module,
+    )
 
     try:
         _, stacktrace_path = get_path_from_module(frame.module, frame.abs_path)
+    except DoesNotFollowJavaPackageNamingConvention:
+        pass
     except Exception:
         # Report but continue
         logger.exception("Investigate. Error munging java frame")
-        return None
+
     return stacktrace_path
 
 
@@ -84,6 +90,11 @@ def cocoa_frame_munger(frame: EventFrame) -> str | None:
     rel_path = package_relative_path(frame.abs_path, frame.package)
     if rel_path:
         return rel_path
+
+    logger.warning(
+        "sentry.issues.frame_munging.failure",
+        extra={"platform": "cocoa", "frame": frame},
+    )
     return None
 
 

tests/sentry/utils/test_event_frames.py
@@ -81,9 +81,10 @@ class FilenameMungingTestCase(unittest.TestCase):
             {
                 "module": "jdk.internal.reflect.NativeMethodAccessorImpl",
                 "filename": "NativeMethodAccessorImpl.java",
+                "abs_path": "NativeMethodAccessorImpl.java",
             }
         ]
-        assert munged_filename_and_frames("java", frames, "munged")
+        assert munged_filename_and_frames("java-new-logic", frames, "munged")
 
 
 class JavaFilenameMungingTestCase(unittest.TestCase):
@@ -92,17 +93,20 @@ class JavaFilenameMungingTestCase(unittest.TestCase):
             {
                 "module": "jdk.internal.reflect.NativeMethodAccessorImpl",
                 "filename": "NativeMethodAccessorImpl.java",
+                "abs_path": "NativeMethodAccessorImpl.java",
             },
             {
                 "module": "io.sentry.example.Application",
                 "filename": "Application.java",
+                "abs_path": "Application.java",
             },
             {
                 "module": "io.sentry.example.Application",
                 "filename": "Application.java",
+                "abs_path": "Application.java",
             },
         ]
-        ret = munged_filename_and_frames("java", frames, "munged_filename")
+        ret = munged_filename_and_frames("java-new-logic", frames, "munged_filename")
         assert ret is not None
         key, munged_frames = ret
         assert len(munged_frames) == 3
@@ -116,15 +120,25 @@ class JavaFilenameMungingTestCase(unittest.TestCase):
         no_filename = {
             "module": "io.sentry.example.Application",
         }
-        no_munged = munged_filename_and_frames("java", [no_filename])
+        no_munged = munged_filename_and_frames("java-new-logic", [no_filename])
         assert not no_munged
 
     def test_platform_java_no_module(self):
         no_module = {
             "filename": "Application.java",
         }
-        no_munged = munged_filename_and_frames("java", [no_module])
+        no_munged = munged_filename_and_frames("java-new-logic", [no_module])
         assert not no_munged
+
+    def test_platform_java_do_not_follow_java_package_naming_convention_does_not_raise_exception(
+        self,
+    ):
+        frame = {
+            "abs_path": "gsp_arcus_drops_proofReadingmodecInspectionProofRead_gsp.groovy",
+            "module": "gsp_arcus_drops_proofReadingmodecInspectionProofRead_gsp$_run_closure2",
+        }
+        munged = munged_filename_and_frames("java-new-logic", [frame])
+        assert munged is None
 
     def test_platform_android_kotlin(self):
         exception_frames = [
@@ -252,7 +266,7 @@ class JavaFilenameMungingTestCase(unittest.TestCase):
                 "in_app": True,
             },
         ]
-        ret = munged_filename_and_frames("java", exception_frames, "munged_filename")
+        ret = munged_filename_and_frames("java-new-logic", exception_frames, "munged_filename")
         assert ret is not None
         key, munged_frames = ret
         assert len(munged_frames) == 16