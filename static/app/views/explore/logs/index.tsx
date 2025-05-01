fbc12b676873077a7ed4688d0f0ec2c2b7e7f953
Kev <kevan.fisher@sentry.io>
2025-05-01 12:50:12 -0400

    Override default period

--- static/app/views/explore/logs/index.tsx
+++ static/app/views/explore/logs/index.tsx
@@ -50,7 +50,17 @@ export default function LogsPage() {
 
   return (
     <SentryDocumentTitle title={t('Logs')} orgSlug={organization?.slug}>
-      <PageFiltersContainer maxPickableDays={maxPickableDays}>
+      <PageFiltersContainer
+        maxPickableDays={maxPickableDays}
+        defaultSelection={{
+          datetime: {
+            period: defaultPeriod,
+            start: null,
+            end: null,
+            utc: null,
+          },
+        }}
+      >
         <Layout.Page>
           <Layout.Header unified={prefersStackedNav}>
             <Layout.HeaderContent unified={prefersStackedNav}>
