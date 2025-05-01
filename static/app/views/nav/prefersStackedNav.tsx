d9e3a8361ee4ae21c4fba2715eeada1c7556902d
MichaelSun48 <michael.sun@sentry.io>
2025-05-01 10:54:03 -0700

    Seperate hook and function

--- static/app/views/nav/prefersStackedNav.tsx
+++ static/app/views/nav/prefersStackedNav.tsx
@@ -1,10 +1,9 @@
 import ConfigStore from 'sentry/stores/configStore';
 import type {Organization} from 'sentry/types/organization';
-import useOrganization from 'sentry/utils/useOrganization';
-import {useUser} from 'sentry/utils/useUser';
 
 // IMPORTANT:
-// These two functions NEED to have the same logic. Make sure to update both if you are changing this logic.
+// This function and the usePrefersStackedNav hook NEED to have the same logic.
+// Make sure to update both if you are changing this logic.
 
 export function prefersStackedNav(organization: Organization) {
   const userStackedNavOption = ConfigStore.get('user')?.options?.prefersStackedNavigation;
@@ -12,21 +11,6 @@ export function prefersStackedNav(organization: Organization) {
   if (
     userStackedNavOption !== false &&
     organization.features.includes('enforce-stacked-navigation')
-  ) {
-    return true;
-  }
-
-  return userStackedNavOption ?? false;
-}
-
-export function usePrefersStackedNav() {
-  const user = useUser();
-  const organization = useOrganization({allowNull: true});
-  const userStackedNavOption = user?.options?.prefersStackedNavigation;
-
-  if (
-    userStackedNavOption !== false &&
-    organization?.features.includes('enforce-stacked-navigation')
   ) {
     return true;
   }
