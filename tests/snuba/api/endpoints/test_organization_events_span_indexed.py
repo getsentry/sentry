aba8f30ba4e7067a36b78ce6674c4760c251b798
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-05-01 15:20:03 -0400

    revert

--- tests/snuba/api/endpoints/test_organization_events_span_indexed.py
+++ tests/snuba/api/endpoints/test_organization_events_span_indexed.py
@@ -2329,6 +2329,7 @@ class OrganizationEventsEAPRPCSpanEndpointTest(OrganizationEventsSpanIndexedEndp
         for source, result in zip(spans, data):
             assert result["id"] == source["span_id"], "id"
             assert result["span.duration"] == 1000.0, "duration"
+            # TODO: once the snuba change to return Nones has merged remove the or
             assert result["span.op"] is None or result["span.op"] == "", "op"
             assert result["span.description"] == source["description"], "description"
             ts = datetime.fromisoformat(result["timestamp"])
