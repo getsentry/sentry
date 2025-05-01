9d86a4d36443eca7e9a4c5f21947d28ac1005f8c
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2025-05-01 13:17:33 -0400

    drop(munging): Drop original Java munging logic (#90521)
    A follow-up to #90518.
    
    So far, the new logic has worked 100% of the time (over 6k calls) and
    there's no Sentry errors.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -116,8 +116,6 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:ds-org-recalibration", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enable custom dynamic sampling rates
     manager.add("organizations:dynamic-sampling-custom", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable new munging logic for java frames
-    manager.add("organizations:java-frame-munging-new-logic", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable archive/escalating issue workflow features in v2
     manager.add("organizations:escalating-issues-v2", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enable emiting escalating data to the metrics backend
