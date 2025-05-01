a9cab22070f12473fef5b56a0a81125c0cb9b42c
Tony Xiao <txiao@sentry.io>
2025-05-01 16:22:22 -0400

    missed normalized description

--- static/app/views/explore/constants.tsx
+++ static/app/views/explore/constants.tsx
@@ -53,6 +53,7 @@ export const SENTRY_SPAN_STRING_TAGS: string[] = [
   SpanIndexedField.TRANSACTION,
   SpanIndexedField.TRACE,
   SpanIndexedField.IS_TRANSACTION, // boolean field but we can expose it as a string
+  SpanIndexedField.NORMALIZED_DESCRIPTION,
 ];
 
 export const SENTRY_SPAN_NUMBER_TAGS: string[] = SENTRY_SEARCHABLE_SPAN_NUMBER_TAGS;
