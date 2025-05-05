7dee3af596cd404df9f6a6d38eb954c3a4f8975f
Adrian <adrian.bonilla@sentry.io>
2025-05-05 12:53:26 -0700

    simplify functionality with query params and local storage

--- static/app/components/codecov/container/container.tsx
+++ static/app/components/codecov/container/container.tsx
@@ -1,27 +1,36 @@
-import {useLayoutEffect, useState} from 'react';
+import {useState} from 'react';
 
-import {
-  CodecovContext,
-  DEFAULT_CODECOV_CONTEXT_VALUES,
-  initializeCodecovContext,
-  setRepositoryToContext,
-} from 'sentry/components/codecov/context/context';
+import {CodecovContext} from 'sentry/components/codecov/context/context';
+import {useLocalStorageState} from 'sentry/utils/useLocalStorageState';
 import {useLocation} from 'sentry/utils/useLocation';
+import {useNavigate} from 'sentry/utils/useNavigate';
 import useOrganization from 'sentry/utils/useOrganization';
 
 // Types
 export type CodecovContextTypes = {
+  branch: string | null;
+  codecovPeriod: string | null;
+  integratedOrg: string | null;
   repository: string | null;
 };
-
 export type CodecovContextSetterTypes = {
-  setRepository: (repository: string) => void;
-  setState: React.Dispatch<React.SetStateAction<CodecovContextTypes>>;
+  setContextState: React.Dispatch<React.SetStateAction<CodecovContextTypes>>;
+  updateSelectorData: (value: Partial<CodecovContextTypes>) => void;
 };
+type CodecovUrlParams = 'repository' | 'integratedOrg' | 'branch' | '';
 
 type CodecovProviderProps = {
   children?: React.ReactNode;
 };
+
+// Constants
+const DEFAULT_CODECOV_CONTEXT_VALUES: CodecovContextTypes = {
+  repository: null,
+  integratedOrg: null,
+  branch: null,
+  codecovPeriod: null,
+};
+const CODECOV_URL_PARAM: CodecovUrlParams[] = ['repository'];
 
 /**
  * This is a provider that's meant to wrap all Codecov product components.
@@ -29,24 +38,66 @@ type CodecovProviderProps = {
  * It also defines setter methods for child components to update context data.
  */
 export default function CodecovProvider({children}: CodecovProviderProps) {
-  const [state, setState] = useState<CodecovContextTypes>(DEFAULT_CODECOV_CONTEXT_VALUES);
   const location = useLocation();
   const queryParams = location.query;
 
   const organization = useOrganization();
   const orgSlug = organization.slug;
 
-  // Main container initialization
-  useLayoutEffect(() => {
-    initializeCodecovContext({queryParams, orgSlug, setState});
-  }, [queryParams, orgSlug, setState]);
+  const [selectorData, setSelectorData] = useLocalStorageState(
+    `codecov-selection:${orgSlug}`,
+    DEFAULT_CODECOV_CONTEXT_VALUES
+  );
+  const navigate = useNavigate();
 
-  // Context Setter Functions
-  const setRepository: CodecovContextSetterTypes['setRepository'] = repository =>
-    setRepositoryToContext({orgSlug, repository, setState});
+  // Initializes context state from query parameters or local storage, otherwise
+  // with a default value.
+  const [contextState, setContextState] = useState(() => {
+    // Get query params
+    const resultsFromQuery = CODECOV_URL_PARAM.reduce<Record<string, string | null>>(
+      (acc, value: string) => {
+        const queryParam = queryParams[value];
+        acc[value] =
+          typeof queryParam === 'string' ? decodeURIComponent(queryParam).trim() : null;
+        return acc;
+      },
+      {}
+    );
+    const hasAnyNonNull = Object.values(resultsFromQuery).some(value => value !== null);
+    if (hasAnyNonNull) {
+      return {...DEFAULT_CODECOV_CONTEXT_VALUES, ...resultsFromQuery};
+    }
+
+    // Get data from local storage
+    if (selectorData) {
+      return {...DEFAULT_CODECOV_CONTEXT_VALUES, ...selectorData};
+    }
+
+    return DEFAULT_CODECOV_CONTEXT_VALUES;
+  });
+
+  // Updates Codecov context, local storage and query params
+  const updateSelectorData: CodecovContextSetterTypes['updateSelectorData'] = data => {
+    return setContextState(prev => {
+      const newState = {...prev, ...data};
+      setSelectorData(newState);
+
+      const currentParams = new URLSearchParams(location.search);
+      for (const [key, value] of Object.entries(newState)) {
+        if (value !== null) {
+          currentParams.set(key, value);
+        }
+      }
+      navigate(`${location.pathname}?${currentParams.toString()}`, {replace: true});
+
+      return newState;
+    });
+  };
 
   return (
-    <CodecovContext.Provider value={{...state, setState, setRepository}}>
+    <CodecovContext.Provider
+      value={{...contextState, setContextState, updateSelectorData}}
+    >
       {children}
     </CodecovContext.Provider>
   );
--- static/app/components/codecov/context/context.tsx
+++ static/app/components/codecov/context/context.tsx
@@ -1,40 +1,9 @@
-import type React from 'react';
 import {createContext, useContext} from 'react';
-import type {Location} from 'history';
 
 import type {
   CodecovContextSetterTypes,
   CodecovContextTypes,
 } from 'sentry/components/codecov/container/container';
-import type {CodecovObjectInLocalStorage} from 'sentry/components/codecov/container/persistence';
-import {
-  getDataFromLocalStorage,
-  makeCodecovLocalStorageKey,
-  setDataToLocalStorage,
-} from 'sentry/components/codecov/container/persistence';
-import {getParamsFromQuery} from 'sentry/components/codecov/container/url';
-
-// Constants
-export const DEFAULT_CODECOV_CONTEXT_VALUES = {
-  repository: null,
-};
-
-export const CODECOV_URL_PARAM = {
-  REPOSITORY: 'repository',
-};
-
-// Types
-export type InitializeCodecovContextParams = {
-  orgSlug: string;
-  queryParams: Location['query'];
-  setState: React.Dispatch<React.SetStateAction<CodecovContextTypes>>;
-};
-
-type SetRepositoryContextParams = {
-  orgSlug: string;
-  repository: string;
-  setState: React.Dispatch<React.SetStateAction<CodecovContextTypes>>;
-};
 
 // Context definition
 export const CodecovContext = createContext<
@@ -46,53 +15,3 @@ export function useCodecovContext() {
   if (!context) throw new Error('useCodecovContext must be used within CodecovProvider');
   return context;
 }
-
-// Context functions
-
-/**
- * This function initializes the Codecov context data from relevant url params and local storage data. It will:
- * 1) Initialize context from URL data if existent - it won't populate data from local storage if this is the case.
- * 2) Initialize context from local storage if data is existent.
- * 3) Initialize context from default data.
- */
-export function initializeCodecovContext({
-  queryParams,
-  orgSlug,
-  setState,
-}: InitializeCodecovContextParams) {
-  const dataFromQuery = getParamsFromQuery(queryParams, CODECOV_URL_PARAM);
-  const hasAnyNonNull = Object.values(dataFromQuery).some(value => value !== null);
-
-  if (hasAnyNonNull) {
-    setState(prev => ({...prev, ...dataFromQuery}));
-    return;
-  }
-
-  const codecovLocalStorageKey = makeCodecovLocalStorageKey(orgSlug);
-  const dataFromLocalStorage =
-    getDataFromLocalStorage<CodecovObjectInLocalStorage>(codecovLocalStorageKey);
-  if (dataFromLocalStorage) {
-    setState(prev => ({...prev, ...dataFromLocalStorage}));
-    return;
-  }
-
-  setState(prev => ({...prev, ...DEFAULT_CODECOV_CONTEXT_VALUES}));
-  return;
-}
-
-// Stores state to local storage, url and Codecov context
-export const setRepositoryToContext = ({
-  orgSlug,
-  repository,
-  setState,
-}: SetRepositoryContextParams) => {
-  return setState(prev => {
-    const newState = {...prev, repository};
-
-    const codecovLocalStorageKey = makeCodecovLocalStorageKey(orgSlug);
-    setDataToLocalStorage<CodecovObjectInLocalStorage>(codecovLocalStorageKey, newState);
-    // TODO: add method to store params in URL
-
-    return newState;
-  });
-};
