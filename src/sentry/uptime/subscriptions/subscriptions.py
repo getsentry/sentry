7dde8591bf0f3d807eb576609c0785c8b4dd360c
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-05-05 12:55:06 -0400

    ref(uptime): Stop writing uptime_status on ProjectUptimeSubscription (#90775)
    We'll drop this column in the future.

--- src/sentry/uptime/subscriptions/subscriptions.py
+++ src/sentry/uptime/subscriptions/subscriptions.py
@@ -231,7 +231,6 @@ def create_project_uptime_subscription(
             name=name,
             owner_user_id=owner_user_id,
             owner_team_id=owner_team_id,
-            uptime_status=uptime_status,
         )
         detector = create_detector_from_project_subscription(uptime_monitor)
 
@@ -354,17 +353,11 @@ def disable_uptime_detector(detector: Detector):
         # Resolve the issue so that we don't see it in the ui anymore
         resolve_uptime_issue(uptime_monitor)
 
-    uptime_monitor.update(
-        status=ObjectStatus.DISABLED,
-        # We set the status back to ok here so that if we re-enable we'll start
-        # from a good state
-        uptime_status=UptimeStatus.OK,
-    )
-    uptime_subscription.update(
-        # We set the status back to ok here so that if we re-enable we'll start
-        # from a good state
-        uptime_status=UptimeStatus.OK
-    )
+    # We set the status back to ok here so that if we re-enable we'll start
+    # from a good state
+    uptime_subscription.update(uptime_status=UptimeStatus.OK)
+
+    uptime_monitor.update(status=ObjectStatus.DISABLED)
     detector.update(enabled=False)
 
     quotas.backend.remove_seat(DataCategory.UPTIME, uptime_monitor)
