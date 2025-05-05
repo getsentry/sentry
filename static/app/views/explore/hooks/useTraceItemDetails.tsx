12eb534eaa61e11f1c17dca8289d170aa5ff00d3
Abdullah Khan <60121741+Abdkhan14@users.noreply.github.com>
2025-05-05 12:20:02 -0400

    feat(trace-eap-waterfall): Triggering capture exception only when query is enabled (#90893)
    Fixes JAVASCRIPT-2ZX1
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/explore/hooks/useTraceItemDetails.tsx
+++ static/app/views/explore/hooks/useTraceItemDetails.tsx
@@ -77,7 +77,8 @@ export function useTraceItemDetails(props: UseTraceItemDetailsProps) {
   const project = useProjectFromId({project_id: props.projectId});
   const enabled = (props.enabled ?? true) && !!project;
 
-  if (!project) {
+  // Only capture exception if the project is not found and the query is enabled.
+  if ((props.enabled ?? true) && !project) {
     captureException(
       new Error(`Project "${props.projectId}" not found in useTraceItemDetails`)
     );
