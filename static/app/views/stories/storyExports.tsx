51a2ebcf695c63307a59272c91de301b825845ac
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-05-05 11:54:57 -0700

    feat(docs): Update stories to show the comitter date of the story file (#90924)
    The date will only be fetched and rendered if you're logged into github.
    I expect that being logged in is the typical case for people working on
    the codebase. If you're not logged in then nothing should render.
    
    
    ![SCR-20250505-khzd](https://github.com/user-attachments/assets/7ad67163-ea27-4848-b450-96dd189e9c1f)

--- static/app/views/stories/storyExports.tsx
+++ static/app/views/stories/storyExports.tsx
@@ -1,5 +1,6 @@
 import {Fragment} from 'react';
 import styled from '@emotion/styled';
+import {ErrorBoundary} from '@sentry/react';
 
 import {space} from 'sentry/styles/space';
 import {StorySourceLinks} from 'sentry/views/stories/storySourceLinks';
@@ -12,7 +13,9 @@ export function StoryExports(props: {story: StoryDescriptor}) {
   return (
     <Fragment>
       <StorySourceLinksContainer>
-        <StorySourceLinks story={props.story} />
+        <ErrorBoundary>
+          <StorySourceLinks story={props.story} />
+        </ErrorBoundary>
       </StorySourceLinksContainer>
       {/* Render default export first */}
       {DefaultExport ? (
