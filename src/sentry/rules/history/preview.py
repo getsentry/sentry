e9eea7a1507385a8c36c45b3a4053a51a2ab2bc7
Gabe Villalobos <gabriel.villalobos@sentry.io>
2025-05-05 15:45:21 -0700

    Addresses PR comments

--- src/sentry/rules/history/preview.py
+++ src/sentry/rules/history/preview.py
@@ -256,7 +256,11 @@ def get_top_groups(
     Since frequency conditions require snuba query(s), we need to limit the number groups we process.
     """
     if has_issue_state_condition:
-        datasets = {dataset_map.get(group) for group in condition_activity.keys()}
+        datasets = {
+            dataset_map.get(group)
+            for group in condition_activity.keys()
+            if dataset_map.get(group) is not None
+        }
     else:
         # condition_activity will be empty because there are no issue state conditions.
         # So, we look to find top groups over all datasets
