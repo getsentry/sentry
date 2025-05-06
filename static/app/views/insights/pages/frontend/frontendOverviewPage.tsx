getsentry/sentry

commit 49723f359ace4bf1c709a0bd2a1b91106e231316

feat(insights): add perf score column to frontend overview (#90736)
1. Remove span.op column for frontend overview
2. Add performance score column
<img width="1204" alt="image"
src="https://github.com/user-attachments/assets/34959713-a21f-4b77-a15c-896dde9a891a"
/>

static/app/views/insights/pages/frontend/frontendOverviewPage.tsx
@@ -42,9 +42,9 @@ import {FrontendHeader} from 'sentry/views/insights/pages/frontend/frontendPageH
 import {OldFrontendOverviewPage} from 'sentry/views/insights/pages/frontend/oldFrontendOverviewPage';
 import {
   DEFAULT_SORT,
+  EAP_OVERVIEW_PAGE_ALLOWED_OPS,
   FRONTEND_LANDING_TITLE,
   FRONTEND_PLATFORMS,
-  OVERVIEW_PAGE_ALLOWED_OPS,
 } from 'sentry/views/insights/pages/frontend/settings';
 import {NextJsOverviewPage} from 'sentry/views/insights/pages/platform/nextjs';
 import {useIsNextJsInsightsEnabled} from 'sentry/views/insights/pages/platform/nextjs/features';
@@ -111,7 +111,7 @@ function EAPOverviewPage() {
   const existingQuery = new MutableSearch(eventView.query);
   // TODO - this query is getting complicated, once were on EAP, we should consider moving this to the backend
   existingQuery.addOp('(');
-  existingQuery.addDisjunctionFilterValues('span.op', OVERVIEW_PAGE_ALLOWED_OPS);
+  existingQuery.addFilterValues('span.op', EAP_OVERVIEW_PAGE_ALLOWED_OPS);
   // add disjunction filter creates a very long query as it seperates conditions with OR, project ids are numeric with no spaces, so we can use a comma seperated list
   if (selectedFrontendProjects.length > 0) {
     existingQuery.addOp('OR');
@@ -183,8 +183,6 @@ function EAPOverviewPage() {
     decodeSorts(location.query?.sort).find(isAValidSort) ?? DEFAULT_SORT,
   ];
 
-  existingQuery.addFilterValue('is_transaction', 'true');
-
   const response = useEAPSpans(
     {
       search: existingQuery,
@@ -192,13 +190,13 @@ function EAPOverviewPage() {
       fields: [
         'is_starred_transaction',
         'transaction',
-        'span.op',
         'project',
         'epm()',
         'p50(span.duration)',
         'p95(span.duration)',
         'failure_rate()',
         'time_spent_percentage(span.duration)',
+        'performance_score(measurements.score.total)',
         'count_unique(user)',
         'sum(span.duration)',
       ],

static/app/utils/discover/fieldRenderers.tsx
@@ -57,6 +57,7 @@ import {decodeScalar} from 'sentry/utils/queryString';
 import {isUrl} from 'sentry/utils/string/isUrl';
 import {QuickContextHoverWrapper} from 'sentry/views/discover/table/quickContext/quickContextWrapper';
 import {ContextType} from 'sentry/views/discover/table/quickContext/utils';
+import {PerformanceBadge} from 'sentry/views/insights/browser/webVitals/components/performanceBadge';
 import {PercentChangeCell} from 'sentry/views/insights/common/components/tableCells/percentChangeCell';
 import {ResponseStatusCodeCell} from 'sentry/views/insights/common/components/tableCells/responseStatusCodeCell';
 import {StarredSegmentCell} from 'sentry/views/insights/common/components/tableCells/starredSegmentCell';
@@ -378,6 +379,7 @@ type SpecialFields = {
   issue: SpecialField;
   'issue.id': SpecialField;
   minidump: SpecialField;
+  'performance_score(measurements.score.total)': SpecialField;
   'profile.id': SpecialField;
   project: SpecialField;
   release: SpecialField;
@@ -401,6 +403,11 @@ const DownloadCount = styled('span')`
 const RightAlignedContainer = styled('span')`
   margin-left: auto;
   margin-right: 0;
+`;
+
+const CenterAlignedContainer = styled('span')`
+  text-align: center;
+  width: 100%;
 `;
 
 /**
@@ -826,6 +833,20 @@ const SPECIAL_FIELDS: SpecialFields = {
         )}
       </Container>
     ),
+  },
+  'performance_score(measurements.score.total)': {
+    sortField: 'performance_score(measurements.score.total)',
+    renderFunc: data => {
+      const score = data['performance_score(measurements.score.total)'];
+      if (typeof score !== 'number') {
+        return <Container>{emptyValue}</Container>;
+      }
+      return (
+        <CenterAlignedContainer>
+          <PerformanceBadge score={Math.round(score * 100)} />
+        </CenterAlignedContainer>
+      );
+    },
   },
 };
 

static/app/views/insights/common/components/tableCells/renderHeadCell.tsx
@@ -77,6 +77,7 @@ const SORTABLE_FIELDS = new Set([
   'p50(span.duration)',
   'p95(span.duration)',
   'failure_rate()',
+  'performance_score(measurements.score.total)',
   'count_unique(user)',
 ]);
 

static/app/views/insights/pages/frontend/frontendOverviewTable.tsx
@@ -26,7 +26,6 @@ type Row = Pick<
   EAPSpanResponse,
   | 'is_starred_transaction'
   | 'transaction'
-  | 'span.op'
   | 'project'
   | 'epm()'
   | 'p50(span.duration)'
@@ -35,12 +34,12 @@ type Row = Pick<
   | 'time_spent_percentage(span.duration)'
   | 'count_unique(user)'
   | 'sum(span.duration)'
+  | 'performance_score(measurements.score.total)'
 >;
 
 type Column = GridColumnHeader<
   | 'is_starred_transaction'
   | 'transaction'
-  | 'span.op'
   | 'project'
   | 'epm()'
   | 'p50(span.duration)'
@@ -49,17 +48,13 @@ type Column = GridColumnHeader<
   | 'time_spent_percentage(span.duration)'
   | 'count_unique(user)'
   | 'sum(span.duration)'
+  | 'performance_score(measurements.score.total)'
 >;
 
 const COLUMN_ORDER: Column[] = [
   {
     key: 'transaction',
     name: t('Transaction'),
-    width: COL_WIDTH_UNDEFINED,
-  },
-  {
-    key: 'span.op',
-    name: t('Operation'),
     width: COL_WIDTH_UNDEFINED,
   },
   {
@@ -97,12 +92,16 @@ const COLUMN_ORDER: Column[] = [
     name: DataTitles.timeSpent,
     width: COL_WIDTH_UNDEFINED,
   },
+  {
+    key: 'performance_score(measurements.score.total)',
+    name: t('Perf Score'),
+    width: COL_WIDTH_UNDEFINED,
+  },
 ];
 
 const SORTABLE_FIELDS = [
   'is_starred_transaction',
   'transaction',
-  'span.op',
   'project',
   'epm()',
   'p50(span.duration)',
@@ -110,6 +109,7 @@ const SORTABLE_FIELDS = [
   'failure_rate()',
   'count_unique(user)',
   'time_spent_percentage(span.duration)',
+  'performance_score(measurements.score.total)',
 ] as const;
 
 export type ValidSort = Sort & {
@@ -211,13 +211,7 @@ function renderBodyCell(
   }
 
   if (column.key === 'transaction') {
-    return (
-      <TransactionCell
-        project={row.project}
-        transaction={row.transaction}
-        transactionMethod={row['span.op']}
-      />
-    );
+    return <TransactionCell project={row.project} transaction={row.transaction} />;
   }
 
   const renderer = getFieldRenderer(column.key, meta.fields, false);

static/app/views/insights/pages/frontend/settings.ts
@@ -8,6 +8,20 @@ export const FRONTEND_LANDING_SUB_PATH = 'frontend';
 export const FRONTEND_LANDING_TITLE = t('Frontend');
 export const FRONTEND_SIDEBAR_LABEL = t('Frontend');
 
+export const EAP_OVERVIEW_PAGE_ALLOWED_OPS = [
+  'pageload',
+  'navigation',
+  'ui.render',
+  'interaction',
+  'ui.interaction',
+  'ui.interaction.click',
+  'ui.interaction.hover',
+  'ui.interaction.drag',
+  'ui.interaction.press',
+  'ui.webvital.cls',
+  'ui.webvital.fcp',
+];
+
 export const OVERVIEW_PAGE_ALLOWED_OPS = [
   'pageload',
   'navigation',

static/app/views/insights/types.tsx
@@ -75,6 +75,14 @@ export enum SpanFields {
   USER = 'user',
 }
 
+type WebVitalsMeasurements =
+  | 'measurements.score.cls'
+  | 'measurements.score.fcp'
+  | 'measurements.score.inp'
+  | 'measurements.score.lcp'
+  | 'measurements.score.ttfb'
+  | 'measurements.score.total';
+
 type SpanBooleanFields =
   | SpanFields.CACHE_HIT
   | SpanFields.IS_TRANSACTION
@@ -173,6 +181,8 @@ export const SPAN_FUNCTIONS = [
   'failure_rate',
 ] as const;
 
+export const WEB_VITAL_FUNCTIONS = ['performance_score'] as const;
+
 type BreakpointCondition = 'less' | 'greater';
 
 type RegressionFunctions = [
@@ -184,6 +194,8 @@ type RegressionFunctions = [
 type SpanAnyFunction = `any(${string})`;
 
 export type SpanFunctions = (typeof SPAN_FUNCTIONS)[number];
+
+type WebVitalsFunctions = (typeof WEB_VITAL_FUNCTIONS)[number];
 
 export type SpanMetricsResponse = {
   [Property in SpanNumberFields as `${Aggregate}(${Property})`]: number;
@@ -226,6 +238,8 @@ export type EAPSpanResponse = {
   [Property in SpanNumberFields as `${Aggregate}(${Property})`]: number;
 } & {
   [Property in SpanFunctions as `${Property}()`]: number;
+} & {
+  [Property in WebVitalsMeasurements as `${WebVitalsFunctions}(${Property})`]: number;
 } & {
   [Property in SpanStringFields as `${Property}`]: string;
 } & {

commit e1ec25f9734167f3ce429114b18bc863aaa21073

fix(insights): frontend overview eap span condition incorrect (#91017)
We should be using `addDisjunctionFilterValues` (i.e an OR filter)
instead of `addfFilterValues` for allowed span ops in frontend page.

this broke in https://github.com/getsentry/sentry/pull/90736/files

static/app/views/insights/pages/frontend/frontendOverviewPage.tsx
@@ -111,7 +111,7 @@ function EAPOverviewPage() {
   const existingQuery = new MutableSearch(eventView.query);
   // TODO - this query is getting complicated, once were on EAP, we should consider moving this to the backend
   existingQuery.addOp('(');
-  existingQuery.addFilterValues('span.op', EAP_OVERVIEW_PAGE_ALLOWED_OPS);
+  existingQuery.addDisjunctionFilterValues('span.op', EAP_OVERVIEW_PAGE_ALLOWED_OPS);
   // add disjunction filter creates a very long query as it seperates conditions with OR, project ids are numeric with no spaces, so we can use a comma seperated list
   if (selectedFrontendProjects.length > 0) {
     existingQuery.addOp('OR');