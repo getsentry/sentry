48c1745514136fb8e61886e93dbc8ae300653940
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-05-06 15:17:10 +0200

    wip

--- src/sentry/analytics/events/project_transferred.py
+++ src/sentry/analytics/events/project_transferred.py
@@ -5,10 +5,10 @@ class ProjectTransferredEvent(analytics.Event):
     type = "project.transferred"
 
     attributes = (
-        analytics.Attribute("user_id", required=False),
-        analytics.Attribute("organization_id"),
-        analytics.Attribute("project_id"),
+        analytics.Attribute("old_organization_id"),
         analytics.Attribute("new_organization_id"),
+        analytics.Attribute("project_id"),
+        analytics.Attribute("platform", required=False),
     )
 
 
