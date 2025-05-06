2842d8c0665c15ae6dcf73c6438cae8d05c41b26
Abdullah Khan <60121741+Abdkhan14@users.noreply.github.com>
2025-05-06 09:40:38 -0400

    feat(trace-eap-waterfall): Adding measurement score ratio, instead of just score (#90938)
    Frontend is already expecting this:
    [link](https://github.com/getsentry/sentry/blob/abdk/trace-eap-measurements/static/app/views/performance/newTraceDetails/traceModels/traceTree.measurements.tsx#L132)
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- src/sentry/snuba/spans_rpc.py
+++ src/sentry/snuba/spans_rpc.py
@@ -381,7 +381,7 @@ def run_trace_query(
         "ttfb",
     }:
         trace_attributes.append(f"measurements.{key}")
-        trace_attributes.append(f"measurements.score.{key}")
+        trace_attributes.append(f"measurements.score.ratio.{key}")
     resolver = get_resolver(params=params, config=SearchResolverConfig())
     columns, _ = resolver.resolve_attributes(trace_attributes)
     meta = resolver.resolve_meta(referrer=referrer)
