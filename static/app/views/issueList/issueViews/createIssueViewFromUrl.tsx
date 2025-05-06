4ea66a961520c0f9062e06c08bf64b5e0216212e
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2025-05-06 10:42:00 -0700

    fix(issue-views): Stats period not triggering unsaved changes (#91030)
    Fixes a minor issue where changing the stats period would not trigger
    unsaved changes sometimes.

--- static/app/views/issueList/issueViews/createIssueViewFromUrl.tsx
+++ static/app/views/issueList/issueViews/createIssueViewFromUrl.tsx
@@ -25,7 +25,7 @@ export function createIssueViewFromUrl({query}: {query: Query}): IssueViewParams
     timeFilters: {
       start: decodeScalar(normalizedTimeFilters.start) ?? null,
       end: decodeScalar(normalizedTimeFilters.end) ?? null,
-      period: decodeScalar(normalizedTimeFilters.period) ?? null,
+      period: decodeScalar(normalizedTimeFilters.statsPeriod) ?? null,
       utc: decodeBoolean(normalizedTimeFilters.utc) ?? null,
     },
   };
