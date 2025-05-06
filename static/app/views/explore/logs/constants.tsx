6e5420662331d9779325712a9e4fdcd1c595a59a
Colin Chartier <colin.chartier@sentry.io>
2025-05-06 14:22:52 -0400

    fix(ourlogs): Hide internal attributes from the details

--- static/app/views/explore/logs/constants.tsx
+++ static/app/views/explore/logs/constants.tsx
@@ -29,7 +29,9 @@ const AlwaysHiddenLogFields: OurLogFieldKey[] = [
   OurLogKnownFieldKey.ID,
   OurLogKnownFieldKey.ORGANIZATION_ID,
   OurLogKnownFieldKey.ITEM_TYPE,
-  OurLogKnownFieldKey.PROJECT,
+  'project',
+  'project.id',
+  'project_id', // these are all aliases that might show up
 ];
 
 /**
@@ -38,6 +40,12 @@ const AlwaysHiddenLogFields: OurLogFieldKey[] = [
 export const HiddenLogDetailFields: OurLogFieldKey[] = [
   ...AlwaysHiddenLogFields,
   OurLogKnownFieldKey.MESSAGE,
+
+  // deprecated/otel fields that clutter the UI
+  'sentry.timestamp_nanos',
+  'tags[sentry.timestamp_precise,number]',
+  'tags[sentry.trace_flags,number]',
+  'span_id',
 ];
 
 export const HiddenColumnEditorLogFields: OurLogFieldKey[] = [...AlwaysHiddenLogFields];
