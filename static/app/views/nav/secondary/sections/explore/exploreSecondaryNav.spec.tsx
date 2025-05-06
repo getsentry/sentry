cd01cfe6b640b40f7a0ec40ece7a30cfc2449382
Edward Gou <edward.gou@sentry.io>
2025-05-06 14:40:08 -0400

    removes prebuilt saved queries feature check in frontend

--- static/app/views/nav/secondary/sections/explore/exploreSecondaryNav.spec.tsx
+++ static/app/views/nav/secondary/sections/explore/exploreSecondaryNav.spec.tsx
@@ -7,11 +7,7 @@ import {NavContextProvider} from 'sentry/views/nav/context';
 describe('ExploreSecondaryNav', () => {
   const {organization} = initializeOrg({
     organization: {
-      features: [
-        'performance-trace-explorer',
-        'performance-view',
-        'performance-default-explore-queries',
-      ],
+      features: ['performance-trace-explorer', 'performance-view'],
     },
   });
 
