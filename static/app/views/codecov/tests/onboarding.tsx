27e28fe165886c41abb219f76e41edeb00b7c5c4
Calvin <calvin.yau@sentry.io>
2025-05-06 16:22:32 -0700

    feat(codecov): Add TA preonboarding (#90629)
    Adding TA preonboarding pieces

--- static/app/views/codecov/tests/onboarding.tsx
+++ static/app/views/codecov/tests/onboarding.tsx
@@ -1,11 +1,13 @@
 import styled from '@emotion/styled';
 
 import {space} from 'sentry/styles/space';
+import TestPreOnboardingPage from 'sentry/views/codecov/tests/preOnboarding';
 
 export default function TestsOnboardingPage() {
   return (
     <LayoutGap>
       <p>Test Analytics Onboarding</p>
+      <TestPreOnboardingPage />
     </LayoutGap>
   );
 }
