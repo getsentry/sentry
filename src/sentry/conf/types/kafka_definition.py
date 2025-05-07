d16789bf7ad292f28dc314a5bd1cdffeeac2209e
Evan Hicks <evanh@users.noreply.github.com>
2025-05-07 14:15:27 -0400

    fix(taskworker): Add taskworker-ingest to Topic (#91077)
    This topic needs to exist to send tasks.

--- src/sentry/conf/types/kafka_definition.py
+++ src/sentry/conf/types/kafka_definition.py
@@ -67,6 +67,7 @@ class Topic(Enum):
     BUFFERED_SEGMENTS = "buffered-segments"
     BUFFERED_SEGMENTS_DLQ = "buffered-segments-dlq"
     TASKWORKER = "taskworker"
+    TASKWORKER_INGEST = "taskworker-ingest"
 
 
 class ConsumerDefinition(TypedDict, total=False):
