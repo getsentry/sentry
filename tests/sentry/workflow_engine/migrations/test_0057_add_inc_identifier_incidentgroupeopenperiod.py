120c30a684e9e7c60c2fd6013c2590f387fa015c
Colleen O'Rourke <colleen@sentry.io>
2025-05-07 10:29:20 -0700

    rebase & remove unique constraint

--- tests/sentry/workflow_engine/migrations/test_0057_add_inc_identifier_incidentgroupeopenperiod.py
+++ tests/sentry/workflow_engine/migrations/test_0057_add_inc_identifier_incidentgroupeopenperiod.py
@@ -16,8 +16,8 @@ from sentry.workflow_engine.models import IncidentGroupOpenPeriod
 )
 class AddIncidentIdentifierTest(TestMigrations):
     app = "workflow_engine"
-    migrate_from = "0055_datasource_define_indexes"
-    migrate_to = "0056_add_inc_identifier_incidentgroupopenperiod"
+    migrate_from = "0056_workflow_fire_history_passed_filters_column"
+    migrate_to = "0057_add_inc_identifier_incidentgroupopenperiod"
 
     def setUp(self):
         return super().setUp()
