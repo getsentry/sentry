959952a66e9a455ee71f891cd46b40da26350b06
ArthurKnaus <arthur.knaus@sentry.io>
2025-05-08 12:45:06 +0200

    ref(platform-insights): Use useEAPSeries (#91215)
    - part of [TET-377: Ensure sampling param is set for all
    requests](https://linear.app/getsentry/issue/TET-377/ensure-sampling-param-is-set-for-all-requests)

--- static/app/views/insights/pages/platform/shared/durationWidget.tsx
+++ static/app/views/insights/pages/platform/shared/durationWidget.tsx
@@ -1,11 +1,7 @@
-import {useCallback, useMemo} from 'react';
-import {useTheme} from '@emotion/react';
+import {useMemo} from 'react';
 
 import {openInsightChartModal} from 'sentry/actionCreators/modal';
 import {t} from 'sentry/locale';
-import type {MultiSeriesEventsStats} from 'sentry/types/organization';
-import type {EventsMetaType} from 'sentry/utils/discover/eventView';
-import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 import type {Release} from 'sentry/views/dashboards/widgets/common/types';
 import {Line} from 'sentry/views/dashboards/widgets/timeSeriesWidget/plottables/line';
@@ -13,7 +9,7 @@ import {TimeSeriesWidgetVisualization} from 'sentry/views/dashboards/widgets/tim
 import {Widget} from 'sentry/views/dashboards/widgets/widget/widget';
 import {Mode} from 'sentry/views/explore/contexts/pageParamsContext/mode';
 import {ChartType} from 'sentry/views/insights/common/components/chart';
-import type {DiscoverSeries} from 'sentry/views/insights/common/queries/useDiscoverSeries';
+import {useEAPSeries} from 'sentry/views/insights/common/queries/useDiscoverSeries';
 import {convertSeriesToTimeseries} from 'sentry/views/insights/common/utils/convertSeriesToTimeseries';
 import {Referrer} from 'sentry/views/insights/pages/platform/laravel/referrers';
 import {usePageFilterChartParams} from 'sentry/views/insights/pages/platform/laravel/utils';
@@ -29,59 +25,27 @@ export function DurationWidget({
   query?: string;
   releases?: Release[];
 }) {
-  const theme = useTheme();
   const organization = useOrganization();
   const pageFilterChartParams = usePageFilterChartParams();
 
   const fullQuery = `span.op:http.server ${query}`.trim();
 
-  const {data, isLoading, error} = useApiQuery<MultiSeriesEventsStats>(
-    [
-      `/organizations/${organization.slug}/events-stats/`,
-      {
-        query: {
-          ...pageFilterChartParams,
-          dataset: 'spans',
-          yAxis: ['avg(span.duration)', 'p95(span.duration)'],
-          orderby: 'avg(span.duration)',
-          partial: 1,
-          query: fullQuery,
-          referrer: Referrer.DURATION_CHART,
-        },
-      },
-    ],
-    {staleTime: 0}
-  );
-
-  const getTimeSeries = useCallback(
-    (field: string, color?: string): DiscoverSeries | undefined => {
-      const series = data?.[field];
-      if (!series || series.data.every(([_, [value]]) => value?.count === 0)) {
-        return undefined;
-      }
-
-      return {
-        data: series.data.map(([time, [value]]) => ({
-          value: value?.count!,
-          name: new Date(time * 1000).toISOString(),
-        })),
-        seriesName: field,
-        meta: series.meta as EventsMetaType,
-        color,
-      } satisfies DiscoverSeries;
+  const {data, isLoading, error} = useEAPSeries(
+    {
+      ...pageFilterChartParams,
+      search: fullQuery,
+      yAxis: ['avg(span.duration)', 'p95(span.duration)'],
+      referrer: Referrer.DURATION_CHART,
     },
-    [data]
+    Referrer.DURATION_CHART
   );
 
   const plottables = useMemo(() => {
-    const colors = theme.chart.getColorPalette(2);
-    return [
-      getTimeSeries('avg(span.duration)', colors[0]),
-      getTimeSeries('p95(span.duration)', colors[1]),
-    ]
-      .filter(series => !!series)
-      .map(ts => new Line(convertSeriesToTimeseries(ts)));
-  }, [getTimeSeries, theme]);
+    return Object.keys(data).map(key => {
+      const series = data[key as keyof typeof data];
+      return new Line(convertSeriesToTimeseries(series));
+    });
+  }, [data]);
 
   const isEmpty = plottables.every(plottable => plottable.isEmpty);
 
