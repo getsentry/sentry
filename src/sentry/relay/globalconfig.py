215f13451c405e615d4e59af4a3ff330218b6028
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2025-05-08 15:31:11 +0200

    ref(relay): Add spans-ignore-trace-id-partitioning to global options (#91219)
    depends on 
    https://github.com/getsentry/relay/pull/4706
    https://github.com/getsentry/relay/pull/4716

--- src/sentry/relay/globalconfig.py
+++ src/sentry/relay/globalconfig.py
@@ -27,6 +27,7 @@ RELAY_OPTIONS: list[str] = [
     "relay.span-extraction.sample-rate",
     "relay.span-normalization.allowed_hosts",
     "relay.drop-transaction-attachments",
+    "relay.spans-ignore-trace-id-partitioning",
 ]
 
 
