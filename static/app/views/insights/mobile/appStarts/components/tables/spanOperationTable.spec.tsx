53a672cf585f02458f3da21575dd79fd7fe19bd6
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-05-08 17:00:49 -0400

    fix test

--- static/app/views/insights/mobile/appStarts/components/tables/spanOperationTable.spec.tsx
+++ static/app/views/insights/mobile/appStarts/components/tables/spanOperationTable.spec.tsx
@@ -8,9 +8,13 @@ import {render, screen, waitFor} from 'sentry-test/reactTestingLibrary';
 import {useLocation} from 'sentry/utils/useLocation';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import {SpanOperationTable} from 'sentry/views/insights/mobile/appStarts/components/tables/spanOperationTable';
+import {MOBILE_LANDING_SUB_PATH} from 'sentry/views/insights/pages/mobile/settings';
+import {DOMAIN_VIEW_BASE_URL} from 'sentry/views/insights/pages/settings';
 
 jest.mock('sentry/utils/usePageFilters');
 jest.mock('sentry/utils/useLocation');
+
+const mockUseLocation = jest.mocked(useLocation);
 
 describe('SpanOpSelector', function () {
   const organization = OrganizationFixture();
@@ -36,6 +40,10 @@ describe('SpanOpSelector', function () {
 
   beforeEach(function () {
     MockApiClient.clearMockResponses();
+
+    mockUseLocation.mockReturnValue(
+      LocationFixture({pathname: `/${DOMAIN_VIEW_BASE_URL}/${MOBILE_LANDING_SUB_PATH}`})
+    );
 
     mockEventsRequest = MockApiClient.addMockResponse({
       url: `/organizations/${organization.slug}/events/`,
