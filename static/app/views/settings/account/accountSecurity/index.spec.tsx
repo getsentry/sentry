240815a5f490c3ba6c8c8091275a4b2184b69362
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-05-08 14:34:50 -0400

    ref(js): Rename auth interface u2f to match backend (#91254)

--- static/app/views/settings/account/accountSecurity/index.spec.tsx
+++ static/app/views/settings/account/accountSecurity/index.spec.tsx
@@ -281,7 +281,7 @@ describe('AccountSecurity', function () {
     renderComponent();
 
     expect(await screen.findByText('Authenticator App')).toBeInTheDocument();
-    expect(screen.getByText('U2F (Universal 2nd Factor)')).toBeInTheDocument();
+    expect(screen.getByText('Passkey / Biometric / Security Key')).toBeInTheDocument();
     expect(screen.queryByText('Text Message')).not.toBeInTheDocument();
   });
 
