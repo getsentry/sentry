c61f48830af1537b5e0571df93d3c4877a8fbfea
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2025-05-09 17:32:35 -0400

    ref: fix scope_list typing in testutils factories (#91393)
    <!-- Describe your PR here. -->

--- src/sentry/testutils/factories.py
+++ src/sentry/testutils/factories.py
@@ -464,8 +464,8 @@ class Factories:
 
     @staticmethod
     @assume_test_silo_mode(SiloMode.CONTROL)
-    def create_api_key(organization, scope_list=None, **kwargs):
-        return ApiKey.objects.create(organization_id=organization.id, scope_list=scope_list)
+    def create_api_key(organization, **kwargs) -> ApiKey:
+        return ApiKey.objects.create(organization_id=organization.id, **kwargs)
 
     @staticmethod
     @assume_test_silo_mode(SiloMode.CONTROL)
