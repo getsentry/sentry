d858a773a05372c8cf9c103f1fc73b846a5b4636
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-05-09 11:58:02 +0200

    demo steps with urls

--- static/app/utils/demoMode/demoTours.tsx
+++ static/app/utils/demoMode/demoTours.tsx
@@ -14,8 +14,13 @@ import {
 } from 'sentry/components/tours/tourContext';
 import {isDemoModeActive} from 'sentry/utils/demoMode';
 import {useLocalStorageState} from 'sentry/utils/useLocalStorageState';
+import {useLocation} from 'sentry/utils/useLocation';
+import {useNavigate} from 'sentry/utils/useNavigate';
 
 export const DEMO_TOURS_STATE_KEY = 'demo-mode:tours';
+const DEMO_SELECTED_PROJECT_ID = '4508160347275264';
+const DEMO_SELECTED_TRANSACTION = '/products';
+const DEMO_SELECTED_TRACE_ID = '171df07fc30e4e269358eb4d7cca7b9f';
 
 export const enum DemoTour {
   SIDEBAR = 'sidebar',
@@ -47,6 +52,118 @@ export const enum DemoTourStep {
   PERFORMANCE_SPAN_TREE = 'demo-tour-performance-span-tree',
 }
 
+type DemoTourStepWithUrl = {
+  id: DemoTourStep;
+  url?: {pathname: string; query?: Record<string, string>};
+};
+
+const TOUR_STEPS: Record<DemoTour, DemoTourStepWithUrl[]> = {
+  [DemoTour.SIDEBAR]: [
+    {
+      id: DemoTourStep.SIDEBAR_PROJECTS,
+    },
+    {
+      id: DemoTourStep.SIDEBAR_ISSUES,
+    },
+    {
+      id: DemoTourStep.SIDEBAR_PERFORMANCE,
+    },
+    {
+      id: DemoTourStep.SIDEBAR_RELEASES,
+    },
+    {
+      id: DemoTourStep.SIDEBAR_DISCOVER,
+    },
+  ],
+  [DemoTour.ISSUES]: [
+    {
+      id: DemoTourStep.ISSUES_STREAM,
+    },
+    {
+      id: DemoTourStep.ISSUES_AGGREGATES,
+    },
+    {
+      id: DemoTourStep.ISSUES_EVENT_DETAILS,
+    },
+    {
+      id: DemoTourStep.ISSUES_DETAIL_SIDEBAR,
+    },
+  ],
+  [DemoTour.RELEASES]: [
+    {
+      id: DemoTourStep.RELEASES_COMPARE,
+    },
+    {
+      id: DemoTourStep.RELEASES_DETAILS,
+    },
+    {
+      id: DemoTourStep.RELEASES_STATES,
+    },
+  ],
+  [DemoTour.PERFORMANCE]: [
+    {
+      id: DemoTourStep.PERFORMANCE_TABLE,
+      url: {
+        pathname: `/insights/frontend/`,
+      },
+    },
+    {
+      id: DemoTourStep.PERFORMANCE_USER_MISERY,
+      url: {
+        pathname: `/insights/frontend/summary/`,
+        query: {
+          transaction: DEMO_SELECTED_TRANSACTION,
+        },
+      },
+    },
+    {
+      id: DemoTourStep.PERFORMANCE_TRANSACTION_SUMMARY_TABLE,
+      url: {
+        pathname: `/insights/frontend/summary/`,
+        query: {
+          transaction: DEMO_SELECTED_TRANSACTION,
+        },
+      },
+    },
+    {
+      id: DemoTourStep.PERFORMANCE_SPAN_TREE,
+      url: {
+        pathname: `/insights/summary/trace/${DEMO_SELECTED_TRACE_ID}`,
+      },
+    },
+  ],
+};
+
+const emptyTourState = {
+  currentStepId: undefined,
+  isCompleted: false,
+  isRegistered: true,
+  orderedStepIds: [],
+};
+
+const TOUR_STATE_INITIAL_VALUE: Record<DemoTour, TourState<any>> = {
+  [DemoTour.SIDEBAR]: {
+    ...emptyTourState,
+    orderedStepIds: TOUR_STEPS[DemoTour.SIDEBAR].map(s => s.id),
+    tourKey: DemoTour.SIDEBAR,
+  },
+  [DemoTour.ISSUES]: {
+    ...emptyTourState,
+    orderedStepIds: TOUR_STEPS[DemoTour.ISSUES].map(s => s.id),
+    tourKey: DemoTour.ISSUES,
+  },
+  [DemoTour.RELEASES]: {
+    ...emptyTourState,
+    orderedStepIds: TOUR_STEPS[DemoTour.RELEASES].map(s => s.id),
+    tourKey: DemoTour.RELEASES,
+  },
+  [DemoTour.PERFORMANCE]: {
+    ...emptyTourState,
+    orderedStepIds: TOUR_STEPS[DemoTour.PERFORMANCE].map(s => s.id),
+    tourKey: DemoTour.PERFORMANCE,
+  },
+};
+
 type DemoToursContextType = {
   issues: TourContextType<DemoTourStep>;
   performance: TourContextType<DemoTourStep>;
@@ -72,67 +189,37 @@ export function useDemoTour(tourKey: DemoTour): TourContextType<DemoTourStep> | 
   return tourContext[tourKey];
 }
 
-const TOUR_STEPS: Record<DemoTour, DemoTourStep[]> = {
-  [DemoTour.SIDEBAR]: [
-    DemoTourStep.SIDEBAR_PROJECTS,
-    DemoTourStep.SIDEBAR_ISSUES,
-    DemoTourStep.SIDEBAR_PERFORMANCE,
-    DemoTourStep.SIDEBAR_RELEASES,
-    DemoTourStep.SIDEBAR_DISCOVER,
-  ],
-  [DemoTour.ISSUES]: [
-    DemoTourStep.ISSUES_STREAM,
-    DemoTourStep.ISSUES_AGGREGATES, // Metadata and metrics // view data in aggregate 1/6
-    DemoTourStep.ISSUES_EVENT_DETAILS, // Explore details // Explore details 3/6
-    DemoTourStep.ISSUES_DETAIL_SIDEBAR, // Share updates // 6/6
-  ],
-  [DemoTour.RELEASES]: [
-    DemoTourStep.RELEASES_COMPARE,
-    DemoTourStep.RELEASES_DETAILS,
-    DemoTourStep.RELEASES_STATES,
-  ],
-  [DemoTour.PERFORMANCE]: [
-    DemoTourStep.PERFORMANCE_TABLE,
-    DemoTourStep.PERFORMANCE_USER_MISERY,
-    DemoTourStep.PERFORMANCE_TRANSACTION_SUMMARY_TABLE,
-    DemoTourStep.PERFORMANCE_SPAN_TREE,
-  ],
-};
+function useNavigateToStep() {
+  const location = useLocation();
+  const navigate = useNavigate();
 
-const emptyTourState = {
-  currentStepId: undefined,
-  isCompleted: false,
-  isRegistered: true,
-  orderedStepIds: [],
-};
+  const getUrlFromStep = (stepId: DemoTourStep) => {
+    const step = TOUR_STEPS[getTourFromStep(stepId)].find(s => s.id === stepId);
 
-const TOUR_STATE_INITIAL_VALUE: Record<DemoTour, TourState<any>> = {
-  [DemoTour.SIDEBAR]: {
-    ...emptyTourState,
-    orderedStepIds: TOUR_STEPS[DemoTour.SIDEBAR],
-    tourKey: DemoTour.SIDEBAR,
-  },
-  [DemoTour.ISSUES]: {
-    ...emptyTourState,
-    orderedStepIds: TOUR_STEPS[DemoTour.ISSUES],
-    tourKey: DemoTour.ISSUES,
-  },
-  [DemoTour.RELEASES]: {
-    ...emptyTourState,
-    orderedStepIds: TOUR_STEPS[DemoTour.RELEASES],
-    tourKey: DemoTour.RELEASES,
-  },
-  [DemoTour.PERFORMANCE]: {
-    ...emptyTourState,
-    orderedStepIds: TOUR_STEPS[DemoTour.PERFORMANCE],
-    tourKey: DemoTour.PERFORMANCE,
-  },
-};
+    return {
+      pathname: step?.url?.pathname ?? '',
+      query: {
+        ...step?.url?.query,
+        project: DEMO_SELECTED_PROJECT_ID,
+      },
+    };
+  };
+
+  const navigateToStep = (stepId: DemoTourStep) => {
+    const target = getUrlFromStep(stepId);
+    if (location.pathname !== target.pathname && target.pathname !== '') {
+      navigate(target);
+    }
+  };
+
+  return navigateToStep;
+}
 
 export function DemoToursProvider({children}: {children: React.ReactNode}) {
   const [tourState, setTourState] = useLocalStorageState<
     Record<DemoTour, TourState<any>>
   >(DEMO_TOURS_STATE_KEY, TOUR_STATE_INITIAL_VALUE);
+  const navigateToStep = useNavigateToStep();
 
   const handleStepChange = useCallback(
     (tourKey: DemoTour, stepId: DemoTourStep) => {
@@ -140,8 +227,9 @@ export function DemoToursProvider({children}: {children: React.ReactNode}) {
         ...prev,
         [tourKey]: {...prev[tourKey], currentStepId: stepId},
       }));
+      navigateToStep(stepId);
     },
-    [setTourState]
+    [setTourState, navigateToStep]
   );
 
   const handleEndTour = useCallback(
@@ -203,7 +291,7 @@ export function DemoToursProvider({children}: {children: React.ReactNode}) {
 
 const getTourFromStep = (step: DemoTourStep): DemoTour => {
   for (const [category, steps] of Object.entries(TOUR_STEPS)) {
-    if (steps.includes(step)) {
+    if (steps.some(s => s.id === step)) {
       return category as DemoTour;
     }
   }
