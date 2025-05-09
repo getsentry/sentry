ceec4f17241eb6f8a2741f6b241514884c49dcde
Scott Cooper <scttcper@gmail.com>
2025-05-09 14:56:48 -0700

    feat(seer): Add seer automation settings page (#91205)

--- static/gsApp/hooks/settingsRoutes.tsx
+++ static/gsApp/hooks/settingsRoutes.tsx
@@ -71,6 +71,11 @@ const settingsRoutes = () =>
         name="Spike Protection"
         component={make(() => import('../views/spikeProtection'))}
       />
+      <Route
+        path="seer/"
+        name="Seer Automation"
+        component={make(() => import('../views/seerAutomation'))}
+      />
 
       <Route
         path="subscription/spend-allocations/"
