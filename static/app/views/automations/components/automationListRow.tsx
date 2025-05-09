a84491d7d0f0a5b7795912a09c455f60851a14b3
Mia Hsu <ameliahsu@gmail.com>
2025-05-09 16:52:24 -0700

    ref(aci): add optional connect/disconnect column to connected automations list

--- static/app/views/automations/components/automationListRow.tsx
+++ static/app/views/automations/components/automationListRow.tsx
@@ -4,8 +4,8 @@ import styled from '@emotion/styled';
 import {Flex} from 'sentry/components/container/flex';
 import {Checkbox} from 'sentry/components/core/checkbox';
 import InteractionStateLayer from 'sentry/components/interactionStateLayer';
-import Link from 'sentry/components/links/link';
 import {ActionCell} from 'sentry/components/workflowEngine/gridCell/actionCell';
+import AutomationTitleCell from 'sentry/components/workflowEngine/gridCell/automationTitleCell';
 import {ConnectionCell} from 'sentry/components/workflowEngine/gridCell/connectionCell';
 import {TimeAgoCell} from 'sentry/components/workflowEngine/gridCell/timeAgoCell';
 import {space} from 'sentry/styles/space';
@@ -39,9 +39,10 @@ export function AutomationListRow({
           }}
         />
         <CellWrapper>
-          <TitleCell to={makeAutomationDetailsPathname(organization.slug, id)}>
-            {name}
-          </TitleCell>
+          <AutomationTitleCell
+            name={name}
+            link={makeAutomationDetailsPathname(organization.slug, id)}
+          />
         </CellWrapper>
       </Flex>
       <CellWrapper className="last-triggered">
@@ -71,18 +72,6 @@ const CellWrapper = styled('div')`
   text-overflow: ellipsis;
   width: 100%;
   min-width: 0;
-`;
-
-const TitleCell = styled(Link)`
-  padding: ${space(2)};
-  margin: -${space(2)};
-  color: ${p => p.theme.textColor};
-
-  &:hover,
-  &:active {
-    text-decoration: underline;
-    color: ${p => p.theme.textColor};
-  }
 `;
 
 const RowWrapper = styled('div')<{disabled?: boolean}>`
--- static/app/views/automations/hooks/utils.tsx
+++ static/app/views/automations/hooks/utils.tsx
@@ -1,3 +1,5 @@
+import {useState} from 'react';
+
 import type {ActionType} from 'sentry/types/workflowEngine/actions';
 import type {Automation} from 'sentry/types/workflowEngine/automations';
 
@@ -11,4 +13,26 @@ export function useAutomationActions(automation: Automation): ActionType[] {
         .filter(x => x)
     ),
   ] as ActionType[];
+}
+
+export function useConnectedIds() {
+  const [connectedIds, setConnectedIds] = useState<Set<string>>(() => {
+    const stored = localStorage.getItem('connectedIds');
+    return stored ? new Set(JSON.parse(stored)) : new Set();
+  });
+
+  const toggleConnected = (id: string) => {
+    setConnectedIds(prev => {
+      const newSet = new Set(prev);
+      if (newSet.has(id)) {
+        newSet.delete(id);
+      } else {
+        newSet.add(id);
+      }
+      localStorage.setItem('connectedIds', JSON.stringify(Array.from(newSet)));
+      return newSet;
+    });
+  };
+
+  return {connectedIds, toggleConnected};
 }
