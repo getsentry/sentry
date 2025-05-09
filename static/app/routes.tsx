342e3d04ec78f352d9f20a471620cd2edd1bd670
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2025-05-09 14:42:35 -0400

    ref(insights): remove app starts module (#91272)
    The mobile vitals modules has been GA for 2 months
    https://github.com/getsentry/sentry-options-automator/pull/3235
    This PR removes the app start module (which was replaced by mobile
    vitals).
    More PRs will be created for other modules

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -1747,20 +1747,6 @@ function buildRoutes() {
           )}
         />
       </Route>
-      <Route path={`${MODULE_BASE_URLS[ModuleName.APP_START]}/`}>
-        <IndexRoute
-          component={make(
-            () =>
-              import('sentry/views/insights/mobile/appStarts/views/appStartsLandingPage')
-          )}
-        />
-        <Route
-          path="spans/"
-          component={make(
-            () => import('sentry/views/insights/mobile/appStarts/views/screenSummaryPage')
-          )}
-        />
-      </Route>
       <Route path={`${MODULE_BASE_URLS[ModuleName.MOBILE_UI]}/`}>
         <IndexRoute
           component={make(
