d5bb859245351934ca6ed6da709eaef1df8d2947
Colin Chartier <colin.chartier@sentry.io>
2025-05-12 15:01:16 -0400

    feat(ourlogs): Use aliases in the frontend

--- static/app/views/explore/logs/types.tsx
+++ static/app/views/explore/logs/types.tsx
@@ -25,13 +25,14 @@ export enum OurLogKnownFieldKey {
   CODE_LINE_NUMBER = 'tags[code.line.number,number]',
   CODE_FUNCTION_NAME = 'code.function.name',
 
+  RELEASE = 'release',
+  TEMPLATE = 'message.template',
+  PARENT_SPAN_ID = 'trace.parent_span_id',
+  SDK_NAME = 'sdk.name',
+  SDK_VERSION = 'sdk.version',
+
   // From the EAP dataset directly not using a column alias.
   ID = 'sentry.item_id',
-  RELEASE = 'sentry.release',
-  TEMPLATE = 'sentry.message.template',
-  PARENT_SPAN_ID = 'sentry.trace.parent_span_id',
-  SDK_NAME = 'sentry.sdk.name',
-  SDK_VERSION = 'sentry.sdk.version',
 
   // From the EAP dataset directly not using a column alias, should be hidden.
   ITEM_TYPE = 'sentry.item_type',
