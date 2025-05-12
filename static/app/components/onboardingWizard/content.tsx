b820f39724c86b7a082ba478188278f2549b62f8
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-05-12 15:16:04 +0200

    layout transition update

--- static/app/components/onboardingWizard/content.tsx
+++ static/app/components/onboardingWizard/content.tsx
@@ -1,7 +1,7 @@
 import {Fragment, useCallback, useEffect, useMemo, useRef, useState} from 'react';
 import {css, useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
-import {AnimatePresence} from 'framer-motion';
+import {AnimatePresence, motion} from 'framer-motion';
 import partition from 'lodash/partition';
 
 import {navigateTo} from 'sentry/actionCreators/navigation';
@@ -24,6 +24,7 @@ import {trackAnalytics} from 'sentry/utils/analytics';
 import {isDemoModeActive} from 'sentry/utils/demoMode';
 import {DemoTour, useDemoTours} from 'sentry/utils/demoMode/demoTours';
 import {updateDemoWalkthroughTask} from 'sentry/utils/demoMode/guides';
+import testableTransition from 'sentry/utils/testableTransition';
 import {useLocalStorageState} from 'sentry/utils/useLocalStorageState';
 import useOrganization from 'sentry/utils/useOrganization';
 import useRouter from 'sentry/utils/useRouter';
@@ -176,6 +177,22 @@ function Task({task, hidePanel}: TaskProps) {
 
   const tours = useDemoTours();
 
+  const [enableLayoutAnimation, setEnableLayoutAnimation] = useState(false);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setEnableLayoutAnimation(true);
+    }, 0);
+
+    return () => clearTimeout(timerId);
+  }, []);
+
+  // We want layout animation to be disabled initially, and if the skip confirmation is open
+  // but we want to animate the task card the task status changes
+  const shouldLayoutAnimate = useMemo(() => {
+    return enableLayoutAnimation && !showSkipConfirmation;
+  }, [enableLayoutAnimation, showSkipConfirmation]);
+
   const handleClick = useCallback(
     (e: React.MouseEvent) => {
       trackAnalytics('quick_start.task_card_clicked', {
@@ -257,7 +274,14 @@ function Task({task, hidePanel}: TaskProps) {
   }, [task.status]);
 
   return (
-    <TaskWrapper>
+    <TaskWrapper
+      key={task.task}
+      layout={'position'}
+      transition={testableTransition({
+        duration: shouldLayoutAnimate ? 0.3 : 0,
+        delay: shouldLayoutAnimate ? 0.5 : 0,
+      })}
+    >
       <TaskCard
         onClick={
           task.status === 'complete' || task.status === 'skipped'
@@ -572,7 +596,7 @@ const TaskGroupBody = styled('ul')`
   margin: 0;
 `;
 
-const TaskWrapper = styled('li')`
+const TaskWrapper = styled(motion.li)`
   gap: ${space(1)};
   p {
     color: ${p => p.theme.subText};
