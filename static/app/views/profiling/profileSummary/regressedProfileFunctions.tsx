1d12681be5c6f79aa46d9faa1ca1108fe58b8e36
Tony Xiao <txiao@sentry.io>
2025-05-12 11:06:41 -0400

    chore(profiling): Cleanup profiling analytic events (#91071)
    This cleans up (adding/removing/updating) the various analytic events
    for profiling views.

--- static/app/views/profiling/profileSummary/regressedProfileFunctions.tsx
+++ static/app/views/profiling/profileSummary/regressedProfileFunctions.tsx
@@ -320,7 +320,7 @@ function RegressedFunctionBeforeAfterFlamechart(
   const onRegressedFunctionClick = useCallback(() => {
     trackAnalytics('profiling_views.go_to_flamegraph', {
       organization: props.organization,
-      source: `profiling_transaction.regressed_functions_table`,
+      source: 'unknown',
     });
   }, [props.organization]);
 
