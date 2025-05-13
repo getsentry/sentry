ec57b9a9cd5081fc6f7b441a074789f108070c7f
Raj Joshi <raj.joshi@sentry.io>
2025-05-13 07:15:36 -0700

    :wrench: chore: update test

--- tests/sentry/workflow_engine/migrations/test_0060_rename_azure_devops_action_to_vsts.py
+++ tests/sentry/workflow_engine/migrations/test_0060_rename_azure_devops_action_to_vsts.py
@@ -1,6 +1,8 @@
 from sentry.testutils.cases import TestMigrations
+from sentry.workflow_engine.models.action import Action
 
 
+# @pytest.mark.skip("Timeout failures—skipping these tests, which pass, to unblock migration.")
 class TestRenameAzureDevopsActionToVsts(TestMigrations):
     app = "workflow_engine"
     migrate_from = "0059_fix_high_priority_condition_triggers"
@@ -9,25 +11,28 @@ class TestRenameAzureDevopsActionToVsts(TestMigrations):
     def setup_initial_state(self):
         # Create actions that should be renamed
         self.azure_action1 = self.create_action()
-        self.azure_action1.update(type="azure_devops")
+        # Need to manually update the type so the validation signals don't run
+        self.azure_action1.update(
+            type="azure_devops", config={"target_identifier": None, "target_type": 0}, data={}
+        )
         self.azure_action1.save()
+
         self.azure_action2 = self.create_action()
-        self.azure_action2.update(type="azure_devops")
+        self.azure_action2.update(
+            type="azure_devops", config={"target_identifier": None, "target_type": 0}, data={}
+        )
         self.azure_action2.save()
 
-        # Don't rename these actions
-        self.slack_action = self.create_action(type="slack")
-        self.other_action = self.create_action(type="other")
+        # Don't rename this action
+        # Defaults to a slack action
+        self.slack_action = self.create_action()
 
     def test(self):
         self.azure_action1.refresh_from_db()
-        assert self.azure_action1.type == "vsts"
+        assert self.azure_action1.type == Action.Type.AZURE_DEVOPS
 
         self.azure_action2.refresh_from_db()
-        assert self.azure_action2.type == "vsts"
+        assert self.azure_action2.type == Action.Type.AZURE_DEVOPS
 
         self.slack_action.refresh_from_db()
-        assert self.slack_action.type == "slack"
-
-        self.other_action.refresh_from_db()
-        assert self.other_action.type == "other"
+        assert self.slack_action.type == Action.Type.SLACK
