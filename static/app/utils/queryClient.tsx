7fbda5a2ba7c214fc630c38f8ca6fd4996a1cc86
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-05-13 11:12:47 -0400

    chore(fe): Convert `<ReleasesList />` to an FC (#91281)
    It's a pretty big rewrite, but here are the callouts:
    - Importing the deprecated ~~`withSentryRouter`~~ `useRouter()` for the
    inner list component. This is just so we don't have to radically change
    two components at once, and can focus on the AsyncComponent in this PR
    - The `getSelectedProjectSlugs` and `projectHasSession` methods were
    unused.
    - In the test file, the one 'empty state' test has been broken up into 4
    to avoid excessive unmount/rerender loops, especially since the new
    syntax for location doesn't work with `rerender`
    
    
    For testing this one out, I kinda just clicked around in `dev-ui` and
    double checked that all the existing behavior was maintained. If you
    notice anything wrong on the deploy preview let me know :+1:

--- static/app/utils/queryClient.tsx
+++ static/app/utils/queryClient.tsx
@@ -96,7 +96,7 @@ export type UseApiQueryResult<TData, TError> = UseQueryResult<TData, TError> & {
  * Query keys should be an array which include an endpoint URL and options such as query params.
  * This wrapper will execute the request using the query key URL.
  *
- * See https://tanstack.com/query/v4/docs/overview for docs on React Query.
+ * See https://tanstack.com/query/v5/docs/framework/react/overview for docs on React Query.
  *
  * Example usage:
  *
