daf22ccc752811365b5bf2a4e7d4c4250d7a64a2
Michael Sun <55160142+MichaelSun48@users.noreply.github.com>
2025-05-13 14:28:51 -0700

    ref(shared-views): Refactor feature flags and remove some more old components  (#91506)
    Removes some components that are no longer being used as part of the
    issue view sharing release.
    
    Also refactors some feature flag checks

--- static/app/views/nav/usePrefersStackedNav.tsx
+++ static/app/views/nav/usePrefersStackedNav.tsx
@@ -29,13 +29,3 @@ export function usePrefersOldNavWithEnforcedStackedNav() {
     user?.options?.prefersStackedNavigation === false
   );
 }
-
-export function useHasIssueViewSharing() {
-  const organization = useOrganization({allowNull: true});
-  const user = useUser();
-
-  return (
-    !!organization?.features.includes('enforce-stacked-navigation') &&
-    user?.options?.prefersStackedNavigation !== false
-  );
-}
