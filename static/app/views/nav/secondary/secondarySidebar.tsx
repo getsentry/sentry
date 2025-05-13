03bc3066d42f39f44e241421e3282c802780abad
MichaelSun48 <michael.sun@sentry.io>
2025-05-13 09:58:41 -0700

    Add new performant hook for resizing

--- static/app/views/nav/secondary/secondarySidebar.tsx
+++ static/app/views/nav/secondary/secondarySidebar.tsx
@@ -1,6 +1,12 @@
+import {useRef} from 'react';
 import styled from '@emotion/styled';
 
-import {SECONDARY_SIDEBAR_WIDTH} from 'sentry/views/nav/constants';
+import useResizable from 'sentry/utils/useResizable';
+import {
+  SECONDARY_SIDEBAR_MAX_WIDTH,
+  SECONDARY_SIDEBAR_MIN_WIDTH,
+  SECONDARY_SIDEBAR_WIDTH,
+} from 'sentry/views/nav/constants';
 import {useNavContext} from 'sentry/views/nav/context';
 import {
   NavTourElement,
@@ -13,28 +19,51 @@ export function SecondarySidebar() {
   const {setSecondaryNavEl} = useNavContext();
   const {currentStepId} = useStackedNavigationTour();
   const stepId = currentStepId ?? StackedNavigationTour.ISSUES;
+  const resizableContainerRef = useRef<HTMLDivElement>(null);
+  const resizeHandleRef = useRef<HTMLDivElement>(null);
+
+  const {
+    onMouseDown: handleStartResize,
+    size,
+    onDoubleClick,
+  } = useResizable({
+    ref: resizableContainerRef,
+    initialSize: SECONDARY_SIDEBAR_WIDTH,
+    minWidth: SECONDARY_SIDEBAR_MIN_WIDTH,
+    maxWidth: SECONDARY_SIDEBAR_MAX_WIDTH,
+    sizeStorageKey: 'secondary-sidebar-width',
+  });
 
   return (
-    <SecondarySidebarWrapper
-      id={stepId}
-      description={STACKED_NAVIGATION_TOUR_CONTENT[stepId].description}
-      title={STACKED_NAVIGATION_TOUR_CONTENT[stepId].title}
-    >
-      <SecondarySidebarInner
-        ref={setSecondaryNavEl}
-        role="navigation"
-        aria-label="Secondary Navigation"
-      />
-    </SecondarySidebarWrapper>
+    <ResizeWrapper ref={resizableContainerRef} onMouseDown={handleStartResize}>
+      <NavTourElement
+        id={stepId}
+        description={STACKED_NAVIGATION_TOUR_CONTENT[stepId].description}
+        title={STACKED_NAVIGATION_TOUR_CONTENT[stepId].title}
+      >
+        <SecondarySidebarInner
+          ref={setSecondaryNavEl}
+          role="navigation"
+          aria-label="Secondary Navigation"
+        />
+        <ResizeHandle
+          ref={resizeHandleRef}
+          onMouseDown={handleStartResize}
+          onDoubleClick={onDoubleClick}
+          atMinWidth={size === SECONDARY_SIDEBAR_MIN_WIDTH}
+          atMaxWidth={size === SECONDARY_SIDEBAR_MAX_WIDTH}
+        />
+      </NavTourElement>
+    </ResizeWrapper>
   );
 }
 
-const SecondarySidebarWrapper = styled(NavTourElement)`
+const ResizeWrapper = styled('div')`
   position: relative;
+  right: 0;
   border-right: 1px solid
     ${p => (p.theme.isChonk ? p.theme.border : p.theme.translucentGray200)};
   background: ${p => (p.theme.isChonk ? p.theme.background : p.theme.surface200)};
-  width: ${SECONDARY_SIDEBAR_WIDTH}px;
   z-index: ${p => p.theme.zIndex.sidebarPanel};
   height: 100%;
 `;
@@ -43,4 +72,34 @@ const SecondarySidebarInner = styled('div')`
   display: grid;
   grid-template-rows: auto 1fr auto;
   height: 100%;
+`;
+
+const ResizeHandle = styled('div')<{atMaxWidth: boolean; atMinWidth: boolean}>`
+  position: absolute;
+  right: 0px;
+  top: 0;
+  bottom: 0;
+  width: 8px;
+  border-radius: 8px;
+  z-index: ${p => p.theme.zIndex.drawer + 2};
+  cursor: ${p => (p.atMinWidth ? 'e-resize' : p.atMaxWidth ? 'w-resize' : 'ew-resize')};
+
+  &:hover,
+  &:active {
+    &::after {
+      background: ${p => p.theme.purple400};
+    }
+  }
+
+  &::after {
+    content: '';
+    position: absolute;
+    right: -2px;
+    top: 0;
+    bottom: 0;
+    width: 4px;
+    opacity: 0.8;
+    background: transparent;
+    transition: background 0.25s ease 0.1s;
+  }
 `;
