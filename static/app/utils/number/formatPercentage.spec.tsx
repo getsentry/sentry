c379f47463a81a693966cbe407ec78999c278196
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-05-15 14:02:41 +0200

    fix(utils): return 0% for null and undefined values in formatpercentage

--- static/app/utils/number/formatPercentage.spec.tsx
+++ static/app/utils/number/formatPercentage.spec.tsx
@@ -16,4 +16,11 @@ describe('formatPercentage()', function () {
     expect(formatPercentage(-0.0001, 0, {minimumValue: 0.001})).toBe('<0.1%');
     expect(formatPercentage(0.00000234, 0, {minimumValue: 0.0001})).toBe('<0.01%');
   });
+
+  it('handles null and undefined inputs', function () {
+    // @ts-expect-error we are testing invalid inputs
+    expect(formatPercentage(null)).toBe('0%');
+    // @ts-expect-error we are testing invalid inputs
+    expect(formatPercentage(undefined)).toBe('0%');
+  });
 });
