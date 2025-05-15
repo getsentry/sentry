bea903cdad0ec17365ce73f09b86f9bec45ddbbe
ArthurKnaus <arthur.knaus@sentry.io>
2025-05-15 10:11:56 +0200

    feat(nextjs-insights): Move dropdown options into segmented control (#91698)
    Move the table's dropdown options into the segmented control.
    Prevent the API table from collapsing as in
    https://github.com/getsentry/sentry/pull/91671
    Add analytics event for switching tables.
    Prevent URL param changes from resetting the scroll position.
    
    - closes [TET-418: Pages Table: Scroll to top when
    switching](https://linear.app/getsentry/issue/TET-418/pages-table-scroll-to-top-when-switching)
    - closes [TET-417: Pages Table: Visibility of display
    dropdown](https://linear.app/getsentry/issue/TET-417/pages-table-visibility-of-display-dropdown)

--- static/app/utils/analytics/nextJsInsightsAnalyticsEvents.tsx
+++ static/app/utils/analytics/nextJsInsightsAnalyticsEvents.tsx
@@ -1,5 +1,10 @@
 type NextJsInsightsEventParameters = {
-  'nextjs-insights.page-view': Record<string, unknown>;
+  'nextjs-insights.page-view': {
+    view: string;
+  };
+  'nextjs-insights.table_view_change': {
+    view: string;
+  };
   'nextjs-insights.ui_toggle': {
     isEnabled: boolean;
   };
@@ -8,5 +13,6 @@ type NextJsInsightsEventParameters = {
 export const nextJsInsightsEventMap: Record<keyof NextJsInsightsEventParameters, string> =
   {
     'nextjs-insights.page-view': 'NextJS Insights: Page View',
+    'nextjs-insights.table_view_change': 'NextJS Insights: Table View Change',
     'nextjs-insights.ui_toggle': 'NextJS Insights: UI Toggle',
   };
