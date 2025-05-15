e6f482658994c70a8692fb1fa3cd0bde91f95c61
Mark Story <mark@mark-story.com>
2025-05-15 13:43:54 -0400

    fix(errors) Remove task binding from preprocess_event functions (#91655)
    These functions have no active .delay() or .apply_async() calls.
    Converting these to simple functions means we don't have to update the
    task signatures for JSON compatibility.

--- src/sentry/ingest/consumer/processors.py
+++ src/sentry/ingest/consumer/processors.py
@@ -202,7 +202,7 @@ def process_event(
         # Otherwise, we have to store the event in processing store here for the save_event task to
         # fetch later
         if no_celery_mode:
-            cache_key = None
+            cache_key: str | None = None
         else:
             with metrics.timer("ingest_consumer._store_event"):
                 cache_key = processing_store.store(data)
@@ -281,7 +281,7 @@ def process_event(
             # cache.
             with sentry_sdk.start_span(op="ingest_consumer.process_event.preprocess_event"):
                 preprocess_event(
-                    cache_key=cache_key,
+                    cache_key=cache_key or "",
                     data=data,
                     start_time=start_time,
                     event_id=event_id,
