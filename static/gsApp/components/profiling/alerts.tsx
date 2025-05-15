f5bb7071b5209acb038e3cbd97ecb17b954bc66c
Valery Brobbey <valery.brobbey@sentry.io>
2025-05-15 16:11:11 -0700

    feat: update profiling trial banner button text

--- static/gsApp/components/profiling/alerts.tsx
+++ static/gsApp/components/profiling/alerts.tsx
@@ -9,6 +9,7 @@ import {space} from 'sentry/styles/space';
 import {DataCategoryExact} from 'sentry/types/core';
 import type {Organization} from 'sentry/types/organization';
 import {useApiQuery} from 'sentry/utils/queryClient';
+import {toTitleCase} from 'sentry/utils/string/toTitleCase';
 import useDismissAlert from 'sentry/utils/useDismissAlert';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
@@ -291,6 +292,7 @@ function ContinuousProfilingBetaAlertBannerInner({
           notificationType="overage_critical"
           referrer={`overage-alert-${eventTypes.join('-')}`}
           source="continuous-profiling-beta-trial-banner"
+          buttonText={toTitleCase(`Update ${subscription.planDetails.budgetTerm} Budget`)}
         />
       }
     >
