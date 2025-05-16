5669b301db2a78f6bea1261c39c2e974d86d47a8
Mia Hsu <ameliahsu@gmail.com>
2025-05-16 13:40:27 -0700

    feat(aci): add eventUniqueUserFrequency and percentSessions data condition nodes

--- static/app/views/automations/components/actionFilters/eventUniqueUserFrequency.tsx
+++ static/app/views/automations/components/actionFilters/eventUniqueUserFrequency.tsx
@@ -7,8 +7,8 @@ import {
 } from 'sentry/views/automations/components/actionFilters/comparisonBranches';
 import {useDataConditionNodeContext} from 'sentry/views/automations/components/dataConditionNodes';
 
-export default function EventFrequencyNode() {
-  return tct('Number of events in an issue is [select]', {
+export default function EventUniqueUserFrequencyNode() {
+  return tct('Number of users affected by an issue is [select]', {
     select: <ComparisonTypeField />,
   });
 }
@@ -16,10 +16,12 @@ export default function EventFrequencyNode() {
 function ComparisonTypeField() {
   const {condition, condition_id, onUpdateType} = useDataConditionNodeContext();
 
-  if (condition.comparison_type === DataConditionType.EVENT_FREQUENCY_COUNT) {
+  if (condition.comparison_type === DataConditionType.EVENT_UNIQUE_USER_FREQUENCY_COUNT) {
     return <CountBranch />;
   }
-  if (condition.comparison_type === DataConditionType.EVENT_FREQUENCY_PERCENT) {
+  if (
+    condition.comparison_type === DataConditionType.EVENT_UNIQUE_USER_FREQUENCY_PERCENT
+  ) {
     return <PercentBranch />;
   }
 
@@ -30,11 +32,11 @@ function ComparisonTypeField() {
       options={[
         {
           label: 'more than...',
-          value: DataConditionType.EVENT_FREQUENCY_COUNT,
+          value: DataConditionType.EVENT_UNIQUE_USER_FREQUENCY_COUNT,
         },
         {
           label: 'relatively higher than...',
-          value: DataConditionType.EVENT_FREQUENCY_PERCENT,
+          value: DataConditionType.EVENT_UNIQUE_USER_FREQUENCY_PERCENT,
         },
       ]}
       onChange={(value: DataConditionType) => {
