64d2a2f5b8897799dd1f13ec97d1998b56f22c05
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2025-05-16 16:00:30 -0700

    fix"

--- src/sentry/utils/performance_issues/detectors/sql_injection_detector.py
+++ src/sentry/utils/performance_issues/detectors/sql_injection_detector.py
@@ -12,7 +12,6 @@ from sentry.utils.performance_issues.base import (
     PerformanceDetector,
     fingerprint_span,
     get_notification_attachment_body,
-    get_span_evidence_value,
 )
 from sentry.utils.performance_issues.performance_problem import PerformanceProblem
 from sentry.utils.performance_issues.types import Span
@@ -73,46 +72,42 @@ class SQLInjectionDetector(PerformanceDetector):
 
         description = span.get("description")
         fingerprint = fingerprint_span(span)
-        hash = span.get("hash", "")
         op = span.get("op", None)
         spans_involved = [span["span_id"]]
 
-        for user_input in self.user_inputs:
-            if user_input in description:
-                self.stored_problems[fingerprint] = PerformanceProblem(
-                    type=SQLInjectionGroupType,
-                    fingerprint=self._fingerprint(hash),
-                    op=op,
-                    desc=description[:MAX_EVIDENCE_VALUE_LENGTH],
-                    cause_span_ids=[],
-                    parent_span_ids=[],
-                    offender_span_ids=spans_involved,
-                    evidence_data={
-                        "op": op,
-                        "cause_span_ids": [],
-                        "parent_span_ids": [],
-                        "offender_span_ids": spans_involved,
-                        "transaction_name": self._event.get("description", ""),
-                        "repeating_spans": get_span_evidence_value(span)[
-                            :MAX_EVIDENCE_VALUE_LENGTH
-                        ],
-                        "repeating_spans_compact": get_span_evidence_value(span, include_op=False)[
-                            :MAX_EVIDENCE_VALUE_LENGTH
-                        ],
-                        "num_repeating_spans": str(len(spans_involved)),
-                    },
-                    evidence_display=[
-                        IssueEvidence(
-                            name="Offending Spans",
-                            value=get_notification_attachment_body(
-                                op,
-                                description,
-                            )[:MAX_EVIDENCE_VALUE_LENGTH],
-                            # Has to be marked important to be displayed in the notifications
-                            important=True,
-                        )
-                    ],
-                )
+        if any(user_input in description for user_input in self.user_inputs):
+            for user_input in self.user_inputs:
+                fingerprint_description = span.get("description")
+                if user_input in fingerprint_description:
+                    fingerprint_description = fingerprint_description.replace(user_input, "")
+
+            self.stored_problems[fingerprint] = PerformanceProblem(
+                type=SQLInjectionGroupType,
+                fingerprint=self._fingerprint(fingerprint_description),
+                op=op,
+                desc=description[:MAX_EVIDENCE_VALUE_LENGTH],
+                cause_span_ids=[],
+                parent_span_ids=[],
+                offender_span_ids=spans_involved,
+                evidence_data={
+                    "op": op,
+                    "cause_span_ids": [],
+                    "parent_span_ids": [],
+                    "offender_span_ids": spans_involved,
+                    "transaction_name": self._event.get("description", ""),
+                },
+                evidence_display=[
+                    IssueEvidence(
+                        name="Offending Spans",
+                        value=get_notification_attachment_body(
+                            op,
+                            description,
+                        )[:MAX_EVIDENCE_VALUE_LENGTH],
+                        # Has to be marked important to be displayed in the notifications
+                        important=True,
+                    )
+                ],
+            )
 
     def is_creation_allowed_for_organization(self, organization: Organization) -> bool:
         return True
@@ -142,7 +137,7 @@ class SQLInjectionDetector(PerformanceDetector):
 
         return True
 
-    def _fingerprint(self, hash: str) -> str:
-        signature = (str(hash)).encode("utf-8")
+    def _fingerprint(self, description: str) -> str:
+        signature = (str(description)).encode("utf-8")
         full_fingerprint = hashlib.sha1(signature).hexdigest()
         return f"1-{SQLInjectionGroupType.type_id}-{full_fingerprint}"
