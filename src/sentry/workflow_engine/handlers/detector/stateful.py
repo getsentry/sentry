ec975381440ab18a9914c26f12c64c2feae854a7
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-05-15 23:01:47 -0700

    Move the docs into pretty markdown

--- src/sentry/workflow_engine/handlers/detector/stateful.py
+++ src/sentry/workflow_engine/handlers/detector/stateful.py
@@ -271,31 +271,6 @@ class StatefulDetectorHandler(
 ):
     """
     Stateful Detectors are provided as a base class for new detectors that need to track state.
-
-    See below for helpful hooks and methods to override.
-    # TODO @saponifi3d - turn this into a markdown file
-    ``````````````````
-    ## Detector Evaluations (`StatefulDetectorHandler.evaluate`)
-    This method will be called for each data packet that is processed by the detector.
-
-    The detector will extract the data from each packet, evaluate the conditions, and return a
-    DetectorEvaluationResult or a group of them.
-
-    ## Detector Counters
-    StatefulDetectorHandlers will track each time the detector reaches a PriorityLevel.
-
-    If a PriorityLevel's threshold is reached, the detector will create an issue occurrence.  By default,
-    each PriorityLevel's threshold value is set to 1, so the detector will create an issue
-    occurrence each time it reaches that PriorityLevel.
-
-    To override these thresholds use the `counters` property in the constructor. For example:
-    ```python
-    class ExampleDetectorHandler(StatefulDetectorHandler):
-        thresholds: DetectorThresholds = {
-            DetectorPriorityLevel.LOW: 10,
-            DetectorPriorityLevel.HIGH: 5,
-        }
-    ```
     """
 
     thresholds: DetectorThresholds = {}
