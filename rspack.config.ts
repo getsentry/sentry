d3989a49390c1adc124ec12913f9dc51f7c01104
Scott Cooper <scttcper@gmail.com>
2025-05-20 14:58:46 -0700

    disable cache

--- rspack.config.ts
+++ rspack.config.ts
@@ -762,17 +762,17 @@ if (CODECOV_TOKEN && ENABLE_CODECOV_BA) {
 }
 
 // Cache rspack builds
-if (env.WEBPACK_CACHE_PATH) {
-  appConfig.cache = true;
-  appConfig.experiments!.cache = {
-    type: 'persistent',
-    // https://rspack.dev/config/experiments#cachestorage
-    storage: {
-      type: 'filesystem',
-      directory: path.join(__dirname, env.WEBPACK_CACHE_PATH),
-    },
-  };
-}
+// if (env.WEBPACK_CACHE_PATH) {
+//   appConfig.cache = true;
+//   appConfig.experiments!.cache = {
+//     type: 'persistent',
+//     // https://rspack.dev/config/experiments#cachestorage
+//     storage: {
+//       type: 'filesystem',
+//       directory: path.join(__dirname, env.WEBPACK_CACHE_PATH),
+//     },
+//   };
+// }
 
 appConfig.plugins?.push(
   sentryWebpackPlugin({
