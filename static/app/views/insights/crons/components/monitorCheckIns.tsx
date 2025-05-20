0dcb508b978258e05a24f70b213ec0a0e2c3b570
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-05-20 19:13:29 -0400

    ref(crons): Share checks table with group details (#91988)
    Prior to this change the checks table for cron issues was a totally
    separate implementation from the table in the cron details page.
    
    This unifies the two tables
    
    <img alt="clipboard.png" width="1815"
    src="https://i.imgur.com/2aPAb8w.png" />

--- static/app/views/insights/crons/components/monitorCheckIns.tsx
+++ static/app/views/insights/crons/components/monitorCheckIns.tsx
@@ -1,19 +1,15 @@
 import {Fragment} from 'react';
-import styled from '@emotion/styled';
 
 import {SectionHeading} from 'sentry/components/charts/styles';
 import LoadingError from 'sentry/components/loadingError';
 import Pagination from 'sentry/components/pagination';
-import {PanelTable} from 'sentry/components/panels/panelTable';
-import Placeholder from 'sentry/components/placeholder';
 import {t} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import type {Monitor, MonitorEnvironment} from 'sentry/views/insights/crons/types';
 import {useMonitorCheckIns} from 'sentry/views/insights/crons/utils/useMonitorCheckIns';
 
-import {CheckInRow} from './checkInRow';
+import {MonitorCheckInsGrid} from './monitorCheckInsGrid';
 
 type Props = {
   monitor: Monitor;
@@ -47,49 +43,16 @@ export function MonitorCheckIns({monitor, monitorEnvs}: Props) {
 
   const hasMultiEnv = monitorEnvs.length > 1;
 
-  const headers = [
-    t('Status'),
-    t('Started'),
-    t('Completed'),
-    t('Duration'),
-    t('Issues'),
-    ...(hasMultiEnv ? [t('Environment')] : []),
-    t('Expected At'),
-  ];
-
   return (
     <Fragment>
       <SectionHeading>{t('Recent Check-Ins')}</SectionHeading>
-      <PanelTable
-        headers={headers}
-        isEmpty={!isPending && checkInList.length === 0}
-        emptyMessage={t('No check-ins have been recorded for this time period.')}
-      >
-        {isPending
-          ? [...new Array(PER_PAGE)].map((_, i) => (
-              <RowPlaceholder key={i}>
-                <Placeholder height="2rem" />
-              </RowPlaceholder>
-            ))
-          : checkInList.map(checkIn => (
-              <CheckInRow
-                key={checkIn.id}
-                monitor={monitor}
-                checkIn={checkIn}
-                hasMultiEnv={hasMultiEnv}
-              />
-            ))}
-      </PanelTable>
+      <MonitorCheckInsGrid
+        checkIns={checkInList ?? []}
+        isLoading={isPending}
+        hasMultiEnv={hasMultiEnv}
+        project={monitor.project}
+      />
       <Pagination pageLinks={getResponseHeader?.('Link')} />
     </Fragment>
   );
 }
-
-const RowPlaceholder = styled('div')`
-  grid-column: 1 / -1;
-  padding: ${space(1)};
-
-  &:not(:last-child) {
-    border-bottom: solid 1px ${p => p.theme.innerBorder};
-  }
-`;
