3313f6eb2654d29afb24b927df472c20ef3df1f1
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-05-21 14:16:23 +0200

    typo

--- static/app/views/insights/agentMonitoring/utils/features.tsx
+++ static/app/views/insights/agentMonitoring/utils/features.tsx
@@ -3,14 +3,14 @@ import {NoAccess} from 'sentry/components/noAccess';
 import type {Organization} from 'sentry/types/organization';
 
 export function hasAgentInsights(organization: Organization) {
-  return organization.features.includes('agent-insights');
+  return organization.features.includes('agents-insights');
 }
 
 type AgentInsightsFeatureProps = Omit<Parameters<typeof Feature>[0], 'features'>;
 
 export function AgentInsightsFeature(props: AgentInsightsFeatureProps) {
   return (
-    <Feature features="agent-insights" renderDisabled={props.renderDisabled ?? NoAccess}>
+    <Feature features="agents-insights" renderDisabled={props.renderDisabled ?? NoAccess}>
       {props.children}
     </Feature>
   );
