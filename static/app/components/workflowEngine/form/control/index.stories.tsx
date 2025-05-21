377fc3285bc5f138d91003eb67de48f7c607ff6b
TkDodo <dominik.dorfmeister@sentry.io>
2025-05-21 09:20:50 +0200

    ref(✂️): remove unused workflowEngine code

--- static/app/components/workflowEngine/form/control/index.stories.tsx
+++ static/app/components/workflowEngine/form/control/index.stories.tsx
@@ -4,7 +4,6 @@ import {Flex} from 'sentry/components/container/flex';
 import Form from 'sentry/components/forms/form';
 import JSXNode from 'sentry/components/stories/jsxNode';
 import PriorityControl from 'sentry/components/workflowEngine/form/control/priorityControl';
-import {FormSection} from 'sentry/components/workflowEngine/form/section';
 import storyBook from 'sentry/stories/storyBook';
 import {space} from 'sentry/styles/space';
 
@@ -18,17 +17,7 @@ export default storyBook('Form Controls', story => {
 
       <Form hideFooter>
         <Flex column gap={space(2)}>
-          <FormSection
-            title="Prioritize"
-            description="Update issue priority when the following thresholds are met:"
-          >
-            <PriorityControl name="priority" />
-          </FormSection>
-
-          <FormSection
-            title="Prioritize"
-            description="Sentry will automatically update priority."
-          />
+          <PriorityControl name="priority" />
         </Flex>
       </Form>
     </Fragment>
