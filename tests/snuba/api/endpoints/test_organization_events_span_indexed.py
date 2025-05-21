4976d1602843ed4641019f312e7bec032a33e81c
Tony Xiao <txiao@sentry.io>
2025-05-21 14:54:50 -0400

    tests(eap): Test special characters in eap attributes

--- tests/snuba/api/endpoints/test_organization_events_span_indexed.py
+++ tests/snuba/api/endpoints/test_organization_events_span_indexed.py
@@ -4646,3 +4646,35 @@ class OrganizationEventsEAPRPCSpanEndpointTest(OrganizationEventsSpanIndexedEndp
         assert len(data) == 1
         assert data[0]["spans.http"] == 100
         assert meta["dataset"] == self.dataset
+
+    def test_special_characters(self):
+        characters = "_.-"
+        span = self.create_span(
+            {"tags": {f"tag{c}": c for c in characters}},
+            start_ts=self.ten_mins_ago,
+        )
+        self.store_spans(
+            [
+                span,
+                self.create_span(start_ts=self.ten_mins_ago),
+            ],
+            is_eap=self.is_eap,
+        )
+
+        for c in characters:
+            response = self.do_request(
+                {
+                    "field": [f"tag{c}"],
+                    "query": f"tag{c}:{c}",
+                    "project": self.project.id,
+                    "dataset": self.dataset,
+                }
+            )
+            assert response.status_code == 200, response.content
+            assert response.data["data"] == [
+                {
+                    "id": span["span_id"],
+                    "project.name": self.project.slug,
+                    f"tag{c}": c,
+                }
+            ]
