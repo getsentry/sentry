7bc91d2234163b6d97e0ff425d896e39b3453788
TkDodo <dominik.dorfmeister@sentry.io>
2025-05-21 16:49:02 +0200

    chore: revert test

--- static/app/utils/dynamicSampling/features.tsx
+++ static/app/utils/dynamicSampling/features.tsx
@@ -2,7 +2,7 @@ import type {Organization} from 'sentry/types/organization';
 
 export function hasDynamicSamplingCustomFeature(organization: Organization) {
   return (
-    organization.features.includes('dynamic-samplin') &&
+    organization.features.includes('dynamic-sampling') &&
     organization.features.includes('dynamic-sampling-custom')
   );
 }
