d4394acd09f630e19d40ac7ebb24b6427d230328
Jonas <jonas.badalic@sentry.io>
2025-05-21 09:23:07 -0400

    tag: fix bad tag change and flickering text (#91927)
    Fixes a bad tag change where the text became bolded (I must have
    modified the wrong tag and not noticed it)
    
    Also removes a custom component for the tag pill (this must have been
    like this before), which makes this look better aligned and with a more
    appropriate font size
    
    Before
    ![CleanShot 2025-05-20 at 10 12
    07@2x](https://github.com/user-attachments/assets/6f27d0fc-aa32-4ba1-8248-601f6da9fa68)
    
    After
    ![CleanShot 2025-05-20 at 10 11
    55@2x](https://github.com/user-attachments/assets/dfbe3aec-10c9-4645-a633-654ee828f765)
    
    Also fixed this awkward flickering caused by the tooltip on the avatar
    (first interaction is fixed state, second one is from prod)
    
    
    https://github.com/user-attachments/assets/dd604408-3185-4243-9d0e-25e63326b9c2

--- static/app/components/core/badge/index.tsx
+++ static/app/components/core/badge/index.tsx
@@ -77,7 +77,7 @@ const StyledBadge = styled('span')<BadgeProps>`
   min-width: 20px;
   line-height: 20px;
   border-radius: 20px;
-  font-weight: ${p => p.theme.fontWeightBold};
+  font-weight: ${p => p.theme.fontWeightNormal};
 
   /* @TODO(jonasbadalic) can we standardize this transition? */
   transition: background 100ms linear;
