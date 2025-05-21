9ac4468beac69f96a693ff43f835c9ad1b5f81e9
Anton Pirker <anton.pirker@sentry.io>
2025-05-21 14:46:11 +0200

    one more comment

--- src/sentry/taskworker/workerchild.py
+++ src/sentry/taskworker/workerchild.py
@@ -291,6 +291,7 @@ def child_process(
                     origin="taskworker",
                 ) as root_span,
             ):
+                # TODO-anton: split dict into multiple attributes
                 root_span.set_attribute(
                     "taskworker-task", {"args": args, "kwargs": kwargs, "id": activation.id}
                 )
