6ece92d9e9048f0069e3f86e36bd892dea92b896
Katie Byers <lobsterkatie@gmail.com>
2025-05-20 22:21:14 -0700

    move main exception functions constant out of function

--- src/sentry/grouping/strategies/newstyle.py
+++ src/sentry/grouping/strategies/newstyle.py
@@ -861,10 +861,12 @@ def react_error_with_cause(exceptions: list[SingleException]) -> int | None:
     return main_exception_id
 
 
+MAIN_EXCEPTION_ID_FUNCS = [
+    react_error_with_cause,
+]
+
+
 def determine_main_exception_id(exceptions: list[SingleException]) -> int | None:
-    MAIN_EXCEPTION_ID_FUNCS = [
-        react_error_with_cause,
-    ]
     main_exception_id = None
     for func in MAIN_EXCEPTION_ID_FUNCS:
         main_exception_id = func(exceptions)
