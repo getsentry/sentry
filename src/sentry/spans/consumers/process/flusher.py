b8e7d608444643842007f1c5b89cc58f2cc5e9e0
Markus Unterwaditzer <markus-honeypot@unterwaditzer.net>
2025-05-22 14:00:51 +0200

    feat(span-buffer): Record segment sizes (#92100)

--- src/sentry/spans/consumers/process/flusher.py
+++ src/sentry/spans/consumers/process/flusher.py
@@ -144,6 +144,7 @@ class SpanFlusher(ProcessingStrategy[FilteredPayload | int]):
                         None, rapidjson.dumps({"spans": spans}).encode("utf8"), []
                     )
 
+                    metrics.timing("spans.buffer.segment_size_bytes", len(kafka_payload.value))
                     produce(kafka_payload)
 
                 for future in producer_futures:
