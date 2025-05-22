034d3413ab3e02945018a9a8f2fabeb2a0018fde
Kev <kevan.fisher@sentry.io>
2025-05-22 16:30:11 -0400

    Fix cursor conflict change

--- static/app/views/explore/contexts/logs/sortBys.tsx
+++ static/app/views/explore/contexts/logs/sortBys.tsx
@@ -3,6 +3,7 @@ import type {Location} from 'history';
 import {defined} from 'sentry/utils';
 import type {Sort} from 'sentry/utils/discover/fields';
 import {decodeSorts} from 'sentry/utils/queryString';
+import {LOGS_CURSOR_KEY} from 'sentry/views/explore/contexts/logs/logsPageParams';
 import {OurLogKnownFieldKey} from 'sentry/views/explore/logs/types';
 
 export const LOGS_SORT_BYS_KEY = 'logsSortBys';
@@ -44,11 +45,13 @@ export function updateLocationWithLogSortBys(
   sortBys: Sort[] | null | undefined
 ) {
   if (defined(sortBys)) {
-    const newSortBys = sortBys.map(sortBy =>
+    location.query[LOGS_SORT_BYS_KEY] = sortBys.map(sortBy =>
       sortBy.kind === 'desc' ? `-${sortBy.field}` : sortBy.field
     );
-    location.query[LOGS_SORT_BYS_KEY] = newSortBys;
-  } else {
+
+    // make sure to clear the cursor every time the query is updated
+    delete location.query[LOGS_CURSOR_KEY];
+  } else if (sortBys === null) {
     delete location.query[LOGS_SORT_BYS_KEY];
   }
 }
