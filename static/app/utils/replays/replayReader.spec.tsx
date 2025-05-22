getsentry/sentry

commit 592bf6e46597b95b981db72e5d17dbee210dea84

feat(replay): Filter out `style` mutations when extracting DOM nodes (#83016)
We have an org that has a small handful of replays where the
replayStepper
causes massive perf issues to the extent that it freezes the browser. I
narrowed it down to the `diff()` code inside of `rrdom` and a recent
upstream
PR (https://github.com/getsentry/rrweb/pull/233) seems to have
exacerbated the
problem. I have not been able to figure out the root cause for the perf
issues,
but it seems to be related to CSS and the mutations that add `style`
elements.
We will want to try to identify what exactly in these replays are
causing the
perf issues.

In the meantime we can filter out these mutations. Since we are only
interested
in generating and extracting the HTML for certain breadcrumb events, the
styles
should have no affect on the data we are interested in using.

Closes https://github.com/getsentry/sentry/issues/82221

static/app/utils/replays/replayReader.tsx
@@ -432,22 +432,26 @@ export default class ReplayReader {
     return this.processingErrors().length;
   };
 
-  getExtractDomNodes = memoize(async () => {
-    if (this._fetching) {
-      return null;
+  getExtractDomNodes = memoize(
+    async ({withoutStyles}: {withoutStyles?: boolean} = {}) => {
+      if (this._fetching) {
+        return null;
+      }
+      const {onVisitFrame, shouldVisitFrame} = extractDomNodes;
+
+      const results = await replayerStepper({
+        frames: this.getDOMFrames(),
+        rrwebEvents: withoutStyles
+          ? this.getRRWebFramesWithoutStyles()
+          : this.getRRWebFrames(),
+        startTimestampMs: this.getReplay().started_at.getTime() ?? 0,
+        onVisitFrame,
+        shouldVisitFrame,
+      });
+
+      return results;
     }
-    const {onVisitFrame, shouldVisitFrame} = extractDomNodes;
-
-    const results = await replayerStepper({
-      frames: this.getDOMFrames(),
-      rrwebEvents: this.getRRWebFrames(),
-      startTimestampMs: this.getReplay().started_at.getTime() ?? 0,
-      onVisitFrame,
-      shouldVisitFrame,
-    });
-
-    return results;
-  });
+  );
 
   getClipWindow = () => this._clipWindow;
 
@@ -532,6 +536,35 @@ export default class ReplayReader {
       }
     });
     return eventsWithSnapshots;
+  });
+
+  /**
+   * Filter out style mutations as they can cause perf problems especially when
+   * used in replayStepper
+   */
+  getRRWebFramesWithoutStyles = memoize(() => {
+    return this.getRRWebFrames().map(e => {
+      if (
+        e.type === EventType.IncrementalSnapshot &&
+        'source' in e.data &&
+        e.data.source === IncrementalSource.Mutation
+      ) {
+        return {
+          ...e,
+          data: {
+            ...e.data,
+            adds: e.data.adds.filter(
+              add =>
+                !(
+                  (add.node.type === 3 && add.node.isStyle) ||
+                  (add.node.type === 2 && add.node.tagName === 'style')
+                )
+            ),
+          },
+        };
+      }
+      return e;
+    });
   });
 
   getRRwebTouchEvents = memoize(() =>

static/app/utils/replays/hooks/useExtractDomNodes.tsx
@@ -10,7 +10,10 @@ export default function useExtractDomNodes({
 }): UseQueryResult<Map<ReplayFrame, Extraction>> {
   return useQuery({
     queryKey: ['getDomNodes', replay],
-    queryFn: () => replay?.getExtractDomNodes(),
+    // Note: we filter out `style` mutations due to perf issues.
+    // We can do this as long as we only need the HTML and not need to
+    // visualize the rendered elements
+    queryFn: () => replay?.getExtractDomNodes({withoutStyles: true}),
     enabled: Boolean(replay),
     gcTime: Infinity,
   });

commit 25c4b9903f38d3e4aedf21a2d5775016d22627bb

fix(replay): Fix `replayStepper` perf issues (#92167)
This PR fixes an perf regression from #83016 -- we were completely
removing these style elements from the tree which causes issues when
rebuilding the tree and ultimately we were hitting [this
timeout](https://github.com/getsentry/rrweb/blob/6b8c4596bfc89967f2b7279bd0c054a3d8694fa7/packages/rrweb/src/replay/index.ts#L1766)
too frequently, causing the browser to lockup.

static/app/utils/replays/replayReader.tsx
@@ -571,8 +571,8 @@ export default class ReplayReader {
   });
 
   /**
-   * Filter out style mutations as they can cause perf problems especially when
-   * used in replayStepper
+   * Do not include style mutation content as they can cause perf problems when
+   * used in replayStepper. However, we need to keep the nodes itself as to not affect the tree structure.
    */
   getRRWebFramesWithoutStyles = memoize(() => {
     return this.getRRWebFrames().map(e => {
@@ -581,17 +581,62 @@ export default class ReplayReader {
         'source' in e.data &&
         e.data.source === IncrementalSource.Mutation
       ) {
+        // Example `data` object:
+        // [{
+        //       "parentId": 4,
+        //       "nextId": 21,
+        //       "node": {
+        //           "type": 2,
+        //           "tagName": "style",
+        //           "attributes": {
+        //               "data-emotion": "css",
+        //               "data-s": "",
+        //               "_cssText": ".css {...} "
+        //           },
+        //           "childNodes": [],
+        //           "id": 47
+        //       }
+        //   },
+        //   {
+        //       "parentId": 414,
+        //       "nextId": null,
+        //       "node": {
+        //           "type": 3,
+        //           "textContent": ".css {...}",
+        //           "isStyle": true,
+        //           "id": 427
+        //       }
+        //   }
+        // ]
+
         return {
           ...e,
           data: {
             ...e.data,
-            adds: e.data.adds.filter(
-              add =>
-                !(
-                  (add.node.type === 3 && add.node.isStyle) ||
-                  (add.node.type === 2 && add.node.tagName === 'style')
-                )
-            ),
+            adds: e.data.adds.map(add => {
+              if (add.node.type === 3 && add.node.isStyle) {
+                return {
+                  ...add,
+                  node: {
+                    ...add.node,
+                    textContent: '',
+                  },
+                };
+              }
+
+              if (add.node.type === 2 && add.node.tagName === 'style') {
+                return {
+                  ...add,
+                  node: {
+                    ...add.node,
+                    attributes: {},
+                    childNodes: [],
+                  },
+                };
+              }
+
+              return add;
+            }),
           },
         };
       }

static/app/utils/replays/replayReader.spec.tsx
@@ -494,4 +494,115 @@ describe('ReplayReader', () => {
       ]);
     });
   });
+
+  describe('getRRWebFramesWithoutStyles', () => {
+    it('should remove style nodes and their content', () => {
+      const reader = ReplayReader.factory({
+        attachments: [
+          {
+            type: EventType.FullSnapshot,
+            data: {
+              node: {
+                type: 1,
+                tagName: 'html',
+                childNodes: [],
+              },
+            },
+            timestamp: 0,
+          },
+          {
+            type: EventType.IncrementalSnapshot,
+            data: {
+              source: IncrementalSource.Mutation,
+              adds: [
+                {
+                  parentId: 4,
+                  nextId: 21,
+                  node: {
+                    type: 2,
+                    tagName: 'style',
+                    attributes: {
+                      'data-emotion': 'css',
+                      'data-s': '',
+                      _cssText: '.css {...} ',
+                    },
+                    childNodes: [],
+                    id: 47,
+                  },
+                },
+                {
+                  parentId: 414,
+                  nextId: null,
+                  node: {
+                    type: 3,
+                    textContent: '.css {...}',
+                    isStyle: true,
+                    id: 427,
+                  },
+                },
+                {
+                  parentId: 5,
+                  nextId: 22,
+                  node: {
+                    type: 1,
+                    tagName: 'div',
+                    attributes: {
+                      class: 'test',
+                    },
+                    childNodes: [],
+                    id: 48,
+                  },
+                },
+              ],
+            },
+            timestamp: 0,
+          },
+        ],
+        errors: [],
+        fetching: false,
+        replayRecord: ReplayRecordFixture(),
+      });
+
+      if (!reader) {
+        throw new Error('Failed to create ReplayReader instance');
+      }
+
+      const result = reader.getRRWebFramesWithoutStyles();
+
+      expect(result).toEqual([
+        {data: {node: {childNodes: [], tagName: 'html', type: 1}}, timestamp: 0, type: 2},
+        {
+          data: {
+            adds: [
+              {
+                nextId: 21,
+                node: {attributes: {}, childNodes: [], id: 47, tagName: 'style', type: 2},
+                parentId: 4,
+              },
+              {
+                nextId: null,
+                node: {id: 427, isStyle: true, textContent: '', type: 3},
+                parentId: 414,
+              },
+              {
+                nextId: 22,
+                node: {
+                  attributes: {class: 'test'},
+                  childNodes: [],
+                  id: 48,
+                  tagName: 'div',
+                  type: 1,
+                },
+                parentId: 5,
+              },
+            ],
+            source: 0,
+          },
+          timestamp: 0,
+          type: 3,
+        },
+        {data: {payload: {}, tag: 'replay.end'}, timestamp: expect.any(Number), type: 5},
+      ]);
+    });
+  });
 });