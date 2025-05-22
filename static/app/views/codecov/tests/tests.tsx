c67373173ff4c140ef08760a6d22ae3cb674bd59
Adrian <adrian.bonilla@sentry.io>
2025-05-22 13:22:10 -0700

    feat: adjust codecov time selector to use codecov context

--- static/app/views/codecov/tests/tests.tsx
+++ static/app/views/codecov/tests/tests.tsx
@@ -1,29 +1,20 @@
 import styled from '@emotion/styled';
 
+import CodecovQueryParamsProvider from 'sentry/components/codecov/container/codecovParamsProvider';
 import {DatePicker} from 'sentry/components/codecov/datePicker/datePicker';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
-import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import {space} from 'sentry/styles/space';
 import {Summaries} from 'sentry/views/codecov/tests/summaries/summaries';
-
-const DEFAULT_CODECOV_DATETIME_SELECTION = {
-  start: null,
-  end: null,
-  utc: false,
-  period: '24h',
-};
 
 export default function TestsPage() {
   return (
     <LayoutGap>
       <p>Test Analytics</p>
-      <PageFiltersContainer
-        defaultSelection={{datetime: DEFAULT_CODECOV_DATETIME_SELECTION}}
-      >
+      <CodecovQueryParamsProvider>
         <PageFilterBar condensed>
           <DatePicker />
         </PageFilterBar>
-      </PageFiltersContainer>
+      </CodecovQueryParamsProvider>
       {/* TODO: Conditionally show these if the branch we're in is the main branch */}
       <Summaries />
     </LayoutGap>
