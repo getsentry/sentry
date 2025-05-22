30593e142db014d032a39280c2825008de3531f8
Fabian Schindler <fabian.schindler@sentry.io>
2025-05-22 16:50:52 +0200

    feat(demo-mode): add middleware that clears the activeorg when it is a demo org and the URI is home

---
+++ src/sentry/middleware/demo_mode_guard.py
@@ -0,0 +1,35 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+
+from django.http.request import HttpRequest
+from django.http.response import HttpResponseBase
+
+from sentry import options
+from sentry.demo_mode.utils import is_demo_mode_enabled, is_demo_org
+from sentry.organizations.services.organization import organization_service
+
+
+def _get_org(slug):
+    if not slug:
+        return None
+    return organization_service.get_org_by_slug(slug=slug)
+
+
+class DemoModeGuardMiddleware:
+    def __init__(self, get_response: Callable[[HttpRequest], HttpResponseBase]) -> None:
+        self.get_response = get_response
+
+    def __call__(self, request: HttpRequest) -> HttpResponseBase:
+        # only in "sentry.io/"
+        if (
+            not request.subdomain  # TODO: what about region subdomains?
+            and request.path in ("", "/")
+            and is_demo_mode_enabled()
+            and options.get("demo-mode.disable-sandbox-redirect")
+        ):
+            session = getattr(request, "session", None)
+            if session and is_demo_org(_get_org(session.get("activeorg"))):
+                del session["activeorg"]
+
+        return self.get_response(request)
