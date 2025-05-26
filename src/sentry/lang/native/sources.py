32e82c80a4b47a739d92dbb89f6b5e66b7a5c4af
Alexander Tarasov <alex.tarasov@sentry.io>
2025-05-26 17:56:03 +0200

    fix(symbolicator): copy source to avoid modification of settings

--- src/sentry/lang/native/sources.py
+++ src/sentry/lang/native/sources.py
@@ -584,6 +584,8 @@ def get_sources_for_project(project):
                 # if target_credentials.token is None it means that the
                 # token could not be fetched successfully
                 if token is not None:
+                    # Create a new dict to avoid reference issues
+                    source = source.copy()
                     source["bearer_token"] = token
 
                     # Remove other credentials if we have a token
