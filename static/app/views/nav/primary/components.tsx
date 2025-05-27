4663ea9664aee0ff99db7c46c7c9d6ab67ffdd86
JonasBa <jonas@badalic.com>
2025-05-27 15:16:35 -0400

    sidebar: update chonk colors

--- static/app/views/nav/primary/components.tsx
+++ static/app/views/nav/primary/components.tsx
@@ -399,7 +399,7 @@ const ChonkNavLink = chonkStyled(Link, {
     width: 4px;
     height: 20px;
     border-radius: ${p => p.theme.radius.micro};
-    background-color: ${p => p.theme.colors.blue400};
+    background-color: ${p => p.theme.tokens.graphics.accent};
     transition: opacity 0.1s ease-in-out;
     opacity: 0;
   }
@@ -421,7 +421,7 @@ const ChonkNavLink = chonkStyled(Link, {
   }
 
   &[aria-selected='true'] {
-    color: ${p => p.theme.purple400};
+    color: ${p => p.theme.tokens.content.accent};
 
     &::before { opacity: 1; }
     ${NavLinkIconContainer} {
