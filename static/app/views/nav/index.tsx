6e7769edc6262b5087eb96e4fbcfce553adc0b6f
Malachi Willey <malwilley@gmail.com>
2025-05-27 09:45:27 -0700

    ref(nav): Remove secondary nav portal (#92165)
    We used to have a portal here so that the secondary nav components could
    be rendered anywhere in the hierarchy, but that feature is no longer
    necessary so I'm removing it and just rendering in separately in each
    location.

--- static/app/views/nav/index.tsx
+++ static/app/views/nav/index.tsx
@@ -4,21 +4,17 @@ import styled from '@emotion/styled';
 
 import {useNavContext} from 'sentry/views/nav/context';
 import MobileTopbar from 'sentry/views/nav/mobileTopbar';
-import {SecondaryNav} from 'sentry/views/nav/secondary/secondary';
-import {SecondaryNavContent} from 'sentry/views/nav/secondary/secondaryNavContent';
 import {Sidebar} from 'sentry/views/nav/sidebar';
 import {
   NavigationTourProvider,
   useStackedNavigationTour,
 } from 'sentry/views/nav/tour/tour';
 import {NavLayout} from 'sentry/views/nav/types';
-import {useActiveNavGroup} from 'sentry/views/nav/useActiveNavGroup';
 
 function NavContent() {
   const {layout, navParentRef} = useNavContext();
   const {currentStepId, endTour} = useStackedNavigationTour();
   const tourIsActive = currentStepId !== null;
-  const activeNavGroup = useActiveNavGroup();
 
   // The tour only works with the sidebar layout, so if we change to the mobile
   // layout in the middle of the tour, it needs to end.
@@ -35,9 +31,6 @@ function NavContent() {
       isMobile={layout === NavLayout.MOBILE}
     >
       {layout === NavLayout.SIDEBAR ? <Sidebar /> : <MobileTopbar />}
-      <SecondaryNav>
-        <SecondaryNavContent group={activeNavGroup} />
-      </SecondaryNav>
     </NavContainer>
   );
 }
