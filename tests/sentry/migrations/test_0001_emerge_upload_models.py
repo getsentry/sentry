59ab2c70b70839f3c4de61ec28ff569a8be3b3f5
Trevor Elkins <trevor@emergetools.com>
2025-05-27 16:56:27 -0400

    Add migration test

---
+++ tests/sentry/migrations/test_0001_emerge_upload_models.py
@@ -0,0 +1,17 @@
+from sentry.testutils.cases import TestMigrations
+
+
+class AddPreprodModelsTests(TestMigrations):
+    app = "preprod"
+    migrate_from = "0001_emerge_upload_models"
+    migrate_to = "0001_emerge_upload_models"
+
+    def setup_initial_state(self):
+        pass
+
+    def test(self):
+        from sentry.preprod.models import PreprodArtifact
+
+        org = self.create_organization()
+        project = self.create_project(organization=org)
+        PreprodArtifact.objects.create(project=project)
