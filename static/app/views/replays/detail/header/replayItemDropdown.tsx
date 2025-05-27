146126cadd9d77bd94add1840f4bc743617ccbf4
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-05-27 16:11:48 -0700

    refactor existing menus, and wire up downloadObjectAsJson() to download json

---
+++ static/app/views/replays/detail/header/replayItemDropdown.tsx
@@ -0,0 +1,135 @@
+import type {ReactNode} from 'react';
+import styled from '@emotion/styled';
+
+import {addErrorMessage} from 'sentry/actionCreators/indicator';
+import type {MenuItemProps} from 'sentry/components/dropdownMenu';
+import {DropdownMenu} from 'sentry/components/dropdownMenu';
+import {IconDelete, IconDownload, IconEllipsis, IconUpload} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {defined} from 'sentry/utils';
+import {downloadObjectAsJson} from 'sentry/utils/downloadObjectAsJson';
+import {isActiveSuperuser} from 'sentry/utils/isActiveSuperuser';
+import useDeleteReplay from 'sentry/utils/replays/hooks/useDeleteReplay';
+import useShareReplayAtTimestamp from 'sentry/utils/replays/hooks/useShareReplayAtTimestamp';
+import type ReplayReader from 'sentry/utils/replays/replayReader';
+import {useIsSentryEmployee} from 'sentry/utils/useIsSentryEmployee';
+import {useNavigate} from 'sentry/utils/useNavigate';
+import useOrganization from 'sentry/utils/useOrganization';
+import type {ReplayRecord} from 'sentry/views/replays/types';
+
+interface Props {
+  projectSlug: string | null;
+  replay: ReplayReader | undefined;
+  // Accept the replay and replayRecord in case the replay doesn't load properly,
+  // we still want to be able to sent the Delete request.
+  replayRecord: ReplayRecord | undefined;
+}
+
+export default function ReplayItemDropdown({projectSlug, replay, replayRecord}: Props) {
+  const navigate = useNavigate();
+  const organization = useOrganization();
+  const isEmployee = useIsSentryEmployee();
+  const isSuperUser = isActiveSuperuser();
+
+  const replayId = replayRecord?.id;
+  const isMobile = replay?.isVideoReplay();
+
+  const canSeeEmployeeLinks = isEmployee || isSuperUser;
+  const canDownload = projectSlug && replay;
+
+  const onShareReplay = useShareReplayAtTimestamp();
+
+  const canDelete = replayId && projectSlug;
+  const onDeleteReplay = useDeleteReplay({replayId, projectSlug});
+
+  const dropdownItems: MenuItemProps[] = [
+    makeItem({
+      key: 'download-rrweb',
+      label: t('Download JSON'),
+      icon: <IconDownload />,
+      onAction: () => {
+        try {
+          if (!replay) {
+            addErrorMessage(t('Replay not found'));
+            return;
+          }
+          downloadObjectAsJson(replay.getRRWebFrames(), 'rrweb');
+        } catch (error) {
+          addErrorMessage(
+            'Could not export replay as rrweb data. Please wait or try again'
+          );
+        }
+      },
+      disabled: !canDownload,
+    }),
+    canSeeEmployeeLinks && isMobile
+      ? makeItem({
+          key: 'download-1st-video',
+          label: t('Download 1st video segment (employee only)'),
+          icon: <IconDownload />,
+          onAction: () =>
+            navigate(
+              `/api/0/projects/${organization.slug}/${projectSlug}/replays/${replayId}/videos/0/`
+            ),
+          disabled: !canDownload,
+        })
+      : null,
+    makeItem({
+      key: 'share',
+      label: t('Share'),
+      icon: <IconUpload />,
+      onAction: onShareReplay,
+      disabled: !replayId,
+    }),
+    makeItem({
+      key: 'delete',
+      label: t('Delete'),
+      icon: <IconDelete />,
+      onAction: onDeleteReplay,
+      disabled: !canDelete,
+    }),
+  ].filter(defined);
+
+  return (
+    <DropdownMenu
+      position="bottom-end"
+      triggerProps={{
+        showChevron: false,
+        icon: <IconEllipsis color="subText" />,
+      }}
+      size="sm"
+      items={dropdownItems}
+      isDisabled={dropdownItems.length === 0}
+    />
+  );
+}
+
+function makeItem({
+  key,
+  label,
+  onAction,
+  disabled,
+  icon,
+}: Pick<MenuItemProps, 'key' | 'onAction' | 'disabled'> & {
+  icon: ReactNode;
+  label: string;
+}): MenuItemProps {
+  return {
+    key,
+    label: (
+      <ItemSpacer>
+        {icon}
+        {label}
+      </ItemSpacer>
+    ),
+    onAction,
+    disabled,
+  };
+}
+
+const ItemSpacer = styled('div')`
+  display: flex;
+  gap: ${space(1)};
+  align-items: center;
+`;
