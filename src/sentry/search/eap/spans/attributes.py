fef168691f140e7f9d637a8456399818738a1bf8
Pierre Massat <pierre.massat@sentry.io>
2025-05-28 16:43:16 -0400

    Remove validator for span_id

--- src/sentry/search/eap/spans/attributes.py
+++ src/sentry/search/eap/spans/attributes.py
@@ -365,7 +365,6 @@ SPAN_ATTRIBUTE_DEFINITIONS = {
             public_alias="span_id",
             internal_name="sentry.item_id",
             search_type="string",
-            validator=is_span_id,
         ),
         simple_sentry_field("trace.status"),
         simple_sentry_field("transaction.method"),
