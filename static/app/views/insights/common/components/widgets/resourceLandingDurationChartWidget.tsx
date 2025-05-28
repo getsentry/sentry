81a5b841e44d4f67f917a7e3f47a095753a7ea0a
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2025-05-28 11:55:25 -0400

    ref(insights): pass in `search` to all asset module charts (#92337)
    Pass in the `search` query into `InsightsLineChartWidget` so that when
    you `open in explore` or `create alert` the query is correctly
    pre-populated when you navigate to it.

--- static/app/views/insights/common/components/widgets/resourceLandingDurationChartWidget.tsx
+++ static/app/views/insights/common/components/widgets/resourceLandingDurationChartWidget.tsx
@@ -1,5 +1,8 @@
 import {InsightsLineChartWidget} from 'sentry/views/insights/common/components/insightsLineChartWidget';
-import {useResourceLandingSeries} from 'sentry/views/insights/common/components/widgets/hooks/useResourceLandingSeries';
+import {
+  useResourceLandingSeries,
+  useResourceLandingSeriesSearch,
+} from 'sentry/views/insights/common/components/widgets/hooks/useResourceLandingSeries';
 import type {LoadableChartWidgetProps} from 'sentry/views/insights/common/components/widgets/types';
 import {getDurationChartTitle} from 'sentry/views/insights/common/views/spans/types';
 import {SpanMetricsField} from 'sentry/views/insights/types';
@@ -9,13 +12,17 @@ const {SPAN_SELF_TIME} = SpanMetricsField;
 export default function ResourceLandingDurationChartWidget(
   props: LoadableChartWidgetProps
 ) {
+  const {search, enabled} = useResourceLandingSeriesSearch();
   const {data, isPending, error} = useResourceLandingSeries({
+    search,
+    enabled,
     pageFilters: props.pageFilters,
   });
 
   return (
     <InsightsLineChartWidget
       {...props}
+      search={search}
       id="resourceLandingDurationChartWidget"
       title={getDurationChartTitle('resource')}
       series={[data[`avg(${SPAN_SELF_TIME})`]]}
