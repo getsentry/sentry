1e650f7a2b0b6f3d78469c0c2be2e4c0290b5aea
nikkikapadia <nikki.kapadia@sentry.io>
2025-05-28 14:59:32 -0400

    dashboards -> EAP comparison script

---
+++ src/sentry/discover/compare_tables.py
@@ -0,0 +1,192 @@
+import logging
+from enum import Enum
+from typing import TypedDict, cast
+
+import sentry_sdk
+
+from sentry.discover.arithmetic import is_equation
+from sentry.discover.dashboard_widget_split import _get_snuba_dataclass_for_dashboard_widget
+from sentry.discover.dataset_split import _get_equation_list, _get_field_list
+from sentry.discover.translation.mep_to_eap import (
+    QueryParts,
+    translate_columns,
+    translate_mep_to_eap,
+)
+from sentry.models.dashboard import Dashboard
+from sentry.models.dashboard_widget import DashboardWidget, DashboardWidgetQuery
+from sentry.models.organization import Organization
+from sentry.models.project import Project
+from sentry.search.eap.types import EAPResponse, SearchResolverConfig
+from sentry.search.events.types import SAMPLING_MODES, EventsResponse, SnubaParams
+from sentry.snuba import spans_rpc
+from sentry.snuba.metrics_performance import query as metrics_query
+
+logger = logging.getLogger(__name__)
+
+
+class CompareTableResult(Enum):
+    BOTH_FAILED = "both_requests_failed"
+    FIELD_NOT_FOUND = "field_not_found"
+    METRICS_FAILED = "metrics_failed"
+    NO_FIELDS = "no_fields"
+    NO_PROJECT = "no_project"
+    PASSED = "passed"
+    SNQL_EAP_FAILED = "snql_eap_failed"
+
+
+class CompareTableResultDict(TypedDict):
+    passed: bool
+    reason: CompareTableResult
+    fields: list[str] | None
+    widget_query: DashboardWidgetQuery
+    mismatches: list[str] | None
+
+
+def compare_table_results(metrics_query_result: EventsResponse, snql_eap_result: EAPResponse):
+    # print("Comparing table results")
+    # print("Metrics query result: %s", metrics_query_result)
+    # print("EAP query result: %s", snql_eap_result)
+    eap_data_row = snql_eap_result["data"][0] if len(snql_eap_result["data"]) > 0 else {}
+    metrics_data_row = (
+        metrics_query_result["data"][0] if len(metrics_query_result["data"]) > 0 else {}
+    )
+    mismatches = []
+    for field, data in metrics_data_row.items():
+        if is_equation(field):
+            continue
+        translated_field, *rest = translate_columns([field])
+        if data is not None and eap_data_row[translated_field] is None:
+            logger.info("Field %s not found in EAP response", field)
+            sentry_sdk.set_tag("dashboard_comparison_mismatch_field", field)
+            mismatches.append(field)
+
+    return (len(mismatches) == 0, mismatches)
+
+
+def compare_tables_for_dashboard_widget_queries(
+    widget_query: DashboardWidgetQuery,
+) -> CompareTableResultDict:
+    widget: DashboardWidget = widget_query.widget
+    dashboard: Dashboard = widget.dashboard
+    organization: Organization = dashboard.organization
+    projects: list[Project] = dashboard.projects.all()
+    project = projects.first()
+    if len(list(projects)) == 0:
+        return {
+            "passed": False,
+            "reason": CompareTableResult.NO_PROJECT,
+            "fields": None,
+            "widget_query": widget_query,
+        }
+
+    fields = widget_query.fields
+    if len(fields) == 0:
+        return {
+            "passed": False,
+            "reason": CompareTableResult.NO_FIELDS,
+            "fields": None,
+            "widget_query": widget_query,
+        }
+
+    snuba_dataclass = _get_snuba_dataclass_for_dashboard_widget(widget, list(projects))
+    selected_columns = _get_field_list(fields)
+    equations = [equation for equation in _get_equation_list(widget_query.fields or []) if equation]
+    query = widget_query.conditions
+
+    has_metrics_error = False
+    has_snql_eap_error = False
+
+    try:
+        metrics_query_result = metrics_query(
+            selected_columns,
+            query,
+            snuba_dataclass,
+            equations,
+            orderby=None,
+            offset=None,
+            limit=1,
+            referrer="dashboards.transactions_spans_comparison",
+            transform_alias_to_input_format=True,
+        )
+
+    except Exception as e:
+        # print(e)
+        logger.info("Metrics query failed: %s", e)
+        has_metrics_error = True
+
+    environments = dashboard.filters.get("environment", [])
+
+    snuba_params = SnubaParams(
+        environments=environments,
+        projects=[project],
+        organization=organization,
+        stats_period="7d",
+    )
+
+    eap_query_parts = translate_mep_to_eap(
+        QueryParts(
+            query=query,
+            selected_columns=selected_columns,
+            equations=equations,
+            orderby=None,
+        )
+    )
+
+    try:
+        snql_eap_result = spans_rpc.run_table_query(
+            params=snuba_params,
+            query_string=eap_query_parts["query"],
+            selected_columns=eap_query_parts["selected_columns"],
+            orderby=eap_query_parts["orderby"],
+            offset=None,
+            limit=1,
+            referrer="dashboards.transactions_spans_comparison",
+            config=SearchResolverConfig(),
+            sampling_mode=cast(SAMPLING_MODES, "NORMAL"),
+        )
+    except Exception as e:
+        # print(e)
+        logger.info("EAP query failed: %s", e)
+        has_snql_eap_error = True
+
+    if has_metrics_error and has_snql_eap_error:
+        return {
+            "passed": False,
+            "reason": CompareTableResult.BOTH_FAILED,
+            "fields": fields,
+            "widget_query": widget_query,
+        }
+    elif has_metrics_error:
+        return {
+            "passed": False,
+            "reason": CompareTableResult.METRICS_FAILED,
+            "fields": fields,
+            "widget_query": widget_query,
+        }
+    elif has_snql_eap_error:
+        return {
+            "passed": False,
+            "reason": CompareTableResult.SNQL_EAP_FAILED,
+            "fields": fields,
+            "widget_query": widget_query,
+        }
+    else:
+        passed, mismatches = compare_table_results(metrics_query_result, snql_eap_result)
+        if passed:
+            sentry_sdk.set_tag("dashboard_comparison_passed", True)
+            return {
+                "passed": True,
+                "reason": CompareTableResult.PASSED,
+                "fields": fields,
+                "widget_query": widget_query,
+                "mismatches": mismatches,
+            }
+        else:
+            sentry_sdk.set_tag("dashboard_comparison_passed", False)
+            return {
+                "passed": False,
+                "reason": CompareTableResult.FIELD_NOT_FOUND,
+                "fields": fields,
+                "widget_query": widget_query,
+                "mismatches": mismatches,
+            }
---
+++ tests/sentry/discover/test_compare_tables.py
@@ -0,0 +1,217 @@
+from datetime import UTC, datetime, timedelta
+from uuid import uuid4
+
+import pytest
+
+from sentry.discover.compare_tables import (
+    CompareTableResult,
+    compare_tables_for_dashboard_widget_queries,
+)
+from sentry.models.dashboard_widget import (
+    DashboardWidget,
+    DashboardWidgetDisplayTypes,
+    DashboardWidgetQuery,
+    DashboardWidgetTypes,
+)
+from sentry.snuba.metrics.naming_layer.mri import TransactionMRI
+from sentry.testutils.cases import BaseMetricsLayerTestCase, BaseSpansTestCase, TestCase
+from sentry.utils.samples import load_data
+
+pytestmark = pytest.mark.sentry_metrics
+
+
+class CompareTablesTestCase(BaseMetricsLayerTestCase, TestCase, BaseSpansTestCase):
+    @property
+    def now(self):
+        return datetime.now(UTC).replace(microsecond=0)
+
+    def setUp(self):
+        super().setUp()
+        self.organization = self.create_organization()
+        self.project = self.create_project(organization=self.organization)
+        self.dashboard = self.create_dashboard(
+            organization=self.organization, filters={"environment": []}
+        )
+        self.dashboard.projects.set([self.project])
+
+        self.successful_widget = DashboardWidget.objects.create(
+            dashboard=self.dashboard,
+            title="Test Successful Widget",
+            order=0,
+            display_type=DashboardWidgetDisplayTypes.TABLE,
+            widget_type=DashboardWidgetTypes.TRANSACTION_LIKE,
+        )
+        self.successful_widget_query = DashboardWidgetQuery.objects.create(
+            widget=self.successful_widget,
+            name="Test Successful Widget Query",
+            order=0,
+            conditions="",
+            aggregates=["count()"],
+            columns=["count()", "transaction"],
+            fields=["count()", "transaction"],
+        )
+
+        self.empty_field_widget = DashboardWidget.objects.create(
+            dashboard=self.dashboard,
+            title="Test Empty Field Widget",
+            order=1,
+            display_type=DashboardWidgetDisplayTypes.TABLE,
+            widget_type=DashboardWidgetTypes.TRANSACTION_LIKE,
+        )
+
+        self.empty_field_widget_query = DashboardWidgetQuery.objects.create(
+            widget=self.empty_field_widget,
+            name="Test Empty Field Widget Query",
+            order=1,
+            conditions="",
+            aggregates=["apdex()"],
+            columns=["apdex()", "http.status_code"],
+            fields=["apdex()", "http.status_code"],
+        )
+
+        self.empty_field_widget_2 = DashboardWidget.objects.create(
+            dashboard=self.dashboard,
+            title="Test Empty Field Widget",
+            order=2,
+            display_type=DashboardWidgetDisplayTypes.TABLE,
+            widget_type=DashboardWidgetTypes.TRANSACTION_LIKE,
+        )
+
+        self.empty_field_widget_query_2 = DashboardWidgetQuery.objects.create(
+            widget=self.empty_field_widget_2,
+            name="Test Empty Field Widget Query",
+            order=2,
+            conditions="",
+            aggregates=["failure_rate()", "count()"],
+            columns=["failure_rate()", "count()", "http.status_code"],
+            fields=["failure_rate()", "count()", "http.status_code"],
+        )
+
+        self.double_write_segment(
+            project=self.project,
+            trace_id=uuid4().hex,
+            transaction_id=uuid4().hex,
+            span_id="1" + uuid4().hex[:15],
+            transaction="foo",
+            duration=50,
+            exclusive_time=50,
+            days_before_now=1,
+        )
+
+        self.double_write_segment(
+            project=self.project,
+            trace_id=uuid4().hex,
+            transaction_id=uuid4().hex,
+            span_id="1" + uuid4().hex[:15],
+            transaction="bar",
+            duration=100,
+            exclusive_time=100,
+            days_before_now=2,
+        )
+
+        self.double_write_segment(
+            project=self.project,
+            trace_id=uuid4().hex,
+            transaction_id=uuid4().hex,
+            span_id="1" + uuid4().hex[:15],
+            transaction="http",
+            duration=100,
+            exclusive_time=100,
+            days_before_now=2,
+            tags={"sentry.http.status_code": "200"},
+        )
+
+    def double_write_segment(
+        self,
+        *,
+        project,
+        trace_id,
+        transaction_id,
+        span_id,
+        duration,
+        days_before_now: int = 0,
+        hours_before_now: int = 0,
+        minutes_before_now: int = 0,
+        seconds_before_now: int = 0,
+        **kwargs,
+    ):
+        kwargs.setdefault("measurements", {})
+        if "lcp" not in kwargs["measurements"]:
+            kwargs["measurements"]["lcp"] = duration
+        if "client_sample_rate" not in kwargs["measurements"]:
+            kwargs["measurements"]["client_sample_rate"] = 0.1
+
+        timestamp = self.adjust_timestamp(
+            self.now
+            - timedelta(
+                days=days_before_now,
+                hours=hours_before_now,
+                minutes=minutes_before_now,
+                seconds=seconds_before_now,
+            )
+        )
+        end_timestamp = timestamp + timedelta(microseconds=duration * 1000)
+
+        data = load_data(
+            "transaction",
+            start_timestamp=timestamp,
+            timestamp=end_timestamp,
+            trace=trace_id,
+            span_id=span_id,
+            spans=[],
+            event_id=transaction_id,
+        )
+
+        for measurement, value in kwargs.get("measurements", {}).items():
+            data["measurements"][measurement] = {"value": value}
+
+        if tags := kwargs.get("tags", {}):
+            data["tags"] = [[key, val] for key, val in tags.items()]
+
+        self.store_segment(
+            project_id=project.id,
+            trace_id=trace_id,
+            transaction_id=transaction_id,
+            span_id=span_id,
+            timestamp=timestamp,
+            duration=duration,
+            organization_id=project.organization.id,
+            is_eap=True,
+            **kwargs,
+        )
+
+        self.store_performance_metric(
+            name=TransactionMRI.DURATION.value,
+            tags={"transaction.status": "unknown"},
+            project_id=project.id,
+            org_id=project.organization.id,
+            value=duration,
+            days_before_now=days_before_now,
+            hours_before_now=hours_before_now,
+            minutes_before_now=minutes_before_now,
+            seconds_before_now=seconds_before_now,
+        )
+
+    def test_compare_successful_tables(self):
+        comparison_result = compare_tables_for_dashboard_widget_queries(
+            self.successful_widget_query
+        )
+        assert comparison_result["passed"]
+        assert comparison_result["mismatches"] == []
+
+    def test_compare_empty_field_tables(self):
+        # testing with apdex() field, which is not supported in EAP
+        comparison_result = compare_tables_for_dashboard_widget_queries(
+            self.empty_field_widget_query
+        )
+        assert comparison_result["passed"] is False
+        assert comparison_result["reason"] == CompareTableResult.SNQL_EAP_FAILED
+
+    def test_compare_empty_field_tables_2(self):
+        # testing with failure_rate() field, which is not supported in EAP
+        comparison_result = compare_tables_for_dashboard_widget_queries(
+            self.empty_field_widget_query_2
+        )
+        assert comparison_result["passed"] is False
+        assert comparison_result["reason"] == CompareTableResult.FIELD_NOT_FOUND
+        assert comparison_result["mismatches"] == ["failure_rate()"]
