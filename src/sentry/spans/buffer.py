e80062ce38d63f53980cb42ba26997130df436c4
Jan Michael Auer <mail@jauer.org>
2025-05-28 12:57:57 +0200

    fix: Typing

--- src/sentry/spans/buffer.py
+++ src/sentry/spans/buffer.py
@@ -194,8 +194,9 @@ class SpansBuffer:
             with self.client.pipeline(transaction=False) as p:
                 for (project_and_trace, parent_span_id), subsegment in trees.items():
                     set_key = f"span-buf:s:{{{project_and_trace}}}:{parent_span_id}"
-                    mapping = {span.payload: span.end_timestamp_precise for span in subsegment}
-                    p.zadd(set_key, mapping)
+                    p.zadd(
+                        set_key, {span.payload: span.end_timestamp_precise for span in subsegment}
+                    )
 
                 p.execute()
 
