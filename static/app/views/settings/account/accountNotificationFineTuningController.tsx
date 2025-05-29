7316d7240742071f098ec69e0d10da5daac3b247
Billy Vong <billyvg@users.noreply.github.com>
2025-05-29 14:23:11 -0400

    fix(settings): Fix loading settings -> account -> notifications -> fine tuning (#92486)
    Loading the Fine Tuning views can be problematic when these conditions
    occur:
    
    * fresh page load (i.e. no organization in context)
    * no org in subdomain
    * no organization id URL query parameter
    * user is a member of multiple organizations
    
    When this happens, we do not know what organization to use and so the
    previous logic selected the first alphabetical organization. Depending
    on your org membership, this could leave you in a broken state (e.g. if
    the org is disabled or over the member limit).
    
    This PR:
    
    - removes the assumption of using the first alphabetical org (when you
    have more than 1 org) and forces the user to select an org
    - pulls up `<NotificationSettingsByType>` into
    `<AccountNotificationFineTuningController>` which saves a bunch of
    un-necessary API requests
    
    Test by visiting preview deploy
    (https://sentry-g53mk4t1m.sentry.dev/settings/account/notifications/)
    and clicking into each type, there are at least 3 different branches:
    
    
    | link | entityType | component |
    | ---- | ---------- | --------- |
    |
    https://sentry-g53mk4t1m.sentry.dev/settings/account/notifications/alerts/
    | `project` | `NotificationSettingsByEntity` |
    |
    https://sentry-g53mk4t1m.sentry.dev/settings/account/notifications/reports/
    | `organization` | `NotificationSettingsByEntity` |
    |
    https://sentry-g53mk4t1m.sentry.dev/settings/account/notifications/email/
    | N/A | `AccountNotificationFineTuning` |
    
    Fixes JAVASCRIPT-300C

--- static/app/views/settings/account/accountNotificationFineTuningController.tsx
+++ static/app/views/settings/account/accountNotificationFineTuningController.tsx
@@ -1,7 +1,10 @@
 import LoadingIndicator from 'sentry/components/loadingIndicator';
 import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import type {Organization} from 'sentry/types/organization';
+import {useParams} from 'sentry/utils/useParams';
 import withOrganizations from 'sentry/utils/withOrganizations';
+import {NotificationSettingsByType} from 'sentry/views/settings/account/notifications/notificationSettingsByType';
+import {getNotificationTypeFromPathname} from 'sentry/views/settings/account/notifications/utils';
 
 import AccountNotificationFineTuning from './accountNotificationFineTuning';
 
@@ -11,12 +14,31 @@ interface AccountNotificationFineTuningControllerProps
   organizationsLoading?: boolean;
 }
 
+const accountNotifications = [
+  'alerts',
+  'deploy',
+  'workflow',
+  'approval',
+  'quota',
+  'spikeProtection',
+  'reports',
+  'brokenMonitors',
+];
+
 function AccountNotificationFineTuningController({
   organizationsLoading,
   ...props
 }: AccountNotificationFineTuningControllerProps) {
+  const params = useParams<{fineTuneType: string}>();
+  const {fineTuneType: pathnameType} = params;
+  const fineTuneType = getNotificationTypeFromPathname(pathnameType);
+
   if (organizationsLoading) {
     return <LoadingIndicator />;
+  }
+
+  if (accountNotifications.includes(fineTuneType)) {
+    return <NotificationSettingsByType notificationType={fineTuneType} />;
   }
 
   return <AccountNotificationFineTuning {...props} />;
