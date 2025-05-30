a2b12957fb779da1d8bc4af885cd74fe1fb51f26
Christinarlong <christina.long@sentry.io>
2025-05-30 08:52:50 -0700

    add org slug to the frontnend props

--- tests/sentry/integrations/github/test_integration.py
+++ tests/sentry/integrations/github/test_integration.py
@@ -1316,7 +1316,11 @@ class GitHubIntegrationTest(IntegrationTestCase):
         mock_render.assert_called_with(
             request=ANY,
             pipeline_name="githubInstallationSelect",
-            props={"installation_info": installations, "has_scm_multi_org": True},
+            props={
+                "installation_info": installations,
+                "has_scm_multi_org": True,
+                "organization_slug": "foo",
+            },
         )
 
         # SLO assertions
@@ -1524,7 +1528,11 @@ class GitHubIntegrationTest(IntegrationTestCase):
         mock_render.assert_called_with(
             request=ANY,
             pipeline_name="githubInstallationSelect",
-            props={"installation_info": installations, "has_scm_multi_org": False},
+            props={
+                "installation_info": installations,
+                "has_scm_multi_org": False,
+                "organization_slug": "foo",
+            },
         )
 
         # SLO assertions
@@ -1575,7 +1583,11 @@ class GitHubIntegrationTest(IntegrationTestCase):
         mock_render.assert_called_with(
             request=ANY,
             pipeline_name="githubInstallationSelect",
-            props={"installation_info": installations, "has_scm_multi_org": False},
+            props={
+                "installation_info": installations,
+                "has_scm_multi_org": False,
+                "organization_slug": "foo",
+            },
         )
 
         # We rendered the GithubOrganizationSelection UI and the user chose to skip
