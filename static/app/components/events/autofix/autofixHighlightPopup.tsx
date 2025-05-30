24a7e9b9244760fa10faecb4372cf7ab51955ef0
Jenn Mueng <30991498+jennmueng@users.noreply.github.com>
2025-05-30 21:25:37 +0700

    feat(autofix): Add key analytics events (#92533)
    Adds analytics items as discussed in:
    https://linear.app/getsentry/issue/AIML-242/autofix-add-analytics-events

--- static/app/components/events/autofix/autofixHighlightPopup.tsx
+++ static/app/components/events/autofix/autofixHighlightPopup.tsx
@@ -25,6 +25,7 @@ import LoadingIndicator from 'sentry/components/loadingIndicator';
 import {IconClose} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
+import {trackAnalytics} from 'sentry/utils/analytics';
 import testableTransition from 'sentry/utils/testableTransition';
 import useApi from 'sentry/utils/useApi';
 import useMedia from 'sentry/utils/useMedia';
@@ -140,6 +141,8 @@ function AutofixHighlightPopupContent({
   isFocused,
   onShouldPersistChange,
 }: Props & {isFocused?: boolean}) {
+  const organization = useOrganization();
+
   const {mutate: submitComment} = useCommentThread({groupId, runId});
   const {mutate: closeCommentThread} = useCloseCommentThread({groupId, runId});
 
@@ -247,6 +250,14 @@ function AutofixHighlightPopupContent({
       is_agent_comment: isAgentComment ?? false,
     });
     setComment('');
+
+    trackAnalytics('autofix.comment_thread.submit', {
+      organization,
+      group_id: groupId,
+      run_id: runId,
+      step_index: stepIndex,
+      is_agent_comment: isAgentComment ?? false,
+    });
   };
 
   const handleContainerClick = (e: React.MouseEvent) => {
