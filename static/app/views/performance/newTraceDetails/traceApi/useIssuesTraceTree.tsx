9bb97311b3edd7b52e3d71a7bba39491b0c0f396
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-06-02 11:48:24 -0400

    feat(trace-eap-waterfall): Adding no instrumentation nodes to eap waterfall

--- static/app/views/performance/newTraceDetails/traceApi/useIssuesTraceTree.tsx
+++ static/app/views/performance/newTraceDetails/traceApi/useIssuesTraceTree.tsx
@@ -7,6 +7,7 @@ import useProjects from 'sentry/utils/useProjects';
 import {traceAnalytics} from 'sentry/views/performance/newTraceDetails/traceAnalytics';
 import {IssuesTraceTree} from 'sentry/views/performance/newTraceDetails/traceModels/issuesTraceTree';
 import type {TraceTree} from 'sentry/views/performance/newTraceDetails/traceModels/traceTree';
+import {useTraceState} from 'sentry/views/performance/newTraceDetails/traceState/traceStateProvider';
 import type {ReplayRecord} from 'sentry/views/replays/types';
 
 import type {TraceMetaQueryResults} from './useTraceMeta';
@@ -42,6 +43,7 @@ export function useIssuesTraceTree({
 }: UseTraceTreeParams): IssuesTraceTree {
   const api = useApi();
   const {projects} = useProjects();
+  const traceState = useTraceState();
   const organization = useOrganization();
 
   const [tree, setTree] = useState<IssuesTraceTree>(IssuesTraceTree.Empty());
@@ -84,6 +86,7 @@ export function useIssuesTraceTree({
       const newTree = IssuesTraceTree.FromTrace(trace.data, {
         meta: meta.data,
         replay,
+        preferences: traceState.preferences,
       });
 
       setTree(newTree);
@@ -100,6 +103,7 @@ export function useIssuesTraceTree({
     trace.data,
     meta.data,
     traceSlug,
+    traceState.preferences,
   ]);
 
   return tree;
