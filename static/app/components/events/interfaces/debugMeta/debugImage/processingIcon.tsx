dfca1c03e05c6786a4650b7fd3cef933d10595c9
Scott Cooper <scttcper@gmail.com>
2025-06-02 10:34:12 -0700

    remove link

--- static/app/components/events/interfaces/debugMeta/debugImage/processingIcon.tsx
+++ static/app/components/events/interfaces/debugMeta/debugImage/processingIcon.tsx
@@ -79,7 +79,6 @@ function ProcessingIcon({status}: Props) {
       // 1. Relay filters out debug images with completely unknown/unrecognized types during event normalization
       // 2. Symbolicator finds a debug file but it's incompatible with the event type
       //    (e.g., trying to symbolicate .NET events with Windows PDB files)
-      // See: https://github.com/getsentry/team-ingest/issues/550
       return (
         <Tooltip
           containerDisplayMode="inline-flex"
