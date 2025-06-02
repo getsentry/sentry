f01075034686b1f046e2cc01052bed3fbd5d16d5
Scott Cooper <scttcper@gmail.com>
2025-06-02 08:38:49 -0700

    Update static/app/components/searchQueryBuilder/tokens/filterKeyListBox/useFilterKeyListBox.tsx
    Co-authored-by: Dominik Dorfmeister <dominik.dorfmeister@sentry.io>

--- static/app/components/searchQueryBuilder/tokens/filterKeyListBox/useFilterKeyListBox.tsx
+++ static/app/components/searchQueryBuilder/tokens/filterKeyListBox/useFilterKeyListBox.tsx
@@ -224,8 +224,8 @@ export function useFilterKeyListBox({filterValue}: {filterValue: string}) {
       e: React.KeyboardEvent<HTMLInputElement>,
       {state}: {state: ComboBoxState<FilterKeyItem>}
     ) => {
-      const focusedKey = state.selectionManager.focusedKey!;
-      const focusedItem = state.collection.getItem(focusedKey);
+      const focusedKey = state.selectionManager.focusedKey;
+      const focusedItem = focusedKey ? state.collection.getItem(focusedKey) : null;
 
       const direction = e.key === 'ArrowDown' ? 'after' : 'before';
       const nextItem = findNextMatchingItem(
