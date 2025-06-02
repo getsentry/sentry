623d04f18650350913eb59c3b5ef0db2373a0315
Brendan Hy <brendan.hy@sentry.io>
2025-06-02 11:17:22 -0700

    feat(admin): provision sales led with seer (#92456)
    closes
    https://linear.app/getsentry/issue/BIL-543/provisioning-subscriptions-with-seer

--- tests/js/getsentry-test/fixtures/am2Plans.ts
+++ tests/js/getsentry-test/fixtures/am2Plans.ts
@@ -37,7 +37,7 @@ const AM2_CATEGORY_DISPLAY_NAMES = {
   },
   profileDurationUI: {plural: 'UI profile hours', singular: 'UI profile hour'},
   uptime: {singular: 'uptime monitor', plural: 'uptime monitors'},
-  seerAutoFix: {singular: 'issue fix', plural: 'issue fixes'},
+  seerAutofix: {singular: 'issue fix', plural: 'issue fixes'},
   seerScanner: {singular: 'issue scan', plural: 'issue scans'},
 };
 
