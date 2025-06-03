966714e19507c77db7829e1dc9c012001c4dae1b
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2025-06-03 11:08:21 -0700

    fix(detectors): Exclude "prisma:engine:connection" from MN+1 Experiment Issues (#92680)
    When looking at results from the MN+1 Experiment, we noticed a lot of
    issues were being created with "prisma:engine:connection" as the
    repeating span, which isn't actually useful as an MN+1 Issue. Now, if
    this is seen an the span, we don't create the performance issue

--- src/sentry/utils/performance_issues/detectors/experiments/mn_plus_one_db_span_detector.py
+++ src/sentry/utils/performance_issues/detectors/experiments/mn_plus_one_db_span_detector.py
@@ -231,7 +231,7 @@ class ContinuingMNPlusOne(MNPlusOneState):
             metrics.incr("mn_plus_one_db_span_detector.no_parent_span")
             return None
 
-        db_span = self._first_db_span()
+        db_span = self._first_relevant_db_span()
         if not db_span:
             metrics.incr("mn_plus_one_db_span_detector.no_db_span")
             return None
@@ -274,9 +274,12 @@ class ContinuingMNPlusOne(MNPlusOneState):
             ],
         )
 
-    def _first_db_span(self) -> Span | None:
+    def _first_relevant_db_span(self) -> Span | None:
         for span in self.spans:
-            if span["op"].startswith("db"):
+            if (
+                span["op"].startswith("db")
+                and get_span_evidence_value(span, include_op=False) != "prisma:engine:connection"
+            ):
                 return span
         return None
 
