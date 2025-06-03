6baf8c4367aa56aafce933b5fb906206047495a8
Colleen O'Rourke <colleen@sentry.io>
2025-06-03 12:01:28 -0700

    move test and add more tests

---
+++ tests/sentry/workflow_engine/handlers/detector/test_metric_issue_detector_handler.py
@@ -0,0 +1,176 @@
+from datetime import UTC, datetime, timedelta
+
+from sentry.incidents.grouptype import MetricIssueDetectorHandler
+from sentry.incidents.utils.constants import INCIDENTS_SNUBA_SUBSCRIPTION_TYPE
+from sentry.incidents.utils.types import QuerySubscriptionUpdate
+from sentry.snuba.dataset import Dataset
+from sentry.snuba.models import SnubaQuery, SnubaQueryEventType
+from sentry.snuba.subscriptions import create_snuba_query, create_snuba_subscription
+from sentry.testutils.helpers.datetime import freeze_time
+from sentry.workflow_engine.models import DataPacket
+from sentry.workflow_engine.models.data_condition import Condition
+from sentry.workflow_engine.types import DetectorPriorityLevel
+from tests.sentry.workflow_engine.handlers.detector.test_base import BaseDetectorHandlerTest
+
+
+@freeze_time()
+class TestEvaluateMetricDetector(BaseDetectorHandlerTest):
+    def setUp(self):
+        super().setUp()
+        self.detector, self.critical_detector_trigger = self.create_detector_and_condition(
+            "handler_with_state"
+        )
+        self.warning_detector_trigger = self.create_data_condition(
+            comparison=3,
+            type=Condition.GREATER,
+            condition_result=DetectorPriorityLevel.MEDIUM,
+            condition_group=self.detector.workflow_condition_group,
+        )
+        with self.tasks():
+            self.snuba_query = create_snuba_query(
+                query_type=SnubaQuery.Type.ERROR,
+                dataset=Dataset.Events,
+                query="hello",
+                aggregate="count()",
+                time_window=timedelta(minutes=1),
+                resolution=timedelta(minutes=1),
+                environment=self.environment,
+                event_types=[SnubaQueryEventType.EventType.ERROR],
+            )
+            self.query_subscription = create_snuba_subscription(
+                project=self.detector.project,
+                subscription_type=INCIDENTS_SNUBA_SUBSCRIPTION_TYPE,
+                snuba_query=self.snuba_query,
+            )
+        self.alert_rule = self.create_alert_rule()
+        self.create_alert_rule_detector(alert_rule_id=self.alert_rule.id, detector=self.detector)
+
+        self.handler = MetricIssueDetectorHandler(self.detector)
+
+    def test_metric_issue_occurence(self):
+        value = self.critical_detector_trigger.comparison + 1
+        packet = QuerySubscriptionUpdate(
+            entity="entity",
+            subscription_id=self.query_subscription.id,
+            values={"value": value},
+            timestamp=datetime.now(UTC),
+        )
+        data_packet = DataPacket[QuerySubscriptionUpdate](
+            source_id=str(self.query_subscription.id), packet=packet
+        )
+        result = self.handler.evaluate(data_packet)
+
+        detector_group_key = None
+        evidence_data = {
+            "detector_id": self.detector.id,
+            "value": self.critical_detector_trigger.condition_result,
+            "data_condition_ids": [self.critical_detector_trigger.id],
+            "data_condition_type": self.critical_detector_trigger.type.value,
+            "data_condition_comparison_value": value,
+            "alert_id": self.alert_rule.id,
+            "conditions": [
+                {
+                    "id": self.critical_detector_trigger.id,
+                    "type": self.critical_detector_trigger.type,
+                    "comparison": self.critical_detector_trigger.comparison,
+                    "condition_result": self.critical_detector_trigger.condition_result.value,
+                },
+                {
+                    "id": self.warning_detector_trigger.id,
+                    "type": self.warning_detector_trigger.type,
+                    "comparison": self.warning_detector_trigger.comparison,
+                    "condition_result": self.warning_detector_trigger.condition_result.value,
+                },
+            ],
+        }
+        occurrence = result[detector_group_key].result
+        assert occurrence.project_id == self.detector.project_id
+        assert occurrence.issue_title == self.detector.name
+        assert occurrence.subtitle == self.handler.construct_title(
+            snuba_query=self.snuba_query,
+            detector_trigger=self.critical_detector_trigger,
+            priority=self.critical_detector_trigger.condition_result,
+        )
+        assert occurrence.evidence_data == evidence_data
+        assert occurrence.level == "error"
+        assert occurrence.priority == self.critical_detector_trigger.condition_result
+
+    def test_warning_level(self):
+        pass
+
+    def test_return_none(self):
+        pass
+
+
+class TestConstructTitle(TestEvaluateMetricDetector):
+    def test_title_critical(self):
+        title = self.handler.construct_title(
+            snuba_query=self.snuba_query,
+            detector_trigger=self.critical_detector_trigger,
+            priority=self.critical_detector_trigger.condition_result,
+        )
+        assert (
+            title
+            == f"Critical: Number of events in the last minute above {self.critical_detector_trigger.comparison}"
+        )
+
+    def test_title_warning(self):
+        title = self.handler.construct_title(
+            snuba_query=self.snuba_query,
+            detector_trigger=self.warning_detector_trigger,
+            priority=self.warning_detector_trigger.condition_result,
+        )
+        assert (
+            title
+            == f"Warning: Number of events in the last minute above {self.warning_detector_trigger.comparison}"
+        )
+
+    def test_title_comparison_delta(self):
+        self.detector.config.update({"comparison_delta": 60 * 60})
+
+        title = self.handler.construct_title(
+            snuba_query=self.snuba_query,
+            detector_trigger=self.critical_detector_trigger,
+            priority=self.critical_detector_trigger.condition_result,
+        )
+        assert (
+            title
+            == "Critical: Number of events in the last minute greater than same time one hour ago"
+        )
+
+    def test_title_below_threshold(self):
+        self.warning_detector_trigger.type = Condition.LESS
+        self.warning_detector_trigger.save()
+
+        title = self.handler.construct_title(
+            snuba_query=self.snuba_query,
+            detector_trigger=self.warning_detector_trigger,
+            priority=self.warning_detector_trigger.condition_result,
+        )
+        assert (
+            title
+            == f"Warning: Number of events in the last minute below {self.warning_detector_trigger.comparison}"
+        )
+
+    def test_title_different_aggregate(self):
+        self.snuba_query.aggregate = "count_unique(tags[sentry:user])"
+        title = self.handler.construct_title(
+            snuba_query=self.snuba_query,
+            detector_trigger=self.critical_detector_trigger,
+            priority=self.critical_detector_trigger.condition_result,
+        )
+        assert (
+            title
+            == f"Critical: Number of users affected in the last minute above {self.critical_detector_trigger.comparison}"
+        )
+
+        self.snuba_query.aggregate = "percentage(sessions_crashed, sessions)"
+        title = self.handler.construct_title(
+            snuba_query=self.snuba_query,
+            detector_trigger=self.critical_detector_trigger,
+            priority=self.critical_detector_trigger.condition_result,
+        )
+        assert (
+            title
+            == f"Critical: Crash free session rate in the last minute above {self.critical_detector_trigger.comparison}"
+        )
