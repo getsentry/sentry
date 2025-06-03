e773caae24678afd0aa3eb04e4356bcf990dc7cb
Malachi Willey <malwilley@gmail.com>
2025-06-03 15:17:49 -0700

    fix(nav): Settings nav preview should always show org settings (#92775)
    Fixes an issue where, if you were on the project details page, hovering
    over the settings nav item would preview project settings instead of org
    settings.

--- static/app/views/nav/secondary/sections/settings/settingsSecondaryNav.tsx
+++ static/app/views/nav/secondary/sections/settings/settingsSecondaryNav.tsx
@@ -1,14 +1,17 @@
 import {defined} from 'sentry/utils';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
+import {PrimaryNavGroup} from 'sentry/views/nav/types';
+import {useActiveNavGroup} from 'sentry/views/nav/useActiveNavGroup';
 import OrganizationSettingsNavigation from 'sentry/views/settings/organization/organizationSettingsNavigation';
 import ProjectSettingsNavigation from 'sentry/views/settings/project/projectSettingsNavigation';
 
 export function SettingsSecondaryNav() {
   const organization = useOrganization();
   const params = useParams();
-
-  const isProjectSettings = defined(params.projectId);
+  const activeNavGroup = useActiveNavGroup();
+  const isProjectSettings =
+    defined(params.projectId) && activeNavGroup === PrimaryNavGroup.SETTINGS;
 
   return isProjectSettings ? (
     <ProjectSettingsNavigation organization={organization} />
