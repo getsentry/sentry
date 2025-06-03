8488a8264351a097ccb5c474f4f016891d809e2e
nsdeschenes <nicholas.deschenes@sentry.io>
2025-06-03 11:46:04 -0300

    :white_check_mark: Add in tests for new replacing raw search

--- static/app/components/searchQueryBuilder/index.spec.tsx
+++ static/app/components/searchQueryBuilder/index.spec.tsx
@@ -4345,4 +4345,30 @@ describe('SearchQueryBuilder', function () {
       });
     });
   });
+
+  describe('replaceRawSearchKeys', function () {
+    it('should replace raw search keys with defined key:value', async function () {
+      render(
+        <SearchQueryBuilder
+          {...defaultProps}
+          initialQuery=""
+          replaceRawSearchKeys={['span.description']}
+        />
+      );
+
+      await userEvent.type(screen.getByRole('textbox'), 'randomValue');
+
+      expect(
+        within(screen.getByRole('listbox')).getByText('span.description')
+      ).toBeInTheDocument();
+
+      await userEvent.click(
+        within(screen.getByRole('listbox')).getByText('span.description')
+      );
+
+      expect(
+        screen.getByRole('row', {name: 'span.description:randomValue'})
+      ).toBeInTheDocument();
+    });
+  });
 });
