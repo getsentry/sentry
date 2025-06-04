8a52584b0adf7e5a9e15c5fadb6a8d941b4ce9f4
Dan Fuller <dfuller@sentry.io>
2025-06-04 10:53:39 -0700

    fix(uptime): Add ability to skip seat assignment when enabling/disabling an uptime monitor (#92794)
    Billing is currently enabling/disabling monitors by setting the status
    to disabled on the monitor itself, which doesn't properly enable/disable
    the monitor itself.
    
    We need to have a way to call our disable function without triggering
    seat assignment so that billing can call these instead.
    
    <!-- Describe your PR here. -->

--- src/sentry/uptime/subscriptions/subscriptions.py
+++ src/sentry/uptime/subscriptions/subscriptions.py
@@ -351,7 +351,7 @@ def update_project_uptime_subscription(
     uptime_monitor.refresh_from_db()
 
 
-def disable_uptime_detector(detector: Detector):
+def disable_uptime_detector(detector: Detector, skip_quotas: bool = False):
     """
     Disables a uptime detector. The associated ProjectUptimeSubscription will also be disabled,
     and if the UptimeSubscription no longer has any active ProjectUptimeSubscriptions, it will
@@ -381,7 +381,8 @@ def disable_uptime_detector(detector: Detector):
         uptime_monitor.update(status=ObjectStatus.DISABLED)
         detector.update(enabled=False)
 
-        quotas.backend.disable_seat(DataCategory.UPTIME, uptime_monitor)
+        if not skip_quotas:
+            quotas.backend.disable_seat(DataCategory.UPTIME, uptime_monitor)
 
         # Are there any other project subscriptions associated to the subscription
         # that are NOT disabled?
@@ -396,7 +397,9 @@ def disable_uptime_detector(detector: Detector):
             delete_remote_uptime_subscription.delay(uptime_subscription.id)
 
 
-def enable_uptime_detector(detector: Detector, ensure_assignment: bool = False):
+def enable_uptime_detector(
+    detector: Detector, ensure_assignment: bool = False, skip_quotas: bool = False
+):
     """
     Enable a uptime detector. If the uptime subscription was also disabled it
     will be re-activated and the remote subscription will be published.
@@ -417,17 +420,18 @@ def enable_uptime_detector(detector: Detector, ensure_assignment: bool = False):
     ):
         return
 
-    seat_assignment = quotas.backend.check_assign_seat(DataCategory.UPTIME, uptime_monitor)
-    if not seat_assignment.assignable:
-        disable_uptime_detector(detector)
-        raise UptimeMonitorNoSeatAvailable(seat_assignment)
+    if not skip_quotas:
+        seat_assignment = quotas.backend.check_assign_seat(DataCategory.UPTIME, uptime_monitor)
+        if not seat_assignment.assignable:
+            disable_uptime_detector(detector)
+            raise UptimeMonitorNoSeatAvailable(seat_assignment)
 
-    outcome = quotas.backend.assign_seat(DataCategory.UPTIME, uptime_monitor)
-    if outcome != Outcome.ACCEPTED:
-        # Race condition, we were unable to assign the seat even though the
-        # earlier assignment check indicated assignability
-        disable_uptime_detector(detector)
-        raise UptimeMonitorNoSeatAvailable(None)
+        outcome = quotas.backend.assign_seat(DataCategory.UPTIME, uptime_monitor)
+        if outcome != Outcome.ACCEPTED:
+            # Race condition, we were unable to assign the seat even though the
+            # earlier assignment check indicated assignability
+            disable_uptime_detector(detector)
+            raise UptimeMonitorNoSeatAvailable(None)
 
     uptime_subscription: UptimeSubscription = uptime_monitor.uptime_subscription
     uptime_monitor.update(status=ObjectStatus.ACTIVE)
