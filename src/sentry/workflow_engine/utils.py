26863f180e182294a29a425e82596b06112fa7f4
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-06-03 17:38:14 -0700

    Create a utils file for the metrics_incr method that prefixes everything for workflow engine.

---
+++ src/sentry/workflow_engine/utils.py
@@ -0,0 +1,33 @@
+from typing import Any
+
+from sentry.utils import metrics
+
+METRIC_PREFIX = "workflow_engine."
+MetricTags = dict[str, Any]
+
+
+def get_metric_name(metric_name: str) -> str:
+    """
+    Add the prefix to the metric name
+    metric_name (str): The name of the metric.
+
+    Returns:
+        str: The full metric name with the prefix.
+    """
+    return f"{METRIC_PREFIX}{metric_name}"
+
+
+def metric_incr(
+    metric_name: str,
+    value: int = 1,
+    tags: MetricTags | None = None,
+) -> None:
+    """,
+    Send a metric for the workflow engine.
+
+    metric_name (str): The name of the metric.
+    value (int): The number to increment by
+    tags: MetricTags | None: Optional tags to add to the metric.
+    """
+    full_metric_name = get_metric_name(metric_name)
+    metrics.incr(full_metric_name, value, tags=tags)
