214eb5d00e5f2e8ee41961e29fa9e6e7c7250207
mia hsu <55610339+ameliahsu@users.noreply.github.com>
2025-06-04 16:14:41 -0400

    feat(aci): add remaining conditions to automation details conditions panel (#92863)
    added remaining conditions and updated styling (text color)
    
    example:
    <img width="383" alt="Screenshot 2025-06-04 at 11 27 11 AM"
    src="https://github.com/user-attachments/assets/bb085648-cc97-42d7-aae1-26653201d9a5"
    />
    
    
    example with subfilters:
    <img width="383" alt="Screenshot 2025-06-04 at 11 26 35 AM"
    src="https://github.com/user-attachments/assets/903b50cd-9df0-4ca7-b80c-11cefce79218"
    />
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/views/automations/components/actionFilters/issueOccurrences.tsx
+++ static/app/views/automations/components/actionFilters/issueOccurrences.tsx
@@ -1,6 +1,13 @@
 import AutomationBuilderNumberField from 'sentry/components/workflowEngine/form/automationBuilderNumberField';
 import {tct} from 'sentry/locale';
+import type {DataCondition} from 'sentry/types/workflowEngine/dataConditions';
 import {useDataConditionNodeContext} from 'sentry/views/automations/components/dataConditionNodes';
+
+export function IssueOccurrencesDetails({condition}: {condition: DataCondition}) {
+  return tct('The issue has happened at least [value] times', {
+    value: condition.comparison.value,
+  });
+}
 
 export default function IssueOccurrencesNode() {
   return tct('The issue has happened at least [value] times', {
--- static/app/views/automations/components/conditionsPanel.tsx
+++ static/app/views/automations/components/conditionsPanel.tsx
@@ -1,7 +1,6 @@
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
-import {Flex} from 'sentry/components/container/flex';
 import {ConditionBadge} from 'sentry/components/workflowEngine/ui/conditionBadge';
 import {t, tct} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
@@ -21,18 +20,18 @@ type ConditionsPanelProps = {
 function ConditionsPanel({triggers, actionFilters}: ConditionsPanelProps) {
   return (
     <Panel>
-      <Flex column gap={space(1)}>
-        <div>
+      <ConditionGroupWrapper>
+        <ConditionGroupHeader>
           {tct('[when:When] any of the following occur', {
             when: <ConditionBadge />,
           })}
-        </div>
+        </ConditionGroupHeader>
         {triggers.conditions.map((trigger, index) => (
           <div key={index}>
             <DataConditionDetails condition={trigger} />
           </div>
         ))}
-      </Flex>
+      </ConditionGroupWrapper>
       {actionFilters.map((actionFilter, index) => (
         <div key={index}>
           <ActionFilter actionFilter={actionFilter} totalFilters={actionFilters.length} />
@@ -49,12 +48,12 @@ interface ActionFilterProps {
 
 function ActionFilter({actionFilter, totalFilters}: ActionFilterProps) {
   return (
-    <ActionFilterWrapper showDivider={totalFilters > 1}>
-      <div>
+    <ConditionGroupWrapper showDivider={totalFilters > 1}>
+      <ConditionGroupHeader>
         {tct('[if:If] any of these filters match', {
           if: <ConditionBadge />,
         })}
-      </div>
+      </ConditionGroupHeader>
       {actionFilter.conditions.length > 0
         ? actionFilter.conditions.map((filter, index) => (
             <div key={index}>
@@ -62,17 +61,17 @@ function ActionFilter({actionFilter, totalFilters}: ActionFilterProps) {
             </div>
           ))
         : t('Any event')}
-      <div>
+      <ConditionGroupHeader>
         {tct('[then:Then] perform these actions', {
           then: <ConditionBadge />,
         })}
-      </div>
+      </ConditionGroupHeader>
       {actionFilter.actions?.map((action, index) => (
         <div key={index}>
           <ActionDetails action={action} />
         </div>
       ))}
-    </ActionFilterWrapper>
+    </ConditionGroupWrapper>
   );
 }
 
@@ -108,10 +107,15 @@ const Panel = styled('div')`
   padding: ${space(1.5)};
 `;
 
-const ActionFilterWrapper = styled('div')<{showDivider?: boolean}>`
+const ConditionGroupHeader = styled('div')`
+  color: ${p => p.theme.textColor};
+`;
+
+const ConditionGroupWrapper = styled('div')<{showDivider?: boolean}>`
   display: flex;
   flex-direction: column;
   gap: ${space(1)};
+  color: ${p => p.theme.subText};
 
   ${p =>
     p.showDivider &&
