683511149a39db5e45b35bf23ae3b5ac9585d200
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-06-04 13:29:22 -0400

    fix(trace-eap-waterfall): Converting ms to s

--- src/sentry/api/endpoints/organization_trace.py
+++ src/sentry/api/endpoints/organization_trace.py
@@ -110,7 +110,7 @@ class OrganizationTraceEndpoint(OrganizationEventsV2EndpointBase):
             )
         elif event.get("event_type") == "error":
             timestamp = (
-                event["timestamp_ms"] / 1000.0
+                datetime.datetime.fromisoformat(event["timestamp_ms"]).timestamp()
                 if "timestamp_ms" in event and event["timestamp_ms"] is not None
                 else event["timestamp"]
             )
