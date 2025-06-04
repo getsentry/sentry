8e699ecc44d0237d26a42c9fedb423aaa91effc1
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-06-04 15:28:40 -0400

    add open to explore to cold start device distrubution chart

--- static/app/views/insights/mobile/appStarts/components/startDurationWidget.tsx
+++ static/app/views/insights/mobile/appStarts/components/startDurationWidget.tsx
@@ -113,7 +113,7 @@ function StartDurationWidget({additionalFilters}: Props) {
       search={search}
       showReleaseAs="none"
       showLegend="always"
-      height={'100%'}
+      height={220}
     />
   );
 }
