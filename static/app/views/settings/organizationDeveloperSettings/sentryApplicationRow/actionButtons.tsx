4e6d96e8e75d87c7cd82375875e55843a783fbd8
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-06-05 16:44:40 -0400

    ref(ui): Clean up integrations list (#92963)
    - Converts Row component to FC
    - Simplifies action bar component
    - Use normal button sizes for actions

--- static/app/views/settings/organizationDeveloperSettings/sentryApplicationRow/actionButtons.tsx
+++ static/app/views/settings/organizationDeveloperSettings/sentryApplicationRow/actionButtons.tsx
@@ -1,11 +1,9 @@
-import styled from '@emotion/styled';
-
 import ConfirmDelete from 'sentry/components/confirmDelete';
 import {Button} from 'sentry/components/core/button';
+import {ButtonBar} from 'sentry/components/core/button/buttonBar';
 import {LinkButton} from 'sentry/components/core/button/linkButton';
 import {IconDelete, IconStats, IconUpgrade} from 'sentry/icons';
 import {t} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
 import type {SentryApp} from 'sentry/types/integrations';
 import type {Organization} from 'sentry/types/organization';
 
@@ -33,25 +31,25 @@ function ActionButtons({
   disableDeleteReason,
 }: Props) {
   const appDashboardButton = (
-    <StyledLinkButton
-      size="xs"
+    <LinkButton
+      size="sm"
       icon={<IconStats />}
       to={`/settings/${org.slug}/developer-settings/${app.slug}/dashboard/`}
     >
       {t('Dashboard')}
-    </StyledLinkButton>
+    </LinkButton>
   );
 
   const publishRequestButton = showPublish ? (
-    <StyledButton
+    <Button
       disabled={!!disablePublishReason}
       title={disablePublishReason}
       icon={<IconUpgrade />}
-      size="xs"
+      size="sm"
       onClick={onPublish}
     >
       {t('Publish')}
-    </StyledButton>
+    </Button>
   ) : null;
 
   const deleteConfirmMessage = t(
@@ -60,10 +58,10 @@ function ActionButtons({
   );
   const deleteButton = showDelete ? (
     disableDeleteReason ? (
-      <StyledButton
+      <Button
         disabled
         title={disableDeleteReason}
-        size="xs"
+        size="sm"
         icon={<IconDelete />}
         aria-label={t('Delete')}
       />
@@ -75,35 +73,19 @@ function ActionButtons({
           priority="danger"
           onConfirm={() => onDelete(app)}
         >
-          <StyledButton size="xs" icon={<IconDelete />} aria-label={t('Delete')} />
+          <Button size="sm" icon={<IconDelete />} aria-label={t('Delete')} />
         </ConfirmDelete>
       )
     )
   ) : null;
 
   return (
-    <ButtonHolder>
+    <ButtonBar gap={1}>
       {appDashboardButton}
       {publishRequestButton}
       {deleteButton}
-    </ButtonHolder>
+    </ButtonBar>
   );
 }
-
-const ButtonHolder = styled('div')`
-  flex-direction: row;
-  display: flex;
-  & > * {
-    margin-left: ${space(0.5)};
-  }
-`;
-
-const StyledButton = styled(Button)`
-  color: ${p => p.theme.subText};
-`;
-
-const StyledLinkButton = styled(LinkButton)`
-  color: ${p => p.theme.subText};
-`;
 
 export default ActionButtons;
