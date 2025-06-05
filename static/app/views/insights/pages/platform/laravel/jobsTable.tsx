7bcffdbf03e45790a7e5f5d2915658f2ec426a52
ArthurKnaus <arthur.knaus@sentry.io>
2025-06-05 13:51:46 +0200

    feat(laravel-insights): Add commands and jobs table (#92924)
    Add jobs and commands table to the laravel insights page.
    
    - closes [TET-486: Add switch on top of the
    table](https://linear.app/getsentry/issue/TET-486/add-switch-on-top-of-the-table)

---
+++ static/app/views/insights/pages/platform/laravel/jobsTable.tsx
@@ -0,0 +1,132 @@
+import {useCallback} from 'react';
+
+import {
+  COL_WIDTH_UNDEFINED,
+  type GridColumnHeader,
+  type GridColumnOrder,
+} from 'sentry/components/gridEditable';
+import Link from 'sentry/components/links/link';
+import {t} from 'sentry/locale';
+import {useLocation} from 'sentry/utils/useLocation';
+import {HeadSortCell} from 'sentry/views/insights/agentMonitoring/components/headSortCell';
+import {TimeSpentCell} from 'sentry/views/insights/common/components/tableCells/timeSpentCell';
+import {useModuleURL} from 'sentry/views/insights/common/utils/useModuleURL';
+import {Referrer} from 'sentry/views/insights/pages/platform/laravel/referrers';
+import {PlatformInsightsTable} from 'sentry/views/insights/pages/platform/shared/table';
+import {DurationCell} from 'sentry/views/insights/pages/platform/shared/table/DurationCell';
+import {ErrorRateCell} from 'sentry/views/insights/pages/platform/shared/table/ErrorRateCell';
+import {NumberCell} from 'sentry/views/insights/pages/platform/shared/table/NumberCell';
+import {useTableData} from 'sentry/views/insights/pages/platform/shared/table/useTableData';
+
+const defaultColumnOrder: Array<GridColumnOrder<string>> = [
+  {key: 'messaging.destination.name', name: t('Job Name'), width: COL_WIDTH_UNDEFINED},
+  {key: 'count()', name: t('Processed'), width: 124},
+  {key: 'failure_rate()', name: t('Error Rate'), width: 124},
+  {
+    key: 'avg(messaging.message.receive.latency)',
+    name: t('Avg Time in Queue'),
+    width: 164,
+  },
+  {
+    key: 'avg_if(span.duration,span.op,queue.process)',
+    name: t('Avg Processing Time'),
+    width: 184,
+  },
+  {key: 'sum(span.duration)', name: t('Time Spent'), width: 120},
+];
+
+const rightAlignColumns = new Set([
+  'count()',
+  'failure_rate()',
+  'sum(span.duration)',
+  'avg(messaging.message.receive.latency)',
+  'avg_if(span.duration,span.op,queue.process)',
+]);
+
+export function JobsTable() {
+  const tableDataRequest = useTableData({
+    query: 'span.op:queue.process',
+    fields: [
+      'count()',
+      'messaging.destination.name',
+      'avg(messaging.message.receive.latency)',
+      'avg_if(span.duration,span.op,queue.process)',
+      'failure_rate()',
+      'sum(span.duration)',
+    ],
+    cursorParamName: 'jobsCursor',
+    referrer: Referrer.PATHS_TABLE,
+  });
+
+  const renderHeadCell = useCallback((column: GridColumnHeader<string>) => {
+    return (
+      <HeadSortCell
+        sortKey={column.key}
+        align={rightAlignColumns.has(column.key) ? 'right' : 'left'}
+        forceCellGrow={column.key === 'messaging.destination.name'}
+        cursorParamName="jobsCursor"
+      >
+        {column.name}
+      </HeadSortCell>
+    );
+  }, []);
+
+  const renderBodyCell = useCallback(
+    (
+      column: GridColumnOrder<string>,
+      dataRow: (typeof tableDataRequest.data)[number]
+    ) => {
+      switch (column.key) {
+        case 'messaging.destination.name':
+          return <DestinationCell destination={dataRow['messaging.destination.name']} />;
+        case 'failure_rate()':
+          return <ErrorRateCell errorRate={dataRow['failure_rate()']} />;
+        case 'avg(messaging.message.receive.latency)':
+        case 'avg_if(span.duration,span.op,queue.process)':
+          return <DurationCell milliseconds={dataRow[column.key]} />;
+        case 'count()':
+          return <NumberCell value={dataRow['count()']} />;
+        case 'sum(span.duration)':
+          return <TimeSpentCell total={dataRow['sum(span.duration)']} />;
+        default:
+          return <div />;
+      }
+    },
+    [tableDataRequest]
+  );
+
+  return (
+    <PlatformInsightsTable
+      isLoading={tableDataRequest.isPending}
+      error={tableDataRequest.error}
+      data={tableDataRequest.data}
+      initialColumnOrder={defaultColumnOrder}
+      stickyHeader
+      grid={{
+        renderBodyCell,
+        renderHeadCell,
+      }}
+      cursorParamName="jobsCursor"
+      pageLinks={tableDataRequest.pageLinks}
+      isPlaceholderData={tableDataRequest.isPlaceholderData}
+    />
+  );
+}
+
+function DestinationCell({destination}: {destination: string}) {
+  const moduleURL = useModuleURL('queue');
+  const {query} = useLocation();
+  return (
+    <Link
+      to={{
+        pathname: `${moduleURL}/destination/`,
+        query: {
+          ...query,
+          destination,
+        },
+      }}
+    >
+      {destination}
+    </Link>
+  );
+}
