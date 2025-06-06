a331d1605b555cbfb2aeba74c6b485b3aae2de11
Yuval Mandelboum <yuval.mandelboum@sentry.io>
2025-06-05 17:33:43 -0700

    feat(sampling) - Changed times_seen increments on groups to take error sample rate into account

--- src/sentry/event_manager.py
+++ src/sentry/event_manager.py
@@ -749,7 +749,7 @@ def _derive_client_error_sampling_rate(jobs: Sequence[Job], projects: ProjectsMa
                 )
 
                 if client_sample_rate is not None and isinstance(client_sample_rate, (int, float)):
-                    if 0 <= client_sample_rate <= 1:
+                    if 0 < client_sample_rate <= 1:
                         job["data"]["sample_rate"] = client_sample_rate
                     else:
                         logger.warning(
@@ -1471,6 +1471,15 @@ def _create_group(
     group_data["metadata"]["initial_priority"] = priority
     group_creation_kwargs["data"] = group_data
 
+    # Set initial times_seen
+    group_creation_kwargs["times_seen"] = 1
+
+    # If the project is in the allowlist, use the client sample rate to weight the times_seen
+    if project.id in options.get("issues.client_error_sampling.project_allowlist"):
+        client_sample_rate = event.data.get("sample_rate")
+        if client_sample_rate is not None and client_sample_rate > 0:
+            group_creation_kwargs["times_seen"] = int(1 / client_sample_rate)
+
     try:
         with transaction.atomic(router.db_for_write(Group)):
             # This is the 99.999% path. The rest of the function is all to handle a very rare and
@@ -1821,7 +1830,16 @@ def _process_existing_aggregate(
 
     # We pass `times_seen` separately from all of the other columns so that `buffer_inr` knows to
     # increment rather than overwrite the existing value
-    buffer_incr(Group, {"times_seen": 1}, {"id": group.id}, updated_group_values)
+    if group.project.id in options.get("issues.client_error_sampling.project_allowlist"):
+        client_sample_rate = event.data.get("sample_rate")
+        if client_sample_rate is not None and client_sample_rate > 0:
+            weighted_times_seen = int(1 / client_sample_rate)
+        else:
+            weighted_times_seen = 1
+    else:
+        weighted_times_seen = 1
+
+    buffer_incr(Group, {"times_seen": weighted_times_seen}, {"id": group.id}, updated_group_values)
 
     return bool(is_regression)
 
