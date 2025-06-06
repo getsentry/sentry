2356c105767efc1378b4a32b3230d849632cc129
Mia Hsu <ameliahsu@gmail.com>
2025-06-06 13:52:47 -0700

    feat(aci): add action details to conditions panel

--- static/app/views/automations/components/actions/github.tsx
+++ static/app/views/automations/components/actions/github.tsx
@@ -1,8 +1,25 @@
 import {ActionMetadata} from 'sentry/components/workflowEngine/ui/actionMetadata';
 import {tct} from 'sentry/locale';
-import {ActionType} from 'sentry/types/workflowEngine/actions';
+import {Action, ActionHandler, ActionType} from 'sentry/types/workflowEngine/actions';
 import {IntegrationField} from 'sentry/views/automations/components/actions/integrationField';
 import {TicketActionSettingsButton} from 'sentry/views/automations/components/actions/ticketActionSettingsButton';
+
+export function GithubDetails({
+  action,
+  handler,
+}: {
+  action: Action;
+  handler: ActionHandler;
+}) {
+  const integrationName =
+    handler.integrations?.find(i => i.id === action.integrationId)?.name ||
+    action.integrationId;
+
+  return tct('Create a [logo] GitHub issue in [integration]', {
+    logo: ActionMetadata[ActionType.GITHUB]?.icon,
+    integration: integrationName,
+  });
+}
 
 export function GithubNode() {
   return tct('Create a [logo] GitHub issue in [integration] with these [settings]', {
