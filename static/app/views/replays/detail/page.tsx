137627c6055032050a4e91479e65aa4d59301fc1
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-06-06 12:51:30 -0700

    fix(replay): Fix replay share at current timestamp button (#93054)
    This broke from the changes in
    https://github.com/getsentry/sentry/pull/92230
    
    What we need is to hoist the `<ReplayContext>` up so it wraps the header
    and main part of the page again.
    
    Fixes 93032

--- static/app/views/replays/detail/page.tsx
+++ static/app/views/replays/detail/page.tsx
@@ -5,7 +5,6 @@ import ArchivedReplayAlert from 'sentry/components/replays/alerts/archivedReplay
 import ReplayLoadingState from 'sentry/components/replays/player/replayLoadingState';
 import ReplayProcessingError from 'sentry/components/replays/replayProcessingError';
 import type useLoadReplayReader from 'sentry/utils/replays/hooks/useLoadReplayReader';
-import ReplayDetailsProviders from 'sentry/views/replays/detail/body/replayDetailsProviders';
 import ReplaysLayout from 'sentry/views/replays/detail/layout';
 import ReplayDetailsError from 'sentry/views/replays/detail/replayDetailsError';
 
@@ -49,13 +48,11 @@ export default function ReplayDetailsPage({readerResult}: Props) {
       )}
     >
       {({replay}) => (
-        <ReplayDetailsProviders replay={replay} projectSlug={readerResult.projectSlug}>
-          <ReplaysLayout
-            isVideoReplay={replay.isVideoReplay()}
-            replayRecord={replay.getReplay()}
-            isLoading={false}
-          />
-        </ReplayDetailsProviders>
+        <ReplaysLayout
+          isVideoReplay={replay.isVideoReplay()}
+          replayRecord={replay.getReplay()}
+          isLoading={false}
+        />
       )}
     </ReplayLoadingState>
   );
