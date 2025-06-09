e3da33e19820a95b5e345e4c84c30f7f3617c911
Steven Eubank <47563310+smeubank@users.noreply.github.com>
2025-06-09 10:47:52 -0700

    Update static/app/views/alerts/create.tsx
    Co-authored-by: Evan Purkhiser <evanpurkhiser@gmail.com>

--- static/app/views/alerts/create.tsx
+++ static/app/views/alerts/create.tsx
@@ -126,8 +126,7 @@ function Create(props: Props) {
       : 'issues';
   }
 
-  const title =
-    alertType === AlertRuleType.UPTIME ? t('Create Uptime Monitor') : t('New Alert Rule');
+  const title = TITLES[alertType] ?? t('New Alert Rule');
 
   return (
     <Fragment>
