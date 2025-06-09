b26d6802570bf5aacb67eba3b81a66a83247b0ee
anthony sottile <anthony.sottile@sentry.io>
2025-06-09 14:48:20 -0400

    ref: improve types of utils.warnings and status_checks

--- src/sentry/status_checks/__init__.py
+++ src/sentry/status_checks/__init__.py
@@ -1,5 +1,3 @@
-__all__ = ("check_all", "sort_by_severity", "Problem", "StatusCheck")
-
 from sentry.utils.warnings import seen_warnings
 
 from .base import Problem, StatusCheck, sort_by_severity
@@ -7,8 +5,11 @@ from .celery_alive import CeleryAliveCheck
 from .celery_app_version import CeleryAppVersionCheck
 from .warnings import WarningStatusCheck
 
+__all__ = ("check_all", "sort_by_severity", "Problem", "StatusCheck")
+
+
 checks = [CeleryAliveCheck(), CeleryAppVersionCheck(), WarningStatusCheck(seen_warnings)]
 
 
-def check_all():
+def check_all() -> dict[StatusCheck, list[Problem]]:
     return {check: check.check() for check in checks}
