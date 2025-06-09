267fdab9fd2c848b831e1b0baf53b9119a66f745
Tony Xiao <txiao@sentry.io>
2025-06-09 16:28:00 -0400

    fix(charts): Use 24h format for chart when requested

--- static/app/views/dashboards/widgets/timeSeriesWidget/formatters/formatXAxisTimestamp.spec.tsx
+++ static/app/views/dashboards/widgets/timeSeriesWidget/formatters/formatXAxisTimestamp.spec.tsx
@@ -1,4 +1,7 @@
 import moment from 'moment-timezone';
+import {UserFixture} from 'sentry-fixture/user';
+
+import ConfigStore from 'sentry/stores/configStore';
 
 import {formatXAxisTimestamp} from './formatXAxisTimestamp';
 
@@ -23,7 +26,40 @@ describe('formatXAxisTimestamp', () => {
     ['2025-02-05T12:10:05', '12:10:05 PM'],
     ['2025-02-05T12:10:06', '12:10:06 PM'],
     ['2025-02-05T05:25:10', '5:25:10 AM'],
-  ])('formats %s as %s', (raw, formatted) => {
+  ])('formats %s as %s with 12h format', (raw, formatted) => {
+    const user = UserFixture();
+    user.options.clock24Hours = false;
+    ConfigStore.set('user', user);
+
+    const timestamp = moment(raw).unix() * 1000;
+    expect(formatXAxisTimestamp(timestamp)).toEqual(formatted);
+  });
+
+  it.each([
+    // Year starts
+    ['2025-01-01T00:00:00', 'Jan 1st 2025'],
+    ['2024-01-01T00:00:00', 'Jan 1st 2024'],
+    // // Month starts
+    ['2025-02-01T00:00:00', 'Feb 1st'],
+    ['2024-03-01T00:00:00', 'Mar 1st'],
+    // // Day starts
+    ['2025-02-05T00:00:00', 'Feb 5th'],
+    // // Hour starts
+    ['2025-02-05T12:00:00', '12:00'],
+    ['2025-02-05T17:00:00', '17:00'],
+    ['2025-02-01T13:00:00', '13:00'],
+    // Minute starts
+    ['2025-02-05T12:11:00', '12:11'],
+    ['2025-02-05T17:25:00', '17:25'],
+    // Seconds
+    ['2025-02-05T12:10:05', '12:10:05'],
+    ['2025-02-05T12:10:06', '12:10:06'],
+    ['2025-02-05T17:25:10', '17:25:10'],
+  ])('formats %s as %s with 24h format', (raw, formatted) => {
+    const user = UserFixture();
+    user.options.clock24Hours = true;
+    ConfigStore.set('user', user);
+
     const timestamp = moment(raw).unix() * 1000;
     expect(formatXAxisTimestamp(timestamp)).toEqual(formatted);
   });
