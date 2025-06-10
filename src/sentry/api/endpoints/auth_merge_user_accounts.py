3d104bd23625a3d6669f0fd1f7fa605bfd30ca08
Michelle Fu <michelle.fu@sentry.io>
2025-06-10 13:51:32 -0700

    first pass

--- src/sentry/api/endpoints/auth_merge_user_accounts.py
+++ src/sentry/api/endpoints/auth_merge_user_accounts.py
@@ -9,6 +9,7 @@ from sentry.api.permissions import SentryIsAuthenticated
 from sentry.api.serializers import serialize
 from sentry.users.api.serializers.user import UserSerializerWithOrgMemberships
 from sentry.users.models.user import User
+from sentry.users.models.useremail import UserEmail
 
 
 @control_silo_endpoint
@@ -38,3 +39,45 @@ class AuthMergeUserAccountsEndpoint(Endpoint):
             on_results=lambda x: serialize(x, user, UserSerializerWithOrgMemberships()),
             paginator_cls=OffsetPaginator,
         )
+
+    def post(self, request: Request) -> Response:
+        user = request.user
+        if isinstance(user, AnonymousUser):
+            return Response(
+                status=401,
+                data={"error": "You must be authenticated to use this endpoint"},
+            )
+
+        primary_user = User.objects.get(id=user.id)
+        user_email = UserEmail.objects.get(user_id=primary_user.id, email=primary_user.email)
+        if not user_email.is_verified:
+            return Response(
+                status=403,
+                data={"error": "You must verify your primary email address to use this endpoint"},
+            )
+
+        ids_to_delete = request.data.get("ids_to_delete", [])
+        ids_to_merge = request.data.get("ids_to_merge", [])
+        shared_email = primary_user.email
+        affected_user_emails = User.objects.filter(
+            id__in=(ids_to_delete + ids_to_merge)
+        ).values_list("email", flat=True)
+
+        if any(email != shared_email for email in affected_user_emails):
+            return Response(
+                status=403,
+                data={
+                    "error": "One or more of the accounts in your request does not share your primary email address"
+                },
+            )
+
+        users_to_delete = User.objects.filter(id__in=ids_to_delete)
+        for user in users_to_delete:
+            user.delete()
+
+        users_to_merge = User.objects.filter(id__in=ids_to_merge)
+        for user in users_to_merge:
+            user.merge_to(primary_user)
+            user.delete()
+
+        return Response("Successfully merged user accounts.")
--- tests/sentry/api/endpoints/test_auth_merge_user_accounts.py
+++ tests/sentry/api/endpoints/test_auth_merge_user_accounts.py
@@ -1,5 +1,6 @@
 from sentry.testutils.cases import APITestCase
 from sentry.testutils.silo import control_silo_test
+from sentry.users.models.user import User
 
 
 @control_silo_test
@@ -38,3 +39,31 @@ class ListUserAccountsWithSharedEmailTest(APITestCase):
         assert response.data[0]["organizations"] == [org1.name, org2.name]
         assert response.data[1]["organizations"] == [org3.name]
         assert response.data[2]["organizations"] == []
+
+
+@control_silo_test
+class MergeUserAccountsWithSharedEmailTest(APITestCase):
+    endpoint = "sentry-api-0-auth-merge-accounts"
+    method = "post"
+
+    def setUp(self):
+        self.user1 = self.create_user(username="powerful mifu", email="mifu@email.com")
+        self.user2 = self.create_user(username="transcendent mifu", email="mifu@email.com")
+        self.user3 = self.create_user(username="garden variety mifu", email="mifu@email.com")
+
+        org1 = self.create_organization(name="hojicha")
+        org2 = self.create_organization(name="matcha")
+        org3 = self.create_organization(name="oolong")
+
+        self.create_member(user=self.user1, organization=org1)
+        self.create_member(user=self.user1, organization=org2)
+        self.create_member(user=self.user2, organization=org3)
+
+        self.login_as(self.user1)
+
+    def test_simple(self):
+        data = {"ids_to_delete": [self.user3.id], "ids_to_merge": [self.user2.id]}
+        self.get_success_response(**data)
+
+        assert not User.objects.filter(id=self.user2.id).exists()
+        assert not User.objects.filter(id=self.user3.id).exists()
