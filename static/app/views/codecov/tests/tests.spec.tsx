a1ce6a51e3705b701d5746ac7638520150148e65
Adrian <adrian.bonilla@sentry.io>
2025-06-10 10:09:13 -0700

    Adrian/feat add test analytics infinite hook (#93198)
    This PR adds an infiniteQuery hook to fetch test analytics data from
    Codecov's backend via Sentry.
    
    Notes
    - Adds the `useInfiniteTestResults` hook. This is a preliminary version
    of the hook and minimally supports query parameters or sorting.
    - Header css styling in SortableHeader.tsx.
    - Removes fake test data from test.tsx page and moves CodecovProvider to
    the Wrapper parent component to allow context data to be used by this
    page.

--- static/app/views/codecov/tests/tests.spec.tsx
+++ static/app/views/codecov/tests/tests.spec.tsx
@@ -1,18 +1,51 @@
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 
+import CodecovQueryParamsProvider from 'sentry/components/codecov/container/codecovParamsProvider';
 import TestsPage from 'sentry/views/codecov/tests/tests';
+
+const mockTestResultsData = [
+  {
+    name: 'tests.symbolicator.test_unreal_full.SymbolicatorUnrealIntegrationTest::test_unreal_crash_with_attachments',
+    avgDuration: 4,
+    flakeRate: 0.4,
+    commitsFailed: 1,
+    lastRun: '2025-04-17T22:26:19.486793+00:00',
+    totalFailCount: 1,
+    totalFlakyFailCount: 2,
+    totalPassCount: 0,
+    totalSkipCount: 3,
+    updatedAt: '2025-04-17T22:26:19.486793+00:00',
+  },
+];
+
+const mockApiCall = () =>
+  MockApiClient.addMockResponse({
+    url: `/prevent/owner/some-org-name/repository/some-repository/test-results/`,
+    method: 'GET',
+    body: {data: mockTestResultsData},
+  });
 
 describe('CoveragePageWrapper', () => {
   describe('when the wrapper is used', () => {
+    mockApiCall();
     it('renders the passed children', async () => {
-      render(<TestsPage />, {
-        initialRouterConfig: {
-          location: {
-            pathname: '/codecov/tests',
-            query: {codecovPeriod: '7d'},
+      render(
+        <CodecovQueryParamsProvider>
+          <TestsPage />
+        </CodecovQueryParamsProvider>,
+        {
+          initialRouterConfig: {
+            location: {
+              pathname: '/codecov/tests',
+              query: {
+                codecovPeriod: '7d',
+                integratedOrg: 'some-org-name',
+                repository: 'some-repository',
+              },
+            },
           },
-        },
-      });
+        }
+      );
 
       const testsAnalytics = await screen.findByText('Test Analytics');
       expect(testsAnalytics).toBeInTheDocument();
