54aafd740330ea2201b7548b968af55b686a7394
Evan Hicks <evanh@users.noreply.github.com>
2025-06-10 10:50:22 -0400

    fix(taskscheduler): Add a flag for which schedules should be on taskscheduler (#93139)
    This enables specific schedules to be run on either the taskscheduler or
    celerybeat.

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3426,6 +3426,14 @@ register(
     default={},
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
+
+# Flags for taskworker scheduler rollout
+register(
+    "taskworker.scheduler.rollout",
+    default=["sync_options_trial"],
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
+
 # Orgs for which compression should be disabled in the chunk upload endpoint.
 # This is intended to circumvent sporadic 503 errors reported by some customers.
 register("chunk-upload.no-compression", default=[], flags=FLAG_AUTOMATOR_MODIFIABLE)
