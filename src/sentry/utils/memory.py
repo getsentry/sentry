1c7436d207dc5a191602ca5d86002d03ba89f894
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2025-06-10 14:05:11 -0400

    ref: stricter typing for a few utils modules (#93184)
    <!-- Describe your PR here. -->

--- src/sentry/utils/memory.py
+++ src/sentry/utils/memory.py
@@ -1,15 +1,19 @@
+from __future__ import annotations
+
 import resource
+from collections.abc import Generator
 from contextlib import contextmanager
+from typing import Any
 
 from sentry.utils import metrics
 
 
-def get_rss_usage():
+def get_rss_usage() -> int:
     return resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
 
 
 @contextmanager
-def track_memory_usage(metric, **kwargs):
+def track_memory_usage(metric: str, **kwargs: Any) -> Generator[None]:
     before = get_rss_usage()
     try:
         yield
