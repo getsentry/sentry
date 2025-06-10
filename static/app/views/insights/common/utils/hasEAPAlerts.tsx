2bce2c76774de451967df8a984965eb48fe3d5e5
Shruthi <shruthilaya.jaganathan@sentry.io>
2025-06-10 13:51:02 -0400

    feat(eap-alerts): UI deprecation of transaction alerts (#93096)
    If the feature flag is enabled, all the aggregation based alerts
    point to span metrics instead of transactions. This is in preparation
    for transaction alert deprecation.

--- static/app/views/insights/common/utils/hasEAPAlerts.tsx
+++ static/app/views/insights/common/utils/hasEAPAlerts.tsx
@@ -3,3 +3,7 @@ import type {Organization} from 'sentry/types/organization';
 export function hasEAPAlerts(organization: Organization): boolean {
   return organization.features.includes('visibility-explore-view');
 }
+
+export function deprecateTransactionAlerts(organization: Organization): boolean {
+  return organization.features.includes('performance-transaction-deprecation-alerts');
+}
