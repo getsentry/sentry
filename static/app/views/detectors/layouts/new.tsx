c8684156ffd594eb9d8048b11e44dffdac9f3ea4
Scott Cooper <scttcper@gmail.com>
2025-06-10 15:27:37 -0700

    feat(aci): Pass project id, environment to new detector
    On the first step of creating a new detector we let the user decide:
    - Detector type
    - Project
    - Environment (optional)
    - title (optional)
    
    We send them to a new route on "next", we preserve their choices by setting query parameters.

--- static/app/views/detectors/layouts/new.tsx
+++ static/app/views/detectors/layouts/new.tsx
@@ -1,21 +1,22 @@
-import {useState} from 'react';
-
 import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
 import {BreadcrumbsProvider} from 'sentry/components/workflowEngine/layout/breadcrumbs';
 import EditLayout from 'sentry/components/workflowEngine/layout/edit';
 import {t} from 'sentry/locale';
+import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import {makeMonitorBasePathname} from 'sentry/views/detectors/pathnames';
 
 export default function NewDetectorLayout({children}: {children: React.ReactNode}) {
+  const location = useLocation();
   const organization = useOrganization();
-  const [title, setTitle] = useState(t('New Monitor'));
+  const title = (location.query.title as string | undefined) ?? '';
+
   return (
-    <SentryDocumentTitle title={title} noSuffix>
+    <SentryDocumentTitle title={title || t('New Monitor')}>
       <BreadcrumbsProvider
         crumb={{label: t('Monitors'), to: makeMonitorBasePathname(organization.slug)}}
       >
-        <EditLayout onTitleChange={value => setTitle(value)}>{children}</EditLayout>
+        <EditLayout title={title}>{children}</EditLayout>
       </BreadcrumbsProvider>
     </SentryDocumentTitle>
   );
