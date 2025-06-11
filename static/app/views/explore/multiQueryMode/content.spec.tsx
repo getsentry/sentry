f624275f6d0dba68d72913b55b8c5c64f19da41f
Malachi Willey <malwilley@gmail.com>
2025-06-11 15:45:35 -0700

    Fix test with extra mock

--- static/app/views/explore/multiQueryMode/content.spec.tsx
+++ static/app/views/explore/multiQueryMode/content.spec.tsx
@@ -86,6 +86,10 @@ describe('MultiQueryModeContent', function () {
       body: [],
     });
     MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/recent-searches/`,
+      method: 'POST',
+    });
+    MockApiClient.addMockResponse({
       url: `/subscriptions/${organization.slug}/`,
       method: 'GET',
       body: [],
