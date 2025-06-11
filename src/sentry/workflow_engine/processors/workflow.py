67919dfb93217c961389f6f4bf0a20f79a6ca17c
Raj Joshi <raj.joshi@sentry.io>
2025-06-11 14:39:55 -0700

    :bug: fix: typing

--- src/sentry/workflow_engine/processors/workflow.py
+++ src/sentry/workflow_engine/processors/workflow.py
@@ -7,7 +7,6 @@ from django.db import router, transaction
 from django.db.models import F, Q
 
 from sentry import buffer, features
-from sentry.db.models.manager.base_query_set import BaseQuerySet
 from sentry.eventstore.models import GroupEvent
 from sentry.models.environment import Environment
 from sentry.utils import json
@@ -138,7 +137,7 @@ def evaluate_workflow_triggers(
 def evaluate_workflows_action_filters(
     workflows: set[Workflow],
     event_data: WorkflowEventData,
-) -> BaseQuerySet[Action]:
+) -> set[DataConditionGroup]:
     filtered_action_groups: set[DataConditionGroup] = set()
     action_conditions = (
         DataConditionGroup.objects.filter(workflowdataconditiongroup__workflow__in=workflows)
