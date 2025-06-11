c96fe2dc0b0070af7fcba6cebad098f44d36b753
Martin Linzmayer <martin.linzmayer@sentry.io>
2025-06-11 13:15:06 +0200

    ref(symbolicator): only fetch gcp token if no private_key is configured (#93010)
    This PR changes the order of operations for obtaining GCP token. Instead
    of attempting to fetch it in all cases, it will only try to obtain it if
    no `private_key` is defined.
    This way we can ensure that previously configured projects work as
    before without having to unnecessary attempt to fetch the token.
    By restoring the old behaviour for sources with `client_email` and
    `private_key`, it allows us to remove the feature flag.

--- src/sentry/lang/native/sources.py
+++ src/sentry/lang/native/sources.py
@@ -574,11 +574,13 @@ def get_sources_for_project(project):
                 if other_source.get("type") == "alias":
                     yield from resolve_alias(other_source, organization)
                 else:
-                    yield fetch_token_for_gcp_source(other_source, organization)
+                    yield fetch_token_for_gcp_source_if_necessary(other_source, organization)
 
-    def fetch_token_for_gcp_source(source, organization):
-        if features.has("organizations:gcp-bearer-token-authentication", organization):
-            if source.get("type") == "gcs":
+    def fetch_token_for_gcp_source_if_necessary(source, organization):
+        if source.get("type") == "gcs":
+            if "client_email" in source and "private_key" in source:
+                return source
+            else:
                 client_email = source.get("client_email")
                 token = get_gcp_token(client_email)
                 # if target_credentials.token is None it means that the
@@ -609,7 +611,7 @@ def get_sources_for_project(project):
         if source.get("type") == "alias":
             sources.extend(resolve_alias(source, organization))
         else:
-            sources.append(fetch_token_for_gcp_source(source, organization))
+            sources.append(fetch_token_for_gcp_source_if_necessary(source, organization))
 
     return sources
 
