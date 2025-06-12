b6b3737369fe9fb37d35a33e8bc98046dc9d1a38
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-06-12 12:47:02 -0400

    fix(ui): Include link button in panelheader style exclusion (#93438)

--- static/app/components/panels/panelHeader.tsx
+++ static/app/components/panels/panelHeader.tsx
@@ -30,7 +30,7 @@ const PanelHeader = styled('div')<Props>`
   justify-content: space-between;
 
   /* Do not apply text styles to overlay elements such as dropdowns */
-  > *:not(:has([data-overlay], button), button),
+  > *:not(:has([data-overlay], button, a[role='button']), button, a[role='button']),
   &:not(:has(> *)) {
     color: ${p => (p.lightText ? p.theme.subText : p.theme.textColor)};
     font-size: ${p => p.theme.fontSizeSmall};
