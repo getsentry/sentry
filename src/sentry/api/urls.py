5c17012c5e189c52690a9fb2aca1f14bea688927
Vishnu Satish <vishnu.satish@sentry.io>
2025-06-12 09:51:16 -0700

    feat(feedback): API endpoint for AI feedback summary (#93134)
    Linear ticket:
    https://linear.app/getsentry/issue/REPLAY-292/api-endpoint-to-generate-summary-for-feedbacks
    
    With tests
    - Includes changes from https://github.com/getsentry/sentry/pull/92932
    (adding the `feedbacksummaries` usecase)
    - Q: Should the feature flag be added in this PR?
    - Todo: add the `feedbacksummaries` usecase with its respective model in
    getsentry options
    --
    - PR to add feedbacksummaries LLM usecase:
    https://github.com/getsentry/getsentry/pull/17647
    - PRs to add feature flag:
    https://github.com/getsentry/sentry/pull/93162 and
    https://github.com/getsentry/sentry-options-automator/pull/4115

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -15,6 +15,7 @@ from sentry.api.endpoints.organization_events_anomalies import OrganizationEvent
 from sentry.api.endpoints.organization_events_root_cause_analysis import (
     OrganizationEventsRootCauseAnalysisEndpoint,
 )
+from sentry.api.endpoints.organization_feedback_summary import OrganizationFeedbackSummaryEndpoint
 from sentry.api.endpoints.organization_fork import OrganizationForkEndpoint
 from sentry.api.endpoints.organization_insights_tree import OrganizationInsightsTreeEndpoint
 from sentry.api.endpoints.organization_member_invite.details import (
@@ -2062,6 +2063,11 @@ ORGANIZATION_URLS: list[URLPattern | URLResolver] = [
         r"^(?P<organization_id_or_slug>[^\/]+)/user-feedback/$",
         OrganizationUserReportsEndpoint.as_view(),
         name="sentry-api-0-organization-user-feedback",
+    ),
+    re_path(
+        r"^(?P<organization_id_or_slug>[^\/]+)/feedback-summary/$",
+        OrganizationFeedbackSummaryEndpoint.as_view(),
+        name="sentry-api-0-organization-user-feedback-summary",
     ),
     re_path(
         r"^(?P<organization_id_or_slug>[^\/]+)/user-teams/$",
