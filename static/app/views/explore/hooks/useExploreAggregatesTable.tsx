9310d04d7c7c0fd775baf78fa16d2b29214c3565
Tony Xiao <txiao@sentry.io>
2025-06-12 10:51:28 -0400

    feat(explore): Start storing explore saved queries using aggregate fields
    Storing it in aggregate fields allows us to preserve the order of the columns.

--- static/app/views/explore/hooks/useExploreAggregatesTable.tsx
+++ static/app/views/explore/hooks/useExploreAggregatesTable.tsx
@@ -6,11 +6,11 @@ import EventView from 'sentry/utils/discover/eventView';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import {
-  useExploreAggregateFields,
   useExploreDataset,
+  useExploreGroupBys,
   useExploreSortBys,
+  useExploreVisualizes,
 } from 'sentry/views/explore/contexts/pageParamsContext';
-import {isGroupBy} from 'sentry/views/explore/contexts/pageParamsContext/aggregateFields';
 import {formatSort} from 'sentry/views/explore/contexts/pageParamsContext/sortBys';
 import type {SpansRPCQueryExtras} from 'sentry/views/explore/hooks/useProgressiveQuery';
 import {useProgressiveQuery} from 'sentry/views/explore/hooks/useProgressiveQuery';
@@ -60,32 +60,33 @@ function useExploreAggregatesTableImp({
   const {selection} = usePageFilters();
 
   const dataset = useExploreDataset();
-  const aggregateFields = useExploreAggregateFields();
+  const groupBys = useExploreGroupBys();
   const sorts = useExploreSortBys();
+  const visualizes = useExploreVisualizes();
 
   const fields = useMemo(() => {
     // When rendering the table, we want the group bys first
     // then the aggregates.
     const allFields: string[] = [];
 
-    for (const aggregateField of aggregateFields) {
-      if (isGroupBy(aggregateField)) {
-        if (allFields.includes(aggregateField.groupBy)) {
+    for (const groupBy of groupBys) {
+      if (allFields.includes(groupBy)) {
+        continue;
+      }
+      allFields.push(groupBy);
+    }
+
+    for (const visualize of visualizes) {
+      for (const yAxis of visualize.yAxes) {
+        if (allFields.includes(yAxis)) {
           continue;
         }
-        allFields.push(aggregateField.groupBy);
-      } else {
-        for (const yAxis of aggregateField.yAxes) {
-          if (allFields.includes(yAxis)) {
-            continue;
-          }
-          allFields.push(yAxis);
-        }
+        allFields.push(yAxis);
       }
     }
 
     return allFields.filter(Boolean);
-  }, [aggregateFields]);
+  }, [groupBys, visualizes]);
 
   const eventView = useMemo(() => {
     const search = new MutableSearch(query);
