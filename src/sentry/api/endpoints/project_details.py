6736f904a18fd1c09b29277ea7a770866d29f61e
Katie Byers <lobsterkatie@gmail.com>
2025-06-11 17:59:25 -0700

    disallow grouping config changes

--- src/sentry/api/endpoints/project_details.py
+++ src/sentry/api/endpoints/project_details.py
@@ -1,6 +1,4 @@
 import logging
-import math
-import time
 from datetime import timedelta
 from uuid import uuid4
 
@@ -355,22 +353,15 @@ E.g. `['release', 'environment']`""",
 
         return value
 
+    # TODO: Once these have been in place for a while we can probably remove them
+    def validate_groupingConfig(self, value):
+        raise serializers.ValidationError("Grouping config cannot be manually set.")
+
+    def validate_secondaryGroupingConfig(self, value):
+        raise serializers.ValidationError("Secondary grouping config cannot be manually set.")
+
     def validate_secondaryGroupingExpiry(self, value):
-        if not isinstance(value, (int, float)) or math.isnan(value):
-            raise serializers.ValidationError(
-                f"Grouping expiry must be a numerical value, a UNIX timestamp with second resolution, found {type(value)}"
-            )
-        now = time.time()
-        if value < now:
-            raise serializers.ValidationError(
-                "Grouping expiry must be sometime within the next 90 days and not in the past. Perhaps you specified the timestamp not in seconds?"
-            )
-
-        max_expiry_date = now + (91 * 24 * 3600)
-        if value > max_expiry_date:
-            value = max_expiry_date
-
-        return value
+        raise serializers.ValidationError("Secondary grouping expiry cannot be manually set.")
 
     def validate_fingerprintingRules(self, value):
         if not value:
@@ -659,9 +650,6 @@ class ProjectDetailsEndpoint(ProjectEndpoint):
         if result.get("scrubIPAddresses") is not None:
             if project.update_option("sentry:scrub_ip_address", result["scrubIPAddresses"]):
                 changed_proj_settings["sentry:scrub_ip_address"] = result["scrubIPAddresses"]
-        if result.get("groupingConfig") is not None:
-            if project.update_option("sentry:grouping_config", result["groupingConfig"]):
-                changed_proj_settings["sentry:grouping_config"] = result["groupingConfig"]
         if result.get("groupingEnhancements") is not None:
             if project.update_option(
                 "sentry:grouping_enhancements", result["groupingEnhancements"]
@@ -672,20 +660,6 @@ class ProjectDetailsEndpoint(ProjectEndpoint):
         if result.get("fingerprintingRules") is not None:
             if project.update_option("sentry:fingerprinting_rules", result["fingerprintingRules"]):
                 changed_proj_settings["sentry:fingerprinting_rules"] = result["fingerprintingRules"]
-        if result.get("secondaryGroupingConfig") is not None:
-            if project.update_option(
-                "sentry:secondary_grouping_config", result["secondaryGroupingConfig"]
-            ):
-                changed_proj_settings["sentry:secondary_grouping_config"] = result[
-                    "secondaryGroupingConfig"
-                ]
-        if result.get("secondaryGroupingExpiry") is not None:
-            if project.update_option(
-                "sentry:secondary_grouping_expiry", result["secondaryGroupingExpiry"]
-            ):
-                changed_proj_settings["sentry:secondary_grouping_expiry"] = result[
-                    "secondaryGroupingExpiry"
-                ]
         if result.get("securityToken") is not None:
             if project.update_option("sentry:token", result["securityToken"]):
                 changed_proj_settings["sentry:token"] = result["securityToken"]
