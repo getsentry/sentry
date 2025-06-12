04002c5d9b28630ee163151cac9946f8ea03d6ee
Scott Cooper <scttcper@gmail.com>
2025-06-12 14:46:45 -0700

    add description

--- static/app/utils/flatten.tsx
+++ static/app/utils/flatten.tsx
@@ -10,4 +10,9 @@
 
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
+/**
+ * Useful to flatten the type output to improve type hints shown in editors.
+ * And also to transform an interface into a type to aide with assignability.
+ * see more examples @link https://github.com/sindresorhus/type-fest/blob/main/source/simplify.d.ts
+ */
 export type Flatten<T> = {[KeyType in keyof T]: T[KeyType]} & {};
