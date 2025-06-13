efa89aeb007a95663e6247dee7a9792dcabcd141
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-06-13 10:56:17 -0400

    feat(notif-platform): Implement a notification provider registry (#93119)
    Follows the scheme from workflow of using a decorator for registration.
    Registers the classes behind the providers rather than instances (as
    described in the spec).
    
    I did see some existing types for a few registrations (Slack and
    MSTeams) but since this they're loaded on app init, they were having
    circular import issues. Gonna work through that when I start digging
    into individual providers, so skipping it for now,.

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -408,6 +408,7 @@ INSTALLED_APPS: tuple[str, ...] = (
     "sentry.sentry_apps",
     "sentry.integrations",
     "sentry.notifications",
+    "sentry.notifications.platform",
     "sentry.flags",
     "sentry.monitors",
     "sentry.uptime",
