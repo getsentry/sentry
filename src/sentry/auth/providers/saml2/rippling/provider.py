d2e4b17ba8ef88539dbd06cd134abf82d3b8f6b6
Markus Unterwaditzer <markus-github@unterwaditzer.net>
2025-06-13 11:37:16 +0200

    Revert "ref: fix method name for AuthView (#93459)"
    This reverts commit 5a7cb541f6ec9dbb9ece7deda7ccacdd5fb295ef.

--- src/sentry/auth/providers/saml2/rippling/provider.py
+++ src/sentry/auth/providers/saml2/rippling/provider.py
@@ -18,7 +18,7 @@ class WaitForCompletion(AuthView):
     This is simply an extra step to wait for them to complete that.
     """
 
-    def dispatch(self, request: HttpRequest, helper) -> HttpResponseBase:
+    def handle(self, request: HttpRequest, helper) -> HttpResponseBase:
         if "continue_setup" in request.POST:
             return helper.next_step()
 
