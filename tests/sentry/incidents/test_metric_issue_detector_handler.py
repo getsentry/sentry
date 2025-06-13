f646557a2b167afd3558d8825b0ac2124ef8695e
Colleen O'Rourke <colleen@sentry.io>
2025-06-13 11:18:11 -0700

    break tests out into own files

--- tests/sentry/incidents/test_metric_issue_detector_handler.py
+++ tests/sentry/incidents/test_metric_issue_detector_handler.py
@@ -1,19 +1,13 @@
 from datetime import UTC, datetime, timedelta
-from unittest.mock import patch
 
-from sentry import eventstore
-from sentry.eventstream.types import EventStreamEventType
-from sentry.incidents.grouptype import MetricIssue, MetricIssueDetectorHandler
+from sentry.incidents.grouptype import MetricIssueDetectorHandler
 from sentry.incidents.utils.constants import INCIDENTS_SNUBA_SUBSCRIPTION_TYPE
 from sentry.incidents.utils.types import QuerySubscriptionUpdate
 from sentry.issues.issue_occurrence import IssueOccurrence
-from sentry.issues.producer import PayloadType, produce_occurrence_to_kafka
 from sentry.snuba.dataset import Dataset
 from sentry.snuba.models import SnubaQuery, SnubaQueryEventType
 from sentry.snuba.subscriptions import create_snuba_query, create_snuba_subscription
-from sentry.tasks.post_process import post_process_group
 from sentry.testutils.helpers.datetime import freeze_time
-from sentry.testutils.helpers.features import with_feature
 from sentry.workflow_engine.models import DataCondition, DataPacket
 from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.types import (
@@ -21,18 +15,18 @@ from sentry.workflow_engine.types import (
     DetectorGroupKey,
     DetectorPriorityLevel,
 )
-from tests.sentry.workflow_engine.test_base import BaseWorkflowTest
+from tests.sentry.workflow_engine.handlers.detector.test_base import BaseDetectorHandlerTest
 
 
 @freeze_time()
-class TestEvaluateMetricDetector(BaseWorkflowTest):
+class TestEvaluateMetricDetector(BaseDetectorHandlerTest):
     def setUp(self):
         super().setUp()
         self.detector_group_key = None
         self.detector = self.create_detector(
             project=self.project,
             workflow_condition_group=self.create_data_condition_group(),
-            type=MetricIssue.slug,
+            type="handler_with_state",
             created_by_id=self.user.id,
         )
         self.critical_detector_trigger = self.create_data_condition(
@@ -132,56 +126,6 @@ class TestEvaluateMetricDetector(BaseWorkflowTest):
         assert occurrence.priority == self.critical_detector_trigger.condition_result
         assert occurrence.assignee
         assert occurrence.assignee.id == self.detector.created_by_id
-
-    @with_feature("organizations:issue-metric-issue-ingest")
-    @with_feature("organizations:issue-metric-issue-post-process-group")
-    @with_feature("organizations:workflow-engine-metric-alert-processing")
-    @with_feature("organizations:workflow-engine-process-metric-issue-workflows")
-    @patch("sentry.workflow_engine.processors.workflow.process_workflows")
-    def test_occurrence_post_process(self, mock_process_workflows):
-        value = self.critical_detector_trigger.comparison + 1
-        packet = QuerySubscriptionUpdate(
-            entity="entity",
-            subscription_id=str(self.query_subscription.id),
-            values={"value": value},
-            timestamp=datetime.now(UTC),
-        )
-        data_packet = DataPacket[QuerySubscriptionUpdate](
-            source_id=str(self.query_subscription.id), packet=packet
-        )
-        result: dict[DetectorGroupKey, DetectorEvaluationResult] = self.handler.evaluate(
-            data_packet
-        )
-        evaluation_result: DetectorEvaluationResult = result[self.detector_group_key]
-        assert isinstance(evaluation_result.result, IssueOccurrence)
-        occurrence: IssueOccurrence = evaluation_result.result
-
-        assert occurrence is not None
-
-        produce_occurrence_to_kafka(
-            payload_type=PayloadType.OCCURRENCE,
-            occurrence=occurrence,
-            event_data=evaluation_result.event_data,
-        )
-        occurrence.save()
-        stored_occurrence = IssueOccurrence.fetch(occurrence.id, occurrence.project_id)
-        assert stored_occurrence
-        event = eventstore.backend.get_event_by_id(
-            occurrence.project_id, stored_occurrence.event_id
-        )
-        assert event
-
-        post_process_group(
-            is_new=True,
-            is_regression=False,
-            is_new_group_environment=True,
-            cache_key=None,
-            occurrence_id=occurrence.id,
-            group_id=event.group_id,
-            project_id=occurrence.project_id,
-            eventstream_type=EventStreamEventType.Generic.value,
-        )
-        assert mock_process_workflows.call_count == 1
 
     def test_warning_level(self):
         value = self.warning_detector_trigger.comparison + 1
