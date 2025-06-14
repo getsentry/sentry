ef531065cbf112756aeb7c1fd984ebaf44cdb623
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-06-13 17:36:45 -0700

    couple more mypy errors

--- src/sentry/workflow_engine/processors/action.py
+++ src/sentry/workflow_engine/processors/action.py
@@ -11,6 +11,7 @@ from django.utils import timezone
 from sentry import features
 from sentry.constants import ObjectStatus
 from sentry.db.models.manager.base_query_set import BaseQuerySet
+from sentry.eventstore.models import GroupEvent
 from sentry.exceptions import NotRegistered
 from sentry.integrations.base import IntegrationFeatures
 from sentry.integrations.manager import default_manager as integrations_manager
@@ -90,11 +91,16 @@ def create_workflow_fire_histories(
         ).values_list("workflow_id", flat=True)
     )
 
+    event_id = (
+        event_data.event.event_id
+        if isinstance(event_data.event, GroupEvent)
+        else event_data.event.id
+    )
     fire_histories = [
         WorkflowFireHistory(
             workflow_id=workflow_id,
             group=event_data.event.group,
-            event_id=event_data.event.event_id,
+            event_id=event_id,
         )
         for workflow_id in workflow_ids
     ]
@@ -116,7 +122,7 @@ def filter_recently_fired_actions(
         .distinct()
     )
 
-    group = event_data.event.group
+    group = event_data.group
 
     now = timezone.now()
     statuses = get_action_last_updated_statuses(now, actions, group)
@@ -254,7 +260,7 @@ def filter_recently_fired_workflow_actions(
 
     action_to_statuses = get_workflow_action_group_statuses(
         action_to_workflows_ids=action_to_workflows_ids,
-        group=event_data.event.group,
+        group=event_data.group,
         workflow_ids=workflow_ids,
     )
     now = timezone.now()
@@ -263,7 +269,7 @@ def filter_recently_fired_workflow_actions(
             action_to_workflows_ids=action_to_workflows_ids,
             action_to_statuses=action_to_statuses,
             workflows=workflows,
-            group=event_data.event.group,
+            group=event_data.group,
             now=now,
         )
     )
@@ -348,7 +354,7 @@ def _get_integration_features(action_type: Action.Type) -> frozenset[Integration
     return integration.features
 
 
-# The features that are relevent to Action behaviors;
+# The features that are relevant to Action behaviors;
 # if the organization doesn't have access to all of the features an integration
 # requires that are in this list, the action should not be permitted.
 _ACTION_RELEVANT_INTEGRATION_FEATURES = {
