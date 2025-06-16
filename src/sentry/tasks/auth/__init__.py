1a814c0d7ba6edfd235e8ef1e102cbf1f289bb08
Mark Story <mark@mark-story.com>
2025-06-16 13:53:59 -0400

    fix(taskworker) Remove redundant namespace definition (#93602)
    This was resulting in tasks not being found.
    
    Fixes SENTRY-419F
    
    ---------
    
    Co-authored-by: Enoch Tang <enoch.tang@sentry.io>

--- src/sentry/tasks/auth/__init__.py
+++
@@ -1,4 +0,0 @@
-from sentry.taskworker.registry import TaskNamespace, taskregistry
-
-auth_tasks: TaskNamespace = taskregistry.create_namespace("auth")
-auth_control_tasks: TaskNamespace = taskregistry.create_namespace("auth.control")
