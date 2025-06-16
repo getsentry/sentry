ded3098f9f6fc902b38eae292ec5c2c69a9743d6
Anton Pirker <anton.pirker@sentry.io>
2025-06-16 17:04:28 +0200

    let test access db

--- tests/sentry/api/endpoints/test_index.py
+++ tests/sentry/api/endpoints/test_index.py
@@ -1,3 +1,4 @@
+import pytest
 from django.urls import reverse
 
 from sentry.models.apikey import ApiKey
@@ -6,6 +7,7 @@ from sentry.testutils.cases import APITestCase
 from sentry.testutils.silo import control_silo_test
 
 
+@pytest.mark.django_db
 @control_silo_test
 def test_anonymous(client):
     response = client.get(reverse("sentry-api-index"))
