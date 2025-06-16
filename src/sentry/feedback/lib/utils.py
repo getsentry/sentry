ebb96db8d6661cb6a61f7090d4d1c1ba4cebe40f
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-06-16 15:13:38 -0700

    Add log, update metric and outcome reason

--- src/sentry/feedback/lib/utils.py
+++ src/sentry/feedback/lib/utils.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 
 from django.conf import settings
@@ -15,6 +16,8 @@ feedback_rate_limiter = RedisSlidingWindowRateLimiter(
     **settings.SENTRY_USER_FEEDBACK_RATE_LIMITER_OPTIONS
 )
 
+logger = logging.getLogger(__name__)
+
 
 def check_feedback_quota_granted(
     project_id: int,
@@ -23,7 +26,7 @@ def check_feedback_quota_granted(
     dt: datetime | None = None,
 ) -> bool:
     """
-    Checks the feedback rate limiter, emitting an outcome, metric, and returning False if exceeded.
+    Checks the feedback rate limiter, emitting an outcome, metric, log, and returning False if exceeded.
     We apply this in creation sources to prevent abuse of the feedback system and protect downstream infra.
     """
     granted_quota = feedback_rate_limiter.check_and_use_quotas(
@@ -43,13 +46,24 @@ def check_feedback_quota_granted(
             project_id=project_id,
             key_id=None,
             outcome=Outcome.RATE_LIMITED,
-            reason="group_type_rate_limit",
+            reason="feedback_abuse_quota",
             timestamp=dt or timezone.now(),
             event_id=event_id,
             category=DataCategory.USER_REPORT_V2,
             quantity=1,
         )
-        metrics.incr("feedback.rate_limited", tags={"referrer": source.value})
+
+        metrics.incr("feedback.abuse_quota_exceeded", tags={"referrer": source.value})
+        logger.warning(
+            "Feedback abuse quota exceeded",
+            extra={
+                "project_id": project_id,
+                "project_slug": project.slug,
+                "organization_id": project.organization_id,
+                "organization_slug": project.organization.slug,
+                "source": source.value,
+            },
+        )
         return False
 
     return True
