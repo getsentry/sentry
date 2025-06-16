7abd7dbd587cc5daca79b573c7b32d1319fc68fd
Michelle Fu <michelle.fu@sentry.io>
2025-06-16 13:09:31 -0700

    show modal on launch

--- static/app/components/modals/missingPrimaryEmailModal.tsx
+++ static/app/components/modals/missingPrimaryEmailModal.tsx
@@ -1,8 +1,10 @@
 import {Fragment} from 'react';
+import styled from '@emotion/styled';
 
 import type {ModalRenderProps} from 'sentry/actionCreators/modal';
 import {LinkButton} from 'sentry/components/core/button/linkButton';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import TextBlock from 'sentry/views/settings/components/text/textBlock';
 
 type Props = Pick<ModalRenderProps, 'Body' | 'Header'>;
@@ -10,14 +12,17 @@ type Props = Pick<ModalRenderProps, 'Body' | 'Header'>;
 function MissingPrimaryEmailModal({Header, Body}: Props) {
   return (
     <Fragment>
-      <Header closeButton>{t('Action Required')}</Header>
+      <Header>
+        <Heading>{t('Action Required')}</Heading>
+      </Header>
       <Body>
+        <TextBlock>{t('Your account is missing a primary email address.')}</TextBlock>
         <TextBlock>
           {t(
-            'Your account is missing a primary email address.\nWe require all Sentry users to have a primary email address. Please add a primary email address to your account within User Settings. If you do not do so within 30 days, your account will be deleted.'
+            ' We require all Sentry users to have a primary email address. Please add a primary email address to your account within User Settings. If you do not do so within 30 days, your account will be deleted.'
           )}
         </TextBlock>
-        <LinkButton to={`/settings/account/emails/`}>
+        <LinkButton to={`/settings/account/emails/`} priority="primary">
           {t('Go to User Settings')}
         </LinkButton>
       </Body>
@@ -26,3 +31,10 @@ function MissingPrimaryEmailModal({Header, Body}: Props) {
 }
 
 export default MissingPrimaryEmailModal;
+
+const Heading = styled('h1')`
+  font-weight: ${p => p.theme.fontWeightNormal};
+  font-size: ${p => p.theme.headerFontSize};
+  margin-top: 0;
+  margin-bottom: ${space(0.75)};
+`;
