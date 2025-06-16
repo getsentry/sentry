904c962e762a8a615f4d68fc11291e954da79b90
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2025-06-16 13:54:48 -0400

    fix(insights): open in explore/alerts query wrong (#93632)
    Should be greater then and equal to so that it includes the right codes

--- static/app/views/insights/common/components/widgets/httpDomainSummaryResponseCodesChartWidget.tsx
+++ static/app/views/insights/common/components/widgets/httpDomainSummaryResponseCodesChartWidget.tsx
@@ -50,17 +50,17 @@ export default function HttpDomainSummaryResponseCodesChartWidget(
     {
       yAxes: ['count()'],
       label: '3xx',
-      query: `${stringifiedSearch} ${responseRateField}:>300 ${responseRateField}:<=399`,
+      query: `${stringifiedSearch} ${responseRateField}:>=300 ${responseRateField}:<=399`,
     },
     {
       yAxes: ['count()'],
       label: '4xx',
-      query: `${stringifiedSearch} ${responseRateField}:>400 ${responseRateField}:<=499`,
+      query: `${stringifiedSearch} ${responseRateField}:>=400 ${responseRateField}:<=499`,
     },
     {
       yAxes: ['count()'],
       label: '5xx',
-      query: `${stringifiedSearch} ${responseRateField}:>500 ${responseRateField}:<=599`,
+      query: `${stringifiedSearch} ${responseRateField}:>=500 ${responseRateField}:<=599`,
     },
   ];
 
