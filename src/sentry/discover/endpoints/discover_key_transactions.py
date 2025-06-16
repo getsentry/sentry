f0dca9ec1e7c3488c31ea81de86601b3a0704004
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-06-16 10:40:05 -0400

    user id check

--- src/sentry/discover/endpoints/discover_key_transactions.py
+++ src/sentry/discover/endpoints/discover_key_transactions.py
@@ -108,22 +108,23 @@ class KeyTransactionEndpoint(KeyTransactionBase):
                             for project_team in unkeyed_project_teams
                         ]
                     )
-                    try:
-                        InsightsStarredSegment.objects.bulk_create(
-                            [
-                                InsightsStarredSegment.objects.create(
-                                    organization=organization,
-                                    user_id=request.user.id,
-                                    project=project,
-                                    segment_name=data["transaction"],
-                                    project=project_team.project,
-                                )
-                                for project_team in unkeyed_project_teams
-                            ]
-                        )
-                    except IntegrityError:
-                        # We don't want the request to fail if the double write failed
-                        return Response(status=201)
+                    if request.user.id:
+                        try:
+                            InsightsStarredSegment.objects.bulk_create(
+                                [
+                                    InsightsStarredSegment.objects.create(
+                                        organization=organization,
+                                        user_id=request.user.id,
+                                        project=project,
+                                        segment_name=data["transaction"],
+                                        project=project_team.project,
+                                    )
+                                    for project_team in unkeyed_project_teams
+                                ]
+                            )
+                        except IntegrityError:
+                            # We don't want the request to fail if the double write failed
+                            return Response(status=201)
                     return Response(status=201)
                 # Even though we tried to avoid it, the TeamKeyTransaction was created already
                 except IntegrityError:
