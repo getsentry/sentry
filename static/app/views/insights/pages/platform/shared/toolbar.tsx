946b0bdb1553a8ce09c159e702ea3a0b2ce9827d
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-06-16 15:04:20 -0400

    code

--- static/app/views/insights/pages/platform/shared/toolbar.tsx
+++ static/app/views/insights/pages/platform/shared/toolbar.tsx
@@ -1,12 +1,15 @@
 import {Button} from 'sentry/components/core/button';
-import {DropdownMenu} from 'sentry/components/dropdownMenu';
-import {IconEllipsis, IconExpand} from 'sentry/icons';
+import {IconExpand} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
+import {Dataset} from 'sentry/views/alerts/rules/metric/types';
 import {Widget} from 'sentry/views/dashboards/widgets/widget/widget';
 import {getExploreUrl} from 'sentry/views/explore/utils';
+import {BaseChartActionDropdown} from 'sentry/views/insights/common/components/chartActionDropdown';
 import type {LoadableChartWidgetProps} from 'sentry/views/insights/common/components/widgets/types';
+import {getAlertsUrl} from 'sentry/views/insights/common/utils/getAlertsUrl';
+import {useAlertsProject} from 'sentry/views/insights/common/utils/useAlertsProject';
 
 type ExploreParams = Parameters<typeof getExploreUrl>[0];
 
@@ -19,33 +22,38 @@ interface ToolbarProps {
 export function Toolbar({exploreParams, onOpenFullScreen, loaderSource}: ToolbarProps) {
   const organization = useOrganization();
   const {selection} = usePageFilters();
+  const project = useAlertsProject();
+
   const exploreUrl =
     exploreParams && getExploreUrl({...exploreParams, organization, selection});
+
+  const yAxes = exploreParams?.visualize?.flatMap(v => v.yAxes) || [];
+
+  const alertsUrls = yAxes.map((yAxis, index) => {
+    return {
+      key: `${yAxis}-${index}`,
+      label: yAxis,
+      to: getAlertsUrl({
+        project,
+        query: exploreParams?.query,
+        dataset: Dataset.EVENTS_ANALYTICS_PLATFORM,
+        pageFilters: selection,
+        aggregate: yAxis,
+        organization,
+      }),
+    };
+  });
 
   return (
     <Widget.WidgetToolbar>
       {exploreUrl ? (
-        <DropdownMenu
-          size="xs"
-          position="bottom-end"
-          trigger={triggerProps => (
-            <Button
-              {...triggerProps}
-              size="xs"
-              borderless
-              icon={<IconEllipsis />}
-              aria-label={t('More actions')}
-            />
-          )}
-          items={[
-            {
-              key: 'open-in-explore',
-              label: t('Open in Explore'),
-              to: exploreUrl,
-            },
-          ]}
+        <BaseChartActionDropdown
+          exploreUrl={exploreUrl}
+          alertMenuOptions={alertsUrls}
+          referrer={loaderSource || 'insights.platform.toolbar'}
         />
       ) : null}
+
       {loaderSource !== 'releases-drawer' && (
         <Button
           size="xs"
