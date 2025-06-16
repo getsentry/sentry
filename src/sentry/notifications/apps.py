fd6fe82d6d46bb7a791e60de36c8e321df379bf7
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-06-16 12:31:33 -0400

    ♻️ Use existing notifications app, instead of platform specific one

--- src/sentry/notifications/apps.py
+++ src/sentry/notifications/apps.py
@@ -5,4 +5,8 @@ class Config(AppConfig):
     name = "sentry.notifications"
 
     def ready(self):
-        pass
+        # Register the NotificationProviders for the platform
+        from .platform.discord.provider import DiscordNotificationProvider  # NOQA
+        from .platform.email.provider import EmailNotificationProvider  # NOQA
+        from .platform.msteams.provider import MSTeamsNotificationProvider  # NOQA
+        from .platform.slack.provider import SlackNotificationProvider  # NOQA
