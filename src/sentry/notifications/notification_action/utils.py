f2085a215f540411fba22c751082d6baa95e2d61
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-06-16 17:54:13 -0700

    update notification action to use activity.send_notification and all the types

--- src/sentry/notifications/notification_action/utils.py
+++ src/sentry/notifications/notification_action/utils.py
@@ -1,5 +1,6 @@
 import logging
 
+from sentry.models.activity import Activity
 from sentry.notifications.notification_action.registry import (
     group_type_notification_registry,
     issue_alert_handler_registry,
@@ -12,11 +13,23 @@ logger = logging.getLogger(__name__)
 
 
 def execute_via_group_type_registry(
-    job: WorkflowEventData, action: Action, detector: Detector
+    event_data: WorkflowEventData, action: Action, detector: Detector
 ) -> None:
+    """
+    Generic "notification action handler" this method will lookup which registry
+    to send the notification to, based on the type of detector that created it.
+
+    This currently only supported detector types: 'error', 'metric_issue'
+
+    If an `Activity` model for a `Group` is provided in the event data
+    it will send an activity notification instead.
+    """
+    if isinstance(event_data.event, Activity):
+        return event_data.event.send_notification()
+
     try:
         handler = group_type_notification_registry.get(detector.type)
-        handler.handle_workflow_action(job, action, detector)
+        handler.handle_workflow_action(event_data, action, detector)
     except NoRegistrationExistsError:
         logger.exception(
             "No notification handler found for detector type: %s",
