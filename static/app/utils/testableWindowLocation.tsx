4eb224b3cc45f8c4ee5d24be749466844c2faf3c
Scott Cooper <scttcper@gmail.com>
2025-06-17 11:03:55 -0700

    limit functionality via types

--- static/app/utils/testableWindowLocation.tsx
+++ static/app/utils/testableWindowLocation.tsx
@@ -1,4 +1,8 @@
 /**
  * This object is mocked in tests to work around JSDOM's non configurable location.
  */
-export const testableWindowLocation = window.location;
+export const testableWindowLocation = window.location as Pick<
+  typeof window.location,
+  // Use `setWindowLocation` to modify the window.location.pathname instead of mocking properties on the testableLocation object
+  'assign' | 'replace' | 'reload'
+>;
