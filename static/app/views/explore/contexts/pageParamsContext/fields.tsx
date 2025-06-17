183a00b55b5c4fa29c0b5ce619d4bb5f9e483709
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-06-17 14:48:28 -0400

    Set OTel default fields if feature-flaggin

--- static/app/views/explore/contexts/pageParamsContext/fields.tsx
+++ static/app/views/explore/contexts/pageParamsContext/fields.tsx
@@ -1,9 +1,14 @@
 import type {Location} from 'history';
 
+import type {Organization} from 'sentry/types/organization';
 import {defined} from 'sentry/utils';
 import {decodeList} from 'sentry/utils/queryString';
 
-export function defaultFields(): string[] {
+export function defaultFields(organization?: Organization): string[] {
+  if (organization?.features.includes('performance-otel-friendly-ui')) {
+    return ['id', 'name', 'span.duration', 'timestamp'];
+  }
+
   return [
     'id',
     'span.op',
@@ -14,14 +19,17 @@ export function defaultFields(): string[] {
   ];
 }
 
-export function getFieldsFromLocation(location: Location): string[] {
+export function getFieldsFromLocation(
+  location: Location,
+  organization?: Organization
+): string[] {
   const fields = decodeList(location.query.field);
 
   if (fields.length) {
     return fields;
   }
 
-  return defaultFields();
+  return defaultFields(organization);
 }
 
 export function updateLocationWithFields(
