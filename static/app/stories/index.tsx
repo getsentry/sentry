92a27536910d83332aca066c212b731006d469f4
Jonas <jonas.badalic@sentry.io>
2025-06-17 09:19:01 -0400

    storybook: export APIReference on storybook namespace (#92496)
    Export APIReference just like the other components

--- static/app/stories/index.tsx
+++ static/app/stories/index.tsx
@@ -5,3 +5,4 @@ export {SideBySide} from './layout';
 export {SizingWindow, Grid} from './layout';
 export {story} from './storybook';
 export {ThemeSwitcher, ThemeToggle} from './theme';
+export {APIReference} from './apiReference';
