ff3b19b52804fc70f35f0e96716d5102b6194f56
Billy Vong <billyvg@users.noreply.github.com>
2025-06-17 12:48:50 -0400

    feat(replay): Debug replay summarizer failure
    In Seer access logs, Seer responds with a 200, but this endpoint is somehow returning a `ParseError`. Add some logging when status_code is not 200.

--- src/sentry/replays/endpoints/project_replay_summarize_breadcrumbs.py
+++ src/sentry/replays/endpoints/project_replay_summarize_breadcrumbs.py
@@ -6,7 +6,6 @@ import requests
 import sentry_sdk
 from django.conf import settings
 from drf_spectacular.utils import extend_schema
-from rest_framework.exceptions import ParseError
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -78,8 +77,18 @@ def make_seer_request(request_data: str) -> bytes:
             **sign_with_seer_secret(request_data.encode()),
         },
     )
+
     if response.status_code != 200:
-        raise ParseError("A summary could not be produced at this time.")
+        sentry_sdk.logger.warning(
+            "Replay: Failed to produce a summary for a replay breadcrumbs request",
+            extra={
+                "status_code": response.status_code,
+                "response": response.text,
+                "content": response.content,
+            },
+        )
+
+    response.raise_for_status()
 
     return response.content
 
