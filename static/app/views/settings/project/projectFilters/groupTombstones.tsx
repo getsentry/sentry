6a24ba6ea9e438f8a979e533a468b03a9c3e09b9
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-06-18 07:02:04 +0200

    hide new changes behind feature flag

--- static/app/views/settings/project/projectFilters/groupTombstones.tsx
+++ static/app/views/settings/project/projectFilters/groupTombstones.tsx
@@ -7,17 +7,21 @@ import Confirm from 'sentry/components/confirm';
 import {UserAvatar} from 'sentry/components/core/avatar/userAvatar';
 import {Button} from 'sentry/components/core/button';
 import Count from 'sentry/components/count';
+import EmptyMessage from 'sentry/components/emptyMessage';
 import ErrorBoundary from 'sentry/components/errorBoundary';
 import EventOrGroupHeader from 'sentry/components/eventOrGroupHeader';
 import LoadingError from 'sentry/components/loadingError';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
 import Pagination from 'sentry/components/pagination';
 import Panel from 'sentry/components/panels/panel';
+import PanelItem from 'sentry/components/panels/panelItem';
 import {PanelTable} from 'sentry/components/panels/panelTable';
 import TimeSince from 'sentry/components/timeSince';
 import {IconDelete} from 'sentry/icons';
 import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
 import type {GroupTombstone} from 'sentry/types/group';
+import type {Organization} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
 import {defined} from 'sentry/utils';
 import {useApiQuery} from 'sentry/utils/queryClient';
@@ -31,11 +35,79 @@ interface GroupTombstoneRowProps {
   onUndiscard: (id: string) => void;
 }
 
+function hasGrouptombstonesHitCounter(organization: Organization) {
+  return organization.features.includes('grouptombstones-hit-counter');
+}
+
 function GroupTombstoneRow({data, disabled, onUndiscard}: GroupTombstoneRowProps) {
+  const organization = useOrganization();
+
   const actor = data.actor;
 
+  if (hasGrouptombstonesHitCounter(organization)) {
+    return (
+      <Fragment>
+        <StyledBox>
+          <EventOrGroupHeader
+            hideIcons
+            data={{...data, isTombstone: true}}
+            source="group-tombstome"
+          />
+        </StyledBox>
+        <RightAlignedColumn>
+          {data.lastSeen ? (
+            <TimeSince
+              date={data.lastSeen}
+              unitStyle="short"
+              suffix="ago"
+              disabledAbsoluteTooltip
+            />
+          ) : (
+            '-'
+          )}
+        </RightAlignedColumn>
+        <RightAlignedColumn>
+          {defined(data.timesSeen) ? <Count value={data.timesSeen} /> : '-'}
+        </RightAlignedColumn>
+        <CenteredAlignedColumn>
+          {actor ? (
+            <UserAvatar
+              user={actor}
+              hasTooltip
+              tooltip={t('Discarded by %s', actor.name || actor.email)}
+            />
+          ) : (
+            '-'
+          )}
+        </CenteredAlignedColumn>
+        <CenteredAlignedColumn>
+          <Confirm
+            message={t(
+              'Undiscarding this issue means that incoming events that match this will no longer be discarded. New incoming events will count toward your event quota and will display on your issues dashboard. Are you sure you wish to continue?'
+            )}
+            onConfirm={() => onUndiscard(data.id)}
+            disabled={disabled}
+          >
+            <Button
+              type="button"
+              aria-label={t('Undiscard')}
+              title={
+                disabled
+                  ? t('You do not have permission to perform this action')
+                  : t('Undiscard')
+              }
+              size="sm"
+              icon={<IconDelete />}
+              disabled={disabled}
+            />
+          </Confirm>
+        </CenteredAlignedColumn>
+      </Fragment>
+    );
+  }
+
   return (
-    <Fragment>
+    <PanelItem center>
       <StyledBox>
         <EventOrGroupHeader
           hideIcons
@@ -43,33 +115,16 @@ function GroupTombstoneRow({data, disabled, onUndiscard}: GroupTombstoneRowProps
           source="group-tombstome"
         />
       </StyledBox>
-      <RightAlignedColumn>
-        {data.lastSeen ? (
-          <TimeSince
-            date={data.lastSeen}
-            unitStyle="short"
-            suffix="ago"
-            disabledAbsoluteTooltip
-          />
-        ) : (
-          '-'
-        )}
-      </RightAlignedColumn>
-      <RightAlignedColumn>
-        {defined(data.timesSeen) ? <Count value={data.timesSeen} /> : '-'}
-      </RightAlignedColumn>
-      <CenteredAlignedColumn>
-        {actor ? (
+      <AvatarContainer>
+        {actor && (
           <UserAvatar
             user={actor}
             hasTooltip
             tooltip={t('Discarded by %s', actor.name || actor.email)}
           />
-        ) : (
-          '-'
         )}
-      </CenteredAlignedColumn>
-      <CenteredAlignedColumn>
+      </AvatarContainer>
+      <ActionContainer>
         <Confirm
           message={t(
             'Undiscarding this issue means that incoming events that match this will no longer be discarded. New incoming events will count toward your event quota and will display on your issues dashboard. Are you sure you wish to continue?'
@@ -85,13 +140,13 @@ function GroupTombstoneRow({data, disabled, onUndiscard}: GroupTombstoneRowProps
                 ? t('You do not have permission to perform this action')
                 : t('Undiscard')
             }
-            size="sm"
+            size="xs"
             icon={<IconDelete />}
             disabled={disabled}
           />
         </Confirm>
-      </CenteredAlignedColumn>
-    </Fragment>
+      </ActionContainer>
+    </PanelItem>
   );
 }
 
@@ -149,31 +204,56 @@ function GroupTombstones({project}: GroupTombstonesProps) {
     return <LoadingError onRetry={refetch} />;
   }
 
+  if (!tombstones?.length && !hasGrouptombstonesHitCounter(organization)) {
+    return (
+      <Panel>
+        <EmptyMessage>{t('You have no discarded issues')}</EmptyMessage>
+      </Panel>
+    );
+  }
+
   return (
     <ErrorBoundary>
       <Access access={['project:write']} project={project}>
         {({hasAccess}) => (
           <Fragment>
-            <StyledPanelTable
-              headers={[
-                <LeftAlignedColumn key="lastSeen">{t('Issue')}</LeftAlignedColumn>,
-                <RightAlignedColumn key="lastSeen">{t('Last Seen')}</RightAlignedColumn>,
-                <RightAlignedColumn key="events">{t('Events')}</RightAlignedColumn>,
-                <CenteredAlignedColumn key="member">{t('Member')}</CenteredAlignedColumn>,
-                <CenteredAlignedColumn key="actions" />,
-              ]}
-              isEmpty={!tombstones.length}
-              emptyMessage={t('You have no discarded issues')}
-            >
-              {tombstones.map(data => (
-                <GroupTombstoneRow
-                  key={data.id}
-                  data={data}
-                  disabled={!hasAccess}
-                  onUndiscard={handleUndiscard}
-                />
-              ))}
-            </StyledPanelTable>
+            {hasGrouptombstonesHitCounter(organization) ? (
+              <StyledPanelTable
+                headers={[
+                  <LeftAlignedColumn key="lastSeen">{t('Issue')}</LeftAlignedColumn>,
+                  <RightAlignedColumn key="lastSeen">
+                    {t('Last Seen')}
+                  </RightAlignedColumn>,
+                  <RightAlignedColumn key="events">{t('Events')}</RightAlignedColumn>,
+                  <CenteredAlignedColumn key="member">
+                    {t('Member')}
+                  </CenteredAlignedColumn>,
+                  <CenteredAlignedColumn key="actions" />,
+                ]}
+                isEmpty={!tombstones.length}
+                emptyMessage={t('You have no discarded issues')}
+              >
+                {tombstones.map(data => (
+                  <GroupTombstoneRow
+                    key={data.id}
+                    data={data}
+                    disabled={!hasAccess}
+                    onUndiscard={handleUndiscard}
+                  />
+                ))}
+              </StyledPanelTable>
+            ) : (
+              <Panel>
+                {tombstones.map(data => (
+                  <GroupTombstoneRow
+                    key={data.id}
+                    data={data}
+                    disabled={!hasAccess}
+                    onUndiscard={handleUndiscard}
+                  />
+                ))}
+              </Panel>
+            )}
             {tombstonesPageLinks && <Pagination pageLinks={tombstonesPageLinks} />}
           </Fragment>
         )}
@@ -209,6 +289,17 @@ const LeftAlignedColumn = styled(Column)`
 
 const CenteredAlignedColumn = styled(Column)`
   justify-content: center;
+`;
+
+const AvatarContainer = styled('div')`
+  margin: 0 ${space(3)};
+  flex-shrink: 1;
+  align-items: center;
+`;
+
+const ActionContainer = styled('div')`
+  flex-shrink: 1;
+  align-items: center;
 `;
 
 export default GroupTombstones;
