ee5ce3b01b3d21b0e54601ac5fba261ca7fd173a
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-06-18 10:01:47 -0700

    chore: Cleanup the employee feedback button. its replaced by the toolbar (#93830)
    Bye to this little guy
    <img width="256" alt="SCR-20250618-iviu"
    src="https://github.com/user-attachments/assets/2cf07160-b5a2-4f22-a3fd-374103f0d72d"
    />

--- static/gsApp/components/organizationHeader.tsx
+++ static/gsApp/components/organizationHeader.tsx
@@ -1,8 +1,5 @@
-import {Fragment} from 'react';
-
 import type {Organization} from 'sentry/types/organization';
 
-import EmployeeFeedbackButton from 'getsentry/components/employeeFeedbackButton';
 import GSBanner from 'getsentry/components/gsBanner';
 import useFeedbackInit from 'getsentry/utils/useFeedbackInit';
 import useReplayInit from 'getsentry/utils/useReplayInit';
@@ -12,18 +9,8 @@ interface Props {
 }
 
 export function OrganizationHeader({organization}: Props) {
-  const showDevToolbar = organization.features.includes('devtoolbar');
-
-  // The employee feedback button was the precursor to the toolbar, we only need to show one at a time
-  const showEmployeeFeedbackButton = !showDevToolbar;
-
   useFeedbackInit();
   useReplayInit();
 
-  return (
-    <Fragment>
-      <GSBanner organization={organization} />
-      {showEmployeeFeedbackButton ? <EmployeeFeedbackButton /> : null}
-    </Fragment>
-  );
+  return <GSBanner organization={organization} />;
 }
