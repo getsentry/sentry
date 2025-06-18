b2e5d655887e3b8b40a2cce5ec5b6577052c9e6d
Nico Hinderling <NicoHinderling@users.noreply.github.com>
2025-06-18 08:50:12 -0700

    chore(preprod): move endpoint to separate file to share with other preprod endpoints

---
+++ src/sentry/preprod/api/endpoints/urls.py
@@ -0,0 +1,11 @@
+from django.urls import re_path
+
+from .organization_preprod_artifact_assemble import ProjectPreprodArtifactAssembleEndpoint
+
+preprod_urlpatterns = [
+    re_path(
+        r"^(?P<organization_id_or_slug>[^\/]+)/(?P<project_id_or_slug>[^\/]+)/files/preprodartifacts/assemble/$",
+        ProjectPreprodArtifactAssembleEndpoint.as_view(),
+        name="sentry-api-0-assemble-preprod-artifact-files",
+    ),
+]
