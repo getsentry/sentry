236558bdb4c1fed57bf2b76a364e80600e9b2ccc
Calvin Yau <calvin.yau@sentry.io>
2025-06-18 11:38:09 -0700

    fix(codecov): Style and look changes to step 3 steps

--- static/app/views/codecov/tests/onboardingSteps/editGHAWorkflow.tsx
+++ static/app/views/codecov/tests/onboardingSteps/editGHAWorkflow.tsx
@@ -4,6 +4,7 @@ import {CodeSnippet} from 'sentry/components/codeSnippet';
 import Link from 'sentry/components/links/link';
 import {t, tct} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
+import {InlineCodeSnippet} from 'sentry/views/codecov/styles';
 import {OnboardingStep} from 'sentry/views/codecov/tests/onboardingSteps/onboardingStep';
 
 interface EditWorkflowProps {
@@ -28,30 +29,32 @@ export function EditGHAWorkflow({step}: EditWorkflowProps) {
     <OnboardingStep.Container>
       <OnboardingStep.Header>{headerText}</OnboardingStep.Header>
       <OnboardingStep.Content>
-        <ContentHeader>
-          {tct(
-            'Add [permissions] block at the top level in your CI YAML file to run Sentry Prevent',
-            {
-              permissions: <InlineCodeSnippet>permissions</InlineCodeSnippet>,
-            }
-          )}
-        </ContentHeader>
-        <CodeSnippet dark language="yaml">
-          {PERMISSIONS_SNIPPET}
-        </CodeSnippet>
-        <Paragraph>
-          {tct(
-            'Set this permission at the workflow or job level. For better security, define it at the job level as it limits access to only the job that needs the OIDC token. Learn more about [permissionsSettings].',
-            {
-              permissionsSettings: <Link to="">{t('permissions settings')}</Link>,
-            }
-          )}
-        </Paragraph>
-        <ContentHeader>
+        <TopParagraph>
+          <SubHeader>
+            {tct(
+              'Add [permissions] block at the top level in your CI YAML file to run Sentry Prevent',
+              {
+                permissions: <InlineCodeSnippet>permissions</InlineCodeSnippet>,
+              }
+            )}
+          </SubHeader>
+          <CodeSnippet dark language="yaml">
+            {PERMISSIONS_SNIPPET}
+          </CodeSnippet>
+          <Paragraph>
+            {tct(
+              'Set this permission at the workflow or job level. For better security, define it at the job level as it limits access to only the job that needs the OIDC token. Learn more about [permissionsSettings].',
+              {
+                permissionsSettings: <Link to="">{t('permissions settings')}</Link>,
+              }
+            )}
+          </Paragraph>
+        </TopParagraph>
+        <SubHeader>
           {tct('Add the script [actionName] to your CI YAML file', {
             actionName: <InlineCodeSnippet>getsentry/prevent-action</InlineCodeSnippet>,
           })}
-        </ContentHeader>
+        </SubHeader>
         <Paragraph>
           {t('In your CI YAML file, add below scripts to the end of your test run.')}
         </Paragraph>
@@ -69,24 +72,18 @@ export function EditGHAWorkflow({step}: EditWorkflowProps) {
   );
 }
 
-const InlineCodeSnippet = styled('span')`
-  background-color: ${p => p.theme.black};
-  color: ${p => p.theme.white};
-  font-family: ${p => p.theme.text.familyMono};
-  font-size: ${p => p.theme.fontSizeSmall};
-  font-weight: ${p => p.theme.fontWeightBold};
-  border-radius: ${p => p.theme.borderRadius};
-  padding: ${space(0.75)} ${space(1)};
-  line-height: 1;
-`;
-
-const ContentHeader = styled('p')`
+const SubHeader = styled('div')`
   font-size: ${p => p.theme.fontSizeExtraLarge};
   font-weight: ${p => p.theme.fontWeightBold};
   color: ${p => p.theme.gray300};
   margin-bottom: 0;
+  line-height: 31px;
 `;
 
-const Paragraph = styled('p')`
-  margin-top: ${space(1.5)};
+const TopParagraph = styled('div')`
+  margin-bottom: ${space(3)};
+`;
+
+const Paragraph = styled('div')`
+  margin: ${space(1)} 0;
 `;
