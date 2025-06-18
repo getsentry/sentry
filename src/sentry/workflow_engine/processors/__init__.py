dad74843473518440b31a2ac3d08db0f18e5ff01
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-06-18 14:13:50 -0700

    feat(workflow_engine): Setup the Task for process_workflow_updates (#93553)
    ## Description
    - Create a celery task namespace for the workflow engine tasks
    - Create a task for processing an activity in the workflow_engine --
    this will allow us to create a celery task to async execute process
    workflows when the issue platform creates an activity
    
    In the code I mention there being a follow-up PR:
    https://github.com/getsentry/sentry/pull/93580 is the initial PR to
    support the `Activity` / `Group` models.

--- src/sentry/workflow_engine/processors/__init__.py
+++ src/sentry/workflow_engine/processors/__init__.py
@@ -4,9 +4,11 @@ __all__ = [
     "process_workflows",
     "process_data_packet",
     "process_delayed_workflows",
+    "process_workflows",
     "DelayedWorkflow",
 ]
 
 from .data_source import process_data_sources
 from .delayed_workflow import DelayedWorkflow, process_delayed_workflows
 from .detector import process_detectors
+from .workflow import process_workflows
