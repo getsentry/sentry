2e2e6a650a896b5518e9d19c9e1e886574f01579
Tony Xiao <txiao@sentry.io>
2025-06-19 10:48:39 -0400

    feat(profiling): Flag to test always using direct continuous profile … (#93705)
    …chunks
    
    Testing the strategy of always including direct continuous profile
    chunks. This was a concern previously on web servers where idle time was
    the majority of the flamegraph. But with lifecycle=trace, this is less
    of an issue.
    
    And by always including it, we can improve the situation where SDK devs
    are switching between lifecycle=trace and lifecycle=manual.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -290,6 +290,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:profiling-browser", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enables separate differential flamegraph page
     manager.add("organizations:profiling-differential-flamegraph-page", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    # Enables ability usage of direct profile chunks all the time
+    manager.add("organizations:profiling-flamegraph-use-direct-chunks", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable global suspect functions in profiling
     manager.add("organizations:profiling-global-suspect-functions", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable profiling summary redesign view
