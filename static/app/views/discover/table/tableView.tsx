4f6999285cb26ecc528e1fa112f5e50e4e2b78ca
lzhao-sentry <lucy.zhao@sentry.io>
2025-06-19 17:03:07 -0400

    feat(explore): add copy to clipboard to cell actions (#93907)
    ### Changes
    Add a copy to clipboard option to the tables that use the cell action
    dropdown. Option does not render if the value is empty/undefined.
    Currently only enabled for tables in explore.
    
    ### Video
    
    
    https://github.com/user-attachments/assets/b1b06b33-c566-40ff-9986-19c30fa7d9e2

--- static/app/views/discover/table/tableView.tsx
+++ static/app/views/discover/table/tableView.tsx
@@ -66,7 +66,7 @@ import {makeReleasesPathname} from 'sentry/views/releases/utils/pathnames';
 
 import {QuickContextHoverWrapper} from './quickContext/quickContextWrapper';
 import {ContextType} from './quickContext/utils';
-import CellAction, {Actions, updateQuery} from './cellAction';
+import CellAction, {Actions, copyToClipBoard, updateQuery} from './cellAction';
 import ColumnEditModal, {modalCss} from './columnEditModal';
 import TableActions from './tableActions';
 import {TopResultsIndicator} from './topResultsIndicator';
@@ -622,6 +622,10 @@ function TableView(props: TableViewProps) {
           );
 
           return;
+        }
+        case Actions.COPY_TO_CLIPBOARD: {
+          copyToClipBoard(value);
+          break;
         }
         default: {
           // Some custom perf metrics have units.
