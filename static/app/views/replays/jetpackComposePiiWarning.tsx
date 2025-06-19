f65db1c028147a94b4d5eb23c766c51d6dbc7280
Billy Vong <billyvg@users.noreply.github.com>
2025-06-19 14:26:22 -0400

    feat(replay): Add warning banners for Jetpack Compose 1.8+
    This displays warning banners on the replay list and details when:
    
    - any replay has sdk name `sentry.java.android`
    - sdk version is < 8.14.0
    - on replay details we also check to see if it is a video replay

---
+++ static/app/views/replays/jetpackComposePiiWarning.tsx
@@ -0,0 +1,29 @@
+import styled from '@emotion/styled';
+
+import {Alert} from 'sentry/components/core/alert';
+import ExternalLink from 'sentry/components/links/externalLink';
+import {tct} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {MIN_JETPACK_COMPOSE_VIEW_HIERARCHY_PII_FIX} from 'sentry/utils/replays/sdkVersions';
+
+export function JetpackComposePiiWarning() {
+  return (
+    <AndroidSdkWarningContainer>
+      <Alert type="warning" showIcon>
+        {tct(
+          'There is a known PII/masking issue with [jetpack:Jetpack Compose versions 1.8 and above]. [link:Update your Sentry SDK to version 8.14.0 or later] to ensure replays are properly masked.',
+          {
+            jetpack: <strong />,
+            link: (
+              <ExternalLink href={MIN_JETPACK_COMPOSE_VIEW_HIERARCHY_PII_FIX.changelog} />
+            ),
+          }
+        )}
+      </Alert>
+    </AndroidSdkWarningContainer>
+  );
+}
+
+const AndroidSdkWarningContainer = styled('div')`
+  margin-bottom: ${space(2)};
+`;
