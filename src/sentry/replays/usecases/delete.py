03f8441c6de4ab131098fbf1afc1df950fca30ab
Colton Allen <colton.allen@sentry.io>
2025-06-20 18:48:53 -0500

    Match segment_id rows

--- src/sentry/replays/usecases/delete.py
+++ src/sentry/replays/usecases/delete.py
@@ -145,6 +145,9 @@ def fetch_rows_matching_pattern(
             Condition(Column("project_id"), Op.EQ, project_id),
             Condition(Column("timestamp"), Op.LT, end),
             Condition(Column("timestamp"), Op.GTE, start),
+            # We only match segment rows because those contain the PII we want to delete. This also
+            # means we won't match our is_archived rows putting us in an infinite deletion loop.
+            Condition(Column("segment_id"), Op.IS_NOT_NULL),
             *where,
         ],
         groupby=[Column("replay_id")],
