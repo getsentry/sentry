8e6344fd3e401ce5fa14d6cbc9f7eda1997bf69d
Matt Duncan <14761+mrduncan@users.noreply.github.com>
2025-06-20 14:10:48 -0700

    feat(issues): Add hex parameterization experiment (#93970)
    Our existing hex parameterization supports hex values of any length
    however they must begin with either `0x` or `0X`. We frequently see hex
    values in messages which do not have this prefix (ex: trace ids or span
    ids).
    
    This adds support for hex values which do not have the `0x` prefix. To
    ensure it does not over-parameterize the following requirements:
    - Must be exactly 4 or 8 bytes (8 or 16 characters)
    - Must contain both a number and a letter
    - Must use lowercase letters (anecdotally I've never seen examples where
    uppercase would be beneficial)
    
    The addition of `EXPERIMENT_PARAMETERIZATION_REGEXES_MAP` and other
    parameters will all be removed once the rollout of this is completed.

--- src/sentry/grouping/strategies/message.py
+++ src/sentry/grouping/strategies/message.py
@@ -85,7 +85,9 @@ def normalize_message_for_grouping(message: str, event: Event, share_analytics: 
         )
 
     parameterizer = Parameterizer(
-        regex_pattern_keys=REGEX_PATTERN_KEYS, experiments=(UniqueIdExperiment,)
+        regex_pattern_keys=REGEX_PATTERN_KEYS,
+        experiments=(UniqueIdExperiment,),
+        enable_regex_experiments=_should_run_experiment("regex"),
     )
 
     normalized = parameterizer.parameterize_all(trimmed, _should_run_experiment)
