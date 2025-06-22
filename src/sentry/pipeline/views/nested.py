4c3eed3cac421dab024d2df214a533986a9518a3
anthony sottile <anthony.sottile@sentry.io>
2025-06-22 16:58:56 -0400

    ref: make Pipeline views a Protocol

--- src/sentry/pipeline/views/nested.py
+++ src/sentry/pipeline/views/nested.py
@@ -6,17 +6,11 @@ from typing import TYPE_CHECKING, Any
 from django.http.request import HttpRequest
 from django.http.response import HttpResponseBase
 
-from sentry.db.models.base import Model
-from sentry.pipeline.store import PipelineSessionStore
-from sentry.pipeline.views.base import PipelineView
-
 if TYPE_CHECKING:
     from sentry.pipeline.base import Pipeline
 
 
-class NestedPipelineView[M: Model, S: PipelineSessionStore, U: Model, V: PipelineSessionStore](
-    PipelineView[M, S]
-):
+class NestedPipelineView[P1: Pipeline[Any, Any], P2: Pipeline[Any, Any]]:
     """
     A NestedPipelineView can be used within other pipelines to process another
     pipeline within a pipeline. Note that the nested pipelines finish_pipeline
@@ -29,7 +23,7 @@ class NestedPipelineView[M: Model, S: PipelineSessionStore, U: Model, V: Pipelin
     def __init__(
         self,
         bind_key: str,
-        pipeline_cls: type[Pipeline[U, V]],
+        pipeline_cls: type[P2],
         provider_key: str,
         config: Mapping[str, Any] | None = None,
     ) -> None:
@@ -38,7 +32,7 @@ class NestedPipelineView[M: Model, S: PipelineSessionStore, U: Model, V: Pipelin
         self.config = config or {}
 
         class NestedPipeline(pipeline_cls):  # type: ignore[misc, valid-type]
-            def set_parent_pipeline(self, parent_pipeline: Pipeline[M, S]) -> None:
+            def set_parent_pipeline(self, parent_pipeline: P1) -> None:
                 self.parent_pipeline = parent_pipeline
 
             def finish_pipeline(self) -> HttpResponseBase:
@@ -49,7 +43,7 @@ class NestedPipelineView[M: Model, S: PipelineSessionStore, U: Model, V: Pipelin
 
         self.pipeline_cls = NestedPipeline
 
-    def dispatch(self, request: HttpRequest, pipeline: Pipeline[M, S]) -> HttpResponseBase:
+    def dispatch(self, request: HttpRequest, pipeline: P1) -> HttpResponseBase:
         nested_pipeline = self.pipeline_cls(
             organization=pipeline.organization,
             request=request,
