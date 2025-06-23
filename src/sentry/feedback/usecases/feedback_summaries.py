68139c2382ee9d6eb7bb15a96215e68ad50259af
Vishnu Satish <vishnu.satish@sentry.io>
2025-06-23 13:35:10 -0700

    ref(feedback): change prompt so AI summary is one sentence (#94105)

--- src/sentry/feedback/usecases/feedback_summaries.py
+++ src/sentry/feedback/usecases/feedback_summaries.py
@@ -13,13 +13,13 @@ def make_input_prompt(
     feedbacks_string = "\n------\n".join(feedbacks)
     return f"""Instructions:
 
-You are an assistant that summarizes customer feedback. Given a list of customer feedback entries, generate a concise summary of 1-2 sentences where you summarize the broader themes that represent the list of feedbacks that you are given.
+You are an assistant that summarizes customer feedback. Given a list of customer feedback entries, generate a concise, one sentence summary, where you summarize the broader themes that represent the list of feedbacks that you are given.
 
 Your goal is to help someone quickly understand the main patterns or themes that represent the feedback entries. Don't mention ANY specific examples of themes. The summary should remain broad.
 
 Begin the summary with "Users...", for example, "Users say...". Don't make overly generic statements like "Users report a variety of issues."
 
-The summary must be AT MOST 55 words, that is an absolute upper limit, and you must write AT MOST two sentences. You can leave certain things out, and when deciding what topics/themes to mention, make sure it is proportional to the number of times they appear in different customer feedback entries.
+The summary must be AT MOST 35 words, that is an absolute upper limit, and you must write AT MOST one sentence. You can leave certain things out, and when deciding what topics/themes to mention, make sure it is proportional to the number of times they appear in different customer feedback entries.
 
 User Feedbacks:
 
@@ -27,7 +27,7 @@ User Feedbacks:
 
 Output Format:
 
-<1-2 sentence summary>
+<1 sentence summary>
 """
 
 
