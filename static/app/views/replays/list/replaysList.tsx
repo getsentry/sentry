88589e19ddd10b861a9d95eae2ee4ffb73a51bae
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-06-23 15:50:56 -0700

    ref(replay): Extract useNeedsJetpackComposePiiNotice() from replayList.tsx (#94134)
    Pulling this out so i can re-use it and not forget about it.

--- static/app/views/replays/list/replaysList.tsx
+++ static/app/views/replays/list/replaysList.tsx
@@ -7,16 +7,17 @@ import {trackAnalytics} from 'sentry/utils/analytics';
 import {decodeList, decodeScalar, decodeSorts} from 'sentry/utils/queryString';
 import useFetchReplayList from 'sentry/utils/replays/hooks/useFetchReplayList';
 import useReplayListQueryKey from 'sentry/utils/replays/hooks/useReplayListQueryKey';
-import {MIN_JETPACK_COMPOSE_VIEW_HIERARCHY_PII_FIX} from 'sentry/utils/replays/sdkVersions';
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import useLocationQuery from 'sentry/utils/url/useLocationQuery';
 import {useNavigate} from 'sentry/utils/useNavigate';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import useProjectSdkNeedsUpdate from 'sentry/utils/useProjectSdkNeedsUpdate';
-import {semverCompare} from 'sentry/utils/versions/semverCompare';
 import useAllMobileProj from 'sentry/views/replays/detail/useAllMobileProj';
-import {JetpackComposePiiNotice} from 'sentry/views/replays/jetpackComposePiiNotice';
+import {
+  JetpackComposePiiNotice,
+  useNeedsJetpackComposePiiNotice,
+} from 'sentry/views/replays/jetpackComposePiiNotice';
 import ReplayTable from 'sentry/views/replays/replayTable';
 import {ReplayColumn} from 'sentry/views/replays/replayTable/types';
 
@@ -89,15 +90,7 @@ function ReplaysList() {
         ReplayColumn.ACTIVITY,
       ];
 
-  const needsJetpackComposePiiWarning = replays?.find(replay => {
-    return (
-      replay?.sdk.name === 'sentry.java.android' &&
-      semverCompare(
-        replay?.sdk.version ?? '',
-        MIN_JETPACK_COMPOSE_VIEW_HIERARCHY_PII_FIX.minVersion
-      ) === -1
-    );
-  });
+  const needsJetpackComposePiiWarning = useNeedsJetpackComposePiiNotice({replays});
 
   return (
     <Fragment>
