cc6e3f230f284e1db75acb51115766e7497177a5
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-06-24 12:36:35 -0400

    feat(trace-suspect-tags): Adding comments

--- static/app/views/explore/hooks/useChartBoxSelect.tsx
+++ static/app/views/explore/hooks/useChartBoxSelect.tsx
@@ -43,9 +43,18 @@ export function useChartBoxSelect({
   const enabledBoxSelect = organization.features.includes(
     'performance-spans-suspect-attributes'
   );
+
+  // This exposes the coordinate range of the selected area. It is clamped to extremes
+  // of the chart's x and y axes.
   const [brushArea, setBrushArea] = useState<EchartBrushAreas | null>(null);
+
+  // This exposes the page coordinates when the user finishes drawing the box. This is used
+  // to render floating CTAs on top of the chart.
   const [pageCoords, setPageCoords] = useState<{x: number; y: number} | null>(null);
 
+  // This increments a counter to force a re-activation of the brush mode. We expose the
+  // re-activation function in the return value, so that the parent component can call it
+  // for example when the chart data changes.
   const [forceReActivateSelection, reActivateSelection] = useReducer(
     x => (x + 1) % Number.MAX_SAFE_INTEGER,
     0
