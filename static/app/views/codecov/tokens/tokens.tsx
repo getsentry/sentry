fdf96fd83ce4f228f94dd2975266154c9fdda37b
Ajay Singh <ajay.singh@sentry.io>
2025-06-24 16:17:19 -0700

    add spec and button

--- static/app/views/codecov/tokens/tokens.tsx
+++ static/app/views/codecov/tokens/tokens.tsx
@@ -1,21 +1,36 @@
 import styled from '@emotion/styled';
 
+import {useCodecovContext} from 'sentry/components/codecov/context/codecovContext';
 import {IntegratedOrgSelector} from 'sentry/components/codecov/integratedOrgSelector/integratedOrgSelector';
+import {Button} from 'sentry/components/core/button';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 
 export default function TokensPage() {
+  const {integratedOrg} = useCodecovContext();
+
   return (
     <LayoutGap>
       <PageFilterBar condensed>
         <IntegratedOrgSelector />
       </PageFilterBar>
-      <p>{t('Repository tokens')}</p>
-      <p>
-        {t(`View the list of tokens created for your repositories in Turing-Corp. Use them for
-        uploading reports to all Sentry Prevent's features.`)}
-      </p>
+      <HeaderValue>{t('Repository tokens')}</HeaderValue>
+      <TopRow>
+        <p>
+          {t('View the list of tokens created for your repositories in')}{' '}
+          <strong>{integratedOrg}</strong>.
+          {t(" Use them for uploading reports to all Sentry Prevent's features.")}
+        </p>
+        <StyledButton
+          size="sm"
+          priority="primary"
+          onClick={() => {}}
+          aria-label="regenerate token"
+        >
+          Regenerate token
+        </StyledButton>
+      </TopRow>
     </LayoutGap>
   );
 }
@@ -23,4 +38,19 @@ export default function TokensPage() {
 const LayoutGap = styled('div')`
   display: grid;
   gap: ${space(2)};
+  max-width: 1200px;
+`;
+
+const HeaderValue = styled('div')`
+  font-size: ${p => p.theme.headerFontSize};
+  font-weight: ${p => p.theme.fontWeightBold};
+`;
+
+const StyledButton = styled(Button)`
+  max-width: 175px;
+`;
+
+const TopRow = styled('div')`
+  display: flex;
+  justify-content: space-between;
 `;
