0fe0045f0bda4b095da23e217697546088b0e959
Malachi Willey <malwilley@gmail.com>
2025-06-25 11:52:41 -0700

    feat(aci): Improved connected monitors table (#94253)
    - Update table to use new components (fixes title overflow and assignee
    column)
    - Add responsive column dropping

--- static/app/components/workflowEngine/simpleTable/index.tsx
+++ static/app/components/workflowEngine/simpleTable/index.tsx
@@ -1,8 +1,9 @@
-import type {HTMLAttributes} from 'react';
+import type {CSSProperties, HTMLAttributes} from 'react';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import InteractionStateLayer from 'sentry/components/core/interactionStateLayer';
+import {Flex} from 'sentry/components/core/layout/flex';
 import Panel from 'sentry/components/panels/panel';
 import {IconArrow} from 'sentry/icons';
 import {space} from 'sentry/styles/space';
@@ -36,8 +37,8 @@ function HeaderCell({
   sort,
   handleSortClick,
 }: {
-  children: React.ReactNode;
   name: string;
+  children?: React.ReactNode;
   handleSortClick?: () => void;
   sort?: Sort;
   sortKey?: string;
@@ -52,7 +53,7 @@ function HeaderCell({
       role="columnheader"
       as={sortKey ? 'button' : 'div'}
     >
-      <HeaderDivider />
+      {children && <HeaderDivider />}
       {sortKey && <InteractionStateLayer />}
       <HeadingText>{children}</HeadingText>
       {sortKey && (
@@ -76,9 +77,17 @@ function Row({children, variant = 'default', ...props}: RowProps) {
   );
 }
 
-function RowCell({children, name}: {children: React.ReactNode; name: string}) {
+function RowCell({
+  children,
+  name,
+  justify,
+}: {
+  children: React.ReactNode;
+  name: string;
+  justify?: CSSProperties['justifyContent'];
+}) {
   return (
-    <StyledRowCell className={name} role="cell">
+    <StyledRowCell className={name} role="cell" align="center" justify={justify}>
       {children}
     </StyledRowCell>
   );
@@ -103,9 +112,7 @@ const StyledPanelHeader = styled('div')`
   grid-column: 1 / -1;
 `;
 
-const StyledRowCell = styled('div')`
-  display: flex;
-  align-items: center;
+const StyledRowCell = styled(Flex)`
   overflow: hidden;
   padding: ${space(2)};
 `;
