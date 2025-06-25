74a3272111825b573f66d3b5a935dae287e577b4
Adrian <adrian.bonilla@sentry.io>
2025-06-25 12:28:00 -0700

    feat: add reset ability for selectors (#93923)
    This PR adds the ability to reset state for Codecov selectors based on
    the selector you choose. This is directly implemented in the repository
    selector as a POC.

--- static/app/components/codecov/context/codecovContext.tsx
+++ static/app/components/codecov/context/codecovContext.tsx
@@ -3,11 +3,14 @@ import {createContext, useContext} from 'react';
 import type {CodecovPeriodOptions} from 'sentry/components/codecov/datePicker/dateSelector';
 
 export type CodecovContextData = {
-  branch: string | null;
+  changeContextValue: (value: Partial<CodecovContextDataParams>) => void;
   codecovPeriod: CodecovPeriodOptions;
-  integratedOrg: string | null;
-  repository: string | null;
+  branch?: string;
+  integratedOrg?: string;
+  repository?: string;
 };
+
+export type CodecovContextDataParams = Omit<CodecovContextData, 'changeContextValue'>;
 
 export const CodecovContext = createContext<CodecovContextData | undefined>(undefined);
 
