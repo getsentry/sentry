19167139ab7a2b9bee8ee984168b2bf726dc573d
Katie Byers <lobsterkatie@gmail.com>
2025-06-24 17:33:51 -0700

    pull frame and exception out into variables

--- src/sentry/grouping/utils.py
+++ src/sentry/grouping/utils.py
@@ -90,6 +90,9 @@ def bool_from_string(value: str) -> bool | None:
 def resolve_fingerprint_variable(
     variable_key: str, event_data: NodeData | Mapping[str, Any]
 ) -> str | None:
+    frame = get_crash_frame_from_event_data(event_data)
+    exception = get_path(event_data, "exception", "values", -1)
+
     if variable_key == "transaction":
         return event_data.get("transaction") or "<no-transaction>"
 
@@ -97,40 +100,35 @@ def resolve_fingerprint_variable(
         message = (
             get_path(event_data, "logentry", "formatted")
             or get_path(event_data, "logentry", "message")
-            or get_path(event_data, "exception", "values", -1, "value")
+            or get_path(exception, "value")
         )
         return message or "<no-message>"
 
     elif variable_key in ("type", "error.type"):
-        exception_type = get_path(event_data, "exception", "values", -1, "type")
+        exception_type = get_path(exception, "type")
         return exception_type or "<no-type>"
 
     elif variable_key in ("value", "error.value"):
-        value = get_path(event_data, "exception", "values", -1, "value")
+        value = get_path(exception, "value")
         return value or "<no-value>"
 
     elif variable_key in ("function", "stack.function"):
-        frame = get_crash_frame_from_event_data(event_data)
         func = frame.get("function") if frame else None
         return func or "<no-function>"
 
     elif variable_key in ("path", "stack.abs_path"):
-        frame = get_crash_frame_from_event_data(event_data)
         abs_path = frame.get("abs_path") or frame.get("filename") if frame else None
         return abs_path or "<no-abs-path>"
 
     elif variable_key == "stack.filename":
-        frame = get_crash_frame_from_event_data(event_data)
         filename = frame.get("filename") or frame.get("abs_path") if frame else None
         return filename or "<no-filename>"
 
     elif variable_key in ("module", "stack.module"):
-        frame = get_crash_frame_from_event_data(event_data)
         module = frame.get("module") if frame else None
         return module or "<no-module>"
 
     elif variable_key in ("package", "stack.package"):
-        frame = get_crash_frame_from_event_data(event_data)
         pkg = frame.get("package") if frame else None
         if pkg:
             # If the package is formatted as either a POSIX or Windows path, grab the last segment
