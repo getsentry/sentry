690abc0f3cf1e041fcb4a191bdd794a2cbddf5f5
Brendan Hy <brendan.hy@sentry.io>
2025-06-25 13:14:32 -0700

    fix(usage stats): add chartTransform param to EnhancedUsageStatsOrganization (#94355)
    closes
    https://linear.app/getsentry/issue/RTC-1058/stats-graph-does-not-update-when-switching-from-periodic-to-cumulative
    
    <img width="1408" alt="Screenshot 2025-06-25 at 12 43 57 PM"
    src="https://github.com/user-attachments/assets/55023ad8-1ab8-4c69-9b17-6e8fd66fecd8"
    />

--- static/gsApp/hooks/spendVisibility/enhancedUsageStatsOrganization.tsx
+++ static/gsApp/hooks/spendVisibility/enhancedUsageStatsOrganization.tsx
@@ -210,6 +210,7 @@ interface EnhancedUsageStatsOrganizationProps
   isSingleProject: boolean;
   projects: Project[];
   subscription: Subscription;
+  chartTransform?: string;
   spikeCursor?: string;
 }
 /**
@@ -233,6 +234,7 @@ function EnhancedUsageStatsOrganization({
   spikeCursor,
   clientDiscard,
   handleChangeState,
+  chartTransform,
 }: EnhancedUsageStatsOrganizationProps) {
   const project = projects.find(p => p.id === `${projectIds[0]}`);
   const endpointQueryDatetime = getEndpointQueryDatetime(dataDatetime);
@@ -342,6 +344,7 @@ function EnhancedUsageStatsOrganization({
       endpointQuery={newEndpointQuery}
       handleChangeState={handleChangeState}
       clientDiscard={clientDiscard}
+      chartTransform={chartTransform}
     >
       {usageStats => {
         const loadingStatuses = [usageStats.orgStats.isPending];
