a59cd82f8d91ca4ad227b90186e946590acd30f3
Kyle Consalus <kyle.consalus@sentry.io>
2025-06-26 09:20:37 -0700

    feat(aci): Add task for process_workflows (#94222)
    Set up a task for running process_workflows outside of post_process.
    Guard it with a feature flag so we can roll it out gradually and safely.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -499,6 +499,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:workflow-engine-ui-links", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Use workflow engine serializers to return data for old rule / incident endpoints
     manager.add("organizations:workflow-engine-rule-serializers", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
+    # Enable async processing of event workflows in a task rather than as part of post_process.
+    manager.add("organizations:workflow-engine-post-process-async", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable EventUniqueUserFrequencyConditionWithConditions special alert condition
     manager.add("organizations:event-unique-user-frequency-condition-with-conditions", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Use spans instead of transactions for dynamic sampling calculations. This will become the new default.
