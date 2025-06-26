5ea0df54198fef51614cf04cf2d56267698b199c
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-06-26 09:49:22 -0700

    fix(simpleTable): Rename prop `name` to `className` (#94380)
    Before we were taking the `name` prop and passing it into the
    `className` prop on the component. We shouldn't ever be setting
    className to a string like this directly, instead emotion should
    generate the classNames for us.
    
    There's a story and the extra classnames were visible in the html
    (before):
    
    <img width="1079" alt="SCR-20250625-onus"
    src="https://github.com/user-attachments/assets/89a1498b-d9d4-48bb-816d-799eac3c6d42"
    />

--- static/app/views/automations/components/connectedMonitorsList.tsx
+++ static/app/views/automations/components/connectedMonitorsList.tsx
@@ -39,33 +39,35 @@ export default function ConnectedMonitorsList({
     <Container>
       <SimpleTableWithColumns>
         <SimpleTable.Header>
-          <SimpleTable.HeaderCell name="name">{t('Name')}</SimpleTable.HeaderCell>
-          <SimpleTable.HeaderCell name="type">{t('Type')}</SimpleTable.HeaderCell>
-          <SimpleTable.HeaderCell name="last-issue">
+          <SimpleTable.HeaderCell className="name">{t('Name')}</SimpleTable.HeaderCell>
+          <SimpleTable.HeaderCell className="type">{t('Type')}</SimpleTable.HeaderCell>
+          <SimpleTable.HeaderCell className="last-issue">
             {t('Last Issue')}
           </SimpleTable.HeaderCell>
-          <SimpleTable.HeaderCell name="owner">{t('Assignee')}</SimpleTable.HeaderCell>
-          {canEdit && <SimpleTable.HeaderCell name="connected" />}
+          <SimpleTable.HeaderCell className="owner">
+            {t('Assignee')}
+          </SimpleTable.HeaderCell>
+          {canEdit && <SimpleTable.HeaderCell className="connected" />}
         </SimpleTable.Header>
         {monitors.length === 0 && (
           <SimpleTable.Empty>{t('No monitors connected')}</SimpleTable.Empty>
         )}
         {monitors.map(monitor => (
           <SimpleTable.Row key={monitor.id}>
-            <SimpleTable.RowCell name="name">
+            <SimpleTable.RowCell className="name">
               <DetectorLink detector={monitor} />
             </SimpleTable.RowCell>
-            <SimpleTable.RowCell name="type">
+            <SimpleTable.RowCell className="type">
               <DetectorTypeCell type={monitor.type} />
             </SimpleTable.RowCell>
-            <SimpleTable.RowCell name="last-issue">
+            <SimpleTable.RowCell className="last-issue">
               <IssueCell group={undefined} />
             </SimpleTable.RowCell>
-            <SimpleTable.RowCell name="owner">
+            <SimpleTable.RowCell className="owner">
               <DetectorAssigneeCell assignee={monitor.owner} />
             </SimpleTable.RowCell>
             {canEdit && (
-              <SimpleTable.RowCell name="connected" justify="flex-end">
+              <SimpleTable.RowCell className="connected" justify="flex-end">
                 <Button onClick={() => toggleConnected(monitor.id)} size="sm">
                   {connectedIds?.has(monitor.id) ? t('Disconnect') : t('Connect')}
                 </Button>
