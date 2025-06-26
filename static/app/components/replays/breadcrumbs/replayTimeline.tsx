3049f3af7aeeb3b4a1d43aaf4c82c922877ca5f2
Billy Vong <billyvg@users.noreply.github.com>
2025-06-26 15:16:24 -0400

    wip: scatterplot for errors

--- static/app/components/replays/breadcrumbs/replayTimeline.tsx
+++ static/app/components/replays/breadcrumbs/replayTimeline.tsx
@@ -3,6 +3,7 @@ import {useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import {AreaChart} from 'sentry/components/charts/areaChart';
+import BaseChart from 'sentry/components/charts/baseChart';
 import StackedAreaChart from 'sentry/components/charts/stackedAreaChart';
 import Panel from 'sentry/components/panels/panel';
 import Placeholder from 'sentry/components/placeholder';
@@ -47,12 +48,12 @@ function createStackedChartData(
     const bucketData: Record<string, any> = {
       name: startTimestampMs + index * bucketSizeMs,
       time: startTimestampMs + index * bucketSizeMs,
-      // time: (index * bucketSizeMs) / 1000, // Time in seconds for display
+      data: {},
     };
 
     // Initialize all categories to 0
     categories.forEach(category => {
-      bucketData[category] = 0;
+      bucketData.data[category] = 0;
     });
 
     return bucketData;
@@ -67,7 +68,7 @@ function createStackedChartData(
     const bucketIndex = Math.floor(frameTimeMs / bucketSizeMs);
 
     if (bucketIndex >= 0 && bucketIndex < bucketCount) {
-      buckets[bucketIndex][category] += 1;
+      buckets[bucketIndex].data[category] += 1;
     }
   });
 
@@ -162,26 +163,99 @@ export default function ReplayTimeline() {
         }))
       : [];
 
+  const [maxIssueCount, maxOtherCount] = stackedData.reduce<[number, number]>(
+    (acc, bucket) => {
+      if ((bucket.data.issue ?? 0) > acc[0]) {
+        acc[0] = bucket.data.issue ?? 0;
+      }
+      const nonIssueTotal = Object.entries<number>(bucket.data)
+        .filter(([category]) => category !== 'issue')
+        .reduce((sum, [, value]) => {
+          return sum + (value ?? 0);
+        }, 0);
+
+      if (nonIssueTotal > acc[1]) {
+        acc[1] = nonIssueTotal;
+      }
+      return acc;
+    },
+    [0, 0]
+  );
+
+  console.log({maxIssueCount, maxOtherCount});
+
   const series1 =
     chapterFrames.length > 0
       ? [...new Set(chapterFrames.map(frame => getFrameOpOrCategory(frame)))]
           .filter(Boolean)
-          .map(category => ({
-            type: 'line',
-            areaStyle: {
+          .map(category => {
+            if (category === 'issue') {
+              const data = {
+                type: 'scatter',
+                xAxisIndex: 1,
+                // singleAxisIndex: 0,
+                // coordinateSystem: 'singleAxis',
+                showSymbol: true,
+                symbolSize: dataItem => {
+                  if (dataItem[2] === 0) {
+                    return 0;
+                  }
+                  // `maxIssueCount` is the max number of issues across all buckets.
+                  // `dataItem[1]` is the number of issues in the current bucket.
+                  // I want to scale the symbol size based on the number of issues in the current bucket relative to the max number of issues across all buckets.
+                  // I want to scale the symbol size between 4 and 16.
+                  return (dataItem[2] / maxIssueCount) * 12 + 4;
+                  // return (dataItem[1] / maxIssueCount) * 8;
+                },
+                // areaStyle: {
+                //   opacity: category === 'issue' ? 1 : 0.5,
+                // },
+                seriesName: category,
+                name: category,
+                // opacity: category === 'issue' ? 1 : 0.5,
+                smooth: true,
+                color: theme.tokens.graphics[frameDetails.get(category)],
+                data: stackedData.map(bucket => [
+                  bucket.time,
+                  maxOtherCount + 4,
+                  bucket.data[category] || 0,
+                ]),
+                // data: stackedData.map(bucket => ({
+                //   color: theme.tokens.graphics[frameDetails.get(category)],
+                //   opacity: category === 'issue' ? 1 : 0.5,
+                //   name: bucket.time,
+                //   value: bucket[category] || 0,
+                // })),
+              };
+              console.log({data});
+              return data;
+            }
+
+            return {
+              type: 'line',
+              xAxisIndex: 0,
+              stack: 'activity',
+              areaStyle: {
+                // opacity: 1,
+              },
+              lineStyle: {
+                color: theme.tokens.graphics[frameDetails.get(category)],
+                // opacity: 1,
+                width: 0.4,
+              },
+              showSymbol: false,
+              seriesName: category,
+              name: category,
               opacity: category === 'issue' ? 1 : 0.5,
-            },
-            seriesName: category,
-            opacity: category === 'issue' ? 1 : 0.5,
-            smooth: true,
-            color: theme.tokens.graphics[frameDetails.get(category)],
-            data: stackedData.map(bucket => ({
+              smooth: true,
               color: theme.tokens.graphics[frameDetails.get(category)],
-              opacity: category === 'issue' ? 1 : 0.5,
-              name: bucket.time,
-              value: bucket[category] || 0,
-            })),
-          }))
+              // data: stackedData.map(bucket => ({
+              //   name: bucket.time,
+              //   value: bucket[category] || 0,
+              // })),
+              data: stackedData.map(bucket => [bucket.time, bucket.data[category] || 0]),
+            };
+          })
       : [];
 
   console.log({chapterFrames, chapterByCategory, frameDetails, stackedData, series});
@@ -203,38 +277,68 @@ export default function ReplayTimeline() {
           videoEvents={videoEvents}
         />
         <div>
-          <StackedAreaChart
+          <BaseChart
+            stacked
             height={50}
             tooltip={{appendToBody: true}}
-            yAxis={{
-              show: false,
-              axisLabel: {
+            yAxes={[
+              {
                 show: false,
+                axisLabel: {
+                  show: false,
+                },
+                axisTick: {
+                  show: false,
+                },
+                axisLine: {
+                  show: false,
+                },
+                boundaryGap: false,
               },
-              axisTick: {
+              {
                 show: false,
+                axisLabel: {
+                  show: false,
+                },
+                axisTick: {
+                  show: false,
+                },
+                axisLine: {
+                  show: false,
+                },
+                boundaryGap: false,
               },
-              axisLine: {
-                show: false,
+            ]}
+            xAxes={[
+              {
+                boundaryGap: false,
+                axisTick: {show: false},
+                axisLine: {show: false},
+                axisLabel: {
+                  show: false,
+                },
+                splitLine: {
+                  show: false,
+                  lineStyle: {color: 'rgba(24, 20, 35, 0.05)'},
+                },
               },
-              boundaryGap: false,
-            }}
-            xAxis={{
-              boundaryGap: false,
-              axisTick: {show: false},
-              axisLine: {show: false},
-              axisLabel: {
-                show: false,
+              {
+                boundaryGap: false,
+                axisTick: {show: false},
+                axisLine: {show: false, onZero: false},
+                axisLabel: {
+                  show: false,
+                },
+                splitLine: {
+                  show: false,
+                },
               },
-              splitLine: {
-                show: false,
-              },
-            }}
+            ]}
             grid={{
-              left: 0,
-              right: 0,
-              top: 10,
-              bottom: 0,
+              left: 8,
+              right: 8,
+              top: 8,
+              bottom: 8,
             }}
             series={[
               ...series1.filter(s => s.seriesName === 'issue'),
