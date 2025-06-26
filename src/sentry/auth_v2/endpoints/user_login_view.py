624143c52bd92b3d437cbbf0a9409f1df103f409
Danny Lee <danny@dongwei.li>
2025-06-26 15:20:06 -0700

    fix(pr): Add publish_status to endpoint

--- src/sentry/auth_v2/endpoints/user_login_view.py
+++ src/sentry/auth_v2/endpoints/user_login_view.py
@@ -3,12 +3,16 @@ from rest_framework.request import Request
 from rest_framework.response import Response
 
 from sentry.api.api_owners import ApiOwner
+from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import Endpoint, control_silo_endpoint
 
 
 @control_silo_endpoint
 class UserLoginView(Endpoint):
     owner = ApiOwner.ENTERPRISE
+    publish_status = {
+        "GET": ApiPublishStatus.PRIVATE,
+    }
     permission_classes = (AllowAny,)
 
     def get(self, request: Request) -> Response:
