b6b427541599d46b5858a5927dc178390ecc12d9
Danny Lee <dlee@sentry.io>
2025-06-26 09:56:48 -0700

    feat(auth-v2): Set up feature flag (#93740)
    See https://github.com/getsentry/sentry-options-automator/pull/4203

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -58,6 +58,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:anr-analyze-frames", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Rollout of the new API rate limits for organization events
     manager.add("organizations:api-organization_events-rate-limit-reduced-rollout", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
+    # Enables the frontend to use the new auth flow for specific users.
+    manager.add("organizations:auth-v2", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enables the cron job to auto-enable codecov integrations.
     manager.add("organizations:auto-enable-codecov", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enable GenAI features such as Autofix and Issue Summary
