bac3e610e8d3353c11c1f3e3e1a5c9cd555be235
Danny Lee <danny@dongwei.li>
2025-06-25 23:52:33 -0700

    fix(pr): Add API Owner

--- src/sentry/auth_v2/api/user_login_view.py
+++ src/sentry/auth_v2/api/user_login_view.py
@@ -2,11 +2,13 @@ from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 
+from sentry.api.api_owners import ApiOwner
 from sentry.api.base import Endpoint, control_silo_endpoint
 
 
 @control_silo_endpoint
 class UserLoginView(Endpoint):
+    owner = ApiOwner.ENTERPRISE
     permission_classes = (AllowAny,)
 
     def get(self, request: Request) -> Response:
