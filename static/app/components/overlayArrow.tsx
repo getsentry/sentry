ce06d1c2e5e3e4b320f1d8ec8f2dc27587074865
JonasBa <jonas@badalic.com>
2025-06-27 12:08:24 -0400

    tooltip: use single rect

--- static/app/components/overlayArrow.tsx
+++ static/app/components/overlayArrow.tsx
@@ -18,47 +18,40 @@ export interface OverlayArrowProps extends React.ComponentPropsWithRef<'div'> {
 
 export const OverlayArrow = withChonk(LegacyOverlayArrow, ChonkOverlayArrow);
 
-function ChonkOverlayArrow({placement, ref}: OverlayArrowProps) {
+function ChonkOverlayArrow({
+  placement,
+  ref,
+  size = 16,
+  background,
+  border,
+}: OverlayArrowProps) {
   const theme = useTheme();
-  const fill = theme.tokens.background.primary;
-  const stroke = theme.tokens.border.primary;
-  const arrowProps = {fill, stroke};
+
+  const offset = placement?.startsWith('top') ? 3 : 1.5;
+  const topOffset = placement?.startsWith('top') ? 3 : 1.5;
+  const sizeRatio = 0.5;
+  const heightRatio = 0.3;
 
   return (
-    <ChonkWrap size={20} ref={ref} placement={placement}>
-      {placement?.startsWith('top') && <ArrowBottom {...arrowProps} />}
-      {placement?.startsWith('bottom') && <ArrowTop {...arrowProps} />}
-      {(placement?.startsWith('right') || placement?.startsWith('left')) && (
-        <ArrowLeft {...arrowProps} />
-      )}
+    <ChonkWrap size={size} ref={ref} placement={placement}>
+      <svg
+        width={size * sizeRatio}
+        height={size * sizeRatio}
+        viewBox={`0 0 ${size} ${size}`}
+        fill="none"
+      >
+        <polygon
+          points={`0,0 ${size}, 0 ${size / 2},${size * heightRatio + topOffset}`}
+          fill={border ? (theme[border] as string) : theme.tokens.border.primary}
+        />
+        <polygon
+          points={`${offset},0 ${size - offset}, 0 ${size / 2},${size * heightRatio}`}
+          fill={
+            background ? (theme[background] as string) : theme.tokens.background.primary
+          }
+        />
+      </svg>
     </ChonkWrap>
-  );
-}
-
-function ArrowBottom({stroke, fill}: {fill: string; stroke: string}) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 6">
-      <path fill={stroke} d="M12.2 5a4 4 0 0 1-4.4 0L.4 0h19.2l-7.4 5Z" />
-      <path fill={fill} d="M11.1 3.3a2 2 0 0 1-2.2 0L4 0h12l-4.9 3.3Z" />
-    </svg>
-  );
-}
-
-function ArrowTop({stroke, fill}: {fill: string; stroke: string}) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 5">
-      <path fill={stroke} d="M12.2 1a4 4 0 0 0-4.4 0L1.9 5h16.2l-5.9-4Z" />
-      <path fill={fill} d="M11.1 1.7a2 2 0 0 0-2.2 0L4 5h12l-4.9-3.3Z" />
-    </svg>
-  );
-}
-
-function ArrowLeft({stroke, fill}: {fill: string; stroke: string}) {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 18">
-      <path fill={stroke} d="M5.4 17.2 1 10.8a4.4 4.4 0 0 1 0-4.4L5.4 0v17.2Z" />
-      <path fill={fill} d="M2 7.5a2 2 0 0 0 0 2.1l3.4 4.7V2.9L2 7.5Z" />
-    </svg>
   );
 }
 
@@ -66,34 +59,28 @@ const ChonkWrap = chonkStyled('div')<{
   size: number;
   placement?: PopperProps<any>['placement'];
 }>`
-  position: relative;
-  display: flex;
-
-  > svg {
-    position: absolute;
-    bottom: 50%;
-    width: 20px;
-  }
+  width: ${p => p.size}px;
+  height: ${p => p.size}px;
+  position: absolute;
+  transform-origin: center;
 
   ${p =>
     p.placement?.startsWith('top') &&
-    `bottom: 0; transform: translateY(50%) rotate(90deg);`}
-  ${p => p.placement?.startsWith('bottom') && `top: 0; transform: translateY(-50%) ;`}
-  ${p => p.placement?.startsWith('left') && `right: 0; transform: translateX(50%);`}
+    `top: 100%; left: 50%; transform: translateX(-50%) rotate(0deg);`}
+  ${p => p.placement?.startsWith('bottom') && `bottom: 100%; left: 50%; transform: translateX(-50%) rotate(180deg);`}
+  ${p => p.placement?.startsWith('left') && `left: 100%; top: 50%; transform: translateY(-50%) rotate(-90deg);`}
   ${p =>
     p.placement?.startsWith('right') &&
-    `left: 0; transform: translateX(-50%) rotate(-90deg);`}
+    `right: 100%; top: 50%; transform: translateY(-50%) rotate(90deg);`}
+
+  > svg {
+    width: ${p => p.size}px;
+    height: ${p => p.size}px;
+  }
+
 `;
 
-function LegacyOverlayArrow({
-  size = 16,
-  strokeWidth = 1,
-  placement,
-  background = 'backgroundElevated',
-  border = 'translucentBorder',
-  ref,
-  ...props
-}: OverlayArrowProps) {
+function LegacyOverlayArrow({size = 16, placement, ref, ...props}: OverlayArrowProps) {
   /**
    * SVG height
    */
@@ -105,7 +92,7 @@ function LegacyOverlayArrow({
   /**
    * SVG stroke width
    */
-  const s = strokeWidth;
+  const s = 1;
   const arrowPath = [
     `M 0 ${h - s / 2}`,
     `C ${w * 0.25} ${h - s / 2} ${w * 0.45} ${s / 2} ${w / 2} ${s / 2}`,
@@ -122,18 +109,12 @@ function LegacyOverlayArrow({
         width={w}
         height={h}
         viewBox={`0 0 ${w} ${h}`}
-        background={background}
-        border={border}
+        background="backgroundElevated"
+        border="translucentBorder"
       >
         <defs>
           <mask id={strokeMaskId}>
-            <rect
-              x="0"
-              y={-strokeWidth}
-              width="100%"
-              height={h + strokeWidth + 4}
-              fill="white"
-            />
+            <rect x="0" y={-1} width="100%" height={h + 1 + 4} fill="white" />
           </mask>
           <mask id={fillMaskId}>
             <rect x="0" y="0" width="100%" height="100%" fill="white" />
