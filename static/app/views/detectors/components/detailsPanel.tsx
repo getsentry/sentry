e7ecd4c0fa75315405ed0f9b3ab6614eed958842
Scott Cooper <scttcper@gmail.com>
2025-06-27 12:40:52 -0700

    feat(aci): Setup detector details sidebar more (#94442)

--- static/app/views/detectors/components/detailsPanel.tsx
+++ static/app/views/detectors/components/detailsPanel.tsx
@@ -1,3 +1,4 @@
+import {Fragment} from 'react';
 import styled from '@emotion/styled';
 
 import {Flex} from 'sentry/components/core/layout';
@@ -21,13 +22,17 @@ function SnubaQueryDetails({dataSource}: {dataSource: SnubaQueryDataSource}) {
       <Flex direction="column" gap={space(0.5)}>
         <Heading>{t('Query:')}</Heading>
         <Query>
-          <Label>{t('visualize:')}</Label>{' '}
+          <Label>{t('visualize:')}</Label>
           <Value>{dataSource.queryObj.snubaQuery.aggregate}</Value>
-          <Label>{t('where:')}</Label>{' '}
-          <Value>{dataSource.queryObj.snubaQuery.query}</Value>
+          {dataSource.queryObj.snubaQuery.query && (
+            <Fragment>
+              <Label>{t('where:')}</Label>
+              <Value>{dataSource.queryObj.snubaQuery.query}</Value>
+            </Fragment>
+          )}
         </Query>
       </Flex>
-      <Flex gap={space(0.5)}>
+      <Flex gap={space(0.5)} align="center">
         <Heading>{t('Threshold:')}</Heading>
         <Value>{getExactDuration(dataSource.queryObj.snubaQuery.timeWindow, true)}</Value>
       </Flex>
@@ -63,9 +68,8 @@ const Heading = styled('h4')`
 const Query = styled('dl')`
   display: grid;
   grid-template-columns: auto auto;
-  grid-template-rows: repeat(3, 1fr);
   width: fit-content;
-  gap: ${space(0.5)} ${space(1)};
+  gap: ${space(0.25)} ${space(0.5)};
   margin: 0;
 `;
 
@@ -76,7 +80,7 @@ const Label = styled('dt')`
 `;
 
 const Value = styled('dl')`
-  ${p => p.theme.overflowEllipsis};
+  word-break: break-all;
   margin: 0;
 `;
 
