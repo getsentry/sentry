ba58c731abb9ea4b7426cf1885be3878fac7b023
Malachi Willey <malwilley@gmail.com>
2025-06-27 08:37:43 -0700

    feat(aci): Add skeleton cron and uptime detector forms

---
+++ static/app/views/detectors/components/forms/uptime/index.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled';
+
+import {Container} from 'sentry/components/workflowEngine/ui/container';
+import Section from 'sentry/components/workflowEngine/ui/section';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {AssigneeField} from 'sentry/views/detectors/components/forms/assigneeField';
+import {UptimeDetectorFormDetectSection} from 'sentry/views/detectors/components/forms/uptime/detect';
+
+export function UptimeDetectorForm() {
+  return (
+    <FormStack>
+      <UptimeDetectorFormDetectSection />
+      <AssignSection />
+    </FormStack>
+  );
+}
+
+function AssignSection() {
+  return (
+    <Container>
+      <Section title={t('Assign')}>
+        <AssigneeField />
+      </Section>
+    </Container>
+  );
+}
+
+const FormStack = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: ${space(3)};
+  max-width: ${p => p.theme.breakpoints.xl};
+`;
