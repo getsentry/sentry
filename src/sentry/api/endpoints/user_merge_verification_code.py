2965ce6b71197d3571863376a139822961507e83
Michelle Fu <michelle.fu@sentry.io>
2025-06-27 12:04:48 -0700

    first pass

---
+++ src/sentry/api/endpoints/user_merge_verification_code.py
@@ -0,0 +1,51 @@
+from django.contrib.auth.models import AnonymousUser
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from sentry.api.api_publish_status import ApiPublishStatus
+from sentry.api.base import Endpoint, control_silo_endpoint
+from sentry.api.permissions import SentryIsAuthenticated
+from sentry.users.models.user_merge_verification_code import UserMergeVerificationCode
+
+
+@control_silo_endpoint
+class AuthMergeUserAccountsEndpoint(Endpoint):
+    publish_status = {
+        "POST": ApiPublishStatus.PRIVATE,
+        "PUT": ApiPublishStatus.PRIVATE,
+    }
+    permission_classes = (SentryIsAuthenticated,)
+    """
+    List and merge user accounts with the same primary email address.
+    """
+
+    def post(self, request: Request) -> Response:
+        user = request.user
+        if isinstance(user, AnonymousUser):
+            return Response(
+                status=401,
+                data={"error": "You must be authenticated to use this endpoint"},
+            )
+
+        UserMergeVerificationCode.objects.create(user=user)
+        # TODO: send email
+        return Response("Successfully posted merge account verification code.")
+
+    def put(self, request: Request) -> Response:
+        user = request.user
+        if isinstance(user, AnonymousUser):
+            return Response(
+                status=401,
+                data={"error": "You must be authenticated to use this endpoint"},
+            )
+
+        try:
+            code: UserMergeVerificationCode = UserMergeVerificationCode.objects.get(user=user)
+        except UserMergeVerificationCode.DoesNotExist:
+            return Response(
+                status=404,
+                data={"error": "No verification code exists for the requesting user."},
+            )
+        code.regenerate_token()
+        # TODO: send email
+        return Response("Successfully regenerated merge account verification code.")
