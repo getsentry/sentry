a3095d0e2a18a77ebd8dfcbd7fb6429e0737b032
Michelle Fu <michelle.fu@sentry.io>
2025-06-30 12:41:51 -0700

    implement ivy design with placeholder

--- static/app/views/settings/account/mergeAccounts.tsx
+++ static/app/views/settings/account/mergeAccounts.tsx
@@ -1,4 +1,4 @@
-import {Fragment} from 'react';
+import {createContext, Fragment, useContext} from 'react';
 import styled from '@emotion/styled';
 
 import {Button} from 'sentry/components/core/button';
@@ -19,6 +19,7 @@ type UserRowProps = {
   organizations: string;
   // more stuff for checkbox
 };
+const UserContext = createContext<boolean>(false);
 
 function MergeAccounts() {
   return (
@@ -28,11 +29,17 @@ function MergeAccounts() {
 
       <TextBlock>
         {t(`Select the accounts that you want to merge into your currently active account,
-          then confirm and merge.`)}
+          then confirm and merge. The accounts that you do not select will be deleted.`)}
       </TextBlock>
-
-      <Users />
-      <Button priority="danger">Confirm and merge</Button>
+      <TextBlock>{t(`Your currently active account:`)}</TextBlock>
+      <UserContext value>
+        <Users />
+      </UserContext>
+      <TextBlock>{t(`Your other accounts:`)}</TextBlock>
+      <UserContext value={false}>
+        <Users />
+      </UserContext>
+      <Button priority="danger">Merge [3] accounts into [michelle.primary]</Button>
     </Fragment>
   );
 }
@@ -40,13 +47,32 @@ function MergeAccounts() {
 export default MergeAccounts;
 
 function Users() {
+  const isPrimaryUser = useContext(UserContext);
+  if (isPrimaryUser) {
+    return (
+      <Panel>
+        <UserPanelHeader>
+          <div>{t('Name')}</div>
+          <div>{t('Last Active')}</div>
+          <div>{t('Organizations')}</div>
+        </UserPanelHeader>
+        <PanelBody>
+          <UserRow
+            name="michelle"
+            lastSeen="05 October 2024 14:48 UTC"
+            organizations="hojicha, matcha, sentry, sencha, mf-test-n7"
+          />
+        </PanelBody>
+      </Panel>
+    );
+  }
   return (
     <Panel>
       <UserPanelHeader>
         <div>{t('Name')}</div>
         <div>{t('Last Active')}</div>
         <div>{t('Organizations')}</div>
-        <div>{t('Keep')}</div>
+        <div>{t('Merge')}</div>
       </UserPanelHeader>
       <PanelBody>
         <UserRow
@@ -54,33 +80,38 @@ function Users() {
           lastSeen="05 October 2024 14:48 UTC"
           organizations="hojicha, matcha, sentry, sencha, mf-test-n7"
         />
-        <UserRow
-          name="michelle.fu"
-          lastSeen="05 October 2011 14:48 UTC"
-          organizations=""
-        />
+        <UserRow name="michelle.fu" lastSeen="" organizations="" />
       </PanelBody>
     </Panel>
   );
 }
 
 function UserRow({name, lastSeen, organizations}: UserRowProps) {
+  const isPrimaryUser = useContext(UserContext);
   return (
     <UserPanelItem>
       <Name>{name}</Name>
-      <div>
-        <StyledTimeSince date={lastSeen} />
-      </div>
+      {isPrimaryUser ? (
+        'Currently active'
+      ) : lastSeen === '' ? (
+        'Never'
+      ) : (
+        <div>
+          <StyledTimeSince date={lastSeen} />
+        </div>
+      )}
       <Organizations>{organizations}</Organizations>
-      <div>
-        <input
-          type="checkbox"
-          name="user"
-          value={name}
-          onChange={() => null}
-          style={{margin: 5}}
-        />
-      </div>
+      {isPrimaryUser ? null : (
+        <div>
+          <input
+            type="checkbox"
+            name="user"
+            value={name}
+            onChange={() => null}
+            style={{margin: 5}}
+          />
+        </div>
+      )}
     </UserPanelItem>
   );
 }
@@ -98,11 +129,11 @@ const UserPanelItem = styled(PanelItem)`
 
 const Name = styled('div')`
   margin-bottom: ${space(0.5)};
-  font-weight: ${p => p.theme.fontWeightBold};
+  font-weight: ${p => p.theme.fontWeight.bold};
 `;
 
 const StyledTimeSince = styled(TimeSince)`
-  font-size: ${p => p.theme.fontSizeRelativeSmall};
+  font-size: ${p => p.theme.fontSize.md};
 `;
 
 const UserPanelHeader = styled(PanelHeader)`
