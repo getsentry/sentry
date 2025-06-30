e7c888547bc46e773503d665cae23e19b1a6b88a
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2025-06-30 11:23:08 -0400

    feat(detectors): Update query injection detectors  (#94568)
    this pr makes a few updates to the query injection detectors:
    1) if the matched value is a string, it replaces the whole string
    instead of just the contents (replaced "hello" instead of just hello in
    the description)
    2) Replaces the ? in the description with [UNTRUSTED_INPUT] to make it
    more clear that the query has a problem. the previous version could look
    too much like a properly parameterized query.

--- tests/sentry/performance_issues/test_sql_injection_detector.py
+++ tests/sentry/performance_issues/test_sql_injection_detector.py
@@ -33,9 +33,12 @@ class SQLInjectionDetectorTest(TestCase):
         assert len(problems) == 1
         problem = problems[0]
         assert problem.type == DBQueryInjectionVulnerabilityGroupType
-        assert problem.fingerprint == "1-1020-d9460b7b6c17b64a51f0390b53390cb1b4c39662"
+        assert problem.fingerprint == "1-1020-29c2b54639697556914d1bbb843f0bbb8cdd3397"
         assert problem.op == "db"
-        assert problem.desc == "SELECT * FROM users WHERE username = '?' ORDER BY username ASC"
+        assert (
+            problem.desc
+            == "SELECT * FROM users WHERE username = [UNTRUSTED_INPUT] ORDER BY username ASC"
+        )
         assert problem.evidence_data is not None
         assert problem.evidence_data["vulnerable_parameters"] == [("username", "hello")]
         assert problem.evidence_data["request_url"] == "http://localhost:3001/vulnerable-login"
@@ -47,9 +50,9 @@ class SQLInjectionDetectorTest(TestCase):
         assert len(problems) == 1
         problem = problems[0]
         assert problem.type == DBQueryInjectionVulnerabilityGroupType
-        assert problem.fingerprint == "1-1020-841b1fd77bae6e89b3570a2ab0bf43d3c8cfbac6"
+        assert problem.fingerprint == "1-1020-197d2a9a95568a75551e4448423a7fe658ac21d2"
         assert problem.op == "db"
-        assert problem.desc == "SELECT * FROM users WHERE username = '?'"
+        assert problem.desc == "SELECT * FROM users WHERE username = [UNTRUSTED_INPUT]"
         assert problem.evidence_data is not None
         assert problem.evidence_data["vulnerable_parameters"] == [("username", "hello")]
         assert problem.evidence_data["request_url"] == "http://localhost:3001/vulnerable-login"
