0b3f7254536e8b52655ea1d8eeb698e7da70304c
Armen Zambrano G <44410+armenzg@users.noreply.github.com>
2025-06-30 12:39:37 -0400

    WIP

--- static/app/components/events/interfaces/frame/stacktraceLinkModal.tsx
+++ static/app/components/events/interfaces/frame/stacktraceLinkModal.tsx
@@ -81,11 +81,13 @@ function StacktraceLinkModal({
   );
 
   const suggestions = uniq(
-    suggestedCodeMappings
-      ?.map(suggestion => {
-        return `https://github.com/${suggestion.repo_name}/blob/${suggestion.repo_branch}/${suggestion.filename}`;
-      })
-      .slice(0, 2)
+    Array.isArray(suggestedCodeMappings)
+      ? suggestedCodeMappings
+          .map(suggestion => {
+            return `https://github.com/${suggestion.repo_name}/blob/${suggestion.repo_branch}/${suggestion.filename}`;
+          })
+          .slice(0, 2)
+      : []
   );
 
   const onHandleChange = (input: string) => {
