9d1ff8949daeb47f79a422f3b8963cfe65d4060f
Michelle Fu <michelle.fu@sentry.io>
2025-06-30 16:19:48 -0700

    full design with placeholder data

--- static/app/views/settings/account/mergeAccounts.tsx
+++ static/app/views/settings/account/mergeAccounts.tsx
@@ -2,6 +2,9 @@ import {createContext, Fragment, useContext} from 'react';
 import styled from '@emotion/styled';
 
 import {Button} from 'sentry/components/core/button';
+import {Input} from 'sentry/components/core/input';
+import List from 'sentry/components/list';
+import ListItem from 'sentry/components/list/listItem';
 import Panel from 'sentry/components/panels/panel';
 import PanelBody from 'sentry/components/panels/panelBody';
 import PanelHeader from 'sentry/components/panels/panelHeader';
@@ -26,7 +29,30 @@ function MergeAccounts() {
     <Fragment>
       <SentryDocumentTitle title={t('Merge Accounts')} />
       <SettingsPageHeader title={t('Merge Accounts')} />
+      <List symbol="colored-numeric">
+        <StyledListItem>{t('Generate Verification Code')}</StyledListItem>
+        <div>{t(`Check your email for your code. You'll need it in Step 3.`)}</div>
+        <ButtonSection>
+          <Button priority="primary">Generate verification code</Button>
+        </ButtonSection>
+        {renderSelectAccounts()}
+        <StyledListItem>{t('Enter Your Verification Code')}</StyledListItem>
+        <StyledInput type="text" />
+        <StyledListItem>{t('Submit')}</StyledListItem>
+        <ButtonSection>
+          <Button priority="danger">Merge [3] accounts into [michelle.primary]</Button>
+        </ButtonSection>
+      </List>
+    </Fragment>
+  );
+}
 
+export default MergeAccounts;
+
+function renderSelectAccounts() {
+  return (
+    <Fragment>
+      <StyledListItem>{t('Select Your Accounts')}</StyledListItem>
       <TextBlock>
         {t(`Select the accounts that you want to merge into your currently active account,
           then confirm and merge. The accounts that you do not select will be deleted.`)}
@@ -39,12 +65,9 @@ function MergeAccounts() {
       <UserContext value={false}>
         <Users />
       </UserContext>
-      <Button priority="danger">Merge [3] accounts into [michelle.primary]</Button>
     </Fragment>
   );
 }
-
-export default MergeAccounts;
 
 function Users() {
   const isPrimaryUser = useContext(UserContext);
@@ -143,4 +166,21 @@ const UserPanelHeader = styled(PanelHeader)`
 
 const Organizations = styled('div')`
   margin-bottom: ${space(0.5)};
+`;
+
+const StyledListItem = styled(ListItem)`
+  margin-bottom: ${space(0.5)};
+  font-size: ${p => p.theme.fontSize.xl};
+  line-height: 1.3;
+`;
+
+const ButtonSection = styled('div')`
+  margin-top: ${space(1)};
+  margin-bottom: ${space(3)};
+`;
+
+const StyledInput = styled(Input)`
+  margin-top: ${space(1)};
+  margin-bottom: ${space(3)};
+  flex: 1;
 `;
