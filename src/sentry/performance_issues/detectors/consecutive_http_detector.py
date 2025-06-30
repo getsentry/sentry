53c7717dc6ed958eda43fd22111268a51ae20d55
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2025-06-30 15:09:28 -0700

    add more filters

--- src/sentry/performance_issues/detectors/consecutive_http_detector.py
+++ src/sentry/performance_issues/detectors/consecutive_http_detector.py
@@ -160,7 +160,21 @@ class ConsecutiveHTTPSpanDetector(PerformanceDetector):
         ):  # Just using all methods to see if anything interesting pops up
             return False
 
-        if any([x in description for x in ["_next/static/", "_next/data/", "googleapis.com"]]):
+        if any(
+            [
+                x in description
+                for x in [
+                    "_next/static/",
+                    "_next/data/",
+                    "googleapis.com",
+                    "amazonaws.com",
+                    "core.windows.net",
+                    "azure.com",
+                    "azure.net",
+                    "microsoftonline.com",
+                ]
+            ]
+        ):
             return False
 
         return True
