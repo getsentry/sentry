5efe559bc79ff2a5060a1f688b001d75bce3242c
Fabian Schindler <fabian.schindler@sentry.io>
2025-06-30 14:00:37 +0200

    ci: -m ref(demo-mode): cleaning up DemoModeGuard middleware Removing options: - demo-mode.disable-sandbox-redirect - demo-mode.sandbox-redirect-logout

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3114,18 +3114,6 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
-register(
-    "demo-mode.disable-sandbox-redirect",
-    default=False,
-    flags=FLAG_AUTOMATOR_MODIFIABLE,
-)
-
-register(
-    "demo-mode.sandbox-redirect-logout",
-    default=False,
-    flags=FLAG_AUTOMATOR_MODIFIABLE,
-)
-
 # option for sample size when fetching project tag keys
 register(
     "visibility.tag-key-sample-size",
