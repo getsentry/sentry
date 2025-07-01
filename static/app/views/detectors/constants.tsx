e63c4c38b6461166abd1ad8d642edabeda69ef30
Malachi Willey <malwilley@gmail.com>
2025-07-01 14:29:30 -0700

    Fix typo on cron type

--- static/app/views/detectors/constants.tsx
+++ static/app/views/detectors/constants.tsx
@@ -8,7 +8,7 @@ export const DETECTOR_TYPE_LABELS: Record<DetectorType, string> = {
   metric_issue: t('Metric'),
   uptime_domain_failure: t('Uptime'),
   error: t('Error'),
-  uptime_subscription: t('Uptime'),
+  uptime_subscription: t('Cron'),
 };
 
 export const DETECTOR_FILTER_KEYS: Record<
