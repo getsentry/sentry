9a63a075d11ac71d17a493029fa9bf0c3349c07e
Billy Vong <billyvg@users.noreply.github.com>
2025-07-01 17:24:13 -0400

    feat(replay): Update design of Replay Summaries (#94569)
    Updates the Replay Summaries tab to be a little more aligned with
    Jesse's designs.
    
    For now, I have the chapter title bolded when the chapter is "active"
    
    closed:
    <img width="669" alt="image"
    src="https://github.com/user-attachments/assets/8ffad263-704a-4b80-be84-bccb0297d4ae"
    />
    
    opened:
    <img width="674" alt="image"
    src="https://github.com/user-attachments/assets/c2654939-ecf2-4961-9c99-b90efda6ff40"
    />
    
    multiple:
    <img width="664" alt="image"
    src="https://github.com/user-attachments/assets/e7a7cad1-977a-49eb-8976-30ff04fe90eb"
    />
    
    ---------
    
    Co-authored-by: Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>

---
+++ static/app/views/replays/detail/ai/ai.tsx
@@ -0,0 +1,227 @@
+import styled from '@emotion/styled';
+
+import {Alert} from 'sentry/components/core/alert';
+import {Badge} from 'sentry/components/core/badge';
+import {Button} from 'sentry/components/core/button';
+import {Flex} from 'sentry/components/core/layout';
+import ErrorBoundary from 'sentry/components/errorBoundary';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {useReplayContext} from 'sentry/components/replays/replayContext';
+import {IconSync, IconThumb} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import {useFeedbackForm} from 'sentry/utils/useFeedbackForm';
+import useOrganization from 'sentry/utils/useOrganization';
+import useProjectFromId from 'sentry/utils/useProjectFromId';
+import {ChapterList} from 'sentry/views/replays/detail/ai/chapterList';
+import FluidHeight from 'sentry/views/replays/detail/layout/fluidHeight';
+import TabItemContainer from 'sentry/views/replays/detail/tabItemContainer';
+
+import {useFetchReplaySummary} from './useFetchReplaySummary';
+
+export default function Ai() {
+  return (
+    <PaddedFluidHeight>
+      <SummaryTabItemContainer data-test-id="replay-details-ai-summary-tab">
+        <ErrorBoundary mini>
+          <AiContent />
+        </ErrorBoundary>
+      </SummaryTabItemContainer>
+    </PaddedFluidHeight>
+  );
+}
+
+function AiContent() {
+  const organization = useOrganization();
+  const {replay} = useReplayContext();
+  const replayRecord = replay?.getReplay();
+  const project = useProjectFromId({project_id: replayRecord?.project_id});
+  const {
+    data: summaryData,
+    isPending,
+    isError,
+    isRefetching,
+    refetch,
+  } = useFetchReplaySummary({
+    staleTime: 0,
+    enabled: Boolean(
+      replayRecord?.id &&
+        project?.slug &&
+        organization.features.includes('replay-ai-summaries')
+    ),
+    retry: false,
+  });
+
+  const openForm = useFeedbackForm();
+
+  const feedbackButton = ({type}: {type: 'positive' | 'negative'}) => {
+    return openForm ? (
+      <Button
+        aria-label={t('Give feedback on the AI summary section')}
+        icon={<IconThumb direction={type === 'positive' ? 'up' : 'down'} />}
+        title={type === 'positive' ? t('I like this') : t(`I don't like this`)}
+        size={'xs'}
+        onClick={() =>
+          openForm({
+            messagePlaceholder:
+              type === 'positive'
+                ? t('What did you like about the AI summary and chapters?')
+                : t('How can we make the AI summary and chapters work better for you?'),
+            tags: {
+              ['feedback.source']: 'replay_ai_summary',
+              ['feedback.owner']: 'replay',
+              ['feedback.type']: type,
+            },
+          })
+        }
+      />
+    ) : null;
+  };
+
+  if (!organization.features.includes('replay-ai-summaries')) {
+    return (
+      <SummaryContainer>
+        <Alert type="info">
+          {t('Replay AI summary is not available for this organization.')}
+        </Alert>
+      </SummaryContainer>
+    );
+  }
+
+  if (replayRecord?.project_id && !project) {
+    return (
+      <SummaryContainer>
+        <Alert type="error">{t('Project not found. Unable to load AI summary.')}</Alert>
+      </SummaryContainer>
+    );
+  }
+
+  if (isPending || isRefetching) {
+    return (
+      <LoadingContainer>
+        <LoadingIndicator />
+      </LoadingContainer>
+    );
+  }
+
+  if (isError) {
+    return (
+      <SummaryContainer>
+        <Alert type="error">{t('Failed to load AI summary')}</Alert>
+      </SummaryContainer>
+    );
+  }
+
+  if (!summaryData) {
+    return (
+      <SummaryContainer>
+        <Alert type="info">{t('No summary available for this replay.')}</Alert>
+      </SummaryContainer>
+    );
+  }
+
+  return (
+    <ErrorBoundary mini>
+      <SplitContainer>
+        <Summary>
+          <SummaryLeft>
+            <SummaryLeftTitle>
+              {t('Replay Summary')}
+              <Badge type="internal">{t('Internal')}</Badge>
+            </SummaryLeftTitle>
+            <SummaryText>{summaryData.data.summary}</SummaryText>
+          </SummaryLeft>
+          <SummaryRight>
+            <Flex gap={space(0.5)}>
+              {feedbackButton({type: 'positive'})}
+              {feedbackButton({type: 'negative'})}
+            </Flex>
+            <Button
+              priority="default"
+              type="button"
+              size="xs"
+              onClick={() => refetch()}
+              icon={<IconSync size="xs" />}
+            >
+              {t('Regenerate')}
+            </Button>
+          </SummaryRight>
+        </Summary>
+        <ChapterList summaryData={summaryData} />
+      </SplitContainer>
+    </ErrorBoundary>
+  );
+}
+
+const SummaryTabItemContainer = styled(TabItemContainer)`
+  .beforeHoverTime:last-child {
+    border-bottom-color: transparent;
+  }
+  .beforeCurrentTime:last-child {
+    border-bottom-color: transparent;
+  }
+  details.beforeHoverTime + details.afterHoverTime,
+  details.beforeCurrentTime + details.afterCurrentTime {
+    border-top-color: transparent;
+  }
+`;
+
+const SplitContainer = styled('div')`
+  display: flex;
+  flex-direction: column;
+  overflow: auto;
+`;
+
+const PaddedFluidHeight = styled(FluidHeight)`
+  padding-top: ${space(1)};
+`;
+
+const LoadingContainer = styled('div')`
+  display: flex;
+  justify-content: center;
+  padding: ${space(4)};
+`;
+
+const SummaryContainer = styled('div')`
+  padding: ${space(2)};
+  overflow: auto;
+`;
+
+const Summary = styled('div')`
+  display: flex;
+  align-items: center;
+  padding: ${space(1)} ${space(1.5)};
+  border-bottom: 1px solid ${p => p.theme.border};
+  gap: ${space(4)};
+`;
+
+const SummaryLeft = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: ${space(0.5)};
+  justify-content: space-between;
+  font-size: ${p => p.theme.fontSize.lg};
+  font-weight: ${p => p.theme.fontWeight.bold};
+`;
+
+const SummaryRight = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: ${space(1)};
+  align-items: flex-end;
+`;
+
+const SummaryLeftTitle = styled('div')`
+  display: flex;
+  align-items: center;
+  gap: ${space(1)};
+`;
+
+const SummaryText = styled('p')`
+  line-height: 1.6;
+  white-space: pre-wrap;
+  margin: 0;
+  font-size: ${p => p.theme.fontSize.md};
+  color: ${p => p.theme.subText};
+  font-weight: ${p => p.theme.fontWeight.normal};
+`;
---
+++ static/app/views/replays/detail/ai/chapterList.tsx
@@ -0,0 +1,263 @@
+import {useCallback, useMemo} from 'react';
+import styled from '@emotion/styled';
+import classNames from 'classnames';
+
+import {Alert} from 'sentry/components/core/alert';
+import EmptyMessage from 'sentry/components/emptyMessage';
+import {useReplayContext} from 'sentry/components/replays/replayContext';
+import {IconChevron} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {space} from 'sentry/styles/space';
+import useCrumbHandlers from 'sentry/utils/replays/hooks/useCrumbHandlers';
+import useCurrentHoverTime from 'sentry/utils/replays/playback/providers/useCurrentHoverTime';
+import type {ReplayFrame} from 'sentry/utils/replays/types';
+import BreadcrumbRow from 'sentry/views/replays/detail/breadcrumbs/breadcrumbRow';
+import TimestampButton from 'sentry/views/replays/detail/timestampButton';
+
+import type {SummaryResponse} from './useFetchReplaySummary';
+
+interface Props {
+  summaryData: SummaryResponse;
+}
+
+export function ChapterList({summaryData}: Props) {
+  const {replay, setCurrentTime} = useReplayContext();
+  const onClickChapterTimestamp = useCallback(
+    (event: React.MouseEvent<Element>, start: number) => {
+      event.stopPropagation();
+      setCurrentTime(start - (replay?.getStartTimestampMs() ?? 0));
+    },
+    [replay, setCurrentTime]
+  );
+
+  const chapterData = useMemo(
+    () =>
+      summaryData?.data.time_ranges
+        .map(({period_title, period_start, period_end}) => ({
+          title: period_title,
+          start: period_start,
+          end: period_end,
+          breadcrumbs:
+            replay
+              ?.getChapterFrames()
+              .filter(
+                breadcrumb =>
+                  breadcrumb.timestampMs >= period_start &&
+                  breadcrumb.timestampMs <= period_end
+              ) ?? [],
+        }))
+        .sort((a, b) => a.start - b.start),
+    [summaryData, replay]
+  );
+
+  if (!chapterData.length) {
+    return (
+      <EmptyContainer>
+        <Alert type="info">{t('No chapters available for this replay.')}</Alert>
+      </EmptyContainer>
+    );
+  }
+
+  return (
+    <ChaptersList>
+      {chapterData.map(({title, start, end, breadcrumbs}, i) => (
+        <ChapterRow
+          key={i}
+          title={title}
+          start={start}
+          end={end}
+          breadcrumbs={breadcrumbs}
+          onClickChapterTimestamp={onClickChapterTimestamp}
+        />
+      ))}
+    </ChaptersList>
+  );
+}
+
+function ChapterRow({
+  title,
+  start,
+  end,
+  breadcrumbs,
+  onClickChapterTimestamp,
+  className,
+}: {
+  breadcrumbs: ReplayFrame[];
+  end: number;
+  onClickChapterTimestamp: (event: React.MouseEvent<Element>, start: number) => void;
+  start: number;
+  title: string;
+  className?: string;
+}) {
+  const {replay, currentTime} = useReplayContext();
+  const {onClickTimestamp} = useCrumbHandlers();
+  const [currentHoverTime] = useCurrentHoverTime();
+
+  const startOffset = Math.max(start - (replay?.getStartTimestampMs() ?? 0), 0);
+  const endOffset = Math.max(end - (replay?.getStartTimestampMs() ?? 0), 0);
+  const hasOccurred = currentTime >= startOffset;
+  const isBeforeHover = currentHoverTime === undefined || currentHoverTime >= startOffset;
+
+  return (
+    <ChapterWrapper
+      className={classNames(className, {
+        beforeCurrentTime: hasOccurred,
+        afterCurrentTime: !hasOccurred,
+        activeChapter: currentTime >= startOffset && currentTime <= endOffset,
+        beforeHoverTime: currentHoverTime === undefined ? undefined : isBeforeHover,
+        afterHoverTime: currentHoverTime === undefined ? undefined : !isBeforeHover,
+      })}
+    >
+      <Chapter>
+        <ChapterIconArrowWrapper>
+          <ChapterIconArrow direction="right" size="xs" />
+        </ChapterIconArrowWrapper>
+        <ChapterTitle>
+          <span>{title}</span>
+
+          <ReplayTimestamp>
+            <TimestampButton
+              startTimestampMs={replay?.getStartTimestampMs() ?? 0}
+              timestampMs={start}
+              onClick={event => {
+                onClickChapterTimestamp(event, start);
+              }}
+            />
+          </ReplayTimestamp>
+        </ChapterTitle>
+      </Chapter>
+      <div>
+        {!breadcrumbs.length && (
+          <EmptyMessage>{t('No breadcrumbs for this chapter')}</EmptyMessage>
+        )}
+        {breadcrumbs.map((breadcrumb, j) => (
+          <ChapterBreadcrumbRow
+            frame={breadcrumb}
+            index={j}
+            onClick={onClickTimestamp}
+            onInspectorExpanded={() => {}}
+            onShowSnippet={() => {}}
+            showSnippet={false}
+            allowShowSnippet={false}
+            startTimestampMs={replay?.getStartTimestampMs() ?? 0}
+            key={`breadcrumb-${j}`}
+            style={{}}
+          />
+        ))}
+      </div>
+    </ChapterWrapper>
+  );
+}
+
+const ChapterIconArrowWrapper = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: ${space(1)} ${space(0.5)};
+  background-color: ${p => p.theme.background};
+  z-index: 2; /* needs to be above "ChapterWrapper summary::after" */
+`;
+
+const ChapterIconArrow = styled(IconChevron)`
+  details[open] & {
+    transform: rotate(180deg);
+  }
+`;
+
+const ChaptersList = styled('div')`
+  flex: 1;
+  overflow: auto;
+`;
+
+const ChapterWrapper = styled('details')`
+  width: 100%;
+  position: relative;
+  margin: 0;
+
+  /* vertical line connecting items */
+  summary::after {
+    content: '';
+    position: absolute;
+    left: 16.5px;
+    width: 1px;
+    top: 1px;
+    bottom: -9px;
+    background: ${p => p.theme.innerBorder};
+  }
+
+  &:first-child summary::after {
+    top: 12px;
+  }
+
+  &:last-child summary::after {
+    bottom: 0;
+  }
+
+  &.activeChapter .beforeCurrentTime:last-child {
+    border-bottom-color: ${p => p.theme.purple300};
+  }
+`;
+
+const ChapterBreadcrumbRow = styled(BreadcrumbRow)`
+  padding: ${space(0.5)} ${space(0.75)};
+
+  &::before {
+    display: none;
+  }
+  &:last-child {
+    background-color: transparent;
+  }
+  details[open]:last-child &:last-child {
+    background-color: ${p => p.theme.background};
+  }
+`;
+
+const Chapter = styled('summary')`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  font-size: ${p => p.theme.fontSize.lg};
+  padding: 0 ${space(0.75)};
+
+  /* sorry */
+  &:focus-visible {
+    outline: none;
+  }
+
+  list-style-type: none;
+  &::-webkit-details-marker {
+    display: none;
+  }
+`;
+
+const ChapterTitle = styled('div')`
+  display: flex;
+  flex: 1;
+  align-items: center;
+  gap: ${space(1)};
+  justify-content: space-between;
+  font-size: ${p => p.theme.fontSize.md};
+  padding: ${space(1)} 0;
+
+  .activeChapter & {
+    font-weight: ${p => p.theme.fontWeight.bold};
+  }
+
+  :not(details[open] &) {
+    border-bottom: 1px solid ${p => p.theme.innerBorder};
+  }
+  details:last-child:not([open]) & {
+    border-bottom: none;
+  }
+`;
+
+// Copied from breadcrumbItem
+const ReplayTimestamp = styled('div')`
+  color: ${p => p.theme.textColor};
+  font-size: ${p => p.theme.fontSize.sm};
+  font-weight: ${p => p.theme.fontWeight.normal};
+`;
+
+const EmptyContainer = styled('div')`
+  padding: ${space(2)};
+`;
