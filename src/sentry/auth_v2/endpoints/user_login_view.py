f13354ca047c0e6ab658b500d26744eed3cb4549
Danny Lee <danny@dongwei.li>
2025-07-01 00:27:16 -0700

    fix(pr): Update UserLoginView

--- src/sentry/auth_v2/endpoints/user_login_view.py
+++ src/sentry/auth_v2/endpoints/user_login_view.py
@@ -1,19 +1,19 @@
-from rest_framework.permissions import AllowAny
 from rest_framework.request import Request
 from rest_framework.response import Response
 
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
-from sentry.api.base import Endpoint, control_silo_endpoint
+from sentry.api.base import control_silo_endpoint
+
+from .base import AuthV2Endpoint
 
 
 @control_silo_endpoint
-class UserLoginView(Endpoint):
+class UserLoginView(AuthV2Endpoint):
     owner = ApiOwner.ENTERPRISE
     publish_status = {
         "GET": ApiPublishStatus.PRIVATE,
     }
-    permission_classes = (AllowAny,)
 
     def get(self, request: Request) -> Response:
         return Response({"message": "Hello world"})
