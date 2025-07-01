32fd98f211c07c5a5755692f88864720fc4186b1
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-07-01 10:47:26 -0700

    feat(replay): Add a banner on the Replay List page showing if there are in-progress bulk-delete tasks

---
+++ static/app/views/replays/list/bulkDeleteAlert.tsx
@@ -0,0 +1,58 @@
+import {Fragment} from 'react';
+
+import {hasEveryAccess} from 'sentry/components/acl/access';
+import {Alert} from 'sentry/components/core/alert';
+import Link from 'sentry/components/links/link';
+import useReplayBulkDeleteAuditLog from 'sentry/components/replays/bulkDelete/useFetchBulkDeleteLogs';
+import useOrganization from 'sentry/utils/useOrganization';
+import useProjectFromId from 'sentry/utils/useProjectFromId';
+
+interface Props {
+  projectId: string;
+}
+
+export default function BulkDeleteAlert({projectId}: Props) {
+  const organization = useOrganization();
+  const project = useProjectFromId({project_id: projectId});
+  const hasWriteAccess = hasEveryAccess(['project:write'], {organization, project});
+
+  const {data} = useReplayBulkDeleteAuditLog({
+    projectSlug: project?.slug ?? '',
+    query: {per_page: 10, offset: 0, referrer: 'replay-settings'},
+    enabled: project && hasWriteAccess,
+  });
+
+  if (!hasWriteAccess || !project) {
+    return null;
+  }
+
+  // TODO: This only looks at the first page of results.
+  // It would be better to be able to search for jobs in progress to be sure.
+  // The expectation is that only one job is running at a time.
+  const hasAnyInProgress = data?.data?.some(job =>
+    ['pending', 'in-progress'].includes(job?.status ?? '')
+  );
+
+  if (hasAnyInProgress) {
+    return (
+      <Alert
+        type="info"
+        showIcon
+        expand={
+          <Fragment>
+            Visit the{' '}
+            <Link
+              to={`/organizations/${organization.slug}/settings/projects/${project.slug}/replays/?replaySettingsTab=bulk-delete`}
+            >
+              Job Log
+            </Link>{' '}
+            to see more details and track progress.
+          </Fragment>
+        }
+      >
+        Replays are being deleted in the background. Items shown below may be stale.
+      </Alert>
+    );
+  }
+  return null;
+}
