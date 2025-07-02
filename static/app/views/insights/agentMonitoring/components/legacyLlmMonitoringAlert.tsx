fe5796840fc604485ea9a3938671afbe83773612
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-07-02 12:33:11 +0200

    use trailingitems of alert

--- static/app/views/insights/agentMonitoring/components/legacyLlmMonitoringAlert.tsx
+++ static/app/views/insights/agentMonitoring/components/legacyLlmMonitoringAlert.tsx
@@ -24,22 +24,26 @@ export function LegacyLLMMonitoringInfoAlert() {
   }
 
   return (
-    <StyledAlert type="info">
-      <ContentWrapper>
-        <Message>
-          {t('Looking for the old LLM Monitoring Experience?')}
-          <Button size="xs" priority="primary" onClick={handleSwitchUI}>
-            {t('Switch UI')}
-          </Button>
-        </Message>
-        <DismissButton
+    <StyledAlert
+      type="info"
+      showIcon
+      trailingItems={
+        <Button
           priority="link"
           size="sm"
           icon={<IconClose />}
           onClick={dismiss}
           aria-label={t('Close Alert')}
+          borderless
         />
-      </ContentWrapper>
+      }
+    >
+      <Message>
+        {t('Looking for the old LLM Monitoring Experience?')}
+        <Button size="xs" priority="primary" onClick={handleSwitchUI}>
+          {t('Switch UI')}
+        </Button>
+      </Message>
     </StyledAlert>
   );
 }
@@ -48,25 +52,9 @@ const StyledAlert = styled(Alert)`
   margin-bottom: ${space(2)};
 `;
 
-const ContentWrapper = styled('div')`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-`;
-
 const Message = styled('span')`
   display: flex;
   flex-grow: 1;
   gap: ${space(1)};
   align-items: center;
-`;
-
-const DismissButton = styled(Button)`
-  pointer-events: all;
-  margin-left: ${space(2)};
-  flex-shrink: 0;
-  &:hover {
-    opacity: 0.5;
-  }
 `;
