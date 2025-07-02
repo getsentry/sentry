f9b671f73effbd8d3c6067bb8418077ff43497b7
Michelle Fu <michelle.fu@sentry.io>
2025-07-02 14:51:37 -0700

    moving stuff around + one e2e test

--- tests/sentry/incidents/test_metric_issue_post_process.py
+++ tests/sentry/incidents/test_metric_issue_post_process.py
@@ -1,5 +1,5 @@
 from datetime import UTC, datetime, timedelta
-from unittest.mock import patch
+from unittest.mock import MagicMock, patch
 
 import pytest
 
@@ -15,7 +15,7 @@ from sentry.snuba.models import SnubaQuery, SnubaQueryEventType
 from sentry.snuba.subscriptions import create_snuba_query, create_snuba_subscription
 from sentry.tasks.post_process import post_process_group
 from sentry.testutils.helpers.features import Feature
-from sentry.workflow_engine.models import DataPacket
+from sentry.workflow_engine.models import DataPacket, Detector
 from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.types import (
     DetectorEvaluationResult,
@@ -25,7 +25,85 @@ from sentry.workflow_engine.types import (
 from tests.sentry.workflow_engine.test_base import BaseWorkflowTest
 
 
-class TestMetricDetectorPostProcess(BaseWorkflowTest):
+@patch("sentry.workflow_engine.processors.workflow.Action.trigger")
+class MetricIssueIntegrationTest(BaseWorkflowTest):
+    def create_metric_issue_workflow(self, detector: Detector):
+        # create the canonical workflow for a metric issue
+        workflow = self.create_workflow()
+        self.create_detector_workflow(detector=detector, workflow=workflow)
+
+        critical_dcg = self.create_data_condition_group(organization=self.organization)
+        self.create_workflow_data_condition_group(condition_group=critical_dcg, workflow=workflow)
+        self.create_data_condition(
+            comparison=DetectorPriorityLevel.HIGH,
+            condition_result=True,
+            type=Condition.ISSUE_PRIORITY_GREATER_OR_EQUAL,
+            condition_group=critical_dcg,
+        )
+
+        self.create_data_condition(
+            comparison=DetectorPriorityLevel.HIGH,
+            condition_result=True,
+            type=Condition.ISSUE_PRIORITY_DEESCALATING,
+            condition_group=critical_dcg,
+        )
+        critical_action = self.create_action()
+        self.create_data_condition_group_action(critical_action, critical_dcg)
+
+        warning_dcg = self.create_data_condition_group(organization=self.organization)
+        self.create_workflow_data_condition_group(condition_group=warning_dcg, workflow=workflow)
+        self.create_data_condition(
+            comparison=DetectorPriorityLevel.MEDIUM,
+            condition_result=True,
+            type=Condition.ISSUE_PRIORITY_GREATER_OR_EQUAL,
+            condition_group=warning_dcg,
+        )
+
+        self.create_data_condition(
+            comparison=DetectorPriorityLevel.MEDIUM,
+            condition_result=True,
+            type=Condition.ISSUE_PRIORITY_DEESCALATING,
+            condition_group=warning_dcg,
+        )
+
+        warning_action = self.create_action()
+        self.create_data_condition_group_action(warning_action, warning_dcg)
+
+        return (
+            critical_action,
+            warning_action,
+        )
+
+    def create_data_packet(self, value: int) -> DataPacket:
+        packet = QuerySubscriptionUpdate(
+            entity="entity",
+            subscription_id=str(self.query_subscription.id),
+            values={"value": value},
+            timestamp=datetime.now(UTC),
+        )
+        return DataPacket[QuerySubscriptionUpdate](
+            source_id=str(self.query_subscription.id), packet=packet
+        )
+
+    def call_post_process_group(self, occurrence):
+        stored_occurrence = IssueOccurrence.fetch(occurrence.id, occurrence.project_id)
+        assert stored_occurrence
+        event = eventstore.backend.get_event_by_id(
+            occurrence.project_id, stored_occurrence.event_id
+        )
+        assert event
+
+        post_process_group(
+            is_new=True,
+            is_regression=False,
+            is_new_group_environment=True,
+            cache_key=None,
+            occurrence_id=occurrence.id,
+            group_id=event.group_id,
+            project_id=occurrence.project_id,
+            eventstream_type=EventStreamEventType.Generic.value,
+        )
+
     def setUp(self):
         super().setUp()
         self.detector_group_key = None
@@ -47,6 +125,13 @@ class TestMetricDetectorPostProcess(BaseWorkflowTest):
             condition_result=DetectorPriorityLevel.MEDIUM,
             condition_group=self.detector.workflow_condition_group,
         )
+        self.resolve_detector_trigger = self.create_data_condition(
+            type=Condition.LESS,
+            comparison=3,
+            condition_result=DetectorPriorityLevel.OK,
+            condition_group=self.detector.workflow_condition_group,
+        )
+
         with self.tasks():
             self.snuba_query = create_snuba_query(
                 query_type=SnubaQuery.Type.ERROR,
@@ -67,6 +152,7 @@ class TestMetricDetectorPostProcess(BaseWorkflowTest):
         self.create_alert_rule_detector(alert_rule_id=self.alert_rule.id, detector=self.detector)
 
         self.handler = MetricIssueDetectorHandler(self.detector)
+        self.critical_action, self.warning_action = self.create_metric_issue_workflow(self.detector)
 
     @pytest.fixture(autouse=True)
     def with_feature_flags(self):
@@ -76,22 +162,16 @@ class TestMetricDetectorPostProcess(BaseWorkflowTest):
                 "organizations:issue-metric-issue-post-process-group": True,
                 "organizations:workflow-engine-metric-alert-processing": True,
                 "organizations:workflow-engine-process-metric-issue-workflows": True,
+                "organizations:workflow-engine-trigger-actions": True,
+                "organizations:issue-open-periods": True,
             }
         ):
             yield
 
-    @patch("sentry.workflow_engine.processors.workflow.process_workflows")
-    def test_occurrence_post_process(self, mock_process_workflows):
+    def test_occurrence_post_process(self, mock_trigger):
         value = self.critical_detector_trigger.comparison + 1
-        packet = QuerySubscriptionUpdate(
-            entity="entity",
-            subscription_id=str(self.query_subscription.id),
-            values={"value": value},
-            timestamp=datetime.now(UTC),
-        )
-        data_packet = DataPacket[QuerySubscriptionUpdate](
-            source_id=str(self.query_subscription.id), packet=packet
-        )
+        self.critical_action.trigger = MagicMock(name="trigger")
+        data_packet = self.create_data_packet(value)
         result: dict[DetectorGroupKey, DetectorEvaluationResult] = self.handler.evaluate(
             data_packet
         )
@@ -107,21 +187,6 @@ class TestMetricDetectorPostProcess(BaseWorkflowTest):
             event_data=evaluation_result.event_data,
         )
         occurrence.save()
-        stored_occurrence = IssueOccurrence.fetch(occurrence.id, occurrence.project_id)
-        assert stored_occurrence
-        event = eventstore.backend.get_event_by_id(
-            occurrence.project_id, stored_occurrence.event_id
-        )
-        assert event
+        self.call_post_process_group(occurrence)
 
-        post_process_group(
-            is_new=True,
-            is_regression=False,
-            is_new_group_environment=True,
-            cache_key=None,
-            occurrence_id=occurrence.id,
-            group_id=event.group_id,
-            project_id=occurrence.project_id,
-            eventstream_type=EventStreamEventType.Generic.value,
-        )
-        assert mock_process_workflows.call_count == 1
+        assert mock_trigger.call_count == 2  # warning + critical actions
