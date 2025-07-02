fda6064af975a1793d9256575ae952b151f2b198
Arthur Knaus <arthur.knaus@sentry.io>
2025-07-02 14:07:47 +0200

    feat(agent-monitoring): Allow filtering by span attributes

--- static/app/views/insights/common/components/widgets/overviewPageloadsChartWidget.tsx
+++ static/app/views/insights/common/components/widgets/overviewPageloadsChartWidget.tsx
@@ -2,14 +2,17 @@ import {t} from 'sentry/locale';
 import type {LoadableChartWidgetProps} from 'sentry/views/insights/common/components/widgets/types';
 import {Referrer} from 'sentry/views/insights/pages/platform/laravel/referrers';
 import {BaseTrafficWidget} from 'sentry/views/insights/pages/platform/shared/baseTrafficWidget';
+import {useTransactionNameQuery} from 'sentry/views/insights/pages/platform/shared/useTransactionNameQuery';
 
 export default function OverviewPageloadsChartWidget(props: LoadableChartWidgetProps) {
+  const {query} = useTransactionNameQuery();
+  const fullQuery = `'span.op:[pageload]' ${query}`.trim();
   return (
     <BaseTrafficWidget
       id="overviewPageloadsChartWidget"
       title={t('Pageloads')}
       trafficSeriesName={t('Pageloads')}
-      baseQuery={'span.op:[pageload]'}
+      query={fullQuery}
       referrer={Referrer.REQUESTS_CHART}
       {...props}
     />
