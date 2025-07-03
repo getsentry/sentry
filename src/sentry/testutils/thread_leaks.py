57a25270b458a10842966ee41ce87f929bae7dda
Buck Evan <buck.evan@sentry.io>
2025-07-03 11:45:35 -0500

    improve traceback more

--- src/sentry/testutils/thread_leaks.py
+++ src/sentry/testutils/thread_leaks.py
@@ -6,11 +6,30 @@ does a bit of work to record and show exactly where the Thread came from, which
 proved essential when working to fix these things.
 """
 
+import sys
 import threading
 import traceback
 from contextlib import contextmanager
 from typing import Any
 from unittest import mock
+
+
+def _get_irrelevant_frame_paths() -> tuple[str, ...]:
+    result = (
+        path + "/"
+        for path in (sys.prefix, sys.exec_prefix, getattr(sys, "_stdlib_dir", None))
+        if isinstance(path, str)
+    )
+
+    return tuple(sorted(set(result)))
+
+
+# a set of "not our code" directories, suitable for str.startswith()
+_IRRELEVANT_FRAME_PATHS = _get_irrelevant_frame_paths()
+
+
+def _relevant_frames(stack: traceback.StackSummary):
+    return [frame for frame in stack[:-1] if not frame.filename.startswith(_IRRELEVANT_FRAME_PATHS)]
 
 
 def _threads_to_diffable_str(threads: list[threading.Thread]) -> str:
@@ -39,8 +58,8 @@ def threading_remembers_where():
     orig = threading.Thread.__init__
 
     def new(self: threading.Thread, *a: Any, **k: Any) -> None:
-        stack = traceback.extract_stack(limit=6)[:-1]
-        setattr(self, "_where", "".join(traceback.format_list(stack)))
+        frames = _relevant_frames(traceback.extract_stack())
+        setattr(self, "_where", "".join(traceback.format_list(frames)))
         orig(self, *a, **k)
 
     with mock.patch.object(threading.Thread, "__init__", new):
