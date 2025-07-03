71253deef6cb66a767833b8da61138e612e33122
Abdullah Khan <60121741+Abdkhan14@users.noreply.github.com>
2025-07-03 12:10:33 -0400

    feat(explore-suspect-attrs): Querying suspect attrs (#94842)
    - This just lists the attribute names for now, will help us test the
    performance of the endpoint, propagate some traces around it.
    - Will iterate on it in following PRs, it's under the flag:
    `performance-spans-suspect-attributes`
    
    
    https://github.com/user-attachments/assets/068fb16f-62a5-4b8f-8139-d7a405c9a44e
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/explore/components/suspectTags/drawer.tsx
+++ static/app/views/explore/components/suspectTags/drawer.tsx
@@ -1,10 +1,22 @@
 import styled from '@emotion/styled';
 
 import {DrawerBody, DrawerHeader} from 'sentry/components/globalDrawer/components';
+import LoadingError from 'sentry/components/loadingError';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
+import type {ChartInfo} from 'sentry/views/explore/charts';
+import type {BoxSelectOptions} from 'sentry/views/explore/hooks/useChartBoxSelect';
+import useSuspectAttributes from 'sentry/views/explore/hooks/useSuspectAttributes';
 
-export function Drawer() {
+type Props = {
+  boxSelectOptions: BoxSelectOptions;
+  chartInfo: ChartInfo;
+};
+
+export function Drawer({boxSelectOptions, chartInfo}: Props) {
+  const {data, isLoading, isError} = useSuspectAttributes({boxSelectOptions, chartInfo});
+
   return (
     <DrawerContainer>
       <DrawerHeader hideBar />
@@ -15,7 +27,20 @@ export function Drawer() {
             'Comparing selected and unselected (baseline) data, we sorted  attributes that differ the most in frequency. This indicates how suspicious they are. '
           )}
         </SubTitle>
-        <span>TODO: Add suspect attributes</span>
+        {isLoading ? (
+          <LoadingIndicator />
+        ) : isError ? (
+          <LoadingError message={t('Failed to load suspect attributes')} />
+        ) : (
+          <AttributeNames>
+            {/* TODO Abdullah Khan: Add suspect attributes distribution charts, just listing the names for now */}
+            {data?.rankedAttributes.map(a => (
+              <div key={a.attributeName}>
+                {a.attributeName} <br />
+              </div>
+            ))}
+          </AttributeNames>
+        )}
       </StyledDrawerBody>
     </DrawerContainer>
   );
@@ -44,4 +69,11 @@ const DrawerContainer = styled('div')`
   > header {
     flex-shrink: 0;
   }
+`;
+
+const AttributeNames = styled('div')`
+  flex: 1;
+  overflow: hidden;
+  overflow-y: scroll;
+  overscroll-behavior: none;
 `;
