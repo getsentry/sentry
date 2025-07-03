d6e8da00d056dd806d2dc0e55644dfd28a5b1870
Simon Hellmayr <shellmayr@users.noreply.github.com>
2025-07-03 14:21:30 +0200

    feat(laravel-insights): create real jobs table instead of queues table (#94849)

--- static/app/views/insights/pages/platform/laravel/index.tsx
+++ static/app/views/insights/pages/platform/laravel/index.tsx
@@ -105,7 +105,7 @@ export function LaravelOverviewPage() {
         <TableControl value={currentView} onChange={handleViewChange} size="sm">
           <TableControlItem key={TableType.PATHS}>{t('Paths')}</TableControlItem>
           <TableControlItem key={TableType.COMMANDS}>{t('Commands')}</TableControlItem>
-          <TableControlItem key={TableType.QUEUES}>{t('Queues')}</TableControlItem>
+          <TableControlItem key={TableType.QUEUES}>{t('Jobs')}</TableControlItem>
         </TableControl>
       </ControlsWrapper>
       {currentView === TableType.QUEUES && <JobsTable />}
