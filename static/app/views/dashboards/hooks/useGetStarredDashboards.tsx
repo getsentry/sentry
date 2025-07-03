7a28371340d66716bc689bd63430e9fd46297ed0
Nar Saynorath <nar.saynorath@sentry.io>
2025-07-03 13:42:00 -0400

    feat(dashboards): Use starred endpoint for ordered dashboards (#94810)
    This endpoint returns starred dashboards in order of position. It uses
    the same serializer so the handling of the response does not need to
    change.
    
    In the backend, behind the scenes, it also backfills in position data
    from the original sorting if there are no positions set yet (i.e. it'll
    order by title)

--- static/app/views/dashboards/hooks/useGetStarredDashboards.tsx
+++ static/app/views/dashboards/hooks/useGetStarredDashboards.tsx
@@ -1,20 +1,30 @@
+import type {Organization} from 'sentry/types/organization';
+import type {ApiQueryKey} from 'sentry/utils/queryClient';
 import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 import type {DashboardListItem} from 'sentry/views/dashboards/types';
 
+function getQueryKey(organization: Organization): ApiQueryKey {
+  const DASHBOARDS_QUERY_KEY = [
+    `/organizations/${organization.slug}/dashboards/`,
+    {
+      query: {
+        filter: 'onlyFavorites',
+      },
+    },
+  ] as const;
+  const STARRED_DASHBOARDS_QUERY_KEY = [
+    `/organizations/${organization.slug}/dashboards/starred/`,
+    {},
+  ] as const;
+  return organization.features.includes('dashboards-starred-reordering')
+    ? STARRED_DASHBOARDS_QUERY_KEY
+    : DASHBOARDS_QUERY_KEY;
+}
+
 export function useGetStarredDashboards() {
   const organization = useOrganization();
-  return useApiQuery<DashboardListItem[]>(
-    [
-      `/organizations/${organization.slug}/dashboards/`,
-      {
-        query: {
-          filter: 'onlyFavorites',
-        },
-      },
-    ],
-    {
-      staleTime: Infinity,
-    }
-  );
+  return useApiQuery<DashboardListItem[]>(getQueryKey(organization), {
+    staleTime: Infinity,
+  });
 }
