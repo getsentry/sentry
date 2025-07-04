80abf79f8bc268ecabb20cd8db1656c5bd1c5c63
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-07-04 13:13:54 -0400

    🚧 Undo changes to chartcuterie dir

---
+++ tests/acceptance/chartcuterie/__init__.py
@@ -0,0 +1,9 @@
+"""
+Acceptance Tests
+
+An acceptance test is a full end-to-end (frontend and backend) test of a system
+designed as real use case scenarios. At Sentry, we test these actions with
+Selenium, an automated browser simulator. The advantage of using acceptance
+tests rather than unit tests is that they can ensure an entire feature's flow is
+functional. The disadvantage is that they can be slow and in some cases flaky.
+"""
