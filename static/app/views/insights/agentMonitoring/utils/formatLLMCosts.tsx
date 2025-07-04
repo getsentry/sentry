3ef173bd0508762a30f4aedec68afbe8b43b70a9
ArthurKnaus <arthur.knaus@sentry.io>
2025-07-04 14:33:05 +0200

    fix(agent-monitoring): Hotfix wrong LLM costs (#94909)

--- static/app/views/insights/agentMonitoring/utils/formatLLMCosts.tsx
+++ static/app/views/insights/agentMonitoring/utils/formatLLMCosts.tsx
@@ -1,8 +1,13 @@
 import {formatAbbreviatedNumberWithDynamicPrecision} from 'sentry/utils/formatters';
 
 export function formatLLMCosts(cost: string | number) {
-  if (Number(cost) < 0.01) {
+  let number = Number(cost);
+  // TODO: remove this hotfix for bug on BE that results in costs sometimes being multiplied by 1000000
+  if (number > 100) {
+    number = number / 1_000_000;
+  }
+  if (number < 0.01) {
     return `<$${(0.01).toLocaleString()}`;
   }
-  return `$${formatAbbreviatedNumberWithDynamicPrecision(cost)}`;
+  return `$${formatAbbreviatedNumberWithDynamicPrecision(number)}`;
 }
