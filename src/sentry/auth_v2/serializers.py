ca8c10c22db824bae9b575db4b55f190f9bebccd
Danny Lee <danny@dongwei.li>
2025-07-07 14:17:11 -0700

    fix(pr): Address comments

--- src/sentry/auth_v2/serializers.py
+++ src/sentry/auth_v2/serializers.py
@@ -1,11 +1,25 @@
 # import serializer from rest_framework
 
+from datetime import datetime
+from typing import TypedDict
+
 from rest_framework.request import Request
 
 from sentry.api.serializers import Serializer
 
-# import model from models.py
 from .types import SessionData
+
+
+class SessionSerializerResponse(TypedDict, total=False):
+    # Flags to control the authentication flow on frontend.
+    # NOTE(dlee): Keep the keys sorted in order of importance!! Maintaining the control flow hierarchy on the serializer is good context for future engineers.
+    todoEmailVerification: bool
+    todo2faVerification: bool
+
+    userId: str
+    sessionCsrfToken: str
+    sessionExpiryDate: datetime
+    sessionOrgs: list[str]
 
 
 class SessionSerializer(Serializer):
@@ -14,7 +28,7 @@ class SessionSerializer(Serializer):
     https://docs.djangoproject.com/en/5.2/topics/http/sessions/#django.contrib.sessions.backends.base.SessionBase
     """
 
-    def serialize(self, request: Request) -> dict:
+    def serialize(self, request: Request) -> SessionSerializerResponse:
         """
         This serializer takes in the entire request object because it needs to access
         the CSRF token
@@ -22,12 +36,12 @@ class SessionSerializer(Serializer):
         session: SessionData = request.session
         return {
             # Flags to control the authentication flow on frontend.
-            # NOTE(dlee): Keep the keys sorted in order of importance!!
-            "todo_email_verification": session.get("todo_email_verification"),
-            "todo_2fa_verification": session.get("todo_2fa_verification"),
-            # Post-authentication data
-            "user_id": session.get("_auth_user_id"),
-            "session_csrf_token": request.META.get("CSRF_COOKIE"),
-            "session_expiry_date": session.get_expiry_date(),
-            "session_orgs": session.get("session_orgs"),
+            # NOTE(dlee): Keep the keys sorted in order of importance!! Maintaining the control flow hierarchy on the serializer is good context for future engineers.
+            "todoEmailVerification": session.get("todo_email_verification"),
+            "todo2faVerification": session.get("todo_2fa_verification"),
+            # Post-authenticated session data
+            "userId": session.get("_auth_user_id"),
+            "sessionCsrfToken": request.META.get("CSRF_COOKIE"),
+            "sessionExpiryDate": session.get_expiry_date(),
+            "sessionOrgs": session.get("session_orgs"),
         }
