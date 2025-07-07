b06eebacd286ea74a8aae0b6546fa98c0ebe7607
Jasmin <77064737+jas-kas@users.noreply.github.com>
2025-07-07 18:26:56 -0400

    Update tests/sentry/feedback/usecases/test_create_feedback.py
    Co-authored-by: Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>

--- tests/sentry/feedback/usecases/test_create_feedback.py
+++ tests/sentry/feedback/usecases/test_create_feedback.py
@@ -1205,7 +1205,6 @@ def test_create_feedback_issue_uses_generated_title(
 
     assert mock_produce_occurrence_to_kafka.call_count == 1
 
-    # Get the occurrence from the call
     call_args = mock_produce_occurrence_to_kafka.call_args
     occurrence = call_args[1]["occurrence"]
 
