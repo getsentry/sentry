f6a2b17a4478a45d6a8fa13979c67cb7987752cd
Edward Gou <edward.gou@sentry.io>
2025-07-07 13:48:23 -0400

    fix query filter for lcp vitals

--- static/app/views/insights/browser/webVitals/settings.ts
+++ static/app/views/insights/browser/webVitals/settings.ts
@@ -10,10 +10,10 @@ export const MODULE_DOC_LINK =
   'https://docs.sentry.io/product/insights/frontend/web-vitals/';
 
 export const DEFAULT_QUERY_FILTER =
-  'transaction.op:[pageload,""] span.op:[ui.interaction.click,ui.interaction.hover,ui.interaction.drag,ui.interaction.press,ui.webvital.cls,""] !transaction:"<< unparameterized >>"';
+  'transaction.op:[pageload,""] span.op:[ui.interaction.click,ui.interaction.hover,ui.interaction.drag,ui.interaction.press,ui.webvital.cls,ui.webvital.lcp,""] !transaction:"<< unparameterized >>"';
 
 export const DEFAULT_EAP_QUERY_FILTER =
-  'span.op:[ui.interaction.click,ui.interaction.hover,ui.interaction.drag,ui.interaction.press,ui.webvital.cls,pageload,""] !transaction:"<< unparameterized >>"';
+  'span.op:[ui.interaction.click,ui.interaction.hover,ui.interaction.drag,ui.interaction.press,ui.webvital.cls,ui.webvital.lcp,pageload,""] !transaction:"<< unparameterized >>"';
 
 export const MODULE_FEATURES = ['insights-initial-modules'];
 
