75e09eb66308106af839703a0063648b93346ec6
Colton Allen <colton.allen@sentry.io>
2025-07-07 11:37:02 -0500

    Drop dom_index module

--- src/sentry/replays/usecases/ingest/event_logger.py
+++ src/sentry/replays/usecases/ingest/event_logger.py
@@ -2,17 +2,12 @@ import logging
 import uuid
 from collections.abc import Generator
 from hashlib import md5
-from typing import Any, TypedDict
+from typing import Any, Literal, TypedDict
 
 import sentry_sdk
 
 from sentry.models.project import Project
 from sentry.replays.lib.kafka import publish_replay_event
-from sentry.replays.usecases.ingest.dom_index import (
-    ReplayActionsEvent,
-    ReplayActionsEventPayload,
-    ReplayActionsEventPayloadClick,
-)
 from sentry.replays.usecases.ingest.event_parser import ClickEvent, ParsedEventMeta
 from sentry.replays.usecases.ingest.issue_creation import (
     report_hydration_error_issue_with_replay_event,
@@ -21,6 +16,44 @@ from sentry.replays.usecases.ingest.issue_creation import (
 from sentry.utils import json, metrics
 
 logger = logging.getLogger()
+
+
+ReplayActionsEventPayloadClick = TypedDict(
+    "ReplayActionsEventPayloadClick",
+    {
+        "alt": str,
+        "aria_label": str,
+        "class": list[str],
+        "event_hash": str,
+        "id": str,
+        "node_id": int,
+        "component_name": str,
+        "role": str,
+        "tag": str,
+        "testid": str,
+        "text": str,
+        "timestamp": int,
+        "title": str,
+        "is_dead": int,
+        "is_rage": int,
+    },
+)
+
+
+class ReplayActionsEventPayload(TypedDict):
+    environment: str
+    clicks: list[ReplayActionsEventPayloadClick]
+    replay_id: str
+    type: Literal["replay_actions"]
+
+
+class ReplayActionsEvent(TypedDict):
+    payload: list[int]
+    project_id: int
+    replay_id: str
+    retention_days: int
+    start_time: float
+    type: Literal["replay_event"]
 
 
 @sentry_sdk.trace
