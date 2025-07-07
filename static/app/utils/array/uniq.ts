2e945e66757e2c49cc6299e13c8bcdc1af96e547
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2025-07-07 12:16:36 -0400

    fix(uniq): Handle non-array cases (#94354)
    Fixes #93417.

--- static/app/utils/array/uniq.ts
+++ static/app/utils/array/uniq.ts
@@ -1,6 +1,9 @@
 /**
  * Returns unique values of the given array.
  */
-export function uniq<T = unknown>(items: T[] | undefined): T[] {
+export function uniq<T = unknown>(items: T[] | undefined | null): T[] {
+  if (!Array.isArray(items)) {
+    return [];
+  }
   return [...new Set(items)];
 }
