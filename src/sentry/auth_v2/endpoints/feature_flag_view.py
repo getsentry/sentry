140720c36b34184d5aa5fdbe396186de4e66f65e
Danny Lee <danny@dongwei.li>
2025-07-08 14:54:49 -0700

    feat(auth-v2): Add endpoint to validate HTTP headers are set

---
+++ src/sentry/auth_v2/endpoints/feature_flag_view.py
@@ -0,0 +1,20 @@
+from rest_framework.request import Request
+from rest_framework.response import Response
+
+from sentry.api.base import ApiOwner, ApiPublishStatus, AuthV2Endpoint, RateLimit, RateLimitCategory
+
+
+class FeatureFlagView(AuthV2Endpoint):
+    owner = ApiOwner.ENTERPRISE
+    publish_status = {"GET": ApiPublishStatus.EXPERIMENTAL}
+    enforce_rate_limit = True
+    rate_limits = {
+        "GET": {RateLimitCategory.IP: RateLimit(limit=30, window=60)}  # 30 per minute per IP
+    }
+
+    def get(self, request: Request) -> Response:
+        """
+        Check if the feature flag is set correctly on your machine.
+        curl -X GET "https://sentry.io/api/0/auth-v2/feature-flag/" -H "X-Sentry-Auth-V2: ***"
+        """
+        return Response({"message": "Hello world"})
