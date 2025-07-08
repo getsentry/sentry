d77a21d5ac47e29b1a77ce85fd7cb1d95bb0dfb1
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2025-07-08 08:49:49 -0700

    add span

--- src/sentry/performance_issues/performance_detection.py
+++ src/sentry/performance_issues/performance_detection.py
@@ -371,14 +371,15 @@ def _detect_performance_problems(
     with sentry_sdk.start_span(op="function", name="get_detection_settings"):
         detection_settings = get_detection_settings(project.id)
 
-    if standalone or features.has("organizations:issue-detection-sort-spans", organization):
-        # The performance detectors expect the span list to be ordered/flattened in the way they
-        # are structured in the tree. This is an implicit assumption in the performance detectors.
-        # So we build a tree and flatten it depth first.
-        # TODO: See if we can update the detectors to work without this assumption so we can
-        # just pass it a list of spans.
-        tree, segment_id = build_tree(data.get("spans", []))
-        data = {**data, "spans": flatten_tree(tree, segment_id)}
+    with sentry_sdk.start_span(op="function", name="sort_spans"):
+        if standalone or features.has("organizations:issue-detection-sort-spans", organization):
+            # The performance detectors expect the span list to be ordered/flattened in the way they
+            # are structured in the tree. This is an implicit assumption in the performance detectors.
+            # So we build a tree and flatten it depth first.
+            # TODO: See if we can update the detectors to work without this assumption so we can
+            # just pass it a list of spans.
+            tree, segment_id = build_tree(data.get("spans", []))
+            data = {**data, "spans": flatten_tree(tree, segment_id)}
 
     with sentry_sdk.start_span(op="initialize", name="PerformanceDetector"):
         detectors: list[PerformanceDetector] = [
