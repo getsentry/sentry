6fc574556debd9468569f88232dab3669b379ba0
Michelle Fu <michelle.fu@sentry.io>
2025-07-08 13:25:44 -0700

    move helper + fix

--- src/sentry/incidents/subscription_processor.py
+++ src/sentry/incidents/subscription_processor.py
@@ -51,10 +51,11 @@ from sentry.incidents.utils.types import (
 )
 from sentry.models.project import Project
 from sentry.seer.anomaly_detection.get_anomaly_data import get_anomaly_data_from_seer_legacy
-from sentry.seer.anomaly_detection.get_historical_anomalies import (
+from sentry.seer.anomaly_detection.utils import (
+    anomaly_has_confidence,
     get_anomaly_evaluation_from_workflow_engine,
+    has_anomaly,
 )
-from sentry.seer.anomaly_detection.utils import anomaly_has_confidence, has_anomaly
 from sentry.snuba.dataset import Dataset
 from sentry.snuba.models import QuerySubscription
 from sentry.snuba.subscriptions import delete_snuba_subscription
