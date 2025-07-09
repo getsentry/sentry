e0533e78113bd160758d4f63c244b4014d5c178f
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-07-09 10:15:52 -0400

    Add clarifying comments

--- static/app/views/dashboards/widgets/timeSeriesWidget/formatters/formatTimeSeriesName.tsx
+++ static/app/views/dashboards/widgets/timeSeriesWidget/formatters/formatTimeSeriesName.tsx
@@ -9,9 +9,9 @@ import WidgetLegendNameEncoderDecoder from 'sentry/views/dashboards/widgetLegend
 import type {TimeSeries} from 'sentry/views/dashboards/widgets/common/types';
 
 export function formatTimeSeriesName(timeSeries: TimeSeries): string {
-  // If the timeSeries has `groupBy` information, don't bother including the Y
-  // axis name, just format the groupBy by concatenating the values. This is the
-  // most common desired behavior.
+  // If the timeSeries has `groupBy` information, the label is made by
+  // concatenating the values of the groupBy, since there's no point repeating
+  // the name of the Y axis multiple times in the legend.
   if (timeSeries.groupBy?.length && timeSeries.groupBy.length > 0) {
     return `${timeSeries.groupBy
       ?.map(groupBy => {
@@ -29,13 +29,17 @@ export function formatTimeSeriesName(timeSeries: TimeSeries): string {
   // Decode from series name disambiguation
   seriesName = WidgetLegendNameEncoderDecoder.decodeSeriesNameForLegend(seriesName)!;
 
-  // Check if it's a release version. NOTE: This is not a good idea.
-  // `formatVersion` will aggressively assume that any `@` symbol in the string
-  // signifies a version, so user emails will be formatted as versions. All
-  // usage of `TimeSeriesWidgetVisualization` should be careful about using the
-  // `groupBy` property of a `TimeSeries`, because it'll _only parse the release
-  // tag_ as a version, and leave others alone. Once `groupBy` is in wide use,
-  // we can remove this parsing.
+  // Attempt to parse the `seriesName` as a version. A correct `TimeSeries`
+  // would have a `yAxis` like `p50(span.duration)` with a `groupBy` like
+  // `[{key: "release", value: "proj@1.2.3"}]`. `groupBy` was only introduced
+  // recently though, so many `TimeSeries` instead mash the group by information
+  // into the `yAxis` property, e.g., the `yAxis` might have been set to
+  // `"proj@1.2.3"` just to get the correct rendering in the chart legend. We
+  // cover these cases by parsing the `yAxis` as a series name. This works badly
+  // because sometimes it'll interpet email addresses as versions, which causes
+  // bugs. We should update all usages of `TimeSeriesWidgetVisualization` to
+  // correctly specify `yAxis` and `groupBy`, and/or to use the time
+  // `/events-timeseries` endpoint which does this automatically.
   seriesName = formatVersion(seriesName);
 
   // Check for special-case measurement formatting
--- static/app/views/dashboards/widgets/timeSeriesWidget/plottables/continuousTimeSeries.tsx
+++ static/app/views/dashboards/widgets/timeSeriesWidget/plottables/continuousTimeSeries.tsx
@@ -58,6 +58,9 @@ export abstract class ContinuousTimeSeries<
     this.config = config;
   }
 
+  /**
+   * Continuous time series names need to be unique to disambiguate them from other series. We use both the `yAxis` and the `groupBy` to create the name. This makes it possible to pass in two different time series with the same `yAxis` as long as they have different `groupBy` information.
+   */
   get name(): string {
     let name = `${this.timeSeries.yAxis}`;
 
