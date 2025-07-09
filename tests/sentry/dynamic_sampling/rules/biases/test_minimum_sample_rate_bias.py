24b1a6e696c9cfede8c679a576a302ba7002ed2a
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-07-09 15:24:42 +0200

    remove superfluous test

--- tests/sentry/dynamic_sampling/rules/biases/test_minimum_sample_rate_bias.py
+++ tests/sentry/dynamic_sampling/rules/biases/test_minimum_sample_rate_bias.py
@@ -4,26 +4,6 @@ from sentry.testutils.pytest.fixtures import django_db_all
 
 
 @django_db_all
-def test_generate_minimum_sample_rate_rule(default_project):
-    base_sample_rate = 0.1
-    rules = MinimumSampleRateBias().generate_rules(
-        project=default_project, base_sample_rate=base_sample_rate
-    )
-    expected_rules = [
-        {
-            "samplingValue": {"type": "minimumSampleRate", "value": base_sample_rate},
-            "type": "trace",
-            "condition": {
-                "inner": [],
-                "op": "and",
-            },
-            "id": 1006,
-        }
-    ]
-    assert rules == expected_rules
-
-
-@django_db_all
 def test_generate_rules_with_different_sample_rates(default_project):
     test_rates = [0.01, 0.05, 0.1, 0.5, 1.0]
 
