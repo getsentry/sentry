722511af307bbe928826f5fc127089462c0a92f9
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-07-09 09:35:40 -0400

    ref(insights): Improve `TimeSeriesWidgetVisualization` series name rendering (#95078)
    Preparation for an incoming refactor.
    
    1. `formatSeriesName` is now called `formatTimeSeriesName`, because it
    accepts a `TimeSeries`! This is an important change because soon I will
    take `TimeSeries.groupBy` into account when formatting the names
    2. Remove direct usage for `formatSeriesName` from Release Health
    charts. That's a very specific usage, it's _only_ doing version
    formatting, so it should use the underlying helper

--- static/app/views/dashboards/widgets/timeSeriesWidget/formatters/formatTimeSeriesName.tsx
+++ static/app/views/dashboards/widgets/timeSeriesWidget/formatters/formatTimeSeriesName.tsx
@@ -6,8 +6,10 @@ import {
 } from 'sentry/utils/discover/fields';
 import {formatVersion} from 'sentry/utils/versions/formatVersion';
 import WidgetLegendNameEncoderDecoder from 'sentry/views/dashboards/widgetLegendNameEncoderDecoder';
+import type {TimeSeries} from 'sentry/views/dashboards/widgets/common/types';
 
-export function formatSeriesName(seriesName: string): string {
+export function formatTimeSeriesName(timeSeries: TimeSeries): string {
+  let {yAxis: seriesName} = timeSeries;
   // Decode from series name disambiguation
   seriesName = WidgetLegendNameEncoderDecoder.decodeSeriesNameForLegend(seriesName)!;
 
