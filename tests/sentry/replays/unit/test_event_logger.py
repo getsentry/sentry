1f78127fdc06d2008303c66b2f75cc3764904766
Colton Allen <colton.allen@sentry.io>
2025-07-09 14:56:10 -0500

    Assert trace-items are emitted to EAP

--- tests/sentry/replays/unit/test_event_logger.py
+++ tests/sentry/replays/unit/test_event_logger.py
@@ -1,7 +1,16 @@
 import uuid
 from unittest import mock
 
-from sentry.replays.usecases.ingest.event_logger import emit_click_events, gen_rage_clicks
+from google.protobuf.timestamp_pb2 import Timestamp
+from sentry_protos.snuba.v1.request_common_pb2 import TraceItemType
+from sentry_protos.snuba.v1.trace_item_pb2 import TraceItem
+
+from sentry.conf.types.kafka_definition import Topic
+from sentry.replays.usecases.ingest.event_logger import (
+    emit_click_events,
+    emit_trace_items_to_eap,
+    gen_rage_clicks,
+)
 from sentry.replays.usecases.ingest.event_parser import ClickEvent, ParsedEventMeta
 
 
@@ -66,3 +75,33 @@ def test_emit_click_events_environment_handling():
         assert producer.call_args is not None
         assert producer.call_args.args[0].name == "ingest-replay-events"
         assert producer.call_args.args[1].value is not None
+
+
+def test_emit_trace_items_to_eap():
+    timestamp = Timestamp()
+    timestamp.FromMilliseconds(1000)
+
+    trace_items = [
+        TraceItem(
+            organization_id=1,
+            project_id=2,
+            trace_id=uuid.uuid4().hex,
+            item_id=uuid.uuid4().bytes,
+            item_type=TraceItemType.TRACE_ITEM_TYPE_REPLAY,
+            timestamp=timestamp,
+            attributes={},
+            client_sample_rate=1.0,
+            server_sample_rate=1.0,
+            retention_days=90,
+            received=timestamp,
+        )
+    ]
+
+    with mock.patch("arroyo.backends.kafka.consumer.KafkaProducer.produce") as producer:
+        emit_trace_items_to_eap(trace_items)
+
+        assert producer.called
+        assert producer.call_args[0][0].name == Topic.SNUBA_ITEMS.value
+        assert producer.call_args[0][1].key is None
+        assert producer.call_args[0][1].headers == []
+        assert isinstance(producer.call_args[0][1].value, bytes)
