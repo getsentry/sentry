45d4d273a20e73bd16f3cc8b86515cb1473cef2e
Danny Lee <danny@dongwei.li>
2025-07-09 10:19:38 -0700

    fix(pr): Hallucinated types

--- src/sentry/auth_v2/endpoints/feature_flag_view.py
+++ src/sentry/auth_v2/endpoints/feature_flag_view.py
@@ -1,7 +1,9 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry.api.base import ApiOwner, ApiPublishStatus, RateLimit, RateLimitCategory
+from sentry.api.api_owners import ApiOwner
+from sentry.api.api_publish_status import ApiPublishStatus
+from sentry.types.ratelimit import RateLimit, RateLimitCategory
 
 from .base import AuthV2Endpoint
 
