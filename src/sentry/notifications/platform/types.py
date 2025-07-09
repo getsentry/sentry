3e046da9ddd8ec4f289af43d436a2b99dc4caf6b
Leander Rodrigues <leander.rodrigues@sentry.io>
2025-07-09 12:32:01 -0400

    feat(notif-platform): Prototype the template layer (#94369)
    This PR introduces some more structure around templates, including a
    template registry. I've also added some tests to ensure developer
    interface with it correctly.
    
    For example, registering with an unknown key:
    
    ```
    @template_registry.register('some-unknown-key')
    MyTemplate(...): ...
    ```
    will yield:
    ```
    tests/sentry/notifications/platform/test_registry.py:40: in test_no_unknown_registration_keys
        assert (
    E   AssertionError: Template <class 'sentry.notifications.platform.registry.DemoNotificationTemplate'> was registered with an unknown key (some-unknown-key)
    E
    E   assert 'some-unknown-key' in NotificationTemplateKey
    ```
    and forgetting a template, but adding to NotificationTemplateKey
    
    ```
    tests/sentry/notifications/platform/test_registry.py:55: in test_no_unused_production_keys
        raise AssertionError(
    E   AssertionError: Known NotificationTemplateKey(s) have no associated template registered: 'my_new_template'
    E   Use @template_registry.register() to register them, or explicitly exclude them within this test.
    
    ```
    
    We've discussed this below block offline, it's the way forward for now
    so collapsing it for legibility
      <details> 
      <summary>Explanation of NotificationRenderedTemplate</summary>
      
      This has a few things to consider though: 
    - For email, we have flexibility, we can just pass the existing django
    templates and get whatever level of customization we want by modifying
    the templates.
    - For integrations though, `body` (for example) is just a `str`. So if a
    notification wanted bold the text of a slack message, it'd have to be
    signaled within the `str` to bold somehow, likely from a shared
    templating language (e.g. markdown `**`s), which is idealistic. This
    would have to be understood by all providers; which is reasonable for
    MSTeams/Slack/Discord, but might not work for future providers(?) if
    they don't parse markdown, or do so differently (e.g notion uses `|
    text` for quotes, md uses `> text` even though most other syntax is
    md-like)
    - If we allow markdown in the `str` attributes, it's likely we'd get
    bugs as soon as someone tries a provider specific md formatting (e.g.
    imagine like `f"[John](@john)"` or something)
    - Onboarding new providers would also become a minefield if there is
    syntax assumed to be understood by all that is done differently for
    other integrations (e.g. if jira uses [img:link](...) but slack uses
    ![link](...)) -- If a new provider needs a new format, it means every
    notify call must now be updated to include the new syntax :(
      
        
    The only way around this I can see is restricting to top level building
    blocks (e.g. `subject`, `body`, `action`, `chart`) that explicitly will
    not allow custom formatting, and deferring to renderers to do all
    non-standard elements. Pros: Consistency out of the box, and devs never
    need to learn slack markdown vs discord markdown. Cons: If a dev wants
    to bold text, they need a custom renderer for each provider 🙃
      </details>

--- src/sentry/notifications/platform/types.py
+++ src/sentry/notifications/platform/types.py
@@ -1,7 +1,9 @@
 from __future__ import annotations
 
+import abc
+from dataclasses import dataclass
 from enum import StrEnum
-from typing import Any, Protocol
+from typing import Any, Protocol, TypedDict
 
 from sentry.integrations.types import ExternalProviderEnum
 
@@ -13,17 +15,16 @@ class NotificationCategory(StrEnum):
     The exception is the `DEBUG` category, which is used for testing and development.
     """
 
-    DEBUG = "debug"
     # TODO(ecosystem): Connect this to NotificationSettingEnum
+    DEBUG = "debug"
+
+    def get_sources(self) -> list[str]:
+        return NOTIFICATION_SOURCE_MAP[self]
 
 
-class NotificationSource(StrEnum):
-    """
-    An unique identifier for each notification. Each source should map to one way a notification
-    can be sent.
-    """
-
-    TEST = "test"
+NOTIFICATION_SOURCE_MAP = {
+    NotificationCategory.DEBUG: ["test"],
+}
 
 
 class NotificationProviderKey(StrEnum):
@@ -59,30 +60,115 @@ class NotificationTarget(Protocol):
     specific_data: dict[str, Any] | None
 
 
-class NotificationData(Protocol):
-    """
-    All data passing through the notification platform must adhere to this protocol.
-    """
-
-    category: NotificationCategory
-    source: NotificationSource
-
-
-# TODO(ecosystem): Replace this 'Any' with a concrete known template output.
-type NotificationRenderedTemplate = Any
-
-
-class NotificationTemplate[T: NotificationData](Protocol):
-    """
-    All templates of the notification platform must adhere to this protocol.
-    """
-
-    def process(self, *, data: T) -> NotificationRenderedTemplate: ...
-
-
 class NotificationStrategy(Protocol):
     """
     A strategy for determining which targets to send a notification to.
     """
 
     def get_targets(self) -> list[NotificationTarget]: ...
+
+
+class NotificationData(Protocol):
+    """
+    All data passing through the notification platform must adhere to this protocol.
+    """
+
+    source: str
+    """
+    The source is uniquely attributable to the way this notification was sent. It will be tracked in
+    metrics/analytics to determine the egress from a given code-path or service.
+    """
+
+
+class NotificationRenderedAction(TypedDict):
+    """
+    A rendered action for an integration.
+    """
+
+    label: str
+    """
+    The text content of the action (usually appears as a button).
+    This string should not contain any formatting, and will be displayed as is.
+    """
+    link: str
+    """
+    The underlying link of the action.
+    """
+
+
+@dataclass(frozen=True)
+class NotificationRenderedTemplate:
+    subject: str
+    """
+    The subject or title of the notification. It's expected that the receiver understand the
+    expected content of the notification based on this alone, and it will be the first thing
+    they see. This string should not contain any formatting, and will be displayed as is.
+    """
+    body: str
+    """
+    The full contents of the notification. Put the details of the notification here, but consider
+    keeping it concise and useful to the receiver. This string should not contain any formatting,
+    and will be displayed as is.
+    """
+    actions: list[NotificationRenderedAction]
+    """
+    The list of actions that a receiver may take after having received the notification.
+    """
+    chart: str | None = None
+    """
+    The accessible URL of a chart that will be displayed in the notification.
+    """
+    footer: str | None = None
+    """
+    Extra notification content that will appear after any actions, separate from the body. Optional,
+    and consider omitting if the extra data is not necessary for your notification to be useful.
+    This string should not contain any formatting, and will be displayed as is.
+    """
+
+    # The following are optional, as omitting them will use a default email template which expects
+    # the required fields above to be present instead.
+    email_html_path: str | None = None
+    """
+    The email HTML template file path. The associated NotificationData will be passed as context.
+    In general, try to avoid including different information in these Django Templates than appear
+    in the required fields, as it will make the contents of your notification vary from email to other
+    providers.
+    """
+    email_text_path: str | None = None
+    """
+    The email text template file path. The associated NotificationData will be passed as context.
+    In general, try to avoid including different information in these Django Templates than appear
+    in the required fields, as it will make the contents of your notification vary from email to other
+    providers.
+    """
+
+
+class NotificationTemplate[T: NotificationData](abc.ABC):
+    category: NotificationCategory
+    """
+    The category that a notification belongs to. This will be used to determine which settings a
+    user needs to modify to manage receipt of these notifications (if applicable).
+    """
+    # @property
+    # @abc.abstractmethod
+    # def category(self) -> NotificationCategory:
+    #     """
+    #     The category that a notification belongs to. This will be used to determine which settings a
+    #     user needs to modify to manage receipt of these notifications (if applicable).
+    #     """
+    #     ...
+
+    @abc.abstractmethod
+    def render(self, data: T) -> NotificationRenderedTemplate:
+        """
+        Produce a rendered template given the notification data. Usually, this will involve
+        formatting the data into user-friendly strings of text.
+        """
+        ...
+
+    @abc.abstractmethod
+    def render_example(self) -> NotificationRenderedTemplate:
+        """
+        Used to produce a debugging example rendered template for this notification. This
+        implementation should be pure, and not populate with any live data.
+        """
--- tests/sentry/notifications/platform/test_service.py
+++ tests/sentry/notifications/platform/test_service.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import Any
 from unittest import mock
 
@@ -7,37 +6,15 @@ import pytest
 from sentry.notifications.platform.service import NotificationService, NotificationServiceError
 from sentry.notifications.platform.target import GenericNotificationTarget, prepare_targets
 from sentry.notifications.platform.types import (
-    NotificationCategory,
-    NotificationData,
     NotificationProviderKey,
-    NotificationRenderedTemplate,
-    NotificationSource,
-    NotificationStrategy,
-    NotificationTarget,
     NotificationTargetResourceType,
-    NotificationTemplate,
 )
 from sentry.testutils.cases import TestCase
-
-
-@dataclass(kw_only=True, frozen=True)
-class MockNotification(NotificationData):
-    category = NotificationCategory.DEBUG
-    source = NotificationSource.TEST
-    message: str
-
-
-class MockNotificationTemplate(NotificationTemplate[MockNotification]):
-    def process(self, *, data: MockNotification) -> NotificationRenderedTemplate:
-        return data.message
-
-
-class MockStrategy(NotificationStrategy):
-    def __init__(self, *, targets: list[NotificationTarget]):
-        self.targets = targets
-
-    def get_targets(self) -> list[NotificationTarget]:
-        return self.targets
+from sentry.testutils.notifications.platform import (
+    MockNotification,
+    MockNotificationTemplate,
+    MockStrategy,
+)
 
 
 class NotificationServiceTest(TestCase):
@@ -51,10 +28,7 @@ class NotificationServiceTest(TestCase):
 
     def test_basic_notify(self):
         service = NotificationService(data=MockNotification(message="this is a test notification"))
-        service.notify(
-            targets=[self.target],
-            template=self.template,
-        )
+        service.notify(targets=[self.target])
 
     @mock.patch("sentry.notifications.platform.service.logger")
     def test_validation_on_notify(self, mock_logger):
@@ -63,16 +37,16 @@ class NotificationServiceTest(TestCase):
             NotificationServiceError,
             match="Must provide either a strategy or targets. Strategy is preferred.",
         ):
-            service.notify(template=self.template)
+            service.notify()
 
         strategy = MockStrategy(targets=[])
         with pytest.raises(
             NotificationServiceError,
             match="Cannot provide both strategy and targets, only one is permitted. Strategy is preferred.",
         ):
-            service.notify(strategy=strategy, targets=[self.target], template=self.template)
+            service.notify(strategy=strategy, targets=[self.target])
 
-        service.notify(strategy=strategy, template=self.template)
+        service.notify(strategy=strategy)
         mock_logger.info.assert_called_once_with(
             "Strategy '%s' did not yield targets", strategy.__class__.__name__
         )
@@ -80,10 +54,7 @@ class NotificationServiceTest(TestCase):
     def test_basic_notify_prepared_target(self):
         service = NotificationService(data=MockNotification(message="this is a test notification"))
         prepare_targets([self.target])
-        service.notify_prepared_target(
-            target=self.target,
-            template=self.template,
-        )
+        service.notify_prepared_target(target=self.target)
 
     def test_validation_on_notify_prepared_target(self):
         empty_data_service: NotificationService[Any] = NotificationService(data=None)
@@ -91,14 +62,11 @@ class NotificationServiceTest(TestCase):
             NotificationServiceError,
             match="Notification service must be initialized with data before sending!",
         ):
-            empty_data_service.notify_prepared_target(target=self.target, template=self.template)
+            empty_data_service.notify_prepared_target(target=self.target)
 
         service = NotificationService(data=MockNotification(message="this is a test notification"))
         with pytest.raises(
             NotificationServiceError,
             match="Target must have `prepare_targets` called prior to sending",
         ):
-            service.notify_prepared_target(
-                target=self.target,
-                template=self.template,
-            )
+            service.notify_prepared_target(target=self.target)
