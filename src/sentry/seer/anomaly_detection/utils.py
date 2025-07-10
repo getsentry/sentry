78a7f82cab8c0c45724c7ed61dbcd3c8bff2b5d8
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2025-07-10 16:15:03 -0700

    fix(aci milestone 3): more anomaly detection processing fixes (#95302)
    Not sure if the memory location is the cause, but in any case this is
    the same logic but more reliable.

--- src/sentry/seer/anomaly_detection/utils.py
+++ src/sentry/seer/anomaly_detection/utils.py
@@ -45,7 +45,7 @@ def get_anomaly_evaluation_from_workflow_engine(
 ) -> bool | DetectorEvaluationResult | None:
     evaluation = None
     for result in data_packet_processing_results:
-        if result[0] == detector:
+        if result[0].id == detector.id:
             evaluation = result[1].get("values")
             if evaluation:
                 return evaluation.priority == DetectorPriorityLevel.HIGH
