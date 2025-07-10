4028adf770563745557478195e366760d28c4a42
Charles Paul <CharlieLPaul+github@gmail.com>
2025-07-10 15:06:48 -0700

    remove redundant option

--- static/app/views/organizationStats/teamInsights/controls.tsx
+++ static/app/views/organizationStats/teamInsights/controls.tsx
@@ -233,7 +233,6 @@ function TeamStatsControls({
           ...relativeOptions,
           ...props.arbitraryOptions,
         })}
-        disallowArbitraryRelativeRanges={false}
         triggerLabel={
           period &&
           // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
