8143000e1aaec75e37ef19223ec125e41d0a87b3
Fabian Schindler <fabian.schindler@sentry.io>
2025-07-10 09:22:53 +0200

    ref(analytics): Transform analytics events for TET-825
    - Transform event classes to use @analytics.eventclass decorator
    - Transform analytics.record calls to use event class instances
    - Update imports as needed
    
    Closes TET-825

--- src/sentry/analytics/events/eventuser_snuba_query.py
+++ src/sentry/analytics/events/eventuser_snuba_query.py
@@ -1,17 +1,14 @@
 from sentry import analytics
 
 
+@analytics.eventclass("eventuser_snuba.query")
 class EventUserSnubaQuery(analytics.Event):
-    type = "eventuser_snuba.query"
-
-    attributes = (
-        analytics.Attribute("project_ids", type=list),
-        analytics.Attribute("query"),
-        analytics.Attribute("query_try", type=int),
-        analytics.Attribute("count_rows_returned", required=True, type=int),
-        analytics.Attribute("count_rows_filtered", required=True, type=int),
-        analytics.Attribute("query_time_ms", type=int),
-    )
+    project_ids: list
+    query: str
+    query_try: int
+    count_rows_returned: int
+    count_rows_filtered: int
+    query_time_ms: int
 
 
 analytics.register(EventUserSnubaQuery)
