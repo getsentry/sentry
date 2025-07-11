a1ece246ad169397620cbfa6cf4d031f4d6f9e57
Rohan Agarwal <rohan.agarwal@sentry.io>
2025-07-11 10:39:12 -0400

    Settings clarity improvements

--- static/app/views/settings/projectSeer/autofixRepositories.tsx
+++ static/app/views/settings/projectSeer/autofixRepositories.tsx
@@ -279,7 +279,7 @@ export function AutofixRepositories({project}: ProjectSeerProps) {
         </LoadingContainer>
       ) : filteredSelectedRepositories.length === 0 ? (
         <EmptyMessage>
-          {t('No repositories selected. Click "Add Repos" to get started.')}
+          {t('Your code is not available to Seer. Click "Add Repos" to get started.')}
         </EmptyMessage>
       ) : (
         <ReposContainer>
@@ -313,7 +313,7 @@ const ReposContainer = styled('div')`
 
 const EmptyMessage = styled('div')`
   padding: ${space(2)};
-  color: ${p => p.theme.subText};
+  color: ${p => p.theme.errorText};
   text-align: center;
   font-size: ${p => p.theme.fontSize.md};
 `;
--- static/app/views/settings/projectSeer/constants.ts
+++ static/app/views/settings/projectSeer/constants.ts
@@ -1,1 +1,44 @@
+import {t} from 'sentry/locale';
+
 export const MAX_REPOS_LIMIT = 8;
+
+export const SEER_THRESHOLD_OPTIONS = [
+  {
+    value: 'off',
+    label: t('Off'),
+    details: t('Seer will never analyze any issues without manually clicking Start.'),
+  },
+  {
+    value: 'super_low',
+    label: t('Only the Most Actionable Issues'),
+    details: t(
+      'Seer will automatically run on issues that it thinks have an actionability of "super high." This targets around 2% of issues, but may vary by project.'
+    ),
+  },
+  {
+    value: 'low',
+    label: t('Highly Actionable and Above'),
+    details: t(
+      'Seer will automatically run on issues that it thinks have an actionability of "high" or above. This targets around 10% of issues, but may vary by project.'
+    ),
+  },
+  {
+    value: 'medium',
+    label: t('Moderately Actionable and Above'),
+    details: t(
+      'Seer will automatically run on issues that it thinks have an actionability of "medium" or above. This targets around 30% of issues, but may vary by project.'
+    ),
+  },
+  {
+    value: 'high',
+    label: t('Minimally Actionable and Above'),
+    details: t(
+      'Seer will automatically run on issues that it thinks have an actionability of "low" or above. This targets around 70% of issues, but may vary by project.'
+    ),
+  },
+  {
+    value: 'always',
+    label: t('All Issues'),
+    details: t('Seer will automatically run on all new issues.'),
+  },
+] as const;
