dea7bfff1a92fffe9a0478d029c07c5dbf1647a9
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-07-10 21:03:31 -0700

    fix labels and pluralization

--- static/app/components/replays/table/replayTableHeaderActions.tsx
+++ static/app/components/replays/table/replayTableHeaderActions.tsx
@@ -5,7 +5,7 @@ import {Alert} from 'sentry/components/core/alert';
 import {Flex} from 'sentry/components/core/layout/flex';
 import {ReplaySelectColumn} from 'sentry/components/replays/table/replayTableColumns';
 import {SimpleTable} from 'sentry/components/tables/simpleTable';
-import {t, tct} from 'sentry/locale';
+import {t, tct, tn} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {useListItemCheckboxContext} from 'sentry/utils/list/useListItemCheckboxState';
 
@@ -34,10 +34,10 @@ export default function ReplayTableHeaderActions({listItemCheckboxState}: Props)
       {isAllSelected === 'indeterminate' ? (
         <FullGridAlert type="warning" system>
           <Flex justify="center" wrap="wrap" gap={space(1)}>
-            {t('Selected %s replays.', countSelected)}
+            {tn('Selected %s replay.', 'Selected %s replays.', countSelected)}
             <a onClick={selectAll}>
               {queryString
-                ? tct('Select all replays that matching: [queryString].', {
+                ? tct('Select all replays that match: [queryString].', {
                     queryString: <var>{queryString}</var>,
                   })
                 : t('Select all replays.')}
