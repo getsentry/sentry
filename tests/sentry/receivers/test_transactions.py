2d2ec4d3d94831dd4a69a343896991a37df4f183
Charlie Luo <cvxluo@gmail.com>
2025-07-13 14:35:31 -0700

    clean up typos + tests

--- tests/sentry/receivers/test_transactions.py
+++ tests/sentry/receivers/test_transactions.py
@@ -66,7 +66,6 @@ class RecordFirstTransactionTest(TestCase):
 
     @patch("sentry.analytics.record")
     def test_analytics_event(self, mock_record):
-        self.project.update(platform="python")
         assert not self.project.flags.has_transactions
         event = self.store_event(
             data={
