2ef3e196b32d84bbe55e860eff4e8dc76b21b215
Jasmin <77064737+jas-kas@users.noreply.github.com>
2025-07-14 15:35:42 -0400

    Update static/app/components/replays/table/replayTableColumns.tsx
    Co-authored-by: Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>

--- static/app/components/replays/table/replayTableColumns.tsx
+++ static/app/components/replays/table/replayTableColumns.tsx
@@ -421,7 +421,7 @@ export const ReplaySelectColumn: ReplayTableColumn = {
 
 export const ReplaySessionColumn: ReplayTableColumn = {
   Header: () => (
-    <Tooltip title={t('Replays are sorted by time sent.')}>{t('Replay')}</Tooltip>
+    <Tooltip title={t('By default, replays are sorted by time sent.')}>{t('Replay')}</Tooltip>
   ),
   sortKey: 'started_at',
   width: 'minmax(150px, 1fr)',
