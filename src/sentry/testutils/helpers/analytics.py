edc176b300967b2f4bf000640ba4b99f70b661bb
Fabian Schindler <fabian.schindler@sentry.io>
2025-07-14 14:08:07 +0200

    test(analytics): add additional assertion helper functions

--- src/sentry/testutils/helpers/analytics.py
+++ src/sentry/testutils/helpers/analytics.py
@@ -10,12 +10,20 @@ def assert_event_equal(
     recorded_event: Event,
     check_uuid: bool = False,
     check_datetime: bool = False,
+    exclude_fields: list[str] | None = None,
 ):
+    if type(expected_event) is not type(recorded_event):
+        raise AssertionError(
+            f"Expected event type {type(expected_event)} but got {type(recorded_event)}"
+        )
+
     assert expected_event.type == recorded_event.type
     for field in fields(expected_event):
         if field.name == "uuid_" and not check_uuid:
             continue
         if field.name == "datetime_" and not check_datetime:
+            continue
+        if exclude_fields and field.name in exclude_fields:
             continue
         assert getattr(expected_event, field.name) == getattr(recorded_event, field.name)
 
@@ -25,11 +33,32 @@ def assert_analytics_events_recorded(
     expected_events: list[Event],
     check_uuid: bool = False,
     check_datetime: bool = False,
+    exclude_fields: list[str] | None = None,
 ):
     recorded_events = [call.args[0] for call in mock_record.call_args_list]
     assert len(expected_events) == len(recorded_events)
     for expected_event, recorded_event in zip(expected_events, recorded_events):
-        assert_event_equal(expected_event, recorded_event, check_uuid, check_datetime)
+        assert_event_equal(
+            expected_event, recorded_event, check_uuid, check_datetime, exclude_fields
+        )
+
+
+def get_last_analytics_event(mock_record: MagicMock) -> Event:
+    return mock_record.call_args_list[-1].args[0]
+
+
+def assert_last_analytics_event(
+    mock_record: MagicMock,
+    expected_event: Event,
+    check_uuid: bool = False,
+    check_datetime: bool = False,
+):
+    assert_event_equal(
+        expected_event,
+        get_last_analytics_event(mock_record),
+        check_uuid,
+        check_datetime,
+    )
 
 
 @contextlib.contextmanager
@@ -37,6 +66,7 @@ def assert_analytics_events(
     expected_events: list[Event],
     check_uuid: bool = False,
     check_datetime: bool = False,
+    exclude_fields: list[str] | None = None,
 ):
     """
     Context manager that allows you to track analytics events recorded during the context.
@@ -48,4 +78,6 @@ def assert_analytics_events(
     """
     with patch("sentry.analytics.record") as mock_record:
         yield
-        assert_analytics_events_recorded(mock_record, expected_events, check_uuid, check_datetime)
+        assert_analytics_events_recorded(
+            mock_record, expected_events, check_uuid, check_datetime, exclude_fields
+        )
