8e18a0335b04df81d14e218e8758b680754ba236
Fabian Schindler <fabian.schindler@sentry.io>
2025-07-14 14:57:18 +0200

    fix(analytics): typing issues

--- src/sentry/analytics/events/project_created.py
+++ src/sentry/analytics/events/project_created.py
@@ -3,11 +3,11 @@ from sentry import analytics
 
 @analytics.eventclass("project.created")
 class ProjectCreatedEvent(analytics.Event):
-    user_id: str | None = None
+    user_id: int | None = None
     default_user_id: str
     organization_id: str
     origin: str | None = None
-    project_id: str
+    project_id: int
     platform: str | None = None
 
 
