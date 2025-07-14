c576043b953977d59decb4a1340a1b8817cb947a
Malachi Willey <malwilley@gmail.com>
2025-07-14 12:47:22 -0700

    feat(aci): Implement uptime detector details page (#95391)

--- static/app/views/detectors/components/details/uptime/index.tsx
+++ static/app/views/detectors/components/details/uptime/index.tsx
@@ -1,21 +1,24 @@
+import {KeyValueTableRow} from 'sentry/components/keyValueTable';
 import DetailLayout from 'sentry/components/workflowEngine/layout/detail';
+import Section from 'sentry/components/workflowEngine/ui/section';
+import {t} from 'sentry/locale';
 import type {Project} from 'sentry/types/project';
-import type {Detector} from 'sentry/types/workflowEngine/detectors';
+import type {UptimeDetector} from 'sentry/types/workflowEngine/detectors';
+import getDuration from 'sentry/utils/duration/getDuration';
 import {DetectorDetailsAssignee} from 'sentry/views/detectors/components/details/common/assignee';
 import {DetectorDetailsAutomations} from 'sentry/views/detectors/components/details/common/automations';
 import {DetectorExtraDetails} from 'sentry/views/detectors/components/details/common/extraDetails';
 import {DetectorDetailsHeader} from 'sentry/views/detectors/components/details/common/header';
 import {DetectorDetailsOngoingIssues} from 'sentry/views/detectors/components/details/common/ongoingIssues';
 
-type FallbackDetectorDetailsProps = {
-  detector: Detector;
+type UptimeDetectorDetailsProps = {
+  detector: UptimeDetector;
   project: Project;
 };
 
-export function FallbackDetectorDetails({
-  detector,
-  project,
-}: FallbackDetectorDetailsProps) {
+export function UptimeDetectorDetails({detector, project}: UptimeDetectorDetailsProps) {
+  const dataSource = detector.dataSources[0];
+
   return (
     <DetailLayout>
       <DetectorDetailsHeader detector={detector} project={project} />
@@ -25,12 +28,28 @@ export function FallbackDetectorDetails({
           <DetectorDetailsAutomations detector={detector} />
         </DetailLayout.Main>
         <DetailLayout.Sidebar>
+          <Section title={t('Detect')}>{t('Three consecutive failed checks.')}</Section>
+          <Section title={t('Resolve')}>
+            {t('Three consecutive successful checks.')}
+          </Section>
           <DetectorDetailsAssignee owner={detector.owner} />
           <DetectorExtraDetails>
+            <KeyValueTableRow
+              keyName={t('Interval')}
+              value={t('Every %s', getDuration(dataSource.queryObj.intervalSeconds))}
+            />
+            <KeyValueTableRow
+              keyName={t('URL')}
+              value={detector.dataSources[0].queryObj.url}
+            />
+            <KeyValueTableRow
+              keyName={t('Method')}
+              value={detector.dataSources[0].queryObj.method}
+            />
+            <DetectorExtraDetails.Environment detector={detector} />
             <DetectorExtraDetails.DateCreated detector={detector} />
             <DetectorExtraDetails.CreatedBy detector={detector} />
             <DetectorExtraDetails.LastModified detector={detector} />
-            <DetectorExtraDetails.Environment detector={detector} />
           </DetectorExtraDetails>
         </DetailLayout.Sidebar>
       </DetailLayout.Body>
