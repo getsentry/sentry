785dea8dbf1bee9928e958e5158f81da45a363b0
Fabian Schindler <fabian.schindler@sentry.io>
2025-07-14 10:23:45 +0200

    fix(analytics): field types and constructor parameters

--- src/sentry/analytics/events/organization_joined.py
+++ src/sentry/analytics/events/organization_joined.py
@@ -3,8 +3,8 @@ from sentry import analytics
 
 @analytics.eventclass("organization.joined")
 class OrganizationJoinedEvent(analytics.Event):
-    user_id: str
-    organization_id: str
+    user_id: int
+    organization_id: int
 
 
 analytics.register(OrganizationJoinedEvent)
