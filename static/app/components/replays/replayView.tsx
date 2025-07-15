917ac97c392d3e0f2b259f0f339a813aa7e46b1a
Jasmin <77064737+jas-kas@users.noreply.github.com>
2025-07-15 16:00:55 -0400

    Update static/app/components/replays/replayView.tsx

--- static/app/components/replays/replayView.tsx
+++ static/app/components/replays/replayView.tsx
@@ -137,7 +137,6 @@ function ReplayView({toggleFullscreen, isLoading}: Props) {
     return (
       <TooltipContext
         value={{
-          // container instead of document.body to avoid being clipped. This ensures
           // that browser/OS icons and size information tooltips remain visible.
           // See: https://developer.mozilla.org/en-US/docs/Web/API/Fullscreen_API#things_to_keep_in_mind
           container: document.fullscreenElement,
