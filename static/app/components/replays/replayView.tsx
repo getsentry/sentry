bab8750410a897134ed7e160b74c8dab4aab3351
Jasmin <77064737+jas-kas@users.noreply.github.com>
2025-07-15 15:59:49 -0400

    Update static/app/components/replays/replayView.tsx

--- static/app/components/replays/replayView.tsx
+++ static/app/components/replays/replayView.tsx
@@ -133,7 +133,6 @@ function ReplayView({toggleFullscreen, isLoading}: Props) {
     </Fragment>
   );
 
-  // to prevent them from being clipped by the fullscreen boundary
   if (isFullscreen) {
     return (
       <TooltipContext
