374303a1965a6a78970616842f169bac6bafdadb
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-07-15 10:33:33 -0700

    ref(feedback): Replace `react-virtualized` with `@tanstack/react-virtual` in Feedback (#95398)
    The list still looks the same, this was surprisingly easy actually. 
    
    
    <img width="448" height="322" alt="SCR-20250712-kygo"
    src="https://github.com/user-attachments/assets/4fef63f2-57c7-48ae-9484-ab4f45f8ed64"
    />
    
    2 helper files i pulled in from sentry-toolbar
    ([InfiniteListItems](https://github.com/getsentry/sentry-toolbar/blob/main/src/lib/components/InfiniteListItems.tsx),
    [InfiniteListState](https://github.com/getsentry/sentry-toolbar/blob/main/src/lib/components/InfiniteListState.tsx))
    made it easier too, they wrap a bunch of state conditions and also wrap
    the load-more triggers which really cleanup this `<FeedbackList>`
    component now.
    
    Related to https://github.com/getsentry/frontend-tsc/issues/91

--- static/app/components/feedback/list/feedbackListItem.tsx
+++ static/app/components/feedback/list/feedbackListItem.tsx
@@ -1,4 +1,3 @@
-import type {CSSProperties} from 'react';
 import styled from '@emotion/styled';
 
 import {ActorAvatar} from 'sentry/components/core/avatar/actorAvatar';
@@ -29,8 +28,6 @@ interface Props {
   feedbackItem: FeedbackIssueListItem;
   isSelected: 'all-selected' | boolean;
   onSelect: (isSelected: boolean) => void;
-  ref?: React.Ref<HTMLDivElement>;
-  style?: CSSProperties;
 }
 
 function useIsSelectedFeedback({feedbackItem}: {feedbackItem: FeedbackIssueListItem}) {
@@ -41,7 +38,7 @@ function useIsSelectedFeedback({feedbackItem}: {feedbackItem: FeedbackIssueListI
   return feedbackId === feedbackItem.id;
 }
 
-function FeedbackListItem({feedbackItem, isSelected, onSelect, style, ref}: Props) {
+export default function FeedbackListItem({feedbackItem, isSelected, onSelect}: Props) {
   const organization = useOrganization();
   const isOpen = useIsSelectedFeedback({feedbackItem});
   const {feedbackHasReplay} = useReplayCountForFeedbacks();
@@ -53,7 +50,7 @@ function FeedbackListItem({feedbackItem, isSelected, onSelect, style, ref}: Prop
   const hasComments = feedbackItem.numComments > 0;
 
   return (
-    <CardSpacing ref={ref} style={style}>
+    <CardSpacing>
       <LinkedFeedbackCard
         data-selected={isOpen}
         to={{
@@ -168,8 +165,6 @@ function FeedbackListItem({feedbackItem, isSelected, onSelect, style, ref}: Prop
     </CardSpacing>
   );
 }
-
-export default FeedbackListItem;
 
 const LinkedFeedbackCard = styled(Link)`
   position: relative;
