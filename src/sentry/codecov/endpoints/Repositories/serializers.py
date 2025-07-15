81841a06fa838555bc82d65a1e87bcfb215cc367
Calvin Yau <calvin.yau@sentry.io>
2025-07-15 15:00:02 -0700

    refactor(codecov): Move pageInfo serializer to be shared

--- src/sentry/codecov/endpoints/Repositories/serializers.py
+++ src/sentry/codecov/endpoints/Repositories/serializers.py
@@ -3,7 +3,7 @@ import logging
 import sentry_sdk
 from rest_framework import serializers
 
-from sentry.codecov.endpoints.TestResults.serializers import PageInfoSerializer
+from sentry.codecov.endpoints.serializers import PageInfoSerializer
 
 logger = logging.getLogger(__name__)
 
--- src/sentry/codecov/endpoints/TestResults/serializers.py
+++ src/sentry/codecov/endpoints/TestResults/serializers.py
@@ -3,6 +3,8 @@ import logging
 import sentry_sdk
 from rest_framework import serializers
 
+from sentry.codecov.endpoints.serializers import PageInfoSerializer
+
 logger = logging.getLogger(__name__)
 
 
@@ -25,17 +27,6 @@ class TestResultNodeSerializer(serializers.Serializer):
     totalSkipCount = serializers.IntegerField()
     totalPassCount = serializers.IntegerField()
     lastDuration = serializers.FloatField()
-
-
-class PageInfoSerializer(serializers.Serializer):
-    """
-    Serializer for pagination information
-    """
-
-    endCursor = serializers.CharField(allow_null=True)
-    startCursor = serializers.CharField(allow_null=True)
-    hasPreviousPage = serializers.BooleanField()
-    hasNextPage = serializers.BooleanField()
 
 
 class TestResultSerializer(serializers.Serializer):
