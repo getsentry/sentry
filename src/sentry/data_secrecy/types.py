fb6168b13a635286b5ede947c237316e34ad913d
Raj Joshi <raj.joshi@sentry.io>
2025-07-15 10:34:41 -0700

    :mag: nit: update cache key

--- src/sentry/data_secrecy/types.py
+++ src/sentry/data_secrecy/types.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
 
-CACHE_KEY_PATTERN = "data_secrecy_waiver:{organization_id}"
+CACHE_KEY_PATTERN = "data_access_grant:effective_grant_status:{}"
 NEGATIVE_CACHE_TTL = 15 * 60  # 15 minutes
 NEGATIVE_CACHE_VALUE = {"no_active_grants": True}  # Special value for negative caching
