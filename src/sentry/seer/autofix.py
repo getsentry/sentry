c70c167053b05d042bb826522e3a8544d7828141
William Mak <william@wmak.io>
2025-07-16 16:26:02 -0400

    fix: seer code

--- src/sentry/seer/autofix.py
+++ src/sentry/seer/autofix.py
@@ -19,6 +19,7 @@ from sentry.eventstore.models import Event, GroupEvent
 from sentry.models.group import Group
 from sentry.models.project import Project
 from sentry.profiles.utils import get_from_profiling_service
+from sentry.search.eap.types import SearchResolverConfig
 from sentry.search.events.types import EventsResponse, SnubaParams
 from sentry.seer.seer_setup import get_seer_org_acknowledgement
 from sentry.seer.signed_seer_api import sign_with_seer_secret
@@ -57,7 +58,9 @@ def _get_logs_for_event(
         organization=project.organization,
     )
 
-    results: EventsResponse = ourlogs.query(
+    results: EventsResponse = ourlogs.run_table_query(
+        snuba_params,
+        f"trace:{trace_id}",
         selected_columns=[
             "project.id",
             "timestamp",
@@ -66,12 +69,11 @@ def _get_logs_for_event(
             "code.file.path",
             "code.function.name",
         ],
-        query=f"trace:{trace_id}",
-        snuba_params=snuba_params,
         orderby=["-timestamp"],
         offset=0,
         limit=100,
         referrer=Referrer.API_GROUP_AI_AUTOFIX,
+        config=SearchResolverConfig(),
     )
     data = results["data"]
 
