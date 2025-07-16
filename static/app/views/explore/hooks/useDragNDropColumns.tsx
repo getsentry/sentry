243e6c7b2485a30a3cb8dc8f2f3688bd048f8dd7
Tony Xiao <txiao@sentry.io>
2025-07-16 14:59:08 -0400

    ref(explore): Simplify useDragNDropColumns hook
    This simplifies the `useDragNDropColumns` hook as it was unnecessarily using
    states and effects. This is causing unexpected updates in tests.

--- static/app/views/explore/hooks/useDragNDropColumns.tsx
+++ static/app/views/explore/hooks/useDragNDropColumns.tsx
@@ -1,4 +1,4 @@
-import {useEffect, useMemo, useState} from 'react';
+import {useMemo} from 'react';
 import type {DragEndEvent} from '@dnd-kit/core';
 import {arrayMove} from '@dnd-kit/sortable';
 
@@ -13,70 +13,33 @@ interface UseDragAndDropColumnsProps<T> {
   setColumns: (columns: T[], op: 'insert' | 'update' | 'delete' | 'reorder') => void;
 }
 
-function extractColumns<T>(editableColumns: Array<Column<T>>) {
-  return editableColumns.map(({column}) => column);
-}
-
 export function useDragNDropColumns<T>({
   columns,
   defaultColumn,
   setColumns,
 }: UseDragAndDropColumnsProps<T>) {
-  const mappedColumns = useMemo(() => {
+  const editableColumns = useMemo(() => {
     return columns.map((column, i) => ({id: i + 1, column}));
   }, [columns]);
 
-  const [editableColumns, setEditableColumns] = useState<Array<Column<T>>>(mappedColumns);
-
-  useEffect(() => {
-    setEditableColumns(prevEditableColumns => {
-      // Only update if there's a change between columns and editableColumns
-      if (
-        JSON.stringify(prevEditableColumns.map(c => c.column)) !== JSON.stringify(columns)
-      ) {
-        return mappedColumns;
-      }
-      return prevEditableColumns;
-    });
-  }, [columns, mappedColumns]);
-
-  const [nextId, setNextId] = useState(editableColumns.length + 1);
-
   function insertColumn(column?: T) {
-    setEditableColumns(oldEditableColumns => {
-      const newEditableColumns = oldEditableColumns.slice();
-      newEditableColumns.push({id: nextId, column: column ?? defaultColumn()});
-
-      setNextId(nextId + 1); // make sure to increment the id for the next time
-
-      setColumns(extractColumns(newEditableColumns), 'insert');
-
-      return newEditableColumns;
-    });
+    setColumns([...columns, column ?? defaultColumn()], 'insert');
   }
 
   function updateColumnAtIndex(i: number, column: T) {
-    setEditableColumns(oldEditableColumns => {
-      const newEditableColumns = [...oldEditableColumns];
-      newEditableColumns[i]!.column = column;
-
-      setColumns(extractColumns(newEditableColumns), 'update');
-
-      return newEditableColumns;
-    });
+    setColumns(
+      columns.map((col: T, j: number) => {
+        return i === j ? column : col;
+      }),
+      'update'
+    );
   }
 
   function deleteColumnAtIndex(i: number) {
-    setEditableColumns(oldEditableColumns => {
-      const newEditableColumns =
-        oldEditableColumns.length === 1
-          ? [{id: 1, column: defaultColumn()}]
-          : [...oldEditableColumns.slice(0, i), ...oldEditableColumns.slice(i + 1)];
-
-      setColumns(extractColumns(newEditableColumns), 'delete');
-
-      return newEditableColumns;
-    });
+    setColumns(
+      columns.filter((_: T, j: number) => i !== j),
+      'delete'
+    );
   }
 
   function onDragEnd(event: DragEndEvent) {
@@ -85,14 +48,7 @@ export function useDragNDropColumns<T>({
     if (active.id !== over?.id) {
       const oldIndex = editableColumns.findIndex(({id}) => id === active.id);
       const newIndex = editableColumns.findIndex(({id}) => id === over?.id);
-
-      setEditableColumns(oldEditableColumns => {
-        const newEditableColumns = arrayMove(oldEditableColumns, oldIndex, newIndex);
-
-        setColumns(extractColumns(newEditableColumns), 'reorder');
-
-        return newEditableColumns;
-      });
+      setColumns(arrayMove(columns, oldIndex, newIndex), 'reorder');
     }
   }
 
