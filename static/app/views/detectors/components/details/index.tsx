2b799cb7b4188447719926c4c46c558bef2dab10
Malachi Willey <malwilley@gmail.com>
2025-07-16 14:25:00 -0700

    feat(aci): Add error detector details page

--- static/app/views/detectors/components/details/index.tsx
+++ static/app/views/detectors/components/details/index.tsx
@@ -1,6 +1,7 @@
 import type {Project} from 'sentry/types/project';
 import type {Detector} from 'sentry/types/workflowEngine/detectors';
 import {unreachable} from 'sentry/utils/unreachable';
+import {ErrorDetectorDetails} from 'sentry/views/detectors/components/details/error';
 import {FallbackDetectorDetails} from 'sentry/views/detectors/components/details/fallback';
 import {MetricDetectorDetails} from 'sentry/views/detectors/components/details/metric';
 import {UptimeDetectorDetails} from 'sentry/views/detectors/components/details/uptime';
@@ -19,7 +20,7 @@ export function DetectorDetailsContent({detector, project}: DetectorDetailsConte
       return <UptimeDetectorDetails detector={detector} project={project} />;
     case 'uptime_subscription':
     case 'error':
-      return <FallbackDetectorDetails detector={detector} project={project} />;
+      return <ErrorDetectorDetails detector={detector} project={project} />;
     default:
       unreachable(detectorType);
       return <FallbackDetectorDetails detector={detector} project={project} />;
