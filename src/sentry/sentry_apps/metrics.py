5174e58bf30728b89a0adaf7a08cc0ca3eef843a
Christinarlong <christina.long@sentry.io>
2025-07-16 10:48:07 -0700

    ignore-restricted-ip-error

--- src/sentry/sentry_apps/metrics.py
+++ src/sentry/sentry_apps/metrics.py
@@ -68,6 +68,7 @@ class SentryAppWebhookHaltReason(StrEnum):
     GOT_CLIENT_ERROR = "got_client_error"
     INTEGRATOR_ERROR = "integrator_error"
     MISSING_INSTALLATION = "missing_installation"
+    RESTRICED_IP = "restricted_ip"
 
 
 class SentryAppExternalRequestFailureReason(StrEnum):
