b2dd12a511282b8db0a6f9663797ed57aea08e52
Cathy Teng <cathy.teng@sentry.io>
2025-07-16 12:15:27 -0700

    keep subclassing and move index to meta

--- src/sentry/workflow_engine/models/workflow_fire_history.py
+++ src/sentry/workflow_engine/models/workflow_fire_history.py
@@ -2,12 +2,13 @@ from django.db import models
 from django.db.models import Index
 
 from sentry.backup.scopes import RelocationScope
-from sentry.db.models import CharField, FlexibleForeignKey, Model, region_silo_model, sane_repr
+from sentry.db.models import CharField, FlexibleForeignKey, region_silo_model, sane_repr
+from sentry.db.models.base import DefaultFieldsModel
 from sentry.db.models.fields.uuid import UUIDField
 
 
 @region_silo_model
-class WorkflowFireHistory(Model):
+class WorkflowFireHistory(DefaultFieldsModel):
     __relocation_scope__ = RelocationScope.Excluded
 
     detector = FlexibleForeignKey("workflow_engine.Detector", null=True, on_delete=models.SET_NULL)
@@ -16,12 +17,10 @@ class WorkflowFireHistory(Model):
     event_id = CharField(max_length=32)
     notification_uuid = UUIDField(auto_add=True, unique=True)
     is_single_written = models.BooleanField(db_default=False, db_index=True)
-    date_updated = models.DateTimeField(auto_now=True)
-    date_added = models.DateTimeField(auto_now_add=True, db_index=True)
 
     class Meta:
         db_table = "workflow_engine_workflowfirehistory"
         app_label = "workflow_engine"
-        indexes = [Index(fields=["workflow", "date_added"])]
+        indexes = [Index(fields=["date_added"]), Index(fields=["workflow", "date_added"])]
 
     __repr__ = sane_repr("workflow_id", "group_id", "event_id", "date_added")
