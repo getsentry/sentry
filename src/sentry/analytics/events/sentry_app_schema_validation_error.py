212a0632ab8e2080f657bc4731928098465ab9b8
Fabian Schindler <fabian.schindler@sentry.io>
2025-07-17 12:11:05 +0200

    fix(analytics): shim for SentryAppSchemaValidationError

--- src/sentry/analytics/events/sentry_app_schema_validation_error.py
+++ src/sentry/analytics/events/sentry_app_schema_validation_error.py
@@ -1,14 +1,24 @@
+from typing import Any
+
 from sentry import analytics
 
 
 @analytics.eventclass("sentry_app.schema_validation_error")
 class SentryAppSchemaValidationError(analytics.Event):
-    schema: str
+    app_schema: str
     user_id: str
     sentry_app_id: str | None = None
     sentry_app_name: str
     organization_id: str
     error_message: str
+
+    def serialize(self) -> dict[str, Any]:
+        # with pydantic dataclasses we are prohibited to have a field named "schema"
+        # so we need to rename it to "app_schema" and replace it while serializing
+        # TODO: when moving from pydantic we can remove this shim
+        serialized = super().serialize()
+        serialized["data"]["schema"] = serialized["data"].pop("app_schema")
+        return serialized
 
 
 analytics.register(SentryAppSchemaValidationError)
