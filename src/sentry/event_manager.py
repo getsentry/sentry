111492b920a076fcefae4eb38f24f38583de61f7
Charlie Luo <cvxluo@gmail.com>
2025-07-17 13:03:21 -0700

    more detailed metric

--- src/sentry/event_manager.py
+++ src/sentry/event_manager.py
@@ -718,7 +718,14 @@ def _set_project_platform_if_needed(project: Project, event: Event) -> None:
                 event=audit_log.get_event_id("PROJECT_EDIT"),
                 data={**project.get_audit_log_data(), "platform": event.platform},
             )
-            metrics.incr("issues.infer_project_platform.success", sample_rate=1.0)
+            metrics.incr(
+                "issues.infer_project_platform.success",
+                sample_rate=1.0,
+                tags={
+                    "reason": "new_project" if not project.first_event else "backfill",
+                    "platform": event.platform,
+                },
+            )
 
     except Exception:
         logger.exception("Failed to infer and set project platform")
