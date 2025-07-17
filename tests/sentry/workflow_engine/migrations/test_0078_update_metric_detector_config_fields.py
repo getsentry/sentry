6e4ae46ffd84246a6d654b9373b162f0d83e0f26
Michelle Fu <michelle.fu@sentry.io>
2025-07-17 10:55:03 -0700

    tests

---
+++ tests/sentry/workflow_engine/migrations/test_0078_update_metric_detector_config_fields.py
@@ -0,0 +1,34 @@
+from sentry.testutils.cases import TestMigrations
+
+
+class UpdateMetricDetectorConfigFieldsTest(TestMigrations):
+    migrate_from = "0077_add_wfh_single_write_col"
+    migrate_to = "0078_update_metric_detector_config_fields"
+    app = "workflow_engine"
+
+    def setup_initial_state(self):
+        self.detector = self.create_detector(
+            type="metric_issue",
+            config={
+                "threshold_period": 15,
+                "sensitivity": None,
+                "seasonality": None,
+                "detection_type": "percent",
+                "comparison_delta": 3600,
+            },
+        )
+        self.other_detector = self.create_detector(
+            type="uptime_domain_failure", config={"mode": 1, "environment": "development"}
+        )
+        return super().setup_initial_state()
+
+    def test_simple(self):
+        self.detector.refresh_from_db()
+        assert self.detector.config == {
+            "detection_type": "percent",
+            "comparison_delta": 3600,
+        }
+
+    def test_unaffected(self):
+        self.other_detector.refresh_from_db()
+        assert self.other_detector.config == {"mode": 1, "environment": "development"}
