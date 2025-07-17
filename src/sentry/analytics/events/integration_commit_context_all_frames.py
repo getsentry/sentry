ebc0c32310b545a0d1283f578cfcd804dcb1ccac
Fabian Schindler <fabian.schindler@sentry.io>
2025-07-17 12:00:28 +0200

    fix(analytics): typing issue

--- src/sentry/analytics/events/integration_commit_context_all_frames.py
+++ src/sentry/analytics/events/integration_commit_context_all_frames.py
@@ -3,10 +3,10 @@ from sentry import analytics
 
 @analytics.eventclass("integrations.failed_to_fetch_commit_context_all_frames")
 class IntegrationsFailedToFetchCommitContextAllFrames(analytics.Event):
-    organization_id: str
-    project_id: str
-    group_id: str
-    event_id: str
+    organization_id: int
+    project_id: int
+    group_id: int
+    event_id: int
     num_frames: int
     num_successfully_mapped_frames: int
     reason: str
@@ -14,17 +14,17 @@ class IntegrationsFailedToFetchCommitContextAllFrames(analytics.Event):
 
 @analytics.eventclass("integrations.successfully_fetched_commit_context_all_frames")
 class IntegrationsSuccessfullyFetchedCommitContextAllFrames(analytics.Event):
-    organization_id: str
-    project_id: str
-    group_id: str
-    event_id: str
+    organization_id: int
+    project_id: int
+    group_id: int
+    event_id: int
     num_frames: int
     num_unique_commits: int
     num_unique_commit_authors: int
     num_successfully_mapped_frames: int
     selected_frame_index: int | None
-    selected_provider: str
-    selected_code_mapping_id: str
+    selected_provider: str | None
+    selected_code_mapping_id: int
 
 
 analytics.register(IntegrationsSuccessfullyFetchedCommitContextAllFrames)
