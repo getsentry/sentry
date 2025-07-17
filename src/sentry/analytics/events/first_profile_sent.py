0b04172be0e2f15b56296c609867060ec58471c0
Fabian Schindler <fabian.schindler@sentry.io>
2025-07-17 14:26:10 +0200

    fix(analytics): typing issues

--- src/sentry/analytics/events/first_profile_sent.py
+++ src/sentry/analytics/events/first_profile_sent.py
@@ -3,10 +3,10 @@ from sentry import analytics
 
 @analytics.eventclass("first_profile.sent")
 class FirstProfileSentEvent(analytics.Event):
-    organization_id: str
-    project_id: str
+    organization_id: int
+    project_id: int
     platform: str | None = None
-    user_id: str | None = None
+    user_id: int | None = None
 
 
 analytics.register(FirstProfileSentEvent)
