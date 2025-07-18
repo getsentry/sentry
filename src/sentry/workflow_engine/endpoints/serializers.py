ce4d8aa58e84e3e7ff0fe37d1000882f54467eb0
Scott Cooper <scttcper@gmail.com>
2025-07-18 13:37:53 -0700

    feat(aci): Add alert rule id to detector (#95912)
    Going from the detector to the alert rule is very helpful in the ui. "Go
    to old ui" Ideally removed once we stop mirroring detectors to alert
    rules.

--- src/sentry/workflow_engine/endpoints/serializers.py
+++ src/sentry/workflow_engine/endpoints/serializers.py
@@ -22,6 +22,7 @@ from sentry.sentry_apps.models.sentry_app_installation import prepare_ui_compone
 from sentry.utils.cursors import Cursor, CursorResult
 from sentry.workflow_engine.models import (
     Action,
+    AlertRuleDetector,
     DataCondition,
     DataConditionGroup,
     DataSource,
@@ -310,6 +311,17 @@ class DetectorSerializer(Serializer):
         for detector_id, workflow_id in detector_workflows:
             workflows_map[detector_id].append(str(workflow_id))
 
+        # Fetch alert rule mappings
+        # TODO: Remove alert rule mappings as they're deprecated
+        alert_rule_mappings = list(AlertRuleDetector.objects.filter(detector__in=item_list))
+        alert_rule_map = {
+            mapping.detector_id: {
+                "alert_rule_id": mapping.alert_rule_id,
+                "rule_id": mapping.rule_id,
+            }
+            for mapping in alert_rule_mappings
+        }
+
         filtered_item_list = [item for item in item_list if item.type == ErrorGroupType.slug]
         project_ids = [item.project_id for item in filtered_item_list]
 
@@ -331,6 +343,13 @@ class DetectorSerializer(Serializer):
                 str(item.workflow_condition_group_id)
             )
             attrs[item]["workflow_ids"] = workflows_map[item.id]
+            attrs[item]["alert_rule_mapping"] = alert_rule_map.get(
+                item.id,
+                {
+                    "alert_rule_id": None,
+                    "rule_id": None,
+                },
+            )
             if item.id in configs:
                 attrs[item]["config"] = configs[item.id]
             else:
@@ -342,6 +361,7 @@ class DetectorSerializer(Serializer):
         return attrs
 
     def serialize(self, obj: Detector, attrs: Mapping[str, Any], user, **kwargs) -> dict[str, Any]:
+        alert_rule_mapping = attrs.get("alert_rule_mapping", {})
         return {
             "id": str(obj.id),
             "projectId": str(obj.project_id),
@@ -356,6 +376,8 @@ class DetectorSerializer(Serializer):
             "conditionGroup": attrs.get("condition_group"),
             "config": convert_dict_key_case(attrs.get("config"), snake_to_camel_case),
             "enabled": obj.enabled,
+            "alertRuleId": alert_rule_mapping.get("alert_rule_id"),
+            "ruleId": alert_rule_mapping.get("rule_id"),
         }
 
 
