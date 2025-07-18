c0da52d020ec9b5258f0127048b6ffc0699d8965
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2025-07-18 16:57:29 +0200

    fix how the organization is fetched

--- src/sentry/profiles/flamegraph.py
+++ src/sentry/profiles/flamegraph.py
@@ -21,7 +21,6 @@ from snuba_sdk import (
 )
 
 from sentry import features, options
-from sentry.models.organization import Organization
 from sentry.search.eap.types import EAPResponse, SearchResolverConfig
 from sentry.search.events.builder.discover import DiscoverQueryBuilder
 from sentry.search.events.builder.profile_functions import ProfileFunctionsQueryBuilder
@@ -103,18 +102,16 @@ class FlamegraphExecutor:
         if self.data_source == "functions":
             return self.get_profile_candidates_from_functions()
         elif self.data_source == "transactions":
-            organization = Organization.objects.get_from_cache(id=self.snuba_params.organization_id)
             if features.has(
                 "organizations:profiling-flamegraph-use-increased-chunks-query-strategy",
-                organization,
+                self.snuba_params.organization,
             ):
                 return self.get_profile_candidates_from_transactions_v2()
             return self.get_profile_candidates_from_transactions()
         elif self.data_source == "profiles":
-            organization = Organization.objects.get_from_cache(id=self.snuba_params.organization_id)
             if features.has(
                 "organizations:profiling-flamegraph-use-increased-chunks-query-strategy",
-                organization,
+                self.snuba_params.organization,
             ):
                 return self.get_profile_candidates_from_profiles_v2()
             return self.get_profile_candidates_from_profiles()
