6d2c53b61e58fa37908b8e8bfdc35c756c366905
Francesco Vigliaturo <francesco.vigliaturo@sentry.io>
2025-07-18 16:47:11 +0200

    fix feature type

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -89,7 +89,7 @@ def register_temporary_features(manager: FeatureManager):
     # Enable transaction profiles processing with vroomrs
     manager.add("projects:transaction-profiling-vroomrs-processing", ProjectFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable querying profile candidates with exponentially growing datetime range chunks
-    manager.add("organizations:profiling-flamegraph-use-increased-chunks-query-strategy", ProjectFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
+    manager.add("organizations:profiling-flamegraph-use-increased-chunks-query-strategy", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable daily summary
     manager.add("organizations:daily-summary", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enables import/export functionality for dashboards
