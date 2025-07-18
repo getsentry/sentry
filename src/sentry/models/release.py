bc49a88324b2cb509f6b57ab95780909be517391
Colton Allen <colton.allen@sentry.io>
2025-07-18 15:46:33 -0500

    Explicit re-exports

--- src/sentry/models/release.py
+++ src/sentry/models/release.py
@@ -1,1 +1,17 @@
-from sentry.releases.models.release import *  # noqa: F401,F403
+from sentry.releases.models.release import (
+    Release,
+    ReleaseStatus,
+    _get_cache_key,
+    follows_semver_versioning_scheme,
+    get_artifact_counts,
+    get_previous_release,
+)
+
+__all__ = (
+    "Release",
+    "ReleaseStatus",
+    "follows_semver_versioning_scheme",
+    "_get_cache_key",
+    "get_previous_release",
+    "get_artifact_counts",
+)
--- src/sentry/models/releasefile.py
+++ src/sentry/models/releasefile.py
@@ -1,1 +1,23 @@
-from sentry.releases.models.releasefile import *  # noqa: F401,F403
+from sentry.releases.models.releasefile import (
+    ARTIFACT_INDEX_FILENAME,
+    ARTIFACT_INDEX_TYPE,
+    ReleaseArchive,
+    ReleaseFile,
+    _ArtifactIndexData,
+    _ArtifactIndexGuard,
+    delete_from_artifact_index,
+    read_artifact_index,
+    update_artifact_index,
+)
+
+__all__ = (
+    "ReleaseFile",
+    "read_artifact_index",
+    "_ArtifactIndexData",
+    "_ArtifactIndexGuard",
+    "update_artifact_index",
+    "ARTIFACT_INDEX_FILENAME",
+    "ARTIFACT_INDEX_TYPE",
+    "delete_from_artifact_index",
+    "ReleaseArchive",
+)
