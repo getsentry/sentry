49ecc83dc008fa8c882925e290ee3178b116c52a
nikkikapadia <nikki.kapadia@sentry.io>
2025-07-21 12:06:01 -0400

    wip keeping url the same

--- static/app/views/dashboards/widgetBuilder/hooks/useSegmentSpanWidgetState.tsx
+++ static/app/views/dashboards/widgetBuilder/hooks/useSegmentSpanWidgetState.tsx
@@ -3,17 +3,21 @@ import {useCallback} from 'react';
 import {WidgetType} from 'sentry/views/dashboards/types';
 import {useWidgetBuilderContext} from 'sentry/views/dashboards/widgetBuilder/contexts/widgetBuilderContext';
 import {BuilderStateAction} from 'sentry/views/dashboards/widgetBuilder/hooks/useWidgetBuilderState';
+import {convertBuilderStateToWidget} from 'sentry/views/dashboards/widgetBuilder/utils/convertBuilderStateToWidget';
+import {convertWidgetToBuilderStateParams} from 'sentry/views/dashboards/widgetBuilder/utils/convertWidgetToBuilderStateParams';
 
 export function useSegmentSpanWidgetState() {
-  const {dispatch} = useWidgetBuilderContext();
+  const {dispatch, state} = useWidgetBuilderContext();
 
   const setSegmentSpanBuilderState = useCallback(() => {
     const nextDataset = WidgetType.SPANS;
+    const widget = convertBuilderStateToWidget(state);
+    const stateParams = convertWidgetToBuilderStateParams(widget);
     dispatch({
       type: BuilderStateAction.SET_STATE,
-      payload: {dataset: nextDataset, query: ['is_transaction:true']},
+      payload: {...stateParams, dataset: nextDataset, query: ['is_transaction:true']},
     });
-  }, [dispatch]);
+  }, [dispatch, state]);
 
   return {
     setSegmentSpanBuilderState,
