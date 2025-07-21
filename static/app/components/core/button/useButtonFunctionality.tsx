41caa0c6fa75b1d114b30458116d1dbf1cfa2794
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2025-07-21 11:07:27 +0200

    ref: decouple button from HookStore (#93940)
    This PR decouples `core/button` from the sentry specific `HookStore` by
    using another layer of indirection via React Context. The goal here is
    to make sure `core` components are independent.
    
    Ideally, I would like to remove the `HookStore` value
    `react-hook:use-button-tracking` because, as far as I can see, we always
    do the following now:
    
    ```
    HookStore --> React Context --> Button
    ```
    
    - for self-hosted, we never register the hooks, so the `HookStore` will
    return `undefined`, puts that in context, the button reads it and falls
    back to the default tracking impl.
    - for `getSentry`, we register the hooks, read those, put that into
    context and use those.
    
    So ideally, it would just be:
    
    ```
    React Context --> Button
    ```
    
    This would not only get rid of the extra layer of indirection, it would
    also allow us to just put a function `(props) => void` into context
    rather than the extra indirection `(props) => () => void`, _and_ we
    wouldn’t need to pass a _real_ react hook around. Passing hooks as
    values is discouraged as the react compiler won’t be able to optimize
    those (it’s also against the rules of react, strictly speaking).
    
    The problem is that registering hooks happens very early, outside of a
    react context, and then we just render the app.
    
    W would probably need to refactor this towards:
    
    ```
    if (config.sentryMode === 'SELF_HOSTED') {
      initializeMain(config)
    }
    
    ...
    
    initializeMain(config, Wrapper)
    ```
    
    where `Wrapper` would render the correct context providers around the
    app that gets rendered in `renderMain`.
    
    Unless I’m missing something, this means we could get rid of the
    `HookStore` altogether, at least for things we don’t need outside of
    react? Please tell me what I’m missing 😅
    
    @evanpurkhiser @JonasBa FYI

--- static/app/components/core/button/useButtonFunctionality.tsx
+++ static/app/components/core/button/useButtonFunctionality.tsx
@@ -1,5 +1,4 @@
-// eslint-disable-next-line boundaries/element-types
-import HookStore from 'sentry/stores/hookStore';
+import {useButtonTracking} from 'sentry/components/core/trackingContext';
 
 import type {
   DO_NOT_USE_ButtonProps as ButtonProps,
@@ -14,39 +13,7 @@ export function useButtonFunctionality(props: ButtonProps | LinkButtonProps) {
     props['aria-label'] ??
     (typeof props.children === 'string' ? props.children : undefined);
 
-  const useButtonTrackingLogger = () => {
-    const hasAnalyticsDebug = window.localStorage?.getItem('DEBUG_ANALYTICS') === '1';
-    const hasCustomAnalytics =
-      props.analyticsEventName || props.analyticsEventKey || props.analyticsParams;
-    if (!hasCustomAnalytics || !hasAnalyticsDebug) {
-      return () => {};
-    }
-
-    return () => {
-      // eslint-disable-next-line no-console
-      console.log('buttonAnalyticsEvent', {
-        eventKey: props.analyticsEventKey,
-        eventName: props.analyticsEventName,
-        priority: props.priority,
-        href: 'href' in props ? props.href : undefined,
-        ...props.analyticsParams,
-      });
-    };
-  };
-
-  const useButtonTracking =
-    HookStore.get('react-hook:use-button-tracking')[0] ?? useButtonTrackingLogger;
-
-  const buttonTracking = useButtonTracking({
-    analyticsEventName: props.analyticsEventName,
-    analyticsEventKey: props.analyticsEventKey,
-    analyticsParams: {
-      priority: props.priority,
-      href: 'href' in props ? props.href : undefined,
-      ...props.analyticsParams,
-    },
-    'aria-label': accessibleLabel || '',
-  });
+  const buttonTracking = useButtonTracking();
 
   const handleClick = (e: React.MouseEvent<HTMLButtonElement | HTMLAnchorElement>) => {
     // Don't allow clicks when disabled or busy
@@ -56,7 +23,16 @@ export function useButtonFunctionality(props: ButtonProps | LinkButtonProps) {
       return;
     }
 
-    buttonTracking();
+    buttonTracking({
+      analyticsEventName: props.analyticsEventName,
+      analyticsEventKey: props.analyticsEventKey,
+      analyticsParams: {
+        priority: props.priority,
+        href: 'href' in props ? props.href : undefined,
+        ...props.analyticsParams,
+      },
+      'aria-label': accessibleLabel || '',
+    });
     // @ts-expect-error at this point, we don't know if the button is a button or a link
     props.onClick?.(e);
   };
