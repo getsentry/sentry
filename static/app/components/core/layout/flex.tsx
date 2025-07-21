5476257fef852971e4f079c2af8d7e6762451979
TkDodo <dominik.dorfmeister@sentry.io>
2025-07-21 10:44:52 +0200

    ref: allow space values to be passed to flex-gap
    for backwards compatibility

--- static/app/components/core/layout/flex.tsx
+++ static/app/components/core/layout/flex.tsx
@@ -1,13 +1,15 @@
 import type {CSSProperties} from 'react';
+import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
+import type {space} from 'sentry/styles/space';
 import type {Space} from 'sentry/utils/theme/theme';
 
 interface FlexProps {
   align?: CSSProperties['alignItems'];
   direction?: CSSProperties['flexDirection'];
   flex?: CSSProperties['flex'];
-  gap?: Space;
+  gap?: Space | ReturnType<typeof space>;
   /**
    * Determines whether the flex container should be displayed as an inline-flex.
    */
@@ -24,7 +26,11 @@ export const Flex = styled('div', {
   flex-direction: ${p => p.direction};
   justify-content: ${p => p.justify};
   align-items: ${p => p.align};
-  gap: ${p => p.gap && p.theme.space[p.gap]};
+  ${p =>
+    p.gap &&
+    css`
+      gap: ${p.gap in p.theme.space ? p.theme.space[p.gap as Space] : p.gap};
+    `};
   flex-wrap: ${p => p.wrap};
   flex: ${p => p.flex};
 `;
