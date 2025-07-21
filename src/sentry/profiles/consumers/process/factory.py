e91f324b856a6b7b672189e4143aac853a04e403
Enoch Tang <enoch.tang@sentry.io>
2025-07-21 12:09:24 -0400

    chore(taskworker): Remove unused compression param in process_profile (#95980)
    This parameter is no longer being used to determine whether the process
    task payload should be zlib decompressed. The taskworker platform
    handles compression now.

--- src/sentry/profiles/consumers/process/factory.py
+++ src/sentry/profiles/consumers/process/factory.py
@@ -17,7 +17,7 @@ def process_message(message: Message[KafkaPayload]) -> None:
 
     if sampled or options.get("profiling.profile_metrics.unsampled_profiles.enabled"):
         b64encoded = b64encode(message.payload.value).decode("utf-8")
-        process_profile_task.delay(payload=b64encoded, sampled=sampled, compressed_profile=False)
+        process_profile_task.delay(payload=b64encoded, sampled=sampled)
 
 
 class ProcessProfileStrategyFactory(ProcessingStrategyFactory[KafkaPayload]):
