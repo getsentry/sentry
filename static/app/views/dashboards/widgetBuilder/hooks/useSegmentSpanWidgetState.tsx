156a93b68f1ea4650270bf4167a77bc170d7ce11
nikkikapadia <nikki.kapadia@sentry.io>
2025-07-21 16:59:35 -0400

    make new conversion function

--- static/app/views/dashboards/widgetBuilder/hooks/useSegmentSpanWidgetState.tsx
+++ static/app/views/dashboards/widgetBuilder/hooks/useSegmentSpanWidgetState.tsx
@@ -3,16 +3,14 @@ import {useCallback} from 'react';
 import {WidgetType} from 'sentry/views/dashboards/types';
 import {useWidgetBuilderContext} from 'sentry/views/dashboards/widgetBuilder/contexts/widgetBuilderContext';
 import {BuilderStateAction} from 'sentry/views/dashboards/widgetBuilder/hooks/useWidgetBuilderState';
-import {convertBuilderStateToWidget} from 'sentry/views/dashboards/widgetBuilder/utils/convertBuilderStateToWidget';
-import {convertWidgetToBuilderStateParams} from 'sentry/views/dashboards/widgetBuilder/utils/convertWidgetToBuilderStateParams';
+import {convertBuilderStateToStateQueryParams} from 'sentry/views/dashboards/widgetBuilder/utils/convertBuilderStateToStateQueryParams';
 
 export function useSegmentSpanWidgetState() {
   const {dispatch, state} = useWidgetBuilderContext();
 
   const setSegmentSpanBuilderState = useCallback(() => {
     const nextDataset = WidgetType.SPANS;
-    const widget = convertBuilderStateToWidget(state);
-    const stateParams = convertWidgetToBuilderStateParams(widget);
+    const stateParams = convertBuilderStateToStateQueryParams(state);
     dispatch({
       type: BuilderStateAction.SET_STATE,
       payload: {
