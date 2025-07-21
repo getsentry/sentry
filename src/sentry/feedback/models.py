66c60e4862bd8d31ae3ca9486fc545945f6e3ff1
Vishnu Satish <vishnu.satish@sentry.io>
2025-07-21 07:58:33 -0700

    update based on review

--- src/sentry/feedback/models.py
+++ src/sentry/feedback/models.py
@@ -40,23 +40,16 @@ class GroupFeedbackLabel(DefaultFieldsModel):
     group = FlexibleForeignKey("sentry.Group")
     feedback_label = FlexibleForeignKey("feedback.FeedbackLabel")
 
-    # Denormalized fields from group, for performance
     project = FlexibleForeignKey("sentry.Project")
     first_seen = models.DateTimeField(default=timezone.now)
-    # TODO: if we want to filter by environment(s), we should add a field for that here
 
     class Meta:
         app_label = "feedback"
         db_table = "feedback_groupfeedbacklabel"
         unique_together = (("group", "feedback_label"),)
         indexes = [
-            # rm: Allows us to quickly find all labels that a certain feedback has
-            # Q: do we need feedback_label here? or is group enough
-            models.Index(fields=("group", "feedback_label")),
-            # rm: Allows us to quickly find all feedbacks in a given project and date range that have certain label(s)
-            # Also allows us to find all feedbacks and associated labels in a given project and date range
-            # Does this support the query: get top 10 labels by number of groups (counting groups only in a date range and project)? Or do we need another field in this index, like "group"
-            models.Index(fields=("project", "first_seen", "feedback_label")),
+            models.Index(fields=("project", "first_seen")),
+            models.Index(fields=("project", "feedback_label", "first_seen")),
         ]
 
     __repr__ = sane_repr("group_id", "feedback_label_id")
