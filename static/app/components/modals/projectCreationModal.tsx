a3189d781b0e07ba4e24b717def4a2f4c31bdfff
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-07-22 10:43:12 +0200

    feat: Keep tab category by unselecting platform (#96058)
    follow-up of
    https://github.com/getsentry/sentry/pull/95856#discussion_r2221356540.
    It seems that my commit reverting the last feedback was not pushed.

--- static/app/components/modals/projectCreationModal.tsx
+++ static/app/components/modals/projectCreationModal.tsx
@@ -42,6 +42,7 @@ export default function ProjectCreationModal({
   closeModal,
   defaultCategory,
 }: Props) {
+  const [category, setCategory] = useState<Category | undefined>(defaultCategory);
   const [platform, setPlatform] = useState<OnboardingSelectedSDK | undefined>(undefined);
   const [step, setStep] = useState(0);
   const [projectName, setProjectName] = useState('');
@@ -61,6 +62,7 @@ export default function ProjectCreationModal({
       ...omit(selectedPlatform, 'id'),
       key: selectedPlatform.id,
     });
+    setCategory(selectedPlatform.category);
   }
 
   const createProject = useCallback(async () => {
@@ -136,7 +138,7 @@ export default function ProjectCreationModal({
         <Fragment>
           <Subtitle>{t('Choose a Platform')}</Subtitle>
           <PlatformPicker
-            defaultCategory={platform?.category ?? defaultCategory}
+            defaultCategory={category}
             setPlatform={handlePlatformChange}
             organization={organization}
             platform={platform?.key}
