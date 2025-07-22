205b829f3f15af6ee3822b1fe832d4515120ec76
Katie Byers <lobsterkatie@gmail.com>
2025-07-22 15:51:17 -0700

    remove `php_detect_anonymous_classes` switch

--- src/sentry/grouping/strategies/configurations.py
+++ src/sentry/grouping/strategies/configurations.py
@@ -42,8 +42,6 @@ BASE_STRATEGY = create_strategy_configuration_class(
         # Platforms for which context line should be taken into
         # account when grouping.
         "contextline_platforms": ("javascript", "node", "python", "php", "ruby"),
-        # This detects anonymous classes in PHP code.
-        "php_detect_anonymous_classes": True,
         # Turns on falling back to exception values when there
         # is no stacktrace.
         "with_exception_value_fallback": True,
