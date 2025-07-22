cc8eaa24ae716c7648e6e1ac0df38eca90339460
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-07-22 16:07:43 +0200

    make user id optional

--- src/sentry/analytics/events/weekly_report.py
+++ src/sentry/analytics/events/weekly_report.py
@@ -4,7 +4,7 @@ from sentry import analytics
 @analytics.eventclass("weekly_report.sent")
 class WeeklyReportSent(analytics.Event):
     organization_id: int
-    user_id: int
+    user_id: int | None = None
     notification_uuid: str
     user_project_count: int
 
