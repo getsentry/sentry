77ca933d62b006a6b93ea6046512b90d030a30b0
Snigdha Sharma <snigdha.sharma@sentry.io>
2025-07-23 11:39:36 -0700

    Fix tests

--- tests/sentry/rules/conditions/test_existing_high_priority_issue.py
+++ tests/sentry/rules/conditions/test_existing_high_priority_issue.py
@@ -18,11 +18,11 @@ class ExistingHighPriorityIssueConditionTest(RuleTestCase):
 
         # This will never pass for non-new or non-escalating issuesalways pass
         self.event.group.update(priority=PriorityLevel.HIGH)
-        self.assertPasses(rule, is_new=False, has_reappeared=True, has_escalated=False)
+        self.assertPasses(rule, is_new=False, has_reappeared=True, has_escalated=True)
         self.assertPasses(rule, is_new=False, has_reappeared=False, has_escalated=True)
 
         # This will never pass
-        self.assertDoesNotPass(rule, is_new=True, has_reappeared=False, has_escalated=True)
+        self.assertDoesNotPass(rule, is_new=True, has_reappeared=False, has_escalated=False)
         self.assertDoesNotPass(rule, is_new=True, has_reappeared=True, has_escalated=False)
 
         self.event.group.update(priority=PriorityLevel.LOW)
