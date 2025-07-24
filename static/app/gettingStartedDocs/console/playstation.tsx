8b1ffdf44374b6f70762c88f8fbae79496975a91
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-07-24 10:04:01 +0200

    feat: Add generic modal for requesting access to private gaming repository

--- static/app/gettingStartedDocs/console/playstation.tsx
+++ static/app/gettingStartedDocs/console/playstation.tsx
@@ -1,3 +1,4 @@
+import {openPrivateGamingSdkAccessModal} from 'sentry/actionCreators/modal';
 import {Button} from 'sentry/components/core/button';
 import {ExternalLink} from 'sentry/components/core/link';
 import List from 'sentry/components/list';
@@ -11,7 +12,7 @@ import {IconLock} from 'sentry/icons/iconLock';
 import {t, tct} from 'sentry/locale';
 
 const onboarding: OnboardingConfig = {
-  install: () => [
+  install: params => [
     {
       type: StepType.INSTALL,
       content: [
@@ -42,7 +43,18 @@ const onboarding: OnboardingConfig = {
           ),
           showIcon: true,
           trailingItems: (
-            <Button size="sm" priority="primary">
+            <Button
+              size="sm"
+              priority="primary"
+              onClick={() => {
+                openPrivateGamingSdkAccessModal({
+                  organization: params.organization,
+                  projectSlug: params.projectSlug,
+                  sdkName: 'PlayStation',
+                  gamingPlatform: 'playstation',
+                });
+              }}
+            >
               {t('Request Access')}
             </Button>
           ),
