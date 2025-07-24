6734d2d609106aa89ecedc3bf53fa67a9065980e
Vishnu Satish <vishnu.satish@sentry.io>
2025-07-24 15:31:27 -0700

    getting started

---
+++ src/sentry/feedback/usecases/label_generation.py
@@ -0,0 +1,42 @@
+from typing import TypedDict
+
+import requests
+from django.conf import settings
+
+from sentry.seer.signed_seer_api import sign_with_seer_secret
+from sentry.utils import json, metrics
+
+
+class LabelRequest(TypedDict):
+    """Corresponds to GenerateFeedbackLabelsRequest in Seer."""
+
+    organization_id: int
+    feedback_message: str
+
+
+@metrics.wraps("feedback.generate_labels", sample_rate=1.0)
+def generate_labels(feedback_message: str, organization_id: int) -> list[str]:
+    request = LabelRequest(
+        organization_id=organization_id,
+        feedback_message=feedback_message,
+    )
+
+    serialized_request = json.dumps(request)
+
+    response = requests.post(
+        f"{settings.SEER_AUTOFIX_URL}/v1/automation/summarize/feedback/labels",
+        data=serialized_request,
+        headers={
+            "content-type": "application/json;charset=utf-8",
+            **sign_with_seer_secret(serialized_request.encode()),
+        },
+    )
+
+    if response.status_code != 200:
+        # XXX: Should we be raising something else here? Or logging it somewhere?
+        raise Exception(f"Failed to generate labels: {response.status_code} {response.text}")
+
+    # This could throw if not valid JSON and if the structure isn't what we expect
+    labels = json.loads(response.content.decode("utf-8"))["data"]["labels"]
+
+    return labels
---
+++ tests/sentry/feedback/usecases/test_label_generation.py
@@ -0,0 +1,1 @@
+# def test_generate_labels():
