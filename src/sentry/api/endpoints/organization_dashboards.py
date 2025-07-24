180830529a9694b94a2ea3323c2885eb707fe551
Nar Saynorath <nar.saynorath@sentry.io>
2025-07-24 12:12:26 -0400

    feat(dashboards): Add starring behavior on POST (#96315)
    Dashboards that are created at the moment do not auto-star themselves.
    This adds a query param `isFavorited` to tell the backend to create and
    insert the dashboard as a favorite. The frontend will pass this along in
    the required requests to favorite a dashboard.

--- src/sentry/api/endpoints/organization_dashboards.py
+++ src/sentry/api/endpoints/organization_dashboards.py
@@ -1,8 +1,10 @@
 from __future__ import annotations
 
+import sentry_sdk
 from django.db import IntegrityError, router, transaction
 from django.db.models import Case, Exists, F, IntegerField, OrderBy, OuterRef, Subquery, Value, When
 from drf_spectacular.utils import extend_schema
+from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -97,7 +99,7 @@ class OrganizationDashboardsEndpoint(OrganizationEndpoint):
         Retrieve a list of custom dashboards that are associated with the given organization.
         """
         if not request.user.is_authenticated:
-            return Response(status=400)
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
 
         if not features.has("organizations:dashboards-basic", organization, actor=request.user):
             return Response(status=404)
@@ -286,6 +288,9 @@ class OrganizationDashboardsEndpoint(OrganizationEndpoint):
         """
         Create a new dashboard for the given Organization
         """
+        if not request.user.is_authenticated:
+            return Response(status=status.HTTP_401_UNAUTHORIZED)
+
         if not features.has("organizations:dashboards-edit", organization, actor=request.user):
             return Response(status=404)
 
@@ -305,6 +310,22 @@ class OrganizationDashboardsEndpoint(OrganizationEndpoint):
         try:
             with transaction.atomic(router.db_for_write(Dashboard)):
                 dashboard = serializer.save()
+
+                if features.has(
+                    "organizations:dashboards-starred-reordering",
+                    organization,
+                    actor=request.user,
+                ):
+                    if serializer.validated_data.get("is_favorited"):
+                        try:
+                            DashboardFavoriteUser.objects.insert_favorite_dashboard(
+                                organization=organization,
+                                user_id=request.user.id,
+                                dashboard=dashboard,
+                            )
+                        except Exception as e:
+                            sentry_sdk.capture_exception(e)
+
             return Response(serialize(dashboard, request.user), status=201)
         except IntegrityError:
             duplicate = request.data.get("duplicate", False)
