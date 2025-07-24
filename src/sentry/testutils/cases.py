67e7c9ecac39beafbc05a579d673431285ba2d72
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2025-07-24 06:57:22 -0400

    ref: upgrade mypy to 1.17 (#96293)
    other PRs split out from this stack:
    - https://github.com/getsentry/sentry/pull/92766
    - https://github.com/getsentry/sentry/pull/92767
    - https://github.com/getsentry/sentry/pull/92768
    - https://github.com/getsentry/sentry/pull/92783
    - https://github.com/getsentry/sentry/pull/92841
    - https://github.com/getsentry/sentry/pull/92850
    - https://github.com/getsentry/sentry/pull/92936
    - https://github.com/getsentry/sentry/pull/92970
    - https://github.com/getsentry/sentry/pull/93107
    - https://github.com/getsentry/sentry/pull/93183
    - https://github.com/getsentry/sentry/pull/93185
    - https://github.com/getsentry/sentry/pull/93268
    - https://github.com/getsentry/sentry/pull/93276
    - https://github.com/getsentry/sentry/pull/93366
    - https://github.com/getsentry/sentry/pull/93433
    - https://github.com/getsentry/sentry/pull/93477
    - https://github.com/getsentry/sentry/pull/93722
    - https://github.com/getsentry/sentry/pull/94045
    - https://github.com/getsentry/sentry/pull/94049
    - https://github.com/getsentry/getsentry/pull/17783
    - https://github.com/getsentry/sentry/pull/94065
    - https://github.com/getsentry/sentry/pull/94084
    - https://github.com/getsentry/sentry/pull/94092
    - https://github.com/getsentry/sentry/pull/94110
    - https://github.com/getsentry/sentry/pull/94132
    - https://github.com/getsentry/sentry/pull/94333
    - https://github.com/getsentry/sentry/pull/95063
    - https://github.com/getsentry/sentry/pull/95128
    - https://github.com/getsentry/sentry/pull/95129
    - https://github.com/getsentry/sentry/pull/95130
    - https://github.com/getsentry/sentry/pull/95132
    - https://github.com/getsentry/sentry/pull/96060
    - https://github.com/getsentry/sentry/pull/96079
    - https://github.com/getsentry/sentry/pull/96080
    - https://github.com/getsentry/sentry/pull/96082
    - https://github.com/getsentry/sentry/pull/96083
    - https://github.com/getsentry/sentry/pull/96084
    - https://github.com/getsentry/sentry/pull/96100
    - https://github.com/getsentry/sentry/pull/96101
    - https://github.com/getsentry/sentry/pull/96102
    - https://github.com/getsentry/sentry/pull/96103
    - https://github.com/getsentry/sentry/pull/96104
    - https://github.com/getsentry/sentry/pull/96106
    - https://github.com/getsentry/sentry/pull/96192
    - https://github.com/getsentry/sentry/pull/96194
    - https://github.com/getsentry/sentry/pull/96196
    - https://github.com/getsentry/sentry/pull/96197
    - https://github.com/getsentry/sentry/pull/96290
    - (via `@untitaker` https://github.com/getsentry/sentry/pull/96146 )
    
    <!-- Describe your PR here. -->

--- src/sentry/testutils/cases.py
+++ src/sentry/testutils/cases.py
@@ -831,8 +831,7 @@ class DRFPermissionTestCase(TestCase):
         Override the return type of make_request b/c DRF permission classes
         expect a DRF request (go figure)
         """
-        drf_request: Request = super().make_request(*arg, **kwargs)  # type: ignore[assignment]
-        return drf_request
+        return super().make_request(*arg, **kwargs)  # type: ignore[return-value]
 
     def setUp(self):
         self.superuser = self.create_user(is_superuser=True, is_staff=False)
