227c0b5d1fa7ff7906f3fd5a5585fb75ac4e6899
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-07-24 08:36:20 +0200

    id types should be int

--- src/sentry/integrations/analytics.py
+++ src/sentry/integrations/analytics.py
@@ -12,71 +12,71 @@ class IntegrationAddedEvent(analytics.Event):
 
 @analytics.eventclass("integration.disabled.notified")
 class IntegrationDisabledNotified(analytics.Event):
-    organization_id: str
+    organization_id: int
     provider: str
     integration_type: str
-    integration_id: str
-    user_id: str | None = None
+    integration_id: int
+    user_id: int | None = None
 
 
 @analytics.eventclass("integration.issue.created")
 class IntegrationIssueCreatedEvent(analytics.Event):
     provider: str
     id: str
-    organization_id: str
-    user_id: str | None = None
-    default_user_id: str
+    organization_id: int
+    user_id: int | None = None
+    default_user_id: int
 
 
 @analytics.eventclass("integration.issue.linked")
 class IntegrationIssueLinkedEvent(analytics.Event):
     provider: str
     id: str
-    organization_id: str
-    user_id: str | None = None
-    default_user_id: str
+    organization_id: int
+    user_id: int | None = None
+    default_user_id: int
 
 
 @analytics.eventclass("integration.issue.status.synced")
 class IntegrationIssueStatusSyncedEvent(analytics.Event):
     provider: str
     id: int
-    organization_id: str
+    organization_id: int
 
 
 @analytics.eventclass("integration.issue.assignee.synced")
 class IntegrationIssueAssigneeSyncedEvent(analytics.Event):
     provider: str
     id: int
-    organization_id: str
+    organization_id: int
 
 
 @analytics.eventclass("integration.issue.comments.synced")
 class IntegrationIssueCommentsSyncedEvent(analytics.Event):
     provider: str
     id: str
-    organization_id: str
+    organization_id: int
 
 
 @analytics.eventclass("integration.repo.added")
 class IntegrationRepoAddedEvent(analytics.Event):
     provider: str
     id: str
-    organization_id: str
+    organization_id: int
 
 
 @analytics.eventclass("integration.resolve.commit")
 class IntegrationResolveCommitEvent(analytics.Event):
     provider: str | None
     id: str
-    organization_id: str
+    organization_id: int
 
 
 @analytics.eventclass("integration.resolve.pr")
 class IntegrationResolvePREvent(analytics.Event):
     provider: str | None
     id: str
-    organization_id: str
+    organization_id: int
 
 
 @analytics.eventclass("integration.stacktrace.linked")
@@ -84,7 +84,7 @@ class IntegrationStacktraceLinkEvent(analytics.Event):
     provider: str
     config_id: str
     project_id: int
-    organization_id: str
+    organization_id: int
     filepath: str
     status: str
     link_fetch_iterations: int
