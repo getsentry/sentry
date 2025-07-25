68907ff8c6523a3d133dd16eba929dceb5b6e1af
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-07-25 15:43:30 +0200

    fix type

--- src/sentry/analytics/events/release_set_commits.py
+++ src/sentry/analytics/events/release_set_commits.py
@@ -4,7 +4,7 @@ from sentry import analytics
 @analytics.eventclass("release.set_commits_local")
 class ReleaseSetCommitsLocalEvent(analytics.Event):
     user_id: int | None = None
-    organization_id: str
+    organization_id: int
     project_ids: list[int] | None
     user_agent: str | None = None
 
