b4e063a929b2fe55eac8cb406200d4d8a92d4a27
Malachi Willey <malwilley@gmail.com>
2025-07-25 09:27:34 -0700

    knip again

--- static/app/types/workflowEngine/detectors.tsx
+++ static/app/types/workflowEngine/detectors.tsx
@@ -189,7 +189,3 @@ export interface MetricDetectorUpdatePayload extends BaseDetectorUpdatePayload {
   dataSource: UpdateSnubaDataSourcePayload;
   type: 'metric_issue';
 }
-
-export interface ErrorDetectorUpdatePayload extends BaseDetectorUpdatePayload {
-  type: 'error';
-}
