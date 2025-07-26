ca698a05f52fdea61825087c2a985f9a98d6f8c9
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2025-07-25 17:18:05 -0700

    Add treemap/categories selector button

--- static/app/views/preprod/main/buildDetailsMainContent.tsx
+++ static/app/views/preprod/main/buildDetailsMainContent.tsx
@@ -1,7 +1,12 @@
+import {useState} from 'react';
 import styled from '@emotion/styled';
 
 import {Alert} from 'sentry/components/core/alert';
+import {SegmentedControl} from 'sentry/components/core/segmentedControl';
+import {Heading} from 'sentry/components/core/text';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {IconGrid} from 'sentry/icons';
+import {IconGraphCircle} from 'sentry/icons/iconGraphCircle';
 import type {UseApiQueryResult} from 'sentry/utils/queryClient';
 import type RequestError from 'sentry/utils/requestError/requestError';
 import {AppSizeTreemap} from 'sentry/views/preprod/main/appSizeTreemap';
@@ -18,6 +23,10 @@ export function BuildDetailsMainContent(props: BuildDetailsMainContentProps) {
     isError: isAppSizeError,
     error: appSizeError,
   } = props.appSizeQuery;
+
+  const [selectedContent, setSelectedContent] = useState<'treemap' | 'categories'>(
+    'treemap'
+  );
 
   if (isAppSizePending) {
     return (
@@ -43,14 +52,39 @@ export function BuildDetailsMainContent(props: BuildDetailsMainContentProps) {
     );
   }
 
+  const content =
+    selectedContent === 'treemap' ? (
+      <AppSizeTreemap treemapData={appSizeData.treemap} />
+    ) : (
+      <Heading as="h2">Categories (Coming soon)</Heading>
+    );
+
   return (
     <MainContentContainer>
-      <AppSizeTreemap treemapData={appSizeData.treemap} />
+      <MainContentControls>
+        <SegmentedControl
+          value={selectedContent}
+          onChange={value => setSelectedContent(value)}
+        >
+          <SegmentedControl.Item key="treemap" icon={<IconGrid />} />
+          <SegmentedControl.Item key="categories" icon={<IconGraphCircle />} />
+        </SegmentedControl>
+      </MainContentControls>
+      {content}
     </MainContentContainer>
   );
 }
 
 const MainContentContainer = styled('div')`
   width: 100%;
-  height: 508px;
+  height: 700px;
+  display: flex;
+  flex-direction: column;
+  gap: ${p => p.theme.space.lg};
+`;
+
+const MainContentControls = styled('div')`
+  display: flex;
+  align-items: center;
+  gap: ${p => p.theme.space.lg};
 `;
