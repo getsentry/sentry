9aadd2d3ed100d72919936e4f26cbd7ffcd8c671
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-07-28 13:13:46 -0400

    ref(rr6): Remove legacy react-router 3 route building compats (#96532)

--- static/app/components/route.tsx
+++ static/app/components/route.tsx
@@ -1,40 +1,9 @@
-import type {RouteProps} from 'sentry/types/legacyReactRouter';
-
-// This module contains the "fake" react components that are used as we migrade
-// off of react-router 3 to 6. The shims in the utils/reactRouter6Compat module
-// read the props off tese components and construct a real react router 6 tree.
-
-type CustomProps = {
-  /**
-   * Human readable route name. This is primarily used in the settings routes.
-   */
-  name?: string;
-  /**
-   * XXX(epurkhiser): This should ONLY be used as we migrate routes away from
-   * the legacy `Route` style tree.
-   */
-  newStyleChildren?: SentryRouteObject[];
-  /**
-   * Ensure this route renders two routes, one for the "org" path which
-   * includes the :orgId slug, and one without.
-   *
-   * Setting this to `true` will prefix the provided path of the secondary
-   * route with:
-   *
-   *   /organizations/:orgId
-   *
-   * Setting this will wrap the two routes in withDomainRequired and
-   * withDomainRedirect respectively.
-   */
-  withOrgPath?: boolean;
-};
-
 /**
  * This is our "custom" route object. It varies a bit from react-router 6's
  * routing object in that it doesn't take a rendered component, but instead a
  * component type and handles the rendering itself.
  */
-export interface SentryRouteObject extends CustomProps {
+export interface SentryRouteObject {
   /**
    * child components to render under this route
    */
@@ -52,6 +21,15 @@ export interface SentryRouteObject extends CustomProps {
    */
   index?: boolean;
   /**
+   * Human readable route name. This is primarily used in the settings routes.
+   */
+  name?: string;
+  /**
+   * XXX(epurkhiser): This should ONLY be used as we migrate routes away from
+   * the legacy `Route` style tree.
+   */
+  newStyleChildren?: SentryRouteObject[];
+  /**
    * The react router path of this component
    */
   path?: string;
@@ -61,17 +39,22 @@ export interface SentryRouteObject extends CustomProps {
    * ignored in this case.
    */
   redirectTo?: string;
+  /**
+   * Ensure this route renders two routes, one for the "org" path which
+   * includes the :orgId slug, and one without.
+   *
+   * Setting this to `true` will prefix the provided path of the secondary
+   * route with:
+   *
+   *   /organizations/:orgId
+   *
+   * Setting this will wrap the two routes in withDomainRequired and
+   * withDomainRedirect respectively.
+   */
+  withOrgPath?: boolean;
 
   // XXX(epurkhiser): In the future we can introduce a `requiresLegacyProps`
   // prop here that will pass in the react-router 3 style routing props. We can
   // use this as a way to slowly get rid of react router 3 style prosp in favor
   // of using the route hooks.
 }
-
-interface SentryRouteProps extends React.PropsWithChildren<RouteProps & CustomProps> {}
-
-export function Route(_props: SentryRouteProps) {
-  // XXX: These routes are NEVER rendered
-  return null;
-}
-Route.displayName = 'Route';
