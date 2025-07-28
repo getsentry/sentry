6aafef6c4d658c88f15274a73e095e137c833f0b
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-07-28 13:45:27 +0200

    ref: Update tempest logic to look for playstation in enabledConsolePlatforms

--- src/sentry/tempest/utils.py
+++ src/sentry/tempest/utils.py
@@ -9,4 +9,12 @@ from sentry.users.services.user import RpcUser
 def has_tempest_access(
     organization: Organization | None, actor: User | RpcUser | AnonymousUser | None = None
 ) -> bool:
-    return features.has("organizations:tempest-access", organization, actor=actor)
+    has_feature = features.has("organizations:tempest-access", organization, actor=actor)
+
+    if organization is None:
+        return has_feature
+
+    enabled_platforms = organization.get_option("sentry:enabled_console_platforms", [])
+    has_playstation = "playstation" in enabled_platforms
+
+    return has_feature or has_playstation
