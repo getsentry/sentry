29f6cdd7de4450388aa7d7a559b64cae3e93a682
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-07-28 16:28:37 -0400

    feat(trace-eap-waterfall): Checking for valid event uuid

--- static/app/views/performance/newTraceDetails/traceApi/utils.tsx
+++ static/app/views/performance/newTraceDetails/traceApi/utils.tsx
@@ -106,3 +106,9 @@ export const isTraceItemDetailsResponse = (
 ): data is TraceItemDetailsResponse => {
   return data !== undefined && 'attributes' in data;
 };
+
+export const isValidEventUUID = (id: string): boolean => {
+  const uuidRegex =
+    /^[0-9a-f]{8}[0-9a-f]{4}[1-5][0-9a-f]{3}[89ab][0-9a-f]{3}[0-9a-f]{12}$/i;
+  return uuidRegex.test(id);
+};
