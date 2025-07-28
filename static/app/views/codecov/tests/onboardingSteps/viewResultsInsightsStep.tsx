1d809e006a22d89feb0f07ae5365fb361584e062
Calvin Yau <calvin.yau@sentry.io>
2025-07-28 11:59:27 -0700

    feat(codecov): Add TA onboarding steps for steps 4,5,6

---
+++ static/app/views/codecov/tests/onboardingSteps/viewResultsInsightsStep.tsx
@@ -0,0 +1,39 @@
+import styled from '@emotion/styled';
+
+import {t, tct} from 'sentry/locale';
+import {OnboardingStep} from 'sentry/views/codecov/tests/onboardingSteps/onboardingStep';
+
+interface ViewResultsInsightsStepProps {
+  step: string;
+}
+
+export function ViewResultsInsightsStep({step}: ViewResultsInsightsStepProps) {
+  const headerText = tct('Step [step]: View results and insights', {
+    step,
+  });
+
+  return (
+    <OnboardingStep.Container>
+      <OnboardingStep.Header>{headerText}</OnboardingStep.Header>
+      <OnboardingStep.Content>
+        <StyledP>
+          {t(
+            "After the test run completion, you'll be able to see the failed tests result on the following areas:"
+          )}
+        </StyledP>
+        <StyledUl>
+          <li>{t('Github pull request comment')}</li>
+          <li>{t('Failed tests dashboard here')}</li>
+        </StyledUl>
+      </OnboardingStep.Content>
+    </OnboardingStep.Container>
+  );
+}
+
+const StyledP = styled('p')`
+  margin: 0;
+`;
+
+const StyledUl = styled('ul')`
+  margin: 0;
+`;
