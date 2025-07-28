a08fef6e31abeb3746168487b42ed936b7145c57
Scott Cooper <scttcper@gmail.com>
2025-07-28 11:04:28 -0700

    add deprecated note

--- tests/js/fixtures/routerFixture.ts
+++ tests/js/fixtures/routerFixture.ts
@@ -2,6 +2,10 @@ import {LocationFixture} from 'sentry-fixture/locationFixture';
 
 import type {InjectedRouter} from 'sentry/types/legacyReactRouter';
 
+/**
+ * @deprecated Use `initialRouterConfig` and the real react-router instead.
+ * https://develop.sentry.dev/frontend/using-rtl/#testing-route-changes
+ */
 export function RouterFixture(params = {}): InjectedRouter {
   return {
     push: jest.fn(),
