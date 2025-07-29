38de91557361ad9b21e5c1e5d3b55b40bd33d15c
Vjeran Grozdanic <vjeran.grozdanic@sentry.io>
2025-07-29 11:08:46 +0200

    ref(hybridcloud): Refactor queries on cacheversion tables

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3535,3 +3535,10 @@ register(
     default=0.0,
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
+
+register(
+    "sentry.hybridcloud.cacheversion.rollout",
+    type=Float,
+    default=0.0,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
