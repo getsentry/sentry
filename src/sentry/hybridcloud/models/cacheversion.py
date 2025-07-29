4f7618fe8914656e3d9886ed58f6cf2adcf4ed04
Vjeran Grozdanić <vjeran.grozdanic@sentry.io>
2025-07-29 16:51:28 +0200

    ref(hybridcloud): Refactor queries on cacheversion tables (#96627)
    Lately `hybridcloud_regioncacheversion` has been quite hot, and one of
    the reasons for it could be inefficient queries performed.
    
    This PR optimizes query executed on the table, from (worst case
    scenario) total of 4 to 2 queries. Before we were using our custom
    `create_or_update` method, instead of Django's `update_or_create` which
    is slightly better and more optimized, and before we used to perform
    extra SELECT after we have updated the value, which caused additional
    load on the database without the need, since we already had the object
    in memory.
    
    
    Since this is a critical path, the change will be rolled out gradually
    in 10-50-100 percentages.

--- src/sentry/hybridcloud/models/cacheversion.py
+++ src/sentry/hybridcloud/models/cacheversion.py
@@ -7,6 +7,7 @@ from django.db.models import F
 from sentry.backup.scopes import RelocationScope
 from sentry.db.models import Model, control_silo_model, region_silo_model
 from sentry.db.postgres.transactions import enforce_constraints
+from sentry.options.rollout import in_random_rollout
 
 logger = logging.getLogger(__name__)
 
@@ -20,6 +21,20 @@ class CacheVersionBase(Model):
 
     @classmethod
     def incr_version(cls, key: str) -> int:
+        if in_random_rollout("sentry.hybridcloud.cacheversion.rollout"):
+            with enforce_constraints(transaction.atomic(router.db_for_write(cls))):
+                if random.random() < 0.01:
+                    logger.info("cacheversion.incr_version", extra={"key": key})
+                obj, created = cls.objects.select_for_update().get_or_create(
+                    key=key, defaults=dict(version=1)
+                )
+                if created:
+                    return obj.version
+
+                obj.version += 1
+                obj.save(update_fields=["version"])
+                return obj.version
+
         with enforce_constraints(transaction.atomic(router.db_for_write(cls))):
             if random.random() < 0.01:
                 logger.info("cacheversion.incr_version", extra={"key": key})
