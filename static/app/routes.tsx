06dbb37069108881b334aa8c8b2ab089ea3e634a
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-07-29 14:06:30 -0700

    except for the last prop, were good

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -686,6 +686,7 @@ function buildRoutes(): RouteObject[] {
       path: 'toolbar/',
       name: t('Developer Toolbar'),
       component: make(() => import('sentry/views/settings/project/projectToolbar')),
+      deprecatedRouteProps: true, // Can be false except for ProjectContext passed as `outletContext`
     },
     {
       path: 'source-maps/',
