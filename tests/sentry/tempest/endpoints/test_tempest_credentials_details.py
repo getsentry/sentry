6e21e2a76b5d0505a063c2f61be961c324b2af14
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-07-29 10:46:08 +0200

    ref: Update tempest logic to look for playstation in enabled console platforms (#96619)
    closes
    https://linear.app/getsentry/issue/TET-958/update-tempest-logic-to-check-for-playstation-in

--- tests/sentry/tempest/endpoints/test_tempest_credentials_details.py
+++ tests/sentry/tempest/endpoints/test_tempest_credentials_details.py
@@ -48,12 +48,50 @@ class TestTempestCredentialsDetails(APITestCase):
         assert not TempestCredentials.objects.filter(id=self.tempest_credentials.id).exists()
         create_audit_entry.assert_called()
 
+    @patch(
+        "sentry.tempest.endpoints.tempest_credentials_details.TempestCredentialsDetailsEndpoint.create_audit_entry"
+    )
+    def test_delete_tempest_credentials_as_org_admin_enabled_console_platforms(
+        self, create_audit_entry
+    ):
+        with Feature({"organizations:project-creation-games-tab": True}):
+            self.organization.update_option("sentry:enabled_console_platforms", ["playstation"])
+            self.login_as(self.user)
+            response = self.get_response(
+                self.project.organization.slug,
+                self.project.slug,
+                self.tempest_credentials.id,
+                method="DELETE",
+            )
+
+            assert response.status_code == 204
+            assert not TempestCredentials.objects.filter(id=self.tempest_credentials.id).exists()
+            create_audit_entry.assert_called()
+
     def test_non_admin_cant_delete_credentials(self) -> None:
         non_admin_user = self.create_user()
         self.create_member(
             user=non_admin_user, organization=self.project.organization, role="member"
         )
         with Feature({"organizations:tempest-access": True}):
+            self.login_as(non_admin_user)
+            response = self.get_response(
+                self.project.organization.slug,
+                self.project.slug,
+                self.tempest_credentials.id,
+                method="DELETE",
+            )
+
+        assert response.status_code == 403
+        assert TempestCredentials.objects.filter(id=self.tempest_credentials.id).exists()
+
+    def test_non_admin_cant_delete_credentials_enabled_console_platforms(self) -> None:
+        non_admin_user = self.create_user()
+        self.create_member(
+            user=non_admin_user, organization=self.project.organization, role="member"
+        )
+        with Feature({"organizations:project-creation-games-tab": True}):
+            self.organization.update_option("sentry:enabled_console_platforms", ["playstation"])
             self.login_as(non_admin_user)
             response = self.get_response(
                 self.project.organization.slug,
