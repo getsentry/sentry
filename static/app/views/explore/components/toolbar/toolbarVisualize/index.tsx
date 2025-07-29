c72fe1a6d59c3746e2aff63db39cdb9d3c5dec4f
Tony Xiao <txiao@sentry.io>
2025-07-29 10:28:13 -0400

    ref(explore): Use shared visualize and group by components in explore… (#96572)
    … spans
    
    This updates explore spans to use the shared components with the span
    specific customizations on top. Once the page params are merged, most of
    the customizations should go away as well.

--- static/app/views/explore/components/toolbar/toolbarVisualize/index.tsx
+++ static/app/views/explore/components/toolbar/toolbarVisualize/index.tsx
@@ -1,10 +1,18 @@
+import styled from '@emotion/styled';
+
+import {Button} from 'sentry/components/core/button';
+import type {SelectKey, SelectOption} from 'sentry/components/core/compactSelect';
+import {CompactSelect} from 'sentry/components/core/compactSelect';
 import {Tooltip} from 'sentry/components/core/tooltip';
 import {IconAdd} from 'sentry/icons';
+import {IconDelete} from 'sentry/icons/iconDelete';
 import {t} from 'sentry/locale';
+import type {ParsedFunction} from 'sentry/utils/discover/fields';
 import {
   ToolbarFooterButton,
   ToolbarHeader,
   ToolbarLabel,
+  ToolbarRow,
 } from 'sentry/views/explore/components/toolbar/styles';
 
 export function ToolbarVisualizeHeader() {
@@ -19,6 +27,53 @@ export function ToolbarVisualizeHeader() {
         <ToolbarLabel>{t('Visualize')}</ToolbarLabel>
       </Tooltip>
     </ToolbarHeader>
+  );
+}
+
+interface ToolbarVisualizeDropdownProps {
+  aggregateOptions: Array<SelectOption<SelectKey>>;
+  canDelete: boolean;
+  fieldOptions: Array<SelectOption<SelectKey>>;
+  onChangeAggregate: (option: SelectOption<SelectKey>) => void;
+  onChangeArgument: (option: SelectOption<SelectKey>) => void;
+  onDelete: () => void;
+  parsedFunction: ParsedFunction | null;
+}
+
+export function ToolbarVisualizeDropdown({
+  aggregateOptions,
+  canDelete,
+  fieldOptions,
+  onChangeAggregate,
+  onChangeArgument,
+  onDelete,
+  parsedFunction,
+}: ToolbarVisualizeDropdownProps) {
+  return (
+    <ToolbarRow>
+      <AggregateCompactSelect
+        searchable
+        options={aggregateOptions}
+        value={parsedFunction?.name ?? ''}
+        onChange={onChangeAggregate}
+      />
+      <FieldCompactSelect
+        searchable
+        options={fieldOptions}
+        value={parsedFunction?.arguments[0] ?? ''}
+        onChange={onChangeArgument}
+        disabled={fieldOptions.length === 1}
+      />
+      {canDelete ? (
+        <Button
+          borderless
+          icon={<IconDelete />}
+          size="zero"
+          onClick={onDelete}
+          aria-label={t('Remove Overlay')}
+        />
+      ) : null}
+    </ToolbarRow>
   );
 }
 
@@ -58,3 +113,20 @@ export function ToolbarVisualizeAddEquation({add, disabled}: ToolbarVisualizeAdd
     </ToolbarFooterButton>
   );
 }
+
+const AggregateCompactSelect = styled(CompactSelect)`
+  width: 100px;
+
+  > button {
+    width: 100%;
+  }
+`;
+
+const FieldCompactSelect = styled(CompactSelect)`
+  flex: 1 1;
+  min-width: 0;
+
+  > button {
+    width: 100%;
+  }
+`;
--- static/app/views/explore/toolbar/index.tsx
+++ static/app/views/explore/toolbar/index.tsx
@@ -1,22 +1,16 @@
-import {useCallback} from 'react';
 import styled from '@emotion/styled';
 
-import type {SelectOption} from 'sentry/components/core/compactSelect';
 import {defined} from 'sentry/utils';
-import {ToolbarGroupBy} from 'sentry/views/explore/components/toolbar/toolbarGroupBy';
 import {
   useExploreGroupBys,
   useExploreVisualizes,
   useSetExploreGroupBys,
   useSetExploreVisualizes,
 } from 'sentry/views/explore/contexts/pageParamsContext';
-import {Mode} from 'sentry/views/explore/contexts/pageParamsContext/mode';
-import {useTraceItemTags} from 'sentry/views/explore/contexts/spanTagsContext';
-import {useGroupByFields} from 'sentry/views/explore/hooks/useGroupByFields';
+import {ToolbarGroupBy} from 'sentry/views/explore/toolbar/toolbarGroupBy';
 import {ToolbarSaveAs} from 'sentry/views/explore/toolbar/toolbarSaveAs';
 import {ToolbarSortBy} from 'sentry/views/explore/toolbar/toolbarSortBy';
 import {ToolbarVisualize} from 'sentry/views/explore/toolbar/toolbarVisualize';
-import {TraceItemDataset} from 'sentry/views/explore/types';
 
 type Extras = 'equations';
 
@@ -26,29 +20,11 @@ interface ExploreToolbarProps {
 }
 
 export function ExploreToolbar({extras, width}: ExploreToolbarProps) {
-  const {tags} = useTraceItemTags('string');
-
   const visualizes = useExploreVisualizes();
   const setVisualizes = useSetExploreVisualizes();
 
   const groupBys = useExploreGroupBys();
-  const _setGroupBys = useSetExploreGroupBys();
-  const setGroupBys = useCallback(
-    (columns: string[], op: 'insert' | 'update' | 'delete' | 'reorder') => {
-      // automatically switch to aggregates mode when a group by is inserted/updated
-      if (op === 'insert' || op === 'update') {
-        _setGroupBys(columns, Mode.AGGREGATE);
-      } else {
-        _setGroupBys(columns);
-      }
-    },
-    [_setGroupBys]
-  );
-  const options: Array<SelectOption<string>> = useGroupByFields({
-    groupBys,
-    tags,
-    traceItemType: TraceItemDataset.SPANS,
-  });
+  const setGroupBys = useSetExploreGroupBys();
 
   return (
     <Container width={width}>
@@ -57,12 +33,7 @@ export function ExploreToolbar({extras, width}: ExploreToolbarProps) {
         setVisualizes={setVisualizes}
         allowEquations={extras?.includes('equations') || false}
       />
-      <ToolbarGroupBy
-        allowMultiple
-        groupBys={groupBys}
-        setGroupBys={setGroupBys}
-        options={options}
-      />
+      <ToolbarGroupBy groupBys={groupBys} setGroupBys={setGroupBys} />
       <ToolbarSortBy />
       <ToolbarSaveAs />
     </Container>
