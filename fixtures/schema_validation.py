5c0dd481363e5efcf04e4e10c3059c00a5f220a8
anthony sottile <103459774+asottile-sentry@users.noreply.github.com>
2025-07-29 15:03:38 -0400

    ref: fix typing for fixtures.schema_validation (#96667)
    this decorator caused anything decorated to become Any
    
    <!-- Describe your PR here. -->

--- fixtures/schema_validation.py
+++ fixtures/schema_validation.py
@@ -1,20 +1,27 @@
+import functools
+from collections.abc import Callable
+
 import pytest
 from jsonschema import ValidationError
 
 
-def invalid_schema(func):
-    def inner(self, *args, **kwargs):
+def invalid_schema[**P](func: Callable[P, None]) -> Callable[P, None]:
+    @functools.wraps(func)
+    def inner(*args: P.args, **kwargs: P.kwargs) -> None:
         with pytest.raises(ValidationError):
-            func(self)
+            func(*args, **kwargs)
 
     return inner
 
 
-def invalid_schema_with_error_message(message):
-    def decorator(func):
-        def inner(self, *args, **kwargs):
+def invalid_schema_with_error_message[
+    **P
+](message: str) -> Callable[[Callable[P, None]], Callable[P, None]]:
+    def decorator(func: Callable[P, None]) -> Callable[P, None]:
+        @functools.wraps(func)
+        def inner(*args: P.args, **kwargs: P.kwargs) -> None:
             with pytest.raises(ValidationError) as excinfo:
-                func(self)
+                func(*args, **kwargs)
             assert excinfo.value.message == message
 
         return inner
