7a920e01feaa67b7122d891784ef2d798535a9ae
TkDodo <dominik.dorfmeister@sentry.io>
2025-07-29 13:01:59 +0200

    feat: getApiUrl

---
+++ static/app/api/getApiUrl.spec.ts
@@ -0,0 +1,87 @@
+import {getApiUrl} from './getApiUrl';
+
+describe('getApiUrl', () => {
+  test('should replace path parameters with their values', () => {
+    const url = getApiUrl('/projects/$orgSlug/$projectSlug/', {
+      path: {
+        orgSlug: 'my-org',
+        projectSlug: 'my-project',
+      },
+    });
+
+    expect(url).toBe('/projects/my-org/my-project/');
+  });
+
+  test('should not require path parameters if none are present', () => {
+    const url = getApiUrl('/projects/');
+
+    expect(url).toBe('/projects/');
+  });
+
+  test('should encode path parameters correctly', () => {
+    const url = getApiUrl('/projects/$orgSlug/$projectSlug/releases/$releaseVersion/', {
+      path: {
+        orgSlug: 'my-org',
+        projectSlug: 'my-project',
+        releaseVersion: 'v 1.0.0',
+      },
+    });
+
+    expect(url).toBe('/projects/my-org/my-project/releases/v%201.0.0/');
+  });
+
+  test('should not modify already encoded parameters', () => {
+    const url = getApiUrl('/search/$query/', {
+      path: {query: 'test%20query'},
+    });
+
+    expect(url).toBe('/search/test%20query/');
+  });
+
+  test('should stringify number path params', () => {
+    const url = getApiUrl('/items/$id/', {
+      path: {id: 123},
+    });
+
+    expect(url).toBe('/items/123/');
+  });
+
+  test('should not do accidental replacements', () => {
+    const url = getApiUrl('/projects/$id1/$id', {
+      path: {id: '123', id1: '456'},
+    });
+
+    expect(url).toBe('/projects/456/123');
+  });
+
+  describe('types', () => {
+    test('should not allow invalid path parameters', () => {
+      getApiUrl('/projects/$orgSlug/', {
+        // @ts-expect-error Invalid path parameter
+        path: {orgSlug: 'my-org', invalidParam: 'invalid'},
+      });
+    });
+
+    test('should not allow excess path parameters', () => {
+      getApiUrl('/projects/$orgSlug/', {
+        staleTime: 0,
+        // @ts-expect-error Excess path parameter
+        path: {orgSlug: 'my-org', extraParam: 'extra'},
+      });
+    });
+
+    test('should require path params for paths with parameters', () => {
+      expect(() => {
+        getApiUrl('/projects/$orgSlug/', {
+          // @ts-expect-error Missing required path parameter
+          path: {},
+        });
+      }).toThrow('Missing path param: orgSlug');
+    });
+
+    test('should not allow empty path parameters for paths without parameters', () => {
+      // @ts-expect-error Expected 1 argument, but got 2
+      getApiUrl('/projects/', {path: {}});
+    });
+  });
+});
---
+++ static/app/api/getApiUrl.ts
@@ -0,0 +1,48 @@
+function safeEncodeURIComponent(input: string) {
+  try {
+    // If decoding and re-encoding gives the same string, it's already encoded
+    if (encodeURIComponent(decodeURIComponent(input)) === input) {
+      return input;
+    }
+  } catch (e) {
+    // decodeURIComponent threw an error, so it was not encoded
+  }
+  return encodeURIComponent(input);
+}
+
+type ExtractPathParams<TApiPath extends string> =
+  TApiPath extends `${string}$${infer Param}/${infer Rest}`
+    ? Param | ExtractPathParams<`/${Rest}`>
+    : TApiPath extends `${string}$${infer Param}`
+      ? Param
+      : never;
+
+type PathParamOptions<TApiPath extends string> =
+  ExtractPathParams<TApiPath> extends never
+    ? {path?: never}
+    : {path: Record<ExtractPathParams<TApiPath>, string | number>};
+
+type OptionalPathParams<TApiPath extends string> =
+  ExtractPathParams<TApiPath> extends never
+    ? [] // eslint-disable-line @typescript-eslint/no-restricted-types
+    : [PathParamOptions<TApiPath>];
+
+const paramRegex = /\$([a-zA-Z0-9_-]+)/g;
+
+export function getApiUrl<TApiPath extends string>(
+  path: TApiPath,
+  ...[options]: OptionalPathParams<TApiPath>
+): string {
+  let url: string = path;
+  const pathParams = options?.path;
+  if (pathParams) {
+    // Replace path parameters in the URL with their corresponding values
+    url = url.replace(paramRegex, (_, key: string) => {
+      if (!(key in pathParams)) {
+        throw new Error(`Missing path param: ${key}`);
+      }
+      return safeEncodeURIComponent(String(pathParams[key as keyof typeof pathParams]));
+    });
+  }
+  return url;
+}
