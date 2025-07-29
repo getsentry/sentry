9d72406817d449a5d87167c29819ef7bbacba01a
Richard Roggenkemper <rroggenkemper22@berkeley.edu>
2025-07-29 15:09:13 -0700

    update for ui1

--- static/app/icons/iconAllProjects.tsx
+++ static/app/icons/iconAllProjects.tsx
@@ -1,4 +1,3 @@
-import {Fragment} from 'react';
 import {useTheme} from '@emotion/react';
 
 import type {SVGIconProps} from './svgIcon';
@@ -6,23 +5,50 @@ import {SvgIcon} from './svgIcon';
 
 function IconAllProjects(props: SVGIconProps) {
   const theme = useTheme();
-  return (
+  return theme.isChonk ? (
     <SvgIcon {...props} kind={theme.isChonk ? 'stroke' : 'path'}>
-      <Fragment>
-        <rect
-          x="5"
-          y="5"
-          width="8.25"
-          height="8.25"
-          rx="1"
-          ry="1"
-          transform="translate(18.25 0) rotate(90)"
-        />
-        <path d="M2.75,9.5V3.75c0-.55.45-1,1-1h5.75" />
-        <line x1="7.75" y1="7.75" x2="10.5" y2="7.75" />
-        <line x1="7.75" y1="10.5" x2="10.5" y2="10.5" />
-      </Fragment>
+      <rect
+        x="5"
+        y="5"
+        width="8.25"
+        height="8.25"
+        rx="1"
+        ry="1"
+        transform="translate(18.25 0) rotate(90)"
+      />
+      <path d="M2.75,9.5V3.75c0-.55.45-1,1-1h5.75" />
+      <line x1="7.75" y1="7.75" x2="10.5" y2="7.75" />
+      <line x1="7.75" y1="10.5" x2="10.5" y2="10.5" />
     </SvgIcon>
+  ) : (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <rect width="13" height="13" rx="2" fill={theme.gray200} />
+      <rect
+        x="0.5"
+        y="0.5"
+        width="12"
+        height="12"
+        rx="1.5"
+        stroke={theme.gray400}
+        strokeOpacity="0.14"
+      />
+      <rect x="4" y="4" width="13" height="13" rx="2" fill={theme.gray200} />
+      <rect
+        x="4.5"
+        y="4.5"
+        width="12"
+        height="12"
+        rx="1.5"
+        stroke={theme.gray400}
+        strokeOpacity="0.14"
+      />
+    </svg>
   );
 }
 
--- static/app/icons/iconMyProjects.tsx
+++ static/app/icons/iconMyProjects.tsx
@@ -1,4 +1,3 @@
-import {Fragment} from 'react';
 import {useTheme} from '@emotion/react';
 
 import type {SVGIconProps} from './svgIcon';
@@ -6,24 +5,51 @@ import {SvgIcon} from './svgIcon';
 
 function IconMyProjects(props: SVGIconProps) {
   const theme = useTheme();
-  return (
+  return theme.isChonk ? (
     <SvgIcon {...props} kind={theme.isChonk ? 'stroke' : 'path'}>
-      <Fragment>
-        <rect
-          x="5.02"
-          y="5"
-          width="8.25"
-          height="8.25"
-          rx="1"
-          ry="1"
-          transform="translate(18.27 -.02) rotate(90)"
-        />
-        <path d="M2.77,9.5V3.75c0-.55.45-1,1-1h5.75" />
-        <path d="M7.52,10.17c.78.78,2.34.78,3.25,0" />
-        <circle cx="10.52" cy="7.75" r=".25" />
-        <circle cx="7.77" cy="7.75" r=".25" />
-      </Fragment>
+      <rect
+        x="5.02"
+        y="5"
+        width="8.25"
+        height="8.25"
+        rx="1"
+        ry="1"
+        transform="translate(18.27 -.02) rotate(90)"
+      />
+      <path d="M2.77,9.5V3.75c0-.55.45-1,1-1h5.75" />
+      <path d="M7.52,10.17c.78.78,2.34.78,3.25,0" />
+      <circle cx="10.52" cy="7.75" r=".25" />
+      <circle cx="7.77" cy="7.75" r=".25" />
     </SvgIcon>
+  ) : (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="18"
+      height="18"
+      viewBox="0 0 18 18"
+      fill="none"
+    >
+      <rect width="13" height="13" rx="2" fill={theme.gray200} />
+      <rect
+        x="0.5"
+        y="0.5"
+        width="12"
+        height="12"
+        rx="1.5"
+        stroke={theme.gray400}
+        strokeOpacity="0.14"
+      />
+      <rect x="4" y="4" width="13" height="13" rx="2" fill={theme.gray200} />
+      <rect
+        x="4.5"
+        y="4.5"
+        width="12"
+        height="12"
+        rx="1.5"
+        stroke={theme.gray400}
+        strokeOpacity="0.14"
+      />
+    </svg>
   );
 }
 
