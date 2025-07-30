bbee22ed4fab148210b169812498dd471d5fbcfc
Simon Hellmayr <shellmayr@users.noreply.github.com>
2025-07-30 09:56:11 +0200

    chore(analytics): fix casing of codeowners (#96728)

--- src/sentry/analytics/events/codeowners_updated.py
+++ src/sentry/analytics/events/codeowners_updated.py
@@ -2,11 +2,11 @@ from sentry import analytics
 
 
 @analytics.eventclass("codeowners.updated")
-class CodeownersUpdated(analytics.Event):
+class CodeOwnersUpdated(analytics.Event):
     user_id: int | None = None
     organization_id: int
     project_id: int
     codeowners_id: int
 
 
-analytics.register(CodeownersUpdated)
+analytics.register(CodeOwnersUpdated)
