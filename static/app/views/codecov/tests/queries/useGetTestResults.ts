5039a72641ba635e37c435ef63e44e7054f58f9d
Adrian <adrian.bonilla@sentry.io>
2025-07-30 10:58:30 -0600

    adjust querykey

--- static/app/views/codecov/tests/queries/useGetTestResults.ts
+++ static/app/views/codecov/tests/queries/useGetTestResults.ts
@@ -129,7 +129,7 @@ export function useInfiniteTestResults({
                 ],
               sortBy: signedSortBy,
               term,
-              ...(filterBranch ? {branch: filterBranch} : {}),
+              branch: filterBranch,
               ...(mappedFilterBy ? {filterBy: mappedFilterBy} : {}),
               ...(testSuites ? {testSuites} : {}),
               ...(cursor ? {cursor} : {}),
