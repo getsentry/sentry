259ef6150c447726bd1b50da9aa5dcf9e839288b
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-07-30 11:33:59 -0400

    ref(rr6): useNavigate in eventInputName

--- static/app/views/discover/eventInputName.tsx
+++ static/app/views/discover/eventInputName.tsx
@@ -1,8 +1,9 @@
+import {useNavigate} from 'react-router-dom';
+
 import EditableText from 'sentry/components/editableText';
 import * as Layout from 'sentry/components/layouts/thirds';
 import {t} from 'sentry/locale';
 import type {Organization, SavedQuery} from 'sentry/types/organization';
-import {browserHistory} from 'sentry/utils/browserHistory';
 import EventView from 'sentry/utils/discover/eventView';
 import normalizeUrl from 'sentry/utils/url/normalizeUrl';
 import useApi from 'sentry/utils/useApi';
@@ -25,6 +26,7 @@ const HOMEPAGE_DEFAULT = t('New Query');
  */
 function EventInputName({organization, eventView, savedQuery, isHomepage}: Props) {
   const api = useApi();
+  const navigate = useNavigate();
 
   function handleChange(nextQueryName: string) {
     // Do not update automatically if
@@ -48,9 +50,7 @@ function EventInputName({organization, eventView, savedQuery, isHomepage}: Props
         const renamedEventView = eventView.clone();
         renamedEventView.name = nextQueryName;
 
-        browserHistory.push(
-          normalizeUrl(renamedEventView.getResultsViewUrlTarget(organization))
-        );
+        navigate(normalizeUrl(renamedEventView.getResultsViewUrlTarget(organization)));
       }
     );
   }
