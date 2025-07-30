c7f001407d0dd4d04be1dab00ff73b1d0bc92c94
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-07-30 11:33:27 -0400

    ref(js): Convert serviceHookSettingsForm to a FC

--- static/app/views/settings/project/serviceHookSettingsForm.tsx
+++ static/app/views/settings/project/serviceHookSettingsForm.tsx
@@ -1,5 +1,3 @@
-import {Component} from 'react';
-
 import ApiForm from 'sentry/components/forms/apiForm';
 import MultipleCheckbox from 'sentry/components/forms/controls/multipleCheckbox';
 import BooleanField from 'sentry/components/forms/fields/booleanField';
@@ -11,8 +9,8 @@ import PanelHeader from 'sentry/components/panels/panelHeader';
 import {t} from 'sentry/locale';
 import type {ServiceHook} from 'sentry/types/integrations';
 import type {Organization} from 'sentry/types/organization';
-import {browserHistory} from 'sentry/utils/browserHistory';
 import normalizeUrl from 'sentry/utils/url/normalizeUrl';
+import {useNavigate} from 'sentry/utils/useNavigate';
 
 const EVENT_CHOICES = ['event.alert', 'event.created'];
 
@@ -23,62 +21,62 @@ type Props = {
   hookId?: string;
 };
 
-export default class ServiceHookSettingsForm extends Component<Props> {
-  onSubmitSuccess = () => {
-    const {organization, projectId} = this.props;
-    browserHistory.push(
-      normalizeUrl(`/settings/${organization.slug}/projects/${projectId}/hooks/`)
-    );
+export default function ServiceHookSettingsForm({
+  initialData,
+  organization,
+  projectId,
+  hookId,
+}: Props) {
+  const navigate = useNavigate();
+
+  const onSubmitSuccess = () => {
+    navigate(normalizeUrl(`/settings/${organization.slug}/projects/${projectId}/hooks/`));
   };
 
-  render() {
-    const {initialData, organization, projectId, hookId} = this.props;
+  const endpoint = hookId
+    ? `/projects/${organization.slug}/${projectId}/hooks/${hookId}/`
+    : `/projects/${organization.slug}/${projectId}/hooks/`;
 
-    const endpoint = hookId
-      ? `/projects/${organization.slug}/${projectId}/hooks/${hookId}/`
-      : `/projects/${organization.slug}/${projectId}/hooks/`;
-
-    return (
-      <Panel>
-        <ApiForm
-          apiMethod={hookId ? 'PUT' : 'POST'}
-          apiEndpoint={endpoint}
-          initialData={initialData}
-          onSubmitSuccess={this.onSubmitSuccess}
-          footerStyle={{
-            marginTop: 0,
-            paddingRight: 20,
-          }}
-          submitLabel={hookId ? t('Save Changes') : t('Create Hook')}
-        >
-          <PanelHeader>{t('Hook Configuration')}</PanelHeader>
-          <PanelBody>
-            <BooleanField name="isActive" label={t('Active')} />
-            <TextField
-              name="url"
-              label={t('URL')}
-              required
-              help={t('The URL which will receive events.')}
-            />
-            <FormField
-              name="events"
-              label={t('Events')}
-              inline={false}
-              help={t('The event types you wish to subscribe to.')}
-            >
-              {({name, value, onChange}: any) => (
-                <MultipleCheckbox onChange={onChange} value={value} name={name}>
-                  {EVENT_CHOICES.map(event => (
-                    <MultipleCheckbox.Item key={event} value={event}>
-                      {event}
-                    </MultipleCheckbox.Item>
-                  ))}
-                </MultipleCheckbox>
-              )}
-            </FormField>
-          </PanelBody>
-        </ApiForm>
-      </Panel>
-    );
-  }
+  return (
+    <Panel>
+      <ApiForm
+        apiMethod={hookId ? 'PUT' : 'POST'}
+        apiEndpoint={endpoint}
+        initialData={initialData}
+        onSubmitSuccess={onSubmitSuccess}
+        footerStyle={{
+          marginTop: 0,
+          paddingRight: 20,
+        }}
+        submitLabel={hookId ? t('Save Changes') : t('Create Hook')}
+      >
+        <PanelHeader>{t('Hook Configuration')}</PanelHeader>
+        <PanelBody>
+          <BooleanField name="isActive" label={t('Active')} />
+          <TextField
+            name="url"
+            label={t('URL')}
+            required
+            help={t('The URL which will receive events.')}
+          />
+          <FormField
+            name="events"
+            label={t('Events')}
+            inline={false}
+            help={t('The event types you wish to subscribe to.')}
+          >
+            {({name, value, onChange}: any) => (
+              <MultipleCheckbox onChange={onChange} value={value} name={name}>
+                {EVENT_CHOICES.map(event => (
+                  <MultipleCheckbox.Item key={event} value={event}>
+                    {event}
+                  </MultipleCheckbox.Item>
+                ))}
+              </MultipleCheckbox>
+            )}
+          </FormField>
+        </PanelBody>
+      </ApiForm>
+    </Panel>
+  );
 }
