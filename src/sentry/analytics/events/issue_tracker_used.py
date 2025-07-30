7a149c8d1c621c53b7842b8c67a823ddbe21b14b
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-07-30 15:57:44 +0200

    fix types

--- src/sentry/analytics/events/issue_tracker_used.py
+++ src/sentry/analytics/events/issue_tracker_used.py
@@ -4,10 +4,10 @@ from sentry import analytics
 @analytics.eventclass("issue_tracker.used")
 class IssueTrackerUsedEvent(analytics.Event):
     user_id: int | None = None
-    default_user_id: str
-    organization_id: str
+    default_user_id: int | str
+    organization_id: int
     issue_tracker: str
-    project_id: str
+    project_id: int
 
 
 analytics.register(IssueTrackerUsedEvent)
--- src/sentry/analytics/events/monitor_mark_failed.py
+++ src/sentry/analytics/events/monitor_mark_failed.py
@@ -3,10 +3,10 @@ from sentry import analytics
 
 @analytics.eventclass("monitor_environment.mark_failed")
 class MonitorEnvironmentMarkFailed(analytics.Event):
-    organization_id: str
-    monitor_id: str
-    project_id: str
-    environment_id: str
+    organization_id: int
+    monitor_id: int
+    project_id: int
+    environment_id: int
 
 
 analytics.register(MonitorEnvironmentMarkFailed)
