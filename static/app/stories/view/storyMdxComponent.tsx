9337faa0312f4079387e497210814af791bec957
Nate Moore <nate.moore@sentry.io>
2025-07-31 15:41:45 -0500

    ref(stories): adjust heading levels

--- static/app/stories/view/storyMdxComponent.tsx
+++ static/app/stories/view/storyMdxComponent.tsx
@@ -1,3 +1,5 @@
+import {Text} from 'sentry/components/core/text';
+
 import {StoryHeading} from './storyHeading';
 
 type HeadingProps = {
@@ -5,10 +7,10 @@ type HeadingProps = {
 };
 
 export const storyMdxComponents = {
-  h1: (props: HeadingProps) => <StoryHeading as="h1" size="2xl" {...props} />,
-  h2: (props: HeadingProps) => <StoryHeading as="h2" size="2xl" {...props} />,
-  h3: (props: HeadingProps) => <StoryHeading as="h3" size="xl" {...props} />,
-  h4: (props: HeadingProps) => <StoryHeading as="h4" size="lg" {...props} />,
-  h5: (props: HeadingProps) => <StoryHeading as="h5" size="md" {...props} />,
-  h6: (props: HeadingProps) => <StoryHeading as="h6" size="sm" {...props} />,
+  h1: (props: HeadingProps) => <StoryHeading as="h3" size="xl" {...props} />,
+  h2: (props: HeadingProps) => <StoryHeading as="h4" size="lg" {...props} />,
+  h3: (props: HeadingProps) => <StoryHeading as="h5" size="md" {...props} />,
+  h4: (props: HeadingProps) => <StoryHeading as="h6" size="sm" {...props} />,
+  h5: (props: HeadingProps) => <Text bold as="p" size="md" {...props} />,
+  h6: (props: HeadingProps) => <Text bold as="p" size="sm" {...props} />,
 };
