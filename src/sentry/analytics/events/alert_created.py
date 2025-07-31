0a9d91a1c1af44a3595c1b52c13c5e39f4cc57f9
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-07-31 11:55:07 +0200

    fix types

--- src/sentry/analytics/events/alert_created.py
+++ src/sentry/analytics/events/alert_created.py
@@ -4,7 +4,7 @@ from sentry import analytics
 @analytics.eventclass("alert.created")
 class AlertCreatedEvent(analytics.Event):
     user_id: int | None = None
-    default_user_id: str
+    default_user_id: int | str | None = None
     organization_id: int
     project_id: int
     rule_id: int
