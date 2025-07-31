e36b8c8f7b60d56bdc322fab767cdc8bfbba69a4
Ajay Singh <ajay.singh@sentry.io>
2025-07-31 10:57:44 -0700

    create repo token list endpoint

---
+++ src/sentry/codecov/endpoints/repository_tokens/query.py
@@ -0,0 +1,36 @@
+query = """query RepoTokensForOwner(
+  $owner: String!
+  $filters: RepositorySetFilters!
+  $ordering: RepositoryOrdering!
+  $direction: OrderingDirection!
+  $first: Int
+  $after: String
+  $last: Int
+  $before: String
+) {
+  owner(username: $owner) {
+    repositories(
+      filters: $filters
+      ordering: $ordering
+      orderingDirection: $direction
+      first: $first
+      after: $after
+      last: $last
+      before: $before
+    ) {
+      edges {
+        node {
+          name
+          uploadToken
+        }
+      }
+      pageInfo {
+          startCursor
+          endCursor
+          hasNextPage
+          hasPreviousPage
+      }
+      totalCount
+    }
+  }
+}"""
