0dad420ec146ac9eb63239d07c68d65a3bb0cad3
Nate Moore <nate.moore@sentry.io>
2025-07-31 16:03:15 -0500

    ref(stories): adjust levels

--- static/app/stories/view/storyMdxComponent.tsx
+++ static/app/stories/view/storyMdxComponent.tsx
@@ -7,10 +7,10 @@ type HeadingProps = {
 };
 
 export const storyMdxComponents = {
-  h1: (props: HeadingProps) => <StoryHeading as="h3" size="xl" {...props} />,
-  h2: (props: HeadingProps) => <StoryHeading as="h4" size="lg" {...props} />,
-  h3: (props: HeadingProps) => <StoryHeading as="h5" size="md" {...props} />,
-  h4: (props: HeadingProps) => <StoryHeading as="h6" size="sm" {...props} />,
-  h5: (props: HeadingProps) => <Text bold as="p" size="md" {...props} />,
+  h1: () => null,
+  h2: (props: HeadingProps) => <StoryHeading as="h3" size="xl" {...props} />,
+  h3: (props: HeadingProps) => <StoryHeading as="h4" size="lg" {...props} />,
+  h4: (props: HeadingProps) => <StoryHeading as="h5" size="md" {...props} />,
+  h5: (props: HeadingProps) => <StoryHeading as="h6" size="sm" {...props} />,
   h6: (props: HeadingProps) => <Text bold as="p" size="sm" {...props} />,
 };
