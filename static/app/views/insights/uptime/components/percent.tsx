fdf3bb81de7cc2c9218674f32ff3c78f987576c2
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-07-31 16:48:06 -0400

    fix(uptime): Cramer math (#96901)

--- static/app/views/insights/uptime/components/percent.tsx
+++ static/app/views/insights/uptime/components/percent.tsx
@@ -17,14 +17,16 @@ type UptimePercentProps = {
 };
 
 export function UptimePercent({summary, note, size}: UptimePercentProps) {
-  const knownChecks = summary.totalChecks - summary.missedWindowChecks;
+  // Do not consider missed or failed checks in the downtime % calculations
+  const knownChecks =
+    summary.totalChecks - summary.missedWindowChecks - summary.failedChecks;
 
   if (knownChecks === 0) {
     return <Text variant="muted">0.0%</Text>;
   }
 
-  const percentFull = ((knownChecks - summary.downtimeChecks) / knownChecks) * 100;
-  const successChecks = knownChecks - summary.downtimeChecks - summary.failedChecks;
+  const successChecks = knownChecks - summary.downtimeChecks;
+  const percentFull = (successChecks / knownChecks) * 100;
 
   // Round down to 3 decimals
   const percent = Math.floor(percentFull * 1000) / 1000;
