a66687755c33c6192a43823397dc2c57bc1de287
Raj Joshi <raj.joshi@sentry.io>
2025-08-01 07:50:13 -0700

    :bug: fix: typing

--- src/sentry/identity/gitlab/provider.py
+++ src/sentry/identity/gitlab/provider.py
@@ -94,7 +94,7 @@ class GitlabIdentityProvider(OAuth2Provider):
             "client_secret": client_secret,
         }
 
-    def refresh_identity(self, identity: Identity | RpcIdentity, **kwargs: Any) -> RpcIdentity:
+    def refresh_identity(self, identity: Identity | RpcIdentity, **kwargs: Any) -> None:
         refresh_token = identity.data.get("refresh_token")
         refresh_token_url = kwargs.get("refresh_token_url")
 
@@ -130,10 +130,4 @@ class GitlabIdentityProvider(OAuth2Provider):
             payload = {}
 
         identity.data.update(get_oauth_data(payload))
-        rpc_identity = identity_service.update_data(identity_id=identity.id, data=identity.data)
-
-        # This should not happen since we are using the same identity object
-        if rpc_identity is None:
-            raise IdentityNotValid("Failed to refresh identity: No RPC identity found")
-
-        return rpc_identity
+        identity_service.update_data(identity_id=identity.id, data=identity.data)
