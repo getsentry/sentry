2033326e915609289a2b31a21b285e56333a10c5
JonasBa <jonas@badalic.com>
2025-07-31 20:38:19 -0400

    add faulty component

--- static/app/stories/view/storyExports.tsx
+++ static/app/stories/view/storyExports.tsx
@@ -170,12 +170,20 @@ function StoryAPI() {
 
   return (
     <Fragment>
+      <FaultyComponent />
       {Object.entries(story.exports.types).map(([key, value]) => {
         return <Storybook.APIReference key={key} types={value} />;
       })}
     </Fragment>
   );
 }
+
+const FaultyComponent = styled('div')`
+  display: flex;
+  flex-direction: column;
+  gap: ${space(1)};
+  padding: ${space(2)};
+`;
 
 const StoryHeader = styled('header')`
   background: ${p => p.theme.tokens.background.secondary};
