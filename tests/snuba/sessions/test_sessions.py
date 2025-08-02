ae2b8b45a0e17ce024504dd9c55aab6d03d89a25
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-08-01 19:29:25 -0700

    forget about the typedict for now

--- tests/snuba/sessions/test_sessions.py
+++ tests/snuba/sessions/test_sessions.py
@@ -9,7 +9,7 @@ from django.utils import timezone
 
 from sentry.release_health.base import OverviewStat
 from sentry.release_health.metrics import MetricsReleaseHealthBackend
-from sentry.testutils.cases import BaseMetricsTestCase, Session, TestCase
+from sentry.testutils.cases import BaseMetricsTestCase, TestCase
 
 pytestmark = pytest.mark.sentry_metrics
 
@@ -32,7 +32,7 @@ class ReleaseHealthBaseTestCase(BaseMetricsTestCase):
     user_2 = "39887d89-13b2-4c84-8c23-5d13d2102667"
     user_3 = "39887d89-13b2-4c84-8c23-5d13d2102668"
 
-    def create_sessions__v2_crashed(self) -> list[Session]:
+    def create_sessions__v2_crashed(self) -> list[dict]:
         self.received = time.time()
         self.session_started = time.time() // 60 * 60
 
@@ -77,7 +77,7 @@ class ReleaseHealthBaseTestCase(BaseMetricsTestCase):
             ),
         ]
 
-    def create_sessions__v1(self) -> list[Session]:
+    def create_sessions__v1(self) -> list[dict]:
         self.received = time.time()
         self.session_started = time.time() // 60 * 60
 
