a64a629091bf168b213e1d34201222ed5a6c400d
David Herberth <david.herberth@sentry.io>
2025-08-04 12:59:50 +0200

    ref(ourlogs): Remove meta feature flag (#96834)
    This is now always enabled.
    
    Options Automator:
    https://github.com/getsentry/sentry-options-automator/pull/4748
    Relay: https://github.com/getsentry/relay/pull/5006

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -525,8 +525,6 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:ourlogs-enabled", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable our logs product to be ingested via Relay.
     manager.add("organizations:ourlogs-ingestion", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable extraction of meta attributes for logs.
-    manager.add("organizations:ourlogs-meta-attributes", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable our logs stats to be displayed in the UI.
     manager.add("organizations:ourlogs-stats", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable the visualize sidebar in the logs UI.
