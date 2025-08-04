9e9ef2c42128fecc9b19bf558add5381dd755ddd
Giancarlo Buenaflor <giancarlobuenaflor97@gmail.com>
2025-08-04 16:33:23 +0200

    Add support for dart symbol map

--- src/sentry/lang/dart/utils.py
+++ src/sentry/lang/dart/utils.py
@@ -1,13 +1,11 @@
 from __future__ import annotations
 
 import os
-import re
 from typing import Any
 
 import orjson
 import sentry_sdk
 
-from sentry.lang.java.utils import deobfuscation_template
 from sentry.models.debugfile import ProjectDebugFile
 from sentry.models.project import Project
 from sentry.utils.safe import get_path
@@ -16,102 +14,133 @@ from sentry.utils.safe import get_path
 # both "xyz" or "abc" need to be deobfuscated. It may also be possible for
 # the values to be more complicated such as "_xyz", so the regex should capture
 # any values other than "<" and ">".
-VIEW_HIERARCHY_TYPE_REGEX = re.compile(r"([^<>]+)(?:<([^<>]+)>)?")
-
-
-def is_valid_image(image):
-    return bool(image) and image.get("type") == "dart_symbols" and image.get("uuid") is not None
-
-
-def has_dart_symbols_file(data):
-    """
-    Checks whether an event contains a dart symbols file
-    """
-    images = get_path(data, "debug_meta", "images", filter=True)
-    return get_path(images, 0, "type") == "dart_symbols"
+# VIEW_HIERARCHY_TYPE_REGEX = re.compile(r"([^<>]+)(?:<([^<>]+)>)?")
 
 
 def get_dart_symbols_images(event: dict[str, Any]) -> set[str]:
     return {
-        str(image["uuid"]).lower()
-        for image in get_path(event, "debug_meta", "images", filter=is_valid_image, default=())
+        str(image["debug_id"]).lower()
+        for image in get_path(event, "debug_meta", "images", default=())
     }
 
 
-def generate_dart_symbols_map(uuid: str, project: Project):
+def generate_dart_symbols_map(debug_id: str, project: Project):
     """
-    NOTE: This function makes assumptions about the structure of the debug file
-    since we are not currently storing the file. This may need to be updated if we
-    decide to do some pre-processing on the debug file before storing it.
-
-    In its current state, the debug file is expected to be a json file with a list
-    of strings. The strings alternate between deobfuscated and obfuscated names.
-
-    If we preprocess it into a map, we can remove this code and just fetch the file.
+    Fetches and returns the dart symbols mapping for the given debug_id.
     """
-    obfuscated_to_deobfuscated_name_map = {}
-    with sentry_sdk.start_span(op="dart_symbols.generate_dart_symbols_map") as span:
+    with sentry_sdk.start_span(op="dartsymbolmap.generate_dart_symbols_map") as span:
         try:
-            dif_paths = ProjectDebugFile.difcache.fetch_difs(project, [uuid], features=["mapping"])
-            debug_file_path = dif_paths.get(uuid)
+            dif_paths = ProjectDebugFile.difcache.fetch_difs(
+                project, [debug_id], features=["mapping"]
+            )
+            debug_file_path = dif_paths.get(debug_id)
             if debug_file_path is None:
                 return
 
             dart_symbols_file_size_in_mb = os.path.getsize(debug_file_path) / (1024 * 1024.0)
-            span.set_tag("dart_symbols_file_size_in_mb", dart_symbols_file_size_in_mb)
+            span.set_tag("dartsymbolmap_file_size_in_mb", dart_symbols_file_size_in_mb)
 
             with open(debug_file_path, "rb") as f:
-                debug_array = orjson.loads(f.read())
+                data = orjson.loads(f.read())
 
-            if len(debug_array) % 2 != 0:
-                raise Exception("Debug array contains an odd number of elements")
-
-            # Obfuscated names are the odd indices and deobfuscated names are the even indices
-            obfuscated_to_deobfuscated_name_map = dict(zip(debug_array[1::2], debug_array[::2]))
+            if isinstance(data, dict):
+                # Map format
+                return data
+            elif isinstance(data, list):
+                # Array format - transform it to map
+                if len(data) % 2 != 0:
+                    raise Exception("Debug array contains an odd number of elements")
+                # Obfuscated names are the odd indices and deobfuscated names are the even indices
+                return dict(zip(data[1::2], data[::2]))
+            else:
+                raise Exception(f"Unexpected dartsymbolmap format: {type(data)}")
         except Exception as err:
             sentry_sdk.capture_exception(err)
             return
 
-    return obfuscated_to_deobfuscated_name_map
 
+def deobfuscate_exception_type(data: dict[str, Any]):
+    project = Project.objects.get_from_cache(id=data["project"])
 
-def _deobfuscate_view_hierarchy(event_data: dict[str, Any], project: Project, view_hierarchy):
-    """
-    Deobfuscates a view hierarchy in-place.
+    exceptions = data.get("exception", {}).get("values", [])
 
-    If we're unable to fetch a dart symbols uuid, then the view hierarchy remains unmodified.
-    """
-    dart_symbols_uuids = get_dart_symbols_images(event_data)
-    if len(dart_symbols_uuids) == 0:
+    debug_ids = get_dart_symbols_images(data)
+    if len(debug_ids) == 0:
         return
 
-    with sentry_sdk.start_span(op="dart_symbols.deobfuscate_view_hierarchy_data"):
-        for dart_symbols_uuid in dart_symbols_uuids:
-            map = generate_dart_symbols_map(dart_symbols_uuid, project)
+    with sentry_sdk.start_span(op="dartsymbolmap.deobfuscate_exception_type"):
+        if len(debug_ids) == 0:
+            return
+
+        exceptions = data.get("exception", {}).get("values", [])
+        if not exceptions:
+            return
+
+        # There should only be one mapping file per Flutter build so we can break out of the loop once we find it
+        found_mapping_file = False
+        for dart_symbols_debug_id in debug_ids:
+            if found_mapping_file:
+                break
+
+            map = generate_dart_symbols_map(dart_symbols_debug_id, project)
             if map is None:
-                return
+                continue
 
-            windows_to_deobfuscate = [*view_hierarchy.get("windows")]
-            while windows_to_deobfuscate:
-                window = windows_to_deobfuscate.pop()
+            found_mapping_file = True
 
-                if window.get("type") is None:
-                    # If there is no type, then skip this window
+            for exception in exceptions:
+                exception_type = exception.get("type")
+                if exception_type is None:
                     continue
 
-                matcher = re.match(VIEW_HIERARCHY_TYPE_REGEX, window.get("type"))
-                if not matcher:
+                obfuscated_symbol = exception["type"]
+                symbolicated_symbol = map[obfuscated_symbol]
+                exception["type"] = symbolicated_symbol
+
+                exception_value = exception.get("value")
+                if exception_value is None:
                     continue
-                obfuscated_values = matcher.groups()
-                for obfuscated_value in obfuscated_values:
-                    if obfuscated_value is not None and obfuscated_value in map:
-                        window["type"] = window["type"].replace(
-                            obfuscated_value, map[obfuscated_value]
-                        )
 
-                if children := window.get("children"):
-                    windows_to_deobfuscate.extend(children)
+                exception["value"] = exception_value.replace(obfuscated_symbol, symbolicated_symbol)
 
 
-def deobfuscate_view_hierarchy(data):
-    return deobfuscation_template(data, "dart_symbols", _deobfuscate_view_hierarchy)
+# TODO: Add this back in when we decide to deobfuscate view hierarchies
+# def _deobfuscate_view_hierarchy(event_data: dict[str, Any], project: Project, view_hierarchy):
+#     """
+#     Deobfuscates a view hierarchy in-place.
+
+#     If we're unable to fetch a dart symbols uuid, then the view hierarchy remains unmodified.
+#     """
+#     dart_symbols_uuids = get_dart_symbols_images(event_data)
+#     if len(dart_symbols_uuids) == 0:
+#         return
+
+#     with sentry_sdk.start_span(op="dartsymbolmap.deobfuscate_view_hierarchy_data"):
+#         for dart_symbols_uuid in dart_symbols_uuids:
+#             map = generate_dart_symbols_map(dart_symbols_uuid, project)
+#             if map is None:
+#                 return
+
+#             windows_to_deobfuscate = [*view_hierarchy.get("windows")]
+#             while windows_to_deobfuscate:
+#                 window = windows_to_deobfuscate.pop()
+
+#                 if window.get("type") is None:
+#                     # If there is no type, then skip this window
+#                     continue
+
+#                 matcher = re.match(VIEW_HIERARCHY_TYPE_REGEX, window.get("type"))
+#                 if not matcher:
+#                     continue
+#                 obfuscated_values = matcher.groups()
+#                 for obfuscated_value in obfuscated_values:
+#                     if obfuscated_value is not None and obfuscated_value in map:
+#                         window["type"] = window["type"].replace(
+#                             obfuscated_value, map[obfuscated_value]
+#                         )
+
+#                 if children := window.get("children"):
+#                     windows_to_deobfuscate.extend(children)
+
+# def deobfuscate_view_hierarchy(data):
+#     return deobfuscation_template(data, "dartsymbolmap", _deobfuscate_view_hierarchy)
