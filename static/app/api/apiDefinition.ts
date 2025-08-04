063bb8935f71490ac521acfaf25a83049f30537f
TkDodo <dominik.dorfmeister@sentry.io>
2025-08-04 20:13:09 +0200

    ref: remove unnecessary type gymnastics

--- static/app/api/apiDefinition.ts
+++ static/app/api/apiDefinition.ts
@@ -1,6 +1,3 @@
 type KnownApiUrls = ['/projects/$orgSlug/$projectSlug/releases/$releaseVersion/'];
 
-type ApiMapping = Record<KnownApiUrls[number], never>;
-type ApiPath = keyof ApiMapping;
-// adding a union with string & {} enables auto-completion
-export type MaybeApiPath = ApiPath | (string & {});
+export type MaybeApiPath = KnownApiUrls[number] | (string & {});
