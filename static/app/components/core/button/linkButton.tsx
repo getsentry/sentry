79597d77c5405535cae2d92d1ea8a00684d973a7
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2025-08-04 15:46:39 +0200

    fix(ui2): hover state for disabled buttons (#97064)
    in #95974, we wanted to make sure that disabled link buttons don't get
    hover styles applied; however, the fix being at the button styles
    introduced a slight hover effect for disabled, non-link buttons
    
    in this PR, we've reverted the fix from #95974 and instead applied the
    same thing directly on linkButton styles

--- static/app/components/core/button/linkButton.tsx
+++ static/app/components/core/button/linkButton.tsx
@@ -96,8 +96,15 @@ const StyledLinkButton = styled(
       (typeof prop === 'string' && isPropValid(prop)),
   }
 )<LinkButtonProps>`
-  ${p => (p.theme.isChonk ? getChonkButtonStyles(p as any) : getButtonStyles(p as any))}
+  ${p => (p.theme.isChonk ? getChonkLinkButtonStyles(p) : getButtonStyles(p as any))}
 `;
+
+const getChonkLinkButtonStyles = (p: LinkButtonProps) => {
+  return {
+    ...getChonkButtonStyles(p as any),
+    ...(p.disabled || p.busy ? {color: 'inherit'} : undefined),
+  };
+};
 
 const ButtonLabel = styled('span', {
   shouldForwardProp: prop =>
