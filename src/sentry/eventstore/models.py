6f1dcbd26dc42f245f52bf1c2c9a85165fa1fcc7
Charlie Luo <cvxluo@gmail.com>
2025-08-05 15:19:56 -0700

    use more precision in virtual breadcrumb

--- src/sentry/eventstore/models.py
+++ src/sentry/eventstore/models.py
@@ -110,7 +110,7 @@ class BaseEvent(metaclass=abc.ABCMeta):
 
     @property
     def datetime(self) -> datetime:
-        column = self._get_column_name(Columns.TIMESTAMP)
+        column = self._get_column_name(Columns.TIMESTAMP_MS)
         if column in self._snuba_data:
             return parse_date(self._snuba_data[column]).replace(tzinfo=timezone.utc)
 
