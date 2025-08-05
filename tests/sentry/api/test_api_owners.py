a811143c2558f8b51d720cb179d4a62face3a967
Athena Moghaddam <athena.moghaddam@sentry.io>
2025-08-05 09:38:27 -0700

    fix(apis): fix api owner test

--- tests/sentry/api/test_api_owners.py
+++ tests/sentry/api/test_api_owners.py
@@ -12,9 +12,8 @@ class APIOwnersTestCase(TestCase):
         lines = code_owners_file.readlines()
         code_owners_file.close()
         for line in lines:
-            if line.startswith("/src/"):
-                tokens = [s.strip() for s in line.split("@getsentry/")]
-                self.teams.update(tokens[1:])
+            tokens = [s.strip() for s in line.split("@getsentry/")]
+            self.teams.update(tokens[1:])
 
     def test_api_owner_owns_api(self) -> None:
         for owner in ApiOwner:
