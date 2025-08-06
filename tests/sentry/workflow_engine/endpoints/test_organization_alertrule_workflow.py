9bca0e492f896350cf95f490ee3411e8b058b443
Mia Hsu <ameliahsu@gmail.com>
2025-08-06 16:53:13 -0700

    fix tests

--- tests/sentry/workflow_engine/endpoints/test_organization_alertrule_workflow.py
+++ tests/sentry/workflow_engine/endpoints/test_organization_alertrule_workflow.py
@@ -42,7 +42,6 @@ class OrganizationAlertRuleWorkflowIndexGetTest(OrganizationAlertRuleWorkflowAPI
 
     def test_get_with_alert_rule_id_filter(self) -> None:
         response = self.get_success_response(self.organization.slug, alert_rule_id="12345")
-        assert response.data == serialize(self.alert_rule_workflow_1, self.user)
 
         assert response.data["alertRuleId"] == "12345"
         assert response.data["ruleId"] is None
@@ -50,11 +49,19 @@ class OrganizationAlertRuleWorkflowIndexGetTest(OrganizationAlertRuleWorkflowAPI
 
     def test_get_with_rule_id_filter(self) -> None:
         response = self.get_success_response(self.organization.slug, rule_id="67890")
-        assert response.data == serialize(self.alert_rule_workflow_2, self.user)
 
         assert response.data["ruleId"] == "67890"
         assert response.data["alertRuleId"] is None
         assert response.data["workflowId"] == str(self.workflow_2.id)
+
+    def test_get_with_multiple_filters(self) -> None:
+        response = self.get_success_response(
+            self.organization.slug,
+            workflow_id=str(self.workflow_1.id),
+            alert_rule_id="12345",
+        )
+
+        assert response.data == serialize(self.alert_rule_workflow_1, self.user)
 
     def test_get_with_nonexistent_workflow_id(self) -> None:
         self.get_error_response(self.organization.slug, workflow_id="99999", status_code=404)
@@ -64,13 +71,6 @@ class OrganizationAlertRuleWorkflowIndexGetTest(OrganizationAlertRuleWorkflowAPI
 
     def test_get_with_nonexistent_rule_id(self) -> None:
         self.get_error_response(self.organization.slug, rule_id="99999", status_code=404)
-
-    def test_get_with_multiple_filters(self) -> None:
-        self.get_error_response(
-            self.organization.slug,
-            workflow_id=str(self.workflow_1.id),
-            alert_rule_id="12345",
-        )
 
     def test_organization_isolation(self) -> None:
         self.get_error_response(
