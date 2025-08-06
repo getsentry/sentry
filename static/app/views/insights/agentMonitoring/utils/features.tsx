365210436437f80d19dff922e68ac1376e51cefb
Ogi <ognjen.bostjancic@gmail.com>
2025-08-06 12:58:26 +0200

    chore(agents-insights): remove llm monitoring (#97154)

--- static/app/views/insights/agentMonitoring/utils/features.tsx
+++ static/app/views/insights/agentMonitoring/utils/features.tsx
@@ -1,9 +1,6 @@
 import Feature from 'sentry/components/acl/feature';
 import {NoAccess} from 'sentry/components/noAccess';
 import type {Organization} from 'sentry/types/organization';
-import useMutateUserOptions from 'sentry/utils/useMutateUserOptions';
-import useOrganization from 'sentry/utils/useOrganization';
-import {useUser} from 'sentry/utils/useUser';
 
 type InsightsFeaturePropsWithoutFeatures = Omit<
   Parameters<typeof Feature>[0],
@@ -18,38 +15,10 @@ export function hasMCPInsightsFeature(organization: Organization) {
   return organization.features.includes('mcp-insights');
 }
 
-export function usePreferedAiModule() {
-  const organization = useOrganization();
-  const user = useUser();
-
-  if (!hasAgentInsightsFeature(organization)) {
-    return 'llm-monitoring';
-  }
-
-  return user.options.prefersAgentsInsightsModule ? 'agents-insights' : 'llm-monitoring';
-}
-
-export function useTogglePreferedAiModule(): [string, () => void] {
-  const preferedAiModule = usePreferedAiModule();
-  const {mutate: mutateUserOptions} = useMutateUserOptions();
-
-  const togglePreferedModule = () => {
-    const prefersAgentsInsightsModule = preferedAiModule === 'agents-insights';
-    const newPrefersAgentsInsightsModule = !prefersAgentsInsightsModule;
-    mutateUserOptions({
-      ['prefersAgentsInsightsModule']: newPrefersAgentsInsightsModule,
-    });
-  };
-
-  return [preferedAiModule, togglePreferedModule];
-}
-
 export function AIInsightsFeature(props: InsightsFeaturePropsWithoutFeatures) {
-  const preferedAiModule = usePreferedAiModule();
-
   return (
     <Feature
-      features={preferedAiModule}
+      features={['agents-insights']}
       renderDisabled={props.renderDisabled ?? NoAccess}
     >
       {props.children}
