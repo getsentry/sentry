3a344bce5c7f73dba3de08460fdae3956f7d4765
Simon Hellmayr <shellmayr@users.noreply.github.com>
2025-08-06 10:14:06 +0200

    chore(dynamic-sampling): add logging for project configs (#97160)

--- src/sentry/relay/config/__init__.py
+++ src/sentry/relay/config/__init__.py
@@ -1138,6 +1138,33 @@ def _get_project_config(
         if quotas_config := get_quotas(project, keys=project_keys):
             config["quotas"] = quotas_config
 
+    if features.has("organizations:log-project-config", project.organization):
+        try:
+            logger.info(
+                "Logging project config for project %s in org %s.",
+                project.id,
+                project.organization.id,
+                extra={
+                    "project_config": config,
+                    "project_id": str(project.id),
+                    "org_id": str(project.organization.id),
+                    "dynamic_sampling_feature_flag": features.has(
+                        "organizations:dynamic-sampling", project.organization
+                    ),
+                    "dynamic_sampling_custom_feature_flag": features.has(
+                        "organizations:dynamic-sampling-custom", project.organization
+                    ),
+                    "dynamic_sampling_mode": project.organization.get_option(
+                        "sentry:dynamic_sampling_mode", None
+                    ),
+                    "dynamic_sampling_org_target_rate": project.organization.get_option(
+                        "sentry:target_sample_rate", None
+                    ),
+                },
+            )
+        except Exception:
+            capture_exception()
+
     return ProjectConfig(project, **cfg)
 
 
