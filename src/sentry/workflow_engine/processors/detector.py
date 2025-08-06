403e3e335ffaf73caea146960f56e75ab8de5cf3
Raj Joshi <raj.joshi@sentry.io>
2025-08-06 16:39:35 -0700

    :bug: fix: nit

--- src/sentry/workflow_engine/processors/detector.py
+++ src/sentry/workflow_engine/processors/detector.py
@@ -207,7 +207,7 @@ def create_issue_platform_payload(result: DetectorEvaluationResult, detector_typ
         metrics.incr(
             "workflow_engine.issue_platform.payload.sent.occurrence",
             tags={"detector_type": detector_type},
-            sample_rate=0.01,
+            sample_rate=1,
         )
     else:
         status_change = result.result
@@ -227,9 +227,11 @@ def create_issue_platform_payload(result: DetectorEvaluationResult, detector_typ
 
 
 @sentry_sdk.trace
-def process_detectors[T](
-    data_packet: DataPacket[T], detectors: list[Detector]
-) -> list[tuple[Detector, dict[DetectorGroupKey, DetectorEvaluationResult]]]:
+def process_detectors[
+    T
+](data_packet: DataPacket[T], detectors: list[Detector]) -> list[
+    tuple[Detector, dict[DetectorGroupKey, DetectorEvaluationResult]]
+]:
     results: list[tuple[Detector, dict[DetectorGroupKey, DetectorEvaluationResult]]] = []
 
     for detector in detectors:
