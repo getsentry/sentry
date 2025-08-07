2d79a0c9c3659f105daf97a56303b33f6a215c10
Kyle Consalus <kyle.consalus@sentry.io>
2025-08-07 11:14:08 -0700

    fix(aci): Monitor alert trigger latency (#97353)
    Add metrics to track time from event to alert for the rules and workflow
    execution implementatinos.

--- src/sentry/rules/processing/delayed_processing.py
+++ src/sentry/rules/processing/delayed_processing.py
@@ -50,6 +50,7 @@ from sentry.taskworker.config import TaskworkerConfig
 from sentry.taskworker.namespaces import issues_tasks
 from sentry.taskworker.retry import Retry
 from sentry.utils import json, metrics
+from sentry.utils.dates import ensure_aware
 from sentry.utils.iterators import chunked
 from sentry.utils.retries import ConditionalRetryPolicy, exponential_delay
 from sentry.utils.safe import safe_execute
@@ -573,6 +574,13 @@ def fire_rules(
 
                     notification_uuid = str(uuid.uuid4())
                     groupevent = group_to_groupevent[group]
+                    metrics.timing(
+                        "rule_fire_history.latency",
+                        (
+                            datetime.now(tz=timezone.utc) - ensure_aware(groupevent.datetime)
+                        ).total_seconds(),
+                        tags={"delayed": True, "group_type": group.issue_type.slug},
+                    )
                     rule_fire_history = history.record(
                         rule, group, groupevent.event_id, notification_uuid
                     )
