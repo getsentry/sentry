0e9c2fc02f0d8d93637d6eca2e50b375ba3a9c6c
Joshua Ferge <josh.ferge@sentry.io>
2025-08-07 16:30:58 -0400

    fix: Complete type annotations for moved test files

--- tests/sentry/issues/endpoints/test_group_attachments.py
+++ tests/sentry/issues/endpoints/test_group_attachments.py
@@ -1,3 +1,4 @@
+from collections.abc import Sequence
 from urllib.parse import urlencode
 
 from sentry.models.eventattachment import EventAttachment
@@ -10,8 +11,12 @@ pytestmark = [requires_snuba]
 
 class GroupEventAttachmentsTest(APITestCase):
     def create_attachment(
-        self, type=None, event_id=None, file_name="hello.png", group_id=None
-    ) -> None:
+        self,
+        type: str | None = None,
+        event_id: str | None = None,
+        file_name: str = "hello.png",
+        group_id: int | None = None,
+    ) -> EventAttachment:
         if type is None:
             type = "event.attachment"
 
@@ -26,7 +31,12 @@ class GroupEventAttachmentsTest(APITestCase):
 
         return self.attachment
 
-    def path(self, types=None, event_ids=None, screenshot=False) -> str:
+    def path(
+        self,
+        types: Sequence[str] | None = None,
+        event_ids: Sequence[str] | None = None,
+        screenshot: bool = False,
+    ) -> str:
         path = f"/api/0/issues/{self.group.id}/attachments/"
 
         query = [("types", t) for t in types or ()]
