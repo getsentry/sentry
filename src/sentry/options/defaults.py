865662d8772fa6aa7cac1ea3053fb58edeb9af78
Charlie Luo <cvxluo@gmail.com>
2025-08-07 16:01:27 -0700

    remove option for using snuba data for access logs

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3431,8 +3431,6 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
-# Enable enhancing access logs with snuba responses
-register("issues.use-snuba-error-data", type=Float, default=0.0, flags=FLAG_AUTOMATOR_MODIFIABLE)
 
 # Use "first-seen" group instead of "most-seen" group when merging
 register(
