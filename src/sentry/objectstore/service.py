403712cb2cc21d871bcceffa99c28b9bcac91e5c
Arpad Borsos <arpad.borsos@sentry.io>
2025-08-07 10:29:43 +0200

    Implement a draft StorageClient API (#96076)
    This implements a client-side API for our now blobstore initiative.
    
    It already offers a scoped API, which requires providing a scope (the
    organization/project) for each access. The API also offers a way to
    uploading already-compressed, or uncompressible data, and to do
    transparent decompression on read (or not if so desired).
    
    This should give us a starting point to discuss the client-side API.
    
    ---
    
    REF FS-61
    REF FS-64

---
+++ src/sentry/objectstore/service.py
@@ -0,0 +1,197 @@
+from __future__ import annotations
+
+from io import BytesIO
+from typing import IO, Literal, NamedTuple, NotRequired, Self, TypedDict, cast
+
+import urllib3
+import zstandard
+from django.utils import timezone
+from urllib3.connectionpool import HTTPConnectionPool
+
+from sentry.objectstore.metadata import (
+    HEADER_EXPIRATION,
+    HEADER_META_PREFIX,
+    Compression,
+    ExpirationPolicy,
+    Metadata,
+    format_expiration,
+)
+from sentry.objectstore.metrics import measure_storage_put
+from sentry.utils import jwt, metrics
+
+Permission = Literal["read", "write"]
+
+JWT_VALIDITY_SECS = 30
+
+
+class Scope(TypedDict):
+    organization: int
+    project: NotRequired[int]
+
+
+class GetResult(NamedTuple):
+    metadata: Metadata
+    payload: IO[bytes]
+
+
+class ClientBuilder:
+    def __init__(self, usecase: str, options: dict | None = None):
+        self._usecase = usecase
+        self._options = options
+        self._default_compression: Compression = "zstd"
+
+    def _make_client(self, scope: Scope) -> Client:
+        from sentry import options as options_store
+
+        options = self._options or options_store.get("objectstore.config")
+        pool = urllib3.connectionpool.connection_from_url(options["base_url"])
+        jwt_secret = options["jwt_secret"]
+
+        claim = {
+            "usecase": self._usecase,
+            "scope": scope,
+        }
+
+        return Client(pool, self._default_compression, self._usecase, claim, jwt_secret)
+
+    def default_compression(self, default_compression: Compression) -> Self:
+        self._default_compression = default_compression
+        return self
+
+    def for_organization(self, organization_id: int) -> Client:
+        return self._make_client({"organization": organization_id})
+
+    def for_project(self, organization_id: int, project_id: int) -> Client:
+        return self._make_client({"organization": organization_id, "project": project_id})
+
+
+class Client:
+    def __init__(
+        self,
+        pool: HTTPConnectionPool,
+        default_compression: Compression,
+        usecase: str,
+        claim: dict,
+        jwt_secret: str,
+    ):
+        self._pool = pool
+        self._default_compression = default_compression
+        self._usecase = usecase
+        self._jwt_secret = jwt_secret
+        self._claim = claim
+
+    def _make_headers(self, permission: Permission) -> dict:
+        now = int(timezone.now().timestamp())
+        exp = now + JWT_VALIDITY_SECS
+        claims = {
+            "iat": now,
+            "exp": exp,
+            **self._claim,
+            "permissions": [permission],
+        }
+
+        authorization = jwt.encode(claims, self._jwt_secret)
+        return {"Authorization": authorization}
+
+    def put(
+        self,
+        contents: bytes | IO[bytes],
+        id: str | None = None,
+        compression: Compression | Literal["none"] | None = None,
+        metadata: dict[str, str] | None = None,
+        expiration_policy: ExpirationPolicy | None = None,
+    ) -> str:
+        """
+        Uploads the given `contents` to blob storage.
+
+        If no `id` is provided, one will be automatically generated and returned
+        from this function.
+
+        The client will select the configured `default_compression` if none is given
+        explicitly.
+        This can be overridden by explicitly giving a `compression` argument.
+        Providing `"none"` as the argument will instruct the client to not apply
+        any compression to this upload, which is useful for uncompressible formats.
+        """
+        headers = self._make_headers("write")
+        body = BytesIO(contents) if isinstance(contents, bytes) else contents
+        original_body: IO[bytes] = body
+
+        compression = compression or self._default_compression
+        if compression == "zstd":
+            cctx = zstandard.ZstdCompressor()
+            body = cctx.stream_reader(original_body)
+            headers["Content-Encoding"] = "zstd"
+
+        if expiration_policy:
+            headers[HEADER_EXPIRATION] = format_expiration(expiration_policy)
+
+        if metadata:
+            for k, v in metadata.items():
+                headers[f"{HEADER_META_PREFIX}{k}"] = v
+
+        with measure_storage_put(None, self._usecase, compression) as measurement:
+            response = self._pool.request(
+                "PUT",
+                f"/{id}" if id else "/",
+                body=body,
+                headers=headers,
+                preload_content=True,
+                decode_content=True,
+            ).json()
+
+            measurement.upload_size = body.tell()
+            if compression != "none":
+                metrics.distribution(
+                    "storage.put.size",
+                    original_body.tell(),
+                    tags={"usecase": self._usecase, "compression": "none"},
+                    unit="byte",
+                )
+
+            return response["key"]
+
+    def get(self, id: str, decompress: bool = True) -> GetResult:
+        """
+        This fetches the blob with the given `id`, returning an `IO` stream that
+        can be read.
+
+        By default, content that was uploaded compressed will be automatically
+        decompressed, unless `decompress=True` is passed.
+        """
+        headers = self._make_headers("read")
+
+        response = self._pool.request(
+            "GET",
+            f"/{id}",
+            headers=headers,
+            preload_content=False,
+            decode_content=False,
+        )
+        # OR: should I use `response.stream()`?
+        stream = cast(IO[bytes], response)
+        metadata = Metadata.from_headers(response.headers)
+
+        if metadata.compression and decompress:
+            if metadata.compression != "zstd":
+                raise NotImplementedError(
+                    "Transparent decoding of anything but `zstd` is not implemented yet"
+                )
+
+            metadata.compression = None
+            dctx = zstandard.ZstdDecompressor()
+            stream = dctx.stream_reader(stream, read_across_frames=True)
+
+        return GetResult(metadata, stream)
+
+    def delete(self, id: str):
+        """
+        Deletes the blob with the given `id`.
+        """
+        headers = self._make_headers("write")
+
+        self._pool.request(
+            "DELETE",
+            f"/{id}",
+            headers=headers,
+        )
