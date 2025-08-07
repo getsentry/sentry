8c26d5771d328ee247f27fe92cace184712958ce
Charlie Luo <cvxluo@gmail.com>
2025-08-07 08:44:57 -0700

    feat(issues): use more precision in virtual breadcrumb (#97235)
    We construct a virtual breadcrumb based on the event that triggered an
    issue, but it is missing millisecond precision. With the work on adding
    `timestamp_ms` as a column on errors, we already have millisecond
    precision, and can present it to the user.
    
    Old:
    <img width="866" height="48" alt="Screenshot 2025-08-05 at 3 27 58 PM"
    src="https://github.com/user-attachments/assets/64ff53b3-9cb9-4dc2-a76c-47b310459ba2"
    />
    
    New:
    <img width="912" height="55" alt="Screenshot 2025-08-05 at 3 35 17 PM"
    src="https://github.com/user-attachments/assets/75f304f8-04b5-4cfb-9c45-181d87a8602d"
    />
    
    Closes
    https://linear.app/getsentry/issue/ID-603/precise-error-ordering-on-breadcrumbs-queries
    
    Side note: we're planning to add ms precision to occurrences, which
    should be the only case that does not have ms precision that also uses
    this field, so once we do that we can just use TIMESTAMP_MS here, and
    not fall back to TIMESTAMP.

--- src/sentry/eventstore/models.py
+++ src/sentry/eventstore/models.py
@@ -110,6 +110,12 @@ class BaseEvent(metaclass=abc.ABCMeta):
 
     @property
     def datetime(self) -> datetime:
+        # If we have millisecond precision timestamps, use them
+        column = self._get_column_name(Columns.TIMESTAMP_MS)
+        if column in self._snuba_data and self._snuba_data[column]:
+            return parse_date(self._snuba_data[column]).replace(tzinfo=timezone.utc)
+
+        # Otherwise, use the second precision timestamp
         column = self._get_column_name(Columns.TIMESTAMP)
         if column in self._snuba_data:
             return parse_date(self._snuba_data[column]).replace(tzinfo=timezone.utc)
