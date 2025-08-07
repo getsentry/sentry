628ab8b51009e8777ceb92b08fac92f80aa93a28
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-08-07 13:09:32 -0700

    fix(workflow_engine): Reduce number of queries when evaluating detectors (#97337)
    # Description
    We only need to lookup the thresholds on detectors that aren't fully
    decorated. Currently we fetch these conditions each time.
    
    This PR will check to see if the model has the associated data, if it
    doesn't the query will still happen. However, if the detector was
    fetched using `process_data_source` we can skip this second query and
    access the conditions directly.

--- tests/sentry/workflow_engine/handlers/detector/test_stateful.py
+++ tests/sentry/workflow_engine/handlers/detector/test_stateful.py
@@ -3,7 +3,7 @@ import unittest.mock as mock
 from sentry.issues.issue_occurrence import IssueOccurrence
 from sentry.issues.status_change_message import StatusChangeMessage
 from sentry.testutils.cases import TestCase
-from sentry.workflow_engine.models import DataPacket
+from sentry.workflow_engine.models import DataPacket, Detector
 from sentry.workflow_engine.types import DetectorGroupKey, DetectorPriorityLevel
 from tests.sentry.workflow_engine.handlers.detector.test_base import MockDetectorStateHandler
 
@@ -16,6 +16,21 @@ class TestStatefulDetectorHandler(TestCase):
             name="Stateful Detector",
             project=self.project,
         )
+
+    def _get_full_detector(self) -> Detector:
+        """
+        Fetches the full detector with its workflow condition group and conditions.
+        This is useful to ensure that the detector is fully populated for testing.
+        """
+        detector = (
+            Detector.objects.filter(id=self.detector.id)
+            .select_related("workflow_condition_group")
+            .prefetch_related("workflow_condition_group__conditions")
+            .first()
+        )
+
+        assert detector is not None
+        return detector
 
     def test__init_creates_default_thresholds(self) -> None:
         handler = MockDetectorStateHandler(detector=self.detector)
@@ -34,6 +49,51 @@ class TestStatefulDetectorHandler(TestCase):
     def test_init__creates_correct_state_counters(self) -> None:
         handler = MockDetectorStateHandler(detector=self.detector)
         assert handler.state_manager.counter_names == [Level.OK]
+
+    def test_init__threshold_query(self) -> None:
+        self.detector.workflow_condition_group = self.create_data_condition_group()
+        self.detector.save()
+
+        self.create_data_condition(
+            type="eq",
+            comparison="HIGH",
+            condition_group=self.detector.workflow_condition_group,
+            condition_result=Level.HIGH,
+        )
+
+        fetched_detector = self._get_full_detector()
+
+        with self.assertNumQueries(0):
+            handler = MockDetectorStateHandler(detector=fetched_detector)
+            assert handler._thresholds == {Level.OK: 1, Level.HIGH: 1}
+
+    def test_init__threshold_query_no_conditions(self) -> None:
+        self.detector.workflow_condition_group = self.create_data_condition_group()
+        self.detector.save()
+
+        fetched_detector = self._get_full_detector()
+
+        with self.assertNumQueries(0):
+            handler = MockDetectorStateHandler(detector=fetched_detector)
+            assert handler._thresholds == {Level.OK: 1}
+
+    def test_init__threshold_makes_query(self) -> None:
+        self.detector.workflow_condition_group = self.create_data_condition_group()
+        self.detector.save()
+
+        self.create_data_condition(
+            type="eq",
+            comparison="HIGH",
+            condition_group=self.detector.workflow_condition_group,
+            condition_result=Level.HIGH,
+        )
+
+        fetched_detector = Detector.objects.get(id=self.detector.id)
+
+        with self.assertNumQueries(1):
+            # Should make a query since we don't know the detector conditions
+            handler = MockDetectorStateHandler(detector=fetched_detector)
+            assert handler._thresholds == {Level.OK: 1, Level.HIGH: 1}
 
 
 class TestStatefulDetectorIncrementThresholds(TestCase):
