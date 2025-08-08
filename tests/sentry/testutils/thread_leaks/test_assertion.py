f0810ff7225c023ce6a8745ebcfe2ade82bf9cc3
Buck Evan <buck.evan@sentry.io>
2025-08-08 10:53:12 -0500

    fixy testy

--- tests/sentry/testutils/thread_leaks/test_assertion.py
+++ tests/sentry/testutils/thread_leaks/test_assertion.py
@@ -1,18 +1,12 @@
 """Integration tests for thread leak assertion utilities."""
 
+import re
+import sys
 from threading import Event, Thread
 
 import pytest
 
 from sentry.testutils.thread_leaks.assertion import ThreadLeakAssertionError, assert_none
-
-
-def norm(stack_diff: object):
-    """remove some meaningless uniqueness from the output."""
-    import re
-
-    stack_diff = str(stack_diff).replace("# noqa", "")
-    return re.sub("[0-9]+", "$N", stack_diff)
 
 
 class TestAssertNoneIntegration:
@@ -35,18 +29,24 @@ class TestAssertNoneIntegration:
             stop.set()
             thread.join()
 
+        stack_diff = str(exc_info.value)
+        sys.stdout.write(f"ORIG: {stack_diff}\n")
+
+        # all of the numbers are effectively random
+        stack_diff = re.sub("[0-9]+", "$N", stack_diff)
+        assert "\n  <_MainThread(MainThread, started $N)>@None\n" in stack_diff
+
+        # Remove context lines, containing thread leaks from other tests
+        stack_diff = re.sub("^ .*\n", "", stack_diff, flags=re.MULTILINE)
+
         # Verify the error message contains useful debugging info
-        assert norm(exc_info.value) == norm(
-            """
-  <Thread(Thread-1 (run_server), started daemon 6107525120)>@pytest_rerunfailures.ServerStatusDB.run_server
-  # noqa
-+ <Thread(Thread-2 (wait), started daemon 6124351488)>@threading.Event.wait
-+     File "./tests/sentry/testutils/thread_leaks/test_assertion.py", line 26, in test_thread_leak_strict_mode_raises
+        assert (
+            stack_diff
+            == """
++ <Thread(Thread-$N (wait), started daemon $N)>@threading.Event.wait
++     File "./tests/sentry/testutils/thread_leaks/test_assertion.py", line $N, in test_thread_leak_strict_mode_raises
 +       thread = Thread(target=stop.wait, daemon=True)
-+ # noqa
-  <_MainThread(MainThread, started 8767659776)>@None
-  # noqa
-"""
++ \n"""  # note: that's a load-bearing whitespace!
         )
 
     def test_thread_that_exits_during_context_passes(self):
