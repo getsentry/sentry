c3318fbb39bfa3152a34e5f83c8298de95c1e083
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-08-08 14:57:39 -0700

    combine files

--- static/app/components/replays/table/replayTableHeader.tsx
+++ static/app/components/replays/table/replayTableHeader.tsx
@@ -1,11 +1,18 @@
 import {Fragment} from 'react';
+import styled from '@emotion/styled';
 
-import type {ReplayTableColumn} from 'sentry/components/replays/table/replayTableColumns';
-import ReplayTableHeaderActions from 'sentry/components/replays/table/replayTableHeaderActions';
-import ReplayTableSelectionBanner from 'sentry/components/replays/table/replayTableSelectionBanner';
+import {Alert} from 'sentry/components/core/alert';
+import {Flex} from 'sentry/components/core/layout/flex';
+import DeleteReplays from 'sentry/components/replays/table/deleteReplays';
+import {
+  ReplaySelectColumn,
+  type ReplayTableColumn,
+} from 'sentry/components/replays/table/replayTableColumns';
 import {SimpleTable} from 'sentry/components/tables/simpleTable';
+import {t, tct, tn} from 'sentry/locale';
 import type {Sort} from 'sentry/utils/discover/fields';
 import {useListItemCheckboxContext} from 'sentry/utils/list/useListItemCheckboxState';
+import {parseQueryKey} from 'sentry/utils/queryClient';
 import type {ReplayListRecord} from 'sentry/views/replays/types';
 
 type Props = {
@@ -17,10 +24,14 @@ type Props = {
 
 export default function ReplayTableHeader({columns, replays, onSortClick, sort}: Props) {
   const listItemCheckboxState = useListItemCheckboxContext();
+  const {countSelected, isAllSelected, isAnySelected, queryKey, selectAll, selectedIds} =
+    listItemCheckboxState;
+  const queryOptions = parseQueryKey(queryKey).options;
+  const queryString = queryOptions?.query?.query;
 
   return (
     <Fragment>
-      <SimpleTable.Header style={{gridRow: '1'}}>
+      <TableHeader>
         {columns.map(({Header, sortKey}, columnIndex) => (
           <SimpleTable.HeaderCell
             key={`${sortKey}-${columnIndex}`}
@@ -34,19 +45,85 @@ export default function ReplayTableHeader({columns, replays, onSortClick, sort}:
               : Header}
           </SimpleTable.HeaderCell>
         ))}
-      </SimpleTable.Header>
+      </TableHeader>
 
-      {listItemCheckboxState.isAnySelected ? (
-        <ReplayTableHeaderActions
-          listItemCheckboxState={listItemCheckboxState}
-          replays={replays}
-        />
+      {isAnySelected ? (
+        <TableHeader>
+          <TableCellFirst>
+            <ReplaySelectColumn.Header
+              columnIndex={0}
+              listItemCheckboxState={listItemCheckboxState}
+              replays={replays}
+            />
+          </TableCellFirst>
+          <TableCellsRemaining>
+            <DeleteReplays
+              queryOptions={queryOptions}
+              replays={replays}
+              selectedIds={selectedIds}
+            />
+          </TableCellsRemaining>
+        </TableHeader>
       ) : null}
 
-      <ReplayTableSelectionBanner
-        listItemCheckboxState={listItemCheckboxState}
-        replays={replays}
-      />
+      {isAllSelected === 'indeterminate' ? (
+        <FullGridAlert type="warning" system>
+          <Flex justify="center" wrap="wrap" gap="md">
+            {tn(
+              'Selected %s visible replay.',
+              'Selected %s visible replays.',
+              countSelected
+            )}
+            <a onClick={selectAll}>
+              {queryString
+                ? tct('Select all replays that match: [queryString].', {
+                    queryString: <var>{queryString}</var>,
+                  })
+                : t('Select all replays.')}
+            </a>
+          </Flex>
+        </FullGridAlert>
+      ) : null}
+
+      {isAllSelected === true ? (
+        <FullGridAlert type="warning" system>
+          <Flex justify="center" wrap="wrap">
+            <span>
+              {queryString
+                ? tct('Selected all replays matching: [queryString].', {
+                    queryString: <var>{queryString}</var>,
+                  })
+                : countSelected > replays.length
+                  ? t('Selected all %s+ replays.', replays.length)
+                  : tn(
+                      'Selected all %s replay.',
+                      'Selected all %s replays.',
+                      countSelected
+                    )}
+            </span>
+          </Flex>
+        </FullGridAlert>
+      ) : null}
     </Fragment>
   );
 }
+
+const TableHeader = styled(SimpleTable.Header)`
+  grid-row: 1;
+  z-index: ${p => p.theme.zIndex.initial};
+`;
+
+const TableCellFirst = styled(SimpleTable.HeaderCell)`
+  grid-column: 1;
+`;
+
+const TableCellsRemaining = styled('div')`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  grid-column: 2 / -1;
+`;
+
+const FullGridAlert = styled(Alert)`
+  grid-column: 1 / -1;
+`;
