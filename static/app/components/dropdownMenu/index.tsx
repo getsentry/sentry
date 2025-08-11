a3957b26681fd4adbf32e34640624d3141d270ba
lzhao-sentry <lucy.zhao@sentry.io>
2025-08-11 12:16:52 -0400

    chore(cell-actions): use popper strategy fixed instead of portalling

--- static/app/components/dropdownMenu/index.tsx
+++ static/app/components/dropdownMenu/index.tsx
@@ -68,6 +68,7 @@ export interface DropdownMenuProps
       | 'preventOverflowOptions'
       | 'flipOptions'
       | 'shouldApplyMinWidth'
+      | 'strategy'
     > {
   /**
    * Items to display inside the dropdown menu. If the item has a `children`
@@ -129,6 +130,8 @@ export interface DropdownMenuProps
    * only be enabled if necessary, e.g. when the dropdown menu is inside a small,
    * scrollable container that messes with the menu's position. Some features, namely
    * submenus, will not work correctly inside portals.
+   *
+   * Consider passing `strategy` as `'fixed'` before using `usePortal`
    */
   usePortal?: boolean;
 }
@@ -163,6 +166,7 @@ function DropdownMenu({
   portalContainerRef,
   shouldApplyMinWidth,
   minMenuWidth,
+  strategy,
   ...props
 }: DropdownMenuProps) {
   const isDisabled = disabledProp ?? (!items || items.length === 0);
@@ -188,6 +192,7 @@ function DropdownMenu({
     flipOptions,
     onOpenChange,
     shouldApplyMinWidth,
+    strategy,
   });
 
   const {menuTriggerProps, menuProps} = useMenuTrigger(
