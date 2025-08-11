0a977eb1d9cff079b31c0e693c3e405b9439d01b
isabellaenriquez <isabella.enriquez@sentry.io>
2025-08-11 17:44:36 -0400

    feat(checkout v3): Product select

--- static/gsApp/views/amCheckout/steps/planSelect.tsx
+++ static/gsApp/views/amCheckout/steps/planSelect.tsx
@@ -129,6 +129,7 @@ function PlanSelect({
   subscription,
   formData,
   referrer,
+  isNewCheckout,
   onEdit,
   onToggleLegacy,
   onUpdate,
@@ -330,7 +331,12 @@ function PlanSelect({
       />
       {isActive && renderBody()}
       {isActive && (
-        <ProductSelect activePlan={activePlan} formData={formData} onUpdate={onUpdate} />
+        <ProductSelect
+          activePlan={activePlan}
+          formData={formData}
+          onUpdate={onUpdate}
+          isNewCheckout={isNewCheckout}
+        />
       )}
       {isActive && renderFooter()}
     </Panel>
