d3ec63cdeccbdf2a3365dadf051fa2549f4b3ea8
Vishnu Satish <vishnu.satish@sentry.io>
2025-08-11 14:01:08 -0700

    remove backend changes oops

--- src/sentry/feedback/endpoints/organization_feedback_categories.py
+++ src/sentry/feedback/endpoints/organization_feedback_categories.py
@@ -132,14 +132,13 @@ class OrganizationFeedbackCategoriesEndpoint(OrganizationEndpoint):
 
         categories_cache = cache.get(categorization_cache_key)
         if categories_cache:
-            # return Response(
-            #     {
-            #         "categories": categories_cache["categories"],
-            #         "success": True,
-            #         "numFeedbacksContext": categories_cache["numFeedbacksContext"],
-            #     }
-            # )
-            pass
+            return Response(
+                {
+                    "categories": categories_cache["categories"],
+                    "success": True,
+                    "numFeedbacksContext": categories_cache["numFeedbacksContext"],
+                }
+            )
 
         recent_feedbacks = query_recent_feedbacks_with_ai_labels(
             organization_id=organization.id,
@@ -151,13 +150,13 @@ class OrganizationFeedbackCategoriesEndpoint(OrganizationEndpoint):
 
         if len(recent_feedbacks) < MIN_FEEDBACKS_CONTEXT:
             logger.error("Too few feedbacks to generate categories")
-            # return Response(
-            #     {
-            #         "categories": None,
-            #         "success": False,
-            #         "numFeedbacksContext": 0,
-            #     }
-            # )
+            return Response(
+                {
+                    "categories": None,
+                    "success": False,
+                    "numFeedbacksContext": 0,
+                }
+            )
 
         context_feedbacks = []
         total_chars = 0
--- src/sentry/feedback/endpoints/organization_feedback_summary.py
+++ src/sentry/feedback/endpoints/organization_feedback_summary.py
@@ -114,13 +114,13 @@ class OrganizationFeedbackSummaryEndpoint(OrganizationEndpoint):
 
         if groups.count() < MIN_FEEDBACKS_TO_SUMMARIZE:
             logger.error("Too few feedbacks to summarize")
-            # return Response(
-            #     {
-            #         "summary": None,
-            #         "success": False,
-            #         "numFeedbacksUsed": 0,
-            #     }
-            # )
+            return Response(
+                {
+                    "summary": None,
+                    "success": False,
+                    "numFeedbacksUsed": 0,
+                }
+            )
 
         # Also cap the number of characters that we send to the LLM
         group_feedbacks = []
@@ -184,9 +184,6 @@ def make_seer_request(request: SummaryRequest) -> bytes:
             },
         )
 
-    response.raise_for_status()
-
-    return response.content
     response.raise_for_status()
 
     return response.content
