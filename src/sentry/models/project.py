994be97ad72c300ebea8c7b085b6bc985690f736
Vjeran Grozdanić <vjeran.grozdanic@sentry.io>
2025-08-11 15:56:37 +0200

    chore(project-options): Remove template option lookup (#97565)
    Project tempaltes is the feature that was paused, and is not currently
    not used anywhere, but it may be revisited so we don't want to
    completely remove it.
    
    This PR removes code that causes an extra unnecessary lookup, and
    simplifies the code in `get_option` method.

--- src/sentry/models/project.py
+++ src/sentry/models/project.py
@@ -443,10 +443,6 @@ class Project(Model):
     def get_option(
         self, key: str, default: Any | None = None, validate: Callable[[object], bool] | None = None
     ) -> Any:
-        # if the option is not set, check the template
-        if not self.option_manager.isset(self, key) and self.template is not None:
-            return self.template_manager.get_value(self.template, key, default, validate)
-
         return self.option_manager.get_value(self, key, default, validate)
 
     def update_option(self, key: str, value: Any, reload_cache: bool = True) -> bool:
