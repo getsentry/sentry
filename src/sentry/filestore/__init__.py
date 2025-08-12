2a4aa4147d9b5f909b4eaf0c08d9107976218f36
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-08-12 17:39:46 -0400

    ref(dx): Remove filestore shims (#97714)

--- src/sentry/filestore/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-Backward compatibility shim for getsentry.
-
-This module re-exports everything from the new sentry.services.filestore location
-to maintain compatibility with existing getsentry imports.
-"""
-
-# Re-export everything from the new location
-from sentry.services.filestore import *  # noqa: F401, F403
