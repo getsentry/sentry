edd501173c27d474c34c4b5e5cae7bcabf81b3c7
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2025-08-12 16:14:12 -0700

    feat(preprod): Create shell of buildComparison page and wireup routes (#97731)
    Create shell of buildComparison page and wireup routes
    
    Just placeholder content for now, will begin implementing designs soon

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -2496,14 +2496,21 @@ function buildRoutes(): RouteObject[] {
 
   const preprodChildren: SentryRouteObject[] = [
     {
-      index: true,
+      path: ':artifactId/',
       component: make(() => import('sentry/views/preprod/buildDetails/buildDetails')),
+      deprecatedRouteProps: true,
+    },
+    {
+      path: 'compare/:headArtifactId/:baseArtifactId/',
+      component: make(
+        () => import('sentry/views/preprod/buildComparison/buildComparison')
+      ),
       deprecatedRouteProps: true,
     },
   ];
   const preprodRoutes: SentryRouteObject = {
-    path: '/preprod/:projectId/:artifactId/',
-    component: make(() => import('sentry/views/preprod/buildDetails/index')),
+    path: '/preprod/:projectId/',
+    component: make(() => import('sentry/views/preprod/index')),
     withOrgPath: true,
     children: preprodChildren,
     deprecatedRouteProps: true,
