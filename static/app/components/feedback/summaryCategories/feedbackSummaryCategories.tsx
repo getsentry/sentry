30b46300a5e3425bb735fb2f6709d8e5b3933d1c
Vishnu Satish <vishnu.satish@sentry.io>
2025-08-12 14:22:07 -0700

    feat(feedback): frontend for categories (#97402)

--- static/app/components/feedback/summaryCategories/feedbackSummaryCategories.tsx
+++ static/app/components/feedback/summaryCategories/feedbackSummaryCategories.tsx
@@ -2,19 +2,27 @@ import styled from '@emotion/styled';
 
 import {Button} from 'sentry/components/core/button';
 import {Flex} from 'sentry/components/core/layout';
-import useFeedbackSummary from 'sentry/components/feedback/list/useFeedbackSummary';
-import LoadingIndicator from 'sentry/components/loadingIndicator';
+import FeedbackCategories from 'sentry/components/feedback/summaryCategories/feedbackCategories';
+import FeedbackSummary from 'sentry/components/feedback/summaryCategories/feedbackSummary';
 import {IconThumb} from 'sentry/icons';
 import {IconSeer} from 'sentry/icons/iconSeer';
 import {t} from 'sentry/locale';
-import {space} from 'sentry/styles/space';
 import {useFeedbackForm} from 'sentry/utils/useFeedbackForm';
+import useOrganization from 'sentry/utils/useOrganization';
 
-export default function FeedbackSummary() {
-  const {isError, isPending, summary, tooFewFeedbacks, numFeedbacksUsed} =
-    useFeedbackSummary();
+export default function FeedbackSummaryCategories() {
+  const organization = useOrganization();
 
   const openForm = useFeedbackForm();
+
+  const showSummaryCategories =
+    organization.features.includes('gen-ai-features') &&
+    (organization.features.includes('user-feedback-ai-summaries') ||
+      organization.features.includes('user-feedback-ai-categorization-features'));
+
+  if (!showSummaryCategories) {
+    return null;
+  }
 
   const feedbackButton = ({type}: {type: 'positive' | 'negative'}) => {
     return openForm ? (
@@ -33,7 +41,6 @@ export default function FeedbackSummary() {
               ['feedback.source']: 'feedback_ai_summary',
               ['feedback.owner']: 'replay',
               ['feedback.type']: type,
-              ['feedback.num_feedbacks_used']: numFeedbacksUsed,
             },
           })
         }
@@ -52,41 +59,21 @@ export default function FeedbackSummary() {
             {feedbackButton({type: 'negative'})}
           </Flex>
         </Flex>
-
-        {isPending ? (
-          <LoadingContainer>
-            <StyledLoadingIndicator size={24} />
-            <SummaryContent>{t('Summarizing feedback received...')}</SummaryContent>
-          </LoadingContainer>
-        ) : tooFewFeedbacks ? (
-          <SummaryContent>
-            {t('Bummer... Not enough feedback to summarize (yet).')}
-          </SummaryContent>
-        ) : isError ? (
-          <SummaryContent>{t('Error summarizing feedback.')}</SummaryContent>
-        ) : (
-          <SummaryContent>{summary}</SummaryContent>
+        {organization.features.includes('user-feedback-ai-summaries') && (
+          <FeedbackSummary />
+        )}
+        {organization.features.includes('user-feedback-ai-categorization-features') && (
+          <FeedbackCategories />
         )}
       </SummaryContainer>
     </SummaryIconContainer>
   );
 }
 
-const StyledLoadingIndicator = styled(LoadingIndicator)`
-  margin: ${space(0.5)} 0 0 0;
-`;
-
-const LoadingContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-  gap: ${space(1)};
-  align-items: center;
-`;
-
 const SummaryContainer = styled('div')`
   display: flex;
   flex-direction: column;
-  gap: ${space(1)};
+  gap: ${p => p.theme.space.md};
   width: 100%;
 `;
 
@@ -96,16 +83,10 @@ const SummaryHeader = styled('p')`
   margin: 0;
 `;
 
-const SummaryContent = styled('p')`
-  font-size: ${p => p.theme.fontSize.sm};
-  color: ${p => p.theme.subText};
-  margin: 0;
-`;
-
 const SummaryIconContainer = styled('div')`
   display: flex;
-  gap: ${space(1)};
-  padding: ${space(2)};
+  gap: ${p => p.theme.space.md};
+  padding: ${p => p.theme.space.xl};
   border: 1px solid ${p => p.theme.border};
   border-radius: ${p => p.theme.borderRadius};
   align-items: baseline;
