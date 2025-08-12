2a2f775d962b689ae876378b78337b4026c35226
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2025-08-12 13:05:30 -0700

    feat(detectors): Add filtered URL check to detectors that use `urlparse` (#97694)
    follow up to https://github.com/getsentry/sentry/pull/97104 - adds the
    logic to other detectors

--- src/sentry/performance_issues/detectors/experiments/n_plus_one_api_calls_detector.py
+++ src/sentry/performance_issues/detectors/experiments/n_plus_one_api_calls_detector.py
@@ -20,7 +20,7 @@ from sentry.performance_issues.base import (
     parameterize_url,
     parameterize_url_with_result,
 )
-from sentry.performance_issues.detectors.utils import get_total_span_duration
+from sentry.performance_issues.detectors.utils import get_total_span_duration, has_filtered_url
 from sentry.performance_issues.performance_problem import PerformanceProblem
 from sentry.performance_issues.types import Span
 
@@ -126,6 +126,9 @@ class NPlusOneAPICallsExperimentalDetector(PerformanceDetector):
             return False
 
         if not url:
+            return False
+
+        if has_filtered_url(self._event, span):
             return False
 
         # Once most users update their SDKs to use the latest standard, we
