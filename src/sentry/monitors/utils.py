70675f2f478ee076aec104acf5028893304d8402
Dan Fuller <dfuller@sentry.io>
2025-08-12 10:21:29 -0700

    feat(crons): Add `get_detector_for_monitor` function (#97547)
    Add a simple function to get the detector associated with a cron
    monitor, if one exists.

--- src/sentry/monitors/utils.py
+++ src/sentry/monitors/utils.py
@@ -9,6 +9,7 @@ from rest_framework.request import Request
 from sentry import audit_log
 from sentry.api.serializers.rest_framework.rule import RuleSerializer
 from sentry.db.models import BoundedPositiveIntegerField
+from sentry.issues.grouptype import MonitorIncidentType
 from sentry.models.group import Group
 from sentry.models.project import Project
 from sentry.models.rule import Rule, RuleActivity, RuleActivityType, RuleSource
@@ -392,7 +393,6 @@ def update_issue_alert_rule(
 
 
 def ensure_cron_detector(monitor: Monitor):
-    from sentry.issues.grouptype import MonitorCheckInFailure
 
     try:
         with atomic_transaction(using=router.db_for_write(DataSource)):
@@ -403,7 +403,7 @@ def ensure_cron_detector(monitor: Monitor):
             )
             if created:
                 detector = Detector.objects.create(
-                    type=MonitorCheckInFailure.slug,
+                    type=MonitorIncidentType.slug,
                     project_id=monitor.project_id,
                     name=monitor.name,
                     owner_user_id=monitor.owner_user_id,
@@ -412,3 +412,14 @@ def ensure_cron_detector(monitor: Monitor):
                 DataSourceDetector.objects.create(data_source=data_source, detector=detector)
     except Exception:
         logger.exception("Error creating cron detector")
+
+
+def get_detector_for_monitor(monitor: Monitor) -> Detector | None:
+    try:
+        return Detector.objects.get(
+            datasource__type=DATA_SOURCE_CRON_MONITOR,
+            datasource__source_id=str(monitor.id),
+            datasource__organization_id=monitor.organization_id,
+        )
+    except Detector.DoesNotExist:
+        return None
--- tests/sentry/monitors/test_utils.py
+++ tests/sentry/monitors/test_utils.py
@@ -4,9 +4,9 @@ from django.db import IntegrityError
 
 from sentry.issues.grouptype import MonitorIncidentType
 from sentry.monitors.types import DATA_SOURCE_CRON_MONITOR
-from sentry.monitors.utils import ensure_cron_detector
+from sentry.monitors.utils import ensure_cron_detector, get_detector_for_monitor
 from sentry.testutils.cases import TestCase
-from sentry.workflow_engine.models import DataSource, DataSourceDetector, Detector
+from sentry.workflow_engine.models import DataSource, Detector
 
 
 class EnsureCronDetectorTest(TestCase):
@@ -15,66 +15,24 @@ class EnsureCronDetectorTest(TestCase):
         self.monitor = self.create_monitor(owner_user_id=None)
 
     def test_creates_data_source_and_detector_for_new_monitor(self):
-        assert not DataSource.objects.filter(
-            type=DATA_SOURCE_CRON_MONITOR,
-            organization_id=self.monitor.organization_id,
-            source_id=str(self.monitor.id),
-        ).exists()
-
+        assert not get_detector_for_monitor(self.monitor)
         ensure_cron_detector(self.monitor)
-        data_source = DataSource.objects.get(
-            type=DATA_SOURCE_CRON_MONITOR,
-            organization_id=self.monitor.organization_id,
-            source_id=str(self.monitor.id),
-        )
-        assert data_source is not None
-        detector = Detector.objects.get(
-            type=MonitorIncidentType.slug,
-            project_id=self.monitor.project_id,
-            name=self.monitor.name,
-        )
+        detector = get_detector_for_monitor(self.monitor)
         assert detector is not None
+        assert detector.type == "monitor_check_in_failure"
+        assert detector.project_id == self.monitor.project_id
+        assert detector.name == self.monitor.name
         assert detector.owner_user_id == self.monitor.owner_user_id
         assert detector.owner_team_id == self.monitor.owner_team_id
-        assert DataSourceDetector.objects.filter(
-            data_source=data_source,
-            detector=detector,
-        ).exists()
 
     def test_idempotent_for_existing_data_source(self):
         ensure_cron_detector(self.monitor)
-        data_source = DataSource.objects.get(
-            type=DATA_SOURCE_CRON_MONITOR,
-            organization_id=self.monitor.organization_id,
-            source_id=str(self.monitor.id),
-        )
-        detector = Detector.objects.get(
-            type=MonitorIncidentType.slug,
-            project_id=self.monitor.project_id,
-            name=self.monitor.name,
-        )
-        link = DataSourceDetector.objects.get(
-            data_source=data_source,
-            detector=detector,
-        )
+        detector = get_detector_for_monitor(self.monitor)
+        assert detector
         ensure_cron_detector(self.monitor)
-        data_source_after = DataSource.objects.get(
-            type=DATA_SOURCE_CRON_MONITOR,
-            organization_id=self.monitor.organization_id,
-            source_id=str(self.monitor.id),
-        )
-        detector_after = Detector.objects.get(
-            type=MonitorIncidentType.slug,
-            project_id=self.monitor.project_id,
-            name=self.monitor.name,
-        )
-        link_after = DataSourceDetector.objects.get(
-            data_source=data_source,
-            detector=detector,
-        )
-        assert data_source.id == data_source_after.id
+        detector_after = get_detector_for_monitor(self.monitor)
+        assert detector_after is not None
         assert detector.id == detector_after.id
-        assert link.id == link_after.id
 
     def test_with_owner_user(self):
         self.monitor.owner_user_id = self.user.id
@@ -118,3 +76,38 @@ class EnsureCronDetectorTest(TestCase):
         assert not DataSource.objects.filter(
             type=DATA_SOURCE_CRON_MONITOR, source_id=str(self.monitor.id)
         ).exists()
+
+
+class GetDetectorForMonitorTest(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.monitor = self.create_monitor()
+
+    def test_returns_none_when_no_detector_exists(self):
+        detector = get_detector_for_monitor(self.monitor)
+        assert detector is None
+
+    def test_returns_detector_when_exists(self):
+        ensure_cron_detector(self.monitor)
+
+        detector = get_detector_for_monitor(self.monitor)
+        assert detector is not None
+        assert detector.type == "monitor_check_in_failure"
+        assert detector.project_id == self.monitor.project_id
+        assert detector.name == self.monitor.name
+
+    def test_returns_correct_detector_for_specific_monitor(self):
+        monitor1 = self.monitor
+        monitor2 = self.create_monitor(name="Monitor 2")
+
+        ensure_cron_detector(monitor1)
+        ensure_cron_detector(monitor2)
+
+        detector1 = get_detector_for_monitor(monitor1)
+        detector2 = get_detector_for_monitor(monitor2)
+
+        assert detector1 is not None
+        assert detector2 is not None
+        assert detector1.id != detector2.id
+        assert detector1.name == monitor1.name
+        assert detector2.name == monitor2.name
