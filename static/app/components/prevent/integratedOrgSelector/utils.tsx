6e213ac52265bd10a31e55948957553ae24a4f76
Adrian <adrian.bonilla@sentry.io>
2025-08-12 17:25:16 -0600

    capitalize + shorten words

--- static/app/components/prevent/integratedOrgSelector/utils.tsx
+++ static/app/components/prevent/integratedOrgSelector/utils.tsx
@@ -3,8 +3,8 @@ import type {Integration} from 'sentry/types/integrations';
 
 export const integratedOrgIdToName = (id?: string, integrations?: Integration[]) => {
   if (!id || !integrations) {
-    return t('Select integrated organization');
+    return t('Select Integrated Org');
   }
   const result = integrations.find(item => item.id === id);
-  return result?.name || t('Select integrated organization');
+  return result?.name || t('Select Integrated Org');
 };
