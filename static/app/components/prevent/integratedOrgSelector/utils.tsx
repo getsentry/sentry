379b5efffb50c11a642ea13765eb7d1858c07ae4
Adrian <adrian.bonilla@sentry.io>
2025-08-12 16:42:15 -0600

    update empty message

--- static/app/components/prevent/integratedOrgSelector/utils.tsx
+++ static/app/components/prevent/integratedOrgSelector/utils.tsx
@@ -1,9 +1,11 @@
 import type {Integration} from 'sentry/types/integrations';
 
+const EMPTY_MESSAGE = 'Unknown Integration';
+
 export const integratedOrgIdToName = (id?: string, integrations?: Integration[]) => {
   if (!id || !integrations) {
-    return 'No Integration';
+    return EMPTY_MESSAGE;
   }
   const result = integrations.find(item => item.id === id);
-  return result ? result.name : 'Unknown Integration';
+  return result ? result.name : EMPTY_MESSAGE;
 };
