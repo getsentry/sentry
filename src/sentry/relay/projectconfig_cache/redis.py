846ab05b4a2a6cdbe8370cf9663f5f5b54f37330
David Herberth <david.herberth@sentry.io>
2025-08-13 12:41:10 +0200

    fix(project-config): Handle cases when new project configs do not have a revision (#97751)
    It is possible that for some reason new project configs do not also have
    a new revision. In this case the revision in the cache should be reset
    instead of left untouched.

--- src/sentry/relay/projectconfig_cache/redis.py
+++ src/sentry/relay/projectconfig_cache/redis.py
@@ -57,6 +57,8 @@ class RedisProjectConfigCache(ProjectConfigCache):
             # made transactional.
             if rev := config.get("rev"):
                 p.setex(self.__get_redis_rev_key(public_key), REDIS_CACHE_TIMEOUT, rev)
+            else:
+                p.delete(self.__get_redis_rev_key(public_key))
 
         p.execute()
 
@@ -64,12 +66,12 @@ class RedisProjectConfigCache(ProjectConfigCache):
         # Note: Those are multiple pipelines, one per cluster node
         with self.cluster.pipeline() as p:
             for public_key in public_keys:
-                p.delete(self.__get_redis_key(public_key))
+                p.delete(self.__get_redis_key(public_key), self.__get_redis_rev_key(public_key))
             return_values = p.execute()
 
-        metrics.incr(
-            "relay.projectconfig_cache.write", amount=sum(return_values), tags={"action": "delete"}
-        )
+        # Count deletions of project configs, not deletions of individual Redis keys.
+        amount = sum(1 for rv in return_values if rv >= 1)
+        metrics.incr("relay.projectconfig_cache.write", amount=amount, tags={"action": "delete"})
 
     def get(self, public_key):
         rv_b = self.cluster_read.get(self.__get_redis_key(public_key))
