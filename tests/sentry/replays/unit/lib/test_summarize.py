2cb362266f0dfa73e35ae0ffdf6a3eaf0520ea1c
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-08-13 13:44:26 -0700

    :recycle: new status code

--- tests/sentry/replays/unit/lib/test_summarize.py
+++ tests/sentry/replays/unit/lib/test_summarize.py
@@ -216,7 +216,7 @@ def test_as_log_message() -> None:
     }
 
     result = as_log_message(event)
-    assert result is None
+    assert result is None  # we filter out 200 status codes
 
     event = {
         "type": 5,
@@ -230,7 +230,7 @@ def test_as_log_message() -> None:
                 "startTimestamp": 0.0,
                 "data": {
                     "method": "GET",
-                    "statusCode": 200,
+                    "statusCode": 404,
                     "response": {"size": 0},
                 },
             },
