db4bc8d6e6fc6d8a40e694322c8d9082f95504b0
Christinarlong <christina.long@sentry.io>
2025-08-13 09:29:15 -0700

    pr fixes

--- src/sentry/notifications/platform/slack/provider.py
+++ src/sentry/notifications/platform/slack/provider.py
@@ -39,19 +39,21 @@ class SlackRenderer(NotificationRenderer[SlackRenderable]):
         subject = HeaderBlock(text=PlainTextObject(text=rendered_template.subject))
         body = SectionBlock(text=MarkdownTextObject(text=rendered_template.body))
 
-        actions_block = ActionsBlock(elements=[])
-        for action in rendered_template.actions:
-            actions_block.elements.append(
-                ButtonElement(text=action.get("label", ""), url=action.get("link", ""))
-            )
+        blocks = [subject, body]
 
-        blocks = [subject, body, actions_block]
+        if len(rendered_template.actions) > 0:
+            actions_block = ActionsBlock(elements=[])
+            for action in rendered_template.actions:
+                actions_block.elements.append(ButtonElement(text=action.label, url=action.link))
+            blocks.append(actions_block)
 
         if rendered_template.footer:
             footer = SectionBlock(text=MarkdownTextObject(text=rendered_template.footer))
             blocks.append(footer)
         if rendered_template.chart:
-            chart = ImageBlock(image_url=rendered_template.chart, alt_text="chart")
+            chart = ImageBlock(
+                image_url=rendered_template.chart.url, alt_text=rendered_template.chart.alt_text
+            )
             blocks.append(chart)
 
         return SlackRenderable(blocks=blocks)
