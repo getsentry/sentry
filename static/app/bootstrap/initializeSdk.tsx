e315812ba2cec9e545097091915b799bed418e23
Lukas Stracke <lukas.stracke@sentry.io>
2025-08-14 11:35:07 +0200

    feat(sdk): Bump Sentry SDKs to 10.5.0 (#97753)

--- static/app/bootstrap/initializeSdk.tsx
+++ static/app/bootstrap/initializeSdk.tsx
@@ -191,6 +191,7 @@ export function initializeSdk(config: Config) {
       return event;
     },
     enableLogs: true,
+    sendDefaultPii: true,
   });
 
   // Track timeOrigin Selection by the SDK to see if it improves transaction durations
