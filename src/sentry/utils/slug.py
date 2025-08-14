e020a0e1a64c61471e421193bd4f6078879e7126
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-08-14 15:08:14 -0400

    ref(dx): Move sentry.slug -> sentry.utils.slug (#97795)
    I realize this is just polluting the utils folder more, but I think this
    is better than polluting the root folder

--- src/sentry/utils/slug.py
+++ src/sentry/utils/slug.py
@@ -1,5 +1,20 @@
 import re
 
+from django.core.validators import RegexValidator
+from django.utils.translation import gettext_lazy as _
+
+# Error messages
+DEFAULT_SLUG_ERROR_MESSAGE = _(
+    "Enter a valid slug consisting of lowercase letters, numbers, underscores or hyphens. "
+    "It cannot be entirely numeric."
+)
+
+ORG_SLUG_ERROR_MESSAGE = _(
+    "Enter a valid slug consisting of letters, numbers, or hyphens. "
+    "It cannot be entirely numeric or start/end with a hyphen."
+)
+
+# Regex patterns
 r"""
 Standard slug pattern:
     (?![0-9]+$) - Negative lookahead to ensure the slug is not entirely numeric
@@ -17,3 +32,14 @@ Organization slug pattern:
 """
 ORG_SLUG_PATTERN = r"^(?![0-9]+$)[a-zA-Z0-9][a-zA-Z0-9-]*(?<!-)$"
 ORG_SLUG_REGEX = re.compile(ORG_SLUG_PATTERN)
+
+# Validators
+no_numeric_validator = RegexValidator(
+    regex=MIXED_SLUG_REGEX,
+    message=DEFAULT_SLUG_ERROR_MESSAGE,
+)
+
+org_slug_validator = RegexValidator(
+    regex=ORG_SLUG_REGEX,
+    message=ORG_SLUG_ERROR_MESSAGE,
+)
