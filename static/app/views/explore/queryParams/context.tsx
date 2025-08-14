feda7b4a7c71845af2be3623b9769169c9c5b36b
Tony Xiao <txiao@sentry.io>
2025-08-14 19:21:22 -0400

    feat(logs): Use query params to write aggregate fields (#97739)
    This switches the writes for aggregate fields to use query params
    context.

--- static/app/views/explore/queryParams/context.tsx
+++ static/app/views/explore/queryParams/context.tsx
@@ -1,12 +1,19 @@
 import type {ReactNode} from 'react';
 import {useCallback, useMemo} from 'react';
 
+import {defined} from 'sentry/utils';
 import type {Sort} from 'sentry/utils/discover/fields';
 import {createDefinedContext} from 'sentry/utils/performance/contexts/utils';
 import {TOP_EVENTS_LIMIT} from 'sentry/views/explore/hooks/useTopEvents';
+import type {WritableAggregateField} from 'sentry/views/explore/queryParams/aggregateField';
+import {isGroupBy} from 'sentry/views/explore/queryParams/groupBy';
 import type {Mode} from 'sentry/views/explore/queryParams/mode';
 import {ReadableQueryParams} from 'sentry/views/explore/queryParams/readableQueryParams';
-import type {Visualize} from 'sentry/views/explore/queryParams/visualize';
+import {
+  isVisualize,
+  type BaseVisualize,
+  type Visualize,
+} from 'sentry/views/explore/queryParams/visualize';
 import type {WritableQueryParams} from 'sentry/views/explore/queryParams/writableQueryParams';
 
 interface QueryParamsContextValue {
@@ -37,7 +44,7 @@ export function QueryParamsContextProvider({
   return <QueryParamsContext value={value}>{children}</QueryParamsContext>;
 }
 
-function useQueryParams() {
+export function useQueryParams() {
   const {queryParams} = useQueryParamsContext();
   return queryParams;
 }
@@ -47,9 +54,24 @@ function useSetQueryParams() {
 
   return useCallback(
     (writableQueryParams: WritableQueryParams) => {
+      if (shouldResetCursors(writableQueryParams)) {
+        // setting it to null tells the implementer that it should be reset
+        writableQueryParams.cursor = null;
+        writableQueryParams.aggregateCursor = null;
+      }
       setQueryParams(writableQueryParams);
     },
     [setQueryParams]
+  );
+}
+
+function shouldResetCursors(queryParams: WritableQueryParams): boolean {
+  return (
+    defined(queryParams.aggregateFields) ||
+    defined(queryParams.aggregateSortBys) ||
+    defined(queryParams.fields) ||
+    defined(queryParams.query) ||
+    defined(queryParams.sortBys)
   );
 }
 
@@ -74,9 +96,78 @@ export function useQueryParamsVisualizes(): readonly Visualize[] {
   return queryParams.visualizes;
 }
 
+export function useSetQueryParamsVisualizes() {
+  const queryParams = useQueryParams();
+  const setQueryParams = useSetQueryParams();
+
+  return useCallback(
+    (visualizes: BaseVisualize[]) => {
+      const aggregateFields: WritableAggregateField[] = [];
+
+      const iter = visualizes[Symbol.iterator]();
+
+      for (const aggregateField of queryParams.aggregateFields) {
+        if (isVisualize(aggregateField)) {
+          const {value: visualize, done} = iter.next();
+          if (!done) {
+            aggregateFields.push(visualize);
+          }
+        } else if (isGroupBy(aggregateField)) {
+          aggregateFields.push(aggregateField);
+        } else {
+          throw new Error(`Unknown aggregate field: ${JSON.stringify(aggregateField)}`);
+        }
+      }
+
+      for (const visualize of iter) {
+        aggregateFields.push(visualize);
+      }
+
+      setQueryParams({aggregateFields});
+    },
+    [queryParams, setQueryParams]
+  );
+}
+
 export function useQueryParamsGroupBys(): readonly string[] {
   const queryParams = useQueryParams();
   return queryParams.groupBys;
+}
+
+export function useSetQueryParamsGroupBys() {
+  const queryParams = useQueryParams();
+  const setQueryParams = useSetQueryParams();
+
+  return useCallback(
+    (groupBys: string[]) => {
+      const aggregateFields: WritableAggregateField[] = [];
+
+      const iter = groupBys[Symbol.iterator]();
+
+      for (const aggregateField of queryParams.aggregateFields) {
+        if (isVisualize(aggregateField)) {
+          aggregateFields.push({
+            yAxes: [aggregateField.yAxis],
+            chartType: aggregateField.selectedChartType,
+          });
+        } else if (isGroupBy(aggregateField)) {
+          const {value: groupBy, done} = iter.next();
+          if (!done) {
+            aggregateFields.push({groupBy});
+          }
+        } else {
+          throw new Error('Unknown aggregate field', aggregateField);
+        }
+      }
+
+      for (const groupBy of iter) {
+        aggregateFields.push({groupBy});
+      }
+
+      setQueryParams({aggregateFields});
+    },
+    [queryParams, setQueryParams]
+  );
 }
 
 export function useQueryParamsTopEventsLimit(): number | undefined {
