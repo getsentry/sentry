0dbbe293a34531424524d014304d01494e3867dc
Vishnu Satish <vishnu.satish@sentry.io>
2025-08-15 11:23:09 -0700

    remove commented out tests and comment out cache.set

--- src/sentry/feedback/endpoints/organization_feedback_summary.py
+++ src/sentry/feedback/endpoints/organization_feedback_summary.py
@@ -91,7 +91,7 @@ class OrganizationFeedbackSummaryEndpoint(OrganizationEndpoint):
 
         summary_cache_key = f"feedback_summary:{organization.id}:{start.strftime('%Y-%m-%d-%H')}:{end.strftime('%Y-%m-%d-%H')}:{hashed_project_ids}"
         summary_cache = cache.get(summary_cache_key)
-        # TODO(vishnupsatish): remove this once we are to EA. This is allow users to regenerate the summary on reload.
+        # TODO(vishnupsatish): remove this once we are to EA. This is to allow users to regenerate the summary on reload.
         if summary_cache:
             # return Response(
             #     {
@@ -149,11 +149,12 @@ class OrganizationFeedbackSummaryEndpoint(OrganizationEndpoint):
             logger.exception("Error generating summary of user feedbacks")
             return Response({"detail": "Error generating summary"}, status=500)
 
-        cache.set(
-            summary_cache_key,
-            {"summary": summary, "numFeedbacksUsed": len(group_feedbacks)},
-            timeout=SUMMARY_CACHE_TIMEOUT,
-        )
+        # TODO(vishnupsatish): remove this once we are to EA. This is to allow users to regenerate the summary on reload.
+        # cache.set(
+        #     summary_cache_key,
+        #     {"summary": summary, "numFeedbacksUsed": len(group_feedbacks)},
+        #     timeout=SUMMARY_CACHE_TIMEOUT,
+        # )
 
         return Response(
             {
