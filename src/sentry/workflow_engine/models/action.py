376cc333aac86aa7722c533b51ce08284e06b31f
Raj Joshi <raj.joshi@sentry.io>
2025-08-15 15:16:17 -0700

    :mag: nit(aci): reduce action trigger log length (#97960)

--- src/sentry/workflow_engine/models/action.py
+++ src/sentry/workflow_engine/models/action.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import builtins
 import logging
-from dataclasses import asdict
 from enum import StrEnum
 from typing import TYPE_CHECKING
 
@@ -111,7 +110,6 @@ class Action(DefaultFieldsModel, JSONConfigBase):
             extra={
                 "detector_id": detector.id,
                 "action_id": self.id,
-                "event_data": asdict(event_data),
             },
         )
 
