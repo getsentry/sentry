ccbd7e469f4eadaf652f2f24862cf8e1e17175f2
Malachi Willey <malwilley@gmail.com>
2025-08-15 13:28:03 -0700

    Gate with feature flag

--- static/gsApp/hooks/useMetricDetectorLimit.tsx
+++ static/gsApp/hooks/useMetricDetectorLimit.tsx
@@ -1,11 +1,19 @@
+import useOrganization from 'sentry/utils/useOrganization';
+
 type MetricDetectorLimitResponse = {
   isLimitExceeded: boolean;
   limit: number;
   numMetricMonitors: number;
-} | null;
+} | null; // null means there is no limit
 
 // TODO: Replace with actual hook
 export function useMetricDetectorLimit(): MetricDetectorLimitResponse {
+  const organization = useOrganization();
+
+  if (!organization.features.includes('workflow-engine-metric-detector-limit')) {
+    return null;
+  }
+
   return {
     isLimitExceeded: true,
     numMetricMonitors: 20,
