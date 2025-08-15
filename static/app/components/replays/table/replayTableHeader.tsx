c532807b4de49494335b5008fb74cb5a166b0d82
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-08-15 14:19:16 -0700

    fix(replay): Keep Replay Table column widths static when selecting items (#97525)
    Before when you selected some rows we were showing
    `<ReplayTableHeaderActions>` instead of the
    `columns.map(<SimpleTable.HeaderCell>)` stuff. This was causing the
    column widths to get small, because the header labels were removed and
    not holding the space anymore.
    
    Now we just render everything, the actions are on top of the
    `<SimpleTable.HeaderCell>`, so columns stay the same size.
    
    **Before**
    <img width="1074" height="230" alt="SCR-20250808-nbjt"
    src="https://github.com/user-attachments/assets/18d500c6-3576-44ef-bf52-05190c11b6b8"
    />
    
    
    **After**
    <img width="1028" height="201" alt="SCR-20250808-nbnb"
    src="https://github.com/user-attachments/assets/7488023f-0d3a-43f1-8427-308b291cea02"
    />

--- static/app/components/replays/table/replayTableHeader.tsx
+++ static/app/components/replays/table/replayTableHeader.tsx
@@ -1,8 +1,18 @@
-import type {ReplayTableColumn} from 'sentry/components/replays/table/replayTableColumns';
-import ReplayTableHeaderActions from 'sentry/components/replays/table/replayTableHeaderActions';
+import {Fragment} from 'react';
+import styled from '@emotion/styled';
+
+import {Alert} from 'sentry/components/core/alert';
+import {Flex} from 'sentry/components/core/layout/flex';
+import DeleteReplays from 'sentry/components/replays/table/deleteReplays';
+import {
+  ReplaySelectColumn,
+  type ReplayTableColumn,
+} from 'sentry/components/replays/table/replayTableColumns';
 import {SimpleTable} from 'sentry/components/tables/simpleTable';
+import {t, tct, tn} from 'sentry/locale';
 import type {Sort} from 'sentry/utils/discover/fields';
 import {useListItemCheckboxContext} from 'sentry/utils/list/useListItemCheckboxState';
+import {parseQueryKey} from 'sentry/utils/queryClient';
 import type {ReplayListRecord} from 'sentry/views/replays/types';
 
 type Props = {
@@ -14,31 +24,106 @@ type Props = {
 
 export default function ReplayTableHeader({columns, replays, onSortClick, sort}: Props) {
   const listItemCheckboxState = useListItemCheckboxContext();
-
-  if (listItemCheckboxState.isAnySelected) {
-    return (
-      <ReplayTableHeaderActions
-        listItemCheckboxState={listItemCheckboxState}
-        replays={replays}
-      />
-    );
-  }
+  const {countSelected, isAllSelected, isAnySelected, queryKey, selectAll, selectedIds} =
+    listItemCheckboxState;
+  const queryOptions = parseQueryKey(queryKey).options;
+  const queryString = queryOptions?.query?.query;
 
   return (
-    <SimpleTable.Header>
-      {columns.map(({Header, sortKey}, columnIndex) => (
-        <SimpleTable.HeaderCell
-          key={`${sortKey}-${columnIndex}`}
-          handleSortClick={
-            onSortClick && sortKey ? () => onSortClick(sortKey) : undefined
-          }
-          sort={sortKey && sort?.field === sortKey ? sort.kind : undefined}
-        >
-          {typeof Header === 'function'
-            ? Header({columnIndex, listItemCheckboxState, replays})
-            : Header}
-        </SimpleTable.HeaderCell>
-      ))}
-    </SimpleTable.Header>
+    <Fragment>
+      <TableHeader>
+        {columns.map(({Header, sortKey}, columnIndex) => (
+          <SimpleTable.HeaderCell
+            key={`${sortKey}-${columnIndex}`}
+            handleSortClick={
+              onSortClick && sortKey ? () => onSortClick(sortKey) : undefined
+            }
+            sort={sortKey && sort?.field === sortKey ? sort.kind : undefined}
+          >
+            {typeof Header === 'function'
+              ? Header({columnIndex, listItemCheckboxState, replays})
+              : Header}
+          </SimpleTable.HeaderCell>
+        ))}
+      </TableHeader>
+
+      {isAnySelected ? (
+        <TableHeader>
+          <TableCellFirst>
+            <ReplaySelectColumn.Header
+              columnIndex={0}
+              listItemCheckboxState={listItemCheckboxState}
+              replays={replays}
+            />
+          </TableCellFirst>
+          <TableCellsRemaining>
+            <DeleteReplays
+              queryOptions={queryOptions}
+              replays={replays}
+              selectedIds={selectedIds}
+            />
+          </TableCellsRemaining>
+        </TableHeader>
+      ) : null}
+
+      {isAllSelected === 'indeterminate' ? (
+        <FullGridAlert type="warning" system>
+          <Flex justify="center" wrap="wrap" gap="md">
+            {tn(
+              'Selected %s visible replay.',
+              'Selected %s visible replays.',
+              countSelected
+            )}
+            <a onClick={selectAll}>
+              {queryString
+                ? tct('Select all replays that match: [queryString].', {
+                    queryString: <var>{queryString}</var>,
+                  })
+                : t('Select all replays.')}
+            </a>
+          </Flex>
+        </FullGridAlert>
+      ) : null}
+
+      {isAllSelected === true ? (
+        <FullGridAlert type="warning" system>
+          <Flex justify="center" wrap="wrap">
+            <span>
+              {queryString
+                ? tct('Selected all replays matching: [queryString].', {
+                    queryString: <var>{queryString}</var>,
+                  })
+                : countSelected > replays.length
+                  ? t('Selected all %s+ replays.', replays.length)
+                  : tn(
+                      'Selected all %s replay.',
+                      'Selected all %s replays.',
+                      countSelected
+                    )}
+            </span>
+          </Flex>
+        </FullGridAlert>
+      ) : null}
+    </Fragment>
   );
 }
+
+const TableHeader = styled(SimpleTable.Header)`
+  grid-row: 1;
+  z-index: ${p => p.theme.zIndex.initial};
+`;
+
+const TableCellFirst = styled(SimpleTable.HeaderCell)`
+  grid-column: 1;
+`;
+
+const TableCellsRemaining = styled('div')`
+  display: flex;
+  align-items: center;
+  flex: 1;
+  grid-column: 2 / -1;
+`;
+
+const FullGridAlert = styled(Alert)`
+  grid-column: 1 / -1;
+`;
