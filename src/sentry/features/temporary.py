a699980fc29261d455bfd86286a20b00844788eb
Colleen O'Rourke <colleen@sentry.io>
2025-08-15 13:05:33 -0700

    feat(aci): Write to IncidentGroupOpenPeriod (#97906)
    https://github.com/getsentry/sentry/pull/97697 but it's behind a feature
    flag this time so we can toggle it on and off, plus converts incident
    projects to a list rather than passing a queryset. See [this
    commit](https://github.com/getsentry/sentry/pull/97906/commits/cd0b7f969c10f061a7ac3d8145b578533be59f47)
    for the only change in this from the original implementation.
    
    ---------
    
    Co-authored-by: Snigdha Sharma <snigdha.sharma@sentry.io>

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -167,6 +167,8 @@ def register_temporary_features(manager: FeatureManager):
     manager.add("organizations:issue-taxonomy", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     manager.add("organizations:metric-issue-poc", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     manager.add("projects:metric-issue-creation", ProjectFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
+    # Enable writing to the IncidentGroupOpenPeriod model
+    manager.add("organizations:incident-group-open-period-write", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     manager.add("organizations:issue-open-periods", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     manager.add("organizations:mep-rollout-flag", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     manager.add("organizations:mep-use-default-tags", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
