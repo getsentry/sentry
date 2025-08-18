fd3332646a5c94ac13633fe682f8cae9de45393a
Charles Paul <charles.paul@sentry.io>
2025-08-18 14:54:37 -0700

    Type tests/sentry_plugins (#97998)
    # Summary
    Grabbed this because I thought it would just be a bunch of easy
    missing-return-type errors. It turns out, when you add return types you
    expose other errors. Who'd've thunk? 😅
    
    Here's the complicating factor: the plugin framework uses `Request`.
    `RequestFactory` returns `WSGIRequest`. `BaseTestCase.make_request`
    returns `HttpRequest`. Our typing revealed that a bunch of incorrect
    `Request-ish` types were being passed around.
    
    This PR changes the incompatible frameworks to `MagicMock`, which more
    correctly limits behavior to what is explicitly defined (and not what
    would not actually happen due to type differences).
    
    # Test Plan
    `mypy tests/sentry_plugins` ==> no errors
    `pytest tests/sentry_plugins/**` ==> `162 passed, 1 skipped`

--- tests/sentry_plugins/github/test_plugin.py
+++ tests/sentry_plugins/github/test_plugin.py
@@ -1,10 +1,10 @@
 from functools import cached_property
+from unittest.mock import MagicMock
 
 import orjson
 import pytest
 import responses
 from django.contrib.auth.models import AnonymousUser
-from django.test import RequestFactory
 
 from sentry.exceptions import PluginError
 from sentry.testutils.cases import PluginTestCase
@@ -17,21 +17,19 @@ def test_conf_key() -> None:
 
 class GitHubPluginTest(PluginTestCase):
     @cached_property
-    def plugin(self):
+    def plugin(self) -> GitHubPlugin:
         return GitHubPlugin()
-
-    @cached_property
-    def request(self):
-        return RequestFactory()
 
     def test_get_issue_label(self) -> None:
         group = self.create_group(message="Hello world", culprit="foo.bar")
-        assert self.plugin.get_issue_label(group, 1) == "GH-1"
+        assert self.plugin.get_issue_label(group, "1") == "GH-1"
 
     def test_get_issue_url(self) -> None:
         self.plugin.set_option("repo", "getsentry/sentry", self.project)
         group = self.create_group(message="Hello world", culprit="foo.bar")
-        assert self.plugin.get_issue_url(group, 1) == "https://github.com/getsentry/sentry/issues/1"
+        assert (
+            self.plugin.get_issue_url(group, "1") == "https://github.com/getsentry/sentry/issues/1"
+        )
 
     def test_is_configured(self) -> None:
         assert self.plugin.is_configured(self.project) is False
@@ -49,7 +47,7 @@ class GitHubPluginTest(PluginTestCase):
         self.plugin.set_option("repo", "getsentry/sentry", self.project)
         group = self.create_group(message="Hello world", culprit="foo.bar")
 
-        request = self.request.get("/")
+        request = MagicMock()
         request.user = AnonymousUser()
         form_data = {"title": "Hello", "description": "Fix this."}
         with pytest.raises(PluginError):
@@ -83,7 +81,7 @@ class GitHubPluginTest(PluginTestCase):
         self.plugin.set_option("repo", "getsentry/sentry", self.project)
         group = self.create_group(message="Hello world", culprit="foo.bar")
 
-        request = self.request.get("/")
+        request = MagicMock()
         request.user = AnonymousUser()
         form_data = {"comment": "Hello", "issue_id": "1"}
         with pytest.raises(PluginError):
