666790f43670335422f622894b90e6eb42fe268e
Isabella Enriquez <isabella.enriquez@sentry.io>
2025-08-21 09:57:06 -0400

    fix(typing): auth module (#98020)
    This was more complex than expected 😅

--- src/sentry/auth/providers/github/client.py
+++ src/sentry/auth/providers/github/client.py
@@ -1,5 +1,8 @@
 from __future__ import annotations
 
+from types import TracebackType
+from typing import Any
+
 import orjson
 from requests.exceptions import RequestException
 
@@ -9,23 +12,25 @@ from .constants import API_DOMAIN
 
 
 class GitHubApiError(Exception):
-    def __init__(self, message="", status=0) -> None:
+    def __init__(self, message: str | bytes = "", status: int = 0) -> None:
         super().__init__(message)
         self.status = status
 
 
 class GitHubClient:
-    def __init__(self, access_token) -> None:
+    def __init__(self, access_token: str) -> None:
         self.http = http.build_session()
         self.access_token = access_token
 
     def __enter__(self) -> GitHubClient:
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback) -> None:
+    def __exit__(
+        self, exc_type: type | None, exc_value: Exception | None, traceback: TracebackType | None
+    ) -> None:
         self.http.close()
 
-    def _request(self, path: str):
+    def _request(self, path: str) -> dict[str, Any] | list[dict[str, Any]]:
         headers = {"Authorization": f"token {self.access_token}"}
 
         try:
@@ -39,18 +44,24 @@ class GitHubClient:
             raise GitHubApiError(req.content, status=req.status_code)
         return orjson.loads(req.content)
 
-    def get_org_list(self):
-        return self._request("/user/orgs")
+    def get_org_list(self) -> list[dict[str, Any]]:
+        res = self._request("/user/orgs")
+        if not isinstance(res, list):
+            return [res]
+        return res
 
-    def get_user(self):
+    def get_user(self) -> dict[str, Any] | list[dict[str, Any]]:
         return self._request("/user")
 
-    def get_user_emails(self):
-        return self._request("/user/emails")
+    def get_user_emails(self) -> list[dict[str, Any]]:
+        res = self._request("/user/emails")
+        if not isinstance(res, list):
+            return [res]
+        return res
 
-    def is_org_member(self, org_id) -> bool:
-        org_id = str(org_id)
+    def is_org_member(self, org_id: int) -> bool:
+        org_id_str = str(org_id)
         for o in self.get_org_list():
-            if str(o["id"]) == org_id:
+            if str(o["id"]) == org_id_str:
                 return True
         return False
