bc974c8595a04ec87b5d727f61c7bd44eacee212
Abhijeet Prasad <aprasad@sentry.io>
2025-08-25 04:59:29 -0700

    feat(onboarding): Add logs empty state onboarding for plain js sdks (#97688)
    resolves
    https://linear.app/getsentry/issue/LOGS-296/add-logs-onboarding-for-plain-js-sdks

--- static/app/gettingStartedDocs/javascript/gatsby.tsx
+++ static/app/gettingStartedDocs/javascript/gatsby.tsx
@@ -27,7 +27,10 @@ import {
 } from 'sentry/components/onboarding/gettingStartedDoc/utils/replayOnboarding';
 import {featureFlagOnboarding} from 'sentry/gettingStartedDocs/javascript/javascript';
 import {t, tct} from 'sentry/locale';
-import {getJavascriptProfilingOnboarding} from 'sentry/utils/gettingStartedDocs/javascript';
+import {
+  getJavascriptLogsOnboarding,
+  getJavascriptProfilingOnboarding,
+} from 'sentry/utils/gettingStartedDocs/javascript';
 
 type Params = DocsParams;
 
@@ -380,14 +383,20 @@ const profilingOnboarding = getJavascriptProfilingOnboarding({
     'https://docs.sentry.io/platforms/javascript/guides/gatsby/profiling/browser-profiling/',
 });
 
+const logsOnboarding: OnboardingConfig = getJavascriptLogsOnboarding({
+  installSnippetBlock,
+  docsPlatform: 'gatsby',
+  sdkPackage: '@sentry/gatsby',
+});
+
 const docs: Docs = {
   onboarding,
   feedbackOnboardingNpm: feedbackOnboarding,
   replayOnboarding,
-
   crashReportOnboarding,
   profilingOnboarding,
   featureFlagOnboarding,
+  logsOnboarding,
 };
 
 export default docs;
