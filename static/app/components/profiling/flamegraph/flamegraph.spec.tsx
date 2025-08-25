2e506ea1114a5f9e356a906e7155e0b3b9245279
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-08-25 14:46:05 -0400

    fix(ui): Consistently use min-width media queries (#98098)
    Because we were using a mix of `max-width` and `min-width` media queries
    at the specific pixel size of the query one would apply but not the
    other, in some cases causing the layout to completely break.

--- static/app/components/profiling/flamegraph/flamegraph.spec.tsx
+++ static/app/components/profiling/flamegraph/flamegraph.spec.tsx
@@ -92,20 +92,6 @@ const flamechart = {
   version: '1',
 };
 
-Object.defineProperty(window, 'matchMedia', {
-  writable: true,
-  value: jest.fn().mockImplementation(query => ({
-    matches: false,
-    media: query,
-    onchange: null,
-    addListener: jest.fn(), // Deprecated
-    removeListener: jest.fn(), // Deprecated
-    addEventListener: jest.fn(),
-    removeEventListener: jest.fn(),
-    dispatchEvent: jest.fn(),
-  })),
-});
-
 describe('Flamegraph', () => {
   beforeEach(() => {
     const project = ProjectFixture({slug: 'foo-project'});
