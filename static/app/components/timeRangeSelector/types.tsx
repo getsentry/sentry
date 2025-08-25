85b6b81f7da3122389129a2ceb94cc93502a3ff2
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-08-25 11:30:14 -0400

    Update static/app/components/timeRangeSelector/types.tsx
    Co-authored-by: George Gritsouk <989898+gggritso@users.noreply.github.com>

--- static/app/components/timeRangeSelector/types.tsx
+++ static/app/components/timeRangeSelector/types.tsx
@@ -2,7 +2,7 @@ import {type SelectOption} from 'sentry/components/core/compactSelect';
 
 export interface TimeRangeItem extends SelectOption<string> {
   /**
-   * Altenrative action handler for the time range item
+   * Alternative action handler for the time range item
    */
   onSelect?: () => void;
 }
