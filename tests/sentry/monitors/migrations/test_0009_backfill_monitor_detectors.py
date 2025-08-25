71e42ea30c79a29281d2f86c5d63c35177df8888
Dan Fuller <dfuller@sentry.io>
2025-08-25 10:32:41 -0700

    mypy

--- tests/sentry/monitors/migrations/test_0009_backfill_monitor_detectors.py
+++ tests/sentry/monitors/migrations/test_0009_backfill_monitor_detectors.py
@@ -24,13 +24,17 @@ class BackfillMonitorDetectorsTest(TestMigrations):
         ensure_cron_detector(self.has_detector)
         assert get_detector_for_monitor(self.no_detector) is None
         self.existing_detector = get_detector_for_monitor(self.has_detector)
-        assert self.existing_detector is not None
 
     def test(self):
         new_detector = get_detector_for_monitor(self.no_detector)
         assert new_detector is not None
+        assert self.existing_detector is not None
         assert new_detector.id != self.existing_detector.id
-        assert get_detector_for_monitor(self.has_detector).id == self.existing_detector.id
+        new_existing_detector = get_detector_for_monitor(self.has_detector)
+        assert (
+            new_existing_detector is not None
+            and new_existing_detector.id == self.existing_detector.id
+        )
         assert DataSourceDetector.objects.all().count() == 2
         assert DataSource.objects.all().count() == 2
         assert Detector.objects.all().count() == 2
