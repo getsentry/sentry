b25696e5688619935e4fd187735ca3480332c157
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-08-26 16:47:12 -0400

    Do not fetched on window focus

--- static/app/utils/timeSeries/useFetchEventsTimeSeries.tsx
+++ static/app/utils/timeSeries/useFetchEventsTimeSeries.tsx
@@ -37,6 +37,7 @@ export function useFetchEventsTimeSeries(
       staleTime: Infinity,
       retry: shouldRetryHandler,
       retryDelay: getRetryDelay,
+      refetchOnWindowFocus: false,
       enabled,
     }
   );
