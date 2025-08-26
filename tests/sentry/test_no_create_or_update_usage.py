69ac765ced262290416fc19269d66072171236ee
Vjeran Grozdanić <vjeran.grozdanic@sentry.io>
2025-08-26 08:32:12 +0200

    ref(db): Stop using legacy create_or_update in buffer update (#97840)
    In a lot of places we are still using our custom `create_or_update`
    method. That method is legacy one, and less performant than Django's
    `update_or_create`.

--- tests/sentry/test_no_create_or_update_usage.py
+++ tests/sentry/test_no_create_or_update_usage.py
@@ -10,7 +10,6 @@ from typing import Any
 # create_or_update. Instead, use Django's update_or_create.
 # Reduce over time as code is refactored. DO NOT add new files here.
 ALLOWLIST_FILES: set[str] = {
-    "src/sentry/buffer/base.py",
     "src/sentry/db/models/manager/base.py",
     "src/sentry/notifications/services/impl.py",
     "src/sentry/nodestore/django/backend.py",
