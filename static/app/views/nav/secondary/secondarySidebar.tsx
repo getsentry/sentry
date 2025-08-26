0d7bc3cba1d7a19f8b54bd9037a170aa44399826
Malachi Willey <malwilley@gmail.com>
2025-08-26 09:46:00 -0700

    Handle dobule click to reset properly

--- static/app/views/nav/secondary/secondarySidebar.tsx
+++ static/app/views/nav/secondary/secondarySidebar.tsx
@@ -32,11 +32,7 @@ export function SecondarySidebar() {
     SECONDARY_SIDEBAR_WIDTH
   );
 
-  const {
-    onMouseDown: handleStartResize,
-    size,
-    onDoubleClick,
-  } = useResizable({
+  const {onMouseDown: handleStartResize, size} = useResizable({
     ref: resizableContainerRef,
     initialSize: secondarySidebarWidth,
     minWidth: SECONDARY_SIDEBAR_MIN_WIDTH,
@@ -77,7 +73,9 @@ export function SecondarySidebar() {
             <ResizeHandle
               ref={resizeHandleRef}
               onMouseDown={handleStartResize}
-              onDoubleClick={onDoubleClick}
+              onDoubleClick={() => {
+                setSecondarySidebarWidth(SECONDARY_SIDEBAR_WIDTH);
+              }}
               atMinWidth={size === SECONDARY_SIDEBAR_MIN_WIDTH}
               atMaxWidth={size === SECONDARY_SIDEBAR_MAX_WIDTH}
             />
