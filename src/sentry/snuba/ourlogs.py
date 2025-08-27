74cec40c88610bf298a0f4d772ef9bdeb4017427
William Mak <william@wmak.io>
2025-08-27 17:15:25 -0400

    fix: index

--- src/sentry/snuba/ourlogs.py
+++ src/sentry/snuba/ourlogs.py
@@ -45,7 +45,7 @@ class OurLogs(rpc_dataset_common.RPCBase):
             and len(orderby) == 1
             and orderby[0].lstrip("-") == constants.TIMESTAMP_ALIAS
         ):
-            desc = orderby[0] == "-"
+            desc = orderby[0][0] == "-"
             orderby.append(("-" if desc else "") + constants.TIMESTAMP_PRECISE_ALIAS)
             if constants.TIMESTAMP_PRECISE_ALIAS not in selected_columns:
                 selected_columns.append(constants.TIMESTAMP_PRECISE_ALIAS)
