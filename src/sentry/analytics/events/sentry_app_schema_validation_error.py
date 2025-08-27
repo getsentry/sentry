723d3eea7965bc2f1d5e46f805cb8ce44f508608
Fabian Schindler <fabian.schindler@sentry.io>
2025-08-27 10:53:58 +0200

    fix(analytics): 'schema' -> 'app_schema'

--- src/sentry/analytics/events/sentry_app_schema_validation_error.py
+++ src/sentry/analytics/events/sentry_app_schema_validation_error.py
@@ -1,14 +1,23 @@
+from typing import Any
+
 from sentry import analytics
 
 
 @analytics.eventclass("sentry_app.schema_validation_error")
 class SentryAppSchemaValidationError(analytics.Event):
-    schema: str
+    # TODO (fabian): rename back to schema once we've come back to use built-in dataclasses
+    app_schema: str
     user_id: int
     sentry_app_id: int | None = None
     sentry_app_name: str
     organization_id: int
     error_message: str
+
+    # TODO (fabian): see above
+    def serialize(self) -> dict[str, Any]:
+        serialized = super().serialize()
+        serialized["data"]["schema"] = serialized["data"].pop("app_schema")
+        return serialized
 
 
 analytics.register(SentryAppSchemaValidationError)
