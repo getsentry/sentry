256d64dda7a9d60411e0b5b66609eb11775629d5
Cathy Teng <70817427+cathteng@users.noreply.github.com>
2025-08-28 11:30:13 -0700

    feat(jira): RPC function for Jira issue summaries (#98293)

--- src/sentry/issues/services/issue/model.py
+++ src/sentry/issues/services/issue/model.py
@@ -1,6 +1,13 @@
+from datetime import datetime
+
 from sentry.hybridcloud.rpc import RpcModel
 
 
 class RpcGroupShareMetadata(RpcModel):
     title: str
     message: str
+
+
+class RpcExternalIssueGroupMetadata(RpcModel):
+    title_url: str
+    link_date: datetime
