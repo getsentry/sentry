99412044754dd0e924d937ce7cdc43c01467ed97
Ajay Singh <ajay.singh@sentry.io>
2025-08-27 17:02:52 -0700

    fix memory leak in test

--- static/gsApp/views/amCheckout/steps/reviewAndConfirm.spec.tsx
+++ static/gsApp/views/amCheckout/steps/reviewAndConfirm.spec.tsx
@@ -23,7 +23,26 @@ import ReviewAndConfirm from './reviewAndConfirm';
 
 jest.mock('sentry/actionCreators/indicator');
 jest.mock('getsentry/utils/trackGetsentryAnalytics');
-// Stripe mocks handled by global setup.ts
+
+// Other Stripe mocks handled by global setup.ts
+jest.mock('getsentry/hooks/useStripeInstance', () => ({
+  useStripeInstance: jest.fn(() => ({
+    handleCardAction: jest.fn((secretKey: string) => {
+      if (secretKey === 'ERROR') {
+        return Promise.resolve({error: {message: 'Invalid card', type: 'card_error'}});
+      }
+      if (secretKey === 'GENERIC_ERROR') {
+        return Promise.resolve({
+          error: {
+            message: 'Something bad that users should not see',
+            type: 'internal_error',
+          },
+        });
+      }
+      return Promise.resolve({setupIntent: {payment_method: 'test-pm'}});
+    }),
+  })),
+}));
 
 describe('AmCheckout > ReviewAndConfirm', () => {
   const api = new MockApiClient();
