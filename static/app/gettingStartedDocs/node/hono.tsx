262bc3d116cbb0b174419e5385fa76f0e03381e5
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-08-28 08:21:15 +0200

    feat: Add hono platform to FE

---
+++ static/app/gettingStartedDocs/node/hono.tsx
@@ -0,0 +1,186 @@
+import {ExternalLink} from 'sentry/components/core/link';
+import type {
+  Docs,
+  DocsParams,
+  OnboardingConfig,
+} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {getUploadSourceMapsStep} from 'sentry/components/onboarding/gettingStartedDoc/utils';
+import {t, tct} from 'sentry/locale';
+import {
+  getImportInstrumentSnippet,
+  getInstallConfig,
+  getSdkInitSnippet,
+  getSentryImportSnippet,
+} from 'sentry/utils/gettingStartedDocs/node';
+
+type Params = DocsParams;
+
+const getVerifySnippet = (params: Params) => `app.get("/debug-sentry", () => {${
+  params.isLogsSelected
+    ? `
+  // Send a log before throwing the error
+  Sentry.logger.info('User triggered test error', {
+    action: 'test_error_endpoint',
+  });`
+    : ''
+}
+  throw new Error("My first Sentry error!");
+});`;
+
+const getSdkSetupSnippet = () => `
+${getImportInstrumentSnippet()}
+
+// All other imports below
+${getSentryImportSnippet('node')}
+const { Hono } = require("hono");
+
+
+const app = new Hono()
+  // Add an onError hook to report unhandled exceptions to Sentry.
+  .onError((err, c) => {
+    // Report _all_ unhandled errors.
+    Sentry.captureException(err);
+    if (err instanceof HTTPException) {
+      return err.getResponse();
+    }
+    // Or just report errors which are not instances of HTTPException
+    // Sentry.captureException(err);
+    return c.json({ error: "Internal server error" }, 500);
+  })
+  // Bind global context via Hono middleware
+  .use((c, next) => {
+    Sentry.setUser({
+      email: c.session.user.email,
+    });
+    Sentry.setTag("project_id", c.session.projectId);
+    return next();
+  })
+  // Your routes...
+  .get("/", () => {
+    // ...
+  });
+`;
+
+const onboarding: OnboardingConfig = {
+  install: params => [
+    {
+      type: StepType.INSTALL,
+      content: [
+        {
+          type: 'text',
+          text: t('Add the Sentry Node SDK as a dependency:'),
+        },
+        {
+          type: 'code',
+          tabs: getInstallConfig(params)[0]!.code,
+        },
+      ],
+    },
+  ],
+  configure: params => [
+    {
+      type: StepType.CONFIGURE,
+      content: [
+        {
+          type: 'text',
+          text: t(
+            "Initialize Sentry as early as possible in your application's lifecycle."
+          ),
+        },
+        {
+          type: 'text',
+          text: tct(
+            'To initialize the SDK before everything else, create an external file called [code:instrument.js/mjs].',
+            {code: <code />}
+          ),
+        },
+        {
+          type: 'code',
+          tabs: [
+            {
+              label: 'JavaScript',
+              language: 'javascript',
+              filename: 'instrument.(js|mjs)',
+              value: 'javascript',
+              code: getSdkInitSnippet(params, 'node'),
+            },
+          ],
+        },
+        {
+          type: 'text',
+          text: tct(
+            "Make sure to import [code:instrument.js/mjs] at the top of your file. Set up the error handler. This setup is typically done in your application's entry point file, which is usually [code:index.(js|ts)]. If you're running your application in ESM mode, or looking for alternative ways to set up Sentry, read about [docs:installation methods in our docs].",
+            {
+              code: <code />,
+              docs: (
+                <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/hono/install/" />
+              ),
+            }
+          ),
+        },
+        {
+          type: 'code',
+          tabs: [
+            {
+              label: 'JavaScript',
+              language: 'javascript',
+              filename: 'index.(js|mjs)',
+              value: 'javascript',
+              code: getSdkSetupSnippet(),
+            },
+          ],
+        },
+      ],
+    },
+    getUploadSourceMapsStep({
+      guideLink: 'https://docs.sentry.io/platforms/javascript/guides/hono/sourcemaps/',
+      ...params,
+    }),
+  ],
+  verify: (params: Params) => [
+    {
+      type: StepType.VERIFY,
+      content: [
+        {
+          type: 'text',
+          text: t(
+            'Add the following code snippet to your main application file, adding a route that triggers an error that Sentry will capture.'
+          ),
+        },
+        {
+          type: 'code',
+          tabs: [
+            {
+              label: 'JavaScript',
+              language: 'javascript',
+              code: getVerifySnippet(params),
+            },
+          ],
+        },
+      ],
+    },
+  ],
+  nextSteps: (params: Params) => {
+    const steps = [];
+
+    if (params.isLogsSelected) {
+      steps.push({
+        id: 'logs',
+        name: t('Logging Integrations'),
+        description: t(
+          'Add logging integrations to automatically capture logs from your application.'
+        ),
+        link: 'https://docs.sentry.io/platforms/javascript/guides/hono/logs/#integrations',
+      });
+    }
+
+    return steps;
+  },
+};
+
+const docs: Docs = {
+  onboarding,
+};
+
+export default docs;
