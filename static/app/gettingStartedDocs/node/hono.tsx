9239f423a81075ce89d186cee5bee22fe2ce3e27
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-08-28 13:01:46 +0200

    Priscila/feat/onboarding/hono/add alert info to install (#98428)

--- static/app/gettingStartedDocs/node/hono.tsx
+++ static/app/gettingStartedDocs/node/hono.tsx
@@ -89,6 +89,19 @@ const onboarding: OnboardingConfig = {
       type: StepType.INSTALL,
       content: [
         {
+          type: 'alert',
+          alertType: 'info',
+          showIcon: false,
+          text: tct(
+            "This guide assumes you're using the Node.js runtime for Hono. For setup instructions on Cloudflare Workers, see our [honoCloudFlareLink:Hono on Cloudflare guide].",
+            {
+              honoCloudFlareLink: (
+                <ExternalLink href="https://docs.sentry.io/platforms/javascript/guides/cloudflare/frameworks/hono/" />
+              ),
+            }
+          ),
+        },
+        {
           type: 'text',
           text: t('Add the Sentry Node SDK as a dependency:'),
         },
