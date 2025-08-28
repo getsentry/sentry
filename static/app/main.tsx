6102069471e3d4d79cd5f35ca26d2bb801fc6ccc
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-08-28 17:20:13 -0400

    feat(hackweek): Implement "New Frontend Version available" (FE) (#98031)
    A modern feature many SPA's have is to notify the user when a new
    version of the Sentry frontend has become available. I've implemented
    this feature for Sentry in this pull request.
    
    The app will now detect when there's a new version of the frontend
    available, provide a subtle indicator in the footer that a new version
    of the app is available, and force the app to hard-reload when the user
    navigates while running a stale version of the frontend.
    
    Associated backend changes
    https://github.com/getsentry/sentry/pull/98030
    
    ### Why do this?
    
    Users of Sentry often keep tabs open for hours or even days, however we
    deploy Sentry's frontend multiple times per work-day. This can end up
    leading to scenarios where the users old frontend becomes out-of-sync
    with the backend and in the worst cases can cause the customers app to
    break completely and require them to hard reload.
    
    This leads to us potentially trying to track down errors reported to
    Sentry simply due to the frontend being out of date. With this change
    this will be much more rare. We should see release adoption cut-over
    much improved as well.
    
    ### Here's how it works
    
    - Currently the way the Sentry frontend is deployed is by updating a k8s
    ConfigMap object that the Sentry backend reads to determine the webpack
    entrypoint file to set in the layout.html <script /> tag.
    
    This file is known as `frontend-versions.json`. I've updated the format
    of this file to include the commit SHA that is associated to the
    entrypoints that the file points to, this gives the Sentry backend
    knowledge of the actively deployed Sentry frontend.
    
    The change in getsentry to update the format of the
    frontend-versions.json file is here:
    https://github.com/getsentry/getsentry/pull/18201
    
    - A new `/0/api/internal/frontend-version/` endpoint has been added that
    simply responds with the commit sha of the actively deployed frontend.
    
    - A new `FrontendVersionProvider` has been implemented that polls the
    new endpoint every 5 minutes and updates a context value indicating if
    the frontend app has become stale.
    
    - Our custom `Link` component reads from this context, and in the case
    where the app has become stale, the `reloadDocument` prop is set to true
    on the Link, meaning when a user navigates within Sentry and has an
    outdated version of the frontend, they will automatically reload the app
    and get the new version.
    
    - The `Footer` component reads the context and displays a subtle muted
    pulsing indicator that upon hovering indicates to the user that there's
    an "improved" version of Sentry's frontend available.
    
    ### Ensuring compatibility
    
    Since we've changed the format of the `frontend-versions.json` file I've
    added some compat checks that will ensure both the old and new version
    of this file are respected.
    
    A future pull request will clean up the compat logic once all of the
    frontend-versions.json files have been written with the new format.
    
    
    https://github.com/user-attachments/assets/e4a663eb-7785-4c29-b2d9-ffa149ce65ad

--- static/app/main.tsx
+++ static/app/main.tsx
@@ -4,9 +4,10 @@ import {wrapCreateBrowserRouterV6} from '@sentry/react';
 import {ReactQueryDevtools} from '@tanstack/react-query-devtools';
 
 import {AppQueryClientProvider} from 'sentry/appQueryClient';
+import {FrontendVersionProvider} from 'sentry/components/frontendVersionContext';
 import {OnboardingContextProvider} from 'sentry/components/onboarding/onboardingContext';
 import {ThemeAndStyleProvider} from 'sentry/components/themeAndStyleProvider';
-import {USE_REACT_QUERY_DEVTOOL} from 'sentry/constants';
+import {SENTRY_RELEASE_VERSION, USE_REACT_QUERY_DEVTOOL} from 'sentry/constants';
 import {routes} from 'sentry/routes';
 import {SentryTrackingProvider} from 'sentry/tracking';
 import {DANGEROUS_SET_REACT_ROUTER_6_HISTORY} from 'sentry/utils/browserHistory';
@@ -24,16 +25,18 @@ function Main() {
 
   return (
     <AppQueryClientProvider>
-      <ThemeAndStyleProvider>
-        <OnboardingContextProvider>
-          <SentryTrackingProvider>
-            <RouterProvider router={router} />
-          </SentryTrackingProvider>
-        </OnboardingContextProvider>
-        {USE_REACT_QUERY_DEVTOOL && (
-          <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
-        )}
-      </ThemeAndStyleProvider>
+      <FrontendVersionProvider releaseVersion={SENTRY_RELEASE_VERSION ?? null}>
+        <ThemeAndStyleProvider>
+          <OnboardingContextProvider>
+            <SentryTrackingProvider>
+              <RouterProvider router={router} />
+            </SentryTrackingProvider>
+          </OnboardingContextProvider>
+          {USE_REACT_QUERY_DEVTOOL && (
+            <ReactQueryDevtools initialIsOpen={false} buttonPosition="bottom-left" />
+          )}
+        </ThemeAndStyleProvider>
+      </FrontendVersionProvider>
     </AppQueryClientProvider>
   );
 }
