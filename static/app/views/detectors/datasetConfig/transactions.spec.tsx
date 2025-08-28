42f98068431c8b509953ed4bf4f0fc9034c1174a
Scott Cooper <scttcper@gmail.com>
2025-08-28 11:58:25 -0700

    add unit test for on-demand-metrics

--- static/app/views/detectors/datasetConfig/transactions.spec.tsx
+++ static/app/views/detectors/datasetConfig/transactions.spec.tsx
@@ -53,7 +53,7 @@ describe('DetectorTransactionsConfig', () => {
       });
 
       expect(key[0]).toBe(`/organizations/${organization.slug}/events-stats/`);
-      const params = (key[1] as any).query;
+      const params = key[1]!.query!;
       expect(params.dataset).toBe(DiscoverDatasets.METRICS_ENHANCED);
       expect(params.query).toBe('event.type:transaction transaction.duration:>0');
       expect(params.interval).toBe('1m');
@@ -75,8 +75,32 @@ describe('DetectorTransactionsConfig', () => {
         comparisonDelta: undefined,
       });
 
-      const params = (key[1] as any).query;
+      const params = key[1]!.query!;
       expect(params.statsPeriod).toBe('9998m');
+    });
+
+    it('on-demand success (apdex) returns METRICS_ENHANCED and prefixed query', () => {
+      const orgWithFeature = OrganizationFixture({
+        features: ['on-demand-metrics-extraction', 'on-demand-metrics-ui'],
+      });
+
+      const key = DetectorTransactionsConfig.getSeriesQueryOptions({
+        organization: orgWithFeature,
+        aggregate: 'apdex()',
+        interval: 60,
+        query: 'transaction.duration:>0',
+        environment: '',
+        projectId: '1',
+        dataset: Dataset.GENERIC_METRICS,
+        eventTypes: [EventTypes.TRANSACTION],
+        statsPeriod: '6h',
+        comparisonDelta: undefined,
+      });
+
+      const params = key[1]!.query!;
+      expect(params.dataset).toBe(DiscoverDatasets.METRICS_ENHANCED);
+      expect(params.query).toBe('event.type:transaction transaction.duration:>0');
+      expect(params.interval).toBe('1m');
     });
   });
 });
