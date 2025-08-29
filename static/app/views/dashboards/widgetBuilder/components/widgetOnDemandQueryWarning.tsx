3e6c43912b0536c6b527c43924734d2842db9233
Nar Saynorath <nar.saynorath@sentry.io>
2025-08-29 09:57:51 -0400

    Clean up on demand warning interface

--- static/app/views/dashboards/widgetBuilder/components/widgetOnDemandQueryWarning.tsx
+++ static/app/views/dashboards/widgetBuilder/components/widgetOnDemandQueryWarning.tsx
@@ -1,8 +1,5 @@
-import type {Location} from 'history';
-
 import {OnDemandWarningIcon} from 'sentry/components/alerts/onDemandMetricAlert';
 import {tct} from 'sentry/locale';
-import type {PageFilters} from 'sentry/types/core';
 import {isOnDemandQueryString} from 'sentry/utils/onDemandMetrics';
 import {hasOnDemandMetricWidgetFeature} from 'sentry/utils/onDemandMetrics/features';
 import type {UseApiQueryResult} from 'sentry/utils/queryClient';
@@ -10,33 +7,14 @@ import type RequestError from 'sentry/utils/requestError/requestError';
 import useOrganization from 'sentry/utils/useOrganization';
 import {
   OnDemandExtractionState,
-  type DashboardFilters,
   type ValidateWidgetResponse,
   type WidgetQuery,
-  type WidgetType,
 } from 'sentry/views/dashboards/types';
-
-interface Props {
-  canAddSearchConditions: boolean;
-  hideLegendAlias: boolean;
-  location: Location;
-  onAddSearchConditions: () => void;
-  onQueryChange: (queryIndex: number, newQuery: WidgetQuery) => void;
-  onQueryConditionChange: (isQueryConditionValid: boolean) => void;
-  onQueryRemove: (queryIndex: number) => void;
-  queries: WidgetQuery[];
-  selection: PageFilters;
-  validatedWidgetResponse: UseApiQueryResult<ValidateWidgetResponse, RequestError>;
-  widgetType: WidgetType;
-  dashboardFilters?: DashboardFilters;
-  projectIds?: number[] | readonly number[];
-  queryErrors?: Array<Record<string, any>>;
-}
 
 export function WidgetOnDemandQueryWarning(props: {
   query: WidgetQuery;
   queryIndex: number;
-  validatedWidgetResponse: Props['validatedWidgetResponse'];
+  validatedWidgetResponse: UseApiQueryResult<ValidateWidgetResponse, RequestError>;
 }) {
   const organization = useOrganization();
   if (!hasOnDemandMetricWidgetFeature(organization)) {
