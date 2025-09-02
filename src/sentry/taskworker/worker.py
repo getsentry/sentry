5fbc6512ab1bd8fa59c641b915477525760d6d8b
John <john.yang@sentry.io>
2025-09-02 10:26:40 -0700

    feat(taskworker): add grpc healthcheck for worker (#98559)
    Let worker grpc client touch a healthcheck file every couple of requests. If the worker deadlocks, k8s will be able to recycle it.

--- src/sentry/taskworker/worker.py
+++ src/sentry/taskworker/worker.py
@@ -9,6 +9,7 @@ import time
 from concurrent.futures import ThreadPoolExecutor
 from multiprocessing.context import ForkContext, SpawnContext
 from multiprocessing.process import BaseProcess
+from pathlib import Path
 from typing import Any
 
 import grpc
@@ -16,11 +17,16 @@ from django.conf import settings
 from sentry_protos.taskbroker.v1.taskbroker_pb2 import FetchNextTask
 
 from sentry import options
-from sentry.taskworker.client.client import HostTemporarilyUnavailable, TaskworkerClient
+from sentry.taskworker.client.client import (
+    HealthCheckSettings,
+    HostTemporarilyUnavailable,
+    TaskworkerClient,
+)
 from sentry.taskworker.client.inflight_task_activation import InflightTaskActivation
 from sentry.taskworker.client.processing_result import ProcessingResult
 from sentry.taskworker.constants import (
     DEFAULT_REBALANCE_AFTER,
+    DEFAULT_WORKER_HEALTH_CHECK_REQ_PER_TOUCH,
     DEFAULT_WORKER_QUEUE_SIZE,
     MAX_BACKOFF_SECONDS_WHEN_HOST_UNAVAILABLE,
 )
@@ -54,13 +60,23 @@ class TaskWorker:
         rebalance_after: int = DEFAULT_REBALANCE_AFTER,
         processing_pool_name: str | None = None,
         process_type: str = "spawn",
+        health_check_file_path: str | None = None,
+        health_check_req_per_touch: int = DEFAULT_WORKER_HEALTH_CHECK_REQ_PER_TOUCH,
         **options: dict[str, Any],
     ) -> None:
         self.options = options
         self._max_child_task_count = max_child_task_count
         self._namespace = namespace
         self._concurrency = concurrency
-        self.client = TaskworkerClient(broker_hosts, rebalance_after)
+        self.client = TaskworkerClient(
+            broker_hosts,
+            rebalance_after,
+            health_check_settings=(
+                None
+                if health_check_file_path is None
+                else HealthCheckSettings(Path(health_check_file_path), health_check_req_per_touch)
+            ),
+        )
         if process_type == "fork":
             self.mp_context = multiprocessing.get_context("fork")
         elif process_type == "spawn":
