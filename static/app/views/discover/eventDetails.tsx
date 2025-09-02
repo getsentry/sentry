ad61f754afe6e4fa35453f906daf22b71ec0c814
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-09-02 18:35:10 -0400

    fix(discover-event-details): Re-routing links pointing to discover event details

---
+++ static/app/views/discover/eventDetails.tsx
@@ -0,0 +1,102 @@
+import {useEffect} from 'react';
+import styled from '@emotion/styled';
+
+import {Alert} from 'sentry/components/core/alert';
+import NotFound from 'sentry/components/errors/notFound';
+import {getEventTimestampInSeconds} from 'sentry/components/events/interfaces/utils';
+import LoadingError from 'sentry/components/loadingError';
+import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
+import {t} from 'sentry/locale';
+import type {Event} from 'sentry/types/event';
+import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
+import {useApiQuery} from 'sentry/utils/queryClient';
+import {useLocation} from 'sentry/utils/useLocation';
+import {useNavigate} from 'sentry/utils/useNavigate';
+import useOrganization from 'sentry/utils/useOrganization';
+import {getTraceDetailsUrl} from 'sentry/views/performance/traceDetails/utils';
+
+type Props = RouteComponentProps<{eventSlug: string}>;
+
+function EventDetails({params}: Props) {
+  const organization = useOrganization();
+  const location = useLocation();
+  const eventSlug = typeof params.eventSlug === 'string' ? params.eventSlug.trim() : '';
+  const datetimeSelection = normalizeDateTimeParams(location.query);
+  const navigate = useNavigate();
+
+  const {
+    data: event,
+    isPending,
+    error,
+  } = useApiQuery<Event>(
+    [`/organizations/${organization.slug}/events/${eventSlug}/`],
+    {staleTime: 2 * 60 * 1000} // 2 minutes in milliseonds
+  );
+
+  useEffect(() => {
+    if (!event) return;
+
+    const traceId = event.contexts.trace?.trace_id ?? '';
+    const timestamp = getEventTimestampInSeconds(event);
+
+    if (event?.groupID && event?.eventID) {
+      navigate({
+        pathname: `/organizations/${organization.slug}/issues/${event.groupID}/events/${event.eventID}`,
+      });
+    } else {
+      navigate(
+        getTraceDetailsUrl({
+          organization,
+          traceSlug: traceId,
+          dateSelection: datetimeSelection,
+          timestamp,
+          eventId: event.eventID,
+          location,
+        })
+      );
+    }
+  }, [event, organization, datetimeSelection, location, navigate]);
+
+  if (isPending) {
+    return (
+      <LoadingWrapper>
+        <LoadingIndicator />
+      </LoadingWrapper>
+    );
+  }
+
+  if (error) {
+    const notFound = error.status === 404;
+    const permissionDenied = error.status === 403;
+
+    if (notFound) {
+      return <NotFound />;
+    }
+
+    if (permissionDenied) {
+      return (
+        <LoadingError message={t('You do not have permission to view that event.')} />
+      );
+    }
+
+    return (
+      <Alert.Container>
+        <Alert type="error">{error.message}</Alert>
+      </Alert.Container>
+    );
+  }
+
+  // This is only reachable if the event hasn't loaded for an unknown reason
+  return <LoadingError message={t('Failed to load the details for the event')} />;
+}
+
+const LoadingWrapper = styled('div')`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  height: 100%;
+`;
+
+export default EventDetails;
