f7c03d40593462489d87239cf05a3c53a3b405a7
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2025-09-02 14:53:58 -0400

    feat(insights): Web Vitals initiate Seer Suggestions flow (#98543)
    Updates the Web Vitals Page Overview sidebar to include a "Run Seer
    Analysis" button when there is no issue/autofix run created for the
    page. The button creates an issue for each Web Vital with a score under
    90 and runs autofix on them:
    - Deletes deprecated "Create Issue" button in individual Web Vital
    slideouts
    - Updates data querying logic for fetching Issues and Autofix runs. The
    data querying logic now handles two scenarios:
    - Scenario A: When the user clicks the "Run Seer Analysis" to create new
    issues/autofix run
    - The `/issues` api call now polls until the number of issue results
    matches the expected number of created issues. (We use polling because
    issue creation is async)
    - Once all issues are fetched, we start polling `/autofix` for each
    issue until we reach a non `PROCESSING` state (indicating the autofix
    run has stopped/finished). We display a root cause description if
    available during polling.
    
    - Scenario B: When a user visits the Page Overview page and at least one
    unresolved web vital issue has been created.
            - `/issues` is only called once.
    - We poll `/autofix` for each existing issue until we reach a non
    `PROCESSING` state, same as in Scenario A.

--- static/app/views/insights/browser/webVitals/queries/useWebVitalsIssuesQuery.tsx
+++ static/app/views/insights/browser/webVitals/queries/useWebVitalsIssuesQuery.tsx
@@ -1,47 +1,83 @@
+import {useCallback} from 'react';
+
 import {normalizeDateTimeParams} from 'sentry/components/organizations/pageFilters/parse';
-import type {Group, ISSUE_TYPE_TO_ISSUE_TITLE} from 'sentry/types/group';
+import {IssueType, type Group, type ISSUE_TYPE_TO_ISSUE_TITLE} from 'sentry/types/group';
 import {defined} from 'sentry/utils';
-import {useApiQuery} from 'sentry/utils/queryClient';
+import {useApiQuery, useQueryClient, type ApiQueryKey} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 
-export function getIssueQueryFilter({
-  issueTypes,
-  transaction,
-}: {
-  issueTypes: Array<keyof typeof ISSUE_TYPE_TO_ISSUE_TITLE>;
+export const POLL_INTERVAL = 1000;
+
+const DEFAULT_ISSUE_TYPES = [IssueType.WEB_VITALS];
+
+type QueryProps = {
+  issueTypes?: Array<keyof typeof ISSUE_TYPE_TO_ISSUE_TITLE>;
   transaction?: string;
-}) {
+};
+
+export function getIssueQueryFilter({
+  issueTypes = DEFAULT_ISSUE_TYPES,
+  transaction,
+}: QueryProps) {
   return `is:unresolved issue.type:[${issueTypes?.join(',')}]${defined(transaction) ? ` transaction:${transaction}` : ''}`;
 }
 
-export function useWebVitalsIssuesQuery({
-  issueTypes,
+function useWebVitalsIssuesQueryKey({
+  issueTypes = DEFAULT_ISSUE_TYPES,
   transaction,
-  enabled = true,
-}: {
-  issueTypes: Array<keyof typeof ISSUE_TYPE_TO_ISSUE_TITLE>;
-  enabled?: boolean;
-  transaction?: string;
-}) {
+}: QueryProps): ApiQueryKey {
   const organization = useOrganization();
   const {selection} = usePageFilters();
-  return useApiQuery<Group[]>(
-    [
-      `/organizations/${organization.slug}/issues/`,
-      {
-        query: {
-          query: getIssueQueryFilter({issueTypes, transaction}),
-          project: selection.projects,
-          environment: selection.environments,
-          ...normalizeDateTimeParams(selection.datetime),
-          per_page: 6,
-        },
-      },
-    ],
+  return [
+    `/organizations/${organization.slug}/issues/`,
     {
-      staleTime: 0,
-      enabled: Boolean(issueTypes?.length) && enabled,
-    }
-  );
+      query: {
+        query: getIssueQueryFilter({issueTypes, transaction}),
+        project: selection.projects,
+        environment: selection.environments,
+        ...normalizeDateTimeParams(selection.datetime),
+        per_page: 6,
+      },
+    },
+  ];
+}
+
+export function useInvalidateWebVitalsIssuesQuery({
+  issueTypes = DEFAULT_ISSUE_TYPES,
+  transaction,
+}: QueryProps) {
+  const queryClient = useQueryClient();
+  const queryKey = useWebVitalsIssuesQueryKey({issueTypes, transaction});
+  return useCallback(() => {
+    queryClient.setQueryData(queryKey, undefined);
+    queryClient.invalidateQueries({queryKey});
+  }, [queryClient, queryKey]);
+}
+
+export function useWebVitalsIssuesQuery({
+  issueTypes = DEFAULT_ISSUE_TYPES,
+  transaction,
+  enabled = true,
+  pollInterval,
+  eventIds,
+}: QueryProps & {
+  enabled?: boolean;
+  eventIds?: string[];
+  pollInterval?: number;
+}) {
+  return useApiQuery<Group[]>(useWebVitalsIssuesQueryKey({issueTypes, transaction}), {
+    staleTime: 0,
+    enabled: Boolean(issueTypes?.length) && enabled,
+    refetchInterval: query => {
+      // Poll until the number of issues in the results array matches the number of expected eventIds.
+      if (!pollInterval || !eventIds) {
+        return false;
+      }
+      const result = query.state.data?.[0];
+      return result && Array.isArray(result) && result.length < eventIds.length
+        ? pollInterval
+        : false;
+    },
+  });
 }
