98502bf3d6cdef0ac3530608c84a525a2b5206d3
Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>
2025-09-03 15:56:17 -0700

    fix(replay): add env to selector endpoint validator

--- src/sentry/replays/validators.py
+++ src/sentry/replays/validators.py
@@ -124,6 +124,7 @@ class ReplaySelectorValidator(serializers.Serializer):
         child=serializers.CharField(),
         help_text=("A list of project slugs to filter your results by."),
     )
+    environment = serializers.ListField(help_text="The environment to filter by.", required=False)
     sort = serializers.CharField(help_text="The field to sort the output by.", required=False)
     sortBy = serializers.CharField(help_text="The field to sort the output by.", required=False)
     orderBy = serializers.CharField(help_text="The field to sort the output by.", required=False)
