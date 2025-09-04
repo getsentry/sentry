785af61d6b1fac42a98cf90cbb915e6836467594
Fabian Schindler <fabian.schindler@sentry.io>
2025-09-04 14:45:41 +0200

    fix(analytics): properly wrap several events recordings with try/except

--- src/sentry/receivers/experiments.py
+++ src/sentry/receivers/experiments.py
@@ -1,3 +1,5 @@
+import sentry_sdk
+
 from sentry import analytics
 from sentry.analytics.events.join_request_created import JoinRequestCreatedEvent
 from sentry.analytics.events.join_request_link_viewed import JoinRequestLinkViewedEvent
@@ -7,27 +9,36 @@ from sentry.signals import join_request_created, join_request_link_viewed, user_
 
 @join_request_created.connect(weak=False)
 def record_join_request_created(member, **kwargs):
-    analytics.record(
-        JoinRequestCreatedEvent(
-            member_id=member.id,
-            organization_id=member.organization_id,
-            referrer=kwargs.get("referrer"),
+    try:
+        analytics.record(
+            JoinRequestCreatedEvent(
+                member_id=member.id,
+                organization_id=member.organization_id,
+                referrer=kwargs.get("referrer"),
+            )
         )
-    )
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
 
 
 @join_request_link_viewed.connect(weak=False)
 def record_join_request_link_viewed(organization, **kwargs):
-    analytics.record(JoinRequestLinkViewedEvent(organization_id=organization.id))
+    try:
+        analytics.record(JoinRequestLinkViewedEvent(organization_id=organization.id))
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
 
 
 @user_signup.connect(weak=False)
 def record_user_signup(user, source, **kwargs):
-    analytics.record(
-        UserSignUpEvent(
-            user_id=user.id,
-            source=source,
-            provider=kwargs.get("provider"),
-            referrer=kwargs.get("referrer"),
+    try:
+        analytics.record(
+            UserSignUpEvent(
+                user_id=user.id,
+                source=source,
+                provider=kwargs.get("provider"),
+                referrer=kwargs.get("referrer"),
+            )
         )
-    )
+    except Exception as e:
+        sentry_sdk.capture_exception(e)
