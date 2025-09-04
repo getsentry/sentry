e983843e73273ff7abc0081e4bccc60d1e79ca54
nikkikapadia <nikki.kapadia@sentry.io>
2025-09-04 12:18:49 -0400

    final tweaks on discover queries translation

--- src/sentry/explore/translation/discover_translation.py
+++ src/sentry/explore/translation/discover_translation.py
@@ -3,13 +3,20 @@ from typing import Any
 
 from sentry.discover.arithmetic import is_equation
 from sentry.discover.models import DiscoverSavedQuery
-from sentry.discover.translation.mep_to_eap import (
-    QueryParts,
-    translate_columns,
-    translate_mep_to_eap,
-)
-from sentry.explore.models import ExploreSavedQuery
+from sentry.discover.translation.mep_to_eap import QueryParts, translate_mep_to_eap
+from sentry.explore.models import ExploreSavedQuery, ExploreSavedQueryDataset
 from sentry.integrations.slack.unfurl.discover import is_aggregate
+
+# we're going to keep the chart types from discover
+# bar = 0, line = 1, area = 2
+CHART_TYPES = {
+    "default": 2,
+    "previous": 2,
+    "top5": 2,
+    "daily": 0,
+    "dailytop5": 2,
+    "bar": 0,
+}
 
 
 def _get_translated_orderby_item(orderby, columns, is_negated):
@@ -79,86 +86,91 @@ def _translate_discover_query_field_to_explore_query_schema(
     conditions = query.get("query", "")
     # have to separate equations and fields
     fields = query.get("fields", [])
-    columns = [field for field in fields if not is_equation(field)]
-    equations = [field for field in fields if is_equation(field)]
-    # orderby functions can be formated like -count_unique_user_id for count_unique(user.id)
-    # this does not apply to fields and equations
+    visualized_fields = query.get("yAxis", [])
+    # in explore there is no concept of chart only (yaxis) fields or table only fields,
+    # so we're just adding all the fields into the columns/equations lists
+    columns = [field for field in fields if not is_equation(field)] + [
+        field for field in visualized_fields if not is_equation(field)
+    ]
+    equations = [field for field in fields if is_equation(field)] + [
+        field for field in visualized_fields if is_equation(field)
+    ]
+
     orderby = query.get("orderby", "")
-    # need to make sure all orderby functions are in the correct format
+    # need to make sure all orderby functions are in the correct format (i.e. not in -count_unique_user_id format)
     orderby_converted_list = _format_orderby_for_translation(orderby, columns)
 
     translated_query_parts = translate_mep_to_eap(
         QueryParts(
-            selected_columns=fields,
+            selected_columns=columns,
             query=conditions,
             equations=equations,
             orderby=orderby_converted_list,
         )
     )
 
-    aggregate_fields = [
-        field for field in translated_query_parts["selected_columns"] if is_aggregate(field)
-    ]
+    translated_aggregate_columns = (
+        [field for field in translated_query_parts["selected_columns"] if is_aggregate(field)]
+        if translated_query_parts["selected_columns"] is not None
+        else []
+    )
 
-    # TODO: if it is in aggregate mode we need to fill out aggregateField which is an array of
-    # {groupBy: name} or {yAxes: [name], chartType: type}
-    # IF not in aggregate mode we need to fill out `fields` array
+    translated_non_aggregate_columns = (
+        [field for field in translated_query_parts["selected_columns"] if not is_aggregate(field)]
+        if translated_query_parts["selected_columns"] is not None
+        else []
+    )
 
-    # discover group by is all the fields that are not aggregates or equations
-    # this should go after finding out display type
-    groupby = [
-        field
-        for field in translated_query_parts["selected_columns"]
-        if not (is_aggregate(field) or is_equation(field))
-    ]
-    if len(aggregate_fields) > 0:
+    translated_equations = (
+        [field for field in translated_query_parts["equations"]]
+        if translated_query_parts["equations"] is not None
+        else []
+    )
+
+    # if we have any aggregates or equation we should be in aggregate mode
+    if len(translated_aggregate_columns) > 0 or len(translated_equations) > 0:
         mode = "aggregate"
     else:
-        # if we're in samples mode we don't need a group by
         mode = "samples"
-        groupby = []
 
     display = query.get("display", "default")
     interval = None
 
-    match display:
-        case "default":
-            chart_type = "area"
-        case "previous":
-            chart_type = "area"
-        case "top5":
-            # don't need to update group by because we cannot have top5 without an aggregate
-            chart_type = "area"
-        case "daily":
-            chart_type = "bar"
-            interval = "1d"
-        case "dailytop5":
-            chart_type = "area"
-            interval = "1d"
-        case "bar":
-            chart_type = "bar"
-        case _:
-            chart_type = "area"
+    chart_type = CHART_TYPES[display]
+    # only intervals that matter are the daily ones, rest can be defaulted to explore default
+    if display in ["daily", "dailytop5"]:
+        interval = "1d"
 
-    # yAxis can be equations too
-    y_axes = translate_columns([yaxis for yaxis in query.get("yAxis", [])])
-    # double check this to make sure it's right
-    visualize = [{"chartType": chart_type, "yAxes": y_axes}]
+    y_axes = translated_aggregate_columns + translated_equations
+    # aggregate fields parameter contains groupBys and yAxes
+    aggregate_fields = [
+        {"groupBy": translated_column} for translated_column in translated_non_aggregate_columns
+    ] + [{"yAxes": [y_axis], "chartType": chart_type} for y_axis in y_axes]
+
+    # we want to make sure the id field is always included in samples mode
+    # because without it the 'id' field is not sortable on the samples table
+    fields_with_id = (
+        (["id"] + translated_non_aggregate_columns)
+        if "id" not in translated_non_aggregate_columns
+        else translated_non_aggregate_columns
+    )
 
     query_list = [
         {
             "query": translated_query_parts["query"],
-            "fields": translated_query_parts["selected_columns"],
+            "fields": fields_with_id,
             "orderby": (
                 translated_query_parts["orderby"][0]
-                if len(translated_query_parts["orderby"]) > 0
+                if len(translated_query_parts["orderby"]) > 0 and mode == "samples"
                 else None
             ),
-            "groupby": groupby,
             "mode": mode,
-            "visualize": visualize,  # this should no longer be used, now we have aggregateField
-            # aggregateField: []
-            #
+            "aggregateField": aggregate_fields,
+            "aggregateOrderby": (
+                translated_query_parts["orderby"][0]
+                if len(translated_query_parts["orderby"]) > 0 and mode == "aggregate"
+                else None
+            ),
         }
     ]
 
@@ -188,11 +200,12 @@ def translate_discover_query_to_explore_query(
         created_by_id=discover_query.created_by_id,
         visits=discover_query.visits,
         last_visited=discover_query.last_visited,
-        dataset=101,
+        dataset=ExploreSavedQueryDataset.SEGMENT_SPANS,
         is_multi_query=False,
         organization=discover_query.organization,
         name=discover_query.name,
         query=translated_query_field,
+        # TODO: add changed_reason (after making updated to translation layer)
     )
 
     return new_explore_query
