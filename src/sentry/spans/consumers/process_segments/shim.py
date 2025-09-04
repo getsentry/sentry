f4dfb9ad02f238db1a0fa48a66c922c3c61a8fb6
Joris Bayer <joris.bayer@sentry.io>
2025-09-04 10:59:44 +0200

    ref(spans): Separate span tree enrichment from shimming (#98693)
    This PR contains no functional changes. Its only purpose is to clearly
    separate two enrichment steps in the segment consumer:
    
    1. Enrich spans with information from the span tree (inherit from
    segment, exclusive time from children, etc.)
    2. Shim the Span V1 format to be compatible with the events processing
    pipeline, for logic that is shared between the two pipelines.

---
+++ src/sentry/spans/consumers/process_segments/shim.py
@@ -0,0 +1,103 @@
+"""Functions for making span data compatible with the old event processing pipeline.
+
+This is only necessary for logic thatis shared between the event processing pipeline and the span processing pipeline,
+and thus cannot (yet) be refactored to use the new span schema.
+"""
+
+import uuid
+from copy import deepcopy
+from typing import Any, cast
+
+from sentry_kafka_schemas.schema_types.buffered_segments_v1 import _SentryExtractedTags
+
+from sentry.performance_issues.types import SentryTags as PerformanceIssuesSentryTags
+from sentry.spans.consumers.process_segments.types import CompatibleSpan, TreeSpan, get_span_op
+from sentry.utils.dates import to_datetime
+
+
+def make_compatible(span: TreeSpan) -> CompatibleSpan:
+    # Creates attributes for EAP spans that are required by logic shared with the
+    # event pipeline.
+    #
+    # Spans in the transaction event protocol had a slightly different schema
+    # compared to raw spans on the EAP topic. This function adds the missing
+    # attributes to the spans to make them compatible with the event pipeline
+    # logic.
+    ret = CompatibleSpan(
+        **span,
+        sentry_tags=_sentry_tags(span.get("data") or {}),
+        op=get_span_op(span),
+        # Note: Event protocol spans expect `exclusive_time` while EAP expects
+        # `exclusive_time_ms`. Both are the same value in milliseconds
+        exclusive_time=span["exclusive_time_ms"],
+    )
+
+    return ret
+
+
+def _sentry_tags(data: dict[str, Any]) -> _SentryExtractedTags:
+    """Backfill sentry tags used in performance issue detection.
+
+    Once performance issue detection is only called from process_segments,
+    (not from event_manager), the performance issues code can be refactored to access
+    span attributes instead of sentry_tags.
+    """
+    sentry_tags: _SentryExtractedTags = {}
+    for tag_key in PerformanceIssuesSentryTags.__mutable_keys__:
+        data_key = (
+            "sentry.normalized_description" if tag_key == "description" else f"sentry.{tag_key}"
+        )
+        if data_key in data:
+            sentry_tags[tag_key] = data[data_key]  # type: ignore[literal-required]
+
+    return sentry_tags
+
+
+def build_shim_event_data(
+    segment_span: CompatibleSpan, spans: list[CompatibleSpan]
+) -> dict[str, Any]:
+    """Create a shimmed event payload for performance issue detection."""
+    data = segment_span.get("data", {})
+
+    event: dict[str, Any] = {
+        "type": "transaction",
+        "level": "info",
+        "contexts": {
+            "trace": {
+                "trace_id": segment_span["trace_id"],
+                "type": "trace",
+                "op": data.get("sentry.transaction.op"),
+                "span_id": segment_span["span_id"],
+                "hash": segment_span["hash"],
+            },
+        },
+        "event_id": uuid.uuid4().hex,
+        "project_id": segment_span["project_id"],
+        "transaction": data.get("sentry.transaction"),
+        "release": data.get("sentry.release"),
+        "dist": data.get("sentry.dist"),
+        "environment": data.get("sentry.environment"),
+        "platform": data.get("sentry.platform"),
+        "tags": [["environment", data.get("sentry.environment")]],
+        "received": segment_span["received"],
+        "timestamp": segment_span["end_timestamp_precise"],
+        "start_timestamp": segment_span["start_timestamp_precise"],
+        "datetime": to_datetime(segment_span["end_timestamp_precise"]).strftime(
+            "%Y-%m-%dT%H:%M:%SZ"
+        ),
+        "spans": [],
+    }
+
+    if (profile_id := segment_span.get("profile_id")) is not None:
+        event["contexts"]["profile"] = {"profile_id": profile_id, "type": "profile"}
+
+    # Add legacy span attributes required only by issue detectors. As opposed to
+    # real event payloads, this also adds the segment span so detectors can run
+    # topological sorting on the span tree.
+    for span in spans:
+        event_span = cast(dict[str, Any], deepcopy(span))
+        event_span["start_timestamp"] = span["start_timestamp_precise"]
+        event_span["timestamp"] = span["end_timestamp_precise"]
+        event["spans"].append(event_span)
+
+    return event
--- tests/sentry/spans/consumers/process_segments/test_enrichment.py
+++ tests/sentry/spans/consumers/process_segments/test_enrichment.py
@@ -1,4 +1,5 @@
-from sentry.spans.consumers.process_segments.enrichment import Enricher, compute_breakdowns
+from sentry.spans.consumers.process_segments.enrichment import TreeEnricher, compute_breakdowns
+from sentry.spans.consumers.process_segments.shim import make_compatible
 from tests.sentry.spans.consumers.process import build_mock_span
 
 # Tests ported from Relay
@@ -36,9 +37,10 @@ def test_childless_spans() -> None:
         ),
     ]
 
-    _, enriched = Enricher.enrich_spans(spans)
+    _, enriched = TreeEnricher.enrich_spans(spans)
+    enriched = [make_compatible(span) for span in enriched]
 
-    exclusive_times = {span["span_id"]: span["exclusive_time"] for span in enriched}
+    exclusive_times = {span["span_id"]: span["exclusive_time_ms"] for span in enriched}
     assert exclusive_times == {
         "aaaaaaaaaaaaaaaa": 1123.0,
         "bbbbbbbbbbbbbbbb": 3000.0,
@@ -79,9 +81,9 @@ def test_nested_spans() -> None:
         ),
     ]
 
-    _, enriched = Enricher.enrich_spans(spans)
+    _, enriched = TreeEnricher.enrich_spans(spans)
 
-    exclusive_times = {span["span_id"]: span["exclusive_time"] for span in enriched}
+    exclusive_times = {span["span_id"]: span["exclusive_time_ms"] for span in enriched}
     assert exclusive_times == {
         "aaaaaaaaaaaaaaaa": 4000.0,
         "bbbbbbbbbbbbbbbb": 400.0,
@@ -122,9 +124,9 @@ def test_overlapping_child_spans() -> None:
         ),
     ]
 
-    _, enriched = Enricher.enrich_spans(spans)
+    _, enriched = TreeEnricher.enrich_spans(spans)
 
-    exclusive_times = {span["span_id"]: span["exclusive_time"] for span in enriched}
+    exclusive_times = {span["span_id"]: span["exclusive_time_ms"] for span in enriched}
     assert exclusive_times == {
         "aaaaaaaaaaaaaaaa": 4000.0,
         "bbbbbbbbbbbbbbbb": 400.0,
@@ -165,9 +167,9 @@ def test_child_spans_dont_intersect_parent() -> None:
         ),
     ]
 
-    _, enriched = Enricher.enrich_spans(spans)
+    _, enriched = TreeEnricher.enrich_spans(spans)
 
-    exclusive_times = {span["span_id"]: span["exclusive_time"] for span in enriched}
+    exclusive_times = {span["span_id"]: span["exclusive_time_ms"] for span in enriched}
     assert exclusive_times == {
         "aaaaaaaaaaaaaaaa": 4000.0,
         "bbbbbbbbbbbbbbbb": 1000.0,
@@ -208,9 +210,9 @@ def test_child_spans_extend_beyond_parent() -> None:
         ),
     ]
 
-    _, enriched = Enricher.enrich_spans(spans)
+    _, enriched = TreeEnricher.enrich_spans(spans)
 
-    exclusive_times = {span["span_id"]: span["exclusive_time"] for span in enriched}
+    exclusive_times = {span["span_id"]: span["exclusive_time_ms"] for span in enriched}
     assert exclusive_times == {
         "aaaaaaaaaaaaaaaa": 4000.0,
         "bbbbbbbbbbbbbbbb": 200.0,
@@ -251,9 +253,9 @@ def test_child_spans_consumes_all_of_parent() -> None:
         ),
     ]
 
-    _, enriched = Enricher.enrich_spans(spans)
+    _, enriched = TreeEnricher.enrich_spans(spans)
 
-    exclusive_times = {span["span_id"]: span["exclusive_time"] for span in enriched}
+    exclusive_times = {span["span_id"]: span["exclusive_time_ms"] for span in enriched}
     assert exclusive_times == {
         "aaaaaaaaaaaaaaaa": 4000.0,
         "bbbbbbbbbbbbbbbb": 0.0,
@@ -294,9 +296,9 @@ def test_only_immediate_child_spans_affect_calculation() -> None:
         ),
     ]
 
-    _, enriched = Enricher.enrich_spans(spans)
+    _, enriched = TreeEnricher.enrich_spans(spans)
 
-    exclusive_times = {span["span_id"]: span["exclusive_time"] for span in enriched}
+    exclusive_times = {span["span_id"]: span["exclusive_time_ms"] for span in enriched}
     assert exclusive_times == {
         "aaaaaaaaaaaaaaaa": 4000.0,
         "bbbbbbbbbbbbbbbb": 600.0,
@@ -364,7 +366,7 @@ def test_emit_ops_breakdown() -> None:
     }
 
     # Compute breakdowns for the segment span
-    _ = Enricher.enrich_spans(spans)
+    _ = TreeEnricher.enrich_spans(spans)
     updates = compute_breakdowns(spans, breakdowns_config)
 
     assert updates["span_ops.ops.http"] == 3600000.0
@@ -401,7 +403,8 @@ def test_write_tags_for_performance_issue_detection():
         segment_span,
     ]
 
-    _, spans = Enricher.enrich_spans(spans)
+    _, spans = TreeEnricher.enrich_spans(spans)
+    spans = [make_compatible(span) for span in spans]
 
     child_span, segment_span = spans
 
