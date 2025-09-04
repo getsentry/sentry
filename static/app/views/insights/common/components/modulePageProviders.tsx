956800ee633792b85a39cae05c9f15447347e22d
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-09-04 09:27:10 -0500

    remove date range limit logic

--- static/app/views/insights/common/components/modulePageProviders.tsx
+++ static/app/views/insights/common/components/modulePageProviders.tsx
@@ -8,11 +8,7 @@ import {WidgetSyncContextProvider} from 'sentry/views/dashboards/contexts/widget
 import {useHasDataTrackAnalytics} from 'sentry/views/insights/common/utils/useHasDataTrackAnalytics';
 import {useModuleTitles} from 'sentry/views/insights/common/utils/useModuleTitle';
 import {useDomainViewFilters} from 'sentry/views/insights/pages/useFilters';
-import {
-  INSIGHTS_TITLE,
-  OLD_QUERY_DATE_RANGE_LIMIT,
-  QUERY_DATE_RANGE_LIMIT,
-} from 'sentry/views/insights/settings';
+import {INSIGHTS_TITLE, QUERY_DATE_RANGE_LIMIT} from 'sentry/views/insights/settings';
 import type {ModuleName} from 'sentry/views/insights/types';
 
 type ModuleNameStrings = `${ModuleName}`;
@@ -38,12 +34,7 @@ export function ModulePageProviders({
   const hasDateRangeQueryLimit = organization.features.includes(
     'insights-query-date-range-limit'
   );
-  const shouldIncreaseDefaultDateRange = organization.features.includes(
-    'dashboards-plan-limits'
-  );
-  const defaultPickableDays = shouldIncreaseDefaultDateRange
-    ? QUERY_DATE_RANGE_LIMIT
-    : OLD_QUERY_DATE_RANGE_LIMIT;
+  const defaultPickableDays = QUERY_DATE_RANGE_LIMIT;
 
   useHasDataTrackAnalytics(moduleName as ModuleName, analyticEventName);
 
