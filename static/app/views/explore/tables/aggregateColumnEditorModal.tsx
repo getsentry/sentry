b80ac9e2f5b6d64adebbc24ec8ae48962d984ce2
Tony Xiao <txiao@sentry.io>
2025-09-04 13:34:08 -0400

    feat(explore): Use query params for spans aggregate fields (#98728)
    This migrates explore traces to use query params for the aggregate
    fields.

--- static/app/views/explore/tables/aggregateColumnEditorModal.tsx
+++ static/app/views/explore/tables/aggregateColumnEditorModal.tsx
@@ -22,6 +22,7 @@ import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {TagCollection} from 'sentry/types/group';
 import type {Organization} from 'sentry/types/organization';
+import {defined} from 'sentry/utils';
 import {
   EQUATION_PREFIX,
   parseFunction,
@@ -34,11 +35,7 @@ import {
 } from 'sentry/utils/fields';
 import useOrganization from 'sentry/utils/useOrganization';
 import {DragNDropContext} from 'sentry/views/explore/contexts/dragNDropContext';
-import type {
-  AggregateField,
-  BaseAggregateField,
-  GroupBy,
-} from 'sentry/views/explore/contexts/pageParamsContext/aggregateFields';
+import type {GroupBy} from 'sentry/views/explore/contexts/pageParamsContext/aggregateFields';
 import {
   isGroupBy,
   isVisualize,
@@ -46,18 +43,27 @@ import {
 import {
   DEFAULT_VISUALIZATION,
   updateVisualizeAggregate,
-  Visualize,
 } from 'sentry/views/explore/contexts/pageParamsContext/visualizes';
 import type {Column} from 'sentry/views/explore/hooks/useDragNDropColumns';
 import {useExploreSuggestedAttribute} from 'sentry/views/explore/hooks/useExploreSuggestedAttribute';
 import {useGroupByFields} from 'sentry/views/explore/hooks/useGroupByFields';
 import {useVisualizeFields} from 'sentry/views/explore/hooks/useVisualizeFields';
+import type {
+  AggregateField,
+  WritableAggregateField,
+} from 'sentry/views/explore/queryParams/aggregateField';
+import {
+  isVisualizeEquation,
+  Visualize,
+  VisualizeEquation,
+  VisualizeFunction,
+} from 'sentry/views/explore/queryParams/visualize';
 import {TraceItemDataset} from 'sentry/views/explore/types';
 
 interface AggregateColumnEditorModalProps extends ModalRenderProps {
   columns: AggregateField[];
   numberTags: TagCollection;
-  onColumnsChange: (columns: BaseAggregateField[]) => void;
+  onColumnsChange: (columns: WritableAggregateField[]) => void;
   stringTags: TagCollection;
 }
 
@@ -82,7 +88,21 @@ export function AggregateColumnEditorModal({
   }, [columns]);
 
   const handleApply = useCallback(() => {
-    onColumnsChange(tempColumns.map(col => (isVisualize(col) ? col.toJSON() : col)));
+    const newColumns: WritableAggregateField[] = [];
+
+    for (const col of tempColumns) {
+      if (isGroupBy(col)) {
+        newColumns.push(col);
+      } else if (isVisualize(col)) {
+        if (defined(col.selectedChartType)) {
+          newColumns.push({yAxes: [col.yAxis], chartType: col.selectedChartType});
+        } else {
+          newColumns.push({yAxes: [col.yAxis]});
+        }
+      }
+    }
+
+    onColumnsChange(newColumns);
     closeModal();
   }, [closeModal, onColumnsChange, tempColumns]);
 
@@ -128,7 +148,8 @@ export function AggregateColumnEditorModal({
                     key: 'add-visualize',
                     label: t('Visualize / Function'),
                     details: t('ex. p50(span.duration)'),
-                    onAction: () => insertColumn(new Visualize(DEFAULT_VISUALIZATION)),
+                    onAction: () =>
+                      insertColumn(new VisualizeFunction(DEFAULT_VISUALIZATION)),
                   },
                   ...(organization.features.includes('visibility-explore-equations')
                     ? [
@@ -136,7 +157,8 @@ export function AggregateColumnEditorModal({
                           key: 'add-equation',
                           label: t('Equation'),
                           details: t('ex. p50(span.duration) / 2'),
-                          onAction: () => insertColumn(new Visualize(EQUATION_PREFIX)),
+                          onAction: () =>
+                            insertColumn(new VisualizeEquation(EQUATION_PREFIX)),
                         },
                       ]
                     : []),
@@ -303,7 +325,7 @@ interface VisualizeSelectorProps {
 }
 
 function VisualizeSelector(props: VisualizeSelectorProps) {
-  if (props.visualize.isEquation) {
+  if (isVisualizeEquation(props.visualize)) {
     return <EquationSelector {...props} />;
   }
 
