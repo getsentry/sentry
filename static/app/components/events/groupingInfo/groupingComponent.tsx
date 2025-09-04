0f006ec5668a03aba9967700ece00408f7e99c36
Shayna Chambless <shayna.chambless@sentry.io>
2025-09-04 13:48:19 -0700

    groupingComponentListItems

--- static/app/components/events/groupingInfo/groupingComponent.tsx
+++ static/app/components/events/groupingInfo/groupingComponent.tsx
@@ -25,6 +25,10 @@ function GroupingComponent({
   onCollapsedChange,
 }: Props) {
   const shouldInlineValue = shouldInlineComponentValue(component);
+  const GroupingComponentListItems =
+    component.id === 'stacktrace'
+      ? GroupingComponentStacktrace
+      : GroupingComponentChildren;
   const isStacktrace = component.id === 'stacktrace';
   const stacktraceValues = isStacktrace
     ? (component.values as EventGroupComponent[])
@@ -49,9 +53,6 @@ function GroupingComponent({
     onCollapsedChange?.(newCollapsed);
   };
 
-  const GroupingComponentListItems = isStacktrace
-    ? GroupingComponentStacktrace
-    : GroupingComponentChildren;
   const stacktraceProps = isStacktrace
     ? {collapsed: isCollapsed, onCollapsedChange: toggleCollapsed}
     : {};
