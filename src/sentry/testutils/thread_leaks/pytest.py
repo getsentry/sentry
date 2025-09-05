621e7a1d75987fcd71d8db5b16ea216f6c272573
kenzoengineer <kenzoengineer@gmail.com>
2025-09-05 15:44:32 -0700

    ignores threads involving django core

--- src/sentry/testutils/thread_leaks/pytest.py
+++ src/sentry/testutils/thread_leaks/pytest.py
@@ -7,6 +7,7 @@ from typing import Any
 import pytest
 
 from . import sentry
+from ._threading import get_thread_function_name
 from .assertion import ThreadLeakAssertionError, assert_none
 
 
@@ -31,6 +32,17 @@ def pytest_runtest_call(item: pytest.Item) -> Generator[dict[str, Any]]:
             yield result
     except ThreadLeakAssertionError as error:
         allowlisted = item.get_closest_marker("thread_leak_allowlist")
+
+        # TODO(DI-1282): ignoring all leaks involving this thread, issue is too widespread to use allowlist marks.
+        # actual fix must be implemented. see issue #98988
+        is_dev_server_thread_leak = any(
+            get_thread_function_name(thread)
+            == "django.core.servers.basehttp.ThreadedWSGIServer.process_request_thread"
+            for thread in error.thread_leaks
+        )
+        if is_dev_server_thread_leak:
+            return result
+
         result["events"] = sentry.capture_event(error.thread_leaks, STRICT, allowlisted, item)
 
         if STRICT and not allowlisted:
