d5e379c201ea4a9246e024140c7360effc9f2985
Shayna Chambless <shayna.chambless@sentry.io>
2025-09-05 14:31:22 -0700

    groupingValue

--- static/app/components/events/groupingInfo/groupingComponent.tsx
+++ static/app/components/events/groupingInfo/groupingComponent.tsx
@@ -1,7 +1,5 @@
-import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
-import {space} from 'sentry/styles/space';
 import type {EventGroupComponent} from 'sentry/types/event';
 
 import GroupingComponentChildren from './groupingComponentChildren';
@@ -48,31 +46,8 @@ const GroupingComponentList = styled('ul')<{isInline: boolean}>`
   }
 `;
 
-export const GroupingValue = styled('code')<{
-  valueType: string;
-  contributes?: boolean;
-}>`
-  display: inline-block;
-  margin: ${space(0.25)} ${space(0.5)} ${space(0.25)} 0;
-  font-size: ${p => p.theme.fontSize.sm};
-  padding: 0 ${space(0.25)};
-  background: ${p => (p.contributes ? 'rgba(112, 163, 214, 0.1)' : 'transparent')};
-  color: ${p => (p.contributes ? p.theme.textColor : p.theme.subText)};
-
-  ${({valueType, theme, contributes}) =>
-    (valueType === 'function' || valueType === 'symbol') &&
-    css`
-      font-weight: ${contributes ? theme.fontWeight.bold : 'normal'};
-      color: ${contributes ? theme.textColor : theme.subText};
-    `}
-`;
-
 const GroupingComponentWrapper = styled('div')<{isContributing: boolean}>`
   color: ${p => (p.isContributing ? p.theme.textColor : p.theme.subText)};
-
-  ${GroupingValue}, button {
-    opacity: 1;
-  }
 `;
 
 export const GroupingHint = styled('small')`
