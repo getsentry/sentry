05de2e8505f636a0298446a933f850ed3390411b
Dan Fuller <dfuller@sentry.io>
2025-09-08 12:43:26 -0700

    chore(releases): Add function for dual writing `Commit` rows (#98777)
    Context:
    https://www.notion.so/sentry/Release-Retention-2028b10e4b5d802ea421c193303becaf
    
    This adds a function we can call to dual write `Commit` rows. I'm
    planning on using functions rather than relying on `post_save` signals
    so that we can use the Commit to create `CommitFileChange` rows too.
    
    <!-- Describe your PR here. -->

---
+++ src/sentry/releases/commits.py
@@ -0,0 +1,53 @@
+import logging
+from datetime import datetime
+
+from django.db import router
+
+from sentry import features
+from sentry.models.commit import Commit as OldCommit
+from sentry.models.commitauthor import CommitAuthor
+from sentry.models.organization import Organization
+from sentry.releases.models import Commit
+from sentry.utils.db import atomic_transaction
+
+logger = logging.getLogger(__name__)
+
+
+def create_commit(
+    organization: Organization,
+    repo_id: int,
+    key: str,
+    message: str | None = None,
+    author: CommitAuthor | None = None,
+    date_added: datetime | None = None,
+) -> tuple[OldCommit, Commit | None]:
+    with atomic_transaction(
+        using=(
+            router.db_for_write(OldCommit),
+            router.db_for_write(Commit),
+        )
+    ):
+        commit_kwargs = {}
+        if date_added is not None:
+            commit_kwargs["date_added"] = date_added
+
+        old_commit = OldCommit.objects.create(
+            organization_id=organization.id,
+            repository_id=repo_id,
+            key=key,
+            author=author,
+            message=message,
+            **commit_kwargs,
+        )
+        new_commit = None
+        if features.has("organizations:commit-retention-dual-writing", organization):
+            new_commit = Commit.objects.create(
+                id=old_commit.id,
+                organization_id=old_commit.organization_id,
+                repository_id=old_commit.repository_id,
+                key=old_commit.key,
+                date_added=old_commit.date_added,
+                author=old_commit.author,
+                message=old_commit.message,
+            )
+    return old_commit, new_commit
