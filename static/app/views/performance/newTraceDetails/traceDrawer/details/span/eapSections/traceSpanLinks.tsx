513f2b5d3e3ea319ce60680f4b69b4add9ed526a
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-09-12 10:15:07 -0400

    feat(otlp): Linkify span link attributes in the Span Details panel (#97823)
    Linkify the `trace` and `span_id` fields in the span links section of
    span details. Clicking on those fields directs to the corresponding
    trace and span.
    
    **e.g.,**
    <img width="402" height="256" alt="Screenshot 2025-08-13 at 8 09 22 PM"
    src="https://github.com/user-attachments/assets/6bbb1333-89e0-4f62-ad60-0b236009be7a"
    />
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/performance/newTraceDetails/traceDrawer/details/span/eapSections/traceSpanLinks.tsx
+++ static/app/views/performance/newTraceDetails/traceDrawer/details/span/eapSections/traceSpanLinks.tsx
@@ -2,8 +2,17 @@ import type {Location} from 'history';
 
 import {t} from 'sentry/locale';
 import type {Organization} from 'sentry/types/organization';
+import {
+  getFieldRenderer,
+  type RenderFunctionBaggage,
+} from 'sentry/utils/discover/fieldRenderers';
+import {generateLinkToEventInTraceView} from 'sentry/utils/discover/urls';
 import type {Theme} from 'sentry/utils/theme';
-import {AttributesTree} from 'sentry/views/explore/components/traceItemAttributes/attributesTree';
+import {useNavigate} from 'sentry/utils/useNavigate';
+import {
+  AttributesTree,
+  type AttributesFieldRender,
+} from 'sentry/views/explore/components/traceItemAttributes/attributesTree';
 import {
   type TraceItemResponseAttribute,
   type TraceItemResponseLink,
@@ -11,31 +20,121 @@ import {
 import {SectionKey} from 'sentry/views/issueDetails/streamline/context';
 import {FoldSection} from 'sentry/views/issueDetails/streamline/foldSection';
 import {TraceDrawerComponents} from 'sentry/views/performance/newTraceDetails/traceDrawer/details/styles';
-import type {TraceTree} from 'sentry/views/performance/newTraceDetails/traceModels/traceTree';
+import {TraceTree} from 'sentry/views/performance/newTraceDetails/traceModels/traceTree';
 import type {TraceTreeNode} from 'sentry/views/performance/newTraceDetails/traceModels/traceTreeNode';
+import {useTraceStateDispatch} from 'sentry/views/performance/newTraceDetails/traceState/traceStateProvider';
+import {TraceLayoutTabKeys} from 'sentry/views/performance/newTraceDetails/useTraceLayoutTabs';
 
 interface TraceSpanLinksProps {
   links: TraceItemResponseLink[];
   location: Location;
   node: TraceTreeNode<TraceTree.EAPSpan>;
+  onTabScrollToNode: (node: TraceTreeNode<TraceTree.NodeValue>) => void;
   organization: Organization;
   theme: Theme;
+  traceId: string;
+  tree?: TraceTree;
 }
 
 export function TraceSpanLinks({
+  tree,
   links,
+  node,
   organization,
   location,
   theme,
+  traceId,
+  onTabScrollToNode,
 }: TraceSpanLinksProps) {
-  // Render the links as a tree of attributes. This visual treatment requires
-  // that we format the trace ID and span ID as attributes, even though they are
-  // top level fields. I think we should convince Design to reconsider this
-  // treatment, but for now we'll need to create synthetic attributes for the
-  // span ID and trace ID, and prefix the actual attributes with a special key.
+  const traceDispatch = useTraceStateDispatch();
+  const navigate = useNavigate();
+
+  function closeSpanDetailsDrawer() {
+    traceDispatch({
+      type: 'minimize drawer',
+      payload: true,
+    });
+  }
+
+  // Render the span links an a single attribute tree. For each link, give it a
+  // serial integer prefix. For each of those prefixes, create a custom renderer
+  // for that unique trace ID and span ID field. e.g., for the first link the
+  // prefix is `"span_link_1"`. Create a renderer for the field
+  // `"span_link_1.trace_id"` and `span_link_1.span_id"`. This is somewhat of a
+  // hack, and a cleaner approach would be to render a separate little section
+  // and separate attribute tree for each link, rather than giving them this
+  // awkward prefix just to render them all in a single tree.
+  const customRenderers: AttributesFieldRender<RenderFunctionBaggage>['renderers'] = {};
+
+  const traceIdRenderer = getFieldRenderer('trace', {});
+  const spanIdRenderer = getFieldRenderer('span_id', {});
+
+  const renderBaggage = {
+    organization,
+    location,
+    theme,
+  };
+
   const linksAsAttributes: TraceItemResponseAttribute[] = links.flatMap(
     (link, linkIndex) => {
       const prefix = `span_link_${linkIndex + 1}`;
+
+      customRenderers[`${prefix}.trace_id`] = () => {
+        const traceTarget = generateLinkToEventInTraceView({
+          organization,
+          location,
+          traceSlug: link.traceId,
+          timestamp: node.value.start_timestamp,
+          tab: TraceLayoutTabKeys.WATERFALL,
+        });
+
+        return (
+          <a
+            onClick={() => {
+              // If we are outside the traceview, or the link is to a different trace, we navigate to the trace
+              // otherwise we do nothing
+              if (!tree || link.traceId !== traceId) {
+                closeSpanDetailsDrawer();
+                navigate(traceTarget);
+              }
+            }}
+          >
+            {traceIdRenderer({trace: link.traceId}, renderBaggage)}
+          </a>
+        );
+      };
+
+      customRenderers[`${prefix}.span_id`] = () => {
+        const spanTarget = generateLinkToEventInTraceView({
+          organization,
+          location,
+          traceSlug: link.traceId,
+          spanId: link.itemId,
+          timestamp: node.value.start_timestamp,
+          tab: TraceLayoutTabKeys.WATERFALL,
+        });
+
+        return (
+          <a
+            onClick={() => {
+              // If we are outside the trace waterfall, or the link is to a span in a different trace, we navigate
+              if (!tree || link.traceId !== traceId) {
+                closeSpanDetailsDrawer();
+                navigate(spanTarget);
+                return;
+              }
+
+              // If the link is to the same trace, we look for and navigate to the span in the same trace waterfall
+              const spanNode = TraceTree.FindByID(tree.root, link.itemId);
+              if (spanNode) {
+                onTabScrollToNode(spanNode);
+              }
+            }}
+          >
+            {spanIdRenderer({span_id: link.itemId}, renderBaggage)}
+          </a>
+        );
+      };
 
       return [
         {
@@ -74,13 +173,13 @@ export function TraceSpanLinks({
         columnCount={1}
         config={{
           disableActions: true,
-          disableRichValue: true,
         }}
         rendererExtra={{
           theme,
           location,
           organization,
         }}
+        renderers={customRenderers}
       />
     </FoldSection>
   );
