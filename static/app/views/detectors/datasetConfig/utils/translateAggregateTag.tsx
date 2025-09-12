f97b70082eecb15df51ff08ce8cf067713df00d5
scttcper <scttcper@gmail.com>
2025-09-12 13:33:28 -0700

    what am i doing

--- static/app/views/detectors/datasetConfig/utils/translateAggregateTag.tsx
+++ static/app/views/detectors/datasetConfig/utils/translateAggregateTag.tsx
@@ -1,7 +1,7 @@
 export function translateAggregateTag(aggregate: string) {
   // Translate count_unique(tags[sentry:user]) to count_unique(user)
   if (aggregate.includes('(tags[sentry:user])')) {
-    return aggregate.replace('tags[sentry:user]', 'user');
+    return aggregate.replace('(tags[sentry:user])', '(user)');
   }
 
   return aggregate;
@@ -10,7 +10,7 @@ export function translateAggregateTag(aggregate: string) {
 export function translateAggregateTagBack(aggregate: string) {
   // Translate count_unique(user) to count_unique(tags[sentry:user])
   if (aggregate.includes('(user)')) {
-    return aggregate.replace('user', 'tags[sentry:user]');
+    return aggregate.replace('(user)', '(tags[sentry:user])');
   }
 
   return aggregate;
