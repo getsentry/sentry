01b882a10521bb0ef16453898163c17c8c724361
Arpad Borsos <arpad.borsos@sentry.io>
2025-09-12 10:14:22 +0200

    thanks seer

--- src/sentry/api/endpoints/event_attachment_details.py
+++ src/sentry/api/endpoints/event_attachment_details.py
@@ -68,7 +68,7 @@ class EventAttachmentDetailsEndpoint(ProjectEndpoint):
         def stream_attachment():
             filestore_attachment = attachment.getfile()
             objectstore_attachment: IO[bytes] | ContextManager[None] = contextlib.nullcontext()
-            if in_rollout_group("objectstore.double_write", attachment.project_id):
+            if in_rollout_group("objectstore.double_write.attachments", attachment.project_id):
                 # This is a bit ugly admittedly. We have no way to tell whether
                 # an attachment has actually been double-written. We just assume
                 # it was based on the feature flag, which is not the case for all
@@ -79,7 +79,7 @@ class EventAttachmentDetailsEndpoint(ProjectEndpoint):
                         # We force the attachment model to use the objectstore backend
                         # by changing its prefix. Its a big hack, but hey why not.
                         assert attachment.blob_path is not None
-                        attachment.blob_path.replace(V1_PREFIX, V2_PREFIX)
+                        attachment.blob_path = attachment.blob_path.replace(V1_PREFIX, V2_PREFIX)
                         objectstore_attachment = attachment.getfile()
                         metrics.incr("storage.attachments.double_write.read")
                     except ClientError as e:
