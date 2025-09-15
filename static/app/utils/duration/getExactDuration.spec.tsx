43bdca2ce03d0ffb35fca3a7543d6555da8295ba
scttcper <scttcper@gmail.com>
2025-09-15 10:51:54 -0700

    feat(ui): Fix infinite recursion in getExactDurationManaged to

--- static/app/utils/duration/getExactDuration.spec.tsx
+++ static/app/utils/duration/getExactDuration.spec.tsx
@@ -42,4 +42,8 @@ describe('getExactDuration', () => {
       '387 weeks 2 days 1 hour 23 minutes 55 seconds'
     );
   });
+
+  it('truncates sub-minute durations when precision is minutes (abbr)', () => {
+    expect(getExactDuration(30, true, 'minutes')).toBe('0min');
+  });
 });
