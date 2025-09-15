5bc5993a9ed2d0c71c35c396507d3bf34af85ce7
Mia Hsu <ameliahsu@gmail.com>
2025-09-15 12:49:14 -0700

    convert saved detector owner back to string

--- static/app/types/workflowEngine/detectors.tsx
+++ static/app/types/workflowEngine/detectors.tsx
@@ -182,7 +182,7 @@ interface UpdateUptimeDataSourcePayload {
 
 export interface BaseDetectorUpdatePayload {
   name: string;
-  owner: Detector['owner'];
+  owner: string | null;
   projectId: Detector['projectId'];
   type: Detector['type'];
   workflowIds: string[];
