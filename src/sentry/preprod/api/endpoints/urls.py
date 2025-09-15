401864cd00dc5683602ea832c451b04eaae17f7f
Ryan Brooks <ryanjeffrey.brooks112@gmail.com>
2025-09-15 12:07:50 -0700

    Refactor size analysis endpoint into size_analysis folder

--- src/sentry/preprod/api/endpoints/urls.py
+++ src/sentry/preprod/api/endpoints/urls.py
@@ -1,5 +1,8 @@
 from django.urls import re_path
 
+from sentry.preprod.api.endpoints.size_analysis.project_preprod_artifact_size_analysis_download import (
+    ProjectPreprodArtifactSizeAnalysisDownloadEndpoint,
+)
 from sentry.preprod.api.endpoints.size_analysis.project_preprod_size_analysis_compare import (
     ProjectPreprodArtifactSizeAnalysisCompareEndpoint,
 )
@@ -19,9 +22,6 @@ from .project_preprod_artifact_assemble_generic import ProjectPreprodArtifactAss
 from .project_preprod_artifact_delete import ProjectPreprodArtifactDeleteEndpoint
 from .project_preprod_artifact_download import ProjectPreprodArtifactDownloadEndpoint
 from .project_preprod_artifact_install_details import ProjectPreprodInstallDetailsEndpoint
-from .project_preprod_artifact_size_analysis_download import (
-    ProjectPreprodArtifactSizeAnalysisDownloadEndpoint,
-)
 from .project_preprod_artifact_update import ProjectPreprodArtifactUpdateEndpoint
 from .project_preprod_build_details import ProjectPreprodBuildDetailsEndpoint
 from .project_preprod_check_for_updates import ProjectPreprodArtifactCheckForUpdatesEndpoint
