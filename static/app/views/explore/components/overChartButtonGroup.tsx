114bd582e2c64837e5332fbd3ac13d650f8e29fb
Tony Xiao <txiao@sentry.io>
2025-09-16 13:28:06 -0400

    feat(explore): Add setting to disable extrapolation (#99589)
    This adds a settings button to the explore page that has options to
    enable/disable extrapolation for the page. It does not persist across
    sessions
    so each fresh visit to the explore page starts with extrapolation
    enabled.
    
    Re-applies #99164

--- static/app/views/explore/components/overChartButtonGroup.tsx
+++ static/app/views/explore/components/overChartButtonGroup.tsx
@@ -1,15 +1,13 @@
 import styled from '@emotion/styled';
 
-const OverChartButtonGroup = styled('div')`
+export const OverChartButtonGroup = styled('div')`
   display: flex;
   flex-direction: row;
   gap: ${p => p.theme.space.xs};
   justify-content: space-between;
+  margin-bottom: ${p => p.theme.space.md};
 
   @media (max-width: ${p => p.theme.breakpoints.md}) {
     justify-content: flex-end;
-    margin-bottom: ${p => p.theme.space.md};
   }
 `;
-
-export {OverChartButtonGroup};
