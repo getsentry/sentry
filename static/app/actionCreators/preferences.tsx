bb16257121b45b734236a682de069866e0e796d4
Malachi Willey <malwilley@gmail.com>
2025-09-17 12:53:47 -0700

    chore(nav-cleanup): Remove old nav sidebar code (#99716)

--- static/app/actionCreators/preferences.tsx
+++ static/app/actionCreators/preferences.tsx
@@ -4,17 +4,6 @@ import PreferenceStore from 'sentry/stores/preferencesStore';
 
 const SIDEBAR_COOKIE_KEY = 'sidebar_collapsed';
 const COOKIE_ENABLED = '1';
-const COOKIE_DISABLED = '0';
-
-export function hideSidebar() {
-  PreferenceStore.hideSidebar();
-  Cookies.set(SIDEBAR_COOKIE_KEY, COOKIE_ENABLED);
-}
-
-export function showSidebar() {
-  PreferenceStore.showSidebar();
-  Cookies.set(SIDEBAR_COOKIE_KEY, COOKIE_DISABLED);
-}
 
 export function loadPreferencesState() {
   // Set initial "collapsed" state to true or false
