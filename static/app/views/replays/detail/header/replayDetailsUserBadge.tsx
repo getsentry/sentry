20dc3142db50033e4d04a7d41011ba6ee834f5c2
jerryzhou196 <jerry.zhou@sentry.io>
2025-09-17 14:29:10 -0400

    ref(replays): Update Live text styling and LiveIndicator border for enhanced visibility
    - Replaced the span element with a styled LiveText component for the "LIVE" label, improving visual consistency.
    - Increased the border width of the LiveIndicator from 4px to 6px, enhancing its prominence in the UI.

--- static/app/views/replays/detail/header/replayDetailsUserBadge.tsx
+++ static/app/views/replays/detail/header/replayDetailsUserBadge.tsx
@@ -85,7 +85,7 @@ const DisplayHeader = styled('div')`
 function Live() {
   return (
     <Flex align="center">
-      <span>{t('LIVE')}</span>
+      <LiveText>{t('LIVE')}</LiveText>
       <LiveIndicator />
     </Flex>
   );
@@ -103,6 +103,10 @@ const pulse = keyframes`
   }
 `;
 
+const LiveText = styled('span')`
+  color: ${p => p.theme.success};
+`;
+
 const LiveIndicator = styled('div')`
   background: ${p => p.theme.success};
   height: 8px;
@@ -114,7 +118,7 @@ const LiveIndicator = styled('div')`
   &:before {
     content: '';
     animation: ${pulse} 3s ease-out infinite;
-    border: 4px solid ${p => p.theme.success};
+    border: 6px solid ${p => p.theme.success};
     position: absolute;
     border-radius: 50%;
     height: 20px;
