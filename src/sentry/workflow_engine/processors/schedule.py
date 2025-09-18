9e7ded9406100b7ed80b84b9f92f5a0298f37026
Kyle Consalus <kyle.consalus@sentry.io>
2025-09-18 16:59:40 -0700

    timing, batching

--- src/sentry/workflow_engine/processors/schedule.py
+++ src/sentry/workflow_engine/processors/schedule.py
@@ -9,6 +9,7 @@ from sentry import options
 from sentry.buffer.base import BufferField
 from sentry.db import models
 from sentry.utils import metrics
+from sentry.utils.iterators import chunked
 from sentry.workflow_engine.buffer import get_backend
 from sentry.workflow_engine.buffer.redis_hash_sorted_set_buffer import RedisHashSortedSetBuffer
 from sentry.workflow_engine.tasks.delayed_workflows import (
@@ -139,10 +140,19 @@ def process_buffered_workflows() -> None:
                 for project_id, timestamps in all_project_ids_and_timestamps.items()
             ]
             try:
-                with metrics.timer("workflow_engine.conditional_delete_from_sorted_sets.duration"):
-                    deleted = buffer.conditional_delete_from_sorted_sets(buffer_keys, member_maxes)
+                deleted_project_ids = set[int]()
+                with metrics.timer(
+                    "workflow_engine.conditional_delete_from_sorted_sets.total_duration"
+                ):
+                    # The conditional delete can be slow, so we break it into chunks that probably
+                    # aren't big enough to hold onto the main redis thread for too long.
+                    for chunk in chunked(member_maxes, 500):
+                        with metrics.timer(
+                            "workflow_engine.conditional_delete_from_sorted_sets.chunk_duration"
+                        ):
+                            deleted = buffer.conditional_delete_from_sorted_sets(buffer_keys, chunk)
+                            deleted_project_ids.update(*deleted.values())
 
-                deleted_project_ids = set().union(*deleted.values())
                 logger.info(
                     "process_buffered_workflows.project_ids_deleted",
                     extra={
