09879a7cb52a634ffdd50b8c49009a1ee45146bc
Calvin Yau <calvin.yau@sentry.io>
2025-09-18 14:10:48 -0700

    test(prevent): Add test for active repo filter

--- static/app/views/prevent/tests/queries/useGetActiveIntegratedOrgs.spec.tsx
+++ static/app/views/prevent/tests/queries/useGetActiveIntegratedOrgs.spec.tsx
@@ -15,7 +15,7 @@ describe('useGetActiveIntegratedOrgs', () => {
 
   it('fetches repository data successfully', async () => {
     const organization = OrganizationFixture({slug: 'test-org'});
-    const mockIntegratedOrgs = [
+    const mockIntegrations = [
       {
         id: '123',
         name: 'github-org-name',
@@ -31,7 +31,7 @@ describe('useGetActiveIntegratedOrgs', () => {
 
     MockApiClient.addMockResponse({
       url: `/organizations/${organization.slug}/integrations/`,
-      body: mockIntegratedOrgs,
+      body: mockIntegrations,
     });
 
     const wrapper = ({children}: {children: React.ReactNode}) => (
@@ -48,10 +48,52 @@ describe('useGetActiveIntegratedOrgs', () => {
       expect(result.current.isSuccess).toBe(true);
     });
 
-    expect(result.current.data).toEqual(mockIntegratedOrgs);
+    expect(result.current.data).toEqual(mockIntegrations);
     expect(result.current.data?.[0]?.id).toBe('123');
     expect(result.current.data?.[0]?.name).toBe('github-org-name');
     expect(result.current.data?.[0]?.status).toBe('active');
+  });
+
+  it('filters out inactive integrated orgs', async () => {
+    const organization = OrganizationFixture({slug: 'test-org'});
+    const mockIntegrations = [
+      {
+        id: '123',
+        name: 'github-org-name',
+        status: 'inactive',
+      },
+      {
+        id: '456',
+        name: 'github-org-name-2',
+        status: 'inactive',
+      },
+      {
+        id: '789',
+        name: 'github-org-name-3',
+        status: 'active',
+      },
+    ];
+
+    MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/integrations/`,
+      body: mockIntegrations,
+    });
+
+    const wrapper = ({children}: {children: React.ReactNode}) => (
+      <QueryClientProvider client={makeTestQueryClient()}>
+        <OrganizationContext value={organization}>{children}</OrganizationContext>
+      </QueryClientProvider>
+    );
+
+    const {result} = renderHook(() => useGetActiveIntegratedOrgs({organization}), {
+      wrapper,
+    });
+
+    await waitFor(() => {
+      expect(result.current.isSuccess).toBe(true);
+    });
+
+    expect(result.current.data).toEqual([mockIntegrations[2]]);
   });
 
   it('handles API errors gracefully', async () => {
