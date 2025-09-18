cde62589c6cd31bac6acce4722674284845888fe
Arthur Knaus <arthur.knaus@sentry.io>
2025-09-18 12:06:48 +0200

    ref(content-blocks): Capacitor

--- static/app/components/onboarding/gettingStartedDoc/feedback/widgetCallout.tsx
+++ static/app/components/onboarding/gettingStartedDoc/feedback/widgetCallout.tsx
@@ -1,18 +1,28 @@
 import {Alert} from 'sentry/components/core/alert';
 import {ExternalLink} from 'sentry/components/core/link';
+import type {ContentBlock} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {tct} from 'sentry/locale';
+
+const getCalloutText = (link: string) =>
+  tct(
+    `Want to receive user feedback at any time, not just when an error happens? [link:Read the docs] to learn how to set up our customizable widget.`,
+    {
+      link: <ExternalLink href={link} />,
+    }
+  );
 
 export default function widgetCallout({link}: {link: string}) {
   return (
     <Alert.Container>
-      <Alert type="info">
-        {tct(
-          `Want to receive user feedback at any time, not just when an error happens? [link:Read the docs] to learn how to set up our customizable widget.`,
-          {
-            link: <ExternalLink href={link} />,
-          }
-        )}
-      </Alert>
+      <Alert type="info">{getCalloutText(link)}</Alert>
     </Alert.Container>
   );
+}
+
+export function widgetCalloutBlock({link}: {link: string}): ContentBlock {
+  return {
+    type: 'alert',
+    alertType: 'info',
+    text: getCalloutText(link),
+  };
 }
