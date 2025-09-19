11e0c6478f133fba479a4c09da11d29b1605bf2d
Isabella Enriquez <isabella.enriquez@sentry.io>
2025-09-19 13:51:34 -0400

    fix(checkout v3): Put padding on settings layout content (#99758)
    Lift padding for children rendered within `SubscriptionSettingsLayout`
    out of the individual child components so the padding is consistent
    across the different subscription settings subpages.
    
    See https://sentry.slack.com/archives/C07SSUBN9BK/p1758133957682989

--- static/gsApp/components/subscriptionSettingsLayout.tsx
+++ static/gsApp/components/subscriptionSettingsLayout.tsx
@@ -1,6 +1,6 @@
 import styled from '@emotion/styled';
 
-import {Flex} from 'sentry/components/core/layout';
+import {Container, Flex} from 'sentry/components/core/layout';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import {useParams} from 'sentry/utils/useParams';
@@ -51,7 +51,9 @@ function SubscriptionSettingsLayout(props: Props) {
       </StyledSettingsHeader>
 
       <Flex maxWidth={'1440px'} flex="1">
-        <Content>{children}</Content>
+        <Container padding={{xs: 'xl', md: '3xl'}} minWidth={0} flex={1}>
+          {children}
+        </Container>
       </Flex>
     </SettingsColumn>
   );
@@ -67,15 +69,6 @@ const SettingsColumn = styled(Flex)`
 
 const StyledSettingsBreadcrumb = styled(SettingsBreadcrumb)`
   flex: 1;
-`;
-
-/**
- * Note: `overflow: hidden` will cause some buttons in `SettingsPageHeader` to be cut off because it has negative margin.
- * Will also cut off tooltips.
- */
-const Content = styled('div')`
-  flex: 1;
-  min-width: 0; /* keep children from stretching container */
 `;
 
 const StyledSettingsHeader = styled(SettingsHeader)`
