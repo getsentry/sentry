dd0d2b701f73a4d14f4c1a527e39d68a4175bddf
Cathy Teng <cathy.teng@sentry.io>
2025-09-19 12:29:02 -0700

    starter tests

---
+++ tests/sentry/workflow_engine/endpoints/validators/actions/test_email.py
@@ -0,0 +1,81 @@
+from sentry.notifications.models.notificationaction import ActionTarget
+from sentry.testutils.cases import TestCase
+from sentry.workflow_engine.endpoints.validators.base import BaseActionValidator
+from sentry.workflow_engine.models import Action
+
+
+class TestEmailActionValidator(TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.team = self.create_team(organization=self.organization)
+        self.valid_data = {
+            "type": Action.Type.EMAIL,
+            "config": {"targetType": ActionTarget.USER, "targetIdentifier": str(self.user.id)},
+            "data": {},
+        }
+
+    def test_validate__user(self):
+        validator = BaseActionValidator(
+            data=self.valid_data,
+            context={"organization": self.organization},
+        )
+        result = validator.is_valid()
+        assert result is True
+
+    def test_validate_user__missing_identifier(self):
+        validator = BaseActionValidator(
+            data={
+                **self.valid_data,
+                "config": {"target_type": ActionTarget.USER},
+            },
+            context={"organization": self.organization},
+        )
+        result = validator.is_valid()
+        assert result is False
+
+    def test_validate__team(self):
+        validator = BaseActionValidator(
+            data={
+                **self.valid_data,
+                "config": {
+                    "target_type": ActionTarget.TEAM,
+                    "target_identifier": str(self.team.id),
+                },
+            },
+            context={"organization": self.organization},
+        )
+        result = validator.is_valid()
+        assert result is True
+
+    def test_validate__issue_owners(self):
+        validator = BaseActionValidator(
+            data={
+                **self.valid_data,
+                "config": {"target_type": ActionTarget.ISSUE_OWNERS},
+            },
+            context={"organization": self.organization},
+        )
+        result = validator.is_valid()
+        assert result is True
+
+    def test_validate__issue_owners_extra_identifier(self):
+        validator = BaseActionValidator(
+            data={
+                **self.valid_data,
+                "config": {"target_type": ActionTarget.ISSUE_OWNERS, "target_identifier": "extra"},
+            },
+            context={"organization": self.organization},
+        )
+        result = validator.is_valid()
+        assert result is False
+
+    def test_validate__invalid_target_type(self):
+        validator = BaseActionValidator(
+            data={
+                **self.valid_data,
+                "config": {"targetType": ActionTarget.SPECIFIC},
+            },
+            context={"organization": self.organization},
+        )
+        result = validator.is_valid()
+        assert result is False
