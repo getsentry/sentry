26a548fc011131d87031e25199afbab9f6bfa698
TkDodo <dominik.dorfmeister@sentry.io>
2025-09-19 08:37:06 +0200

    ref: useCombinedReducer is unused
    makeCombinedReducer is used

--- static/app/utils/useCombinedReducer.spec.tsx
+++ static/app/utils/useCombinedReducer.spec.tsx
@@ -1,6 +1,4 @@
-import {act, renderHook} from 'sentry-test/reactTestingLibrary';
-
-import {makeCombinedReducers, useCombinedReducer} from 'sentry/utils/useCombinedReducer';
+import {makeCombinedReducers} from 'sentry/utils/useCombinedReducer';
 
 describe('makeCombinedReducers', () => {
   it('calls reducer only with subset of state', () => {
@@ -24,39 +22,5 @@ describe('makeCombinedReducers', () => {
 
     expect(mockFirstReducer).toHaveBeenCalledWith('first', 'action');
     expect(mockSecondReducer).toHaveBeenCalledWith('second', 'action');
-  });
-});
-
-describe('useCombinedReducer', () => {
-  it('initializes with init state', () => {
-    const {result} = renderHook(
-      (args: Parameters<typeof useCombinedReducer>) =>
-        useCombinedReducer(args[0], args[1]),
-      {initialProps: [{first: jest.fn()}, {first: 'initial'}]}
-    );
-
-    expect(result.current[0]).toEqual({first: 'initial'});
-  });
-
-  it('updates state', () => {
-    const {result} = renderHook(
-      (args: Parameters<typeof useCombinedReducer>) =>
-        useCombinedReducer(args[0], args[1]),
-      {initialProps: [{first: (state, action) => state + action}, {first: 'initial'}]}
-    );
-
-    act(() => result.current[1]('_action'));
-    expect(result.current[0]).toEqual({first: 'initial_action'});
-  });
-  it('doesnt keep old state around', () => {
-    const {result} = renderHook(
-      (args: Parameters<typeof useCombinedReducer>) =>
-        useCombinedReducer(args[0], args[1]),
-      {initialProps: [{first: (state, action) => state + action}, {first: 'initial'}]}
-    );
-
-    act(() => result.current[1]('_action'));
-    act(() => result.current[1]('_action'));
-    expect(result.current[0]).toEqual({first: 'initial_action_action'});
   });
 });
