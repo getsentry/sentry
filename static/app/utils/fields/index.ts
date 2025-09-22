e7d7390d9f67f1c26d2c909edfca43be5a8cf047
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-09-22 12:00:30 -0700

    feat(feedback): always suggest has_linked_error search

--- static/app/utils/fields/index.ts
+++ static/app/utils/fields/index.ts
@@ -2906,6 +2906,7 @@ const REPLAY_CLICK_FIELD_DEFINITIONS: Record<ReplayClickFieldKey, FieldDefinitio
 
 export enum FeedbackFieldKey {
   BROWSER_NAME = 'browser.name',
+  HAS_LINKED_ERROR = 'has_linked_error',
   LOCALE_LANG = 'locale.lang',
   LOCALE_TIMEZONE = 'locale.timezone',
   MESSAGE = 'message',
@@ -2927,6 +2928,7 @@ export const FEEDBACK_FIELDS = [
   FieldKey.GEO_REGION,
   FieldKey.GEO_SUBDIVISION,
   FieldKey.HAS,
+  FeedbackFieldKey.HAS_LINKED_ERROR,
   FieldKey.ID,
   FieldKey.IS,
   FieldKey.LEVEL,
@@ -2952,6 +2954,11 @@ const FEEDBACK_FIELD_DEFINITIONS: Record<FeedbackFieldKey, FieldDefinition> = {
     desc: t('Name of the browser'),
     kind: FieldKind.FIELD,
     valueType: FieldValueType.STRING,
+  },
+  [FeedbackFieldKey.HAS_LINKED_ERROR]: {
+    desc: t('Whether the feedback is explicitly linked to a Sentry error.'),
+    kind: FieldKind.FIELD,
+    valueType: FieldValueType.BOOLEAN,
   },
   [FeedbackFieldKey.LOCALE_LANG]: {
     desc: t('Language preference of the user'),
