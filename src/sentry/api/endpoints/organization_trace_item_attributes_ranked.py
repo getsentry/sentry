bc5f5c5300f1e4025def0047fc5a4872a5ace455
Aayush Seth <aayush.seth@sentry.io>
2025-09-22 12:20:56 -0700

    include totals for each cohort in response

--- src/sentry/api/endpoints/organization_trace_item_attributes_ranked.py
+++ src/sentry/api/endpoints/organization_trace_item_attributes_ranked.py
@@ -262,6 +262,8 @@ class OrganizationTraceItemsAttributesRankedEndpoint(OrganizationEventsV2Endpoin
                 or attr,
                 "cohort1": cohort_1_distribution_map.get(attr),
                 "cohort2": cohort_2_distribution_map.get(attr),
+                "cohort1Total": total_outliers,
+                "cohort2Total": total_baseline,
                 "order": {  # TODO: aayush-se remove this once we have selected a single ranking method
                     "rrf": i,
                     "rrr": rrr_order_map.get(attr),
