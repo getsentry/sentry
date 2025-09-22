02025f2465d53c0eed4440201cd0fabde6fe2c14
Kyle Consalus <kyle.consalus@sentry.io>
2025-09-22 11:51:01 -0700

    leaner test

--- tests/sentry/workflow_engine/tasks/test_workflows.py
+++ tests/sentry/workflow_engine/tasks/test_workflows.py
@@ -1,36 +1,35 @@
-from unittest import mock
+from unittest import TestCase, mock
+from unittest.mock import MagicMock, patch
 
 from sentry.locks import locks
-from sentry.testutils.cases import TestCase
 from sentry.workflow_engine.tasks.workflows import schedule_delayed_workflows
 
 
 class ScheduleDelayedWorkflowsTest(TestCase):
-    @mock.patch("sentry.workflow_engine.processors.schedule.process_buffered_workflows")
+    @patch("sentry.workflow_engine.processors.schedule.process_buffered_workflows")
     def test_schedule_delayed_workflows_locked_out(
         self,
-        mock_process_buffered_workflows: mock.MagicMock,
+        mock_process_buffered_workflows: MagicMock,
     ) -> None:
-        with self.assertLogs("sentry.workflow_engine.tasks.workflows", level="WARNING") as logger:
-            lock = locks.get(
-                "workflow_engine:schedule_delayed_workflows",
-                duration=60,
-                name="schedule_delayed_workflows",
-            )
-            with lock.acquire():
-                schedule_delayed_workflows()
-                self.assertEqual(len(logger.output), 1)
-                assert len(mock_process_buffered_workflows.mock_calls) == 0
-
-        with self.assertNoLogs("sentry.workflow_engine.tasks.workflows", level="WARNING"):
+        lock = locks.get(
+            "workflow_engine:schedule_delayed_workflows",
+            duration=60,
+            name="schedule_delayed_workflows",
+        )
+        with lock.acquire():
+            # Lock already held; no call.
             schedule_delayed_workflows()
-            assert len(mock_process_buffered_workflows.mock_calls) == 1
-            mock_process_buffered_workflows.assert_called_with(mock.ANY)
+            assert len(mock_process_buffered_workflows.mock_calls) == 0
 
-    @mock.patch("sentry.workflow_engine.processors.schedule.process_buffered_workflows")
+        # Lock not held; call.
+        schedule_delayed_workflows()
+        assert len(mock_process_buffered_workflows.mock_calls) == 1
+        mock_process_buffered_workflows.assert_called_with(mock.ANY)
+
+    @patch("sentry.workflow_engine.processors.schedule.process_buffered_workflows")
     def test_schedule_delayed_workflows_normal_operation(
         self,
-        mock_process_buffered_workflows: mock.MagicMock,
+        mock_process_buffered_workflows: MagicMock,
     ) -> None:
         schedule_delayed_workflows()
 
