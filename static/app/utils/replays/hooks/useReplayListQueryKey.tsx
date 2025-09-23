78dc51ae16f4ab7bc70c5bb0720f1cca23f7a72e
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2025-09-23 14:39:59 -0700

    ref(replay): temporary qparam patch for has_viewed (#100150)

--- static/app/utils/replays/hooks/useReplayListQueryKey.tsx
+++ static/app/utils/replays/hooks/useReplayListQueryKey.tsx
@@ -40,7 +40,9 @@ export default function useReplayListQueryKey({
 
     // HACK!!! Because the sort field needs to be in the eventView, but I cannot
     // ask the server for compound fields like `os.name`.
-    const splitFields = REPLAY_LIST_FIELDS.map(field => field.split('.')[0]);
+    const splitFields = REPLAY_LIST_FIELDS.map(field => field.split('.')[0]).filter(
+      field => field !== 'has_viewed'
+    );
     const fields = uniq(splitFields);
 
     // when queryReferrer === 'issueReplays' we override the global view check on the backend
@@ -62,6 +64,7 @@ export default function useReplayListQueryKey({
           per_page: 50,
           ...query,
           fields,
+          field: ['has_viewed'],
           project,
           queryReferrer,
         },
