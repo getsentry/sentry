18c054a0b561472d15c8d14e88f0d0631a16eca1
Colton Allen <colton.allen@sentry.io>
2025-09-23 15:00:50 -0500

    Add data export impl

--- src/sentry/replays/usecases/data_export.py
+++ src/sentry/replays/usecases/data_export.py
@@ -11,7 +11,7 @@ import csv
 import io
 from collections.abc import Callable, Generator, Iterator
 from datetime import datetime
-from typing import Any, Protocol, TypedDict
+from typing import Any, Protocol
 
 from snuba_sdk import (
     Column,
@@ -33,11 +33,6 @@ from sentry.utils.snuba import raw_snql_query
 
 class QueryFnProtocol(Protocol):
     def __call__(self, limit: int, offset: int) -> Request: ...
-
-
-class Cursor(TypedDict):
-    event_hash: str
-    replay_id: str
 
 
 def row_iterator_to_csv(rows: Iterator[dict[str, Any]]) -> io.BytesIO:
@@ -108,115 +103,6 @@ def export_clickhouse_rows(
             break
 
 
-def export_replays_dataset(
-    project_id: int,
-    start: datetime,
-    end: datetime,
-    limit: int,
-    offset: int,
-) -> Request:
-    query = Query(
-        match=Entity("replays"),
-        select=[
-            Column("replay_id"),
-            Column("debug_id"),
-            Column("count_info_events"),
-            Column("count_warning_events"),
-            Column("count_error_events"),
-            Column("info_id"),
-            Column("warning_id"),
-            Column("error_id"),
-            Column("fatal_id"),
-            Column("replay_type"),
-            Column("error_sample_rate"),
-            Column("session_sample_rate"),
-            Column("event_hash"),
-            Column("segment_id"),
-            Column("trace_ids"),
-            Column("title"),
-            Column("url"),
-            Column("urls"),
-            Column("is_archived"),
-            Column("error_ids"),
-            Column("project_id"),
-            Column("timestamp"),
-            Column("replay_start_timestamp"),
-            Column("platform"),
-            Column("environment"),
-            Column("release"),
-            Column("dist"),
-            Column("ip_address_v4"),
-            Column("ip_address_v6"),
-            Column("user"),
-            Column("user_id"),
-            Column("user_name"),
-            Column("user_email"),
-            Column("user_geo_city"),
-            Column("user_geo_country_code"),
-            Column("user_geo_region"),
-            Column("user_geo_subdivision"),
-            Column("viewed_by_id"),
-            Column("os_name"),
-            Column("os_version"),
-            Column("browser_name"),
-            Column("browser_version"),
-            Column("device_name"),
-            Column("device_brand"),
-            Column("device_family"),
-            Column("device_model"),
-            Column("ota_updates_channel"),
-            Column("ota_updates_runtime_version"),
-            Column("ota_updates_update_id"),
-            Column("sdk_name"),
-            Column("sdk_version"),
-            Column("tags"),
-            Column("tags"),
-            Column("click_node_id"),
-            Column("click_tag"),
-            Column("click_id"),
-            Column("click_class"),
-            Column("click_text"),
-            Column("click_role"),
-            Column("click_alt"),
-            Column("click_testid"),
-            Column("click_aria_label"),
-            Column("click_title"),
-            Column("click_component_name"),
-            Column("click_is_dead"),
-            Column("click_is_rage"),
-            Column("count_errors"),
-            Column("count_urls"),
-            Column("retention_days"),
-            Column("partition"),
-            Column("offset"),
-        ],
-        where=[
-            Condition(Column("project_id"), Op.EQ, project_id),
-            Condition(Column("timestamp"), Op.GTE, start),
-            Condition(Column("timestamp"), Op.LT, end),
-        ],
-        orderby=[
-            OrderBy(Column("project_id"), Direction.ASC),
-            OrderBy(Function("toStartOfDay", parameters=[Column("timestamp")]), Direction.ASC),
-            OrderBy(hash_(Column("replay_id")), Direction.ASC),
-            OrderBy(Column("event_hash"), Direction.ASC),
-        ],
-        limit=Limit(limit),
-        offset=Offset(offset),
-    )
-
-    return Request(
-        dataset="replays",
-        app_id="replay-backend-web",
-        query=query,
-        tenant_ids={},
-    )
-
-
-def hash_(value: Column | str) -> Function:
-    return Function("cityHash64", parameters=[value])
-
-
 #  $$$$$$\   $$$$$$\   $$$$$$\
 # $$  __$$\ $$  __$$\ $$  __$$\
 # $$ /  \__|$$ /  \__|$$ /  \__|
@@ -243,27 +129,6 @@ from google.cloud.storage_transfer_v1 import (
 from google.type import date_pb2
 
 from sentry.utils import json
-
-
-def export_projects_replay_blob_data(
-    project_ids: list[int],
-    gcs_project_id: str,
-    destination_bucket: str,
-    pubsub_topic_name: str,
-    source_bucket: str = "sentry-replays",
-    job_duration: timedelta | None = None,
-):
-    job_duration = job_duration or timedelta(days=5)
-    for project_id in project_ids:
-        for retention_days in (30, 60, 90):
-            export_blob_data(
-                gcs_project_id=gcs_project_id,
-                source_bucket=source_bucket,
-                source_prefix=f"{retention_days}/{project_id}",
-                destination_bucket=destination_bucket,
-                pubsub_topic_name=pubsub_topic_name,
-                job_duration=job_duration,
-            )
 
 
 def request_schedule_transfer_job(transfer_job: dict[str, Any]) -> Any:
@@ -366,4 +231,241 @@ def retry_export_blob_data[T](
             transfer_job=TransferJob(status=storage_transfer_v1.TransferJob.Status.ENABLED),
         )
         return retry_transfer_job(request)
-        return retry_transfer_job(request)
+
+
+# $$$$$$$\  $$$$$$$$\ $$$$$$$\  $$\        $$$$$$\ $$\     $$\
+# $$  __$$\ $$  _____|$$  __$$\ $$ |      $$  __$$\\$$\   $$  |
+# $$ |  $$ |$$ |      $$ |  $$ |$$ |      $$ /  $$ |\$$\ $$  /
+# $$$$$$$  |$$$$$\    $$$$$$$  |$$ |      $$$$$$$$ | \$$$$  /
+# $$  __$$< $$  __|   $$  ____/ $$ |      $$  __$$ |  \$$  /
+# $$ |  $$ |$$ |      $$ |      $$ |      $$ |  $$ |   $$ |
+# $$ |  $$ |$$$$$$$$\ $$ |      $$$$$$$$\ $$ |  $$ |   $$ |
+# \__|  \__|\________|\__|      \________|\__|  \__|   \__|
+import uuid
+
+
+def export_replays_dataset(
+    project_id: int,
+    start: datetime,
+    end: datetime,
+    limit: int,
+    offset: int,
+) -> Request:
+    assert start < end, "Start date must be less than the ending date."
+    assert project_id > 0, "Project ID must be greater than zero."
+    assert limit > 0, "limit mut be a positive integer greater than zero."
+    assert offset >= 0, "offset mut be a positive integer greater than or equal to zero."
+
+    def hash_(value: Column | str) -> Function:
+        return Function("cityHash64", parameters=[value])
+
+    query = Query(
+        match=Entity("replays"),
+        select=[
+            Column("replay_id"),
+            Column("debug_id"),
+            Column("count_info_events"),
+            Column("count_warning_events"),
+            Column("count_error_events"),
+            Column("info_id"),
+            Column("warning_id"),
+            Column("error_id"),
+            Column("fatal_id"),
+            Column("replay_type"),
+            Column("error_sample_rate"),
+            Column("session_sample_rate"),
+            Column("event_hash"),
+            Column("segment_id"),
+            Column("trace_ids"),
+            Column("title"),
+            Column("url"),
+            Column("urls"),
+            Column("is_archived"),
+            Column("error_ids"),
+            Column("project_id"),
+            Column("timestamp"),
+            Column("replay_start_timestamp"),
+            Column("platform"),
+            Column("environment"),
+            Column("release"),
+            Column("dist"),
+            Column("ip_address_v4"),
+            Column("ip_address_v6"),
+            Column("user"),
+            Column("user_id"),
+            Column("user_name"),
+            Column("user_email"),
+            Column("user_geo_city"),
+            Column("user_geo_country_code"),
+            Column("user_geo_region"),
+            Column("user_geo_subdivision"),
+            Column("viewed_by_id"),
+            Column("os_name"),
+            Column("os_version"),
+            Column("browser_name"),
+            Column("browser_version"),
+            Column("device_name"),
+            Column("device_brand"),
+            Column("device_family"),
+            Column("device_model"),
+            Column("ota_updates_channel"),
+            Column("ota_updates_runtime_version"),
+            Column("ota_updates_update_id"),
+            Column("sdk_name"),
+            Column("sdk_version"),
+            Column("tags"),
+            Column("tags"),
+            Column("click_node_id"),
+            Column("click_tag"),
+            Column("click_id"),
+            Column("click_class"),
+            Column("click_text"),
+            Column("click_role"),
+            Column("click_alt"),
+            Column("click_testid"),
+            Column("click_aria_label"),
+            Column("click_title"),
+            Column("click_component_name"),
+            Column("click_is_dead"),
+            Column("click_is_rage"),
+            Column("count_errors"),
+            Column("count_urls"),
+            Column("retention_days"),
+            Column("partition"),
+            Column("offset"),
+        ],
+        where=[
+            Condition(Column("project_id"), Op.EQ, project_id),
+            Condition(Column("timestamp"), Op.GTE, start),
+            Condition(Column("timestamp"), Op.LT, end),
+        ],
+        orderby=[
+            OrderBy(Column("project_id"), Direction.ASC),
+            OrderBy(Function("toStartOfDay", parameters=[Column("timestamp")]), Direction.ASC),
+            OrderBy(hash_(Column("replay_id")), Direction.ASC),
+            OrderBy(Column("event_hash"), Direction.ASC),
+        ],
+        limit=Limit(limit),
+        offset=Offset(offset),
+    )
+
+    return Request(
+        dataset="replays",
+        app_id="replay-backend-web",
+        query=query,
+        tenant_ids={},
+    )
+
+
+# $$$$$$\ $$\      $$\ $$$$$$$\  $$\
+# \_$$  _|$$$\    $$$ |$$  __$$\ $$ |
+#   $$ |  $$$$\  $$$$ |$$ |  $$ |$$ |
+#   $$ |  $$\$$\$$ $$ |$$$$$$$  |$$ |
+#   $$ |  $$ \$$$  $$ |$$  ____/ $$ |
+#   $$ |  $$ |\$  /$$ |$$ |      $$ |
+# $$$$$$\ $$ | \_/ $$ |$$ |      $$$$$$$$\
+# \______|\__|     \__|\__|      \________|
+
+from django.db.models import F
+
+from sentry.models.organization import Organization
+from sentry.models.project import Project
+from sentry.tasks.base import instrumented_task
+
+
+@instrumented_task(name="sentry.replays.tasks.export_clickhouse_rows_async")
+def export_clickhouse_rows_async(
+    project_id: int,
+    start: datetime,
+    end: datetime,
+    limit: int,
+    initial_offset: int,
+    destination_bucket: str,
+):
+    row_iterator = export_clickhouse_rows(
+        lambda limit, offset: export_replays_dataset(project_id, start, end, limit, offset),
+        limit=limit,
+        offset=initial_offset,
+        num_pages=10,
+    )
+
+    file_name = f"replay-row-data/{uuid.uuid4().hex}"
+    csv_bytes = row_iterator_to_csv(row_iterator)
+    write_to_bucket(destination_bucket, file_name, csv_bytes)
+
+
+@instrumented_task(name="sentry.replays.tasks.export_replay_clickhouse_data_async")
+def export_replay_clickhouse_data_async(project_id: int, limit: int, destination_bucket: str):
+    def generate_ninety_days_pairs() -> Iterator[tuple[datetime, datetime]]:
+        now = datetime.now(tz=timezone.utc)
+
+        start = datetime(year=now.year, month=now.month, day=now.day) - timedelta(days=89)
+        end = start + timedelta(days=1)
+        for _ in range(90):
+            yield (start, end)
+            start = start + timedelta(days=1)
+            end = end + timedelta(days=1)
+
+    for start, end in generate_ninety_days_pairs():
+        export_clickhouse_rows_async.delay(
+            project_id=project_id,
+            start=start,
+            end=end,
+            limit=limit,
+            initial_offset=0,
+            destination_bucket=destination_bucket,
+        )
+
+
+def export_replay_blob_data(
+    project_id: int,
+    gcs_project_id: str,
+    destination_bucket: str,
+    job_duration: timedelta,
+    pubsub_topic_name: str | None = None,
+    source_bucket: str = "sentry-replays",
+):
+    for retention_days in (30, 60, 90):
+        export_blob_data(
+            gcs_project_id=gcs_project_id,
+            source_bucket=source_bucket,
+            source_prefix=f"{retention_days}/{project_id}",
+            destination_bucket=destination_bucket,
+            pubsub_topic_name=pubsub_topic_name,
+            job_duration=job_duration,
+        )
+
+
+def export_replay_data(
+    organization_id: int,
+    destination_bucket: str,
+    gcs_project_id: int,
+    job_duration: timedelta,
+    source_bucket: str = "sentry-replays",
+    pubsub_topic_name: str | None = None,
+):
+    # Assert the organization exists.
+    organization = Organization.objects.filter(organization_id=organization_id).get()
+
+    # Fetch the organization's projects.
+    projects = Project.objects.filter(
+        organization=organization,
+        flags=F("flags").bitor(Project.flags.has_replays),
+    )
+
+    for project in projects:
+        export_replay_blob_data(
+            project_id=project.id,
+            gcs_project_id=gcs_project_id,
+            destination_bucket=destination_bucket,
+            pubsub_topic_name=pubsub_topic_name,
+            source_bucket=source_bucket,
+            job_duration=job_duration,
+        )
+
+    for project in projects:
+        export_replay_clickhouse_data_async.delay(
+            project_id=project.id,
+            limit=1000,
+            destination_bucket=destination_bucket,
+        )
