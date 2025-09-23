f4232c8e1aeab778a01eaaf063a3a49c05f7f99d
isabellaenriquez <isabella.enriquez@sentry.io>
2025-09-23 13:08:47 -0400

    feat(checkout v3): Add billing info step

---
+++ static/gsApp/views/amCheckout/steps/checkoutV3/addBillingInfo.tsx
@@ -0,0 +1,65 @@
+import {Fragment, useState} from 'react';
+
+import {Flex} from 'sentry/components/core/layout';
+import {t} from 'sentry/locale';
+import {useLocation} from 'sentry/utils/useLocation';
+
+import BillingDetailsPanel from 'getsentry/components/billingDetails/panel';
+import CreditCardPanel from 'getsentry/components/creditCardEdit/panel';
+import {useBillingDetails} from 'getsentry/hooks/useBillingDetails';
+import {FTCConsentLocation} from 'getsentry/types';
+import StepHeader from 'getsentry/views/amCheckout/steps/stepHeader';
+import type {CheckoutV3StepProps} from 'getsentry/views/amCheckout/types';
+
+function AddBillingInformation({
+  subscription,
+  onEdit,
+  stepNumber,
+  organization,
+  activePlan,
+}: CheckoutV3StepProps) {
+  const [isOpen, setIsOpen] = useState(true);
+  const location = useLocation();
+  const {data: billingDetails} = useBillingDetails();
+  const hasSomeBillingInfo = billingDetails || subscription.paymentSource;
+
+  return (
+    <Flex direction="column" gap="xl">
+      <StepHeader
+        isActive
+        isCompleted={false}
+        onEdit={onEdit}
+        onToggleStep={setIsOpen}
+        isOpen={isOpen}
+        stepNumber={stepNumber}
+        title={
+          hasSomeBillingInfo
+            ? t('Edit billing information')
+            : t('Add billing information')
+        }
+        isNewCheckout
+      />
+      {isOpen && (
+        <Fragment>
+          <BillingDetailsPanel
+            organization={organization}
+            subscription={subscription}
+            isNewBillingUI
+            analyticsEvent="checkout.updated_billing_details"
+          />
+          <CreditCardPanel
+            organization={organization}
+            subscription={subscription}
+            isNewBillingUI
+            location={location}
+            ftcLocation={FTCConsentLocation.CHECKOUT}
+            budgetTerm={activePlan.budgetTerm}
+            analyticsEvent="checkout.updated_cc"
+          />
+        </Fragment>
+      )}
+    </Flex>
+  );
+}
+
+export default AddBillingInformation;
