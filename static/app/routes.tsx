37642a0bb70b81773e2eaad8b091c412be7f688e
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-09-24 16:41:56 -0700

    ref(routes): Remove deprecatedRouteProps from some routes that take no props

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -234,7 +234,6 @@ function buildRoutes(): RouteObject[] {
     {
       path: '/organizations/new/',
       component: make(() => import('sentry/views/organizationCreate')),
-      deprecatedRouteProps: true,
     },
     {
       path: '/data-export/:dataExportId',
@@ -250,7 +249,6 @@ function buildRoutes(): RouteObject[] {
           path: '/disabled-member/',
           component: make(() => import('sentry/views/disabledMember')),
           withOrgPath: true,
-          deprecatedRouteProps: true,
         },
       ],
     },
@@ -258,12 +256,10 @@ function buildRoutes(): RouteObject[] {
       path: '/restore/',
       component: make(() => import('sentry/views/organizationRestore')),
       customerDomainOnlyRoute: true,
-      deprecatedRouteProps: true,
     },
     {
       path: '/organizations/:orgId/restore/',
       component: make(() => import('sentry/views/organizationRestore')),
-      deprecatedRouteProps: true,
     },
     {
       path: '/join-request/',
@@ -369,7 +365,6 @@ function buildRoutes(): RouteObject[] {
                 'sentry/views/settings/account/notifications/notificationSettingsController'
               )
           ),
-          deprecatedRouteProps: true,
         },
         {
           path: ':fineTuneType/',
@@ -511,7 +506,6 @@ function buildRoutes(): RouteObject[] {
               component: make(
                 () => import('sentry/views/settings/account/apiApplications/details')
               ),
-              deprecatedRouteProps: true,
             },
           ],
         },
@@ -1336,7 +1330,6 @@ function buildRoutes(): RouteObject[] {
       {
         index: true,
         component: make(() => import('sentry/views/settings/settingsIndex')),
-        deprecatedRouteProps: true,
       },
       accountSettingsRoutes,
       {
