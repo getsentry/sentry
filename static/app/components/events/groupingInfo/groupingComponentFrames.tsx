1e7e6da9abedb55fe7be886a5e34fc0022c99ad9
Shayna Chambless <shayna.chambless@sentry.io>
2025-09-25 16:02:19 -0700

    no css

--- static/app/components/events/groupingInfo/groupingComponentFrames.tsx
+++ static/app/components/events/groupingInfo/groupingComponentFrames.tsx
@@ -1,5 +1,4 @@
 import {Fragment, useEffect, useState} from 'react';
-import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import {Button} from 'sentry/components/core/button';
