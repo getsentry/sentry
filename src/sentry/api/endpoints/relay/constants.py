9ea0f52c24711e40a55259e0ff453f813bb0be6d
Charlie Luo <cvxluo@gmail.com>
2025-09-25 15:02:41 -0700

    use RateLimitConfig instead of dict for relay

--- src/sentry/api/endpoints/relay/constants.py
+++ src/sentry/api/endpoints/relay/constants.py
@@ -1,9 +1,10 @@
+from sentry.ratelimits.config import RateLimitConfig
 from sentry.types.ratelimit import RateLimit, RateLimitCategory
 
-RELAY_AUTH_RATE_LIMITS = {
-    "default": {
+RELAY_AUTH_RATE_LIMITS = RateLimitConfig(
+    limit_overrides={
         RateLimitCategory.IP: RateLimit(limit=200, window=1),
         RateLimitCategory.USER: RateLimit(limit=200, window=1),
         RateLimitCategory.ORGANIZATION: RateLimit(limit=200, window=1),
     }
-}
+)
