5999cec3a0a518e3dfcd1054bf5fbc5e5919be7b
Charlie Luo <cvxluo@gmail.com>
2025-09-26 15:45:49 -0700

    fix http method

--- src/sentry/api/endpoints/relay/constants.py
+++ src/sentry/api/endpoints/relay/constants.py
@@ -3,8 +3,10 @@ from sentry.types.ratelimit import RateLimit, RateLimitCategory
 
 RELAY_AUTH_RATE_LIMITS = RateLimitConfig(
     limit_overrides={
-        RateLimitCategory.IP: RateLimit(limit=200, window=1),
-        RateLimitCategory.USER: RateLimit(limit=200, window=1),
-        RateLimitCategory.ORGANIZATION: RateLimit(limit=200, window=1),
+        "POST": {
+            RateLimitCategory.IP: RateLimit(limit=200, window=1),
+            RateLimitCategory.USER: RateLimit(limit=200, window=1),
+            RateLimitCategory.ORGANIZATION: RateLimit(limit=200, window=1),
+        },
     }
 )
