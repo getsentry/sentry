932d9bd77caae40f9de4de6041595c4e60b02d4a
jerryzhou196 <jerry.zhou@sentry.io>
2025-09-29 14:50:22 -0400

    removed unnecessary useRef hook

--- static/app/views/replays/detail/header/replayDetailsUserBadge.tsx
+++ static/app/views/replays/detail/header/replayDetailsUserBadge.tsx
@@ -1,4 +1,4 @@
-import {useCallback, useEffect, useRef, useState} from 'react';
+import {useCallback, useEffect, useState} from 'react';
 import {keyframes} from '@emotion/react';
 import styled from '@emotion/styled';
 
