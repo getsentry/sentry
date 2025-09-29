d35c2c4e0cd62004fb24b94791870b404b991f5c
Charlie Luo <cvxluo@gmail.com>
2025-09-29 14:46:53 -0700

    transfer code mappings when a project is transfered

--- src/sentry/models/project.py
+++ src/sentry/models/project.py
@@ -500,6 +500,9 @@ class Project(Model):
         from sentry.deletions.models.scheduleddeletion import RegionScheduledDeletion
         from sentry.incidents.models.alert_rule import AlertRule
         from sentry.integrations.models.external_issue import ExternalIssue
+        from sentry.integrations.models.repository_project_path_config import (
+            RepositoryProjectPathConfig,
+        )
         from sentry.models.environment import Environment, EnvironmentProject
         from sentry.models.projectteam import ProjectTeam
         from sentry.models.releaseprojectenvironment import ReleaseProjectEnvironment
@@ -633,6 +636,11 @@ class Project(Model):
         # Manually move over external issues to the new org
         linked_groups = GroupLink.objects.filter(project_id=self.id).values_list(
             "linked_id", flat=True
+        )
+
+        # Manually move over code mappings to the new project
+        RepositoryProjectPathConfig.objects.filter(project_id=self.id).update(
+            project_id=self.id, organization_id=organization.id
         )
 
         for external_issues in chunked(
