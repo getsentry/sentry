7721073eb9db8e18ac4f8f696138c5a5712209c3
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-09-29 11:20:39 +0200

    ref(tempest): Remove tempest feature check BE

--- src/sentry/tempest/utils.py
+++ src/sentry/tempest/utils.py
@@ -1,20 +1,12 @@
-from django.contrib.auth.models import AnonymousUser
-
-from sentry import features
 from sentry.models.organization import Organization
-from sentry.users.models.user import User
-from sentry.users.services.user import RpcUser
 
 
-def has_tempest_access(
-    organization: Organization | None, actor: User | RpcUser | AnonymousUser | None = None
-) -> bool:
-    has_tempest_feature = features.has("organizations:tempest-access", organization, actor=actor)
+def has_tempest_access(organization: Organization | None) -> bool:
 
-    if organization:
-        enabled_platforms = organization.get_option("sentry:enabled_console_platforms", [])
-        has_playstation_access = "playstation" in enabled_platforms
+    if not organization:
+        return False
 
-        return has_tempest_feature or has_playstation_access
+    enabled_platforms = organization.get_option("sentry:enabled_console_platforms", [])
+    has_playstation_access = "playstation" in enabled_platforms
 
-    return has_tempest_feature
+    return has_playstation_access
