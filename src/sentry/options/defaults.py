74de31db24649aec12817a4c653dee068940a157
David Herberth <david.herberth@sentry.io>
2025-09-30 13:48:17 +0200

    ref(relay): Add rollout option for new sessions processing pipeline (#100596)
    Relay PR: https://github.com/getsentry/relay/pull/5189/

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -601,6 +601,14 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
+# Rollout rate for the new sessions processing pipeline.
+register(
+    "relay.session.processing.rollout",
+    type=Float,
+    default=0.0,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
+
 # Analytics
 register("analytics.backend", default="noop", flags=FLAG_NOSTORE)
 register("analytics.options", default={}, flags=FLAG_NOSTORE)
