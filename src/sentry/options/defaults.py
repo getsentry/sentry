bdc287628e574d334248e75a3faa72c602c692e6
Kyle Consalus <kyle.consalus@sentry.io>
2025-09-29 17:11:36 -0700

    initial

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3118,6 +3118,13 @@ register(
 )
 
 register(
+    "workflow_engine.associate_error_detectors",
+    type=Bool,
+    default=False,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
+
+register(
     "grouping.grouphash_metadata.ingestion_writes_enabled",
     type=Bool,
     default=True,
