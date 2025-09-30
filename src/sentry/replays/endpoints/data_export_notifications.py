bacdc5a7e5f15c32590d332cc0ef8ee927ba4e17
Colton Allen <colton.allen@sentry.io>
2025-09-30 13:57:49 -0500

    Add retry endpoint for handling transfer-job failures

--- src/sentry/replays/endpoints/data_export_notifications.py
+++ src/sentry/replays/endpoints/data_export_notifications.py
@@ -5,29 +5,27 @@ from rest_framework.response import Response
 
 from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
-from sentry.api.authentication import ApiKeyAuthentication
 from sentry.api.base import Endpoint, control_silo_endpoint
 from sentry.api.permissions import SentryIsAuthenticated
-from sentry.replays.data_export import retry_transfer_job_run
+from sentry.replays.data_export import request_run_transfer_job, retry_transfer_job_run
 
 logger = logging.getLogger()
 
 
 @control_silo_endpoint
-class NotificationEndpoint(Endpoint):
+class DataExportNotificationsEndpoint(Endpoint):
     """PubSub notifications endpoint."""
 
     owner = ApiOwner.REPLAY
     publish_status = {"GET": ApiPublishStatus.PRIVATE}
-    authentication_classes = (ApiKeyAuthentication,)
     permission_classes = (SentryIsAuthenticated,)
 
-    def get(self, request: Request, service: str) -> Response:
+    def post(self, request: Request, service: str) -> Response:
         if service == "google-cloud":
-            retry_transfer_job_run(request.data)
+            retry_transfer_job_run(request.data, request_run_transfer_job)
         else:
             logger.error("Service has not been implemented.", extra={"service": service})
 
         # We always return 200 because this is an programmatic request issued by a service
         # provider. We don't want to process the same message multiple times.
-        return Response("", status="200")
+        return Response("", status=200)
---
+++ tests/sentry/replays/endpoints/test_data_export_notifications.py
@@ -0,0 +1,38 @@
+import base64
+from unittest.mock import patch
+
+from google.cloud.storage_transfer_v1 import RunTransferJobRequest
+
+from sentry.testutils.cases import APITestCase
+from sentry.testutils.silo import control_silo_test
+from sentry.utils import json
+
+
+@control_silo_test
+class DataExportNotificationsTestCase(APITestCase):
+    endpoint = "sentry-api-0-data-export-notifications"
+
+    def setUp(self) -> None:
+        super().setUp()
+        self.login_as(user=self.user)
+
+    @patch("sentry.replays.data_export.request_run_transfer_job")
+    def test_simple(self, do_request) -> None:
+        data = {
+            "data": base64.b64encode(
+                json.dumps(
+                    {
+                        "transferOperation": {
+                            "status": "FAILED",
+                            "transferJobName": "test",
+                            "projectId": "test-project",
+                        }
+                    }
+                ).encode()
+            ).decode("utf-8")
+        }
+        self.get_success_response("google-cloud", method="post", **data, status_code=200)
+        assert do_request.called
+        assert do_request.call_args[0][0] == RunTransferJobRequest(
+            job_name="test", project_id="test-project"
+        )
