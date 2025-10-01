210391870a52e199f614df8d0d688a5d20197c63
Katie Byers <lobsterkatie@gmail.com>
2025-10-01 10:48:18 -0700

    add `RootGroupingComponent` class

--- src/sentry/grouping/component.py
+++ src/sentry/grouping/component.py
@@ -483,3 +483,27 @@ ContributingComponent = (
     | MessageGroupingComponent
     | TemplateGroupingComponent
 )
+
+
+# Wrapper component used to link component trees to variants
+class RootGroupingComponent(BaseGroupingComponent[ContributingComponent]):
+
+    def __init__(
+        self,
+        variant_name: str,
+        hint: str | None = None,
+        contributes: bool | None = None,
+        values: Sequence[ContributingComponent] | None = None,
+    ):
+        super().__init__(hint, contributes, values)
+        self.variant_name = variant_name
+
+    @property
+    def id(self) -> str:
+        return self.variant_name
+
+    def __repr__(self) -> str:
+        base_repr = super().__repr__()
+        # Fake the class name so that instead of showing as `RootGroupingComponent` in the repr it
+        # shows as `AppGroupingComponent`/`SystemGroupingComponent`/`DefaultGroupingComponent`
+        return base_repr.replace("Root", self.id.title())
