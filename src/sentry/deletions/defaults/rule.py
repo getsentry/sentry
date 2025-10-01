b09cd265df7e4b2e3caf7ad6dd64478a1e53c12d
Cathy Teng <cathy.teng@sentry.io>
2025-10-01 15:43:22 -0700

    delete Workflow before AlertRuleWorkflow

--- src/sentry/deletions/defaults/rule.py
+++ src/sentry/deletions/defaults/rule.py
@@ -1,8 +1,11 @@
+import logging
 from collections.abc import Sequence
 
 from sentry.deletions.base import BaseRelation, ModelDeletionTask, ModelRelation
 from sentry.models.rule import Rule
 from sentry.workflow_engine.models import Workflow
+
+logger = logging.getLogger(__name__)
 
 
 class RuleDeletionTask(ModelDeletionTask[Rule]):
@@ -17,11 +20,16 @@ class RuleDeletionTask(ModelDeletionTask[Rule]):
             ModelRelation(RuleFireHistory, {"rule_id": instance.id}),
             ModelRelation(RuleActivity, {"rule_id": instance.id}),
             ModelRelation(AlertRuleDetector, {"rule_id": instance.id}),
-            ModelRelation(AlertRuleWorkflow, {"rule_id": instance.id}),
         ]
 
-        alert_rule_workflow = AlertRuleWorkflow.objects.get(rule_id=instance.id)
-        model_relations.append(ModelRelation(Workflow, {"id": alert_rule_workflow.workflow_id}))
+        alert_rule_workflow = AlertRuleWorkflow.objects.filter(rule_id=instance.id).first()
+        if alert_rule_workflow:
+            model_relations.append(ModelRelation(Workflow, {"id": alert_rule_workflow.workflow_id}))
+        else:
+            logger.error(
+                "No AlertRuleWorkflow found for rule, skipping", extra={"rule_id": instance.id}
+            )
+        model_relations.append(ModelRelation(AlertRuleWorkflow, {"rule_id": instance.id}))
 
         return model_relations
 
