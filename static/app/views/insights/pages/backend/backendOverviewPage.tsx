7fc1b5a09cd9833c0f059d1d8686bc7ebb57ca68
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-10-01 12:04:05 -0400

    fix

--- static/app/views/insights/pages/backend/backendOverviewPage.tsx
+++ static/app/views/insights/pages/backend/backendOverviewPage.tsx
@@ -51,7 +51,7 @@ import {
   OVERVIEW_PAGE_ALLOWED_OPS as FRONTEND_OVERVIEW_PAGE_OPS,
   WEB_VITALS_OPS,
 } from 'sentry/views/insights/pages/frontend/settings';
-import {OVERVIEW_PAGE_ALLOWED_OPS as BACKEND_OVERVIEW_PAGE_OPS} from 'sentry/views/insights/pages/mobile/settings';
+import {OVERVIEW_PAGE_ALLOWED_OPS as MOBILE_OVERVIEW_PAGE_OPS} from 'sentry/views/insights/pages/mobile/settings';
 import {LaravelOverviewPage} from 'sentry/views/insights/pages/platform/laravel';
 import {useIsLaravelInsightsAvailable} from 'sentry/views/insights/pages/platform/laravel/features';
 import {NextJsOverviewPage} from 'sentry/views/insights/pages/platform/nextjs';
@@ -99,7 +99,7 @@ function EAPBackendOverviewPage() {
   const disallowedOps = [
     ...new Set([
       ...FRONTEND_OVERVIEW_PAGE_OPS,
-      ...BACKEND_OVERVIEW_PAGE_OPS,
+      ...MOBILE_OVERVIEW_PAGE_OPS,
       ...WEB_VITALS_OPS,
     ]),
   ];
