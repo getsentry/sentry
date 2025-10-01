7f2422a3718af3830000fc46869122d35be6d8cf
Yuval Mandelboum <yuval.mandelboum@sentry.io>
2025-10-01 10:04:11 -0700

    removed `artifact.bundle` handling

--- src/sentry/deletions/defaults/releasefile.py
+++ src/sentry/deletions/defaults/releasefile.py
@@ -11,38 +11,16 @@ class ReleaseFileDeletionTask(ModelDeletionTask[ReleaseFile]):
         except File.DoesNotExist:
             return []
 
-        # Always safe to delete Files for these types as they are 1:1 with ReleaseFile
-        always_safe_types = {
+        # Only delete Files for these types as they are 1:1 with ReleaseFile
+        safe_to_delete_types = {
             "release.file",  # Regular uploaded files
             "release.artifact-index",  # Artifact index files (release-specific)
         }
 
-        if file_type in always_safe_types:
+        if file_type in safe_to_delete_types:
             return [
                 ModelRelation(File, {"id": instance.file_id}),
             ]
 
-        # For artifact.bundle files, only delete if no other references exist
-        if file_type == "artifact.bundle":
-            if self._is_last_reference_to_file(instance):
-                return [
-                    ModelRelation(File, {"id": instance.file_id}),
-                ]
-
-        # For other unknown types, don't delete the File to be safe
+        # For other types, don't delete the File to be safe
         return []
-
-    def _is_last_reference_to_file(self, instance: ReleaseFile) -> bool:
-        """Check if this ReleaseFile is the last reference to the File."""
-        from sentry.models.artifactbundle import ArtifactBundle
-
-        # Count other ReleaseFile records pointing to the same file (excluding this one)
-        other_releasefiles = (
-            ReleaseFile.objects.filter(file_id=instance.file_id).exclude(id=instance.id).count()
-        )
-
-        # Count ArtifactBundle records pointing to the same file
-        artifact_bundles = ArtifactBundle.objects.filter(file_id=instance.file_id).count()
-
-        # Only safe to delete if no other references exist
-        return other_releasefiles == 0 and artifact_bundles == 0
