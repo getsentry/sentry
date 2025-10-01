9e80b7e5d5ac89edc53f6b34302284c6944374da
Malachi Willey <malwilley@gmail.com>
2025-10-01 11:27:52 -0700

    fix(alerts): Remove threshold for apdex when using metrics dataset (#100568)
    Ref https://github.com/getsentry/sentry/issues/99890
    
    From discussions, it appears that Apdex now relies on metrics data
    instead of indexed transactions, so the threshold can no longer be
    configured on the alert. As-is, the metric alert form cannot be saved
    when selecting the "Apdex" option. This change allows the form to be
    saved.

--- static/app/views/alerts/rules/metric/ruleForm.spec.tsx
+++ static/app/views/alerts/rules/metric/ruleForm.spec.tsx
@@ -534,6 +534,43 @@ describe('Incident Rules Form', () => {
         })
       );
     });
+
+    it('creates a metrics Apdex rule without satisfaction parameter', async () => {
+      organization.features = [
+        ...organization.features,
+        'performance-view',
+        'mep-rollout-flag',
+      ];
+
+      const rule = MetricRuleFixture();
+      createWrapper({
+        rule: {
+          ...rule,
+          id: undefined,
+          aggregate: 'count()',
+          eventTypes: ['transaction'],
+          dataset: Dataset.TRANSACTIONS,
+        },
+      });
+
+      // Open the wizard and switch to Apdex
+      await userEvent.click(screen.getAllByText('Throughput')[1]!);
+      await userEvent.click(await screen.findByText('Apdex'));
+
+      await userEvent.click(screen.getByLabelText('Save Rule'));
+
+      expect(createRule).toHaveBeenLastCalledWith(
+        expect.anything(),
+        expect.objectContaining({
+          data: expect.objectContaining({
+            // Apdex should be saved without a satisfaction parameter
+            aggregate: 'apdex()',
+            dataset: Dataset.GENERIC_METRICS,
+            eventTypes: ['transaction'],
+          }),
+        })
+      );
+    });
   });
 
   describe('Editing a rule', () => {
