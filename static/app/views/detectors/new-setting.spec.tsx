49bf86b5003b3351ec32ac029848c176584c67ed
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-10-01 16:24:19 -0700

    update dataSource => dataSources in ui

--- static/app/views/detectors/new-setting.spec.tsx
+++ static/app/views/detectors/new-setting.spec.tsx
@@ -142,15 +142,17 @@ describe('DetectorEdit', () => {
                 detectionType: 'static',
                 thresholdPeriod: 1,
               },
-              dataSource: {
-                aggregate: 'count(span.duration)',
-                dataset: 'events_analytics_platform',
-                eventTypes: ['trace_item_span'],
-                query: '',
-                queryType: 1,
-                timeWindow: 3600,
-                environment: null,
-              },
+              dataSources: [
+                {
+                  aggregate: 'count(span.duration)',
+                  dataset: 'events_analytics_platform',
+                  eventTypes: ['trace_item_span'],
+                  query: '',
+                  queryType: 1,
+                  timeWindow: 3600,
+                  environment: null,
+                },
+              ],
             }),
           })
         );
@@ -275,16 +277,18 @@ describe('DetectorEdit', () => {
                 logicType: 'any',
               },
               config: {detectionType: 'static', thresholdPeriod: 1},
-              dataSource: {
-                aggregate: 'count()',
-                dataset: 'events',
-                environment: null,
-                // Event type has moved from the query to the eventTypes field
-                eventTypes: ['error'],
-                query: '',
-                queryType: 0,
-                timeWindow: 3600,
-              },
+              dataSources: [
+                {
+                  aggregate: 'count()',
+                  dataset: 'events',
+                  environment: null,
+                  // Event type has moved from the query to the eventTypes field
+                  eventTypes: ['error'],
+                  query: '',
+                  queryType: 0,
+                  timeWindow: 3600,
+                },
+              ],
               name: 'Foo',
               owner: null,
               projectId: '2',
@@ -426,13 +430,15 @@ describe('DetectorEdit', () => {
               mode: 1,
               recoveryThreshold: 1,
             },
-            dataSource: {
-              intervalSeconds: 60,
-              method: 'GET',
-              timeoutMs: 5000,
-              traceSampling: undefined,
-              url: 'https://uptime.example.com',
-            },
+            dataSources: [
+              {
+                intervalSeconds: 60,
+                method: 'GET',
+                timeoutMs: 5000,
+                traceSampling: undefined,
+                url: 'https://uptime.example.com',
+              },
+            ],
             name: 'New MonitorUptime Monitor',
             projectId: '2',
             type: 'uptime_domain_failure',
@@ -486,13 +492,13 @@ describe('DetectorEdit', () => {
               mode: 1,
               recoveryThreshold: '4',
             },
-            dataSource: {
+            dataSources: [{
               intervalSeconds: 60,
               method: 'GET',
               timeoutMs: 5000,
               traceSampling: undefined,
               url: 'https://uptime-custom.example.com',
-            },
+            }],
             name: 'Uptime check for uptime-custom.example.com',
             projectId: '2',
             type: 'uptime_domain_failure',
