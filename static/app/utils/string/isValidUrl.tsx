ada1813e90814d5989d9dac823c021b702b7764f
Tony Xiao <txiao@sentry.io>
2025-10-02 11:26:46 -0400

    more strict

--- static/app/utils/string/isValidUrl.tsx
+++ static/app/utils/string/isValidUrl.tsx
@@ -1,4 +1,10 @@
+import {isUrl} from 'sentry/utils/string/isUrl';
+
 export function isValidUrl(str: any): boolean {
+  // javascript:void(0) is a valid url so ensure it starts with http:// or https://
+  if (!isUrl(str)) {
+    return false;
+  }
   try {
     return !!new URL(str);
   } catch {
