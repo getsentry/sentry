3519835b296e8eb478cb84892a8f8dac80ad70ca
Yuval Mandelboum <yuval.mandelboum@sentry.io>
2025-10-02 11:16:07 -0700

    chore(cleanup): Add File cleanup task
    Currently only cleans up old orphaned release type Files, which basically had their ReleaseFile
    deleted but stayed hanging. We are trusting the `type` field here to differentiate - release type
    File should not be on their own and are 1:1 with ReleaseFile

---
+++ src/sentry/deletions/defaults/file.py
@@ -0,0 +1,45 @@
+from datetime import datetime, timedelta, timezone
+
+from django.db.models import Q
+
+from sentry.deletions.base import BaseRelation, ModelDeletionTask
+from sentry.models.files.file import File
+
+
+class FileDeletionTask(ModelDeletionTask[File]):
+    def get_query_filter(self) -> Q:
+        """
+        Returns a Q object that filters for orphaned release-type Files.
+        Only targets Files that are:
+        1. Of release-related types (release.file, release.artifact-index)
+        2. Have no corresponding ReleaseFile entry
+        3. Are older than 90 days
+        """
+        from django.db.models import Exists, OuterRef
+
+        from sentry.models.releasefile import ReleaseFile
+
+        cutoff = datetime.now(timezone.utc) - timedelta(days=90)
+
+        # Subquery for checking if ReleaseFile references this File
+        releasefile_exists = Exists(ReleaseFile.objects.filter(file_id=OuterRef("id")))
+
+        return Q(
+            Q(
+                type__in=["release.file", "release.artifact-index"],
+                timestamp__lt=cutoff,
+            )
+            & ~releasefile_exists
+        )
+
+    def get_child_relations(self, instance: File) -> list[BaseRelation]:
+        from sentry.models.files.fileblobindex import FileBlobIndex
+
+        # Delete the associated FileBlobIndex records
+        # The FileBlob cleanup is handled by a separate task
+        return [
+            BaseRelation(
+                params={"model": FileBlobIndex, "query": {"file_id": instance.id}},
+                task=None,  # Use BulkModelDeletionTask
+            ),
+        ]
