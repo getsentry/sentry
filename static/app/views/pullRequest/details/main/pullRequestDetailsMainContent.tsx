461e3b87da974eeff08e12330e69829bae9c806e
Trevor Elkins <trevor.elkins@sentry.io>
2025-10-02 16:52:40 -0400

    feat(pr-page): add files list components (#100802)
    <img width="1428" height="1121" alt="Screenshot 2025-10-02 at 3 05
    06 PM"
    src="https://github.com/user-attachments/assets/a718c80c-d1ed-4ed8-a6a5-a198e6ace0f2"
    />

--- static/app/views/pullRequest/details/main/pullRequestDetailsMainContent.tsx
+++ static/app/views/pullRequest/details/main/pullRequestDetailsMainContent.tsx
@@ -1,7 +1,7 @@
 import {Stack} from 'sentry/components/core/layout/stack';
-import {Heading, Text} from 'sentry/components/core/text';
-import {t} from 'sentry/locale';
 import type {PullRequestDetailsSuccessResponse} from 'sentry/views/pullRequest/types/pullRequestDetailsTypes';
+
+import PRFilesList from './prFilesList';
 
 interface PullRequestDetailsMainContentProps {
   pullRequest: PullRequestDetailsSuccessResponse;
@@ -12,11 +12,7 @@ export function PullRequestDetailsMainContent({
 }: PullRequestDetailsMainContentProps) {
   return (
     <Stack gap="md">
-      <Heading as="h2">
-        {t('Files Changed')} ({pullRequest.files.length})
-      </Heading>
-
-      <Text>Coming soon...</Text>
+      <PRFilesList files={pullRequest.files} />
     </Stack>
   );
 }
