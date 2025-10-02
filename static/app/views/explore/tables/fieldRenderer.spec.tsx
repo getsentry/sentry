b672b76f8818605a6d272f14b5e38b4d04e0dba0
Matt Quinn <matt.quinn@sentry.io>
2025-10-02 11:50:46 -0400

    fix(explore): attach project badge to name field for otel UI
    When the `performance-otel-friendly-ui` organization flag is on, the default
    columns in Explore remove `span.op` and `span.description` in favor of
    `span.name`. `span.description` has special treatment when rendering to include
    a project badge with the project platform icon. When the OTel friendly flag is
    on, apply this special treatment to the `span.name` field instead.

--- static/app/views/explore/tables/fieldRenderer.spec.tsx
+++ static/app/views/explore/tables/fieldRenderer.spec.tsx
@@ -1,21 +1,25 @@
 import type {Location} from 'history';
 import {LocationFixture} from 'sentry-fixture/locationFixture';
 import {OrganizationFixture} from 'sentry-fixture/organization';
+import {ProjectFixture} from 'sentry-fixture/project';
 
 import {render, screen} from 'sentry-test/reactTestingLibrary';
 import {resetMockDate, setMockDate} from 'sentry-test/utils';
 
+import ProjectsStore from 'sentry/stores/projectsStore';
 import EventView from 'sentry/utils/discover/eventView';
 
 import {FieldRenderer} from './fieldRenderer';
 
 const mockedEventData = {
   id: 'spanId',
+  project: 'project-1',
   timestamp: '2024-10-03T10:15:00',
   trace: 'traceId',
   'span.op': 'test_op',
   'transaction.id': 'transactionId',
   'transaction.span_id': 'transactionSpanId',
+  'span.description': 'GET /foo',
 };
 
 describe('FieldRenderer tests', () => {
@@ -25,7 +29,15 @@ describe('FieldRenderer tests', () => {
     query: {
       id: '42',
       name: 'best query',
-      field: ['id', 'timestamp', 'trace', 'span.op', 'transaction.id'],
+      field: [
+        'id',
+        'timestamp',
+        'trace',
+        'span.op',
+        'transaction.id',
+        'span.description',
+        'span.name',
+      ],
     },
   });
 
@@ -34,11 +46,22 @@ describe('FieldRenderer tests', () => {
   beforeAll(() => {
     const mockTimestamp = new Date('2024-10-06T00:00:00').getTime();
     setMockDate(mockTimestamp);
+
+    const projects = [
+      ProjectFixture({
+        id: '1',
+        slug: 'project-1',
+        name: 'Project 1',
+        platform: 'javascript',
+      }),
+    ];
+    ProjectsStore.loadInitialData(projects);
   });
 
   afterAll(() => {
     jest.restoreAllMocks();
     resetMockDate();
+    ProjectsStore.reset();
   });
 
   it('renders span.op', () => {
@@ -117,5 +140,63 @@ describe('FieldRenderer tests', () => {
 
     expect(screen.getByRole('time')).toBeInTheDocument();
     expect(screen.getByText('3d ago')).toBeInTheDocument();
+  });
+
+  it('renders description with project badge', () => {
+    render(
+      <FieldRenderer
+        column={eventView.getColumns()[5]}
+        data={mockedEventData}
+        meta={{}}
+      />,
+      {organization}
+    );
+    expect(screen.getByTestId('platform-icon-javascript')).toBeInTheDocument();
+  });
+
+  it('renders name without project badge', () => {
+    render(
+      <FieldRenderer
+        column={eventView.getColumns()[6]}
+        data={mockedEventData}
+        meta={{}}
+      />,
+      {organization}
+    );
+    expect(screen.queryByTestId('platform-icon-javascript')).not.toBeInTheDocument();
+  });
+
+  describe('with otel friendly UI flag', () => {
+    beforeAll(() => {
+      organization.features.push('performance-otel-friendly-ui');
+    });
+
+    afterAll(() => {
+      organization.features.pop();
+    });
+
+    it('renders description without project badge', () => {
+      render(
+        <FieldRenderer
+          column={eventView.getColumns()[5]}
+          data={mockedEventData}
+          meta={{}}
+        />,
+        {organization}
+      );
+      expect(screen.queryByTestId('platform-icon-javascript')).not.toBeInTheDocument();
+    });
+
+    it('renders name with project badge', () => {
+      render(
+        <FieldRenderer
+          column={eventView.getColumns()[6]}
+          data={mockedEventData}
+          meta={{}}
+        />,
+        {organization}
+      );
+      expect(screen.getByTestId('platform-icon-javascript')).toBeInTheDocument();
+    });
   });
 });
