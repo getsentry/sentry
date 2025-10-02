bf13752480a0eb54031fb14dff6651f586905f4b
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-10-02 11:57:02 -0400

    fix

--- static/app/views/insights/mobile/screenload/components/charts/screenCharts.tsx
+++ static/app/views/insights/mobile/screenload/components/charts/screenCharts.tsx
@@ -14,10 +14,10 @@ import {useFetchSpanTimeSeries} from 'sentry/utils/timeSeries/useFetchEventsTime
 import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import {formatVersion} from 'sentry/utils/versions/formatVersion';
 import type {TimeSeries} from 'sentry/views/dashboards/widgets/common/types';
+import {formatTimeSeriesName} from 'sentry/views/dashboards/widgets/timeSeriesWidget/formatters/formatTimeSeriesName';
 // TODO(release-drawer): Only used in mobile/screenload/components/
 // eslint-disable-next-line no-restricted-imports
 import {InsightsLineChartWidget} from 'sentry/views/insights/common/components/insightsLineChartWidget';
-import {type DiscoverSeries} from 'sentry/views/insights/common/queries/types';
 import {useReleaseSelection} from 'sentry/views/insights/common/queries/useReleases';
 import {appendReleaseFilters} from 'sentry/views/insights/common/utils/releaseComparison';
 import useCrossPlatformProject from 'sentry/views/insights/mobile/common/queries/useCrossPlatformProject';
@@ -140,25 +140,25 @@ export function ScreenCharts({additionalFilters}: Props) {
     }
 
     const isPrimary = releaseName === primaryRelease;
-    const colors = theme.chart.getColorPalette(3);
-    const color = isPrimary ? colors[0] : colors[1];
     const version = formatVersion(releaseName, true);
-
-    const seriesNames = [
-      'avg(measurements.time_to_initial_display)',
-      'avg(measurements.time_to_full_display)',
-      'count()',
-    ] as const;
 
     const yAxis = release.yAxis as
       | 'avg(measurements.time_to_initial_display)'
       | 'avg(measurements.time_to_full_display)'
       | 'count()';
 
-    seriesMap[yAxis].push(release);
+    const plottableName = formatTimeSeriesName(release);
+
+    // This ensures the primary release is always the first in the series, which will give it the first color.
+    if (isPrimary) {
+      seriesMap[yAxis] = [release, ...seriesMap[yAxis]];
+    } else {
+      seriesMap[yAxis] = [...seriesMap[yAxis], release];
+    }
+
     chartAliases = {
       ...chartAliases,
-      [yAxis]: version,
+      [plottableName]: version,
     };
 
     // seriesNames.forEach(seriesName => {
@@ -208,7 +208,7 @@ export function ScreenCharts({additionalFilters}: Props) {
           <InsightsLineChartWidget
             queryInfo={{search: query, groupBy: [groupBy], referrer}}
             title={t('Average TTID')}
-            series={seriesMap['avg(measurements.time_to_initial_display)']}
+            timeSeries={seriesMap['avg(measurements.time_to_initial_display)']}
             isLoading={isSeriesLoading}
             error={seriesError}
             aliases={chartAliases}
@@ -219,7 +219,7 @@ export function ScreenCharts({additionalFilters}: Props) {
           <InsightsLineChartWidget
             queryInfo={{search: query, groupBy: [groupBy], referrer}}
             title={CHART_TITLES[YAxis.COUNT]}
-            series={seriesMap['count()']}
+            timeSeries={seriesMap['count()']}
             isLoading={isSeriesLoading}
             error={seriesError}
             aliases={chartAliases}
@@ -231,7 +231,7 @@ export function ScreenCharts({additionalFilters}: Props) {
           <InsightsLineChartWidget
             queryInfo={{search: query, groupBy: [groupBy], referrer}}
             title={t('Average TTFD')}
-            series={seriesMap['avg(measurements.time_to_full_display)']}
+            timeSeries={seriesMap['avg(measurements.time_to_full_display)']}
             isLoading={isSeriesLoading}
             error={seriesError}
             aliases={chartAliases}
