53f00888290eeb75df8446d1cf28098913a2b84e
isabellaenriquez <isabella.enriquez@sentry.io>
2025-10-02 15:32:12 -0400

    feat(sub v3): Billing Info header card

--- static/gsApp/views/subscriptionPage/headerCards/headerCards.tsx
+++ static/gsApp/views/subscriptionPage/headerCards/headerCards.tsx
@@ -1,13 +1,18 @@
-import {css} from '@emotion/react';
-import styled from '@emotion/styled';
+import {useMemo} from 'react';
+import moment from 'moment-timezone';
 
+import {Container, Flex, Grid} from 'sentry/components/core/layout';
+import {Text} from 'sentry/components/core/text';
 import ErrorBoundary from 'sentry/components/errorBoundary';
-import Panel from 'sentry/components/panels/panel';
-import {space} from 'sentry/styles/space';
+import Placeholder from 'sentry/components/placeholder';
+import {IconSettings, IconUser} from 'sentry/icons';
+import {t, tct} from 'sentry/locale';
 import type {Organization} from 'sentry/types/organization';
 
+import {useBillingDetails} from 'getsentry/hooks/useBillingDetails';
 import type {Subscription} from 'getsentry/types';
 import {hasNewBillingUI} from 'getsentry/utils/billing';
+import SubscriptionHeaderCard from 'getsentry/views/subscriptionPage/headerCards/subscriptionHeaderCard';
 import SeerAutomationAlert from 'getsentry/views/subscriptionPage/seerAutomationAlert';
 
 import {SubscriptionCard} from './subscriptionCard';
@@ -18,29 +23,133 @@ interface HeaderCardsProps {
   subscription: Subscription;
 }
 
-export function HeaderCards({organization, subscription}: HeaderCardsProps) {
+function HeaderCards({organization, subscription}: HeaderCardsProps) {
+  const isNewBillingUI = hasNewBillingUI(organization);
+
+  const cards = [
+    <BillingInfoCard
+      key="billing-info"
+      subscription={subscription}
+      organization={organization}
+    />,
+  ].filter(card => card !== null);
+
   return (
     <ErrorBoundary mini>
       <SeerAutomationAlert organization={organization} />
-      <HeaderCardWrapper hasNewCheckout={hasNewBillingUI(organization)}>
-        <SubscriptionCard organization={organization} subscription={subscription} />
-        <UsageCard organization={organization} subscription={subscription} />
-      </HeaderCardWrapper>
+      {isNewBillingUI ? (
+        <Grid
+          columns={{
+            xs: '1fr',
+            sm: `repeat(${Math.min(cards.length, 2)}, 1fr)`,
+            md: `repeat(${cards.length}, 1fr)`,
+          }}
+        >
+          {cards}
+        </Grid>
+      ) : (
+        <Grid
+          background="primary"
+          border="primary"
+          radius="md"
+          columns={{lg: 'auto minmax(0, 600px)'}}
+          gap={{lg: 'xl'}}
+        >
+          <SubscriptionCard organization={organization} subscription={subscription} />
+          <UsageCard organization={organization} subscription={subscription} />
+        </Grid>
+      )}
     </ErrorBoundary>
   );
 }
 
-// TODO(checkout v3): update this with the real layout
-const HeaderCardWrapper = styled(Panel)<{hasNewCheckout: boolean}>`
-  display: grid;
-  margin-bottom: ${space(2)};
+function BillingInfoCard({
+  subscription,
+  organization,
+}: {
+  organization: Organization;
+  subscription: Subscription;
+}) {
+  const {data: billingDetails, isLoading} = useBillingDetails();
+  const {paymentSource} = subscription;
 
-  ${p =>
-    !p.hasNewCheckout &&
-    css`
-      @media (min-width: ${p.theme.breakpoints.lg}) {
-        grid-template-columns: auto minmax(0, 600px);
-        gap: ${space(2)};
-      }
-    `}
-`;
+  const BillingDetailsInfo = useMemo(() => {
+    if (isLoading) {
+      return (
+        <Flex direction="column" gap="xs">
+          <Placeholder height="16px" />
+          <Placeholder height="16px" />
+          <Placeholder height="16px" />
+          <Placeholder height="16px" />
+        </Flex>
+      );
+    }
+
+    if (!billingDetails) {
+      return (
+        <Container>
+          <Text>{t('No billing details on file')}</Text>
+        </Container>
+      );
+    }
+
+    return (
+      <Flex direction="column" gap="xs">
+        {billingDetails.companyName && (
+          <Text variant="muted" size="sm">
+            {billingDetails.companyName}
+          </Text>
+        )}
+        {billingDetails.billingEmail && (
+          <Text variant="muted" size="sm">
+            {billingDetails.billingEmail}
+          </Text>
+        )}
+        {billingDetails.displayAddress && (
+          <Text variant="muted" size="sm">
+            {billingDetails.displayAddress}
+          </Text>
+        )}
+      </Flex>
+    );
+  }, [billingDetails, isLoading]);
+
+  if (subscription.isSelfServePartner || !subscription.canSelfServe) {
+    return null;
+  }
+
+  const paymentSourceExpiryDate = paymentSource
+    ? moment(new Date(paymentSource.expYear, paymentSource.expMonth - 1))
+    : null;
+
+  const PaymentSourceInfo = paymentSource ? (
+    <Flex direction="column" gap="xs">
+      <Text>{tct('Card ending in [last4]', {last4: paymentSource.last4})}</Text>
+      <Text variant="muted" size="sm">
+        {tct('Expires [expMonth]/[expYear]', {
+          expMonth: paymentSourceExpiryDate?.format('MM'),
+          expYear: paymentSourceExpiryDate?.format('YY'),
+        })}
+      </Text>
+    </Flex>
+  ) : (
+    <Text>{t('No payment method on file')}</Text>
+  );
+
+  return (
+    <SubscriptionHeaderCard
+      title={t('Billing information')}
+      icon={<IconUser />}
+      sections={[BillingDetailsInfo, PaymentSourceInfo]}
+      button={{
+        ariaLabel: t('Edit billing information'),
+        label: t('Edit billing information'),
+        linkTo: `/settings/${organization.slug}/billing/details/`,
+        icon: <IconSettings />,
+        priority: 'default',
+      }}
+    />
+  );
+}
+
+export default HeaderCards;
