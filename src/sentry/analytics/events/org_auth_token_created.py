852e269d9c83bca33669512f8ccfc7ed10becb7e
Simon Hellmayr <shellmayr@users.noreply.github.com>
2025-10-02 11:18:01 +0200

    ref(analytics): unify analytics imports (#100769)

--- src/sentry/analytics/events/org_auth_token_created.py
+++ src/sentry/analytics/events/org_auth_token_created.py
@@ -1,9 +1,8 @@
 from sentry import analytics
-from sentry.analytics import Event, eventclass
 
 
-@eventclass("org_auth_token.created")
-class OrgAuthTokenCreated(Event):
+@analytics.eventclass("org_auth_token.created")
+class OrgAuthTokenCreated(analytics.Event):
     user_id: int | None = None
     organization_id: int
 
