eff78e204ad3a25095e454e136eb6a0885338143
nsdeschenes <nicholas.deschenes@sentry.io>
2025-10-02 13:42:03 -0300

    :construction: Migrating to nuqs

--- static/app/components/searchQueryBuilder/hooks.ts
+++ static/app/components/searchQueryBuilder/hooks.ts
@@ -1,23 +1,10 @@
-import {useCallback, useMemo} from 'react';
-
-import {decodeBoolean} from 'sentry/utils/queryString';
-import {useLocation} from 'sentry/utils/useLocation';
-import {useNavigate} from 'sentry/utils/useNavigate';
+import {parseAsBoolean, useQueryState} from 'nuqs';
 
 export function useCaseInsensitivity(): [boolean | undefined, (c: boolean) => void] {
-  const navigate = useNavigate();
-  const location = useLocation();
-
-  const caseInsensitive = useMemo(
-    () => decodeBoolean(location.query.caseInsensitive),
-    [location]
+  const [caseInsensitive, setCaseInsensitive] = useQueryState(
+    'caseInsensitive',
+    parseAsBoolean
   );
 
-  const setCaseInsensitive = useCallback(
-    (c: boolean) =>
-      navigate({...location, query: {...location.query, caseInsensitive: c}}),
-    [location, navigate]
-  );
-
-  return [caseInsensitive, setCaseInsensitive];
+  return [caseInsensitive ?? undefined, setCaseInsensitive];
 }
