d82f1aba7787281ca8e5324e427e41332010e28e
isabellaenriquez <isabella.enriquez@sentry.io>
2025-10-03 13:42:06 -0400

    make const components functions

--- static/gsApp/views/subscriptionPage/headerCards/billingInfoCard.tsx
+++ static/gsApp/views/subscriptionPage/headerCards/billingInfoCard.tsx
@@ -1,4 +1,3 @@
-import {useMemo} from 'react';
 import moment from 'moment-timezone';
 
 import {Container, Flex} from 'sentry/components/core/layout';
@@ -10,6 +9,7 @@ import type {Organization} from 'sentry/types/organization';
 
 import {useBillingDetails} from 'getsentry/hooks/useBillingDetails';
 import type {Subscription} from 'getsentry/types';
+import {hasSomeBillingDetails} from 'getsentry/utils/billing';
 import SubscriptionHeaderCard from 'getsentry/views/subscriptionPage/headerCards/subscriptionHeaderCard';
 
 function BillingInfoCard({
@@ -19,50 +19,6 @@ function BillingInfoCard({
   organization: Organization;
   subscription: Subscription;
 }) {
-  const {data: billingDetails, isLoading} = useBillingDetails();
-  const {paymentSource} = subscription;
-
-  const BillingDetailsInfo = useMemo(() => {
-    if (isLoading) {
-      return (
-        <Flex direction="column" gap="xs">
-          <Placeholder height="16px" />
-          <Placeholder height="16px" />
-          <Placeholder height="16px" />
-          <Placeholder height="16px" />
-        </Flex>
-      );
-    }
-
-    if (!billingDetails) {
-      return (
-        <Container>
-          <Text>{t('No billing details on file')}</Text>
-        </Container>
-      );
-    }
-
-    return (
-      <Flex direction="column" gap="xs">
-        {billingDetails.companyName && (
-          <Text variant="muted" size="sm">
-            {billingDetails.companyName}
-          </Text>
-        )}
-        {billingDetails.billingEmail && (
-          <Text variant="muted" size="sm">
-            {billingDetails.billingEmail}
-          </Text>
-        )}
-        {billingDetails.displayAddress && (
-          <Text variant="muted" size="sm">
-            {billingDetails.displayAddress}
-          </Text>
-        )}
-      </Flex>
-    );
-  }, [billingDetails, isLoading]);
-
   if (
     subscription.isSelfServePartner ||
     (!subscription.canSelfServe && !subscription.onDemandInvoiced)
@@ -70,11 +26,79 @@ function BillingInfoCard({
     return null;
   }
 
+  return (
+    <SubscriptionHeaderCard
+      title={t('Billing information')}
+      icon={<IconUser />}
+      sections={[
+        <BillingDetailsInfo key="billing-details-info" />,
+        <PaymentSourceInfo key="payment-source-info" subscription={subscription} />,
+      ]}
+      button={{
+        ariaLabel: t('Edit billing information'),
+        label: t('Edit billing information'),
+        linkTo: `/settings/${organization.slug}/billing/details/`,
+        icon: <IconSettings />,
+        priority: 'default',
+      }}
+    />
+  );
+}
+
+function BillingDetailsInfo() {
+  const {data: billingDetails, isLoading} = useBillingDetails();
+
+  if (isLoading) {
+    return (
+      <Flex direction="column" gap="xs">
+        <Placeholder height="16px" />
+        <Placeholder height="16px" />
+        <Placeholder height="16px" />
+        <Placeholder height="16px" />
+      </Flex>
+    );
+  }
+
+  if (!billingDetails || !hasSomeBillingDetails(billingDetails)) {
+    return (
+      <Container>
+        <Text variant="muted">{t('No billing details on file')}</Text>
+      </Container>
+    );
+  }
+
+  return (
+    <Flex direction="column" gap="xs">
+      {billingDetails.companyName && (
+        <Text variant="muted" size="sm">
+          {billingDetails.companyName}
+        </Text>
+      )}
+      {billingDetails.billingEmail && (
+        <Text variant="muted" size="sm">
+          {billingDetails.billingEmail}
+        </Text>
+      )}
+      {billingDetails.displayAddress && (
+        <Text variant="muted" size="sm">
+          {billingDetails.displayAddress}
+        </Text>
+      )}
+    </Flex>
+  );
+}
+
+function PaymentSourceInfo({subscription}: {subscription: Subscription}) {
+  const {paymentSource} = subscription;
   const paymentSourceExpiryDate = paymentSource
     ? moment(new Date(paymentSource.expYear, paymentSource.expMonth - 1))
     : null;
 
-  const PaymentSourceInfo = paymentSource ? (
+  if (!paymentSource) {
+    return <Text variant="muted">{t('No payment method on file')}</Text>;
+  }
+
+  return (
     <Flex direction="column" gap="xs">
       <Text>{tct('Card ending in [last4]', {last4: paymentSource.last4})}</Text>
       <Text variant="muted" size="sm">
@@ -84,23 +108,6 @@ function BillingInfoCard({
         })}
       </Text>
     </Flex>
-  ) : (
-    <Text>{t('No payment method on file')}</Text>
-  );
-
-  return (
-    <SubscriptionHeaderCard
-      title={t('Billing information')}
-      icon={<IconUser />}
-      sections={[BillingDetailsInfo, PaymentSourceInfo]}
-      button={{
-        ariaLabel: t('Edit billing information'),
-        label: t('Edit billing information'),
-        linkTo: `/settings/${organization.slug}/billing/details/`,
-        icon: <IconSettings />,
-        priority: 'default',
-      }}
-    />
   );
 }
 
