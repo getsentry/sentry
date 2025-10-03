212cf49b254a0fd0b39b5c9a3cb73d122330ca7e
Fabian Schindler <fabian.schindler@sentry.io>
2025-10-03 12:37:19 +0200

    ref(analytics): Refactor EventManager to extend Registry for event registration
    The EventManager class has been simplified by extending the Registry class, allowing for more efficient event type registration. The previous manual management of event types has been removed in favor of leveraging the Registry's built-in functionality.

--- src/sentry/analytics/event_manager.py
+++ src/sentry/analytics/event_manager.py
@@ -1,24 +1,13 @@
 __all__ = ("default_manager", "EventManager")
 
-from collections.abc import MutableMapping
-from typing import Any
 
 from sentry.analytics.event import Event
+from sentry.utils.registry import Registry
 
 
-class EventManager:
-    def __init__(self) -> None:
-        self._event_types: MutableMapping[Any, type[Event]] = {}
-
+class EventManager(Registry[type[Event]]):
     def register(self, event_cls: type[Event]) -> None:
-        event_type = event_cls.type
-        if event_type in self._event_types:
-            assert self._event_types[event_type] == event_cls
-        else:
-            self._event_types[event_type] = event_cls
-
-    def get(self, type: str) -> type[Event]:
-        return self._event_types[type]
+        return super().register(event_cls.type)(event_cls)
 
 
 default_manager = EventManager()
