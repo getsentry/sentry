e3e2309a7c36658097a9feaaf33a2276426eb6e5
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-03 14:02:34 -0400

    fix useMemo

--- static/app/views/explore/metrics/utils.tsx
+++ static/app/views/explore/metrics/utils.tsx
@@ -1,31 +1,28 @@
 import type {ReactNode} from 'react';
-import {useMemo} from 'react';
 
 import {t} from 'sentry/locale';
 import type {PickableDays} from 'sentry/views/explore/utils';
 
 export function metricsPickableDays(): PickableDays {
-  return useMemo(() => {
-    const maxPickableDays = 30 as const;
-    const defaultPeriod = '7d' as const;
+  const maxPickableDays = 30 as const;
+  const defaultPeriod = '7d' as const;
 
-    const relativeOptions = ({
-      arbitraryOptions,
-    }: {
-      arbitraryOptions: Record<string, ReactNode>;
-    }) => ({
-      ...arbitraryOptions,
-      '1h': t('Last hour'),
-      '24h': t('Last 24 hours'),
-      '7d': t('Last 7 days'),
-      '14d': t('Last 14 days'),
-      '30d': t('Last 30 days'),
-    });
+  const relativeOptions = ({
+    arbitraryOptions,
+  }: {
+    arbitraryOptions: Record<string, ReactNode>;
+  }) => ({
+    ...arbitraryOptions,
+    '1h': t('Last hour'),
+    '24h': t('Last 24 hours'),
+    '7d': t('Last 7 days'),
+    '14d': t('Last 14 days'),
+    '30d': t('Last 30 days'),
+  });
 
-    return {
-      defaultPeriod,
-      maxPickableDays,
-      relativeOptions,
-    };
-  }, []);
+  return {
+    defaultPeriod,
+    maxPickableDays,
+    relativeOptions,
+  };
 }
