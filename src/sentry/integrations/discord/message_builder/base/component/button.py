6af2da428155819cc5d38bfdd8c1c8be49aa411e
Leander Rodrigues <me@leander.xyz>
2025-10-03 18:19:51 -0400

    ♻️ use link buttons on discord renderer

--- src/sentry/integrations/discord/message_builder/base/component/button.py
+++ src/sentry/integrations/discord/message_builder/base/component/button.py
@@ -28,13 +28,11 @@ class DiscordButton(DiscordMessageComponent):
         custom_id: str,
         style: int = DiscordButtonStyle.SECONDARY,
         label: str | None = None,
-        url: str | None = None,
         disabled: bool = False,
     ) -> None:
-        self.style = style
+        self.style = 5
         self.custom_id = custom_id
         self.label = label
-        self.url = url
         self.disabled = disabled
         super().__init__(type=2)
 
@@ -43,8 +41,38 @@ class DiscordButton(DiscordMessageComponent):
 
         if self.label is not None:
             button["label"] = self.label
-        if self.url is not None:
-            button["url"] = self.url
+        if self.disabled is not None:
+            button["disabled"] = self.disabled
+
+        return button
+
+
+class DiscordLinkButtonDict(DiscordMessageComponentDict):
+    style: int
+    url: str
+    label: NotRequired[str]
+    disabled: NotRequired[bool]
+
+
+class DiscordLinkButton(DiscordMessageComponent):
+    style = DiscordButtonStyle.LINK
+
+    def __init__(
+        self,
+        url: str,
+        label: str | None = None,
+        disabled: bool = False,
+    ) -> None:
+        self.url = url
+        self.label = label
+        self.disabled = disabled
+        super().__init__(type=2)
+
+    def build(self) -> DiscordLinkButtonDict:
+        button = DiscordLinkButtonDict(type=self.type, url=self.url, style=self.style)
+
+        if self.label is not None:
+            button["label"] = self.label
         if self.disabled is not None:
             button["disabled"] = self.disabled
 
