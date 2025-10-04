4404fe95d6a4f3816cc5d746c46106906096c313
buddhsen-tripathi <buddhsent19@gmail.com>
2025-10-03 20:45:11 -0400

    Add proper empty state to CommitYamlPage

--- static/app/views/prevent/coverage/commits/commitYaml.tsx
+++ static/app/views/prevent/coverage/commits/commitYaml.tsx
@@ -1,16 +1,18 @@
 import styled from '@emotion/styled';
 
+import EmptyStateWarning from 'sentry/components/emptyStateWarning';
+import {t} from 'sentry/locale';
+
 import {space} from 'sentry/styles/space';
+
+import {LayoutGap} from '../layout';
 
 export default function CommitYamlPage() {
   return (
     <LayoutGap>
-      <p>Commit YAML Page</p>
+      <EmptyStateWarning>
+        <p>{t('Commit YAML configuration')}</p>
+      </EmptyStateWarning>
     </LayoutGap>
   );
 }
-
-const LayoutGap = styled('div')`
-  display: grid;
-  gap: ${space(2)};
-`;
