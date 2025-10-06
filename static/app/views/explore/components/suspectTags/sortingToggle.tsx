147e879b27f5013ffc79a14e38687669bdb1fd55
Aayush Seth <aayush.seth@sentry.io>
2025-10-06 11:47:51 -0700

    chore(comparative workflows): Change default sort (#100994)
    - change default sorted tab to rrr

--- static/app/views/explore/components/suspectTags/sortingToggle.tsx
+++ static/app/views/explore/components/suspectTags/sortingToggle.tsx
@@ -1,7 +1,7 @@
 import {SegmentedControl} from 'sentry/components/core/segmentedControl';
 import {t} from 'sentry/locale';
 
-type SortingMethod = 'rrf' | 'rrr';
+type SortingMethod = 'rrr' | 'rrf';
 
 interface SortingToggleProps {
   onChange: (value: SortingMethod) => void;
@@ -12,20 +12,20 @@ export function SortingToggle({value, onChange}: SortingToggleProps) {
   return (
     <SegmentedControl value={value} onChange={onChange} size="sm">
       <SegmentedControl.Item
-        key="rrf"
-        tooltip={t(
-          'Reciprocal Rank Fusion (RRF) - Combines multiple ranking signals to identify the most suspicious attributes'
-        )}
-      >
-        {t('RRF')}
-      </SegmentedControl.Item>
-      <SegmentedControl.Item
         key="rrr"
         tooltip={t(
           'Relative Risk Ratio (RRR) - Compares the risk of attributes between selected and baseline data and favors lower cardinality attributes'
         )}
       >
         {t('RRR')}
+      </SegmentedControl.Item>
+      <SegmentedControl.Item
+        key="rrf"
+        tooltip={t(
+          'Reciprocal Rank Fusion (RRF) - Combines multiple ranking signals to identify the most suspicious attributes. WARNING: This method is not finetuned so results may vary.'
+        )}
+      >
+        {t('RRF')}
       </SegmentedControl.Item>
     </SegmentedControl>
   );
