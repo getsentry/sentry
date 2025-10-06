d3b83df6fa9cc7a007799447f1525e95ca33614e
Shashank Jarmale <shashank.jarmale@sentry.io>
2025-10-06 10:28:59 -0700

    ref(settings): Remove usage of `deprecatedRouteProps` from `OrganizationTeams` settings (#100846)
    Migrates usage of `deprecatedRouteProps` for `OrganizationTeams` -
    `sentry/views/settings/organizationTeams`.
    
    
    https://www.notion.so/sentry/Frontend-TSC-Project-Remove-all-uses-of-deprecatedRouteProps-true-26a8b10e4b5d8015a6a2dd14f9d41dd7

--- static/app/routes.tsx
+++ static/app/routes.tsx
@@ -982,7 +982,6 @@ function buildRoutes(): RouteObject[] {
         {
           index: true,
           component: make(() => import('sentry/views/settings/organizationTeams')),
-          deprecatedRouteProps: true,
         },
         {
           path: ':teamId/',
