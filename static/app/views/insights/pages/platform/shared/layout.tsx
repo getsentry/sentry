22cff389267076458fb6902af7b9d259dc9c3d3d
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2025-10-06 15:39:28 +0200

    fix(ui): always render tabs in insights/backend to avoid flash during first navigation (#100950)
    follow-up to:
    
    - #100915 
    
    to get the same experience to `backend` routes

--- static/app/views/insights/pages/platform/shared/layout.tsx
+++ static/app/views/insights/pages/platform/shared/layout.tsx
@@ -14,8 +14,6 @@ import * as ModuleLayout from 'sentry/views/insights/common/components/moduleLay
 import {InsightsProjectSelector} from 'sentry/views/insights/common/components/projectSelector';
 import {ToolRibbon} from 'sentry/views/insights/common/components/ribbon';
 import {useOnboardingProject} from 'sentry/views/insights/common/queries/useOnboardingProject';
-import {BackendHeader} from 'sentry/views/insights/pages/backend/backendPageHeader';
-import {BACKEND_LANDING_TITLE} from 'sentry/views/insights/pages/backend/settings';
 import {useTransactionNameQuery} from 'sentry/views/insights/pages/platform/shared/useTransactionNameQuery';
 import {LegacyOnboarding} from 'sentry/views/performance/onboarding';
 import {getTransactionSearchQuery} from 'sentry/views/performance/utils';
@@ -34,13 +32,7 @@ function getFreeTextFromQuery(query: string) {
   return '';
 }
 
-export function PlatformLandingPageLayout({
-  children,
-  performanceType,
-}: {
-  children: React.ReactNode;
-  performanceType: 'backend' | 'frontend';
-}) {
+export function PlatformLandingPageLayout({children}: {children: React.ReactNode}) {
   const location = useLocation();
   const organization = useOrganization();
   const onboardingProject = useOnboardingProject();
@@ -56,9 +48,6 @@ export function PlatformLandingPageLayout({
       organization={organization}
       renderDisabled={NoAccess}
     >
-      {performanceType === 'backend' ? (
-        <BackendHeader headerTitle={BACKEND_LANDING_TITLE} />
-      ) : null}
       <Layout.Body>
         <Layout.Main fullWidth>
           <ModuleLayout.Layout>
