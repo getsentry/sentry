b23d7bca6006a5373797c75c6467ad6576c7a339
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-06 11:37:43 -0400

    feat(tracemetrics): Add stub UI for chart and related info (#100961)
    Stubs out some UI from the mockups.
    - Adds a chart in a split view with a tabbed section on the right
    - I went with a resizable panel because I thought it might be nice. We
    can totally get rid of it and do static sections if we want.
    - The charts currently do spans requests with `useSortedTimeSeries`, the
    data can be swapped out at a later date
    - The dropdowns are also hardcoded at the moment and can be swapped out
    after as well.
    - Can add widgets, but they're stored in state right now and I don't
    have delete functionality at the moment
    - Chart type is mocked out with state, but should be obtained through a
    hook with the query params context
    
    Follow up:
    - dynamically load options for each of the selection fields
    - make the chart query metrics
    - render the relevant information in the tabbed info sections
    - sync chart hover

--- static/app/views/explore/metrics/metricsTab.tsx
+++ static/app/views/explore/metrics/metricsTab.tsx
@@ -1,9 +1,13 @@
-import {Fragment, useMemo} from 'react';
+import {Fragment, useState} from 'react';
+import styled from '@emotion/styled';
 
+import {Button} from 'sentry/components/core/button';
+import {Flex} from 'sentry/components/core/layout';
 import * as Layout from 'sentry/components/layouts/thirds';
 import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import {EnvironmentPageFilter} from 'sentry/components/organizations/environmentPageFilter';
 import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilter';
+import {IconAdd} from 'sentry/icons';
 import {t} from 'sentry/locale';
 import {
   BottomSectionBody,
@@ -11,7 +15,7 @@ import {
   StyledPageFilterBar,
   TopSectionBody,
 } from 'sentry/views/explore/logs/styles';
-import {MetricRow} from 'sentry/views/explore/metrics/metricRow';
+import {MetricPanel} from 'sentry/views/explore/metrics/metricPanel';
 import {MetricsQueryParamsProvider} from 'sentry/views/explore/metrics/metricsQueryParams';
 import {type TraceMetric} from 'sentry/views/explore/metrics/traceMetric';
 import type {PickableDays} from 'sentry/views/explore/utils';
@@ -61,20 +65,44 @@ function MetricsTabFilterSection({
 }
 
 function MetricsTabBodySection() {
-  const traceMetrics: TraceMetric[] = useMemo(() => {
-    return [{name: 'myfirstmetric'}, {name: 'mysecondmetric'}];
-  }, []);
+  const [traceMetrics, setTraceMetrics] = useState<TraceMetric[]>([
+    {name: 'myfirstmetric'},
+    {name: 'mysecondmetric'},
+  ]);
 
   return (
     <BottomSectionBody>
-      {traceMetrics.map((traceMetric, index) => {
-        return (
-          // TODO: figure out a better `key`
-          <MetricsQueryParamsProvider key={index}>
-            <MetricRow traceMetric={traceMetric} />
-          </MetricsQueryParamsProvider>
-        );
-      })}
+      <Flex direction="column" gap="lg">
+        {traceMetrics.map((traceMetric, index) => {
+          return (
+            // TODO: figure out a better `key`
+            <MetricsQueryParamsProvider key={index}>
+              <MetricPanel traceMetric={traceMetric} />
+            </MetricsQueryParamsProvider>
+          );
+        })}
+        <AddMetricButtonContainer>
+          <Button
+            size="sm"
+            priority="default"
+            icon={<IconAdd />}
+            onClick={() => {
+              setTraceMetrics([
+                ...traceMetrics,
+                {name: `metric${traceMetrics.length + 1}`},
+              ]);
+            }}
+          >
+            {t('Add Metric')}
+          </Button>
+        </AddMetricButtonContainer>
+      </Flex>
     </BottomSectionBody>
   );
 }
+
+const AddMetricButtonContainer = styled('div')`
+  button {
+    width: 100%;
+  }
+`;
