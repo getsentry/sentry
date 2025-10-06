a1d4513e2162b2ceeb42c06f5652375aa10e4d6d
Tony Xiao <txiao@sentry.io>
2025-10-06 09:19:34 -0400

    feat(metrics): Stubs for query params in explore metrics (#100920)
    Some basic stubs for the query params to be used in explore metrics.
    Currently using react state but can easily be swapped out later.
    
    ---------
    
    Co-authored-by: Nar Saynorath <nar.saynorath@sentry.io>

--- static/app/views/explore/metrics/metricsTab.tsx
+++ static/app/views/explore/metrics/metricsTab.tsx
@@ -1,17 +1,19 @@
+import {Fragment, useMemo} from 'react';
+
 import * as Layout from 'sentry/components/layouts/thirds';
 import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import {EnvironmentPageFilter} from 'sentry/components/organizations/environmentPageFilter';
 import {ProjectPageFilter} from 'sentry/components/organizations/projectPageFilter';
-import {SearchQueryBuilderProvider} from 'sentry/components/searchQueryBuilder/context';
 import {t} from 'sentry/locale';
-import {useSearchQueryBuilderProps} from 'sentry/views/explore/components/traceItemSearchQueryBuilder';
 import {
   BottomSectionBody,
   FilterBarContainer,
   StyledPageFilterBar,
   TopSectionBody,
 } from 'sentry/views/explore/logs/styles';
-import {TraceItemDataset} from 'sentry/views/explore/types';
+import {MetricRow} from 'sentry/views/explore/metrics/metricRow';
+import {MetricsQueryParamsProvider} from 'sentry/views/explore/metrics/metricsQueryParams';
+import {type TraceMetric} from 'sentry/views/explore/metrics/traceMetric';
 import type {PickableDays} from 'sentry/views/explore/utils';
 
 type LogsTabProps = PickableDays;
@@ -21,34 +23,58 @@ export function MetricsTabContent({
   maxPickableDays,
   relativeOptions,
 }: LogsTabProps) {
-  const searchQueryBuilderProviderProps = useSearchQueryBuilderProps({
-    itemType: TraceItemDataset.TRACEMETRICS,
-    numberAttributes: {},
-    stringAttributes: {},
-    numberSecondaryAliases: {},
-    stringSecondaryAliases: {},
-    initialQuery: '',
-    searchSource: 'ourmetrics',
-  });
   return (
-    <SearchQueryBuilderProvider {...searchQueryBuilderProviderProps}>
-      <TopSectionBody noRowGap>
-        <Layout.Main fullWidth>
-          <FilterBarContainer>
-            <StyledPageFilterBar condensed>
-              <ProjectPageFilter />
-              <EnvironmentPageFilter />
-              <DatePageFilter
-                defaultPeriod={defaultPeriod}
-                maxPickableDays={maxPickableDays}
-                relativeOptions={relativeOptions}
-                searchPlaceholder={t('Custom range: 2h, 4d, 3w')}
-              />
-            </StyledPageFilterBar>
-          </FilterBarContainer>
-        </Layout.Main>
-      </TopSectionBody>
-      <BottomSectionBody sidebarOpen={false}>Currently in development</BottomSectionBody>
-    </SearchQueryBuilderProvider>
+    <Fragment>
+      <MetricsTabFilterSection
+        defaultPeriod={defaultPeriod}
+        maxPickableDays={maxPickableDays}
+        relativeOptions={relativeOptions}
+      />
+      <MetricsTabBodySection />
+    </Fragment>
+  );
+}
+
+function MetricsTabFilterSection({
+  defaultPeriod,
+  maxPickableDays,
+  relativeOptions,
+}: PickableDays) {
+  return (
+    <TopSectionBody noRowGap>
+      <Layout.Main fullWidth>
+        <FilterBarContainer>
+          <StyledPageFilterBar condensed>
+            <ProjectPageFilter />
+            <EnvironmentPageFilter />
+            <DatePageFilter
+              defaultPeriod={defaultPeriod}
+              maxPickableDays={maxPickableDays}
+              relativeOptions={relativeOptions}
+              searchPlaceholder={t('Custom range: 2h, 4d, 3w')}
+            />
+          </StyledPageFilterBar>
+        </FilterBarContainer>
+      </Layout.Main>
+    </TopSectionBody>
+  );
+}
+
+function MetricsTabBodySection() {
+  const traceMetrics: TraceMetric[] = useMemo(() => {
+    return [{name: 'myfirstmetric'}, {name: 'mysecondmetric'}];
+  }, []);
+
+  return (
+    <BottomSectionBody>
+      {traceMetrics.map((traceMetric, index) => {
+        return (
+          // TODO: figure out a better `key`
+          <MetricsQueryParamsProvider key={index}>
+            <MetricRow traceMetric={traceMetric} />
+          </MetricsQueryParamsProvider>
+        );
+      })}
+    </BottomSectionBody>
   );
 }
