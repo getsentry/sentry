e04301d5867aa8322cde7e203f456dd33df64b66
Suejung Shin <suejung.shin@sentry.io>
2025-10-05 22:11:12 -0700

    fix tests

--- static/app/views/prevent/preventAI/hooks/usePreventAIOrgRepos.tsx
+++ static/app/views/prevent/preventAI/hooks/usePreventAIOrgRepos.tsx
@@ -1,24 +1,22 @@
-import {useCallback} from 'react';
-
 import type {PreventAIOrg} from 'sentry/types/prevent';
 import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
 
-interface PreventAIOrgReposResponse {
+export interface PreventAIOrgReposResponse {
   orgRepos: PreventAIOrg[];
 }
 
 interface PreventAIOrgsReposResult {
   data: PreventAIOrgReposResponse | undefined;
   isError: boolean;
-  isLoading: boolean;
+  isPending: boolean;
   refetch: () => void;
 }
 
 export function usePreventAIOrgRepos(): PreventAIOrgsReposResult {
   const organization = useOrganization();
 
-  const {data, isLoading, isError, refetch} = useApiQuery<PreventAIOrgReposResponse>(
+  const {data, isPending, isError, refetch} = useApiQuery<PreventAIOrgReposResponse>(
     [`/organizations/${organization.slug}/prevent/github/repos/`],
     {
       staleTime: 0,
@@ -28,8 +26,8 @@ export function usePreventAIOrgRepos(): PreventAIOrgsReposResult {
 
   return {
     data,
-    isLoading,
+    isPending,
     isError,
-    refetch: useCallback(() => refetch(), [refetch]),
+    refetch,
   };
 }
