956ef25f1620edc3243e8b9d15a000ac780cfe46
Vjeran Grozdanić <vjeran.grozdanic@sentry.io>
2025-10-06 13:03:25 +0200

    fix(demo-mode): Treat demo user as unauthenticated user in auth pipelines (#100667)
    In the auth pipelines, which are activated when user clicks on register
    or sign in - we have to treat demo user as unauthenticated user,
    otherwise we will try to link the accounts wrongly.

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3511,3 +3511,12 @@ register(
     default=[],
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
+
+# Killswitch for treating demo user as unauthenticated
+# in our auth pipelines.
+register(
+    "demo-user.auth.pipelines.always.unauthenticated.enabled",
+    type=Bool,
+    default=False,
+    flags=FLAG_PRIORITIZE_DISK | FLAG_AUTOMATOR_MODIFIABLE,
+)
