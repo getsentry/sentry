7542869c9ef4de69be7fbb8ada6ac36ea3e34dff
Scott Cooper <scttcper@gmail.com>
2025-10-06 09:47:50 -0700

    feat(issues): Remove router props from issue list (#100903)
    Removes the deprecated router props and their corresponding types from
    the issues list. They were mostly already unused.
    
    part of https://github.com/getsentry/frontend-tsc/issues/93

--- static/app/views/issueList/pages/errorsOutages.tsx
+++ static/app/views/issueList/pages/errorsOutages.tsx
@@ -1,19 +1,16 @@
 import NoProjectMessage from 'sentry/components/noProjectMessage';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import Redirect from 'sentry/components/redirect';
-import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import useOrganization from 'sentry/utils/useOrganization';
 import IssueListContainer from 'sentry/views/issueList';
 import {MonitorsDropdown} from 'sentry/views/issueList/monitorsDropdown';
 import IssueListOverview from 'sentry/views/issueList/overview';
 import {ISSUE_TAXONOMY_CONFIG, IssueTaxonomy} from 'sentry/views/issueList/taxonomies';
 
-type Props = RouteComponentProps;
-
 const CONFIG = ISSUE_TAXONOMY_CONFIG[IssueTaxonomy.ERRORS_AND_OUTAGES];
 const QUERY = `is:unresolved issue.category:[${CONFIG.categories.join(',')}]`;
 
-export default function ErrorsOutagesPage(props: Props) {
+export default function ErrorsOutagesPage() {
   const organization = useOrganization();
 
   const hasIssueTaxonomy = organization.features.includes('issue-taxonomy');
@@ -26,7 +23,6 @@ export default function ErrorsOutagesPage(props: Props) {
       <PageFiltersContainer>
         <NoProjectMessage organization={organization}>
           <IssueListOverview
-            {...props}
             initialQuery={QUERY}
             title={CONFIG.label}
             titleDescription={CONFIG.description}
--- static/app/views/issueList/pages/warnings.tsx
+++ static/app/views/issueList/pages/warnings.tsx
@@ -1,18 +1,15 @@
 import NoProjectMessage from 'sentry/components/noProjectMessage';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import Redirect from 'sentry/components/redirect';
-import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
 import useOrganization from 'sentry/utils/useOrganization';
 import IssueListContainer from 'sentry/views/issueList';
 import IssueListOverview from 'sentry/views/issueList/overview';
 import {ISSUE_TAXONOMY_CONFIG, IssueTaxonomy} from 'sentry/views/issueList/taxonomies';
 
-type Props = RouteComponentProps;
-
 const CONFIG = ISSUE_TAXONOMY_CONFIG[IssueTaxonomy.WARNINGS];
 const QUERY = `is:unresolved issue.category:[${CONFIG.categories.join(',')}]`;
 
-export default function WarningsPage(props: Props) {
+export default function WarningsPage() {
   const organization = useOrganization();
   const hasIssueTaxonomy = organization.features.includes('issue-taxonomy');
   if (!hasIssueTaxonomy) {
@@ -24,7 +21,6 @@ export default function WarningsPage(props: Props) {
       <PageFiltersContainer>
         <NoProjectMessage organization={organization}>
           <IssueListOverview
-            {...props}
             initialQuery={QUERY}
             title={CONFIG.label}
             titleDescription={CONFIG.description}
