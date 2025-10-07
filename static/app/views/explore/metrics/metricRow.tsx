fde39db90442c515575bb4d2fd8b0c0b3ad0010c
Tony Xiao <txiao@sentry.io>
2025-10-07 17:07:48 -0400

    feat(metrics): Encode metric query into url (#101108)
    Previously, the queries was stored via react state. This uses the url to
    persist the queries instead.
    
    Closes LOGS-382

--- static/app/views/explore/metrics/metricRow.tsx
+++ static/app/views/explore/metrics/metricRow.tsx
@@ -9,8 +9,8 @@ import {
   useSearchQueryBuilderProps,
   type TraceItemSearchQueryBuilderProps,
 } from 'sentry/views/explore/components/traceItemSearchQueryBuilder';
+import {type TraceMetric} from 'sentry/views/explore/metrics/metricQuery';
 import {useMetricVisualize} from 'sentry/views/explore/metrics/metricsQueryParams';
-import {type TraceMetric} from 'sentry/views/explore/metrics/traceMetric';
 import {
   useQueryParamsGroupBys,
   useQueryParamsQuery,
