029620e454ac46e2c576531492b9e00be2d94822
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-07 16:09:16 -0400

    remove extra file

--- src/sentry/search/eap/tracemetrics/aggregates.py
+++
@@ -1,5 +0,0 @@
-from sentry.search.eap.ourlogs.aggregates import LOG_AGGREGATE_DEFINITIONS
-
-# For now, trace metrics uses the same aggregates as logs
-# This can be extended in the future with trace metrics specific aggregates
-TRACEMETRICS_AGGREGATE_DEFINITIONS = LOG_AGGREGATE_DEFINITIONS
