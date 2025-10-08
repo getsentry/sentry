36eacbc9e7302581c5bb9e8677f4b83aa142837d
Jonas <jonas.badalic@sentry.io>
2025-10-08 06:28:32 -0700

    core(image) add core image component (#100844)
    Adds a core Image component to the codebase. The main win here is that
    we want to defer image loading and default to lazy loaded images +
    require alt texts to be added to each image.

---
+++ static/app/components/core/image/image.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+
+interface ImageProps extends React.ImgHTMLAttributes<HTMLImageElement> {
+  alt: string;
+  src: string;
+  height?: string;
+  /**
+   * Determines if the image should be loaded eagerly or lazily.
+   * @default 'lazy'
+   */
+  loading?: 'eager' | 'lazy';
+  objectFit?: 'contain' | 'cover';
+  objectPosition?: 'center' | 'top' | 'bottom' | 'left' | 'right' | (string & {});
+  ref?: React.Ref<HTMLImageElement>;
+  width?: string;
+}
+
+export function Image({loading, ...props}: ImageProps) {
+  return <Img loading={loading ?? 'lazy'} {...props} />;
+}
+
+const Img = styled('img')<ImageProps>`
+  width: ${p => p.width ?? '100%'};
+  height: ${p => p.height ?? 'auto'};
+  object-fit: ${p => p.objectFit};
+  object-position: ${p => p.objectPosition};
+`;
