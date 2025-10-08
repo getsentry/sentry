a73156c6b3301a0dc1ba13ee1d3f3b53eb5a5bda
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-10-08 18:27:03 -0400

    feat(workflow_engine): Add useSetAutomaticName hook for detector forms
    Introduces a new useSetAutomaticName hook that enables automatic name
    generation for detector forms based on field values. This hook uses
    mobx autorun to reactively watch form fields and automatically set the
    detector name until the user manually edits it.
    
    Key changes:
    - Created useSetAutomaticName hook in detector forms common directory
    - Extended DetectorFormContext to track hasSetDetectorName state and
      optionally include the detector being edited
    - Updated DetectorBaseFields to set hasSetDetectorName when user edits
      the name field
    - Integrated automatic naming into uptime detector form to generate
      names from URL field (e.g., "Uptime check for example.com/path")
    - Uses URL.parse() for safe URL parsing without try-catch
    
    The hook automatically stops name generation when:
    - User manually edits the detector name
    - Editing an existing detector (detector prop passed to context)
    
    This pattern can be easily extended to other detector types by calling
    useSetAutomaticName with a custom name generation function.
    
    Fixes NEW-556

--- static/app/views/detectors/components/forms/uptime/index.tsx
+++ static/app/views/detectors/components/forms/uptime/index.tsx
@@ -1,8 +1,10 @@
 import styled from '@emotion/styled';
 
+import {t} from 'sentry/locale';
 import type {UptimeDetector} from 'sentry/types/workflowEngine/detectors';
 import {AutomateSection} from 'sentry/views/detectors/components/forms/automateSection';
 import {AssignSection} from 'sentry/views/detectors/components/forms/common/assignSection';
+import {useSetAutomaticName} from 'sentry/views/detectors/components/forms/common/useSetAutomaticName';
 import {EditDetectorLayout} from 'sentry/views/detectors/components/forms/editDetectorLayout';
 import {NewDetectorLayout} from 'sentry/views/detectors/components/forms/newDetectorLayout';
 import {UptimeDetectorFormDetectSection} from 'sentry/views/detectors/components/forms/uptime/detect';
@@ -14,6 +16,24 @@ import {UptimeRegionWarning} from 'sentry/views/detectors/components/forms/uptim
 import {UptimeDetectorResolveSection} from 'sentry/views/detectors/components/forms/uptime/resolve';
 
 function UptimeDetectorForm() {
+  useSetAutomaticName(form => {
+    const url = form.getValue('url');
+
+    if (typeof url !== 'string') {
+      return null;
+    }
+
+    const parsedUrl = URL.parse(url);
+    if (!parsedUrl) {
+      return null;
+    }
+
+    const path = parsedUrl.pathname === '/' ? '' : parsedUrl.pathname;
+    const urlName = `${parsedUrl.hostname}${path}`.replace(/\/$/, '');
+
+    return t('Uptime check for %s', urlName);
+  });
+
   return (
     <FormStack>
       <UptimeRegionWarning />
