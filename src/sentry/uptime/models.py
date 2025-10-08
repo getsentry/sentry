4ef730970f8484114bac51795def5402e592475c
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-10-08 14:23:28 -0400

    feat(uptime): Remove uptime_status{,_update_date} (#101168)

--- src/sentry/uptime/models.py
+++ src/sentry/uptime/models.py
@@ -5,7 +5,6 @@ from typing import ClassVar, Literal, Self, cast, override
 
 from django.db import models
 from django.db.models import Count
-from django.db.models.functions import Now
 
 from sentry.backup.scopes import RelocationScope
 from sentry.constants import ObjectStatus
@@ -93,11 +92,6 @@ class UptimeSubscription(BaseRemoteSubscription, DefaultFieldsModelExisting):
     # How to sample traces for this monitor. Note that we always send a trace_id, so any errors will
     # be associated, this just controls the span sampling.
     trace_sampling = models.BooleanField(default=False, db_default=False)
-    # Tracks the current status of this subscription. This is possibly going
-    # to be replaced in the future with open-periods
-    uptime_status = models.PositiveSmallIntegerField(db_default=UptimeStatus.OK.value)
-    # (Likely) temporary column to keep track of the current uptime status of this monitor
-    uptime_status_update_date = models.DateTimeField(db_default=Now())
 
     objects: ClassVar[BaseManager[Self]] = BaseManager(
         cache_fields=["pk", "subscription_id"],
@@ -110,7 +104,6 @@ class UptimeSubscription(BaseRemoteSubscription, DefaultFieldsModelExisting):
 
         indexes = [
             models.Index(fields=("url_domain_suffix", "url_domain")),
-            models.Index(fields=("uptime_status", "uptime_status_update_date")),
         ]
 
 
