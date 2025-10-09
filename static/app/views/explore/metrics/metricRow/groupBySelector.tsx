67e653e253196ddf93b5045facec6b349fd76794
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-09 14:55:28 -0400

    Add MutableSearch for filter

--- static/app/views/explore/metrics/metricRow/groupBySelector.tsx
+++ static/app/views/explore/metrics/metricRow/groupBySelector.tsx
@@ -1,6 +1,7 @@
 import {CompactSelect} from 'sentry/components/core/compactSelect';
 import type {SelectOption} from 'sentry/components/core/compactSelect/types';
 import LoadingIndicator from 'sentry/components/loadingIndicator';
+import {MutableSearch} from 'sentry/components/searchSyntax/mutableSearch';
 import {useGroupByFields} from 'sentry/views/explore/hooks/useGroupByFields';
 import {useTraceItemAttributeKeys} from 'sentry/views/explore/hooks/useTraceItemAttributeKeys';
 import {
@@ -25,7 +26,9 @@ export function GroupBySelector({metricName}: GroupBySelectorProps) {
   const groupBys = useQueryParamsGroupBys();
   const setGroupBys = useSetQueryParamsGroupBys();
 
-  const metricNameFilter = metricName ? `metric.name:"${metricName}"` : undefined;
+  const metricNameFilter = metricName
+    ? MutableSearch.fromQueryObject({['metric.name']: [metricName]}).formatString()
+    : undefined;
 
   const {attributes: numberTags, isLoading: numberTagsLoading} =
     useTraceItemAttributeKeys({
