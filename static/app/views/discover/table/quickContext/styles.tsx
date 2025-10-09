77a9eb1d1c705ce093c3ee9f717aee5c90f71826
JonasBa <jonas@badalic.com>
2025-10-09 09:55:34 -0700

    core(flex): migrate quickContext/styles.tsx to use Flex/Stack

--- static/app/views/discover/table/quickContext/styles.tsx
+++ static/app/views/discover/table/quickContext/styles.tsx
@@ -1,15 +1,12 @@
 import styled from '@emotion/styled';
 
+import {Flex} from 'sentry/components/core/layout/flex';
+import {Stack} from 'sentry/components/core/layout/stack';
 import {space} from 'sentry/styles/space';
 
-export const ContextContainer = styled('div')`
-  display: flex;
-  flex-direction: column;
-`;
+export const ContextContainer = Stack;
 
-export const ContextHeader = styled('div')`
-  display: flex;
-  align-items: center;
+export const ContextHeader = styled(Flex).attrs({align: 'center'})`
   margin-bottom: ${space(0.75)};
 `;
 
@@ -46,7 +43,4 @@ export const NoContextWrapper = styled('div')`
   min-width: 320px;
 `;
 
-export const ContextRow = styled('div')`
-  display: flex;
-  justify-content: space-between;
-`;
+export const ContextRow = styled(Flex).attrs({justify: 'between'})``;
