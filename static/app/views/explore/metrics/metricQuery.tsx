21051a4c4b72dd3bec8e18714faac9d55d343bff
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-09 16:10:02 -0400

    feat(tracemetrics): Improve metric selection and rendering

--- static/app/views/explore/metrics/metricQuery.tsx
+++ static/app/views/explore/metrics/metricQuery.tsx
@@ -14,6 +14,7 @@ import {ChartType} from 'sentry/views/insights/common/components/chart';
 
 export interface TraceMetric {
   name: string;
+  type: string;
 }
 
 export interface BaseMetricQuery {
@@ -22,8 +23,8 @@ export interface BaseMetricQuery {
 }
 
 export interface MetricQuery extends BaseMetricQuery {
-  setMetricName: (metricName: string) => void;
   setQueryParams: (queryParams: ReadableQueryParams) => void;
+  setTraceMetric: (traceMetric: TraceMetric) => void;
 }
 
 export function decodeMetricsQueryParams(value: string): BaseMetricQuery | null {
@@ -35,7 +36,7 @@ export function decodeMetricsQueryParams(value: string): BaseMetricQuery | null 
   }
 
   const metric = json.metric;
-  if (typeof metric !== 'string') {
+  if (typeof metric !== 'object') {
     return null;
   }
 
@@ -62,7 +63,7 @@ export function decodeMetricsQueryParams(value: string): BaseMetricQuery | null 
   const aggregateFields = [...visualizes, ...groupBys];
 
   return {
-    metric: {name: metric},
+    metric,
     queryParams: new ReadableQueryParams({
       extrapolate: true,
       mode: Mode.AGGREGATE,
@@ -81,7 +82,7 @@ export function decodeMetricsQueryParams(value: string): BaseMetricQuery | null 
 
 export function encodeMetricQueryParams(metricQuery: BaseMetricQuery): string {
   return JSON.stringify({
-    metric: metricQuery.metric.name,
+    metric: metricQuery.metric,
     query: metricQuery.queryParams.query,
     aggregateFields: metricQuery.queryParams.aggregateFields.map(field => {
       if (isVisualize(field)) {
@@ -96,7 +97,7 @@ export function encodeMetricQueryParams(metricQuery: BaseMetricQuery): string {
 
 export function defaultMetricQuery(): BaseMetricQuery {
   return {
-    metric: {name: ''},
+    metric: {name: '', type: ''},
     queryParams: new ReadableQueryParams({
       extrapolate: true,
       mode: Mode.AGGREGATE,
