6289737446bd751432113bea6b44498e32c3e680
nsdeschenes <nicholas.deschenes@sentry.io>
2025-10-09 12:58:50 -0300

    :fire: Remove this eslint rule as it's been already removed in default branch

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -805,7 +805,6 @@ export default typescript.config([
     name: 'files/sentry-test',
     files: ['**/*.spec.{ts,js,tsx,jsx}', 'tests/js/**/*.{ts,js,tsx,jsx}'],
     rules: {
-      '@typescript-eslint/await-thenable': 'off', // For awaiting act(...)
       'no-loss-of-precision': 'off', // Sometimes we have wild numbers hard-coded in tests
       'no-restricted-imports': [
         'error',
