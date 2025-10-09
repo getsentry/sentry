0e1f15afbf935d7c79095ad65e35b1913c95a369
William Mak <william@wmak.io>
2025-10-09 16:16:53 -0400

    feat(timeseries): version of issues-metrics matching timeseries (#100138)
    - Adds a version of issue-metrics that matches the events-timeseries
    endpoint response
    - Updates the params so they match the events-timeseries endpoint closer
    - Adds the ability to group by release optionally, which means it can be
    applied to count(new_issues) or count(resolved_issues) as needed
    - Which does mean its lost the ability to return both counts and a group
    by release

--- src/sentry/issues/endpoints/organization_issue_timeseries.py
+++ src/sentry/issues/endpoints/organization_issue_timeseries.py
@@ -2,8 +2,7 @@ import collections
 from collections.abc import Iterator
 from datetime import datetime, timedelta
 from heapq import nlargest
-from itertools import chain
-from typing import TypedDict
+from typing import Any
 
 from django.db.models import Count, DateTimeField, F, Func, Q
 from django.db.models.functions import Extract
@@ -15,16 +14,24 @@ from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases.organization import OrganizationEndpoint
+from sentry.api.endpoints.timeseries import GroupBy, Row, SeriesMeta, TimeSeries
 from sentry.api.helpers.environments import get_environments
 from sentry.api.utils import get_date_range_from_params
 from sentry.issues.grouptype import FeedbackGroup
 from sentry.models.group import Group, GroupStatus
 from sentry.models.organization import Organization
 from sentry.models.project import Project
+from sentry.snuba.discover import create_groupby_dict, create_result_key
+from sentry.utils.dates import get_rollup_from_request
+
+GROUPBY_TRANSLATION = {
+    "release": "first_release__version",
+}
+INVERSE_GROUPBY_TRANSLATION = {value: key for key, value in GROUPBY_TRANSLATION.items()}
 
 
 @region_silo_endpoint
-class OrganizationIssueMetricsEndpoint(OrganizationEndpoint):
+class OrganizationIssueTimeSeriesEndpoint(OrganizationEndpoint):
     owner = ApiOwner.REPLAY
     publish_status = {"GET": ApiPublishStatus.PRIVATE}
 
@@ -38,26 +45,16 @@ class OrganizationIssueMetricsEndpoint(OrganizationEndpoint):
         if issue_category not in ["issue", "feedback"]:
             raise ParseError("Invalid issue category. Valid options are 'issue' and 'feedback'.")
 
+        raw_groupby = request.GET.getlist("groupBy")
+        if len(raw_groupby) > 0 and raw_groupby != ["release"]:
+            raise ParseError("The only supported groupBy is currently release.")
+        groupby = [GROUPBY_TRANSLATION[field] for field in raw_groupby]
+
         type_filter = (
             ~Q(type=FeedbackGroup.type_id)
             if issue_category == "issue"
             else Q(type=FeedbackGroup.type_id)
         )
-
-        try:
-            interval_s = int(request.GET["interval"]) // 1000
-            if interval_s == 0:
-                raise ParseError("Interval must be greater than 1000 milliseconds.")
-            interval = timedelta(seconds=interval_s)
-        except KeyError:
-            # Defaulting for now. Probably better to compute some known interval. I.e. if we're
-            # close to an hour round up to an hour to ensure the best visual experience.
-            #
-            # Or maybe we require this field and ignore all these problems.
-            interval_s = 3600
-            interval = timedelta(seconds=interval_s)
-        except ValueError:
-            raise ParseError("Could not parse interval value.")
 
         # This step validates our maximum granularity. Without this we could see unbounded
         # cardinality in our queries. Our maximum granularity is 200 which is more than enough to
@@ -67,9 +64,27 @@ class OrganizationIssueMetricsEndpoint(OrganizationEndpoint):
         #   - One week range -> one hour interval.
         #   - One day range -> ten minute interval.
         #   - One hour range -> twenty second interval.
-        number_of_buckets = (end - start).total_seconds() // interval.total_seconds()
-        if number_of_buckets > 200:
-            raise ParseError("The specified granularity is too precise. Increase your interval.")
+        interval = timedelta(
+            seconds=get_rollup_from_request(
+                request, end - start, "1h", ParseError("Invalid Interval"), max_rollup_override=200
+            )
+        )
+
+        SUPPORTED_AXES: dict[str, dict[str, Any]] = {
+            "count(new_issues)": {
+                "objects": Group.objects,
+                "date_column": "first_seen",
+            },
+            "count(resolved_issues)": {
+                "objects": Group.objects.filter(status=GroupStatus.RESOLVED),
+                "date_column": "resolved_at",
+            },
+        }
+
+        axes = request.GET.getlist("yAxis", ["count(new_issues)"])
+        for axis in axes:
+            if axis not in SUPPORTED_AXES:
+                raise ParseError(f"Unknown axis: {axis}")
 
         def gen_ts(
             qs,
@@ -90,18 +105,26 @@ class OrganizationIssueMetricsEndpoint(OrganizationEndpoint):
             )
 
             grouped_counter: collections.defaultdict[str, int] = collections.defaultdict(int)
-            grouped_series: dict[str, list[TimeSeries]] = collections.defaultdict(list)
+            groupby_key_to_dict: dict[str, list[GroupBy]] = {}
+            grouped_series: dict[str, list[Row]] = collections.defaultdict(list)
             for row in qs:
-                grouping = [row[g] for g in group_by]
-                key = "||||".join(grouping)
+                key = create_result_key(row, group_by, {})
+                groupby_dict = create_groupby_dict(row, group_by, {})
+                for groupby_item in groupby_dict:
+                    groupby_item["key"] = INVERSE_GROUPBY_TRANSLATION[groupby_item["key"]]
+                groupby_key_to_dict[key] = groupby_dict
                 grouped_counter[key] += row["value"]
-                grouped_series[key].append({"timestamp": row["timestamp"], "value": row["value"]})
+                grouped_series[key].append(
+                    Row(timestamp=row["timestamp"] * 1000, value=row["value"], incomplete=False)
+                )
 
             # Group the smallest series into the "other" bucket.
             if len(grouped_series) > 4:
-                keys = [v[0] for v in nlargest(5, grouped_counter.items(), key=lambda i: i[0])]
+                keys = [
+                    v[0] for v in nlargest(5, grouped_counter.items(), key=lambda i: (i[1], i[0]))
+                ]
 
-                new_grouped_series: dict[str, list[TimeSeries]] = {}
+                new_grouped_series: dict[str, list[Row]] = {}
                 other_series: collections.defaultdict[float, float] = collections.defaultdict(float)
                 for key, series in grouped_series.items():
                     if key in keys:
@@ -113,7 +136,7 @@ class OrganizationIssueMetricsEndpoint(OrganizationEndpoint):
                 if other_series:
                     new_grouped_series["other"] = list(
                         map(
-                            lambda i: {"timestamp": i[0], "value": i[1]},
+                            lambda i: Row(timestamp=i[0], value=i[1], incomplete=False),
                             sorted(list(other_series.items()), key=lambda i: i[0]),
                         )
                     )
@@ -129,8 +152,8 @@ class OrganizationIssueMetricsEndpoint(OrganizationEndpoint):
                         start=start,
                         end=end,
                         interval=interval,
-                        is_other=False,
-                        order=0,
+                        is_other=None,
+                        order=None,
                         values=[],
                     )
                 ]
@@ -138,67 +161,42 @@ class OrganizationIssueMetricsEndpoint(OrganizationEndpoint):
             return [
                 make_timeseries_result(
                     axis=axis,
-                    group=key.split("||||") if key else [],
+                    group=groupby_key_to_dict.get(key, []),
                     start=start,
                     end=end,
                     interval=interval,
-                    is_other=key == "other",
-                    order=i,
+                    is_other=key == "other" if len(new_grouped_series) > 1 else None,
+                    order=i if len(new_grouped_series) > 1 else None,
                     values=series,
                 )
                 for i, (key, series) in enumerate(new_grouped_series.items())
             ]
 
+        timeseries = []
+        for axis in axes:
+            axis_definition = SUPPORTED_AXES[axis]
+            timeseries.extend(
+                gen_ts(
+                    axis_definition["objects"],
+                    axis=axis,
+                    date_column_name=(
+                        axis_definition["date_column"] if len(groupby) == 0 else "first_seen"
+                    ),
+                    group_by=groupby,
+                )
+            )
+
         return Response(
             {
-                "timeseries": chain(
-                    gen_ts(
-                        Group.objects,
-                        axis="new_issues_count",
-                        date_column_name="first_seen",
-                        group_by=[],
-                    ),
-                    gen_ts(
-                        Group.objects.filter(status=GroupStatus.RESOLVED),
-                        axis="resolved_issues_count",
-                        date_column_name="resolved_at",
-                        group_by=[],
-                    ),
-                    gen_ts(
-                        Group.objects.filter(first_release__isnull=False),
-                        axis="new_issues_count_by_release",
-                        date_column_name="first_seen",
-                        group_by=["first_release__version"],
-                    ),
-                ),
+                "timeSeries": timeseries,
                 "meta": {
-                    "dataset": "issues",
-                    "end": end.timestamp(),
-                    "start": start.timestamp(),
+                    "dataset": issue_category,
+                    "end": end.timestamp() * 1000,
+                    "start": start.timestamp() * 1000,
                 },
             },
             status=200,
         )
-
-
-class TimeSeries(TypedDict):
-    timestamp: float
-    value: float
-
-
-class TimeSeriesResultMeta(TypedDict):
-    interval: float
-    isOther: bool
-    order: int
-    valueType: str
-    valueUnit: str | None
-
-
-class TimeSeriesResult(TypedDict):
-    axis: str
-    groupBy: list[str]
-    meta: TimeSeriesResultMeta
-    values: list[TimeSeries]
 
 
 def make_timeseries_query(
@@ -224,6 +222,8 @@ def make_timeseries_query(
         annotations[group] = F(group)
         order_by.append(group)
         values.append(group)
+        if group == "first_release__version":
+            qs = qs.filter(first_release__isnull=False)
 
     annotations["timestamp"] = Extract(
         Func(
@@ -255,26 +255,29 @@ def make_timeseries_query(
 
 def make_timeseries_result(
     axis: str,
-    group: list[str],
+    group: list[GroupBy],
     start: datetime,
     end: datetime,
     interval: timedelta,
-    is_other: bool,
-    order: int,
-    values: list[TimeSeries],
-) -> TimeSeriesResult:
-    return {
-        "axis": axis,
-        "groupBy": group,
-        "meta": {
-            "interval": interval.total_seconds() * 1000,
-            "isOther": is_other,
-            "order": order,
-            "valueType": "integer",
-            "valueUnit": None,
-        },
-        "values": fill_timeseries(start, end, interval, values),
-    }
+    is_other: bool | None,
+    order: int | None,
+    values: list[Row],
+) -> TimeSeries:
+    meta = SeriesMeta(
+        interval=interval.seconds * 1000,
+        valueType="integer",
+        valueUnit=None,
+    )
+    if is_other is not None:
+        meta["isOther"] = is_other
+    if order is not None:
+        meta["order"] = order
+    return TimeSeries(
+        yAxis=axis,
+        groupBy=group,
+        values=fill_timeseries(start, end, interval, values),
+        meta=meta,
+    )
 
 
 class UnconsumedBuckets(LookupError):
@@ -285,23 +288,23 @@ def fill_timeseries(
     start: datetime,
     end: datetime,
     interval: timedelta,
-    values: list[TimeSeries],
-) -> list[TimeSeries]:
+    values: list[Row],
+) -> list[Row]:
     def iter_interval(start: datetime, end: datetime, interval: timedelta) -> Iterator[int]:
         while start <= end:
-            yield int(start.timestamp())
+            yield int(start.timestamp() * 1000)
             start = start + interval
 
-    filled_values: list[TimeSeries] = []
-    idx = 0
+    filled_values: list[Row] = []
+    index = 0
     for ts in iter_interval(start, end, interval):
-        if idx < len(values) and ts == values[idx]["timestamp"]:
-            filled_values.append(values[idx])
-            idx += 1
+        if index < len(values) and ts == values[index]["timestamp"]:
+            filled_values.append(values[index])
+            index += 1
         else:
-            filled_values.append({"timestamp": ts, "value": 0})
+            filled_values.append(Row(timestamp=ts, value=0, incomplete=False))
 
-    if idx != len(values):
+    if index != len(values):
         raise UnconsumedBuckets("Could not fill every bucket.")
 
     return filled_values
