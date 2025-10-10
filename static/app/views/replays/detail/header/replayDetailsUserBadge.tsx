b21a35e1ecfb4ab60308c22447c36dd7d5882d20
jerryzhou196 <jerry.zhou@sentry.io>
2025-10-10 14:12:02 -0400

    fix(replay) - live indicator refresh button doesn't resize parent container

--- static/app/views/replays/detail/header/replayDetailsUserBadge.tsx
+++ static/app/views/replays/detail/header/replayDetailsUserBadge.tsx
@@ -150,7 +150,7 @@ const TimeContainer = styled('div')`
   align-items: center;
   color: ${p => p.theme.subText};
   font-size: ${p => p.theme.fontSize.md};
-  line-height: 1.4;
+  line-height: 1.9;
 `;
 
 const DisplayHeader = styled('div')`
