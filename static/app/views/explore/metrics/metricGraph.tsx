2246b4a301adf8c42b8260398a977b42f560ea0a
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-10 13:26:10 -0400

    feat(tracemetrics): Clean up group by loading state (#101349)
    - Cleans up some jank around the loading indicator that appears when a
    new metric is added
    - Removes `height` from the chart so it's always the height of its
    container

--- static/app/views/explore/metrics/metricGraph.tsx
+++ static/app/views/explore/metrics/metricGraph.tsx
@@ -122,7 +122,6 @@ function Graph({onChartTypeChange, timeseriesResult, visualize}: GraphProps) {
       Title={Title}
       Actions={Actions}
       Visualization={visualize.visible && <ChartVisualization chartInfo={chartInfo} />}
-      height={visualize.visible ? 200 : 50}
       revealActions="always"
       borderless
     />
