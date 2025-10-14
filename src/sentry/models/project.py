372ed3a28a70ca0da2271d4eab9aabff8a69e308
Charlie Luo <cvxluo@gmail.com>
2025-10-13 20:55:43 -0700

    delete codeowners when transferring projects

--- src/sentry/models/project.py
+++ src/sentry/models/project.py
@@ -504,6 +504,7 @@ class Project(Model):
             RepositoryProjectPathConfig,
         )
         from sentry.models.environment import Environment, EnvironmentProject
+        from sentry.models.projectcodeowners import ProjectCodeOwners
         from sentry.models.projectteam import ProjectTeam
         from sentry.models.releaseprojectenvironment import ReleaseProjectEnvironment
         from sentry.models.releases.release_project import ReleaseProject
@@ -638,6 +639,8 @@ class Project(Model):
             "linked_id", flat=True
         )
 
+        # Delete code mappings to prevent them from being stuck on the old org
+        ProjectCodeOwners.objects.filter(project_id=self.id).delete()
         # Delete code mappings to prevent them from being stuck on the old org
         RepositoryProjectPathConfig.objects.filter(project_id=self.id).delete()
 
