8b6ee8c967d9025be1cbc8f12534b1a5f903cd96
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-14 13:32:06 -0400

    Make sure error result is passed

--- static/app/views/explore/metrics/hooks/useTraceTelemetry.tsx
+++ static/app/views/explore/metrics/hooks/useTraceTelemetry.tsx
@@ -162,6 +162,6 @@ export function useTraceTelemetry({
 
   return {
     data: telemetryData,
-    isLoading: spansResult.isPending || logsResult.isPending,
+    isLoading: spansResult.isPending || logsResult.isPending || errorsResult.isPending,
   };
 }
