c5da0356b4067a3ec9f51b7d67479b4545af00d4
jerryzhou196 <jerry.zhou@sentry.io>
2025-10-14 15:09:27 -0400

    staleTime is a required type :(

--- static/app/utils/replays/hooks/usePollReplayRecord.tsx
+++ static/app/utils/replays/hooks/usePollReplayRecord.tsx
@@ -22,6 +22,7 @@ function usePollReplayRecord({
     refetchInterval: pollInterval,
     enabled,
     refetchIntervalInBackground: true,
+    staleTime: Infinity,
   });
 
   const polledReplayRecord = replayData?.data;
