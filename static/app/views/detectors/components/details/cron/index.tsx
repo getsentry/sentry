eff0680a5626027e102547afb49e6dad4d877a03
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-10-14 13:49:01 -0400

    feat(crons): Add timezone selector to cron detactor details
    Fixes [NEW-561: There should be a timezone selector affecting dates](https://linear.app/getsentry/issue/NEW-561/there-should-be-a-timezone-selector-affecting-dates)

--- static/app/views/detectors/components/details/cron/index.tsx
+++ static/app/views/detectors/components/details/cron/index.tsx
@@ -2,12 +2,14 @@ import {Fragment, useCallback, useState} from 'react';
 import sortBy from 'lodash/sortBy';
 
 import {Alert} from 'sentry/components/core/alert';
+import {Flex} from 'sentry/components/core/layout';
 import ErrorBoundary from 'sentry/components/errorBoundary';
 import {KeyValueTableRow} from 'sentry/components/keyValueTable';
 import {DatePageFilter} from 'sentry/components/organizations/datePageFilter';
 import {EnvironmentPageFilter} from 'sentry/components/organizations/environmentPageFilter';
 import PageFilterBar from 'sentry/components/organizations/pageFilterBar';
 import TimeSince from 'sentry/components/timeSince';
+import {TimezoneProvider, useTimezone} from 'sentry/components/timezoneProvider';
 import DetailLayout from 'sentry/components/workflowEngine/layout/detail';
 import Section from 'sentry/components/workflowEngine/ui/section';
 import {IconJson} from 'sentry/icons';
@@ -25,6 +27,7 @@ import {DetailsTimeline} from 'sentry/views/insights/crons/components/detailsTim
 import {DetailsTimelineLegend} from 'sentry/views/insights/crons/components/detailsTimelineLegend';
 import {MonitorCheckIns} from 'sentry/views/insights/crons/components/monitorCheckIns';
 import {MonitorOnboarding} from 'sentry/views/insights/crons/components/onboarding';
+import {TimezoneOverride} from 'sentry/views/insights/crons/components/timezoneOverride';
 import type {MonitorBucket, MonitorEnvironment} from 'sentry/views/insights/crons/types';
 
 type CronDetectorDetailsProps = {
@@ -48,6 +51,8 @@ function hasLastCheckIn(envs: MonitorEnvironment[]) {
 export function CronDetectorDetails({detector, project}: CronDetectorDetailsProps) {
   const location = useLocation();
   const dataSource = detector.dataSources[0];
+  const userTimezone = useTimezone();
+  const [timezoneOverride, setTimezoneOverride] = useState(userTimezone);
 
   const {failure_issue_threshold, recovery_threshold} = dataSource.queryObj.config;
 
@@ -101,104 +106,116 @@ export function CronDetectorDetails({detector, project}: CronDetectorDetailsProp
   }, []);
 
   return (
-    <DetailLayout>
-      <DetectorDetailsHeader detector={detector} project={project} />
-      <DetailLayout.Body>
-        <DetailLayout.Main>
-          <PageFilterBar condensed>
-            <EnvironmentPageFilter />
-            <DatePageFilter />
-          </PageFilterBar>
-          {hasCheckedIn ? (
-            <Fragment>
-              <DetailsTimeline
-                monitor={filteredMonitor}
-                onStatsLoaded={checkHasUnknown}
+    <TimezoneProvider timezone={timezoneOverride}>
+      <DetailLayout>
+        <DetectorDetailsHeader detector={detector} project={project} />
+        <DetailLayout.Body>
+          <DetailLayout.Main>
+            <Flex gap="sm" justify="between" align="center">
+              <PageFilterBar condensed>
+                <EnvironmentPageFilter />
+                <DatePageFilter />
+              </PageFilterBar>
+              <TimezoneOverride
+                monitor={dataSource.queryObj}
+                userTimezone={userTimezone}
+                onTimezoneSelected={setTimezoneOverride}
               />
-              <ErrorBoundary mini>
-                <DetectorDetailsOngoingIssues
-                  detector={detector}
-                  intervalSeconds={intervalSeconds}
+            </Flex>
+            {hasCheckedIn ? (
+              <Fragment>
+                <DetailsTimeline
+                  monitor={filteredMonitor}
+                  onStatsLoaded={checkHasUnknown}
                 />
-              </ErrorBoundary>
-              <Section title={t('Recent Check-Ins')}>
-                <div>
-                  <MonitorCheckIns
-                    monitorSlug={dataSource.queryObj.slug}
-                    monitorEnvs={filteredMonitor.environments}
-                    project={project}
+                <ErrorBoundary mini>
+                  <DetectorDetailsOngoingIssues
+                    detector={detector}
+                    intervalSeconds={intervalSeconds}
                   />
-                </div>
-              </Section>
-              <DetectorDetailsAutomations detector={detector} />
-            </Fragment>
-          ) : (
-            <MonitorOnboarding monitorSlug={dataSource.queryObj.slug} project={project} />
-          )}
-        </DetailLayout.Main>
-        <DetailLayout.Sidebar>
-          <Section title={t('Detect')}>
-            {tn(
-              'One failed check-in.',
-              '%s consecutive failed check-ins.',
-              failure_issue_threshold ?? 1
+                </ErrorBoundary>
+                <Section title={t('Recent Check-Ins')}>
+                  <div>
+                    <MonitorCheckIns
+                      monitorSlug={dataSource.queryObj.slug}
+                      monitorEnvs={filteredMonitor.environments}
+                      project={project}
+                    />
+                  </div>
+                </Section>
+                <DetectorDetailsAutomations detector={detector} />
+              </Fragment>
+            ) : (
+              <MonitorOnboarding
+                monitorSlug={dataSource.queryObj.slug}
+                project={project}
+              />
             )}
-          </Section>
-          <Section title={t('Resolve')}>
-            {tn(
-              'One successful check-in.',
-              '%s consecutive successful check-ins.',
-              recovery_threshold ?? 1
+          </DetailLayout.Main>
+          <DetailLayout.Sidebar>
+            <Section title={t('Detect')}>
+              {tn(
+                'One failed check-in.',
+                '%s consecutive failed check-ins.',
+                failure_issue_threshold ?? 1
+              )}
+            </Section>
+            <Section title={t('Resolve')}>
+              {tn(
+                'One successful check-in.',
+                '%s consecutive successful check-ins.',
+                recovery_threshold ?? 1
+              )}
+            </Section>
+            <DetectorDetailsAssignee owner={detector.owner} />
+            <Section title={t('Legend')}>
+              <DetailsTimelineLegend
+                checkInMargin={dataSource.queryObj.config.checkin_margin}
+                maxRuntime={dataSource.queryObj.config.max_runtime}
+                showUnknownLegend={showUnknownLegend}
+              />
+            </Section>
+            <DetectorExtraDetails>
+              <KeyValueTableRow
+                keyName={t('Monitor slug')}
+                value={dataSource.queryObj.slug}
+              />
+              <KeyValueTableRow
+                keyName={t('Next check-in')}
+                value={
+                  dataSource.queryObj.status !== 'disabled' && monitorEnv?.nextCheckIn ? (
+                    <TimeSince unitStyle="regular" date={monitorEnv.nextCheckIn} />
+                  ) : (
+                    '-'
+                  )
+                }
+              />
+              <KeyValueTableRow
+                keyName={t('Last check-in')}
+                value={
+                  monitorEnv?.lastCheckIn ? (
+                    <TimeSince unitStyle="regular" date={monitorEnv.lastCheckIn} />
+                  ) : (
+                    '-'
+                  )
+                }
+              />
+              <DetectorExtraDetails.DateCreated detector={detector} />
+              <DetectorExtraDetails.CreatedBy detector={detector} />
+              <DetectorExtraDetails.LastModified detector={detector} />
+            </DetectorExtraDetails>
+            {dataSource.queryObj.isUpserting && (
+              <Alert.Container>
+                <Alert type="muted" icon={<IconJson />}>
+                  {t(
+                    'This monitor is managed in code and updates automatically with each check-in.'
+                  )}
+                </Alert>
+              </Alert.Container>
             )}
-          </Section>
-          <DetectorDetailsAssignee owner={detector.owner} />
-          <Section title={t('Legend')}>
-            <DetailsTimelineLegend
-              checkInMargin={dataSource.queryObj.config.checkin_margin}
-              maxRuntime={dataSource.queryObj.config.max_runtime}
-              showUnknownLegend={showUnknownLegend}
-            />
-          </Section>
-          <DetectorExtraDetails>
-            <KeyValueTableRow
-              keyName={t('Monitor slug')}
-              value={dataSource.queryObj.slug}
-            />
-            <KeyValueTableRow
-              keyName={t('Next check-in')}
-              value={
-                dataSource.queryObj.status !== 'disabled' && monitorEnv?.nextCheckIn ? (
-                  <TimeSince unitStyle="regular" date={monitorEnv.nextCheckIn} />
-                ) : (
-                  '-'
-                )
-              }
-            />
-            <KeyValueTableRow
-              keyName={t('Last check-in')}
-              value={
-                monitorEnv?.lastCheckIn ? (
-                  <TimeSince unitStyle="regular" date={monitorEnv.lastCheckIn} />
-                ) : (
-                  '-'
-                )
-              }
-            />
-            <DetectorExtraDetails.DateCreated detector={detector} />
-            <DetectorExtraDetails.CreatedBy detector={detector} />
-            <DetectorExtraDetails.LastModified detector={detector} />
-          </DetectorExtraDetails>
-          {dataSource.queryObj.isUpserting && (
-            <Alert.Container>
-              <Alert type="muted" icon={<IconJson />}>
-                {t(
-                  'This monitor is managed in code and updates automatically with each check-in.'
-                )}
-              </Alert>
-            </Alert.Container>
-          )}
-        </DetailLayout.Sidebar>
-      </DetailLayout.Body>
-    </DetailLayout>
+          </DetailLayout.Sidebar>
+        </DetailLayout.Body>
+      </DetailLayout>
+    </TimezoneProvider>
   );
 }
