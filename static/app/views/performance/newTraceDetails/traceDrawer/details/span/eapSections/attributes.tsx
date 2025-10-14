19fbec31cca93a8b193730d42b38127d241f7a8b
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-10-14 11:56:39 +0200

    fix

--- static/app/views/performance/newTraceDetails/traceDrawer/details/span/eapSections/attributes.tsx
+++ static/app/views/performance/newTraceDetails/traceDrawer/details/span/eapSections/attributes.tsx
@@ -1,4 +1,4 @@
-import {Fragment, useMemo, useState} from 'react';
+import {useMemo, useState} from 'react';
 import type {Theme} from '@emotion/react';
 import styled from '@emotion/styled';
 import type {Location, LocationDescriptorObject} from 'history';
