51ee146abd2ced3e1cbeb2d548fb42615425451b
Kev <kevan.fisher@sentry.io>
2025-10-16 15:20:26 -0400

    knip...

--- static/app/views/explore/metrics/metricsFlags.tsx
+++ static/app/views/explore/metrics/metricsFlags.tsx
@@ -6,49 +6,8 @@ export const canUseMetricsUI = (organization: Organization) => {
   return organization.features.includes('tracemetrics-enabled');
 };
 
-export const canUseMetricsDashboardsUI = (organization: Organization) => {
-  return (
-    canUseMetricsUI(organization) &&
-    organization.features.includes('tracemetrics-dashboards')
-  );
-};
-
-export const canUseMetricsDatascrubbingUI = (organization: Organization) => {
-  return (
-    canUseMetricsUI(organization) &&
-    organization.features.includes('tracemetrics-datascrubbing-ui')
-  );
-};
-
-export const canUseMetricsExportUI = (organization: Organization) => {
-  return (
-    canUseMetricsUI(organization) && organization.features.includes('tracemetrics-export')
-  );
-};
-
-export const canUseMetricsReplayUI = (organization: Organization) => {
-  return (
-    canUseMetricsUI(organization) &&
-    organization.features.includes('tracemetrics-replay-ui')
-  );
-};
-
-export const canUseMetricsSavedQueriesUI = (organization: Organization) => {
-  return (
-    canUseMetricsUI(organization) &&
-    organization.features.includes('tracemetrics-saved-queries')
-  );
-};
-
 export const canUseMetricsStatsUI = (organization: Organization) => {
   return (
     canUseMetricsUI(organization) && organization.features.includes('tracemetrics-stats')
-  );
-};
-
-export const canUseMetricsTraceViewUI = (organization: Organization) => {
-  return (
-    canUseMetricsUI(organization) &&
-    organization.features.includes('tracemetrics-traceview-ui')
   );
 };
