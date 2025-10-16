254181590212d19d255f1a15a9e23d83b7304a6c
Irene Liu <ireneliu@R7F6F9L07W.local>
2025-10-16 16:39:03 -0700

    import fix

--- src/sentry/integrations/api/serializers/rest_framework/data_forwarder.py
+++ src/sentry/integrations/api/serializers/rest_framework/data_forwarder.py
@@ -2,7 +2,7 @@ import re
 from collections.abc import Mapping, MutableMapping
 from typing import Any, TypedDict
 
-from django import router, transaction
+from django.db import router, transaction
 from rest_framework import serializers
 from rest_framework.serializers import Serializer, ValidationError
 
