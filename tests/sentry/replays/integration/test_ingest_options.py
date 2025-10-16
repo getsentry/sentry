05a786c6a472407913da16a07d06083591547243
Colton Allen <colton.allen@sentry.io>
2025-10-16 14:17:06 -0500

    Fix types

--- tests/sentry/replays/integration/test_ingest_options.py
+++ tests/sentry/replays/integration/test_ingest_options.py
@@ -1,4 +1,5 @@
 from sentry.models.options.project_option import ProjectOption
+from sentry.models.project import Project
 from sentry.replays.usecases.ingest.event_logger import (
     _should_report_hydration_error_issue,
     _should_report_rage_click_issue,
@@ -7,13 +8,13 @@ from sentry.testutils.pytest.fixtures import django_db_all
 
 
 @django_db_all
-def test_should_report_hydration_error_issue_no_value(default_project):
+def test_should_report_hydration_error_issue_no_value(default_project: Project) -> None:
     result = _should_report_hydration_error_issue(default_project.id)
     assert result is False
 
 
 @django_db_all
-def test_should_report_hydration_error_issue(default_project):
+def test_should_report_hydration_error_issue(default_project: Project) -> None:
     ProjectOption.objects.set_value(
         project=default_project, key="sentry:replay_hydration_error_issues", value=True
     )
@@ -23,28 +24,29 @@ def test_should_report_hydration_error_issue(default_project):
 
 
 @django_db_all
-def test_should_report_hydration_error_issue_no_project():
+def test_should_report_hydration_error_issue_no_project() -> None:
     result = _should_report_hydration_error_issue(210492104914)
     assert result is False
 
 
 @django_db_all
-def test_should_report_rage_click_issue_no_value(default_project):
+def test_should_report_rage_click_issue_no_value(default_project: Project) -> None:
     result = _should_report_rage_click_issue(default_project.id)
     assert result is False
 
 
 @django_db_all
-def test_should_report_rage_click_issue_no_project():
+def test_should_report_rage_click_issue_no_project() -> None:
     result = _should_report_rage_click_issue(210492104914)
     assert result is False
 
 
 @django_db_all
-def test_should_report_rage_click_issue(default_project):
+def test_should_report_rage_click_issue(default_project: Project) -> None:
     ProjectOption.objects.set_value(
         project=default_project, key="sentry:replay_rage_click_issues", value=True
     )
 
     result = _should_report_rage_click_issue(default_project.id)
+    assert result is True
     assert result is True
