878930e3f9bac20cd49d600fda9d89355f8602c0
William Mak <william@wmak.io>
2025-10-16 16:14:22 -0400

    fix(stats): Meta was being reused between timeseries (#101629)
    - Fix a bug where the subsequent metas would be wrong since we modified
    the dict during runtime

--- src/sentry/api/bases/organization_events.py
+++ src/sentry/api/bases/organization_events.py
@@ -354,8 +354,9 @@ class OrganizationEventsV2EndpointBase(OrganizationEventsEndpointBase):
     ) -> dict[str, Any]:
         with sentry_sdk.start_span(op="discover.endpoint", name="base.handle_results"):
             data = self.handle_data(request, organization, project_ids, results.get("data"))
-            meta = results.get("meta", {})
-            fields_meta = meta.get("fields", {})
+            # these may get re-used by other timeseries
+            meta = results.get("meta", {}).copy()
+            fields_meta = meta.get("fields", {}).copy()
 
             if standard_meta:
                 isMetricsData = meta.pop("isMetricsData", False)
