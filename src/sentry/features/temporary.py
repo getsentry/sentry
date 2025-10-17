1a4a43d38dc8445a93c0e60b2ade0bd8fddaa2d6
Raj Joshi <raj.joshi@sentry.io>
2025-10-17 09:27:17 -0700

    ✨ feat(github): outbound assignment syncing (#101323)

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -165,6 +165,7 @@ def register_temporary_features(manager: FeatureManager) -> None:
     # Project Management Integrations Feature Parity Flags
     # GitHub inbound assignee sync
     manager.add("organizations:integrations-github-inbound-assignee-sync", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    manager.add("organizations:integrations-github-outbound-assignee-sync", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable inviting billing members to organizations at the member limit.
     manager.add("organizations:invite-billing", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, default=False, api_expose=False)
     # Enable inviting members to organizations.
