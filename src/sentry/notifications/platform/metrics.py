abcf8527cdc80ff08e6a4b4a1418cf2b914e4320
Christinarlong <60594860+Christinarlong@users.noreply.github.com>
2025-10-17 14:43:22 -0700

    feat(np): Specify synchronous notification send w/ slos (#101358)

---
+++ src/sentry/notifications/platform/metrics.py
@@ -0,0 +1,41 @@
+from collections.abc import Mapping
+from dataclasses import dataclass
+from enum import StrEnum
+
+from sentry.integrations.types import EventLifecycleOutcome
+from sentry.integrations.utils.metrics import EventLifecycleMetric
+from sentry.notifications.platform.types import NotificationCategory, NotificationProviderKey
+
+
+class NotificationInteractionType(StrEnum):
+    """Actions involved in notifications"""
+
+    NOTIFY_TARGET_SYNC = "notify_target_sync"
+
+
+@dataclass
+class NotificationEventLifecycleMetric(EventLifecycleMetric):
+    interaction_type: NotificationInteractionType
+    # The template/source of the notification
+    notification_source: str
+    # The sender of the notification
+    notification_provider: NotificationProviderKey | None = None
+    # The category of the notification
+    notification_category: NotificationCategory | None = None
+
+    def get_metric_key(self, outcome: EventLifecycleOutcome) -> str:
+        tokens = ("notifications", "slo", str(outcome))
+        return ".".join(tokens)
+
+    def get_metric_tags(self) -> Mapping[str, str]:
+        tags = {
+            "interaction_type": self.interaction_type,
+            "notification_source": self.notification_source,
+        }
+
+        if self.notification_provider:
+            tags["notification_provider"] = self.notification_provider
+        if self.notification_category:
+            tags["category"] = self.notification_category
+
+        return tags
