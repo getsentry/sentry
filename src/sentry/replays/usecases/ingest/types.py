1b6ca94c70cfca73a6442a2f0d3aae8993f74c91
Colton Allen <colton.allen@sentry.io>
2025-10-17 10:24:12 -0500

    feat(replays): Add memory-efficient project and project option query caches (#101606)
    `get_from_cache` caches results in a thread-local. This means eight
    copies of the same project model (and project-option model) for our
    current configuration. Additionally `get_from_cache` is very coarse
    fetching fields we don't need. By caching just the boolean values we
    care about we can minimize the footprint of each query on our overall
    memory usage. Since we need to cache a lot of projects and
    project-options this is important to maintaining stable memory usage.
    
    Total memory should be reduced by `O(8n)` where `n` is the delta between
    the size of a project model and a boolean and the size of the
    project-option model and a tuple of booleans.
    
    A word on the `AutoCache`. `AutoCache` is safe but not logically atomic.
    We defer to last writer wins and potentially duplicate work. This could
    be improved but we don't expect the results of the `fn` argument to
    produce an effect or be non-deterministic. At least for our current
    case. However, it might be wise to implement better locking behavior in
    `AutoCache.__getitem__` so we don't unnecessarily compute a project or
    project-option query multiple times. This is easy enough to do but I
    think we've done enough already for this pull so we can address this in
    a follow-up!
    
    A `context` object is now being passed around the consumer. This is
    better for testing than using globals. There are more effects that could
    be moved out of the processing logic and into the context object. This
    would make our consumer significantly easier to test and require fewer
    mocks.

---
+++ src/sentry/replays/usecases/ingest/types.py
@@ -0,0 +1,8 @@
+from typing import TypedDict
+
+from sentry.replays.lib.cache import AutoCache
+
+
+class ProcessorContext(TypedDict):
+    has_sent_replays_cache: AutoCache[int, bool] | None
+    options_cache: AutoCache[int, tuple[bool, bool]] | None
