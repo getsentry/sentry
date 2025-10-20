62c90b352aebdba75e4752dcf8dbec80f1b35bf7
ArthurKnaus <arthur.knaus@sentry.io>
2025-10-20 11:04:30 +0200

    ref(content-blocks): Convert javascript utils (#101787)
    - part of [TET-761: Docs: Move from configuration objects to content
    blocks](https://linear.app/getsentry/issue/TET-761/docs-move-from-configuration-objects-to-content-blocks)

--- static/app/gettingStartedDocs/javascript/nextjs.tsx
+++ static/app/gettingStartedDocs/javascript/nextjs.tsx
@@ -548,57 +548,51 @@ const profilingOnboarding = getJavascriptFullStackOnboarding({
     'https://docs.sentry.io/platforms/javascript/guides/nextjs/profiling/browser-profiling/',
   nodeProfilingLink:
     'https://docs.sentry.io/platforms/javascript/guides/nextjs/profiling/node-profiling/',
-  getProfilingHeaderConfig: () => [
+  getProfilingHeaderContent: () => [
     {
-      description: tct(
+      type: 'text',
+      text: tct(
         'In Next.js you can configure document response headers via the headers option in [code:next.config.js]:',
         {
           code: <code />,
         }
       ),
-      code: [
+    },
+    {
+      type: 'code',
+      tabs: [
         {
           label: 'ESM',
-          value: 'esm',
           language: 'javascript',
           filename: 'next.config.js',
           code: `
 export default withSentryConfig({
   async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Document-Policy",
-            value: "js-profiling",
-          },
-        ],
-      },
-    ];
+    return [{
+      source: "/:path*",
+      headers: [{
+        key: "Document-Policy",
+        value: "js-profiling",
+      }],
+    }];
   },
   // ... other Next.js config options
 });`,
         },
         {
           label: 'CJS',
-          value: 'cjs',
           language: 'javascript',
           filename: 'next.config.js',
           code: `
 module.exports = withSentryConfig({
   async headers() {
-    return [
-      {
-        source: "/:path*",
-        headers: [
-          {
-            key: "Document-Policy",
-            value: "js-profiling",
-          },
-        ],
-      },
-    ];
+    return [{
+      source: "/:path*",
+      headers: [{
+        key: "Document-Policy",
+        value: "js-profiling",
+      }],
+    }];
   },
   // ... other Next.js config options
 });`,
