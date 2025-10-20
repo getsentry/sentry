306917e1e9f9c1ccb20b1b1775f81afa9143a2e5
Tony Le <tony.le@sentry.io>
2025-10-20 03:08:17 -0400

    Add tests

---
+++ tests/sentry/taskworker/test_router.py
@@ -0,0 +1,29 @@
+import pytest
+from django.test.utils import override_settings
+
+from sentry.conf.types.kafka_definition import Topic
+from sentry.silo.base import SiloMode
+from sentry.taskworker.router import DefaultRouter
+
+
+@pytest.mark.django_db
+def test_default_router_topic() -> None:
+    router = DefaultRouter()
+    topic = router.route_namespace("test.tasks.test_router.default")
+    assert topic == Topic.TASKWORKER
+
+
+@pytest.mark.django_db
+def test_default_router_topic_region_silo() -> None:
+    with override_settings(SILO_MODE=SiloMode.REGION):
+        router = DefaultRouter()
+        topic = router.route_namespace("test.tasks.test_router.region")
+        assert topic == Topic.TASKWORKER
+
+
+@pytest.mark.django_db
+def test_default_router_topic_control_silo() -> None:
+    with override_settings(SILO_MODE=SiloMode.CONTROL):
+        router = DefaultRouter()
+        topic = router.route_namespace("test.tasks.test_router.control")
+        assert topic == Topic.TASKWORKER_CONTROL
