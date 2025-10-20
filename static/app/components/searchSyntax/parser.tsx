cc51e3efdbd343b1354cb2770872b83579d45aa1
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-10-20 16:01:42 -0400

    Use a plain `...` merge for configuration

--- static/app/components/searchSyntax/parser.tsx
+++ static/app/components/searchSyntax/parser.tsx
@@ -1,5 +1,4 @@
 import * as Sentry from '@sentry/react';
-import merge from 'lodash/merge';
 import moment from 'moment-timezone';
 import type {LocationRange} from 'peggy';
 
@@ -1524,7 +1523,7 @@ export function parseSearch(
   additionalConfig?: Partial<SearchConfig>
 ): ParseResult | null {
   const config = additionalConfig
-    ? merge({...defaultConfig}, additionalConfig)
+    ? {...defaultConfig, ...additionalConfig}
     : defaultConfig;
 
   return tryParseSearch(query, {
