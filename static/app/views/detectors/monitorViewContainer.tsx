5032f4dd80b3a5cbf6b8de36163d68846bcfeb48
Evan Purkhiser <evanpurkhiser@gmail.com>
2025-10-20 14:44:09 -0400

    fix(detectors): Correct automationsLinkPrefix (#101813)
    I missed this here
    https://github.com/getsentry/sentry/pull/101747#discussion_r2441361325

--- static/app/views/detectors/monitorViewContainer.tsx
+++ static/app/views/detectors/monitorViewContainer.tsx
@@ -7,7 +7,7 @@ export default function MonitorViewContainer() {
     <MonitorViewContext.Provider
       value={{
         monitorsLinkPrefix: `monitors`,
-        automationsLinkPrefix: `automations`,
+        automationsLinkPrefix: `monitors/automations`,
       }}
     >
       <Outlet />
