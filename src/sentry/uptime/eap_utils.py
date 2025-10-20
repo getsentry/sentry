7329c15891abf910c3ceb7f99daff357f29b3c96
Dan Fuller <dfuller@sentry.io>
2025-10-20 12:08:25 -0700

    add backwards compat code

--- src/sentry/uptime/eap_utils.py
+++ src/sentry/uptime/eap_utils.py
@@ -1,4 +1,5 @@
 from sentry_protos.snuba.v1.endpoint_trace_item_table_pb2 import Column
+from sentry_protos.snuba.v1.request_common_pb2 import TraceItemType
 
 from sentry.search.eap.uptime_results.attributes import UPTIME_ATTRIBUTE_DEFINITIONS
 
@@ -9,3 +10,10 @@ def get_columns_for_uptime_result() -> list[Column]:
         Column(label=renames.get(col.internal_name, col.internal_name), key=col.proto_definition)
         for col in UPTIME_ATTRIBUTE_DEFINITIONS.values()
     ]
+
+
+# XXX: Backwards compat for this function
+def get_columns_for_uptime_trace_item_type(
+    trace_item_type: TraceItemType.ValueType = TraceItemType.TRACE_ITEM_TYPE_UPTIME_RESULT,
+) -> list[Column]:
+    return get_columns_for_uptime_result()
