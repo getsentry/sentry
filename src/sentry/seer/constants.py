5365409ddfc0e2a4a228ab3a9c68cc9d67e172fe
Rohan Agarwal <rohan.agarwal@sentry.io>
2025-10-21 14:06:31 -0700

    Fix repo definition RPC

---
+++ src/sentry/seer/constants.py
@@ -0,0 +1,13 @@
+"""
+Constants shared across Seer modules.
+"""
+
+from sentry.integrations.types import IntegrationProviderSlug
+
+# Supported repository providers for Seer features
+SEER_SUPPORTED_SCM_PROVIDERS = [
+    "integrations:github",
+    "integrations:github_enterprise",
+    IntegrationProviderSlug.GITHUB.value,
+    IntegrationProviderSlug.GITHUB_ENTERPRISE.value,
+]
