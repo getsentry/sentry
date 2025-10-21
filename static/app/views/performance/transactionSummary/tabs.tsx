c9822f65eccbd123e793c77411f3565869d4564f
TkDodo <dominik.dorfmeister@sentry.io>
2025-10-21 09:28:09 -0700

    ref: remove unused transactionSummary tabs

--- static/app/views/performance/transactionSummary/tabs.tsx
+++ static/app/views/performance/transactionSummary/tabs.tsx
@@ -3,10 +3,8 @@ enum Tab {
   WEB_VITALS = 'vitals',
   TAGS = 'tags',
   EVENTS = 'events',
-  SPANS = 'spans',
   REPLAYS = 'replays',
   PROFILING = 'profiling',
-  AGGREGATE_WATERFALL = 'aggregateWaterfall',
 }
 
 export default Tab;
