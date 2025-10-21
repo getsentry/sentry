35602d2201b9b7027bca7d18e7b5077bb8c8e2e2
Nate Moore <nate.moore@sentry.io>
2025-10-21 16:43:36 -0700

    feat(core): add quote element

---
+++ static/app/components/core/quote/quote.tsx
@@ -0,0 +1,42 @@
+import {Fragment} from 'react';
+import {css, useTheme, type Theme} from '@emotion/react';
+
+import {Stack} from '@sentry/scraps/layout';
+import {Text} from '@sentry/scraps/text';
+
+export const quoteStyles = (theme: Theme) => css`
+  /**
+   * Reset any properties that might be set by the global CSS styles.
+   */
+  margin: 0;
+  padding: 0;
+  border: none;
+`;
+
+interface QuoteProps extends React.HTMLProps<HTMLElementTagNameMap['blockquote']> {
+  source?: {
+    href?: string;
+    label?: string;
+    person?: string;
+  };
+}
+export function Quote(props: QuoteProps) {
+  const theme = useTheme();
+  return (
+    <Stack as="figure" paddingLeft="xl" marginLeft="lg" borderLeft="primary">
+      <blockquote css={quoteStyles(theme)} cite={props.source?.href} {...props} />
+      {props.source ? (
+        <figcaption>
+          <Text as="p">
+            {props.source.person}
+            {props.source?.label ? (
+              <Fragment>
+                , <cite>{props.source.label}</cite>
+              </Fragment>
+            ) : null}
+          </Text>
+        </figcaption>
+      ) : null}
+    </Stack>
+  );
+}
