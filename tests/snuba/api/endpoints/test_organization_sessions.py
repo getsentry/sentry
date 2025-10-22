0fafee0aec4ffb7d056fcc7f43d13492d6ae7d9a
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2025-10-22 13:20:26 -0400

    fix(session): Fix invalid orderby when selecting composite derived metric (#101897)
    Fixes an issue on the `sessions/` endpoint when querying for derived
    metrics using multiple entities. The endpoint incorrectly uses the
    selected derived metric as the orderby which results in a thrown error
    on mismatching entity. Updates so that the orderby is left as `None`
    instead to fall back onto whatever the default is on the platform (which
    I believe is just arbitrary).

--- tests/snuba/api/endpoints/test_organization_sessions.py
+++ tests/snuba/api/endpoints/test_organization_sessions.py
@@ -1879,6 +1879,16 @@ class OrganizationSessionsEndpointTest(APITestCase, BaseMetricsTestCase):
         assert group["totals"]["sum(session)"] == 11
         assert group["totals"]["unhealthy_rate(session)"] == pytest.approx(5 / 11)
 
+        # 5 - succeeds without orderBy
+        response = req(
+            field=[
+                "unhealthy_rate(session)",
+            ],
+        )
+        assert response.status_code == 200, response.content
+        group = response.data["groups"][0]
+        assert group["totals"]["unhealthy_rate(session)"] == pytest.approx(5 / 11)
+
     @freeze_time(MOCK_DATETIME)
     def test_unhandled_rate(self) -> None:
         default_request = {
