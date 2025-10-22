3dc55b5c959992d4d839a70215c6952c593eb307
Shruthi <shruthilaya.jaganathan@sentry.io>
2025-10-22 11:10:18 -0400

    feat(span-first): Support all transaction alerts on on-demand (#101471)
    We want to move all current alerts use on-demand so that transactions
    and generic_metrics
    datasets can be deprecated as a part of span streaming. This PR enables
    prefilling data
    for alerts (for orgs who are opted into the flag) and querying the
    prefilled data (for orgs
    that are opted into the flag).

--- src/sentry/incidents/logic.py
+++ src/sentry/incidents/logic.py
@@ -2031,10 +2031,14 @@ def schedule_update_project_config(
     """
     enabled_features = on_demand_metrics_feature_flags(_unpack_organization(alert_rule))
     prefilling = "organizations:on-demand-metrics-prefill" in enabled_features
+    prefilling_for_deprecation = (
+        "organizations:on-demand-gen-metrics-deprecation-prefill" in enabled_features
+    )
     if (
         not projects
         or "organizations:on-demand-metrics-extraction" not in enabled_features
         and not prefilling
+        and not prefilling_for_deprecation
     ):
         return
 
@@ -2045,6 +2049,7 @@ def schedule_update_project_config(
         alert_snuba_query.query,
         None,
         prefilling,
+        prefilling_for_deprecation=prefilling_for_deprecation,
     )
     if should_use_on_demand:
         for project in projects:
