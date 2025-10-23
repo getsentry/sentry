c6e4d5ff345da3bd21f1490e3fb3e8068f6c950a
Scott Cooper <scttcper@gmail.com>
2025-10-23 10:42:32 -0700

    fix(ui): Audit unused admin/prevent deprecated route props (#101750)
    Mostly admin routes, one quick outlet file

--- static/app/views/prevent/index.spec.tsx
+++ static/app/views/prevent/index.spec.tsx
@@ -7,14 +7,20 @@ import PreventPage from 'sentry/views/prevent';
 const PREVENT_FEATURE = 'prevent-ai';
 
 describe('PreventPage', () => {
+  const initialRouterConfig = {
+    location: {
+      pathname: '/prevent/',
+    },
+    route: '/prevent/',
+    children: [{index: true, element: <p>Test content</p>}],
+  };
+
   describe('when the user has access to the feature', () => {
     it('renders the passed children', () => {
-      render(
-        <PreventPage>
-          <p>Test content</p>
-        </PreventPage>,
-        {organization: OrganizationFixture({features: [PREVENT_FEATURE]})}
-      );
+      render(<PreventPage />, {
+        organization: OrganizationFixture({features: [PREVENT_FEATURE]}),
+        initialRouterConfig,
+      });
 
       const testContent = screen.getByText('Test content');
       expect(testContent).toBeInTheDocument();
@@ -23,12 +29,10 @@ describe('PreventPage', () => {
 
   describe('when the user does not have access to the feature', () => {
     it('renders the NoAccess component', () => {
-      render(
-        <PreventPage>
-          <p>Test content</p>
-        </PreventPage>,
-        {organization: OrganizationFixture({features: []})}
-      );
+      render(<PreventPage />, {
+        organization: OrganizationFixture({features: []}),
+        initialRouterConfig,
+      });
 
       const noAccessText = screen.getByText("You don't have access to this feature");
       expect(noAccessText).toBeInTheDocument();
