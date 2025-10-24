31620f14a9eba998b6bc6997998104d824e163e2
Kev <6111995+k-fish@users.noreply.github.com>
2025-10-24 12:51:22 -0400

    fix(ourlogs): Add more logging around autorefresh errors (#102084)
    ### Summary
    This should help catch reasons why users may be having problems with
    autorefresh

--- static/app/views/explore/logs/useVirtualStreaming.tsx
+++ static/app/views/explore/logs/useVirtualStreaming.tsx
@@ -78,6 +78,8 @@ export function useVirtualStreaming({
   const queryKeyString = JSON.stringify(logsQueryKey);
   const previousQueryKeyString = usePrevious(queryKeyString);
 
+  const organizationRef = useRef(organization);
+
   useEffect(() => {
     if (previousQueryKeyString !== queryKeyString) {
       // We reset the virtual timestamp when the query key changes.
@@ -204,6 +206,12 @@ export function useVirtualStreaming({
 
         if (mostRecentPageDataTimestamp === null) {
           warnRef.current();
+          logger.info(
+            'No most recent page data timestamp found, setting auto-refresh to error',
+            {
+              organization: organizationRef.current.slug,
+            }
+          );
           setLogsAutoRefresh('error');
           return;
         }
