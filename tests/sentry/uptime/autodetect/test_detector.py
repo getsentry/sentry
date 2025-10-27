4f6597b25129dafcfe4d250aef76eae300e55e3f
klochek <christopher.klochek@sentry.io>
2025-10-27 10:59:13 -0400

    ref(uptime): move uptime helper functions to utility file (#102105)
    Sorry. But these are functions that have moved around a couple of times
    due to circular dependency issues, so let's just push them into their
    own file. I also looked at most of the major files in uptime to see if
    there were any other obvious candidates to be moved here, but from what
    I can tell, it's relatively tidy.
    
    (I also put back the get_cluster() call I had to inline, in a preceding
    PR.)

--- tests/sentry/uptime/autodetect/test_detector.py
+++ tests/sentry/uptime/autodetect/test_detector.py
@@ -2,13 +2,14 @@ from sentry.models.organization import Organization
 from sentry.testutils.cases import UptimeTestCase
 from sentry.testutils.helpers.options import override_options
 from sentry.uptime.autodetect.detector import autodetect_base_url_for_project
-from sentry.uptime.autodetect.ranking import _get_cluster, get_organization_bucket_key
+from sentry.uptime.autodetect.ranking import get_organization_bucket_key
+from sentry.uptime.utils import get_cluster
 
 
 class DetectBaseUrlForProjectTest(UptimeTestCase):
     def assert_organization_key(self, organization: Organization, exists: bool) -> None:
         key = get_organization_bucket_key(organization)
-        cluster = _get_cluster()
+        cluster = get_cluster()
         assert exists == cluster.sismember(key, str(organization.id))
 
     def test(self) -> None:
