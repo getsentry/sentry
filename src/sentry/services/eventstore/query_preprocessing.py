8255f11fb4ade451c42ec7e1a5dd7de7a73c900f
Charles Paul <charles.paul@sentry.io>
2025-10-27 15:29:54 -0400

    fix(events): Bail out of event-redirect snuba preprocessing
    If we have too many group IDs in a snuba request then Clickhouse can't handle it. We're seeing that (RARELY) in production. Let's bail if we have too many group IDs (which is a VERY rare case). Fallback will just be original group_id, which will miss the merged groups. We can talk about the plan around those later.

--- src/sentry/services/eventstore/query_preprocessing.py
+++ src/sentry/services/eventstore/query_preprocessing.py
@@ -4,6 +4,14 @@ from django.db.models import Q
 
 from sentry.models.groupredirect import GroupRedirect
 
+"""
+Clickhouse can only handle a query of size 131072b. Our IDs are about 10b and there are
+commas & spaces, so each ID ends up ~12b in the query. That gives ~10k possible group
+IDs; we halve here both for breathing room and to ensure that we're still fine if a
+poorly-formatted query has the same list twice.
+"""
+SIZE_THRESHOLD_FOR_CLICKHOUSE = 5000
+
 
 def get_all_merged_group_ids(group_ids: Sequence[str | int]) -> set[str | int]:
     group_id_set = set(group_ids)
@@ -12,4 +20,6 @@ def get_all_merged_group_ids(group_ids: Sequence[str | int]) -> set[str | int]:
     ).values_list("group_id", "previous_group_id")
     for r in all_related_rows:
         group_id_set.update(r)
+        if len(group_id_set) > SIZE_THRESHOLD_FOR_CLICKHOUSE:
+            return set(group_ids)
     return group_id_set
