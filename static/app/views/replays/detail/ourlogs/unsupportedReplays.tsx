53d9e53a3d24ea4b402f9fd1459be864b0b9ce64
Kev <kevan.fisher@sentry.io>
2025-10-27 13:51:43 -0400

    Only cocoa is missing now

--- static/app/views/replays/detail/ourlogs/unsupportedReplays.tsx
+++ static/app/views/replays/detail/ourlogs/unsupportedReplays.tsx
@@ -1,13 +1,7 @@
 /**
  * SDKs that do not yet support logs in replay
  */
-const UNSUPPORTED_REPLAY_LOGS_SDK_NAMES = [
-  'sentry.cocoa',
-  'sentry.java.android',
-  'sentry.cocoa.flutter',
-  'sentry.dart.flutter',
-  'sentry.java.android.flutter',
-] as const;
+const UNSUPPORTED_REPLAY_LOGS_SDK_NAMES = ['sentry.cocoa'] as const;
 
 type UnsupportedSDKName = (typeof UNSUPPORTED_REPLAY_LOGS_SDK_NAMES)[number];
 
