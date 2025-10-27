d60e0bd58f00a002a48973826e8daebb50ce4ca1
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-27 15:33:40 -0400

    Use multimetrics query params provider and add tracemetric

--- static/app/views/explore/metrics/hooks/testUtils.tsx
+++ static/app/views/explore/metrics/hooks/testUtils.tsx
@@ -3,6 +3,7 @@ import type {ReactNode} from 'react';
 import {useLocation} from 'sentry/utils/useLocation';
 import useOrganization from 'sentry/utils/useOrganization';
 import {MetricsQueryParamsProvider} from 'sentry/views/explore/metrics/metricsQueryParams';
+import {MultiMetricsQueryParamsProvider} from 'sentry/views/explore/metrics/multiMetricsQueryParams';
 import {getReadableQueryParamsFromLocation} from 'sentry/views/explore/spans/spansQueryParams';
 
 export function MockMetricQueryParamsContext({children}: {children: ReactNode}) {
@@ -10,13 +11,16 @@ export function MockMetricQueryParamsContext({children}: {children: ReactNode}) 
   const organization = useOrganization();
   const queryParams = getReadableQueryParamsFromLocation(location, organization);
   return (
-    <MetricsQueryParamsProvider
-      queryParams={queryParams}
-      setQueryParams={() => {}}
-      setTraceMetric={() => {}}
-      removeMetric={() => {}}
-    >
-      {children}
-    </MetricsQueryParamsProvider>
+    <MultiMetricsQueryParamsProvider>
+      <MetricsQueryParamsProvider
+        traceMetric={{name: 'mockMetric', type: 'counter'}}
+        queryParams={queryParams}
+        setQueryParams={() => {}}
+        setTraceMetric={() => {}}
+        removeMetric={() => {}}
+      >
+        {children}
+      </MetricsQueryParamsProvider>
+    </MultiMetricsQueryParamsProvider>
   );
 }
