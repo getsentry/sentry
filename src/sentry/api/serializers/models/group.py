396fbab3f43af9c7f013ba4284d72a094b3d141c
Shashank Jarmale <shashank.jarmale@sentry.io>
2025-10-27 15:37:12 -0400

    Auto resolve an unresolved issue only if it wasn't manually unresolved by a user

--- src/sentry/api/serializers/models/group.py
+++ src/sentry/api/serializers/models/group.py
@@ -172,7 +172,10 @@ def _make_group_project_response(project: Project) -> GroupProjectResponse:
 def _get_status_label(group: Group):
     status = group.status
 
-    if status == GroupStatus.UNRESOLVED and group.is_over_resolve_age():
+    # If the issue is UNRESOLVED but has resolved_at set, it means the user manually
+    # unresolved it after it was auto-resolved. We should respect that and not override
+    # the status back to RESOLVED.
+    if status == GroupStatus.UNRESOLVED and group.is_over_resolve_age() and not group.resolved_at:
         # When an issue is over the auto-resolve age but the task has not yet run
         # Only show as auto-resolved if this group type has auto-resolve enabled
         if group.issue_type.enable_auto_resolve:
@@ -463,7 +466,10 @@ class GroupSerializerBase(Serializer, ABC):
                 )
             else:
                 status = GroupStatus.UNRESOLVED
-        if status == GroupStatus.UNRESOLVED and obj.is_over_resolve_age():
+        # If the issue is UNRESOLVED but has resolved_at set, it means the user manually
+        # unresolved it after it was auto-resolved. We should respect that and not override
+        # the status back to RESOLVED.
+        if status == GroupStatus.UNRESOLVED and obj.is_over_resolve_age() and not obj.resolved_at:
             # When an issue is over the auto-resolve age but the task has not yet run
             # Only show as auto-resolved if this group type has auto-resolve enabled
             if obj.issue_type.enable_auto_resolve:
