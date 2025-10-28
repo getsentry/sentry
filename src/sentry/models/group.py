86892b9be38334f36ab8731876f2e87ce7bb2fa2
Shashank Jarmale <shashank.jarmale@sentry.io>
2025-10-28 12:57:46 -0700

    fix(issues): Auto-resolve an issue only if it wasn't manually unresolved by a user (#102160)
    Our `Group` model/serialization logic currently auto-resolves a group if
    it is past its resolve age, even if that group was manually unresolved
    by an issue.
    
    Note that even with this fix the group will still eventually be
    auto-resolved by the background task running as a cron job, but the
    status displayed in the UI will be accurate.
    
    https://sentry.slack.com/archives/C04KZQBNQ2U/p1761589205280039

--- src/sentry/models/group.py
+++ src/sentry/models/group.py
@@ -819,10 +819,14 @@ class Group(Model):
                 if not snooze.is_valid(group=self):
                     status = GroupStatus.UNRESOLVED
 
-        if status == GroupStatus.UNRESOLVED and self.is_over_resolve_age():
+        # If the issue is UNRESOLVED but has resolved_at set, it means the user manually
+        # unresolved it after it was resolved. We should respect that and not override
+        # the status back to RESOLVED.
+        if status == GroupStatus.UNRESOLVED and self.is_over_resolve_age() and not self.resolved_at:
             # Only auto-resolve if this group type has auto-resolve enabled
             if self.issue_type.enable_auto_resolve:
                 return GroupStatus.RESOLVED
+
         return status
 
     def get_share_id(self):
