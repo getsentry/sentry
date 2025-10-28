0a568ece3a87182fcd8eb28f0a9a7db2d8d449b5
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-10-28 12:58:16 +0100

    revert profiling

--- static/app/gettingStartedDocs/ruby/rack/index.tsx
+++ static/app/gettingStartedDocs/ruby/rack/index.tsx
@@ -1,14 +1,14 @@
 import type {Docs} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {CrashReportWebApiOnboarding} from 'sentry/components/onboarding/gettingStartedDoc/utils/feedbackOnboarding';
+import {getRubyProfilingOnboarding} from 'sentry/gettingStartedDocs/ruby/ruby';
 import {getRubyLogsOnboarding} from 'sentry/utils/gettingStartedDocs/ruby';
 
 import {onboarding} from './onboarding';
-import {profiling} from './profiling';
 
 const docs: Docs = {
   onboarding,
   crashReportOnboarding: CrashReportWebApiOnboarding,
-  profilingOnboarding: profiling,
+  profilingOnboarding: getRubyProfilingOnboarding(),
   logsOnboarding: getRubyLogsOnboarding({
     docsPlatform: 'rack',
   }),
