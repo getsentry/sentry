11a43e9d2058b63de305b4d74f91f8c82acb7b26
Lorenzo Cian <17258265+lcian@users.noreply.github.com>
2025-10-29 23:30:35 +0100

    fix(objectstore): Partially revert #102080 (#102354)

--- src/sentry/objectstore/__init__.py
+++ src/sentry/objectstore/__init__.py
@@ -1,7 +1,5 @@
-from sentry import options as options_store
 from sentry.objectstore.service import Client, ClientBuilder
 
 __all__ = ["attachments", "Client", "ClientBuilder"]
 
-options = options_store.get("objectstore.config")
-attachments = ClientBuilder(options["base_url"], "attachments", options)
+attachments = ClientBuilder("attachments")
