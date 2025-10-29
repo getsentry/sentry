f88cd4915906c0ccdd0681ed791ba0ebe3189ff5
Simon Hellmayr <shellmayr@users.noreply.github.com>
2025-10-29 11:28:19 +0100

    feat(dynamic-sampling): add feature flag for dynamic sampling on span first (#102209)
    - Add feature flag `organizations:dynamic-sampling-on-span-first` we
    will use to control availability of dynamic sampling for span first
    
    Closes TET-1326

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -138,6 +138,13 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:dynamic-sampling-custom", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable dynamic sampling minimum sample rate
     manager.add("organizations:dynamic-sampling-minimum-sample-rate", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    # Enables generation of dynamic sampling rules based on span data for span first
+    manager.add(
+        "organizations:dynamic-sampling-on-span-first",
+        OrganizationFeature,
+        FeatureHandlerStrategy.FLAGPOLE,
+        api_expose=False,
+    )
     # Enable logging project config for debugging
     manager.add("organizations:log-project-config", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable archive/escalating issue workflow features in v2
