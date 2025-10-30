0cf03a1b263303d9863b9ae9a5cf06da18ab2c50
Tony Xiao <txiao@sentry.io>
2025-10-30 10:13:55 -0400

    chore(explore): migrate page params hooks to query params (#102365)
    The last 2 hooks using page params are useExplorePageParams and
    useSetExplorePageParams. Time to get off them and onto the respective
    query params hooks.

--- static/app/views/explore/contexts/pageParamsContext/mode.tsx
+++ static/app/views/explore/contexts/pageParamsContext/mode.tsx
@@ -1,6 +1,5 @@
 import type {Location} from 'history';
 
-import {defined} from 'sentry/utils';
 import {decodeScalar} from 'sentry/utils/queryString';
 
 export enum Mode {
@@ -18,18 +17,4 @@ export function getModeFromLocation(location: Location): Mode {
     return Mode.AGGREGATE;
   }
   return defaultMode();
-}
-
-export function updateLocationWithMode(
-  location: Location,
-  mode: Mode | null | undefined
-) {
-  if (defined(mode)) {
-    location.query.mode = mode;
-
-    // make sure to clear the cursor every time the query is updated
-    delete location.query.cursor;
-  } else if (mode === null) {
-    delete location.query.mode;
-  }
 }
