733ec4e4d78c86a2a3dfc3a247cdd5db4f0d9a27
Abdullah Khan <60121741+Abdkhan14@users.noreply.github.com>
2025-10-30 15:04:36 -0400

    feat(explore-suspect-attrs): Updating charts grid (#102417)
    Before (makes pagination usage non-intuitive):
    <img width="1420" height="809" alt="Screenshot 2025-10-30 at 2 35 22 PM"
    src="https://github.com/user-attachments/assets/9b518de4-3c5e-4836-9c85-d9c2ab4146b9"
    />
    
    After: 
    <img width="1223" height="799" alt="Screenshot 2025-10-30 at 2 36 13 PM"
    src="https://github.com/user-attachments/assets/603b2010-07cd-4c70-acc0-80b1ac6af752"
    />
    
    Added a little box-shadow to make trigger discoverable:
    <img width="1063" height="348" alt="Screenshot 2025-10-30 at 2 37 12 PM"
    src="https://github.com/user-attachments/assets/ec3b7119-074d-425e-a2b7-149c5846e213"
    />
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/explore/components/suspectTags/floatingTrigger.tsx
+++ static/app/views/explore/components/suspectTags/floatingTrigger.tsx
@@ -94,9 +94,7 @@ const List = styled('ul')`
   margin: 0;
   padding: 0;
   margin-bottom: 0 !important;
-  box-shadow:
-    rgba(0, 0, 0, 0.05) 0px 6px 24px 0px,
-    rgba(0, 0, 0, 0.08) 0px 0px 0px 1px;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   background: ${p => p.theme.backgroundElevated};
   color: ${p => p.theme.textColor};
   border-radius: ${p => p.theme.borderRadius};
