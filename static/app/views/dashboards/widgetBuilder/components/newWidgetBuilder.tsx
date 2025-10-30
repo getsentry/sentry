228fcde3dcaa55ffe4d0049edd7ebe5e284d54cf
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-10-30 15:38:49 -0400

    Unroll transform changes

--- static/app/views/dashboards/widgetBuilder/components/newWidgetBuilder.tsx
+++ static/app/views/dashboards/widgetBuilder/components/newWidgetBuilder.tsx
@@ -332,9 +332,9 @@ export function WidgetPreviewContainer({
   };
 
   const animatedProps: MotionNodeAnimationOptions = {
-    initial: {opacity: 0, transform: 'translateX(100%) translateY(0)'},
-    animate: {opacity: 1, transform: 'translateX(0) translateY(0)'},
-    exit: {opacity: 0, transform: 'translateX(100%) translateY(0)'},
+    initial: {opacity: 0, x: '100%', y: 0},
+    animate: {opacity: 1, x: 0, y: 0},
+    exit: {opacity: 0, x: '100%', y: 0},
     transition: transitionSettings,
   };
 
