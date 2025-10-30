338e840695fbb394d81fd4c6b913a3badd11e8c7
Charles Paul <charles.paul@sentry.io>
2025-10-30 12:59:09 -0400

    ref(eventstream): Instrument snuba pre-processing (#102394)
    We're interested in being able to see how many group IDs are (1)
    generated by this function and (2) returned by this function in the
    event of another crash.
    
    This PR adds a span with the appropriate data.

--- src/sentry/services/eventstore/query_preprocessing.py
+++ src/sentry/services/eventstore/query_preprocessing.py
@@ -1,5 +1,6 @@
 from collections.abc import Sequence
 
+import sentry_sdk
 from django.db.models import Q, QuerySet
 
 from sentry.models.groupredirect import GroupRedirect
@@ -24,14 +25,29 @@ def _get_all_related_redirects_query(
         # the oldest redirects if we have >THRESHOLD. We choose to drop the oldest
         # because they're least likely to have data in retention
         .order_by("-date_added")
-    )[:SIZE_THRESHOLD_FOR_CLICKHOUSE]
+    )
 
 
 def get_all_merged_group_ids(group_ids: Sequence[str | int]) -> set[str | int]:
-    group_id_set = set(group_ids)
-    all_related_rows = _get_all_related_redirects_query(group_id_set)
-    for r in all_related_rows:
-        group_id_set.update(r)
-        if len(group_id_set) > SIZE_THRESHOLD_FOR_CLICKHOUSE:
-            return set(group_ids)
-    return group_id_set
+    with sentry_sdk.start_span(op="get_all_merged_group_ids") as span:
+        group_id_set = set(group_ids)
+        all_related_rows = _get_all_related_redirects_query(group_id_set)
+
+        out = None
+
+        for r in all_related_rows:
+            group_id_set.update(r)
+
+            if len(group_id_set) >= SIZE_THRESHOLD_FOR_CLICKHOUSE:
+                # Because we're incrementing the size of group_id_set by either one or two
+                # each iteration, it's fine if we're a bit over. That's negligible compared
+                # to the scale-of-thousands Clickhouse threshold.
+                out = group_id_set.copy()
+
+        if out is None:
+            out = group_id_set
+
+        span.set_data("true_group_id_len", len(group_id_set))
+        span.set_data("returned_group_id_len", len(out))
+
+    return out
