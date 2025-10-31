d1136bc89ab28ecf3fcfd5ea14c50731ab41708a
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-10-31 08:17:38 +0100

    ref(onboarding): Split go onboarding docs (#102384)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/go/negroni/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/go/negroni/onboarding.spec.tsx
@@ -4,9 +4,9 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {ProductSolution} from 'sentry/components/onboarding/gettingStartedDoc/types';
 
-import docs from './echo';
+import docs from '.';
 
-describe('echo onboarding docs', () => {
+describe('negroni onboarding docs', () => {
   it('renders errors onboarding docs correctly', () => {
     renderWithOnboardingLayout(docs);
 
