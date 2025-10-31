97d61b3c9285516953c5454c2baeb884cf8ff022
Cathy Teng <cathy.teng@sentry.io>
2025-10-31 11:42:37 -0700

    fix import

--- tests/sentry/projects/project_rules/test_creator.py
+++ tests/sentry/projects/project_rules/test_creator.py
@@ -7,9 +7,6 @@ from sentry.projects.project_rules.creator import ProjectRuleCreator
 from sentry.testutils.cases import TestCase
 from sentry.testutils.helpers.features import with_feature
 from sentry.types.actor import Actor
-from sentry.workflow_engine.migration_helpers.issue_alert_migration import (
-    UnableToAcquireLockApiError,
-)
 from sentry.workflow_engine.models import (
     Action,
     AlertRuleDetector,
@@ -19,6 +16,7 @@ from sentry.workflow_engine.models import (
 )
 from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.models.detector import Detector
+from sentry.workflow_engine.processors.detector import UnableToAcquireLockApiError
 from sentry.workflow_engine.typings.grouptype import IssueStreamGroupType
 
 
