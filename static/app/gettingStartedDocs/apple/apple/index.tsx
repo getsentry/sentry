c19f5d5f2e03fb9c48013167c571ae57de6f4ac7
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-10-31 08:17:49 +0100

    ref(onboarding): Split apple onboarding docs (#102382)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/apple/apple/index.tsx
+++ static/app/gettingStartedDocs/apple/apple/index.tsx
@@ -1,4 +1,4 @@
-import docs from './macos';
+import docs from 'sentry/gettingStartedDocs/apple/macos';
 
 // apple is the legacy platform key for apple-macos
 // So we simply re-export the docs from apple-macos
