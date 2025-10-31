9bfc75d7675ed705377c6f70be6d907f83f31e3d
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2025-10-31 10:17:01 -0700

    fix metric issue links, we shuold probably change this to use the type id in a reference table to get the different information, then pass along specific IDs

--- static/app/views/alerts/pathnames.tsx
+++ static/app/views/alerts/pathnames.tsx
@@ -1,16 +1,21 @@
 import type {Organization} from 'sentry/types/organization';
 import normalizeUrl from 'sentry/utils/url/normalizeUrl';
 
-const ALERTS_BASE_PATHNAME = 'issues/alerts';
+const ALERTS_BASE_PATHNAME = '/issues/alerts';
 
 export function makeAlertsPathname({
   path,
   organization,
+  useLegacyBasePath = true,
 }: {
   organization: Organization;
   path: '/' | `/${string}/`;
+  useLegacyBasePath?: boolean;
 }) {
-  return normalizeUrl(
-    `/organizations/${organization.slug}/${ALERTS_BASE_PATHNAME}${path}`
-  );
+  let basePath = '';
+  if (useLegacyBasePath) {
+    basePath = ALERTS_BASE_PATHNAME;
+  }
+
+  return normalizeUrl(`/organizations/${organization.slug}${basePath}${path}`);
 }
--- static/app/views/issueDetails/streamline/sidebar/detectorSection.tsx
+++ static/app/views/issueDetails/streamline/sidebar/detectorSection.tsx
@@ -34,18 +34,40 @@ export function getDetectorDetails({
    * for Alert Rule IDs. Hopefully we can consolidate this when we move to the detector system.
    * Ideally, this function wouldn't even check the event, but rather the group/issue.
    */
-  const metricAlertRuleId =
-    event?.occurrence?.evidenceData?.alertId ||
-    event?.contexts?.metric_alert?.alert_rule_id;
-  if (metricAlertRuleId) {
+  const isMetricAlert =
+    !!event?.occurrence?.evidenceData?.alertId ||
+    !!event?.contexts?.metric_alert?.alert_rule_id ||
+    event?.occurrence?.type === 8001; // the issue type for metric issues is 8001
+
+  if (isMetricAlert) {
+    const metricAlertRuleId =
+      event?.occurrence?.evidenceData?.alertId ||
+      event?.contexts?.metric_alert?.alert_rule_id;
+
+    if (metricAlertRuleId) {
+      return {
+        detectorType: 'metric_alert',
+        detectorId: metricAlertRuleId,
+        detectorPath: makeAlertsPathname({
+          path: `/rules/details/${metricAlertRuleId}/`,
+          organization,
+        }),
+        // TODO(issues): We can probably enrich this description with details from the alert itself.
+        description: t(
+          'This issue was created by a metric alert detector. View the detector details to learn more.'
+        ),
+      };
+    }
+
+    const detectorId = event.occurrence?.evidenceData.detectorId;
     return {
       detectorType: 'metric_alert',
-      detectorId: metricAlertRuleId,
+      detectorId,
       detectorPath: makeAlertsPathname({
-        path: `/rules/details/${metricAlertRuleId}/`,
+        path: `/monitors/${detectorId}/`,
         organization,
+        useLegacyBasePath: false,
       }),
-      // TODO(issues): We can probably enrich this description with details from the alert itself.
       description: t(
         'This issue was created by a metric alert detector. View the detector details to learn more.'
       ),
