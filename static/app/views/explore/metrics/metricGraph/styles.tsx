5de6ce6b2e3ff867f86e9acd117a06c348d933b6
Nar Saynorath <nar.saynorath@sentry.io>
2025-10-31 13:44:59 -0400

    Add comment for style override

--- static/app/views/explore/metrics/metricGraph/styles.tsx
+++ static/app/views/explore/metrics/metricGraph/styles.tsx
@@ -3,6 +3,9 @@ import styled from '@emotion/styled';
 
 import {FooterWrapper} from 'sentry/views/dashboards/widgets/widget/widget';
 
+// hideFooterBorder is used to hide the top border of the footer when
+// the aggregates/samples tables are on the bottom. This is so the footer
+// maintains its visual relationship with the graph and not the tables.
 export const WidgetWrapper = styled('div')<{hideFooterBorder?: boolean}>`
   height: 100%;
   ${p =>
