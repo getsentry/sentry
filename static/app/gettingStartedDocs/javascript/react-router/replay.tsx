43ab4a67309ffebb26c0b049978d54bee91ddad3
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-03 12:38:07 +0100

    ref(onboarding): Split react router onboarding docs

---
+++ static/app/gettingStartedDocs/javascript/react-router/replay.tsx
@@ -0,0 +1,37 @@
+import type {
+  DocsParams,
+  OnboardingConfig,
+} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {
+  getReplayConfigureDescription,
+  getReplayVerifyStep,
+} from 'sentry/components/onboarding/gettingStartedDoc/utils/replayOnboarding';
+
+import {getClientSetupSnippet} from './utils';
+import {onboarding} from './onboarding';
+
+
+export const replay: OnboardingConfig = {
+  install: (params: DocsParams) => onboarding.install(params),
+  configure: (params: DocsParams) => [
+    {
+      type: StepType.CONFIGURE,
+      content: [
+        {
+          type: 'text',
+          text: getReplayConfigureDescription({
+            link: 'https://docs.sentry.io/platforms/javascript/guides/react-router/session-replay/',
+          }),
+        },
+        {
+          type: 'code',
+          language: 'tsx',
+          code: getClientSetupSnippet(params),
+        },
+      ],
+    },
+  ],
+  verify: getReplayVerifyStep(),
+  nextSteps: () => [],
+};
