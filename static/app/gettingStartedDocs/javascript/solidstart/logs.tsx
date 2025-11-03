43d2de77f511bbf40fd2e541e371a456bf72f099
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-03 12:40:29 +0100

    ref(onboarding): Split solid start onboarding docs

---
+++ static/app/gettingStartedDocs/javascript/solidstart/logs.tsx
@@ -0,0 +1,6 @@
+import {getJavascriptLogsFullStackOnboarding} from 'sentry/utils/gettingStartedDocs/javascript';
+
+export const logs = getJavascriptLogsFullStackOnboarding({
+  docsPlatform: 'solidstart',
+  packageName: '@sentry/solidstart',
+});
