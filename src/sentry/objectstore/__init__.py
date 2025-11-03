6926b7d3a250cb72c6dcb7c1d2a64fcff2f639b0
Arpad Borsos <arpad.borsos@sentry.io>
2025-11-03 11:57:24 +0100

    pass correct constructor options

--- src/sentry/objectstore/__init__.py
+++ src/sentry/objectstore/__init__.py
@@ -1,5 +1,24 @@
-from objectstore_client import Client, ClientBuilder, ClientError
+from datetime import timedelta
 
-__all__ = ["attachments", "Client", "ClientBuilder", "ClientError"]
+from objectstore_client import Client, ClientBuilder, ClientError, TimeToLive
 
-attachments = ClientBuilder("attachments")
+from sentry.utils import metrics
+
+__all__ = ["get_attachments_client", "Client", "ClientBuilder", "ClientError"]
+
+_attachments_client: ClientBuilder | None = None
+
+
+def get_attachments_client() -> ClientBuilder:
+    global _attachments_client
+    if not _attachments_client:
+        from sentry import options as options_store
+
+        options = options_store.get("objectstore.config")
+        _attachments_client = ClientBuilder(
+            options["base_url"],
+            "attachments",
+            metrics_backend=metrics,
+            default_expiration_policy=TimeToLive(timedelta(days=30)),
+        )
+    return _attachments_client
