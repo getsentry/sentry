38802c8d1893f24fb71278f34beae6d1729c881b
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-03 12:37:16 +0100

    ref(onboarding): Split nuxt onboarding docs

---
+++ static/app/gettingStartedDocs/javascript/nuxt/agentMonitoring.tsx
@@ -0,0 +1,6 @@
+import {getNodeAgentMonitoringOnboarding} from 'sentry/utils/gettingStartedDocs/node';
+
+export const agentMonitoring = getNodeAgentMonitoringOnboarding({
+  packageName: '@sentry/nuxt',
+  configFileName: 'sentry.server.config.ts',
+});
