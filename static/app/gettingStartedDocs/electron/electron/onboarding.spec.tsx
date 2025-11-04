be11ed0c0abed355d1f3b7c5fd49013a341461b1
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-04 09:57:47 +0100

    ref(onboarding): Split electron onboarding docs (#102548)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/electron/electron/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/electron/electron/onboarding.spec.tsx
@@ -1,7 +1,7 @@
 import {renderWithOnboardingLayout} from 'sentry-test/onboarding/renderWithOnboardingLayout';
 import {screen} from 'sentry-test/reactTestingLibrary';
 
-import docs from './electron';
+import docs from '.';
 
 describe('electron onboarding docs', () => {
   it('renders docs correctly', () => {
