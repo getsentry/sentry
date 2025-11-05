7300538d5e08c08d541fb195579288d01f6502ea
Tony Le <tony.le@sentry.io>
2025-11-05 14:00:12 -0500

    ref(producer): Remove rollout option for ConfluentProducer (#102797)
    Since ConfluentProducer are rolled out in s4s, de, and us without any
    errors, next step is rolling out to 100%.
    
    Refs STREAM-549

--- tests/sentry/utils/test_confluent_producer.py
+++ tests/sentry/utils/test_confluent_producer.py
@@ -1,11 +1,9 @@
 from arroyo.backends.kafka import ConfluentProducer
 from confluent_kafka import Producer
 
-from sentry.testutils.helpers.options import override_options
 from sentry.utils.confluent_producer import get_confluent_producer
 
 
-@override_options({"arroyo.producer.confluent-producer-rollout": {"test_producer": True}})
 def test_get_confluent_producer() -> None:
     configuration = {
         "bootstrap.servers": "localhost:9092",
@@ -20,26 +18,3 @@ def test_get_confluent_producer() -> None:
 
     for attrs in Producer.__dict__.keys():
         assert hasattr(producer, attrs)
-
-
-@override_options({"arroyo.producer.confluent-producer-rollout": {"test_producer": False}})
-def test_get_confluent_producer_not_rolled_out() -> None:
-    configuration = {
-        "bootstrap.servers": "localhost:9092",
-        "client.id": "test_producer",
-    }
-
-    producer = get_confluent_producer(configuration)
-    assert isinstance(producer, Producer)
-    assert not isinstance(producer, ConfluentProducer)
-
-
-@override_options({"arroyo.producer.confluent-producer-rollout": {}})
-def test_get_confluent_producer_no_client_id() -> None:
-    configuration = {
-        "bootstrap.servers": "localhost:9092",
-    }
-
-    producer = get_confluent_producer(configuration)
-    assert isinstance(producer, Producer)
-    assert not isinstance(producer, ConfluentProducer)
