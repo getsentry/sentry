aa6d877e8c1c673345842762926caefac963b000
Tony Xiao <txiao@sentry.io>
2025-11-05 15:06:15 -0500

    feat(tracemetrics): Support filtering metrics as optional (#102807)
    We should still allow queries on trace metrics without a top level
    metric and add metric unit as a parameter.

--- src/sentry/api/endpoints/organization_events.py
+++ src/sentry/api/endpoints/organization_events.py
@@ -531,11 +531,12 @@ class OrganizationEventsEndpoint(OrganizationEventsV2EndpointBase):
                     )
                 elif scoped_dataset == TraceMetrics:
                     # tracemetrics uses aggregate conditions
-                    metric_name, metric_type = get_trace_metric_from_request(request, organization)
+                    metric_name, metric_type, metric_unit = get_trace_metric_from_request(request)
 
                     return TraceMetricsSearchResolverConfig(
                         metric_name=metric_name,
                         metric_type=metric_type,
+                        metric_unit=metric_unit,
                         use_aggregate_conditions=use_aggregate_conditions,
                         auto_fields=True,
                         disable_aggregate_extrapolation=disable_aggregate_extrapolation,
