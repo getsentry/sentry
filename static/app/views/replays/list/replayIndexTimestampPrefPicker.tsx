4543a70fb6454bfe8bb482ed483cad1bf6e44d90
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-11-05 07:44:05 -0800

    ref(replay): Move the timestamp-preference-picker into the Replay Index header (#102772)
    I noticed some weird line-breaks at different window sizes with the
    timestamp picker thing.
    
    So I moved it up into the header which gives back a little more space
    for search again.
    
    **Before:**
    <img width="914" height="218" alt="SCR-20251105-tjhu"
    src="https://github.com/user-attachments/assets/11bdb089-fc26-426b-980f-f61e52b274a6"
    />
    
    
    **After:**
    <img width="916" height="179" alt="SCR-20251105-tjim"
    src="https://github.com/user-attachments/assets/cff78f30-f1f6-4a56-8285-3f263f19c5cc"
    />

---
+++ static/app/views/replays/list/replayIndexTimestampPrefPicker.tsx
@@ -0,0 +1,36 @@
+import {Button} from 'sentry/components/core/button';
+import {CompactSelect} from 'sentry/components/core/compactSelect';
+import {REPLAY_TIMESTAMP_OPTIONS} from 'sentry/components/replays/preferences/replayPreferences';
+import {IconSettings} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {useReplayPrefs} from 'sentry/utils/replays/playback/providers/replayPreferencesContext';
+import {toTitleCase} from 'sentry/utils/string/toTitleCase';
+
+export default function ReplayIndexTimestampPrefPicker() {
+  const [prefs, setPrefs] = useReplayPrefs();
+
+  return (
+    <CompactSelect
+      options={[
+        {
+          key: t('Timestamps'),
+          label: t('Timestamps'),
+          options: REPLAY_TIMESTAMP_OPTIONS.map(option => ({
+            label: `${toTitleCase(option)}`,
+            value: option,
+            key: option,
+          })),
+        },
+      ]}
+      trigger={triggerProps => (
+        <Button
+          {...triggerProps}
+          icon={<IconSettings />}
+          aria-label={t('Configure timestamp settings')}
+        />
+      )}
+      value={prefs.timestampType}
+      onChange={opt => setPrefs({timestampType: opt.value})}
+    />
+  );
+}
