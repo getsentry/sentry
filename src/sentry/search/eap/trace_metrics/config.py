04140a7967e05c03bdf8e13ab039cda1f042dfd3
Tony Xiao <txiao@sentry.io>
2025-11-05 15:06:15 -0500

    feat(tracemetrics): Support filtering metrics as optional (#102807)
    We should still allow queries on trace metrics without a top level
    metric and add metric unit as a parameter.

--- src/sentry/search/eap/trace_metrics/config.py
+++ src/sentry/search/eap/trace_metrics/config.py
@@ -1,7 +1,6 @@
 from dataclasses import dataclass
 from typing import Literal, cast
 
-from rest_framework.exceptions import ErrorDetail, ValidationError
 from rest_framework.request import Request
 from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeKey, AttributeValue
 from sentry_protos.snuba.v1.trace_item_filter_pb2 import (
@@ -10,8 +9,6 @@ from sentry_protos.snuba.v1.trace_item_filter_pb2 import (
     TraceItemFilter,
 )
 
-from sentry import features
-from sentry.models.organization import Organization
 from sentry.search.eap.resolver import SearchResolver
 from sentry.search.eap.types import SearchResolverConfig
 
@@ -20,8 +17,9 @@ MetricType = Literal["counter", "gauge", "distribution"]
 
 @dataclass(frozen=True, kw_only=True)
 class TraceMetricsSearchResolverConfig(SearchResolverConfig):
-    metric_name: str
-    metric_type: MetricType
+    metric_name: str | None
+    metric_type: MetricType | None
+    metric_unit: str | None
 
     def extra_conditions(self, search_resolver: SearchResolver) -> TraceItemFilter | None:
         if not self.metric_name or not self.metric_type:
@@ -35,24 +33,38 @@ class TraceMetricsSearchResolverConfig(SearchResolverConfig):
         if not isinstance(metric_type.proto_definition, AttributeKey):
             raise ValueError("Unable to resolve metric.type")
 
-        metric_name_filter = TraceItemFilter(
-            comparison_filter=ComparisonFilter(
-                key=metric_name.proto_definition,
-                op=ComparisonFilter.OP_EQUALS,
-                value=AttributeValue(val_str=self.metric_name),
-            )
-        )
-        metric_type_filter = TraceItemFilter(
-            comparison_filter=ComparisonFilter(
-                key=metric_type.proto_definition,
-                op=ComparisonFilter.OP_EQUALS,
-                value=AttributeValue(val_str=self.metric_type),
-            )
-        )
+        filters = [
+            TraceItemFilter(
+                comparison_filter=ComparisonFilter(
+                    key=metric_name.proto_definition,
+                    op=ComparisonFilter.OP_EQUALS,
+                    value=AttributeValue(val_str=self.metric_name),
+                )
+            ),
+            TraceItemFilter(
+                comparison_filter=ComparisonFilter(
+                    key=metric_type.proto_definition,
+                    op=ComparisonFilter.OP_EQUALS,
+                    value=AttributeValue(val_str=self.metric_type),
+                )
+            ),
+        ]
 
-        return TraceItemFilter(
-            and_filter=AndFilter(filters=[metric_name_filter, metric_type_filter])
-        )
+        if self.metric_unit:
+            metric_unit, _ = search_resolver.resolve_column("metric.unit")
+            if not isinstance(metric_unit.proto_definition, AttributeKey):
+                raise ValueError("Unable to resolve metric.unit")
+            filters.append(
+                TraceItemFilter(
+                    comparison_filter=ComparisonFilter(
+                        key=metric_unit.proto_definition,
+                        op=ComparisonFilter.OP_EQUALS,
+                        value=AttributeValue(val_str=self.metric_unit),
+                    )
+                )
+            )
+
+        return TraceItemFilter(and_filter=AndFilter(filters=filters))
 
 
 ALLOWED_METRIC_TYPES: list[MetricType] = ["counter", "gauge", "distribution"]
@@ -60,34 +72,16 @@ ALLOWED_METRIC_TYPES: list[MetricType] = ["counter", "gauge", "distribution"]
 
 def get_trace_metric_from_request(
     request: Request,
-    organization: Organization,
-) -> tuple[str, MetricType]:
+) -> tuple[str | None, MetricType | None, str | None]:
     metric_name = request.GET.get("metricName")
     metric_type = request.GET.get("metricType")
+    metric_unit = request.GET.get("metricUnit")
 
-    if not features.has(
-        "organizations:tracemetrics-top-level-params", organization=organization, actor=request.user
-    ):
-        if not metric_name:
-            metric_name = ""
-        if not metric_type:
-            metric_type = ""
-    else:
-        errors = {}
+    if not metric_name:
+        metric_name = None
+    if not metric_type:
+        metric_type = None
+    if not metric_unit:
+        metric_unit = None
 
-        if not metric_name:
-            errors["metricName"] = ErrorDetail("This field is required.", code="required")
-
-        if not metric_type:
-            errors["metricType"] = ErrorDetail("This field is required.", code="required")
-        elif metric_type not in ALLOWED_METRIC_TYPES:
-            errors["metricType"] = ErrorDetail(
-                string=f'"{metric_type}" is not a valid choice.', code="invalid_choice"
-            )
-
-        if errors:
-            raise ValidationError(errors)
-
-        assert metric_name
-
-    return metric_name, cast(MetricType, metric_type)
+    return metric_name, cast(MetricType | None, metric_type), metric_unit
