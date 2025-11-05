ff859a3a8df4858e08153a4ae9d37b281c0dca89
Jonas <jonas.badalic@sentry.io>
2025-11-05 08:56:00 -0800

    options: enforce UI2 for users without a preference (#102686)
    Enforce folks into UI2 when force flag is active, and if they have not
    indicated a preference

--- tests/js/fixtures/user.ts
+++ tests/js/fixtures/user.ts
@@ -16,7 +16,7 @@ export function UserFixture(params: Partial<User> = {}): User {
       avatarType: 'letter_avatar',
       stacktraceOrder: -1,
       prefersIssueDetailsStreamlinedUI: true,
-      prefersChonkUI: false,
+      prefersChonkUI: null,
     },
     ip_address: '127.0.0.1',
     hasPasswordAuth: true,
