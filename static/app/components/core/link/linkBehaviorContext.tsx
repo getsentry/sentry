9e28155f37df374c36e86d0539aaf40ded3a55e9
TkDodo <dominik.dorfmeister@sentry.io>
2025-11-05 18:29:27 +0100

    ref: log if there is no linkBehaviorContext

--- static/app/components/core/link/linkBehaviorContext.tsx
+++ static/app/components/core/link/linkBehaviorContext.tsx
@@ -1,20 +1,30 @@
 import {createContext, useContext, type FunctionComponent} from 'react';
 import {Link as RouterLink} from 'react-router-dom';
+import * as Sentry from '@sentry/react';
 
 import type {LinkProps} from './link';
 
-const LinkBehaviorContext = createContext<{
+type LinkBehavior = {
   behavior: (props: LinkProps) => LinkProps;
   component: FunctionComponent<LinkProps>;
-}>({
+};
+
+const LinkBehaviorContext = createContext<LinkBehavior | null>(null);
+
+const defaultLinkBeahvior = {
   component: RouterLink,
   behavior: props => props,
-});
+} satisfies LinkBehavior;
 
 export const LinkBehaviorContextProvider = LinkBehaviorContext.Provider;
 
 export const useLinkBehavior = (props: LinkProps) => {
-  const {component, behavior} = useContext(LinkBehaviorContext);
+  const linkBehavior = useContext(LinkBehaviorContext);
+
+  if (!linkBehavior) {
+    Sentry.logger.warn('LinkBehaviorContext not found');
+  }
+  const {component, behavior} = linkBehavior ?? defaultLinkBeahvior;
 
   return {Component: component, behavior: () => behavior(props)};
 };
