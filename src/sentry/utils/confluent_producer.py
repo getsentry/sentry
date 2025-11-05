e9f19586fdb09f47576a62528c592f6fa7de82f5
Tony Le <tony.le@sentry.io>
2025-11-05 13:20:26 -0500

    ref(producer): Remove rollout option for ConfluentProducer

--- src/sentry/utils/confluent_producer.py
+++ src/sentry/utils/confluent_producer.py
@@ -2,10 +2,7 @@ from __future__ import annotations
 
 from typing import Any
 
-from arroyo.backends.kafka import ConfluentProducer
 from confluent_kafka import Producer
-
-from sentry import options
 
 
 def get_confluent_producer(
@@ -20,14 +17,4 @@ def get_confluent_producer(
     Returns:
         confluent_kafka Producer
     """
-    rollout_config = options.get("arroyo.producer.confluent-producer-rollout", {})
-    name = configuration.get("client.id")
-
-    # f"sentry.sentry_metrics.slicing_router.{current_sliceable}.{current_slice_id}"
-    if name is not None and isinstance(name, str):
-        if name.startswith("sentry.sentry_metrics.slicing_router"):
-            name = "sentry.sentry_metrics.slicing_router"
-
-    if rollout_config.get(name, False):
-        return ConfluentProducer(configuration)
     return Producer(configuration)
