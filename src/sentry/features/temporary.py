a074ec3963c82f1415b8bec588be24372dcb9eb6
Mihir Mavalankar <mihir.d.mavalankar@gmail.com>
2025-11-05 11:50:21 -0800

    feat(triage signals): Add feature flag

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -371,6 +371,8 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:seer-coding-agent-integrations", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable Seer Explorer panel for AI-powered data exploration
     manager.add("organizations:seer-explorer", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    # Enbale Triage signals V0 for AI powered issue classifiaction in sentry
+    manager.add("organizations:triage-signals-v0", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enable search query builder boolean operator select feature
     manager.add("organizations:search-query-builder-add-boolean-operator-select", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable search query builder case insensitivity features
