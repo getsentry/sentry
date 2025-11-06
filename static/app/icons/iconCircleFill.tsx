69f4c995e39444cb3c8773afbd531efe5a7248ed
Jesse Box <me@jessebox.net>
2025-11-06 15:35:47 +0100

    Modified: path for 'iconCircleFill'

--- static/app/icons/iconCircleFill.tsx
+++ static/app/icons/iconCircleFill.tsx
@@ -1,3 +1,5 @@
+import {useTheme} from '@emotion/react';
+
 import type {SVGIconProps} from './svgIcon';
 import {SvgIcon} from './svgIcon';
 
@@ -5,9 +7,14 @@ import {SvgIcon} from './svgIcon';
  * @deprecated This icon will be removed in new UI.
  */
 export function IconCircleFill(props: SVGIconProps) {
+  const theme = useTheme();
   return (
-    <SvgIcon {...props} viewBox="0 0 24 24" kind="path">
-      <circle cx="12" cy="12" r="10" />
+    <SvgIcon {...props} kind="path">
+      {theme.isChonk ? (
+        <path d="M8 0C12.4183 0 16 3.58172 16 8C16 12.4183 12.4183 16 8 16C3.58172 16 0 12.4183 0 8C2.57711e-07 3.58172 3.58172 2.57702e-07 8 0Z" />
+      ) : (
+        <circle cx="12" cy="12" r="10" />
+      )}
     </SvgIcon>
   );
 }
