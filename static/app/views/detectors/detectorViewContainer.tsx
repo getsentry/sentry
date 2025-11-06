7d2e2d76e9c015ae415e4727df14debd17bcf302
Malachi Willey <malwilley@gmail.com>
2025-11-06 10:15:11 -0800

    Fix test

--- static/app/views/detectors/detectorViewContainer.tsx
+++ static/app/views/detectors/detectorViewContainer.tsx
@@ -3,7 +3,7 @@ import {Outlet} from 'react-router-dom';
 import PageFiltersContainer from 'sentry/components/organizations/pageFilters/container';
 import {useWorkflowEngineFeatureGate} from 'sentry/components/workflowEngine/useWorkflowEngineFeatureGate';
 
-export default function MonitorViewContainer() {
+export default function DetectorViewContainer() {
   useWorkflowEngineFeatureGate({redirect: true});
 
   return (
