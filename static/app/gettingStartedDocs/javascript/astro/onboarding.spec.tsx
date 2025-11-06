d098dcdc9e793ae2f394cb6e22630460d76adb77
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-06 14:26:37 +0100

    ref(onboarding): Split Astro onboarding docs (#102559)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/javascript/astro/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/javascript/astro/onboarding.spec.tsx
@@ -4,7 +4,7 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {ProductSolution} from 'sentry/components/onboarding/gettingStartedDoc/types';
 
-import docs from './astro';
+import docs from '.';
 
 describe('javascript-astro onboarding docs', () => {
   it('renders onboarding docs correctly', () => {
