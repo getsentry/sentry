47ba0f6e94b3f4d2c556a89e5f1a12200ddf1d3a
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-06 08:51:04 +0100

    ref(onboarding): Split Angular onboarding docs (#102555)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

---
+++ static/app/gettingStartedDocs/javascript/angular/index.tsx
@@ -0,0 +1,23 @@
+import type {Docs} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {featureFlag} from 'sentry/gettingStartedDocs/javascript/javascript/featureFlag';
+
+import {crashReport} from './crashReport';
+import {feedback} from './feedback';
+import {logs} from './logs';
+import {onboarding} from './onboarding';
+import {profiling} from './profiling';
+import {replay} from './replay';
+import {platformOptions, type PlatformOptions} from './utils';
+
+const docs: Docs<PlatformOptions> = {
+  onboarding,
+  feedbackOnboardingNpm: feedback,
+  replayOnboarding: replay,
+  crashReportOnboarding: crashReport,
+  platformOptions,
+  profilingOnboarding: profiling,
+  featureFlagOnboarding: featureFlag,
+  logsOnboarding: logs,
+};
+
+export default docs;
