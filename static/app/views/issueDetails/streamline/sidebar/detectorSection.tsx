70228f8ba2df03c6a37385916740c8036963f6ec
Malachi Willey <malwilley@gmail.com>
2025-11-06 13:27:20 -0800

    fix(aci): Better naming for metric monitors on issue details page (#102892)

--- static/app/views/issueDetails/streamline/sidebar/detectorSection.tsx
+++ static/app/views/issueDetails/streamline/sidebar/detectorSection.tsx
@@ -55,7 +55,7 @@ export function getDetectorDetails({
         }),
         // TODO(issues): We can probably enrich this description with details from the alert itself.
         description: t(
-          'This issue was created by a metric alert detector. View the detector details to learn more.'
+          'This issue was created by a metric monitor. View the monitor details to learn more.'
         ),
       };
     }
@@ -66,7 +66,7 @@ export function getDetectorDetails({
       detectorId,
       detectorPath: makeMonitorDetailsPathname(organization.slug, detectorId),
       description: t(
-        'This issue was created by a metric alert detector. View the detector details to learn more.'
+        'This issue was created by a metric monitor. View the monitor details to learn more.'
       ),
     };
   }
