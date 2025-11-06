b97b45bf5419c3fe748dad75170125b425930473
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-06 12:08:08 +0100

    ref(onboarding): Split solid onboarding docs (#102569)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/javascript/solid/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/javascript/solid/onboarding.spec.tsx
@@ -4,7 +4,7 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {ProductSolution} from 'sentry/components/onboarding/gettingStartedDoc/types';
 
-import docs from './solid';
+import docs from '.';
 
 describe('javascript-solid onboarding docs', () => {
   it('renders onboarding docs correctly', () => {
