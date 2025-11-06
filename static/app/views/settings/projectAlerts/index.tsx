808eaed6df3936a27ea0297ad94e97acf8d76c52
Shashank Jarmale <shashank.jarmale@sentry.io>
2025-11-06 13:01:04 -0800

    ref(settings): Remove usages of `deprecatedRouteProps` from project settings views (#102709)
    Migrates project settings views off of `deprecatedRouteProps`.
    
    
    https://www.notion.so/sentry/Frontend-TSC-Project-Remove-all-uses-of-deprecatedRouteProps-true-26a8b10e4b5d8015a6a2dd14f9d41dd7

--- static/app/views/settings/projectAlerts/index.tsx
+++ static/app/views/settings/projectAlerts/index.tsx
@@ -1,29 +1,33 @@
-import {cloneElement, Fragment, isValidElement} from 'react';
+import {Fragment} from 'react';
+import {Outlet, useOutletContext} from 'react-router-dom';
 
 import Access from 'sentry/components/acl/access';
-import type {RouteComponentProps} from 'sentry/types/legacyReactRouter';
-import type {Organization} from 'sentry/types/organization';
 import type {Project} from 'sentry/types/project';
+import {useProjectSettingsOutlet} from 'sentry/views/settings/project/projectSettingsLayout';
 
-interface Props extends RouteComponentProps<{organizationId: string; projectId: string}> {
-  children: React.ReactNode;
-  organization: Organization;
+type ProjectAlertsOutletContext = {
+  canEditRule: boolean;
   project: Project;
+};
+
+function ProjectAlertsOutlet(props: ProjectAlertsOutletContext) {
+  return <Outlet context={props} />;
 }
 
-function ProjectAlerts({children, project}: Props) {
+export function useProjectAlertsOutlet() {
+  return useOutletContext<ProjectAlertsOutletContext>();
+}
+
+export default function ProjectAlerts() {
+  const {project} = useProjectSettingsOutlet();
+
   return (
     <Access access={['project:write']} project={project}>
       {({hasAccess}) => (
         <Fragment>
-          {isValidElement(children) &&
-            cloneElement<any>(children, {
-              canEditRule: hasAccess,
-            })}
+          <ProjectAlertsOutlet canEditRule={hasAccess} project={project} />
         </Fragment>
       )}
     </Access>
   );
 }
-
-export default ProjectAlerts;
