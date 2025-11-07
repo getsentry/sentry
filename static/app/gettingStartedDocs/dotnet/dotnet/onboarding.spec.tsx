3f91f1ed5101b09f38b394d51adec3300894dc4b
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-07 09:13:38 +0100

    ref(onboarding): Split dotnet onboarding docs (#102944)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/dotnet/dotnet/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/dotnet/dotnet/onboarding.spec.tsx
@@ -4,7 +4,7 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {ProductSolution} from 'sentry/components/onboarding/gettingStartedDoc/types';
 
-import docs from './dotnet';
+import docs from '.';
 
 describe('dotnet onboarding docs', () => {
   it('renders errors onboarding docs correctly', async () => {
