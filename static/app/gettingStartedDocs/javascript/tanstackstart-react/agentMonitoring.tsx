8eb57cc5c91868fef11d69b1c1dc0ef92920d7bc
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-07 07:18:06 +0100

    ref(onboarding): Split Tanstack onboarding docs (#102560)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

---
+++ static/app/gettingStartedDocs/javascript/tanstackstart-react/agentMonitoring.tsx
@@ -0,0 +1,6 @@
+import {getNodeAgentMonitoringOnboarding} from 'sentry/utils/gettingStartedDocs/node';
+
+export const agentMonitoring = getNodeAgentMonitoringOnboarding({
+  packageName: '@sentry/tanstackstart-react',
+  configFileName: 'app/ssr.tsx',
+});
