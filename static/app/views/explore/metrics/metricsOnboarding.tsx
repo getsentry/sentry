81674ad6946a85b868402f72975bad311c7cb903
Kev <6111995+k-fish@users.noreply.github.com>
2025-11-07 10:47:39 -0500

    ref(tracemetrics): Hide copy about updating projects (#102958)
    This is confusing for the time being since some projects are already
    underway.

--- static/app/views/explore/metrics/metricsOnboarding.tsx
+++ static/app/views/explore/metrics/metricsOnboarding.tsx
@@ -186,13 +186,7 @@ function Onboarding({organization, project}: OnboardingProps) {
     // This currently covers all non-supported platforms as `doesNotSupportMetrics` is empty.
     return (
       <OnboardingPanel project={project} isUnsupportedPlatform>
-        <div>
-          {tct(
-            "We're working to bring Metrics support to [platform]. Stay updated by joining the discussion!",
-
-            {platform: <strong>{currentPlatform?.name || project.slug}</strong>}
-          )}
-        </div>
+        <div>{t('Stay updated by joining the discussion!')}</div>
         <br />
         <div>
           <LinkButton
