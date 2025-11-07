1a763a015be2211a1c7e35c4954b2c338b9c466f
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-07 07:13:52 +0100

    ref(onboarding): Split express onboarding docs (#102658)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/node/express/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/node/express/onboarding.spec.tsx
@@ -6,7 +6,7 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {ProductSolution} from 'sentry/components/onboarding/gettingStartedDoc/types';
 
-import docs from './express';
+import docs from '.';
 
 describe('express onboarding docs', () => {
   it('renders onboarding docs correctly', () => {
