d4a68233e3ac0a86b5416503e7f0c9623046847d
TkDodo <dominik.dorfmeister@sentry.io>
2025-11-08 21:42:58 +0100

    test: just to see if that is responsible for the timing differences

--- tests/js/setupMsw.ts
+++ tests/js/setupMsw.ts
@@ -1,26 +1,26 @@
-import {http, passthrough} from 'msw';
-
-import {server} from 'sentry-test/msw';
-
-beforeAll(() =>
-  server.listen({
-    // This tells MSW to throw an error whenever it
-    // encounters a request that doesn't have a
-    // matching request handler.
-    onUnhandledRequest: 'error',
-  })
-);
-
-beforeEach(() => {
-  server.use(
-    // jest project under Sentry organization (dev productivity team)
-    http.all('https://o1.ingest.us.sentry.io/*', () => {
-      passthrough();
-    })
-  );
-});
-
-afterEach(() => {
-  server.resetHandlers();
-});
-afterAll(() => server.close());
+// import {http, passthrough} from 'msw';
+//
+// import {server} from 'sentry-test/msw';
+//
+// beforeAll(() =>
+//   server.listen({
+//     // This tells MSW to throw an error whenever it
+//     // encounters a request that doesn't have a
+//     // matching request handler.
+//     onUnhandledRequest: 'error',
+//   })
+// );
+//
+// beforeEach(() => {
+//   server.use(
+//     // jest project under Sentry organization (dev productivity team)
+//     http.all('https://o1.ingest.us.sentry.io/*', () => {
+//       passthrough();
+//     })
+//   );
+// });
+//
+// afterEach(() => {
+//   server.resetHandlers();
+// });
+// afterAll(() => server.close());
