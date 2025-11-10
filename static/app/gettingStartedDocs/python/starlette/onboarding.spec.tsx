1bb3359a6a07f8132b009ed9f7898d0c5d38f963
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-10 19:21:42 +0100

    ref(onboarding): Split starlette onboarding docs (#103056)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/python/starlette/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/python/starlette/onboarding.spec.tsx
@@ -6,7 +6,7 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {ProductSolution} from 'sentry/components/onboarding/gettingStartedDoc/types';
 
-import docs from './starlette';
+import docs from '.';
 
 describe('starlette onboarding docs', () => {
   it('renders doc correctly', () => {
