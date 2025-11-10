aeb08837ccdbe9943c06c1d59e4b1f8a38ad5138
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-10 09:14:23 +0100

    ref(onboarding): Split aspnet core onboarding docs (#102946)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/dotnet/aspnetcore/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/dotnet/aspnetcore/onboarding.spec.tsx
@@ -4,7 +4,7 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {ProductSolution} from 'sentry/components/onboarding/gettingStartedDoc/types';
 
-import docs from './aspnetcore';
+import docs from './index';
 
 describe('aspnetcore onboarding docs', () => {
   it('renders errors onboarding docs correctly', async () => {
