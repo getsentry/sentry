a21a5974c32b26cad19557c2874ed526bf60fe48
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-10 14:48:17 +0100

    ref(onboarding): Split sanic onboarding docs (#103054)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/python/sanic/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/python/sanic/onboarding.spec.tsx
@@ -1,7 +1,7 @@
 import {renderWithOnboardingLayout} from 'sentry-test/onboarding/renderWithOnboardingLayout';
 import {screen} from 'sentry-test/reactTestingLibrary';
 
-import docs from './sanic';
+import docs from '.';
 
 describe('sanic onboarding docs', () => {
   it('renders doc correctly', () => {
