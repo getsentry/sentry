c2b742feb23601d6c77f821b45dbf3c969bbd980
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-10 11:17:45 +0100

    ref(onboarding): Split aws lambda onboarding docs (#102945)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/dotnet/awslambda/onboarding.spec.tsx
+++ static/app/gettingStartedDocs/dotnet/awslambda/onboarding.spec.tsx
@@ -4,7 +4,7 @@ import {textWithMarkupMatcher} from 'sentry-test/utils';
 
 import {ProductSolution} from 'sentry/components/onboarding/gettingStartedDoc/types';
 
-import docs from './awslambda';
+import docs from './index';
 
 describe('awslambda onboarding docs', () => {
   it('renders errors onboarding docs correctly', async () => {
