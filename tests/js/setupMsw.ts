be611f77b0bfb88459678e0cf36848319759727c
TkDodo <dominik.dorfmeister@sentry.io>
2025-11-10 13:40:16 +0100

    okay let's try with bypass

--- tests/js/setupMsw.ts
+++ tests/js/setupMsw.ts
@@ -1,4 +1,4 @@
-import {http, passthrough} from 'msw';
+// import {http, passthrough} from 'msw';
 
 import {server} from 'sentry-test/msw';
 
@@ -7,18 +7,18 @@ beforeAll(() =>
     // This tells MSW to throw an error whenever it
     // encounters a request that doesn't have a
     // matching request handler.
-    onUnhandledRequest: 'error',
+    onUnhandledRequest: 'bypass',
   })
 );
 
-beforeEach(() => {
-  server.use(
-    // jest project under Sentry organization (dev productivity team)
-    http.all('https://o1.ingest.us.sentry.io/*', () => {
-      passthrough();
-    })
-  );
-});
+// beforeEach(() => {
+//   server.use(
+//     // jest project under Sentry organization (dev productivity team)
+//     http.all('https://o1.ingest.us.sentry.io/*', () => {
+//       passthrough();
+//     })
+//   );
+// });
 
 afterEach(() => {
   server.resetHandlers();
