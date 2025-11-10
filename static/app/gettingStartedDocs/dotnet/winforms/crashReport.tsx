4e53f2fe7cfdcf2c3c4a9c95f3b62378fe0b9f28
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-10 11:18:00 +0100

    ref(onboarding): Split winforms onboarding docs (#102938)
    Contributes to
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

---
+++ static/app/gettingStartedDocs/dotnet/winforms/crashReport.tsx
@@ -0,0 +1,27 @@
+import type {OnboardingConfig} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {
+  getCrashReportGenericInstallSteps,
+  getCrashReportModalConfigDescription,
+  getCrashReportModalIntroduction,
+} from 'sentry/components/onboarding/gettingStartedDoc/utils/feedbackOnboarding';
+
+export const crashReport: OnboardingConfig = {
+  introduction: () => getCrashReportModalIntroduction(),
+  install: params => getCrashReportGenericInstallSteps(params),
+  configure: () => [
+    {
+      type: StepType.CONFIGURE,
+      content: [
+        {
+          type: 'text',
+          text: getCrashReportModalConfigDescription({
+            link: 'https://docs.sentry.io/platforms/dotnet/guides/winforms/user-feedback/configuration/#crash-report-modal',
+          }),
+        },
+      ],
+    },
+  ],
+  verify: () => [],
+  nextSteps: () => [],
+};
