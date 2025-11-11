9f7b1036042f99502d6f26f649736cd720598f36
Michelle Fu <michelle.fu@sentry.io>
2025-11-11 13:50:49 -0800

    more tests

--- tests/sentry/incidents/serializers/test_workflow_engine_incident.py
+++ tests/sentry/incidents/serializers/test_workflow_engine_incident.py
@@ -1,9 +1,17 @@
 from sentry.api.serializers import serialize
+from sentry.incidents.endpoints.serializers.utils import OFFSET
 from sentry.incidents.endpoints.serializers.workflow_engine_incident import (
     WorkflowEngineDetailedIncidentSerializer,
     WorkflowEngineIncidentSerializer,
 )
 from sentry.incidents.models.incident import IncidentStatus, IncidentStatusMethod, IncidentType
+from sentry.models.groupopenperiodactivity import GroupOpenPeriodActivity, OpenPeriodActivityType
+from sentry.types.group import PriorityLevel
+from sentry.workflow_engine.models import (
+    ActionAlertRuleTriggerAction,
+    AlertRuleDetector,
+    DataConditionAlertRuleTrigger,
+)
 from tests.sentry.incidents.serializers.test_workflow_engine_base import (
     TestWorkflowEngineSerializer,
 )
@@ -47,5 +55,66 @@ class TestIncidentSerializer(TestWorkflowEngineSerializer):
 
     def test_no_incident(self) -> None:
         """
-        Assert that nothing breaks if the IGOP does not exist.
+        Assert that nothing breaks if the legacy models do not exist.
         """
+        self.incident_group_open_period.delete()
+        ard = AlertRuleDetector.objects.filter(detector_id=self.detector.id)
+        dcart = DataConditionAlertRuleTrigger.objects.filter(
+            data_condition_id=self.critical_detector_trigger.id
+        )
+        aarta = ActionAlertRuleTriggerAction.objects.filter(action_id=self.critical_action.id)
+
+        ard.delete()
+        dcart.delete()
+        aarta.delete()
+
+        serialized_incident = serialize(
+            self.group_open_period, self.user, WorkflowEngineIncidentSerializer()
+        )
+        self.expected.update({"id": str(self.detector.id + OFFSET)})
+        self.expected["triggers"][0].update(
+            {
+                "id": str(self.critical_detector_trigger.id + OFFSET),
+                "alertRuleId": str(self.detector.id + OFFSET),
+            }
+        )
+        self.expected["triggers"][1].update(
+            {
+                "alertRuleId": str(self.detector.id + OFFSET),
+            }
+        )
+        self.expected["triggers"][0]["actions"][0].update(
+            {
+                "id": str(self.critical_action.id + OFFSET),
+                "alertRuleTriggerId": str(self.critical_detector_trigger.id + OFFSET),
+            }
+        )
+
+        self.incident_expected.update(
+            {
+                "id": str(self.group_open_period.id + OFFSET),
+                "identifier": str(self.group_open_period.id + OFFSET),
+            }
+        )
+        assert serialized_incident == self.incident_expected
+
+    def test_with_activities(self) -> None:
+        gopa = GroupOpenPeriodActivity.objects.create(
+            date_added=self.group_open_period.date_added,
+            group_open_period=self.group_open_period,
+            type=OpenPeriodActivityType.OPENED,
+            value=self.group.priority,
+        )
+        serialized_incident = serialize(
+            self.group_open_period,
+            self.user,
+            WorkflowEngineIncidentSerializer(expand=["activities"]),
+        )
+        assert len(serialized_incident["activities"]) == 1
+        serialized_activity = serialized_incident["activities"][0]
+        assert serialized_activity == {
+            "id": str(gopa.id),
+            "type": OpenPeriodActivityType.OPENED.to_str(),
+            "value": PriorityLevel(self.group.priority).to_str(),
+            "dateCreated": gopa.date_added,
+        }
