3fe6303e37b10702b9c6c800edb2cba09b0f611e
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-11 15:30:21 +0100

    fix(project-creation): Discord and MSTeams notifications should send channel id instead of name (#103146)
    Contributes to
    https://linear.app/getsentry/issue/TET-1369/user-confusion-when-project-creation-fails-but-succeeds

--- static/app/views/projectInstall/messagingIntegrationAlertRule.spec.tsx
+++ static/app/views/projectInstall/messagingIntegrationAlertRule.spec.tsx
@@ -149,12 +149,12 @@ describe('MessagingIntegrationAlertRule', () => {
       />
     );
     await selectEvent.openMenu(screen.getByLabelText('channel'));
-    expect(await screen.findByText('#alerts')).toBeInTheDocument();
-    expect(screen.getByText('#general')).toBeInTheDocument();
-    await selectEvent.select(screen.getByLabelText('channel'), '#alerts');
+    expect(await screen.findByText('#general (1)')).toBeInTheDocument();
+    expect(screen.getByText('#alerts (2)')).toBeInTheDocument();
+    await selectEvent.select(screen.getByLabelText('channel'), /#alerts/);
     expect(mockSetChannel).toHaveBeenCalledWith({
-      label: '#alerts',
-      value: '#alerts',
+      label: '#alerts (2)',
+      value: '2',
       new: false,
     });
   });
@@ -356,5 +356,36 @@ describe('MessagingIntegrationAlertRule', () => {
     expect(mockClear).toHaveBeenCalled();
 
     spy.mockRestore();
+  });
+
+  it('displays and sends channel id for microsoft teams', async () => {
+    MockApiClient.addMockResponse({
+      url: `/organizations/${organization.slug}/integrations/${msteamsIntegrations[0]!.id}/channels/`,
+      body: {
+        nextCursor: null,
+        results: [
+          {id: '1', name: 'general', display: '#general', type: 'text'},
+          {id: '2', name: 'alerts', display: '#alerts', type: 'text'},
+        ],
+      },
+    });
+    render(
+      <MessagingIntegrationAlertRule
+        {...{
+          ...notificationProps,
+          integration: msteamsIntegrations[0],
+          provider: 'msteams',
+        }}
+      />
+    );
+    await selectEvent.openMenu(screen.getByLabelText('channel'));
+    expect(await screen.findByText('#general (1)')).toBeInTheDocument();
+    expect(screen.getByText('#alerts (2)')).toBeInTheDocument();
+    await selectEvent.select(screen.getByLabelText('channel'), /#alerts/);
+    expect(mockSetChannel).toHaveBeenCalledWith({
+      label: '#alerts (2)',
+      value: '2',
+      new: false,
+    });
   });
 });
