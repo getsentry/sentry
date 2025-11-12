797ec1685838c7d94af0ce1abf7824fabbe4fc03
Markus Unterwaditzer <markus-tarpit+git@unterwaditzer.net>
2025-11-12 16:39:23 +0100

    remove outdated options

--- src/sentry/consumers/__init__.py
+++ src/sentry/consumers/__init__.py
@@ -468,9 +468,6 @@ def get_stream_processor(
     shutdown_strategy_before_consumer: bool = False,
     add_global_tags: bool = False,
     profile_consumer_join: bool = False,
-    enable_autocommit: bool = False,
-    retry_handle_destroyed: bool = False,
-    handle_poll_while_paused: bool = False,
 ) -> StreamProcessor:
     from sentry.utils import kafka_config
 
@@ -528,8 +525,6 @@ def get_stream_processor(
             group_id=group_id,
             auto_offset_reset=auto_offset_reset,
             strict_offset_reset=strict_offset_reset,
-            enable_auto_commit=enable_autocommit,
-            retry_handle_destroyed=retry_handle_destroyed,
         )
 
         if max_poll_interval_ms is not None:
@@ -542,9 +537,8 @@ def get_stream_processor(
         if group_instance_id is not None:
             consumer_config["group.instance.id"] = group_instance_id
 
-        if enable_autocommit:
-            # Set commit interval to 1 second (1000ms)
-            consumer_config["auto.commit.interval.ms"] = 1000
+        # Set commit interval to 1 second (1000ms)
+        consumer_config["auto.commit.interval.ms"] = 1000
 
         return consumer_config
 
@@ -638,7 +632,6 @@ def get_stream_processor(
         join_timeout=join_timeout,
         dlq_policy=dlq_policy,
         shutdown_strategy_before_consumer=shutdown_strategy_before_consumer,
-        handle_poll_while_paused=handle_poll_while_paused,
     )
 
 
