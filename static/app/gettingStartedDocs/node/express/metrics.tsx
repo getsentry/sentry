e248920ddcb9dec41c845cd0c990305c384c7b9c
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-11-12 11:38:51 +0100

    ref(onboarding): flatten folders (#103210)
    closes
    https://linear.app/getsentry/issue/TET-864/introduce-folders-for-onboarding-platforms

--- static/app/gettingStartedDocs/node/express/metrics.tsx
+++
@@ -1,6 +0,0 @@
-import {getNodeMetricsOnboarding} from 'sentry/gettingStartedDocs/node/node/utils';
-
-export const metrics = getNodeMetricsOnboarding({
-  docsPlatform: 'express',
-  packageName: '@sentry/node',
-});
