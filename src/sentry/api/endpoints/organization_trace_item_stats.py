0cce6699a03661d4e4a7e408a08967332f4d0a1f
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2025-11-12 16:20:12 -0500

    address comments

--- src/sentry/api/endpoints/organization_trace_item_stats.py
+++ src/sentry/api/endpoints/organization_trace_item_stats.py
@@ -1,5 +1,6 @@
 import logging
 
+from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
 
@@ -7,7 +8,6 @@ from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import region_silo_endpoint
 from sentry.api.bases import NoProjects, OrganizationEventsV2EndpointBase
-from sentry.exceptions import InvalidSearchQuery
 from sentry.models.organization import Organization
 from sentry.search.eap.constants import SUPPORTED_STATS_TYPES
 from sentry.search.eap.resolver import SearchResolver
@@ -17,6 +17,13 @@ from sentry.snuba.referrer import Referrer
 from sentry.snuba.spans_rpc import Spans
 
 logger = logging.getLogger(__name__)
+
+
+class OrganizationTraceItemsStatsSerializer(serializers.Serializer):
+    query = serializers.CharField(required=False)
+    statsType = serializers.ListField(
+        child=serializers.ChoiceField(SUPPORTED_STATS_TYPES), required=True
+    )
 
 
 @region_silo_endpoint
@@ -32,28 +39,20 @@ class OrganizationTraceItemsStatsEndpoint(OrganizationEventsV2EndpointBase):
         except NoProjects:
             return Response({"data": []})
 
-        stats_types = set(request.GET.getlist("statsType", default=["attributeDistributions"]))
-        if not stats_types:
-            raise InvalidSearchQuery(
-                f"Please specify `statsType`. Supported stats types are: {SUPPORTED_STATS_TYPES}."
-            )
-
-        for stats_type in stats_types:
-            if stats_type not in SUPPORTED_STATS_TYPES:
-                raise InvalidSearchQuery(
-                    f"{stats_type} is not a valid statsType. Please use one of the allowed stats types: {SUPPORTED_STATS_TYPES}."
-                )
+        serializer = OrganizationTraceItemsStatsSerializer(data=request.GET)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=400)
+        serialized = serializer.validated_data
 
         resolver_config = SearchResolverConfig()
         resolver = SearchResolver(
             params=snuba_params, config=resolver_config, definitions=SPAN_DEFINITIONS
         )
 
-        query = request.GET.get("query", "")
         stats_results = Spans.run_stats_query(
             params=snuba_params,
-            stats_types=stats_types,
-            query_string=query,
+            stats_types=serialized.get("statsType"),
+            query_string=serialized.get("query", ""),
             referrer=Referrer.API_SPANS_FREQUENCY_STATS_RPC.value,
             config=resolver_config,
             search_resolver=resolver,
