7eec73e1da130e789e79ae084008f1283bbce125
Mia Hsu <ameliahsu@gmail.com>
2025-11-12 10:57:21 -0800

    redirect workflow details to rule list

--- static/app/views/automations/routes.tsx
+++ static/app/views/automations/routes.tsx
@@ -1,10 +1,7 @@
-import LoadingIndicator from 'sentry/components/loadingIndicator';
 import Redirect from 'sentry/components/redirect';
 import {makeLazyloadComponent as make} from 'sentry/makeLazyloadComponent';
 import type {SentryRouteObject} from 'sentry/router/types';
-import {useApiQuery} from 'sentry/utils/queryClient';
 import useOrganization from 'sentry/utils/useOrganization';
-import {useParams} from 'sentry/utils/useParams';
 import {useUser} from 'sentry/utils/useUser';
 import {makeAlertsPathname} from 'sentry/views/alerts/pathnames';
 
@@ -31,7 +28,7 @@ export const automationRoutes: SentryRouteObject = {
     },
     {
       path: ':automationId/',
-      component: RedirectToRuleDetails,
+      component: RedirectToRuleList,
       deprecatedRouteProps: true,
       children: [
         {
@@ -55,12 +52,14 @@ function RedirectToRuleList({children}: {children: React.ReactNode}) {
     !user.isStaff && !organization.features.includes('workflow-engine-ui');
 
   if (shouldRedirect) {
-    <Redirect
-      to={makeAlertsPathname({
-        path: '/rules/',
-        organization,
-      })}
-    />;
+    return (
+      <Redirect
+        to={makeAlertsPathname({
+          path: '/rules/',
+          organization,
+        })}
+      />
+    );
   }
 
   return children;
@@ -82,58 +81,6 @@ function RedirectToNewRule({children}: {children: React.ReactNode}) {
         })}
       />
     );
-  }
-
-  return children;
-}
-
-interface AlertRuleWorkflow {
-  workflowId: string;
-  alertRuleId?: string;
-  ruleId?: string;
-}
-
-function RedirectToRuleDetails({children}: {children: React.ReactNode}) {
-  const user = useUser();
-  const organization = useOrganization();
-
-  const params = useParams<{automationId: string}>();
-
-  const shouldRedirect =
-    !user.isStaff && !organization.features.includes('workflow-engine-ui');
-
-  const {data, isLoading} = useApiQuery<AlertRuleWorkflow>(
-    [
-      `/organizations/${organization.slug}/alert-rule-workflows/`,
-      {
-        query: {workflowId: params.automationId},
-      },
-    ],
-    {staleTime: Infinity, retry: false, enabled: shouldRedirect}
-  );
-
-  if (shouldRedirect) {
-    if (isLoading) {
-      return <LoadingIndicator />;
-    }
-    if (data) {
-      return (
-        <Redirect
-          to={makeAlertsPathname({
-            path: data.ruleId
-              ? `/rules/details/${data.ruleId}/`
-              : `/rules/details/${data.alertRuleId}/`,
-            organization,
-          })}
-        />
-      );
-    }
-    <Redirect
-      to={makeAlertsPathname({
-        path: '/rules/',
-        organization,
-      })}
-    />;
   }
 
   return children;
