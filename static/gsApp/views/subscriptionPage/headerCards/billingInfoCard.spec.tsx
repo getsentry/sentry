0b8fee7faf9dc7eef9982a0c7106a9c51fa26242
Isabella Enriquez <isabella.enriquez@sentry.io>
2025-11-13 14:17:26 -0500

    feat(sub v3): Surface account balance and card brand (#103094)
    Closes BIL-1731 and BIL-1732.
    
    - Adds account balance to Billing Info card
    - Adds card brand to Billing Info card and Billing Info page
    - Corrects types
    
    Needs https://github.com/getsentry/getsentry/pull/18796
    
    ### billing info page
    
    <img width="293" height="192" alt="Screenshot 2025-11-10 at 4 05 10 PM"
    src="https://github.com/user-attachments/assets/dc399958-da4c-47f7-a7be-25899c8e8414"
    />
    
    ### billing info card on overview page
    <img width="465" height="182" alt="Screenshot 2025-11-10 at 4 05 01 PM"
    src="https://github.com/user-attachments/assets/3db89ada-e85a-4936-aec7-033737105712"
    />

--- static/gsApp/views/subscriptionPage/headerCards/billingInfoCard.spec.tsx
+++ static/gsApp/views/subscriptionPage/headerCards/billingInfoCard.spec.tsx
@@ -23,13 +23,14 @@ describe('BillingInfoCard', () => {
       method: 'GET',
       body: BillingDetailsFixture(),
     });
-    const subscription = SubscriptionFixture({organization});
+    const subscription = SubscriptionFixture({organization, accountBalance: 10_00});
     render(<BillingInfoCard organization={organization} subscription={subscription} />);
 
     expect(screen.getByText('Billing information')).toBeInTheDocument();
     await screen.findByText('Test company, Display Address');
     expect(screen.getByText('Billing email: test@gmail.com')).toBeInTheDocument();
-    expect(screen.getByText('Card ending in 4242')).toBeInTheDocument();
+    expect(screen.getByText('Visa ending in 4242')).toBeInTheDocument();
+    expect(screen.getByText('Account balance: $10')).toBeInTheDocument();
   });
 
   it('renders with some pre-existing info', async () => {
@@ -38,7 +39,7 @@ describe('BillingInfoCard', () => {
       method: 'GET',
       body: BillingDetailsFixture({billingEmail: null, companyName: null}),
     });
-    const subscription = SubscriptionFixture({organization});
+    const subscription = SubscriptionFixture({organization, accountBalance: -10_00});
     render(<BillingInfoCard organization={organization} subscription={subscription} />);
 
     expect(screen.getByText('Billing information')).toBeInTheDocument();
@@ -48,7 +49,8 @@ describe('BillingInfoCard', () => {
     expect(
       screen.getByText('No billing email or tax number on file')
     ).toBeInTheDocument();
-    expect(screen.getByText('Card ending in 4242')).toBeInTheDocument();
+    expect(screen.getByText('Visa ending in 4242')).toBeInTheDocument();
+    expect(screen.getByText('Account balance: $10 credit')).toBeInTheDocument();
   });
 
   it('renders without pre-existing info', async () => {
