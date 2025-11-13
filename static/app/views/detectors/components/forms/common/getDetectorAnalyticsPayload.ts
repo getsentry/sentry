889a70b48423485123a222f3295164cf22a6a0ad
Scott Cooper <scttcper@gmail.com>
2025-11-13 14:29:58 -0800

    friendlier names

--- static/app/views/detectors/components/forms/common/getDetectorAnalyticsPayload.ts
+++ static/app/views/detectors/components/forms/common/getDetectorAnalyticsPayload.ts
@@ -15,13 +15,12 @@ type MetricDetectorAnalytics = {
 
 type UptimeDetectorAnalytics = {
   detector_type: UptimeDetector['type'];
-  environment: UptimeDetector['config']['environment'];
-  mode: UptimeDetector['config']['mode'];
+  uptime_mode: UptimeDetector['config']['mode'];
 };
 
 type MonitorDetectorAnalytics = {
+  cron_schedule_type: MonitorConfig['schedule_type'] | undefined;
   detector_type: CronDetector['type'];
-  schedule_type: MonitorConfig['schedule_type'] | undefined;
 };
 
 type ErrorDetectorAnalytics = {
@@ -49,14 +48,13 @@ export function getDetectorAnalyticsPayload(
     case 'uptime_domain_failure':
       return {
         detector_type: detector.type,
-        environment: detector.config.environment,
-        mode: detector.config.mode,
+        uptime_mode: detector.config.mode,
       };
     case 'monitor_check_in_failure': {
       const monitorConfig = detector.dataSources[0]?.queryObj?.config;
       return {
+        cron_schedule_type: monitorConfig?.schedule_type,
         detector_type: detector.type,
-        schedule_type: monitorConfig?.schedule_type,
       };
     }
     default:
