4e4b3da76b2a1d18d329d68e3d29a43d7fd4ef8d
Cathy Teng <cathy.teng@sentry.io>
2025-11-12 17:46:52 -0800

    fix test

--- tests/sentry/workflow_engine/endpoints/test_organization_test_fire_action.py
+++ tests/sentry/workflow_engine/endpoints/test_organization_test_fire_action.py
@@ -99,10 +99,7 @@ class TestFireActionsEndpointTest(APITestCase, BaseWorkflowTest):
         assert mock_send_trigger.call_count == 1
         pagerduty_data = mock_send_trigger.call_args.kwargs.get("data")
         assert pagerduty_data is not None
-        # test notification has its own type, so it will pick up the issue stream detector
-        assert pagerduty_data["payload"]["summary"].startswith(
-            f"[{self.issue_stream_detector.name}]:"
-        )
+        assert pagerduty_data["payload"]["summary"].startswith(f"[{self.detector.name}]:")
 
     @mock.patch.object(NotifyEventAction, "after")
     @mock.patch(
