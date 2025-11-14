7155b96577de0d42ba840debdf94920c8d4b8880
lcian <17258265+lcian@users.noreply.github.com>
2025-11-14 15:50:26 +0100

    improve

--- src/sentry/lang/native/symbolicator.py
+++ src/sentry/lang/native/symbolicator.py
@@ -180,12 +180,12 @@ class Symbolicator:
             "objectstore.force-stored-symbolication"
         )
         if force_stored_attachment:
-            client = get_attachments_session(self.project.organization_id, self.project.id)
-            minidump.stored_id = client.put(minidump.data)
+            session = get_attachments_session(self.project.organization_id, self.project.id)
+            minidump.stored_id = session.put(minidump.data)
 
         if minidump.stored_id:
-            client = get_attachments_session(self.project.organization_id, self.project.id)
-            storage_url = client.object_url(minidump.stored_id)
+            session = get_attachments_session(self.project.organization_id, self.project.id)
+            storage_url = session.object_url(minidump.stored_id)
             json: dict[str, Any] = {
                 "platform": platform,
                 "sources": sources,
@@ -202,7 +202,7 @@ class Symbolicator:
                 return process_response(res)
             finally:
                 if force_stored_attachment:
-                    client.delete(minidump.stored_id)
+                    session.delete(minidump.stored_id)
                     minidump.stored_id = None
 
         data = {
@@ -225,12 +225,12 @@ class Symbolicator:
             "objectstore.force-stored-symbolication"
         )
         if force_stored_attachment:
-            client = get_attachments_session(self.project.organization_id, self.project.id)
-            report.stored_id = client.put(report.data)
+            session = get_attachments_session(self.project.organization_id, self.project.id)
+            report.stored_id = session.put(report.data)
 
         if report.stored_id:
-            client = get_attachments_session(self.project.organization_id, self.project.id)
-            storage_url = client.object_url(report.stored_id)
+            session = get_attachments_session(self.project.organization_id, self.project.id)
+            storage_url = session.object_url(report.stored_id)
             json: dict[str, Any] = {
                 "platform": platform,
                 "sources": sources,
@@ -246,7 +246,7 @@ class Symbolicator:
                 return process_response(res)
             finally:
                 if force_stored_attachment:
-                    client.delete(report.stored_id)
+                    session.delete(report.stored_id)
                     report.stored_id = None
 
         data = {
