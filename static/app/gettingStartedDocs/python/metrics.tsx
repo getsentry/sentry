68b01e6b7d8f319d65ef3731f78449ed9d2a18da
Alexander Alderman Webb <alexander.webb@sentry.io>
2025-11-14 09:50:34 +0100

    .

--- static/app/gettingStartedDocs/python/metrics.tsx
+++ static/app/gettingStartedDocs/python/metrics.tsx
@@ -5,7 +5,7 @@ import {
   type DocsParams,
   type OnboardingConfig,
 } from 'sentry/components/onboarding/gettingStartedDoc/types';
-import {tct} from 'sentry/locale';
+import {t, tct} from 'sentry/locale';
 
 import {getPythonInstallCodeBlock} from './utils';
 
@@ -15,32 +15,17 @@ export const metricsVerify = (params: DocsParams): ContentBlock => ({
   content: [
     {
       type: 'text',
-      text: tct(
-        'Metrics are automatically enabled in your [code:sentry_sdk.init()] configuration. You can emit metrics using the [code:sentry_sdk.metrics] API.',
-        {
-          code: <code />,
-        }
-      ),
+      text: t('Send metrics from your app to verify metrics are arriving in Sentry.'),
     },
     {
       type: 'code',
       language: 'python',
-      code: `import sentry_sdk
-from sentry_sdk import metrics
+      code: `from sentry_sdk import metrics
 
-sentry_sdk.init(
-  dsn="${params.dsn.public}",
-)
-
+# Emit metrics
 metrics.count("checkout.failed", 1)
 metrics.gauge("queue.depth", 42)
 metrics.distribution("cart.amount_usd", 187.5)`,
-    },
-    {
-      type: 'text',
-      text: tct('For more detailed information, see the [link:metrics documentation].', {
-        link: <ExternalLink href="https://docs.sentry.io/platforms/python/metrics/" />,
-      }),
     },
   ],
 });
@@ -73,8 +58,43 @@ export const metrics = ({
   configure: () => [],
   verify: (params: DocsParams) => [
     {
-      type: StepType.VERIFY,
-      content: [metricsVerify(params)],
+      type: StepType.CONFIGURE,
+      content: [
+        {
+          type: 'text',
+          text: tct(
+            'Metrics are automatically enabled in your [code:sentry_sdk.init()] configuration. You can emit metrics using the [code:sentry_sdk.metrics] API.',
+            {
+              code: <code />,
+            }
+          ),
+        },
+        {
+          type: 'code',
+          language: 'python',
+          code: `import sentry_sdk
+from sentry_sdk import metrics
+
+sentry_sdk.init(
+  dsn="${params.dsn.public}",
+)
+
+metrics.count("checkout.failed", 1)
+metrics.gauge("queue.depth", 42)
+metrics.distribution("cart.amount_usd", 187.5)`,
+        },
+        {
+          type: 'text',
+          text: tct(
+            'For more detailed information, see the [link:metrics documentation].',
+            {
+              link: (
+                <ExternalLink href="https://docs.sentry.io/platforms/python/metrics/" />
+              ),
+            }
+          ),
+        },
+      ],
     },
   ],
 });
