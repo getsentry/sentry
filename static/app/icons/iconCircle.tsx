9a32f88414fd237cc3c5b6b6b1c2439462a54dca
Jesse Box <me@jessebox.net>
2025-11-17 10:22:27 +0100

    Removed: @deprecation statement for 'iconCircle' and 'iconCircleFilled'

--- static/app/icons/iconCircle.tsx
+++ static/app/icons/iconCircle.tsx
@@ -1,9 +1,6 @@
 import type {SVGIconProps} from './svgIcon';
 import {SvgIcon} from './svgIcon';
 
-/**
- * @deprecated This icon will be removed in new UI.
- */
 export function IconCircle(props: SVGIconProps) {
   return (
     <SvgIcon {...props}>
--- static/app/icons/iconCircleFill.tsx
+++ static/app/icons/iconCircleFill.tsx
@@ -1,9 +1,6 @@
 import type {SVGIconProps} from './svgIcon';
 import {SvgIcon} from './svgIcon';
 
-/**
- * @deprecated This icon will be removed in new UI.
- */
 export function IconCircleFill(props: SVGIconProps) {
   return (
     <SvgIcon {...props}>
