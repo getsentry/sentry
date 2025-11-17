getsentry/sentry

commit 15a155ede601782e28d2dc02bdfa85825d103eab

ref(grouping): More small new grouping config refactors (#103268)
This is various bits of refactoring pulled from upcoming PRs to keep them focused and easy to review. No behavior changes.

- Server-side fingerprinting is unrelated to stacktrace normalization for enhancements rule processing. Remove a comment which suggests otherwise, and move server-side fingerprinting to happen before the normalization, so both stacktrace processes happen together.
- Refactor the `_update_frame` helper used in normalization by moving the few in-app-related lines directly into the normalization function and simplifying the rest. Rename the function from `_update_frame` to `_trim_function_name` to reflect it's now-singular purpose.
- Separate out one of the conditionals in the stacktrace strategy code, so it can be handled differently by different configs.
- Add a comment about the `EnhancementsConfigData` type.
- In grouping snapshot tests, always test configs in the same order.

tests/sentry/grouping/__init__.py
@@ -166,7 +166,7 @@ def with_grouping_configs(config_ids: Iterable[str]) -> pytest.MarkDecorator:
         return pytest.mark.skip("no configs to test")
 
     return pytest.mark.parametrize(
-        "config_name", config_ids, ids=lambda config_name: config_name.replace("-", "_")
+        "config_name", sorted(config_ids), ids=lambda config_name: config_name.replace("-", "_")
     )
 
 

src/sentry/grouping/enhancer/__init__.py
@@ -58,6 +58,8 @@ VALID_PROFILING_ACTIONS_SET = frozenset(["+app", "-app"])
 
 @dataclass
 class EnhancementsConfigData:
+    # Note: This is not an actual config, just a container to make it easier to pass data between
+    # functions while loading a config.
     rules: list[EnhancementRule]
     rust_enhancements: RustEnhancements
     version: int | None = None

src/sentry/grouping/ingest/hashing.py
@@ -55,20 +55,15 @@ def _calculate_event_grouping(
     with metrics.timer("save_event._calculate_event_grouping", tags=metric_tags):
         loaded_grouping_config = load_grouping_config(grouping_config)
 
-        with metrics.timer("event_manager.normalize_stacktraces_for_grouping", tags=metric_tags):
-            with sentry_sdk.start_span(op="event_manager.normalize_stacktraces_for_grouping"):
-                event.normalize_stacktraces_for_grouping(loaded_grouping_config)
-
         with metrics.timer("event_manager.apply_server_fingerprinting", tags=metric_tags):
-            # The active grouping config was put into the event in the
-            # normalize step before.  We now also make sure that the
-            # fingerprint was set to `'{{ default }}' just in case someone
-            # removed it from the payload.  The call to `get_hashes_and_variants` will then
-            # look at `grouping_config` to pick the right parameters.
             event.data["fingerprint"] = event.data.data.get("fingerprint") or ["{{ default }}"]
             apply_server_side_fingerprinting(
                 event.data.data, get_fingerprinting_config_for_project(project)
             )
+
+        with metrics.timer("event_manager.normalize_stacktraces_for_grouping", tags=metric_tags):
+            with sentry_sdk.start_span(op="event_manager.normalize_stacktraces_for_grouping"):
+                event.normalize_stacktraces_for_grouping(loaded_grouping_config)
 
         with metrics.timer("event_manager.event.get_hashes", tags=metric_tags):
             hashes, variants = event.get_hashes_and_variants(loaded_grouping_config)

src/sentry/grouping/strategies/newstyle.py
@@ -510,11 +510,12 @@ def _single_stacktrace_variant(
         and frame_components[0].contributes
         and get_behavior_family_for_platform(frames[0].platform or event.platform) == "javascript"
         and not frames[0].function
-        and has_url_origin(frames[0].abs_path, files_count_as_urls=True)
     ):
-        frame_components[0].update(
-            contributes=False, hint="ignored single non-URL JavaScript frame"
-        )
+        should_ignore_frame = has_url_origin(frames[0].abs_path, files_count_as_urls=True)
+        if should_ignore_frame:
+            frame_components[0].update(
+                contributes=False, hint="ignored single non-URL JavaScript frame"
+            )
 
     stacktrace_component = context.config.enhancements.assemble_stacktrace_component(
         variant_name,

src/sentry/stacktraces/processing.py
@@ -328,7 +328,14 @@ def normalize_stacktraces_for_grouping(
         stripped_querystring = False
         for frames in stacktrace_frames:
             for frame in frames:
-                _update_frame(frame, platform)
+                _trim_function_name(frame, platform)
+
+                # Restore the original in_app value before applying in-app stacktrace rules. This
+                # lets us run grouping enhancers on the stacktrace multiple times, as would happen
+                # during a grouping config transition, for example.
+                orig_in_app = get_path(frame, "data", "orig_in_app")
+                if orig_in_app is not None:
+                    frame["in_app"] = None if orig_in_app == -1 else bool(orig_in_app)
 
                 # Track the incoming `in_app` value, before we make any changes. This is different
                 # from the `orig_in_app` value which may be set by
@@ -379,24 +386,18 @@ def normalize_stacktraces_for_grouping(
     data["metadata"] = event_metadata
 
 
-def _update_frame(frame: dict[str, Any], platform: str | None) -> None:
-    """Restore the original in_app value before the first grouping
-    enhancers have been run. This allows to re-apply grouping
-    enhancers on the original frame data.
-    """
-    orig_in_app = get_path(frame, "data", "orig_in_app")
-    if orig_in_app is not None:
-        frame["in_app"] = None if orig_in_app == -1 else bool(orig_in_app)
+def _trim_function_name(frame: dict[str, Any], platform: str | None) -> None:
+    function = frame.get("function")
+    raw_function = frame.get("raw_function")
 
-    if frame.get("raw_function") is not None:
+    # Nothing to trim or trimming has already been done
+    if not function or raw_function is not None:
         return
-    raw_func = frame.get("function")
-    if not raw_func:
-        return
-    function_name = trim_function_name(raw_func, frame.get("platform") or platform)
-    if function_name != raw_func:
-        frame["raw_function"] = raw_func
-        frame["function"] = function_name
+
+    trimmed_function = trim_function_name(function, frame.get("platform", platform))
+    if trimmed_function != function:
+        frame["raw_function"] = function
+        frame["function"] = trimmed_function
 
 
 def should_process_for_stacktraces(data):

commit 39ba2454cda38994ebc509dde274124e95ac89ca

fix(grouping): Ensure custom titles use the correct frame (#103425)
This fixes a bug caused by https://github.com/getsentry/sentry/pull/103268, which moved server-side fingerprinting before the call to `normalize_stacktraces_for_grouping`[1]. While it's 99% true that they're unrelated processes (and therefore can happen in any order), the one exception to that is the handling of custom fingerprints which also include title information. In cases where the custom title includes any of the frame variables (`function`, `module`, `package`, or `abs_path`), the frame that gets used is the top in-app frame in the stacktrace. But in-app rules are applied as part of `normalize_stacktraces_for_grouping`, so having it not run until after the custom title is set is obviously a problem.

To fix this, the handling of such titles (in other words, the filling-in of the variables and adding of the result to the event) has been moved to live alongside the filling-in-of-the-variables which we do for the fingerprint itself, which is after in-app rules have been applied.  A snapshot test illustrating the fix has also been added, showing that it's not the top frame but the top in-app frame which is used for the title.

(Why didn't I just switch the order of the two operations back, you ask? Because switching the order was the first step towards absorbing the normalization into variant calculation call which comes immediately after it[2], so server-side fingerprinting needed to get out of its original spot between them.)


[1] https://github.com/getsentry/sentry/blob/6812bc023ceb6695bf6b0096852fc184feb82509/src/sentry/grouping/ingest/hashing.py#L58-L66
[2] https://github.com/getsentry/sentry/blob/6812bc023ceb6695bf6b0096852fc184feb82509/src/sentry/grouping/ingest/hashing.py#L68-L69

src/sentry/grouping/api.py
@@ -38,6 +38,7 @@ from sentry.issues.auto_source_code_config.constants import DERIVED_ENHANCEMENTS
 from sentry.models.grouphash import GroupHash
 from sentry.utils.cache import cache
 from sentry.utils.hashlib import md5_text
+from sentry.utils.safe import get_path
 
 if TYPE_CHECKING:
     from sentry.grouping.fingerprinting import FingerprintingConfig
@@ -282,16 +283,9 @@ def apply_server_side_fingerprinting(
 
     fingerprint_match = fingerprinting_config.get_fingerprint_values_for_event(event)
     if fingerprint_match is not None:
+        # TODO: We don't need to return attributes as part of the fingerprint match anymore
         matched_rule, new_fingerprint, attributes = fingerprint_match
-
-        # A custom title attribute is stored in the event to override the
-        # default title.
-        if "title" in attributes:
-            event["title"] = expand_title_template(attributes["title"], event)
         event["fingerprint"] = new_fingerprint
-
-        # Persist the rule that matched with the fingerprint in the event
-        # dictionary for later debugging.
         fingerprint_info["matched_rule"] = matched_rule.to_json()
 
     if fingerprint_info:
@@ -405,6 +399,9 @@ def get_grouping_variants_for_event(
         else resolve_fingerprint_values(raw_fingerprint, event.data)
     )
 
+    # Check if the fingerprint includes a custom title, and if so, set the event's title accordingly.
+    _apply_custom_title_if_needed(fingerprint_info, event)
+
     # Run all of the event-data-based grouping strategies. Any which apply will create grouping
     # components, which will then be grouped into variants by variant type (system, app, default).
     context = GroupingContext(config or _load_default_grouping_config(), event)
@@ -456,6 +453,18 @@ def get_grouping_variants_for_event(
         final_variants["fallback"] = FallbackVariant()
 
     return final_variants
+
+
+def _apply_custom_title_if_needed(fingerprint_info: FingerprintInfo, event: Event) -> None:
+    """
+    If the given event has a custom fingerprint which includes a title template, apply the custom
+    title to the event.
+    """
+    custom_title_template = get_path(fingerprint_info, "matched_rule", "attributes", "title")
+
+    if custom_title_template:
+        resolved_title = expand_title_template(custom_title_template, event.data)
+        event.data["title"] = resolved_title
 
 
 def get_contributing_variant_and_component(

tests/sentry/grouping/__init__.py
@@ -26,6 +26,7 @@ from sentry.grouping.strategies.configurations import (
     GROUPING_CONFIG_CLASSES,
     register_grouping_config,
 )
+from sentry.grouping.utils import expand_title_template
 from sentry.grouping.variants import BaseVariant
 from sentry.models.project import Project
 from sentry.services import eventstore
@@ -35,6 +36,7 @@ from sentry.testutils.factories import Factories
 from sentry.testutils.helpers.eventprocessing import save_new_event
 from sentry.testutils.pytest.fixtures import InstaSnapshotter, django_db_all
 from sentry.utils import json
+from sentry.utils.safe import get_path
 
 GROUPING_TESTS_DIR = path.dirname(__file__)
 GROUPING_INPUTS_DIR = path.join(GROUPING_TESTS_DIR, "grouping_inputs")
@@ -82,11 +84,22 @@ class GroupingInput:
         mgr.normalize()
         data = mgr.get_data()
 
-        # Normalize the stacktrace for grouping.  This normally happens in `EventManager.save`.
+        # Before creating the event, manually run the parts of `EventManager.save` which are
+        # necessary for grouping.
+
         normalize_stacktraces_for_grouping(data, load_grouping_config(grouping_config))
 
         data.setdefault("fingerprint", ["{{ default }}"])
         apply_server_side_fingerprinting(data, fingerprinting_config)
+        fingerprint_info = data.get("_fingerprint_info", {})
+        custom_title_template = get_path(fingerprint_info, "matched_rule", "attributes", "title")
+
+        # Technically handling custom titles happens during grouping, not before it, but we're not
+        # running grouping until later, and the title needs to be set before we get metadata below.
+        if custom_title_template:
+            resolved_title = expand_title_template(custom_title_template, data)
+            data["title"] = resolved_title
+
         event_type = get_event_type(data)
         event_metadata = event_type.get_metadata(data)
         data.update(materialize_metadata(data, event_type, event_metadata))
@@ -311,8 +324,18 @@ class FingerprintInput:
         mgr.normalize()
         data = mgr.get_data()
 
+        # Before creating the event, manually run the parts of `EventManager.save` which are
+        # necessary for fingerprinting.
+
         data.setdefault("fingerprint", ["{{ default }}"])
         apply_server_side_fingerprinting(data, config)
+        fingerprint_info = data.get("_fingerprint_info", {})
+        custom_title_template = get_path(fingerprint_info, "matched_rule", "attributes", "title")
+
+        if custom_title_template:
+            resolved_title = expand_title_template(custom_title_template, data)
+            data["title"] = resolved_title
+
         event_type = get_event_type(data)
         event_metadata = event_type.get_metadata(data)
         data.update(materialize_metadata(data, event_type, event_metadata))

tests/sentry/grouping/snapshots/test_fingerprinting/test_event_hash_variant/fingerprint_title_uses_top_in_app_frame.pysnap
@@ -1,0 +1,44 @@
+---
+source: tests/sentry/grouping/test_fingerprinting.py::test_event_hash_variant
+---
+config:
+  rules:
+  - attributes:
+      title: Dogs are great ({{ function }})
+    fingerprint:
+    - '{{ function }}'
+    matchers:
+    - - module
+      - do_dog_stuff
+    text: module:"do_dog_stuff" -> "{{ function }}" title="Dogs are great ({{ function
+      }})"
+  version: 1
+fingerprint:
+- '{{ function }}'
+title: Dogs are great (throw_ball)
+variants:
+  app:
+    component:
+      contributes: false
+      hint: ignored because custom server fingerprint takes precedence
+    contributes: false
+    hint: ignored because custom server fingerprint takes precedence
+    key: app_exception_stacktrace
+    type: component
+  custom_fingerprint:
+    contributes: true
+    hint: null
+    key: custom_fingerprint
+    matched_rule: module:"do_dog_stuff" -> "{{ function }}" title="Dogs are great
+      ({{ function }})"
+    type: custom_fingerprint
+    values:
+    - throw_ball
+  system:
+    component:
+      contributes: false
+      hint: ignored because custom server fingerprint takes precedence
+    contributes: false
+    hint: ignored because custom server fingerprint takes precedence
+    key: system_exception_stacktrace
+    type: component