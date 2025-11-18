ed8ea4a7590aa6fcc89f616ba9fb4ddc4b4f41d0
Christinarlong <christina.long@sentry.io>
2025-11-17 16:19:19 -0800

    truncate to 48 blocks

--- src/sentry/integrations/slack/message_builder/notifications/digest.py
+++ src/sentry/integrations/slack/message_builder/notifications/digest.py
@@ -30,8 +30,10 @@ class DigestNotificationMessageBuilder(SlackNotificationsMessageBuilder):
         """
         digest: Digest = self.context.get("digest", {})
         digest_groups = get_groups(digest)
+        total_issues = len(digest_groups)
         blocks = []
-        for rule, group, event in digest_groups:
+
+        for issue_index, (rule, group, event) in enumerate(digest_groups, start=1):
             alert_as_blocks = SlackIssuesMessageBuilder(
                 group=group,
                 event=event,
@@ -40,8 +42,20 @@ class DigestNotificationMessageBuilder(SlackNotificationsMessageBuilder):
                 notification=self.notification,
                 recipient=self.recipient,
             ).build()
+
+            # Check if adding this issue would exceed 48 blocks
+            # (leaving room for 1 warning block below + 1 title block from notify_recipient = 50 total)
+            if len(alert_as_blocks["blocks"]) + len(blocks) >= 48:
+                blocks.append(
+                    self.get_context_block(
+                        text=f"Showing {issue_index - 1}/{total_issues} issues. This digest was truncated to stay within Slack's 50 block limit. Visit the Sentry project to see all issues."
+                    )
+                )
+                break
+
             # we iterate through the list of blocks created for each alert in the digest and add
             # each block to the list of blocks which is used for the entire digest notification
             for block in alert_as_blocks["blocks"]:
                 blocks.append(block)
+
         return self._build_blocks(*blocks)
