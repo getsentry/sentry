13f0fa4875cfc93e584db07f79bacaeddeafb663
isabellaenriquez <isabella.enriquez@sentry.io>
2025-11-19 13:28:05 -0500

    ref(checkout): Update route

--- static/gsApp/hooks/rootRoutes.tsx
+++ static/gsApp/hooks/rootRoutes.tsx
@@ -1,6 +1,7 @@
 import {makeLazyloadComponent as make} from 'sentry/makeLazyloadComponent';
 import type {SentryRouteObject} from 'sentry/router/types';
 import errorHandler from 'sentry/utils/errorHandler';
+import withDomainRedirect from 'sentry/utils/withDomainRedirect';
 
 import OrganizationSubscriptionContext from 'getsentry/components/organizationSubscriptionContext';
 
@@ -8,10 +9,21 @@ const rootRoutes = (): SentryRouteObject => ({
   children: [
     {
       // TODO(checkout v3): rename this to /checkout/ when the legacy checkout route is removed
-      path: '/checkout-v3/',
+      path: '/checkout/',
       component: errorHandler(OrganizationSubscriptionContext),
       deprecatedRouteProps: true,
       customerDomainOnlyRoute: true,
+      children: [
+        {
+          index: true,
+          component: make(() => import('getsentry/views/decideCheckout')),
+        },
+      ],
+    },
+    {
+      path: '/checkout/:orgId/',
+      component: withDomainRedirect(errorHandler(OrganizationSubscriptionContext)),
+      deprecatedRouteProps: true,
       children: [
         {
           index: true,
