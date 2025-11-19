e1fad56b1f966fc325daa671467f4d0aed21a84e
isabellaenriquez <isabella.enriquez@sentry.io>
2025-11-19 13:45:07 -0500

    add withDomainRequired

--- static/gsApp/hooks/rootRoutes.tsx
+++ static/gsApp/hooks/rootRoutes.tsx
@@ -2,6 +2,7 @@ import {makeLazyloadComponent as make} from 'sentry/makeLazyloadComponent';
 import type {SentryRouteObject} from 'sentry/router/types';
 import errorHandler from 'sentry/utils/errorHandler';
 import withDomainRedirect from 'sentry/utils/withDomainRedirect';
+import withDomainRequired from 'sentry/utils/withDomainRequired';
 
 import OrganizationSubscriptionContext from 'getsentry/components/organizationSubscriptionContext';
 
@@ -10,7 +11,7 @@ const rootRoutes = (): SentryRouteObject => ({
     {
       // TODO(checkout v3): rename this to /checkout/ when the legacy checkout route is removed
       path: '/checkout/',
-      component: errorHandler(OrganizationSubscriptionContext),
+      component: withDomainRequired(errorHandler(OrganizationSubscriptionContext)),
       deprecatedRouteProps: true,
       customerDomainOnlyRoute: true,
       children: [
