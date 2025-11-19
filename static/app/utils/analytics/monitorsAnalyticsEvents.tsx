474d2d17e361267795a5774938e3b82ed814fc79
Scott Cooper <scttcper@gmail.com>
2025-11-19 14:01:08 -0800

    feat(aci): Add monitor created/updated analytics (#103279)
    Adds some basic tracking of type, dataset, aggregate. I think a lot of
    other fields are already normalized in the database and can be queried
    at any time.

--- static/app/utils/analytics/monitorsAnalyticsEvents.tsx
+++ static/app/utils/analytics/monitorsAnalyticsEvents.tsx
@@ -1,5 +1,8 @@
 import type {Organization} from 'sentry/types/organization';
-import {getAutomationAnalyticsPayload} from 'sentry/views/automations/components/forms/common/getAutomationAnalyticsPayload';
+import type {getAutomationAnalyticsPayload} from 'sentry/views/automations/components/forms/common/getAutomationAnalyticsPayload';
+import type {getDetectorAnalyticsPayload} from 'sentry/views/detectors/components/forms/common/getDetectorAnalyticsPayload';
+
+type DetectorAnalyticsEventPayload = ReturnType<typeof getDetectorAnalyticsPayload>;
 
 type AutomationAnalyticsEventPayload = ReturnType<typeof getAutomationAnalyticsPayload>;
 
@@ -14,12 +17,16 @@ export type MonitorsEventParameters = {
     guide: string;
     platform: string;
   };
+  'monitor.created': DetectorAnalyticsEventPayload;
+  'monitor.updated': DetectorAnalyticsEventPayload;
 };
 
 type MonitorsAnalyticsKey = keyof MonitorsEventParameters;
 
 export const monitorsEventMap: Record<MonitorsAnalyticsKey, string> = {
   'landing_page.platform_guide.viewed': 'Crons Landing Page: Viewed Platform Guide',
+  'monitor.created': 'Detectors: Created',
+  'monitor.updated': 'Detectors: Updated',
   'automation.created': 'Automations: Created',
   'automation.updated': 'Automations: Updated',
 };
