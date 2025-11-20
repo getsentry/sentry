a71291bcaee4249ee32147093c99b1f049ea74b5
Kyle Consalus <kyle.consalus@sentry.io>
2025-11-20 13:12:02 -0800

    post-deploy

--- src/sentry/workflow_engine/migrations/0103_workflow_when_condition_group_unique.py
+++ src/sentry/workflow_engine/migrations/0103_workflow_when_condition_group_unique.py
@@ -20,7 +20,7 @@ class Migration(CheckedMigration):
     #   is a schema change, it's completely safe to run the operation after the code has deployed.
     # Once deployed, run these manually via: https://develop.sentry.dev/database-migrations/#migration-deployment
 
-    is_post_deployment = False
+    is_post_deployment = True
 
     dependencies = [
         ("workflow_engine", "0102_cleanup_failed_safe_deletes"),
