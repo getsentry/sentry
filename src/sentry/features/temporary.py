93e77ce6dc15fcd515c678b97e4afdfbd1da2a5e
Charlie Luo <cvxluo@gmail.com>
2025-11-20 14:13:09 -0800

    feat(issues): top issues ui flag (#103776)
    Add a feature flag for dynamic grouping experiment of top issues. See
    https://github.com/getsentry/sentry/pull/103773

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -453,6 +453,8 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:grouptombstones-hit-counter", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable static ClickHouse sampling for `OrganizationTagsEndpoint`
     manager.add("organizations:tag-key-sample-n", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
+    # Enable dynamic grouping UI (top issues)
+    manager.add("organizations:top-issues-ui", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable feature to load new trace view.
     manager.add("organizations:trace-view-v1", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable feature to show link to previous/next traces
