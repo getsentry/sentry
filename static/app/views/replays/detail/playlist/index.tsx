8594471291ceb158f4533689b0e126d910eae249
jerryzhou196 <jerry.zhou@sentry.io>
2025-11-20 14:41:54 -0500

    fix(playlist): Wrap ReplayTable in a div so the table doesn't try to fill the entire container

--- static/app/views/replays/detail/playlist/index.tsx
+++ static/app/views/replays/detail/playlist/index.tsx
@@ -32,14 +32,16 @@ export default function Playlist() {
   const {allMobileProj} = useAllMobileProj({});
   const columns = allMobileProj ? MOBILE_COLUMNS : VISIBLE_COLUMNS;
   return (
-    <ReplayTable
-      columns={columns}
-      error={null}
-      highlightedRowIndex={currentReplayIndex}
-      isPending={replays ? false : true}
-      query={location.query}
-      replays={replays ?? []}
-      showDropdownFilters={false}
-    />
+    <div>
+      <ReplayTable
+        columns={columns}
+        error={null}
+        highlightedRowIndex={currentReplayIndex}
+        isPending={replays ? false : true}
+        query={location.query}
+        replays={replays ?? []}
+        showDropdownFilters={false}
+      />
+    </div>
   );
 }
