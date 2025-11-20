e5845ae8d108f9c67ad64e148a92cf85e292ca59
Irene Liu <71612595+liuirene256@users.noreply.github.com>
2025-11-20 12:52:14 -0600

    chore(integrations): make project_ids not required (#103747)
    Co-authored-by: Irene Liu <ireneliu@R7F6F9L07W.local>

--- static/app/views/settings/organizationDataForwarding/util/forms.tsx
+++ static/app/views/settings/organizationDataForwarding/util/forms.tsx
@@ -91,7 +91,7 @@ function getProjectConfigurationForm({projects}: {projects: Project[]}): JsonFor
         label: 'Forwarding projects',
         type: 'select',
         multiple: true,
-        required: true,
+        required: false,
         defaultValue: [],
         help: 'Select the projects which should forward their data.',
         options: projectOptions,
