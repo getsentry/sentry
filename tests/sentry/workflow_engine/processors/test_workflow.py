88be95b83e1e575735ed5e5cb1c6c4d6467ab8d0
Kyle Consalus <kyle.consalus@sentry.io>
2025-11-21 09:39:17 -0800

    feat(aci): Make Workflow.when_condition_group unique (#103768)
    I was going to document this assumption, but it's roughly just as easy
    to require it and avoid potential weirdness down the road.

--- tests/sentry/workflow_engine/processors/test_workflow.py
+++ tests/sentry/workflow_engine/processors/test_workflow.py
@@ -189,9 +189,9 @@ class TestProcessWorkflows(BaseWorkflowTest):
             workflow=non_matching_env_workflow,
         )
 
-        dcg = self.create_data_condition_group()
+        matching_dcg = self.create_data_condition_group()
         matching_env_workflow = self.create_workflow(
-            when_condition_group=dcg,
+            when_condition_group=matching_dcg,
             environment=env,
         )
         self.create_detector_workflow(
@@ -199,8 +199,9 @@ class TestProcessWorkflows(BaseWorkflowTest):
             workflow=matching_env_workflow,
         )
 
+        mismatched_dcg = self.create_data_condition_group()
         mismatched_env_workflow = self.create_workflow(
-            when_condition_group=dcg, environment=other_env
+            when_condition_group=mismatched_dcg, environment=other_env
         )
         self.create_detector_workflow(
             detector=self.error_detector,
