a7ff6bc4e483d5b46e6f15f46bb2eee61ac53930
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-11-21 12:36:20 -0500

    Add a playground for skeleton UI

--- static/app/stories/playground/slideOverPanel.tsx
+++ static/app/stories/playground/slideOverPanel.tsx
@@ -1,9 +1,13 @@
-import {Fragment, useState} from 'react';
+import {Fragment, useCallback, useState} from 'react';
 import {AnimatePresence} from 'framer-motion';
 
+import {Alert} from '@sentry/scraps/alert';
 import {Button} from '@sentry/scraps/button';
-import {Container} from '@sentry/scraps/layout';
+import {Container, Flex} from '@sentry/scraps/layout';
 import {SlideOverPanel} from '@sentry/scraps/slideOverPanel';
+import {Switch} from '@sentry/scraps/switch';
+
+import Placeholder from 'sentry/components/placeholder';
 
 export function SlideOverPanelPlayground() {
   const [isPanelOpen, setIsPanelOpen] = useState<boolean>(false);
@@ -24,3 +28,69 @@ export function SlideOverPanelPlayground() {
     </Fragment>
   );
 }
+
+export function SlideOverPanelSkeletonPlayground() {
+  const [isPanelOpen, setIsPanelOpen] = useState<boolean>(false);
+  const [isSkeletonEnabled, setIsSkeletonEnabled] = useState<boolean>(false);
+
+  const closePanel = useCallback(() => {
+    setIsPanelOpen(false);
+  }, []);
+
+  return (
+    <Fragment>
+      <Flex direction="column" gap="md">
+        <Flex gap="md" as="label">
+          Enable Skeleton UI
+          <Switch
+            checked={isSkeletonEnabled}
+            onChange={() => setIsSkeletonEnabled(!isSkeletonEnabled)}
+          />
+        </Flex>
+        <Button onClick={() => setIsPanelOpen(true)}>Open Panel</Button>
+      </Flex>
+
+      <AnimatePresence>
+        <SlideOverPanel
+          collapsed={!isPanelOpen}
+          slidePosition="right"
+          skeleton={isSkeletonEnabled ? <SkeletonPanel onClick={closePanel} /> : null}
+        >
+          <Flex direction="column" border="primary" height="100%" gap="md" padding="md">
+            <Button onClick={closePanel}>Close Panel</Button>
+            <Container>
+              <VerySlowComponent />
+            </Container>
+          </Flex>
+        </SlideOverPanel>
+      </AnimatePresence>
+    </Fragment>
+  );
+}
+
+interface SkeletonPanelProps {
+  onClick: ComponentProps<typeof Button>['onClick'];
+}
+
+export function SkeletonPanel({onClick}: SkeletonPanelProps) {
+  return (
+    <Flex direction="column" border="primary" height="100%" gap="md" padding="md">
+      <Button onClick={onClick}>Close Panel</Button>
+      <Container>
+        <Placeholder />
+      </Container>
+    </Flex>
+  );
+}
+
+export function VerySlowComponent() {
+  const start = performance.now();
+
+  while (performance.now() < start + SLOW_COMPONENT_RENDER_DURATION) {
+    // Do nothing
+  }
+
+  return <Alert type="warning">I took a very long time to render!</Alert>;
+}
+
+const SLOW_COMPONENT_RENDER_DURATION = 2000;
