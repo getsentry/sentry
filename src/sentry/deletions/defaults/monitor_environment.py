4388cbf2ecc3f5d1f74be92129a9e5c4a4899cee
Yuval Mandelboum <yuval.mandelboum@sentry.io>
2025-11-20 16:00:25 -0800

    fix(deletion): Fix MonitorCheckIn deletion failures (#103786)
    Our previous attempt (#103495) at correcting processing timeouts for
    MonitorCheckIn deletions failed , and introduced integrity errors
    because MonitorCheckIn cant be bulk deleted without manually removing
    child dependancies (no CASCADE in db FKs).
    Added deletion of MonitorIncident directly before attempting to bulk
    delete MonitorCheckIns, basically removing the children of
    MonitorCheckins before deleting them.

--- src/sentry/deletions/defaults/monitor_environment.py
+++ src/sentry/deletions/defaults/monitor_environment.py
@@ -1,9 +1,4 @@
-from sentry.deletions.base import (
-    BaseRelation,
-    BulkModelDeletionTask,
-    ModelDeletionTask,
-    ModelRelation,
-)
+from sentry.deletions.base import BaseRelation, ModelDeletionTask, ModelRelation
 from sentry.monitors.models import MonitorEnvironment
 
 
@@ -13,8 +8,11 @@ class MonitorEnvironmentDeletionTask(ModelDeletionTask[MonitorEnvironment]):
 
         return [
             ModelRelation(
+                models.MonitorIncident,
+                {"monitor_environment_id": instance.id},
+            ),
+            ModelRelation(
                 models.MonitorCheckIn,
                 {"monitor_environment_id": instance.id},
-                BulkModelDeletionTask,
             ),
         ]
