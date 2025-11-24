53588e9cea7ebf79fcbd2c272a9a7fb7d7eb5bc7
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2025-11-24 17:38:41 +0100

    ref: set checkJs globally to true (#103890)
    This ensures `.js` and `.mjs` files are type-checked (with jsdoc
    comments) per default.
    
    We’ll need this more when e.g. writing custom eslint rules, as they have
    to be `.mjs` because we don’t have a typescript transform pipeline and
    eslint can’t natively read ts.
    
    see:
    - https://github.com/getsentry/sentry/pull/103889

--- tests/js/jest-pegjs-transform.js
+++ tests/js/jest-pegjs-transform.js
@@ -3,6 +3,15 @@
 const nodeCrypto = require('node:crypto');
 const peggy = require('peggy');
 
+/**
+ * Generate cache key for Jest transformer
+ * @param {string} fileData - The file content to transform
+ * @param {string} _filePath - Path to the file (unused)
+ * @param {Object} config - Jest configuration object
+ * @param {string} config.configString - Stringified Jest config
+ * @param {Object} _options - Transform options (unused)
+ * @returns {string} MD5 hash for cache key
+ */
 function getCacheKey(fileData, _filePath, config, _options) {
   return nodeCrypto
     .createHash('md5')
@@ -11,6 +20,11 @@ function getCacheKey(fileData, _filePath, config, _options) {
     .digest('hex');
 }
 
+/**
+ * Transform PEG.js grammar file into JavaScript module
+ * @param {string} sourceText - The PEG.js grammar source code
+ * @returns {{code: string}} Transform result with generated JavaScript module code
+ */
 function process(sourceText) {
   return {
     code: `module.exports = ${peggy.generate(sourceText, {output: 'source'})}`,
