a05b1cccf3d4d48b46b0ac8b662ae0bb1eca4168
Jesse Box <me@jessebox.net>
2025-11-24 12:14:24 +0100

    Removed: isCircled prop from icons and their instances

--- static/app/icons/icons.stories.tsx
+++ static/app/icons/icons.stories.tsx
@@ -390,10 +390,9 @@ const SECTIONS: TSection[] = [
           'previous',
           'west',
         ],
-        additionalProps: ['isCircled', 'direction', 'isDouble'],
+        additionalProps: ['direction', 'isDouble'],
         name: 'Chevron',
         defaultProps: {
-          isCircled: false,
           direction: 'left',
           isDouble: false,
         },
@@ -413,7 +412,6 @@ const SECTIONS: TSection[] = [
         ],
         name: 'Chevron',
         defaultProps: {
-          isCircled: false,
           direction: 'right',
         },
       },
@@ -423,7 +421,6 @@ const SECTIONS: TSection[] = [
         keywords: ['up', 'point', 'direct', 'move', 'arrow', 'top', 'north', 'collapse'],
         name: 'Chevron',
         defaultProps: {
-          isCircled: false,
           direction: 'up',
         },
       },
@@ -442,7 +439,6 @@ const SECTIONS: TSection[] = [
         ],
         name: 'Chevron',
         defaultProps: {
-          isCircled: false,
           direction: 'down',
         },
       },
@@ -772,69 +768,29 @@ const SECTIONS: TSection[] = [
         id: 'add',
         groups: ['action'],
         keywords: ['plus', 'create', 'new', 'insert', 'math'],
-        additionalProps: ['isCircled'],
         name: 'Add',
-        defaultProps: {
-          isCircled: false,
-        },
-      },
-      {
-        id: 'add-isCircled',
-        name: 'Add',
-        defaultProps: {
-          isCircled: true,
-        },
+        defaultProps: {},
       },
       {
         id: 'subtract',
         groups: ['action'],
         keywords: ['minus', 'remove', 'decrease', 'delete', 'math'],
-        additionalProps: ['isCircled'],
         name: 'Subtract',
-        defaultProps: {
-          isCircled: false,
-        },
-      },
-      {
-        id: 'subtract-isCircled',
-        name: 'Subtract',
-        defaultProps: {
-          isCircled: true,
-        },
+        defaultProps: {},
       },
       {
         id: 'checkmark',
         groups: ['action'],
         keywords: ['done', 'finish', 'success', 'confirm', 'resolve'],
-        additionalProps: ['isCircled'],
         name: 'Checkmark',
-        defaultProps: {
-          isCircled: false,
-        },
-      },
-      {
-        id: 'checkmark-isCircled',
-        name: 'Checkmark',
-        defaultProps: {
-          isCircled: true,
-        },
+        defaultProps: {},
       },
       {
         id: 'close',
         groups: ['action'],
         keywords: ['cross', 'deny', 'terminate', 'x', 'cancel', 'exit'],
-        additionalProps: ['isCircled'],
         name: 'Close',
-        defaultProps: {
-          isCircled: false,
-        },
-      },
-      {
-        id: 'close-isCircled',
-        name: 'Close',
-        defaultProps: {
-          isCircled: true,
-        },
+        defaultProps: {},
       },
       {
         id: 'divide',
@@ -1913,7 +1869,7 @@ function propsToVariant(props: Record<string, unknown>): string | null {
     if (['type', 'direction', 'variant'].includes(key)) {
       return typeof value === 'string' ? value : null;
     }
-    // isSolid, isCircled, isZoomIn
+    // isSolid, isZoomIn
     if (key.startsWith('is') && value) {
       return lowerFirst(key.replace('is', ''));
     }
