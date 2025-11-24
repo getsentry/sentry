831f987a97ecb495ad99c4a7e68107eb811ddea0
Colleen O'Rourke <colleen@sentry.io>
2025-11-24 11:59:27 -0800

    make post deployment

--- src/sentry/workflow_engine/migrations/0104_action_data_fallthrough_type.py
+++ src/sentry/workflow_engine/migrations/0104_action_data_fallthrough_type.py
@@ -32,7 +32,7 @@ class Migration(CheckedMigration):
     #   is a schema change, it's completely safe to run the operation after the code has deployed.
     # Once deployed, run these manually via: https://develop.sentry.dev/database-migrations/#migration-deployment
 
-    is_post_deployment = False
+    is_post_deployment = True
 
     dependencies = [
         ("workflow_engine", "0103_add_unique_constraint"),
