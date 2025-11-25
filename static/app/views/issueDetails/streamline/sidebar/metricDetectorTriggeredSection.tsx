a549509e20ba05f23471c5323df805fc8227e5c3
Malachi Willey <malwilley@gmail.com>
2025-11-25 11:29:43 -0800

    Reorganize component

--- static/app/views/issueDetails/streamline/sidebar/metricDetectorTriggeredSection.tsx
+++ static/app/views/issueDetails/streamline/sidebar/metricDetectorTriggeredSection.tsx
@@ -1,5 +1,6 @@
 import {Fragment} from 'react';
 
+import ErrorBoundary from 'sentry/components/errorBoundary';
 import KeyValueList from 'sentry/components/events/interfaces/keyValueList';
 import {AnnotatedText} from 'sentry/components/events/meta/annotatedText';
 import type {Event} from 'sentry/types/event';
@@ -11,12 +12,18 @@ import {getExactDuration} from 'sentry/utils/duration/getExactDuration';
 import {getConditionDescription} from 'sentry/views/detectors/components/details/metric/detect';
 import {InterimSection} from 'sentry/views/issueDetails/streamline/interimSection';
 
-/**
- * Evidence data for metric detector triggered events
- */
-export interface MetricDetectorEvidenceData {
+interface MetricDetectorEvidenceData {
+  /**
+   * The triggered conditions that caused the occurrence to be created
+   */
   conditions: MetricCondition[];
+  /**
+   * The data source at the time that the occurrence was created
+   */
   dataSources: [SnubaQueryDataSource];
+  /**
+   * The evaluated value when the occurrence was created
+   */
   value: number;
 }
 
@@ -24,9 +31,7 @@ interface MetricDetectorTriggeredSectionProps {
   event: Event;
 }
 
-export function MetricDetectorTriggeredSection({
-  event,
-}: MetricDetectorTriggeredSectionProps) {
+function TriggeredConditionDetails({event}: {event: Event}) {
   const evidenceData = event.occurrence?.evidenceData as
     | MetricDetectorEvidenceData
     | undefined;
@@ -42,16 +47,62 @@ export function MetricDetectorTriggeredSection({
   const {conditions, dataSources, value} = evidenceData;
   const dataSource = dataSources[0];
   const snubaQuery = dataSource?.queryObj?.snubaQuery;
-
-  // Find the first triggered condition
-  const triggeredCondition = conditions.find(
-    condition => condition.conditionResult !== undefined
-  );
+  const triggeredCondition = conditions[0];
 
   if (!triggeredCondition || !snubaQuery) {
     return null;
   }
 
+  return (
+    <InterimSection title="Triggered Condition" type="triggered_condition">
+      <KeyValueList
+        shouldSort={false}
+        data={[
+          {
+            key: 'Aggregate',
+            value: snubaQuery.aggregate,
+            subject: 'Aggregate',
+          },
+          ...(snubaQuery.query
+            ? [
+                {
+                  key: 'query',
+                  value: snubaQuery.query,
+                  subject: 'Query',
+                },
+              ]
+            : []),
+          {
+            key: 'interval',
+            value: getExactDuration(snubaQuery.timeWindow),
+            subject: 'Interval',
+          },
+          {
+            key: 'condition',
+            value: getConditionDescription({
+              aggregate: snubaQuery.aggregate,
+              condition: triggeredCondition,
+              // TODO: Record detector config in issue occurrence and use that here
+              config: {
+                detectionType: 'static',
+              },
+            }),
+            subject: 'Triggered Condition',
+          },
+          {
+            key: 'value',
+            value,
+            subject: 'Evaluated Value',
+          },
+        ]}
+      />
+    </InterimSection>
+  );
+}
+
+export function MetricDetectorTriggeredSection({
+  event,
+}: MetricDetectorTriggeredSectionProps) {
   const message = event.occurrence?.subtitle;
 
   return (
@@ -61,49 +112,9 @@ export function MetricDetectorTriggeredSection({
           <AnnotatedText value={message} />
         </InterimSection>
       )}
-      <InterimSection title="Triggered Condition" type="triggered_condition">
-        <KeyValueList
-          shouldSort={false}
-          data={[
-            {
-              key: 'Aggregate',
-              value: snubaQuery.aggregate,
-              subject: 'Aggregate',
-            },
-            ...(snubaQuery.query
-              ? [
-                  {
-                    key: 'query',
-                    value: snubaQuery.query,
-                    subject: 'Query',
-                  },
-                ]
-              : []),
-            {
-              key: 'interval',
-              value: getExactDuration(snubaQuery.timeWindow),
-              subject: 'Interval',
-            },
-            {
-              key: 'condition',
-              value: getConditionDescription({
-                aggregate: snubaQuery.aggregate,
-                condition: triggeredCondition,
-                // TODO: Record detector config in issue occurrence and use that here
-                config: {
-                  detectionType: 'static',
-                },
-              }),
-              subject: 'Triggered Condition',
-            },
-            {
-              key: 'value',
-              value,
-              subject: 'Evaluated Value',
-            },
-          ]}
-        />
-      </InterimSection>
+      <ErrorBoundary mini>
+        <TriggeredConditionDetails event={event} />
+      </ErrorBoundary>
     </Fragment>
   );
 }
