48a3366219e13e24c53bdc6677397e9dc2f14d02
Tony Xiao <txiao@sentry.io>
2025-11-25 13:40:00 -0500

    feat(explore): Add hooks for date page filter props based on data cat… (#103931)
    …egory
    
    This replaces the custom functions we wrote for each data category with
    a hook that accepts a list of data categories so we can automatically
    determine what the expected date page filter props should be. This sets
    it up so that we can replace the hook with an alternate implementation
    in order to support the coming retention changes.
    
    Same as #103822 but fixed typing issues in 17c69c7

--- static/app/views/explore/logs/content.tsx
+++ static/app/views/explore/logs/content.tsx
@@ -8,10 +8,13 @@ import {withoutLoggingSupport} from 'sentry/data/platformCategories';
 import {platforms} from 'sentry/data/platforms';
 import {IconMegaphone, IconOpen} from 'sentry/icons';
 import {t} from 'sentry/locale';
+import {DataCategory} from 'sentry/types/core';
 import {defined} from 'sentry/utils';
 import {trackAnalytics} from 'sentry/utils/analytics';
 import {LogsAnalyticsPageSource} from 'sentry/utils/analytics/logsAnalyticsEvent';
+import {useDatePageFilterProps} from 'sentry/utils/useDatePageFilterProps';
 import {useFeedbackForm} from 'sentry/utils/useFeedbackForm';
+import {useMaxPickableDays} from 'sentry/utils/useMaxPickableDays';
 import useOrganization from 'sentry/utils/useOrganization';
 import usePageFilters from 'sentry/utils/usePageFilters';
 import useProjects from 'sentry/utils/useProjects';
@@ -22,7 +25,6 @@ import {useGetSavedQuery} from 'sentry/views/explore/hooks/useGetSavedQueries';
 import {LogsTabOnboarding} from 'sentry/views/explore/logs/logsOnboarding';
 import {LogsQueryParamsProvider} from 'sentry/views/explore/logs/logsQueryParamsProvider';
 import {LogsTabContent} from 'sentry/views/explore/logs/logsTab';
-import {logsPickableDays} from 'sentry/views/explore/logs/utils';
 import {
   useQueryParamsId,
   useQueryParamsTitle,
@@ -58,22 +60,30 @@ function FeedbackButton() {
 
 export default function LogsContent() {
   const organization = useOrganization();
-  const {defaultPeriod, maxPickableDays, relativeOptions} = logsPickableDays();
+  const maxPickableDays = useMaxPickableDays({
+    dataCategories: [DataCategory.LOG_BYTE],
+    organization,
+  });
+  const datePageFilterProps = useDatePageFilterProps(maxPickableDays);
 
   const onboardingProject = useOnboardingProject({property: 'hasLogs'});
 
   return (
     <SentryDocumentTitle title={t('Logs')} orgSlug={organization?.slug}>
       <PageFiltersContainer
-        maxPickableDays={maxPickableDays}
-        defaultSelection={{
-          datetime: {
-            period: defaultPeriod,
-            start: null,
-            end: null,
-            utc: null,
-          },
-        }}
+        maxPickableDays={datePageFilterProps.maxPickableDays}
+        defaultSelection={
+          datePageFilterProps.defaultPeriod
+            ? {
+                datetime: {
+                  period: datePageFilterProps.defaultPeriod,
+                  start: null,
+                  end: null,
+                  utc: null,
+                },
+              }
+            : undefined
+        }
       >
         <LogsQueryParamsProvider
           analyticsPageSource={LogsAnalyticsPageSource.EXPLORE_LOGS}
@@ -87,16 +97,10 @@ export default function LogsContent() {
                   <LogsTabOnboarding
                     organization={organization}
                     project={onboardingProject}
-                    defaultPeriod={defaultPeriod}
-                    maxPickableDays={maxPickableDays}
-                    relativeOptions={relativeOptions}
+                    datePageFilterProps={datePageFilterProps}
                   />
                 ) : (
-                  <LogsTabContent
-                    defaultPeriod={defaultPeriod}
-                    maxPickableDays={maxPickableDays}
-                    relativeOptions={relativeOptions}
-                  />
+                  <LogsTabContent datePageFilterProps={datePageFilterProps} />
                 )}
               </LogsPageDataProvider>
             </TraceItemAttributeProvider>
