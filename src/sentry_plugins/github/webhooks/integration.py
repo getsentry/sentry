17e58f38e2160026eb4ee54105849bde37c00935
Mark Story <mark@mark-story.com>
2025-11-26 13:35:58 -0500

    Relax silo mode of github plugin

--- src/sentry_plugins/github/webhooks/integration.py
+++ src/sentry_plugins/github/webhooks/integration.py
@@ -10,7 +10,7 @@ from rest_framework.request import Request
 
 from sentry import options
 from sentry.models.organization import Organization
-from sentry.web.frontend.base import region_silo_view
+from sentry.web.frontend.base import all_silo_view
 
 from .base import GithubWebhookBase
 from .events import InstallationEventWebhook, InstallationRepositoryEventWebhook, PushEventWebhook
@@ -18,7 +18,7 @@ from .events import InstallationEventWebhook, InstallationRepositoryEventWebhook
 logger = logging.getLogger(__name__)
 
 
-@region_silo_view
+@all_silo_view
 class GithubPluginIntegrationsWebhookEndpoint(GithubWebhookBase):
     _handlers = {
         "push": PushEventWebhook,
