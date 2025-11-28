1a08385f227392438b49e2483be13e98ba2234dc
nikkikapadia <nikki.kapadia@sentry.io>
2025-11-28 13:53:50 -0500

    add migration check

---
+++ static/app/views/detectors/components/details/metric/utils/useIsMigratedExtrapolation.tsx
@@ -0,0 +1,17 @@
+import {ExtrapolationMode} from 'sentry/views/alerts/rules/metric/types';
+import {DetectorDataset} from 'sentry/views/detectors/datasetConfig/types';
+
+export function useIsMigratedExtrapolation({
+  extrapolationMode,
+  dataset,
+}: {
+  dataset: DetectorDataset;
+  extrapolationMode: ExtrapolationMode | undefined;
+}) {
+  return !!(
+    dataset === DetectorDataset.SPANS &&
+    extrapolationMode &&
+    (extrapolationMode === ExtrapolationMode.SERVER_WEIGHTED ||
+      extrapolationMode === ExtrapolationMode.NONE)
+  );
+}
