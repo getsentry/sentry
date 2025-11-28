23974e591b892fe409dd36b2db0230ee391d9833
Jesse Box <me@jessebox.net>
2025-11-28 14:36:18 +0100

    Removed: prop 'data-test-id'

--- static/app/icons/iconSlashForward.tsx
+++ static/app/icons/iconSlashForward.tsx
@@ -3,7 +3,7 @@ import {SvgIcon} from './svgIcon';
 
 export function IconSlashForward(props: SVGIconProps) {
   return (
-    <SvgIcon {...props} data-test-id="icon-add">
+    <SvgIcon {...props}>
       <path d="M9.544 1.496a.751.751 0 0 1 1.412.508l-4.5 12.5a.75.75 0 0 1-1.412-.508z" />
     </SvgIcon>
   );
