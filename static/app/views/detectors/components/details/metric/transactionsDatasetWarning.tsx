74d936dad5c589a08cc68c88f25211db17e46eb7
nikkikapadia <nikki.kapadia@sentry.io>
2025-11-28 14:13:43 -0500

    warning banner for monitor details page

--- static/app/views/detectors/components/details/metric/transactionsDatasetWarning.tsx
+++ static/app/views/detectors/components/details/metric/transactionsDatasetWarning.tsx
@@ -1,7 +1,10 @@
 import {Alert} from 'sentry/components/core/alert';
-import {ExternalLink} from 'sentry/components/core/link';
+import {ExternalLink, Link} from 'sentry/components/core/link';
 import {tctCode} from 'sentry/locale';
+import type {MetricDetector} from 'sentry/types/workflowEngine/detectors';
 import useOrganization from 'sentry/utils/useOrganization';
+import {makeMonitorDetailsPathname} from 'sentry/views/detectors/pathnames';
+import {useCanEditDetector} from 'sentry/views/detectors/utils/useCanEditDetector';
 
 export const TRANSACTIONS_DATASET_DEPRECATION_MESSAGE = tctCode(
   'The transaction dataset is being deprecated. Please use Span alerts instead. Spans are a superset of transactions, you can isolate transactions by using the [code:is_transaction:true] filter. Please read these [FAQLink:FAQs] for more information.',
@@ -22,4 +25,27 @@ export function TransactionsDatasetWarning() {
   }
 
   return <Alert type="warning">{TRANSACTIONS_DATASET_DEPRECATION_MESSAGE}</Alert>;
+}
+
+export function MigratedAlertWarning({detector}: {detector: MetricDetector}) {
+  const organization = useOrganization();
+  const canEdit = useCanEditDetector({
+    detectorType: detector.type,
+    projectId: detector.projectId,
+  });
+
+  const editLink = `${makeMonitorDetailsPathname(organization.slug, detector.id)}edit/`;
+
+  return (
+    <Alert.Container>
+      <Alert type="warning">
+        {tctCode(
+          'This alert has been migrated from a transaction-based alert to a span-based alert. We have set a different extrapolation mode to mimic the previous alert behavior but this mode will be deprecated. Please [editLink:edit] the thresholds to match the regular extrapolation mode.',
+          {
+            editLink: <Link to={editLink} disabled={!canEdit} />,
+          }
+        )}
+      </Alert>
+    </Alert.Container>
+  );
 }
