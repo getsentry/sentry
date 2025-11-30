238b22811aa75e93ba0238532b82dcc36171dd55
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-11-30 15:47:00 -0500

    feat(explore-attr-breakdowns): Adding comments

--- static/app/views/explore/hooks/useAttributeBreakdownsTooltip.tsx
+++ static/app/views/explore/hooks/useAttributeBreakdownsTooltip.tsx
@@ -10,10 +10,25 @@ import {
   useSetQueryParamsGroupBys,
 } from 'sentry/views/explore/queryParams/context';
 
+const TOOLTIP_POSITION_X_OFFSET = 20;
+const TOOLTIP_POSITION_Y_OFFSET = 5;
+
 type Params = {
+  /**
+   * The ref to the chart component.
+   */
   chartRef: React.RefObject<ReactEchartsRef | null>;
+  /**
+   * The width of the chart. Used to dynamically position the tooltip to mitigate content fron being cut off.
+   */
   chartWidth: number;
+  /**
+   * The formatter function to format the tooltip content.
+   */
   formatter: (params: TooltipComponentFormatterCallbackParams) => string;
+  /**
+   * The function to render the HTML for the tooltip actions. The actions are rendered on click, anywhere on the chart.
+   */
   actionsHtmlRenderer?: (value: string) => string;
 };
 
@@ -24,6 +39,10 @@ export enum Actions {
   COPY_TO_CLIPBOARD = 'copy_value_to_clipboard',
 }
 
+// This hook creates a tooltip configuration for attribute breakdowns charts.
+// Since echarts tooltips do not support actions out of the box, we need to handle them manually.
+// It is used to freeze the tooltip position and show the tooltip actions on click, anywhere on the chart.
+// So that users can intuitively enter the tooltip and click on the actions.
 export function useAttributeBreakdownsTooltip({
   chartRef,
   formatter,
@@ -37,6 +56,10 @@ export function useAttributeBreakdownsTooltip({
   const copyToClipboard = useCopyToClipboard();
   const setGroupBys = useSetQueryParamsGroupBys();
 
+  // This effect runs on load and when the frozen position changes.
+  // - If frozen position is set, trigger a re-render of the tooltip with frozen position to show the
+  //   tooltip actions (if passed in).
+  // - Sets up the listener for clicks anywhere on the chart to toggle the frozen tooltip state.
   useEffect(() => {
     const chartInstance: ECharts | undefined = chartRef.current?.getEchartsInstance();
     if (!chartInstance) return;
@@ -55,10 +78,12 @@ export function useAttributeBreakdownsTooltip({
       event.preventDefault();
       const pixelPoint: [number, number] = [event.offsetX, event.offsetY];
 
+      // If the tooltip is frozen, toggle the frozen state and reset the tooltip params.
       if (frozenPosition) {
         setFrozenPosition(null);
         tooltipParamsRef.current = null;
       } else {
+        // If the tooltip is not frozen, set the frozen position to the current pixel point.
         setFrozenPosition(pixelPoint);
       }
     };
@@ -71,6 +96,8 @@ export function useAttributeBreakdownsTooltip({
     };
   }, [chartRef, frozenPosition]);
 
+  // This effect sets up the on click listeners for the tooltip actions.
+  // e-charts tooltips do not support actions out of the box, so we need to handle them manually.
   useEffect(() => {
     if (!frozenPosition) return;
 
@@ -109,6 +136,8 @@ export function useAttributeBreakdownsTooltip({
       }
     };
 
+    // TODO Abdullah Khan: For now, attaching the listener to document.body
+    // Tried using a more specific selector causes weird race conditions, will need to investigate.
     document.addEventListener('click', handleClickActions);
 
     // eslint-disable-next-line consistent-return
@@ -123,14 +152,29 @@ export function useAttributeBreakdownsTooltip({
     renderMode: 'html',
     enterable: true,
     formatter: (params: TooltipComponentFormatterCallbackParams) => {
+      // If the tooltip is NOT frozen, set the tooltip params and return the formatted content,
+      // including the tooltip actions placeholder.
       if (!frozenPosition) {
         tooltipParamsRef.current = params;
-        return (
-          formatter(params) +
-          '<div class="tooltip-footer" style="display: flex; justify-content: center; padding: 10px;">Click for actions</div>'
-        );
+
+        const actionsPlaceholder = `
+          <div
+            class="tooltip-footer"
+            style="
+              display: flex;
+              justify-content: center;
+              align-items: center;
+              padding: 10px;
+            "
+          >
+            Click for actions
+          </div>
+        `.trim();
+
+        return formatter(params) + actionsPlaceholder;
       }
 
+      // If the tooltip is frozen, return the formatted content, including the tooltip actions.
       const value = (Array.isArray(params) ? params[0]?.name : params.name) ?? '';
       return formatter(tooltipParamsRef.current!) + actionsHtmlRenderer?.(value);
     },
@@ -143,13 +187,14 @@ export function useAttributeBreakdownsTooltip({
       const tooltipWidth = dom?.offsetWidth ?? 0;
       const [rawX = 0, rawY = 0] = frozenPosition ?? point;
 
-      const offsetX = 20;
-      let x = rawX + offsetX;
-      const y = rawY + 5;
+      // Adding offsets to mitigate users accidentally entering the tooltip,
+      // when trying to hover over the chart for values.
+      let x = rawX + TOOLTIP_POSITION_X_OFFSET;
+      const y = rawY - TOOLTIP_POSITION_Y_OFFSET;
 
-      // flip left if it overflows chart width
+      // Flip left if it overflows chart width. Mitigates the content from being cut off.
       if (x + tooltipWidth > chartWidth) {
-        x = rawX - tooltipWidth - offsetX;
+        x = rawX - tooltipWidth - TOOLTIP_POSITION_X_OFFSET;
       }
 
       return [x, y];
