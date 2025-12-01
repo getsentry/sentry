92513ef9d9e01d94e650d3d8fecb72a71486206a
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-12-01 11:49:29 -0800

    Update static/gsApp/views/seerAutomation/components/seerSettingsPageWrapper.tsx
    Co-authored-by: Michelle Zhang <56095982+michellewzhang@users.noreply.github.com>

--- static/gsApp/views/seerAutomation/components/seerSettingsPageWrapper.tsx
+++ static/gsApp/views/seerAutomation/components/seerSettingsPageWrapper.tsx
@@ -27,7 +27,7 @@ export default function SeerSettingsPageWrapper({children}: Props) {
       <SettingsPageHeader
         title={t('Seer')}
         subtitle={tct(
-          'Choose how Seer automatically triages and diagnoses incoming issues, before you even notice them. Seer currently includes [autofix:Autofix], an agent that can root-cause issues and create Pull Requests; and [code_review:AI Code Review], an agent that will review your Pull Requests to detect issues before they happen.',
+          'Choose how Seer automatically triages and diagnoses incoming issues before you even notice them. Seer currently includes [autofix:Autofix], an agent that can root-cause issues and create pull requests, and [code_review:AI Code Review], an agent that will review your pull requests to detect issues before they happen.',
           {
             autofix: (
               <ExternalLink href="https://docs.sentry.io/product/ai-in-sentry/seer/issue-fix/#root-cause-analysis" />
