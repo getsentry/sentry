a1aaacb360c7e720f4b32e2d56a1efd20fac9b6b
jerryzhou196 <jerry.zhou@sentry.io>
2025-12-01 17:45:56 -0500

    undid avatarStyle marginOffset

--- static/app/components/core/avatar/avatarList.tsx
+++ static/app/components/core/avatar/avatarList.tsx
@@ -165,6 +165,7 @@ const AvatarListWrapper = styled('div')`
 
 const AvatarStyle = (p: {theme: Theme}) => css`
   border: 2px solid ${p.theme.background};
+  margin-left: -8px;
   cursor: default;
 
   &:hover {
