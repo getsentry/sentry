0be4b9dc1a4be22eb69393ebe7aad41ef7c610d0
Dominik Buszowiecki <DominikB2014@gmail.com>
2025-12-02 10:26:42 -0500

    move util

---
+++ static/app/views/dashboards/utils/usePopulateLinkedDashboards.tsx
@@ -0,0 +1,70 @@
+import {useMemo} from 'react';
+
+import {defined} from 'sentry/utils';
+import {useApiQuery} from 'sentry/utils/queryClient';
+import useOrganization from 'sentry/utils/useOrganization';
+import type {DashboardDetails} from 'sentry/views/dashboards/types';
+import type {PrebuiltDashboard} from 'sentry/views/dashboards/utils/prebuiltConfigs';
+
+export const usePopulateLinkedDashboards = (dashboard?: PrebuiltDashboard) => {
+  const organization = useOrganization();
+
+  const widgets = useMemo(() => dashboard?.widgets ?? [], [dashboard]);
+
+  const prebuiltIds = useMemo(
+    () =>
+      widgets
+        .flatMap(widget => {
+          return widget.queries
+            .flatMap(query => query.linkedDashboards ?? [])
+            .filter(defined);
+        })
+        .map(d => d.staticDashboardId)
+        .filter(defined),
+    [widgets]
+  );
+
+  const hasLinkedDashboards = prebuiltIds.length > 0;
+  const path = `/organizations/${organization.slug}/dashboards/`;
+
+  const {data, isLoading} = useApiQuery<DashboardDetails[]>(
+    [
+      path,
+      {
+        query: {prebuiltId: prebuiltIds.sort()},
+      },
+    ],
+    {
+      enabled: hasLinkedDashboards,
+      staleTime: 0,
+      retry: false,
+    }
+  );
+
+  return useMemo(() => {
+    if (!hasLinkedDashboards || !data) {
+      return {dashboard, isLoading: false};
+    }
+
+    const populatedDashboard = {
+      ...dashboard,
+      widgets: widgets.map(widget => ({
+        ...widget,
+        queries: widget.queries.map(query => ({
+          ...query,
+          linkedDashboards: query.linkedDashboards?.map(linkedDashboard => {
+            if (!linkedDashboard.staticDashboardId) {
+              return linkedDashboard;
+            }
+            const dashboardId = data.find(
+              d => d.prebuiltId === linkedDashboard.staticDashboardId
+            )?.id;
+            return dashboardId ? {...linkedDashboard, dashboardId} : linkedDashboard;
+          }),
+        })),
+      })),
+    };
+
+    return {dashboard: populatedDashboard, isLoading};
+  }, [dashboard, widgets, data, hasLinkedDashboards, isLoading]);
+};
