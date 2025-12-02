3c474ecf477e26b5f034ffadf13303134986d015
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2025-12-02 13:28:32 -0500

    feat(dashboards): update query module prebuilt titles (#104243)

--- src/sentry/dashboards/endpoints/organization_dashboards.py
+++ src/sentry/dashboards/endpoints/organization_dashboards.py
@@ -80,6 +80,7 @@ class PrebuiltDashboard(TypedDict):
 # deprecate once this feature is released.
 # Note B: Consider storing all dashboard and widget data in the database instead of relying on matching
 # prebuilt_id on the frontend, if there are issues.
+# Note C: These titles should match the titles in the frontend so that the results returned by the API match the titles in the frontend.
 PREBUILT_DASHBOARDS: list[PrebuiltDashboard] = [
     {
         "prebuilt_id": PrebuiltDashboardId.FRONTEND_SESSION_HEALTH,
@@ -87,11 +88,11 @@ PREBUILT_DASHBOARDS: list[PrebuiltDashboard] = [
     },
     {
         "prebuilt_id": PrebuiltDashboardId.BACKEND_QUERIES,
-        "title": "Queries Overview",
+        "title": "Queries",
     },
     {
         "prebuilt_id": PrebuiltDashboardId.BACKEND_QUERIES_SUMMARY,
-        "title": "Query Summary",
+        "title": "Query Details",
     },
 ]
 
