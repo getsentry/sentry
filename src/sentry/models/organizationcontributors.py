03f4e05bc376906add906aa2667dd84682a8f723
Ajay Singh <ajay.singh@sentry.io>
2025-12-02 10:20:02 -0800

    update integration_id cascade to null on oc table

--- src/sentry/models/organizationcontributors.py
+++ src/sentry/models/organizationcontributors.py
@@ -21,7 +21,7 @@ class OrganizationContributors(DefaultFieldsModel):
 
     organization = FlexibleForeignKey("sentry.Organization", on_delete=models.CASCADE)
 
-    integration_id = HybridCloudForeignKey("sentry.Integration", on_delete="CASCADE")
+    integration_id = HybridCloudForeignKey("sentry.Integration", on_delete="SET_NULL", null=True)
 
     external_identifier = models.CharField(max_length=255, db_index=True)
     alias = models.CharField(max_length=255, null=True, blank=True)
