bc83860b072f1972441c6fe2898d3cf7b8e00c66
Mark Story <mark@mark-story.com>
2025-12-02 10:24:39 -0500

    chore(cells) Finish adding silo annotations to View, Endpoint and functions (#104172)
    - Add silo annotations to the last batch of functions + views.
    - Expand tests to ensure that all views, endpoints and functions have
    silo annotations applied.
    - I added some exceptions for basic/builtin django views.
    
    Refs INFRENG-202

--- src/sentry/web/frontend/home.py
+++ src/sentry/web/frontend/home.py
@@ -1,9 +1,10 @@
 from django.http import HttpRequest
 from django.http.response import HttpResponseBase
 
-from sentry.web.frontend.base import BaseView
+from sentry.web.frontend.base import BaseView, all_silo_view
 
 
+@all_silo_view
 class HomeView(BaseView):
     def get(self, request: HttpRequest) -> HttpResponseBase:
         return self.redirect_to_org(request)
--- src/sentry/web/frontend/react_page.py
+++ src/sentry/web/frontend/react_page.py
@@ -22,7 +22,12 @@ from sentry.types.region import (
 )
 from sentry.utils.http import is_using_customer_domain, query_string
 from sentry.web.client_config import get_client_config
-from sentry.web.frontend.base import BaseView, ControlSiloOrganizationView
+from sentry.web.frontend.base import (
+    BaseView,
+    ControlSiloOrganizationView,
+    all_silo_view,
+    control_silo_view,
+)
 from sentry.web.helpers import render_to_response
 
 logger = logging.getLogger(__name__)
@@ -199,8 +204,9 @@ class ReactMixin:
         return response
 
 
-# TODO(dcramer): once we implement basic auth hooks in React we can make this
-# generic
+# While most of our HTML views are rendered in control silo,
+# our tests frequently redirect to HTML views
+@all_silo_view
 class ReactPageView(ControlSiloOrganizationView, ReactMixin):
     def handle_auth_required(self, request: HttpRequest, *args, **kwargs) -> HttpResponse:
         # If user is a superuser (but not active, because otherwise this method would never be called)
@@ -215,11 +221,13 @@ class ReactPageView(ControlSiloOrganizationView, ReactMixin):
         return self.handle_react(request, organization=organization)
 
 
+@all_silo_view
 class GenericReactPageView(BaseView, ReactMixin):
     def handle(self, request: HttpRequest, **kwargs) -> HttpResponse:
         return self.handle_react(request, **kwargs)
 
 
+@control_silo_view
 class AuthV2ReactPageView(GenericReactPageView):
     auth_required = False
 
