fa7b853e3dfd9bd16e4eef5ac1b2ddf76ab9ce19
nsdeschenes <nicholas.deschenes@sentry.io>
2025-12-02 09:23:07 -0400

    :necktie: Wrap in a useMemo to reduce re-creating this object

--- static/app/views/explore/hooks/useCrossEventQueries.tsx
+++ static/app/views/explore/hooks/useCrossEventQueries.tsx
@@ -1,32 +1,36 @@
+import {useMemo} from 'react';
+
 import {defined} from 'sentry/utils';
 import {useQueryParamsCrossEvents} from 'sentry/views/explore/queryParams/context';
 
 export function useCrossEventQueries() {
   const crossEvents = useQueryParamsCrossEvents();
 
-  if (!defined(crossEvents) || crossEvents.length === 0) {
-    return undefined;
-  }
-
-  const logQuery: string[] = [];
-  const metricQuery: string[] = [];
-  const spanQuery: string[] = [];
-
-  for (const crossEvent of crossEvents) {
-    switch (crossEvent.type) {
-      case 'spans':
-        spanQuery.push(crossEvent.query);
-        break;
-      case 'logs':
-        logQuery.push(crossEvent.query);
-        break;
-      case 'metrics':
-        metricQuery.push(crossEvent.query);
-        break;
-      default:
-        break;
+  return useMemo(() => {
+    if (!defined(crossEvents) || crossEvents.length === 0) {
+      return undefined;
     }
-  }
 
-  return {spanQuery, logQuery, metricQuery};
+    const logQuery: string[] = [];
+    const metricQuery: string[] = [];
+    const spanQuery: string[] = [];
+
+    for (const crossEvent of crossEvents) {
+      switch (crossEvent.type) {
+        case 'spans':
+          spanQuery.push(crossEvent.query);
+          break;
+        case 'logs':
+          logQuery.push(crossEvent.query);
+          break;
+        case 'metrics':
+          metricQuery.push(crossEvent.query);
+          break;
+        default:
+          break;
+      }
+    }
+
+    return {spanQuery, logQuery, metricQuery};
+  }, [crossEvents]);
 }
