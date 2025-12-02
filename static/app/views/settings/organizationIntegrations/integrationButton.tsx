6940a9359e4b551dabd5d362f14b0629ba8403c1
Billy Vong <billyvg@users.noreply.github.com>
2025-12-02 14:08:02 -0500

    ref(integrations): Tweak types of `<IntegrationButton>`
    This changes the `buttonProps` prop of `<IntegrationButton>` to more accurately reflect what props it takes in since it is spread to `<AddIntegrationButton>`

--- static/app/views/settings/organizationIntegrations/integrationButton.tsx
+++ static/app/views/settings/organizationIntegrations/integrationButton.tsx
@@ -9,19 +9,15 @@ import {IntegrationContext} from 'sentry/views/settings/organizationIntegrations
 import RequestIntegrationButton from 'sentry/views/settings/organizationIntegrations/integrationRequest/RequestIntegrationButton';
 
 type Props = {
-  buttonProps: ButtonProps;
   onAddIntegration: (integration: Integration) => void;
   onExternalClick: () => void;
   userHasAccess: boolean;
+  buttonProps?: Omit<
+    React.ComponentProps<typeof AddIntegrationButton>,
+    'provider' | 'onAddIntegration' | 'organization'
+  >;
   externalInstallText?: string;
 };
-
-type ButtonProps = {
-  disabled?: any;
-  priority?: any;
-  size?: any;
-  style?: any;
-} | null;
 
 function IntegrationButton({
   userHasAccess,
@@ -51,8 +47,8 @@ function IntegrationButton({
         installStatus={installStatus}
         analyticsParams={analyticsParams}
         modalParams={modalParams}
-        organization={organization}
         {...buttonProps}
+        organization={organization}
       />
     );
   }
