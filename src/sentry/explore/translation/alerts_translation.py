7d92cdbf4454324a9481322b4f6b78f8305f0835
Nikki Kapadia <72356613+nikkikapadia@users.noreply.github.com>
2025-12-03 10:20:23 -0500

    fix(spans-migration): query the query subscription to get to data source in alerts translation (#104275)
    We got a bunch of these
    [issues](https://sentry.sentry.io/issues/7082040359/) when running a
    sample set for the alerts migration. Basically we were querying the data
    source with the snuba query id when it needs to be queried with the
    subscription id as the `source_id`. Just updated this in the translation
    script, rollback script and all the translation tests.

--- src/sentry/explore/translation/alerts_translation.py
+++ src/sentry/explore/translation/alerts_translation.py
@@ -61,8 +61,17 @@ def _get_old_query_info(snuba_query: SnubaQuery):
 
 
 def translate_detector_and_update_subscription_in_snuba(snuba_query: SnubaQuery):
+    query_subscription_qs = QuerySubscription.objects.filter(
+        snuba_query_id=snuba_query.id, status=QuerySubscription.Status.ACTIVE.value
+    )
+    query_subscription = query_subscription_qs.first()
+
+    if not query_subscription:
+        logger.info("No active query subscription found for snuba query %s", snuba_query.id)
+        return
+
     data_source: DataSource = DataSource.objects.get(
-        source_id=str(snuba_query.id), type=DATA_SOURCE_SNUBA_QUERY_SUBSCRIPTION
+        source_id=str(query_subscription.id), type=DATA_SOURCE_SNUBA_QUERY_SUBSCRIPTION
     )
     if not features.has(
         "organizations:migrate-transaction-alerts-to-spans", data_source.organization
@@ -165,8 +174,17 @@ def translate_detector_and_update_subscription_in_snuba(snuba_query: SnubaQuery)
 
 
 def rollback_detector_query_and_update_subscription_in_snuba(snuba_query: SnubaQuery):
+    query_subscription_qs = QuerySubscription.objects.filter(
+        snuba_query_id=snuba_query.id, status=QuerySubscription.Status.ACTIVE.value
+    )
+    query_subscription = query_subscription_qs.first()
+
+    if not query_subscription:
+        logger.info("No active query subscription found for snuba query %s", snuba_query.id)
+        return
+
     data_source: DataSource = DataSource.objects.get(
-        source_id=str(snuba_query.id), type=DATA_SOURCE_SNUBA_QUERY_SUBSCRIPTION
+        source_id=str(query_subscription.id), type=DATA_SOURCE_SNUBA_QUERY_SUBSCRIPTION
     )
     if not features.has(
         "organizations:migrate-transaction-alerts-to-spans", data_source.organization
