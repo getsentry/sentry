67def297aad82213a36820203d57618e2222f955
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-12-03 10:41:32 -0800

    wip: Seer org-level bulk project settings

--- static/gsApp/views/seerAutomation/projects.tsx
+++ static/gsApp/views/seerAutomation/projects.tsx
@@ -1,6 +1,13 @@
+import {Stack} from '@sentry/scraps/layout/stack';
+
 import Feature from 'sentry/components/acl/feature';
 import {NoAccess} from 'sentry/components/noAccess';
+import SentryDocumentTitle from 'sentry/components/sentryDocumentTitle';
+import {t} from 'sentry/locale';
 import useOrganization from 'sentry/utils/useOrganization';
+
+import SeerProjectTable from 'getsentry/views/seerAutomation/components/projectTable/seerProjectTable';
+import {SeerAutomationProjectList} from 'getsentry/views/seerAutomation/components/seerAutomationProjectList';
 
 export default function SeerAutomationProjects() {
   const organization = useOrganization();
@@ -11,7 +18,11 @@ export default function SeerAutomationProjects() {
       organization={organization}
       renderDisabled={NoAccess}
     >
-      <div>SeerAutomationProjects</div>
+      <SentryDocumentTitle title={t('Seer')} orgSlug={organization.slug} />
+      <Stack gap="lg">
+        <SeerProjectTable />
+        <SeerAutomationProjectList />
+      </Stack>
     </Feature>
   );
 }
