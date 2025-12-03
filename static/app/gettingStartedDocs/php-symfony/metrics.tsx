b0b7b0deeb00f1f1db52c396c4bb18cada089e2d
Alexander Alderman Webb <alexander.webb@sentry.io>
2025-12-03 15:39:18 +0100

    remove commented out imports

--- static/app/gettingStartedDocs/php-symfony/metrics.tsx
+++ static/app/gettingStartedDocs/php-symfony/metrics.tsx
@@ -1,4 +1,3 @@
-// import {ExternalLink} from 'sentry/components/core/link';
 import type {OnboardingConfig} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {tct} from 'sentry/locale';
