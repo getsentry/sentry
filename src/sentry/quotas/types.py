bc04ada433b4552e0c9345a74f6d5207f1540de1
ajay-sentry <159853603+ajay-sentry@users.noreply.github.com>
2025-12-02 16:35:06 -0800

    feat: Add organizationcontributor seat type (#104299)
    Adds a new model as a potential seat type
    
    
    <!--
    
      Sentry employees and contractors can delete or ignore the following.
    
    -->
    
    ### Legal Boilerplate
    
    Look, I get it. The entity doing business as "Sentry" was incorporated
    in the State of Delaware in 2015 as Functional Software, Inc. and is
    gonna need some rights from me in order to utilize my contributions in
    this here PR. So here's the deal: I retain all rights, title and
    interest in and to my contributions, and by keeping this boilerplate
    intact I confirm that Sentry can use, modify, copy, and redistribute my
    contributions, under Sentry's choice of terms.

--- src/sentry/quotas/types.py
+++ src/sentry/quotas/types.py
@@ -1,6 +1,7 @@
 from typing import TypeAlias, Union
 
+from sentry.models.organizationcontributors import OrganizationContributors
 from sentry.monitors.models import Monitor
 from sentry.workflow_engine.models import Detector
 
-SeatObject: TypeAlias = Union[Monitor, Detector]
+SeatObject: TypeAlias = Union[Monitor, Detector, OrganizationContributors]
