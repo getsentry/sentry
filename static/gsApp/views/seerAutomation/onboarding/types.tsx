11c4d018413f8a334c72f512379f46bdef070407
Billy Vong <billyvg@users.noreply.github.com>
2025-12-04 17:53:53 -0500

    unused

--- static/gsApp/views/seerAutomation/onboarding/types.tsx
+++ static/gsApp/views/seerAutomation/onboarding/types.tsx
@@ -4,10 +4,3 @@ export enum Steps {
   SETUP_ROOT_CAUSE_ANALYSIS = 'setup-root-cause-analysis',
   NEXT_STEPS = 'next-steps',
 }
-
-export const STEPS_ORDER = [
-  Steps.CONNECT_GITHUB,
-  Steps.SETUP_CODE_REVIEW,
-  Steps.SETUP_ROOT_CAUSE_ANALYSIS,
-  Steps.NEXT_STEPS,
-];
