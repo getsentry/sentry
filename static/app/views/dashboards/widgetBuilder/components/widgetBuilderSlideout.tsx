4aa66e25ed8c909f49385101588e61d7bb354d5c
edwardgou-sentry <83961295+edwardgou-sentry@users.noreply.github.com>
2025-12-04 10:37:50 -0500

    fix(dashboards): Hide group by option for series display Issue widgets (#104370)
    Timeseries Issue widgets don't support groupbys at the moment, so hide
    the selector from the widget builder.

--- static/app/views/dashboards/widgetBuilder/components/widgetBuilderSlideout.tsx
+++ static/app/views/dashboards/widgetBuilder/components/widgetBuilderSlideout.tsx
@@ -129,6 +129,7 @@ function WidgetBuilderSlideout({
   const showQueryFilterBuilder = !(
     state.dataset === WidgetType.ISSUE && isChartDisplayType(state.displayType)
   );
+  const showGroupBySelector = isChartWidget && !(state.dataset === WidgetType.ISSUE);
 
   const customPreviewRef = useRef<HTMLDivElement>(null);
   const templatesPreviewRef = useRef<HTMLDivElement>(null);
@@ -368,7 +369,7 @@ function WidgetBuilderSlideout({
                   />
                 </Section>
               )}
-              {isChartWidget && (
+              {showGroupBySelector && (
                 <Section>
                   <WidgetBuilderGroupBySelector
                     validatedWidgetResponse={validatedWidgetResponse}
