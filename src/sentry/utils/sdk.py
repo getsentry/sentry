43cb221ab296340597823fc1fd620f72be6b2e81
Alex Alderman Webb <alexander.webb@sentry.io>
2025-12-04 10:40:08 +0100

    feat(sdk): Enable Django SQL transaction spans (#104383)
    Upgrade the Python SDK and enable new instrumentation for database transactions issued through Django's ORM.

--- src/sentry/utils/sdk.py
+++ src/sentry/utils/sdk.py
@@ -482,7 +482,12 @@ def configure_sdk():
         transport=MultiplexingTransport(),
         integrations=[
             DjangoAtomicIntegration(),
-            DjangoIntegration(signals_spans=False, cache_spans=True, middleware_spans=False),
+            DjangoIntegration(
+                signals_spans=False,
+                cache_spans=True,
+                middleware_spans=False,
+                db_transaction_spans=True,
+            ),
             # This makes it so all levels of logging are recorded as breadcrumbs,
             # but none are captured as events (that's handled by the `internal`
             # logger defined in `server.py`, which ignores the levels set
