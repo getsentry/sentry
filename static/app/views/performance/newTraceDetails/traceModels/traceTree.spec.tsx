4e06ee5a2c84fd44d62b962cc24708ef33dea231
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-12-04 18:12:08 -0500

    feat(trace-tree-node): Mitigating no instrumentation guards usage

--- static/app/views/performance/newTraceDetails/traceModels/traceTree.spec.tsx
+++ static/app/views/performance/newTraceDetails/traceModels/traceTree.spec.tsx
@@ -4,7 +4,7 @@ import {ReplayRecordFixture} from 'sentry-fixture/replayRecord';
 import {DEFAULT_TRACE_VIEW_PREFERENCES} from 'sentry/views/performance/newTraceDetails/traceState/tracePreferences';
 
 import {
-  isNoInstrumentationNode,
+  isMissingInstrumentationNode,
   isParentAutogroupedNode,
   isSiblingAutogroupedNode,
   isSpanNode,
@@ -1926,7 +1926,7 @@ describe('TraceTree', () => {
       TraceTree.DetectMissingInstrumentation(tree.root);
 
       const missingInstrumentationNode = tree.root.findChild(node =>
-        isNoInstrumentationNode(node)
+        isMissingInstrumentationNode(node)
       )!;
 
       const path = missingInstrumentationNode.pathToNode();
