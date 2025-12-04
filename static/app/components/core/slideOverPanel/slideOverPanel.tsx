306d7f4d5fbffb219023963298cfa2835964a90a
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-12-04 08:17:12 -0500

    feat(ui): Allow deferred renders of `SlideOverPanel` (#103852)
    ## Summary
    
    This PR adds a feature to `SlideOverPanel` that allows deferring
    rendering its contents. This makes an _enormous_ UX difference if the
    contents of the panel are expensive to render (e.g., the Widget Builder,
    and many others). If a `skeleton` prop is provided, the panel shows the
    skeleton UI and defers the render.
    
    The video, I think, speaks for itself, but I added a playground where
    you can see the effects clearly by clicking around.
    
    
    https://github.com/user-attachments/assets/d281bbba-98a8-48a9-a0bc-898ba6755177
    
    
    ## Implementation
    
    This is done via `useDeferredValue` (described in details in the [React
    docs](https://react.dev/reference/react/useDeferredValue)) that shadows
    the `collapsed` value of `SlideOverPanel`. Here's a profile that shows
    what happens without a skeleton:
    
    You can see (if you squint):
    
    <img width="843" height="453" alt="Screenshot 2025-11-21 at 3 31 34 PM"
    src="https://github.com/user-attachments/assets/f5041a90-d211-43de-a5c2-01cfb82f4ffd"
    />
    
    
    1. A _very_ expensive render on the "Blocking" lane in React, causing a
    2s long pointer event. Bad!
    2. A very very fast render immediately after the small one, on the
    "Transition" lane in React
    
    Here's the "after":
    
    <img width="951" height="377" alt="Screenshot 2025-11-21 at 3 32 49 PM"
    src="https://github.com/user-attachments/assets/465fe7d6-9157-4e01-841a-cf7feafbb38e"
    />
    
    1. A very very fast render on the "Blocking" lane and a super fast
    pointer event
    2. A very slow render on the "Transition" lane
    
    Pretty big improvement. This works better than debouncing or throttling
    for two exciting reasons:
    
    1. React will continue to defer the render if other high-priority state
    updates run! If users keep doing stuff, it'll continue deferring the
    expensive render
    2. The render is _cancellable_! You can click "Close" on the panel while
    the expensive render is happening and cancel out! HUGE.
    
    There's a caveat here, which is that cancellability isn't free. React
    will pause every 5ms or so during the deferred render to check if it
    needs to interrupt it, so it'll take slightly longer to render the
    expensive `children` in this case. I did not notice a _huge_ difference
    so IMO it's worth it, though I can get some numbers on that if needed.
    
    ## Questions
    
    This approach is _much_ simpler if the `collapsed` prop is available. If
    the panel is rendered conditionally with `isOpen &&` I would need a
    different approach using `useTransition`. That's viable, but I don't
    think it's viable for deferred rendering to support _both_ a `collapsed`
    prop _and_ conditional rendering. That's a problem, so which one do we
    want?
    
    How do we feel about this overall? Needs a few tests and maybe some docs
    for sure, what else? Any concerns?

--- static/app/components/core/slideOverPanel/slideOverPanel.tsx
+++ static/app/components/core/slideOverPanel/slideOverPanel.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useCallback, useDeferredValue, useEffect} from 'react';
 import isPropValid from '@emotion/is-prop-valid';
 import {css, useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
@@ -22,10 +22,16 @@ const COLLAPSED_STYLES = {
   left: {transform: `translateX(-${LEFT_SIDE_PANEL_WIDTH}) translateY(0)`, opacity: 0},
 };
 
+interface ChildRenderProps {
+  isOpening: boolean;
+}
+
+type ChildRenderFunction = (renderPropProps: ChildRenderProps) => React.ReactNode;
+
 type SlideOverPanelProps = {
-  children: React.ReactNode;
+  children: React.ReactNode | ChildRenderFunction;
   /**
-   * Whether the panel is visible. In most cases it's better to conditionally render this component rather than use this prop, since it'll defer rendering the panel contents until they're needed.
+   * Whether the panel is visible. In most cases it's better to use this prop rather than render the panel conditionally, since it'll defer rendering the contents of the panel to a lower priority React lane.
    */
   collapsed: boolean;
   ariaLabel?: string;
@@ -58,11 +64,43 @@ export function SlideOverPanel({
 }: SlideOverPanelProps) {
   const theme = useTheme();
 
+  const isOpen = !collapsed;
+
+  // Create a deferred version of `isOpen`. Here's how the rendering flow works
+  // when the visibility changes to `true`:
+  //
+  // 1. Parent component sets `collapsed` to `false`. This triggers a render.
+  // 2. The render runs with `isOpen` `true` and `isContentVisible` `false`. We
+  // pass `isOpening: true` to the `children` render prop. The render prop
+  // contents should render a fast skeleton.
+  // 3. The next render runs, with `isOpen` `true` and `isContentVisible`
+  // `true`. This render is scheduled in the "transition" React lane. When this
+  // is done, we pass `isOpening: false` to the children, and the render prop
+  // should render the full UI. React periodically polls the main thread and
+  // interrupt rendering of the full UI it if a high-priority render comes in.
+  // Other state transitions are simpler, because we _only_ show the skeleton
+  // during that "opening" transition.
+  const isContentVisible = useDeferredValue(isOpen);
+
   useEffect(() => {
     if (!collapsed && onOpen) {
       onOpen();
     }
   }, [collapsed, onOpen]);
+
+  // Create a fallback render function, in case the parent component passes
+  // `React.ReactNode` as `children`. This way, even if they don't pass a render
+  // prop they still get the benefit of fast panel opening.
+  const fallbackRenderFunction = useCallback<ChildRenderFunction>(
+    ({isOpening}: ChildRenderProps) => {
+      return isOpening ? null : (children as React.ReactNode); // This function should only ever run for `React.ReactNode`, its whole purpose is to create a render function for `React.ReactNode`
+    },
+    [children]
+  );
+
+  const renderFunctionProps: ChildRenderProps = {
+    isOpening: isOpen !== isContentVisible && !isContentVisible,
+  };
 
   const openStyle = slidePosition ? OPEN_STYLES[slidePosition] : OPEN_STYLES.right;
 
@@ -70,7 +108,7 @@ export function SlideOverPanel({
     ? COLLAPSED_STYLES[slidePosition]
     : COLLAPSED_STYLES.right;
 
-  return collapsed ? null : (
+  return isOpen ? (
     <_SlideOverPanel
       ref={ref}
       initial={collapsedStyle}
@@ -88,9 +126,15 @@ export function SlideOverPanel({
       data-test-id={testId}
       panelWidth={panelWidth}
     >
-      {children}
+      {/* Render the child content. If it's a render prop, pass the `isOpening`
+      prop. We expect the render prop to render a skeleton UI if `isOpening` is
+      true. Otherwise, use the fallback render prop, which shows a blank panel
+      while opening. */}
+      {typeof children === 'function'
+        ? children(renderFunctionProps)
+        : fallbackRenderFunction(renderFunctionProps)}
     </_SlideOverPanel>
-  );
+  ) : null;
 }
 
 const _SlideOverPanel = styled(motion.div, {
