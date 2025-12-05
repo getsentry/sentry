6f9b454e91e83578de1ab3f31777a112f29ec753
Richard Roggenkemper <46740234+roggenkemper@users.noreply.github.com>
2025-12-05 10:48:23 -0800

    chore(detectors): Remove experimental rollout flags  (#104428)
    usages of flag were removed in
    https://github.com/getsentry/sentry/pull/104199 and
    https://github.com/getsentry/sentry-options-automator/pull/5866

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -165,12 +165,6 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:escalating-metrics-backend", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enable returning the migrated discover queries in explore saved queries
     manager.add("organizations:expose-migrated-discover-queries", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable experimental MN+1 Detector
-    manager.add("organizations:experimental-mn-plus-one-detector-rollout", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable experimental N+1 DB Detector
-    manager.add("organizations:experimental-n-plus-one-db-detector-rollout", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable experiemntal N+1 API Calls Detector
-    manager.add("organizations:experimental-n-plus-one-api-detector-rollout", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable GenAI features such as Autofix and Issue Summary
     manager.add("organizations:gen-ai-features", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable the 'generate me a query' functionality on the explore > traces page
