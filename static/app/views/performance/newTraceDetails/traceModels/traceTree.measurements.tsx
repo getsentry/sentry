6eb33d5c580e0d435533e9be1ccbace20185cfb4
Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>
2025-12-07 18:57:27 -0500

    feat(trace-tree-node): Mitingating eap measurements type guard usage

--- static/app/views/performance/newTraceDetails/traceModels/traceTree.measurements.tsx
+++ static/app/views/performance/newTraceDetails/traceModels/traceTree.measurements.tsx
@@ -100,7 +100,7 @@ export function collectTraceMeasurements(
   for (const collectableMeasurement of COLLECTABLE_MEASUREMENTS) {
     const measurement = measurements[collectableMeasurement];
 
-    if (!measurement?.value) {
+    if (!measurement?.value || typeof measurement.value !== 'number') {
       continue;
     }
 
