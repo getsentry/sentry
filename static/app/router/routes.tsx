5fafe2f9e323e7dae58d1a2ceabf536a79c9d026
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-12-08 14:52:04 -0800

    ref(prevent): Refactor prevent settings (#104539)
    The configuration for this is moving in to /settings actually
    
    <!-- Describe your PR here. -->

--- static/app/router/routes.tsx
+++ static/app/router/routes.tsx
@@ -2546,10 +2546,6 @@ function buildRoutes(): RouteObject[] {
               path: 'new/',
               component: make(() => import('sentry/views/prevent/preventAI/onboarding')),
             },
-            {
-              path: 'home/',
-              component: make(() => import('sentry/views/prevent/preventAI/index')),
-            },
           ],
         },
       ],
