030202230cbd98ccab9875b246746d6885268e95
Jonas <jonas.badalic@sentry.io>
2025-12-08 10:05:59 -0800

    reducer: refactor complexity to On (#103672)
    Refactor reducer spread or concat operations in a couple places where
    this might be sensitive

--- static/app/components/dropdownMenu/index.tsx
+++ static/app/components/dropdownMenu/index.tsx
@@ -35,11 +35,12 @@ function removeHiddenItemsAndSetHref(source: MenuItemProps[]): MenuItemProps[] {
  * Recursively finds and returns disabled items
  */
 function getDisabledKeys(source: MenuItemProps[]): Array<MenuItemProps['key']> {
-  return source.reduce<string[]>((acc, cur) => {
+  return source.reduce<Array<MenuItemProps['key']>>((acc, cur) => {
     if (cur.disabled) {
       // If an item is disabled, then its children will be inaccessible, so we
       // can skip them and just return the parent item
-      return acc.concat([cur.key]);
+      acc.push(cur.key);
+      return acc;
     }
 
     if (cur.children) {
