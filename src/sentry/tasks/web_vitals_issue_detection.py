cc88910834a22e1e6a8d891c057593ed546690f4
Edward Gou <edward.gou@sentry.io>
2025-12-09 12:40:24 -0500

    fix

--- src/sentry/tasks/web_vitals_issue_detection.py
+++ src/sentry/tasks/web_vitals_issue_detection.py
@@ -224,7 +224,7 @@ def get_highest_opportunity_page_vitals_for_project(
     The number of samples is set by SAMPLES_COUNT_THRESHOLD.
     """
     try:
-        project = Project.objects.get(id=project_id).select_related("organization")
+        project = Project.objects.select_related("organization").get(id=project_id)
     except Project.DoesNotExist:
         logger.exception(
             "Project does not exist; cannot fetch transactions", extra={"project_id": project_id}
