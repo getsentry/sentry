2b2358821d6409a19f9e73815d0655cf84236189
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-12-09 08:27:57 -0800

    Update static/gsApp/views/seerAutomation/repos.tsx
    Co-authored-by: Billy Vong <billyvg@users.noreply.github.com>

--- static/gsApp/views/seerAutomation/repos.tsx
+++ static/gsApp/views/seerAutomation/repos.tsx
@@ -1,6 +1,6 @@
 import SeerSettingsPageWrapper from 'getsentry/views/seerAutomation/components/seerSettingsPageWrapper';
 
-export default function SeerAutomationSettings() {
+export default function SeerAutomationRepos() {
   return (
     <SeerSettingsPageWrapper>
       <p>Repos</p>
