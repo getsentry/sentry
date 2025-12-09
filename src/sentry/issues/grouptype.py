05aa31cef241fcd41cdb64419964fbf92523e8d3
Hector Dearman <hector.dearman@sentry.io>
2025-12-09 18:54:27 +0000

    feat(preprod): Add preprod issue category and grouptypes (#104579)
    Add preprod issue category and grouptypes.
    See EME-80 and
    https://www.notion.so/sentry/Issues-Size-Analysis-2bc8b10e4b5d80ce9b43c4ace1b3058b

--- src/sentry/issues/grouptype.py
+++ src/sentry/issues/grouptype.py
@@ -68,6 +68,13 @@ class GroupCategory(IntEnum):
 
     AI_DETECTED = 16
 
+    """
+    Issues detected from analysis of uploaded artifacts. This covers
+    both issues detected in a single build (e.g. not 16kb page ready)
+    and those detected between builds (e.g. binary size regression).
+    """
+    PREPROD = 17
+
 
 GROUP_CATEGORIES_CUSTOM_EMAIL = (
     GroupCategory.ERROR,
@@ -686,6 +693,49 @@ class WebVitalsGroup(GroupType):  # TODO: Rename to WebVitalsGroupType
     # Web Vital issues are always triggered for the purpose of using autofix
     always_trigger_seer_automation = True
     released = True
+
+
+@dataclass(frozen=True)
+class PreprodStaticGroupType(GroupType):
+    """
+    Issues detected in a single uploaded artifact. For example an
+    Android app not being 16kb page size ready.
+    Typically these end up grouped across multiple builds e.g. if CI
+    uploads a build of an app for each commit to main each of those
+    uploads could result in an occurrence of some issue like the 16kb
+    page size.
+    """
+
+    type_id = 11001
+    slug = "preprod_static"
+    description = "Static Analysis"
+    category = GroupCategory.PREPROD.value
+    category_v2 = GroupCategory.PREPROD.value
+    default_priority = PriorityLevel.LOW
+    released = False
+    enable_auto_resolve = True
+    enable_escalation_detection = False
+
+
+@dataclass(frozen=True)
+class PreprodDeltaGroupType(GroupType):
+    """
+    Issues detected examining the delta between two uploaded artifacts.
+    For example a binary size regression. These are typically *not*
+    grouped. A size regression between v1 and v2 likely does not have
+    the same root cause (and hence resolution) as another regression
+    between v2 and v3.
+    """
+
+    type_id = 11002
+    slug = "preprod_delta"
+    description = "Static Analysis Delta"
+    category = GroupCategory.PREPROD.value
+    category_v2 = GroupCategory.PREPROD.value
+    default_priority = PriorityLevel.LOW
+    released = False
+    enable_auto_resolve = True
+    enable_escalation_detection = False
 
 
 def should_create_group(
