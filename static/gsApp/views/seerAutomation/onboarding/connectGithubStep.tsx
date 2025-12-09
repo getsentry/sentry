d68feb8aacaf6669eaf36d063919092bec7ca352
Billy Vong <billyvg@users.noreply.github.com>
2025-12-09 15:17:21 -0500

    feat(seer): Add new onboarding for Seer (#104215)
    This adds a new onboarding page for Seer, currently feature flagged for
    sentry-test and sentry-sdks orgs. access via hard-coded url:
    `/settings/seer/onboarding-v2/`

---
+++ static/gsApp/views/seerAutomation/onboarding/connectGithubStep.tsx
@@ -0,0 +1,39 @@
+import {Fragment, useCallback} from 'react';
+
+import {GuidedSteps} from 'sentry/components/guidedSteps/guidedSteps';
+import PanelBody from 'sentry/components/panels/panelBody';
+import {t} from 'sentry/locale';
+
+import {ActionSection, MaxWidthPanel, StepContent} from './common';
+import {GithubButton} from './githubButton';
+
+export function ConnectGithubStep() {
+  const handleAddIntegration = useCallback(() => {
+    window.location.reload();
+  }, []);
+
+  return (
+    <Fragment>
+      <StepContent>
+        <MaxWidthPanel>
+          <PanelBody withPadding>
+            <p>
+              {t(
+                'In order to get the most out of Sentry and use Seer we will need to access your code repositories in GitHub. (We do not currently support Gitlab, Bitbucket, or others)'
+              )}
+            </p>
+            <ActionSection>
+              <GithubButton
+                onAddIntegration={handleAddIntegration}
+                analyticsView="seer_onboarding_github"
+              />
+            </ActionSection>
+            <GuidedSteps.ButtonWrapper>
+              <GuidedSteps.NextButton size="md" />
+            </GuidedSteps.ButtonWrapper>
+          </PanelBody>
+        </MaxWidthPanel>
+      </StepContent>
+    </Fragment>
+  );
+}
