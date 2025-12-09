df8b38762dba52010d1dcc143d21a9f921697e6e
Edward Gou <edward.gou@sentry.io>
2025-12-09 11:13:44 -0500

    registers web vitals dashboard

--- src/sentry/dashboards/endpoints/organization_dashboards.py
+++ src/sentry/dashboards/endpoints/organization_dashboards.py
@@ -65,6 +65,7 @@ class PrebuiltDashboardId(IntEnum):
     FRONTEND_SESSION_HEALTH = 1
     BACKEND_QUERIES = 2
     BACKEND_QUERIES_SUMMARY = 3
+    WEB_VITALS = 4
 
 
 class PrebuiltDashboard(TypedDict):
@@ -93,6 +94,10 @@ PREBUILT_DASHBOARDS: list[PrebuiltDashboard] = [
     {
         "prebuilt_id": PrebuiltDashboardId.BACKEND_QUERIES_SUMMARY,
         "title": "Query Details",
+    },
+    {
+        "prebuilt_id": PrebuiltDashboardId.WEB_VITALS,
+        "title": "Web Vitals",
     },
 ]
 
