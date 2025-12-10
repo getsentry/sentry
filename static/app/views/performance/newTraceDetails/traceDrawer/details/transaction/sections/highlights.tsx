f4d87ef806beaf4d4bb24e47fd0d701b67c7a614
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-12-10 12:45:29 +0100

    fix

--- static/app/views/performance/newTraceDetails/traceDrawer/details/transaction/sections/highlights.tsx
+++ static/app/views/performance/newTraceDetails/traceDrawer/details/transaction/sections/highlights.tsx
@@ -89,7 +89,7 @@ export function TransactionHighlights(props: HighlightProps) {
       bodyContent={bodyContent}
       hideNodeActions={props.hideNodeActions}
       highlightedAttributes={getHighlightedSpanAttributes({
-        op: props.node.value.op,
+        op: props.node.value['transaction.op'],
         attributes: props.event.contexts.trace?.data,
         spanId: props.node.value.span_id,
       })}
