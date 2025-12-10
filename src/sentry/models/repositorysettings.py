aa69385fa728ab78d9e885c34c8b48f7d8eb4fc7
ajay-sentry <159853603+ajay-sentry@users.noreply.github.com>
2025-12-10 10:22:33 -0800

    feat: Create new repository settings table (#104645)
    This PR aims to create our new repository settings table, with a foreign
    key back to the organization repository table.
    
    Table currently stores if code review is enabled, as well as the
    triggers associated with it being enabled. The triggers are associated
    with the enum CodeReviewTrigger, also instantiated on the model
    
    Closes
    https://linear.app/getsentry/issue/ENG-6089/create-repository-settings-table
    
    
    <!--
    
      Sentry employees and contractors can delete or ignore the following.
    
    -->
    
    ### Legal Boilerplate
    
    Look, I get it. The entity doing business as "Sentry" was incorporated
    in the State of Delaware in 2015 as Functional Software, Inc. and is
    gonna need some rights from me in order to utilize my contributions in
    this here PR. So here's the deal: I retain all rights, title and
    interest in and to my contributions, and by keeping this boilerplate
    intact I confirm that Sentry can use, modify, copy, and redistribute my
    contributions, under Sentry's choice of terms.

---
+++ src/sentry/models/repositorysettings.py
@@ -0,0 +1,43 @@
+from __future__ import annotations
+
+from enum import StrEnum
+
+from django.contrib.postgres.fields.array import ArrayField
+from django.db import models
+
+from sentry.backup.scopes import RelocationScope
+from sentry.db.models import FlexibleForeignKey, Model, region_silo_model, sane_repr
+
+
+class CodeReviewTrigger(StrEnum):
+    ON_COMMAND_PHRASE = "on_command_phrase"
+    ON_NEW_COMMIT = "on_new_commit"
+    ON_READY_FOR_REVIEW = "on_ready_for_review"
+
+    @classmethod
+    def as_choices(cls) -> tuple[tuple[str, str], ...]:
+        return tuple((trigger.value, trigger.value) for trigger in cls)
+
+
+@region_silo_model
+class RepositorySettings(Model):
+    """
+    Stores (organization) repository specific settings.
+    """
+
+    __relocation_scope__ = RelocationScope.Global
+
+    repository = FlexibleForeignKey(
+        "sentry.Repository", on_delete=models.CASCADE, unique=True, db_index=True
+    )
+    enabled_code_review = models.BooleanField(default=False)
+    code_review_triggers = ArrayField(
+        models.CharField(max_length=32, choices=CodeReviewTrigger.as_choices()),
+        default=list,
+    )
+
+    class Meta:
+        app_label = "sentry"
+        db_table = "sentry_repositorysettings"
+
+    __repr__ = sane_repr("repository_id", "enabled_code_review")
