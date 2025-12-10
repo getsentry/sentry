d41251303606e0d0df0a33766923f65ee1a64c44
Simon Hellmayr <simon.hellmayr@sentry.io>
2025-12-10 12:24:48 +0100

    add feature check to get_attrs

--- src/sentry/api/serializers/models/organization.py
+++ src/sentry/api/serializers/models/organization.py
@@ -577,23 +577,25 @@ class DetailedOrganizationSerializer(OrganizationSerializer):
     ) -> MutableMapping[Organization, MutableMapping[str, Any]]:
         attrs = super().get_attrs(item_list, user)
 
-        replay_permissions = {
-            opt.organization_id: opt.value
-            for opt in OrganizationOption.objects.filter(
-                organization__in=item_list, key="sentry:granular-replay-permissions"
-            )
-        }
+        replay_permissions = {}
+        if features.has("organizations:granular-replay-permissions", actor=user):
+            replay_permissions = {
+                opt.organization_id: opt.value
+                for opt in OrganizationOption.objects.filter(
+                    organization__in=item_list, key="sentry:granular-replay-permissions"
+                )
+            }
 
-        replay_access_by_org: dict[int, list[int]] = {}
-        for org_id, user_id in OrganizationMemberReplayAccess.objects.filter(
-            organizationmember__organization__in=item_list
-        ).values_list("organizationmember__organization_id", "organizationmember__user_id"):
-            if user_id is not None:
-                replay_access_by_org.setdefault(org_id, []).append(user_id)
+            replay_access_by_org: dict[int, list[int]] = {}
+            for org_id, user_id in OrganizationMemberReplayAccess.objects.filter(
+                organizationmember__organization__in=item_list
+            ).values_list("organizationmember__organization_id", "organizationmember__user_id"):
+                if user_id is not None:
+                    replay_access_by_org.setdefault(org_id, []).append(user_id)
 
-        for item in item_list:
-            attrs[item]["replay_permissions_enabled"] = replay_permissions.get(item.id, False)
-            attrs[item]["replay_access_members"] = replay_access_by_org.get(item.id, [])
+            for item in item_list:
+                attrs[item]["replay_permissions_enabled"] = replay_permissions.get(item.id, False)
+                attrs[item]["replay_access_members"] = replay_access_by_org.get(item.id, [])
 
         return attrs
 
