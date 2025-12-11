78d584347fbda244184e957d1a7519aec901da07
Billy Vong <billyvg@users.noreply.github.com>
2025-12-10 19:07:19 -0500

    feat(ui): Add `useUpdateOrganization`
    This is basically c&p of `useUpdateProject`.
    
    Will follow-up to remove https://github.com/getsentry/sentry/blob/b61b74b40d5e174018d5d9e69e99f7fbaf281d69/static/app/views/settings/dynamicSampling/utils/useUpdateOrganization.tsx#L33

---
+++ static/app/utils/useUpdateOrganization.tsx
@@ -0,0 +1,77 @@
+import OrganizationStore from 'sentry/stores/organizationStore';
+import type {Organization} from 'sentry/types/organization';
+import {
+  fetchMutation,
+  setApiQueryData,
+  useMutation,
+  useQueryClient,
+  type ApiQueryKey,
+} from 'sentry/utils/queryClient';
+
+interface Variables extends Partial<Organization> {}
+
+type Context =
+  | {
+      previousOrganization: Organization;
+      error?: never;
+    }
+  | {
+      error: Error;
+      previousOrganization?: never;
+    };
+
+function makeDetailedOrganizationQueryKey(organization: Organization): ApiQueryKey {
+  return [
+    `/organizations/${organization.slug}/`,
+    {query: {detailed: 1, include_feature_flags: 1}},
+  ];
+}
+
+export function useUpdateOrganization(organization: Organization) {
+  const queryClient = useQueryClient();
+
+  const queryKey = makeDetailedOrganizationQueryKey(organization);
+
+  return useMutation<Organization, Error, Variables, Context>({
+    onMutate: (data: Variables) => {
+      const previousOrganization =
+        queryClient.getQueryData<Organization>(queryKey) ||
+        OrganizationStore.get().organization ||
+        organization;
+
+      if (!previousOrganization) {
+        return {error: new Error('Previous organization not found')};
+      }
+
+      const updatedOrganization = {
+        ...previousOrganization,
+        ...data,
+      };
+
+      // Update caches optimistically
+      OrganizationStore.onUpdate(updatedOrganization);
+      setApiQueryData<Organization>(queryClient, queryKey, updatedOrganization);
+
+      return {previousOrganization};
+    },
+    mutationFn: (data: Variables) => {
+      return fetchMutation<Organization>({
+        method: 'PUT',
+        url: `/organizations/${organization.slug}/`,
+        data: {...data},
+      });
+    },
+    onError: (_error, _variables, context) => {
+      if (context?.previousOrganization) {
+        OrganizationStore.onUpdate(context.previousOrganization);
+        queryClient.setQueryData(queryKey, context.previousOrganization);
+      }
+    },
+    onSettled: () => {
+      // Invalidate to refetch and ensure consistency for the queryCache
+      // ProjectsStore should've been updated already. It could be out of sync if
+      // there are multiple mutations in parallel.
+      queryClient.invalidateQueries({queryKey});
+    },
+  });
+}
