4569d07049fbafd5ab090ea85c0437e0a18f4e3c
ajay-sentry <159853603+ajay-sentry@users.noreply.github.com>
2025-12-11 14:11:51 -0800

    feat: Create bulk PUT seer automation option setting (#104732)
    This endpoint is going to be used to bulk update each project's
    `sentry:autofix_automation_tuning` setting. Needed for the seer settings
    bulk update table.
    
    Basically, takes 2 input params, fixes and pr_creation, where fixes is
    the "default" toggle. If its on it turns the automation setting to
    medium, and if it's off it also updates the seer project preferences to
    be code_changes.
    
    If fixes is true, then we can also look at the value of pr_creation and
    if that's true we can make an RPC call to seer to update those settings
    as well. Similar to how it's being done in
    https://github.com/getsentry/sentry/pull/104642
    
    Closes
    https://linear.app/getsentry/issue/ENG-6106/api-create-put-seer-automation-option-setting-update-endpoint
    
    <!--
    
      Sentry employees and contractors can delete or ignore the following.
    
    -->
    
    ### Legal Boilerplate
    
    Look, I get it. The entity doing business as "Sentry" was incorporated
    in the State of Delaware in 2015 as Functional Software, Inc. and is
    gonna need some rights from me in order to utilize my contributions in
    this here PR. So here's the deal: I retain all rights, title and
    interest in and to my contributions, and by keeping this boilerplate
    intact I confirm that Sentry can use, modify, copy, and redistribute my
    contributions, under Sentry's choice of terms.
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -516,6 +516,9 @@ from sentry.seer.endpoints.group_ai_autofix import GroupAutofixEndpoint
 from sentry.seer.endpoints.group_ai_summary import GroupAiSummaryEndpoint
 from sentry.seer.endpoints.group_autofix_setup_check import GroupAutofixSetupCheck
 from sentry.seer.endpoints.group_autofix_update import GroupAutofixUpdateEndpoint
+from sentry.seer.endpoints.organization_autofix_automation_settings import (
+    OrganizationAutofixAutomationSettingsEndpoint,
+)
 from sentry.seer.endpoints.organization_events_anomalies import OrganizationEventsAnomaliesEndpoint
 from sentry.seer.endpoints.organization_seer_explorer_chat import (
     OrganizationSeerExplorerChatEndpoint,
@@ -2348,6 +2351,11 @@ ORGANIZATION_URLS: list[URLPattern | URLResolver] = [
         r"^(?P<organization_id_or_slug>[^/]+)/seer/onboarding-check/$",
         OrganizationSeerOnboardingCheck.as_view(),
         name="sentry-api-0-organization-seer-onboarding-check",
+    ),
+    re_path(
+        r"^(?P<organization_id_or_slug>[^/]+)/autofix/automation-settings/$",
+        OrganizationAutofixAutomationSettingsEndpoint.as_view(),
+        name="sentry-api-0-organization-autofix-automation-settings",
     ),
     re_path(
         r"^(?P<organization_id_or_slug>[^/]+)/seer-rpc/(?P<method_name>\w+)/$",
