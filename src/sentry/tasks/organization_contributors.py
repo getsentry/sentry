86665bb504b6fdfd3e91902c098dbf72fd064b58
srest2021 <sofia.rest@sentry.io>
2025-12-10 17:12:05 -0800

    add retry: 3 times, 60s delay

--- src/sentry/tasks/organization_contributors.py
+++ src/sentry/tasks/organization_contributors.py
@@ -10,6 +10,7 @@ from sentry.models.organizationcontributors import OrganizationContributors
 from sentry.silo.base import SiloMode
 from sentry.tasks.base import instrumented_task
 from sentry.taskworker.namespaces import integrations_tasks
+from sentry.taskworker.retry import Retry, retry_task
 from sentry.utils.outcomes import Outcome
 
 logger = logging.getLogger(__name__)
@@ -39,6 +40,7 @@ def reset_num_actions_for_organization_contributors(organization_id: int) -> Non
     name="sentry.tasks.organization_contributors.assign_seat_to_organization_contributor",
     namespace=integrations_tasks,
     silo_mode=SiloMode.REGION,
+    retry=Retry(times=3, delay=60),
 )
 def assign_seat_to_organization_contributor(contributor_id) -> None:
     try:
@@ -63,3 +65,4 @@ def assign_seat_to_organization_contributor(contributor_id) -> None:
                 "outcome": outcome,
             },
         )
+        retry_task()
