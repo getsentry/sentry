856aa60d0255d3329d05f9a78c79ffc29049b221
Sofia Rest <68917129+srest2021@users.noreply.github.com>
2025-12-11 14:41:44 -0800

    fix(code-review): do not check the PR review toggle in consent check for seat-based plan (#104700)
    fixes ENG-6047
    
    Do not check the value of `"sentry:enable_pr_review_test_generation"`
    toggle for seat-based plans (ie, orgs under
    `"organizations:seat-based-seer-enabled"`. For usage-based plans (ie,
    orgs under `"organizations:seer-added"`) and default behavior, do check
    the value.

--- src/sentry/seer/endpoints/seer_rpc.py
+++ src/sentry/seer/endpoints/seer_rpc.py
@@ -42,11 +42,7 @@ from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.authentication import AuthenticationSiloLimit, StandardAuthentication
 from sentry.api.base import Endpoint, internal_region_silo_endpoint
 from sentry.api.endpoints.project_trace_item_details import convert_rpc_attribute_to_json
-from sentry.constants import (
-    ENABLE_PR_REVIEW_TEST_GENERATION_DEFAULT,
-    HIDE_AI_FEATURES_DEFAULT,
-    ObjectStatus,
-)
+from sentry.constants import ObjectStatus
 from sentry.exceptions import InvalidSearchQuery
 from sentry.hybridcloud.rpc.service import RpcAuthenticationSetupException, RpcResolutionException
 from sentry.hybridcloud.rpc.sig import SerializableFunctionValueException
@@ -109,6 +105,7 @@ from sentry.snuba.referrer import Referrer
 from sentry.utils import snuba_rpc
 from sentry.utils.dates import parse_stats_period
 from sentry.utils.env import in_test_environment
+from sentry.utils.seer import can_use_prevent_ai_features
 from sentry.utils.snuba_rpc import table_rpc
 
 logger = logging.getLogger(__name__)
@@ -283,20 +280,6 @@ def get_organization_project_ids(*, org_id: int) -> dict:
     return {"projects": projects}
 
 
-def _can_use_prevent_ai_features(org: Organization) -> bool:
-    if not features.has("organizations:gen-ai-features", org):
-        return False
-
-    hide_ai_features = org.get_option("sentry:hide_ai_features", HIDE_AI_FEATURES_DEFAULT)
-    pr_review_test_generation_enabled = bool(
-        org.get_option(
-            "sentry:enable_pr_review_test_generation",
-            ENABLE_PR_REVIEW_TEST_GENERATION_DEFAULT,
-        )
-    )
-    return not hide_ai_features and pr_review_test_generation_enabled
-
-
 class SentryOrganizaionIdsAndSlugs(TypedDict):
     org_ids: list[int]
     org_slugs: list[str]
@@ -333,7 +316,7 @@ def get_sentry_organization_ids(
     )
     organizations = Organization.objects.filter(id__in=organization_ids)
     # We then filter out all orgs that didn't give us consent to use AI features.
-    orgs_with_consent = [org for org in organizations if _can_use_prevent_ai_features(org)]
+    orgs_with_consent = [org for org in organizations if can_use_prevent_ai_features(org)]
 
     return {
         "org_ids": [organization.id for organization in orgs_with_consent],
@@ -364,7 +347,7 @@ def get_organization_seer_consent_by_org_name(
     for org_integration in org_integrations:
         try:
             org = Organization.objects.get(id=org_integration.organization_id)
-            if _can_use_prevent_ai_features(org):
+            if can_use_prevent_ai_features(org):
                 return {"consent": True}
             # If this is the last org we will return this URL as the consent URL
             consent_url = org.absolute_url("/settings/organization/")
