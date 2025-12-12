b10a22f19bc638f437e69b27a41b209baea18b7f
Shruthilaya Jaganathan <shruthilaya.jaganathan@sentry.io>
2025-12-12 10:29:54 -0500

    feat: Do a prefetch on stats endpoint

--- src/sentry/api/endpoints/organization_trace_item_stats.py
+++ src/sentry/api/endpoints/organization_trace_item_stats.py
@@ -1,12 +1,9 @@
 import logging
-from collections import defaultdict
-from concurrent.futures import ThreadPoolExecutor, as_completed
 
 import sentry_sdk
 from rest_framework import serializers
 from rest_framework.request import Request
 from rest_framework.response import Response
-from sentry_protos.snuba.v1.endpoint_trace_item_attributes_pb2 import TraceItemAttributeNamesRequest
 from sentry_protos.snuba.v1.request_common_pb2 import PageToken, TraceItemType
 from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeKey
 
@@ -24,7 +21,7 @@ from sentry.search.eap.constants import SUPPORTED_STATS_TYPES
 from sentry.search.eap.resolver import SearchResolver
 from sentry.search.eap.spans.attributes import (
     SPAN_ATTRIBUTE_DEFINITIONS,
-    SPANS_STATS_EXCLUDED_ATTRIBUTES,
+    SPANS_STATS_EXCLUDED_ATTRIBUTES_PUBLIC_ALIAS,
 )
 from sentry.search.eap.spans.definitions import SPAN_DEFINITIONS
 from sentry.search.eap.types import SearchResolverConfig
@@ -32,6 +29,7 @@ from sentry.snuba.referrer import Referrer
 from sentry.snuba.spans_rpc import Spans
 from sentry.utils import snuba_rpc
 from sentry.utils.cursors import Cursor, CursorResult
+from sentry.utils.snuba_rpc import TraceItemAttributeNamesRequest
 
 logger = logging.getLogger(__name__)
 
@@ -127,13 +125,31 @@ class OrganizationTraceItemsStatsEndpoint(OrganizationEventsEndpointBase):
         attr_type = AttributeKey.Type.TYPE_STRING
         max_attributes = options.get("explore.trace-items.keys.max")
 
-        additional_substring_matches = set()
-        if value_substring_match:
-            additional_substring_matches = {
-                v.internal_name
-                for k, v in SPAN_ATTRIBUTE_DEFINITIONS.items()
-                if value_substring_match in k
-            }
+        def get_table_results():
+            with handle_query_errors():
+                return Spans.run_table_query(
+                    params=snuba_params,
+                    config=SearchResolverConfig(),
+                    offset=0,
+                    limit=1000,
+                    sampling_mode=snuba_params.sampling_mode,
+                    query_string=serialized.get("query", ""),
+                    orderby=["span_id"],
+                    referrer=Referrer.API_SPANS_FREQUENCY_STATS_RPC.value,
+                    selected_columns=["span_id"],
+                )
+
+        def run_stats_query_with_span_ids(span_id_filter):
+            with handle_query_errors():
+                return Spans.run_stats_query(
+                    params=snuba_params,
+                    stats_types=serialized.get("statsType"),
+                    query_string=span_id_filter,
+                    referrer=Referrer.API_SPANS_FREQUENCY_STATS_RPC.value,
+                    config=resolver_config,
+                    search_resolver=resolver,
+                    max_buckets=1,
+                )
 
         def run_stats_query_with_error_handling(attributes):
             with handle_query_errors():
@@ -168,52 +184,42 @@ class OrganizationTraceItemsStatsEndpoint(OrganizationEventsEndpointBase):
                     "num_attrs_with_intersecting_filter", len(attrs_response.attributes)
                 )
 
-            # Chunk attributes and run stats query in parallel
-            chunked_attributes: defaultdict[int, list[AttributeKey]] = defaultdict(list)
-            for i, attr in enumerate(attrs_response.attributes):
-                if attr.name in SPANS_STATS_EXCLUDED_ATTRIBUTES:
+            table_results = get_table_results()
+            span_ids = [row["span_id"] for row in table_results["data"]]
+            span_id_list = ",".join(span_ids)
+
+            preflight_stats = run_stats_query_with_span_ids(f"id:[{span_id_list}]")
+            attr_keys = list(preflight_stats[0]["attribute_distributions"]["data"].keys())
+            sanitized_keys = []
+            for key in attr_keys:
+                if key in SPANS_STATS_EXCLUDED_ATTRIBUTES_PUBLIC_ALIAS:
                     continue
 
-                if attr.name in additional_substring_matches:
-                    # dedupe additional known attrs on the first offset
-                    if offset == 0:
-                        additional_substring_matches.remove(attr.name)
-                    # we've already shown this attr in the first offset, so
-                    # don't show it again
-                    else:
-                        continue
+                if substring_match and substring_match in key:
+                    sanitized_keys.append(key)
+                    continue
 
-                chunked_attributes[i % MAX_THREADS].append(
-                    AttributeKey(name=attr.name, type=AttributeKey.TYPE_STRING)
-                )
-            if offset == 0:
-                for i, additional_attr in enumerate(additional_substring_matches):
-                    if additional_attr in SPANS_STATS_EXCLUDED_ATTRIBUTES:
-                        continue
-                    chunked_attributes[i % MAX_THREADS].append(
-                        AttributeKey(name=additional_attr, type=AttributeKey.TYPE_STRING)
+                sanitized_keys.append(key)
+
+            sanitized_keys = sanitized_keys[offset : offset + limit]
+
+            request_attrs_list = []
+            for requested_key in sanitized_keys:
+                if requested_key in SPAN_ATTRIBUTE_DEFINITIONS:
+                    request_attrs_list.append(
+                        AttributeKey(
+                            name=SPAN_ATTRIBUTE_DEFINITIONS[requested_key].internal_name,
+                            type=AttributeKey.TYPE_STRING,
+                        )
+                    )
+                else:
+                    request_attrs_list.append(
+                        AttributeKey(name=requested_key, type=AttributeKey.TYPE_STRING)
                     )
 
-            stats_results: dict[str, dict[str, dict]] = defaultdict(lambda: {"data": {}})
-            with ThreadPoolExecutor(
-                thread_name_prefix=__name__,
-                max_workers=MAX_THREADS,
-            ) as query_thread_pool:
+            stats_results = run_stats_query_with_error_handling(request_attrs_list)
 
-                futures = [
-                    query_thread_pool.submit(run_stats_query_with_error_handling, attributes)
-                    for attributes in chunked_attributes.values()
-                ]
-
-                for future in as_completed(futures):
-                    result = future.result()
-                    for stats in result:
-                        for stats_type, data in stats.items():
-                            stats_results[stats_type]["data"].update(data["data"])
-
-            return {"data": [{k: v} for k, v in stats_results.items()]}, len(
-                attrs_response.attributes
-            )
+            return {"data": stats_results}, len(request_attrs_list)
 
         return self.paginate(
             request=request,
