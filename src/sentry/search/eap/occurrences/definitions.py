673d53609095af1c933fdec6f653aaa1d8fe2e7b
Charles Paul <charles.paul@sentry.io>
2025-12-11 21:29:39 -0800

    feat(occurrences): Add RPCBase  boilerplate
    There's a lot of good work & validation in this framework. Let's pick it up and use it for our own ends.
    
    This PR adds an implementation of RPCBase for use with Occurrence trace items.

---
+++ src/sentry/search/eap/occurrences/definitions.py
@@ -0,0 +1,40 @@
+from sentry_protos.snuba.v1.request_common_pb2 import TraceItemType
+from sentry_protos.snuba.v1.trace_item_attribute_pb2 import AttributeKey
+
+from sentry.search.eap.columns import ColumnDefinitions, ResolvedAttribute
+from sentry.search.eap.common_columns import COMMON_COLUMNS
+
+OCCURRENCES_ALWAYS_PRESENT_ATTRIBUTES = [
+    AttributeKey(name="group_id", type=AttributeKey.Type.TYPE_INT),
+]
+
+
+OCCURRENCE_COLUMNS = {
+    column.public_alias: column
+    for column in (
+        COMMON_COLUMNS
+        + [
+            ResolvedAttribute(
+                public_alias="id",
+                internal_name="sentry.item_id",
+                search_type="string",
+            ),
+            ResolvedAttribute(
+                public_alias="group_id",
+                internal_name="group_id",
+                search_type="integer",
+            ),
+        ]
+    )
+}
+
+OCCURRENCE_DEFINITIONS = ColumnDefinitions(
+    aggregates={},  # c.f. SPAN_AGGREGATE_DEFINITIONS when we're ready.
+    formulas={},
+    columns=OCCURRENCE_COLUMNS,
+    contexts={},
+    trace_item_type=TraceItemType.TRACE_ITEM_TYPE_OCCURRENCE,
+    filter_aliases={},
+    alias_to_column=None,
+    column_to_alias=None,
+)
