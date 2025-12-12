75e799343b0becd7b156bb13ce23d959febea38c
Nate Moore <natemoo-re@users.noreply.github.com>
2025-12-12 13:40:56 -0500

    ref(scraps): use new typography tokens (#104470)
    Stacked PR on top of #104468. This PR migrates our existing core
    components to use the new typography tokens. Components outside of core
    will be handled in a follow-up, as there are a lot of changes to make.
    
    For ease of review, I recommend reviewing by commit rather than as a
    whole.
    
    - 1a227b682be2a18be398b10963c8254907a6f47d migrates to
    `theme.font.size.*`
    - ea1c234e2491dc683476bf27c909507d69368266 migrates to
    `theme.font.lineHeight.*`
    - c10a57792bcd1c6223fe713eaed6e8fd468ef6f3 migrates to
    `theme.font.family.*`
    - 2a4466bfc268f7aab040404ac651367019cc7107 migrates to
    `theme.font.weight.medium`
    - 80047b8ef886f3c17c1a5372272ca3070a915c80 migrates to
    `theme.font.weight.regular`
    
    Fixes DE-605, DE-311, DE-312, DE-316

--- static/app/components/core/badge/badge.chonk.tsx
+++ static/app/components/core/badge/badge.chonk.tsx
@@ -34,13 +34,13 @@ export function ChonkBadge(props: ChonkBadgeProps) {
 const StyledChonkBadge = chonkStyled('span')<ChonkBadgeProps>`
   ${p => ({...makeChonkBadgeTheme(p, p.theme)})};
   border-radius: ${p => p.theme.radius.sm};
-  font-size: ${p => p.theme.fontSize.sm};
+  font-size: ${p => p.theme.font.size.sm};
 
   display: inline-flex;
   align-items: center;
   line-height: initial;
   height: 20px;
-  font-weight: ${p => p.theme.fontWeight.bold};
+  font-weight: ${p => p.theme.font.weight.medium};
   padding: ${p => p.theme.space.xs} ${p => p.theme.space.xs};
 `;
 
