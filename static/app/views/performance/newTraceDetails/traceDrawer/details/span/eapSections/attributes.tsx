2ab05975a5741f693fc738f24b9013baab6552ad
Ogi <86684834+obostjancic@users.noreply.github.com>
2025-12-15 11:49:11 +0100

    fix

--- static/app/views/performance/newTraceDetails/traceDrawer/details/span/eapSections/attributes.tsx
+++ static/app/views/performance/newTraceDetails/traceDrawer/details/span/eapSections/attributes.tsx
@@ -166,7 +166,7 @@ export function Attributes({
     [SpanFields.GEN_AI_COST_TOTAL_TOKENS]: (props: CustomRenderersProps) => {
       return formatDollars(+Number(props.item.value).toFixed(10));
     },
-    [SpanFields.GEN_AI_USAGE_TOTAL_COST]: (props: CustomRenderersProps) => {
+    [SpanFields.GEN_AI_COST_TOTAL_TOKENS]: (props: CustomRenderersProps) => {
       return formatDollars(+Number(props.item.value).toFixed(10));
     },
   };
