5be46272205d42338182a1a1063a7bde3d4d484b
George Gritsouk <989898+gggritso@users.noreply.github.com>
2025-12-16 17:45:24 -0500

    Stray paren

--- static/app/views/insights/mobile/screenload/views/screenLoadSpansPage.tsx
+++ static/app/views/insights/mobile/screenload/views/screenLoadSpansPage.tsx
@@ -166,7 +166,6 @@ export function ScreenLoadSpansContent() {
                 transaction={transactionName}
               />
             </SampleContainerItem>
-            )
           </SampleContainer>
         )}
         {sampleType === SPANS && (
