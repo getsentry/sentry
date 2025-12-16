3acef97d34956bb6a2108797db76844a8f88eb9f
Nar Saynorath <nar.saynorath@sentry.io>
2025-12-16 11:23:57 -0500

    fix(logs): Use replace for first page param init (#105046)
    When a user navigates to logs, we initialize some base state and push it
    to the URL. That URL push messes up back-navigation because when a user
    immediately hits "back", they go to the base URL that needs
    initialization so it gets re-pushed and the user can't successfully
    navigate.
    
    The solution is to replace the history rather than push onto the stack
    for the first initialization.

--- static/app/views/explore/logs/usePersistentLogsPageParameters.tsx
+++ static/app/views/explore/logs/usePersistentLogsPageParameters.tsx
@@ -1,4 +1,4 @@
-import {useEffect} from 'react';
+import {useEffect, useRef} from 'react';
 import type {Location} from 'history';
 
 import {useLocation} from 'sentry/utils/useLocation';
@@ -20,6 +20,11 @@ export function usePersistentLogsPageParameters() {
   const location = useLocation();
   const navigate = useNavigate();
   const [persistedParams] = usePersistedLogsPageParams();
+
+  // The first render for setting these params in the URL should replace the current URL
+  // to avoid adding extra entries to the history stack that lead to a re-init state (i.e.
+  // a URL state where these props need to be re-applied, blocking back navigation)
+  const firstRender = useRef(true);
   useEffect(() => {
     let changedSomething = false;
     const target: Location = {...location, query: {...location.query}};
@@ -32,7 +37,8 @@ export function usePersistentLogsPageParameters() {
       changedSomething = !!target.query[LOGS_SORT_BYS_KEY];
     }
     if (changedSomething) {
-      navigate(target);
+      navigate(target, {replace: firstRender.current});
+      firstRender.current = false;
     }
   }, [location, navigate, persistedParams]);
 }
