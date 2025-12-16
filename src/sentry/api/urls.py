9906778c90e050c71fc86bd66e01ac220906b56d
Priscila Oliveira <priscila.oliveira@sentry.io>
2025-12-16 12:42:26 +0100

    update endpoint name

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -1614,7 +1614,7 @@ ORGANIZATION_URLS: list[URLPattern | URLResolver] = [
         name="sentry-api-0-organization-api-key-details",
     ),
     re_path(
-        r"^(?P<organization_id_or_slug>[^/]+)/keys/$",
+        r"^(?P<organization_id_or_slug>[^/]+)/project-keys/$",
         OrganizationProjectKeysEndpoint.as_view(),
         name="sentry-api-0-organization-project-keys",
     ),
