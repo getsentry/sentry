f711a6d67265809bcfec1af5f65a0b1511909a13
nsdeschenes <nicholas.deschenes@sentry.io>
2025-12-16 07:58:48 -0400

    :white_check_mark: Add in tests

--- static/app/utils/replays/hooks/useActiveReplayTab.spec.tsx
+++ static/app/utils/replays/hooks/useActiveReplayTab.spec.tsx
@@ -12,70 +12,128 @@ describe('useActiveReplayTab', () => {
     setWindowLocation('http://localhost/');
   });
 
-  describe('breadcrumbs tab is default', () => {
-    it('should use Breadcrumbs as a default', () => {
-      const {result} = renderHook(useActiveReplayTab, {
-        initialProps: {},
-        wrapper: ({children}) => (
-          <OrganizationContext value={OrganizationFixture({features: []})}>
-            {children}
-          </OrganizationContext>
-        ),
+  describe('when AI features are allowed', () => {
+    describe('AI summary tab is default', () => {
+      it('should use AI summary as a default', () => {
+        const {result} = renderHook(useActiveReplayTab, {
+          initialProps: {areAiFeaturesAllowed: true},
+          wrapper: ({children}) => (
+            <OrganizationContext
+              value={OrganizationFixture({features: ['replay-ai-summaries']})}
+            >
+              {children}
+            </OrganizationContext>
+          ),
+        });
+
+        expect(result.current.getActiveTab()).toBe(TabKey.AI);
       });
 
-      expect(result.current.getActiveTab()).toBe(TabKey.BREADCRUMBS);
+      it('should use Breadcrumbs as a default, when there is a click search in the url', () => {
+        setWindowLocation('http://localhost/?query=click.tag:button');
+
+        const {result} = renderHook(useActiveReplayTab, {
+          initialProps: {areAiFeaturesAllowed: true},
+          wrapper: ({children}) => (
+            <OrganizationContext
+              value={OrganizationFixture({features: ['replay-ai-summaries']})}
+            >
+              {children}
+            </OrganizationContext>
+          ),
+        });
+
+        expect(result.current.getActiveTab()).toBe(TabKey.AI);
+      });
+
+      it('should set the default tab if the name is invalid', () => {
+        const {result} = renderHook(useActiveReplayTab, {
+          initialProps: {areAiFeaturesAllowed: true},
+          wrapper: ({children}) => (
+            <OrganizationContext
+              value={OrganizationFixture({features: ['replay-ai-summaries']})}
+            >
+              {children}
+            </OrganizationContext>
+          ),
+        });
+        expect(result.current.getActiveTab()).toBe(TabKey.AI);
+
+        result.current.setActiveTab('foo bar');
+        expect(browserHistory.push).toHaveBeenLastCalledWith({
+          pathname: '/',
+          query: {t_main: TabKey.AI},
+        });
+      });
     });
+  });
 
-    it('should use Breadcrumbs as a default, when there is a click search in the url', () => {
-      setWindowLocation('http://localhost/?query=click.tag:button');
+  describe('when AI features are not allowed', () => {
+    describe('breadcrumbs tab is default', () => {
+      it('should use Breadcrumbs as a default', () => {
+        const {result} = renderHook(useActiveReplayTab, {
+          initialProps: {areAiFeaturesAllowed: false},
+          wrapper: ({children}) => (
+            <OrganizationContext value={OrganizationFixture({features: []})}>
+              {children}
+            </OrganizationContext>
+          ),
+        });
 
-      const {result} = renderHook(useActiveReplayTab, {
-        initialProps: {},
-        wrapper: ({children}) => (
-          <OrganizationContext value={OrganizationFixture({features: []})}>
-            {children}
-          </OrganizationContext>
-        ),
+        expect(result.current.getActiveTab()).toBe(TabKey.BREADCRUMBS);
       });
 
-      expect(result.current.getActiveTab()).toBe(TabKey.BREADCRUMBS);
+      it('should use Breadcrumbs as a default, when there is a click search in the url', () => {
+        setWindowLocation('http://localhost/?query=click.tag:button');
+
+        const {result} = renderHook(useActiveReplayTab, {
+          initialProps: {areAiFeaturesAllowed: false},
+          wrapper: ({children}) => (
+            <OrganizationContext value={OrganizationFixture({features: []})}>
+              {children}
+            </OrganizationContext>
+          ),
+        });
+
+        expect(result.current.getActiveTab()).toBe(TabKey.BREADCRUMBS);
+      });
+
+      it('should set the default tab if the name is invalid', () => {
+        const {result} = renderHook(useActiveReplayTab, {
+          initialProps: {areAiFeaturesAllowed: false},
+          wrapper: ({children}) => (
+            <OrganizationContext value={OrganizationFixture({features: []})}>
+              {children}
+            </OrganizationContext>
+          ),
+        });
+        expect(result.current.getActiveTab()).toBe(TabKey.BREADCRUMBS);
+
+        result.current.setActiveTab('foo bar');
+        expect(browserHistory.push).toHaveBeenLastCalledWith({
+          pathname: '/',
+          query: {t_main: TabKey.BREADCRUMBS},
+        });
+      });
     });
+  });
 
-    it('should set the default tab if the name is invalid', () => {
-      const {result} = renderHook(useActiveReplayTab, {
-        initialProps: {},
-        wrapper: ({children}) => (
-          <OrganizationContext value={OrganizationFixture({features: []})}>
-            {children}
-          </OrganizationContext>
-        ),
-      });
-      expect(result.current.getActiveTab()).toBe(TabKey.BREADCRUMBS);
-
-      result.current.setActiveTab('foo bar');
-      expect(browserHistory.push).toHaveBeenLastCalledWith({
-        pathname: '/',
-        query: {t_main: TabKey.BREADCRUMBS},
-      });
+  it('should allow case-insensitive tab names', () => {
+    const {result} = renderHook(useActiveReplayTab, {
+      initialProps: {areAiFeaturesAllowed: false},
+      wrapper: ({children}) => (
+        <OrganizationContext value={OrganizationFixture({features: []})}>
+          {children}
+        </OrganizationContext>
+      ),
     });
+    expect(result.current.getActiveTab()).toBe(TabKey.BREADCRUMBS);
 
-    it('should allow case-insensitive tab names', () => {
-      const {result} = renderHook(useActiveReplayTab, {
-        initialProps: {},
-        wrapper: ({children}) => (
-          <OrganizationContext value={OrganizationFixture({features: []})}>
-            {children}
-          </OrganizationContext>
-        ),
-      });
-      expect(result.current.getActiveTab()).toBe(TabKey.BREADCRUMBS);
-
-      result.current.setActiveTab('nEtWoRk');
-      expect(browserHistory.push).toHaveBeenLastCalledWith({
-        pathname: '/',
-        state: undefined,
-        query: {t_main: TabKey.NETWORK},
-      });
+    result.current.setActiveTab('nEtWoRk');
+    expect(browserHistory.push).toHaveBeenLastCalledWith({
+      pathname: '/',
+      state: undefined,
+      query: {t_main: TabKey.NETWORK},
     });
   });
 });
