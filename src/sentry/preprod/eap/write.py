a8cc02d2cca86751aa7bf01ebf4e00a4da53649b
Trevor Elkins <trevor@emergetools.com>
2025-12-17 18:32:34 -0500

    fix

--- src/sentry/preprod/eap/write.py
+++ src/sentry/preprod/eap/write.py
@@ -4,16 +4,18 @@ import uuid
 from typing import Any
 
 from arroyo import Topic as ArroyoTopic
-from arroyo.backends.kafka import KafkaPayload
+from arroyo.backends.kafka import KafkaPayload, KafkaProducer
 from google.protobuf.timestamp_pb2 import Timestamp
+from sentry_kafka_schemas.codecs import Codec
 from sentry_protos.snuba.v1.request_common_pb2 import TraceItemType
+from sentry_protos.snuba.v1.trace_item_pb2 import TraceItem
 from sentry_protos.snuba.v1.trace_item_pb2 import TraceItem as EAPTraceItem
 
-from sentry.conf.types.kafka_definition import Topic
+from sentry.conf.types.kafka_definition import Topic, get_topic_codec
 from sentry.preprod.eap.constants import PREPROD_NAMESPACE
 from sentry.preprod.models import PreprodArtifactSizeMetrics
-from sentry.replays.lib.kafka import EAP_ITEMS_CODEC, eap_producer
 from sentry.search.eap.rpc_utils import anyvalue
+from sentry.utils.arroyo_producer import SingletonProducer, get_arroyo_producer
 from sentry.utils.kafka_config import get_topic_definition
 
 
@@ -110,4 +112,18 @@ def write_preprod_size_metric_to_eap(
 
     topic = get_topic_definition(Topic.SNUBA_ITEMS)["real_topic_name"]
     payload = KafkaPayload(None, EAP_ITEMS_CODEC.encode(trace_item), [])
-    eap_producer.produce(ArroyoTopic(topic), payload)
+    _eap_producer.produce(ArroyoTopic(topic), payload)
+
+
+EAP_ITEMS_CODEC: Codec[TraceItem] = get_topic_codec(Topic.SNUBA_ITEMS)
+
+
+def _get_eap_items_producer() -> KafkaProducer:
+    """Get a Kafka producer for EAP TraceItems."""
+    return get_arroyo_producer(
+        name="sentry.preprod.lib.kafka.eap_items",
+        topic=Topic.SNUBA_ITEMS,
+    )
+
+
+_eap_producer = SingletonProducer(_get_eap_items_producer)
