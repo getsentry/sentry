e477ca6d225107b24f0435b3f99f1b32293e96ec
Daniel Szoke <daniel.szoke@sentry.io>
2025-12-17 18:01:49 +0100

    TypeGuard to fix mypy

--- src/sentry/models/apitoken.py
+++ src/sentry/models/apitoken.py
@@ -6,13 +6,14 @@ import re
 import secrets
 from collections.abc import Collection, Mapping
 from datetime import timedelta
-from typing import Any, ClassVar
+from typing import Any, ClassVar, TypeGuard
 
 from django.db import models, router, transaction
 from django.utils import timezone
 from django.utils.crypto import constant_time_compare
 from django.utils.encoding import force_str
 
+from sentry.auth.services.auth import AuthenticatedToken
 from sentry.backup.dependencies import ImportKind, NormalizedModelName, get_model_name
 from sentry.backup.helpers import ImportFlags
 from sentry.backup.sanitize import SanitizableField, Sanitizer
@@ -20,6 +21,7 @@ from sentry.backup.scopes import ImportScope, RelocationScope
 from sentry.constants import SentryAppStatus
 from sentry.db.models import FlexibleForeignKey, control_silo_model, sane_repr
 from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
+from sentry.hybridcloud.models import ApiTokenReplica
 from sentry.hybridcloud.outbox.base import ControlOutboxProducingManager, ReplicatedControlModel
 from sentry.hybridcloud.outbox.category import OutboxCategory
 from sentry.locks import locks
@@ -555,7 +557,7 @@ class ApiToken(ReplicatedControlModel, HasApiScopes):
         return installation.organization_id
 
 
-def is_api_token_auth(auth: object) -> bool:
+def is_api_token_auth(auth: object) -> TypeGuard[AuthenticatedToken | ApiToken | ApiTokenReplica]:
     """:returns True when an API token is hitting the API."""
     from sentry.auth.services.auth import AuthenticatedToken
     from sentry.hybridcloud.models.apitokenreplica import ApiTokenReplica
