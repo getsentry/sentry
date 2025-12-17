9234a5b1fecdd042f6221025d4a2a94db0e39eab
TkDodo <dominik.dorfmeister@sentry.io>
2025-12-17 09:43:06 +0100

    ref: better error message
    by adding a union with a type that contains the descriptive error message

--- static/app/components/core/compactSelect/trigger.tsx
+++ static/app/components/core/compactSelect/trigger.tsx
@@ -1,22 +1,21 @@
 import * as React from 'react';
+import type {DistributedOmit} from 'type-fest';
 
-import {Input} from '@sentry/scraps/input';
-
-import DropdownButton from 'sentry/components/dropdownButton';
+import DropdownButton, {type DropdownButtonProps} from 'sentry/components/dropdownButton';
 
 import {SelectContext} from './control';
 
-type TriggerEl = HTMLButtonElement | HTMLInputElement;
+type TriggerEl =
+  | HTMLButtonElement
+  | (Omit<HTMLButtonElement, 'type'> & {
+      type: 'only use SelectTrigger for the trigger prop!';
+    });
 
 export type SelectTriggerProps = React.HTMLAttributes<TriggerEl> & {
   ref?: React.Ref<TriggerEl>;
 };
 
-export type ButtonTriggerProps = React.ComponentPropsWithoutRef<typeof DropdownButton> & {
-  ref?: React.Ref<TriggerEl>;
-};
-
-type InputTriggerProps = React.ComponentPropsWithoutRef<typeof Input> & {
+export type ButtonTriggerProps = DistributedOmit<DropdownButtonProps, 'ref'> & {
   ref?: React.Ref<TriggerEl>;
 };
 
@@ -31,21 +30,6 @@ export const SelectTrigger = {
         {...contextProps}
         {...componentProps}
         ref={ref as React.Ref<HTMLButtonElement>}
-      />
-    );
-  },
-
-  Input(props: InputTriggerProps) {
-    const {ref, ...componentProps} = props;
-
-    const selectContext = React.useContext(SelectContext);
-    const contextProps = {size: selectContext.size};
-
-    return (
-      <Input
-        {...contextProps}
-        {...componentProps}
-        ref={ref as React.Ref<HTMLInputElement>}
       />
     );
   },
