8608e59adbbe0aed49810d7bfcf2835c35a2b2c0
Charles Paul <charles.paul@sentry.io>
2025-12-17 10:10:09 -0800

    ref(occurrences): Migrate existing callsite to new Comparator
    Made the underlying logic here more generic. Let's use the new standard rather than our old bespoke functions.

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -3699,28 +3699,6 @@ register(
     flags=FLAG_AUTOMATOR_MODIFIABLE,
 )
 
-# Controls whether occurrence data should be read from both Snuba and EAP.
-# Will not use or display the EAP data to the user; rather, will just (1) issue
-# the queries to ensure that reads are functional and (2) compare the data from
-# each source and log whether they match.
-# This option should be controlled on a region-by-region basis.
-register(
-    "eap.occurrences.should_double_read",
-    type=Bool,
-    default=False,
-    flags=FLAG_MODIFIABLE_BOOL | FLAG_AUTOMATOR_MODIFIABLE,
-)
-
-# Controls whether a given callsite should use occurrence data from EAP instead
-# of Snuba. Callsites should only be added here after they're known to be safe.
-# This option should be controlled on a region-by-region basis.
-register(
-    "eap.occurrences.callsites_using_eap_data_allowlist",
-    type=Sequence,
-    default=[],
-    flags=FLAG_ALLOW_EMPTY | FLAG_AUTOMATOR_MODIFIABLE,
-)
-
 # Killswich for LLM issue detection
 register(
     "issue-detection.llm-detection.enabled",
