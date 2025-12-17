90e8d72322d7d63b2de1432230a7657b8dea7e76
Nar Saynorath <nar.saynorath@sentry.io>
2025-12-17 13:50:37 -0500

    ref(dashboards): Use dataset getter for search bar data

--- static/app/views/dashboards/hooks/useDatasetSearchBarData.tsx
+++ static/app/views/dashboards/hooks/useDatasetSearchBarData.tsx
@@ -8,45 +8,19 @@ import {WidgetType} from 'sentry/views/dashboards/types';
 export function useDatasetSearchBarData(): (widgetType: WidgetType) => SearchBarData {
   const {selection} = usePageFilters();
 
-  const errorsData = getDatasetConfig(WidgetType.ERRORS).useSearchBarDataProvider!({
-    pageFilters: selection,
-  });
-
-  const logsData = getDatasetConfig(WidgetType.LOGS).useSearchBarDataProvider!({
-    pageFilters: selection,
-  });
-
-  const spansData = getDatasetConfig(WidgetType.SPANS).useSearchBarDataProvider!({
-    pageFilters: selection,
-  });
-
-  const issuesData = getDatasetConfig(WidgetType.ISSUE).useSearchBarDataProvider!({
-    pageFilters: selection,
-  });
-
-  const releasesData = getDatasetConfig(WidgetType.RELEASE).useSearchBarDataProvider!({
-    pageFilters: selection,
-  });
-
   const getSearchBarData = (widgetType: WidgetType): SearchBarData => {
-    switch (widgetType) {
-      case WidgetType.ERRORS:
-        return errorsData;
-      case WidgetType.LOGS:
-        return logsData;
-      case WidgetType.SPANS:
-        return spansData;
-      case WidgetType.ISSUE:
-        return issuesData;
-      case WidgetType.RELEASE:
-        return releasesData;
-      default:
-        return {
-          getFilterKeySections: () => [],
-          getFilterKeys: () => ({}),
-          getTagValues: () => Promise.resolve([]),
-        };
+    const datasetConfig = getDatasetConfig(widgetType);
+    if (datasetConfig.useSearchBarDataProvider) {
+      return datasetConfig.useSearchBarDataProvider({
+        pageFilters: selection,
+      });
     }
+
+    return {
+      getFilterKeySections: () => [],
+      getFilterKeys: () => ({}),
+      getTagValues: () => Promise.resolve([]),
+    };
   };
 
   return getSearchBarData;
