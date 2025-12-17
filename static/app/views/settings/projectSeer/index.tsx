c66ecff85c65ce1eed08c65c017f16e2b1788b6d
Leander Rodrigues <me@leander.xyz>
2025-12-17 11:21:48 -0500

    🐛 flag name correction

--- static/app/views/settings/projectSeer/index.tsx
+++ static/app/views/settings/projectSeer/index.tsx
@@ -681,7 +681,7 @@ export default function ProjectSeerContainer() {
   if (!organization.features.includes('autofix-seer-preferences')) {
     return (
       <FeatureDisabled
-        features={['autofix-seer-preferences']}
+        features={['organizations:autofix-seer-preferences']}
         hideHelpToggle
         message={t('Autofix is not enabled for this organization.')}
         featureName={t('Autofix')}
