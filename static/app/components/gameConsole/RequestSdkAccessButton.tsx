1b53966d5c369d705a6929246054385970b794bd
Alex Sohn <alex.sohn@sentry.io>
2025-12-18 12:02:56 -0500

    initial commit

---
+++ static/app/components/gameConsole/RequestSdkAccessButton.tsx
@@ -0,0 +1,37 @@
+import {Button} from '@sentry/scraps/button';
+
+import {openPrivateGamingSdkAccessModal} from 'sentry/actionCreators/modal';
+import type {PrivateGamingSdkAccessModalProps} from 'sentry/components/modals/privateGamingSdkAccessModal';
+import {IconLock} from 'sentry/icons';
+import {t} from 'sentry/locale';
+import {useReopenGamingSdkModal} from 'sentry/utils/useReopenGamingSdkModal';
+
+export function RequestSdkAccessButton({
+  gamingPlatform,
+  organization,
+  origin,
+  projectId,
+}: PrivateGamingSdkAccessModalProps) {
+  const buttonProps: PrivateGamingSdkAccessModalProps = {
+    gamingPlatform,
+    organization,
+    origin,
+    projectId,
+  };
+
+  useReopenGamingSdkModal(buttonProps);
+
+  return (
+    <Button
+      priority="primary"
+      size="sm"
+      data-test-id="request-sdk-access"
+      icon={<IconLock locked />}
+      onClick={() => {
+        openPrivateGamingSdkAccessModal(buttonProps);
+      }}
+    >
+      {t('Request SDK Access')}
+    </Button>
+  );
+}
