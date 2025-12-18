f4d8104134bacc943d811370c7cf97abfc69279b
isabellaenriquez <isabella.enriquez@sentry.io>
2025-12-18 11:14:07 -0500

    rm unused

--- static/gsApp/utils/salesTax.tsx
+++ static/gsApp/utils/salesTax.tsx
@@ -198,13 +198,6 @@ function countryHasRegionChoices(
   return !!countryCode && countryCode in REGION_BY_COUNTRY_CODE;
 }
 
-function getRegionChoices(countryCode?: BillingDetails['countryCode']) {
-  return countryHasRegionChoices(countryCode)
-    ? // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-      Object.entries(REGION_BY_COUNTRY_CODE[countryCode])
-    : [];
-}
-
 function getRegionChoiceCode(
   countryCode?: BillingDetails['countryCode'],
   region?: BillingDetails['region']
@@ -233,7 +226,6 @@ function getRegionChoiceName(
 export {
   countryHasRegionChoices,
   countryHasSalesTax,
-  getRegionChoices,
   getRegionChoiceCode,
   getRegionChoiceName,
   getTaxFieldInfo,
