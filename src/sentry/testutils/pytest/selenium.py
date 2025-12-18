d6e714eeefe7f958dadbbce72571cbccc499488d
Joshua Li <joshuarli98@gmail.com>
2025-12-18 13:17:22 -0800

    browser fixture should be at least class

--- src/sentry/testutils/pytest/selenium.py
+++ src/sentry/testutils/pytest/selenium.py
@@ -396,7 +396,7 @@ def start_chrome(**chrome_args):
             )
 
 
-@pytest.fixture(scope="function")
+@pytest.fixture(scope="class")
 def browser(request, live_server):
     window_size = request.config.getoption("window_size")
     window_width, window_height = map(int, window_size.split("x", 1))
