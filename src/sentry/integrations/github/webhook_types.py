4476aec6b4429b26cb5632926e8dfc4fa174ff70
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2025-12-18 07:52:29 -0500

    feat(code_review): Handle GitHub re-run check requests (#104455)
    For code reviews, we create a GitHub check. In some cases, the customer
    may request the check to be re-run (e.g. the task failed or timed out).
    
    This handles the GitHub re-run event by scheduling a task which calls
    Seer with the original run check ID to re-schedule it (it requires
    https://github.com/getsentry/seer/pull/4173).
    
    Fixes
    [CW-18](https://linear.app/getsentry/issue/CW-18/forward-check-re-run-webhook-from-sentry-to-seer).
    
    ---------
    
    Co-authored-by: Giovanni M Guidini <giovanni.guidini@sentry.io>

--- src/sentry/integrations/github/webhook_types.py
+++ src/sentry/integrations/github/webhook_types.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from enum import StrEnum
 
 GITHUB_WEBHOOK_TYPE_HEADER = "HTTP_X_GITHUB_EVENT"
@@ -14,3 +16,4 @@ class GithubWebhookType(StrEnum):
     PULL_REQUEST_REVIEW_COMMENT = "pull_request_review_comment"
     PULL_REQUEST_REVIEW = "pull_request_review"
     PUSH = "push"
+    CHECK_RUN = "check_run"
