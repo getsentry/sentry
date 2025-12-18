e0dd91f5baeb893ea49467502957882eef105395
Max Topolsky <maxptop@gmail.com>
2025-12-18 14:08:46 -0800

    fix help button tooltip position

--- static/app/components/dropdownMenu/index.tsx
+++ static/app/components/dropdownMenu/index.tsx
@@ -292,7 +292,6 @@ function DropdownMenu({
 export {DropdownMenu};
 
 const DropdownMenuWrap = styled('div')`
-  display: contents;
   list-style-type: none;
   > :first-child {
     margin-left: -1px;
