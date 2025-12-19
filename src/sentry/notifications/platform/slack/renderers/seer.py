3e45b510f3b4fc8f3eea9f4986da8759c2c65e5c
Leander Rodrigues <me@leander.xyz>
2025-12-19 11:35:39 -0500

    ✨ implement entrypoint and renderers

--- src/sentry/notifications/platform/slack/renderers/seer.py
+++ src/sentry/notifications/platform/slack/renderers/seer.py
@@ -1,11 +1,17 @@
+import orjson
 from slack_sdk.models.blocks import (
     ActionsBlock,
+    Block,
     ButtonElement,
     ContextBlock,
-    InputBlock,
+    InteractiveElement,
+    LinkButtonElement,
     MarkdownTextObject,
-    PlainTextInputElement,
     PlainTextObject,
+    RichTextBlock,
+    RichTextElementParts,
+    RichTextListElement,
+    RichTextSectionElement,
     SectionBlock,
 )
 
@@ -13,11 +19,12 @@ from sentry.notifications.platform.renderer import NotificationRenderer
 from sentry.notifications.platform.slack.provider import SlackRenderable
 from sentry.notifications.platform.templates.seer import (
     SeerAutofixError,
+    SeerAutofixSuccess,
     SeerAutofixTrigger,
-    SeerContextInput,
-    SeerContextInputComplete,
+    SeerAutofixUpdate,
 )
 from sentry.notifications.platform.types import NotificationData, NotificationRenderedTemplate
+from sentry.seer.autofix.utils import AutofixStoppingPoint
 
 
 class SeerSlackRenderer(NotificationRenderer[SlackRenderable]):
@@ -34,22 +41,33 @@ class SeerSlackRenderer(NotificationRenderer[SlackRenderable]):
             )
         elif isinstance(data, SeerAutofixError):
             return cls.render_autofix_error(data)
-        elif isinstance(data, SeerContextInput):
-            return cls.render_context_input(data)
-        elif isinstance(data, SeerContextInputComplete):
-            return cls.render_context_input_complete(data)
+        elif isinstance(data, SeerAutofixSuccess):
+            return cls.render_autofix_success(data)
+        elif isinstance(data, SeerAutofixUpdate):
+            return cls.render_autofix_update(data)
         else:
             raise ValueError(f"SeerSlackRenderer does not support {data.__class__.__name__}")
 
     @classmethod
+    def create_first_block_id(cls, group_id: int, run_id: int | None) -> str:
+        # The action handler will fail if the first block's block_id is not JSON-encoded with
+        # group data, so we have to modify that block when emitting actions.
+        return orjson.dumps({"issue": group_id, "run_id": run_id}).decode()
+
+    @classmethod
     def render_autofix_button(cls, data: SeerAutofixTrigger) -> ButtonElement:
+        from sentry.integrations.slack.message_builder.routing import encode_action_id
         from sentry.integrations.slack.message_builder.types import SlackAction
 
         return ButtonElement(
             text=data.label,
             style="primary",
-            value=SlackAction.SEER_AUTOFIX_START.value,
-            action_id=SlackAction.SEER_AUTOFIX_START.value,
+            value=data.stopping_point.value,
+            action_id=encode_action_id(
+                action=SlackAction.SEER_AUTOFIX_START.value,
+                organization_id=data.organization_id,
+                project_id=data.project_id,
+            ),
         )
 
     @classmethod
@@ -63,30 +81,140 @@ class SeerSlackRenderer(NotificationRenderer[SlackRenderable]):
         )
 
     @classmethod
-    def render_context_input(cls, data: SeerContextInput) -> SlackRenderable:
-        from sentry.integrations.slack.message_builder.types import SlackAction
+    def render_autofix_success(cls, data: SeerAutofixSuccess) -> SlackRenderable:
+        autofix_mrkdwn_parts: list[str] = []
+        if data.stopping_point == AutofixStoppingPoint.ROOT_CAUSE:
+            autofix_mrkdwn_parts.append(":hourglass: analyzing the root cause...")
+        elif data.stopping_point == AutofixStoppingPoint.SOLUTION:
+            autofix_mrkdwn_parts.append(":white_check_mark: root cause analyzed")
+            autofix_mrkdwn_parts.append(":hourglass: planning solution...")
+        elif data.stopping_point == AutofixStoppingPoint.CODE_CHANGES:
+            autofix_mrkdwn_parts.append(":white_check_mark: root cause analyzed")
+            autofix_mrkdwn_parts.append(":white_check_mark: solution planned")
+            autofix_mrkdwn_parts.append(":hourglass: generating code changes...")
+        elif data.stopping_point == AutofixStoppingPoint.OPEN_PR:
+            autofix_mrkdwn_parts.append(":white_check_mark: root cause analyzed")
+            autofix_mrkdwn_parts.append(":white_check_mark: solution planned")
+            autofix_mrkdwn_parts.append(":white_check_mark: code changes generated")
+            autofix_mrkdwn_parts.append(":hourglass: drafting the pull request...")
 
         return SlackRenderable(
             blocks=[
-                InputBlock(
-                    dispatch_action=True,
-                    label=PlainTextObject(text=data.label),
-                    element=PlainTextInputElement(
-                        placeholder=PlainTextObject(text=data.placeholder),
-                        action_id=SlackAction.SEER_CONTEXT_INPUT.value,
-                    ),
-                ),
+                SectionBlock(text=MarkdownTextObject(text="*Seer's on it*")),
+                SectionBlock(text=MarkdownTextObject(text="\n\n".join(autofix_mrkdwn_parts))),
                 ContextBlock(elements=[PlainTextObject(text=f"Run ID: {data.run_id}")]),
             ],
-            text="Seer Context Input",
+            text="Seer Autofix Success",
         )
 
     @classmethod
-    def render_context_input_complete(cls, data: SeerContextInputComplete) -> SlackRenderable:
-        return SlackRenderable(
-            blocks=[
-                SectionBlock(text=MarkdownTextObject(text="*Seer says thanks.*")),
-                SectionBlock(text=MarkdownTextObject(text=f">{data.provided_context}")),
-            ],
-            text="Seer Context Input Complete",
-        )
+    def render_autofix_update(cls, data: SeerAutofixUpdate) -> SlackRenderable:
+        from sentry.integrations.slack.message_builder.routing import encode_action_id
+        from sentry.integrations.slack.message_builder.types import SlackAction
+
+        first_block_id = cls.create_first_block_id(group_id=data.group_id, run_id=data.run_id)
+        content_blocks: list[Block] = []
+        action_elements: list[InteractiveElement] = [
+            LinkButtonElement(
+                text="View in Sentry",
+                url=data.group_link,
+                action_id=encode_action_id(
+                    action=SlackAction.SEER_AUTOFIX_VIEW_IN_SENTRY.value,
+                    organization_id=data.organization_id,
+                    project_id=data.project_id,
+                ),
+            ),
+        ]
+        if data.current_point == AutofixStoppingPoint.ROOT_CAUSE:
+            current_stage_mrkdwn = ":mag:  *Root Cause Analysis*"
+            action_elements.append(
+                cls.render_autofix_button(
+                    data=SeerAutofixTrigger(
+                        organization_id=data.organization_id,
+                        project_id=data.project_id,
+                        group_id=data.group_id,
+                        run_id=data.run_id,
+                        stopping_point=AutofixStoppingPoint.SOLUTION,
+                    )
+                )
+            )
+        elif data.current_point == AutofixStoppingPoint.SOLUTION:
+            current_stage_mrkdwn = ":test_tube:  *Proposed Solution*"
+            action_elements.append(
+                cls.render_autofix_button(
+                    data=SeerAutofixTrigger(
+                        organization_id=data.organization_id,
+                        project_id=data.project_id,
+                        group_id=data.group_id,
+                        run_id=data.run_id,
+                        stopping_point=AutofixStoppingPoint.CODE_CHANGES,
+                    )
+                )
+            )
+        elif data.current_point == AutofixStoppingPoint.CODE_CHANGES:
+            current_stage_mrkdwn = ":pencil2:  *Code Change Suggestions*"
+            for change in data.changes:
+                content_blocks.append(
+                    SectionBlock(
+                        text=MarkdownTextObject(
+                            text=f"_In {change['repo_name']}_:\n*{change['title']}*\n{change['description']}\n```\n{change['diff']}```"
+                        )
+                    )
+                )
+            action_elements.append(
+                cls.render_autofix_button(
+                    data=SeerAutofixTrigger(
+                        organization_id=data.organization_id,
+                        project_id=data.project_id,
+                        group_id=data.group_id,
+                        run_id=data.run_id,
+                        stopping_point=AutofixStoppingPoint.OPEN_PR,
+                    )
+                )
+            )
+        elif data.current_point == AutofixStoppingPoint.OPEN_PR:
+            current_stage_mrkdwn = ":link:  *Pull Request*"
+            for pull_request in data.pull_requests:
+                action_elements.append(
+                    LinkButtonElement(
+                        text=f"View PR (#{pull_request['pr_number']})",
+                        style="primary",
+                        url=pull_request["pr_url"],
+                        action_id=encode_action_id(
+                            action=SlackAction.SEER_AUTOFIX_VIEW_PR.value,
+                            organization_id=data.organization_id,
+                            project_id=data.project_id,
+                        ),
+                    )
+                )
+
+        blocks: list[Block] = [
+            SectionBlock(
+                text=MarkdownTextObject(text=current_stage_mrkdwn), block_id=first_block_id
+            )
+        ]
+
+        if data.summary:
+            blocks.append(SectionBlock(text=MarkdownTextObject(text=data.summary)))
+        if data.changes:
+            blocks.extend(content_blocks)
+        if data.steps:
+            blocks.append(
+                RichTextBlock(
+                    elements=[
+                        RichTextListElement(
+                            style="ordered",
+                            indent=0,
+                            elements=[
+                                RichTextSectionElement(
+                                    elements=[RichTextElementParts.Text(text=step)]
+                                )
+                                for step in data.steps
+                            ],
+                        )
+                    ]
+                )
+            )
+        blocks.append(ActionsBlock(elements=action_elements))
+        blocks.append(ContextBlock(elements=[MarkdownTextObject(text=f"Run ID: {data.run_id}")]))
+        return SlackRenderable(blocks=blocks, text="Seer Autofix Update")
