7c3bcdaf8b17b67dd3789f67a6a7b9b6ca10bddb
Ryan Albrecht <ryan.albrecht@sentry.io>
2025-12-19 12:15:29 -0500

    Reapply "ref(prevent): Refactor prevent settings (#104539)"
    This reverts commit 594188c75770037659a83653f944b6667f9883df.

--- static/app/router/routes.tsx
+++ static/app/router/routes.tsx
@@ -2489,10 +2489,6 @@ function buildRoutes(): RouteObject[] {
               path: 'new/',
               component: make(() => import('sentry/views/prevent/preventAI/onboarding')),
             },
-            {
-              path: 'home/',
-              component: make(() => import('sentry/views/prevent/preventAI/index')),
-            },
           ],
         },
       ],
