b15d6ce56a25dd72f08281250741033a9aee5c19
Shashank Jarmale <shashank.jarmale@sentry.io>
2025-12-22 18:19:15 -0700

    Minor cleanup

--- src/sentry/tasks/codeowners/code_owners_auto_sync.py
+++ src/sentry/tasks/codeowners/code_owners_auto_sync.py
@@ -69,6 +69,8 @@ def code_owners_auto_sync(commit_id: int, **kwargs: Any) -> None:
         .filter(codeowners_auto_sync=True, has_codeowners=True)
     )
 
+    organization = Organization.objects.get(id=commit.organization_id)
+
     for code_mapping in code_mappings:
         try:
             codeowner_contents = get_codeowner_contents(code_mapping)
@@ -77,12 +79,12 @@ def code_owners_auto_sync(commit_id: int, **kwargs: Any) -> None:
 
         # If we fail to fetch the codeowners file, the user can manually sync. We'll send them an email on failure.
         if not codeowner_contents:
-            return AutoSyncNotification(code_mapping.project).send()
+            AutoSyncNotification(code_mapping.project).send()
+            return
 
         codeowners: ProjectCodeOwners = ProjectCodeOwners.objects.get(
             repository_project_path_config=code_mapping
         )
-        organization = Organization.objects.get(id=code_mapping.organization_id)
         codeowners.update_schema(
             organization=organization,
             raw=codeowner_contents["raw"],
