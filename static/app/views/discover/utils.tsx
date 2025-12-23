15f6f8a0c6be3e1655af39692b7d8cb280b9874d
Nar Saynorath <nar.saynorath@sentry.io>
2025-12-23 16:15:17 -0500

    Patch area chart conversion

--- static/app/views/discover/utils.tsx
+++ static/app/views/discover/utils.tsx
@@ -650,7 +650,7 @@ export function handleAddQueryToDashboard({
   yAxis?: string | string[];
 }) {
   const displayType =
-    widgetType === WidgetType.SPANS
+    widgetType === WidgetType.SPANS || widgetType === WidgetType.TRACEMETRICS
       ? (eventView.display as DisplayType)
       : displayModeToDisplayType(eventView.display as DisplayModes);
   const defaultWidgetQuery = eventViewToWidgetQuery({
