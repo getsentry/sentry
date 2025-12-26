10376cfb317ea82b36e9bca6e7e868ad82d3ba58
Scott Cooper <scttcper@gmail.com>
2025-12-26 14:09:02 -0800

    test(explore): Move explore tests off deprecatedRouterMocks
    switch to initialRouterConfig and the full in memory browser router

--- static/app/views/explore/savedQueries/savedQueriesTable.spec.tsx
+++ static/app/views/explore/savedQueries/savedQueriesTable.spec.tsx
@@ -62,9 +62,7 @@ describe('SavedQueriesTable', () => {
   });
 
   it('should render', async () => {
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     expect(screen.getByText('Name')).toBeInTheDocument();
     expect(screen.getByText('Project')).toBeInTheDocument();
     expect(screen.getByText('Query')).toBeInTheDocument();
@@ -74,9 +72,7 @@ describe('SavedQueriesTable', () => {
   });
 
   it('should request for owned queries', async () => {
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     await waitFor(() =>
       expect(getQueriesMock).toHaveBeenCalledWith(
         `/organizations/${organization.slug}/explore/saved/`,
@@ -92,9 +88,7 @@ describe('SavedQueriesTable', () => {
   });
 
   it('should request for shared queries', async () => {
-    render(<SavedQueriesTable mode="shared" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="shared" title="title" />);
     await waitFor(() =>
       expect(getQueriesMock).toHaveBeenCalledWith(
         `/organizations/${organization.slug}/explore/saved/`,
@@ -110,9 +104,7 @@ describe('SavedQueriesTable', () => {
   });
 
   it('deletes a query', async () => {
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     renderGlobalModal();
     await screen.findByText('Query Name');
     await userEvent.click(screen.getByLabelText('More options'));
@@ -135,9 +127,7 @@ describe('SavedQueriesTable', () => {
   });
 
   it('should link to a single query view', async () => {
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     expect(await screen.findByText('Query Name')).toHaveAttribute(
       'href',
       '/organizations/org-slug/explore/traces/?environment=production&groupBy=&id=1&project=1&title=Query%20Name'
@@ -170,9 +160,7 @@ describe('SavedQueriesTable', () => {
         },
       ],
     });
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     expect(await screen.findByText('Query Name')).toHaveAttribute(
       'href',
       '/organizations/org-slug/explore/traces/compare/?environment=production&id=1&project=1&queries=%7B%22groupBys%22%3A%5B%5D%2C%22yAxes%22%3A%5B%5D%2C%22caseInsensitive%22%3A%221%22%7D&queries=%7B%22groupBys%22%3A%5B%5D%2C%22yAxes%22%3A%5B%5D%7D&title=Query%20Name'
@@ -208,9 +196,7 @@ describe('SavedQueriesTable', () => {
         },
       ],
     });
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     expect(await screen.findByText('Logs Query Name')).toHaveAttribute(
       'href',
       '/organizations/org-slug/explore/logs/?aggregateField=%7B%22groupBy%22%3A%22message%22%7D&caseInsensitive=1&environment=production&id=1&interval=5m&logsFields=timestamp&logsFields=message&logsFields=user.email&logsQuery=message%3A%22System%20time%20zone%20does%20not%20match%20user%20preferences%20time%20zone%22&logsSortBys=user.email&mode=samples&project=1&statsPeriod=1h&title=Logs%20Query%20Name'
@@ -245,9 +231,7 @@ describe('SavedQueriesTable', () => {
         },
       ],
     });
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     expect(await screen.findByText('ABC')).toHaveAttribute(
       'href',
       '/organizations/org-slug/explore/logs/?aggregateField=%7B%22groupBy%22%3A%22message%22%7D&aggregateField=%7B%22yAxes%22%3A%5B%22avg%28tags%5Bamount%2Cnumber%5D%29%22%5D%7D&environment=production&id=1&interval=5m&logsFields=timestamp&logsFields=tags%5Bamount%2Cnumber%5D&logsQuery=message%3Afoo&logsSortBys=user.email&mode=samples&project=1&statsPeriod=1h&title=ABC'
@@ -292,9 +276,7 @@ describe('SavedQueriesTable', () => {
         },
       ],
     });
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     await screen.findByText('Query Name');
     screen.getByText('Starred Query');
     expect(screen.getByLabelText('Unstar')).toBeInTheDocument();
@@ -326,9 +308,7 @@ describe('SavedQueriesTable', () => {
   });
 
   it('should sort by most popular', async () => {
-    render(<SavedQueriesTable mode="owned" sort="mostPopular" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" sort="mostPopular" title="title" />);
     await screen.findByText('Query Name');
     expect(getQueriesMock).toHaveBeenCalledWith(
       `/organizations/${organization.slug}/explore/saved/`,
@@ -339,9 +319,7 @@ describe('SavedQueriesTable', () => {
   });
 
   it('should search for a query', async () => {
-    render(<SavedQueriesTable mode="owned" searchQuery="Query Name" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" searchQuery="Query Name" title="title" />);
     await screen.findByText('Query Name');
     expect(getQueriesMock).toHaveBeenCalledWith(
       `/organizations/${organization.slug}/explore/saved/`,
@@ -352,9 +330,7 @@ describe('SavedQueriesTable', () => {
   });
 
   it('should duplicate a query', async () => {
-    render(<SavedQueriesTable mode="owned" title="title" />, {
-      deprecatedRouterMocks: true,
-    });
+    render(<SavedQueriesTable mode="owned" title="title" />);
     await screen.findByText('Query Name');
     await userEvent.click(screen.getByLabelText('More options'));
     await userEvent.click(screen.getByText('Duplicate'));
