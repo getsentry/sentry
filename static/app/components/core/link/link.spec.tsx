5b7effcd0adb66cc1bab0d6ddb9fdf67021ec591
nsdeschenes <nicholas.deschenes@sentry.io>
2025-12-29 15:51:05 -0400

    :white_check_mark: Update tests

--- static/app/components/core/link/link.spec.tsx
+++ static/app/components/core/link/link.spec.tsx
@@ -4,16 +4,34 @@ import {ExternalLink, Link} from 'sentry/components/core/link';
 
 describe('Link', () => {
   // Note: Links should not support a disabled option, as disabled links are just text elements
-  it('disabled links render as <a> with no href', () => {
-    render(
-      // eslint-disable-next-line no-restricted-syntax
-      <Link disabled to="https://www.sentry.io/">
-        Link
-      </Link>
-    );
 
-    expect(screen.getByText('Link')).toBeEnabled();
-    expect(screen.getByText('Link')).not.toHaveAttribute('href');
+  describe('disabled links', () => {
+    it('renders links with string to prop render as <a> with no href', () => {
+      render(
+        // eslint-disable-next-line no-restricted-syntax
+        <Link disabled to="https://www.sentry.io/">
+          Link
+        </Link>
+      );
+
+      expect(screen.getByText('Link')).toBeEnabled();
+      expect(screen.getByText('Link')).not.toHaveAttribute('href');
+      expect(screen.getByText('Link')).toHaveAttribute('aria-disabled', 'true');
+      expect(screen.getByText('Link')).toHaveAttribute('role', 'link');
+    });
+
+    it('renders links with LocationDescriptor to prop render as <a> with no href', () => {
+      render(
+        <Link disabled to={{pathname: '/settings/account/'}}>
+          Link
+        </Link>
+      );
+
+      expect(screen.getByText('Link')).toBeEnabled();
+      expect(screen.getByText('Link')).not.toHaveAttribute('href');
+      expect(screen.getByText('Link')).toHaveAttribute('aria-disabled', 'true');
+      expect(screen.getByText('Link')).toHaveAttribute('role', 'link');
+    });
   });
 
   it('links render as <a> with href', () => {
