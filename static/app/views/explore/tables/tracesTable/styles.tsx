028bc23fc8bf741749330f460f05e050311f9f16
nsdeschenes <nicholas.deschenes@sentry.io>
2025-12-29 11:22:05 -0400

    :lipstick: Rework StyledPanelHeader to use scraps components

--- static/app/views/explore/tables/tracesTable/styles.tsx
+++ static/app/views/explore/tables/tracesTable/styles.tsx
@@ -1,20 +1,44 @@
-import type {CSSProperties} from 'react';
+import type {ComponentProps, CSSProperties} from 'react';
 import {css} from '@emotion/react';
 import styled from '@emotion/styled';
+
+import {Flex} from '@sentry/scraps/layout';
 
 import Panel from 'sentry/components/panels/panel';
 import PanelHeader from 'sentry/components/panels/panelHeader';
 import PanelItem from 'sentry/components/panels/panelItem';
-import {space} from 'sentry/styles/space';
 
 export const StyledPanel = styled(Panel)`
   margin-bottom: 0px;
 `;
 
-export const StyledPanelHeader = styled(PanelHeader)<{align: 'left' | 'right'}>`
+const StyledFlex = styled(Flex)`
   white-space: nowrap;
-  justify-content: ${p => (p.align === 'left' ? 'flex-start' : 'flex-end')};
 `;
+interface StyledPanelHeaderProps extends ComponentProps<typeof PanelHeader> {
+  align: 'left' | 'right';
+  children: React.ReactNode;
+  radius?: ComponentProps<typeof Flex>['radius'];
+}
+
+export function StyledPanelHeader({
+  children,
+  align,
+  radius,
+  ...props
+}: StyledPanelHeaderProps) {
+  const justify = align === 'left' ? 'start' : 'end';
+
+  return (
+    <StyledFlex justify={justify} radius={radius ? radius : '0'}>
+      {flexProps => (
+        <PanelHeader lightText {...flexProps} {...props}>
+          {children}
+        </PanelHeader>
+      )}
+    </StyledFlex>
+  );
+}
 
 export const TracePanelContent = styled('div')`
   width: 100%;
@@ -28,7 +52,7 @@ export const StyledPanelItem = styled(PanelItem)<{
   span?: number;
 }>`
   align-items: center;
-  padding: ${space(1)} ${space(2)};
+  padding: ${p => p.theme.space.md} ${p => p.theme.space.xl};
   ${p => (p.align === 'left' ? 'justify-content: flex-start;' : null)}
   ${p => (p.align === 'right' ? 'justify-content: flex-end;' : null)}
   ${p => (p.overflow ? p.theme.overflowEllipsis : null)};
@@ -94,7 +118,7 @@ export const EmptyStateText = styled('div')<{
 }>`
   color: ${p => p.theme.subText};
   font-size: ${p => p.theme.fontSize[p.size]};
-  padding-bottom: ${space(1)};
+  padding-bottom: ${p => p.theme.space.md};
   ${p => p.textAlign && `text-align: ${p.textAlign}`};
 `;
 
