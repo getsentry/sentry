4dd5ff28c4a0699cb074e4f7b49db1c25618951d
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2025-12-30 15:24:30 +0100

    use DocsParams directly

--- static/app/gettingStartedDocs/unreal/onboarding.tsx
+++ static/app/gettingStartedDocs/unreal/onboarding.tsx
@@ -9,8 +9,6 @@ import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {getConsoleExtensions} from 'sentry/components/onboarding/gettingStartedDoc/utils/consoleExtensions';
 import {t, tct} from 'sentry/locale';
 
-type Params = DocsParams;
-
 const getVerifySnippet = () => `
 #include "SentrySubsystem.h"
 
@@ -24,7 +22,7 @@ void Verify()
     SentrySubsystem->CaptureMessage(TEXT("Capture message"));
 }`;
 
-const getSettingsConfigureSnippet = (params: Params) => `
+const getSettingsConfigureSnippet = (params: DocsParams) => `
 #include "SentrySubsystem.h"
 
 FConfigureSettingsDelegate OnConfigureSettings;
@@ -62,7 +60,7 @@ void UMyGameInstance::ConfigureSentrySettings(USentrySettings* Settings)
 USentrySubsystem* SentrySubsystem = GEngine->GetEngineSubsystem<USentrySubsystem>();
 SentrySubsystem->InitializeWithSettings(OnConfigureSettings);`;
 
-const getCrashReporterConfigSnippet = (params: Params) => `
+const getCrashReporterConfigSnippet = (params: DocsParams) => `
 [CrashReportClient]
 CrashReportClientVersion=1.0
 DataRouterUrl="${params.dsn.unreal}"`;
