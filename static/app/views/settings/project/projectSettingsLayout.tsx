41d380102a1c672873c88212bd28fd48430720f2
Shashank Jarmale <shashank.jarmale@sentry.io>
2025-12-30 10:11:07 -0500

    ref(settings): Remove usages of `deprecatedRouteProps` for `SettingsLayout` routes (#105517)
    Migrates the `AccountSettingsLayout`, `OrganizationSettingsLayout`, and
    `AdminLayout` off of `deprecatedRouteProps`. `ProjectSettingsLayout` was
    already migrated off of the deprecated router in
    https://github.com/getsentry/sentry/pull/102709, so this PR also updates
    the props passed there. Modifies the `SubscriptionSettingsLayout` to use
    `SettingsLayout` directly rather than through
    `OrganizationSettingsLayout` as an intermediary.
    
    
    https://www.notion.so/sentry/Frontend-TSC-Project-Remove-all-uses-of-deprecatedRouteProps-true-26a8b10e4b5d8015a6a2dd14f9d41dd7

--- static/app/views/settings/project/projectSettingsLayout.tsx
+++ static/app/views/settings/project/projectSettingsLayout.tsx
@@ -1,19 +1,10 @@
 import {Outlet, useOutletContext} from 'react-router-dom';
 
-import type {PlainRoute} from 'sentry/types/legacyReactRouter';
 import type {Project} from 'sentry/types/project';
 import useRouteAnalyticsParams from 'sentry/utils/routeAnalytics/useRouteAnalyticsParams';
-import {useLocation} from 'sentry/utils/useLocation';
 import {useParams} from 'sentry/utils/useParams';
-import {useRoutes} from 'sentry/utils/useRoutes';
 import ProjectContext from 'sentry/views/projects/projectContext';
 import SettingsLayout from 'sentry/views/settings/components/settingsLayout';
-
-type InnerProps = {
-  params: {projectId: string};
-  project: Project;
-  routes: PlainRoute[];
-};
 
 type ProjectSettingsOutletContext = {
   project: Project;
@@ -27,9 +18,7 @@ export function useProjectSettingsOutlet() {
   return useOutletContext<ProjectSettingsOutletContext>();
 }
 
-function InnerProjectSettingsLayout({params, routes, project}: InnerProps) {
-  const location = useLocation();
-
+function InnerProjectSettingsLayout({project}: {project: Project}) {
   // set analytics params for route based analytics
   useRouteAnalyticsParams({
     project_id: project.id,
@@ -37,14 +26,7 @@ function InnerProjectSettingsLayout({params, routes, project}: InnerProps) {
   });
 
   return (
-    <SettingsLayout
-      params={params}
-      routes={routes}
-      location={location}
-      router={undefined as any}
-      route={undefined as any}
-      routeParams={undefined as any}
-    >
+    <SettingsLayout>
       <ProjectSettingsOutlet project={project} />
     </SettingsLayout>
   );
@@ -52,13 +34,10 @@ function InnerProjectSettingsLayout({params, routes, project}: InnerProps) {
 
 export default function ProjectSettingsLayout() {
   const params = useParams<{projectId: string}>();
-  const routes = useRoutes();
 
   return (
     <ProjectContext projectSlug={params.projectId}>
-      {({project}) => (
-        <InnerProjectSettingsLayout params={params} routes={routes} project={project} />
-      )}
+      {({project}) => <InnerProjectSettingsLayout project={project} />}
     </ProjectContext>
   );
 }
