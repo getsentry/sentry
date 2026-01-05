81dfa60f11c7db002c68e7b982717d4656ee963a
Jesse Box <me@jessebox.net>
2026-01-05 10:54:57 +0100

    Migrated: transparent border color values to new tokens

--- static/app/components/timeline/index.tsx
+++ static/app/components/timeline/index.tsx
@@ -166,7 +166,7 @@ const Container = styled('div')`
     width: 1px;
     top: 0;
     bottom: 0;
-    background: ${p => p.theme.border};
+    background: ${p => p.theme.tokens.border.transparent.neutral.muted};
   }
 `;
 
