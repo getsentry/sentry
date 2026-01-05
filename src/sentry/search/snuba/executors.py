efb286f9821b9193dd7708b6cece8ad4788f5562
Shashank Jarmale <shashank.jarmale@sentry.io>
2026-01-05 12:48:55 -0800

    Use a truncation enablement option

--- src/sentry/search/snuba/executors.py
+++ src/sentry/search/snuba/executors.py
@@ -961,7 +961,10 @@ class PostgresSnubaQueryExecutor(AbstractQueryExecutor):
             # If we had too_many_candidates, fall back to truncation instead of
             # returning empty. This handles selective filters (like assigned_to)
             # where random sampling is unlikely to find matches.
-            if too_many_candidates and original_group_ids:
+            is_truncation_enabled = options.get(
+                "snuba.search.truncate-group-ids-for-selective-filters-enabled"
+            )
+            if too_many_candidates and original_group_ids and is_truncation_enabled:
                 metrics.incr("snuba.search.hits_zero_fallback_to_truncation", skip_internal=False)
                 group_ids = original_group_ids[:max_candidates]
                 too_many_candidates = False
