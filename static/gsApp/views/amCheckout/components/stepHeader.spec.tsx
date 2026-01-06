c08d7ef6ba24898eeb4a906796cecaba40acc780
Isabella Enriquez <isabella.enriquez@sentry.io>
2026-01-06 16:32:53 -0500

    fix(checkout): Scroll to step based on hash (#105637)
    Fixes missing scrolling behavior for steps based on URL hash.
    
    
    
    https://github.com/user-attachments/assets/4744155a-9a00-4fb2-bb44-c1154757021a

--- static/gsApp/views/amCheckout/components/stepHeader.spec.tsx
+++ static/gsApp/views/amCheckout/components/stepHeader.spec.tsx
@@ -4,13 +4,11 @@ import StepHeader from 'getsentry/views/amCheckout/components/stepHeader';
 
 describe('StepHeader', () => {
   const mockTitle = 'Mock Title';
-  const stepNumber = 1;
 
-  it('renders for new checkout', () => {
-    render(<StepHeader title={mockTitle} stepNumber={stepNumber} />);
+  it('renders for checkout', () => {
+    render(<StepHeader title={mockTitle} />);
 
     expect(screen.getByText(mockTitle)).toBeInTheDocument();
-    expect(screen.queryByText(`${stepNumber}.`)).not.toBeInTheDocument();
     expect(screen.queryByTestId('icon-check-mark')).not.toBeInTheDocument();
     expect(screen.queryByText('Edit')).not.toBeInTheDocument();
   });
