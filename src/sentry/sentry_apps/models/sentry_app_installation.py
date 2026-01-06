153dfbb3f4d6d10bff97d891d3df2fcfef613647
Cathy Teng <cathy.teng@sentry.io>
2026-01-06 10:26:12 -0800

    Revert "oops"
    This reverts commit 04939cb6abe1b8e17ba47a1e4c025ea3995ac8a8.

--- src/sentry/sentry_apps/models/sentry_app_installation.py
+++ src/sentry/sentry_apps/models/sentry_app_installation.py
@@ -13,12 +13,7 @@ from sentry.api.exceptions import BadRequest
 from sentry.auth.services.auth import AuthenticatedToken
 from sentry.backup.scopes import RelocationScope
 from sentry.constants import SentryAppInstallationStatus
-from sentry.db.models import (
-    BoundedPositiveIntegerField,
-    FlexibleForeignKey,
-    Model,
-    control_silo_model,
-)
+from sentry.db.models import BoundedPositiveIntegerField, FlexibleForeignKey, control_silo_model
 from sentry.db.models.fields.hybrid_cloud_foreign_key import HybridCloudForeignKey
 from sentry.db.models.manager.base_query_set import BaseQuerySet
 from sentry.db.models.paranoia import ParanoidManager, ParanoidModel
@@ -140,7 +135,7 @@ class SentryAppInstallation(ReplicatedControlModel, ParanoidModel):
                 for update_outbox in self.outboxes_for_update():
                     update_outbox.save()  # mimics ControlOutboxProducingModel
 
-                return super(Model, self).delete(*args, **kwargs)
+                return models.Model.delete(self, *args, **kwargs)
 
             return super().delete(*args, **kwargs)
 
