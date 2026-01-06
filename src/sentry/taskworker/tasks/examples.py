91a5080a9932cc81159d2d91c2d4e895ed8c5df1
james-mcnulty <george.berdovskiy@sentry.io>
2026-01-06 13:23:04 -0800

    Randomize Simple Task Time to Match Ingest Transactions Save Event

--- src/sentry/taskworker/tasks/examples.py
+++ src/sentry/taskworker/tasks/examples.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
 
 import logging
+import random
+import time
 from time import sleep
 from typing import Any
 
@@ -51,7 +53,16 @@ def will_retry(failure: str) -> None:
 
 @exampletasks.register(name="examples.simple_task")
 def simple_task(*args: list[Any], **kwargs: dict[str, Any]) -> None:
-    sleep(10)
+    logger.info("Starting simple task...")
+    mu = 0.7
+    sigma = 0.8
+
+    sleep_duration = random.lognormvariate(mu, sigma)
+
+    final_sleep_time = max(0.20, min(sleep_duration, 20))
+
+    time.sleep(final_sleep_time)
+
     logger.info("Simple task complete!")
 
 
@@ -92,4 +103,5 @@ def timed_task(sleep_seconds: float | str, *args: list[Any], **kwargs: dict[str,
 # @exampletasks.register(name="examples.simple_task", compression_type=CompressionType.ZSTD)
 # def simple_task_compressed(*args: list[Any], **kwargs: dict[str, Any]) -> None:
 #     sleep(0.1)
+#     logger.debug("simple_task_compressed complete")
 #     logger.debug("simple_task_compressed complete")
