20509fafceaa013bda27060689c9c7631ba0aff1
Michelle Fu <83109586+mifu67@users.noreply.github.com>
2026-01-06 15:16:37 -0800

    chore(member merge): backfill emails for self-hosted sentry app proxy users (#101887)
    Dupe of https://github.com/getsentry/getsentry/pull/17808 for
    self-hosted sentry.

---
+++ tests/sentry/migrations/test_1015_backfill_self_hosted_sentry_app_emails.py
@@ -0,0 +1,27 @@
+from django.test import override_settings
+
+from sentry.testutils.cases import TestMigrations
+from sentry.testutils.silo import control_silo_test
+
+
+@control_silo_test
+@override_settings(SENTRY_SELF_HOSTED=True)
+class BackfillSelfHostedSentryAppEmailsTest(TestMigrations):
+    migrate_from = "1014_add_pkce_to_apigrant"
+    migrate_to = "1015_backfill_self_hosted_sentry_app_emails"
+    connection = "control"
+
+    def setup_before_migration(self, apps):
+        self.placeholder_domain = "proxy-user.sentry.io"
+        self.proxy_user = self.create_user(
+            username="sentry-app-randomuuid123", email="", is_sentry_app=True
+        )
+        self.regular_user = self.create_user(username="regular@user", email="regular@user")
+
+        return super().setup_before_migration(apps)
+
+    def test(self) -> None:
+        self.proxy_user.refresh_from_db()
+        self.regular_user.refresh_from_db()
+        assert self.proxy_user.email == f"{self.proxy_user.username}@{self.placeholder_domain}"
+        assert self.regular_user.email == "regular@user"
