a3e3b812f7d0b7c63d703656650b549bf2552122
Nico Hinderling <NicoHinderling@users.noreply.github.com>
2026-01-06 13:16:26 -0800

    small improvement

--- src/sentry/deletions/defaults/preprod_artifact.py
+++ src/sentry/deletions/defaults/preprod_artifact.py
@@ -6,10 +6,9 @@ from sentry.preprod.models import PreprodArtifact
 
 class PreprodArtifactDeletionTask(ModelDeletionTask[PreprodArtifact]):
     def delete_instance_bulk(self, instance_list: Sequence[PreprodArtifact]) -> None:
-        from sentry.preprod.helpers.deletion import bulk_delete_artifacts_and_related_data
+        from sentry.preprod.helpers.deletion import bulk_delete_artifacts
 
-        artifact_ids = [instance.id for instance in instance_list]
-        bulk_delete_artifacts_and_related_data(artifact_ids)
+        bulk_delete_artifacts(list(instance_list))
 
     def get_child_relations(self, instance: PreprodArtifact) -> list[BaseRelation]:
         return []
