20b7722274af2d3e87ec5b385e57a0f6e89a72da
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2026-01-07 10:44:57 -0500

    feat(eap-resolver): support between operator (#105538)
    Add support for inclusive `between` operator in `count_if`. 
    
    This can eventually be expanded to other functions, but for now we need
    this to create percentage http response status codes in dashboards via
    formulas.
    i.e `count_if(status_code,between,300,399) / count(span.duration)`

--- src/sentry/search/eap/spans/aggregates.py
+++ src/sentry/search/eap/spans/aggregates.py
@@ -36,6 +36,43 @@ def count_processor(count_value: int | None) -> int:
         return count_value
 
 
+def resolve_attribute_value(
+    attribute: AttributeKey, value: str, invalid_parameter_message: str | None = None
+) -> AttributeValue:
+    attr_value = None
+    if invalid_parameter_message is None:
+        invalid_parameter_message = f"Invalid parameter '{value}'"
+
+    try:
+        if attribute.type == AttributeKey.TYPE_DOUBLE:
+            attr_value = AttributeValue(val_double=float(value))
+        elif attribute.type == AttributeKey.TYPE_FLOAT:
+            attr_value = AttributeValue(val_float=float(value))
+        elif attribute.type == AttributeKey.TYPE_INT:
+            attr_value = AttributeValue(val_int=int(value))
+        else:
+            value = unquote_literal(value)
+            attr_value = AttributeValue(val_str=value)
+
+    except ValueError:
+        expected_type = "string"
+        if attribute.type in [AttributeKey.TYPE_FLOAT, AttributeKey.TYPE_DOUBLE]:
+            expected_type = "number"
+        if attribute.type == AttributeKey.TYPE_INT:
+            expected_type = "integer"
+        raise InvalidSearchQuery(f"{invalid_parameter_message}. Must be of type {expected_type}.")
+
+    if attribute.type == AttributeKey.TYPE_BOOLEAN:
+        lower_value = value.lower()
+        if lower_value not in ["true", "false"]:
+            raise InvalidSearchQuery(
+                f"{invalid_parameter_message}. Must be one of {["true", "false"]}"
+            )
+        attr_value = AttributeValue(val_bool=value == "true")
+
+    return attr_value
+
+
 SPANS_ALWAYS_PRESENT_ATTRIBUTES = [
     AttributeKey(name="sentry.duration_ms", type=AttributeKey.Type.TYPE_DOUBLE),
 ]
@@ -67,39 +104,55 @@ def resolve_key_eq_value_filter(args: ResolvedArguments) -> tuple[AttributeKey, 
         value, str
     ), "Value must be a String"  # This should always be a string. Assertion to deal with typing errors.
 
-    try:
-        if key.type == AttributeKey.TYPE_DOUBLE:
-            attr_value = AttributeValue(val_double=float(value))
-        elif key.type == AttributeKey.TYPE_FLOAT:
-            attr_value = AttributeValue(val_float=float(value))
-        elif key.type == AttributeKey.TYPE_INT:
-            attr_value = AttributeValue(val_int=int(value))
-        else:
-            value = unquote_literal(value)
-            attr_value = AttributeValue(val_str=value)
-    except ValueError:
-        expected_type = "string"
-        if key.type in [AttributeKey.TYPE_FLOAT, AttributeKey.TYPE_DOUBLE]:
-            expected_type = "number"
-        if key.type == AttributeKey.TYPE_INT:
-            expected_type = "integer"
-        raise InvalidSearchQuery(f"Invalid parameter '{value}'. Must be of type {expected_type}.")
+    attr_value = resolve_attribute_value(key, value, f"Invalid third parameter {value}")
 
-    if key.type == AttributeKey.TYPE_BOOLEAN:
-        lower_value = value.lower()
-        if lower_value not in ["true", "false"]:
-            raise InvalidSearchQuery(
-                f"Invalid parameter {value}. Must be one of {["true", "false"]}"
+    if operator == "between":
+        value2 = args[4]
+
+        assert isinstance(value2, str), "Third parameter must be a String"
+
+        # TODO: A bit of a hack here, the default arg is set to an empty string so it's not treated as a required argument.
+        # We check against the default arg to determine if the second value is missing.
+        if value2 == "":
+            raise InvalidSearchQuery("between operator requires two values")
+
+        try:
+            if float(value2) <= float(value):
+                raise InvalidSearchQuery(
+                    f"Fourth parameter {value2} must be greater than third parameter {value}"
+                )
+        except ValueError:
+            raise InvalidSearchQuery("between operator requires two numbers")
+
+        attr_value2 = resolve_attribute_value(key, value2, f"Invalid fourth parameter {value2}")
+        trace_filter = TraceItemFilter(
+            and_filter=AndFilter(
+                filters=[
+                    TraceItemFilter(
+                        comparison_filter=ComparisonFilter(
+                            key=key,
+                            op=ComparisonFilter.OP_GREATER_THAN_OR_EQUALS,
+                            value=attr_value,
+                        )
+                    ),
+                    TraceItemFilter(
+                        comparison_filter=ComparisonFilter(
+                            key=key,
+                            op=ComparisonFilter.OP_LESS_THAN_OR_EQUALS,
+                            value=attr_value2,
+                        )
+                    ),
+                ]
             )
-        attr_value = AttributeValue(val_bool=value == "true")
-
-    trace_filter = TraceItemFilter(
-        comparison_filter=ComparisonFilter(
-            key=key,
-            op=constants.LITERAL_OPERATOR_MAP[operator],
-            value=attr_value,
         )
-    )
+    else:
+        trace_filter = TraceItemFilter(
+            comparison_filter=ComparisonFilter(
+                key=key,
+                op=constants.LITERAL_OPERATOR_MAP[operator],
+                value=attr_value,
+            )
+        )
     return (aggregate_key, trace_filter)
 
 
@@ -228,10 +281,14 @@ SPAN_AGGREGATE_DEFINITIONS = {
                         "greaterOrEquals",
                         "less",
                         "greater",
+                        "between",
                     ]
                 ),
             ),
             ValueArgumentDefinition(argument_types={"string"}),
+            ValueArgumentDefinition(
+                argument_types={"string"}, default_arg="", validator=number_validator
+            ),  # Second value is only for between, so it must be a number
         ],
         aggregate_resolver=resolve_key_eq_value_filter,
     ),
