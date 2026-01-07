7f4d25a0da6d420fc5c3322b0b6682fae88d8e3a
Scott Cooper <scttcper@gmail.com>
2026-01-07 15:21:52 -0800

    more cleanup

--- static/app/components/group/externalIssuesList/hooks/useSentryAppExternalIssues.tsx
+++ static/app/components/group/externalIssuesList/hooks/useSentryAppExternalIssues.tsx
@@ -96,7 +96,6 @@ export function useSentryAppExternalIssues({
                 event,
                 sentryAppComponent: component,
                 sentryAppInstallation: installation,
-                externalIssue,
               });
             },
           },
