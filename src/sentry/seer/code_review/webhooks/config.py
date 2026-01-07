17f28a27b94d85ef1b56b0de5dffbae8d092c43d
Armen Zambrano G. <44410+armenzg@users.noreply.github.com>
2026-01-07 12:27:19 -0500

    deprecate(code_review): Do not forward events to Overwatch (#105749)
    `pull_request_review` and `pull_request_review_comment` have stopped
    working a while ago. This change stops the forwarding to Overwatch.

--- src/sentry/seer/code_review/webhooks/config.py
+++ src/sentry/seer/code_review/webhooks/config.py
@@ -4,6 +4,4 @@ from sentry.integrations.github.webhook_types import GithubWebhookType
 WEBHOOK_TYPE_TO_OPTION_KEY = {
     GithubWebhookType.ISSUE_COMMENT: "github.webhook.issue-comment",
     GithubWebhookType.PULL_REQUEST: "github.webhook.pr",
-    GithubWebhookType.PULL_REQUEST_REVIEW_COMMENT: "github.webhook.pr-review-comment",
-    GithubWebhookType.PULL_REQUEST_REVIEW: "github.webhook.pr-review",
 }
