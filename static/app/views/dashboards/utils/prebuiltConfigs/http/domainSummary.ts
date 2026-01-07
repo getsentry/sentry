1299c9dd220a4bf46025a32f6180f63717e130e6
Dominik Buszowiecki <DominikB2014@gmail.com>
2026-01-07 13:04:56 -0500

    add http response rate percentage

--- static/app/views/dashboards/utils/prebuiltConfigs/http/domainSummary.ts
+++ static/app/views/dashboards/utils/prebuiltConfigs/http/domainSummary.ts
@@ -4,6 +4,11 @@ import {MutableSearch} from 'sentry/utils/tokenizeSearch';
 import {DisplayType, WidgetType, type Widget} from 'sentry/views/dashboards/types';
 import type {PrebuiltDashboard} from 'sentry/views/dashboards/utils/prebuiltConfigs';
 import {
+  PERCENTAGE_3XX,
+  PERCENTAGE_4XX,
+  PERCENTAGE_5XX,
+} from 'sentry/views/dashboards/utils/prebuiltConfigs/http/constants';
+import {
   AVERAGE_DURATION_TEXT,
   BASE_FILTERS,
   DASHBOARD_TITLE,
@@ -94,7 +99,7 @@ const BIG_NUMBER_ROW_WIDGETS: Widget[] = spaceWidgetsEquallyOnRow(
       ],
     },
     {
-      id: '3xx-count-big-number',
+      id: '3xx-percentage-big-number',
       title: t('3XX'),
       displayType: DisplayType.BIG_NUMBER,
       widgetType: WidgetType.SPANS,
@@ -102,16 +107,16 @@ const BIG_NUMBER_ROW_WIDGETS: Widget[] = spaceWidgetsEquallyOnRow(
       queries: [
         {
           name: t('3XX'),
-          conditions: `${FILTER_STRING} tags[http.response.status_code,number]:>=300 tags[http.response.status_code,number]:<=399`,
-          fields: [`count(${SpanFields.SPAN_DURATION})`],
-          aggregates: [`count(${SpanFields.SPAN_DURATION})`],
+          conditions: `${FILTER_STRING}`,
+          fields: [PERCENTAGE_3XX],
+          aggregates: [PERCENTAGE_3XX],
           columns: [],
-          orderby: `count(${SpanFields.SPAN_DURATION})`,
+          orderby: PERCENTAGE_3XX,
         },
       ],
     },
     {
-      id: '4xx-count-big-number',
+      id: '4xx-percentage-big-number',
       title: t('4XX'),
       displayType: DisplayType.BIG_NUMBER,
       widgetType: WidgetType.SPANS,
@@ -119,16 +124,16 @@ const BIG_NUMBER_ROW_WIDGETS: Widget[] = spaceWidgetsEquallyOnRow(
       queries: [
         {
           name: t('4XX'),
-          conditions: `${FILTER_STRING} tags[http.response.status_code,number]:>=400 tags[http.response.status_code,number]:<=499`,
-          fields: [`count(${SpanFields.SPAN_DURATION})`],
-          aggregates: [`count(${SpanFields.SPAN_DURATION})`],
+          conditions: `${FILTER_STRING}`,
+          fields: [PERCENTAGE_4XX],
+          aggregates: [PERCENTAGE_4XX],
           columns: [],
-          orderby: `count(${SpanFields.SPAN_DURATION})`,
+          orderby: PERCENTAGE_4XX,
         },
       ],
     },
     {
-      id: '5xx-count-big-number',
+      id: '5xx-percentage-big-number',
       title: t('5XX'),
       displayType: DisplayType.BIG_NUMBER,
       widgetType: WidgetType.SPANS,
@@ -136,11 +141,11 @@ const BIG_NUMBER_ROW_WIDGETS: Widget[] = spaceWidgetsEquallyOnRow(
       queries: [
         {
           name: t('5XX'),
-          conditions: `${FILTER_STRING} tags[http.response.status_code,number]:>=500 tags[http.response.status_code,number]:<=599`,
-          fields: [`count(${SpanFields.SPAN_DURATION})`],
-          aggregates: [`count(${SpanFields.SPAN_DURATION})`],
+          conditions: `${FILTER_STRING}`,
+          fields: [PERCENTAGE_5XX],
+          aggregates: [PERCENTAGE_5XX],
           columns: [],
-          orderby: `count(${SpanFields.SPAN_DURATION})`,
+          orderby: PERCENTAGE_5XX,
         },
       ],
     },
@@ -211,27 +216,27 @@ const CHART_ROW_WIDGETS: Widget[] = spaceWidgetsEquallyOnRow(
       queries: [
         {
           name: '3XX',
-          conditions: `${FILTER_STRING} tags[http.response.status_code,number]:>=300 tags[http.response.status_code,number]:<=399`,
-          fields: ['count(span.duration)'],
-          aggregates: ['count(span.duration)'],
+          conditions: FILTER_STRING,
+          fields: [PERCENTAGE_3XX],
+          aggregates: [PERCENTAGE_3XX],
           columns: [],
-          orderby: 'count(span.duration)',
+          orderby: PERCENTAGE_3XX,
         },
         {
           name: '4XX',
-          conditions: `${FILTER_STRING} tags[http.response.status_code,number]:>=400 tags[http.response.status_code,number]:<=499`,
-          fields: ['count(span.duration)'],
-          aggregates: ['count(span.duration)'],
+          conditions: FILTER_STRING,
+          fields: [PERCENTAGE_4XX],
+          aggregates: [PERCENTAGE_4XX],
           columns: [],
-          orderby: 'count(span.duration)',
+          orderby: PERCENTAGE_4XX,
         },
         {
           name: '5XX',
-          conditions: `${FILTER_STRING} tags[http.response.status_code,number]:>=500 tags[http.response.status_code,number]:<=599`,
-          fields: ['count(span.duration)'],
-          aggregates: ['count(span.duration)'],
+          conditions: FILTER_STRING,
+          fields: [PERCENTAGE_5XX],
+          aggregates: [PERCENTAGE_5XX],
           columns: [],
-          orderby: 'count(span.duration)',
+          orderby: PERCENTAGE_5XX,
         },
       ],
     },
@@ -249,6 +254,9 @@ const TRANSACTIONS_TABLE: Widget = {
     {
       aggregates: [
         'epm()',
+        PERCENTAGE_3XX,
+        PERCENTAGE_4XX,
+        PERCENTAGE_5XX,
         `avg(${SpanFields.SPAN_SELF_TIME})`,
         `sum(${SpanFields.SPAN_SELF_TIME})`,
       ],
@@ -256,12 +264,18 @@ const TRANSACTIONS_TABLE: Widget = {
       fields: [
         SpanFields.TRANSACTION,
         'epm()',
+        PERCENTAGE_3XX,
+        PERCENTAGE_4XX,
+        PERCENTAGE_5XX,
         `avg(${SpanFields.SPAN_SELF_TIME})`,
         `sum(${SpanFields.SPAN_SELF_TIME})`,
       ],
       fieldAliases: [
         t('Found In'),
         THROUGHPUT_TEXT,
+        t('3XXs'),
+        t('4XXs'),
+        t('5XXs'),
         DataTitles.avg,
         DataTitles.timeSpent,
       ],
