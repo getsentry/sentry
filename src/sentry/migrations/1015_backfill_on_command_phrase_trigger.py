9e64f0f0a727fea9cd0073d80d2df8564baf445b
srest2021 <sofia.rest@sentry.io>
2026-01-07 10:04:59 -0800

    make post deploy

--- src/sentry/migrations/1015_backfill_on_command_phrase_trigger.py
+++ src/sentry/migrations/1015_backfill_on_command_phrase_trigger.py
@@ -52,7 +52,7 @@ class Migration(CheckedMigration):
     #   is a schema change, it's completely safe to run the operation after the code has deployed.
     # Once deployed, run these manually via: https://develop.sentry.dev/database-migrations/#migration-deployment
 
-    is_post_deployment = False
+    is_post_deployment = True
 
     dependencies = [
         ("sentry", "1014_add_pkce_to_apigrant"),
