d713bda7d3dfab4792bb25013c9ea12a50157405
Dan Fuller <dfuller@sentry.io>
2026-01-06 16:44:02 -0800

    fix tests

--- tests/sentry/uptime/test_utils.py
+++ tests/sentry/uptime/test_utils.py
@@ -1,4 +1,4 @@
-from sentry.uptime.utils import build_backlog_key, build_backlog_task_flag_key
+from sentry.uptime.utils import build_backlog_key, build_backlog_task_scheduled_key
 
 
 class TestBuildBacklogKey:
@@ -9,5 +9,7 @@ class TestBuildBacklogKey:
 
 class TestBuildBacklogTaskFlagKey:
     def test_formats_correctly(self):
-        assert build_backlog_task_flag_key("123") == "uptime:backlog_task:123"
-        assert build_backlog_task_flag_key("abc-def") == "uptime:backlog_task:abc-def"
+        assert build_backlog_task_scheduled_key("123") == "uptime:backlog_task_scheduled:123"
+        assert (
+            build_backlog_task_scheduled_key("abc-def") == "uptime:backlog_task_scheduled:abc-def"
+        )
