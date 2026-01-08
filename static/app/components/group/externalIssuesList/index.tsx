2708d623047202ec6ca75657373d2a4db01c47f6
Scott Cooper <scttcper@gmail.com>
2026-01-08 09:34:17 -0800

    demo knip missing files

--- static/app/components/group/externalIssuesList/index.tsx
+++ static/app/components/group/externalIssuesList/index.tsx
@@ -1,27 +1,10 @@
-import type {ReactNode} from 'react';
-import {Fragment} from 'react';
-
-import {AlertLink} from 'sentry/components/core/alert/alertLink';
-import ErrorBoundary from 'sentry/components/errorBoundary';
-import ExternalIssueActions from 'sentry/components/group/externalIssuesList/externalIssueActions';
-import type {
-  ExternalIssueType,
-  IntegrationComponent,
-  PluginActionComponent,
-  PluginIssueComponent,
-  SentryAppIssueComponent,
-} from 'sentry/components/group/externalIssuesList/types';
 import useExternalIssueData from 'sentry/components/group/externalIssuesList/useExternalIssueData';
-import PluginActions from 'sentry/components/group/pluginActions';
-import SentryAppExternalIssueActions from 'sentry/components/group/sentryAppExternalIssueActions';
-import IssueSyncListElement from 'sentry/components/issueSyncListElement';
 import Placeholder from 'sentry/components/placeholder';
 import * as SidebarSection from 'sentry/components/sidebarSection';
 import {t} from 'sentry/locale';
 import type {Event} from 'sentry/types/event';
 import type {Group} from 'sentry/types/group';
 import type {Project} from 'sentry/types/project';
-import useOrganization from 'sentry/utils/useOrganization';
 
 type Props = {
   event: Event;
@@ -30,8 +13,7 @@ type Props = {
 };
 
 export default function ExternalIssueList({group, event, project}: Props) {
-  const organization = useOrganization();
-  const {isLoading, actions} = useExternalIssueData({
+  const {isLoading} = useExternalIssueData({
     group,
     event,
     project,
@@ -48,44 +30,10 @@ export default function ExternalIssueList({group, event, project}: Props) {
     );
   }
 
-  const renderers: Record<ExternalIssueType, (props: any) => ReactNode> = {
-    'sentry-app-issue': ({sentryApp, ...props}: SentryAppIssueComponent['props']) => (
-      <ErrorBoundary key={sentryApp.slug} mini>
-        <SentryAppExternalIssueActions {...props} />
-      </ErrorBoundary>
-    ),
-    'integration-issue': (props: IntegrationComponent['props']) => (
-      <ExternalIssueActions {...props} />
-    ),
-    'plugin-action': ({plugin}: PluginActionComponent['props']) => (
-      <IssueSyncListElement externalIssueLink={plugin[1]}>
-        {plugin[0]}
-      </IssueSyncListElement>
-    ),
-    'plugin-issue': (props: PluginIssueComponent['props']) => (
-      <PluginActions {...props} />
-    ),
-  };
-
   return (
     <SidebarSection.Wrap data-test-id="linked-issues">
       <SidebarSection.Title>{t('Issue Tracking')}</SidebarSection.Title>
-      <SidebarSection.Content>
-        {actions.length ? (
-          actions.map(({type, key, props}) => (
-            <Fragment key={key}>{renderers[type](props)}</Fragment>
-          ))
-        ) : (
-          <AlertLink.Container>
-            <AlertLink
-              variant="muted"
-              to={`/settings/${organization.slug}/integrations/?category=issue%20tracking`}
-            >
-              {t('Track this issue in Jira, GitHub, etc.')}
-            </AlertLink>
-          </AlertLink.Container>
-        )}
-      </SidebarSection.Content>
+      <SidebarSection.Content>empty</SidebarSection.Content>
     </SidebarSection.Wrap>
   );
 }
