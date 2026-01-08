751b1757a963ccc6b8442ec00bfa0d891f7c3970
Mark Story <mark@mark-story.com>
2026-01-08 15:16:03 -0500

    Protect against thread races
    There is a possibility of multiple threads attempting to reconnect to
    memcache concurrently. Use a lock to avoid races.

--- src/sentry/cache/backends/reconnectingmemcache.py
+++ src/sentry/cache/backends/reconnectingmemcache.py
@@ -1,5 +1,6 @@
 import time
 from collections.abc import Mapping
+from threading import Lock
 
 import pymemcache
 from django.core.cache.backends.memcached import PyMemcacheCache
@@ -23,6 +24,7 @@ class ReconnectingMemcache(PyMemcacheCache):
         self._reconnect_age: int = params.get("OPTIONS", {}).pop("reconnect_age", 300)
         self._last_reconnect_at: float = time.time()
         self._backend: pymemcache.Client | None = None
+        self._backend_lock = Lock()
 
         super().__init__(server, params)
 
@@ -31,15 +33,20 @@ class ReconnectingMemcache(PyMemcacheCache):
         """
         Overload PyMemcacheCache._cache with periodic reconnections.
         """
+        reconnect = False
         age = time.time() - self._last_reconnect_at
-        if age >= self._reconnect_age and self._backend:
-            # Close the underlying cache connection if we haven't done that recently.
-            metrics.incr("cache.memcache.reconnect")
-            self._backend.close()
-            self._backend = None
+        if not self._backend or age >= self._reconnect_age:
+            reconnect = True
 
-        if not self._backend:
+        if reconnect and self._backend_lock.acquire(timeout=1.0):
+            # Close the underlying cache connection if we haven't done that recently.
+            if self._backend:
+                self._backend.close()
+                self._backend = None
+
+            metrics.incr("cache.memcache.reconnect")
             self._backend = self._class(self.client_servers, **self._options)
             self._last_reconnect_at = time.time()
+            self._backend_lock.release()
 
         return self._backend
