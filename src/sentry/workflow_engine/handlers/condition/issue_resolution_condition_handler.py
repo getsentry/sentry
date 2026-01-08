6d121186cf6ff9a0bbc4f6550e53db745b726b17
Malachi Willey <malwilley@gmail.com>
2026-01-08 09:33:33 -0800

    Add new condition for resolution trigger

--- src/sentry/workflow_engine/handlers/condition/issue_resolution_condition_handler.py
+++ src/sentry/workflow_engine/handlers/condition/issue_resolution_condition_handler.py
@@ -1,6 +1,5 @@
 from typing import Any
 
-from sentry.models.group import GroupStatus
 from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.registry import condition_handler_registry
 from sentry.workflow_engine.types import DataConditionHandler, WorkflowEventData
@@ -10,12 +9,7 @@ from sentry.workflow_engine.types import DataConditionHandler, WorkflowEventData
 class IssueResolutionConditionHandler(DataConditionHandler[WorkflowEventData]):
     group = DataConditionHandler.Group.WORKFLOW_TRIGGER
 
-    comparison_json_schema = {
-        "type": "boolean",
-    }
-
     @staticmethod
     def evaluate_value(event_data: WorkflowEventData, comparison: Any) -> bool:
         group = event_data.group
-        is_resolved = group.status == GroupStatus.RESOLVED
-        return is_resolved == comparison
+        return group.status == comparison
--- tests/sentry/workflow_engine/handlers/condition/test_issue_resolution_condition_handler.py
+++ tests/sentry/workflow_engine/handlers/condition/test_issue_resolution_condition_handler.py
@@ -1,7 +1,3 @@
-import pytest
-from jsonschema import ValidationError
-
-from sentry.models.group import GroupStatus
 from sentry.workflow_engine.models.data_condition import Condition
 from sentry.workflow_engine.types import WorkflowEventData
 from tests.sentry.workflow_engine.handlers.condition.test_base import ConditionTestCase
@@ -15,29 +11,16 @@ class TestIssueResolutionChangeCondition(ConditionTestCase):
         self.event_data = WorkflowEventData(event=self.group_event, group=self.group_event.group)
         self.dc = self.create_data_condition(
             type=self.condition,
-            comparison=True,
+            comparison=1,
             condition_result=True,
         )
 
     def test_evaluate_value(self) -> None:
-        self.group_event.group.status = GroupStatus.RESOLVED
+        self.group_event.group.status = 1
         result = self.dc.evaluate_value(self.event_data)
         assert result is self.dc.get_condition_result()
 
     def test_evaluate_value__not_matching_comparison(self) -> None:
-        self.group_event.group.status = GroupStatus.UNRESOLVED
+        self.group_event.group.status = 2
         result = self.dc.evaluate_value(self.event_data)
         assert result is None
-
-    def test_json_schema(self) -> None:
-        # Valid
-        self.dc.comparison = True
-        self.dc.save()
-
-        self.dc.comparison = False
-        self.dc.save()
-
-        # Invalid
-        self.dc.comparison = 1
-        with pytest.raises(ValidationError):
-            self.dc.save()
