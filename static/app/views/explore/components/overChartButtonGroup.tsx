028a1bd5824a76653f5d8bc598a37e201be7ab74
JonasBa <jonas@badalic.com>
2026-01-08 13:01:06 -0800

    ref(overchartbuttongroup) fix responsive property

--- static/app/views/explore/components/overChartButtonGroup.tsx
+++ static/app/views/explore/components/overChartButtonGroup.tsx
@@ -1,5 +1,12 @@
 import {Flex, type FlexProps} from '@sentry/scraps/layout';
 
 export function OverChartButtonGroup(props: FlexProps<'div'>) {
-  return <Flex justify="end" marginBottom="md" gap="xs" {...props} />;
+  return (
+    <Flex
+      justify={props.justify ? props.justify : {sm: 'end', md: 'between'}}
+      marginBottom="md"
+      gap="xs"
+      {...props}
+    />
+  );
 }
