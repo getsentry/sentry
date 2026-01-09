35f7996eff5e06fcfd6e3a14e3e604711c48ee08
Shashank Jarmale <shashank.jarmale@sentry.io>
2026-01-09 12:01:21 -0800

    ref(api): Switch from `ParamsType` to `SnubaParams` in group endpoints (#105950)
    Switching from `ParamsType` to `SnubaParams` for specifying query
    filters/params in the `GroupEventsEndpoint` & `GroupAttachmentsEndpoint`
    code paths.

--- src/sentry/issues/endpoints/group_attachments.py
+++ src/sentry/issues/endpoints/group_attachments.py
@@ -18,7 +18,7 @@ from sentry.exceptions import InvalidParams
 from sentry.issues.endpoints.bases.group import GroupEndpoint
 from sentry.models.eventattachment import EventAttachment, event_attachment_screenshot_filter
 from sentry.models.group import Group
-from sentry.search.events.types import ParamsType
+from sentry.search.events.types import SnubaParams
 
 
 def get_event_ids_from_filters(
@@ -44,20 +44,18 @@ def get_event_ids_from_filters(
     if not query and not environments:
         return None
 
-    params: ParamsType = {
-        "project_id": [group.project_id],
-        "organization_id": group.project.organization_id,
-        "start": start if start else default_start,
-        "end": end if end else default_end,
-    }
-
-    if environments:
-        params["environment"] = [env.name for env in environments]
+    snuba_params = SnubaParams(
+        start=start if start else default_start,
+        end=end if end else default_end,
+        environments=environments,
+        projects=[group.project],
+        organization=group.project.organization,
+    )
 
     with handle_query_errors():
         snuba_query = get_query_builder_for_group(
             query=query,
-            snuba_params=params,
+            snuba_params=snuba_params,
             group=group,
             limit=10000,
             offset=0,
