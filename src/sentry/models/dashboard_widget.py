9e77e7b97382dd8f4f6197847bec02219ca052de
Trevor Elkins <trevor@emergetools.com>
2026-01-09 18:11:07 -0500

    revert

--- src/sentry/models/dashboard_widget.py
+++ src/sentry/models/dashboard_widget.py
@@ -75,7 +75,7 @@ class DashboardWidgetTypes(TypesClass):
     """
     Mobile app size metrics from preprod item type on the EAP dataset.
     """
-    MOBILE_BUILDS = 105
+    MOBILE_APP_SIZE = 105
 
     TYPES = [
         (DISCOVER, "discover"),
@@ -89,7 +89,7 @@ class DashboardWidgetTypes(TypesClass):
         (SPANS, "spans"),
         (LOGS, "logs"),
         (TRACEMETRICS, "tracemetrics"),
-        (MOBILE_BUILDS, "mobile-builds"),
+        (MOBILE_APP_SIZE, "mobile-app-size"),
     ]
     TYPE_NAMES = [t[1] for t in TYPES]
 
