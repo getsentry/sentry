73525ce503669a4357047f6b86b5802225e1b613
Colleen O'Rourke <colleen@sentry.io>
2026-01-09 14:26:41 -0800

    use actorserializerersponse

--- src/sentry/workflow_engine/endpoints/serializers/detector_serializer.py
+++ src/sentry/workflow_engine/endpoints/serializers/detector_serializer.py
@@ -1,13 +1,13 @@
 from collections import defaultdict
 from collections.abc import Mapping, MutableMapping, Sequence
 from datetime import datetime
-from typing import Any, NotRequired, TypedDict
+from typing import Any, TypedDict
 
 from django.db.models import Count
 from drf_spectacular.utils import extend_schema_serializer
 
 from sentry.api.serializers import Serializer, register, serialize
-from sentry.api.serializers.models.actor import ActorSerializer
+from sentry.api.serializers.models.actor import ActorSerializer, ActorSerializerResponse
 from sentry.api.serializers.models.group import SimpleGroupSerializer
 from sentry.api.serializers.rest_framework.base import convert_dict_key_case, snake_to_camel_case
 from sentry.grouping.grouptype import ErrorGroupType
@@ -24,15 +24,8 @@ from sentry.workflow_engine.models import (
 )
 
 
-class OwnerSerializerResponse(TypedDict):
-    type: str
-    id: str
-    name: str
-    email: NotRequired[str]
-
-
 class DetectorSerializerResponseOptional(TypedDict, total=False):
-    owner: OwnerSerializerResponse | None
+    owner: ActorSerializerResponse | None
     createdBy: str | None
     alertRuleId: int | None
     ruleId: int | None
