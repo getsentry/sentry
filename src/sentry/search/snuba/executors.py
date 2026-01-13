d84bfb80bbd621facfdfdff35df4167941b50af7
Shashank Jarmale <shashank.jarmale@sentry.io>
2026-01-13 09:29:13 -0800

    chore(issue search): Remove enablement option for truncation of group IDs in Snuba queries (#105732)
    This new truncation logic has been running successfully for all queries
    since Monday 1/5, so it's safe to remove this option.

--- src/sentry/search/snuba/executors.py
+++ src/sentry/search/snuba/executors.py
@@ -920,10 +920,7 @@ class PostgresSnubaQueryExecutor(AbstractQueryExecutor):
             # If we had too_many_candidates, fall back to truncation instead of
             # returning empty. This handles selective filters (like assigned_to)
             # where random sampling is unlikely to find matches.
-            is_truncation_enabled = options.get(
-                "snuba.search.truncate-group-ids-for-selective-filters-enabled"
-            )
-            if too_many_candidates and original_group_ids and is_truncation_enabled:
+            if too_many_candidates and original_group_ids:
                 metrics.incr("snuba.search.hits_zero_fallback_to_truncation", skip_internal=False)
                 group_ids = original_group_ids[:max_candidates]
                 too_many_candidates = False
