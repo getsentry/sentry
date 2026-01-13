e9d7e5fd382a8a8a03b95aa38aa8e3d040025773
Valery Brobbey <valery.brobbey@sentry.io>
2026-01-13 15:25:32 -0800

    chore(billing): add constants for emerge

--- static/app/constants/index.tsx
+++ static/app/constants/index.tsx
@@ -660,6 +660,30 @@ export const DATA_CATEGORY_INFO = {
       `/settings/${organization.slug}/seer/`,
     formatting: DEFAULT_COUNT_FORMATTING,
   },
+  [DataCategoryExact.SIZE_ANALYSIS]: {
+    name: DataCategoryExact.SIZE_ANALYSIS,
+    plural: DataCategory.SIZE_ANALYSIS,
+    singular: 'sizeAnalysis',
+    displayName: 'size analysis',
+    titleName: t('Size Analyses'),
+    productName: t('Size Analysis'),
+    uid: 35,
+    isBilledCategory: false,
+    statsInfo: DEFAULT_STATS_INFO,
+    formatting: DEFAULT_COUNT_FORMATTING,
+  },
+  [DataCategoryExact.INSTALLABLE_BUILD]: {
+    name: DataCategoryExact.INSTALLABLE_BUILD,
+    plural: DataCategory.INSTALLABLE_BUILD,
+    singular: 'installableBuild',
+    displayName: 'build distribution',
+    titleName: t('Build Distributions'),
+    productName: t('Build Distribution'),
+    uid: 36,
+    isBilledCategory: false,
+    statsInfo: DEFAULT_STATS_INFO,
+    formatting: DEFAULT_COUNT_FORMATTING,
+  },
 } as const satisfies Record<DataCategoryExact, DataCategoryInfo>;
 
 // SmartSearchBar settings
