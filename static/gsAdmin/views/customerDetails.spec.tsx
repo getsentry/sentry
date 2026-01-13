1d6a1dc65c4e2a3f7eed8954b3c29bdc6197bda6
Jesse Box <me@jessebox.net>
2026-01-13 11:13:03 +0100

    Updated: the tests...

--- static/gsAdmin/views/customerDetails.spec.tsx
+++ static/gsAdmin/views/customerDetails.spec.tsx
@@ -869,7 +869,7 @@ describe('Customer Details', () => {
           {name: '2021-07-19T00:00:00Z', value: 2000},
           {name: '2021-07-20T00:00:00Z', value: 0},
         ],
-        color: theme.tokens.graphics.promotion.vibrant,
+        color: theme.tokens.graphics.promotion.moderate,
       },
       {
         seriesName: 'Discarded (Client)',
