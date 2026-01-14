7a8b43431feeb058f5914ed78daf647bab9ecc3d
Jonas <jonas.badalic@sentry.io>
2026-01-14 10:10:24 -0800

    ref(theme) remove yellow100-yellow400 (#106144)
    Removes yellow100 to yellow400 deprecated range
    
    Fix DE-304

--- static/app/components/events/attachmentViewers/logFileViewer.tsx
+++ static/app/components/events/attachmentViewers/logFileViewer.tsx
@@ -54,13 +54,13 @@ const SentryStyleAnsi = styled(Ansi)`
     Object.entries(COLOR_MAP).map(
       ([ansiColor, themeColor]) => `
       .ansi-${ansiColor}-bg {
-        background-color: ${themeColor === 'pink' ? p.theme.colors.pink400 : p.theme[`${themeColor}400`]};
+        background-color: ${themeColor === 'pink' ? p.theme.colors.pink500 : themeColor === 'yellow' ? p.theme.colors.yellow500 : p.theme[`${themeColor}400`]};
       }
       .ansi-${ansiColor}-fg {
-        color: ${themeColor === 'pink' ? p.theme.colors.pink400 : p.theme[`${themeColor}400`]};
+        color: ${themeColor === 'pink' ? p.theme.colors.pink500 : themeColor === 'yellow' ? p.theme.colors.yellow500 : p.theme[`${themeColor}400`]};
       }
       .ansi-bright-${ansiColor}-fg {
-        color: ${themeColor === 'pink' ? p.theme.colors.pink200 : p.theme[`${themeColor}200`]};
+        color: ${themeColor === 'pink' ? p.theme.colors.pink200 : themeColor === 'yellow' ? p.theme.colors.yellow200 : p.theme[`${themeColor}200`]};
       }`
     )}
 
