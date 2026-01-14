b430bd0f5f7ad51948a2bf22899d523de861d0dd
Lyn Nagara <lynnagara@users.noreply.github.com>
2026-01-14 14:17:33 -0800

    updates

--- src/sentry/api/endpoints/synapse/authentication.py
+++ src/sentry/api/endpoints/synapse/authentication.py
@@ -3,20 +3,23 @@ from __future__ import annotations
 from rest_framework.permissions import BasePermission
 from rest_framework.request import Request
 
-from sentry.api.authentication import AuthenticationSiloLimit, ServiceRpcSignatureAuthentication
+from sentry.api.authentication import AuthenticationSiloLimit, HmacSignatureAuthentication
 from sentry.silo.base import SiloMode
 
 
 @AuthenticationSiloLimit(SiloMode.CONTROL)
-class SynapseSignatureAuthentication(ServiceRpcSignatureAuthentication):
+class SynapseSignatureAuthentication(HmacSignatureAuthentication):
     """
     Authentication for Synapse RPC requests.
     Requests are sent with an HMAC signed by SYNAPSE_HMAC_SECRET.
     """
 
+    token_name = b"Signature"
     shared_secret_setting_name = "SYNAPSE_HMAC_SECRET"
     service_name = "Synapse"
     sdk_tag_name = "synapse_auth"
+    signature_prefix = "synapse0"
+    include_url_in_signature = True
 
 
 class SynapseAuthPermission(BasePermission):
