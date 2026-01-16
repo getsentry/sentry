5e8b08d3f95963d4848d38c8221624f902f5fd8d
Alex Sohn <alex.sohn@sentry.io>
2026-01-16 13:45:46 +0100

    chore(consoles): remove unused console sdk github app options

--- src/sentry/options/defaults.py
+++ src/sentry/options/defaults.py
@@ -711,8 +711,6 @@ register("github-app.client-secret", flags=FLAG_CREDENTIAL | FLAG_PRIORITIZE_DIS
 register("github-console-sdk-app.id", default=0, flags=FLAG_AUTOMATOR_MODIFIABLE)
 register("github-console-sdk-app.installation-id", default="", flags=FLAG_CREDENTIAL)
 register("github-console-sdk-app.private-key", flags=FLAG_CREDENTIAL | FLAG_PRIORITIZE_DISK)
-register("github-console-sdk-app.client-id", default="", flags=FLAG_AUTOMATOR_MODIFIABLE)
-register("github-console-sdk-app.client-secret", flags=FLAG_CREDENTIAL | FLAG_PRIORITIZE_DISK)
 
 # Github Enterprise Integration
 register(
