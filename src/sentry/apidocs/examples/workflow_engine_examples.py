895ad3553098c1f79ff437c327819a5e50eb4e12
Colleen O'Rourke <colleen@sentry.io>
2026-01-16 12:22:16 -0800

    feat(ACI): Document organization workflow index GET and DELETE endpoints (#106210)
    Add documentation for `OrganizationWorkflowIndexEndpoint`'s GET and
    DELETE endpoints. This will not be actually published yet, but to
    prepare all of the new workflow engine endpoints we're keeping them
    marked as experimental and not adding the new sidebar item just yet.
    
    <img width="984" height="718" alt="Screenshot 2026-01-13 at 11 41 29 AM"
    src="https://github.com/user-attachments/assets/6dd2eb16-4b97-4012-a20b-329db8bde7a7"
    />
    <img width="994" height="683" alt="Screenshot 2026-01-13 at 12 07 45 PM"
    src="https://github.com/user-attachments/assets/9325f530-848d-4567-b39e-134ee9edd842"
    />

--- src/sentry/apidocs/examples/workflow_engine_examples.py
+++ src/sentry/apidocs/examples/workflow_engine_examples.py
@@ -185,3 +185,169 @@ class WorkflowEngineExamples:
             response_only=True,
         )
     ]
+    LIST_WORKFLOWS = [
+        OpenApiExample(
+            "List all workflows in an organization",
+            value=[
+                {
+                    "id": "123",
+                    "name": "Send a notification for high priority issues",
+                    "organizationId": "1",
+                    "createdBy": None,
+                    "dateCreated": "2025-03-18T20:48:55.495059Z",
+                    "dateUpdated": "2025-03-18T20:48:55.579094Z",
+                    "triggers": {
+                        "id": "12345",
+                        "organizationId": "1",
+                        "logicType": "any-short",
+                        "conditions": [
+                            {
+                                "id": "2345",
+                                "type": "new_high_priority_issue",
+                                "comparison": True,
+                                "conditionResult": True,
+                            },
+                            {
+                                "id": "3456",
+                                "type": "existing_high_priority_issue",
+                                "comparison": True,
+                                "conditionResult": True,
+                            },
+                        ],
+                        "actions": [],
+                    },
+                    "actionFilters": [
+                        {
+                            "id": "5678",
+                            "organizationId": "1",
+                            "logicType": "all",
+                            "conditions": [],
+                            "actions": [
+                                {
+                                    "id": "234",
+                                    "type": "email",
+                                    "integrationId": None,
+                                    "data": {"fallthroughType": "ActiveMembers"},
+                                    "config": {
+                                        "targetType": "issue_owners",
+                                        "targetDisplay": None,
+                                        "targetIdentifier": None,
+                                    },
+                                    "status": "active",
+                                }
+                            ],
+                        }
+                    ],
+                    "environment": None,
+                    "config": {"frequency": 30},
+                    "detectorIds": [],
+                    "enabled": True,
+                    "lastTriggered": None,
+                },
+                {
+                    "id": "456",
+                    "name": "Notify team #example-team",
+                    "organizationId": "1",
+                    "createdBy": "34567",
+                    "dateCreated": "2026-01-15T23:46:39.594915Z",
+                    "dateUpdated": "2026-01-15T23:46:39.594903Z",
+                    "triggers": {
+                        "id": "12345",
+                        "organizationId": "1",
+                        "logicType": "any-short",
+                        "conditions": [],
+                        "actions": [],
+                    },
+                    "actionFilters": [
+                        {
+                            "id": "6789",
+                            "organizationId": "1",
+                            "logicType": "any-short",
+                            "conditions": [
+                                {
+                                    "id": "5678",
+                                    "type": "event_frequency_count",
+                                    "comparison": {"value": 100, "interval": "1h"},
+                                    "conditionResult": True,
+                                }
+                            ],
+                            "actions": [
+                                {
+                                    "id": "1234",
+                                    "type": "email",
+                                    "integrationId": None,
+                                    "data": {},
+                                    "config": {
+                                        "targetType": "team",
+                                        "targetDisplay": None,
+                                        "targetIdentifier": "1234567890",
+                                    },
+                                    "status": "active",
+                                }
+                            ],
+                        }
+                    ],
+                    "environment": None,
+                    "config": {"frequency": 1440},
+                    "detectorIds": [],
+                    "enabled": True,
+                    "lastTriggered": None,
+                },
+                {
+                    "id": "789",
+                    "name": "Notify Jane Doe",
+                    "organizationId": "1",
+                    "createdBy": "2345",
+                    "dateCreated": "2026-01-15T23:46:39.594915Z",
+                    "dateUpdated": "2026-01-15T23:49:57.697583Z",
+                    "triggers": {
+                        "id": "56789",
+                        "organizationId": "1",
+                        "logicType": "any-short",
+                        "conditions": [],
+                        "actions": [],
+                    },
+                    "actionFilters": [
+                        {
+                            "id": "56789",
+                            "organizationId": "1",
+                            "logicType": "any-short",
+                            "conditions": [
+                                {
+                                    "id": "234567",
+                                    "type": "event_unique_user_frequency_count",
+                                    "comparison": {
+                                        "value": 100,
+                                        "filters": [{"key": "foo", "match": "eq", "value": "bar"}],
+                                        "interval": "1h",
+                                    },
+                                    "conditionResult": True,
+                                }
+                            ],
+                            "actions": [
+                                {
+                                    "id": "456789",
+                                    "type": "email",
+                                    "integrationId": None,
+                                    "data": {},
+                                    "config": {
+                                        "targetType": "user",
+                                        "targetDisplay": None,
+                                        "targetIdentifier": "123456",
+                                    },
+                                    "status": "active",
+                                }
+                            ],
+                        }
+                    ],
+                    "environment": None,
+                    "config": {"frequency": 1440},
+                    "detectorIds": [],
+                    "enabled": True,
+                    "lastTriggered": None,
+                },
+            ],
+            status_codes=["201"],
+            response_only=True,
+        )
+    ]
