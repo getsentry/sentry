448183a7e4dcf5d571de5a522311b7361c046d6d
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-01-16 20:09:04 -0800

    ref(forms): Convert BooleanField from class to functional component
    Refactors BooleanField from a class component to a functional component to align with modern React patterns and Sentry's frontend guidelines (no new class components).
    
    Changes:
    - Converted class component to functional component
    - Removed Component import and class syntax
    - Made event parameter optional in handleChange to match OnEvent type
    - Simplified event type handling
    - Removed redundant coerceValue helper (inlined logic)
    - Maintained all existing functionality and behavior

--- static/app/components/forms/fields/booleanField.tsx
+++ static/app/components/forms/fields/booleanField.tsx
@@ -1,5 +1,3 @@
-import {Component} from 'react';
-
 import Confirm from 'sentry/components/confirm';
 import {Switch, type SwitchProps} from 'sentry/components/core/switch';
 import {Tooltip} from 'sentry/components/core/tooltip';
@@ -16,95 +14,81 @@ export interface BooleanFieldProps extends InputFieldProps {
   };
 }
 
-export default class BooleanField extends Component<BooleanFieldProps> {
-  coerceValue(value: any) {
-    return !!value;
-  }
+export default function BooleanField({confirm, ...fieldProps}: BooleanFieldProps) {
+  return (
+    <FormField {...fieldProps} resetOnError>
+      {({
+        children: _children,
+        onChange,
+        onBlur,
+        value,
+        disabled,
+        disabledReason,
+        ...props
+      }: {
+        disabled: boolean;
+        disabledReason: boolean;
+        onBlur: OnEvent;
+        onChange: OnEvent;
+        type: string;
+        value: any;
+        children?: React.ReactNode;
+      }) => {
+        const handleChange = (
+          e?: React.ChangeEvent<HTMLInputElement> | React.FormEvent<HTMLInputElement>
+        ) => {
+          // We need to toggle current value because Switch is not an input
+          const newValue = !value;
+          onChange(newValue, e);
+          onBlur(newValue, e);
+        };
 
-  handleChange = (
-    value: any,
-    onChange: OnEvent,
-    onBlur: OnEvent,
-    e: React.FormEvent<HTMLInputElement>
-  ) => {
-    // We need to toggle current value because Switch is not an input
-    const newValue = this.coerceValue(!value);
-    onChange(newValue, e);
-    onBlur(newValue, e);
-  };
-
-  render() {
-    const {confirm, ...fieldProps} = this.props;
-
-    return (
-      <FormField {...fieldProps} resetOnError>
-        {({
-          children: _children,
-          onChange,
-          onBlur,
-          value,
+        const {type: _, ...propsWithoutType} = props;
+        const switchProps: SwitchProps = {
+          ...propsWithoutType,
+          size: 'lg',
+          checked: !!value,
           disabled,
-          disabledReason,
-          ...props
-        }: {
-          disabled: boolean;
-          disabledReason: boolean;
-          onBlur: OnEvent;
-          onChange: OnEvent;
-          type: string;
-          value: any;
-          children?: React.ReactNode;
-        }) => {
-          // Create a function with required args bound
-          const handleChange = this.handleChange.bind(this, value, onChange, onBlur);
+          onChange: handleChange,
+        };
 
-          const {type: _, ...propsWithoutType} = props;
-          const switchProps: SwitchProps = {
-            ...propsWithoutType,
-            size: 'lg',
-            checked: !!value,
-            disabled,
-            onChange: handleChange,
-          };
-
-          if (confirm) {
-            return (
-              <Confirm
-                // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-                renderMessage={() => confirm[(!value).toString()]}
-                onConfirm={() => handleChange({})}
-                isDangerous={confirm.isDangerous}
-              >
-                {({open}) => (
-                  <Tooltip title={disabledReason} skipWrapper disabled={!disabled}>
-                    <Switch
-                      {...switchProps}
-                      onChange={e => {
-                        // If we have a `confirm` prop and enabling switch
-                        // Then show confirm dialog, otherwise propagate change as normal
-                        // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
-                        if (confirm[(!value).toString()]) {
-                          // Open confirm modal
-                          open();
-                          return;
-                        }
-
-                        handleChange(e);
-                      }}
-                    />
-                  </Tooltip>
-                )}
-              </Confirm>
-            );
-          }
-
+        if (confirm) {
           return (
-            <Tooltip title={disabledReason} skipWrapper disabled={!disabled}>
-              <Switch {...switchProps} />
-            </Tooltip>
+            <Confirm
+              // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+              renderMessage={() => confirm[(!value).toString()]}
+              onConfirm={() => handleChange()}
+              isDangerous={confirm.isDangerous}
+            >
+              {({open}) => (
+                <Tooltip title={disabledReason} skipWrapper disabled={!disabled}>
+                  <Switch
+                    {...switchProps}
+                    onChange={e => {
+                      // If we have a `confirm` prop and enabling switch
+                      // Then show confirm dialog, otherwise propagate change as normal
+                      // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
+                      if (confirm[(!value).toString()]) {
+                        // Open confirm modal
+                        open();
+                        return;
+                      }
+
+                      handleChange(e);
+                    }}
+                  />
+                </Tooltip>
+              )}
+            </Confirm>
           );
-        }}
-      </FormField>
-    );
-  }
+        }
+
+        return (
+          <Tooltip title={disabledReason} skipWrapper disabled={!disabled}>
+            <Switch {...switchProps} />
+          </Tooltip>
+        );
+      }}
+    </FormField>
+  );
 }
