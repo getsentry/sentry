74f2f841e221ba9e9f0c7ec5b43d350430aa5a51
Charly Gomez <charly.gomez@sentry.io>
2026-01-19 14:33:41 +0100

    update UI for userFeedback

--- static/app/views/settings/project/loaderScript.spec.tsx
+++ static/app/views/settings/project/loaderScript.spec.tsx
@@ -133,9 +133,10 @@ describe('LoaderScript', () => {
           ],
         },
         dynamicSdkLoaderOptions: {
+          hasDebug: false,
+          hasFeedback: false,
           hasPerformance: false,
           hasReplay: false,
-          hasDebug: false,
         },
       },
     ]);
@@ -282,9 +283,10 @@ describe('LoaderScript', () => {
           ],
         },
         dynamicSdkLoaderOptions: {
+          hasDebug: false,
+          hasFeedback: false,
           hasPerformance: false,
           hasReplay: false,
-          hasDebug: false,
         },
       },
     ]);
