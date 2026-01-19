469c7a1d487240227f1676300c7a3f27848701ce
Filippo Pacifici <fpacifici@sentry.io>
2026-01-19 07:13:32 -0800

    Add some data model documentation to spans buffer (#106504)
    buffer.py and add-buffer.lua lacks some high level architectural
    documentation. The spans data model and the requirments to assemble a
    tree
    are not clear to everybody so I added a readme

--- src/sentry/scripts/spans/add-buffer.lua
+++ src/sentry/scripts/spans/add-buffer.lua
@@ -1,16 +1,34 @@
 --[[
 
-Add a set of spans to the span buffer.
+Adds a batch of Spans (Subsegments) to their Segment.
+Docs about the data model can be found in the README.md file in the `spans` module.
+
+The goal of this script is to recompose Segments (trees of spans) from subsegments of spans
+that can come in any order. Each span only has knowledge of its parent. The root
+span for a segment knows it is the root most of the time.
+
+As spans can arrive in any order, this script has to assemble subtrees with the
+knowledge available, then merge them into the same segment when the common parent
+is received.
+
+This implies that it has to operate according to these steps:
+
+1. Identify the highest level known span for a subsegment.
+2. Merge subsegments when a common parent is found.
+3. Update the redirect set to reflect the current state of the tree.
+
 
 KEYS:
 - "project_id:trace_id" -- just for redis-cluster routing, all keys that the script uses are sharded like this/have this hashtag.
 
 ARGS:
-- num_spans -- int
-- parent_span_id -- str
-- has_root_span -- "true" or "false"
+- num_spans -- int -- Number of spans in the subsegment.
+- parent_span_id -- str -- The parent span id of the root of the subsegment.
+- has_root_span -- "true" or "false" -- Whether the subsegment contains the root of the segment.
 - set_timeout -- int
-- *span_id -- str[]
+- max_segment_bytes -- int -- The maximum number of bytes the segment can contain.
+- byte_count -- int -- The total number of bytes in the subsegment.
+- *span_id -- str[] -- The span ids in the subsegment.
 
 ]]--
 
@@ -29,6 +47,8 @@ local redirect_depth = 0
 
 local main_redirect_key = string.format("span-buf:sr:{%s}", project_and_trace)
 
+-- Navigates the tree up to the highest level parent span we can find. Such
+-- span is needed to know the segment we need to merge the subsegment into.
 for i = 0, 100 do -- Theoretic maximum depth of redirects is 100
     local new_set_span = redis.call("hget", main_redirect_key, set_span_id)
     redirect_depth = i
@@ -42,6 +62,7 @@ end
 local set_key = string.format("span-buf:z:{%s}:%s", project_and_trace, set_span_id)
 local parent_key = string.format("span-buf:z:{%s}:%s", project_and_trace, parent_span_id)
 
+-- Reset the set expiry as we saw a new subsegment for this set
 local has_root_span_key = string.format("span-buf:hrs:%s", set_key)
 has_root_span = has_root_span or redis.call("get", has_root_span_key) == "1"
 if has_root_span then
@@ -51,6 +72,12 @@ end
 local hset_args = {}
 local sunionstore_args = {}
 
+-- Merge the subsegment into the segment we are assembling.
+-- Merging the spans (`sunionstore_args`) is needed to compose the payloads in
+-- the same segment for them to be flushed later.
+-- Updating the redirect set instead is needed when we receive higher level spans
+-- for a tree we are assembling as the segment root each span points at in the
+-- redirect set changes when a new root is found.
 if set_span_id ~= parent_span_id and redis.call("zcard", parent_key) > 0 then
     table.insert(sunionstore_args, parent_key)
 end
