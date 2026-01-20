4c32541ca0452d8a9962939ead720f4b98ca6acd
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2026-01-20 17:42:33 +0100

    cleanup unreal onboarding

--- static/app/gettingStartedDocs/unreal/onboarding.tsx
+++ static/app/gettingStartedDocs/unreal/onboarding.tsx
@@ -170,9 +170,16 @@ export const onboarding: OnboardingConfig = {
           language: 'cpp',
           code: getVerifySnippet(params),
         },
-        logsVerify(params),
       ],
     },
+    ...(params.isLogsSelected
+      ? ([
+          {
+            title: t('Logs'),
+            content: [logsVerify(params)],
+          },
+        ] satisfies OnboardingStep[])
+      : []),
     {
       title: t('Crash Reporter Client'),
       content: [
