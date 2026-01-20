8cf15e0129b7c93425afd507219cfa2b2950929d
Alexander Alderman Webb <alexander.webb@sentry.io>
2026-01-20 09:44:43 +0100

    add metrics.tsx

---
+++ static/app/gettingStartedDocs/dotnet/metrics.tsx
@@ -0,0 +1,41 @@
+import {ExternalLink} from 'sentry/components/core/link';
+import {
+  type ContentBlock,
+  type DocsParams,
+} from 'sentry/components/onboarding/gettingStartedDoc/types';
+import {tct} from 'sentry/locale';
+
+export const metricsVerify = (params: DocsParams): ContentBlock => ({
+  type: 'conditional',
+  condition: params.isMetricsSelected,
+  content: [
+    {
+      type: 'text',
+      text: tct(
+        'Send test metrics from your app to verify metrics are arriving in Sentry.',
+        {code: <code />}
+      ),
+    },
+    {
+      type: 'code',
+      language: 'dotnet',
+      code: `using Sentry;
+
+SentrySdk.Experimental.Metrics.EmitCounter("button_click", 5,
+    [new KeyValuePair<string, object>("browser", "Firefox"), new KeyValuePair<string, object>("app_version", "1.0.0")]);
+SentrySdk.Experimental.Metrics.EmitDistribution("page_load", 15.0, SentryUnits.Duration.Millisecond,
+    [new KeyValuePair<string, object>("page", "/home")]);
+SentrySdk.Experimental.Metrics.EmitGauge("page_load", 15.0, SentryUnits.Duration.Millisecond,
+    [new KeyValuePair<string, object>("page", "/home")]);
+`,
+    },
+    {
+      type: 'text',
+      text: tct('For more detailed information, see the [link:metrics documentation].', {
+        link: (
+          <ExternalLink href="https://docs.sentry.io/platforms/dotnet/guides/metrics/" />
+        ),
+      }),
+    },
+  ],
+});
--- static/app/gettingStartedDocs/dotnet/onboarding.tsx
+++ static/app/gettingStartedDocs/dotnet/onboarding.tsx
@@ -6,7 +6,6 @@ import type {
 } from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {StepType} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {logsVerify} from 'sentry/gettingStartedDocs/dotnet/logs';
-import {metricsVerify} from 'sentry/gettingStartedDocs/dotnet/metrics';
 import {t, tct} from 'sentry/locale';
 import {getPackageVersion} from 'sentry/utils/gettingStartedDocs/getPackageVersion';
 
@@ -268,14 +267,6 @@ export const onboarding: OnboardingConfig = {
           {
             title: t('Verify Logs'),
             content: [logsVerify(params)],
-          },
-        ]
-      : []),
-    ...(params.isMetricsSelected
-      ? [
-          {
-            title: t('Verify Metrics'),
-            content: [metricsVerify(params)],
           },
         ]
       : []),
