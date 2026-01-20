d7ea5f2c4453ba3556107cae98ba5c4b3cfb5fa9
Colleen O'Rourke <colleen@sentry.io>
2026-01-20 11:33:23 -0800

    oops put that stuff back

--- src/sentry/apidocs/examples/workflow_engine_examples.py
+++ src/sentry/apidocs/examples/workflow_engine_examples.py
@@ -3,10 +3,434 @@ from drf_spectacular.utils import OpenApiExample
 
 class WorkflowEngineExamples:
     LIST_ORG_DETECTORS = [
+        OpenApiExample(
+            "List all monitors in an organization",
+            value=[
+                {
+                    "id": "123",
+                    "projectId": "1",
+                    "name": "Error Monitor",
+                    "description": None,
+                    "type": "error",
+                    "workflowIds": ["12345"],
+                    "owner": None,
+                    "createdBy": None,
+                    "dateCreated": "2025-03-12T11:15:50.764865Z",
+                    "dateUpdated": "2025-06-17T14:04:02.485354Z",
+                    "dataSources": None,
+                    "conditionGroup": None,
+                    "config": {},
+                    "enabled": True,
+                    "latestGroup": {
+                        "id": "123456789",
+                        "title": "This is an example Python exception",
+                        "culprit": "/api/0/test/example/",
+                        "shortId": "SENTRY-ABC12",
+                        "level": "info",
+                        "status": "unresolved",
+                        "substatus": "escalating",
+                        "platform": "python",
+                        "project": {
+                            "id": "1",
+                            "name": "Example",
+                            "slug": "sentry",
+                            "platform": "python",
+                        },
+                        "type": "default",
+                        "issueType": "error",
+                        "issueCategory": "error",
+                        "metadata": {
+                            "title": "This is an example Python exception",
+                            "sdk": {
+                                "name": "sentry.python.django",
+                                "name_normalized": "sentry.python",
+                            },
+                            "severity": 0.0,
+                            "severity_reason": "log_level_info",
+                            "initial_priority": 25,
+                        },
+                        "numComments": 0,
+                        "firstSeen": "2026-01-08T21:00:59.737468Z",
+                        "lastSeen": "2026-01-08T21:23:45.723716Z",
+                    },
+                    "openIssues": 100,
+                },
+                {
+                    "id": "234567891",
+                    "projectId": "1",
+                    "name": "[us] Example",
+                    "description": None,
+                    "type": "uptime_domain_failure",
+                    "workflowIds": [],
+                    "owner": {"type": "team", "id": "1234", "name": "abc"},
+                    "createdBy": None,
+                    "dateCreated": "2025-04-21T21:56:32.445528Z",
+                    "dateUpdated": "2025-10-23T00:07:24.809466Z",
+                    "dataSources": [
+                        {
+                            "id": "271218",
+                            "organizationId": "1",
+                            "type": "uptime_subscription",
+                            "sourceId": "267409",
+                            "queryObj": {
+                                "url": "https://example.com",
+                                "method": "POST",
+                                "body": '{\n  "level": "info",\n  "message": "Test-Event",\n  "platform": "javascript"\n}',
+                                "headers": [
+                                    ["content-type", "application/json"],
+                                    ["user-agent", "Sentry Test"],
+                                ],
+                                "intervalSeconds": 60,
+                                "timeoutMs": 5000,
+                                "traceSampling": False,
+                            },
+                        }
+                    ],
+                    "conditionGroup": {
+                        "id": "56789",
+                        "organizationId": "1",
+                        "logicType": "any",
+                        "conditions": [
+                            {
+                                "id": "123456",
+                                "type": "eq",
+                                "comparison": "failure",
+                                "conditionResult": 75,
+                            },
+                            {
+                                "id": "234567",
+                                "type": "eq",
+                                "comparison": "success",
+                                "conditionResult": 0,
+                            },
+                        ],
+                        "actions": [],
+                    },
+                    "config": {
+                        "mode": 1,
+                        "environment": "us",
+                        "downtimeThreshold": 3,
+                        "recoveryThreshold": 1,
+                    },
+                    "enabled": True,
+                    "latestGroup": None,
+                    "openIssues": 0,
+                },
+                {
+                    "id": "1234567",
+                    "projectId": "123",
+                    "name": "Test Alert",
+                    "description": None,
+                    "type": "metric_issue",
+                    "workflowIds": ["1234567"],
+                    "owner": {
+                        "type": "user",
+                        "id": "12345",
+                        "name": "colleen@sentry.io",
+                        "email": "colleen@sentry.io",
+                    },
+                    "createdBy": "12345",
+                    "dateCreated": "2025-04-28T22:46:12.469771Z",
+                    "dateUpdated": "2025-07-29T20:57:06.680844Z",
+                    "dataSources": [
+                        {
+                            "id": "56789",
+                            "organizationId": "1",
+                            "type": "snuba_query_subscription",
+                            "sourceId": "45678",
+                            "queryObj": {
+                                "id": "34567",
+                                "status": 0,
+                                "subscription": "12/3456789",
+                                "snubaQuery": {
+                                    "id": "56789",
+                                    "dataset": "events_analytics_platform",
+                                    "query": "",
+                                    "aggregate": "avg(span.duration)",
+                                    "timeWindow": 14400,
+                                    "environment": None,
+                                    "eventTypes": ["trace_item_span"],
+                                    "extrapolationMode": "unknown",
+                                },
+                            },
+                        }
+                    ],
+                    "conditionGroup": {
+                        "id": "12345",
+                        "organizationId": "1",
+                        "logicType": "any",
+                        "conditions": [
+                            {
+                                "id": "456789",
+                                "type": "gt",
+                                "comparison": 1600.0,
+                                "conditionResult": 75,
+                            },
+                            {
+                                "id": "345678",
+                                "type": "lte",
+                                "comparison": 1600.0,
+                                "conditionResult": 0,
+                            },
+                        ],
+                        "actions": [],
+                    },
+                    "config": {"detectionType": "static", "comparisonDelta": None},
+                    "enabled": True,
+                    "latestGroup": None,
+                    "openIssues": 0,
+                },
+            ],
+            status_codes=["201"],
+            response_only=True,
+        )
     ]
     LIST_WORKFLOWS = [
+        OpenApiExample(
+            "List all workflows in an organization",
+            value=[
+                {
+                    "id": "123",
+                    "name": "Send a notification for high priority issues",
+                    "organizationId": "1",
+                    "createdBy": None,
+                    "dateCreated": "2025-03-18T20:48:55.495059Z",
+                    "dateUpdated": "2025-03-18T20:48:55.579094Z",
+                    "triggers": {
+                        "id": "12345",
+                        "organizationId": "1",
+                        "logicType": "any-short",
+                        "conditions": [
+                            {
+                                "id": "2345",
+                                "type": "new_high_priority_issue",
+                                "comparison": True,
+                                "conditionResult": True,
+                            },
+                            {
+                                "id": "3456",
+                                "type": "existing_high_priority_issue",
+                                "comparison": True,
+                                "conditionResult": True,
+                            },
+                        ],
+                        "actions": [],
+                    },
+                    "actionFilters": [
+                        {
+                            "id": "5678",
+                            "organizationId": "1",
+                            "logicType": "all",
+                            "conditions": [],
+                            "actions": [
+                                {
+                                    "id": "234",
+                                    "type": "email",
+                                    "integrationId": None,
+                                    "data": {"fallthroughType": "ActiveMembers"},
+                                    "config": {
+                                        "targetType": "issue_owners",
+                                        "targetDisplay": None,
+                                        "targetIdentifier": None,
+                                    },
+                                    "status": "active",
+                                }
+                            ],
+                        }
+                    ],
+                    "environment": None,
+                    "config": {"frequency": 30},
+                    "detectorIds": [],
+                    "enabled": True,
+                    "lastTriggered": None,
+                },
+                {
+                    "id": "456",
+                    "name": "Notify team #example-team",
+                    "organizationId": "1",
+                    "createdBy": "34567",
+                    "dateCreated": "2026-01-15T23:46:39.594915Z",
+                    "dateUpdated": "2026-01-15T23:46:39.594903Z",
+                    "triggers": {
+                        "id": "12345",
+                        "organizationId": "1",
+                        "logicType": "any-short",
+                        "conditions": [],
+                        "actions": [],
+                    },
+                    "actionFilters": [
+                        {
+                            "id": "6789",
+                            "organizationId": "1",
+                            "logicType": "any-short",
+                            "conditions": [
+                                {
+                                    "id": "5678",
+                                    "type": "event_frequency_count",
+                                    "comparison": {"value": 100, "interval": "1h"},
+                                    "conditionResult": True,
+                                }
+                            ],
+                            "actions": [
+                                {
+                                    "id": "1234",
+                                    "type": "email",
+                                    "integrationId": None,
+                                    "data": {},
+                                    "config": {
+                                        "targetType": "team",
+                                        "targetDisplay": None,
+                                        "targetIdentifier": "1234567890",
+                                    },
+                                    "status": "active",
+                                }
+                            ],
+                        }
+                    ],
+                    "environment": None,
+                    "config": {"frequency": 1440},
+                    "detectorIds": [],
+                    "enabled": True,
+                    "lastTriggered": None,
+                },
+                {
+                    "id": "789",
+                    "name": "Notify Jane Doe",
+                    "organizationId": "1",
+                    "createdBy": "2345",
+                    "dateCreated": "2026-01-15T23:46:39.594915Z",
+                    "dateUpdated": "2026-01-15T23:49:57.697583Z",
+                    "triggers": {
+                        "id": "56789",
+                        "organizationId": "1",
+                        "logicType": "any-short",
+                        "conditions": [],
+                        "actions": [],
+                    },
+                    "actionFilters": [
+                        {
+                            "id": "56789",
+                            "organizationId": "1",
+                            "logicType": "any-short",
+                            "conditions": [
+                                {
+                                    "id": "234567",
+                                    "type": "event_unique_user_frequency_count",
+                                    "comparison": {
+                                        "value": 100,
+                                        "filters": [{"key": "foo", "match": "eq", "value": "bar"}],
+                                        "interval": "1h",
+                                    },
+                                    "conditionResult": True,
+                                }
+                            ],
+                            "actions": [
+                                {
+                                    "id": "456789",
+                                    "type": "email",
+                                    "integrationId": None,
+                                    "data": {},
+                                    "config": {
+                                        "targetType": "user",
+                                        "targetDisplay": None,
+                                        "targetIdentifier": "123456",
+                                    },
+                                    "status": "active",
+                                }
+                            ],
+                        }
+                    ],
+                    "environment": None,
+                    "config": {"frequency": 1440},
+                    "detectorIds": [],
+                    "enabled": True,
+                    "lastTriggered": None,
+                },
+            ],
+            status_codes=["201"],
+            response_only=True,
+        )
     ]
     CREATE_WORKFLOW = [
+        OpenApiExample(
+            "Workflow successfully created",
+            value={
+                "id": "1234567",
+                "name": "Notify Jane Doe",
+                "organizationId": "1",
+                "createdBy": "2345",
+                "dateCreated": "2026-01-13T21:21:15.975384Z",
+                "dateUpdated": "2026-01-13T21:21:15.975376Z",
+                "triggers": {
+                    "id": "456789",
+                    "organizationId": "1",
+                    "logicType": "any-short",
+                    "conditions": [
+                        {
+                            "id": "234567",
+                            "type": "first_seen_event",
+                            "comparison": True,
+                            "conditionResult": True,
+                        },
+                        {
+                            "id": "1234567",
+                            "type": "issue_resolved_trigger",
+                            "comparison": True,
+                            "conditionResult": True,
+                        },
+                        {
+                            "id": "678912",
+                            "type": "reappeared_event",
+                            "comparison": True,
+                            "conditionResult": True,
+                        },
+                        {
+                            "id": "7891234",
+                            "type": "regression_event",
+                            "comparison": True,
+                            "conditionResult": True,
+                        },
+                    ],
+                    "actions": [],
+                },
+                "actionFilters": [
+                    {
+                        "id": "45678",
+                        "organizationId": "1",
+                        "logicType": "any-short",
+                        "conditions": [
+                            {
+                                "id": "23456789",
+                                "type": "issue_occurrences",
+                                "comparison": {"value": 10},
+                                "conditionResult": True,
+                            }
+                        ],
+                        "actions": [
+                            {
+                                "id": "1234789",
+                                "type": "email",
+                                "integrationId": None,
+                                "data": {},
+                                "config": {
+                                    "targetType": "user",
+                                    "targetDisplay": None,
+                                    "targetIdentifier": "6789",
+                                },
+                                "status": "active",
+                            }
+                        ],
+                    }
+                ],
+                "environment": None,
+                "config": {"frequency": 1440},
+                "detectorIds": ["1000"],
+                "enabled": True,
+                "lastTriggered": None,
+            },
+            status_codes=["201"],
+            response_only=True,
+        )
     ]
     CREATE_DETECTOR = [
         OpenApiExample(
