e8be4a6343d706981a0c095af1724a691d1bfdef
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-01-22 15:29:56 -0800

    ref: Use `getApiURl()` inside of static/app/view/{...rest}/* (#106749)
    See: https://gist.github.com/ryan953/af5458fa4f89ae42cc19e8ea266b7e9a

--- static/app/views/admin/adminUserEdit.tsx
+++ static/app/views/admin/adminUserEdit.tsx
@@ -15,6 +15,7 @@ import LoadingIndicator from 'sentry/components/loadingIndicator';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {User} from 'sentry/types/user';
+import getApiUrl from 'sentry/utils/api/getApiUrl';
 import {
   setApiQueryData,
   useApiQuery,
@@ -119,7 +120,7 @@ function RemoveUserModal({user, onRemove, closeModal}: RemoveModalProps) {
 
 function AdminUserEdit() {
   const {id} = useParams<{id: string}>();
-  const userEndpoint = `/users/${id}/`;
+  const userEndpoint = getApiUrl('/users/$userId/', {path: {userId: id}});
   const [formModel] = useState(() => new FormModel());
   const api = useApi({persistInFlight: true});
   const navigate = useNavigate();
