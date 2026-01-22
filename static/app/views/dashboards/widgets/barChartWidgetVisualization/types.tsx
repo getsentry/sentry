154c0100ed7274522bb3fe559de2a61b3cec2431
nsdeschenes <nicholas.deschenes@sentry.io>
2026-01-22 10:47:19 -0400

    :necktie: Rework to create bar categorical class

--- static/app/views/dashboards/widgets/barChartWidgetVisualization/types.tsx
+++ static/app/views/dashboards/widgets/barChartWidgetVisualization/types.tsx
@@ -1,5 +1,12 @@
 import type {DataUnit} from 'sentry/utils/discover/fields';
-import type {PlottableTimeSeriesValueType} from 'sentry/views/dashboards/widgets/timeSeriesWidget/plottables/plottable';
+import type {PLOTTABLE_TIME_SERIES_VALUE_TYPES} from 'sentry/views/dashboards/widgets/common/settings';
+
+/**
+ * The type of values in a categorical series.
+ * This is the same set of types supported by time series, but defined locally
+ * to avoid coupling to time series widgets.
+ */
+export type CategoricalValueType = (typeof PLOTTABLE_TIME_SERIES_VALUE_TYPES)[number];
 
 /**
  * A single item in a categorical bar chart series.
@@ -22,7 +29,7 @@ export interface CategoricalSeriesMeta {
   /**
    * The type of the values (e.g., "duration", "number")
    */
-  valueType: PlottableTimeSeriesValueType;
+  valueType: CategoricalValueType;
   /**
    * The unit of the values, if applicable.
    */
