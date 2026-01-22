539a83fde4caa58f85e609d22fe185f4e6fb6746
JoshuaMoelans <60878493+JoshuaMoelans@users.noreply.github.com>
2026-01-22 16:05:36 +0100

    fix godot param

--- static/app/gettingStartedDocs/godot/onboarding.tsx
+++ static/app/gettingStartedDocs/godot/onboarding.tsx
@@ -91,7 +91,7 @@ export const onboarding: OnboardingConfig = {
         {
           type: 'code',
           language: 'gdscript',
-          code: getVerifySnippet(params),
+          code: getVerifySnippet(),
         },
         {
           type: 'text',
