255dfa3a515727df7795258141e170cf9e9e42df
Andrew Liu <159852527+aliu39@users.noreply.github.com>
2026-01-22 14:34:38 -0800

    update group tag endpoint

--- src/sentry/issues/endpoints/group_tags.py
+++ src/sentry/issues/endpoints/group_tags.py
@@ -11,6 +11,7 @@ from sentry.api.base import region_silo_endpoint
 from sentry.api.helpers.environments import get_environments
 from sentry.api.helpers.mobile import get_readable_device_name
 from sentry.api.serializers import serialize
+from sentry.api.utils import get_date_range_from_params
 from sentry.issues.endpoints.bases.group import GroupEndpoint
 from sentry.ratelimits.config import RateLimitConfig
 from sentry.search.utils import DEVICE_CLASS
@@ -62,12 +63,15 @@ class GroupTagsEndpoint(GroupEndpoint):
 
         environment_ids = [e.id for e in get_environments(request, group.project.organization)]
 
+        start, end = get_date_range_from_params(request.GET, optional=True)
         tag_keys = backend.get_group_tag_keys_and_top_values(
             group,
             environment_ids,
             keys=keys,
             value_limit=value_limit,
             tenant_ids={"organization_id": group.project.organization_id},
+            start=start,
+            end=end,
         )
 
         data = serialize(tag_keys, request.user)
