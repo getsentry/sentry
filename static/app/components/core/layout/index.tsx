d7c8b561455fff5f73070b7031787a688e1d32a5
Nate Moore <natemoo-re@users.noreply.github.com>
2026-01-23 10:36:10 -0500

    feat(core): add surface variant (#106408)
    [Surface
    component](https://sentry-git-feat-surface.sentry.dev/stories/core/surface)
    
    Closes DE-746
    
    ---------
    
    Co-authored-by: Jonas <jonas.badalic@sentry.io>

--- static/app/components/core/layout/index.tsx
+++ static/app/components/core/layout/index.tsx
@@ -3,6 +3,7 @@ export {
   type ContainerProps,
   type ContainerPropsWithRenderFunction,
 } from './container';
+export {Surface} from './surface';
 export {Flex, type FlexProps, type FlexPropsWithRenderFunction} from './flex';
 export {Grid, type GridProps, type GridPropsWithRenderFunction} from './grid';
 export {Stack, type StackProps, type StackPropsWithRenderFunction} from './stack';
