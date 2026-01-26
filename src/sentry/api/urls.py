d43596a4bd88270d52493fbc50d735e66ebcf305
Mark Story <mark@mark-story.com>
2026-01-26 16:06:35 -0500

    chore(cells) Remove RelayIndex endpoint
    I'd like to remove this endpoint for a few reasons:
    
    - It is not compatible with cell based routing which requires an
      organization or cell id to be present in the URL.
    - There is no production usage of this endpoint. I wasn't able to find
      any requests to this endpoint in the last week.
    - The endpoint doesn't currently work. In testing the endpoint locally,
      it always fails with
    
    ```
    Traceback (most recent call last):
      File "/Users/markstory/code/sentry/src/sentry/api/serializers/base.py", line 119, in _serialize
        return self.serialize(obj, attrs, user, **kwargs)
               ~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/Users/markstory/code/sentry/src/sentry/api/serializers/models/relay.py", line 21, in serialize
        "version": str(obj.version),
                       ^^^^^^^^^^^
    AttributeError: 'Relay' object has no attribute 'version'
    20:59:06 [ERROR] sentry.api.serializers.base: Failed to serialize (instance=<Relay at 0x312481400: id=1>)
    ```
    
    Refs https://www.notion.so/sentry/Cell-safe-API-endpoints-2a38b10e4b5d8036931fd6781e01eb23

--- src/sentry/api/urls.py
+++ src/sentry/api/urls.py
@@ -830,7 +830,6 @@ from .endpoints.prompts_activity import PromptsActivityEndpoint
 from .endpoints.relay import (
     RelayDetailsEndpoint,
     RelayHealthCheck,
-    RelayIndexEndpoint,
     RelayProjectConfigsEndpoint,
     RelayPublicKeysEndpoint,
     RelayRegisterChallengeEndpoint,
@@ -1094,11 +1093,6 @@ RELOCATION_URLS = [
 ]
 
 RELAY_URLS = [
-    re_path(
-        r"^$",
-        RelayIndexEndpoint.as_view(),
-        name="sentry-api-0-relays-index",
-    ),
     re_path(
         r"^register/challenge/$",
         RelayRegisterChallengeEndpoint.as_view(),
