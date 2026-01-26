18bda34227b545eb5ff333e89dd89f97885b9b5c
Dominik Buszowiecki <44422760+DominikB2014@users.noreply.github.com>
2026-01-26 14:47:53 -0500

    test(dashboards): fix flakey test (#106976)
    Wait for the api call to finish, before asserting on what's rendered.
    Going to run this several times to see if it passes consistently

--- static/app/components/modals/widgetViewerModal.spec.tsx
+++ static/app/components/modals/widgetViewerModal.spec.tsx
@@ -806,9 +806,7 @@ describe('Modals -> WidgetViewerModal', () => {
         expect(eventsStatsMock).toHaveBeenCalled();
       });
 
-      // TODO: this test is flakey in CI https://linear.app/getsentry/issue/BROWSE-264/fix-flakey-test
-      // eslint-disable-next-line jest/no-disabled-tests
-      it.skip('appends the orderby to the query if it is not already selected as an aggregate', async () => {
+      it('appends the orderby to the query if it is not already selected as an aggregate', async () => {
         const eventsStatsMock = mockEventsStats();
         mockEvents();
 
@@ -826,15 +824,18 @@ describe('Modals -> WidgetViewerModal', () => {
         });
 
         await renderModal({initialData, widget});
+        await waitFor(() => {
+          expect(eventsStatsMock).toHaveBeenCalledWith(
+            '/organizations/org-slug/events-stats/',
+            expect.objectContaining({
+              query: expect.objectContaining({
+                field: ['country', 'count()', 'epm()'],
+              }),
+            })
+          );
+        });
+
         expect(await screen.findByText('epm()')).toBeInTheDocument();
-        expect(eventsStatsMock).toHaveBeenCalledWith(
-          '/organizations/org-slug/events-stats/',
-          expect.objectContaining({
-            query: expect.objectContaining({
-              field: ['country', 'count()', 'epm()'],
-            }),
-          })
-        );
       });
     });
 
