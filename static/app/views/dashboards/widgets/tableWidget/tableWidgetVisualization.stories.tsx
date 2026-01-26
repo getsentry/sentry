71ad083dd492daf9ee3d79419a87fd1a7cbc48ed
Nick <nicholas.deschenes@sentry.io>
2026-01-26 13:27:26 -0400

    feat(dashboards): Add cell actions getter function (#106963)
    This PR extends the `TableWidgetVisualization` component by expanding
    the `allowedCellActions` prop. This prop now a function to be passed
    enabling finer grain control over which actions a cell has, that can be
    determined from the following info: `column`, `columnIndex`, `dataRow`,
    and `rowIndex`.
    
    Ticket: DAIN-1170

--- static/app/views/dashboards/widgets/tableWidget/tableWidgetVisualization.stories.tsx
+++ static/app/views/dashboards/widgets/tableWidget/tableWidgetVisualization.stories.tsx
@@ -391,6 +391,36 @@ function onTriggerCellAction(actions: Actions, value: string | number) {
 }
         `}
         </CodeBlock>
+        <p>
+          To customize actions per cell, use <code>allowedCellActions</code> as a
+          function. This function receives the full cell info, including the column type.
+          There are also default actions defined in <code>ALLOWED_CELL_ACTIONS</code>,
+          it's recommended to use them as a base.
+        </p>
+        <TableWidgetVisualization
+          tableData={sampleHTTPRequestTableData}
+          allowedCellActions={cellInfo => {
+            if (cellInfo.column.type === 'integer') {
+              return [Actions.SHOW_GREATER_THAN, Actions.SHOW_LESS_THAN];
+            }
+
+            return [Actions.ADD, Actions.EXCLUDE];
+          }}
+        />
+        <CodeBlock language="tsx">
+          {`
+<TableWidgetVisualization
+  tableData={sampleHTTPRequestTableData}
+  allowedCellActions={cellInfo => {
+    if (cellInfo.column.type === 'integer') {
+      return [Actions.SHOW_GREATER_THAN, Actions.SHOW_LESS_THAN];
+    }
+
+    return [Actions.ADD, Actions.EXCLUDE];
+  }}
+/>
+          `}
+        </CodeBlock>
       </Fragment>
     );
   });
