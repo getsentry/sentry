27f0f5c95078007ade49b9120a1329bc397966be
Hector Dearman <hector.dearman@sentry.io>
2026-01-26 15:29:49 +0000

    ref(preprod): Settings URL preprod->builds (#106965)
    Backend part of #106964

--- tests/sentry/preprod/vcs/status_checks/size/test_templates.py
+++ tests/sentry/preprod/vcs/status_checks/size/test_templates.py
@@ -799,7 +799,7 @@ class SuccessStateFormattingTest(StatusCheckTestBase):
 
         android_url = f"http://testserver/organizations/{self.organization.slug}/preprod/size/{android_artifact.id}?project={self.project.slug}"
         ios_url = f"http://testserver/organizations/{self.organization.slug}/preprod/size/{ios_artifact.id}?project={self.project.slug}"
-        settings_url = f"http://testserver/settings/projects/{self.project.slug}/preprod/"
+        settings_url = f"http://testserver/settings/projects/{self.project.slug}/builds/"
 
         expected = f"""\
 ## 2 Apps Analyzed
@@ -1128,7 +1128,7 @@ class TriggeredRulesFormattingTest(StatusCheckTestBase):
 
         artifact_url = f"http://testserver/organizations/{self.organization.slug}/preprod/size/{artifact.id}?project={self.project.slug}"
         settings_url = (
-            f"http://testserver/settings/projects/{self.project.slug}/preprod/?expanded=rule-1"
+            f"http://testserver/settings/projects/{self.project.slug}/builds/?expanded=rule-1"
         )
 
         expected = f"""\
@@ -1224,7 +1224,7 @@ class TriggeredRulesFormattingTest(StatusCheckTestBase):
         )
 
         artifact_url = f"http://testserver/organizations/{self.organization.slug}/preprod/size/{artifact.id}?project={self.project.slug}"
-        settings_url = f"http://testserver/settings/projects/{self.project.slug}/preprod/?expanded=rule-download-absolute&expanded=rule-install-diff&expanded=rule-download-percent"
+        settings_url = f"http://testserver/settings/projects/{self.project.slug}/builds/?expanded=rule-download-absolute&expanded=rule-install-diff&expanded=rule-download-percent"
 
         expected = f"""\
 ## ❌ 1 App Failed Size Checks
@@ -1331,7 +1331,7 @@ class TriggeredRulesFormattingTest(StatusCheckTestBase):
 
         artifact1_url = f"http://testserver/organizations/{self.organization.slug}/preprod/size/{artifact1.id}?project={self.project.slug}"
         artifact2_url = f"http://testserver/organizations/{self.organization.slug}/preprod/size/{artifact2.id}?project={self.project.slug}"
-        settings_url = f"http://testserver/settings/projects/{self.project.slug}/preprod/?expanded=rule-1&expanded=rule-2"
+        settings_url = f"http://testserver/settings/projects/{self.project.slug}/builds/?expanded=rule-1&expanded=rule-2"
 
         expected = f"""\
 ## ❌ 2 Apps Failed Size Checks
@@ -1435,7 +1435,7 @@ class TriggeredRulesFormattingTest(StatusCheckTestBase):
         failed_url = f"http://testserver/organizations/{self.organization.slug}/preprod/size/{failed_artifact.id}?project={self.project.slug}"
         passed_url = f"http://testserver/organizations/{self.organization.slug}/preprod/size/{passed_artifact.id}?project={self.project.slug}"
         settings_url = (
-            f"http://testserver/settings/projects/{self.project.slug}/preprod/?expanded=rule-1"
+            f"http://testserver/settings/projects/{self.project.slug}/builds/?expanded=rule-1"
         )
 
         expected = f"""\
