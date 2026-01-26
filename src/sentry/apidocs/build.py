3a7de1591f755ced94d5cd7511658d9a4cd73b67
Colleen O'Rourke <colleen@sentry.io>
2026-01-26 14:47:45 -0800

    chore(ACI): Add 'beta' warning to ACI endpoints (#107008)
    Add a warning that the new endpoints may not have a UI attached to them
    yet (for users not in LA as of now).
    
    
    Example of a single endpoint:
    <img width="1143" height="634" alt="Screenshot 2026-01-26 at 1 16 45 PM"
    src="https://github.com/user-attachments/assets/f83c235c-a419-4a67-b2ed-cc60e77b113e"
    />
    
    Warning on the main page for these endpoints:
    <img width="1312" height="614" alt="Screenshot 2026-01-26 at 1 32 21 PM"
    src="https://github.com/user-attachments/assets/f97aaeef-3dc6-4279-88fc-9ff860ffe42b"
    />

--- src/sentry/apidocs/build.py
+++ src/sentry/apidocs/build.py
@@ -157,8 +157,8 @@ OPENAPI_TAGS = [
     {
         "name": "Monitors",
         "x-sidebar-name": "Monitors & Alerts",
-        "description": "Endpoints for Monitors and Alerts",
-        "x-display-description": False,
+        "description": "⚠️ These endpoints are currently in **beta** and may be subject to change. They are supported by [New Monitors and Alerts](/product/new-monitors-and-alerts/) and may not be viewable in the UI today.",
+        "x-display-description": True,
         "externalDocs": {
             "description": "Found an error? Let us know.",
             "url": "https://github.com/getsentry/sentry-docs/issues/new/?title=API%20Documentation%20Error:%20/monitors/&template=api_error_template.md",
