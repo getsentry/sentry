4f47be203ee3cbd8ab7494a06be98c714c05dbf6
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2026-01-27 10:16:53 +0100

    fix(scraps): align leadingItems in compactSelect with check box/icon (#106972)
    This is the second attempt to fix the alignment, after
    https://github.com/getsentry/sentry/pull/106167 was reverted because it
    introduced an issue in the environment selector.
    
    The problem was that, in order to do the alignment, we wrapped the
    `leadingItems` again in another container, because the internally
    created checkbox is also wrapped.
    
    This fixed the alignment issue, however there are cases where
    `hideCheck:true` is passed to hide the internal checkbox and then a
    separate checkbox is rendered inside `leadingItems` 🤯 .
    
    This is what `HybridFilter` does, and that checkbox should never get
    wrapped because of how pointer events are handled.
    
    The approach in this PR always just plainly returns the `leadingItems`
    when `hideCheck` is passed to never do any wrapping. This is also what
    happened before, but it’s now a bit more explicit. This is the important
    change:
    
    
    https://github.com/getsentry/sentry/commit/02cee3ce3cac0d867f1e4977b9eac6ee8724c766
    
    The rest are the exact changes re-applied from the reverted PR.
    
    Note: I think `HybridFilter` only does this because it has an option to
    also show the checkbox on the right side, which is used by the `Project`
    selector. If we streamline this, like @Jesse-Box has planned, we can
    probably just use the internal checkbox from `CompactSelect` and remove
    all of these workarounds.

--- static/app/components/core/compactSelect/compactSelect.stories.tsx
+++ static/app/components/core/compactSelect/compactSelect.stories.tsx
@@ -561,6 +561,8 @@ const arrayToOptions = (array: string[]) =>
   array.map(item => ({
     value: item,
     label: item,
+    details: 'Details',
+    leadingItems: <IconSiren size="xs" />,
   }));
 
 const COUNTRY_NAMES = Object.keys(countryNameToCode).sort();
--- static/app/components/core/compactSelect/gridList/option.tsx
+++ static/app/components/core/compactSelect/gridList/option.tsx
@@ -8,7 +8,7 @@ import type {ListState} from '@react-stately/list';
 import type {Node} from '@react-types/shared';
 
 import {Checkbox} from 'sentry/components/core/checkbox';
-import {CheckWrap} from 'sentry/components/core/compactSelect/styles';
+import {LeadWrap} from 'sentry/components/core/compactSelect/styles';
 import {InnerWrap, MenuListItem} from 'sentry/components/core/menuListItem';
 import {IconCheckmark} from 'sentry/icons';
 import {space} from 'sentry/styles/space';
@@ -84,34 +84,35 @@ export function GridListOption({node, listState, size}: GridListOptionProps) {
   const leadingItemsMemo = useMemo(() => {
     const checkboxSize = size === 'xs' ? 'xs' : 'sm';
 
-    if (hideCheck && !leadingItems) {
-      return null;
+    const leading =
+      typeof leadingItems === 'function'
+        ? leadingItems({disabled: isDisabled, isFocused, isSelected})
+        : leadingItems;
+
+    if (hideCheck) {
+      return leading;
     }
 
     return (
       <Fragment>
-        {!hideCheck && (
-          <CheckWrap multiple={multiple} isSelected={isSelected} role="presentation">
-            {multiple ? (
-              <Checkbox
-                {...checkboxProps}
-                size={checkboxSize}
-                checked={isSelected}
-                disabled={isDisabled}
-                readOnly
-              />
-            ) : (
-              isSelected && <IconCheckmark size={checkboxSize} {...checkboxProps} />
-            )}
-          </CheckWrap>
-        )}
-        {typeof leadingItems === 'function'
-          ? leadingItems({disabled: isDisabled, isFocused, isSelected})
-          : leadingItems}
+        <LeadWrap role="presentation">
+          {multiple ? (
+            <Checkbox
+              {...checkboxProps}
+              size={checkboxSize}
+              checked={isSelected}
+              disabled={isDisabled}
+              readOnly
+            />
+          ) : (
+            isSelected && <IconCheckmark size={checkboxSize} {...checkboxProps} />
+          )}
+        </LeadWrap>
+        {leading ? <LeadWrap role="presentation">{leading}</LeadWrap> : null}
       </Fragment>
     );
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [multiple, isSelected, isDisabled, size, leadingItems, hideCheck]);
+  }, [multiple, isSelected, isDisabled, isFocused, size, leadingItems, hideCheck]);
 
   return (
     <StyledMenuListItem
