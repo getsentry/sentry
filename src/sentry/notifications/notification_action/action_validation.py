849cbf6ae7ac04dd8c296908d307cecf96f5fdd4
Colleen O'Rourke <colleen@sentry.io>
2026-01-28 10:27:57 -0800

    clean up that function too

--- src/sentry/notifications/notification_action/action_validation.py
+++ src/sentry/notifications/notification_action/action_validation.py
@@ -15,12 +15,11 @@ from sentry.notifications.notification_action.registry import action_validator_r
 from sentry.rules.actions.integrations.create_ticket.form import IntegrationNotifyServiceForm
 from sentry.rules.actions.notify_event_service import NotifyEventServiceForm
 from sentry.rules.actions.sentry_apps.utils import validate_sentry_app_action
-from sentry.sentry_apps.services.app import RpcSentryAppInstallation, app_service
+from sentry.sentry_apps.services.app import app_service
 from sentry.sentry_apps.utils.errors import SentryAppBaseError
 from sentry.utils import json
 from sentry.workflow_engine.models.action import Action
 from sentry.workflow_engine.processors.action import get_notification_plugins_for_org
-from sentry.workflow_engine.typings.notification_action import SentryAppIdentifier
 
 from .types import BaseActionValidatorHandler
 
@@ -206,32 +205,14 @@ class SentryAppActionValidatorHandler:
         self.validated_data = validated_data
         self.organization = organization
 
-    def _get_sentry_app_installation(
-        self, sentry_app_identifier: SentryAppIdentifier, target_identifier: str
-    ) -> RpcSentryAppInstallation | None:
-        """
-        Get the sentry app installation based on the sentry app id in the target identifier
-        """
+    def clean_data(self) -> dict[str, Any]:
+        target_identifier = self.validated_data["config"]["target_identifier"]
         installations = app_service.get_many(
             filter=dict(app_ids=[int(target_identifier)], organization_id=self.organization.id)
         )
-        return installations[0] if len(installations) else None
-
-    def clean_data(self) -> dict[str, Any]:
-        sentry_app_identifier = SentryAppIdentifier(
-            self.validated_data["config"]["sentry_app_identifier"]
-        )
-        target_identifier = self.validated_data["config"]["target_identifier"]
-        installation = self._get_sentry_app_installation(sentry_app_identifier, target_identifier)
+        installation = installations[0] if len(installations) else None
         if not installation:
             raise ValidationError("Sentry app installation not found.")
-
-        if sentry_app_identifier == SentryAppIdentifier.SENTRY_APP_INSTALLATION_UUID:
-            # convert to use sentry_app_id until we can migrate all the data
-            self.validated_data["config"][
-                "sentry_app_identifier"
-            ] = SentryAppIdentifier.SENTRY_APP_ID
-            self.validated_data["config"]["target_identifier"] = str(installation.sentry_app.id)
 
         settings = self.validated_data["data"].get("settings", [])
         action = {
