a4af8682948e6a705f1b241153e6b0947b8f27e1
Dominik Buszowiecki <DominikB2014@gmail.com>
2026-01-28 12:34:58 -0500

    implement mobile session health basic

---
+++ static/app/views/dashboards/utils/prebuiltConfigs/mobileSessionHealth.ts
@@ -0,0 +1,173 @@
+import {t} from 'sentry/locale';
+import {DisplayType, WidgetType, type Widget} from 'sentry/views/dashboards/types';
+import type {PrebuiltDashboard} from 'sentry/views/dashboards/utils/prebuiltConfigs';
+import {spaceWidgetsEquallyOnRow} from 'sentry/views/dashboards/utils/prebuiltConfigs/utils/spaceWidgetsEquallyOnRow';
+
+const FIRST_ROW_WIDGETS = spaceWidgetsEquallyOnRow(
+  [
+    {
+      id: 'crash-free-sessions',
+      title: t('Crash Free Sessions'),
+      displayType: DisplayType.BIG_NUMBER,
+      widgetType: WidgetType.RELEASE,
+      interval: '5m',
+      queries: [
+        {
+          fields: ['crash_free_rate(session)'],
+          aggregates: ['crash_free_rate(session)'],
+          columns: [],
+          name: '',
+          orderby: '',
+          conditions: '',
+        },
+      ],
+    },
+    {
+      id: 'crash-free-users',
+      title: t('Crash Free Users'),
+      displayType: DisplayType.BIG_NUMBER,
+      widgetType: WidgetType.RELEASE,
+      interval: '5m',
+      queries: [
+        {
+          fields: ['count_healthy(user)'],
+          aggregates: ['count_healthy(user)'],
+          columns: [],
+          name: '',
+          orderby: '',
+          conditions: '',
+        },
+      ],
+    },
+    {
+      id: 'anr-rate',
+      title: t('ANR Rate'),
+      displayType: DisplayType.BIG_NUMBER,
+      widgetType: WidgetType.RELEASE,
+      interval: '5m',
+      queries: [
+        {
+          fields: ['anr_rate()'],
+          aggregates: ['anr_rate()'],
+          columns: [],
+          name: '',
+          orderby: '',
+          conditions: '',
+        },
+      ],
+    },
+  ],
+  0,
+  {h: 1, minH: 1}
+);
+
+const SECOND_ROW_WIDGETS = spaceWidgetsEquallyOnRow(
+  [
+    {
+      id: 'crash-free-session-line-chart',
+      title: t('Crash Free Sessions'),
+      displayType: DisplayType.LINE,
+      widgetType: WidgetType.RELEASE,
+      interval: '5m',
+      queries: [
+        {
+          aggregates: ['crash_free_rate(session)'],
+          columns: [],
+          name: '',
+          orderby: '',
+          conditions: '',
+        },
+      ],
+    },
+    {
+      id: 'crash-free-user-line-chart',
+      title: t('Crash Free Users'),
+      displayType: DisplayType.LINE,
+      widgetType: WidgetType.RELEASE,
+      interval: '5m',
+      queries: [
+        {
+          fields: ['count_healthy(user)'],
+          aggregates: ['count_healthy(user)'],
+          columns: [],
+          name: '',
+          orderby: '',
+          conditions: '',
+        },
+      ],
+    },
+  ],
+  1
+);
+
+const THIRD_ROW_WIDGETS = spaceWidgetsEquallyOnRow(
+  [
+    {
+      id: 'issue-counts',
+      title: t('Issue Counts'),
+      displayType: DisplayType.BAR,
+      widgetType: WidgetType.ISSUE,
+      interval: '5m',
+      queries: [
+        {
+          fields: ['count(new_issues)', 'count(resolved_issues)'],
+          aggregates: ['count(new_issues)', 'count(resolved_issues)'],
+          columns: [],
+          name: '',
+          orderby: '',
+          conditions: '',
+        },
+      ],
+    },
+    {
+      id: 'total-sessions',
+      title: t('Total Sessions'),
+      displayType: DisplayType.LINE,
+      widgetType: WidgetType.RELEASE,
+      interval: '5m',
+      queries: [
+        {
+          fields: ['sum(session)'],
+          aggregates: ['sum(session)'],
+          columns: [],
+          name: '',
+          orderby: '',
+          conditions: '',
+        },
+      ],
+    },
+  ],
+  4
+);
+
+const RELEASE_TABLE: Widget = {
+  id: 'release-table',
+  title: t('Releases'),
+  displayType: DisplayType.TABLE,
+  widgetType: WidgetType.RELEASE,
+  interval: '5m',
+  queries: [
+    {
+      name: '',
+      conditions: '',
+      fields: ['project', 'release', 'crash_free_rate(session)', 'sum(session)'],
+      aggregates: ['crash_free_rate(session)', 'sum(session)'],
+      columns: ['project', 'release'],
+      orderby: '-release',
+    },
+  ],
+  layout: {x: 0, y: 6, w: 6, h: 3, minH: 2},
+};
+
+export const MOBILE_SESSION_HEALTH_PREBUILT_CONFIG: PrebuiltDashboard = {
+  dateCreated: '',
+  filters: {},
+  projects: [],
+  title: t('Mobile Session Health'),
+  widgets: [
+    ...FIRST_ROW_WIDGETS,
+    ...SECOND_ROW_WIDGETS,
+    ...THIRD_ROW_WIDGETS,
+    RELEASE_TABLE,
+  ],
+};
