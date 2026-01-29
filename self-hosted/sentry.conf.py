14712649635d361d3fb1874f211d87b7ceb28dc8
Giovanni Barillari <giovanni.barillari@sentry.io>
2026-01-29 20:31:49 +0100

    chore: migrate from `uwsgi` to `granian` (#107267)
    SSIA
    
    Re-do of #106089

--- self-hosted/sentry.conf.py
+++ self-hosted/sentry.conf.py
@@ -224,7 +224,7 @@ if not secret_key:
         "Error: SENTRY_SECRET_KEY is undefined, run `generate-secret-key` and set to -e SENTRY_SECRET_KEY"
     )
 
-if "SENTRY_RUNNING_UWSGI" not in os.environ and len(secret_key) < 32:
+if "SENTRY_RUNNING_GRANIAN" not in os.environ and len(secret_key) < 32:
     print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     print("!!                    CAUTION                       !!")
     print("!! Your SENTRY_SECRET_KEY is potentially insecure.  !!")
