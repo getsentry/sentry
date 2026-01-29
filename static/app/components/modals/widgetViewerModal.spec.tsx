b0cfd43ab75e96d1cd2f29b977cc91e960130dce
Nar Saynorath <nar.saynorath@sentry.io>
2026-01-29 14:56:44 -0500

    fix(widget-viewer): Release test flaky

--- static/app/components/modals/widgetViewerModal.spec.tsx
+++ static/app/components/modals/widgetViewerModal.spec.tsx
@@ -1285,34 +1285,39 @@ describe('Modals -> WidgetViewerModal', () => {
       );
     });
 
-    it('adds the release column to the table if no group by is set', async () => {
-      mockQuery = {
-        conditions: '',
-        fields: [`sum(session)`],
-        columns: [],
-        aggregates: ['sum(session)'],
-        name: 'Query Name',
-        orderby: '',
-      };
-      mockWidget = {
-        id: '1',
-        title: 'Release Widget',
-        displayType: DisplayType.LINE,
-        interval: '5m',
-        queries: [mockQuery],
-        widgetType: WidgetType.RELEASE,
-      };
-      await renderModal({initialData, widget: mockWidget});
-      expect(await screen.findByText('release')).toBeInTheDocument();
-      expect(metricsMock).toHaveBeenCalledWith(
-        '/organizations/org-slug/metrics/data/',
-        expect.objectContaining({
-          query: expect.objectContaining({
-            groupBy: ['release'],
-          }),
-        })
-      );
-    });
+    describe.each(Array.from({length: 100}, (_, i) => [i]))(
+      'adds the release column to the table if no group by is set (run #%i)',
+      _runNumber => {
+        it('should add release column and call metrics API with groupBy release', async () => {
+          mockQuery = {
+            conditions: '',
+            fields: [`sum(session)`],
+            columns: [],
+            aggregates: ['sum(session)'],
+            name: 'Query Name',
+            orderby: '',
+          };
+          mockWidget = {
+            id: '1',
+            title: 'Release Widget',
+            displayType: DisplayType.LINE,
+            interval: '5m',
+            queries: [mockQuery],
+            widgetType: WidgetType.RELEASE,
+          };
+          await renderModal({initialData, widget: mockWidget});
+          expect(await screen.findByText('release')).toBeInTheDocument();
+          expect(metricsMock).toHaveBeenCalledWith(
+            '/organizations/org-slug/metrics/data/',
+            expect.objectContaining({
+              query: expect.objectContaining({
+                groupBy: ['release'],
+              }),
+            })
+          );
+        });
+      }
+    );
 
     it('does not add a release grouping to the table if a group by is set', async () => {
       mockQuery = {
