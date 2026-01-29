5db5071711952c8ef5b9ca7b11afd0514f4e8f52
Katie Byers <katie.byers@sentry.io>
2026-01-29 11:40:57 -0800

    ref(grouping): Improve hex parameterization in messages (#107204)
    This makes two improvements to the way we parameterize hex values when grouping on message.
    
    Currently, we only parameterize values with capital A-F if they're preceded by the `0x` prefix, and we only parameterize prefix-less values if they're exactly 8 or 16 digits long. This expands our regexes to include all values with capital A-F (whether or not there's a prefix) and loosens the length requirement for prefix-less values to include anything 16 digits or longer, up to 64 digits. (8-digit values are also still included.)

--- src/sentry/grouping/parameterization.py
+++ src/sentry/grouping/parameterization.py
@@ -167,8 +167,9 @@ DEFAULT_PARAMETERIZATION_REGEXES = [
             # the prefix pretty much guarantees it's hex).
             (\b0[xX][0-9a-fA-F]+\b) |
 
-            # Hex value without `0x/0X` prefix (including a number, and either 8 or 16 digits -
-            # we're more conservative here on both scores in order to reduce false positives).
+            # Hex value without `0x/0X` prefix (including a number, either 8 or 16-64 digits, and
+            # either all uppercase or all lowercase - we're more conservative here on all three
+            # scores in order to reduce false positives).
             #
             # Note: We use a lookahead for `0-9` but don't need one for `a-f/A-F` since if
             #   a) the value consists of nothing but potential hex digits, but
@@ -177,10 +178,12 @@ DEFAULT_PARAMETERIZATION_REGEXES = [
             # so the only thing we need the lookahead to guard against is it being all letters.
             #
             # Each regex consists of two parts:
-            # (?=.*[0-9])           The aforementioned lookahead - at least one `0-9` is present
-            # [0-9a-f]{8/16}        8 or 16 hex characters
+            # (?=.*[0-9])             The aforementioned lookahead - at least one `0-9` is present
+            # [0-9a-f/A-F]{8/16,64}   8 or 16-64 hex characters
             (\b(?=.*[0-9])[0-9a-f]{8}\b) |
-            (\b(?=.*[0-9])[0-9a-f]{16}\b)
+            (\b(?=.*[0-9])[0-9a-f]{16,64}\b) |
+            (\b(?=.*[0-9])[0-9A-F]{8}\b) |
+            (\b(?=.*[0-9])[0-9A-F]{16,64}\b)
         """,
     ),
     ParameterizationRegex(name="float", raw_pattern=r"""-\d+\.\d+\b | \b\d+\.\d+\b"""),
