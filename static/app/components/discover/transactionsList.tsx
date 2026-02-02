5bc83eaaf4e3429267abde5ee0a0c5a6945781bd
Priscila Oliveira <priscila.oliveira@sentry.io>
2026-02-02 13:43:59 +0100

    feat(dynamically-sampling): Remove Get Samples button from UI (#106953)
    closes
    https://linear.app/getsentry/issue/TET-1590/retire-get-samples-button-in-frontend

--- static/app/components/discover/transactionsList.tsx
+++ static/app/components/discover/transactionsList.tsx
@@ -8,14 +8,12 @@ import {OverlayTrigger} from '@sentry/scraps/overlayTrigger';
 
 import GuideAnchor from 'sentry/components/assistant/guideAnchor';
 import DiscoverButton from 'sentry/components/discoverButton';
-import {InvestigationRuleCreation} from 'sentry/components/dynamicSampling/investigationRule';
 import type {CursorHandler} from 'sentry/components/pagination';
 import Pagination from 'sentry/components/pagination';
 import {t} from 'sentry/locale';
 import {space} from 'sentry/styles/space';
 import type {Organization} from 'sentry/types/organization';
 import {browserHistory} from 'sentry/utils/browserHistory';
-import {parseCursor} from 'sentry/utils/cursor';
 import {DemoTourElement, DemoTourStep} from 'sentry/utils/demoMode/demoTours';
 import type {TableDataRow} from 'sentry/utils/discover/discoverQuery';
 import DiscoverQuery from 'sentry/utils/discover/discoverQuery';
@@ -127,7 +125,6 @@ type Props = {
   handleOpenInDiscoverClick?: (e: React.MouseEvent<Element>) => void;
   referrer?: string;
   showTransactions?: TransactionFilterOptions;
-  supportsInvestigationRule?: boolean;
   /**
    * A list of preferred table headers to use over the field names.
    */
@@ -268,14 +265,10 @@ class _TransactionsList extends Component<Props> {
   }
 
   renderHeader({
-    cursor,
     numSamples,
-    supportsInvestigationRule,
     view,
   }: {
     numSamples: number | null | undefined;
-    cursor?: string | undefined;
-    supportsInvestigationRule?: boolean;
     view?: DomainView;
   }): React.ReactNode {
     const {
@@ -287,11 +280,8 @@ class _TransactionsList extends Component<Props> {
       handleOpenInDiscoverClick,
       showTransactions,
       breakdown,
-      eventView,
     } = this.props;
-    const cursorOffset = parseCursor(cursor)?.offset ?? 0;
     numSamples = numSamples ?? null;
-    const totalNumSamples = numSamples === null ? null : numSamples + cursorOffset;
     return (
       <Fragment>
         <div>
@@ -304,15 +294,6 @@ class _TransactionsList extends Component<Props> {
             onChange={opt => handleDropdownChange(opt.value)}
           />
         </div>
-        {supportsInvestigationRule && (
-          <InvestigationRuleWrapper>
-            <InvestigationRuleCreation
-              buttonProps={{size: 'xs'}}
-              eventView={eventView}
-              numSamples={totalNumSamples}
-            />
-          </InvestigationRuleWrapper>
-        )}
         {!this.isTrend() &&
           (handleOpenAllEventsClick ? (
             <GuideAnchor target="release_transactions_open_in_transaction_events">
@@ -420,8 +401,6 @@ class _TransactionsList extends Component<Props> {
             tableData={tableData}
             header={this.renderHeader({
               numSamples: tableData?.data?.length ?? null,
-              supportsInvestigationRule: this.props.supportsInvestigationRule,
-              cursor,
               view: domainViewFilters?.view,
             })}
           />
@@ -471,7 +450,6 @@ class _TransactionsList extends Component<Props> {
             paginationCursorSize="sm"
             header={this.renderHeader({
               numSamples: null,
-              supportsInvestigationRule: false,
               view: domainViewFilters?.view,
             })}
             titles={['transaction', 'percentage', 'difference']}
@@ -511,10 +489,6 @@ const Header = styled('div')`
 
 const StyledPagination = styled(Pagination)`
   margin: 0 0 0 ${space(1)};
-`;
-
-const InvestigationRuleWrapper = styled('div')`
-  margin-right: ${space(1)};
 `;
 
 function TransactionsList(
