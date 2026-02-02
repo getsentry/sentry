372bbce122d3c99ca292ec96eb5f2e9abe9aa05f
nsdeschenes <nicholas.deschenes@sentry.io>
2026-02-02 14:17:19 -0400

    :fire: Remove metric from type

--- static/app/views/explore/queryParams/crossEvent.ts
+++ static/app/views/explore/queryParams/crossEvent.ts
@@ -2,7 +2,7 @@ import type {Location} from 'history';
 
 import {defined} from 'sentry/utils';
 
-export type CrossEventType = 'logs' | 'spans' | 'metrics';
+export type CrossEventType = 'logs' | 'spans' /* | 'metrics' */;
 
 export interface CrossEvent {
   query: string;
