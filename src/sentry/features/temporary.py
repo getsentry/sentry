2303d0a409b2306a05f44e6022f30ca5e3e266d9
srest2021 <sofia.rest@sentry.io>
2026-02-02 10:19:54 -0800

    make feature flag permanent

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -74,8 +74,6 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:code-review-run-per-commit", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enabled for orgs that participated in the code review beta
     manager.add("organizations:code-review-beta", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
-    # Enable skipping non-essential GitHub API calls
-    manager.add("organizations:github-rate-limit-sensitive", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
     # Enables Prevent Test Analytics
     manager.add("organizations:prevent-test-analytics", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable the improved command menu (Cmd+K)
