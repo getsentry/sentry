63bcb95d058da3562a1fed9fca2337a9712e84dd
Mihir Mavalankar <mihir.d.mavalankar@gmail.com>
2026-02-03 10:18:31 -0800

    comment update

--- src/sentry/tasks/post_process.py
+++ src/sentry/tasks/post_process.py
@@ -1658,7 +1658,7 @@ def kick_off_seer_automation(job: PostProcessJob) -> None:
             if group.seer_autofix_last_triggered is not None:
                 return
 
-            # Don't run automation on old issues
+            # Don't run automation on old issues - TODO: Remove this when all orgs are on Seer new pricing
             if group.first_seen < (timezone.now() - timedelta(days=14)):
                 return
 
