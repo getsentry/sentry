37c97c41e3507e5f513d834eabe5a0997488b3f7
Kev <kevan.fisher@sentry.io>
2026-02-03 14:28:15 -0500

    knip

--- static/app/views/explore/metrics/metricToolbar/metricOptionLabel.tsx
+++ static/app/views/explore/metrics/metricToolbar/metricOptionLabel.tsx
@@ -1,5 +1,4 @@
 import {Tag} from '@sentry/scraps/badge/tag';
-import {Flex} from '@sentry/scraps/layout';
 
 import type {TraceMetricTypeValue} from 'sentry/views/explore/metrics/types';
 
@@ -8,18 +7,4 @@ export function MetricTypeBadge({metricType}: {metricType: TraceMetricTypeValue}
     return null;
   }
   return <Tag variant="muted">{metricType}</Tag>;
-}
-
-interface MetricOptionLabelProps {
-  label: string;
-  metricType?: TraceMetricTypeValue;
-}
-
-export function MetricOptionLabel({label, metricType}: MetricOptionLabelProps) {
-  return (
-    <Flex gap="sm" align="center">
-      <span>{label}</span>
-      {metricType ? <MetricTypeBadge metricType={metricType} /> : null}
-    </Flex>
-  );
 }
