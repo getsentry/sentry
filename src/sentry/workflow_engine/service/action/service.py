c56596bec032c3444816e35e670b7f4993a3290e
Colleen O'Rourke <colleen@sentry.io>
2026-02-04 14:09:55 -0800

    missed a spot

--- src/sentry/workflow_engine/service/action/service.py
+++ src/sentry/workflow_engine/service/action/service.py
@@ -42,7 +42,7 @@ class ActionService(RpcService):
         organization_id: int,
         status: int,
         sentry_app_install_uuid: str,
-        sentry_app_id: int | None,
+        sentry_app_id: int | None = None,
     ) -> None:
         pass
 
