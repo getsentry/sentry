0c961093822e111a0ea6eba2d51a4fad1e636d87
Colleen O'Rourke <colleen@sentry.io>
2026-02-03 16:43:43 -0800

    don't pass 0

--- src/sentry/sentry_apps/models/sentry_app_installation.py
+++ src/sentry/sentry_apps/models/sentry_app_installation.py
@@ -164,7 +164,7 @@ class SentryAppInstallation(ReplicatedControlModel, ParanoidModel):
                 object_identifier=self.id,
                 category=OutboxCategory.SENTRY_APP_INSTALLATION_DELETE,
                 region_name=region_name,
-                payload={"uuid": self.uuid, "sentry_app_id": self.sentry_app_id or 0},
+                payload={"uuid": self.uuid, "sentry_app_id": self.sentry_app_id},
             )
             for region_name in find_all_region_names()
         ]
