bc55bf7b76d5161d249709230250c63baddbdb9e
Dominik Buszowiecki <DominikB2014@gmail.com>
2026-02-04 16:01:31 -0500

    build basic frontend overview page

---
+++ static/app/views/insights/pages/frontend/utils/useHasPlatformizedFrontendOverview.ts
@@ -0,0 +1,8 @@
+import useOrganization from 'sentry/utils/useOrganization';
+
+export default function useHasPlatformizedFrontendOverview() {
+  const organization = useOrganization();
+
+  return true;
+  return organization.features.includes('insights-frontend-overview-dashboard-migration');
+}
