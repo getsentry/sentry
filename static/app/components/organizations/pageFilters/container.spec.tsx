38695abbf07dbf0383c45525dbbac45360a745cb
nsdeschenes <nicholas.deschenes@sentry.io>
2026-02-04 13:12:39 -0400

    :white_check_mark: Update tests

--- static/app/components/organizations/pageFilters/container.spec.tsx
+++ static/app/components/organizations/pageFilters/container.spec.tsx
@@ -491,6 +491,44 @@ describe('PageFiltersContainer', () => {
       );
     });
 
+    it('keeps absolute range when within maxPickableDays on decrease', async () => {
+      const start = moment().subtract(2, 'days').format('YYYY-MM-DDTHH:mm:ss');
+      const end = moment().subtract(1, 'days').format('YYYY-MM-DDTHH:mm:ss');
+
+      const {rerender} = render(<PageFiltersContainer maxPickableDays={30} />, {
+        organization,
+        initialRouterConfig: {
+          location: {
+            pathname: '/organizations/org-slug/test/',
+            query: {start, end},
+          },
+          route: '/organizations/:orgId/test/',
+        },
+      });
+
+      await waitFor(() =>
+        expect(PageFiltersStore.getState().selection.datetime.period).toBeNull()
+      );
+
+      const initialDatetime = PageFiltersStore.getState().selection.datetime;
+
+      rerender(<PageFiltersContainer maxPickableDays={7} />);
+
+      await waitFor(() =>
+        expect(PageFiltersStore.getState().selection.datetime.period).toBeNull()
+      );
+      await waitFor(() =>
+        expect(PageFiltersStore.getState().selection.datetime.start).toEqual(
+          initialDatetime.start
+        )
+      );
+      await waitFor(() =>
+        expect(PageFiltersStore.getState().selection.datetime.end).toEqual(
+          initialDatetime.end
+        )
+      );
+    });
+
     it('does not use maxPickableDays if the query parms do not exceed it', async () => {
       const {router} = render(<PageFiltersContainer maxPickableDays={7} />, {
         organization,
