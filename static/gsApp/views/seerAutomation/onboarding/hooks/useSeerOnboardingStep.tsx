5da91d68ba46363a8e8529c289329ab9ebb991be
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-02-05 14:45:53 -0800

    need to check for autofix||needsConfigReminder before getting to the final step

--- static/gsApp/views/seerAutomation/onboarding/hooks/useSeerOnboardingStep.tsx
+++ static/gsApp/views/seerAutomation/onboarding/hooks/useSeerOnboardingStep.tsx
@@ -16,15 +16,18 @@ export function useSeerOnboardingStep() {
       needsConfigReminder,
     } = data;
 
-    if (isSeerConfigured) {
-      initialStep = Steps.WRAP_UP; // Next steps
-    } else if (!hasSupportedScmIntegration) {
+    // Once the org has setup code-review then we'll stop bugging them.
+    const shouldShowConfigReminder = needsConfigReminder && !isCodeReviewEnabled;
+
+    if (!hasSupportedScmIntegration) {
       initialStep = Steps.CONNECT_GITHUB; // Connect GitHub
     } else if (!isCodeReviewEnabled) {
       initialStep = Steps.SETUP_CODE_REVIEW; // Setup Code Review
-    } else if (!isAutofixEnabled || needsConfigReminder) {
+    } else if (!isAutofixEnabled || shouldShowConfigReminder) {
       // This shouldn't happen because `isSeerConfigured` = `isCodeReviewEnabled` OR `isAutofixEnabled`
       initialStep = Steps.SETUP_ROOT_CAUSE_ANALYSIS; // Setup Root Cause Analysis
+    } else if (isSeerConfigured) {
+      initialStep = Steps.WRAP_UP; // Next steps
     }
   }
 
