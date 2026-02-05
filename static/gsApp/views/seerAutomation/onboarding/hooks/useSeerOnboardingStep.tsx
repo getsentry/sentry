0ef454e116ae906b21f8cc4d99298079a163b244
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-02-05 15:06:15 -0800

    ok, best logic

--- static/gsApp/views/seerAutomation/onboarding/hooks/useSeerOnboardingStep.tsx
+++ static/gsApp/views/seerAutomation/onboarding/hooks/useSeerOnboardingStep.tsx
@@ -12,22 +12,22 @@ export function useSeerOnboardingStep() {
       hasSupportedScmIntegration,
       isCodeReviewEnabled,
       isAutofixEnabled,
-      isSeerConfigured,
       needsConfigReminder,
     } = data;
 
     // Once the org has setup code-review then we'll stop bugging them.
     const shouldShowConfigReminder = needsConfigReminder && !isCodeReviewEnabled;
 
-    if (!hasSupportedScmIntegration) {
+    if (hasSupportedScmIntegration) {
+      if (isAutofixEnabled && isCodeReviewEnabled) {
+        initialStep = Steps.WRAP_UP; // Next steps
+      } else if (!isAutofixEnabled || shouldShowConfigReminder) {
+        initialStep = Steps.SETUP_ROOT_CAUSE_ANALYSIS; // Setup Root Cause Analysis
+      } else if (!isCodeReviewEnabled) {
+        initialStep = Steps.SETUP_CODE_REVIEW; // Setup Code Review
+      }
+    } else {
       initialStep = Steps.CONNECT_GITHUB; // Connect GitHub
-    } else if (!isCodeReviewEnabled) {
-      initialStep = Steps.SETUP_CODE_REVIEW; // Setup Code Review
-    } else if (!isAutofixEnabled || shouldShowConfigReminder) {
-      // This shouldn't happen because `isSeerConfigured` = `isCodeReviewEnabled` OR `isAutofixEnabled`
-      initialStep = Steps.SETUP_ROOT_CAUSE_ANALYSIS; // Setup Root Cause Analysis
-    } else if (isSeerConfigured) {
-      initialStep = Steps.WRAP_UP; // Next steps
     }
   }
 
