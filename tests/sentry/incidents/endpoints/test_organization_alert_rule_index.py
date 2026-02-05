0d82079368bf49a76af34ca3b452cc98126d50ad
nikkikapadia <nikki.kapadia@sentry.io>
2026-02-05 14:07:23 -0500

    another test fix

--- tests/sentry/incidents/endpoints/test_organization_alert_rule_index.py
+++ tests/sentry/incidents/endpoints/test_organization_alert_rule_index.py
@@ -1942,7 +1942,7 @@ class AlertRuleCreateEndpointTest(AlertRuleIndexBase, SnubaTestCase):
             resp = self.get_error_response(self.organization.slug, status_code=400, **data)
         assert (
             resp.data[0]
-            == "server_weighted extrapolation mode is not supported for new alerts. Allowed modes are: client_and_server_weighted, unknow."
+            == "server_weighted extrapolation mode is not supported for new alerts. Allowed modes are: client_and_server_weighted, unknown."
         )
 
     def test_owner_team_not_member_denied(self) -> None:
