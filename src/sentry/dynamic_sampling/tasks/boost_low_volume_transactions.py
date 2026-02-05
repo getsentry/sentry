cd5e17a32b69d04521cbb62888c353238f47f4aa
Simon Hellmayr <simon.hellmayr@sentry.io>
2026-02-05 13:04:52 +0100

    raise limit because we do multiple queries when this is on

--- src/sentry/dynamic_sampling/tasks/boost_low_volume_transactions.py
+++ src/sentry/dynamic_sampling/tasks/boost_low_volume_transactions.py
@@ -92,7 +92,7 @@ def _get_segments_org_ids() -> set[int]:
 @instrumented_task(
     name="sentry.dynamic_sampling.tasks.boost_low_volume_transactions",
     namespace=telemetry_experience_tasks,
-    processing_deadline_duration=6 * 60 + 5,
+    processing_deadline_duration=10 * 60 + 5,
     retry=Retry(times=5, delay=5),
     silo_mode=SiloMode.REGION,
 )
