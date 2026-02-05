a204d6eafbd8e153ee568e8c3638dc3f7c2c2900
Nate Moore <natemoo-re@users.noreply.github.com>
2026-02-05 12:59:41 -0500

    feat(lint): catch `theme.tokens.background` violations (#107452)
    enable `@sentry/scraps/use-semantic-token` rule for
    `theme.tokens.background`
    
    blocked by #107451, closes DE-782
    
    ---------
    
    Co-authored-by: getsantry[bot] <66042841+getsantry[bot]@users.noreply.github.com>

--- static/app/components/core/avatar/letterAvatar.tsx
+++ static/app/components/core/avatar/letterAvatar.tsx
@@ -41,7 +41,8 @@ const LetterAvatarComponent = styled('svg')<LetterAvatarProps>`
   rect {
     fill: ${props =>
       props.suggested
-        ? props.theme.tokens.background.primary
+        ? // eslint-disable-next-line @sentry/scraps/use-semantic-token
+          props.theme.tokens.background.primary
         : getColor(props.identifier, props.theme).background};
   }
 
