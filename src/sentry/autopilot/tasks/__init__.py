73a693ace064a9d3d4fb7e2f9485923e716921bb
Arthur Knaus <arthur.knaus@sentry.io>
2026-02-05 11:47:28 +0100

    ref(autopilot): Update task imports for split modules
    Update TASKWORKER_IMPORTS to point to the individual detector modules
    instead of the package. Remove unnecessary re-exports from __init__.py
    since tasks are discovered by their registered names.

--- src/sentry/autopilot/tasks/__init__.py
+++
@@ -1,9 +0,0 @@
-from sentry.autopilot.tasks.missing_sdk_integration import run_missing_sdk_integration_detector
-from sentry.autopilot.tasks.sdk_update import run_sdk_update_detector
-from sentry.autopilot.tasks.trace_instrumentation import run_trace_instrumentation_detector
-
-__all__ = [
-    "run_missing_sdk_integration_detector",
-    "run_sdk_update_detector",
-    "run_trace_instrumentation_detector",
-]
