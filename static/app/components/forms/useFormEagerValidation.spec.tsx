60004895b833a27a45f462e4e64ea507060a8e59
Jay Goss <jaygoss@gmail.com>
2026-02-05 17:11:22 -0600

    test(forms): Add unit tests for useFormEagerValidation hook

---
+++ static/app/components/forms/useFormEagerValidation.spec.tsx
@@ -0,0 +1,56 @@
+import {act, renderHook} from 'sentry-test/reactTestingLibrary';
+
+import FormModel from 'sentry/components/forms/model';
+
+import {useFormEagerValidation} from './useFormEagerValidation';
+
+describe('useFormEagerValidation', () => {
+  let formModel: FormModel;
+
+  beforeEach(() => {
+    formModel = new FormModel();
+    jest.spyOn(formModel, 'validateForm');
+  });
+
+  it('does not validate during initial render', () => {
+    renderHook(() => useFormEagerValidation(formModel));
+
+    expect(formModel.validateForm).not.toHaveBeenCalled();
+  });
+
+  it('validates on blur after initialization', () => {
+    const {result} = renderHook(() => useFormEagerValidation(formModel));
+
+    act(() => result.current.onBlur());
+
+    expect(formModel.validateForm).toHaveBeenCalledTimes(1);
+  });
+
+  it('validates on every blur', () => {
+    const {result} = renderHook(() => useFormEagerValidation(formModel));
+
+    act(() => result.current.onBlur());
+    act(() => result.current.onBlur());
+    act(() => result.current.onBlur());
+
+    expect(formModel.validateForm).toHaveBeenCalledTimes(3);
+  });
+
+  it('validates on first field change', () => {
+    const {result} = renderHook(() => useFormEagerValidation(formModel));
+
+    act(() => result.current.onFieldChange());
+
+    expect(formModel.validateForm).toHaveBeenCalledTimes(1);
+  });
+
+  it('does not validate on subsequent field changes', () => {
+    const {result} = renderHook(() => useFormEagerValidation(formModel));
+
+    act(() => result.current.onFieldChange());
+    act(() => result.current.onFieldChange());
+    act(() => result.current.onFieldChange());
+
+    expect(formModel.validateForm).toHaveBeenCalledTimes(1);
+  });
+});
