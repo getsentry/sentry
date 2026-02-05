ea952c785813e53e6388ce739dbf688c86e614b6
Scott Cooper <scttcper@gmail.com>
2026-02-05 10:06:41 -0800

    revert

--- static/app/components/events/interfaces/frame/defaultTitle/index.tsx
+++ static/app/components/events/interfaces/frame/defaultTitle/index.tsx
@@ -270,7 +270,4 @@ const StyledGroupingIndicator = styled(GroupingIndicator)`
 
 const StyledQuestionTooltip = styled(QuestionTooltip)`
   margin-left: ${space(0.5)};
-  display: inline-flex;
-  align-items: center;
-  vertical-align: middle;
 `;
