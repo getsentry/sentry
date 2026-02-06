2c8972a4399467fc3cf5684f2287b1a25e5872c7
Colleen O'Rourke <colleen@sentry.io>
2026-02-06 10:49:03 -0800

    handle possibility of no org id

--- src/sentry/workflow_engine/service/action/impl.py
+++ src/sentry/workflow_engine/service/action/impl.py
@@ -74,12 +74,19 @@ class DatabaseBackedActionService(ActionService):
                 type=Action.Type.SENTRY_APP,
                 dataconditiongroupaction__condition_group__organization_id=organization_id,
             )
-        installation_uuid_actions = Action.objects.filter(
-            config__target_identifier=sentry_app_install_uuid,
-            type=Action.Type.SENTRY_APP,
-            config__sentry_app_identifier=SentryAppIdentifier.SENTRY_APP_INSTALLATION_UUID,
-            dataconditiongroupaction__condition_group__organization_id=organization_id,
-        )
+        if organization_id:
+            installation_uuid_actions = Action.objects.filter(
+                config__target_identifier=sentry_app_install_uuid,
+                type=Action.Type.SENTRY_APP,
+                config__sentry_app_identifier=SentryAppIdentifier.SENTRY_APP_INSTALLATION_UUID,
+                dataconditiongroupaction__condition_group__organization_id=organization_id,
+            )
+        else:
+            installation_uuid_actions = Action.objects.filter(
+                config__target_identifier=sentry_app_install_uuid,
+                type=Action.Type.SENTRY_APP,
+                config__sentry_app_identifier=SentryAppIdentifier.SENTRY_APP_INSTALLATION_UUID,
+            )
 
         actions = sentry_app_id_actions | installation_uuid_actions
         if actions:
