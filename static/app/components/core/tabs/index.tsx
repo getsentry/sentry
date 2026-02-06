9f0c7d5641621d90eb40795581f4028b6e209a49
Nate Moore <nate.moore@sentry.io>
2026-02-06 11:14:37 -0500

    fix(knip): do not export tabs props

--- static/app/components/core/tabs/index.tsx
+++ static/app/components/core/tabs/index.tsx
@@ -1,5 +1,5 @@
-export {Tabs, TabStateProvider, type TabsProps} from './tabs';
+export {Tabs, TabStateProvider} from './tabs';
 
-export {TabList, type TabListProps} from './tabList';
+export {TabList} from './tabList';
 export {type TabListItemProps} from './item';
 export {TabPanels} from './tabPanels';
