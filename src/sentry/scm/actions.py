4477f825bb5defe1c4a60761d289fc64229a7509
Alex Sohn <alex.sohn@sentry.io>
2026-02-06 20:05:41 -0500

    Fix docstring typo and stale type annotation in SourceCodeManager

--- src/sentry/scm/actions.py
+++ src/sentry/scm/actions.py
@@ -49,12 +49,12 @@ class SourceCodeManager:
         :param referrer: Referrers specify who made a request. Referrers are used to log usage
         metrics and are used to allocate service-provider quota for critical products.
         :type referrer: Referrer
-        :param fetch_repository: Translates a "RepositoryId" type into a "Respository" type.
+        :param fetch_repository: Translates a "RepositoryId" type into a "Repository" type.
         Fetches a repository from the store and validates it has a correct state.
         :type fetch_repository: Callable[[int, RepositoryId], Repository | None]
         :param fetch_service_provider: Translates a "Repository" type into a "Provider" instance.
         Abstracts integration lookup and API client acquisition.
-        :type fetch_service_provider: Callable[[Repository], Provider]
+        :type fetch_service_provider: Callable[[int, int], Provider]
         """
         self.organization_id = organization_id
         self.repository_id = repository_id
