dd98834b6d6182c1b9d0aead003022deb6581087
Abdullah Khan <60121741+Abdkhan14@users.noreply.github.com>
2026-02-09 15:30:36 -0500

    feat(uptime-assertion-failure-tree) Adding snapshot tests for tree class (#107783)
    Makes testing the model much simpler:
    <img width="1255" height="607" alt="Screenshot 2026-02-06 at 1 02 30 PM"
    src="https://github.com/user-attachments/assets/9ca0f06a-e814-467a-a715-7b06d50e021e"
    />
    
    Also the snapshots let you visualize what you are expecting in the
    tests:
    <img width="505" height="195" alt="Screenshot 2026-02-06 at 1 03 41 PM"
    src="https://github.com/user-attachments/assets/158ad469-014a-4be3-945a-83bd7d906ec1"
    />
    
    ---------
    
    Co-authored-by: Abdullah Khan <abdullahkhan@PG9Y57YDXQ.local>

--- static/app/views/alerts/rules/uptime/assertions/assertionFailure/models/andOpTreeNode.tsx
+++ static/app/views/alerts/rules/uptime/assertions/assertionFailure/models/andOpTreeNode.tsx
@@ -6,6 +6,10 @@ import type {AndOp} from 'sentry/views/alerts/rules/uptime/types';
 import {TreeNode} from './treeNode';
 
 export class AndOpTreeNode extends TreeNode<AndOp> {
+  printNode(): string {
+    return `AND - ${this.id}`;
+  }
+
   renderRow(): ReactNode {
     return <AndOpRow node={this} />;
   }
