5e1f824f68c43778d04710a02ad783df95489ef8
Colleen O'Rourke <colleen@sentry.io>
2026-02-09 13:13:14 -0800

    chore(ACI): Make sentry_app_installation_uuid optional

--- src/sentry/workflow_engine/service/action/impl.py
+++ src/sentry/workflow_engine/service/action/impl.py
@@ -33,10 +33,11 @@ class DatabaseBackedActionService(ActionService):
         *,
         organization_id: int,
         status: int,
-        sentry_app_install_uuid: str,
+        sentry_app_install_uuid: str | None = None,
         sentry_app_id: int | None = None,
     ) -> None:
         sentry_app_id_actions = Action.objects.none()
+        installation_uuid_actions = Action.objects.none()
 
         if sentry_app_id:
             sentry_app_id_actions = Action.objects.filter(
@@ -45,12 +46,13 @@ class DatabaseBackedActionService(ActionService):
                 type=Action.Type.SENTRY_APP,
                 dataconditiongroupaction__condition_group__organization_id=organization_id,
             )
-        installation_uuid_actions = Action.objects.filter(
-            config__sentry_app_identifier=SentryAppIdentifier.SENTRY_APP_INSTALLATION_UUID,
-            config__target_identifier=sentry_app_install_uuid,
-            type=Action.Type.SENTRY_APP,
-            dataconditiongroupaction__condition_group__organization_id=organization_id,
-        )
+        if sentry_app_install_uuid:
+            installation_uuid_actions = Action.objects.filter(
+                config__sentry_app_identifier=SentryAppIdentifier.SENTRY_APP_INSTALLATION_UUID,
+                config__target_identifier=sentry_app_install_uuid,
+                type=Action.Type.SENTRY_APP,
+                dataconditiongroupaction__condition_group__organization_id=organization_id,
+            )
 
         actions = sentry_app_id_actions | installation_uuid_actions
         if actions:
@@ -61,11 +63,12 @@ class DatabaseBackedActionService(ActionService):
         *,
         region_name: str,
         status: int,
-        sentry_app_install_uuid: str,
+        sentry_app_install_uuid: str | None = None,
         organization_id: int | None = None,
         sentry_app_id: int | None = None,
     ) -> None:
         sentry_app_id_actions = Action.objects.none()
+        installation_uuid_actions = Action.objects.none()
 
         if sentry_app_id and organization_id:
             sentry_app_id_actions = Action.objects.filter(
@@ -74,14 +77,14 @@ class DatabaseBackedActionService(ActionService):
                 type=Action.Type.SENTRY_APP,
                 dataconditiongroupaction__condition_group__organization_id=organization_id,
             )
-        if organization_id:
+        if sentry_app_install_uuid and organization_id:
             installation_uuid_actions = Action.objects.filter(
                 config__target_identifier=sentry_app_install_uuid,
                 type=Action.Type.SENTRY_APP,
                 config__sentry_app_identifier=SentryAppIdentifier.SENTRY_APP_INSTALLATION_UUID,
                 dataconditiongroupaction__condition_group__organization_id=organization_id,
             )
-        else:
+        elif sentry_app_install_uuid:
             installation_uuid_actions = Action.objects.filter(
                 config__target_identifier=sentry_app_install_uuid,
                 type=Action.Type.SENTRY_APP,
