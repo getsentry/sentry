877d9cfad2c4bc927279d0da9c3c96380761ff30
JonasBa <jonas@badalic.com>
2026-02-10 13:26:22 -0800

    ref(pagefilters) move isSelectionEqual outside of utils

--- static/app/components/pageFilters/utils.tsx
+++ static/app/components/pageFilters/utils.tsx
@@ -1,6 +1,5 @@
 import type {Location} from 'history';
 import identity from 'lodash/identity';
-import isEqual from 'lodash/isEqual';
 import pick from 'lodash/pick';
 import pickBy from 'lodash/pickBy';
 
@@ -33,32 +32,4 @@ export function getDefaultSelection(): PageFilters {
  */
 export function extractSelectionParameters(query: Location['query']) {
   return pickBy(pick(query, Object.values(URL_PARAM)), identity);
-}
-
-/**
- * Compare the non-utc values of two selections.
- * Useful when re-fetching data based on page filters changing.
- *
- * utc is not compared as there is a problem somewhere in the selection
- * data flow that results in it being undefined | null | boolean instead of null | boolean.
- * The additional undefined state makes this function just as unreliable as isEqual(selection, other)
- */
-export function isSelectionEqual(selection: PageFilters, other: PageFilters): boolean {
-  if (
-    !isEqual(selection.projects, other.projects) ||
-    !isEqual(selection.environments, other.environments)
-  ) {
-    return false;
-  }
-
-  // Use string comparison as we aren't interested in the identity of the datetimes.
-  if (
-    selection.datetime.period !== other.datetime.period ||
-    selection.datetime.start?.toString() !== other.datetime.start?.toString() ||
-    selection.datetime.end?.toString() !== other.datetime.end?.toString()
-  ) {
-    return false;
-  }
-
-  return true;
 }
