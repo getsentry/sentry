9a10bf2a58e8b008e76fb3c1f69c7d0de51a35ea
Stefan Pölz <38893694+Flash0ver@users.noreply.github.com>
2026-02-10 14:22:17 +0100

    ref: simplify .NET SDK version selector for getting started snippets

--- static/app/gettingStartedDocs/dotnet/utils.tsx
+++ static/app/gettingStartedDocs/dotnet/utils.tsx
@@ -1,22 +1,10 @@
 import type {DocsParams} from 'sentry/components/onboarding/gettingStartedDoc/types';
 import {getPackageVersion} from 'sentry/utils/gettingStartedDocs/getPackageVersion';
 
-export const getInstallSnippetPackageManager = (params: DocsParams) => {
-  let version = '6.0.0';
-  if (params.isMetricsSelected) {
-    version = '6.1.0';
-  }
+// Sentry SDK for .NET 6.1.0 adds initial experimental support for Metrics
+export const getInstallSnippetPackageManager = (params: DocsParams) => `
+Install-Package Sentry -Version ${getPackageVersion(params, 'sentry.dotnet', '6.1.0')}`;
 
-  return `
-Install-Package Sentry -Version ${getPackageVersion(params, 'sentry.dotnet', version)}`;
-};
-
-export const getInstallSnippetCoreCli = (params: DocsParams) => {
-  let version = '6.0.0';
-  if (params.isMetricsSelected) {
-    version = '6.1.0';
-  }
-
-  return `
-dotnet add package Sentry -v ${getPackageVersion(params, 'sentry.dotnet', version)}`;
-};
+// Sentry SDK for .NET 6.1.0 adds initial experimental support for Metrics
+export const getInstallSnippetCoreCli = (params: DocsParams) => `
+dotnet add package Sentry -v ${getPackageVersion(params, 'sentry.dotnet', '6.1.0')}`;
