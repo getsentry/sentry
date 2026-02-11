159734eb392613d4becb14d9e53a7967912e6a7a
Nar Saynorath <nar.saynorath@sentry.io>
2026-02-11 10:39:28 -0500

    fix helper

--- static/app/views/explore/metrics/hooks/useHasMetricUnitsUI.tsx
+++ static/app/views/explore/metrics/hooks/useHasMetricUnitsUI.tsx
@@ -2,6 +2,5 @@ import useOrganization from 'sentry/utils/useOrganization';
 
 export function useHasMetricUnitsUI() {
   const organization = useOrganization();
-  return true;
   return organization.features.includes('tracemetrics-units-ui');
 }
