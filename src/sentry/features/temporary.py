0eef9b720aa9fa24fe79f263e41cabced0729cf8
Leander Rodrigues <leander.rodrigues@sentry.io>
2026-02-11 15:10:37 -0500

    chore(data-forwarding): Remove data-forwarding-revamp flag (#108065)
    It has been GA'd, code no longer references it. Requires deploying
    options automator PR first.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -126,10 +126,6 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:data-secrecy-v2", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable default anomaly detection metric monitor for new projects
     manager.add("organizations:default-anomaly-detector", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=False)
-    # Enables access to the global data forwarding features
-    # Note: organizations:data-forwarding is a permanent, plan-based flag that enables access to the actual feature.
-    #       This flag will only be used to access the new UI/UX and endpoints before release.
-    manager.add("organizations:data-forwarding-revamp-access", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enables synthesis of device.class in ingest
     manager.add("organizations:device-class-synthesis", OrganizationFeature, FeatureHandlerStrategy.INTERNAL, api_expose=False)
     # Enable device.class as a selectable column
