7986df2d66c9cd11d08838f27673a7cff1a73e22
Trevor Elkins <trevor@emergetools.com>
2026-02-12 14:09:24 -0500

    fix(frontend): include react config for our pipeline asset

--- src/sentry/pipeline/views/base.py
+++ src/sentry/pipeline/views/base.py
@@ -7,6 +7,7 @@ from django.http.request import HttpRequest
 from django.http.response import HttpResponseBase
 
 from sentry.utils import json
+from sentry.web.client_config import get_client_config
 from sentry.web.helpers import render_to_response
 
 
@@ -22,5 +23,9 @@ def render_react_view(
     return render_to_response(
         template="sentry/bases/react_pipeline.html",
         request=request,
-        context={"pipelineName": pipeline_name, "props": json.dumps(props)},
+        context={
+            "pipelineName": pipeline_name,
+            "props": json.dumps(props),
+            "react_config": get_client_config(request),
+        },
     )
