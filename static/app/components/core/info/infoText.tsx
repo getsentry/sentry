0408b4c2e0a08b18ca7e70ec2836cb92e5507273
TkDodo <dominik.dorfmeister@sentry.io>
2026-02-13 11:31:33 +0100

    fix: type parameters

--- static/app/components/core/info/infoText.tsx
+++ static/app/components/core/info/infoText.tsx
@@ -3,11 +3,15 @@ import styled from '@emotion/styled';
 import {Text, type TextProps} from '@sentry/scraps/text';
 import {Tooltip} from '@sentry/scraps/tooltip';
 
-type InfoTextProps = TextProps<'span'> & {
+type InfoTextProps<T extends 'span' | 'p' | 'label' | 'div'> = TextProps<T> & {
   title: React.ReactNode;
 };
 
-export function InfoText({title, children, ...textProps}: InfoTextProps) {
+export function InfoText<T extends 'span' | 'p' | 'label' | 'div' = 'span'>({
+  title,
+  children,
+  ...textProps
+}: InfoTextProps<T>) {
   return (
     <Tooltip title={title} skipWrapper isHoverable showUnderline>
       <StyledText {...textProps} tabIndex={0}>
