09d2703a27473d9dc78889538b379e16da2431f1
Dominik Dorfmeister <dominik.dorfmeister@sentry.io>
2026-02-13 09:06:52 +0100

    poc: new form system (#107142)
    Demonstrating a composable form system based on TanStack Form & zod.
    
    closes https://linear.app/getsentry/issue/DE-758/form-exploration

---
+++ static/app/components/core/form/field/inputField.tsx
@@ -0,0 +1,50 @@
+import {useAutoSaveContext} from '@sentry/scraps/form/autoSaveContext';
+import {type InputProps} from '@sentry/scraps/input';
+import {InputGroup} from '@sentry/scraps/input/inputGroup';
+import {Tooltip} from '@sentry/scraps/tooltip';
+
+import {BaseField, type BaseFieldProps} from './baseField';
+
+export function InputField({
+  onChange,
+  disabled,
+  ...props
+}: BaseFieldProps &
+  Omit<InputProps, 'value' | 'onChange' | 'onBlur' | 'disabled'> & {
+    onChange: (value: string) => void;
+    value: string;
+    disabled?: boolean | string;
+  }) {
+  const autoSaveContext = useAutoSaveContext();
+  const isDisabled = !!disabled || autoSaveContext?.status === 'pending';
+  const disabledReason = typeof disabled === 'string' ? disabled : undefined;
+
+  return (
+    <BaseField>
+      {(fieldProps, {indicator}) => {
+        const input = (
+          <InputGroup>
+            <InputGroup.Input
+              {...fieldProps}
+              {...props}
+              aria-disabled={isDisabled}
+              readOnly={isDisabled}
+              onChange={e => onChange(e.target.value)}
+            />
+            <InputGroup.TrailingItems>{indicator}</InputGroup.TrailingItems>
+          </InputGroup>
+        );
+
+        if (disabledReason) {
+          return (
+            <Tooltip skipWrapper title={disabledReason}>
+              {input}
+            </Tooltip>
+          );
+        }
+
+        return input;
+      }}
+    </BaseField>
+  );
+}
