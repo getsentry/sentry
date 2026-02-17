7a7c1cce59cc9b5180e9d03e6a0f4950a1f377dc
nikkikapadia <nikki.kapadia@sentry.io>
2026-02-17 15:05:27 -0500

    accomodate text widget in widget builder

--- static/app/views/dashboards/utils.tsx
+++ static/app/views/dashboards/utils.tsx
@@ -638,6 +638,7 @@ export const usesTimeSeriesData = (displayType?: DisplayType) => {
     DisplayType.TABLE,
     DisplayType.WHEEL,
     DisplayType.RAGE_AND_DEAD_CLICKS,
+    DisplayType.TEXT,
   ].includes(displayType);
 };
 
