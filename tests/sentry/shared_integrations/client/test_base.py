3e7a709b379c394da909a7f4e1665026e7d00eaa
Matt Duncan <matt.duncan@sentry.io>
2026-02-17 13:16:32 -0800

    perf(tests): Convert API client and silo tests to plain functions
    Convert test files that don't need DB access:
    - test_base.py (shared_integrations/client): 1.60s -> 0.10s
    - test_base.py (silo): 2.72s -> 1.19s

--- tests/sentry/shared_integrations/client/test_base.py
+++ tests/sentry/shared_integrations/client/test_base.py
@@ -1,84 +1,92 @@
 from unittest.mock import MagicMock, patch
 
+import pytest
 import responses
-from pytest import raises
 from requests import PreparedRequest, Request
 
 from sentry.exceptions import RestrictedIPAddress
 from sentry.net.http import Session
 from sentry.shared_integrations.client.base import BaseApiClient
 from sentry.shared_integrations.exceptions import ApiHostError
-from sentry.testutils.cases import TestCase
 from sentry.testutils.helpers.socket import override_blocklist
 
 
-class BaseApiClientTest(TestCase):
+class Client(BaseApiClient):
+    """Test client for BaseApiClient tests."""
+
+    integration_type = "integration"
+    integration_name = "base"
+
+
+@responses.activate
+@patch.object(BaseApiClient, "finalize_request", side_effect=lambda req: req)
+def test_finalize_request(mock_finalize_request) -> None:
     """
     The BaseApiClient was created after ApiClient, so many tests can be found
     there instead (tests/sentry/integrations/test_client.py)
     """
+    api_client = Client()
 
-    def setUp(self) -> None:
-        class Client(BaseApiClient):
-            integration_type = "integration"
-            integration_name = "base"
+    # Ensure finalize_request is called before all requests
+    get_response = responses.add(responses.GET, "https://example.com/get", json={})
+    assert not mock_finalize_request.called
+    assert get_response.call_count == 0
+    api_client.get("https://example.com/get")
+    assert mock_finalize_request.called
+    assert get_response.call_count == 1
 
-        self.api_client = Client()
+    # Ensure finalize_request can modify the request prior to sending
+    put_response = responses.add(responses.PUT, "https://example.com/put", json={})
+    assert put_response.call_count == 0
 
-    @responses.activate
-    @patch.object(BaseApiClient, "finalize_request", side_effect=lambda req: req)
-    def test_finalize_request(self, mock_finalize_request) -> None:
-        # Ensure finalize_request is called before all requests
-        get_response = responses.add(responses.GET, "https://example.com/get", json={})
-        assert not mock_finalize_request.called
-        assert get_response.call_count == 0
-        self.api_client.get("https://example.com/get")
-        assert mock_finalize_request.called
-        assert get_response.call_count == 1
+    class TestClient(BaseApiClient):
+        integration_type = "integration"
+        integration_name = "test"
 
-        # Ensure finalize_request can modify the request prior to sending
-        put_response = responses.add(responses.PUT, "https://example.com/put", json={})
-        assert put_response.call_count == 0
+        def finalize_request(self, prepared_request: PreparedRequest) -> PreparedRequest:
+            prepared_request.method = "PUT"
+            prepared_request.url = "https://example.com/put"
+            return prepared_request
 
-        class TestClient(BaseApiClient):
-            integration_type = "integration"
-            integration_name = "test"
+    # Method and url are overridden in TestClient.finalize_request
+    TestClient().get("https://example.com/get")
+    assert get_response.call_count == 1
+    assert put_response.call_count == 1
 
-            def finalize_request(self, prepared_request: PreparedRequest) -> PreparedRequest:
-                prepared_request.method = "PUT"
-                prepared_request.url = "https://example.com/put"
-                return prepared_request
 
-        # Method and url are overridden in TestClient.finalize_request
-        TestClient().get("https://example.com/get")
-        assert get_response.call_count == 1
-        assert put_response.call_count == 1
+@responses.activate
+def test__request_prepared_request() -> None:
+    api_client = Client()
 
-    @responses.activate
-    def test__request_prepared_request(self) -> None:
-        put_response = responses.add(responses.PUT, "https://example.com/put", json={})
-        prepared_request = Request(method="PUT", url="https://example.com/put").prepare()
-        # Client should use prepared request instead of using other params
-        assert put_response.call_count == 0
-        self.api_client.get("https://example.com/get", prepared_request=prepared_request)
-        assert put_response.call_count == 1
+    put_response = responses.add(responses.PUT, "https://example.com/put", json={})
+    prepared_request = Request(method="PUT", url="https://example.com/put").prepare()
+    # Client should use prepared request instead of using other params
+    assert put_response.call_count == 0
+    api_client.get("https://example.com/get", prepared_request=prepared_request)
+    assert put_response.call_count == 1
 
-    @responses.activate
-    @patch.object(BaseApiClient, "finalize_request", side_effect=lambda req: req)
-    @patch.object(Session, "send", side_effect=RestrictedIPAddress())
-    @override_blocklist("172.16.0.0/12")
-    def test_restricted_ip_address(self, mock_finalize_request, mock_session_send) -> None:
-        assert not mock_finalize_request.called
-        with raises(ApiHostError):
-            self.api_client.get("https://172.31.255.255")
-        assert mock_finalize_request.called
 
-    @patch.object(Session, "send")
-    def test_default_timeout(self, mock_session_send) -> None:
-        response = MagicMock()
-        response.status_code = 204
-        mock_session_send.return_value = response
+@responses.activate
+@patch.object(BaseApiClient, "finalize_request", side_effect=lambda req: req)
+@patch.object(Session, "send", side_effect=RestrictedIPAddress())
+@override_blocklist("172.16.0.0/12")
+def test_restricted_ip_address(mock_session_send, mock_finalize_request) -> None:
+    api_client = Client()
 
-        self.api_client.get("https://172.31.255.255")
-        assert mock_session_send.call_count == 1
-        assert mock_session_send.mock_calls[0].kwargs["timeout"] == 30
+    assert not mock_finalize_request.called
+    with pytest.raises(ApiHostError):
+        api_client.get("https://172.31.255.255")
+    assert mock_finalize_request.called
+
+
+@patch.object(Session, "send")
+def test_default_timeout(mock_session_send) -> None:
+    api_client = Client()
+
+    response = MagicMock()
+    response.status_code = 204
+    mock_session_send.return_value = response
+
+    api_client.get("https://172.31.255.255")
+    assert mock_session_send.call_count == 1
+    assert mock_session_send.mock_calls[0].kwargs["timeout"] == 30
--- tests/sentry/silo/test_base.py
+++ tests/sentry/silo/test_base.py
@@ -3,66 +3,63 @@ import django.urls
 from django.views.generic import RedirectView, TemplateView, View
 
 from sentry.api.base import Endpoint
-from sentry.testutils.cases import TestCase
 
 
-class SiloLimitCoverageTest(TestCase):
+def test_all_models_have_silo_limit_decorator() -> None:
     """Check that all subclasses have expected SiloLimit decorators."""
+    undecorated_model_classes = []
 
-    def test_all_models_have_silo_limit_decorator(self) -> None:
-        undecorated_model_classes = []
+    for model_class in django.apps.apps.get_models():
+        if model_class._meta.app_label == "sentry" and not hasattr(model_class._meta, "silo_limit"):
+            undecorated_model_classes.append(model_class)
 
-        for model_class in django.apps.apps.get_models():
-            if model_class._meta.app_label == "sentry" and not hasattr(
-                model_class._meta, "silo_limit"
-            ):
-                undecorated_model_classes.append(model_class)
+    assert len(undecorated_model_classes) == 0, (
+        "Model classes missing ModelSiloLimit: "
+        f"{', '.join(m.__name__ for m in undecorated_model_classes)}"
+    )
 
-        assert len(undecorated_model_classes) == 0, (
-            "Model classes missing ModelSiloLimit: "
-            f"{', '.join(m.__name__ for m in undecorated_model_classes)}"
-        )
 
-    def test_all_endpoints_have_silo_mode_decorator(self) -> None:
-        undecorated_endpoint_classes = []
-        undecorated_view_classes = []
-        undecorated_view_functions = []
+def test_all_endpoints_have_silo_mode_decorator() -> None:
+    """Check that all subclasses have expected SiloLimit decorators."""
+    undecorated_endpoint_classes = []
+    undecorated_view_classes = []
+    undecorated_view_functions = []
 
-        url_mappings = django.urls.get_resolver().reverse_dict.items()
-        for view_function, bindings in url_mappings:
-            view_class = getattr(view_function, "view_class", None)
-            if (
-                view_class
-                and issubclass(view_class, Endpoint)
-                and not hasattr(view_class, "silo_limit")
-            ):
-                undecorated_endpoint_classes.append(view_class)
-            elif (
-                view_class
-                and issubclass(view_class, View)
-                and view_class != RedirectView
-                and view_class != TemplateView
-                and not hasattr(view_class, "silo_limit")
-            ):
-                undecorated_view_classes.append(view_class)
-            elif (
-                view_class is None
-                and callable(view_function)
-                and not hasattr(view_function, "silo_limit")
-                # Django's internal media views/static
-                and "django.views.static" not in view_function.__module__
-            ):
-                undecorated_view_functions.append(view_function)
+    url_mappings = django.urls.get_resolver().reverse_dict.items()
+    for view_function, bindings in url_mappings:
+        view_class = getattr(view_function, "view_class", None)
+        if (
+            view_class
+            and issubclass(view_class, Endpoint)
+            and not hasattr(view_class, "silo_limit")
+        ):
+            undecorated_endpoint_classes.append(view_class)
+        elif (
+            view_class
+            and issubclass(view_class, View)
+            and view_class != RedirectView
+            and view_class != TemplateView
+            and not hasattr(view_class, "silo_limit")
+        ):
+            undecorated_view_classes.append(view_class)
+        elif (
+            view_class is None
+            and callable(view_function)
+            and not hasattr(view_function, "silo_limit")
+            # Django's internal media views/static
+            and "django.views.static" not in view_function.__module__
+        ):
+            undecorated_view_functions.append(view_function)
 
-        assert len(undecorated_endpoint_classes) == 0, (
-            "Endpoint classes missing EndpointSiloLimit: "
-            f"{', '.join(f'{m.__module__}.{m.__name__}' for m in undecorated_endpoint_classes)}"
-        )
-        assert len(undecorated_view_classes) == 0, (
-            "View classes missing ViewSiloLimit: "
-            f"{', '.join(f'{m.__module__}.{m.__name__}' for m in undecorated_view_classes)}"
-        )
-        assert len(undecorated_view_functions) == 0, (
-            "View functions missing ViewSiloLimit: "
-            f"{', '.join(f'{m.__module__}.{m}' for m in undecorated_view_functions)}"
-        )
+    assert len(undecorated_endpoint_classes) == 0, (
+        "Endpoint classes missing EndpointSiloLimit: "
+        f"{', '.join(f'{m.__module__}.{m.__name__}' for m in undecorated_endpoint_classes)}"
+    )
+    assert len(undecorated_view_classes) == 0, (
+        "View classes missing ViewSiloLimit: "
+        f"{', '.join(f'{m.__module__}.{m.__name__}' for m in undecorated_view_classes)}"
+    )
+    assert len(undecorated_view_functions) == 0, (
+        "View functions missing ViewSiloLimit: "
+        f"{', '.join(f'{m.__module__}.{m}' for m in undecorated_view_functions)}"
+    )
