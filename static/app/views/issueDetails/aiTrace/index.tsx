d5cb723b0e6a3a6ced397ac6409b0ab6bf20ab38
Antonis Lilis <antonis.lilis@sentry.io>
2026-02-18 12:41:15 +0100

    Switch to real data mode for testing

--- static/app/views/issueDetails/aiTrace/index.tsx
+++ static/app/views/issueDetails/aiTrace/index.tsx
@@ -38,7 +38,7 @@ interface AITraceSectionProps {
 const MCP_SERVER_URL = 'http://localhost:8080';
 
 // Demo mode: set to true to use hardcoded data for UI testing
-const USE_DEMO_DATA = true;
+const USE_DEMO_DATA = false;
 
 const DEMO_DATA: AITraceData = {
   metadata: {
