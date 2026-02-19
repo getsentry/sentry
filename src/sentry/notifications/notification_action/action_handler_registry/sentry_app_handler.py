d6e1d859df668490e021061e31ed7c6dbec34ff2
Colleen O'Rourke <colleen@sentry.io>
2026-02-18 17:12:32 -0800

    addtl prop -> True

--- src/sentry/notifications/notification_action/action_handler_registry/sentry_app_handler.py
+++ src/sentry/notifications/notification_action/action_handler_registry/sentry_app_handler.py
@@ -25,7 +25,7 @@ class SentryAppActionHandler(ActionHandler):
             },
         },
         "required": ["target_type", "target_identifier"],
-        "additionalProperties": False,
+        "additionalProperties": True,
     }
 
     data_schema = {
