cf8357b39d92fc7f4103100a65afafb24566d913
Christinarlong <christina.long@sentry.io>
2026-02-19 13:32:15 -0800

    move notifying to after the commit

--- src/sentry/sentry_apps/api/endpoints/installation_details.py
+++ src/sentry/sentry_apps/api/endpoints/installation_details.py
@@ -45,19 +45,9 @@ class SentryAppInstallationDetailsEndpoint(SentryAppInstallationBaseEndpoint):
 
     def delete(self, request: Request, installation) -> Response:
         sentry_app_installation = SentryAppInstallation.objects.get(id=installation.id)
-        with transaction.atomic(using=router.db_for_write(SentryAppInstallation)):
-            try:
-                assert request.user.is_authenticated, (
-                    "User must be authenticated to delete installation"
-                )
-                SentryAppInstallationNotifier(
-                    sentry_app_installation=sentry_app_installation,
-                    user=request.user,
-                    action="deleted",
-                ).run()
-            # if the error is from a request exception, log the error and continue
-            except RequestException as exc:
-                sentry_sdk.capture_exception(exc)
+        db = router.db_for_write(SentryAppInstallation)
+
+        with transaction.atomic(using=db):
             sentry_app_installation.update(status=SentryAppInstallationStatus.PENDING_DELETION)
             ScheduledDeletion.schedule(sentry_app_installation, days=0, actor=request.user)
             create_audit_entry(
@@ -67,7 +57,22 @@ class SentryAppInstallationDetailsEndpoint(SentryAppInstallationBaseEndpoint):
                 event=audit_log.get_event_id("SENTRY_APP_UNINSTALL"),
                 data={"sentry_app": sentry_app_installation.sentry_app.name},
             )
-        if request.user.is_authenticated:
+
+            def notify_on_commit() -> None:
+                try:
+                    assert request.user.is_authenticated, (
+                        "User must be authenticated to delete installation"
+                    )
+                    SentryAppInstallationNotifier(
+                        sentry_app_installation=sentry_app_installation,
+                        user=request.user,
+                        action="deleted",
+                    ).run()
+                except RequestException as exc:
+                    sentry_sdk.capture_exception(exc)
+
+            transaction.on_commit(notify_on_commit, using=db)
+
             analytics.record(
                 SentryAppUninstalledEvent(
                     user_id=request.user.id,
