ba8b06868c0ede641cc538906794285d5376f4ba
Joshua Li <joshuarli98@gmail.com>
2026-02-19 13:29:30 -0800

    only create gh issue if detected pollution

--- tests/sentry/debug/utils/test_exception_reporter_filter.py
+++ tests/sentry/debug/utils/test_exception_reporter_filter.py
@@ -1,10 +1,20 @@
+from unittest import mock
+
 from django.test import override_settings
+from django.urls import reverse
 
 from sentry.testutils.cases import TestCase
+from sentry.web.frontend.error_500 import Error500View
 
 
 @override_settings(DEBUG=True)
 @override_settings(DEBUG_PROPAGATE_EXCEPTIONS=False)
 class TestNoSettingsInDebugView(TestCase):
     def test(self) -> None:
-        assert True == False
+        self.client.raise_request_exception = False
+        url = reverse("error-500")
+        # force an unhandled exception
+        with mock.patch.object(Error500View, "dispatch", side_effect=ValueError):
+            resp = self.client.get(url)
+        # we should have scrubbed the settings from the output
+        assert b"SETTINGS_MODULE" not in resp.content
