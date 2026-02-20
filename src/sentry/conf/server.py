469df7593d9b15c46fbb4dd2496e130660622689
Mihir-Mavalankar <mihir.d.mavalankar@gmail.com>
2026-02-20 14:20:21 -0800

    fixed test

--- src/sentry/conf/server.py
+++ src/sentry/conf/server.py
@@ -960,6 +960,7 @@ TASKWORKER_IMPORTS: tuple[str, ...] = (
     "sentry.workflow_engine.tasks.workflows",
     "sentry.workflow_engine.tasks.actions",
     "sentry.tasks.seer_explorer_index",
+    "sentry.tasks.explorer_context_engine_tasks",
     # Used for tests
     "sentry.taskworker.tasks.examples",
 )
