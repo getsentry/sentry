0015b6e9efcddf09388ba6b85a87f33ea4a5893c
Dominik Buszowiecki <DominikB2014@gmail.com>
2026-02-23 11:25:43 -0500

    feat(dashboards): Populate Laravel overview with backend overview widgets

--- static/app/views/dashboards/utils/prebuiltConfigs/laravelOverview/laravelOverview.ts
+++ static/app/views/dashboards/utils/prebuiltConfigs/laravelOverview/laravelOverview.ts
@@ -1,9 +1,32 @@
+import {FieldKind} from 'sentry/utils/fields';
+import {WidgetType} from 'sentry/views/dashboards/types';
 import type {PrebuiltDashboard} from 'sentry/views/dashboards/utils/prebuiltConfigs';
+import {
+  BACKEND_OVERVIEW_FIRST_ROW_WIDGETS,
+  BACKEND_OVERVIEW_SECOND_ROW_WIDGETS,
+} from 'sentry/views/dashboards/utils/prebuiltConfigs/backendOverview/backendOverview';
 import {DASHBOARD_TITLE} from 'sentry/views/dashboards/utils/prebuiltConfigs/laravelOverview/settings';
+import {SpanFields} from 'sentry/views/insights/types';
 
 export const LARAVEL_OVERVIEW_PREBUILT_CONFIG: PrebuiltDashboard = {
+  filters: {
+    globalFilter: [
+      {
+        dataset: WidgetType.SPANS,
+        tag: {
+          key: SpanFields.TRANSACTION,
+          name: SpanFields.TRANSACTION,
+          kind: FieldKind.TAG,
+        },
+        value: '',
+      },
+    ],
+  },
   dateCreated: '',
   projects: [],
   title: DASHBOARD_TITLE,
-  widgets: [],
+  widgets: [
+    ...BACKEND_OVERVIEW_FIRST_ROW_WIDGETS,
+    ...BACKEND_OVERVIEW_SECOND_ROW_WIDGETS,
+  ],
 };
