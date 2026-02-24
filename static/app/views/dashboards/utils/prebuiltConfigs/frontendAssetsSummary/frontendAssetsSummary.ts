c724eac672b5db742362d400415056a45ec779f6
Dominik Buszowiecki <DominikB2014@gmail.com>
2026-02-24 15:11:47 -0500

    feat(dashboards): Add blank FRONTEND_ASSETS_SUMMARY prebuilt dashboard

---
+++ static/app/views/dashboards/utils/prebuiltConfigs/frontendAssetsSummary/frontendAssetsSummary.ts
@@ -0,0 +1,10 @@
+import type {PrebuiltDashboard} from 'sentry/views/dashboards/utils/prebuiltConfigs';
+import {DASHBOARD_TITLE} from 'sentry/views/dashboards/utils/prebuiltConfigs/frontendAssetsSummary/settings';
+
+export const FRONTEND_ASSETS_SUMMARY_PREBUILT_CONFIG: PrebuiltDashboard = {
+  dateCreated: '',
+  projects: [],
+  filters: {},
+  title: DASHBOARD_TITLE,
+  widgets: [],
+};
