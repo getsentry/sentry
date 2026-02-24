b34c2f7cd9487a0e0c0ca64bba7d95a24579a322
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-02-24 15:44:22 -0800

    rm extra newline

--- static/gsApp/views/seerAutomation/onboarding/hooks/seerOnboardingContext.tsx
+++ static/gsApp/views/seerAutomation/onboarding/hooks/seerOnboardingContext.tsx
@@ -120,7 +120,6 @@ export function SeerOnboardingProvider({children}: {children: React.ReactNode}) 
     fetchNextPageRepositories,
     hasNextPageRepositories,
     isRepositoriesError,
-
     isFetchingNextPageRepositories,
   ]);
 
