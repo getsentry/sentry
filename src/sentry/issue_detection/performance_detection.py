f75ad60791d48f869feb861bcceaf1a708b67b87
Kyle Consalus <kyle.consalus@sentry.io>
2026-02-27 10:40:58 -0800

    add missing import

--- src/sentry/issue_detection/performance_detection.py
+++ src/sentry/issue_detection/performance_detection.py
@@ -10,7 +10,7 @@ from typing import Any
 
 import sentry_sdk
 
-from sentry import nodestore, options, projectoptions
+from sentry import features, nodestore, options, projectoptions
 from sentry.models.options.project_option import ProjectOption
 from sentry.models.organization import Organization
 from sentry.models.project import Project
