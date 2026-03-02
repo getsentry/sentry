dec385e2f60c4b6ab1499477224fc4b1538f3c99
Christinarlong <christina.long@sentry.io>
2026-03-02 15:41:59 -0800

    merr another cursor comment

--- src/sentry/workflow_engine/receivers/detector.py
+++ src/sentry/workflow_engine/receivers/detector.py
@@ -32,6 +32,9 @@ def enforce_config_schema_signal(sender: type[Detector], instance: Detector, **k
 @receiver(post_save, sender=Detector)
 @receiver(pre_delete, sender=Detector)
 def invalidate_detector_cache(sender: type[Detector], instance: Detector, **kwargs: Any) -> None:
+    if kwargs.get("created") or not instance.id:
+        return
+
     data_sources = list(instance.data_sources.values_list("source_id", "type"))
 
     def invalidate_cache() -> None:
