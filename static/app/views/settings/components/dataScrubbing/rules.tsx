d30c7e170a6fc426e7fc7d81d9f25f51a8af28ab
TkDodo <dominik.dorfmeister@sentry.io>
2026-03-03 12:02:56 +0100

    fix: use tokens for color

--- static/app/views/settings/components/dataScrubbing/rules.tsx
+++ static/app/views/settings/components/dataScrubbing/rules.tsx
@@ -84,7 +84,7 @@ const List = styled('ul')<{
   ${p =>
     p.isDisabled &&
     css`
-      color: ${p.theme.colors.gray200};
+      color: ${p.theme.tokens.content.disabled};
       background: ${p.theme.tokens.background.secondary};
     `}
 `;
@@ -96,10 +96,6 @@ const ListItem = styled('li')`
   align-items: center;
   padding: ${space(1)} ${space(2)};
   border-bottom: 1px solid ${p => p.theme.tokens.border.primary};
-  &:hover {
-    background-color: ${p =>
-      p.theme.tokens.interactive.transparent.neutral.background.hover};
-  }
   &:last-child {
     border-bottom: 0;
   }
