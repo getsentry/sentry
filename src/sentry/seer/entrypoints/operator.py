d678512d615e7a47baa5cc18ccd10a119b323192
Tony Xiao <txiao@sentry.io>
2026-03-04 12:15:21 -0500

    chore(autofix): Remove seer-slack-workflows-explorer (#109871)
    The slack workflows should now work with with explorer autofix so remove
    this additional feature flag.

--- src/sentry/seer/entrypoints/operator.py
+++ src/sentry/seer/entrypoints/operator.py
@@ -77,15 +77,6 @@ class SeerOperator[CachePayloadT]:
         if not has_seer_access(organization):
             return False
 
-        # Currently, this feature is built around legacy autofix
-        # The explorer autofix pipeline and history is entirely separate, so the runs we trigger
-        # at the moment, won't be visible in-app to users with this flag.
-        # This check can only be removed once this feature migrates to explorer-based autofix.
-        if features.has("organizations:autofix-on-explorer", organization) and not features.has(
-            "organizations:seer-slack-workflows-explorer", organization
-        ):
-            return False
-
         if entrypoint_key:
             if entrypoint_key not in entrypoint_registry.registrations:
                 logger.error(
