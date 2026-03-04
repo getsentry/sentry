3c4fb2ff173b2b9c20cebaf3348eaff0bde8c565
TkDodo <dominik.dorfmeister@sentry.io>
2026-03-04 09:10:34 +0100

    chore: fix knip

--- static/app/components/core/form/index.ts
+++ static/app/components/core/form/index.ts
@@ -1,3 +1,4 @@
+/** @public */
 export {useScrapsForm, defaultFormOptions, setFieldErrors} from './scrapsForm';
 export {AutoSaveField} from './field/autoSaveField';
 export {FieldGroup} from './layout/fieldGroup';
