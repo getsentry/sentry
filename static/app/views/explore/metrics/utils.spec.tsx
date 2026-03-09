cd19bc79f50eb43c8a728a6d8299c60aea6697e6
Nar Saynorath <nar.saynorath@sentry.io>
2026-03-09 11:52:03 -0400

    Use it.each to avoid boilerplate repetition in tests

--- static/app/views/explore/metrics/utils.spec.tsx
+++ static/app/views/explore/metrics/utils.spec.tsx
@@ -1,69 +1,20 @@
 import {mapMetricUnitToFieldType} from 'sentry/views/explore/metrics/utils';
 
 describe('mapMetricUnitToFieldType', () => {
-  it('maps duration units', () => {
-    expect(mapMetricUnitToFieldType('millisecond')).toEqual({
-      fieldType: 'duration',
-      unit: 'millisecond',
-    });
-    expect(mapMetricUnitToFieldType('nanosecond')).toEqual({
-      fieldType: 'duration',
-      unit: 'nanosecond',
-    });
-    expect(mapMetricUnitToFieldType('second')).toEqual({
-      fieldType: 'duration',
-      unit: 'second',
-    });
-    expect(mapMetricUnitToFieldType('minute')).toEqual({
-      fieldType: 'duration',
-      unit: 'minute',
-    });
-  });
-
-  it('maps size units', () => {
-    expect(mapMetricUnitToFieldType('byte')).toEqual({
-      fieldType: 'size',
-      unit: 'byte',
-    });
-    expect(mapMetricUnitToFieldType('kibibyte')).toEqual({
-      fieldType: 'size',
-      unit: 'kibibyte',
-    });
-    expect(mapMetricUnitToFieldType('megabyte')).toEqual({
-      fieldType: 'size',
-      unit: 'megabyte',
-    });
-  });
-
-  it('maps percentage units', () => {
-    expect(mapMetricUnitToFieldType('ratio')).toEqual({
-      fieldType: 'percentage',
-      unit: 'ratio',
-    });
-    expect(mapMetricUnitToFieldType('percent')).toEqual({
-      fieldType: 'percentage',
-      unit: 'percent',
-    });
-  });
-
-  it('returns number for undefined unit', () => {
-    expect(mapMetricUnitToFieldType(undefined)).toEqual({
-      fieldType: 'number',
-      unit: undefined,
-    });
-  });
-
-  it('returns number for dash (unitless) unit', () => {
-    expect(mapMetricUnitToFieldType('-')).toEqual({
-      fieldType: 'number',
-      unit: undefined,
-    });
-  });
-
-  it('returns number for unknown unit strings', () => {
-    expect(mapMetricUnitToFieldType('custom_unit')).toEqual({
-      fieldType: 'number',
-      unit: undefined,
-    });
+  it.each([
+    ['millisecond', {fieldType: 'duration', unit: 'millisecond'}],
+    ['nanosecond', {fieldType: 'duration', unit: 'nanosecond'}],
+    ['second', {fieldType: 'duration', unit: 'second'}],
+    ['minute', {fieldType: 'duration', unit: 'minute'}],
+    ['byte', {fieldType: 'size', unit: 'byte'}],
+    ['kibibyte', {fieldType: 'size', unit: 'kibibyte'}],
+    ['megabyte', {fieldType: 'size', unit: 'megabyte'}],
+    ['ratio', {fieldType: 'percentage', unit: 'ratio'}],
+    ['percent', {fieldType: 'percentage', unit: 'percent'}],
+    [undefined, {fieldType: 'number', unit: undefined}],
+    ['-', {fieldType: 'number', unit: undefined}],
+    ['custom_unit', {fieldType: 'number', unit: undefined}],
+  ])('maps %s to the correct field type', (unit, expected) => {
+    expect(mapMetricUnitToFieldType(unit)).toEqual(expected);
   });
 });
