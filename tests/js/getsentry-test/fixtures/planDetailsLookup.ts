8f9b39654ab81969a47f6afb6d33e87293a528a5
Josh Goldberg ✨ <joshua.goldberg@sentry.io>
2026-03-10 08:01:51 -0400

    ref: PlanIds using satisfies

--- tests/js/getsentry-test/fixtures/planDetailsLookup.ts
+++ tests/js/getsentry-test/fixtures/planDetailsLookup.ts
@@ -8,7 +8,12 @@ import MM2_PLANS from 'getsentry-test/fixtures/mm2Plans';
 
 import {PlanTier} from 'getsentry/types';
 
-type PlanIds = string;
+type PlanIds =
+  | keyof typeof AM1_PLANS
+  | keyof typeof AM2_PLANS
+  | keyof typeof AM3_PLANS
+  | keyof typeof MM1_PLANS
+  | keyof typeof MM2_PLANS;
 
 // Pass a planId to get back details for that particular plan, or 'all'
 // to get a list of all plan detail objects for a plan tier.
@@ -19,14 +24,14 @@ export function PlanDetailsLookupFixture(planId: PlanIds, tier?: PlanTier) {
 
   const planData =
     (tier ?? planId.startsWith(PlanTier.AM3))
-      ? AM3_PLANS[planId]
+      ? AM3_PLANS[planId as keyof typeof AM3_PLANS]
       : planId.startsWith(PlanTier.AM1)
-        ? AM1_PLANS[planId]
+        ? AM1_PLANS[planId as keyof typeof AM1_PLANS]
         : planId.startsWith(PlanTier.AM2)
-          ? AM2_PLANS[planId]
+          ? AM2_PLANS[planId as keyof typeof AM2_PLANS]
           : planId.startsWith(PlanTier.MM2)
-            ? MM2_PLANS[planId]
-            : MM1_PLANS[planId];
+            ? MM2_PLANS[planId as keyof typeof MM2_PLANS]
+            : MM1_PLANS[planId as keyof typeof MM1_PLANS];
 
   return cloneDeep(planData);
 }
