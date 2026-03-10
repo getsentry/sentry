6f8fcaa00db9627e181046e631f1b45c44e484bf
Lyn Nagara <1779792+lynnagara@users.noreply.github.com>
2026-03-10 13:21:10 -0700

    ref(cells): Remove region_name param from get_organizations_by_user_and_scope (#110276)
    all callers have been migrated to use cell_name

--- src/sentry/organizations/services/organization/impl.py
+++ src/sentry/organizations/services/organization/impl.py
@@ -177,8 +177,7 @@ class DatabaseBackedOrganizationService(OrganizationService):
     def get_organizations_by_user_and_scope(
         self,
         *,
-        cell_name: str | None = None,  # TODO(cells): make required when getsentry is updated
-        region_name: str | None = None,  # TODO(cells): remove when getsentry is updated
+        cell_name: str,
         user: RpcUser,
         scope: str | None = None,
     ) -> list[RpcOrganization]:
