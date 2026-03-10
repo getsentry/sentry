f3c2292fce75b137edb4727f0a4afbf807e05957
JonasBa <jonas@badalic.com>
2026-03-10 15:10:31 -0700

    ref(nav) remove router import

--- static/app/views/navigation/primary/components.tsx
+++ static/app/views/navigation/primary/components.tsx
@@ -1,7 +1,7 @@
 import {Fragment, useEffect, useRef, type MouseEventHandler} from 'react';
 import {css, useTheme} from '@emotion/react';
 import styled from '@emotion/styled';
-import type {To} from '@remix-run/router';
+import type {LocationDescriptor} from 'history';
 
 import type {ButtonProps} from '@sentry/scraps/button';
 import {Button} from '@sentry/scraps/button';
@@ -516,7 +516,7 @@ export const SidebarList = styled('ul')<{isMobile: boolean; compact?: boolean}>`
 `;
 
 export function isSidebarLinkActive(
-  to: To,
+  to: LocationDescriptor | string,
   pathname: string,
   options: {end?: boolean} = {end: false}
 ): boolean {
