03bcffaffd6725a3d59a3cdecd7be74868229d0e
Jonas <jonas.badalic@sentry.io>
2026-03-11 08:30:32 -0700

    ref(nav) move admin nav to nav secondary (#110375)
    Address the todo comment in admin nav and move the nav definition inside
    the nav file

--- static/app/views/navigation/secondary/sections/admin/adminSecondaryNavigation.tsx
+++ static/app/views/navigation/secondary/sections/admin/adminSecondaryNavigation.tsx
@@ -1,5 +1,33 @@
-import {AdminNavigation} from 'sentry/views/admin/adminLayout';
+import {PrimaryNavigationGroup} from 'sentry/views/navigation/types';
+import SettingsNavigation from 'sentry/views/settings/components/settingsNavigation';
 
 export function AdminSecondaryNavigation() {
-  return <AdminNavigation />;
+  return (
+    <SettingsNavigation
+      stickyTop="0"
+      navigationObjects={[
+        {
+          id: 'admin-system-status',
+          name: 'System Status',
+          items: [
+            {path: '/manage/status/environment/', title: 'Environment'},
+            {path: '/manage/status/packages/', title: 'Packages'},
+            {path: '/manage/status/mail/', title: 'Mail'},
+            {path: '/manage/status/warnings/', title: 'Warnings'},
+            {path: '/manage/settings/', title: 'Settings'},
+          ],
+        },
+        {
+          id: 'admin-manage',
+          name: 'Manage',
+          items: [
+            {path: '/manage/organizations/', title: 'Organizations'},
+            {path: '/manage/projects/', title: 'Projects'},
+            {path: '/manage/users/', title: 'Users'},
+          ],
+        },
+      ]}
+      primaryNavGroup={PrimaryNavigationGroup.ADMIN}
+    />
+  );
 }
