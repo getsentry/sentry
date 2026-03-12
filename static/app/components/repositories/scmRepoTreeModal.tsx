15805791a8fa89fab0901d03791b0bc4c58d771c
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-03-12 08:22:41 -0700

    adjust spacing

--- static/app/components/repositories/scmRepoTreeModal.tsx
+++ static/app/components/repositories/scmRepoTreeModal.tsx
@@ -1,12 +1,14 @@
 import {Fragment, useState} from 'react';
 
-import {Flex} from '@sentry/scraps/layout';
+import {Flex, Stack} from '@sentry/scraps/layout';
+import {ExternalLink} from '@sentry/scraps/link';
+import {Text} from '@sentry/scraps/text';
 
 import type {ModalRenderProps} from 'sentry/actionCreators/modal';
 import {ScmIntegrationTree} from 'sentry/components/repositories/scmIntegrationTree/scmIntegrationTree';
 import ScmTreeFilters from 'sentry/components/repositories/scmIntegrationTree/scmTreeFilters';
 import type {RepoFilter} from 'sentry/components/repositories/scmIntegrationTree/types';
-import {t} from 'sentry/locale';
+import {t, tct} from 'sentry/locale';
 
 export function ScmRepoTreeModal({Header, Body}: ModalRenderProps) {
   const [search, setSearch] = useState('');
@@ -18,19 +20,36 @@ export function ScmRepoTreeModal({Header, Body}: ModalRenderProps) {
         <h4>{t('Add Repository')}</h4>
       </Header>
       <Body>
-        <Flex gap="sm" style={{marginBottom: 12}}>
-          <ScmTreeFilters
-            repoFilter={repoFilter}
-            setRepoFilter={setRepoFilter}
-            searchTerm={search}
-            setSearchTerm={setSearch}
-          />
-        </Flex>
-        <ScmIntegrationTree
-          search={search}
-          repoFilter={repoFilter}
-          providerFilter="seer-supported"
-        />
+        <Stack gap="2xl">
+          <Text size="md">
+            {tct(
+              `Integrate with a Seer compatible Source Code Management provider and then connect repositories with Sentry. Seer needs read access to your source code to perform code review, and analyze your issues. [read_the_docs:Read the docs] and our [privacy:AI Privacy Principles] to learn more.`,
+              {
+                privacy: (
+                  <ExternalLink href="https://docs.sentry.io/product/ai-in-sentry/ai-privacy-and-security/" />
+                ),
+                read_the_docs: (
+                  <ExternalLink href="https://docs.sentry.io/product/ai-in-sentry/seer/#seer-capabilities" />
+                ),
+              }
+            )}
+          </Text>
+          <Stack gap="lg">
+            <Flex gap="sm">
+              <ScmTreeFilters
+                repoFilter={repoFilter}
+                setRepoFilter={setRepoFilter}
+                searchTerm={search}
+                setSearchTerm={setSearch}
+              />
+            </Flex>
+            <ScmIntegrationTree
+              search={search}
+              repoFilter={repoFilter}
+              providerFilter="seer-supported"
+            />
+          </Stack>
+        </Stack>
       </Body>
     </Fragment>
   );
