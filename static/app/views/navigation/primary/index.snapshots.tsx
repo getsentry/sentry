df48b4f804a7f254d840bb36a7c70512e825acc5
JonasBa <jonas@badalic.com>
2026-03-17 11:53:11 -0700

    ref(navigation) mock matchmedia

--- static/app/views/navigation/primary/index.snapshots.tsx
+++ static/app/views/navigation/primary/index.snapshots.tsx
@@ -1,20 +1,26 @@
 import {ThemeAndStyleProvider} from 'sentry/components/themeAndStyleProvider';
 import {ConfigStore} from 'sentry/stores/configStore';
+import * as useMedia from 'sentry/utils/useMedia';
 
 import {Navigation} from '..';
 
 describe('PrimaryNavigation', () => {
   describe.each(['light', 'dark'] as const)('theme-%s', type => {
-    beforeAll(() => {
-      ConfigStore.set('theme', type);
-    });
+    describe.each(['desktop', 'mobile'] as const)('layout-%s', layout => {
+      beforeAll(() => {
+        ConfigStore.set('theme', type);
+        // We query matchMedia < md breakpoint to determine nav layout,
+        // returning false means we are on a desktop layout
+        jest.spyOn(useMedia, 'useMedia').mockReturnValue(layout === 'desktop');
+      });
 
-    it.snapshot('Primary Navigaton: %s', () => {
-      return (
-        <ThemeAndStyleProvider>
-          <Navigation />
-        </ThemeAndStyleProvider>
-      );
+      it.snapshot('Primary Navigaton: %s', () => {
+        return (
+          <ThemeAndStyleProvider>
+            <Navigation />
+          </ThemeAndStyleProvider>
+        );
+      });
     });
   });
 });
