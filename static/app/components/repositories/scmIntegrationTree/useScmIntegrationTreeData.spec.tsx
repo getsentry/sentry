16ad4991f29c74ef7e2d7f777a51ae92ebb0a3d1
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-03-17 10:25:06 -0700

    use fixtures

--- static/app/components/repositories/scmIntegrationTree/useScmIntegrationTreeData.spec.tsx
+++ static/app/components/repositories/scmIntegrationTree/useScmIntegrationTreeData.spec.tsx
@@ -1,3 +1,5 @@
+import {GitHubIntegrationFixture} from 'sentry-fixture/githubIntegration';
+import {GitHubIntegrationProviderFixture} from 'sentry-fixture/githubIntegrationProvider';
 import {OrganizationFixture} from 'sentry-fixture/organization';
 
 import {renderHookWithProviders, waitFor} from 'sentry-test/reactTestingLibrary';
@@ -7,44 +9,9 @@ import {useScmIntegrationTreeData} from './useScmIntegrationTreeData';
 describe('useScmIntegrationTreeData', () => {
   const organization = OrganizationFixture();
 
-  const githubProvider = {
-    key: 'github',
-    slug: 'github',
-    name: 'GitHub',
-    canAdd: true,
-    canDisable: false,
-    features: ['commits'],
-    metadata: {
-      aspects: {},
-      author: 'The Sentry Team',
-      description: '',
-      features: [{featureId: 1, featureGate: 'integrations-commits', description: ''}],
-      issue_url: '',
-      noun: 'Installation',
-      source_url: '',
-    },
-    setupDialog: {height: 600, url: '', width: 600},
-  };
+  const githubProvider = GitHubIntegrationProviderFixture();
 
-  const githubIntegration = {
-    id: '1',
-    name: 'GitHub',
-    provider: {
-      key: 'github',
-      slug: 'github',
-      name: 'GitHub',
-      canAdd: true,
-      canDisable: false,
-      features: [],
-      aspects: {},
-    },
-    domainName: 'github.com/test',
-    icon: null,
-    accountType: null,
-    gracePeriodEnd: null,
-    organizationIntegrationStatus: 'active',
-    status: 'active',
-  };
+  const githubIntegration = GitHubIntegrationFixture();
 
   beforeEach(() => {
     MockApiClient.addMockResponse({
