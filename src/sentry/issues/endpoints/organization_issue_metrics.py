11071d4e5b7325bc692e6e38fe61f4c1eb4e0d89
William Mak <william@wmak.io>
2026-03-18 16:30:09 -0400

    chore(replays): Update ownership to databrowsing
    - The replay code is being absorbed by the databrowsing teams so
      changing the ownership to match

--- src/sentry/issues/endpoints/organization_issue_metrics.py
+++ src/sentry/issues/endpoints/organization_issue_metrics.py
@@ -11,7 +11,6 @@ from rest_framework.exceptions import ParseError
 from rest_framework.request import Request
 from rest_framework.response import Response
 
-from sentry.api.api_owners import ApiOwner
 from sentry.api.api_publish_status import ApiPublishStatus
 from sentry.api.base import cell_silo_endpoint
 from sentry.api.bases.organization import OrganizationEndpoint
@@ -25,7 +24,6 @@ from sentry.models.project import Project
 
 @cell_silo_endpoint
 class OrganizationIssueMetricsEndpoint(OrganizationEndpoint):
-    owner = ApiOwner.REPLAY
     publish_status = {"GET": ApiPublishStatus.PRIVATE}
 
     def get(self, request: Request, organization: Organization) -> Response:
