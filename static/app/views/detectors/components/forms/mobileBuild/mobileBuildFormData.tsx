484a3088869b7f195321c6906ffbda585b04e6d1
Hector Dearman <hector.dearman@gmail.com>
2026-03-18 16:09:21 -0700

    Revert "feat(preprod): Add filters for monitors (#110405)"
    This reverts commit 39382465cef631972b74cc696747615eb7ca5365.

--- static/app/views/detectors/components/forms/mobileBuild/mobileBuildFormData.tsx
+++ static/app/views/detectors/components/forms/mobileBuild/mobileBuildFormData.tsx
@@ -20,7 +20,6 @@ interface PreprodDetectorFormData {
   name: string;
   owner: string;
   projectId: string;
-  query: string;
   thresholdType: PreprodThresholdType;
   workflowIds: string[];
 }
@@ -37,7 +36,6 @@ export const PREPROD_DETECTOR_FORM_FIELDS = {
   thresholdType: 'thresholdType',
   highThreshold: 'highThreshold',
   lowThreshold: 'lowThreshold',
-  query: 'query',
 } satisfies Record<PreprodDetectorFormFieldName, PreprodDetectorFormFieldName>;
 
 export const PREPROD_DEFAULT_FORM_DATA: Partial<PreprodDetectorFormData> = {
@@ -45,7 +43,6 @@ export const PREPROD_DEFAULT_FORM_DATA: Partial<PreprodDetectorFormData> = {
   thresholdType: 'absolute',
   highThreshold: '',
   lowThreshold: '',
-  query: '',
 };
 
 export function usePreprodDetectorFormField<T extends PreprodDetectorFormFieldName>(
@@ -102,7 +99,6 @@ export function preprodFormDataToEndpointPayload(
     config: {
       measurement: data.measurement,
       thresholdType: data.thresholdType,
-      query: data.query || undefined,
     },
   };
 }
@@ -138,7 +134,6 @@ export function preprodSavedDetectorToFormData(
     workflowIds: detector.workflowIds,
     measurement: detector.config.measurement,
     thresholdType: detector.config.thresholdType,
-    query: detector.config.query || '',
     highThreshold: conditionToThreshold(highCondition?.comparison, isPercentage),
     lowThreshold: conditionToThreshold(lowCondition?.comparison, isPercentage),
   };
