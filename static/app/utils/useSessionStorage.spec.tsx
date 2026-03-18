0bb0d0c6f1b97a79c0fc0bbc10923abb560aae33
Jay Goss <jaygoss@gmail.com>
2026-03-18 10:35:28 -0500

    ref(utils): Add functional updater support to useSessionStorage
    Support SetStateAction<T> (function updater) in useSessionStorage's
    setter, matching React's useState API. This prevents stale-state bugs
    when multiple sequential calls spread the same snapshot, since each
    updater receives the latest prev value.

--- static/app/utils/useSessionStorage.spec.tsx
+++ static/app/utils/useSessionStorage.spec.tsx
@@ -69,6 +69,7 @@ describe('useSessionStorage', () => {
     expect(sessionStorageWrapper.getItem('key')).toBeNull();
   });
 
+
   it('removes item from storage', () => {
     sessionStorageWrapper.setItem('key', JSON.stringify('stored'));
     const {result} = renderHook(() => useSessionStorage('key', 'initial'));
