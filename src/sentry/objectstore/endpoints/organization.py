88ad8822a992f1e24f6128d427196e3d004d4a94
Joshua Li <joshuarli98@gmail.com>
2026-03-19 11:55:05 -0700

    revert test

--- src/sentry/objectstore/endpoints/organization.py
+++ src/sentry/objectstore/endpoints/organization.py
@@ -32,7 +32,6 @@ from sentry.objectstore import parse_accept_encoding
 
 @cell_silo_endpoint
 class OrganizationObjectstoreEndpoint(OrganizationEndpoint):
-    print("hi")
     publish_status = {
         "GET": ApiPublishStatus.EXPERIMENTAL,
         "PUT": ApiPublishStatus.EXPERIMENTAL,
