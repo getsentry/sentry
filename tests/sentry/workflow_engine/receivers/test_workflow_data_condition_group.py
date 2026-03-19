e45377d6080e0014454306d640de299b02f419de
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2026-03-18 21:54:50 -0700

    refactor the workflow data condition group to use the ActionFilterTestCase

--- tests/sentry/workflow_engine/receivers/test_workflow_data_condition_group.py
+++ tests/sentry/workflow_engine/receivers/test_workflow_data_condition_group.py
@@ -1,71 +1,35 @@
-from sentry.testutils.cases import TestCase
-from sentry.workflow_engine.caches.action_filters import (
-    _action_filters_cache,
-    _ActionFilterCacheKey,
-    _populate_cache,
-)
-from sentry.workflow_engine.models.data_condition_group import DataConditionGroup
-from sentry.workflow_engine.models.workflow import Workflow
+from sentry.workflow_engine.caches.action_filters import ActionFiltersByWorkflow
 from sentry.workflow_engine.models.workflow_data_condition_group import WorkflowDataConditionGroup
+from tests.sentry.workflow_engine.caches.test_action_filters import ActionFilterTestCase
 
 
-class TestWorkflowDataConditionGroupReceivers(TestCase):
-    def create_workflow_with_filters(
-        self, num_filters: int = 1, num_conditions: int = 1
-    ) -> tuple[
-        Workflow,
-        list[DataConditionGroup],
-        list[WorkflowDataConditionGroup],
-    ]:
-        workflow = self.create_workflow()
-        action_filters: list[DataConditionGroup] = [
-            self.create_data_condition_group() for _ in range(num_filters)
-        ]
+class TestWorkflowDataConditionGroupReceivers(ActionFilterTestCase):
+    def setUp(self) -> None:
+        self.workflow = self.create_workflow()
+        self.action_filters = self.create_action_filters_for_workflow(
+            workflow=self.workflow,
+        )
 
-        workflow_condition_groups: list[WorkflowDataConditionGroup] = []
-        for action_filter in action_filters:
-            wcg = self.create_workflow_data_condition_group(
-                workflow=workflow, condition_group=action_filter
-            )
-
-            workflow_condition_groups.append(wcg)
-
-            action_filter.conditions.set(
-                [
-                    self.create_data_condition(condition_group=action_filter)
-                    for _ in range(num_conditions)
-                ]
-            )
-
-        return workflow, action_filters, workflow_condition_groups
+        action_filter_ids = [action_filter.id for action_filter in self.action_filters]
+        self.workflow_data_condition_groups = WorkflowDataConditionGroup.objects.filter(
+            condition_group_id__in=action_filter_ids,
+        )
 
     def test_cache_invalidation_on_delete(self) -> None:
-        workflow, action_filters, workflow_data_condition_groups = (
-            self.create_workflow_with_filters()
-        )
-
-        _populate_cache({workflow.id: action_filters})
-        cache_key = _ActionFilterCacheKey(workflow.id)
+        cache_data: ActionFiltersByWorkflow = {self.workflow.id: self.action_filters}
+        cache_keys = self.populate_action_filter_cache(cache_data)
 
         # ensure the cache is populated
-        assert len(_action_filters_cache.get(cache_key)) == 1
+        assert self.get_data_from_cache(cache_keys) == cache_data
 
-        workflow_data_condition_groups[0].delete()
-        assert _action_filters_cache.get(cache_key) is None
+        self.workflow_data_condition_groups[0].delete()
+        assert self.get_data_from_cache(cache_keys) == {self.workflow.id: None}
 
     def test_cache_invalidation_on_save(self) -> None:
-        workflow, action_filters, _ = self.create_workflow_with_filters()
-
-        # ensure the cache is populated
-        _populate_cache({workflow.id: action_filters})
-        cache_key = _ActionFilterCacheKey(workflow.id)
-        assert len(_action_filters_cache.get(cache_key)) == 1
+        cache_data: ActionFiltersByWorkflow = {self.workflow.id: self.action_filters}
+        cache_keys = self.populate_action_filter_cache(cache_data)
 
         # add another data condition group to the workflow
-        new_action_filter = self.create_data_condition_group()
-        self.create_workflow_data_condition_group(
-            workflow=workflow,
-            condition_group=new_action_filter,
-        )
+        self.create_action_filters_for_workflow(workflow=self.workflow, num_filters=1)
 
-        assert _action_filters_cache.get(cache_key) is None
+        assert self.get_data_from_cache(cache_keys) == {self.workflow.id: None}
