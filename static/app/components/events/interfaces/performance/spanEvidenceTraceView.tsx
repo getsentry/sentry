b894349d643f7542c331c4ea09c3b5aac6d44042
Nick <nicholas.deschenes@sentry.io>
2026-03-19 12:15:30 -0300

    Revert "ref(trace): Remove useIsEAPTraceEnabled hook and non-EAP code paths" (#111084)
    Reverts getsentry/sentry#110845

--- static/app/components/events/interfaces/performance/spanEvidenceTraceView.tsx
+++ static/app/components/events/interfaces/performance/spanEvidenceTraceView.tsx
@@ -81,7 +81,7 @@ function SpanEvidenceTraceViewImpl({
     timestamp,
     traceSlug: traceId,
     limit: 10000,
-    targetEventId: event.eventID,
+    targetEventId: event.id,
   });
   const tree = useIssuesTraceTree({trace, replay: null});
 
