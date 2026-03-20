83b262d49c30080febfac5054a696a58102e815f
TkDodo <dominik.dorfmeister@sentry.io>
2026-03-20 14:43:49 +0100

    oh jest ...

--- tests/js/sentry-test/queryClient.tsx
+++ tests/js/sentry-test/queryClient.tsx
@@ -1,6 +1,12 @@
+import {notifyManager} from '@tanstack/react-query';
 import merge from 'lodash/merge';
 
 import {DEFAULT_QUERY_CLIENT_CONFIG, QueryClient} from 'sentry/utils/queryClient';
+
+// Reset the scheduler to the default (setTimeout) for tests.
+// In production, we use queueMicrotask (see appQueryClient.tsx) for faster updates,
+// but this causes act() warnings in tests that don't await async query updates.
+notifyManager.setScheduler(cb => setTimeout(cb, 0));
 
 export const makeTestQueryClient = () =>
   new QueryClient(
