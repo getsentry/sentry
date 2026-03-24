ab66d88bdff27fac55da513b7a979876afa58118
Nate Moore <nate.moore@sentry.io>
2026-03-24 16:38:03 -0400

    ref(scraps): remove unused exports

--- static/app/components/core/hotkey/index.tsx
+++ static/app/components/core/hotkey/index.tsx
@@ -1,4 +1,3 @@
-export {Kbd, type KbdProps} from './kbd';
-export {Hotkey, type HotkeyProps} from './hotkey';
+export {Kbd} from './kbd';
+export {Hotkey} from './hotkey';
 export {useHotkeys} from './useHotkeys';
-export {getKeyCode} from './keyMappings';
