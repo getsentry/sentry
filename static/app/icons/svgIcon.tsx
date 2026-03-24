d8c4ac502ae2039d871c7ee5d30ddc8ca5d6e55d
Nate Moore <natemoo-re@users.noreply.github.com>
2026-03-24 10:38:20 -0400

    fix(scraps): omit `type` from `SVGIconProps` (#111353)
    @jaydgoss hit an [interesting type
    issue](https://github.com/getsentry/sentry/pull/111160#discussion_r2977247438)
    in #111160
    
    The gist is `SVGIconProps` extends `React.SVGAttributes<SVGSVGElement>`
    which [includes `type:
    string`](https://developer.mozilla.org/en-US/docs/Web/SVG/Reference/Attribute/type),
    but that's not actually valid for the root `<svg>` of our icon
    component.
    
    By omitting `type` from `SVGIconProps`, `IconGraphProps` no longer
    narrows the `type: string` prop, so `type` becomes a new optional prop.
    The eliminates the variance conflict, so `ComponentType<IconGraph>`
    should be directly assignable to `SVGIconProps`.

--- static/app/icons/svgIcon.tsx
+++ static/app/icons/svgIcon.tsx
@@ -4,7 +4,10 @@ import type {ContentVariant, IconSize} from 'sentry/utils/theme';
 
 import {useIconDefaults} from './useIconDefaults';
 
-export interface SVGIconProps extends Omit<React.SVGAttributes<SVGSVGElement>, 'color'> {
+export interface SVGIconProps extends Omit<
+  React.SVGAttributes<SVGSVGElement>,
+  'color' | 'type'
+> {
   /**
    * DO NOT USE THIS! Please use the `size` prop
    *
