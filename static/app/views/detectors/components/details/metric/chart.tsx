f0a4d18cc3901f0a2bb5598bb385147488638dbc
Malachi Willey <malwilley@gmail.com>
2026-03-25 16:53:39 -0700

    feat(aci): Align metric chart data points with open period markers

--- static/app/views/detectors/components/details/metric/chart.tsx
+++ static/app/views/detectors/components/details/metric/chart.tsx
@@ -16,6 +16,7 @@ import {normalizeDateTimeParams} from 'sentry/components/pageFilters/parse';
 import {Placeholder} from 'sentry/components/placeholder';
 import {IconInfo, IconWarning} from 'sentry/icons';
 import {t} from 'sentry/locale';
+import type {Series} from 'sentry/types/echarts';
 import type {GroupOpenPeriod} from 'sentry/types/group';
 import type {MetricDetector, SnubaQuery} from 'sentry/types/workflowEngine/detectors';
 import {axisLabelFormatterUsingAggregateOutputType} from 'sentry/utils/discover/charts';
@@ -44,6 +45,27 @@ import {useMetricDetectorThresholdSeries} from 'sentry/views/detectors/hooks/use
 import {useMetricTimestamps} from 'sentry/views/detectors/hooks/useMetricTimestamps';
 import {useOpenPeriods} from 'sentry/views/detectors/hooks/useOpenPeriods';
 import {getDetectorChartFormatters} from 'sentry/views/detectors/utils/detectorChartFormatting';
+
+/**
+ * Shift data points from bucket-start to bucket-end timestamps.
+ * The API returns timestamps at the start of each bucket (e.g. 1pm for 1pm–2pm),
+ * but we want to display them at the end (2pm) since the value represents
+ * the data collected during the preceding interval.
+ *
+ * The reason we do this for metric charts is that it's important that the data points
+ * visually align with the open period markers. If an open period starts at 1pm, it should
+ * be vertically aligned with the data point for 12pm-1pm.
+ */
+function shiftSeriesToBucketEnd(seriesList: Series[], intervalSeconds: number): Series[] {
+  const offsetMs = intervalSeconds * 1000;
+  return seriesList.map(s => ({
+    ...s,
+    data: s.data.map(point => ({
+      ...point,
+      name: (point.name as number) + offsetMs,
+    })),
+  }));
+}
 
 interface IncidentTooltipContext {
   period: IncidentPeriod;
@@ -391,13 +413,31 @@ export function useMetricDetectorChart({
       showTimeInTooltip: true,
       height,
       stacked: false,
-      series,
+      series: shiftSeriesToBucketEnd(series, snubaQuery.timeWindow),
       additionalSeries,
       yAxes: yAxes.length > 1 ? yAxes : undefined,
       yAxis: yAxes.length === 1 ? yAxes[0] : undefined,
       grid,
       xAxis: openPeriodMarkerResult.incidentMarkerXAxis,
       tooltip: {
+        // Data points are shifted to bucket-end timestamps
+        // so we need to subtract the interval to get the bucket-start time.
+        formatAxisLabel: (
+          value,
+          isTimestamp,
+          utc,
+          showTimeInTooltip,
+          addSecondsToTimeFormat,
+          bucketSize
+        ) =>
+          defaultFormatAxisLabel(
+            value - snubaQuery.timeWindow * 1000,
+            isTimestamp,
+            utc,
+            showTimeInTooltip,
+            addSecondsToTimeFormat,
+            bucketSize
+          ).toString(),
         valueFormatter: getDetectorChartFormatters({
           detectionType,
           aggregate,
@@ -410,7 +450,7 @@ export function useMetricDetectorChart({
         chartZoomProps.onChartReady(chart);
         openPeriodMarkerResult.onChartReady(chart);
       },
-    };
+    } satisfies AreaChartProps;
   }, [
     additionalSeries,
     aggregate,
@@ -423,6 +463,7 @@ export function useMetricDetectorChart({
     openPeriodMarkerResult,
     series,
     serverOutputType,
+    snubaQuery.timeWindow,
     unit,
     yAxes,
   ]);
