39cecea4a1f4311120e49ddce840feae64f467df
JonasBa <jonas@badalic.com>
2026-03-25 15:19:57 -0700

    idk what happened here

--- static/app/actionCreators/modal.tsx
+++ static/app/actionCreators/modal.tsx
@@ -1,3 +1,7 @@
+import type {
+  CommandPaletteState,
+  CommandPaletteDispatch,
+} from 'sentry/components/commandPalette/ui/commandPaletteStateContext';
 import type {ModalTypes} from 'sentry/components/globalModal';
 import type {CreateReleaseIntegrationModalOptions} from 'sentry/components/modals/createReleaseIntegrationModal';
 import type {DashboardWidgetQuerySelectorModalOptions} from 'sentry/components/modals/dashboardWidgetQuerySelectorModal';
@@ -148,13 +152,29 @@ export async function openCommandPaletteDeprecated(options: ModalOptions = {}) {
   openModal(deps => <Modal {...deps} {...options} />, {modalCss});
 }
 
-export async function openCommandPalette(options: ModalOptions = {}) {
+export async function toggleCommandPalette(
+  options: ModalOptions = {},
+  state: CommandPaletteState,
+  dispatch: CommandPaletteDispatch
+) {
   const {default: Modal, modalCss} =
     await import('sentry/components/commandPalette/ui/modal');
 
-  openModal(deps => <Modal {...deps} {...options} />, {modalCss});
-}
+  function closeCommandPaletteModal() {
+    dispatch({type: 'toggle modal'});
+  }
 
+  if (state.open) {
+    closeCommandPaletteModal();
+    closeModal();
+  } else {
+    dispatch({type: 'toggle modal'});
+    openModal(deps => <Modal {...deps} {...options} />, {
+      modalCss,
+      onClose: closeCommandPaletteModal,
+    });
+  }
+}
 type RecoveryModalOptions = {
   authenticatorName: string;
 };
