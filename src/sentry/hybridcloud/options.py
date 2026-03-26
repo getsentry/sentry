f7405e961b86910f740e5ae6c0a28e50cff450d4
Lyn Nagara <1779792+lynnagara@users.noreply.github.com>
2026-03-26 11:18:13 -0700

    feat(cells): Wire projectkeymapping to the control silo (#110231)
    Now creates/updates/deletes on the ProjectKey model are asynchronously
    replicated to the ProjectKeyMapping model in the control silo.
    
    This currently applies to newly created records, as the table still
    needs to be backfilled.

--- src/sentry/hybridcloud/options.py
+++ src/sentry/hybridcloud/options.py
@@ -149,6 +149,13 @@ register(
 )
 
 register(
+    "outbox_replication.sentry_projectkey.replication_version",
+    type=Int,
+    default=0,
+    flags=FLAG_AUTOMATOR_MODIFIABLE,
+)
+
+register(
     "hybrid_cloud.authentication.disabled_organization_shards",
     type=Sequence,
     default=[],
