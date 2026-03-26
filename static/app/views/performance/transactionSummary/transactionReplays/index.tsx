f29df4c3f28b3f8ec4dc6e6d7be58f4b7ed34c56
JonasBa <jonas@badalic.com>
2026-03-26 09:05:00 -0700

    ref(layout): use Layout.Page on performance summary
    The parent pageLayout.tsx already renders Layout.Page wrapping the Outlet,
    making all 4 transaction summary routes compliant. Remove the redundant
    Layout.Page from the transactionReplays renderNoAccess fallback to avoid
    double-wrapping.

--- static/app/views/performance/transactionSummary/transactionReplays/index.tsx
+++ static/app/views/performance/transactionSummary/transactionReplays/index.tsx
@@ -1,7 +1,6 @@
 import {Alert} from '@sentry/scraps/alert';
 
 import Feature from 'sentry/components/acl/feature';
-import * as Layout from 'sentry/components/layouts/thirds';
 import {t} from 'sentry/locale';
 import {useOrganization} from 'sentry/utils/useOrganization';
 
@@ -9,13 +8,11 @@ import {TransactionReplays} from './transactionReplays';
 
 function renderNoAccess() {
   return (
-    <Layout.Page withPadding>
-      <Alert.Container>
-        <Alert variant="warning" showIcon={false}>
-          {t("You don't have access to this feature")}
-        </Alert>
-      </Alert.Container>
-    </Layout.Page>
+    <Alert.Container>
+      <Alert variant="warning" showIcon={false}>
+        {t("You don't have access to this feature")}
+      </Alert>
+    </Alert.Container>
   );
 }
 
