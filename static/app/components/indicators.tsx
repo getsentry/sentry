49c3548b8c199f5db07c1e088da966703d5bd85b
Nate Moore <natemoo-re@users.noreply.github.com>
2026-03-26 15:45:05 -0400

    Apply suggestions from code review
    Co-authored-by: Nate Moore <natemoo-re@users.noreply.github.com>

--- static/app/components/indicators.tsx
+++ static/app/components/indicators.tsx
@@ -37,7 +37,7 @@ export default Indicators;
 
 const Toasts = styled('div')`
   position: fixed;
-  right: calc(30px + var(--scrollbar-size, 0));
+  right: calc(30px + var(--scrollbar-size, 0px));
   bottom: 30px;
   z-index: ${p => p.theme.zIndex.toast};
 `;
