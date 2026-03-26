1a173c855a1e65df5a76ef4dafb21bfc62e6bf1c
Lyn Nagara <lynnagara@users.noreply.github.com>
2026-03-26 16:44:47 -0700

    flip back to clear_region_cache for now

--- src/sentry/receivers/outbox/control.py
+++ src/sentry/receivers/outbox/control.py
@@ -25,7 +25,7 @@ from sentry.receivers.outbox import maybe_process_tombstone
 from sentry.sentry_apps.models.sentry_app import SentryApp
 from sentry.sentry_apps.models.sentry_app_installation import SentryAppInstallation
 from sentry.sentry_apps.services.hook.service import hook_service
-from sentry.sentry_apps.tasks.sentry_apps import clear_cell_cache
+from sentry.sentry_apps.tasks.sentry_apps import clear_region_cache
 from sentry.users.models.identity import Identity
 from sentry.workflow_engine.service.action.service import action_service
 
@@ -59,7 +59,8 @@ def process_sentry_app_updates(object_identifier: int, region_name: str, **kwds:
 
     # Spawn a task to clear caches, as there can be 1000+ installations
     # for a sentry app.
-    clear_cell_cache.delay(sentry_app_id=sentry_app.id, cell_name=region_name)
+    # TODO(cells): switch to clear_cell_cache once deployed on all pods
+    clear_region_cache.delay(sentry_app_id=sentry_app.id, cell_name=region_name)
 
 
 @receiver(process_control_outbox, sender=OutboxCategory.SENTRY_APP_DELETE)
