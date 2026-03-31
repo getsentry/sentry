99ef9a4c41a0ee8a74640d4e7f8c80db6e3def5f
Dan Fuller <dfuller@sentry.io>
2026-03-31 15:18:05 -0700

    skip ghe for now

--- src/sentry/middleware/integrations/parsers/github_enterprise.py
+++ src/sentry/middleware/integrations/parsers/github_enterprise.py
@@ -4,8 +4,11 @@ import logging
 from collections.abc import Mapping
 from typing import Any
 
+from django.http import HttpRequest
+
 from sentry.hybridcloud.outbox.category import WebhookProviderIdentifier
 from sentry.integrations.github.webhook import get_github_external_id
+from sentry.integrations.github.webhook_types import GITHUB_WEBHOOK_TYPE_HEADER, GithubWebhookType
 from sentry.integrations.github_enterprise.webhook import GitHubEnterpriseWebhookEndpoint, get_host
 from sentry.integrations.types import IntegrationProviderSlug
 from sentry.middleware.integrations.parsers.github import GithubRequestParser
@@ -17,6 +20,13 @@ class GithubEnterpriseRequestParser(GithubRequestParser):
     provider = IntegrationProviderSlug.GITHUB_ENTERPRISE.value
     webhook_identifier = WebhookProviderIdentifier.GITHUB_ENTERPRISE
     webhook_endpoint = GitHubEnterpriseWebhookEndpoint
+
+    def should_route_to_control_silo(
+        self, parsed_event: Mapping[str, Any], request: HttpRequest
+    ) -> bool:
+        # GHE only routes installation events to control silo.
+        # installation_repositories is not yet supported for GHE.
+        return request.META.get(GITHUB_WEBHOOK_TYPE_HEADER) == GithubWebhookType.INSTALLATION
 
     def _get_external_id(self, event: Mapping[str, Any]) -> str | None:
         host = get_host(request=self.request)
