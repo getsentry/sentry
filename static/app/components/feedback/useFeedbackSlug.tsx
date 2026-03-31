5cd87258ec2c38656af0a1a60d34f10bac952ab9
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-03-31 11:15:20 -0700

    feat(feedback): Redirect to the feedback details page when given a projectName & eventId

---
+++ static/app/components/feedback/useFeedbackSlug.tsx
@@ -0,0 +1,19 @@
+import {createParser, useQueryState} from 'nuqs';
+
+type FeedbackSlug = {
+  feedbackId: string;
+  projectSlug: string;
+};
+
+const parseFeedbackSlug = createParser<FeedbackSlug>({
+  parse: value => {
+    if (value.includes(':')) {
+      const [projectSlug, feedbackId] = value.split(':');
+      return {projectSlug: projectSlug!, feedbackId: feedbackId!};
+    }
+    return {feedbackId: value, projectSlug: ''};
+  },
+  serialize: value => (value ? `${value.projectSlug}:${value.feedbackId}` : ''),
+});
+
+export const useFeedbackSlug = () => useQueryState('feedbackSlug', parseFeedbackSlug);
