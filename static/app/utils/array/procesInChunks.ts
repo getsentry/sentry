d5b47148c77df127dac449676f84934c8ae28388
Ryan Albrecht <ryan.albrecht@sentry.io>
2026-03-31 09:11:20 -0700

    add tests for processInChunks

--- static/app/utils/array/procesInChunks.ts
+++ static/app/utils/array/procesInChunks.ts
@@ -1,15 +1,15 @@
-interface Props<T> {
+interface Props<Item, Result> {
   chunkSize: number;
-  fn: (item: T) => Promise<unknown>;
-  items: T[];
+  fn: (item: Item) => Promise<Result>;
+  items: Item[];
 }
 
-export async function processInChunks<T>({
+export async function processInChunks<Item, Result>({
   items,
   chunkSize,
   fn,
-}: Props<T>): Promise<Array<PromiseSettledResult<unknown>>> {
-  const results: Array<PromiseSettledResult<unknown>> = [];
+}: Props<Item, Result>): Promise<Array<PromiseSettledResult<Result>>> {
+  const results: Array<PromiseSettledResult<Result>> = [];
   for (let i = 0; i < items.length; i += chunkSize) {
     const chunk = items.slice(i, i + chunkSize);
     const chunkResults = await Promise.allSettled(chunk.map(fn));
