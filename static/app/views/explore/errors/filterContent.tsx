fc1942370f14921433b187f4172f79c9ac716aad
nikkikapadia <nikki.kapadia@sentry.io>
2026-04-01 11:53:55 -0400

    feat(errors): add page filters and search bar UI

---
+++ static/app/views/explore/errors/filterContent.tsx
@@ -0,0 +1,51 @@
+import styled from '@emotion/styled';
+
+import {Grid} from '@sentry/scraps/layout';
+
+import * as Layout from 'sentry/components/layouts/thirds';
+import {DatePageFilter} from 'sentry/components/pageFilters/date/datePageFilter';
+import {EnvironmentPageFilter} from 'sentry/components/pageFilters/environment/environmentPageFilter';
+import {PageFilterBar} from 'sentry/components/pageFilters/pageFilterBar';
+import {ProjectPageFilter} from 'sentry/components/pageFilters/project/projectPageFilter';
+import {SearchQueryBuilderProvider} from 'sentry/components/searchQueryBuilder/context';
+import {t} from 'sentry/locale';
+import {TraceItemSearchQueryBuilder} from 'sentry/views/explore/components/traceItemSearchQueryBuilder';
+import {TraceItemDataset} from 'sentry/views/explore/types';
+
+export function ErrorsFilterSection() {
+  return (
+    <Layout.Main width="full">
+      <SearchQueryBuilderProvider
+        filterKeys={{}}
+        getTagValues={() => Promise.resolve([])}
+        initialQuery=""
+        searchSource="errors-filter"
+        placeholder={t('Search for errors, users, tags, and more')}
+      >
+        <Grid gap="md" columns={{sm: '1fr', md: 'minmax(300px, auto) 1fr min-content'}}>
+          <StyledPageFilterBar condensed>
+            <ProjectPageFilter />
+            <EnvironmentPageFilter />
+            <DatePageFilter />
+          </StyledPageFilterBar>
+
+          <TraceItemSearchQueryBuilder
+            initialQuery=""
+            searchSource="errors-filter"
+            booleanAttributes={{}}
+            booleanSecondaryAliases={{}}
+            itemType={TraceItemDataset.ERRORS}
+            numberAttributes={{}}
+            numberSecondaryAliases={{}}
+            stringAttributes={{}}
+            stringSecondaryAliases={{}}
+          />
+        </Grid>
+      </SearchQueryBuilderProvider>
+    </Layout.Main>
+  );
+}
+
+const StyledPageFilterBar = styled(PageFilterBar)`
+  width: auto;
+`;
