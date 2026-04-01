b3384a921e6426f154d3a6e64efdc5766fc89697
Simon Hellmayr <simon.hellmayr@sentry.io>
2026-04-01 10:17:28 +0200

    fix test

--- tests/sentry/users/models/test_user.py
+++ tests/sentry/users/models/test_user.py
@@ -13,6 +13,7 @@ from sentry.incidents.models.incident import IncidentActivity
 from sentry.models.activity import Activity
 from sentry.models.authidentity import AuthIdentity
 from sentry.models.dashboard import Dashboard, DashboardFavoriteUser
+from sentry.models.dynamicsampling import CustomDynamicSamplingRule
 from sentry.models.groupassignee import GroupAssignee
 from sentry.models.groupbookmark import GroupBookmark
 from sentry.models.groupsearchview import GroupSearchView
@@ -382,6 +383,7 @@ class UserMergeToTest(BackupTestCase, HybridCloudTestMixin):
         Activity,
         AlertRule,
         AlertRuleActivity,
+        CustomDynamicSamplingRule,
         Dashboard,
         DashboardFavoriteUser,
         GroupAssignee,
@@ -425,6 +427,7 @@ class UserMergeToTest(BackupTestCase, HybridCloudTestMixin):
         Activity,
         AlertRule,
         AlertRuleActivity,
+        CustomDynamicSamplingRule,
         Dashboard,
         DashboardFavoriteUser,
         GroupAssignee,
