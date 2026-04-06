600a528e173f2144eda6920aa9e3d849ade4e2dc
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2026-04-06 15:00:49 -0700

    ref(workflow_engine): Cleanup on project create code (#112246)
    # Description
    - Move the `ensure_default_detector` code out of the file for
    `process_detectors` and into `defaults/detectors.py`
    - Move the signals from the global folder to `workflow_engine` module
    for discoverability
    
    There shouldn't be any logic changes to this code, it's just a simple
    move from one file to another (this is setting up a few more PRs)

--- src/sentry/workflow_engine/receivers/project_detectors.py
+++ src/sentry/workflow_engine/receivers/project_detectors.py
@@ -1,5 +1,6 @@
 import logging
 from contextlib import contextmanager
+from typing import Any, Iterator
 
 import sentry_sdk
 from django.db.models.signals import post_save
@@ -7,7 +8,8 @@ from django.db.models.signals import post_save
 from sentry import features
 from sentry.models.project import Project
 from sentry.signals import project_created
-from sentry.workflow_engine.processors.detector import (
+from sentry.users.models.user import User
+from sentry.workflow_engine.defaults.detectors import (
     UnableToAcquireLockApiError,
     ensure_default_anomaly_detector,
     ensure_default_detectors,
@@ -17,7 +19,7 @@ logger = logging.getLogger(__name__)
 
 
 @contextmanager
-def disable_default_detector_creation():
+def disable_default_detector_creation() -> Iterator[None]:
     """
     Context manager that temporarily disconnects the signal handlers that create
     default detectors, preventing them from being created when a project is saved.
@@ -46,7 +48,11 @@ def disable_default_detector_creation():
         )
 
 
-def create_project_detectors(instance, created, **kwargs):
+def create_project_detectors(
+    instance: Project,
+    created: bool,
+    **kwargs: Any,
+) -> None:
     if created:
         try:
             ensure_default_detectors(instance)
@@ -54,7 +60,11 @@ def create_project_detectors(instance, created, **kwargs):
             sentry_sdk.capture_exception(e)
 
 
-def create_default_anomaly_detector(project: Project, user=None, user_id=None, **kwargs):
+def create_default_anomaly_detector(
+    project: Project,
+    user: User | None = None,
+    **kwargs: Any,
+) -> None:
     """
     Creates default anomaly detector when project is created, with the team as owner.
     This listens to project_created signal which provides user information.
@@ -76,28 +86,43 @@ def create_default_anomaly_detector(project: Project, user=None, user_id=None, *
             "organizations:anomaly-detection-alerts", project.organization, actor=user
         )
         detector = ensure_default_anomaly_detector(
-            project, owner_team_id=owner_team.id if owner_team else None, enabled=enabled
+            project,
+            owner_team_id=owner_team.id if owner_team else None,
+            enabled=enabled,
         )
         if detector:
             logger.info(
                 "create_default_anomaly_detector.created",
-                extra={"project_id": project.id, "detector_id": detector.id, "enabled": enabled},
+                extra={
+                    "project_id": project.id,
+                    "detector_id": detector.id,
+                    "enabled": enabled,
+                },
             )
     except UnableToAcquireLockApiError as e:
         logger.warning(
             "create_default_anomaly_detector.lock_failed",
-            extra={"project_id": project.id, "organization_id": project.organization_id},
+            extra={
+                "project_id": project.id,
+                "organization_id": project.organization_id,
+            },
         )
         sentry_sdk.capture_exception(e)
     except Exception:
         logger.exception(
             "create_default_anomaly_detector.failed",
-            extra={"project_id": project.id, "organization_id": project.organization_id},
+            extra={
+                "project_id": project.id,
+                "organization_id": project.organization_id,
+            },
         )
 
 
 post_save.connect(
-    create_project_detectors, sender=Project, dispatch_uid="create_project_detectors", weak=False
+    create_project_detectors,
+    sender=Project,
+    dispatch_uid="create_project_detectors",
+    weak=False,
 )
 project_created.connect(
     create_default_anomaly_detector,
