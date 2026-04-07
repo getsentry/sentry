191081ac87c6c3e77a0aca1514e182c81bda058b
Nar Saynorath <nar.saynorath@sentry.io>
2026-04-07 14:24:34 -0400

    feat(tracemetrics): Add feature flag for equations in metrics explorer (#112025)
    Feature flag to gate equations in metrics explorer. I figured as soon as
    this is completed we can release it separately from alerts since this
    will come first.

--- src/sentry/features/temporary.py
+++ src/sentry/features/temporary.py
@@ -481,6 +481,8 @@ def register_temporary_features(manager: FeatureManager) -> None:
     manager.add("organizations:tracemetrics-attributes-dropdown-side-panel", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable trace metrics product (known internally as tracemetrics) in UI and backend
     manager.add("organizations:tracemetrics-enabled", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
+    # Enable equations in trace metrics explore
+    manager.add("organizations:tracemetrics-equations-in-explore", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable trace metrics product to be ingested via Relay
     manager.add("organizations:tracemetrics-ingestion", OrganizationFeature, FeatureHandlerStrategy.FLAGPOLE, api_expose=True)
     # Enable trace metrics multi-metric selection in dashboards
